48df7f625bf7657acba9a2fcfdd5bd8c3febc386
Spencer Kimball <spencer.kimball@gmail.com>
2014-02-13 01:46:03 -0500

    Added copyright attribution and AUTHORS file.

--- gossip/gossip.go
+++ gossip/gossip.go
@@ -1,4 +1,20 @@
-/* Package gossip implements a protocol for sharing information
+// Copyright 2014 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.  See the License for the specific language governing
+// permissions and limitations under the License. See the AUTHORS file
+// for names of contributors.
+
+/*
+Package gossip implements a protocol for sharing information
 between Cockroach nodes using an ad-hoc, peer-to-peer network. The
 self-assembled network aims to minimize time for new information
 to reach each node, and minimize network traffic required.
