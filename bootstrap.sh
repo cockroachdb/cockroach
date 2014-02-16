b276bcd20bba9aa1335b1f5ce9dd2d9c6663e833
Peter Mattis <petermattis@gmail.com>
2014-02-16 14:05:40 -0500

    Updated README.md and bootstrap.sh to talk about gerrithub.io.
    Make sure "go vet" is installed. Make sure "git review" is installed. Make sure
    "git review" is configured. Added .gitreview configuration file.
    
    Change-Id: I83b657383bea64ebe46b04b1ff74c1211a301aa7

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
+# Verify "git review" is installed.
+if [ ! -x /usr/local/bin/git-review ]; then
+  cat <<EOF
+Please install git-review:
+
+  sudo pip install git-review
+EOF
+  exit 1
+fi
+
+# Verify "git config gitreview.username" is set.
+if [ -z "$(git config gitreview.username)" ]; then
+  cat <<EOF
+Please set gitreview username:
+
+  git config --add gitreview.username <username>
+EOF
+  exit 1
+fi
+
 set -e -x
 
 # Grab binaries required by git hooks.
 go get github.com/golang/lint/golint
+go get code.google.com/p/go.tools/cmd/vet
 go get code.google.com/p/go.tools/cmd/goimports
 
 # Create symlinks to all git hooks in your own .git dir.
-for f in $(ls -d githooks/*)
-do ln -s ../../$f .git/hooks
+for f in $(ls -d githooks/*); do
+  rm .git/hooks/$(basename $f)
+  ln -s ../../$f .git/hooks/$(basename $f)
 done && ls -al .git/hooks | grep githooks
