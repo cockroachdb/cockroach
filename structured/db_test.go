20a9f3d802c15c61c6a920581a730223fa772087
vagrant <vagrant@precise64.(none)>
2014-08-18 21:16:36 +0000

    fix circular dependency

--- structured/db_test.go
+++ structured/db_test.go
@@ -15,13 +15,14 @@
 //
 // Author: Andrew Bonventre (andybons@gmail.com)
 
-package structured
+package structured_test
 
 import (
 	"testing"
 
 	"github.com/cockroachdb/cockroach/server"
 	"github.com/cockroachdb/cockroach/storage/engine"
+	"github.com/cockroachdb/cockroach/structured"
 )
 
 func TestPutGetDeleteSchema(t *testing.T) {
@@ -34,7 +35,7 @@ func TestPutGetDeleteSchema(t *testing.T) {
 	if err != nil {
 		t.Fatalf("unable to boostrap cluster: %v", err)
 	}
-	db := NewDB(localDB)
+	db := structured.NewDB(localDB)
 	if err := db.PutSchema(s); err != nil {
 		t.Fatalf("could not register schema: %v", err)
 	}
@@ -54,4 +55,74 @@ func TestPutGetDeleteSchema(t *testing.T) {
 	if s != nil {
 		t.Errorf("expected schema to be nil; got %+v", s)
 	}
+}
+
+// User is a top-level table. User IDs are scattered, meaning a two
+// byte hash of the ID from the UserID sequence is prepended to yield
+// a randomly distributed keyspace.
+type User struct {
+	ID   int64  `roach:"id,pk,auto,scatter"`
+	Name string `roach:"na"`
+}
+
+// Identity is a top-level table as identities must be queried by key
+// at login.
+type Identity struct {
+	Key    string `roach:"ke,pk,scatter"` // (e.g. email:spencer.kimball@gmail.com, phone:6464174337)
+	UserID int64  `roach:"ui,fk=User.ID,ondelete=setnull"`
+}
+
+// Photo is a top-level table. While photos can only be contributed by
+// a single user, they are then shared and accessible to any number of
+// other users. See notes on scatter option for User.ID.
+type Photo struct {
+	ID       int64              `roach:"id,pk,auto=10000,scatter"`
+	UserID   int64              `roach:"ui,fk=User.ID,ondelete=setnull"`
+	Location structured.LatLong `roach:"lo,locationindex"`
+}
+
+// PhotoStream is a top-level table as streams are shared by multiple
+// users. The user ID for a photo stream has an index so that all
+// photo streams for a user may be easily queried. Photo stream titles
+// are full-text indexed for public searching.
+type PhotoStream struct {
+	ID     int64  `roach:"id,pk,auto,scatter"`
+	UserID int64  `roach:"ui,fk=User.ID"`
+	Title  string `roach:"ti,fulltextindex"`
+}
+
+// StreamPost is an interleaved join table (on PhotoStream), as the
+// posts which link a Photo to a PhotoStream are scoped to a single
+// PhotoStream. With interleaved tables, it's not necessary to specify
+// the "ondelete" roach option, as it is always set to "cascade".
+type StreamPost struct {
+	PhotoStreamID int64 `roach:"si,pk,fk=PhotoStream.ID,interleave"`
+	PhotoID       int64 `roach:"pi,pk,fk=Photo.ID"`
+	Timestamp     int64 `roach:"ti"`
+}
+
+// Comment is an interleaved table (on PhotoStream), as comments are
+// scoped to a single PhotoStream.
+type Comment struct {
+	PhotoStreamID int64  `roach:"si,pk,fk=PhotoStream.ID,interleave"`
+	ID            int64  `roach:"id,pk,auto"`
+	UserID        int64  `roach:"ui,fk=User.ID"`
+	Message       string `roach:"me,fulltextindex"`
+	Timestamp     int64  `roach:"ti"`
+}
+
+func createTestSchema() (*structured.Schema, error) {
+	sm := map[string]interface{}{
+		"us": User{},
+		"id": Identity{},
+		"ph": Photo{},
+		"ps": PhotoStream{},
+		"sp": StreamPost{},
+		"co": Comment{},
+	}
+	s, err := structured.NewGoSchema("PhotoDB", "pdb", sm)
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
 }
