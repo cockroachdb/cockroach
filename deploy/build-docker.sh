ab08d4cbcbe7d76bad1cd6c0613b63efae477725
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2014-11-01 22:36:10 +0100

    refactor Dockerfile into a base image and deploy
    The base image resides in cockroachdb/docker_base. This allows for CircleCI to
    cache only this image locally, and squash it when creating the cache without
    resorting to hacky shell scripting.
    
    Updated circle.yml to do what is described above.

--- deploy/build-docker.sh
+++ deploy/build-docker.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+REQUIRED_IMAGES="cockroachdb/docker_base"
+
 # Verify docker installation.
 source "$(dirname $0)/verify-docker.sh"
 
 # Create the docker cockroach image.
 echo "Building Docker Cockroach image..."
-docker build -t="cockroachdb/cockroach" "$(dirname $0)/../"
+for IMG in ${REQUIRED_IMAGES}; do
+  if [ $(docker images | awk '{ print $1 }' | grep -c "${IMG}") -eq 0 ]; then
+    docker build -t "${IMG}" "github.com/${IMG}.git"
+  fi
+done
+
+docker build -t "cockroachdb/cockroach" "$(dirname $0)/.."
