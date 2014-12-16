2eca0c976b5f96e34bb76b3bae12cf37f63c0ec0
Andrew Bonventre <abonventre@palantir.com>
2014-12-16 12:13:24 -0500

    fix test for now

--- storage/store.go
+++ storage/store.go
@@ -442,8 +442,7 @@ func (s *Store) Bootstrap(ident proto.StoreIdent) error {
 	if err != nil {
 		return util.Errorf("unable to scan engine to verify empty: %s", err)
 	} else if len(kvs) > 0 {
-		key, _, _ := engine.MVCCDecodeKey(kvs[0].Key)
-		return util.Errorf("non-empty engine %s (first key: %q)", s.engine, key)
+		return util.Errorf("non-empty engine %s (first key: %q)", s.engine, kvs[0].Key)
 	}
 	err = engine.MVCCPutProto(s.engine, nil, engine.KeyLocalIdent, proto.ZeroTimestamp, nil, &s.Ident)
 	return err
