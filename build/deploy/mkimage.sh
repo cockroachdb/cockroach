135d917858face601f6a8c0d0b2ed581f36f6245
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2015-02-21 00:54:09 +0100

    light refactoring of mkimage.sh and deploy Dockerfile
    prior to this change, creating a deploy image would leave a directory
    `./build/deploy/build` which contains files belonging to root (created
    by mounting the folder into the docker container).
    
    this change simply invokes `rm` in a similar container to clean up
    those temporary files.
    
    additionally the process of extracting files from the dev-build
    has been refactored slightly to avoid unnecessary copying.

--- build/deploy/mkimage.sh
+++ build/deploy/mkimage.sh
@@ -13,19 +13,16 @@ set -ex
 cd -P "$(dirname $0)"
 DIR=$(pwd -P)
 
-rm -rf resources ui cockroach
-mkdir -p build
-docker run -v "$(pwd)/build":/build "cockroachdb/cockroach-dev" shell "cd /cockroach && \
+rm -rf build && mkdir -p build
+docker run -v "${DIR}/build":/build "cockroachdb/cockroach-dev" shell "cd /cockroach && \
   rm -rf /build/*
   make clean build testbuild >/dev/null 2>&1 && \
   find . -name '*.test' -type f -printf "\"/build/%h \"" | xargs mkdir -p && \
   find . -name '*.test' -type f -exec mv {} "/build/{}" \; && \
-  cp -r resources /build/resources && \
-  cp -r ui /build/ui && \
+  cp -r {ui,resources} /build/ && \
   cp cockroach /build/cockroach"
 
-cp -r build/resources resources
-cp -r build/ui ./ui
-cp build/cockroach cockroach
 docker build -t cockroachdb/cockroach .
 docker run -v "${DIR}/build":/build cockroachdb/cockroach
+# Files in ./build may belong to root, so let's delete that folder via this hack.
+docker run -v "${DIR}/build":/build "cockroachdb/cockroach-dev" shell "rm -rf /build/*"
