e9ce2a06dd8774b5cfda21a9848a13b15a192304
Ben Darnell <ben@bendarnell.com>
2015-03-05 09:12:00 -0500

    Switch to mainline etcd branch, remove _vendor and submodule.

--- build/build-docker-dev.sh
+++ build/build-docker-dev.sh
@@ -3,20 +3,7 @@ set -e
 cd "$(dirname $0)/.."
 
 # Verify docker installation.
-source "./build/verify-docker.sh"
-
-# We bake the submodule SHAs into a docker layer that is built before
-# the submodules are cloned. This ensures that the submodules are
-# freshly cloned whenever the SHA pointers are updated.
-ETCD_SHA=$(git ls-tree HEAD _vendor/src/github.com/coreos/etcd | awk '{print $3}')
-WANTED_CONTENT=$(sed -e "s/ETCD_SHA/${ETCD_SHA}/g" \
-    < build/devbase/Dockerfile.template)
-if diff <(echo "${WANTED_CONTENT}") "build/devbase/Dockerfile" &> /dev/null; then
-  echo "Not updating Dockerfile from template, contents are equal"
-else
-  echo "Rewriting Dockerfile"
-  echo "${WANTED_CONTENT}" > build/devbase/Dockerfile
-fi
+./build/verify-docker.sh
 
 # Create the docker cockroach image.
 echo "Building Docker Cockroach images..."
