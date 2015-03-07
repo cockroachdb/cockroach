a7344f3f9127f03139d04ae3defe30a133f4c3cf
joezxy <joe.zxy@foxmail.com>
2015-03-07 22:29:04 +0800

    Fix the bug of address length comparision
    Suppose there are 5 replicas, and requires 3 successful response.
    In the existing code: if opts.N < len(addrs) , it will always fall into error branch.
    Should be modified to if len(addrs) < opts.N

--- rpc/send.go
+++ rpc/send.go
@@ -96,7 +96,9 @@ func (s SendError) CanRetry() bool { return s.canRetry }
 // number of required replies.
 func Send(opts Options, method string, addrs []net.Addr, getArgs func(addr net.Addr) interface{},
 	getReply func() interface{}, context *Context) ([]interface{}, error) {
-	if opts.N < len(addrs) {
+
+	// Return error if provided addrs is less. modified by joezxy 2015/03/07
+	if len(addrs) < opts.N {
 		return nil, SendError{
 			errMsg:   fmt.Sprintf("insufficient replicas (%d) to satisfy send request of %d", len(addrs), opts.N),
 			canRetry: false,
