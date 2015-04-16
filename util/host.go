5eafcacf30d2a6d2747ddcd098e7cb5296578718
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2015-04-16 10:59:07 -0400

    add :8080 -> hostname:8080 completion helper
    this is hopefully a good way of dealing with this issue related
    to #662, #672.

---
+++ util/host.go
@@ -0,0 +1,38 @@
+// Copyright 2015 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License. See the AUTHORS file
+// for names of contributors.
+//
+// Author: Tobias Schottdorf (tobias.schottdorf@gmail.com)
+
+package util
+
+import (
+	"os"
+	"strings"
+)
+
+// EnsureHost takes a host:port pair, where the host portion is optional.
+// If a host is present, the output is equal to the input. Otherwise,
+// the output will contain a host portion equal to the hostname (or
+// "127.0.0.1" as a fallback).
+func EnsureHost(addr string) string {
+	if !strings.HasPrefix(addr, ":") {
+		return addr
+	}
+	host, err := os.Hostname()
+	if err != nil {
+		host = "127.0.0.1"
+	}
+	return host + addr
+}
