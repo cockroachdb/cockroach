668e8bd9ebcc10ea6b78daa55c67d00be1d12781
Vivek Menezes <vivek1@viveks-MacBook-Pro.local>
2015-04-30 14:58:15 -0400

    Added ReplyBytes() as a convenience method that returns the []bytes in a Reply from a Get()

--- client/call.go
+++ client/call.go
@@ -48,6 +48,14 @@ func (c *Call) Method() proto.Method {
 	return c.Args.Method()
 }
 
+// Method returns bytes received on a Get reply.
+func (c *Call) ReplyBytes() []byte {
+	if gr := c.Reply.(*proto.GetResponse); gr.Value != nil {
+		return gr.Value.Bytes
+	}
+	return nil
+}
+
 // Get returns a Call object initialized to get the value at key.
 func Get(key proto.Key) Call {
 	return Call{
