25d8c0e2a5ce3dc5f2f275594521e7bf8f87fc74
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2015-05-12 18:13:05 -0400

    disable two flaky tests temporarily

--- storage/client_raft_test.go
+++ storage/client_raft_test.go
@@ -572,6 +572,7 @@ func TestProgressWithDownNode(t *testing.T) {
 
 func TestReplicateAddAndRemove(t *testing.T) {
 	defer leaktest.AfterTest(t)
+	t.Skip("currently disabled due to data race, see #1010")
 
 	// Run the test twice, once adding the replacement before removing
 	// the downed node, and once removing the downed node first.
