ecc1a62c8b7562725f2cc89dfcf30711763aeca7
marc <marc@cockroachlabs.com>
2015-06-01 16:29:23 -0400

    add simple multi-user acceptance test

--- acceptance/util.go
+++ acceptance/util.go
@@ -27,12 +27,19 @@ import (
 	"github.com/cockroachdb/cockroach/client"
 	"github.com/cockroachdb/cockroach/keys"
 	"github.com/cockroachdb/cockroach/proto"
+	"github.com/cockroachdb/cockroach/util"
+	gogoproto "github.com/gogo/protobuf/proto"
 )
 
 // makeDBClient creates a DB client for node 'i' using the cluster certs dir.
 func makeDBClient(t *testing.T, cluster *localcluster.Cluster, node int) *client.DB {
+	return makeDBClientForUser(t, cluster, "root", node)
+}
+
+// makeDBClientForUser creates a DB client for node 'i' and user 'user'.
+func makeDBClientForUser(t *testing.T, cluster *localcluster.Cluster, user string, node int) *client.DB {
 	// We always run these tests with certs.
-	c, err := client.Open("https://root@" +
+	c, err := client.Open("https://" + user + "@" +
 		cluster.Nodes[node].Addr("").String() +
 		"?certs=" + cluster.CertsDir)
 	if err != nil {
@@ -58,4 +65,35 @@ func setDefaultRangeMaxBytes(t *testing.T, c *client.DB, maxBytes int64) {
 	if _, err := c.Put(keys.ConfigZonePrefix, zone); err != nil {
 		t.Fatal(err)
 	}
+}
+
+// getPermConfig fetches the permissions config for 'prefix'.
+func getPermConfig(client *client.DB, prefix string) (*proto.PermConfig, error) {
+	r, err := client.Get(keys.MakeKey(keys.ConfigPermissionPrefix, proto.Key(prefix)))
+	if err != nil {
+		return nil, err
+	}
+	if len(r.Rows) == 0 {
+		return nil, util.Errorf("no permissions config for prefix %q", prefix)
+	}
+	config := &proto.PermConfig{}
+	msg := r.Rows[0].Value.([]byte)
+	if err := gogoproto.Unmarshal(msg, config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+// putPermConfig writes the permissions config for 'prefix'.
+func putPermConfig(client *client.DB, prefix string, config *proto.PermConfig) error {
+	msg, err := gogoproto.Marshal(config)
+	if err != nil {
+		return err
+	}
+	r, err := client.Put(keys.MakeKey(keys.ConfigPermissionPrefix, proto.Key(prefix)), msg)
+	if err != nil {
+		return err
+	}
+	return r.Err
 }
