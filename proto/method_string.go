9cc7f6d1cbb7bf6a9e07d7daf4d73c4d264d8da7
Tamir Duberstein <tamird@gmail.com>
2015-06-03 13:49:31 -0400

    Update dependencies

--- proto/method_string.go
+++ proto/method_string.go
@@ -9,7 +9,7 @@ const _Method_name = "ContainsGetPutConditionalPutIncrementDeleteDeleteRangeScan
 var _Method_index = [...]uint16{0, 8, 11, 14, 28, 37, 43, 54, 58, 72, 81, 94, 108, 113, 123, 133, 152, 172, 182, 197, 218, 244, 257, 276, 295, 308}
 
 func (i Method) String() string {
-	if i < 0 || i+1 >= Method(len(_Method_index)) {
+	if i < 0 || i >= Method(len(_Method_index)-1) {
 		return fmt.Sprintf("Method(%d)", i)
 	}
 	return _Method_name[_Method_index[i]:_Method_index[i+1]]
