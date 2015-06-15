3fe1c7c7fb892e877cc0579aef036b836ef631e7
Matt Tracy <matt@cockroachlabs.com>
2015-06-15 16:38:35 -0400

    Remove server/status/status.go
    Remove this unused file, which was a very early skeleton for a status system
    that was not implemented.
    
    Commit also removes raw /_status/ handler, which just returned an empty
    structure with no data. Two tests used this method to simply verify that the
    server returns well-structured replies; these tests have been modified to use
    /_status/nodes instead.

--- server/status_test.go
+++ server/status_test.go
@@ -97,7 +97,7 @@ func TestStatusJson(t *testing.T) {
 		t.Fatal(err)
 	}
 	testCases := []TestCase{
-		{statusKeyPrefix, "{}"},
+		{statusNodeKeyPrefix, "\"d\":"},
 	}
 	// Test the /_status/local/ endpoint only in a go release branch.
 	if !strings.HasPrefix(runtime.Version(), "devel") {
