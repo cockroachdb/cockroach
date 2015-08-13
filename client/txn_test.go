1e471ef3703e15202011804c1193baa083db2f30
Vivek Menezes <vivek.menezes@gmail.com>
2015-08-12 21:24:52 -0400

    gorename -from '"github.com/cockroachdb/cockroach/client".Txn.txn' -to Txn

--- client/txn_test.go
+++ client/txn_test.go
@@ -116,7 +116,7 @@ func TestTxnResetTxnOnAbort(t *testing.T) {
 	txn.db.Sender.Send(context.Background(),
 		proto.Call{Args: testPutReq, Reply: &proto.PutResponse{}})
 
-	if len(txn.txn.ID) != 0 {
+	if len(txn.Txn.ID) != 0 {
 		t.Errorf("expected txn to be cleared")
 	}
 }
