14bab2607d9d3bdd3242cc3c9fff7391a4604f93
Tamir Duberstein <tamird@gmail.com>
2015-09-03 15:40:33 -0400

    client: remove ./rpc

--- client/rpc_sender.go
+++ client/rpc_sender.go
@@ -15,7 +15,7 @@
 //
 // Author: Peter Mattis (peter@cockroachlabs.com)
 
-package rpc
+package client
 
 import (
 	"net"
@@ -24,37 +24,33 @@ import (
 	"golang.org/x/net/context"
 
 	"github.com/cockroachdb/cockroach/base"
-	"github.com/cockroachdb/cockroach/client"
 	"github.com/cockroachdb/cockroach/proto"
-	roachrpc "github.com/cockroachdb/cockroach/rpc"
+	"github.com/cockroachdb/cockroach/rpc"
 	"github.com/cockroachdb/cockroach/util/hlc"
 	"github.com/cockroachdb/cockroach/util/log"
 	"github.com/cockroachdb/cockroach/util/retry"
 )
 
 func init() {
-	f := func(u *url.URL, ctx *base.Context, retryOpts retry.Options) (client.Sender, error) {
+	f := func(u *url.URL, ctx *base.Context, retryOpts retry.Options) (Sender, error) {
 		ctx.Insecure = (u.Scheme != "rpcs")
-		return newSender(u.Host, ctx, retryOpts)
+		return newRPCSender(u.Host, ctx, retryOpts)
 	}
-	client.RegisterSender("rpc", f)
-	client.RegisterSender("rpcs", f)
+	RegisterSender("rpc", f)
+	RegisterSender("rpcs", f)
 }
 
-// Sender is an implementation of Sender which exposes the
+// rpcSender is an implementation of Sender which exposes the
 // Key-Value database provided by a Cockroach cluster by connecting
 // via RPC to a Cockroach node. Overly-busy nodes will redirect this
 // client to other nodes.
-//
-// TODO(pmattis): This class is insufficiently tested and not intended
-// for use outside of benchmarking.
-type Sender struct {
-	client    *roachrpc.Client
+type rpcSender struct {
+	client    *rpc.Client
 	retryOpts retry.Options
 }
 
-// newSender returns a new instance of Sender.
-func newSender(server string, context *base.Context, retryOpts retry.Options) (*Sender, error) {
+// newRPCSender returns a new instance of rpcSender.
+func newRPCSender(server string, context *base.Context, retryOpts retry.Options) (*rpcSender, error) {
 	addr, err := net.ResolveTCPAddr("tcp", server)
 	if err != nil {
 		return nil, err
@@ -63,9 +59,9 @@ func newSender(server string, context *base.Context, retryOpts retry.Options) (*
 	if context.Insecure {
 		log.Warning("running in insecure mode, this is strongly discouraged. See --insecure and --certs.")
 	}
-	ctx := roachrpc.NewContext(context, hlc.NewClock(hlc.UnixNano), nil)
-	client := roachrpc.NewClient(addr, ctx)
-	return &Sender{
+	ctx := rpc.NewContext(context, hlc.NewClock(hlc.UnixNano), nil)
+	client := rpc.NewClient(addr, ctx)
+	return &rpcSender{
 		client:    client,
 		retryOpts: retryOpts,
 	}, nil
@@ -77,7 +73,7 @@ func newSender(server string, context *base.Context, retryOpts retry.Options) (*
 // command ID to avoid reporting failure when in fact the command may have gone
 // through and been executed successfully. We retry here to eventually get
 // through with the same client command ID and be given the cached response.
-func (s *Sender) Send(_ context.Context, call proto.Call) {
+func (s *rpcSender) Send(_ context.Context, call proto.Call) {
 	var err error
 	for r := retry.Start(s.retryOpts); r.Next(); {
 		select {
@@ -103,6 +99,7 @@ func (s *Sender) Send(_ context.Context, call proto.Call) {
 		// On successful post, we're done with retry loop.
 		break
 	}
+
 	if err != nil {
 		call.Reply.Header().SetGoError(err)
 	}
