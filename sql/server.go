0df0dce5d3abff8e0b0495aa584bc1ac98daa5f1
Tamir Duberstein <tamird@gmail.com>
2015-09-07 07:55:20 -0400

    sql: BEGIN inside a transaction is an error

--- sql/server.go
+++ sql/server.go
@@ -33,6 +33,7 @@ import (
 )
 
 var errTransactionAborted = errors.New("current transaction is aborted, commands ignored until end of transaction block")
+var errTransactionInProgress = errors.New("there is already a transaction in progress")
 
 type server struct {
 	db client.DB
@@ -99,20 +100,23 @@ func (s server) execStmts(sql string, params parameters, planMaker *planner) dri
 
 func (s server) execStmt(stmt parser.Statement, params parameters, planMaker *planner) (driver.Result, error) {
 	var result driver.Result
-	if planMaker.txn == nil {
-		if _, ok := stmt.(*parser.BeginTransaction); ok {
-			// Start a transaction here and not in planMaker to prevent begin
-			// transaction from being called within an auto-transaction below.
-			planMaker.txn = client.NewTxn(s.db)
-			planMaker.txn.SetDebugName("sql", 0)
+	switch stmt.(type) {
+	case *parser.BeginTransaction:
+		if planMaker.txn != nil {
+			return result, errTransactionInProgress
 		}
-	} else if planMaker.txn.Proto.Status == proto.ABORTED {
-		switch stmt.(type) {
-		case *parser.CommitTransaction, *parser.RollbackTransaction:
+		// Start a transaction here and not in planMaker to prevent begin
+		// transaction from being called within an auto-transaction below.
+		planMaker.txn = client.NewTxn(s.db)
+		planMaker.txn.SetDebugName("sql", 0)
+	case *parser.CommitTransaction, *parser.RollbackTransaction:
+		if planMaker.txn != nil && planMaker.txn.Proto.Status == proto.ABORTED {
 			// Reset to allow starting a new transaction.
 			planMaker.txn = nil
 			return result, nil
-		default:
+		}
+	default:
+		if planMaker.txn != nil && planMaker.txn.Proto.Status == proto.ABORTED {
 			return result, errTransactionAborted
 		}
 	}
