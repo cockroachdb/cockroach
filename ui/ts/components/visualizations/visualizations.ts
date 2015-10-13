f2d5e79ce3d87c6ba755f1b6cefe291a1bac020c
Max Lang <max@cockroachlabs.com>
2015-10-13 13:53:52 -0400

    Build out number visualization with wrapper

--- ui/ts/components/visualizations/visualizations.ts
+++ ui/ts/components/visualizations/visualizations.ts
@@ -8,4 +8,36 @@
 
 module Visualizations {
   "use strict";
+  import MithrilVirtualElement = _mithril.MithrilVirtualElement;
+
+  export interface VisualizationData {
+    key?: string;
+    value: number;
+  }
+
+  export interface VisualizationWrapperInfo {
+    title?: string;
+    virtualVisualizationElement: MithrilVirtualElement;
+  }
+
+  export module VisualizationWrapper {
+    import MithrilVirtualElement = _mithril.MithrilVirtualElement;
+
+    export function controller(): void {
+    }
+
+    export function view(ctrl: any, info: VisualizationWrapperInfo): MithrilVirtualElement {
+
+      return m(
+        ".visualization-wrapper",
+        [
+          // TODO: pass in and display info icon tooltip
+          m(".viz-top", m(".viz-info-icon", m(".icon-cockroach-17"))), // Icon Cockroach 17 is the info icon.
+         info.virtualVisualizationElement,
+          m(".viz-bottom", m(".viz-title", info.title))
+        ]
+      );
+    }
+  }
+
 }
