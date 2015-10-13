390cfa3ef92047b6f18e26c3f5a5ca51fbf924f4
Max Lang <max@cockroachlabs.com>
2015-10-13 13:49:59 -0400

    Stub out Visualizations module

---
+++ ui/ts/components/visualizations/pie.ts
@@ -0,0 +1,13 @@
+/// <reference path="../../external/mithril/mithril.d.ts" />
+/// <reference path="../../../typings/d3/d3.d.ts" />
+
+// Author: Max Lang (max@cockroachlabs.com)
+
+module Visualizations {
+  "use strict";
+
+  export module PieVisualization {
+
+  }
+
+}
---
+++ ui/ts/components/visualizations/visualizations.ts
@@ -0,0 +1,11 @@
+/// <reference path="./bar.ts" />
+/// <reference path="./bullet.ts" />
+/// <reference path="./line.ts" />
+/// <reference path="./number.ts" />
+/// <reference path="./pie.ts" />
+
+// Author: Max Lang (max@cockroachlabs.com)
+
+module Visualizations {
+  "use strict";
+}
