a6ddbff5edab9399c899361115940d988cc890c5
Max Lang <max@cockroachlabs.com>
2015-10-13 13:53:52 -0400

    Build out number visualization with wrapper

--- ui/ts/components/visualizations/number.ts
+++ ui/ts/components/visualizations/number.ts
@@ -5,6 +5,29 @@
 
 module Visualizations {
   "use strict";
+
+  import MithrilVirtualElement = _mithril.MithrilVirtualElement;
+
+  let formatFn: (n: number) => string;
+
   export module NumberVisualization {
+    export function controller(): any {
+      return {};
+    }
+
+    export function view(ctrl: any, info: VisualizationInfo): MithrilVirtualElement {
+
+      formatFn = info.visualizationArguments.formatFn || d3.format(info.visualizationArguments.format || ".3s");
+
+      let data: VisualizationData = _.isArray(info.data) ? info.data[0] : info.data;
+
+      return m(
+        ".visualization",
+        {style: "width:" + info.width.toString() + "px; height:" + info.height.toString() + "px; " +
+          "line-height:" + info.height.toString() + "px; " },
+        [
+          m(".number", {style: "zoom:" + (info.visualizationArguments.zoom || "100%") + ";"}, formatFn(data.value))
+        ]);
+    }
   }
 }
