47488f7d83f4bf26f0a86a689aabfc965b4414e0
Tamir Duberstein <tamird@gmail.com>
2015-10-22 18:55:02 -0400

    [TBS] cleanup

--- sql/parser/normalize.go
+++ sql/parser/normalize.go
@@ -28,37 +28,235 @@ type normalizableExpr interface {
 }
 
 func (expr *AndExpr) normalize(v *normalizeVisitor) Expr {
-	return v.normalizeAndExpr(expr)
+	// Use short-circuit evaluation to simplify AND expressions.
+	if isConst(expr.Left) {
+		expr.Left, v.err = expr.Left.Eval(v.ctx)
+		if v.err != nil {
+			return expr
+		}
+		if expr.Left != DNull {
+			if d, err := getBool(expr.Left.(Datum)); err != nil {
+				return DNull
+			} else if !d {
+				return expr.Left
+			}
+			return expr.Right
+		}
+		return expr
+	}
+	if isConst(expr.Right) {
+		expr.Right, v.err = expr.Right.Eval(v.ctx)
+		if v.err != nil {
+			return expr
+		}
+		if expr.Right != DNull {
+			if d, err := getBool(expr.Right.(Datum)); err != nil {
+				return DNull
+			} else if d {
+				return expr.Left
+			}
+			return expr.Right
+		}
+		return expr
+	}
+	return expr
 }
 
-// func (expr *BinaryExpr) normalize(v *normalizeVisitor) Expr   {}
-// func (expr *CaseExpr) normalize(v *normalizeVisitor) Expr     {}
-// func (expr *CastExpr) normalize(v *normalizeVisitor) Expr     {}
-// func (expr *CoalesceExpr) normalize(v *normalizeVisitor) Expr {}
 func (expr *ComparisonExpr) normalize(v *normalizeVisitor) Expr {
-	return v.normalizeComparisonExpr(expr)
+	switch expr.Operator {
+	case EQ, GE, GT, LE, LT:
+		// We want var nodes (VariableExpr, QualifiedName, etc) to be immediate
+		// children of the comparison expression and not second or third
+		// children. That is, we want trees that look like:
+		//
+		//    cmp            cmp
+		//   /   \          /   \
+		//  a    op        op    a
+		//      /  \      /  \
+		//     1    2    1    2
+		//
+		// Not trees that look like:
+		//
+		//      cmp          cmp        cmp          cmp
+		//     /   \        /   \      /   \        /   \
+		//    op    2      op    2    1    op      1    op
+		//   /  \         /  \            /  \         /  \
+		//  a    1       1    a          a    2       2    a
+		//
+		// We loop attempting to simplify the comparison expression. As a
+		// pre-condition, we know there is at least one variable in the expression
+		// tree or we would not have entered this code path.
+		for {
+			if isConst(expr.Left) {
+				switch expr.Right.(type) {
+				case *BinaryExpr, VariableExpr, *QualifiedName, ValArg:
+					break
+				default:
+					return expr
+				}
+				// The left side is const and the right side is a binary expression or a
+				// variable. Flip the comparison op so that the right side is const and
+				// the left side is a binary expression or variable.
+				expr.Operator = invertComparisonOp(expr.Operator)
+				expr.Left, expr.Right = expr.Right, expr.Left
+			} else if !isConst(expr.Right) {
+				return expr
+			}
+
+			left, ok := expr.Left.(*BinaryExpr)
+			if !ok {
+				return expr
+			}
+
+			// The right is const and the left side is a binary expression. Rotate the
+			// comparison combining portions that are const.
+
+			switch {
+			case isConst(left.Right):
+				//        cmp          cmp
+				//       /   \        /   \
+				//    [+-/]   2  ->  a   [-+*]
+				//   /     \            /     \
+				//  a       1          2       1
+
+				switch left.Operator {
+				case Plus, Minus, Div:
+					expr.Left = left.Left
+					left.Left = expr.Right
+					if left.Operator == Plus {
+						left.Operator = Minus
+					} else if left.Operator == Minus {
+						left.Operator = Plus
+					} else {
+						left.Operator = Mult
+					}
+					// Clear the function cache now that we've changed the operator.
+					left.fn.fn = nil
+					expr.Right, v.err = left.Eval(v.ctx)
+					if v.err != nil {
+						return nil
+					}
+					if !isVar(expr.Left) {
+						// Continue as long as the left side of the comparison is not a
+						// variable.
+						continue
+					}
+				}
+
+			case isConst(left.Left):
+				//       cmp              cmp
+				//      /   \            /   \
+				//    [+-]   2  ->     [+-]   a
+				//   /    \           /    \
+				//  1      a         1      2
+
+				switch left.Operator {
+				case Plus, Minus:
+					left.Right, expr.Right = expr.Right, left.Right
+					if left.Operator == Plus {
+						left.Operator = Minus
+						left.Left, left.Right = left.Right, left.Left
+					} else {
+						expr.Operator = invertComparisonOp(expr.Operator)
+					}
+					expr.Left, v.err = left.Eval(v.ctx)
+					if v.err != nil {
+						return nil
+					}
+					expr.Left, expr.Right = expr.Right, expr.Left
+					if !isVar(expr.Left) {
+						// Continue as long as the left side of the comparison is not a
+						// variable.
+						continue
+					}
+				}
+			}
+
+			// We've run out of work to do.
+			break
+		}
+	case In, NotIn:
+		// If the right tuple in an In or NotIn comparison expression is constant, it can
+		// be normalized.
+		tuple, ok := expr.Right.(DTuple)
+		if ok {
+			tuple.Normalize()
+			expr.Right = tuple
+		}
+	}
+
+	return expr
 }
 
-// func (expr *ExistsExpr) normalize(v *normalizeVisitor) Expr   {}
-// func (expr *FuncExpr) normalize(v *normalizeVisitor) Expr     {}
-// func (expr *IfExpr) normalize(v *normalizeVisitor) Expr       {}
-// func (expr *IsOfTypeExpr) normalize(v *normalizeVisitor) Expr {}
-// func (expr *NotExpr) normalize(v *normalizeVisitor) Expr      {}
-// func (expr *NullIfExpr) normalize(v *normalizeVisitor) Expr   {}
 func (expr *OrExpr) normalize(v *normalizeVisitor) Expr {
-	return v.normalizeOrExpr(expr)
+	// Use short-circuit evaluation to simplify OR expressions.
+	if isConst(expr.Left) {
+		expr.Left, v.err = expr.Left.Eval(v.ctx)
+		if v.err != nil {
+			return expr
+		}
+		if expr.Left != DNull {
+			if d, err := getBool(expr.Left.(Datum)); err != nil {
+				return DNull
+			} else if d {
+				return expr.Left
+			}
+			return expr.Right
+		}
+	}
+	if isConst(expr.Right) {
+		expr.Right, v.err = expr.Right.Eval(v.ctx)
+		if v.err != nil {
+			return expr
+		}
+		if expr.Right != DNull {
+			if d, err := getBool(expr.Right.(Datum)); err != nil {
+				return DNull
+			} else if d {
+				return expr.Right
+			}
+			return expr.Left
+		}
+	}
+	return expr
 }
 
 func (expr *ParenExpr) normalize(v *normalizeVisitor) Expr {
 	return expr.Expr
 }
 
-// func (expr *QualifiedName) normalize(v *normalizeVisitor) Expr {}
 func (expr *RangeCond) normalize(v *normalizeVisitor) Expr {
-	return v.normalizeRangeCond(expr)
+	if expr.Not {
+		// "a NOT BETWEEN b AND c" -> "a < b OR a > c"
+		return &OrExpr{
+			Left: &ComparisonExpr{
+				Operator: LT,
+				Left:     expr.Left,
+				Right:    expr.From,
+			},
+			Right: &ComparisonExpr{
+				Operator: GT,
+				Left:     expr.Left,
+				Right:    expr.To,
+			},
+		}
+	}
+
+	// "a BETWEEN b AND c" -> "a >= b AND a <= c"
+	return &AndExpr{
+		Left: &ComparisonExpr{
+			Operator: GE,
+			Left:     expr.Left,
+			Right:    expr.From,
+		},
+		Right: &ComparisonExpr{
+			Operator: LE,
+			Left:     expr.Left,
+			Right:    expr.To,
+		},
+	}
 }
 
-// func (expr *Subquery) normalize(v *normalizeVisitor) Expr      {}
 func (expr *UnaryExpr) normalize(v *normalizeVisitor) Expr {
 	// Ugliness: when we see a UnaryMinus, check to see if the expression
 	// being negated is math.MinInt64. This IntVal is only possible if we
@@ -81,25 +279,10 @@ func (expr *UnaryExpr) normalize(v *normalizeVisitor) Expr {
 	return expr
 }
 
-// func (expr Array) normalize(v *normalizeVisitor) Expr          {}
-// func (expr DefaultVal) normalize(v *normalizeVisitor) Expr     {}
-// func (expr IntVal) normalize(v *normalizeVisitor) Expr         {}
-// func (expr NumVal) normalize(v *normalizeVisitor) Expr         {}
 func (expr Row) normalize(v *normalizeVisitor) Expr {
 	return Tuple(expr)
 }
 
-// func (expr Tuple) normalize(v *normalizeVisitor) Expr          {}
-// func (expr ValArg) normalize(v *normalizeVisitor) Expr         {}
-// func (expr DBool) normalize(v *normalizeVisitor) Expr          {}
-// func (expr DBytes) normalize(v *normalizeVisitor) Expr         {}
-// func (expr DDate) normalize(v *normalizeVisitor) Expr          {}
-// func (expr DFloat) normalize(v *normalizeVisitor) Expr         {}
-// func (expr DInt) normalize(v *normalizeVisitor) Expr           {}
-// func (expr DInterval) normalize(v *normalizeVisitor) Expr      {}
-// func (expr dNull) normalize(v *normalizeVisitor) Expr          {}
-// func (expr DString) normalize(v *normalizeVisitor) Expr        {}
-// func (expr DTimestamp) normalize(v *normalizeVisitor) Expr     {}
 func (expr DTuple) normalize(_ *normalizeVisitor) Expr {
 	expr.Normalize()
 	return expr
@@ -165,234 +348,6 @@ func (v *normalizeVisitor) Visit(expr Expr, pre bool) (Visitor, Expr) {
 	}
 
 	return v, expr
-}
-
-func (v *normalizeVisitor) normalizeRangeCond(n *RangeCond) Expr {
-	var expr Expr
-	if n.Not {
-		// "a NOT BETWEEN b AND c" -> "a < b OR a > c"
-		expr = &OrExpr{
-			Left: &ComparisonExpr{
-				Operator: LT,
-				Left:     n.Left,
-				Right:    n.From,
-			},
-			Right: &ComparisonExpr{
-				Operator: GT,
-				Left:     n.Left,
-				Right:    n.To,
-			},
-		}
-	} else {
-		// "a BETWEEN b AND c" -> "a >= b AND a <= c"
-		expr = &AndExpr{
-			Left: &ComparisonExpr{
-				Operator: GE,
-				Left:     n.Left,
-				Right:    n.From,
-			},
-			Right: &ComparisonExpr{
-				Operator: LE,
-				Left:     n.Left,
-				Right:    n.To,
-			},
-		}
-	}
-	return expr
-}
-
-func (v *normalizeVisitor) normalizeAndExpr(n *AndExpr) Expr {
-	// Use short-circuit evaluation to simplify AND expressions.
-	if isConst(n.Left) {
-		n.Left, v.err = n.Left.Eval(v.ctx)
-		if v.err != nil {
-			return n
-		}
-		if n.Left != DNull {
-			if d, err := getBool(n.Left.(Datum)); err != nil {
-				return DNull
-			} else if !d {
-				return n.Left
-			}
-			return n.Right
-		}
-		return n
-	}
-	if isConst(n.Right) {
-		n.Right, v.err = n.Right.Eval(v.ctx)
-		if v.err != nil {
-			return n
-		}
-		if n.Right != DNull {
-			if d, err := getBool(n.Right.(Datum)); err != nil {
-				return DNull
-			} else if d {
-				return n.Left
-			}
-			return n.Right
-		}
-		return n
-	}
-	return n
-}
-
-func (v *normalizeVisitor) normalizeOrExpr(n *OrExpr) Expr {
-	// Use short-circuit evaluation to simplify OR expressions.
-	if isConst(n.Left) {
-		n.Left, v.err = n.Left.Eval(v.ctx)
-		if v.err != nil {
-			return n
-		}
-		if n.Left != DNull {
-			if d, err := getBool(n.Left.(Datum)); err != nil {
-				return DNull
-			} else if d {
-				return n.Left
-			}
-			return n.Right
-		}
-	}
-	if isConst(n.Right) {
-		n.Right, v.err = n.Right.Eval(v.ctx)
-		if v.err != nil {
-			return n
-		}
-		if n.Right != DNull {
-			if d, err := getBool(n.Right.(Datum)); err != nil {
-				return DNull
-			} else if d {
-				return n.Right
-			}
-			return n.Left
-		}
-	}
-	return n
-}
-
-func (v *normalizeVisitor) normalizeComparisonExpr(n *ComparisonExpr) Expr {
-	switch n.Operator {
-	case EQ, GE, GT, LE, LT:
-		// We want var nodes (VariableExpr, QualifiedName, etc) to be immediate
-		// children of the comparison expression and not second or third
-		// children. That is, we want trees that look like:
-		//
-		//    cmp            cmp
-		//   /   \          /   \
-		//  a    op        op    a
-		//      /  \      /  \
-		//     1    2    1    2
-		//
-		// Not trees that look like:
-		//
-		//      cmp          cmp        cmp          cmp
-		//     /   \        /   \      /   \        /   \
-		//    op    2      op    2    1    op      1    op
-		//   /  \         /  \            /  \         /  \
-		//  a    1       1    a          a    2       2    a
-		//
-		// We loop attempting to simplify the comparison expression. As a
-		// pre-condition, we know there is at least one variable in the expression
-		// tree or we would not have entered this code path.
-		for {
-			if isConst(n.Left) {
-				switch n.Right.(type) {
-				case *BinaryExpr, VariableExpr, *QualifiedName, ValArg:
-					break
-				default:
-					return n
-				}
-				// The left side is const and the right side is a binary expression or a
-				// variable. Flip the comparison op so that the right side is const and
-				// the left side is a binary expression or variable.
-				n.Operator = invertComparisonOp(n.Operator)
-				n.Left, n.Right = n.Right, n.Left
-			} else if !isConst(n.Right) {
-				return n
-			}
-
-			left, ok := n.Left.(*BinaryExpr)
-			if !ok {
-				return n
-			}
-
-			// The right is const and the left side is a binary expression. Rotate the
-			// comparison combining portions that are const.
-
-			switch {
-			case isConst(left.Right):
-				//        cmp          cmp
-				//       /   \        /   \
-				//    [+-/]   2  ->  a   [-+*]
-				//   /     \            /     \
-				//  a       1          2       1
-
-				switch left.Operator {
-				case Plus, Minus, Div:
-					n.Left = left.Left
-					left.Left = n.Right
-					if left.Operator == Plus {
-						left.Operator = Minus
-					} else if left.Operator == Minus {
-						left.Operator = Plus
-					} else {
-						left.Operator = Mult
-					}
-					// Clear the function cache now that we've changed the operator.
-					left.fn.fn = nil
-					n.Right, v.err = left.Eval(v.ctx)
-					if v.err != nil {
-						return nil
-					}
-					if !isVar(n.Left) {
-						// Continue as long as the left side of the comparison is not a
-						// variable.
-						continue
-					}
-				}
-
-			case isConst(left.Left):
-				//       cmp              cmp
-				//      /   \            /   \
-				//    [+-]   2  ->     [+-]   a
-				//   /    \           /    \
-				//  1      a         1      2
-
-				switch left.Operator {
-				case Plus, Minus:
-					left.Right, n.Right = n.Right, left.Right
-					if left.Operator == Plus {
-						left.Operator = Minus
-						left.Left, left.Right = left.Right, left.Left
-					} else {
-						n.Operator = invertComparisonOp(n.Operator)
-					}
-					n.Left, v.err = left.Eval(v.ctx)
-					if v.err != nil {
-						return nil
-					}
-					n.Left, n.Right = n.Right, n.Left
-					if !isVar(n.Left) {
-						// Continue as long as the left side of the comparison is not a
-						// variable.
-						continue
-					}
-				}
-			}
-
-			// We've run out of work to do.
-			break
-		}
-	case In, NotIn:
-		// If the right tuple in an In or NotIn comparison expression is constant, it can
-		// be normalized.
-		tuple, ok := n.Right.(DTuple)
-		if ok {
-			tuple.Normalize()
-			n.Right = tuple
-		}
-	}
-
-	return n
 }
 
 func invertComparisonOp(op ComparisonOp) ComparisonOp {
