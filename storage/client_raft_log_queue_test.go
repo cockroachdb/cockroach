ea2e2d9710e97dd1f639ea06c2bffd1c10a1b6a1
Bram Gruneir <bram@cockroachlabs.com>
2015-11-02 18:05:06 -0500

    Add raft log truncation queue.

---
+++ storage/client_raft_log_queue_test.go
@@ -0,0 +1,109 @@
+// Copyright 2015 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License. See the AUTHORS file
+// for names of contributors.
+//
+// Author: Bram Gruneir (bram+code@cockroachlabs.com)
+
+package storage_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/client"
+	"github.com/cockroachdb/cockroach/storage"
+	"github.com/cockroachdb/cockroach/util"
+	"github.com/cockroachdb/cockroach/util/leaktest"
+)
+
+// TestRaftLogQueue verifies that the raft log queue correctly truncates the
+// raft log.
+// TODO(bram): Add a new test case in which the raft leader differs from the
+// lease holder.
+func TestRaftLogQueue(t *testing.T) {
+	defer leaktest.AfterTest(t)
+
+	mtc := startMultiTestContext(t, 3)
+	defer mtc.Stop()
+
+	// Write a single value to ensure we have a leader.
+	pArgs := putArgs([]byte("key"), []byte("value"))
+	if _, err := client.SendWrapped(rg1(mtc.stores[0]), nil, &pArgs); err != nil {
+		t.Fatal(err)
+	}
+
+	rep := mtc.stores[0].LookupReplica([]byte("a"), nil)
+	originalIndex, err := rep.FirstIndex()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Write a collection of values to increase the raft log.
+	for i := 0; i < storage.RaftLogQueueStaleThreshold+1; i++ {
+		pArgs = putArgs([]byte(fmt.Sprintf("key-%d", i)), []byte("value"))
+		if _, err := client.SendWrapped(rg1(mtc.stores[0]), nil, &pArgs); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// Check the raft log and make sure it has advanced.
+	desc := rep.Desc()
+	raftStatus := mtc.stores[0].RaftStatus(desc.RangeID)
+	if raftStatus == nil {
+		t.Fatalf("the raft group doesn't exist for range %d", desc.RangeID)
+	}
+
+	for _, progress := range raftStatus.Progress {
+		if progress.Match <= originalIndex {
+			t.Fatalf("raft log has not advanced, match:%d originalIndex:%d", progress.Match, originalIndex)
+		}
+	}
+
+	// Force a truncation check.
+	for _, store := range mtc.stores {
+		store.ForceRaftLogScan(t)
+	}
+
+	// Wait until the firstIndex has increased indicating that the log
+	// truncation has occurred.
+	var currentIndex uint64
+	util.SucceedsWithin(t, time.Second, func() error {
+		var err error
+		currentIndex, err = rep.FirstIndex()
+		if err != nil {
+			return err
+		}
+		if currentIndex <= originalIndex {
+			return util.Errorf("raft log has not been truncated yet, currentIndex:%d originalIndex:%d",
+				currentIndex, originalIndex)
+		}
+		return nil
+	})
+
+	// Force a truncation check again to ensure that attempting to truncate an
+	// already truncated log has no effect.
+	for _, store := range mtc.stores {
+		store.ForceRaftLogScan(t)
+	}
+
+	finalIndex, err := rep.FirstIndex()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if currentIndex != finalIndex {
+		t.Errorf("raft log was truncated again and it shouldn't have been, post 1st truncation index:%d post 2nd truncation index:%d",
+			currentIndex, finalIndex)
+	}
+}
---
+++ storage/raft_log_queue.go
@@ -0,0 +1,143 @@
+// Copyright 2015 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License. See the AUTHORS file
+// for names of contributors.
+//
+// Author: Bram Gruneir (bram+code@cockroachlabs.com)
+
+package storage
+
+import (
+	"time"
+
+	"github.com/cockroachdb/cockroach/client"
+	"github.com/cockroachdb/cockroach/config"
+	"github.com/cockroachdb/cockroach/gossip"
+	"github.com/cockroachdb/cockroach/roachpb"
+	"github.com/cockroachdb/cockroach/util"
+	"github.com/cockroachdb/cockroach/util/log"
+	"github.com/coreos/etcd/raft"
+)
+
+const (
+	// raftLogQueueMaxSize is the max size of the queue.
+	raftLogQueueMaxSize = 100
+	// RaftLogQueueTimerDuration is the duration between checking the
+	// raft logs.
+	RaftLogQueueTimerDuration = time.Second
+	// RaftLogQueueStaleThreshold is the minimum threshold for stale raft log
+	// entries. A stale entry is one which all replicas of the range have
+	// progressed past and thus is no longer needed and can be pruned.
+	RaftLogQueueStaleThreshold = 1
+)
+
+// raftLogQueue manages a queue of replicas slated to have their raft logs
+// truncated by removing unneeded entries.
+type raftLogQueue struct {
+	baseQueue
+	db *client.DB
+}
+
+// newRaftLogQueue returns a new instance of raftLogQueue.
+func newRaftLogQueue(db *client.DB, gossip *gossip.Gossip) *raftLogQueue {
+	rlq := &raftLogQueue{
+		db: db,
+	}
+	rlq.baseQueue = makeBaseQueue("raftlog", rlq, gossip, raftLogQueueMaxSize)
+	return rlq
+}
+
+func (*raftLogQueue) needsLeaderLease() bool {
+	return false
+}
+
+func (*raftLogQueue) acceptsUnsplitRanges() bool {
+	return true
+}
+
+// getTruncatableIndexes returns the total number of stale raft log entries that
+// can be truncated and the oldest index that cannot be pruned.
+func getTruncatableIndexes(r *Replica) (uint64, uint64, error) {
+	rangeID := r.Desc().RangeID
+	raftStatus := r.store.RaftStatus(rangeID)
+	if raftStatus == nil {
+		return 0, 0, util.Errorf("the raft group doesn't exist for range %d", rangeID)
+	}
+
+	// Is this the raft leader?
+	if raftStatus.RaftState != raft.StateLeader {
+		return 0, 0, nil
+	}
+
+	// Find the oldest index still in use by the range.
+	oldestIndex := raftStatus.Applied
+	for _, progress := range raftStatus.Progress {
+		if progress.Match < oldestIndex {
+			oldestIndex = progress.Match
+		}
+	}
+
+	firstIndex, err := r.FirstIndex()
+	if err != nil {
+		return 0, 0, util.Errorf("error retrieving first index for range %d: %s", rangeID, err)
+	}
+
+	if oldestIndex < firstIndex {
+		return 0, 0, util.Errorf("raft log's oldest index is less than the first index for range %d", rangeID)
+	}
+
+	// Return the number of truncatable indexes.
+	return oldestIndex - firstIndex, oldestIndex, nil
+}
+
+// shouldQueue determines whether a range should be queued for truncating. This
+// is true only if the replica is the raft leader and if the total number of
+// the range's raft log's stale entries exceeds RaftLogQueueStaleThreshold.
+func (*raftLogQueue) shouldQueue(now roachpb.Timestamp, r *Replica, _ *config.SystemConfig) (shouldQ bool,
+	priority float64) {
+
+	truncatableIndexes, _, err := getTruncatableIndexes(r)
+	if err != nil {
+		log.Warning(err)
+		return false, 0
+	}
+
+	return truncatableIndexes > RaftLogQueueStaleThreshold, float64(truncatableIndexes)
+}
+
+// process truncates the raft log of the range if the replica is the raft
+// leader and if the total number of the range's raft log's stale entries
+// exceeds RaftLogQueueStaleThreshold.
+func (rlq *raftLogQueue) process(now roachpb.Timestamp, r *Replica, _ *config.SystemConfig) error {
+
+	truncatableIndexes, oldestIndex, err := getTruncatableIndexes(r)
+	if err != nil {
+		return err
+	}
+
+	// Can and should the raft logs be truncated?
+	if truncatableIndexes > RaftLogQueueStaleThreshold {
+		if log.V(1) {
+			log.Infof("truncating the raft log of range %d to %d", r.Desc().RangeID, oldestIndex)
+		}
+		b := &client.Batch{}
+		b.InternalAddRequest(&roachpb.TruncateLogRequest{Index: oldestIndex})
+		return rlq.db.Run(b)
+	}
+	return nil
+}
+
+// timer returns interval between processing successive queued truncations.
+func (*raftLogQueue) timer() time.Duration {
+	return RaftLogQueueTimerDuration
+}
