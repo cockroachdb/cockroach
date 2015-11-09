3e7b3666ca0df22ddb68d1d18e3184df95b0edcb
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2015-11-09 15:55:30 -0500

    use less trivial CmdIDs in SendWrappedWith

--- client/sender.go
+++ client/sender.go
@@ -96,7 +96,7 @@ func SendWrappedWith(sender Sender, ctx context.Context, h roachpb.Header, args 
 	}
 	ba := roachpb.BatchRequest{}
 	ba.Header = h
-	ba.CmdID = ba.GetOrCreateCmdID(0)
+	ba.CmdID = ba.GetOrCreateCmdID(time.Now().UnixNano())
 	ba.Add(args)
 
 	br, pErr := sender.Send(ctx, ba)
