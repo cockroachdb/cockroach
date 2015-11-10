64ef4af0b0ebd08e4302b2d93faa05ac15020cb6
marc <marc@cockroachlabs.com>
2015-11-10 09:11:55 -0500

    Keep track of latest file pushed and only push a new one once a day.
    I'll probably change this to use timestamps for greater flexibility, but
    I don't think we need to be that precise. And I like having a readable
    date in the filename.

--- build/push_aws.sh
+++ build/push_aws.sh
@@ -10,8 +10,36 @@
 set -eux
 
 BUCKET_PATH="cockroachdb/bin"
-BINARY_NAME="cockroach.$(date +"%Y%m%d-%H:%M:%S").${CIRCLE_SHA1}"
+LATEST="LATEST"
 
+now=$(date +"%Y%m%d-%H%M%S")
+today=$(echo ${now} | cut -d '-' -f1)
+binary_name="cockroach.${now}.${CIRCLE_SHA1}"
+
+# Fetch name of most recent binary.
+# TODO(marc): some versions of `aws s3 cp` allow - for stdin/stdout, but not all.
+tmpfile=$(mktemp /tmp/cockroach-push.XXXXXX)
+# Don't exit on errors, clearing the latest file is an easy way to force multiple uploads.
+set +e
+time aws s3 cp s3://${BUCKET_PATH}/${LATEST} ${tmpfile}
+set -e
+contents=$(cat ${tmpfile})
+rm -f ${tmpfile}
+latest_date=$(echo ${contents} | sed 's/cockroach.\([0-9]\+\)-.*/\1/')
+
+if [ "${latest_date}" == "${today}" ]; then
+  echo "Latest binary is from today, skipping: ${contents}"
+  exit 0
+fi
+
+# Latest file did not exist, was empty, or pointed to an old binary.
+# Upload binary.
 cd $(dirname $0)/..
 strip -S cockroach
-time aws s3 cp cockroach s3://${BUCKET_PATH}/${BINARY_NAME}
+time aws s3 cp cockroach s3://${BUCKET_PATH}/${binary_name}
+
+# Upload LATEST file.
+tmpfile=$(mktemp /tmp/cockroach-push.XXXXXX)
+echo ${binary_name} > ${tmpfile}
+time aws s3 cp ${tmpfile} s3://${BUCKET_PATH}/${LATEST}
+rm -r ${tmpfile}
