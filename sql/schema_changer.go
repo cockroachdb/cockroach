ca3f5a8c1a9108022f07c1a665549a30aca07b57
Vivek Menezes <vivek.menezes@gmail.com>
2015-11-14 13:41:23 -0500

    Process schema changes through mutations
    All the schema change commands:
    1. CREATE INDEX
    2. DROP INDEX
    3. ALTER TABLE
    
    are modified to create table descriptor mutations, and
    add the mutations to the mutation queue on the table descriptor.
    They then proceed to call applyMutations() on the table descriptor,
    to apply the mutations.
    
    This change is necessary for a future where applyMutations
    is called asynchronously on the roachNode that is the table
    leader for the table.
    
    Additionally, the schema change commands are also modified to
    deal with mutations that are queued by other commands. This is
    also needed in a future where applyMutations is called
    asynchronously, and for now is only exercised in tests.
    For now, applyMutations is called synchronously
    in the same transaction as the command, resulting in commands
    never confronting queued mutations.

---
+++ sql/schema_changer.go
@@ -0,0 +1,59 @@
+// Copyright 2015 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License. See the AUTHORS file
+// for names of contributors.
+//
+// Author: Vivek Menezes (vivek@cockroachlabs.com)
+
+package sql
+
+import (
+	"github.com/cockroachdb/cockroach/client"
+	"github.com/cockroachdb/cockroach/sql/parser"
+	"github.com/gogo/protobuf/proto"
+)
+
+// Future home of the aysynchronous schema changer that picks up
+// queued schema changes and processes them.
+//
+// applyMutations applies the queued mutations for a table.
+// TODO(vivek): Eliminate the need to pass in tableName.
+func (p *planner) applyMutations(tableDesc *TableDescriptor, tableName *parser.QualifiedName) error {
+	if len(tableDesc.Mutations) == 0 {
+		return nil
+	}
+	newTableDesc := proto.Clone(tableDesc).(*TableDescriptor)
+	// Make all mutations active.
+	for _, mutation := range newTableDesc.Mutations {
+		newTableDesc.makeMutationComplete(mutation)
+	}
+	newTableDesc.Mutations = nil
+	if err := newTableDesc.Validate(); err != nil {
+		return err
+	}
+
+	b := client.Batch{}
+	if err := p.backfillBatch(&b, tableName, tableDesc, newTableDesc); err != nil {
+		return err
+	}
+	// TODO(pmattis): This is a hack. Remove when schema change operations work
+	// properly.
+	p.hackNoteSchemaChange(newTableDesc)
+
+	b.Put(MakeDescMetadataKey(newTableDesc.GetID()), wrapDescriptor(newTableDesc))
+
+	if err := p.txn.Run(&b); err != nil {
+		return convertBatchError(newTableDesc, b, err)
+	}
+	return nil
+}
