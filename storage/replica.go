34b203726e95f1166b9101972dd30a07f6f019b2
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2015-11-19 13:28:34 -0500

    fix lost tsCache updates
    previously, concurrent reads during the execution of the
    splitTrigger could add updates to the timestamp cache
    on the old range after it had already been copied to
    the new range. With this change, reads happen atomically
    with respect to commit triggers.
    
    fixes #3148.

--- storage/replica.go
+++ storage/replica.go
@@ -163,6 +163,11 @@ type Replica struct {
 	// proposeRaftCommandFn can be set to mock out the propose operation.
 	proposeRaftCommandFn func(cmdIDKey, roachpb.RaftCommand) <-chan error
 
+	// Held in read mode during read-only commands. Held in exclusive mode to
+	// prevent read-only commands from executing. Acquired before the embedded
+	// RWMutex
+	readOnlyCmdMu sync.RWMutex
+
 	sync.RWMutex                 // Protects the following fields:
 	cmdQ         *CommandQueue   // Enforce at most one command is running per key(s)
 	tsCache      *TimestampCache // Most recent timestamps for keys / key ranges
@@ -762,7 +767,10 @@ func (r *Replica) addReadOnlyCmd(ctx context.Context, ba roachpb.BatchRequest) (
 		}
 	}
 
-	// Execute read-only batch command.
+	r.readOnlyCmdMu.RLock()
+	// Execute read-only batch command. It checks for matching key range; note
+	// that holding readMu throughout is important to avoid reads from the
+	// "wrong" key range being served after the range has been split.
 	br, intents, err := r.executeBatch(r.store.Engine(), nil, ba)
 
 	if err == nil && ba.Txn != nil {
@@ -775,6 +783,8 @@ func (r *Replica) addReadOnlyCmd(ctx context.Context, ba roachpb.BatchRequest) (
 
 	// Remove keys from command queue.
 	r.endCmds(cmdKeys, ba, err)
+	// Important to unlock only here to capture the timestamp cache update.
+	r.readOnlyCmdMu.RUnlock()
 
 	if err != nil {
 		return nil, err
