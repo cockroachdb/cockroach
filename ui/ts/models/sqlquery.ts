b6a655ef35349ecf92e3c83f77348dd8286f073c
Max Lang <max@cockroachlabs.com>
2015-12-01 10:41:18 -0500

    bring changes and new type defs into sqlquery

--- ui/ts/models/sqlquery.ts
+++ ui/ts/models/sqlquery.ts
@@ -1,22 +1,18 @@
 /// <reference path="../../bower_components/mithriljs/mithril.d.ts" />
 /// <reference path="../../typings/tsd.d.ts" />
-/// <reference path="../generated/protos/wireproto.ts" />
-///<reference path="../../typings/protobufjs/protobufjs.d.ts"/>
+/// <reference path="../generated/protos/wireproto.d.ts" />
+/// <reference path="../../typings/protobufjs/protobufjs.d.ts"/>
 
 module Models {
   "use strict";
   export module SQLQuery {
+    import RequestJSON = Proto2TypeScript.cockroach.sql.driver.RequestJSON;
 
-    // TODO: eliminate 'any's
     export function runQuery(q: string): _mithril.MithrilPromise<any> {
-      let root: any = Protos._root;
 
-      let SQL: any = root.cockroach.sql.driver;
+      let data: RequestJSON = {user: "root", sql: q};
 
-      let req: any = new SQL.Request({user: "root", sql: q});
-      let buf: any = req.encode().toBuffer();
-
-      let xhrConfig: any = function(xhr: any): any {
+      let xhrConfig = function(xhr: XMLHttpRequest): XMLHttpRequest {
         xhr.setRequestHeader("Content-Type", "application/x-protobuf");
         xhr.setRequestHeader("Accept", "application/json");
 
@@ -30,18 +26,7 @@ module Models {
         url: "/sql/Execute",
         config: xhrConfig,
         method: "POST",
-        data: buf,
-        serialize: function(data: any): any {
-          // TODO: mithril PR to accept ArrayBuffer/Blob data
-          data.constructor = FormData; // HACK: circumvent mithril's request datatype sanity check
-          return data;
-        },
-        deserialize: function(data: any): any {
-          // TODO: receive data as protobuf instead of JSON
-          // let resp = new SQL.Response.decode(data, "binary");
-          // return resp;
-          return JSON.parse(data);
-        },
+        data: data,
       });
     }
 
