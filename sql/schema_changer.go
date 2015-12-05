c88cc238a0b67e4e21526ea9acbe036bd84eb673
Vivek Menezes <vivek.menezes@gmail.com>
2015-12-05 13:12:07 -0500

    table leader lease library
    Once schema changes are scheduled to be executed through mutations,
    a goroutine on the cluster acquires the table leader lease and executes
    schema changes on the table. Any goroutine can acquire the lease.
    In the future, the goroutine that executes the schema change command
    (RENAME, ALTER, etc) will try to acquire the lease immediately
    after marking the up_version bit on the table descriptor. Additionally
    goroutines on every node will wake up on gossip notifications and will
    attempt to acquire the lease if they see outstanding up_version bit
    or mutations on a table descriptor.

--- sql/schema_changer.go
+++ sql/schema_changer.go
@@ -18,7 +18,12 @@
 package sql
 
 import (
+	"errors"
+	"time"
+
 	"github.com/cockroachdb/cockroach/client"
+	"github.com/cockroachdb/cockroach/sql/parser"
+	"github.com/cockroachdb/cockroach/util"
 	"github.com/gogo/protobuf/proto"
 )
 
@@ -53,4 +58,77 @@ func (p *planner) applyMutations(tableDesc *TableDescriptor) error {
 	}
 	p.notifyCompletedSchemaChange(newTableDesc.ID)
 	return nil
+}
+
+type TableLeaderLeaser struct {
+	p *planner
+}
+
+// NewTableLeaderLeaserForTesting.
+func NewTableLeaderLeaserForTesting(txn *client.Txn) TableLeaderLeaser {
+	return TableLeaderLeaser{p: &planner{txn: txn}}
+}
+
+var errExistingTableLeader = errors.New("an outstanding lease exists")
+
+// Acquire acquires a leader lease on the table if
+// an unexpired lease doesn't exist. It returns the lease.
+func (t TableLeaderLeaser) Acquire(tableID ID) (TableDescriptor_LeaderLease, error) {
+	t.p.txn.SetSystemDBTrigger()
+	tableDesc, err := t.p.getTableDescFromID(tableID)
+	if err != nil {
+		return TableDescriptor_LeaderLease{}, err
+	}
+
+	now := time.Now()
+	// Add 5 seconds to the lease expiration to deal with time uncertainty across nodes.
+	if tableDesc.Lease != nil && time.Unix(tableDesc.Lease.ExpirationTime+5, 0).After(now) {
+		// Return the existing lease with an error.
+		return *tableDesc.Lease, errExistingTableLeader
+	}
+
+	leader := parser.GenerateUniqueBytes(t.p.evalCtx.NodeID)
+	lease := TableDescriptor_LeaderLease{Leader: string(leader), ExpirationTime: now.Add(jitteredLeaseDuration()).Unix()}
+	tableDesc.Lease = &lease
+	err = t.p.txn.Put(MakeDescMetadataKey(tableDesc.ID), wrapDescriptor(tableDesc))
+	return lease, err
+}
+
+func (p *planner) findTableWithLease(tableID ID, lease TableDescriptor_LeaderLease) (*TableDescriptor, error) {
+	tableDesc, err := p.getTableDescFromID(tableID)
+	if err != nil {
+		return nil, err
+	}
+	if tableDesc.Lease == nil {
+		return nil, util.Errorf("no lease present for tableID: %d", tableID)
+	}
+	if tableDesc.Lease.Leader != lease.Leader {
+		return nil, util.Errorf("lease for table ID: %d, owned by different leader: %q, expected: %q", tableID, tableDesc.Lease.Leader, lease.Leader)
+	}
+	return tableDesc, nil
+}
+
+// Release the table lease if leader is the lease leader.
+func (t TableLeaderLeaser) Release(tableID ID, lease TableDescriptor_LeaderLease) error {
+	tableDesc, err := t.p.findTableWithLease(tableID, lease)
+	if err != nil {
+		return err
+	}
+	tableDesc.Lease = nil
+	t.p.txn.SetSystemDBTrigger()
+	return t.p.txn.Put(MakeDescMetadataKey(tableDesc.ID), wrapDescriptor(tableDesc))
+}
+
+// Extend the lease for the current leader.
+func (t TableLeaderLeaser) Extend(tableID ID, lease TableDescriptor_LeaderLease) (TableDescriptor_LeaderLease, error) {
+	tableDesc, err := t.p.findTableWithLease(tableID, lease)
+	if err != nil {
+		return TableDescriptor_LeaderLease{}, err
+	}
+	leader := parser.GenerateUniqueBytes(t.p.evalCtx.NodeID)
+	lease = TableDescriptor_LeaderLease{Leader: string(leader), ExpirationTime: time.Now().Add(jitteredLeaseDuration()).Unix()}
+	tableDesc.Lease = &lease
+	t.p.txn.SetSystemDBTrigger()
+	err = t.p.txn.Put(MakeDescMetadataKey(tableDesc.ID), wrapDescriptor(tableDesc))
+	return lease, err
 }
