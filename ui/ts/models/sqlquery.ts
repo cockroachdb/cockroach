015b8cb330d47e5fcd823d2e67a834d8b0874cae
Max Lang <max@cockroachlabs.com>
2015-12-07 20:36:04 -0500

    simpler sqlquery comment

--- ui/ts/models/sqlquery.ts
+++ ui/ts/models/sqlquery.ts
@@ -19,9 +19,7 @@ module Models {
         /**
          * TODO: receive data as protobuf instead of JSON
          *
-         * Currently there are two separate protobuf issues:
-         *
-         * 1) Mithril doesn't support sending/receiving binary data, but xhr
+         * Mithril doesn't support sending/receiving binary data, but xhr
          * does. We just need the following lines to send/receive binary
          * data at the xhr level, but mithril would need to be modified to
          * support it, or we'd need a different ajax library.
@@ -29,40 +27,9 @@ module Models {
          *   xhr.setRequestHeader("Accept", "application/x-protobuf");
          *   xhr.responseType = "arraybuffer";
          *
-         *  2) proto2typescript (https://github.com/SINTEF-9012/Proto2TypeScript
-         *  or the fork github.com/aliok/Proto2TypeScript) doesn't handle the
-         *  proto oneof fields the way Go serializes them by default.
-         *  The former ellides the oneof field and just includes the subfield
-         *  direclty, whereas the latter creates an extra key and nests the
-         *  subfield under that key. For example:
-         *
-         *  From wire.proto:
-         *
-         *   message Datum {
-         *    oneof payload {
-         *      bool bool_val = 1;
-         *      int64 int_val = 2;
-         *      ...
-         *    }
-         *  }
-         *
-         * Typescript definition from proto2typescript:
-         *
-         * interface Datum {
-         *   bool_val?: boolean;
-         *   int_val?: number;
-         *   ...
-         * }
-         *
-         * GO serialized JSON:
-         *
-         * {
-         *    Datum: {
-         *      Payload: {
-         *        BoolVal: ...
-         *      }
-         *    }
-         *  }
+         * Once this was enabled, we could use proto2typescript to generate
+         * typescript definitions of protobufs.
+         * 
          */
         return xhr;
       };
