626d5bf221530a192b74914fbc138fd5b3a8dc44
Tamir Duberstein <tamird@gmail.com>
2015-12-09 13:18:28 -0500

    kv: don't call `t.Fatal` outside the main thread
    This removes a data race from #3361, though it does not fix the issue.

--- kv/dist_sender_server_test.go
+++ kv/dist_sender_server_test.go
@@ -68,16 +68,17 @@ func TestRangeLookupWithOpenTransaction(t *testing.T) {
 	// intent error. If it did, it would go into a deadloop attempting
 	// to push the transaction, which in turn requires another range
 	// lookup, etc, ad nauseam.
-	success := make(chan struct{})
+	errors := make(chan error)
 	go func() {
-		if _, err := db.Get("a"); err != nil {
-			t.Fatal(err)
-		}
-		close(success)
+		_, err := db.Get("a")
+		errors <- err
 	}()
 
 	select {
-	case <-success:
+	case err := <-errors:
+		if err != nil {
+			t.Fatal(err)
+		}
 		// Hurrah!
 	case <-time.After(5 * time.Second):
 		t.Errorf("get request did not succeed in face of range metadata intent")
