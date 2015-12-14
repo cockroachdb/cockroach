905d6d4ea2abc4fd0e2e95f06a8020ae2473ef6f
Tamir Duberstein <tamird@gmail.com>
2015-12-14 10:48:19 -0500

    protobuf: consult git when constructing proto source list
    Fixes `go generate` in the presence of ignored protos (e.g. ui deps).
    
    cc @maxlang

--- build/protobuf.mk
+++ build/protobuf.mk
@@ -39,8 +39,8 @@ GOGOPROTO_PATH  := $(GOGOPROTO_ROOT):$(GOGOPROTO_ROOT)/protobuf
 
 COREOS_PATH := $(GITHUB_ROOT)/coreos
 
-GO_PROTOS  := $(sort $(shell find $(REPO_ROOT) -not -path '*/.*' -name *.proto -type f))
-GO_SOURCES  := $(GO_PROTOS:%.proto=%.pb.go)
+GO_PROTOS := $(addprefix $(REPO_ROOT)/, $(sort $(shell cd $(REPO_ROOT) && git ls-files --exclude-standard --cached --others -- '*.proto')))
+GO_SOURCES := $(GO_PROTOS:%.proto=%.pb.go)
 
 CPP_PROTOS := $(filter %/api.proto %/metadata.proto %/data.proto %/errors.proto %/internal.proto %/mvcc.proto %/unresolved_addr.proto,$(GO_PROTOS))
 CPP_HEADERS := $(subst ./,$(NATIVE_ROOT)/,$(CPP_PROTOS:%.proto=%.pb.h)) $(subst $(GOGOPROTO_ROOT),$(NATIVE_ROOT),$(GOGOPROTO_PROTO:%.proto=%.pb.h))
@@ -57,7 +57,7 @@ REPO_NAME := cockroachdb
 IMPORT_PREFIX := github.com/$(REPO_NAME)/
 
 $(GO_SOURCES): $(PROTOC) $(GO_PROTOS) $(GOGOPROTO_PROTO)
-	find $(REPO_ROOT) -not -path '*/.*' -name *.pb.go | xargs rm
+	(cd $(REPO_ROOT) && git ls-files --exclude-standard --cached --others -- '*.pb.go' | xargs rm -f)
 	for dir in $(sort $(dir $(GO_PROTOS))); do \
 	  $(PROTOC) -I.:$(GOGOPROTO_PATH):$(COREOS_PATH) --plugin=$(PROTOC_PLUGIN) --$(PLUGIN_SUFFIX)_out=import_prefix=$(IMPORT_PREFIX):$(ORG_ROOT) $$dir/*.proto; \
 	  sed -i.bak -E 's!import (fmt|math) "$(IMPORT_PREFIX)(fmt|math)"! !g' $$dir/*.pb.go; \
@@ -68,13 +68,16 @@ $(GO_SOURCES): $(PROTOC) $(GO_PROTOS) $(GOGOPROTO_PROTO)
 	done
 
 $(CPP_HEADERS) $(CPP_SOURCES): $(PROTOC) $(CPP_PROTOS) $(GOGOPROTO_PROTO)
-	find $(REPO_ROOT) -not -path '*/.*' -name *.pb.h -o -name *.pb.cc | xargs rm
+	(cd $(REPO_ROOT) && git ls-files --exclude-standard --cached --others -- '*.pb.h' '*.pb.cc' | xargs rm -f)
 	$(PROTOC) -I.:$(GOGOPROTO_PATH) --cpp_out=$(NATIVE_ROOT) $(CPP_PROTOS)
 	$(PROTOC) -I.:$(GOGOPROTO_PATH) --cpp_out=$(NATIVE_ROOT) $(GOGOPROTO_PROTO)
 	@# For c++, protoc generates a directory structure mirroring the package
 	@# structure (and these directories must be in the include path), but cgo can
 	@# only compile a single directory so we symlink the generated pb.cc files
 	@# into the storage/engine directory.
+	@# We use `find` and not `git ls-files` here because `git ls-files` will
+	@# include deleted files (i.e. these very symlinks) in its output, resulting
+	@# in recursive symlinks, which is Bad™.
 	(cd $(NATIVE_ROOT) && find . -name *.pb.cc | xargs -I % ln -sf % .)
 
 $(ENGINE_CPP_HEADERS) $(ENGINE_CPP_SOURCES): $(PROTOC) $(ENGINE_CPP_PROTOS)
