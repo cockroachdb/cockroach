8e6faaa63b6b0c8f14e510bb59cf5e7eb30e3444
marc <marc@cockroachlabs.com>
2015-12-15 14:02:08 -0500

    Build and push all tests to AWS.
    * add DIR and STRIPPED args to testbuild rule
    * build all tests and tar them up
    
    We still build sql.test by itself to make the nightly logictest easier
    (it can grab that single binary as opposed to grabbing the tarball and
    extracting the needed one).
    
    Gzipping the tarball to reduce size. This is particularly critical when
    running from the office.

--- build/build-static-binaries.sh
+++ build/build-static-binaries.sh
@@ -12,17 +12,26 @@ source $(dirname $0)/build-common.sh
 # commands in the if-branch to be executed within the docker
 # container.
 if [ "${1-}" = "docker" ]; then
+    test_build_dir=$(mktemp -d test-binaries.XXXX)
     time make STATIC=1 build
-    time make STATIC=1 testbuild PKG=./sql
-    time make STATIC=1 testbuild PKG=./acceptance TAGS=acceptance
+    # sql/sql.test is built standalone as well to simplify the nightly logictest runs.
+    time make STATIC=1 testbuild STRIPPED=1 PKG=./sql
+    time make STATIC=1 testbuild STRIPPED=1 PKG=./acceptance TAGS=acceptance
+    time make STATIC=1 testbuild STRIPPED=1 DIR=${test_build_dir}
 
+    # We don't check all test binaries, but one from each invocation.
     check_static cockroach
     check_static sql/sql.test
     check_static acceptance/acceptance.test
+    check_static ${test_build_dir}/github.com/cockroachdb/cockroach/sql/sql.test
 
     strip -S cockroach
-    strip -S sql/sql.test
-    strip -S acceptance/acceptance.test
+
+    rm -f static-tests.tar.gz
+    # Skip the project/repo part of the path inside the tarball.
+    # Even for stripped binaries, gzip results in 167MB (21s spent) vs 512MB (3s spent).
+    # It makes a big difference when fetching from outside AWS.
+    time tar cfz static-tests.tar.gz -C ${test_build_dir}/github.com/cockroachdb/ cockroach/
     exit 0
 fi
 
