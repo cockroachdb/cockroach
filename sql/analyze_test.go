6ff9b933b1ae99923874177ae7163198aa62fecb
Andrei Matei <andrei@cockroachlabs.com>
2015-12-16 11:51:59 -0500

    sql: fix test comment

--- sql/analyze_test.go
+++ sql/analyze_test.go
@@ -209,7 +209,7 @@ func TestSimplifyExpr(t *testing.T) {
 		{`(a, b) = (1, 2)`, `(a, b) IN ((1, 2))`, true},
 		{`(a, b) = (1, 2) OR (a, b) = (3, 4)`, `(a, b) IN ((1, 2), (3, 4))`, true},
 		{`(a, b) IN ((2, 1), (1, 2), (1, 2), (2, 1))`, `(a, b) IN ((1, 2), (2, 1))`, true},
-		// Expressions that don't simplify properly:
+		// Expressions that don't simplify as of Dec 2015, although they could:
 		// {`a <= 5 AND (a, b) IN ((1, 2))`, `(a, b) IN ((1, 2))`, true},
 		// {`a <= 5 AND b >= 6 AND (a, b) IN ((1, 2))`, `false`, true},
 
