72d7879d297049983740a1bcca88cc60d5423b89
Peter Mattis <petermattis@gmail.com>
2015-12-18 14:34:47 -0500

    Remove usage of homebrew-go-resources.
    Fix nits revealed by `brew audit --strict build/cockroach.rb`.

--- build/cockroach.rb
+++ build/cockroach.rb
@@ -11,9 +11,8 @@ class Cockroach < Formula
       :tag => "v0.1-alpha",
       :revision => "26088f81e5ecfb2fd63f8f15f524102c9a0c1c05"
   head "https://github.com/cockroachdb/cockroach.git"
-  
-  depends_on "go" => :build
 
+  depends_on "go" => :build
 
   go_resource "github.com/biogo/store" do
     url "https://github.com/biogo/store.git",
@@ -126,17 +125,17 @@ class Cockroach < Formula
   end
 
   go_resource "golang.org/x/crypto" do
-    url "https://go.googlesource.com/crypto.git",
+    url "https://golang.org/x/crypto.git",
       :revision => "7b85b097bf7527677d54d3220065e966a0e3b613"
   end
 
   go_resource "golang.org/x/net" do
-    url "https://go.googlesource.com/net.git",
+    url "https://golang.org/x/net.git",
       :revision => "21af302bb52737bcf1e6ce0128ec5932bf0d8ae9"
   end
 
   go_resource "golang.org/x/text" do
-    url "https://go.googlesource.com/text.git",
+    url "https://golang.org/x/text.git",
       :revision => "00e205363f74f74c3c03b7a12427ec0747a23089"
   end
 
@@ -150,17 +149,28 @@ class Cockroach < Formula
     ENV["GOPATH"] = buildpath
     ENV["GOHOME"] = buildpath
 
-    files = Dir.glob('*')
+    files = Dir.glob("*")
     mkdir_p buildpath/"src/github.com/cockroachdb/cockroach"
     mv files, buildpath/"src/github.com/cockroachdb/cockroach"
     Language::Go.stage_deps resources, buildpath/"src"
-    
+
     # TODO(peter): We should be using the cockroach makefile so that
     # we can get the build information baked into the
     # binary. Unfortunately, this is dying with some weird dwarf
     # linker error:
     #
     #   make -C src/github.com/cockroachdb/cockroach build SKIP_BOOTSTRAP=1
+    #
+    # Something truly bizarre is going on. The error is:
+    #
+    # # github.com/cockroachdb/c-snappy
+    # cannot load DWARF output from $WORK/github.com/cockroachdb/c-snappy/_obj//_cgo_.o: decoding dwarf section info at offset 0x0: too short
+    #
+    # But that error only happens when building with make. Using go
+    # directly and everything works fine. Additionally,
+    # build/depvers.sh returns nothing when run in the homebrew build
+    # environment. But if I execute the "go list" directives inside
+    # the debugging shell it works. Wtf?
     system "go", "build", "-v", "-o", bin/"cockroach", "github.com/cockroachdb/cockroach"
   end
 
