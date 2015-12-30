0d4ccde75a7e0a53a519f9e1314b2c919a532689
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2015-12-30 13:38:00 -0500

    make circle-trigger-nightly.sh standalone
    it now uses an ad-hoc call to our main repo to resolve the
    given ref, and triggers that:
    
    `./build/circle-trigger-nightly.sh <token> master`

--- build/circle-trigger-nightly.sh
+++ build/circle-trigger-nightly.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-set -eu
+set -euo pipefail
 
-REV=$(git describe --tags)
-
-if [ -z "${1-}" ]; then
+if [ $# -ne 2 ]; then
   cat <<EOF
-Trigger a CircleCI nightly build for the revision currently checked out (${REV}).
+Trigger a CircleCI nightly build for the given ref (for example "master").
 
-  Usage: $0 <circle_api_token>
+  Usage: $0 <circle_api_token> <ref>
 EOF
   exit 2
 fi
 
+REV=$(git ls-remote --exit-code git@github.com:cockroachdb/cockroach.git "${2}" | awk '{print $1}')
+echo "Triggering ${2} (${REV})"
 curl -X POST --header 'Content-Type: application/json' \
   -d ' { "build_parameters": { "NIGHTLY": "true" } }' \
   "https://circleci.com/api/v1/project/cockroachdb/cockroach/tree/${REV}?circle-token=${1}"
