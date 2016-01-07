4fbf018f0dd34abc31fb6ca0fa7285d4c0e06d4f
Matt Jibson <matt.jibson@gmail.com>
2016-01-07 13:27:26 -0500

    sql: support python psycopg module
    This requires three changes.
    
    1) During connection init, specify UTF8 as the client_encoding. Without
    this, psycopg refuses to proceed.
    
    2) Also during init, specify ISO as the DateStyle. Without this option,
    psycopg requests ISO as the DateStyle, which is a message we currently do
    not support.
    
    3) When a (client-side) cursor on psycopg starts, it sends a BEGIN statement
    and expects a CommandComplete message. A real Postgres sends a BEGIN tag
    in response, but we can get by with an empty one.
    
    While here, refactor out the common docker test functionality.

--- acceptance/python_test.go
+++ acceptance/python_test.go
@@ -20,15 +20,17 @@ package acceptance
 
 import "testing"
 
-// TestRuby connects to a cluster with ruby.
-func TestRuby(t *testing.T) {
-	testDocker(t, "ruby", []string{"ruby", "-e", ruby})
+// TestPython connects to a cluster with python.
+func TestPython(t *testing.T) {
+	testDocker(t, "python", []string{"-c", python})
 }
 
-const ruby = `
-require 'pg'
-
-conn = PG.connect()
-res = conn.exec_params('SELECT 1, 2 > $1, $1', [3])
-raise 'Unexpected: ' + res.values.to_s unless res.values == [["1", "f", "3"]]
+const python = `
+import psycopg2
+import os
+conn = psycopg2.connect('')
+cur = conn.cursor()
+cur.execute("SELECT 1, 2+3")
+v = cur.fetchall()
+assert v == [(1, 5)]
 `
