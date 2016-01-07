155bd1c713fadda4424c151e5b927030a6a7f461
Matt Jibson <matt.jibson@gmail.com>
2016-01-07 16:37:17 -0500

    acceptance: ignore some docker events

--- acceptance/cluster/localcluster.go
+++ acceptance/cluster/localcluster.go
@@ -413,6 +413,10 @@ func (l *LocalCluster) processEvent(e dockerclient.EventOrError, monitorStopper 
 		l.events <- Event{NodeIndex: -1, Status: eventDie}
 		return false
 	}
+	switch e.Status {
+	case "create", "start", "pull":
+		return true
+	}
 
 	for i, n := range l.Nodes {
 		if n != nil && n.ID == e.Id {
