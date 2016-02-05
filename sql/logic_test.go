07af92f6c11599dee7fe98c3001b0c07a9fc8645
Tamir Duberstein <tamird@gmail.com>
2016-02-05 13:45:08 -0500

    Revert "sql: use a single temp dir and simplify cleanup"
    This reverts commit f8423bf1e388e8b5f232ec8ddfe71b9d32348853.

--- sql/logic_test.go
+++ sql/logic_test.go
@@ -178,7 +178,9 @@ func (t *logicTest) close() {
 }
 
 // setUser sets the DB client to the specified user.
-func (t *logicTest) setUser(tempDir, user string) {
+// It returns a cleanup function to be run when the credentials
+// are no longer needed.
+func (t *logicTest) setUser(user string) func() {
 	if t.db != nil {
 		var dbName string
 
@@ -199,21 +201,22 @@ func (t *logicTest) setUser(tempDir, user string) {
 	}
 	if db, ok := t.clients[user]; ok {
 		t.db = db
-		return
+		// No cleanup necessary, but return a no-op func to avoid nil pointer dereference.
+		return func() {}
 	}
 
-	credsDir, err := ioutil.TempDir(tempDir, "user-credentials-"+user)
+	credsDir, err := ioutil.TempDir("", "user-credentials")
 	if err != nil {
 		t.Fatal(err)
 	}
-	pgUrl, _ := sqlutils.PGUrl(t.T, &t.srv.TestServer, user, credsDir, "TestLogic")
+	pgUrl, cleanupFunc := sqlutils.PGUrl(t.T, &t.srv.TestServer, user, credsDir, "TestLogic")
 	db, err := sql.Open("postgres", pgUrl.String())
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.clients[user] = db
 	t.db = db
-	return
+	return cleanupFunc
 }
 
 // TODO(tschottdorf): some logic tests currently take a long time to run.
@@ -248,7 +251,8 @@ func (t *logicTest) run(path string) {
 
 	// db may change over the lifetime of this function, with intermediate
 	// values cached in t.clients and finally closed in t.close().
-	t.setUser(tempDir, security.RootUser)
+	cleanupFunc := t.setUser(security.RootUser)
+	defer cleanupFunc()
 
 	if _, err := t.db.Exec(`
 CREATE DATABASE test;
@@ -405,7 +409,8 @@ SET DATABASE = test;
 			if len(fields[1]) == 0 {
 				t.Fatal("user command requires a non-blank argument")
 			}
-			t.setUser(tempDir, fields[1])
+			cleanupUserFunc := t.setUser(fields[1])
+			defer cleanupUserFunc()
 
 		case "skipif":
 			if len(fields) < 2 {
