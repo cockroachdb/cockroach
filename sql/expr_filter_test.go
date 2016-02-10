528a961471c76da0e7c13a66ae586ab173a00990
Radu Berinde <radu.berinde@gmail.com>
2016-02-10 17:30:48 -0500

    sql: code for splitting filter expressions
    Adding code that splits a filter expression according to a set of VariableExprs
    (qvalues). This will be used:
      - to convert qvalues to a type that is suitable to be evaluated in scanNode
        (and later on the remote side or in the KV layer),
      - in indexJoin to run whatever part of the filter we can on the keys in the
        index, and
      - (later) when we filter across multiple tables (joins).

---
+++ sql/expr_filter_test.go
@@ -0,0 +1,112 @@
+// Copyright 2016 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// Author: Radu Berinde (radu@cockroachlabs.com)
+
+package sql
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/sql/parser"
+	"github.com/cockroachdb/cockroach/util/leaktest"
+)
+
+func TestSplitFilter(t *testing.T) {
+	defer leaktest.AfterTest(t)
+
+	testData := []struct {
+		expr        string
+		columns     []string
+		expectedRes string
+		expectedRem string
+	}{
+		{`a = 1`, []string{""}, `<nil>`, `a = 1`},
+		{`a = 1`, []string{"a"}, `a = 1`, `<nil>`},
+
+		{`NOT a = 1`, []string{""}, `<nil>`, `NOT a = 1`},
+		{`NOT a = 1`, []string{"a"}, `NOT a = 1`, `<nil>`},
+
+		{`a = 1 AND b = 5`, []string{""}, `<nil>`, `a = 1 AND b = 5`},
+		{`a = 1 AND b = 5`, []string{"a"}, `a = 1`, `b = 5`},
+		{`a = 1 AND b = 5`, []string{"a", "b"}, `a = 1 AND b = 5`, `<nil>`},
+
+		{`NOT (a = 1 AND b = 5)`, []string{""}, `<nil>`, `NOT a = 1 AND b = 5`},
+		{`NOT (a = 1 AND b = 5)`, []string{"a"}, `<nil>`, `NOT a = 1 AND b = 5`},
+		{`NOT (a = 1 AND b = 5)`, []string{"a", "b"}, `NOT a = 1 AND b = 5`, `<nil>`},
+
+		{`a = 1 OR b = 5`, []string{""}, `<nil>`, `a = 1 OR b = 5`},
+		{`a = 1 OR b = 5`, []string{"a"}, `<nil>`, `a = 1 OR b = 5`},
+		{`a = 1 OR b = 5`, []string{"a", "b"}, `a = 1 OR b = 5`, `<nil>`},
+
+		{`NOT (a = 1 OR b = 5)`, []string{""}, `<nil>`, `NOT a = 1 OR b = 5`},
+		{`NOT (a = 1 OR b = 5)`, []string{"a"}, `NOT a = 1`, `NOT b = 5`},
+		{`NOT (a = 1 OR b = 5)`, []string{"a", "b"}, `NOT a = 1 OR b = 5`, `<nil>`},
+
+		{`(a = 1 AND b = 5) OR (a = 2 AND b = 6)`,
+			[]string{"a"},
+			`a = 1 OR a = 2`,
+			`a = 1 AND b = 5 OR a = 2 AND b = 6`,
+		},
+		{`(NOT (a = 1 AND b = 5)) OR (a = 2 AND b = 6)`,
+			[]string{"a"},
+			`<nil>`,
+			`NOT a = 1 AND b = 5 OR a = 2 AND b = 6`,
+		},
+		{`NOT ((a = 1 AND b = 5) OR (a = 2 AND b = 6))`,
+			[]string{"a", "b"},
+			`NOT a = 1 AND b = 5 OR a = 2 AND b = 6`,
+			`<nil>`,
+		},
+
+		{`a < b`, []string{"a"}, `<nil>`, `a < b`},
+		{`a < b`, []string{"a", "a", "b"}, `a < b`, `<nil>`},
+
+		{`a < b AND a < j`, []string{"a", "b"}, `a < b`, `a < j`},
+		{`a + b = 1 AND a + j = 2`, []string{"a", "b"}, `a + b = 1`, `a + j = 2`},
+
+		{`a IN (1, b) AND c`, []string{"a"}, `<nil>`, `a IN (1, b) AND c`},
+		{`a IN (1, b) AND c`, []string{"a", "b"}, `a IN (1, b)`, `c`},
+		{`a IN (1, b) AND c`, []string{"c"}, `c`, `a IN (1, b)`},
+	}
+
+	for _, d := range testData {
+		// A function that "converts" only columns in the list.
+		conv := func(expr parser.VariableExpr) parser.VariableExpr {
+			q := expr.(*qvalue)
+			colName := q.colRef.get().Name
+			for _, col := range d.columns {
+				if colName == col {
+					return q
+				}
+			}
+			return nil
+		}
+		expr, _ := parseAndNormalizeExpr(t, d.expr)
+		res, rem := splitFilter(expr, conv)
+		// We use Sprint to handle the 'nil' case correctly.
+		resStr := fmt.Sprint(res)
+		remStr := fmt.Sprint(rem)
+		if testing.Verbose() {
+			fmt.Printf("Expr `%s` split along (%s): `%s`,`%s`\n",
+				expr, strings.Join(d.columns, ","), resStr, remStr)
+		}
+		if resStr != d.expectedRes || remStr != d.expectedRem {
+			t.Errorf("`%s` split along (%s): expected:\n   `%s`,`%s`\ngot:\n   `%s`,`%s`",
+				d.expr, strings.Join(d.columns, ","), d.expectedRes, d.expectedRem, resStr, remStr)
+		}
+	}
+}
