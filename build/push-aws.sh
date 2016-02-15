f6e47864339bd2b4f017b1b1536ea4bae2249e7e
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2016-02-14 23:07:51 -0500

    Allow nightly builds of OSX binary
    Triggered via a master build with the `NIGHTLY` environment variable. The full
    build should run in 10-15min, so no caching should be necessary.

--- build/push-aws.sh
+++ build/push-aws.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env sh
 # Push binaries to AWS.
 # This is run by circle-ci after successful docker push.
 #
-# Requisites:
+# Prerequisites:
 # - binaries must be statically linked by running build/build-static-binaries.sh
 # - circleci must have AWS credentials configured
 # - AWS credentials must have S3 write permissions on the bucket
@@ -14,30 +14,12 @@
 
 set -eux
 
-BUCKET_NAME="cockroach"
-LATEST_SUFFIX=".LATEST"
-REPO_NAME="cockroach"
+OSARCH="" # TODO(mberhault): should be "-linux-amd64"
 SHA="${CIRCLE_SHA1-$(git rev-parse HEAD)}"
 
-# push_one_binary takes the path to the binary inside the repo.
-# eg: push_one_binary sql/sql.test
-# The file will be pushed to: s3://BUCKET_NAME/REPO_NAME/sql.test.SHA
-# The S3 basename will be stored in s3://BUCKET_NAME/REPO_NAME/sql.test.LATEST
-function push_one_binary {
-  rel_path=$1
-  binary_name=$(basename $1)
+cd "$(dirname "${0}")"
 
-  cd $(dirname $0)/..
-  time aws s3 cp ${rel_path} s3://${BUCKET_NAME}/${REPO_NAME}/${binary_name}.${SHA}
-
-  # Upload LATEST file.
-  tmpfile=$(mktemp /tmp/cockroach-push.XXXXXX)
-  echo ${SHA} > ${tmpfile}
-  time aws s3 cp ${tmpfile} s3://${BUCKET_NAME}/${REPO_NAME}/${binary_name}${LATEST_SUFFIX}
-  rm -f ${tmpfile}
-}
-
-push_one_binary cockroach
-push_one_binary sql/sql.test
-push_one_binary acceptance/acceptance.test
-push_one_binary static-tests.tar.gz
+./push-one-binary.sh ${SHA} cockroach cockroach${OSARCH}
+./push-one-binary.sh ${SHA} sql/sql${OSARCH}.test
+./push-one-binary.sh ${SHA} acceptance/acceptance${OSARCH}.test
+./push-one-binary.sh ${SHA} static-tests${OSARCH}.tar.gz
