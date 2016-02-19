455e13d1dd239f646af97eb2ac9185485a72f56c
David Taylor <tinystatemachine@gmail.com>
2016-02-19 22:41:35 +0000

    sql: Skip scan of unfiltered spans during delete if possible.
    ```
    name                   old time/op    new time/op    delta
    Delete1_Cockroach-8       830µs ±10%     572µs ± 2%  -31.14%  (p=0.000 n=10+10)
    Delete10_Cockroach-8     2.50ms ± 3%    1.83ms ±13%  -26.69%  (p=0.000 n=10+10)
    Delete100_Cockroach-8    17.2ms ± 4%    13.7ms ± 7%  -20.40%   (p=0.001 n=5+10)
    
    name                   old alloc/op   new alloc/op   delta
    Delete1_Cockroach-8      34.6kB ± 0%    28.4kB ± 0%  -17.97%  (p=0.000 n=10+10)
    Delete10_Cockroach-8      135kB ± 0%      85kB ± 0%  -36.61%  (p=0.000 n=10+10)
    Delete100_Cockroach-8    1.09MB ± 0%    0.65MB ± 0%  -39.95%    (p=0.003 n=4+9)
    
    name                   old allocs/op  new allocs/op  delta
    Delete1_Cockroach-8         572 ± 0%       448 ± 0%  -21.64%  (p=0.000 n=10+10)
    Delete10_Cockroach-8      1.75k ± 0%     1.22k ± 0%  -30.31%   (p=0.000 n=10+6)
    Delete100_Cockroach-8     12.9k ± 0%      8.6k ± 0%  -33.62%   (p=0.000 n=5+10)
    ```

--- sql/delete.go
+++ sql/delete.go
@@ -17,6 +17,8 @@
 package sql
 
 import (
+	"bytes"
+
 	"github.com/cockroachdb/cockroach/roachpb"
 	"github.com/cockroachdb/cockroach/sql/parser"
 	"github.com/cockroachdb/cockroach/sql/privilege"
@@ -76,7 +78,19 @@ func (p *planner) Delete(n *parser.Delete, autoCommit bool) (planNode, *roachpb.
 		}
 	}
 
+	if isSystemConfigID(tableDesc.GetID()) {
+		// Mark transaction as operating on the system DB.
+		p.txn.SetSystemConfigTrigger()
+	}
+
+	// Check if we can avoid doing a round-trip to read the values and just
+	// "fast-path" skip to deleting the key ranges without reading them first.
+	if canDeleteWithoutScan(n, rows, len(indexes)) {
+		return p.fastDelete(rows, result, autoCommit)
+	}
+
 	b := p.txn.NewBatch()
+
 	for rows.Next() {
 		rowVals := rows.Values()
 		result.rowCount++
@@ -117,11 +131,6 @@ func (p *planner) Delete(n *parser.Delete, autoCommit bool) (planNode, *roachpb.
 		return nil, pErr
 	}
 
-	if isSystemConfigID(tableDesc.GetID()) {
-		// Mark transaction as operating on the system DB.
-		p.txn.SetSystemConfigTrigger()
-	}
-
 	if autoCommit {
 		// An auto-txn can commit the transaction with the batch. This is an
 		// optimization to avoid an extra round-trip to the transaction
@@ -134,5 +143,87 @@ func (p *planner) Delete(n *parser.Delete, autoCommit bool) (planNode, *roachpb.
 		return nil, pErr
 	}
 
+	return result, nil
+}
+
+// Determine if the deletion of `rows` can be done without actually scanning them,
+// i.e. if we do not need to know their values for filtering expressions or a
+// RETURNING clause or for updating secondary indexes.
+func canDeleteWithoutScan(n *parser.Delete, rows planNode, indexCount int) bool {
+	if sel, ok := rows.(*selectNode); ok {
+		if indexCount != 0 {
+			if log.V(2) {
+				log.Infof("delete forced to scan: values required to update %d secondary indexes", indexCount)
+			}
+			return false
+		}
+		if n.Returning != nil {
+			if log.V(2) {
+				log.Infof("delete forced to scan: values required for RETURNING")
+			}
+			return false
+		}
+		if sel.filter != nil {
+			if log.V(2) {
+				log.Infof("delete forced to scan: values required for filter (%s)", sel.filter)
+			}
+			return false
+		}
+		return true
+	}
+	return false
+}
+
+// `fastDelete` skips the scan of rows and just deletes the ranges that
+// `rows` would scan. Should only be used if `canDeleteWithoutScan` indicates
+// that it is safe to do so.
+func (p *planner) fastDelete(rows planNode, result *returningNode, autoCommit bool) (planNode, *roachpb.Error) {
+	b := p.txn.NewBatch()
+	sel := rows.(*selectNode)
+	scan := sel.table.node.(*scanNode)
+
+	for _, span := range scan.spans {
+		if log.V(2) {
+			log.Infof("Skipping scan and just deleting %s - %s", span.start, span.end)
+		}
+		b.DelRange(span.start, span.end, true)
+	}
+
+	if autoCommit {
+		// An auto-txn can commit the transaction with the batch. This is an
+		// optimization to avoid an extra round-trip to the transaction
+		// coordinator.
+		if pErr := p.txn.CommitInBatch(b); pErr != nil {
+			return nil, pErr
+		}
+	} else {
+		if pErr := p.txn.Run(b); pErr != nil {
+			return nil, pErr
+		}
+	}
+
+	if !scan.initScan() {
+		return nil, scan.pErr
+	}
+
+	for _, r := range b.Results {
+		var prev []byte
+		for _, i := range r.Keys {
+			// If prefix is same, don't bother decoding key.
+			if len(prev) > 0 && bytes.HasPrefix(i, prev) {
+				continue
+			}
+
+			after, err := scan.readIndexKey(i)
+			if err != nil {
+				return nil, roachpb.NewError(err)
+			}
+			k := i[:len(i)-len(after)]
+			if !bytes.Equal(k, prev) {
+				prev = k
+				result.rowCount++
+			}
+		}
+	}
 	return result, nil
 }
