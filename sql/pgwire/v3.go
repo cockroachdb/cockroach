ff39418bbaaa3efbd0f870e7f2e667fed1110909
Kenji Kaneda <kenji.kaneda@gmail.com>
2016-02-18 20:46:39 -0800

    Revert "sql: Return error from Executor.Prepare"
    This reverts commit 571270f7f85a2f2bc2daf79365b20c8f1501d66b.
    
    Discussed in #4480. We might want to pass roachpb.Error for #2625.

--- sql/pgwire/v3.go
+++ sql/pgwire/v3.go
@@ -324,9 +324,9 @@ func (c *v3Conn) handleParse(buf *readBuffer) error {
 		}
 		args[fmt.Sprint(i+1)] = v
 	}
-	cols, err := c.executor.Prepare(c.opts.user, query, c.session, args)
-	if err != nil {
-		return c.sendError(err.Error())
+	cols, pErr := c.executor.Prepare(c.opts.user, query, c.session, args)
+	if pErr != nil {
+		return c.sendError(pErr.String())
 	}
 	pq := preparedStatement{
 		query:       query,
