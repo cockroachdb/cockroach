7fcd583b1a4ddc57ebf0af4d45fc4745f0e04604
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2016-02-25 19:21:19 -0500

    Skip TestAuthentication

--- kv/db_test.go
+++ kv/db_test.go
@@ -239,6 +239,7 @@ func TestKVDBTransaction(t *testing.T) {
 // TestAuthentication tests authentication for the KV endpoint.
 func TestAuthentication(t *testing.T) {
 	defer leaktest.AfterTest(t)
+	t.Skip("#4653")
 	s := server.StartTestServer(t)
 	defer s.Stop()
 
