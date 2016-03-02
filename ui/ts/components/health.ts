669e7aaf0baca17d882f0603a280d2dd379627bc
Max Lang <max@cockroachlabs.com>
2016-03-02 13:31:14 -0500

    add health check

---
+++ ui/ts/components/health.ts
@@ -0,0 +1,73 @@
+// source: components/health.ts
+/// <reference path="../../bower_components/mithriljs/mithril.d.ts" />
+/// <reference path="../../typings/browser.d.ts" />
+/// <reference path="../util/property.ts" />
+
+// Author: Max Lang (max@cockroachlabs.com)
+
+module Components {
+  "use strict";
+
+  /**
+   * Health returns the health status as an icon
+   */
+  export module Health {
+
+    import MithrilPromise = _mithril.MithrilPromise;
+
+    class HealthController {
+      healthy: boolean = true; // Starts out OK because the page loaded
+      refreshing: boolean = true;
+      interval: number;
+      getHealth: () => MithrilPromise<void> = (): MithrilPromise<void> => {
+        this.refreshing = true;
+        m.redraw();
+        return m.request({
+          url: "/_admin/v1/health",
+          deserialize: (d: any): any => { return d; },
+          config: function(xhr: XMLHttpRequest): void { xhr.timeout = 2000; },
+        })
+        .then((r: any): void => {
+          this.healthy = _.startsWith(r.toString(), "ok");
+          this.refreshing = false;
+        })
+        .catch((r: any): void => {
+          this.healthy = _.startsWith(r.toString(), "ok");
+          this.refreshing = false;
+        });
+      };
+
+      startRefresh(): void {
+        this.interval = setInterval(this.getHealth, 2000);
+      }
+
+      onunload: () => void = (): void => {
+        if (this.interval) {
+          clearInterval(this.interval);
+        }
+      };
+    }
+
+    export function controller(): HealthController {
+      let hc: HealthController = new HealthController();
+      hc.startRefresh();
+      // TODO: create global refresher
+      return hc;
+    }
+
+    export function view(ctrl: HealthController): _mithril.MithrilVirtualElement {
+      if (ctrl.healthy) {
+        return m("div", [
+          m("span.health-icon.icon-check-circle" + (ctrl.refreshing ? ".refreshing" : "")),
+          m("span.refreshing-text", ctrl.refreshing ? " Refreshing..." : ""),
+        ]);
+      } else {
+        return m("div", [
+          m("span.health-icon.icon-x" + (ctrl.refreshing ? ".refreshing" : "")),
+          m("span.unreachable-text", " Can't reach node. "),
+          m("span.refreshing-text", ctrl.refreshing ? " Refreshing..." : ""),
+        ]);
+      }
+    }
+  }
+}
