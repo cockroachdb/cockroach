d11669b68a8fb355e24ff74e0f0fbd3db82b9c2d
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2016-03-03 18:02:30 -0500

    Introduce partition nemesis in acceptance tests
    This nemesis allows node partitions to be set up during tests. Partitions can
    be bidirectional, but also one-way (i.e. one node can initiate connections to
    another, but not the other way around). The latter likely isn't too
    interesting with the change to streaming RPCs, but it should be possible to
    introduce interesting phenomena nevertheless.
    
    Wrote a bank test with the nemesis. I found the existing bank test too
    complicated for its own sake, so I re-wrote it.

--- acceptance/chaos_test.go
+++ acceptance/chaos_test.go
@@ -298,13 +298,11 @@ func waitClientsStop(num int, state *testState, stallDuration time.Duration) {
 // performance, but cluster recovery.
 func TestClusterRecovery(t *testing.T) {
 	t.Skipf("TODO(pmattis): #4517")
+	runTestOnConfigs(t, testClusterRecoveryInner)
 }
 
 func testClusterRecoveryInner(t *testing.T, c cluster.Cluster, cfg cluster.TestConfig) {
 	num := c.NumNodes()
-	if num <= 0 {
-		t.Fatalf("%d nodes in cluster", num)
-	}
 
 	// One client for each node.
 	initBank(t, c.PGUrl(0))
@@ -363,8 +361,8 @@ func TestNodeRestart(t *testing.T) {
 
 func testNodeRestartInner(t *testing.T, c cluster.Cluster, cfg cluster.TestConfig) {
 	num := c.NumNodes()
-	if num <= 0 {
-		t.Fatalf("%d nodes in cluster", num)
+	if num <= 3 {
+		t.Skip("not enough nodes for this test")
 	}
 
 	// One client for each node.
