2629ff75b8edf2389181f2c270d000c3c6cc88ad
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2016-03-02 23:09:47 -0500

    util/cache: Fix baseCahce.Clear by walking linked list and dumping store
    Before this change, there was a subtle bug where `IntervalCache.clear`
    would fail to clear all entries from the cache. This issue came about
    when interval keys had been modified in the cache in certain ways, such
    that when deleting entries from the underlying red-black tree (in an
    in-order depth-first ordering provided by `tree.Do`), rotations occurred.
    If a rotation occurred, the traversal could be messed up and nodes could
    be missed.
    
    This change fixes this issue by simply replacing the entire tree on
    clear, which should also be more efficient. It also includes a repro test
    case for the issue it fixes.
    
    It follows this pattern with the `OrderedCache` and `UnorderedCache` by
    adding a new `init` method to the `cacheStore` interface and removing
    `del` from the interface. A mildly noteworthy part of this change is
    that it requires us to now use the `baseCache`'s linked list even if
    the eviction policy is set to `CacheNone`.

--- util/cache/cache.go
+++ util/cache/cache.go
@@ -100,14 +100,14 @@ func (e *Entry) Range() interval.Range {
 
 // cacheStore is an interface for the backing store used for the cache.
 type cacheStore interface {
+	// init initializes or clears all entries.
+	init()
 	// get returns the entry by key.
 	get(key interface{}) *Entry
 	// add stores an entry.
 	add(e *Entry)
 	// del removes an entry.
 	del(key interface{})
-	// clear clears all entries.
-	clear()
 	// len is number of items in store.
 	length() int
 }
@@ -131,6 +131,7 @@ func newBaseCache(config Config) baseCache {
 func (bc *baseCache) init(store cacheStore) {
 	bc.ll.Init()
 	bc.store = store
+	bc.store.init()
 }
 
 // Add adds a value to the cache.
@@ -168,12 +169,10 @@ func (bc *baseCache) add(key, value interface{}, entry, after *Entry) {
 	if e == nil {
 		e = &Entry{Key: key, Value: value}
 	}
-	if bc.Policy != CacheNone {
-		if after != nil {
-			e.le = bc.ll.InsertBefore(e, after.le)
-		} else {
-			e.le = bc.ll.PushFront(e)
-		}
+	if after != nil {
+		e.le = bc.ll.InsertBefore(e, after.le)
+	} else {
+		e.le = bc.ll.PushFront(e)
 	}
 	bc.store.add(e)
 	// Evict as many elements as we can.
@@ -205,7 +204,14 @@ func (bc *baseCache) DelEntry(entry *Entry) {
 
 // Clear clears all entries from the cache.
 func (bc *baseCache) Clear() {
-	bc.store.clear()
+	if bc.OnEvicted != nil {
+		for e := bc.ll.Back(); e != nil; e = e.Prev() {
+			entry := e.Value.(*Entry)
+			bc.OnEvicted(entry.Key, entry.Value)
+		}
+	}
+	bc.ll.Init()
+	bc.store.init()
 }
 
 // Len returns the number of items in the cache.
@@ -220,9 +226,7 @@ func (bc *baseCache) access(e *Entry) {
 }
 
 func (bc *baseCache) removeElement(e *Entry) {
-	if bc.Policy != CacheNone {
-		bc.ll.Remove(e.le)
-	}
+	bc.ll.Remove(e.le)
 	bc.store.del(e.Key)
 	if bc.OnEvicted != nil {
 		bc.OnEvicted(e.Key, e.Value)
@@ -266,13 +270,15 @@ type UnorderedCache struct {
 func NewUnorderedCache(config Config) *UnorderedCache {
 	mc := &UnorderedCache{
 		baseCache: newBaseCache(config),
-		hmap:      make(map[interface{}]interface{}),
 	}
 	mc.baseCache.init(mc)
 	return mc
 }
 
 // Implementation of cacheStore interface.
+func (mc *UnorderedCache) init() {
+	mc.hmap = make(map[interface{}]interface{})
+}
 func (mc *UnorderedCache) get(key interface{}) *Entry {
 	if e, ok := mc.hmap[key].(*Entry); ok {
 		return e
@@ -284,11 +290,6 @@ func (mc *UnorderedCache) add(e *Entry) {
 }
 func (mc *UnorderedCache) del(key interface{}) {
 	delete(mc.hmap, key)
-}
-func (mc *UnorderedCache) clear() {
-	for key := range mc.hmap {
-		mc.Del(key)
-	}
 }
 func (mc *UnorderedCache) length() int {
 	return len(mc.hmap)
@@ -318,6 +319,9 @@ func NewOrderedCache(config Config) *OrderedCache {
 }
 
 // Implementation of cacheStore interface.
+func (oc *OrderedCache) init() {
+	oc.llrb = llrb.Tree{}
+}
 func (oc *OrderedCache) get(key interface{}) *Entry {
 	if e, ok := oc.llrb.Get(&Entry{Key: key}).(*Entry); ok {
 		return e
@@ -329,12 +333,6 @@ func (oc *OrderedCache) add(e *Entry) {
 }
 func (oc *OrderedCache) del(key interface{}) {
 	oc.llrb.Delete(&Entry{Key: key})
-}
-func (oc *OrderedCache) clear() {
-	oc.llrb.Do(func(e llrb.Comparable) (done bool) {
-		oc.Del(e.(*Entry).Key)
-		return
-	})
 }
 func (oc *OrderedCache) length() int {
 	return oc.llrb.Len()
@@ -419,7 +417,6 @@ func (ik IntervalKey) Contains(lk IntervalKey) bool {
 func NewIntervalCache(config Config) *IntervalCache {
 	ic := &IntervalCache{
 		baseCache: newBaseCache(config),
-		tree:      interval.Tree{Overlapper: interval.Range.OverlapExclusive},
 	}
 	ic.baseCache.init(ic)
 	return ic
@@ -446,6 +443,10 @@ func (ic *IntervalCache) MakeKey(start, end []byte) IntervalKey {
 }
 
 // Implementation of cacheStore interface.
+func (ic *IntervalCache) init() {
+	ic.tree = interval.Tree{Overlapper: interval.Range.OverlapExclusive}
+}
+
 func (ic *IntervalCache) get(key interface{}) *Entry {
 	ik := key.(*IntervalKey)
 	ic.getID = ik.id
@@ -475,13 +476,6 @@ func (ic *IntervalCache) del(key interface{}) {
 	if err := ic.tree.Delete(&ic.tmpEntry, false); err != nil {
 		log.Error(err)
 	}
-}
-
-func (ic *IntervalCache) clear() {
-	ic.tree.Do(func(e interval.Interface) (done bool) {
-		ic.Del(e.(*Entry).Key.(*IntervalKey))
-		return
-	})
 }
 
 func (ic *IntervalCache) length() int {
