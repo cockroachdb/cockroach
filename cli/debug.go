909aef04058ea1fb754b498db84ac0a61151c934
Ben Darnell <ben@cockroachlabs.com>
2016-03-07 23:44:39 -0500

    cli: add debug raft-log command

--- cli/debug.go
+++ cli/debug.go
@@ -21,11 +21,13 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/cockroachdb/cockroach/keys"
 	"github.com/cockroachdb/cockroach/roachpb"
 	"github.com/cockroachdb/cockroach/storage/engine"
 	"github.com/cockroachdb/cockroach/util/stop"
+	"github.com/coreos/etcd/raft/raftpb"
 	"github.com/spf13/cobra"
 )
 
@@ -38,12 +40,7 @@ var debugKeysCmd = &cobra.Command{
 	RunE: runDebugKeys,
 }
 
-func openStore(cmd *cobra.Command, args []string, stopper *stop.Stopper) (engine.Engine, error) {
-	if len(args) != 1 {
-		return nil, errors.New("one argument is required")
-	}
-	dir := args[0]
-
+func openStore(cmd *cobra.Command, dir string, stopper *stop.Stopper) (engine.Engine, error) {
 	db := engine.NewRocksDB(roachpb.Attributes{}, dir, 0, 0, 0, stopper)
 	if err := db.Open(); err != nil {
 		return nil, err
@@ -61,7 +58,11 @@ func runDebugKeys(cmd *cobra.Command, args []string) error {
 	stopper := stop.NewStopper()
 	defer stopper.Stop()
 
-	db, err := openStore(cmd, args, stopper)
+	if len(args) != 1 {
+		return errors.New("one argument is required")
+	}
+
+	db, err := openStore(cmd, args[0], stopper)
 	if err != nil {
 		return err
 	}
@@ -105,7 +106,11 @@ func runDebugRangeDescriptors(cmd *cobra.Command, args []string) error {
 	stopper := stop.NewStopper()
 	defer stopper.Stop()
 
-	db, err := openStore(cmd, args, stopper)
+	if len(args) != 1 {
+		return errors.New("one argument is required")
+	}
+
+	db, err := openStore(cmd, args[0], stopper)
 	if err != nil {
 		return err
 	}
@@ -119,9 +124,73 @@ func runDebugRangeDescriptors(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
+var debugRaftLogCmd = &cobra.Command{
+	Use:   "raft-log [directory] [range id]",
+	Short: "print the raft log for a range",
+	Long: `
+  Prints all log entries in a store for the given range.
+`,
+	RunE: runDebugRaftLog,
+}
+
+func printRaftLogEntry(kv engine.MVCCKeyValue) (bool, error) {
+	var meta engine.MVCCMetadata
+	if err := meta.Unmarshal(kv.Value); err != nil {
+		return false, err
+	}
+	value := roachpb.Value{
+		RawBytes: meta.RawBytes,
+	}
+	var ent raftpb.Entry
+	if err := value.GetProto(&ent); err != nil {
+		return false, err
+	}
+	if len(ent.Data) > 0 {
+		var cmd roachpb.RaftCommand
+		if err := cmd.Unmarshal(ent.Data[9:]); err != nil {
+			return false, err
+		}
+		ent.Data = nil
+		fmt.Printf("%s\n", &ent)
+		fmt.Printf("%s\n", &cmd)
+	} else {
+		fmt.Printf("%s: EMPTY\n", &ent)
+	}
+	return false, nil
+}
+
+func runDebugRaftLog(cmd *cobra.Command, args []string) error {
+	stopper := stop.NewStopper()
+	defer stopper.Stop()
+
+	if len(args) != 2 {
+		return errors.New("required arguments: dir range_id")
+	}
+
+	db, err := openStore(cmd, args[0], stopper)
+	if err != nil {
+		return err
+	}
+
+	rangeIDInt, err := strconv.ParseInt(args[1], 10, 64)
+	if err != nil {
+		return err
+	}
+	rangeID := roachpb.RangeID(rangeIDInt)
+
+	start := engine.MakeMVCCMetadataKey(keys.RaftLogPrefix(rangeID))
+	end := engine.MakeMVCCMetadataKey(keys.RaftLogPrefix(rangeID).PrefixEnd())
+
+	if err := db.Iterate(start, end, printRaftLogEntry); err != nil {
+		return err
+	}
+	return nil
+}
+
 var debugCmds = []*cobra.Command{
 	debugKeysCmd,
 	debugRangeDescriptorsCmd,
+	debugRaftLogCmd,
 	kvCmd,
 	rangeCmd,
 }
