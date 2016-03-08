d99d00db7fa9db43a9120946a4f378d000337de2
Max Lang <max@cockroachlabs.com>
2016-03-08 02:22:25 -0500

    Fix updated time in top bar when update time isn't present
    fixes #4954

--- ui/ts/components/topbar.ts
+++ ui/ts/components/topbar.ts
@@ -1,6 +1,7 @@
 // source: components/topbar.ts
 
 /// <reference path="./health.ts" />
+/// <reference path="../../typings/browser.d.ts" />
 
 // Author: Max Lang (max@cockroachlabs.com)
 
@@ -10,18 +11,21 @@ module Components {
   import MithrilVirtualElement = _mithril.MithrilVirtualElement;
 
   export module Topbar {
-    export function controller(): void {}
+    export function controller(): any {
+      return {
+        updatedStr: "-",
+      };
+    }
     export function view(ctrl: any, args: {title?: string|MithrilVirtualElement; updated: number; titleImage?: MithrilVirtualElement; }): MithrilVirtualElement {
-      let updatedStr: string = "-";
-      if (args.updated !== 0 && args.updated !== -Infinity) {
-        updatedStr = Utils.Format.Date(new Date(Utils.Convert.NanoToMilli(args.updated)));
+      if (args.updated && _.isFinite(args.updated)) {
+        ctrl.updatedStr = Utils.Format.Date(new Date(Utils.Convert.NanoToMilli(args.updated)));
       }
 
       return m(".topbar", [
         (args.title && m("h2", args.title) || (args.titleImage)),
         m(".info-container", [
           m(".health", [m("strong", "health:"), m.component(Components.Health, {})]),
-          m(".last-updated", [ m("strong", "Updated: "), updatedStr ]),
+          m(".last-updated", [ m("strong", "Updated: "), ctrl.updatedStr ]),
         ]),
       ]);
     }
