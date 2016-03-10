b8f3cd55b3a99edcc90d562b113f32c97f24973a
Tamir Duberstein <tamird@gmail.com>
2016-03-10 13:17:46 -0500

    rpc: return an error instead of calling log.Fatal and add a test

--- rpc/context_test.go
+++ rpc/context_test.go
@@ -24,6 +24,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 
+	"github.com/cockroachdb/cockroach/testutils"
 	"github.com/cockroachdb/cockroach/util"
 	"github.com/cockroachdb/cockroach/util/hlc"
 	"github.com/cockroachdb/cockroach/util/leaktest"
@@ -190,4 +191,124 @@ func (ac *AdvancingClock) UnixNano() int64 {
 	time := ac.time
 	ac.time = time + ac.advancementInterval
 	return time
+}
+
+func TestRemoteOffsetUnhealthy(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	stopper := stop.NewStopper()
+	defer stopper.Stop()
+
+	const maxOffset = 100 * time.Millisecond
+
+	type nodeContext struct {
+		offset  time.Duration
+		ctx     *Context
+		errChan chan error
+	}
+
+	start := time.Date(2012, 12, 07, 0, 0, 0, 0, time.UTC)
+	offsetClock := func(offset time.Duration) *hlc.Clock {
+		return hlc.NewClock(func() int64 {
+			return start.Add(offset).UnixNano()
+		})
+	}
+
+	nodeCtxs := []nodeContext{
+		{offset: 0},
+		{offset: 0},
+		{offset: 0},
+		// Due to measurement uncertainty being at least equal to maxOffset (see
+		// comments in clock_offset.go), this is the minimum offset that actually
+		// triggers node death.
+		{offset: maxOffset*2 + 1},
+	}
+
+	for i := range nodeCtxs {
+		clock := offsetClock(nodeCtxs[i].offset)
+		nodeCtxs[i].errChan = make(chan error, 1)
+
+		clock.SetMaxOffset(maxOffset)
+		nodeCtxs[i].ctx = newNodeTestContext(clock, stopper)
+		nodeCtxs[i].ctx.RemoteClocks.monitorInterval = maxOffset / 2
+		// Apparently heartbeats must happen more frequently than monitor events.
+		// Good thing this is documented.
+		nodeCtxs[i].ctx.HeartbeatInterval = nodeCtxs[i].ctx.RemoteClocks.monitorInterval / 2
+
+		s, ln := newTestServer(t, nodeCtxs[i].ctx, true)
+		RegisterHeartbeatServer(s, &HeartbeatService{
+			clock:              clock,
+			remoteClockMonitor: nodeCtxs[i].ctx.RemoteClocks,
+		})
+		nodeCtxs[i].ctx.localAddr = ln.Addr().String()
+	}
+
+	// Fully connect the nodes.
+	for i, clientNodeContext := range nodeCtxs {
+		for j, serverNodeContext := range nodeCtxs {
+			if i == j {
+				continue
+			}
+			if _, err := clientNodeContext.ctx.GRPCDial(serverNodeContext.ctx.localAddr); err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
+
+	// Wait until all nodes are connected to all other nodes. We do this before
+	// starting the clock monitors to prevent the case where a node is connected
+	// e.g. only to the outlier node at the time of the first clock monitor
+	// event. This would cause that node to incorrectly deduce that it is offset
+	// from the cluster and commit suicide.
+	//
+	// TODO(tamird): The code responsible for this should be made more resilient (e.g.
+	// don't commit suicide if there are only two known nodes). This is likely
+	// not a problem in practice, since the clock monitor interval is quite
+	// large.
+	for _, nodeCtx := range nodeCtxs {
+		util.SucceedsSoon(t, func() error {
+			nodeCtx.ctx.RemoteClocks.mu.Lock()
+			defer nodeCtx.ctx.RemoteClocks.mu.Unlock()
+
+			if a, e := len(nodeCtx.ctx.RemoteClocks.mu.offsets), len(nodeCtxs)-1; a != e {
+				return util.Errorf("not yet fully connected: have %d of %d connections: %v", a, e, nodeCtx.ctx.RemoteClocks.mu.offsets)
+			}
+			return nil
+		})
+	}
+
+	// Now that all the nodes are connected, start the clock monitors.
+	for _, nodeCtx := range nodeCtxs {
+		// Asynchronously closing over a range variable is unsafe.
+		ctx := nodeCtx.ctx
+		errChan := nodeCtx.errChan
+		stopper.RunWorker(func() {
+			errChan <- ctx.RemoteClocks.MonitorRemoteOffsets(stopper)
+		})
+	}
+
+	const errOffsetGreaterThanMaxOffset = "the true offset is greater than the max offset"
+	for i, nodeCtx := range nodeCtxs {
+		waitTime := nodeCtx.ctx.RemoteClocks.monitorInterval * 5
+
+		if nodeOffset := nodeCtx.offset; nodeOffset > maxOffset {
+			select {
+			case err := <-nodeCtx.errChan:
+				if testutils.IsError(err, errOffsetGreaterThanMaxOffset) {
+					t.Logf("max offset: %s - node %d with excessive clock offset of %s returned expected error: %s", maxOffset, i, nodeOffset, err)
+				} else {
+					t.Errorf("max offset: %s - node %d with excessive clock offset of %s returned unexpected error: %s", maxOffset, i, nodeOffset, err)
+				}
+			case <-time.After(waitTime):
+				t.Errorf("max offset: %s - node %d with excessive clock offset of %s should have return an error, but did not", maxOffset, i, nodeOffset)
+			}
+		} else {
+			select {
+			case err := <-nodeCtx.errChan:
+				t.Errorf("max offset: %s - node %d with acceptable clock offset of %s returned unexpected error: %s", maxOffset, i, nodeOffset, err)
+			case <-time.After(waitTime):
+				t.Logf("max offset: %s - node %d with acceptable clock offset of %s did not return an error, as expected", maxOffset, i, nodeOffset)
+			}
+		}
+	}
 }
