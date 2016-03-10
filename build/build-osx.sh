8bee71050781edc6c8e801329ef2cf3c123f6f3f
marc <marc@cockroachlabs.com>
2016-03-09 19:43:53 -0500

    Push tarball of linux/osx binaries to s3://binaries.cockroachdb.com
    These will be the links used on the download page.
    We still push linux-only binaries to the current bucket.
    binaries.cockroachdb.com is special because it is used by cloudfront
    which provides https.
    
    Download links will end up being:
    https://binaries.cockroachdb.com/cockroach.{linux,darwin-10.9}-amd64.tgz

--- build/build-osx.sh
+++ build/build-osx.sh
@@ -4,4 +4,5 @@ set -eu
 
 go get github.com/karalabe/xgo
 # OSX 10.9 is the most recent version available at time of writing.
+# If changing the OS/arch target, adjust the filename in push-aws.sh.
 xgo --targets=darwin-10.9/amd64 --go=1.6 --ldflags="$($(dirname "${0}")/ldflags.sh)" ${GOPATH%%:*}/src/github.com/cockroachdb/cockroach
---
+++ build/push-release-tarball.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# This file uses `bash` and not `sh` due to the `time` builtin (the external
+# `time` is not available on CircleCI).
+
+set -eu
+
+BUCKET_NAME="binaries.cockroachdb.com"
+
+# $0 takes the path to the binary inside the repo and the tarball base name.
+# eg: $0 cockroach-darwin-10.9-amd64 cockroach.darwin-amd64
+#   generates cockroach.darwin-amd64.tgz which expands into cockroach/cockroach.darwin-amd64
+#   copies tarball to s3://BUCKET_NAME/cockroach.darwin-amd64.tgz
+
+rel_path=$1
+tarball_base=$2
+
+cd "$(dirname "${0}")/.."
+
+tmpdir=$(mktemp -d /tmp/cockroach-push.XXXXXX)
+mkdir -p "${tmpdir}/cockroach"
+
+cp ${rel_path} "${tmpdir}/cockroach/${tarball_base}"
+time tar cz -C ${tmpdir} -f ${tarball_base}.tgz cockroach
+time aws s3 cp ${tarball_base}.tgz s3://${BUCKET_NAME}/${tarball_base}.tgz
+
+rm -rf ${tmpdir}
