79bf9aa303847a43820413f4c113278ecb41831f
Jingguo Yao <yaojingguo@gmail.com>
2016-03-13 17:02:56 +0800

    client: remove the equality checks for SetIsolation and SetUserPriority methods for client.Txn
    Fixes #4003

--- client/txn.go
+++ client/txn.go
@@ -108,6 +108,7 @@ type Txn struct {
 	// systemConfigTrigger is set to true when modifying keys from the SystemConfig
 	// span. This sets the SystemConfigTrigger on EndTransactionRequest.
 	systemConfigTrigger bool
+	retrying            bool
 }
 
 // NewTxn returns a new txn.
@@ -142,12 +143,13 @@ func (txn *Txn) DebugName() string {
 // serializable isolation. The isolation must be set before any operations are
 // performed on the transaction.
 func (txn *Txn) SetIsolation(isolation roachpb.IsolationType) error {
-	if txn.Proto.Isolation != isolation {
-		if txn.Proto.IsInitialized() {
-			return util.Errorf("cannot change the isolation level of a running transaction")
-		}
-		txn.Proto.Isolation = isolation
+	if txn.retrying {
+		return nil
 	}
+	if txn.Proto.IsInitialized() {
+		return util.Errorf("cannot change the isolation level of a running transaction")
+	}
+	txn.Proto.Isolation = isolation
 	return nil
 }
 
@@ -155,15 +157,16 @@ func (txn *Txn) SetIsolation(isolation roachpb.IsolationType) error {
 // normal user priority. The user priority must be set before any operations are
 // performed on the transaction.
 func (txn *Txn) SetUserPriority(userPriority roachpb.UserPriority) error {
-	if txn.UserPriority != userPriority {
-		if txn.Proto.IsInitialized() {
-			return util.Errorf("cannot change the user priority of a running transaction")
-		}
-		if userPriority < roachpb.MinUserPriority || userPriority > roachpb.MaxUserPriority {
-			return util.Errorf("the given user priority %f is out of the allowed range [%f, %d]", userPriority, roachpb.MinUserPriority, roachpb.MaxUserPriority)
-		}
-		txn.UserPriority = userPriority
+	if txn.retrying {
+		return nil
 	}
+	if txn.Proto.IsInitialized() {
+		return util.Errorf("cannot change the user priority of a running transaction")
+	}
+	if userPriority < roachpb.MinUserPriority || userPriority > roachpb.MaxUserPriority {
+		return util.Errorf("the given user priority %f is out of the allowed range [%f, %d]", userPriority, roachpb.MinUserPriority, roachpb.MaxUserPriority)
+	}
+	txn.UserPriority = userPriority
 	return nil
 }
 
@@ -481,6 +484,9 @@ func (txn *Txn) Exec(
 RetryLoop:
 	for r := retry.Start(retryOptions); r.Next(); {
 		pErr = fn(txn, &opt)
+		if txn != nil {
+			txn.retrying = true
+		}
 		if (pErr == nil) && opt.AutoCommit && (txn.Proto.Status == roachpb.PENDING) {
 			// fn succeeded, but didn't commit.
 			pErr = txn.CommitNoCleanup()
