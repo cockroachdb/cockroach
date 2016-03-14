f331cee98c1ae219047e09ba16f1247f738102bb
Max Lang <max@cockroachlabs.com>
2016-03-14 14:12:15 -0400

    Add logs page to individual node page tab

--- ui/ts/app.ts
+++ ui/ts/app.ts
@@ -14,15 +14,12 @@ m.mount(document.getElementById("header"), AdminViews.SubModules.TitleBar);
 
 m.route.mode = "hash";
 m.route(document.getElementById("root"), "/cluster", {
-  "/logs": AdminViews.Log.Page,
-  "/logs/:node_id": AdminViews.Log.Page,
-  "/node": AdminViews.Nodes.NodesPage,
   "/nodes": AdminViews.Nodes.NodeOverviewReroute,
   "/nodes/overview": AdminViews.Nodes.NodesPage,
+  "/nodes/events": AdminViews.Events.Page,
   "/nodes/graphs": AdminViews.Nodes.NodesPage,
-  "/node/:node_id": AdminViews.Nodes.NodePage,
   "/nodes/:node_id": AdminViews.Nodes.NodePage,
-  "/node/:node_id/:detail": AdminViews.Nodes.NodePage,
+  "/nodes/:node_id/logs": AdminViews.Log.Page,
   "/nodes/:node_id/:detail": AdminViews.Nodes.NodePage,
   "/databases": AdminViews.Databases.DatabaseListPage,
   "/databases/:database": AdminViews.Databases.DatabasePage,
