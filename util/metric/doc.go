0c7a3a453f117a022e28dbc19021f13bff110078
Cuong Do <cdo@cockroachlabs.com>
2016-03-14 11:52:06 -0400

    Write node clock offset interval to time series
    Also reformatted util/metric/doc.go to follow 80-column guideline for
    comments and lightly updated the content.
    
    Resolves #859

--- util/metric/doc.go
+++ util/metric/doc.go
@@ -15,14 +15,17 @@
 // Author: Cuong Do (cdo@cockroachlabs.com)
 
 /*
-Package metric provides server metrics (a.k.a. transient stats) for a CockroachDB server. These
-metrics are persisted to the time-series database and are viewable through the web interface and the
-/_status/metrics/<NODEID> HTTP endpoint.
+Package metric provides server metrics (a.k.a. transient stats) for a
+CockroachDB server. These metrics are persisted to the time-series database and
+are viewable through the web interface and the /_status/metrics/<NODEID> HTTP
+endpoint.
 
 Adding a new metric
 
 First, add the metric to a Registry.
-Next, call methods such as Counter() and Rate() on the Registry to register the metric. For example:
+
+Next, call methods such as Counter() and Rate() on the Registry to register the
+metric. For example:
 
 	exec := &Executor{
 		...
@@ -30,33 +33,35 @@ Next, call methods such as Counter() and Rate() on the Registry to register the 
 		...
 	}
 
-This code block registers the metric "sql.select.count" in sql.Registry. The metric can then be
-accessed through the "selectCount" variable, which can be updated as follows:
+This code block registers the metric "sql.select.count" in sql.Registry. The
+metric can then be accessed through the "selectCount" variable, which can be
+updated as follows:
 
 	func (e *Executor) doSelect() {
 		// do the SELECT
 		e.selectCount.Inc(1)
 	}
 
-To add the metric to the web UI, modify the appropriate file in "ui/ts/pages/*.ts". Someone more
-qualified than me can elaborate, like @maxlang.
+To add the metric to the web UI, modify the appropriate file in
+"ui/ts/pages/*.ts". Someone more qualified than me can elaborate, like @maxlang.
 
 Sub-registries
 
-It's common for a Registry to become part of another Registry through the "Add" and "MustAdd"
-methods. For example:
+It's common for a Registry to become part of another Registry through the "Add"
+and "MustAdd" methods.
 
 	func NewNodeStatusMonitor(serverRegistry *registry) {
 		nodeRegistry := metric.NewRegistry()
 
-		// Add the registry for this node to the root-level server Registry. When accessed from
-		// through the serverRegistry, all metrics from the nodeRegistry will have the prefix
-		// "cr.node.".
+		// Add the registry for this node to the root-level server Registry. When
+		// accessed from through the serverRegistry, all metrics from the
+		// nodeRegistry will have the prefix "cr.node.".
 		serverRegistry.MustAdd("cr.node.%s", nodeRegistry)
 	}
 
-I recommend keeping a root-level registry (for CockroachDB, that's Server.registry) and creating
-a hierarchy of Registry instances underneath that to make your metrics more manageable.
+Node-level sub-registries are added by calling:
+
+	(*metric.MetricRecorder).AddNodeRegistry(YOUR_NODE_SUBREGISTRY)
 
 Testing
 
@@ -65,9 +70,9 @@ probably want to call methods in TestServer such as MustGetSQLCounter() to
 verify that the metric was updated correctly. See "sql/metric_test.go" for an
 example.
 
-Additionally, you can manually verify that your metric is updating by using the metrics
-endpoint.  For example, if you're running the Cockroach DB server with the "--insecure"
-flag, you can use access the endpoint as follows:
+Additionally, you can manually verify that your metric is updating by using the
+metrics endpoint.  For example, if you're running the Cockroach DB server with
+the "--insecure" flag, you can use access the endpoint as follows:
 
 	$ curl http://localhost:26257/_status/metrics/1
 
