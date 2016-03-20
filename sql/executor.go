4766e4a22c40c460990a5084b58702bbb3b942cd
Andrei Matei <andrei@cockroachlabs.com>
2016-03-20 00:34:32 -0400

    sql: move txnState to session.go. No code changes.
    It's a member of Session.
    Move the planner pool there too; the planner will become a member of the
    Session too in the next commit.

--- sql/executor.go
+++ sql/executor.go
@@ -55,19 +55,6 @@ var defaultRetryOpt = retry.Options{
 	Multiplier:     2,
 }
 
-// Release through releasePlanner().
-var plannerPool = sync.Pool{
-	New: func() interface{} {
-		return makePlanner()
-	},
-}
-
-func releasePlanner(p *planner) {
-	// Ensure future users don't clobber the session just used.
-	p.session = nil
-	plannerPool.Put(p)
-}
-
 type traceResult struct {
 	tag   string
 	count int
@@ -380,53 +367,6 @@ func (scc *schemaChangerCollection) execSchemaChanges(
 		}
 	}
 	scc.schemaChangers = scc.schemaChangers[:0]
-}
-
-// reset creates a new Txn and initializes it using the session defaults.
-func (ts *txnState) reset(e *Executor, s *Session) {
-	*ts = txnState{}
-	ts.txn = client.NewTxn(*e.ctx.DB)
-	ts.txn.Proto.Isolation = s.DefaultIsolationLevel
-	ts.tr = s.Trace
-}
-
-func (ts *txnState) willBeRetried() bool {
-	return ts.autoRetry || ts.retryIntent
-}
-
-func (ts *txnState) resetStateAndTxn(state TxnStateEnum) {
-	ts.State = state
-	ts.txn = nil
-}
-
-// updateStateAndCleanupOnErr updates txnState based on the type of error that we
-// received. If it's a retriable error and we're going to retry the txn,
-// then the state moves to RestartWait. Otherwise, the state moves to Aborted
-// and the KV txn is cleaned up.
-func (ts *txnState) updateStateAndCleanupOnErr(pErr *roachpb.Error, e *Executor) {
-	if pErr == nil {
-		panic("updateStateAndCleanupOnErr called with no error")
-	}
-	if pErr.TransactionRestart == roachpb.TransactionRestart_NONE || !ts.willBeRetried() {
-		// We can't or don't want to retry this txn, so the txn is over.
-		e.txnAbortCount.Inc(1)
-		ts.txn.CleanupOnError(pErr)
-		ts.resetStateAndTxn(Aborted)
-	} else {
-		// If we got a retriable error, move the SQL txn to the RestartWait state.
-		// Note that TransactionAborted is also a retriable error, handled here;
-		// in this case cleanup for the txn has been done for us under the hood.
-		switch pErr.TransactionRestart {
-		case roachpb.TransactionRestart_BACKOFF:
-			// TODO(spencer): Get rid of BACKOFF retries. Note that we don't propagate
-			// the backoff hint to the client anyway. See #5249
-			fallthrough
-		case roachpb.TransactionRestart_IMMEDIATE:
-			ts.State = RestartWait
-		default:
-			panic(fmt.Sprintf("unexpected restart value: %s", pErr.TransactionRestart))
-		}
-	}
 }
 
 // ExecuteStatements executes the given statement(s) and returns a response.
--- sql/session.go
+++ sql/session.go
@@ -17,6 +17,8 @@
 package sql
 
 import (
+	"fmt"
+	"sync"
 	"time"
 
 	"golang.org/x/net/trace"
@@ -42,6 +44,19 @@ type SessionOffset struct {
 
 func (*SessionLocation) isSessionTimezone() {}
 func (*SessionOffset) isSessionTimezone()   {}
+
+// Release through releasePlanner().
+var plannerPool = sync.Pool{
+	New: func() interface{} {
+		return makePlanner()
+	},
+}
+
+func releasePlanner(p *planner) {
+	// Ensure future users don't clobber the session just used.
+	p.session = nil
+	plannerPool.Put(p)
+}
 
 // Session contains the state of a SQL client connection.
 type Session struct {
@@ -117,4 +132,51 @@ type txnState struct {
 	// TODO(andrei): this is the same as Session.Trace. Consider removing this and
 	// passing the Session along everywhere the trace is needed.
 	tr trace.Trace
+}
+
+// reset creates a new Txn and initializes it using the session defaults.
+func (ts *txnState) reset(e *Executor, s *Session) {
+	*ts = txnState{}
+	ts.txn = client.NewTxn(*e.ctx.DB)
+	ts.txn.Proto.Isolation = s.DefaultIsolationLevel
+	ts.tr = s.Trace
+}
+
+func (ts *txnState) willBeRetried() bool {
+	return ts.autoRetry || ts.retryIntent
+}
+
+func (ts *txnState) resetStateAndTxn(state TxnStateEnum) {
+	ts.State = state
+	ts.txn = nil
+}
+
+// updateStateAndCleanupOnErr updates txnState based on the type of error that we
+// received. If it's a retriable error and we're going to retry the txn,
+// then the state moves to RestartWait. Otherwise, the state moves to Aborted
+// and the KV txn is cleaned up.
+func (ts *txnState) updateStateAndCleanupOnErr(pErr *roachpb.Error, e *Executor) {
+	if pErr == nil {
+		panic("updateStateAndCleanupOnErr called with no error")
+	}
+	if pErr.TransactionRestart == roachpb.TransactionRestart_NONE || !ts.willBeRetried() {
+		// We can't or don't want to retry this txn, so the txn is over.
+		e.txnAbortCount.Inc(1)
+		ts.txn.CleanupOnError(pErr)
+		ts.resetStateAndTxn(Aborted)
+	} else {
+		// If we got a retriable error, move the SQL txn to the RestartWait state.
+		// Note that TransactionAborted is also a retriable error, handled here;
+		// in this case cleanup for the txn has been done for us under the hood.
+		switch pErr.TransactionRestart {
+		case roachpb.TransactionRestart_BACKOFF:
+			// TODO(spencer): Get rid of BACKOFF retries. Note that we don't propagate
+			// the backoff hint to the client anyway. See #5249
+			fallthrough
+		case roachpb.TransactionRestart_IMMEDIATE:
+			ts.State = RestartWait
+		default:
+			panic(fmt.Sprintf("unexpected restart value: %s", pErr.TransactionRestart))
+		}
+	}
 }
