5f00ba0923e5146075b9630c18688a4804614311
Bram Gruneir <bram@cockroachlabs.com>
2016-03-21 16:03:33 -0400

    acceptance: adding repair test
    This commit adds a dynamic client which is designed to handle clusters with
    changing shapes. So it is adaptable to adding and removing nodes.
    
    Also, this includes the first isolated load. This is the first of many loads
    that we will be able to apply to tests in a more generic way. This load is
    designed to be a non-contentious one that just inserts into a table and checks
    that the values in the table are still correct.
    
    The repair test so far is only an insert test. I will be following this up with
    changes to cluster to handle adding and permanently removing nodes.
    
    This is part of #5345.

---
+++ acceptance/repair_test.go
@@ -0,0 +1,55 @@
+// Copyright 2015 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+
+package acceptance
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/acceptance/cluster"
+	"github.com/cockroachdb/cockroach/util/timeutil"
+)
+
+// RepairTest kills and starts new nodes systematically to ensure we do
+// indeed repair the cluster.
+func TestRepair(t *testing.T) {
+	runTestOnConfigs(t, testRepairInner)
+}
+
+func testRepairInner(t *testing.T, c cluster.Cluster, cfg cluster.TestConfig) {
+	start := timeutil.Now()
+	deadline := start.Add(cfg.Duration)
+
+	running := func() bool {
+		select {
+		case <-stopper:
+			return false
+		default:
+		}
+		return timeutil.Now().Before(deadline)
+	}
+
+	dc := newDynamicClient(t, c, running)
+
+	// Add some loads.
+	for i := 0; i < c.NumNodes()*2; i++ {
+		go insertLoad(dc)
+	}
+
+	// TODO(bram): #5345 add repair mechanism.
+
+	for running() {
+	}
+}
