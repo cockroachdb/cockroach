8847236482d3354e57f395e5e97b74625bc996a5
Max Lang <max@cockroachlabs.com>
2016-03-23 16:37:43 -0700

    Checks the clusters version to ensure it's up to date.
    - Shows a warning if the cluster is out of date
    - Shows a warning if the node versions are mismatched

---
+++ ui/ts/components/outdatedbanner.ts
@@ -0,0 +1,30 @@
+// source: components/outdatabanner.ts
+/// <reference path="../../bower_components/mithriljs/mithril.d.ts" />
+/// <reference path="banner.ts" />
+
+module Components {
+  "use strict";
+  /**
+   * HelpUs is used for the sign up flow to capture user information
+   */
+  export module OutdatedBanner {
+
+    export function controller(): any {
+    }
+
+    export function view(ctrl: any): _mithril.MithrilVirtualElement {
+      if (Models.CockroachLabs.cockroachLabsSingleton.versionStatus.error) {
+        return m.component(Components.Banner, {
+          bannerClass: ".outdated",
+          content: [
+            m("span.icon-warning"),
+            Models.CockroachLabs.cockroachLabsSingleton.versionStatus.message,
+            m("a", {href: "https://www.cockroachlabs.com/docs/install-cockroachdb.html", target: "_blank"}, m("button", "Update")),
+          ],
+          onclose: Models.CockroachLabs.cockroachLabsSingleton.dismissVersionCheck,
+        });
+      }
+      return m("");
+    }
+  }
+}
