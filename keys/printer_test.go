5201cfb749b6b52ee950f306dbe748739bbbd03e
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2016-03-25 16:59:00 -0400

    encoding: Remove unnecessary TimeDesc tag
    Instead, we can just invert the encoding of Time for descending
    direction encodings like we do for ints, floats, decimals, etc...

--- keys/printer_test.go
+++ keys/printer_test.go
@@ -127,7 +127,7 @@ func TestPrettyPrint(t *testing.T) {
 			"/Table/42/Sat Mar  7 11:06:39 UTC 2015"},
 		{makeKey(MakeTablePrefix(42),
 			roachpb.RKey(encoding.EncodeTimeDescending(nil, tm))),
-			"/Table/42/Sat Mar  7 11:06:39 UTC 2015"},
+			"/Table/42/Mon Oct 27 12:53:19 UTC 1924"},
 		{makeKey(MakeTablePrefix(42),
 			roachpb.RKey(encoding.EncodeDecimalAscending(nil, inf.NewDec(1234, 2)))),
 			"/Table/42/12.34"},
