67796f0be4c494d262240b98f32c6e44c309f476
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2016-03-30 17:16:36 -0400

    sql: Add support for pgwire flush clientMessageType
    This enables placeholder support for the node.js driver when talking to
    CockroachDB.

--- sql/pgwire/clientmessagetype_string.go
+++ sql/pgwire/clientmessagetype_string.go
@@ -6,16 +6,18 @@ import "fmt"
 
 const (
 	_clientMessageType_name_0 = "clientMsgBindclientMsgCloseclientMsgDescribeclientMsgExecute"
-	_clientMessageType_name_1 = "clientMsgParseclientMsgSimpleQuery"
-	_clientMessageType_name_2 = "clientMsgSync"
-	_clientMessageType_name_3 = "clientMsgTerminate"
+	_clientMessageType_name_1 = "clientMsgFlush"
+	_clientMessageType_name_2 = "clientMsgParseclientMsgSimpleQuery"
+	_clientMessageType_name_3 = "clientMsgSync"
+	_clientMessageType_name_4 = "clientMsgTerminate"
 )
 
 var (
 	_clientMessageType_index_0 = [...]uint8{0, 13, 27, 44, 60}
-	_clientMessageType_index_1 = [...]uint8{0, 14, 34}
-	_clientMessageType_index_2 = [...]uint8{0, 13}
-	_clientMessageType_index_3 = [...]uint8{0, 18}
+	_clientMessageType_index_1 = [...]uint8{0, 14}
+	_clientMessageType_index_2 = [...]uint8{0, 14, 34}
+	_clientMessageType_index_3 = [...]uint8{0, 13}
+	_clientMessageType_index_4 = [...]uint8{0, 18}
 )
 
 func (i clientMessageType) String() string {
@@ -23,13 +25,15 @@ func (i clientMessageType) String() string {
 	case 66 <= i && i <= 69:
 		i -= 66
 		return _clientMessageType_name_0[_clientMessageType_index_0[i]:_clientMessageType_index_0[i+1]]
+	case i == 72:
+		return _clientMessageType_name_1
 	case 80 <= i && i <= 81:
 		i -= 80
-		return _clientMessageType_name_1[_clientMessageType_index_1[i]:_clientMessageType_index_1[i+1]]
+		return _clientMessageType_name_2[_clientMessageType_index_2[i]:_clientMessageType_index_2[i+1]]
 	case i == 83:
-		return _clientMessageType_name_2
-	case i == 88:
 		return _clientMessageType_name_3
+	case i == 88:
+		return _clientMessageType_name_4
 	default:
 		return fmt.Sprintf("clientMessageType(%d)", i)
 	}
