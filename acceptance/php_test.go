8d4de5c129ccbfe05a4f6d78a9d94ab0bdf0627f
David Taylor <tinystatemachine@gmail.com>
2016-03-31 20:24:15 +0000

    sql/pgwire: explicitly handle 0, 1 or n format codes
    refactored slightly to explicitly handle the cases allowed by the spec and check for a few
    invalid cases that we were previously allowing (eg 0 < format codes < params).
    
    Fixes #5758.

--- acceptance/php_test.go
+++ acceptance/php_test.go
@@ -38,4 +38,14 @@ $result = pg_query_params('SELECT 1, 2 > $1, $1', [%v])
 	or kill('Query failed: ' . pg_last_error());
 $arr = pg_fetch_row($result);
 ($arr === ['1', 'f', '3']) or kill('Unexpected: ' . print_r($arr, true));
+
+$dbh = new PDO('pgsql:','', null, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$dbh->exec('CREATE database bank');
+$dbh->exec('CREATE table bank.accounts (id INT PRIMARY KEY, balance INT)');
+$dbh->exec('INSERT INTO bank.accounts (id, balance) VALUES (1, 1000), (2, 250)');
+$dbh->beginTransaction();
+$stmt = $dbh->prepare('UPDATE bank.accounts SET balance = balance + :deposit WHERE id=:account');
+$stmt->execute(array('account' => 1, 'deposit' => 10));
+$stmt->execute(array('account' => 2, 'deposit' => -10));
+$dbh->commit();
 `
