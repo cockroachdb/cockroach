9e63a490ee457f84e6031898d60b3963b899c9be
Seif Lotfy <seif.lotfy@gmail.com>
2016-03-31 13:03:17 +0200

    Add backfilling functionality for altering tables by adding columns
    When adding new columns make sure default values are backfilled.
    If a column is to be mutated with a non-null column but no default
    value, return error.
    Add column mutation (Addition) test coverage
    * Test NULL allowed without Default
    * Test NOT NULL with DEFAULT value
    * Test NULL allowed with Default
    * Test NOT NULL without Default (err)
    
    Fixes #5751

--- sql/backfill.go
+++ sql/backfill.go
@@ -18,6 +18,7 @@ package sql
 
 import (
 	"bytes"
+	"fmt"
 	"sort"
 
 	"github.com/cockroachdb/cockroach/client"
@@ -71,6 +72,7 @@ func (ids indexesByID) Swap(i, j int) {
 func (p *planner) backfillBatch(b *client.Batch, tableDesc *TableDescriptor) *roachpb.Error {
 	var droppedColumnDescs []ColumnDescriptor
 	var droppedIndexDescs []IndexDescriptor
+	var newColumnDescs []ColumnDescriptor
 	var newIndexDescs []IndexDescriptor
 	// Mutations are applied in a FIFO order. Only apply the first set
 	// of mutations.
@@ -84,8 +86,7 @@ func (p *planner) backfillBatch(b *client.Batch, tableDesc *TableDescriptor) *ro
 		case DescriptorMutation_ADD:
 			switch t := m.Descriptor_.(type) {
 			case *DescriptorMutation_Column:
-				// TODO(vivek): Add column to new columns and use it
-				// to fill in default values.
+				newColumnDescs = append(newColumnDescs, *t.Column)
 
 			case *DescriptorMutation_Index:
 				newIndexDescs = append(newIndexDescs, *t.Index)
@@ -101,16 +102,27 @@ func (p *planner) backfillBatch(b *client.Batch, tableDesc *TableDescriptor) *ro
 			}
 		}
 	}
-
 	// TODO(vivek): Break these backfill operations into chunks. All of them
 	// will fail on big tables (see #3274).
 
-	// Delete the entire dropped columns.
-	// This used to use SQL UPDATE in the past to update the dropped
-	// column to NULL; but a column in the process of being
-	// dropped is placed in the table descriptor mutations, and
-	// a SQL UPDATE of a column in mutations will fail.
-	if len(droppedColumnDescs) > 0 {
+	defaultExprs, err := p.makeDefaultExprs(newColumnDescs)
+	if err != nil {
+		return roachpb.NewError(err)
+	}
+	if defaultExprs != nil && len(defaultExprs) != len(newColumnDescs) {
+		panic(fmt.Sprintf("Number of default expressions %d != %d num of columns", len(defaultExprs), len(newColumnDescs)))
+	}
+
+	// Check if we're adding any column that requires a default value if the table is not empty
+	nonNullableColumn := ""
+	for _, columnDesc := range newColumnDescs {
+		if columnDesc.DefaultExpr == nil && !columnDesc.Nullable {
+			nonNullableColumn = columnDesc.Name
+		}
+		tableDesc.AddColumn(columnDesc)
+	}
+
+	if len(droppedColumnDescs) > 0 || nonNullableColumn != "" || len(defaultExprs) > 0 {
 		// Run a scan across the table using the primary key.
 		start := roachpb.Key(MakeIndexKeyPrefix(tableDesc.ID, tableDesc.PrimaryIndex.ID))
 		// Use a different batch to perform the scan.
@@ -119,6 +131,16 @@ func (p *planner) backfillBatch(b *client.Batch, tableDesc *TableDescriptor) *ro
 		if pErr := p.txn.Run(batch); pErr != nil {
 			return pErr
 		}
+
+		// Don't add a column without a default expression that is non-nullable when the table is not empty.
+		if nonNullableColumn != "" {
+			for _, result := range batch.Results {
+				if len(result.Rows) > 0 {
+					return roachpb.NewErrorf("column %s contains null values", nonNullableColumn)
+				}
+			}
+		}
+
 		for _, result := range batch.Results {
 			var sentinelKey roachpb.Key
 			for _, kv := range result.Rows {
@@ -127,6 +149,12 @@ func (p *planner) backfillBatch(b *client.Batch, tableDesc *TableDescriptor) *ro
 					// ID. Strip off that suffix to determine the prefix shared with the
 					// other keys for the row.
 					sentinelKey = stripColumnIDLength(kv.Key)
+
+					// Delete the entire dropped columns.
+					// This used to use SQL UPDATE in the past to update the dropped
+					// column to NULL; but a column in the process of being
+					// dropped is placed in the table descriptor mutations, and
+					// a SQL UPDATE of a column in mutations will fail.
 					for _, columnDesc := range droppedColumnDescs {
 						// Delete the dropped column.
 						colKey := keys.MakeColumnKey(sentinelKey, uint32(columnDesc.ID))
@@ -134,6 +162,26 @@ func (p *planner) backfillBatch(b *client.Batch, tableDesc *TableDescriptor) *ro
 							log.Infof("Del %s", colKey)
 						}
 						b.Del(colKey)
+					}
+					// Add columns.
+					for i, expr := range defaultExprs {
+						if expr == nil {
+							continue
+						}
+						col := newColumnDescs[i]
+						colKey := keys.MakeColumnKey(sentinelKey, uint32(col.ID))
+						d, err := expr.Eval(p.evalCtx)
+						if err != nil {
+							return roachpb.NewError(err)
+						}
+						val, err := marshalColumnValue(col, d, p.evalCtx.Args)
+						if err != nil {
+							return roachpb.NewError(err)
+						}
+						if log.V(2) {
+							log.Infof("CPut %s -> %v", colKey, val)
+						}
+						b.CPut(colKey, val, nil)
 					}
 				}
 			}
