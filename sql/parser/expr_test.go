52408501ca8a202c4b5106bfeb1c9dbd8e67a3c3
Andrei Matei <andrei@cockroachlabs.com>
2016-04-04 13:55:31 -0400

    sql: make qname keep the original verion, before normalization
    This will be used have more discerning qname.String() implementations
    that work on the original qname. We'll have one that ignores the @
    indirections.

--- sql/parser/expr_test.go
+++ sql/parser/expr_test.go
@@ -87,7 +87,7 @@ func TestNormalizeTableName(t *testing.T) {
 		if err != nil {
 			t.Fatalf("%s: expected success, but found %v", tc.in, err)
 		}
-		q.(*QualifiedName).ClearString()
+		q.(*QualifiedName).ClearOrig()
 		if out := q.String(); tc.out != out {
 			t.Errorf("%s: expected %s, but found %s", tc.in, tc.out, out)
 		}
@@ -130,7 +130,7 @@ func TestNormalizeColumnName(t *testing.T) {
 		if err != nil {
 			t.Fatalf("%s: expected success, but found %v", tc.in, err)
 		}
-		q.(*QualifiedName).ClearString()
+		q.(*QualifiedName).ClearOrig()
 		if out := q.String(); tc.out != out {
 			t.Errorf("%s: expected %s, but found %s", tc.in, tc.out, out)
 		}
