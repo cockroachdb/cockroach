4213440ac47d357f8c539e1ebe786754adc45c76
Daniel Harrison <daniel.harrison@gmail.com>
2016-04-05 15:05:31 -0400

    Support parsing pgwire timestamps as they are sent by lib/pq

---
+++ sql/pgwire/types_test.go
@@ -0,0 +1,49 @@
+// Copyright 2016 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// Author: Dan Harrison (daniel.harrison@gmail.com)
+
+package pgwire
+
+import (
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/util/leaktest"
+)
+
+var parseTsTests = []struct {
+	strTimestamp string
+	expected     time.Time
+}{
+	// time.RFC3339Nano for github.com/lib/pq.
+	{"2006-07-08T00:00:00.000000123Z", time.Date(2006, 7, 8, 0, 0, 0, 123, time.FixedZone("UTC", 0))},
+}
+
+// The assertions in this test should also be caught by the integration tests on
+// various drivers.
+func TestParseTs(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	for i, test := range parseTsTests {
+		parsed, err := parseTs(test.strTimestamp)
+		if err != nil {
+			t.Errorf("%d could not parse [%s]: %v", i, test.strTimestamp, err)
+			continue
+		}
+		if !parsed.Equal(test.expected) {
+			t.Errorf("%d parsing [%s] got [%s] expected [%s]", i, test.strTimestamp, parsed, test.expected)
+		}
+	}
+}
