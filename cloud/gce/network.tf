a54ae41c865a01c2a124e1406369ebfa440eb5ea
Cuong Do <cuongdo@users.noreply.github.com>
2016-04-05 17:28:37 -0400

    Make terraform+CockroachDB work on GCE
    This updated terraform config allows one to create an insecure
    CockroachDB cluster on GCE.
    
    Caveats:
    - Secure clusters are NOT supported.
    - I didn't investigate the load balancer issues at all, so that's
      probably still broken.
    - There's no supervisor config for running `cockroach`.

--- cloud/gce/network.tf
+++ cloud/gce/network.tf
@@ -1,9 +1,9 @@
 resource "google_compute_http_health_check" "default" {
   name = "cockroach-health-check"
   request_path = "/"
-  port = "${var.cockroach_port}"
-  check_interval_sec = 2
-  healthy_threshold = 2
+  port = "${var.http_port}"
+  check_interval_sec = 5
+  healthy_threshold = 5
   unhealthy_threshold = 2
   timeout_sec = 2
 }
@@ -19,7 +19,9 @@ resource "google_compute_target_pool" "default" {
 resource "google_compute_forwarding_rule" "default" {
   name = "cockroach-forwarding-rule"
   target = "${google_compute_target_pool.default.self_link}"
-  port_range = "${var.cockroach_port}"
+  # Forward all unprivileged ports, relying on the firewall to filter everything
+  # but the whitelisted ports.
+  port_range = "1024-65535"
 }
 
 resource "google_compute_firewall" "default" {
@@ -28,7 +30,7 @@ resource "google_compute_firewall" "default" {
 
   allow {
     protocol = "tcp"
-    ports = ["${var.cockroach_port}"]
+    ports = ["${var.sql_port}", "${var.http_port}"]
   }
 
   source_ranges = ["0.0.0.0/0"]
--- cloud/gce/variables.tf
+++ cloud/gce/variables.tf
@@ -1,6 +1,10 @@
 # Port used for the load balancer and backends.
-variable "cockroach_port" {
+variable "sql_port" {
   default = "26257"
+}
+
+variable "http_port" {
+  default = "8080"
 }
 
 # GCE region to use.
@@ -13,32 +17,37 @@ variable "gce_zone" {
   default = "us-east1-b"
 }
 
-# GCE project name.
-variable "gce_project" {
-  default = "cockroach-marc"
-}
+# The GCE project under which you want to run your cluster. You'll want to
+# change this.
+variable "gce_project" { }
 
-# GCE account file.
-variable "gce_account_file" {
-  default = "~/terraform/cockroach-marc-64455dfdb138.json"
-}
+# Your JSON-format Google Cloud application credentials. You'll want to change this.
+# To learn how to download your credentials, go here:
+#
+# https://developers.google.com/identity/protocols/application-default-credentials#howtheywork
+variable "gce_account_file" { }
 
 # GCE image name.
 variable "gce_image" {
   default = "ubuntu-os-cloud/ubuntu-1510-wily-v20151021"
 }
 
-# Path to the cockroach binary.
+# Path to the cockroach binary. An empty value results in the latest official
+# binary being used.
 variable "cockroach_binary" {
-  default = "~/cockroach/src/github.com/cockroachdb/cockroach/cockroach"
+  default = ""
 }
 
 # Name of the ssh key pair to use for GCE instances.
 # The public key will be passed at instance creation, and the private
 # key will be used by the local ssh client.
+#
 # The path is expanded to: ~/.ssh/<key_name>.pub
+#
+# If you use `gcloud compute ssh` or `gcloud compute copy-files`, you may want
+# to leave this as "google_compute_engine" for convenience.
 variable "key_name" {
-  default = "gce_cockroach"
+  default = "google_compute_engine"
 }
 
 # Action is one of "init" or "start". init should only be specified when
@@ -56,12 +65,12 @@ variable "gossip" {
   #default = ""
 }
 
-variable "load_balancer_address" {
-  default = "104.196.4.96"
-  #default = ""
-}
-
 # Number of instances to start.
 variable "num_instances" {
-  default = 3
 }
+
+# SHA of the cockroach binary to pull down. If none, the latest is fetched.
+variable "cockroach_sha" {
+  default = ""
+}
+
