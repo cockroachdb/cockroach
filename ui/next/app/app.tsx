a375e5e834e1e55488f32882a0b1fef4fd57dbcd
Matt Tracy <matt@cockroachlabs.com>
2016-04-11 19:56:01 -0400

    SystemJS + jspm + React
    Modifies the next UI target to utilize the SystemJS loading system, along with
    the jspm package management system designed for usage with SystemJS. Commit also
    adds React and React-Dom as the first jspm dependencies of the next UI target.
    
    The folder `ui/next` now maintains a separate set of dependencies from the main
    `ui/next` - this includes dependencies from npm, jspm, and typings.
    
    All "next"-related targets in the Makefile have also been moved to a separate
    makefile under `ui/next`. It can still be made from the top level `ui/` directory
    with `make next`.
    
    Details of SystemJS/jspm:
    
    + `make next` target no longer compiles the `next` files into an embedded
      bundle. Instead, we depend on CockroachDB's 'uidebug' build target, which
      ignores the embedded UI and insetead serves directly from the file system.
      When `uidebug` is enabled, the next version of the UI is available directly
      from the `/next` URL when accessing the admin website.
    + SystemJS is able to compile typescript directly in the browser. There is thus
      no need for a build step (i.e. `make next`) when developing changes to
      typescript files; a page refresh will automatically compile the new changes.
    + `make next` still must be run in order to pull down dependencies. It should
      also be run before committing changes, in order to use the linter.
    
    Additional changes:
    
    + Added jspm/typings dependencies for React and React-Dom.
    + Added tslint.json to "next" build directory.
    + "Next" no longer builds CSS - the next UI version is only a technical rewrite,
      its target appearance is exactly the same as the current ui.

---
+++ ui/next/app/app.tsx
@@ -0,0 +1,14 @@
+/// <reference path="../typings/main.d.ts" />
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div id="content">
+    <h1>Cockroach Admin UI Next</h1>
+    <p>
+      The Cockroach Admin UI Next version is a major refactoring of the current web UI which is under development.
+    </p>
+  </div>,
+  document.getElementById("root")
+);
