86e931aca160d63ad9bc03aad02b2aaba3ffc57c
Matt Tracy <matt@cockroachlabs.com>
2016-04-15 20:46:30 -0400

    ui next: React router + top level routes
    Commit installs react-router project into the next ui. The four current
    top-level routes ("Cluster", "Nodes", "Databases", and "Help Us") have all been
    configured in the main application.
    
    Commit also recreates the side navigation bar and header bar from the current UI
    (minus the "health" indicator in the header). All links in the navigation bar
    are route-aware; the active page is highlighted, and clicking links will
    change the current route. The pages have no content yet, only placeholder text
    to indicate that they are selected.
    
    This commit establishes an initial skeleton file structure for the next ui;
    `components` directory contains stateless view-only components, while
    `containers` should contain "smart" components which should eventually be aware
    of the program's state.

---
+++ ui/next/app/containers/databases.tsx
@@ -0,0 +1,23 @@
+/// <reference path="../../typings/main.d.ts" />
+
+import * as React from "react";
+
+/**
+ * DatabasesTitle renders the main content of the databases page.
+ */
+export class DatabasesMain extends React.Component<{}, {}> {
+	render() {
+		return <div className="section">
+			<h1>Databases Page</h1>
+		</div>;
+	}
+}
+
+/**
+ * DatabasesTitle renders the header of the databases page.
+ */
+export class DatabasesTitle extends React.Component<{}, {}> {
+	render() {
+		return <h2>Databases</h2>;
+	}
+}
---
+++ ui/next/app/containers/layout.tsx
@@ -0,0 +1,40 @@
+/// <reference path="../../typings/main.d.ts" />
+
+import * as React from "react";
+import SideBar from "../components/layoutSidebar.tsx";
+import Header from "../components/layoutHeader.tsx";
+
+/**
+ * LayoutProps are the props supported by the Layout class.
+ */
+interface LayoutProps<T, V> {
+	main: React.ReactElement<T>;
+	title: React.ReactElement<V>;
+}
+
+/**
+ * Layout defines the main layout of all admin ui pages. This includes static
+ * navigation bars and footers which should be present on every page.
+ *
+ * Individual pages provide their content via react-router, passing in named
+ * components "main" and "title".
+ */
+class Layout<T, V> extends React.Component<LayoutProps<T, V>, {}> {
+	render() {
+		return <div id="content">
+			<SideBar/>
+			<div id="page-container">
+				<div id="root">
+					<div className="page">
+					<Header>
+						{this.props.title}
+					</Header>
+					{this.props.main}
+					</div>
+				</div>
+			</div>
+		</div>;
+	}
+}
+
+export default Layout;
