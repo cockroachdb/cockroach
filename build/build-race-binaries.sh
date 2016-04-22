562bb54f86997052aa93b63f5c3104e92df9270e
marc <marc@cockroachlabs.com>
2016-04-22 11:00:31 +0200

    Build and push binaries with race-detection enabled.
    We really don't need to build those every time, so I'm using the branch
    'data-race' as the trigger.

---
+++ build/build-race-binaries.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Build cockroach binary with race detection enabled.
+
+set -euo pipefail
+
+source $(dirname $0)/build-common.sh
+
+# This is mildly tricky: This script runs itself recursively. The
+# first time it is run it does not take the if-branch below and
+# executes on the host computer. It uses the builder.sh script to run
+# itself inside of docker passing "docker" as the argument causing the
+# commands in the if-branch to be executed within the docker
+# container.
+if [ "${1-}" = "docker" ]; then
+    time make STATIC=1 build GOFLAGS="-race"
+    check_static cockroach
+    strip -S cockroach
+    exit 0
+fi
+
+# Build the cockroach and test binaries.
+$(dirname $0)/builder.sh $0 docker
