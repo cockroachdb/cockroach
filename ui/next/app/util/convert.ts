661649497cef7c9f27773355ff5528967284ecfe
Matt Tracy <matt@cockroachlabs.com>
2016-05-11 13:26:00 -0400

    ui next: Line Graph Component
    Commit adds the `LineGraph` component. This component is designed to be placed
    inside of a MetricsDataProvider component. Most of the NVD3 code has been taken
    directly from the previous UI, with some modifications to work with the new data
    model.
    
    Commit also changes the name of 'Selector' to 'Metric', to avoid confusion with
    the Reselect library's selectors.
    
    Commit also adds a TODO list in app/app.tsx, cataloging the remaining
    unimplemented components and pages in the ui next project.

--- ui/next/app/util/convert.ts
+++ ui/next/app/util/convert.ts
@@ -1,6 +1,13 @@
 /**
- * NanosToMilli converts a nanoseconds value into to milliseconds.
+ * NanoToMilli converts a nanoseconds value into milliseconds.
  */
 export function NanoToMilli(nano: number): number {
   return nano / 1.0e6;
+}
+
+/**
+ * MilliToNano converts a millisecond value into nanoseconds.
+ */
+export function MilliToNano(milli: number): number {
+  return milli * 1.0e6;
 }
