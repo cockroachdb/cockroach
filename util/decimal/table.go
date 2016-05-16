75b2f22461183d99e07a1992f88b2084feb55b89
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2016-05-16 15:27:57 -0400

    decimal: Create powers of 10 lookup table for inf.Dec and big.Int
    Fixes #5814.
    
    This change creates a powers of 10 lookup table and then uses it where
    possible. This allows us to reduce the need to allocate in a few key
    places when dealing with decimals.

---
+++ util/decimal/table.go
@@ -0,0 +1,58 @@
+// Copyright 2016 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// Author: Nathan VanBenschoten (nvanbenschoten@gmail.com)
+
+package decimal
+
+import (
+	"math/big"
+
+	"gopkg.in/inf.v0"
+)
+
+// tableSize is the magnitude of the maximum power of 10 exponent that is stored
+// in the pow10LookupTable. The table will store both negative and positive powers
+// 10 up to this magnitude. For instance, if the tableSize if 3, then the lookup
+// table will store power of 10 values from 10^-3 to 10^3.
+const tableSize = 32
+
+var pow10LookupTable [2*tableSize + 1]inf.Dec
+
+func init() {
+	tmpInt := new(big.Int)
+	for i := -tableSize; i <= tableSize; i++ {
+		bi := pow10LookupTable[i+tableSize].UnscaledBig()
+		bi.SetInt64(10)
+		bi.Exp(bi, tmpInt.SetInt64(int64(i)), nil)
+	}
+}
+
+// PowerOfTenDec returns an *inf.Dec with the value 10^pow. It should
+// be treated as immutable.
+func PowerOfTenDec(pow int) *inf.Dec {
+	if pow >= -tableSize && pow <= tableSize {
+		return &pow10LookupTable[pow+tableSize]
+	}
+	dec := inf.NewDec(10, 0)
+	bi := dec.UnscaledBig()
+	bi.Exp(bi, big.NewInt(int64(pow)), nil)
+	return dec
+}
+
+// PowerOfTenInt returns a *big.Int with the value 10^pow. It should
+// be treated as immutable.
+func PowerOfTenInt(pow int) *big.Int {
+	return PowerOfTenDec(pow).UnscaledBig()
+}
