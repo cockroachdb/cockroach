3e563b0bc2fb39f5024406565aeb70872e7560d3
Radu Berinde <radu.berinde@gmail.com>
2016-05-16 17:57:37 -0400

    sql: rowReceiver interface, channel message type
    rowReceiver is an interface for entities which can accept rows; it will be
    implemented by output routers, input synchronizers, and outgoing mailboxes.
    
    Also adding the message type for channels implementing internal streams. I was
    torn between using a single channel with a row + error message, or separate
    data and error channels. I ran some benchmarks and the single channel was
    faster: https://github.com/RaduBerinde/playground/blob/601b367/chan_test/main_test.go#L1

---
+++ sql/distsql/basic.go
@@ -0,0 +1,39 @@
+// Copyright 2016 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// Author: Radu Berinde (radu@cockroachlabs.com)
+
+package distsql
+
+import "github.com/cockroachdb/cockroach/sql/sqlbase"
+
+type row []sqlbase.EncDatum
+
+// rowReceiver is any component of a flow that receives rows from another
+// component. It can be an input synchronizer, a router, or a mailbox.
+type rowReceiver interface {
+	// pushRow sends a row to this receiver. May block.
+	pushRow(row row)
+	// close is called when we have no more rows; it causes the rowReceiver to
+	// process all rows and clean up. If err is not null, the error is sent to
+	// the receiver (and the function may block).
+	close(err error)
+}
+
+// streamMsg is the message used in the channels that implement
+// local physical streams.
+type streamMsg struct {
+	row row
+	err error
+}
