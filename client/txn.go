75bdb902c5200a3dbb96db527d1a5b732167c349
Kenji Kaneda <kenji.kaneda@gmail.com>
2016-05-18 07:14:10 -0700

    client: Return TransactionAbortedError on a  deadline-exceeded error
    This is consistent with what Replica.EndTransaction does.
    
    TransactionAbortedError will be converted to RetryableTxnError since
    its canRestartTransaction() returns TransactionRestart_BACKOFF.

--- client/txn.go
+++ client/txn.go
@@ -654,11 +654,7 @@ func (txn *Txn) send(ba roachpb.BatchRequest) (*roachpb.BatchResponse, *roachpb.
 		// Check that read only transactions do not violate their deadline.
 		if endTxnRequest.Deadline != nil {
 			if endTxnRequest.Deadline.Less(txn.Proto.Timestamp) {
-				return nil, roachpb.NewErrorf(
-					"read-only txn timestamp violates deadline: %s < %s",
-					endTxnRequest.Deadline,
-					txn.Proto.Timestamp,
-				)
+				return nil, roachpb.NewError(roachpb.NewTransactionAbortedError())
 			}
 		}
 		// This normally happens on the server and sent back in response
