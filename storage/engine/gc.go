e9371911b90bd38e2c85f36288704b7234233363
Matt Jibson <matt.jibson@gmail.com>
2016-05-19 01:51:36 -0400

    storage: change the GC to keep values within the GC interval
    Time travel queries (#5963) will need to be able to request data from
    the MVCC layer for anytime within the GC interval. As a first change,
    modify the GC to return correct results for all reads with timestamps
    within the GC interval. This means we must now keep any value that was
    valid for any time during the GC, which in particular includes values
    with timestamps before the GC interval. A deleted value is the exception:
    if the most recent value is deleted and it is outside the GC window,
    it is marked for GC.
    
    Further changes will record this valid GC window per replica and enforce
    that reads are not done outside of it.

--- storage/engine/gc.go
+++ storage/engine/gc.go
@@ -44,7 +44,9 @@ func MakeGarbageCollector(now roachpb.Timestamp, policy config.GCPolicy) Garbage
 // garbage collection policy for batches of values for the same key.
 // Returns the timestamp including, and after which, all values should
 // be garbage collected. If no values should be GC'd, returns
-// roachpb.ZeroTimestamp.
+// roachpb.ZeroTimestamp. keys must be in descending time order.
+// Values deleted at or before the returned timestamp can be deleted without
+// invalidating any reads in the time interval [gc.expiration, \infinity).
 func (gc GarbageCollector) Filter(keys []MVCCKey, values [][]byte) roachpb.Timestamp {
 	if gc.policy.TTLSeconds <= 0 {
 		return roachpb.ZeroTimestamp
@@ -54,34 +56,31 @@ func (gc GarbageCollector) Filter(keys []MVCCKey, values [][]byte) roachpb.Times
 	}
 
 	// Loop over values. All should be MVCC versions.
+	var i int
+	var key MVCCKey
 	delTS := roachpb.ZeroTimestamp
-	survivors := false
-	for i, key := range keys {
+	for i, key = range keys {
 		if !key.IsValue() {
 			log.Errorf("unexpected MVCC metadata encountered: %q", key)
 			return roachpb.ZeroTimestamp
 		}
-		deleted := len(values[i]) == 0
-		if i == 0 {
-			// If the first value isn't a deletion tombstone, don't consider
-			// it for GC. It should always survive if non-deleted.
-			if !deleted {
-				survivors = true
-				continue
-			}
+		if gc.expiration.Less(key.Timestamp) {
+			continue
 		}
-		// If we encounter a version older than our GC timestamp, mark for deletion.
-		if key.Timestamp.Less(gc.expiration) {
-			delTS = key.Timestamp
-			break
-		} else if !deleted {
-			survivors = true
+		// Now key.Timestamp is <= gc.expiration.
+		// Mark the previous timestamp for deletion.
+		if i+1 < len(keys) {
+			delTS = keys[i+1].Timestamp
 		}
+		break
 	}
-	// If there are no non-deleted survivors, return timestamp of first key
-	// to delete all entries.
-	if !survivors {
-		return keys[0].Timestamp
+	// We can GC deleted values.
+	for ; i >= 0; i-- {
+		deleted := len(values[i]) == 0
+		if !deleted {
+			break
+		}
+		delTS = keys[i].Timestamp
 	}
 	return delTS
 }
