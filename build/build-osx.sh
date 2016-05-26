1d187c574b332cf3f99d46699ba12535a98b7f33
Tamir Duberstein <tamird@gmail.com>
2016-05-26 16:26:54 -0400

    build: use upstream's xgo image
    This is reasonably safe now that they have basic smoke tests.

--- build/build-osx.sh
+++ build/build-osx.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-go get github.com/karalabe/xgo
+go get -u github.com/karalabe/xgo
 # OSX 10.9 is the most recent version available at time of writing.
 # If changing the OS/arch target, adjust the filename in push-aws.sh.
-xgo --image cockroachdb/xgo:20160413 --targets=darwin-10.9/amd64 --go=1.6.x --ldflags="$($(dirname "${0}")/ldflags.sh)" ${GOPATH%%:*}/src/github.com/cockroachdb/cockroach
+xgo --targets=darwin-10.9/amd64 --go=1.6.2 --ldflags="$($(dirname "${0}")/ldflags.sh)" ${GOPATH%%:*}/src/github.com/cockroachdb/cockroach
