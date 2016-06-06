d2505c7dd20a8b8ac414858e8bf6dfe6a562d1fd
Tamir Duberstein <tamird@gmail.com>
2016-06-06 12:10:59 -0400

    Update dependencies
    Omits https://github.com/grpc/grpc-go/pull/690 which "temporarily"
    removed the (*grpc.ClientConn).{State,WaitForStateChange} APIs.

--- ts/timeseries.pb.gw.go
+++ ts/timeseries.pb.gw.go
@@ -83,7 +83,11 @@ func RegisterTimeSeriesHandler(ctx context.Context, mux *runtime.ServeMux, conn 
 			}(ctx.Done(), cn.CloseNotify())
 		}
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		resp, md, err := request_TimeSeries_Query_0(runtime.AnnotateContext(ctx, req), inboundMarshaler, client, req, pathParams)
+		rctx, err := runtime.AnnotateContext(ctx, req)
+		if err != nil {
+			runtime.HTTPError(ctx, outboundMarshaler, w, req, err)
+		}
+		resp, md, err := request_TimeSeries_Query_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, outboundMarshaler, w, req, err)
