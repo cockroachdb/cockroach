bc3c2fd566d3b000204af1b572fb19748f501f36
Daniel Harrison <daniel.harrison@gmail.com>
2016-06-08 10:23:52 -0400

    acceptance: bidir and forward reference tests
    Split the ReadWrite reference test into one that tests bidirectional
    compatibility and one that tests forward-only.

--- acceptance/reference_test.go
+++ acceptance/reference_test.go
@@ -18,10 +18,10 @@ package acceptance
 
 import "testing"
 
-func TestDockerReadWriteReferenceVersion(t *testing.T) {
+func TestDockerReadWriteBidirectionalReferenceVersion(t *testing.T) {
 	func() { // OneShot assumes it is at specific call depth from TestFoo.
-		if testDockerSingleNode(t, "reference", []string{"/cockroach", "version"}) != nil {
-			t.Skip(`TODO(dt): No /cockroach binary in one-shot container. See #6086.`)
+		if err := testDockerSingleNode(t, "reference", []string{"/cockroach", "version"}); err != nil {
+			t.Skipf(`TODO(dt): No /cockroach binary in one-shot container, see #6086: %s`, err)
 		}
 	}()
 	testDockerSuccess(t, "reference", []string{"/bin/bash", "-c", `
@@ -32,8 +32,8 @@ cd /old
 export PGHOST=localhost
 export PGPORT=""
 
-bin=/reference-version/cockroach
-# TODO(bdarnell): when --background is in reference-version, use it here and below.
+bin=/bidirectional-reference-version/cockroach
+# TODO(bdarnell): when --background is in bidirectional-reference-version, use it here and below.
 $bin start &
 sleep 1
 echo "Use the reference binary to write a couple rows, then render its output to a file and shut down."
@@ -59,12 +59,54 @@ $bin quit
 sleep 1
 
 echo "Read the modified data using the reference binary again."
-bin=/reference-version/cockroach
+bin=/bidirectional-reference-version/cockroach
 $bin start &
 sleep 1
 $bin sql -d old -e "SELECT i, b, s, d, f, v, extract(epoch FROM t) FROM testing" > old.everything
 # diff returns non-zero if different. With set -e above, that would exit here.
 diff new.everything old.everything
 $bin quit && wait
+`})
+}
+
+func TestDockerReadWriteForwardReferenceVersion(t *testing.T) {
+	func() { // OneShot assumes it is at specific call depth from TestFoo.
+		if err := testDockerSingleNode(t, "reference", []string{"/cockroach", "version"}); err != nil {
+			t.Skipf(`TODO(dt): No /cockroach binary in one-shot container, see #6086: %s`, err)
+		}
+	}()
+	testDockerSuccess(t, "reference", []string{"/bin/bash", "-c", `
+set -xe
+mkdir /old
+cd /old
+
+export PGHOST=localhost
+export PGPORT=""
+
+bin=/forward-reference-version/cockroach
+# TODO(bdarnell): when --background is in forward-reference-version, use it here and below.
+$bin start &
+sleep 1
+echo "Use the reference binary to write a couple rows, then render its output to a file and shut down."
+$bin sql -e "CREATE DATABASE old"
+$bin sql -d old -e "CREATE TABLE testing (i int primary key, b bool, s string unique, d decimal, f float, t timestamp, v interval, index sb (s, b))"
+$bin sql -d old -e "INSERT INTO testing values (1, true, 'hello', decimal '3.14159', 3.14159, NOW(), interval '1h')"
+$bin sql -d old -e "INSERT INTO testing values (2, false, 'world', decimal '0.14159', 0.14159, NOW(), interval '234h45m2s234ms')"
+$bin sql -d old -e "SELECT i, b, s, d, f, v, extract(epoch FROM t) FROM testing" > old.everything
+$bin quit && wait # wait will block until all background jobs finish.
+
+bin=/cockroach
+$bin start --background
+echo "Read data written by reference version using new binary"
+$bin sql -d old -e "SELECT i, b, s, d, f, v, extract(epoch FROM t) FROM testing" > new.everything
+# diff returns non-zero if different. With set -e above, that would exit here.
+diff new.everything old.everything
+
+echo "Add a row with the new binary and render the updated data before shutting down."
+$bin sql -d old -e "INSERT INTO testing values (3, false, '!', decimal '2.14159', 2.14159, NOW(), interval '3h')"
+$bin sql -d old -e "SELECT i, b, s, d, f, v, extract(epoch FROM t) FROM testing" > new.everything
+$bin quit && wait
+
+# TODO(dan): Once we have a version that's only forward compatible, test the failure message.
 `})
 }
