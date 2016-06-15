7a2fbce306a434dcf738e51631474aa5d9a17bb0
Vivek Menezes <vivek.menezes@gmail.com>
2016-06-15 13:14:40 -0400

    rpc: properly close connection when stopper stops

--- rpc/context.go
+++ rpc/context.go
@@ -167,7 +167,7 @@ func (ctx *Context) GRPCDial(target string, opts ...grpc.DialOption) (*grpc.Clie
 		}
 		ctx.conns.cache[target] = conn
 
-		ctx.Stopper.RunTask(func() {
+		if !ctx.Stopper.RunTask(func() {
 			ctx.Stopper.RunWorker(func() {
 				if err := ctx.runHeartbeat(conn, target); err != nil && !grpcutil.IsClosedConnection(err) {
 					log.Error(err)
@@ -176,7 +176,9 @@ func (ctx *Context) GRPCDial(target string, opts ...grpc.DialOption) (*grpc.Clie
 				ctx.removeConn(target, conn)
 				ctx.conns.Unlock()
 			})
-		})
+		}) {
+			ctx.removeConn(target, conn)
+		}
 	}
 	return conn, err
 }
