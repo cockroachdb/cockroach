8fe39a1b801be613f472e5539e430c00a84e46dc
Vivek Menezes <vivek.menezes@gmail.com>
2016-06-15 11:45:52 -0400

    add 'created by google.golang.org/grpc' to goroutines allowed to leak

--- util/leaktest/leaktest.go
+++ util/leaktest/leaktest.go
@@ -44,7 +44,8 @@ func interestingGoroutines() (gs []string) {
 			strings.Contains(stack, "signal.signal_recv") ||
 			strings.Contains(stack, "sigterm.handler") ||
 			strings.Contains(stack, "runtime_mcall") ||
-			strings.Contains(stack, "goroutine in C code") {
+			strings.Contains(stack, "goroutine in C code") ||
+			strings.Contains(stack, "created by google.golang.org/grpc") {
 			continue
 		}
 		gs = append(gs, g)
