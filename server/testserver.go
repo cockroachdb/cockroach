94c2041b77a6c953e3201d3a195e7f9a56cc556e
Will Haack <will@cockroachlabs.com>
2016-06-20 13:33:40 -0400

    added waitForFullReplication and addresssed some of Bens comments

--- server/testserver.go
+++ server/testserver.go
@@ -217,7 +217,7 @@ func (ts *TestServer) Start() error {
 	return ts.StartWithStopper(nil, false)
 }
 
-// Starts a testserver with parameters anticipating other nodes joining.
+// StartMultinode starts a testserver with parameters anticipating other nodes joining.
 func (ts *TestServer) StartMultinode() error {
 	return ts.StartWithStopper(nil, true)
 }
