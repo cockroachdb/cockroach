6fe872877b6c078195b6b5fcf676faf5d73a2057
Daniel Harrison <daniel.harrison@gmail.com>
2016-06-21 11:58:51 -0400

    sql: use new encoding for column family values
    NB: This is safe because no codepath currently writes non-empty data in a
    roachpb.Value of type Tuple.
    
    - Put the old implementations of roachpb.SetFoo/GetFoo back. They have to stay
    for backwards compatibility
    - Switch EncDatum to use the new value encoding.
    - Update rowWriter and rowFetcher to use the new encoding, now with less
    internal knowledge! Also store column id diffs instead of column ids in the
    column family encoding.
    
    Switching the roachpb methods back got faster (inlining?). Allocations were unchanged.
    
    name               old time/op    new time/op    delta
    ValueSetBytes-8      73.1ns ± 2%    71.4ns ± 1%   -2.27%          (p=0.016 n=5+5)
    ValueSetFloat-8      70.9ns ± 6%    65.2ns ± 1%   -8.01%          (p=0.008 n=5+5)
    ValueSetBool-8       72.5ns ± 1%    57.7ns ± 1%  -20.46%          (p=0.008 n=5+5)
    ValueSetInt-8        78.3ns ± 1%    64.8ns ± 6%  -17.25%          (p=0.008 n=5+5)
    ValueSetProto-8       188ns ± 1%     184ns ± 1%   -1.71%          (p=0.024 n=5+5)
    ValueSetTime-8       94.4ns ± 4%    90.9ns ± 1%   -3.77%          (p=0.008 n=5+5)
    ValueSetDecimal-8     186ns ± 1%     182ns ± 4%     ~             (p=0.119 n=5+5)
    ValueSetTuple-8      70.2ns ± 1%    70.8ns ± 1%     ~             (p=0.111 n=5+5)
    ValueGetBytes-8      8.56ns ± 3%    8.86ns ±15%     ~             (p=1.000 n=5+5)
    ValueGetFloat-8      20.3ns ± 1%    18.6ns ± 5%   -8.66%          (p=0.008 n=5+5)
    ValueGetBool-8       23.1ns ± 8%    12.4ns ± 1%  -46.31%          (p=0.008 n=5+5)
    ValueGetInt-8        23.4ns ± 4%    14.4ns ± 1%  -38.36%          (p=0.008 n=5+5)
    ValueGetProto-8      60.9ns ± 1%    61.1ns ± 1%     ~             (p=0.802 n=5+5)
    ValueGetTime-8       53.3ns ± 2%    50.3ns ± 5%   -5.52%          (p=0.008 n=5+5)
    ValueGetDecimal-8     186ns ± 0%     182ns ± 1%   -1.99%          (p=0.016 n=4+5)
    ValueGetTuple-8      8.49ns ± 2%    8.69ns ± 5%     ~             (p=0.246 n=5+5)

--- sql/sqlbase/encoded_datum.go
+++ sql/sqlbase/encoded_datum.go
@@ -20,7 +20,6 @@ import (
 	"bytes"
 	"fmt"
 
-	"github.com/cockroachdb/cockroach/roachpb"
 	"github.com/cockroachdb/cockroach/sql/parser"
 	"github.com/cockroachdb/cockroach/util"
 	"github.com/cockroachdb/cockroach/util/encoding"
@@ -80,20 +79,24 @@ func (ed *EncDatum) SetEncoded(typ ColumnType_Kind, enc DatumEncoding, val []byt
 func (ed *EncDatum) SetFromBuffer(
 	typ ColumnType_Kind, enc DatumEncoding, buf []byte,
 ) (remaining []byte, err error) {
-	var encLen int
 	switch enc {
 	case DatumEncoding_ASCENDING_KEY, DatumEncoding_DESCENDING_KEY:
-		encLen, err = encoding.PeekLength(buf)
+		encLen, err := encoding.PeekLength(buf)
+		if err != nil {
+			return nil, err
+		}
+		ed.SetEncoded(typ, enc, buf[:encLen])
+		return buf[encLen:], nil
 	case DatumEncoding_VALUE:
-		encLen, err = roachpb.PeekValueLength(buf)
+		typeOffset, encLen, err := encoding.PeekValueLength(buf)
+		if err != nil {
+			return nil, err
+		}
+		ed.SetEncoded(typ, enc, buf[typeOffset:encLen])
+		return buf[encLen:], nil
 	default:
 		panic(fmt.Sprintf("unknown encoding %s", ed.encoding))
 	}
-	if err != nil {
-		return nil, err
-	}
-	ed.SetEncoded(typ, enc, buf[:encLen])
-	return buf[encLen:], nil
 }
 
 // SetDatum initializes the EncDatum with the given Datum.
@@ -136,11 +139,14 @@ func (ed *EncDatum) Decode(a *DatumAlloc) error {
 	default:
 		panic(fmt.Sprintf("unknown encoding %s", ed.encoding))
 	}
+	if err != nil {
+		return err
+	}
 	if len(rem) != 0 {
 		ed.Datum = nil
 		return util.Errorf("%d trailing bytes in encoded value", len(rem))
 	}
-	return err
+	return nil
 }
 
 // Encoding returns the encoding that is already available (the latter indicated
@@ -168,7 +174,7 @@ func (ed *EncDatum) Encode(a *DatumAlloc, enc DatumEncoding, appendTo []byte) ([
 	case DatumEncoding_DESCENDING_KEY:
 		return EncodeTableKey(appendTo, ed.Datum, encoding.Descending)
 	case DatumEncoding_VALUE:
-		return EncodeTableValue(appendTo, ed.Datum)
+		return EncodeTableValue(appendTo, ColumnID(encoding.NoColumnID), ed.Datum)
 	default:
 		panic(fmt.Sprintf("unknown encoding requested %s", enc))
 	}
