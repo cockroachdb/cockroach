3ef15a08dbd510e591273ada44a3f767e1dd39c8
Matt Jibson <matt.jibson@gmail.com>
2016-06-22 13:32:47 -0400

    sql: correctly escape SQL strings
    Similar to changes in encodeSQLBytes, remove all non-printable characters
    from the output. Unlike encodeSQLBytes, this function is unicode-aware
    and escapes unicode code points.

--- sql/parser/parse_test.go
+++ sql/parser/parse_test.go
@@ -1131,13 +1131,21 @@ func BenchmarkParse(b *testing.B) {
 }
 
 func TestEncodeSQLBytes(t *testing.T) {
+	testEncodeSQL(t, encodeSQLBytes)
+}
+
+func TestEncodeSQLString(t *testing.T) {
+	testEncodeSQL(t, encodeSQLString)
+}
+
+func testEncodeSQL(t *testing.T, encode func(*bytes.Buffer, string)) {
 	type entry struct{ i, j int }
 	seen := make(map[string]entry)
 	for i := 0; i < 256; i++ {
 		for j := 0; j < 256; j++ {
 			s := string([]byte{byte(i), byte(j)})
 			var buf bytes.Buffer
-			encodeSQLBytes(&buf, s)
+			encode(&buf, s)
 			sql := fmt.Sprintf("SELECT %s", buf.String())
 			for n := 0; n < len(sql); n++ {
 				ch := sql[n]
@@ -1147,8 +1155,7 @@ func TestEncodeSQLBytes(t *testing.T) {
 			}
 			stmts, err := parseTraditional(sql)
 			if err != nil {
-				t.Errorf("%s: expected success, but found %s", sql, err)
-				continue
+				t.Fatalf("%s: expected success, but found %s", sql, err)
 			}
 			stmt := stmts.String()
 			if e, ok := seen[stmt]; ok {
@@ -1156,7 +1163,7 @@ func TestEncodeSQLBytes(t *testing.T) {
 			}
 			seen[stmt] = entry{i, j}
 			if sql != stmt {
-				t.Errorf("expected %s, but found %s", sql, stmt)
+				t.Fatalf("expected %s, but found %s", sql, stmt)
 			}
 		}
 	}
