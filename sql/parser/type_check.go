2c0c916c14f587233aecfe0b18680ac6dbc108bb
Raphael 'kena' Poss <knz@cockroachlabs.com>
2016-06-28 20:56:42 +0000

    sql: fix and enhance expression normalization.
    This patch is needed so that the expressions in the Sqllogictest do
    not cause the server to panic.
    
    Summary:
    
    - Bug fixes:
    
      - an invalid cast was attempted for some expressions; is now fixed.
      - NULL caused invalid transformations, NULL is now normalized on a
        fast path.
      - transformations that rebalance arithmetic across a comparison now
        first check that the proposed replacement arithmetic expression
        has a valid overload w.r.t typing.
    
    - Improvements: more simplifications.

--- sql/parser/type_check.go
+++ sql/parser/type_check.go
@@ -49,6 +49,23 @@ var (
 	TypeTuple Datum = &DTuple{}
 )
 
+// dividerDatum is used during normalization to determine which Datums
+// are possible valid second operands to an arithmetic divide. See
+// CanBeZeroDivider() for an example.
+// Ensure this is implemented appropriately if/when new numeric types
+// are added.
+type dividerDatum interface {
+	isDividerDatum()
+}
+
+var _ dividerDatum = NewDInt(0)
+var _ dividerDatum = NewDFloat(0)
+var _ dividerDatum = &DDecimal{}
+
+func (*DInt) isDividerDatum()     {}
+func (*DFloat) isDividerDatum()   {}
+func (*DDecimal) isDividerDatum() {}
+
 // SemaContext defines the context in which to perform semantic analysis on an
 // expression syntax tree.
 type SemaContext struct {
