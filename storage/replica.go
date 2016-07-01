24bcf1899593a384d29a09b58e58216b8ae0dd72
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2016-07-01 18:53:19 -0400

    storage: always write a HardState
    As discovered in
    https://github.com/cockroachdb/cockroach/issues/6991#issuecomment-230054238,
    it's possible that we apply a Raft snapshot without writing a corresponding
    HardState since we write the snapshot in its own batch first and only then
    write a HardState.
    
    If that happens, the server is going to panic on restart: It will have a
    nontrivial first index, but a committed index of zero (from the empty
    HardState).
    
    This change prevents us from applying a snapshot when there is no HardState
    supplied along with it, except when applying a preemptive snapshot (in which
    case we synthesize a HardState).

--- storage/replica.go
+++ storage/replica.go
@@ -1432,7 +1432,7 @@ func (r *Replica) handleRaftReady() error {
 
 	if !raft.IsEmptySnap(rd.Snapshot) {
 		var err error
-		lastIndex, err = r.applySnapshot(rd.Snapshot, normalSnapshot)
+		lastIndex, err = r.applySnapshot(rd.Snapshot, normalSnapshot, rd.HardState)
 		if err != nil {
 			return err
 		}
