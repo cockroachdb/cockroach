e973a7728bd42b80d11f57882823d0654bf6b778
Tamir Duberstein <tamird@gmail.com>
2016-07-03 18:30:18 -0400

    storage: log some visibility into the command queue

--- storage/replica.go
+++ storage/replica.go
@@ -906,6 +906,11 @@ func (r *Replica) beginCmds(ctx context.Context, ba *roachpb.BatchRequest) (func
 			select {
 			case <-ch:
 			case <-ctxDone:
+				err := ctx.Err()
+				errStr := fmt.Sprintf("%s while in command queue: %s", err, ba)
+				log.Warning(errStr)
+				log.Trace(ctx, errStr)
+				defer log.Trace(ctx, "removed from command queue")
 				// The command is moot, so we don't need to bother executing.
 				// However, the command queue assumes that commands don't drop
 				// out before their prerequisites, so we still have to wait it
@@ -920,7 +925,7 @@ func (r *Replica) beginCmds(ctx context.Context, ba *roachpb.BatchRequest) (func
 				r.mu.Lock()
 				r.mu.cmdQ.remove(cmd)
 				r.mu.Unlock()
-				return nil, ctx.Err()
+				return nil, err
 			}
 		}
 	}
