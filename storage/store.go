13dbe56154f412718f720f4d2aa01d86cb7c18ee
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2016-07-08 15:01:53 -0400

    storage: unit test migrations

--- storage/store.go
+++ storage/store.go
@@ -866,54 +866,13 @@ func IterateRangeDescriptors(
 	return err
 }
 
-// MIGRATION(tschottdorf): As of #7310, we make sure that a Replica always has
-// a complete Raft state on disk. Prior versions may not have that, which
-// causes issues due to the fact that we used to synthesize a TruncatedState
-// and do so no more. To make up for that, write a missing TruncatedState here.
-// That key is in the replicated state, but since during a cluster upgrade, all
-// nodes do it, it's fine (and we never CPut on that key, so anything in the
-// Raft pipeline will simply overwrite it).
-//
-// Migration(tschottdorf): See #6991. It's possible that the HardState is
-// missing after a snapshot was applied (so there is a TruncatedState). In this
-// case, synthesize a HardState (simply setting everything that was in the
-// snapshot to committed). Having lost the original HardState can theoretically
-// mean that the replica was further ahead or had voted, and so there's no
-// guarantee that this will be correct. But it will be correct in the majority
-// of cases, and some state *has* to be recovered.
-func (s *Store) migrate7310And6991(desc roachpb.RangeDescriptor) {
-	if !desc.IsInitialized() {
-		log.Fatalf("found uninitialized descriptor on range: %+v", desc)
-	}
+func (s *Store) migrate(desc roachpb.RangeDescriptor) {
 	batch := s.engine.NewBatch()
-	state, err := loadState(batch, &desc)
-	if err != nil {
-		log.Fatal(errors.Wrap(err, "could not migrate TruncatedState: %s"))
+	if err := migrate7310And6991(batch, desc); err != nil {
+		log.Fatal(errors.Wrap(err, "committing migration"))
 	}
-	if (*state.TruncatedState == roachpb.RaftTruncatedState{}) {
-		state.TruncatedState.Term = raftInitialLogTerm
-		state.TruncatedState.Index = raftInitialLogIndex
-		if _, err := saveState(batch, state); err != nil {
-			log.Fatal(errors.Wrapf(err, "could not migrate TruncatedState to %+v", &state.TruncatedState))
-		}
-		log.Warningf("migration: synthesized TruncatedState for %+v", desc)
-	}
-
-	hs, err := loadHardState(batch, desc.RangeID)
-	if err != nil {
-		log.Fatal(errors.Wrap(err, "unable to load HardState"))
-	}
-	// Only update the HardState when there is a nontrivial Commit field. We
-	// don't have a snapshot here, so we could wind up lowering the commit
-	// index (which would error out and fatal us).
-	if hs.Commit == 0 {
-		if err := synthesizeHardState(batch, state); err != nil {
-			log.Fatal(errors.Wrap(err, "could not migrate HardState"))
-		}
-	}
-
 	if err := batch.Commit(); err != nil {
-		log.Fatalf("could not migrate Raft state: %s", err)
+		log.Fatal(errors.Wrap(err, "committing migrations"))
 	}
 }
 
@@ -1003,8 +962,10 @@ func (s *Store) Start(stopper *stop.Stopper) error {
 			// (which is necessary to have a non-nil raft group)
 			return false, s.destroyReplicaData(&desc)
 		}
-
-		s.migrate7310And6991(desc)
+		if !desc.IsInitialized() {
+			return false, errors.Errorf("found uninitialized RangeDescriptor: %+v", desc)
+		}
+		s.migrate(desc)
 
 		rng, err := NewReplica(&desc, s, 0)
 		if err != nil {
