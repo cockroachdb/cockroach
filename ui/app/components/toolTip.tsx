ad14b76707318549ef70d17ff447af6ea57aa814
Max Lang <max@cockroachlabs.com>
2016-07-12 03:30:09 -0400

    Add warning for low number of nodes. Includes tooltip warning design.
    Fixes #5978

--- ui/app/components/toolTip.tsx
+++ ui/app/components/toolTip.tsx
@@ -4,6 +4,8 @@ import classNames = require("classnames");
 interface ToolTipProps {
   text: React.ReactNode;
   title: string;
+  warningTitle?: string;
+  warning?: React.ReactNode;
   position?: "right" | "left";
 }
 
@@ -13,10 +15,17 @@ export class ToolTip extends React.Component<ToolTipProps, {}> {
   };
 
   render() {
-    let { position, title, text } = this.props;
+    let { position, title, text, warning, warningTitle } = this.props;
     return <div className={ classNames("tooltip", "viz-tooltip", position) }>
       <div className="title">{ title }</div>
       <div className="content">{ text }</div>
+      {warning ?
+        <div className="warning">
+          <div className="icon-warning" />
+          <div className="warning-title">{ warningTitle }</div>
+          <div className="warningContent">{ warning }</div>
+        </div>
+        : null }
     </div>;
   }
 }
