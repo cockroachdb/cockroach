95d559095fe378d8c2f78a4c7e41664584590ef1
Arjun Narayan <arjun@cockroachlabs.com>
2016-07-18 17:35:04 -0400

    storage: move rangeDesc out of replica.mu, use in Desc()
    In #7871, replica.String() was refactored to use a new atomically
    readable copy of the RangeDescriptor. replica.Desc() could use this as
    well (so as to not lock replica.mu), but this change was
    conservatively deferred. However, with the issue of replica.Desc()
    acquiring locks being raised in #7888, and after discussion with
    @bdarnell, it should be safe to refactor replica.Desc() as well.
    
    Finally replica.mu.rangeDesc is moved outside mu to replica.rangeDesc
    to maintain the semantics of replica.mu

--- storage/replica.go
+++ storage/replica.go
@@ -197,6 +197,12 @@ type Replica struct {
 	// RWMutex.
 	readOnlyCmdMu sync.RWMutex
 
+	// rangeDesc is a *RangeDescriptor that can be atomically read from in
+	// replica.Desc() without needing to acquire the replica.mu lock. All
+	// updates to state.Desc should be duplicated here (as is done in
+	// updateRangeDescriptorLocked())
+	rangeDesc atomic.Value
+
 	mu struct {
 		// Protects all fields in the mu struct.
 		sync.Mutex
@@ -204,11 +210,6 @@ type Replica struct {
 		destroyed error
 		// The state of the Raft state machine.
 		state storagebase.ReplicaState
-		// rangeDesc is a *RangeDescriptor that can be atomically read from. All
-		// updates to state.Desc should be duplicated here (as is done in
-		// updateRangeDescriptorLocked()) so that it can be read without acquiring
-		// the lock.
-		rangeDesc atomic.Value
 		// Counter used for assigning lease indexes for proposals.
 		lastAssignedLeaseIndex uint64
 		// Enforces at most one command is running per key(s).
@@ -363,7 +364,7 @@ func (r *Replica) newReplicaInner(desc *roachpb.RangeDescriptor, clock *hlc.Cloc
 	if r.mu.state, err = loadState(r.store.Engine(), desc); err != nil {
 		return err
 	}
-	r.mu.rangeDesc.Store(r.mu.state.Desc)
+	r.rangeDesc.Store(r.mu.state.Desc)
 
 	r.mu.lastIndex, err = loadLastIndex(r.store.Engine(), r.RangeID)
 	if err != nil {
@@ -392,7 +393,7 @@ func (r *Replica) newReplicaInner(desc *roachpb.RangeDescriptor, clock *hlc.Cloc
 
 // String returns a string representation of the range.
 func (r *Replica) String() string {
-	desc := r.mu.rangeDesc.Load().(*roachpb.RangeDescriptor)
+	desc := r.Desc()
 	return fmt.Sprintf("%s range=%d [%s-%s)", r.store,
 		desc.RangeID, desc.StartKey, desc.EndKey)
 }
@@ -631,9 +632,7 @@ func (r *Replica) isInitializedLocked() bool {
 
 // Desc returns the range's descriptor.
 func (r *Replica) Desc() *roachpb.RangeDescriptor {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	return r.mu.state.Desc
+	return r.rangeDesc.Load().(*roachpb.RangeDescriptor)
 }
 
 // setDesc atomically sets the range's descriptor. This method calls
@@ -666,7 +665,7 @@ func (r *Replica) setDescWithoutProcessUpdateLocked(desc *roachpb.RangeDescripto
 	}
 
 	r.mu.state.Desc = desc
-	r.mu.rangeDesc.Store(desc)
+	r.rangeDesc.Store(desc)
 }
 
 // GetReplicaDescriptor returns the replica for this range from the range
