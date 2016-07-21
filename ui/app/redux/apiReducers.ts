d7d2ad45c2cf5262e031286ace1b3a6304c058ce
Max Lang <max@cockroachlabs.com>
2016-07-21 17:17:05 -0400

    ui: create a cached data reducer that operates on data with ids

--- ui/app/redux/apiReducers.ts
+++ ui/app/redux/apiReducers.ts
@@ -1,6 +1,6 @@
 import _ = require("lodash");
 import { combineReducers } from "redux";
-import { CachedDataReducer, CachedDataReducerState } from "./cachedDataReducer";
+import { CachedDataReducer, CachedDataReducerState, KeyedCachedDataReducerState } from "./cachedDataReducer";
 import * as api from "../util/api";
 import { VersionList } from "../interfaces/cockroachlabs";
 import { versionCheck, VersionCheckRequest } from "../util/cockroachlabsAPI";
@@ -12,6 +12,11 @@ type EventsResponseMessage = cockroach.server.serverpb.EventsResponseMessage;
 type HealthResponseMessage = cockroach.server.serverpb.HealthResponseMessage;
 type RaftDebugResponseMessage = cockroach.server.serverpb.RaftDebugResponseMessage;
 type NodesResponseMessage = cockroach.server.serverpb.NodesResponseMessage;
+type DatabasesResponseMessage = cockroach.server.serverpb.DatabasesResponseMessage;
+type DatabaseDetailsRequest = cockroach.server.serverpb.DatabaseDetailsRequest;
+type DatabaseDetailsResponseMessage = cockroach.server.serverpb.DatabaseDetailsResponseMessage;
+type TableDetailsRequest= cockroach.server.serverpb.TableDetailsRequest;
+type TableDetailsResponseMessage = cockroach.server.serverpb.TableDetailsResponseMessage;
 
 const TEN_SECONDS = 10 * 1000;
 const TWO_SECONDS = 2 * 1000;
@@ -42,6 +47,25 @@ export let refreshRaft = raftReducerObj.refresh;
 export let versionReducerObj = new CachedDataReducer<VersionCheckRequest, VersionList>(versionCheck, "version");
 export let refreshVersion = versionReducerObj.refresh;
 
+export let databasesReducerObj = new CachedDataReducer<void, DatabasesResponseMessage>(api.getDatabaseList, "databases");
+export let refreshDatabases = databasesReducerObj.refresh;
+
+export let databaseRequestToID = (req: DatabaseDetailsRequest): string => req.database;
+
+export let databaseDetailsReducerObj = new CachedDataReducer<DatabaseDetailsRequest, DatabaseDetailsResponseMessage>(api.getDatabaseDetails, "databaseDetails", databaseRequestToID);
+export let refreshDatabaseDetails = databaseDetailsReducerObj.refresh;
+
+// NOTE: We encode the db and table name so we can combine them as a string.
+// TODO(maxlang): there's probably a nicer way to do this
+export function generateTableID(db: string, table: string) {
+  return `${encodeURIComponent(db)}/${encodeURIComponent(table)}`;
+}
+
+export let tableRequestToID = (req: TableDetailsRequest): string => generateTableID(req.database, req.table);
+
+export let tableDetailsReducerObj = new CachedDataReducer<TableDetailsRequest, TableDetailsResponseMessage>(api.getTableDetails, "tableDetails", tableRequestToID);
+export let refreshTableDetails = tableDetailsReducerObj.refresh;
+
 export interface APIReducersState {
   cluster: CachedDataReducerState<ClusterResponseMessage>;
   events: CachedDataReducerState<EventsResponseMessage>;
@@ -49,6 +73,9 @@ export interface APIReducersState {
   nodes: CachedDataReducerState<NodeStatus[]>;
   raft: CachedDataReducerState<RaftDebugResponseMessage>;
   version: CachedDataReducerState<VersionList>;
+  databases: CachedDataReducerState<DatabasesResponseMessage>;
+  databaseDetails: KeyedCachedDataReducerState<DatabaseDetailsResponseMessage>;
+  tableDetails: KeyedCachedDataReducerState<TableDetailsResponseMessage>;
 }
 
 export default combineReducers<APIReducersState>({
@@ -58,4 +85,7 @@ export default combineReducers<APIReducersState>({
   [nodesReducerObj.actionNamespace]: nodesReducerObj.reducer,
   [raftReducerObj.actionNamespace]: raftReducerObj.reducer,
   [versionReducerObj.actionNamespace]: versionReducerObj.reducer,
+  [databasesReducerObj.actionNamespace]: databasesReducerObj.reducer,
+  [databaseDetailsReducerObj.actionNamespace]: databaseDetailsReducerObj.keyedReducer,
+  [tableDetailsReducerObj.actionNamespace]: tableDetailsReducerObj.keyedReducer,
 });
