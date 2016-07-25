1b3c644d9146849fa35bed17727c42145d1eef23
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2016-07-25 05:37:48 -0400

    engine: avoid early free of cache
    cc @petermattis

--- storage/engine/in_mem.go
+++ storage/engine/in_mem.go
@@ -29,7 +29,7 @@ type InMem struct {
 // NewInMem allocates and returns a new, opened InMem engine.
 func NewInMem(attrs roachpb.Attributes, cacheSize int64, stopper *stop.Stopper) InMem {
 	cache := NewRocksDBCache(cacheSize)
-	defer cache.Release()
+	stopper.AddCloser(stop.CloserFn(cache.Release))
 	db := InMem{
 		RocksDB: newMemRocksDB(attrs, cache, 512<<20 /* 512 MB */, stopper),
 	}
