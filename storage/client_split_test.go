dba53026c71e3e6f7cadd90057c07777b14e6caa
Peter Mattis <petermattis@gmail.com>
2016-07-26 17:03:59 -0400

    storage: improve conflicting split error messsage
    Fixes #5629

--- storage/client_split_test.go
+++ storage/client_split_test.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 	"math/rand"
 	"reflect"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"testing"
@@ -279,21 +280,31 @@ func TestStoreRangeSplitConcurrent(t *testing.T) {
 	defer stopper.Stop()
 
 	splitKey := roachpb.Key("a")
-	concurrentCount := int32(10)
-	wg := sync.WaitGroup{}
-	wg.Add(int(concurrentCount))
-	failureCount := int32(0)
-	for i := int32(0); i < concurrentCount; i++ {
+	concurrentCount := 10
+	errCh := make(chan *roachpb.Error, concurrentCount)
+	for i := 0; i < concurrentCount; i++ {
 		go func() {
 			args := adminSplitArgs(roachpb.KeyMin, splitKey)
 			_, pErr := client.SendWrapped(rg1(store), nil, &args)
-			if pErr != nil {
-				atomic.AddInt32(&failureCount, 1)
-			}
-			wg.Done()
+			errCh <- pErr
 		}()
 	}
-	wg.Wait()
+
+	var failureCount int
+	for i := 0; i < concurrentCount; i++ {
+		pErr := <-errCh
+		if pErr != nil {
+			// There are only two expected errors from concurrent splits: conflicting
+			// range descriptors if the splits are initiated concurrently or that the
+			// range is already split at the specified key.
+			s := pErr.String()
+			if strings.Index(s, "conflict updating range descriptors") == -1 &&
+				strings.Index(s, "range is already split at key") == -1 {
+				t.Fatalf("unexpected error: %v", pErr)
+			}
+			failureCount++
+		}
+	}
 	if failureCount != concurrentCount-1 {
 		t.Fatalf("concurrent splits succeeded unexpectedly; failureCount=%d", failureCount)
 	}
