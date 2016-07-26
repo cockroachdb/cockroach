cbf4e98102bb004db86f439643262f37f4925c49
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2016-07-26 05:33:30 -0400

    storage: hoist split side effects
    Previously, a committed split would have the Store process that split
    via a `batch.Defer`. As part of #6286, this `batch.Defer` was removed
    and explicit metainformation is now passed up the callchain up to
    `(*Replica).processRaftCommand`, where it is translated to calls to
    the Store.
    
    A similar mechanism was previously used for passing up skipped intents,
    and that mechanism has been folded into this new one (with more side
    effects to be extracted in future work).

---
+++ storage/replica_trigger.go
@@ -0,0 +1,65 @@
+// Copyright 2016 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// Author: Tobias Schottdorf (tobias.schottdorf@gmail.com)
+
+package storage
+
+import (
+	"github.com/cockroachdb/cockroach/roachpb"
+	"github.com/cockroachdb/cockroach/storage/engine/enginepb"
+)
+
+// postCommitSplit is emitted when a Replica commits a split trigger and
+// signals that the Replica has prepared the on-disk state for both the left
+// and right hand sides of the split, and that the left hand side Replica
+// should be updated as well as the right hand side created.
+type postCommitSplit struct {
+	roachpb.SplitTrigger
+	enginepb.MVCCStats
+}
+
+// PostCommitTrigger is returned from Raft processing as a side effect which
+// signals that further action should be taken as part of the processing of the
+// Raft command.
+// Depending on the content, actions may be executed on all Replicas, the lease
+// holder, or a Replica determined by other conditions present in the specific
+// trigger.
+type PostCommitTrigger struct {
+	// intents stores any intents encountered but not conflicted with. They
+	// should be handed off to asynchronous intent processing so that an
+	// attempt to resolve them is made.
+	intents []intentsWithArg
+	// split contains a postCommitSplit trigger emitted on a split.
+	split *postCommitSplit
+}
+
+// updateTrigger takes a previous and new commit trigger and combines their
+// contents into an updated trigger, consuming both inputs. It will panic on
+// illegal combinations (such as being asked to combine two split triggers).
+func updateTrigger(old, new *PostCommitTrigger) *PostCommitTrigger {
+	if old == nil {
+		old = new
+	} else {
+		if new.intents != nil {
+			old.intents = append(old.intents, new.intents...)
+		}
+		if old.split == nil {
+			old.split = new.split
+		} else if new.split != nil {
+			panic("more than one split trigger")
+		}
+	}
+	return old
+}
