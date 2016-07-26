6e768dcb7c1ec2224ed8b1756540c4dae1e550f0
Peter Mattis <petermattis@gmail.com>
2016-07-26 09:57:33 -0400

    all: add AdminTransferLease
    Allow external use of RangeLookup requests.

--- roachpb/method_string.go
+++ roachpb/method_string.go
@@ -4,9 +4,9 @@ package roachpb
 
 import "fmt"
 
-const _Method_name = "GetPutConditionalPutIncrementDeleteDeleteRangeScanReverseScanBeginTransactionEndTransactionAdminSplitAdminMergeHeartbeatTxnGCPushTxnRangeLookupResolveIntentResolveIntentRangeNoopMergeTruncateLogRequestLeaseTransferLeaseComputeChecksumVerifyChecksumCheckConsistencyInitPutChangeFrozen"
+const _Method_name = "GetPutConditionalPutIncrementDeleteDeleteRangeScanReverseScanBeginTransactionEndTransactionAdminSplitAdminMergeAdminTransferLeaseHeartbeatTxnGCPushTxnRangeLookupResolveIntentResolveIntentRangeNoopMergeTruncateLogRequestLeaseTransferLeaseComputeChecksumVerifyChecksumCheckConsistencyInitPutChangeFrozen"
 
-var _Method_index = [...]uint16{0, 3, 6, 20, 29, 35, 46, 50, 61, 77, 91, 101, 111, 123, 125, 132, 143, 156, 174, 178, 183, 194, 206, 219, 234, 248, 264, 271, 283}
+var _Method_index = [...]uint16{0, 3, 6, 20, 29, 35, 46, 50, 61, 77, 91, 101, 111, 129, 141, 143, 150, 161, 174, 192, 196, 201, 212, 224, 237, 252, 266, 282, 289, 301}
 
 func (i Method) String() string {
 	if i < 0 || i >= Method(len(_Method_index)-1) {
