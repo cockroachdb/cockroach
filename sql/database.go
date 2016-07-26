0c5d5a75c87c13f4aa5fa61c0454291982ccc8f1
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2016-07-26 11:16:50 -0400

    sql: add renameDatabase method to DatabaseAccessor interface
    This pulls logic about the renaming of databases into the
    DatabaseAccessor interface.

--- sql/database.go
+++ sql/database.go
@@ -21,6 +21,8 @@ import (
 
 	"golang.org/x/net/context"
 
+	"github.com/cockroachdb/cockroach/config"
+	"github.com/cockroachdb/cockroach/internal/client"
 	"github.com/cockroachdb/cockroach/keys"
 	"github.com/cockroachdb/cockroach/roachpb"
 	"github.com/cockroachdb/cockroach/sql/parser"
@@ -114,6 +116,11 @@ type DatabaseAccessor interface {
 	// or an error if one was encountered. The ifNotExists flag is used to declare
 	// if the "already existed" state should be an error (false) or a no-op (true).
 	createDatabase(desc *sqlbase.DatabaseDescriptor, ifNotExists bool) (bool, error)
+
+	// renameDatabase attempts to rename the database with the provided DatabaseDescriptor
+	// to a new name. The method will mutate the provided DatabaseDescriptor, updating its
+	// name with the new name.
+	renameDatabase(oldDesc *sqlbase.DatabaseDescriptor, newName string) error
 }
 
 var _ DatabaseAccessor = &planner{}
@@ -204,4 +211,42 @@ func (p *planner) getDatabaseID(name string) (sqlbase.ID, error) {
 // createDatabase implements the DatabaseAccessor interface.
 func (p *planner) createDatabase(desc *sqlbase.DatabaseDescriptor, ifNotExists bool) (bool, error) {
 	return p.createDescriptor(databaseKey{desc.Name}, desc, ifNotExists)
+}
+
+// renameDatabase implements the DatabaseAccessor interface.
+func (p *planner) renameDatabase(oldDesc *sqlbase.DatabaseDescriptor, newName string) error {
+	oldName := oldDesc.Name
+	oldDesc.SetName(newName)
+	if err := oldDesc.Validate(); err != nil {
+		return err
+	}
+
+	oldKey := databaseKey{oldName}.Key()
+	newKey := databaseKey{newName}.Key()
+	descID := oldDesc.GetID()
+	descKey := sqlbase.MakeDescMetadataKey(descID)
+	descDesc := sqlbase.WrapDescriptor(oldDesc)
+
+	b := client.Batch{}
+	b.CPut(newKey, descID, nil)
+	b.Put(descKey, descDesc)
+	b.Del(oldKey)
+
+	if err := p.txn.Run(&b); err != nil {
+		if _, ok := err.(*roachpb.ConditionFailedError); ok {
+			return fmt.Errorf("the new database name %q already exists", newName)
+		}
+		return err
+	}
+
+	p.setTestingVerifyMetadata(func(systemConfig config.SystemConfig) error {
+		if err := expectDescriptorID(systemConfig, newKey, descID); err != nil {
+			return err
+		}
+		if err := expectDescriptor(systemConfig, descKey, descDesc); err != nil {
+			return err
+		}
+		return expectDeleted(systemConfig, oldKey)
+	})
+	return nil
 }
