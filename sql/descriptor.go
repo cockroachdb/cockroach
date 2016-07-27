80f21d85ba08568887ea5e95b90926880662acaa
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2016-07-27 17:11:15 -0400

    sql: Introduce virtualSchema infrastructure
    This first commit creates the skeleton of the `virtualSchema`
    infrastructure. To test this out, it adds the initial
    "information_schema" database, with no tables.
    
    Note that this change will result in previous databases called
    `information_schema` being inaccessible. This is because the new
    virtual database will shadow it.

--- sql/descriptor.go
+++ sql/descriptor.go
@@ -73,6 +73,15 @@ func (p *planner) checkPrivilege(descriptor sqlbase.DescriptorProto, privilege p
 		p.session.User, privilege, descriptor.TypeName(), descriptor.GetName())
 }
 
+type descriptorAlreadyExistsErr struct {
+	desc sqlbase.DescriptorProto
+	name string
+}
+
+func (d descriptorAlreadyExistsErr) Error() string {
+	return fmt.Sprintf("%s %q already exists", d.desc.TypeName(), d.name)
+}
+
 // createDescriptor implements the DescriptorAccessor interface.
 func (p *planner) createDescriptor(plainKey sqlbase.DescriptorKey, descriptor sqlbase.DescriptorProto, ifNotExists bool) (bool, error) {
 	idKey := plainKey.Key()
@@ -88,7 +97,7 @@ func (p *planner) createDescriptor(plainKey sqlbase.DescriptorKey, descriptor sq
 			return false, nil
 		}
 		// Key exists, but we don't want it to: error out.
-		return false, fmt.Errorf("%s %q already exists", descriptor.TypeName(), plainKey.Name())
+		return false, descriptorAlreadyExistsErr{descriptor, plainKey.Name()}
 	}
 
 	// Increment unique descriptor counter.
