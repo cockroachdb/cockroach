7a8729a3c503d9c163319f4b532cc8abe9f4abc6
Peter Mattis <petermattis@gmail.com>
2016-07-31 16:35:56 -0400

    rpc: mark heartbeat RPCs as fail-fast
    We want heartbeat RPCs to be fail-fast so that we get notified of
    transport failures and close the connection. Failure to do this left the
    gRPC client connections permanently open and trying to reconnect to down
    nodes, fouling up the circuit breaker expectations in Raft transport.
    
    Changed client.NewSender to create a separate gRPC connection which will
    not be heartbeat by the rpc.Context. We don't want the heartbeat service
    and we don't want these connections closed.
    
    Fixes #8130

--- internal/client/rpc_sender.go
+++ internal/client/rpc_sender.go
@@ -22,6 +22,7 @@ import (
 
 	"github.com/cockroachdb/cockroach/roachpb"
 	"github.com/cockroachdb/cockroach/rpc"
+	"github.com/cockroachdb/cockroach/util/stop"
 	"github.com/pkg/errors"
 )
 
@@ -33,10 +34,20 @@ type sender struct {
 // database provided by a Cockroach cluster by connecting via RPC to a
 // Cockroach node.
 func NewSender(ctx *rpc.Context, target string) (Sender, error) {
-	conn, err := ctx.GRPCDial(target)
+	// We don't use ctx.GRPCDial because this is an external client connection
+	// and we don't want to run the heartbeat service which will close the
+	// connection if the transport fails.
+	dialOpt, err := ctx.GRPCDialOption()
 	if err != nil {
 		return nil, err
 	}
+	conn, err := grpc.Dial(target, dialOpt)
+	if err != nil {
+		return nil, err
+	}
+	ctx.Stopper.AddCloser(stop.CloserFn(func() {
+		_ = conn.Close() // we're closing, ignore the error
+	}))
 	return sender{roachpb.NewExternalClient(conn)}, nil
 }
 
