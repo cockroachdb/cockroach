98d81026989feb17bf06643c8c3fcde0a237e3d8
Tamir Duberstein <tamird@gmail.com>
2016-08-01 12:29:04 -0400

    build: include the sources when tar'ing up binaries
    Note that we're still not tar'ing up the git repo itself. This should
    grant us most of the space savings we were previously enjoying.
    
    Fixes #8167.

--- build/build-static-binaries.sh
+++ build/build-static-binaries.sh
@@ -5,25 +5,17 @@ set -euo pipefail
 
 source $(dirname $0)/build-common.sh
 
-test_build_dir=$(mktemp -d test-binaries.XXXX)
 time make STATIC=1 build
-# sql/sql.test is built standalone as well to simplify the nightly logictest runs.
-time make STATIC=1 testbuild PKG=./sql
-time make STATIC=1 testbuild PKG=./acceptance TAGS=acceptance
-time make STATIC=1 testbuildall DIR=${test_build_dir}
+time make STATIC=1 testbuild PKG=./... TAGS=acceptance
 
 # We don't check all test binaries, but one from each invocation.
 check_static cockroach
-check_static sql/sql.test
-check_static acceptance/acceptance.test
-check_static ${test_build_dir}/github.com/cockroachdb/cockroach/sql/sql.test
+check_static cli/cli.test
 
+# Strip the binary and all the tests.
 strip -S cockroach
-strip -S sql/sql.test
-strip -S acceptance/acceptance.test
+find . -name '*.test' | xargs strip -S
 
+cd ..
 rm -f static-tests.tar.gz
-# Skip the project/repo part of the path inside the tarball.
-# Even for stripped binaries, gzip results in 167MB (21s spent) vs 512MB (3s spent).
-# It makes a big difference when fetching from outside AWS.
-time tar cfz static-tests.tar.gz -C ${test_build_dir}/github.com/cockroachdb/ cockroach/
+time tar cfz static-tests.tar.gz $(find cockroach -name '*.test') $(git -C cockroach ls-files)
