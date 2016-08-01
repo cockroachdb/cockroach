58320ad5e3103ef110604fbe028b0a3c3648d2b2
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2016-08-01 04:17:24 -0400

    storage: move trigger handling to separate file

--- storage/replica_trigger.go
+++ storage/replica_trigger.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cockroachdb/cockroach/util/log"
 	"github.com/cockroachdb/cockroach/util/timeutil"
 	"github.com/cockroachdb/cockroach/util/uuid"
+	"github.com/coreos/etcd/raft"
 	"github.com/pkg/errors"
 	"golang.org/x/net/context"
 )
@@ -275,4 +276,179 @@ func (r *Replica) verifyChecksumTrigger(
 
 		logFunc(ctx, "consistency check failed on replica: %s, checksum mismatch: e = %x, v = %x", r, args.Checksum, c.checksum)
 	}
+}
+
+func (r *Replica) handleTrigger(
+	ctx context.Context,
+	originReplica roachpb.ReplicaDescriptor,
+	trigger PostCommitTrigger,
+) {
+	if trigger.split != nil {
+		splitTriggerPostCommit(
+			context.Background(),
+			trigger.split.RightDeltaMS,
+			&trigger.split.SplitTrigger,
+			r,
+		)
+	}
+	if trigger.merge != nil {
+		r.mu.Lock()
+		r.mu.tsCache.Clear(r.store.Clock())
+		r.mu.Unlock()
+
+		if err := r.store.MergeRange(r, trigger.merge.LeftDesc.EndKey,
+			trigger.merge.RightDesc.RangeID,
+		); err != nil {
+			// Our in-memory state has diverged from the on-disk state.
+			log.Fatalf(ctx, "%s: failed to update store after merging range: %s", r, err)
+		}
+	}
+
+	if trigger.gcThreshold != nil {
+		r.mu.Lock()
+		r.mu.state.GCThreshold = *trigger.gcThreshold
+		r.mu.Unlock()
+	}
+	if trigger.truncatedState != nil {
+		r.mu.Lock()
+		r.mu.state.TruncatedState = trigger.truncatedState
+		r.mu.Unlock()
+	}
+	if trigger.raftLogSize != nil {
+		r.mu.Lock()
+		r.mu.raftLogSize = *trigger.raftLogSize
+		r.mu.Unlock()
+	}
+	if trigger.frozen != nil {
+		r.mu.Lock()
+		r.mu.state.Frozen = *trigger.frozen
+		r.mu.Unlock()
+	}
+
+	if trigger.desc != nil {
+		if err := r.setDesc(trigger.desc); err != nil {
+			// Log the error. There's not much we can do because the commit may have already occurred at this point.
+			log.Fatalf(ctx, "%s: failed to update range descriptor to %+v: %s",
+				r, trigger.desc, err)
+		}
+	}
+	if trigger.lease != nil {
+		r.mu.Lock()
+		prevLease := r.mu.state.Lease
+		r.mu.state.Lease = trigger.lease
+		replicaID := r.mu.replicaID
+		r.mu.Unlock()
+
+		if prevLease.Replica.StoreID != trigger.lease.Replica.StoreID {
+			// The lease is changing hands. Is this replica the new lease holder?
+			if trigger.lease.Replica.ReplicaID == replicaID {
+				// If this replica is a new holder of the lease, update the low water
+				// mark of the timestamp cache. Note that clock offset scenarios are
+				// handled via a stasis period inherent in the lease which is documented
+				// in on the Lease struct.
+				//
+				// The introduction of lease transfers implies that the previous lease
+				// may have been shortened and we are now applying a formally overlapping
+				// lease (since the old lease holder has promised not to serve any more
+				// requests, this is kosher). This means that we don't use the old
+				// lease's expiration but instead use the new lease's start to initialize
+				// the timestamp cache low water.
+				log.Infof(ctx, "%s: new range lease %s following %s [physicalTime=%s]",
+					r, trigger.lease, prevLease, r.store.Clock().PhysicalTime())
+				r.mu.tsCache.SetLowWater(trigger.lease.Start)
+
+				// Gossip the first range whenever its lease is acquired. We check to
+				// make sure the lease is active so that a trailing replica won't process
+				// an old lease request and attempt to gossip the first range.
+				if r.IsFirstRange() && trigger.lease.Covers(r.store.Clock().Now()) {
+					func() {
+						r.mu.Lock()
+						defer r.mu.Unlock()
+						r.gossipFirstRangeLocked(ctx)
+					}()
+				}
+			} else if trigger.lease.Covers(r.store.Clock().Now()) {
+				if err := r.withRaftGroup(func(raftGroup *raft.RawNode) error {
+					if raftGroup.Status().RaftState == raft.StateLeader {
+						// If this replica is the raft leader but it is not the new lease
+						// holder, then try to transfer the raft leadership to match the
+						// lease.
+						log.Infof(ctx, "range %v: replicaID %v transfer raft leadership to replicaID %v",
+							r.RangeID, replicaID, trigger.lease.Replica.ReplicaID)
+						raftGroup.TransferLeader(uint64(trigger.lease.Replica.ReplicaID))
+					}
+					return nil
+				}); err != nil {
+					// An error here indicates that this Replica has been destroyed
+					// while lacking the necessary synchronization (or even worse, it
+					// fails spuriously - could be a storage error), and so we avoid
+					// sweeping that under the rug.
+					//
+					// TODO(tschottdorf): this error is not handled any more
+					// at this level.
+					log.Fatal(context.Background(), newReplicaCorruptionError(err))
+				}
+			}
+		}
+	}
+
+	if trigger.gossipFirstRange {
+		// We need to run the gossip in an async task because gossiping requires
+		// the range lease and we'll deadlock if we try to acquire it while
+		// holding processRaftMu. Specifically, Replica.redirectOnOrAcquireLease
+		// blocks waiting for the lease acquisition to finish but it can't finish
+		// because we're not processing raft messages due to holding
+		// processRaftMu (and running on the processRaft goroutine).
+		if err := r.store.Stopper().RunAsyncTask(func() {
+			// Create a new context because this is an asynchronous task and we
+			// don't want to share the trace.
+			ctxInner := context.Background()
+			if hasLease, pErr := r.getLeaseForGossip(ctxInner); hasLease {
+				r.mu.Lock()
+				defer r.mu.Unlock()
+				r.gossipFirstRangeLocked(ctxInner)
+			} else {
+				log.Infof(ctxInner, "unable to gossip first range; hasLease=%t, err=%v", hasLease, pErr)
+			}
+		}); err != nil {
+			log.Errorf(ctx, "unable to gossip first range: %+v", err)
+		}
+	}
+
+	if trigger.addToReplicaGCQueue {
+		if err := r.store.replicaGCQueue.Add(r, 1.0); err != nil {
+			// Log the error; the range should still be GC'd eventually.
+			log.Errorf(ctx, "%s: unable to add to GC queue: %s", r, err)
+		}
+	}
+
+	if trigger.maybeAddToSplitQueue {
+		r.store.splitQueue.MaybeAdd(r, r.store.Clock().Now())
+	}
+
+	if trigger.maybeGossipSystemConfig {
+		r.maybeGossipSystemConfig()
+	}
+
+	// On the replica on which this command originated, resolve skipped intents
+	// asynchronously - even on failure.
+	//
+	// TODO(tschottdorf): EndTransaction will use this pathway to return
+	// intents which should immediately be resolved. However, there's
+	// a slight chance that an error between the origin of that intents
+	// slice and here still results in that intent slice arriving here
+	// without the EndTransaction having committed. We should clearly
+	// separate the part of the trigger which also applies on errors.
+	if originReplica.StoreID == r.store.StoreID() {
+		r.store.intentResolver.processIntentsAsync(r, trigger.intents)
+	}
+
+	if trigger.computeChecksum != nil {
+		r.computeChecksumTrigger(context.TODO(), *trigger.computeChecksum)
+	}
+
+	if trigger.verifyChecksum != nil {
+		r.verifyChecksumTrigger(context.TODO(), *trigger.verifyChecksum)
+	}
+
 }
