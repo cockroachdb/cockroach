e2056e5fbe00659c624a09c3870da988466660e2
Radu Berinde <radu.berinde@gmail.com>
2016-08-02 11:14:17 -0400

    sql: plumb contexts to logs
    Fixes context for log callsites in `sql/*.go`, with the exception of schema
    changer and leases.
    
    We can now pass a "base context" to NewSession. Any Executor calls use the
    session's context; removing ctx arguments in the interface, and updating
    admin.go accordingly.
    
    Part of #7992.

--- sql/pgwire/v3.go
+++ sql/pgwire/v3.go
@@ -124,8 +124,8 @@ type v3Conn struct {
 }
 
 func makeV3Conn(
-	conn net.Conn, executor *sql.Executor,
-	metrics *serverMetrics, sessionArgs sql.SessionArgs) v3Conn {
+	conn net.Conn, executor *sql.Executor, metrics *serverMetrics, sessionArgs sql.SessionArgs,
+) v3Conn {
 	return v3Conn{
 		conn:     conn,
 		rd:       bufio.NewReader(conn),
@@ -133,7 +133,7 @@ func makeV3Conn(
 		executor: executor,
 		writeBuf: writeBuffer{bytecount: metrics.bytesOutCount},
 		metrics:  metrics,
-		session:  sql.NewSession(sessionArgs, executor, conn.RemoteAddr()),
+		session:  sql.NewSession(executor.Ctx(), sessionArgs, executor, conn.RemoteAddr()),
 	}
 }
 
@@ -189,7 +189,7 @@ var statusReportParams = map[string]string{
 }
 
 func (c *v3Conn) serve(authenticationHook func(string, bool) error) error {
-	ctx := c.session.Context()
+	ctx := c.session.Ctx()
 
 	if authenticationHook != nil {
 		if err := authenticationHook(c.session.User, true /* public */); err != nil {
@@ -636,7 +636,7 @@ func (c *v3Conn) executeStatements(
 	limit int,
 ) error {
 	tracing.AnnotateTrace()
-	results := c.executor.ExecuteStatements(ctx, c.session, stmts, pinfo)
+	results := c.executor.ExecuteStatements(c.session, stmts, pinfo)
 
 	tracing.AnnotateTrace()
 	if results.Empty {
