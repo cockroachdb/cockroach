2c509dd3b1e443b4a106824e6459e912b03fe988
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2016-08-03 06:06:11 -0400

    storage: do not use non-authoritative Desc()
    After ec007326db720f9028f793438ea40a6ce1dc4da1, we were using a denormalized
    copy of the descriptor which is not protected by the same mutex in places
    where we shouldn't, resulting in subtle races. Change it back to use the
    authoritative one almost everywhere, except for informational callsites
    (such as stringifying a `Replica`).
    
    Fixes #8086.
    Fixes #8157.

--- storage/store_test.go
+++ storage/store_test.go
@@ -1062,28 +1062,28 @@ func TestStoreReplicasByKey(t *testing.T) {
 	r4 := splitTestRange(store, roachpb.RKey("X"), roachpb.RKey("ZZ"), t)
 
 	if r := store.LookupReplica(roachpb.RKey("0"), nil); r != r0 {
-		t.Errorf("mismatched range %+v != %+v", r.Desc(), r0.Desc())
+		t.Errorf("mismatched range %+v != %+v", r.InformalDesc(), r0.InformalDesc())
 	}
 	if r := store.LookupReplica(roachpb.RKey("B"), nil); r != r1 {
-		t.Errorf("mismatched range %+v != %+v", r.Desc(), r1.Desc())
+		t.Errorf("mismatched range %+v != %+v", r.InformalDesc(), r1.InformalDesc())
 	}
 	if r := store.LookupReplica(roachpb.RKey("C"), nil); r != r2 {
-		t.Errorf("mismatched range %+v != %+v", r.Desc(), r2.Desc())
+		t.Errorf("mismatched range %+v != %+v", r.InformalDesc(), r2.InformalDesc())
 	}
 	if r := store.LookupReplica(roachpb.RKey("M"), nil); r != r2 {
-		t.Errorf("mismatched range %+v != %+v", r.Desc(), r2.Desc())
+		t.Errorf("mismatched range %+v != %+v", r.InformalDesc(), r2.InformalDesc())
 	}
 	if r := store.LookupReplica(roachpb.RKey("X"), nil); r != r3 {
-		t.Errorf("mismatched range %+v != %+v", r.Desc(), r3.Desc())
+		t.Errorf("mismatched range %+v != %+v", r.InformalDesc(), r3.InformalDesc())
 	}
 	if r := store.LookupReplica(roachpb.RKey("Z"), nil); r != r3 {
-		t.Errorf("mismatched range %+v != %+v", r.Desc(), r3.Desc())
+		t.Errorf("mismatched range %+v != %+v", r.InformalDesc(), r3.InformalDesc())
 	}
 	if r := store.LookupReplica(roachpb.RKey("ZZ"), nil); r != r4 {
-		t.Errorf("mismatched range %+v != %+v", r.Desc(), r4.Desc())
+		t.Errorf("mismatched range %+v != %+v", r.InformalDesc(), r4.InformalDesc())
 	}
 	if r := store.LookupReplica(roachpb.RKey("\xff\x00"), nil); r != r4 {
-		t.Errorf("mismatched range %+v != %+v", r.Desc(), r4.Desc())
+		t.Errorf("mismatched range %+v != %+v", r.InformalDesc(), r4.InformalDesc())
 	}
 	if store.LookupReplica(roachpb.RKeyMax, nil) != nil {
 		t.Errorf("expected roachpb.KeyMax to not have an associated range")
