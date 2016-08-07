650664150355d0b856fd15ff9523cb70da0890f6
Tamir Duberstein <tamird@gmail.com>
2016-08-07 08:30:14 -0400

    build: update to go1.7rc5
    Also change chmod to only affect GOTOOLDIR, since not all of GOROOT
    needs to be writable (GOROOT/pkg is mounted from the host).

--- build/build-osx.sh
+++ build/build-osx.sh
@@ -5,6 +5,4 @@ set -eu
 go get -u github.com/karalabe/xgo
 # OSX 10.9 is the most recent version available at time of writing.
 # If changing the OS/arch target, adjust the filename in push-aws.sh.
-# TODO(tamird): update to go1.6.3 or go1.7 when
-# https://github.com/karalabe/xgo/pull/60/files is merged.
-xgo --image cockroachdb/xgo:20160526 --targets=darwin-10.9/amd64 --go=1.6.2 --ldflags="$($(dirname "${0}")/ldflags.sh)" ${GOPATH%%:*}/src/github.com/cockroachdb/cockroach
+xgo --image cockroachdb/xgo:20160808 --targets=darwin-10.9/amd64 --go=1.7rc5 --ldflags="$($(dirname "${0}")/ldflags.sh)" ${GOPATH%%:*}/src/github.com/cockroachdb/cockroach
