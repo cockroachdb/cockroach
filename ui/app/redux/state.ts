6e7e4e8ef7b2699c1ae82d2a2511659c0e2e8c49
Max Lang <max@cockroachlabs.com>
2016-08-10 17:04:19 -0400

    ui: temporary fix so that redux devtools will work

--- ui/app/redux/state.ts
+++ ui/app/redux/state.ts
@@ -10,6 +10,8 @@ import metricsReducer, * as metrics from "./metrics";
 import timeWindowReducer, * as timewindow from "./timewindow";
 import apiReducersReducer, * as apiReducers from "./apiReducers";
 
+import { PayloadAction } from "../interfaces/action";
+
 export interface AdminUIState {
     routing: IRouterState;
     ui: ui.UISettingsDict;
@@ -32,7 +34,30 @@ export const store = createStore<AdminUIState>(
     applyMiddleware(thunk),
     // Support for redux dev tools
     // https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd
-    (window as any).devToolsExtension ? (window as any).devToolsExtension() : _.identity
+    (window as any).devToolsExtension ? (window as any).devToolsExtension({
+      /**
+       * HACK HACK HACK
+       * The state object insn't currently serializeable, which redux dev tools
+       * expects, because there's a circular reference which is causing
+       * JSON.stringify to fail. The specific path with the circular reference
+       * appears to be state.cachedData.nodes.data.metrics.field
+       *
+       * Fix inspired by this suggestion for avoiding large blobs in redux dev
+       * tools: https://github.com/zalmoxisus/redux-devtools-extension/issues/159#issuecomment-231034408
+       * TODO (maxlang): file issues upstream
+       *
+       * NOTE: this only affects environments with react dev tools installed
+       */
+      actionsFilter: (action: PayloadAction<any>): PayloadAction<any> => (/nodes/).test(action.type) ? {type: action.type, payload: "<<NODE_DATA>>"} : action,
+      statesFilter: (state: AdminUIState): AdminUIState => {
+        if (state.cachedData.nodes.data) {
+          let clone = _.clone(state);
+          clone.cachedData.nodes.data = <any>"<<NODE_DATA>>";
+          return clone;
+        }
+        return state;
+      },
+    }) : _.identity
   ) as StoreEnhancer<AdminUIState>
 );
 
