118f7cbd54d2fc1c274e0532f7cad5c557d27a35
Andrei Matei <andrei@cockroachlabs.com>
2016-08-18 18:49:04 -0400

    sql: move span collection from a client.Txn to a TxnState
    The TxnState matches the lifetime of a SQL txn, whereas the client.Txn
    goes away when the SQL txn goes, for example, to the Aborted state.
    Plus, the SQL executor has direct access to a txnState, so it's natural
    for it to use the TxnState's context for log messages, and we want these
    messages aassocited with the "transaction's trace". In fact, the
    TxnState was already the the holder of a txn's root span, so this is in
    effect unifying the owner of the span and of all the collected children
    spans.
    
    This commit also makes all SQL transactions be executed with a root
    span.

--- sql/executor.go
+++ sql/executor.go
@@ -41,6 +41,7 @@ import (
 	"github.com/cockroachdb/cockroach/util/metric"
 	"github.com/cockroachdb/cockroach/util/stop"
 	"github.com/cockroachdb/cockroach/util/syncutil"
+	"github.com/cockroachdb/cockroach/util/tracing"
 	"github.com/pkg/errors"
 )
 
@@ -244,6 +245,9 @@ type ExecutorTestingKnobs struct {
 // NewExecutor creates an Executor and registers a callback on the
 // system config.
 func NewExecutor(cfg ExecutorConfig, stopper *stop.Stopper) *Executor {
+	if tracing.TracerFromCtx(cfg.Context) == nil {
+		panic("ExecutorConfig.Ctx must have a tracer in it")
+	}
 	exec := &Executor{
 		cfg:     cfg,
 		reCache: parser.NewRegexpCache(512),
@@ -282,7 +286,8 @@ func NewExecutor(cfg ExecutorConfig, stopper *stop.Stopper) *Executor {
 
 // NewDummyExecutor creates an empty Executor that is used for certain tests.
 func NewDummyExecutor() *Executor {
-	return &Executor{cfg: ExecutorConfig{Context: context.Background()}}
+	return &Executor{cfg: ExecutorConfig{
+		Context: tracing.WithTracer(context.Background(), tracing.NewTracer())}}
 }
 
 // Ctx returns the Context associated with this Executor.
@@ -349,6 +354,9 @@ func (e *Executor) Prepare(
 
 	// Prepare needs a transaction because it needs to retrieve db/table
 	// descriptors for type checking.
+	// TODO(andrei): is this OK? If we're preparing as part of a SQL txn, how do
+	// we check that they're reading descriptors consistent with the txn in which
+	// they'll be used?
 	txn := client.NewTxn(session.Ctx(), *e.cfg.DB)
 	txn.Proto.Isolation = session.DefaultIsolationLevel
 	session.planner.setTxn(txn)
@@ -482,8 +490,7 @@ func (e *Executor) execRequest(session *Session, sql string) StatementResults {
 					}()
 				}
 			}
-			txnState.reset(session.Ctx(), e, session)
-			txnState.State = Open
+			txnState.resetForNewSQLTxn(e, session)
 			txnState.autoRetry = true
 			txnState.sqlTimestamp = e.cfg.Clock.PhysicalTime()
 			if execOpt.AutoCommit {
@@ -528,7 +535,7 @@ func (e *Executor) execRequest(session *Session, sql string) StatementResults {
 		if err != nil {
 			lastResult := &results[len(results)-1]
 			if aErr, ok := err.(*client.AutoCommitError); ok {
-				// Until #7881 fixed.
+				// TODO(andrei): Until #7881 fixed.
 				if txn == nil {
 					log.Errorf(session.Ctx(), "AutoCommitError on nil txn: %+v, "+
 						"txnState %+v, execOpt %+v, stmts %+v, remaining %+v",
@@ -561,6 +568,12 @@ func (e *Executor) execRequest(session *Session, sql string) StatementResults {
 			// If execOpt.AutoCommit was set, then the txn no longer exists at this point.
 			txnState.resetStateAndTxn(NoTxn)
 		}
+
+		// If we're no longer in a transaction, finish the trace.
+		if txnState.State == NoTxn {
+			txnState.finishSQLTxn()
+		}
+
 		// If the txn is in any state but Open, exec the schema changes. They'll
 		// short-circuit themselves if the mutation that queued them has been
 		// rolled back from the table descriptor.
@@ -1050,13 +1063,11 @@ func commitSQLTransaction(
 		switch commitType {
 		case release:
 			// We'll now be waiting for a COMMIT.
-			txnState.State = CommitWait
+			txnState.resetStateAndTxn(CommitWait)
 		case commit:
 			// We're done with this txn.
-			txnState.State = NoTxn
+			txnState.resetStateAndTxn(NoTxn)
 		}
-		txnState.dumpTrace()
-		txnState.txn = nil
 	}
 	// Reset transaction to prevent running further commands on this planner.
 	p.resetTxn()
