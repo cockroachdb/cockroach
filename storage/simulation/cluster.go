01babf11da8309af7567611629ebf946ffb77239
Tristan Rice <rice@fn.lc>
2016-08-22 20:30:42 -0500

    storage: implemented RuleSolver into allocator

--- storage/simulation/cluster.go
+++ storage/simulation/cluster.go
@@ -99,7 +99,6 @@ func createCluster(
 		storePool: storePool,
 		allocator: storage.MakeAllocator(storePool, storage.AllocatorOptions{
 			AllowRebalance: true,
-			Deterministic:  true,
 		}),
 		storeGossiper:   gossiputil.NewStoreGossiper(g),
 		nodes:           make(map[roachpb.NodeID]*Node),
