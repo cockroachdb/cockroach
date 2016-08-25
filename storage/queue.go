42da68e199418769ef8145ca34ec13f293097c57
Radu Berinde <radu.berinde@gmail.com>
2016-08-25 18:56:09 -0400

    storage: remove unnecessary RunTask
    Now that we no longer have a race with stopping the queues, we can remove the
    RunTask hack that was used to avoid it.
    
    Fixes #8561.

--- storage/queue.go
+++ storage/queue.go
@@ -144,10 +144,6 @@ func (l queueLog) Error(err error) {
 	l.traceLog.Errorf(format, err)
 }
 
-func (l queueLog) Finish() {
-	l.traceLog.Finish()
-}
-
 type queueConfig struct {
 	// maxSize is the maximum number of replicas to queue.
 	maxSize int
@@ -279,9 +275,6 @@ func (bq *baseQueue) Start(clock *hlc.Clock, stopper *stop.Stopper) {
 func (bq *baseQueue) Add(repl *Replica, priority float64) (bool, error) {
 	bq.mu.Lock()
 	defer bq.mu.Unlock()
-	if bq.mu.stopped {
-		return false, errQueueStopped
-	}
 	return bq.addInternal(repl, true, priority)
 }
 
@@ -339,7 +332,7 @@ func (bq *baseQueue) requiresSplit(cfg config.SystemConfig, repl *Replica) bool 
 // priority. Expects the queue lock is held by caller.
 func (bq *baseQueue) addInternal(repl *Replica, should bool, priority float64) (bool, error) {
 	if bq.mu.stopped {
-		panic("stopped queue")
+		return false, errQueueStopped
 	}
 
 	if bq.mu.disabled {
