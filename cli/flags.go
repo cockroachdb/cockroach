c2bd4fac387326db22414a9d290ee398012b9841
Daniel Harrison <daniel.harrison@gmail.com>
2016-08-25 10:08:26 -0400

    cli: move backup/restore under debug
    And get rid of the `experimental` build tag, this file was not checked by the CI
    and was too easy to break. Three files (one for the code, plus one each for the
    build tag and its inverse) would also have worked, but debug is just as good a
    place to hide an in-progress feature and less hassle.

--- cli/flags.go
+++ cli/flags.go
@@ -342,7 +342,7 @@ func init() {
 	clientCmds = append(clientCmds, userCmds...)
 	clientCmds = append(clientCmds, zoneCmds...)
 	clientCmds = append(clientCmds, nodeCmds...)
-	clientCmds = append(clientCmds, backupCmds...)
+	clientCmds = append(clientCmds, backupCmd, restoreCmd)
 	for _, cmd := range clientCmds {
 		f := cmd.PersistentFlags()
 		stringFlag(f, &connHost, cliflags.ClientHost, "")
