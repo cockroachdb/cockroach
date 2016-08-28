d8ff53a8d4566caa73a63dcedb65d61cb31fd7c8
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2016-08-28 01:02:46 -0400

    stability: expose (and fix) data races in Transport
    A BatchRequest may be passed on to multiple goroutines within the kv.Transport,
    and so effectively `DistSender` and `TxnCoordSender` should not assume that
    they have exclusive access to any of the pointer fields in it. This was not
    always obeyed and various data races are fixed in this commit.
    
    They were identified with a newly introduced mechanism, which during
    race-enabled builds intercepts all BatchRequests passing through GRPCTransport
    and continuously accesses them. This vastly improving race detector coverage in
    our test suite for this type of race.
    
    Most of the races are directly related to updating a `Transaction` without
    copy-on-write; one highlighted that we should copy the request in the transport
    even when bypassing GRPC completely (for requests which go to the local Node).
    
    At times, it was necessary to increase the race detector's history size to
    capture the offending goroutines' stack traces:
    
        make testrace GORACE=history_size=7 PKG=./sql TESTS=TestAsOfRetry
    
    Fixes #8020.

---
+++ kv/transport_race.go
@@ -0,0 +1,90 @@
+// Copyright 2016 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License. See the AUTHORS file
+// for names of contributors.
+//
+// Author: Tobias Schottdorf
+
+// +build race
+
+package kv
+
+import (
+	"encoding/gob"
+	"io/ioutil"
+	"math/rand"
+	"sync/atomic"
+	"time"
+
+	"golang.org/x/net/context"
+
+	"github.com/cockroachdb/cockroach/roachpb"
+	"github.com/cockroachdb/cockroach/rpc"
+	"github.com/cockroachdb/cockroach/util/log"
+)
+
+var running atomic.Value // bool
+var incoming chan *roachpb.BatchRequest
+
+func init() {
+	running.Store(false)
+	incoming = make(chan *roachpb.BatchRequest, 100)
+}
+
+// grpcTransportFactory during race builds wraps the implementation and
+// intercepts all BatchRequests, reading them in a tight loop. This allows the
+// race detector to catch any mutations of a batch passed to the transport.
+func grpcTransportFactory(
+	opts SendOptions,
+	rpcContext *rpc.Context,
+	replicas ReplicaSlice,
+	args roachpb.BatchRequest,
+) (Transport, error) {
+	if !running.Load().(bool) {
+		running.Store(true)
+		rpcContext.Stopper.RunWorker(func() {
+			var iters int
+			defer running.Store(false)
+			defer func() {
+				log.Infof(context.Background(), "transport race promotion: ran %d iterations", iters)
+			}()
+			const size = 1000
+			bas := make([]*roachpb.BatchRequest, size)
+			encoder := gob.NewEncoder(ioutil.Discard)
+			var curIdx int
+			for {
+				iters++
+				select {
+				case ba := <-incoming:
+					bas[curIdx] = ba
+					curIdx = (curIdx + 1) % size
+				case <-rpcContext.Stopper.ShouldStop():
+					return
+				case <-time.After(time.Duration(rand.Int63n(int64(time.Microsecond)))):
+				}
+				for _, ba := range bas {
+					if ba != nil {
+						if err := encoder.Encode(&ba); err != nil {
+							panic(err)
+						}
+					}
+				}
+			}
+		})
+	}
+	select {
+	case incoming <- &args:
+	default:
+	}
+	return grpcTransportFactoryImpl(opts, rpcContext, replicas, args)
+}
