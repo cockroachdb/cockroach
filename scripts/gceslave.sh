5ecfd4b9a48d0e9fd1ee1adf9631572684263c0b
Tamir Duberstein <tamird@gmail.com>
2016-08-29 13:18:21 -0400

    scripts/gceslave.sh: use environment variables instead of flags

--- scripts/gceslave.sh
+++ scripts/gceslave.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euxo pipefail
 
-project=${COCKROACH_PROJECT}
-zone=${GCESLAVE_ZONE-us-east1-b}
+export CLOUDSDK_CORE_PROJECT=${CLOUDSDK_CORE_PROJECT-${GOOGLE_PROJECT}}
+export CLOUDSDK_COMPUTE_ZONE=${GCESLAVE_ZONE-${CLOUDSDK_COMPUTE_ZONE-us-east1-b}}
+
 name=${GCESLAVE_NAME-gceslave}
 
 cd "$(dirname "${0}")"
 
-case $1 in
+case ${1-} in
     create)
-    gcloud compute --project "${project}" \
-           instances create "${name}" \
-           --zone "${zone}" \
+    gcloud compute instances \
+           create "${name}" \
            --machine-type "custom-32-65536" \
            --network "default" \
            --maintenance-policy "MIGRATE" \
@@ -22,8 +22,8 @@ case $1 in
            --boot-disk-device-name "${name}"
     sleep 20 # avoid SSH timeout on copy-files
 
-    gcloud compute copy-files --zone "${zone}" . "${name}:scripts"
-    gcloud compute --project "${project}" ssh "${name}" --zone "${zone}" ./scripts/bootstrap-debian.sh
+    gcloud compute copy-files . "${name}:scripts"
+    gcloud compute ssh "${name}" ./scripts/bootstrap-debian.sh
     # Install automatic shutdown after ten minutes of operation without a
     # logged in user. To disable this, `sudo touch /.active`.
     # This is much more intricate than it looks. A few complications which
@@ -36,25 +36,25 @@ case $1 in
     #   once a shutdown has been scheduled, we don't want to keep scheduling
     #   later ones or we will never actually shut down - hence the pgrep.
     # - This is invoked via `sh -c`, and so no `bash` features must be used.
-    gcloud compute --project "${project}" ssh "${name}" --zone "${zone}" \
+    gcloud compute ssh "${name}" \
       "echo '* * * * * rm -f /etc/nologin; (w -hs | (grep -q pts && /sbin/shutdown -c --no-wall) || [ -f /.active ] || pgrep -flx /lib/systemd/systemd-shutdownd || /sbin/shutdown --no-wall -h +10) >>/root/idle.log 2>&1' | sudo crontab -"
 
     ;;
     start)
-    gcloud compute --project "${project}" instances start "${name}" --zone "${zone}"
+    gcloud compute instances start "${name}"
     ;;
     stop)
-    gcloud compute --project "${project}" instances stop "${name}" --zone "${zone}"
+    gcloud compute instances stop "${name}"
     ;;
     destroy)
-    gcloud compute --project "${project}" instances delete "${name}" --zone "${zone}"
+    gcloud compute instances delete "${name}"
     ;;
     ssh)
     shift
-    gcloud compute --project "${project}" ssh "${name}" --zone "${zone}" "$@"
+    gcloud compute ssh "${name}" "$@"
     ;;
     *)
-    echo "$0: unknown command: $1, use one of create, start, stop, destroy, or ssh"
+    echo "$0: unknown command: ${1-}, use one of create, start, stop, destroy, or ssh"
     exit 1
     ;;
 esac
