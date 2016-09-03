78a046d7e36ca69de72ee1262907e281499cf661
Peter Mattis <petermattis@gmail.com>
2016-09-03 11:39:58 -0400

    storage: fix race in TestReplicaNotLeaseHolderError

--- storage/replica_test.go
+++ storage/replica_test.go
@@ -654,9 +654,9 @@ func TestReplicaNotLeaseHolderError(t *testing.T) {
 		StoreID:   2,
 		ReplicaID: 2,
 	}
-	rngDesc := tc.rng.Desc()
+	rngDesc := *tc.rng.Desc()
 	rngDesc.Replicas = append(rngDesc.Replicas, secondReplica)
-	tc.rng.setDescWithoutProcessUpdate(rngDesc)
+	tc.rng.setDescWithoutProcessUpdate(&rngDesc)
 
 	tc.manualClock.Set(leaseExpiry(tc.rng))
 	now := tc.clock.Now()
