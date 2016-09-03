ea8b8fa6550f7267348a0fa75df53fd50fd70d48
Raphael 'kena' Poss <knz@cockroachlabs.com>
2016-09-03 12:33:43 +0000

    cli: add interactive tests.
    This introduces tests that use expect
    ( https://en.wikipedia.org/wiki/Expect )
    to interact with the CockroachDB binary as
    if it was used interactively.

---
+++ cli/interactive_tests/test_reconnect.tcl
@@ -0,0 +1,47 @@
+#! /usr/bin/env expect -f
+
+source [file join [file dirname $argv0] common.tcl]
+
+start_server $argv
+
+spawn $argv sql
+eexpect root@
+
+send "select 1;\r"
+eexpect "1 row"
+eexpect root@
+
+# Check that the client properly detects the server went down.
+stop_server $argv
+
+send "select 1;\r"
+eexpect "bad connection"
+eexpect root@
+
+send "select 1;\r"
+eexpect "connection refused"
+eexpect root@
+
+# Check that the client automatically reconnects when the server goes up again.
+start_server $argv
+
+send "select 1;\r"
+eexpect "1 row"
+eexpect root@
+
+# Check that the client picks up when the server was restarted.
+stop_server $argv
+start_server $argv
+
+send "select 1;\r"
+eexpect "bad connection"
+eexpect root@
+
+send "select 1;\r"
+eexpect "1 row"
+eexpect root@
+
+send "\\q\r"
+eexpect eof
+
+stop_server $argv
