b969d5c829e3ec1dfefecc4deac32ead22b7298e
Jordan Lewis <jordanthelewis@gmail.com>
2016-09-07 13:45:08 -0400

    storage: stream snapshots via a new GRPC endpoint.
    This is a rework of the previous streaming snapshots work from #8726 for
    compatibility with the removal of processRaftMu.
    
    This commit changes the mechanism of transport for raft snapshots to a
    new streaming GRPC method, as per the first phase of the streaming
    snapshots RFC [0]. Snapshots are not yet truly streamed - this commit is
    mostly a plumbing change. Instead, they are still sent in a large blob,
    but over the new streaming transport.
    
    Also, delete the unused `RaftMessageSync` method and associated
    infrastructure as it is superseded by streaming snapshots.
    
    [0]: https://github.com/cockroachdb/cockroach/blob/3bc6103fc46665b7089e4d80d1966f8f3e2bcbb2/docs/RFCS/streaming_snapshots.md

--- storage/store_test.go
+++ storage/store_test.go
@@ -2341,8 +2341,11 @@ func TestStoreRemovePlaceholderOnError(t *testing.T) {
 		},
 	}
 	const expected = "preemptive snapshot from term 0 received"
-	if err := s.HandleRaftRequest(ctx, req, nil); !testutils.IsError(
-		errors.Errorf("%s", err), expected) {
+	if err := s.processRaftRequest(ctx, req,
+		IncomingSnapshot{
+			SnapUUID:        uuid.MakeV4(),
+			RangeDescriptor: *rng1.Desc(),
+		}); !testutils.IsError(err.GoError(), expected) {
 		t.Fatalf("expected %s, but found %v", expected, err)
 	}
 
@@ -2418,7 +2421,11 @@ func TestStoreRemovePlaceholderOnRaftIgnored(t *testing.T) {
 			},
 		},
 	}
-	if err := s.HandleRaftRequest(ctx, req, nil); err != nil {
+	if err := s.processRaftRequest(ctx, req,
+		IncomingSnapshot{
+			SnapUUID:        uuid.MakeV4(),
+			RangeDescriptor: *rng1.Desc(),
+		}); err != nil {
 		t.Fatal(err)
 	}
 
