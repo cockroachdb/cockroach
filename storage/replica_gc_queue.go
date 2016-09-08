c579d287274d408d2e2dab2a063917207d5c546e
Peter Mattis <petermattis@gmail.com>
2016-09-08 16:44:07 -0400

    storage: add replica log tag to queue context
    Add the replica log tag to the context passed to queue.process. Fixed up
    a bunch of log messages in the process to removing the resulting
    redundant information.

--- storage/replica_gc_queue.go
+++ storage/replica_gc_queue.go
@@ -179,10 +179,7 @@ func (q *replicaGCQueue) process(
 	replyDesc := reply.Ranges[0]
 	if _, currentMember := replyDesc.GetReplicaDescriptor(rng.store.StoreID()); !currentMember {
 		// We are no longer a member of this range; clean up our local data.
-		if log.V(1) {
-			log.Infof(ctx, "destroying local data from range %d", desc.RangeID)
-		}
-		log.Trace(ctx, "destroying local data")
+		log.VTracef(1, ctx, "destroying local data")
 		if err := rng.store.RemoveReplica(rng, replyDesc, true); err != nil {
 			return err
 		}
@@ -191,10 +188,7 @@ func (q *replicaGCQueue) process(
 		// away. But currentMember is true, so we are still a member of the
 		// subsuming range. Shut down raft processing for the former range
 		// and delete any remaining metadata, but do not delete the data.
-		if log.V(1) {
-			log.Infof(ctx, "removing merged range %d", desc.RangeID)
-		}
-		log.Trace(ctx, "removing merged range")
+		log.VTracef(1, ctx, "removing merged range")
 		if err := rng.store.RemoveReplica(rng, replyDesc, false); err != nil {
 			return err
 		}
--- storage/split_queue.go
+++ storage/split_queue.go
@@ -96,7 +96,7 @@ func (sq *splitQueue) process(
 	desc := rng.Desc()
 	splitKeys := sysCfg.ComputeSplitKeys(desc.StartKey, desc.EndKey)
 	if len(splitKeys) > 0 {
-		log.Infof(ctx, "splitting %s at keys %v", rng, splitKeys)
+		log.Infof(ctx, "splitting at keys %v", splitKeys)
 		for _, splitKey := range splitKeys {
 			if err := sq.db.AdminSplit(splitKey.AsRawKey()); err != nil {
 				return errors.Errorf("unable to split %s at key %q: %s", rng, splitKey, err)
@@ -113,7 +113,7 @@ func (sq *splitQueue) process(
 	size := rng.GetMVCCStats().Total()
 	// FIXME: why is this implementation not the same as the one above?
 	if float64(size)/float64(zone.RangeMaxBytes) > 1 {
-		log.Infof(ctx, "splitting %s size=%d max=%d", rng, size, zone.RangeMaxBytes)
+		log.Infof(ctx, "splitting size=%d max=%d", size, zone.RangeMaxBytes)
 		if _, pErr := client.SendWrappedWith(rng, ctx, roachpb.Header{
 			Timestamp: now,
 		}, &roachpb.AdminSplitRequest{
