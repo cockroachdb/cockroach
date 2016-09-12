3d3be963aa28acc4dc6cd28f1f977dd07d1e0878
Peter Mattis <petermattis@gmail.com>
2016-09-12 17:16:53 -0400

    storage: quiesce ranges
    Quiesce ranges when there is no pending work. A quiesced range is not
    ticked and thus doesn't create MsgHeartbeat requests or cause
    elections. The Raft leader for a range initiates quiescence just before
    ticking the local replica and checks various conditions: no pending raft
    commands, no pending raft ready, all of the followers are up to date,
    etc. Quiescence is initiated by a special MsgHeartbeat that is tagged as
    Quiesce. Upon receipt, the follower checks to see if the term/commit
    matches and marks the local replica as quiescent. If the term/commit do
    not match the MsgHeartbeat is passed through to Raft which will generate
    a MsgHeartbeatResp that will unquiesce the sender.
    
    Any Raft operation on the local replica will unquiesce the Replica. For
    example, a Raft operation initiated on a follower will unquiesce the
    follower which will send a MsgProp to the leader that will unquiesce the
    leader. If the leader of a quiesced range dies, followers will not
    notice, though any request directed to the range will eventually end up
    on a follower which will unquiesce the follower and lead to an election.

--- storage/store.go
+++ storage/store.go
@@ -2247,10 +2247,6 @@ func (s *Store) HandleRaftRequest(
 ) (pErr *roachpb.Error) {
 	s.metrics.raftRcvdMessages[req.Message.Type].Inc(1)
 
-	if req.Message.Type == raftpb.MsgHeartbeat {
-		// TODO(bdarnell): handle coalesced heartbeats.
-	}
-
 	if respStream == nil {
 		return s.processRaftRequest(ctx, req)
 	}
@@ -2285,6 +2281,17 @@ func (s *Store) processRaftRequest(
 	}
 	defer r.raftUnlock(uninitRaftLocked)
 	r.setLastReplicaDescriptors(req)
+
+	if req.Quiesce {
+		if req.Message.Type != raftpb.MsgHeartbeat {
+			panic(fmt.Sprintf("unexpected quiesce: %+v", req))
+		}
+		status := r.RaftStatus()
+		if status.Term == req.Message.Term && status.Commit == req.Message.Commit {
+			r.setQuiescent(true)
+			return
+		}
+	}
 
 	// Check to see if a snapshot can be applied. Snapshots can always be applied
 	// to initialized replicas. Note that if we add a placeholder we need to
@@ -2864,11 +2871,17 @@ func (s *Store) updateReplicationGauges() error {
 		return errors.Errorf("%s: system config not yet available", s)
 	}
 
-	var raftLeaderCount, leaseHolderCount, raftLeaderNotLeaseHolderCount, availableRangeCount,
-		replicaAllocatorNoopCount,
-		replicaAllocatorAddCount,
-		replicaAllocatorRemoveCount,
+	var (
+		raftLeaderCount                 int64
+		leaseHolderCount                int64
+		raftLeaderNotLeaseHolderCount   int64
+		quiescentCount                  int64
+		availableRangeCount             int64
+		replicaAllocatorNoopCount       int64
+		replicaAllocatorAddCount        int64
+		replicaAllocatorRemoveCount     int64
 		replicaAllocatorRemoveDeadCount int64
+	)
 
 	timestamp := s.ctx.Clock.Now()
 
@@ -2895,6 +2908,9 @@ func (s *Store) updateReplicationGauges() error {
 		rep.mu.Lock()
 		raftStatus := rep.raftStatusLocked()
 		lease := rep.mu.state.Lease
+		if rep.mu.quiescent {
+			quiescentCount++
+		}
 		rep.mu.Unlock()
 
 		leaseCovers := lease.Covers(timestamp)
@@ -2949,6 +2965,7 @@ func (s *Store) updateReplicationGauges() error {
 	s.metrics.RaftLeaderCount.Update(raftLeaderCount)
 	s.metrics.RaftLeaderNotLeaseHolderCount.Update(raftLeaderNotLeaseHolderCount)
 	s.metrics.LeaseHolderCount.Update(leaseHolderCount)
+	s.metrics.QuiescentCount.Update(quiescentCount)
 
 	s.metrics.AvailableRangeCount.Update(availableRangeCount)
 
