28229a9e93475958659e10fc064638b199098d99
marc <marc@cockroachlabs.com>
2016-09-13 10:04:26 -0400

    Fix multi-store metrics.
    Fixes #9302
    
    Metrics with the same name but different labels need to be exported
    together (ie: there must be a single HELP and TYPE line for a given
    metric). This means that we need to combine metrics with the same name,
    most of which are currently the store-level metrics.

---
+++ util/metric/prometheus_exporter.go
@@ -0,0 +1,92 @@
+// Copyright 2016 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// Author: Marc Berhault (marc@cockroachlabs.com)
+
+package metric
+
+import (
+	"io"
+
+	"github.com/gogo/protobuf/proto"
+	prometheusgo "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/expfmt"
+)
+
+// PrometheusExporter contains a map of metric families (a metric with multiple labels).
+// It initializes each metric family once and reuses it for each prometheus scrape.
+// It is NOT thread-safe.
+//  pe := NewPrometheusExporter()
+//  pe.AddRegistry(nodeRegistry)
+//  pe.AddRegistry(storeOneRegistry)
+//  ...
+//  pe.AddRegistry(storeNRegistry)
+//  pe.Export(w)
+type PrometheusExporter struct {
+	families map[string]*prometheusgo.MetricFamily
+}
+
+// NewPrometheusExporter returns an initialized prometheus exporter.
+func NewPrometheusExporter() PrometheusExporter {
+	return PrometheusExporter{families: map[string]*prometheusgo.MetricFamily{}}
+}
+
+// find the family for the passed-in metric, or create and return it if not found.
+func (pm *PrometheusExporter) findOrCreateFamily(prom PrometheusExportable) *prometheusgo.MetricFamily {
+	familyName := exportedName(prom.GetName())
+	if family, ok := pm.families[familyName]; ok {
+		return family
+	}
+
+	family := &prometheusgo.MetricFamily{
+		Name: proto.String(familyName),
+		Help: proto.String(prom.GetHelp()),
+		Type: prom.GetType(),
+	}
+
+	pm.families[familyName] = family
+	return family
+}
+
+// AddRegistry takes a registry and adds all metrics to the metric family map.
+// It creates a new family if needed.
+func (pm *PrometheusExporter) AddRegistry(registry *Registry) {
+	labels := registry.getLabels()
+	for _, metric := range registry.tracked {
+		metric.Inspect(func(v interface{}) {
+			if prom, ok := v.(PrometheusExportable); ok {
+				m := prom.ToPrometheusMetric()
+				// Set registry and metric labels.
+				m.Label = append(labels, prom.GetLabels()...)
+
+				family := pm.findOrCreateFamily(prom)
+				family.Metric = append(family.Metric, m)
+			}
+		})
+	}
+}
+
+// Export writes all metrics in the families map to the iowriter in
+// prometheus' text format. It removes individual metrics from the families
+// as it goes, readying the families for another found of registry additions.
+func (pm *PrometheusExporter) Export(w io.Writer) error {
+	for _, family := range pm.families {
+		if _, err := expfmt.MetricFamilyToText(w, family); err != nil {
+			return err
+		}
+		// Clear metrics for reuse.
+		family.Metric = []*prometheusgo.Metric{}
+	}
+	return nil
+}
