50bc217aa96978e10c0ed95534b41ddb7358bb8a
Ben Darnell <ben@cockroachlabs.com>
2016-09-19 20:22:20 +0800

    scripts: Install docker on gceworker
    This makes it possible to run `make acceptance`.

--- scripts/bootstrap-debian.sh
+++ scripts/bootstrap-debian.sh
@@ -9,11 +9,13 @@ GOVERSION="${GOVERSION-1.7.1}"
 
 cd "$(dirname "${0}")"
 
-sudo apt-get update -q && sudo apt-get install -q -y --no-install-recommends build-essential git gdb patch bzip2
+sudo apt-get update -q && sudo apt-get install -q -y --no-install-recommends build-essential git gdb patch bzip2 docker.io
+
+sudo adduser "${USER}" docker
 
 mkdir -p ~/go-bootstrap
-curl "https://storage.googleapis.com/golang/go${GOVERSION}.linux-amd64.tar.gz" | tar -C ~/go-bootstrap -xvz --strip=1
-curl "https://storage.googleapis.com/golang/go${GOVERSION}.src.tar.gz" | tar -C ~ -xvz
+curl "https://storage.googleapis.com/golang/go${GOVERSION}.linux-amd64.tar.gz" | tar -C ~/go-bootstrap -xz --strip=1
+curl "https://storage.googleapis.com/golang/go${GOVERSION}.src.tar.gz" | tar -C ~ -xz
 
 # Apply the patch for the "major" go version (e.g. 1.6, 1.7).
 GOPATCHVER=$(echo ${GOVERSION} | grep -o "^[0-9]\+\.[0-9]\+")
