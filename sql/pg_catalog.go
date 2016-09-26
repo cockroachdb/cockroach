6cd5cfaa148b65727a90aff031bb6631d0c0f69f
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2016-09-26 19:57:28 -0400

    sql: Add support for the pg_namespace table in pg_catalog
    The pg_namespace table stores namespaces.
    
    This commit also introduces the `oidHasher` type, which hashes database
    objects to consistent object ids. This will be used throughout the
    population of pg_catalog tables.

--- sql/pg_catalog.go
+++ sql/pg_catalog.go
@@ -16,8 +16,97 @@
 
 package sql
 
+import (
+	"encoding/binary"
+	"hash"
+	"hash/fnv"
+
+	"github.com/cockroachdb/cockroach/sql/parser"
+	"github.com/cockroachdb/cockroach/sql/sqlbase"
+)
+
 // pgCatalog contains a set of system tables mirroring PostgreSQL's pg_catalog schema.
 var pgCatalog = virtualSchema{
-	name:   "pg_catalog",
-	tables: []virtualSchemaTable{},
+	name: "pg_catalog",
+	tables: []virtualSchemaTable{
+		pgCatalogNamespaceTable,
+	},
+}
+
+var pgCatalogNamespaceTable = virtualSchemaTable{
+	schema: `
+CREATE TABLE pg_catalog.pg_namespace (
+	oid INT,
+	nspname STRING NOT NULL DEFAULT '',
+	nspowner INT,
+	aclitem STRING
+);
+`,
+	populate: func(p *planner, addRow func(...parser.Datum) error) error {
+		h := makeOidHasher()
+		return forEachDatabaseDesc(p, func(db *sqlbase.DatabaseDescriptor) error {
+			return addRow(
+				h.DBOid(db),                // oid
+				parser.NewDString(db.Name), // nspname
+				parser.DNull,               // nspowner
+				parser.DNull,               // aclitem
+			)
+		})
+	},
+}
+
+// oidHasher provides a consistent hashing mechanism for object identifiers in
+// pg_catalog tables, allowing for reliable joins across tables.
+type oidHasher struct {
+	h hash.Hash32
+}
+
+func makeOidHasher() oidHasher {
+	return oidHasher{h: fnv.New32()}
+}
+
+func (h oidHasher) writeStr(s string) {
+	if _, err := h.h.Write([]byte(s)); err != nil {
+		panic(err)
+	}
+}
+
+func (h oidHasher) writeUInt8(i uint8) {
+	if err := binary.Write(h.h, binary.BigEndian, i); err != nil {
+		panic(err)
+	}
+}
+
+func (h oidHasher) writeUInt32(i uint32) {
+	if err := binary.Write(h.h, binary.BigEndian, i); err != nil {
+		panic(err)
+	}
+}
+
+type oidTypeTag uint8
+
+const (
+	_ oidTypeTag = iota
+	databaseTypeTag
+)
+
+func (h oidHasher) writeTypeTag(tag oidTypeTag) {
+	h.writeUInt8(uint8(tag))
+}
+
+func (h oidHasher) getOid() *parser.DInt {
+	i := h.h.Sum32()
+	h.h.Reset()
+	return parser.NewDInt(parser.DInt(i))
+}
+
+func (h oidHasher) writeDB(db *sqlbase.DatabaseDescriptor) {
+	h.writeUInt32(uint32(db.ID))
+	h.writeStr(db.Name)
+}
+
+func (h oidHasher) DBOid(db *sqlbase.DatabaseDescriptor) *parser.DInt {
+	h.writeTypeTag(databaseTypeTag)
+	h.writeDB(db)
+	return h.getOid()
 }
