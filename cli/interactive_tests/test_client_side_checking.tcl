da0e54d8058e3ed99792aadb655488144ac325ac
Raphael 'kena' Poss <knz@cockroachlabs.com>
2016-09-28 21:00:23 +0000

    cli: enhance the `sql` CLI utility and fix long-standing bugs.
    This patch introduces a few features to enhance UX:
    
    - client-side syntax checking. By default in interactive shells, the
      client will check the syntax locally and the statement is not sent
      to the server at all if an error is detected. This feature ensures
      that a user who enters by mistake a statement containing a syntax
      error during a transaction does not cause the transaction to abort
      and force the user to start over.
    
      This feature is disabled by default in non-interactive shells.
    
    - for statements with valid syntax, the statement is pretty-printed
      before it is added to the interactive history. This exposes users to
      the "canonical syntax" of their input, which may increase SQL
      literacy of beginners over time. For example:
    
      `select * from intro.mytable where l%2=0;`
    
      becomes:
    
      `SELECT * FROM intro.mytable WHERE (l % 2) = 0;`
    
    - consistent error handling. When running non-interactively, the shell
      will stop reading further lines of input upon the first error
      encountered. This makes the handling of errors across lines
      consistent with the handling of errors when multiple statements are
      present on the same line.
    
      This feature is disabled by default in interactive shells.
    
    In addition to these user-visible features, a couple other features
    are added to make the above more configurable:
    
    - the SQL shell now accepts two built-in commands `\set` and `\unset`
      to set client-side variables/options. `\set` without argument lists
      the options set by default.
    
    - two client-side options are now supported:
    
      - `errexit` (named after the bash option of the same name): set by
        default when non-interactive, unset when interactive. Causes the
        shell to exit upon encountering an error.
    
      - `check_syntax`: set by default when interactive, unset when
        non-interactive. Causes the shell to validate syntax locally.

---
+++ cli/interactive_tests/test_client_side_checking.tcl
@@ -0,0 +1,73 @@
+#! /usr/bin/env expect -f
+
+source [file join [file dirname $argv0] common.tcl]
+
+start_server $argv
+
+spawn $argv sql
+eexpect root@
+
+# Check that syntax errors are handled client-side when running interactive.
+send "begin;\r"
+eexpect BEGIN
+eexpect root@
+
+send "select 3+;\r"
+eexpect "statement not sent"
+eexpect root@
+
+send "select 1;\r"
+eexpect "1 row"
+eexpect root@
+
+send "commit;\r"
+eexpect COMMIT
+eexpect root@
+
+# Check that the user can force server-side handling.
+send "\\unset check_syntax\r"
+eexpect root@
+
+send "begin;\r"
+eexpect BEGIN
+
+send "select 3+;\r"
+eexpect "pq: syntax error"
+eexpect root@
+
+send "select 1;\r"
+eexpect "current transaction is aborted"
+eexpect root@
+
+send "commit;\r"
+eexpect "ROLLBACK"
+eexpect root@
+
+send "\003"
+eexpect eof
+
+# Check that syntax errors are handled server-side by default when running non-interactive.
+spawn /bin/bash
+send "PS1=':''/# '\r"
+eexpect ":/# "
+
+send "(echo '\\unset errexit'; echo 'begin;'; echo 'select 1+;'; echo 'select 1;') | $argv sql\r"
+eexpect "syntax error"
+eexpect "current transaction is aborted"
+eexpect ":/# "
+send "echo \$?\r"
+eexpect "1\r\n:/# "
+
+send "(echo '\\unset errexit'; echo '\\set check_syntax'; echo 'begin;'; echo 'select 1+;'; echo 'select 1;'; echo 'commit;') | $argv sql\r"
+eexpect "syntax error"
+eexpect "1 row"
+eexpect "COMMIT"
+eexpect ":/# "
+send "echo \$?\r"
+eexpect "0\r\n:/# "
+
+send "exit 0\r"
+eexpect eof
+
+stop_server $argv
+
