e3f8e8557d0b16178723ea2a8a249c23724db8ab
Arjun Narayan <arjun@cockroachlabs.com>
2016-10-05 17:49:25 -0400

    storage: coalesce heartbeats
    Coalesce heartbeats and heartbeat responses bound for the same store
    into a single proto. Introduce a new environment flag (default: true
    to enable this in integration tests, will be changed to false before
    merging) COCKROACH_ENABLE_COALESCED_HEARTBEATS to turn this feature
    on. Added metrics and a graph in the admin UI to track the number of
    queued heartbeats waiting to be coalesced.

--- storage/metrics.go
+++ storage/metrics.go
@@ -136,11 +136,11 @@ var (
 	}
 	metaRaftRcvdHeartbeat = metric.Metadata{
 		Name: "raft.rcvd.heartbeat",
-		Help: "Total number of MsgHeartbeat messages received by this store",
+		Help: "Total number of (coalesced, if enabled) MsgHeartbeat messages received by this store",
 	}
 	metaRaftRcvdHeartbeatResp = metric.Metadata{
 		Name: "raft.rcvd.heartbeatresp",
-		Help: "Total number of MsgHeartbeatResp messages received by this store",
+		Help: "Total number of (coalesced, if enabled) MsgHeartbeatResp messages received by this store",
 	}
 	metaRaftRcvdTransferLeader = metric.Metadata{
 		Name: "raft.rcvd.transferleader",
@@ -154,9 +154,14 @@ var (
 		Name: "raft.rcvd.dropped",
 		Help: "Number of dropped incoming Raft messages",
 	}
-
-	metaRaftEnqueuedPending = metric.Metadata{Name: "raft.enqueued.pending",
-		Help: "Number of pending outgoing messages in the Raft Transport queue"}
+	metaRaftEnqueuedPending = metric.Metadata{
+		Name: "raft.enqueued.pending",
+		Help: "Number of pending outgoing messages in the Raft Transport queue",
+	}
+	metaRaftCoalescedHeartbeatsPending = metric.Metadata{
+		Name: "raft.heartbeats.pending",
+		Help: "Number of pending heartbeats and responses waiting to be coalesced",
+	}
 
 	// Replica queue metrics.
 	metaGCQueueSuccesses = metric.Metadata{Name: "queue.gc.process.success",
@@ -342,7 +347,8 @@ type StoreMetrics struct {
 	// TODO(arjun): eliminate this duplication.
 	raftRcvdMessages map[raftpb.MessageType]*metric.Counter
 
-	RaftEnqueuedPending *metric.Gauge
+	RaftEnqueuedPending            *metric.Gauge
+	RaftCoalescedHeartbeatsPending *metric.Gauge
 
 	// Replica queue metrics.
 	GCQueueSuccesses                *metric.Counter
@@ -486,6 +492,10 @@ func newStoreMetrics() *StoreMetrics {
 		raftRcvdMessages:          make(map[raftpb.MessageType]*metric.Counter, len(raftpb.MessageType_name)),
 
 		RaftEnqueuedPending: metric.NewGauge(metaRaftEnqueuedPending),
+
+		// This Gauge measures the number of heartbeats queued up just before
+		// the queue is cleared, to avoid flapping wildly.
+		RaftCoalescedHeartbeatsPending: metric.NewGauge(metaRaftCoalescedHeartbeatsPending),
 
 		// Replica queue metrics.
 		GCQueueSuccesses:                metric.NewCounter(metaGCQueueSuccesses),
