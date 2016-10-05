177a6e80be3da64deb180bd6dfb841a80c8df858
Tamir Duberstein <tamird@gmail.com>
2016-10-05 00:36:11 -0400

    build: produce race-enabled test binaries
    Also updates a bunch of scripts to run `*.test*` rather than `*.test`.
    
    Updates #8897.

--- build/build-race-binaries.sh
+++ build/build-race-binaries.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
-# Build cockroach binary with race detection enabled.
+# Build various statically linked binaries with race detection enabled.
+#
+# TODO(tamird): consolidate with build-static-binaries. We shouldn't have two
+# files that do the same thing.
 
 set -euo pipefail
 
+archive=$1
+
 source "$(dirname "${0}")"/build-common.sh
 
-time make STATIC=1 build GOFLAGS="-race"
-check_static cockroach
-strip -S cockroach
+time make STATIC=1 build GOFLAGS=-race SUFFIX=.race
+# Build test binaries. Note that the acceptance binary will not be included in
+# the archive, but is instead uploaded directly by push-aws.sh.
+time make STATIC=1 testbuild PKG=./... TAGS=acceptance GOFLAGS=-race SUFFIX=.race
+
+# We don't check all test binaries, but one from each invocation.
+check_static cockroach.race
+check_static cli/cli.test.race
+
+strip -S cockroach.race
+find . -type f -name '*.test*' -exec strip -S {} ';'
+
+rm -f "$archive"
+time tar cfz "$archive" -C .. $(git ls-files | sed -r 's,^,cockroach/,') $(find . -type f -name '*.test*' -and -not -name 'acceptance.test*' | sed 's,^\./,cockroach/,')
