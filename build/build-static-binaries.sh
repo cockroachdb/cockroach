c6c16a2564959e66a883f7a2c8cb43952fb73a2b
Tamir Duberstein <tamird@gmail.com>
2016-10-05 09:35:31 -0400

    build: remove detritus
    This used to be used for stdmalloc, but we no longer build that.

--- build/build-static-binaries.sh
+++ build/build-static-binaries.sh
@@ -4,12 +4,11 @@
 set -euo pipefail
 
 archive=$1
-tags=${2-}
 
 source "$(dirname "${0}")"/build-common.sh
 
 time make STATIC=1 build
-time make STATIC=1 testbuild PKG=./... TAGS="$tags acceptance"
+time make STATIC=1 testbuild PKG=./... TAGS=acceptance
 
 # We don't check all test binaries, but one from each invocation.
 check_static cockroach
