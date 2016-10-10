5b281ab40b6553edb62db8a0c3ae8254748f936e
David Taylor <tinystatemachine@gmail.com>
2016-10-10 17:48:33 +0000

    build: add rest of TC scripts to build

---
+++ build/teamcity-check.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+build/builder.sh make check 2>&1 | go-test-teamcity
+build/builder.sh go generate ./... 2>&1
+build/builder.sh /bin/bash -c '! git status --porcelain | read || (git status; git diff -a; exit 1)' 2>&1
+
+# If the code is new enough to have go generate not
+# run the ui tests, run the ui tests.
+if grep "make generate" ui/ui.go; then
+    build/builder.sh make -C ui;
+fi
