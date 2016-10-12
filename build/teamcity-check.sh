4559e4895fe8c6ab279a192d8e699bf1bc64ab4e
David Taylor <tinystatemachine@gmail.com>
2016-10-12 06:04:03 +0000

    *: add vendor submodule
    At a high level, this change adds a git submodule at vendor pointing to a new, standalone repo that contains a snapshot of all of our dependencies, then it calls git submodule update --init in the .bootstrap hook that currently maintains our dependencies.
    
    The standalone repo https://github.com/cockroachdb/vendored is currently generated with gvt, but could be generated with anything.
    
    Glock is still being run for now, to install the (vendored) tools we use into GOPATH/bin (though a followup could remove this, installing them to a local bin directory where check could find them, rather than relying on/clobbering the user's PATH).
    
    Updating dependencies would be a two step process: first run gvt (or whatever) to update the dependencies repo and push that ref, then send a pull request to the main repo bumping the submodule to point to the new ref.
    
    The .bootstrap hook doesn't currently run submodule sync, so that those that want to can update the remote url (e.g. to be able to push).

--- build/teamcity-check.sh
+++ build/teamcity-check.sh
@@ -2,7 +2,7 @@
 set -exuo pipefail
 build/builder.sh make check 2>&1 | go-test-teamcity
 build/builder.sh make gotestdashi 2>&1
-build/builder.sh go generate ./... 2>&1
+build/builder.sh go generate ./pkg/... 2>&1
 build/builder.sh /bin/bash -c '! git status --porcelain | read || (git status; git diff -a; exit 1)' 2>&1
 
 # If the code is new enough to have go generate not
