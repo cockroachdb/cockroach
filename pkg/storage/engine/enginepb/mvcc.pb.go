26cb2150e20bab11a3361fd583f400b5feb8cc36
Daniel Harrison <daniel.harrison@gmail.com>
2016-10-13 10:40:47 -0400

    engine: store timestamp bounds in RocksDB sstable user metadata
    RocksDB exposes a hook (TablePropertiesCollector) to allow adding metadata to
    each sstable as it's being built. Use this to keep track of the lowest and
    highest mvcc timestamp of the keys in each sstable. This will later be used to
    more efficiently implement incremental backups.
    
    I ran block_writer on a `roachdemo -n 1` cluster with and without this change
    for 10 minutes. Seems within the noise:
    
    Before 600.0s: 2639887 total 4399.8/sec
    BenchmarkBlockWriter     2639889        227283.2 ns/op
    
    After 600.0s: 2652571 total 4420.9/sec
    BenchmarkBlockWriter     2652573        226196.6 ns/op
    
    For #9293.

--- pkg/storage/engine/enginepb/mvcc.pb.go
+++ pkg/storage/engine/enginepb/mvcc.pb.go
@@ -7,11 +7,14 @@
 
 	It is generated from these files:
 		cockroach/pkg/storage/engine/enginepb/mvcc.proto
+		cockroach/pkg/storage/engine/enginepb/rocksdb.proto
 
 	It has these top-level messages:
 		TxnMeta
 		MVCCMetadata
 		MVCCStats
+		SSTUserProperties
+		SSTUserPropertiesCollection
 */
 package enginepb
 
