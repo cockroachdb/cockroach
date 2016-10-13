6aa995917d0539a1beba482820e35c26838be5db
David Taylor <tinystatemachine@gmail.com>
2016-10-13 02:50:51 +0000

    util/caller: strip pkg/ prefixes
    Fixes #9930
    
    Downside is that this means path/to/file.go:123 is going to be missing pkg and thus not clickable
    in an iTerm session in the root.

--- pkg/util/caller/resolver_test.go
+++ pkg/util/caller/resolver_test.go
@@ -54,7 +54,7 @@ func TestDefaultCallResolver(t *testing.T) {
 			t.Fatalf("unexpected caller reported: %s", fun)
 		}
 
-		if file != filepath.Join("pkg", "util", "caller", "resolver_test.go") {
+		if file != filepath.Join("util", "caller", "resolver_test.go") {
 			t.Fatalf("wrong file '%s'", file)
 		}
 	}
