2ab81db41dd72e6b858a681b45a94956cc190ff8
Tamir Duberstein <tamird@gmail.com>
2016-10-18 14:40:01 -0400

    build: stress{,-meta} scripts
    Should've checked this in along with the addition of the binaries to
    the GLOCKFILE.

---
+++ build/teamcity-stress.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+mkdir artifacts
+
+exit_status=0
+build/builder.sh make stress PKG=${PKG} GOFLAGS=${GOFLAGS-} TESTTIMEOUT=0 TESTFLAGS='-test.v' STRESSFLAGS='-maxtime 15m -maxfails 1 -stderr' 2>&1 | tee artifacts/stress.log || exit_status=$?
+
+if [ $exit_status -ne 0 ]; then
+  post-github < artifacts/stress.log
+fi;
+
+exit $exit_status
