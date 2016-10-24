bf0cd94295d0596398db00b40c64c55308e6b67e
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2016-10-24 15:22:25 -0400

    acceptance: clean up logging
    Do not create log directories unless asked.
    On TeamCity, put the logs in the artifacts directory (this got lost
    during the `pkg` transition). Noticed during triage of #10056.

--- build/teamcity-acceptance.sh
+++ build/teamcity-acceptance.sh
@@ -4,4 +4,4 @@ build/builder.sh make build
 build/builder.sh make install
 build/builder.sh go test -v -c -tags acceptance ./pkg/acceptance
 cd pkg/acceptance
-../../acceptance.test -nodes 3 -l artifacts/acceptance -test.v -test.timeout 10m 2>&1 | go-test-teamcity
+../../acceptance.test -nodes 3 -l ../../artifacts/acceptance -test.v -test.timeout 10m 2>&1 | go-test-teamcity
