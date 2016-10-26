fd60277b2fdf5664068240a852435653e8713468
Matt Jibson <matt.jibson@gmail.com>
2016-10-26 14:41:38 -0400

    sql: fix LOG of very large numbers
    Previously a LOG of a large number would result in the server consuming
    all CPU and hanging. This is because our LOG implementation would take 1/x
    at a precision low enough that the division of the large x would result
    in a 0. The algorithm would proceed to double it until it reached 0.5,
    which obviously would never succeed. Fix this by using a high enough
    precision to prevent 0 from being returned.
    
    During the investigation of this bug, it was also discovered that if a
    large number was near the vicinity of producing a 0, it would incorrectly
    truncate some precision of the division, thus producing slightly incorrect
    results to the LOG function.
    
    This thus fixes various bugs and hangs in the POW, LOG, and LN builtins.
    
    Fixes current RSG test failures.
    
    Benchmark shows no change:
    
    name          old time/op  new time/op  delta
    DecimalLog-4   106µs ± 3%   105µs ± 1%  -1.26%  (p=0.029 n=10+10)
    DecimalExp-4  18.6ms ±17%  18.4ms ± 8%    ~     (p=0.897 n=10+8)
    DecimalPow-4  1.25ms ±18%  1.25ms ±21%    ~     (p=0.853 n=10+10)

--- pkg/util/decimal/table.go
+++ pkg/util/decimal/table.go
@@ -82,3 +82,62 @@ func PowerOfTenInt(pow int) *big.Int {
 	}
 	return dec.UnscaledBig()
 }
+
+// digitsLookupTable is used to map binary digit counts to their corresponding
+// decimal border values. The map relies on the proof that (without leading zeros)
+// for any given number of binary digits r, such that the number represented is
+// between 2^r and 2^(r+1)-1, there are only two possible decimal digit counts
+// k and k+1 that the binary r digits could be representing.
+//
+// Using this proof, for a given digit count, the map will return the lower number
+// of decimal digits (k) the binary digit count could represent, along with the
+// value of the border between the two decimal digit counts (10^k).
+const digitsTableSize = 128
+
+var digitsLookupTable [digitsTableSize + 1]tableVal
+
+type tableVal struct {
+	digits int
+	border big.Int
+}
+
+func init() {
+	curVal := big.NewInt(1)
+	curExp := new(big.Int)
+	for i := 1; i <= digitsTableSize; i++ {
+		if i > 1 {
+			curVal.Lsh(curVal, 1)
+		}
+
+		elem := &digitsLookupTable[i]
+		elem.digits = len(curVal.String())
+
+		elem.border.SetInt64(10)
+		curExp.SetInt64(int64(elem.digits))
+		elem.border.Exp(&elem.border, curExp, nil)
+	}
+}
+
+func lookupBits(bitLen int) (tableVal, bool) {
+	if bitLen > 0 && bitLen < len(digitsLookupTable) {
+		return digitsLookupTable[bitLen], true
+	}
+	return tableVal{}, false
+}
+
+// NumDigits returns the number of decimal digits that make up
+// big.Int value. The function first attempts to look this digit
+// count up in the digitsLookupTable. If the value is not there,
+// it defaults to constructing a string value for the big.Int and
+// using this to determine the number of digits. If a string value
+// is constructed, it will be returned so it can be used again.
+func NumDigits(bi *big.Int, tmp []byte) (int, []byte) {
+	if val, ok := lookupBits(bi.BitLen()); ok {
+		if bi.Cmp(&val.border) < 0 {
+			return val.digits, nil
+		}
+		return val.digits + 1, nil
+	}
+	bs := bi.Append(tmp, 10)
+	return len(bs), bs
+}
