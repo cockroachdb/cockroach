b3e8a8fefd4edcf23bbe181237dc45734b45a3c6
Tamir Duberstein <tamird@gmail.com>
2016-10-26 10:47:33 -0400

    storage/engine: correct MVCCGetProto doc comment

--- pkg/storage/engine/mvcc.go
+++ pkg/storage/engine/mvcc.go
@@ -441,12 +441,9 @@ func MVCCSetRangeStats(
 	return MVCCPutProto(ctx, engine, nil, keys.RangeStatsKey(rangeID), hlc.ZeroTimestamp, nil, ms)
 }
 
-// MVCCGetProto fetches the value at the specified key and unmarshals
-// it using a protobuf decoder. Returns true on success or false if
-// the key was not found. In the event of a WriteIntentError when
-// consistent=false, we return the error and the decoded result; for
-// all other errors (or when consistent=true) the decoded value is
-// invalid.
+// MVCCGetProto fetches the value at the specified key and unmarshals it into
+// msg if msg is non-nil. Returns true on success or false if the key was not
+// found. The semantics of consistent are the same as in MVCCGet.
 func MVCCGetProto(
 	ctx context.Context,
 	engine Reader,
