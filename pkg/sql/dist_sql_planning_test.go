e17cf47ccfca0e887b785feb2bd514ade8f9db1c
Radu Berinde <radu@cockroachlabs.com>
2016-10-27 13:33:06 -0400

    distsql: introduce the distSQL planner
    A basic physical planner for distSQL. The physical plan is derived from a
    planNode tree; only a small subset of trees is supported.
    
    The "hybrid tree" code (distSQLNode) is removed.

---
+++ pkg/sql/dist_sql_planning_test.go
@@ -0,0 +1,52 @@
+// Copyright 2016 The Cockroach Authors.
+
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// Author: Radu Berinde (radu@cockroachlabs.com)
+
+package sql
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+)
+
+func TestDistSQLPlanner(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	args := base.TestClusterArgs{ReplicationMode: base.ReplicationManual}
+	tc := serverutils.StartTestCluster(t, 1, args)
+	defer tc.Stopper().Stop()
+
+	sqlutils.CreateTable(
+		t, tc.ServerConn(0), "t",
+		"num INT PRIMARY KEY, str STRING",
+		3, sqlutils.ToRowFn(sqlutils.RowIdxFn, sqlutils.RowEnglishFn),
+	)
+
+	r := sqlutils.MakeSQLRunner(t, tc.ServerConn(0))
+	r.Exec("SET DIST_SQL = ALWAYS")
+	r.CheckQueryResults(
+		"SELECT * FROM test.t",
+		[][]string{
+			{"1", "one"},
+			{"2", "two"},
+			{"3", "three"},
+		},
+	)
+}
