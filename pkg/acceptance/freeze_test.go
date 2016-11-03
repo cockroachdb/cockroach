8496012d160ac97ab4e20c7bc90389ab6afc4e02
Tamir Duberstein <tamird@gmail.com>
2016-11-03 15:26:22 -0400

    acceptance: check cluster consistency in chaosMonkey
    This should be safe now that the following PRs have been merged:
    - https://github.com/grpc/grpc-go/pull/818
    - https://github.com/grpc/grpc-go/pull/864
    
    Also remove a TODO about creating the client outside of a retry loop;
    creation of these clients during node startup is inherently prone to
    non-temporary connection refused errors.

--- pkg/acceptance/freeze_test.go
+++ pkg/acceptance/freeze_test.go
@@ -152,14 +152,6 @@ func testFreezeClusterInner(t *testing.T, c cluster.Cluster, cfg cluster.TestCon
 			t.Fatal(err)
 		}
 
-		// TODO(tschottdorf): moving the client creation outside of the retry
-		// loop will break the test with the following message:
-		//
-		//   client/rpc_sender.go:61: roachpb.Batch RPC failed as client
-		//   connection was closed
-		//
-		// Perhaps the cluster updates the address too late after restarting
-		// the node.
 		db, dbStopper := c.NewClient(t, 0)
 		defer dbStopper.Stop()
 
