ca5a9565950b36330124866fb4382e6bb536b4e6
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2016-11-04 15:06:17 -0400

    build: plumb COCKROACH_PROPOSER_EVALUATED_KV env var
    This should make it easy to run a nightly build for which tests are run using
    proposer-evaluated KV.
    
    Updates #10454.

--- build/teamcity-testrace.sh
+++ build/teamcity-testrace.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-build/builder.sh make testrace TESTFLAGS='-v' 2>&1 | go-test-teamcity
+build/builder.sh make testrace COCKROACH_PROPOSER_EVALUATED_KV="${COCKROACH_PROPOSER_EVALUATED_KV-}" TESTFLAGS='-v' 2>&1 | go-test-teamcity
 
 build/builder.sh env BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" TARGET=stressrace github-pull-request-make
