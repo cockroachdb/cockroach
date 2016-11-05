297423c087255e611b3cf437c714d7bb109ddc06
Max Lang <max@cockroachlabs.com>
2016-11-05 02:22:33 -0400

    ui: inital nav changes
    Subset of Q4 UI refresh changes
    - Changes the look of the top and side nav
    - Adds a select component that interacts with the URL
    - Removes the subnav from the nodes list page
    - The graphgroup and nodegraphs components now choose what to
    display based on the URL query string.
    
    A dependency was added on react-select and react-router was
    updated to version 3.

---
+++ pkg/ui/app/components/selector.tsx
@@ -0,0 +1,77 @@
+import _ from "lodash";
+import Select from "react-select";
+import * as React from "react";
+import { IInjectedProps, IRouter } from "react-router";
+
+export interface SelectorOption {
+  value: string;
+  label: string;
+  default?: boolean;
+}
+
+interface SelectorOwnProps {
+  urlKey: string; // The URL key used to retrieve/save the state.
+  options: SelectorOption[];
+  onChange?: (selected: SelectorOption) => void; // Callback when the value changes.
+}
+
+class SelectorState {
+  selected: number;
+}
+
+/**
+ * Selector component that uses the URL query string for state.
+ */
+export default class Selector extends React.Component<SelectorOwnProps, SelectorState> {
+  // Magic to add react router to the context.
+  // See https://github.com/ReactTraining/react-router/issues/975
+  static contextTypes = {
+    router: React.PropTypes.object.isRequired,
+  };
+  context: { router?: IRouter & IInjectedProps; };
+
+  state = new SelectorState();
+
+  componentWillMount() {
+    this.propsChange();
+  }
+
+  componentWillReceiveProps(props: SelectorOwnProps) {
+    this.propsChange(props);
+  }
+
+  propsChange(props = this.props) {
+    let selected: number;
+    let query: any = this.context.router.location.query;
+    // If there are no options, wait for options to be populated.
+    if (!props.options || !props.options.length) {
+      return;
+    // If a value is specified in the URL, set that value
+    } else if (_.has(query, props.urlKey)) {
+      selected = _.findIndex(props.options, { value: query[props.urlKey] });
+    // Otherwise use the default value or, barring that, the first value.
+    } else {
+      selected = _.findIndex(props.options, { default: true });
+      if (selected === -1) {
+        selected = 0;
+      }
+    }
+    if (this.state.selected !== selected ) {
+      this.setState({ selected });
+      this.props.onChange(props.options[selected]);
+    }
+  }
+
+  onChange = (selected: SelectorOption) => {
+    let location = _.clone(this.context.router.location);
+    (location.query as any)[this.props.urlKey] = selected.value;
+    this.context.router.push(location);
+    this.props.onChange(selected);
+  }
+
+  render() {
+    let selected = this.props.options && _.isNumber(this.state.selected) && this.props.options[this.state.selected] || null;
+    let options = this.props.options || [];
+    return <Select clearable={false} options={options} value={selected} onChange={this.onChange} />;
+  }
+}
