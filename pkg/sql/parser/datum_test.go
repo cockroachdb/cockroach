df2d4b2eae2b0bbf6aecc2e2a3d737db2b59379f
Raphael 'kena' Poss <knz@cockroachlabs.com>
2016-11-05 11:49:51 +0000

    sql: fix end key selection with tuples
    Prior to this patch, queries using ordering predicates (<, >, <=, >=)
    and indices across multiple columns where some columns have no
    Prev/Next values (such as STRING, BYTES) would return incorrect
    results. This patch corrects this situation.
    
    Thanks to @eisenstatdavid for uncovering this.
    
    This patch also incidentally adds some new functionality
    and removes a few bug:
    
    - new feature: unary negation now works with INTERVAL too.
    
    - new feature: input string syntax for intervals now supports
      nanoseconds and microseconds.
    
    - bug fix: the "next" and "previous" operators used internally for
      timestamps was incorrectly using nanosecond precision, whereas only
      microseconds are supported since
      d5a3660955441a2ad1a7af088f4c9ef65272391a.
    
    - bug fix: the upper and lower bounds to compare for float values are
      +/- infinity.

--- pkg/sql/parser/datum_test.go
+++ pkg/sql/parser/datum_test.go
@@ -21,6 +21,269 @@ import (
 	"testing"
 )
 
+func prepareExpr(t *testing.T, datumExpr string) TypedExpr {
+	expr, err := ParseExprTraditional(datumExpr)
+	if err != nil {
+		t.Fatalf("%s: %v", datumExpr, err)
+	}
+	// Type checking ensures constant folding is performed and type
+	// annotations have come into effect.
+	typedExpr, err := TypeCheck(expr, nil, NoTypePreference)
+	if err != nil {
+		t.Fatalf("%s: %v", datumExpr, err)
+	}
+	// Normalization ensures that casts are processed.
+	ctx := &EvalContext{}
+	typedExpr, err = ctx.NormalizeExpr(typedExpr)
+	if err != nil {
+		t.Fatalf("%s: %v", datumExpr, err)
+	}
+	return typedExpr
+}
+
+func TestDatumOrdering(t *testing.T) {
+	testData := []struct {
+		datumExpr string
+		prev      string
+		next      string
+		min       string
+		max       string
+	}{
+		// Integers
+		{`1`, `0`, `2`, `-9223372036854775808`, `9223372036854775807`},
+		{`-9223372036854775808`, `min`, `-9223372036854775807`, `-9223372036854775808`, `9223372036854775807`},
+		{`9223372036854775807`, `9223372036854775806`, `max`, `-9223372036854775808`, `9223372036854775807`},
+
+		// Boolean
+		{`true`, `false`, `max`, `false`, `true`},
+		{`false`, `min`, `true`, `false`, `true`},
+
+		// Floats
+		{`3.14:::float`, `3.1399999999999997`, `3.1400000000000006`, `-Inf`, `+Inf`},
+		{`9.223372036854776e+18:::float`, `9.223372036854775e+18`, `9.223372036854778e+18`, `-Inf`, `+Inf`},
+		{`-(1:::float/0)`, `min`, `-1.7976931348623157e+308`, `-Inf`, `+Inf`},
+		{`(1:::float/0)`, `1.7976931348623157e+308`, `max`, `-Inf`, `+Inf`},
+		{`-1.7976931348623157e+308:::float`, `-Inf`, `-1.7976931348623155e+308`, `-Inf`, `+Inf`},
+		{`1.7976931348623157e+308:::float`, `1.7976931348623155e+308`, `+Inf`, `-Inf`, `+Inf`},
+
+		// Decimal
+		{`1.0:::decimal`, ``, ``, ``, ``},
+
+		// Strings and byte arrays
+		{`'':::string`, `min`, `e'\x00'`, `''`, ``},
+		{`e'\x00'`, ``, `e'\x00\x00'`, `''`, ``},
+		{`'abc':::string`, ``, `e'abc\x00'`, `''`, ``},
+		{`'':::bytes`, `min`, `b'\x00'`, `b''`, ``},
+		{`'abc':::bytes`, ``, `b'abc\x00'`, `b''`, ``},
+
+		// Dates
+		{`'2006-01-02':::date`, `2006-01-01`, `2006-01-03`, ``, ``},
+		{`'0000-01-01':::date`, `-0001-12-31`, `0000-01-02`, ``, ``},
+		{`'4000-01-01':::date`, `3999-12-31`, `4000-01-02`, ``, ``},
+		{`'2006-01-02 03:04:05.123123':::timestamp`,
+			`2006-01-02 03:04:05.123122+00:00`, `2006-01-02 03:04:05.123124+00:00`, ``, ``},
+
+		// Intervals
+		{`'1 day':::interval`, ``, ``,
+			`INTERVAL '-9223372036854775808m-9223372036854775808d-2562047h47m16.854775808s'`,
+			`INTERVAL '9223372036854775807m9223372036854775807d2562047h47m16.854775807s'`},
+		// Max interval: we use Postgres syntax, because Go doesn't accept
+		// months/days and ISO8601 doesn't accept nanoseconds.
+		{`'9223372036854775807 months 9223372036854775807 days ` +
+			`2562047 hours 47 minutes 16 seconds 854775807 nanoseconds':::interval`,
+			``, `max`,
+			`INTERVAL '-9223372036854775808m-9223372036854775808d-2562047h47m16.854775808s'`,
+			`INTERVAL '9223372036854775807m9223372036854775807d2562047h47m16.854775807s'`},
+		// It's hard to generate a minimum interval! We can't use a
+		// negative value inside the string constant, because that's not
+		// allowed in a Postgres duration specification. We can't use the
+		// full positive value (9223372036854775808) and then negate that
+		// after parsing, because that would overflow. So we generate
+		// first a slightly smaller absolute value by conversion, negate,
+		// then add the missing bits.
+		{`-'9223372036854775807 months 9223372036854775807 days ` +
+			`2562047 hours 47 minutes 16 seconds':::interval` +
+			`-'1 month 1 day 854775808 nanoseconds':::interval`,
+			`min`, ``,
+			`INTERVAL '-9223372036854775808m-9223372036854775808d-2562047h47m16.854775808s'`,
+			`INTERVAL '9223372036854775807m9223372036854775807d2562047h47m16.854775807s'`},
+
+		// NULL
+		{`NULL`, `min`, `max`, `NULL`, `NULL`},
+
+		// Tuples
+		{`row()`, `min`, `max`, `()`, `()`},
+
+		{`row(NULL)`, `min`, `max`, `(NULL)`, `(NULL)`},
+
+		{`row(true)`, `(false)`, `max`, `(false)`, `(true)`},
+		{`row(false)`, `min`, `(true)`, `(false)`, `(true)`},
+
+		{`row(true, false, false)`, `(false, true, true)`, `(true, false, true)`,
+			`(false, false, false)`, `(true, true, true)`},
+		{`row(false, true, true)`, `(false, true, false)`, `(true, false, false)`,
+			`(false, false, false)`, `(true, true, true)`},
+
+		{`row(0, 0)`, `(0, -1)`, `(0, 1)`,
+			`(-9223372036854775808, -9223372036854775808)`,
+			`(9223372036854775807, 9223372036854775807)`},
+
+		{`row(0, 9223372036854775807)`,
+			`(0, 9223372036854775806)`, `(1, -9223372036854775808)`,
+			`(-9223372036854775808, -9223372036854775808)`,
+			`(9223372036854775807, 9223372036854775807)`},
+		{`row(9223372036854775807, 9223372036854775807)`,
+			`(9223372036854775807, 9223372036854775806)`, `max`,
+			`(-9223372036854775808, -9223372036854775808)`,
+			`(9223372036854775807, 9223372036854775807)`},
+
+		{`row(0, 0:::decimal)`, ``, ``, ``, ``},
+		{`row(0:::decimal, 0)`, `(0, -1)`, `(0, 1)`, ``, ``},
+
+		{`row(10, '')`, ``, `(10, e'\x00')`,
+			`(-9223372036854775808, '')`, ``},
+		{`row(-9223372036854775808, '')`, `min`, `(-9223372036854775808, e'\x00')`,
+			`(-9223372036854775808, '')`, ``},
+		{`row(-9223372036854775808, 'abc')`, ``, `(-9223372036854775808, e'abc\x00')`,
+			`(-9223372036854775808, '')`, ``},
+
+		{`row(10, NULL)`, `(9, NULL)`, `(11, NULL)`,
+			`(-9223372036854775808, NULL)`, `(9223372036854775807, NULL)`},
+		{`row(NULL, 10)`, `(NULL, 9)`, `(NULL, 11)`,
+			`(NULL, -9223372036854775808)`, `(NULL, 9223372036854775807)`},
+
+		{`row(true, NULL, false)`, `(false, NULL, true)`, `(true, NULL, true)`,
+			`(false, NULL, false)`, `(true, NULL, true)`},
+		{`row(false, NULL, true)`, `(false, NULL, false)`, `(true, NULL, false)`,
+			`(false, NULL, false)`, `(true, NULL, true)`},
+
+		{`row(row(true), row(false))`, `((false), (true))`, `((true), (true))`,
+			`((false), (false))`, `((true), (true))`},
+		{`row(row(false), row(true))`, `((false), (false))`, `((true), (false))`,
+			`((false), (false))`, `((true), (true))`},
+
+		// Arrays
+
+		// TODO(nathan) Until we support literals for empty arrays, this
+		// is the easiest way to construct one.
+		{`current_schemas(false)`, `min`, `max`, `{}`, `{}`},
+
+		{`array[NULL]`, `min`, `max`, `{NULL}`, `{NULL}`},
+
+		{`array[true]`, `{false}`, `max`, `{false}`, `{true}`},
+		{`array[false]`, `min`, `{true}`, `{false}`, `{true}`},
+
+		{`array[true, false, false]`, `{false,true,true}`, `{true,false,true}`,
+			`{false,false,false}`, `{true,true,true}`},
+		{`array[false, true, true]`, `{false,true,false}`, `{true,false,false}`,
+			`{false,false,false}`, `{true,true,true}`},
+
+		{`array[0, 0]`, `{0,-1}`, `{0,1}`,
+			`{-9223372036854775808,-9223372036854775808}`,
+			`{9223372036854775807,9223372036854775807}`},
+
+		{`array[0, 9223372036854775807]`,
+			`{0,9223372036854775806}`, `{1,-9223372036854775808}`,
+			`{-9223372036854775808,-9223372036854775808}`,
+			`{9223372036854775807,9223372036854775807}`},
+		{`array[9223372036854775807, 9223372036854775807]`,
+			`{9223372036854775807,9223372036854775806}`, `max`,
+			`{-9223372036854775808,-9223372036854775808}`,
+			`{9223372036854775807,9223372036854775807}`},
+
+		{`array[true, NULL, false]`, `{false,NULL,true}`, `{true,NULL,true}`,
+			`{false,NULL,false}`, `{true,NULL,true}`},
+		{`array[false, NULL, true]`, `{false,NULL,false}`, `{true,NULL,false}`,
+			`{false,NULL,false}`, `{true,NULL,true}`},
+
+		{`array[true]`, `{false}`, `max`, `{false}`, `{true}`},
+		{`array[false]`, `min`, `{true}`, `{false}`, `{true}`},
+
+		// Multi-dim arrays.
+		{`ARRAY[ARRAY[true], ARRAY[false]]`, `{{false},{true}}`, `{{true},{true}}`,
+			`{{false},{false}}`, `{{true},{true}}`},
+		{`ARRAY[ARRAY[false], ARRAY[true]]`, `{{false},{false}}`, `{{true},{false}}`,
+			`{{false},{false}}`, `{{true},{true}}`},
+
+		// Mixed tuple/array datums.
+		{`row(ARRAY[true], row(false))`, `({false}, (true))`, `({true}, (true))`,
+			`({false}, (false))`, `({true}, (true))`},
+		{`row(row(false), ARRAY[true])`, `((false), {false})`, `((true), {false})`,
+			`((false), {false})`, `((true), {true})`},
+	}
+	for _, td := range testData {
+		expr := prepareExpr(t, td.datumExpr)
+
+		d := expr.(Datum)
+		if td.prev == "" {
+			if d.HasPrev() {
+				if !d.IsMin() {
+					t.Errorf("%s: HasPrev() true but IsMin() false, expected !HasPrev() or IsMin() true", td.datumExpr)
+				}
+			}
+		} else {
+			if !d.HasPrev() && td.prev != "min" {
+				t.Errorf("%s: HasPrev(): got false, expected true", td.datumExpr)
+				continue
+			}
+			isMin := d.IsMin()
+			if isMin != (td.prev == "min") {
+				t.Errorf("%s: IsMin() %v, expected %v", td.datumExpr, isMin, (td.prev == "min"))
+				continue
+			}
+			if !isMin {
+				dPrev := d.Prev().String()
+				if dPrev != td.prev {
+					t.Errorf("%s: Prev(): got %s, expected %s", td.datumExpr, dPrev, td.prev)
+				}
+			}
+		}
+		if td.next == "" {
+			if d.HasNext() {
+				if !d.IsMax() {
+					t.Errorf("%s: HasNext() true but IsMax() false, expected (!HasNext() || IsMax())", td.datumExpr)
+				}
+			}
+		} else {
+			if !d.HasNext() && td.next != "max" {
+				t.Errorf("%s: HasNext(): got false, expected true", td.datumExpr)
+				continue
+			}
+			isMax := d.IsMax()
+			if isMax != (td.next == "max") {
+				t.Errorf("%s: IsMax() %v, expected %v", td.datumExpr, isMax, (td.next == "max"))
+				continue
+			}
+			if !isMax {
+				dNext := d.Next().String()
+				if dNext != td.next {
+					t.Errorf("%s: Next(): got %s, expected %s", td.datumExpr, dNext, td.next)
+				}
+			}
+		}
+		if td.min == "" {
+			if d.hasMin() {
+				t.Errorf("%s: hasMin() true, expected false", td.datumExpr)
+			}
+		} else {
+			dMin := d.min().String()
+			if dMin != td.min {
+				t.Errorf("%s: Min(): got %s, expected %s", td.datumExpr, dMin, td.min)
+			}
+		}
+		if td.max == "" {
+			if d.hasMax() {
+				t.Errorf("%s: hasMax() true, expected false", td.datumExpr)
+			}
+		} else {
+			dMax := d.max().String()
+			if dMax != td.max {
+				t.Errorf("%s: Max(): got %s, expected %s", td.datumExpr, dMax, td.max)
+			}
+		}
+	}
+}
+
 func TestDFloatCompare(t *testing.T) {
 	values := []Datum{DNull}
 	for _, x := range []float64{math.NaN(), math.Inf(-1), -1, 0, 1, math.Inf(1)} {
