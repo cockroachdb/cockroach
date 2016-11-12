4b311bfc52ee1a538d945367ccb3739957e4d04b
Radu Berinde <radu.berinde@gmail.com>
2016-11-12 09:57:26 -0500

    parser: add an IndexedVar formatting interceptor to FmtFlags
    A bit of infrastructure to allow us "custom" formatting for IndexedVars. This
    will be used when serializing expressions for distsql.

--- pkg/sql/parser/indexed_vars_test.go
+++ pkg/sql/parser/indexed_vars_test.go
@@ -71,6 +71,20 @@ func TestIndexedVars(t *testing.T) {
 		t.Errorf("invalid expression string '%s', expected '%s'", str, expectedStr)
 	}
 
+	// Test formatting using the indexed var format interceptor.
+	var buf bytes.Buffer
+	typedExpr.Format(
+		&buf,
+		FmtIndexedVarFormat(func(buf *bytes.Buffer, _ FmtFlags, _ IndexedVarContainer, idx int) {
+			fmt.Fprintf(buf, "customVar%d", idx)
+		}),
+	)
+	str = buf.String()
+	expectedStr = "customVar0 + (customVar1 * customVar2)"
+	if str != expectedStr {
+		t.Errorf("invalid expression string '%s', expected '%s'", str, expectedStr)
+	}
+
 	typ := typedExpr.ResolvedType()
 	if !typ.Equal(TypeInt) {
 		t.Errorf("invalid expression type %s", typ)
