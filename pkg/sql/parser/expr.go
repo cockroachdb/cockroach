2017be7c7e2402b89f030e0e3577b83aae1305be
Radu Berinde <radu@cockroachlabs.com>
2016-11-15 09:44:58 -0500

    distsql: verify expressions for unsupported stuff
    Verify expressions don't contain subqueries or context-dependent casts or
    functions. For the latter, we add a `ctxDependent` property for builtins.

--- pkg/sql/parser/expr.go
+++ pkg/sql/parser/expr.go
@@ -834,6 +834,12 @@ func (node *FuncExpr) IsImpure() bool {
 	return node.fn.impure
 }
 
+// IsContextDependent returns whether the function depends on data stored in the
+// EvalContext.
+func (node *FuncExpr) IsContextDependent() bool {
+	return node.fn.ContextDependent()
+}
+
 type funcType int
 
 // FuncExpr.Type
