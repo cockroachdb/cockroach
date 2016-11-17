141b64ceaa3eeec2a8b8550029a3e1211c476fa8
Tamir Duberstein <tamird@gmail.com>
2016-11-17 07:16:03 -0500

    build: plumb TC_SERVER_URL
    This is pretty.

--- build/teamcity-stress-meta.sh
+++ build/teamcity-stress-meta.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-build/builder.sh env TC_API_USER="$TC_API_USER" TC_API_PASSWORD="$TC_API_PASSWORD" teamcity-trigger
+build/builder.sh env TC_API_USER="$TC_API_USER" TC_API_PASSWORD="$TC_API_PASSWORD" TC_SERVER_URL="$TC_SERVER_URL" teamcity-trigger
