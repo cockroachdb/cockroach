9d9b258714cc649ff23df4e61875faef79c91154
Matt Tracy <matt@cockroachlabs.com>
2016-11-21 18:07:54 -0500

    ui: Refactor Dropdown, add to Databases page
    Replaces the tab navigation on the databases page with a dropdown selector, in
    accordance with current designs.
    
    In order to re-use the same dropdown from the cluster page, that component was
    also refactored. Previously, it held state and interacted directly with the URL
    query string. It has now been modified to be stateless and does not interact
    with the route.
    
    The cluster page no longer uses query parameters to encode the currently
    selected dashboard or node; instead, path components are used. The same strategy
    is used on the databases page.

--- pkg/ui/app/components/selector.tsx
+++ pkg/ui/app/components/selector.tsx
@@ -1,81 +1,27 @@
-import _ from "lodash";
 import Select from "react-select";
 import * as React from "react";
-import { IInjectedProps, IRouter } from "react-router";
 
 export interface SelectorOption {
   value: string;
   label: string;
-  default?: boolean;
 }
 
 interface SelectorOwnProps {
   title: string;
-  urlKey: string; // The URL key used to retrieve/save the state.
+  selected: string;
   options: SelectorOption[];
   onChange?: (selected: SelectorOption) => void; // Callback when the value changes.
-}
-
-class SelectorState {
-  selected: number;
 }
 
 /**
  * Selector component that uses the URL query string for state.
  */
-export default class Selector extends React.Component<SelectorOwnProps, SelectorState> {
-  // Magic to add react router to the context.
-  // See https://github.com/ReactTraining/react-router/issues/975
-  static contextTypes = {
-    router: React.PropTypes.object.isRequired,
-  };
-  context: { router?: IRouter & IInjectedProps; };
-
-  state = new SelectorState();
-
-  componentWillMount() {
-    this.propsChange();
-  }
-
-  componentWillReceiveProps(props: SelectorOwnProps) {
-    this.propsChange(props);
-  }
-
-  propsChange(props = this.props) {
-    let selected: number;
-    let query: any = this.context.router.location.query;
-    // If there are no options, wait for options to be populated.
-    if (!props.options || !props.options.length) {
-      return;
-    // If a value is specified in the URL, set that value
-    } else if (_.has(query, props.urlKey)) {
-      selected = _.findIndex(props.options, { value: query[props.urlKey] });
-    // Otherwise use the default value or, barring that, the first value.
-    } else {
-      selected = _.findIndex(props.options, { default: true });
-      if (selected === -1) {
-        selected = 0;
-      }
-    }
-    if (this.state.selected !== selected ) {
-      this.setState({ selected });
-      this.props.onChange(props.options[selected]);
-    }
-  }
-
-  onChange = (selected: SelectorOption) => {
-    let location = _.clone(this.context.router.location);
-    (location.query as any)[this.props.urlKey] = selected.value;
-    this.context.router.push(location);
-    this.props.onChange(selected);
-  }
-
+export default class Selector extends React.Component<SelectorOwnProps, {}> {
   render() {
-    let selected = this.props.options && _.isNumber(this.state.selected) && this.props.options[this.state.selected] || null;
-    let options = this.props.options || [];
+    let {selected, options, onChange} = this.props;
     return <div className="dropdown-option">
       <span className="dropdown-option__title">{ this.props.title }:</span>
-      <Select className="dropdown-option__select" clearable={false} options={options} value={selected} onChange={this.onChange} />
+      <Select className="dropdown-option__select" clearable={false} options={options} value={selected} onChange={onChange} />
     </div>;
   }
 }
