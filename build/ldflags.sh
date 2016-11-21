42565bc0237d23968b3f010b257e337b0bf69415
marc <marc@cockroachlabs.com>
2016-11-21 16:01:55 +0100

    Tweak version in binary.
    If we're exactly at a tag, use that, otherwise, use the current sha.
    
    The current version always uses the most recent tag + the number of
    commits since. eg: beta-20161110-295-gd75dd1a

--- build/ldflags.sh
+++ build/ldflags.sh
@@ -5,6 +5,6 @@ set -eu
 cd "$(dirname "${0}")"/..
 
 # The build.utcTime format must remain in sync with TimeFormat in info.go.
-echo '-X "github.com/cockroachdb/cockroach/pkg/build.tag='$(git describe --dirty --tags)'"' \
+echo '-X "github.com/cockroachdb/cockroach/pkg/build.tag='$(git describe --tags --exact-match 2> /dev/null || git rev-parse --short HEAD)'"' \
      '-X "github.com/cockroachdb/cockroach/pkg/build.utcTime='$(date -u '+%Y/%m/%d %H:%M:%S')'"' \
      '-X "github.com/cockroachdb/cockroach/pkg/build.deps='$(build/depvers.sh)'"'
