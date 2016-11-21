683f8f27bfd5fa389bd484a0b975f1f69dca1db8
marc <marc@cockroachlabs.com>
2016-11-21 16:01:55 +0100

    Tweak version in binary.
    If we're exactly at a tag, use that, otherwise, use the current sha.
    Append "-dirty" if we have uncommitted changes.
    
    Version displayed is now:
    ```
    Dirty: 42565bc-dirty
    Committed: a7a7850
    Beta tag: beta-20161027
    ```
    
    The current version always uses the most recent tag + the number of
    commits since. eg: beta-20161110-295-gd75dd1a

--- build/ldflags.sh
+++ build/ldflags.sh
@@ -4,7 +4,12 @@ set -eu
 
 cd "$(dirname "${0}")"/..
 
+build_version="$(git describe --tags --exact-match 2> /dev/null || git rev-parse --short HEAD)"
+if [ -n "$(git status --porcelain)" ]; then
+  build_version="${build_version}-dirty"
+fi
+
 # The build.utcTime format must remain in sync with TimeFormat in info.go.
-echo '-X "github.com/cockroachdb/cockroach/pkg/build.tag='$(git describe --dirty --tags)'"' \
+echo '-X "github.com/cockroachdb/cockroach/pkg/build.tag='${build_version}'"' \
      '-X "github.com/cockroachdb/cockroach/pkg/build.utcTime='$(date -u '+%Y/%m/%d %H:%M:%S')'"' \
      '-X "github.com/cockroachdb/cockroach/pkg/build.deps='$(build/depvers.sh)'"'
