d56437db342e619e72e962e349b5dacc5a2f7d3e
a6802739 <songhao9021@gmail.com>
2016-11-23 20:54:10 +0800

    kv: add metrics for RPCs

--- pkg/kv/transport.go
+++ pkg/kv/transport.go
@@ -48,6 +48,8 @@ type SendOptions struct {
 	SendNextTimeout time.Duration
 
 	transportFactory TransportFactory
+
+	metrics *DistSenderMetrics
 }
 
 type batchClient struct {
@@ -138,6 +140,7 @@ func grpcTransportFactoryImpl(
 
 	return &grpcTransport{
 		opts:           opts,
+		metrics:        opts.metrics,
 		rpcContext:     rpcContext,
 		orderedClients: clients,
 	}, nil
@@ -145,6 +148,7 @@ func grpcTransportFactoryImpl(
 
 type grpcTransport struct {
 	opts            SendOptions
+	metrics         *DistSenderMetrics
 	rpcContext      *rpc.Context
 	clientIndex     int
 	orderedClients  []batchClient
@@ -167,8 +171,14 @@ func (gt *grpcTransport) SendNext(done chan<- BatchCall) {
 	if log.V(2) {
 		log.Infof(gt.opts.ctx, "sending request to %s: %+v", addr, client.args)
 	}
+	if gt.metrics != nil {
+		gt.metrics.total.Inc(1)
+	}
 
 	if localServer := gt.rpcContext.GetLocalInternalServerForAddr(addr); enableLocalCalls && localServer != nil {
+		if gt.metrics != nil {
+			gt.metrics.local.Inc(1)
+		}
 		// Clone the request. At the time of writing, Replica may mutate it
 		// during command execution which can lead to data races.
 		//
