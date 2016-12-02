630b2bb138b2ab35f3d825665e7757312618c5c5
Alex Robinson <alexdwanerobinson@gmail.com>
2016-12-02 10:38:33 -0500

    storage: Drastically speed up TestGossipHandlesReplacedNode
    It used to take > 12 seconds on laptops, now it's a few hundred
    milliseconds.
    
    The downside of this is that not using the node that holds most of the
    leases isn't as good of a test. When I initially wrote this test,
    it would pass if I used the 2nd/3rd nodes, but not if I used the first,
    so there's a meaningful difference in coverage to doing this. It'd be
    nice to be able to switch back once we actively drain leases.

--- pkg/storage/gossip_test.go
+++ pkg/storage/gossip_test.go
@@ -142,24 +142,30 @@ func TestGossipHandlesReplacedNode(t *testing.T) {
 		})
 	defer tc.Stopper().Stop()
 
-	// Take down the first node of the cluster and replace it with a new one.
-	// We replace the first node rather than the second or third to be adversarial
-	// because it typically has the most leases on it.
-	oldNodeIdx := 0
+	// Take down a node other than the first node and replace it with a new one.
+	// Replacing the first node would be better from an adversarial testing
+	// perspective because it typically has the most leases on it, but that also
+	// causes the test to take significantly longer as a result.
+	// TODO(a-robinson): Try switching back to using the first node once we start
+	// actively draining leases from nodes that are going down (#10765).
+	oldNodeIdx := 1
 	newServerArgs := base.TestServerArgs{
 		Addr:          tc.Servers[oldNodeIdx].ServingAddr(),
 		PartOfCluster: true,
-		JoinAddr:      tc.Servers[1].ServingAddr(),
+		JoinAddr:      tc.Servers[0].ServingAddr(),
 	}
 	tc.StopServer(oldNodeIdx)
 	tc.AddServer(t, newServerArgs)
-	tc.WaitForStores(t, tc.Server(1).Gossip())
+	tc.WaitForStores(t, tc.Server(0).Gossip())
 
 	// Ensure that all servers still running are responsive. If the two remaining
 	// original nodes don't refresh their connection to the address of the first
 	// node, they can get stuck here.
-	for i := 1; i < 4; i++ {
-		kvClient := tc.Server(i).KVClient().(*client.DB)
+	for i, server := range tc.Servers {
+		if i == oldNodeIdx {
+			continue
+		}
+		kvClient := server.KVClient().(*client.DB)
 		if err := kvClient.Put(ctx, fmt.Sprintf("%d", i), i); err != nil {
 			t.Errorf("failed Put to node %d: %s", i, err)
 		}
