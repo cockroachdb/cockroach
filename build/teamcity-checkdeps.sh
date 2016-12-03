8c48ea8f7c6ef2e441b973633347aa9256e17fa1
Tamir Duberstein <tamird@gmail.com>
2016-12-02 20:29:33 -0500

    build/teamcity-checkdeps: also check commands' dependencies

--- build/teamcity-checkdeps.sh
+++ build/teamcity-checkdeps.sh
@@ -4,15 +4,17 @@ set -exuo pipefail
 # This is intended to run in a CI to ensure dependencies are properly vendored.
 
 echo "checking that 'vendor' matches manifest"
-rm -rf vendor/*/*
+rm -rf vendor/*
 glide install --strip-vcs
-! git -C vendor status --porcelain | read || (git -C vendor status; git -C vendor diff -a; exit 1)
+! git -C vendor status --porcelain | read || (git -C vendor status; git -C vendor diff -a 1>&2; exit 1)
 
 echo "checking that all deps are in 'vendor''"
-missing=$(go list -f '{{ join .XTestImports "\n"}}{{"\n"}}{{ join .TestImports "\n"}}{{"\n"}}{{ join .Deps "\n"}}' . ./pkg/... \
-   | grep -v "^github.com/cockroachdb/cockroach" \
-   | sort | uniq \
-   | xargs go list -f '{{if not .Standard}}{{.ImportPath}}{{end}}')
+
+missing=$(cat GLOCKFILE | cut -f2 -d' ' | grep -v '^./pkg/' | awk '{print} END {print "./pkg/..."}' \
+  | xargs go list -f '{{ join .Deps "\n"}}{{"\n"}}{{ join .TestImports "\n"}}{{"\n"}}{{ join .XTestImports "\n"}}' \
+  | grep -v '^github.com/cockroachdb/cockroach' \
+  | sort | uniq \
+  | xargs go list -f '{{if not .Standard}}{{.ImportPath}}{{end}}')
 
 if [ -n "$missing" ]; then
   echo "vendor is missing some 3rd-party dependencies:"
