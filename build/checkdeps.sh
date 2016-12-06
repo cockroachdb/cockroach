a65c416b7ef4e9ffb4258e8072adabfbcef33b0f
David Taylor <tinystatemachine@gmail.com>
2016-12-06 14:50:51 +0000

    build: install glide in checkdeps

---
+++ build/checkdeps.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+
+# This is intended to run in a CI to ensure dependencies are properly vendored.
+
+echo "installing desired glide version"
+go get github.com/Masterminds/glide
+git -C $GOPATH/src/github.com/Masterminds/glide checkout v0.12.3
+go install github.com/Masterminds/glide
+
+echo "checking that 'vendor' matches manifest"
+./scripts/glide.sh install
+! git -C vendor status --porcelain | read || (git -C vendor status; git -C vendor diff -a 1>&2; exit 1)
+
+echo "checking that all deps are in 'vendor''"
+
+missing=$(cat GLOCKFILE | cut -f2 -d' ' | grep -v '^./pkg/' | awk '{print} END {print "./pkg/..."}' \
+  | xargs go list -f '{{ join .Deps "\n"}}{{"\n"}}{{ join .TestImports "\n"}}{{"\n"}}{{ join .XTestImports "\n"}}' \
+  | grep -v '^github.com/cockroachdb/cockroach' \
+  | sort | uniq \
+  | xargs go list -f '{{if not .Standard}}{{.ImportPath}}{{end}}')
+
+if [ -n "$missing" ]; then
+  echo "vendor is missing some 3rd-party dependencies:"
+  echo "$missing"
+  exit 1
+fi
