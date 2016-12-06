35ad47afbe48095221798ff6385081e5d6dc6c8a
David Taylor <tinystatemachine@gmail.com>
2016-12-06 16:22:38 +0000

    scripts: always clear glide cache in wrapper
    'up' can use stale dep versions, sometimes moving unrelated (cached) deps backwards, if they cache isn't cleared before every usage.

--- scripts/glide.sh
+++ scripts/glide.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+glide cache-clear
+
 # Glide *replaces* the vendor directory when it operates on it (e.g. in update
 # or get) which has the side-effect of breaking the .git submodule pointer.
 mv vendor/.git vendorgit
