a38fd1e6d22ec46eb0e46fa3f94c1b65a3abb76f
Max Lang <max@cockroachlabs.com>
2016-12-06 16:23:02 -0500

    ui: fix dropdown styles
    This commit changes the css so that the dropdown style matches the
    current mocks.

--- pkg/ui/app/containers/databases/databases.tsx
+++ pkg/ui/app/containers/databases/databases.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 import { connect } from "react-redux";
 import { IRouter, IInjectedProps } from "react-router";
 
-import Selector, { SelectorOption } from "../../components/selector";
+import Selector, { SelectorOption } from "../../components/dropdown";
 import { PageConfig, PageConfigItem } from "../../components/pageconfig";
 
 import { AdminUIState } from "../../redux/state";
