315e6b42a82582ab74920d831e1770cd2ca1d269
Max Lang <max@cockroachlabs.com>
2016-12-07 23:15:50 -0500

    ui: Change time selector to a dropdown
    This matches the time selector to the current designs:
    - time selection is now a dropdown
    - the currently selected time range is now visible at all times
    - you can shift the timerange backwards and forwards in time using
      the ui widget and the custom time range is shown

--- pkg/ui/app/components/dropdown.tsx
+++ pkg/ui/app/components/dropdown.tsx
@@ -1,27 +1,41 @@
 import Select from "react-select";
 import * as React from "react";
 
-export interface SelectorOption {
+import {leftArrow, rightArrow} from "./icons";
+import { trustIcon } from "../util/trust";
+
+export interface DropdownOption {
   value: string;
   label: string;
 }
 
-interface SelectorOwnProps {
+export enum ArrowDirection {
+  LEFT, RIGHT
+}
+
+interface DropdownOwnProps {
   title: string;
   selected: string;
-  options: SelectorOption[];
-  onChange?: (selected: SelectorOption) => void; // Callback when the value changes.
+  options: DropdownOption[];
+  onChange?: (selected: DropdownOption) => void; // Callback when the value changes.
+  // If true, don't display the arrow next to the dropdown, display left and
+  // right arrows to either side instead.
+  sideArrows?: boolean;
+  onArrowClick?: (direction: ArrowDirection) => void;
 }
 
 /**
- * Selector component that uses the URL query string for state.
+ * Dropdown component that uses the URL query string for state.
  */
-export default class Selector extends React.Component<SelectorOwnProps, {}> {
+export default class Dropdown extends React.Component<DropdownOwnProps, {}> {
   render() {
     let {selected, options, onChange} = this.props;
     return <div className="dropdown">
-      <span className="dropdown__title">{ this.props.title }:</span>
+      {/* TODO (maxlang): consider moving arrows outside the dropdown component */}
+      <span className="dropdown__side-arrow" dangerouslySetInnerHTML={trustIcon(leftArrow)} onClick={() => this.props.onArrowClick(ArrowDirection.LEFT)}></span>
+      <span className="dropdown__title">{this.props.title}{this.props.title ? ":" : ""}</span>
       <Select className="dropdown__select" clearable={false} options={options} value={selected} onChange={onChange} />
+      <span className="dropdown__side-arrow" dangerouslySetInnerHTML={trustIcon(rightArrow)} onClick={() => this.props.onArrowClick(ArrowDirection.RIGHT)}></span>
     </div>;
   }
 }
--- pkg/ui/app/containers/timescale.tsx
+++ pkg/ui/app/containers/timescale.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import classNames from "classnames";
 import _ from "lodash";
 import moment from "moment";
+
+import Dropdown, { DropdownOption, ArrowDirection } from "../components/dropdown";
 
 import { AdminUIState } from "../redux/state";
 import { refreshNodes } from "../redux/apiReducers";
@@ -16,7 +17,43 @@ import { LongToMoment } from "../util/convert";
 // the entire app so that changing pages doesn't cause it to reset.
 const UI_TIMESCALE_DEFAULT_SET = "timescale/default_set";
 
-interface TimeScaleSelectorProps {
+interface TimeRangeProps {
+  start: moment.Moment;
+  end: moment.Moment;
+}
+
+// TimeRange is a prettified string representation of the current timescale.
+class TimeRange extends React.Component<TimeRangeProps, {}> {
+  render() {
+    let s = this.props.start;
+    let e = this.props.end;
+    let startTimeSpan = <span className="time-range__time">{s.format("HH:mm:SS")}</span>;
+    let endTimeSpan = <span className="time-range__time">{e.format("HH:mm:SS")}</span>;
+    let startDateSpan = <span className="time-range__date">{s.format("MMM DD, YYYY")}</span>;
+    let endDateSpan = <span className="time-range__date">{e.format("MMM DD, YYYY")}</span>;
+
+    let rangeHours = e.diff(s, "hours");
+    let pastHours = moment().diff(s, "hours");
+    // If start and end times are on the same day, show the times.
+    if (rangeHours < 24 && s.date() === e.date()) {
+      // If start and end times are today, omit the date, otherwise include it.
+      if (pastHours < 24 && s.date() === moment().date()) {
+        return <span className="time-range">{startTimeSpan} to {endTimeSpan}</span>;
+      } else {
+        return <span className="time-range">{startTimeSpan} to {endTimeSpan} on {endDateSpan}</span>;
+      }
+    }
+    // If start and end times are within 48 hours show times and dates,
+    // otherwise omit times and only show the dates.
+    if ( rangeHours < 48) {
+      return <span className="time-range">{startTimeSpan} on {startDateSpan} to {endTimeSpan} on {endDateSpan}</span>;
+    } else {
+      return <span className="time-range">{startDateSpan} to {endDateSpan}</span>;
+    }
+  }
+}
+
+interface TimeScaleDropdownProps {
   currentScale: timewindow.TimeScale;
   availableScales: timewindow.TimeScaleCollection;
   setTimeScale: typeof timewindow.setTimeScale;
@@ -29,39 +66,59 @@ interface TimeScaleSelectorProps {
   defaultTimescaleSet: boolean;
 }
 
-interface TimeScaleSelectorState {
-  controlsVisible: boolean; // Timescale selector controls visible.
-}
-
-class TimeScaleSelector extends React.Component<TimeScaleSelectorProps, TimeScaleSelectorState> {
-
-  timescaleBtn: Element;
-
-  constructor() {
-    super();
-    this.state = {
-      controlsVisible: false,
-    };
-  }
-
-  setVisible = (visible: boolean) => {
-    this.setState({
-      controlsVisible: visible,
-    });
-  };
-
-  hide = (e: Event) => {
-    if (e.target !== this.timescaleBtn) {
-      this.setVisible(false);
+// TimeScaleDropdown is the dropdown that allows users to select the time range
+// for graphs.
+class TimeScaleDropdown extends React.Component<TimeScaleDropdownProps, {}> {
+  changeSettings = (newTimescaleKey: DropdownOption) => {
+    let newSettings = timewindow.availableTimeScales[newTimescaleKey.value];
+    if (newSettings) {
+      this.props.setTimeScale(newSettings);
     }
-  };
-
-  changeSettings(newSettings: timewindow.TimeScale) {
-    this.props.setTimeScale(newSettings);
   }
 
-  isSelected(scale: timewindow.TimeScale) {
-    return scale === this.props.currentScale;
+  arrowClick = (direction: ArrowDirection) => {
+    let selected = _.clone(this.props.currentScale);
+
+    switch (direction) {
+      case ArrowDirection.RIGHT:
+        if (selected.windowEnd) {
+          selected.windowEnd.add(selected.windowSize);
+        }
+        break;
+      case ArrowDirection.LEFT:
+        selected.windowEnd = selected.windowEnd || moment();
+        selected.windowEnd.subtract(selected.windowSize);
+        break;
+      default:
+        console.error("Unknown direction: ", direction);
+    }
+
+    // If the timescale extends into the future then fallback to a default
+    // timescale. Otherwise set the key to "Custom" so it appears correctly.
+    if (!selected.windowEnd || selected.windowEnd > moment().subtract(selected.windowValid)) {
+      selected = _.find(timewindow.availableTimeScales, { windowSize: selected.windowSize });
+    } else {
+      selected.key = "Custom";
+    }
+
+    this.props.setTimeScale(selected);
+  }
+
+  getTimescaleOptions = () => {
+    let timescaleOptions = _.map(timewindow.availableTimeScales, (ts, k) => {
+      return { value: k, label: "Last " + k };
+    });
+
+    // This just ensures that if the key is "Custom" it will show up in the
+    // dropdown options. If a custom value isn't currently selected, "Custom"
+    // won't show up in the list of options.
+    if (!_.has(timewindow.availableTimeScales, this.props.currentScale.key)) {
+      timescaleOptions.push({
+        value: this.props.currentScale.key,
+        label: this.props.currentScale.key,
+      });
+    }
+    return timescaleOptions;
   }
 
   // Sets the default timescale based on the start time of the oldest node.
@@ -69,6 +126,7 @@ class TimeScaleSelector extends React.Component<TimeScaleSelectorProps, TimeScal
     if (props.nodeStatusesValid && !props.defaultTimescaleSet) {
       let oldestNode = _.minBy(props.nodeStatuses, (nodeStatus: NodeStatus) => nodeStatus.started_at);
       let clusterStarted = LongToMoment(oldestNode.started_at);
+      // TODO (maxlang): This uses the longest uptime, not the oldest
       let clusterDurationHrs = moment.utc().diff(clusterStarted, "hours");
       if (clusterDurationHrs > 1) {
         if (clusterDurationHrs < 6) {
@@ -86,13 +144,11 @@ class TimeScaleSelector extends React.Component<TimeScaleSelectorProps, TimeScal
   }
 
   componentWillMount() {
-    // Hide the popup when you click anywhere on the page
-    document.body.addEventListener("click", this.hide);
     this.props.refreshNodes();
     this.setDefaultTime();
   }
 
-  componentWillReceiveProps(props: TimeScaleSelectorProps) {
+  componentWillReceiveProps(props: TimeScaleDropdownProps) {
     if (!props.nodeStatusesValid) {
       this.props.refreshNodes();
     } else {
@@ -100,36 +156,30 @@ class TimeScaleSelector extends React.Component<TimeScaleSelectorProps, TimeScal
     }
   }
 
-  componentWillUnmount() {
-    // Remove popup hiding event listener on unmount
-    document.body.removeEventListener("click", this.hide);
-  }
-
   render() {
-    let selectorClass = classNames({
-      "timescale-selector": true,
-      "show": this.state.controlsVisible,
-    });
-    return <div className="timescale-selector-container">
-      <button
-        className="timescale"
-        ref={(timescaleBtn) => this.timescaleBtn = timescaleBtn}
-        onClick={() => this.setVisible(!this.state.controlsVisible)}>
-          Select Timescale
-      </button>
-      <div className={selectorClass}>
-        <div className="text">View Last: </div>
-        {
-          _.map(this.props.availableScales, (scale, key) => {
-            let theseSettings = scale;
-            return <button key={key}
-                           className={classNames({selected: this.isSelected(scale)})}
-                           onClick={(e) => { this.changeSettings(theseSettings); this.setVisible(false); } }>
-                           { key }
-            </button>;
-          })
-        }
-      </div>
+    let currentScale = this.props.currentScale;
+    let timeRange: React.ReactNode = "";
+
+    let className = "Select--side-arrows";
+
+    // If the time scale doesn't end at the present, display the time range.
+    if (currentScale.windowEnd) {
+      let start = currentScale.windowEnd.clone().subtract(currentScale.windowSize);
+      timeRange = <TimeRange start={start} end={currentScale.windowEnd} />;
+    } else {
+      // If the time scale does end at the present hide the right arrow.
+      className += " Select--no-right-arrow";
+    }
+
+    return <div className={className}>
+      <Dropdown
+        title=""
+        options={this.getTimescaleOptions()}
+        selected={currentScale.key}
+        onChange={this.changeSettings}
+        onArrowClick={this.arrowClick}
+        />
+      <span>{timeRange}</span>
     </div>;
   }
 }
@@ -149,4 +199,4 @@ export default connect(
     refreshNodes: refreshNodes,
     setUISetting: setUISetting,
   }
-)(TimeScaleSelector);
+)(TimeScaleDropdown);
