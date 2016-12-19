cockroachdb/cockroach

commit 9450059ada3d31fe73f0fe4573f4b39b4cbb9d79

sql: factor the code around addRender()
Both the code for RETURNING, ORDER BY and for window functions was
doing something like addRender() but not quite. This patch recognizes
the commonalities and refactors as follows:

- `addRender()` becomes `computeRender()` which computes a list of
  rendered columns without adding them to the `selectNode`.

- a new method `addOrMergeRenders()` is introduced which attempts
  (under control of a flag) to reuse existing renders when possible.

- the code that was previously using addRender() now uses the new
  methods, resulting in somewhat shorter but mostly more readable and
  maintainable code.

pkg/sql/parser/type_check.go
@@ -367,16 +367,6 @@ func (expr *FuncExpr) TypeCheck(ctx *SemaContext, desired Type) (TypedExpr, erro
 			expr.Func, strings.Join(typeNames, ", "), desStr)
 	}
 
-	if expr.WindowDef != nil {
-		for i, partition := range expr.WindowDef.Partitions {
-			typedPartition, err := partition.TypeCheck(ctx, TypeAny)
-			if err != nil {
-				return nil, err
-			}
-			expr.WindowDef.Partitions[i] = typedPartition
-		}
-	}
-
 	builtin := fn.(Builtin)
 	if expr.IsWindowFunctionApplication() {
 		// Make sure the window function application is of either a built-in window

pkg/sql/window.go
@@ -176,29 +176,40 @@ func (n *windowNode) constructWindowDefinitions(sc *parser.SelectClause, s *sele
 
 		// Validate PARTITION BY clause.
 		for _, partition := range windowDef.Partitions {
-			windowFn.partitionIdxs = append(windowFn.partitionIdxs, len(s.render)-origRenderLen)
-
-			err := s.addRender(parser.SelectExpr{Expr: partition}, parser.TypeAny)
+			cols, exprs, _, err := s.planner.computeRender(parser.SelectExpr{Expr: partition},
+				parser.TypeAny, s.source.info, s.ivarHelper, true)
 			if err != nil {
 				return err
+			}
+
+			// FIXME: why does this break if we merge the renders here?
+			colIdxs := s.addOrMergeRenders(cols, exprs, false)
+			for _, idx := range colIdxs {
+				windowFn.partitionIdxs = append(windowFn.partitionIdxs, idx-origRenderLen)
 			}
 		}
 
 		// Validate ORDER BY clause.
 		for _, orderBy := range windowDef.OrderBy {
+			cols, exprs, _, err := s.planner.computeRender(parser.SelectExpr{Expr: orderBy.Expr},
+				parser.TypeAny, s.source.info, s.ivarHelper, true)
+			if err != nil {
+				return err
+			}
+
 			direction := encoding.Ascending
 			if orderBy.Direction == parser.Descending {
 				direction = encoding.Descending
 			}
-			ordering := sqlbase.ColumnOrderInfo{
-				ColIdx:    len(s.render) - origRenderLen,
-				Direction: direction,
-			}
-			windowFn.columnOrdering = append(windowFn.columnOrdering, ordering)
 
-			err := s.addRender(parser.SelectExpr{Expr: orderBy.Expr}, parser.TypeAny)
-			if err != nil {
-				return err
+			// FIXME: why does this break if we merge the renders here?
+			colIdxs := s.addOrMergeRenders(cols, exprs, false)
+			for _, idx := range colIdxs {
+				ordering := sqlbase.ColumnOrderInfo{
+					ColIdx:    idx - origRenderLen,
+					Direction: direction,
+				}
+				windowFn.columnOrdering = append(windowFn.columnOrdering, ordering)
 			}
 		}
 

pkg/sql/testdata/window
@@ -61,6 +61,16 @@ SELECT avg(k) OVER (PARTITION BY v) FROM kv ORDER BY 1
 5.5000000000000000
 
 query R
+SELECT avg(k) OVER (PARTITION BY kv.*) FROM kv ORDER BY 1
+----
+1.0000000000000000
+3.0000000000000000
+5.0000000000000000
+6.0000000000000000
+7.0000000000000000
+8.0000000000000000
+
+query R
 SELECT avg(k) OVER (ORDER BY w) FROM kv ORDER BY 1
 ----
 5.0000000000000000
@@ -69,6 +79,16 @@ SELECT avg(k) OVER (ORDER BY w) FROM kv ORDER BY 1
 5.5000000000000000
 7.5000000000000000
 7.5000000000000000
+
+query R
+SELECT avg(k) OVER (ORDER BY kv.*) FROM kv ORDER BY 1
+----
+1.0000000000000000
+2.0000000000000000
+3.0000000000000000
+3.7500000000000000
+4.4000000000000000
+5.0000000000000000
 
 query R
 SELECT avg(k) OVER (ORDER BY w DESC) FROM kv ORDER BY 1
@@ -495,8 +515,8 @@ EXPLAIN (TYPES) SELECT k, stddev(d) OVER (PARTITION BY v, 'a') FROM kv ORDER BY 
 0  select         result                                         (k int, "stddev(d) OVER (PARTITION BY v, 'a')" decimal)
 1  sort           result                                         (k int, "stddev(d) OVER (PARTITION BY v, 'a')" decimal)
 2  window         result                                         (k int, "stddev(d) OVER (PARTITION BY v, 'a')" decimal, "variance(d) OVER (PARTITION BY v, 100)" decimal)
-2  window         render stddev(d) OVER (PARTITION BY v, 'a')    (stddev((d)[decimal]) OVER (PARTITION BY (v)[int], ('a')[string]))[decimal]
-2  window         render variance(d) OVER (PARTITION BY v, 100)  (variance((d)[decimal]) OVER (PARTITION BY (v)[int], (100)[int]))[decimal]
+2  window         render stddev(d) OVER (PARTITION BY v, 'a')    (stddev((d)[decimal]) OVER (PARTITION BY (v)[int], 'a'))[decimal]
+2  window         render variance(d) OVER (PARTITION BY v, 100)  (variance((d)[decimal]) OVER (PARTITION BY (v)[int], 100))[decimal]
 3  render/filter  result                                         (k int, d decimal, d decimal, v int, "'a'" string, v int, "100" int)
 3  render/filter  render 0                                       (k)[int]
 3  render/filter  render 1                                       (d)[decimal]
@@ -513,8 +533,8 @@ EXPLAIN (TYPES) SELECT k, k + stddev(d) OVER (PARTITION BY v, 'a') FROM kv ORDER
 0  select         result                                           (k int, "k + stddev(d) OVER (PARTITION BY v, 'a')" decimal)
 1  sort           result                                           (k int, "k + stddev(d) OVER (PARTITION BY v, 'a')" decimal)
 2  window         result                                           (k int, "k + stddev(d) OVER (PARTITION BY v, 'a')" decimal, "variance(d) OVER (PARTITION BY v, 100)" decimal)
-2  window         render k + stddev(d) OVER (PARTITION BY v, 'a')  ((k)[int] + (stddev((d)[decimal]) OVER (PARTITION BY (v)[int], ('a')[string]))[decimal])[decimal]
-2  window         render variance(d) OVER (PARTITION BY v, 100)    (variance((d)[decimal]) OVER (PARTITION BY (v)[int], (100)[int]))[decimal]
+2  window         render k + stddev(d) OVER (PARTITION BY v, 'a')  ((k)[int] + (stddev((d)[decimal]) OVER (PARTITION BY (v)[int], 'a'))[decimal])[decimal]
+2  window         render variance(d) OVER (PARTITION BY v, 100)    (variance((d)[decimal]) OVER (PARTITION BY (v)[int], 100))[decimal]
 3  render/filter  result                                           (k int, d decimal, d decimal, v int, "'a'" string, v int, "100" int, k int)
 3  render/filter  render 0                                         (k)[int]
 3  render/filter  render 1                                         (d)[decimal]

pkg/sql/group.go
@@ -51,10 +51,11 @@ func (p *planner) groupBy(n *parser.SelectClause, s *selectNode) (*groupNode, er
 	// Invalid: `SELECT k, SUM(v) FROM kv GROUP BY UPPER(k)`
 	// - `k` does not appear in GROUP BY; UPPER(k) does nothing to help here.
 	//
-	// In the construction of the outer selectNode, when renders are added (via
-	// addRender()), the expressions are normalized. In order to compare these
-	// normalized render expressions to GROUP BY expressions, we need to
-	// normalize the GROUP BY expressions as well.
+	// In the construction of the outer selectNode, when renders are
+	// processed (via computeRender()), the expressions are
+	// normalized. In order to compare these normalized render
+	// expressions to GROUP BY expressions, we need to normalize the
+	// GROUP BY expressions as well.
 	// This is done before determining if aggregation is being performed, because
 	// that determination is made during validation, which will require matching
 	// expressions.
@@ -74,7 +75,7 @@ func (p *planner) groupBy(n *parser.SelectClause, s *selectNode) (*groupNode, er
 			expr = n.Exprs[col].Expr
 		} else {
 			// We do not need to fully analyze the GROUP BY expression here
-			// (as per analyzeExpr) because this is taken care of by addRender
+			// (as per analyzeExpr) because this is taken care of by computeRender
 			// below.
 			resolvedExpr, _, err := p.resolveNames(expr, s.sourceInfo, s.ivarHelper)
 			if err != nil {
@@ -191,9 +192,18 @@ func (p *planner) groupBy(n *parser.SelectClause, s *selectNode) (*groupNode, er
 
 	// Add the group-by expressions so they are available for bucketing.
 	for _, g := range groupByExprs {
-		if err := s.addRender(parser.SelectExpr{Expr: g}, parser.TypeAny); err != nil {
+		cols, exprs, hasStar, err := s.planner.computeRender(parser.SelectExpr{Expr: g}, parser.TypeAny,
+			s.source.info, s.ivarHelper, true)
+		if err != nil {
 			return nil, err
 		}
+		s.isStar = s.isStar || hasStar
+		// Note that it may appear unwise to request the grouping
+		// expressions to be merged in the render list. However this
+		// should be safe here because it's not correct to group by an
+		// aggregate function so the merge loop cannot encounter an
+		// equivalent expression in the aggregation list.
+		_ = s.addOrMergeRenders(cols, exprs, false)
 	}
 
 	group.desiredOrdering = desiredAggregateOrdering(group.funcs)

pkg/sql/returning.go
@@ -72,37 +72,13 @@ func (p *planner) newReturningHelper(
 	rh.source = newSourceInfoForSingleTable(aliasTableName, makeResultColumns(tablecols))
 	rh.exprs = make([]parser.TypedExpr, 0, len(r))
 	ivarHelper := parser.MakeIndexedVarHelper(rh, len(tablecols))
-	for i, target := range r {
-		// Pre-normalize VarNames at the top level so that checkRenderStar can see stars.
-		if err := target.NormalizeTopLevelVarName(); err != nil {
-			return nil, err
-		}
-
-		if isStar, cols, typedExprs, err := checkRenderStar(
-			target, rh.source, ivarHelper, true); err != nil {
-			return nil, err
-		} else if isStar {
-			rh.exprs = append(rh.exprs, typedExprs...)
-			rh.columns = append(rh.columns, cols...)
-			continue
-		}
-
-		// When generating an output column name it should exactly match the original
-		// expression, so determine the output column name before we perform any
-		// manipulations to the expression.
-		outputName := getRenderColName(target)
-
-		desired := parser.TypeAny
-		if len(desiredTypes) > i {
-			desired = desiredTypes[i]
-		}
-
-		typedExpr, err := rh.p.analyzeExpr(target.Expr, multiSourceInfo{rh.source}, ivarHelper, desired, false, "")
+	for _, target := range r {
+		cols, typedExprs, _, err := p.computeRender(target, parser.TypeAny, rh.source, ivarHelper, true)
 		if err != nil {
 			return nil, err
 		}
-		rh.exprs = append(rh.exprs, typedExpr)
-		rh.columns = append(rh.columns, ResultColumn{Name: outputName, Typ: typedExpr.ResolvedType()})
+		rh.columns = append(rh.columns, cols...)
+		rh.exprs = append(rh.exprs, typedExprs...)
 	}
 	return rh, nil
 }

pkg/sql/select.go
@@ -43,7 +43,7 @@ type selectNode struct {
 	// sourceInfo contains the reference to the dataSourceInfo in the
 	// source planDataSource that is needed for name resolution.
 	// We keep one instance of multiSourceInfo cached here so as to avoid
-	// re-creating it every time analyzeExpr() is called in addRender().
+	// re-creating it every time analyzeExpr() is called in computeRender().
 	sourceInfo multiSourceInfo
 
 	// Helper for indexed vars. This holds the actual instances of
@@ -52,7 +52,7 @@ type selectNode struct {
 	ivarHelper parser.IndexedVarHelper
 
 	// Rendering expressions for rows and corresponding output columns.
-	// populated by addRender()
+	// populated by addOrMergeRenders()
 	// as invoked initially by initTargets() and initWhere().
 	// sortNode peeks into the render array defined by initTargets() as an optimization.
 	// sortNode adds extra selectNode renders for sort columns not requested as select targets.
@@ -499,9 +499,13 @@ func (s *selectNode) initTargets(targets parser.SelectExprs, desiredTypes []pars
 		if len(desiredTypes) > i {
 			desiredType = desiredTypes[i]
 		}
-		if err := s.addRender(target, desiredType); err != nil {
+		cols, exprs, hasStar, err := s.planner.computeRender(target, desiredType,
+			s.source.info, s.ivarHelper, true)
+		if err != nil {
 			return err
 		}
+		s.isStar = s.isStar || hasStar
+		_ = s.addOrMergeRenders(cols, exprs, false)
 	}
 	// `groupBy` or `orderBy` may internally add additional columns which we
 	// do not want to include in validation of e.g. `GROUP BY 2`. We record the

pkg/sql/sort.go
@@ -164,28 +164,13 @@ func (p *planner) orderBy(orderBy parser.OrderBy, n planNode) (*sortNode, error)
 				// alias, resolveNames below would be incorrect.
 				sortExpr = s.render[index]
 			}
-			resolved, hasRowDependentValues, err := p.resolveNames(sortExpr, s.sourceInfo, s.ivarHelper)
+			_, hasRowDependentValues, err := p.resolveNames(sortExpr, s.sourceInfo, s.ivarHelper)
 			if err != nil {
 				return nil, err
 			}
 			if !hasRowDependentValues {
 				// No sorting needed!
 				continue
-			}
-
-			// Now, try to find an equivalent render. We use the syntax
-			// representation as approximation of equivalence.
-			// We also use the expression after name resolution so
-			// that comparison occurs after replacing ordinal references
-			// to IndexedVars.
-			if index == -1 {
-				exprStr := parser.AsStringWithFlags(resolved, parser.FmtSymbolicVars)
-				for j, render := range s.render {
-					if parser.AsStringWithFlags(render, parser.FmtSymbolicVars) == exprStr {
-						index = j
-						break
-					}
-				}
 			}
 		}
 
@@ -195,34 +180,26 @@ func (p *planner) orderBy(orderBy parser.OrderBy, n planNode) (*sortNode, error)
 		// If we are dealing with a UNION or something else we would need
 		// to fabricate an intermediate selectNode to add the new render.
 		if index == -1 && s != nil {
-			// We need to add a new render, but let's be careful: if there's
-			// a star in there, we're really adding multiple columns. These
-			// all become sort columns! And if no columns are expanded, then
-			// no columns become sort keys.
-			nextRenderIdx := len(s.render)
-
-			// TODO(knz) the optimizations above which reuse existing
-			// renders and skip ordering for constant expressions are not
-			// applied by addRender(). In other words, "ORDER BY foo.*" will
-			// not be optimized as well as "ORDER BY foo.x, foo.y".  We
-			// could do this either here or as a separate later
-			// optimization.
-			err := s.addRender(parser.SelectExpr{Expr: expr}, parser.TypeAny)
+			cols, exprs, hasStar, err := p.computeRender(parser.SelectExpr{Expr: expr}, parser.TypeAny,
+				s.source.info, s.ivarHelper, true)
 			if err != nil {
 				return nil, err
 			}
+			s.isStar = s.isStar || hasStar
 
-			for extraIdx := nextRenderIdx; extraIdx < len(s.render)-1; extraIdx++ {
+			if len(cols) == 0 {
+				// Nothing was expanded! No order here.
+				continue
+			}
+
+			colIdxs := s.addOrMergeRenders(cols, exprs, true)
+			for i := 0; i < len(colIdxs)-1; i++ {
 				// If more than 1 column were expanded, turn them into sort columns too.
 				// Except the last one, which will be added below.
 				ordering = append(ordering,
-					sqlbase.ColumnOrderInfo{ColIdx: extraIdx, Direction: direction})
+					sqlbase.ColumnOrderInfo{ColIdx: colIdxs[i], Direction: direction})
 			}
-			if len(s.render) == nextRenderIdx {
-				// Nothing was expanded! So there is no order here.
-				continue
-			}
-			index = len(s.render) - 1
+			index = colIdxs[len(colIdxs)-1]
 		}
 
 		if index == -1 {

pkg/sql/targets.go
@@ -21,20 +21,26 @@ import (
 	"github.com/pkg/errors"
 )
 
-func (s *selectNode) addRender(target parser.SelectExpr, desiredType parser.Type) error {
+// computeRender expands a target expression into a result column (or more
+// than one, in case there is a star). Whether star expansion occurred
+// is indicated by the third return value.
+func (p *planner) computeRender(
+	target parser.SelectExpr,
+	desiredType parser.Type,
+	info *dataSourceInfo,
+	ivarHelper parser.IndexedVarHelper,
+	allowStars bool,
+) (columns ResultColumns, exprs []parser.TypedExpr, hasStar bool, err error) {
 	// Pre-normalize any VarName so the work is not done twice below.
 	if err := target.NormalizeTopLevelVarName(); err != nil {
-		return err
+		return nil, nil, false, err
 	}
 
-	if isStar, cols, typedExprs, err := checkRenderStar(
-		target, s.source.info, s.ivarHelper, true); err != nil {
-		return err
-	} else if isStar {
-		s.columns = append(s.columns, cols...)
-		s.render = append(s.render, typedExprs...)
-		s.isStar = true
-		return nil
+	if hasStar, cols, typedExprs, err := checkRenderStar(
+		target, info, ivarHelper, allowStars); err != nil {
+		return nil, nil, false, err
+	} else if hasStar {
+		return cols, typedExprs, hasStar, nil
 	}
 
 	// When generating an output column name it should exactly match the original
@@ -42,14 +48,51 @@ func (s *selectNode) addRender(target parser.SelectExpr, desiredType parser.Type
 	// manipulations to the expression.
 	outputName := getRenderColName(target)
 
-	normalized, err := s.planner.analyzeExpr(target.Expr, s.sourceInfo, s.ivarHelper, desiredType, false, "")
+	normalized, err := p.analyzeExpr(
+		target.Expr, multiSourceInfo{info}, ivarHelper, desiredType, false, "")
 	if err != nil {
-		return err
+		return nil, nil, false, err
 	}
 
-	s.addRenderColumn(normalized, ResultColumn{Name: outputName, Typ: normalized.ResolvedType()})
+	return ResultColumns{
+		ResultColumn{Name: outputName, Typ: normalized.ResolvedType()},
+	}, []parser.TypedExpr{normalized}, false, nil
+}
 
-	return nil
+// addOrMergeRenders adds the given result columns to the select
+// render list and returns their column indices. If an expression is
+// already rendered, and the reuse flag is true, no new render is
+// added and the index of the existing column is returned instead.
+func (s *selectNode) addOrMergeRenders(
+	cols ResultColumns, exprs []parser.TypedExpr, reuseExistingRender bool,
+) (colIdxs []int) {
+	colIdxs = make([]int, len(cols))
+	for i := range cols {
+		index := -1
+
+		if reuseExistingRender {
+			// Now, try to find an equivalent render. We use the syntax
+			// representation as approximation of equivalence.  At this
+			// point the expressions must have underwent name resolution
+			// already so that comparison occurs after replacing column names
+			// to IndexedVars.
+			exprStr := parser.AsStringWithFlags(exprs[i], parser.FmtSymbolicVars)
+			for j, render := range s.render {
+				if parser.AsStringWithFlags(render, parser.FmtSymbolicVars) == exprStr {
+					index = j
+					break
+				}
+			}
+		}
+
+		if index == -1 {
+			index = len(s.render)
+			s.addRenderColumn(exprs[i], cols[i])
+		}
+		colIdxs[i] = index
+	}
+
+	return colIdxs
 }
 
 // checkRenderStar checks if the SelectExpr is either an

commit b6e4a789cf00f353db60b2b00c319570a9539289

sql: fix the type checking of window definitions.
Commit 9450059ada3d31fe73f0fe4573f4b39b4cbb9d79 has introduced a
regression by which the result of type checking for expressions used
for PARTITION BY and ORDER BY in window definitions would not be
stored back in the syntax node, resulting in incorrect renders which
could cause the server to crash upon visiting those nodes with
EXPLAIN.

This patch fixes the issue by ensuring that type checking is performed
early again.

It also incidentally fixes another issue where ORDER BY clauses for
window function definitions were not visited by type checking, such
that clauses like `... OVER (ORDER BY w+1)` would cause the server to
panic.

While it fixes the aforementioned panic-inducing issues, this patch
also introduces a feature regression: early type checking does not
(yet) support star expansion, so that `PARTITION BY t.*` and `ORDER BY
t.*` clauses in window function definitions do not work any more.
This feature was introduced by the same faulty commit
9450059ada3d31fe73f0fe4573f4b39b4cbb9d79, and as discussed above this
wasn't properly implemented anyway. The feature may be reintroduced later.

pkg/sql/parser/type_check.go
@@ -384,6 +384,23 @@ func (expr *FuncExpr) TypeCheck(ctx *SemaContext, desired Type) (TypedExpr, erro
 			expr.Func, strings.Join(typeNames, ", "), desStr)
 	}
 
+	if expr.WindowDef != nil {
+		for i, partition := range expr.WindowDef.Partitions {
+			typedPartition, err := partition.TypeCheck(ctx, TypeAny)
+			if err != nil {
+				return nil, err
+			}
+			expr.WindowDef.Partitions[i] = typedPartition
+		}
+		for i, orderBy := range expr.WindowDef.OrderBy {
+			typedOrderBy, err := orderBy.Expr.TypeCheck(ctx, TypeAny)
+			if err != nil {
+				return nil, err
+			}
+			expr.WindowDef.OrderBy[i].Expr = typedOrderBy
+		}
+	}
+
 	builtin := fn.(Builtin)
 	if expr.IsWindowFunctionApplication() {
 		// Make sure the window function application is of either a built-in window

pkg/sql/parser/walk.go
@@ -198,6 +198,13 @@ func (expr *FuncExpr) CopyNode() *FuncExpr {
 	exprCopy.Exprs = append(Exprs(nil), exprCopy.Exprs...)
 	if windowDef := exprCopy.WindowDef; windowDef != nil {
 		windowDef.Partitions = append(Exprs(nil), windowDef.Partitions...)
+		if len(windowDef.OrderBy) > 0 {
+			newOrderBy := make(OrderBy, len(windowDef.OrderBy))
+			for i, o := range windowDef.OrderBy {
+				newOrderBy[i] = &Order{Expr: o.Expr, Direction: o.Direction}
+			}
+			windowDef.OrderBy = newOrderBy
+		}
 	}
 	return &exprCopy
 }
@@ -222,6 +229,15 @@ func (expr *FuncExpr) Walk(v Visitor) Expr {
 					ret = expr.CopyNode()
 				}
 				ret.WindowDef.Partitions[i] = e
+			}
+		}
+		for i := range expr.WindowDef.OrderBy {
+			e, changed := WalkExpr(v, expr.WindowDef.OrderBy[i].Expr)
+			if changed {
+				if ret == expr {
+					ret = expr.CopyNode()
+				}
+				ret.WindowDef.OrderBy[i].Expr = e
 			}
 		}
 	}

pkg/sql/window.go
@@ -182,7 +182,9 @@ func (n *windowNode) constructWindowDefinitions(sc *parser.SelectClause, s *sele
 				return err
 			}
 
-			// FIXME: why does this break if we merge the renders here?
+			// TODO(knz/nvanbenschoten): we can't merge this yet because the
+			// rest of the code assumes that the renders for window
+			// definitions are disjoint from the rest.
 			colIdxs := s.addOrMergeRenders(cols, exprs, false)
 			for _, idx := range colIdxs {
 				windowFn.partitionIdxs = append(windowFn.partitionIdxs, idx-origRenderLen)
@@ -202,7 +204,9 @@ func (n *windowNode) constructWindowDefinitions(sc *parser.SelectClause, s *sele
 				direction = encoding.Descending
 			}
 
-			// FIXME: why does this break if we merge the renders here?
+			// TODO(knz/nvanbenschoten): we can't merge this yet because the
+			// rest of the code assumes that the renders for sorting columns
+			// are disjoint from the rest.
 			colIdxs := s.addOrMergeRenders(cols, exprs, false)
 			for _, idx := range colIdxs {
 				ordering := sqlbase.ColumnOrderInfo{
@@ -252,14 +256,14 @@ func constructWindowDef(
 	}
 
 	// referencedSpec.Partitions is always used.
-	if def.Partitions != nil {
+	if len(def.Partitions) > 0 {
 		return def, errors.Errorf("cannot override PARTITION BY clause of window %q", refName)
 	}
 	def.Partitions = referencedSpec.Partitions
 
 	// referencedSpec.OrderBy is used if set.
-	if referencedSpec.OrderBy != nil {
-		if def.OrderBy != nil {
+	if len(referencedSpec.OrderBy) > 0 {
+		if len(def.OrderBy) > 0 {
 			return def, errors.Errorf("cannot override ORDER BY clause of window %q", refName)
 		}
 		def.OrderBy = referencedSpec.OrderBy

pkg/sql/testdata/window
@@ -60,15 +60,16 @@ SELECT avg(k) OVER (PARTITION BY v) FROM kv ORDER BY 1
 5.5000000000000000
 5.5000000000000000
 
-query R
-SELECT avg(k) OVER (PARTITION BY kv.*) FROM kv ORDER BY 1
-----
-1.0000000000000000
-3.0000000000000000
-5.0000000000000000
-6.0000000000000000
-7.0000000000000000
-8.0000000000000000
+# Not working until #12482 is fixed.
+#query R
+#SELECT avg(k) OVER (PARTITION BY kv.*) FROM kv ORDER BY 1
+#----
+#1.0000000000000000
+#3.0000000000000000
+#5.0000000000000000
+#6.0000000000000000
+#7.0000000000000000
+#8.0000000000000000
 
 query R
 SELECT avg(k) OVER (ORDER BY w) FROM kv ORDER BY 1
@@ -81,14 +82,25 @@ SELECT avg(k) OVER (ORDER BY w) FROM kv ORDER BY 1
 7.5000000000000000
 
 query R
-SELECT avg(k) OVER (ORDER BY kv.*) FROM kv ORDER BY 1
+SELECT avg(k) OVER (ORDER BY 1-w) FROM kv ORDER BY 1
 ----
-1.0000000000000000
-2.0000000000000000
-3.0000000000000000
 3.7500000000000000
-4.4000000000000000
+3.7500000000000000
+4.0000000000000000
+4.0000000000000000
 5.0000000000000000
+5.0000000000000000
+
+# Not working until #12482 is fixed.
+#query R
+#SELECT avg(k) OVER (ORDER BY kv.*) FROM kv ORDER BY 1
+#----
+#1.0000000000000000
+#2.0000000000000000
+#3.0000000000000000
+#3.7500000000000000
+#4.4000000000000000
+#5.0000000000000000
 
 query R
 SELECT avg(k) OVER (ORDER BY w DESC) FROM kv ORDER BY 1
@@ -515,8 +527,8 @@ EXPLAIN (TYPES) SELECT k, stddev(d) OVER (PARTITION BY v, 'a') FROM kv ORDER BY 
 0  select         result                                         (k int, "stddev(d) OVER (PARTITION BY v, 'a')" decimal)
 1  sort           result                                         (k int, "stddev(d) OVER (PARTITION BY v, 'a')" decimal)
 2  window         result                                         (k int, "stddev(d) OVER (PARTITION BY v, 'a')" decimal, "variance(d) OVER (PARTITION BY v, 100)" decimal)
-2  window         render stddev(d) OVER (PARTITION BY v, 'a')    (stddev((d)[decimal]) OVER (PARTITION BY (v)[int], 'a'))[decimal]
-2  window         render variance(d) OVER (PARTITION BY v, 100)  (variance((d)[decimal]) OVER (PARTITION BY (v)[int], 100))[decimal]
+2  window         render stddev(d) OVER (PARTITION BY v, 'a')    (stddev((d)[decimal]) OVER (PARTITION BY (v)[int], ('a')[string]))[decimal]
+2  window         render variance(d) OVER (PARTITION BY v, 100)  (variance((d)[decimal]) OVER (PARTITION BY (v)[int], (100)[int]))[decimal]
 3  render/filter  result                                         (k int, d decimal, d decimal, v int, "'a'" string, v int, "100" int)
 3  render/filter  render 0                                       (k)[int]
 3  render/filter  render 1                                       (d)[decimal]
@@ -528,13 +540,27 @@ EXPLAIN (TYPES) SELECT k, stddev(d) OVER (PARTITION BY v, 'a') FROM kv ORDER BY 
 4  scan           result                                         (k int, v int, w[omitted] int, f[omitted] float, d decimal, s[omitted] string, b[omitted] bool)
 
 query ITTT
+EXPLAIN (TYPES,NORMALIZE) SELECT k, stddev(d) OVER (PARTITION BY v, 'a') FROM kv ORDER BY k
+----
+0  select         result                                       (k int, "stddev(d) OVER (PARTITION BY v, 'a')" decimal)
+1  sort           result                                       (k int, "stddev(d) OVER (PARTITION BY v, 'a')" decimal)
+2  window         result                                       (k int, "stddev(d) OVER (PARTITION BY v, 'a')" decimal)
+2  window         render stddev(d) OVER (PARTITION BY v, 'a')  (stddev((d)[decimal]) OVER (PARTITION BY (v)[int], ('a')[string]))[decimal]
+3  render/filter  result                                       (k int, d decimal, v int, "'a'" string)
+3  render/filter  render 0                                     (k)[int]
+3  render/filter  render 1                                     (d)[decimal]
+3  render/filter  render 2                                     (v)[int]
+3  render/filter  render 3                                     ('a')[string]
+4  scan           result                                       (k int, v int, w[omitted] int, f[omitted] float, d decimal, s[omitted] string, b[omitted] bool)
+
+query ITTT
 EXPLAIN (TYPES) SELECT k, k + stddev(d) OVER (PARTITION BY v, 'a') FROM kv ORDER BY variance(d) OVER (PARTITION BY v, 100), k
 ----
 0  select         result                                           (k int, "k + stddev(d) OVER (PARTITION BY v, 'a')" decimal)
 1  sort           result                                           (k int, "k + stddev(d) OVER (PARTITION BY v, 'a')" decimal)
 2  window         result                                           (k int, "k + stddev(d) OVER (PARTITION BY v, 'a')" decimal, "variance(d) OVER (PARTITION BY v, 100)" decimal)
-2  window         render k + stddev(d) OVER (PARTITION BY v, 'a')  ((k)[int] + (stddev((d)[decimal]) OVER (PARTITION BY (v)[int], 'a'))[decimal])[decimal]
-2  window         render variance(d) OVER (PARTITION BY v, 100)    (variance((d)[decimal]) OVER (PARTITION BY (v)[int], 100))[decimal]
+2  window         render k + stddev(d) OVER (PARTITION BY v, 'a')  ((k)[int] + (stddev((d)[decimal]) OVER (PARTITION BY (v)[int], ('a')[string]))[decimal])[decimal]
+2  window         render variance(d) OVER (PARTITION BY v, 100)    (variance((d)[decimal]) OVER (PARTITION BY (v)[int], (100)[int]))[decimal]
 3  render/filter  result                                           (k int, d decimal, d decimal, v int, "'a'" string, v int, "100" int, k int)
 3  render/filter  render 0                                         (k)[int]
 3  render/filter  render 1                                         (d)[decimal]