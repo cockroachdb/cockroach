d8cd5b254021d89b1d7e7229aa79e77362ffa6a1
Raphael 'kena' Poss <knz@cockroachlabs.com>
2016-12-20 18:12:53 +0000

    sql/parser: export fmtFlags.ShowTableAliases.

--- pkg/sql/parser/format.go
+++ pkg/sql/parser/format.go
@@ -23,7 +23,7 @@ import (
 
 type fmtFlags struct {
 	showTypes        bool
-	showTableAliases bool
+	ShowTableAliases bool
 	symbolicVars     bool
 	// tableNameNormalizer will be called on all NormalizableTableNames if it is
 	// non-nil. Its results will be used if they are non-nil, or ignored if they
@@ -50,7 +50,7 @@ var FmtSimple FmtFlags = &fmtFlags{showTypes: false}
 
 // FmtQualify instructs the pretty-printer to qualify names with the
 // table name.
-var FmtQualify FmtFlags = &fmtFlags{showTableAliases: true}
+var FmtQualify FmtFlags = &fmtFlags{ShowTableAliases: true}
 
 // FmtShowTypes instructs the pretty-printer to
 // annotate expressions with their resolved types.
