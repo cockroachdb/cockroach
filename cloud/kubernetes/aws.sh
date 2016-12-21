4d151cdcbf24ee51215702761082395a2c1df178
Alex Robinson <alexdwanerobinson@gmail.com>
2016-12-21 12:31:41 -0500

    cloud: Update Kubernetes docs to reflect the changes in version 1.5

--- cloud/kubernetes/aws.sh
+++ cloud/kubernetes/aws.sh
@@ -3,8 +3,8 @@
 set -exuo pipefail
 
 # Clean up anything from a prior run:
-kubectl delete petsets,pods,persistentvolumes,persistentvolumeclaims,services -l app=cockroachdb
+kubectl delete statefulsets,pods,persistentvolumes,persistentvolumeclaims,services -l app=cockroachdb
 
 # The persistent volume auto-provisioner will create the necessary persistent
 # volumes for us, unlike when using Minikube.
-kubectl create -f cockroachdb-petset.yaml
+kubectl create -f cockroachdb-statefulset.yaml
