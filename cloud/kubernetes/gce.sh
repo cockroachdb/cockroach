0187871d244530006cfc31dcfca3a39c2c39c81e
Alex Robinson <alexdwanerobinson@gmail.com>
2016-12-21 23:28:16 -0500

    kubernetes: Properly delete PodDisruptionBudget resources
    Also add an ImagePullPolicy to the init container, since apparently
    1.5.1 has some issues with not adding a default pull policy to init
    containers (and it's decent practice anyway)

--- cloud/kubernetes/gce.sh
+++ cloud/kubernetes/gce.sh
@@ -3,7 +3,7 @@
 set -exuo pipefail
 
 # Clean up anything from a prior run:
-kubectl delete statefulsets,pods,persistentvolumes,persistentvolumeclaims,services -l app=cockroachdb
+kubectl delete statefulsets,pods,persistentvolumes,persistentvolumeclaims,services,poddisruptionbudget -l app=cockroachdb
 
 # The persistent volume auto-provisioner will create the necessary persistent
 # volumes for us, unlike when using Minikube.
