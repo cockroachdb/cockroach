fcceba04f962e7725ff0c3e5a72ddd9c0712feb6
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-01-03 19:01:34 +0000

    prod+test: capture more output in acceptance/CI builds.
    This patch ensures that more logging output lands into the `artifacts`
    sub-directory, and also reports detailed tracing of shell script
    execution.

--- build/teamcity-acceptance.sh
+++ build/teamcity-acceptance.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 set -euxo pipefail
+
 build/builder.sh make build
 build/builder.sh make install
 build/builder.sh go test -v -c -tags acceptance ./pkg/acceptance
+
+# The log files that should be created by -l below can only
+# be created if the parent directory already exists. Ensure
+# that it exists before running the test.
+mkdir -p artifacts/acceptance
+
 cd pkg/acceptance
 ../../acceptance.test -nodes 3 -l ../../artifacts/acceptance -test.v -test.timeout 10m 2>&1 | go-test-teamcity
