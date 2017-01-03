9e2e7922b3c8cfd106e22ddcf57f070eab80d377
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-01-03 18:59:53 +0000

    acceptance: make logs go to files
    This patch causes acceptance logs to be emitted to files, with only
    errors (severity ERROR or higher) emitted to stderr.
    
    The files are deleted if the test succeeds, but preserved if a test
    fails and the location of the directory is printed in the test's
    failure message.

--- pkg/acceptance/gossip_peerings_test.go
+++ pkg/acceptance/gossip_peerings_test.go
@@ -33,6 +33,9 @@ const longWaitTime = 2 * time.Minute
 const shortWaitTime = 20 * time.Second
 
 func TestGossipPeerings(t *testing.T) {
+	s := log.Scope(t, "")
+	defer s.Close(t)
+
 	runTestOnConfigs(t, testGossipPeeringsInner)
 }
 
@@ -75,6 +78,9 @@ func testGossipPeeringsInner(
 // re-bootstrapped after a time when all nodes were down
 // simultaneously.
 func TestGossipRestart(t *testing.T) {
+	s := log.Scope(t, "")
+	defer s.Close(t)
+
 	// TODO(bram): #4559 Limit this test to only the relevant cases. No chaos
 	// agents should be required.
 	runTestOnConfigs(t, testGossipRestartInner)
