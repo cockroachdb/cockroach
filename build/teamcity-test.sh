bf8fbb02e104c8b52f2695f993abf7d54539d68f
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-01-04 11:50:08 +0000

    build: use `env` to set environment variables in docker run.

--- build/teamcity-test.sh
+++ build/teamcity-test.sh
@@ -3,6 +3,15 @@ set -euxo pipefail
 
 mkdir -p artifacts
 
-build/builder.sh make test COCKROACH_PROPOSER_EVALUATED_KV="${COCKROACH_PROPOSER_EVALUATED_KV-}" TESTFLAGS='-v' 2>&1 | tee artifacts/test.log | go-test-teamcity
+build/builder.sh env \
+		 COCKROACH_PROPOSER_EVALUATED_KV="${COCKROACH_PROPOSER_EVALUATED_KV:-false}" \
+		 make test \
+		 TESTFLAGS='-v' \
+		 2>&1 \
+    | tee artifacts/test.log \
+    | go-test-teamcity
 
-build/builder.sh env BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" TARGET=stress github-pull-request-make
+build/builder.sh env \
+		 BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" \
+		 TARGET=stress \
+		 github-pull-request-make
