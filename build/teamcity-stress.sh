ac4e780048bbdfbb8a7b6c488f3d5d9c9ee5c0d5
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-01-04 11:50:08 +0000

    build: properly propagate COCKROACH_PROPOSER_EVALUATED_KV to the docker builds.

--- build/teamcity-stress.sh
+++ build/teamcity-stress.sh
@@ -4,10 +4,26 @@ set -euxo pipefail
 mkdir -p artifacts
 
 exit_status=0
-build/builder.sh make stress COCKROACH_NIGHTLY_STRESS="${COCKROACH_NIGHTLY_STRESS-}" COCKROACH_PROPOSER_EVALUATED_KV="${COCKROACH_PROPOSER_EVALUATED_KV-}" PKG="$PKG" GOFLAGS="${GOFLAGS-}" TAGS="${TAGS-}" TESTTIMEOUT=30m TESTFLAGS='-test.v' STRESSFLAGS='-maxtime 15m -maxfails 1 -stderr' 2>&1 | tee artifacts/stress.log || exit_status=$?
+build/builder.sh env \
+		 COCKROACH_NIGHTLY_STRESS="${COCKROACH_NIGHTLY_STRESS-false}" \
+		 COCKROACH_PROPOSER_EVALUATED_KV="${COCKROACH_PROPOSER_EVALUATED_KV-false}" \
+		 make stress \
+		 PKG="$PKG" GOFLAGS="${GOFLAGS-}" TAGS="${TAGS-}" \
+		 TESTTIMEOUT=30m TESTFLAGS='-test.v' \
+		 STRESSFLAGS='-maxtime 15m -maxfails 1 -stderr' \
+		 2>&1 \
+    | tee artifacts/stress.log \
+    || exit_status=$?
 
 if [ $exit_status -ne 0 ]; then
-  build/builder.sh env GITHUB_API_TOKEN="$GITHUB_API_TOKEN" BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" TC_BUILD_ID="$TC_BUILD_ID" TC_SERVER_URL="$TC_SERVER_URL" COCKROACH_PROPOSER_EVALUATED_KV="${COCKROACH_PROPOSER_EVALUATED_KV-}" PKG="$PKG" GOFLAGS="${GOFLAGS-}" TAGS="${TAGS-}" github-post < artifacts/stress.log
+    build/builder.sh env \
+		     GITHUB_API_TOKEN="$GITHUB_API_TOKEN" \
+		     BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" \
+		     TC_BUILD_ID="$TC_BUILD_ID" \
+		     TC_SERVER_URL="$TC_SERVER_URL" \
+		     COCKROACH_PROPOSER_EVALUATED_KV="${COCKROACH_PROPOSER_EVALUATED_KV-false}" \
+		     PKG="$PKG" GOFLAGS="${GOFLAGS-}" TAGS="${TAGS-}" \
+		     github-post < artifacts/stress.log
 fi;
 
 exit $exit_status
--- build/teamcity-testrace.sh
+++ build/teamcity-testrace.sh
@@ -3,6 +3,6 @@ set -euxo pipefail
 
 mkdir -p artifacts
 
-build/builder.sh make testrace COCKROACH_PROPOSER_EVALUATED_KV="${COCKROACH_PROPOSER_EVALUATED_KV-}" TESTFLAGS='-v' 2>&1 | tee artifacts/testrace.log | go-test-teamcity
+build/builder.sh env COCKROACH_PROPOSER_EVALUATED_KV="${COCKROACH_PROPOSER_EVALUATED_KV-false}" make testrace TESTFLAGS='-v' 2>&1 | tee artifacts/testrace.log | go-test-teamcity
 
 build/builder.sh env BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" TARGET=stressrace github-pull-request-make
