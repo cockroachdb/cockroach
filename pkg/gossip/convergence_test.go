f80c741578c3b0f901fb28514287d8e1cb3a025f
Alex Robinson <alexdwanerobinson@gmail.com>
2017-01-12 00:24:42 -0500

    gossip: Enforce 0 <= nodeSet.len() <= nodeSet.maxSize
    Before this change, we'd let nodes open up more than maxSize outgoing
    connections because new connections would only be added to the outgoing
    nodeSet after they had been established (so that we'd have the peerID).
    Until a connection was established and we received the peer's ID, the
    calls to outgoing.hasSpace in gossip.go would let more connections be
    established.
    
    In small clusters, this isn't a big deal, but in large clusters it can
    cause far too many connections to get opened, leading to a lot of
    connections getting bounced around as they get refused by nodes that are
    at their limit of incoming connections. Making matters even worse is
    that doDisconnected doesn't check whether there's room to connect to
    the forwardAddr, meaning that once a cluster has
    `total outgoing conns` > `total allowed incoming conns`, those extra
    connections can just keep bouncing around indefinitely.
    
    By ensuring we don't open too many connections, we can keep that from
    happening.
    
    On a 100 cycle simulation of a 64 node cluster, this takes the number of
    refused connections down from ~3700 to ~750 (or from ~5300 to ~875) if
    you take out the previous change to avoid duplicates in tightenNetwork).
    
    More notably, the number of refused connections stops growing completely
    shortly after ~50 cycles, indicating a lack of the indefinite thrashing
    that we've seen on such large clusters in the wild.

--- pkg/gossip/convergence_test.go
+++ pkg/gossip/convergence_test.go
@@ -19,15 +19,30 @@ package gossip_test
 import (
 	"testing"
 
-	"golang.org/x/net/context"
-
 	"github.com/cockroachdb/cockroach/pkg/gossip/simulation"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
-	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
 )
 
-// TestConvergence verifies a 10 node gossip network converges within
+// The tests in this package have fairly small cluster sizes for the sake of
+// not taking too long to run when run as part of the normal unit tests. If
+// you're testing out gossip network behavior, you may find it useful to
+// increase the network size for these tests (adjusting the max thresholds
+// accordingly) and see how things behave.
+const (
+	testConvergenceSize        = 10
+	testReachesEquilibriumSize = 24
+)
+
+func connectionsRefused(network *simulation.Network) int64 {
+	var connsRefused int64
+	for _, node := range network.Nodes {
+		connsRefused += node.Gossip.GetNodeMetrics().ConnectionsRefused.Count()
+	}
+	return connsRefused
+}
+
+// TestConvergence verifies that a node gossip network converges within
 // a fixed number of simulation cycles. It's really difficult to
 // determine the right number for cycles because different things can
 // happen during a single cycle, depending on how much CPU time is
@@ -35,16 +50,68 @@ import (
 // synchronization primitives in place for the simulation is possible,
 // though two attempts so far have introduced more complexity into the
 // actual production gossip code than seems worthwhile for a unittest.
+// As such, the thresholds are drastically higher than is normally needed.
+//
+// As of Jan 2017, this normally takes ~12 cycles and 8-12 refused connections.
 func TestConvergence(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	stopper := stop.NewStopper()
 	defer stopper.Stop()
 
-	network := simulation.NewNetwork(stopper, 10, true)
+	network := simulation.NewNetwork(stopper, testConvergenceSize, true)
 
 	const maxCycles = 100
 	if connectedCycle := network.RunUntilFullyConnected(); connectedCycle > maxCycles {
-		log.Warningf(context.TODO(), "expected a fully-connected network within %d cycles; took %d",
+		t.Errorf("expected a fully-connected network within %d cycles; took %d",
 			maxCycles, connectedCycle)
+	}
+
+	const maxConnsRefused = 50
+	if connsRefused := connectionsRefused(network); connsRefused > maxConnsRefused {
+		t.Errorf("expected network to fully connect with <= %d connections refused; took %d",
+			maxConnsRefused, connsRefused)
+	}
+}
+
+// TestNetworkReachesEquilibrium ensures that the gossip network stops bouncing
+// refused connections around after a while and settles down.
+// As mentioned in the comment for TestConvergence, there is a large amount of
+// variability in how much gets done in each network cycle, and thus we have
+// to set thresholds that are drastically higher than is needed in the normal
+// case.
+//
+// As of Jan 2017, this normally takes 8-9 cycles and 50-60 refused connections.
+func TestNetworkReachesEquilibrium(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	stopper := stop.NewStopper()
+	defer stopper.Stop()
+
+	network := simulation.NewNetwork(stopper, testReachesEquilibriumSize, true)
+
+	var connsRefused int64
+	var cyclesWithoutChange int
+	var numCycles int
+	network.SimulateNetwork(func(cycle int, network *simulation.Network) bool {
+		numCycles = cycle
+		newConnsRefused := connectionsRefused(network)
+		if newConnsRefused > connsRefused {
+			connsRefused = newConnsRefused
+			cyclesWithoutChange = 0
+		} else {
+			cyclesWithoutChange++
+		}
+		return cyclesWithoutChange < 5
+	})
+
+	const maxCycles = 200
+	if numCycles > maxCycles {
+		t.Errorf("expected a non-thrashing network within %d cycles; took %d",
+			maxCycles, numCycles)
+	}
+
+	const maxConnsRefused = 1000
+	if connsRefused > maxConnsRefused {
+		t.Errorf("expected thrashing to die down with <= %d connections refused; took %d",
+			maxConnsRefused, connsRefused)
 	}
 }
