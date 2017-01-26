8ae59898477e7bf7dce89eb1af29e60341f0872f
Vivek Menezes <vivek.menezes@gmail.com>
2017-01-25 21:31:04 -0500

    sql: improve testing of table deletion in the middle of a schema change
    The test now returns a deadline exceeded error in the middle of a schema
    change so that the schema change is retried. Eventually the schema
    change retry fails with ErrDescriptorNotFound because the table is deleted.
    
    The prior test was flaky because normally the schema change would move
    forward until completion, with the table deletion queued up behind the
    schema change waiting to acquire the schema change lease on the table.
    Every once in a while the schema change would hit a retryable error
    giving up the schema change lease and then retry the command after gaining
    the lease again. But in the interim the delete table command would acquire
    the lease and delete the table, resulting in the schema change command
    every once in a while seeing the ErrDescriptorNotFound error. This change
    makes the schema change in the test predictably see ErrDescriptorNotFound
    after partially completing the schema change.
    
    fixes #13014

--- pkg/sql/schema_changer_test.go
+++ pkg/sql/schema_changer_test.go
@@ -518,16 +518,28 @@ func bulkInsertIntoTable(sqlDB *gosql.DB, maxValue int) error {
 func TestRaceWithBackfill(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	var backfillNotification chan bool
+	var backfillCount int64
+	var partialBackfill bool
 	params, _ := createTestServerParams()
 	// Disable asynchronous schema change execution to allow synchronous path
 	// to trigger start of backfill notification.
 	params.Knobs = base.TestingKnobs{
 		SQLSchemaChanger: &csql.SchemaChangerTestingKnobs{
 			RunBeforeBackfillChunk: func(sp roachpb.Span) error {
-				if backfillNotification != nil {
-					// Close channel to notify that the backfill has started.
-					close(backfillNotification)
-					backfillNotification = nil
+				notifyBackfill := func() {
+					if backfillNotification != nil {
+						// Close channel to notify that the backfill has started.
+						close(backfillNotification)
+						backfillNotification = nil
+					}
+				}
+				if partialBackfill {
+					if atomic.AddInt64(&backfillCount, 1) > 1 {
+						notifyBackfill()
+						return context.DeadlineExceeded
+					}
+				} else {
+					notifyBackfill()
 				}
 				return nil
 			},
@@ -648,24 +660,22 @@ CREATE UNIQUE INDEX vidx ON t.test (v);
 
 	notification := make(chan bool)
 	backfillNotification = notification
+	partialBackfill = true
 	// Run the schema change in a separate goroutine.
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
-		// Start schema change that eventually runs a backfill.
-		if _, err := sqlDB.Exec("CREATE UNIQUE INDEX bar ON t.test (v)"); err != nil {
+		// Start schema change that eventually runs a partial backfill.
+		if _, err := sqlDB.Exec(
+			"CREATE UNIQUE INDEX bar ON t.test (v)",
+		); !testutils.IsError(err, sqlbase.ErrDescriptorNotFound.Error()) {
 			t.Error(err)
 		}
 		wg.Done()
 	}()
 
-	// Wait until the schema change backfill starts.
+	// Wait until the schema change backfill is partially complete.
 	<-notification
-
-	// Wait for a short bit to ensure that the backfill has likely progressed
-	// and written some data, but not long enough that the backfill has
-	// completed.
-	time.Sleep(10 * time.Millisecond)
 
 	if _, err := sqlDB.Exec("DROP TABLE t.test"); err != nil {
 		t.Fatal(err)
