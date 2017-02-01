385fac1c6cae1d1c85593b0533be3c2728ace9d4
marc <marc@cockroachlabs.com>
2017-02-01 11:19:06 -0500

    Determine path to build/ subdirectory at start of script

--- build/teamcity-stress-meta.sh
+++ build/teamcity-stress-meta.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-build/builder.sh make .bootstrap # explicitly recompile teamcity-trigger
-build/builder.sh env TC_API_USER="$TC_API_USER" TC_API_PASSWORD="$TC_API_PASSWORD" TC_SERVER_URL="$TC_SERVER_URL" teamcity-trigger
+build_dir="$(dirname $0)"
+
+"${build_dir}"/builder.sh make .bootstrap # explicitly recompile teamcity-trigger
+"${build_dir}"/builder.sh env TC_API_USER="$TC_API_USER" TC_API_PASSWORD="$TC_API_PASSWORD" TC_SERVER_URL="$TC_SERVER_URL" teamcity-trigger
