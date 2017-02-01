cockroachdb/cockroach

commit 51cf49687eae9982d50cf412b3a5d210d797b4b9

move .dockercfg template closer to use

build/teamcity-release-upload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-sed "s/<EMAIL>/$DOCKER_EMAIL/;s/<AUTH>/$DOCKER_AUTH/" < "resource/deploy_templates/.dockercfg.template" > ~/.dockercfg
+sed "s/<EMAIL>/$DOCKER_EMAIL/;s/<AUTH>/$DOCKER_AUTH/" < .dockercfg.in > ~/.dockercfg
 
 case "$TC_BUILD_BRANCH" in
   master)

commit b0c1c3e2468195b0eabc1217e7c38e8387f52cf6

Fix path to .dockercfg.in.
This has been broken since 51cf49687eae9982d50cf412b3a5d210d797b4b9 and
has prevented master builds since.

It would be best to build the local path (eg: `dirname $0`), but we're
already using a fixed relative path everywhere else in this script.

build/teamcity-release-upload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-sed "s/<EMAIL>/$DOCKER_EMAIL/;s/<AUTH>/$DOCKER_AUTH/" < .dockercfg.in > ~/.dockercfg
+sed "s/<EMAIL>/$DOCKER_EMAIL/;s/<AUTH>/$DOCKER_AUTH/" < build/.dockercfg.in > ~/.dockercfg
 
 case "$TC_BUILD_BRANCH" in
   master)