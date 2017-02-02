cockroachdb/cockroach

commit 4b0881a2179e64df808794206783ba42c0a4d400

sql: disentangle planNode from SetLimitHint.
This patch extracts the SetLimitHint logic into its own recursion and
sanitizes it. It also ensures that all limits are propagated during
`startPlan`.

pkg/sql/limit_opt.go
@@ -1,0 +1,199 @@
+// Copyright 2017 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// Author: Raphael 'kena' Poss (knz@cockroachlabs.com)
+
+package sql
+
+import (
+	"fmt"
+	"math"
+)
+
+// applyLimit tells this node to optimize things under the assumption that
+// we will only need the first `numRows` rows.
+//
+// The special value math.MaxInt64 indicates "no limit".
+//
+// If soft is true, this is a "soft" limit and is only a hint; the node must
+// still be able to produce all results if requested.
+//
+// If soft is false, this is a "hard" limit and is a promise that Next will
+// never be called more than numRows times.
+//
+// The action of calling this method triggers limit-based query plan
+// optimizations, e.g. in expandSelectNode(). The primary user is
+// limitNode.Start() after it has fully evaluated the limit and
+// offset expressions. EXPLAIN also does this, see expandPlan() for
+// explainPlanNode.
+//
+// TODO(radu) Arguably, this interface has room for improvement.  A
+// limitNode may have a hard limit locally which is larger than the
+// soft limit propagated up by nodes downstream. We may want to
+// improve this API to pass both the soft and hard limit.
+func applyLimit(plan planNode, numRows int64, soft bool) {
+	switch n := plan.(type) {
+	case *scanNode:
+		// Either a limitNode or EXPLAIN is pushing a limit down onto this
+		// node. The special value math.MaxInt64 means "no limit".
+		if !n.disableBatchLimits && numRows != math.MaxInt64 {
+			n.limitHint = numRows
+			// If we have a filter, some of the rows we retrieve may not pass the
+			// filter so the limit becomes "soft".
+			n.limitSoft = soft || !isFilterTrue(n.filter)
+		}
+
+	case *limitNode:
+		// A higher-level limitNode or EXPLAIN is pushing a limit down onto
+		// this node. Accept it unless the local limit is definitely
+		// smaller, in which case we propagate that as a hard limit instead.
+		// TODO(radu): we may get a smaller "soft" limit from the upper node
+		// and we may have a larger "hard" limit locally. In general, it's
+		// not clear which of those results in less work.
+		if !n.evaluated {
+			n.estimateLimit()
+		}
+		hintCount := numRows
+		if hintCount > n.count {
+			hintCount = n.count
+			soft = false
+		}
+		applyLimit(n.plan, getLimit(hintCount, n.offset), soft)
+
+	case *sortNode:
+		if n.needSort && numRows != math.MaxInt64 {
+			v := n.p.newContainerValuesNode(n.plan.Columns(), int(numRows))
+			v.ordering = n.ordering
+			if soft {
+				n.sortStrategy = newIterativeSortStrategy(v)
+			} else {
+				n.sortStrategy = newSortTopKStrategy(v, numRows)
+			}
+		}
+		if n.needSort {
+			// We can't propagate the limit, because the sort
+			// potentially needs all rows.
+			numRows = math.MaxInt64
+			soft = true
+			applyLimit(n.plan, numRows, soft)
+		}
+
+	case *groupNode:
+		if len(n.desiredOrdering) > 0 {
+			match := computeOrderingMatch(n.desiredOrdering, n.plan.Ordering(), false)
+			if match == len(n.desiredOrdering) {
+				// We have a single MIN/MAX function and the underlying plan's
+				// ordering matches the function. We only need to retrieve one row.
+				applyLimit(n.plan, 1, false /* !soft */)
+				n.needOnlyOneRow = true
+				return
+			}
+		}
+		setUnlimited(n.plan)
+
+	case *indexJoinNode:
+		applyLimit(n.index, numRows, soft)
+		setUnlimited(n.table)
+
+	case *unionNode:
+		applyLimit(n.right, numRows, true)
+		applyLimit(n.left, numRows, true)
+
+	case *distinctNode:
+		applyLimit(n.plan, numRows, true)
+
+	case *filterNode:
+		applyLimit(n.source.plan, numRows, soft || !isFilterTrue(n.filter))
+
+	case *renderNode:
+		applyLimit(n.source.plan, numRows, soft)
+
+	case *windowNode:
+		setUnlimited(n.plan)
+
+	case *joinNode:
+		setUnlimited(n.left.plan)
+		setUnlimited(n.right.plan)
+
+	case *ordinalityNode:
+		applyLimit(n.source, numRows, soft)
+
+	case *deleteNode:
+		setUnlimited(n.run.rows)
+	case *updateNode:
+		setUnlimited(n.run.rows)
+	case *insertNode:
+		setUnlimited(n.run.rows)
+	case *createTableNode:
+		if n.sourcePlan != nil {
+			applyLimit(n.sourcePlan, numRows, soft)
+		}
+	case *createViewNode:
+		setUnlimited(n.sourcePlan)
+	case *explainDebugNode:
+		setUnlimited(n.plan)
+	case *explainTraceNode:
+		setUnlimited(n.plan)
+	case *explainPlanNode:
+		setUnlimited(n.plan)
+
+	case *delayedNode:
+		if n.plan != nil {
+			setUnlimited(n.plan)
+		}
+
+	case *selectTopNode:
+		// A selectTopNode can only be encountered here if using
+		// EXPLAIN(NOEXPAND). At this point, the selectTopNode is not
+		// fully connected so we must be naive about limits.
+		if n.limit != nil || n.group != nil || n.distinct != nil || n.sort != nil {
+			numRows = math.MaxInt64
+			soft = true
+		}
+		applyLimit(n.source, numRows, soft)
+
+	case *valuesNode:
+	case *alterTableNode:
+	case *copyNode:
+	case *createDatabaseNode:
+	case *createIndexNode:
+	case *createUserNode:
+	case *dropDatabaseNode:
+	case *dropIndexNode:
+	case *dropTableNode:
+	case *dropViewNode:
+	case *emptyNode:
+	case *hookFnNode:
+	case *splitNode:
+	case *valueGenerator:
+
+	default:
+		panic(fmt.Sprintf("unhandled node type: %T", plan))
+	}
+}
+
+func setUnlimited(plan planNode) {
+	applyLimit(plan, math.MaxInt64, true)
+}
+
+// getLimit computes the actual number of rows to request from the
+// data source to honour both the required count and offset together.
+// This also ensures that the resulting number of rows does not
+// overflow.
+func getLimit(count, offset int64) int64 {
+	if offset > math.MaxInt64-count {
+		count = math.MaxInt64 - offset
+	}
+	return count + offset
+}

pkg/sql/alter_table.go
@@ -464,14 +464,13 @@ func (n *alterTableNode) Start() error {
 	return nil
 }
 
-func (n *alterTableNode) Next() (bool, error)          { return false, nil }
-func (n *alterTableNode) Close()                       {}
-func (n *alterTableNode) Columns() ResultColumns       { return make(ResultColumns, 0) }
-func (n *alterTableNode) Ordering() orderingInfo       { return orderingInfo{} }
-func (n *alterTableNode) Values() parser.DTuple        { return parser.DTuple{} }
-func (n *alterTableNode) DebugValues() debugValues     { return debugValues{} }
-func (n *alterTableNode) SetLimitHint(_ int64, _ bool) {}
-func (n *alterTableNode) MarkDebug(mode explainMode)   {}
+func (n *alterTableNode) Next() (bool, error)        { return false, nil }
+func (n *alterTableNode) Close()                     {}
+func (n *alterTableNode) Columns() ResultColumns     { return make(ResultColumns, 0) }
+func (n *alterTableNode) Ordering() orderingInfo     { return orderingInfo{} }
+func (n *alterTableNode) Values() parser.DTuple      { return parser.DTuple{} }
+func (n *alterTableNode) DebugValues() debugValues   { return debugValues{} }
+func (n *alterTableNode) MarkDebug(mode explainMode) {}
 
 func applyColumnMutation(
 	col *sqlbase.ColumnDescriptor, mut parser.ColumnMutationCmd, searchPath parser.SearchPath,

pkg/sql/backfill.go
@@ -674,14 +674,16 @@ func (sc *SchemaChanger) backfillIndexesChunk(
 		scan := planner.Scan()
 		scan.desc = *tableDesc
 		scan.spans = []roachpb.Span{sp}
-		scan.SetLimitHint(chunkSize, false)
 		scan.initDescDefaults(publicAndNonPublicColumns)
+
+		// We manually invoke selectIndex() because for now expandPlan()
+		// can only do so itself when looking at a renderNode.
 		rows, err := selectIndex(scan, nil, false)
 		if err != nil {
 			return err
 		}
 
-		if err := planner.startPlan(rows); err != nil {
+		if err := planner.startPlanWithLimit(rows, chunkSize); err != nil {
 			return err
 		}
 

pkg/sql/copy.go
@@ -49,12 +49,11 @@ type copyNode struct {
 	rowsMemAcc    WrappableMemoryAccount
 }
 
-func (n *copyNode) Columns() ResultColumns     { return n.resultColumns }
-func (*copyNode) Ordering() orderingInfo       { return orderingInfo{} }
-func (*copyNode) Values() parser.DTuple        { return nil }
-func (*copyNode) SetLimitHint(_ int64, _ bool) {}
-func (*copyNode) MarkDebug(_ explainMode)      {}
-func (*copyNode) Next() (bool, error)          { return false, nil }
+func (n *copyNode) Columns() ResultColumns { return n.resultColumns }
+func (*copyNode) Ordering() orderingInfo   { return orderingInfo{} }
+func (*copyNode) Values() parser.DTuple    { return nil }
+func (*copyNode) MarkDebug(_ explainMode)  {}
+func (*copyNode) Next() (bool, error)      { return false, nil }
 
 func (n *copyNode) Close() {
 	n.rowsMemAcc.Wtxn(n.p.session).Close()

pkg/sql/create.go
@@ -112,14 +112,13 @@ func (n *createDatabaseNode) Start() error {
 	return nil
 }
 
-func (n *createDatabaseNode) Next() (bool, error)          { return false, nil }
-func (n *createDatabaseNode) Close()                       {}
-func (n *createDatabaseNode) Columns() ResultColumns       { return make(ResultColumns, 0) }
-func (n *createDatabaseNode) Ordering() orderingInfo       { return orderingInfo{} }
-func (n *createDatabaseNode) Values() parser.DTuple        { return parser.DTuple{} }
-func (n *createDatabaseNode) DebugValues() debugValues     { return debugValues{} }
-func (n *createDatabaseNode) SetLimitHint(_ int64, _ bool) {}
-func (n *createDatabaseNode) MarkDebug(mode explainMode)   {}
+func (n *createDatabaseNode) Next() (bool, error)        { return false, nil }
+func (n *createDatabaseNode) Close()                     {}
+func (n *createDatabaseNode) Columns() ResultColumns     { return make(ResultColumns, 0) }
+func (n *createDatabaseNode) Ordering() orderingInfo     { return orderingInfo{} }
+func (n *createDatabaseNode) Values() parser.DTuple      { return parser.DTuple{} }
+func (n *createDatabaseNode) DebugValues() debugValues   { return debugValues{} }
+func (n *createDatabaseNode) MarkDebug(mode explainMode) {}
 
 type createIndexNode struct {
 	p         *planner
@@ -223,14 +222,13 @@ func (n *createIndexNode) Start() error {
 	return nil
 }
 
-func (n *createIndexNode) Next() (bool, error)          { return false, nil }
-func (n *createIndexNode) Close()                       {}
-func (n *createIndexNode) Columns() ResultColumns       { return make(ResultColumns, 0) }
-func (n *createIndexNode) Ordering() orderingInfo       { return orderingInfo{} }
-func (n *createIndexNode) Values() parser.DTuple        { return parser.DTuple{} }
-func (n *createIndexNode) DebugValues() debugValues     { return debugValues{} }
-func (n *createIndexNode) SetLimitHint(_ int64, _ bool) {}
-func (n *createIndexNode) MarkDebug(mode explainMode)   {}
+func (n *createIndexNode) Next() (bool, error)        { return false, nil }
+func (n *createIndexNode) Close()                     {}
+func (n *createIndexNode) Columns() ResultColumns     { return make(ResultColumns, 0) }
+func (n *createIndexNode) Ordering() orderingInfo     { return orderingInfo{} }
+func (n *createIndexNode) Values() parser.DTuple      { return parser.DTuple{} }
+func (n *createIndexNode) DebugValues() debugValues   { return debugValues{} }
+func (n *createIndexNode) MarkDebug(mode explainMode) {}
 
 type createUserNode struct {
 	p        *planner
@@ -301,14 +299,13 @@ func (n *createUserNode) Start() error {
 	return nil
 }
 
-func (n *createUserNode) Next() (bool, error)          { return false, nil }
-func (n *createUserNode) Close()                       {}
-func (n *createUserNode) Columns() ResultColumns       { return make(ResultColumns, 0) }
-func (n *createUserNode) Ordering() orderingInfo       { return orderingInfo{} }
-func (n *createUserNode) Values() parser.DTuple        { return parser.DTuple{} }
-func (n *createUserNode) DebugValues() debugValues     { return debugValues{} }
-func (n *createUserNode) SetLimitHint(_ int64, _ bool) {}
-func (n *createUserNode) MarkDebug(mode explainMode)   {}
+func (n *createUserNode) Next() (bool, error)        { return false, nil }
+func (n *createUserNode) Close()                     {}
+func (n *createUserNode) Columns() ResultColumns     { return make(ResultColumns, 0) }
+func (n *createUserNode) Ordering() orderingInfo     { return orderingInfo{} }
+func (n *createUserNode) Values() parser.DTuple      { return parser.DTuple{} }
+func (n *createUserNode) DebugValues() debugValues   { return debugValues{} }
+func (n *createUserNode) MarkDebug(mode explainMode) {}
 
 type createViewNode struct {
 	p           *planner
@@ -461,13 +458,12 @@ func (n *createViewNode) Close() {
 	n.sourcePlan = nil
 }
 
-func (n *createViewNode) Next() (bool, error)          { return false, nil }
-func (n *createViewNode) Columns() ResultColumns       { return make(ResultColumns, 0) }
-func (n *createViewNode) Ordering() orderingInfo       { return orderingInfo{} }
-func (n *createViewNode) Values() parser.DTuple        { return parser.DTuple{} }
-func (n *createViewNode) DebugValues() debugValues     { return debugValues{} }
-func (n *createViewNode) SetLimitHint(_ int64, _ bool) {}
-func (n *createViewNode) MarkDebug(mode explainMode)   {}
+func (n *createViewNode) Next() (bool, error)        { return false, nil }
+func (n *createViewNode) Columns() ResultColumns     { return make(ResultColumns, 0) }
+func (n *createViewNode) Ordering() orderingInfo     { return orderingInfo{} }
+func (n *createViewNode) Values() parser.DTuple      { return parser.DTuple{} }
+func (n *createViewNode) DebugValues() debugValues   { return debugValues{} }
+func (n *createViewNode) MarkDebug(mode explainMode) {}
 
 type createTableNode struct {
 	p          *planner
@@ -685,13 +681,12 @@ func (n *createTableNode) Close() {
 	}
 }
 
-func (n *createTableNode) Next() (bool, error)          { return false, nil }
-func (n *createTableNode) Columns() ResultColumns       { return make(ResultColumns, 0) }
-func (n *createTableNode) Ordering() orderingInfo       { return orderingInfo{} }
-func (n *createTableNode) Values() parser.DTuple        { return parser.DTuple{} }
-func (n *createTableNode) DebugValues() debugValues     { return debugValues{} }
-func (n *createTableNode) SetLimitHint(_ int64, _ bool) {}
-func (n *createTableNode) MarkDebug(mode explainMode)   {}
+func (n *createTableNode) Next() (bool, error)        { return false, nil }
+func (n *createTableNode) Columns() ResultColumns     { return make(ResultColumns, 0) }
+func (n *createTableNode) Ordering() orderingInfo     { return orderingInfo{} }
+func (n *createTableNode) Values() parser.DTuple      { return parser.DTuple{} }
+func (n *createTableNode) DebugValues() debugValues   { return debugValues{} }
+func (n *createTableNode) MarkDebug(mode explainMode) {}
 
 type indexMatch bool
 

pkg/sql/delayed.go
@@ -30,8 +30,6 @@ type delayedNode struct {
 
 type nodeConstructor func(p *planner) (planNode, error)
 
-func (d *delayedNode) SetLimitHint(_ int64, _ bool) {}
-
 func (d *delayedNode) Close() {
 	if d.plan != nil {
 		d.plan.Close()

pkg/sql/delete.go
@@ -237,5 +237,3 @@ func (d *deleteNode) DebugValues() debugValues {
 }
 
 func (d *deleteNode) Ordering() orderingInfo { return orderingInfo{} }
-
-func (d *deleteNode) SetLimitHint(numRows int64, soft bool) {}

pkg/sql/distinct.go
@@ -198,11 +198,6 @@ func (n *distinctNode) encodeValues(values parser.DTuple) ([]byte, []byte, error
 	return prefix, suffix, err
 }
 
-func (n *distinctNode) SetLimitHint(numRows int64, soft bool) {
-	// Any limit becomes a "soft" limit underneath.
-	n.plan.SetLimitHint(numRows, true)
-}
-
 func (n *distinctNode) Close() {
 	n.plan.Close()
 	n.prefixSeen = nil

commit 1fd46d2ecf4bdda0d391b21e760fd3268cda9124

sql: enable limit propagation past "nosort" nodes.
An oversight in 4b0881a2179e64df808794206783ba42c0a4d400 caused limit
propagation to be blocked by "nosort" nodes (sortNode with !needSort).

Found and fixed by @petermattis.

pkg/sql/limit_opt.go
@@ -86,8 +86,8 @@ func applyLimit(plan planNode, numRows int64, soft bool) {
 			// potentially needs all rows.
 			numRows = math.MaxInt64
 			soft = true
-			applyLimit(n.plan, numRows, soft)
 		}
+		applyLimit(n.plan, numRows, soft)
 
 	case *groupNode:
 		if len(n.desiredOrdering) > 0 {

pkg/sql/testdata/order_by
@@ -158,6 +158,19 @@ EXPLAIN SELECT b FROM t ORDER BY a DESC
 2           table  t@primary
 2           spans  ALL
 
+# Check that LIMIT propagates past nosort nodes.
+query ITTT
+EXPLAIN SELECT b FROM t ORDER BY a LIMIT 1
+----
+0  limit
+1  nosort
+1          order  +a
+2  render
+3  scan
+3          table  t@primary
+3          spans  ALL
+3          limit  1
+
 query ITTT
 EXPLAIN SELECT b FROM t ORDER BY a DESC, b ASC
 ----