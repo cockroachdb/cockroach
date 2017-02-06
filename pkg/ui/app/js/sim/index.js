9bc43be910a7ab8eced101be24121da10cc72166
Matt Tracy <matt@cockroachlabs.com>
2017-02-06 14:24:45 -0500

    ui: Update ClusterViz to latest version
    The Cluster visualization was being updated by spencer in parallel with its
    "conversion" into our Admin UI. This commit installs the latest version, after
    which development should move directly into the admin UI environment.

--- pkg/ui/app/js/sim/index.js
+++ pkg/ui/app/js/sim/index.js
@@ -1,19 +1,26 @@
+var d3 = require("d3");
 var Model = require("./model.js");
 var Visualization = require("./visualization.js");
 var Datacenter = require("./datacenter.js");
+var AggV1 = require("./aggv1.js");
 
 function initModel(el, model) {
   var viewHeight = Visualization.viewHeight;
   var viewWidth = Visualization.viewWidth;
-  model.nodeRadius = 20
-  model.nodeDistance = 80
+  model.nodeRadius = 10
+  model.nodeDistance = 30
   model.desiredReplicas = 5
+  model.projection = d3.geo.mercator()
+  model.skin = new AggV1()
+  model.enablePlayAndReload = false
+  model.enableAddNodeAndApp = false
+  model.displaySimState = false
 
-  new Datacenter(viewWidth * 0.2, viewHeight * 0.50, model)
-  new Datacenter(viewWidth * 0.33, viewHeight * 0.933, model)
-  new Datacenter(viewWidth * 0.5, viewHeight * 0.333, model)
-  new Datacenter(viewWidth * 0.66, viewHeight * 0.933, model)
-  new Datacenter(viewWidth * 0.8, viewHeight * 0.50, model)
+  new Datacenter(-74.0059, 40.7128, model).label = "New York City";
+  new Datacenter(-93.6091, 41.6005, model).label = "Des Moines";
+  new Datacenter(-96.7970, 32.7767, model).label = "Dallas";
+  new Datacenter(-122.0839, 37.3861, model).label = "Mountain";
+  new Datacenter(-122.3321, 47.6062, model).label = "Seattle";
   for (var i = 0; i < model.datacenters.length; i++) {
     for (var j = 0; j < 5; j++) {
       Model.addNodeToModel(model, model.datacenters[i])
