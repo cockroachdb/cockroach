a86c52ae644308654c5d15ad58d860d63a2769f6
Andrei Matei <andrei@cockroachlabs.com>
2017-02-06 15:36:33 -0500

    util: refact WaitGroup.Wait() to return the error

--- pkg/ccl/sqlccl/backup.go
+++ pkg/ccl/sqlccl/backup.go
@@ -478,8 +478,7 @@ func presplitRanges(baseCtx context.Context, db client.DB, input []roachpb.Key) 
 
 	wg.Add(1)
 	splitFn(input)
-	wg.Wait()
-	return wg.FirstError()
+	return wg.Wait()
 }
 
 func restoreTableDesc(
