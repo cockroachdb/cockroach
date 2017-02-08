fd6c511d74a35f4fe5a644c9e72f6ec0add62222
Alex Robinson <alexdwanerobinson@gmail.com>
2017-02-08 14:27:59 -0500

    storage: Remove logging of prop-eval-kv setting during package init
    Also clean up hack in CLI interactive tests that was working around the
    existence of logging during init.
    
    The setting will still be logged during startup due to our logging of
    all environment variables affecting configuration, e.g.:
    
    I170208 14:24:13.200762 9 cli/start.go:361  using local environment variables: COCKROACH_PROPOSER_EVALUATED_KV

--- pkg/storage/replica.go
+++ pkg/storage/replica.go
@@ -92,13 +92,7 @@ var txnAutoGC = true
 var tickQuiesced = envutil.EnvOrDefaultBool("COCKROACH_TICK_QUIESCED", true)
 
 // Whether to enable experimental support for proposer-evaluated KV.
-var propEvalKV = func() bool {
-	enabled := envutil.EnvOrDefaultBool("COCKROACH_PROPOSER_EVALUATED_KV", false)
-	if enabled {
-		log.Warningf(context.Background(), "running with experimental support for proposer-evaluated KV, see #10431")
-	}
-	return enabled
-}()
+var propEvalKV = envutil.EnvOrDefaultBool("COCKROACH_PROPOSER_EVALUATED_KV", false)
 
 // ProposerEvaluatedKVEnabled returns whether experimental support for
 // proposer-evaluated KV is enabled.
