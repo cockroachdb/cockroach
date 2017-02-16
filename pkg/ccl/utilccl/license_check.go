81a1d717b22f66a40ca0e4f4c483359da8d2a4e1
David Taylor <tinystatemachine@gmail.com>
2017-02-16 14:54:17 +0000

    utilccl: add stubbed helper for gating enterprise features
    The implementation of `EnterpriseEnabled` is expected to change to some sort of entered key at some point.

---
+++ pkg/ccl/utilccl/license_check.go
@@ -0,0 +1,37 @@
+// Copyright 2017 The Cockroach Authors.
+//
+// Licensed under the Cockroach Community Licence (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/LICENSE
+
+package utilccl
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/util/envutil"
+	"github.com/pkg/errors"
+)
+
+// EnterpriseEnabled indicates if the cluster has enterprise features enabled.
+// TODO(dt): this is a stub for now, to be replaced with some sort of runtime
+// configurable license key or other control mechanism.
+var EnterpriseEnabled = envutil.EnvOrDefaultBool("COCKROACH_ENTERPRISE_ENABLED", false)
+
+// ErrIfNotEnterpriseEnabled returns a non-nil error if the requested enterprise
+// feature is not enabled, including information or a link explaining how to
+// enable it.
+func ErrIfNotEnterpriseEnabled(feature string) error {
+	if EnterpriseEnabled {
+		return nil
+	}
+	// TODO(dt): link to some stable URL that then redirects to a helpful page
+	// that explains what to do here.
+	link := "https://cockroachlabs.com/pricing"
+	return errors.Errorf(
+		"Use of %s requires an enterprise license. "+
+			"See %s details on how to enable enterprise features.",
+		feature,
+		link,
+	)
+}
