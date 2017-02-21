ee6d2abacc4df4aaca2897aa5b5cfce9e4015db0
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-02-21 15:58:40 -0500

    sql/sqlbase: prevent users from dropping any system tables
    Cockroach prevents users from dropping system config tables, but allows
    users to drop non-config system tables. This is problematic; the system
    expects all system tables to exist, including non-config tables, and has
    no facility to recreate them if the user drops them.
    
    This commit ensures that, in new clusters, users cannot drop any system
    tables. This commit does not attempt to revoke the CREATE and DROP
    permissions on non-config system tables in existing clusters. Fixes
     #13691.

--- pkg/sql/privilege/privilege.go
+++ pkg/sql/privilege/privilege.go
@@ -18,6 +18,7 @@ package privilege
 
 import (
 	"bytes"
+	"fmt"
 	"sort"
 	"strings"
 )
@@ -137,4 +138,33 @@ func ListFromBitField(m uint32) List {
 		}
 	}
 	return ret
+}
+
+// Lists is a list of privilege lists
+type Lists []List
+
+// Contains returns whether the list of privilege lists contains exactly the
+// privilege list represented by bitfield m. This intentionally treats ALL and
+// {CREATE, ..., UPDATE} as distinct privilege lists.
+func (pls Lists) Contains(m uint32) bool {
+	for _, pl := range pls {
+		if pl.ToBitField() == m {
+			return true
+		}
+	}
+	return false
+}
+
+// String stringifies each constituent list of privileges, groups each inside of
+// {} braces, and joins them with " or ".
+//
+// Examples:
+//   {{SELECT}}.String() -> "{SELECT}"
+//   {{SELECT}, {SELECT, GRANT}}.String() -> "{SELECT} or {SELECT, GRANT}"
+func (pls Lists) String() string {
+	ret := make([]string, len(pls))
+	for i, pl := range pls {
+		ret[i] = fmt.Sprintf("{%s}", pl)
+	}
+	return strings.Join(ret, " or ")
 }
