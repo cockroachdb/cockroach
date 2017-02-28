aa87e0ca243cd9d1c9609fbb68543cd15116fec7
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-02-28 19:18:19 +0000

    sql: fix EXPLAIN(TYPES) for array subscript expressions
    There was a bug lurking in the representation of array indexing
    (subscripting) operations: the subscript expression was hidden as a
    field in an ArraySubscript node itself hidden in an `UnresolvedName`
    array (the `Indirection` field of IndirectionExpr), and
    `UnresolvedName` (an array of `NamePart`) is not pretty-printable for
    EXPLAIN(TYPES) -- causing a panic both in `EXPLAIN` statements and the
    plan logging code when running the executor with verbosity level 3 or
    higher.
    
    The reason for the panic is sound: we want to disallow reaching the
    EXPLAIN(TYPES) codepath unless all expressions have been typed
    properly, which (in principle) cannot happen unless all names have
    been resolved. The problem here is that `IndirectionExpr` was
    incorrectly using the `UnresolvedName` type for something that has
    absolutely nothing to do with names nor resolution, so of course the
    `UnresolvedName` array was ignored during name resolution and would
    remain intact there just waiting to make EXPLAIN(TYPES) very unhappy.
    
    This patch addresses the issue by changing `IndirectionExprs`'s
    `Indirection` field to use a dedicated new type `ArraySubscripts`
    (typedef of `[]*ArraySubscript`) instead.
    
    This also incidentally simplifies the code around subscripting a bit.

---
+++ pkg/cli/interactive_tests/test_high_verbosity.tcl
@@ -0,0 +1,27 @@
+#! /usr/bin/env expect -f
+
+source [file join [file dirname $argv0] common.tcl]
+
+system "$argv start --verbosity 3 & echo \$! > server_pid"
+sleep 1
+
+spawn /bin/bash
+send "PS1=':''/# '\r"
+eexpect ":/# "
+
+# Check that queries using tables can complete without error.
+send "echo 'create database d; create table d.t(x int); insert into d.t values(1); select x from d.t;' | $argv sql\r"
+eexpect "1 row"
+eexpect ":/# "
+send "echo 'select x\[1\] from (select array\[1,2,3\]) as t(x);' | $argv sql\r"
+eexpect "1 row"
+eexpect ":/# "
+
+# Check that the node is alive.
+send "$argv node status\r"
+eexpect ":/# "
+send "echo \$?\r"
+eexpect "\r\n0\r\n"
+eexpect ":/# "
+
+stop_server $argv
