4f9ac5946f9bb657cfacf1fbee38a9a4a9ec883b
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-03-06 23:47:07 -0500

    sql: document return value of ExecuteStatementInTransaction

--- pkg/sql/internal.go
+++ pkg/sql/internal.go
@@ -39,7 +39,8 @@ type InternalExecutor struct {
 var _ sqlutil.InternalExecutor = InternalExecutor{}
 
 // ExecuteStatementInTransaction executes the supplied SQL statement as part of
-// the supplied transaction. Statements are currently executed as the root user.
+// the supplied transaction and returns the number of rows affected. Statements
+// are currently executed as the root user.
 func (ie InternalExecutor) ExecuteStatementInTransaction(
 	ctx context.Context, opName string, txn *client.Txn, statement string, qargs ...interface{},
 ) (int, error) {
