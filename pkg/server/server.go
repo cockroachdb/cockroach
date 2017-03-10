9b3cc627dc7b486cf8697b8524cac6a432998a0d
marc <marc@cockroachlabs.com>
2017-03-10 07:34:15 -0500

    core: Set tcp keep-alive for server-side pgwire connections.
    Sets tcp keep-alive interval on pgwire connections if
    COCKROACH_SQL_TCP_KEEP_ALIVE > 0.
    That's all we get to set through Go. under linux, that impacts both
    the time to first keep-alive and the repeat interval. We don't get to
    set the count of keep-alive packets until the connection is considered
    dead.
    
    This would get around #13823, without requiring client library support
    for keep alive.
    We need to figure out if there are downsides to something like this
    though.
    
    Testing would kind of suck too. I don't think we can extract this kind
    of information from a client connection in progress.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -533,6 +533,33 @@ func (s *Server) Start(ctx context.Context) error {
 	// traffic until the migrations are done, as indicated by this channel.
 	serveSQL := make(chan bool)
 
+	tcpKeepAlive := envutil.EnvOrDefaultDuration("COCKROACH_SQL_TCP_KEEP_ALIVE", 0)
+
+	// Attempt to set TCP keep-alive on connection. Don't fail on errors.
+	setTCPKeepAlive := func(connCtx context.Context, conn net.Conn) {
+		if tcpKeepAlive == 0 {
+			return
+		}
+
+		var tcpConn *net.TCPConn
+		if muxConn, ok := conn.(*cmux.MuxConn); !ok {
+			return
+		} else if tcpConn, ok = muxConn.Conn.(*net.TCPConn); !ok {
+			return
+		}
+
+		if err := tcpConn.SetKeepAlive(true); err != nil {
+			log.VEventf(connCtx, 1, "TCPConn.SetKeepAlive failed: %v", err)
+			return
+		}
+		if err := tcpConn.SetKeepAlivePeriod(tcpKeepAlive); err != nil {
+			log.VEventf(connCtx, 1, "TCPConn.SetKeepAlivePreiod failed: %v", err)
+			return
+		}
+
+		log.VEventf(connCtx, 2, "tcp keep-alive: %s", tcpKeepAlive)
+	}
+
 	s.stopper.RunWorker(func() {
 		select {
 		case <-serveSQL:
@@ -542,6 +569,8 @@ func (s *Server) Start(ctx context.Context) error {
 		pgCtx := s.pgServer.AmbientCtx.AnnotateCtx(context.Background())
 		netutil.FatalIfUnexpected(httpServer.ServeWith(s.stopper, pgL, func(conn net.Conn) {
 			connCtx := log.WithLogTagStr(pgCtx, "client", conn.RemoteAddr().String())
+			setTCPKeepAlive(connCtx, conn)
+
 			if err := s.pgServer.ServeConn(connCtx, conn); err != nil && !netutil.IsClosedConnection(err) {
 				// Report the error on this connection's context, so that we
 				// know which remote client caused the error when looking at
