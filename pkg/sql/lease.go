b14b198621cb08b0eed1a057ea526991f90ca481
Vivek Menezes <vivek.menezes@gmail.com>
2017-03-13 22:08:53 -0400

    sql: check that Publish() increments the table version once
    We used to allow updating the table version in the function passed
    to Publish(). This was dangerous because it allowed the system to
    violate the--only two versions--invariant. Luckily it was only
    being violated in a test.
    
    In TestSchemaChangeRetryOnVersionChange we were incrementing the
    version by more than 1 to (v + 2) and as a result waiting on (v + 1)
    instead of (v). As a result the schema changer would sometimes race
    and pick up a lease on (v) resulting in more than one retry.
    
    fixes  #13878

--- pkg/sql/lease.go
+++ pkg/sql/lease.go
@@ -356,18 +356,22 @@ func (s LeaseStore) Publish(
 			}
 
 			// Run the update closure.
+			version := tableDesc.Version
 			if err := update(tableDesc); err != nil {
 				return err
 			}
+			if version != tableDesc.Version {
+				return errors.Errorf("updated version to: %d, expected: %d",
+					tableDesc.Version, version)
+			}
 
-			// Bump the version and modification time.
+			// Bump the version and modification time. Bump the version
+			// after calling update().
 			tableDesc.Version++
 			now := s.clock.Now()
 			tableDesc.ModificationTime = now
-			if log.V(3) {
-				log.Infof(txn.Context, "publish: descID=%d (%s) version=%d mtime=%s",
-					tableDesc.ID, tableDesc.Name, tableDesc.Version, now.GoTime())
-			}
+			log.Infof(txn.Context, "publish: descID=%d (%s) version=%d mtime=%s",
+				tableDesc.ID, tableDesc.Name, tableDesc.Version, now.GoTime())
 			if err := tableDesc.ValidateTable(); err != nil {
 				return err
 			}
