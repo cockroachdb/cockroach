8ff548c5b621618b7b037621d2d179283cd7f37e
Andrei Matei <andrei@cockroachlabs.com>
2017-03-16 18:54:40 -0400

    distsqlrun: improve outbox error handling
    Remove the possibility for a consumer to signal an error to a producer.
    It wasn't used.
    
    Also improve communication error handling in the outbox and add a test.

--- pkg/sql/distsqlrun/outbox_test.go
+++ pkg/sql/distsqlrun/outbox_test.go
@@ -32,6 +32,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/rpc"
 	"github.com/cockroachdb/cockroach/pkg/sql/parser"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/util"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
@@ -241,6 +242,122 @@ func TestOutboxInitializesStreamBeforeRecevingAnyRows(t *testing.T) {
 	outboxWG.Wait()
 }
 
+// Test that the outbox responds to the consumer shutting down in an unexpected
+// way by closing.
+func TestOutboxClosesWhenConsumerCloses(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	testCases := []struct {
+		// When set, the outbox will establish the stream with a FlowRpc call. When
+		// not set, the consumer will establish the stream with RunSyncFlow.
+		outboxIsClient bool
+		// Only takes effect with outboxIsClient is set. When set, the consumer
+		// (i.e. the server) returns an error from RunSyncFlow. This error will be
+		// translated into a grpc error received by the client (i.e. the outbox) in
+		// its stream.Recv()) call. Otherwise, the client doesn't return an error
+		// (and the outbox should receive io.EOF).
+		serverReturnsError bool
+	}{
+		{outboxIsClient: true, serverReturnsError: false},
+		{outboxIsClient: true, serverReturnsError: true},
+		{outboxIsClient: false},
+	}
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			stopper := stop.NewStopper()
+			defer stopper.Stop()
+			mockServer, addr, err := startMockDistSQLServer(stopper)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			flowCtx := FlowCtx{
+				evalCtx: parser.EvalContext{},
+				rpcCtx:  newInsecureRPCContext(stopper),
+			}
+			flowID := FlowID{uuid.MakeV4()}
+			streamID := StreamID(42)
+			var outbox *outbox
+			var wg sync.WaitGroup
+			wg.Add(1)
+			cleanup := func(error) { /*noop*/ }
+			var expectedErr error
+			if tc.outboxIsClient {
+				outbox = newOutbox(&flowCtx, addr.String(), flowID, streamID)
+				outbox.start(context.TODO(), &wg)
+				// Wait for the outbox to connect the stream.
+				streamNotification := <-mockServer.inboundStreams
+
+				// Have the server return from the FlowStream call. This should prompt the
+				// outbox to finish.
+				if tc.serverReturnsError {
+					expectedErr = errors.Errorf("FlowStream server error")
+				}
+				streamNotification.donec <- expectedErr
+			} else {
+				// We're going to perform a RunSyncFlow call and then have the client
+				// cancel the call's context.
+				conn, err := flowCtx.rpcCtx.GRPCDial(addr.String())
+				if err != nil {
+					t.Fatal(err)
+				}
+				client := NewDistSQLClient(conn)
+				var outStream DistSQL_RunSyncFlowClient
+				ctx, cancel := context.WithCancel(context.Background())
+				expectedErr = errors.Errorf("context canceled")
+				go func() {
+					outStream, err = client.RunSyncFlow(ctx)
+					if err != nil {
+						t.Error(err)
+					}
+					// Check that Recv() receives an error once the context is cancelled.
+					// Perhaps this is not terribly important to test; one can argue that
+					// the client should either not be Recv()ing after it cancelled the
+					// ctx or that it otherwise should otherwise be aware of the
+					// cancellation when processing the results, but I've put it here
+					// because bidi streams are confusing and this provides some
+					// information.
+					for {
+						_, err := outStream.Recv()
+						if err == nil {
+							continue
+						}
+						if !testutils.IsError(err, expectedErr.Error()) {
+							t.Errorf("expected err: %q, got %v", expectedErr, err)
+						}
+						break
+					}
+				}()
+				call := <-mockServer.runSyncFlowCalls
+				outbox = newOutboxSyncFlowStream(call.stream)
+				// In a RunSyncFlow call, the outbox runs under the call's context.
+				outbox.start(call.stream.Context(), &wg)
+				// cancel the RPC's context. This is how a streaming RPC client can inform
+				// the server that it's done. We expect the outbox to finish.
+				cancel()
+				cleanup = func(err error) {
+					// Allow the RunSyncFlow RPC to finish.
+					call.donec <- err
+				}
+			}
+
+			wg.Wait()
+			if expectedErr == nil {
+				if outbox.err != nil {
+					t.Fatalf("unexpected outbox.err: %s", outbox.err)
+				}
+			} else {
+				// We use error string comparison because we actually expect a grpc
+				// error wrapping the expected error.
+				if !testutils.IsError(outbox.err, expectedErr.Error()) {
+					t.Fatalf("expected err: %q, got %v", expectedErr, outbox.err)
+				}
+			}
+			cleanup(outbox.err)
+		})
+	}
+}
+
 // startMockDistSQLServer starts a MockDistSQLServer and returns the address on
 // which it's listening.
 func startMockDistSQLServer(stopper *stop.Stopper) (*MockDistSQLServer, net.Addr, error) {
@@ -267,7 +384,8 @@ func newInsecureRPCContext(stopper *stop.Stopper) *rpc.Context {
 // MockDistSQLServer implements the DistSQLServer (gRPC) interface and allows
 // clients to control the inbound streams.
 type MockDistSQLServer struct {
-	inboundStreams chan InboundStreamNotification
+	inboundStreams   chan InboundStreamNotification
+	runSyncFlowCalls chan RunSyncFlowCall
 }
 
 // InboundStreamNotification is the MockDistSQLServer's way to tell its clients
@@ -278,16 +396,26 @@ type InboundStreamNotification struct {
 	donec  chan<- error
 }
 
+type RunSyncFlowCall struct {
+	stream DistSQL_RunSyncFlowServer
+	donec  chan<- error
+}
+
 // MockDistSQLServer implements the DistSQLServer interface.
 var _ DistSQLServer = &MockDistSQLServer{}
 
 func newMockDistSQLServer() *MockDistSQLServer {
-	return &MockDistSQLServer{inboundStreams: make(chan InboundStreamNotification)}
+	return &MockDistSQLServer{
+		inboundStreams:   make(chan InboundStreamNotification),
+		runSyncFlowCalls: make(chan RunSyncFlowCall),
+	}
 }
 
 // RunSyncFlow is part of the DistSQLServer interface.
 func (ds *MockDistSQLServer) RunSyncFlow(stream DistSQL_RunSyncFlowServer) error {
-	panic("RunSyncFlow not implemented")
+	donec := make(chan error)
+	ds.runSyncFlowCalls <- RunSyncFlowCall{stream: stream, donec: donec}
+	return <-donec
 }
 
 // SetupFlow is part of the DistSQLServer interface.
