809e483d178744f511a0aaae2bd06b4ec71302cd
Daniel Harrison <daniel.harrison@gmail.com>
2017-03-16 13:15:29 -0400

    storageccl: use MultiIterator in Import
    Previously, Import would construct WriteBatches by iterating through
    each file in the import and adding keys to the batch. Then it would
    periodically send a WriteBatch request when the batch size reached a
    threshold. WriteBatch clears any existing data in the affected keyrange
    when applied, so later files could overwrite data in earlier ones. This
    creates incorrect data at the end of the import.
    
    MultiIterator multiplexes iteration over the sstables in parallel and
    returns the latest version of each key (skipping that key if the latest
    version is a tombstone). This means each keyrange is only affected by
    WriteBatch once.
    
    Closes #14152.

--- pkg/ccl/storageccl/import_test.go
+++ pkg/ccl/storageccl/import_test.go
@@ -9,7 +9,10 @@
 package storageccl
 
 import (
+	"fmt"
 	"path/filepath"
+	"reflect"
+	"strconv"
 	"testing"
 	"time"
 
@@ -26,22 +29,23 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/encoding"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/pkg/errors"
 )
 
-func writeOneKeySST(path string, key []byte) error {
-	sst := engine.MakeRocksDBSstFileWriter()
-	if err := sst.Open(path); err != nil {
-		_ = sst.Close()
-		return err
+func clientKVsToEngineKVs(kvs []client.KeyValue) []engine.MVCCKeyValue {
+	var ret []engine.MVCCKeyValue
+	for _, kv := range kvs {
+		if kv.Value == nil {
+			continue
+		}
+		k := engine.MVCCKey{
+			Key:       kv.Key,
+			Timestamp: kv.Value.Timestamp,
+		}
+		ret = append(ret, engine.MVCCKeyValue{Key: k, Value: kv.Value.RawBytes})
 	}
-	ts := hlc.NewClock(hlc.UnixNano, time.Nanosecond).Now()
-	value := roachpb.MakeValueFromString("bar")
-	value.InitChecksum(key)
-	kv := engine.MVCCKeyValue{Key: engine.MVCCKey{Key: key, Timestamp: ts}, Value: value.RawBytes}
-	if err := sst.Add(kv); err != nil {
-		return err
-	}
-	return sst.Close()
+	return ret
 }
 
 func TestImport(t *testing.T) {
@@ -52,20 +56,96 @@ func TestImport(t *testing.T) {
 
 	dir, dirCleanupFn := testutils.TempDir(t, 0)
 	defer dirCleanupFn()
-	ctx := context.Background()
 
-	kr := KeyRewriter([]roachpb.KeyRewrite{
-		{OldPrefix: keys.MakeTablePrefix(0), NewPrefix: keys.MakeTablePrefix(100)},
-	})
+	writeSST := func(keys ...[]byte) string {
+		path := strconv.FormatInt(hlc.UnixNano(), 10)
 
-	const sstName = "data.sst"
-	key := encoding.EncodeBytesAscending(kr[0].OldPrefix, []byte("foo"))
-	if err := writeOneKeySST(filepath.Join(dir, sstName), key); err != nil {
-		t.Fatalf("%+v", err)
+		sst := engine.MakeRocksDBSstFileWriter()
+		if err := sst.Open(filepath.Join(dir, path)); err != nil {
+			_ = sst.Close()
+			t.Fatalf("%+v", err)
+		}
+		ts := hlc.NewClock(hlc.UnixNano, time.Nanosecond).Now()
+		value := roachpb.MakeValueFromString("bar")
+		for _, key := range keys {
+			value.ClearChecksum()
+			value.InitChecksum(key)
+			kv := engine.MVCCKeyValue{Key: engine.MVCCKey{Key: key, Timestamp: ts}, Value: value.RawBytes}
+			if err := sst.Add(kv); err != nil {
+				t.Fatalf("%+v", err)
+			}
+		}
+		if err := sst.Close(); err != nil {
+			t.Fatalf("%+v", err)
+		}
+		return path
 	}
 
-	dataStartKey := roachpb.Key(key)
-	dataEndKey := dataStartKey.PrefixEnd()
+	slurpSSTablesLatestKey := func(paths []string, kr KeyRewriter) ([]engine.MVCCKeyValue, error) {
+		start, end := engine.MVCCKey{Key: keys.MinKey}, engine.MVCCKey{Key: keys.MaxKey}
+
+		e := engine.NewInMem(roachpb.Attributes{}, 1<<20)
+		defer e.Close()
+		batch := e.NewBatch()
+		defer batch.Close()
+
+		for _, path := range paths {
+			sst, err := engine.MakeRocksDBSstFileReader()
+			if err != nil {
+				return nil, err
+			}
+			defer sst.Close()
+			if err := sst.AddFile(filepath.Join(dir, path)); err != nil {
+				return nil, err
+			}
+			if err := sst.Iterate(start, end, func(kv engine.MVCCKeyValue) (bool, error) {
+				var ok bool
+				kv.Key.Key, ok = kr.RewriteKey(kv.Key.Key)
+				if !ok {
+					return true, errors.Errorf("could not rewrite key: %s", roachpb.Key(kv.Key.Key))
+				}
+				v := roachpb.Value{RawBytes: kv.Value}
+				v.ClearChecksum()
+				v.InitChecksum(kv.Key.Key)
+				if err := batch.Put(kv.Key, v.RawBytes); err != nil {
+					return true, err
+				}
+				return false, nil
+			}); err != nil {
+				return nil, err
+			}
+		}
+
+		var kvs []engine.MVCCKeyValue
+		it := batch.NewIterator(false)
+		defer it.Close()
+		for it.Seek(start); it.Valid() && it.UnsafeKey().Less(end); it.NextKey() {
+			kvs = append(kvs, engine.MVCCKeyValue{Key: it.Key(), Value: it.Value()})
+		}
+		if err := it.Error(); err != nil {
+			return nil, err
+		}
+		return kvs, nil
+	}
+
+	kr := KeyRewriter([]roachpb.KeyRewrite{
+		{OldPrefix: keys.MakeTablePrefix(51), NewPrefix: keys.MakeTablePrefix(100)},
+	})
+	var keys [][]byte
+	for i := 0; i < 4; i++ {
+		key := append([]byte(nil), kr[0].OldPrefix...)
+		key = encoding.EncodeStringAscending(key, fmt.Sprintf("foo%d", i))
+		keys = append(keys, key)
+	}
+
+	files := []string{
+		writeSST(keys[2], keys[3]),
+		writeSST(keys[0], keys[1]),
+		writeSST(keys[2], keys[3]),
+	}
+
+	dataStartKey := roachpb.Key(keys[0])
+	dataEndKey := roachpb.Key(keys[3]).PrefixEnd()
 	reqStartKey, ok := kr.RewriteKey(append([]byte(nil), dataStartKey...))
 	if !ok {
 		t.Fatalf("failed to rewrite key: %s", reqStartKey)
@@ -74,36 +154,53 @@ func TestImport(t *testing.T) {
 	if !ok {
 		t.Fatalf("failed to rewrite key: %s", reqEndKey)
 	}
+
+	ctx := context.Background()
+	s, _, kvDB := serverutils.StartServer(t, base.TestServerArgs{})
+	defer s.Stopper().Stop()
+
 	storage, err := ExportStorageConfFromURI(dir)
 	if err != nil {
 		t.Fatalf("%+v", err)
 	}
 
-	s, _, kvDB := serverutils.StartServer(t, base.TestServerArgs{})
-	defer s.Stopper().Stop()
+	for i := 0; i <= len(files); i++ {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			req := &roachpb.ImportRequest{
+				Span:        roachpb.Span{Key: reqStartKey},
+				DataSpan:    roachpb.Span{Key: dataStartKey, EndKey: dataEndKey},
+				KeyRewrites: kr,
+			}
 
-	// Import may be retried by DistSender if it takes too long to return, so
-	// make sure it's idempotent.
-	for i := 0; i < 3; i++ {
-		req := &roachpb.ImportRequest{
-			Span:        roachpb.Span{Key: reqStartKey},
-			DataSpan:    roachpb.Span{Key: dataStartKey, EndKey: dataEndKey},
-			KeyRewrites: kr,
-			Files: []roachpb.ImportRequest_File{
-				{Dir: storage, Path: sstName},
-			},
-		}
-		b := &client.Batch{}
-		b.AddRawRequest(req)
-		if err := kvDB.Run(ctx, b); err != nil {
-			t.Fatalf("%+v", err)
-		}
-		kvs, err := kvDB.Scan(ctx, reqStartKey, reqEndKey, 0)
-		if err != nil {
-			t.Fatalf("%+v", err)
-		}
-		if len(kvs) != 1 {
-			t.Fatalf("expected 1 kv got %d", len(kvs))
-		}
+			for _, f := range files[:i] {
+				req.Files = append(req.Files, roachpb.ImportRequest_File{Dir: storage, Path: f})
+			}
+			expectedKVs, err := slurpSSTablesLatestKey(files[:i], kr)
+			if err != nil {
+				t.Fatalf("%+v", err)
+			}
+
+			// Import may be retried by DistSender if it takes too long to return, so
+			// make sure it's idempotent.
+			for j := 0; j < 3; j++ {
+				b := &client.Batch{}
+				b.AddRawRequest(req)
+				if err := kvDB.Run(ctx, b); err != nil {
+					t.Fatalf("%+v", err)
+				}
+				clientKVs, err := kvDB.Scan(ctx, reqStartKey, reqEndKey, 0)
+				if err != nil {
+					t.Fatalf("%+v", err)
+				}
+				kvs := clientKVsToEngineKVs(clientKVs)
+
+				if !reflect.DeepEqual(kvs, expectedKVs) {
+					for _, kv := range append(kvs, expectedKVs...) {
+						log.Info(ctx, kv)
+					}
+					t.Fatalf("got %+v expected %+v", kvs, expectedKVs)
+				}
+			}
+		})
 	}
 }
