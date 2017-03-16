c5df7681b6a28751bcf7bd3044333db0d46586e9
Jordan Lewis <jordanthelewis@gmail.com>
2017-03-16 14:57:53 -0400

    distsqlrun: extract abstract distsql backfiller
    The index backfiller distsql processor had a bunch of functionality
    in common with the upcoming column backfiller which has been extracted
    into a new `backfiller` interface and `abstractBackfiller` base
    implementation.

--- pkg/sql/distsqlrun/backfiller.go
+++ pkg/sql/distsqlrun/backfiller.go
@@ -13,10 +13,14 @@
 // permissions and limitations under the License.
 //
 // Author: Vivek Menezes (vivek@cockroachlabs.com)
+// Author: Jordan Lewis (jordan@cockroachlabs.com)
 
 package distsqlrun
 
 import (
+	"fmt"
+	"sync"
+
 	"golang.org/x/net/context"
 
 	"github.com/cockroachdb/cockroach/pkg/internal/client"
@@ -24,9 +28,119 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/cockroach/pkg/util/tracing"
 	"github.com/pkg/errors"
 )
+
+type chunkBackfiller interface {
+	// runChunk returns the next-key and an error. next-key is nil
+	// once the backfill is complete.
+	runChunk(
+		ctx context.Context,
+		mutations []sqlbase.DescriptorMutation,
+		span roachpb.Span,
+		chunkSize int64,
+	) (roachpb.Key, error)
+}
+
+// MutationFilter is the type of a simple predicate on a mutation.
+type MutationFilter func(sqlbase.DescriptorMutation) bool
+
+// backfiller is a processor that implements a distributed backfill of
+// an entity, like indexes or columns, during a schema change.
+type backfiller struct {
+	chunkBackfiller
+	// name is the name of the kind of entity this backfiller processes.
+	name string
+	// mutationFilter returns true if the mutation should be processed by the
+	// chunkBackfiller.
+	filter MutationFilter
+
+	spec    BackfillerSpec
+	output  RowReceiver
+	flowCtx *FlowCtx
+	fetcher sqlbase.RowFetcher
+}
+
+// Run is part of the processor interface.
+func (b *backfiller) Run(ctx context.Context, wg *sync.WaitGroup) {
+	if wg != nil {
+		defer wg.Done()
+	}
+
+	opName := fmt.Sprintf("%sBackfiller", b.name)
+	ctx = log.WithLogTagInt(ctx, opName, int(b.spec.Table.ID))
+	ctx, span := tracing.ChildSpan(ctx, opName)
+	defer tracing.FinishSpan(span)
+
+	log.VEventf(ctx, 1, "starting")
+	if log.V(1) {
+		defer log.Infof(ctx, "exiting")
+	}
+
+	if err := b.mainLoop(ctx); err != nil {
+		b.output.Push(nil /* row */, ProducerMetadata{Err: err})
+	}
+	b.output.ProducerDone()
+}
+
+// mainLoop invokes runChunk on chunks of rows.
+// It does not close the output.
+func (b *backfiller) mainLoop(ctx context.Context) error {
+	var mutations []sqlbase.DescriptorMutation
+	const noNewIndex = -1
+	addedIndexMutationIdx := noNewIndex
+	desc := b.spec.Table
+	if len(desc.Mutations) == 0 {
+		return errors.Errorf("no schema changes for table ID=%d", desc.ID)
+	}
+	mutationID := desc.Mutations[0].MutationID
+	for i, m := range desc.Mutations {
+		if m.MutationID != mutationID {
+			break
+		}
+		if b.filter(m) {
+			mutations = append(mutations, m)
+			if addedIndexMutationIdx == noNewIndex {
+				addedIndexMutationIdx = i
+			}
+		}
+	}
+
+	if addedIndexMutationIdx == noNewIndex ||
+		len(b.spec.Spans) == 0 {
+		return errors.Errorf("completed processing all spans for %s backfill (%d, %d)", b.name, desc.ID, mutationID)
+	}
+	work := b.spec.Spans[0].Span
+
+	// Backfill the mutations for all the rows.
+	chunkSize := b.spec.ChunkSize
+	start := timeutil.Now()
+	var resume roachpb.Span
+	sp := work
+	for row := int64(0); sp.Key != nil; row += chunkSize {
+		if log.V(2) {
+			log.Infof(ctx, "%s backfill (%d, %d) at row: %d, span: %s",
+				b.name, desc.ID, mutationID, row, sp)
+		}
+		var err error
+		sp.Key, err = b.runChunk(ctx, mutations, sp, chunkSize)
+		if err != nil {
+			return err
+		}
+		if timeutil.Since(start) > b.spec.Duration && sp.Key != nil {
+			resume = sp
+			break
+		}
+	}
+	return WriteResumeSpan(ctx,
+		b.flowCtx.clientDB,
+		b.spec.Table.ID,
+		work,
+		resume,
+		addedIndexMutationIdx)
+}
 
 // WriteResumeSpan writes a checkpoint for the backfill work on origSpan.
 // origSpan is the span of keys that were assigned to be backfilled,
