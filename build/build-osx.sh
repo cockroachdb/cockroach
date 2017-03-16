a2aa22102edecce980086c82d5f6a5906e40954c
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-03-16 01:54:33 -0400

    build: disable Go version check on macOS builds for TeamCity
    We run `xgo` directly on TeamCity agents (not in a Docker image), which
    don't have a Go toolchain. This fails the Makefile's Go version check,
    even though we're going to build with the xgo toolchain anyway. So this
    commit disable the version check in build/build-osx.sh to unbrick TC
    release builds.
    
    Fixes #14104.

--- build/build-osx.sh
+++ build/build-osx.sh
@@ -5,6 +5,10 @@ set -eu
 cd "$(dirname "${0}")"/..
 
 go get -u github.com/karalabe/xgo
-# OSX 10.9 is the most recent version available at time of writing.
-# If changing the OS/arch target, adjust the filename in push-aws.sh.
-make xgo-build GOFLAGS='--image cockroachdb/xgo:20170218 --targets=darwin-10.9/amd64' TYPE=release
+# OSX 10.9 is the most recent version available at time of writing. If changing
+# the OS/arch target, adjust the filename in push-aws.sh.
+#
+# We additionally set GOVERS= to disable the Go version check. This allows
+# building in environments without a Go toolchain, like TeamCity build agents.
+# xgo will use the Go toolchain in its Docker image regardless.
+make xgo-build GOVERS= GOFLAGS='--image cockroachdb/xgo:20170218 --targets=darwin-10.9/amd64' TYPE=release
