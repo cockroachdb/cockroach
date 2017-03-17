16ca6c9854bc78bcb31e043aac86308b71dbc4fe
Jordan Lewis <jordanthelewis@gmail.com>
2017-03-17 16:47:24 -0400

    main: fix comment typo

--- main.go
+++ main.go
@@ -17,8 +17,8 @@
 package main
 
 // The ccl hook import below means building this will produce CCL'ed binaries.
-// This file itself reamins Apache2 though to preserve the organization of ccl
-// code under the /pkg/ccl subtree, but is unused for pure FLOSS builds.
+// This file itself remains Apache2 to preserve the organization of ccl code
+// under the /pkg/ccl subtree, but is unused for pure FLOSS builds.
 import (
 	_ "github.com/cockroachdb/cockroach/pkg/ccl" // ccl init hooks
 	"github.com/cockroachdb/cockroach/pkg/cli"
