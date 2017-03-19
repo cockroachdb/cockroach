c1053c258acbe44ad87c2bcfc9cecba0fb2e79f7
Matt Tracy <matt@cockroachlabs.com>
2017-03-19 18:15:14 -0400

    ui: Liveness info on Nodes List page
    Two main additions:
    + The Admin UI now queries Liveness data from the backend, and can now use it to better
    inform status visuals across the cluster. This data is added to the Redux state;
    additionally, numerous selectors have been created in the new `redux/nodes.ts`
    which provide useful computed information to various components.
    + This information is now being used by the Nodes List to divide all nodes into
    "Live" and "Dead".
    
    The nodes page has been changed to display two tables - one for live nodes, and
    another for dead nodes. This fixes #5415, as dead nodes now have a different set
    of columns than live nodes and thus we are no longer displaying out-of-date
    values for these nodes. This also fixes #12422, as dead nodes are no longer
    considered in summation totals. Also makes progress towards #13895.
    
    While overhauling the nodes page, have also added a tooltip to the colored
    health indicators on the nodes list to help explain the "healthy", "suspect" and
    "dead" states. Fixes #7547.

---
+++ pkg/ui/src/util/types.ts
@@ -0,0 +1,30 @@
+/**
+ * nullOfReturnType allows us to extract the return type of a function; this is
+ * necessary because typescript does not currently have a syntax to directly
+ * extract the return type of a function.
+ *
+ * This is needed in cases where:
+ * 1. A function returns an object with a complex type which is not explicitly
+ * declared. This occurs, for example, when constructing lookup objects that
+ * contain many fields: explicitly declaring the return type results in
+ * significant boilerplate which is unnecessary, because typescript knows the
+ * type of the variable being returned.
+ * 2. We need to declare a variable to hold the output of this function, but
+ * cannot immediately assign the output of the function to the variable.
+ *
+ * The output of nullOfReturnType() cannot be directly fed into a typeof
+ * statement; instead, its result must be stored in a variable, and in turn
+ * that variable can be used with a typeof statement.
+ *
+ * An example of extracting type information using nullOfReturnType():
+ *
+ * function fnWithComplicatedResult() {
+ *  return {...};
+ * }
+ *
+ * const complicatedReturnType = nullOfReturnType(fnWithComplicatedReturn);
+ * let complicatedVariable: typeof complicatedReturnType;
+ */
+export function nullOfReturnType<R> (_: (...args: any[]) => R): R {
+    return null;
+}
