ac716ef3cd74a27978157f30a3841e2567b6459a
Daniel Upton <daniel@floppy.co>
2017-03-20 08:26:18 +0000

    sql: Unify variable getters and setters into a single map
    Managing getters and setters in separate files often leads to new
    variables being added that are either write-only or read-only.
    
    This change also lays the ground work for adding proper support for
    `SET ... TO DEFAULT` and `RESET ...` statements.

--- pkg/sql/set.go
+++ pkg/sql/set.go
@@ -42,7 +42,8 @@ func (p *planner) Set(ctx context.Context, n *parser.Set) (planNode, error) {
 
 	// By using VarName.String() here any variables that are keywords will
 	// be double quoted.
-	name := strings.ToUpper(n.Name.String())
+	origName := n.Name.String()
+	name := strings.ToLower(origName)
 	typedValues := make([]parser.TypedExpr, len(n.Values))
 	for i, expr := range n.Values {
 		typedValue, err := parser.TypeCheck(expr, nil, parser.TypeString)
@@ -51,148 +52,24 @@ func (p *planner) Set(ctx context.Context, n *parser.Set) (planNode, error) {
 		}
 		typedValues[i] = typedValue
 	}
-	switch name {
-	case `DATABASE`:
-		dbName, err := p.getStringVal(name, typedValues)
-		if err != nil {
+
+	if v, ok := varGen[name]; ok {
+		// SET ... TO ...
+		if v.Set == nil {
+			return nil, fmt.Errorf("variable \"%s\" cannot be changed", name)
+		} else if err := v.Set(ctx, p, typedValues); err != nil {
 			return nil, err
 		}
-		if len(dbName) != 0 {
-			// Verify database descriptor exists.
-			if _, err := MustGetDatabaseDesc(ctx, p.txn, p.getVirtualTabler(), dbName); err != nil {
-				return nil, err
-			}
-		}
-		p.session.Database = dbName
-		p.evalCtx.Database = dbName
-
-	case `SYNTAX`:
-		s, err := p.getStringVal(name, typedValues)
-		if err != nil {
-			return nil, err
-		}
-		switch parser.Name(s).Normalize() {
-		case parser.ReNormalizeName(parser.Modern.String()):
-			p.session.Syntax = parser.Modern
-		case parser.ReNormalizeName(parser.Traditional.String()):
-			p.session.Syntax = parser.Traditional
-		default:
-			return nil, fmt.Errorf("%s: \"%s\" is not in (%q, %q)", name, s, parser.Modern, parser.Traditional)
-		}
-
-	case `DEFAULT_TRANSACTION_ISOLATION`:
-		// It's unfortunate that clients want us to support both SET
-		// SESSION CHARACTERISTICS AS TRANSACTION ..., which takes the
-		// isolation level as keywords/identifiers (e.g. JDBC), and SET
-		// DEFAULT_TRANSACTION_ISOLATION TO '...', which takes an
-		// expression (e.g. psycopg2). But that's how it is.  Just ensure
-		// this code keeps in sync with SetDefaultIsolation() below.
-		s, err := p.getStringVal(name, typedValues)
-		if err != nil {
-			return nil, err
-		}
-		switch strings.ToUpper(s) {
-		case `READ UNCOMMITTED`, `READ COMMITTED`, `SNAPSHOT`:
-			p.session.DefaultIsolationLevel = enginepb.SNAPSHOT
-		case `REPEATABLE READ`, `SERIALIZABLE`:
-			p.session.DefaultIsolationLevel = enginepb.SERIALIZABLE
-		default:
-			return nil, fmt.Errorf("%s: unknown isolation level: %q", name, s)
-		}
-
-	case `DISTSQL`:
-		s, err := p.getStringVal(name, typedValues)
-		if err != nil {
-			return nil, err
-		}
-		switch parser.Name(s).Normalize() {
-		case parser.ReNormalizeName("off"):
-			p.session.DistSQLMode = distSQLOff
-		case parser.ReNormalizeName("on"):
-			p.session.DistSQLMode = distSQLOn
-		case parser.ReNormalizeName("always"):
-			p.session.DistSQLMode = distSQLAlways
-		default:
-			return nil, fmt.Errorf("%s: \"%s\" not supported", name, s)
-		}
-
-	// These settings are sent by various client drivers. We don't support
-	// changing them, so we either silently ignore them or throw an error given
-	// a setting that we do not respect.
-	case `EXTRA_FLOAT_DIGITS`:
-	// See https://www.postgresql.org/docs/9.6/static/runtime-config-client.html
-	case `APPLICATION_NAME`:
-		// Set by clients to improve query logging.
-		// See https://www.postgresql.org/docs/9.6/static/runtime-config-logging.html#GUC-APPLICATION-NAME
-		s, err := p.getStringVal(name, typedValues)
-		if err != nil {
-			return nil, err
-		}
-		p.session.resetApplicationName(s)
-
-	case `CLIENT_ENCODING`:
-		// See https://www.postgresql.org/docs/9.6/static/multibyte.html
-		s, err := p.getStringVal(name, typedValues)
-		if err != nil {
-			return nil, err
-		}
-		if strings.ToUpper(s) != "UTF8" {
-			return nil, fmt.Errorf("non-UTF8 encoding %s not supported", s)
-		}
-
-	case `SEARCH_PATH`:
-		// https://www.postgresql.org/docs/9.6/static/runtime-config-client.html
-		newSearchPath := make(parser.SearchPath, len(typedValues))
-		foundPgCatalog := false
-		for i, v := range typedValues {
-			val, err := v.Eval(&p.evalCtx)
-			if err != nil {
-				return nil, err
-			}
-			s, ok := parser.AsDString(val)
-			if !ok {
-				return nil, fmt.Errorf("%s: requires string values: %s is %s not string",
-					name, v, val.ResolvedType())
-			}
-			if s == pgCatalogName {
-				foundPgCatalog = true
-			}
-			newSearchPath[i] = parser.Name(s).Normalize()
-		}
-		if !foundPgCatalog {
-			// "The system catalog schema, pg_catalog, is always searched,
-			// whether it is mentioned in the path or not. If it is
-			// mentioned in the path then it will be searched in the
-			// specified order. If pg_catalog is not in the path then it
-			// will be searched before searching any of the path items."
-			newSearchPath = append([]string{"pg_catalog"}, newSearchPath...)
-		}
-		p.session.SearchPath = newSearchPath
-
-	case `STANDARD_CONFORMING_STRINGS`:
-		// If true, escape backslash literals in strings. We do this by default,
-		// and we do not support the opposite behavior.
-		// See https://www.postgresql.org/docs/9.1/static/runtime-config-compatible.html#GUC-STANDARD-CONFORMING-STRINGS
-		s, err := p.getStringVal(name, typedValues)
-		if err != nil {
-			return nil, err
-		}
-		if parser.Name(s).Normalize() != parser.ReNormalizeName("on") {
-			return nil, fmt.Errorf("%s: \"%s\" not supported", name, s)
-		}
-	case `CLIENT_MIN_MESSAGES`:
-	// Controls returned message verbosity. We don't support this.
-	// See https://www.postgresql.org/docs/9.6/static/runtime-config-compatible.html
-
-	default:
-		return nil, fmt.Errorf("unknown variable: %q", name)
+	} else {
+		return nil, fmt.Errorf("unknown variable: %q", origName)
 	}
+
 	return &emptyNode{}, nil
 }
 
 func (p *planner) getStringVal(name string, values []parser.TypedExpr) (string, error) {
 	if len(values) != 1 {
-		return "", fmt.Errorf("%s: requires a single string value", name)
+		return "", fmt.Errorf("set %s: requires a single string value", name)
 	}
 	val, err := values[0].Eval(&p.evalCtx)
 	if err != nil {
@@ -200,7 +77,7 @@ func (p *planner) getStringVal(name string, values []parser.TypedExpr) (string, 
 	}
 	s, ok := parser.AsDString(val)
 	if !ok {
-		return "", fmt.Errorf("%s: requires a single string value: %s is a %s",
+		return "", fmt.Errorf("set %s: requires a single string value: %s is a %s",
 			name, values[0], val.ResolvedType())
 	}
 	return string(s), nil
