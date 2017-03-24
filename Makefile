cockroachdb/cockroach

commit 75db0294be1150128c3564d30a2460b1b3478072

Makefile: fix Git hook directory detection for Git <2.5
Git 2.5 introduced a useful `git rev-parse --git-path hooks` command
that, since Git 2.10, takes into account the `core.hooksPath` varible.
This command unfortunately fails with garbage output but a successful
exit code on TeamCity, which runs an older version of Git. The garbage
output contains spaces, which causes Make to print a warning about
"mixed implicit and normal rules", since $(GITHOOKSDIR)/% is used as a
target and Make cannot handle targets with spaces.

So this patch reverts to manually computing the Git hooks directory with
`git rev-parse --git-dir`/hooks, which works on all known versions of
Git.

The more I think about this, the more I think this may have been the
correct behavior all along. The `core.hooksPath` variable applies
globally, so if a user has configured it, s/he probably doesn't want us
installing Cockroach-specific hooks into that directory anyway.

Fixes #14330.

Makefile
@@ -335,11 +335,7 @@ ifneq ($(GIT_DIR),)
 endif
 
 ifneq ($(GIT_DIR),)
-# If we're in a git worktree, the git hooks directory may not be in our root,
-# so we ask git for the location.
-#
-# Note that `git rev-parse --git-path hooks` requires git 2.5+.
-GITHOOKSDIR := $(shell git rev-parse --git-path hooks 2> /dev/null || echo "$(GIT_DIR)/hooks")
+GITHOOKSDIR := $(GIT_DIR)/hooks
 GITHOOKS := $(subst githooks/,$(GITHOOKSDIR)/,$(wildcard githooks/*))
 $(GITHOOKSDIR)/%: githooks/%
 	@echo installing $<

commit fe162e81bbae8ca5f6076237bac67ae7af8fee1f

Makefile: fix Git hook installation for Git worktress
75db029 broke Git hook installation in worktrees (#14354). This is my
fault for refactoring the logic when I introduced the GIT_DIR variable
in 6c091c00. This commit restores the logic from 6c091c00^, which is
tried and true on TeamCity and Git worktrees.

Fixes #14354.

Makefile
@@ -335,7 +335,11 @@ ifneq ($(GIT_DIR),)
 endif
 
 ifneq ($(GIT_DIR),)
-GITHOOKSDIR := $(GIT_DIR)/hooks
+# If we're in a git worktree, the git hooks directory may not be in our root,
+# so we ask git for the location.
+#
+# Note that `git rev-parse --git-path hooks` requires git 2.5+.
+GITHOOKSDIR := $(shell test -d .git && echo '.git/hooks' || git rev-parse --git-path hooks)
 GITHOOKS := $(subst githooks/,$(GITHOOKSDIR)/,$(wildcard githooks/*))
 $(GITHOOKSDIR)/%: githooks/%
 	@echo installing $<