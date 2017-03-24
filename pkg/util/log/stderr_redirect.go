c7e31e7c5b28fcbd992311ea24441e0775002e56
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-03-24 19:14:08 +0000

    cli: make it more likely that the user will know a panic has occurred.

--- pkg/util/log/stderr_redirect.go
+++ pkg/util/log/stderr_redirect.go
@@ -16,6 +16,7 @@
 package log
 
 import (
+	"fmt"
 	"os"
 	"syscall"
 )
@@ -27,6 +28,10 @@ var OrigStderrFd int
 
 // OrigStderr points to the original stderr stream.
 var OrigStderr *os.File
+
+// stderrRedirected attempts to track whether stderr was redirected.
+// This is used to de-duplicate the panic log.
+var stderrRedirected bool
 
 func init() {
 	var err error
@@ -46,10 +51,44 @@ func init() {
 // A client that wishes to use the original stderr must use
 // OrigStderrFd / OrigStderr defined above.
 func hijackStderr(fd int) error {
+	stderrRedirected = true
 	return syscall.Dup2(fd, syscall.Stderr)
 }
 
 // restoreStderr cancels the effect of hijackStderr()
 func restoreStderr() error {
+	stderrRedirected = false
 	return syscall.Dup2(OrigStderrFd, syscall.Stderr)
+}
+
+// RecoverAndReportPanic can be invoked on goroutines that run with
+// stderr redirected to logs to ensure the user gets informed on the
+// real stderr a panic has occurred.
+func RecoverAndReportPanic() {
+	if r := recover(); r != nil {
+		ReportPanic(r)
+		panic(r)
+	}
+}
+
+// ReportPanic reports a panic has occurred on the real stderr.
+func ReportPanic(r interface{}) {
+	// Ensure that the logs are flushed before letting a panic
+	// terminate the server.
+	Flush()
+
+	if stderrRedirected {
+		// The panic message will go to "stderr" which is actually the log
+		// file. Copy it to the real stderr to give the user a chance to
+		// see it.
+		fmt.Fprintf(OrigStderr, "%v\n", r)
+	} else {
+		// We're not redirecting stderr at this point, so the panic
+		// message should be printed below. However we're not very strict
+		// in this package about whether "stderrRedirected" is accurate,
+		// so hint the user that they may still need to look at the log
+		// file.
+		fmt.Fprintln(OrigStderr, "\nERROR: a panic has occurred!\n"+
+			"If no details are printed below, check the log file for details.")
+	}
 }
