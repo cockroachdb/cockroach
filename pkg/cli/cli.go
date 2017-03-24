cockroachdb/cockroach

commit 31232153983ba65a74b39e83a0bd0c53099ec873

util/log: redirect stderr to the INFO log
Redirect stderr to the current INFO log file as long as we're not
logging INFO to stderr. This allows us to capture the panic stack traces
generated by the Go runtime.

Fixes #13383

pkg/cli/cli.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"syscall"
 	"text/tabwriter"
 
 	"github.com/cockroachdb/cockroach/pkg/build"
@@ -39,13 +40,24 @@ func Main() {
 		os.Args = append(os.Args, "help")
 	}
 	if err := Run(os.Args[1:]); err != nil {
-		fmt.Fprintf(os.Stderr, "Failed running %q\n", os.Args[1])
+		fmt.Fprintf(stderr, "Failed running %q\n", os.Args[1])
 		os.Exit(ErrorCode)
 	}
 }
 
 // Proxy to allow overrides in tests.
-var osStderr = os.Stderr
+var stderr *os.File
+
+func init() {
+	// If we're not logging to stderr, the logging system will redirect stderr to
+	// a file. We want error messages generated by the sql shell to always go to
+	// the original stderr.
+	newFd, err := syscall.Dup(int(os.Stderr.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	stderr = os.NewFile(uintptr(newFd), "stderr")
+}
 
 var versionCmd = &cobra.Command{
 	Use:   "version",

pkg/util/log/clog.go
@@ -34,6 +34,7 @@ import (
 	"strconv"
 	"strings"
 	"sync/atomic"
+	"syscall"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/build"
@@ -595,6 +596,8 @@ type loggingT struct {
 	hasColorProfile bool          // True if the color profile has been determined
 	colorProfile    *colorProfile // Set via call to getTermColorProfile
 
+	noStderrRedirect bool
+
 	// Level flag. Handled atomically.
 	stderrThreshold Severity // The -alsologtostderr flag.
 
@@ -928,6 +931,21 @@ func (sb *syncBuffer) rotateFile(now time.Time) error {
 	sb.nbytes = 0
 	if err != nil {
 		return err
+	}
+
+	// Redirect stderr to the current INFO log file in order to capture panic
+	// stack traces that are written by the Go runtime to stderr. Note that if
+	// --logtostderr is true we'll never enter this code path and panic stack
+	// traces will go to the original stderr as you would expect.
+	if sb.sev == Severity_INFO &&
+		logging.stderrThreshold > Severity_INFO &&
+		!logging.noStderrRedirect {
+		// NB: any concurrent output to stderr may straddle the old and new
+		// files. This doesn't apply to log messages as we won't reach this code
+		// unless we're not logging to stderr.
+		if err := syscall.Dup2(int(sb.file.Fd()), syscall.Stderr); err != nil {
+			return err
+		}
 	}
 
 	sb.Writer = bufio.NewWriterSize(sb.file, bufferSize)

pkg/cli/cli_test.go
@@ -133,7 +133,7 @@ func newCLITest(params cliTestParams) cliTest {
 
 	// Ensure that CLI error messages are logged to stdout, where they
 	// can be captured.
-	osStderr = os.Stdout
+	stderr = os.Stdout
 
 	return c
 }
@@ -146,7 +146,7 @@ func (c cliTest) cleanup() {
 	}
 
 	// Restore stderr.
-	osStderr = os.Stderr
+	stderr = os.Stderr
 
 	if c.TestServer != nil {
 		select {

pkg/cli/flags.go
@@ -266,6 +266,8 @@ func init() {
 		// TODO(peter): Decide if we want to make the lightstep flags visible.
 		if strings.HasPrefix(flag.Name, "lightstep_") {
 			flag.Hidden = true
+		} else if flag.Name == "no-redirect-stderr" {
+			flag.Hidden = true
 		}
 		pf.AddFlag(flag)
 	})

pkg/cli/interactive_tests/test_sql_mem_monitor.tcl
@@ -44,7 +44,7 @@ send "ulimit -v [ expr {2*$vmem+400} ]\r"
 eexpect ":/# "
 
 # Start a server with this limit set. The server will now run in the foreground.
-send "$argv start\r"
+send "$argv start --no-redirect-stderr\r"
 eexpect "restarted pre-existing node"
 sleep 1
 
@@ -84,7 +84,7 @@ eexpect root@
 
 # Re-launch a server with relatively lower limit for SQL memory
 set spawn_id $shell_spawn_id
-send "$argv start --max-sql-memory=150K\r"
+send "$argv start --max-sql-memory=150K --no-redirect-stderr\r"
 eexpect "restarted pre-existing node"
 sleep 2
 

pkg/cli/sql.go
@@ -218,15 +218,15 @@ func (c *cliState) addHistory(line string) {
 func (c *cliState) invalidSyntax(
 	nextState cliStateEnum, format string, args ...interface{},
 ) cliStateEnum {
-	fmt.Fprint(osStderr, "invalid syntax: ")
-	fmt.Fprintf(osStderr, format, args...)
-	fmt.Fprintln(osStderr)
+	fmt.Fprint(stderr, "invalid syntax: ")
+	fmt.Fprintf(stderr, format, args...)
+	fmt.Fprintln(stderr)
 	c.exitErr = errInvalidSyntax
 	return nextState
 }
 
 func (c *cliState) invalidOptionChange(nextState cliStateEnum, opt string) cliStateEnum {
-	fmt.Fprintf(osStderr, "cannot change option during multi-line editing: %s\n", opt)
+	fmt.Fprintf(stderr, "cannot change option during multi-line editing: %s\n", opt)
 	return nextState
 }
 
@@ -293,7 +293,7 @@ func (c *cliState) handleSet(args []string, nextState, errState cliStateEnum) cl
 		return c.invalidOptionChange(errState, args[0])
 	}
 	if err := opt.set(c, args[1:]); err != nil {
-		fmt.Fprintf(osStderr, "\\set %s: %v\n", strings.Join(args, " "), err)
+		fmt.Fprintf(stderr, "\\set %s: %v\n", strings.Join(args, " "), err)
 		return errState
 	}
 	return nextState
@@ -312,7 +312,7 @@ func (c *cliState) handleUnset(args []string, nextState, errState cliStateEnum) 
 		return c.invalidOptionChange(errState, args[0])
 	}
 	if err := opt.unset(c); err != nil {
-		fmt.Fprintf(osStderr, "\\unset %s: %v\n", args[0], err)
+		fmt.Fprintf(stderr, "\\unset %s: %v\n", args[0], err)
 		return errState
 	}
 	return nextState
@@ -341,7 +341,7 @@ func execSyscmd(command string) (string, error) {
 
 	var out bytes.Buffer
 	cmd.Stdout = &out
-	cmd.Stderr = os.Stderr
+	cmd.Stderr = stderr
 
 	if err := cmd.Run(); err != nil {
 		return "", fmt.Errorf("error in external command: %s", err)
@@ -356,14 +356,14 @@ var errInvalidSyntax = errors.New("invalid syntax")
 func (c *cliState) runSyscmd(line string, nextState, errState cliStateEnum) cliStateEnum {
 	command := strings.Trim(line[2:], " \r\n\t\f")
 	if command == "" {
-		fmt.Fprintf(osStderr, "Usage:\n  \\! [command]\n")
+		fmt.Fprintf(stderr, "Usage:\n  \\! [command]\n")
 		c.exitErr = errInvalidSyntax
 		return errState
 	}
 
 	cmdOut, err := execSyscmd(command)
 	if err != nil {
-		fmt.Fprintf(osStderr, "command failed: %s\n", err)
+		fmt.Fprintf(stderr, "command failed: %s\n", err)
 		c.exitErr = err
 		return errState
 	}
@@ -376,14 +376,14 @@ func (c *cliState) runSyscmd(line string, nextState, errState cliStateEnum) cliS
 func (c *cliState) pipeSyscmd(line string, nextState, errState cliStateEnum) cliStateEnum {
 	command := strings.Trim(line[2:], " \n\r\t\f")
 	if command == "" {
-		fmt.Fprintf(osStderr, "Usage:\n  \\| [command]\n")
+		fmt.Fprintf(stderr, "Usage:\n  \\| [command]\n")
 		c.exitErr = errInvalidSyntax
 		return errState
 	}
 
 	cmdOut, err := execSyscmd(command)
 	if err != nil {
-		fmt.Fprintf(osStderr, "command failed: %s\n", err)
+		fmt.Fprintf(stderr, "command failed: %s\n", err)
 		c.exitErr = err
 		return errState
 	}
@@ -408,20 +408,20 @@ func (c *cliState) refreshTransactionStatus(nextState cliStateEnum) cliStateEnum
 	query := makeQuery(`SHOW TRANSACTION STATUS`)
 	rows, err := query(c.conn)
 	if err != nil {
-		fmt.Fprintf(osStderr, "error retrieving the transaction status: %v", err)
+		fmt.Fprintf(stderr, "error retrieving the transaction status: %v", err)
 		return c.refreshDatabaseName(" ?", nextState)
 	}
 	defer func() { _ = rows.Close() }()
 
 	if len(rows.Columns()) == 0 {
-		fmt.Fprintf(osStderr, "invalid transaction status\n")
+		fmt.Fprintf(stderr, "invalid transaction status\n")
 		return c.refreshDatabaseName(" ?", nextState)
 	}
 
 	val := make([]driver.Value, len(rows.Columns()))
 	err = rows.Next(val)
 	if err != nil {
-		fmt.Fprintf(osStderr, "invalid transaction status: %v\n", err)
+		fmt.Fprintf(stderr, "invalid transaction status: %v\n", err)
 		return c.refreshDatabaseName(" ?", nextState)
 	}
 
@@ -467,13 +467,13 @@ func (c *cliState) refreshDatabaseName(txnStatus string, nextState cliStateEnum)
 	query := makeQuery(`SHOW DATABASE`)
 	rows, err := query(c.conn)
 	if err != nil {
-		fmt.Fprintf(osStderr, "error retrieving the database name: %v", err)
+		fmt.Fprintf(stderr, "error retrieving the database name: %v", err)
 		return c.refreshPrompts(txnStatus, nextState)
 	}
 	defer func() { _ = rows.Close() }()
 
 	if len(rows.Columns()) == 0 {
-		fmt.Fprintf(osStderr, "cannot get the database name")
+		fmt.Fprintf(stderr, "cannot get the database name")
 		return c.refreshPrompts(txnStatus, nextState)
 	}
 
@@ -602,7 +602,7 @@ func (c *cliState) doQuerySyntax(nextState cliStateEnum) cliStateEnum {
 
 	newSyntax, err := getSyntax(c.conn)
 	if err != nil {
-		fmt.Fprintf(osStderr, "warning: could not get session syntax: %s\n", err)
+		fmt.Fprintf(stderr, "warning: could not get session syntax: %s\n", err)
 		// For now this is just a warning.
 	} else {
 		c.syntax = newSyntax
@@ -706,7 +706,7 @@ func (c *cliState) doReadLine(nextState cliStateEnum) cliStateEnum {
 
 	default:
 		// Other errors terminate the shell.
-		fmt.Fprintf(osStderr, "input error: %s\n", err)
+		fmt.Fprintf(stderr, "input error: %s\n", err)
 		c.exitErr = err
 		return cliStop
 	}
@@ -766,7 +766,7 @@ func (c *cliState) doHandleCliCmd(loopState, nextState cliStateEnum) cliStateEnu
 
 	case `\show`:
 		if len(c.partialLines) == 0 {
-			fmt.Fprintf(osStderr, "No input so far. Did you mean SHOW?\n")
+			fmt.Fprintf(stderr, "No input so far. Did you mean SHOW?\n")
 		} else {
 			for _, s := range c.partialLines {
 				fmt.Println(s)
@@ -779,7 +779,7 @@ func (c *cliState) doHandleCliCmd(loopState, nextState cliStateEnum) cliStateEnu
 	default:
 		if strings.HasPrefix(cmd[0], `\d`) {
 			// Unrecognized command for now, but we want to be helpful.
-			fmt.Fprint(osStderr, "Suggestion: use the SQL SHOW statement to inspect your schema.\n")
+			fmt.Fprint(stderr, "Suggestion: use the SQL SHOW statement to inspect your schema.\n")
 		}
 		return c.invalidSyntax(errState, `%s. Try \? for help.`, c.lastInputLine)
 	}
@@ -817,7 +817,7 @@ func (c *cliState) doPrepareStatementLine(
 	if c.atEOF {
 		// Definitely no more input expected.
 		if !endsWithSemi {
-			fmt.Fprintf(osStderr, "missing semicolon at end of statement: %s\n", c.concatLines)
+			fmt.Fprintf(stderr, "missing semicolon at end of statement: %s\n", c.concatLines)
 			c.exitErr = fmt.Errorf("last statement was not executed: %s", c.concatLines)
 			return cliStop
 		}
@@ -893,7 +893,7 @@ func (c *cliState) doCheckStatement(startState, contState, execState cliStateEnu
 	// the user to enter input over multiple lines.
 	if endsWithIncompleteTxn(parsedStmts) && c.lastInputLine != "" {
 		if c.partialStmtsLen == 0 {
-			fmt.Fprintln(osStderr, "Now adding input for a multi-line SQL transaction client-side.\n"+
+			fmt.Fprintln(stderr, "Now adding input for a multi-line SQL transaction client-side.\n"+
 				"Press Enter two times to send the SQL text collected so far to the server, or Ctrl+C to cancel.")
 		}
 
@@ -909,7 +909,7 @@ func (c *cliState) doRunStatement(nextState cliStateEnum) cliStateEnum {
 	c.exitErr = runQueryAndFormatResults(c.conn, os.Stdout, makeQuery(c.concatLines),
 		cliCtx.tableDisplayFormat)
 	if c.exitErr != nil {
-		fmt.Fprintln(osStderr, c.exitErr)
+		fmt.Fprintln(stderr, c.exitErr)
 		if c.errExit {
 			return cliStop
 		}

pkg/cli/sql_util.go
@@ -51,7 +51,7 @@ type sqlConn struct {
 func (c *sqlConn) ensureConn() error {
 	if c.conn == nil {
 		if c.reconnecting && isInteractive {
-			fmt.Fprintf(osStderr, "connection lost; opening new connection and resetting session parameters...\n")
+			fmt.Fprintf(stderr, "connection lost; opening new connection and resetting session parameters...\n")
 		}
 		conn, err := pq.Open(c.url)
 		if err != nil {

pkg/cli/start.go
@@ -479,6 +479,7 @@ func runStart(cmd *cobra.Command, args []string) error {
 	case sig := <-signalCh:
 		returnErr = fmt.Errorf("received signal '%s' during shutdown, initiating hard shutdown", sig)
 		log.Errorf(shutdownCtx, "%v", returnErr)
+		fmt.Fprintln(os.Stdout, returnErr)
 		// This new signal is not welcome, as it interferes with the
 		// graceful shutdown process.  On Unix, a signal that was not
 		// handled gracefully by the application should be visible to
@@ -490,6 +491,7 @@ func runStart(cmd *cobra.Command, args []string) error {
 	case <-time.After(time.Minute):
 		returnErr = errors.New("time limit reached, initiating hard shutdown")
 		log.Errorf(shutdownCtx, "%v", returnErr)
+		fmt.Fprintln(os.Stdout, returnErr)
 		// NB: we do not return here to go through log.Flush below.
 	case <-stopper.IsStopped():
 		const msgDone = "server drained and shutdown completed"
@@ -517,7 +519,7 @@ func rerunBackground() error {
 	}
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	cmd.Stderr = stderr
 	return sdnotify.Exec(cmd)
 }
 
@@ -648,7 +650,7 @@ func runQuit(_ *cobra.Command, _ []string) (err error) {
 	if err := doShutdown(ctx, c, onModes); err != nil {
 		if _, ok := err.(*errTryHardShutdown); ok {
 			fmt.Fprintf(
-				os.Stderr, "graceful shutdown failed: %s\nproceeding with hard shutdown\n", err,
+				os.Stdout, "graceful shutdown failed: %s\nproceeding with hard shutdown\n", err,
 			)
 		} else {
 			return err

commit 39965059b76885a9391d1ab29962c572305b5e51

log: restore the original stderr as failover error stream
The patch in 31232153983ba65a74b39e83a0bd0c53099ec873 has introduced a
mechanism whereby syscall.Stderr (file descriptor 2) becomes an alias
to the log file, when a log file is created. This ensures that e.g. Go
panic reports are sent to the log file for later inspection.

This patch complements the original change by ensuring that the really
important informational messages from the running process are still
emitted to the original stderr, despite this redirect. These
informational messages include:

- errors that occur while writing to a log file.
- the informational message that indicates which directory contains
  the log files, emitted by the TestLogScope object when used in tests.

pkg/cli/cli.go
@@ -20,10 +20,10 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
-	"syscall"
 	"text/tabwriter"
 
 	"github.com/cockroachdb/cockroach/pkg/build"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/mattn/go-isatty"
 	"github.com/spf13/cobra"
@@ -45,19 +45,10 @@ func Main() {
 	}
 }
 
-// Proxy to allow overrides in tests.
-var stderr *os.File
-
-func init() {
-	// If we're not logging to stderr, the logging system will redirect stderr to
-	// a file. We want error messages generated by the sql shell to always go to
-	// the original stderr.
-	newFd, err := syscall.Dup(int(os.Stderr.Fd()))
-	if err != nil {
-		panic(err)
-	}
-	stderr = os.NewFile(uintptr(newFd), "stderr")
-}
+// stderr aliases log.OrigStderr; we use an alias here so that tests
+// in this package can redirect the output of CLI commands to stdout
+// to be captured.
+var stderr = log.OrigStderr
 
 var versionCmd = &cobra.Command{
 	Use:   "version",

pkg/util/log/clog.go
@@ -34,7 +34,6 @@ import (
 	"strconv"
 	"strings"
 	"sync/atomic"
-	"syscall"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/build"
@@ -771,7 +770,7 @@ func (l *loggingT) outputLogEntry(s Severity, file string, line int, msg string)
 
 func (l *loggingT) outputToStderr(entry Entry, stacks []byte) {
 	buf := l.processForStderr(entry, stacks)
-	if _, err := os.Stderr.Write(buf.Bytes()); err != nil {
+	if _, err := OrigStderr.Write(buf.Bytes()); err != nil {
 		panic(err)
 	}
 	l.putBuffer(buf)
@@ -793,7 +792,7 @@ func (l *loggingT) getTermColorProfile() *colorProfile {
 	if !l.hasColorProfile {
 		l.hasColorProfile = true
 		if !l.nocolor {
-			fi, _ := os.Stderr.Stat() // get the FileInfo struct describing the standard input.
+			fi, _ := OrigStderr.Stat() // get the FileInfo struct describing the standard input.
 			if (fi.Mode() & os.ModeCharDevice) != 0 {
 				term := os.Getenv("TERM")
 				switch term {
@@ -821,7 +820,7 @@ func timeoutFlush(timeout time.Duration) {
 	select {
 	case <-done:
 	case <-time.After(timeout):
-		fmt.Fprintln(os.Stderr, "clog: Flush took longer than", timeout)
+		fmt.Fprintln(OrigStderr, "clog: Flush took longer than", timeout)
 	}
 }
 
@@ -854,7 +853,7 @@ var logExitFunc func(error)
 // It flushes the logs and exits the program; there's no point in hanging around.
 // l.mu is held.
 func (l *loggingT) exit(err error) {
-	fmt.Fprintf(os.Stderr, "log: exiting because of error: %s\n", err)
+	fmt.Fprintf(OrigStderr, "log: exiting because of error: %s\n", err)
 	// If logExitFunc is set, we do that instead of exiting.
 	if logExitFunc != nil {
 		logExitFunc(err)
@@ -923,7 +922,7 @@ func (sb *syncBuffer) rotateFile(now time.Time) error {
 		// NB: any concurrent output to stderr may straddle the old and new
 		// files. This doesn't apply to log messages as we won't reach this code
 		// unless we're not logging to stderr.
-		if err := syscall.Dup2(int(sb.file.Fd()), syscall.Stderr); err != nil {
+		if err := hijackStderr(int(sb.file.Fd())); err != nil {
 			return err
 		}
 	}
@@ -1039,7 +1038,7 @@ func (l *loggingT) gcOldFiles() {
 
 	allFiles, err := ListLogFiles()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "unable to GC log files: %s\n", err)
+		fmt.Fprintf(OrigStderr, "unable to GC log files: %s\n", err)
 		return
 	}
 
@@ -1058,7 +1057,7 @@ func (l *loggingT) gcOldFiles() {
 		}
 		path := filepath.Join(dir, f.Name)
 		if err := os.Remove(path); err != nil {
-			fmt.Fprintf(os.Stderr, "%s\n", err)
+			fmt.Fprintln(OrigStderr, err)
 		}
 	}
 }

pkg/util/log/stderr_redirect.go
@@ -1,0 +1,50 @@
+// Copyright 2017 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+
+package log
+
+import (
+	"os"
+	"syscall"
+)
+
+// OrigStderrFd is a file descriptor that is initialized to a copy of
+// the original stderr file descriptor 2, before file descriptor 2 is
+// replaced to point to a log file by hijackStderr().
+var OrigStderrFd int
+
+// OrigStderr points to the original stderr stream.
+var OrigStderr *os.File
+
+func init() {
+	var err error
+	OrigStderrFd, err = syscall.Dup(syscall.Stderr)
+	if err != nil {
+		panic(err)
+	}
+	OrigStderr = os.NewFile(uintptr(OrigStderrFd), "/dev/stderr")
+	if OrigStderr == nil {
+		panic(err)
+	}
+}
+
+// hijackStderr replaces syscall.Stderr (and thus the target of
+// os.Stderr and pretty much anything that targets stderr using
+// standard ways) by the given file descriptor.
+// A client that wishes to use the original stderr must use
+// OrigStderrFd / OrigStderr defined above.
+func hijackStderr(fd int) error {
+	return syscall.Dup2(fd, syscall.Stderr)
+}

pkg/cli/cli_test.go
@@ -131,8 +131,9 @@ func newCLITest(params cliTestParams) cliTest {
 		}
 	}
 
-	// Ensure that CLI error messages are logged to stdout, where they
-	// can be captured.
+	// Ensure that CLI error messages and anything meant for the
+	// original stderr is redirected to stdout, where it can be
+	// captured.
 	stderr = os.Stdout
 
 	return c
@@ -146,7 +147,7 @@ func (c cliTest) cleanup() {
 	}
 
 	// Restore stderr.
-	stderr = os.Stderr
+	stderr = log.OrigStderr
 
 	if c.TestServer != nil {
 		select {

pkg/util/log/test_log_scope.go
@@ -99,7 +99,7 @@ func (l TestLogScope) Close(t tShim) {
 		if (t.Failed() || calledDuringPanic()) && !emptyDir {
 			// If the test failed, we keep the log files for further investigation,
 			// but only if there were any.
-			fmt.Fprintf(os.Stderr, "test log files left over in: %s\n", l)
+			fmt.Fprintln(OrigStderr, "test logs left over in:", l)
 		} else {
 			// Clean up.
 			if err := os.RemoveAll(string(l)); err != nil {