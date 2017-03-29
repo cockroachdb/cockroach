7c066e0885c99750d6664c2ef9b9857e40200c5c
Tamir Duberstein <tamird@gmail.com>
2017-03-29 19:10:16 -0400

    *: remove `strictld`; this has served its purpose

--- pkg/storage/engine/rocksdb_unix.go
+++ pkg/storage/engine/rocksdb_unix.go
@@ -22,6 +22,6 @@ import (
 	_ "github.com/cockroachdb/c-rocksdb"
 )
 
-// #cgo !strictld,darwin LDFLAGS: -Wl,-undefined -Wl,dynamic_lookup
-// #cgo !strictld,!darwin LDFLAGS: -Wl,-unresolved-symbols=ignore-all
+// #cgo darwin LDFLAGS: -Wl,-undefined -Wl,dynamic_lookup
+// #cgo !darwin LDFLAGS: -Wl,-unresolved-symbols=ignore-all
 import "C"
