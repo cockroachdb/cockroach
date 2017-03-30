49db77415811920e204e544379b227528a2b2d07
David Taylor <tinystatemachine@gmail.com>
2017-03-30 12:43:29 +0000

    scripts: rm glide.sh

--- build/checkdeps.sh
+++ build/checkdeps.sh
@@ -7,7 +7,7 @@ echo "installing vendored glide"
 go install ./vendor/github.com/Masterminds/glide
 
 echo "checking that 'vendor' matches manifest"
-./scripts/glide.sh install
+glide install
 ! git -C vendor status --porcelain | read || (git -C vendor status; git -C vendor diff -a 1>&2; exit 1)
 
 echo "all 3rd-party dependencies appear to be properly vendored."
