a5f7c7e0e6f23f2732ab1ba57753fb72c43c295f
Tamir Duberstein <tamird@gmail.com>
2017-03-30 13:18:30 -0400

    build: checkdeps only if relevant changes were made
    Also clean up the test scripts while I'm here.

--- build/teamcity-test.sh
+++ build/teamcity-test.sh
@@ -8,15 +8,15 @@ export BUILDER_HIDE_GOPATH_SRC=1
 build/builder.sh go install ./pkg/cmd/github-pull-request-make
 
 build/builder.sh env \
-		 COCKROACH_PROPOSER_EVALUATED_KV="${COCKROACH_PROPOSER_EVALUATED_KV:-false}" \
-		 make test \
-		 TESTFLAGS='-v' \
-		 2>&1 \
-    | tee artifacts/test.log \
-    | go-test-teamcity
+	COCKROACH_PROPOSER_EVALUATED_KV="${COCKROACH_PROPOSER_EVALUATED_KV:-false}" \
+	BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" \
+	TARGET=stress \
+	github-pull-request-make
 
 build/builder.sh env \
-		 COCKROACH_PROPOSER_EVALUATED_KV="${COCKROACH_PROPOSER_EVALUATED_KV:-false}" \
-		 BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" \
-		 TARGET=stress \
-		 github-pull-request-make
+	COCKROACH_PROPOSER_EVALUATED_KV="${COCKROACH_PROPOSER_EVALUATED_KV:-false}" \
+	make test \
+	TESTFLAGS='-v' \
+	2>&1 \
+	| tee artifacts/test.log \
+	| go-test-teamcity
