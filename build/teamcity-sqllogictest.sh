68a9d0f5c42357329bd343f615e2e04f68b92206
Jordan Lewis <jordanthelewis@gmail.com>
2017-03-31 10:23:07 -0400

    build: don't hide GOPATH from sqllogictest
    It needs another directory from the host GOPATH that contains the tests.

--- build/teamcity-sqllogictest.sh
+++ build/teamcity-sqllogictest.sh
@@ -3,7 +3,11 @@ set -euxo pipefail
 
 mkdir -p artifacts
 
-export BUILDER_HIDE_GOPATH_SRC=1
+# make bigtest needs the sqllogictest repo from the host's GOPATH, so we can't
+# hide it like we do in the other teamcity build scripts.
+# TODO(jordan) improve builder.sh to allow partial GOPATH hiding rather than
+# the all-on/all-off strategy BULIDER_HIDE_GOPATH_SRC gives us.
+export BUILDER_HIDE_GOPATH_SRC=0
 build/builder.sh env \
 		 make -C pkg/sql bigtest \
 		 TESTFLAGS='-v' \
