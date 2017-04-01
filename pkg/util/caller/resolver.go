d9f7809a60815afed8fe574bc597159c943f0929
Tamir Duberstein <tamird@gmail.com>
2017-04-01 17:27:25 -0400

    util/caller: runtime.Caller always returns unix paths

--- pkg/util/caller/resolver.go
+++ pkg/util/caller/resolver.go
@@ -17,8 +17,7 @@
 package caller
 
 import (
-	"os"
-	"path/filepath"
+	"path"
 	"regexp"
 	"runtime"
 	"strings"
@@ -54,31 +53,31 @@ var defaultRE = func() *regexp.Regexp {
 	if !ok {
 		return reStripNothing
 	}
-	const sep = string(os.PathSeparator)
-	path := filepath.Dir(file)
+	const sep = "/"
+	root := path.Dir(file)
 	// Coverage tests report back as `[...]/util/caller/_test/_obj_test`;
 	// strip back to this package's directory.
-	for strings.Contains(path, sep) && !strings.HasSuffix(path, "caller") {
-		path = filepath.Dir(filepath.Clean(path))
+	for strings.Contains(root, sep) && !strings.HasSuffix(root, "caller") {
+		root = path.Dir(path.Clean(root))
 	}
 	// Strip to $GOPATH/src.
 	for i := 0; i < 6; i++ {
-		path = filepath.Dir(filepath.Clean(path))
+		root = path.Dir(path.Clean(root))
 	}
 	qSep := regexp.QuoteMeta(sep)
 	// Part of the regexp that matches `/github.com/username/reponame/(pkg/)`.
 	pkgStrip := qSep + strings.Repeat(strings.Join([]string{"[^", "]+", ""}, qSep), 3) + "(?:pkg/)?(.*)"
-	if !strings.Contains(path, sep) {
+	if !strings.Contains(root, sep) {
 		// This is again the unusual case above. The actual callsites will have
 		// a "real" caller, so now we don't exactly know what to strip; going
 		// up to the rightmost "src" directory will be correct unless someone
 		// creates packages inside of a "src" directory within their GOPATH.
 		return regexp.MustCompile(".*" + qSep + "src" + pkgStrip)
 	}
-	if !strings.HasSuffix(path, sep+"src") && !strings.HasSuffix(path, sep+"vendor") {
-		panic("unable to find base path for default call resolver, got " + path)
+	if !strings.HasSuffix(root, sep+"src") && !strings.HasSuffix(root, sep+"vendor") {
+		panic("unable to find base path for default call resolver, got " + root)
 	}
-	return regexp.MustCompile(regexp.QuoteMeta(path) + pkgStrip)
+	return regexp.MustCompile(regexp.QuoteMeta(root) + pkgStrip)
 }()
 
 var defaultCallResolver = NewCallResolver(defaultRE)
@@ -120,7 +119,7 @@ func (cr *CallResolver) Lookup(depth int) (file string, line int, fun string) {
 		if len(matches) == 1 {
 			file = matches[0]
 		} else {
-			file = filepath.Join(matches[1:]...)
+			file = path.Join(matches[1:]...)
 		}
 	}
 
