001173747142f183105ff4aa6ae7f0803463a2db
Tamir Duberstein <tamird@gmail.com>
2017-04-01 16:07:12 -0400

    log: correctly implement stderr redirects
    The previous implementation passed a handle as the first argument to
    SetStdHandle, but that function expects an enum.

--- pkg/util/log/stderr_redirect_linux.go
+++ pkg/util/log/stderr_redirect_linux.go
@@ -15,15 +15,16 @@
 package log
 
 import (
+	"os"
 	"syscall"
 )
 
-func dupFD2(oldfd uintptr, newfd uintptr) error {
+func redirectStderr(f *os.File) error {
 	// NB: This uses Dup3 instead of Dup2 because newer architectures like
 	// linux/arm64 do not include legacy syscalls like Dup2. Dup3 was introduced
 	// in Kernel 2.6.27; at the time of writing, CockroachDB targets 2.6.32, so
 	// we can assume Dup3 is available.
 	//
 	// See https://groups.google.com/forum/#!topic/golang-dev/zpeFtN2z5Fc.
-	return syscall.Dup3(int(oldfd), int(newfd), 0)
+	return syscall.Dup3(int(f.Fd()), syscall.Stderr, 0)
 }
