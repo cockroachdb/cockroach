3524c8ebfdfb966b2e19a6a24bd911eed77112e2
Tamir Duberstein <tamird@gmail.com>
2017-04-02 15:22:41 -0400

    build: install glide before checking deps
    Might address an issue seen in CI:
    
    	[18:19:42]Step 2/3: checkdeps (Command Line) (6s)
    	[18:19:42][Step 2/3] Starting: /home/agent/work/.go/src/github.com/cockroachdb/cockroach/build/teamcity-checkdeps.sh
    	[18:19:42][Step 2/3] in directory: /home/agent/work/.go/src/github.com/cockroachdb/cockroach
    	[18:19:42][Step 2/3] + mkdir -p artifacts
    	[18:19:42][Step 2/3] + export BUILDER_HIDE_GOPATH_SRC=1
    	[18:19:42][Step 2/3] + BUILDER_HIDE_GOPATH_SRC=1
    	[18:19:42][Step 2/3] + build/builder.sh go install ./pkg/cmd/github-pull-request-make
    	[18:19:46][Step 2/3] + build/builder.sh env BUILD_VCS_NUMBER=a5cf014416ca5d4f222f67df39b04700ee0f95b0 TARGET=checkdeps github-pull-request-make
    	[18:19:46][Step 2/3] 2017/04/02 18:19:46 GitHub API token environment variable GITHUB_API_TOKEN is not set
    	[18:19:46][Step 2/3] 2017/04/02 18:19:46 [go install github.com/cockroachdb/cockroach/vendor/github.com/Masterminds/glide]
    	[18:19:48][Step 2/3] 2017/04/02 18:19:48 [glide install]
    	[18:19:48][Step 2/3] 2017/04/02 18:19:48 exec: "glide": executable file not found in $PATH
    	[18:19:49][Step 2/3] Process exited with code 1
    	[18:19:49][Step 2/3] Step checkdeps (Command Line) failed
    
    https://teamcity.cockroachdb.com/viewLog.html?tab=buildLog&logTab=tree&filter=debug&expand=all&buildId=203996#_focus=130

--- build/teamcity-checkdeps.sh
+++ build/teamcity-checkdeps.sh
@@ -5,7 +5,7 @@ mkdir -p artifacts
 
 export BUILDER_HIDE_GOPATH_SRC=1
 
-build/builder.sh go install ./pkg/cmd/github-pull-request-make
+build/builder.sh go install ./vendor/github.com/Masterminds/glide ./pkg/cmd/github-pull-request-make
 
 build/builder.sh env \
 	BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" \
