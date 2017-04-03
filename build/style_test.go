15853e10736e547ec30333a0aeafadf1ce73cf80
Tamir Duberstein <tamird@gmail.com>
2017-04-03 14:48:03 -0400

    sql/pgwire: avoid hardcoding "/tmp"

--- build/style_test.go
+++ build/style_test.go
@@ -139,8 +139,11 @@ func TestStyle(t *testing.T) {
 
 		if err := stream.ForEach(stream.Sequence(
 			filter,
-			stream.GrepNot(`^cmd/`),
-			stream.GrepNot(`^(build/style_test\.go|ccl/sqlccl/backup_cloud_test\.go|ccl/storageccl/export_storage_test\.go|ccl/acceptanceccl/backup_test\.go|acceptance(/.*)?/\w+\.go)\b`),
+			stream.GrepNot(`^cmd(/.*)?/\w+\.go\b`),
+			stream.GrepNot(`^build/style_test\.go\b`),
+			stream.GrepNot(`^ccl/(sqlccl/backup_cloud|storageccl/export_storage|acceptanceccl/backup)_test\.go\b`),
+			stream.GrepNot(`^sql/pgwire/pgwire_test.go\b`),
+			stream.GrepNot(`^acceptance(/.*)?/\w+\.go\b`),
 			stream.GrepNot(`^util/(log|envutil|sdnotify)/\w+\.go\b`),
 		), func(s string) {
 			t.Errorf(`%s <- forbidden; use "envutil" instead`, s)
--- pkg/sql/pgwire/pgwire_test.go
+++ pkg/sql/pgwire/pgwire_test.go
@@ -1426,17 +1426,36 @@ func TestPrepareSyntax(t *testing.T) {
 func TestPGWireOverUnixSocket(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 
-	// We need a temp directory in which we'll create the
-	// unix socket ".s.PGSQL.<port>".
-	// We hard-code "/tmp" as the directory as the osx default can cause
-	// the socket filename length to exceed 104 characters, triggering an error.
-	tempDir, err := ioutil.TempDir("/tmp", "cockroach-unix")
+	// We need a temp directory in which we'll create the unix socket.
+	//
+	// On BSD, binding to a socket is limited to a path length of 104 characters
+	// (including the NUL terminator). In glibc, this limit is 108 characters.
+	//
+	// macOS has a tendency to produce very long temporary directory names, so
+	// we hack around it here.
+	//
+	// See https://github.com/golang/go/blob/go1.8/src/os/file_unix.go#L266.
+	const unixEnvKey = "TMPDIR"
+	if tmpdir, ok := os.LookupEnv(unixEnvKey); ok {
+		defer func() {
+			if err := os.Setenv(unixEnvKey, tmpdir); err != nil {
+				t.Fatal(err)
+			}
+		}()
+		if err := os.Unsetenv(unixEnvKey); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	tempDir, err := ioutil.TempDir("", "cockroach-unix")
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer func() { _ = os.RemoveAll(tempDir) }()
 
-	socketFile := filepath.Join(tempDir, ".s.PGSQL.123456")
+	const port = "123456"
+
+	socketFile := filepath.Join(tempDir, ".s.PGSQL."+port)
 
 	params := base.TestServerArgs{
 		Insecure:   true,
@@ -1452,7 +1471,7 @@ func TestPGWireOverUnixSocket(t *testing.T) {
 	pgURL := url.URL{
 		Scheme:   "postgres",
 		User:     url.User(security.RootUser),
-		Host:     ":123456",
+		Host:     net.JoinHostPort("", port),
 		RawQuery: options.Encode(),
 	}
 	t.Logf("PGURL: %s", pgURL.String())
