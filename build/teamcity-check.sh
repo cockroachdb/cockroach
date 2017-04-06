f15df4d4957999acfd2f9f946218d904ce45adf8
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-04-06 11:17:40 -0400

    build: create a Make target for `go generate`
    PR #14629 adjusted our Makefile to install tools into REPO_ROOT/bin
    instead of GOBIN. This change appears to have broken builds on fresh
    TeamCity agents. Because `go generate` was previously invoked outside
    the Makefile, `go generate` ran with a PATH that did not include
    REPO_ROOT/bin.
    
    This commit adds a `generate` target to the Makefile that ensures `go
    generate` is run with the correct PATH. It has the added benefit of
    symmetry with the other Make targets that shadow Go tools, like `make
    build`, `make install`, and `make test`.
    
    This commit additionally adjusts `make check` and `make generate` to
    depend on `gotestdashi`; there was previously an implicit dependency
    here that could lead to hard-to-track-down failures. (See the comment
    that was moved from teamcity-check.sh to the Makefile for details.)

--- build/teamcity-check.sh
+++ build/teamcity-check.sh
@@ -1,25 +1,16 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
-# All packages need to be installed before we can run (some) of the checks
-# and code generators reliably. More precisely, anything that using
-# x/tools/go/loader is fragile (this includes stringer, vet and others).
-#
-# The blocking issue is https://github.com/golang/go/issues/14120; see
-# https://github.com/golang/go/issues/10249 for some more concrete discussion
-# on `stringer` and https://github.com/golang/go/issues/16086 for `vet`.
 export BUILDER_HIDE_GOPATH_SRC=1
-build/builder.sh make gotestdashi
 
 mkdir -p artifacts
 
 build/builder.sh make check 2>&1 | tee artifacts/check.log | go-test-teamcity
 
-build/builder.sh go generate ./pkg/...
+build/builder.sh make generate
 build/builder.sh /bin/bash -c '! git status --porcelain | read || (git status; git diff -a 1>&2; exit 1)'
 
-# If the code is new enough to have go generate not
-# run the ui tests, run the ui tests.
-if grep "make generate" pkg/ui/ui.go; then
-    build/builder.sh make -C pkg/ui
-fi
+# Run the UI tests. This logically belongs in teamcity-test.sh, but we do it
+# here to minimize total build time since the rest of this script completes
+# faster than the non-UI tests.
+build/builder.sh make -C pkg/ui test
