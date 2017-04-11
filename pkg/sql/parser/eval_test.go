a4e7ca4ca7fcd32a1c3c6f1b38a9f2068d43496e
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-04-11 16:50:27 +0000

    sql: use knz/strtime for time/string conversions for cross-platform stability.
    Prior to this patch `experimental_strptime` was using the host
    system's strptime however that tends to change behavior depending on
    the platform.  This patch fixes the issue by using the function from
    package `knz/strtime` instead, which provides a common implementation
    for all platforms.
    
    Additionally, this package ensures that the `strftime` and `strptime`
    support the same conversion specifiers; they are also both extended to
    support nanosecond precision on second fractional parts.
    
    Finally, the interface of the SQL built-in function `experimental_strptime`
    is changed to adopt the standard argument order (stringtoconvert, format).

--- pkg/sql/parser/eval_test.go
+++ pkg/sql/parser/eval_test.go
@@ -17,6 +17,7 @@
 package parser
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
 	"testing"
@@ -695,14 +696,6 @@ func TestEval(t *testing.T) {
 		{`extract_duration(second from '10m20s30ms'::interval)`, `620`},
 		{`extract_duration(millisecond from '20s30ms40µs'::interval)`, `20030`},
 		{`extract_duration(microsecond from '12345ns'::interval)`, `12`},
-		// Time and date conversion.
-		{`experimental_strftime('2016-09-28'::date, '%d@%Y/%m')`, `'28@2016/09'`},
-		{`experimental_strftime('2010-01-10 12:13:14.123456+00:00'::timestamp, '%a %A %w %d %b %B %m %y %Y %H %I %p %M %S %f %z %Z %j %U %W %c %x %X %%')`,
-			`'Sun Sunday 0 10 Jan January 01 10 2010 12 12 PM 13 14 123456 +0000 UTC 010 02 01 Sun Jan 10 12:13:14 2010 01/10/10 12:13:14 %'`},
-		{`experimental_strftime('2010-01-10 12:13:14.123456+00:00'::timestamptz, '%a %A %w %d %b %B %m %y %Y %H %I %p %M %S %f %z %Z %j %U %W %c %x %X %%')`,
-			`'Sun Sunday 0 10 Jan January 01 10 2010 12 12 PM 13 14 123456 +0000 UTC 010 02 01 Sun Jan 10 12:13:14 2010 01/10/10 12:13:14 %'`},
-		{`experimental_strptime('%d %Y %B', '03 2006 December')`, `'2006-12-03 00:00:00+00:00'`},
-		{`experimental_strptime('%y %m %d %M %S %H', '06 12 21 05 33 14')`, `'2006-12-21 14:05:33+00:00'`},
 		// Need two interval ops to verify the return type matches the return struct type.
 		{`'2010-09-28 12:00:00.1-04:00'::timestamptz - '0s'::interval - '0s'::interval`, `'2010-09-28 16:00:00.1+00:00'`},
 		{`'12h2m1s23ms'::interval + '1h'::interval`, `'13h2m1s23ms'`},
@@ -854,6 +847,151 @@ func TestEval(t *testing.T) {
 		}
 		if s := r.String(); d.expected != s {
 			t.Errorf("%s: expected %s, but found %s", d.expr, d.expected, s)
+		}
+	}
+}
+
+func TestTimeConversion(t *testing.T) {
+	tests := []struct {
+		start     string
+		format    string
+		tm        string
+		revformat string
+		reverse   string
+	}{
+		// %a %A %b %B (+ %Y)
+		{`Wed Oct 05 2016`, `%a %b %d %Y`, `2016-10-05 00:00:00+00:00`, ``, ``},
+		{`Wednesday October 05 2016`, `%A %B %d %Y`, `2016-10-05 00:00:00+00:00`, ``, ``},
+		// %c
+		{`Wed Oct 5 01:02:03 2016`, `%c`, `2016-10-05 01:02:03+00:00`, ``, ``},
+		// %C %d (+ %m %y)
+		{`20 06 10 12`, `%C %y %m %d`, `2006-10-12 00:00:00+00:00`, ``, ``},
+		// %D
+		{`10/12/06`, `%D`, `2006-10-12 00:00:00+00:00`, ``, ``},
+		// %e (+ %Y %m)
+		{`2006 10  3`, `%Y %m %e`, `2006-10-03 00:00:00+00:00`, ``, ``},
+		// %f (+ %c)
+		{`Wed Oct 5 01:02:03 2016 .123`, `%c .%f`, `2016-10-05 01:02:03.123+00:00`, `.%f`, `.123000000`},
+		{`Wed Oct 5 01:02:03 2016 .123456`, `%c .%f`, `2016-10-05 01:02:03.123456+00:00`, `.%f`, `.123456000`},
+		{`Wed Oct 5 01:02:03 2016 .123456789`, `%c .%f`, `2016-10-05 01:02:03.123457+00:00`, `.%f`, `.123457000`},
+		{`Wed Oct 5 01:02:03 2016 .999999999`, `%c .%f`, `2016-10-05 01:02:04+00:00`, `.%f`, `.000000000`},
+		// %F
+		{`2006-10-03`, `%F`, `2006-10-03 00:00:00+00:00`, ``, ``},
+		// %h (+ %Y %d)
+		{`2006 Oct 03`, `%Y %h %d`, `2006-10-03 00:00:00+00:00`, ``, ``},
+		// %H (+ %S %M)
+		{`20061012 01:03:02`, `%Y%m%d %H:%S:%M`, `2006-10-12 01:02:03+00:00`, ``, ``},
+		// %I (+ %Y %m %d)
+		{`20161012 11`, `%Y%m%d %I`, `2016-10-12 11:00:00+00:00`, ``, ``},
+		// %j (+ %Y)
+		{`2016 286`, `%Y %j`, `2016-10-12 00:00:00+00:00`, ``, ``},
+		// %k (+ %Y %m %d)
+		{`20061012 23`, `%Y%m%d %k`, `2006-10-12 23:00:00+00:00`, ``, ``},
+		// %l (+ %Y %m %d %p)
+		{`20061012  5 PM`, `%Y%m%d %l %p`, `2006-10-12 17:00:00+00:00`, ``, ``},
+		// %n (+ %Y %m %d)
+		{"2006\n10\n03", `%Y%n%m%n%d`, `2006-10-03 00:00:00+00:00`, ``, ``},
+		// %p cannot be parsed before hour specifiers, so be sure that
+		// they appear in this order.
+		{`20161012 11 PM`, `%Y%m%d %I %p`, `2016-10-12 23:00:00+00:00`, ``, ``},
+		{`20161012 11 AM`, `%Y%m%d %I %p`, `2016-10-12 11:00:00+00:00`, ``, ``},
+		// %r
+		{`20161012 11:02:03 PM`, `%Y%m%d %r`, `2016-10-12 23:02:03+00:00`, ``, ``},
+		// %R
+		{`20161012 11:02`, `%Y%m%d %R`, `2016-10-12 11:02:00+00:00`, ``, ``},
+		// %s
+		{`1491920586`, `%s`, `2017-04-11 14:23:06+00:00`, ``, ``},
+		// %t (+ %Y %m %d)
+		{"2006\t10\t03", `%Y%t%m%t%d`, `2006-10-03 00:00:00+00:00`, ``, ``},
+		// %T (+ %Y %m %d)
+		{`20061012 01:02:03`, `%Y%m%d %T`, `2006-10-12 01:02:03+00:00`, ``, ``},
+		// %U %u (+ %Y)
+		{`2018 10 4`, `%Y %U %u`, `2018-03-15 00:00:00+00:00`, ``, ``},
+		// %W %w (+ %Y)
+		{`2018 10 4`, `%Y %W %w`, `2018-03-08 00:00:00+00:00`, ``, ``},
+		// %x
+		{`10/12/06`, `%x`, `2006-10-12 00:00:00+00:00`, ``, ``},
+		// %X
+		{`20061012 01:02:03`, `%Y%m%d %X`, `2006-10-12 01:02:03+00:00`, ``, ``},
+		// %y (+ %m %d)
+		{`000101`, `%y%m%d`, `2000-01-01 00:00:00+00:00`, ``, ``},
+		{`680101`, `%y%m%d`, `2068-01-01 00:00:00+00:00`, ``, ``},
+		{`690101`, `%y%m%d`, `1969-01-01 00:00:00+00:00`, ``, ``},
+		{`990101`, `%y%m%d`, `1999-01-01 00:00:00+00:00`, ``, ``},
+		// %Y
+		{`19000101`, `%Y%m%d`, `1900-01-01 00:00:00+00:00`, ``, ``},
+		{`20000101`, `%Y%m%d`, `2000-01-01 00:00:00+00:00`, ``, ``},
+		{`30000101`, `%Y%m%d`, `3000-01-01 00:00:00+00:00`, ``, ``},
+		// %z causes the time zone to adjust the time when parsing, but the time zone information
+		// is not retained when printing the timestamp out back.
+		{`20160101 13:00 +0655`, `%Y%m%d %H:%M %z`, `2016-01-01 06:05:00+00:00`, `%Y%m%d %H:%M %z`, `20160101 06:05 +0000`},
+	}
+
+	for _, test := range tests {
+		ctx := &EvalContext{}
+		exprStr := fmt.Sprintf("experimental_strptime('%s', '%s')", test.start, test.format)
+		expr, err := ParseExprTraditional(exprStr)
+		if err != nil {
+			t.Errorf("%s: %v", exprStr, err)
+			continue
+		}
+		typedExpr, err := expr.TypeCheck(nil, TypeTimestamp)
+		if err != nil {
+			t.Errorf("%s: %v", exprStr, err)
+			continue
+		}
+		r, err := typedExpr.Eval(ctx)
+		if err != nil {
+			t.Errorf("%s: %v", exprStr, err)
+			continue
+		}
+		ts, ok := r.(*DTimestampTZ)
+		if !ok {
+			t.Errorf("%s: result not a timestamp: %s", exprStr, r)
+			continue
+		}
+
+		tmS := ts.String()
+		tmS = tmS[1 : len(tmS)-1] // strip the quote delimiters
+		if tmS != test.tm {
+			t.Errorf("%s: got %q, expected %q", exprStr, tmS, test.tm)
+			continue
+		}
+
+		revfmt := test.format
+		if test.revformat != "" {
+			revfmt = test.revformat
+		}
+
+		ref := test.start
+		if test.reverse != "" {
+			ref = test.reverse
+		}
+
+		exprStr = fmt.Sprintf("experimental_strftime('%s'::timestamp, '%s')", tmS, revfmt)
+		expr, err = ParseExprTraditional(exprStr)
+		if err != nil {
+			t.Errorf("%s: %v", exprStr, err)
+			continue
+		}
+		typedExpr, err = expr.TypeCheck(nil, TypeTimestamp)
+		if err != nil {
+			t.Errorf("%s: %v", exprStr, err)
+			continue
+		}
+		r, err = typedExpr.Eval(ctx)
+		if err != nil {
+			t.Errorf("%s: %v", exprStr, err)
+			continue
+		}
+		rs, ok := r.(*DString)
+		if !ok {
+			t.Errorf("%s: result not a string: %s", exprStr, r)
+			continue
+		}
+		revS := string(*rs)
+		if ref != revS {
+			t.Errorf("%s: got %q, expected %q", exprStr, revS, ref)
 		}
 	}
 }
