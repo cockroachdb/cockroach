486c66b805fa32096c6be973ab9a4974c9cda734
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-04-17 16:21:58 -0400

    sqlccl: optionally use time bound iterators for BACKUP
    This brings a huge speedup to BACKUPs that don't need to inspect much
    data (e.g. an incremental backup over a small time window).

--- pkg/ccl/sqlccl/bench_test.go
+++ pkg/ccl/sqlccl/bench_test.go
@@ -12,10 +12,15 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"path/filepath"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/tracing"
+	"github.com/pkg/errors"
 )
 
 func bankStatementBuf(numAccounts int) *bytes.Buffer {
@@ -127,4 +132,78 @@ func BenchmarkLoadSQL(b *testing.B) {
 		sqlDB.Exec(line)
 	}
 	b.StopTimer()
+}
+
+const withTimeBoundIterators = true
+const withoutTimeBoundIterators = false
+
+func setTimeBoundIterators(tb testing.TB, tc *testcluster.TestCluster, timeBoundIterators bool) {
+	runner := sqlutils.MakeSQLRunner(tb, tc.Conns[0])
+	runner.Exec(
+		fmt.Sprintf("SET CLUSTER SETTING engineccl.time.bound.iterators.enabled = %t", timeBoundIterators),
+	)
+
+	show := "SHOW CLUSTER SETTING engineccl.time.bound.iterators.enabled"
+	for _, conn := range tc.Conns {
+		testutils.SucceedsSoon(tb, func() error {
+			var set string
+			sqlutils.MakeSQLRunner(tb, conn).QueryRow(show).Scan(&set)
+			if set != fmt.Sprintf("%t", timeBoundIterators) {
+				return errors.Errorf("engineccl.time.bound.iterators.enabled is not %t", timeBoundIterators)
+			}
+			return nil
+		})
+	}
+}
+
+func runEmptyIncrementalBackup(b *testing.B, timeBoundIterators bool) {
+	defer tracing.Disable()()
+
+	const numStatements = 100000
+
+	ctx, dir, tc, sqlDB, cleanupFn := backupRestoreTestSetup(b, multiNode, 0)
+	defer cleanupFn()
+
+	setTimeBoundIterators(b, tc, timeBoundIterators)
+
+	restoreDir := filepath.Join(dir, "restore")
+	fullDir := filepath.Join(dir, "full")
+
+	ts := hlc.Timestamp{WallTime: hlc.UnixNano()}
+	if _, err := Load(
+		ctx, sqlDB.DB, bankStatementBuf(numStatements), "bench", restoreDir, ts, 0, restoreDir,
+	); err != nil {
+		b.Fatalf("%+v", err)
+	}
+	sqlDB.Exec(`RESTORE bench.* FROM $1`, restoreDir)
+
+	var unused string
+	var dataSize int64
+	sqlDB.QueryRow(`BACKUP DATABASE bench TO $1`, fullDir).Scan(
+		&unused, &unused, &unused, &dataSize,
+	)
+
+	// We intentionally don't write anything to the database between the full and
+	// incremental backup.
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		incrementalDir := filepath.Join(dir, fmt.Sprintf("incremental%d", i))
+		sqlDB.Exec(`BACKUP DATABASE bench TO $1 INCREMENTAL FROM $2`, incrementalDir, fullDir)
+	}
+	b.StopTimer()
+
+	// We report the number of bytes that incremental backup was able to
+	// *skip*--i.e., the number of bytes in the full backup.
+	b.SetBytes(int64(b.N) * dataSize)
+}
+
+func BenchmarkClusterEmptyIncrementalBackup(b *testing.B) {
+	b.Run("Normal", func(b *testing.B) {
+		runEmptyIncrementalBackup(b, withoutTimeBoundIterators)
+	})
+
+	b.Run("TimeBound", func(b *testing.B) {
+		runEmptyIncrementalBackup(b, withTimeBoundIterators)
+	})
 }
