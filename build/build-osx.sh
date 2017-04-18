2a8e3b2a447d2ae7b48416d966ce2c97fdeba7ee
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-04-18 09:49:05 -0400

    build: separate XGOFLAGS from GOFLAGS
    xgo does not accept `-installsuffix`, so we need a separate variable for
    its flags now that `-installsuffix` is added to GOFLAGS when TYPE is
    set.
    
    This should fix the release build.

--- build/build-osx.sh
+++ build/build-osx.sh
@@ -11,4 +11,4 @@ go get -u github.com/karalabe/xgo
 # We additionally set GOVERS= to disable the Go version check. This allows
 # building in environments without a Go toolchain, like TeamCity build agents.
 # xgo will use the Go toolchain in its Docker image regardless.
-make xgo-build GOVERS= GOFLAGS='--image cockroachdb/xgo:20170218 --targets=darwin-10.9/amd64' TYPE=release
+make xgo-build GOVERS= XGOFLAGS='--image cockroachdb/xgo:20170218 --targets=darwin-10.9/amd64' TYPE=release
