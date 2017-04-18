25c6dcdc6991bf77fa8b2e7f836a82c2861e3f18
Radu Berinde <radu@cockroachlabs.com>
2017-04-17 22:43:32 -0400

    sql: rip out the SQL event log from the session context
    Embedding the event log in the session context doesn't do what we want anymore;
    we want to log the SQL statements to the event log even when they go to a
    trace. Create the event log separately and write to it the executed statements
    and results.
    
    Fixes #15007.

--- pkg/sql/executor.go
+++ pkg/sql/executor.go
@@ -1079,6 +1079,18 @@ func (e *Executor) execStmtInCommitWaitTxn(
 	}
 }
 
+// sessionEventf logs an event to the current transaction context and to the
+// session event log.
+func sessionEventf(session *Session, format string, args ...interface{}) {
+	if session.eventLog == nil {
+		log.Eventf(session.context, format, args...)
+	} else {
+		str := fmt.Sprintf(format, args...)
+		log.Event(session.context, str)
+		session.eventLog.Printf("%s", str)
+	}
+}
+
 // execStmtInOpenTxn executes one statement in the context
 // of the session's current transaction (which is assumed to exist).
 // It handles statements that affect the transaction state (BEGIN, COMMIT)
@@ -1120,7 +1132,7 @@ func (e *Executor) execStmtInOpenTxn(
 		panic("execStmtInOpenTxn called outside of an open txn")
 	}
 
-	log.Eventf(session.Ctx(), "%s", stmt)
+	sessionEventf(session, "%s", stmt)
 
 	// Do not double count automatically retried transactions.
 	if automaticRetryCount == 0 {
@@ -1260,7 +1272,7 @@ func (e *Executor) execStmtInOpenTxn(
 	case parser.Rows:
 		tResult.count = result.Rows.Len()
 	}
-	log.Eventf(session.Ctx(), "%s done", tResult)
+	sessionEventf(session, "%s done", tResult)
 	return result, nil
 }
 
