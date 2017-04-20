68c4614e72fac7f1c6791289573265c59ddee7dd
Spencer Kimball <spencer.kimball@gmail.com>
2017-04-20 09:21:57 -0400

    storage: wait on lease renewal when in stasis, kv: retry not leaseholder errors
    In storage, discovered a bug where we would fail to wait on a lease renewal
    for a range where the existing lease was in state=stasis.
    
    In kv, `DistSender.sendToReplicas` now retries replicas which previously
    failed with retryable errors (right now this is just `NotLeaseHolderErrors`).
    
    Fixes #11221

--- pkg/sql/ambiguous_commit_test.go
+++ pkg/sql/ambiguous_commit_test.go
@@ -18,8 +18,10 @@ package sql_test
 
 import (
 	"bytes"
+	"fmt"
 	"sync/atomic"
 	"testing"
+	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/keys"
@@ -44,111 +46,127 @@ import (
 func TestAmbiguousCommitDueToLeadershipChange(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 
-	// Create a command filter which prevents EndTransaction from
-	// returning a response.
-	params := base.TestServerArgs{}
-	committed := make(chan struct{})
-	wait := make(chan struct{})
-	var tableStartKey atomic.Value
-	var responseCount int32
+	for _, requireDistSenderRetry := range []bool{true, false} {
+		t.Run(fmt.Sprintf("retry=%t", requireDistSenderRetry), func(t *testing.T) {
+			// Create a command filter which prevents EndTransaction from
+			// returning a response.
+			params := base.TestServerArgs{}
+			committed := make(chan struct{})
+			wait := make(chan struct{})
+			var tableStartKey atomic.Value
+			var responseCount int32
 
-	// Prevent the first conditional put on table 51 from returning to
-	// waiting client in order to simulate a lost update or slow network
-	// link.
-	params.Knobs.Store = &storage.StoreTestingKnobs{
-		TestingResponseFilter: func(ba roachpb.BatchRequest, br *roachpb.BatchResponse) *roachpb.Error {
-			req, ok := ba.GetArg(roachpb.ConditionalPut)
-			tsk := tableStartKey.Load()
-			if tsk == nil {
-				return nil
+			// Prevent the first conditional put on table 51 from returning to
+			// waiting client in order to simulate a lost update or slow network
+			// link.
+			params.Knobs.Store = &storage.StoreTestingKnobs{
+				TestingResponseFilter: func(ba roachpb.BatchRequest, br *roachpb.BatchResponse) *roachpb.Error {
+					req, ok := ba.GetArg(roachpb.ConditionalPut)
+					tsk := tableStartKey.Load()
+					if tsk == nil {
+						return nil
+					}
+					if !ok || !bytes.HasPrefix(req.Header().Key, tsk.([]byte)) {
+						return nil
+					}
+					// If this is the first write to the table, wait to respond to the
+					// client in order to simulate a retry.
+					if atomic.AddInt32(&responseCount, 1) == 1 {
+						close(committed)
+						<-wait
+					}
+					return nil
+				},
 			}
-			if !ok || !bytes.HasPrefix(req.Header().Key, tsk.([]byte)) {
-				return nil
+			params.SendNextTimeout = 50 * time.Millisecond
+			testClusterArgs := base.TestClusterArgs{
+				ReplicationMode: base.ReplicationAuto,
+				ServerArgs:      params,
 			}
-			// If this is the first write to the table, wait to respond to the
-			// client in order to simulate a retry.
-			if atomic.AddInt32(&responseCount, 1) == 1 {
-				close(committed)
-				<-wait
-			}
-			return nil
-		},
-	}
-	testClusterArgs := base.TestClusterArgs{
-		ReplicationMode: base.ReplicationAuto,
-		ServerArgs:      params,
-	}
-	const numReplicas = 3
-	tc := testcluster.StartTestCluster(t, numReplicas, testClusterArgs)
-	defer tc.Stopper().Stop()
+			const numReplicas = 3
+			tc := testcluster.StartTestCluster(t, numReplicas, testClusterArgs)
+			defer tc.Stopper().Stop()
 
-	sqlDB := sqlutils.MakeSQLRunner(t, tc.Conns[0])
+			sqlDB := sqlutils.MakeSQLRunner(t, tc.Conns[0])
 
-	sqlDB.Exec(`CREATE DATABASE test`)
-	sqlDB.Exec(`CREATE TABLE test.t (k SERIAL PRIMARY KEY, v INT)`)
+			sqlDB.Exec(`CREATE DATABASE test`)
+			sqlDB.Exec(`CREATE TABLE test.t (k SERIAL PRIMARY KEY, v INT)`)
 
-	tableID, err := sqlutils.QueryTableID(tc.Conns[0], "test", "t")
-	if err != nil {
-		t.Fatal(err)
-	}
-	tableStartKey.Store(keys.MakeTablePrefix(tableID))
-
-	// Wait for new table to split & replication.
-	if err := tc.WaitForSplitAndReplication(tableStartKey.Load().([]byte)); err != nil {
-		t.Fatal(err)
-	}
-
-	// Lookup the lease.
-	tableRangeDesc, err := tc.LookupRange(keys.MakeRowSentinelKey(tableStartKey.Load().([]byte)))
-	if err != nil {
-		t.Fatal(err)
-	}
-	leaseHolder, err := tc.FindRangeLeaseHolder(
-		tableRangeDesc,
-		&roachpb.ReplicationTarget{
-			NodeID:  tc.Servers[0].GetNode().Descriptor.NodeID,
-			StoreID: tc.Servers[0].GetFirstStoreID(),
-		})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// In a goroutine, send an insert which will commit but not return
-	// from the leader (due to the command filter we installed on node 0).
-	sqlErrCh := make(chan error, 1)
-	go func() {
-		// Use a connection other than through the node which is the current
-		// leaseholder to ensure that we use GRPC instead of the local server.
-		// If we use a local server, the hanging response we simulate takes
-		// up the dist sender thread of execution because local requests are
-		// executed synchronously.
-		sqlConn := tc.Conns[leaseHolder.NodeID%numReplicas]
-		_, err := sqlConn.Exec(`INSERT INTO test.t (v) VALUES (1)`)
-		sqlErrCh <- err
-		close(wait)
-	}()
-	// Wait until the insert has committed.
-	<-committed
-
-	// Find a node other than the current lease holder to transfer the lease to.
-	for i, s := range tc.Servers {
-		if leaseHolder.StoreID != s.GetFirstStoreID() {
-			if err := tc.TransferRangeLease(tableRangeDesc, tc.Target(i)); err != nil {
+			tableID, err := sqlutils.QueryTableID(tc.Conns[0], "test", "t")
+			if err != nil {
 				t.Fatal(err)
 			}
-			break
-		}
-	}
+			tableStartKey.Store(keys.MakeTablePrefix(tableID))
 
-	// Wait for the error from the pending SQL insert.
-	if err := <-sqlErrCh; !testutils.IsError(err, "result is ambiguous") {
-		t.Errorf("expected ambiguous commit error; got %v", err)
-	}
+			// Wait for new table to split & replication.
+			if err := tc.WaitForSplitAndReplication(tableStartKey.Load().([]byte)); err != nil {
+				t.Fatal(err)
+			}
 
-	// Verify a single row exists in the table.
-	var rowCount int
-	sqlDB.QueryRow(`SELECT count(*) FROM test.t`).Scan(&rowCount)
-	if rowCount != 1 {
-		t.Errorf("expected 1 row but found %d", rowCount)
+			// Lookup the lease.
+			tableRangeDesc, err := tc.LookupRange(keys.MakeRowSentinelKey(tableStartKey.Load().([]byte)))
+			if err != nil {
+				t.Fatal(err)
+			}
+			// Loop until we can get the lease holder. We query in the loop to
+			// ensure that the lease is acquired.
+			var leaseHolder roachpb.ReplicationTarget
+			testutils.SucceedsSoon(t, func() error {
+				rows := sqlDB.Query(`SELECT * FROM test.t`)
+				rows.Close()
+				leaseHolder, err = tc.FindRangeLeaseHolder(
+					tableRangeDesc,
+					&roachpb.ReplicationTarget{
+						NodeID:  tc.Servers[0].GetNode().Descriptor.NodeID,
+						StoreID: tc.Servers[0].GetFirstStoreID(),
+					})
+				return err
+			})
+
+			// In a goroutine, send an insert which will commit but not return
+			// from the leader (due to the command filter we installed on node 0).
+			sqlErrCh := make(chan error, 1)
+			go func() {
+				// Use a connection other than through the node which is the current
+				// leaseholder to ensure that we use GRPC instead of the local server.
+				// If we use a local server, the hanging response we simulate takes
+				// up the dist sender thread of execution because local requests are
+				// executed synchronously.
+				sqlConn := tc.Conns[leaseHolder.NodeID%numReplicas]
+				_, err := sqlConn.Exec(`INSERT INTO test.t (v) VALUES (1)`)
+				sqlErrCh <- err
+				close(wait)
+			}()
+			// Wait until the insert has committed.
+			<-committed
+
+			// If requested, wait for two further attempts from the distributed sender.
+			if requireDistSenderRetry {
+				// Wait for twice the send next timeout.
+				time.Sleep(2 * params.SendNextTimeout)
+			}
+
+			// Find a node other than the current lease holder to transfer the lease to.
+			for i, s := range tc.Servers {
+				if leaseHolder.StoreID != s.GetFirstStoreID() {
+					if err := tc.TransferRangeLease(tableRangeDesc, tc.Target(i)); err != nil {
+						t.Fatal(err)
+					}
+					break
+				}
+			}
+
+			// Wait for the error from the pending SQL insert.
+			if err := <-sqlErrCh; !testutils.IsError(err, "result is ambiguous") {
+				t.Errorf("expected ambiguous commit error; got %v", err)
+			}
+
+			// Verify a single row exists in the table.
+			var rowCount int
+			sqlDB.QueryRow(`SELECT count(*) FROM test.t`).Scan(&rowCount)
+			if rowCount != 1 {
+				t.Errorf("expected 1 row but found %d", rowCount)
+			}
+		})
 	}
 }
