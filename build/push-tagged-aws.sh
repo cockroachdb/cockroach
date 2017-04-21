99f5b4b38285c85b1d014d1128a54903b4c42f92
Tamir Duberstein <tamird@gmail.com>
2017-04-20 20:40:30 -0400

    build: package Windows binaries as zip rather than tgz
    Closes #15223.

--- build/push-tagged-aws.sh
+++ build/push-tagged-aws.sh
@@ -21,12 +21,12 @@ deploy_file() {
   time aws s3 cp "$1" "s3://${BUCKET_NAME}/$1"
 }
 
-# make_and_deploy_tarball takes the path to the binary inside the repo and the
-# tarball base name.
-# eg: make_and_deploy_tarball cockroach-darwin-10.9-amd64 cockroach.darwin-amd64
+# make_and_deploy_archive takes the path to the binary inside the repo and the
+# archive base name.
+# eg: make_and_deploy_archive cockroach-darwin-10.9-amd64 cockroach.darwin-amd64
 #   generates cockroach.darwin-amd64.tgz which expands into cockroach.darwin-amd64/cockroach
-#   copies tarball to s3://BUCKET_NAME/cockroach.darwin-amd64.tgz
-make_and_deploy_tarball() {
+#   copies archive to s3://BUCKET_NAME/cockroach.darwin-amd64.tgz
+make_and_deploy_archive() {
   rel_path=$1
   tarball_base=$2
 
@@ -38,25 +38,29 @@ make_and_deploy_tarball() {
   case "${rel_path}" in
   *.exe)
     cp "${rel_path}" "${tmpdir}/${tarball_base}/cockroach.exe"
+    pushd "$tmpdir"
+    time zip -r "${tarball_base}".zip "${tarball_base}"
+    deploy_file "${tarball_base}.zip"
+    popd
     ;;
   *)
     cp "${rel_path}" "${tmpdir}/${tarball_base}/cockroach"
+    time tar cz -C "${tmpdir}" -f "${tarball_base}".tgz "${tarball_base}"
+    deploy_file "${tarball_base}.tgz"
     ;;
   esac
-  time tar cz -C "${tmpdir}" -f "${tarball_base}".tgz "${tarball_base}"
-  deploy_file "${tarball_base}.tgz"
   rm -rf "${tmpdir}"
 }
 
 # Tar up and push binaries. One tagged, one called "latest".
-make_and_deploy_tarball cockroach cockroach-"${VERSION}".linux-amd64
-make_and_deploy_tarball cockroach cockroach-latest.linux-amd64
-make_and_deploy_tarball cockroach-linux-2.6.32-musl-amd64 cockroach-"${VERSION}".linux-musl-amd64
-make_and_deploy_tarball cockroach-linux-2.6.32-musl-amd64 cockroach-latest.linux-musl-amd64
-make_and_deploy_tarball cockroach-darwin-10.9-amd64 cockroach-"${VERSION}".darwin-10.9-amd64
-make_and_deploy_tarball cockroach-darwin-10.9-amd64 cockroach-latest.darwin-10.9-amd64
-make_and_deploy_tarball cockroach-windows-6.2-amd64.exe cockroach-"${VERSION}".windows-6.2-amd64
-make_and_deploy_tarball cockroach-windows-6.2-amd64.exe cockroach-latest.windows-6.2-amd64
+make_and_deploy_archive cockroach cockroach-"${VERSION}".linux-amd64
+make_and_deploy_archive cockroach cockroach-latest.linux-amd64
+make_and_deploy_archive cockroach-linux-2.6.32-musl-amd64 cockroach-"${VERSION}".linux-musl-amd64
+make_and_deploy_archive cockroach-linux-2.6.32-musl-amd64 cockroach-latest.linux-musl-amd64
+make_and_deploy_archive cockroach-darwin-10.9-amd64 cockroach-"${VERSION}".darwin-10.9-amd64
+make_and_deploy_archive cockroach-darwin-10.9-amd64 cockroach-latest.darwin-10.9-amd64
+make_and_deploy_archive cockroach-windows-6.2-amd64.exe cockroach-"${VERSION}".windows-6.2-amd64
+make_and_deploy_archive cockroach-windows-6.2-amd64.exe cockroach-latest.windows-6.2-amd64
 
 # Push source tarball for Homebrew and other package managers that like
 # tarballs. One tagged, one called "latest".
