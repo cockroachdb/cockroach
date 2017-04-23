10511c76bc77c895cecfdfc65afa05aa6b4f9b52
Richard Artoul <richardartoul@gmail.com>
2017-04-23 14:41:42 -0700

    pkg/cli: Refactor CLI to support streaming formatted results
    Fixes #13362

--- pkg/cli/format_table.go
+++ pkg/cli/format_table.go
@@ -29,15 +29,82 @@ import (
 	"github.com/olekukonko/tablewriter"
 )
 
+// rowStrIter is an iterator interface for the printQueryOutput function. It is
+// used so that results can be streamed to the row formatters as they arrive
+// to the CLI.
+type rowStrIter interface {
+	Next() (row []string, err error)
+	ToSlice() (allRows [][]string, err error)
+}
+
+// rowSliceIter is an implementation of the rowStrIter interface and it is used
+// to wrap a slice of rows that have already been completely buffered into
+// memory.
+type rowSliceIter struct {
+	allRows [][]string
+	index   int
+}
+
+func (iter *rowSliceIter) Next() (row []string, err error) {
+	if iter.index >= len(iter.allRows) {
+		return nil, io.EOF
+	}
+	row = iter.allRows[iter.index]
+	iter.index = iter.index + 1
+	return row, nil
+}
+
+func (iter *rowSliceIter) ToSlice() ([][]string, error) {
+	return iter.allRows, nil
+}
+
+// newRowSliceIter is an implementation of the rowStrIter interface and it is
+// used when the rows have not been buffered into memory yet and we want to
+// stream them to the row formatters as they arrive over the network.
+func newRowSliceIter(allRows [][]string) *rowSliceIter {
+	return &rowSliceIter{
+		allRows: allRows,
+		index:   0,
+	}
+}
+
+type rowIter struct {
+	rows          *sqlRows
+	showMoreChars bool
+}
+
+func (iter *rowIter) Next() (row []string, err error) {
+	nextRowString, err := getNextRowStrings(iter.rows, iter.showMoreChars)
+	if nextRowString == nil {
+		return nil, io.EOF
+	}
+	if err != nil {
+		return nil, err
+	}
+	return nextRowString, nil
+}
+
+func (iter *rowIter) ToSlice() ([][]string, error) {
+	return getAllRowStrings(iter.rows, iter.showMoreChars)
+}
+
+func newRowIter(rows *sqlRows, showMoreChars bool) *rowIter {
+	return &rowIter{
+		rows:          rows,
+		showMoreChars: showMoreChars,
+	}
+}
+
 // printQueryOutput takes a list of column names and a list of row contents
-// writes a formatted table to 'w', or simply the tag if empty.
+// writes a formatted table to 'w', or simply the tag if empty. Note that
+// printQueryOutput expects the tag to already be properly formatted.
 func printQueryOutput(
-	w io.Writer, cols []string, allRows [][]string, tag string, displayFormat tableDisplayFormat,
-) {
+	w io.Writer, cols []string, allRows rowStrIter, tag string, displayFormat tableDisplayFormat,
+) error {
 	if len(cols) == 0 {
 		// This operation did not return rows, just show the tag.
 		fmt.Fprintln(w, tag)
-		return
+		return nil
 	}
 
 	switch displayFormat {
@@ -47,28 +114,40 @@ func printQueryOutput(
 		table.SetAutoFormatHeaders(false)
 		table.SetAutoWrapText(false)
 		table.SetHeader(cols)
-		for _, row := range allRows {
+		nRows := 0
+		for {
+			row, err := allRows.Next()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return err
+			}
 			for i, r := range row {
 				row[i] = expandTabsAndNewLines(r)
 			}
 			table.Append(row)
+			nRows++
 		}
 		table.Render()
-		nRows := len(allRows)
 		fmt.Fprintf(w, "(%d row%s)\n", nRows, util.Pluralize(int64(nRows)))
 
 	case tableDisplayTSV:
 		fallthrough
 	case tableDisplayCSV:
-		fmt.Fprintf(w, "%d row%s\n", len(allRows),
-			util.Pluralize(int64(len(allRows))))
+		allRowsSlice, err := allRows.ToSlice()
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(w, "%d row%s\n", len(allRowsSlice),
+			util.Pluralize(int64(len(allRowsSlice))))
 
 		csvWriter := csv.NewWriter(w)
 		if displayFormat == tableDisplayTSV {
 			csvWriter.Comma = '\t'
 		}
 		_ = csvWriter.Write(cols)
-		_ = csvWriter.WriteAll(allRows)
+		_ = csvWriter.WriteAll(allRowsSlice)
 
 	case tableDisplayHTML:
 		fmt.Fprint(w, "<table>\n<thead><tr>")
@@ -76,7 +155,14 @@ func printQueryOutput(
 			fmt.Fprintf(w, "<th>%s</th>", html.EscapeString(col))
 		}
 		fmt.Fprint(w, "</tr></head>\n<tbody>\n")
-		for _, row := range allRows {
+		for {
+			row, err := allRows.Next()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return err
+			}
 			fmt.Fprint(w, "<tr>")
 			for _, r := range row {
 				fmt.Fprintf(w, "<td>%s</td>", strings.Replace(html.EscapeString(r), "\n", "<br/>", -1))
@@ -94,7 +180,14 @@ func printQueryOutput(
 			}
 		}
 
-		for i, row := range allRows {
+		for i := 0; ; i++ {
+			row, err := allRows.Next()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return err
+			}
 			fmt.Fprintf(w, "-[ RECORD %d ]\n", i+1)
 			for j, r := range row {
 				lines := strings.Split(r, "\n")
@@ -122,7 +215,14 @@ func printQueryOutput(
 			fmt.Fprint(w, "\n")
 		}
 		fmt.Fprint(w, ");\n\n")
-		for _, row := range allRows {
+		for {
+			row, err := allRows.Next()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return err
+			}
 			fmt.Fprint(w, "INSERT INTO results VALUES (")
 			for i, r := range row {
 				s := parser.DString(r)
@@ -134,4 +234,5 @@ func printQueryOutput(
 			fmt.Fprint(w, ");\n")
 		}
 	}
+	return nil
 }
