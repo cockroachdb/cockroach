67e4f966cc85615e30ebe8ec72297d54970bb907
Vivek Menezes <vivek.menezes@gmail.com>
2017-04-24 15:44:17 -0400

    sql: acquire a table lease without the users transaction
    table leases are owned by the node and are  acquired
    outside of the context of a user's transaction. This prevents
    old user transactions to be used in acquiring leases.
    
    Also prevent a transaction from using a lease on a table
    descriptor version written after the transaction timestamp.
    Restart transaction whenever it sees a future table lease.
    
    database name -> database id lookup is no longer done
    using the user transaction.
    
    Schema changes made during a transaction are cached and
    used by future commands during the same transaction. Future
    commands use the cached schema and do not attempt to
    acquire a new lease on a table descriptor.
    
    fixes #6774
    fixes #6773

--- pkg/sql/txn_restart_test.go
+++ pkg/sql/txn_restart_test.go
@@ -37,6 +37,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/security"
 	"github.com/cockroachdb/cockroach/pkg/server"
 	"github.com/cockroachdb/cockroach/pkg/sql"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/storage"
 	"github.com/cockroachdb/cockroach/pkg/storage/storagebase"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
@@ -1400,5 +1401,103 @@ func TestDistSQLRetryableError(t *testing.T) {
 
 	if err := txn.Rollback(); err != nil {
 		t.Fatal(err)
+	}
+}
+
+// Test that a transaction cannot use a table lease referencing a table
+// descriptor written after the transaction timestamp.
+func TestCantUseFutureTableLease(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	params, _ := createTestServerParams()
+	retries := 0
+	modifySchema := func() {}
+	params.Knobs = base.TestingKnobs{
+		SQLExecutor: &sql.ExecutorTestingKnobs{
+			StatementFilter: func(ctx context.Context, stmt string, result *sql.Result) {
+				if strings.HasPrefix(stmt, `INSERT INTO d.kv(k, v) VALUES`) {
+					retries++
+				}
+			},
+		},
+		SQLLeaseManager: &sql.LeaseManagerTestingKnobs{
+			LeaseStoreTestingKnobs: sql.LeaseStoreTestingKnobs{
+				RemoveOnceDereferenced: true,
+				LeaseAcquiringEvent: func(id sqlbase.ID) {
+					if id == 51 {
+						if modifySchema != nil {
+							modifySchema()
+						}
+					}
+				},
+			},
+		},
+	}
+	s, sqlDB, _ := serverutils.StartServer(t, params)
+	defer s.Stopper().Stop(context.TODO())
+
+	if _, err := sqlDB.Exec(`
+CREATE DATABASE d;
+CREATE TABLE d.kv (k CHAR PRIMARY KEY, v CHAR);
+`); err != nil {
+		t.Fatal(err)
+	}
+
+	modifySchema = func() {
+		modifySchema = nil
+		if _, err := sqlDB.Exec(`CREATE INDEX foo ON d.kv (v);`); err != nil {
+			t.Error(err)
+		}
+	}
+	retries = 0
+	if _, err := sqlDB.Exec(`INSERT INTO d.kv (k,v) VALUES ('a', 'b');`); err != nil {
+		t.Fatal(err)
+	}
+
+	if retries != 2 {
+		t.Fatalf("%d retries", retries)
+	}
+
+	// Test that a command that can't be retried automatically generates an error
+	// with the correct code.
+	modifySchema = func() {
+		modifySchema = nil
+		if _, err := sqlDB.Exec(`CREATE INDEX bar ON d.kv (v);`); err != nil {
+			t.Error(err)
+		}
+	}
+	retries = 0
+
+	for i := 0; i < 2; i++ {
+		tx, err := sqlDB.Begin()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = tx.Exec(`INSERT INTO d.kv (k,v) VALUES ('c', 'd');`)
+
+		if i == 0 {
+			// TODO(vivek): use isRetryableErr once we assign a Txn ID
+			// at the start of a transaction instead of in the TxnCoordinator.
+			if !testutils.IsError(err, "retryable error from another txn") {
+				t.Fatalf("err = %v", err)
+			}
+
+			if err := tx.Rollback(); err != nil {
+				t.Fatal(err)
+			}
+			continue
+		}
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err := tx.Commit(); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	if retries != 2 {
+		t.Fatalf("%d retries", retries)
 	}
 }
