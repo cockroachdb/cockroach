69d0a9641ad8a0cddde9b330e7aace499564cd56
Justin Jaffray <justin@cockroachlabs.com>
2017-04-25 15:06:40 -0400

    sql: introduce MakeTestingEvalContext
    Up until now, testing EvalContexts have been created ad-hoc and inline.
    With the introduction of more memory tracking and the inclusion of a
    MemoryMonitor in EvalContext, it's become more important to make sure
    that an EvalContext for testing is properly initialized.

--- pkg/sql/distsqlrun/hashjoiner_test.go
+++ pkg/sql/distsqlrun/hashjoiner_test.go
@@ -493,7 +493,9 @@ func TestHashJoiner(t *testing.T) {
 			leftInput := NewRowBuffer(nil /* types */, c.inputs[0], RowBufferArgs{})
 			rightInput := NewRowBuffer(nil /* types */, c.inputs[1], RowBufferArgs{})
 			out := &RowBuffer{}
-			flowCtx := FlowCtx{evalCtx: parser.EvalContext{Mon: &monitor}}
+			evalCtx := parser.MakeTestingEvalContext()
+			defer evalCtx.Stop(context.Background())
+			flowCtx := FlowCtx{evalCtx: evalCtx}
 
 			post := PostProcessSpec{Projection: true, OutputColumns: c.outCols}
 			h, err := newHashJoiner(&flowCtx, &hs, leftInput, rightInput, &post, out)
@@ -599,9 +601,9 @@ func TestHashJoinerDrain(t *testing.T) {
 	out := NewRowBuffer(
 		nil /* types */, nil, /* rows */
 		RowBufferArgs{AccumulateRowsWhileDraining: true})
-	monitor := mon.MakeUnlimitedMonitor(context.Background(), "test", nil, nil, math.MaxInt64)
-	defer monitor.Stop(context.Background())
-	flowCtx := FlowCtx{evalCtx: parser.EvalContext{Mon: &monitor}}
+	evalCtx := parser.MakeTestingEvalContext()
+	defer evalCtx.Stop(context.Background())
+	flowCtx := FlowCtx{evalCtx: evalCtx}
 
 	post := PostProcessSpec{Projection: true, OutputColumns: outCols}
 	h, err := newHashJoiner(&flowCtx, &spec, leftInput, rightInput, &post, out)
@@ -708,9 +710,9 @@ func TestHashJoinerDrainAfterBuildPhaseError(t *testing.T) {
 	out := NewRowBuffer(
 		nil /* types */, nil, /* rows */
 		RowBufferArgs{})
-	monitor := mon.MakeUnlimitedMonitor(context.Background(), "test", nil, nil, math.MaxInt64)
-	defer monitor.Stop(context.Background())
-	flowCtx := FlowCtx{evalCtx: parser.EvalContext{Mon: &monitor}}
+	evalCtx := parser.MakeTestingEvalContext()
+	defer evalCtx.Stop(context.Background())
+	flowCtx := FlowCtx{evalCtx: evalCtx}
 
 	post := PostProcessSpec{Projection: true, OutputColumns: outCols}
 	h, err := newHashJoiner(&flowCtx, &spec, leftInput, rightInput, &post, out)
