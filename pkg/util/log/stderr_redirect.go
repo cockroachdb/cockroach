f9c98a55352f809ffd8b78372cb43fa5bc943da9
David Taylor <tinystatemachine@gmail.com>
2017-04-25 22:01:15 +0000

    log: add crash reporting to panic handler

--- pkg/util/log/stderr_redirect.go
+++ pkg/util/log/stderr_redirect.go
@@ -15,11 +15,7 @@
 package log
 
 import (
-	"fmt"
 	"os"
-	"runtime/debug"
-
-	"golang.org/x/net/context"
 )
 
 // OrigStderr points to the original stderr stream.
@@ -49,34 +45,4 @@ func hijackStderr(f *os.File) error {
 func restoreStderr() error {
 	stderrRedirected = false
 	return redirectStderr(OrigStderr)
-}
-
-// RecoverAndReportPanic can be invoked on goroutines that run with
-// stderr redirected to logs to ensure the user gets informed on the
-// real stderr a panic has occurred.
-func RecoverAndReportPanic(ctx context.Context) {
-	if r := recover(); r != nil {
-		ReportPanic(ctx, r)
-		panic(r)
-	}
-}
-
-// ReportPanic reports a panic has occurred on the real stderr.
-func ReportPanic(ctx context.Context, r interface{}) {
-	Shout(ctx, Severity_ERROR, "a panic has occurred!")
-
-	// Ensure that the logs are flushed before letting a panic
-	// terminate the server.
-	Flush()
-
-	// We do not use Shout() to print the panic details here, because
-	// if stderr is not redirected (e.g. when logging to file is
-	// disabled) Shout() would copy its argument to stderr
-	// unconditionally, and we don't want that: Go's runtime system
-	// already unconditonally copies the panic details to stderr.
-	// Instead, we copy manually the details to stderr, only when stderr
-	// is redirected to a file otherwise.
-	if stderrRedirected {
-		fmt.Fprintf(OrigStderr, "%v\n\n%s\n", r, debug.Stack())
-	}
 }
