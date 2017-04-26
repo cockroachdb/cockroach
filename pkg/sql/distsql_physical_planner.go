40d1af0b05fcc924ff193f2aff743cf61394b75f
Radu Berinde <radu@cockroachlabs.com>
2017-04-25 23:22:32 -0400

    distsql: use AddProjection when planning table readers
    AddProjection already implements the logic of adding columns that appear in the
    merge ordering. Use that instead of duplicating this logic when creating a plan
    for tablereaders.

--- pkg/sql/distsql_physical_planner.go
+++ pkg/sql/distsql_physical_planner.go
@@ -591,29 +591,31 @@ func (dsp *distSQLPlanner) createTableReaders(
 		p.ResultRouters = append(p.ResultRouters, pIdx)
 	}
 
-	if overrideResultColumns != nil {
-		post.OutputColumns = overrideResultColumns
-	} else {
-		post.OutputColumns = getOutputColumnsFromScanNode(n)
+	planToStreamColMap := make([]int, len(n.resultColumns))
+	for i := range planToStreamColMap {
+		planToStreamColMap[i] = i
 	}
 
-	planToStreamColMap := makePlanToStreamColMap(len(n.resultColumns))
-	for i, col := range post.OutputColumns {
-		planToStreamColMap[col] = i
-	}
 	if len(p.ResultRouters) > 1 && len(n.ordering.ordering) > 0 {
 		// We have to maintain a certain ordering between the parallel streams. This
 		// might mean we need to add output columns.
-		for _, col := range n.ordering.ordering {
-			if planToStreamColMap[col.ColIdx] == -1 {
-				// This column is not part of the output; add it.
-				planToStreamColMap[col.ColIdx] = len(post.OutputColumns)
-				post.OutputColumns = append(post.OutputColumns, uint32(col.ColIdx))
-			}
-		}
 		p.SetMergeOrdering(dsp.convertOrdering(n.ordering.ordering, planToStreamColMap))
 	}
 	p.SetLastStagePost(post, getTypesForPlanResult(n, planToStreamColMap))
+
+	outCols := overrideResultColumns
+	if outCols == nil {
+		outCols = getOutputColumnsFromScanNode(n)
+	}
+	p.AddProjection(outCols)
+
+	post = p.GetLastStagePost()
+	for i := range planToStreamColMap {
+		planToStreamColMap[i] = -1
+	}
+	for i, col := range post.OutputColumns {
+		planToStreamColMap[col] = i
+	}
 	p.planToStreamColMap = planToStreamColMap
 	return p, nil
 }
