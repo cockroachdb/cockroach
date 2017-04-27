02f31cddf0621e33d4e6a6dabcaf9fc904effc7d
Alex Robinson <alexdwanerobinson@gmail.com>
2017-04-26 21:42:46 -0400

    storage: Add settings for allocator environment variables
    In the process, remove the now-unused EnvOrDefaultFloat.

--- pkg/util/envutil/env.go
+++ pkg/util/envutil/env.go
@@ -202,19 +202,6 @@ func EnvOrDefaultInt64(name string, value int64) int64 {
 	return value
 }
 
-// EnvOrDefaultFloat returns the value set by the specified environment
-// variable, if any, otherwise the specified default value.
-func EnvOrDefaultFloat(name string, value float64) float64 {
-	if str, present := getEnv(name, 1); present {
-		v, err := strconv.ParseFloat(str, 64)
-		if err != nil {
-			panic(fmt.Sprintf("error parsing %s: %s", name, err))
-		}
-		return v
-	}
-	return value
-}
-
 // EnvOrDefaultBytes returns the value set by the specified environment
 // variable, if any, otherwise the specified default value.
 func EnvOrDefaultBytes(name string, value int64) int64 {
