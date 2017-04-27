4163dac708265cc2cb2289424b079315eaaa6f96
Daniel Harrison <daniel.harrison@gmail.com>
2017-04-27 15:37:07 -0400

    storageccl: rate limit the WriteBatch command
    Many of our RESTORE stability problems seem to be starting with
    overloaded disks, which caused contention in RocksDB, which slowed down
    heartbeats, which caused mass lease transfers. WriteBatch and the
    resuting write amplification is the largest part of this, so add a rate
    limit directly for that.
    
    The WriteBatch limiting also lets us be less conservative about the
    client-side Import request limiting in Restore.
    
    For #15341.

--- pkg/ccl/storageccl/import.go
+++ pkg/ccl/storageccl/import.go
@@ -26,14 +26,19 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/tracing"
 )
 
-// importRequestLimit is the number of Import requests that can run at once.
-// Each downloads a file from cloud storage to a temp file, iterates it, and
-// sends WriteBatch requests to batch insert it. After accounting for write
-// amplification, a single ImportRequest and the resulting WriteBatch
-// requests is enough to keep an SSD busy. Any more and we risk contending
-// RocksDB, which slows down heartbeats, which can cause mass lease
-// transfers.
-const importRequestLimit = 1
+const (
+	// importRequestLimit is the number of Import requests that can run at once.
+	// Each downloads a file from cloud storage to a temp file, iterates it, and
+	// sends WriteBatch requests to batch insert it. After accounting for write
+	// amplification, a single ImportRequest and the resulting WriteBatch
+	// requests is enough to keep an SSD busy. Any more and we risk contending
+	// RocksDB, which slows down heartbeats, which can cause mass lease
+	// transfers.
+	importRequestLimit = 1
+
+	// Arrived at by tuning and watching the effect on BenchmarkRestore.
+	importBatchSizeBytes = 1000000
+)
 
 var importRequestLimiter = makeConcurrentRequestLimiter(importRequestLimit)
 
@@ -68,9 +73,6 @@ func evalImport(ctx context.Context, cArgs storage.CommandArgs) (*roachpb.Import
 	}
 	defer importRequestLimiter.endLimitedRequest()
 	log.Infof(ctx, "import [%s,%s)", importStart, importEnd)
-
-	// Arrived at by tuning and watching the effect on BenchmarkRestore.
-	const batchSizeBytes = 1000000
 
 	type batchBuilder struct {
 		batch         engine.RocksDBBatchBuilder
@@ -219,7 +221,7 @@ func evalImport(ctx context.Context, cArgs storage.CommandArgs) (*roachpb.Import
 			b.batchEndKey = append(b.batchEndKey[:0], key.Key...)
 		}
 
-		if b.batch.Len() > batchSizeBytes {
+		if b.batch.Len() > importBatchSizeBytes {
 			sendWriteBatch()
 		}
 	}
