e6dd4b5c013df9b156fb15277e903cfcb9e5c6ee
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-04-27 14:22:10 +0000

    acceptance/cli: improve the expect tests
    This patch does two things of note:
    
    1. it ensures that `cockroach.log` is also dumped in the test logs in
       case of failures. Prior to this patch, this information was simply
       lost, making issues difficult to troubleshoot.
    
    2. it also creates a log file of the test's progress, and delinates
       the test actions using log messages that go both to this log file
       and the standard output. The log file is also dumped in case of
       failures.
    
    With this patch in, we will get more useful output out of `expect`.
    
    Also, this patch makes it more useful and readable to run `expect -f
    ...` instead of `expect -d -f ...` as was previously advised.

--- pkg/cli/interactive_tests/common.tcl
+++ pkg/cli/interactive_tests/common.tcl
@@ -18,21 +18,51 @@ set timeout 30
 # and this confuses readline. Ensure sane defaults here.
 set stty_init "cols 80 rows 25"
 
+# Convenience function to tag what's going on in log files.
+proc report {text} {
+    system "echo; echo `date '+.%y%m%d %H:%M:%S.%N'` EXPECT TEST: '$text' | tee -a cmd.log"
+}
+
+# Upon termination
+proc report_log_files {} {
+    system "(echo '==COMMAND OUTPUT=='; cat cmd.log; echo '==DB LOG=='; cat cockroach-data/logs/cockroach.log; echo '==END==') || true"
+}
+
+# Catch signals
+proc mysig {} {
+    report "EXPECT KILLED BY SIGNAL"
+    report_log_files
+    exit 130
+}
+trap mysig SIGINT
+trap mysig SIGTERM
+
+# Convenience functions to tag a test
+proc start_test {text} {
+    report "START TEST: $text"
+}
+proc end_test {} {
+    report "END TEST"
+}
+
 # Convenience wrapper function, which ensures that all expects are
 # mandatory (i.e. with a mandatory fail if the expected output doesn't
 # show up fast).
+proc handle_timeout {text} {
+    report "TIMEOUT WAITING FOR \"$text\""
+    report_log_files
+    exit 1
+}
 proc eexpect {text} {
     expect {
 	$text {}
-	timeout {
-	    system "echo SERVER STDOUT; cat stdout.log; echo SERVER STDERR; cat stderr.log"
-	    exit 1
-	}
+	timeout { handle_timeout $text }
     }
 }
 
 # Convenience function that sends Ctrl+C to the monitored process.
 proc interrupt {} {
+    report "INTERRUPT TO FOREGROUND PROCESS"
     send "\003"
     sleep 0.4
 }
@@ -41,8 +71,12 @@ proc interrupt {} {
 # Preserves the invariant that the server's PID is saved
 # in `server_pid`.
 proc start_server {argv} {
-    system "mkfifo pid_fifo || true; $argv start --insecure --pid-file=pid_fifo --background >>stdout.log 2>>stderr.log & cat pid_fifo > server_pid"
+    report "BEGIN START SERVER"
+    system "mkfifo pid_fifo || true; $argv start --insecure --pid-file=pid_fifo --background >>cmd.log 2>&1 & cat pid_fifo > server_pid"
+    report "START SERVER DONE"
 }
 proc stop_server {argv} {
+    report "BEGIN STOP SERVER"
     system "set -e; if kill -CONT `cat server_pid`; then $argv quit || true & sleep 1; kill -9 `cat server_pid` || true; else $argv quit || true; fi"
+    report "END STOP SERVER"
 }
--- pkg/cli/interactive_tests/test_example_data.tcl
+++ pkg/cli/interactive_tests/test_example_data.tcl
@@ -8,31 +8,35 @@ spawn /bin/bash
 send "PS1=':''/# '\r"
 eexpect ":/# "
 
-# Check that the startrek example can be loaded in the db.
+start_test "Check that the startrek example can be loaded in the db."
 send "$argv gen example-data startrek | $argv sql\r"
 eexpect ":/# "
 send "echo \$?\r"
 eexpect "0\r\n:/# "
+end_test
 
-# Check that the startrek example is loaded.
+start_test "Check that the startrek example is loaded."
 send "$argv sql -e 'SELECT COUNT(*) FROM startrek.quotes'\r"
 eexpect "COUNT"
 eexpect "200"
 eexpect "1 row"
 eexpect ":/# "
+end_test
 
-# Check that the intro example can be loaded in the db.
+start_test "Check that the intro example can be loaded in the db."
 send "$argv gen example-data intro | $argv sql\r"
 eexpect ":/# "
 send "echo \$?\r"
 eexpect "0\r\n:/# "
+end_test
 
-# Check that the startrek example is loaded.
+start_test "Check that the startrek example is loaded."
 send "$argv sql -e 'SELECT COUNT(*) FROM intro.mytable'\r"
 eexpect "COUNT"
 eexpect "42"
 eexpect "1 row"
 eexpect ":/# "
+end_test
 
 # Clean up.
 send "exit 0\r"
