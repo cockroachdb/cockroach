2cde6604b9612d27521130de5b0b0bdb542246ba
Arjun Narayan <arjunravinarayan@gmail.com>
2017-04-27 23:16:27 -0400

    sql: add mutex to settings worker.
    This fixes a race in tests when multiple workers were changing a
    global setting.

--- pkg/server/settingsworker.go
+++ pkg/server/settingsworker.go
@@ -29,6 +29,8 @@ import (
 	"github.com/pkg/errors"
 )
 
+var settingsWorkerMu = make(chan struct{}, 1)
+
 // RefreshSettings starts a settings-changes listener.
 func (s *Server) refreshSettings() {
 	tbl := sqlbase.SettingsTable
@@ -106,6 +108,17 @@ func (s *Server) refreshSettings() {
 
 	ctx := s.AnnotateCtx(context.Background())
 	s.stopper.RunWorker(ctx, func(ctx context.Context) {
+		// The setting values are global and updating them is a global process.
+		// If we have multiple testservers or testcluster at the same time, let
+		// only one of them populate the values.
+		select {
+		case settingsWorkerMu <- struct{}{}:
+			break
+		case <-s.stopper.ShouldStop():
+			return
+		}
+		defer func() { <-settingsWorkerMu }()
+
 		gossipUpdateC := s.gossip.RegisterSystemConfigChannel()
 		// No new settings can be defined beyond this point.
 		settings.Freeze()
