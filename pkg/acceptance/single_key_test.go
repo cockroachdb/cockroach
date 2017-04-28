37e8db4ca378660a7d475d9c1141b050ec11be39
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-04-28 20:35:22 +0000

    acceptance: skip flaky TestSingleKey

--- pkg/acceptance/single_key_test.go
+++ pkg/acceptance/single_key_test.go
@@ -35,6 +35,8 @@ import (
 // up an N node cluster and running N workers that are all
 // incrementing the value associated with a single key.
 func TestSingleKey(t *testing.T) {
+	t.Skip("#15472")
+
 	s := log.Scope(t)
 	defer s.Close(t)
 
