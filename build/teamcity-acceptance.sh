6668a8c7b141e6ca903b4c6a47c68c2ea6416d5d
Tamir Duberstein <tamird@gmail.com>
2017-04-28 17:40:43 -0400

    acceptance: avoid relying on GOPATH
    In particular, avoid relying on the host knowing where the container's
    $GOPATH/bin is going to end up. Instead, rely on the cwd, which is
    always consistent when running acceptance tests.
    
    Also converge TC and the local acceptance runner to avoid similar
    shenanigans in the future.

--- build/teamcity-acceptance.sh
+++ build/teamcity-acceptance.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env bash
+
 set -euxo pipefail
 
 export BUILDER_HIDE_GOPATH_SRC=1
 
-# Ensure that no stale binary remains.
-rm -f pkg/acceptance/acceptance.test
+"$(dirname "${0}")"/../pkg/acceptance/prepare.sh
 
-build/builder.sh make TYPE=release-linux-gnu build
-mv cockroach-linux-2.6.32-gnu-amd64 cockroach
 build/builder.sh make TYPE=release-linux-gnu testbuild TAGS=acceptance PKG=./pkg/acceptance
-
-# The log files that should be created by -l below can only
-# be created if the parent directory already exists. Ensure
-# that it exists before running the test.
-mkdir -p artifacts/acceptance
-export TMPDIR=$PWD/artifacts/acceptance
-
 cd pkg/acceptance
 ./acceptance.test -nodes 3 -l "$TMPDIR" -test.v -test.timeout 10m 2>&1 | tee "$TMPDIR/acceptance.log" | go-test-teamcity
