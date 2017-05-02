5f5cd32fac673be8c8751035bea8090463183e09
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-05-02 17:33:43 -0400

    build: add TeamCity script to verify archive

---
+++ build/teamcity-verify-archive.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+build/builder.sh make archive ARCHIVE=build/cockroach.src.tgz
+
+# We use test the source archive in a minimal image; the builder image bundles
+# too much developer configuration to simulate a build on a fresh user machine.
+docker run \
+  --volume="$(cd "$(dirname "$0")" && pwd):/work" \
+  --workdir="/work" \
+  golang:1.8.1 ./verify-archive.sh
