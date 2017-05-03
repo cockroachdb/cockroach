549631bf732a31772aa20a6ea03d0a43dc7fc986
Andrei Matei <andrei@cockroachlabs.com>
2017-05-02 23:22:41 -0400

    sql, distsql: marshall EvalContext members through distsql requests
    Before this patch, EvalContext data was not marshalled through flow
    request, and distsql was rejecting builtin functions that needed it.
    Flows used for schema change backfills were not subject to that
    rejection so they were outright using an empty EvalContext (the column
    backfiller had partial initilization for some timestamps).
    
    This patch marshall this data and removes the distsql check against
    builtins that use it. For regular queries this means that more queries
    are supported (e.g. queries using timestamp casts, date and interval
    arithmetic, now() and various others).
    
    For schema changes, this means that a DEFAULT value of unique_rowid()
    should work correctly (not sure what it did before, but nothing good in
    the absence of the node identifier). As before, the evaluation of
    expressions involved in backfills, at least for expressions that need to
    be evaluated for each row, uses the UTC timezone; now it does this
    explicitly, before it happened because the respective field was not set.
    The Database field is empty for backfills and SearchPath is set to
    "pg_catalog".
    The timestamps returned by now() and friends during backfills are the
    same for all rows processed by one attempt to perform the backfill (so
    one node that dequeues the respective schemaChanger). Before, new
    timestamps were generated once per "chunk" performed by a backfill
    processor.
    
    Some new functions for dealing with the session's time.Location are
    needed to correctly marshall it regardless of whether it was initialized
    from a system-defined tz (e.g. `SET TIME ZONE EST`) or an offset from
    GMT (SET TIME ZONE -1).
    
    I've incremented the minimum accepted distsql version because the
    backfills now need the planner to have populated the timestamps and
    also because we need queries from new gateways (which haven't checked
    the old "contextDependent" to be rejected by old nodes).

--- pkg/sql/session.go
+++ pkg/sql/session.go
@@ -297,7 +297,7 @@ func NewSession(
 	s := &Session{
 		Database:         args.Database,
 		DistSQLMode:      distSQLMode,
-		SearchPath:       parser.SearchPath{"pg_catalog"},
+		SearchPath:       sqlbase.DefaultSearchPath,
 		Location:         time.UTC,
 		User:             args.User,
 		virtualSchemas:   e.virtualSchemas,
@@ -800,7 +800,8 @@ func (scc *schemaChangerCollection) execSchemaChanges(
 		sc.testingKnobs = e.cfg.SchemaChangerTestingKnobs
 		sc.distSQLPlanner = e.distSQLPlanner
 		for r := retry.Start(base.DefaultRetryOptions()); r.Next(); {
-			if err := sc.exec(ctx); err != nil {
+			evalCtx := createSchemaChangeEvalCtx(e.cfg.Clock.Now())
+			if err := sc.exec(ctx, evalCtx); err != nil {
 				if err != errExistingSchemaChangeLease {
 					log.Warningf(ctx, "Error executing schema change: %s", err)
 				}
