674655aa7bdbcd1630a0ba7eff9a81473f8e42bf
David Eisenstat <eisen@cockroachlabs.com>
2017-05-04 11:15:21 -0400

    1.0.1 cherrypick: sql: allow CREATE USER WITH PASSWORD from CLI transaction
    Cherrypick #15693 into 1.0.1.
    
    The CLI parses and serializes the CREATE USER statement before sending
    it to the server, which previously caused the password string to be
    redacted by stars.
    
    This commit is part of a push to ensure that Serialize is a right
    inverse for Parse.

--- pkg/cli/interactive_tests/test_secure.tcl
+++ pkg/cli/interactive_tests/test_secure.tcl
@@ -58,6 +58,42 @@ send "woof\r"
 eexpect "Confirm password:"
 send "woof\r"
 eexpect "carl@"
+send "\\q\r"
+eexpect $prompt
+end_test
+
+start_test "Check that CREATE USER WITH PASSWORD can be used from transactions."
+# Create a user from a transaction.
+send "$argv sql --certs-dir=$certs_dir\r"
+eexpect "root@"
+send "BEGIN TRANSACTION;\r"
+eexpect " ->"
+send "CREATE USER eisen WITH PASSWORD 'hunter2';\r"
+eexpect " ->"
+send "COMMIT TRANSACTION;\r"
+eexpect "root@"
+send "\\q\r"
+# Log in with the correct password.
+eexpect $prompt
+send "$argv sql --certs-dir=$certs_dir --user=eisen\r"
+eexpect "Enter password:"
+send "hunter2\r"
+eexpect "eisen@"
+send "\\q\r"
+# Try to log in with an incorrect password.
+eexpect $prompt
+send "$argv sql --certs-dir=$certs_dir --user=eisen\r"
+eexpect "Enter password:"
+send "*****\r"
+eexpect "Error: pq: invalid password"
+eexpect "Failed running \"sql\""
+# Check that history is scrubbed.
+send "$argv sql --certs-dir=$certs_dir\r"
+eexpect "root@"
+# Ctrl+R eisen
+send "\022eisen"
+eexpect "CREATE USER eisen WITH PASSWORD \\*\\*\\*\\*\\*;"
+interrupt
 end_test
 
 # Terminate with Ctrl+C.
