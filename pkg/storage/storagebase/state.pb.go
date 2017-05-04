637e0303a1b75b9100fdcb5443d6e9255bb42eb4
Andrei Matei <andrei@cockroachlabs.com>
2017-05-04 19:18:34 -0400

    storage: clarify that various leases being passed around can't be nil
    This was motivated primarily by the need to get rid of a reference to
    raftCmd.BatchRequest in replica.propose (raftCmd.BatchRequest is a field
    that only exists for testing now). Besides, this patch removes FUD and
    takes care of some TODOs.
    
    I've marked the lease as non-nilable in some protos, but otherwise have
    continued passing leases through pointers inside a node. I've added
    comments and removed unnecessary nil guards. Before writing this code,
    I've sprinkled panics in the various guards that I've removed; nothing
    fired in tests.

--- pkg/storage/storagebase/state.pb.go
+++ pkg/storage/storagebase/state.pb.go
@@ -43,7 +43,7 @@ type ReplicaState struct {
 	// Changes of the descriptor should always go through one of the
 	// (*Replica).setDesc* methods.
 	Desc *cockroach_roachpb.RangeDescriptor `protobuf:"bytes,3,opt,name=desc" json:"desc,omitempty"`
-	// The latest range lease, if any.
+	// The latest range lease. This field is always populated.
 	Lease *cockroach_roachpb1.Lease `protobuf:"bytes,4,opt,name=lease" json:"lease,omitempty"`
 	// The truncation state of the Raft log.
 	TruncatedState *cockroach_roachpb4.RaftTruncatedState `protobuf:"bytes,5,opt,name=truncated_state,json=truncatedState" json:"truncated_state,omitempty"`
