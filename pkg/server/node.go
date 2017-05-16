9248c94a90ecd79d50b1d23223a4152fc6292ea2
Andrei Matei <andrei@cockroachlabs.com>
2017-05-16 15:49:10 -0400

    server: retry store id allocation on the correct error
    Fixes #15870
    
    The code was retrying on an error only meant for transactional requests,
    even though it was performing non-transactional requests.

--- pkg/server/node.go
+++ pkg/server/node.go
@@ -175,7 +175,7 @@ func incVal(ctx context.Context, db *client.DB, key roachpb.Key, inc int64) (int
 	for r := retry.Start(base.DefaultRetryOptions()); r.Next(); {
 		res, err = db.Inc(ctx, key, inc)
 		switch err.(type) {
-		case *roachpb.HandledRetryableTxnError, *roachpb.AmbiguousResultError:
+		case *roachpb.UnhandledRetryableError, *roachpb.AmbiguousResultError:
 			continue
 		}
 		break
