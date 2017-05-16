99c0a75e15b8395703df44f1a24d9e8763140806
Radu Berinde <radu@cockroachlabs.com>
2017-05-16 10:51:50 -0400

    sql: move constant detection from sortNode to renderNode
    Move the one-off constant detection code in sortNode to
    renderNode.computeOrdering, where it is more generally useful. The sortNode can
    use this information to avoid sorting just like before.
    
    Fixes #13476.

--- pkg/sql/expand_plan.go
+++ pkg/sql/expand_plan.go
@@ -346,8 +346,7 @@ func expandRenderNode(
 		}
 	}
 
-	r.ordering = r.computeOrdering(r.source.plan.Ordering())
-
+	r.computeOrdering(r.source.plan.Ordering())
 	return r, nil
 }
 
@@ -527,7 +526,7 @@ func simplifyOrderings(plan planNode, usefulOrdering sqlbase.ColumnOrdering) pla
 		// TODO(radu): in some cases there may be multiple possible n.orderings for
 		// a given source plan ordering; we should pass usefulOrdering to help make
 		// that choice (#13709).
-		n.ordering = n.computeOrdering(n.source.plan.Ordering())
+		n.computeOrdering(n.source.plan.Ordering())
 
 	case *delayedNode:
 		n.plan = simplifyOrderings(n.plan, usefulOrdering)
