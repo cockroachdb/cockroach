edfc6b890f6d394ee86679aaaedcb4a7c403e4f7
Matt Tracy <matt@cockroachlabs.com>
2017-06-07 18:48:46 -0400

    ui: Refactor nodeGraphs page into multiple files
    Splits the very large "nodeGraphs" component, which contained all graphs
    on the cluster page, into multiple files - one for each "dashboard"
    selectable on the cluster page. This should make it easier to find or
    modify the graphs on individual dashboards; it should also serve as an
    example of how to break up a complicated component into multiple files,
    while still maintaining an approachable "public" interface in index.tsx.
    
    + Dashboards are defined in files under
    /views/cluster/containers/nodeGraphs/dashboards. These are imported and
    used in nodeGraph's index.tsx file.
    + Brought the formatting for the JSX of our graphs up to date with our
    upcoming style guide (not currently enforced by linters)
    + Dashboard labels are defined only in one place now; previously, there
    were several collections where string values needed to be kept in sync.
    + Removed the "GraphGroup" component which is no longer needed.
    + Removed the "Nodes" component, which was an unneeded wrapper that
    provided the dropdown options to the node graphs page. These options are
    now displayed directly by the graphs page.

---
+++ pkg/ui/src/views/cluster/containers/nodeGraphs/dashboards/overview.tsx
@@ -0,0 +1,118 @@
+import React from "react";
+import _ from "lodash";
+
+import { LineGraph } from "src/views/cluster/components/linegraph";
+import { Metric, Axis, AxisUnits } from "src/views/shared/components/metricQuery";
+
+import { GraphDashboardProps, nodeAddress, storeIDsForNode } from "./dashboardUtils";
+
+export default function (props: GraphDashboardProps) {
+  const { nodeIDs, nodesSummary, nodeSources, storeSources, tooltipSelection } = props;
+
+  return [
+    <LineGraph
+      title="SQL Queries"
+      sources={nodeSources}
+      tooltip={
+        `The average number of SELECT, INSERT, UPDATE,
+           and DELETE statements per second ${tooltipSelection}.`
+      }
+    >
+      <Axis>
+        <Metric name="cr.node.sql.select.count" title="Total Reads" nonNegativeRate />
+        <Metric name="cr.node.sql.distsql.select.count" title="DistSQL Reads" nonNegativeRate />
+        <Metric name="cr.node.sql.update.count" title="Updates" nonNegativeRate />
+        <Metric name="cr.node.sql.insert.count" title="Inserts" nonNegativeRate />
+        <Metric name="cr.node.sql.delete.count" title="Deletes" nonNegativeRate />
+      </Axis>
+    </LineGraph>,
+
+    <LineGraph
+      title="Service Latency: SQL, 99th percentile"
+      tooltip={(
+        <div>
+          Over the last minute, this node executed 99% of queries within this time.&nbsp;
+            <em>This time does not include network latency between the node and client.</em>
+        </div>
+      )}
+    >
+      <Axis units={AxisUnits.Duration}>
+        {
+          _.map(nodeIDs, (node) => (
+            <Metric
+              key={node}
+              name="cr.node.sql.service.latency-p99"
+              title={nodeAddress(nodesSummary, node)}
+              sources={[node]}
+              downsampleMax
+            />
+          ))
+        }
+      </Axis>
+    </LineGraph>,
+
+    <LineGraph
+      title="Replicas per Node"
+      tooltip={(
+        <div>
+          The number of range replicas stored on this node.&nbsp;
+            <em>Ranges are subsets of your data, which are replicated to ensure survivability.</em>
+        </div>
+      )}
+    >
+      <Axis>
+        {
+          _.map(nodeIDs, (nid) => (
+            <Metric
+              key={nid}
+              name="cr.store.replicas"
+              title={nodeAddress(nodesSummary, nid)}
+              sources={storeIDsForNode(nodesSummary, nid)}
+            />
+          ))
+        }
+      </Axis>
+    </LineGraph>,
+
+    <LineGraph
+      title="Capacity"
+      sources={storeSources}
+      tooltip={(
+        <div>
+          <dl>
+            <dt>Capacity</dt>
+            <dd>
+              Total disk space available {tooltipSelection} to CockroachDB.
+                <em>
+                Control this value per node with the
+                  <code>
+                  <a
+                    href="https://www.cockroachlabs.com/docs/start-a-node.html#flags"
+                    target="_blank"
+                  >
+                    --store
+                    </a>
+                </code>
+                flag.
+                </em>
+            </dd>
+            <dt>Available</dt>
+            <dd>Free disk space available {tooltipSelection} to CockroachDB.</dd>
+          </dl>
+        </div>
+      )}
+    >
+      <Axis units={AxisUnits.Bytes}>
+        <Metric name="cr.store.capacity" title="Capacity" />
+        {
+          // TODO(mrtracy): We really want to display a used capacity
+          // stat, but that is not directly recorded. We either need to
+          // start directly recording it, or add the ability to create
+          // derived series.
+        }
+        <Metric name="cr.store.capacity.available" title="Available" />
+      </Axis>
+    </LineGraph>,
+
+  ];
+}
---
+++ pkg/ui/src/views/cluster/containers/nodeGraphs/dashboards/runtime.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+
+import { LineGraph } from "src/views/cluster/components/linegraph";
+import { Metric, Axis, AxisUnits } from "src/views/shared/components/metricQuery";
+
+import { GraphDashboardProps } from "./dashboardUtils";
+
+export default function (props: GraphDashboardProps) {
+  const { nodeSources, tooltipSelection } = props;
+
+  return [
+    <LineGraph title="Node Count" tooltip="The number of nodes active on the cluster.">
+      <Axis>
+        <Metric name="cr.node.liveness.livenodes" title="Live Nodes" aggregateMax />
+      </Axis>
+    </LineGraph>,
+
+    <LineGraph
+      title="Memory Usage"
+      sources={nodeSources}
+      tooltip={(
+        <div>
+          {`Memory in use ${tooltipSelection}:`}
+          <dl>
+            <dt>RSS</dt>
+            <dd>Total memory in use by CockroachDB</dd>
+            <dt>Go Allocated</dt>
+            <dd>Memory allocated by the Go layer</dd>
+            <dt>Go Total</dt>
+            <dd>Total memory managed by the Go layer</dd>
+            <dt>C Allocated</dt>
+            <dd>Memory allocated by the C layer</dd>
+            <dt>C Total</dt>
+            <dd>Total memory managed by the C layer</dd>
+          </dl>
+        </div>
+      )}
+    >
+      <Axis units={AxisUnits.Bytes}>
+        <Metric name="cr.node.sys.rss" title="Total memory (RSS)" />
+        <Metric name="cr.node.sys.go.allocbytes" title="Go Allocated" />
+        <Metric name="cr.node.sys.go.totalbytes" title="Go Total" />
+        <Metric name="cr.node.sys.cgo.allocbytes" title="CGo Allocated" />
+        <Metric name="cr.node.sys.cgo.totalbytes" title="CGo Total" />
+      </Axis>
+    </LineGraph>,
+
+    <LineGraph
+      title="Goroutine Count"
+      sources={nodeSources}
+      tooltip={
+        `The number of Goroutines ${tooltipSelection}.
+           This count should rise and fall based on load.`
+      }
+    >
+      <Axis>
+        <Metric name="cr.node.sys.goroutines" title="Goroutine Count" />
+      </Axis>
+    </LineGraph>,
+
+    // TODO(mrtracy): The following two graphs are a good first example of a graph with
+    // two axes; the two series should be highly correlated, but have different units.
+    <LineGraph
+      title="GC Runs"
+      sources={nodeSources}
+      tooltip={
+        `The number of times that Go’s garbage collector was invoked per second ${tooltipSelection}.`
+      }
+    >
+      <Axis>
+        <Metric name="cr.node.sys.gc.count" title="GC Runs" nonNegativeRate />
+      </Axis>
+    </LineGraph>,
+
+    <LineGraph
+      title="GC Pause Time"
+      sources={nodeSources}
+      tooltip={
+        `The amount of processor time used by Go’s garbage collector
+           per second ${tooltipSelection}.
+           During garbage collection, application code execution is paused.`
+      }
+    >
+      <Axis units={AxisUnits.Duration}>
+        <Metric name="cr.node.sys.gc.pause.ns" title="GC Pause Time" nonNegativeRate />
+      </Axis>
+    </LineGraph>,
+
+    <LineGraph
+      title="CPU Time"
+      sources={nodeSources}
+      tooltip={
+        `The amount of CPU time used by CockroachDB (User)
+           and system-level operations (Sys) ${tooltipSelection}.`
+      }
+    >
+      <Axis units={AxisUnits.Duration}>
+        <Metric name="cr.node.sys.cpu.user.ns" title="User CPU Time" nonNegativeRate />
+        <Metric name="cr.node.sys.cpu.sys.ns" title="Sys CPU Time" nonNegativeRate />
+        <Metric name="cr.node.sys.gc.pause.ns" title="GC Pause Time" nonNegativeRate />
+      </Axis>
+    </LineGraph>,
+  ];
+}
