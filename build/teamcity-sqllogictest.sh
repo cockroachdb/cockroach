e7b9ece025900dbed685e2bf93fa47f17f88488e
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-06-12 10:02:17 +0000

    sql: make teamcity-logictest.sh use the new package location

--- build/teamcity-sqllogictest.sh
+++ build/teamcity-sqllogictest.sh
@@ -11,7 +11,7 @@ export BUILDER_HIDE_GOPATH_SRC=0
 
 for config in default distsql; do
     build/builder.sh env \
-        make test TESTFLAGS="-v -bigtest -config ${config}" TESTTIMEOUT='24h' PKG='./pkg/sql' TESTS='^TestLogic$$' 2>&1 \
+        make test TESTFLAGS="-v -bigtest -config ${config}" TESTTIMEOUT='24h' PKG='./pkg/sql/logictest' TESTS='^TestLogic$$' 2>&1 \
         | tee "artifacts/${config}.log" \
         | go-test-teamcity
 done
