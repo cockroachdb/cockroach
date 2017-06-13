a66c184a586ba73f2bc4942b6d17210b0b2c58c9
David Taylor <tinystatemachine@gmail.com>
2017-06-13 14:33:43 +0000

    utilccl: add enterprise licence checking
    This adds a new enterprise.licence setting to manage a CCL cluster's license.
    
    Attempts to use Enterprise features will return a license error if the cluster
    does not have a currently valid license.
    
    A license can be obtained from Cockroach Labs (means TBD) and contains some
    identifying information, notably the cluster(s) to which it applies and an
    expiration, encoded as a string.
    
    Currently a license is applied via SET CLUSTER SETTING, which, via the standard
    validated setting patter, will validate that the supplied value is well-formed
    (i.e. that it actually is an encoded license). It will not however verify that
    it is currently valid for that particular cluster though. A future change that
    adds SET LICENSE and SHOW LICENSE sugar on top of the setting could provide this
    validation and hide the setting.
    
    There's no attempt made to sign, encrypt or really even obfuscate how these
    strings are encoded: given that the code is open for all to see, a user
    intending to circumvent our license check has easier technical means to do so
    already, so there's little point in making things harder here.
    
    For now, the stopgap boolean setting is also still respected but it will be
    removed after a transition period.

--- pkg/ccl/utilccl/license_check.go
+++ pkg/ccl/utilccl/license_check.go
@@ -9,9 +9,23 @@
 package utilccl
 
 import (
-	"github.com/cockroachdb/cockroach/pkg/settings"
+	"bytes"
+	"encoding/base64"
+	"strings"
+	"sync/atomic"
+	"time"
+
 	"github.com/pkg/errors"
+	"golang.org/x/net/context"
+
+	"github.com/cockroachdb/cockroach/pkg/settings"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 )
+
+// LicensePrefix is a prefix on license strings to make them easily recognized.
+const LicensePrefix = "cl-0-"
 
 // EnterpriseEnabled is temporary, until #14114 is implemented.
 var EnterpriseEnabled = func() *settings.BoolSetting {
@@ -20,20 +34,120 @@ var EnterpriseEnabled = func() *settings.BoolSetting {
 	return s
 }()
 
+var currentLicense = func() *atomic.Value {
+	setting := settings.RegisterValidatedStringSetting(
+		"enterprise.license", "the encoded cluster license", "",
+		func(s string) error {
+			_, err := decodeLicense(s)
+			return err
+		})
+
+	var ref atomic.Value
+	setting.OnChange(func() {
+		cur, err := decodeLicense(setting.Get())
+		if err != nil {
+			log.Warningf(context.Background(), "error decoding license: %v", err)
+		} else {
+			ref.Store(cur)
+		}
+	})
+	return &ref
+}()
+
+func decodeLicense(s string) (*License, error) {
+	if s == "" {
+		return nil, nil
+	}
+	if !strings.HasPrefix(s, LicensePrefix) {
+		return nil, errors.New("invalid license string")
+	}
+	s = strings.TrimPrefix(s, LicensePrefix)
+	data, err := base64.RawStdEncoding.DecodeString(s)
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid license string")
+	}
+	var lic License
+	if err := lic.Unmarshal(data); err != nil {
+		return nil, errors.Wrap(err, "invalid license string")
+	}
+	return &lic, nil
+}
+
 // CheckEnterpriseEnabled returns a non-nil error if the requested enterprise
 // feature is not enabled, including information or a link explaining how to
 // enable it.
-func CheckEnterpriseEnabled(feature string) error {
+func CheckEnterpriseEnabled(cluster uuid.UUID, feature string) error {
+	// TODO(dt): delete this after a transition period.
 	if EnterpriseEnabled.Get() {
 		return nil
 	}
+	return checkEnterpriseEnabledAt(cluster, timeutil.Now(), feature)
+}
+
+func checkEnterpriseEnabledAt(cluster uuid.UUID, at time.Time, feature string) error {
+	if licPtr := currentLicense.Load(); licPtr != nil {
+		lic := licPtr.(*License)
+		if lic != nil {
+			if err := lic.checkCluster(cluster); err != nil {
+				return err
+			}
+			return lic.checkExpiration(at)
+		}
+	}
+
 	// TODO(dt): link to some stable URL that then redirects to a helpful page
 	// that explains what to do here.
-	link := "https://cockroachlabs.com/pricing"
+	link := "https://cockroachlabs.com/pricing?cluster=%s"
 	return errors.Errorf(
 		"use of %s requires an enterprise license. "+
-			"see %s for details on how to enable enterprise features",
+			"see %s%s for details on how to enable enterprise features",
 		feature,
 		link,
+		cluster.String(),
 	)
+}
+
+func (l License) checkCluster(cluster uuid.UUID) error {
+	for _, c := range l.ClusterID {
+		if cluster == c {
+			return nil
+		}
+	}
+	// no match, so compose an error message.
+	var matches bytes.Buffer
+	for i, c := range l.ClusterID {
+		if i > 0 {
+			matches.WriteString(", ")
+		}
+		matches.WriteString(c.String())
+	}
+	return errors.Errorf(
+		"license for cluster(s) %s is not valid for cluster %s", matches.String(), cluster.String(),
+	)
+}
+
+// checkExpiration returns an error if the license is expired.
+func (l License) checkExpiration(at time.Time) error {
+	if expiration := l.ValidUntil(); at.After(expiration) {
+		// We extend some grace period to enterprise license holders rather than
+		// suddenly throwing errors at them.
+		if l.Type != License_Enterprise {
+			return errors.Errorf("license expired at %s", expiration.String())
+		}
+	}
+	return nil
+}
+
+// ValidUntil wraps the license expiration time in a time.Time.
+func (l License) ValidUntil() time.Time {
+	return time.Unix(l.ValidUntilUnixSec, 0)
+}
+
+// Encode serializes the license as a string.
+func (l License) Encode() (string, error) {
+	bytes, err := l.Marshal()
+	if err != nil {
+		return "", err
+	}
+	return LicensePrefix + base64.RawStdEncoding.EncodeToString(bytes), nil
 }
---
+++ pkg/ccl/utilccl/license_check_test.go
@@ -0,0 +1,93 @@
+// Copyright 2017 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/LICENSE
+
+package utilccl
+
+import (
+	"testing"
+	"time"
+
+	"fmt"
+
+	"github.com/cockroachdb/cockroach/pkg/settings"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/util/uuid"
+)
+
+func TestLicense(t *testing.T) {
+	clusterA, _ := uuid.FromString("A0000000-0000-0000-0000-00000000000A")
+	clusterB, _ := uuid.FromString("B0000000-0000-0000-0000-00000000000B")
+
+	epoch := time.Unix(0, 0)
+	after := epoch.Add(time.Hour * 24)
+	before := epoch.Add(time.Hour * -24)
+	wayAfter := epoch.Add(time.Hour * 24 * 365 * 200)
+
+	for i, tc := range []struct {
+		licType      License_Type
+		grantedTo    uuid.UUID
+		expiration   time.Time
+		checkCluster uuid.UUID
+		checkTime    time.Time
+		err          string
+	}{
+		{licType: -1, err: "requires an enterprise license"},
+
+		{License_Evaluation, clusterA, epoch, clusterA, epoch, ""},
+		{License_Enterprise, clusterA, epoch, clusterA, epoch, ""},
+		{License_NonCommercial, clusterA, epoch, clusterA, epoch, ""},
+
+		{License_Evaluation, clusterA, after, clusterA, epoch, ""},
+		{License_Evaluation, clusterA, epoch, clusterA, before, ""},
+		{License_Evaluation, clusterA, wayAfter, clusterA, epoch, ""},
+
+		{License_Evaluation, clusterA, epoch, clusterA, after, "expired"},
+		{License_Evaluation, clusterA, after, clusterA, wayAfter, "expired"},
+		{License_NonCommercial, clusterA, after, clusterA, wayAfter, "expired"},
+		{License_Enterprise, clusterA, after, clusterA, wayAfter, "expired"},
+
+		{licType: -1, err: "requires an enterprise license"},
+
+		{License_Enterprise, clusterA, epoch, clusterB, epoch, "not valid for cluster"},
+		{License_Enterprise, clusterB, epoch, clusterA, epoch, "not valid for cluster"},
+
+		{licType: -1, err: "requires an enterprise license"},
+	} {
+		licStr := ""
+		if tc.licType != -1 {
+			licStr, _ = License{
+				[]uuid.UUID{tc.grantedTo},
+				tc.expiration.Unix(),
+				License_Evaluation,
+				fmt.Sprintf("tc-%d", i),
+			}.Encode()
+		}
+		if err := (settings.Updater{}).Set("enterprise.license", licStr, "s"); err != nil {
+			t.Fatal(err)
+		}
+
+		err := checkEnterpriseEnabledAt(tc.checkCluster, tc.checkTime, "")
+		if !testutils.IsError(err, tc.err) {
+			t.Fatalf("%d: lic for %s to %s, checked by %s at %s.\n got %q", i,
+				tc.grantedTo.Short(), tc.expiration, tc.checkCluster.Short(), tc.checkTime, err)
+		}
+	}
+}
+
+func TestBadLicenseStrings(t *testing.T) {
+	for _, tc := range []struct{ lic, err string }{
+		{"blah", "invalid license string"},
+		{"cl-0-blah", "invalid license string"},
+	} {
+		if err := (settings.Updater{}).Set("enterprise.license", tc.lic, "s"); !testutils.IsError(
+			err, tc.err,
+		) {
+			t.Fatalf("%q: expected err %q, got %v", tc.lic, tc.err, err)
+		}
+	}
+}
