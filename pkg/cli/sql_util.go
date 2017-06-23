5c2b9bc6e8b4053edf2ca0d2af1f238ae5023a95
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-06-23 20:16:32 +0000

    cli/sql: report the cluster ID, node ID and server version upon new connections
    This patch ensures the client prints out the cluster ID, node ID,
    server version, and if different from the server version, also the
    client version, both upon the initial connection and upon reconnects
    *if they change*.
    
    For example:
    
        $ ./cockroach sql
        # Welcome to the cockroach SQL interface.
        # All statements must be terminated by a semicolon.
        # To exit: CTRL + D.
        #
        # Server version: CockroachDB CCL 0da9d8f-dirty (linux amd64, built 2017/06/23 19:50:15, go1.8) (same version as client)
        # Cluster ID: fe26b6f3-3153-4c75-a7fb-3027c327e5ab
        # Node ID: 1
        #
        # Enter \? for a brief introduction.
        #
        root@localhost:26257/>
    
    Then upon a reconnect, if the node ID changes:
    
        root@localhost:26257/> SELECT 1;
        driver: bad connection
        connection lost; opening new connection: all session settings will be lost
        # Node ID: 2
        root@localhost:26257/>
    
    If the server version has changed:
    
        root@localhost:26257/> select 1;
        driver: bad connection
        connection lost; opening new connection: all session settings will be lost
        # Client version: CockroachDB CCL a1b6734-dirty (linux amd64, built 2017/06/23 19:14:15, go1.8)
        # Server version: CockroachDB CCL a1b6734-dirty (linux amd64, built 2017/06/23 19:15:00, go1.8)
        root@localhost:26257/>
    
    If the cluster ID has changed:
    
        root@localhost:26257/> SELECT 1;
        driver: bad connection
        connection lost; opening new connection: all session settings will be lost
    
        # WARNING: the cluster ID has changed!
    
        # Previous cluster ID: b8d0cc65-3a71-4db8-bcda-6fa86206ebb0
        # Cluster ID: fe26b6f3-3153-4c75-a7fb-3027c327e5ab
        root@localhost:26257/>

--- pkg/cli/sql_util.go
+++ pkg/cli/sql_util.go
@@ -22,6 +22,7 @@ import (
 	"fmt"
 	"io"
 	"net/url"
+	"reflect"
 	"strings"
 	"text/tabwriter"
 	"time"
@@ -31,6 +32,7 @@ import (
 	"github.com/lib/pq"
 	"golang.org/x/net/context"
 
+	"github.com/cockroachdb/cockroach/pkg/build"
 	"github.com/cockroachdb/cockroach/pkg/security"
 	"github.com/cockroachdb/cockroach/pkg/sql/parser"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
@@ -50,6 +52,13 @@ type sqlConn struct {
 	// dbName is the last known current database, to be reconfigured in
 	// case of automatic reconnects.
 	dbName string
+
+	// clusterID, nodeID and serverBuildInfo are the last known
+	// corresponding values from the server, used to report any changes
+	// upon (re)connects.
+	clusterID       string
+	nodeID          string
+	serverBuildInfo build.Info
 }
 
 func (c *sqlConn) ensureConn() error {
@@ -71,7 +80,113 @@ func (c *sqlConn) ensureConn() error {
 		}
 		c.reconnecting = false
 		c.conn = conn.(sqlConnI)
+		if err := c.checkServerMetadata(); err != nil {
+			return err
+		}
 	}
+	return nil
+}
+
+var clientVersionShort = build.GetInfo().Short()
+var unknownServerBuild = build.Info{
+	GoVersion:    "?",
+	Tag:          "?",
+	Time:         "?",
+	Revision:     "?",
+	CgoCompiler:  "?",
+	Platform:     "?",
+	Distribution: "?",
+	Type:         "?",
+	Dependencies: nil,
+}
+
+// checkServerMetadata reports the server version, cluster ID and the
+// node ID upon the initial connection or if either has changed since
+// the last connection, based on the last known values in the sqlConn
+// struct.
+func (c *sqlConn) checkServerMetadata() error {
+	if !isInteractive {
+		// Version reporting is just noise in non-interactive sessions.
+		return nil
+	}
+
+	newNodeID := ""
+	newServerBuildInfo := unknownServerBuild
+	newClusterID := ""
+
+	// Retrieve the cluster ID.
+	clusterVal, _ := c.getServerValue("cluster ID", "SELECT crdb_internal.cluster_id()::STRING")
+	if s, ok := clusterVal.(string); ok {
+		newClusterID = s
+	}
+
+	// Retrieve the node ID and server build info.
+	rows, err := c.Query("SELECT * FROM crdb_internal.node_build_info", nil)
+	if err == driver.ErrBadConn {
+		return err
+	}
+	if err != nil {
+		fmt.Fprintln(stderr, "unable to retrieve the server's version")
+	} else {
+		defer func() { _ = rows.Close() }()
+
+		// Read the node_build_info table as an array of strings.
+		rowVals, err := getAllRowStrings(rows, true /* showMoreChars */)
+		if err != nil || len(rowVals) == 0 || len(rowVals[0]) != 3 {
+			fmt.Fprintln(stderr, "error while rerieving the server's version")
+			return err
+		}
+
+		// Extract the build.Info fields from the query results.
+		var zeroValue reflect.Value
+		infoStruct := reflect.ValueOf(&newServerBuildInfo).Elem()
+		for _, row := range rowVals {
+			newNodeID = row[0]
+			if vField := infoStruct.FieldByName(row[1]); vField != zeroValue {
+				vField.Set(reflect.ValueOf(row[2]))
+			}
+		}
+	}
+
+	// Report the server version only if it the revision has been
+	// fetched successfully, and the revision has changed since the last
+	// connection.
+	if newServerBuildInfo != c.serverBuildInfo && newServerBuildInfo.Revision != "?" {
+		c.serverBuildInfo = newServerBuildInfo
+
+		serverVersionShort := newServerBuildInfo.Short()
+		isSame := ""
+		if serverVersionShort == clientVersionShort {
+			isSame = " (same version as client)"
+		}
+
+		if isSame == "" {
+			fmt.Println("# Client version:", clientVersionShort)
+		}
+		fmt.Printf("# Server version: %s%s\n", serverVersionShort, isSame)
+	}
+
+	// Report the cluster ID only if it it could be fetched
+	// successfully, and it has changed since the last connection.
+	if newClusterID != c.clusterID && newClusterID != "" {
+		// As a special case, we complain loudly if the cluster ID
+		// actually changes. This is sign of a potentially very bad load
+		// balancer configuration.
+		if c.clusterID != "" {
+			fmt.Print("\n# WARNING: the cluster ID has changed!\n\n")
+			fmt.Println("# Previous cluster ID:", c.clusterID)
+		}
+		c.clusterID = newClusterID
+		fmt.Println("# Cluster ID:", newClusterID)
+	}
+
+	// Report the node ID only if it it could be fetched successfully,
+	// and it has changed since the last connection.
+	if newNodeID != c.nodeID && newNodeID != "" {
+		c.nodeID = newNodeID
+		fmt.Println("# Node ID:", newNodeID)
+	}
+
 	return nil
 }
 
