efe2593cbd37a0740271ec90ea2a7dab6861e5ec
David Taylor <tinystatemachine@gmail.com>
2017-07-06 23:10:38 +0000

    server: preserve stat collection buckets
    previously when reporting we iterated over the various stats buckets to build a
    report, but we mistakenly overwrote an existing bucket if the same query also
    existed with a different DistSQL or failure value.
    
    also switch to a list of stats bundles (query+info+stats) rather than a map:
    maps require unique keys and we don't have a convenient one, plus we actually
    don't want the lookup semantics of a map anyway -- the consumer (e.g. our
    diagnostic collector) can decide always if/how it wants to group the info as
    fits its needs.

--- pkg/server/updates.go
+++ pkg/server/updates.go
@@ -34,6 +34,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings"
+	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/util/envutil"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
@@ -101,7 +102,7 @@ type reportingInfo struct {
 	Node       nodeInfo                                          `json:"node"`
 	Stores     []storeInfo                                       `json:"stores"`
 	Schema     []sqlbase.TableDescriptor                         `json:"schema"`
-	QueryStats map[string]map[string]roachpb.StatementStatistics `json:"sqlstats"`
+	QueryStats map[string][]sql.CollectedStats `json:"queryStats"`
 	// UnimplementedErrors reports when unimplemented features are encountered.
 	UnimplementedErrors map[string]uint `json:"unimplemented"`
 }
