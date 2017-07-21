81304af5358fcb2048b4ff5105f174c247b45b8f
Sergei Turukin <rampage644@gmail.com>
2017-07-21 13:05:43 +0300

    Properly render empty strings
    Empty strings are rendered as "(empty)" as per sqllogictest documentation.

--- pkg/sql/logictest/logic_test.go
+++ pkg/sql/logictest/logic_test.go
@@ -1287,6 +1287,11 @@ func (t *logicTest) execQuery(query logicQuery) error {
 						val = str
 					}
 				}
+				// Empty strings are rendered as "(empty)" as per sqllogictest
+				// documentation.
+				if val == "" {
+					val = "(empty)"
+				}
 				// We split string results on whitespace and append a separate result
 				// for each string. A bit unusual, but otherwise we can't match strings
 				// containing whitespace.
