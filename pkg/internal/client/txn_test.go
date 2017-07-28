3d3219fb4a1a71d0908f6e0b102de3524c069007
Andrei Matei <andreimatei1@gmail.com>
2017-07-28 17:26:52 -0400

    client: move Transaction initialization into constructor
    Fixes #13894.
    Fixes #16908.
    Touches #16827.
    
    Before this patch, we were initializing a Transaction proto the first
    time client.Txn.Send() was called. This patch moves the initialization
    of the proto to the client.Txn ctor. This means that a txn ID is
    assigned early and so that all code can rely on transactions having IDs,
    which simplifies things.
    The transaction timestamp is also assigned early, and so
    TxnExecOptions.AssignOrigTimestamp can go away.

--- pkg/internal/client/txn_test.go
+++ pkg/internal/client/txn_test.go
@@ -438,7 +438,7 @@ func TestEndWriteRestartReadOnlyTransaction(t *testing.T) {
 				// HACK ALERT: to do without a TxnCoordSender, we jump through hoops to
 				// get the retryable error expected by db.Txn().
 				return roachpb.NewHandledRetryableTxnError(
-					"bogus retryable error", txn.Proto().ID, *txn.Proto())
+					"bogus retryable error", *txn.Proto().ID, *txn.Proto())
 			}
 			if !success {
 				return errors.New("aborting on purpose")
@@ -493,7 +493,7 @@ func TestTransactionKeyNotChangedInRestart(t *testing.T) {
 			// get the retryable error expected by db.Txn().
 			return nil, roachpb.NewError(
 				roachpb.NewHandledRetryableTxnError(
-					"bogus retryable error", ba.Txn.ID, *ba.Txn))
+					"bogus retryable error", *ba.Txn.ID, *ba.Txn))
 		}
 		return ba.CreateReply(), nil
 	}), clock)
@@ -587,7 +587,7 @@ func TestRunTransactionRetryOnErrors(t *testing.T) {
 								// HACK ALERT: to do without a TxnCoordSender, we jump through
 								// hoops to get the retryable error expected by db.Txn().
 								return nil, roachpb.NewError(roachpb.NewHandledRetryableTxnError(
-									pErr.Message, ba.Txn.ID, *ba.Txn))
+									pErr.Message, *ba.Txn.ID, *ba.Txn))
 							}
 							return nil, pErr
 						}
@@ -680,9 +680,6 @@ func TestTimestampSelectionInOptions(t *testing.T) {
 	db := NewDB(newTestSender(nil), clock)
 	txn := NewTxn(db)
 
-	execOpt := TxnExecOptions{
-		AssignTimestampImmediately: true,
-	}
 	refTimestamp := clock.Now()
 
 	txnClosure := func(ctx context.Context, txn *Txn, opt *TxnExecOptions) error {
@@ -690,7 +687,7 @@ func TestTimestampSelectionInOptions(t *testing.T) {
 		return txn.Put(ctx, "a", "b")
 	}
 
-	if err := txn.Exec(context.Background(), execOpt, txnClosure); err != nil {
+	if err := txn.Exec(context.Background(), TxnExecOptions{}, txnClosure); err != nil {
 		t.Fatal(err)
 	}
 
@@ -766,7 +763,7 @@ func TestWrongTxnRetry(t *testing.T) {
 			// HACK ALERT: to do without a TxnCoordSender, we jump through hoops to
 			// get the retryable error expected by txn.Exec().
 			return roachpb.NewHandledRetryableTxnError(
-				"test error", innerTxn.Proto().ID, *innerTxn.Proto())
+				"test error", *innerTxn.Proto().ID, *innerTxn.Proto())
 		}
 		innerTxn := NewTxn(db)
 		err := innerTxn.Exec(ctx, execOpt, innerClosure)
