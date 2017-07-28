593c8077dfca88485843a4e86808edb8d255a26c
Andrei Matei <andrei@cockroachlabs.com>
2017-07-28 17:39:02 -0400

    storage: remove unnecessary txn.ID != nil checks
    Transactional requests are never sent with nil txn ids, so these checks
    were unnecessary.

--- pkg/storage/replica_command.go
+++ pkg/storage/replica_command.go
@@ -113,7 +113,8 @@ func DefaultDeclareKeys(
 	} else {
 		spans.Add(SpanReadWrite, req.Header())
 	}
-	if header.Txn != nil && header.Txn.ID != nil {
+	if header.Txn != nil {
+		header.Txn.AssertInitialized(context.TODO())
 		spans.Add(SpanReadOnly, roachpb.Span{
 			Key: keys.AbortCacheKey(header.RangeID, *header.Txn.ID),
 		})
@@ -470,7 +471,8 @@ func verifyTransaction(h roachpb.Header, args roachpb.Request) error {
 func declareKeysWriteTransaction(
 	_ roachpb.RangeDescriptor, header roachpb.Header, req roachpb.Request, spans *SpanSet,
 ) {
-	if header.Txn != nil && header.Txn.ID != nil {
+	if header.Txn != nil {
+		header.Txn.AssertInitialized(context.TODO())
 		spans.Add(SpanReadWrite, roachpb.Span{
 			Key: keys.TransactionKey(req.Header().Key, *header.Txn.ID),
 		})
@@ -574,7 +576,8 @@ func declareKeysEndTransaction(
 	for _, span := range et.IntentSpans {
 		spans.Add(SpanReadWrite, span)
 	}
-	if header.Txn != nil && header.Txn.ID != nil {
+	if header.Txn != nil {
+		header.Txn.AssertInitialized(context.TODO())
 		spans.Add(SpanReadWrite, roachpb.Span{Key: keys.AbortCacheKey(header.RangeID, *header.Txn.ID)})
 	}
 
@@ -1351,7 +1354,8 @@ func declareKeysHeartbeatTransaction(
 	desc roachpb.RangeDescriptor, header roachpb.Header, req roachpb.Request, spans *SpanSet,
 ) {
 	declareKeysWriteTransaction(desc, header, req, spans)
-	if header.Txn != nil && header.Txn.ID != nil {
+	if header.Txn != nil {
+		header.Txn.AssertInitialized(context.TODO())
 		spans.Add(SpanReadOnly, roachpb.Span{
 			Key: keys.AbortCacheKey(header.RangeID, *header.Txn.ID),
 		})
