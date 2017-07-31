bd81208457de2fff8ac4382380c5221989a7d928
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-07-31 00:58:00 -0400

    sql/jobs: add storage.NodeLiveness mock

---
+++ pkg/sql/jobs/helpers_test.go
@@ -0,0 +1,117 @@
+// Copyright 2017 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// Author: Nikhil Benesch (nikhil.benesch@gmail.com)
+
+package jobs
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/storage"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/cockroach/pkg/util/uint128"
+	"github.com/cockroachdb/cockroach/pkg/util/uuid"
+)
+
+// FakeNodeID is a dummy node ID for use in tests. It always stores 1.
+var FakeNodeID = func() *base.NodeIDContainer {
+	nodeID := base.NodeIDContainer{}
+	nodeID.Reset(1)
+	return &nodeID
+}()
+
+// FakeClusterID is a dummy cluster ID for use in tests. It always returns
+// "deadbeef-dead-beef-dead-beefdeadbeef" as a uuid.UUID.
+var FakeClusterID = func() func() uuid.UUID {
+	clusterID := uuid.FromUint128(uint128.FromInts(0xdeadbeefdeadbeef, 0xdeadbeefdeadbeef))
+	return func() uuid.UUID { return clusterID }
+}()
+
+// FakeNodeLiveness allows simulating liveness failures without the full
+// storage.NodeLiveness machinery.
+type FakeNodeLiveness struct {
+	clock *hlc.Clock
+	mu    struct {
+		syncutil.Mutex
+		livenessMap map[roachpb.NodeID]*storage.Liveness
+	}
+
+	// A non-blocking send is performed over these channels when the corresponding
+	// method is called.
+	SelfCalledCh          chan struct{}
+	GetLivenessesCalledCh chan struct{}
+}
+
+// NewFakeNodeLiveness initializes a new NodeLiveness with nodeCount live nodes.
+func NewFakeNodeLiveness(clock *hlc.Clock, nodeCount int) *FakeNodeLiveness {
+	nl := &FakeNodeLiveness{
+		clock:                 clock,
+		SelfCalledCh:          make(chan struct{}),
+		GetLivenessesCalledCh: make(chan struct{}),
+	}
+	nl.mu.livenessMap = make(map[roachpb.NodeID]*storage.Liveness)
+	for i := 0; i < nodeCount; i++ {
+		nodeID := roachpb.NodeID(i + 1)
+		nl.mu.livenessMap[nodeID] = &storage.Liveness{
+			Epoch:      1,
+			Expiration: hlc.MaxTimestamp,
+			NodeID:     nodeID,
+		}
+	}
+	return nl
+}
+
+// Self implements the implicit storage.NodeLiveness interface. It uses NodeID
+// as the node ID. On every call, a nonblocking send is performed over nl.ch to
+// allow tests to execute a callback.
+func (nl *FakeNodeLiveness) Self() (*storage.Liveness, error) {
+	select {
+	case nl.SelfCalledCh <- struct{}{}:
+	default:
+	}
+	nl.mu.Lock()
+	defer nl.mu.Unlock()
+	return nl.mu.livenessMap[FakeNodeID.Get()], nil
+}
+
+// GetLivenesses implements the implicit storage.NodeLiveness interface.
+func (nl *FakeNodeLiveness) GetLivenesses() (out []storage.Liveness) {
+	select {
+	case nl.GetLivenessesCalledCh <- struct{}{}:
+	default:
+	}
+	nl.mu.Lock()
+	defer nl.mu.Unlock()
+	for _, liveness := range nl.mu.livenessMap {
+		out = append(out, *liveness)
+	}
+	return out
+}
+
+// FakeIncrementEpoch increments the epoch for the node with the specified ID.
+func (nl *FakeNodeLiveness) FakeIncrementEpoch(id roachpb.NodeID) {
+	nl.mu.Lock()
+	defer nl.mu.Unlock()
+	nl.mu.livenessMap[id].Epoch++
+}
+
+// FakeSetExpiration sets the expiration time of the liveness for the node with
+// the specified ID to ts.
+func (nl *FakeNodeLiveness) FakeSetExpiration(id roachpb.NodeID, ts hlc.Timestamp) {
+	nl.mu.Lock()
+	defer nl.mu.Unlock()
+	nl.mu.livenessMap[id].Expiration = ts
+}
