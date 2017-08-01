3a792e07a109348262f7e964f83f8ee0db703b8a
Radu Berinde <radu@cockroachlabs.com>
2017-08-01 09:04:55 -0400

    distsql: generalize outbox.start calls
    We will require a start call for the new hash router, similar to outboxes.
    Making the flow code keep track of a list of generic `startable`s instead of
    outboxes. Also moving the WaitGroup.Add() calls inside start.

--- pkg/sql/distsqlrun/outbox.go
+++ pkg/sql/distsqlrun/outbox.go
@@ -55,7 +55,6 @@ type outbox struct {
 	numRows int
 
 	err error
-	wg  *sync.WaitGroup
 }
 
 var _ RowReceiver = &outbox{}
@@ -301,7 +300,7 @@ func (m *outbox) listenForDrainSignalFromConsumer(ctx context.Context) (<-chan d
 	return ch, nil
 }
 
-func (m *outbox) run(ctx context.Context) {
+func (m *outbox) run(ctx context.Context, wg *sync.WaitGroup) {
 	err := m.mainLoop(ctx)
 	if m.stream != nil {
 		closeErr := m.stream.CloseSend()
@@ -310,13 +309,15 @@ func (m *outbox) run(ctx context.Context) {
 		}
 	}
 	m.err = err
-	if m.wg != nil {
-		m.wg.Done()
+	if wg != nil {
+		wg.Done()
 	}
 }
 
 func (m *outbox) start(ctx context.Context, wg *sync.WaitGroup) {
-	m.wg = wg
+	if wg != nil {
+		wg.Add(1)
+	}
 	m.RowChannel.Init(nil)
-	go m.run(ctx)
+	go m.run(ctx, wg)
 }
