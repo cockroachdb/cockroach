4ba5fb310092eaec7b2f398b49e574a6da03eace
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-08-01 11:00:09 -0400

    build: avoid Git race condition when computing build tag
    Occasionally, TeamCity release builds would have a build tag that ended
    with "-dirty", despite being built from a clean checkout. Turns out this
    was due to an performance optimization in `git diff-index`. Git plumbing
    commands can report false positives if files change too quickly on a
    filesystem where mtimes have only one-second resolution; Git porcelain
    commands exist to detect and remove these race conditions.
    
    The solution is to simply use the corresponding porcelain command, `git
    diff`, in place of `git diff-index`.
    
    Fixes #14284.

--- build/teamcity-publish-s3-binaries.sh
+++ build/teamcity-publish-s3-binaries.sh
@@ -7,25 +7,9 @@ set -euxo pipefail
 
 export BUILDER_HIDE_GOPATH_SRC=1
 
-echo 'starting release build'
-cat .buildinfo/tag || true
-cat .buildinfo/rev || true
-build/builder.sh git status
-
 build/builder.sh go install ./pkg/cmd/publish-artifacts
-
-echo 'installed release builder'
-cat .buildinfo/tag || true
-cat .buildinfo/rev || true
-build/builder.sh git status
-
 build/builder.sh env \
 	AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
 	AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
 	TC_BUILD_BRANCH="$TC_BUILD_BRANCH" \
 	publish-artifacts "$@"
-
-echo 'built and uploaded artifacts'
-cat .buildinfo/tag || true
-cat .buildinfo/rev || true
-build/builder.sh git status
