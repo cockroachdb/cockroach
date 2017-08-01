83f873cc9862796086c85bd5ea94400ae6acbf9a
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-08-01 23:29:01 +0000

    sql: re-vamp the view dependency analysis
    Prior to this patch, view dependency analysis during CREATE VIEW was
    broken because it would only check dependencies after plan
    optimization, i.e. possibly after some dependency were lost. For
    example, with the queries:
    
    ```sql
    CREATE VIEW v AS SELECT k FROM (SELECT k,v FROM kv) -- loses dependency on kv.v
    CREATE VIEW v AS SELECT k,v FROM kv WHERE FALSE -- loses dependency on kv
    ```
    
    This patch addresses the issue as follows:
    
    - the dependencies are now collected during the initial construction
      of the query plan, before any optimization are applied. This way we
      ensure the dependency tracking is complete.
    - a migration is implemented that will fix any view descriptor and
      corresponding dependency information that was populated prior to
      this fix.

--- pkg/sql/walk.go
+++ pkg/sql/walk.go
@@ -359,9 +359,8 @@ func (v *planVisitor) visit(plan planNode) {
 
 	case *createViewNode:
 		if v.observer.attr != nil {
-			v.observer.attr(name, "query", n.sourceQuery)
+			v.observer.attr(name, "query", parser.AsStringWithFlags(n.n.AsSource, parser.FmtParsable))
 		}
-		v.visit(n.sourcePlan)
 
 	case *delayedNode:
 		if v.observer.attr != nil {
