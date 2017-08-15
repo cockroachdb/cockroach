cockroachdb/cockroach

commit 24ce72220b5f2d35e165409e90bf65ec8d4db690

storage: Fix the DisableReplicaRebalancing testing knob
It previously wasn't guaranteed to always respected since it was only
being checked in shouldQueue -- that made it possible for the replica to
be added to the replicate queue for some other reason but then end up
hitting this code path and getting rebalanced.

pkg/storage/replicate_queue.go
@@ -462,29 +462,33 @@ func (rq *replicateQueue) processOneChange(
 			}
 		}
 
-		rebalanceStore := rq.allocator.RebalanceTarget(
-			ctx, zone.Constraints, rangeInfo, storeFilterThrottled)
-		if rebalanceStore == nil {
-			if log.V(1) {
-				log.Infof(ctx, "no suitable rebalance target")
+		if !rq.store.TestingKnobs().DisableReplicaRebalancing {
+			rebalanceStore := rq.allocator.RebalanceTarget(
+				ctx, zone.Constraints, rangeInfo, storeFilterThrottled)
+			if rebalanceStore == nil {
+				if log.V(1) {
+					log.Infof(ctx, "no suitable rebalance target")
+				}
+			} else {
+				rebalanceReplica := roachpb.ReplicationTarget{
+					NodeID:  rebalanceStore.Node.NodeID,
+					StoreID: rebalanceStore.StoreID,
+				}
+				rq.metrics.RebalanceReplicaCount.Inc(1)
+				if log.V(1) {
+					log.Infof(ctx, "rebalancing to %+v: %s",
+						rebalanceReplica, rangeRaftProgress(repl.RaftStatus(), desc.Replicas))
+				}
+				if err := rq.addReplica(
+					ctx, repl, rebalanceReplica, desc, SnapshotRequest_REBALANCE); err != nil {
+					return false, err
+				}
 			}
-			// No action was necessary and no rebalance target was found. Return
-			// without re-queuing this replica.
-			return false, nil
 		}
-		rebalanceReplica := roachpb.ReplicationTarget{
-			NodeID:  rebalanceStore.Node.NodeID,
-			StoreID: rebalanceStore.StoreID,
-		}
-		rq.metrics.RebalanceReplicaCount.Inc(1)
-		if log.V(1) {
-			log.Infof(ctx, "rebalancing to %+v: %s",
-				rebalanceReplica, rangeRaftProgress(repl.RaftStatus(), desc.Replicas))
-		}
-		if err := rq.addReplica(
-			ctx, repl, rebalanceReplica, desc, SnapshotRequest_REBALANCE); err != nil {
-			return false, err
-		}
+
+		// No action was necessary and no rebalance target was found. Return
+		// without re-queuing this replica.
+		return false, nil
 	}
 
 	return true, nil

commit 0a93486e7987d9de1d628b9a26f77f42a298c53f

storage: Re-add replica to replicate queue after rebalancing
I broke this in 24ce72220b5f2d35e165409e90bf65ec8d4db690

pkg/storage/replicate_queue.go
@@ -490,6 +490,7 @@ func (rq *replicateQueue) processOneChange(
 					ctx, repl, rebalanceReplica, desc, SnapshotRequest_REBALANCE); err != nil {
 					return false, err
 				}
+				return true, nil
 			}
 		}
 