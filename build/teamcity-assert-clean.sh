baf937ea2a83124a9aa9083a46c275593823ff66
David Taylor <tinystatemachine@gmail.com>
2017-08-17 15:04:20 +0000

    build: nuke everything on failure

--- build/teamcity-assert-clean.sh
+++ build/teamcity-assert-clean.sh
@@ -3,4 +3,8 @@ set -exuo pipefail
 
 export BUILDER_HIDE_GOPATH_SRC=1
 
-build/builder.sh /bin/bash -c '! git status --porcelain | read || (git status; git diff -a 1>&2; exit 1)'
+if ! build/builder.sh /bin/bash -c '! git status --porcelain | read || (git status; git diff -a 1>&2; exit 1)';
+then
+  docker run --volume="$(cd "$(dirname "$0")/.." && pwd):/nuke" --workdir="/nuke" golang:stretch git clean -fdx
+  exit 1
+fi
