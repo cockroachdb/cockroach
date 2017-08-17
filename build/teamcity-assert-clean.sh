ac6df9ce8d7c9cbae152905af461fa2d568322f0
David Taylor <tinystatemachine@gmail.com>
2017-08-17 15:04:20 +0000

    build: nuke everything on failure

--- build/teamcity-assert-clean.sh
+++ build/teamcity-assert-clean.sh
@@ -3,4 +3,7 @@ set -exuo pipefail
 
 export BUILDER_HIDE_GOPATH_SRC=1
 
-build/builder.sh /bin/bash -c '! git status --porcelain | read || (git status; git diff -a 1>&2; exit 1)'
+if ! build/builder.sh /bin/bash -c '! git status --porcelain | read || (git status; git diff -a 1>&2; exit 1)';
+  build/builder.sh git clean -fdx
+  exit 1
+fi
