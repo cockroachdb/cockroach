04f5d3812c3b3cf35351e3b22b49a737f6c6ead9
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-08-22 10:44:14 +0200

    util/uint128: clean up error messages

--- pkg/util/uint128/uint128.go
+++ pkg/util/uint128/uint128.go
@@ -17,7 +17,6 @@ package uint128
 import (
 	"encoding/binary"
 	"encoding/hex"
-	"fmt"
 
 	"github.com/pkg/errors"
 )
@@ -70,11 +69,11 @@ func FromBytes(b []byte) Uint128 {
 // FromString parses a hexadecimal string as a 128-bit big-endian unsigned integer.
 func FromString(s string) (Uint128, error) {
 	if len(s) > 32 {
-		return Uint128{}, fmt.Errorf("Input string %s too large for uint128", s)
+		return Uint128{}, errors.Errorf("input string %s too large for uint128", s)
 	}
 	bytes, err := hex.DecodeString(s)
 	if err != nil {
-		return Uint128{}, errors.Wrapf(err, "Could not decode %s as hex", s)
+		return Uint128{}, errors.Wrapf(err, "could not decode %s as hex", s)
 	}
 
 	// Grow the byte slice if it's smaller than 16 bytes, by prepending 0s
