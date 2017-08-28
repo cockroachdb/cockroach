b50d9990059c5d5a3fe2bc139e736ed4d1e7b626
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2017-08-28 12:49:02 -0400

    acceptance: build the local binary in the right place

--- pkg/acceptance/run.sh
+++ pkg/acceptance/run.sh
@@ -10,6 +10,6 @@ set -euxo pipefail
 mkdir -p artifacts/acceptance
 export TMPDIR=$PWD/artifacts/acceptance
 
-make test PKG=./pkg/acceptance TESTTIMEOUT="${TESTTIMEOUT-30m}" TAGS=acceptance TESTFLAGS="${TESTFLAGS--v -nodes 4} -l $TMPDIR"
-# For the tests that don't use Docker.
+# For the acceptance tests that run without Docker.
 make build
+make test PKG=./pkg/acceptance TESTTIMEOUT="${TESTTIMEOUT-30m}" TAGS=acceptance TESTFLAGS="${TESTFLAGS--v -nodes 4} -l $TMPDIR"
