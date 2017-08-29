0b212d85ab3922767f92ddc50ffdcf260bfda0be
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2017-08-29 11:29:31 -0400

    acceptance: restore GetStopper()
    It is used by acceptanceccl.

--- pkg/acceptance/util.go
+++ pkg/acceptance/util.go
@@ -32,6 +32,11 @@ import (
 
 var stopper = stop.NewStopper()
 
+// GetStopper returns the stopper used by acceptance tests.
+func GetStopper() *stop.Stopper {
+	return stopper
+}
+
 // RunTests runs the tests in a package while gracefully handling interrupts.
 func RunTests(m *testing.M) {
 	randutil.SeedForTests()
