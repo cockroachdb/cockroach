f54d002935077f7bda8e7abf3f371138571ff137
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-08-31 23:15:16 +0000

    cli/sql: enable contextual help without leaving the current statement
    With this patch, when a user presses the TAB key on the interactive
    prompt *after entering a question mark*, the current line of input is
    processed by the parser for syntactic analysis.
    
    If a help message is available, it is displayed and the user can
    continue entering SQL.
    
    If a syntax error is detected, it is immediately reported so that the
    user gets a chance to fix the statement before pressing enter or
    entering a final semicolon.
    
    If there is no question mark, the user is informed about the lack of
    auto-completion:
    
    ```
    root@:26257/> select <tab>
    tab completion not supported; append '?' and press tab for contextual help
    
    select _
    ```
    
    Note: this patch is *not* implementing *auto-completion*: the TAB key
    only displays the help text if there is already a question mark. It
    does *not* propose a list of identifiers / table names / function
    names that are valid in that context. This may be implemented in a
    later iteration.

--- pkg/cli/interactive_tests/test_contextual_help.tcl
+++ pkg/cli/interactive_tests/test_contextual_help.tcl
@@ -13,6 +13,16 @@ eexpect "TRUNCATE"
 eexpect "SHOW"
 eexpect "ROLLBACK"
 eexpect root@
+
+send "?\t"
+eexpect "TRUNCATE"
+eexpect "SHOW"
+eexpect "ROLLBACK"
+eexpect "?"
+send "\010"
+send "select 1;\r"
+eexpect "1 row"
+eexpect root@
 end_test
 
 start_test "Check that a ? after a simple statement prints help."
@@ -33,6 +43,7 @@ eexpect "EXPLAIN"
 eexpect "SHOW"
 eexpect "See also"
 eexpect root@
+
 end_test
 
 start_test "Check that the last statement with help text made it to history."
@@ -44,7 +55,38 @@ eexpect "See also"
 eexpect root@
 end_test
 
+
+start_test "Check that ?-tab works."
+send "select ?\t"
+eexpect "Command: "
+eexpect "SELECT"
+eexpect "data manipulation"
+eexpect "FROM"
+eexpect "ORDER BY"
+eexpect "See also"
+eexpect "select ?"
+send "\010"
+send "1;\r"
+eexpect "1 row"
+eexpect root@
+
+send "select * from ?\t"
+eexpect "Command: "
+eexpect "data source"
+eexpect "JOIN"
+eexpect "EXPLAIN"
+eexpect "SHOW"
+eexpect "See also"
+eexpect "select * from ?"
+send "\010"
+send "(values (1));\r"
+eexpect "1 row"
+eexpect root@
+
+end_test
+
 start_test "Check that a ? in a function call context prints help about that function."
+
 send "select count(?\r"
 eexpect "Function: "
 eexpect "count"
@@ -52,6 +94,19 @@ eexpect "built-in functions"
 eexpect "Signature"
 eexpect "See also"
 eexpect root@
+
+send "select count(?\t"
+eexpect "Function: "
+eexpect "count"
+eexpect "built-in functions"
+eexpect "Signature"
+eexpect "See also"
+eexpect "select count(?"
+send "\010"
+send "1);\r"
+eexpect "1 row"
+eexpect root@
+
 end_test
 
 # Finally terminate with Ctrl+C.
--- pkg/cli/sql.go
+++ pkg/cli/sql.go
@@ -556,6 +556,30 @@ func endsWithIncompleteTxn(stmts parser.StatementList) bool {
 
 var cmdHistFile = envutil.EnvOrDefaultString("COCKROACH_SQL_CLI_HISTORY", ".cockroachdb_history")
 
+// Do implements the readline.AutoCompleter interface.
+func (c *cliState) Do(line []rune, pos int) (newLine [][]rune, length int) {
+	var p parser.Parser
+	sql := string(line)
+	if !strings.HasSuffix(sql, "?") {
+		fmt.Fprintf(c.ins.Stdout(),
+			"\ntab completion not supported; append '?' and press tab for contextual help\n\n%s", sql)
+		return nil, 0
+	}
+
+	_, err := p.Parse(sql)
+	if err != nil {
+		if pgErr, ok := pgerror.GetPGCause(err); ok &&
+			err.Error() == "help token in input" &&
+			strings.HasPrefix(pgErr.Hint, "help:") {
+			fmt.Fprintf(c.ins.Stdout(), "\nSuggestion:\n%s\n", pgErr.Hint[6:])
+		} else {
+			fmt.Fprintf(c.ins.Stdout(), "\n%v\n", err)
+			maybeShowErrorDetails(c.ins.Stdout(), err, false)
+		}
+	}
+	return nil, 0
+}
+
 func (c *cliState) doStart(nextState cliStateEnum) cliStateEnum {
 	// Common initialization.
 	c.partialLines = []string{}
@@ -577,6 +601,7 @@ func (c *cliState) doStart(nextState cliStateEnum) cliStateEnum {
 			cfg := c.ins.Config.Clone()
 			cfg.HistoryFile = histFile
 			cfg.HistorySearchFold = true
+			cfg.AutoComplete = c
 			c.ins.SetConfig(cfg)
 		}
 
