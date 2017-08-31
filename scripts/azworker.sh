4c75c2a15dc361d08eeb41490f915e5ff73db73a
Radu Berinde <radu@cockroachlabs.com>
2017-08-30 20:17:29 -0400

    azworker: update commands to azure CLI 2.0
    Updating the script to use the Azure CLI 2.0.
    
    Installation instructions:
    https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest

--- scripts/azworker.sh
+++ scripts/azworker.sh
@@ -26,30 +26,27 @@ FQDN=${DOMAIN}.${LOCATION}.cloudapp.azure.com
 
 case ${1-} in
   create)
-    if azure group show "${RG}" >/dev/null 2>/dev/null; then
+    if [ $(az group show -n "${RG}" | wc -l) -gt 0 ]; then
       echo "Resource group ${RG} already exists; adding worker VM to it"
     else
       echo "Creating resource group ${RG}"
-      azure group create "${RG}" -l "${LOCATION}"
-      azure network vnet create -g "${RG}" -n "${NET}" -a 192.168.0.0/16 -l "${LOCATION}"
-      azure network vnet subnet create -g "${RG}" -e "${NET}" -n "${SUBNET}" -a 192.168.1.0/24
+      az group create -n "${RG}" -l "${LOCATION}"
+      az network vnet create -g "${RG}" -n "${NET}" --address-prefixes 192.168.0.0/16 -l "${LOCATION}"
+      az network vnet subnet create -g "${RG}" --vnet-name "${NET}" -n "${SUBNET}" --address-prefix 192.168.1.0/24
     fi
 
-    azure network public-ip create -g "${RG}" -n "${IP}" -l "${LOCATION}" -d "${DOMAIN}"
-    azure network nic create -g "${RG}" -n "${NIC}" -l "${LOCATION}" -p "${IP}" --subnet-name "${SUBNET}" --subnet-vnet-name "${NET}"
+    az network public-ip create -g "${RG}" -n "${IP}" -l "${LOCATION}" --dns-name "${DOMAIN}"
+    az network nic create -g "${RG}" -n "${NIC}" -l "${LOCATION}" --public-ip-address "${IP}" --subnet "${SUBNET}" --vnet-name "${NET}"
 
-    azure vm create \
+    az vm create \
         --resource-group "${RG}" \
         --name "${NAME}" \
         --location "${LOCATION}" \
-        --os-type linux \
-        --image-urn canonical:UbuntuServer:16.04-LTS:latest \
-        --ssh-publickey-file ~/.ssh/id_rsa.pub \
+        --image canonical:UbuntuServer:16.04-LTS:latest \
+        --ssh-key-value ~/.ssh/id_rsa.pub \
         --admin-username "${USER}" \
-        --vm-size "${MACHINE_SIZE}" \
-        --nic-name "${NIC}" \
-        --vnet-name "${NET}" \
-        --vnet-subnet-name "${SUBNET}"
+        --size "${MACHINE_SIZE}" \
+        --nics "${NIC}"
 
     # Clear any cached host keys for this hostname and accept the new one.
     ssh-keygen -R "${FQDN}"
@@ -82,25 +79,25 @@ case ${1-} in
     echo "VM now running at ${FQDN}"
     ;;
   start)
-    azure vm start "${RG}" "${NAME}"
+    az vm start -g "${RG}" -n "${NAME}"
     ;;
   stop)
-    azure vm deallocate "${RG}" "${NAME}"
+    az vm deallocate -g "${RG}" -n "${NAME}"
     ;;
   delete)
     # The straightforward thing to do would be to first delete the VM, then
     # check if there are any virtual machines left in the group. However, the
     # deleted VM doesn't disappear right away from the listing. So we instead
     # count the initial number of VMs.
-    NUM_VMS=$(azure group show "${RG}" | (grep -c "Type *: *virtualMachines" || true))
-    azure vm delete "${RG}" "${NAME}"
+    NUM_VMS=$(az vm list -g "${RG}" | (grep -c "\"type\":.*virtualMachines" || true))
+    az vm delete -g "${RG}" -n "${NAME}"
     if [ "$NUM_VMS" -gt 1 ]; then
-      azure network nic delete -g "${RG}" -n "${NIC}" -q
-      azure network public-ip delete -g "${RG}" -n "${IP}" -q
+      az network nic delete -g "${RG}" -n "${NIC}"
+      az network public-ip delete -g "${RG}" -n "${IP}"
       echo "Resource group ${RG} still contains VMs; leaving in place"
     else
       echo "Deleting resource group ${RG}"
-      azure group delete "${RG}" -q
+      az group delete -n "${RG}" --yes
     fi
     ;;
   ssh)
