dc7ba3bb98b085e6b38780021d86178146b678b5
Alex Robinson <alexdwanerobinson@gmail.com>
2017-09-05 15:12:34 -0400

    storage: Keep estimatedCommitIndex up to date on raft leader
    Since we only previously only bumped Replica.mu.estimatedCommitIndex
    based on received raft messages, the raft leader never incremented its
    own commit index. This meant that after losing/transferring its
    leadership, there would be a short period of time during which a
    replica's getEstimatedBehindCountRLocked method would assume that it had
    never heard from the leader. This causes Store.updateReplicationGauges
    to disable rebalancing to the store until the next time it runs.
    
    Fixes #17375, because it was frequently happening that node 1 would
    transfer away a few leases and then have its rebalancing disabled as a
    result of this chain of events. Since the test waits for rebalancing to
    happen, it would then time out.

--- pkg/storage/client_replica_test.go
+++ pkg/storage/client_replica_test.go
@@ -1512,8 +1512,6 @@ func TestDrainRangeRejection(t *testing.T) {
 func TestSystemZoneConfigs(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 
-	t.Skip("https://github.com/cockroachdb/cockroach/issues/17375")
-
 	if testing.Short() {
 		t.Skip("short flag")
 	}
