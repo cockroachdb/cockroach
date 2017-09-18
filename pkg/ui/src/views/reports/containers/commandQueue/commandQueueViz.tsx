e1c37033aa5c01dab60555b54b48fe5e39a8317e
Pete Vilter <vilterp@cockroachlabs.com>
2017-09-18 17:50:09 -0400

    add table which shows details on mouse over

--- pkg/ui/src/views/reports/containers/commandQueue/commandQueueViz.tsx
+++ pkg/ui/src/views/reports/containers/commandQueue/commandQueueViz.tsx
@@ -4,49 +4,28 @@ import Long from "long";
 import * as protos from "src/js/protos";
 import * as dagre from "dagre-layout";
 
-interface CommandQueueVizProps {
+interface QueueVizProps {
   // TODO(vilterp): doesn't compile without $Properties; not sure why
   queue: protos.cockroach.storage.CommandQueueSnapshot$Properties;
 }
 
-const testData: CommandQueueVizProps = {
-  queue: {
-    commands: [
-      {
-        id: Long.fromNumber(0),
-        prereqs: [],
-        readonly: false,
-        span: null,
-        timestamp: null,
-      },
-      {
-        id: Long.fromNumber(1),
-        prereqs: [
-          Long.fromNumber(0),
-        ],
-        readonly: false,
-        span: null,
-        timestamp: null,
-      },
-      {
-        id: Long.fromNumber(2),
-        prereqs: [
-          Long.fromNumber(0),
-        ],
-        readonly: false,
-        span: null,
-        timestamp: null,
-      },
-    ],
-  },
-};
+interface QueueVizState {
+  hoveredNode: Long;
+}
 
 const COMMAND_WIDTH = 60;
 const COMMAND_HEIGHT = 40;
 
-export default class CommandQueueViz extends React.Component<CommandQueueVizProps, {}> {
+export default class CommandQueueViz extends React.Component<QueueVizProps, QueueVizState> {
 
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      hoveredNode: null,
+    };
+  }
+
+  computeLayout() {
     const g = new dagre.graphlib.Graph();
 
     g.setGraph({
@@ -68,6 +47,40 @@ export default class CommandQueueViz extends React.Component<CommandQueueVizProp
 
     dagre.layout(g);
 
+    return g;
+  }
+
+  detailsTable(command: protos.cockroach.storage.CommandQueueCommand$Properties) {
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <td>ID</td>
+            <td>{`${command.id.toString()}`}</td>
+          </tr>
+          <tr>
+            <td>Read Only</td>
+            <td>{`${command.readonly}`}</td>
+          </tr>
+          <tr>
+            <td>Key Range</td>
+            <td>{command.span.key} to {command.span.end_key}</td>
+          </tr>
+          <tr>
+            <td>Timestamp</td>
+            <td>
+              {command.timestamp.logical} Logical{" "}
+              {command.timestamp.wall_time.toString()} Wall
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+
+  render() {
+    const g = this.computeLayout();
+
     const nodeIds = g.nodes() as Array<string>;
     const nodes = nodeIds.map((nodeId) => (
       g.node(nodeId)
@@ -83,39 +96,49 @@ export default class CommandQueueViz extends React.Component<CommandQueueVizProp
     }
 
     return (
-      <svg
-        width={g.graph().width}
-        height={g.graph().height}
-        style={{border: "1px solid black"}}>
-        {nodes.map((node) => (
-          <rect
-            key={node.command.id.toString()}
-            x={node.x - COMMAND_WIDTH / 2}
-            y={node.y - COMMAND_HEIGHT / 2}
-            width={node.width}
-            height={node.height}
-            style={{fill: node.command.readonly ? "lightgreen" : "pink", stroke: "black"}} />
-        ))}
-        {nodes.map((node) => (
-          <text
-            key={node.command.id.toString()}
-            textAnchor="middle"
-            dominantBaseline="middle"
-            x={node.x}
-            y={node.y}>
-            {node.command.id.toString()}
-          </text>
-        ))}
-        {edges.map((edge, idx: number) => {
-          return (
-            <polyline
-              key={idx}
-              points={edge.points.map(({x, y}) => (`${x},${y}`)).join(" ")}
-              // TODO: line ending: arrow (copy from DistSQL plan viz)
-              style={{stroke: "black", fill: "none"}} />
-          );
-        })}
-      </svg>
+      <div>
+        <svg
+          width={g.graph().width}
+          height={g.graph().height}
+          style={{border: "1px solid black"}}>
+          {nodes.map((node) => (
+            <rect
+              key={node.command.id.toString()}
+              x={node.x - COMMAND_WIDTH / 2}
+              y={node.y - COMMAND_HEIGHT / 2}
+              onMouseEnter={() => { this.setState({ hoveredNode: node.command.id }); }}
+              width={node.width}
+              height={node.height}
+              style={{
+                fill: node.command.readonly ? "lightgreen" : "pink",
+                stroke: (this.state.hoveredNode !== null && node.command.id.equals(this.state.hoveredNode))
+                  ? "red" : "black",
+              }} />
+          ))}
+          {nodes.map((node) => (
+            <text
+              key={node.command.id.toString()}
+              textAnchor="middle"
+              dominantBaseline="middle"
+              x={node.x}
+              y={node.y}>
+              {node.command.id.toString()}
+            </text>
+          ))}
+          {edges.map((edge, idx: number) => {
+            return (
+              <polyline
+                key={idx}
+                points={edge.points.map(({x, y}) => (`${x},${y}`)).join(" ")}
+                // TODO: line ending: arrow (copy from DistSQL plan viz)
+                style={{stroke: "black", fill: "none"}} />
+            );
+          })}
+        </svg>
+        {this.state.hoveredNode !== null
+          ? this.detailsTable(g.node(this.state.hoveredNode.toString()).command)
+          : null}
+      </div>
     );
   }
 
