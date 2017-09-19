d532a3a407c272ccf0f8f8cbc4de07b5eec74505
David Taylor <tinystatemachine@gmail.com>
2017-09-19 17:21:22 +0000

    sqlccl: batch csv rows into convertRecord
    ```
    name             old time/op    new time/op    delta
    ConvertRecord-8    6.50µs ± 8%    5.00µs ± 6%  -23.12%        (p=0.000 n=10+10)
    
    name             old speed      new speed      delta
    ConvertRecord-8  18.5MB/s ± 8%  24.0MB/s ± 6%  +29.99%        (p=0.000 n=10+10)
    
    name             old alloc/op   new alloc/op   delta
    ConvertRecord-8    3.56kB ± 0%    3.63kB ± 0%   +2.08%        (p=0.000 n=10+10)
    
    name             old allocs/op  new allocs/op  delta
    ConvertRecord-8      93.0 ± 0%      93.0 ± 0%     ~     (all samples are equal)
    ```

--- pkg/ccl/sqlccl/csv_test.go
+++ pkg/ccl/sqlccl/csv_test.go
@@ -731,15 +731,25 @@ func BenchmarkConvertRecord(b *testing.B) {
 			return convertRecord(ctx, recordCh, kvCh, nil, tableDesc)
 		})
 	}
+	const batchSize = 500
+
+	batch := csvRecord{
+		file:      "some/path/to/some/file/of/csv/data.tbl",
+		rowOffset: 1,
+		r:         make([][]string, 0, batchSize),
+	}
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		recordCh <- csvRecord{
-			file: "some/path/to/some/file/of/csv/data.tbl",
-			row:  i,
-			r:    tpchLineItemDataRows[i%len(tpchLineItemDataRows)],
+		if len(batch.r) > batchSize {
+			recordCh <- batch
+			batch.r = make([][]string, 0, batchSize)
+			batch.rowOffset = i
 		}
+
+		batch.r = append(batch.r, tpchLineItemDataRows[i%len(tpchLineItemDataRows)])
 	}
+	recordCh <- batch
 	close(recordCh)
 
 	if err := group.Wait(); err != nil {
