4a70fb50ff234729e67e0ab66af0b7f2ac028660
Justin Jaffray <justin@cockroachlabs.com>
2017-09-19 00:22:42 -0400

    sql: support casting strings to arrays
    Fixes #18419.
    
    Support expressions like '{1,2,3}'::INT[], or '{abc,"xyz"}'::STRING[].
    
    More testing, incl. fuzzing might be necessary.
    I'm also iffy on the current ad-hoc construction of an EvalContext for
    the purposes of having a location.

--- pkg/ccl/sqlccl/csv.go
+++ pkg/ccl/sqlccl/csv.go
@@ -428,7 +428,7 @@ func convertRecord(
 	}
 
 	parse := parser.Parser{}
-	evalCtx := parser.EvalContext{}
+	evalCtx := parser.EvalContext{Location: &time.UTC}
 	// Although we don't yet support DEFAULT expressions on visible columns,
 	// we do on hidden columns (which is only the default _rowid one). This
 	// allows those expressions to run.
@@ -444,7 +444,7 @@ func convertRecord(
 			if nullif != nil && r == *nullif {
 				datums[i] = parser.DNull
 			} else {
-				datums[i], err = parser.ParseStringAs(visibleCols[i].Type.ToDatumType(), r, time.UTC)
+				datums[i], err = parser.ParseStringAs(visibleCols[i].Type.ToDatumType(), r, &evalCtx)
 				if err != nil {
 					return errors.Wrapf(err, "%s: row %d: parse %q as %s", record.file, record.row, visibleCols[i].Name, visibleCols[i].Type.SQLString())
 				}
