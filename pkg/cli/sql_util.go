8423fe372cb38f06bd0c615a7bf611ac0d0d86a7
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-09-19 02:35:20 +0200

    cli: fix broken output of special characters in queries
    Prior to this patch, the CLI tools that display SQL rows (namely,
    `sql`, `user`, `zone`) would make an effort to display anything with
    the Unicode graphical property, and all whitespace, unchanged.
    
    As this is the most common case, a bug slipped through: when a
    character is neither graphical nor whitespace, it would erroneously
    get processed two times for escape: a first one by the low-level value
    formatter, and a second time by the table formatter.
    
    For example:
    
    ```
    > cockroach sql -e "select e'\x01' as x"
    x
    """\x01"""
    (1 row)
    ```
    
    The triple quotes in the output were because first the ASCII 1 byte
    would be escaped to the string `"\x01"` (first layer of quoting), then
    the TSV formatter would introduce the additional necessary CSV escapes
    by tripling them.
    
    The error was that although the content of the string must be escaped
    by the first-level formatter, that formatter should not introduce
    boundary quotes -- the boundary quoting is under the responsibility of
    the table formatter.
    
    With this patch:
    
    ```
    > cockroach sql -e "select e'\x01' as x"
    x
    \x01
    (1 row)
    ```
    
    Note that although this patch addresses the most egregious
    consequences of the original bug, the approach is arguably not yet
    complete: we probably would like the SQL formatter to insert values
    that are *equivalent* to those received from the server; this means
    that *whether to quote or not should be decided by the formatter*.
    Note that only the `raw` and `sql` formatters may be interested in
    this change; the more common `pretty`, `tsv`, `csv` and `records`
    formatter are fine as-is. A subsequent commit can introduce the
    corresponding necessary plumbing.
    
    Release note (cli change): CSV and TSV output have been improved:
    result rows that contain special characters are now quoted properly
    when formatted with `--pretty=csv` or `--pretty=tsv`.

--- pkg/cli/sql_util.go
+++ pkg/cli/sql_util.go
@@ -726,7 +726,10 @@ func formatVal(val driver.Value, showPrintableUnicode bool, showNewLinesAndTabs 
 				return t
 			}
 		}
-		return fmt.Sprintf("%+q", t)
+		s := fmt.Sprintf("%+q", t)
+		// Strip the start and final quotes. The surrounding display
+		// format (e.g. CSV/TSV) will add its own quotes.
+		return s[1 : len(s)-1]
 
 	case []byte:
 		if showPrintableUnicode {
@@ -742,7 +745,10 @@ func formatVal(val driver.Value, showPrintableUnicode bool, showNewLinesAndTabs 
 				return string(t)
 			}
 		}
-		return fmt.Sprintf("%+q", t)
+		// Strip the start and final quotes. The surrounding display
+		// format (e.g. CSV/TSV) will add its own quotes.
+		s := fmt.Sprintf("%+q", t)
+		return s[1 : len(s)-1]
 
 	case time.Time:
 		return t.Format(tree.TimestampOutputFormat)
