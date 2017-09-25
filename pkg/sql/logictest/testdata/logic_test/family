cockroachdb/cockroach

commit d5c9932a710c0c613024318b3caf4a828d383150

sql/sqlbase: don't read idx key if not necessary
During row fetching, if we don't need any of the columns contained
within the index key and the index is not interleaved, we can completely
skip reading and processing the index key.

This gives a nice speed boost for count(*) queries. I observe about 15%
improvement on a million row dataset on a local 1 node cluster.

pkg/sql/sqlbase/rowfetcher.go
@@ -61,6 +61,10 @@ type RowFetcher struct {
 	// The set of ColumnIDs that are required.
 	neededCols util.FastIntSet
 
+	// True if the index key must be decoded. This is only true if there are no
+	// needed columns and the table has no interleave children.
+	mustDecodeIndexKey bool
+
 	// Map used to get the index for columns in cols.
 	colIdxMap map[ColumnID]int
 
@@ -140,6 +144,19 @@ func (rf *RowFetcher) Init(
 				break
 			}
 		}
+	}
+
+	// If there are interleaves, we need to read the index key in order to
+	// determine whether this row is actually part of the index we're scanning.
+	// If we need to return any values from the row, we also have to read the
+	// index key to either get those values directly or determine the row's
+	// column family id to map the row values to their columns.
+	// Otherwise, we can completely avoid decoding the index key.
+	// TODO(jordan): Relax this restriction. Ideally we could skip doing key
+	// reading work if we need values from outside of the key, but not from
+	// inside of the key.
+	if !rf.neededCols.Empty() || len(rf.index.InterleavedBy) > 0 || len(rf.index.Interleave.Ancestors) > 0 {
+		rf.mustDecodeIndexKey = true
 	}
 
 	var indexColumnIDs []ColumnID
@@ -233,14 +250,18 @@ func (rf *RowFetcher) NextKey(ctx context.Context) (rowDone bool, err error) {
 			return true, nil
 		}
 
-		rf.keyRemainingBytes, ok, err = rf.ReadIndexKey(rf.kv.Key)
-		if err != nil {
-			return false, err
-		}
-		if !ok {
-			// The key did not match the descriptor, which means it's
-			// interleaved data from some other table or index.
-			continue
+		// See Init() for a detailed description of when we can get away with not
+		// reading the index key.
+		if rf.mustDecodeIndexKey {
+			rf.keyRemainingBytes, ok, err = rf.ReadIndexKey(rf.kv.Key)
+			if err != nil {
+				return false, err
+			}
+			if !ok {
+				// The key did not match the descriptor, which means it's
+				// interleaved data from some other table or index.
+				continue
+			}
 		}
 
 		// For unique secondary indexes, the index-key does not distinguish one row

pkg/server/settingsworker.go
@@ -31,7 +31,7 @@ import (
 
 // RefreshSettings starts a settings-changes listener.
 func (s *Server) refreshSettings() {
-	tbl := sqlbase.SettingsTable
+	tbl := &sqlbase.SettingsTable
 
 	a := &sqlbase.DatumAlloc{}
 	settingsTablePrefix := keys.MakeTablePrefix(uint32(tbl.ID))
@@ -46,7 +46,7 @@ func (s *Server) refreshSettings() {
 		// First we need to decode the setting name field from the index key.
 		{
 			nameRow := []sqlbase.EncDatum{{Type: tbl.Columns[0].Type}}
-			_, matches, err := sqlbase.DecodeIndexKey(a, &tbl, &tbl.PrimaryIndex, nameRow, nil, kv.Key)
+			_, matches, err := sqlbase.DecodeIndexKey(a, tbl, &tbl.PrimaryIndex, nameRow, nil, kv.Key)
 			if err != nil {
 				return errors.Wrap(err, "failed to decode key")
 			}

commit 91531a4459e40961f03a81bcb5f4a3722cfbd462

sql,keys: bugfix to rowfetcher for COUNT(*)
d5c9932 introduced a critical bug to rowfetcher that prevented it from
correctly understanding tables with multiple column families when no
columns from the table were marked as needed, such as in the case of
`COUNT(*)`.

This commit fixes the bug by reusing the logic from EnsureSafeSplitKey
to advance the rowfetcher's key to the family id section as it must,
without needing to decode any of the prefix of the key.

Also add a test to ensure COUNT(*) and multiple column families interact
properly.

pkg/keys/keys.go
@@ -615,12 +615,11 @@ func MakeFamilyKey(key []byte, famID uint32) []byte {
 	return encoding.EncodeUvarintAscending(key, uint64(len(key)-size))
 }
 
-// EnsureSafeSplitKey transforms an SQL table key such that it is a valid split key
-// (i.e. does not occur in the middle of a row).
-func EnsureSafeSplitKey(key roachpb.Key) (roachpb.Key, error) {
+// findFamilyIndex returns the index of the column family varint in a key. If
+// the key is not a table key, the index returned is the end of the key.
+func findFamilyIndex(key roachpb.Key) (int, error) {
 	if encoding.PeekType(key) != encoding.Int {
-		// Not a table key, so already a split key.
-		return key, nil
+		return len(key), nil
 	}
 
 	n := len(key)
@@ -630,7 +629,7 @@ func EnsureSafeSplitKey(key roachpb.Key) (roachpb.Key, error) {
 	buf := key[n-1:]
 	if encoding.PeekType(buf) != encoding.Int {
 		// The last byte is not a valid column ID suffix.
-		return nil, errors.Errorf("%s: not a valid table key", key)
+		return 0, errors.Errorf("%s: not a valid table key", key)
 	}
 
 	// Strip off the family ID / column ID suffix from the buf. The last byte of the buf
@@ -638,7 +637,7 @@ func EnsureSafeSplitKey(key roachpb.Key) (roachpb.Key, error) {
 	// does not contain a column ID suffix).
 	_, colIDLen, err := encoding.DecodeUvarintAscending(buf)
 	if err != nil {
-		return nil, err
+		return 0, err
 	}
 	// Note how this next comparison (and by extension the code after it) is overflow-safe. There
 	// are more intuitive ways of writing this that aren't as safe. See #18628.
@@ -651,9 +650,29 @@ func EnsureSafeSplitKey(key roachpb.Key) (roachpb.Key, error) {
 		// EnsureSafeSplitKey can be called on keys that look like table
 		// keys but which do not have a column ID length suffix (e.g
 		// by SystemConfig.ComputeSplitKey).
-		return nil, errors.Errorf("%s: malformed table key", key)
+		return 0, errors.Errorf("%s: malformed table key", key)
 	}
-	return key[:len(key)-int(colIDLen)-1], nil
+	return len(key) - int(colIDLen) - 1, nil
+}
+
+// GetFamilySuffix returns a byte slice of the input table key containing just
+// the family id/column id length suffix.
+func GetFamilySuffix(key roachpb.Key) ([]byte, error) {
+	idx, err := findFamilyIndex(key)
+	if err != nil {
+		return nil, err
+	}
+	return key[idx:], nil
+}
+
+// EnsureSafeSplitKey transforms an SQL table key such that it is a valid split key
+// (i.e. does not occur in the middle of a row).
+func EnsureSafeSplitKey(key roachpb.Key) (roachpb.Key, error) {
+	idx, err := findFamilyIndex(key)
+	if err != nil {
+		return nil, err
+	}
+	return key[:idx], nil
 }
 
 // Range returns a key range encompassing all the keys in the Batch.

pkg/sql/sqlbase/rowfetcher.go
@@ -22,6 +22,7 @@ import (
 	"golang.org/x/net/context"
 
 	"github.com/cockroachdb/cockroach/pkg/internal/client"
+	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/parser"
 	"github.com/cockroachdb/cockroach/pkg/util"
@@ -265,6 +266,13 @@ func (rf *RowFetcher) NextKey(ctx context.Context) (rowDone bool, err error) {
 				// The key did not match the descriptor, which means it's
 				// interleaved data from some other table or index.
 				continue
+			}
+		} else {
+			// We still need to consume the key until the family id, so processKV can
+			// know whether we've finished a row or not.
+			rf.keyRemainingBytes, err = keys.GetFamilySuffix(rf.kv.Key)
+			if err != nil {
+				return false, err
 			}
 		}
 

pkg/keys/keys_test.go
@@ -499,6 +499,15 @@ func TestEnsureSafeSplitKey(t *testing.T) {
 		if !d.expected.Equal(out) {
 			t.Fatalf("%d: %s: expected %s, but got %s", i, d.in, d.expected, out)
 		}
+
+		suffix, err := GetFamilySuffix(d.in)
+		if err != nil {
+			t.Fatalf("%d: %s: unexpected error: %v", i, d.in, err)
+		}
+		expectedSuffix := d.in[len(d.expected):]
+		if !bytes.Equal(suffix, expectedSuffix) {
+			t.Fatalf("%d: %s: expected %s, but got %s", i, d.in, expectedSuffix, suffix)
+		}
 	}
 
 	errorData := []struct {

pkg/sql/logictest/testdata/logic_test/family
@@ -26,6 +26,9 @@ abcd  CREATE TABLE abcd (
       )
 
 statement ok
+CREATE INDEX d_idx ON abcd(d)
+
+statement ok
 INSERT INTO abcd VALUES (1, 2, 3, 4), (5, 6, 7, 8)
 
 query IIII rowsort
@@ -33,6 +36,16 @@ SELECT * FROM abcd
 ----
 1 2 3 4
 5 6 7 8
+
+query I
+SELECT COUNT(*) FROM abcd
+----
+2
+
+query I
+SELECT COUNT(*) FROM abcd@d_idx
+----
+2
 
 statement ok
 UPDATE abcd SET b = 9, d = 10, c = NULL where c = 7
@@ -117,6 +130,7 @@ abcd  CREATE TABLE abcd (
       i INT NULL,
       j INT NULL,
       CONSTRAINT "primary" PRIMARY KEY (a ASC),
+      INDEX d_idx (d ASC),
       FAMILY f1 (a, b, e, f),
       FAMILY fam_1_c_d (c, d),
       FAMILY fam_2_g (g),