ffd068ee63222f678dacad5d81f5b601096648c7
Alfonso Subiotto Marqués <alfonso@cockroachlabs.com>
2017-09-28 13:30:57 -0400

    distsql: add distsql-disk configuration to logic tests
    The sorter processor has also been changed to observe the testing knob
    on the flow context that specifies a memory limit since that is the
    knob used by logic tests.

--- pkg/sql/logictest/logic_test.go
+++ pkg/sql/logictest/logic_test.go
@@ -276,6 +276,7 @@ var logicTestConfigs = []testClusterConfig{
 	{name: "distsql-disk", numNodes: 3, useFakeSpanResolver: true, overrideDistSQLMode: "On", distSQLUseDisk: true},
 	{name: "5node", numNodes: 5, overrideDistSQLMode: "Off"},
 	{name: "5node-distsql", numNodes: 5, overrideDistSQLMode: "On"},
+	{name: "5node-distsql-disk", numNodes: 5, overrideDistSQLMode: "On", distSQLUseDisk: true},
 }
 
 // An index in the above slice.
