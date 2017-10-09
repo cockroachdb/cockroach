becaa174365352099ac0e276763ce794664466de
Vivek Menezes <vivek@cockroachlabs.com>
2017-10-09 15:27:52 -0400

    storage: properly cleanup intentResolver's inFlight refcount

--- pkg/storage/intent_resolver_test.go
+++ pkg/storage/intent_resolver_test.go
@@ -36,9 +36,20 @@ func TestPushTransactionsWithNonPendingIntent(t *testing.T) {
 	defer stopper.Stop(context.TODO())
 	tc.Start(t, stopper)
 
-	intents := []roachpb.Intent{{Span: roachpb.Span{Key: roachpb.Key("a")}, Status: roachpb.ABORTED}}
-	if _, pErr := tc.store.intentResolver.maybePushTransactions(
-		context.Background(), intents, roachpb.Header{}, roachpb.PUSH_TOUCH, true); !testutils.IsPError(pErr, "unexpected aborted/resolved intent") {
-		t.Errorf("expected error on aborted/resolved intent, but got %s", pErr)
+	testCases := [][]roachpb.Intent{
+		{{Span: roachpb.Span{Key: roachpb.Key("a")}, Status: roachpb.PENDING},
+			{Span: roachpb.Span{Key: roachpb.Key("b")}, Status: roachpb.ABORTED}},
+		{{Span: roachpb.Span{Key: roachpb.Key("a")}, Status: roachpb.PENDING},
+			{Span: roachpb.Span{Key: roachpb.Key("b")}, Status: roachpb.COMMITTED}},
+	}
+	for _, intents := range testCases {
+		if _, pErr := tc.store.intentResolver.maybePushTransactions(
+			context.Background(), intents, roachpb.Header{}, roachpb.PUSH_TOUCH, true,
+		); !testutils.IsPError(pErr, "unexpected (ABORTED|COMMITTED) intent") {
+			t.Errorf("expected error on aborted/resolved intent, but got %s", pErr)
+		}
+		if cnt := len(tc.store.intentResolver.mu.inFlight); cnt != 0 {
+			t.Errorf("expected no inflight refcount map entries, found %d", cnt)
+		}
 	}
 }
