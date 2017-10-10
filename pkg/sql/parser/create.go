312bd5a0e9c8a0899271e3867cc6258c072fd569
Daniel Harrison <daniel.harrison@gmail.com>
2017-10-10 16:36:46 -0400

    parser: add PARTITION BY syntax
    It will eventually be on all of CREATE TABLE/INDEX and ALTER TABLE/INDEX but this commit starts with
    only CREATE TABLE.
    
    The two types of partitioning are list and range. Each partition in a
    partitioning can be further sub-partitioned. A list partition is
    has a list of tuples, but a range partition only has one tuple.
    
    See the RFC for full details:
    https://github.com/cockroachdb/cockroach/pull/18683
    
    Some examples of the syntax:
    
    ```sql
    CREATE TABLE roachmart.users (
      id SERIAL,
      country STRING,
      email STRING,
      ...,
      PRIMARY KEY (country, id)
    ) PARTITION BY LIST (country) (
      PARTITION australia VALUES ('AU'), ('NZ'),
      PARTITION north_america VALUES ('CA'), ('MX'), ('US'),
      PARTITION default VALUES (DEFAULT),
    );
    ```
    
    ```sql
    CREATE TABLE roachblog.articles (
      id SERIAL,
      published TIMESTAMP,
      author_id INT,
      ...,
      PRIMARY KEY (published, id)
    ) PARTITION BY RANGE (published) (
      PARTITION archived VALUES LESS THAN ('2017-01-01'),
      PARTITION recent VALUES LESS THAN ('2018-01-01')
    )
    ```

--- pkg/sql/parser/create.go
+++ pkg/sql/parser/create.go
@@ -636,11 +636,86 @@ func (node *InterleaveDef) Format(buf *bytes.Buffer, f FmtFlags) {
 	}
 }
 
+// PartitionBy represents an PARTITION BY definition within a CREATE/ALTER
+// TABLE/INDEX statement.
+type PartitionBy struct {
+	Fields NameList
+	// Exactly one of List or Range is required to be non-empty.
+	List  []ListPartition
+	Range []RangePartition
+}
+
+// Format implements the NodeFormatter interface.
+func (node *PartitionBy) Format(buf *bytes.Buffer, f FmtFlags) {
+	if len(node.List) > 0 {
+		buf.WriteString(` PARTITION BY LIST (`)
+	} else if len(node.Range) > 0 {
+		buf.WriteString(` PARTITION BY RANGE (`)
+	}
+	FormatNode(buf, f, node.Fields)
+	buf.WriteString(`) (`)
+	for i, p := range node.List {
+		if i > 0 {
+			buf.WriteString(", ")
+		}
+		FormatNode(buf, f, p)
+	}
+	for i, p := range node.Range {
+		if i > 0 {
+			buf.WriteString(", ")
+		}
+		FormatNode(buf, f, p)
+	}
+	buf.WriteString(`)`)
+}
+
+// ListPartition represents a PARTITION definition within a PARTITION BY LIST.
+type ListPartition struct {
+	Name         Name
+	Tuples       []*Tuple
+	Subpartition *PartitionBy
+}
+
+// Format implements the NodeFormatter interface.
+func (node ListPartition) Format(buf *bytes.Buffer, f FmtFlags) {
+	buf.WriteString(`PARTITION `)
+	FormatNode(buf, f, node.Name)
+	buf.WriteString(` VALUES `)
+	for i, n := range node.Tuples {
+		if i > 0 {
+			buf.WriteString(", ")
+		}
+		FormatNode(buf, f, n)
+	}
+	if node.Subpartition != nil {
+		FormatNode(buf, f, node.Subpartition)
+	}
+}
+
+// RangePartition represents a PARTITION definition within a PARTITION BY LIST.
+type RangePartition struct {
+	Name         Name
+	Tuple        *Tuple
+	Subpartition *PartitionBy
+}
+
+// Format implements the NodeFormatter interface.
+func (node RangePartition) Format(buf *bytes.Buffer, f FmtFlags) {
+	buf.WriteString(`PARTITION `)
+	FormatNode(buf, f, node.Name)
+	buf.WriteString(` VALUES LESS THAN `)
+	FormatNode(buf, f, node.Tuple)
+	if node.Subpartition != nil {
+		FormatNode(buf, f, node.Subpartition)
+	}
+}
+
 // CreateTable represents a CREATE TABLE statement.
 type CreateTable struct {
 	IfNotExists   bool
 	Table         NormalizableTableName
 	Interleave    *InterleaveDef
+	PartitionBy   *PartitionBy
 	Defs          TableDefs
 	AsSource      *Select
 	AsColumnNames NameList // Only to be used in conjunction with AsSource
@@ -673,6 +748,9 @@ func (node *CreateTable) Format(buf *bytes.Buffer, f FmtFlags) {
 		buf.WriteByte(')')
 		if node.Interleave != nil {
 			FormatNode(buf, f, node.Interleave)
+		}
+		if node.PartitionBy != nil {
+			FormatNode(buf, f, node.PartitionBy)
 		}
 	}
 }
