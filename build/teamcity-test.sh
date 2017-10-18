26cc5ed73adba7995564a1cc2d3567af498e3d9e
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-10-18 14:14:24 -0400

    fetch test binaries

--- build/teamcity-test.sh
+++ build/teamcity-test.sh
@@ -12,10 +12,5 @@ build/builder.sh env \
 	TARGET=stress \
 	github-pull-request-make
 
-build/builder.sh env \
-	TZ=America/New_York \
-	make test \
-	TESTFLAGS='-v' \
-	2>&1 \
-	| tee artifacts/test.log \
-	| go-test-teamcity
+build/builder.sh bash -c 'env TZ=America/New_York make test TESTFLAGS=-v &> artifacts/test.log'
+go-test-teamcity <artifacts/test.log
