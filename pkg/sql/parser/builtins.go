b1faab545fba723d5cc5a245ce7b08ff9fe13501
Dmitry Saveliev <d.e.saveliev@gmail.com>
2017-10-19 21:39:01 +0400

    sql: add builtin function for random UUID generation

--- pkg/sql/parser/builtins.go
+++ pkg/sql/parser/builtins.go
@@ -365,6 +365,20 @@ var Builtins = map[string][]Builtin{
 		},
 	},
 
+	"gen_random_uuid": {
+		Builtin{
+			Types:      ArgTypes{},
+			ReturnType: fixedReturnType(TypeUUID),
+			category:   categoryIDGeneration,
+			impure:     true,
+			fn: func(_ *EvalContext, _ Datums) (Datum, error) {
+				uv := uuid.MakeV4()
+				return NewDUuid(DUuid{uv}), nil
+			},
+			Info: "Generates a random UUID and returns it as a value of UUID type.",
+		},
+	},
+
 	"to_uuid": {
 		Builtin{
 			Types:      ArgTypes{{"val", TypeString}},
