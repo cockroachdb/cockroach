e21ec82ae90dba53de9f34d1f87f37df891d2bbf
Joey Pereira <joey@pereira.io>
2017-10-23 13:43:46 -0400

    sql: Fix DROP INDEX IF EXISTS idx_name nooping if any index doesn't exist
    This changes `DROP INDEX IF EXISTS idx_name` behaviour to not do an
    outright no-op plan if only one index doesn't exist. Instead, we only
    continue with indexes that exist (using the `idx_name` syntax, this does
    not pertain to `table_name@idx_name` syntax).

--- pkg/sql/drop.go
+++ pkg/sql/drop.go
@@ -242,8 +242,11 @@ type fullIndexName struct {
 //   Notes: postgres allows only the index owner to DROP an index.
 //          mysql requires the INDEX privilege on the table.
 func (p *planner) DropIndex(ctx context.Context, n *parser.DropIndex) (planNode, error) {
-	idxNames := make([]fullIndexName, len(n.IndexList))
-	for i, index := range n.IndexList {
+	// Keep a track of the indexes that exist to check. When the IF EXISTS
+	// options are provided, we will simply not include any indexes that
+	// don't exist and continue execution.
+	idxNames := make([]fullIndexName, 0, len(n.IndexList))
+	for _, index := range n.IndexList {
 		tn, err := p.expandIndexName(ctx, index, false /* requireTable */)
 		if err != nil {
 			return nil, err
@@ -251,8 +254,8 @@ func (p *planner) DropIndex(ctx context.Context, n *parser.DropIndex) (planNode,
 			// Only index names of the form "idx" throw an error here if they
 			// don't exist.
 			if n.IfExists {
-				// Noop.
-				return &zeroNode{}, nil
+				// Skip this index and don't return an error.
+				continue
 			}
 			// Index does not exist, but we want it to error out.
 			return nil, fmt.Errorf("index %q not found", index.Index)
@@ -267,8 +270,7 @@ func (p *planner) DropIndex(ctx context.Context, n *parser.DropIndex) (planNode,
 			return nil, err
 		}
 
-		idxNames[i].tn = tn
-		idxNames[i].idxName = index.Index
+		idxNames = append(idxNames, fullIndexName{tn: tn, idxName: index.Index})
 	}
 	return &dropIndexNode{n: n, idxNames: idxNames}, nil
 }
