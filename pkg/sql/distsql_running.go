b37e66b1d13c67e6bd2a3994b779ab684198b61f
Joey Pereira <joey@pereira.io>
2017-10-25 13:18:45 -0400

    sql: Unexport methods on DistSQLPlanner that don't escape pkg/sql
    These exported methods are used outside of the file they're defined, but
    not out of the packaged.

--- pkg/sql/distsql_running.go
+++ pkg/sql/distsql_running.go
@@ -439,7 +439,7 @@ func (dsp *DistSQLPlanner) PlanAndRun(
 	recv *distSQLReceiver,
 	evalCtx parser.EvalContext,
 ) error {
-	planCtx := dsp.NewPlanningCtx(ctx, txn)
+	planCtx := dsp.newPlanningCtx(ctx, txn)
 
 	log.VEvent(ctx, 1, "creating DistSQL plan")
 
