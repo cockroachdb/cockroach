2ad308c4f2035b1fbfa858376e8787a4216397d5
Jordan Lewis <jordanthelewis@gmail.com>
2017-10-25 19:28:40 -0400

    sql: evaluate placeholders in EXPLAIN EXECUTE ...
    `EXPLAIN EXECUTE` is somewhat of a special case of `EXPLAIN` queries.
    Normally in `EXPLAIN`, placeholders don't get assigned values and
    therefore should always be printed in placeholder marker format (`$1`
    etc).
    
    In `EXPLAIN EXECUTE`, however, the user has explicitly passed in
    placeholder values, and we want to output those values in `EXPLAIN`.
    Once placeholders aren't replaced, we'll need to make sure to evaluate
    them before printing. To make this happen, we change the formatter
    that's used to output expressions in `EXPLAIN EXECUTE` to dereference
    placeholders.

--- pkg/sql/explain.go
+++ pkg/sql/explain.go
@@ -142,8 +142,8 @@ func (p *planner) Explain(ctx context.Context, n *parser.Explain) (planNode, err
 	case explainPlan:
 		// We may want to show placeholder types, so ensure no values
 		// are missing.
-		p.semaCtx.Placeholders.FillUnassigned()
-		return p.makeExplainPlanNode(explainer, expanded, optimized, plan), nil
+		p.semaCtx.Placeholders.PermitUnassigned()
+		return p.makeExplainPlanNode(explainer, expanded, optimized, n.Statement, plan), nil
 
 	default:
 		return nil, fmt.Errorf("unsupported EXPLAIN mode: %d", mode)
