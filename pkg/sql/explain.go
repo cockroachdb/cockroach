e3bb332df50dc275e7cc11ede8d5411a678f6f74
Joey Pereira <joey@pereira.io>
2017-10-25 11:53:36 -0400

    sql: Change DistSQLPlanner to be exported
    This will let us initialize a single DistSQLPlanner in pkg/server.

--- pkg/sql/explain.go
+++ pkg/sql/explain.go
@@ -156,7 +156,7 @@ type explainDistSQLNode struct {
 	optColumnsSlot
 
 	plan           planNode
-	distSQLPlanner *distSQLPlanner
+	distSQLPlanner *DistSQLPlanner
 
 	// txn is the current transaction (used for the fake span resolver).
 	txn *client.Txn
