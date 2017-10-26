00548ba8aacb0e3404c1c9e5b8b75c649d82c01c
Tamir Duberstein <tamird@gmail.com>
2017-10-25 20:47:26 -0400

    storage: allocate the slice just once

--- pkg/storage/allocator.go
+++ pkg/storage/allocator.go
@@ -534,11 +534,8 @@ func (a Allocator) RebalanceTarget(
 			ReplicaID: rangeInfo.Desc.NextReplicaID,
 		}
 
-		// Deep-copy the Replicas slice since we'll mutate it.
 		desc := *rangeInfo.Desc
-		desc.Replicas = append([]roachpb.ReplicaDescriptor(nil), desc.Replicas...)
-
-		desc.Replicas = append(desc.Replicas, newReplica)
+		desc.Replicas = append(desc.Replicas[:len(desc.Replicas):len(desc.Replicas)], newReplica)
 		rangeInfo.Desc = &desc
 
 		replicaCandidates := simulateFilterUnremovableReplicas(
