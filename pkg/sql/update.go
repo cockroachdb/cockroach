2172cc773a01aea33edc612e6461578d13826afe
Bram Gruneir <bram@cockroachlabs.com>
2017-10-31 10:28:59 -0400

    sql: Adds ON DELETE CASCADE for foreign key references
    - Enables the addition of the ON DELETE CASCADE action for foreign keys references.
    - Augments the TablesNeededForFKs function to fetch all tables that might be needed.
    - Adds the cascader struct that sits as a coordinator between fk.go and rowwriter.go.
    - DeleteRow now preemtively deletes rows, cascades deletes, then checks to see if there are any fk violations. Note that his will add new intents on deletes.
    - Adds a new cascade logic test specifically for testing the convoluted cascade scenarios.
    
    There are a ton of improvements and additions that should happen here. This is just a MVP implementation.
    
    Follow up PRs will add memory monitoring and even more direct testing of the cascader.
    
    Release notes (sql): ON DELETE CASCADE foreign key constraints are fully supported

--- pkg/sql/update.go
+++ pkg/sql/update.go
@@ -104,8 +104,10 @@ func (p *planner) Update(
 		requestedCols = en.tableDesc.Columns
 	}
 
-	fkTables := sqlbase.TablesNeededForFKs(*en.tableDesc, sqlbase.CheckUpdates)
-	if err := p.fillFKTableMap(ctx, fkTables); err != nil {
+	fkTables, err := sqlbase.TablesNeededForFKs(
+		ctx, *en.tableDesc, sqlbase.CheckUpdates, p.lookupFKTable, p.CheckPrivilege,
+	)
+	if err != nil {
 		return nil, err
 	}
 	ru, err := sqlbase.MakeRowUpdater(p.txn, en.tableDesc, fkTables, updateCols,
