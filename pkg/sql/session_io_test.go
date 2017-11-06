05f1cde33d64648848d0ae41d34eb399ac0d157e
Andrei Matei <andrei@cockroachlabs.com>
2017-11-06 18:19:37 -0500

    sql: introduce utils for Session communication
    First parts of a larger Session rewrite. This commit contains i/o utils
    for a session to receive statements that it needs to run and to produce
    results that another module needs to deliver to the client.
    
    session_refactor.go contains a diagram hinting to how the various pieces
    will fit together.

---
+++ pkg/sql/session_io_test.go
@@ -0,0 +1,159 @@
+// Copyright 2017 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package sql
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/parser"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"golang.org/x/net/context"
+)
+
+func TestStmtBuf(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	ctx := context.TODO()
+	batch1, err := parser.Parse("SELECT 1; SELECT 2; SELECT 3;")
+	if err != nil {
+		t.Fatal(err)
+	}
+	batch2, err := parser.Parse("SELECT 4; SELECT 5; SELECT 6;")
+	if err != nil {
+		t.Fatal(err)
+	}
+	buf := newStmtBuf()
+	buf.push(batch1)
+	buf.push(batch2)
+
+	// Check that, while we don't manually advance the cursor, we keep getting the
+	// same statement.
+	expPos := cursorPosition{queryStrPos: 0, stmtIdx: 0}
+	for i := 0; i < 2; i++ {
+		stmt, pos := buf.curStmt(ctx)
+		if pos.compare(expPos) != 0 {
+			t.Fatalf("expected pos to be %s, got: %s", expPos, pos)
+		}
+		if stmt.String() != "SELECT 1" {
+			t.Fatalf("wrong statement: %s", stmt)
+		}
+	}
+
+	buf.advanceOne(ctx)
+	expPos.stmtIdx = 1
+	stmt, pos := buf.curStmt(ctx)
+	if pos.compare(expPos) != 0 {
+		t.Fatalf("expected pos to be %s, got: %s", expPos, pos)
+	}
+	if stmt.String() != "SELECT 2" {
+		t.Fatalf("wrong statement: %s", stmt)
+	}
+
+	buf.advanceOne(ctx)
+	expPos.stmtIdx = 2
+	stmt, pos = buf.curStmt(ctx)
+	if pos.compare(expPos) != 0 {
+		t.Fatalf("expected pos to be %s, got: %s", expPos, pos)
+	}
+	if stmt.String() != "SELECT 3" {
+		t.Fatalf("wrong statement: %s", stmt)
+	}
+
+	buf.advanceOne(ctx)
+	expPos.queryStrPos = 1
+	expPos.stmtIdx = 0
+	stmt, pos = buf.curStmt(ctx)
+	if pos.compare(expPos) != 0 {
+		t.Fatalf("expected pos to be %s, got: %s", expPos, pos)
+	}
+	if stmt.String() != "SELECT 4" {
+		t.Fatalf("wrong statement: %s", stmt)
+	}
+
+	// Now rewind to the middle of the first batch.
+	expPos.queryStrPos = 0
+	expPos.stmtIdx = 1
+	buf.rewind(ctx, expPos)
+	stmt, pos = buf.curStmt(ctx)
+	if pos.compare(expPos) != 0 {
+		t.Fatalf("expected pos to be %s, got: %s", expPos, pos)
+	}
+	if stmt.String() != "SELECT 2" {
+		t.Fatalf("wrong statement: %s", stmt)
+	}
+
+	// Now seek to the beginning of the second batch.
+	expPos.queryStrPos = 1
+	expPos.stmtIdx = 0
+	buf.seekToNextQueryStr()
+	stmt, pos = buf.curStmt(ctx)
+	if pos.compare(expPos) != 0 {
+		t.Fatalf("expected pos to be %s, got: %s", expPos, pos)
+	}
+	if stmt.String() != "SELECT 4" {
+		t.Fatalf("wrong statement: %s", stmt)
+	}
+}
+
+// Test that a reader blocked for an incoming statement is unblocked when that
+// statement arrives.
+func TestStmtBufSignal(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	buf := newStmtBuf()
+	batch, err := parser.Parse("SELECT 1; SELECT 2; SELECT 3;")
+	if err != nil {
+		t.Fatal(err)
+	}
+	go func() {
+		buf.push(batch)
+	}()
+
+	expPos := cursorPosition{queryStrPos: 0, stmtIdx: 0}
+	stmt, pos := buf.curStmt(context.TODO())
+	if pos.compare(expPos) != 0 {
+		t.Fatalf("expected pos to be %s, got: %s", expPos, pos)
+	}
+	if stmt.String() != "SELECT 1" {
+		t.Fatalf("wrong statement: %s", stmt)
+	}
+}
+
+func TestStmtBufLtrim(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	ctx := context.TODO()
+	buf := newStmtBuf()
+	for i := 0; i < 3; i++ {
+		batch, err := parser.Parse(
+			fmt.Sprintf("SELECT %d; SELECT %d;", i*2, i*2+1))
+		if err != nil {
+			t.Fatal(err)
+		}
+		buf.push(batch)
+	}
+	// Advance the cursor so that we can trim.
+	buf.seekToNextQueryStr()
+	buf.seekToNextQueryStr()
+	trimPos := cursorPosition{queryStrPos: 2, stmtIdx: 0}
+	buf.ltrim(ctx, trimPos)
+	if l := len(buf.mu.queryStrings); l != 1 {
+		t.Fatalf("expected 1 query string left, got: %d", l)
+	}
+	if s := buf.mu.startPos; s != 2 {
+		t.Fatalf("expected start pos 2, got: %d", s)
+	}
+}
---
+++ pkg/sql/session_refactor.go
@@ -0,0 +1,108 @@
+// Copyright 2017 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package sql
+
+// A SQL Session is in charge of executing queries received on a given client
+// connection. The Session implements a state machine (mostly defined by the
+// Postgres/pgwire session semantics). The state machine is supposed to run
+// asynchronously wrt the client connection: it receives input statements
+// through a statementBuffer and produces results through a clientComm
+// interface. The session maintains a cursor over the statementBuffer and
+// executes statements / produces results for one statement at a time. The
+// cursor points at all times to the statement that the Session is currently
+// executing. Results for statements before the cursor have already been
+// produced (but not necessarily delivered to the client). Statements after the
+// cursor are queued for future execution. Keeping already executed statements
+// in the buffer is useful in case of automatic retries (in which case
+// statements from the retried transaction have to be executed again); the
+// Session is in charge of removing old statements that are no longer needed for
+// retries from the (head of the) buffer.
+//
+// The Session has two main responsibilities: to dispatch queries to the
+// execution engine(s) and relay their results to the clientComm and to
+// implement the state machine maintaining the various aspects of a session's
+// state. The state machine implementation is further divided into two aspects:
+// maintaining the transaction status of the session (outside of a txn, inside a
+// txn, in an aborted txn, in a txn awaiting client restart, etc.) and
+// maintaining the cursor position indicating the next statement for the buffer
+// to be executed.
+//
+// The cursor normally advances one statement at a time, but it can also skip
+// some statements (remaining statements in a query string are skipped once an
+// error is encountered) and it can sometimes be rewound when performing
+// automatic retries. Rewinding can only be done if results for the rewound
+// statements have not actually been delivered to the client; see below.
+//
+//     +--------------------+
+//     |stmtBuf             |                       +-----------------+
+//     |                    |                       |Session          |
+//     |                    |                       |                 |
+//     | batches of stmts   |   statements are read |                 |
+//     | +-+-+ +-+-+ +-+-+  +----------------------->     +--------+  |
+//     | | | | | | | | | |  |                       |     |txnState|  |
+// +---> +-+-+ +++-+ +-+-+  |                       |     +--------+  |
+// |   |        ^           |                       |                 |
+// |   |        |   +-------------------------------+                 |
+// |   |        +   v       | cursor is advanced    |                 |
+// |   |       cursor       |           +           |                 |
+// |   +--------------------+           |           +------------+----+
+// |                                    |                        |
+// |                                    |                        |
+// +-------------+                      |                        |
+//               +-------+              |                        |
+//               | parser|              |results are produced    |
+//               +-------+              |                        |
+//               |                      |            +-----------v------+
+//               |                      |            |execution engine  |
+//               |                      |            |(local and DistSQL|
+//       +-------+------+               |            +------------------+
+//       | pgwire conn  |               |
+//       |              |               |
+//       |   +----------+               |
+//       |   |clientComm<---------------+
+//       |   +----------+
+//       |              |
+//       |              |
+//       |              |
+//       +--------------+
+//
+//
+// The Session is disconnected from client communication (i.e. generally network
+// communication - pgwire); the module doing client communication is responsible
+// for pushing statements into the buffer and for providing an implementation of
+// the clientConn interface (and thus consume results and send them to the
+// client). The Session does not control when results are delivered to the
+// client, but still it does have some influence over that; this is because of
+// the fact that the possibility of doing automatic retries is broken the moment
+// results for the transaction in question are delivered to the client. The
+// communication module has full freedom in sending results whenever it sees
+// fit; however the Session influences communication in the following ways:
+//
+// a) The Session calls clientComm.flush(), informing the implementer that all
+// the previous results can be delivered to the client at will.
+//
+// b) When deciding whether an automatic retry can be performed for a
+// transaction, the Session needs to
+//
+//   1) query the communication status to check that no results for the txn have
+//   been delivered to the client and, if this check passes:
+//   2) lock the communication so that no further results are delivered to the
+//   client, and, eventually:
+//   3) rewind the clientComm to a certain position corresponding to the start
+//   of the transaction, thereby discarding all the results that had been
+//   accumulated for the previous attempt to run the transaction in question.
+//
+// These steps are all orchestrated through clientComm.lockCommunication() and
+// rewindCapability{}.
