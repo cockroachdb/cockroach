b41ff553179921a64696c6dad52255912b64b935
Andrew Couch <andrew@cockroachlabs.com>
2017-11-06 16:16:51 -0500

    ui: demo of UI CCL component

---
+++ pkg/ui/ccl/src/views/shared/components/licenseType/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+ * LicenseType is an indicator showing the current build license.
+ */
+export default class LicenseType extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div>
+        <h1>CCL</h1>
+      </div>
+    );
+  }
+}
