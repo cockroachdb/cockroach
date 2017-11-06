ca0a43965b916e9c83595796803bf7a5cea931d4
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-11-05 22:47:46 -0500

    build: inline variables in protobuf.mk
    Prepare for Makefile merging by reducing the number of variables
    introduced in protobuf.mk. I find this easier to read anyway.

--- build/protobuf.mk
+++ build/protobuf.mk
@@ -18,27 +18,23 @@ REPO_ROOT := ./cockroach
 include $(REPO_ROOT)/build/common.mk
 
 NATIVE_ROOT := $(LIBROACH_SRC_DIR)/protos
-GITHUB_ROOT := $(REPO_ROOT)/vendor/github.com
 
-GOGO_PROTOBUF_PACKAGE := github.com/gogo/protobuf
-GOGO_PROTOBUF_PATH := $(GITHUB_ROOT)/../$(GOGO_PROTOBUF_PACKAGE)
+GOGO_PROTOBUF_PATH := $(REPO_ROOT)/vendor/github.com/gogo/protobuf
 PROTOBUF_PATH  := $(GOGO_PROTOBUF_PATH)/protobuf
 
-PLUGIN_SUFFIX   := gogoroach
-PROTOC_PLUGIN   := $(LOCAL_BIN)/protoc-gen-$(PLUGIN_SUFFIX)
+PROTOC_PLUGIN   := $(LOCAL_BIN)/protoc-gen-gogoroach
 GOGOPROTO_PROTO := $(GOGO_PROTOBUF_PATH)/gogoproto/gogo.proto
 
-COREOS_PATH := $(GITHUB_ROOT)/coreos
+COREOS_PATH := $(REPO_ROOT)/vendor/github.com/coreos
 COREOS_RAFT_PROTOS := $(addprefix $(COREOS_PATH)/etcd/raft/, $(sort $(shell git -C $(COREOS_PATH)/etcd/raft ls-files --exclude-standard --cached --others -- '*.proto')))
 
-GRPC_GATEWAY_PACKAGE := github.com/grpc-ecosystem/grpc-gateway
-GRPC_GATEWAY_GOOGLEAPIS_PACKAGE := $(GRPC_GATEWAY_PACKAGE)/third_party/googleapis
-GRPC_GATEWAY_GOOGLEAPIS_PATH := $(GITHUB_ROOT)/../$(GRPC_GATEWAY_GOOGLEAPIS_PACKAGE)
+GRPC_GATEWAY_GOOGLEAPIS_PACKAGE := github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis
+GRPC_GATEWAY_GOOGLEAPIS_PATH := $(REPO_ROOT)/vendor/$(GRPC_GATEWAY_GOOGLEAPIS_PACKAGE)
 
 # Map protobuf includes to the Go package containing the generated Go code.
 MAPPINGS :=
 MAPPINGS := $(MAPPINGS)Mgoogle/api/annotations.proto=$(GRPC_GATEWAY_GOOGLEAPIS_PACKAGE)/google/api,
-MAPPINGS := $(MAPPINGS)Mgoogle/protobuf/timestamp.proto=$(GOGO_PROTOBUF_PACKAGE)/types,
+MAPPINGS := $(MAPPINGS)Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,
 
 GW_SERVER_PROTOS := $(PKG_ROOT)/server/serverpb/admin.proto $(PKG_ROOT)/server/serverpb/status.proto $(PKG_ROOT)/server/serverpb/authentication.proto
 GW_TS_PROTOS := $(PKG_ROOT)/ts/tspb/timeseries.proto
@@ -60,19 +56,16 @@ CPP_PROTOS := $(filter %/roachpb/metadata.proto %/roachpb/data.proto %/roachpb/i
 CPP_HEADERS := $(subst ./,$(NATIVE_ROOT)/,$(CPP_PROTOS:%.proto=%.pb.h))
 CPP_SOURCES := $(subst ./,$(NATIVE_ROOT)/,$(CPP_PROTOS:%.proto=%.pb.cc))
 
-ORG_NAME := cockroachdb
-IMPORT_PREFIX := github.com/$(ORG_NAME)/
-
 GO_SOURCES_TARGET := $(LOCAL_BIN)/.go_protobuf_sources
 $(GO_SOURCES_TARGET): $(PROTOC) $(PROTOC_PLUGIN) $(GO_PROTOS) $(GOGOPROTO_PROTO)
 	(cd $(REPO_ROOT) && git ls-files --exclude-standard --cached --others -- '*.pb.go' | xargs rm -f)
 	for dir in $(sort $(dir $(GO_PROTOS))); do \
-	  $(PROTOC) -I.:$(GOGO_PROTOBUF_PATH):$(PROTOBUF_PATH):$(COREOS_PATH):$(GRPC_GATEWAY_GOOGLEAPIS_PATH) --plugin=$(PROTOC_PLUGIN) --$(PLUGIN_SUFFIX)_out=$(MAPPINGS),plugins=grpc,import_prefix=$(IMPORT_PREFIX):$(ORG_ROOT) $$dir/*.proto; \
+	  $(PROTOC) -I.:$(GOGO_PROTOBUF_PATH):$(PROTOBUF_PATH):$(COREOS_PATH):$(GRPC_GATEWAY_GOOGLEAPIS_PATH) --plugin=$(PROTOC_PLUGIN) --gogoroach_out=$(MAPPINGS),plugins=grpc,import_prefix=github.com/cockroachdb/:$(ORG_ROOT) $$dir/*.proto; \
 	done
 	$(SED_INPLACE) '/import _/d' $(GO_SOURCES)
-	$(SED_INPLACE) -E 's!import (fmt|math) "$(IMPORT_PREFIX)(fmt|math)"! !g' $(GO_SOURCES)
-	$(SED_INPLACE) -E 's!$(ORG_NAME)/(etcd)!coreos/\1!g' $(GO_SOURCES)
-	$(SED_INPLACE) -E 's!$(IMPORT_PREFIX)(bytes|encoding/binary|errors|fmt|io|math|github\.com|(google\.)?golang\.org)!\1!g' $(GO_SOURCES)
+	$(SED_INPLACE) -E 's!import (fmt|math) "github.com/cockroachdb/(fmt|math)"! !g' $(GO_SOURCES)
+	$(SED_INPLACE) -E 's!cockroachdb/(etcd)!coreos/\1!g' $(GO_SOURCES)
+	$(SED_INPLACE) -E 's!github.com/cockroachdb/(bytes|encoding/binary|errors|fmt|io|math|github\.com|(google\.)?golang\.org)!\1!g' $(GO_SOURCES)
 	gofmt -s -w $(GO_SOURCES)
 	touch $@
 
