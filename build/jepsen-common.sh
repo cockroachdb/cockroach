bc9fd209e6fbc00588172f56c23d9ecf0e98b94f
Ben Darnell <ben@cockroachlabs.com>
2017-11-08 10:22:25 -0500

    build: Expand jepsen test coverage
    Enable more nemeses: subcritical-skews, strobe-skews, split
    Enable more tests: bank-mulittable, g2
    
    Updates #19697

--- build/jepsen-common.sh
+++ build/jepsen-common.sh
@@ -38,22 +38,25 @@ function progress {
 }
 
 nemeses=(
-    # big-skews disabled since they assume an eth0 interface.
-    #"--nemesis big-skews"
     "--nemesis majority-ring"
-    "--nemesis start-stop-2"
+    "--nemesis split"
     "--nemesis start-kill-2"
-    #"--nemesis majority-ring --nemesis2 big-skews"
-    #"--nemesis big-skews --nemesis2 start-kill-2"
+    "--nemesis start-stop-2"
+    "--nemesis strobe-skews"
+    "--nemesis subcritical-skews"
+    "--nemesis majority-ring --nemesis2 subcritical-skews"
+    "--nemesis subcritical-skews --nemesis2 start-kill-2"
     "--nemesis majority-ring --nemesis2 start-kill-2"
     "--nemesis parts --nemesis2 start-kill-2"
 )
 
 tests=(
     "bank"
+    "bank-multitable"
     "comments"
     "register"
     "monotonic"
     "sets"
     "sequential"
+    "g2"
 )
