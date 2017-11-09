db146a52bbdc4c2146ec7c8e24d463618046c8e1
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-11-09 19:19:14 +0100

    sql/parser: move FunctionClass and Builtin to their own file

--- pkg/sql/parser/builtins.go
+++ pkg/sql/parser/builtins.go
@@ -62,23 +62,6 @@ var (
 
 const errInsufficientArgsFmtString = "unknown signature: %s()"
 
-// FunctionClass specifies the class of the builtin function.
-type FunctionClass int
-
-const (
-	// NormalClass is a standard builtin function.
-	NormalClass FunctionClass = iota
-	// AggregateClass is a builtin aggregate function.
-	AggregateClass
-	// WindowClass is a builtin window function.
-	WindowClass
-	// GeneratorClass is a builtin generator function.
-	GeneratorClass
-)
-
-// Avoid vet warning about unused enum value.
-var _ = NormalClass
-
 const (
 	categoryComparison    = "Comparison"
 	categoryCompatibility = "Compatibility"
@@ -89,80 +72,6 @@ const (
 	categoryArray         = "Array"
 	categorySystemInfo    = "System Info"
 )
-
-// Builtin is a built-in function.
-type Builtin struct {
-	Types      typeList
-	ReturnType returnTyper
-
-	// When multiple overloads are eligible based on types even after all of of
-	// the heuristics to pick one have been used, if one of the overloads is a
-	// Builtin with the `preferredOverload` flag set to true it can be selected
-	// rather than returning a no-such-method error.
-	// This should generally be avoided -- avoiding introducing ambiguous
-	// overloads in the first place is a much better solution -- and only done
-	// after consultation with @knz @nvanbenschoten.
-	preferredOverload bool
-
-	// Set to true when a function potentially returns a different value
-	// when called in the same statement with the same parameters.
-	// e.g.: random(), clock_timestamp(). Some functions like now()
-	// return the same value in the same statement, but different values
-	// in separate statements, and should not be marked as impure.
-	impure bool
-
-	// Set to true when a function depends on members of the EvalContext that are
-	// not marshalled by DistSQL (e.g. planner). Currently used for DistSQL to
-	// determine if expressions can be evaluated on a different node without
-	// sending over the EvalContext.
-	//
-	// TODO(andrei): Get rid of the planner from the EvalContext and then we can
-	// get rid of this blacklist.
-	distsqlBlacklist bool
-
-	// Set to true when a function's definition can handle NULL arguments. When
-	// set, the function will be given the chance to see NULL arguments. When not,
-	// the function will evaluate directly to NULL in the presence of any NULL
-	// arguments.
-	//
-	// NOTE: when set, a function should be prepared for any of its arguments to
-	// be NULL and should act accordingly.
-	nullableArgs bool
-
-	// Set to true when a function may change at every row whether or
-	// not it is applied to an expression that contains row-dependent
-	// variables. Used e.g. by `random` and aggregate functions.
-	needsRepeatedEvaluation bool
-
-	// Set to true when the built-in can only be used by security.RootUser.
-	privileged bool
-
-	class    FunctionClass
-	category string
-
-	// Info is a description of the function, which is surfaced on the CockroachDB
-	// docs site on the "Functions and Operators" page. Descriptions typically use
-	// third-person with the function as an implicit subject (e.g. "Calculates
-	// infinity"), but should focus more on ease of understanding so other structures
-	// might be more appropriate.
-	Info string
-
-	AggregateFunc func([]types.T, *EvalContext) AggregateFunc
-	WindowFunc    func([]types.T, *EvalContext) WindowFunc
-	fn            func(*EvalContext, Datums) (Datum, error)
-}
-
-func (b Builtin) params() typeList {
-	return b.Types
-}
-
-func (b Builtin) returnType() returnTyper {
-	return b.ReturnType
-}
-
-func (b Builtin) preferred() bool {
-	return b.preferredOverload
-}
 
 func categorizeType(t types.T) string {
 	switch t {
@@ -175,46 +84,6 @@ func categorizeType(t types.T) string {
 	default:
 		return strings.ToUpper(t.String())
 	}
-}
-
-// Category is used to categorize a function (for documentation purposes).
-func (b Builtin) Category() string {
-	return b.category
-}
-
-// Class returns the FunctionClass of this builtin.
-func (b Builtin) Class() FunctionClass {
-	return b.class
-}
-
-// Impure returns false if this builtin is a pure function of its inputs.
-func (b Builtin) Impure() bool {
-	return b.impure
-}
-
-// DistSQLBlacklist returns true if the builtin is not supported by DistSQL.
-// See distsqlBlacklist.
-func (b Builtin) DistSQLBlacklist() bool {
-	return b.distsqlBlacklist
-}
-
-// Fn returns the Go function which implements the builtin.
-func (b Builtin) Fn() func(*EvalContext, Datums) (Datum, error) {
-	return b.fn
-}
-
-// FixedReturnType returns a fixed type that the function returns, returning Any
-// if the return type is based on the function's arguments.
-func (b Builtin) FixedReturnType() types.T {
-	if b.ReturnType == nil {
-		return nil
-	}
-	return returnTypeToFixedType(b.ReturnType)
-}
-
-// Signature returns a human-readable signature.
-func (b Builtin) Signature() string {
-	return fmt.Sprintf("(%s) -> %s", b.Types.String(), b.FixedReturnType())
 }
 
 var digitNames = []string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
