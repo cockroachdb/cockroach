0e37550fd6e3c12e93ea108a71e94a21200e7138
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-11-09 17:52:32 -0500

    *: clarify licensing
    Move all licenses in the codebase into a licenses folder, and add a note
    explaining the situation in LICENSES. Adjust comments that refer to a
    particular license file to point to the proper location.
    
    Fix #19263.

--- pkg/util/interval/llrb_based_interval.go
+++ pkg/util/interval/llrb_based_interval.go
@@ -1,6 +1,6 @@
 // Copyright ©2012 The bíogo Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in licenses/BSD-biogo.txt.
 
 package interval
 
