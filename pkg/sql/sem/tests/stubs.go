218b6dd717541c4186690745fa14f1dfda79e430
Raphael 'kena' Poss <knz@cockroachlabs.com>
2017-11-09 16:45:31 +0100

    sql/sem/tests: new test folder for tests that need multiple packages
    This folder is intended for use by tests from the `sql/parser` package
    that have dependencies on stuff from both `parser` and other packages
    that will be split off `parser` but depend on it.
    
    A separate test directory is needed to avoid errors from the Go
    compiler about dependency cycles.
    
    The directory is initially populated with stub aliases from the
    `parser` package so that git more readily recognizes file moves.

---
+++ pkg/sql/sem/tests/stubs.go
@@ -0,0 +1,77 @@
+// Copyright 2017 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package tests
+
+import "github.com/cockroachdb/cockroach/pkg/sql/parser"
+
+// AsStringWithFlags is a temporary alias.
+var AsStringWithFlags = parser.AsStringWithFlags
+
+// FmtAnonymize is a temporary alias.
+var FmtAnonymize = parser.FmtAnonymize
+
+// FmtFlags is a temporary alias.
+type FmtFlags = parser.FmtFlags
+
+// FmtSimpleWithPasswords is a temporary alias.
+var FmtSimpleWithPasswords = parser.FmtSimpleWithPasswords
+
+// FmtShowTypes is a temporary alias.
+var FmtShowTypes = parser.FmtShowTypes
+
+// FmtBareStrings is a temporary alias.
+var FmtBareStrings = parser.FmtBareStrings
+
+// FmtArrays is a temporary alias.
+var FmtArrays = parser.FmtArrays
+
+// FmtBareIdentifiers is a temporary alias.
+var FmtBareIdentifiers = parser.FmtBareIdentifiers
+
+// FmtParsable is a temporary alias.
+var FmtParsable = parser.FmtParsable
+
+// FmtCheckEquivalence is a temporary alias.
+var FmtCheckEquivalence = parser.FmtCheckEquivalence
+
+// FmtHideConstants is a temporary alias.
+var FmtHideConstants = parser.FmtHideConstants
+
+// FmtSimple is a temporary alias.
+var FmtSimple = parser.FmtSimpleQualified
+
+// FmtSimpleQualified is a temporary alias.
+var FmtSimpleQualified = parser.FmtSimpleQualified
+
+// FmtAlwaysGroupExprs is a temporary alias.
+var FmtAlwaysGroupExprs = parser.FmtAlwaysGroupExprs
+
+// FmtReformatTableNames is a temporary alias.
+var FmtReformatTableNames = parser.FmtReformatTableNames
+
+// MakeSemaContext is a temporary alias.
+var MakeSemaContext = parser.MakeSemaContext
+
+// NormalizableTableName is a temporary alias.
+type NormalizableTableName = parser.NormalizableTableName
+
+// ParseExpr is a temporary alias.
+var ParseExpr = parser.ParseExpr
+
+// ParseOne is a temporary alias.
+var ParseOne = parser.ParseOne
+
+// TypeCheck is a temporary alias.
+var TypeCheck = parser.TypeCheck
