c88baf86655795291af896fd3e293192abd79d06
Matt Tracy <matt@cockroachlabs.com>
2017-11-14 16:48:46 -0500

    ui: Initial Commit of Cluster Visualization Work
    Adds an initial version of cluster visualization, worked on by both
    myself and Spencer.
    
    This code cannot currently be accessed because it is under the CCL
    license tree, but our build system does not yet link CCL code.
    
    Currently the rewrite is not yet at parity with the original prototype.
    It does not:
    - Group nodes into localities
    - Display numeric values for traffic between node pairs
    - Show locality links for only the currently hovered node (all links are
    shown).
    
    The rewrite also uses simulated location data; nodes on the cluster are
    assigned simulated long/lat pairs in order to place them on the map for
    visual effect.
    
    Note that it is likely that the rewrite will begin to diverge from the
    prototype somewhat at this point, and complete parity should not be
    considered a upcoming milestone.
    
    Release Notes: none

---
+++ pkg/ui/ccl/src/views/clusterviz/containers/map/statusHistory.ts
@@ -0,0 +1,56 @@
+// Copyright 2017 The Cockroach Authors.
+//
+// Licensed under the Cockroach Community Licence (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/LICENSE
+
+import * as protos from "src/js/protos";
+import { NanoToMilli } from "src/util/convert";
+
+type NodeStatus = protos.cockroach.server.status.NodeStatus$Properties;
+
+export default class NodeStatusHistory {
+    clientActivityRate: number;
+    private statusHistory: NodeStatus[];
+    private maxHistory = 2;
+
+    constructor(initialStatus: NodeStatus) {
+        this.statusHistory = [initialStatus];
+        this.computeClientActivityRate();
+    }
+
+    update(nextStatus: NodeStatus) {
+        if (this.statusHistory[0].updated_at.lessThan(nextStatus.updated_at)) {
+            this.statusHistory.unshift(nextStatus);
+            if (this.statusHistory.length > this.maxHistory) {
+                this.statusHistory.pop();
+            }
+
+            this.computeClientActivityRate();
+        }
+    }
+
+    id() {
+        return this.statusHistory[0].desc.node_id;
+    }
+
+    latest() {
+        return this.statusHistory[0];
+    }
+
+    private computeClientActivityRate() {
+        this.clientActivityRate = 0;
+        if (this.statusHistory.length > 1) {
+            const [latest, prev] = this.statusHistory;
+            const seconds = NanoToMilli(latest.updated_at.subtract(prev.updated_at).toNumber()) / 1000;
+            const totalQPS = (latest.metrics["sql.select.count"] - prev.metrics["sql.select.count"]) +
+                (latest.metrics["sql.distsql.select.count"] - prev.metrics["sql.distsql.select.count"]) +
+                (latest.metrics["sql.update.count"] - prev.metrics["sql.update.count"]) +
+                (latest.metrics["sql.insert.count"] - prev.metrics["sql.insert.count"]) +
+                (latest.metrics["sql.delete.count"] - prev.metrics["sql.delete.count"]);
+            this.clientActivityRate = totalQPS / seconds;
+        }
+    }
+}
