e8182b0c94f9a55eabe7c48300b8b963c9234026
Radu Berinde <radu@cockroachlabs.com>
2017-11-16 14:45:26 -0500

    util: improve FastIntSet.String
    We now show runs of consecutive integers as ranges.

--- pkg/util/fast_int_set.go
+++ pkg/util/fast_int_set.go
@@ -357,18 +357,42 @@ func (s *FastIntSet) Shift(delta int) FastIntSet {
 	return result
 }
 
+// String returns a list representation of elements. Sequential runs of positive
+// numbers are shown as ranges. For example:
+// MakeFastIntSet
 func (s FastIntSet) String() string {
 	var buf bytes.Buffer
 	buf.WriteByte('(')
-	first := true
-	s.ForEach(func(i int) {
-		if first {
-			first = false
-		} else {
+	appendRange := func(start, end int) {
+		if buf.Len() > 1 {
 			buf.WriteByte(',')
 		}
-		fmt.Fprintf(&buf, "%d", i)
+		if start == end {
+			fmt.Fprintf(&buf, "%d", start)
+		} else if start+1 == end {
+			fmt.Fprintf(&buf, "%d,%d", start, end)
+		} else {
+			fmt.Fprintf(&buf, "%d-%d", start, end)
+		}
+	}
+	rangeStart, rangeEnd := -1, -1
+	s.ForEach(func(i int) {
+		if i < 0 {
+			appendRange(i, i)
+			return
+		}
+		if rangeStart != -1 && rangeEnd == i-1 {
+			rangeEnd = i
+		} else {
+			if rangeStart != -1 {
+				appendRange(rangeStart, rangeEnd)
+			}
+			rangeStart, rangeEnd = i, i
+		}
 	})
+	if rangeStart != -1 {
+		appendRange(rangeStart, rangeEnd)
+	}
 	buf.WriteByte(')')
 	return buf.String()
 }
