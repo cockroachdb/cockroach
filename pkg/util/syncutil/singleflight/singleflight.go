dfd20a10cafb9f6122024050c9adb319f335bbd1
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-11-17 15:44:41 -0500

    *: standardize license boilerplate on derived code
    Consistently format our license boilerplate in files in which the
    original copyright does not belong to The Cockroach Authors. All files
    in which we've made substantial modifications now note that we hold the
    copyright on the modifications and that they're licensed under the APL.

--- pkg/util/syncutil/singleflight/singleflight.go
+++ pkg/util/syncutil/singleflight/singleflight.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in licenses/BSD-golang.txt.
 
+// This code originated in Go's internal/singleflight package.
+
 // Package singleflight provides a duplicate function call suppression
 // mechanism.
 package singleflight
