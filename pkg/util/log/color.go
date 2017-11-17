ee0aff713c37d0ebd38a1cf11eb3530bee7c1bbd
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-11-17 15:44:41 -0500

    *: standardize license boilerplate on derived code
    Consistently format our license boilerplate in files in which the
    original copyright does not belong to The Cockroach Authors. All files
    in which we've made substantial modifications now note that we hold the
    copyright on the modifications and that they're licensed under the APL.

--- pkg/util/log/color.go
+++ pkg/util/log/color.go
@@ -13,6 +13,8 @@
 // implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
+// This code originated in the github.com/golang/glog package.
+
 package log
 
 import (
--- pkg/util/syncutil/singleflight/singleflight_test.go
+++ pkg/util/syncutil/singleflight/singleflight_test.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in licenses/BSD-golang.txt.
 
+// This code originated in Go's internal/singleflight package.
+
 package singleflight
 
 import (
