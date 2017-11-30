60244f9a24a5781d537b100ffec67d4accca028d
Matt Jibson <matt.jibson@gmail.com>
2017-11-30 15:31:46 -0500

    sqlccl: allow canceling of IMPORT jobs
    Special case import jobs in the job cancel function. This allows them
    to be marked as canceled, even though they are not resumable. This
    is a short term fix (for some unknown definition of short term) until
    the jobs API can be yet again improved to support jobs that start jobs.
    
    IMPORT needed to be taught about monitoring the progress callback so
    it could cancel itself.
    
    While here, improve the error message around attempting to
    pause/cancel/resume schema change jobs.
    
    Release note (sql change): allow canceling of IMPORT jobs.

--- pkg/sql/jobs/jobs_test.go
+++ pkg/sql/jobs/jobs_test.go
@@ -1022,6 +1022,21 @@ func TestJobLifecycle(t *testing.T) {
 			t.Fatal(err)
 		}
 	})
+
+	t.Run("cannot pause or cancel uncontrollable jobs", func(t *testing.T) {
+		job, _ := createJob(jobs.Record{
+			Details: jobs.SchemaChangeDetails{},
+		})
+		if err := registry.Pause(ctx, nil, *job.ID()); !testutils.IsError(err, "is not controllable") {
+			t.Fatalf("unexpected %v", err)
+		}
+		if err := registry.Cancel(ctx, nil, *job.ID()); !testutils.IsError(err, "is not controllable") {
+			t.Fatalf("unexpected %v", err)
+		}
+		if err := registry.Resume(ctx, nil, *job.ID()); !testutils.IsError(err, "is not controllable") {
+			t.Fatalf("unexpected %v", err)
+		}
+	})
 }
 
 func TestRunAndWaitForTerminalState(t *testing.T) {
