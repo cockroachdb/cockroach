d49013e84fd46775e9e1a0095130cc25425bde3a
Radu Berinde <radu@cockroachlabs.com>
2017-12-06 15:21:29 -0500

    opt: introduce optimizer expression trees and build them from TypedExprs
    This change brings in a subset of
    https://github.com/petermattis/opttoy/tree/master/v3
    
    This change introduces:
    
     - the expr tree: cascades-style optimizers operate on expression
       trees which can represent both scalar and relational expressions;
       this is a departure from the way we represent expressions and
       statements (sem/tree) so we need a new tree structure.
    
     - scalar operators: initially, we focus only on scalar expressions.
    
     - building an expr tree from a sem/tree.TypedExpr.
    
     - opt version of logic tests
    
    See the RFC in #19135 for more context on the optimizer.
    
    This is the first step of an initial project related to the optimizer:
    generating index constraints from scalar expressions. This will be a
    rewrite of the current index constraint generation code (which has
    many problems, see #6346).  Roughly, the existing
    `makeIndexConstraints` will call into the optimizer with a `TypedExpr`
    and the optimizer will return index constraints.
    
    Release note: None

---
+++ pkg/sql/opt/scalar.go
@@ -0,0 +1,138 @@
+// Copyright 2017 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package opt
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/types"
+	"github.com/cockroachdb/cockroach/pkg/util/treeprinter"
+)
+
+func init() {
+	scalarOpNames := map[operator]string{
+		variableOp:          "variable",
+		constOp:             "const",
+		listOp:              "list",
+		orderedListOp:       "ordered-list",
+		andOp:               "and",
+		orOp:                "or",
+		notOp:               "not",
+		eqOp:                "eq",
+		ltOp:                "lt",
+		gtOp:                "gt",
+		leOp:                "le",
+		geOp:                "ge",
+		neOp:                "ne",
+		inOp:                "in",
+		notInOp:             "not-in",
+		likeOp:              "like",
+		notLikeOp:           "not-like",
+		iLikeOp:             "ilike",
+		notILikeOp:          "not-ilike",
+		similarToOp:         "similar-to",
+		notSimilarToOp:      "not-similar-to",
+		regMatchOp:          "regmatch",
+		notRegMatchOp:       "not-regmatch",
+		regIMatchOp:         "regimatch",
+		notRegIMatchOp:      "not-regimatch",
+		isDistinctFromOp:    "is-distinct-from",
+		isNotDistinctFromOp: "is-not-distinct-from",
+		isOp:                "is",
+		isNotOp:             "is-not",
+		anyOp:               "any",
+		someOp:              "some",
+		allOp:               "all",
+		bitandOp:            "bitand",
+		bitorOp:             "bitor",
+		bitxorOp:            "bitxor",
+		plusOp:              "plus",
+		minusOp:             "minus",
+		multOp:              "mult",
+		divOp:               "div",
+		floorDivOp:          "floor-div",
+		modOp:               "mod",
+		powOp:               "pow",
+		concatOp:            "concat",
+		lShiftOp:            "lshift",
+		rShiftOp:            "rshift",
+		unaryPlusOp:         "unary-plus",
+		unaryMinusOp:        "unary-minus",
+		unaryComplementOp:   "complement",
+		functionCallOp:      "func",
+	}
+
+	for op, name := range scalarOpNames {
+		registerOperator(op, name, scalarClass{})
+	}
+}
+
+// scalarProps are properties specific to scalar operators.
+type scalarProps struct {
+	typ types.T
+}
+
+type scalarClass struct{}
+
+var _ operatorClass = scalarClass{}
+
+func (scalarClass) format(e *expr, tp treeprinter.Node) {
+	var buf bytes.Buffer
+	fmt.Fprintf(&buf, "%v", e.op)
+	if e.private != nil {
+		fmt.Fprintf(&buf, " (%v)", e.private)
+	}
+	fmt.Fprintf(&buf, " (type: %s)", e.scalarProps.typ)
+	n := tp.Child(buf.String())
+	formatExprs(n, "inputs", e.inputs())
+}
+
+// The following initializers are called on an already allocated expression
+// node. The scalarProps are be initialized separately.
+
+// initConstExpr initializes a constOp expression node.
+func initConstExpr(e *expr, datum tree.Datum) {
+	e.op = constOp
+	e.private = datum
+}
+
+// initFunctionCallExpra initializes a functionCallOp expression node.
+func initFunctionCallExpr(e *expr, def *tree.FunctionDefinition, children []*expr) {
+	e.op = functionCallOp
+	e.children = children
+	e.private = def
+}
+
+// initUnaryExpr initializes expression nodes for operators with a single input.
+func initUnaryExpr(e *expr, op operator, input *expr) {
+	e.op = op
+	e.children = []*expr{input}
+}
+
+// initBinaryExpr initializes expression nodes for operators with two inputs.
+func initBinaryExpr(e *expr, op operator, input1 *expr, input2 *expr) {
+	e.op = op
+	e.children = []*expr{input1, input2}
+}
+
+// initVariableExpr initializes a variableOp expression node.
+// TODO(radu): currently variableOp refers to a column index (derived from an
+// IndexedVar).
+func initVariableExpr(e *expr, index int) {
+	e.op = variableOp
+	e.private = index
+}
