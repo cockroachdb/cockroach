eb8899fcab990d7b6c84dbc8bfdfa3565ebe5bbb
Radu Berinde <radu@cockroachlabs.com>
2017-12-06 15:21:29 -0500

    opt: introduce optimizer expression trees and build them from TypedExprs
    This change brings in a subset of
    https://github.com/petermattis/opttoy/tree/master/v3
    
    This change introduces:
    
     - the expr tree: cascades-style optimizers operate on expression
       trees which can represent both scalar and relational expressions;
       this is a departure from the way we represent expressions and
       statements (sem/tree) so we need a new tree structure.
    
     - scalar operators: initially, we focus only on scalar expressions.
    
     - building an expr tree from a sem/tree.TypedExpr.
    
     - opt version of logic tests
    
    See the RFC in #19135 for more context on the optimizer.
    
    This is the first step of an initial project related to the optimizer:
    generating index constraints from scalar expressions. This will be a
    rewrite of the current index constraint generation code (which has
    many problems, see #6346).  Roughly, the existing
    `makeIndexConstraints` will call into the optimizer with a `TypedExpr`
    and the optimizer will return index constraints.
    
    Release note: None

---
+++ pkg/sql/opt/expr.go
@@ -0,0 +1,70 @@
+// Copyright 2017 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package opt
+
+import "github.com/cockroachdb/cockroach/pkg/util/treeprinter"
+
+// expr implements the node of a unified expressions tree for both relational
+// and scalar expressions in a query.
+//
+// Expressions have optional inputs. Expressions also maintain properties. For
+// scalar expressions, the properties are stored in scalarProps.
+//
+// Currently, it only supports scalar expressions and operators. More
+// information pertaining to relational operators will be added when they are
+// supported.
+//
+// TODO(radu): support relational operators and extend this description.
+type expr struct {
+	op operator
+	// Child expressions. The interpretation of the children is operator
+	// dependent.
+	children []*expr
+	// Scalar properties (properties that pertain only to scalar operators).
+	scalarProps *scalarProps
+	// Operator-dependent data used by this expression. For example, constOp
+	// stores a pointer to the constant value.
+	private interface{}
+}
+
+func (e *expr) opClass() operatorClass {
+	return operatorTab[e.op].class
+}
+
+func (e *expr) inputs() []*expr {
+	return e.children
+}
+
+func formatExprs(tp treeprinter.Node, title string, exprs []*expr) {
+	if len(exprs) > 0 {
+		n := tp.Child(title)
+		for _, e := range exprs {
+			if e != nil {
+				e.format(n)
+			}
+		}
+	}
+}
+
+// format is part of the operatorClass interface.
+func (e *expr) format(tp treeprinter.Node) {
+	e.opClass().format(e, tp)
+}
+
+func (e *expr) String() string {
+	tp := treeprinter.New()
+	e.format(tp)
+	return tp.String()
+}
