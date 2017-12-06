03961a520f85ee4a274a281daa691fb8a356a9d5
marc <marc@cockroachlabs.com>
2017-12-06 15:24:01 -0500

    libroach: clang-format C++ code.
    Release note: None
    
    Still testing.
    I've grabbed the default (LLVM) format from clang-format using
    `clang-format -dump-config` and changed the following:
    
    $ diff original.format .clang-format
    3,4c3
    < AccessModifierOffset: -2
    ---
    > AccessModifierOffset: -1
    39c38
    < ColumnLimit:     80
    ---
    > ColumnLimit:     120
    41c40
    < ConstructorInitializerAllOnOneLineOrOnePerLine: false
    ---
    > ConstructorInitializerAllOnOneLineOrOnePerLine: true
    80c79
    < SpacesBeforeTrailingComments: 1
    ---
    > SpacesBeforeTrailingComments: 2
    
    The config is described at
    https://clang.llvm.org/docs/ClangFormatStyleOptions.html
    
    I'd like to iterate on this a bit and ideally have a lint check for
    this. I've found I cannot live without auto-formatters anymore.

--- c-deps/libroach/encoding.cc
+++ c-deps/libroach/encoding.cc
@@ -12,48 +12,45 @@
 // implied.  See the License for the specific language governing
 // permissions and limitations under the License.
 
-#include "rocksdb/slice.h"
 #include "encoding.h"
+#include "rocksdb/slice.h"
 
 // TODO(benesch): Set up a CI pipeline to test these functions.
 
-void EncodeUint32(std::string* buf, uint32_t v) {
+void EncodeUint32(std::string *buf, uint32_t v) {
   const uint8_t tmp[sizeof(v)] = {
-    uint8_t(v >> 24), uint8_t(v >> 16), uint8_t(v >> 8), uint8_t(v),
+      uint8_t(v >> 24), uint8_t(v >> 16), uint8_t(v >> 8), uint8_t(v),
   };
-  buf->append(reinterpret_cast<const char*>(tmp), sizeof(tmp));
+  buf->append(reinterpret_cast<const char *>(tmp), sizeof(tmp));
 }
 
-void EncodeUint64(std::string* buf, uint64_t v) {
+void EncodeUint64(std::string *buf, uint64_t v) {
   const uint8_t tmp[sizeof(v)] = {
-    uint8_t(v >> 56), uint8_t(v >> 48), uint8_t(v >> 40), uint8_t(v >> 32),
-    uint8_t(v >> 24), uint8_t(v >> 16), uint8_t(v >> 8), uint8_t(v),
+      uint8_t(v >> 56), uint8_t(v >> 48), uint8_t(v >> 40), uint8_t(v >> 32),
+      uint8_t(v >> 24), uint8_t(v >> 16), uint8_t(v >> 8),  uint8_t(v),
   };
-  buf->append(reinterpret_cast<const char*>(tmp), sizeof(tmp));
+  buf->append(reinterpret_cast<const char *>(tmp), sizeof(tmp));
 }
 
-bool DecodeUint32(rocksdb::Slice* buf, uint32_t* value) {
+bool DecodeUint32(rocksdb::Slice *buf, uint32_t *value) {
   const int N = sizeof(*value);
   if (buf->size() < N) {
     return false;
   }
-  const uint8_t* b = reinterpret_cast<const uint8_t*>(buf->data());
-  *value = (uint32_t(b[0]) << 24) | (uint32_t(b[1]) << 16) |
-      (uint32_t(b[2]) << 8) | uint32_t(b[3]);
+  const uint8_t *b = reinterpret_cast<const uint8_t *>(buf->data());
+  *value = (uint32_t(b[0]) << 24) | (uint32_t(b[1]) << 16) | (uint32_t(b[2]) << 8) | uint32_t(b[3]);
   buf->remove_prefix(N);
   return true;
 }
 
-bool DecodeUint64(rocksdb::Slice* buf, uint64_t* value) {
+bool DecodeUint64(rocksdb::Slice *buf, uint64_t *value) {
   const int N = sizeof(*value);
   if (buf->size() < N) {
     return false;
   }
-  const uint8_t* b = reinterpret_cast<const uint8_t*>(buf->data());
-  *value = (uint64_t(b[0]) << 56) | (uint64_t(b[1]) << 48) |
-      (uint64_t(b[2]) << 40) | (uint64_t(b[3]) << 32) |
-      (uint64_t(b[4]) << 24) | (uint64_t(b[5]) << 16) |
-      (uint64_t(b[6]) << 8) | uint64_t(b[7]);
+  const uint8_t *b = reinterpret_cast<const uint8_t *>(buf->data());
+  *value = (uint64_t(b[0]) << 56) | (uint64_t(b[1]) << 48) | (uint64_t(b[2]) << 40) | (uint64_t(b[3]) << 32) |
+           (uint64_t(b[4]) << 24) | (uint64_t(b[5]) << 16) | (uint64_t(b[6]) << 8) | uint64_t(b[7]);
   buf->remove_prefix(N);
   return true;
 }
