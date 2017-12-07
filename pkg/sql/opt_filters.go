c38a30dd7e8a5f783350111dfa0cef92c488e2ab
Adam Yao <adam.yao.qinglin@gmail.com>
2017-12-07 16:05:28 +0800

    sql/distsql: push down NOT NULL constraints on equality columns during planning
    We can push down a NOT NULL filter depending on the join type.
    This can reduce the data that is streamed to the join.
    
    All inner joins
    When doing a left or right outer join, we can push the constraint down to the other side of the join
    (e.g. when doing a LEFT OUTER JOIN, we can push it down the right side of the join)
    
    Release note: None

--- pkg/sql/opt_filters.go
+++ pkg/sql/opt_filters.go
@@ -540,8 +540,13 @@ func (p *planner) addRenderFilter(
 //
 // The expanded expression can then be split as necessary.
 func expandOnCond(n *joinNode, cond tree.TypedExpr, evalCtx *tree.EvalContext) tree.TypedExpr {
-	if isFilterTrue(cond) || len(n.pred.leftEqualityIndices) == 0 {
+	if len(n.pred.leftEqualityIndices) == 0 {
 		return cond
+	}
+	// Add constrains on left/rightEqualityIndices
+	notNullConj := addEqualNotNullConj(n)
+	if isFilterTrue(cond) {
+		return notNullConj
 	}
 	numLeft := len(n.left.info.sourceColumns)
 
@@ -634,6 +639,39 @@ func expandOnCond(n *joinNode, cond tree.TypedExpr, evalCtx *tree.EvalContext) t
 		// calls consumed it completely.
 		if !isFilterTrue(leftRemainder) && !isFilterTrue(rightRemainder) {
 			result = mergeConj(result, e)
+		}
+	}
+	result = mergeConj(result, notNullConj)
+
+	return result
+}
+
+// addEqualNotNullConj loops over the left/rightEqualityIndices to construct
+// not-null constraint expressions.
+// For Inner Join, both left and right column should fetch not-null values.
+// For Left Join, right column should fetch not-null values.
+// For Right Join, left column should fetch not-null values.
+// Examples: SELECT * FROM t1 AS l INNER JOIN t1 AS r ON l.id = r.id
+// result: SELECT * FROM t1 AS l INNER JOIN t1 AS r ON l.id = r.id
+// AND l.id IS NOT NULL AND r.id IS NOT NULL
+func addEqualNotNullConj(n *joinNode) tree.TypedExpr {
+	// left equality not null conj
+	var result tree.TypedExpr = tree.DBoolTrue
+	if n.joinType == joinTypeInner || n.joinType == joinTypeRightOuter {
+		for _, c := range n.pred.leftEqualityIndices {
+			leftIndexedVar := n.pred.iVarHelper.IndexedVar(c)
+			leftNotNull := tree.NewTypedComparisonExpr(tree.IsDistinctFrom, leftIndexedVar, tree.DNull)
+			result = mergeConj(result, leftNotNull)
+		}
+	}
+
+	// right equality not null conj
+	if n.joinType == joinTypeInner || n.joinType == joinTypeLeftOuter {
+		for _, c := range n.pred.rightEqualityIndices {
+			c = c + len(n.left.info.sourceColumns)
+			rightIndexedVar := n.pred.iVarHelper.IndexedVar(c)
+			rightNotNull := tree.NewTypedComparisonExpr(tree.IsDistinctFrom, rightIndexedVar, tree.DNull)
+			result = mergeConj(result, rightNotNull)
 		}
 	}
 	return result
