bd27ee122678b24e7ca36cb4cb5a77669c1a806f
marc <marc@cockroachlabs.com>
2017-12-09 11:37:31 -0500

    libroach: add unittest targets for libroach
    Release note: None
    
    This adds a new `testlibroach` target which builds the tests and runs
    them. Tests currently do nothing, this is just the framework.
    
    The next step is probably to get
    [gtest](https://github.com/google/googletest) hooked up, who wants to
    write tests the old-fashioned way anyway?
    
    ```
    $ make testlibroach
    GOPATH set to /home/marc/cockroach
    Running make with -j8
    [ 71%] Built target roach
    Scanning dependencies of target libroach_test
    [ 76%] Building CXX object
    CMakeFiles/libroach_test.dir/libroach_test.cc.o
    [ 85%] Built target roachccl
    Scanning dependencies of target libroach_ccl_test
    [ 90%] Building CXX object
    CMakeFiles/libroach_ccl_test.dir/ccl/libroach_ccl_test.cc.o
    [ 95%] Linking CXX executable libroach_test
    [100%] Linking CXX executable libroach_ccl_test
    [100%] Built target libroach_test
    [100%] Built target libroach_ccl_test
    Scanning dependencies of target check
    Test project /home/marc/cockroach/native/x86_64-linux-gnu/libroach
        Start 1: libroach_test
    1/2 Test #1: libroach_test ....................   Passed    0.00 sec
        Start 2: libroach_ccl_test
    2/2 Test #2: libroach_ccl_test ................   Passed    0.00 sec
    
    100% tests passed, 0 tests failed out of 2
    
    Total Test time (real) =   0.00 sec
    [100%] Built target check
    ```

--- c-deps/libroach/encoding.h
+++ c-deps/libroach/encoding.h
@@ -14,8 +14,8 @@
 
 #pragma once
 
+#include <rocksdb/slice.h>
 #include <stdint.h>
-#include "rocksdb/slice.h"
 
 // EncodeUint32 encodes the uint32 value using a big-endian 4 byte
 // representation. The bytes are appended to the supplied buffer.
