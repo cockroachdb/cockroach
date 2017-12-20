cockroachdb/cockroach

commit 715ed49b5cf7c595030249578dcbc959688b918f

cli: split the notions of "interactive session" and "terminal output"
Prior to this patch, a single boolean notion "interactive session" was
used to decide default settings for client commands that issue SQL
queries. This characteristic was decided based on whether both standard
input and standard output are on a terminal.

This characterisation was erroneous however as it didn't properly
account for the following:

- sessions started with `cockroach sql -e`, or non-interactive
  commands like `cockroach node`. These are definitely
  non-interactive, even if the standard input is a terminal.
- sessions started with `cockroach sql >foo.out`, i.e. letting the
  human enter commands from standard input but redirecting output to a
  file. These are arguably interactive and should benefit from the
  various defaults meant to help human users.

To achieve this, this patch separates the notions:

- an *interactive session* is one where the `cockroach` command is
  confident there's a user *manually entering queries*, looking at
  results in-between inputs. This is only decided to be true if 1) the
  `cockroach sql` command is used, 2) the standard input is a terminal
  3) `-e` is not used.
- a session with *terminal output* is where the `cockroach` command
  is confident there's a user *looking at the output*.

These characteristics are then used as follows to decide defaults:

| Setting                        | New default                                             | Previous bug(s) |
|--------------------------------|---------------------------------------------------------|------------------|
| `sql_safe_updates` (session)   | true when the session is interactive and `--unsafe-updates` not given, false otherwise. | defaulted to true always, not only when interactive |
| `check_syntax` (client)        | true when the session is interactive, false otherwise.  | erroneously false when inputting from user with output redirected |

pkg/cli/sql.go
@@ -39,6 +39,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	readline "github.com/knz/go-libedit"
 	"github.com/lib/pq"
+	isatty "github.com/mattn/go-isatty"
 	"github.com/spf13/cobra"
 )
 
@@ -192,10 +193,16 @@ More documentation about our SQL dialect and the CLI shell is available online:
 	fmt.Println()
 }
 
+const noLineEditor readline.EditLine = -1
+
+func (c *cliState) hasEditor() bool {
+	return c.ins != noLineEditor
+}
+
 // addHistory persists a line of input to the readline history
 // file.
 func (c *cliState) addHistory(line string) {
-	if !isInteractive || len(line) == 0 {
+	if !c.hasEditor() || len(line) == 0 {
 		return
 	}
 
@@ -238,7 +245,7 @@ var options = map[string]struct {
 		},
 		func(_ *cliState) error {
 			displayFormat := tableDisplayTSV
-			if isInteractive {
+			if cliCtx.terminalOutput {
 				displayFormat = tableDisplayPretty
 			}
 			cliCtx.tableDisplayFormat = displayFormat
@@ -448,7 +455,7 @@ func (c *cliState) pipeSyscmd(line string, nextState, errState cliStateEnum) cli
 // doRefreshPrompts refreshes the prompts of the client depending on the
 // status of the current transaction.
 func (c *cliState) doRefreshPrompts(nextState cliStateEnum) cliStateEnum {
-	if !isInteractive {
+	if !c.hasEditor() {
 		return nextState
 	}
 
@@ -601,19 +608,35 @@ func (c *cliState) doStart(nextState cliStateEnum) cliStateEnum {
 	// Common initialization.
 	c.partialLines = []string{}
 
-	if isInteractive {
-		c.checkSyntax = true
-		c.errExit = false
-		c.smartPrompt = true
+	if cliCtx.isInteractive {
+		// If a human user is providing the input, we want to help them with
+		// what they are entering:
+		c.checkSyntax = true // client-side error checking to help with large txns
+		c.errExit = false    // let the user retry failing commands
+
+		fmt.Println("#\n# Enter \\? for a brief introduction.\n#")
+	} else {
+		// When running non-interactive, by default we want errors to stop
+		// further processing and we can just let syntax checking to be
+		// done server-side to avoid client-side churn.
+		c.errExit = true
+		c.checkSyntax = false
+		// We also don't need (smart) prompts at all.
+	}
+
+	if c.hasEditor() {
+		// We only enable prompt and history management when the
+		// interactive input prompter is enabled. This saves on churn and
+		// memory when e.g. piping a large SQL script through the
+		// command-line client.
+
+		c.smartPrompt = true // enquire the db in between statements
 		c.promptPrefix, c.fullPrompt, c.continuePrompt = preparePrompts(c.conn.url)
 
 		c.ins.SetCompleter(c)
 		if err := c.ins.UseHistory(-1 /*maxEntries*/, true /*dedup*/); err != nil {
 			log.Warningf(context.TODO(), "cannot enable history: %v", err)
 		} else {
-			// We only enable history management when the terminal is actually
-			// interactive. This saves on memory when e.g. piping a large SQL
-			// script through the command-line client.
 			homeDir, err := envutil.HomeDir()
 			if err != nil {
 				log.Warningf(context.TODO(), "cannot retrieve user information: %v", err)
@@ -628,14 +651,6 @@ func (c *cliState) doStart(nextState cliStateEnum) cliStateEnum {
 				c.ins.SetAutoSaveHistory(histFile, true)
 			}
 		}
-
-		fmt.Println("#\n# Enter \\? for a brief introduction.\n#")
-	} else {
-		// When running non-interactive, by default we want errors to stop
-		// further processing and all syntax checking to be done
-		// server-side.
-		c.errExit = true
-		c.checkSyntax = false
 	}
 
 	return nextState
@@ -647,7 +662,7 @@ func (c *cliState) doStartLine(nextState cliStateEnum) cliStateEnum {
 	c.partialLines = c.partialLines[:0]
 	c.partialStmtsLen = 0
 
-	if isInteractive {
+	if c.hasEditor() {
 		c.currentPrompt = c.fullPrompt
 		c.ins.SetLeftPrompt(c.currentPrompt)
 	}
@@ -658,7 +673,7 @@ func (c *cliState) doStartLine(nextState cliStateEnum) cliStateEnum {
 func (c *cliState) doContinueLine(nextState cliStateEnum) cliStateEnum {
 	c.atEOF = false
 
-	if isInteractive {
+	if c.hasEditor() {
 		c.currentPrompt = c.continuePrompt
 		c.ins.SetLeftPrompt(c.currentPrompt)
 	}
@@ -705,7 +720,7 @@ func (c *cliState) doReadLine(nextState cliStateEnum) cliStateEnum {
 		// Good to go.
 
 	case readline.ErrInterrupted:
-		if !isInteractive {
+		if !cliCtx.isInteractive {
 			// Ctrl+C terminates non-interactive shells in all cases.
 			c.exitErr = err
 			return cliStop
@@ -730,7 +745,7 @@ func (c *cliState) doReadLine(nextState cliStateEnum) cliStateEnum {
 	case io.EOF:
 		c.atEOF = true
 
-		if isInteractive {
+		if cliCtx.isInteractive {
 			// In interactive mode, EOF terminates.
 			// exitErr is left to be whatever has set it previously.
 			return cliStop
@@ -916,7 +931,7 @@ func (c *cliState) doCheckStatement(startState, contState, execState cliStateEnu
 		return contState
 	}
 
-	if !isInteractive {
+	if !cliCtx.isInteractive {
 		return execState
 	}
 
@@ -985,7 +1000,7 @@ func maybeShowErrorDetails(w io.Writer, err error, printNewline bool) {
 func (c *cliState) doDecidePath() cliStateEnum {
 	if len(c.partialLines) == 0 {
 		return cliProcessFirstLine
-	} else if isInteractive {
+	} else if cliCtx.isInteractive {
 		// In interactive mode, we allow client-side commands to be
 		// issued on intermediate lines.
 		return cliHandleCliCmd
@@ -1006,15 +1021,16 @@ func runInteractive(conn *sqlConn) (exitErr error) {
 		}
 		switch state {
 		case cliStart:
-			// chzyer/readline is exceptionally slow at reading long lines, so we
-			// only use it in interactive mode.
-			if isInteractive {
-				// If interactive and the table display format is "pretty", also
-				// enable printing of times.
-				if cliCtx.tableDisplayFormat == tableDisplayPretty {
-					cliCtx.showTimes = true
-				}
+			// If results are shown on a terminal and the table display
+			// format is "pretty", also enable printing of times.
+			if cliCtx.terminalOutput && cliCtx.tableDisplayFormat == tableDisplayPretty {
+				cliCtx.showTimes = true
+			}
 
+			// An interactive readline prompter is comparatively slow at
+			// reading input, so we only use it in interactive mode and when
+			// there is also a terminal on stdout.
+			if cliCtx.isInteractive && cliCtx.terminalOutput {
 				// The readline initialization is not placed in
 				// the doStart() method because of the defer.
 				c.ins, c.exitErr = readline.InitFiles("cockroach",
@@ -1036,6 +1052,7 @@ func runInteractive(conn *sqlConn) (exitErr error) {
 				c.ins.RebindControlKeys()
 				defer c.ins.Close()
 			} else {
+				c.ins = noLineEditor
 				c.buf = bufio.NewReader(stdin)
 			}
 
@@ -1101,7 +1118,15 @@ func runTerm(cmd *cobra.Command, args []string) error {
 		return usageAndError(cmd)
 	}
 
-	if isInteractive && len(sqlCtx.execStmts) == 0 {
+	// We don't consider sessions interactives unless we have a
+	// serious hunch they are. For now, only `cockroach sql` *without*
+	// `-e` has the ability to input from a (presumably) human user,
+	// and we'll also assume that there is no human if the standard
+	// input is not terminal-like -- likely redirected from a file,
+	// etc.
+	cliCtx.isInteractive = len(sqlCtx.execStmts) == 0 && isatty.IsTerminal(os.Stdin.Fd())
+
+	if cliCtx.isInteractive {
 		// The user only gets to see the info screen on interactive sessions.
 		fmt.Print(infoMessage)
 	}
@@ -1118,7 +1143,7 @@ func runTerm(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	if !sqlCtx.unsafeUpdates {
+	if cliCtx.isInteractive && !sqlCtx.unsafeUpdates {
 		if err := conn.Exec("SET sql_safe_updates = TRUE", nil); err != nil {
 			return err
 		}

pkg/cli/interactive_tests/test_sql_safe_updates.tcl
@@ -1,0 +1,53 @@
+#! /usr/bin/env expect -f
+
+source [file join [file dirname $argv0] common.tcl]
+
+start_server $argv
+
+spawn /bin/bash
+send "PS1=':''/# '\r"
+eexpect ":/# "
+
+start_test "Check that dangerous statements are properly rejected when running interactively with terminal output."
+send "$argv sql\r"
+eexpect root@
+send "create database d;\rcreate table d.t(x int);\r"
+eexpect "CREATE"
+eexpect "CREATE"
+eexpect root@
+
+send "delete from d.t;\r"
+eexpect "rejected: DELETE without WHERE clause (sql_safe_updates = true)"
+eexpect root@
+end_test
+
+send "\\q\r"
+eexpect ":/# "
+
+start_test "Check that dangerous statements are properly rejected when inputting from user even with output redirected."
+send "$argv sql | cat\r"
+# We can't immediately send input, because the shell will eat stdin just before it's ready.
+eexpect "brief introduction"
+sleep 0.4
+send "show sql_safe_updates;\r"
+eexpect "true"
+eexpect "1 row"
+send "delete from d.t;\r"
+eexpect "rejected: DELETE without WHERE clause (sql_safe_updates = true)"
+send "\\q\r"
+eexpect ":/# "
+end_test
+
+start_test "Check that dangerous statements are not rejected when input redirected."
+send "echo 'delete from d.t;' | $argv sql \r"
+eexpect "DELETE"
+eexpect ":/# "
+end_test
+
+start_test "Check that dangerous statements are not rejected when using -e."
+send "$argv sql -e 'delete from d.t'\r"
+eexpect "DELETE"
+eexpect ":/# "
+end_test
+
+stop_server $argv

pkg/cli/cli.go
@@ -20,7 +20,6 @@ import (
 	"os"
 	"text/tabwriter"
 
-	"github.com/mattn/go-isatty"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"golang.org/x/net/context"
@@ -110,11 +109,6 @@ var cockroachCmd = &cobra.Command{
 	// in fact, a result of a bad invocation, e.g. too many arguments.
 	SilenceUsage: true,
 }
-
-// isInteractive indicates whether both stdin and stdout refer to the
-// terminal.
-var isInteractive = isatty.IsTerminal(os.Stdout.Fd()) &&
-	isatty.IsTerminal(os.Stdin.Fd())
 
 func init() {
 	cobra.EnableCommandSorting = false

pkg/cli/cli_test.go
@@ -705,7 +705,6 @@ func Example_sql() {
 
 	c.RunWithArgs([]string{"sql", "-e", "show application_name"})
 	c.RunWithArgs([]string{"sql", "-e", "create database t; create table t.f (x int, y int); insert into t.f values (42, 69)"})
-	c.RunWithArgs([]string{"sql", "-e", "delete from t.f"})
 	c.RunWithArgs([]string{"sql", "-e", "select 3", "-e", "select * from t.f"})
 	c.RunWithArgs([]string{"sql", "-e", "begin", "-e", "select 3", "-e", "commit"})
 	c.RunWithArgs([]string{"sql", "-e", "select * from t.f"})
@@ -733,8 +732,6 @@ func Example_sql() {
 	// # 1 row
 	// sql -e create database t; create table t.f (x int, y int); insert into t.f values (42, 69)
 	// INSERT 1
-	// sql -e delete from t.f
-	// pq: rejected: DELETE without WHERE clause (sql_safe_updates = true)
 	// sql -e select 3 -e select * from t.f
 	// 3
 	// 3

pkg/cli/context.go
@@ -16,6 +16,7 @@ package cli
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 
@@ -26,6 +27,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/storage/engine"
+	isatty "github.com/mattn/go-isatty"
 	"github.com/pkg/errors"
 	"golang.org/x/net/context"
 )
@@ -51,6 +53,17 @@ type cliContext struct {
 	// Embed the base context.
 	*base.Config
 
+	// isInteractive indicates whether the session is interactive, that
+	// is, the commands executed are extremely likely to be *input* from
+	// a human user: the standard input is a terminal and `-e` was not
+	// used (the shell has a prompt).
+	isInteractive bool
+
+	// terminalOutput indicates whether output is going to a terminal,
+	// that is, it is not going to a file, another program for automated
+	// processing, etc.: the standard output is a terminal.
+	terminalOutput bool
+
 	// tableDisplayFormat indicates how to format result tables.
 	tableDisplayFormat tableDisplayFormat
 
@@ -74,7 +87,15 @@ func GetServerCfgStores() base.StoreSpecList {
 }
 
 var baseCfg = serverCfg.Config
-var cliCtx = cliContext{Config: baseCfg}
+var cliCtx = cliContext{
+	Config: baseCfg,
+
+	terminalOutput: isatty.IsTerminal(os.Stdout.Fd()),
+	// isInteractive is only set to `true` by `cockroach sql` -- all
+	// other client commands are non-interactive, regardless of whether
+	// the standard input is a terminal.
+	isInteractive: false,
+}
 
 type tableDisplayFormat int
 

pkg/cli/flags.go
@@ -359,7 +359,7 @@ func init() {
 	// in the CLI shell.
 	cliCtx.tableDisplayFormat = tableDisplayTSV
 	cliCtx.showTimes = false
-	if isInteractive {
+	if cliCtx.terminalOutput {
 		cliCtx.tableDisplayFormat = tableDisplayPretty
 	}
 	for _, cmd := range tableOutputCommands {

pkg/cli/interactive_tests/test_last_statement.tcl
@@ -68,12 +68,12 @@ eexpect ":/# "
 end_test
 
 start_test "Check that a final comment after a final statement does not cause an error message. #9482"
-send "printf 'select 1;-- final comment' | $argv sql\r"
+send "printf 'select 1;-- final comment' | $argv sql | cat\r"
 eexpect "1\r\n1\r\n# 1 row\r\n:/# "
 end_test
 
 start_test "Check that a final comment does not cause an error message. #9243"
-send "printf 'select 1;\\n-- final comment' | $argv sql\r"
+send "printf 'select 1;\\n-- final comment' | $argv sql | cat\r"
 eexpect "1\r\n1\r\n# 1 row\r\n:/# "
 end_test
 

pkg/cli/interactive_tests/test_pretty.tcl
@@ -10,18 +10,25 @@ eexpect ":/# "
 
 # Check table ASCII art with and without --format=pretty. (#7268)
 
-start_test "Check that tables are pretty-printed when input is not a terminal but --format=pretty is specified."
-send "echo 'select 1;' | $argv sql --format=pretty\r"
+start_test "Check that tables are pretty-printed when output is not a terminal but --format=pretty is specified."
+send "echo 'select 1;' | $argv sql --format=pretty | cat\r"
 eexpect "+-*+\r\n*\r\n+-*+\r\n*1 row"
 eexpect ":/# "
 end_test
 
-start_test "Check that tables are not pretty-printed when input is not a terminal and --format=pretty is not specified."
+start_test "Check that tables are pretty-printed when input is not a terminal and --format=pretty is not specified, but output is a terminal."
 send "echo begin; echo 'select 1;' | $argv sql\r"
-eexpect "begin\r\n1\r\n1\r\n# 1 row\r\n"
+eexpect "+-*+\r\n*\r\n+-*+\r\n*1 row"
+eexpect ":/# "
 end_test
 
-start_test "Check that tables are pretty-printed when input is a terminal and --format=pretty is not specified."
+start_test "Check that tables are not pretty-printed when output is not a terminal and --format=pretty is not specified"
+send "echo begin; echo 'select 1;' | $argv sql | cat\r"
+eexpect "begin\r\n1\r\n1\r\n# 1 row\r\n"
+eexpect ":/# "
+end_test
+
+start_test "Check that tables are pretty-printed when input and output are a terminal and --format=pretty is not specified."
 send "$argv sql\r"
 eexpect root@
 send "select 1;\r"
@@ -37,7 +44,7 @@ eexpect "+-*+\r\n*1 row"
 eexpect root@
 end_test
 
-start_test "Check that tables are not pretty-printed when input is a terminal and --format=tsv is specified."
+start_test "Check that tables are not pretty-printed when output is a terminal and --format=tsv is specified."
 send "\\q\r"
 eexpect ":/# "
 send "$argv sql --format=tsv\r"

commit a18ad4f177676d44415882466ba747096b5b1031

cli/sql: ensure that --safe-updates works even in non-interactive sessions
The intended behavior of `--safe-updates`, when specified explicitly,
is to disallow "dangerous statements". This feature should work
whenever the flag is explicitly specified, regardless of whether the
session is interactive.

Prior to this patch, the code was erroneously ignoring the
`--safe-updates` flag in non-interactive session, causing it to become
ineffective when e.g. combined with `-e`. This patch fixes the
behavior.

Release note (cli change, bug fix): `cockroach sql --safe-updates` can
now be used effectively in non-interactive sessions.

Note: this fixes a regression introduced in
715ed49b5cf7c595030249578dcbc959688b918f. The flag was functioning
properly before that.

pkg/cli/sql.go
@@ -30,6 +30,7 @@ import (
 	"golang.org/x/net/context"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/cli/cliflags"
 	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/parser"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
@@ -1144,7 +1145,7 @@ func runTerm(cmd *cobra.Command, args []string) error {
 	}
 
 	// Enable safe updates, unless disabled.
-	setupSafeUpdates(conn)
+	setupSafeUpdates(cmd, conn)
 
 	if len(sqlCtx.execStmts) > 0 {
 		// Single-line sql; run as simple as possible, without noise on stdout.
@@ -1156,8 +1157,19 @@ func runTerm(cmd *cobra.Command, args []string) error {
 // setupSafeUpdates attempts to enable "safe mode" if the session is
 // interactive and the user is not disabling this behavior with
 // --safe-updates=false.
-func setupSafeUpdates(conn *sqlConn) {
-	if !cliCtx.isInteractive || !sqlCtx.safeUpdates {
+func setupSafeUpdates(cmd *cobra.Command, conn *sqlConn) {
+	pf := cmd.Flags()
+	vf := pf.Lookup(cliflags.SafeUpdates.Name)
+
+	if !vf.Changed {
+		// If `--safe-updates` was not specified, we need to set the default
+		// based on whether the session is interactive. We cannot do this
+		// earlier, because "session is interactive" depends on knowing
+		// whether `-e` is also provided.
+		sqlCtx.safeUpdates = cliCtx.isInteractive
+	}
+
+	if !sqlCtx.safeUpdates {
 		// nothing to do.
 		return
 	}

pkg/cli/interactive_tests/test_sql_safe_updates.tcl
@@ -50,4 +50,10 @@ eexpect "DELETE"
 eexpect ":/# "
 end_test
 
+start_test "Check that dangerous statements are properly rejected when using --safe-updates -e."
+send "$argv sql --safe-updates -e 'delete from d.t'\r"
+eexpect "rejected: DELETE without WHERE clause"
+eexpect ":/# "
+end_test
+
 stop_server $argv