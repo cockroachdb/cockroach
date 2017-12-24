072d74b88dab7a4425ac41747b5efdf0a303f217
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2017-12-23 23:36:34 -0500

    engine: improve readability
    Release note: None

--- pkg/storage/engine/mvcc.go
+++ pkg/storage/engine/mvcc.go
@@ -337,7 +337,7 @@ func updateStatsOnResolve(
 	}
 
 	if sys {
-		ms.SysBytes += metaKeySize + metaValSize - origMetaValSize - origMetaKeySize
+		ms.SysBytes += (metaKeySize + metaValSize) - (origMetaValSize + origMetaKeySize)
 	} else {
 		// At orig.Timestamp, the original meta key disappears.
 		ms.KeyBytes -= origMetaKeySize + orig.KeyBytes
@@ -365,7 +365,7 @@ func updateStatsOnResolve(
 
 		// At meta.Timestamp, the new meta key appears.
 		if !meta.Deleted {
-			ms.LiveBytes += metaKeySize + metaValSize + meta.KeyBytes + meta.ValBytes
+			ms.LiveBytes += (metaKeySize + metaValSize) + (meta.KeyBytes + meta.ValBytes)
 		}
 		ms.KeyBytes += metaKeySize + meta.KeyBytes
 		ms.ValBytes += metaValSize + meta.ValBytes
