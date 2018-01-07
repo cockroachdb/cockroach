80c2bb458df3077f6b014bcf31968b51c1e6e253
Raphael 'kena' Poss <knz@cockroachlabs.com>
2018-01-07 16:12:47 +0100

    sem/tree: add a space between ARRAY and its subquery
    Release note: none

--- pkg/sql/sem/tree/expr.go
+++ pkg/sql/sem/tree/expr.go
@@ -777,7 +777,7 @@ type ArrayFlatten struct {
 
 // Format implements the NodeFormatter interface.
 func (node *ArrayFlatten) Format(ctx *FmtCtx) {
-	ctx.WriteString("ARRAY")
+	ctx.WriteString("ARRAY ")
 	exprFmtWithParen(ctx, node.Subquery)
 }
 
