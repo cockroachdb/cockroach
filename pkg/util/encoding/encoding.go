dbe5a6db5f0c6c01be628161403f0adabcf057c3
Jordan Lewis <jordanthelewis@gmail.com>
2018-01-11 15:38:14 -0500

    sqlbase: do less work in processValueBytes
    Previously we decoded the value tag for every column in every value
    twice, during row decode.
    
    Now we just do it once.
    
    Release note: None

--- pkg/util/encoding/encoding.go
+++ pkg/util/encoding/encoding.go
@@ -1799,41 +1799,50 @@ func PeekValueLength(b []byte) (typeOffset int, length int, err error) {
 	if err != nil {
 		return 0, 0, err
 	}
+	length, err = PeekValueLengthWithOffsetsAndType(b, dataOffset, typ)
+	return typeOffset, length, err
+}
+
+// PeekValueLengthWithOffsetsAndType is the same as PeekValueLength, except it
+// expects a dataOffset and typ value from a previous call to DecodeValueTag
+// on its input byte slice. Use this if you've already called DecodeValueTag
+// on the input for another reason, to avoid it getting called twice.
+func PeekValueLengthWithOffsetsAndType(b []byte, dataOffset int, typ Type) (length int, err error) {
 	b = b[dataOffset:]
 	switch typ {
 	case Null:
-		return typeOffset, dataOffset, nil
+		return dataOffset, nil
 	case True, False:
-		return typeOffset, dataOffset, nil
+		return dataOffset, nil
 	case Int:
 		_, n, _, err := DecodeNonsortingStdlibVarint(b)
-		return typeOffset, dataOffset + n, err
+		return dataOffset + n, err
 	case Float:
-		return typeOffset, dataOffset + floatValueEncodedLength, nil
+		return dataOffset + floatValueEncodedLength, nil
 	case Bytes, Array, JSON:
 		_, n, i, err := DecodeNonsortingUvarint(b)
-		return typeOffset, dataOffset + n + int(i), err
+		return dataOffset + n + int(i), err
 	case Decimal:
 		_, n, i, err := DecodeNonsortingStdlibUvarint(b)
-		return typeOffset, dataOffset + n + int(i), err
+		return dataOffset + n + int(i), err
 	case Time:
 		n, err := getMultiNonsortingVarintLen(b, 2)
-		return typeOffset, dataOffset + n, err
+		return dataOffset + n, err
 	case Duration:
 		n, err := getMultiNonsortingVarintLen(b, 3)
-		return typeOffset, dataOffset + n, err
+		return dataOffset + n, err
 	case UUID:
-		return typeOffset, dataOffset + uuidValueEncodedLength, err
+		return dataOffset + uuidValueEncodedLength, err
 	case IPAddr:
 		family := ipaddr.IPFamily(b[0])
 		if family == ipaddr.IPv4family {
-			return typeOffset, dataOffset + ipaddr.IPv4size, err
+			return dataOffset + ipaddr.IPv4size, err
 		} else if family == ipaddr.IPv6family {
-			return typeOffset, dataOffset + ipaddr.IPv6size, err
+			return dataOffset + ipaddr.IPv6size, err
 		}
-		return 0, 0, errors.Errorf("got invalid INET IP family: %d", family)
+		return 0, errors.Errorf("got invalid INET IP family: %d", family)
 	default:
-		return 0, 0, errors.Errorf("unknown type %s", typ)
+		return 0, errors.Errorf("unknown type %s", typ)
 	}
 }
 
