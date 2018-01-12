062dfa7d441bbec4eec461ce54ae997d7ad633fc
Nikhil Benesch <nikhil.benesch@gmail.com>
2018-01-12 16:46:10 -0500

    build: only require a minimum version of Go
    Instead of requiring a specific version (or versions) of Go, enforce a
    minimum version of Go. This means we'll no longer need to adjust the
    rule whenever a new version of Go comes out.
    
    This means it's now possible to build CockroachDB with a Go 1.10 beta
    without explicitly disabling the version check.
    
    Fix #19431.
    
    Release note (build change): The build system now enforces a minimum
    version of Go, rather than enforcing a specific version of Go. Since the
    Go 1.x series has strict backwards compatibility guarantees, the old
    rule was unnecessarily restrictive.

---
+++ build/go-version-check.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Detect whether the installed version of Go can build this version of
+# CockroachDB.
+#
+# To bump the required version of Go, edit the last conditional appropriately.
+#
+# Note: this script is invoked by Make, so it has odd error reporting semantics.
+# Errors are printed to stdout; the Go version is compatible iff nothing is
+# printed to stdout. The exit code is meaningless.
+
+# Ensure stray error messages are printed to stdout, or they'll go unnoticed by
+# Make.
+exec 2>&1
+
+go=${1-go}
+
+if ! raw_version=$("$go" version 2>&1); then
+  echo "unable to detect go version: $raw_version"
+  exit
+fi
+
+if ! version=$(grep -oE "[0-9]+\.[0-9]+" <<< "$raw_version"); then
+  echo "unable to parse go version '$raw_version'"
+  exit
+fi
+
+version_major=$(cut -f1 -d. <<< "$version")
+version_minor=$(cut -f2 -d. <<< "$version")
+if (( version_major != 1 )) || (( version_minor < 9 )); then
+  echo "go1.9+ required (detected go$version)"
+  exit
+fi
