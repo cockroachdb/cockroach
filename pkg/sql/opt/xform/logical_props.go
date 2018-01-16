51b7e3587a958b7a25c23e3b7c4c0768739a0caf
Andrew Kimball <kimball.andy@gmail.com>
2018-01-16 07:44:21 -0800

    opt: Integrate initial versions of v4 expr, memo, and factory
    This PR takes a subset of the files from the v4 opttoy branch, adds
    comments and tests, and puts them in a new opt/xform package.
    Ultimately, I'd expect us to have packages like:
    
      opt/build - contains AST => v4 Expr builder
      opt/xform - transformation and optimization of v4 Exprs
      opt/exec  - contains v4 Expr => plan nodes (is exec best name?)
    
    The testing of this PR is not complete. I'll be adding more tests
    once the builder is in place, so that I can use SQL. For now, this
    PR does some rudimentary testing using the memo factory directly.
    
    Also, in order to pass the lint test, *.og.go files were excluded
    from crlfmt, since they are auto-generated by the new optgen tool.

---
+++ pkg/sql/opt/xform/logical_props.go
@@ -0,0 +1,73 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package xform
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/cockroachdb/cockroach/pkg/util/treeprinter"
+)
+
+// LogicalProps describe the content and characteristics of data returned by
+// all expression variants within a memo group. While each expression in the
+// group may return rows or columns in a different order, or compute the
+// result using different algorithms, the complete set of data is returned
+// and can be transformed into whatever layout or presentation format that is
+// is desired.
+type LogicalProps struct {
+	// Relational contains the set of properties that describe relational
+	// operators, like select, join, and project. It is nil for scalar
+	// operators.
+	Relational *RelationalProps
+}
+
+// RelationalProps are the subset of logical properties that are computed for
+// relational expressions that return rows and columns rather than scalar
+// values.
+type RelationalProps struct {
+	// OutputCols is the set of columns that can be projected by the
+	// expression. Ordering, naming, and duplication of columns is not
+	// representable by this property; those are physical properties.
+	OutputCols ColSet
+
+	// NotNullCols is the subset of output columns which cannot be NULL.
+	// The NULL-ability of columns flows from the inputs and can also be
+	// derived from filters that are NULL-intolerant.
+	NotNullCols ColSet
+}
+
+func (p *LogicalProps) format(mem *memo, tp treeprinter.Node) {
+	p.formatOutputCols(mem, tp)
+}
+
+func (p *LogicalProps) formatOutputCols(mem *memo, tp treeprinter.Node) {
+	if p.Relational != nil && !p.Relational.OutputCols.Empty() {
+		var buf bytes.Buffer
+		buf.WriteString("columns:")
+		p.Relational.OutputCols.ForEach(func(i int) {
+			colIndex := ColumnIndex(i)
+			label := mem.metadata.ColumnLabel(colIndex)
+			buf.WriteByte(' ')
+			buf.WriteString(label)
+			buf.WriteByte(':')
+			if !p.Relational.NotNullCols.Contains(int(colIndex)) {
+				buf.WriteString("null:")
+			}
+			fmt.Fprintf(&buf, "%d", colIndex)
+		})
+		tp.Child(buf.String())
+	}
+}
