b69e686274db401c9edaf4f36d9ab0ce542243a1
David Taylor <tinystatemachine@gmail.com>
2018-01-16 20:17:10 +0000

    sqlccl: disallow bulk IO statements in txn
    these aren't transactional, so don't let them pretend they are.
    
    Fixes #17498.
    
    Release note: disallow non-transactional bulk IO statements inside transactions.

--- pkg/sql/planner.go
+++ pkg/sql/planner.go
@@ -222,7 +222,7 @@ func makeInternalPlanner(
 
 	s := &Session{
 		data:     data,
-		TxnState: txnState{Ctx: ctx},
+		TxnState: txnState{Ctx: ctx, implicitTxn: true},
 		context:  ctx,
 		tables:   TableCollection{databaseCache: newDatabaseCache(config.SystemConfig{})},
 	}
