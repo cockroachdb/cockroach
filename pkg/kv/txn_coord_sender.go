3c5de3a728b38cb61bcbe4b062768741a87c0af0
Spencer Kimball <spencer.kimball@gmail.com>
2018-01-21 14:40:55 -0700

    kv: fix race condition
    Fixes #21602
    Fixes #21613
    Fixes #21619
    Fixes #21628
    Fixes #21629
    
    Release note: None

--- pkg/kv/txn_coord_sender.go
+++ pkg/kv/txn_coord_sender.go
@@ -269,7 +269,7 @@ func (tc *TxnCoordSender) AugmentMeta(meta roachpb.TxnCoordMeta) {
 		return
 	}
 	tc.mu.meta.Txn.Update(&meta.Txn)
-	tc.mu.meta.Intents, _ = roachpb.MergeSpans(append(tc.mu.meta.Intents, meta.Intents...))
+	tc.mu.meta.Intents, _ = roachpb.MergeSpans(append(meta.Intents, tc.mu.meta.Intents...))
 	tc.mu.meta.CommandCount += meta.CommandCount
 
 	// Recompute the size of the intents.
