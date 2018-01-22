f6d6dd5e56e431a8032d1e0f340d334e7f9e2dc7
marc <marc@cockroachlabs.com>
2018-01-22 21:29:05 +0100

    Add license files for rocksdbutils
    Since these files are mostly copied from rocksdb, they are kept separate
    along with their licensed. It clearer to put the license here as opposed
    to either;
    * referring to the root cockroach repo
    * referring to the rocksdb repo

--- c-deps/libroach/rocksdbutils/aligned_buffer.h
+++ c-deps/libroach/rocksdbutils/aligned_buffer.h
@@ -1,11 +1,12 @@
 //  Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
 //  This source code is licensed under both the GPLv2 (found in the
-//  COPYING file in the root directory) and Apache 2.0 License
-//  (found in the LICENSE.Apache file in the root directory).
+//  COPYING file in this directory) and Apache 2.0 License
+//  (found in the LICENSE.Apache file in this directory).
 //
 // Copyright (c) 2011 The LevelDB Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file. See the AUTHORS file for names of contributors.
+// found in the LICENSE.leveldb file in this directory.
+// See the AUTHORS file for names of contributors.
 #pragma once
 
 #include <algorithm>
