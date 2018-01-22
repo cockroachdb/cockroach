623cc5f396debfefc44cbde1a300c0de15b1445b
Andrew Couch <andrew@cockroachlabs.com>
2018-01-22 12:54:07 -0500

    ui: add comments to locations util functions
    Release note: None

--- pkg/ui/src/util/locations.ts
+++ pkg/ui/src/util/locations.ts
@@ -3,6 +3,10 @@ import _ from "lodash";
 import { LocalityTier } from "src/redux/localities";
 import { LocationTree } from "src/redux/locations";
 
+/*
+ * getLocation retrieves the location for a given locality tier from the
+ * LocationTree, or null if none is found.
+ */
 export function getLocation(locations: LocationTree, tier: LocalityTier) {
   if (!locations[tier.key]) {
     return null;
@@ -11,10 +15,20 @@ export function getLocation(locations: LocationTree, tier: LocalityTier) {
   return locations[tier.key][tier.value];
 }
 
+/*
+ * hasLocation is a predicate to determine if a given locality tier exists in
+ * the LocationTree.
+ */
 export function hasLocation(locations: LocationTree, tier: LocalityTier) {
   return !_.isNil(getLocation(locations, tier));
 }
 
+/*
+ * findMostSpecificLocation searches for a location matching the given locality
+ * tiers in the LocationTree.  It tries to find the most specific location that
+ * applies, and thus begins searching from the end of the list of tiers for a
+ * tier with a matching location.  Returns null if none is found.
+ */
 export function findMostSpecificLocation(locations: LocationTree, tiers: LocalityTier[]) {
   let currentIndex = tiers.length - 1;
   while (currentIndex >= 0) {
