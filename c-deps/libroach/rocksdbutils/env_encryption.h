90c9db4ec14201279ea06f5c2cf85a946e1ea67f
marc <marc@cockroachlabs.com>
2018-01-22 21:29:05 +0100

    Add license files for rocksdbutils
    Since these files are mostly copied from rocksdb, they are kept separate
    along with their licensed. It clearer to put the license here as opposed
    to either;
    * referring to the root cockroach repo
    * referring to the rocksdb repo

--- c-deps/libroach/rocksdbutils/env_encryption.h
+++ c-deps/libroach/rocksdbutils/env_encryption.h
@@ -1,7 +1,7 @@
-//  Copyright (c) 2016-present, Facebook, Inc.  All rights reserved.
+//  Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
 //  This source code is licensed under both the GPLv2 (found in the
-//  COPYING file in the root directory) and Apache 2.0 License
-//  (found in the LICENSE.Apache file in the root directory).
+//  COPYING file in this directory) and Apache 2.0 License
+//  (found in the LICENSE.Apache file in this directory).
 
 #pragma once
 
