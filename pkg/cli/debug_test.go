472714ce5d8a862a8476e8f0c3ce1122fea08898
pocockn <pocockn@hotmail.co.uk>
2018-01-25 08:37:43 +0000

    cli: Open RocksDB in ReadOnly mode for debug commands
    Fixes #21130.
    
    This change opens RockDB in ReadOnly mode for debug commands
    that don't involve writing to the DB. This improves store speed
    and reduce chances of a bug in the debug command accidently
    modifying the store.
    
    Release note (general change): debug commands now open RocksDB in
    read-only mode, making them faster and able to run in parallel.

--- pkg/cli/debug_test.go
+++ pkg/cli/debug_test.go
@@ -17,9 +17,11 @@ package cli
 
 import (
 	"context"
+	"fmt"
 	"path/filepath"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/server"
 	"github.com/cockroachdb/cockroach/pkg/storage/engine"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
@@ -27,33 +29,34 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
 )
 
+func createStore(t *testing.T, path string) {
+	t.Helper()
+	cache := engine.NewRocksDBCache(server.DefaultCacheSize)
+	defer cache.Release()
+	db, err := engine.NewRocksDB(
+		engine.RocksDBConfig{
+			Dir:       path,
+			MustExist: false,
+		},
+		cache,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	db.Close()
+}
+
 func TestOpenExistingStore(t *testing.T) {
 	defer leaktest.AfterTest(t)()
+	stopper := stop.NewStopper()
+	defer stopper.Stop(context.Background())
 
 	baseDir, dirCleanupFn := testutils.TempDir(t)
 	defer dirCleanupFn()
 
 	dirExists := filepath.Join(baseDir, "exists")
 	dirMissing := filepath.Join(baseDir, "missing")
-
-	func() {
-		cache := engine.NewRocksDBCache(server.DefaultCacheSize)
-		defer cache.Release()
-		db, err := engine.NewRocksDB(
-			engine.RocksDBConfig{
-				Dir:       dirExists,
-				MustExist: false,
-			},
-			cache,
-		)
-		if err != nil {
-			t.Fatal(err)
-		}
-		db.Close()
-	}()
-
-	stopper := stop.NewStopper()
-	defer stopper.Stop(context.Background())
+	createStore(t, dirExists)
 
 	for _, test := range []struct {
 		dir    string
@@ -68,9 +71,51 @@ func TestOpenExistingStore(t *testing.T) {
 			expErr: `could not open rocksdb instance: .* does not exist \(create_if_missing is false\)`,
 		},
 	} {
-		_, err := openExistingStore(test.dir, stopper)
-		if !testutils.IsError(err, test.expErr) {
-			t.Fatalf("%s: wanted %s but got %v", test.dir, test.expErr, err)
-		}
+		t.Run(fmt.Sprintf("dir=%s", test.dir), func(t *testing.T) {
+			_, err := openExistingStore(test.dir, stopper, false /* readOnly */)
+			if !testutils.IsError(err, test.expErr) {
+				t.Errorf("wanted %s but got %v", test.expErr, err)
+			}
+		})
+	}
+}
+
+func TestOpenReadOnlyStore(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	stopper := stop.NewStopper()
+	defer stopper.Stop(context.Background())
+
+	baseDir, dirCleanupFn := testutils.TempDir(t)
+	defer dirCleanupFn()
+
+	storePath := filepath.Join(baseDir, "store")
+	createStore(t, storePath)
+
+	for _, test := range []struct {
+		readOnly bool
+		expErr   string
+	}{
+		{
+			readOnly: false,
+			expErr:   "",
+		},
+		{
+			readOnly: true,
+			expErr:   `Not supported operation in read only mode.`,
+		},
+	} {
+		t.Run(fmt.Sprintf("readOnly=%t", test.readOnly), func(t *testing.T) {
+			db, err := openExistingStore(storePath, stopper, test.readOnly)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			key := engine.MakeMVCCMetadataKey(roachpb.Key("key"))
+			val := []byte("value")
+			err = db.Put(key, val)
+			if !testutils.IsError(err, test.expErr) {
+				t.Fatalf("wanted %s but got %v", test.expErr, err)
+			}
+		})
 	}
 }
