d85397be74ed5376a1fd13b42156e9ef212bc1b7
Raphael 'kena' Poss <knz@cockroachlabs.com>
2018-01-25 01:26:29 +0100

    sql/sem/tree: make IMPORT use normalizable table references
    ... for consistency with every other use of `table_name`.
    
    Release note: None

--- pkg/ccl/sqlccl/csv.go
+++ pkg/ccl/sqlccl/csv.go
@@ -1001,8 +1001,7 @@ func importPlanHook(
 
 		var create *tree.CreateTable
 		if importStmt.CreateDefs != nil {
-			normName := tree.NormalizableTableName{TableNameReference: &importStmt.Table}
-			create = &tree.CreateTable{Table: normName, Defs: importStmt.CreateDefs}
+			create = &tree.CreateTable{Table: importStmt.Table, Defs: importStmt.CreateDefs}
 		} else {
 			filename, err := createFileFn()
 			if err != nil {
