ab7d645545f9f544ac9a4ef9a87fc6ff9635388a
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2018-01-25 00:21:21 -0500

    storage: remove range size limit for snapshots
    Now that we are able to effectively limit the size that ranges grow to,
    this was no longer necessary.

--- pkg/storage/replica_raftstorage.go
+++ pkg/storage/replica_raftstorage.go
@@ -392,27 +392,6 @@ func (r *Replica) GetSnapshot(
 	defer r.mu.RUnlock()
 	rangeID := r.RangeID
 
-	// TODO(nvanbenschoten): We should never block snapshots indefinitely. Doing
-	// so can reduce a range's ability to recover from an under-replicated state
-	// and can cause unavailability even when a majority of replicas remain
-	// live. For instance, if a range gets too large to snapshot and requires a
-	// split in order to do so again, the loss of one up-to-date replica could
-	// cause it to permanently lose quorum.
-	//
-	// For now we still need this check because unbounded snapshots can result
-	// in OOM errors that crash entire nodes. However, once snapshots are
-	// streamed from disk to disk, never needing to buffer in-memory on the
-	// sending or receiving side, we should be able to remove any snapshot size
-	// limit. See #16954 for more.
-	if r.exceedsDoubleSplitSizeRLocked() && !r.mu.permitLargeSnapshots {
-		maxBytes := r.mu.maxBytes
-		size := r.mu.state.Stats.Total()
-		err := errors.Errorf(
-			"%s: not generating %s snapshot because replica is too large: %d > 2 * %d",
-			r, snapType, size, maxBytes)
-		return nil, err
-	}
-
 	startKey := r.mu.state.Desc.StartKey
 	ctx, sp := r.AnnotateCtxWithSpan(ctx, "snapshot")
 	defer sp.Finish()
