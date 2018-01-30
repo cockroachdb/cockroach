0775ed0dd758f1afe11866c103a3e5606bf0e0c4
Nikhil Benesch <nikhil.benesch@gmail.com>
2018-01-30 15:34:19 -0500

    gceworker: learn to sync files
    Teach the GCE worker a sync command, which uses Unison to perform
    bidirectional syncing of the CockroachDB repo. This makes the GCE
    development workflow quite pleasant.
    
    This is an extension of #8981, which used SSHFS to similar effect.
    Unison is far more performant than SSHFS, if a bit more complicated to
    set up. SSHFS has to hit the network to service every filesystem
    request, making builds excruciatingly slow, whereas Unison maintains a
    full "replica" of the synced folder on each machine and syncs the
    replicas when notified of changes by either filesystem.
    
    Release note: None

---
+++ build/bootstrap/bootstrap-unison.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# On a Debian/Ubuntu system, bootstraps the Unison file-syncer.
+
+set -euxo pipefail
+
+sudo apt-get install -y --no-install-recommends ocaml-nox
+
+git clone --branch=unison-2.48 https://github.com/bcpierce00/unison
+(cd unison && git cherry-pick -n c64b007ca0c20e1cea94a79b82d7c415aa3729eb && make)
+for bin in unison unison-fsmonitor; do
+  sudo install -m 755 "unison/src/$bin" "/usr/local/bin/$bin"
+done
+
+echo fs.inotify.max_user_watches=524288 | sudo tee /etc/sysctl.d/60-max-user-watches.conf
+sudo service procps restart
+
+echo "UNISONLOCALHOSTNAME=$(hostname)-$(date +%Y%m%d-%H%M%S)" | sudo tee -a /etc/environment
