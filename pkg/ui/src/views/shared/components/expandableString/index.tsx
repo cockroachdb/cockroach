9a647345ce579ca90e64cdccbf68a5dc12f5a393
Andrew Couch <andrew@cockroachlabs.com>
2018-01-30 15:00:14 -0500

    ui: also let the expanded text collapse again
    Release note (admin ui change): Allow long descriptions in the jobs table to be
    collapsed again after expanding.

--- pkg/ui/src/views/shared/components/expandableString/index.tsx
+++ pkg/ui/src/views/shared/components/expandableString/index.tsx
@@ -1,10 +1,14 @@
+import _ from "lodash";
 import React from "react";
 
 import { trustIcon } from "src/util/trust";
 
+import collapseIcon from "!!raw-loader!assets/collapse.svg";
 import expandIcon from "!!raw-loader!assets/expand.svg";
 
 import "./expandable.styl";
+
+const truncateLength = 50;
 
 interface ExpandableStringProps {
   short?: string;
@@ -25,22 +29,36 @@ export class ExpandableString extends React.Component<ExpandableStringProps, Exp
 
   onClick = (ev: any) => {
     ev.preventDefault();
-    this.setState({ expanded: true });
+    this.setState({ expanded: !this.state.expanded });
   }
 
-  render() {
-    const truncateLength = 50;
-
-    if (this.state.expanded || this.props.long.length <= truncateLength + 2) {
-      return <span>{ this.props.long }</span>;
+  renderText(expanded: boolean) {
+    if (expanded) {
+      return (
+        <span className="expandable__text expandable__text--expanded">{ this.props.long }</span>
+      );
     }
 
     const short = this.props.short || this.props.long.substr(0, truncateLength).trim();
+    return (
+      <span className="expandable__text">{ short }&nbsp;&hellip;</span>
+    );
+  }
 
+  render() {
+    const { short, long } = this.props;
+
+    const neverCollapse = _.isNil(short) && long.length <= truncateLength + 2;
+    if (neverCollapse) {
+      return <span>this.props.long</span>;
+    }
+
+    const { expanded } = this.state;
+    const icon = expanded ? collapseIcon : expandIcon;
     return (
       <div className="expandable" onClick={ this.onClick }>
-        <span className="expandable__text">{ short }&nbsp;&hellip;</span>
-        <span className="expandable__icon" dangerouslySetInnerHTML={ trustIcon(expandIcon) } />
+        { this.renderText(expanded) }
+        <span className="expandable__icon" dangerouslySetInnerHTML={ trustIcon(icon) } />
       </div>
     );
   }
