e88b34aea14f3e0f2c63403ecf31b1cf65d13059
Ben Darnell <ben@cockroachlabs.com>
2018-02-01 21:14:00 -0500

    storage: Enable PreVote by default.
    Fixes #16950
    
    Release note (performance improvement): Reduced disruption from nodes
    recovering from network partitions.

--- pkg/storage/store.go
+++ pkg/storage/store.go
@@ -114,7 +114,7 @@ var storeSchedulerConcurrency = envutil.EnvOrDefaultInt(
 	"COCKROACH_SCHEDULER_CONCURRENCY", 8*runtime.NumCPU())
 
 var enablePreVote = envutil.EnvOrDefaultBool(
-	"COCKROACH_ENABLE_PREVOTE", false)
+	"COCKROACH_ENABLE_PREVOTE", true)
 
 var enableTickQuiesced = envutil.EnvOrDefaultBool(
 	"COCKROACH_ENABLE_TICK_QUIESCED", true)
