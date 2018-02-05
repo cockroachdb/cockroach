18b0ef82e06d160d5804d1c3e9082542ee368e48
Nikhil Benesch <nikhil.benesch@gmail.com>
2018-02-05 17:10:25 -0500

    nightly: create package for nightly tests, incl. partitioning
    Move new-style nightly tests--i.e., those that use roachperf--into a
    dedicated nightly package, and add partitioning tests to the suite.
    
    Release note: none

--- build/nightly-workload.sh
+++ build/nightly-workload.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
-set -ex
+set -eo pipefail
 
-# Below assumes TC_BUILD_ID, so if it's unset, fill it in with something.
-TC_BUILD_ID="${TC_BUILD_ID:-nobuild-`date +%Y-%m-%d-%H%M%S`}"
+if [[ "$GOOGLE_EPHEMERAL_CREDENTIALS" ]]; then
+  echo "$GOOGLE_EPHEMERAL_CREDENTIALS" > creds.json
+  gcloud auth activate-service-account --key-file=creds.json
+  export ROACHPROD_USER=teamcity
+else
+  echo 'warning: GOOGLE_EPHEMERAL_CREDENTIALS not set' >&2
+  echo "Assuming that you've run \`gcloud auth login\` from inside the builder." >&2
+fi
 
-go get -u github.com/cockroachdb/roachprod
+# IMPORTANT: To avoid leaking credentials, we don't set -x until after
+# processing credentials.
+set -x
 
-# teamcity-nightlies assumes cockroach and workload are the cwd.
+go get -u -v github.com/benesch/roachprod
+
 make build TYPE=release-linux-gnu
-# Use this instead of make build for faster debugging iteration.
+
+# Use this instead of the `make build` above for faster debugging iteration.
 # curl -L https://edge-binaries.cockroachdb.com/cockroach/cockroach.linux-gnu-amd64.LATEST -o cockroach-linux-2.6.32-gnu-amd64
 # chmod +x cockroach-linux-2.6.32-gnu-amd64
-cp cockroach-linux-2.6.32-gnu-amd64 cockroach
+
 make bin/workload
-cp bin/workload .
 
-# This `set +x`/paren magic turns off the `set -x` so we don't leak secrets.
-(set +x; echo $GOOGLE_EPHEMERAL_CREDENTIALS > creds_ephemeral.json)
-gcloud auth activate-service-account --key-file=creds_ephemeral.json
-
-# teamcity-nightlies puts the arg we give into the name of every cluster it
-# creates, so this should let us match up clusters to invocations of this build.
-mkdir -p ~/.roachprod/hosts
-./workload teamcity-nightlies teamcity-nightly-workload-${TC_BUILD_ID}
-mkdir -p artifacts
-cp -R workload-test*/* artifacts/
-gsutil -h "Content-Type:text/plain" cp \
-    -R workload-test*/* gs://cockroach-acceptance-results/teamcity-nightly/${TC_BUILD_ID}/
+testflags=(
+  -v -slow
+  -clusterid "${TC_BUILD_ID}"
+  -cockroach "$PWD/cockroach-linux-2.6.32-gnu-amd64"
+  -workload "$PWD/bin/workload"
+  -artifacts "$PWD/artifacts"
+)
+make test \
+  PKG=./pkg/nightly \
+  TESTFLAGS="$(printf "%q " "${testflags[@]}")" \
+  TESTS="${TESTS:-.}" \
+  TESTTIMEOUT=1h
