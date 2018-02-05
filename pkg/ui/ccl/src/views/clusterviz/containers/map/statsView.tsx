da694e633178ddef5a63b14bd4f0c121ba8c6f3c
Pete Vilter <vilterp@cockroachlabs.com>
2018-02-05 13:59:24 -0500

    dump in svg
    Release note: None

--- pkg/ui/ccl/src/views/clusterviz/containers/map/statsView.tsx
+++ pkg/ui/ccl/src/views/clusterviz/containers/map/statsView.tsx
@@ -126,16 +126,91 @@ export class StatsView extends React.Component<StatsViewProps, any> {
     );
   }
 
+  // render() {
+  //   return (
+  //     <g className="locality">
+  //       <g className="capacity-centric">
+  //         {this.renderBackground()}
+  //         {this.renderCapacityArc()}
+  //         {this.props.nodeHistory ? this.renderNetworkActivity() : null}
+  //         {this.renderLabel()}
+  //       </g>
+  //     </g>
+  //   );
+  // }
+
   render() {
     return (
-      <g className="locality">
-        <g className="capacity-centric">
-          {this.renderBackground()}
-          {this.renderCapacityArc()}
-          {this.props.nodeHistory ? this.renderNetworkActivity() : null}
-          {this.renderLabel()}
+      <g fill="none" fillRule="evenodd">
+        <g fill="#3A7DE1">
+          <text transform="translate(21 144)">
+            <tspan x=".194" y="12">320 GB</tspan>
+          </text>
+          <text opacity=".65" transform="translate(21 144)">
+            <tspan x="97.194" y="13">500 GB</tspan>
+          </text>
+        </g>
+        <path stroke="#3A7DE1" strokeLinecap="round" strokeWidth="6" d="M36.244 137.47c-5.536-15.686-4.138-33.656 5.363-48.86 16.563-26.506 51.478-34.566 77.986-18.002 23.374 14.606 32.406 43.48 22.86 68.276" opacity=".35"/>
+        <path stroke="#3A7DE1" strokeLinecap="round" strokeWidth="6" d="M36.244 137.47c-5.536-15.686-4.138-33.656 5.363-48.86 16.563-26.506 51.478-34.566 77.986-18.002"/>
+        <text fill="#3A7DE1" fontFamily="Lato-Bold, Lato" fontSize="34" fontWeight="bold" transform="translate(15 8)">
+          <tspan x="41.129" y="105">64%</tspan>
+        </text>
+        <text fill="#152849" fontFamily="Lato-Bold, Lato" fontSize="12" fontWeight="bold" letterSpacing="1.333" transform="translate(15 8)">
+          <tspan x="41.088" y="124">CAPACITY</tspan>
+        </text>
+        <g fontFamily="Lato-Bold, Lato" fontSize="14" fontWeight="bold" letterSpacing="1">
+          <text fill="#152849" transform="translate(50 8)">
+            <tspan x=".17" y="14">US West Seattle</tspan>
+          </text>
+          <text fill="#3A7DE1" transform="translate(50 8)">
+            <tspan x=".17" y="34">6 Nodes</tspan>
+          </text>
+        </g>
+        <g strokeLinecap="round" strokeLinejoin="round" transform="matrix(-1 0 0 1 36.667 17)">
+          <use fill="#3A7DE1" xlink:href="#a"/>
+          <path stroke="#FFF" d="M14.664 14.086c.002.024.002.038.003.08 0 1.8-3.405 3-7.584 3s-7.583-1.2-7.583-3c0-.062 0-.062.005-.038A1.513 1.513 0 0 1-.5 14V2.667c0-.043.002-.086.006-.041C-.5 2.563-.5 2.563-.5 2.5c0-1.798 3.404-3 7.583-3 4.18 0 7.584 1.202 7.584 3-.001.063-.001.063-.006.038.004.043.006.086.006.129V14c0 .029-.001.057-.003.086z"/>
+        </g>
+        <path stroke="#FFF" strokeWidth="1.03" d="M37.182 20.652H21.985v.515c0 1.81 3.411 3.015 7.598 3.015 4.187 0 7.599-1.204 7.599-3.015v-.515zM37.182 25.652H21.985v.515c0 1.81 3.411 3.015 7.598 3.015 4.187 0 7.599-1.204 7.599-3.015v-.515z"/>
+        <g strokeLinecap="round" strokeLinejoin="round" transform="matrix(-1 0 0 1 30 22)">
+          <use fill="#3A7DE1" xlink:href="#b"/>
+          <path stroke="#FFF" d="M14.664 14.086c.002.024.002.038.003.08 0 1.8-3.405 3-7.584 3s-7.583-1.2-7.583-3c0-.062 0-.062.005-.038A1.513 1.513 0 0 1-.5 14V2.667c0-.043.002-.086.006-.041C-.5 2.563-.5 2.563-.5 2.5c0-1.798 3.404-3 7.583-3 4.18 0 7.584 1.202 7.584 3-.001.063-.001.063-.006.038.004.043.006.086.006.129V14c0 .029-.001.057-.003.086z"/>
+        </g>
+        <path stroke="#FFF" strokeWidth="1.03" d="M30.515 25.652H15.318v.515c0 1.81 3.412 3.015 7.599 3.015s7.598-1.204 7.598-3.015v-.515zM30.515 30.652H15.318v.515c0 1.81 3.412 3.015 7.599 3.015s7.598-1.204 7.598-3.015v-.515z"/>
+        <g strokeLinecap="round" strokeLinejoin="round" transform="matrix(-1 0 0 1 40 25.333)">
+          <use fill="#3A7DE1" xlink:href="#c"/>
+          <path stroke="#FFF" d="M14.664 14.086c.002.024.002.038.003.08 0 1.8-3.405 3-7.584 3s-7.583-1.2-7.583-3c0-.062 0-.062.005-.038A1.513 1.513 0 0 1-.5 14V2.667c0-.043.002-.086.006-.041C-.5 2.563-.5 2.563-.5 2.5c0-1.798 3.404-3 7.583-3 4.18 0 7.584 1.202 7.584 3-.001.063-.001.063-.006.038.004.043.006.086.006.129V14c0 .029-.001.057-.003.086z"/>
+        </g>
+        <path stroke="#FFF" strokeWidth="1.03" d="M40.515 28.985H25.318v.515c0 1.811 3.412 3.015 7.599 3.015s7.598-1.204 7.598-3.015v-.515zM40.515 33.985H25.318v.515c0 1.811 3.412 3.015 7.599 3.015s7.598-1.204 7.598-3.015v-.515z"/>
+        <g transform="translate(56 188)">
+          <mask id="e" fill="#fff">
+            <use xlink:href="#d"/>
+          </mask>
+          <use fill="#3A7DE1" opacity=".35" xlink:href="#d"/>
+          <path stroke="#3A7DE1" strokeWidth="2" d="M-.838 4.29l5.819 3.355L10.984 9l4.429-3.04 5.311 1.685L26.178 2l5.397 7 5.334-3.04h10.656l6.037-.331L57.625 2l4.402 3.922 7.898-2.683" mask="url(#e)"/>
+        </g>
+        <text fill="#3A7DE1" fontFamily="Lato-Black, Lato" fontSize="12" fontWeight="700" transform="translate(15 8)">
+          <tspan x="123" y="189">1342</tspan>
+        </text>
+        <text fill="#152849" fontFamily="Lato-Bold, Lato" fontSize="12" fontWeight="bold" transform="translate(15 8)">
+          <tspan x="5.468" y="189">QPS</tspan>
+        </text>
+        <text fill="#152849" fontFamily="Lato-Bold, Lato" fontSize="12" fontWeight="bold" transform="translate(15 8)">
+          <tspan x="4.718" y="170">CPU</tspan>
+        </text>
+        <text fill="#3A7DE1" fontFamily="Lato-Black, Lato" fontSize="12" fontWeight="700" transform="translate(15 8)">
+          <tspan x="123" y="170">85%</tspan>
+        </text>
+        <path fill="#3A7DE1" d="M56 169h69v10H56z" opacity=".35"/>
+        <path fill="#3A7DE1" d="M56 172h54a2 2 0 0 1 0 4H56v-4z"/>
+        <g>
+          <g transform="translate(5 5)">
+            <use fill="#54B30E" xlink:href="#f"/>
+            <circle cx="8" cy="8" r="8.5" stroke="#FFF"/>
+          </g>
+          <path stroke="#FFF" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="M8.81 13.284l2.878 2.879L16.84 9.57"/>
         </g>
       </g>
     );
   }
+
 }
