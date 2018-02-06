7bad6b97a034cd2644e782e0ec0a1f5be9f4c478
Radu Berinde <radu@cockroachlabs.com>
2018-02-06 16:02:33 -0500

    opt: proper handling of IndexedVars in the exec builder
    `IndexedVar`s are relative to a specific execution node; we need to
    allow arbitrary mappings of `ColumnIndex`es to `IndexedVar`s when
    building scalars. This change adds a scalar build context containing
    the indexed var helper and a column map.
    
    We are able to get away with not having an indexed var container by
    using `IndexedVarWithType`.
    
    Release note: None

--- pkg/sql/opt/exec/build/relational_builder.go
+++ pkg/sql/opt/exec/build/relational_builder.go
@@ -20,9 +20,35 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/exec"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/opt"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/xform"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 )
 
-func (b *Builder) buildRelational(ev xform.ExprView) (exec.Node, error) {
+type execPlan struct {
+	root exec.Node
+
+	// outputCols maps columns in the output set of a relational expression to
+	// indices in the result columns of the ExecNode.
+	//
+	// The reason we need to keep track of this (instead of using just the
+	// relational properties) is that the relational properties don't force a
+	// single "schema": any ordering of the output columns is possible. We choose
+	// the schema that is most convenient: for scans, we use the table's column
+	// ordering. Consider:
+	//   SELECT a, b FROM t WHERE a = b
+	// and the following two cases:
+	//   1. The table is defined as (k INT PRIMARY KEY, a INT, b INT). The scan will
+	//      return (k, a, b).
+	//   2. The table is defined as (k INT PRIMARY KEY, b INT, a INT). The scan will
+	//      return (k, b, a).
+	// In these two cases, the relational properties are effectively the same.
+	//
+	// An alternative to this would be to always use a "canonical" schema, for
+	// example the output columns in increasing index order. However, this would
+	// require a lot of otherwise unnecessary projections.
+	outputCols opt.ColMap
+}
+
+func (b *Builder) buildRelational(ev xform.ExprView) (execPlan, error) {
 	switch ev.Operator() {
 	case opt.ScanOp:
 		return b.buildScan(ev)
@@ -35,17 +61,47 @@ func (b *Builder) buildRelational(ev xform.ExprView) (exec.Node, error) {
 	}
 }
 
-func (b *Builder) buildScan(ev xform.ExprView) (exec.Node, error) {
+func (b *Builder) buildScan(ev xform.ExprView) (execPlan, error) {
 	tblIndex := ev.Private().(opt.TableIndex)
-	tbl := ev.Metadata().Table(tblIndex)
-	return b.factory.ConstructScan(tbl)
+	md := ev.Metadata()
+	tbl := md.Table(tblIndex)
+	node, err := b.factory.ConstructScan(tbl)
+	if err != nil {
+		return execPlan{}, err
+	}
+	res := execPlan{root: node}
+	for i := 0; i < tbl.NumColumns(); i++ {
+		res.outputCols.Set(int(md.TableColumn(tblIndex, i)), i)
+	}
+	return res, nil
 }
 
-func (b *Builder) buildSelect(ev xform.ExprView) (exec.Node, error) {
+func (b *Builder) buildSelect(ev xform.ExprView) (execPlan, error) {
 	input, err := b.buildRelational(ev.Child(0))
 	if err != nil {
-		return nil, err
+		return execPlan{}, err
 	}
-	filter := b.buildScalar(ev.Child(1))
-	return b.factory.ConstructFilter(input, filter)
+	// We need to be able to create IndexedVars for the columns of the input plan.
+	// We get the maximum value from the map to find out how many we need to
+	// support. In most cases this is the same as the Len() of the map; it is TBD
+	// if we will have cases where this doesn't hold.
+	// TODO(radu): Should we just remember the number of columns in execPlan?
+	num := 0
+	if max, ok := input.outputCols.MaxValue(); ok {
+		num = max + 1
+	}
+	ctx := buildScalarCtx{
+		ivh:     tree.MakeIndexedVarHelper(nil /* no container */, num),
+		ivarMap: input.outputCols,
+	}
+	filter := b.buildScalar(&ctx, ev.Child(1))
+	node, err := b.factory.ConstructFilter(input.root, filter)
+	if err != nil {
+		return execPlan{}, err
+	}
+	return execPlan{
+		root: node,
+		// A filtering node does not modify the schema.
+		outputCols: input.outputCols,
+	}, nil
 }
