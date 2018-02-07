a440a0af8182f410e7ff05ba5db6c4f9b7ac09a0
Nikhil Benesch <nikhil.benesch@gmail.com>
2018-02-07 11:45:10 -0500

    build: make TeamCity build logs more readable
    Tap into TeamCity's support for declaring collapsible build log blocks.
    
    Release note: None

--- build/teamcity-test.sh
+++ build/teamcity-test.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
-set -euxo pipefail
 
-export BUILDER_HIDE_GOPATH_SRC=1
+set -euo pipefail
 
 source "$(dirname "${0}")/teamcity-support.sh"
-maybe_ccache
 
-mkdir -p artifacts
+tc_prepare
 
-build/builder.sh go install ./pkg/cmd/github-pull-request-make
+tc_start_block "Maybe stress pull request"
+run build/builder.sh go install ./pkg/cmd/github-pull-request-make
+run build/builder.sh env BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" TARGET=stress github-pull-request-make
+tc_end_block "Maybe stress pull request"
 
-build/builder.sh env \
-	BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" \
-	TARGET=stress \
-	github-pull-request-make
+tc_start_block "Compile"
+run build/builder.sh make -Otarget gotestdashi
+tc_end_block "Compile"
 
-build/builder.sh env \
-	TZ=America/New_York \
-	make test \
-	TESTFLAGS='-v' \
-	2>&1 \
+tc_start_block "Run Go tests"
+run build/builder.sh env TZ=America/New_York make test TESTFLAGS='-v' 2>&1 \
 	| tee artifacts/test.log \
 	| go-test-teamcity
+tc_end_block "Run Go tests"
 
-build/builder.sh make check-libroach
+tc_start_block "Run C++ tests"
+run build/builder.sh make check-libroach
+tc_end_block "Run C++ tests"
--- build/teamcity-testrace.sh
+++ build/teamcity-testrace.sh
@@ -1,39 +1,36 @@
 #!/usr/bin/env bash
-set -euxo pipefail
 
-export BUILDER_HIDE_GOPATH_SRC=1
+set -euo pipefail
 
 source "$(dirname "${0}")/teamcity-support.sh"
-maybe_ccache
 
-mkdir -p artifacts
+tc_prepare
 
+tc_start_block "Maybe stressrace pull request"
 build/builder.sh go install ./pkg/cmd/github-pull-request-make
+build/builder.sh env BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" TARGET=stressrace github-pull-request-make
+tc_end_block "Maybe stressrace pull request"
 
-build/builder.sh env \
-	BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" \
-	TARGET=stressrace \
-	github-pull-request-make
-
-if [[ "${TC_BUILD_BRANCH}" == master ]] || [[ "${TC_BUILD_BRANCH}" == release* ]] ; then
-	PKGSPEC="./pkg/..."
+tc_start_block "Determine changed packages"
+if tc_release_branch; then
+	pkgspec=./pkg/...
+  echo "On release branch ($TC_BUILD_BRANCH), so running testrace on all packages ($pkgspec)"
 else
-	git fetch origin master
-	PKGSPEC=$(git diff --name-only origin/master... | grep "^pkg.*\.go$" || true)
-	if [ -z "${PKGSPEC}" ]; then
-		echo "No changed packages; skipping race detector tests"
+  pkgspec=$(changed_go_pkgs)
+	if [[ -z "$pkgspec" ]]; then
+		echo "PR #$TC_BUILD_BRANCH has no changed packages; skipping race detector tests"
 		exit 0
 	fi
-	# Only keep the pathspecs for which there are go files remaining.
-	PKGSPEC=$(echo "${PKGSPEC}" | xargs -n1 dirname | sort | uniq | sed 's/^/.\//' | { while read path; do if ls "$path"/*.go >/dev/null 2>&1; then echo "$path"; fi; done; } | paste -sd " " -)
-	echo "Running testrace on packages ${PKGSPEC}"
+	echo "PR #$TC_BUILD_BRANCH has changed packages; running race detector tests on $pkgspec"
 fi
+tc_end_block "Determine changed packages"
 
-build/builder.sh env \
-	COCKROACH_LOGIC_TESTS_SKIP=true \
-	make testrace \
-	PKG="${PKGSPEC}" \
-	TESTFLAGS='-v' \
-	2>&1 \
+tc_start_block "Compile"
+run build/builder.sh make -Otarget gotestdashi GOFLAGS=-race
+tc_end_block "Compile"
+
+tc_start_block "Run Go tests under race detector"
+run build/builder.sh env COCKROACH_LOGIC_TESTS_SKIP=true make testrace PKG="$pkgspec" TESTFLAGS='-v' 2>&1 \
 	| tee artifacts/testrace.log \
 	| go-test-teamcity
+tc_end_block "Run Go tests under race detector"
