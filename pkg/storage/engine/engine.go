8666448a44797105a51e8018f22619e7cf5a5f62
David Taylor <tinystatemachine@gmail.com>
2018-02-09 20:19:32 +0000

    storage: don't let Rocks delete sideload's SST
    On successful ingestion, a move_files IngestExternalFile call deletes the passed path, so we need to give rocksdb its own link to the SST so it does not delete the sideload's copy.
    
    Release note (bug fix): Fix AddSSTable accidentially destorying files in the log on success.

--- pkg/storage/engine/engine.go
+++ pkg/storage/engine/engine.go
@@ -249,8 +249,11 @@ type Engine interface {
 	// by invoking Close(). Note that snapshots must not be used after the
 	// original engine has been stopped.
 	NewSnapshot() Reader
-	// IngestExternalFile links a file into the RocksDB log-structured
-	// merge-tree.
+	// IngestExternalFile links a file into the RocksDB log-structured merge-tree.
+	// Removes the passed path on success if `move` is true. May modify the file
+	// (including the underlying file in the case of hard-links) if
+	// allowFileModification is passed as well. See additional comments in db.cc's
+	// IngestExternalFile explaining modification behavior.
 	IngestExternalFile(ctx context.Context, path string, move, allowFileModification bool) error
 	// ApproximateDiskBytes returns an approximation of the on-disk size for the given key span.
 	ApproximateDiskBytes(from, to roachpb.Key) (uint64, error)
