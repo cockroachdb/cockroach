c6fb6497e3efab93bfa50ccfe2a56c03d764ce34
Peter Mattis <petermattis@gmail.com>
2018-02-11 16:16:23 -0500

    cmd/roachtest: refactor nightly tests into roachtest tool
    This converts the tests in the `nightly` package to a new `roachtest`
    tool. At this point, the functionality is equivalent, leaving little
    reason to prefer one approach over the other. Future work is to improve
    the control over parallelism and provide better control of test output.
    
    Release note: None

---
+++ pkg/cmd/roachtest/roachmart.go
@@ -0,0 +1,81 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License. See the AUTHORS file
+// for names of contributors.
+
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func init() {
+	runRoachmart := func(t *test, partition bool) {
+		ctx := context.Background()
+		c := newCluster(ctx, t, "--geo", "--nodes", "9")
+		defer c.Destroy(ctx)
+
+		c.Put(ctx, cockroach, "<cockroach>")
+		c.Put(ctx, workload, "<workload>")
+		c.Start(ctx, 1, 9)
+
+		// TODO(benesch): avoid hardcoding this list.
+		nodes := []struct {
+			i    int
+			zone string
+		}{
+			{1, "us-east1-b"},
+			{4, "us-west1-b"},
+			{7, "europe-west2-b"},
+		}
+
+		roachmartRun := func(ctx context.Context, i int, args ...string) {
+			args = append(args,
+				"--local-zone="+nodes[i].zone,
+				"--local-percent=90",
+				"--users=10",
+				"--orders=100",
+				fmt.Sprintf("--partition=%v", partition))
+
+			l, err := c.l.childLogger(fmt.Sprint(nodes[i].i))
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer l.close()
+			c.RunL(ctx, l, nodes[i].i, args...)
+		}
+		roachmartRun(ctx, 0, "<workload>", "init", "roachmart")
+
+		duration := " --duration=" + ifLocal("10s", "10m")
+
+		m := newMonitor(ctx, c)
+		for i := range nodes {
+			i := i
+			m.Go(func(ctx context.Context) error {
+				roachmartRun(ctx, i, "<workload>", "run", "roachmart", duration)
+				return nil
+			})
+		}
+
+		m.Wait()
+	}
+
+	for _, v := range []bool{true, false} {
+		v := v
+		tests.Add(fmt.Sprintf("roachmart/partition=%v", v),
+			func(t *test) {
+				runRoachmart(t, v)
+			})
+	}
+}
