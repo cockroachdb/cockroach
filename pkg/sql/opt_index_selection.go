d7f35984dd44b275a2cbb282756382e8c110ddc9
Masha Schneider <masha@cockroachlabs.com>
2018-02-12 15:20:41 -0500

    sql: use full index scan for [] and {} with @> queries
    Before we used an inverted index encoding to look up all rows with json arrays or
    json objects. That only yielded rows with an explicit empty object or
    empty array.
    
    Now we are using a full index scan so we get every array for column @> '[]'
    and all object for queries that are c @> '{}'.
    
    Closes #22591
    
    Release note: None

--- pkg/sql/opt_index_selection.go
+++ pkg/sql/opt_index_selection.go
@@ -185,6 +185,17 @@ func (p *planner) selectIndex(
 		}
 	}
 
+	// We're removing inverted index candidates which aren't generating any spans.
+	for i := 0; i < len(candidates); {
+		spans, ok := candidates[i].ic.Spans()
+		if candidates[i].index.Type == sqlbase.IndexDescriptor_INVERTED && (!ok || len(spans) == 0) {
+			candidates[i] = candidates[len(candidates)-1]
+			candidates = candidates[:len(candidates)-1]
+		} else {
+			i++
+		}
+	}
+
 	for _, c := range candidates {
 		if analyzeOrdering != nil {
 			c.analyzeOrdering(ctx, s, analyzeOrdering, preferOrderMatching, p.EvalContext())
@@ -243,7 +254,7 @@ func (p *planner) selectIndex(
 	s.reverse = c.reverse
 
 	var plan planNode
-	if c.covering && c.index.Type != sqlbase.IndexDescriptor_INVERTED {
+	if c.covering {
 		s.initOrdering(c.exactPrefix, p.EvalContext())
 		plan = s
 	} else {
@@ -347,6 +358,9 @@ func (v *indexInfo) analyzeOrdering(
 // the index. This allows a scan of only the index to be performed without requiring subsequent
 // lookup of the full row.
 func (v *indexInfo) isCoveringIndex(scan *scanNode) bool {
+	if v.index.Type == sqlbase.IndexDescriptor_INVERTED {
+		return false
+	}
 	if v.index == &v.desc.PrimaryIndex {
 		// The primary key index always covers all of the columns.
 		return true
