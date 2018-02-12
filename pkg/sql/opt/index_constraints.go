3431601bfe1b160e8a85848b276fcfb9212dede7
Masha Schneider <masha@cockroachlabs.com>
2018-02-12 15:20:41 -0500

    sql: use full index scan for [] and {} with @> queries
    Before we used an inverted index encoding to look up all rows with json arrays or
    json objects. That only yielded rows with an explicit empty object or
    empty array.
    
    Now we are using a full index scan so we get every array for column @> '[]'
    and all object for queries that are c @> '{}'.
    
    Closes #22591
    
    Release note: None

--- pkg/sql/opt/index_constraints.go
+++ pkg/sql/opt/index_constraints.go
@@ -836,6 +836,11 @@ func (c *indexConstraintCtx) makeInvertedIndexSpansForExpr(
 
 		switch rd.Type() {
 		case json.ArrayJSONType, json.ObjectJSONType:
+			// We want to have a full index scan for an empty json array or object on the RHS of @>.
+			if rd.Len() < 1 {
+				return nil, false, false
+			}
+
 			return LogicalSpans{c.makeEqSpan(0 /* offset */, rhs.private.(tree.Datum))}, true, true
 		default:
 			// If we find a scalar on the right side of the @> operator it means that we need to find
