38c6f0747a0552e7cdfba21603682c15d9651791
Andrew Couch <hi@andrewcou.ch>
2018-02-12 17:34:00 -0500

    ui: parameterize createChartComponent on container
    This will allow a chart to be rendered into other element types.
    
    Release note: None

--- pkg/ui/src/views/shared/util/d3-react.tsx
+++ pkg/ui/src/views/shared/util/d3-react.tsx
@@ -2,33 +2,34 @@ import d3 from "d3";
 import React from "react";
 
 type Chart<T> = (sel: d3.Selection<T>) => void;
-type Charter<T> = () => Chart<T>;
 
 /**
  * createChartComponent wraps a D3 reusable chart in a React component.
  * See https://bost.ocks.org/mike/chart/
  */
-export default function createChartComponent<T>(chart: Charter<T>) {
-  return class WrappedChart extends React.Component<T, {}> {
-    svgEl: SVGElement;
-    chart = chart();
+export default function createChartComponent<T>(containerTy: string, chart: Chart<T>) {
+  return class WrappedChart extends React.Component<T> {
+    containerEl: Element;
 
     componentDidMount() {
-      d3.select(this.svgEl)
+      d3.select(this.containerEl)
         .datum(this.props)
-        .call(this.chart);
+        .call(chart);
     }
 
     shouldComponentUpdate(props: T) {
-      d3.select(this.svgEl)
+      d3.select(this.containerEl)
         .datum(props)
-        .call(this.chart);
+        .call(chart);
 
       return false;
     }
 
     render() {
-      return <svg ref={(el) => this.svgEl = el} />;
+      return React.createElement(
+        containerTy,
+        { ref: (el) => this.containerEl = el },
+      );
     }
   };
 }
