3bd1cefd6669f98512a073cedb384c8e60710455
Pete Vilter <vilterp@cockroachlabs.com>
2018-02-13 00:24:54 -0500

    10s => 10h
    Release note: None

--- pkg/ui/src/redux/apiReducers.ts
+++ pkg/ui/src/redux/apiReducers.ts
@@ -53,7 +53,7 @@ export const nodesReducerObj = new CachedDataReducer(
   (req: api.NodesRequestMessage, timeout?: moment.Duration) =>
     api.getNodes(req, timeout).then(rollupStoreMetrics),
   "nodes",
-  moment.duration(10, "h"),
+  moment.duration(10, "s"),
 );
 export const refreshNodes = nodesReducerObj.refresh;
 
