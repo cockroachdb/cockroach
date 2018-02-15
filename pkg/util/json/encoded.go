2130181cd1173ffac143e56b2d4caa495d83fd68
何羿宏 <heyihong.cn@gmail.com>
2018-02-15 12:06:48 +0800

    sql: return original JSON in JSON.RemovePath when JSON path is not valid
    Release note: None

--- pkg/util/json/encoded.go
+++ pkg/util/json/encoded.go
@@ -627,10 +627,10 @@ func (j *jsonEncoded) Format(buf *bytes.Buffer) {
 }
 
 // RemoveIndex implements the JSON interface.
-func (j *jsonEncoded) RemoveIndex(idx int) (JSON, error) {
+func (j *jsonEncoded) RemoveIndex(idx int) (JSON, bool, error) {
 	decoded, err := j.shallowDecode()
 	if err != nil {
-		return nil, err
+		return nil, false, err
 	}
 	return decoded.RemoveIndex(idx)
 }
@@ -645,26 +645,26 @@ func (j *jsonEncoded) Concat(other JSON) (JSON, error) {
 }
 
 // RemoveKey implements the JSON interface.
-func (j *jsonEncoded) RemoveKey(key string) (JSON, error) {
+func (j *jsonEncoded) RemoveKey(key string) (JSON, bool, error) {
 	decoded, err := j.shallowDecode()
 	if err != nil {
-		return nil, err
+		return nil, false, err
 	}
 	return decoded.RemoveKey(key)
 }
 
-func (j *jsonEncoded) RemovePath(path []string) (JSON, error) {
+func (j *jsonEncoded) RemovePath(path []string) (JSON, bool, error) {
 	decoded, err := j.shallowDecode()
 	if err != nil {
-		return nil, err
+		return nil, false, err
 	}
 	return decoded.RemovePath(path)
 }
 
-func (j *jsonEncoded) doRemovePath(path []string) (JSON, error) {
+func (j *jsonEncoded) doRemovePath(path []string) (JSON, bool, error) {
 	decoded, err := j.shallowDecode()
 	if err != nil {
-		return nil, err
+		return nil, false, err
 	}
 	return decoded.doRemovePath(path)
 }
