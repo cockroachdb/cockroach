26009221f6a87798e9bf80f904d507878d58930a
Andrew Kimball <andyk@cockroachlabs.com>
2018-02-24 23:11:31 -0800

    opt: Add Ordering physical property
    The Ordering physical property requires result rows to be sorted
    according to a desired key. This is usually the result of an ORDER BY
    expression, but can also be a requirement imposed by an operator like
    merge join. If an expression can provide the required ordering (e.g.
    a Scan operator that returns rows in primary key order), then nothing
    more is needed. If not, then the optimizer will insert a Sort enforcer
    into the expression tree to provide the ordering.
    
    The memo group now keeps a "best expression map", which remembers the
    lowest cost expression for that group, that also provides each required
    set of properties. As various alternatives are explored, the best expr
    is updated as lower cost expressions are found. The optimizer now
    traverses the whole expression tree in order to find best expression(s)
    for each group and to insert enforcers where needed.
    
    This PR also contains a bare-bones implementation of ORDER BY for
    testing purposes; it will need to be fully implemented later.
    
    Release note: None

--- pkg/sql/opt/xform/memo_group.go
+++ pkg/sql/opt/xform/memo_group.go
@@ -15,10 +15,8 @@
 package xform
 
 import (
-	"bytes"
-	"fmt"
-
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/opt"
+	"github.com/cockroachdb/cockroach/pkg/util"
 )
 
 // exprID is the index of an expression within its group. exprID = 0 is always
@@ -33,9 +31,7 @@ const (
 // memoGroup stores a set of logically equivalent expressions. See the comments
 // on memoExpr for the definition of logical equivalency. In addition, for each
 // required set of physical properties, the group stores the expression that
-// provides those properties at the lowest known cost.
-//
-// TODO(andyk): Need to add the lowest cost expression map.
+// provides those properties at the lowest known cost in the bestExprs slice.
 type memoGroup struct {
 	// Id is the index of this group within the memo.
 	id opt.GroupID
@@ -48,6 +44,12 @@ type memoGroup struct {
 	// the group. The first expression is always the group's normalized
 	// expression.
 	exprs []memoExpr
+
+	// bestExprs remembers the lowest cost expression in the group that provides
+	// a particular set of physical properties. The FastIntMap key is an
+	// opt.PhysicalPropID, and the value is an index into bestExprs.
+	bestExprsMap util.FastIntMap
+	bestExprs    []bestExpr
 }
 
 // lookupExpr looks up an expression in the group by its index.
@@ -55,57 +57,42 @@ func (g *memoGroup) lookupExpr(eid exprID) *memoExpr {
 	return &g.exprs[eid]
 }
 
-func (g *memoGroup) memoGroupString(mem *memo) string {
-	var buf bytes.Buffer
-
-	for i, mexpr := range g.exprs {
-		if i != 0 {
-			buf.WriteByte(' ')
-		}
-
-		// Wrap the memo expr in ExprView to make it easy to get children.
-		ev := ExprView{
-			mem:      mem,
-			loc:      memoLoc{group: g.id, expr: exprID(i)},
-			op:       mexpr.op,
-			required: opt.NormPhysPropsID,
-		}
-
-		fmt.Fprintf(&buf, "[%s", ev.Operator())
-
-		private := ev.Private()
-		if private != nil {
-			switch t := private.(type) {
-			case nil:
-			case opt.TableIndex:
-				fmt.Fprintf(&buf, " %s", mem.metadata.Table(t).TabName())
-			case opt.ColumnIndex:
-				fmt.Fprintf(&buf, " %s", mem.metadata.ColumnLabel(t))
-			case *opt.ColSet, *opt.ColMap:
-				// Don't show anything, because it's mostly redundant.
-			default:
-				fmt.Fprintf(&buf, " %s", private)
-			}
-		}
-
-		if ev.ChildCount() > 0 {
-			fmt.Fprintf(&buf, " [")
-			for i := 0; i < ev.ChildCount(); i++ {
-				child := ev.ChildGroup(i)
-				if i > 0 {
-					buf.WriteString(" ")
-				}
-				if child <= 0 {
-					buf.WriteString("-")
-				} else {
-					fmt.Fprintf(&buf, "%d", child)
-				}
-			}
-			fmt.Fprintf(&buf, "]")
-		}
-
-		buf.WriteString("]")
+// lookupBestExpr looks up the bestExpr that has the lowest cost for the given
+// required properties. If no bestExpr exists yet, lookupBestExpr returns nil.
+// NOTE: The returned bestExpr reference is only valid until the next call to
+//       ensureBestExpr, which may cause a resize of the bestExprs slice.
+func (g *memoGroup) lookupBestExpr(required opt.PhysicalPropsID) *bestExpr {
+	index, ok := g.bestExprsMap.Get(int(required))
+	if !ok {
+		return nil
 	}
+	return &g.bestExprs[index]
+}
 
-	return buf.String()
+// ensureBestExpr looks up the bestExpr that has the lowest cost for the given
+// required properties. If no bestExpr exists yet, then ensureBestExpr creates
+// an empty bestExpr, adds it to bestExprsMap, and returns it.
+// NOTE: The returned bestExpr reference is only valid until the next call to
+//       ensureBestExpr, which may cause a resize of the bestExprs slice.
+func (g *memoGroup) ensureBestExpr(required opt.PhysicalPropsID) *bestExpr {
+	best := g.lookupBestExpr(required)
+	if best == nil {
+		// Add new best expression.
+		index := len(g.bestExprs)
+		g.bestExprs = append(g.bestExprs, bestExpr{})
+		g.bestExprsMap.Set(int(required), index)
+		best = &g.bestExprs[index]
+	}
+	return best
+}
+
+// forEachBestExpr iterates over the bestExpr map and calls the given function
+// for each pair of (required property set, best expression).
+// NOTE: The bestExpr references passed to the function are only valid until
+//       the next call to ensureBestExpr, which may cause a resize of the
+//       bestExprs slice.
+func (g *memoGroup) forEachBestExpr(fn func(required opt.PhysicalPropsID, best *bestExpr)) {
+	g.bestExprsMap.ForEach(func(required, index int) {
+		fn(opt.PhysicalPropsID(required), &g.bestExprs[index])
+	})
 }
---
+++ pkg/sql/opt/xform/physical_props_factory.go
@@ -0,0 +1,165 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package xform
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/opt"
+)
+
+// physicalPropsFactory determines what physical properties a given expression
+// provides and constructs the physical properties required of its children
+// based on those properties. The optimizer calls the canProvide methods to
+// determine whether an expression provides a required physical property. If it
+// does not, then the optimizer inserts an enforcer operator that is able to
+// provide it. Some operators, like Select and Project, may not directly
+// provide a required physical property, but do "pass through" the requirement
+// to their input. Operators that do this should return true from the
+// appropriate canProvide method and then pass through that property in the
+// constructChildProps method.
+// NOTE: The factory is defined as an empty struct with methods rather than as
+//       functions in order to keep the methods grouped and self-contained.
+type physicalPropsFactory struct{}
+
+// canProvide returns true if the given expression can provide the required
+// physical properties. Don't access child expressions using the view (child
+// groups are OK), because they are not guaranteed to be available yet, since
+// physical properties are required starting at the root of the tree and
+// proceeding downwards (top-down).
+func (f physicalPropsFactory) canProvide(ev ExprView, required opt.PhysicalPropsID) bool {
+	requiredProps := ev.mem.lookupPhysicalProps(required)
+
+	if requiredProps.Presentation.Defined() {
+		if !f.canProvidePresentation(ev, requiredProps.Presentation) {
+			return false
+		}
+	}
+
+	if requiredProps.Ordering.Defined() {
+		if !f.canProvideOrdering(ev, requiredProps.Ordering) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// canProvidePresentation returns true if the given expression can provide the
+// required presentation property. Currently, all relational operators are
+// capable of doing this.
+func (f physicalPropsFactory) canProvidePresentation(ev ExprView, required opt.Presentation) bool {
+	if !ev.IsRelational() {
+		panic("presentation property doesn't apply to non-relational operators")
+	}
+
+	// All operators can provide the Presentation property.
+	return true
+}
+
+// canProvideOrdering returns true if the given expression can provide the
+// required ordering property.
+func (f physicalPropsFactory) canProvideOrdering(ev ExprView, required opt.Ordering) bool {
+	if !ev.IsRelational() {
+		panic("ordering property doesn't apply to non-relational operators")
+	}
+
+	switch ev.Operator() {
+	case opt.SelectOp:
+		// Select operator can always pass through ordering to its input.
+		return true
+
+	case opt.ProjectOp:
+		// Project operator can pass through ordering if it operates only on
+		// input columns.
+		inputCols := ev.lookupChildGroup(0).logical.Relational.OutputCols
+		for _, colOrder := range required {
+			if colOrder < 0 {
+				// Descending order, so recover original index.
+				colOrder = -colOrder
+			}
+			if !inputCols.Contains(int(colOrder)) {
+				return false
+			}
+		}
+		return true
+
+	case opt.ScanOp:
+		// Scan naturally orders according to the primary index.
+		tblIdx := ev.Private().(opt.TableIndex)
+		primary := ev.Metadata().Table(tblIdx).Primary()
+
+		ordering := make(opt.Ordering, primary.ColumnCount())
+		for i := 0; i < primary.ColumnCount(); i++ {
+			idxCol := primary.Column(i)
+			ordering[i] = ev.Metadata().TableColumn(tblIdx, idxCol.Ordinal)
+			if idxCol.Descending {
+				// Negative metadata column index indicates descending order.
+				ordering[i] = -ordering[i]
+			}
+		}
+
+		return ordering.Provides(required)
+	}
+
+	return false
+}
+
+// constructChildProps returns the set of physical properties required of the
+// nth child, based upon the properties required of the parent. For example,
+// the Project operator passes through any ordering requirement to its child,
+// but provides any presentation requirement. This method is heavily called
+// during ExprView traversal, so performance is important.
+func (f physicalPropsFactory) constructChildProps(ev ExprView, nth int) opt.PhysicalPropsID {
+	// Fast path taken in common case when no properties are required of parent;
+	// in that case, no properties are required of children. This will change in
+	// the future when certain operators like MergeJoin require input properties
+	// from their children regardless of what's required of them.
+	if ev.required == opt.MinPhysPropsID {
+		return opt.MinPhysPropsID
+	}
+
+	parentProps := ev.Physical()
+
+	var childProps opt.PhysicalProps
+	var changed bool
+
+	// Presentation property is provided by all the relational operators, so
+	// don't add it to childProps.
+	if parentProps.Presentation.Defined() {
+		changed = true
+	}
+
+	// Check for operators that might pass through the ordering property to
+	// input.
+	if parentProps.Ordering.Defined() {
+		switch ev.Operator() {
+		case opt.SelectOp, opt.ProjectOp:
+			if nth == 0 {
+				childProps.Ordering = parentProps.Ordering
+				break
+			}
+			fallthrough
+
+		default:
+			changed = true
+		}
+	}
+
+	// If properties haven't changed, then don't need to re-intern them.
+	if !changed {
+		return ev.required
+	}
+
+	return ev.mem.internPhysicalProps(&childProps)
+}
