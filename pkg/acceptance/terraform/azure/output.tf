adf4749a7db99dc1abed16de8f013cb13992bb2b
Daniel Harrison <daniel.harrison@gmail.com>
2018-02-28 11:11:36 -0500

    acceptance: remove terraform and terrafarm
    All the tests have been migrated to the new roachtest nightlies.
    
    Release note: None
    
    Closes #22646.

--- pkg/acceptance/terraform/azure/output.tf
+++
@@ -1,8 +0,0 @@
-# Unlike the other cloud providers, this is a list of FQDNs instead of IP
-# addresses. This is because of an issue preventing the querying of dynamic
-# public IPs:
-#
-# https://github.com/hashicorp/terraform/issues/6634
-output "instances" {
-  value = "${join(",", azurerm_public_ip.cockroach.*.fqdn)}"
-}
