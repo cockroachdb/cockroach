6139ff55f2fb7b39966bd5f02af94f8f70e46ce3
Pete Vilter <vilterp@cockroachlabs.com>
2018-02-28 17:31:58 -0500

    ui: clusterviz: correct liveness status text on nodes
    It used to always say "up for <duration>" no matter what the liveness
    status was. Now it will still say that if the node is live, and will say
    the status of the node otherwise.
    
    Release note (admin ui change): clusterviz: correct liveness status text on nodes

--- pkg/ui/ccl/src/views/clusterviz/containers/map/nodeView.tsx
+++ pkg/ui/ccl/src/views/clusterviz/containers/map/nodeView.tsx
@@ -23,7 +23,7 @@ import { CapacityArc } from "src/views/clusterviz/components/nodeOrLocality/capa
 import { Sparklines } from "src/views/clusterviz/components/nodeOrLocality/sparklines";
 import { LongToMoment } from "src/util/convert";
 
-type NodeLivenessStatus = cockroach.storage.NodeLivenessStatus;
+import NodeLivenessStatus = cockroach.storage.NodeLivenessStatus;
 
 interface NodeViewProps {
   node: NodeStatus$Properties;
@@ -45,12 +45,36 @@ export class NodeView extends React.Component<NodeViewProps> {
     return liveIcon;
   }
 
+  getUptimeText() {
+    const { node, liveness } = this.props;
+
+    const thisLiveness = liveness[node.desc.node_id];
+
+    switch (thisLiveness) {
+      case NodeLivenessStatus.DEAD: {
+        // TODO(vilterp): pipe in how long it's been dead for
+        return "dead";
+      }
+      case NodeLivenessStatus.LIVE: {
+        const startTime = LongToMoment(node.started_at);
+        return "up for " + moment.duration(startTime.diff(moment())).humanize();
+      }
+      case NodeLivenessStatus.DECOMMISSIONED:
+        return "decommissioned";
+      case NodeLivenessStatus.DECOMMISSIONING:
+        return "decommissioning";
+      case NodeLivenessStatus.UNKNOWN:
+        return "unknown";
+      case NodeLivenessStatus.UNAVAILABLE:
+        return "unavailable";
+      default:
+        return ""; // idk man
+    }
+  }
+
   render() {
     const { node, liveness } = this.props;
     const { capacityUsable, capacityUsed, nodeCounts } = sumNodeStats([node], liveness);
-
-    const startTime = LongToMoment(node.started_at);
-    const uptimeText = "up for " + moment.duration(startTime.diff(moment())).humanize();
 
     return (
       <Link
@@ -58,10 +82,9 @@ export class NodeView extends React.Component<NodeViewProps> {
         style={{ cursor: "pointer" }}
       >
         <g transform={`translate(${TRANSLATE_X},${TRANSLATE_Y})scale(${SCALE_FACTOR})`}>
-          <rect width={180} height={210} opacity={0} />
           <Labels
             label={`Node ${node.desc.node_id}`}
-            subLabel={uptimeText}
+            subLabel={this.getUptimeText()}
             tooltip={node.desc.address.address_field}
           />
           <g dangerouslySetInnerHTML={trustIcon(nodeIcon)} transform="translate(14 14)" />
