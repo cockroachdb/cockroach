681d3b4a8c54008fdcb659698ea1db215e67877f
Pete Vilter <vilterp@cockroachlabs.com>
2018-03-06 17:26:23 -0500

    ui: clusterviz: expand/collapse instruction box; persist to localStorage
    Release note: None

--- pkg/ui/ccl/src/views/clusterviz/components/instructionsBox/index.tsx
+++ pkg/ui/ccl/src/views/clusterviz/components/instructionsBox/index.tsx
@@ -11,15 +11,36 @@ interface InstructionsBoxProps {
   locationTree: LocationTree;
 }
 
-export class InstructionsBox extends React.Component<InstructionsBoxProps> {
+interface InstructionBoxState {
+  expanded: boolean;
+}
+
+const LOCAL_STORAGE_KEY = "clusterviz-instruction-box-expanded";
+
+export class InstructionsBox extends React.Component<InstructionsBoxProps, InstructionBoxState> {
   constructor() {
     super();
+    const valFromStorage = localStorage.getItem(LOCAL_STORAGE_KEY);
+    let expanded = true;
+    switch (valFromStorage) {
+      case "false":
+        expanded = false;
+    }
     this.state = {
-      collapsed: false,
+      expanded,
     };
   }
 
-  render() {
+  handleToggle = () => {
+    const expanded = !this.state.expanded;
+    const val = `${expanded}`;
+    localStorage.setItem(LOCAL_STORAGE_KEY, val);
+    this.setState({
+      expanded,
+    });
+  }
+
+  renderExpanded() {
     const status = getConfigStatus(this.props.localityTree, this.props.locationTree);
 
     const todos: string[] = [];
@@ -38,10 +59,28 @@ export class InstructionsBox extends React.Component<InstructionsBoxProps> {
     }
 
     return (
-      <div className="instructions-box">
-        To see a map, {todos.join(" and ")}.
-        <br />
-        <a href="http://cockroachlabs.com/docs">Docs</a>
+      <div>
+        To see a map, {todos.join(" and ")}.{" "}
+        (<a href="http://cockroachlabs.com/docs">See Docs</a>)
+      </div>
+    );
+  }
+
+  renderCollapsed() {
+    return "?";
+  }
+
+  render() {
+    let content = null;
+    if (this.state.expanded) {
+      content = this.renderExpanded();
+    } else {
+      content = this.renderCollapsed();
+    }
+
+    return (
+      <div onClick={() => this.handleToggle()} className="instructions-box">
+        {content}
       </div>
     );
   }
