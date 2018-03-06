e7bf0404cb25e085a3956dde212986921d5f4a24
Pete Vilter <vilterp@cockroachlabs.com>
2018-03-06 16:50:01 -0500

    ui: clusterviz: add box with screenshot config instructions
    The box appears when
    - you are looking at the circle layout
    - AND the circle layout has some localities in it
    - AND some nodes don't have localities.
    
    Release note (admin ui change): Add box in bottom right of cluster viz
    with instructions on how to configure it to get a map.

---
+++ pkg/ui/ccl/src/views/clusterviz/components/instructionsBox/instructionsBox.spec.ts
@@ -0,0 +1,52 @@
+import { assert } from "chai";
+
+import { showInstructionsBox } from "src/views/clusterviz/components/instructionsBox";
+import { LocalityTier } from "src/redux/localities";
+
+describe("InstructionsBox component", () => {
+
+  describe("showInstructionsBox", () => {
+
+    interface TestCase {
+      showMap: boolean;
+      tiers: LocalityTier[];
+      expected: boolean;
+      desc: string;
+    }
+
+    const cases: TestCase[] = [
+      {
+        desc: "showing map, at top level",
+        showMap: true,
+        tiers: [],
+        expected: false,
+      },
+      {
+        desc: "showing map, down a level",
+        showMap: true,
+        tiers: [{ key: "foo", value: "bar" }],
+        expected: false,
+      },
+      {
+        desc: "not showing map, at top level",
+        showMap: false,
+        tiers: [],
+        expected: true,
+      },
+      {
+        desc: "not showing map, down a level",
+        showMap: false,
+        tiers: [{ key: "foo", value: "bar" }],
+        expected: false,
+      },
+    ];
+
+    cases.forEach((testCase) => {
+      it(`returns ${testCase.expected} for case "${testCase.desc}"`, () => {
+        assert.equal(showInstructionsBox(testCase.showMap, testCase.tiers), testCase.expected);
+      });
+    });
+
+  });
+
+});
