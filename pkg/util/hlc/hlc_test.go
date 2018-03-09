edafd1f63fe6b0ab9ec5803be27265d8e18b2ea8
Vijay Karthik <vijay.karthik@rubrik.com>
2018-03-09 12:08:48 +0530

    util/hlc: option to panic on clock jumps
    A cluster setting is added to panic on clock jumps. The existing
    safeguard in forward clock jumps is in periodic inter-node heartbeats,
    so there is a window where anomalies could occur.
    
    This change adds forward jump checks to HLC (and a background
    goroutine to keep lastPhysicalTime up to date).
    
    Release note (general change): Added cluster settings for HLC to
    panic on clock jumps

--- pkg/util/hlc/hlc_test.go
+++ pkg/util/hlc/hlc_test.go
@@ -17,10 +17,14 @@ package hlc
 import (
 	"context"
 	"fmt"
+	"os"
+	"regexp"
 	"testing"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/stretchr/testify/assert"
 )
 
 type Event uint8
@@ -95,6 +99,176 @@ func TestHLCEqual(t *testing.T) {
 	a = c.Now() // add one to logical clock from b
 	if b == a {
 		t.Errorf("expected %+v < %+v", b, a)
+	}
+}
+
+// isErrSimilar returns true of the expected error is similar to the
+// actual error
+func isErrSimilar(expected *regexp.Regexp, actual error) bool {
+	if actual == nil {
+		return expected == nil
+	}
+	// actual != nil
+	return expected != nil && expected.FindString(actual.Error()) != ""
+}
+
+func TestHLCPhysicalClockJump(t *testing.T) {
+	var fatal bool
+	defer log.SetExitFunc(os.Exit)
+	log.SetExitFunc(func(r int) {
+		if r != 0 {
+			fatal = true
+		}
+	})
+
+	testCases := []struct {
+		name       string
+		actualJump time.Duration
+		maxOffset  time.Duration
+		isFatal    bool
+	}{
+		{
+			name:       "small forward jump",
+			actualJump: 50 * time.Millisecond,
+			maxOffset:  500 * time.Millisecond,
+			isFatal:    false,
+		},
+		{
+			name:       "half max offset jump",
+			actualJump: 250 * time.Millisecond,
+			maxOffset:  500 * time.Millisecond,
+			isFatal:    true,
+		},
+		{
+			name:       "large forward jump",
+			actualJump: 400 * time.Millisecond,
+			maxOffset:  500 * time.Millisecond,
+			isFatal:    true,
+		},
+		{
+			name:       "large forward jump large thresh",
+			actualJump: 400 * time.Millisecond,
+			maxOffset:  900 * time.Millisecond,
+			isFatal:    false,
+		},
+		{
+			name:       "small backward jump",
+			actualJump: -40 * time.Millisecond,
+			maxOffset:  500 * time.Millisecond,
+			isFatal:    false,
+		},
+		{
+			name:       "large backward jump",
+			actualJump: -700 * time.Millisecond,
+			maxOffset:  500 * time.Millisecond,
+			isFatal:    false,
+		},
+		{
+			name:       "large backward jump large thresh",
+			actualJump: -700 * time.Millisecond,
+			maxOffset:  900 * time.Millisecond,
+			isFatal:    false,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			a := assert.New(t)
+
+			m := NewManualClock(1)
+			c := NewClock(m.UnixNano, test.maxOffset)
+			var tickerDuration time.Duration
+			tickerCh := make(chan time.Time)
+			tickProcessedCh := make(chan struct{})
+			forwardJumpCheckEnabledCh := make(chan bool, 1)
+			defer close(forwardJumpCheckEnabledCh)
+
+			if err := c.StartMonitoringForwardClockJumps(
+				forwardJumpCheckEnabledCh,
+				func(d time.Duration) *time.Ticker {
+					tickerDuration = d
+					ticker := time.NewTicker(d)
+					ticker.Stop()
+					ticker.C = tickerCh
+					return ticker
+				},
+				func() {
+					tickProcessedCh <- struct{}{}
+				},
+			); err != nil {
+				t.Error(err)
+				return
+			}
+
+			if err := c.StartMonitoringForwardClockJumps(
+				forwardJumpCheckEnabledCh,
+				time.NewTicker,
+				nil, /* tick callback */
+			); !isErrSimilar(regexp.MustCompile("already being monitored"), err) {
+				t.Error("expected an error when starting monitor goroutine twice")
+			}
+
+			fatal = false
+			t0 := c.Now()
+			a.Equal(false, fatal)
+
+			// forward jump check should be disabled unless set to true. This should
+			// not fatal even though it is a large jump
+			m.Increment(int64(test.maxOffset))
+			fatal = false
+			t1 := c.Now()
+			a.True(t0.Less(t1), fmt.Sprintf("expected %+v < %+v", t0, t1))
+			a.Equal(false, fatal)
+
+			forwardJumpCheckEnabledCh <- true
+			<-tickProcessedCh
+
+			m.Increment(int64(test.actualJump))
+			tickerCh <- timeutil.Now()
+			<-tickProcessedCh
+
+			fatal = false
+			t2 := c.Now()
+			a.True(t1.Less(t2), fmt.Sprintf("expected %+v < %+v", t1, t2))
+			// This should not fatal as tickerCh has ticked
+			a.Equal(false, fatal)
+			// After ticker ticks, last physical time should be equal to physical now
+			lastPhysicalTime := c.lastPhysicalTime()
+			physicalNow := c.PhysicalNow()
+			a.Equal(lastPhysicalTime, physicalNow)
+
+			// Potentially a fatal jump depending on the test case
+			fatal = false
+			m.Increment(int64(test.actualJump))
+			t3 := c.Now()
+			a.True(t2.Less(t3), fmt.Sprintf("expected %+v < %+v", t2, t3))
+			a.Equal(test.isFatal, fatal)
+
+			a.True(
+				tickerDuration <= test.maxOffset,
+				fmt.Sprintf(
+					"ticker duration %+v should be less than max jump %+v",
+					tickerDuration,
+					test.maxOffset,
+				),
+			)
+
+			// A jump by maxOffset is surely fatal
+			fatal = false
+			m.Increment(int64(test.maxOffset))
+			t4 := c.Now()
+			a.True(t3.Less(t4), fmt.Sprintf("expected %+v < %+v", t3, t4))
+			a.Equal(true, fatal)
+
+			// disable forward jump check
+			forwardJumpCheckEnabledCh <- false
+			<-tickProcessedCh
+			fatal = false
+			m.Increment(int64(test.actualJump))
+			t5 := c.Now()
+			a.True(t4.Less(t5), fmt.Sprintf("expected %+v < %+v", t4, t5))
+			a.Equal(false, fatal)
+		})
 	}
 }
 
