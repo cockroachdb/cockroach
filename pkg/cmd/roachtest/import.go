2f842f9022a60721232d203746cbcfd2479544d5
Peter Mattis <petermattis@gmail.com>
2018-03-09 14:52:00 -0500

    roachtest: rework test registration
    Replaced the paramters to `registry.Add` with a `testSpec` structur. In
    addition to a test name and run function, `testSpec` contains a `Nodes`
    spec. This allows declarative specification of the resources needed for
    a test which will allow the test runner to better utilize resources when
    scheduling tests and ensure that running tests in parallel doesn't
    exceed resource quotas.
    
    Fixes #22942
    
    Release note: None

--- pkg/cmd/roachtest/import.go
+++ pkg/cmd/roachtest/import.go
@@ -25,21 +25,17 @@ const (
 )
 
 func init() {
-	runImportTPCC := func(t *test, warehouses, nodes int) {
-		ctx := context.Background()
-		c := newCluster(ctx, t, nodes)
-		defer c.Destroy(ctx)
-
-		c.Put(ctx, cockroach, "./cockroach", c.Range(1, nodes))
-		c.Put(ctx, workload, "./workload", c.Range(1, nodes))
+	runImportTPCC := func(ctx context.Context, t *test, c *cluster, warehouses int) {
+		c.Put(ctx, cockroach, "./cockroach")
+		c.Put(ctx, workload, "./workload")
 		t.Status("starting csv servers")
-		c.Start(ctx, c.Range(1, nodes))
-		for node := 1; node <= nodes; node++ {
+		c.Start(ctx)
+		for node := 1; node <= c.nodes; node++ {
 			c.Run(ctx, node, `./workload csv-server --port=8081 &> logs/workload-csv-server.log < /dev/null &`)
 		}
 
 		t.Status("running workload")
-		m := newMonitor(ctx, c, c.Range(1, nodes))
+		m := newMonitor(ctx, c)
 		m.Go(func(ctx context.Context) error {
 			cmd := fmt.Sprintf(
 				`./workload fixtures make tpcc --warehouses=%d --csv-server='http://localhost:8081' `+
@@ -51,28 +47,31 @@ func init() {
 		m.Wait()
 	}
 
-	const warehouses, nodes = 1000, 4
-	tests.Add(fmt.Sprintf("import/tpcc/warehouses=%d/nodes=%d", warehouses, nodes), func(t *test) {
-		runImportTPCC(t, warehouses, nodes)
+	const warehouses = 1000
+	const numNodes = 4
+	tests.Add(testSpec{
+		Name:  fmt.Sprintf("import/tpcc/warehouses=%d/nodes=%d", warehouses, numNodes),
+		Nodes: nodes(numNodes),
+		Run: func(ctx context.Context, t *test, c *cluster) {
+			runImportTPCC(ctx, t, c, warehouses)
+		},
 	})
 }
 
 func init() {
-	for _, nodes := range []int{4, 8} {
-		nodes := nodes
-		tests.Add(fmt.Sprintf(`import/tpch/nodes=%d`, nodes), func(t *test) {
-			ctx := context.Background()
-			c := newCluster(ctx, t, nodes)
-			defer c.Destroy(ctx)
-
-			c.Put(ctx, cockroach, "./cockroach")
-			c.Start(ctx)
-			conn := c.Conn(ctx, 1)
-			if _, err := conn.Exec(`create database csv`); err != nil {
-				t.Fatal(err)
-			}
-			c.status(`running import`)
-			if _, err := conn.Exec(`
+	for _, n := range []int{4, 8} {
+		tests.Add(testSpec{
+			Name:  fmt.Sprintf(`import/tpch/nodes=%d`, n),
+			Nodes: nodes(n),
+			Run: func(ctx context.Context, t *test, c *cluster) {
+				c.Put(ctx, cockroach, "./cockroach")
+				c.Start(ctx)
+				conn := c.Conn(ctx, 1)
+				if _, err := conn.Exec(`create database csv`); err != nil {
+					t.Fatal(err)
+				}
+				c.status(`running import`)
+				if _, err := conn.Exec(`
 				IMPORT TABLE csv.lineitem
 				CREATE USING 'gs://cockroach-fixtures/tpch-csv/schema/lineitem.sql'
 				CSV DATA (
@@ -86,8 +85,9 @@ func init() {
 				'gs://cockroach-fixtures/tpch-csv/sf-100/lineitem.tbl.8'
 				) WITH  delimiter='|'
 			`); err != nil {
-				t.Fatal(err)
-			}
+					t.Fatal(err)
+				}
+			},
 		})
 	}
 }
