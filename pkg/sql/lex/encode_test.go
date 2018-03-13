ffb775df1eda6a2e948234ecfb09f830721db16b
Jordan Lewis <jordanthelewis@gmail.com>
2018-03-13 11:23:23 -0400

    sql: add quote_ident builtin to pg_catalog
    Release note (sql change): add the quote_ident builtin for increased
    postgres compatibility.

--- pkg/sql/lex/encode_test.go
+++ pkg/sql/lex/encode_test.go
@@ -96,3 +96,41 @@ func BenchmarkEncodeSQLString(b *testing.B) {
 		}
 	})
 }
+
+func TestEncodeRestrictedSQLIdent(t *testing.T) {
+	testCases := []struct {
+		input  string
+		output string
+	}{
+		{`foo`, `foo`},
+		{``, `""`},
+		{`3`, `"3"`},
+		{`foo3`, `foo3`},
+		{`foo"`, `"foo"""`},
+		{`fo"o"`, `"fo""o"""`},
+		{`fOo`, `"fOo"`},
+		{`_foo`, `_foo`},
+		{`-foo`, `"-foo"`},
+		{`select`, `"select"`},
+		{`integer`, `"integer"`},
+		// N.B. These type names are examples of type names that *should* be
+		// unrestricted (left out of the reserved keyword list) because they're not
+		// part of the sql standard type name list. This is important for Postgres
+		// compatibility. If you find yourself about to change this, don't - you can
+		// convince yourself of such by looking at the output of `quote_ident`
+		// against a Postgres instance.
+		{`int8`, `int8`},
+		{`date`, `date`},
+		{`inet`, `inet`},
+	}
+
+	for _, tc := range testCases {
+		var buf bytes.Buffer
+		lex.EncodeRestrictedSQLIdent(&buf, tc.input, lex.EncBareStrings)
+		out := buf.String()
+
+		if out != tc.output {
+			t.Errorf("`%s`: expected `%s`, got `%s`", tc.input, tc.output, out)
+		}
+	}
+}
