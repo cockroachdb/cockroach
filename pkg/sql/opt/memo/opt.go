02d53d05150a7daf68ef7c136e0f7162b558eb6a
Andrew Kimball <andyk@cockroachlabs.com>
2018-03-17 20:36:09 -0700

    opt: Move files to new memo package directory
    Move files to new location. This commit will be followed up by another
    that makes the changes necessary to build.
    
    Release note: None

--- pkg/sql/opt/memo/opt.go
+++ pkg/sql/opt/memo/opt.go
@@ -12,7 +12,7 @@
 // implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
-package opt
+package memo
 
 // GroupID identifies a memo group. Groups have numbers greater than 0; a
 // GroupID of 0 indicates an invalid group.
