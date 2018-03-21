cockroachdb/cockroach

commit 75cbeb84848c807437ce84dcb93229d17fbb4a7e

roachpb: disable roachpb.Value checksum computation
Fixes #21435.

This change disables roachpb.Value checksum computation, leaving
the checksum field blank on roachpb.Values.

See https://github.com/cockroachdb/cockroach/issues/21435#issuecomment-358389164
for details on the measured performance improvement from this change.

Release note (performance improvement): Unnecessary value
checksums are no longer computed, speeding up database writes.

pkg/roachpb/data.go
@@ -34,6 +34,7 @@ import (
 
 	"github.com/cockroachdb/apd"
 	"github.com/cockroachdb/cockroach/pkg/storage/engine/enginepb"
+	"github.com/cockroachdb/cockroach/pkg/util"
 	"github.com/cockroachdb/cockroach/pkg/util/duration"
 	"github.com/cockroachdb/cockroach/pkg/util/encoding"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
@@ -236,13 +237,37 @@ func (v *Value) setChecksum(cksum uint32) {
 	}
 }
 
-// InitChecksum initializes a checksum based on the provided key and
-// the contents of the value. If the value contains a byte slice, the
-// checksum includes it directly.
+// ChecksumInterceptor will be called with every value before its checksum is
+// conditionally computed. This is used to prove that the decision to init
+// checksums on values or not does not affect beneath-raft consistency.
+// Interceptor is not safe to modify concurrently with calls to InitChecksum.
+var ChecksumInterceptor = func(_ *Value) {}
+
+// InitChecksum initializes a checksum based on the provided key and the
+// contents of the value if checksum computation is enabled. If the value
+// contains a byte slice, the checksum includes it directly.
+//
+// If you require that the checksum be set even if checksum computations are
+// disabled, use MustInitChecksum insted.
 //
 // TODO(peter): This method should return an error if the Value is corrupted
 // (e.g. the RawBytes field is > 0 but smaller than the header size).
+//
+// TODO(nvanbenschoten): This method and all users can be removed in any
+// binary version > 2.1.
 func (v *Value) InitChecksum(key []byte) {
+	// The branch is behind a constant conditional so that this
+	// entire method is optimized out in production builds.
+	if util.RaceEnabled {
+		ChecksumInterceptor(v)
+	}
+}
+
+// MustInitChecksum is like InitChecksum, but does not depend on the
+// shouldInitChecksum setting. This method can be used beneath raft where
+// different binaries cannot decide independently whether checksums should
+// be initialized or not.
+func (v *Value) MustInitChecksum(key []byte) {
 	if v.RawBytes == nil {
 		return
 	}

pkg/roachpb/data_test.go
@@ -229,7 +229,7 @@ func TestValueChecksumEmpty(t *testing.T) {
 func TestValueChecksumWithBytes(t *testing.T) {
 	k := []byte("key")
 	v := MakeValueFromString("abc")
-	v.InitChecksum(k)
+	v.MustInitChecksum(k)
 	if err := v.Verify(k); err != nil {
 		t.Error(err)
 	}
@@ -245,7 +245,7 @@ func TestValueChecksumWithBytes(t *testing.T) {
 	}
 	// Test ClearChecksum and reinitialization of checksum.
 	v.ClearChecksum()
-	v.InitChecksum(k)
+	v.MustInitChecksum(k)
 	if err := v.Verify(k); err != nil {
 		t.Error(err)
 	}

pkg/ccl/storageccl/add_sstable_test.go
@@ -174,7 +174,7 @@ func runTestDBAddSSTable(ctx context.Context, t *testing.T, db *client.DB) {
 	{
 		key := engine.MVCCKey{Key: []byte("bb"), Timestamp: hlc.Timestamp{WallTime: 1}}
 		value := roachpb.MakeValueFromString("1")
-		value.InitChecksum([]byte("foo"))
+		value.MustInitChecksum([]byte("foo"))
 		data, err := singleKVSSTable(key, value.RawBytes)
 		if err != nil {
 			t.Fatalf("%+v", err)

pkg/ccl/storageccl/engineccl/rocksdb_test.go
@@ -54,7 +54,7 @@ func TestVerifyBatchRepr(t *testing.T) {
 		var batch engine.RocksDBBatchBuilder
 		key := engine.MVCCKey{Key: []byte("bb"), Timestamp: hlc.Timestamp{WallTime: 1}}
 		value := roachpb.MakeValueFromString("1")
-		value.InitChecksum([]byte("foo"))
+		value.MustInitChecksum([]byte("foo"))
 		batch.Put(key, value.RawBytes)
 		data := batch.Finish()
 

pkg/ccl/storageccl/writebatch_test.go
@@ -97,7 +97,7 @@ func TestDBWriteBatch(t *testing.T) {
 		var batch engine.RocksDBBatchBuilder
 		key := engine.MVCCKey{Key: []byte("bb"), Timestamp: hlc.Timestamp{WallTime: 1}}
 		value := roachpb.MakeValueFromString("1")
-		value.InitChecksum([]byte("foo"))
+		value.MustInitChecksum([]byte("foo"))
 		batch.Put(key, value.RawBytes)
 		data := batch.Finish()
 

pkg/storage/stateloader/stateloader.go
@@ -350,7 +350,15 @@ func (rsl StateLoader) LoadMVCCStats(
 func (rsl StateLoader) SetMVCCStats(
 	ctx context.Context, eng engine.ReadWriter, newMS *enginepb.MVCCStats,
 ) error {
-	return engine.MVCCPutProto(ctx, eng, nil, rsl.RangeStatsKey(), hlc.Timestamp{}, nil, newMS)
+	key := rsl.RangeStatsKey()
+	value := roachpb.Value{}
+	if err := value.SetProto(newMS); err != nil {
+		return err
+	}
+	// We're beneath Raft and need to maintain strict consistency on this key
+	// because it is replicated, so we must always initialize the checksum.
+	value.MustInitChecksum(key)
+	return engine.MVCCPut(ctx, eng, nil /* ms */, key, hlc.Timestamp{}, value, nil /* txn */)
 }
 
 // The rest is not technically part of ReplicaState.

pkg/storage/track_raft_protos.go
@@ -32,6 +32,20 @@ func funcName(f interface{}) string {
 	return runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
 }
 
+func forCallers(fn func(runtime.Frame) bool) {
+	var pcs [256]uintptr
+	if numCallers := runtime.Callers(1, pcs[:]); numCallers == len(pcs) {
+		panic(fmt.Sprintf("number of callers %d might have exceeded slice size %d", numCallers, len(pcs)))
+	}
+	frames := runtime.CallersFrames(pcs[:])
+	for {
+		f, more := frames.Next()
+		if !fn(f) || !more {
+			return
+		}
+	}
+}
+
 // TrackRaftProtos instruments proto marshaling to track protos which are
 // marshaled downstream of raft. It returns a function that removes the
 // instrumentation and returns the list of downstream-of-raft protos.
@@ -40,13 +54,26 @@ func TrackRaftProtos() func() []reflect.Type {
 	processRaftFunc := funcName((*Replica).processRaftCommand)
 	// We only need to track protos that could cause replica divergence
 	// by being written to disk downstream of raft.
-	whitelist := []string{
+	marshalWhitelist := []string{
 		// Some raft operations trigger gossip, but we don't require
 		// strict consistency there.
 		funcName((*gossip.Gossip).AddInfoProto),
 		// Replica destroyed errors are written to disk, but they are
 		// deliberately per-replica values.
 		funcName((stateloader.StateLoader).SetReplicaDestroyedError),
+	}
+	// We only need to track checksum computations for values that could
+	// cause replica divergence by being written to disk downstream of raft.
+	checksumWhitelist := []string{
+		// Some raft operations trigger gossip, but we don't require
+		// strict consistency there.
+		funcName((*gossip.Gossip).AddInfo),
+		// Replica destroyed errors are written to disk, but they are
+		// deliberately per-replica values.
+		funcName((stateloader.StateLoader).SetReplicaDestroyedError),
+		// HardState is unreplicated, so we don't require strict
+		// consistency on its value.
+		funcName((stateloader.StateLoader).SetHardState),
 	}
 
 	belowRaftProtos := struct {
@@ -81,39 +108,41 @@ func TrackRaftProtos() func() []reflect.Type {
 			return
 		}
 
-		var pcs [256]uintptr
-		if numCallers := runtime.Callers(0, pcs[:]); numCallers == len(pcs) {
-			panic(fmt.Sprintf("number of callers %d might have exceeded slice size %d", numCallers, len(pcs)))
-		}
-		frames := runtime.CallersFrames(pcs[:])
-		for {
-			f, more := frames.Next()
-
-			whitelisted := false
-			for _, s := range whitelist {
+		forCallers(func(f runtime.Frame) bool {
+			for _, s := range marshalWhitelist {
 				if strings.Contains(f.Function, s) {
-					whitelisted = true
-					break
+					return false
 				}
-			}
-			if whitelisted {
-				break
 			}
 
 			if strings.Contains(f.Function, processRaftFunc) {
 				belowRaftProtos.Lock()
 				belowRaftProtos.inner[t] = struct{}{}
 				belowRaftProtos.Unlock()
-				break
+				return false
 			}
-			if !more {
-				break
+			return true
+		})
+	}
+
+	roachpb.ChecksumInterceptor = func(v *roachpb.Value) {
+		forCallers(func(f runtime.Frame) bool {
+			for _, s := range checksumWhitelist {
+				if strings.Contains(f.Function, s) {
+					return false
+				}
 			}
-		}
+
+			if strings.Contains(f.Function, processRaftFunc) {
+				panic(fmt.Sprintf("unexpected below raft checksum computation on value %v", v))
+			}
+			return true
+		})
 	}
 
 	return func() []reflect.Type {
 		protoutil.Interceptor = func(_ protoutil.Message) {}
+		roachpb.ChecksumInterceptor = func(_ *roachpb.Value) {}
 
 		belowRaftProtos.Lock()
 		types := make([]reflect.Type, 0, len(belowRaftProtos.inner))

commit 33957ae2098a7c799e9ed5d5b6f168f171d0a12b

roachpb: enforce checksum optionality, add Value.EqualData method
Fixes #23984.
Fixes #22636.

In #22636 we noticed that conditional puts were not respecting the fact
that Value checksums are meant to be optional. This is because they include
checksums when performing the equality check between expected and existing
values. This caused serious issues and forced us to roll back 75cbeb8.
This was unfortunate, but the damage was contained. It demonstrated that
we need to be very careful about changing checksums on values.

Later, we found in #23984 that a path somewhere in IMPORT was forgetting
to set checksums on Values. This was causing SQL operations at a much later
time to fail for the same reason that 75cbeb8 caused issues. This bug will
be fixed in a different change, but the fact that it exists shows how
fragile the current approach is because despite the intention, checksums
are absolutely not optional.

This change addresses both of these problems by making checksums optional,
as they were intended to be. CPut and InitPut no longer compare the checksums
of the expected and existing values. Instead, they call into a new `EqualData`
method, which ignores the checksum header when comparing byte values.

This is a bit higher risk than most of the changes we've been accepting at
this stage of the release cycle, but it would be really nice to get this
change in to 2.0 so that we can stay on track with the timeline proposed
in https://github.com/cockroachdb/cockroach/issues/22636#issuecomment-365323962
(although I'm not positive that the last step of the timeline is correct
because I don't think we can rely on the "baked in" protection with such
a low-level change). It will also save anyone who has used IMPORT already
and hit #22636.

This change should be safe from beneath Raft inconsistency for the same
reason that 75cbeb8 was safe. All CPuts beneath Raft are still careful
to set checksums, so we should not see any divergence in Raft state

pkg/config/system.go
@@ -53,7 +53,7 @@ func (s SystemConfig) Equal(other SystemConfig) bool {
 			return false
 		}
 		leftVal, rightVal := leftKV.Value, rightKV.Value
-		if !bytes.Equal(leftVal.RawBytes, rightVal.RawBytes) {
+		if !leftVal.EqualData(rightVal) {
 			return false
 		}
 		if leftVal.Timestamp != rightVal.Timestamp {

pkg/gossip/util.go
@@ -83,7 +83,7 @@ func (df *SystemConfigDeltaFilter) ForModified(
 				// Deleted key.
 				lastIdx++
 			case 0:
-				if !bytes.Equal(newKV.Value.RawBytes, oldKV.Value.RawBytes) {
+				if !newKV.Value.EqualData(oldKV.Value) {
 					// Modified value.
 					fn(newKV)
 				}

pkg/roachpb/data.go
@@ -326,6 +326,18 @@ func (v Value) dataBytes() []byte {
 	return v.RawBytes[headerSize:]
 }
 
+// EqualData returns a boolean reporting whether the receiver and the parameter
+// have equivalent byte values. This check ignores the optional checksum field
+// in the Values' byte slices, returning only whether the Values have the same
+// tag and encoded data.
+//
+// This method should be used whenever the raw bytes of two Values are being
+// compared instead of comparing the RawBytes slices directly because it ignores
+// the checksum header, which is optional.
+func (v Value) EqualData(o Value) bool {
+	return bytes.Equal(v.RawBytes[checksumSize:], o.RawBytes[checksumSize:])
+}
+
 // SetBytes sets the bytes and tag field of the receiver and clears the checksum.
 func (v *Value) SetBytes(b []byte) {
 	v.RawBytes = make([]byte, headerSize+len(b))

pkg/sql/sqlbase/rowfetcher.go
@@ -1090,10 +1090,10 @@ func (rf *RowFetcher) checkSecondaryIndexDatumEncodings(ctx context.Context) err
 			return scrub.WrapError(scrub.IndexKeyDecodingError, errors.Errorf(
 				"secondary index key failed to round-trip encode. expected %#v, got: %#v",
 				rf.rowReadyTable.lastKV.Key, indexEntry.Key))
-		} else if !bytes.Equal(indexEntry.Value.RawBytes[4:], table.lastKV.Value.RawBytes[4:]) {
+		} else if !indexEntry.Value.EqualData(table.lastKV.Value) {
 			return scrub.WrapError(scrub.IndexValueDecodingError, errors.Errorf(
 				"secondary index value failed to round-trip encode. expected %#v, got: %#v",
-				rf.rowReadyTable.lastKV.Value.RawBytes[4:], indexEntry.Value.RawBytes[4:]))
+				rf.rowReadyTable.lastKV.Value, indexEntry.Value))
 		}
 	}
 	return nil

pkg/sql/sqlbase/rowwriter.go
@@ -866,7 +866,7 @@ func (ru *RowUpdater) UpdateRow(
 				log.VEventf(ctx, 2, "Del %s", keys.PrettyPrint(ru.Helper.secIndexValDirs[i], oldSecondaryIndexEntry.Key))
 			}
 			batch.Del(oldSecondaryIndexEntry.Key)
-		} else if !bytes.Equal(newSecondaryIndexEntry.Value.RawBytes, oldSecondaryIndexEntry.Value.RawBytes) {
+		} else if !newSecondaryIndexEntry.Value.EqualData(oldSecondaryIndexEntry.Value) {
 			expValue = &oldSecondaryIndexEntry.Value
 		} else {
 			continue

pkg/storage/engine/mvcc.go
@@ -1460,7 +1460,7 @@ func mvccConditionalPutUsingIter(
 		func(existVal *roachpb.Value) ([]byte, error) {
 			if expValPresent, existValPresent := expVal != nil, existVal.IsPresent(); expValPresent && existValPresent {
 				// Every type flows through here, so we can't use the typed getters.
-				if !bytes.Equal(expVal.RawBytes, existVal.RawBytes) {
+				if !expVal.EqualData(*existVal) {
 					return nil, &roachpb.ConditionFailedError{
 						ActualValue: existVal.ShallowClone(),
 					}
@@ -1531,7 +1531,7 @@ func mvccInitPutUsingIter(
 				return nil, &roachpb.ConditionFailedError{ActualValue: existVal.ShallowClone()}
 			}
 			if existVal.IsPresent() {
-				if !bytes.Equal(value.RawBytes, existVal.RawBytes) {
+				if !value.EqualData(*existVal) {
 					return nil, &roachpb.ConditionFailedError{
 						ActualValue: existVal.ShallowClone(),
 					}

pkg/roachpb/data_test.go
@@ -210,6 +210,71 @@ func TestIsPrev(t *testing.T) {
 	}
 }
 
+func TestValueDataEquals(t *testing.T) {
+	strVal := func(s string) *Value {
+		var v Value
+		v.SetString(s)
+		return &v
+	}
+
+	a := strVal("val1")
+
+	b := strVal("val1")
+	b.InitChecksum([]byte("key1"))
+
+	c := strVal("val1")
+	c.InitChecksum([]byte("key2"))
+
+	// Different values.
+	d := strVal("val2")
+
+	e := strVal("val2")
+	e.InitChecksum([]byte("key1"))
+
+	// Different tags.
+	f := strVal("val1")
+	f.setTag(ValueType_INT)
+
+	g := strVal("val1")
+	g.setTag(ValueType_INT)
+	g.InitChecksum([]byte("key1"))
+
+	for i, tc := range []struct {
+		v1, v2 *Value
+		eq     bool
+	}{
+		{v1: a, v2: b, eq: true},
+		{v1: a, v2: c, eq: true},
+		{v1: a, v2: d, eq: false},
+		{v1: a, v2: e, eq: false},
+		{v1: a, v2: f, eq: false},
+		{v1: a, v2: g, eq: false},
+		{v1: b, v2: c, eq: true},
+		{v1: b, v2: d, eq: false},
+		{v1: b, v2: e, eq: false},
+		{v1: b, v2: f, eq: false},
+		{v1: b, v2: g, eq: false},
+		{v1: c, v2: d, eq: false},
+		{v1: c, v2: e, eq: false},
+		{v1: c, v2: f, eq: false},
+		{v1: c, v2: g, eq: false},
+		{v1: d, v2: e, eq: true},
+		{v1: d, v2: f, eq: false},
+		{v1: d, v2: g, eq: false},
+		{v1: e, v2: f, eq: false},
+		{v1: e, v2: g, eq: false},
+		{v1: f, v2: g, eq: true},
+	} {
+		if tc.eq != tc.v1.EqualData(*tc.v2) {
+			t.Errorf("%d: wanted eq=%t", i, tc.eq)
+		}
+		// Test symmetry.
+		if tc.eq != tc.v2.EqualData(*tc.v1) {
+			t.Errorf("%d: wanted eq=%t", i, tc.eq)
+		}
+	}
+}
+
 func TestValueChecksumEmpty(t *testing.T) {
 	k := []byte("key")
 	v := Value{}