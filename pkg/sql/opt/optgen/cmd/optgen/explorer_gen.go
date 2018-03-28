29618bb1d409667f1d2a7d7471e9defd72347a03
Andrew Kimball <andyk@cockroachlabs.com>
2018-03-27 17:30:51 -0700

    opt: Use Optgen to generate the GenerateIndexScans rule
    Previous PR's implemented the GenerateIndexScans rule manually, and also
    made Optgen capable of generating explorer rules. This PR puts the two
    together, and uses Optgen to generate code for the GenerateIndexScans
    rule. This uncovered some bugs in Optgen, which this PR fixes:
    
    - A custom replace function needs to return a slice of result expressions
      rather than trying to memoize them itself.
    - The explore methods need to take an ExprID rather than a GroupID,
      because the methods needs to know the expression's ordinal within its
      group.
    
    Release note: None

--- pkg/sql/opt/optgen/cmd/optgen/explorer_gen.go
+++ pkg/sql/opt/optgen/cmd/optgen/explorer_gen.go
@@ -54,8 +54,8 @@ func (g *explorerGen) generate(compiled *lang.CompiledExpr, w io.Writer) {
 //   ) (_fullyExplored bool) {
 //     _expr := _e.mem.Expr(_eid)
 //     switch _expr.Operator() {
-//       case opt.ScanOp: return _e.exploreScan(_state, _eid, _expr.AsScan())
-//       case opt.SelectOp: return _e.exploreSelect(_state, _eid, _expr.AsSelect())
+//       case opt.ScanOp: return _e.exploreScan(_state, _eid)
+//       case opt.SelectOp: return _e.exploreSelect(_state, _eid)
 //     }
 //
 //     // No rules for other operator types.
@@ -63,10 +63,7 @@ func (g *explorerGen) generate(compiled *lang.CompiledExpr, w io.Writer) {
 //   }
 //
 func (g *explorerGen) genDispatcher() {
-	g.w.nestIndent("func (_e *explorer) exploreExpr(\n")
-	g.w.writeIndent("_state *exploreState, _eid memo.ExprID,\n")
-	g.w.unnest(") (_fullyExplored bool)")
-	g.w.nestIndent(" {\n")
+	g.w.nestIndent("func (_e *explorer) exploreExpr(_state *exploreState, _eid memo.ExprID) (_fullyExplored bool) {\n")
 	g.w.writeIndent("_expr := _e.mem.Expr(_eid)\n")
 	g.w.writeIndent("switch _expr.Operator() {\n")
 
@@ -74,8 +71,8 @@ func (g *explorerGen) genDispatcher() {
 		// Only include exploration rules.
 		rules := g.compiled.LookupMatchingRules(string(define.Name)).WithTag("Explore")
 		if len(rules) > 0 {
-			format := "case opt.%sOp: return _e.explore%s(_state, _eid, _expr.As%s())\n"
-			g.w.writeIndent(format, define.Name, define.Name, define.Name)
+			format := "case opt.%sOp: return _e.explore%s(_state, _eid)\n"
+			g.w.writeIndent(format, define.Name, define.Name)
 		}
 	}
 
@@ -88,9 +85,8 @@ func (g *explorerGen) genDispatcher() {
 // genRuleFuncs generates a method for each operator that has at least one
 // explore rule defined. The code is similar to this:
 //
-//   func (_e *explorer) exploreScan(
-//     _state *exploreState, _scan memo.GroupID, _scanExpr memo.ScanExpr,
-//   ) (_fullyExplored bool) {
+//   func (_e *explorer) exploreScan(_state *exploreState, _eid memo.ExprID) (_fullyExplored bool) {
+//     _scanExpr := _e.mem.Expr(_eid).AsScan()
 //     _fullyExplored = true
 //
 //     ... exploration rule code goes here ...
@@ -105,14 +101,10 @@ func (g *explorerGen) genRuleFuncs() {
 			continue
 		}
 
-		groupVar := fmt.Sprintf("_%s", unTitle(string(define.Name)))
-		exprVar := fmt.Sprintf("_%sExpr", unTitle(string(define.Name)))
-
-		g.w.nestIndent("func (_e *explorer) explore%s(\n", define.Name)
-		format := "_state *exploreState, %s memo.GroupID, %s memo.%sExpr,\n"
-		g.w.writeIndent(format, groupVar, exprVar, define.Name)
-		g.w.unnest(") (_fullyExplored bool) ")
-		g.w.nest("{\n")
+		exprName := fmt.Sprintf("_%sExpr", unTitle(string(define.Name)))
+		format := "func (_e *explorer) explore%s(_state *exploreState, _eid memo.ExprID) (_fullyExplored bool) {\n"
+		g.w.nestIndent(format, define.Name)
+		g.w.writeIndent("%s := _e.mem.Expr(_eid).As%s()\n", exprName, define.Name)
 		g.w.writeIndent("_fullyExplored = true\n\n")
 
 		for _, rule := range rules {
