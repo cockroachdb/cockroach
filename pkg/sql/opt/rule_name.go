701f3701faa467e2c7844c763d916ab78947c4cf
Andrew Kimball <andyk@cockroachlabs.com>
2018-03-27 20:23:04 -0700

    opt: Move files to new norm package
    This is prep for the next commit which changes the files, making that
    commit easier to review.
    
    Release note: None

--- pkg/sql/opt/rule_name.go
+++ pkg/sql/opt/rule_name.go
@@ -12,7 +12,7 @@
 // implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
-package xform
+package opt
 
 //go:generate stringer -output=rule_name_string.go -type=RuleName rule_name.go rule_name.og.go
 
