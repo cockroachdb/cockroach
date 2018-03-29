1b9cd99ed97fd2b1339a716940528ac483136cd3
Nikhil Benesch <nikhil.benesch@gmail.com>
2018-03-29 18:09:40 -0400

    build: ensure all packages are compiled before linting
    We still have some linters that depend on compiled object files being
    available in GOPATH/pkg. Run 'go test -i pkg/...' before running our
    linters to ensure all possible object files are up-to-date.
    
    We actually used to do this, but I prematurely removed it in 15ab56b4f
    when I thought Go 1.9 had fixed these woes.
    
    Release note: None

---
+++ pkg/ccl/logictestccl/doc.go
@@ -0,0 +1,10 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+// Package logictestccl houses the SQL logic tests that test CCL features.
+package logictestccl
