e63a7e59282d1c2be5451bef611d20833ed496d3
Victor Chen <victor@cockroachlabs.com>
2018-04-05 13:20:19 -0400

    cluster: Finalize cluster version upgrade automatically.
    Added a feature to auto upgrade the cluster setting version after a rolling
    upgrade. Now operators no longer have to manually type the upgrade command.
    
    Fixes #23912.
    
    Release note: None

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -289,6 +289,7 @@ func NewServer(cfg Config, stopper *stop.Stopper) (*Server, error) {
 		s.gossip,
 		nlActive,
 		nlRenewal,
+		s.st,
 		s.cfg.HistogramWindowInterval(),
 	)
 	s.registry.AddMetricStruct(s.nodeLiveness.Metrics())
@@ -1541,6 +1542,9 @@ If problems persist, please see ` + base.DocsURL("cluster-setup-troubleshooting.
 
 	log.Info(ctx, "serving sql connections")
 	// Start servicing SQL connections.
+
+	// Attempt to upgrade cluster version.
+	s.startAttemptUpgrade(ctx)
 
 	pgCtx := s.pgServer.AmbientCtx.AnnotateCtx(context.Background())
 	s.stopper.RunWorker(pgCtx, func(pgCtx context.Context) {
