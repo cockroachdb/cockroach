00365e527158e15cf1d248236daabdf12efa317f
Justin Jaffray <justin@cockroachlabs.com>
2018-04-05 18:29:35 -0400

    sql: add tests regarding all datum types
    This commit adds a couple guardrails around adding a new Datum type
    (suggestions for more welcome) by using types.AnyNonArray to check that
    a handful of important switch statements are exhaustive.
    
    Release note: None

--- pkg/sql/sem/tree/testutils.go
+++ pkg/sql/sem/tree/testutils.go
@@ -14,7 +14,15 @@
 
 package tree
 
-import "github.com/cockroachdb/cockroach/pkg/sql/sem/types"
+import (
+	"fmt"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/types"
+	"github.com/cockroachdb/cockroach/pkg/util/duration"
+	"github.com/cockroachdb/cockroach/pkg/util/timeofday"
+	"github.com/cockroachdb/cockroach/pkg/util/uuid"
+)
 
 // presetTypesForTesting is a mapping of qualified names to types that can be mocked out
 // for tests to allow the qualified names to be type checked without throwing an error.
@@ -25,5 +33,51 @@ func MockNameTypes(types map[string]types.T) func() {
 	presetTypesForTesting = types
 	return func() {
 		presetTypesForTesting = nil
+	}
+}
+
+// SampleDatum is intended to be a more lightweight version of RandDatum for
+// when you just need one consistent example of a datum.
+func SampleDatum(t types.T) Datum {
+	switch t {
+	case types.Bool:
+		return MakeDBool(true)
+	case types.Int:
+		return NewDInt(123)
+	case types.Float:
+		f := DFloat(123.456)
+		return &f
+	case types.Decimal:
+		d := &DDecimal{}
+		d.Decimal.SetExponent(6)
+		// int64(rng.Uint64()) to get negative numbers, too
+		d.Decimal.SetCoefficient(3)
+		return d
+	case types.String:
+		return NewDString("Carl")
+	case types.Bytes:
+		return NewDBytes("Princess")
+	case types.Date:
+		return NewDDate(123123)
+	case types.Time:
+		return MakeDTime(timeofday.FromInt(789))
+	case types.Timestamp:
+		return MakeDTimestamp(time.Unix(123, 123), time.Second)
+	case types.TimestampTZ:
+		return MakeDTimestampTZ(time.Unix(123, 123), time.Second)
+	case types.Interval:
+		return &DInterval{duration.Duration{}}
+	case types.UUID:
+		return NewDUuid(DUuid{uuid.Nil})
+	case types.INet:
+		i, _ := ParseDIPAddrFromINetString("127.0.0.1")
+		return i
+	case types.JSON:
+		j, _ := ParseDJSON(`{"a": "b"}`)
+		return j
+	case types.Oid:
+		return NewDOid(DInt(0))
+	default:
+		panic(fmt.Sprintf("SampleDatum not implemented for ", t))
 	}
 }
