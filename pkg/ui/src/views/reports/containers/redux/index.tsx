04cceb13f212028e78b7e4d47331df758bdbd3ce
Andrew Couch <andrew@cockroachlabs.com>
2018-04-10 12:55:51 -0400

    ui: events cleanup
    Release note: None

--- pkg/ui/src/views/reports/containers/redux/index.tsx
+++ pkg/ui/src/views/reports/containers/redux/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Helmet } from "react-helmet":
+import { Helmet } from "react-helmet";
 import { connect } from "react-redux";
 import classNames from "classnames";
 import CopyToClipboard from "react-copy-to-clipboard";
