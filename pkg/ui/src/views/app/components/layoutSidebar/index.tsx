ab45f2ed1407aa4f7e47c089b56e02e527478e53
Andrew Couch <andrew@cockroachlabs.com>
2018-04-11 15:48:49 -0400

    ui: highlight sidebar links for nested pages
    The route reorganization broke one of react-router's assumptions: that
    logically nested pages are always on strictly-nested routes.  This
    fixes the sidebar link highlighting for nested pages whose route
    doesn't start with the exact route the link is going to.
    
    Fixes: #24069
    Release note: None

--- pkg/ui/src/views/app/components/layoutSidebar/index.tsx
+++ pkg/ui/src/views/app/components/layoutSidebar/index.tsx
@@ -1,4 +1,7 @@
+import classNames from "classnames";
+import _ from "lodash";
 import React from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router";
 import { cockroachIcon } from "src/views/shared/components/icons";
 import { trustIcon } from "src/util/trust";
@@ -12,6 +15,7 @@ interface IconLinkProps {
   icon?: string;
   title?: string;
   to: string;
+  activeFor?: string | string[];
   onlyActiveOnIndex?: boolean;
   className?: string;
 }
@@ -26,11 +30,29 @@ class IconLink extends React.Component<IconLinkProps, {}> {
     onlyActiveOnIndex: false,
   };
 
+  static contextTypes = {
+    router: PropTypes.object,
+  };
+
   render() {
-    const { icon, title, to, onlyActiveOnIndex, className } = this.props;
+    const { icon, title, to, activeFor, onlyActiveOnIndex, className } = this.props;
+
+    let isActive = false;
+    if (!_.isNil(activeFor)) {
+      const router = this.context.router;
+      const options = typeof activeFor === "string" ? [activeFor] : activeFor;
+      isActive = _.some(options, (opt) => router.isActive(opt, onlyActiveOnIndex));
+    }
+    const classOverrides = classNames({ active: isActive });
+
     return (
       <li className={className} >
-        <Link to={to} activeClassName="active" onlyActiveOnIndex={onlyActiveOnIndex}>
+        <Link
+          to={to}
+          activeClassName="active"
+          onlyActiveOnIndex={onlyActiveOnIndex}
+          className={classOverrides}
+        >
           <div className="image-container"
                dangerouslySetInnerHTML={trustIcon(icon)}/>
           <div>{title}</div>
@@ -49,10 +71,10 @@ export default class extends React.Component<{}, {}> {
   render() {
     return <nav className="navigation-bar">
       <ul className="navigation-bar__list">
-        <IconLink to="/overview" icon={homeIcon} title="Overview" />
+        <IconLink to="/overview" icon={homeIcon} title="Overview" activeFor="/node" />
         <IconLink to="/metrics" icon={metricsIcon} title="Metrics" />
-        <IconLink to="/databases" icon={databasesIcon} title="Databases"/>
-        <IconLink to="/jobs" icon={jobsIcon} title="Jobs"/>
+        <IconLink to="/databases" icon={databasesIcon} title="Databases" activeFor="/database" />
+        <IconLink to="/jobs" icon={jobsIcon} title="Jobs" />
       </ul>
       <ul className="navigation-bar__list navigation-bar__list--bottom">
         <IconLink to="/" icon={cockroachIcon} className="cockroach" />
