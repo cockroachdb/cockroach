bc64f6005e65655492a2256b8b0df5a65fe51c33
Matt Tracy <matt@cockroachlabs.com>
2018-04-18 16:31:55 -0700

    ts: Move memory-related Query params to a struct
    Memory-related parameters for the DB.Query() method are now grouped into
    a struct QueryMemoryContext. Methods related specifically to memory
    management should be associated with this structure as much as possible
    going forward.
    
    Release note: None

--- pkg/ts/db_test.go
+++ pkg/ts/db_test.go
@@ -284,6 +284,18 @@ func (tm *testModel) prune(nowNanos int64, timeSeries ...timeSeriesResolutionInf
 	}
 }
 
+func (tm *testModel) makeMemoryContext(interpolationLimitNanos int64) QueryMemoryContext {
+	return MakeQueryMemoryContext(
+		tm.workerMemMonitor,
+		tm.resultMemMonitor,
+		QueryMemoryOptions{
+			BudgetBytes:             tm.queryMemoryBudget,
+			EstimatedSources:        tm.model.UniqueSourceCount(),
+			InterpolationLimitNanos: interpolationLimitNanos,
+		},
+	)
+}
+
 // modelDataSource is used to create a mock DataSource. It returns a
 // deterministic set of data to GetTimeSeriesData, storing the returned data in
 // the model whenever GetTimeSeriesData is called. Data is returned until all
