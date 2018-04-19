cockroachdb/cockroach

commit 1c07645b847a220ff8eeb01e8173d3be821ca69c

workload: improve `workload init tpch` error
The TPCH workload doesn't yet have initial data functions implemented
because we don't yet have a golang port of the logic for generating it.
Trying to call Setup (which is what powers `workload init`) on it
previously segfaulted, but now returns an error.

Closes #24913

Release note: None

pkg/workload/workload.go
@@ -156,13 +156,16 @@ type BatchedTuples struct {
 
 // Tuples returns a BatchedTuples where each batch has size 1.
 func Tuples(count int, fn func(int) []interface{}) BatchedTuples {
-	return BatchedTuples{
+	t := BatchedTuples{
 		NumBatches: count,
 		NumTotal:   count,
-		Batch: func(batchIdx int) [][]interface{} {
-			return [][]interface{}{fn(batchIdx)}
-		},
 	}
+	if fn != nil {
+		t.Batch = func(batchIdx int) [][]interface{} {
+			return [][]interface{}{fn(batchIdx)}
+		}
+	}
+	return t
 }
 
 // QueryLoad represents some SQL query workload performable on a database
@@ -283,8 +286,8 @@ func Setup(
 	var size int64
 	for _, table := range tables {
 		if table.InitialRows.Batch == nil {
-			// Some workloads don't support initial table data.
-			continue
+			return 0, errors.Errorf(
+				`initial data is not supported for workload %s`, gen.Meta().Name)
 		}
 		batchesPerWorker := table.InitialRows.NumBatches / concurrency
 		g, gCtx := errgroup.WithContext(ctx)

commit 104c324ec85274cdbecdb0c91356efd19da5dcfc

workload: fix `./workload init kv`
We only want to return the "unsupported" error if a table has non-zero
rows and it doesn't have a function for computing them (currently only
tpch). This is a regression in #24914.

Release note: None

pkg/workload/workload.go
@@ -285,7 +285,9 @@ func Setup(
 
 	var size int64
 	for _, table := range tables {
-		if table.InitialRows.Batch == nil {
+		if table.InitialRows.NumBatches == 0 {
+			continue
+		} else if table.InitialRows.Batch == nil {
 			return 0, errors.Errorf(
 				`initial data is not supported for workload %s`, gen.Meta().Name)
 		}