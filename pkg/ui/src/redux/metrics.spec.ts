b36c366489fcc2fedaf30e5badba62d866a2084b
Matt Tracy <matt@cockroachlabs.com>
2018-04-30 15:46:19 -0700

    ui: Test metrics saga with redux-test-saga-plan
    Rewrite tests of the existing metrics query system, which uses
    redux-saga, to use the third-party testing tool redux-test-saga-plan.
    This is a powerful tool which makes tightly-coupled saga tests much more
    succinct, but also allows for loosely-coupled integration tests with
    powerful mocking and expectation options.
    
    + Rewrites most existing tests for the metrics saga to use "testSaga()"
    primative, which is still tightly coupled to saga output, but is much
    more succinct than the previous code.
    + Rewrites one test to use "expectSaga()" primitive, which actually runs
    a saga in the background and allows result expectations to be divided
    more clearly from the underlying saga.
    + Adds three new integration-level tests using "expectSaga()".
    
    Also adds a useful "ui-test-debug" make target, which is similar to the
    existing "ui-test-watch" target but opens up a Chrome browser for
    interactive debugging with Karma. I believe this is now acceptable,
    since the UI tests depend on a chrome installation.
    
    Release note: none

--- pkg/ui/src/redux/metrics.spec.ts
+++ pkg/ui/src/redux/metrics.spec.ts
@@ -1,10 +1,12 @@
 import { assert } from "chai";
 import _ from "lodash";
 import Long from "long";
+import { expectSaga, testSaga } from "redux-saga-test-plan";
+import * as matchers from "redux-saga-test-plan/matchers";
 
 import { delay } from "redux-saga";
-import { AllEffect, call, ForkEffect, put, take } from "redux-saga/effects";
-import { queryTimeSeries } from "src/util/api";
+import { call, put } from "redux-saga/effects";
+import { queryTimeSeries, TimeSeriesQueryRequestMessage } from "src/util/api";
 import * as protos from "src/js/protos";
 
 import * as metrics from "./metrics";
@@ -166,61 +168,85 @@ describe("metrics reducer", function() {
       });
     }
 
-    function createResponse(queries: protos.cockroach.ts.tspb.IQuery[]) {
+    function createResponse(
+      queries: protos.cockroach.ts.tspb.IQuery[],
+      datapoints: protos.cockroach.ts.tspb.TimeSeriesDatapoint[]  = [],
+    ) {
       return new protos.cockroach.ts.tspb.TimeSeriesQueryResponse({
-        results: queries.map(q => {
+        results: queries.map(query => {
           return {
-            query: q,
-            datapoints: [],
+            query,
+            datapoints,
           };
         }),
       });
     }
 
-    describe("queryMetricsSaga", function() {
+    function createDatapoints(val: number) {
+      const result: protos.cockroach.ts.tspb.TimeSeriesDatapoint[] = [];
+      for (let i = 0; i < val; i++) {
+        result.push(new protos.cockroach.ts.tspb.TimeSeriesDatapoint({
+          timestamp_nanos: new Long(val),
+          value: val,
+        }));
+      }
+      return result;
+    }
+
+    describe("queryMetricsSaga plan", function() {
       it("initially waits for incoming request objects", function () {
-        const saga = metrics.queryMetricsSaga();
-        assert.deepEqual(saga.next().value, take(metrics.REQUEST));
+        testSaga(metrics.queryMetricsSaga)
+          .next()
+          .take(metrics.REQUEST);
       });
 
       it("correctly accumulates batches", function () {
         const requestAction = metrics.requestMetrics("id", createRequest(shortTimespan, "short.1"));
-        const saga = metrics.queryMetricsSaga();
-        saga.next();
+        const beginAction = metrics.beginMetrics(requestAction.payload.id, requestAction.payload.data);
 
-        // Pass in a request, the generator should put the request to the
-        // redux store, fork "sendBatches" process, then await another request.
-        assert.deepEqual(saga.next(requestAction).value, put(requestAction));
-        const sendBatchesFork = saga.next().value as ForkEffect;
-        assert.isDefined(sendBatchesFork.FORK);
-        assert.deepEqual(saga.next().value, take(metrics.REQUEST));
-
-        // Pass in two additional requests, the generator should not yield another
-        // fork.
-        for (let i = 0; i < 2; i++) {
-          assert.deepEqual(saga.next(requestAction).value, put(requestAction));
-          assert.deepEqual(saga.next().value, take(metrics.REQUEST));
-        }
-
-        // Run the fork function. It should initially call delay (to defer to the
-        // event queue), then call sendRequestBatches with the currently
-        // accumulated batches, then complete.
-        const accumulateBatch = sendBatchesFork.FORK.fn() as IterableIterator<any>;
-        assert.deepEqual(accumulateBatch.next().value, call(delay, 0));
-        assert.deepEqual(
-          accumulateBatch.next().value,
-          call(
-            metrics.batchAndSendRequests,
-            [requestAction.payload, requestAction.payload, requestAction.payload],
-          ),
-        );
-        assert.isTrue(accumulateBatch.next().done);
-
-        // Pass in a request, the generator should again yield a "fork" followed
-        // by another take for request objects.
-        assert.deepEqual(saga.next(requestAction).value, put(requestAction));
-        assert.isDefined((saga.next().value as ForkEffect).FORK);
-        assert.deepEqual(saga.next().value, take(metrics.REQUEST));
+        return expectSaga(metrics.queryMetricsSaga)
+          // Stub out calls to batchAndSendRequests.
+          .provide([
+            [matchers.call.fn(metrics.batchAndSendRequests), null],
+          ])
+          // Dispatch six requests, with delays inserted in order to trigger
+          // batch sends.
+          .dispatch(requestAction)
+          .dispatch(requestAction)
+          .dispatch(requestAction)
+          .delay(0)
+          .dispatch(requestAction)
+          .delay(0)
+          .dispatch(requestAction)
+          .dispatch(requestAction)
+          .run()
+          .then((result) => {
+            const { effects } = result;
+            // Verify the order of call dispatches.
+            assert.deepEqual(
+              effects.call,
+              [
+                call(delay, 0),
+                call(metrics.batchAndSendRequests, [requestAction.payload, requestAction.payload, requestAction.payload]),
+                call(delay, 0),
+                call(metrics.batchAndSendRequests, [requestAction.payload]),
+                call(delay, 0),
+                call(metrics.batchAndSendRequests, [requestAction.payload, requestAction.payload]),
+              ],
+            );
+            // Verify that all beginAction puts were dispatched.
+            assert.deepEqual(
+              effects.put,
+              [
+                put(beginAction),
+                put(beginAction),
+                put(beginAction),
+                put(beginAction),
+                put(beginAction),
+                put(beginAction),
+              ],
+            );
+          });
       });
     });
 
@@ -239,25 +265,25 @@ describe("metrics reducer", function() {
 
         // Mix the requests together and send the combined request set.
         const mixedRequests = _.flatMap(shortRequests, (short, i) => [short, longRequests[i]]);
-        const sendBatches = metrics.batchAndSendRequests(mixedRequests);
 
-        // sendBatches next puts a "fetchMetrics" action into the store.
-        assert.deepEqual(sendBatches.next().value, put(metrics.fetchMetrics()));
-
-        // Next, sendBatches dispatches a "all" effect with a "call" for each
-        // batch; there should be two batches in total, one containing the
-        // short requests and one containing the long requests. The order
-        // of requests in each batch is maintained.
-        const allEffect = sendBatches.next().value as AllEffect;
-        assert.isArray(allEffect.ALL);
-        assert.deepEqual(allEffect.ALL, [
-          call(metrics.sendRequestBatch, shortRequests),
-          call(metrics.sendRequestBatch, longRequests),
-        ]);
-
-        // After completion, puts "fetchMetricsComplete" to store.
-        assert.deepEqual(sendBatches.next().value, put(metrics.fetchMetricsComplete()));
-        assert.isTrue(sendBatches.next().done);
+        testSaga(metrics.batchAndSendRequests, mixedRequests)
+          // sendBatches next puts a "fetchMetrics" action into the store.
+          .next()
+          .put(metrics.fetchMetrics())
+          .next()
+          // Next, sendBatches dispatches a "all" effect with a "call" for each
+          // batch; there should be two batches in total, one containing the
+          // short requests and one containing the long requests. The order of
+          // requests in each batch is maintained.
+          .all([
+            call(metrics.sendRequestBatch, shortRequests),
+            call(metrics.sendRequestBatch, longRequests),
+          ])
+          // After completion, puts "fetchMetricsComplete" to store.
+          .next()
+          .put(metrics.fetchMetricsComplete())
+          .next()
+          .isDone();
       });
     });
 
@@ -269,47 +295,210 @@ describe("metrics reducer", function() {
       ];
 
       it("correctly sends batch as single request, correctly handles valid response", function() {
-        const sendBatch = metrics.sendRequestBatch(requests);
+        // The expected request that will be generated by sendRequestBatch.
         const expectedRequest = createRequest(shortTimespan, "short.1", "short.2", "short.3", "short.4");
-        assert.deepEqual(sendBatch.next().value, call(queryTimeSeries, expectedRequest));
-
         // Return a valid response.
         const response = createResponse(expectedRequest.queries);
+        // Generate the expected put effects to be generated after receiving the response.
+        const expectedEffects = _.map(requests, req => metrics.receiveMetrics(
+          req.id, req.data, createResponse(req.data.queries),
+        ));
 
-        // Expect three puts to the underlying store.
-        const actualEffects = [
-          sendBatch.next(response).value,
-          sendBatch.next().value,
-          sendBatch.next().value,
-        ];
-        const expectedEffects = requests.map(req => put(metrics.receiveMetrics(
-          req.id,
-          req.data,
-          createResponse(req.data.queries),
-        )));
-        assert.deepEqual(actualEffects, expectedEffects);
-        assert.isTrue(sendBatch.next().done);
+        testSaga(metrics.sendRequestBatch, requests)
+          .next()
+          .call(queryTimeSeries, expectedRequest)
+          .next(response)
+          .put(expectedEffects[0])
+          .next()
+          .put(expectedEffects[1])
+          .next()
+          .put(expectedEffects[2])
+          .next()
+          .isDone();
       });
 
       it("correctly handles error response", function() {
-        const sendBatch = metrics.sendRequestBatch(requests);
+        // The expected request that will be generated by sendRequestBatch.
         const expectedRequest = createRequest(shortTimespan, "short.1", "short.2", "short.3", "short.4");
-        assert.deepEqual(sendBatch.next().value, call(queryTimeSeries, expectedRequest));
-
-        // Throw an exception, a network error.  Expect three puts, which are
-        // are error responses.
+        // Return an error response.
         const err = new Error("network error");
-        const actualEffects = [
-          sendBatch.throw(err).value,
-          sendBatch.next().value,
-          sendBatch.next().value,
-        ];
-        const expectedEffects = requests.map(req => put(metrics.errorMetrics(
-          req.id,
-          err,
-        )));
-        assert.deepEqual(actualEffects, expectedEffects);
-        assert.isTrue(sendBatch.next().done);
+        // Generate the expected put effects to be generated after receiving the response.
+        const expectedEffects = _.map(requests, req => metrics.errorMetrics(
+          req.id, err,
+        ));
+
+        testSaga(metrics.sendRequestBatch, requests)
+          .next()
+          .call(queryTimeSeries, expectedRequest)
+          .throw(err)
+          .put(expectedEffects[0])
+          .next()
+          .put(expectedEffects[1])
+          .next()
+          .put(expectedEffects[2])
+          .next()
+          .isDone();
+      });
+    });
+
+    describe("integration test", function() {
+      const shortRequests = [
+        metrics.requestMetrics("id.0", createRequest(shortTimespan, "short.1")),
+        metrics.requestMetrics("id.2", createRequest(shortTimespan, "short.2", "short.3")),
+        metrics.requestMetrics("id.4", createRequest(shortTimespan, "short.4")),
+      ];
+      const longRequests = [
+        metrics.requestMetrics("id.1", createRequest(longTimespan, "long.1")),
+        metrics.requestMetrics("id.3", createRequest(longTimespan, "long.2", "long.3")),
+        metrics.requestMetrics("id.5", createRequest(longTimespan, "long.4", "long.5")),
+      ];
+
+      const createMetricsState = (
+        id: string, ts: timespan, metricNames: string[], datapointCount: number,
+      ): metrics.MetricsQuery => {
+        const request = createRequest(ts, ...metricNames);
+        const state = new metrics.MetricsQuery(id);
+        state.request = request;
+        state.nextRequest = request;
+        state.data = createResponse(request.queries, createDatapoints(datapointCount));
+        state.error = undefined;
+        return state;
+      };
+
+      const createMetricsErrorState = (
+        id: string, ts: timespan, metricNames: string[], err: Error,
+      ): metrics.MetricsQuery => {
+        const request = createRequest(ts, ...metricNames);
+        const state = new metrics.MetricsQuery(id);
+        state.nextRequest = request;
+        state.error = err;
+        return state;
+      };
+
+      const createMetricsInFlightState = (
+        id: string, ts: timespan, metricNames: string[],
+      ): metrics.MetricsQuery => {
+        const request = createRequest(ts, ...metricNames);
+        const state = new metrics.MetricsQuery(id);
+        state.nextRequest = request;
+        return state;
+      };
+
+      it("handles success correctly", function() {
+        const expectedState = new metrics.MetricsState();
+        expectedState.inFlight = 0;
+        expectedState.queries = {
+          "id.0": createMetricsState("id.0", shortTimespan, ["short.1"], 3),
+          "id.1": createMetricsState("id.1", longTimespan, ["long.1"], 3),
+          "id.2": createMetricsState("id.2", shortTimespan, ["short.2", "short.3"], 3),
+          "id.3": createMetricsState("id.3", longTimespan, ["long.2", "long.3"], 3),
+          "id.4": createMetricsState("id.4", shortTimespan, ["short.4"], 3),
+          "id.5": createMetricsState("id.5", longTimespan, ["long.4", "long.5"], 3),
+        };
+
+        return expectSaga(metrics.queryMetricsSaga)
+          .withReducer(metrics.metricsReducer)
+          .hasFinalState(expectedState)
+          .provide({
+            call(effect, next) {
+              if (effect.fn === queryTimeSeries) {
+                return new Promise((resolve) => {
+                  setTimeout(
+                    () => resolve(createResponse((effect.args[0] as TimeSeriesQueryRequestMessage).queries, createDatapoints(3))),
+                    10,
+                  );
+                });
+              }
+              return next();
+            },
+          })
+          .dispatch(shortRequests[0])
+          .dispatch(longRequests[0])
+          .dispatch(shortRequests[1])
+          .delay(0)
+          .dispatch(longRequests[1])
+          .dispatch(shortRequests[2])
+          .dispatch(longRequests[2])
+          .run();
+      });
+
+      it("handles errors correctly", function() {
+        const fakeError = new Error("connection error");
+
+        const expectedState = new metrics.MetricsState();
+        expectedState.inFlight = 0;
+        expectedState.queries = {
+          "id.0": createMetricsState("id.0", shortTimespan, ["short.1"], 3),
+          "id.1": createMetricsState("id.1", longTimespan, ["long.1"], 3),
+          "id.2": createMetricsState("id.2", shortTimespan, ["short.2", "short.3"], 3),
+          "id.3": createMetricsErrorState("id.3", longTimespan, ["long.2", "long.3"], fakeError),
+          "id.4": createMetricsErrorState("id.4", shortTimespan, ["short.4"], fakeError),
+          "id.5": createMetricsErrorState("id.5", longTimespan, ["long.4", "long.5"], fakeError),
+        };
+
+        let callCounter = 0;
+        return expectSaga(metrics.queryMetricsSaga)
+          .withReducer(metrics.metricsReducer)
+          .hasFinalState(expectedState)
+          .provide({
+            call(effect, next) {
+              if (effect.fn === queryTimeSeries) {
+                callCounter++;
+                if (callCounter > 2) {
+                  throw fakeError;
+                }
+                return createResponse((effect.args[0] as TimeSeriesQueryRequestMessage).queries, createDatapoints(3));
+              }
+              return next();
+            },
+          })
+          .dispatch(shortRequests[0])
+          .dispatch(longRequests[0])
+          .dispatch(shortRequests[1])
+          .delay(0)
+          .dispatch(longRequests[1])
+          .dispatch(shortRequests[2])
+          .dispatch(longRequests[2])
+          .run();
+      });
+
+      it("handles inflight counter correctly", function() {
+        const expectedState = new metrics.MetricsState();
+        expectedState.inFlight = 1;
+        expectedState.queries = {
+          "id.0": createMetricsState("id.0", shortTimespan, ["short.1"], 3),
+          "id.1": createMetricsState("id.1", longTimespan, ["long.1"], 3),
+          "id.2": createMetricsState("id.2", shortTimespan, ["short.2", "short.3"], 3),
+          "id.3": createMetricsInFlightState("id.3", longTimespan, ["long.2", "long.3"]),
+          "id.4": createMetricsInFlightState("id.4", shortTimespan, ["short.4"]),
+          "id.5": createMetricsInFlightState("id.5", longTimespan, ["long.4", "long.5"]),
+        };
+
+        let callCounter = 0;
+        return expectSaga(metrics.queryMetricsSaga)
+          .withReducer(metrics.metricsReducer)
+          .hasFinalState(expectedState)
+          .provide({
+            call(effect, next) {
+              if (effect.fn === queryTimeSeries) {
+                callCounter++;
+                if (callCounter > 2) {
+                  // return a promise that never resolves.
+                  return new Promise((_resolve) => {});
+                }
+                return createResponse((effect.args[0] as TimeSeriesQueryRequestMessage).queries, createDatapoints(3));
+              }
+              return next();
+            },
+          })
+          .dispatch(shortRequests[0])
+          .dispatch(longRequests[0])
+          .dispatch(shortRequests[1])
+          .delay(0)
+          .dispatch(longRequests[1])
+          .dispatch(shortRequests[2])
+          .dispatch(longRequests[2])
+          .run();
       });
     });
   });
