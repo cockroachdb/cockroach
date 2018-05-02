2aba151f38c6ee6cb0a8f33e666c9ff513f13edc
Victor Chen <victor@cockroachlabs.com>
2018-05-02 10:05:34 -0400

    encryption: add support for encryption to `writeFileSyncing`.
    `writeFileSyncing()` now will be able to write encrypted content to RocksDB's
    env. This commit is part of `use encryption for all local disk usage (non-logs)`.
    
    Issue: #19783.
    Release note: None

--- pkg/storage/client_test.go
+++ pkg/storage/client_test.go
@@ -202,6 +202,8 @@ type multiTestContext struct {
 	dbs         []*client.DB
 	gossips     []*gossip.Gossip
 	storePools  []*storage.StorePool
+	dirCleanups []func()
+	caches      []engine.RocksDBCache
 	// We use multiple stoppers so we can restart different parts of the
 	// test individually. transportStopper is for 'transport', and the
 	// 'stoppers' slice corresponds to the 'stores'.
@@ -338,6 +340,14 @@ func (m *multiTestContext) Stop() {
 			}
 		}
 		m.transportStopper.Stop(context.TODO())
+
+		for _, cleanup := range m.dirCleanups {
+			cleanup()
+		}
+
+		for _, cache := range m.caches {
+			cache.Release()
+		}
 
 		for _, s := range m.engineStoppers {
 			s.Stop(context.TODO())
@@ -693,7 +703,20 @@ func (m *multiTestContext) addStore(idx int) {
 	} else {
 		engineStopper := stop.NewStopper()
 		m.engineStoppers = append(m.engineStoppers, engineStopper)
-		eng = engine.NewInMem(roachpb.Attributes{}, 1<<20)
+
+		dir, cleanup := testutils.TempDir(m.t)
+		cache := engine.NewRocksDBCache(1 << 20)
+		var err error
+		eng, err = engine.NewRocksDB(engine.RocksDBConfig{
+			Dir:       dir,
+			MustExist: false,
+		}, cache)
+		if err != nil {
+			m.t.Fatal(err)
+		}
+
+		m.dirCleanups = append(m.dirCleanups, cleanup)
+		m.caches = append(m.caches, cache)
 		engineStopper.AddCloser(eng)
 		m.engines = append(m.engines, eng)
 		needBootstrap = true
