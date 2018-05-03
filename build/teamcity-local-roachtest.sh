6b113976e6a2d17af1c4ec3605d369514075c340
Daniel Harrison <daniel.harrison@gmail.com>
2018-05-03 17:45:54 -0400

    build: add roachtests to be run on every PR
    Release note: None

---
+++ build/teamcity-local-roachtest.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source "$(dirname "${0}")/teamcity-support.sh"
+
+tc_start_block "Prepare environment"
+run mkdir -p artifacts
+maybe_ccache
+tc_end_block "Prepare environment"
+
+tc_start_block "Install roachprod"
+run build/builder.sh go get -u -v github.com/cockroachdb/roachprod
+tc_end_block "Install roachprod"
+
+tc_start_block "Compile CockroachDB"
+run build/builder.sh make build
+tc_end_block "Compile CockroachDB"
+
+tc_start_block "Compile workload/roachtest"
+run build/builder.sh make bin/workload bin/roachtest
+tc_end_block "Compile workload/roachtest"
+
+tc_start_block "Run local roachtests"
+# TODO(dan): Run kv/splits as a proof of concept of running roachtest on every
+# PR. After we're sure this is stable, curate a suite of the tests that work
+# locally.
+run build/builder.sh ./bin/roachtest run kv/splits \
+  --local \
+  --cockroach "cockroach" \
+  --workload "bin/workload" \
+  --artifacts artifacts \
+  --teamcity
+tc_end_block "Run local roachtests"
