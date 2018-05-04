64e4cd3509b6588a8294deef957e16eb33c29794
Radu Berinde <radu@cockroachlabs.com>
2018-05-04 10:40:54 -0400

    opt: initial support for optimizer + distsql combo
    This change adds the following:
    
     - support for running DistSQL queries through the TestEngine
    
     - execbuilder now runs every query in both local and DistSQL mode,
       unless a `nodist` flag is set. Not letting the DistSQL planner
       automatically decide if it can distribute a plan is intentional: we
       want it to be clear from the tests what can and what can't run as
       DistSQL; and we want to make sure that what we think should run
       actually gets executed.
    
     - Adding nodist as appropriate in the execbuilder tests. The most
       common is for EXPLAIN queries, but there are a few things DistSQL
       doesn't support: subqueries, tuples, ordinality.
    
    Note that even with DistSQL, we still run in a single node mode, which
    does not exercise all aspects of distributed planning. In a future PR,
    we will tweak the configuration to have multiple nodes, and possibly
    use the fake span resolver.
    
    Release note: None

--- pkg/sql/opt/testutils/opt_tester.go
+++ pkg/sql/opt/testutils/opt_tester.go
@@ -97,6 +97,12 @@ func NewOptTester(catalog opt.Catalog, sql string) *OptTester {
 	}
 }
 
+// SetCatalog sets the catalog; used when the catalog is not known upfront
+// (a nil catalog was passed to NewOptTester).
+func (ot *OptTester) SetCatalog(catalog opt.Catalog) {
+	ot.catalog = catalog
+}
+
 // RunCommand implements commands that are used by most tests:
 //
 //  - exec-ddl
@@ -402,7 +408,9 @@ func (ot *OptTester) ExecBuild(eng exec.TestEngine) (exec.Plan, error) {
 }
 
 // Exec builds the exec node tree for the SQL query and then executes it.
-func (ot *OptTester) Exec(eng exec.TestEngine) (sqlbase.ResultColumns, []tree.Datums, error) {
+func (ot *OptTester) Exec(
+	eng exec.TestEngine, useDistSQL bool,
+) (sqlbase.ResultColumns, []tree.Datums, error) {
 	plan, err := ot.ExecBuild(eng)
 	if err != nil {
 		return nil, nil, err
@@ -411,7 +419,7 @@ func (ot *OptTester) Exec(eng exec.TestEngine) (sqlbase.ResultColumns, []tree.Da
 	columns := eng.Columns(plan)
 
 	var datums []tree.Datums
-	datums, err = eng.Execute(plan)
+	datums, err = eng.Execute(plan, useDistSQL)
 	if err != nil {
 		return nil, nil, err
 	}
