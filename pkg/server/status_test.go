783584b595b8f063ea955cdb421f14313f6eed0e
Sean Loiselle <himself@seanloiselle.com>
2018-05-05 19:13:11 -0400

    server: Add test for new metric.Metadata struct
    To ensure that charts can be consistently and meaningfully interpreted, this test ensures that all metrics have Name, Help,
    AxisLabel, and MetricType set.
    
    Release note: None

--- pkg/server/status_test.go
+++ pkg/server/status_test.go
@@ -412,6 +412,35 @@ func TestMetricsEndpoint(t *testing.T) {
 	}
 }
 
+// TestMetricsMetadata ensures that the server's recorder return metrics and
+// that each metric has a Name, Help, Unit, and DisplayUnit defined.
+func TestMetricsMetadata(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	s := startServer(t)
+	defer s.Stopper().Stop(context.TODO())
+
+	metricsMetadata := s.recorder.GetMetricsMetadata()
+
+	if len(metricsMetadata) == 0 {
+		t.Fatal("s.recorder.GetMetricsMetadata() returned no metric metadata")
+	}
+
+	for _, v := range metricsMetadata {
+		if v.Name == "" {
+			t.Fatal("metric missing name")
+		}
+		if v.Help == "" {
+			t.Fatalf("%s missing Help", v.Name)
+		}
+		if v.Unit == "" {
+			t.Fatalf("%s missing Unit", v.Name)
+		}
+		if v.DisplayUnit == 0 {
+			t.Fatalf("%s missing DisplayUnit", v.Name)
+		}
+	}
+}
+
 func TestRangesResponse(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer storage.EnableLeaseHistory(100)()
