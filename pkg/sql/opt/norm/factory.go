46a5d649b900fc958e9437ac2b3e0e7dd9b74a8f
Andrew Kimball <andyk@cockroachlabs.com>
2018-05-08 17:29:19 -0700

    opt: Enable inlining of projected expressions
    There are two cases where inlining Project expressions is useful:
    
    1. When an outer projection has no more than one dependency on each
       of an inner projection's expressions. In this case, it makes sense
       to merge the two projections in order to eliminate extra operators.
    
    2. When an outer Select operator depends on "simple" expressions that
       are projected by an inner Project operator. A simple expression
       consists only of operators like Variable, Const, Eq, Plus, etc.
       By inlining the expressions, Select conjuncts can be made
       independent of the inner Project, which then allows them to be more
       freely moved (e.g. pushed below the Project into a Scan).
    
    Release note: None

--- pkg/sql/opt/norm/factory.go
+++ pkg/sql/opt/norm/factory.go
@@ -152,6 +152,10 @@ func (f *Factory) onConstruct(e memo.Expr) memo.GroupID {
 //
 // ----------------------------------------------------------------------
 
+func (f *Factory) extractColID(private memo.PrivateID) opt.ColumnID {
+	return f.mem.LookupPrivate(private).(opt.ColumnID)
+}
+
 func (f *Factory) extractColList(private memo.PrivateID) opt.ColList {
 	return f.mem.LookupPrivate(private).(opt.ColList)
 }
@@ -452,13 +456,6 @@ func (f *Factory) hasCorrelatedSubquery(group memo.GroupID) bool {
 	return f.lookupScalar(group).HasCorrelatedSubquery
 }
 
-// isScalarGroupBy returns true if the given grouping columns come from a
-// "scalar" GroupBy operator. A scalar GroupBy always returns exactly one row,
-// with any aggregate functions operating over the entire input expression.
-func (f *Factory) isScalarGroupBy(groupingCols memo.PrivateID) bool {
-	return f.mem.LookupPrivate(groupingCols).(opt.ColSet).Empty()
-}
-
 // ----------------------------------------------------------------------
 //
 // Select Rules
@@ -636,6 +633,13 @@ func (f *Factory) colsAreKey(cols memo.PrivateID, group memo.GroupID) bool {
 		}
 	}
 	return false
+}
+
+// isScalarGroupBy returns true if the given grouping columns come from a
+// "scalar" GroupBy operator. A scalar GroupBy always returns exactly one row,
+// with any aggregate functions operating over the entire input expression.
+func (f *Factory) isScalarGroupBy(groupingCols memo.PrivateID) bool {
+	return f.mem.LookupPrivate(groupingCols).(opt.ColSet).Empty()
 }
 
 // ----------------------------------------------------------------------
---
+++ pkg/sql/opt/norm/inline.go
@@ -0,0 +1,108 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package norm
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/sql/opt"
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/memo"
+)
+
+// hasDuplicateRefs returns true if the target expression references any
+// variable more than one time. For example:
+//
+//   SELECT * FROM a WHERE x=1 OR x=2 OR y=0
+//
+// hasDuplicateRefs would be true for the filter expression, since the x column
+// is referenced twice.
+func (f *Factory) hasDuplicateRefs(target memo.GroupID) bool {
+	var refs opt.ColSet
+
+	// When a column reference is found, add it to the refs set. If the set
+	// already contains a reference to that column, then there is a duplicate.
+	var countRefs func(group memo.GroupID) bool
+	countRefs = func(group memo.GroupID) bool {
+		expr := f.mem.NormExpr(group)
+		if expr.Operator() == opt.VariableOp {
+			colID := f.extractColID(expr.AsVariable().Col())
+			if refs.Contains(int(colID)) {
+				return true
+			}
+			refs.Add(int(colID))
+			return false
+		}
+
+		for i := 0; i < expr.ChildCount(); i++ {
+			if countRefs(expr.ChildGroup(f.mem, i)) {
+				return true
+			}
+		}
+		return false
+	}
+	return countRefs(target)
+}
+
+// canInline returns true if the given expression consists only of "simple"
+// operators like Variable, Const, Eq, and Plus. These operators are assumed to
+// be relatively inexpensive to evaluate, and therefore potentially evaluating
+// them multiple times is not a big concern.
+func (f *Factory) canInline(group memo.GroupID) bool {
+	expr := f.mem.NormExpr(group)
+	switch expr.Operator() {
+	case opt.ProjectionsOp,
+		opt.AndOp, opt.OrOp, opt.NotOp, opt.TrueOp, opt.FalseOp,
+		opt.EqOp, opt.NeOp, opt.LeOp, opt.LtOp, opt.GeOp, opt.GtOp,
+		opt.IsOp, opt.IsNotOp, opt.InOp, opt.NotInOp,
+		opt.VariableOp, opt.ConstOp, opt.NullOp,
+		opt.PlusOp, opt.MinusOp, opt.MultOp:
+
+		// Recursively verify that children are also inlinable.
+		for i := 0; i < expr.ChildCount(); i++ {
+			if !f.canInline(expr.ChildGroup(f.mem, i)) {
+				return false
+			}
+		}
+		return true
+	}
+	return false
+}
+
+// inlineProjections searches the target expression for any references to
+// columns in the projections expression. Target variable references are
+// replaced by the inlined projection expression.
+func (f *Factory) inlineProjections(target, projections memo.GroupID) memo.GroupID {
+	projectionsExpr := f.mem.NormExpr(projections).AsProjections()
+	projectionsElems := f.mem.LookupList(projectionsExpr.Elems())
+	projectionsColList := f.extractColList(projectionsExpr.Cols())
+
+	// Recursively walk the tree looking for references to projection expressions
+	// that need to be replaced.
+	var replace memo.ReplaceChildFunc
+	replace = func(child memo.GroupID) memo.GroupID {
+		varExpr := f.mem.NormExpr(child).AsVariable()
+		if varExpr != nil {
+			varColID := f.extractColID(varExpr.Col())
+			for i, id := range projectionsColList {
+				if varColID == id {
+					return projectionsElems[i]
+				}
+			}
+		}
+		ev := memo.MakeNormExprView(f.mem, child)
+		return ev.Replace(f.evalCtx, replace).Group()
+	}
+
+	ev := memo.MakeNormExprView(f.mem, target)
+	return ev.Replace(f.evalCtx, replace).Group()
+}
