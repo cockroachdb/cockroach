5cd654b60fa37d7d076b47fe999a3c63fc78da94
Andrei Matei <andrei@cockroachlabs.com>
2018-05-15 18:09:17 -0400

    client: explicitly reject requests sent after an error
    Requests other than a rollback cannot be sent on a Txn after a previous
    request received a non-retriable error because the state of the txn
    record is unclear (does it exist? Should another BeginTxn be sent?).
    This patch adds explicit checks for rejecting such batches.

--- pkg/roachpb/api.pb.go
+++ pkg/roachpb/api.pb.go
@@ -149,6 +149,7 @@
 		UnhandledRetryableError
 		HandledRetryableTxnError
 		TxnPrevAttemptError
+		TxnAlreadyEncounteredErrorError
 		IntegerOverflowError
 		MixedSuccessError
 		BatchTimestampBeforeGCError
