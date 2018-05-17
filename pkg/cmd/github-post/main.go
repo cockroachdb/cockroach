8100e54e8e6668bbdca11a09c73f23b896349d93
Peter Mattis <petermattis@gmail.com>
2018-05-17 10:46:32 -0400

    roachtest: post github issues on failure
    Refactored determination of the issue assignee, splitting it up between
    determining the author of the failing line of code and the mapping the
    author email to a github login.
    
    Fixes #24894
    
    Release note: None

--- pkg/cmd/github-post/main.go
+++ pkg/cmd/github-post/main.go
@@ -17,9 +17,13 @@ package main
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"os/exec"
+	"regexp"
+	"strings"
 
 	"github.com/cockroachdb/cockroach/pkg/cmd/internal/issues"
 	"github.com/pkg/errors"
@@ -37,15 +41,15 @@ func main() {
 	}
 }
 
-func postIssue(ctx context.Context, packageName, testName, testMessage string) error {
+func postIssue(ctx context.Context, packageName, testName, testMessage, authorEmail string) error {
 	const detail = " under stress"
-	return issues.Post(ctx, detail, packageName, testName, testMessage)
+	return issues.Post(ctx, detail, packageName, testName, testMessage, authorEmail)
 }
 
 func listFailures(
 	ctx context.Context,
 	input io.Reader,
-	f func(ctx context.Context, packageName, testName, testMessage string) error,
+	f func(ctx context.Context, packageName, testName, testMessage, authorEmail string) error,
 ) error {
 	var inputBuf bytes.Buffer
 	input = io.TeeReader(input, &inputBuf)
@@ -68,7 +72,11 @@ func listFailures(
 		for _, test := range suite.Tests {
 			switch test.Status {
 			case lib.Failed:
-				if err := f(ctx, packageName, test.Name, test.Message); err != nil {
+				authorEmail, err := getAuthorEmail(ctx, packageName, test.Name)
+				if err != nil {
+					log.Printf("unable to determine test author email: %s\n", err)
+				}
+				if err := f(ctx, packageName, test.Name, test.Message, authorEmail); err != nil {
 					return errors.Wrap(err, "failed to post issue")
 				}
 				posted = true
@@ -85,9 +93,57 @@ func listFailures(
 		if !ok {
 			packageName = unknown
 		}
-		if err := f(ctx, packageName, unknown, inputBuf.String()); err != nil {
+		if err := f(ctx, packageName, unknown, inputBuf.String(), ""); err != nil {
 			return errors.Wrap(err, "failed to post issue")
 		}
 	}
 	return nil
+}
+
+func getAuthorEmail(ctx context.Context, packageName, testName string) (string, error) {
+	// Search the source code for the email address of the last committer to touch
+	// the first line of the source code that contains testName. Then, ask GitHub
+	// for the GitHub username of the user with that email address by searching
+	// commits in cockroachdb/cockroach for commits authored by the address.
+	subtests := strings.Split(testName, "/")
+	testName = subtests[0]
+	packageName = strings.TrimPrefix(packageName, "github.com/cockroachdb/cockroach/")
+	cmd := exec.Command(`/bin/bash`, `-c`,
+		fmt.Sprintf(`git grep -n "func %s" $(git rev-parse --show-toplevel)/%s/*_test.go`,
+			testName, packageName))
+	// This command returns output such as:
+	// ../ccl/storageccl/export_test.go:31:func TestExportCmd(t *testing.T) {
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", errors.Errorf("couldn't find test %s in %s: %s %s",
+			testName, packageName, err, string(out))
+	}
+	re := regexp.MustCompile(`(.*):(.*):`)
+	// The first 2 :-delimited fields are the filename and line number.
+	matches := re.FindSubmatch(out)
+	if matches == nil {
+		return "", errors.Errorf("couldn't find filename/line number for test %s in %s: %s",
+			testName, packageName, string(out))
+	}
+	filename := matches[1]
+	linenum := matches[2]
+
+	// Now run git blame.
+	cmd = exec.Command(`/bin/bash`, `-c`,
+		fmt.Sprintf(`git blame --porcelain -L%s,+1 %s | grep author-mail`,
+			linenum, filename))
+	// This command returns output such as:
+	// author-mail <jordan@cockroachlabs.com>
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		return "", errors.Errorf("couldn't find author of test %s in %s: %s %s",
+			testName, packageName, err, string(out))
+	}
+	re = regexp.MustCompile("author-mail <(.*)>")
+	matches = re.FindSubmatch(out)
+	if matches == nil {
+		return "", errors.Errorf("couldn't find author email of test %s in %s: %s",
+			testName, packageName, string(out))
+	}
+	return string(matches[1]), nil
 }
