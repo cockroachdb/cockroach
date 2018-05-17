bfb49f6523602eda3bfbfe84a4597343d4804f06
Andrei Matei <andrei@cockroachlabs.com>
2018-05-17 18:09:36 -0400

    storage: deal with txn wait queue race
    There was a race between cleaning up a txn and a pusher: if the pusher
    had observed a timestamp, then the pushee is cleaned up, and only then
    the pusher enters the wait queue, it'll take 2s for the pusher to
    inspect the pushee's record again and notice that it's gone.
    This patch adds a check of the pushee's record immediately upon entering
    the queue.
    
    Release note: None

--- pkg/storage/txnwait/queue.go
+++ pkg/storage/txnwait/queue.go
@@ -454,12 +454,20 @@ func (q *Queue) MaybeWaitForPush(
 	pusherPriority := req.PusherTxn.Priority
 	pusheePriority := req.PusheeTxn.Priority
 
+	first := true
 	for {
 		// Set the timer to check for the pushee txn's expiration.
-		{
+		if !first {
 			expiration := TxnExpiration(pending.txn.Load().(*roachpb.Transaction)).GoTime()
 			now := q.store.Clock().Now().GoTime()
 			pusheeTxnTimer.Reset(expiration.Sub(now))
+		} else {
+			// The first time we want to check the pushee's txn record immediately:
+			// the pushee might be gone by the time the pusher gets here if it cleaned
+			// itself up after the pusher saw an intent but before it entered this
+			// queue.
+			pusheeTxnTimer.Reset(0)
+			first = false
 		}
 
 		select {
