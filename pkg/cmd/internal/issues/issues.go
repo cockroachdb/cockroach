50733d5939ec02ea1f1652eaf34287b75156509e
Peter Mattis <petermattis@gmail.com>
2018-05-17 09:56:45 -0400

    cmd/github-post: refactor issue posting into a library
    See #24894
    
    Release note: None

--- pkg/cmd/internal/issues/issues.go
+++ pkg/cmd/internal/issues/issues.go
@@ -12,84 +12,58 @@
 // implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
-package main
+package issues
 
 import (
-	"bytes"
 	"context"
 	"fmt"
-	"io"
 	"log"
 	"net/url"
 	"os"
 	"os/exec"
 	"regexp"
 	"strings"
+	"sync"
 
 	"golang.org/x/oauth2"
 
 	"github.com/google/go-github/github"
 	version "github.com/hashicorp/go-version"
 	"github.com/pkg/errors"
-	"github.com/tebeka/go2xunit/lib"
 )
 
-const githubAPITokenEnv = "GITHUB_API_TOKEN"
-const teamcityVCSNumberEnv = "BUILD_VCS_NUMBER"
-const teamcityBuildIDEnv = "TC_BUILD_ID"
-const teamcityServerURLEnv = "TC_SERVER_URL"
-const githubUser = "cockroachdb"
-const githubRepo = "cockroach"
+const (
+	githubAPITokenEnv    = "GITHUB_API_TOKEN"
+	teamcityVCSNumberEnv = "BUILD_VCS_NUMBER"
+	teamcityBuildIDEnv   = "TC_BUILD_ID"
+	teamcityServerURLEnv = "TC_SERVER_URL"
+	tagsEnv              = "TAGS"
+	goFlagsEnv           = "GOFLAGS"
+	githubUser           = "cockroachdb"
+	githubRepo           = "cockroach"
+	cockroachPkgPrefix   = "github.com/cockroachdb/cockroach/pkg/"
+)
 
-const pkgEnv = "PKG"
-const tagsEnv = "TAGS"
-const goFlagsEnv = "GOFLAGS"
-const cockroachPkgPrefix = "github.com/cockroachdb/cockroach/pkg/"
-
-var issueLabels = []string{"O-robot", "C-test-failure"}
+var (
+	issueLabels  = []string{"O-robot", "C-test-failure"}
+	stacktraceRE = regexp.MustCompile(`(?m:^goroutine\s\d+)`)
+)
 
 // Based on the following observed API response:
 //
-// 422 Validation Failed [{Resource:Issue Field:body Code:custom Message:body is too long (maximum is 65536 characters)}]
+// 422 Validation Failed [{Resource:Issue Field:body Code:custom Message:body
+// is too long (maximum is 65536 characters)}]
 const githubIssueBodyMaximumLength = 1<<16 - 1
 
-func main() {
-	token, ok := os.LookupEnv(githubAPITokenEnv)
-	if !ok {
-		log.Fatalf("GitHub API token environment variable %s is not set", githubAPITokenEnv)
-	}
-
-	ctx := context.Background()
-
-	client := github.NewClient(oauth2.NewClient(ctx, oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: token},
-	)))
-
-	getLatestTag := func() (string, error) {
-		cmd := exec.Command("git", "describe", "--abbrev=0", "--tags")
-		out, err := cmd.CombinedOutput()
-		if err != nil {
-			return "", err
-		}
-		return strings.TrimSpace(string(out)), nil
-	}
-
-	if err := runGH(
-		ctx,
-		os.Stdin,
-		client.Issues.Create,
-		client.Search.Issues,
-		client.Issues.CreateComment,
-		client.Repositories.ListCommits,
-		client.Issues.ListMilestones,
-		getLatestTag,
-	); err != nil {
-		log.Fatal(err)
-	}
-}
-
-var stacktraceRE = regexp.MustCompile(`(?m:^goroutine\s\d+)`)
-
+// trimIssueRequestBody trims message such that the total size of an issue body
+// is less than githubIssueBodyMaximumLength. usedCharacters specifies the
+// number of characters that have already been used for the issue body (see
+// newIssueRequest below). message is usually the test failure message and
+// possibly includes stacktraces for all of the goroutines (which is what makes
+// the message very large).
+//
+// TODO(peter): Rather than trimming the message like this, perhaps it can be
+// added as an attachment or some other expandable comment.
 func trimIssueRequestBody(message string, usedCharacters int) string {
 	maxLength := githubIssueBodyMaximumLength - usedCharacters
 
@@ -134,7 +108,8 @@ var oldFriendsMap = map[string]string{
 func getAssignee(
 	ctx context.Context,
 	packageName, testName string,
-	listCommits func(ctx context.Context, owner string, repo string, opts *github.CommitsListOptions) ([]*github.RepositoryCommit, *github.Response, error),
+	listCommits func(ctx context.Context, owner string, repo string,
+		opts *github.CommitsListOptions) ([]*github.RepositoryCommit, *github.Response, error),
 ) (string, error) {
 	// Search the source code for the email address of the last committer to touch
 	// the first line of the source code that contains testName. Then, ask GitHub
@@ -143,34 +118,42 @@ func getAssignee(
 	subtests := strings.Split(testName, "/")
 	testName = subtests[0]
 	packageName = strings.TrimPrefix(packageName, "github.com/cockroachdb/cockroach/")
-	cmd := exec.Command(`/bin/bash`, `-c`, fmt.Sprintf(`git grep -n "func %s" $(git rev-parse --show-toplevel)/%s/*_test.go`, testName, packageName))
+	cmd := exec.Command(`/bin/bash`, `-c`,
+		fmt.Sprintf(`git grep -n "func %s" $(git rev-parse --show-toplevel)/%s/*_test.go`,
+			testName, packageName))
 	// This command returns output such as:
 	// ../ccl/storageccl/export_test.go:31:func TestExportCmd(t *testing.T) {
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return "", errors.Errorf("couldn't find test %s in %s: %s %s", testName, packageName, err, string(out))
+		return "", errors.Errorf("couldn't find test %s in %s: %s %s",
+			testName, packageName, err, string(out))
 	}
 	re := regexp.MustCompile(`(.*):(.*):`)
 	// The first 2 :-delimited fields are the filename and line number.
 	matches := re.FindSubmatch(out)
 	if matches == nil {
-		return "", errors.Errorf("couldn't find filename/line number for test %s in %s: %s", testName, packageName, string(out))
+		return "", errors.Errorf("couldn't find filename/line number for test %s in %s: %s",
+			testName, packageName, string(out))
 	}
 	filename := matches[1]
 	linenum := matches[2]
 
 	// Now run git blame.
-	cmd = exec.Command(`/bin/bash`, `-c`, fmt.Sprintf(`git blame --porcelain -L%s,+1 %s | grep author-mail`, linenum, filename))
+	cmd = exec.Command(`/bin/bash`, `-c`,
+		fmt.Sprintf(`git blame --porcelain -L%s,+1 %s | grep author-mail`,
+			linenum, filename))
 	// This command returns output such as:
 	// author-mail <jordan@cockroachlabs.com>
 	out, err = cmd.CombinedOutput()
 	if err != nil {
-		return "", errors.Errorf("couldn't find author of test %s in %s: %s %s", testName, packageName, err, string(out))
+		return "", errors.Errorf("couldn't find author of test %s in %s: %s %s",
+			testName, packageName, err, string(out))
 	}
 	re = regexp.MustCompile("author-mail <(.*)>")
 	matches = re.FindSubmatch(out)
 	if matches == nil {
-		return "", errors.Errorf("couldn't find author email of test %s in %s: %s", testName, packageName, string(out))
+		return "", errors.Errorf("couldn't find author email of test %s in %s: %s",
+			testName, packageName, string(out))
 	}
 	email := string(matches[1])
 
@@ -195,10 +178,20 @@ func getAssignee(
 	return assignee, nil
 }
 
+func getLatestTag() (string, error) {
+	cmd := exec.Command("git", "describe", "--abbrev=0", "--tags")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(out)), nil
+}
+
 func getProbableMilestone(
 	ctx context.Context,
 	getLatestTag func() (string, error),
-	listMilestones func(ctx context.Context, owner string, repo string, opt *github.MilestoneListOptions) ([]*github.Milestone, *github.Response, error),
+	listMilestones func(ctx context.Context, owner string, repo string,
+		opt *github.MilestoneListOptions) ([]*github.Milestone, *github.Response, error),
 ) *int {
 	tag, err := getLatestTag()
 	if err != nil {
@@ -236,69 +229,77 @@ func getProbableMilestone(
 	return nil
 }
 
-func runGH(
-	ctx context.Context,
-	input io.Reader,
-	createIssue func(ctx context.Context, owner string, repo string, issue *github.IssueRequest) (*github.Issue, *github.Response, error),
-	searchIssues func(ctx context.Context, query string, opt *github.SearchOptions) (*github.IssuesSearchResult, *github.Response, error),
-	createComment func(ctx context.Context, owner string, repo string, number int, comment *github.IssueComment) (*github.IssueComment, *github.Response, error),
-	listCommits func(ctx context.Context, owner string, repo string, opts *github.CommitsListOptions) ([]*github.RepositoryCommit, *github.Response, error),
-	listMilestones func(ctx context.Context, owner string, repo string, opt *github.MilestoneListOptions) ([]*github.Milestone, *github.Response, error),
-	getLatestTag func() (string, error),
-) error {
-	sha, ok := os.LookupEnv(teamcityVCSNumberEnv)
+type poster struct {
+	sha       string
+	buildID   string
+	serverURL string
+	milestone *int
+
+	createIssue func(ctx context.Context, owner string, repo string,
+		issue *github.IssueRequest) (*github.Issue, *github.Response, error)
+	searchIssues func(ctx context.Context, query string,
+		opt *github.SearchOptions) (*github.IssuesSearchResult, *github.Response, error)
+	createComment func(ctx context.Context, owner string, repo string, number int,
+		comment *github.IssueComment) (*github.IssueComment, *github.Response, error)
+	listCommits func(ctx context.Context, owner string, repo string,
+		opts *github.CommitsListOptions) ([]*github.RepositoryCommit, *github.Response, error)
+	listMilestones func(ctx context.Context, owner string, repo string,
+		opt *github.MilestoneListOptions) ([]*github.Milestone, *github.Response, error)
+	getLatestTag func() (string, error)
+}
+
+func newPoster() *poster {
+	token, ok := os.LookupEnv(githubAPITokenEnv)
 	if !ok {
-		return errors.Errorf("VCS number environment variable %s is not set", teamcityVCSNumberEnv)
+		log.Fatalf("GitHub API token environment variable %s is not set", githubAPITokenEnv)
 	}
 
-	buildID, ok := os.LookupEnv(teamcityBuildIDEnv)
-	if !ok {
+	ctx := context.Background()
+	client := github.NewClient(oauth2.NewClient(ctx, oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)))
+
+	return &poster{
+		createIssue:    client.Issues.Create,
+		searchIssues:   client.Search.Issues,
+		createComment:  client.Issues.CreateComment,
+		listCommits:    client.Repositories.ListCommits,
+		listMilestones: client.Issues.ListMilestones,
+		getLatestTag:   getLatestTag,
+	}
+}
+
+func (p *poster) init() {
+	var ok bool
+	if p.sha, ok = os.LookupEnv(teamcityVCSNumberEnv); !ok {
+		log.Fatalf("VCS number environment variable %s is not set", teamcityVCSNumberEnv)
+	}
+	if p.buildID, ok = os.LookupEnv(teamcityBuildIDEnv); !ok {
 		log.Fatalf("teamcity build ID environment variable %s is not set", teamcityBuildIDEnv)
 	}
-
-	serverURL, ok := os.LookupEnv(teamcityServerURLEnv)
-	if !ok {
+	if p.serverURL, ok = os.LookupEnv(teamcityServerURLEnv); !ok {
 		log.Fatalf("teamcity server URL environment variable %s is not set", teamcityServerURLEnv)
 	}
+	p.milestone = getProbableMilestone(context.Background(), p.getLatestTag, p.listMilestones)
+}
 
-	options := url.Values{}
-	options.Add("buildId", buildID)
-	options.Add("tab", "buildLog")
+func (p *poster) post(ctx context.Context, detail, packageName, testName, message string) error {
+	packageName = cockroachPkgPrefix + packageName
 
-	u, err := url.Parse(serverURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	u.Scheme = "https"
-	u.Path = "viewLog.html"
-	u.RawQuery = options.Encode()
-
-	var inputBuf bytes.Buffer
-	input = io.TeeReader(input, &inputBuf)
-
-	var parameters []string
-	for _, parameter := range []string{
-		tagsEnv,
-		goFlagsEnv,
-	} {
-		if val, ok := os.LookupEnv(parameter); ok {
-			parameters = append(parameters, parameter+"="+val)
-		}
-	}
-	parametersStr := "```\n" + strings.Join(parameters, "\n") + "\n```"
-	const bodyTemplate = `SHA: https://github.com/cockroachdb/cockroach/commits/%s
+	const bodyTemplate = `SHA: https://github.com/cockroachdb/cockroach/commits/%[1]s
 
 Parameters:
-%s
+` + "```" + `
+%[2]s
+` + "```" + `
 
-Stress build found a failed test: %s`
-
-	milestone := getProbableMilestone(ctx, getLatestTag, listMilestones)
+Failed test: %[3]s`
+	const messageTemplate = "\n\n```\n%s\n```"
 
 	newIssueRequest := func(packageName, testName, message, assignee string) *github.IssueRequest {
-		title := fmt.Sprintf("%s: %s failed under stress",
-			strings.TrimPrefix(packageName, cockroachPkgPrefix), testName)
-		body := fmt.Sprintf(bodyTemplate, sha, parametersStr, u.String()) + "\n\n```\n%s\n```"
+		title := fmt.Sprintf("%s: %s failed%s",
+			strings.TrimPrefix(packageName, cockroachPkgPrefix), testName, detail)
+		body := fmt.Sprintf(bodyTemplate, p.sha, p.parameters(), p.teamcityURL()) + messageTemplate
 		// We insert a raw "%s" above so we can figure out the length of the
 		// body so far, without the actual error text. We need this length so we
 		// can calculate the maximum amount of error text we can include in the
@@ -311,86 +312,100 @@ Stress build found a failed test: %s`
 			Body:      &body,
 			Labels:    &issueLabels,
 			Assignee:  &assignee,
-			Milestone: milestone,
+			Milestone: p.milestone,
 		}
 	}
 
 	newIssueComment := func(packageName, testName string) *github.IssueComment {
-		body := fmt.Sprintf(bodyTemplate, sha, parametersStr, u.String())
+		body := fmt.Sprintf(bodyTemplate, p.sha, p.parameters(), p.teamcityURL())
 		return &github.IssueComment{Body: &body}
 	}
 
-	suites, err := lib.ParseGotest(input, "")
+	assignee, err := getAssignee(ctx, packageName, testName, p.listCommits)
 	if err != nil {
-		return errors.Wrap(err, "failed to parse `go test` output")
-	}
-	posted := false
-	for _, suite := range suites {
-		packageName := suite.Name
-		if packageName == "" {
-			var ok bool
-			packageName, ok = os.LookupEnv(pkgEnv)
-			if !ok {
-				log.Fatalf("package name environment variable %s is not set", pkgEnv)
-			}
-		}
-		for _, test := range suite.Tests {
-			switch test.Status {
-			case lib.Failed:
-				assignee, err := getAssignee(ctx, packageName, test.Name, listCommits)
-				if err != nil {
-					// if we *can't* assign anyone, sigh, feel free to hard-code me.
-					// -- tschottdorf, 11/3/2017
-					assignee = "tschottdorf"
-					test.Message += fmt.Sprintf("\n\nFailed to find issue assignee: \n%s", err)
-				}
-				issueRequest := newIssueRequest(packageName, test.Name, test.Message, assignee)
-				searchQuery := fmt.Sprintf(`"%s" user:%s repo:%s is:open`, *issueRequest.Title, githubUser, githubRepo)
-				for _, label := range issueLabels {
-					searchQuery = searchQuery + fmt.Sprintf(` label:"%s"`, label)
-				}
-
-				var foundIssue *int
-
-				result, _, err := searchIssues(ctx, searchQuery, &github.SearchOptions{
-					ListOptions: github.ListOptions{
-						PerPage: 1,
-					},
-				})
-				if err != nil {
-					return errors.Wrapf(err, "failed to search GitHub with query %s", github.Stringify(searchQuery))
-				}
-				if *result.Total > 0 {
-					foundIssue = result.Issues[0].Number
-				}
-
-				if foundIssue == nil {
-					if _, _, err := createIssue(ctx, githubUser, githubRepo, issueRequest); err != nil {
-						return errors.Wrapf(err, "failed to create GitHub issue %s", github.Stringify(issueRequest))
-					}
-				} else {
-					comment := newIssueComment(packageName, test.Name)
-					if _, _, err := createComment(ctx, githubUser, githubRepo, *foundIssue, comment); err != nil {
-						return errors.Wrapf(err, "failed to update issue #%d with %s", *foundIssue, github.Stringify(comment))
-					}
-				}
-				posted = true
-			}
-		}
+		// if we *can't* assign anyone, sigh, feel free to hard-code me.
+		// -- tschottdorf, 11/3/2017
+		assignee = "tschottdorf"
+		message += fmt.Sprintf("\n\nFailed to find issue assignee: \n%s", err)
 	}
 
-	const unknown = "(unknown)"
+	issueRequest := newIssueRequest(packageName, testName, message, assignee)
+	searchQuery := fmt.Sprintf(`"%s" user:%s repo:%s is:open`,
+		*issueRequest.Title, githubUser, githubRepo)
+	for _, label := range issueLabels {
+		searchQuery = searchQuery + fmt.Sprintf(` label:"%s"`, label)
+	}
 
-	if !posted {
-		packageName, ok := os.LookupEnv(pkgEnv)
-		if !ok {
-			packageName = unknown
+	var foundIssue *int
+	result, _, err := p.searchIssues(ctx, searchQuery, &github.SearchOptions{
+		ListOptions: github.ListOptions{
+			PerPage: 1,
+		},
+	})
+	if err != nil {
+		return errors.Wrapf(err, "failed to search GitHub with query %s",
+			github.Stringify(searchQuery))
+	}
+	if *result.Total > 0 {
+		foundIssue = result.Issues[0].Number
+	}
+
+	if foundIssue == nil {
+		if _, _, err := p.createIssue(ctx, githubUser, githubRepo, issueRequest); err != nil {
+			return errors.Wrapf(err, "failed to create GitHub issue %s",
+				github.Stringify(issueRequest))
 		}
-		issueRequest := newIssueRequest(packageName, unknown, inputBuf.String(), "")
-		if _, _, err := createIssue(ctx, githubUser, githubRepo, issueRequest); err != nil {
-			return errors.Wrapf(err, "failed to create GitHub issue %s", github.Stringify(issueRequest))
+	} else {
+		comment := newIssueComment(packageName, testName)
+		if _, _, err := p.createComment(
+			ctx, githubUser, githubRepo, *foundIssue, comment); err != nil {
+			return errors.Wrapf(err, "failed to update issue #%d with %s",
+				*foundIssue, github.Stringify(comment))
 		}
 	}
 
 	return nil
+}
+
+func (p *poster) teamcityURL() *url.URL {
+	options := url.Values{}
+	options.Add("buildId", p.buildID)
+	options.Add("tab", "buildLog")
+
+	u, err := url.Parse(p.serverURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	u.Scheme = "https"
+	u.Path = "viewLog.html"
+	u.RawQuery = options.Encode()
+	return u
+}
+
+func (p *poster) parameters() string {
+	var parameters []string
+	for _, parameter := range []string{
+		tagsEnv,
+		goFlagsEnv,
+	} {
+		if val, ok := os.LookupEnv(parameter); ok {
+			parameters = append(parameters, parameter+"="+val)
+		}
+	}
+	return strings.Join(parameters, "\n")
+}
+
+var defaultP struct {
+	sync.Once
+	*poster
+}
+
+// Post either creates a new issue for a failed test, or posts a comment to an
+// existing open issue.
+func Post(ctx context.Context, detail, packageName, testName, message string) error {
+	defaultP.Do(func() {
+		defaultP.poster = newPoster()
+		defaultP.init()
+	})
+	return defaultP.post(ctx, detail, packageName, testName, message)
 }
