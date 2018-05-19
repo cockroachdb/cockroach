17700ca48d006d6d15c3bf523e32e530f5782aa1
Andrew Kimball <andyk@cockroachlabs.com>
2018-05-19 08:21:04 -0700

    sql: Split SqlLite tests from main TestLogic tests
    During our nightly runs, we pass the -bigtest flag to the main
    LogicTest. This causes it to run a completely different set of
    tests, pulled from a fork of:
    
      https://github.com/cockroachdb/sqllogictest
    
    This commit segregates those tests into a new top level go test,
    called TestSqlLiteLogic. The -bigtest flag is still required in
    order to run the test, since it takes too long to run by default.
    
    This commit prepares the way for separating out other aspects of
    the monolithic TestLogic test, such as variations that are intended
    to test the 2.0 heuristic planner. Eventually, we will get to a
    point where TestLogic only tests the correctness of relational
    output, rather than the specifics of how that output was derived.
    This will allow the 2.0 heuristic planner and the 2.1 cost-based
    optimizer to share these tests.
    
    Other aspects of our testing that cannot be easily shared can go
    into other top-level tests.
    
    Release note: None

--- build/teamcity-sqllogictest.sh
+++ build/teamcity-sqllogictest.sh
@@ -3,15 +3,15 @@ set -euxo pipefail
 
 mkdir -p artifacts
 
-# make bigtest needs the sqllogictest repo from the host's GOPATH, so we can't
-# hide it like we do in the other teamcity build scripts.
+# TestSqlLiteLogic needs the sqllogictest repo from the host's GOPATH, so we
+# can't hide it like we do in the other teamcity build scripts.
 # TODO(jordan) improve builder.sh to allow partial GOPATH hiding rather than
 # the all-on/all-off strategy BULIDER_HIDE_GOPATH_SRC gives us.
 export BUILDER_HIDE_GOPATH_SRC=0
 
 for config in default distsql distsql-disk; do
     build/builder.sh env \
-        make test TESTFLAGS="-v -bigtest -config ${config}" TESTTIMEOUT='24h' PKG='./pkg/sql/logictest' TESTS='^TestLogic$$' 2>&1 \
+        make test TESTFLAGS="-v -bigtest -config ${config}" TESTTIMEOUT='24h' PKG='./pkg/sql/logictest' TESTS='^TestSqlLiteLogic$$' 2>&1 \
         | tee "artifacts/${config}.log" \
         | go-test-teamcity
 done
