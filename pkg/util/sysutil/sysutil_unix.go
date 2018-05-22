8e218cb8c1af0eb89e1fc07b591031c7736d09c3
Nikhil Benesch <nikhil.benesch@gmail.com>
2018-05-22 16:42:32 +0800

    sysutil: improve interface of Statfs
    Unlike the syscall package, we don't need to closely mirror the OS's C
    API, and should, in fact, strive to provide a nicer, Go-style API.
    Accordingly, adjust Statfs to use multiple return values instead of an
    out pointers. Also rename it to StatFS to better match Go's naming
    style, and rename its return type to FSInfo, which is analogous to
    os.Stat's return type of FileInfo.
    
    Additionally, provide a stub implementation of StatFS for Windows to
    unbreak the Windows build.
    
    Release note: None

--- pkg/util/sysutil/sysutil_unix.go
+++ pkg/util/sysutil/sysutil_unix.go
@@ -14,10 +14,13 @@
 
 // +build !windows
 
+//lint:file-ignore Unconvert (redundant conversions are necessary for cross-platform compatibility)
+
 package sysutil
 
 import (
 	"fmt"
+	"math"
 
 	"golang.org/x/sys/unix"
 )
@@ -27,4 +30,28 @@ import (
 func ProcessIdentity() string {
 	return fmt.Sprintf("uid %d euid %d gid %d egid %d",
 		unix.Getuid(), unix.Geteuid(), unix.Getgid(), unix.Getegid())
+}
+
+// StatFS returns an FSInfo describing the named filesystem. It is only
+// supported on Unix-like platforms.
+func StatFS(path string) (*FSInfo, error) {
+	var fs unix.Statfs_t
+	if err := unix.Statfs(path, &fs); err != nil {
+		return nil, err
+	}
+	// Statfs_t's fields have different types on different platforms. Our FSInfo
+	// type uses int64s for all fields, so make sure the values returned by the OS
+	// will fit.
+	if uint64(fs.Bfree) > math.MaxInt64 ||
+		uint64(fs.Bavail) > math.MaxInt64 ||
+		uint64(fs.Blocks) > math.MaxInt64 ||
+		uint64(fs.Bsize) > math.MaxInt64 {
+		return nil, fmt.Errorf("statfs syscall returned unrepresentable value %#v", fs)
+	}
+	return &FSInfo{
+		FreeBlocks:  int64(fs.Bfree),
+		AvailBlocks: int64(fs.Bavail),
+		TotalBlocks: int64(fs.Blocks),
+		BlockSize:   int64(fs.Bsize),
+	}, nil
 }
