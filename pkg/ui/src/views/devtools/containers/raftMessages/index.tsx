cockroachdb/cockroach

commit ea53221b5e390c903271ab88ef37b9291d275e72

ui: upgrade tslint
Unfortunately a few rules triggered that weren't before, so this also
includes a handful of fixes for those.

Release note: None

pkg/ui/src/views/devtools/containers/raftMessages/index.tsx
@@ -6,7 +6,7 @@ import { InjectedRouter, RouterState } from "react-router";
 import { createSelector } from "reselect";
 
 import { refreshNodes, refreshLiveness } from "src/redux/apiReducers";
-import { hoverStateSelector, HoverState, hoverOn, hoverOff } from "src/redux/hover";
+import { hoverStateSelector, HoverState, hoverOn as hoverOnAction, hoverOff as hoverOffAction } from "src/redux/hover";
 import { nodesSummarySelector, NodesSummary } from "src/redux/nodes";
 import { AdminUIState } from "src/redux/state";
 import { nodeIDAttr } from "src/util/constants";
@@ -24,8 +24,8 @@ import messagesDashboard from "./messages";
 interface NodeGraphsOwnProps {
   refreshNodes: typeof refreshNodes;
   refreshLiveness: typeof refreshLiveness;
-  hoverOn: typeof hoverOn;
-  hoverOff: typeof hoverOff;
+  hoverOn: typeof hoverOnAction;
+  hoverOff: typeof hoverOffAction;
   nodesQueryValid: boolean;
   livenessQueryValid: boolean;
   nodesSummary: NodesSummary;
@@ -176,7 +176,7 @@ function mapStateToProps(state: AdminUIState) {
 const actions = {
   refreshNodes,
   refreshLiveness,
-  hoverOn,
-  hoverOff,
+  hoverOnAction,
+  hoverOffAction,
 };
 export default connect(mapStateToProps, actions)(NodeGraphs);

pkg/ui/src/views/shared/containers/metricDataProvider/index.tsx
@@ -7,7 +7,7 @@ import moment from "moment";
 
 import * as protos from  "src/js/protos";
 import { AdminUIState } from "src/redux/state";
-import { requestMetrics, MetricsQuery } from "src/redux/metrics";
+import { requestMetrics as requestMetricsAction, MetricsQuery } from "src/redux/metrics";
 import {
   Metric, MetricProps, MetricsDataComponentProps, QueryTimeInfo,
 } from "src/views/shared/components/metricQuery";
@@ -69,7 +69,7 @@ function queryFromProps(
 interface MetricsDataProviderConnectProps {
   metrics: MetricsQuery;
   timeInfo: QueryTimeInfo;
-  requestMetrics: typeof requestMetrics;
+  requestMetrics: typeof requestMetricsAction;
 }
 
 /**
@@ -223,7 +223,7 @@ const metricsDataProviderConnected = connect(
     };
   },
   {
-    requestMetrics,
+    requestMetricsAction,
   },
 )(MetricsDataProvider);
 

pkg/ui/src/views/cluster/components/linegraph/index.tsx
@@ -181,10 +181,10 @@ export class LineGraph extends React.Component<LineGraphProps, {}> {
   }
 
   render() {
-    const { title, subtitle, tooltip, data, hoverOn} = this.props;
+    const { title, subtitle, tooltip, data } = this.props;
 
     let hoverProps: Partial<React.SVGProps<SVGSVGElement>> = {};
-    if (hoverOn) {
+    if (this.props.hoverOn) {
       hoverProps = {
         onMouseMove: this.mouseMove,
         onMouseLeave: this.mouseLeave,

pkg/ui/src/views/cluster/containers/nodeGraphs/index.tsx
@@ -143,7 +143,7 @@ class NodeGraphs extends React.Component<NodeGraphsProps, {}> {
   }
 
   render() {
-    const { params, nodesSummary, hoverState, hoverOn, hoverOff } = this.props;
+    const { params, nodesSummary, hoverState } = this.props;
     const selectedDashboard = params[dashboardNameAttr];
     const dashboard = _.has(dashboards, selectedDashboard)
       ? selectedDashboard
@@ -179,6 +179,12 @@ class NodeGraphs extends React.Component<NodeGraphsProps, {}> {
       tooltipSelection,
     };
 
+    const forwardParams = {
+      hoverOn: this.props.hoverOn,
+      hoverOff: this.props.hoverOff,
+      hoverState: this.props.hoverState,
+    };
+
     // Generate graphs for the current dashboard, wrapping each one in a
     // MetricsDataProvider with a unique key.
     const graphs = dashboards[dashboard].component(dashboardProps);
@@ -187,7 +193,7 @@ class NodeGraphs extends React.Component<NodeGraphsProps, {}> {
       return (
         <div key={key}>
           <MetricsDataProvider id={key}>
-            { React.cloneElement(graph, { hoverOn, hoverOff, hoverState }) }
+            { React.cloneElement(graph, forwardParams) }
           </MetricsDataProvider>
         </div>
       );

pkg/ui/src/views/databases/containers/databaseGrants/index.tsx
@@ -14,7 +14,7 @@ import {
 } from "src/redux/apiReducers";
 
 import {
-    DatabaseSummaryBase, DatabaseSummaryExplicitData, databaseDetails, tableInfos, grants,
+    DatabaseSummaryBase, DatabaseSummaryExplicitData, databaseDetails, tableInfos, grants as selectGrants,
 } from "src/views/databases/containers/databaseSummary";
 
 class DatabaseGrantsSortedTable extends SortedTable<protos.cockroach.server.serverpb.DatabaseDetailsResponse.Grant> {}
@@ -86,7 +86,7 @@ export default connect(
       tableInfos: tableInfos(state, ownProps.name),
       sortSetting: grantsSortSetting.selector(state),
       dbResponse: databaseDetails(state)[ownProps.name] && databaseDetails(state)[ownProps.name].data,
-      grants: grants(state, ownProps.name),
+      grants: selectrants(state, ownProps.name),
     };
   },
   {

pkg/ui/src/views/databases/containers/databaseTables/index.tsx
@@ -18,7 +18,7 @@ import { Bytes } from "src/util/format";
 import { TableInfo } from "src/views/databases/data/tableInfo";
 
 import {
-    DatabaseSummaryBase, DatabaseSummaryExplicitData, databaseDetails, tableInfos, grants,
+    DatabaseSummaryBase, DatabaseSummaryExplicitData, databaseDetails, tableInfos as selectTableInfos, grants,
 } from "src/views/databases/containers/databaseSummary";
 
 const databaseTablesSortSetting = new LocalSetting<AdminUIState, SortSetting>(
@@ -124,7 +124,7 @@ class DatabaseSummaryTables extends DatabaseSummaryBase {
 export default connect(
   (state: AdminUIState, ownProps: DatabaseSummaryExplicitData) => {
     return {
-      tableInfos: tableInfos(state, ownProps.name),
+      tableInfos: selectTableInfos(state, ownProps.name),
       sortSetting: databaseTablesSortSetting.selector(state),
       dbResponse: databaseDetails(state)[ownProps.name] && databaseDetails(state)[ownProps.name].data,
       grants: grants(state, ownProps.name),

pkg/ui/src/views/databases/containers/tableDetails/index.tsx
@@ -143,7 +143,7 @@ class TableMain extends React.Component<TableMainProps, {}> {
  *         SELECTORS
  */
 
-function tableInfo(state: AdminUIState, props: RouterState): TableInfo {
+function selectTableInfo(state: AdminUIState, props: RouterState): TableInfo {
   const db = props.params[databaseNameAttr];
   const table = props.params[tableNameAttr];
   const details = state.cachedData.tableDetails[generateTableID(db, table)];
@@ -155,7 +155,7 @@ function tableInfo(state: AdminUIState, props: RouterState): TableInfo {
 const tableMainConnected = connect(
   (state: AdminUIState, ownProps: RouterState) => {
     return {
-      tableInfo: tableInfo(state, ownProps),
+      tableInfo: selectTableInfo(state, ownProps),
       grantsSortSetting: databaseTableGrantsSortSetting.selector(state),
     };
   },

pkg/ui/src/views/devtools/containers/raftRanges/index.tsx
@@ -271,13 +271,13 @@ class RangesMain extends React.Component<RangesMainProps, RangesMainState> {
  */
 
 // Base selectors to extract data from redux state.
-const raftState = (state: AdminUIState): CachedDataReducerState<protos.cockroach.server.serverpb.RaftDebugResponse> => state.cachedData.raft;
+const selectRaftState = (state: AdminUIState): CachedDataReducerState<protos.cockroach.server.serverpb.RaftDebugResponse> => state.cachedData.raft;
 
 // Connect the RangesMain class with our redux store.
 const rangesMainConnected = connect(
   (state: AdminUIState) => {
     return {
-      state: raftState(state),
+      state: selectRaftState(state),
     };
   },
   {

commit 4232d7f289a8b8bddf688bc1adc90212e3a33f7d

ui: fix action names broken by #25823
In that PR, I mechanically replaced identifiers when there were new
warnings about shadowing, but in two places I accidentally changed
semantics due to the shorthand form of object literals.  :man_facepalming:

Release note: None

pkg/ui/src/views/devtools/containers/raftMessages/index.tsx
@@ -176,7 +176,7 @@ function mapStateToProps(state: AdminUIState) {
 const actions = {
   refreshNodes,
   refreshLiveness,
-  hoverOnAction,
-  hoverOffAction,
+  hoverOn: hoverOnAction,
+  hoverOff: hoverOffAction,
 };
 export default connect(mapStateToProps, actions)(NodeGraphs);

pkg/ui/src/views/shared/containers/metricDataProvider/index.tsx
@@ -223,7 +223,7 @@ const metricsDataProviderConnected = connect(
     };
   },
   {
-    requestMetricsAction,
+    requestMetrics: requestMetricsAction,
   },
 )(MetricsDataProvider);
 