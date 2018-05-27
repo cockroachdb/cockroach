8482aef2c724a0fdc5b209e5258a3ff0ac6e1c0b
Andrew Kimball <andyk@cockroachlabs.com>
2018-05-27 01:07:03 -0700

    opt: Mark features as unsupported and match 2.0 error messages
    Mark several features as unsupported so that we can fall back
    to the heuristic planner when the optbuilder encounters them.
    Also update several error messages that make it easier for the
    new semantic analyzer to raise the same error messages as the
    2.0 semantic analyzer.
    
    Release note: None

--- pkg/sql/sem/tree/type_check.go
+++ pkg/sql/sem/tree/type_check.go
@@ -753,7 +753,8 @@ func (expr *ColumnItem) TypeCheck(_ *SemaContext, desired types.T) (TypedExpr, e
 	if _, ok := presetTypesForTesting[name]; ok {
 		return expr, nil
 	}
-	return nil, pgerror.NewErrorf(pgerror.CodeInternalError, "name \"%s\" is not defined", name)
+	return nil, pgerror.NewErrorf(pgerror.CodeUndefinedColumnError,
+		"column name %q not found", ErrString(expr))
 }
 
 // TypeCheck implements the Expr interface.
