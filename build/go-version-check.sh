52ee15c4b2be8fc3c03f75ef5c70a961d5a85c9e
Nikhil Benesch <nikhil.benesch@gmail.com>
2018-05-29 18:48:08 -0400

    build: improve Make startup time
    Cache shell invocations to avoid spending 300ms on every Make invocation
    forking various shell utilities.
    
    Release note: None

--- build/go-version-check.sh
+++ build/go-version-check.sh
@@ -3,31 +3,23 @@
 # Detect whether the installed version of Go can build this version of
 # CockroachDB.
 #
-# To bump the required version of Go, edit the last conditional appropriately.
-#
-# Note: this script is invoked by Make, so it has odd error reporting semantics.
-# Errors are printed to stdout; the Go version is compatible iff nothing is
-# printed to stdout. The exit code is meaningless.
-
-# Ensure stray error messages are printed to stdout, or they'll go unnoticed by
-# Make.
-exec 2>&1
+# To bump the required version of Go, edit the last conditional appropriately.s
 
 go=${1-go}
 
 if ! raw_version=$("$go" version 2>&1); then
-  echo "unable to detect go version: $raw_version"
-  exit
+  echo "unable to detect go version: $raw_version" >&2
+  exit 1
 fi
 
 if ! version=$(grep -oE "[0-9]+\.[0-9]+" <<< "$raw_version" | head -n1); then
-  echo "unable to parse go version '$raw_version'"
-  exit
+  echo "unable to parse go version '$raw_version'" >&2
+  exit 1
 fi
 
 version_major=$(cut -f1 -d. <<< "$version")
 version_minor=$(cut -f2 -d. <<< "$version")
 if (( version_major != 1 )) || (( version_minor < 10 )); then
-  echo "go1.10+ required (detected go$version)"
-  exit
+  echo "go1.10+ required (detected go$version)" >&2
+  exit 1
 fi
