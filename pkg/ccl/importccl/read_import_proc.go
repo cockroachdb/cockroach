a170ff531a5482a921694f25577a360c14be771b
Matt Jibson <matt.jibson@gmail.com>
2018-05-30 04:15:20 -0400

    importccl: fix edge cases in mysqloutfile reading
    Previously, the MYSQLOUTFILE file reader would convert its data to
    CSV and use the CSV workers to convert it to datums. This worked for
    most data, but was not able to correctly handle all NULL encodings.
    
    To fix this, decouple MYSQLOUTFILE from CSV so that it can parse
    datums in its own way and thus handle NULLs correctly. This does lose
    parallelization, but we can come up with ways to do that in the future.
    
    As a side effect, this fixes the listed bug, which occurred because
    the csvRecord field wasn't being correctly initialized since readFile
    was shadowing the CSV readFile.
    
    Fixes #26153
    
    Release note (bug fix): correct NULL handling during IMPORT of
    MYSQLOUTFILE.

--- pkg/ccl/importccl/read_import_proc.go
+++ pkg/ccl/importccl/read_import_proc.go
@@ -382,7 +382,12 @@ func (cp *readImportDataProcessor) Run(ctx context.Context, wg *sync.WaitGroup) 
 	case roachpb.IOFileFormat_CSV:
 		conv = newCSVInputReader(kvCh, cp.inputFromat.Csv, &cp.tableDesc, evalCtx)
 	case roachpb.IOFileFormat_MysqlOutfile:
-		conv = newMysqloutfileReader(kvCh, cp.inputFromat.MysqlOut, &cp.tableDesc, evalCtx)
+		c, err := newMysqloutfileReader(kvCh, cp.inputFromat.MysqlOut, &cp.tableDesc, evalCtx)
+		if err != nil {
+			distsqlrun.DrainAndClose(ctx, cp.output, err, func(context.Context) {} /* pushTrailingMeta */)
+			return
+		}
+		conv = c
 	case roachpb.IOFileFormat_Mysqldump:
 		c, err := newMysqldumpReader(kvCh, &cp.tableDesc, evalCtx)
 		if err != nil {
