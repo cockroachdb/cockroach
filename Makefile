cockroachdb/cockroach

commit 7d58905e0ae4b7052c09d6412648d904e3171140

build: avoid busting test binary cache on every rebuild
Injecting the current time into test binaries busts Go's cache.

Release note: None

Makefile
@@ -784,16 +784,20 @@ $(go-targets-ccl): $(C_LIBS_CCL)
 BUILDINFO = .buildinfo/tag .buildinfo/rev
 BUILD_TAGGED_RELEASE =
 
-# The build.utcTime format must remain in sync with TimeFormat in pkg/build/info.go.
 $(go-targets): bin/.bootstrap $(BUILDINFO) $(CGO_FLAGS_FILES) $(PROTOBUF_TARGETS)
 $(go-targets): $(SQLPARSER_TARGETS) $(OPTGEN_TARGETS)
 $(go-targets): override LINKFLAGS += \
 	-X "github.com/cockroachdb/cockroach/pkg/build.tag=$(shell cat .buildinfo/tag)" \
-	-X "github.com/cockroachdb/cockroach/pkg/build.utcTime=$(shell date -u '+%Y/%m/%d %H:%M:%S')" \
 	-X "github.com/cockroachdb/cockroach/pkg/build.rev=$(shell cat .buildinfo/rev)" \
 	-X "github.com/cockroachdb/cockroach/pkg/build.cgoTargetTriple=$(TARGET_TRIPLE)" \
 	$(if $(BUILDCHANNEL),-X "github.com/cockroachdb/cockroach/pkg/build.channel=$(BUILDCHANNEL)") \
 	$(if $(BUILD_TAGGED_RELEASE),-X "github.com/cockroachdb/cockroach/pkg/util/log.crashReportEnv=$(shell cat .buildinfo/tag)")
+
+# The build.utcTime format must remain in sync with TimeFormat in
+# pkg/build/info.go. It is not installed in tests to avoid busting the cache on
+# every rebuild.
+$(COCKROACH) build buildoss buildshort go-install: override LINKFLAGS += \
+	-X "github.com/cockroachdb/cockroach/pkg/build.utcTime=$(shell date -u '+%Y/%m/%d %H:%M:%S')"
 
 SETTINGS_DOC_PAGE := docs/generated/settings/settings.html
 

commit 16dbfa7ed8def01d9776d20cb72d72c9b0afb129

Makefile: compute GOEXE with XGO, not GO
To account for cross-compilation, we need to compute GOEXE with XGO, not
GO. Broken by #26187.

Release note: None

Makefile
@@ -375,33 +375,6 @@ endif
 
 IGNORE_GOVERS :=
 
-# defs.mk stores cached values of shell commands to avoid recomputing them on
-# every Make invocation. This has a small but noticeable effect, especially on
-# noop builds.
-build/defs.mk: Makefile build/defs.mk.sig
-ifndef IGNORE_GOVERS
-	@build/go-version-check.sh $(GO) || { echo "Disable this check with IGNORE_GOVERS=1." >&2; exit 1; }
-endif
-	@echo "macos-version = $$(sw_vers -productVersion 2>/dev/null | grep -oE '[0-9]+\.[0-9]+')" > $@
-	@echo "export GOPATH ?= $$($(GO) env GOPATH)" >> $@
-	@echo "GOEXE = $$($(GO) env GOEXE)" >> $@
-	@echo "NCPUS = $$({ getconf _NPROCESSORS_ONLN || sysctl -n hw.ncpu || nproc; } 2>/dev/null)" >> $@
-	@echo "UNAME = $$(uname)" >> $@
-	@echo "HOST_TRIPLE = $$($$($(GO) env CC) -dumpmachine)" >> $@
-	@echo "GIT_DIR = $$(git rev-parse --git-dir 2>/dev/null)" >> $@
-	@echo "GITHOOKSDIR = $$(test -d .git && echo '.git/hooks' || git rev-parse --git-path hooks)" >> $@
-	@echo "have-defs = 1" >> $@
-	$(if $(have-defs),$(info Detected change in build system. Rebooting Make.))
-
-# defs.mk.sig attempts to capture common cases where defs.mk needs to be
-# recomputed, like when compiling for a different platform or using a different
-# Go binary. It is not intended to be perfect. Upgrading the compiler toolchain
-# in place will go unnoticed, for example. Similar problems exist in all Make-
-# based build systems and are not worth solving.
-build/defs.mk.sig: sig = $(PATH):$(CURDIR):$(GO):$(GOPATH):$(CC):$(CXX):$(TYPE):$(IGNORE_GOVERS)
-build/defs.mk.sig: .ALWAYS_REBUILD
-	@echo '$(sig)' | cmp -s - $@ || echo '$(sig)' > $@
-
 # Make doesn't expose a list of the variables declared in a given file, so we
 # resort to sed magic. Roughly, this sed command prints VARIABLE in lines of the
 # following forms:
@@ -736,6 +709,33 @@ override TAGS += make $(NATIVE_SPECIFIER_TAG)
 export LC_COLLATE=C
 
 XGO := $(strip $(if $(XGOOS),GOOS=$(XGOOS)) $(if $(XGOARCH),GOARCH=$(XGOARCH)) $(if $(XHOST_TRIPLE),CC=$(CC_PATH) CXX=$(CXX_PATH)) $(GO))
+
+# defs.mk stores cached values of shell commands to avoid recomputing them on
+# every Make invocation. This has a small but noticeable effect, especially on
+# noop builds.
+build/defs.mk: Makefile build/defs.mk.sig
+ifndef IGNORE_GOVERS
+	@build/go-version-check.sh $(GO) || { echo "Disable this check with IGNORE_GOVERS=1." >&2; exit 1; }
+endif
+	@echo "macos-version = $$(sw_vers -productVersion 2>/dev/null | grep -oE '[0-9]+\.[0-9]+')" > $@
+	@echo "export GOPATH ?= $$($(GO) env GOPATH)" >> $@
+	@echo "GOEXE = $$($(XGO) env GOEXE)" >> $@
+	@echo "NCPUS = $$({ getconf _NPROCESSORS_ONLN || sysctl -n hw.ncpu || nproc; } 2>/dev/null)" >> $@
+	@echo "UNAME = $$(uname)" >> $@
+	@echo "HOST_TRIPLE = $$($$($(GO) env CC) -dumpmachine)" >> $@
+	@echo "GIT_DIR = $$(git rev-parse --git-dir 2>/dev/null)" >> $@
+	@echo "GITHOOKSDIR = $$(test -d .git && echo '.git/hooks' || git rev-parse --git-path hooks)" >> $@
+	@echo "have-defs = 1" >> $@
+	$(if $(have-defs),$(info Detected change in build system. Rebooting Make.))
+
+# defs.mk.sig attempts to capture common cases where defs.mk needs to be
+# recomputed, like when compiling for a different platform or using a different
+# Go binary. It is not intended to be perfect. Upgrading the compiler toolchain
+# in place will go unnoticed, for example. Similar problems exist in all Make-
+# based build systems and are not worth solving.
+build/defs.mk.sig: sig = $(PATH):$(CURDIR):$(XGO):$(GOPATH):$(CC):$(CXX):$(TYPE):$(IGNORE_GOVERS)
+build/defs.mk.sig: .ALWAYS_REBUILD
+	@echo '$(sig)' | cmp -s - $@ || echo '$(sig)' > $@
 
 COCKROACH := ./cockroach$(SUFFIX)$(GOEXE)
 