ba05917966e5d721ef54c94e0d0065535341de9f
Emmanuel <absolutezero2a03@gmail.com>
2018-06-06 13:21:42 -0400

    fix linting errors

--- pkg/sql/tablewriter_upsert_strict.go
+++ pkg/sql/tablewriter_upsert_strict.go
@@ -1,3 +1,17 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
 package sql
 
 import (
@@ -41,7 +55,10 @@ func (tu *strictTableUpserter) init(txn *client.Txn, evalCtx *tree.EvalContext) 
 	tu.twb.init(txn)
 	tableDesc := tu.tableDesc()
 
-	tu.getUniqueIndexes()
+	err := tu.getUniqueIndexes()
+	if err != nil {
+		return err
+	}
 
 	tu.resultCount = 0
 
@@ -157,10 +174,9 @@ func (tu *strictTableUpserter) getUniqueIndexes() (err error) {
 //
 // The return value is a set of all the rows (by their place in tu.InsertRows)
 // that are in conflict with either an existing row in the table or another insert row.
-func (tu *strictTableUpserter) getConflictingRows(ctx context.Context, _ bool) (
-	conflictingRows map[int]struct{},
-	err error,
-) {
+func (tu *strictTableUpserter) getConflictingRows(
+	ctx context.Context, _ bool,
+) (conflictingRows map[int]struct{}, err error) {
 	tableDesc := tu.tableDesc()
 
 	conflictingRows = make(map[int]struct{})
