566c3b52fe802e47a0e98dbec2e90f76ed4c6ea4
Alex Robinson <alexdwanerobinson@gmail.com>
2018-06-11 15:40:15 -0500

    [prototype] storage: Make rebalance decisions at store-level
    As outlined in recent comments on #26059, we need to bring back some
    form of stats-based rebalancing in order to perform well on TPC-C
    without manual partitioning and replica placement.
    
    This commit contains a prototype that demonstrates the effectiveness of
    changing our approach to making rebalancing decisions from making them
    in the replicate queue, which operates on arbitrarily ordered replicas
    of the ranges on a store, to making them at a higher-level. This
    prototype makes them at a cluster level by running the logic on only one
    node, but my real proposal is to make them at the store level.
    
    This change in abstraction reflects what a human would do if asked to
    even out the load on a cluster given perfect information about
    everything happening in the cluster:
    
    1. First, determine which stores have the most load on them (or overfull
       -- but for the prototype I only considered the one dimension that
       affects TPC-C the most)
    2. Decide whether the most loaded stores are so overloaded that action
       needs to be taken.
    3. Examine the hottest replicas on the store (maybe not the absolute
       hottest in practice, since moving that one could disrupt user traffic,
       but in the prototype this seems to work fine) and attempt to move them
       to under-utilized stores.  If this can be done simply by transferring
       leases to under-utilized stores, then do so. If moving leases isn't
       enough, then also rebalance replicas from the hottest store to
       under-utilized stores.
    4. Repeat periodically to handle changes in load or cluster membership.
    
    In a real versino of this code, the plan is roughly:
    1. Each store will independently run their own control loop like this
       that is only responsible for moving leases/replicas off itself, not off
       other stores. This avoids needing a centralized coordinator, and will
       avoid the need to use the raft debug endpoint as long as we start
       gossiping QPS per store info, since the store already has details about
       the replicas on itself.
    2. The existing replicate queue will stop making decisions motivated by
       balance. It will switch to only making decisions based on
       constraints/diversity/lease preferences, which is still needed since
       the new store-level logic will only check for store-level balance,
       not that all replicas' constraints are properly met.
    3. The new code will have to avoid violating constraints/diversity/lease
       preferences.
    4. The new code should consider range count, disk fullness, and maybe
       writes per second as well.
    5. In order to avoid making decisions based on bad data, I'd like to
       extend lease transfers to pass along QPS data to the new leaseholder
       and preemptive snapshots to pass along WPS data to the new replica.
       This may not be strictly necessary, as shown by the success of this
       prototype, but should make for more reliable decision making.
    
    I tested this out on TPC-C 5k on 15 nodes and am able to consistently
    get 94% efficiency, which is the max I've seen using a build of the
    workload generator that erroneously includes the ramp-up period in its
    final stats. The first run with this code only got 85% because it took a
    couple minutes to make all the lease transfers it wanted, but then all
    subsequent runs got the peak efficiency while making negligibly few
    lease transfers.
    
    Note that I didn't even have to implement replica rebalancing to get
    these results, which oddly contradicts my previous claims. However, I
    believe that's because I did the initial split/scatter using a binary
    containing #26438, so the replicas were already better scattered than by
    default. I ran TPC-C on that build without these changes a couple times,
    though, and didn't get better than 65% efficiency, so the scatter wasn't
    the cause of the good results here.
    
    Touches #26059, #17979
    
    Release note: None

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1647,6 +1647,8 @@ If problems persist, please see ` + base.DocsURL("cluster-setup-troubleshooting.
 		}
 	}
 
+	s.stopper.RunWorker(ctx, s.RunStoreLevelAllocator)
+
 	log.Event(ctx, "server ready")
 
 	return nil
