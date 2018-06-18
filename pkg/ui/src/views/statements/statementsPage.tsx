f37e24ade432dab43a52438540df11be08af1ea9
Andrew Couch <hi@andrewcou.ch>
2018-06-18 16:59:39 -0400

    ui: add app to statements page
    Release note (admin ui change): Add app name to the Statements page
    and Statement Details page.

--- pkg/ui/src/views/statements/statementsPage.tsx
+++ pkg/ui/src/views/statements/statementsPage.tsx
@@ -71,6 +71,11 @@ function makeStatementsColumns(statements: CollectedStatementStatistics$Properti
 
   return [
     {
+      title: "App",
+      cell: (query) => query.key.app,
+      sort: (query) => query.key.app,
+    },
+    {
       title: "Statement",
       className: "statements-table__col-query-text",
       cell: (query) => <StatementLink statement={ query.key.query } />,
@@ -105,7 +110,7 @@ class StatementsPage extends React.Component<StatementsPageProps, StatementsPage
     super(props);
     this.state = {
       sortSetting: {
-        sortKey: 1,
+        sortKey: 2,
         ascending: false,
       },
     };
