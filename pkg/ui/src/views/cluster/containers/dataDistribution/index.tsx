372192ab349041330346fa8ffb990d710b34a545
Pete Vilter <vilterp@cockroachlabs.com>
2018-06-21 04:23:16 -0400

    ui: remove width restriction on repl matrix page
    TODO: factor out into styl file
    
    Release note: None

--- pkg/ui/src/views/cluster/containers/dataDistribution/index.tsx
+++ pkg/ui/src/views/cluster/containers/dataDistribution/index.tsx
@@ -142,7 +142,7 @@ class DataDistributionPage extends React.Component<DataDistributionPageProps> {
         <section className="section">
           <h1>Data Distribution</h1>
         </section>
-        <section className="section">
+        <section style={{ paddingTop: 10, paddingLeft: 30, display: "block" }}>
           <Loading
             className="loading-image loading-image__spinner-left"
             loading={!this.props.dataDistribution || !this.props.localityTree}
