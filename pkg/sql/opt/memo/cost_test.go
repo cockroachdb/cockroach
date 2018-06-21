b6558e2a2fcd89b0680e828df03f97a366625fb1
Radu Berinde <radu@cockroachlabs.com>
2018-06-21 10:59:33 -0400

    opt: improve the ratchetCost epsilon test
    The epsilon tolerance in ratchetCost doesn't work well when the values
    are very small (and we see very small costs when we have constraints
    on many columns). The error needs to be relative to the magnitude of
    the numbers.
    
    This change improves this by using the bit representation of the
    floats to constrain how many "units of least precision" there can be
    between the two values.
    
    This issue doesn't currently affect many existing tests; but it is
    more problematic with some upcoming changes in costing.
    
    Release note: None

--- pkg/sql/opt/memo/cost_test.go
+++ pkg/sql/opt/memo/cost_test.go
@@ -21,15 +21,26 @@ import (
 )
 
 func TestCostLess(t *testing.T) {
-	testLess := func(left, right memo.Cost, expected bool) {
-		if left.Less(right) != expected {
-			t.Errorf("expected %v.Less(%v) to be %v, got %v", left, right, expected, !expected)
+	testCases := []struct {
+		left, right memo.Cost
+		expected    bool
+	}{
+		{0.0, 1.0, true},
+		{0.0, 1e-20, true},
+		{0.0, 0.0, false},
+		{1.0, 0.0, false},
+		{1e-20, 1.0000000000001e-20, false},
+		{1e-20, 1.000001e-20, true},
+		{1, 1.00000000000001, false},
+		{1, 1.00000001, true},
+		{1000, 1000.00000000001, false},
+		{1000, 1000.00001, true},
+	}
+	for _, tc := range testCases {
+		if tc.left.Less(tc.right) != tc.expected {
+			t.Errorf("expected %v.Less(%v) to be %v", tc.left, tc.right, tc.expected)
 		}
 	}
-
-	testLess(memo.Cost(0.0), memo.Cost(1.0), true)
-	testLess(memo.Cost(0.0), memo.Cost(0.0), false)
-	testLess(memo.Cost(1.0), memo.Cost(0.0), false)
 }
 
 func TestCostSub(t *testing.T) {
