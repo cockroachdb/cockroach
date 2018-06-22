38b33685f3ce71d878b1eb3384bed54980093155
Andrei Matei <andrei@cockroachlabs.com>
2018-06-22 16:09:19 -0400

    storage: add a testing knob for disabling the consistency queue
    Release note: None

--- pkg/storage/store.go
+++ pkg/storage/store.go
@@ -737,6 +737,8 @@ type StoreTestingKnobs struct {
 	DisableTimeSeriesMaintenanceQueue bool
 	// DisableRaftSnapshotQueue disables the raft snapshot queue.
 	DisableRaftSnapshotQueue bool
+	// DisableConsistencyQueue disables the consistency checker.
+	DisableConsistencyQueue bool
 	// DisableScanner disables the replica scanner.
 	DisableScanner bool
 	// DisablePeriodicGossips disables periodic gossiping.
@@ -962,6 +964,9 @@ func NewStore(cfg StoreConfig, eng engine.Engine, nodeDesc *roachpb.NodeDescript
 	}
 	if cfg.TestingKnobs.DisableRaftSnapshotQueue {
 		s.setRaftSnapshotQueueActive(false)
+	}
+	if cfg.TestingKnobs.DisableConsistencyQueue {
+		s.setConsistencyQueueActive(false)
 	}
 	if cfg.TestingKnobs.DisableScanner {
 		s.setScannerActive(false)
@@ -4270,6 +4275,9 @@ func (s *Store) setTimeSeriesMaintenanceQueueActive(active bool) {
 }
 func (s *Store) setRaftSnapshotQueueActive(active bool) {
 	s.raftSnapshotQueue.SetDisabled(!active)
+}
+func (s *Store) setConsistencyQueueActive(active bool) {
+	s.consistencyQueue.SetDisabled(!active)
 }
 func (s *Store) setScannerActive(active bool) {
 	s.scanner.SetDisabled(!active)
