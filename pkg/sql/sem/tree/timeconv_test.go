6a52ebff14259d37e7a59f947875312a9d816929
Andrei Matei <andrei@cockroachlabs.com>
2018-06-25 19:42:03 -0400

    sem/tree: cleanup TestClusterTimestampConversion
    Instead of modifying a txn's timestamp by manually writing in its proto,
    create the txn with the right timestamp from the beginning.
    
    Release note: None

--- pkg/sql/sem/tree/timeconv_test.go
+++ pkg/sql/sem/tree/timeconv_test.go
@@ -15,15 +15,20 @@
 package tree_test
 
 import (
-	"context"
 	"testing"
+	"time"
 
-	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/internal/client"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	_ "github.com/cockroachdb/cockroach/pkg/sql/sem/builtins"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/storage/engine/enginepb"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 )
 
+// Test that EvalContext.GetClusterTimestamp() gets its timestamp from the
+// transaction, and also that the conversion to decimal works properly.
 func TestClusterTimestampConversion(t *testing.T) {
 	testData := []struct {
 		walltime int64
@@ -37,12 +42,34 @@ func TestClusterTimestampConversion(t *testing.T) {
 		{9223372036854775807, 2147483647, "9223372036854775807.2147483647"},
 	}
 
-	ctx := tree.NewTestingEvalContext(cluster.MakeTestingClusterSettings())
-	defer ctx.Mon.Stop(context.Background())
-	ctx.PrepareOnly = true
+	clock := hlc.NewClock(hlc.UnixNano, time.Nanosecond)
+
+	factory := client.TxnSenderFactoryFunc(func(client.TxnType) client.TxnSender {
+		return nil
+	})
+	db := client.NewDB(
+		testutils.MakeAmbientCtx(),
+		factory,
+		clock)
+
 	for _, d := range testData {
 		ts := hlc.Timestamp{WallTime: d.walltime, Logical: d.logical}
-		ctx.Txn.Proto().OrigTimestamp = ts
+		ctx := tree.EvalContext{
+			Txn: client.NewTxnWithProto(
+				db,
+				1, /* gatewayNodeID */
+				client.RootTxn,
+				roachpb.MakeTransaction(
+					"test",
+					nil, // baseKey
+					roachpb.NormalUserPriority,
+					enginepb.SERIALIZABLE,
+					ts,
+					0, /* maxOffsetNs */
+				),
+			),
+		}
+
 		dec := ctx.GetClusterTimestamp()
 		final := dec.Text('f')
 		if final != d.expected {
