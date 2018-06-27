4e654157fc963ca6e05b5c5a0934ca5438982840
neeral <neeral@users.noreply.github.com>
2018-06-27 10:24:44 -0700

    cli: Deprecate --wait=live in `node decommission`
    Refer to issue #26880.
    
    When you try to decommission a node that is down, today one has to use
    `decommission --wait=live`, which does not verify whether the down node
    is part of any ranges, and manually verify that the cluster has
    up-replicated all ranges elsewhere. This is far from ideal, in particular
    since there is no automated way to reliably check this.
    
    `--wait=live` is deprecated and its behaviour replaced by that of
    `--wait=all`. Instead of relying on metrics for replica counts, which
    may be stale, look at authoritive source - range metadata.
    
    Release note (cli change):
    Deprecate `--wait=live` parameter for `node decommission`. `--wait=all`
    is the default behaviour. This ensures CockroachDB checks ranges are on
    the node to be decommissioned are not under-replicated before the node
    is decommissioned.

--- pkg/server/admin.go
+++ pkg/server/admin.go
@@ -19,7 +19,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"math"
 	"sort"
 	"strconv"
 	"strings"
@@ -1298,19 +1297,35 @@ func (s *adminServer) DecommissionStatus(
 	// a lookup table to check whether we care about a given node.
 	replicaCounts := make(map[roachpb.NodeID]int64)
 	for _, nodeID := range nodeIDs {
-		replicaCounts[nodeID] = math.MaxInt64
+		replicaCounts[nodeID] = 0
 	}
-
-	for _, nodeStatus := range ns.Nodes {
-		nodeID := nodeStatus.Desc.NodeID
-		if _, ok := replicaCounts[nodeID]; !ok {
-			continue // not interested in this node
+	const pageSize = 10000
+	if err = s.server.db.Txn(ctx, func(ctx context.Context, txn *client.Txn) error {
+		begin := keys.Meta2Prefix
+		for {
+			rows, err := s.server.db.Scan(ctx, begin, keys.MetaMax, pageSize)
+			if err != nil {
+				return errors.Wrap(err, "scanning meta2 keys")
+			}
+			rangeDesc := roachpb.RangeDescriptor{}
+			for _, row := range rows {
+				if err := row.ValueProto(&rangeDesc); err != nil {
+					return errors.Wrapf(err, "%s: unable to unmarshal range descriptor", row.Key)
+				}
+				for _, r := range rangeDesc.Replicas {
+					if _, ok := replicaCounts[r.NodeID]; ok {
+						replicaCounts[r.NodeID]++
+					}
+				}
+			}
+			if len(rows) < pageSize {
+				return nil
+			}
+			begin = rows[len(rows)-1].Key.Next()
+			time.Sleep(time.Second)
 		}
-		var replicas float64
-		for _, storeStatus := range nodeStatus.StoreStatuses {
-			replicas += storeStatus.Metrics["replicas"]
-		}
-		replicaCounts[nodeID] = int64(replicas)
+	}); err != nil {
+		return nil, err
 	}
 
 	var res serverpb.DecommissionStatusResponse
