a64d4c14aab45557f593a6346227594d682fdda0
Andrei Matei <andrei@cockroachlabs.com>
2018-06-27 12:00:45 -0400

    workload/ycsb: don't run test under testshort
    They take a bunch of seconds.
    
    Release note: None

---
+++ pkg/workload/ycsb/main_test.go
@@ -0,0 +1,17 @@
+// Copyright 2018 the cockroach authors.
+//
+// licensed under the apache license, version 2.0 (the "license");
+// you may not use this file except in compliance with the license.
+// you may obtain a copy of the license at
+//
+//     http://www.apache.org/licenses/license-2.0
+//
+// unless required by applicable law or agreed to in writing, software
+// distributed under the license is distributed on an "as is" basis,
+// without warranties or conditions of any kind, either express or
+// implied. see the license for the specific language governing
+// permissions and limitations under the license.
+
+package ycsb_test
+
+//go:generate ../../util/leaktest/add-leaktest.sh *_test.go
