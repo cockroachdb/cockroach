2f6dc5e53e3e16a9294a7cc76ba7c7b7cb989588
Andrew Kimball <andyk@cockroachlabs.com>
2018-06-28 00:05:49 -0700

    opt: Fix FDs generated for apply joins
    This commit fixes a bug in which incorrect constant dependencies are
    sometimes generated for apply joins. For example:
    
      SELECT *
      FROM abcde
      INNER JOIN LATERAL (SELECT * FROM mnpq WHERE m=a LIMIT 1)
      ON True
    
    A constant dependency for the columns of mnpq is generated, since all
    columns in a relation having zero or one rows are constants. However,
    since the value of the inner relation can change based on the value of
    the outer relation, this constant dependency cannot be propagated to
    the join's logical properties.
    
    Release note: None

--- pkg/sql/opt/memo/logical_props_builder.go
+++ pkg/sql/opt/memo/logical_props_builder.go
@@ -349,7 +349,11 @@ func (b *logicalPropsBuilder) buildJoinProps(ev ExprView) props.Logical {
 		//   2. Apply ON predicate filter on resulting rows.
 		//   3. For outer joins, add non-matching rows, extended with NULL values
 		//      for the null-supplying side of the join.
-		relational.FuncDeps.MakeProduct(&rightProps.FuncDeps)
+		if ev.IsJoinApply() {
+			relational.FuncDeps.MakeApply(&rightProps.FuncDeps)
+		} else {
+			relational.FuncDeps.MakeProduct(&rightProps.FuncDeps)
+		}
 
 		// TODO(andyk): Add ON predicate effects, like null rejection, equivalent
 		//              columns, and constant columns.
