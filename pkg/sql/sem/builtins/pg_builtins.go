23677f9786ded17e0efada2a162d099bcff65e10
Brett Snyder <bsnyder788@gmail.com>
2018-07-03 12:13:13 +0000

    sql: support pg_get_indexdef with 3 arguments Release note (sql change): support pg_get_indexdef with 3 arguments

--- pkg/sql/sem/builtins/pg_builtins.go
+++ pkg/sql/sem/builtins/pg_builtins.go
@@ -163,6 +163,59 @@ var (
 	datEncodingUTF8ShortName = tree.NewDString("UTF8")
 )
 
+// Make a pg_get_indexdef function with the given arguments.
+func makePGGetIndexDef(argTypes tree.ArgTypes) tree.Overload {
+	return tree.Overload{
+		Types:      argTypes,
+		ReturnType: tree.FixedReturnType(types.String),
+		Fn: func(ctx *tree.EvalContext, args tree.Datums) (tree.Datum, error) {
+			colNumber := *tree.NewDInt(0)
+			if len(args) == 3 {
+				colNumber = *args[1].(*tree.DInt)
+			}
+			r, err := ctx.InternalExecutor.QueryRow(
+				ctx.Ctx(), "pg_get_indexdef",
+				ctx.Txn,
+				"SELECT indexdef FROM pg_catalog.pg_indexes WHERE crdb_oid = $1", args[0])
+			if err != nil {
+				return nil, err
+			}
+			// If the index does not exist we return null.
+			if len(r) == 0 {
+				return tree.DNull, nil
+			}
+			// The 1 argument and 3 argument variants are equivalent when column number 0 is passed.
+			if colNumber == 0 {
+				return r[0], nil
+			}
+			// The 3 argument variant for column number other than 0 returns the column name.
+			r, err = ctx.InternalExecutor.QueryRow(
+				ctx.Ctx(), "pg_get_indexdef",
+				ctx.Txn,
+				`SELECT ischema.column_name as pg_get_indexdef 
+		               FROM information_schema.statistics AS ischema 
+											INNER JOIN pg_catalog.pg_indexes AS pgindex 
+													ON ischema.table_schema = pgindex.schemaname 
+													AND ischema.table_name = pgindex.tablename 
+													AND ischema.index_name = pgindex.indexname 
+													AND pgindex.crdb_oid = $1 
+													AND ischema.seq_in_index = $2`, args[0], args[1])
+			if err != nil {
+				return nil, err
+			}
+			// If the column number does not exist in the index we return an empty string.
+			if len(r) == 0 {
+				return tree.NewDString(""), nil
+			}
+			if len(r) > 1 {
+				return nil, pgerror.NewErrorf(pgerror.CodeInternalError, "programming error: pg_get_indexdef query has more than 1 result row: %+v", r)
+			}
+			return r[0], nil
+		},
+		Info: notUsableInfo,
+	}
+}
+
 // Make a pg_get_viewdef function with the given arguments.
 func makePGGetViewDef(argTypes tree.ArgTypes) tree.Overload {
 	return tree.Overload{
@@ -532,29 +585,8 @@ var pgBuiltins = map[string]builtinDefinition{
 	// pg_get_indexdef functions like SHOW CREATE INDEX would if we supported that
 	// statement.
 	"pg_get_indexdef": makeBuiltin(tree.FunctionProperties{DistsqlBlacklist: true},
-		tree.Overload{
-			Types: tree.ArgTypes{
-				{"index_oid", types.Oid},
-			},
-			ReturnType: tree.FixedReturnType(types.String),
-			Fn: func(ctx *tree.EvalContext, args tree.Datums) (tree.Datum, error) {
-				r, err := ctx.InternalExecutor.QueryRow(
-					ctx.Ctx(), "pg_get_indexdef",
-					ctx.Txn,
-					"SELECT indexdef FROM pg_catalog.pg_indexes WHERE crdb_oid=$1", args[0])
-				if err != nil {
-					return nil, err
-				}
-				if len(r) == 0 {
-					return nil, pgerror.NewErrorf(pgerror.CodeInvalidParameterValueError, "unknown index (OID=%s)", args[0])
-				}
-				return r[0], nil
-			},
-			Info: notUsableInfo,
-		},
-		// The other overload for this function, pg_get_indexdef(index_oid,
-		// column_no, pretty_bool), is unimplemented, because it isn't used by
-		// supported ORMs.
+		makePGGetIndexDef(tree.ArgTypes{{"index_oid", types.Oid}}),
+		makePGGetIndexDef(tree.ArgTypes{{"index_oid", types.Oid}, {"column_no", types.Int}, {"pretty_bool", types.Bool}}),
 	),
 
 	// pg_get_viewdef functions like SHOW CREATE VIEW but returns the same format as
