999972a1a374baa90ebd7459e5cceda97e2d73ae
Tamir Duberstein <tamird@gmail.com>
2018-07-09 12:43:50 -0400

    Use `go test -json` instead of `go2xunit`
    Fixes #26168.
    
    Release note: None

--- build/teamcity-stress.sh
+++ build/teamcity-stress.sh
@@ -15,7 +15,7 @@ build/builder.sh go install ./pkg/cmd/github-post
 build/builder.sh env COCKROACH_NIGHTLY_STRESS=true \
 		 make stress \
 		 PKG="$PKG" GOFLAGS="${GOFLAGS:-}" TAGS="${TAGS:-}" \
-		 TESTTIMEOUT=30m TESTFLAGS='-test.v' \
+		 TESTTIMEOUT=30m TESTFLAGS='-json' \
 		 STRESSFLAGS='-maxtime 15m -maxfails 1 -stderr' \
 		 2>&1 \
     | tee artifacts/stress.log \
