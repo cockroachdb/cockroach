0326e878e0122f22670a01abfbb444e75016edc1
Andrew Couch <andrew@cockroachlabs.com>
2018-07-10 15:35:08 -0400

    ui: remove unused constants
    Release note: None

--- pkg/ui/src/util/constants.ts
+++ pkg/ui/src/util/constants.ts
@@ -1,9 +1,7 @@
 export const appAttr = "app";
-export const dashQueryString = "dash";
 export const dashboardNameAttr = "dashboard_name";
 export const databaseNameAttr = "database_name";
 export const nodeIDAttr = "node_id";
-export const nodeQueryString = "node";
 export const rangeIDAttr = "range_id";
 export const statementAttr = "statement";
 export const tableNameAttr = "table_name";
