2a125af31fee97ddf22a4ef2e89ed53707ea73fb
Matt Tracy <matt@cockroachlabs.com>
2018-07-11 13:34:53 -0700

    roachtest/workload: Queue-like workload
    Initial creation of a "queue-like" workload and a simple roachtest which
    utilizes the new workload.
    
    The roachtest runs the queue workload for a short 10second period, and
    then samples the execution time of a statement that requires a scan of
    the queue table. It then runs the queue workload for a much longer time,
    and afterwards executes the same scanning command. If the scan after is
    significantly greater than the scan before (current threshold is 30x),
    the test fails.
    
    Release note: None

--- pkg/ccl/workloadccl/allccl/all.go
+++ pkg/ccl/workloadccl/allccl/all.go
@@ -19,6 +19,7 @@ import (
 	_ "github.com/cockroachdb/cockroach/pkg/workload/jsonload"
 	_ "github.com/cockroachdb/cockroach/pkg/workload/kv"
 	_ "github.com/cockroachdb/cockroach/pkg/workload/querybench"
+	_ "github.com/cockroachdb/cockroach/pkg/workload/queue"
 	_ "github.com/cockroachdb/cockroach/pkg/workload/tpcc"
 	_ "github.com/cockroachdb/cockroach/pkg/workload/tpch"
 	_ "github.com/cockroachdb/cockroach/pkg/workload/ycsb"
