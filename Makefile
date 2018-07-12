cockroachdb/cockroach

commit 4a6c5acc709ff79d38730143b21fa9dc54effc37

Makefile: simplify `stress{,race}` using `-exec`
Release note: None

Makefile
@@ -878,9 +878,17 @@ bench: TESTTIMEOUT := $(BENCHTIMEOUT)
 # that longer running benchmarks can skip themselves.
 benchshort: override TESTFLAGS += -benchtime=1ns -short
 
+stress: ## Run tests under stress.
+stress stressrace: override GOFLAGS += -exec 'stress $(STRESSFLAGS)'
+stress stressrace: override TESTFLAGS += -v # necessary for interactive output from `stress`
+
+stressrace: ## Run tests under stress with the race detector enabled.
+stressrace: override GOFLAGS += -race
+stressrace: TESTTIMEOUT := $(RACETIMEOUT)
+
 .PHONY: check test testshort testrace testlogic testccllogic bench benchshort
 test: ## Run tests.
-check test testshort testrace bench benchshort:
+check test testshort testrace bench benchshort stress stressrace:
 	$(XGO) test $(GOFLAGS) -tags '$(TAGS)' -ldflags '$(LINKFLAGS)' -run "$(TESTS)" $(if $(BENCHES),-bench "$(BENCHES)") -timeout $(TESTTIMEOUT) $(PKG) $(TESTFLAGS)
 
 testlogic: ## Run SQL Logic Tests.
@@ -901,24 +909,6 @@ testraceslow: TESTTIMEOUT := $(RACETIMEOUT)
 testslow testraceslow: override TESTFLAGS += -v
 testslow testraceslow:
 	$(XGO) test $(GOFLAGS) -tags '$(TAGS)' -ldflags '$(LINKFLAGS)' -run "$(TESTS)" $(if $(BENCHES),-bench "$(BENCHES)") -timeout $(TESTTIMEOUT) $(PKG) $(TESTFLAGS) | grep -F ': Test' | sed -E 's/(--- PASS: |\(|\))//g' | awk '{ print $$2, $$1 }' | sort -rn | head -n 10
-
-stressrace: override GOFLAGS += -race
-stressrace: TESTTIMEOUT := $(RACETIMEOUT)
-
-# Beware! This target is complicated because it needs to handle complexity:
-# - PKG may be specified as relative (e.g. './gossip') or absolute (e.g.
-# github.com/cockroachdb/cockroach/gossip), and this target needs to create
-# the test binary in the correct location and `cd` to the correct directory.
-# This is handled by having `go list` produce the command line.
-# - PKG may also be recursive (e.g. './pkg/...'). This is also handled by piping
-# through `go list`.
-# - PKG may not contain any tests! This is handled with an `if` statement that
-# checks for the presence of a test binary before running `stress` on it.
-.PHONY: stress stressrace
-stress: ## Run tests under stress.
-stressrace: ## Run tests under stress with the race detector enabled.
-stress stressrace:
-	$(GO) list -tags '$(TAGS)' -f '$(XGO) test -v $(GOFLAGS) -tags '\''$(TAGS)'\'' -ldflags '\''$(LINKFLAGS)'\'' -c {{.ImportPath}} -o '\''{{.Dir}}'\''/stress.test && (cd '\''{{.Dir}}'\'' && if [ -f stress.test ]; then stress $(STRESSFLAGS) ./stress.test -test.run '\''$(TESTS)'\'' $(if $(BENCHES),-test.bench '\''$(BENCHES)'\'') -test.timeout $(TESTTIMEOUT) $(TESTFLAGS); fi)' $(PKG) | $(SHELL)
 
 .PHONY: upload-coverage
 upload-coverage: bin/.bootstrap

commit 41ff49a487724be513d35b61232142f0c87e5ce1

build: apply TESTTIMEOUT to individual tests during stress
4a6c5ac inadvertently changed the behavior of TESTTIMEOUT during 'make
stress'. Prior to that change it controlled the timeout for each
individual invocation of the process under stress; after that change it
controlled the overall timeout for the entire stress operation. By
popular demand, restore it to its previous behavior.

Specifying a timeout for the overall stress run is still possible, as it
always has been, by passing the -maxtime flag to the stress process
through STRESSFLAGS.

While I'm here, also deduplicate -v flags that appear in TESTFLAGS. 'go
test' complains if multiple -v flags are passed.

Release note: None

Makefile
@@ -780,9 +780,9 @@ testbuild:
 testshort: override TESTFLAGS += -short
 
 testrace: ## Run tests with the Go race detector enabled.
-testrace: override GOFLAGS += -race
-testrace: export GORACE := halt_on_error=1
-testrace: TESTTIMEOUT := $(RACETIMEOUT)
+testrace stressrace: override GOFLAGS += -race
+testrace stressrace: export GORACE := halt_on_error=1
+testrace stressrace: TESTTIMEOUT := $(RACETIMEOUT)
 
 # Directory scans in the builder image are excruciatingly slow when running
 # Docker for Mac, so we filter out the 20k+ UI dependencies that are
@@ -798,18 +798,16 @@ bench: TESTTIMEOUT := $(BENCHTIMEOUT)
 # that longer running benchmarks can skip themselves.
 benchshort: override TESTFLAGS += -benchtime=1ns -short
 
-stress: ## Run tests under stress.
-stress stressrace: override GOFLAGS += -exec 'stress $(STRESSFLAGS)'
-stress stressrace: override TESTFLAGS += -v # necessary for interactive output from `stress`
-
-stressrace: ## Run tests under stress with the race detector enabled.
-stressrace: override GOFLAGS += -race
-stressrace: TESTTIMEOUT := $(RACETIMEOUT)
-
 .PHONY: check test testshort testrace testlogic testccllogic bench benchshort
 test: ## Run tests.
-check test testshort testrace bench benchshort stress stressrace:
+check test testshort testrace bench benchshort:
 	$(xgo) test $(GOFLAGS) -tags '$(TAGS)' -ldflags '$(LINKFLAGS)' -run "$(TESTS)" $(if $(BENCHES),-bench "$(BENCHES)") -timeout $(TESTTIMEOUT) $(PKG) $(TESTFLAGS)
+
+.PHONY: stress stressrace
+stress: ## Run tests under stress.
+stressrace: ## Run tests under stress with the race detector enabled.
+stress stressrace:
+	$(xgo) test -exec 'stress $(STRESSFLAGS)' -tags '$(TAGS)' -ldflags '$(LINKFLAGS)' -run "$(TESTS)" -timeout 0 $(PKG) $(filter-out -v,$(TESTFLAGS)) -v -args -test.timeout $(TESTTIMEOUT)
 
 testlogic: ## Run SQL Logic Tests.
 testlogic: bin/logictest