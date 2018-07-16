5bd7b9be8728d47bc97f677b55abf86fd14edd36
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2018-07-16 06:51:10 +0200

    [wip] storage: add closed timestamp subsystem
    This commit introduces the basic closed timestamp subsystem. The system
    is far from complete (as a starter, it's 100% untested, so it definitely
    has lots of bugs and won't work properly at all), but the point of this
    PR is to get buy-in on the overall architectural approach, so that
    shortcomings can be addressed before they are cemented in more code.
    
    Great care has been taken to make each individual component self-
    contained and cleanly interfaced to the outside world, which should aid
    greatly in unit testing them in isolation. The package layout relative
    to `ct` is the following:
    
    .: slim root package containing only the most basic CT interfaces
    ./ctpb: protobuf definitions and service
    ./ctconfig: fat package pulling in everything needed to actually run
    ./ctstorage: storage for per-node closed timestamp state
    ./minprop: minimum proposal timestamp tracker
    ./provider: the Provider.
    
    Most of these packages depend on `ct` (more precisely, on the interfaces
    they need to use), but in turn don't depend on each other. I'm open to
    changing the name of individual packages (for example, prefixing them
    all with `ct`).
    
    As an entry point for review, the following diagram together with the
    `ctconfig` and `ct` packages should be instructive in getting a bird's
    eye view of how the subsystem fits into CockroachDB: All of the
    components are grouped in a Container (intended as a pass-around
    per-instance Singleton) and is passed down to the Stores via the
    StoreConfig. Replicas proposing commands talk to the Tracker; replicas
    trying to serve follower reads talk to the Provider, which receives
    closed timestamp updates for the local node and its peers.
    
    ```
                                Node 1 | Node 2
                                       |
    +---------+  Close  +-----------+  |  +-----------+
    | Tracker |<--------|           |  |  |           |
    +-----+---+         | +-------+ |  |  | +-------+ |  CanServe
          ^             | |Storage| |  |  | |Storage| |<---------+
          |             | --------+ |  |  | +-------+ |          |
          |Track        |           |  |  |           |     +----+----+
          |             | Provider  |  |  | Provider  |     | Follower|
          |             +-----------+  |  +-----------+     | Replica |
          |                 ^                  ^            +----+----+
          |                 |Subscribe         |Notify           |
          |                 |                  |                 |
    +---------+             |      Request     |                 |
    |Proposing| Refresh +---+----+ <----- +----+-----+  Request  |
    | Replica |<--------| Server |        |  Clients |<----------+
    +---------+         +--------+ -----> +----------+  EnsureClient
                                     CT
    ```
    
    Release note: None

---
+++ pkg/storage/ct/ctstorage/storage.go
@@ -0,0 +1,139 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package ctstorage
+
+import (
+	"fmt"
+
+	"unsafe"
+
+	"bytes"
+	"sort"
+
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/storage/ct"
+	"github.com/cockroachdb/cockroach/pkg/storage/ct/ctpb"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+)
+
+// SingleStorage stores and manages closed timestamp updates originating from a
+// single source (i.e. node). A SingleStorage internally maintains multiple
+// buckets for historical closed timestamp information. The reason for this is
+// twofold:
+//
+// 1. The most recent closed timestamp update is also the hardest to prove a
+// read for, since it comes with larger minimum lease applied indexes. In
+// situations in which followers are lagging behind with their command
+// application, this could lead to a runaway scenario, in which a closed
+// timestamp update can never be used until it is replaced by a new one, which
+// in turn also will never be used, etc. Instead, a SingleStorage keeps some
+// amount of history and upstream systems can try to prove a follower read using
+// an older closed timestamp instead.
+//
+// 2. Follower reads can be used to implement recovery of a consistent
+// cluster-wide snapshot after catastrophic loss of quorum. To do this, the
+// mechanism must locate at least one replica of every range in the cluster, and
+// for each range find the largest possible timestamp at which follower reads
+// are possible among the surviving replicas. Of all these per-range timestamps,
+// the smallest can be used to read from all ranges, resulting in a consistent
+// snapshot. This makes it crucial that every replica can serve at least some
+// follower reads, even when regularly outpaced by the closed timestamp
+// frontier. Emitted MLAIs may never even be proposed to Raft in the event of
+// an ill-timed crash, and so historic information is invaluable.
+//
+// TODO(tschottdorf): this shouldn't be an interface but a concrete impl, the
+// interface should sit below SingleStorage and abstract out only the buckets.
+// Or there isn't an interface at all but simply a persistence hook that can
+// be set when periodically writing to disk is desired.
+type SingleStorage interface {
+	fmt.Stringer
+	// VisitAscending walks through the buckets of the storage in ascending
+	// closed timestamp order, until the closure returns true (or all buckets
+	// have been visited).
+	VisitAscending(func(ctpb.Entry) (done bool))
+	// Add adds a new Entry to this storage. The entry is added to the most
+	// recent bucket and remaining buckets are rotated as indicated by their age
+	// relative to the newly added Entry.
+	Add(ctpb.Entry)
+}
+
+type entry struct {
+	SingleStorage
+}
+
+// MultiStorage implements the ct.Storage interface.
+type MultiStorage struct {
+	f func() SingleStorage
+	// TODO(tschottdorf): clean up storages that haven't been used for extended
+	// periods of time.
+	m syncutil.IntMap
+}
+
+var _ ct.Storage = (*MultiStorage)(nil)
+
+// NewMultiStorage sets up a MultiStorage which uses the given factory method
+// for setting up the SingleStorage used for each individual NodeID for which
+// operations are received.
+func NewMultiStorage(f func() SingleStorage) *MultiStorage {
+	return &MultiStorage{f: f}
+}
+
+func (ms *MultiStorage) getOrCreate(nodeID roachpb.NodeID) SingleStorage {
+	key := int64(nodeID)
+	p, found := ms.m.Load(key)
+	if found {
+		// Fast path that avoids calling f().
+		return (*entry)(p).SingleStorage
+	}
+
+	ss := ms.f()
+	p, _ = ms.m.LoadOrStore(key, unsafe.Pointer(&entry{ss}))
+	return (*entry)(p).SingleStorage
+}
+
+// VisitAscending implements ct.Storage.
+func (ms *MultiStorage) VisitAscending(nodeID roachpb.NodeID, f func(ctpb.Entry) (done bool)) {
+	ss := ms.getOrCreate(nodeID)
+	ss.VisitAscending(f)
+}
+
+// Add implements ct.Storage.
+func (ms *MultiStorage) Add(nodeID roachpb.NodeID, entry ctpb.Entry) {
+	ss := ms.getOrCreate(nodeID)
+	ss.Add(entry)
+}
+
+// String prints a tabular rundown of the contents of the MultiStorage.
+func (ms *MultiStorage) String() string {
+	type tuple struct {
+		roachpb.NodeID
+		SingleStorage
+	}
+
+	var sl []tuple
+	ms.m.Range(func(k int64, p unsafe.Pointer) bool {
+		sl = append(sl, tuple{roachpb.NodeID(k), (*entry)(p).SingleStorage})
+		return true // want more
+	})
+	sort.Slice(sl, func(i, j int) bool {
+		return sl[i].NodeID < sl[j].NodeID
+	})
+	var buf bytes.Buffer
+	for i := range sl {
+		buf.WriteString(fmt.Sprintf("\n***** n%d *****\n", sl[i].NodeID))
+		buf.WriteString(sl[i].SingleStorage.String())
+	}
+	return buf.String()
+}
