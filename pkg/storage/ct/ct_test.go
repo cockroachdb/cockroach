bf05d984262cd17cceb79bc71174ab06b770d307
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2018-07-16 06:51:10 +0200

    [wip] storage: add closed timestamp subsystem
    This commit introduces the basic closed timestamp subsystem. The system
    is far from complete (as a starter, it's 100% untested, so it definitely
    has lots of bugs and won't work properly at all), but the point of this
    PR is to get buy-in on the overall architectural approach, so that
    shortcomings can be addressed before they are cemented in more code.
    
    Great care has been taken to make each individual component self-
    contained and cleanly interfaced to the outside world, which should aid
    greatly in unit testing them in isolation. The package layout relative
    to `ct` is the following:
    
    .: slim root package containing only the most basic CT interfaces
    ./ctpb: protobuf definitions and service
    ./ctconfig: fat package pulling in everything needed to actually run
    ./ctstorage: storage for per-node closed timestamp state
    ./minprop: minimum proposal timestamp tracker
    ./provider: the Provider.
    
    Most of these packages depend on `ct` (more precisely, on the interfaces
    they need to use), but in turn don't depend on each other. I'm open to
    changing the name of individual packages (for example, prefixing them
    all with `ct`).
    
    As an entry point for review, the following diagram together with the
    `ctconfig` and `ct` packages should be instructive in getting a bird's
    eye view of how the subsystem fits into CockroachDB: All of the
    components are grouped in a Container (intended as a pass-around
    per-instance Singleton) and is passed down to the Stores via the
    StoreConfig. Replicas proposing commands talk to the Tracker; replicas
    trying to serve follower reads talk to the Provider, which receives
    closed timestamp updates for the local node and its peers.
    
    ```
                                Node 1 | Node 2
                                       |
    +---------+  Close  +-----------+  |  +-----------+
    | Tracker |<--------|           |  |  |           |
    +-----+---+         | +-------+ |  |  | +-------+ |  CanServe
          ^             | |Storage| |  |  | |Storage| |<---------+
          |             | --------+ |  |  | +-------+ |          |
          |Track        |           |  |  |           |     +----+----+
          |             | Provider  |  |  | Provider  |     | Follower|
          |             +-----------+  |  +-----------+     | Replica |
          |                 ^                  ^            +----+----+
          |                 |Subscribe         |Notify           |
          |                 |                  |                 |
    +---------+             |      Request     |                 |
    |Proposing| Refresh +---+----+ <----- +----+-----+  Request  |
    | Replica |<--------| Server |        |  Clients |<----------+
    +---------+         +--------+ -----> +----------+  EnsureClient
                                     CT
    ```
    
    Release note: None

---
+++ pkg/storage/ct/ct_test.go
@@ -0,0 +1,99 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package ct_test
+
+import (
+	"errors"
+	"testing"
+
+	"context"
+
+	"time"
+
+	"sync/atomic"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/rpc"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/storage/ct"
+	"github.com/cockroachdb/cockroach/pkg/storage/ct/ctconfig"
+	"github.com/cockroachdb/cockroach/pkg/storage/ct/ctpb"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+)
+
+type mockDialer struct{}
+
+func (mc *mockDialer) Dial(context.Context, roachpb.NodeID) (ctpb.Client, error) {
+	return nil, errors.New("unimplemented")
+}
+func (mc *mockDialer) Ready(roachpb.NodeID) bool {
+	return false
+}
+
+func TestContainer(t *testing.T) {
+	stopper := stop.NewStopper()
+	defer stopper.Stop(context.Background())
+
+	rawClock := hlc.NewClock(hlc.NewManualClock(1E9).UnixNano, 500*time.Millisecond)
+	epoch, live := int64(1), int32(1) // atomically
+
+	clock := func() (liveNow hlc.Timestamp, liveEpoch int64, _ error) {
+		if atomic.LoadInt32(&live) != 1 {
+			return hlc.Timestamp{}, 0, errors.New("not live")
+		}
+		return rawClock.Now(), atomic.LoadInt64(&epoch), nil
+	}
+
+	var refreshed struct {
+		syncutil.Mutex
+		sl []roachpb.RangeID
+	}
+	refresh := func(requested ...roachpb.RangeID) {
+		refreshed.Lock()
+		refreshed.sl = append(refreshed.sl, requested...)
+		refreshed.Unlock()
+	}
+
+	dialer := &mockDialer{}
+
+	st := cluster.MakeTestingClusterSettings()
+	rpcContext := rpc.NewContext(
+		log.AmbientContext{Tracer: st.Tracer},
+		&base.Config{Insecure: true},
+		rawClock,
+		stopper,
+		&st.Version,
+	)
+	grpcServer := rpc.NewServer(rpcContext)
+
+	cfg := ctconfig.Config{
+		Settings:   st,
+		Stopper:    stopper,
+		Clock:      clock,
+		Refresh:    refresh,
+		Dialer:     dialer,
+		GRPCServer: grpcServer,
+	}
+	c := ctconfig.NewContainer(cfg)
+	c.Start()
+
+	// Silence unused warnings.
+	var _, _ = ct.Epoch(0), ct.LAI(0)
+	var _ = ctconfig.DialerAdapter(nil)
+}
