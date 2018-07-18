b2426793444f8986e61b292d5aa035d22c0d61b3
Raphael 'kena' Poss <knz@cockroachlabs.com>
2018-07-18 17:44:13 +0200

    sql/tree: fix the walk code for OrderBy clauses
    This was broken previously.
    
    Release note: None

--- pkg/sql/sem/tree/walk.go
+++ pkg/sql/sem/tree/walk.go
@@ -993,7 +993,9 @@ func walkOrderBy(v Visitor, order OrderBy) (OrderBy, bool) {
 				order = append(OrderBy(nil), order...)
 				copied = true
 			}
-			order[i].Expr = e
+			orderByCopy := *order[i]
+			orderByCopy.Expr = e
+			order[i] = &orderByCopy
 		}
 	}
 	return order, copied
