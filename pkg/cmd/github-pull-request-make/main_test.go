a183e8e414c4d9c802d4a9c84a6c8f8a8c9c6d1d
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2018-07-25 21:30:08 +0200

    github-pull-request-make: avoid confusion when pkgs are moved
    The CI stress runner would get confused when packages were renamed or
    removed in a PR and would try to run tests on nonexistent packages,
    which would then fail. This was because it was using the "Patch" format, which
    gives the patch for each individual push. By switching to the "diff"
    format, we get a single diff for the whole PR, which still can trip
    but at least is immune to such churn within a PR.
    
    Also added a helper that makes it easier to generate new test cases.
    In the process, I regenerated one of the existing test cases (which
    was tied to a PR). The PR had changed a lot, and so did the test
    result. I reviewed this manually it generally looked like it was
    doing the expected thing. I'm not sure if the special casing such
    as stressing when unskipping a test, etc, was preserved.
    Unfortunately there wasn't any rationale in the original code about
    preferring the Patch format over Diff.
    
    Release note: None

--- pkg/cmd/github-pull-request-make/main_test.go
+++ pkg/cmd/github-pull-request-make/main_test.go
@@ -15,19 +15,33 @@
 package main
 
 import (
+	"context"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"reflect"
+	"strconv"
+	"strings"
 	"testing"
+
+	"github.com/kr/pretty"
 )
 
 func TestPkgsFromDiff(t *testing.T) {
 	for filename, expPkgs := range map[string]map[string]pkg{
 		"testdata/10305.diff": {
-			filepath.Join("pkg", "roachpb"): {tests: []string{"TestLeaseVerify"}},
+			"pkg/roachpb": {tests: []string{"TestLeaseEquivalence"}},
+			"pkg/storage": {tests: []string{"TestStoreRangeLease", "TestStoreRangeLeaseSwitcheroo"}},
 		},
 		"testdata/skip.diff": {
-			filepath.Join("pkg", "ccl", "storageccl"): {tests: []string{"TestPutS3"}},
+			"pkg/ccl/storageccl": {tests: []string{"TestPutS3"}},
+		},
+		// This PR had some churn and renamed packages. This was formerly problematic
+		// because nonexistent packages would be emitted.
+		"testdata/27595.diff": {
+			"pkg/storage/closedts/transport": {tests: []string{"TestTransportConnectOnRequest", "TestTransportClientReceivesEntries"}},
+			"pkg/storage/closedts/container": {tests: []string{"TestTwoNodes"}},
+			"pkg/storage/closedts/storage":   {tests: []string{"TestConcurrent"}},
 		},
 	} {
 		t.Run(filename, func(t *testing.T) {
@@ -45,4 +59,34 @@ func TestPkgsFromDiff(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestPkgsFromDiffHelper(t *testing.T) {
+	// This helper can easily generate new test cases.
+	t.Skip("only for manual use")
+
+	ctx := context.Background()
+	client := ghClient(ctx)
+
+	const prNum = 10305
+
+	diff, err := getDiff(ctx, client, "cockroachdb", "cockroach", prNum)
+	if err != nil {
+		t.Fatal(err)
+	}
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	name := filepath.Join(wd, "testdata", strconv.Itoa(prNum)+".diff")
+	if err := ioutil.WriteFile(name, []byte(diff), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	pkgs, err := pkgsFromDiff(strings.NewReader(diff))
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Errorf("read the following information:\n%v\n\ndiff at %s", pretty.Sprint(pkgs), name)
 }
