ccf810b53030196320e1e2a7c9cfc98b00c556c6
madhavsuresh <madhav@cockroachlabs.com>
2018-08-01 14:00:29 -0400

    opt: add optimizer support for numeric references
    This commit adds support in the optimizer for the following
    type of query:
    
    `SELECT * FROM [53 as t]`
    
    Release note: None

--- pkg/sql/opt/testutils/testcat/create_table.go
+++ pkg/sql/opt/testutils/testcat/create_table.go
@@ -21,6 +21,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/opt"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/types"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/util"
 )
 
@@ -104,12 +105,15 @@ func (tc *Catalog) CreateTable(stmt *tree.CreateTable) *Table {
 
 		case *tree.IndexTableDef:
 			tab.addIndex(def, nonUniqueIndex)
-		}
+
 		// TODO(rytaft): In the future we will likely want to check for unique
 		// constraints, indexes, and foreign key constraints to determine
 		// nullability, uniqueness, etc.
 	}
 
+	// We need to keep track of the tableID from
+	// numeric references
+	tab.tableID = sqlbase.ID(len(tc.tables))
 	// Add the new table to the catalog.
 	tc.AddTable(tab)
 
