cockroachdb/cockroach

commit 2e99384dd266215b37264f0d21ca608af34ba1d1

cli: make --host/--{listen,advertise,http}-addr recognize port numbers
Prior to this patch, the various `cockroach` sub-commands would take
separate flags to specify an address/hostanme and to specify a port
number.

Meanwhile:

1. `--join` would recognize the syntax `host:port`.
2. the web UI, docs and other places often refer to a "server address"
   as the pair hostname:portnr.

For user convenience, it is thus important to make the interface more
straightforward/regular. This patch achieves this as follows:

- the flags
  `--listen-addr`/`--advertise-addr`/`--http-addr` (server-side) and
  `--host` (client-side) now recognize the syntax `host/addr:port`.
- the server-side `--port` flags are still recognized for backward
  compatibility but are marked as deprecated.
  The client-side `--port` is still recognized and not
  deprecated for now, but hidden from the contextual help.

As a side-effect of recognizing the port number inside the same flag,
the syntax with square brackets for IPv6 addresses now becomes
necessary when specifying also a port number. The syntax without
square brackets (and without port number) is temporarily still
recognized for backward compatibility, but is also marked as
deprecated.

Release note (cli change): the server-side command line flag
`--listen-addr`, which replaces the previous `--host` flag, is now
equipped to recognize both a hostname/address and port number. The
`--port` flag is deprecated as a result.

Release note (cli change): the server-side command line flag
`--http-addr`, which replaces the previous `--http-host` flag, is now
equipped to recognize both a hostname/address and port number. The
`--http-port` flag is deprecated as a result.

Release note (cli change): the server-side command line flag
`--advertise-addr`, which replaces the previous `--advertise-host`
flag, is now equipped to recognize both a hostname/address and
port number. The `--advertise-port` flag is deprecated as a result.

pkg/cmd/roachtest/upgrade.go
@@ -86,7 +86,7 @@ func registerUpgrade(r *registry) {
 
 		stop := func(node int) error {
 			port := fmt.Sprintf("{pgport:%d}", node)
-			if err := c.RunE(ctx, c.Node(node), "./cockroach quit --insecure --port "+port); err != nil {
+			if err := c.RunE(ctx, c.Node(node), "./cockroach quit --insecure --host=:"+port); err != nil {
 				return err
 			}
 			c.Stop(ctx, c.Node(node))
@@ -95,7 +95,7 @@ func registerUpgrade(r *registry) {
 
 		decommissionAndStop := func(node int) error {
 			port := fmt.Sprintf("{pgport:%d}", node)
-			if err := c.RunE(ctx, c.Node(node), "./cockroach quit --decommission --insecure --port "+port); err != nil {
+			if err := c.RunE(ctx, c.Node(node), "./cockroach quit --decommission --insecure --host=:"+port); err != nil {
 				return err
 			}
 			c.Stop(ctx, c.Node(node))

pkg/acceptance/cli_test.go
@@ -117,9 +117,9 @@ function finish() {
 trap finish EXIT
 
 HOST=$(hostname -f)
-$bin start --logtostderr=INFO --background --insecure --listen-addr="${HOST}" --listen-port=12345 &> out
-$bin sql --insecure --host="${HOST}" --port=12345 -e "show databases"
-$bin quit --insecure --host="${HOST}" --port=12345
+$bin start --logtostderr=INFO --background --insecure --listen-addr="${HOST}":12345 &> out
+$bin sql --insecure --host="${HOST}":12345 -e "show databases"
+$bin quit --insecure --host="${HOST}":12345
 `
 	containerConfig.Cmd = []string{"/bin/bash", "-c", script}
 	if err := testDockerOneShot(ctx, t, "start_flags_test", containerConfig); err != nil {

pkg/acceptance/cluster/cluster.go
@@ -43,7 +43,7 @@ type Cluster interface {
 	// dismantle the cluster.
 	AssertAndStop(context.Context, testing.TB)
 	// ExecCLI runs `./cockroach <args>`, while filling in required flags such as
-	// --insecure, --certs-dir, --host or --port.
+	// --insecure, --certs-dir, --host.
 	//
 	// Returns stdout, stderr, and an error.
 	ExecCLI(ctx context.Context, i int, args []string) (string, string, error)

pkg/acceptance/cluster/dockercluster.go
@@ -541,7 +541,7 @@ func (l *DockerCluster) startNode(ctx context.Context, node *testNode) {
   pprof:     docker exec -it %[4]s pprof https+insecure://$(hostname):%[5]s/debug/pprof/heap
   cockroach: %[6]s
 
-  cli-env:   COCKROACH_INSECURE=false COCKROACH_CERTS_DIR=%[7]s COCKROACH_HOST=%s COCKROACH_PORT=%d`,
+  cli-env:   COCKROACH_INSECURE=false COCKROACH_CERTS_DIR=%[7]s COCKROACH_HOST=%s:%d`,
 		node.Name(), "https://"+httpAddr.String(), localLogDir, node.Container.id[:5],
 		base.DefaultHTTPPort, cmd, certsDir, httpAddr.IP, httpAddr.Port)
 }

pkg/acceptance/localcluster/localcluster.go
@@ -77,7 +77,7 @@ func (b *LocalCluster) AssertAndStop(ctx context.Context, t testing.TB) {
 // ExecCLI implements cluster.Cluster.
 func (b *LocalCluster) ExecCLI(ctx context.Context, i int, cmd []string) (string, string, error) {
 	cmd = append([]string{b.Cfg.Binary}, cmd...)
-	cmd = append(cmd, "--insecure", "--port", b.Port(ctx, i))
+	cmd = append(cmd, "--insecure", "--host", ":"+b.Port(ctx, i))
 	c := exec.CommandContext(ctx, cmd[0], cmd[1:]...)
 	var o, e bytes.Buffer
 	c.Stdout, c.Stderr = &o, &e

pkg/ccl/cliccl/cli_test.go
@@ -42,7 +42,7 @@ func newCLITest(args base.TestServerArgs) cliTest {
 	}
 	return cliTest{
 		TestServer: s.(*server.TestServer),
-		connArgs:   []string{"--insecure", "--host=" + host, "--port=" + port},
+		connArgs:   []string{"--insecure", "--host=" + host + ":" + port},
 	}
 }
 

pkg/cli/cli_test.go
@@ -290,8 +290,7 @@ func (c cliTest) RunWithArgs(origArgs []string) {
 				args = append(args, "--insecure=false")
 				args = append(args, fmt.Sprintf("--certs-dir=%s", c.certsDir))
 			}
-			args = append(args, fmt.Sprintf("--host=%s", h))
-			args = append(args, fmt.Sprintf("--port=%s", p))
+			args = append(args, fmt.Sprintf("--host=%s:%s", h, p))
 		}
 		args = append(args, origArgs[1:]...)
 

pkg/cli/cliflags/flags.go
@@ -206,16 +206,22 @@ can also be specified (e.g. .25).`,
 	}
 
 	ClientHost = FlagInfo{
-		Name:        "host",
-		EnvVar:      "COCKROACH_HOST",
-		Description: `Database server host to connect to.`,
+		Name:   "host",
+		EnvVar: "COCKROACH_HOST",
+		Description: `
+CockroachDB node to connect to.
+This can be specified either as an address/hostname, or
+together with a port number as in -s myhost:26257.
+If the port number is left unspecified, it defaults to 26257.
+An IPv6 address can also be specified with the notation [...], for
+example [::1]:26257 or [fe80::f6f2:::]:26257.`,
 	}
 
 	ClientPort = FlagInfo{
 		Name:        "port",
 		Shorthand:   "p",
 		EnvVar:      "COCKROACH_PORT",
-		Description: `Database server port to connect to.`,
+		Description: `Deprecated. Use --host=<host>:<port>.`,
 	}
 
 	Database = FlagInfo{
@@ -267,8 +273,7 @@ sql_safe_updates = FALSE.`,
 	}
 
 	Set = FlagInfo{
-		Name:      "set",
-		Shorthand: "s",
+		Name: "set",
 		Description: `
 Set a client-side configuration parameter before running the SQL
 shell. This flag may be specified multiple times.`,
@@ -327,9 +332,12 @@ or both forms can be used together, for example:
 	ListenAddr = FlagInfo{
 		Name: "listen-addr",
 		Description: `
-The address or hostname to listen on. An IPv6 address can also be
-specified with the notation [...], for example [::1] or
-[fe80::f6f2:::].
+The address/hostname and port to listen on, for example
+--listen-addr=myhost:26257 or --listen-addr=:26257 (listen on all
+interfaces). If the port part is left unspecified, it defaults
+to 26257.
+An IPv6 address can also be specified with the notation [...], for
+example [::1]:26257 or [fe80::f6f2:::]:26257.
 If --advertise-addr is left unspecified, the node will also announce
 this address for use by other nodes. It is strongly recommended to use
 --advertise-addr in cloud and container deployments or any setup where
@@ -341,16 +349,6 @@ NAT is present between cluster nodes.`,
 		Description: `Alias for --listen-addr. Deprecated.`,
 	}
 
-	ListenPort = FlagInfo{
-		Name:      "listen-port",
-		Shorthand: "p",
-		Description: `
-The port to bind to.
-If --advertise-port is left unspecified, the node will also announce
-this port for use by other nodes. If a router implements NAT or N:M
-port forwarding, be sure to also use --advertise-port.`,
-	}
-
 	ServerPort = FlagInfo{
 		Name:        "port",
 		Description: `Alias for --listen-port. Deprecated.`,
@@ -359,10 +357,14 @@ port forwarding, be sure to also use --advertise-port.`,
 	AdvertiseAddr = FlagInfo{
 		Name: "advertise-addr",
 		Description: `
-The address or hostname to advertise to other CockroachDB nodes for intra-cluster
-communication; it must resolve and be routable from other nodes in the cluster.
+The address/hostname and port to advertise to other CockroachDB nodes
+for intra-cluster communication. It must resolve and be routable from
+other nodes in the cluster.
+If left unspecified, it defaults to the setting of --listen-addr.
 An IPv6 address can also be specified with the notation [...], for
-example [::1] or [fe80::f6f2:::].`,
+example [::1]:26257 or [fe80::f6f2:::]:26257.
+The port number should be the same as in --listen-addr unless port
+forwarding is set up on an intermediate firewall/router.`,
 	}
 
 	AdvertiseHost = FlagInfo{
@@ -371,16 +373,18 @@ example [::1] or [fe80::f6f2:::].`,
 	}
 
 	AdvertisePort = FlagInfo{
-		Name: "advertise-port",
-		Description: `
-The port to advertise to other CockroachDB nodes for intra-cluster
-communication. This should not be set differently from --listen-port
-unless port forwarding is set up on an intermediate firewall/router.`,
+		Name:        "advertise-port",
+		Description: `Deprecated. Use --advertise-addr=<host>:<port>.`,
 	}
 
 	ListenHTTPAddr = FlagInfo{
-		Name:        "http-addr",
-		Description: `The hostname or IP address to bind to for HTTP requests.`,
+		Name: "http-addr",
+		Description: `
+The hostname or IP address to bind to for HTTP requests.
+If left unspecified, the address part defaults to the setting of
+--listen-addr. The port number defaults to 8080.
+An IPv6 address can also be specified with the notation [...], for
+example [::1]:8080 or [fe80::f6f2:::]:8080.`,
 	}
 
 	ListenHTTPAddrAlias = FlagInfo{
@@ -390,7 +394,7 @@ unless port forwarding is set up on an intermediate firewall/router.`,
 
 	ListenHTTPPort = FlagInfo{
 		Name:        "http-port",
-		Description: `The port to bind to for HTTP requests.`,
+		Description: `Deprecated. Use --http-addr=<host>:<port>.`,
 	}
 
 	ListeningURLFile = FlagInfo{

commit 767fd5dacadb509a49e96fa60ce7ee7231243ba9

roachtest: fix upgrade test
This is the same fix as #28472, but for a different test. Use a `quit`
command line that is compatible with 2.0. This was accidentally broken
in #28373.

Fixes #28452

Release note: None

pkg/cmd/roachtest/upgrade.go
@@ -86,7 +86,10 @@ func registerUpgrade(r *registry) {
 
 		stop := func(node int) error {
 			port := fmt.Sprintf("{pgport:%d}", node)
-			if err := c.RunE(ctx, c.Node(node), "./cockroach quit --insecure --host=:"+port); err != nil {
+			// Note that the following command line needs to run against both v2.0
+			// and the current branch. Do not change it in a manner that is
+			// incompatible with 2.0.
+			if err := c.RunE(ctx, c.Node(node), "./cockroach quit --insecure --port="+port); err != nil {
 				return err
 			}
 			c.Stop(ctx, c.Node(node))
@@ -95,7 +98,10 @@ func registerUpgrade(r *registry) {
 
 		decommissionAndStop := func(node int) error {
 			port := fmt.Sprintf("{pgport:%d}", node)
-			if err := c.RunE(ctx, c.Node(node), "./cockroach quit --decommission --insecure --host=:"+port); err != nil {
+			// Note that the following command line needs to run against both v2.0
+			// and the current branch. Do not change it in a manner that is
+			// incompatible with 2.0.
+			if err := c.RunE(ctx, c.Node(node), "./cockroach quit --decommission --insecure --port="+port); err != nil {
 				return err
 			}
 			c.Stop(ctx, c.Node(node))