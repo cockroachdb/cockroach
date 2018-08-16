12d90fb4968f9e275f740b26a0f07aee78fc5682
Arjun Narayan <arjun@cockroachlabs.com>
2018-08-16 13:56:39 -0400

    sql: move subquery exec mode to package distsql
    In preparation for having subquery related logic live in the distsql
    runner. This commit is pure enum movement and associated renames
    
    Release note: None

---
+++ pkg/sql/distsqlrun/subquery.go
@@ -0,0 +1,46 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package distsqlrun
+
+type SubqueryExecMode int
+
+const (
+	SubqueryExecModeUnknown SubqueryExecMode = iota
+	// Subquery is argument to EXISTS.
+	// Result type is Bool.
+	SubqueryExecModeExists
+	// Subquery is argument to IN, ANY, SOME, or ALL. Any number of rows
+	// expected. Result type is tuple of rows. As a special case, if
+	// there is only one column selected, the result is a tuple of the
+	// selected values (instead of a tuple of 1-tuples).
+	SubqueryExecModeAllRowsNormalized
+	// Subquery is argument to an ARRAY constructor. Any number of rows
+	// expected, and exactly one column is expected. Result type is tuple
+	// of selected values.
+	SubqueryExecModeAllRows
+	// Subquery is argument to another function. At most 1 row
+	// expected. Result type is tuple of columns, unless there is
+	// exactly 1 column in which case the result type is that column's
+	// type. If there are no rows, the result is NULL.
+	SubqueryExecModeOneRow
+)
+
+var SubqueryExecModeNames = map[SubqueryExecMode]string{
+	SubqueryExecModeUnknown:           "<unknown>",
+	SubqueryExecModeExists:            "exists",
+	SubqueryExecModeAllRowsNormalized: "all rows normalized",
+	SubqueryExecModeAllRows:           "all rows",
+	SubqueryExecModeOneRow:            "one row",
+}
