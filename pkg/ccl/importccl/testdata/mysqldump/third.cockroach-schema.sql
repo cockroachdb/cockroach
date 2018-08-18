44abe060dd349284eafc39251f482788b61cec4e
David Taylor <tinystatemachine@gmail.com>
2018-08-18 13:27:44 +0000

    importccl: use normalized, complaint names in mysqldump import
    This uses the 'compliant' names for all mysql identifiers -- lower-case with any non-letters and non-digits replaced with '_'.
    
    Release note: none.

--- pkg/ccl/importccl/testdata/mysqldump/third.cockroach-schema.sql
+++ pkg/ccl/importccl/testdata/mysqldump/third.cockroach-schema.sql
@@ -2,9 +2,9 @@ CREATE TABLE third (
     i INT PRIMARY KEY,
     a INT,
     b INT,
-    "C" INT,
+    c INT,
     INDEX a (a, b),
-    INDEX "C" ("C"),
+    INDEX c (c),
     FOREIGN KEY (a, b) REFERENCES second (i, k),
     FOREIGN KEY (c) REFERENCES third (i) ON UPDATE CASCADE
 );
