9f32267d4b91b24e72404a5cae4e2292dedae8eb
Alex Robinson <alexdwanerobinson@gmail.com>
2018-08-20 17:31:38 -0500

    storage: Up-replicate before removing replicas from dead stores
    Fixes #25392, by preventing the following series of events:
    
    1. Node x dies
    2. We remove node x's replica of some range
    3. Node y dies before we up-replicate, leaving the range unavailable (1
       out of 2 replicas dead)
    4. Node x comes back online. It can't help the situation, because its
       replica was officially removed from the range.
    
    Instead, we now up-replicate first before removing node x's replica.
    
    Release note: None

--- pkg/storage/replicate_queue.go
+++ pkg/storage/replicate_queue.go
@@ -286,7 +286,7 @@ func (rq *replicateQueue) processOneChange(
 		newStore, details, err := rq.allocator.AllocateTarget(
 			ctx,
 			zone,
-			desc.Replicas,
+			liveReplicas, // only include liveReplicas, since deadReplicas should soon be removed
 			rangeInfo,
 			disableStatsBasedRebalancing,
 		)
