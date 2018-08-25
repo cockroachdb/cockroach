0a6fffb7e574dd2aed119c3d6e2da65e9193961f
Vivek Menezes <vivek@cockroachlabs.com>
2018-08-25 16:32:22 -0400

    sql: stop setting avoidCachedDescriptors in analyzeViewQuery
    Release note: None

--- pkg/sql/create_view.go
+++ pkg/sql/create_view.go
@@ -87,7 +87,14 @@ func (p *planner) CreateView(ctx context.Context, n *tree.CreateView) (planNode,
 		return nil, fmtErr
 	}
 
-	planDeps, sourceColumns, err := p.analyzeViewQuery(ctx, n.AsSource)
+	var planDeps planDependencies
+	var sourceColumns sqlbase.ResultColumns
+	// To avoid races with ongoing schema changes to tables that the view
+	// depends on, make sure we use the most recent versions of table
+	// descriptors rather than the copies in the lease cache.
+	p.runWithOptions(resolveFlags{skipCache: true}, func() {
+		planDeps, sourceColumns, err = p.analyzeViewQuery(ctx, n.AsSource)
+	})
 	if err != nil {
 		return nil, err
 	}
