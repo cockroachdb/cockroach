db9b0abc2ef04d91f8b2bd7538c3d1e0245df1f1
Andrei Matei <andrei@cockroachlabs.com>
2018-08-29 13:51:51 -0400

    kv: disable merge queue for TestTxnCoordSenderRetries
    Fixes #29088
    
    Release note: None

--- pkg/kv/dist_sender_server_test.go
+++ pkg/kv/dist_sender_server_test.go
@@ -1852,6 +1852,9 @@ func TestTxnCoordSenderRetries(t *testing.T) {
 			}
 			return nil
 		}
+	// Don't clobber the test's splits.
+	storeKnobs.DisableMergeQueue = true
+
 	s, _, _ := serverutils.StartServer(t,
 		base.TestServerArgs{Knobs: base.TestingKnobs{Store: &storeKnobs}})
 	ctx := context.Background()
