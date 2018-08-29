d4d31787d57374a4e3f9f08ae12f2d46b5c63dee
Daniel Harrison <daniel.harrison@gmail.com>
2018-08-29 12:46:23 -0700

    changefeedccl: add an experimental-sql sink
    This is the [Table Sink] described by the RFC.
    
    sqlSink mirrors the semantics offered by kafkaSink as closely as
    possible, but writes to a SQL table (presumably in CockroachDB).
    Currently only for testing.
    
    Each emitted row or resolved timestamp is stored as a row in the table.
    Each table gets 3 partitions. Similar to kafkaSink, the order between
    two emits is only preserved if they are emitted to by the same node and
    to the same partition.
    
    Release note: None
    
    [table sink]: https://github.com/cockroachdb/cockroach/blob/release-2.1/docs/RFCS/20180501_change_data_capture.md#sql-table-sink

--- pkg/ccl/changefeedccl/sink_test.go
+++ pkg/ccl/changefeedccl/sink_test.go
@@ -10,11 +10,20 @@ package changefeedccl
 
 import (
 	"context"
+	"net/url"
+	"strconv"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/Shopify/sarama"
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/security"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/pkg/errors"
 )
@@ -117,4 +126,108 @@ func TestKafkaSink(t *testing.T) {
 	if err := sink.Flush(ctx); err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestSQLSink(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	ctx := context.Background()
+	s, sqlDBRaw, _ := serverutils.StartServer(t, base.TestServerArgs{UseDatabase: "d"})
+	defer s.Stopper().Stop(ctx)
+	sqlDB := sqlutils.MakeSQLRunner(sqlDBRaw)
+	sqlDB.Exec(t, `CREATE DATABASE d`)
+
+	sinkURL, cleanup := sqlutils.PGUrl(t, s.ServingAddr(), t.Name(), url.User(security.RootUser))
+	defer cleanup()
+	sinkURL.Path = `d`
+
+	targets := map[sqlbase.ID]string{0: `foo`, 1: `bar`}
+	sink, err := makeSQLSink(sinkURL.String(), `sink`, targets)
+	require.NoError(t, err)
+	defer func() { require.NoError(t, sink.Close()) }()
+
+	// Empty
+	require.NoError(t, sink.Flush(ctx))
+
+	// Undeclared topic
+	require.EqualError(t, sink.EmitRow(ctx, `nope`, nil, nil), `cannot emit to undeclared topic: nope`)
+
+	// With one row, nothing flushes until Flush is called.
+	require.NoError(t, sink.EmitRow(ctx, `foo`, []byte(`k1`), []byte(`v0`)))
+	sqlDB.CheckQueryResults(t, `SELECT key, value FROM sink ORDER BY PRIMARY KEY sink`,
+		[][]string{},
+	)
+	require.NoError(t, sink.Flush(ctx))
+	sqlDB.CheckQueryResults(t, `SELECT key, value FROM sink ORDER BY PRIMARY KEY sink`,
+		[][]string{{`k1`, `v0`}},
+	)
+	sqlDB.Exec(t, `TRUNCATE sink`)
+
+	// Verify the implicit flushing
+	sqlDB.CheckQueryResults(t, `SELECT count(*) FROM sink`, [][]string{{`0`}})
+	for i := 0; i < sqlSinkRowBatchSize+1; i++ {
+		require.NoError(t, sink.EmitRow(ctx, `foo`, []byte(`k1`), []byte(`v`+strconv.Itoa(i))))
+	}
+	// Should have auto flushed after sqlSinkRowBatchSize
+	sqlDB.CheckQueryResults(t, `SELECT count(*) FROM sink`, [][]string{{`3`}})
+	require.NoError(t, sink.Flush(ctx))
+	sqlDB.CheckQueryResults(t, `SELECT count(*) FROM sink`, [][]string{{`4`}})
+	sqlDB.Exec(t, `TRUNCATE sink`)
+
+	// Two tables interleaved in time
+	require.NoError(t, sink.EmitRow(ctx, `foo`, []byte(`kfoo`), []byte(`v0`)))
+	require.NoError(t, sink.EmitRow(ctx, `bar`, []byte(`kbar`), []byte(`v0`)))
+	require.NoError(t, sink.EmitRow(ctx, `foo`, []byte(`kfoo`), []byte(`v1`)))
+	require.NoError(t, sink.Flush(ctx))
+	sqlDB.CheckQueryResults(t, `SELECT topic, key, value FROM sink ORDER BY PRIMARY KEY sink`,
+		[][]string{{`bar`, `kbar`, `v0`}, {`foo`, `kfoo`, `v0`}, {`foo`, `kfoo`, `v1`}},
+	)
+	sqlDB.Exec(t, `TRUNCATE sink`)
+
+	// Multiple keys interleaved in time. Use sqlSinkNumPartitions+1 keys to
+	// guarantee that at lease two of them end up in the same partition.
+	for i := 0; i < sqlSinkNumPartitions+1; i++ {
+		require.NoError(t, sink.EmitRow(ctx, `foo`, []byte(`v`+strconv.Itoa(i)), []byte(`v0`)))
+	}
+	for i := 0; i < sqlSinkNumPartitions+1; i++ {
+		require.NoError(t, sink.EmitRow(ctx, `foo`, []byte(`v`+strconv.Itoa(i)), []byte(`v1`)))
+	}
+	require.NoError(t, sink.Flush(ctx))
+	sqlDB.CheckQueryResults(t, `SELECT partition, key, value FROM sink ORDER BY PRIMARY KEY sink`,
+		[][]string{
+			{`0`, `v3`, `v0`},
+			{`0`, `v3`, `v1`},
+			{`1`, `v1`, `v0`},
+			{`1`, `v2`, `v0`},
+			{`1`, `v1`, `v1`},
+			{`1`, `v2`, `v1`},
+			{`2`, `v0`, `v0`},
+			{`2`, `v0`, `v1`},
+		},
+	)
+	sqlDB.Exec(t, `TRUNCATE sink`)
+
+	// Emit resolved
+	require.NoError(t, sink.EmitResolvedTimestamp(ctx, []byte(`r0`)))
+	require.NoError(t, sink.EmitRow(ctx, `foo`, []byte(`foo0`), []byte(`v0`)))
+	require.NoError(t, sink.EmitResolvedTimestamp(ctx, []byte(`r1`)))
+	require.NoError(t, sink.Flush(ctx))
+	sqlDB.CheckQueryResults(t,
+		`SELECT topic, partition, key, value, resolved FROM sink ORDER BY PRIMARY KEY sink`,
+		[][]string{
+			{`bar`, `0`, ``, ``, `r0`},
+			{`bar`, `0`, ``, ``, `r1`},
+			{`bar`, `1`, ``, ``, `r0`},
+			{`bar`, `1`, ``, ``, `r1`},
+			{`bar`, `2`, ``, ``, `r0`},
+			{`bar`, `2`, ``, ``, `r1`},
+			{`foo`, `0`, ``, ``, `r0`},
+			{`foo`, `0`, `foo0`, `v0`, ``},
+			{`foo`, `0`, ``, ``, `r1`},
+			{`foo`, `1`, ``, ``, `r0`},
+			{`foo`, `1`, ``, ``, `r1`},
+			{`foo`, `2`, ``, ``, `r0`},
+			{`foo`, `2`, ``, ``, `r1`},
+		},
+	)
 }
