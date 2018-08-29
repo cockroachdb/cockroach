0cf3419cfa387558f585d8389a4d27649cef3666
Daniel Harrison <daniel.harrison@gmail.com>
2018-08-29 12:46:23 -0700

    changefeedccl: add an experimental-sql sink
    This is the [Table Sink] described by the RFC.
    
    sqlSink mirrors the semantics offered by kafkaSink as closely as
    possible, but writes to a SQL table (presumably in CockroachDB).
    Currently only for testing.
    
    Each emitted row or resolved timestamp is stored as a row in the table.
    Each table gets 3 partitions. Similar to kafkaSink, the order between
    two emits is only preserved if they are emitted to by the same node and
    to the same partition.
    
    Release note: None
    
    [table sink]: https://github.com/cockroachdb/cockroach/blob/release-2.1/docs/RFCS/20180501_change_data_capture.md#sql-table-sink

--- pkg/ccl/changefeedccl/sink.go
+++ pkg/ccl/changefeedccl/sink.go
@@ -10,11 +10,17 @@ package changefeedccl
 
 import (
 	"context"
+	gosql "database/sql"
+	"fmt"
+	"hash"
+	"hash/fnv"
 	"net/url"
 	"strings"
 	"sync"
 
 	"github.com/Shopify/sarama"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/builtins"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
@@ -52,6 +58,14 @@ func getSink(sinkURI string, targets map[sqlbase.ID]string) (Sink, error) {
 	case sinkSchemeKafka:
 		kafkaTopicPrefix := u.Query().Get(sinkParamTopicPrefix)
 		return getKafkaSink(kafkaTopicPrefix, u.Host, targets)
+	case sinkSchemeExperimentalSQL:
+		// Swap the changefeed prefix for the sql connection one that sqlSink
+		// expects.
+		u.Scheme = `postgres`
+		// TODO(dan): Make tableName configurable or based on the job ID or
+		// something.
+		tableName := `sqlsink`
+		return makeSQLSink(u.String(), tableName, targets)
 	default:
 		return nil, errors.Errorf(`unsupported sink: %s`, u.Scheme)
 	}
@@ -302,6 +316,147 @@ func (p *changefeedPartitioner) Partition(
 		return message.Partition, nil
 	}
 	return p.hash.Partition(message, numPartitions)
+}
+
+const (
+	sqlSinkCreateTableStmt = `CREATE TABLE IF NOT EXISTS "%s" (
+		topic STRING,
+		partition INT,
+		message_id INT,
+		key BYTES, value BYTES,
+		resolved BYTES,
+		PRIMARY KEY (topic, partition, message_id)
+	)`
+	sqlSinkEmitStmt = `INSERT INTO "%s" (topic, partition, message_id, key, value, resolved)`
+	sqlSinkEmitCols = 6
+	// Some amount of batching to mirror a bit how kafkaSink works.
+	sqlSinkRowBatchSize = 3
+	// While sqlSink is only used for testing, hardcode the number of
+	// partitions to something small but greater than 1.
+	sqlSinkNumPartitions = 3
+)
+
+// sqlSink mirrors the semantics offered by kafkaSink as closely as possible,
+// but writes to a SQL table (presumably in CockroachDB). Currently only for
+// testing.
+//
+// Each emitted row or resolved timestamp is stored as a row in the table. Each
+// table gets 3 partitions. Similar to kafkaSink, the order between two emits is
+// only preserved if they are emitted to by the same node and to the same
+// partition.
+type sqlSink struct {
+	db *gosql.DB
+
+	tableName string
+	topics    map[string]struct{}
+	hasher    hash.Hash32
+
+	rowBuf []interface{}
+}
+
+func makeSQLSink(uri, tableName string, targets map[sqlbase.ID]string) (*sqlSink, error) {
+	if u, err := url.Parse(uri); err != nil {
+		return nil, err
+	} else if u.Path == `` {
+		return nil, errors.Errorf(`must specify database`)
+	}
+	db, err := gosql.Open(`postgres`, uri)
+	if err != nil {
+		return nil, err
+	}
+	if _, err := db.Exec(fmt.Sprintf(sqlSinkCreateTableStmt, tableName)); err != nil {
+		return nil, err
+	}
+
+	s := &sqlSink{
+		db:        db,
+		tableName: tableName,
+		topics:    make(map[string]struct{}),
+		hasher:    fnv.New32a(),
+	}
+	for _, tableName := range targets {
+		s.topics[tableName] = struct{}{}
+	}
+	return s, nil
+}
+
+// EmitRow implements the Sink interface.
+func (s *sqlSink) EmitRow(ctx context.Context, topic string, key, value []byte) error {
+	if _, ok := s.topics[topic]; !ok {
+		return errors.Errorf(`cannot emit to undeclared topic: %s`, topic)
+	}
+
+	// Hashing logic copied from sarama.HashPartitioner.
+	s.hasher.Reset()
+	if _, err := s.hasher.Write(key); err != nil {
+		return err
+	}
+	partition := int32(s.hasher.Sum32()) % sqlSinkNumPartitions
+	if partition < 0 {
+		partition = -partition
+	}
+
+	var noResolved []byte
+	return s.emit(ctx, topic, partition, key, value, noResolved)
+}
+
+// EmitResolvedTimestamp implements the Sink interface.
+func (s *sqlSink) EmitResolvedTimestamp(ctx context.Context, payload []byte) error {
+	var noKey, noValue []byte
+	for topic := range s.topics {
+		for partition := int32(0); partition < sqlSinkNumPartitions; partition++ {
+			if err := s.emit(ctx, topic, partition, noKey, noValue, payload); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (s *sqlSink) emit(
+	ctx context.Context, topic string, partition int32, key, value, resolved []byte,
+) error {
+	// Generate the message id on the client to match the guaranttees of kafka
+	// (two messages are only guaranteed to keep their order if emitted from the
+	// same producer to the same partition).
+	messageID := builtins.GenerateUniqueInt(roachpb.NodeID(partition))
+	s.rowBuf = append(s.rowBuf, topic, partition, messageID, key, value, resolved)
+	if len(s.rowBuf)/sqlSinkEmitCols >= sqlSinkRowBatchSize {
+		return s.Flush(ctx)
+	}
+	return nil
+}
+
+// Flush implements the Sink interface.
+func (s *sqlSink) Flush(ctx context.Context) error {
+	if len(s.rowBuf) == 0 {
+		return nil
+	}
+
+	var stmt strings.Builder
+	fmt.Fprintf(&stmt, sqlSinkEmitStmt, s.tableName)
+	for i := 0; i < len(s.rowBuf); i++ {
+		if i == 0 {
+			stmt.WriteString(` VALUES (`)
+		} else if i%sqlSinkEmitCols == 0 {
+			stmt.WriteString(`),(`)
+		} else {
+			stmt.WriteString(`,`)
+		}
+		fmt.Fprintf(&stmt, `$%d`, i+1)
+	}
+	stmt.WriteString(`)`)
+	_, err := s.db.Exec(stmt.String(), s.rowBuf...)
+	if err != nil {
+		return err
+	}
+	s.rowBuf = s.rowBuf[:0]
+	return nil
+}
+
+// Close implements the Sink interface.
+func (s *sqlSink) Close() error {
+	return s.db.Close()
 }
 
 // encDatumRowBuffer is a FIFO of `EncDatumRow`s.
