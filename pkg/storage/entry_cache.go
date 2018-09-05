3afbd217567aa86c847e9104bd928b871c295e12
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2018-09-05 15:02:50 +0200

    storage: update import path for Raft
    upstream moved from github.com/etcd to go.etcd.io/etcd. We're going
    to have to pick up an update soon to fix #28918, so it's easier to
    switch now.
    
    We're not picking up any new commits except for the renames.
    
    Release note: None

--- pkg/storage/entry_cache.go
+++ pkg/storage/entry_cache.go
@@ -17,7 +17,7 @@ package storage
 
 import (
 	"github.com/biogo/store/llrb"
-	"github.com/coreos/etcd/raft/raftpb"
+	"go.etcd.io/etcd/raft/raftpb"
 
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/util/cache"
--- pkg/storage/store_snapshot_test.go
+++ pkg/storage/store_snapshot_test.go
@@ -26,7 +26,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
-	"github.com/coreos/etcd/raft/raftpb"
+	"go.etcd.io/etcd/raft/raftpb"
 	"golang.org/x/time/rate"
 )
 
