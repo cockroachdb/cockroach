be69480e71423c7172ce702bc484d34769c9baa2
Daniel Harrison <daniel.harrison@gmail.com>
2018-09-11 13:25:52 -0700

    changefeedccl: add experimental support format=avro
    Avro is specifically designed to encode data and to enforce backward
    and/or forward compatibility of that data as its schema changes.
    
    https://avro.apache.org/docs/current/
    
    This commit adds a method to convert our internal respresention of a
    table schema into an avro schema. Avro foundationally includes the
    schema with the data, either in the same bytes as the encoded records or
    by pointing at unique ID in some instance of the Confluent Schema
    Registry. The convention with Kafka is the latter, so when the Avro
    format is used, we require the user also provide the url of their schema
    registry.
    
    A bunch of column types are still unsupported and this needs some
    end-to-end tests to make sure the table descriptor to avro schema
    mapping is right, so no release note for now.
    
    Release note: None
    
    #
    # Commit message recommendations:
    #
    #     ---
    #     <pkg>: <short description>
    #
    #     <long description>
    #
    #     Release note (category): <release note description>
    #     ---
    #
    # Wrap long lines! 72 columns is best.
    #
    # The release note must be present if your commit has
    # user-facing changes. Leave the default above if not.
    #
    # Categories for release notes:
    # - cli change
    # - sql change
    # - admin ui change
    # - general change (e.g., change of required Go version)
    # - build change (e.g., compatibility with older CPUs)
    # - enterprise change (e.g., change to backup/restore)
    # - backwards-incompatible change
    # - performance improvement
    # - bug fix

--- pkg/ccl/changefeedccl/encoder.go
+++ pkg/ccl/changefeedccl/encoder.go
@@ -10,13 +10,24 @@ package changefeedccl
 
 import (
 	"bytes"
+	"encoding/binary"
 	gojson "encoding/json"
+	"net/http"
+	"net/url"
+	"path/filepath"
 
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/json"
 	"github.com/pkg/errors"
+)
+
+const (
+	confluentSchemaContentType   = `application/vnd.schemaregistry.v1+json`
+	confluentSubjectSuffixKey    = `-key`
+	confluentSubjectSuffixValue  = `-value`
+	confluentAvroWireFormatMagic = byte(0)
 )
 
 // Encoder turns a row into a serialized changefeed key, value, or resolved
@@ -37,10 +48,17 @@ type Encoder interface {
 	EncodeResolvedTimestamp(hlc.Timestamp) ([]byte, error)
 }
 
-func getEncoder(opts map[string]string) (Encoder, error) {
+func getEncoder(opts map[string]string, sinkURI string) (Encoder, error) {
 	switch formatType(opts[optFormat]) {
 	case ``, optFormatJSON:
 		return makeJSONEncoder(opts), nil
+	case optFormatAvro, optFormatAvroJSON:
+		uri, err := url.Parse(sinkURI)
+		if err != nil {
+			return nil, err
+		}
+		registryURL := uri.Query().Get(sinkParamConfluentSchemaRegistry)
+		return newConfluentAvroEncoder(opts, registryURL)
 	default:
 		return nil, errors.Errorf(`unknown %s: %s`, optFormat, opts[optFormat])
 	}
@@ -132,4 +150,162 @@ func (e *jsonEncoder) EncodeResolvedTimestamp(resolved hlc.Timestamp) ([]byte, e
 		},
 	}
 	return gojson.Marshal(resolvedMetaRaw)
+}
+
+// confluentAvroEncoder encodes changefeed entries as Avro's binary or textual
+// JSON format. Keys are the primary key columns in a record. Values are all
+// columns in a record.
+type confluentAvroEncoder struct {
+	registryURL string
+	json        bool
+
+	keyCache   map[tableIDAndVersion]confluentRegisteredSchema
+	valueCache map[tableIDAndVersion]confluentRegisteredSchema
+}
+
+type tableIDAndVersion uint64
+
+func makeTableIDAndVersion(id sqlbase.ID, version sqlbase.DescriptorVersion) tableIDAndVersion {
+	return tableIDAndVersion(id)<<32 + tableIDAndVersion(version)
+}
+
+type confluentRegisteredSchema struct {
+	schema     *avroSchemaRecord
+	registryID int32
+}
+
+var _ Encoder = &confluentAvroEncoder{}
+
+func newConfluentAvroEncoder(
+	opts map[string]string, registryURL string,
+) (*confluentAvroEncoder, error) {
+	// TODO(dan): Figure out what updated and resolved timestamps should
+	// look like with avro.
+	for _, opt := range []string{optUpdatedTimestamps, optResolvedTimestamps} {
+		if _, ok := opts[optUpdatedTimestamps]; ok {
+			return nil, errors.Errorf(
+				`%s=%s is not yet compatible with %s`, optFormat, optFormatAvro, opt)
+		}
+	}
+	e := &confluentAvroEncoder{
+		registryURL: registryURL,
+		json:        formatType(opts[optFormat]) == optFormatAvroJSON,
+		keyCache:    make(map[tableIDAndVersion]confluentRegisteredSchema),
+		valueCache:  make(map[tableIDAndVersion]confluentRegisteredSchema),
+	}
+	if !e.json && len(e.registryURL) == 0 {
+		return nil, errors.Errorf(`parameter %s is required with %s=%s`,
+			sinkParamConfluentSchemaRegistry, optFormat, optFormatAvro)
+	}
+	return e, nil
+}
+
+// EncodeKey implements the Encoder interface.
+func (e *confluentAvroEncoder) EncodeKey(
+	tableDesc *sqlbase.TableDescriptor, row sqlbase.EncDatumRow,
+) ([]byte, error) {
+	cacheKey := makeTableIDAndVersion(tableDesc.ID, tableDesc.Version)
+	registered, ok := e.keyCache[cacheKey]
+	if !ok {
+		var err error
+		registered.schema, err = indexToAvroSchema(tableDesc, &tableDesc.PrimaryIndex)
+		if err != nil {
+			return nil, err
+		}
+
+		if !e.json {
+			registered.registryID, err = e.register(registered.schema, confluentSubjectSuffixKey)
+			if err != nil {
+				return nil, err
+			}
+		}
+		// TODO(dan): Bound the size of this cache.
+		e.keyCache[cacheKey] = registered
+	}
+
+	if e.json {
+		return registered.schema.TextualFromRow(row)
+	}
+	// https://docs.confluent.io/current/schema-registry/docs/serializer-formatter.html#wire-format
+	header := []byte{
+		confluentAvroWireFormatMagic,
+		0, 0, 0, 0, // Placeholder for the ID.
+	}
+	binary.BigEndian.PutUint32(header[1:5], uint32(registered.registryID))
+	return registered.schema.BinaryFromRow(header, row)
+}
+
+// EncodeValue implements the Encoder interface.
+func (e *confluentAvroEncoder) EncodeValue(
+	tableDesc *sqlbase.TableDescriptor, row sqlbase.EncDatumRow, _ hlc.Timestamp,
+) ([]byte, error) {
+	cacheKey := makeTableIDAndVersion(tableDesc.ID, tableDesc.Version)
+	registered, ok := e.valueCache[cacheKey]
+	if !ok {
+		var err error
+		registered.schema, err = tableToAvroSchema(tableDesc)
+		if err != nil {
+			return nil, err
+		}
+
+		if !e.json {
+			registered.registryID, err = e.register(registered.schema, confluentSubjectSuffixValue)
+			if err != nil {
+				return nil, err
+			}
+		}
+		// TODO(dan): Bound the size of this cache.
+		e.valueCache[cacheKey] = registered
+	}
+	if e.json {
+		return registered.schema.TextualFromRow(row)
+	}
+	// https://docs.confluent.io/current/schema-registry/docs/serializer-formatter.html#wire-format
+	header := []byte{
+		confluentAvroWireFormatMagic,
+		0, 0, 0, 0, // Placeholder for the ID.
+	}
+	binary.BigEndian.PutUint32(header[1:5], uint32(registered.registryID))
+	return registered.schema.BinaryFromRow(header, row)
+}
+
+// EncodeResolvedTimestamp implements the Encoder interface.
+func (e *confluentAvroEncoder) EncodeResolvedTimestamp(resolved hlc.Timestamp) ([]byte, error) {
+	panic(`unimplemented`)
+}
+
+func (e *confluentAvroEncoder) register(
+	schema *avroSchemaRecord, subjectSuffix string,
+) (int32, error) {
+	type confluentSchemaVersionRequest struct {
+		Schema string `json:"schema"`
+	}
+	type confluentSchemaVersionResponse struct {
+		ID int32 `json:"id"`
+	}
+
+	url, err := url.Parse(e.registryURL)
+	if err != nil {
+		return 0, err
+	}
+	subject := schema.Name + subjectSuffix
+	url.Path = filepath.Join(url.EscapedPath(), `subjects`, subject, `versions`)
+
+	req := confluentSchemaVersionRequest{Schema: schema.codec.Schema()}
+	var buf bytes.Buffer
+	if err := gojson.NewEncoder(&buf).Encode(req); err != nil {
+		return 0, err
+	}
+
+	resp, err := http.Post(url.String(), confluentSchemaContentType, &buf)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+	var res confluentSchemaVersionResponse
+	if err := gojson.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return 0, err
+	}
+
+	return res.ID, nil
 }
