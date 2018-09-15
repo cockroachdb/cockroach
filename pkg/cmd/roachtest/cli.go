05db97de0f7179ffff2d61df53549e7ebb75e0d1
Peter Mattis <petermattis@gmail.com>
2018-09-15 11:28:00 -0400

    roachtest: deflake acceptance/cli/node-status
    The test expects node IDs to correspond to roachprod node indexes, so
    pass `--sequential` when starting the cluster.
    
    Fixes #30249
    
    Release note: None

--- pkg/cmd/roachtest/cli.go
+++ pkg/cmd/roachtest/cli.go
@@ -24,7 +24,7 @@ import (
 
 func runCLINodeStatus(ctx context.Context, t *test, c *cluster) {
 	c.Put(ctx, cockroach, "./cockroach")
-	c.Start(ctx, c.Range(1, 3))
+	c.Start(ctx, c.Range(1, 3), startArgs("--sequential"))
 
 	db := c.Conn(ctx, 1)
 	defer db.Close()
