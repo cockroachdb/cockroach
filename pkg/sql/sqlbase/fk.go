920333526fa5a8bb3d92c569545a98d5fbfe4330
Jordan Lewis <jordanthelewis@gmail.com>
2018-09-18 11:13:11 -0400

    sqlbase: bugfix in rowfetcher with multiple cfs
    A bug (introduced during the 2.1 cycle) caused scans issued via distsql
    against tables with multiple column families to return incorrect results
    or panic the server if some of the column families were marked as
    non-nullable. This bug could only occur in distsql because it only
    manifested when a rowfetcher was initialized with multiple input spans,
    which only occurs in distsql where a particular node owns 2
    non-contiguous portions of the keyspace. In this case, the code would
    misinterpret the empty response from the scan request on the second
    input span to mean that there was a limit on the last scan and that
    we might have entered a new input span, which caused the rowfetcher to
    mark the current row as ended and expect a full row starting from the
    next key retrieved.
    
    This was not the case, as this could happen in normal circumstances.
    When a row had multiple column families, the rowfetcher would fail to
    remember the results it had already seen for the earlier column families
    in a row, if a batch happened to end half way through the row's column
    families, and return incorrect and/or panic depending on the definition
    of the table.
    
    This patch removes this problem and adds a test for it. The test is a
    little complicated but I verified that it does in fact panic before the
    patch. It should catch problems like this in the future. We had no
    explicit tests of the rowfetcher before with multiple input spans.
    
    Note that this patch also deletes a test case, that tests correct
    behavior in the case of seeing duplicate rows from the input stream
    during an index join. This test case is contrived - index joins can
    never encounter duplicate rows from the input stream - so it's safe to
    delete.
    
    Release note: None

--- pkg/sql/sqlbase/fk.go
+++ pkg/sql/sqlbase/fk.go
@@ -275,20 +275,13 @@ type SpanKVFetcher struct {
 // nextBatch implements the kvFetcher interface.
 func (f *SpanKVFetcher) nextBatch(
 	_ context.Context,
-) (
-	ok bool,
-	kvs []roachpb.KeyValue,
-	batchResponse []byte,
-	numKvs int64,
-	maybeNewSpan bool,
-	err error,
-) {
+) (ok bool, kvs []roachpb.KeyValue, batchResponse []byte, numKvs int64, err error) {
 	if len(f.KVs) == 0 {
-		return false, nil, nil, 0, true, nil
+		return false, nil, nil, 0, nil
 	}
 	res := f.KVs
 	f.KVs = nil
-	return true, res, nil, 0, true, nil
+	return true, res, nil, 0, nil
 }
 
 // getRangesInfo implements the kvFetcher interface.
