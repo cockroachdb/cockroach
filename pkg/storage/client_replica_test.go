a189f303e8779f229a79ba768431e48481de1156
Alex Robinson <alexdwanerobinson@gmail.com>
2018-09-24 14:47:48 -0500

    storage: Skip TestSystemZoneConfigs when running race detector
    Touches #30483
    
    Release note: None

--- pkg/storage/client_replica_test.go
+++ pkg/storage/client_replica_test.go
@@ -42,6 +42,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
+	"github.com/cockroachdb/cockroach/pkg/util"
 	"github.com/cockroachdb/cockroach/pkg/util/caller"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
@@ -1747,7 +1748,7 @@ func TestSystemZoneConfigs(t *testing.T) {
 	// This test is relatively slow and resource intensive. When run under
 	// stressrace on a loaded machine (as in the nightly tests), sometimes the
 	// SucceedsSoon conditions below take longer than the allotted time (#25273).
-	if testing.Short() || testutils.NightlyStress() {
+	if testing.Short() || util.RaceEnabled || testutils.NightlyStress() {
 		t.Skip()
 	}
 
