9b85aa0ef6f9df4544e728371b9ca6a34b097173
Andrew Couch <hi@andrewcou.ch>
2018-09-26 13:38:16 -0400

    ui: add per-node charts to sql dashboard
    Release note: None

--- pkg/ui/src/views/cluster/containers/nodeGraphs/dashboards/sql.tsx
+++ pkg/ui/src/views/cluster/containers/nodeGraphs/dashboards/sql.tsx
@@ -1,92 +1,178 @@
 import React from "react";
 import _ from "lodash";
 
+import { AggregationLevel } from "src/redux/aggregationLevel";
 import { LineGraph } from "src/views/cluster/components/linegraph";
 import { Metric, Axis, AxisUnits } from "src/views/shared/components/metricQuery";
 
 import { GraphDashboardProps, nodeDisplayName } from "./dashboardUtils";
 
 export default function (props: GraphDashboardProps) {
-  const { nodeIDs, nodesSummary, nodeSources, tooltipSelection } = props;
+  const { nodeIDs, nodesSummary, nodeSources, tooltipSelection, aggregationLevel } = props;
 
-  return [
+  function default_aggregate(name: string, title: string, otherProps?: { [key: string]: boolean }) {
+    if (aggregationLevel === AggregationLevel.Cluster) {
+      return (
+        <Metric name={name} title={title} sources={nodeSources} {...otherProps} />
+      );
+    }
+
+    return nodeIDs.map((nid) => (
+      <Metric name={name} title={nodeDisplayName(nodesSummary, nid)} sources={[nid]} {...otherProps} />
+    ));
+  }
+
+  const charts = [];
+
+  charts.push(
     <LineGraph
       title="SQL Connections"
-      sources={nodeSources}
       tooltip={`The total number of active SQL connections ${tooltipSelection}.`}
     >
       <Axis label="connections">
-        <Metric name="cr.node.sql.conns" title="Client Connections" />
+        {default_aggregate("cr.node.sql.conns", "Client Connections")}
       </Axis>
     </LineGraph>,
+  );
 
-    <LineGraph
-      title="SQL Byte Traffic"
-      sources={nodeSources}
-      tooltip={
-        `The total amount of SQL client network traffic in bytes per second ${tooltipSelection}.`
-      }
-    >
-      <Axis units={AxisUnits.Bytes} label="byte traffic">
-        <Metric name="cr.node.sql.bytesin" title="Bytes In" nonNegativeRate />
-        <Metric name="cr.node.sql.bytesout" title="Bytes Out" nonNegativeRate />
-      </Axis>
-    </LineGraph>,
+  if (aggregationLevel === AggregationLevel.Cluster) {
+    charts.push(
+      <LineGraph
+        title="SQL Byte Traffic"
+        sources={nodeSources}
+        tooltip={
+          `The total amount of SQL client network traffic in bytes per second ${tooltipSelection}.`
+        }
+      >
+        <Axis units={AxisUnits.Bytes} label="byte traffic">
+          <Metric name="cr.node.sql.bytesin" title="Bytes In" nonNegativeRate />
+          <Metric name="cr.node.sql.bytesout" title="Bytes Out" nonNegativeRate />
+        </Axis>
+      </LineGraph>,
+    );
+  } else {
+    charts.push(
+      <LineGraph
+        title="SQL Byte Traffic"
+        subtitle="In"
+        sources={nodeSources}
+        tooltip={
+          `The total amount of SQL client network traffic in bytes per second ${tooltipSelection}.`
+        }
+      >
+        <Axis units={AxisUnits.Bytes} label="byte traffic">
+          {nodeIDs.map((nid) => (
+            <Metric
+              name="cr.node.sql.bytesin"
+              title={nodeDisplayName(nodesSummary, nid)}
+              nonNegativeRate
+            />
+          ))}
+        </Axis>
+      </LineGraph>,
+    );
+    charts.push(
+      <LineGraph
+        title="SQL Byte Traffic"
+        subtitle="Out"
+        sources={nodeSources}
+        tooltip={
+          `The total amount of SQL client network traffic in bytes per second ${tooltipSelection}.`
+        }
+      >
+        <Axis units={AxisUnits.Bytes} label="byte traffic">
+          {nodeIDs.map((nid) => (
+            <Metric
+              name="cr.node.sql.bytesout"
+              title={nodeDisplayName(nodesSummary, nid)}
+              nonNegativeRate
+            />
+          ))}
+        </Axis>
+      </LineGraph>,
+    );
+  }
 
-    <LineGraph
-      title="SQL Queries"
-      sources={nodeSources}
-      tooltip={
-        `A ten-second moving average of the # of SELECT, INSERT, UPDATE, and DELETE statements
-        started per second ${tooltipSelection}.`
-      }
-    >
-      <Axis label="queries">
-        <Metric name="cr.node.sql.select.count" title="Selects" nonNegativeRate />
-        <Metric name="cr.node.sql.update.count" title="Updates" nonNegativeRate />
-        <Metric name="cr.node.sql.insert.count" title="Inserts" nonNegativeRate />
-        <Metric name="cr.node.sql.delete.count" title="Deletes" nonNegativeRate />
-      </Axis>
-    </LineGraph>,
+  if (props.aggregationLevel === AggregationLevel.Cluster) {
+    charts.push(
+      <LineGraph
+        title="Statements"
+        sources={nodeSources}
+        tooltip={
+          `A ten-second moving average of the count of SELECT, INSERT, UPDATE, and DELETE statements
+          started per second ${tooltipSelection}.`
+        }
+      >
+        <Axis label="statements">
+          <Metric name="cr.node.sql.select.count" title="Selects" nonNegativeRate />
+          <Metric name="cr.node.sql.update.count" title="Updates" nonNegativeRate />
+          <Metric name="cr.node.sql.insert.count" title="Inserts" nonNegativeRate />
+          <Metric name="cr.node.sql.delete.count" title="Deletes" nonNegativeRate />
+        </Axis>
+      </LineGraph>,
+    );
+  } else {
+    charts.push(
+      <LineGraph
+        title="Statements"
+        tooltip={
+          `A ten-second moving average of the count of SQL statements
+          started per second ${tooltipSelection}.`
+        }
+      >
+        <Axis label="statements">
+          {
+            _.map(nodeIDs, (node) => (
+              <Metric
+                key={node}
+                name="cr.node.sql.query.count"
+                title={nodeDisplayName(nodesSummary, node)}
+                sources={[node]}
+                nonNegativeRate
+              />
+            ))
+          }
+        </Axis>
+      </LineGraph>,
+    );
+  }
 
+  charts.push(
     <LineGraph
       title="SQL Query Errors"
       sources={nodeSources}
       tooltip={"The number of statements which returned a planning or runtime error."}
     >
       <Axis label="errors">
-        <Metric name="cr.node.sql.failure.count" title="Errors" nonNegativeRate />
+        {default_aggregate("cr.node.sql.failure.count", "Errors", { nonNegativeRate: true })}
       </Axis>
     </LineGraph>,
+  );
 
+  charts.push(
     <LineGraph
       title="Active Distributed SQL Queries"
       sources={nodeSources}
       tooltip={`The total number of distributed SQL queries currently running ${tooltipSelection}.`}
     >
       <Axis label="queries">
-        <Metric name="cr.node.sql.distsql.queries.active" title="Active Queries" />
+        {default_aggregate("cr.node.sql.distsql.queries.active", "Active Queries")}
       </Axis>
     </LineGraph>,
+  );
 
+  charts.push(
     <LineGraph
       title="Active Flows for Distributed SQL Queries"
       tooltip="The number of flows on each node contributing to currently running distributed SQL queries."
     >
       <Axis label="flows">
-        {
-          _.map(nodeIDs, (node) => (
-            <Metric
-              key={node}
-              name="cr.node.sql.distsql.flows.active"
-              title={nodeDisplayName(nodesSummary, node)}
-              sources={[node]}
-            />
-          ))
-        }
+        {default_aggregate("cr.node.sql.distsql.flows.active", "Flows")}
       </Axis>
     </LineGraph>,
+  );
 
+  charts.push(
     <LineGraph
       title="Service Latency: SQL, 99th percentile"
       tooltip={(
@@ -111,7 +197,9 @@ export default function (props: GraphDashboardProps) {
         }
       </Axis>
     </LineGraph>,
+  );
 
+  charts.push(
     <LineGraph
       title="Service Latency: SQL, 90th percentile"
       tooltip={(
@@ -136,7 +224,9 @@ export default function (props: GraphDashboardProps) {
         }
       </Axis>
     </LineGraph>,
+  );
 
+  charts.push(
     <LineGraph
       title="Service Latency: DistSQL, 99th percentile"
       tooltip={
@@ -158,7 +248,9 @@ export default function (props: GraphDashboardProps) {
         }
       </Axis>
     </LineGraph>,
+  );
 
+  charts.push(
     <LineGraph
       title="Service Latency: DistSQL, 90th percentile"
       tooltip={
@@ -180,7 +272,9 @@ export default function (props: GraphDashboardProps) {
         }
       </Axis>
     </LineGraph>,
+  );
 
+  charts.push(
     <LineGraph
       title="Execution Latency: 99th percentile"
       tooltip={
@@ -202,7 +296,9 @@ export default function (props: GraphDashboardProps) {
         }
       </Axis>
     </LineGraph>,
+  );
 
+  charts.push(
     <LineGraph
       title="Execution Latency: 90th percentile"
       tooltip={
@@ -224,7 +320,9 @@ export default function (props: GraphDashboardProps) {
         }
       </Axis>
     </LineGraph>,
+  );
 
+  charts.push(
     <LineGraph
       title="Transactions"
       sources={nodeSources}
@@ -240,15 +338,19 @@ export default function (props: GraphDashboardProps) {
         <Metric name="cr.node.sql.txn.abort.count" title="Aborts" nonNegativeRate />
       </Axis>
     </LineGraph>,
+  );
 
+  charts.push(
     <LineGraph
       title="Schema Changes"
       sources={nodeSources}
       tooltip={`The total number of DDL statements per second ${tooltipSelection}.`}
     >
       <Axis label="statements">
-        <Metric name="cr.node.sql.ddl.count" title="DDL Statements" nonNegativeRate />
+        {default_aggregate("cr.node.sql.ddl.count", "DDL Statements", { nonNegativeRate: true })}
       </Axis>
     </LineGraph>,
-  ];
+  );
+
+  return charts;
 }
