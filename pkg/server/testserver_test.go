b56782d9915de288de016df899165048ae037726
Peter Mattis <petermattis@gmail.com>
2018-09-27 11:26:10 -0400

    server: add TestServerTest
    Add the most basic test possible of `TestServer`.
    
    Release note: None

---
+++ pkg/server/testserver_test.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+)
+
+func TestServerTest(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	s, _, _ := serverutils.StartServer(t, base.TestServerArgs{})
+	defer s.Stopper().Stop(context.TODO())
+}
