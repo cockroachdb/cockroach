9aca220febcb08c50e2eb1bfa4b4d223e16f071d
Alex Robinson <alexdwanerobinson@gmail.com>
2018-10-05 23:58:47 -0500

    ui: Add debug page for the EnqueueRange admin endpoint
    It's probably the worst-constructed page this UI has ever seen, but I
    managed to get something working. Feedback appreciated, but keep in mind
    that we'd like to squeeze something in for 2.1 (unless there are
    security-related reasons to keep it out, in which case the endpoint
    itself would be a concern moreseo than this page), since without this
    sort of page using the endpoint is pretty tough.
    
    The only issue I still have is that 400 errors don't display any detail
    about the error, just "Bad Request", and I'm not sure how to fix that.
    It's far from a show stopper, though.
    
    Release note (admin ui change): Added a debug page with a form that lets
    you manually enqueue a range in one of the various store-level replica
    queues on the store of your choice. For advanced users only.

---
+++ pkg/ui/src/views/reports/containers/enqueueRange/index.tsx
@@ -0,0 +1,213 @@
+import React from "react";
+import Helmet from "react-helmet";
+import { withRouter, WithRouterProps } from "react-router";
+import { connect } from "react-redux";
+
+import { enqueueRange } from "src/util/api";
+import { AdminUIState } from "src/redux/state";
+import { cockroach } from "src/js/protos";
+import { getDataFromServer } from "src/util/dataFromServer";
+import Print from "src/views/reports/containers/range/print";
+
+import EnqueueRangeRequest = cockroach.server.serverpb.EnqueueRangeRequest;
+import EnqueueRangeResponse = cockroach.server.serverpb.EnqueueRangeResponse;
+
+interface EnqueueRangeProps {
+  handleEnqueueRange: (queue: string, rangeID: string, nodeID: string, skipShouldQueue: boolean) => Promise<EnqueueRangeResponse>;
+}
+
+interface EnqueueRangeState {
+  queue: string;
+  rangeID: string;
+  nodeID: string;
+  skipShouldQueue: boolean;
+  response: EnqueueRangeResponse;
+  error: Error;
+  // TODO: nodeID, skipShouldQueue
+}
+
+class EnqueueRange extends React.Component<EnqueueRangeProps & WithRouterProps, EnqueueRangeState> {
+  constructor(props: EnqueueRangeProps & WithRouterProps) {
+    super(props);
+    this.state = {
+      queue: "",
+      rangeID: "",
+      nodeID: "",
+      skipShouldQueue: false,
+    };
+  }
+
+  handleUpdateQueue = (evt: React.FormEvent<{ value: string }>) => {
+    this.setState({
+      queue: evt.currentTarget.value,
+    });
+  }
+
+  handleUpdateRangeID = (evt: React.FormEvent<{ value: string }>) => {
+    this.setState({
+      rangeID: evt.currentTarget.value,
+    });
+  }
+
+  handleUpdateNodeID = (evt: React.FormEvent<{ value: string }>) => {
+    this.setState({
+      nodeID: evt.currentTarget.value,
+    });
+  }
+
+  handleSubmit = (evt: React.FormEvent<any>) => {
+    evt.preventDefault();
+
+    this.props.handleEnqueueRange(
+      this.state.queue,
+      this.state.rangeID,
+      this.state.nodeID,
+      this.state.skipShouldQueue
+    ).then(
+      (response) => {
+        this.setState({ response: response, error: null });
+      },
+      (err) => {
+        this.setState({ response: null, error: err });
+      },
+    );
+  }
+
+  renderResponse() {
+    const { response } = this.state;
+
+    if (!response) {
+      return null;
+    }
+
+    return (
+      <div>
+        <h2>Enqueue Range Output</h2>
+        {
+          _.map(response.details, (details) => (
+            <div>
+              <h3>Node n{details.node_id}</h3>
+              <table className="enqueue-range-table">
+                <tbody>
+                  <tr className="enqueue-range-table__row enqueue-range-table__row--header">
+                    <th className="enqueue-range-table__cell enqueue-range-table__cell--header">Timestamp</th>
+                    <th className="enqueue-range-table__cell enqueue-range-table__cell--header">Message</th>
+                  </tr>
+                  {
+                    _.map(details.events, (event) => (
+                      <tr className="enqueue-range-table__row">
+                        <td className="enqueue-range-table__cell enqueue-range-table__cell--date">{Print.Timestamp(event.time)}</td>
+                        <td className="enqueue-range-table__cell">{event.message}</td>
+                      </tr>
+                    ))
+                  }
+                </tbody>
+              </table>
+            {details.error ? <div><b>Error:</b> {details.error}</div> : null}
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
+
+  renderError() {
+    const { error } = this.state;
+
+    if (!error) {
+      return null;
+    }
+
+    return (
+      <div>Error running EnqueueRange: { error.message }</div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>Login</title>
+        </Helmet>
+        <div className="content">
+          <section className="section">
+            <div className="form-container">
+              <h1 className="heading">Manually enqueue range in a replica queue</h1>
+              <br />
+              <form onSubmit={this.handleSubmit} className="form-internal" method="post">
+                <input
+                  type="text"
+                  name="queue"
+                  className="input-text"
+                  onChange={this.handleUpdateQueue}
+                  value={this.state.queue}
+                  placeholder="Queue"
+                />
+                <br />
+                <input
+                  type="text"
+                  name="rangeID"
+                  className="input-text"
+                  onChange={this.handleUpdateRangeID}
+                  value={this.state.rangeID}
+                  placeholder="RangeID"
+                />
+                <br />
+                <input
+                  type="text"
+                  name="nodeID"
+                  className="input-text"
+                  onChange={this.handleUpdateNodeID}
+                  value={this.state.nodeID}
+                  placeholder="NodeID (optional)"
+                />
+                <br />
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={this.state.skipShouldQueue}
+                    name="skipShouldQueue"
+                    onChange={() => this.setState({ skipShouldQueue: !this.state.skipShouldQueue })}
+                  />
+                  SkipShouldQueue
+                </label>
+                <br />
+                <input
+                  type="submit"
+                  className="submit-button"
+                  value="Submit"
+                />
+              </form>
+            </div>
+          </section>
+        </div>
+        <div>
+          <section className="section">
+            {this.renderResponse()}
+            {this.renderError()}
+          </section>
+        </div>
+      </div>
+    );
+  }
+}
+
+// tslint:disable-next-line:variable-name
+const EnqueueRangeConnected = connect(
+  (state: AdminUIState) => {
+    return {};
+  },
+  (dispatch) => ({
+    handleEnqueueRange: (queue: string, rangeID: string, nodeID: string, skipShouldQueue: boolean) => {
+      const req = new EnqueueRangeRequest({
+        queue: queue,
+        range_id: rangeID,
+        node_id: nodeID,
+        skip_should_queue: skipShouldQueue,
+      });
+      return enqueueRange(req);
+    },
+  }),
+)(withRouter(EnqueueRange));
+
+export default EnqueueRangeConnected;
