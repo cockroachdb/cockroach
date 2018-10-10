1aa6d1ba70b1fa216cde1756bbf5ab8fb5dfec9a
Raphael 'kena' Poss <knz@cockroachlabs.com>
2018-10-10 22:35:35 +0200

    sql: produce the right pg error codes for column constraint failures
    Release note: None

--- pkg/sql/insert.go
+++ pkg/sql/insert.go
@@ -652,7 +652,7 @@ func GenerateInsertRow(
 	// Ensure that the values honor the specified column widths.
 	for i := range rowVals {
 		if err := sqlbase.CheckValueWidth(
-			insertCols[i].Type, rowVals[i], insertCols[i].Name); err != nil {
+			insertCols[i].Type, rowVals[i], &insertCols[i].Name); err != nil {
 			return nil, err
 		}
 	}
