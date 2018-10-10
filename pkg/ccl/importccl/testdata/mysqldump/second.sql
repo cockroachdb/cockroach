9e5eb46ba23afb99c13d4fb0a7b973b02fc8efaa
David Taylor <tinystatemachine@gmail.com>
2018-10-10 15:06:09 +0000

    importccl: add explicit default to mysql testdata timestamp
    this makes the testdata work on mysql 8.0.2+, where the timestamp type no longer has the implicit defaults.
    
    Release note: none.

--- pkg/ccl/importccl/testdata/mysqldump/second.sql
+++ pkg/ccl/importccl/testdata/mysqldump/second.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 5.7.23, for osx10.14 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.12, for osx10.14 (x86_64)
 --
 -- Host: localhost    Database: cockroachtestdata
 -- ------------------------------------------------------
--- Server version	5.7.23
+-- Server version	8.0.12
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+ SET NAMES utf8mb4 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `SECOND`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `SECOND` (
   `i` int(11) NOT NULL,
   `k` int(11) DEFAULT NULL,
@@ -51,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-09 22:55:39
+-- Dump completed on 2018-10-10 15:04:47
