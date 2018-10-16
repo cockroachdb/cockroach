72175b98f4aa9ebd221f4d1229521757391e4bfe
David Taylor <tinystatemachine@gmail.com>
2018-10-16 14:56:12 +0000

    build: make build and tracing able to build cgo-free
    Release note: none.

--- pkg/build/info.go
+++ pkg/build/info.go
@@ -23,17 +23,6 @@ import (
 	version "github.com/hashicorp/go-version"
 )
 
-// const char* compilerVersion() {
-// #if defined(__clang__)
-// 	return __VERSION__;
-// #elif defined(__GNUC__) || defined(__GNUG__)
-// 	return "gcc " __VERSION__;
-// #else
-// 	return "non-gcc, non-clang (or an unrecognized version)";
-// #endif
-// }
-import "C"
-
 // TimeFormat is the reference format for build.Time. Make sure it stays in sync
 // with the string passed to the linker in the root Makefile.
 const TimeFormat = "2006/01/02 15:04:05"
@@ -44,7 +33,7 @@ var (
 	tag             = "unknown" // Tag of this build (git describe --tags w/ optional '-dirty' suffix)
 	utcTime         string      // Build time in UTC (year/month/day hour:min:sec)
 	rev             string      // SHA-1 of this build (git rev-parse)
-	cgoCompiler     = C.GoString(C.compilerVersion())
+	cgoCompiler     = cgoVersion()
 	cgoTargetTriple string
 	platform        = fmt.Sprintf("%s %s", runtime.GOOS, runtime.GOARCH)
 	// Distribution is changed by the CCL init-time hook in non-APL builds.
