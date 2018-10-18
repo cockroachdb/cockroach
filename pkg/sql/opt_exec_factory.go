18a6bc12f01e18d635c0815ff8e3fdd1a15dfe4d
Radu Berinde <radu@cockroachlabs.com>
2018-10-18 16:23:00 -0400

    opt: plumb "point spans" hint
    Release note: None

--- pkg/sql/opt_exec_factory.go
+++ pkg/sql/opt_exec_factory.go
@@ -69,6 +69,7 @@ func (ef *execFactory) ConstructScan(
 	indexConstraint *constraint.Constraint,
 	hardLimit int64,
 	reverse bool,
+	allPointSpans bool,
 	reqOrdering exec.OutputOrdering,
 ) (exec.Node, error) {
 	tabDesc := table.(*optTable).desc
