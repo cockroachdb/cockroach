1f37c0684078b69b56ca7d522433f6f477d7259b
Jordan Lewis <jordanthelewis@gmail.com>
2018-10-22 15:10:25 -0400

    exec: [][]byte, not Bytes
    Similar reason as the Bools commit. This is pointless complexity for
    now.
    
    Release note: None

--- pkg/sql/exec/rowstovec_test.go
+++ pkg/sql/exec/rowstovec_test.go
@@ -91,8 +91,8 @@ func TestEncDatumRowsToColVecString(t *testing.T) {
 		t.Fatal(err)
 	}
 	expected := newMemColumn(types.Bytes, 2)
-	expected.Bytes().Set(0, []byte("foo"))
-	expected.Bytes().Set(1, []byte("bar"))
+	expected.Bytes()[0] = []byte("foo")
+	expected.Bytes()[1] = []byte("bar")
 	if !reflect.DeepEqual(vec, expected) {
 		t.Errorf("expected vector %+v, got %+v", expected, vec)
 	}
