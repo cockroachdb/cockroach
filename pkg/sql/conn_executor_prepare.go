de19d431ffdc2e7792f59895d6be434929439b68
Bob Vawter <bob@cockroachlabs.com>
2018-10-23 14:43:58 -0400

    sql: Generalize date/time parsing
    This change updates the date/time parsing code with a new implementation that
    does not delegate to `time.ParseInLocation()`.  This new version accepts all
    date/time formats which PostgreSQL is documented to accept.
    
    The following table compares the new `ParseTimestamp()` function to calling
    `ParseInLocation()`.  While it is true that `ParseInLocation()` is generally
    faster for any given pattern, the current parsing code must call it repeatedly,
    trying each supported date format until one succeeds.  The use of a named
    location, such as 'America/New_York' causes significant performance
    degredations if the results from `LoadLocation()` are not cached.
    
    ```
    2003-06-12/ParseTimestamp-8         	10000000	       122 ns/op	  81.53 MB/s
    2003-06-12/ParseInLocation-8        	30000000	        35.6 ns/op	 281.29 MB/s
    2003-06-12_01:02:03/ParseTimestamp-8         	10000000	       163 ns/op	 116.45 MB/s
    2003-06-12_01:02:03/ParseInLocation-8        	30000000	        54.4 ns/op	 349.16 MB/s
    2003-06-12_04:05:06.789-04:00/ParseTimestamp-8         	10000000	       238 ns/op	 121.69 MB/s
    2003-06-12_04:05:06.789-04:00/ParseInLocation-8        	10000000	       161 ns/op	 180.05 MB/s
    2000-01-01T02:02:02.567+09:30/ParseTimestamp-8         	 5000000	       233 ns/op	 124.01 MB/s
    2000-01-01T02:02:02.567+09:30/ParseInLocation-8        	10000000	       158 ns/op	 182.41 MB/s
    2003-06-12_04:05:06.789_America/New_York/ParseTimestamp-8         	 3000000	       475 ns/op	  84.06 MB/s
    2003-06-12_04:05:06.789_America/New_York/ParseInLocation-8        	  200000	      7313 ns/op	   3.15 MB/s
    ```
    
    The tests in `parsing_test.go` have an optional mode to cross-check the test
    data aginst a PostgreSQL server.  This is useful for developing, but is not
    part of the automated build.
    
    Parsing of BC dates is supported, #28099 could then be completed by changing
    the date-formatting code to print a BC date.
    
    This change would allow #30697 (incomplete handling of datestyle) to be
    re-evaluated, since the parser does allow configuration of YMD, DMY, or MDY
    input styles.
    
    Resolves #27500
    Resolves #27501
    
    Release note (sql change): A wider variety of date, time, and timestamp formats
    are now accepted by the SQL frontend.
    
    Release note (backward-incompatible change): Timezone abbreviations, such as
    EST, are no longer allowed when parsing or converting to a date/time type.
    Previously, an abbreviation would be accepted if it were an alias for the
    session's timezone.

--- pkg/sql/conn_executor_prepare.go
+++ pkg/sql/conn_executor_prepare.go
@@ -330,6 +330,8 @@ func (ex *connExecutor) execBind(
 					"expected %d arguments, got %d", numQArgs, len(bindCmd.Args)))
 		}
 
+		ptCtx := tree.NewParseTimeContext(ex.sessionData.DurationAdditionMode, ex.state.sqlTimestamp)
+
 		for i, arg := range bindCmd.Args {
 			k := strconv.Itoa(i + 1)
 			t := ps.InTypes[i]
@@ -337,7 +339,7 @@ func (ex *connExecutor) execBind(
 				// nil indicates a NULL argument value.
 				qargs[k] = tree.DNull
 			} else {
-				d, err := pgwirebase.DecodeOidDatum(t, qArgFormatCodes[i], arg)
+				d, err := pgwirebase.DecodeOidDatum(ptCtx, t, qArgFormatCodes[i], arg)
 				if err != nil {
 					if _, ok := err.(*pgerror.Error); ok {
 						return retErr(err)
