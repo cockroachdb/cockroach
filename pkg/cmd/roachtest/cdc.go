cockroachdb/cockroach

commit a3eaebfd91b3c92e7e922c47bb5df35e860c8eb2

cdc: Re-enable Fingerprint Validator
Fingerprint validator was previously disabled due to its lack of support
for "initial scan" situations. This commit fixes fingerprint validator
  to support initial scans correctly and re-enables the validator in
  tests that were using it.

Resolves #31110

Release note: None

pkg/cmd/roachtest/cdc.go
@@ -229,8 +229,7 @@ func runCDCBank(ctx context.Context, t *test, c *cluster) {
 		var numResolved, rowsSinceResolved int
 		v := changefeedccl.Validators{
 			changefeedccl.NewOrderValidator(`bank`),
-			// TODO(mrtracy): Disabled by #30902. Re-enabling is tracked by #31110.
-			// changefeedccl.NewFingerprintValidator(sqlDB.DB, `bank`, `fprint`, tc.partitions),
+			changefeedccl.NewFingerprintValidator(db, `bank`, `fprint`, tc.partitions),
 		}
 		if _, err := db.Exec(
 			`CREATE TABLE fprint (id INT PRIMARY KEY, balance INT, payload STRING)`,

pkg/ccl/changefeedccl/validations_test.go
@@ -72,8 +72,7 @@ func TestValidations(t *testing.T) {
 
 			v := Validators{
 				NewOrderValidator(`bank`),
-				// TODO(mrtracy): Disabled by #30902. Re-enabling is tracked by #31110.
-				// NewFingerprintValidator(db, `bank`, `fprint`, bank.Partitions()),
+				NewFingerprintValidator(db, `bank`, `fprint`, bank.Partitions()),
 			}
 			sqlDB.Exec(t, `CREATE TABLE fprint (id INT PRIMARY KEY, balance INT, payload STRING)`)
 			for {

pkg/ccl/changefeedccl/validator.go
@@ -176,6 +176,7 @@ func (v *fingerprintValidator) NoteResolved(partition string, resolved hlc.Times
 	if !v.resolved.Less(newResolved) {
 		return nil
 	}
+	initialScanComplete := v.resolved != (hlc.Timestamp{})
 	v.resolved = newResolved
 
 	// NB: Intentionally not stable sort because it shouldn't matter.
@@ -191,17 +192,23 @@ func (v *fingerprintValidator) NoteResolved(partition string, resolved hlc.Times
 		row := v.buffer[0]
 		v.buffer = v.buffer[1:]
 
-		if row.updated != lastUpdated {
-			if lastUpdated != (hlc.Timestamp{}) {
-				if err := v.fingerprint(lastUpdated); err != nil {
+		// If we have already completed the initial scan, verify the fingerprint at
+		// every point in time. Before the initial scan is complete, the fingerprint
+		// table might not have the earliest version of every row present in the
+		// table.
+		if initialScanComplete {
+			if row.updated != lastUpdated {
+				if lastUpdated != (hlc.Timestamp{}) {
+					if err := v.fingerprint(lastUpdated); err != nil {
+						return err
+					}
+				}
+				if err := v.fingerprint(row.updated.Prev()); err != nil {
 					return err
 				}
 			}
-			if err := v.fingerprint(row.updated.Prev()); err != nil {
-				return err
-			}
+			lastUpdated = row.updated
 		}
-		lastUpdated = row.updated
 
 		value := make(map[string]interface{})
 		if err := gojson.Unmarshal([]byte(row.value), &value); err != nil {

pkg/ccl/changefeedccl/validator_test.go
@@ -167,6 +167,7 @@ func TestFingerprintValidator(t *testing.T) {
 	t.Run(`missed_initial`, func(t *testing.T) {
 		sqlDB.Exec(t, `CREATE TABLE missed_initial (k INT PRIMARY KEY, v INT)`)
 		v := NewFingerprintValidator(sqlDB.DB, `foo`, `missed_initial`, []string{`p`})
+		noteResolved(t, v, `p`, ts[0])
 		// Intentionally missing {"k":1,"v":1} at ts[1].
 		v.NoteRow(ignored, `[1]`, `{"k":1,"v":2}`, ts[2])
 		v.NoteRow(ignored, `[1]`, `{"k":2,"v":2}`, ts[2])
@@ -179,6 +180,7 @@ func TestFingerprintValidator(t *testing.T) {
 	t.Run(`missed_middle`, func(t *testing.T) {
 		sqlDB.Exec(t, `CREATE TABLE missed_middle (k INT PRIMARY KEY, v INT)`)
 		v := NewFingerprintValidator(sqlDB.DB, `foo`, `missed_middle`, []string{`p`})
+		noteResolved(t, v, `p`, ts[0])
 		v.NoteRow(ignored, `[1]`, `{"k":1,"v":1}`, ts[1])
 		// Intentionally missing {"k":1,"v":2} at ts[2].
 		v.NoteRow(ignored, `[1]`, `{"k":2,"v":2}`, ts[2])
@@ -190,6 +192,28 @@ func TestFingerprintValidator(t *testing.T) {
 			`fingerprints did not match at `+ts[3].Prev().AsOfSystemTime()+
 				`: 1099511631581 vs 1099511631582`,
 		)
+	})
+	t.Run(`missed_end`, func(t *testing.T) {
+		sqlDB.Exec(t, `CREATE TABLE missed_end (k INT PRIMARY KEY, v INT)`)
+		v := NewFingerprintValidator(sqlDB.DB, `foo`, `missed_end`, []string{`p`})
+		noteResolved(t, v, `p`, ts[0])
+		v.NoteRow(ignored, `[1]`, `{"k":1,"v":1}`, ts[1])
+		v.NoteRow(ignored, `[1]`, `{"k":1,"v":2}`, ts[2])
+		v.NoteRow(ignored, `[1]`, `{"k":2,"v":2}`, ts[2])
+		// Intentionally missing {"k":1,"v":3} at ts[3].
+		noteResolved(t, v, `p`, ts[3])
+		assertValidatorFailures(t, v,
+			`fingerprints did not match at `+ts[3].AsOfSystemTime()+
+				`: 1099511631580 vs 1099511631581`,
+		)
+	})
+	t.Run(`initial_scan`, func(t *testing.T) {
+		sqlDB.Exec(t, `CREATE TABLE initial_scan (k INT PRIMARY KEY, v INT)`)
+		v := NewFingerprintValidator(sqlDB.DB, `foo`, `initial_scan`, []string{`p`})
+		v.NoteRow(ignored, `[1]`, `{"k":1,"v":3}`, ts[4])
+		v.NoteRow(ignored, `[1]`, `{"k":2,"v":2}`, ts[4])
+		noteResolved(t, v, `p`, ts[4])
+		assertValidatorFailures(t, v)
 	})
 	t.Run(`unknown_partition`, func(t *testing.T) {
 		v := NewFingerprintValidator(sqlDB.DB, `foo`, `unknown_partition`, []string{`p`})

commit 2dd417947b7afef8832be16c504ba7371692924e

cdc: Fix cdc/bank roachtest
Fix a bug introduced by #31774; the test was creating a fingerprint
validator which requires a table name, but in this case needed the fully
qualified name with the database (`bank.bank` instead of `bank`).

Also fixed a nil reference that occurred when exiting the test early
from the command line.

Resolves #31903

Release note: None

pkg/cmd/roachtest/cdc.go
@@ -236,7 +236,7 @@ func runCDCBank(ctx context.Context, t *test, c *cluster) {
 		var numResolved, rowsSinceResolved int
 		v := changefeedccl.Validators{
 			changefeedccl.NewOrderValidator(`bank`),
-			changefeedccl.NewFingerprintValidator(db, `bank`, `fprint`, tc.partitions),
+			changefeedccl.NewFingerprintValidator(db, `bank.bank`, `fprint`, tc.partitions),
 		}
 		if _, err := db.Exec(
 			`CREATE TABLE fprint (id INT PRIMARY KEY, balance INT, payload STRING)`,
@@ -246,6 +246,9 @@ func runCDCBank(ctx context.Context, t *test, c *cluster) {
 
 		for {
 			m := tc.Next(ctx)
+			if m == nil {
+				return fmt.Errorf("unexpected end of changefeed")
+			}
 			updated, resolved, err := changefeedccl.ParseJSONValueTimestamps(m.Value)
 			if err != nil {
 				return err