1dae763e41d1edd523638f4346d464bc60554862
Radu Berinde <radu@cockroachlabs.com>
2018-10-26 21:42:07 -0400

    opt: move ordering logic to new package
    Many relational operators have a `CanProvideOrdering` function used to
    figure out whether they can satisfy a required ordering, and another
    piece of code that builds the child required orderings (in
    `buildChildPhysicalProps`). The logic for these has to be "in sync"
    but the code lives in separate places. This makes changes hard to
    develop, as you have to constantly jump back and forth. Worse, we will
    need to add a third routine to figure out the ordering that is being
    provided.
    
    We create a new `ordering` package that will be home to this logic for
    all operators. Each operator will have a separate file in the package,
    which will make it easy to work with a given operator's code
    holistically.
    
    With this package, we also have a good place for unit testing of these
    functions (which allows simpler, more targeted tests than the usual
    opttester approach).
    
    No significant differences in benchmarks:
    
    name                               old time/op  new time/op  delta
    Phases/kv-read/Parse               2.31ns ± 1%  2.32ns ± 1%    ~
    Phases/kv-read/OptBuild            8.53µs ± 1%  8.10µs ± 1%  -4.97%
    Phases/kv-read/Normalize           8.48µs ± 1%  8.13µs ± 2%  -4.09%
    Phases/kv-read/Explore             14.3µs ± 1%  13.9µs ± 0%  -2.94%
    Phases/kv-read/ExecBuild           15.3µs ± 2%  14.8µs ± 1%  -2.95%
    Phases/kv-read-no-prep/Parse       15.2µs ± 1%  15.0µs ± 3%    ~
    Phases/kv-read-no-prep/OptBuild    35.6µs ± 1%  35.7µs ± 2%    ~
    Phases/kv-read-no-prep/Normalize   35.9µs ± 1%  35.2µs ± 0%  -1.87%
    Phases/kv-read-no-prep/Explore     43.1µs ± 2%  42.2µs ± 1%    ~
    Phases/kv-read-no-prep/ExecBuild   44.2µs ± 1%  43.4µs ± 1%  -1.81%
    Phases/kv-read-const/Parse         2.33ns ± 1%  2.41ns ± 1%  +3.44%
    Phases/kv-read-const/OptBuild       180ns ± 2%   178ns ± 2%    ~
    Phases/kv-read-const/Normalize      173ns ± 3%   173ns ± 2%    ~
    Phases/kv-read-const/Explore        196ns ± 4%   195ns ± 4%    ~
    Phases/kv-read-const/ExecBuild      662ns ± 0%   670ns ± 1%    ~
    Phases/tpcc-new-order/Parse        2.37ns ± 1%  2.32ns ± 1%    ~
    Phases/tpcc-new-order/OptBuild     15.9µs ± 1%  15.5µs ± 1%  -2.43%
    Phases/tpcc-new-order/Normalize    16.0µs ± 1%  15.4µs ± 1%  -3.45%
    Phases/tpcc-new-order/Explore      44.8µs ± 1%  44.2µs ± 1%  -1.34%
    Phases/tpcc-new-order/ExecBuild    46.4µs ± 1%  45.9µs ± 2%    ~
    Phases/tpcc-delivery/Parse         2.36ns ± 2%  2.35ns ± 1%    ~
    Phases/tpcc-delivery/OptBuild      15.2µs ± 1%  14.4µs ± 5%    ~
    Phases/tpcc-delivery/Normalize     15.2µs ± 3%  14.5µs ± 1%  -4.23%
    Phases/tpcc-delivery/Explore       29.2µs ± 1%  29.1µs ± 0%    ~
    Phases/tpcc-delivery/ExecBuild     29.9µs ± 1%  30.3µs ± 1%    ~
    Phases/tpcc-stock-level/Parse      2.38ns ± 0%  2.33ns ± 1%  -1.79%
    Phases/tpcc-stock-level/OptBuild   54.8µs ± 1%  52.5µs ± 1%  -4.23%
    Phases/tpcc-stock-level/Normalize  52.8µs ± 1%  51.7µs ± 1%  -2.13%
    Phases/tpcc-stock-level/Explore     135µs ± 1%   136µs ± 1%    ~
    Phases/tpcc-stock-level/ExecBuild   146µs ± 1%   145µs ± 1%    ~
    Exec/kv-read/ExecPlan               190µs ± 1%   194µs ± 9%    ~
    Exec/kv-read/ExecOpt                176µs ± 0%   175µs ± 2%    ~
    Exec/kv-read-no-prep/ExecPlan       356µs ± 2%   359µs ± 2%    ~
    Exec/kv-read-no-prep/ExecOpt        377µs ± 2%   374µs ± 2%    ~
    Exec/kv-read-const/ExecPlan         185µs ± 2%   184µs ± 2%    ~
    Exec/kv-read-const/ExecOpt          141µs ± 2%   140µs ± 0%    ~
    Exec/tpcc-new-order/ExecPlan        303µs ± 1%   300µs ± 1%    ~
    Exec/tpcc-new-order/ExecOpt         290µs ± 1%   289µs ± 3%    ~
    Exec/tpcc-delivery/ExecPlan         248µs ± 2%   247µs ± 3%    ~
    Exec/tpcc-delivery/ExecOpt          244µs ± 2%   227µs ± 2%  -6.83%
    Exec/tpcc-stock-level/ExecPlan      669µs ± 2%   674µs ± 2%    ~
    Exec/tpcc-stock-level/ExecOpt       639µs ± 0%   635µs ± 1%    ~
    
    Release note: None

---
+++ pkg/sql/opt/ordering/project.go
@@ -0,0 +1,71 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package ordering
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/memo"
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/props"
+)
+
+func projectCanProvideOrdering(expr memo.RelExpr, required *props.OrderingChoice) bool {
+	// Project can pass through its ordering if the ordering depends only on
+	// columns present in the input.
+	return isOrderingBoundBy(expr.(*memo.ProjectExpr).Input, required)
+}
+
+func projectBuildChildReqOrdering(
+	parent memo.RelExpr, required *props.OrderingChoice, childIdx int,
+) props.OrderingChoice {
+	if childIdx != 0 {
+		return props.OrderingChoice{}
+	}
+
+	// We may need to remove ordering columns that are not output by the input
+	// expression.
+	input := parent.(*memo.ProjectExpr).Input
+	result := projectOrderingToInput(input, required)
+
+	// Project can prune input columns, which can cause its FD set to be
+	// pruned as well. Check the ordering to see if it can be simplified
+	// with respect to the input FD set.
+	fdSet := &input.Relational().FuncDeps
+	if result.CanSimplify(fdSet) {
+		result = result.Copy()
+		result.Simplify(fdSet)
+	}
+	return result
+}
+
+// isOrderingBoundBy returns true if the given ordering can be satisfied using
+// only the columns produced by input.
+func isOrderingBoundBy(input memo.RelExpr, ordering *props.OrderingChoice) bool {
+	inputCols := input.Relational().OutputCols
+	return ordering.CanProjectCols(inputCols)
+}
+
+// projectOrderingToInput projects out columns from an ordering (if necessary);
+// can only be used if isOrderingBoundBy is true for the ordering. If projection
+// is not necessary, returns a shallow copy of the ordering.
+func projectOrderingToInput(
+	input memo.RelExpr, ordering *props.OrderingChoice,
+) props.OrderingChoice {
+	childOutCols := input.Relational().OutputCols
+	if ordering.SubsetOfCols(childOutCols) {
+		return *ordering
+	}
+	result := ordering.Copy()
+	result.ProjectCols(childOutCols)
+	return result
+}
