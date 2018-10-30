09f18820483df46c018034d6463ef2aa62efc292
Celia La <celia@cockroachlabs.com>
2018-10-29 20:40:58 -0400

    ui: teach Loading component to render multiple errors
    Release note: None

--- pkg/ui/src/views/shared/components/loading/index.tsx
+++ pkg/ui/src/views/shared/components/loading/index.tsx
@@ -19,10 +19,31 @@ import "./index.styl";
 
 interface LoadingProps {
   loading: boolean;
-  error?: Error | null;
+  error?: Error | Error[] | null;
   className?: string;
   image?: string;
   render: () => React.ReactNode;
+}
+
+/**
+ * getValidErrorsList eliminates any null Error values, and returns either
+ * null or a non-empty list of Errors.
+ */
+function getValidErrorsList (errors?: Error | Error[] | null): Error[] | null {
+  if (errors) {
+    if (!Array.isArray(errors)) {
+      // Put single Error into a list to simplify logic in main Loading component.
+      return [errors];
+    } else {
+      // Remove null values from Error[].
+      const validErrors = errors.filter(e => !!e);
+      if (validErrors.length === 0) {
+        return null;
+      }
+      return validErrors;
+    }
+  }
+  return null;
 }
 
 /**
@@ -30,18 +51,28 @@ interface LoadingProps {
  * loading prop is true.
  */
 export default function Loading(props: LoadingProps) {
-  const className = props.className || "loading-image loading-image__spinner-left";
+  const className = props.className || "loading-image loading-image__spinner";
   const imageURL = props.image || spinner;
   const image = {
     "backgroundImage": `url(${imageURL})`,
   };
+
+  const errors = getValidErrorsList(props.error);
+
   // Check for `error` before `loading`, since tests for `loading` often return
   // true even if CachedDataReducer has an error and is no longer really "loading".
-  if (props.error) {
+  if (errors) {
+    const errorCountMessage = (errors.length > 1) ? "Multiple errors occurred" : "An error was encountered";
     return (
       <div className="loading-error">
-        <p>An error was encountered while loading this data:</p>
-        <pre>{props.error.message}</pre>
+        <p>{errorCountMessage} while loading this data:</p>
+        <ul>
+          {errors.map((error, idx) => (
+            <li key={idx}>
+              <pre>{error.message}</pre>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
