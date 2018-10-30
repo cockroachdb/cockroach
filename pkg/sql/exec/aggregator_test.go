819e9d47e91612cc96be4a6b1a545b8d00ef5be4
Alfonso Subiotto Marqués <alfonso@cockroachlabs.com>
2018-10-30 11:38:42 -0400

    exec: add constrained ordered aggregator
    The ordered aggregator currently only performs n sumInt64s on the input
    columns.
    
    To be templated and extended with multiple aggregate functions.
    
    Release note: None

---
+++ pkg/sql/exec/aggregator_test.go
@@ -0,0 +1,280 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package exec
+
+import (
+	"testing"
+
+	"fmt"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/exec/types"
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
+)
+
+var (
+	defaultGroupCols  = []uint32{0}
+	defaultGroupTypes = []types.T{types.Int64}
+	defaultAggCols    = [][]uint32{{1}}
+)
+
+type aggregatorTestCase struct {
+	// groupCols, groupTypes, and aggCols will be set to their default values
+	// before running a test if nil.
+	groupCols  []uint32
+	groupTypes []types.T
+	aggCols    [][]uint32
+	input      tuples
+	expected   tuples
+	// {output}BatchSize if not 0 are passed in to NewOrderedAggregator to
+	// divide input/output batches.
+	batchSize       int
+	outputBatchSize int
+	name            string
+}
+
+func (tc *aggregatorTestCase) init() {
+	groupCols := defaultGroupCols
+	if tc.groupCols == nil {
+		tc.groupCols = groupCols
+	}
+	groupTypes := defaultGroupTypes
+	if tc.groupTypes == nil {
+		tc.groupTypes = groupTypes
+	}
+	aggCols := defaultAggCols
+	if tc.aggCols == nil {
+		tc.aggCols = aggCols
+	}
+	batchSize := ColBatchSize
+	if tc.batchSize == 0 {
+		tc.batchSize = batchSize
+	}
+	outputBatchSize := ColBatchSize
+	if tc.outputBatchSize == 0 {
+		tc.outputBatchSize = outputBatchSize
+	}
+}
+
+func TestAggregatorOneFunc(t *testing.T) {
+	testCases := []aggregatorTestCase{
+		{
+			input: tuples{
+				{0, 1},
+			},
+			expected: tuples{
+				{1},
+			},
+			name:            "OneTuple",
+			outputBatchSize: 4,
+		},
+		{
+			input: tuples{
+				{0, 1},
+				{0, 1},
+			},
+			expected: tuples{
+				{2},
+			},
+			name: "OneGroup",
+		},
+		{
+			input: tuples{
+				{0, 1},
+				{0, 0},
+				{0, 1},
+				{1, 4},
+				{2, 5},
+			},
+			expected: tuples{
+				{2},
+				{4},
+				{5},
+			},
+			batchSize: 2,
+			name:      "MultiGroup",
+		},
+		{
+			input: tuples{
+				{0, 1},
+				{0, 2},
+				{0, 3},
+				{1, 4},
+				{1, 5},
+			},
+			expected: tuples{
+				{6},
+				{9},
+			},
+			batchSize: 1,
+			name:      "CarryBetweenInputBatches",
+		},
+		{
+			input: tuples{
+				{0, 1},
+				{0, 2},
+				{0, 3},
+				{0, 4},
+				{1, 5},
+				{2, 6},
+			},
+			expected: tuples{
+				{10},
+				{5},
+				{6},
+			},
+			batchSize:       2,
+			outputBatchSize: 1,
+			name:            "CarryBetweenOutputBatches",
+		},
+		{
+			input: tuples{
+				{0, 1},
+				{0, 1},
+				{1, 2},
+				{2, 3},
+				{2, 3},
+				{3, 4},
+				{3, 4},
+				{4, 5},
+				{5, 6},
+				{6, 7},
+				{7, 8},
+			},
+			expected: tuples{
+				{2},
+				{2},
+				{6},
+				{8},
+				{5},
+				{6},
+				{7},
+				{8},
+			},
+			batchSize:       4,
+			outputBatchSize: 1,
+			name:            "CarryBetweenInputAndOutputBatches",
+		},
+	}
+
+	// Run tests with deliberate batch sizes and no selection vectors.
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.init()
+
+			tupleSource := newOpTestInput(uint16(tc.batchSize), tc.input)
+			a, err := NewOrderedAggregator(tupleSource, tc.groupCols, tc.aggCols, tc.groupTypes)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			out := newOpTestOutput(a, []int{0}, tc.expected)
+			// Explicitly reinitialize the aggregator with the given output batch
+			// size.
+			a.(*orderedAggregator).initWithBatchSize(tc.batchSize, tc.outputBatchSize)
+			if err := out.Verify(); err != nil {
+				t.Fatal(err)
+			}
+
+			// Run randomized tests on this test case.
+			t.Run(fmt.Sprintf("Randomized"), func(t *testing.T) {
+				runTests(t, []tuples{tc.input}, nil, func(t *testing.T, input []Operator) {
+					a, err := NewOrderedAggregator(input[0], tc.groupCols, tc.aggCols, tc.groupTypes)
+					if err != nil {
+						t.Fatal(err)
+					}
+					out := newOpTestOutput(a, []int{0}, tc.expected)
+					if err := out.Verify(); err != nil {
+						t.Fatal(err)
+					}
+				})
+			})
+		})
+	}
+}
+
+func TestAggregatorMultiFunc(t *testing.T) {
+	testCases := []aggregatorTestCase{
+		{
+			aggCols: [][]uint32{
+				{2}, {1},
+			},
+			input: tuples{
+				{0, 1, 2},
+				{0, 1, 2},
+			},
+			expected: tuples{
+				{4, 2},
+			},
+			name: "OutputOrder",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%s/Randomized", tc.name), func(t *testing.T) {
+			tc.init()
+			runTests(t, []tuples{tc.input}, nil, func(t *testing.T, input []Operator) {
+				a, err := NewOrderedAggregator(input[0], tc.groupCols, tc.aggCols, tc.groupTypes)
+				if err != nil {
+					t.Fatal(err)
+				}
+				out := newOpTestOutput(a, []int{0, 1}, tc.expected)
+				if err := out.Verify(); err != nil {
+					t.Fatal(err)
+				}
+			})
+		})
+	}
+}
+
+func BenchmarkSumInt(b *testing.B) {
+	rng, _ := randutil.NewPseudoRand()
+
+	for _, groupSize := range []int{1, 2, ColBatchSize / 2, ColBatchSize} {
+		for _, numInputBatches := range []int{1, 2, 32, 64} {
+			batch := NewMemBatch([]types.T{types.Int64, types.Int64})
+			groups, ints := batch.ColVec(0).Int64(), batch.ColVec(1).Int64()
+			curGroup := 0
+			for i := 0; i < ColBatchSize; i++ {
+				ints[i] = rng.Int63()
+				groups[i] = int64(curGroup)
+				if groupSize == 1 || i%groupSize == 0 {
+					curGroup++
+				}
+			}
+			batch.SetLength(ColBatchSize)
+			source := newRepeatableBatchSource(batch)
+
+			a, err := NewOrderedAggregator(source, []uint32{0}, [][]uint32{{1}}, []types.T{types.Int64})
+			if err != nil {
+				b.Fatal(err)
+			}
+			a.Init()
+
+			b.Run(
+				fmt.Sprintf("groupSize=%d/numInputBatches=%d", groupSize, numInputBatches),
+				func(b *testing.B) {
+					// Only count the int64 column.
+					b.SetBytes(int64(8 * ColBatchSize * numInputBatches))
+					for i := 0; i < b.N; i++ {
+						a.(*orderedAggregator).Reset()
+						source.resetBatchesToReturn(numInputBatches)
+						// Exhaust aggregator until all batches have been read.
+						for b := a.Next(); b.Length() != 0; b = a.Next() {
+						}
+					}
+				})
+		}
+	}
+}
--- pkg/sql/exec/sum_int64_agg.go
+++ pkg/sql/exec/sum_int64_agg.go
@@ -14,156 +14,74 @@
 
 package exec
 
-import (
-	"github.com/cockroachdb/cockroach/pkg/sql/exec/types"
-)
-
-// sumInt64Agg takes two input columns, one bool column which specifies whether
-// the current position is a new group and a corresponding batch of int64s. It
-// outputs in one column the sum of each group.
 type sumInt64Agg struct {
-	input Operator
+	done bool
 
+	groups  []bool
 	scratch struct {
-		// ColBatch is the batch to return.
-		ColBatch
-		// vec points at the only column in ColBatch that we are updating.
+		curIdx int
+		// vec points to the output vector we are updating.
 		vec []int64
 	}
-
-	// carry is non-zero if we have an accumulated sum from the last batch but we
-	// were unable to output it to the output batch.
-	carry int64
-
-	// nextBatch is set when the batch we are returning hits the size limit. This
-	// variable will point to the remaining upstream batch that needs to be
-	// processed in the next iteration.
-	nextBatch ColBatch
-	resumeIdx uint16
-
-	// outputBatchSize is ColBatchSize by default.
-	outputBatchSize uint16
 }
 
-var _ Operator = &sumInt64Agg{}
-
-func (a *sumInt64Agg) initWithBatchSize(batchSize uint16) {
-	a.scratch.ColBatch = NewMemBatch([]types.T{types.Int64})
-	a.scratch.vec = a.scratch.ColBatch.ColVec(0).Int64()
-	a.outputBatchSize = batchSize
-	a.input.Init()
-}
-
-func (a *sumInt64Agg) Init() {
-	a.initWithBatchSize(ColBatchSize)
-}
-
-// next is a utility function that abstracts the behavior of resuming a batch
-// if the aggregator's output capacity is hit.
-func (a *sumInt64Agg) next() (uint16, ColBatch) {
-	if a.nextBatch != nil {
-		resumeIdx := a.resumeIdx
-		nextBatch := a.nextBatch
-		a.resumeIdx = 0
-		a.nextBatch = nil
-		return resumeIdx, nextBatch
-	}
-	a.resumeIdx = 0
-	return a.resumeIdx, a.input.Next()
-}
+var _ aggregateFunc = &sumInt64Agg{}
 
 var zeroInt64Batch = make([]int64, ColBatchSize)
 
-// Next returns the next ColBatch.
-func (a *sumInt64Agg) Next() ColBatch {
-	// curIdx acts as a cursor over the output batch.
-	curIdx := -1
-	// Re-initialize the output batch.
-	copy(a.scratch.vec, zeroInt64Batch[:a.outputBatchSize])
-	for curIdx < int(a.outputBatchSize) {
-		resumeIdx, batch := a.next()
-		if batch.Length() == 0 {
-			// Finalize the last batch.
-			curIdx++
-			break
+func (a *sumInt64Agg) Init(groups []bool, v ColVec) {
+	a.groups = groups
+	a.scratch.vec = v.Int64()
+	a.Reset()
+}
+
+func (a *sumInt64Agg) Reset() {
+	copy(a.scratch.vec, zeroInt64Batch)
+	a.scratch.curIdx = -1
+}
+
+func (a *sumInt64Agg) CurrentOutputIndex() int {
+	return a.scratch.curIdx
+}
+
+func (a *sumInt64Agg) SetOutputIndex(idx int) {
+	if a.scratch.curIdx != -1 {
+		a.scratch.curIdx = idx
+		copy(a.scratch.vec[idx+1:], zeroInt64Batch)
+	}
+}
+
+func (a *sumInt64Agg) Compute(b ColBatch, inputIdxs []uint32) {
+	if a.done {
+		return
+	}
+	inputLen := b.Length()
+	if inputLen == 0 {
+		// The aggregation is finished. Flush the last value.
+		a.scratch.curIdx++
+		a.done = true
+		return
+	}
+	ints, sel := b.ColVec(int(inputIdxs[0])).Int64(), b.Selection()
+	if sel != nil {
+		sel = sel[:inputLen]
+		for _, i := range sel {
+			x := 0
+			if a.groups[i] {
+				x = 1
+			}
+			a.scratch.curIdx += x
+			a.scratch.vec[a.scratch.curIdx] += ints[i]
 		}
-
-		groups, ints, sel := batch.ColVec(0).Bool(), batch.ColVec(1).Int64(), batch.Selection()
-
-		if curIdx == -1 {
-			// When curIdx == -1, the output batch has not been touched. We need to
-			// determine what state we are in and perform any initialization needed.
-			if resumeIdx != 0 {
-				// We hit a new group in the last iteration and had no capacity to
-				// initialize the sum in the output batch. This initial value is set in
-				// the carry value.
-				a.scratch.vec[0] = a.carry
+	} else {
+		ints = ints[:inputLen]
+		for i := range ints {
+			x := 0
+			if a.groups[i] {
+				x = 1
 			}
-			firstIdx := uint16(0)
-			if len(sel) > 0 {
-				firstIdx = sel[0]
-			}
-			// We overwrite the indicator of a new group if there is one in the groups
-			// to be processed to avoid incrementing curIdx.
-			if groups[firstIdx] {
-				groups[firstIdx] = false
-			}
-			curIdx = 0
-		}
-
-		// toSum represents the last value read from the ints to sum. On every
-		// iteration of the loop, the value that was previously read is added to the
-		// current position in the output batch. The reason one cannot increment the
-		// current index when finding a new group and then write the sum to it is
-		// that in the case where we find a new group when we are at the end of the
-		// current batch, incrementing the index and accessing the output buffer
-		// would result in a panic. In these cases, we save the sum into a.carry
-		// and add it to the new output buffer on the next call to Next().
-		// TODO(asubiotto): Slicing the input column/selection vector would be more
-		// efficient. We could also allocate 2*ColBatchSize for the output batch to
-		// not have to worry about bounds checking the output batch.
-		toSum := int64(0)
-		if sel != nil {
-			for ; resumeIdx < batch.Length() && curIdx < int(a.outputBatchSize); resumeIdx++ {
-				i := sel[resumeIdx]
-				a.scratch.vec[curIdx] += toSum
-				// This if statement is optimized out. See
-				// https://github.com/golang/go/issues/6011#issuecomment-254303032
-				x := 0
-				if groups[i] {
-					x = 1
-				}
-				curIdx += x
-				toSum = ints[i]
-			}
-		} else {
-			i := resumeIdx
-			for ; i < batch.Length() && curIdx < int(a.outputBatchSize); i++ {
-				a.scratch.vec[curIdx] += toSum
-				// This if statement is optimized out. See
-				// https://github.com/golang/go/issues/6011#issuecomment-254303032
-				x := 0
-				if groups[i] {
-					x = 1
-				}
-				curIdx += x
-				toSum = ints[i]
-			}
-			resumeIdx = i
-		}
-
-		if curIdx == int(a.outputBatchSize) {
-			// We hit a new group but do not have space in the output batch to add
-			// it.
-			a.resumeIdx = resumeIdx
-			a.nextBatch = batch
-			a.carry = toSum
-		} else {
-			// curIdx is valid, flush the sum and loop again.
-			a.scratch.vec[curIdx] += toSum
+			a.scratch.curIdx += x
+			a.scratch.vec[a.scratch.curIdx] += ints[i]
 		}
 	}
-
-	a.scratch.SetLength(uint16(curIdx))
-	return a.scratch
 }
