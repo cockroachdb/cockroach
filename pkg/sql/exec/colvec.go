1309e3e24886461479d37e40fc24746553f1f9a2
Alfonso Subiotto Marqués <alfonso@cockroachlabs.com>
2018-11-02 13:54:36 -0400

    exec: add Copy method to ColVec
    Release note: None

--- pkg/sql/exec/colvec.go
+++ pkg/sql/exec/colvec.go
@@ -66,6 +66,9 @@ type ColVec interface {
 	// maximum size of ColBatchSize, filtered by the given selection vector.
 	AppendWithSel(vec ColVec, sel []uint16, batchSize uint16, colType types.T, toLength uint64)
 
+	// Copy copies src[srcStartIdx:srcEndIdx] into this ColVec.
+	Copy(src ColVec, srcStartIdx, srcEndIdx int, typ types.T)
+
 	// CopyWithSelInt64 copies vec, filtered by sel, into this ColVec. It replaces
 	// the contents of this ColVec.
 	CopyWithSelInt64(vec ColVec, sel []uint64, nSel uint16, colType types.T)
