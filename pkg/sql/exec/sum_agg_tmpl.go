eb99d29de606e8f341af697fd9276169c9996804
Solon Gordon <solon@cockroachlabs.com>
2018-11-13 10:29:36 -0500

    exec: switch .og.go suffix to .eg.go
    I accidentally cargo culted the ".og.go" suffix from optgen.
    execgen-generated files now use ".eg.go" to distinguish themselves.
    
    Release note: None

--- pkg/sql/exec/sum_agg_tmpl.go
+++ pkg/sql/exec/sum_agg_tmpl.go
@@ -15,7 +15,7 @@
 // {{/*
 // +build execgen_template
 //
-// This file is the execgen template for sum_agg.og.go. It's formatted in a
+// This file is the execgen template for sum_agg.eg.go. It's formatted in a
 // special way, so it's both valid Go and a valid text/template input. This
 // permits editing this file with editor support.
 //
