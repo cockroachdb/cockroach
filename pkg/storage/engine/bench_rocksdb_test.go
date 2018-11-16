d2c47574f78fad3f202a5d5ded8ffe321b7d4907
Nikhil Benesch <nikhil.benesch@gmail.com>
2018-11-16 15:16:17 -0500

    storage/engine: enforce separation of general and RocksDB benchmarks
    Benchmarks in this package are designed so that they could be run
    against an engine besides RocksDB. To that end, each benchmark is
    supposed to be split into two parts: a general-purpose function that
    uses only the Engine interface that lives in bench_test.go, and a
    driver function that calls that function with a RocksDB engine in
    bench_rocksdb_test.go.
    
    Several benchmarks were disobeying this convention. Move them between
    files to match the convention as best as possible. A future commit will
    clean up some particularly bad offenders. This commit is pure code
    movement.
    
    Release note: None

--- pkg/storage/engine/bench_rocksdb_test.go
+++ pkg/storage/engine/bench_rocksdb_test.go
@@ -15,13 +15,20 @@
 package engine
 
 import (
+	"context"
 	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/storage/engine/enginepb"
 	"github.com/cockroachdb/cockroach/pkg/util/encoding"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 )
 
 func setupMVCCRocksDB(b testing.TB, dir string) Engine {
@@ -220,6 +227,29 @@ func BenchmarkMVCCBlindInitPut_RocksDB(b *testing.B) {
 	}
 }
 
+func BenchmarkMVCCPutDelete_RocksDB(b *testing.B) {
+	rocksdb := setupMVCCInMemRocksDB(b, "put_delete")
+	defer rocksdb.Close()
+
+	r := rand.New(rand.NewSource(timeutil.Now().UnixNano()))
+	value := roachpb.MakeValueFromBytes(randutil.RandBytes(r, 10))
+	var blockNum int64
+
+	for i := 0; i < b.N; i++ {
+		blockID := r.Int63()
+		blockNum++
+		key := encoding.EncodeVarintAscending(nil, blockID)
+		key = encoding.EncodeVarintAscending(key, blockNum)
+
+		if err := MVCCPut(context.Background(), rocksdb, nil, key, hlc.Timestamp{}, value, nil /* txn */); err != nil {
+			b.Fatal(err)
+		}
+		if err := MVCCDelete(context.Background(), rocksdb, nil, key, hlc.Timestamp{}, nil /* txn */); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
 func BenchmarkMVCCBatchPut_RocksDB(b *testing.B) {
 	for _, valueSize := range []int{10} {
 		b.Run(fmt.Sprintf("valueSize=%d", valueSize), func(b *testing.B) {
@@ -240,6 +270,23 @@ func BenchmarkMVCCBatchTimeSeries_RocksDB(b *testing.B) {
 	}
 }
 
+// BenchmarkMVCCMergeTimeSeries computes performance of merging time series
+// data. Uses an in-memory engine.
+func BenchmarkMVCCMergeTimeSeries_RocksDB(b *testing.B) {
+	ts := &roachpb.InternalTimeSeriesData{
+		StartTimestampNanos: 0,
+		SampleDurationNanos: 1000,
+		Samples: []roachpb.InternalTimeSeriesSample{
+			{Offset: 0, Count: 1, Sum: 5.0},
+		},
+	}
+	var value roachpb.Value
+	if err := value.SetProto(ts); err != nil {
+		b.Fatal(err)
+	}
+	runMVCCMerge(b, setupMVCCInMemRocksDB, &value, 1024)
+}
+
 // DeleteRange benchmarks below (using on-disk data).
 
 func BenchmarkMVCCDeleteRange_RocksDB(b *testing.B) {
@@ -249,6 +296,122 @@ func BenchmarkMVCCDeleteRange_RocksDB(b *testing.B) {
 	for _, valueSize := range []int{8, 32, 256} {
 		b.Run(fmt.Sprintf("valueSize=%d", valueSize), func(b *testing.B) {
 			runMVCCDeleteRange(b, setupMVCCRocksDB, valueSize)
+		})
+	}
+}
+
+func BenchmarkClearRange_RocksDB(b *testing.B) {
+	if testing.Short() {
+		b.Skip("TODO: fix benchmark")
+	}
+	runBenchmarkClearRange(b, func(eng Engine, batch Batch, start, end MVCCKey) error {
+		return batch.ClearRange(start, end)
+	})
+}
+
+func BenchmarkClearIterRange_RocksDB(b *testing.B) {
+	runBenchmarkClearRange(b, func(eng Engine, batch Batch, start, end MVCCKey) error {
+		iter := eng.NewIterator(IterOptions{UpperBound: roachpb.KeyMax})
+		defer iter.Close()
+		return batch.ClearIterRange(iter, start, end)
+	})
+}
+
+func BenchmarkMVCCGarbageCollect(b *testing.B) {
+	if testing.Short() {
+		b.Skip("short flag")
+	}
+	defer leaktest.AfterTest(b)()
+	ctx := context.Background()
+	ts := hlc.Timestamp{}.Add(time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC).UnixNano(), 0)
+
+	// Write 'numKeys' of the given 'keySize' and 'valSize' to the given engine.
+	// For each key, write 'numVersions' versions, and add a GCRequest_GCKey to
+	// the returned slice that affects the oldest 'deleteVersions' versions. The
+	// first write for each key will be at `ts`, the second one at `ts+(0,1)`,
+	// etc.
+	//
+	// NB: a real invocation of MVCCGarbageCollect typically has most of the keys
+	// in sorted order. Here they will be ordered randomly.
+	setup := func(
+		engine Engine, keySize, valSize, numKeys, numVersions, deleteVersions int,
+	) []roachpb.GCRequest_GCKey {
+		var ms enginepb.MVCCStats
+		var gcKeys []roachpb.GCRequest_GCKey
+
+		mkKey := func() []byte {
+			k := make([]byte, keySize)
+			if n, err := rand.Read(k); err != nil || n != keySize {
+				b.Fatalf("error or read too little: n=%d err=%v", n, err)
+			}
+			return k
+		}
+		valBytes := make([]byte, valSize)
+		if n, err := rand.Read(valBytes); err != nil || n != valSize {
+			b.Fatalf("error or read too little: n=%d err=%v", n, err)
+		}
+		v := roachpb.MakeValueFromBytesAndTimestamp(valBytes, hlc.Timestamp{})
+
+		for j := 0; j < numKeys; j++ {
+			curKey := mkKey()
+			if deleteVersions > 0 {
+				gcKeys = append(gcKeys, roachpb.GCRequest_GCKey{
+					Timestamp: ts.Add(0, int32(deleteVersions-1)),
+					Key:       curKey,
+				})
+			}
+
+			for i := 0; i < numVersions; i++ {
+				if err := MVCCPut(
+					ctx, engine, &ms, curKey, ts.Add(0, int32(i)), v, nil); err != nil {
+					b.Fatal(err)
+				}
+			}
+		}
+		return gcKeys
+	}
+
+	// We write values at ts+(0,i), set now=ts+(1,0) so that we're ahead of all
+	// the writes. This value doesn't matter in practice, it's used only for
+	// stats updates.
+	now := ts.Add(1, 0)
+
+	run := func(
+		b *testing.B, keySize, valSize, numKeys, numVersions, deleteVersions int,
+	) {
+		engine := createTestEngine()
+		defer engine.Close()
+
+		for i := 0; i < b.N; i++ {
+			b.StopTimer()
+			gcKeys := setup(engine, keySize, valSize, numKeys, numVersions, deleteVersions)
+
+			b.StartTimer()
+			if err := MVCCGarbageCollect(
+				ctx, engine, &enginepb.MVCCStats{}, gcKeys, now,
+			); err != nil {
+				b.Fatal(err)
+			}
+		}
+	}
+
+	// NB: To debug #16068, test only 128-128-15000-6.
+	for _, keySize := range []int{128} {
+		b.Run(fmt.Sprintf("keySize=%d", keySize), func(b *testing.B) {
+			for _, valSize := range []int{128} {
+				b.Run(fmt.Sprintf("valSize=%d", valSize), func(b *testing.B) {
+					for _, numKeys := range []int{1, 1024} {
+						b.Run(fmt.Sprintf("numKeys=%d", numKeys), func(b *testing.B) {
+							for _, numVersions := range []int{2, 1024} {
+								b.Run(fmt.Sprintf("numVersions=%d", numVersions), func(b *testing.B) {
+									deleteVersions := numVersions - 1
+									run(b, keySize, valSize, numKeys, numVersions, deleteVersions)
+								})
+							}
+						})
+					}
+				})
+			}
 		})
 	}
 }
--- pkg/storage/engine/bench_test.go
+++ pkg/storage/engine/bench_test.go
@@ -24,14 +24,12 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/storage/engine/enginepb"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/util/encoding"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
-	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
@@ -509,23 +507,6 @@ func runMVCCMerge(b *testing.B, emk engineMaker, value *roachpb.Value, numKeys i
 	b.StopTimer()
 }
 
-// BenchmarkMVCCMergeTimeSeries computes performance of merging time series data.
-// Uses an in-memory engine.
-func BenchmarkMVCCMergeTimeSeries_RocksDB(b *testing.B) {
-	ts := &roachpb.InternalTimeSeriesData{
-		StartTimestampNanos: 0,
-		SampleDurationNanos: 1000,
-		Samples: []roachpb.InternalTimeSeriesSample{
-			{Offset: 0, Count: 1, Sum: 5.0},
-		},
-	}
-	var value roachpb.Value
-	if err := value.SetProto(ts); err != nil {
-		b.Fatal(err)
-	}
-	runMVCCMerge(b, setupMVCCInMemRocksDB, &value, 1024)
-}
-
 func copyDir(from, to string) error {
 	return filepath.Walk(from, func(srcPath string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -697,29 +678,6 @@ func runBatchApplyBatchRepr(
 	b.StopTimer()
 }
 
-func BenchmarkMVCCPutDelete_RocksDB(b *testing.B) {
-	rocksdb := setupMVCCInMemRocksDB(b, "put_delete")
-	defer rocksdb.Close()
-
-	r := rand.New(rand.NewSource(timeutil.Now().UnixNano()))
-	value := roachpb.MakeValueFromBytes(randutil.RandBytes(r, 10))
-	var blockNum int64
-
-	for i := 0; i < b.N; i++ {
-		blockID := r.Int63()
-		blockNum++
-		key := encoding.EncodeVarintAscending(nil, blockID)
-		key = encoding.EncodeVarintAscending(key, blockNum)
-
-		if err := MVCCPut(context.Background(), rocksdb, nil, key, hlc.Timestamp{}, value, nil /* txn */); err != nil {
-			b.Fatal(err)
-		}
-		if err := MVCCDelete(context.Background(), rocksdb, nil, key, hlc.Timestamp{}, nil /* txn */); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
 func runBenchmarkClearRange(
 	b *testing.B, clearRange func(e Engine, b Batch, start, end MVCCKey) error,
 ) {
@@ -749,120 +707,4 @@ func runBenchmarkClearRange(
 	}
 
 	b.StopTimer()
-}
-
-func BenchmarkClearRange_RocksDB(b *testing.B) {
-	if testing.Short() {
-		b.Skip("TODO: fix benchmark")
-	}
-	runBenchmarkClearRange(b, func(eng Engine, batch Batch, start, end MVCCKey) error {
-		return batch.ClearRange(start, end)
-	})
-}
-
-func BenchmarkClearIterRange_RocksDB(b *testing.B) {
-	runBenchmarkClearRange(b, func(eng Engine, batch Batch, start, end MVCCKey) error {
-		iter := eng.NewIterator(IterOptions{UpperBound: roachpb.KeyMax})
-		defer iter.Close()
-		return batch.ClearIterRange(iter, start, end)
-	})
-}
-
-func BenchmarkMVCCGarbageCollect(b *testing.B) {
-	if testing.Short() {
-		b.Skip("short flag")
-	}
-	defer leaktest.AfterTest(b)()
-	ctx := context.Background()
-	ts := hlc.Timestamp{}.Add(time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC).UnixNano(), 0)
-
-	// Write 'numKeys' of the given 'keySize' and 'valSize' to the given engine.
-	// For each key, write 'numVersions' versions, and add a GCRequest_GCKey to
-	// the returned slice that affects the oldest 'deleteVersions' versions. The
-	// first write for each key will be at `ts`, the second one at `ts+(0,1)`,
-	// etc.
-	//
-	// NB: a real invocation of MVCCGarbageCollect typically has most of the keys
-	// in sorted order. Here they will be ordered randomly.
-	setup := func(
-		engine Engine, keySize, valSize, numKeys, numVersions, deleteVersions int,
-	) []roachpb.GCRequest_GCKey {
-		var ms enginepb.MVCCStats
-		var gcKeys []roachpb.GCRequest_GCKey
-
-		mkKey := func() []byte {
-			k := make([]byte, keySize)
-			if n, err := rand.Read(k); err != nil || n != keySize {
-				b.Fatalf("error or read too little: n=%d err=%v", n, err)
-			}
-			return k
-		}
-		valBytes := make([]byte, valSize)
-		if n, err := rand.Read(valBytes); err != nil || n != valSize {
-			b.Fatalf("error or read too little: n=%d err=%v", n, err)
-		}
-		v := roachpb.MakeValueFromBytesAndTimestamp(valBytes, hlc.Timestamp{})
-
-		for j := 0; j < numKeys; j++ {
-			curKey := mkKey()
-			if deleteVersions > 0 {
-				gcKeys = append(gcKeys, roachpb.GCRequest_GCKey{
-					Timestamp: ts.Add(0, int32(deleteVersions-1)),
-					Key:       curKey,
-				})
-			}
-
-			for i := 0; i < numVersions; i++ {
-				if err := MVCCPut(
-					ctx, engine, &ms, curKey, ts.Add(0, int32(i)), v, nil); err != nil {
-					b.Fatal(err)
-				}
-			}
-		}
-		return gcKeys
-	}
-
-	// We write values at ts+(0,i), set now=ts+(1,0) so that we're ahead of all
-	// the writes. This value doesn't matter in practice, it's used only for
-	// stats updates.
-	now := ts.Add(1, 0)
-
-	run := func(
-		b *testing.B, keySize, valSize, numKeys, numVersions, deleteVersions int,
-	) {
-		engine := createTestEngine()
-		defer engine.Close()
-
-		for i := 0; i < b.N; i++ {
-			b.StopTimer()
-			gcKeys := setup(engine, keySize, valSize, numKeys, numVersions, deleteVersions)
-
-			b.StartTimer()
-			if err := MVCCGarbageCollect(
-				ctx, engine, &enginepb.MVCCStats{}, gcKeys, now,
-			); err != nil {
-				b.Fatal(err)
-			}
-		}
-	}
-
-	// NB: To debug #16068, test only 128-128-15000-6.
-	for _, keySize := range []int{128} {
-		b.Run(fmt.Sprintf("keySize=%d", keySize), func(b *testing.B) {
-			for _, valSize := range []int{128} {
-				b.Run(fmt.Sprintf("valSize=%d", valSize), func(b *testing.B) {
-					for _, numKeys := range []int{1, 1024} {
-						b.Run(fmt.Sprintf("numKeys=%d", numKeys), func(b *testing.B) {
-							for _, numVersions := range []int{2, 1024} {
-								b.Run(fmt.Sprintf("numVersions=%d", numVersions), func(b *testing.B) {
-									deleteVersions := numVersions - 1
-									run(b, keySize, valSize, numKeys, numVersions, deleteVersions)
-								})
-							}
-						})
-					}
-				})
-			}
-		})
-	}
 }
