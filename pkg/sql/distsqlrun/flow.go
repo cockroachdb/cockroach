24bc4c8857e76ac682287f17de3e2dfa6aa715a5
David Taylor <tinystatemachine@gmail.com>
2018-11-19 14:21:18 +0000

    sql: plumb BulkAdder though distsql
    Since the implementation uses RocksDB, we want to inject it and only depend on an interface.
    This keeps SQL from depending directly on RocksDB and the rest of the c++ build.
    
    Release note: none.

--- pkg/sql/distsqlrun/flow.go
+++ pkg/sql/distsqlrun/flow.go
@@ -31,6 +31,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlutil"
 	"github.com/cockroachdb/cockroach/pkg/storage/diskmap"
+	"github.com/cockroachdb/cockroach/pkg/storage/storagebase"
 	"github.com/cockroachdb/cockroach/pkg/util/contextutil"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/mon"
@@ -98,6 +99,11 @@ type FlowCtx struct {
 	// This is not supposed to be used as a general engine.Engine and thus
 	// one should sparingly use the set of features offered by it.
 	TempStorage diskmap.Factory
+
+	// BulkAdder is used by backfill/bulk-ingestion processors to ingest large
+	// amounts of data in bulk as SSTs.
+	BullkAdder storagebase.BulkAdderFactory
+
 	// diskMonitor is used to monitor temporary storage disk usage.
 	diskMonitor *mon.BytesMonitor
 
---
+++ pkg/storage/storagebase/bulk_adder.go
@@ -0,0 +1,40 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package storagebase
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/internal/client"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+)
+
+// BulkAdderFactory describes a factory function for BulkAdders.
+type BulkAdderFactory func(
+	ctx context.Context, db *client.DB, flushBytes int64, timestamp hlc.Timestamp,
+) (BulkAdder, error)
+
+// BulkAdder describes a bulk-adding helper that can be used to add lots of KVs.
+type BulkAdder interface {
+	// Add adds a KV pair to the adder's buffer, potentially flushing if needed.
+	Add(ctx context.Context, key roachpb.Key, value []byte) error
+	// Flush explicitly flushes anything remaining in the adder's buffer.
+	Flush(ctx context.Context) error
+	// GetSummary returns a summary of rows/bytes/etc written by this batcher.
+	GetSummary() roachpb.BulkOpSummary
+	// Close closes the underlying buffers/writers.
+	Close()
+}
