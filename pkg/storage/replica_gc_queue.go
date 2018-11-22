c290a7eca345266031a51e6ae856ad24bf19e826
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2018-11-22 12:13:04 +0100

    storage: log info when gc'ing recent preemptive snap
    GC'ing a preemptive snapshot is necessary if the corresponding
    replication change failed, but if it didn't, it causes a Raft snapshot.
    Unfortunately, it's impossible to decide which one is which based on
    the local information. Even worse, an obsolete preemptive snapshot can
    also block required overlapping snapshots, so simply delaying the GC
    is also a dangerous option. For now, log at INFO (in principle these
    events can already be grepped from the logs, but I expect the
    explicitness to be useful still).
    
    Release note: None

--- pkg/storage/replica_gc_queue.go
+++ pkg/storage/replica_gc_queue.go
@@ -227,7 +227,28 @@ func (rgcq *replicaGCQueue) process(
 		// We are no longer a member of this range, but the range still exists.
 		// Clean up our local data.
 
-		if replyDesc.EndKey.Less(desc.EndKey) {
+		repl.mu.RLock()
+		replicaID := repl.mu.replicaID
+		ticks := repl.mu.ticks
+		repl.mu.RUnlock()
+
+		if replicaID == 0 {
+			// This is a preemptive replica. GC'ing a preemptive replica is a
+			// good idea if and only if the up-replication that it was a part of
+			// did *NOT* commit. If it *did* commit and we're removing the
+			// preemptive snapshot, the newly added follower will first need a
+			// Raft snapshot to catch up, and that snapshot will be delayed by
+			// #31875.
+			// Log if the replica hasn't been around for very long.
+			//
+			// TODO(tschottdorf): avoid these, ideally without a time-based mechanism.
+			// The replica carrying out the replication change could keep the
+			// snapshot alive until it has either committed or aborted the txn.
+			// Or we try to use Raft learners for this purpose.
+			if ticks < 10 {
+				log.Infof(ctx, "removing young preemptive snapshot (%d ticks)", ticks)
+			}
+		} else if replyDesc.EndKey.Less(desc.EndKey) {
 			// The meta records indicate that the range has split but that this
 			// replica hasn't processed the split trigger yet. By removing this
 			// replica, we're also wiping out the data of what would become the
