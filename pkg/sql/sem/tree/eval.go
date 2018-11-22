4120b61edbfcfa5f6c36bc0f52b72f381089ee89
Raphael 'kena' Poss <knz@cockroachlabs.com>
2018-11-22 16:49:50 +0100

    sql: replace references to #2115 by #32551
    Release note: None

--- pkg/sql/sem/tree/eval.go
+++ pkg/sql/sem/tree/eval.go
@@ -3408,7 +3408,7 @@ func (expr *IndirectionExpr) Eval(ctx *EvalContext) (Datum, error) {
 	var subscriptIdx int
 	for i, t := range expr.Indirection {
 		if t.Slice {
-			return nil, pgerror.UnimplementedWithIssueErrorf(2115, "ARRAY slicing in %s", expr)
+			return nil, pgerror.UnimplementedWithIssueErrorf(32551, "ARRAY slicing in %s", expr)
 		}
 		if i > 0 {
 			return nil, pgerror.UnimplementedWithIssueErrorf(2115, "multidimensional ARRAY %s", expr)
