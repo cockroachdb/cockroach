658f069b888379c386044c83030a72ab8b3cd502
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2018-11-23 11:07:48 +0100

    storage: improve raftDescribeMessage RejectHint handling
    The RejectHint is always used. See:
    
    https://github.com/cockroachdb/cockroach/pull/32533#issuecomment-441199252
    
    Release note: None

--- pkg/storage/raft.go
+++ pkg/storage/raft.go
@@ -147,10 +147,7 @@ func raftDescribeMessage(m raftpb.Message, f raft.EntryFormatter) string {
 	var buf bytes.Buffer
 	fmt.Fprintf(&buf, "%x->%x %v Term:%d Log:%d/%d", m.From, m.To, m.Type, m.Term, m.LogTerm, m.Index)
 	if m.Reject {
-		fmt.Fprintf(&buf, " Rejected")
-		if m.RejectHint != 0 {
-			fmt.Fprintf(&buf, "(Hint:%d)", m.RejectHint)
-		}
+		fmt.Fprintf(&buf, " Rejected (Hint: %d)", m.RejectHint)
 	}
 	if m.Commit != 0 {
 		fmt.Fprintf(&buf, " Commit:%d", m.Commit)
