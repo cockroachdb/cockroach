77ceb39a9a38b3e27e742e7934f25eed854f6b28
Masha Schneider <masha@cockroachlabs.com>
2018-11-26 14:46:49 -0500

    cli: add cpu benchmark to cockroach
    Release note: None

--- pkg/cli/systembench/disk_bench.go
+++ pkg/cli/systembench/disk_bench.go
@@ -18,10 +18,8 @@ import (
 	"context"
 	"fmt"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
-	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/cockroach/pkg/util/sysutil"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
-	"github.com/codahale/hdrhistogram"
 	"github.com/pkg/errors"
 	"golang.org/x/sync/errgroup"
 	"io/ioutil"
@@ -33,13 +31,8 @@ import (
 )
 
 const (
-	minLatency  = 100 * time.Microsecond
-	maxLatency  = 10 * time.Second
 	maxFileSize = 1 << 30 // A gigabyte
 )
-
-var numOps uint64
-var numBytes uint64
 
 // DiskBenchmarkType represents an I/O benchmark.
 type DiskBenchmarkType int
@@ -61,22 +54,10 @@ type DiskOptions struct {
 	Type DiskBenchmarkType
 }
 
-// latency is a histogram for the latency of a single worker.
-type latency struct {
-	syncutil.Mutex
-	*hdrhistogram.WindowedHistogram
-}
-
-// worker represents a single worker process generating load.
-type worker interface {
-	getLatencyHistogram() *latency
-	run(ctx context.Context) error
-}
-
 // workerSeqWrite holds a temp file and random byte array to write to
 // the temp file.
 type workerSeqWrite struct {
-	latency latency
+	latency *namedHistogram
 	data    []byte
 
 	tempFileDir  string
@@ -85,7 +66,9 @@ type workerSeqWrite struct {
 
 // newWorkerSeqWrite creates a worker that writes writeSize sized
 // sequential blocks with fsync every syncInterval bytes to a file.
-func newWorkerSeqWrite(ctx context.Context, diskOptions *DiskOptions) worker {
+func newWorkerSeqWrite(
+	ctx context.Context, diskOptions *DiskOptions, registry *histogramRegistry,
+) worker {
 	data := make([]byte, diskOptions.WriteSize)
 	_, err := rand.Read(data)
 	if err != nil {
@@ -96,8 +79,7 @@ func newWorkerSeqWrite(ctx context.Context, diskOptions *DiskOptions) worker {
 		tempFileDir:  diskOptions.Dir,
 		syncInterval: diskOptions.SyncInterval,
 	}
-	w.latency.WindowedHistogram = hdrhistogram.NewWindowed(1,
-		minLatency.Nanoseconds(), maxLatency.Nanoseconds(), 1)
+	w.latency = registry.Register("ops")
 	return w
 }
 
@@ -149,18 +131,13 @@ func (w *workerSeqWrite) run(ctx context.Context) error {
 		bytes := uint64(writeSize)
 		atomic.AddUint64(&numOps, 1)
 		atomic.AddUint64(&numBytes, bytes)
-		elapsed := timeutil.Since(start)
-		w.latency.Lock()
-		if err := w.latency.Current.RecordValue(elapsed.Nanoseconds()); err != nil {
-			return err
-		}
-		w.latency.Unlock()
+		w.latency.Record(timeutil.Since(start))
 	}
 }
 
 // workerSeqWrite implements the worker interface.
-func (w *workerSeqWrite) getLatencyHistogram() *latency {
-	return &w.latency
+func (w *workerSeqWrite) getLatencyHistogram() *namedHistogram {
+	return w.latency
 }
 
 // newTempFile creates a temporary file, closes it and
@@ -183,6 +160,7 @@ func newTempFile(dir string) (*os.File, error) {
 // Run runs I/O benchmarks specified by diskOpts.
 func Run(diskOpts DiskOptions) error {
 	ctx := context.Background()
+	reg := newHistogramRegistry()
 
 	// Check if the directory exists.
 	_, err := os.Stat(diskOpts.Dir)
@@ -193,7 +171,7 @@ func Run(diskOpts DiskOptions) error {
 	log.Infof(ctx, "writing to %s\n", diskOpts.Dir)
 
 	workers := make([]worker, diskOpts.Concurrency)
-	var workerCreator func(ctx context.Context, diskOptions *DiskOptions) worker
+	var workerCreator func(ctx context.Context, diskOptions *DiskOptions, registry *histogramRegistry) worker
 
 	switch diskOpts.Type {
 	case SeqWriteTest:
@@ -209,7 +187,7 @@ func Run(diskOpts DiskOptions) error {
 	defer cancel()
 
 	for i := range workers {
-		workers[i] = workerCreator(ctx, &diskOpts)
+		workers[i] = workerCreator(ctx, &diskOpts, reg)
 	}
 
 	for i := range workers {
@@ -245,37 +223,9 @@ func Run(diskOpts DiskOptions) error {
 	var lastOps uint64
 	var lastBytes uint64
 
-	// This is a histogram that combines the output for multiple workers.
-	var cumulative *hdrhistogram.Histogram
-
 	for i := 0; ; i++ {
 		select {
 		case <-ticker.C:
-			var h *hdrhistogram.Histogram
-			for _, w := range workers {
-				workerLatency := w.getLatencyHistogram()
-				workerLatency.Lock()
-				m := workerLatency.Merge()
-				workerLatency.Rotate()
-				workerLatency.Unlock()
-				if h == nil {
-					h = m
-				} else {
-					h.Merge(m)
-				}
-			}
-
-			if cumulative == nil {
-				cumulative = h
-			} else {
-				cumulative.Merge(h)
-			}
-
-			p50 := h.ValueAtQuantile(50)
-			p95 := h.ValueAtQuantile(95)
-			p99 := h.ValueAtQuantile(99)
-			pMax := h.ValueAtQuantile(100)
-
 			now := timeutil.Now()
 			elapsed := now.Sub(lastNow)
 			ops := atomic.LoadUint64(&numOps)
@@ -284,15 +234,18 @@ func Run(diskOpts DiskOptions) error {
 			if i%20 == 0 {
 				fmt.Println("_elapsed____ops/sec___mb/sec__p50(ms)__p95(ms)__p99(ms)_pMax(ms)")
 			}
-			fmt.Printf("%8s %10.1f %8.1f %8.1f %8.1f %8.1f %8.1f\n",
-				time.Duration(timeutil.Since(start).Seconds()+0.5)*time.Second,
-				float64(ops-lastOps)/elapsed.Seconds(),
-				float64(bytes-lastBytes)/(1024.0*1024.0)/elapsed.Seconds(),
-				time.Duration(p50).Seconds()*1000,
-				time.Duration(p95).Seconds()*1000,
-				time.Duration(p99).Seconds()*1000,
-				time.Duration(pMax).Seconds()*1000,
-			)
+			reg.Tick(func(tick histogramTick) {
+				h := tick.Hist
+				fmt.Printf("%8s %10.1f %8.1f %8.1f %8.1f %8.1f %8.1f\n",
+					time.Duration(timeutil.Since(start).Seconds()+0.5)*time.Second,
+					float64(ops-lastOps)/elapsed.Seconds(),
+					float64(bytes-lastBytes)/(1024.0*1024.0)/elapsed.Seconds(),
+					time.Duration(h.ValueAtQuantile(50)).Seconds()*1000,
+					time.Duration(h.ValueAtQuantile(95)).Seconds()*1000,
+					time.Duration(h.ValueAtQuantile(99)).Seconds()*1000,
+					time.Duration(h.ValueAtQuantile(100)).Seconds()*1000,
+				)
+			})
 			lastNow = now
 			lastOps = ops
 			lastBytes = bytes
@@ -302,16 +255,18 @@ func Run(diskOpts DiskOptions) error {
 			startElapsed := timeutil.Since(start)
 			const totalHeader = "\n_elapsed____ops(total)__mb(total)__avg(ms)__p50(ms)__p95(ms)__p99(ms)_pMax(ms)"
 			fmt.Println(totalHeader + `__total`)
-			fmt.Printf("%8s %13d %10.1f %8.1f %8.1f %8.1f %8.1f %8.1f\n",
-				time.Duration(startElapsed.Seconds())*time.Second,
-				atomic.LoadUint64(&numOps),
-				float64(atomic.LoadUint64(&numBytes)/(1024.0*1024.0)),
-				time.Duration(cumulative.Mean()).Seconds()*1000,
-				time.Duration(cumulative.ValueAtQuantile(50)).Seconds()*1000,
-				time.Duration(cumulative.ValueAtQuantile(95)).Seconds()*1000,
-				time.Duration(cumulative.ValueAtQuantile(99)).Seconds()*1000,
-				time.Duration(cumulative.ValueAtQuantile(100)).Seconds()*1000,
-			)
+			reg.Tick(func(tick histogramTick) {
+				h := tick.Cumulative
+				fmt.Printf("%8s %13d %10.1f %8.1f %8.1f %8.1f %8.1f %8.1f\n",
+					time.Duration(startElapsed.Seconds())*time.Second,
+					atomic.LoadUint64(&numOps),
+					float64(atomic.LoadUint64(&numBytes)/(1024.0*1024.0)),
+					time.Duration(h.Mean()).Seconds()*1000,
+					time.Duration(h.ValueAtQuantile(50)).Seconds()*1000,
+					time.Duration(h.ValueAtQuantile(95)).Seconds()*1000,
+					time.Duration(h.ValueAtQuantile(99)).Seconds()*1000,
+					time.Duration(h.ValueAtQuantile(100)).Seconds()*1000)
+			})
 			return nil
 		case err := <-errs:
 			return err
