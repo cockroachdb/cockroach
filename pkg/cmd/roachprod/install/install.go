3ba9cd5aa68698e0cbb66b7d39ac3f6042dc6dec
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2018-11-27 21:46:28 -0500

    roachtest: add sysbench roachtests
    Closes #24507.
    
    This change introduces 4 new roachtest variants. These run each
    of the non-transactional sysbench workloads.
    
    Next steps will be to hook the results up to roachperf. We can
    also add `roachtest bench` variants of this setup.
    
    Release note: None

--- pkg/cmd/roachprod/install/install.go
+++ pkg/cmd/roachprod/install/install.go
@@ -131,6 +131,12 @@ sudo apt-get install -y \
   ntpdate;
 `,
 
+	"sysbench": `
+curl -s https://packagecloud.io/install/repositories/akopytov/sysbench/script.deb.sh | sudo bash;
+sudo apt-get update;
+sudo apt-get install -y sysbench;
+`,
+
 	"tools": `
 sudo apt-get update;
 sudo apt-get install -y \
