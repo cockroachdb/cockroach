c1d96b60d4903debe1290708839c799f280eea42
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2018-11-29 10:41:30 +0100

    backupccl: return better error on failed backup
    The previous code would tend to return a context cancellation error when
    a backup operation failed due to an error originating from an
    ExportRequest, leaving no clue about the original failure condition
    (this cost us a few hours today to discover when a user ran into it).
    
    The root problem here is that `g.Err()` isn't supposed to exist and The
    methods invoked by `Go` already have a `ctx` which they should use
    instead, and outside of that you almost certainly want to check `Wait()`
    to get a better error. Correspondingly, `g.Err()` has been removed to
    avoid this pitfall in the future. This affected some other locations in
    the codebase.
    
    Release note: None

---
+++ pkg/util/ctxgroup/ctxgroup_test.go
@@ -0,0 +1,48 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package ctxgroup
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+)
+
+func TestErrorAfterCancel(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	testutils.RunTrueAndFalse(t, "canceled", func(t *testing.T, canceled bool) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		g := WithContext(ctx)
+		g.Go(func() error {
+			return nil
+		})
+		expErr := context.Canceled
+		if !canceled {
+			expErr = nil
+		} else {
+			cancel()
+		}
+
+		if err := g.Wait(); err != expErr {
+			t.Errorf("expected %v, got %v", expErr, err)
+		}
+	})
+}
