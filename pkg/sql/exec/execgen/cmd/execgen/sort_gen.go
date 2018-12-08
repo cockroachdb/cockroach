480b1e4302d0ac0faa52eca32186ca926e73a96f
Jordan Lewis <jordanthelewis@gmail.com>
2018-12-07 19:03:54 -0500

    exec: add support for descending sorts
    This extends the sort templates by 2x, adding a descending variant for
    every column sorter.
    
    Release note: None

--- pkg/sql/exec/execgen/cmd/execgen/sort_gen.go
+++ pkg/sql/exec/execgen/cmd/execgen/sort_gen.go
@@ -21,8 +21,23 @@ import (
 	"strings"
 	"text/template"
 
+	"github.com/cockroachdb/cockroach/pkg/sql/exec/types"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 )
+
+type sortOverload struct {
+	*overload
+	Dir       string
+	DirString string
+}
+
+type sortOverloads struct {
+	LTyp      types.T
+	Overloads []sortOverload
+}
+
+// sortOverloads maps type to distsqlpb.Ordering_Column_Direction to overload.
+var typesToSortOverloads map[types.T]sortOverloads
 
 func genSortOps(wr io.Writer) error {
 	d, err := ioutil.ReadFile("pkg/sql/exec/sort_tmpl.go")
@@ -36,6 +51,8 @@ func genSortOps(wr io.Writer) error {
 	s = strings.Replace(s, "_GOTYPE", "{{.LTyp.GoTypeName}}", -1)
 	s = strings.Replace(s, "_TYPES_T", "types.{{.LTyp}}", -1)
 	s = strings.Replace(s, "_TYPE", "{{.LTyp}}", -1)
+	s = strings.Replace(s, "_DIR_ENUM", "{{.Dir}}", -1)
+	s = strings.Replace(s, "_DIR", "{{.DirString}}", -1)
 	s = strings.Replace(s, "_TemplateType", "{{.LTyp}}", -1)
 
 	assignLtRe := regexp.MustCompile(`_ASSIGN_LT\((.*),(.*),(.*)\)`)
@@ -47,7 +64,7 @@ func genSortOps(wr io.Writer) error {
 		return err
 	}
 
-	return tmpl.Execute(wr, comparisonOpToOverloads[tree.LT])
+	return tmpl.Execute(wr, typesToSortOverloads)
 }
 
 func genQuickSortOps(wr io.Writer) error {
@@ -60,6 +77,7 @@ func genQuickSortOps(wr io.Writer) error {
 
 	// Replace the template variables.
 	s = strings.Replace(s, "_TYPE", "{{.LTyp}}", -1)
+	s = strings.Replace(s, "_DIR", "{{.DirString}}", -1)
 
 	// Now, generate the op, from the template.
 	tmpl, err := template.New("quicksort").Parse(s)
@@ -67,10 +85,23 @@ func genQuickSortOps(wr io.Writer) error {
 		return err
 	}
 
-	return tmpl.Execute(wr, comparisonOpToOverloads[tree.LT])
+	return tmpl.Execute(wr, typesToSortOverloads)
 }
 
 func init() {
 	registerGenerator(genSortOps, "sort.eg.go")
 	registerGenerator(genQuickSortOps, "quicksort.eg.go")
+	typesToSortOverloads = make(map[types.T]sortOverloads)
+	for _, o := range comparisonOpToOverloads[tree.LT] {
+		typesToSortOverloads[o.LTyp] = sortOverloads{
+			LTyp: o.LTyp,
+			Overloads: []sortOverload{
+				{overload: o, Dir: "distsqlpb.Ordering_Column_ASC", DirString: "Asc"},
+				{}},
+		}
+	}
+	for _, o := range comparisonOpToOverloads[tree.GT] {
+		typesToSortOverloads[o.LTyp].Overloads[1] = sortOverload{
+			overload: o, Dir: "distsqlpb.Ordering_Column_DESC", DirString: "Desc"}
+	}
 }
