20a665845052a704dc7c613722e962bffcf475a5
changangela <angelachang27@gmail.com>
2018-12-20 11:08:53 -0500

    exec: make function regex
    This commit adds a helper function to help make a function regex with a
    specified number of arguments. This is used by the hash joiner to more easily
    generate templated functions.
    
    Release note: None

--- pkg/sql/exec/execgen/cmd/execgen/hashjoiner_gen.go
+++ pkg/sql/exec/execgen/cmd/execgen/hashjoiner_gen.go
@@ -17,7 +17,6 @@ package main
 import (
 	"io"
 	"io/ioutil"
-	"regexp"
 	"strings"
 	"text/template"
 
@@ -37,37 +36,36 @@ func genHashJoiner(wr io.Writer) error {
 	s = strings.Replace(s, "_TemplateType", "{{.LTyp}}", -1)
 	s = strings.Replace(s, "_SEL_IND", "{{.SelInd}}", -1)
 
-	assignNeRe := regexp.MustCompile(`_ASSIGN_NE\((.*),(.*),(.*)\)`)
+	assignNeRe := makeFunctionRegex("_ASSIGN_NE", 3)
 	s = assignNeRe.ReplaceAllString(s, `{{.Global.Assign "$1" "$2" "$3"}}`)
 
-	assignHash := regexp.MustCompile(`_ASSIGN_HASH\((.*),(.*)\)`)
+	assignHash := makeFunctionRegex("_ASSIGN_HASH", 2)
 	s = assignHash.ReplaceAllString(s, `{{.Global.UnaryAssign "$1" "$2"}}`)
 
-	rehash := regexp.MustCompile(`_REHASH_BODY\(.*,(.*)\)`)
-	s = rehash.ReplaceAllString(s, `{{template "rehashBody" buildDict "Global" . "SelInd" $1}}`)
+	rehash := makeFunctionRegex("_REHASH_BODY", 4)
+	s = rehash.ReplaceAllString(s, `{{template "rehashBody" buildDict "Global" . "SelInd" $4}}`)
 
-	checkCol := regexp.MustCompile(`(?s)_CHECK_COL_WITH_NULLS\(.*?,.*?,.*?,.*?,.*?,.*?,\s*(.*?)\)`)
-	s = checkCol.ReplaceAllString(s, `{{template "checkColWithNulls" buildDict "Global" . "SelInd" $1}}`)
+	checkCol := makeFunctionRegex("_CHECK_COL_WITH_NULLS", 7)
+	s = checkCol.ReplaceAllString(s, `{{template "checkColWithNulls" buildDict "Global" . "SelInd" $7}}`)
 
-	distinctCollectRightOuter := regexp.MustCompile(`(?s)_DISTINCT_COLLECT_RIGHT_OUTER\(\s*(.*?),\s*(.*?),\s*(.*?)\)`)
+	distinctCollectRightOuter := makeFunctionRegex("_DISTINCT_COLLECT_RIGHT_OUTER", 3)
 	s = distinctCollectRightOuter.ReplaceAllString(s, `{{template "distinctCollectRightOuter" buildDict "Global" . "SelInd" $3}}`)
 
-	distinctCollectNoOuter := regexp.MustCompile(`(?s)_DISTINCT_COLLECT_NO_OUTER\(\s*(.*?),\s*(.*?),\s*(.*?),\s*(.*?)\)`)
+	distinctCollectNoOuter := makeFunctionRegex("_DISTINCT_COLLECT_NO_OUTER", 4)
 	s = distinctCollectNoOuter.ReplaceAllString(s, `{{template "distinctCollectNoOuter" buildDict "Global" . "SelInd" $4}}`)
 
-	collectRightOuter := regexp.MustCompile(`(?s)_COLLECT_RIGHT_OUTER\(\s*(.*?),\s*(.*?),\s*(.*?),\s*(.*?),\s*(.*?)\)`)
+	collectRightOuter := makeFunctionRegex("_COLLECT_RIGHT_OUTER", 5)
 	s = collectRightOuter.ReplaceAllString(s, `{{template "collectRightOuter" buildDict "Global" . "SelInd" $5}}`)
 
-	collectNoOuter := regexp.MustCompile(`(?s)_COLLECT_NO_OUTER\(\s*(.*?),\s*(.*?),\s*(.*?),\s*(.*?),\s*(.*?)\)`)
+	collectNoOuter := makeFunctionRegex("_COLLECT_NO_OUTER", 5)
 	s = collectNoOuter.ReplaceAllString(s, `{{template "collectNoOuter" buildDict "Global" . "SelInd" $5}}`)
 
-	checkColMain := regexp.MustCompile(`_CHECK_COL_MAIN\((.*\))`)
+	checkColMain := makeFunctionRegex("_CHECK_COL_MAIN", 1)
 	s = checkColMain.ReplaceAllString(s, `{{template "checkColMain" .}}`)
 
-	checkColBody := regexp.MustCompile(`_CHECK_COL_BODY\((.*),(.*),(.*)\)`)
-	s = checkColBody.ReplaceAllString(s, `{{template "checkColBody" buildDict "Global" .Global "SelInd" .SelInd "ProbeHasNulls" $2 "BuildHasNulls" $3}}`)
+	checkColBody := makeFunctionRegex("_CHECK_COL_BODY", 8)
+	s = checkColBody.ReplaceAllString(s, `{{template "checkColBody" buildDict "Global" .Global "SelInd" .SelInd "ProbeHasNulls" $7 "BuildHasNulls" $8}}`)
 
-	print(s)
 	tmpl, err := template.New("hashjoiner_op").Funcs(template.FuncMap{"buildDict": buildDict}).Parse(s)
 
 	if err != nil {
