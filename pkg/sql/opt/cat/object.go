0c1c80db32881ab2e297e81128809f65d2bb0974
Andrew Kimball <andyk@cockroachlabs.com>
2018-12-25 16:05:51 -0800

    opt: Add Schema support to catalog and metadata
    Database schemas are collections that contain other catalog objects like
    tables and view. Users must have the CREATE privilege on the schema in
    order to create objects within it.
    
    In preparation for CREATE TABLE optimizer support, this commit adds a
    Catalog.ResolveSchema method to lookup schemas in the catalog, and an
    AddSchema method to add schema references to query metadata. optCatalog
    implements ResolveSchema by calling TableName.ResolveTarget.
    
    Release note: None

---
+++ pkg/sql/opt/cat/object.go
@@ -0,0 +1,22 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package cat
+
+// Object is implemented by all objects in the catalog.
+type Object interface {
+	// ID is the unique, stable identifier for this object. See the comment for
+	// StableID for more detail.
+	ID() StableID
+}
