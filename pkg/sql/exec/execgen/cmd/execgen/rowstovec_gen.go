e285510c3b26ad76839981c1c7484a36edd077b2
Alfonso Subiotto Marqués <alfonso@cockroachlabs.com>
2018-12-27 18:28:36 +0100

    exec: improvements to EncDatumRows to ColVec conversion
    - Introduces a switch statement instead of an if statement per
    SemanticType given that the conditions are mutually exclusive.
    - ColumnTypes are now not required to have a 0 width. This avoids
    panics in cases where we were attempting to convert a fixed width
    column type (e.g. STRING(25)).
    
    Release note: None

--- pkg/sql/exec/execgen/cmd/execgen/rowstovec_gen.go
+++ pkg/sql/exec/execgen/cmd/execgen/rowstovec_gen.go
@@ -16,73 +16,34 @@ package main
 
 import (
 	"io"
+	"io/ioutil"
+	"strings"
 	"text/template"
 
 	"github.com/cockroachdb/cockroach/pkg/sql/exec/types"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 )
 
-const rowsToVecTemplate = `
-package exec
-
-import (
-  "fmt"
-
-	"github.com/cockroachdb/apd"
-	"github.com/cockroachdb/cockroach/pkg/sql/exec/types"
-	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
-	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
-)
-
-// EncDatumRowsToColVec converts one column from EncDatumRows to a column
-// vector. columnIdx is the 0-based index of the column in the EncDatumRows.
-func EncDatumRowsToColVec(
-	rows sqlbase.EncDatumRows,
-	vec ColVec,
-	columnIdx int,
-	columnType *sqlbase.ColumnType,
-	alloc *sqlbase.DatumAlloc,
-) error {
-	nRows := uint16(len(rows))
-  // TODO(solon): Make this chain of conditionals more efficient: either a
-  // switch statement or even better a lookup table on SemanticType. Also get
-  // rid of the somewhat dubious assumption that Width is unset (0) for column
-  // types where it does not apply.
-	{{range .}}
-	if columnType.SemanticType == sqlbase.{{.SemanticType}} && columnType.Width == {{.Width}} {
-		col := vec.{{.ExecType}}()
-		datumToPhysicalFn := types.GetDatumToPhysicalFn(*columnType)
-		for i := uint16(0); i < nRows; i++ {
-			if rows[i][columnIdx].Datum == nil {
-				if err := rows[i][columnIdx].EnsureDecoded(columnType, alloc); err != nil {
-					return err
-				}
-			}
-			datum := rows[i][columnIdx].Datum
-			if datum == tree.DNull {
-				vec.SetNull(i)
-			} else {
-				v, err := datumToPhysicalFn(datum)
-				if err != nil {
-					return err
-				}
-				col[i] = v.({{.GoType}})
-			}
-		}
-		return nil
-	}
-	{{end}}
-	panic(fmt.Sprintf("Unsupported column type and width: %s, %d", columnType.SQLString(), columnType.Width))
+// Width is used when a SemanticType has a width that has an associated distinct
+// ExecType. One or more of these structs is used as a special case when
+// multiple widths need to be associated to one SemanticType in a
+// columnConversion struct.
+type Width struct {
+	Width    int32
+	ExecType string
+	GoType   string
 }
-`
 
 // columnConversion defines a conversion from a sqlbase.ColumnType to an
 // exec.ColVec.
 type columnConversion struct {
 	// SemanticType is the semantic type of the ColumnType.
 	SemanticType string
-	// Width is the optional width of the ColumnType.
-	Width int32
+
+	// Widths is set if this SemanticType has several widths to special-case. If
+	// set, only the ExecType and GoType in the Widths is used.
+	Widths []Width
+
 	// ExecType is the exec.T to which we're converting. It should correspond to
 	// a method name on exec.ColVec.
 	ExecType string
@@ -90,27 +51,53 @@ type columnConversion struct {
 }
 
 func genRowsToVec(wr io.Writer) error {
+	f, err := ioutil.ReadFile("pkg/sql/exec/rowstovec_tmpl.go")
+	if err != nil {
+		return err
+	}
+
+	s := string(f)
+
+	// Replace the template variables.
+	s = strings.Replace(s, "_TemplateType", "{{.ExecType}}", -1)
+	s = strings.Replace(s, "_GOTYPE", "{{.GoType}}", -1)
+	s = strings.Replace(s, "_SEMANTIC_TYPE", "sqlbase.{{.SemanticType}}", -1)
+	s = strings.Replace(s, "_WIDTH", "{{.Width}}", -1)
+
+	rowsToVecRe := makeFunctionRegex("_ROWS_TO_COL_VEC", 4)
+	s = rowsToVecRe.ReplaceAllString(s, `{{ template "rowsToColVec" . }}`)
+
 	// Build the list of supported column conversions.
 	var columnConversions []columnConversion
 	for s, name := range sqlbase.ColumnType_SemanticType_name {
 		semanticType := sqlbase.ColumnType_SemanticType(s)
-		for _, width := range getWidths(semanticType) {
-			ct := sqlbase.ColumnType{SemanticType: semanticType, Width: width}
+		ct := sqlbase.ColumnType{SemanticType: semanticType}
+		conversion := columnConversion{
+			SemanticType: "ColumnType_" + name,
+		}
+		widths := getWidths(semanticType)
+		for _, width := range widths {
+			ct.Width = width
 			t := types.FromColumnType(ct)
 			if t == types.Unhandled {
 				continue
 			}
-			conversion := columnConversion{
-				SemanticType: "ColumnType_" + name,
-				Width:        width,
-				ExecType:     t.String(),
-				GoType:       t.GoTypeName(),
-			}
-			columnConversions = append(columnConversions, conversion)
+			conversion.Widths = append(
+				conversion.Widths, Width{Width: width, ExecType: t.String(), GoType: t.GoTypeName()},
+			)
 		}
+		if widths == nil {
+			t := types.FromColumnType(ct)
+			if t == types.Unhandled {
+				continue
+			}
+			conversion.ExecType = t.String()
+			conversion.GoType = t.GoTypeName()
+		}
+		columnConversions = append(columnConversions, conversion)
 	}
 
-	tmpl, err := template.New("rowsToVec").Parse(rowsToVecTemplate)
+	tmpl, err := template.New("rowsToVec").Parse(s)
 	if err != nil {
 		return err
 	}
@@ -122,10 +109,10 @@ func init() {
 }
 
 // getWidths returns allowable ColumnType.Width values for the specified
-// SemanticType.
+// SemanticType. If the returned slice is nil, any width is allowed.
 func getWidths(semanticType sqlbase.ColumnType_SemanticType) []int32 {
 	if semanticType == sqlbase.ColumnType_INT {
 		return []int32{0, 8, 16, 32, 64}
 	}
-	return []int32{0}
+	return nil
 }
