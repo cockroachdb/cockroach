1327ae141304bfaf36305c7d9d06f2e493a36944
Justin Jaffray <justin@cockroachlabs.com>
2019-01-10 09:36:37 -0500

    opt: add reorder joins flag
    Release note: None

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -1681,6 +1681,10 @@ func (m *sessionDataMutator) SetZigzagJoinEnabled(val bool) {
 	m.data.ZigzagJoinEnabled = val
 }
 
+func (m *sessionDataMutator) SetReorderJoins(val bool) {
+	m.data.ZigzagJoinEnabled = val
+}
+
 func (m *sessionDataMutator) SetVectorize(val sessiondata.VectorizeExecMode) {
 	m.data.Vectorize = val
 }
