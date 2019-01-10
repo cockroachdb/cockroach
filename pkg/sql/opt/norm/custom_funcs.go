1167b8faf1f2d1f7f613446de25d8f5ad25154c7
Justin Jaffray <justin@cockroachlabs.com>
2019-01-10 09:54:20 -0500

    opt: reorder joins (off by default)
    This commit introduces the AssociateJoin rule, which, if the appropriate
    session variable is set, will explore additional join orderings.
    
    This commit is presented as a first step - it's not remotely
    production-ready (i.e., nobody should be turning on the flag).
    
    Release note (sql change): introduced the experimental_reorder_joins
    flags which will (slowly) attempt to find better join orderings for a
    query. Not practical for real-world usage today.

--- pkg/sql/opt/norm/custom_funcs.go
+++ pkg/sql/opt/norm/custom_funcs.go
@@ -1478,3 +1478,9 @@ func (c *CustomFuncs) SortFilters(f memo.FiltersExpr) memo.FiltersExpr {
 	})
 	return result
 }
+
+// ShouldReorderJoins returns whether the optimizer should attempt to find
+// a better ordering of inner joins.
+func (c *CustomFuncs) ShouldReorderJoins() bool {
+	return c.f.evalCtx.SessionData.ReorderJoins
+}
