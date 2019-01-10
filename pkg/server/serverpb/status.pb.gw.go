f13a6c90d4ce59aa95dffce3e463738b64f07ce2
Radu Berinde <radu@cockroachlabs.com>
2019-01-10 11:43:06 -0500

    *: update deps
    Update all deps except:
     - errcheck (due to https://github.com/kisielk/errcheck/issues/162)
     - honnef.co/go/tools (requires refactoring)
     - some `github.com/cockroachdb/` deps which have been updated for go1.11.
    
    Changes:
    https://github.com/Azure/azure-storage-blob-go/compare/5152f14a...45d0c5e3
    https://github.com/Shopify/sarama/compare/bbdbe644...879f6318
    https://github.com/aws/aws-sdk-go/compare/ddc06f9f...1f8a2469
    https://github.com/axiomhq/hyperloglog/compare/e8c19f17...4b99d0c2
    https://github.com/cenkalti/backoff/compare/62661b46...1e4cf3da
    https://github.com/cockroachdb/circuitbreaker/compare/4f5b1686...3e861b2e
    https://github.com/docker/distribution/compare/40b7b583...91b0f055
    https://github.com/docker/docker/compare/a4a816b6...b4842cfe
    https://github.com/go-ole/go-ole/compare/a41e3c4b...39dc8486
    https://github.com/go-sql-driver/mysql/compare/60d456a4...c45f530f
    https://github.com/google/go-github/compare/35781f7f...a5cb647b
    https://github.com/google/pprof/compare/3ea8567a...e84dfd68
    https://github.com/googleapis/gax-go/compare/b001040c...c8a15bac
    https://github.com/grpc-ecosystem/grpc-gateway/compare/92583770...aeab1d96
    https://github.com/jmespath/go-jmespath/compare/0b12d6b5...c2b33e84
    https://github.com/linkedin/goavro/compare/1beee2a7...af12b3c4
    https://github.com/mattn/go-runewidth/compare/ce7b0b5c...3ee7d812
    https://github.com/montanaflynn/stats/compare/db72e6ca...945b007c
    https://github.com/pkg/errors/compare/645ef004...ba968bfe
    https://github.com/prometheus/client_model/compare/5c3871d8...f287a105
    https://github.com/prometheus/common/compare/4724e925...2998b132
    https://github.com/prometheus/procfs/compare/1dc9a6cb...b1a0a9a3
    https://github.com/shirou/gopsutil/compare/0f70a4a0...ccc1c101
    https://github.com/sirupsen/logrus/compare/bcd833df...e1e72e9d
    https://github.com/stretchr/testify/compare/f35b8ab0...ffdc059b
    https://github.com/etcd-io/etcd/compare/1900a8e2...fae6e924
    https://github.com/golang/crypto/compare/505ab145...ff983b9c
    https://github.com/golang/net/compare/351d144f...1e06a53d
    https://github.com/golang/sync/compare/42b31787...37e7f081
    https://github.com/golang/sys/compare/70b957f3...48ac38b7
    https://github.com/golang/tools/compare/bcd4e47d...d30e00c2
    https://github.com/google/google-api-go-client/compare/9c79deeb...19e022d8
    https://github.com/golang/appengine/compare/150dc57a...e9657d88
    
    Informs #30774.
    
    Release note: None

--- pkg/server/serverpb/status.pb.gw.go
+++ pkg/server/serverpb/status.pb.gw.go
@@ -751,14 +751,14 @@ func RegisterStatusHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMu
 	defer func() {
 		if err != nil {
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Printf("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 			return
 		}
 		go func() {
 			<-ctx.Done()
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Printf("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 		}()
 	}()
@@ -772,8 +772,8 @@ func RegisterStatusHandler(ctx context.Context, mux *runtime.ServeMux, conn *grp
 	return RegisterStatusHandlerClient(ctx, mux, NewStatusClient(conn))
 }
 
-// RegisterStatusHandler registers the http handlers for service Status to "mux".
-// The handlers forward requests to the grpc endpoint over the given implementation of "StatusClient".
+// RegisterStatusHandlerClient registers the http handlers for service Status
+// to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "StatusClient".
 // Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "StatusClient"
 // doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
 // "StatusClient" to call the correct interceptors.
