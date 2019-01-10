c34cdbc4059ba390f746941a787e706c2c2996c5
Justin Jaffray <justin@cockroachlabs.com>
2019-01-10 09:54:20 -0500

    opt: reorder joins (off by default)
    This commit introduces the AssociateJoin rule, which, if the appropriate
    session variable is set, will explore additional join orderings.
    
    This commit is presented as a first step - it's not remotely
    production-ready (i.e., nobody should be turning on the flag).
    
    The next step should be finding a way to limit the application of this
    rule so that planning can't take egreguously long in cases with a lot of
    joins. I think the easiest approach here will be to limit its
    application to joins of less than some small number of relations (5 or
    6), but alternatively we could introduce mechanisms into the optimizer
    to limit exploration done to some set amount of *time*. The latter seems
    riskier to me, while the former is pretty straightforward.
    
    Release note (sql change): introduced the experimental_reorder_joins
    flags which will (slowly) attempt to find better join orderings for a
    query. Not practical for real-world usage today.

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -1682,7 +1682,7 @@ func (m *sessionDataMutator) SetZigzagJoinEnabled(val bool) {
 }
 
 func (m *sessionDataMutator) SetReorderJoins(val bool) {
-	m.data.ZigzagJoinEnabled = val
+	m.data.ReorderJoins = val
 }
 
 func (m *sessionDataMutator) SetVectorize(val sessiondata.VectorizeExecMode) {
