2526f1766548222e049a63c162b10b2af40532e1
Matt Jibson <matt.jibson@gmail.com>
2019-01-11 03:06:03 -0500

    sql/lex: use a switch statement for keyword lookup
    Inspired by the below post, I did some experiments with our keyword lookup
    performance. First attempt was to use a Go perfect hashing package. I
    was not able to make it faster than a normal map lookup. Using a switch
    statement with all generated cases was another attempt that yielded
    some nice improvements. I also tried improving the speed of the previous
    (i.e., before this diff) algorithm, and was able to get a few percent,
    but the switch version is still overall faster.
    
    name                    old time/op  new time/op  delta
    Parse/simple-12         9.17µs ± 1%  7.96µs ± 4%  -13.23%  (p=0.008 n=5+5)
    Parse/string-12         12.7µs ± 1%  11.1µs ± 1%  -12.55%  (p=0.008 n=5+5)
    Parse/tpcc-delivery-12  16.7µs ± 2%  15.8µs ± 3%   -5.52%  (p=0.008 n=5+5)
    Parse/account-12        27.0µs ± 1%  26.0µs ± 2%   -3.52%  (p=0.008 n=5+5)
    
    See: https://www.postgresql.org/message-id/flat/E1ghOVt-0007os-2V%40gemulon.postgresql.org
    
    Release note: None

--- pkg/cmd/all-keywords/main.go
+++ pkg/cmd/all-keywords/main.go
@@ -109,4 +109,19 @@ var Keywords = map[string]struct {
 	"{{.Lower}}": { {{.Match}}, "{{.Category}}" },
 {{end -}}
 }
+
+// GetKeywordID returns the lex id of the SQL keyword k or IDENT if k is
+// not a keyword.
+func GetKeywordID(k string) int32 {
+	// The previous implementation generated a map that did a string ->
+	// id lookup. Various ideas were benchmarked and the implementation below
+	// was the fastest of those, between 3% and 10% faster (at parsing, so the
+	// scanning speedup is even more) than the map implementation.
+	switch k {
+	{{range . -}}
+	case "{{.Lower}}": return {{.Match}}
+	{{end -}}
+	default: return IDENT
+	}
+}
 `
