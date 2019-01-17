eccc8518b3bb137dcf5ce28b5fa102380c0bed99
Andrew Couch <andrew@cockroachlabs.com>
2019-01-16 23:29:00 -0500

    remove dead code

--- pkg/server/statements.go
+++ pkg/server/statements.go
@@ -22,8 +22,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
-	// "google.golang.org/grpc/codes"
-	// "google.golang.org/grpc/status"
 )
 
 func buildNumericStat(mean, stddev tree.Datum) roachpb.NumericStat {
@@ -125,74 +123,5 @@ ORDER BY statement_key, node_id, application_name, distributed, optimized, has_e
 		response.Statements = append(response.Statements, stmt)
 	}
 
-	/*
-		localReq := &serverpb.StatementsRequest{
-			NodeID: "local",
-		}
-
-		if len(req.NodeID) > 0 {
-			requestedNodeID, local, err := s.parseNodeID(req.NodeID)
-			if err != nil {
-				return nil, status.Errorf(codes.InvalidArgument, err.Error())
-			}
-			if local {
-				return s.StatementsLocal(ctx)
-			}
-			status, err := s.dialNode(ctx, requestedNodeID)
-			if err != nil {
-				return nil, err
-			}
-			return status.Statements(ctx, localReq)
-		}
-
-		dialFn := func(ctx context.Context, nodeID roachpb.NodeID) (interface{}, error) {
-			client, err := s.dialNode(ctx, nodeID)
-			return client, err
-		}
-		nodeStatement := func(ctx context.Context, client interface{}, _ roachpb.NodeID) (interface{}, error) {
-			status := client.(serverpb.StatusClient)
-			return status.Statements(ctx, localReq)
-		}
-
-		if err := s.iterateNodes(ctx, fmt.Sprintf("statement statistics for node %s", req.NodeID),
-			dialFn,
-			nodeStatement,
-			func(nodeID roachpb.NodeID, resp interface{}) {
-				statementsResp := resp.(*serverpb.StatementsResponse)
-				response.Statements = append(response.Statements, statementsResp.Statements...)
-				if response.LastReset.After(statementsResp.LastReset) {
-					response.LastReset = statementsResp.LastReset
-				}
-			},
-			func(nodeID roachpb.NodeID, err error) {
-				// TODO(couchand): do something here...
-			},
-		); err != nil {
-			return nil, err
-		}
-	*/
-
 	return response, nil
-}
-
-func (s *statusServer) StatementsLocal(ctx context.Context) (*serverpb.StatementsResponse, error) {
-	stmtStats := s.admin.server.pgServer.SQLServer.GetStmtExecutionsAggregated()
-	lastReset := s.admin.server.pgServer.SQLServer.GetStmtExecutionsLastReset()
-
-	resp := &serverpb.StatementsResponse{
-		Statements: make([]serverpb.StatementsResponse_CollectedStatementStatistics, len(stmtStats)),
-		LastReset:  lastReset,
-	}
-
-	for i, stmt := range stmtStats {
-		resp.Statements[i] = serverpb.StatementsResponse_CollectedStatementStatistics{
-			Key: serverpb.StatementsResponse_ExtendedStatementStatisticsKey{
-				KeyData: stmt.Key,
-				NodeID:  s.gossip.NodeID.Get(),
-			},
-			Stats: stmt.Stats,
-		}
-	}
-
-	return resp, nil
 }
