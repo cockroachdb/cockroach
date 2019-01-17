6ae5aff8db6d51209b87c2acf556171e6c223465
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2019-01-17 15:55:45 +0100

    roachtest: don't run `upgrade` on release-2.1
    Upgrading from 2.1.3 to the same or a higher 2.1.x patch release
    is a no-op, but the test expects something to happen.
    
    Fixes #34088.
    
    Release note: None

--- pkg/cmd/roachtest/upgrade.go
+++ pkg/cmd/roachtest/upgrade.go
@@ -257,7 +257,7 @@ func registerUpgrade(r *registry) {
 	for _, n := range []int{5} {
 		r.Add(testSpec{
 			Name:       fmt.Sprintf("upgrade/oldVersion=%s/nodes=%d", oldVersion, n),
-			MinVersion: "v2.1.0",
+			MinVersion: "v2.2.0",
 			Nodes:      nodes(n),
 			Run: func(ctx context.Context, t *test, c *cluster) {
 				runUpgrade(ctx, t, c, oldVersion)
