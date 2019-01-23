08cb87e47454f2d2653cea7ce521b65aa6d68647
Andrew Werner <ajwerner@cockroachlabs.com>
2019-01-23 17:07:41 -0500

    util/batcher: create library for batching requests based on range ID
    Almost certainly this package is in the wrong place. Should it be in
    pkg/internal/client/batchsender? Another thought was that instead of having
    the object have its own interface which speaks to how it works, it could just
    be a client.Sender that rejects batch requests which have a header with anything
    other than RangeID but that feels like another attempt to overload an already
    somewhat overloaded interface. Lastly my hunch is that the naming is not good
    and it should probably be a BatchSender but that felt also confusing given
    the Sender interface and the BatchRequest we so commonly send.
    
    There are plenty of things left to be done to this package but I wanted to get
    it out.
    
    Release note: None

---
+++ pkg/util/batcher/batcher_test.go
@@ -0,0 +1,129 @@
+package batcher
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/sync/errgroup"
+)
+
+type batchResp struct {
+	br *roachpb.BatchResponse
+	pe *roachpb.Error
+}
+
+type batchSend struct {
+	ba       roachpb.BatchRequest
+	respChan chan<- batchResp
+}
+
+type chanSender chan batchSend
+
+func (c chanSender) Send(
+	ctx context.Context, ba roachpb.BatchRequest,
+) (*roachpb.BatchResponse, *roachpb.Error) {
+	respChan := make(chan batchResp)
+	select {
+	case c <- batchSend{ba: ba, respChan: respChan}:
+	case <-ctx.Done():
+		return nil, roachpb.NewError(ctx.Err())
+	}
+	resp := <-respChan
+	return resp.br, resp.pe
+}
+
+func TestBatcherSend(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	stopper := stop.NewStopper()
+	defer stopper.Stop(context.Background())
+	sc := make(chanSender)
+	b := New(Config{
+		MaxIdle:         50 * time.Millisecond,
+		MaxWait:         50 * time.Millisecond,
+		MaxMsgsPerBatch: 3,
+		Sender:          sc,
+		Stopper:         stopper,
+	})
+	var g errgroup.Group
+	sendRequest := func(rangeID roachpb.RangeID, request roachpb.Request) {
+		g.Go(func() error {
+			_, _, err := b.Send(context.Background(), rangeID, request)
+			return err
+		})
+	}
+	// Send 3 requests to range 2 and 2 to range 1.
+	// The 3rd range 2 request will trigger immediate sending due to the
+	// MaxMsgsPerBatch configuration. The range 1 batch will be sent after the
+	// MaxWait timeout expires.
+	sendRequest(1, &roachpb.GetRequest{})
+	sendRequest(2, &roachpb.GetRequest{})
+	sendRequest(1, &roachpb.GetRequest{})
+	sendRequest(2, &roachpb.GetRequest{})
+	sendRequest(2, &roachpb.GetRequest{})
+	// Wait for the range 2 request and ensure it contains 3 requests.
+	s := <-sc
+	assert.Len(t, s.ba.Requests, 3)
+	s.respChan <- batchResp{}
+	// Wait for the range 1 request and ensure it contains 2 requests.
+	s = <-sc
+	assert.Len(t, s.ba.Requests, 2)
+	s.respChan <- batchResp{}
+	// Make sure everything gets a response.
+	if err := g.Wait(); err != nil {
+		t.Fatalf("expected no errors, got %v", err)
+	}
+}
+
+func TestSendAfterStopped(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	stopper := stop.NewStopper()
+	sc := make(chanSender)
+	b := New(Config{
+		Sender:  sc,
+		Stopper: stopper,
+	})
+	stopper.Stop(context.Background())
+	_, _, err := b.Send(context.Background(), 1, &roachpb.GetRequest{})
+	assert.Equal(t, err, stop.ErrUnavailable)
+}
+
+func TestSendAfterCanceled(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	sc := make(chanSender)
+	stopper := stop.NewStopper()
+	defer stopper.Stop(context.Background())
+	b := New(Config{
+		Sender:  sc,
+		Stopper: stopper,
+	})
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+	_, _, err := b.Send(ctx, 1, &roachpb.GetRequest{})
+	assert.Equal(t, err, ctx.Err())
+}
+
+func TestStopDuringSend(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	stopper := stop.NewStopper()
+	sc := make(chanSender, 1)
+	b := New(Config{
+		Sender:  sc,
+		Stopper: stopper,
+		MaxWait: 10 * time.Millisecond,
+		MaxIdle: 10 * time.Millisecond,
+	})
+	errChan := make(chan error)
+	go func() {
+		_, _, err := b.Send(context.Background(), 1, &roachpb.GetRequest{})
+		errChan <- err
+	}()
+	r := <-sc
+	go stopper.Stop(context.Background())
+	assert.Equal(t, <-errChan, stop.ErrUnavailable)
+	r.respChan <- batchResp{}
+}
