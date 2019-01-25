29ec46e9d4f6a290ff57216368c5642e41b2ba40
Peter Mattis <petermattis@gmail.com>
2019-01-24 20:16:30 -0500

    cli: skip TestRemoveDeadReplicas
    Release note: None

--- pkg/cli/debug_test.go
+++ pkg/cli/debug_test.go
@@ -132,6 +132,7 @@ func TestOpenReadOnlyStore(t *testing.T) {
 
 func TestRemoveDeadReplicas(t *testing.T) {
 	defer leaktest.AfterTest(t)()
+	t.Skipf("incompatible with adaptive replication factor on 2.1")
 	ctx := context.Background()
 
 	baseDir, dirCleanupFn := testutils.TempDir(t)
