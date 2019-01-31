7d930b2672c1fa8f1a6d8c5f93b0efe00c3808cc
Celia La <celia@cockroachlabs.com>
2019-01-31 17:06:24 -0500

    WIP, ui: implement logical plan design
    This commit implements the designs as specified in
    Zeplin design "Logical Plans - v1":
    https://app.zeplin.io/project/5c40e9dab2616abf79e3eb99/screen/5c4b6f23143de2bfcb12ae4e
    
    Still todo:
    * [ ] write tests
    * [ ] Fix known design issues (see images below):
      * [ ] 1. Shift up horizontal node-connecting line.
      * [ ] 2. Replace HTML characters with Zeplin icons.
      * [ ] 3. Removing trailing vertical node-connecting line.
      * [ ] 4. Fix z-index: orange (warn) nodes should appear in front of connectors.
      * [ ] 5. Fix z-index: expanded nodes should appear in front of connectors.
    
    ![image](https://user-images.githubusercontent.com/3051672/52218034-bed6c700-2867-11e9-9395-5c8cb0e39869.png)
    
    ![image](https://user-images.githubusercontent.com/3051672/52218042-c4cca800-2867-11e9-8983-35851714fafb.png)
    
    Release note: None

--- pkg/ui/src/views/statements/planView.tsx
+++ pkg/ui/src/views/statements/planView.tsx
@@ -16,115 +16,274 @@ import _ from "lodash";
 import React from "react";
 
 import { cockroach } from "src/js/protos";
-import { intersperse } from "src/util/intersperse";
 import IExplainTreePlanNode = cockroach.sql.IExplainTreePlanNode;
-import ExplainTreePlanNode_IAttr = cockroach.sql.ExplainTreePlanNode.IAttr;
+import IAttr = cockroach.sql.ExplainTreePlanNode.IAttr;
+import {ToolTipWrapper} from "oss/src/views/shared/components/toolTip";
+
+interface IPlanNode {
+  name: string;
+  attrs: IAttr[];
+  children: IPlanNode[][];
+}
+
+/* ************************* HELPER FUNCTIONS ************************* */
+
+function planNode(name: string, attrs: any, children: IPlanNode[][]): IPlanNode {
+  return {
+    "name": name,
+    "attrs": attrs,
+    "children": children,
+  };
+}
+
+// TODO(celia): explain.
+function flattenTree(treePlan: IExplainTreePlanNode): IPlanNode[] {
+  const flattenedPlan = [ planNode(
+    treePlan.name,
+    treePlan.attrs,
+    []),
+  ];
+
+  if (treePlan.children.length === 0) {
+    return flattenedPlan;
+  }
+  const flattenedChildren =
+    treePlan.children
+      .map( (child) => flattenTree(child));
+  if (treePlan.children.length === 1) {
+    flattenedPlan.push(...flattenedChildren[0]);
+  } else {
+    flattenedPlan[0].children = flattenedChildren;
+  }
+  return flattenedPlan;
+}
+
+interface IPlanNodeHeaderProps {
+  title: string;
+  subtitle: string;
+  warn: boolean;
+}
+
+// TODO(celia): explain.
+function planNodeHeaderProps(node: IPlanNode): IPlanNodeHeaderProps {
+  let title = _.capitalize(node.name);
+  let subtitle = null;
+  let warn = false;
+
+  switch (node.name) {
+    case "scan":
+      title = "Table scan";
+      if (_.find(node.attrs, {key: "spans", value: "ALL"})) {
+        warn = true;
+        const tableAttr = _.filter(node.attrs, (attr) => ( attr.key === "table"));
+        subtitle = tableAttr[0].value;
+        const ampersandPosition = subtitle.indexOf("@");
+        if (ampersandPosition > 0) {
+          subtitle = subtitle.substring(0, ampersandPosition - 1);
+        }
+      }
+      break;
+    case "join":
+      const typeAttr = _.filter(node.attrs, (attr) => ( attr.key === "type"));
+      title = _.capitalize(typeAttr[0].value + " " + node.name);
+      break;
+    default:
+      break;
+  }
+
+  return {
+    title, subtitle, warn,
+  };
+}
+
+/* ************************* PLAN NODES ************************* */
+
+function PlanNodeDescription (props: {
+  header: IPlanNodeHeaderProps;
+}) {
+  return <span>
+    {props.header.warn &&
+    <span>&#x26A0; </span>
+    }
+    {!props.header.warn &&
+    <span>&#x26AC; </span>
+    }
+    {props.header.title}
+    {props.header.subtitle &&
+    <span>: <b>{props.header.subtitle}</b></span>
+    }
+  </span>;
+}
+
+interface IPlanNodeDetailProps {
+  node: IPlanNode;
+  expanded: boolean;
+  toggleExpanded: () => void;
+}
+
+class PlanNodeDetails extends React.Component<IPlanNodeDetailProps> {
+
+  renderArrow() {
+    const node = this.props.node;
+    if (node.attrs && node.attrs.length > 0) {
+      if (this.props.expanded) {
+        // up arrow
+        return <div className="arrow-expanded">&uarr;</div>;
+      }
+      // down arrow
+      return <div className="arrow">&darr;</div>;
+    }
+  }
+
+  renderMaybeExpandedDetail() {
+    const node = this.props.node;
+    if (node.attrs && node.attrs.length > 0) {
+      if (this.props.expanded) {
+        return (
+          <div className="nodeAttributes">
+            {node.attrs.map( (attr) => (
+              <div key={attr.key} className="attr">
+                {attr.key}
+                {attr.value &&
+                  <span>={attr.value}</span>
+                }
+              </div>
+            ))}
+          </div>
+        );
+      }
+    }
+  }
+
+  renderClassName(warn: boolean) {
+    const node = this.props.node;
+    if (node.attrs && node.attrs.length > 0 && this.props.expanded) {
+      if (warn) {
+        return "nodeDetails warn expanded";
+      }
+      return "nodeDetails expanded";
+    } else if (warn) {
+      return "nodeDetails warn";
+    }
+    return "nodeDetails";
+  }
+
+  render() {
+    const node = this.props.node;
+    const header = planNodeHeaderProps(node);
+    return (
+      <div
+        className={this.renderClassName(header.warn)}
+        onClick={() => {
+          this.props.toggleExpanded();
+        }}
+      >
+        <PlanNodeDescription
+          header={header}
+        /> {this.renderArrow()}
+        {this.renderMaybeExpandedDetail()}
+      </div>
+    );
+  }
+}
+
+interface IPlanNodeProps {
+  node: IPlanNode;
+}
+
+interface IPlanNodeState {
+  expanded: boolean;
+}
+
+class PlanNode extends React.Component<IPlanNodeProps, IPlanNodeState> {
+  constructor(props: IPlanNodeProps) {
+    super(props);
+    this.state = {
+      expanded: false,
+    };
+
+    // This binding is necessary to make `this` work in the callback
+    this.toggleExpanded = this.toggleExpanded.bind(this);
+  }
+
+  // TODO(celia): toggleExpanded() was originally inside <PlanNodeDetails/>,
+  // but I've moved this here for now, because the outer <li> may need
+  // to know state.expanded in order to finish/fix up the styling of the
+  // node connecting lines. Should move this back into <PlanNodeDetails/>
+  // if <li> ends up not needing to know about expanded.
+  toggleExpanded() {
+    this.setState(state => ({
+      expanded: !state.expanded,
+    }));
+  }
+
+  renderClassName() {
+    const node = this.props.node;
+    const expanded = false; /* TODO */
+    if (node.attrs && node.attrs.length > 0 && expanded) {
+      return "expanded";
+    }
+  }
+
+  render() {
+    const node = this.props.node;
+    const expanded = this.state.expanded;
+    return <li className={this.renderClassName()}>
+      <PlanNodeDetails
+        node={node}
+        expanded={expanded}
+        toggleExpanded={this.toggleExpanded}
+      />
+      {node.children && node.children.map( (child) => (
+        <PlanNodes
+          nodes={child}
+        />
+      ))
+      }
+    </li>;
+  }
+}
+
+function PlanNodes(props: {
+  nodes: IPlanNode[],
+}): React.ReactElement<{}> {
+  const nodes = props.nodes;
+  return <ul>
+    {nodes.map( (node) => {
+      return <PlanNode node={node}/>;
+    })}
+  </ul>;
+}
 
 export function PlanView(props: {
   title: string,
   plan: IExplainTreePlanNode,
 }) {
-  if (!props.plan) {
-    return <div className="plan-view">
-      <h3>{props.title}</h3>
-      <p>No plan captured yet.</p>
-    </div>;
-  }
-  return <div className="plan-view">
-    <h3>{props.title}</h3>
-    <div className="plan-node">
-      <ul>
-        <li>
-          <PlanNode node={props.plan} />
-        </li>
-      </ul>
-    </div>
-  </div>;
-}
-
-interface PlanNodeProps {
-  node: IExplainTreePlanNode;
-}
-
-function PlanNode(props: PlanNodeProps) {
-  const node = props.node;
-  const collapsedAttrs = collapseRepeatedAttrs(node.attrs);
-  return (
-    <div>
-      <span className="plan-node__name">{node.name}</span>
-      <span className="plan-node__attrs">
-        {collapsedAttrs.map((attr) => (
-          <span className="plan-node__attr" key={attr.key}>
-            <span className="plan-node__attr-key">{attr.key}</span>
-            <span className="plan-node__attr-eq">=</span>
-            { renderAttrValueList(attr.value) }
-          </span>
-        ))}
-      </span>
-      { renderChildren(node.children) }
-    </div>
-  );
-}
-
-function renderChildren(children: cockroach.sql.IExplainTreePlanNode[]|null): React.ReactElement<PlanNodeProps> {
-  if (!children) {
-    return null;
-  }
-  return (
-    <ul>
-      {children.map((child, idx) => (
-        <li>
-          <PlanNode key={idx} node={child} />
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-function renderAttrValueList(values: string[]) {
-  return (
-    <span className="plan-node__attr-value-list">
-      [
-      {intersperse(
-        values.map((value, idx) => (
-          <span className="plan-node__attr-value" key={idx}>{value}</span>
-        )),
-        <span>, </span>,
-      )}
-      ]
-    </span>
-  );
-}
-
-interface CollapsedPlanAttr {
-  key: string;
-  value: string[];
-}
-
-export function collapseRepeatedAttrs(attrs: ExplainTreePlanNode_IAttr[]): CollapsedPlanAttr[] {
-  if (!attrs) {
-    return [];
-  }
-  const collapsed: { [key: string]: CollapsedPlanAttr } = {};
-
-  attrs.forEach((attr) => {
-    if (attr.key === null || attr.value === null) {
-      return;
-    }
-    const existingAttr = collapsed[attr.key];
-    if (!existingAttr) {
-      collapsed[attr.key] = {
-        key: attr.key,
-        value: [attr.value],
-      };
-      return;
-    } else {
-      collapsed[attr.key].value.push(attr.value);
-    }
-  });
-
-  const collapsedAttrs = _.values(collapsed);
-  return _.sortBy(collapsedAttrs, (ca) => (
-    ca.key === "table" ? "table" : "z" + ca.key
-  ));
+  const flattenedPlanNodes = flattenTree(props.plan);
+  return <table className="plan-view-table">
+    <thead>
+    <tr className="plan-view-table__row--header">
+      <th className="plan-view-table__cell">
+        {props.title}
+        <div className="plan-view-table__tooltip">
+          <ToolTipWrapper
+            text="This is the most recently sampled plan. Plans are sampled every five minutes.">
+            <div className="plan-view-table__tooltip-hover-area">
+              <div className="plan-view-table__info-icon">i</div>
+            </div>
+          </ToolTipWrapper>
+        </div>
+      </th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr className="plan-view-table__row--body">
+      <td className="plan-view plan-view-table__cell" style={{ textAlign: "left" }}>
+        <PlanNodes
+          nodes={flattenedPlanNodes}
+        />
+      </td>
+    </tr>
+    </tbody>
+  </table>;
 }
