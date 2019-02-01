3f5e0f61ba0e29b789403f17cdb730dad2b2f332
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2019-02-01 22:11:40 +0100

    storage: ignore non-live probing follower during log truncation
    In the previous code, a follower in probing status which was not
    recently active (i.e. a dead node) would permanently suppress
    log truncations unless the Raft log was above threshold size (but the
    size tracks only what the current leaseholder has written, i.e., it
    can undercount dramatically). As a result, snapshots to other nodes
    would get blocked if the log was in fact large (>16mb), leading to
    ranges which effectively couldn't change their set of members.
    
    This wasn't caught in TestComputeTruncateDecisionProgressStatusProbe
    because of a bug in the test, which was accidentally setting the Match
    instead of the Next of the probing follower. By setting Match, the
    probing follower behaved differently from the case which would trigger
    the bug, and so the outcome the test asserts is actually still the same
    (except that it failed when the bug in the test was fixed until the bug
    in the truncation code was also fixed).
    
    Release note (bug fix): Prevent a problem that would cause the Raft log
    to grow very large which in turn could prevent replication changes.
    
    wiptest

--- pkg/storage/raft_log_queue.go
+++ pkg/storage/raft_log_queue.go
@@ -321,6 +321,13 @@ func computeTruncateDecision(input truncateDecisionInput) truncateDecision {
 	decision.ChosenVia = truncatableIndexChosenViaQuorumIndex
 
 	for _, progress := range input.RaftStatus.Progress {
+		if !progress.RecentActive {
+			// If a follower isn't recently active, don't lower the truncation
+			// index for it as the follower is likely not online at all and would
+			// block log truncation forever.
+			continue
+		}
+
 		// Generally we truncate to the quorum commit index when the log becomes
 		// too large, but we make an exception for live followers which are
 		// being probed (i.e. the leader doesn't know how far they've caught
@@ -340,10 +347,11 @@ func computeTruncateDecision(input truncateDecisionInput) truncateDecision {
 		// ranges will be split many times over, resulting in a flurry of
 		// snapshots with overlapping bounds that put significant stress on the
 		// Raft snapshot queue.
-		probing := (progress.RecentActive && progress.State == raft.ProgressStateProbe)
-		if probing && decision.NewFirstIndex > decision.Input.FirstIndex {
-			decision.NewFirstIndex = decision.Input.FirstIndex
-			decision.ChosenVia = truncatableIndexChosenViaProbingFollower
+		if progress.State == raft.ProgressStateProbe {
+			if decision.NewFirstIndex > decision.Input.FirstIndex {
+				decision.NewFirstIndex = decision.Input.FirstIndex
+				decision.ChosenVia = truncatableIndexChosenViaProbingFollower
+			}
 		} else if !input.LogTooLarge() && decision.NewFirstIndex > progress.Match {
 			decision.NewFirstIndex = progress.Match
 			decision.ChosenVia = truncatableIndexChosenViaFollowers
