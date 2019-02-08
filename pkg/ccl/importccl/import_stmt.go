bd80dedb0e2b43269d59b901f91c79cc5e85aa06
David Taylor <tinystatemachine@gmail.com>
2019-02-08 19:14:29 +0000

    importccl: add direct-ingest, no sort IMPORT mode
    This adds a prototype of a no-sort IMPORT where SSTs are generated and added directly from the readers. Each reader builds a batch of keys, sorts it and makes it into an SST and sends it.
    This means ranges receive many overlapping SSTs — i.e. one from each batch in each reader — and are charged with compacting them, potentially increasing write amplification — but avoids pushing all the KV data into distsql and sorting and buffering it in its entirety to produce perfectly non-overlapping SSTs.
    
    The lack of a sort and conflict detection in adding SSTs also means that we may no longer observe uniqueness/PK conflicts — if two different reader batches produce the same key, the one in second SST applied would simply shadow the first. This issue was worked around in index backfills by counting the resulting index entries and comparing that to the number of rows in the table — since every row produced an entry, if one entry shadowed another, the counts would not line up. However we will need to determine a way detect conflicts during IMPORT before we could use this approach as the default if we want to correctly return uniqueness violations.
    
    This is an early prototype. All of the new functionality is behind a flag, so we potentially could merge it with the above issue unaddressed — to allow easier experimenting, tuning and benchmarking.
    
    Release note: none.

--- pkg/ccl/importccl/import_stmt.go
+++ pkg/ccl/importccl/import_stmt.go
@@ -66,6 +66,8 @@ const (
 	importOptionOversample = "oversample"
 	importOptionSkipFKs    = "skip_foreign_keys"
 
+	importOptionDirectIngest = "experimental_direct_ingestion"
+
 	pgCopyDelimiter = "delimiter"
 	pgCopyNull      = "nullif"
 
@@ -89,6 +91,8 @@ var importOptionExpectValues = map[string]sql.KVStringOptValidate{
 	importOptionOversample: sql.KVStringOptRequireValue,
 
 	importOptionSkipFKs: sql.KVStringOptRequireNoValue,
+
+	importOptionDirectIngest: sql.KVStringOptRequireNoValue,
 
 	pgMaxRowSize: sql.KVStringOptRequireValue,
 }
@@ -692,6 +696,11 @@ func importPlanHook(
 			}
 		}
 
+		_, ingestDirectly := opts[importOptionDirectIngest]
+		if transform != "" && ingestDirectly {
+			return errors.Errorf("cannot use %q and %q options together", importOptionDirectIngest, importOptionTransform)
+		}
+
 		var tableDescs []*sqlbase.TableDescriptor
 		var jobDesc string
 		var names []string
@@ -836,15 +845,16 @@ func importPlanHook(
 			Description: jobDesc,
 			Username:    p.User(),
 			Details: jobspb.ImportDetails{
-				URIs:       files,
-				Format:     format,
-				ParentID:   parentID,
-				Tables:     tableDetails,
-				BackupPath: transform,
-				SSTSize:    sstSize,
-				Oversample: oversample,
-				Walltime:   walltime,
-				SkipFKs:    skipFKs,
+				URIs:           files,
+				Format:         format,
+				ParentID:       parentID,
+				Tables:         tableDetails,
+				BackupPath:     transform,
+				SSTSize:        sstSize,
+				Oversample:     oversample,
+				Walltime:       walltime,
+				SkipFKs:        skipFKs,
+				IngestDirectly: ingestDirectly,
 			},
 			Progress: jobspb.ImportProgress{},
 		})
@@ -868,7 +878,13 @@ func doDistributedCSVTransform(
 	walltime int64,
 	sstSize int64,
 	oversample int64,
+	ingestDirectly bool,
 ) (roachpb.BulkOpSummary, error) {
+	if ingestDirectly {
+		return sql.DistIngest(ctx, p, job, tables, files, format, walltime)
+		// TODO(dt): check for errors in job records as is done below.
+	}
+
 	evalCtx := p.ExtendedEvalContext()
 
 	ci := sqlbase.ColTypeInfoFromColTypes([]sqlbase.ColumnType{
@@ -1015,6 +1031,7 @@ func (r *importResumer) Resume(
 	sstSize := details.SSTSize
 	format := details.Format
 	oversample := details.Oversample
+	ingestDirectly := details.IngestDirectly
 
 	if sstSize == 0 {
 		// The distributed importer will correctly chunk up large ranges into
@@ -1054,7 +1071,7 @@ func (r *importResumer) Resume(
 	}
 
 	res, err := doDistributedCSVTransform(
-		ctx, job, files, p, parentID, tables, transform, format, walltime, sstSize, oversample,
+		ctx, job, files, p, parentID, tables, transform, format, walltime, sstSize, oversample, ingestDirectly,
 	)
 	if err != nil {
 		return err
