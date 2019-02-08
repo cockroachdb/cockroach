94e81e666e593e735f5e558b7cb247453ccecac1
Alfonso Subiotto Marqués <alfonso@cockroachlabs.com>
2019-02-08 10:32:24 -0500

    roachtest: don't write tpchbench results in scalefactor subdirectory
    There was no need to do this, we want to treat scalefactor runs as
    different tests (i.e. show performance evolution over time for each one).
    
    Release note: None

--- pkg/cmd/roachtest/tpchbench.go
+++ pkg/cmd/roachtest/tpchbench.go
@@ -81,10 +81,9 @@ func runSQL20Bench(ctx context.Context, t *test, c *cluster, b tpchBenchSpec) {
 
 		// Run with only one worker to get best-case single-query performance.
 		cmd := fmt.Sprintf(
-			"./workload run querybench --db=tpch --concurrency=1 --query-file=%s --duration=5m {pgurl%s} --histograms=logs/scalefactor=%d/stats.json",
+			"./workload run querybench --db=tpch --concurrency=1 --query-file=%s --duration=5m {pgurl%s} --histograms=logs/stats.json",
 			filename,
 			roachNodes,
-			b.ScaleFactor,
 		)
 		if err := c.RunE(ctx, loadNode, cmd); err != nil {
 			t.Fatal(err)
