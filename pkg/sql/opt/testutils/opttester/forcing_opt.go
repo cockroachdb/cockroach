5043bdc2e43cd947f230f2d43214f56accd9fb29
Radu Berinde <radu@cockroachlabs.com>
2019-02-10 17:17:59 -0500

    opt: move opttester to separate package
    Moving OptTester to an `opttester` sub-package. This allows code that
    opttester depends on to use the remaining test utils.
    
    Release note: None

--- pkg/sql/opt/testutils/opttester/forcing_opt.go
+++ pkg/sql/opt/testutils/opttester/forcing_opt.go
@@ -12,7 +12,7 @@
 // implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
-package testutils
+package opttester
 
 import (
 	"github.com/cockroachdb/cockroach/pkg/sql/opt"
