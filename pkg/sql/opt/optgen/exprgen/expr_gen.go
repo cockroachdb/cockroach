0b0ee1385e80368c1e739711b5b61fd6584f1630
Radu Berinde <radu@cockroachlabs.com>
2019-02-13 16:14:56 -0500

    sql: introduce PREPARE AS OPT PLAN
    This commit introduces a way to run a "canned" optimizer plan (using
    exprgen). A predefined plan can be prepared as in the following
    example:
    
    ```
    PREPARE a AS OPT PLAN '
    (Root
      (Scan [ (Table "t") (Cols "k,str") ])
      (Presentation "k,str")
      (NoOrdering)
    )';
    EXECUTE a
    ```
    
    This is a testing facility and should not be documented.
    
    The commit also contains some fixes and cleanup to exprgen.
    
    Release note: None

--- pkg/sql/opt/optgen/exprgen/expr_gen.go
+++ pkg/sql/opt/optgen/exprgen/expr_gen.go
@@ -27,7 +27,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/optgen/lang"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/ordering"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/props/physical"
-	"github.com/cockroachdb/cockroach/pkg/sql/opt/testutils"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/xform"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/types"
@@ -54,6 +53,24 @@ import (
 //    of the form [ (FiledName <value>) ]. For example:
 //      [ (Table "abc") (Index "abc@ab") (Cols "a,b") ]
 //    Implicit conversions are allowed here for column lists, orderings, etc.
+//
+//  - A Root custom function is used to set the physical properties of the root.
+//    Setting the physical properties (in particular the presentation) is always
+//    necessary for the plan to be run via PREPARE .. AS OPT PLAN '..'.
+//
+// Some examples of valid inputs:
+//    (Tuple [ (True) (False) ] "tuple{bool, bool}" )
+//
+//    (Root
+//      (Scan [ (Table "abc") (Index "abc@ab") (Cols "a,b") ])
+//      (Presentation "a,b")
+//      (OrderingChoice "+a,+b")
+//    )
+//
+//    (Select
+//      (Scan [ (Table "abc") (Cols "a,b,c") ])
+//      [ (Eq (Var "a") (Const 1)) ]
+//    )
 //
 // For more examples, see the various testdata/ files.
 //
@@ -91,6 +108,9 @@ func Build(catalog cat.Catalog, factory *norm.Factory, input string) (_ opt.Expr
 		expr = result.(opt.Expr)
 	}
 	eg.populateBestProps(expr, required)
+	if rel, ok := expr.(memo.RelExpr); ok {
+		eg.mem.SetRoot(rel, required)
+	}
 	return expr, nil
 }
 
@@ -187,20 +207,21 @@ func (eg *exprGen) call(name string, method reflect.Value, args lang.SliceExpr) 
 		}
 
 		arg := eg.eval(args[i])
-		argVals[i] = eg.castToDesiredType(arg, desiredType)
-		if !argVals[i].IsValid() {
+		converted := eg.castToDesiredType(arg, desiredType)
+		if converted == nil {
 			panic(errorf("%s: using %T as type %s", name, arg, desiredType))
 		}
+		argVals[i] = reflect.ValueOf(converted)
 	}
 	return method.Call(argVals)[0].Interface()
 }
 
 // castToDesiredType tries to convert the given argument to a value of the given
-// type.
-func (eg *exprGen) castToDesiredType(arg interface{}, desiredType reflect.Type) reflect.Value {
+// type. Returns nil if conversion was not possible.
+func (eg *exprGen) castToDesiredType(arg interface{}, desiredType reflect.Type) interface{} {
 	actualType := reflect.TypeOf(arg)
 	if actualType.AssignableTo(desiredType) {
-		return reflect.ValueOf(arg)
+		return arg
 	}
 
 	if slice, ok := arg.([]interface{}); ok {
@@ -210,39 +231,39 @@ func (eg *exprGen) castToDesiredType(arg interface{}, desiredType reflect.Type) 
 			for i := range slice {
 				col, ok := slice[i].(opt.ColumnID)
 				if !ok {
-					return reflect.Value{}
+					return nil
 				}
 				set.Add(int(col))
 			}
-			return reflect.ValueOf(set)
+			return set
 		}
 
 		if desiredType.Kind() != reflect.Slice {
-			return reflect.Value{}
+			return nil
 		}
 
 		// See if we can convert all elements to the desired slice element type.
-		converted := convertSlice(slice, desiredType, func(v interface{}) reflect.Value {
+		converted := convertSlice(slice, desiredType, func(v interface{}) interface{} {
 			if val := reflect.ValueOf(v); val.Type().AssignableTo(desiredType.Elem()) {
-				return val.Convert(desiredType.Elem())
+				return val.Convert(desiredType.Elem()).Interface()
 			}
-			return reflect.Value{}
+			return nil
 		})
-		if converted.IsValid() {
+		if converted != nil {
 			return converted
 		}
 
 		// Special case for converting slice of values implementing ScalarExpr to a
 		// FiltersExpr.
 		if desiredType == reflect.TypeOf(memo.FiltersExpr{}) {
-			converted := convertSlice(slice, desiredType, func(v interface{}) reflect.Value {
+			converted := convertSlice(slice, desiredType, func(v interface{}) interface{} {
 				expr, ok := v.(opt.ScalarExpr)
 				if !ok {
-					return reflect.Value{}
+					return nil
 				}
-				return reflect.ValueOf(memo.FiltersItem{Condition: expr})
+				return memo.FiltersItem{Condition: expr}
 			})
-			if converted.IsValid() {
+			if converted != nil {
 				return converted
 			}
 		}
@@ -251,39 +272,53 @@ func (eg *exprGen) castToDesiredType(arg interface{}, desiredType reflect.Type) 
 	if str, ok := arg.(string); ok {
 		// String to type.
 		if desiredType == reflect.TypeOf((*types.T)(nil)).Elem() {
-			typ, err := testutils.ParseType(str)
+			typ, err := ParseType(str)
 			if err != nil {
 				panic(exprGenErr{err})
 			}
-			return reflect.ValueOf(typ)
+			return typ
+		}
+
+		// String to Ordering.
+		if desiredType == reflect.TypeOf(opt.Ordering{}) {
+			return eg.Ordering(str)
 		}
 
 		// String to OrderingChoice.
 		if desiredType == reflect.TypeOf(physical.OrderingChoice{}) {
-			return reflect.ValueOf(physical.ParseOrderingChoice(eg.substituteCols(str)))
+			return eg.OrderingChoice(str)
+		}
+
+		// String to ColList.
+		if desiredType == reflect.TypeOf(opt.ColList{}) {
+			return eg.ColList(str)
+		}
+
+		// String to ColSet.
+		if desiredType == reflect.TypeOf(opt.ColSet{}) {
+			return eg.ColSet(str)
 		}
 	}
-	return reflect.Value{}
+	return nil
 }
 
 // convertSlice tries to create a slice of the given type; each element is the
 // result of calling mapFn on the input slice element.
 //
-// If mapFn returns an invalid Value, convertSlice also returns an invalid
-// Value.
+// If mapFn returns nil for any element, convertSlice also returns nil.
 func convertSlice(
-	slice []interface{}, toType reflect.Type, mapFn func(v interface{}) reflect.Value,
-) reflect.Value {
+	slice []interface{}, toType reflect.Type, mapFn func(v interface{}) interface{},
+) interface{} {
 	res := reflect.MakeSlice(toType, len(slice), len(slice))
 
 	for i, v := range slice {
 		val := mapFn(v)
-		if !val.IsValid() {
-			return reflect.Value{}
+		if val == nil {
+			return nil
 		}
-		res.Index(i).Set(val)
+		res.Index(i).Set(reflect.ValueOf(val))
 	}
-	return res
+	return res.Interface()
 }
 
 // populateBestProps sets the physical properties and costs of the expressions
--- pkg/sql/opt/testutils/utils.go
+++ pkg/sql/opt/testutils/utils.go
@@ -15,58 +15,13 @@
 package testutils
 
 import (
-	"fmt"
 	"path/filepath"
-	"strings"
 	"testing"
 
-	"github.com/cockroachdb/cockroach/pkg/sql/coltypes"
 	"github.com/cockroachdb/cockroach/pkg/sql/parser"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/types"
 )
-
-// ParseType parses a string describing a type.
-// It supports tuples using the syntax "tuple{<type>, <type>, ...}" but does not
-// support tuples of tuples.
-func ParseType(typeStr string) (types.T, error) {
-	// Special case for tuples for which there is no SQL syntax.
-	if strings.HasPrefix(typeStr, "tuple{") && strings.HasSuffix(typeStr, "}") {
-		s := strings.TrimPrefix(typeStr, "tuple{")
-		s = strings.TrimSuffix(s, "}")
-		// Hijack the PREPARE syntax which takes a list of types.
-		// TODO(radu): this won't work for tuples of tuples; we would need to add
-		// some special syntax.
-		parsed, err := parser.ParseOne(fmt.Sprintf("PREPARE x ( %s ) AS SELECT 1", s))
-		if err != nil {
-			return nil, fmt.Errorf("cannot parse %s as a type: %s", typeStr, err)
-		}
-		colTypes := parsed.AST.(*tree.Prepare).Types
-		res := types.TTuple{Types: make([]types.T, len(colTypes))}
-		for i := range colTypes {
-			res.Types[i] = coltypes.CastTargetToDatumType(colTypes[i])
-		}
-		return res, nil
-	}
-	colType, err := parser.ParseType(typeStr)
-	if err != nil {
-		return nil, err
-	}
-	return coltypes.CastTargetToDatumType(colType), nil
-}
-
-// ParseTypes parses a list of types.
-func ParseTypes(colStrs []string) ([]types.T, error) {
-	res := make([]types.T, len(colStrs))
-	for i, s := range colStrs {
-		var err error
-		res[i], err = ParseType(s)
-		if err != nil {
-			return nil, err
-		}
-	}
-	return res, nil
-}
 
 // ParseScalarExpr parses a scalar expression and converts it to a
 // tree.TypedExpr.
