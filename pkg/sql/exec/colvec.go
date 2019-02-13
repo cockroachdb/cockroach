c79b9fed5029a1be76415837aa9d25ccd2798f2e
George Utsin <george@cockroachlabs.com>
2019-02-13 10:51:48 -0500

    exec: vectorized merge join operator
    mergeJoinOp is an operator that implements sort-merge join.
    It performs a merge on the left and right input sources, based on the equality
    columns, assuming both inputs are in sorted order.
    
    The merge join operator uses a "two scan" approach to generate the join.
    What this means is that instead of going through and expanding the cross
    product row by row, the operator performs two passes.
    The first pass generates a list of chunks of matching rows based on the
    equality column. A "chunk" is a ADT representing a run, or in other words,
    multiple repeated values. This run is represented as the starting row ordinal,
    the ending row ordinal, and the number of times this chunk is
    expanded/repeated.
    The second pass is where each of these chunks are either expanded or repeated
    into the output or savedOutput buffer, saving big on the type introspection.
    
    The current implementation only works on an inner join
    with a single equality column of Int64s.
    
    Two buffers are used, one for the run on the left table and one for the run on
    the right table. These buffers are only used if the run ends with a batch, to
    make sure that we don't miss any cross product entries while expanding the
    chunks (repeatedRows and repeatedGroups) when a run spans multiple batches.
    
    Some benchmarks:
    ```
    BenchmarkMergeJoiner/rows=1024-8                      	   20000	     66913 ns/op	 979.42 MB/s
    BenchmarkMergeJoiner/rows=4096-8                      	    5000	    265602 ns/op	 986.98 MB/s
    BenchmarkMergeJoiner/rows=16384-8                     	    2000	   1042486 ns/op	1005.84 MB/s
    BenchmarkMergeJoiner/rows=1048576-8                   	      20	  65653445 ns/op	1022.17 MB/s
    BenchmarkMergeJoiner/oneSideRepeat-rows=1024-8         	   20000	     68281 ns/op	 959.79 MB/s
    BenchmarkMergeJoiner/oneSideRepeat-rows=4096-8         	    5000	    267793 ns/op	 978.90 MB/s
    BenchmarkMergeJoiner/oneSideRepeat-rows=16384-8        	    2000	   1049688 ns/op	 998.94 MB/s
    BenchmarkMergeJoiner/oneSideRepeat-rows=1048576-8      	      20	  67160497 ns/op	 999.23 MB/s
    BenchmarkMergeJoiner/bothSidesRepeat-rows=1024-8       	   20000	     69234 ns/op	 946.58 MB/s
    BenchmarkMergeJoiner/bothSidesRepeat-rows=4096-8       	    2000	    605505 ns/op	 432.93 MB/s
    BenchmarkMergeJoiner/bothSidesRepeat-rows=16384-8      	     500	   2841285 ns/op	 369.05 MB/s
    BenchmarkMergeJoiner/bothSidesRepeat-rows=1048576-8    	       3	 428893269 ns/op	 156.47 MB/s
    ```
    
    Release note: None

--- pkg/sql/exec/colvec.go
+++ pkg/sql/exec/colvec.go
@@ -53,6 +53,9 @@ type ColVec interface {
 	// Col returns the raw, typeless backing storage for this ColVec.
 	Col() interface{}
 
+	// SetCol sets the member column (in the case of mutable columns).
+	SetCol(col column)
+
 	// TemplateType returns an []interface{} and is used for operator templates.
 	// Do not call this from normal code - it'll always panic.
 	_TemplateType() []interface{}
@@ -61,9 +64,18 @@ type ColVec interface {
 	// elements of this ColVec, assuming that both ColVecs are of type colType.
 	Append(vec ColVec, colType types.T, toLength uint64, fromLength uint16)
 
+	// AppendSlice appends vec[srcStartIdx:srcEndIdx] elements to
+	// this ColVec starting at destStartIdx.
+	AppendSlice(vec ColVec, colType types.T, destStartIdx uint64, srcStartIdx uint16, srcEndIdx uint16)
+
 	// AppendWithSel appends into itself another column vector from a ColBatch with
 	// maximum size of ColBatchSize, filtered by the given selection vector.
 	AppendWithSel(vec ColVec, sel []uint16, batchSize uint16, colType types.T, toLength uint64)
+
+	// AppendSliceWithSel appends srcEndIdx - srcStartIdx elements to this ColVec starting
+	// at destStartIdx. These elements come from vec, filtered by the selection
+	// vector sel.
+	AppendSliceWithSel(vec ColVec, colType types.T, destStartIdx uint64, srcStartIdx uint16, srcEndIdx uint16, sel []uint16)
 
 	// Copy copies src[srcStartIdx:srcEndIdx] into this ColVec.
 	Copy(src ColVec, srcStartIdx, srcEndIdx uint64, typ types.T)
@@ -71,9 +83,11 @@ type ColVec interface {
 	// CopyWithSelInt64 copies vec, filtered by sel, into this ColVec. It replaces
 	// the contents of this ColVec.
 	CopyWithSelInt64(vec ColVec, sel []uint64, nSel uint16, colType types.T)
+
 	// CopyWithSelInt16 copies vec, filtered by sel, into this ColVec. It replaces
 	// the contents of this ColVec.
 	CopyWithSelInt16(vec ColVec, sel []uint16, nSel uint16, colType types.T)
+
 	// CopyWithSelAndNilsInt64 copies vec, filtered by sel, unless nils is set,
 	// into ColVec. It replaces the contents of this ColVec.
 	CopyWithSelAndNilsInt64(vec ColVec, sel []uint64, nSel uint16, nils []bool, colType types.T)
@@ -85,6 +99,14 @@ type ColVec interface {
 	// PrettyValueAt returns a "pretty"value for the idx'th value in this ColVec.
 	// It uses the reflect package and is not suitable for calling in hot paths.
 	PrettyValueAt(idx uint16, colType types.T) string
+
+	// ExtendNulls extends the null member of a ColVec and sets the right indexes to null,
+	// needed when the length of the underlying column changes.
+	ExtendNulls(vec ColVec, outputLen uint64, srcStartIdx uint16, batchSize uint16, destStartIdx uint64)
+
+	// ExtendNullsWithSel extends the null member of a ColVec and sets the right indexes to null
+	// with the selection vector in mind, needed when the length of the underlying column changes.
+	ExtendNullsWithSel(vec ColVec, outputLen uint64, srcStartIdx uint16, batchSize uint16, destStartIdx uint64, sel []uint16)
 }
 
 // Nulls represents a list of potentially nullable values.
@@ -180,6 +202,10 @@ func (m *memColumn) NullAt(i uint16) bool {
 
 func (m *memColumn) SetNull(i uint16) {
 	m.SetNull64(uint64(i))
+}
+
+func (m *memColumn) SetCol(col column) {
+	m.col = col
 }
 
 func (m *memColumn) UnsetNulls() {
