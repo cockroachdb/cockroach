6329e6beea00b3a696af16924ecf793a62416c11
Andrew Kimball <andyk@cockroachlabs.com>
2019-02-15 16:54:58 -0800

    opt: Fix CopyAndReplace to work for optimized expressions
    Factory.CopyAndReplace currently assumes that the input expression is
    normalized but not yet optimized. This causes failures when it comes across
    operators that can only be in the optimized tree, like MergeJoinOp. The
    fix is to explicitly copy the normalized tree in CopyAndReplace by using
    the FirstExpr method to always grab the normalized expression in each
    memo group.
    
    Release note: None

--- pkg/sql/opt/norm/factory_test.go
+++ pkg/sql/opt/norm/factory_test.go
@@ -15,14 +15,19 @@
 package norm_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/memo"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/norm"
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/optbuilder"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/testutils/testcat"
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/xform"
+	"github.com/cockroachdb/cockroach/pkg/sql/parser"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/types"
 )
 
 // TestSimplifyFilters tests factory.SimplifyFilters. It's hard to fully test
@@ -33,7 +38,10 @@ func TestSimplifyFilters(t *testing.T) {
 	var f norm.Factory
 	f.Init(&evalCtx)
 
-	cat := createFiltersCatalog(t)
+	cat := testcat.New()
+	if _, err := cat.ExecuteDDL("CREATE TABLE a (x INT PRIMARY KEY, y INT)"); err != nil {
+		t.Fatal(err)
+	}
 	a := f.Metadata().AddTable(cat.Table(tree.NewTableName("t", "a")))
 	ax := a.ColumnID(0)
 
@@ -57,10 +65,56 @@ func TestSimplifyFilters(t *testing.T) {
 	}
 }
 
-func createFiltersCatalog(t *testing.T) *testcat.Catalog {
+// Test CopyAndReplace on an already optimized join. Before CopyAndReplace is
+// called, the join has a placeholder that causes the optimizer to use a merge
+// join. After CopyAndReplace substitutes a constant for the placeholder, the
+// optimizer switches to a lookup join. A similar pattern is used by the
+// ApplyJoin execution operator which replaces variables with constants in an
+// already optimized tree. The CopyAndReplace code must take care to copy over
+// the normalized tree rather than the optimized tree by using the FirstExpr
+// method.
+func TestCopyAndReplace(t *testing.T) {
 	cat := testcat.New()
-	if _, err := cat.ExecuteDDL("CREATE TABLE a (x INT PRIMARY KEY, y INT)"); err != nil {
+	if _, err := cat.ExecuteDDL("CREATE TABLE ab (a INT PRIMARY KEY, b INT)"); err != nil {
 		t.Fatal(err)
 	}
-	return cat
+	if _, err := cat.ExecuteDDL("CREATE TABLE cde (c INT PRIMARY KEY, d INT, e INT, INDEX(d))"); err != nil {
+		t.Fatal(err)
+	}
+
+	evalCtx := tree.MakeTestingEvalContext(cluster.MakeTestingClusterSettings())
+	semaCtx := tree.MakeSemaContext()
+
+	var o xform.Optimizer
+	o.Init(&evalCtx)
+
+	stmt, err := parser.ParseOne("SELECT * FROM ab INNER JOIN cde ON a=c AND d=$1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := semaCtx.Placeholders.Init(stmt.NumPlaceholders, nil /* typeHints */); err != nil {
+		t.Fatal(err)
+	}
+	b := optbuilder.New(context.Background(), &semaCtx, &evalCtx, cat, o.Factory(), stmt.AST)
+	if err := b.Build(); err != nil {
+		t.Fatal(err)
+	}
+
+	if e := o.Optimize(); e.Op() != opt.MergeJoinOp {
+		t.Errorf("expected optimizer to choose merge-join, not %v", e.Op())
+	}
+
+	m := o.Factory().DetachMemo()
+
+	o.Init(&evalCtx)
+	o.Factory().CopyAndReplace(m.RootExpr().(memo.RelExpr), m.RootProps(), func(e opt.Expr) opt.Expr {
+		if e.Op() == opt.PlaceholderOp {
+			return o.Factory().ConstructConstVal(tree.NewDInt(1), types.Int)
+		}
+		return e
+	})
+
+	if e := o.Optimize(); e.Op() != opt.LookupJoinOp {
+		t.Errorf("expected optimizer to choose lookup-join, not %v", e.Op())
+	}
 }
