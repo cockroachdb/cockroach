079f95fcdc925314339ec6f0f81b02c232cbd349
Yahor Yuzefovich <yahor@cockroachlabs.com>
2019-02-15 21:07:33 -0800

    exec: implement sort chunks operator
    Implements sort chunks operator in vectorized engine to be used when
    the tuples are already ordered on a prefix of ordering columns. The
    "meat" of implementation is a new chunker operator which produces
    chunks of tuples (delimited by a zero-length batch) which are then
    inputted into a general sorter. The sorter is reset for every chunk.
    
    Release note: None

--- pkg/sql/exec/sort.go
+++ pkg/sql/exec/sort.go
@@ -22,7 +22,7 @@ import (
 )
 
 // NewSorter returns a new sort operator, which sorts its input on the columns
-// given in sortColIdxs. The inputTypes must correspond 1-1 with the columns in
+// given in orderingCols. The inputTypes must correspond 1-1 with the columns in
 // the input operator.
 func NewSorter(
 	input Operator, inputTypes []types.T, orderingCols []distsqlpb.Ordering_Column,
@@ -76,7 +76,7 @@ type sortOp struct {
 	isOrderingCol []bool
 	// sorters contains one colSorter per sort column.
 	sorters []colSorter
-	// partitioners contains one partitioner per sort solumn except for the last,
+	// partitioners contains one partitioner per sort column except for the last,
 	// which doesn't need to be partitioned.
 	partitioners []partitioner
 
@@ -96,7 +96,8 @@ type sortOp struct {
 	// state is the current state of the sort.
 	state sortState
 
-	output ColBatch
+	workingSpace []uint64
+	output       ColBatch
 }
 
 // colSorter is a single-column sorter, specialized on a particular type.
@@ -191,18 +192,28 @@ func (p *sortOp) spoolAndSort() {
 	}
 	p.spooledTuples = nTuples
 
+	// Allocate p.order and p.workingSpace if it hasn't been allocated yet or the
+	// underlying memory is insufficient.
+	if p.order == nil {
+		p.order = make([]uint64, nTuples)
+		p.workingSpace = make([]uint64, nTuples)
+	} else if uint64(cap(p.order)) < nTuples {
+		p.order = make([]uint64, nTuples)
+		p.workingSpace = make([]uint64, nTuples)
+	}
+	p.order = p.order[:nTuples]
+	p.workingSpace = p.workingSpace[:nTuples]
+
 	// Initialize the order vector to the ordinal positions within the input set.
-	p.order = make([]uint64, nTuples)
 	for i := uint64(0); i < uint64(len(p.order)); i++ {
 		p.order[i] = i
 	}
 
-	workingSpace := make([]uint64, nTuples)
 	for i := range p.orderingCols {
-		p.sorters[i].init(p.values[p.orderingCols[i].ColIdx], p.order, workingSpace)
+		p.sorters[i].init(p.values[p.orderingCols[i].ColIdx], p.order, p.workingSpace)
 	}
 
-	// Now, sort each column in turn. The first column is doesn't need special
+	// Now, sort each column in turn. The first column doesn't need special
 	// treatment - we just globally sort it.
 
 	p.sorters[0].sort()
@@ -251,4 +262,353 @@ func (p *sortOp) spoolAndSort() {
 		// columns we've seen so far), sort based on the new column.
 		sorter.sortPartitions(partitions)
 	}
+}
+
+func (p *sortOp) reset() {
+	p.emitted = 0
+	p.spooledTuples = 0
+	p.state = sortSpooling
+}
+
+// NewSortChunks returns a new sort chunks operator, which sorts its input on
+// the columns given in orderingCols. The inputTypes must correspond 1-1 with
+// the columns in the input operator. The input tuples must be sorted on first
+// matchLen columns.
+func NewSortChunks(
+	input Operator, inputTypes []types.T, orderingCols []distsqlpb.Ordering_Column, matchLen int,
+) (Operator, error) {
+	chunker, err := newChunker(input, inputTypes, matchLen)
+	if err != nil {
+		return nil, err
+	}
+
+	var sorter Operator
+	if len(orderingCols) > matchLen {
+		sorter, err = NewSorter(chunker, inputTypes, orderingCols[matchLen:])
+	} else {
+		sorter = &noopOperator{input: chunker}
+	}
+	if err != nil {
+		return nil, err
+	}
+	// TODO(yuzefovich): current implementation of sort chunks will be returning
+	// batches of sizes equal to sizes of the corresponding chunks. Should we use
+	// a coalescer operator around it?
+	return &sortChunksOp{input: chunker, sorter: sorter}, nil
+}
+
+type sortChunksOp struct {
+	input  *chunker
+	sorter Operator
+}
+
+func (c *sortChunksOp) Init() {
+	c.input.Init()
+	c.sorter.Init()
+}
+
+func (c *sortChunksOp) Next() ColBatch {
+	for {
+		batch := c.sorter.Next()
+		if batch.Length() == 0 {
+			if c.input.inputDone {
+				// We're done, so return a zero-length batch.
+				return batch
+			}
+			// We're not yet done - need to process another chunk.
+			if resetter, ok := c.sorter.(resetter); ok {
+				resetter.reset()
+			} else {
+				panic(fmt.Sprintf("sorter doesn't implement resetter interface"))
+			}
+		} else {
+			return batch
+		}
+	}
+}
+
+// chunkerState represents the state of the chunker operator.
+type chunkerState int
+
+const (
+	// chunkerReading is the state of the chunker operator in which it reads a
+	// batch from its input and partitions the batch into chunks. Depending on
+	// current state of the chunker's buffer and number of chunks in the batch,
+	// chunker might stay in chunkerReading state or switch to either of the
+	// emitting states.
+	chunkerReading chunkerState = iota
+	// chunkerEmittingFromBuffer is the state of the chunker operator in which it
+	// emits tuples that have been buffered. All the tuples belong to the same
+	// chunk. After all tuples are emitted, the buffer is cleared, a zero-length
+	// batch is emitted to indicate the end of the chunk, and the chunker
+	// transitions into chunkerEmittingFromBatch state.
+	chunkerEmittingFromBuffer
+	// chunkerEmittingFromBatch is the state of the chunker operator in which it
+	// emits all chunks that are fully-contained within the last read batch (i.e.
+	// all except the last chunk which might include tuples from the next batch).
+	// The last chunk is buffered, and the chunker transitions into
+	// chunkerReading state.
+	chunkerEmittingFromBatch
+)
+
+// chunker is an operator that produces chunks from its input when the tuples
+// are already ordered on the first matchLen columns. To indicate the end of a
+// chunk, the chunker emits a single zero-length batch. To indicate that the
+// input has been fully consumed it emits the second zero-length batch (and
+// will continue to do so when Next()'ed).
+//
+// Note: chunker assumes that its input produces batches with no selection
+// vector, so it always puts a deselection operator on top of its input.
+type chunker struct {
+	input Operator
+	// inputTypes contains the types of all of the columns from input.
+	inputTypes []types.T
+	// inputDone indicates whether input has been fully consumed.
+	inputDone bool
+	// matchLen indicates the number of first columns on which input is ordered.
+	matchLen int
+
+	// batch is the last read batch from input.
+	batch ColBatch
+	// partitioners contains one partitioner for each of matchLen first already
+	// ordered columns.
+	partitioners []partitioner
+	// partitionCol is a bool slice for partitioners' output to be ORed.
+	partitionCol []bool
+	// initPartitionCol is a slice of the same size with partitionCol initialized
+	// with false to use for copying into partitionCol to reset the latter.
+	initPartitionCol []bool
+	// differentiators contains one tupleDifferentiator for each of matchLen
+	// first columns.
+	differentiators []tupleDifferentiator
+
+	// chunks contains the indices of the first tuples within different chunks
+	// found in the last read batch. Note: the first chunk might be a part of
+	// the chunk that is currently being buffered, and similarly the last chunk
+	// might include tuples from the batches to be read.
+	chunks []uint64
+	// chunksProcessedIdx indicates which chunk within s.chunks should be
+	// processed next.
+	chunksProcessedIdx int
+	// shouldEmitChunkEnd indicates whether a zero-length batch should be emitted
+	// to indicate the end of the last emitted chunk.
+	shouldEmitChunkEnd bool
+
+	// buffered indicates the number of currently buffered tuples.
+	buffered uint64
+	// emittedFromBuffer indicates the number of already emitted tuples from
+	// buffer.
+	emittedFromBuffer uint64
+	// bufferedValues is a buffer to store tuples when a chunk is bigger than
+	// ColBatchSize or when the chunk is the last in the last read batch (we
+	// don't know yet where the end of such chunk is).
+	bufferedValues []ColVec
+
+	// initialized indicates whether chunker has already been initialized (used
+	// to do a noop on all except for the first call to chunker.Init()).
+	initialized bool
+	output      ColBatch
+	state       chunkerState
+}
+
+func newChunker(input Operator, inputTypes []types.T, matchLen int) (*chunker, error) {
+	var err error
+	partitioners := make([]partitioner, matchLen)
+	differentiators := make([]tupleDifferentiator, matchLen)
+	for col := 0; col < matchLen; col++ {
+		partitioners[col], err = newPartitioner(inputTypes[col])
+		if err != nil {
+			return nil, err
+		}
+		differentiators[col], err = newTupleDifferentiator(inputTypes[col])
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &chunker{
+		input:           NewDeselectorOp(input, inputTypes),
+		inputTypes:      inputTypes,
+		matchLen:        matchLen,
+		partitioners:    partitioners,
+		differentiators: differentiators,
+		state:           chunkerReading,
+	}, nil
+}
+
+func (s *chunker) Init() {
+	if !s.initialized {
+		s.input.Init()
+		s.output = NewMemBatch(s.inputTypes)
+		s.bufferedValues = make([]ColVec, len(s.inputTypes))
+		for i := 0; i < len(s.inputTypes); i++ {
+			s.bufferedValues[i] = newMemColumn(s.inputTypes[i], 0)
+		}
+		s.partitionCol = make([]bool, ColBatchSize)
+		s.initPartitionCol = make([]bool, ColBatchSize)
+		s.chunks = make([]uint64, 0, 16)
+		s.initialized = true
+	}
+}
+
+func (s *chunker) Next() ColBatch {
+	for {
+		switch s.state {
+		case chunkerReading:
+			s.batch = s.input.Next()
+			if s.batch.Length() == 0 {
+				s.inputDone = true
+				if s.buffered > 0 {
+					s.state = chunkerEmittingFromBuffer
+				} else {
+					s.state = chunkerEmittingFromBatch
+				}
+				continue
+			}
+			if s.batch.Selection() != nil {
+				// We assume that the input has been deselected, so the batch should
+				// never have a selection vector set.
+				panic(fmt.Sprintf("unexpected: batch with non-nil selection vector"))
+			}
+
+			// TODO(yuzefovich): is this the best way to reinitialize s.partitionCol
+			// to false?
+			copy(s.partitionCol, s.initPartitionCol)
+			for i, partitioner := range s.partitioners {
+				partitioner.partition(s.batch.ColVec(i), s.partitionCol, uint64(s.batch.Length()))
+			}
+			s.chunks = boolVecToSel64(s.partitionCol, s.chunks[:0])
+
+			if s.buffered == 0 {
+				// There are no buffered tuples, so a new chunk starts in the current
+				// batch.
+				if len(s.chunks) > 1 {
+					// There is at least one chunk that is fully contained within
+					// s.batch, so we proceed to emitting it.
+					s.state = chunkerEmittingFromBatch
+					continue
+				}
+				// All tuples in s.batch belong to the same chunk. Possibly tuples from
+				// the next batch will also belong to this chunk, so we buffer the full
+				// s.batch.
+				s.buffer(0 /* start */, s.batch.Length())
+				s.state = chunkerReading
+				continue
+			} else {
+				// There are some buffered tuples, so we need to check whether the
+				// first tuple of s.batch belongs to the chunk that is being buffered.
+				differ := false
+				for i, differentiator := range s.differentiators {
+					differentiator.differ(s.bufferedValues[i], 0, s.batch.ColVec(i), 0, &differ)
+				}
+				if differ {
+					// Buffered tuples comprise a full chunk, so we proceed to emitting
+					// it.
+					s.state = chunkerEmittingFromBuffer
+					continue
+				}
+
+				// The first tuple of s.batch belongs to the chunks that is being
+				// buffered.
+				if len(s.chunks) == 1 {
+					// All tuples in s.batch belong to the same chunk that is being
+					// buffered. Possibly tuples from the next batch will also belong to
+					// this chunk, so we buffer the full s.batch.
+					s.buffer(0 /* start */, s.batch.Length())
+					s.state = chunkerReading
+					continue
+				}
+				// First s.chunks[1] tuples belong to the same chunk that is being
+				// buffered, so we buffer them and proceed to emitting all buffered
+				// tuples.
+				s.buffer(0 /* start */, uint16(s.chunks[1]))
+				s.chunksProcessedIdx = 1
+				s.state = chunkerEmittingFromBuffer
+				continue
+			}
+		case chunkerEmittingFromBuffer:
+			newEmitted := s.emittedFromBuffer + ColBatchSize
+			if newEmitted > s.buffered {
+				newEmitted = s.buffered
+			}
+			s.output.SetLength(uint16(newEmitted - s.emittedFromBuffer))
+			if s.output.Length() == 0 {
+				s.state = chunkerEmittingFromBatch
+				s.buffered = 0
+				s.emittedFromBuffer = 0
+				s.shouldEmitChunkEnd = false
+				return s.output
+			}
+			for i := 0; i < len(s.bufferedValues); i++ {
+				s.output.ColVec(i).Copy(s.bufferedValues[i], s.emittedFromBuffer, newEmitted, s.inputTypes[i])
+			}
+			s.emittedFromBuffer = newEmitted
+			return s.output
+		case chunkerEmittingFromBatch:
+			if s.shouldEmitChunkEnd {
+				// Emit a zero-length batch to indicate the end of the last emitted
+				// chunk.
+				s.output.SetLength(0)
+				s.shouldEmitChunkEnd = false
+				return s.output
+			}
+			if s.chunksProcessedIdx+1 < len(s.chunks) {
+				// This chunk is fully contained within s.batch, so we emit it right
+				// away.
+				chunkStart := s.chunks[s.chunksProcessedIdx]
+				chunkEnd := s.chunks[s.chunksProcessedIdx+1]
+				s.output.SetLength(uint16(chunkEnd - chunkStart))
+				for i := 0; i < len(s.inputTypes); i++ {
+					s.output.ColVec(i).Copy(s.batch.ColVec(i), chunkStart, chunkEnd, s.inputTypes[i])
+				}
+				s.chunksProcessedIdx++
+				s.shouldEmitChunkEnd = true
+				return s.output
+			} else if s.chunksProcessedIdx == len(s.chunks)-1 {
+				// Other tuples might belong to this chunk, so we buffer it.
+				s.buffer(uint16(s.chunks[s.chunksProcessedIdx]), s.batch.Length())
+				// All tuples in s.batch have been processed, so we reset s.chunks and
+				// the corresponding variables.
+				s.chunks = s.chunks[:0]
+				s.chunksProcessedIdx = 0
+				s.shouldEmitChunkEnd = false
+				s.state = chunkerReading
+			} else {
+				// All tuples in s.batch have been emitted.
+				if s.inputDone {
+					s.output.SetLength(0)
+					return s.output
+				}
+				panic(fmt.Sprintf("unexpected: chunkerEmittingFromBatch state" +
+					"when s.chunks is fully processed and input is not done"))
+			}
+		default:
+			panic(fmt.Sprintf("invalid chunker operator state %v", s.state))
+		}
+	}
+}
+
+// buffer appends all tuples in range [start,end) from s.batch to already
+// buffered tuples.
+func (s *chunker) buffer(start uint16, end uint16) {
+	if start == 0 {
+		for i := 0; i < len(s.bufferedValues); i++ {
+			s.bufferedValues[i].Append(
+				s.batch.ColVec(i),
+				s.inputTypes[i],
+				s.buffered,
+				end,
+			)
+		}
+	} else {
+		for i := 0; i < len(s.bufferedValues); i++ {
+			s.bufferedValues[i].AppendSlice(
+				s.batch.ColVec(i),
+				s.inputTypes[i],
+				s.buffered,
+				start,
+				end,
+			)
+		}
+	}
+	s.buffered += uint64(end - start)
 }
--- pkg/sql/exec/sort_test.go
+++ pkg/sql/exec/sort_test.go
@@ -203,7 +203,7 @@ func BenchmarkSort(b *testing.B) {
 					ordCols[i].Direction = distsqlpb.Ordering_Column_Direction(rng.Int() % 2)
 
 					col := batch.ColVec(i).Int64()
-					for j := 0; i < ColBatchSize; i++ {
+					for j := 0; j < ColBatchSize; j++ {
 						col[j] = rng.Int63() % int64((j*1024)+1)
 					}
 				}
@@ -224,6 +224,316 @@ func BenchmarkSort(b *testing.B) {
 					}
 				}
 			})
+		}
+	}
+}
+
+func TestChunker(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	tcs := []struct {
+		tuples   tuples
+		expected []tuples
+		matchLen int
+		typ      []types.T
+	}{
+		{
+			tuples: tuples{{0, 2}},
+			expected: []tuples{
+				{{0, 2}},
+			},
+			typ:      []types.T{types.Int64, types.Int64},
+			matchLen: 1,
+		},
+		{
+			tuples: tuples{{0, 2}, {0, 2}},
+			expected: []tuples{
+				{{0, 2}, {0, 2}},
+			},
+			typ:      []types.T{types.Int64, types.Int64},
+			matchLen: 1,
+		},
+		{
+			tuples: tuples{{0, 2}, {0, 1}, {0, 3}, {0, 1}},
+			expected: []tuples{
+				{{0, 1}, {0, 2}, {0, 3}, {0, 1}},
+			},
+			typ:      []types.T{types.Int64, types.Int64},
+			matchLen: 1,
+		},
+		{
+			tuples: tuples{{0, 1}, {0, 1}, {0, 3}, {2, 3}, {2, 1}},
+			expected: []tuples{
+				{{0, 1}, {0, 1}, {0, 3}},
+				{{2, 1}, {2, 3}},
+			},
+			typ:      []types.T{types.Int64, types.Int64},
+			matchLen: 1,
+		},
+		{
+			tuples: tuples{{0, 2}, {0, 1}, {0, 3}, {2, 3}, {2, 4}, {2, 1}},
+			expected: []tuples{
+				{{0, 2}, {0, 1}, {0, 3}},
+				{{2, 3}, {2, 4}, {2, 1}},
+			},
+			typ:      []types.T{types.Int64, types.Int64},
+			matchLen: 1,
+		},
+		{
+			tuples: tuples{{0, 0, 2}, {0, 0, 1}, {0, 2, 3}, {0, 2, 3}, {0, 2, 4}, {1, 2, 1}},
+			expected: []tuples{
+				{{0, 0, 2}, {0, 0, 1}},
+				{{0, 2, 3}, {0, 2, 3}, {0, 2, 4}},
+				{{1, 2, 1}},
+			},
+			typ:      []types.T{types.Int64, types.Int64, types.Int64},
+			matchLen: 2,
+		},
+	}
+	for _, tc := range tcs {
+		runTests(t, []tuples{tc.tuples}, func(t *testing.T, input []Operator) {
+			s, err := newChunker(input[0], tc.typ, tc.matchLen)
+			if err != nil {
+				t.Fatal(err)
+			}
+			cols := make([]int, len(tc.typ))
+			for i := range cols {
+				cols[i] = i
+			}
+			chunk := 0
+			for !s.inputDone {
+				out := newOpTestOutput(s, cols, tc.expected[chunk])
+				if err := out.VerifyAnyOrder(); err != nil {
+					t.Fatal(err)
+				}
+				chunk++
+			}
+		})
+	}
+}
+
+func TestSortChunks(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	tcs := []struct {
+		tuples   tuples
+		expected tuples
+		typ      []types.T
+		ordCols  []distsqlpb.Ordering_Column
+		matchLen int
+	}{
+		{
+			tuples:   tuples{{1}, {1}, {1}, {1}, {1}, {1}, {1}, {1}, {1}, {1}},
+			expected: tuples{{1}, {1}, {1}, {1}, {1}, {1}, {1}, {1}, {1}, {1}},
+			typ:      []types.T{types.Int64},
+			ordCols:  []distsqlpb.Ordering_Column{{ColIdx: 0}},
+			matchLen: 1,
+		},
+		{
+			tuples:   tuples{{1, 2}, {1, 2}, {1, 3}, {1, 1}, {5, 5}, {6, 6}, {6, 1}},
+			expected: tuples{{1, 1}, {1, 2}, {1, 2}, {1, 3}, {5, 5}, {6, 1}, {6, 6}},
+			typ:      []types.T{types.Int64, types.Int64},
+			ordCols:  []distsqlpb.Ordering_Column{{ColIdx: 0}, {ColIdx: 1}},
+			matchLen: 1,
+		},
+		{
+			tuples: tuples{
+				{0, 1, 0},
+				{0, 1, 0},
+				{0, 1, 1},
+				{0, 0, 1},
+				{0, 0, 0},
+			},
+			expected: tuples{
+				{0, 0, 0},
+				{0, 0, 1},
+				{0, 1, 0},
+				{0, 1, 0},
+				{0, 1, 1},
+			},
+			typ:      []types.T{types.Int64, types.Int64, types.Int64},
+			ordCols:  []distsqlpb.Ordering_Column{{ColIdx: 0}, {ColIdx: 1}, {ColIdx: 2}},
+			matchLen: 1,
+		},
+		{
+			tuples: tuples{
+				{0, 1, 2},
+				{0, 1, 0},
+				{0, 1, 1},
+				{0, 2, 1},
+				{0, 2, 0},
+			},
+			expected: tuples{
+				{0, 1, 0},
+				{0, 1, 1},
+				{0, 1, 2},
+				{0, 2, 0},
+				{0, 2, 1},
+			},
+			typ:      []types.T{types.Int64, types.Int64, types.Int64},
+			ordCols:  []distsqlpb.Ordering_Column{{ColIdx: 0}, {ColIdx: 1}, {ColIdx: 2}},
+			matchLen: 2,
+		},
+	}
+	for _, tc := range tcs {
+		runTests(t, []tuples{tc.tuples}, func(t *testing.T, input []Operator) {
+			sorter, err := NewSortChunks(input[0], tc.typ, tc.ordCols, tc.matchLen)
+			if err != nil {
+				t.Fatal(err)
+			}
+			cols := make([]int, len(tc.typ))
+			for i := range cols {
+				cols[i] = i
+			}
+			out := newOpTestOutput(sorter, cols, tc.expected)
+
+			if err := out.Verify(); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func TestSortChunksRandomized(t *testing.T) {
+	rng, _ := randutil.NewPseudoRand()
+	nCols := 2
+	nTups := 8
+	typs := make([]types.T, nCols)
+	ordCols := make([]distsqlpb.Ordering_Column, nCols)
+	for i := range typs {
+		ordCols[i].ColIdx = uint32(i)
+		ordCols[i].Direction = distsqlpb.Ordering_Column_Direction(rng.Int() % 2)
+		typs[i] = types.Int64
+	}
+	tups := make(tuples, nTups)
+	for i := range tups {
+		tups[i] = make(tuple, nCols)
+		for j := range tups[i] {
+			// Small range so we can test partitioning
+			tups[i][j] = rng.Int63() % 2048
+		}
+	}
+
+	// Sort tups on the first column needed for sort chunks operator.
+	sortedTups := make(tuples, nTups)
+	copy(sortedTups, tups)
+	sort.Slice(sortedTups, func(i, j int) bool {
+		if sortedTups[i][ordCols[0].ColIdx].(int64) < sortedTups[j][ordCols[0].ColIdx].(int64) {
+			return ordCols[0].Direction == distsqlpb.Ordering_Column_ASC
+		} else if sortedTups[i][ordCols[0].ColIdx].(int64) > sortedTups[j][ordCols[0].ColIdx].(int64) {
+			return ordCols[0].Direction == distsqlpb.Ordering_Column_DESC
+		}
+		return false
+	})
+
+	expected := make(tuples, nTups)
+	copy(expected, tups)
+	sort.Slice(expected, func(i, j int) bool {
+		for k := 0; k < nCols; k++ {
+			l := ordCols[k].ColIdx
+			if expected[i][l].(int64) < expected[j][l].(int64) {
+				return ordCols[k].Direction == distsqlpb.Ordering_Column_ASC
+			} else if expected[i][l].(int64) > expected[j][l].(int64) {
+				return ordCols[k].Direction == distsqlpb.Ordering_Column_DESC
+			}
+		}
+		return false
+	})
+
+	runTests(t, []tuples{sortedTups}, func(t *testing.T, input []Operator) {
+		sorter, err := NewSortChunks(input[0], typs, ordCols, 1 /* matchLen */)
+		if err != nil {
+			t.Fatal(err)
+		}
+		cols := make([]int, len(typs))
+		for i := range cols {
+			cols[i] = i
+		}
+		out := newOpTestOutput(sorter, cols, expected)
+
+		if err := out.Verify(); err != nil {
+			t.Fatalf("for input %v:\n%v", sortedTups, err)
+		}
+	})
+}
+
+func BenchmarkSortChunks(b *testing.B) {
+	rng, _ := randutil.NewPseudoRand()
+
+	sorterConstructors := []func(Operator, []types.T, []distsqlpb.Ordering_Column, int) (Operator, error){
+		NewSortChunks,
+		func(input Operator, inputTypes []types.T, orderingCols []distsqlpb.Ordering_Column, _ int) (Operator, error) {
+			return NewSorter(input, inputTypes, orderingCols)
+		},
+	}
+	sorterNames := []string{"CHUNKS", "ALL"}
+	for _, nBatches := range []int{1 << 2, 1 << 6} {
+		for _, nCols := range []int{2, 4} {
+			for _, matchLen := range []int{1, 2, 3} {
+				for _, avgChunkSize := range []int{1 << 2, 1 << 5, 1 << 8} {
+					for sorterIdx, sorterConstructor := range sorterConstructors {
+						if matchLen >= nCols {
+							continue
+						}
+						b.Run(
+							fmt.Sprintf("%s/rows=%d/cols=%d/matchLen=%d/avgChunkSize=%d",
+								sorterNames[sorterIdx], nBatches*ColBatchSize, nCols, matchLen, avgChunkSize),
+							func(b *testing.B) {
+								// 8 (bytes / int64) * nBatches (number of batches) * ColBatchSize (rows /
+								// batch) * nCols (number of columns / row).
+								b.SetBytes(int64(8 * nBatches * ColBatchSize * nCols))
+								typs := make([]types.T, nCols)
+								for i := range typs {
+									typs[i] = types.Int64
+								}
+								batch := NewMemBatch(typs)
+								batch.SetLength(ColBatchSize)
+								ordCols := make([]distsqlpb.Ordering_Column, nCols)
+								for i := range ordCols {
+									ordCols[i].ColIdx = uint32(i)
+									if i < matchLen {
+										ordCols[i].Direction = distsqlpb.Ordering_Column_Direction(distsqlpb.Ordering_Column_ASC)
+									} else {
+										ordCols[i].Direction = distsqlpb.Ordering_Column_Direction(rng.Int() % 2)
+									}
+
+									col := batch.ColVec(i).Int64()
+									col[0] = 0
+									for j := 1; j < ColBatchSize; j++ {
+										if i < matchLen {
+											col[j] = col[j-1]
+											if rng.Float64() < 1.0/float64(avgChunkSize) {
+												col[j]++
+											}
+										} else {
+											col[j] = rng.Int63() % int64((j*1024)+1)
+										}
+									}
+								}
+								rowsTotal := nBatches * ColBatchSize
+								b.ResetTimer()
+								for n := 0; n < b.N; n++ {
+									source := newFiniteChunksSource(batch, nBatches, matchLen)
+									sorter, err := sorterConstructor(source, typs, ordCols, matchLen)
+									if err != nil {
+										b.Fatal(err)
+									}
+
+									sorter.Init()
+									rowsEmitted := 0
+									for rowsEmitted < rowsTotal {
+										out := sorter.Next()
+										if out.Length() == 0 {
+											b.Fail()
+										}
+										rowsEmitted += int(out.Length())
+									}
+								}
+								b.StopTimer()
+							})
+					}
+				}
+			}
 		}
 	}
 }
