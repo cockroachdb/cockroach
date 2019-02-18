a957b9e729dff5711fc9c7c6057430dab4055f21
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2019-02-18 09:44:31 +0100

    changefeedccl: unskip TestChangefeedDataTTL
    It passes with a minor tweak to an error message.
    
    Release note: None

--- pkg/ccl/changefeedccl/changefeed_test.go
+++ pkg/ccl/changefeedccl/changefeed_test.go
@@ -1114,10 +1114,6 @@ func TestChangefeedDataTTL(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 
 	testFn := func(t *testing.T, db *gosql.DB, f cdctest.TestFeedFactory) {
-		if PushEnabled.Get(&f.Server().ClusterSettings().SV) {
-			t.Skip(`covered in storage.TestReplicaRangefeed`)
-		}
-
 		// Set a very simple channel-based, wait-and-resume function as the
 		// BeforeEmitRow hook.
 		var shouldWait int32
