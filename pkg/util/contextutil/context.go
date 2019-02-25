3df5e9757f0c6bc8db2dcdbdf45114a6bf9f308a
Jordan Lewis <jordanthelewis@gmail.com>
2019-02-25 11:51:35 -0500

    contextutil: RunWithTimeout preserves cause
    Previously, RunWithTimeout could throw away the error cause in some
    cases. Prevent this from happening.
    
    Release note: None

--- pkg/util/contextutil/context.go
+++ pkg/util/contextutil/context.go
@@ -48,6 +48,7 @@ func wrap(ctx context.Context, cancel context.CancelFunc) (context.Context, cont
 type TimeoutError struct {
 	operation string
 	duration  time.Duration
+	cause     error
 }
 
 func (t TimeoutError) Error() string {
@@ -61,10 +62,13 @@ func (TimeoutError) Timeout() bool { return true }
 func (TimeoutError) Temporary() bool { return true }
 
 // Cause implements Causer.
-func (TimeoutError) Cause() error {
+func (t TimeoutError) Cause() error {
 	// This ensures that people looking for DeadlineExceeded in particular still
 	// see it.
-	return context.DeadlineExceeded
+	if t.cause == nil {
+		return context.DeadlineExceeded
+	}
+	return t.cause
 }
 
 // We implement net.Error the same way that context.DeadlineExceeded does, so
@@ -87,6 +91,7 @@ func RunWithTimeout(
 		return TimeoutError{
 			operation: op,
 			duration:  timeout,
+			cause:     err,
 		}
 	}
 	return err
