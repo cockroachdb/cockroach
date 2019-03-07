f9bbba9e4b2bf1cd5af7815a6b1f048407e59634
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2019-03-07 18:10:20 +0100

    roachtest: remove redundant --sequential flags
    These are redundant now that --sequential defaults to true.
    
    Release note: None

--- pkg/cmd/roachtest/store_gen.go
+++ pkg/cmd/roachtest/store_gen.go
@@ -65,7 +65,7 @@ func registerStoreGen(r *registry, args []string) {
 		Run: func(ctx context.Context, t *test, c *cluster) {
 			c.Put(ctx, cockroach, "./cockroach")
 			c.Put(ctx, workload, "./workload")
-			c.Start(ctx, t, startArgs("--sequential"))
+			c.Start(ctx, t)
 
 			{
 				m := newMonitor(ctx, c)
