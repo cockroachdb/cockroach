cockroachdb/cockroach

commit 6c1e1ac4f08c46da09d87d74fe91382cd1573028

jobs, sql, ui: Create `AutoCreateStats` job type
With #34279, enabling the cluster setting
`sql.stats.experimental_automatic_collection.enabled` has the potential
to create many CreateStats jobs, which can cause the Jobs view on the
AdminUI to become cluttered.

This commit creates a new `AutoCreateStats` job type for these auto-created
CreateStats jobs, so that users are able to still see their own manual runs
of CREATE STATISTICS, via the pre-existing `CreateStats` type.

![jobs-auto-create-stats](https://user-images.githubusercontent.com/3051672/54212467-5cea2c80-44b9-11e9-9c11-db749814f019.gif)

Release note (admin ui change): AutoCreateStats type added to
Jobs page to filter automatic statistics jobs.

Fixes #34377.

pkg/ui/src/views/jobs/index.tsx
@@ -64,6 +64,7 @@ const typeOptions = [
   { value: JobType.SCHEMA_CHANGE.toString(), label: "Schema Changes" },
   { value: JobType.CHANGEFEED.toString(), label: "Changefeed"},
   { value: JobType.CREATE_STATS.toString(), label: "Statistics Creation"},
+  { value: JobType.AUTO_CREATE_STATS.toString(), label: "Auto-Statistics Creation"},
 ];
 
 const typeSetting = new LocalSetting<AdminUIState, number>(

pkg/jobs/jobspb/jobs.proto
@@ -281,4 +281,5 @@ enum Type {
   IMPORT = 4 [(gogoproto.enumvalue_customname) = "TypeImport"];
   CHANGEFEED = 5 [(gogoproto.enumvalue_customname) = "TypeChangefeed"];
   CREATE_STATS = 6 [(gogoproto.enumvalue_customname) = "TypeCreateStats"];
+  AUTO_CREATE_STATS = 7 [(gogoproto.enumvalue_customname) = "TypeAutoCreateStats"];
 }

pkg/jobs/jobspb/wrap.go
@@ -19,6 +19,7 @@ import (
 	"strings"
 
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
+	"github.com/cockroachdb/cockroach/pkg/sql/stats"
 )
 
 // Details is a marker interface for job details proto structs.
@@ -58,6 +59,10 @@ func DetailsType(d isPayload_Details) Type {
 	case *Payload_Changefeed:
 		return TypeChangefeed
 	case *Payload_CreateStats:
+		createStatsName := d.(*Payload_CreateStats).CreateStats.Name
+		if createStatsName == stats.AutoStatsName {
+			return TypeAutoCreateStats
+		}
 		return TypeCreateStats
 	default:
 		panic(fmt.Sprintf("Payload.Type called on a payload with an unknown details type: %T", d))

pkg/sql/create_stats.go
@@ -383,7 +383,7 @@ func checkRunningJobs(ctx context.Context, job *jobs.Job, p *planner) error {
 			return err
 		}
 
-		if payload.Type() == jobspb.TypeCreateStats {
+		if payload.Type() == jobspb.TypeCreateStats || payload.Type() == jobspb.TypeAutoCreateStats {
 			id := (*int64)(row[0].(*tree.DInt))
 			if *id == jobID {
 				break
@@ -443,7 +443,7 @@ func (r *createStatsResumer) OnTerminal(
 
 func init() {
 	jobs.AddResumeHook(func(typ jobspb.Type, settings *cluster.Settings) jobs.Resumer {
-		if typ != jobspb.TypeCreateStats {
+		if typ != jobspb.TypeCreateStats && typ != jobspb.TypeAutoCreateStats {
 			return nil
 		}
 		return &createStatsResumer{}

commit fa71a3d742d1f5cffa41761345b97a26cfe5d5fe

ui: fix word-wrapping and tooltip spacing on jobs page
- Fixed spacing for tooltip copy
- Fixed styling bug that was introduced in #35651: Job description
  previously truncated long job descriptions in collapsed view.

Tooltip copy - before:

<img width="300" alt="jobs before" src="https://user-images.githubusercontent.com/3051672/54454081-0be36e00-472f-11e9-8fc2-7e59b4565d51.png">

Tooltip copy - after:

<img width="300" alt="fix tooltip-spacing" src="https://user-images.githubusercontent.com/3051672/54454082-0be36e00-472f-11e9-9683-3b63490fbfe1.png">

Job description - before:

<img width="400" alt="broken text-wrapping" src="https://user-images.githubusercontent.com/3051672/54454213-58c74480-472f-11e9-81fd-7e075510d96f.png">

Job description - after:

<img width="400" alt="fix text-overflow" src="https://user-images.githubusercontent.com/3051672/54454227-5e248f00-472f-11e9-96b3-2f38a7b6dd6e.png">

Release note: None

Fixes: #35604

pkg/ui/src/views/jobs/index.styl
@@ -66,7 +66,7 @@
     white-space nowrap
     max-width 500px
     word-wrap break-word
-    &--sql
+    .sql
       font-family Inconsolata-Regular
 
   &__status-icon

pkg/ui/src/views/jobs/index.tsx
@@ -200,7 +200,7 @@ const jobsTableColumns: ColumnDescriptor<Job>[] = [
       // If a [SQL] job.statement exists, it means that job.description
       // is a human-readable message. Otherwise job.description is a SQL
       // statement.
-      const additionalStyle = (job.statement ? "" : "--sql");
+      const additionalStyle = (job.statement ? "" : " sql");
       return <div className={`jobs-table__cell--description${additionalStyle}`}>{job.description}</div>;
     },
     sort: job => job.description,
@@ -244,8 +244,8 @@ interface JobsTableProps {
 const titleTooltip = (
   <span>
     Some jobs can be paused or canceled through SQL. For details, view the docs
-    on the <a href={docsURL.pauseJob} target="_blank"><code>PAUSE JOB</code></a>
-    and <a href={docsURL.cancelJob} target="_blank"><code>CANCEL JOB</code></a>
+    on the <a href={docsURL.pauseJob} target="_blank"><code>PAUSE JOB</code></a>{" "}
+    and <a href={docsURL.cancelJob} target="_blank"><code>CANCEL JOB</code></a>{" "}
     statements.
   </span>
 );