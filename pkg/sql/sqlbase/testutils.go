35a5fbe02d627b38df88e037b151414ff8ec376b
Yahor Yuzefovich <yahor@cockroachlabs.com>
2019-03-22 19:19:44 -0700

    distsqlrun: add test infra to compare results of processors and operators
    Adds test infrastructure that sets up a processor and the corresponding
    columnar operator (as well as necessary columnarizers and materializers),
    runs both paths, and checks whether the output matches.
    
    Also, adds tests for general sorter and sort chunks using the introduced
    infrastructure.
    
    Release note: None

--- pkg/sql/sqlbase/testutils.go
+++ pkg/sql/sqlbase/testutils.go
@@ -686,6 +686,19 @@ func MakeRandIntRows(rng *rand.Rand, numRows int, numCols int) EncDatumRows {
 	return rows
 }
 
+// MakeRandIntRowsModulus constructs a numRows * numCols table where the values
+// are random integers in the range [0, modulus).
+func MakeRandIntRowsModulus(rng *rand.Rand, numRows int, numCols int, modulus int) EncDatumRows {
+	rows := make(EncDatumRows, numRows)
+	for i := range rows {
+		rows[i] = make(EncDatumRow, numCols)
+		for j := 0; j < numCols; j++ {
+			rows[i][j] = IntEncDatum(rng.Intn(modulus))
+		}
+	}
+	return rows
+}
+
 // MakeRepeatedIntRows constructs a numRows x numCols table where blocks of n
 // consecutive rows have the same value.
 func MakeRepeatedIntRows(n int, numRows int, numCols int) EncDatumRows {
