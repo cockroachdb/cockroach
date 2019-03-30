b7fc3d5ed430b1cc7ba6fa9c09ad426d29c40a55
Andrew Kimball <andyk@cockroachlabs.com>
2019-03-29 18:40:07 -0700

    sql: Show more specific types when available
    Update the String() method on types.T to show a type name that is closer to what
    coltypes.T shows. For example, varchar and bit are currently never returned by
    types.T, which instead maps them to string and varbit, respectively.
    
    This commit prepares for unification of types.T and coltypes.T.
    
    Release note: None

--- pkg/sql/sem/types/types.go
+++ pkg/sql/sem/types/types.go
@@ -437,33 +437,48 @@ func (t *ColumnType) DebugString() string {
 	return (*InternalColumnType)(t).String()
 }
 
-func (t *ColumnType) String() string {
-	switch t.Oid() {
-	case oid.T_name:
-		return "name"
-	}
-
+// Name returns a single word description of the type that describes it
+// succinctly, but without all the details, such as width, locale, etc. The name
+// is sometimes the same as the name returned by SQLStandardName, but is more
+// CRDB friendly.
+//
+// TODO(andyk): Should these be changed to be the same as SQLStandardName?
+func (t *ColumnType) Name() string {
 	switch t.SemanticType {
 	case NULL:
 		return "unknown"
 	case BOOL:
 		return "bool"
 	case INT:
+		switch t.Width {
+		case 16:
+			return "int2"
+		case 32:
+			return "int4"
+		}
 		return "int"
-	case STRING:
+	case STRING, COLLATEDSTRING:
+		switch t.Oid() {
+		case oid.T_bpchar:
+			return "char"
+		case oid.T_char:
+			// Yes, that's the name. The ways of PostgreSQL are inscrutable.
+			return `"char"`
+		case oid.T_varchar:
+			return "varchar"
+		case oid.T_name:
+			return "name"
+		}
 		return "string"
 	case BIT:
-		return "varbit"
+		if t.Oid() == oid.T_varbit {
+			return "varbit"
+		}
+		return "bit"
 	case FLOAT:
 		return "float"
 	case DECIMAL:
 		return "decimal"
-	case COLLATEDSTRING:
-		if *t.Locale == "" {
-			// Used in telemetry.
-			return "collatedstring{*}"
-		}
-		return fmt.Sprintf("collatedstring{%s}", *t.Locale)
 	case BYTES:
 		return "bytes"
 	case DATE:
@@ -483,6 +498,29 @@ func (t *ColumnType) String() string {
 	case INET:
 		return "inet"
 	case TUPLE:
+		// TUPLE is currently an anonymous type, with no name.
+		return ""
+	case ARRAY:
+		return t.ArrayContents.String() + "[]"
+	case ANY:
+		return "anyelement"
+	case OID:
+		return t.SQLStandardName()
+	}
+
+	panic(pgerror.NewAssertionErrorf("unexpected SemanticType: %s", t.SemanticType))
+}
+
+func (t *ColumnType) String() string {
+	switch t.SemanticType {
+	case COLLATEDSTRING:
+		if *t.Locale == "" {
+			// Used in telemetry.
+			return fmt.Sprintf("collated%s{*}", t.Name())
+		}
+		return fmt.Sprintf("collated%s{%s}", t.Name(), *t.Locale)
+
+	case TUPLE:
 		var buf bytes.Buffer
 		buf.WriteString("tuple")
 		if len(t.TupleContents) != 0 && !IsWildcardTupleType(t) {
@@ -500,15 +538,8 @@ func (t *ColumnType) String() string {
 			buf.WriteByte('}')
 		}
 		return buf.String()
-	case ARRAY:
-		return t.ArrayContents.String() + "[]"
-	case ANY:
-		return "anyelement"
-	case OID:
-		return t.SQLStandardName()
 	}
-
-	panic(pgerror.NewAssertionErrorf("unexpected SemanticType: %s", t.SemanticType))
+	return t.Name()
 }
 
 var (
