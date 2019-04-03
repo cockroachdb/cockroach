2b90b63776cc65512841c8e84d17c35f06523dbd
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2019-04-03 14:58:08 +0200

    server: make engine stall checker warn, not fatal, by default
    The environment variable will allows us to go back to testing the
    absence of stalls, though we may also chose to do something more
    programmatic based on RocksDB's internal tracking of write stalls.
    
    Release note: None

--- pkg/cmd/roachtest/disk_stall.go
+++ pkg/cmd/roachtest/disk_stall.go
@@ -108,7 +108,8 @@ func runDiskStalledDetection(
 	go func() {
 		t.WorkerStatus("running server")
 		out, err := c.RunWithBuffer(ctx, l, n,
-			fmt.Sprintf("timeout --signal 9 %ds env COCKROACH_ENGINE_MAX_SYNC_DURATION=%s COCKROACH_LOG_MAX_SYNC_DURATION=%s "+
+			fmt.Sprintf("timeout --signal 9 %ds env COCKROACH_ENGINE_MAX_SYNC_DURATION_FATAL=true "+
+				"COCKROACH_ENGINE_MAX_SYNC_DURATION=%s COCKROACH_LOG_MAX_SYNC_DURATION=%s "+
 				"./cockroach start --insecure --logtostderr=INFO --store {store-dir}/%s --log-dir {store-dir}/%s",
 				int(dur.Seconds()), maxDataSync, maxLogSync, dataDir, logDir,
 			),
