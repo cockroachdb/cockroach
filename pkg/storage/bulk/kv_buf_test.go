c489cbd63bac1f1c60b19a8d905ca2f26db566a1
David Taylor <tinystatemachine@gmail.com>
2019-04-05 18:33:45 +0000

    storage/bulk: use slab-backed buffer in BufferingAdder
    This uses the slab-backed key/value buffer to store the buffered kvs
    instead of individual slices. This cuts the per-pair overhead from 48b
    to 16b.
    
    Release note (performance improvement): improved memory efficiency of
    bulk ingestion and index backfills buffing.

--- pkg/storage/bulk/kv_buf_test.go
+++ pkg/storage/bulk/kv_buf_test.go
@@ -1,3 +1,17 @@
+// Copyright 2019 The Cockroach Authors.
+//
+/// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
 package bulk
 
 import (
@@ -5,9 +19,16 @@ import (
 	"sort"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 )
+
+// kvPair is a bytes -> bytes kv pair.
+type kvPair struct {
+	key   roachpb.Key
+	value []byte
+}
 
 func makeTestData(num int) (kvs []kvPair, totalSize int) {
 	kvs = make([]kvPair, num)
