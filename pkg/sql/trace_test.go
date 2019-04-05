4a72b8d47d1db1bf2ece609a8c1bdfbdfcba329a
Jordan Lewis <jordanthelewis@gmail.com>
2019-04-05 10:16:09 -0400

    distsqlpb: rename protos back to distsqlrun
    The move of code to the `distsqlpb` package also inadvertently renamed
    the actual protobuf service endpoints and messages, which represented an
    accidental breaking change between 2.1 and 19.1 and would result in
    errors in mixed clusters.
    
    This commit reverts that change, restoring harmony for mixed-version
    clusters.
    
    Release note: None

--- pkg/sql/trace_test.go
+++ pkg/sql/trace_test.go
@@ -135,7 +135,7 @@ func TestTrace(t *testing.T) {
 			// Depending on whether the data is local or not, we may not see these
 			// spans.
 			optionalSpans: []string{
-				"/cockroach.sql.distsqlpb.DistSQL/SetupFlow",
+				"/cockroach.sql.distsqlrun.DistSQL/SetupFlow",
 				"noop",
 			},
 		},
@@ -201,7 +201,7 @@ func TestTrace(t *testing.T) {
 			// Depending on whether the data is local or not, we may not see these
 			// spans.
 			optionalSpans: []string{
-				"/cockroach.sql.distsqlpb.DistSQL/SetupFlow",
+				"/cockroach.sql.distsqlrun.DistSQL/SetupFlow",
 				"noop",
 			},
 		},
