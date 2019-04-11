2cb2a64a7a2b0440bfbddcc9b125cf51fbf8153c
Solon Gordon <solon@cockroachlabs.com>
2019-04-11 12:08:45 -0400

    exec: support NOT LIKE
    like_ops_gen.go now also generates the negated version of its operators
    for use with NOT LIKE.
    
    Refers #34498
    
    Release note: None

--- pkg/sql/exec/like_ops.go
+++ pkg/sql/exec/like_ops.go
@@ -21,12 +21,19 @@ import (
 )
 
 // GetLikeOperator returns a selection operator which applies the specified LIKE
-// pattern. The implementation varies depending on the complexity of the
-// pattern.
+// pattern, or NOT LIKE if the negate argument is true. The implementation
+// varies depending on the complexity of the pattern.
 func GetLikeOperator(
-	ctx *tree.EvalContext, input Operator, colIdx int, pattern string,
+	ctx *tree.EvalContext, input Operator, colIdx int, pattern string, negate bool,
 ) (Operator, error) {
 	if pattern == "" {
+		if negate {
+			return &selNEBytesBytesConstOp{
+				input:    input,
+				colIdx:   colIdx,
+				constArg: []byte{},
+			}, nil
+		}
 		return &selEQBytesBytesConstOp{
 			input:    input,
 			colIdx:   colIdx,
@@ -34,12 +41,23 @@ func GetLikeOperator(
 		}, nil
 	}
 	if pattern == "%" {
+		if negate {
+			return NewZeroOp(input), nil
+		}
 		// Matches everything.
+		// TODO(solon): Replace this with a NOT NULL operator.
 		return NewNoop(input), nil
 	}
 	if len(pattern) > 1 && !strings.ContainsAny(pattern[1:len(pattern)-1], "_%") {
 		// Special cases for patterns which are just a prefix or suffix.
 		if pattern[0] == '%' {
+			if negate {
+				return &selNotSuffixBytesBytesConstOp{
+					input:    input,
+					colIdx:   colIdx,
+					constArg: []byte(pattern[1:]),
+				}, nil
+			}
 			return &selSuffixBytesBytesConstOp{
 				input:    input,
 				colIdx:   colIdx,
@@ -47,6 +65,13 @@ func GetLikeOperator(
 			}, nil
 		}
 		if pattern[len(pattern)-1] == '%' {
+			if negate {
+				return &selNotPrefixBytesBytesConstOp{
+					input:    input,
+					colIdx:   colIdx,
+					constArg: []byte(pattern[:len(pattern)-1]),
+				}, nil
+			}
 			return &selPrefixBytesBytesConstOp{
 				input:    input,
 				colIdx:   colIdx,
@@ -58,6 +83,13 @@ func GetLikeOperator(
 	re, err := tree.ConvertLikeToRegexp(ctx, pattern, false, '\\')
 	if err != nil {
 		return nil, err
+	}
+	if negate {
+		return &selNotRegexpBytesBytesConstOp{
+			input:    input,
+			colIdx:   colIdx,
+			constArg: re,
+		}, nil
 	}
 	return &selRegexpBytesBytesConstOp{
 		input:    input,
