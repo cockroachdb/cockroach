cockroachdb/cockroach

commit 47d26211bd24947130b8a83caeb06a4e207fcad3

build: fix harmless buglet in roachprod-stress binary name
Previously, running `roachprod-stress` on a package with a trailing
slash would result in a binary named `.test`. Now the binary is properly
named using the package name (i.e. `<package-basename>.test`).

See #34083

Release note: None

Makefile
@@ -868,7 +868,7 @@ stress stressrace:
 roachprod-stress roachprod-stressrace: bin/roachprod-stress
 	# The bootstrap target creates, among other things, ./bin/stress.
 	build/builder.sh make bin/.bootstrap
-	build/builder.sh make test GOFLAGS="$(GOFLAGS) -v -c -o $(notdir $(PKG)).test" PKG=$(PKG)
+	build/builder.sh make test GOFLAGS="$(GOFLAGS) -v -c -o $(notdir $(patsubst %/,%,$(PKG))).test" PKG=$(PKG)
 	@if [ -z "$(CLUSTER)" ]; then \
 	  echo "ERROR: missing or empty CLUSTER"; \
 	else \

commit d72bbd1ec2d787437e7d87b9456147009a3dfefb

Makefile: fix roachprod-stressrace
I think 47d26211bd24947130b8a83caeb06a4e207fcad3 broke this. I suspect
that it was tested when that change was made, but that an errant `.test`
file was still around and made it seem like it worked.

Release note: None

Makefile
@@ -908,7 +908,7 @@ roachprod-stress roachprod-stressrace: bin/roachprod-stress
 	@if [ -z "$(CLUSTER)" ]; then \
 	  echo "ERROR: missing or empty CLUSTER"; \
 	else \
-	  bin/roachprod-stress $(CLUSTER) $(STRESSFLAGS) ./$(notdir $(PKG)).test \
+	  bin/roachprod-stress $(CLUSTER) $(STRESSFLAGS) ./$(notdir $(patsubst %/,%,$(PKG))).test \
 	    -test.run "$(TESTS)" $(filter-out -v,$(TESTFLAGS)) -test.v -test.timeout $(TESTTIMEOUT); \
 	fi
 