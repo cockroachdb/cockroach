01573ed09831ab891677b52a49829f3b3cd8d389
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2019-04-15 17:38:44 -0400

    workload/tpcc: address TODO in configureZone
    This breaks compatibility with 2.0 nodes, but that's far enough back
    that we don't care anymore.
    
    Release note: None

--- pkg/workload/tpcc/partition.go
+++ pkg/workload/tpcc/partition.go
@@ -19,7 +19,6 @@ import (
 	"bytes"
 	gosql "database/sql"
 	"fmt"
-	"strings"
 
 	"github.com/pkg/errors"
 	"golang.org/x/exp/rand"
@@ -164,19 +163,9 @@ func configureZone(db *gosql.DB, table, partition string, constraint int, zones 
 		constraints = fmt.Sprintf("[+rack=%d]", constraint)
 	}
 
-	// We are removing the EXPERIMENTAL keyword in 2.1. For compatibility
-	// with 2.0 clusters we still need to try with it if the
-	// syntax without EXPERIMENTAL fails.
-	// TODO(knz): Remove this in 2.2.
 	sql := fmt.Sprintf(`ALTER PARTITION %s OF TABLE %s CONFIGURE ZONE USING constraints = '%s'`,
 		partition, table, constraints)
-	_, err := db.Exec(sql)
-	if err != nil && strings.Contains(err.Error(), "syntax error") {
-		sql = fmt.Sprintf(`ALTER PARTITION %s OF TABLE %s EXPERIMENTAL CONFIGURE ZONE 'constraints: %s'`,
-			partition, table, constraints)
-		_, err = db.Exec(sql)
-	}
-	if err != nil {
+	if _, err := db.Exec(sql); err != nil {
 		return errors.Wrapf(err, "Couldn't exec %q", sql)
 	}
 	return nil
