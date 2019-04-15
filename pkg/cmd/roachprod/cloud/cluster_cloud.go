cockroachdb/cockroach

commit 3e903d6f60bba99685705d4d9dd2438620affc92

roachprod: add a setup-ssh command
This PR refactors cluster creation to separate ssh set up from the rest
of cluster creation. It then makes this setup process idempotent and lastly
it exposes this step as an independent command.

Having setup-ssh as a separate command can be useful to recover from cases
where SSH setup might have failed or to update keys after new users were added
or if roachprod was updated to have new behavior.

Release note: None

pkg/cmd/roachprod/cloud/cluster_cloud.go
@@ -150,6 +150,16 @@ func (c *Cluster) IsLocal() bool {
 	return c.Name == config.Local
 }
 
+// HasProvider returns true if there is a vm in VMs with the given provider.
+func (c *Cluster) HasProvider(provider string) bool {
+	for i := range c.VMs {
+		if c.VMs[i].Provider == provider {
+			return true
+		}
+	}
+	return false
+}
+
 func namesFromVM(v vm.VM) (string, string, error) {
 	if v.IsLocal() {
 		return config.Local, config.Local, nil

pkg/cmd/roachprod/install/cluster_synced.go
@@ -521,7 +521,21 @@ func (c *SyncedCluster) Wait() error {
 	return nil
 }
 
-// SetupSSH TODO(peter): document
+// SetupSSH configures the cluster for use with SSH. This is generally run after
+// the cloud.Cluster has been synced which resets the SSH credentials on the
+// machines and sets them up for the current user. This method enables the
+// hosts to talk to eachother and optionally confiures additional keys to be
+// added to the hosts via the c.AuthorizedKeys field. It does so in the following
+// steps:
+//
+//   1. Creates an ssh key pair on the first host to be used on all hosts if
+//      none exists.
+//   2. Distributes the public key, private key, and authorized_keys file from
+//      the first host to the others.
+//   3. Merges the data in c.AuthorizedKeys with the existing authorized_keys
+//      files on all hosts.
+//
+// This call strives to be idempotent.
 func (c *SyncedCluster) SetupSSH() error {
 	if c.IsLocal() {
 		return nil
@@ -617,18 +631,24 @@ tar cf - .ssh/id_rsa .ssh/id_rsa.pub .ssh/authorized_keys
 		}
 		defer sess.Close()
 
-		// Note that this is not idempotent. If we run the ssh-keyscan multiple
-		// times on the same node the known_hosts file will grow. This isn't a
-		// problem for the usage here which only performs the keyscan once when the
-		// cluster is created. ssh-keyscan may return fewer than the desired number
-		// of entries if the remote nodes are not responding yet, so we loop until
-		// we have a scan that found host keys for all of the IPs.
+		// ssh-keyscan may return fewer than the desired number of entries if the
+		// remote nodes are not responding yet, so we loop until we have a scan that
+		// found host keys for all of the IPs. Merge the newly scanned keys with the
+		// existing list to make this process idempotent.
 		cmd := `
+set -e
+tmp1="$(tempfile -d ~/.ssh -p 'known_hosts.roachprod.tmp.' )"
+tmp2="$(tempfile -d ~/.ssh -p 'known_hosts.roachprod.tmp.' )"
+on_exit() {
+    rm -f "${tmp1}" "${tmp2}"
+}
+trap on_exit EXIT
 for i in {1..20}; do
-  ssh-keyscan -T 60 -t rsa ` + strings.Join(ips, " ") + ` > .ssh/known_hosts.tmp
-  if [ "$(cat .ssh/known_hosts.tmp | wc -l)" -eq "` + fmt.Sprint(len(ips)) + `" ]; then
-    cat .ssh/known_hosts.tmp >> .ssh/known_hosts
-    rm -f .ssh/known_hosts.tmp
+  ssh-keyscan -T 60 -t rsa ` + strings.Join(ips, " ") + ` > "${tmp1}"
+  if [[ "$(wc < ${tmp1} -l)" -eq "` + fmt.Sprint(len(ips)) + `" ]]; then
+    [[ -f .ssh/known_hosts ]] && cat .ssh/known_hosts >> "${tmp1}"
+    sort -u < "${tmp1}" > "${tmp2}"
+    mv "${tmp2}" .ssh/known_hosts
     exit 0
   fi
   sleep 1
@@ -638,9 +658,11 @@ exit 1
 		if out, err := sess.CombinedOutput(cmd); err != nil {
 			return nil, errors.Wrapf(err, "~ %s\n%s", cmd, out)
 		}
+
 		return nil, nil
 	})
 
+	// Note that if this is  will overwrite the existing keys
 	if len(c.AuthorizedKeys) > 0 {
 		c.Parallel("adding additional authorized keys", len(c.Nodes), 0, func(i int) ([]byte, error) {
 			sess, err := c.newSession(c.Nodes[i])
@@ -650,7 +672,20 @@ exit 1
 			defer sess.Close()
 
 			sess.SetStdin(bytes.NewReader(c.AuthorizedKeys))
-			cmd := `cat >> ~/.ssh/authorized_keys`
+
+			cmd := `
+keys_data="$(cat)"
+set -e
+tmp1="$(tempfile -d ~/.ssh -p 'authorized_keys.roachprod.tmp.' )"
+tmp2="$(tempfile -d ~/.ssh -p 'authorized_keys.roachprod.tmp.' )"
+on_exit() {
+    rm -f "${tmp1}" "${tmp2}"
+}
+trap on_exit EXIT
+[[ -f ~/.ssh/authorized_keys ]] && cat ~/.ssh/authorized_keys > "${tmp1}"
+sort -u < "${tmp1}" > "${tmp2}"
+mv "${tmp2}" ~/.ssh/authorized_keys
+`
 			if out, err := sess.CombinedOutput(cmd); err != nil {
 				return nil, errors.Wrapf(err, "~ %s\n%s", cmd, out)
 			}

pkg/cmd/roachprod/main.go
@@ -37,7 +37,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachprod/ssh"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachprod/ui"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachprod/vm"
-	_ "github.com/cockroachdb/cockroach/pkg/cmd/roachprod/vm/aws"
+	"github.com/cockroachdb/cockroach/pkg/cmd/roachprod/vm/aws"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachprod/vm/gce"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachprod/vm/local"
 	"github.com/cockroachdb/cockroach/pkg/util/flagutil"
@@ -356,77 +356,91 @@ Local Clusters
 		if createErr := cld.CreateCluster(clusterName, numNodes, createVMOpts); createErr != nil {
 			return createErr
 		}
-		if clusterName != config.Local {
-			{
-				cloud, err := cld.ListCloud()
-				if err != nil {
-					return err
-				}
 
-				c, ok := cloud.Clusters[clusterName]
-				if !ok {
-					return fmt.Errorf("could not find %s in list of cluster", clusterName)
-				}
-				c.PrintDetails()
-
-				// Run ssh-keygen -R serially on each new VM in case an IP address has been recycled
-				for _, v := range c.VMs {
-					cmd := exec.Command("ssh-keygen", "-R", v.PublicIP)
-					out, err := cmd.CombinedOutput()
-					if err != nil {
-						log.Printf("could not clear ssh key for hostname %s:\n%s", v.PublicIP, string(out))
-					}
-				}
-
-				if err := syncAll(cloud, false /* quiet */); err != nil {
-					return err
-				}
-			}
-
-			{
-				// Wait for the nodes in the cluster to start.
-				install.Clusters = map[string]*install.SyncedCluster{}
-				if err := loadClusters(); err != nil {
-					return err
-				}
-
-				c, err := newCluster(clusterName, false)
-				if err != nil {
-					return err
-				}
-				if err := c.Wait(); err != nil {
-					return err
-				}
-				// If we are using AWS we need to fetch public keys from gcloud to set
-				// up ssh access for other users.
-				var haveAWS bool
-				for _, p := range createVMOpts.VMProviders {
-					if haveAWS = p == "aws"; haveAWS {
-						break
-					}
-				}
-				if haveAWS {
-					c.AuthorizedKeys, err = gce.GetUserAuthorizedKeys()
-					// Don't fail if we can't get other user's authorized keys, just log.
-					if err != nil {
-						fmt.Fprintf(os.Stderr, "failed to retrieve authorized keys from gcloud: %v", err)
-					}
-				}
-				if err := c.SetupSSH(); err != nil {
-					return err
-				}
-			}
-		} else {
+		// Just create directories for the local cluster as there's no need for ssh.
+		if clusterName == config.Local {
 			for i := 0; i < numNodes; i++ {
 				err := os.MkdirAll(fmt.Sprintf(os.ExpandEnv("${HOME}/local/%d"), i+1), 0755)
 				if err != nil {
 					return err
 				}
 			}
+			return nil
 		}
-
-		return nil
+		return setupSSH(clusterName)
 	}),
+}
+
+var setupSSHCmd = &cobra.Command{
+	Use:   "setup-ssh <cluster>",
+	Short: "set up ssh for a cluster",
+	Long: `Sets up the keys and host keys for the vms in the cluster.
+
+It first resets the machine credentials as though the cluster were newly created
+using the cloud provider APIs and then proceeds to ensure that the hosts can
+SSH into eachother and lastly adds additional public keys to AWS hosts as read
+from the GCP project. This operation is performed as the last step of creating
+a new cluster but can be useful to re-run if the operation failed previously or
+if the user would like to update the keys on the remote hosts.
+`,
+
+	Args: cobra.ExactArgs(1),
+	Run: wrap(func(cmd *cobra.Command, args []string) (retErr error) {
+		clusterName, err := verifyClusterName(args[0])
+		if err != nil {
+			return err
+		}
+		return setupSSH(clusterName)
+	}),
+}
+
+func setupSSH(clusterName string) error {
+	cloud, err := cld.ListCloud()
+	if err != nil {
+		return err
+	}
+
+	cloudCluster, ok := cloud.Clusters[clusterName]
+	if !ok {
+		return fmt.Errorf("could not find %s in list of cluster", clusterName)
+	}
+	cloudCluster.PrintDetails()
+	// Run ssh-keygen -R serially on each new VM in case an IP address has been recycled
+	for _, v := range cloudCluster.VMs {
+		cmd := exec.Command("ssh-keygen", "-R", v.PublicIP)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Printf("could not clear ssh key for hostname %s:\n%s", v.PublicIP, string(out))
+		}
+	}
+
+	if err := syncAll(cloud, false /* quiet */); err != nil {
+		return err
+	}
+
+	// Wait for the nodes in the cluster to start.
+	install.Clusters = map[string]*install.SyncedCluster{}
+	if err := loadClusters(); err != nil {
+		return err
+	}
+
+	installCluster, err := newCluster(clusterName, false)
+	if err != nil {
+		return err
+	}
+	if err := installCluster.Wait(); err != nil {
+		return err
+	}
+	// If we are using AWS we need to fetch public keys from gcloud to set
+	// up ssh access for other users.
+	if cloudCluster.HasProvider(aws.ProviderName) {
+		installCluster.AuthorizedKeys, err = gce.GetUserAuthorizedKeys()
+		// Don't fail if we can't get other user's authorized keys, just log.
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to retrieve authorized keys from gcloud: %v", err)
+		}
+	}
+	return installCluster.SetupSSH()
 }
 
 func cleanupFailedCreate(clusterName string) error {
@@ -1407,6 +1421,7 @@ func main() {
 		listCmd,
 		syncCmd,
 		gcCmd,
+		setupSSHCmd,
 
 		statusCmd,
 		monitorCmd,

commit 84ea907bc6232589c964d5859333b4d78f930678

roachprod: distributed authorized keys file for shared ubuntu user on gcloud
This PR removes the aws specific logic for setting up the ubuntu user for shared
use on roachprod hosts. It also fixes a bug introduced in
3e903d6f60bba99685705d4d9dd2438620affc92 which inadvertently omitted the writing
of key_data to the authorized_keys file.

This is the first step in moving to a shared user model in roachprod for gcloud
like we already do in aws.

Release note: None

pkg/cmd/roachprod/cloud/cluster_cloud.go
@@ -150,16 +150,6 @@ func (c *Cluster) IsLocal() bool {
 	return c.Name == config.Local
 }
 
-// HasProvider returns true if there is a vm in VMs with the given provider.
-func (c *Cluster) HasProvider(provider string) bool {
-	for i := range c.VMs {
-		if c.VMs[i].Provider == provider {
-			return true
-		}
-	}
-	return false
-}
-
 func namesFromVM(v vm.VM) (string, string, error) {
 	if v.IsLocal() {
 		return config.Local, config.Local, nil

pkg/cmd/roachprod/install/cluster_synced.go
@@ -673,7 +673,8 @@ exit 1
 
 			sess.SetStdin(bytes.NewReader(c.AuthorizedKeys))
 
-			cmd := `
+			const sharedUser = "ubuntu"
+			const cmd = `
 keys_data="$(cat)"
 set -e
 tmp1="$(tempfile -d ~/.ssh -p 'authorized_keys.roachprod.tmp.' )"
@@ -683,9 +684,11 @@ on_exit() {
 }
 trap on_exit EXIT
 [[ -f ~/.ssh/authorized_keys ]] && cat ~/.ssh/authorized_keys > "${tmp1}"
+echo "${keys_data}" >> "${tmp1}"
 sort -u < "${tmp1}" > "${tmp2}"
-mv "${tmp2}" ~/.ssh/authorized_keys
-`
+sudo install --mode 0600 --owner ` + sharedUser +
+				` --group ` + sharedUser +
+				`"${tmp2}" ~` + sharedUser + `/.ssh/authorized_keys`
 			if out, err := sess.CombinedOutput(cmd); err != nil {
 				return nil, errors.Wrapf(err, "~ %s\n%s", cmd, out)
 			}

pkg/cmd/roachprod/main.go
@@ -37,7 +37,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachprod/ssh"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachprod/ui"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachprod/vm"
-	"github.com/cockroachdb/cockroach/pkg/cmd/roachprod/vm/aws"
+	_ "github.com/cockroachdb/cockroach/pkg/cmd/roachprod/vm/aws"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachprod/vm/gce"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachprod/vm/local"
 	"github.com/cockroachdb/cockroach/pkg/util/flagutil"
@@ -446,14 +446,11 @@ func setupSSH(clusterName string) error {
 	if err := installCluster.Wait(); err != nil {
 		return err
 	}
-	// If we are using AWS we need to fetch public keys from gcloud to set
-	// up ssh access for other users.
-	if cloudCluster.HasProvider(aws.ProviderName) {
-		installCluster.AuthorizedKeys, err = gce.GetUserAuthorizedKeys()
-		// Don't fail if we can't get other user's authorized keys, just log.
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to retrieve authorized keys from gcloud: %v", err)
-		}
+	// Fetch public keys from gcloud to set up ssh access for all users into the
+	// shared ubuntu user.
+	installCluster.AuthorizedKeys, err = gce.GetUserAuthorizedKeys()
+	if err != nil {
+		return errors.Wrap(err, "failed to retrieve authorized keys from gcloud")
 	}
 	return installCluster.SetupSSH()
 }