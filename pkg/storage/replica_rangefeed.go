cockroachdb/cockroach

commit 3e3a610b68614f4f8b26e44a4171522605e61078

storage: nudge slow closed timestamps being used by rangefeed
Changefeeds and, more proximately, RangeFeeds need closed timestamps to
keep advancing to make progress, but sometimes we have bugs that lead to
closed timestamps getting stuck. We should and will fix the bugs, but
given that even one stuck closed timestamp will stall changefeed-wide
progress, this commit introduces a fallback to try nudging the closed
timestamp when it's behind. Since these are unexected, we also log them
and keep a metric.

Touches #35142

Release note (bug fix): Made `CHANGEFEED`s more resilient to a class of
bugs which manifest as stalls.

pkg/storage/replica_rangefeed.go
@@ -24,14 +24,16 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/storage/batcheval/result"
+	"github.com/cockroachdb/cockroach/pkg/storage/closedts"
 	"github.com/cockroachdb/cockroach/pkg/storage/engine"
 	"github.com/cockroachdb/cockroach/pkg/storage/engine/enginepb"
 	"github.com/cockroachdb/cockroach/pkg/storage/intentresolver"
 	"github.com/cockroachdb/cockroach/pkg/storage/rangefeed"
 	"github.com/cockroachdb/cockroach/pkg/storage/storagepb"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
-	"github.com/cockroachdb/cockroach/pkg/util/limit"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 	"github.com/pkg/errors"
 )
@@ -128,9 +130,7 @@ func (i iteratorWithCloser) Close() {
 // complete. The provided ConcurrentRequestLimiter is used to limit the number
 // of rangefeeds using catchup iterators at the same time.
 func (r *Replica) RangeFeed(
-	args *roachpb.RangeFeedRequest,
-	stream roachpb.Internal_RangeFeedServer,
-	iteratorLimiter limit.ConcurrentRequestLimiter,
+	args *roachpb.RangeFeedRequest, stream roachpb.Internal_RangeFeedServer,
 ) *roachpb.Error {
 	if !RangefeedEnabled.Get(&r.store.cfg.Settings.SV) {
 		return roachpb.NewErrorf("rangefeeds require the kv.rangefeed.enabled setting. See " +
@@ -149,14 +149,8 @@ func (r *Replica) RangeFeed(
 		return roachpb.NewError(err)
 	}
 
-	// Make sure there's a leaseholder, otherwise nothing will close timestamps
-	// for this range, which means we won't make checkpoints.
-	if _, err := r.redirectOnOrAcquireLease(ctx); err != nil {
-		// We don't care if this replica is the leaseholder, only that someone is,
-		// so ignore NotLeaseHolderError.
-		if _, ok := err.GetDetail().(*roachpb.NotLeaseHolderError); !ok {
-			return err
-		}
+	if err := r.ensureClosedTimestampStarted(ctx); err != nil {
+		return err
 	}
 
 	checkTS := args.Timestamp
@@ -173,12 +167,13 @@ func (r *Replica) RangeFeed(
 	var iterSemRelease func()
 	if !args.Timestamp.IsEmpty() {
 		usingCatchupIter = true
-		if err := iteratorLimiter.Begin(ctx); err != nil {
+		lim := &r.store.limiters.ConcurrentRangefeedIters
+		if err := lim.Begin(ctx); err != nil {
 			return roachpb.NewError(err)
 		}
 		// Finish the iterator limit, but only if we exit before
 		// creating the iterator itself.
-		iterSemRelease = iteratorLimiter.Finish
+		iterSemRelease = lim.Finish
 		defer func() {
 			if iterSemRelease != nil {
 				iterSemRelease()
@@ -205,7 +200,7 @@ func (r *Replica) RangeFeed(
 	}
 
 	// Ensure that the rangefeed processor is running.
-	p := r.maybeInitRangefeedRaftMuLocked()
+	p := r.maybeInitRangefeedRaftMuLocked(ctx)
 
 	// Register the stream with a catch-up iterator.
 	var catchUpIter engine.SimpleIterator
@@ -255,7 +250,7 @@ const defaultEventChanCap = 4096
 
 // maybeInitRangefeedRaftMuLocked initializes a rangefeed for the Replica if one
 // is not already running. Requires raftMu be locked.
-func (r *Replica) maybeInitRangefeedRaftMuLocked() *rangefeed.Processor {
+func (r *Replica) maybeInitRangefeedRaftMuLocked(ctx context.Context) *rangefeed.Processor {
 	if r.raftMu.rangefeed != nil {
 		return r.raftMu.rangefeed
 	}
@@ -290,7 +285,7 @@ func (r *Replica) maybeInitRangefeedRaftMuLocked() *rangefeed.Processor {
 
 	// Check for an initial closed timestamp update immediately to help
 	// initialize the rangefeed's resolved timestamp as soon as possible.
-	r.handleClosedTimestampUpdateRaftMuLocked()
+	r.handleClosedTimestampUpdateRaftMuLocked(ctx)
 
 	return r.raftMu.rangefeed
 }
@@ -411,21 +406,53 @@ func (r *Replica) handleLogicalOpLogRaftMuLocked(ctx context.Context, ops *stora
 // handleClosedTimestampUpdate determines the current maximum closed timestamp
 // for the replica and informs the rangefeed, if one is running. No-op if a
 // rangefeed is not active.
-func (r *Replica) handleClosedTimestampUpdate() {
+func (r *Replica) handleClosedTimestampUpdate(ctx context.Context) {
+	ctx = r.AnnotateCtx(ctx)
 	r.raftMu.Lock()
 	defer r.raftMu.Unlock()
-	r.handleClosedTimestampUpdateRaftMuLocked()
+	r.handleClosedTimestampUpdateRaftMuLocked(ctx)
 }
 
 // handleClosedTimestampUpdateRaftMuLocked is like handleClosedTimestampUpdate,
 // but it requires raftMu to be locked.
-func (r *Replica) handleClosedTimestampUpdateRaftMuLocked() {
+func (r *Replica) handleClosedTimestampUpdateRaftMuLocked(ctx context.Context) {
 	if r.raftMu.rangefeed == nil {
 		return
 	}
 
 	// Determine what the maximum closed timestamp is for this replica.
-	closedTS := r.maxClosed(context.Background())
+	closedTS := r.maxClosed(ctx)
+
+	// If the closed timestamp is sufficiently stale, signal that we want an
+	// update to the leaseholder so that it will eventually begin to progress
+	// again.
+	slowClosedTSThresh := 5 * closedts.TargetDuration.Get(&r.store.cfg.Settings.SV)
+	if d := timeutil.Since(closedTS.GoTime()); d > slowClosedTSThresh {
+		m := r.store.metrics.RangeFeedMetrics
+		if m.RangeFeedSlowClosedTimestampLogN.ShouldLog() {
+			if closedTS.IsEmpty() {
+				log.Infof(ctx, "RangeFeed closed timestamp is empty")
+			} else {
+				log.Infof(ctx, "RangeFeed closed timestamp %s is behind by %s", closedTS, d)
+			}
+		}
+
+		// Asynchronously attempt to nudge the closed timestamp in case it's stuck.
+		key := fmt.Sprintf(`rangefeed-slow-closed-timestamp-nudge-r%d`, r.RangeID)
+		// Ignore the result of DoChan since, to keep this all async, it always
+		// returns nil and any errors are logged by the closure passed to the
+		// `DoChan` call.
+		_, _ = m.RangeFeedSlowClosedTimestampNudge.DoChan(key, func() (interface{}, error) {
+			// Also ignore the result of RunTask, since it only returns errors when
+			// the task didn't start because we're shutting down.
+			_ = r.store.stopper.RunTask(ctx, key, func(context.Context) {
+				if err := r.ensureClosedTimestampStarted(ctx); err != nil {
+					log.Infof(ctx, `RangeFeed failed to nudge: %s`, err)
+				}
+			})
+			return nil, nil
+		})
+	}
 
 	// If the closed timestamp is not empty, inform the Processor.
 	if closedTS.IsEmpty() {
@@ -435,4 +462,26 @@ func (r *Replica) handleClosedTimestampUpdateRaftMuLocked() {
 		// Consumption failed and the rangefeed was stopped.
 		r.resetRangefeedRaftMuLocked()
 	}
+}
+
+func (r *Replica) ensureClosedTimestampStarted(ctx context.Context) *roachpb.Error {
+	// Make sure there's a leaseholder. If there's no leaseholder, there's no
+	// closed timestamp updates.
+	var leaseholderNodeID roachpb.NodeID
+	_, err := r.redirectOnOrAcquireLease(ctx)
+	if err == nil {
+		// We have the lease. Request is essentially a wrapper for calling EmitMLAI
+		// on a remote node, so cut out the middleman.
+		r.EmitMLAI()
+		return nil
+	} else if lErr, ok := err.GetDetail().(*roachpb.NotLeaseHolderError); ok {
+		leaseholderNodeID = lErr.LeaseHolder.NodeID
+	} else {
+		return err
+	}
+	// Request fixes any issues where we've missed a closed timestamp update or
+	// where we're not connected to receive them from this node in the first
+	// place.
+	r.store.cfg.ClosedTimestamp.Clients.Request(leaseholderNodeID, r.RangeID)
+	return nil
 }

pkg/storage/closedts/closedts.go
@@ -94,6 +94,9 @@ type Storage interface {
 	// Add merges the given Entry into the state for the given NodeID. The first
 	// Entry passed in for any given Entry.Epoch must have Entry.Full set.
 	Add(roachpb.NodeID, ctpb.Entry)
+	// Clear removes all closed timestamp information from the Storage. It can
+	// be used to simulate the loss of information caused by a process restart.
+	Clear()
 }
 
 // A Notifyee is a sink for closed timestamp updates.

pkg/storage/closedts/container/noop.go
@@ -68,6 +68,7 @@ func (noopEverything) Track(
 func (noopEverything) VisitAscending(roachpb.NodeID, func(ctpb.Entry) (done bool))  {}
 func (noopEverything) VisitDescending(roachpb.NodeID, func(ctpb.Entry) (done bool)) {}
 func (noopEverything) Add(roachpb.NodeID, ctpb.Entry)                               {}
+func (noopEverything) Clear()                                                       {}
 func (noopEverything) Notify(roachpb.NodeID) chan<- ctpb.Entry {
 	return nil // will explode when used, but nobody would use this
 }

pkg/storage/closedts/provider/testutils/storage.go
@@ -71,6 +71,13 @@ func (s *TestStorage) Add(nodeID roachpb.NodeID, entry ctpb.Entry) {
 	})
 }
 
+// Clear implements closedts.Storage.
+func (s *TestStorage) Clear() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.m = nil
+}
+
 // Snapshot returns a copy of the data contain within the TestStorage.
 func (s *TestStorage) Snapshot() map[roachpb.NodeID][]ctpb.Entry {
 	s.mu.Lock()

pkg/storage/closedts/storage/storage.go
@@ -67,6 +67,8 @@ type SingleStorage interface {
 	// recent bucket and remaining buckets are rotated as indicated by their age
 	// relative to the newly added Entry.
 	Add(ctpb.Entry)
+	// Clear removes all Entries from this storage.
+	Clear()
 }
 
 type entry struct {
@@ -121,6 +123,14 @@ func (ms *MultiStorage) VisitDescending(nodeID roachpb.NodeID, f func(ctpb.Entry
 func (ms *MultiStorage) Add(nodeID roachpb.NodeID, entry ctpb.Entry) {
 	ss := ms.getOrCreate(nodeID)
 	ss.Add(entry)
+}
+
+// Clear implements closedts.Storage.
+func (ms *MultiStorage) Clear() {
+	ms.m.Range(func(_ int64, p unsafe.Pointer) bool {
+		(*entry)(p).SingleStorage.Clear()
+		return true // continue
+	})
 }
 
 // String prints a tabular rundown of the contents of the MultiStorage.

pkg/storage/closedts/storage/storage_mem.go
@@ -164,6 +164,14 @@ func (m *memStorage) VisitDescending(f func(ctpb.Entry) (done bool)) {
 	}
 }
 
+func (m *memStorage) Clear() {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	for i := 0; i < len(m.mu.buckets); i++ {
+		m.mu.buckets[i] = ctpb.Entry{}
+	}
+}
+
 func merge(e, ee ctpb.Entry) ctpb.Entry {
 	// TODO(tschottdorf): if either of these hit, check that what we're
 	// returning has Full set. If we make it past, check that either of

pkg/storage/closedts/storage/storage_test.go
@@ -123,6 +123,10 @@ func ExampleSingleStorage() {
 	fmt.Println("This would resolve itself if reasonably spaced updates kept coming in.")
 	fmt.Println(s)
 
+	fmt.Println("Finally, when the storage is cleared, all buckets are reset.")
+	s.Clear()
+	fmt.Println(s)
+
 	// Output:
 	// The empty storage renders as below:
 	// +--+---------------------+----------------------+----------------------+----------------------+
@@ -241,6 +245,14 @@ func ExampleSingleStorage() {
 	//   r8                   711                    711                       711
 	//   r9                  2020                   2020                      2020                    2020
 	// +----+---------------------+----------------------+-------------------------+-----------------------+
+	//
+	// Finally, when the storage is cleared, all buckets are reset.
+	// +--+---------------------+----------------------+----------------------+----------------------+
+	//         0.000000000,0         0.000000000,0          0.000000000,0          0.000000000,0
+	//      age=0s (target ≤0s)   age=0s (target ≤10s)   age=0s (target ≤20s)   age=0s (target ≤40s)
+	//            epoch=0               epoch=0                epoch=0                epoch=0
+	// +--+---------------------+----------------------+----------------------+----------------------+
+	// +--+---------------------+----------------------+----------------------+----------------------+
 }
 
 func ExampleMultiStorage_epoch() {

pkg/storage/helpers_test.go
@@ -173,6 +173,12 @@ func (s *Store) ReservationCount() int {
 	return len(s.snapshotApplySem)
 }
 
+// ClearClosedTimestampStorage clears the closed timestamp storage of all
+// knowledge about closed timestamps.
+func (s *Store) ClearClosedTimestampStorage() {
+	s.cfg.ClosedTimestamp.Storage.Clear()
+}
+
 // AssertInvariants verifies that the store's bookkeping is self-consistent. It
 // is only valid to call this method when there is no in-flight traffic to the
 // store (e.g., after the store is shut down).

commit 1ecaf72fea02fdae22fcb404c2dea50e84beed19

storage: fix panic in ensureClosedTimestampStarted
When I introduced this method in #36684, I copied the usage of the
LeaseHolder field from canServeFollowerRead but missed the part where it
first checks that the field is set. Noticed by seeing this panic with a
nil pointer dereference in a roachtest.

Release note (bug fix): fixes a panic that can happen while RangeFeeds
are active

pkg/storage/replica_rangefeed.go
@@ -490,6 +490,15 @@ func (r *Replica) ensureClosedTimestampStarted(ctx context.Context) *roachpb.Err
 		r.EmitMLAI()
 		return nil
 	} else if lErr, ok := err.GetDetail().(*roachpb.NotLeaseHolderError); ok {
+		if lErr.LeaseHolder == nil {
+			// It's possible for redirectOnOrAcquireLease to return
+			// NotLeaseHolderErrors with LeaseHolder unset, but these should be
+			// transient conditions. If this method is being called by RangeFeed to
+			// nudge a stuck closedts, then essentially all we can do here is nothing
+			// and assume that redirectOnOrAcquireLease will do something different
+			// the next time it's called.
+			return nil
+		}
 		leaseholderNodeID = lErr.LeaseHolder.NodeID
 	} else {
 		return err