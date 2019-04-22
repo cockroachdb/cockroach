cockroachdb/cockroach

commit fb8e85da7bdd94dacdd38e27a53822785dc5078d

pgerror: clean up API
The various public-facing functions in pgerror have a number of
inconsistencies:
 - some functions start with `New`, others don't
 - the `f` suffix isn't used consistently (like `Unimplemented`, which
   caused unnecessary `Sprintf`s in some callers)
 - some functions contain `Error`, others don't.

This commit cleans this up:
 - we remove `Error` because it is redundant with the package name
 - we remove `New` for all wrappers
 - we make all wrappers have `f` and non-`f` versions.

Release note: None

pkg/sql/pgwire/pgerror/internal_errors.go
@@ -63,16 +63,16 @@ If you would rather not post publicly, please contact us directly at:
 The Cockroach Labs team appreciates your feedback.
 `
 
-// NewAssertionErrorf creates an internal error.
-func NewAssertionErrorf(format string, args ...interface{}) *Error {
-	return NewAssertionErrorWithDepthf(1, format, args...)
+// AssertionFailedf creates an internal error.
+func AssertionFailedf(format string, args ...interface{}) *Error {
+	return AssertionFailedWithDepthf(1, format, args...)
 }
 
 const internalErrorPrefix = "internal error: "
 
-// NewAssertionErrorWithDepthf creates an internal error.
-func NewAssertionErrorWithDepthf(depth int, format string, args ...interface{}) *Error {
-	err := NewErrorWithDepthf(depth+1, CodeInternalError, internalErrorPrefix+format, args...)
+// AssertionFailedWithDepthf creates an internal error.
+func AssertionFailedWithDepthf(depth int, format string, args ...interface{}) *Error {
+	err := NewWithDepthf(depth+1, CodeInternalError, internalErrorPrefix+format, args...)
 	st := log.NewStackTrace(depth + 1)
 
 	// Internal details, reportable, must not contain PII.
@@ -97,7 +97,7 @@ func NewAssertionErrorWithWrappedErrf(err error, format string, args ...interfac
 		return pgErr
 	}
 	// The wrap was refused (it's a special error - eg a retry error). Force an assertion error.
-	return NewAssertionErrorWithDepthf(1, "%v", err)
+	return AssertionFailedWithDepthf(1, "%v", err)
 }
 
 // NewInternalTrackingError instantiates an error
@@ -107,7 +107,7 @@ func NewAssertionErrorWithWrappedErrf(err error, format string, args ...interfac
 // above.
 func NewInternalTrackingError(issue int, detail string) *Error {
 	key := fmt.Sprintf("#%d.%s", issue, detail)
-	err := NewAssertionErrorWithDepthf(1, key)
+	err := AssertionFailedWithDepthf(1, key)
 	err.TelemetryKey = key
 	return err.SetHintf("See: https://github.com/cockroachdb/cockroach/issues/%d", issue)
 }

pkg/testutils/lint/lint_test.go
@@ -1327,16 +1327,17 @@ func TestLint(t *testing.T) {
 			"NewAssertionErrorWithDepthf",
 			"NewAssertionErrorWithWrappedErrf",
 			"NewAssertionErrorf",
-			"NewDangerousStatementErrorf",
-			"NewError",
-			"NewErrorWithDepthf",
-			"NewErrorf",
+			"DangerousStatementf",
+			"pgerror.New",
+			"pgerror.NewWithDepthf",
+			"pgerror.Newf",
 			"SetDetailf",
 			"SetHintf",
 			"Unimplemented",
-			"UnimplementedWithDepth",
-			"UnimplementedWithIssueDetailErrorf",
-			"UnimplementedWithIssueErrorf",
+			"Unimplementedf",
+			"UnimplementedWithDepthf",
+			"UnimplementedWithIssueDetailf",
+			"UnimplementedWithIssuef",
 			"VEvent",
 			"VEventf",
 			"Warning",

commit d03ad969722567e2acbec3ed7b9e61830f453966

sql: s/NewAssertionError/AssertionFailedf/
Fallout from fb8e85da7bdd94dacdd38e27a53822785dc5078d.

Release note: None

pkg/sql/pgwire/pgerror/internal_errors.go
@@ -103,7 +103,7 @@ func NewAssertionErrorWithWrappedErrf(err error, format string, args ...interfac
 // NewInternalTrackingError instantiates an error
 // meant for use with telemetry.ReportError directly.
 //
-// Do not use this! Convert uses to NewAssertionErrorf or similar
+// Do not use this! Convert uses to AssertionFailedf or similar
 // above.
 func NewInternalTrackingError(issue int, detail string) *Error {
 	key := fmt.Sprintf("#%d.%s", issue, detail)

pkg/storage/replica_gc_queue.go
@@ -214,7 +214,7 @@ func (rgcq *replicaGCQueue) process(
 		//
 		// TODO(knz): we should really have a separate type for assertion
 		// errors that trigger telemetry, like
-		// pgerror.NewAssertionErrorf() does.
+		// pgerror.AssertionFailedf() does.
 		return errors.Errorf("expected 1 range descriptor, got %d", len(rs))
 	}
 	replyDesc := rs[0]

pkg/testutils/lint/lint_test.go
@@ -436,7 +436,7 @@ func TestLint(t *testing.T) {
 		}
 
 		if err := stream.ForEach(filter, func(s string) {
-			t.Errorf("\n%s <- forbidden; use panic(pgerror.NewAssertionErrorf()) instead", s)
+			t.Errorf("\n%s <- forbidden; use panic(pgerror.AssertionFailedf()) instead", s)
 		}); err != nil {
 			t.Error(err)
 		}
@@ -466,7 +466,7 @@ func TestLint(t *testing.T) {
 		}
 
 		if err := stream.ForEach(filter, func(s string) {
-			t.Errorf("\n%s <- forbidden; use pgerror.NewAssertionErrorf() instead", s)
+			t.Errorf("\n%s <- forbidden; use pgerror.AssertionFailedf() instead", s)
 		}); err != nil {
 			t.Error(err)
 		}
@@ -1324,9 +1324,9 @@ func TestLint(t *testing.T) {
 			"Info",
 			"Infof",
 			"InfofDepth",
-			"NewAssertionErrorWithDepthf",
+			"AssertionFailedf",
+			"AssertionFailedWithDepthf",
 			"NewAssertionErrorWithWrappedErrf",
-			"NewAssertionErrorf",
 			"DangerousStatementf",
 			"pgerror.New",
 			"pgerror.NewWithDepthf",