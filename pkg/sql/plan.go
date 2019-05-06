4749d340b4c7ca8e72ad49b71c80a104a2ed7f58
Jordan Lewis <jordanthelewis@gmail.com>
2019-05-06 01:13:57 -0400

    sql: fix crash on prepared show statements
    Some show statements were missing a prepare path.
    
    Release note (sql change): fix crashes when trying to run certain SHOW
    commands via the pgwire prepare path

--- pkg/sql/plan.go
+++ pkg/sql/plan.go
@@ -855,6 +855,10 @@ func (p *planner) doPrepare(ctx context.Context, stmt tree.Statement) (planNode,
 		return p.ShowJobs(ctx, n)
 	case *tree.ShowRoleGrants:
 		return p.ShowRoleGrants(ctx, n)
+	case *tree.ShowHistogram:
+		return p.ShowHistogram(ctx, n)
+	case *tree.ShowRanges:
+		return p.ShowRanges(ctx, n)
 	case *tree.ShowRoles:
 		return p.ShowRoles(ctx, n)
 	case *tree.ShowSessions:
@@ -865,14 +869,16 @@ func (p *planner) doPrepare(ctx context.Context, stmt tree.Statement) (planNode,
 		return p.ShowSchemas(ctx, n)
 	case *tree.ShowSequences:
 		return p.ShowSequences(ctx, n)
+	case *tree.ShowTableStats:
+		return p.ShowTableStats(ctx, n)
 	case *tree.ShowTraceForSession:
 		return p.ShowTrace(ctx, n)
 	case *tree.ShowUsers:
 		return p.ShowUsers(ctx, n)
 	case *tree.ShowTransactionStatus:
 		return p.ShowTransactionStatus(ctx)
-	case *tree.ShowRanges:
-		return p.ShowRanges(ctx, n)
+	case *tree.ShowZoneConfig:
+		return p.ShowZoneConfig(ctx, n)
 	case *tree.Split:
 		return p.Split(ctx, n)
 	case *tree.Truncate:
