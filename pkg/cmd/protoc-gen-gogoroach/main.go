f5cc083438624e838d5d96cf1b235d456acc18b1
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2019-05-06 17:16:53 -0400

    protoc: disable XXX_NoUnkeyedLiteral generation in protos
    Because the field is now at the end of generated protos, it is now
    taking up space even though it is an empty struct:
    https://github.com/golang/go/issues/9401
    
    Since this is just a glorified lint which is already enforced by
    `go vet`'s “composites” check, it doesn't seem worth the cost.
    
    Release note: None

--- pkg/cmd/protoc-gen-gogoroach/main.go
+++ pkg/cmd/protoc-gen-gogoroach/main.go
@@ -76,9 +76,13 @@ func main() {
 		// Something something extensions; we don't use 'em currently.
 		// vanity.TurnOffGoExtensionsMapAll,
 
-		// Disable XXX_unrecognized and XXX_sizecache generation. These fields
-		// aren't worth their associated runtime cost.
+		// Disable generation of the following fields, which aren't worth
+		// their associated runtime cost:
+		// - XXX_unrecognized
+		// - XXX_NoUnkeyedLiteral
+		// - XXX_sizecache
 		vanity.TurnOffGoUnrecognizedAll,
+		vanity.TurnOffGoUnkeyedAll,
 		vanity.TurnOffGoSizecacheAll,
 
 		// Adds unnecessary dependency on golang/protobuf.
