cockroachdb/cockroach

commit b963c0d4960938dd29fbdbf896ccbdf1792795c6

workload: allow columnar data generation (alloc -73%, MB/s +60%)
A workload.Generator previously could specifiy its initial table data
using one of two methods. One outputs a single row as `[]interface{}`
and the other is a batch of `[][]interface{}`. This commit switches the
latter to be a columar batch, allowing selected performance-critical
workload generators (tpcc, bank) to generate initial table data with
dramatically reduced allocations.

The single row option still exists and will continue to be used by most
workload.Generators, as it's much simpler. An adaptor from the columnar
batch to the old row oriented `[][]interface{}` batch is included for
ease of use in non-performance sensitive consumers of initial table
data. IMPORT, on the other hand, is switched to using the columnar
batches directly.

Some of the savings here is reusing batches, but the majority comes from
`[]interface{}` requiring that everything assinged to it be on the heap,
while something like `[]int` doesn't. We also get some amount of speedup
in initial table data consumers from fewer type switches.

This could also be accomplished without being columnar, but doing the
minimum non-columnar thing to get the same results will require
reinventing most of the work that the columnar does. Plus it will almost
certainly be convenient for columnar exec benchmarking to have a common
format.

Benchmark results:

    name                               old time/op    new time/op    delta
    InitialData/tpcc/warehouses=1-8       552ms ± 1%     454ms ± 0%  -17.81%  (p=0.008 n=5+5)
    InitialData/bank/rows=1000-8          461µs ± 0%     332µs ± 1%  -28.10%  (p=0.008 n=5+5)
    WriteCSVRows-8                       15.0µs ± 1%    16.8µs ± 1%  +12.44%  (p=0.008 n=5+5)
    CSVRowsReader-8                      17.8µs ± 1%    19.3µs ± 1%   +8.41%  (p=0.008 n=5+5)

    name                               old speed      new speed      delta

pkg/workload/csv.go
@@ -22,7 +22,11 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"unsafe"
 
+	"github.com/cockroachdb/cockroach/pkg/sql/exec/coldata"
+	"github.com/cockroachdb/cockroach/pkg/sql/exec/types"
+	"github.com/cockroachdb/cockroach/pkg/util/bufalloc"
 	"github.com/cockroachdb/cockroach/pkg/util/encoding/csv"
 	"github.com/pkg/errors"
 	"github.com/spf13/pflag"
@@ -39,6 +43,9 @@ const (
 func WriteCSVRows(
 	ctx context.Context, w io.Writer, table Table, rowStart, rowEnd int, sizeBytesLimit int64,
 ) (rowBatchIdx int, err error) {
+	cb := coldata.NewMemBatchWithSize(nil, 0)
+	var a bufalloc.ByteAllocator
+
 	bytesWrittenW := &bytesWrittenWriter{w: w}
 	csvW := csv.NewWriter(bytesWrittenW)
 	var rowStrings []string
@@ -52,14 +59,16 @@ func WriteCSVRows(
 			return 0, ctx.Err()
 		default:
 		}
-		for _, row := range table.InitialRows.Batch(rowBatchIdx) {
-			if cap(rowStrings) < len(row) {
-				rowStrings = make([]string, len(row))
-			} else {
-				rowStrings = rowStrings[:len(row)]
-			}
-			for i, datum := range row {
-				rowStrings[i] = datumToCSVString(datum)
+		a = a[:0]
+		table.InitialRows.FillBatch(rowBatchIdx, cb, &a)
+		if numCols := cb.Width(); cap(rowStrings) < numCols {
+			rowStrings = make([]string, numCols)
+		} else {
+			rowStrings = rowStrings[:numCols]
+		}
+		for rowIdx, numRows := 0, int(cb.Length()); rowIdx < numRows; rowIdx++ {
+			for colIdx, col := range cb.ColVecs() {
+				rowStrings[colIdx] = colDatumToCSVString(col, rowIdx)
 			}
 			if err := csvW.Write(rowStrings); err != nil {
 				return 0, err
@@ -74,13 +83,21 @@ type csvRowsReader struct {
 	t                    Table
 	batchStart, batchEnd int
 
-	buf        bytes.Buffer
-	csvW       *csv.Writer
-	batchIdx   int
+	buf  bytes.Buffer
+	csvW *csv.Writer
+
+	batchIdx int
+	cb       coldata.Batch
+	a        bufalloc.ByteAllocator
+
 	stringsBuf []string
 }
 
 func (r *csvRowsReader) Read(p []byte) (n int, err error) {
+	if r.cb == nil {
+		r.cb = coldata.NewMemBatchWithSize(nil, 0)
+	}
+
 	for {
 		if r.buf.Len() > 0 {
 			return r.buf.Read(p)
@@ -89,16 +106,17 @@ func (r *csvRowsReader) Read(p []byte) (n int, err error) {
 		if r.batchIdx == r.batchEnd {
 			return 0, io.EOF
 		}
-		batch := r.t.InitialRows.Batch(r.batchIdx)
+		r.a = r.a[:0]
+		r.t.InitialRows.FillBatch(r.batchIdx, r.cb, &r.a)
 		r.batchIdx++
-		for _, row := range batch {
-			if cap(r.stringsBuf) < len(row) {
-				r.stringsBuf = make([]string, len(row))
-			} else {
-				r.stringsBuf = r.stringsBuf[:len(row)]
-			}
-			for i, datum := range row {
-				r.stringsBuf[i] = datumToCSVString(datum)
+		if numCols := r.cb.Width(); cap(r.stringsBuf) < numCols {
+			r.stringsBuf = make([]string, numCols)
+		} else {
+			r.stringsBuf = r.stringsBuf[:numCols]
+		}
+		for rowIdx, numRows := 0, int(r.cb.Length()); rowIdx < numRows; rowIdx++ {
+			for colIdx, col := range r.cb.ColVecs() {
+				r.stringsBuf[colIdx] = colDatumToCSVString(col, rowIdx)
 			}
 			if err := r.csvW.Write(r.stringsBuf); err != nil {
 				return 0, err
@@ -120,22 +138,22 @@ func NewCSVRowsReader(t Table, batchStart, batchEnd int) io.Reader {
 	return r
 }
 
-func datumToCSVString(datum interface{}) string {
-	if datum == nil {
+func colDatumToCSVString(col coldata.Vec, rowIdx int) string {
+	if col.Nulls().NullAt64(uint64(rowIdx)) {
 		return `NULL`
 	}
-	switch t := datum.(type) {
-	case int:
-		return strconv.Itoa(t)
-	case int64:
-		return fmt.Sprint(t)
-	case float64:
-		return strconv.FormatFloat(t, 'f', -1, 64)
-	case string:
-		return t
-	default:
-		panic(fmt.Sprintf("unsupported type %T: %v", datum, datum))
+	switch col.Type() {
+	case types.Bool:
+		return strconv.FormatBool(col.Bool()[rowIdx])
+	case types.Int64:
+		return strconv.FormatInt(col.Int64()[rowIdx], 10)
+	case types.Float64:
+		return strconv.FormatFloat(col.Float64()[rowIdx], 'f', -1, 64)
+	case types.Bytes:
+		bytes := col.Bytes()[rowIdx]
+		return *(*string)(unsafe.Pointer(&bytes))
 	}
+	panic(fmt.Sprintf(`unhandled type %s`, col.Type().GoTypeName()))
 }
 
 // HandleCSV configures a Generator with url params and outputs the data for a
@@ -174,7 +192,7 @@ func HandleCSV(w http.ResponseWriter, req *http.Request, prefix string, meta Met
 	if table == nil {
 		return errors.Errorf(`could not find table %s in generator %s`, tableName, meta.Name)
 	}
-	if table.InitialRows.Batch == nil {
+	if table.InitialRows.FillBatch == nil {
 		return errors.Errorf(`csv-server is not supported for workload %s`, meta.Name)
 	}
 

pkg/workload/workload.go
@@ -27,10 +27,15 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
 	"sync/atomic"
 	"time"
 
+	"github.com/cockroachdb/cockroach/pkg/sql/exec/coldata"
+	"github.com/cockroachdb/cockroach/pkg/sql/exec/types"
 	"github.com/cockroachdb/cockroach/pkg/sql/lex"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/util/bufalloc"
 	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/workload/histogram"
@@ -163,23 +168,138 @@ type BatchedTuples struct {
 	// NumTotal is the total number of tuples in all batches. Not all generators
 	// will know this, it's set to 0 when unknown.
 	NumTotal int
-	// Batch is a function to deterministically compute a batch of tuples given
-	// its index.
-	Batch func(int) [][]interface{}
+	// FillBatch is a function to deterministically compute a columnar-batch of
+	// tuples given its index.
+	//
+	// To save allocations, the Vecs in the passed Batch are reused when possible,
+	// so the results of this call are invalidated the next time the same Batch is
+	// passed to FillBatch. Ditto the ByteAllocator, which can be reset in between
+	// calls. If a caller needs the Batch and its contents to be long lived,
+	// simply pass a new Batch to each call and don't reset the ByteAllocator.
+	FillBatch func(int, coldata.Batch, *bufalloc.ByteAllocator)
 }
 
-// Tuples returns a BatchedTuples where each batch has size 1.
+// Tuples is like TypedTuples except that it tries to guess the type of each
+// datum. However, if the function ever returns nil for one of the datums, you
+// need to use TypedTuples instead and specify the types.
 func Tuples(count int, fn func(int) []interface{}) BatchedTuples {
+	return TypedTuples(count, nil /* colTypes */, fn)
+}
+
+// TypedTuples returns a BatchedTuples where each batch has size 1. It's
+// intended to be easier to use than directly specifying a BatchedTuples, but
+// the tradeoff is some bit of performance. If colTypes is nil, an attempt is
+// made to infer them.
+func TypedTuples(count int, colTypes []types.T, fn func(int) []interface{}) BatchedTuples {
+	// The FillBatch we create has to be concurrency safe, so we can't let it do
+	// the one-time initialization of colTypes without this protection.
+	var colTypesOnce sync.Once
+
 	t := BatchedTuples{
 		NumBatches: count,
 		NumTotal:   count,
 	}
 	if fn != nil {
-		t.Batch = func(batchIdx int) [][]interface{} {
-			return [][]interface{}{fn(batchIdx)}
+		t.FillBatch = func(batchIdx int, cb coldata.Batch, _ *bufalloc.ByteAllocator) {
+			row := fn(batchIdx)
+
+			colTypesOnce.Do(func() {
+				if colTypes == nil {
+					colTypes = make([]types.T, len(row))
+					for i, datum := range row {
+						if datum == nil {
+							panic(fmt.Sprintf(
+								`can't determine type of nil column; call TypedTuples directly: %v`, row))
+						} else {
+							switch datum.(type) {
+							case time.Time:
+								colTypes[i] = types.Bytes
+							default:
+								colTypes[i] = types.FromGoType(datum)
+							}
+						}
+					}
+				}
+			})
+
+			cb.Reset(colTypes, 1)
+			for colIdx, col := range cb.ColVecs() {
+				switch d := row[colIdx].(type) {
+				case nil:
+					col.Nulls().SetNull(0)
+				case bool:
+					col.Bool()[0] = d
+				case int:
+					col.Int64()[0] = int64(d)
+				case float64:
+					col.Float64()[0] = d
+				case string:
+					col.Bytes()[0] = []byte(d)
+				case []byte:
+					col.Bytes()[0] = d
+				case time.Time:
+					col.Bytes()[0] = []byte(d.Round(time.Microsecond).UTC().Format(tree.TimestampOutputFormat))
+				default:
+					panic(fmt.Sprintf(`unhandled datum type %T`, d))
+				}
+			}
 		}
 	}
 	return t
+}
+
+// BatchRows is a function to deterministically compute a row-batch of tuples
+// given its index. BatchRows doesn't attempt any reuse and so is allocation
+// heavy. In performance-critical code, FillBatch should be used directly,
+// instead.
+func (b BatchedTuples) BatchRows(batchIdx int) [][]interface{} {
+	cb := coldata.NewMemBatchWithSize(nil, 0)
+	var a bufalloc.ByteAllocator
+	b.FillBatch(batchIdx, cb, &a)
+	return ColBatchToRows(cb)
+}
+
+// ColBatchToRows materializes the columnar data in a coldata.Batch into rows.
+func ColBatchToRows(cb coldata.Batch) [][]interface{} {
+	numRows, numCols := int(cb.Length()), cb.Width()
+	// Allocate all the []interface{} row slices in one go.
+	datums := make([]interface{}, numRows*numCols)
+	for colIdx, col := range cb.ColVecs() {
+		nulls := col.Nulls()
+		switch col.Type() {
+		case types.Bool:
+			for rowIdx, datum := range col.Bool() {
+				if !nulls.NullAt64(uint64(rowIdx)) {
+					datums[rowIdx*numCols+colIdx] = datum
+				}
+			}
+		case types.Int64:
+			for rowIdx, datum := range col.Int64() {
+				if !nulls.NullAt64(uint64(rowIdx)) {
+					datums[rowIdx*numCols+colIdx] = datum
+				}
+			}
+		case types.Float64:
+			for rowIdx, datum := range col.Float64() {
+				if !nulls.NullAt64(uint64(rowIdx)) {
+					datums[rowIdx*numCols+colIdx] = datum
+				}
+			}
+		case types.Bytes:
+			for rowIdx, datum := range col.Bytes() {
+				if !nulls.NullAt64(uint64(rowIdx)) {
+					datums[rowIdx*numCols+colIdx] = datum
+				}
+			}
+		default:
+			panic(fmt.Sprintf(`unhandled type %s`, col.Type().GoTypeName()))
+		}
+	}
+	rows := make([][]interface{}, numRows)
+	for rowIdx := 0; rowIdx < numRows; rowIdx++ {
+		rows[rowIdx] = datums[rowIdx*numCols : (rowIdx+1)*numCols]
+	}
+	return rows
 }
 
 // QueryLoad represents some SQL query workload performable on a database
@@ -277,6 +397,8 @@ func ApproxDatumSize(x interface{}) int64 {
 		// used to batch things by size and table of all `0`s should not get
 		// infinite size batches.
 		return int64(bits.Len(uint(t))+8) / 8
+	case int64:
+		return int64(bits.Len64(uint64(t))+8) / 8
 	case uint64:
 		return int64(bits.Len64(t)+8) / 8
 	case float64:
@@ -333,7 +455,7 @@ func Setup(
 	for _, table := range tables {
 		if table.InitialRows.NumBatches == 0 {
 			continue
-		} else if table.InitialRows.Batch == nil {
+		} else if table.InitialRows.FillBatch == nil {
 			return 0, errors.Errorf(
 				`initial data is not supported for workload %s`, gen.Meta().Name)
 		}
@@ -365,8 +487,8 @@ func Setup(
 				}
 				_ = flush()
 
-				for rowBatchIdx := startIdx; rowBatchIdx < endIdx; rowBatchIdx++ {
-					for _, row := range table.InitialRows.Batch(rowBatchIdx) {
+				for batchIdx := startIdx; batchIdx < endIdx; batchIdx++ {
+					for _, row := range table.InitialRows.BatchRows(batchIdx) {
 						if len(params) != 0 {
 							insertStmtBuf.WriteString(`,`)
 						}
@@ -427,7 +549,7 @@ func Split(ctx context.Context, db *gosql.DB, table Table, concurrency int) erro
 	}
 	splitPoints := make([][]interface{}, 0, table.Splits.NumBatches)
 	for splitIdx := 0; splitIdx < table.Splits.NumBatches; splitIdx++ {
-		splitPoints = append(splitPoints, table.Splits.Batch(splitIdx)...)
+		splitPoints = append(splitPoints, table.Splits.BatchRows(splitIdx)...)
 	}
 	sort.Sort(sliceSliceInterface(splitPoints))
 
@@ -525,6 +647,8 @@ func StringTuple(datums []interface{}) []string {
 		switch x := datum.(type) {
 		case int:
 			s[i] = strconv.Itoa(x)
+		case int64:
+			s[i] = strconv.FormatInt(x, 10)
 		case uint64:
 			s[i] = strconv.FormatUint(x, 10)
 		case string:
@@ -554,6 +678,13 @@ func (s sliceSliceInterface) Less(i, j int) bool {
 		switch x := s[i][offset].(type) {
 		case int:
 			if y := s[j][offset].(int); x < y {
+				return true
+			} else if x > y {
+				return false
+			}
+			continue
+		case int64:
+			if y := s[j][offset].(int64); x < y {
 				return true
 			} else if x > y {
 				return false

pkg/ccl/backupccl/bench_test.go
@@ -29,7 +29,7 @@ func bankBuf(numAccounts int) *bytes.Buffer {
 	var buf bytes.Buffer
 	fmt.Fprintf(&buf, "CREATE TABLE %s %s;\n", bankData.Name, bankData.Schema)
 	for rowIdx := 0; rowIdx < bankData.InitialRows.NumBatches; rowIdx++ {
-		for _, row := range bankData.InitialRows.Batch(rowIdx) {
+		for _, row := range bankData.InitialRows.BatchRows(rowIdx) {
 			rowBatch := strings.Join(workload.StringTuple(row), `,`)
 			fmt.Fprintf(&buf, "INSERT INTO %s VALUES (%s);\n", bankData.Name, rowBatch)
 		}

pkg/ccl/changefeedccl/bench_test.go
@@ -278,7 +278,7 @@ func loadWorkloadBatches(sqlDB *gosql.DB, table workload.Table) ([]time.Time, in
 		params = params[:0]
 		insertStmtBuf.Reset()
 		insertStmtBuf.WriteString(`INSERT INTO "` + table.Name + `" VALUES `)
-		for _, row := range table.InitialRows.Batch(batchIdx) {
+		for _, row := range table.InitialRows.BatchRows(batchIdx) {
 			if len(params) != 0 {
 				insertStmtBuf.WriteString(`,`)
 			}

pkg/ccl/importccl/load_test.go
@@ -30,7 +30,7 @@ func bankBuf(numAccounts int) *bytes.Buffer {
 	var buf bytes.Buffer
 	fmt.Fprintf(&buf, "CREATE TABLE %s %s;\n", bankData.Name, bankData.Schema)
 	for rowIdx := 0; rowIdx < bankData.InitialRows.NumBatches; rowIdx++ {
-		for _, row := range bankData.InitialRows.Batch(rowIdx) {
+		for _, row := range bankData.InitialRows.BatchRows(rowIdx) {
 			rowTuple := strings.Join(workload.StringTuple(row), `,`)
 			fmt.Fprintf(&buf, "INSERT INTO %s VALUES (%s);\n", bankData.Name, rowTuple)
 		}
@@ -79,8 +79,8 @@ func TestImportOutOfOrder(t *testing.T) {
 		t.Fatal(err)
 	}
 	bankData := bank.FromRows(2).Tables()[0]
-	row1 := workload.StringTuple(bankData.InitialRows.Batch(0)[0])
-	row2 := workload.StringTuple(bankData.InitialRows.Batch(1)[0])
+	row1 := workload.StringTuple(bankData.InitialRows.BatchRows(0)[0])
+	row2 := workload.StringTuple(bankData.InitialRows.BatchRows(1)[0])
 
 	var buf bytes.Buffer
 	fmt.Fprintf(&buf, "CREATE TABLE %s %s;\n", bankData.Name, bankData.Schema)

pkg/ccl/importccl/read_import_workload.go
@@ -10,20 +10,23 @@ package importccl
 
 import (
 	"context"
-	"fmt"
 	"net/url"
 	"runtime"
 	"strings"
 	"sync/atomic"
-	"time"
+	"unsafe"
 
+	"github.com/cockroachdb/apd"
 	"github.com/cockroachdb/cockroach/pkg/ccl/storageccl"
 	"github.com/cockroachdb/cockroach/pkg/jobs"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/sql/exec/coldata"
+	"github.com/cockroachdb/cockroach/pkg/sql/exec/types"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
-	"github.com/cockroachdb/cockroach/pkg/sql/types"
+	sqltypes "github.com/cockroachdb/cockroach/pkg/sql/types"
+	"github.com/cockroachdb/cockroach/pkg/util/bufalloc"
 	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/workload"
@@ -51,34 +54,56 @@ func (w *workloadReader) inputFinished(ctx context.Context) {
 	close(w.kvCh)
 }
 
-// makeDatumFromRaw tries to fast-path a few workload-generated types into
+// makeDatumFromColOffset tries to fast-path a few workload-generated types into
 // directly datums, to dodge making a string and then the parsing it.
-func makeDatumFromRaw(
-	alloc *sqlbase.DatumAlloc, datum interface{}, hint *types.T, evalCtx *tree.EvalContext,
+func makeDatumFromColOffset(
+	alloc *sqlbase.DatumAlloc,
+	hint *sqltypes.T,
+	evalCtx *tree.EvalContext,
+	col coldata.Vec,
+	rowIdx int,
 ) (tree.Datum, error) {
-	if datum == nil {
+	if col.Nulls().NullAt64(uint64(rowIdx)) {
 		return tree.DNull, nil
 	}
-	switch t := datum.(type) {
-	case int:
-		return alloc.NewDInt(tree.DInt(t)), nil
-	case int64:
-		return alloc.NewDInt(tree.DInt(t)), nil
-	case []byte:
-		return alloc.NewDBytes(tree.DBytes(t)), nil
-	case time.Time:
+	switch col.Type() {
+	case types.Bool:
+		return tree.MakeDBool(tree.DBool(col.Bool()[rowIdx])), nil
+	case types.Int64:
 		switch hint.Family() {
-		case types.TimestampTZFamily:
-			return tree.MakeDTimestampTZ(t, time.Microsecond), nil
-		case types.TimestampFamily:
-			return tree.MakeDTimestamp(t, time.Microsecond), nil
+		case sqltypes.IntFamily:
+			return alloc.NewDInt(tree.DInt(col.Int64()[rowIdx])), nil
+		case sqltypes.DecimalFamily:
+			d := *apd.New(col.Int64()[rowIdx], 0)
+			return alloc.NewDDecimal(tree.DDecimal{Decimal: d}), nil
 		}
-	case tree.DString:
-		return alloc.NewDString(t), nil
-	case string:
-		return tree.ParseDatumStringAs(hint, t, evalCtx)
+	case types.Float64:
+		switch hint.Family() {
+		case sqltypes.FloatFamily:
+			return alloc.NewDFloat(tree.DFloat(col.Float64()[rowIdx])), nil
+		case sqltypes.DecimalFamily:
+			var d apd.Decimal
+			if _, err := d.SetFloat64(col.Float64()[rowIdx]); err != nil {
+				return nil, err
+			}
+			return alloc.NewDDecimal(tree.DDecimal{Decimal: d}), nil
+		}
+	case types.Bytes:
+		switch hint.Family() {
+		case sqltypes.BytesFamily:
+			return alloc.NewDBytes(tree.DBytes(col.Bytes()[rowIdx])), nil
+		case sqltypes.StringFamily:
+			data := col.Bytes()[rowIdx]
+			str := *(*string)(unsafe.Pointer(&data))
+			return alloc.NewDString(tree.DString(str)), nil
+		default:
+			data := col.Bytes()[rowIdx]
+			str := *(*string)(unsafe.Pointer(&data))
+			return tree.ParseDatumStringAs(hint, str, evalCtx)
+		}
 	}
-	return tree.ParseDatumStringAs(hint, fmt.Sprint(datum), evalCtx)
+	return nil, errors.Errorf(
+		`don't know how to interpret %s column as %s`, col.Type().GoTypeName(), hint)
 }
 
 func (w *workloadReader) readFiles(
@@ -150,8 +175,7 @@ func (w *workloadReader) readFiles(
 			w.table, t.InitialRows, int(conf.BatchBegin), int(conf.BatchEnd), w.kvCh)
 		if err := ctxgroup.GroupWorkers(ctx, runtime.NumCPU(), func(ctx context.Context) error {
 			evalCtx := w.newEvalCtx()
-			var alloc sqlbase.DatumAlloc
-			return wc.Worker(ctx, evalCtx, &alloc, finishedBatchFn)
+			return wc.Worker(ctx, evalCtx, finishedBatchFn)
 		}); err != nil {
 			return err
 		}
@@ -199,21 +223,30 @@ func NewWorkloadKVConverter(
 //
 // This worker needs its own EvalContext and DatumAlloc.
 func (w *WorkloadKVConverter) Worker(
-	ctx context.Context, evalCtx *tree.EvalContext, alloc *sqlbase.DatumAlloc, finishedBatchFn func(),
+	ctx context.Context, evalCtx *tree.EvalContext, finishedBatchFn func(),
 ) error {
 	conv, err := newRowConverter(w.tableDesc, evalCtx, w.kvCh)
 	if err != nil {
 		return err
 	}
 
+	var alloc sqlbase.DatumAlloc
+	var a bufalloc.ByteAllocator
+	cb := coldata.NewMemBatchWithSize(nil, 0)
+
 	for {
 		batchIdx := int(atomic.AddInt64(&w.batchIdxAtomic, 1))
 		if batchIdx >= w.batchEnd {
 			break
 		}
-		for rowIdx, rows := range w.rows.Batch(batchIdx) {
-			for colIdx, datum := range rows {
-				converted, err := makeDatumFromRaw(alloc, datum, conv.visibleColTypes[colIdx], evalCtx)
+		a = a[:0]
+		w.rows.FillBatch(batchIdx, cb, &a)
+		for rowIdx, numRows := 0, int(cb.Length()); rowIdx < numRows; rowIdx++ {
+			for colIdx, col := range cb.ColVecs() {
+				// TODO(dan): This does a type switch once per-datum. Reduce this to
+				// a one-time switch per column.
+				converted, err := makeDatumFromColOffset(
+					&alloc, conv.visibleColTypes[colIdx], evalCtx, col, rowIdx)
 				if err != nil {
 					return err
 				}

pkg/ccl/utilccl/sampledataccl/bankdata.go
@@ -53,7 +53,7 @@ func toBackup(t testing.TB, data workload.Table, dir string, chunkBytes int64) (
 	var stmts bytes.Buffer
 	fmt.Fprintf(&stmts, "CREATE TABLE %s %s;\n", data.Name, data.Schema)
 	for rowIdx := 0; rowIdx < data.InitialRows.NumBatches; rowIdx++ {
-		for _, row := range data.InitialRows.Batch(rowIdx) {
+		for _, row := range data.InitialRows.BatchRows(rowIdx) {
 			rowBatch := strings.Join(workload.StringTuple(row), `,`)
 			fmt.Fprintf(&stmts, "INSERT INTO %s VALUES (%s);\n", data.Name, rowBatch)
 		}

commit 23915b7755b460c3deaa729a57f327e16cdfef30

workload: fix --splits regression introduced in #35349
workload's Table schemas are SQL schemas, but #35349 switched the
initial data to be returned as a coldata.Batch, which has a more limited
set of types. (Or, in the case of simple workloads that return a
[]interface{}, it's roundtripped through coldata.Batch by the `Tuples`
helper.) Notably, this means a SQL STRING column is represented the same
as a BYTES column (ditto UUID, etc).

This caused a regression in splits, which received some []byte data for
a column tried to hand it to SPLIT as a SQL BYTES datum. This didn't
work for the UUID column in tpcc's history table nor the VARCHAR in
ycsb's usertable. Happily, a STRING works for both of these. It also
seems to work for BYTES columns, so it seems like the ambiguity is fine
in this case. When/if someone wants to add a workload that splits a
BYTES primary key column containing non-utf8 data, we'll may need to
revisit.

A more principled fix would be to get the fidelity back by parsing the
SQL schema, which in fact we do in `importccl.makeDatumFromColOffset`.
However, at the moment, this hack works and avoids the complexity and
the undesirable pkg/sql/parser dep.

Closes #37383
Closes #37382
Closes #37381
Closes #37380
Closes #37379
Closes #37378
Closes #37377
Closes #37393

Release note: None

pkg/workload/csv.go
@@ -150,6 +150,7 @@ func colDatumToCSVString(col coldata.Vec, rowIdx int) string {
 	case types.Float64:
 		return strconv.FormatFloat(col.Float64()[rowIdx], 'f', -1, 64)
 	case types.Bytes:
+		// See the HACK comment in ColBatchToRows.
 		bytes := col.Bytes()[rowIdx]
 		return *(*string)(unsafe.Pointer(&bytes))
 	}

pkg/workload/workload.go
@@ -286,6 +286,19 @@ func ColBatchToRows(cb coldata.Batch) [][]interface{} {
 				}
 			}
 		case types.Bytes:
+			// HACK: workload's Table schemas are SQL schemas, but the initial data is
+			// returned as a coldata.Batch, which has a more limited set of types.
+			// (Or, in the case of simple workloads that return a []interface{}, it's
+			// roundtripped through coldata.Batch by the `Tuples` helper.)
+			//
+			// Notably, this means a SQL STRING column is represented the same as a
+			// BYTES column (ditto UUID, etc). We could get the fidelity back by
+			// parsing the SQL schema, which in fact we do in
+			// `importccl.makeDatumFromColOffset`. At the moment, the set of types
+			// used in workloads is limited enough that the users of initial
+			// data/splits are okay with the fidelity loss. So, to avoid the
+			// complexity and the undesirable pkg/sql/parser dep, we simply treat them
+			// all as bytes and let the caller deal with the ambiguity.
 			for rowIdx, datum := range col.Bytes() {
 				if !nulls.NullAt64(uint64(rowIdx)) {
 					datums[rowIdx*numCols+colIdx] = datum
@@ -581,6 +594,9 @@ func Split(ctx context.Context, db *gosql.DB, table Table, concurrency int) erro
 
 					buf.Reset()
 					fmt.Fprintf(&buf, `ALTER TABLE %s SPLIT AT VALUES (%s)`, table.Name, split)
+					// If you're investigating an error coming out of this Exec, see the
+					// HACK comment in ColBatchToRows for some context that may (or may
+					// not) help you.
 					if _, err := db.Exec(buf.String()); err != nil {
 						return errors.Wrap(err, buf.String())
 					}
@@ -656,7 +672,8 @@ func StringTuple(datums []interface{}) []string {
 		case float64:
 			s[i] = fmt.Sprintf(`%f`, x)
 		case []byte:
-			s[i] = fmt.Sprintf(`X'%x'`, x)
+			// See the HACK comment in ColBatchToRows.
+			s[i] = lex.EscapeSQLString(string(x))
 		default:
 			panic(fmt.Sprintf("unsupported type %T: %v", x, x))
 		}
@@ -690,6 +707,13 @@ func (s sliceSliceInterface) Less(i, j int) bool {
 				return false
 			}
 			continue
+		case float64:
+			if y := s[j][offset].(float64); x < y {
+				return true
+			} else if x > y {
+				return false
+			}
+			continue
 		case uint64:
 			if y := s[j][offset].(uint64); x < y {
 				return true
@@ -699,6 +723,8 @@ func (s sliceSliceInterface) Less(i, j int) bool {
 			continue
 		case string:
 			cmp = strings.Compare(x, s[j][offset].(string))
+		case []byte:
+			cmp = bytes.Compare(x, s[j][offset].([]byte))
 		default:
 			panic(fmt.Sprintf("unsupported type %T: %v", x, x))
 		}

pkg/workload/workload_test.go
@@ -17,6 +17,7 @@ package workload_test
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
@@ -24,6 +25,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 	"github.com/cockroachdb/cockroach/pkg/workload"
 	"github.com/cockroachdb/cockroach/pkg/workload/bank"
 )
@@ -86,33 +88,74 @@ func TestSetup(t *testing.T) {
 func TestSplits(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 
-	const rows, payloadBytes, concurrency = 10, 0, 10
-	tests := []int{1, 2, 3, 4, 10}
-
 	ctx := context.Background()
 	s, db, _ := serverutils.StartServer(t, base.TestServerArgs{UseDatabase: `test`})
 	defer s.Stopper().Stop(ctx)
 	sqlutils.MakeSQLRunner(db).Exec(t, `CREATE DATABASE test`)
 
-	for _, ranges := range tests {
+	for _, ranges := range []int{1, 2, 3, 4, 10} {
+
+		tables := []workload.Table{
+			{
+				Name:   `ints`,
+				Schema: `(a INT PRIMARY KEY)`,
+				Splits: workload.Tuples(ranges-1, func(i int) []interface{} {
+					return []interface{}{i}
+				}),
+			},
+			{
+				Name:   `floats`,
+				Schema: `(a FLOAT PRIMARY KEY)`,
+				Splits: workload.Tuples(ranges-1, func(i int) []interface{} {
+					return []interface{}{float64(i)}
+				}),
+			},
+			{
+				Name:   `strings`,
+				Schema: `(a STRING PRIMARY KEY)`,
+				Splits: workload.Tuples(ranges-1, func(i int) []interface{} {
+					return []interface{}{strconv.Itoa(i)}
+				}),
+			},
+			{
+				Name:   `bytes`,
+				Schema: `(a BYTES PRIMARY KEY)`,
+				Splits: workload.Tuples(ranges-1, func(i int) []interface{} {
+					return []interface{}{strconv.Itoa(i)}
+				}),
+			},
+			{
+				Name:   `uuids`,
+				Schema: `(a UUID PRIMARY KEY)`,
+				Splits: workload.Tuples(ranges-1, func(i int) []interface{} {
+					u, err := uuid.NewV4()
+					if err != nil {
+						panic(err)
+					}
+					return []interface{}{u.String()}
+				}),
+			},
+		}
+
 		t.Run(fmt.Sprintf("ranges=%d", ranges), func(t *testing.T) {
 			sqlDB := sqlutils.MakeSQLRunner(db)
-			sqlDB.Exec(t, `DROP TABLE IF EXISTS bank`)
+			for _, table := range tables {
+				sqlDB.Exec(t, fmt.Sprintf(`DROP TABLE IF EXISTS %s`, table.Name))
+				sqlDB.Exec(t, fmt.Sprintf(`CREATE TABLE %s %s`, table.Name, table.Schema))
 
-			gen := bank.FromConfig(rows, payloadBytes, ranges)
-			table := gen.Tables()[0]
-			sqlDB.Exec(t, fmt.Sprintf(`CREATE TABLE %s %s`, table.Name, table.Schema))
+				const concurrency = 10
+				if err := workload.Split(ctx, db, table, concurrency); err != nil {
+					t.Fatalf("%+v", err)
+				}
 
-			if err := workload.Split(ctx, db, table, concurrency); err != nil {
-				t.Fatalf("%+v", err)
-			}
-
-			var actual int
-			sqlDB.QueryRow(
-				t, `SELECT count(*) FROM [SHOW EXPERIMENTAL_RANGES FROM TABLE test.bank]`,
-			).Scan(&actual)
-			if ranges != actual {
-				t.Errorf(`expected %d got %d`, ranges, actual)
+				countRangesQ := fmt.Sprintf(
+					`SELECT count(*) FROM [SHOW EXPERIMENTAL_RANGES FROM TABLE test.%s]`, table.Name,
+				)
+				var actual int
+				sqlDB.QueryRow(t, countRangesQ).Scan(&actual)
+				if ranges != actual {
+					t.Errorf(`expected %d got %d`, ranges, actual)
+				}
 			}
 		})
 	}