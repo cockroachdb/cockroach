25f9458146eb2c771168411f99f32a2f86a82858
Yahor Yuzefovich <yahor@cockroachlabs.com>
2019-05-08 17:38:07 -0700

    roachtest: add TPCH benchmark to roachperf
    Adds TPCH benchmark to be run regularly and displayed on roachperf.
    At the moment, query 15 is not being run.
    
    Release note: None

--- pkg/cmd/roachtest/tpchbench_string.go
+++ pkg/cmd/roachtest/tpchbench_string.go
@@ -4,9 +4,9 @@ package main
 
 import "strconv"
 
-const _tpchBench_name = "sql20"
+const _tpchBench_name = "sql20tpch"
 
-var _tpchBench_index = [...]uint8{0, 5}
+var _tpchBench_index = [...]uint8{0, 5, 9}
 
 func (i tpchBench) String() string {
 	if i < 0 || i >= tpchBench(len(_tpchBench_index)-1) {
