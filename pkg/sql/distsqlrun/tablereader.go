ab2f2eb57c3e8bba6960e7b28a3ecf1bc8ad4d52
Yahor Yuzefovich <yahor@cockroachlabs.com>
2019-05-15 18:31:51 -0700

    sql: add bytes read stats
    Adds stats for the number of bytes read by the kv fetcher. This is
    accumulated with every batch read, so there should be no visible
    performance decrease. It is tracked unconditionally, but, at the
    moment, it is exposed only via EXPLAIN ANALYZE (i.e. when tracing
    is on).
    
    Release note: None

--- pkg/sql/distsqlrun/tablereader.go
+++ pkg/sql/distsqlrun/tablereader.go
@@ -16,6 +16,7 @@ package distsqlrun
 
 import (
 	"context"
+	"fmt"
 	"sync"
 
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
@@ -24,6 +25,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util"
+	"github.com/cockroachdb/cockroach/pkg/util/humanizeutil"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/tracing"
 	"github.com/opentracing/opentracing-go"
@@ -299,12 +301,17 @@ const tableReaderTagPrefix = "tablereader."
 
 // Stats implements the SpanStats interface.
 func (trs *TableReaderStats) Stats() map[string]string {
-	return trs.InputStats.Stats(tableReaderTagPrefix)
+	inputStatsMap := trs.InputStats.Stats(tableReaderTagPrefix)
+	inputStatsMap[tableReaderTagPrefix+bytesReadTagSuffix] = humanizeutil.IBytes(trs.BytesRead)
+	return inputStatsMap
 }
 
 // StatsForQueryPlan implements the DistSQLSpanStats interface.
 func (trs *TableReaderStats) StatsForQueryPlan() []string {
-	return trs.InputStats.StatsForQueryPlan("" /* prefix */)
+	return append(
+		trs.InputStats.StatsForQueryPlan("" /* prefix */),
+		fmt.Sprintf("%s: %s", bytesReadQueryPlanSuffix, humanizeutil.IBytes(trs.BytesRead)),
+	)
 }
 
 // outputStatsToTrace outputs the collected tableReader stats to the trace. Will
@@ -315,7 +322,10 @@ func (tr *tableReader) outputStatsToTrace() {
 		return
 	}
 	if sp := opentracing.SpanFromContext(tr.Ctx); sp != nil {
-		tracing.SetSpanStats(sp, &TableReaderStats{InputStats: is})
+		tracing.SetSpanStats(sp, &TableReaderStats{
+			InputStats: is,
+			BytesRead:  tr.fetcher.GetBytesRead(),
+		})
 	}
 }
 
