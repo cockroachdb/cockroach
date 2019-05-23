62a8e63865e046f1fc8a9cd24f551be20a38419e
Rafi Shamim <rafi@cockroachlabs.com>
2019-05-23 11:56:29 -0400

    tree: handle null in suboperator expressions
    Before this change, a null right operand in a suboperator would cause an
    unhandled error. We were receiving stack trace reports from the wild.
    Add a test and fix it.
    
    Release note (bug fix): a null right operand now causes the suboperator
    expression to return null

--- pkg/sql/sem/tree/eval.go
+++ pkg/sql/sem/tree/eval.go
@@ -3669,8 +3669,10 @@ func (expr *ComparisonExpr) Eval(ctx *EvalContext) (Datum, error) {
 	op := expr.Operator
 	if op.hasSubOperator() {
 		var datums Datums
-		// Right is either a tuple or an array of Datums.
-		if tuple, ok := AsDTuple(right); ok {
+		// Right is either null, or a tuple or an array of Datums.
+		if right == DNull {
+			return DNull, nil
+		} else if tuple, ok := AsDTuple(right); ok {
 			datums = tuple.D
 		} else if array, ok := AsDArray(right); ok {
 			datums = array.Array
