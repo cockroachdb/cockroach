3eaaf97d952c1c5d181f0ad0605354ec48597d40
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2019-05-28 11:36:56 -0400

    workload/ycsb: add skewed latest generator and support for workload D
    The skewed latest generator is used for workload D and skews to the most
    recently inserted keys. Additionally, changed the API for zipfGenerator
    to generate an inclusive bound in [0, defaultIMax - 1], instead of [1,
    defaultIMax].
    
    Release note: None

---
+++ pkg/workload/ycsb/skewed_latest_generator.go
@@ -0,0 +1,67 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License. See the AUTHORS file
+// for names of contributors.
+
+package ycsb
+
+import (
+	"math/rand"
+
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+)
+
+// SkewedLatestGenerator is a random number generator that generates numbers in
+// the range [iMin, iMax], but skews it towards iMax using a zipfian
+// distribution.
+type SkewedLatestGenerator struct {
+	mu struct {
+		syncutil.Mutex
+		iMax    uint64
+		zipfGen *ZipfGenerator
+	}
+}
+
+// NewSkewedLatestGenerator constructs a new SkewedLatestGenerator with the
+// given parameters. It returns an error if the parameters are outside the
+// accepted range.
+func NewSkewedLatestGenerator(
+	rng *rand.Rand, iMin, iMax uint64, theta float64, verbose bool,
+) (*SkewedLatestGenerator, error) {
+
+	z := SkewedLatestGenerator{}
+	z.mu.iMax = iMax
+	zipfGen, err := NewZipfGenerator(rng, 0, iMax-iMin, theta, verbose)
+	if err != nil {
+		return nil, err
+	}
+	z.mu.zipfGen = zipfGen
+
+	return &z, nil
+}
+
+// IncrementIMax increments iMax.
+func (z *SkewedLatestGenerator) IncrementIMax() error {
+	z.mu.Lock()
+	defer z.mu.Unlock()
+	z.mu.iMax++
+	return z.mu.zipfGen.IncrementIMax()
+}
+
+// Uint64 returns a random Uint64 between iMin and iMax, where keys near iMax
+// are most likely to be drawn.
+func (z *SkewedLatestGenerator) Uint64() uint64 {
+	z.mu.Lock()
+	defer z.mu.Unlock()
+	return z.mu.iMax - z.mu.zipfGen.Uint64()
+}
