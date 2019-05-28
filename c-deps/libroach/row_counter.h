33fd2d4ae2d592ad0ea9dec5a0d017c4aa2901f2
Aditya Maru <adityamaru@cockroachlabs.com>
2019-05-28 10:40:51 -0400

    libroach: Migrate RowCounter logic to C++
    The stats collection for an `ExportRequest` previously required
    an additional iteration over the newly created SSTable, once the
    data had been exported. This was beacuse the RowCounter used in
    this process only existed on the go side.
    This migration pushes the `RowCounter` to C++ enabling us to
    accumulate statistics on the fly while exporting data.
    
    Release note: Perfromance improvement to the BACKUP process.

---
+++ c-deps/libroach/row_counter.h
@@ -0,0 +1,34 @@
+// Copyright 2016 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License included
+// in the file licenses/BSL.txt and at www.mariadb.com/bsl11.
+//
+// Change Date: 2022-10-01
+//
+// On the date above, in accordance with the Business Source License, use
+// of this software will be governed by the Apache License, Version 2.0,
+// included in the file licenses/APL.txt and at
+// https://www.apache.org/licenses/LICENSE-2.0
+
+#pragma once
+
+#include <libroach.h>
+#include "db.h"
+#include "protos/roachpb/api.pb.h"
+
+// MaxReservedDescID is the maximum value of reserved descriptor IDs. Reserved
+// IDs are used by namespaces and tables used internally by cockroach.
+const int MaxReservedDescID = 49;
+
+// RowCounter counts how many distinct rows appear in the KVs that is is shown
+// via `Count`. Note: the `DataSize` field of the BulkOpSummary is *not*
+// populated by this and should be set separately.
+struct RowCounter {
+  bool Count(const rocksdb::Slice& key, cockroach::roachpb::BulkOpSummary* summary);
+
+ private:
+  void EnsureSafeSplitKey(rocksdb::Slice* key);
+  int GetRowPrefixLength(rocksdb::Slice* key);
+  DBString prev_key;
+  rocksdb::Slice prev;
+};
