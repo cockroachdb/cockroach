c5218e01c91859cd31c7e6a73294714c50996ad9
Rebecca Taft <becca@cockroachlabs.com>
2019-06-03 17:13:26 -0400

    opt: fix issue with dangling pointer in statisticsBuilder
    This commit fixes an issue where the statisticsBuilder was using
    an invalid pointer to update statistics for some multi-column stats
    in the local table statistics cache. The fix is to re-fetch the
    pointer to the stats just before updating them, in case the
    previously fetched address is no longer valid.
    
    This problem is very hard to reproduce, so I had to use the somewhat
    complex query from the issue for a regression test.
    
    Fixes #37953
    
    Release note: None

--- pkg/sql/opt/memo/statistics_builder.go
+++ pkg/sql/opt/memo/statistics_builder.go
@@ -380,6 +380,8 @@ func (sb *statisticsBuilder) colStatLeaf(
 				colStat.NullCount = s.RowCount * unknownNullCountRatio
 			} else {
 				colStatLeaf := sb.colStatLeaf(nullableCols, s, fd, notNullCols)
+				// Fetch the colStat again since it may now have a different address.
+				colStat, _ = s.ColStats.Lookup(colSet)
 				colStat.NullCount = colStatLeaf.NullCount
 			}
 		}
@@ -408,6 +410,8 @@ func (sb *statisticsBuilder) colStatLeaf(
 				nullCount += colStatLeaf.NullCount * (1 - nullCount/s.RowCount)
 			}
 		})
+		// Fetch the colStat again since it may now have a different address.
+		colStat, _ = s.ColStats.Lookup(colSet)
 		colStat.DistinctCount = min(distinctCount, s.RowCount)
 		colStat.NullCount = min(nullCount, s.RowCount)
 	}
