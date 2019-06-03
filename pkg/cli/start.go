94bcd259836bccfe317145ff467d726078d27383
Raphael 'kena' Poss <knz@cockroachlabs.com>
2019-06-03 17:13:39 +0200

    cli: fix use of IPv6 addresses with RPC client commands
    Release note (bug fix): the `cockroach` command line utilities that
    internally use a RPC connection (e.g. `cockroach quit`, `cockroach
    init`, etc) again properly support passing an IPv6 address literal via
    the `--host` argument.

--- pkg/cli/start.go
+++ pkg/cli/start.go
@@ -1105,9 +1105,6 @@ func addrWithDefaultHost(addr string) (string, error) {
 	if host == "" {
 		host = "localhost"
 	}
-	if strings.Contains(host, ":") {
-		host = "[" + host + "]"
-	}
 	return net.JoinHostPort(host, port), nil
 }
 
