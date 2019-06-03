2ca9030ce79aadcff54e0d4b22f34118f9a8e9e9
Rohan Yadav <rohany@alumni.cmu.edu>
2019-06-03 10:21:47 -0400

    backend and tests done for backend of show index cmd

--- pkg/sql/sem/tree/show.go
+++ pkg/sql/sem/tree/show.go
@@ -155,14 +155,14 @@ func (node *ShowIndexes) Format(ctx *FmtCtx) {
 	ctx.FormatNode(node.Table)
 }
 
-// ShowDbIndexes represents a SHOW INDEXES FROM DATABASE statement.
+// ShowDbIndexes represents a SHOW DBINDEXES FROM DATABASE statement.
 type ShowDbIndexes struct {
 	Database string
 }
 
 // Format implements the NodeFormatter interface.
 func (node *ShowDbIndexes) Format(ctx *FmtCtx) {
-	ctx.WriteString("SHOW INDEXES FROM DATABASE ")
+	ctx.WriteString("SHOW DBINDEXES FROM ")
 	ctx.WriteString(node.Database)
 }
 
