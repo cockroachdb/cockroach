7fdc7560148cc92ef2879b7f9359f214e0b17f34
Raphael 'kena' Poss <knz@cockroachlabs.com>
2019-06-05 09:31:31 +0200

    sql: fix the retry counter in the statement statistics
    The statement statistics contain a column `max_retries` intended
    to show the maximum number of times a statement was retried.
    
    Due to a bug, this counter was previously always zero, *even for
    statements that were actually retried*. We hadn't noticed because
    there was no test to ensure that the value would increase.
    
    This patch fixes the bugs and verifies that the value does indeed
    increase.
    
    Release note (bug fix): The `max_retries` column in the statement
    statistics now properly displays the maximum number of times
    a statement was retried.

--- pkg/sql/conn_executor.go
+++ pkg/sql/conn_executor.go
@@ -1032,8 +1032,6 @@ func (ex *connExecutor) resetExtraTxnState(
 
 	ex.extraTxnState.tables.databaseCache = dbCacheHolder.getDatabaseCache()
 
-	ex.extraTxnState.autoRetryCounter = 0
-
 	// Close all portals.
 	for name, p := range ex.extraTxnState.prepStmtsNamespace.portals {
 		p.decRef(ctx)
@@ -1966,6 +1964,7 @@ func (ex *connExecutor) txnStateTransitionsApplyWrapper(
 	switch advInfo.txnEvent {
 	case noEvent:
 	case txnStart:
+		ex.extraTxnState.autoRetryCounter = 0
 	case txnCommit:
 		if res.Err() != nil {
 			err := errorutil.UnexpectedWithIssueErrorf(
