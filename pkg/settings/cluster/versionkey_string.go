985d828d1781039bf089e800248d872d8ab2009a
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2019-06-07 14:27:03 +0200

    cluster: remove Version2_0
    Release note: None

--- pkg/settings/cluster/versionkey_string.go
+++ pkg/settings/cluster/versionkey_string.go
@@ -8,29 +8,28 @@ func _() {
 	// An "invalid array index" compiler error signifies that the constant values have changed.
 	// Re-run the stringer command to generate them again.
 	var x [1]struct{}
-	_ = x[Version2_0-0]
-	_ = x[Version2_1-1]
-	_ = x[VersionCascadingZoneConfigs-2]
-	_ = x[VersionLoadSplits-3]
-	_ = x[VersionExportStorageWorkload-4]
-	_ = x[VersionLazyTxnRecord-5]
-	_ = x[VersionSequencedReads-6]
-	_ = x[VersionUnreplicatedRaftTruncatedState-7]
-	_ = x[VersionCreateStats-8]
-	_ = x[VersionDirectImport-9]
-	_ = x[VersionSideloadedStorageNoReplicaID-10]
-	_ = x[VersionPushTxnToInclusive-11]
-	_ = x[VersionSnapshotsWithoutLog-12]
-	_ = x[Version19_1-13]
-	_ = x[VersionStart19_2-14]
-	_ = x[VersionQueryTxnTimestamp-15]
-	_ = x[VersionStickyBit-16]
-	_ = x[VersionParallelCommits-17]
+	_ = x[Version2_1-0]
+	_ = x[VersionCascadingZoneConfigs-1]
+	_ = x[VersionLoadSplits-2]
+	_ = x[VersionExportStorageWorkload-3]
+	_ = x[VersionLazyTxnRecord-4]
+	_ = x[VersionSequencedReads-5]
+	_ = x[VersionUnreplicatedRaftTruncatedState-6]
+	_ = x[VersionCreateStats-7]
+	_ = x[VersionDirectImport-8]
+	_ = x[VersionSideloadedStorageNoReplicaID-9]
+	_ = x[VersionPushTxnToInclusive-10]
+	_ = x[VersionSnapshotsWithoutLog-11]
+	_ = x[Version19_1-12]
+	_ = x[VersionStart19_2-13]
+	_ = x[VersionQueryTxnTimestamp-14]
+	_ = x[VersionStickyBit-15]
+	_ = x[VersionParallelCommits-16]
 }
 
-const _VersionKey_name = "Version2_0Version2_1VersionCascadingZoneConfigsVersionLoadSplitsVersionExportStorageWorkloadVersionLazyTxnRecordVersionSequencedReadsVersionUnreplicatedRaftTruncatedStateVersionCreateStatsVersionDirectImportVersionSideloadedStorageNoReplicaIDVersionPushTxnToInclusiveVersionSnapshotsWithoutLogVersion19_1VersionStart19_2VersionQueryTxnTimestampVersionStickyBitVersionParallelCommits"
+const _VersionKey_name = "Version2_1VersionCascadingZoneConfigsVersionLoadSplitsVersionExportStorageWorkloadVersionLazyTxnRecordVersionSequencedReadsVersionUnreplicatedRaftTruncatedStateVersionCreateStatsVersionDirectImportVersionSideloadedStorageNoReplicaIDVersionPushTxnToInclusiveVersionSnapshotsWithoutLogVersion19_1VersionStart19_2VersionQueryTxnTimestampVersionStickyBitVersionParallelCommits"
 
-var _VersionKey_index = [...]uint16{0, 10, 20, 47, 64, 92, 112, 133, 170, 188, 207, 242, 267, 293, 304, 320, 344, 360, 382}
+var _VersionKey_index = [...]uint16{0, 10, 37, 54, 82, 102, 123, 160, 178, 197, 232, 257, 283, 294, 310, 334, 350, 372}
 
 func (i VersionKey) String() string {
 	if i < 0 || i >= VersionKey(len(_VersionKey_index)-1) {
