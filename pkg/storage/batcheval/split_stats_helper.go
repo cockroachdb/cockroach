3b0b7fdf76e16b3681dc63136d2df875bd086c81
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2019-06-07 23:02:25 +0200

    batcheval: refactor split trigger stats computation
    Pulling out the stats-related computations into a helper taylored
    exactly to what happens during a split provides a convenient venue for
    lengthy rumination on the finer points of said stats computations, while
    decluttering the split trigger's main method.
    
    This was motivated by #37583, which needs to adjust some of this code.
    Now that it's (hopefully) more accessible, it should be more straight-
    forward to figure out what exactly to do.
    
    Release note: None

---
+++ pkg/storage/batcheval/split_stats_helper.go
@@ -0,0 +1,169 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License included
+// in the file licenses/BSL.txt and at www.mariadb.com/bsl11.
+//
+// Change Date: 2022-06-06
+//
+// On the date above, in accordance with the Business Source License, use
+// of this software will be governed by the Apache License, Version 2.0,
+// included in the file licenses/APL.txt and at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+package batcheval
+
+import "github.com/cockroachdb/cockroach/pkg/storage/engine/enginepb"
+
+// SplitStatsHelper codifies and explains the stats computations related to a
+// split. The quantities known during a split (i.e. while the split trigger
+// is evaluating) are
+//
+// - AbsPreSplitBothEstimated: the stats of the range before the split trigger,
+//   i.e. without accounting for any writes in the batch. This can have
+//   ContainsEstimates set.
+// - DeltaBatchEstimated: the writes in the batch. Since we have no reason to
+//   introduce ContainsEstimates in a split trigger, this typically has
+//   ContainsEstimates unset, but this code does not rely on that.
+// - AbsPostSplitLeft: the stats of the range after applying the split, i.e.
+//   accounting both for the shrinking as well as for the writes in DeltaBatch
+//   related to the shrunk keyrange.
+//   In practice, we obtain this by recomputing the stats, and so we don't
+//   expect ContainsEstimates to be set in them.
+//
+// We are interested in computing from this the quantities
+//
+// - AbsPostSplitRight(): the stats of the right hand side created by the split,
+//   i.e. the data taken over from the left hand side plus whatever was written
+//   to the right hand side in the process (metadata etc). We can recompute this,
+//   but try to avoid it unless necessary (when CombinedError below is nonzero).
+// - DeltaPostSplitLeft(): the stats delta that should be emitted by the split
+//   trigger itself, i.e. the data which the left hand side (initially comprising
+//   both halves) loses by moving data into the right hand side (including whatever
+//   DeltaBatch contained in contributions attributable to the keyspace on the
+//   left).
+// - CombinedError: the difference between (AbsPreSplitBoth+DeltaBatch) and the
+//   recomputation of the pre-split range including the batch. This is zero if
+//   neither of the inputs contains estimates. If it's not zero, we need to
+//   recompute from scratch to obtain AbsPostSplitRight. What's interesting
+//   about this quantity is that we never care what exactly it is, but we do
+//   care whether it's zero or not because if it's zero we get to do less work.
+//
+// Moreover, we want both neither of AbsPostSplit{Right,Left} to end up with
+// estimates. The way splits are set up right now, we sort of get this "for
+// free" for the left hand side (since we recompute that unconditionally; there
+// is a guarantee that the left hand side is never too large). We also don't
+// want to create new ranges that start out with estimates (just to prevent the
+// unbounded proliferation of estimates).
+//
+// The two unknown quantities can be expressed in terms of the known quantities
+// because
+//
+// (1) AbsPreSplitBoth + DeltaBatch - CombinedError = AbsPostSplitLeft + AbsPostSplitRight
+//
+// In words, this corresponds to "all bytes are accounted for": from the initial
+// stats that we have (accounting for the fact that AbsPreSplitBoth+DeltaBatch
+// may contain estimates), everything we add/remove during the split ends up
+// tracked either on the left and on the right, and nothing is created out of
+// thin air.
+//
+// (2) AbsPreSplitBoth + DeltaPostSplitLeft() = AbsPostSplitLeft
+//
+// This expresses the fact that is always true whenever a command applies on a
+// range without introducing an estimate: the stats before the command plus the
+// delta emitted by the command equal the stats after the command. In this case,
+// the stats before the command are that of the range before the split (remember
+// that the split shrinks the range towards the start key, i.e. the left hand
+// side is the same range as the pre-split one).
+//
+// These two equations are easily solved for the unknowns. First, we can express
+// DeltaPostSplitLeft() in known quantities via (2) as
+//
+//     DeltaPostSplitLeft() = AbsPostSplitLeft - AbsPreSplitBothEstimated.
+//
+// Note that if we start out with estimates, DeltaPostSplitLeft() will wipe out
+// those estimates when added to the absolute stats.
+//
+// For AbsPostSplitRight(), there are two cases. First, due to the identity
+//
+//     CombinedError =   AbsPreSplitBothEstimated + DeltaBatchEstimated
+//                     -(AbsPostSplitLeft + AbsPostSplitRight)
+//
+// and the fact that the second line coontains no estimates, we know that
+// CombinedError is zero if the first line contains no estimates. Using this,
+// we can rearrange as
+//
+//     AbsPostSplitRight() = AbsPreSplitBoth + DeltaBatch - AbsPostSplitLeft.
+//
+// where all quantities on the right are known. If CombinedError is nonzero,
+// we effectively have one more unknown in our linear system and we need to
+// recompute AbsPostSplitRight from scratch. (As fallout, we can in principle
+// compute CombinedError, but we don't care).
+type SplitStatsHelper struct {
+	in SplitStatsHelperInput
+
+	absPostSplitRight *enginepb.MVCCStats
+}
+
+// SplitStatsHelperInput is passed to MakeSplitStatsHelper.
+type SplitStatsHelperInput struct {
+	AbsPreSplitBothEstimated enginepb.MVCCStats
+	DeltaBatchEstimated      enginepb.MVCCStats
+	AbsPostSplitLeft         enginepb.MVCCStats
+	AbsPostSplitRightFn      func() (enginepb.MVCCStats, error)
+}
+
+// MakeSplitStatsHelper initializes a SplitStatsHelper. The values in the input
+// are assumed to not change outside of the helper and must no longer be used.
+// The provided AbsPostSplitRightFn recomputes the right hand side of the split
+// after accounting for the split trigger batch. This is only invoked at most
+// once, and only when necessary.
+func MakeSplitStatsHelper(input SplitStatsHelperInput) (SplitStatsHelper, error) {
+	h := SplitStatsHelper{
+		in: input,
+	}
+
+	if !h.in.AbsPreSplitBothEstimated.ContainsEstimates &&
+		!h.in.DeltaBatchEstimated.ContainsEstimates {
+		// We have CombinedError zero, so use arithmetic to compute
+		// AbsPostSplitRight().
+		ms := h.in.AbsPreSplitBothEstimated
+		ms.Subtract(h.in.AbsPostSplitLeft)
+		ms.Add(h.in.DeltaBatchEstimated)
+		h.absPostSplitRight = &ms
+		return h, nil
+	}
+	// Estimates are contained in the input, so ask the oracle for
+	// AbsPostSplitRight().
+	ms, err := input.AbsPostSplitRightFn()
+	if err != nil {
+		return SplitStatsHelper{}, err
+	}
+	h.absPostSplitRight = &ms
+	return h, nil
+}
+
+// AbsPostSplitRight returns the stats of the right hand side created by the
+// split. The result is returned as a pointer because the caller can freely
+// modify it, assuming they're adding only stats corresponding to mutations that
+// they know only affect the right hand side. (If estimates are introduced in
+// the process, the right hand side will start out with estimates). Implicitly
+// this changes the DeltaBatchEstimated supplied to MakeSplitStatsHelper, but
+// the contract assumes that that value will no longer be used.
+func (h SplitStatsHelper) AbsPostSplitRight() *enginepb.MVCCStats {
+	return h.absPostSplitRight
+
+}
+
+// DeltaPostSplitLeft return the stats delta to be emitted on the left hand side
+// as the result of the split. It accounts for the data moved to the right hand
+// side as well as any mutations to the left hand side carried out during the
+// split, and additionally removes any estimates present in the pre-split stats.
+func (h SplitStatsHelper) DeltaPostSplitLeft() enginepb.MVCCStats {
+	// NB: if we ever wanted to also write to the left hand side after init'ing
+	// the helper, we can make that work, too.
+	// NB: note how none of this depends on mutations to absPostSplitRight.
+	ms := h.in.AbsPostSplitLeft
+	ms.Subtract(h.in.AbsPreSplitBothEstimated)
+
+	return ms
+}
