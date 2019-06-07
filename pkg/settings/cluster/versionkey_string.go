1e31e6d1c1c60f57e3371a89f330753c9c10e507
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2019-06-07 14:23:43 +0200

    cluster: remove VersionSecondaryLookupJoins
    Release note: None

--- pkg/settings/cluster/versionkey_string.go
+++ pkg/settings/cluster/versionkey_string.go
@@ -12,29 +12,28 @@ func _() {
 	_ = x[VersionImportSkipRecords-1]
 	_ = x[VersionProposedTSLeaseRequest-2]
 	_ = x[VersionImportFormats-3]
-	_ = x[VersionSecondaryLookupJoins-4]
-	_ = x[Version2_1-5]
-	_ = x[VersionCascadingZoneConfigs-6]
-	_ = x[VersionLoadSplits-7]
-	_ = x[VersionExportStorageWorkload-8]
-	_ = x[VersionLazyTxnRecord-9]
-	_ = x[VersionSequencedReads-10]
-	_ = x[VersionUnreplicatedRaftTruncatedState-11]
-	_ = x[VersionCreateStats-12]
-	_ = x[VersionDirectImport-13]
-	_ = x[VersionSideloadedStorageNoReplicaID-14]
-	_ = x[VersionPushTxnToInclusive-15]
-	_ = x[VersionSnapshotsWithoutLog-16]
-	_ = x[Version19_1-17]
-	_ = x[VersionStart19_2-18]
-	_ = x[VersionQueryTxnTimestamp-19]
-	_ = x[VersionStickyBit-20]
-	_ = x[VersionParallelCommits-21]
+	_ = x[Version2_1-4]
+	_ = x[VersionCascadingZoneConfigs-5]
+	_ = x[VersionLoadSplits-6]
+	_ = x[VersionExportStorageWorkload-7]
+	_ = x[VersionLazyTxnRecord-8]
+	_ = x[VersionSequencedReads-9]
+	_ = x[VersionUnreplicatedRaftTruncatedState-10]
+	_ = x[VersionCreateStats-11]
+	_ = x[VersionDirectImport-12]
+	_ = x[VersionSideloadedStorageNoReplicaID-13]
+	_ = x[VersionPushTxnToInclusive-14]
+	_ = x[VersionSnapshotsWithoutLog-15]
+	_ = x[Version19_1-16]
+	_ = x[VersionStart19_2-17]
+	_ = x[VersionQueryTxnTimestamp-18]
+	_ = x[VersionStickyBit-19]
+	_ = x[VersionParallelCommits-20]
 }
 
-const _VersionKey_name = "Version2_0VersionImportSkipRecordsVersionProposedTSLeaseRequestVersionImportFormatsVersionSecondaryLookupJoinsVersion2_1VersionCascadingZoneConfigsVersionLoadSplitsVersionExportStorageWorkloadVersionLazyTxnRecordVersionSequencedReadsVersionUnreplicatedRaftTruncatedStateVersionCreateStatsVersionDirectImportVersionSideloadedStorageNoReplicaIDVersionPushTxnToInclusiveVersionSnapshotsWithoutLogVersion19_1VersionStart19_2VersionQueryTxnTimestampVersionStickyBitVersionParallelCommits"
+const _VersionKey_name = "Version2_0VersionImportSkipRecordsVersionProposedTSLeaseRequestVersionImportFormatsVersion2_1VersionCascadingZoneConfigsVersionLoadSplitsVersionExportStorageWorkloadVersionLazyTxnRecordVersionSequencedReadsVersionUnreplicatedRaftTruncatedStateVersionCreateStatsVersionDirectImportVersionSideloadedStorageNoReplicaIDVersionPushTxnToInclusiveVersionSnapshotsWithoutLogVersion19_1VersionStart19_2VersionQueryTxnTimestampVersionStickyBitVersionParallelCommits"
 
-var _VersionKey_index = [...]uint16{0, 10, 34, 63, 83, 110, 120, 147, 164, 192, 212, 233, 270, 288, 307, 342, 367, 393, 404, 420, 444, 460, 482}
+var _VersionKey_index = [...]uint16{0, 10, 34, 63, 83, 93, 120, 137, 165, 185, 206, 243, 261, 280, 315, 340, 366, 377, 393, 417, 433, 455}
 
 func (i VersionKey) String() string {
 	if i < 0 || i >= VersionKey(len(_VersionKey_index)-1) {
