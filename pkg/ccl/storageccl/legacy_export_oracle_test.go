4c9fd14afa71174cef8b2965da52708b2aaf4d12
Ben Darnell <ben@bendarnell.com>
2019-06-08 11:10:49 -0400

    *: Add linter for license headers
    Update a few licenses that got missed. Standardize the language used
    on the CCL side (javascript was using slightly different language).
    
    Release note: None

--- pkg/ccl/storageccl/legacy_export_oracle_test.go
+++ pkg/ccl/storageccl/legacy_export_oracle_test.go
@@ -1,14 +1,11 @@
 // Copyright 2019 The Cockroach Authors.
 //
-// Use of this software is governed by the Business Source License included
-// in the file licenses/BSL.txt and at www.mariadb.com/bsl11.
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
 //
-// Change Date: 2022-10-01
-//
-// On the date above, in accordance with the Business Source License, use
-// of this software will be governed by the Apache License, Version 2.0,
-// included in the file licenses/APL.txt and at
-// http://www.apache.org/licenses/LICENSE-2.0
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
 package storageccl
 
 import (
