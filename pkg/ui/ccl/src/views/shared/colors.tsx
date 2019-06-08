77330146cc99787b20f8a252ca8e327d0a9e6be5
Ben Darnell <ben@bendarnell.com>
2019-06-08 11:10:49 -0400

    *: Add linter for license headers
    Update a few licenses that got missed. Standardize the language used
    on the CCL side (javascript was using slightly different language).
    
    Release note: None

--- pkg/ui/ccl/src/views/shared/colors.tsx
+++ pkg/ui/ccl/src/views/shared/colors.tsx
@@ -1,3 +1,11 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
 // TODO(vilterp): pull this out into CSS...
 
 export const MAIN_BLUE = "#3A7DE1";  // This should match $main-blue-color in palette.styl
