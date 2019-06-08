ac2572bc5f9a01035db7ff4fc6171e9455acd235
Ben Darnell <ben@bendarnell.com>
2019-06-08 11:10:49 -0400

    *: Add linter for license headers
    Update a few licenses that got missed. Standardize the language used
    on the CCL side (javascript was using slightly different language).
    
    Release note: None

--- pkg/ccl/storageccl/engineccl/enable_cc.cc
+++ pkg/ccl/storageccl/engineccl/enable_cc.cc
@@ -1,3 +1,11 @@
+// Copyright 2017 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
 // Since we link against C++ libraries, like RocksDB and libroach, we need to
 // link against the C++ standard library. This presence of this file convinces
 // cgo to link this package using the C++ compiler instead of the C compiler,
