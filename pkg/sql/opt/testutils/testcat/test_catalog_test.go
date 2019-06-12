d863581cd4f8a9483b02771fdd3ec8e86a988ea7
Radu Berinde <radu@cockroachlabs.com>
2019-06-12 14:55:21 +0300

    opt: test catalog improvements
    This commit makes the following changes to the test catalog:
    
     - `exec-ddl` now supports `SHOW CREATE` which prints information
       about a table; the information is extended to include
       column default/computed values, inbound foreign keys, zones.
    
     - we add specific datadriven tests for the test catalog.
    
     - other `exec-ddl` statements no longer output any information;
       outside of debugging the test catalog itself (for which
       `SHOW CREATE` can be used), this is unnecessary clutter.
    
    This change was motivated by some bugs in the catalog (which will be
    fixed in subsequent commits).
    
    Release note: None

---
+++ pkg/sql/opt/testutils/testcat/test_catalog_test.go
@@ -0,0 +1,35 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License included
+// in the file licenses/BSL.txt and at www.mariadb.com/bsl11.
+//
+// Change Date: 2022-10-01
+//
+// On the date above, in accordance with the Business Source License, use
+// of this software will be governed by the Apache License, Version 2.0,
+// included in the file licenses/APL.txt and at
+// https://www.apache.org/licenses/LICENSE-2.0
+
+package testcat_test
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/testutils/opttester"
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/testutils/testcat"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/datadriven"
+)
+
+func TestCatalog(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	datadriven.Walk(t, "testdata", func(t *testing.T, path string) {
+		catalog := testcat.New()
+
+		datadriven.RunTest(t, path, func(d *datadriven.TestData) string {
+			tester := opttester.New(catalog, d.Input)
+			return tester.RunCommand(t, d)
+		})
+	})
+}
