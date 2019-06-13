d82b78f5083c8a963fca26b53c0caf0e20161da3
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2019-06-13 12:03:28 -0400

    workload/tpcc: fix --partition-affinity flag
    This was broken by b4e7796.
    
    Release note: None

--- pkg/workload/tpcc/tpcc.go
+++ pkg/workload/tpcc/tpcc.go
@@ -551,12 +551,11 @@ func (w *tpcc) Ops(urls []string, reg *histogram.Registry) (workload.QueryLoad, 
 
 	fmt.Printf("Initializing %d workers and preparing statements...\n", w.workers)
 	ql := workload.QueryLoad{SQLDatabase: sqlDatabase}
-	ql.WorkerFns = make([]func(context.Context) error, w.workers)
 	var group errgroup.Group
 	// Limit the amount of workers we initialize in parallel, to avoid running out
 	// of memory (#36897).
 	sem := make(chan struct{}, 100)
-	for workerIdx := range ql.WorkerFns {
+	for workerIdx := 0; workerIdx < w.workers; workerIdx++ {
 		workerIdx := workerIdx
 		warehouse := w.wPart.totalElems[workerIdx%len(w.wPart.totalElems)]
 
@@ -568,11 +567,13 @@ func (w *tpcc) Ops(urls []string, reg *histogram.Registry) (workload.QueryLoad, 
 		dbs := partitionDBs[p]
 		db := dbs[warehouse%len(dbs)]
 
+		ql.WorkerFns = append(ql.WorkerFns, nil)
+		idx := len(ql.WorkerFns) - 1
 		sem <- struct{}{}
 		group.Go(func() error {
 			worker, err := newWorker(context.TODO(), w, db, reg.GetHandle(), warehouse)
 			if err == nil {
-				ql.WorkerFns[workerIdx] = worker.run
+				ql.WorkerFns[idx] = worker.run
 			}
 			<-sem
 			return err
