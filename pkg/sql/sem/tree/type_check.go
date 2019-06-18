1419ceb44c31bb3f62333bb66dc81c9481462694
Justin Jaffray <justin@cockroachlabs.com>
2019-06-18 12:02:38 -0400

    sql: prohibit offset with GROUPS containing vars
    This commit adds a check during semantic analysis to verify that a
    window function using GROUPS mode does not include an offset expression
    containing variable references.
    
    Release note: None

--- pkg/sql/sem/tree/type_check.go
+++ pkg/sql/sem/tree/type_check.go
@@ -685,6 +685,7 @@ func (expr *ComparisonExpr) TypeCheck(ctx *SemaContext, desired *types.T) (Typed
 
 var (
 	errOrderByIndexInWindow = pgerror.New(pgerror.CodeFeatureNotSupportedError, "ORDER BY INDEX in window definition is not supported")
+	errVarOffsetGroups      = pgerror.New(pgerror.CodeWindowingError, fmt.Sprintf("GROUPS offset cannot contain variables"))
 	errStarNotAllowed       = pgerror.New(pgerror.CodeSyntaxError, "cannot use \"*\" in this context")
 	errInvalidDefaultUsage  = pgerror.New(pgerror.CodeSyntaxError, "DEFAULT can only appear in a VALUES list within INSERT or on the right side of a SET")
 	errInvalidMaxUsage      = pgerror.New(pgerror.CodeSyntaxError, "MAXVALUE can only appear within a range partition expression")
@@ -990,6 +991,16 @@ func (f *WindowFrame) TypeCheck(ctx *SemaContext, windowDef *WindowDef) error {
 			}
 		}
 	case GROUPS:
+		if startBound != nil && startBound.HasOffset() {
+			if ContainsVars(startBound.OffsetExpr) {
+				return errVarOffsetGroups
+			}
+		}
+		if endBound != nil && endBound.HasOffset() {
+			if ContainsVars(endBound.OffsetExpr) {
+				return errVarOffsetGroups
+			}
+		}
 		// In GROUPS mode, offsets must be non-null, non-negative integers.
 		// Non-nullity and non-negativity will be checked later.
 		requiredType = types.Int
