5ca0791ac6815ba200e0deccbbee877c7a117943
Rafi Shamim <rafi@cockroachlabs.com>
2019-06-19 18:13:00 -0400

    exec: implement vectorized ordinality operator
    > make bench PKG=./pkg/sql/exec BENCHES=BenchmarkOrdinality TESTFLAGS=-benchmem
    
    BenchmarkOrdinality-12    2150 ns/op    11427.83 MB/s
    
    Release note: None

---
+++ pkg/sql/exec/ordinality.go
@@ -0,0 +1,75 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License included
+// in the file licenses/BSL.txt and at www.mariadb.com/bsl11.
+//
+// Change Date: 2022-10-01
+//
+// On the date above, in accordance with the Business Source License, use
+// of this software will be governed by the Apache License, Version 2.0,
+// included in the file licenses/APL.txt and at
+// https://www.apache.org/licenses/LICENSE-2.0
+
+package exec
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/exec/coldata"
+	"github.com/cockroachdb/cockroach/pkg/sql/exec/types"
+)
+
+// ordinalityOp is an operator that implements WITH ORDINALITY, which adds
+// an additional column to the result with an ordinal number.
+type ordinalityOp struct {
+	input Operator
+
+	// the number of columns in the output batch.
+	numOutCols int
+	// the number of tuples seen so far.
+	counter int64
+}
+
+var _ Operator = &ordinalityOp{}
+
+// NewOrdinalityOp returns a new WITH ORDINALITY operator.
+func NewOrdinalityOp(input Operator, numOutCols int) Operator {
+	c := &ordinalityOp{
+		input:      input,
+		numOutCols: numOutCols,
+		counter:    1,
+	}
+	return c
+}
+
+func (c *ordinalityOp) Init() {
+	c.input.Init()
+}
+
+func (c *ordinalityOp) Next(ctx context.Context) coldata.Batch {
+	bat := c.input.Next(ctx)
+	numBatCols := bat.Width()
+	if numBatCols < c.numOutCols {
+		bat.AppendCol(types.Int64)
+	} else if numBatCols != c.numOutCols {
+		panic(fmt.Sprintf("Incorrect number of columns in batch: %d. Expected %d or %d",
+			numBatCols, c.numOutCols-1, c.numOutCols))
+	}
+	vec := bat.ColVec(bat.Width() - 1).Int64()
+	sel := bat.Selection()
+
+	if sel != nil {
+		for i := uint16(0); i < bat.Length(); i++ {
+			vec[sel[i]] = c.counter
+			c.counter++
+		}
+	} else {
+		for i := uint16(0); i < bat.Length(); i++ {
+			vec[i] = c.counter
+			c.counter++
+		}
+	}
+
+	return bat
+}
