3eb0d7a19149fb0a569067173d82d67bbf1a45dd
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2019-06-20 18:25:13 -0400

    sql: handle database_name and composite primary key for UNSPLIT ALL
    UNSPLIT ALL was not using database in the query to
    crdb_internal.ranges_no_leases. It was also not handling composite keys
    correctly as the result column is a single column of bytes. Finally, the
    retry mechanism was not working because the benign error was not
    returned.
    
    Release note (bug fix): Handle database_name and composite primary key
    for UNSPLIT ALL.

--- pkg/sql/unsplit_test.go
+++ pkg/sql/unsplit_test.go
@@ -42,110 +42,161 @@ func TestUnsplitAt(t *testing.T) {
 		INDEX s_idx (s)
 	)`)
 	r.Exec(t, `CREATE TABLE d.i (k INT PRIMARY KEY)`)
-
-	// Create initial splits
-	splitStmts := []string{
-		"ALTER TABLE d.t SPLIT AT VALUES (2, 'b'), (3, 'c'), (4, 'd'), (5, 'd'), (6, 'e'), (7, 'f')",
-		"ALTER TABLE d.t SPLIT AT VALUES (10)",
-		"ALTER TABLE d.i SPLIT AT VALUES (1), (8)",
-		"ALTER INDEX d.t@s_idx SPLIT AT VALUES ('f')",
-	}
-
-	for _, splitStmt := range splitStmts {
-		var key roachpb.Key
-		var pretty string
-		var expirationTimestamp gosql.NullString
-		if err := db.QueryRow(splitStmt).Scan(&key, &pretty, &expirationTimestamp); err != nil {
-			t.Fatalf("unexpected error setting up test: %s", err)
-		}
-	}
+	r.Exec(t, `CREATE TABLE i (k INT PRIMARY KEY)`)
 
 	tests := []struct {
-		in    string
+		splitStmt   string
+		unsplitStmt string
+		// Number of unsplits expected.
+		count int
 		error string
 		args  []interface{}
 	}{
 		{
-			in: "ALTER TABLE d.t UNSPLIT AT VALUES (2, 'b')",
+			splitStmt:   "ALTER TABLE d.t SPLIT AT VALUES (2, 'b')",
+			unsplitStmt: "ALTER TABLE d.t UNSPLIT AT VALUES (2, 'b')",
+			count:       1,
 		},
 		{
-			in: "ALTER TABLE d.t UNSPLIT AT VALUES (3, 'c'), (4, 'd')",
+			splitStmt:   "ALTER TABLE d.t SPLIT AT VALUES (3, 'c'), (4, 'd')",
+			unsplitStmt: "ALTER TABLE d.t UNSPLIT AT VALUES (3, 'c'), (4, 'd')",
+			count:       2,
 		},
 		{
-			in: "ALTER TABLE d.t UNSPLIT AT SELECT 5, 'd'",
+			splitStmt:   "ALTER TABLE d.t SPLIT AT VALUES (5, 'd')",
+			unsplitStmt: "ALTER TABLE d.t UNSPLIT AT SELECT 5, 'd'",
+			count:       1,
 		},
 		{
-			in: "ALTER TABLE d.t UNSPLIT AT SELECT * FROM (VALUES (6, 'e'), (7, 'f')) AS a",
+			splitStmt:   "ALTER TABLE d.t SPLIT AT VALUES (6, 'e'), (7, 'f')",
+			unsplitStmt: "ALTER TABLE d.t UNSPLIT AT SELECT * FROM (VALUES (6, 'e'), (7, 'f')) AS a",
+			count:       2,
 		},
 		{
-			in: "ALTER TABLE d.t UNSPLIT AT VALUES (10)",
+			splitStmt:   "ALTER TABLE d.t SPLIT AT VALUES (10)",
+			unsplitStmt: "ALTER TABLE d.t UNSPLIT AT VALUES (10)",
+			count:       1,
 		},
 		{
-			in:   "ALTER TABLE d.i UNSPLIT AT VALUES ($1)",
-			args: []interface{}{8},
+			splitStmt:   "ALTER TABLE d.i SPLIT AT VALUES (1)",
+			unsplitStmt: "ALTER TABLE d.i UNSPLIT AT VALUES ((SELECT 1))",
+			count:       1,
 		},
 		{
-			in: "ALTER TABLE d.i UNSPLIT AT VALUES ((SELECT 1))",
+			splitStmt:   "ALTER TABLE d.i SPLIT AT VALUES (8)",
+			unsplitStmt: "ALTER TABLE d.i UNSPLIT AT VALUES ($1)",
+			args:        []interface{}{8},
+			count:       1,
 		},
 		{
-			in: "ALTER INDEX d.t@s_idx UNSPLIT AT VALUES ('f')",
+			splitStmt:   "ALTER INDEX d.t@s_idx SPLIT AT VALUES ('f')",
+			unsplitStmt: "ALTER INDEX d.t@s_idx UNSPLIT AT VALUES ('f')",
+			count:       1,
 		},
 		{
-			in:    "ALTER TABLE d.t UNSPLIT AT VALUES (1, 'non-existent')",
-			error: "could not UNSPLIT AT (1, 'non-existent')",
+			splitStmt:   "ALTER TABLE d.t SPLIT AT VALUES (8, 'g'), (9, 'h'), (10, 'i')",
+			unsplitStmt: "ALTER TABLE d.t UNSPLIT ALL",
+			count:       3,
 		},
 		{
-			in:    "ALTER TABLE d.t UNSPLIT AT VALUES ('c', 3)",
-			error: "could not parse \"c\" as type int",
+			splitStmt:   "ALTER INDEX d.t@s_idx SPLIT AT VALUES ('g'), ('h'), ('i')",
+			unsplitStmt: "ALTER INDEX d.t@s_idx UNSPLIT ALL",
+			count:       3,
 		},
 		{
-			in:    "ALTER TABLE d.t UNSPLIT AT VALUES (i, s)",
-			error: `column "i" does not exist`,
+			splitStmt:   "ALTER TABLE d.i SPLIT AT VALUES (10), (11), (12)",
+			unsplitStmt: "ALTER TABLE d.i UNSPLIT ALL",
+			count:       3,
 		},
 		{
-			in:    "ALTER INDEX d.t@not_present UNSPLIT AT VALUES ('g')",
-			error: `index "not_present" does not exist`,
+			splitStmt:   "ALTER TABLE i SPLIT AT VALUES (10), (11), (12)",
+			unsplitStmt: "ALTER TABLE i UNSPLIT ALL",
+			count:       3,
 		},
 		{
-			in:    "ALTER TABLE d.i UNSPLIT AT VALUES (avg(1::float))",
-			error: "aggregate functions are not allowed in VALUES",
+			unsplitStmt: "ALTER TABLE d.t UNSPLIT AT VALUES (1, 'non-existent')",
+			error:       "could not UNSPLIT AT (1, 'non-existent')",
 		},
 		{
-			in:    "ALTER TABLE d.i UNSPLIT AT VALUES ($1)",
-			error: "no value provided for placeholder: $1",
+			unsplitStmt: "ALTER TABLE d.t UNSPLIT AT VALUES ('c', 3)",
+			error:       "could not parse \"c\" as type int",
 		},
 		{
-			in:    "ALTER TABLE d.i UNSPLIT AT VALUES ($1)",
-			args:  []interface{}{"blah"},
-			error: "error in argument for $1: strconv.ParseInt",
+			unsplitStmt: "ALTER TABLE d.t UNSPLIT AT VALUES (i, s)",
+			error:       `column "i" does not exist`,
 		},
 		{
-			in:    "ALTER TABLE d.i UNSPLIT AT VALUES ($1::string)",
-			args:  []interface{}{"1"},
-			error: "UNSPLIT AT data column 1 (k) must be of type int, not type string",
+			unsplitStmt: "ALTER INDEX d.t@not_present UNSPLIT AT VALUES ('g')",
+			error:       `index "not_present" does not exist`,
+		},
+		{
+			unsplitStmt: "ALTER TABLE d.i UNSPLIT AT VALUES (avg(1::float))",
+			error:       "aggregate functions are not allowed in VALUES",
+		},
+		{
+			unsplitStmt: "ALTER TABLE d.i UNSPLIT AT VALUES ($1)",
+			error:       "no value provided for placeholder: $1",
+		},
+		{
+			unsplitStmt: "ALTER TABLE d.i UNSPLIT AT VALUES ($1)",
+			args:        []interface{}{"blah"},
+			error:       "error in argument for $1: strconv.ParseInt",
+		},
+		{
+			unsplitStmt: "ALTER TABLE d.i UNSPLIT AT VALUES ($1::string)",
+			args:        []interface{}{"1"},
+			error:       "UNSPLIT AT data column 1 (k) must be of type int, not type string",
 		},
 	}
 
 	for _, tt := range tests {
 		var key roachpb.Key
 		var pretty string
-		err := db.QueryRow(tt.in, tt.args...).Scan(&key, &pretty)
+		var expirationTimestamp gosql.NullString
+
+		if tt.splitStmt != "" {
+			rows, err := db.Query(tt.splitStmt)
+			if err != nil {
+				t.Fatalf("%s: unexpected error setting up test: %s", tt.splitStmt, err)
+			}
+			for rows.Next() {
+				if err := rows.Scan(&key, &pretty, &expirationTimestamp); err != nil {
+					t.Fatalf("%s: unexpected error setting up test: %s", tt.splitStmt, err)
+				}
+			}
+			rows.Close()
+		}
+
+		rows, err := db.Query(tt.unsplitStmt, tt.args...)
 		if err != nil && tt.error == "" {
-			t.Fatalf("%s: unexpected error: %s", tt.in, err)
+			t.Fatalf("%s: unexpected error: %s", tt.unsplitStmt, err)
 		} else if tt.error != "" && err == nil {
-			t.Fatalf("%s: expected error: %s", tt.in, tt.error)
+			t.Fatalf("%s: expected error: %s", tt.unsplitStmt, tt.error)
 		} else if err != nil && tt.error != "" {
 			if !strings.Contains(err.Error(), tt.error) {
-				t.Fatalf("%s: unexpected error: %s", tt.in, err)
+				t.Fatalf("%s: unexpected error: %s", tt.unsplitStmt, err)
 			}
 		} else {
-			// Successful unsplit, verify it happened.
-			rng, err := s.(*server.TestServer).LookupRange(key)
-			if err != nil {
-				t.Fatal(err)
+			actualCount := 0
+			for rows.Next() {
+				actualCount++
+				err := rows.Scan(&key, &pretty)
+				if err != nil {
+					t.Fatalf("%s: expected error: %s", tt.unsplitStmt, tt.error)
+				}
+				// Successful unsplit, verify it happened.
+				rng, err := s.(*server.TestServer).LookupRange(key)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if (rng.GetStickyBit() != hlc.Timestamp{}) {
+					t.Fatalf("%s: expected range sticky bit to be hlc.MinTimestamp, got %s", tt.unsplitStmt, rng.StickyBit)
+				}
 			}
-			if (rng.GetStickyBit() != hlc.Timestamp{}) {
-				t.Fatalf("%s: expected range sticky bit to be hlc.MinTimestamp, got %s", tt.in, pretty)
+			rows.Close()
+
+			if tt.count != actualCount {
+				t.Fatalf("%s: expected %d unsplits, got %d", tt.unsplitStmt, tt.count, actualCount)
 			}
 		}
 	}
