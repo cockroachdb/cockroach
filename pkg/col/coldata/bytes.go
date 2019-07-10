a1ded56e46d51829b75a14a8f18bae8c3af9083b
Alfonso Subiotto Marqués <alfonso@cockroachlabs.com>
2019-07-10 11:18:28 -0400

    coldata: switch underlying Bytes representation to flat []byte slice
    Release note: None

--- pkg/col/coldata/bytes.go
+++ pkg/col/coldata/bytes.go
@@ -10,71 +10,324 @@
 
 package coldata
 
-// Bytes is a wrapper type for a two-dimensional byte slice.
+import (
+	"fmt"
+	"strings"
+)
+
+// Bytes is a wrapper type for a two-dimensional byte slice ([][]byte).
 type Bytes struct {
-	data [][]byte
+	// data is the slice of all bytes
+	data []byte
+	// offsets contains the offsets for each []byte slice in data. The offsets
+	// may be out of order (e.g. offsets = []int{3, 0}), which is reflected in
+	// outOfOrder. If the offsets are in order, fast paths can be used in various
+	// situations,
+	offsets []int32
+	// lengths[i] contains the length of the []byte value beginning at offsets[i].
+	lengths []int32
+
+	// outOfOrder specifies whether the offsets slice is out of order.
+	outOfOrder bool
+	// maxSetIndex specifies the last index set by the user of this struct. This
+	// enables us to set outOfOrder if an element is set prior to maxSetIndex.
+	maxSetIndex int
 }
 
-// NewBytes returns a Bytes struct with enough capacity for n elements.
+// NewBytes returns a Bytes struct with enough capacity for n zero-length
+// []byte values. It is legal to call Set on the returned Bytes at this point,
+// but Get is undefined until at least one element is Set.
 func NewBytes(n int) *Bytes {
 	return &Bytes{
-		data: make([][]byte, n),
+		// Given that the []byte slices are of variable length, we multiply the
+		// number of elements by some constant factor.
+		data:    make([]byte, 0, n*64),
+		offsets: make([]int32, n),
+		lengths: make([]int32, n),
 	}
 }
 
 // Get returns the ith []byte in Bytes.
 func (b Bytes) Get(i int) []byte {
-	return b.data[i]
+	return b.data[b.offsets[i] : b.offsets[i]+b.lengths[i]]
 }
 
 // Set sets the ith []byte in Bytes.
-func (b Bytes) Set(i int, v []byte) {
-	b.data[i] = v
+func (b *Bytes) Set(i int, v []byte) {
+	b.offsets[i] = int32(len(b.data))
+	b.lengths[i] = int32(len(v))
+	// Append to b.data since these Sets could be random and we want to minimize
+	// data movement.
+	b.data = append(b.data, v...)
+	b.outOfOrder = b.outOfOrder || i < b.maxSetIndex
+	if i > b.maxSetIndex {
+		b.maxSetIndex = i
+	}
 }
 
 // Swap swaps the ith []byte with the jth []byte and vice-versa.
-func (b Bytes) Swap(i int, j int) {
-	b.data[i], b.data[j] = b.data[j], b.data[i]
+func (b *Bytes) Swap(i int, j int) {
+	// Swaps are unsupported because we currently do not use physical swaps in the
+	// vectorized execution engine. The abstraction of having a flat byte slice
+	// support out of order sets is supported but could lead to some unintended
+	// performance consequences. The execution engine will slowly move towards not
+	// doing these either, but we currently can only disallow Swaps.
+	panic("physical flat Bytes Swaps are unsupported")
 }
 
-// Slice returns a new Bytes struct with its internal data sliced according to
-// start and end.
-func (b Bytes) Slice(start, end int) *Bytes {
-	return &Bytes{data: b.data[start:end]}
+// Slice modifies and returns the receiver Bytes struct sliced according to
+// start and end. Returning the result is not necessary but is done for
+// compatibility with the usage of other data representations in vectorized
+// execution.
+func (b *Bytes) Slice(start, end int) *Bytes {
+	if start == 0 && end == len(b.offsets) {
+		return b
+	}
+	b.offsets = b.offsets[start:end]
+	b.lengths = b.lengths[start:end]
+	b.maxSetIndex = end - start - 1
+	return b
 }
 
-// CopySlice copies srcStartIdx inclusive and srcEndIdx inclusive []byte values
-// from src into the receiver starting at destIdx.
-func (b Bytes) CopySlice(src *Bytes, destIdx, srcStartIdx, srcEndIdx int) {
-	copy(b.data[destIdx:], src.data[srcStartIdx:srcEndIdx])
+// CopySlice copies srcStartIdx inclusive and srcEndIdx exclusive []byte values
+// from src into the receiver starting at destIdx. Similar to the copy builtin,
+// min(dest.Len(), src.Len()) values will be copied. Note that due to the
+// possibility of unordered offsets, data that is overwritten might still be
+// kept around.
+func (b *Bytes) CopySlice(src *Bytes, destIdx, srcStartIdx, srcEndIdx int) {
+	if destIdx < 0 || destIdx > b.Len() {
+		panic(fmt.Sprintf("dest index %d out of range (len=%d)", destIdx, b.Len()))
+	} else if srcStartIdx < 0 || srcStartIdx >= src.Len() || srcEndIdx > src.Len() || srcStartIdx > srcEndIdx {
+		panic(
+			fmt.Sprintf("source index start %d or end %d invalid (len=%d)", srcStartIdx, srcEndIdx, src.Len()),
+		)
+	}
+	toCopy := srcEndIdx - srcStartIdx
+	if toCopy == 0 || len(b.offsets) == 0 || destIdx == len(b.offsets) {
+		return
+	}
+
+	newMaxIdx := destIdx + (toCopy - 1)
+	if newMaxIdx > b.maxSetIndex {
+		b.maxSetIndex = newMaxIdx
+	}
+	if destIdx == 0 && toCopy >= len(b.offsets) {
+		// Fast path; this fully overwrites the destination data.
+		if cap(b.data) < len(src.data) {
+			// Ensure we have the correct capacity for a full copy. Note that the
+			// capacity of b.data has no correlation with the number of values.
+			b.data = make([]byte, len(src.data))
+		}
+		b.data = b.data[:cap(b.data)]
+		copy(b.data, src.data)
+		copy(b.offsets, src.offsets[srcStartIdx:srcEndIdx])
+		copy(b.lengths, src.lengths[srcStartIdx:srcEndIdx])
+		b.outOfOrder = src.outOfOrder
+		return
+	}
+
+	if destIdx+toCopy < len(b.offsets) {
+		// There will still be elements left over after the last element to copy,
+		// therefore the destination will be out of order (since we only append).
+		b.outOfOrder = true
+	}
+
+	// TODO(asubiotto): There are several performance improvements we can do here
+	//  if !b.outOfOrder and !src.outOfOrder to use the builtin copy. The only
+	//  thing to watch out for is whether there is a need to shift elements past
+	//  the end of the last element copied. I'm wary of introducing more special
+	//  casing in the first iteration of this so leaving this for the future.
+
+	srcOffsets := src.offsets
+	srcLengths := src.lengths
+	if src == b {
+		// If we're copying from the same Bytes struct, we don't want to
+		// invalidate source offsets as we're setting the destination offsets. We
+		// have to create a copy of the source offsets and lengths.
+		srcOffsets = make([]int32, toCopy)
+		srcLengths = make([]int32, toCopy)
+		copy(srcOffsets, src.offsets[srcStartIdx:srcEndIdx])
+		copy(srcLengths, src.lengths[srcStartIdx:srcEndIdx])
+		srcStartIdx = 0
+		srcEndIdx = len(srcOffsets)
+	}
+
+	// If the copy is not a full overwrite of b, we iterate element over element.
+	// Note that since the offsets could be out of order, b.data is appended to.
+	for i, j := destIdx, srcStartIdx; i < len(b.offsets) && j < srcEndIdx; i, j = i+1, j+1 {
+		v := src.data[srcOffsets[j] : srcOffsets[j]+srcLengths[j]]
+		b.offsets[i] = int32(len(b.data))
+		b.lengths[i] = int32(len(v))
+		b.data = append(b.data, v...)
+	}
 }
 
-// AppendSlice appends srcStartIdx inclusive and srcEndIdx inclusive []byte
+// AppendSlice appends srcStartIdx inclusive and srcEndIdx exclusive []byte
 // values from src into the receiver starting at destIdx.
 func (b *Bytes) AppendSlice(src *Bytes, destIdx, srcStartIdx, srcEndIdx int) {
-	b.data = append(b.data[:destIdx], src.data[srcStartIdx:srcEndIdx]...)
+	if destIdx < 0 || destIdx > b.Len() {
+		panic(fmt.Sprintf("dest index %d out of range (len=%d)", destIdx, b.Len()))
+	} else if srcStartIdx < 0 || srcStartIdx >= src.Len() || srcEndIdx > src.Len() || srcStartIdx > srcEndIdx {
+		panic(
+			fmt.Sprintf("source index start %d or end %d invalid (len=%d)", srcStartIdx, srcEndIdx, src.Len()),
+		)
+	}
+	toAppend := srcEndIdx - srcStartIdx
+	b.maxSetIndex = destIdx + (toAppend - 1)
+	if toAppend == 0 {
+		if destIdx == len(b.offsets) {
+			return
+		}
+		// This is a truncation.
+		if !b.outOfOrder {
+			b.data = b.data[:b.offsets[destIdx]]
+		}
+		b.offsets = b.offsets[:destIdx]
+		b.lengths = b.lengths[:destIdx]
+		return
+	}
+	if destIdx == 0 {
+		// Fast path, this append is overwriting the receiver (append truncates past
+		// the end of the elements that are appended).
+		b.data = append(b.data[:0], src.data...)
+		b.offsets = append(b.offsets[:0], src.offsets[srcStartIdx:srcEndIdx]...)
+		b.lengths = append(b.lengths[:0], src.lengths[srcStartIdx:srcEndIdx]...)
+		b.outOfOrder = src.outOfOrder
+		return
+	}
+
+	if !b.outOfOrder && !src.outOfOrder {
+		destDataIdx := b.offsets[destIdx-1] + b.lengths[destIdx-1]
+		b.data = append(
+			b.data[:destDataIdx],
+			src.data[src.offsets[srcStartIdx]:src.offsets[srcEndIdx-1]+src.lengths[srcEndIdx-1]]...,
+		)
+		translateBy := int32(destDataIdx - src.offsets[srcStartIdx])
+		b.offsets = append(b.offsets[:destIdx], src.offsets[srcStartIdx:srcEndIdx]...)
+		b.lengths = append(b.lengths[:destIdx], src.lengths[srcStartIdx:srcEndIdx]...)
+		for i := destIdx; i < destIdx+toAppend; i++ {
+			b.offsets[i] += translateBy
+		}
+		return
+	}
+
+	b.outOfOrder = b.outOfOrder || src.outOfOrder
+
+	srcOffsets := src.offsets
+	srcLengths := src.lengths
+	if src == b {
+		// If we're appending from the same Bytes struct, we don't want to
+		// invalidate source offsets as we're appending to the destination offsets.
+		// Since we can't use the builtin append as above, we have to create a copy
+		// of the source offsets and lengths.
+		srcOffsets = make([]int32, toAppend)
+		srcLengths = make([]int32, toAppend)
+		copy(srcOffsets, src.offsets[srcStartIdx:srcEndIdx])
+		copy(srcLengths, src.lengths[srcStartIdx:srcEndIdx])
+		srcStartIdx = 0
+		srcEndIdx = len(srcOffsets)
+	}
+
+	b.offsets = b.offsets[:destIdx]
+	b.lengths = b.lengths[:destIdx]
+	for i := srcStartIdx; i < srcEndIdx; i++ {
+		v := src.data[srcOffsets[i] : srcOffsets[i]+srcLengths[i]]
+		b.offsets = append(b.offsets, int32(len(b.data)))
+		b.lengths = append(b.lengths, int32(len(v)))
+		b.data = append(b.data, v...)
+	}
 }
 
 // AppendVal appends the given []byte value to the end of the receiver.
 func (b *Bytes) AppendVal(v []byte) {
-	b.data = append(b.data, v)
+	b.maxSetIndex = len(b.offsets)
+	b.offsets = append(b.offsets, int32(len(b.data)))
+	b.lengths = append(b.lengths, int32(len(v)))
+	b.data = append(b.data, v...)
 }
 
 // Len returns how many []byte values the receiver contains.
 func (b Bytes) Len() int {
-	return len(b.data)
+	return len(b.offsets)
 }
 
-var zeroBytesColumn = make([][]byte, BatchSize)
+var (
+	zeroBytesSlice = make([]byte, BatchSize)
+	zeroInt32Slice = make([]int32, BatchSize)
+)
 
 // Zero zeroes out the underlying bytes.
 func (b Bytes) Zero() {
-	for n := 0; n < len(b.data); n += copy(b.data[n:], zeroBytesColumn) {
+	b.data = b.data[:0]
+	for n := 0; n < len(b.offsets); n += copy(b.offsets[n:], zeroInt32Slice) {
 	}
+	for n := 0; n < len(b.lengths); n += copy(b.lengths[n:], zeroInt32Slice) {
+	}
+	b.outOfOrder = false
+	b.maxSetIndex = 0
+}
+
+// String is used for debugging purposes.
+func (b Bytes) String() string {
+	var builder strings.Builder
+	for i := range b.offsets {
+		builder.WriteString(
+			fmt.Sprintf("%d: %v\n", i, b.data[b.offsets[i]:b.offsets[i]+b.lengths[i]]),
+		)
+	}
+	return builder.String()
 }
 
 // PrimitiveRepr is a temprorary migration tool.
 // TODO(asubiotto): Remove this.
 func (b Bytes) PrimitiveRepr() [][]byte {
-	return b.data
+	bytes := make([][]byte, b.Len())
+	for i := 0; i < b.Len(); i++ {
+		bytes[i] = b.Get(i)
+	}
+	return bytes
+}
+
+// BytesFromArrowSerializationFormat takes an Arrow byte slice and accompanying
+// offsets and populates b.
+func BytesFromArrowSerializationFormat(b *Bytes, data []byte, offsets []int32) {
+	if cap(b.lengths) < len(offsets)-1 {
+		b.lengths = b.lengths[:cap(b.lengths)]
+		b.lengths = append(b.lengths, make([]int32, len(offsets)-1)...)
+	}
+	b.lengths = b.lengths[:len(offsets)-1]
+	for i, offset := range offsets[1:] {
+		// Starting from the second index, the length for each element is the
+		// current offset minus the previous offset.
+		b.lengths[i] = offset - offsets[i]
+	}
+	// Trim the last offset, which is just the length of the data slice.
+	offsets = offsets[:len(offsets)-1]
+	b.data = data
+	b.offsets = offsets
+	b.outOfOrder = false
+	b.maxSetIndex = len(offsets) - 1
+}
+
+// ToArrowSerializationFormat returns a bytes slice and offsets that are
+// Arrow-compatible.
+func (b Bytes) ToArrowSerializationFormat() ([]byte, []int32) {
+	data := b.data
+	offsets := b.offsets
+	if b.outOfOrder {
+		// Reorder for Arrow compatibility. Not doing this would probably be a
+		// performance optimization but keeping the serialization format compatible
+		// gives us the option of improving this flat data representation in the
+		// future without needing to worry about multiversion node interactions.
+		data = make([]byte, 0, len(b.data))
+		offsets = make([]int32, 0, len(b.offsets)+1)
+		offsets = append(offsets, 0)
+		for i := range b.offsets {
+			data = append(data, b.data[b.offsets[i]:b.offsets[i]+b.lengths[i]]...)
+			offsets = append(offsets, int32(len(data)))
+		}
+	} else {
+		offsets = append(offsets, int32(len(data)))
+	}
+	return data, offsets
 }
---
+++ pkg/col/coldata/bytes_test.go
@@ -0,0 +1,338 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package coldata
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"strings"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+)
+
+type bytesMethod int
+
+const (
+	set bytesMethod = iota
+	slice
+	copySlice
+	appendSlice
+	appendVal
+)
+
+func (m bytesMethod) String() string {
+	switch m {
+	case set:
+		return "Set"
+	case slice:
+		return "Slice"
+	case copySlice:
+		return "CopySlice"
+	case appendSlice:
+		return "AppendSlice"
+	case appendVal:
+		return "AppendVal"
+	default:
+		panic(fmt.Sprintf("unknown bytes method %d", m))
+	}
+}
+
+var bytesMethods = []bytesMethod{set, slice, copySlice, appendSlice, appendVal}
+
+// applyMethodsAndVerify applies the given methods on b1 and a reference
+// [][]byte implementation and checks if the results are equal. If
+// selfReferencingSources is true, this is an indication by the caller that we
+// are testing an edge case where the source for copies/appends refers to the
+// destination. In cases where *Bytes updates itself under the hood, we also
+// update the corresponding b2Source to mirror the behavior.
+func applyMethodsAndVerify(
+	rng *rand.Rand,
+	b1, b1Source *Bytes,
+	b2, b2Source [][]byte,
+	methods []bytesMethod,
+	selfReferencingSources bool,
+) error {
+	if err := verifyEqual(b1, b2); err != nil {
+		return errors.Wrap(err, "arguments should start as equal")
+	}
+	if err := verifyEqual(b1Source, b2Source); err != nil {
+		return errors.Wrap(err, "argument sources should start as equal")
+	}
+	var debugString string
+	for _, m := range methods {
+		n := b1.Len()
+		if n != len(b2) {
+			return errors.Errorf("length mismatch between flat and reference: %d != %d", n, len(b2))
+		}
+		sourceN := b1Source.Len()
+		if sourceN != len(b2Source) {
+			return errors.Errorf("length mismatch between flat and reference sources: %d != %d", sourceN, len(b2Source))
+		}
+		debugString += m.String()
+		switch m {
+		case set:
+			i := rng.Intn(n)
+			new := make([]byte, rng.Intn(16))
+			rng.Read(new)
+			debugString += fmt.Sprintf("(%d, %v)", i, new)
+			b1.Set(i, new)
+			b2[i] = new
+		case slice:
+			start := rng.Intn(n)
+			end := rng.Intn(n + 1)
+			if start > end {
+				end = start + 1
+			} else if start == end {
+				// If start == end, do a noop Slice, otherwise the rest of the methods
+				// won't do much (and rng.Intn will panic with an n of 0).
+				start = 0
+				end = n
+			}
+			debugString += fmt.Sprintf("(%d, %d)", start, end)
+			b1 = b1.Slice(start, end)
+			b2 = b2[start:end]
+			if selfReferencingSources {
+				b2Source = b2
+			}
+		case copySlice, appendSlice:
+			// Generate a length-inclusive destIdx.
+			destIdx := rng.Intn(n + 1)
+			srcStartIdx := rng.Intn(sourceN)
+			srcEndIdx := rng.Intn(sourceN)
+			if srcStartIdx > srcEndIdx {
+				srcEndIdx = srcStartIdx + 1
+			} else if srcStartIdx == srcEndIdx {
+				// Avoid whittling down our destination slice.
+				srcStartIdx = 0
+				srcEndIdx = sourceN
+			}
+			debugString += fmt.Sprintf("(%d, %d, %d)", destIdx, srcStartIdx, srcEndIdx)
+			var numNewVals int
+			if m == copySlice {
+				b1.CopySlice(b1Source, destIdx, srcStartIdx, srcEndIdx)
+				numNewVals = copy(b2[destIdx:], b2Source[srcStartIdx:srcEndIdx])
+			} else {
+				b1.AppendSlice(b1Source, destIdx, srcStartIdx, srcEndIdx)
+				b2 = append(b2[:destIdx], b2Source[srcStartIdx:srcEndIdx]...)
+				if selfReferencingSources {
+					b2Source = b2
+				}
+				numNewVals = srcEndIdx - srcStartIdx
+			}
+			// Deep copy the copied/appended byte slices.
+			b2Slice := b2[destIdx : destIdx+numNewVals]
+			for i := range b2Slice {
+				b2Slice[i] = append([]byte(nil), b2Slice[i]...)
+			}
+		case appendVal:
+			v := make([]byte, 16)
+			rng.Read(v)
+			debugString += fmt.Sprintf("(%v)", v)
+			b1.AppendVal(v)
+			b2 = append(b2, v)
+			if selfReferencingSources {
+				b2Source = b2
+			}
+		default:
+			return errors.Errorf("unknown method name: %s", m)
+		}
+		debugString += "\n"
+		if err := verifyEqual(b1, b2); err != nil {
+			return errors.Wrap(err, fmt.Sprintf("\ndebugString:\n%sflat (ooo=%t, maxSetIdx=%d):\n%sreference:\n%s", debugString, b1.outOfOrder, b1.maxSetIndex, b1.String(), prettyByteSlice(b2)))
+		}
+	}
+	return nil
+}
+
+func verifyEqual(flat *Bytes, b [][]byte) error {
+	if flat.Len() != len(b) {
+		return errors.Errorf("mismatched lengths %d != %d", flat.Len(), len(b))
+	}
+	for i := range b {
+		if !bytes.Equal(b[i], flat.Get(i)) {
+			return errors.Errorf("mismatch at index %d", i)
+		}
+	}
+	return nil
+}
+
+func prettyByteSlice(b [][]byte) string {
+	var builder strings.Builder
+	for i := range b {
+		builder.WriteString(
+			fmt.Sprintf("%d: %v\n", i, b[i]),
+		)
+	}
+	return builder.String()
+}
+
+func TestBytesRefImpl(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	rng, _ := randutil.NewPseudoRand()
+
+	const (
+		maxNumberOfCalls = 164
+		maxLength        = 16
+	)
+
+	n := 1 + rng.Intn(maxLength)
+
+	flat := NewBytes(n)
+	reference := make([][]byte, n)
+	for i := 0; i < n; i++ {
+		v := make([]byte, rng.Intn(16))
+		rng.Read(v)
+		flat.Set(i, append([]byte(nil), v...))
+		reference[i] = append([]byte(nil), v...)
+	}
+
+	// Make a pair of sources to copy/append from. Use the destination variables
+	// with a certain probability.
+	sourceN := n
+	flatSource := flat
+	referenceSource := reference
+	selfReferencingSources := true
+	if rng.Float64() < 0 {
+		selfReferencingSources = false
+		sourceN = 1 + rng.Intn(maxLength)
+		flatSource = NewBytes(sourceN)
+		referenceSource = make([][]byte, sourceN)
+		for i := 0; i < sourceN; i++ {
+			v := make([]byte, rng.Intn(16))
+			rng.Read(v)
+			flatSource.Set(i, append([]byte(nil), v...))
+			referenceSource[i] = append([]byte(nil), v...)
+		}
+	}
+
+	if err := verifyEqual(flat, reference); err != nil {
+		t.Fatalf("not equal: %v\nflat:\n%sreference:\n%s", err, flat, prettyByteSlice(reference))
+	}
+
+	if err := verifyEqual(flatSource, referenceSource); err != nil {
+		t.Fatalf("sources not equal: %v\nflat:\n%sreference:\n%s", err, flat, prettyByteSlice(reference))
+	}
+
+	require.False(t, flat.outOfOrder, "flat was set in order")
+
+	numCalls := 1 + rng.Intn(maxNumberOfCalls)
+	methods := make([]bytesMethod, 0, numCalls)
+	for i := 0; i < numCalls; i++ {
+		methods = append(methods, bytesMethods[rng.Intn(len(bytesMethods))])
+	}
+	if err := applyMethodsAndVerify(rng, flat, flatSource, reference, referenceSource, methods, selfReferencingSources); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestBytes(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	t.Run("Simple", func(t *testing.T) {
+		b1 := NewBytes(0)
+		b1.AppendVal([]byte("hello"))
+		require.Equal(t, "hello", string(b1.Get(0)))
+		b1.AppendVal(nil)
+		require.Equal(t, []byte{}, b1.Get(1))
+		require.Equal(t, 2, b1.Len())
+		// Verify that we can overwrite a value.
+		b1.Set(1, []byte("overwrite"))
+		require.Equal(t, "overwrite", string(b1.Get(1)))
+		// "hello" should still be there.
+		require.Equal(t, "hello", string(b1.Get(0)))
+
+		// Verify that if we zero the Bytes, we get back a zero bytes slice.
+		b1.Zero()
+		require.Equal(t, []byte{}, b1.Get(1))
+		require.Panics(t, func() { b1.Get(2) }, "expected index out of bounds")
+	})
+
+	t.Run("Append", func(t *testing.T) {
+		b1 := NewBytes(0)
+		b2 := NewBytes(0)
+		b2.AppendVal([]byte("source bytes value"))
+		b1.AppendVal([]byte("one"))
+		b1.AppendVal([]byte("two"))
+		// Truncate b1.
+		require.Equal(t, 2, b1.Len())
+		b1.AppendSlice(b2, 0, 0, 0)
+		require.Equal(t, 0, b1.Len())
+
+		b1.AppendVal([]byte("hello again"))
+
+		// Try appending b2 3 times. The first time will overwrite the current
+		// present value in b1.
+		for i := 0; i < 3; i++ {
+			b1.AppendSlice(b2, i, 0, b2.Len())
+			require.Equal(t, i+1, b1.Len())
+			for j := 0; j <= i; j++ {
+				require.Equal(t, "source bytes value", string(b1.Get(j)))
+			}
+		}
+
+		b2 = NewBytes(0)
+		b2.AppendVal([]byte("hello again"))
+		b2.AppendVal([]byte("hello again"))
+		b2.AppendVal([]byte("hello again"))
+		// Try to append only a subset of the source keeping the first element of
+		// b1 intact.
+		b1.AppendSlice(b2, 1, 1, 2)
+		require.Equal(t, 2, b1.Len())
+		require.Equal(t, "source bytes value", string(b1.Get(0)))
+		require.Equal(t, "hello again", string(b1.Get(1)))
+	})
+
+	t.Run("Copy", func(t *testing.T) {
+		b1 := NewBytes(0)
+		b2 := NewBytes(0)
+		b1.AppendVal([]byte("one"))
+		b1.AppendVal([]byte("two"))
+		b1.AppendVal([]byte("three"))
+
+		b2.AppendVal([]byte("source one"))
+		b2.AppendVal([]byte("source two"))
+
+		// Copy "source two" into "two"'s position. This also tests that elements
+		// following the copied element are correctly shifted.
+		b1.CopySlice(b2, 1, 1, 2)
+		require.Equal(t, 3, b1.Len())
+		require.Equal(t, "one", string(b1.Get(0)))
+		require.Equal(t, "source two", string(b1.Get(1)))
+		require.Equal(t, "three", string(b1.Get(2)))
+
+		// Copy will only copy as many elements as there is capacity for. In this
+		// call, the copy starts at index 2, so there is only capacity for one
+		// element.
+		b1.CopySlice(b2, 2, 0, b2.Len())
+		require.Equal(t, "one", string(b1.Get(0)))
+		require.Equal(t, "source two", string(b1.Get(1)))
+		require.Equal(t, "source one", string(b1.Get(2)))
+
+		// Slice b1 to test slicing logic and follow it with testing a full
+		// overwrite of only one element.
+		b1 = b1.Slice(0, 1)
+		require.Equal(t, 1, b1.Len())
+		b1.CopySlice(b2, 0, 0, b2.Len())
+		require.Equal(t, 1, b1.Len())
+		require.Equal(t, "source one", string(b1.Get(0)))
+
+		// Verify a full overwrite with a non-zero source start index.
+		b1.CopySlice(b2, 0, 1, b2.Len())
+		require.Equal(t, 1, b1.Len())
+		require.Equal(t, "source two", string(b1.Get(0)))
+	})
+}
