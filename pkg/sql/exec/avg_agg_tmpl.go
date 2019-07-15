cbd191e39ef81d68899b338ab958bef9c8ec4af0
Rafi Shamim <rafi@cockroachlabs.com>
2019-07-15 11:09:57 -0400

    exec: handle empty table for aggregate operators
    If the table is empty, the COUNT aggregates should return 0, and all the
    other aggregates should return NULL.
    
    Release note: None

--- pkg/sql/exec/avg_agg_tmpl.go
+++ pkg/sql/exec/avg_agg_tmpl.go
@@ -124,19 +124,21 @@ func (a *avg_TYPEAgg) Compute(b coldata.Batch, inputIdxs []uint32) {
 	}
 	inputLen := b.Length()
 	if inputLen == 0 {
-
 		// The aggregation is finished. Flush the last value. If we haven't found
-		// any non-nulls for this group so far, the output for this group should
-		// be null. If a.scratch.curIdx is negative, it means the input has zero rows, and
-		// there should be no output at all.
+		// any non-nulls for this group so far, the output for this group should be
+		// null. If a.scratch.curIdx is negative, it means the input has zero rows,
+		// and the output should be NULL.
 		if a.scratch.curIdx >= 0 {
 			if !a.scratch.foundNonNullForCurrentGroup {
 				a.scratch.nulls.SetNull(uint16(a.scratch.curIdx))
 			} else {
 				_ASSIGN_DIV_INT64("a.scratch.vec[a.scratch.curIdx]", "a.scratch.curSum", "a.scratch.curCount")
 			}
+			a.scratch.curIdx++
+		} else {
+			a.scratch.nulls.SetNull(0)
+			a.scratch.curIdx = 1
 		}
-		a.scratch.curIdx++
 		a.done = true
 		return
 	}
