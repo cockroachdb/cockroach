2a0ee2f78d4a982c7833444e2d9362facb69f56c
Rafi Shamim <rafi@cockroachlabs.com>
2019-07-17 16:50:32 -0400

    exec: overflow handling for vectorized arithmetic
    The overflow checks are done as part of the code generation in
    overloads.go. The checks are done inline, rather than calling the
    functions in the arith package for performance reasons.
    
    The checks are only done for integer math. float math is already
    well-defined since overflow will result in +Inf and -Inf as necessary.
    
    The operations that these checks are relevant for are the SUM_INT
    aggregator and projection. In the future, AVG will also benefit from
    these overflow checks.
    
    This changes the error message produced by overflows in the
    non-vectorized SUM_INT aggregator so that the messages are consistent.
    This should be fine in terms of postgres-compatibility since SUM_INT is
    unique to CRDB and eventually we will get rid of it anyway.
    
    resolves #38775
    
    Release note: None

---
+++ pkg/sql/exec/execgen/cmd/execgen/overloads_test_utils_gen.go
@@ -0,0 +1,65 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package main
+
+import (
+	"io"
+	"text/template"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/exec/types"
+)
+
+const overloadsTestUtilsTemplate = `
+package exec
+
+import (
+  "math"
+
+	"github.com/cockroachdb/apd"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+)
+
+{{define "opName"}}perform{{.Name}}{{.LTyp}}{{end}}
+
+{{/* The outer range is a types.T, and the inner is the overloads associated
+     with that type. */}}
+{{range .}}
+{{range .}}
+
+func {{template "opName" .}}(a, b {{.LTyp.GoTypeName}}) {{.RetTyp.GoTypeName}} {
+	{{(.Assign "a" "a" "b")}}
+	return a
+}
+
+{{end}}
+{{end}}
+`
+
+// genOverloadsTestUtils creates a file that has a function for each overload
+// defined in overloads.go. This is so that we can more easily test each
+// overload.
+func genOverloadsTestUtils(wr io.Writer) error {
+	tmpl, err := template.New("overloads_test_utils").Parse(overloadsTestUtilsTemplate)
+	if err != nil {
+		return err
+	}
+
+	typToOverloads := make(map[types.T][]*overload)
+	for _, overload := range binaryOpOverloads {
+		typ := overload.LTyp
+		typToOverloads[typ] = append(typToOverloads[typ], overload)
+	}
+	return tmpl.Execute(wr, typToOverloads)
+}
+
+func init() {
+	registerGenerator(genOverloadsTestUtils, "overloads_test_utils.eg.go")
+}
