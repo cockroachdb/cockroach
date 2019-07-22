ef8278902d5477d5846e2f29f41686105d4be118
Matt Jibson <matt.jibson@gmail.com>
2019-07-22 17:03:58 -0400

    sql: add partial support for pgwire row count limits
    Previously we supported row count limits as long as the limit was higher
    than the number of returned rows. Here we add a feature that supports
    this part of the spec in a partial way. This should unblock simple JDBC
    usage of this feature.
    
    Supported use cases:
    - implicit transactions (which auto close the portal after suspension)
    - explicit transactions executed to completion
    
    Unsupported use cases (with explicit transactions):
    - interleaved execution
    - explicitly closing a portal after partial execution
    
    Many options were evaluated during implementation. The one here is based
    on work where the pgwire package itself takes over the state machine
    processing during AddRow and looks for further ExecPortal messages. This
    has a number of problems: there are now two state machines, we can only
    support a part of the spec. However it also has a number of benefits
    like it is a simple implementation that is easy to understand.
    
    Two other solutions were evaluated.
    
    First, teaching distsql how to pause and resume execution (a
    proof-of-concept branch for this was produced). I did not pursue this
    route because of my own unfamiliarity with distsql, and I thought that
    attempting to reach a high level of confidence that all of the new pause,
    resume, and error logic flows were correct would be very difficult
    (I may be wrong about this). Also, this approach needed to address how
    to handle post-distsql execution cleanup and stats gathering. That is,
    after the call into distsql was paused and returned control back to
    the sql executor, there's a lot of code that gets run to cleanup stuff
    and report stats in various places, including some defers. These would
    need to be audited and only execute once per statement, not once per
    portal execution.
    
    Second, start distsql execution in a new go routine and send exec portal
    requests to it over a channel. This would avoid teaching distsql how
    to pause and resume itself, but instead move that complexity into the
    channel and go routine handling, another area ripe for race conditions
    and deadlocks. This also needed to deal with the post-distsql execution
    cleanup and defers handling discussed above.
    
    For now, we decided that this limited implementation is good enough for
    what we need today. In order to one day either support the full spec
    or pay down some of our technical debt, we can probably do a number of
    preliminary refactors that will make invoking much of the distsql path
    multpile times easier.
    
    pgx got a version bump to get support for PortalSuspended. The current
    pgx version had a few new dependencies too.
    
    See #4035
    
    Release note (sql change): add partial support for row limits during
    portal execution in pgwire.

--- pkg/sql/distsql_running.go
+++ pkg/sql/distsql_running.go
@@ -30,6 +30,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
+	"github.com/cockroachdb/cockroach/pkg/util/errorutil/unimplemented"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/mon"
@@ -683,22 +684,44 @@ func (r *DistSQLReceiver) Push(
 	r.tracing.TraceExecRowsResult(r.ctx, r.row)
 	// Note that AddRow accounts for the memory used by the Datums.
 	if commErr := r.resultWriter.AddRow(r.ctx, r.row); commErr != nil {
-		r.commErr = commErr
-		// Set the error on the resultWriter too, for the convenience of some of the
-		// clients. If clients don't care to differentiate between communication
-		// errors and query execution errors, they can simply inspect
-		// resultWriter.Err(). Also, this function itself doesn't care about the
-		// distinction and just uses resultWriter.Err() to see if we're still
-		// accepting results.
-		r.resultWriter.SetError(commErr)
+		// LimitedResultDone is not a real error, it is a signal to
+		// stop distsql and return success to the client.
+		if !errors.Is(commErr, LimitedResultDone) {
+			// Set the error on the resultWriter too, for the convenience of some of the
+			// clients. If clients don't care to differentiate between communication
+			// errors and query execution errors, they can simply inspect
+			// resultWriter.Err(). Also, this function itself doesn't care about the
+			// distinction and just uses resultWriter.Err() to see if we're still
+			// accepting results.
+			r.resultWriter.SetError(commErr)
+
+			// We don't need to shut down the connection
+			// if there's a portal-related error. This is
+			// definitely a layering violation, but is part
+			// of some accepted technical debt (see comments on
+			// sql/pgwire.limitedCommandResult.moreResultsNeeded). Instead
+			// of changing the signature of AddRow, we have a sentinel
+			// error that is handled specially here.
+			if !errors.Is(commErr, LimitedResultErr) {
+				r.commErr = commErr
+			}
+		}
 		// TODO(andrei): We should drain here. Metadata from this query would be
 		// useful, particularly as it was likely a large query (since AddRow()
 		// above failed, presumably with an out-of-memory error).
 		r.status = distsqlrun.ConsumerClosed
-		return r.status
 	}
 	return r.status
 }
+
+var (
+	// LimitedResultErr is an error produced by pgwire indicating an unsupported
+	// feature of row count limits was attempted.
+	LimitedResultErr = unimplemented.NewWithIssue(4035, "execute row count limits only partially supported")
+	// LimitedResultDone is a sentinel error produced by pgwire indicating
+	// the portal should be closed without error.
+	LimitedResultDone = errors.New("row count limit closed")
+)
 
 // ProducerDone is part of the RowReceiver interface.
 func (r *DistSQLReceiver) ProducerDone() {
