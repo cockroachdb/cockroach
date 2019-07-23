10b6fa589735a85a50530ba6c33f4783352451c7
Rohan Yadav <rohany@alumni.cmu.edu>
2019-07-23 17:32:21 -0400

    sql: Add locality information to SHOW EXPERIMENTAL_RANGES
    This commit also adds the rowsort directive to some
    logic tests that relied on the output order of
    SHOW EXPERIMENTAL_RANGES command.
    
    Release note: None

---
+++ pkg/sql/show_ranges_test.go
@@ -0,0 +1,52 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package sql_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+)
+
+func TestShowRangesWithLocality(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	const numNodes = 4
+	ctx := context.Background()
+	tcArgs := base.TestClusterArgs{ServerArgs: base.TestServerArgs{}}
+
+	tc := testcluster.StartTestCluster(t, numNodes, tcArgs)
+	defer tc.Stopper().Stop(ctx)
+
+	sqlDB := sqlutils.MakeSQLRunner(tc.Conns[0])
+	sqlDB.Exec(t, `CREATE TABLE t (x INT PRIMARY KEY)`)
+	for i := 0; i < 20; i++ {
+		sqlDB.Exec(t, fmt.Sprintf(`ALTER TABLE t SPLIT AT VALUES (%d)`, i))
+	}
+
+	const nodeColIdx = 4
+	const localityColIdx = 5
+
+	result := sqlDB.QueryStr(t, `SHOW EXPERIMENTAL_RANGES FROM TABLE t`)
+	for _, row := range result {
+		// Because StartTestCluster changes the locality no matter what the
+		// arguments are, we expect whatever the test server sets up.
+		locality := fmt.Sprintf("region=test,dc=dc%s", row[nodeColIdx])
+		if row[localityColIdx] != locality {
+			t.Fatalf("expected %s found %s", locality, row[localityColIdx])
+		}
+	}
+}
