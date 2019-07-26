ea6c797aca7cf7f6914a7b9d155a4389e425a29a
Radu Berinde <radu@cockroachlabs.com>
2019-07-26 09:49:29 -0400

    opt: add IndexOrdinal alias
    Adding a `cat.IndexOrdinal` alias to make it more explicit when a
    value is an index ordinal. It is only an alias because a separate type
    would make things like loops more annoying.
    
    Release note: None

--- pkg/sql/opt_catalog.go
+++ pkg/sql/opt_catalog.go
@@ -720,7 +720,7 @@ func (ot *optTable) DeletableIndexCount() int {
 }
 
 // Index is part of the cat.Table interface.
-func (ot *optTable) Index(i int) cat.Index {
+func (ot *optTable) Index(i cat.IndexOrdinal) cat.Index {
 	return &ot.indexes[i]
 }
 
