ad765c8c404cf677c88c83bc8dde89a5eae2f4c1
Matt Jibson <matt.jibson@gmail.com>
2019-07-29 03:32:00 -0400

    testutils/lint: convert some checkers to go/analysis
    Convert checkers to go/analysis
    (https://godoc.org/golang.org/x/tools/go/analysis). This is the new,
    official static analysis framework for Go. These were previously
    disabled.
    
    Make a new roachlint program to do the actual checking. I am not able to
    find an easy way to run the checkers from tests, leading me to believe
    that they are not supposed to be run from tests. Thus, invoke roachlint
    like our other linters. Currently it is only hooked up to the timer test,
    since the others are failing.
    
    See #33669. Although that mentions using honnef.co/go/tools, that tool
    now uses go/analysis: https://staticcheck.io/changes/2019.2#go-analysis.
    
    Release note: None

---
+++ pkg/testutils/lint/passes/float/float_test.go
@@ -0,0 +1,23 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package float_test
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/testutils/lint/passes/float"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+func Test(t *testing.T) {
+	testdata := analysistest.TestData()
+	analysistest.Run(t, testdata, float.Analyzer, "a")
+}
