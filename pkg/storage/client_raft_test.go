1aed677736bb11c1abf8ff297c38f75be66409f0
Andrew Werner <ajwerner@cockroachlabs.com>
2019-07-30 08:23:07 -0400

    storage: add a unit test demonstrating the use of separate connections
    This adds a unit test that exercises the change to the raftTransport to
    use a separate connection for system traffic from regular traffic.
    
    Release note: None

--- pkg/storage/client_raft_test.go
+++ pkg/storage/client_raft_test.go
@@ -50,6 +50,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.etcd.io/etcd/raft"
 	"go.etcd.io/etcd/raft/raftpb"
+	"google.golang.org/grpc"
 )
 
 // mustGetInt decodes an int64 value from the bytes field of the receiver
@@ -4445,4 +4446,109 @@ func TestTracingDoesNotRaceWithCancelation(t *testing.T) {
 		go put(i)()
 	}
 	wg.Wait()
+}
+
+type disablingClientStream struct {
+	grpc.ClientStream
+	disabled *atomic.Value
+}
+
+func (cs *disablingClientStream) SendMsg(m interface{}) error {
+	if cs.disabled.Load().(bool) {
+		return nil
+	}
+	return cs.ClientStream.SendMsg(m)
+}
+
+// TestDefaultConnectionDisruptionDoesNotInterfereWithSystemTraffic tests that
+// disconnection on connections of the rpc.DefaultClass do not interfere with
+// traffic on the SystemClass connection.
+func TestDefaultConnectionDisruptionDoesNotInterfereWithSystemTraffic(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	clock := hlc.NewClock(hlc.UnixNano, time.Nanosecond)
+	stopper := stop.NewStopper()
+	ctx := context.Background()
+	defer stopper.Stop(ctx)
+	// disabled controls whether to disrupt DefaultClass streams.
+	var disabled atomic.Value
+	disabled.Store(false)
+	knobs := rpc.ContextTestingKnobs{
+		StreamClientInterceptor: func(target string, class rpc.ConnectionClass) grpc.StreamClientInterceptor {
+			if class == rpc.SystemClass {
+				return nil
+			}
+			return func(
+				ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn,
+				method string, streamer grpc.Streamer, opts ...grpc.CallOption,
+			) (grpc.ClientStream, error) {
+				cs, err := streamer(ctx, desc, cc, method, opts...)
+				if err != nil {
+					return nil, err
+				}
+				return &disablingClientStream{
+					disabled:     &disabled,
+					ClientStream: cs,
+				}, nil
+			}
+		},
+	}
+	// Prevent the split queue from creating additional ranges while we're
+	// waiting for replication.
+	sc := storage.TestStoreConfig(nil)
+	mtc := &multiTestContext{
+		storeConfig:     &sc,
+		clock:           clock,
+		rpcTestingKnobs: knobs,
+	}
+	const numReplicas = 3
+	mtc.Start(t, numReplicas)
+	defer mtc.Stop()
+	for _, s := range mtc.stores {
+		s.SetReplicateQueueActive(true)
+	}
+
+	mtc.replicateRange(1, 1, 2)
+	// Make a key that's in the user data space.
+	keyA := append(keys.MakeTablePrefix(100), 'a')
+	replica1 := mtc.stores[0].LookupReplica(roachpb.RKey(keyA))
+	mtc.replicateRange(replica1.RangeID, 1, 2)
+	// Put some data in the range so we'll have something to test for.
+	putReq := &roachpb.PutRequest{}
+	putReq.RequestHeader.Key = keyA
+	putReq.Value.SetInt(1)
+	_, pErr := client.SendWrapped(context.Background(), mtc.stores[0].TestSender(), putReq)
+	require.Nil(t, pErr)
+
+	// Wait for all nodes to catch up.
+	mtc.waitForValues(keyA, []int64{1, 1, 1})
+	disabled.Store(true)
+	repl1, err := mtc.stores[0].GetReplica(1)
+	require.Nil(t, err)
+	// Transfer the lease on range 1
+	lease, _ := repl1.GetLease()
+	var target int
+	for i := roachpb.StoreID(1); i <= numReplicas; i++ {
+		if lease.Replica.StoreID != i {
+			target = int(i - 1)
+			break
+		}
+	}
+	mtc.transferLease(ctx, 1, int(lease.Replica.StoreID-1), target)
+	// Set a relatively short timeout so that this test doesn't take too long.
+	// We should always hit it.
+	withTimeout, cancel := context.WithTimeout(ctx, 20*time.Millisecond)
+	defer cancel()
+	putReq.Value.SetInt(2)
+	_, pErr = client.SendWrapped(withTimeout, mtc.stores[0].TestSender(), putReq)
+	require.NotNil(t, pErr, "expected an error when sending to a disconnected store")
+	// Transfer the lease back to demonstrate that the system range is still live.
+	mtc.transferLease(ctx, 1, target, int(lease.Replica.StoreID-1))
+	// Heal the partition, the previous proposal may now succeed but it may have
+	// have been canceled.
+	disabled.Store(false)
+	// Overwrite with a new value and ensure that it propagates.
+	putReq.Value.SetInt(3)
+	_, pErr = client.SendWrapped(ctx, mtc.stores[0].TestSender(), putReq)
+	require.Nil(t, pErr, "expected to succeed after healing the partition")
+	mtc.waitForValues(keyA, []int64{3, 3, 3})
 }
