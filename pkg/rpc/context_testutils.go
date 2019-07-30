dc9856219e36f3a6563ac65e0f00ab0b7a7f0db3
Andrew Werner <ajwerner@cockroachlabs.com>
2019-07-30 01:00:50 -0400

    rpc: add testing knobs to inject inteceptors
    Testing multiple connections requires being able to interfere with connections.
    Initial testing code used a TCP proxy to disrupt underlying communication.
    That approach was deemed to heavyweight and not particularly extensible.
    This approach allows tests to exercise a wide degree of control over rpc
    communication.
    
    Release note: None

--- pkg/rpc/context_testutils.go
+++ pkg/rpc/context_testutils.go
@@ -11,8 +11,6 @@
 package rpc
 
 import (
-	"context"
-
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
@@ -20,7 +18,30 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
 	"github.com/cockroachdb/cockroach/pkg/util/tracing"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
+	"google.golang.org/grpc"
 )
+
+// ContextTestingKnobs provides hooks to aid in testing the system. The testing
+// knob functions are called at various points in the Context life cycle if they
+// are non-nil.
+type ContextTestingKnobs struct {
+
+	// UnaryClientInterceptor if non-nil will be called at dial time to provide
+	// the base unary interceptor for client connections.
+	// This function may return a nil interceptor to avoid injecting behavior
+	// for a given target and class.
+	UnaryClientInterceptor func(target string, class ConnectionClass) grpc.UnaryClientInterceptor
+
+	// StreamClient if non-nil will be called at dial time to provide
+	// the base stream interceptor for client connections.
+	// This function may return a nil interceptor to avoid injecting behavior
+	// for a given target and class.
+	StreamClientInterceptor func(target string, class ConnectionClass) grpc.StreamClientInterceptor
+
+	// ClusterID initializes the Context's ClusterID container to this value if
+	// non-nil at construction time.
+	ClusterID *uuid.UUID
+}
 
 // NewInsecureTestingContext creates an insecure rpc Context suitable for tests.
 func NewInsecureTestingContext(clock *hlc.Clock, stopper *stop.Stopper) *Context {
@@ -33,16 +54,22 @@ func NewInsecureTestingContext(clock *hlc.Clock, stopper *stop.Stopper) *Context
 func NewInsecureTestingContextWithClusterID(
 	clock *hlc.Clock, stopper *stop.Stopper, clusterID uuid.UUID,
 ) *Context {
-	ctx := NewContext(
+	return NewInsecureTestingContextWithKnobs(clock, stopper, ContextTestingKnobs{
+		ClusterID: &clusterID,
+	})
+}
+
+// NewInsecureTestingContextWithKnobs creates an insecure rpc Context
+// suitable for tests configured with the provided knobs.
+func NewInsecureTestingContextWithKnobs(
+	clock *hlc.Clock, stopper *stop.Stopper, knobs ContextTestingKnobs,
+) *Context {
+	return NewContextWithTestingKnobs(
 		log.AmbientContext{Tracer: tracing.NewTracer()},
 		&base.Config{Insecure: true},
 		clock,
 		stopper,
 		&cluster.MakeTestingClusterSettings().Version,
+		knobs,
 	)
-	// Ensure that tests using this test context and restart/shut down
-	// their servers do not inadvertently start talking to servers from
-	// unrelated concurrent tests.
-	ctx.ClusterID.Set(context.TODO(), clusterID)
-	return ctx
 }
