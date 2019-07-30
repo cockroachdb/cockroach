cockroachdb/cockroach

commit 888813cd2bdb24fbee802995e9f970dd30f725ca

cli: Add locality information to cockroach node status command.
Addressing #38438.

Release note (cli change): Add locality information to `cockroach
node status` command.

pkg/cmd/roachtest/decommission.go
@@ -331,7 +331,7 @@ func runDecommissionAcceptance(ctx context.Context, t *test, c *cluster) {
 			"Please verify cluster health before removing the nodes.",
 	}
 	statusHeader := []string{
-		"id", "address", "build", "started_at", "updated_at", "is_available", "is_live",
+		"id", "address", "build", "started_at", "updated_at", "locality", "is_available", "is_live",
 	}
 	waitLiveDeprecated := "--wait=live is deprecated and is treated as --wait=all"
 
@@ -385,10 +385,10 @@ func runDecommissionAcceptance(ctx context.Context, t *test, c *cluster) {
 		}
 		exp := [][]string{
 			statusHeader,
-			{`1`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`2`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`3`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`4`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			{`1`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			{`2`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			{`3`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			{`4`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
 		}
 		if err := matchCSV(o, exp); err != nil {
 			t.Fatal(err)
@@ -579,9 +579,9 @@ func runDecommissionAcceptance(ctx context.Context, t *test, c *cluster) {
 
 		exp := [][]string{
 			statusHeader,
-			{`2`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`3`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`4`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			{`2`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			{`3`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			{`4`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
 		}
 		if err := matchCSV(o, exp); err != nil {
 			time.Sleep(time.Second)
@@ -608,10 +608,10 @@ func runDecommissionAcceptance(ctx context.Context, t *test, c *cluster) {
 
 		exp := [][]string{
 			statusHeader,
-			{`2`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`3`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`4`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`5`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			{`2`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			{`3`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			{`4`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			{`5`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
 		}
 		return matchCSV(o, exp)
 	}); err != nil {

pkg/cli/node.go
@@ -77,6 +77,7 @@ var baseNodeColumnHeaders = []string{
 	"build",
 	"started_at",
 	"updated_at",
+	"locality",
 	"is_available",
 	"is_live",
 }
@@ -149,12 +150,13 @@ func runStatusNodeInner(showDecommissioned bool, args []string) ([]string, [][]s
             address,
             build_tag AS build,
             started_at,
-            updated_at,
+			updated_at,
+			locality,
             CASE WHEN split_part(expiration,',',1)::decimal > now()::decimal
                  THEN true
                  ELSE false
                  END AS is_available,
-            ifnull(is_live, false)
+			ifnull(is_live, false)
      FROM crdb_internal.gossip_liveness LEFT JOIN crdb_internal.gossip_nodes USING (node_id)`,
 	)
 
@@ -250,7 +252,7 @@ func getStatusNodeHeaders() []string {
 }
 
 func getStatusNodeAlignment() string {
-	align := "rllll"
+	align := "rlllll"
 	if nodeCtx.statusShowAll || nodeCtx.statusShowRanges {
 		align += "rrrrrr"
 	}

commit 90c57556d990bf4f19da3c628591e5086a136ae6

roachtest: Fix backwards incompatible test failure.
Fixed failure caused by commit 888813c.

Release note: None

pkg/cmd/roachtest/decommission.go
@@ -282,6 +282,22 @@ func runDecommissionAcceptance(ctx context.Context, t *test, c *cluster) {
 		return execCLI(ctx, t, c, runNode, args...)
 	}
 
+	getCsvNumCols := func(csvStr string) (cols int, err error) {
+		defer func() {
+			if err != nil {
+				err = errors.Errorf("error reading csv input: \n %v\n", csvStr, err)
+			}
+		}()
+
+		reader := csv.NewReader(strings.NewReader(csvStr))
+		reader.FieldsPerRecord = -1
+		records, err := reader.Read()
+		if err != nil {
+			return 0, err
+		}
+		return len(records), nil
+	}
+
 	matchCSV := func(csvStr string, matchColRow [][]string) (err error) {
 		defer func() {
 			if err != nil {
@@ -333,6 +349,10 @@ func runDecommissionAcceptance(ctx context.Context, t *test, c *cluster) {
 	statusHeader := []string{
 		"id", "address", "build", "started_at", "updated_at", "locality", "is_available", "is_live",
 	}
+	statusHeaderNoLocality := []string{
+		"id", "address", "build", "started_at", "updated_at", "is_available", "is_live",
+	}
+
 	waitLiveDeprecated := "--wait=live is deprecated and is treated as --wait=all"
 
 	t.l.Printf("decommissioning first node from the second, polling the status manually\n")
@@ -383,12 +403,29 @@ func runDecommissionAcceptance(ctx context.Context, t *test, c *cluster) {
 		if err != nil {
 			t.Fatalf("node-status failed: %v", err)
 		}
-		exp := [][]string{
-			statusHeader,
-			{`1`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`2`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`3`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`4`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+		var exp [][]string
+		numCols, err := getCsvNumCols(o)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// Different output here to be backwards compatible with earlier
+		// versions of cockroach.
+		if numCols == 7 {
+			exp = [][]string{
+				statusHeaderNoLocality,
+				{`1`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`2`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`3`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`4`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			}
+		} else {
+			exp = [][]string{
+				statusHeader,
+				{`1`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`2`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`3`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`4`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			}
 		}
 		if err := matchCSV(o, exp); err != nil {
 			t.Fatal(err)
@@ -576,12 +613,27 @@ func runDecommissionAcceptance(ctx context.Context, t *test, c *cluster) {
 		if err != nil {
 			t.Fatalf("node-status failed: %v", err)
 		}
-
-		exp := [][]string{
-			statusHeader,
-			{`2`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`3`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`4`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+		var exp [][]string
+		numCols, err := getCsvNumCols(o)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// Different output here to be backwards compatible with earlier
+		// versions of cockroach.
+		if numCols == 7 {
+			exp = [][]string{
+				statusHeaderNoLocality,
+				{`2`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`3`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`4`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			}
+		} else {
+			exp = [][]string{
+				statusHeader,
+				{`2`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`3`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`4`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			}
 		}
 		if err := matchCSV(o, exp); err != nil {
 			time.Sleep(time.Second)
@@ -605,13 +657,29 @@ func runDecommissionAcceptance(ctx context.Context, t *test, c *cluster) {
 		if err != nil {
 			t.Fatalf("node-status failed: %v", err)
 		}
-
-		exp := [][]string{
-			statusHeader,
-			{`2`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`3`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`4`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
-			{`5`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+		var exp [][]string
+		numCols, err := getCsvNumCols(o)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// Different output here to be backwards compatible with earlier
+		// versions of cockroach.
+		if numCols == 7 {
+			exp = [][]string{
+				statusHeaderNoLocality,
+				{`2`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`3`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`4`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`5`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			}
+		} else {
+			exp = [][]string{
+				statusHeader,
+				{`2`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`3`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`4`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+				{`5`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`, `.*`},
+			}
 		}
 		return matchCSV(o, exp)
 	}); err != nil {