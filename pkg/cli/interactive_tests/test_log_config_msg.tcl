b8cc29cbc99723ec862ade7b686d4c3fb0e5c705
Raphael 'kena' Poss <knz@cockroachlabs.com>
2019-08-03 23:25:28 +0200

    cli/start: write the URL file as soon as the server is ready
    Prior to this patch, `cockroach start` would wait until the cluster
    was initialized to write the SQL URL to the file indicated by
    `--listening-url-file`. This contrasts with the PID file which is
    written as soon as the server starts listening.
    
    This prior behavior was motivated by the original design of the
    startup routine, where the SQL server was not ready/started until the
    cluster was ready.
    
    Since then, #21682 was merged which accepts, but blocks, SQL clients
    until the cluster is initialized. Since the SQL clients can now
    connect as soon as the server starts listening, there is no reason
    any more to wait longer.
    
    This patch thus simplifies the code to emit the URL to the file at the
    same time as the PID. This also makes it possible to use a SQL
    client/command reliably as soon as the process detaches from the
    terminal with `--background`.
    
    Note: if you want to test the new behavior manually in an interactive
    terminal, beware that the `cockroach` CLI client commands separately
    have a connection timeout, which defaults to 5 seconds. This can be
    overridden with the env var COCKROACH_CONNECT_TIMEOUT.
    
    Release note (cli change): `cockroach start` now writes the client URL
    to the file specified via `--listen-url-file` as soon as the server
    is ready to accept connections. This also happens before the server
    detaches from the terminal when `--background` is specified.

--- pkg/cli/interactive_tests/test_log_config_msg.tcl
+++ pkg/cli/interactive_tests/test_log_config_msg.tcl
@@ -14,7 +14,9 @@ end_test
 
 
 # Make a server with a tiny log buffer so as to force frequent log rotation.
-system "mkfifo url_fifo || true; $argv start-single-node --insecure --pid-file=server_pid --listening-url-file=url_fifo --background -s=path=logs/db --log-file-max-size=2k >>logs/expect-cmd.log 2>&1 & cat url_fifo > server_url"
+system "$argv start-single-node --insecure --pid-file=server_pid --background -s=path=logs/db --log-file-max-size=2k >>logs/expect-cmd.log 2>&1;
+        $argv sql --insecure -e 'select 1'"
+# Stop the server, which also flushes and closes the log files.
 stop_server $argv
 
 start_test "Check that the cluster ID is reported at the start of new log files."
