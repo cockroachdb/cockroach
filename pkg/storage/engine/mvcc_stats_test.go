cockroachdb/cockroach

commit cc5013d4d9ba52127c3cd369597670bf940e0122

engine: track MVCC stats in MVCCClearTimeRange
I originally thought I could leave this to a follow-up change, and just recalculate via iteration for now.
However the batch does not reflect the ClearRanges we add so that iteration-based calculation doesn't work.
Instead, this switches to incrementally calculate stats changes using the method usually used when an
intent is aborted (just without the intent count changes).

Release note: none.

pkg/storage/engine/mvcc.go
@@ -467,6 +467,25 @@ func updateStatsOnAbort(
 	orig, restored *enginepb.MVCCMetadata,
 	restoredNanos int64,
 ) enginepb.MVCCStats {
+	ms := updateStatsOnClear(
+		key, origMetaKeySize, origMetaValSize, restoredMetaKeySize, restoredMetaValSize, orig, restored, restoredNanos,
+	)
+	ms.IntentBytes -= (orig.KeyBytes + orig.ValBytes)
+	ms.IntentCount--
+	return ms
+}
+
+// updateStatsOnClear updates stat counters by subtracting a
+// cleared value's key and value byte sizes. If an earlier version
+// was restored, the restored values are added to live bytes and
+// count if the restored value isn't a deletion tombstone.
+func updateStatsOnClear(
+	key roachpb.Key,
+	origMetaKeySize, origMetaValSize, restoredMetaKeySize, restoredMetaValSize int64,
+	orig, restored *enginepb.MVCCMetadata,
+	restoredNanos int64,
+) enginepb.MVCCStats {
+
 	var ms enginepb.MVCCStats
 
 	if isSysLocal(key) {
@@ -486,7 +505,7 @@ func updateStatsOnAbort(
 	// 1. the previous value is a tombstone, so according to rule 2 it accrues
 	// GCBytesAge from its own timestamp on (we need to adjust only for the
 	// implicit meta key that "pops up" at that timestamp), -- or --
-	// 2. it is not, and it has been shadowed by the intent we're now aborting,
+	// 2. it is not, and it has been shadowed by the key we are clearing,
 	// in which case we need to offset its GCBytesAge contribution from
 	// restoredNanos to orig.Timestamp (rule 1).
 	if restored != nil {
@@ -531,8 +550,6 @@ func updateStatsOnAbort(
 	ms.ValBytes -= (orig.ValBytes + origMetaValSize)
 	ms.KeyCount--
 	ms.ValCount--
-	ms.IntentBytes -= (orig.KeyBytes + orig.ValBytes)
-	ms.IntentCount--
 
 	return ms
 }
@@ -1863,6 +1880,7 @@ func MVCCMerge(
 func MVCCClearTimeRange(
 	ctx context.Context,
 	batch ReadWriter,
+	ms *enginepb.MVCCStats,
 	key, endKey roachpb.Key,
 	startTime, endTime hlc.Timestamp,
 	maxBatchSize int64,
@@ -1936,6 +1954,9 @@ func MVCCClearTimeRange(
 	it := batch.NewIterator(IterOptions{LowerBound: key, UpperBound: endKey})
 	defer it.Close()
 
+	var clearedMetaKey MVCCKey
+	var clearedMeta enginepb.MVCCMetadata
+	var restoredMeta enginepb.MVCCMetadata
 	for it.Seek(MVCCKey{Key: key}); ; it.Next() {
 		ok, err := it.Valid()
 		if err != nil {
@@ -1943,14 +1964,37 @@ func MVCCClearTimeRange(
 		} else if !ok {
 			break
 		}
+
 		k := it.UnsafeKey()
+
+		if ms != nil && len(clearedMetaKey.Key) > 0 {
+			metaKeySize := int64(clearedMetaKey.EncodedSize())
+			if bytes.Equal(clearedMetaKey.Key, k.Key) {
+				// Since the key matches, our previous clear "restored" this revision of
+				// the this key, so update the stats with this as the "restored" key.
+				valueSize := int64(len(it.Value()))
+				restoredMeta.KeyBytes = mvccVersionTimestampSize
+				restoredMeta.Deleted = valueSize == 0
+				restoredMeta.ValBytes = valueSize
+				restoredMeta.Timestamp = hlc.LegacyTimestamp(k.Timestamp)
+
+				ms.Add(updateStatsOnClear(
+					clearedMetaKey.Key, metaKeySize, 0, metaKeySize, 0, &clearedMeta, &restoredMeta, k.Timestamp.WallTime,
+				))
+			} else {
+				// We cleared a revision of a different key, so nothing was "restored".
+				ms.Add(updateStatsOnClear(clearedMetaKey.Key, metaKeySize, 0, 0, 0, &clearedMeta, nil, 0))
+			}
+			clearedMetaKey.Key = clearedMetaKey.Key[:0]
+		}
+
 		if c := k.Key.Compare(endKey); c >= 0 {
 			break
 		}
 
-		// We need to check for and fail on any intents in our time-range as we do
+		// We need to check for and fail on any intents in our time-range, as we do
 		// not want to clear any running transactions. We don't _expect_ to hit this
-		// since the RevertRange is only intended for non-live key spans but there
+		// since the RevertRange is only intended for non-live key spans, but there
 		// could be an intent leftover.
 		var meta enginepb.MVCCMetadata
 		if !k.IsValue() {
@@ -1973,12 +2017,26 @@ func MVCCClearTimeRange(
 				break
 			}
 			clearMatchingKey(k)
+			if ms != nil {
+				clearedMetaKey.Key = append(clearedMetaKey.Key[:0], k.Key...)
+				clearedMeta.KeyBytes = mvccVersionTimestampSize
+				clearedMeta.ValBytes = int64(len(it.UnsafeValue()))
+				clearedMeta.Deleted = clearedMeta.ValBytes == 0
+				clearedMeta.Timestamp = hlc.LegacyTimestamp(k.Timestamp)
+			}
 		} else {
 			// This key does not match, so we need to flush our run of matching keys.
 			if err := flushClearedKeys(k); err != nil {
 				return nil, err
 			}
 		}
+	}
+
+	if ms != nil && len(clearedMetaKey.Key) > 0 {
+		// If we cleared on the last iteration, no older revision of that key was
+		// "restored", since otherwise we would have iterated over it.
+		origMetaKeySize := int64(clearedMetaKey.EncodedSize())
+		ms.Add(updateStatsOnClear(clearedMetaKey.Key, origMetaKeySize, 0, 0, 0, &clearedMeta, nil, 0))
 	}
 
 	return resume, flushClearedKeys(MVCCKey{Key: endKey})

pkg/storage/batcheval/cmd_revert_range.go
@@ -87,36 +87,12 @@ func RevertRange(
 
 	log.VEventf(ctx, 2, "clearing keys with timestamp (%v, %v]", args.TargetTime, cArgs.Header.Timestamp)
 
-	// Get the initial MVCC stats for all the keys in the affected span.
-	statsBefore, err := computeStatsDelta(
-		ctx, batch, cArgs, engine.MVCCKey{Key: args.Key}, engine.MVCCKey{Key: args.EndKey},
-	)
-	if err != nil {
-		return result.Result{}, err
-	}
-
 	resume, err := engine.MVCCClearTimeRange(
-		ctx, batch, args.Key, args.EndKey, args.TargetTime, cArgs.Header.Timestamp, cArgs.MaxKeys,
+		ctx, batch, cArgs.Stats, args.Key, args.EndKey, args.TargetTime, cArgs.Header.Timestamp, cArgs.MaxKeys,
 	)
 	if err != nil {
 		return result.Result{}, err
 	}
-
-	// Compute the true stats for the affected span after modification.
-	iter := batch.NewIterator(engine.IterOptions{UpperBound: args.Key})
-	statsAfter, err := iter.ComputeStats(
-		engine.MVCCKey{Key: args.Key}, engine.MVCCKey{Key: args.EndKey}, cArgs.Header.Timestamp.WallTime,
-	)
-	iter.Close()
-	if err != nil {
-		return result.Result{}, err
-	}
-
-	// Compute the difference between the computed post-modification stats and the
-	// initial stats for the span and
-	statsAfter.Subtract(statsBefore)
-
-	*cArgs.Stats = statsAfter
 
 	if resume != nil {
 		reply.ResumeSpan = resume

pkg/storage/engine/mvcc_test.go
@@ -2434,7 +2434,7 @@ func TestMVCCClearTimeRange(t *testing.T) {
 	t.Run("clear > ts0", func(t *testing.T) {
 		e := setupKVs(t)
 		defer e.Close()
-		_, err := MVCCClearTimeRange(ctx, e, keyMin, keyMax, ts0, ts5, 10)
+		_, err := MVCCClearTimeRange(ctx, e, nil, keyMin, keyMax, ts0, ts5, 10)
 		require.NoError(t, err)
 		assertKVs(t, e, ts0, ts0Content)
 		assertKVs(t, e, ts1, ts0Content)
@@ -2444,7 +2444,7 @@ func TestMVCCClearTimeRange(t *testing.T) {
 	t.Run("clear > ts1 ", func(t *testing.T) {
 		e := setupKVs(t)
 		defer e.Close()
-		_, err := MVCCClearTimeRange(ctx, e, keyMin, keyMax, ts1, ts5, 10)
+		_, err := MVCCClearTimeRange(ctx, e, nil, keyMin, keyMax, ts1, ts5, 10)
 		require.NoError(t, err)
 		assertKVs(t, e, ts1, ts1Content)
 		assertKVs(t, e, ts2, ts1Content)
@@ -2454,7 +2454,7 @@ func TestMVCCClearTimeRange(t *testing.T) {
 	t.Run("clear > ts2", func(t *testing.T) {
 		e := setupKVs(t)
 		defer e.Close()
-		_, err := MVCCClearTimeRange(ctx, e, keyMin, keyMax, ts2, ts5, 10)
+		_, err := MVCCClearTimeRange(ctx, e, nil, keyMin, keyMax, ts2, ts5, 10)
 		require.NoError(t, err)
 		assertKVs(t, e, ts2, ts2Content)
 		assertKVs(t, e, ts5, ts2Content)
@@ -2463,7 +2463,7 @@ func TestMVCCClearTimeRange(t *testing.T) {
 	t.Run("clear > ts3", func(t *testing.T) {
 		e := setupKVs(t)
 		defer e.Close()
-		_, err := MVCCClearTimeRange(ctx, e, keyMin, keyMax, ts3, ts5, 10)
+		_, err := MVCCClearTimeRange(ctx, e, nil, keyMin, keyMax, ts3, ts5, 10)
 		require.NoError(t, err)
 		assertKVs(t, e, ts3, ts3Content)
 		assertKVs(t, e, ts5, ts3Content)
@@ -2472,7 +2472,7 @@ func TestMVCCClearTimeRange(t *testing.T) {
 	t.Run("clear > ts4 (nothing) ", func(t *testing.T) {
 		e := setupKVs(t)
 		defer e.Close()
-		_, err := MVCCClearTimeRange(ctx, e, keyMin, keyMax, ts4, ts5, 10)
+		_, err := MVCCClearTimeRange(ctx, e, nil, keyMin, keyMax, ts4, ts5, 10)
 		require.NoError(t, err)
 		assertKVs(t, e, ts4, ts4Content)
 		assertKVs(t, e, ts5, ts4Content)
@@ -2481,7 +2481,7 @@ func TestMVCCClearTimeRange(t *testing.T) {
 	t.Run("clear > ts5 (nothing)", func(t *testing.T) {
 		e := setupKVs(t)
 		defer e.Close()
-		_, err := MVCCClearTimeRange(ctx, e, keyMin, keyMax, ts5, ts5, 10)
+		_, err := MVCCClearTimeRange(ctx, e, nil, keyMin, keyMax, ts5, ts5, 10)
 		require.NoError(t, err)
 		assertKVs(t, e, ts4, ts4Content)
 		assertKVs(t, e, ts5, ts4Content)
@@ -2490,7 +2490,7 @@ func TestMVCCClearTimeRange(t *testing.T) {
 	t.Run("clear up to k5 to ts0", func(t *testing.T) {
 		e := setupKVs(t)
 		defer e.Close()
-		_, err := MVCCClearTimeRange(ctx, e, testKey1, testKey5, ts0, ts5, 10)
+		_, err := MVCCClearTimeRange(ctx, e, nil, testKey1, testKey5, ts0, ts5, 10)
 		require.NoError(t, err)
 		assertKVs(t, e, ts2, []roachpb.KeyValue{{Key: testKey5, Value: v2}})
 		assertKVs(t, e, ts5, []roachpb.KeyValue{{Key: testKey5, Value: v4}})
@@ -2499,7 +2499,7 @@ func TestMVCCClearTimeRange(t *testing.T) {
 	t.Run("clear > ts0 in empty span (nothing)", func(t *testing.T) {
 		e := setupKVs(t)
 		defer e.Close()
-		_, err := MVCCClearTimeRange(ctx, e, testKey3, testKey5, ts0, ts5, 10)
+		_, err := MVCCClearTimeRange(ctx, e, nil, testKey3, testKey5, ts0, ts5, 10)
 		require.NoError(t, err)
 		assertKVs(t, e, ts2, ts2Content)
 		assertKVs(t, e, ts5, ts4Content)
@@ -2508,7 +2508,7 @@ func TestMVCCClearTimeRange(t *testing.T) {
 	t.Run("clear > ts0 in empty span [k3,k5) (nothing)", func(t *testing.T) {
 		e := setupKVs(t)
 		defer e.Close()
-		_, err := MVCCClearTimeRange(ctx, e, testKey3, testKey5, ts0, ts5, 10)
+		_, err := MVCCClearTimeRange(ctx, e, nil, testKey3, testKey5, ts0, ts5, 10)
 		require.NoError(t, err)
 		assertKVs(t, e, ts2, ts2Content)
 		assertKVs(t, e, ts5, ts4Content)
@@ -2517,7 +2517,7 @@ func TestMVCCClearTimeRange(t *testing.T) {
 	t.Run("clear k3 and up in ts0 > x >= ts1 (nothing)", func(t *testing.T) {
 		e := setupKVs(t)
 		defer e.Close()
-		_, err := MVCCClearTimeRange(ctx, e, testKey3, keyMax, ts0, ts1, 10)
+		_, err := MVCCClearTimeRange(ctx, e, nil, testKey3, keyMax, ts0, ts1, 10)
 		require.NoError(t, err)
 		assertKVs(t, e, ts2, ts2Content)
 		assertKVs(t, e, ts5, ts4Content)
@@ -2533,21 +2533,21 @@ func TestMVCCClearTimeRange(t *testing.T) {
 	t.Run("clear everything hitting intent fails", func(t *testing.T) {
 		e := setupKVsWithIntent(t)
 		defer e.Close()
-		_, err := MVCCClearTimeRange(ctx, e, keyMin, keyMax, ts0, ts5, 10)
+		_, err := MVCCClearTimeRange(ctx, e, nil, keyMin, keyMax, ts0, ts5, 10)
 		require.EqualError(t, err, "conflicting intents on \"/db3\"")
 	})
 
 	t.Run("clear exactly hitting intent fails", func(t *testing.T) {
 		e := setupKVsWithIntent(t)
 		defer e.Close()
-		_, err := MVCCClearTimeRange(ctx, e, testKey3, testKey4, ts2, ts3, 10)
+		_, err := MVCCClearTimeRange(ctx, e, nil, testKey3, testKey4, ts2, ts3, 10)
 		require.EqualError(t, err, "conflicting intents on \"/db3\"")
 	})
 
 	t.Run("clear everything above intent", func(t *testing.T) {
 		e := setupKVsWithIntent(t)
 		defer e.Close()
-		_, err := MVCCClearTimeRange(ctx, e, keyMin, keyMax, ts3, ts5, 10)
+		_, err := MVCCClearTimeRange(ctx, e, nil, keyMin, keyMax, ts3, ts5, 10)
 		require.NoError(t, err)
 		assertKVs(t, e, ts2, ts2Content)
 
@@ -2572,10 +2572,23 @@ func TestMVCCClearTimeRange(t *testing.T) {
 		e := setupKVsWithIntent(t)
 		defer e.Close()
 		assertKVs(t, e, ts2, ts2Content)
-		_, err := MVCCClearTimeRange(ctx, e, keyMin, keyMax, ts1, ts2, 10)
+		_, err := MVCCClearTimeRange(ctx, e, nil, keyMin, keyMax, ts1, ts2, 10)
 		require.NoError(t, err)
 		assertKVs(t, e, ts2, ts1Content)
 	})
+}
+
+func computeStats(
+	t *testing.T, batch Reader, from, to roachpb.Key, nowNanos int64,
+) enginepb.MVCCStats {
+	t.Helper()
+	iter := batch.NewIterator(IterOptions{UpperBound: to})
+	defer iter.Close()
+	s, err := ComputeStatsGo(iter, MVCCKey{Key: from}, MVCCKey{Key: to}, nowNanos)
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+	return s
 }
 
 // TestMVCCClearTimeRangeOnRandomData sets up mostly random KVs and then picks
@@ -2593,6 +2606,8 @@ func TestMVCCClearTimeRangeOnRandomData(t *testing.T) {
 	defer e.Close()
 
 	now := hlc.Timestamp{WallTime: 100000000}
+
+	var ms enginepb.MVCCStats
 
 	// Setup numKVs random kv by writing to random keys [0, keyRange) except for
 	// the span [swathStart, swathEnd). Then fill in that swath with kvs all
@@ -2618,22 +2633,31 @@ func TestMVCCClearTimeRangeOnRandomData(t *testing.T) {
 		wrote[k] = ts
 
 		key := roachpb.Key(fmt.Sprintf("%05d", k))
-		v := roachpb.MakeValueFromString(fmt.Sprintf("v-%d", i))
-		require.NoError(t, MVCCPut(ctx, e, nil, key, hlc.Timestamp{WallTime: ts}, v, nil))
+		if rand.Float64() > 0.8 {
+			require.NoError(t, MVCCDelete(ctx, e, &ms, key, hlc.Timestamp{WallTime: ts}, nil))
+		} else {
+			v := roachpb.MakeValueFromString(fmt.Sprintf("v-%d", i))
+			require.NoError(t, MVCCPut(ctx, e, &ms, key, hlc.Timestamp{WallTime: ts}, v, nil))
+		}
 	}
 	swathTime := rand.Intn(randTimeRange-100) + 100
 	for i := swathStart; i < swathEnd; i++ {
 		key := roachpb.Key(fmt.Sprintf("%05d", i))
 		v := roachpb.MakeValueFromString(fmt.Sprintf("v-%d", i))
-		require.NoError(t, MVCCPut(ctx, e, nil, key, hlc.Timestamp{WallTime: int64(swathTime)}, v, nil))
+		require.NoError(t, MVCCPut(ctx, e, &ms, key, hlc.Timestamp{WallTime: int64(swathTime)}, v, nil))
 	}
 
 	// Add another swath of keys above to exercise an after-iteration range flush.
 	for i := keyRange; i < keyRange+200; i++ {
 		key := roachpb.Key(fmt.Sprintf("%05d", i))
 		v := roachpb.MakeValueFromString(fmt.Sprintf("v-%d", i))
-		require.NoError(t, MVCCPut(ctx, e, nil, key, hlc.Timestamp{WallTime: int64(randTimeRange + 1)}, v, nil))
+		require.NoError(t, MVCCPut(ctx, e, &ms, key, hlc.Timestamp{WallTime: int64(randTimeRange + 1)}, v, nil))
 	}
+
+	ms.AgeTo(2000)
+
+	// Sanity check starting stats.
+	require.Equal(t, computeStats(t, e, keyMin, keyMax, 2000), ms)
 
 	// Pick timestamps to which we'll revert, and sort them so we can go back
 	// though them in order. The largest will still be less than randTimeRange so
@@ -2655,7 +2679,7 @@ func TestMVCCClearTimeRangeOnRandomData(t *testing.T) {
 			// Revert to the revert time.
 			startKey := keyMin
 			for {
-				resume, err := MVCCClearTimeRange(ctx, e, startKey, keyMax, revertTo, now, 100)
+				resume, err := MVCCClearTimeRange(ctx, e, &ms, startKey, keyMax, revertTo, now, 100)
 				require.NoError(t, err)
 				if resume == nil {
 					break
@@ -2663,6 +2687,7 @@ func TestMVCCClearTimeRangeOnRandomData(t *testing.T) {
 				startKey = resume.Key
 			}
 
+			require.Equal(t, computeStats(t, e, keyMin, keyMax, 2000), ms)
 			// Scanning at "now" post-revert should yield the same result as scanning
 			// at revert-time pre-revert.
 			scannedAfter, _, _, err := MVCCScan(ctx, e, keyMin, keyMax, numKVs, now, MVCCScanOptions{})
@@ -2671,6 +2696,7 @@ func TestMVCCClearTimeRangeOnRandomData(t *testing.T) {
 		})
 	}
 }
+
 func TestMVCCConditionalPut(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 

commit 2edae4fbc513dd194fe671437f20a6fd663711a4

storage/engine: don't decrement IntentCount on system intent abort
Fixes #39390.
Fixes #39407.
Fixes #39370.
Fixes #39419.

A bug introduced in #39251 was causing the IntentCount field in MVCCStats
to get out of sync.

I was expecting `TestMVCCStatsRandomized/sys/inline=false` to easily
catch the bug, but it wasn't. It turns out that the test was updating
the transaction's write timestamp but not its read timestamp, which
was causing it to hit this error:
https://github.com/cockroachdb/cockroach/blob/41cb28e85575292926790e79912c893d13e1054d/pkg/storage/engine/mvcc.go#L1349
This was preventing the randomized test from performing the series of steps
we needed it to to hit the bug.

This commit fixes the bug, fixes the randomized test so that it would
have caught it, and adds a targeted regression test for this case.

Release note: None

pkg/storage/engine/mvcc.go
@@ -49,7 +49,7 @@ func MakeValue(meta enginepb.MVCCMetadata) roachpb.Value {
 
 // IsIntentOf returns true if the meta record is an intent of the supplied
 // transaction.
-func IsIntentOf(meta enginepb.MVCCMetadata, txn *roachpb.Transaction) bool {
+func IsIntentOf(meta *enginepb.MVCCMetadata, txn *roachpb.Transaction) bool {
 	return meta.Txn != nil && txn != nil && meta.Txn.ID == txn.ID
 }
 
@@ -362,7 +362,7 @@ func updateStatsOnResolve(
 	key roachpb.Key,
 	prevValSize int64,
 	origMetaKeySize, origMetaValSize, metaKeySize, metaValSize int64,
-	orig, meta enginepb.MVCCMetadata,
+	orig, meta *enginepb.MVCCMetadata,
 	commit bool,
 ) enginepb.MVCCStats {
 	var ms enginepb.MVCCStats
@@ -457,24 +457,6 @@ func updateStatsOnResolve(
 	return ms
 }
 
-// updateStatsOnAbort updates stat counters by subtracting an
-// aborted value's key and value byte sizes. If an earlier version
-// was restored, the restored values are added to live bytes and
-// count if the restored value isn't a deletion tombstone.
-func updateStatsOnAbort(
-	key roachpb.Key,
-	origMetaKeySize, origMetaValSize, restoredMetaKeySize, restoredMetaValSize int64,
-	orig, restored *enginepb.MVCCMetadata,
-	restoredNanos int64,
-) enginepb.MVCCStats {
-	ms := updateStatsOnClear(
-		key, origMetaKeySize, origMetaValSize, restoredMetaKeySize, restoredMetaValSize, orig, restored, restoredNanos,
-	)
-	ms.IntentBytes -= (orig.KeyBytes + orig.ValBytes)
-	ms.IntentCount--
-	return ms
-}
-
 // updateStatsOnClear updates stat counters by subtracting a
 // cleared value's key and value byte sizes. If an earlier version
 // was restored, the restored values are added to live bytes and
@@ -550,6 +532,10 @@ func updateStatsOnClear(
 	ms.ValBytes -= (orig.ValBytes + origMetaValSize)
 	ms.KeyCount--
 	ms.ValCount--
+	if orig.Txn != nil {
+		ms.IntentBytes -= (orig.KeyBytes + orig.ValBytes)
+		ms.IntentCount--
+	}
 
 	return ms
 }
@@ -837,7 +823,7 @@ func mvccGetInternal(
 		timestamp = metaTimestamp.Prev()
 	}
 
-	ownIntent := IsIntentOf(*meta, txn) // false if txn == nil
+	ownIntent := IsIntentOf(meta, txn) // false if txn == nil
 	if !timestamp.Less(metaTimestamp) && meta.Txn != nil && !ownIntent {
 		// Trying to read the last value, but it's another transaction's intent;
 		// the reader will have to act on this.
@@ -2581,7 +2567,7 @@ func mvccResolveWriteIntent(
 		// Update stat counters related to resolving the intent.
 		if ms != nil {
 			ms.Add(updateStatsOnResolve(intent.Key, prevValSize, origMetaKeySize, origMetaValSize,
-				metaKeySize, metaValSize, *meta, buf.newMeta, commit))
+				metaKeySize, metaValSize, meta, &buf.newMeta, commit))
 		}
 
 		// Log the logical MVCC operation.
@@ -2635,7 +2621,7 @@ func mvccResolveWriteIntent(
 		}
 		// Clear stat counters attributable to the intent we're aborting.
 		if ms != nil {
-			ms.Add(updateStatsOnAbort(intent.Key, origMetaKeySize, origMetaValSize, 0, 0, meta, nil, 0))
+			ms.Add(updateStatsOnClear(intent.Key, origMetaKeySize, origMetaValSize, 0, 0, meta, nil, 0))
 		}
 		return true, nil
 	}
@@ -2656,7 +2642,7 @@ func mvccResolveWriteIntent(
 
 	// Update stat counters with older version.
 	if ms != nil {
-		ms.Add(updateStatsOnAbort(intent.Key, origMetaKeySize, origMetaValSize,
+		ms.Add(updateStatsOnClear(intent.Key, origMetaKeySize, origMetaValSize,
 			metaKeySize, metaValSize, meta, &buf.newMeta, unsafeNextKey.Timestamp.WallTime))
 	}
 

pkg/storage/engine/mvcc_stats_test.go
@@ -1134,6 +1134,74 @@ func TestMVCCStatsTxnSysPutPut(t *testing.T) {
 	assertEq(t, engine, "after intent rewrite", aggMS, &expMS)
 }
 
+// TestMVCCStatsTxnSysPutAbort prevents regression of a bug that, when aborting
+// an intent on a sys key, would lead to undercounting `ms.IntentBytes` and
+// `ms.IntentCount`.
+func TestMVCCStatsTxnSysPutAbort(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	engine := createTestEngine()
+	defer engine.Close()
+
+	ctx := context.Background()
+	aggMS := &enginepb.MVCCStats{}
+
+	assertEq(t, engine, "initially", aggMS, &enginepb.MVCCStats{})
+
+	key := keys.RangeDescriptorKey(roachpb.RKey("a"))
+
+	ts1 := hlc.Timestamp{WallTime: 1E9}
+	txn := &roachpb.Transaction{
+		TxnMeta:       enginepb.TxnMeta{ID: uuid.MakeV4(), Timestamp: ts1},
+		OrigTimestamp: ts1,
+	}
+
+	// Write a system intent at ts1.
+	val1 := roachpb.MakeValueFromString("value")
+	if err := MVCCPut(ctx, engine, aggMS, key, txn.OrigTimestamp, val1, txn); err != nil {
+		t.Fatal(err)
+	}
+
+	mKeySize := int64(mvccKey(key).EncodedSize())
+	require.EqualValues(t, mKeySize, 11)
+
+	mValSize := int64((&enginepb.MVCCMetadata{
+		Timestamp: hlc.LegacyTimestamp(ts1),
+		Deleted:   false,
+		Txn:       &txn.TxnMeta,
+	}).Size())
+	require.EqualValues(t, mValSize, 46)
+
+	vKeySize := mvccVersionTimestampSize
+	require.EqualValues(t, vKeySize, 12)
+
+	vVal1Size := int64(len(val1.RawBytes))
+	require.EqualValues(t, vVal1Size, 10)
+
+	val2 := roachpb.MakeValueFromString("longvalue")
+	vVal2Size := int64(len(val2.RawBytes))
+	require.EqualValues(t, vVal2Size, 14)
+
+	expMS := enginepb.MVCCStats{
+		LastUpdateNanos: 1E9,
+		SysBytes:        mKeySize + mValSize + vKeySize + vVal1Size, // 11+44+12+10 = 77
+		SysCount:        1,
+	}
+	assertEq(t, engine, "after first put", aggMS, &expMS)
+
+	// Now abort the intent.
+	txn.Status = roachpb.ABORTED
+	if err := MVCCResolveWriteIntent(ctx, engine, aggMS, roachpb.Intent{
+		Span: roachpb.Span{Key: key}, Status: txn.Status, Txn: txn.TxnMeta,
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	expMS = enginepb.MVCCStats{
+		LastUpdateNanos: 1E9,
+	}
+	assertEq(t, engine, "after aborting", aggMS, &expMS)
+}
+
 // TestMVCCStatsSysPutPut prevents regression of a bug that, when writing a new
 // value on top of an existing system key, would undercount.
 func TestMVCCStatsSysPutPut(t *testing.T) {
@@ -1267,14 +1335,14 @@ func (s *randomTest) step(t *testing.T) {
 		s.TS = hlc.Timestamp{}
 	}
 
-	if s.Txn != nil {
+	restart := s.Txn != nil && s.rng.Intn(2) == 0
+	if restart {
 		// TODO(tschottdorf): experiment with s.TS != s.Txn.TS. Which of those
 		// cases are reasonable and which should we catch and error out?
 		//
 		// Note that we already exercise txn.Timestamp > s.TS since we call
 		// Forward() here (while s.TS may move backwards).
-		s.Txn.Timestamp.Forward(s.TS)
-		s.Txn.Sequence++
+		s.Txn.Restart(0, 0, s.TS)
 	}
 	s.cycle++
 
@@ -1351,7 +1419,8 @@ func TestMVCCStatsRandomized(t *testing.T) {
 	}
 	actions["EnsureTxn"] = func(s *state) string {
 		if s.Txn == nil {
-			s.Txn = &roachpb.Transaction{TxnMeta: enginepb.TxnMeta{ID: uuid.MakeV4(), Timestamp: s.TS}}
+			txn := roachpb.MakeTransaction("test", nil, 0, s.TS, 0)
+			s.Txn = &txn
 		}
 		return ""
 	}