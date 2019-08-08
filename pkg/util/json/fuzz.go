20d280000c13dcd783681bfa316bca9d2a477537
Matt Jibson <matt.jibson@gmail.com>
2019-08-08 11:21:22 -0600

    cmd/fuzz: add new fuzz command
    fuzz is a CLI program that takes a packages spec (like ./pkg/...),
    searches for fuzz functions in it, builds and runs them, and reports
    the results.
    
    Various fuzzers have been added.
    
    A `make fuzz` rule has been added to facilite fuzz testing. It supports
    the optional PKG, TESTS, and TESTTIMEOUT variables. TESTTIMEOUT specifies
    how long to run each fuzz func for. For example:
    
    `make fuzz PKG=./pkg/sql/... TESTS=Decimal TESTTIMEOUT=1m`
    
    My investigation showed that go-fuzz-build was not able to be used
    from a vendor directory because during its execution it requires the
    go-fuzz-dep package be available, but didn't pick it up if it was in
    the vendor directory. Thus, we require users install go-fuzz manually.
    
    https://github.com/thepudds/fzgo is a program that does similar things
    to cmd/fuzz, but it is very early and had some bugs and behavior that
    wasn't what I wanted just yet. We may investigate it more in the future.
    
    Release note: None

---
+++ pkg/util/json/fuzz.go
@@ -0,0 +1,21 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+// +build gofuzz
+
+package json
+
+func FuzzParseJSON(data []byte) int {
+	_, err := ParseJSON(string(data))
+	if err != nil {
+		return 0
+	}
+	return 1
+}
