a667e087f688604ffd30c8f0f79b2e60d345b9a2
Andrew Werner <ajwerner@cockroachlabs.com>
2019-08-12 14:50:37 -0400

    closedts: shorten target_duration from 30s to 3s
    This PR dramatically shortens the closed timestamp target interval. With this
    setting the experimental_follower_read_timestamp() will now be 4.8 seconds in
    the past as opposed to the current 48 seconds in the past. This relatively
    aggressive setting of 3s is intended to shake out issues.
    
    This value has been verified to work on the `schemachange/mixed/tpcc` roachtest
    introduced in #39096 and for vanilla TPC-C runs around where we have
    established a baseline in the past using tpccbench. Specifically several runs
    at 2300 warehouses on 3x c5d.4xlarge nodes which is right at the passing
    boundary without the change were run only a very small difference in
    efficiency or tail latency was observed.
    
    It seems reasonable to attempt to live with this value on master for a while
    and see what happens.
    
    Fixes #37083.
    
    Release note (enterprise change): Shorten the default closed timestamp target
    interval from 30s to 3s leading to permit follower reads at 4.8 seconds in
    the past rather than the previous 48 seconds.

--- pkg/ccl/followerreadsccl/followerreads_test.go
+++ pkg/ccl/followerreadsccl/followerreads_test.go
@@ -32,7 +32,13 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 )
 
-const expectedFollowerReadOffset = -1 * (30 * (1 + .2*3)) * time.Second
+const (
+	defaultInterval                          = 3
+	defaultFraction                          = .2
+	defaultMultiple                          = 3
+	expectedFollowerReadOffset time.Duration = 1e9 * /* 1 second */
+		-defaultInterval * (1 + defaultFraction*defaultMultiple)
+)
 
 func TestEvalFollowerReadOffset(t *testing.T) {
 	defer leaktest.AfterTest(t)()
