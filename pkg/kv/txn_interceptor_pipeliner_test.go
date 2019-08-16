1a84480056786eb3fc11e6bb4f35d3c229fbc53f
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2019-08-15 20:01:41 -0400

    kv: don't update transaction proto directly from heartbeat loop
    Fixes #39652.
    Fixes #39661.
    Fixes #35144.
    
    This commit fixes the referenced issues by eliminating the practice of updating
    the transaction coordinator's proto directly from its heartbeat loop. This was
    problematic because of the race described in
    https://github.com/cockroachdb/cockroach/blob/dc3686f79b3750500efaff7092c81a3e5ce6d02c/pkg/kv/txn_interceptor_heartbeater.go#L357-L364.
    The heartbeat loop doesn't know if it's racing with an EndTransaction request
    and it could incorrectly interpret a missing transaction record if it is. The
    safest thing to do is to limit the path in which it informs the TxnCoordSender
    of updates.
    
    This limits the responsibility of the heartbeat loop. Its job is now
    only to:
    1. update the transaction record to maintain liveness
    2. eagerly clean up a transaction if it is found to be aborted
    3. inform the transaction coordinator about an aborted transaction
       record IF the transaction coordinator is continuing to send requests
       through the interceptor.
    
    Notably, the heartbeat loop no longer blindly updates the transaction
    coordinator's transaction proto. There wasn't a strong reason for it to be able
    to do so, especially now that we no longer push transactions or ratchet their
    priority frequently. Moreover, even if those were still frequent occurrences,
    updating the proto from the heartbeat loop prevented usual restart handling from
    being used. For instance, doing so might prevent us from refreshing the
    transaction. All in all, allowing this didn't seem worth the complexity.
    
    This commit also includes some cleanup. For instance, it removes a confusing
    dependency where the txnHeartbeater called back into the TxnCoordSender. It also
    addresses a longstanding TODO to actually unit test the txnHeartbeater.
    
    Release note: None

--- pkg/kv/txn_interceptor_pipeliner_test.go
+++ pkg/kv/txn_interceptor_pipeliner_test.go
@@ -26,7 +26,8 @@ import (
 )
 
 // mockLockedSender implements the lockedSender interface and provides a way to
-// mock out and adjust the SendLocked method.
+// mock out and adjust the SendLocked method. If no mock function is set, a call
+// to SendLocked will return the default successful response.
 type mockLockedSender struct {
 	mockFn func(roachpb.BatchRequest) (*roachpb.BatchResponse, *roachpb.Error)
 }
@@ -34,6 +35,11 @@ type mockLockedSender struct {
 func (m *mockLockedSender) SendLocked(
 	ctx context.Context, ba roachpb.BatchRequest,
 ) (*roachpb.BatchResponse, *roachpb.Error) {
+	if m.mockFn == nil {
+		br := ba.CreateReply()
+		br.Txn = ba.Txn
+		return br, nil
+	}
 	return m.mockFn(ba)
 }
 
@@ -171,7 +177,7 @@ func TestTxnPipelinerTrackInFlightWrites(t *testing.T) {
 
 		br = ba.CreateReply()
 		br.Txn = ba.Txn
-		br.Responses[0].GetInner().(*roachpb.QueryIntentResponse).FoundIntent = true
+		br.Responses[0].GetQueryIntent().FoundIntent = true
 		return br, nil
 	})
 
@@ -341,7 +347,7 @@ func TestTxnPipelinerReads(t *testing.T) {
 
 		br = ba.CreateReply()
 		br.Txn = ba.Txn
-		br.Responses[0].GetInner().(*roachpb.QueryIntentResponse).FoundIntent = true
+		br.Responses[0].GetQueryIntent().FoundIntent = true
 		return br, nil
 	})
 
@@ -423,7 +429,7 @@ func TestTxnPipelinerRangedWrites(t *testing.T) {
 
 		br = ba.CreateReply()
 		br.Txn = ba.Txn
-		br.Responses[0].GetInner().(*roachpb.QueryIntentResponse).FoundIntent = true
+		br.Responses[0].GetQueryIntent().FoundIntent = true
 		br.Responses[2].GetInner().(*roachpb.QueryIntentResponse).FoundIntent = true
 		br.Responses[3].GetInner().(*roachpb.QueryIntentResponse).FoundIntent = true
 		return br, nil
@@ -488,7 +494,7 @@ func TestTxnPipelinerNonTransactionalRequests(t *testing.T) {
 
 		br = ba.CreateReply()
 		br.Txn = ba.Txn
-		br.Responses[0].GetInner().(*roachpb.QueryIntentResponse).FoundIntent = true
+		br.Responses[0].GetQueryIntent().FoundIntent = true
 		br.Responses[1].GetInner().(*roachpb.QueryIntentResponse).FoundIntent = true
 		return br, nil
 	})
@@ -845,7 +851,7 @@ func TestTxnPipelinerEnableDisableMixTxn(t *testing.T) {
 
 		br = ba.CreateReply()
 		br.Txn = ba.Txn
-		br.Responses[0].GetInner().(*roachpb.QueryIntentResponse).FoundIntent = true
+		br.Responses[0].GetQueryIntent().FoundIntent = true
 		return br, nil
 	})
 
@@ -878,7 +884,7 @@ func TestTxnPipelinerEnableDisableMixTxn(t *testing.T) {
 		br = ba.CreateReply()
 		br.Txn = ba.Txn
 		br.Txn.Status = roachpb.COMMITTED
-		br.Responses[0].GetInner().(*roachpb.QueryIntentResponse).FoundIntent = true
+		br.Responses[0].GetQueryIntent().FoundIntent = true
 		return br, nil
 	})
 
@@ -979,7 +985,7 @@ func TestTxnPipelinerMaxInFlightSize(t *testing.T) {
 
 		br = ba.CreateReply()
 		br.Txn = ba.Txn
-		br.Responses[0].GetInner().(*roachpb.QueryIntentResponse).FoundIntent = true
+		br.Responses[0].GetQueryIntent().FoundIntent = true
 		br.Responses[2].GetInner().(*roachpb.QueryIntentResponse).FoundIntent = true
 		return br, nil
 	})
@@ -1026,7 +1032,7 @@ func TestTxnPipelinerMaxInFlightSize(t *testing.T) {
 
 		br = ba.CreateReply()
 		br.Txn = ba.Txn
-		br.Responses[0].GetInner().(*roachpb.QueryIntentResponse).FoundIntent = true
+		br.Responses[0].GetQueryIntent().FoundIntent = true
 		br.Responses[2].GetInner().(*roachpb.QueryIntentResponse).FoundIntent = true
 		return br, nil
 	})
@@ -1113,7 +1119,7 @@ func TestTxnPipelinerMaxBatchSize(t *testing.T) {
 
 		br = ba.CreateReply()
 		br.Txn = ba.Txn
-		br.Responses[0].GetInner().(*roachpb.QueryIntentResponse).FoundIntent = true
+		br.Responses[0].GetQueryIntent().FoundIntent = true
 		return br, nil
 	})
 
