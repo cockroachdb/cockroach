32234aa98dea302445da141101f119a398ca05be
David Taylor <tinystatemachine@gmail.com>
2019-08-18 21:49:06 +0000

    build: run explicit dep build first
    We explicitly build dependencies in their own stage, but if 'Maybe Stress' decides to run
    something it'll trigger an implicit build of them instead, defeating the purpose of a
    standalone, separately measured stage. This moves the dependency-build stage first so that
    if maybe-stress decides to run anything, that step is already done.
    
    Release note: none.

--- build/teamcity-testrace.sh
+++ build/teamcity-testrace.sh
@@ -9,11 +9,6 @@ tc_prepare
 export TMPDIR=$PWD/artifacts/testrace
 mkdir -p "$TMPDIR"
 
-tc_start_block "Maybe stressrace pull request"
-build/builder.sh go install ./pkg/cmd/github-pull-request-make
-build/builder.sh env BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" TARGET=stressrace github-pull-request-make
-tc_end_block "Maybe stressrace pull request"
-
 tc_start_block "Determine changed packages"
 if tc_release_branch; then
 	pkgspec=./pkg/...
@@ -40,6 +35,11 @@ tc_start_block "Compile C dependencies"
 run build/builder.sh make -Otarget c-deps GOFLAGS=-race &> artifacts/race-c-build.log || (cat artifacts/race-c-build.log && false)
 rm artifacts/race-c-build.log
 tc_end_block "Compile C dependencies"
+
+tc_start_block "Maybe stressrace pull request"
+build/builder.sh go install ./pkg/cmd/github-pull-request-make
+build/builder.sh env BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" TARGET=stressrace github-pull-request-make
+tc_end_block "Maybe stressrace pull request"
 
 tc_start_block "Run Go tests under race detector"
 true >artifacts/testrace.log
