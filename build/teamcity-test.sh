0984c3a1f32b04dc47ce2d7112826b862e6866aa
David Taylor <tinystatemachine@gmail.com>
2019-08-18 21:49:06 +0000

    build: run explicit dep build first
    We explicitly build dependencies in their own stage, but if 'Maybe Stress' decides to run
    something it'll trigger an implicit build of them instead, defeating the purpose of a
    standalone, separately measured stage. This moves the dependency-build stage first so that
    if maybe-stress decides to run anything, that step is already done.
    
    Release note: none.

--- build/teamcity-test.sh
+++ build/teamcity-test.sh
@@ -9,17 +9,16 @@ tc_prepare
 export TMPDIR=$PWD/artifacts/test
 mkdir -p "$TMPDIR"
 
-tc_start_block "Maybe stress pull request"
-run build/builder.sh go install ./pkg/cmd/github-pull-request-make
-run build/builder.sh env BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" TARGET=stress github-pull-request-make
-tc_end_block "Maybe stress pull request"
-
 tc_start_block "Compile C dependencies"
 # Buffer noisy output and only print it on failure.
 run build/builder.sh make -Otarget c-deps &> artifacts/c-build.log || (cat artifacts/c-build.log && false)
 rm artifacts/c-build.log
 tc_end_block "Compile C dependencies"
 
+tc_start_block "Maybe stress pull request"
+run build/builder.sh go install ./pkg/cmd/github-pull-request-make
+run build/builder.sh env BUILD_VCS_NUMBER="$BUILD_VCS_NUMBER" TARGET=stress github-pull-request-make
+tc_end_block "Maybe stress pull request"
 
 tc_start_block "Run Go tests"
 run build/builder.sh \
