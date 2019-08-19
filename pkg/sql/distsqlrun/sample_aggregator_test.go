a24314125d650c7cfcf42d8db670082517ef14fa
Rebecca Taft <becca@cockroachlabs.com>
2019-08-19 16:29:09 +0200

    distsqlrun: fail gracefully if out of memory during stats creation
    This commit ensures that the sampler and sampleAggregator processors
    fail gracefully if they run out of memory. Instead of causing the
    entire CREATE STATISTICS job to fail in case of an out of memory error,
    they will continue to collect statistics for row counts, distinct
    counts and null counts, but will disable collection of histograms.
    
    Release note: None

--- pkg/sql/distsqlrun/sample_aggregator_test.go
+++ pkg/sql/distsqlrun/sample_aggregator_test.go
@@ -40,116 +40,123 @@ func TestSampleAggregator(t *testing.T) {
 	st := cluster.MakeTestingClusterSettings()
 	evalCtx := tree.MakeTestingEvalContext(st)
 	defer evalCtx.Stop(context.Background())
-	flowCtx := FlowCtx{
-		EvalCtx: &evalCtx,
-		Cfg: &ServerConfig{
-			Settings: st,
-			DB:       kvDB,
-			Executor: server.InternalExecutor().(sqlutil.InternalExecutor),
-			Gossip:   server.Gossip(),
-		},
-	}
 
-	inputRows := [][]int{
-		{-1, 1},
-		{1, 1},
-		{2, 2},
-		{1, 3},
-		{2, 4},
-		{1, 5},
-		{2, 6},
-		{1, 7},
-		{2, 8},
-		{-1, 3},
-		{1, -1},
-	}
+	runTest := func(memLimitBytes int64, expectOutOfMemory bool) {
+		flowCtx := FlowCtx{
+			EvalCtx: &evalCtx,
+			Cfg: &ServerConfig{
+				Settings: st,
+				DB:       kvDB,
+				Executor: server.InternalExecutor().(sqlutil.InternalExecutor),
+				Gossip:   server.Gossip(),
+			},
+		}
+		// Override the default memory limit. If memLimitBytes is small but
+		// non-zero, the processor will hit this limit and disable sampling.
+		flowCtx.Cfg.TestingKnobs.MemoryLimitBytes = memLimitBytes
 
-	// We randomly distribute the input rows between multiple Samplers and
-	// aggregate the results.
-	numSamplers := 3
+		inputRows := [][]int{
+			{-1, 1},
+			{1, 1},
+			{2, 2},
+			{1, 3},
+			{2, 4},
+			{1, 5},
+			{2, 6},
+			{1, 7},
+			{2, 8},
+			{-1, 3},
+			{1, -1},
+		}
 
-	samplerOutTypes := []types.T{
-		*types.Int,   // original column
-		*types.Int,   // original column
-		*types.Int,   // rank
-		*types.Int,   // sketch index
-		*types.Int,   // num rows
-		*types.Int,   // null vals
-		*types.Bytes, // sketch data
-	}
+		// We randomly distribute the input rows between multiple Samplers and
+		// aggregate the results.
+		numSamplers := 3
 
-	sketchSpecs := []distsqlpb.SketchSpec{
-		{
-			SketchType:        distsqlpb.SketchType_HLL_PLUS_PLUS_V1,
-			Columns:           []uint32{0},
-			GenerateHistogram: false,
-			StatName:          "a",
-		},
-		{
-			SketchType:          distsqlpb.SketchType_HLL_PLUS_PLUS_V1,
-			Columns:             []uint32{1},
-			GenerateHistogram:   true,
-			HistogramMaxBuckets: 4,
-		},
-	}
+		samplerOutTypes := []types.T{
+			*types.Int,   // original column
+			*types.Int,   // original column
+			*types.Int,   // rank
+			*types.Int,   // sketch index
+			*types.Int,   // num rows
+			*types.Int,   // null vals
+			*types.Bytes, // sketch data
+		}
 
-	rng, _ := randutil.NewPseudoRand()
-	rowPartitions := make([][][]int, numSamplers)
-	for _, row := range inputRows {
-		j := rng.Intn(numSamplers)
-		rowPartitions[j] = append(rowPartitions[j], row)
-	}
+		sketchSpecs := []distsqlpb.SketchSpec{
+			{
+				SketchType:        distsqlpb.SketchType_HLL_PLUS_PLUS_V1,
+				Columns:           []uint32{0},
+				GenerateHistogram: false,
+				StatName:          "a",
+			},
+			{
+				SketchType:          distsqlpb.SketchType_HLL_PLUS_PLUS_V1,
+				Columns:             []uint32{1},
+				GenerateHistogram:   true,
+				HistogramMaxBuckets: 4,
+			},
+		}
 
-	outputs := make([]*RowBuffer, numSamplers)
-	for i := 0; i < numSamplers; i++ {
-		rows := sqlbase.GenEncDatumRowsInt(rowPartitions[i])
-		in := NewRowBuffer(sqlbase.TwoIntCols, rows, RowBufferArgs{})
-		outputs[i] = NewRowBuffer(samplerOutTypes, nil /* rows */, RowBufferArgs{})
+		rng, _ := randutil.NewPseudoRand()
+		rowPartitions := make([][][]int, numSamplers)
+		for _, row := range inputRows {
+			j := rng.Intn(numSamplers)
+			rowPartitions[j] = append(rowPartitions[j], row)
+		}
 
-		spec := &distsqlpb.SamplerSpec{SampleSize: 100, Sketches: sketchSpecs}
-		p, err := newSamplerProcessor(&flowCtx, 0 /* processorID */, spec, in, &distsqlpb.PostProcessSpec{}, outputs[i])
+		outputs := make([]*RowBuffer, numSamplers)
+		for i := 0; i < numSamplers; i++ {
+			rows := sqlbase.GenEncDatumRowsInt(rowPartitions[i])
+			in := NewRowBuffer(sqlbase.TwoIntCols, rows, RowBufferArgs{})
+			outputs[i] = NewRowBuffer(samplerOutTypes, nil /* rows */, RowBufferArgs{})
+
+			spec := &distsqlpb.SamplerSpec{SampleSize: 100, Sketches: sketchSpecs}
+			p, err := newSamplerProcessor(
+				&flowCtx, 0 /* processorID */, spec, in, &distsqlpb.PostProcessSpec{}, outputs[i],
+			)
+			if err != nil {
+				t.Fatal(err)
+			}
+			p.Run(context.Background())
+		}
+		// Randomly interleave the output rows from the samplers into a single buffer.
+		samplerResults := NewRowBuffer(samplerOutTypes, nil /* rows */, RowBufferArgs{})
+		for len(outputs) > 0 {
+			i := rng.Intn(len(outputs))
+			row, meta := outputs[i].Next()
+			if meta != nil {
+				if meta.SamplerProgress == nil {
+					t.Fatalf("unexpected metadata: %v", meta)
+				}
+			} else if row == nil {
+				outputs = append(outputs[:i], outputs[i+1:]...)
+			} else {
+				samplerResults.Push(row, nil /* meta */)
+			}
+		}
+
+		// Now run the sample aggregator.
+		finalOut := NewRowBuffer([]types.T{}, nil /* rows*/, RowBufferArgs{})
+		spec := &distsqlpb.SampleAggregatorSpec{
+			SampleSize:       100,
+			Sketches:         sketchSpecs,
+			SampledColumnIDs: []sqlbase.ColumnID{100, 101},
+			TableID:          13,
+		}
+
+		agg, err := newSampleAggregator(
+			&flowCtx, 0 /* processorID */, spec, samplerResults, &distsqlpb.PostProcessSpec{}, finalOut,
+		)
 		if err != nil {
 			t.Fatal(err)
 		}
-		p.Run(context.Background())
-	}
-	// Randomly interleave the output rows from the samplers into a single buffer.
-	samplerResults := NewRowBuffer(samplerOutTypes, nil /* rows */, RowBufferArgs{})
-	for len(outputs) > 0 {
-		i := rng.Intn(len(outputs))
-		row, meta := outputs[i].Next()
-		if meta != nil {
-			if meta.SamplerProgress == nil {
-				t.Fatalf("unexpected metadata: %v", meta)
-			}
-		} else if row == nil {
-			outputs = append(outputs[:i], outputs[i+1:]...)
-		} else {
-			samplerResults.Push(row, nil /* meta */)
-		}
-	}
+		agg.Run(context.Background())
+		// Make sure there was no error.
+		finalOut.GetRowsNoMeta(t)
+		r := sqlutils.MakeSQLRunner(sqlDB)
 
-	// Now run the sample aggregator.
-	finalOut := NewRowBuffer([]types.T{}, nil /* rows*/, RowBufferArgs{})
-	spec := &distsqlpb.SampleAggregatorSpec{
-		SampleSize:       100,
-		Sketches:         sketchSpecs,
-		SampledColumnIDs: []sqlbase.ColumnID{100, 101},
-		TableID:          13,
-	}
-
-	agg, err := newSampleAggregator(
-		&flowCtx, 0 /* processorID */, spec, samplerResults, &distsqlpb.PostProcessSpec{}, finalOut,
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-	agg.Run(context.Background())
-	// Make sure there was no error.
-	finalOut.GetRowsNoMeta(t)
-	r := sqlutils.MakeSQLRunner(sqlDB)
-
-	rows := r.Query(t, `
+		rows := r.Query(t, `
 	  SELECT "tableID",
 					 "name",
 					 "columnIDs",
@@ -159,95 +166,104 @@ func TestSampleAggregator(t *testing.T) {
 					 histogram
 	  FROM system.table_statistics
   `)
-	defer rows.Close()
+		defer rows.Close()
 
-	type resultBucket struct {
-		numEq, numRange, upper int
-	}
+		type resultBucket struct {
+			numEq, numRange, upper int
+		}
 
-	type result struct {
-		tableID                            int
-		name, colIDs                       string
-		rowCount, distinctCount, nullCount int
-		buckets                            []resultBucket
-	}
+		type result struct {
+			tableID                            int
+			name, colIDs                       string
+			rowCount, distinctCount, nullCount int
+			buckets                            []resultBucket
+		}
 
-	expected := []result{
-		{
-			tableID:       13,
-			name:          "a",
-			colIDs:        "{100}",
-			rowCount:      11,
-			distinctCount: 3,
-			nullCount:     2,
-		},
-		{
-			tableID:       13,
-			name:          "<NULL>",
-			colIDs:        "{101}",
-			rowCount:      11,
-			distinctCount: 9,
-			nullCount:     1,
-			buckets: []resultBucket{
-				{numEq: 2, numRange: 0, upper: 1},
-				{numEq: 2, numRange: 1, upper: 3},
-				{numEq: 1, numRange: 1, upper: 5},
-				{numEq: 1, numRange: 2, upper: 8},
+		expected := []result{
+			{
+				tableID:       13,
+				name:          "a",
+				colIDs:        "{100}",
+				rowCount:      11,
+				distinctCount: 3,
+				nullCount:     2,
 			},
-		},
-	}
-
-	for _, exp := range expected {
-		if !rows.Next() {
-			t.Fatal("fewer rows than expected")
+			{
+				tableID:       13,
+				name:          "<NULL>",
+				colIDs:        "{101}",
+				rowCount:      11,
+				distinctCount: 9,
+				nullCount:     1,
+				buckets: []resultBucket{
+					{numEq: 2, numRange: 0, upper: 1},
+					{numEq: 2, numRange: 1, upper: 3},
+					{numEq: 1, numRange: 1, upper: 5},
+					{numEq: 1, numRange: 2, upper: 8},
+				},
+			},
 		}
 
-		var histData []byte
-		var name gosql.NullString
-		var r result
-		if err := rows.Scan(
-			&r.tableID, &name, &r.colIDs, &r.rowCount, &r.distinctCount, &r.nullCount, &histData,
-		); err != nil {
-			t.Fatal(err)
-		}
-		if name.Valid {
-			r.name = name.String
-		} else {
-			r.name = "<NULL>"
-		}
+		for _, exp := range expected {
+			if !rows.Next() {
+				t.Fatal("fewer rows than expected")
+			}
+			if expectOutOfMemory {
+				exp.buckets = nil
+			}
 
-		if len(histData) > 0 {
-			var h stats.HistogramData
-			if err := protoutil.Unmarshal(histData, &h); err != nil {
+			var histData []byte
+			var name gosql.NullString
+			var r result
+			if err := rows.Scan(
+				&r.tableID, &name, &r.colIDs, &r.rowCount, &r.distinctCount, &r.nullCount, &histData,
+			); err != nil {
 				t.Fatal(err)
 			}
-
-			for _, b := range h.Buckets {
-				ed, _, err := sqlbase.EncDatumFromBuffer(
-					types.Int, sqlbase.DatumEncoding_ASCENDING_KEY, b.UpperBound,
-				)
-				if err != nil {
-					t.Fatal(err)
-				}
-				var d sqlbase.DatumAlloc
-				if err := ed.EnsureDecoded(types.Int, &d); err != nil {
-					t.Fatal(err)
-				}
-				r.buckets = append(r.buckets, resultBucket{
-					numEq:    int(b.NumEq),
-					numRange: int(b.NumRange),
-					upper:    int(*ed.Datum.(*tree.DInt)),
-				})
+			if name.Valid {
+				r.name = name.String
+			} else {
+				r.name = "<NULL>"
 			}
-		} else if len(exp.buckets) > 0 {
-			t.Error("no histogram")
-		}
 
-		if !reflect.DeepEqual(exp, r) {
-			t.Errorf("Expected:\n  %v\ngot:\n  %v", exp, r)
+			if len(histData) > 0 {
+				var h stats.HistogramData
+				if err := protoutil.Unmarshal(histData, &h); err != nil {
+					t.Fatal(err)
+				}
+
+				for _, b := range h.Buckets {
+					ed, _, err := sqlbase.EncDatumFromBuffer(
+						types.Int, sqlbase.DatumEncoding_ASCENDING_KEY, b.UpperBound,
+					)
+					if err != nil {
+						t.Fatal(err)
+					}
+					var d sqlbase.DatumAlloc
+					if err := ed.EnsureDecoded(types.Int, &d); err != nil {
+						t.Fatal(err)
+					}
+					r.buckets = append(r.buckets, resultBucket{
+						numEq:    int(b.NumEq),
+						numRange: int(b.NumRange),
+						upper:    int(*ed.Datum.(*tree.DInt)),
+					})
+				}
+			} else if len(exp.buckets) > 0 {
+				t.Error("no histogram")
+			}
+
+			if !reflect.DeepEqual(exp, r) {
+				t.Errorf("Expected:\n  %v\ngot:\n  %v", exp, r)
+			}
+		}
+		if rows.Next() {
+			t.Fatal("more rows than expected")
 		}
 	}
-	if rows.Next() {
-		t.Fatal("more rows than expected")
-	}
+
+	runTest(0 /* memLimitBytes */, false /* expectOutOfMemory */)
+	runTest(1 /* memLimitBytes */, true /* expectOutOfMemory */)
+	runTest(20 /* memLimitBytes */, true /* expectOutOfMemory */)
+	runTest(20*1024 /* memLimitBytes */, false /* expectOutOfMemory */)
 }
