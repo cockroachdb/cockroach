e025492f6638900685e9cc37eee3ced9f09f5e86
Yahor Yuzefovich <yahor@cockroachlabs.com>
2019-08-22 21:02:04 -0700

    exec: add support for ON expression for LEFT SEMI and LEFT ANTI merge join
    This commit adds support for ON expression for LEFT SEMI and LEFT ANTI
    merge joins. This is achieved by setting up a side chain of two operators
    filterFeedOperator -> filterOperator which lives alongside the merge join
    but not on the same chain. Then, whenever a filter is present, we run
    the matched tuples, one at a time, from the left and right inputs through it.
    When the filter expression only references the columns from the left side, we
    can optimize the filtering to do the check once for every tuple in question
    from the left; however, when the filter references the columns from the right,
    we need to filter the full cross product of each left tuple with the full
    matching group of tuples from the right which gives us quadratic behavior.
    
    Release note: None

--- pkg/sql/distsqlrun/columnar_operators_test.go
+++ pkg/sql/distsqlrun/columnar_operators_test.go
@@ -256,10 +256,12 @@ func TestMergeJoinerAgainstProcessor(t *testing.T) {
 			anyOrder: true,
 		},
 		{
-			joinType: sqlbase.JoinType_LEFT_SEMI,
+			joinType:        sqlbase.JoinType_LEFT_SEMI,
+			onExprSupported: true,
 		},
 		{
-			joinType: sqlbase.JoinType_LEFT_ANTI,
+			joinType:        sqlbase.JoinType_LEFT_ANTI,
+			onExprSupported: true,
 		},
 	}
 
