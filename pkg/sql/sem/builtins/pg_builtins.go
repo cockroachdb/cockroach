b6d76ccee9480ec2fa32eb2448183c9159e668ac
Jordan Lewis <jordanthelewis@gmail.com>
2019-08-23 21:34:30 -0400

    builtins: add pg_my_temp_schema
    Release note (sql change): add the pg_my_temp_schema builtin for
    Postgres compatibility.

--- pkg/sql/sem/builtins/pg_builtins.go
+++ pkg/sql/sem/builtins/pg_builtins.go
@@ -624,6 +624,17 @@ var pgBuiltins = map[string]builtinDefinition{
 		makePGGetViewDef(tree.ArgTypes{{"view_oid", types.Oid}, {"pretty_bool", types.Bool}}),
 	),
 
+	"pg_my_temp_schema": makeBuiltin(defProps(),
+		tree.Overload{
+			Types:      tree.ArgTypes{},
+			ReturnType: tree.FixedReturnType(types.Oid),
+			Fn: func(_ *tree.EvalContext, _ tree.Datums) (tree.Datum, error) {
+				return tree.NewDOid(0), nil
+			},
+			Info: notUsableInfo,
+		},
+	),
+
 	// TODO(bram): Make sure the reported type is correct for tuples. See #25523.
 	"pg_typeof": makeBuiltin(tree.FunctionProperties{NullableArgs: true},
 		tree.Overload{
