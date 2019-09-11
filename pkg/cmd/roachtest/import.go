cockroachdb/cockroach

commit 160f6da18e0bf605338e5284bf2ba28c8f620ac7

roachtest: bump up index KV buffer size in TPCH import test
This table has 8 secondary indexes so the default buffer size, which is smaller to make workload's concurrent imports of many tables work, isn't doing it any favors. Since this test only runs one IMPORT, we can let it use 4x more buffer than usual.

Release note: none

pkg/cmd/roachtest/import.go
@@ -167,6 +167,7 @@ func registerImportTPCH(r *testRegistry) {
 					t.WorkerStatus(`running import`)
 					defer t.WorkerStatus()
 					_, err := conn.Exec(`
+					SET CLUSTER SETTING kv.bulk_ingest.max_index_buffer_size = '2gb';
 				IMPORT TABLE csv.lineitem
 				CREATE USING 'gs://cockroach-fixtures/tpch-csv/schema/lineitem.sql'
 				CSV DATA (

commit c2aa390036fc578e2d27ec6c59217c9d73f0ecda

roachtest: fix import/tpch roachtest for release-19.1
See https://github.com/cockroachdb/cockroach/issues/39113#issuecomment-530491432.

This was broken by #40576.

Release note: None

pkg/cmd/roachtest/import.go
@@ -13,6 +13,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/util/retry"
@@ -136,6 +137,11 @@ func registerImportTPCH(r *testRegistry) {
 				`); err != nil {
 					t.Fatal(err)
 				}
+				if _, err := conn.Exec(
+					`SET CLUSTER SETTING kv.bulk_ingest.max_index_buffer_size = '2gb'`,
+				); err != nil && !strings.Contains(err.Error(), "unknown cluster setting") {
+					t.Fatal(err)
+				}
 				// Wait for all nodes to be ready.
 				if err := retry.ForDuration(time.Second*30, func() error {
 					var nodes int
@@ -173,7 +179,6 @@ func registerImportTPCH(r *testRegistry) {
 					t.WorkerStatus(`running import`)
 					defer t.WorkerStatus()
 					_, err := conn.Exec(`
-					SET CLUSTER SETTING kv.bulk_ingest.max_index_buffer_size = '2gb';
 				IMPORT TABLE csv.lineitem
 				CREATE USING 'gs://cockroach-fixtures/tpch-csv/schema/lineitem.sql'
 				CSV DATA (