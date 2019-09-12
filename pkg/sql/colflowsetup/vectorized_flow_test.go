496610f01bdd8673e470ef639297b7095861df95
Yahor Yuzefovich <yahor@cockroachlabs.com>
2019-09-12 01:39:34 -0700

    WIP move colrpc from colexec to colflowsetup
    Release note: None

--- pkg/sql/colflowsetup/vectorized_flow_test.go
+++ pkg/sql/colflowsetup/vectorized_flow_test.go
@@ -19,7 +19,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql/colexec"
-	"github.com/cockroachdb/cockroach/pkg/sql/colexec/colrpc"
+	"github.com/cockroachdb/cockroach/pkg/sql/colflowsetup/colrpc"
 	"github.com/cockroachdb/cockroach/pkg/sql/execinfra"
 	"github.com/cockroachdb/cockroach/pkg/sql/execinfrapb"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
