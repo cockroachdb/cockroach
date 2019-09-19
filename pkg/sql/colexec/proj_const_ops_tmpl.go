7dc9082f6dae949ca30fd4f38157a0b6318727c6
Yahor Yuzefovich <yahor@cockroachlabs.com>
2019-09-19 14:49:22 -0700

    colexec: refactor the template for projection operators
    The old version of the template for projection operators was written
    before we gained more familiarity with the templates in general, so
    it was a huge single string. This commit refactor that huge single
    string template into a newer version.
    
    Besides giving us a benefit of taking a step towards unifying our
    templates, this will allow us to customize the behavior when nulls
    are present with a lot less effort.
    
    Release justification: Category 1: Non-production code changes.
    
    Release note: None

---
+++ pkg/sql/colexec/proj_const_ops_tmpl.go
@@ -0,0 +1,132 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+// {{/*
+// +build execgen_template
+//
+// This file is the execgen template for a projection operator with one
+// constant argument. It is separated from proj_const_ops_gen_tmpl.go because
+// this template is also used by other template files.
+//
+// */}}
+
+package colexec
+
+import (
+	"bytes"
+	"context"
+	"math"
+
+	"github.com/cockroachdb/apd"
+	"github.com/cockroachdb/cockroach/pkg/col/coldata"
+	"github.com/cockroachdb/cockroach/pkg/col/coltypes"
+	"github.com/cockroachdb/cockroach/pkg/sql/colexec/execerror"
+	// {{/*
+	"github.com/cockroachdb/cockroach/pkg/sql/colexec/execgen"
+	// */}}
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+)
+
+// {{/*
+// Declarations to make the template compile properly.
+
+// Dummy import to pull in "bytes" package.
+var _ bytes.Buffer
+
+// Dummy import to pull in "apd" package.
+var _ apd.Decimal
+
+// Dummy import to pull in "tree" package.
+var _ tree.Datum
+
+// Dummy import to pull in "math" package.
+var _ = math.MaxInt64
+
+// _ASSIGN is the template function for assigning the first input to the result
+// of computation an operation on the second and the third inputs.
+func _ASSIGN(_, _, _ interface{}) {
+	execerror.VectorizedInternalPanic("")
+}
+
+// _RET_UNSAFEGET is the template function that will be replaced by
+// "execgen.UNSAFEGET" which uses _RET_TYP.
+func _RET_UNSAFEGET(_, _ interface{}) interface{} {
+	execerror.VectorizedInternalPanic("")
+}
+
+// */}}
+
+// {{define "projConstOp" }}
+
+type _OP_CONST_NAME struct {
+	projConstOpBase
+	// {{ if _IS_CONST_LEFT }}
+	constArg _L_GO_TYPE
+	// {{ else }}
+	constArg _R_GO_TYPE
+	// {{ end }}
+}
+
+func (p _OP_CONST_NAME) EstimateStaticMemoryUsage() int {
+	return EstimateBatchSizeBytes([]coltypes.T{coltypes._RET_TYP}, coldata.BatchSize)
+}
+
+func (p _OP_CONST_NAME) Next(ctx context.Context) coldata.Batch {
+	batch := p.input.Next(ctx)
+	n := batch.Length()
+	if p.outputIdx == batch.Width() {
+		batch.AppendCol(coltypes._RET_TYP)
+	}
+	if n == 0 {
+		return batch
+	}
+	vec := batch.ColVec(p.colIdx)
+	// {{if _IS_CONST_LEFT}}
+	col := vec._R_TYP()
+	// {{else}}
+	col := vec._L_TYP()
+	// {{end}}
+	projVec := batch.ColVec(p.outputIdx)
+	projCol := projVec._RET_TYP()
+	if sel := batch.Selection(); sel != nil {
+		sel = sel[:n]
+		for _, i := range sel {
+			arg := execgen.UNSAFEGET(col, int(i))
+			// {{if _IS_CONST_LEFT}}
+			_ASSIGN("projCol[i]", "p.constArg", "arg")
+			// {{else}}
+			_ASSIGN("projCol[i]", "arg", "p.constArg")
+			// {{end}}
+		}
+	} else {
+		col = execgen.SLICE(col, 0, int(n))
+		colLen := execgen.LEN(col)
+		_ = _RET_UNSAFEGET(projCol, colLen-1)
+		for execgen.RANGE(i, col) {
+			arg := execgen.UNSAFEGET(col, i)
+			// {{if _IS_CONST_LEFT}}
+			_ASSIGN("projCol[i]", "p.constArg", "arg")
+			// {{else}}
+			_ASSIGN("projCol[i]", "arg", "p.constArg")
+			// {{end}}
+		}
+	}
+	if vec.Nulls().MaybeHasNulls() {
+		nulls := vec.Nulls().Copy()
+		projVec.SetNulls(&nulls)
+	}
+	return batch
+}
+
+func (p _OP_CONST_NAME) Init() {
+	p.input.Init()
+}
+
+// {{end}}
