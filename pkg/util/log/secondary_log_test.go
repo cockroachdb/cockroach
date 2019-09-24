5bd9fa19f8e4e9191dda37976646becd37ef4008
Raphael 'kena' Poss <knz@thaumogen.net>
2019-09-24 10:00:14 +0200

    util/log: fix GC of secondary loggers
    Release justification: bug fix
    
    Release note (bug fix): CockroachDB will now properly remove excess
    secondary log files (SQL audit logging, statement execution logging,
    and RocksDB events).

--- pkg/util/log/secondary_log_test.go
+++ pkg/util/log/secondary_log_test.go
@@ -15,7 +15,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
+	"sync/atomic"
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
@@ -111,5 +113,111 @@ func TestRedirectStderrWithSecondaryLoggersActive(t *testing.T) {
 	}
 	if strings.Contains(string(contents2), stderrText) {
 		t.Errorf("secondary log erronously contains stderr text\n%s", contents2)
+	}
+}
+
+func TestSecondaryGC(t *testing.T) {
+	s := ScopeWithoutShowLogs(t)
+	defer s.Close(t)
+
+	logging.mu.Lock()
+	logging.disableDaemons = true
+	defer func(previous bool) {
+		logging.mu.Lock()
+		logging.disableDaemons = previous
+		logging.mu.Unlock()
+	}(logging.disableDaemons)
+	logging.mu.Unlock()
+
+	setFlags()
+
+	const newLogFiles = 20
+
+	// Prevent writes to stderr from being sent to log files which would screw up
+	// the expected number of log file calculation below.
+	logging.noStderrRedirect = true
+
+	// Ensure the main log has at least one entry. Otherwise, the test
+	// log scope will erase the temporary directory, preventing further
+	// investigation.
+	Infof(context.Background(), "hello world")
+
+	tmpDir, err := ioutil.TempDir(logging.logDir.String(), "gctest")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if !t.Failed() {
+			// If the test failed, we'll want to keep the artifacts for
+			// troubleshooting.
+			_ = os.RemoveAll(tmpDir)
+		}
+	}()
+	tmpDirName := DirName{name: tmpDir}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	l := NewSecondaryLogger(ctx, &tmpDirName, "woo", true /*enableGc*/, false /*syncWrites*/, true /*msgCount*/)
+
+	// Create 1 log file to figure out its size.
+	l.Logf(context.Background(), "0")
+	Flush()
+
+	allFilesOriginal, err := l.logger.listLogFiles()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if e, a := 1, len(allFilesOriginal); e != a {
+		t.Fatalf("expected %d files, but found %d", e, a)
+	}
+	dir, err := l.logger.logDir.get()
+	if err != nil {
+		t.Fatal(err)
+	}
+	f, err := os.Open(filepath.Join(dir, allFilesOriginal[0].Name))
+	if err != nil {
+		t.Fatal(err)
+	}
+	stat, err := f.Stat()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		t.Fatal(err)
+	}
+	// logFileSize is the size of the first log file we wrote to.
+	logFileSize := stat.Size()
+	const expectedFilesAfterGC = 2
+	// Pick a max total size that's between 2 and 3 log files in size.
+	maxTotalLogFileSize := logFileSize*expectedFilesAfterGC + logFileSize // 2
+
+	defer func(previous int64) { LogFileMaxSize = previous }(LogFileMaxSize)
+	LogFileMaxSize = 1 // ensure rotation on every log write
+	defer func(previous int64) {
+		atomic.StoreInt64(&LogFilesCombinedMaxSize, previous)
+	}(LogFilesCombinedMaxSize)
+	atomic.StoreInt64(&LogFilesCombinedMaxSize, maxTotalLogFileSize)
+
+	for i := 1; i < newLogFiles; i++ {
+		l.Logf(context.Background(), "%d", i)
+		Flush()
+	}
+
+	allFilesBefore, err := l.logger.listLogFiles()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if e, a := newLogFiles, len(allFilesBefore); e != a {
+		t.Fatalf("expected %d files, but found %d", e, a)
+	}
+
+	l.logger.gcOldFiles()
+
+	allFilesAfter, err := l.logger.listLogFiles()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if e, a := expectedFilesAfterGC, len(allFilesAfter); e != a {
+		t.Fatalf("expected %d files, but found %d", e, a)
 	}
 }
