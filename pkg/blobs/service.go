e7999cafde998b7cf6bb010efb14bc198cc34cad
Georgia Hong <georgiah@cockroachlabs.com>
2019-09-25 15:37:54 -0400

    storage: Add file sharing service between nodes
    The blob service created here is an inter-node file
    exchanging service that (currently) supports 3 major
    functionalities:
    - Reading a remote node's file
    - Write to a remote node's file system
    - Listing files from a remote node
    
    The intended use looks like:
    ```
    if blobClient.GetNodeID() != destinationNodeID {
        // Returns io.ReadCloser, error
        return blobClient.ReadFile(ctx, destinationNodeID, filePath)
    }
    // Otherwise use existing code.
    ```
    
    This allows us to `ReadFile`, `WriteFile`, and `ListFiles`
    from any node, given the nodeID and filepath of a file that
    is stored locally on any node. This service is to be used
    in `ExternalStorage`, for nodelocal file access, specified
    in a URI such as: `nodelocal://<nodeID>/file/path`.
    
    Release note: None

---
+++ pkg/blobs/service.go
@@ -0,0 +1,133 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+/*
+Package blobs contains a gRPC service to be used for remote file access.
+It is used for bulk file reads and writes to files on any CockroachDB node.
+
+Each node will run a blob service, which serves the file access for files on
+that node. Each node will also have a blob client, which uses the nodedialer
+to connect to another node's blob service, and access its files. The blob client
+is the point of entry to this service and it supports the `BlobClient` interface,
+which includes the following functionalities:
+- ReadFile
+- WriteFile
+- List
+- Delete
+- Stat
+for any file on either the local node, or a remote node.
+*/
+package blobs
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/cockroachdb/cockroach/pkg/blobs/blobspb"
+	"github.com/pkg/errors"
+)
+
+// Service implements the gRPC BlobService which exchanges bulk files between different nodes.
+type Service struct {
+	base string
+}
+
+var _ blobspb.BlobServer = &Service{}
+
+// NewBlobService instantiates a blob service server.
+func NewBlobService(basePath string) (*Service, error) {
+	absPath, err := filepath.Abs(basePath)
+	if err != nil {
+		return nil, errors.Wrap(err, "externalIODir should be an absolute path")
+	}
+	return &Service{base: absPath}, nil
+}
+
+func (s *Service) prependExternalIODir(path string) (string, error) {
+	localBase := filepath.Join(s.base, path)
+	// Make sure we didn't ../ our way back out.
+	if !strings.HasPrefix(localBase, s.base) {
+		return "", errors.Errorf("local file access to paths outside of external-io-dir is not allowed")
+	}
+	return localBase, nil
+}
+
+// GetBlob implements the gRPC service.
+func (s *Service) GetBlob(
+	ctx context.Context, req *blobspb.GetRequest,
+) (*blobspb.GetResponse, error) {
+	localFile, err := s.prependExternalIODir(req.Filename)
+	if err != nil {
+		return nil, err
+	}
+	payload, err := ioutil.ReadFile(localFile)
+	return &blobspb.GetResponse{Payload: payload}, err
+}
+
+// PutBlob implements the gRPC service.
+func (s *Service) PutBlob(
+	ctx context.Context, req *blobspb.PutRequest,
+) (*blobspb.PutResponse, error) {
+	localFile, err := s.prependExternalIODir(req.Filename)
+	if err != nil {
+		return nil, err
+	}
+	return &blobspb.PutResponse{}, writeFileLocally(localFile, bytes.NewReader(req.Payload))
+}
+
+// List implements the gRPC service.
+func (s *Service) List(
+	ctx context.Context, req *blobspb.GlobRequest,
+) (*blobspb.GlobResponse, error) {
+	if req.Pattern == "" {
+		return nil, errors.New("pattern cannot be empty")
+	}
+	fullPath, err := s.prependExternalIODir(req.Pattern)
+	if err != nil {
+		return nil, err
+	}
+	matches, err := filepath.Glob(fullPath)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to match pattern provided")
+	}
+	return &blobspb.GlobResponse{Files: matches}, err
+}
+
+// Delete implements the gRPC service.
+func (s *Service) Delete(
+	ctx context.Context, req *blobspb.DeleteRequest,
+) (*blobspb.DeleteResponse, error) {
+	fullPath, err := s.prependExternalIODir(req.Filename)
+	if err != nil {
+		return nil, err
+	}
+	return &blobspb.DeleteResponse{}, os.Remove(fullPath)
+}
+
+// Stat implements the gRPC service.
+func (s *Service) Stat(ctx context.Context, req *blobspb.StatRequest) (*blobspb.BlobStat, error) {
+	fullPath, err := s.prependExternalIODir(req.Filename)
+	if err != nil {
+		return nil, err
+	}
+
+	fi, err := os.Stat(fullPath)
+	if err != nil {
+		return nil, err
+	}
+	if fi.IsDir() {
+		return nil, errors.Errorf("expected a file but %q is a directory", fi.Name())
+	}
+	return &blobspb.BlobStat{Filesize: fi.Size()}, nil
+}
