cockroachdb/cockroach

commit 3dfd6cb964dfeb84c46f66b6ad7b98970dfd4d95

sql: refactor dates to be fully PG compatible
Previously dates were an int64 number of days from 1970-01-01. Functions
and operators needing to act on a date could manipulate the value of
the date without any overflow checks, causing multiple bugs:

- The postgres binary wire protocol uses an int32 number of days since
2000-01-01, thus it was possible to have dates in cockroach outside
of the expressible range over the wire. We were blindly converting the
int64 to an int32, possibly discarding data.
- Adding dates to ints was done by normal addition. However this could
overflow. Our normal int + int path uses overflow-checked addition,
but dates were not subject to such checks.
- Converting dates to a text representation (for sending over pgwire or
our distsql serialization) converted the number of days to a time.Time
using the standard library time.Date method, which (more-or-less) stores
time as an int64 number of nanos. A very high year could overflow the
nanosecond part causing it to silently by incorrect.
- The infinity dates were not treated as infinity, but converted to an
actual date.

This change adds a new pgdate.Date type. It internally represented as
an int32 number of days since 2000-01-01, because that's exactly what
is needed by the postgres wire protocol. It doesn't export any internal
properties, and instead forces users of it to use its API, which is
guaranteed to be safe. The API does all overflow and bounds checking,
and correctly handles the infinity dates. This fixes the above bugs
since it's now not possible to mistakenly misuse Dates.

When reading from any existing on-disk data, dates outside of the postgres
bounds are converted to +/- infinity. This presents a problem since
datums need to roundtrip to the same on-disk encoding so that old index
entries can be deleted. In order to round trip correctly, the original
on-disk integer is saved and used during serialization if it exists.

Fixes #36557

pkg/sql/pgwire/types.go
@@ -37,7 +37,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/ipaddr"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/timeofday"
-	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/lib/pq/oid"
 	"github.com/pkg/errors"
 )
@@ -98,8 +97,6 @@ func mapResultOid(o oid.Oid) oid.Oid {
 	return o
 }
 
-const secondsInDay = 24 * 60 * 60
-
 func (b *writeBuffer) writeTextDatum(
 	ctx context.Context, d tree.Datum, conv sessiondata.DataConversionConfig,
 ) {
@@ -154,11 +151,9 @@ func (b *writeBuffer) writeTextDatum(
 		b.writeLengthPrefixedString(v.Contents)
 
 	case *tree.DDate:
-		t := timeutil.Unix(int64(*v)*secondsInDay, 0)
-		// Start at offset 4 because `putInt32` clobbers the first 4 bytes.
-		s := formatDate(t, nil, b.putbuf[4:4])
+		s := v.Date.String()
 		b.putInt32(int32(len(s)))
-		b.write(s)
+		b.write([]byte(s))
 
 	case *tree.DTime:
 		// Start at offset 4 because `putInt32` clobbers the first 4 bytes.
@@ -437,7 +432,7 @@ func (b *writeBuffer) writeBinaryDatum(
 
 	case *tree.DDate:
 		b.putInt32(4)
-		b.putInt32(dateToPgBinary(v))
+		b.putInt32(v.PGEpochDays())
 
 	case *tree.DTime:
 		b.putInt32(8)
@@ -523,10 +518,6 @@ func formatTs(t time.Time, offset *time.Location, tmp []byte) (b []byte) {
 	return formatTsWithFormat(format, t, offset, tmp)
 }
 
-func formatDate(t time.Time, offset *time.Location, tmp []byte) []byte {
-	return formatTsWithFormat(pgDateFormat, t, offset, tmp)
-}
-
 // formatTsWithFormat formats t with an optional offset into a format
 // lib/pq understands, appending to the provided tmp buffer and
 // reallocating if needed. The function will then return the resulting
@@ -563,11 +554,4 @@ func timeToPgBinary(t time.Time, offset *time.Location) int64 {
 		t = t.UTC()
 	}
 	return duration.DiffMicros(t, pgwirebase.PGEpochJDate)
-}
-
-// dateToPgBinary calculates the Postgres binary format for a date. The date is
-// represented as the number of days between the given date and Jan 1, 2000
-// (dubbed the PGEpochJDate), stored within an int32.
-func dateToPgBinary(d *tree.DDate) int32 {
-	return int32(*d) - pgwirebase.PGEpochJDateFromUnix
 }

pkg/ccl/changefeedccl/avro.go
@@ -20,7 +20,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/timeofday"
-	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/linkedin/goavro"
 	"github.com/pkg/errors"
 )
@@ -196,15 +195,17 @@ func columnDescToAvroSchema(colDesc *sqlbase.ColumnDescriptor) (*avroSchemaField
 			LogicalType: `date`,
 		}
 		schema.encodeFn = func(d tree.Datum) (interface{}, error) {
-			days := time.Duration(*d.(*tree.DDate)) * 24 * time.Hour
+			date := *d.(*tree.DDate)
+			if !date.IsFinite() {
+				return nil, errors.Errorf(
+					`column %s: infinite date not yet supported with avro`, colDesc.Name)
+			}
 			// The avro library requires us to return this as a time.Time.
-			t := timeutil.Unix(0, days.Nanoseconds()).UTC()
-			return t, nil
+			return date.ToTime()
 		}
 		schema.decodeFn = func(x interface{}) (tree.Datum, error) {
 			// The avro library hands this back as a time.Time.
-			numDays := x.(time.Time).UnixNano() / int64(24*time.Hour)
-			return tree.NewDDate(tree.DDate(numDays)), nil
+			return tree.NewDDateFromTime(x.(time.Time))
 		}
 	case types.TimeFamily:
 		avroType = avroLogicalType{

pkg/cli/dump.go
@@ -659,7 +659,10 @@ func dumpTableData(w io.Writer, conn *sqlConn, clusterTS string, bmd basicMetada
 				case time.Time:
 					switch ct := md.columnTypes[cols[si]]; ct.Family() {
 					case types.DateFamily:
-						d = tree.NewDDateFromTime(t, time.UTC)
+						d, err = tree.NewDDateFromTime(t)
+						if err != nil {
+							return err
+						}
 					case types.TimeFamily:
 						// pq awkwardly represents TIME as a time.Time with date 0000-01-01.
 						d = tree.MakeDTime(timeofday.FromTime(t))

pkg/cli/dump_test.go
@@ -37,6 +37,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil/pgdate"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 	"github.com/spf13/pflag"
 )
@@ -235,7 +236,7 @@ func TestDumpRandom(t *testing.T) {
 			// Generate a random number of random inserts.
 			i := rnd.Int63()
 			f := rnd.Float64()
-			d := timeutil.Unix(0, rnd.Int63()).Round(time.Hour * 24)
+			d, _ := pgdate.MakeCompatibleDateFromDisk(rnd.Int63n(10000)).ToTime()
 			m := timeutil.Unix(0, rnd.Int63()).Round(time.Microsecond)
 			sign := 1 - rnd.Int63n(2)*2
 			dur := duration.MakeDuration(sign*rnd.Int63(), sign*rnd.Int63n(1000), sign*rnd.Int63n(1000))

pkg/cmd/generate-binary/main.go
@@ -312,8 +312,18 @@ var inputs = map[string][]string{
 		"1999-12-30",
 		"1996-02-29",
 		"0001-01-01",
+		"0001-12-31 BC",
 		"0001-01-01 BC",
 		"3592-12-31 BC",
+		"4713-01-01 BC",
+		"4714-11-24 BC",
+		"5874897-12-31",
+		"2000-01-01",
+		"2000-01-02",
+		"1999-12-31",
+		"infinity",
+		"-infinity",
+		"epoch",
 	},
 
 	"'%s'::time": {

pkg/sql/distsqlrun/materializer.go
@@ -25,6 +25,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil/pgdate"
 	"github.com/lib/pq/oid"
 )
 
@@ -164,7 +165,7 @@ func (m *materializer) Next() (sqlbase.EncDatumRow, *ProducerMetadata) {
 			case types.DecimalFamily:
 				m.row[outIdx].Datum = m.da.NewDDecimal(tree.DDecimal{Decimal: col.Decimal()[rowIdx]})
 			case types.DateFamily:
-				m.row[outIdx].Datum = tree.NewDDate(tree.DDate(col.Int64()[rowIdx]))
+				m.row[outIdx].Datum = tree.NewDDate(pgdate.MakeCompatibleDateFromDisk(col.Int64()[rowIdx]))
 			case types.StringFamily:
 				b := col.Bytes()[rowIdx]
 				if ct.Oid() == oid.T_name {

pkg/sql/distsqlrun/materializer_test.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil/pgdate"
 )
 
 func TestColumnarizeMaterialize(t *testing.T) {
@@ -110,7 +111,7 @@ func TestMaterializeTypes(t *testing.T) {
 		sqlbase.EncDatum{Datum: tree.NewDInt(tree.DInt(31))},
 		sqlbase.EncDatum{Datum: tree.NewDFloat(37.41)},
 		sqlbase.EncDatum{Datum: &tree.DDecimal{Decimal: *apd.New(43, 47)}},
-		sqlbase.EncDatum{Datum: tree.NewDDate(53)},
+		sqlbase.EncDatum{Datum: tree.NewDDate(pgdate.MakeCompatibleDateFromDisk(53))},
 		sqlbase.EncDatum{Datum: tree.NewDString("hello")},
 		sqlbase.EncDatum{Datum: tree.NewDBytes("ciao")},
 		sqlbase.EncDatum{Datum: tree.NewDName("aloha")},

pkg/sql/exec/selection_ops_test.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	semtypes "github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil/pgdate"
 )
 
 func TestSelLTInt64Int64ConstOp(t *testing.T) {
@@ -70,7 +71,7 @@ func TestGetSelectionConstOperator(t *testing.T) {
 	var input Operator
 	colIdx := 3
 	constVal := int64(31)
-	constArg := tree.NewDDate(tree.DDate(constVal))
+	constArg := tree.NewDDate(pgdate.MakeCompatibleDateFromDisk(constVal))
 	op, err := GetSelectionConstOperator(semtypes.Date, cmpOp, input, colIdx, constArg)
 	if err != nil {
 		t.Error(err)

commit 0783b61f0d6841f10fd6c8345050189f85b38613

sql/pgwire: avoid new memory allocation in writeTextDatum(Date)
3dfd6cb introduced a performance regression in the speed of encoding
Date datums to their text representation. This commit partially avoids
the regression.

```
name              old time/op  new time/op  delta
WriteTextDate-16   446ns ± 4%   318ns ± 1%  -28.75%  (p=0.000 n=10+9)
```

Release justification: Avoids performance regression.

Release note: None

pkg/sql/pgwire/types.go
@@ -141,9 +141,8 @@ func (b *writeBuffer) writeTextDatum(
 		b.writeLengthPrefixedString(v.Contents)
 
 	case *tree.DDate:
-		s := v.Date.String()
-		b.putInt32(int32(len(s)))
-		b.write([]byte(s))
+		b.textFormatter.FormatNode(v)
+		b.writeFromFmtCtx(b.textFormatter)
 
 	case *tree.DTime:
 		// Start at offset 4 because `putInt32` clobbers the first 4 bytes.