afe0c74655390c683530afd35fcfbc69f684436f
Andrew Werner <ajwerner@cockroachlabs.com>
2019-10-07 10:54:32 -0400

    roachprod/vm/aws: use one az for non-geo clusters
    In #40931 we changed the way that az flags control vm placement.
    This was a good change and greatly improved the usability of roachprod on AWS
    for geo-distributed clusters. A side-effect of this change was that vms for
    non-geo-distributed clusters became spread out over all of the AZs in a region.
    
    Having a workload running in a different AZ than a cockroach cluster can have
    large performance implications which we observed in roachtest. This PR ensures
    that when we create non-geo clusters we always put all of the nodes in the same
    AZ.
    
    Release justification: non-production code changes.
    
    Release note: None

--- pkg/cmd/roachprod/vm/aws/aws.go
+++ pkg/cmd/roachprod/vm/aws/aws.go
@@ -216,27 +216,33 @@ func (p *Provider) Create(names []string, opts vm.CreateOpts) error {
 		return errors.Errorf("Please specify a valid region.")
 	}
 
-	// Only use one region if we're not creating a distributed cluster.
+	var zones []string // contains an az corresponding to each entry in names
+	// Only use one zone in one region if we're not creating a distributed cluster.
 	if !opts.GeoDistributed {
-		regions = []string{regions[0]}
-	}
-
-	// Distribute the nodes amongst availability zones.
-	zonesPerRegion := len(names) / len(regions)
-	leftover := len(names) % len(regions)
-	var zones []string
-	for i, region := range regions {
-		regionZones, err := p.regionZones(region, p.opts.CreateZones)
+		regionZones, err := p.regionZones(regions[0], p.opts.CreateZones)
 		if err != nil {
 			return err
 		}
-		totalZonesPerRegion := zonesPerRegion
-		if leftover > i {
-			totalZonesPerRegion++
+		for range names {
+			zones = append(zones, regionZones[0])
 		}
-		for j := 0; j < totalZonesPerRegion; j++ {
-			zoneIndex := j % len(regionZones)
-			zones = append(zones, regionZones[zoneIndex])
+	} else {
+		// Distribute the nodes amongst availability zones if geo distributed.
+		zonesPerRegion := len(names) / len(regions)
+		leftover := len(names) % len(regions)
+		for i, region := range regions {
+			regionZones, err := p.regionZones(region, p.opts.CreateZones)
+			if err != nil {
+				return err
+			}
+			totalZonesPerRegion := zonesPerRegion
+			if leftover > i {
+				totalZonesPerRegion++
+			}
+			for j := 0; j < totalZonesPerRegion; j++ {
+				zoneIndex := j % len(regionZones)
+				zones = append(zones, regionZones[zoneIndex])
+			}
 		}
 	}
 	var g errgroup.Group
