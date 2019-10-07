cockroachdb/cockroach

commit ee48ea7d0f3e0726b96c3e5b789c5b70841952d5

sql: record total transaction time
This commit adds the collection of a few things about the transactions
(namely, average transaction time, whether it was committed, and whether
it was implicit). This information is stored in a virtual in-memory table.

Additionally, p90 and p99 of per-application average transaction time are
displayed as two new dashboards on SQL metrics page.

Release note (admin ui change): metrics about p90 and p99 percentiles of
per-application average transaction time are now exposed in the UI.

pkg/sql/conn_executor.go
@@ -305,6 +305,8 @@ func makeMetrics(internal bool) Metrics {
 				6*metricsSampleInterval),
 			SQLServiceLatency: metric.NewLatency(getMetricMeta(MetaSQLServiceLatency, internal),
 				6*metricsSampleInterval),
+			SQLTxnLatency: metric.NewLatency(getMetricMeta(MetaSQLTxnLatency, internal),
+				6*metricsSampleInterval),
 
 			TxnAbortCount: metric.NewCounter(getMetricMeta(MetaTxnAbort, internal)),
 			FailureCount:  metric.NewCounter(getMetricMeta(MetaFailure, internal)),
@@ -328,11 +330,11 @@ func (s *Server) Start(ctx context.Context, stopper *stop.Stopper) {
 			}
 		}
 	})
-	s.PeriodicallyClearStmtStats(ctx, stopper)
+	s.PeriodicallyClearSQLStats(ctx, stopper)
 }
 
-// ResetStatementStats resets the executor's collected statement statistics.
-func (s *Server) ResetStatementStats(ctx context.Context) {
+// ResetSQLStats resets the executor's collected sql statistics.
+func (s *Server) ResetSQLStats(ctx context.Context) {
 	s.sqlStats.resetStats(ctx)
 }
 
@@ -588,6 +590,7 @@ func (s *Server) newConnExecutor(
 
 	ex.sessionTracing.ex = ex
 	ex.transitionCtx.sessionTracing = &ex.sessionTracing
+	ex.statsCollector = ex.newStatsCollector()
 	ex.initPlanner(ctx, &ex.planner)
 
 	return ex, nil
@@ -650,18 +653,18 @@ func (s *Server) newConnExecutorWithTxn(
 	return ex, nil
 }
 
-var maxStmtStatReset = settings.RegisterNonNegativeDurationSetting(
+var maxSQLStatReset = settings.RegisterNonNegativeDurationSetting(
 	"diagnostics.forced_stat_reset.interval",
 	"interval after which pending diagnostics statistics should be discarded even if not reported",
 	time.Hour*2, // 2 x diagnosticReportFrequency
 )
 
-// PeriodicallyClearStmtStats runs a loop to ensure that sql stats are reset.
+// PeriodicallyClearSQLStats runs a loop to ensure that sql stats are reset.
 // Usually we expect those stats to be reset by diagnostics reporting, after it
 // generates its reports. However if the diagnostics loop crashes and stops
 // resetting stats, this loop ensures stats do not accumulate beyond a
 // the diagnostics.forced_stat_reset.interval limit.
-func (s *Server) PeriodicallyClearStmtStats(ctx context.Context, stopper *stop.Stopper) {
+func (s *Server) PeriodicallyClearSQLStats(ctx context.Context, stopper *stop.Stopper) {
 	stopper.RunWorker(ctx, func(ctx context.Context) {
 		var timer timeutil.Timer
 		for {
@@ -670,10 +673,10 @@ func (s *Server) PeriodicallyClearStmtStats(ctx context.Context, stopper *stop.S
 			last := s.sqlStats.lastReset
 			s.sqlStats.Unlock()
 
-			next := last.Add(maxStmtStatReset.Get(&s.cfg.Settings.SV))
+			next := last.Add(maxSQLStatReset.Get(&s.cfg.Settings.SV))
 			wait := next.Sub(timeutil.Now())
 			if wait < 0 {
-				s.ResetStatementStats(ctx)
+				s.ResetSQLStats(ctx)
 			} else {
 				timer.Reset(wait)
 				select {
@@ -732,10 +735,15 @@ func (ex *connExecutor) closeWrapper(ctx context.Context, recovered interface{})
 func (ex *connExecutor) close(ctx context.Context, closeType closeType) {
 	ex.sessionEventf(ctx, "finishing connExecutor")
 
+	ev := noEvent
+	if _, noTxn := ex.machine.CurState().(stateNoTxn); !noTxn {
+		ev = txnAborted
+	}
+
 	if closeType == normalClose {
 		// We'll cleanup the SQL txn by creating a non-retriable (commit:true) event.
 		// This event is guaranteed to be accepted in every state.
-		ev := eventNonRetriableErr{IsCommit: fsm.FromBool(true)}
+		ev := eventNonRetriableErr{IsCommit: fsm.True}
 		payload := eventNonRetriableErrPayload{err: pgerror.Newf(pgcode.AdminShutdown,
 			"connExecutor closing")}
 		if err := ex.machine.ApplyWithPayload(ctx, ev, payload); err != nil {
@@ -745,7 +753,7 @@ func (ex *connExecutor) close(ctx context.Context, closeType closeType) {
 		ex.state.finishExternalTxn()
 	}
 
-	if err := ex.resetExtraTxnState(ctx, ex.server.dbCache); err != nil {
+	if err := ex.resetExtraTxnState(ctx, ex.server.dbCache, ev); err != nil {
 		log.Warningf(ctx, "error while cleaning up connExecutor: %s", err)
 	}
 
@@ -872,6 +880,11 @@ type connExecutor struct {
 		// txnRewindPos is advanced. Prepared statements are shared between the two
 		// collections, but these collections are periodically reconciled.
 		prepStmtsNamespaceAtTxnRewindPos prepStmtNamespace
+
+		// onTxnFinish (if non-nil) will be called when txn is finished (either
+		// committed or aborted). It is set when txn is started but can remain
+		// unset when txn is executed within another higher-level txn.
+		onTxnFinish func(txnEvent)
 	}
 
 	// sessionData contains the user-configurable connection variables.
@@ -905,9 +918,14 @@ type connExecutor struct {
 	// safe to use when a statement is not being parallelized. It must be reset
 	// before using.
 	planner planner
-	// phaseTimes tracks session-level phase times. It is copied-by-value
-	// to each planner in session.newPlanner.
+	// phaseTimes tracks session- and transaction-level phase times. It is
+	// copied-by-value when resetting statsCollector before executing each
+	// statement.
 	phaseTimes phaseTimes
+
+	// statsCollector is used to collect statistics about SQL statements and
+	// transactions.
+	statsCollector *sqlStatsCollector
 
 	// mu contains of all elements of the struct that can be changed
 	// after initialization, and may be accessed from another thread.
@@ -1015,7 +1033,7 @@ func (ns *prepStmtNamespace) resetTo(ctx context.Context, to prepStmtNamespace) 
 // resetExtraTxnState resets the fields of ex.extraTxnState when a transaction
 // commits, rolls back or restarts.
 func (ex *connExecutor) resetExtraTxnState(
-	ctx context.Context, dbCacheHolder *databaseCacheHolder,
+	ctx context.Context, dbCacheHolder *databaseCacheHolder, ev txnEvent,
 ) error {
 	ex.extraTxnState.schemaChangers.reset()
 
@@ -1027,6 +1045,15 @@ func (ex *connExecutor) resetExtraTxnState(
 	for name, p := range ex.extraTxnState.prepStmtsNamespace.portals {
 		p.decRef(ctx)
 		delete(ex.extraTxnState.prepStmtsNamespace.portals, name)
+	}
+
+	switch ev {
+	case txnCommit, txnAborted:
+		// After txn is finished, we need to call onTxnFinish (if it's non-nil).
+		if ex.extraTxnState.onTxnFinish != nil {
+			ex.extraTxnState.onTxnFinish(ev)
+			ex.extraTxnState.onTxnFinish = nil
+		}
 	}
 
 	return nil
@@ -1602,6 +1629,8 @@ func (ex *connExecutor) execCopyIn(
 			// state machine, but the copyMachine manages its own transactions without
 			// going through the state machine.
 			ex.state.sqlTimestamp = txnTS
+			ex.statsCollector = ex.newStatsCollector()
+			ex.statsCollector.reset(&ex.server.sqlStats, ex.appStats, ex.phaseTimes)
 			ex.initPlanner(ctx, p)
 			ex.resetPlanner(ctx, p, txn, stmtTS, 0 /* numAnnotations */)
 		},
@@ -1809,17 +1838,18 @@ func (ex *connExecutor) initEvalCtx(ctx context.Context, evalCtx *extendedEvalCo
 			InternalExecutor: ie,
 			DB:               ex.server.cfg.DB,
 		},
-		SessionMutator:  ex.dataMutator,
-		VirtualSchemas:  ex.server.cfg.VirtualSchemas,
-		Tracing:         &ex.sessionTracing,
-		StatusServer:    ex.server.cfg.StatusServer,
-		MemMetrics:      &ex.memMetrics,
-		Tables:          &ex.extraTxnState.tables,
-		ExecCfg:         ex.server.cfg,
-		DistSQLPlanner:  ex.server.cfg.DistSQLPlanner,
-		TxnModesSetter:  ex,
-		SchemaChangers:  &ex.extraTxnState.schemaChangers,
-		schemaAccessors: scInterface,
+		SessionMutator:    ex.dataMutator,
+		VirtualSchemas:    ex.server.cfg.VirtualSchemas,
+		Tracing:           &ex.sessionTracing,
+		StatusServer:      ex.server.cfg.StatusServer,
+		MemMetrics:        &ex.memMetrics,
+		Tables:            &ex.extraTxnState.tables,
+		ExecCfg:           ex.server.cfg,
+		DistSQLPlanner:    ex.server.cfg.DistSQLPlanner,
+		TxnModesSetter:    ex,
+		SchemaChangers:    &ex.extraTxnState.schemaChangers,
+		schemaAccessors:   scInterface,
+		sqlStatsCollector: ex.statsCollector,
 	}
 }
 
@@ -1869,7 +1899,6 @@ func (ex *connExecutor) implicitTxn() bool {
 // query in the context of this session.
 func (ex *connExecutor) initPlanner(ctx context.Context, p *planner) {
 	p.cancelChecker = sqlbase.NewCancelChecker(ctx)
-	p.statsCollector = ex.newStatsCollector()
 
 	ex.initEvalCtx(ctx, &p.extendedEvalCtx, p)
 
@@ -1891,7 +1920,6 @@ func (ex *connExecutor) resetPlanner(
 	p.stmt = nil
 
 	p.cancelChecker.Reset(ctx)
-	p.statsCollector.Reset(&ex.server.sqlStats, ex.appStats, &ex.phaseTimes)
 
 	p.semaCtx = tree.MakeSemaContext()
 	p.semaCtx.Location = &ex.sessionData.DataConversion.Location
@@ -1939,6 +1967,7 @@ func (ex *connExecutor) txnStateTransitionsApplyWrapper(
 	case noEvent:
 	case txnStart:
 		ex.extraTxnState.autoRetryCounter = 0
+		ex.extraTxnState.onTxnFinish = ex.recordTransactionStart()
 	case txnCommit:
 		if res.Err() != nil {
 			err := errorutil.UnexpectedWithIssueErrorf(
@@ -2003,7 +2032,7 @@ func (ex *connExecutor) txnStateTransitionsApplyWrapper(
 
 		fallthrough
 	case txnRestart, txnAborted:
-		if err := ex.resetExtraTxnState(ex.Ctx(), ex.server.dbCache); err != nil {
+		if err := ex.resetExtraTxnState(ex.Ctx(), ex.server.dbCache, advInfo.txnEvent); err != nil {
 			return advanceInfo{}, err
 		}
 	default:
@@ -2042,10 +2071,10 @@ func (ex *connExecutor) recordError(ctx context.Context, err error) {
 	sqltelemetry.RecordError(ctx, err, &ex.server.cfg.Settings.SV)
 }
 
-// newStatsCollector returns an sqlStatsCollector that will record stats in the
+// newStatsCollector returns a sqlStatsCollector that will record stats in the
 // session's stats containers.
-func (ex *connExecutor) newStatsCollector() sqlStatsCollector {
-	return newSQLStatsCollectorImpl(&ex.server.sqlStats, ex.appStats, &ex.phaseTimes)
+func (ex *connExecutor) newStatsCollector() *sqlStatsCollector {
+	return newSQLStatsCollector(&ex.server.sqlStats, ex.appStats, ex.phaseTimes)
 }
 
 // cancelQuery is part of the registrySession interface.

pkg/sql/conn_executor_exec.go
@@ -362,6 +362,7 @@ func (ex *connExecutor) execStmtInOpenState(
 
 	p := &ex.planner
 	stmtTS := ex.server.cfg.Clock.PhysicalTime()
+	ex.statsCollector.reset(&ex.server.sqlStats, ex.appStats, ex.phaseTimes)
 	ex.resetPlanner(ctx, p, ex.state.mu.txn, stmtTS, stmt.NumAnnotations)
 
 	if os.ImplicitTxn.Get() {
@@ -608,7 +609,7 @@ func (ex *connExecutor) dispatchToExecutionEngine(
 ) error {
 	stmt := planner.stmt
 	ex.sessionTracing.TracePlanStart(ctx, stmt.AST.StatementTag())
-	planner.statsCollector.PhaseTimes()[plannerStartLogicalPlan] = timeutil.Now()
+	ex.statsCollector.phaseTimes[plannerStartLogicalPlan] = timeutil.Now()
 
 	// Prepare the plan. Note, the error is processed below. Everything
 	// between here and there needs to happen even if there's an error.
@@ -635,10 +636,17 @@ func (ex *connExecutor) dispatchToExecutionEngine(
 	}
 
 	defer func() {
-		planner.maybeLogStatement(ctx, "exec", ex.extraTxnState.autoRetryCounter, res.RowsAffected(), res.Err())
+		planner.maybeLogStatement(
+			ctx,
+			"exec",
+			ex.extraTxnState.autoRetryCounter,
+			res.RowsAffected(),
+			res.Err(),
+			ex.statsCollector.phaseTimes[sessionQueryReceived],
+		)
 	}()
 
-	planner.statsCollector.PhaseTimes()[plannerEndLogicalPlan] = timeutil.Now()
+	ex.statsCollector.phaseTimes[plannerEndLogicalPlan] = timeutil.Now()
 	ex.sessionTracing.TracePlanEnd(ctx, err)
 
 	// Finally, process the planning error from above.
@@ -667,7 +675,7 @@ func (ex *connExecutor) dispatchToExecutionEngine(
 		ex.server.cfg.TestingKnobs.BeforeExecute(ctx, stmt.String())
 	}
 
-	planner.statsCollector.PhaseTimes()[plannerStartExecStmt] = timeutil.Now()
+	ex.statsCollector.phaseTimes[plannerStartExecStmt] = timeutil.Now()
 
 	ex.mu.Lock()
 	queryMeta, ok := ex.mu.ActiveQueries[stmt.queryID]
@@ -699,7 +707,7 @@ func (ex *connExecutor) dispatchToExecutionEngine(
 	ex.sessionTracing.TraceExecStart(ctx, "distributed")
 	bytesRead, rowsRead, err := ex.execWithDistSQLEngine(ctx, planner, stmt.AST.StatementType(), res, distributePlan)
 	ex.sessionTracing.TraceExecEnd(ctx, res.Err(), res.RowsAffected())
-	planner.statsCollector.PhaseTimes()[plannerEndExecStmt] = timeutil.Now()
+	ex.statsCollector.phaseTimes[plannerEndExecStmt] = timeutil.Now()
 
 	// Record the statement summary. This also closes the plan if the
 	// plan has not been closed earlier.
@@ -846,6 +854,7 @@ func (ex *connExecutor) beginTransactionTimestampsAndReadMode(
 		rwMode = ex.readWriteModeWithSessionDefault(s.Modes.ReadWriteMode)
 		return rwMode, now.GoTime(), nil, nil
 	}
+	ex.statsCollector.reset(&ex.server.sqlStats, ex.appStats, ex.phaseTimes)
 	p := &ex.planner
 	ex.resetPlanner(ctx, p, nil /* txn */, now.GoTime(), 0 /* numAnnotations */)
 	ts, err := p.EvalAsOfTimestamp(s.Modes.AsOf)
@@ -1267,4 +1276,30 @@ func (ex *connExecutor) validateSavepointName(savepoint tree.Name) error {
 				"with SET force_savepoint_restart=true")
 	}
 	return nil
+}
+
+// recordTransactionStart records the start of the transaction and returns a
+// closure to be called once the transaction finishes.
+func (ex *connExecutor) recordTransactionStart() func(txnEvent) {
+	// We don't need to write down the transaction start time into
+	// ex.statsCollector.phaseTimes because it will be overwritten in
+	// ex.statsCollector.reset() before executing the statements of the
+	// transaction.
+	ex.phaseTimes[transactionStart] = timeutil.Now()
+	implicit := ex.implicitTxn()
+	return func(ev txnEvent) { ex.recordTransaction(ev, implicit) }
+}
+
+func (ex *connExecutor) recordTransaction(ev txnEvent, implicit bool) {
+	phaseTimes := ex.statsCollector.phaseTimes
+	phaseTimes[transactionEnd] = timeutil.Now()
+	txnStart := phaseTimes[transactionStart]
+	txnEnd := phaseTimes[transactionEnd]
+	txnTime := txnEnd.Sub(txnStart)
+	ex.metrics.EngineMetrics.SQLTxnLatency.RecordValue(txnTime.Nanoseconds())
+	ex.statsCollector.recordTransaction(
+		txnTime.Seconds(),
+		ev,
+		implicit,
+	)
 }

pkg/sql/conn_executor_prepare.go
@@ -152,6 +152,7 @@ func (ex *connExecutor) prepare(
 	// anything other than getting a timestamp.
 	txn := client.NewTxn(ctx, ex.server.cfg.DB, ex.server.cfg.NodeID.Get(), client.RootTxn)
 
+	ex.statsCollector.reset(&ex.server.sqlStats, ex.appStats, ex.phaseTimes)
 	p := &ex.planner
 	ex.resetPlanner(ctx, p, txn, ex.server.cfg.Clock.PhysicalTime() /* stmtTS */, stmt.NumAnnotations)
 	p.stmt = &stmt

pkg/sql/exec_util.go
@@ -262,6 +262,12 @@ var (
 		Measurement: "SQL Statements",
 		Unit:        metric.Unit_COUNT,
 	}
+	MetaSQLTxnLatency = metric.Metadata{
+		Name:        "sql.txn.latency",
+		Help:        "Latency of SQL transactions",
+		Measurement: "Latency",
+		Unit:        metric.Unit_NANOSECONDS,
+	}
 
 	// Below are the metadata for the statement started counters.
 
@@ -1854,43 +1860,32 @@ func (m *sessionDataMutator) RecordLatestSequenceVal(seqID uint32, val int64) {
 	m.data.SequenceState.RecordValue(seqID, val)
 }
 
-type sqlStatsCollectorImpl struct {
-	// sqlStats tracks per-application statistics for all
-	// applications on each node.
+type sqlStatsCollector struct {
+	// sqlStats tracks per-application statistics for all applications on each
+	// node.
 	sqlStats *sqlStats
-	// appStats track per-application SQL usage statistics. This is a pointer into
-	// sqlStats set as the session's current app.
+	// appStats track per-application SQL usage statistics. This is a pointer
+	// into sqlStats set as the session's current app.
 	appStats *appStats
-	// phaseTimes tracks session-level phase times. It is copied-by-value
-	// to each planner in session.newPlanner.
-	phaseTimes phaseTimes
+	// phaseTimes tracks session-level phase times.
+	phaseTimes *phaseTimes
 }
 
-// sqlStatsCollectorImpl implements the sqlStatsCollector interface.
-var _ sqlStatsCollector = &sqlStatsCollectorImpl{}
-
-// newSQLStatsCollectorImpl creates an instance of sqlStatsCollectorImpl.
-//
-// note that phaseTimes is an array, not a slice, so this performs a copy-by-value.
-func newSQLStatsCollectorImpl(
-	sqlStats *sqlStats, appStats *appStats, phaseTimes *phaseTimes,
-) *sqlStatsCollectorImpl {
-	return &sqlStatsCollectorImpl{
+// newSQLStatsCollector creates an instance of sqlStatsCollector. Note that
+// phaseTimes is an array, not a slice, so this performs a copy-by-value.
+func newSQLStatsCollector(
+	sqlStats *sqlStats, appStats *appStats, phaseTimes phaseTimes,
+) *sqlStatsCollector {
+	return &sqlStatsCollector{
 		sqlStats:   sqlStats,
 		appStats:   appStats,
-		phaseTimes: *phaseTimes,
+		phaseTimes: &phaseTimes,
 	}
 }
 
-// PhaseTimes is part of the sqlStatsCollector interface.
-func (s *sqlStatsCollectorImpl) PhaseTimes() *phaseTimes {
-	return &s.phaseTimes
-}
-
-// RecordStatement is part of the sqlStatsCollector interface.
-//
-// samplePlanDescription can be nil, as these are only sampled periodically per unique fingerprint.
-func (s *sqlStatsCollectorImpl) RecordStatement(
+// recordStatement records stats for one statement. samplePlanDescription can
+// be nil, as these are only sampled periodically per unique fingerprint.
+func (s *sqlStatsCollector) recordStatement(
 	stmt *Statement,
 	samplePlanDescription *roachpb.ExplainTreePlanNode,
 	distSQLUsed bool,
@@ -1907,17 +1902,15 @@ func (s *sqlStatsCollectorImpl) RecordStatement(
 		parseLat, planLat, runLat, svcLat, ovhLat, bytesRead, rowsRead)
 }
 
-// SQLStats is part of the sqlStatsCollector interface.
-func (s *sqlStatsCollectorImpl) SQLStats() *sqlStats {
-	return s.sqlStats
+// recordTransaction records stats for one transaction.
+func (s *sqlStatsCollector) recordTransaction(txnTimeSec float64, ev txnEvent, implicit bool) {
+	s.appStats.recordTransaction(txnTimeSec, ev, implicit)
 }
 
-func (s *sqlStatsCollectorImpl) Reset(
-	sqlStats *sqlStats, appStats *appStats, phaseTimes *phaseTimes,
-) {
-	*s = sqlStatsCollectorImpl{
+func (s *sqlStatsCollector) reset(sqlStats *sqlStats, appStats *appStats, phaseTimes phaseTimes) {
+	*s = sqlStatsCollector{
 		sqlStats:   sqlStats,
 		appStats:   appStats,
-		phaseTimes: *phaseTimes,
+		phaseTimes: &phaseTimes,
 	}
 }

pkg/sql/executor_statement_metrics.go
@@ -48,6 +48,10 @@ const (
 	plannerStartExecStmt    // Execution starts.
 	plannerEndExecStmt      // Execution ends.
 
+	// Transaction phases.
+	transactionStart // Transaction starts.
+	transactionEnd   // Transaction ends.
+
 	// sessionNumPhases must be listed last so that it can be used to
 	// define arrays sufficiently large to hold all the other values.
 	sessionNumPhases
@@ -75,6 +79,7 @@ type EngineMetrics struct {
 	SQLExecLatency        *metric.Histogram
 	DistSQLServiceLatency *metric.Histogram
 	SQLServiceLatency     *metric.Histogram
+	SQLTxnLatency         *metric.Histogram
 
 	// TxnAbortCount counts transactions that were aborted, either due
 	// to non-retriable errors, or retriable errors when the client-side
@@ -123,7 +128,7 @@ func (ex *connExecutor) recordStatementSummary(
 	bytesRead int64,
 	rowsRead int64,
 ) {
-	phaseTimes := planner.statsCollector.PhaseTimes()
+	phaseTimes := ex.statsCollector.phaseTimes
 
 	// Compute the run latency. This is always recorded in the
 	// server metrics.
@@ -162,7 +167,7 @@ func (ex *connExecutor) recordStatementSummary(
 		m.SQLServiceLatency.RecordValue(svcLatRaw.Nanoseconds())
 	}
 
-	planner.statsCollector.RecordStatement(
+	ex.statsCollector.recordStatement(
 		stmt, planner.curPlan.savedPlanForStats,
 		flags.IsSet(planFlagDistributed), flags.IsSet(planFlagOptUsed), flags.IsSet(planFlagImplicitTxn),
 		automaticRetryCount, rowsAffected, err,

docs/generated/settings/settings.html
@@ -111,6 +111,7 @@
 <tr><td><code>sql.metrics.statement_details.plan_collection.enabled</code></td><td>boolean</td><td><code>true</code></td><td>periodically save a logical plan for each fingerprint</td></tr>
 <tr><td><code>sql.metrics.statement_details.plan_collection.period</code></td><td>duration</td><td><code>5m0s</code></td><td>the time until a new logical plan is collected</td></tr>
 <tr><td><code>sql.metrics.statement_details.threshold</code></td><td>duration</td><td><code>0s</code></td><td>minimum execution time to cause statistics to be collected</td></tr>
+<tr><td><code>sql.metrics.transaction_details.enabled</code></td><td>boolean</td><td><code>true</code></td><td>collect per-application transaction statistics</td></tr>
 <tr><td><code>sql.parallel_scans.enabled</code></td><td>boolean</td><td><code>true</code></td><td>parallelizes scanning different ranges when the maximum result size can be deduced</td></tr>
 <tr><td><code>sql.query_cache.enabled</code></td><td>boolean</td><td><code>true</code></td><td>enable the query cache</td></tr>
 <tr><td><code>sql.stats.automatic_collection.enabled</code></td><td>boolean</td><td><code>true</code></td><td>automatic statistics collection mode</td></tr>

pkg/cli/cli_test.go
@@ -2139,12 +2139,13 @@ writing ` + os.DevNull + `
   debug/nodes/1/crdb_internal.gossip_network.txt
   debug/nodes/1/crdb_internal.gossip_nodes.txt
   debug/nodes/1/crdb_internal.leases.txt
-  debug/nodes/1/crdb_internal.node_statement_statistics.txt
   debug/nodes/1/crdb_internal.node_build_info.txt
   debug/nodes/1/crdb_internal.node_metrics.txt
   debug/nodes/1/crdb_internal.node_queries.txt
   debug/nodes/1/crdb_internal.node_runtime_info.txt
   debug/nodes/1/crdb_internal.node_sessions.txt
+  debug/nodes/1/crdb_internal.node_statement_statistics.txt
+  debug/nodes/1/crdb_internal.node_txn_stats.txt
   debug/nodes/1/details.json
   debug/nodes/1/gossip.json
   debug/nodes/1/enginestats.json

pkg/cli/zip.go
@@ -77,12 +77,13 @@ var debugZipTablesPerNode = []string{
 
 	"crdb_internal.leases",
 
-	"crdb_internal.node_statement_statistics",
 	"crdb_internal.node_build_info",
 	"crdb_internal.node_metrics",
 	"crdb_internal.node_queries",
 	"crdb_internal.node_runtime_info",
 	"crdb_internal.node_sessions",
+	"crdb_internal.node_statement_statistics",
+	"crdb_internal.node_txn_stats",
 }
 
 type zipper struct {

commit bdc6c244d5f91bfd3268caeba7981573b327bda9

sql: don't allocate phaseTimes on each call to sqlStatsCollector.reset
This allocation was responsible for **0.28%** of a CPU profile when running
Sysbench's oltp_point_select workload.

This was broken in ee48ea7.

Release justification: Reverts perf regression due to newly introduced memory
allocations.

Release note: None

pkg/sql/conn_executor.go
@@ -1635,7 +1635,7 @@ func (ex *connExecutor) execCopyIn(
 			// going through the state machine.
 			ex.state.sqlTimestamp = txnTS
 			ex.statsCollector = ex.newStatsCollector()
-			ex.statsCollector.reset(&ex.server.sqlStats, ex.appStats, ex.phaseTimes)
+			ex.statsCollector.reset(&ex.server.sqlStats, ex.appStats, &ex.phaseTimes)
 			ex.initPlanner(ctx, p)
 			ex.resetPlanner(ctx, p, txn, stmtTS, 0 /* numAnnotations */)
 		},
@@ -2079,7 +2079,7 @@ func (ex *connExecutor) recordError(ctx context.Context, err error) {
 // newStatsCollector returns a sqlStatsCollector that will record stats in the
 // session's stats containers.
 func (ex *connExecutor) newStatsCollector() *sqlStatsCollector {
-	return newSQLStatsCollector(&ex.server.sqlStats, ex.appStats, ex.phaseTimes)
+	return newSQLStatsCollector(&ex.server.sqlStats, ex.appStats, &ex.phaseTimes)
 }
 
 // cancelQuery is part of the registrySession interface.

pkg/sql/conn_executor_exec.go
@@ -362,7 +362,7 @@ func (ex *connExecutor) execStmtInOpenState(
 
 	p := &ex.planner
 	stmtTS := ex.server.cfg.Clock.PhysicalTime()
-	ex.statsCollector.reset(&ex.server.sqlStats, ex.appStats, ex.phaseTimes)
+	ex.statsCollector.reset(&ex.server.sqlStats, ex.appStats, &ex.phaseTimes)
 	ex.resetPlanner(ctx, p, ex.state.mu.txn, stmtTS, stmt.NumAnnotations)
 
 	if os.ImplicitTxn.Get() {
@@ -863,7 +863,7 @@ func (ex *connExecutor) beginTransactionTimestampsAndReadMode(
 		rwMode = ex.readWriteModeWithSessionDefault(s.Modes.ReadWriteMode)
 		return rwMode, now.GoTime(), nil, nil
 	}
-	ex.statsCollector.reset(&ex.server.sqlStats, ex.appStats, ex.phaseTimes)
+	ex.statsCollector.reset(&ex.server.sqlStats, ex.appStats, &ex.phaseTimes)
 	p := &ex.planner
 	ex.resetPlanner(ctx, p, nil /* txn */, now.GoTime(), 0 /* numAnnotations */)
 	ts, err := p.EvalAsOfTimestamp(s.Modes.AsOf)
@@ -1300,7 +1300,7 @@ func (ex *connExecutor) recordTransactionStart() func(txnEvent) {
 }
 
 func (ex *connExecutor) recordTransaction(ev txnEvent, implicit bool) {
-	phaseTimes := ex.statsCollector.phaseTimes
+	phaseTimes := &ex.statsCollector.phaseTimes
 	phaseTimes[transactionEnd] = timeutil.Now()
 	txnStart := phaseTimes[transactionStart]
 	txnEnd := phaseTimes[transactionEnd]

pkg/sql/conn_executor_prepare.go
@@ -152,7 +152,7 @@ func (ex *connExecutor) prepare(
 	// anything other than getting a timestamp.
 	txn := client.NewTxn(ctx, ex.server.cfg.DB, ex.server.cfg.NodeID.Get(), client.RootTxn)
 
-	ex.statsCollector.reset(&ex.server.sqlStats, ex.appStats, ex.phaseTimes)
+	ex.statsCollector.reset(&ex.server.sqlStats, ex.appStats, &ex.phaseTimes)
 	p := &ex.planner
 	ex.resetPlanner(ctx, p, txn, ex.server.cfg.Clock.PhysicalTime() /* stmtTS */, stmt.NumAnnotations)
 	p.stmt = &stmt

pkg/sql/exec_util.go
@@ -1875,18 +1875,18 @@ type sqlStatsCollector struct {
 	// into sqlStats set as the session's current app.
 	appStats *appStats
 	// phaseTimes tracks session-level phase times.
-	phaseTimes *phaseTimes
+	phaseTimes phaseTimes
 }
 
 // newSQLStatsCollector creates an instance of sqlStatsCollector. Note that
 // phaseTimes is an array, not a slice, so this performs a copy-by-value.
 func newSQLStatsCollector(
-	sqlStats *sqlStats, appStats *appStats, phaseTimes phaseTimes,
+	sqlStats *sqlStats, appStats *appStats, phaseTimes *phaseTimes,
 ) *sqlStatsCollector {
 	return &sqlStatsCollector{
 		sqlStats:   sqlStats,
 		appStats:   appStats,
-		phaseTimes: &phaseTimes,
+		phaseTimes: *phaseTimes,
 	}
 }
 
@@ -1914,10 +1914,10 @@ func (s *sqlStatsCollector) recordTransaction(txnTimeSec float64, ev txnEvent, i
 	s.appStats.recordTransaction(txnTimeSec, ev, implicit)
 }
 
-func (s *sqlStatsCollector) reset(sqlStats *sqlStats, appStats *appStats, phaseTimes phaseTimes) {
+func (s *sqlStatsCollector) reset(sqlStats *sqlStats, appStats *appStats, phaseTimes *phaseTimes) {
 	*s = sqlStatsCollector{
 		sqlStats:   sqlStats,
 		appStats:   appStats,
-		phaseTimes: &phaseTimes,
+		phaseTimes: *phaseTimes,
 	}
 }

pkg/sql/executor_statement_metrics.go
@@ -128,7 +128,7 @@ func (ex *connExecutor) recordStatementSummary(
 	bytesRead int64,
 	rowsRead int64,
 ) {
-	phaseTimes := ex.statsCollector.phaseTimes
+	phaseTimes := &ex.statsCollector.phaseTimes
 
 	// Compute the run latency. This is always recorded in the
 	// server metrics.