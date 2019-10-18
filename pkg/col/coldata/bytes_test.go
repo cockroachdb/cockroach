f30accb87b06841b41268ada825e70c25a2a33b3
Yahor Yuzefovich <yahor@cockroachlabs.com>
2019-10-18 11:33:11 -0400

    coldata: fix a couple of issues with flat bytes implementation
    This commit fixes the following:
    - Slice method no longer modifies the receiver in-place and returns
      a shallow copy instead (as regular slices do)
    - in CopySlice, srcEndIdx has been updated to be "trimmed" so that
      offsets and lengths are not overwritten past the copied values.
      Also, now when appending srcDataToCopy, we correctly set the index
      to copy into.
    
    Release note: None

--- pkg/col/coldata/bytes_test.go
+++ pkg/col/coldata/bytes_test.go
@@ -16,6 +16,7 @@ import (
 	"math/rand"
 	"strings"
 	"testing"
+	"unsafe"
 
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
@@ -59,33 +60,22 @@ var bytesMethods = []bytesMethod{set, slice, copySlice, appendSlice, appendVal}
 // destination. In cases where *Bytes updates itself under the hood, we also
 // update the corresponding b2Source to mirror the behavior.
 func applyMethodsAndVerify(
-	rng *rand.Rand,
-	b1, b1Source *Bytes,
-	b2, b2Source [][]byte,
-	methods []bytesMethod,
-	selfReferencingSources bool,
+	rng *rand.Rand, b1 *Bytes, b2 [][]byte, methods []bytesMethod, maxLength int,
 ) error {
 	if err := verifyEqual(b1, b2); err != nil {
 		return errors.Wrap(err, "arguments should start as equal")
 	}
-	if err := verifyEqual(b1Source, b2Source); err != nil {
-		return errors.Wrap(err, "argument sources should start as equal")
-	}
-	var debugString string
+	debugString := fmt.Sprintf("\ninitial:\n%s\n", b1)
 	for _, m := range methods {
 		n := b1.Len()
 		if n != len(b2) {
 			return errors.Errorf("length mismatch between flat and reference: %d != %d", n, len(b2))
 		}
-		sourceN := b1Source.Len()
-		if sourceN != len(b2Source) {
-			return errors.Errorf("length mismatch between flat and reference sources: %d != %d", sourceN, len(b2Source))
-		}
 		debugString += m.String()
 		switch m {
 		case set:
-			// Can only Set the last index.
-			i := b1.Len() - 1
+			// Can only Set starting from maxSetIndex.
+			i := b1.maxSetIndex + rng.Intn(b1.Len()-b1.maxSetIndex)
 			new := make([]byte, rng.Intn(16))
 			rng.Read(new)
 			debugString += fmt.Sprintf("(%d, %v)", i, new)
@@ -105,12 +95,19 @@ func applyMethodsAndVerify(
 			debugString += fmt.Sprintf("(%d, %d)", start, end)
 			b1 = b1.Slice(start, end)
 			b2 = b2[start:end]
-			if selfReferencingSources {
-				b2Source = b2
-			}
 		case copySlice, appendSlice:
 			// Generate a length-inclusive destIdx.
 			destIdx := rng.Intn(n + 1)
+			// Make a pair of sources to copy/append from.
+			sourceN := 1 + rng.Intn(maxLength)
+			flatSource := NewBytes(sourceN)
+			referenceSource := make([][]byte, sourceN)
+			for i := 0; i < sourceN; i++ {
+				v := make([]byte, rng.Intn(16))
+				rng.Read(v)
+				flatSource.Set(i, append([]byte(nil), v...))
+				referenceSource[i] = append([]byte(nil), v...)
+			}
 			srcStartIdx := rng.Intn(sourceN)
 			srcEndIdx := rng.Intn(sourceN)
 			if srcStartIdx > srcEndIdx {
@@ -121,22 +118,12 @@ func applyMethodsAndVerify(
 				srcEndIdx = sourceN
 			}
 			debugString += fmt.Sprintf("(%d, %d, %d)", destIdx, srcStartIdx, srcEndIdx)
-			var numNewVals int
 			if m == copySlice {
-				b1.CopySlice(b1Source, destIdx, srcStartIdx, srcEndIdx)
-				numNewVals = copy(b2[destIdx:], b2Source[srcStartIdx:srcEndIdx])
+				b1.CopySlice(flatSource, destIdx, srcStartIdx, srcEndIdx)
+				copy(b2[destIdx:], referenceSource[srcStartIdx:srcEndIdx])
 			} else {
-				b1.AppendSlice(b1Source, destIdx, srcStartIdx, srcEndIdx)
-				b2 = append(b2[:destIdx], b2Source[srcStartIdx:srcEndIdx]...)
-				if selfReferencingSources {
-					b2Source = b2
-				}
-				numNewVals = srcEndIdx - srcStartIdx
-			}
-			// Deep copy the copied/appended byte slices.
-			b2Slice := b2[destIdx : destIdx+numNewVals]
-			for i := range b2Slice {
-				b2Slice[i] = append([]byte(nil), b2Slice[i]...)
+				b1.AppendSlice(flatSource, destIdx, srcStartIdx, srcEndIdx)
+				b2 = append(b2[:destIdx], referenceSource[srcStartIdx:srcEndIdx]...)
 			}
 		case appendVal:
 			v := make([]byte, 16)
@@ -144,13 +131,10 @@ func applyMethodsAndVerify(
 			debugString += fmt.Sprintf("(%v)", v)
 			b1.AppendVal(v)
 			b2 = append(b2, v)
-			if selfReferencingSources {
-				b2Source = b2
-			}
 		default:
 			return errors.Errorf("unknown method name: %s", m)
 		}
-		debugString += "\n"
+		debugString += fmt.Sprintf("\n%s\n", b1)
 		if err := verifyEqual(b1, b2); err != nil {
 			return errors.Wrap(err, fmt.Sprintf("\ndebugString:\n%sflat (maxSetIdx=%d):\n%sreference:\n%s", debugString, b1.maxSetIndex, b1.String(), prettyByteSlice(b2)))
 		}
@@ -188,53 +172,34 @@ func TestBytesRefImpl(t *testing.T) {
 	const (
 		maxNumberOfCalls = 64
 		maxLength        = 16
+		nRuns            = 100
 	)
 
-	n := 1 + rng.Intn(maxLength)
+	for nRun := 0; nRun < nRuns; nRun++ {
+		n := 1 + rng.Intn(maxLength)
 
-	flat := NewBytes(n)
-	reference := make([][]byte, n)
-	for i := 0; i < n; i++ {
-		v := make([]byte, rng.Intn(16))
-		rng.Read(v)
-		flat.Set(i, append([]byte(nil), v...))
-		reference[i] = append([]byte(nil), v...)
-	}
-
-	// Make a pair of sources to copy/append from. Use the destination variables
-	// with a certain probability.
-	sourceN := n
-	flatSource := flat
-	referenceSource := reference
-	selfReferencingSources := true
-	if rng.Float64() < 0.5 {
-		selfReferencingSources = false
-		sourceN = 1 + rng.Intn(maxLength)
-		flatSource = NewBytes(sourceN)
-		referenceSource = make([][]byte, sourceN)
-		for i := 0; i < sourceN; i++ {
+		flat := NewBytes(n)
+		reference := make([][]byte, n)
+		for i := 0; i < n; i++ {
 			v := make([]byte, rng.Intn(16))
 			rng.Read(v)
-			flatSource.Set(i, append([]byte(nil), v...))
-			referenceSource[i] = append([]byte(nil), v...)
+			flat.Set(i, append([]byte(nil), v...))
+			reference[i] = append([]byte(nil), v...)
 		}
-	}
 
-	if err := verifyEqual(flat, reference); err != nil {
-		t.Fatalf("not equal: %v\nflat:\n%sreference:\n%s", err, flat, prettyByteSlice(reference))
-	}
+		if err := verifyEqual(flat, reference); err != nil {
+			t.Fatalf("not equal: %v\nflat:\n%sreference:\n%s", err, flat, prettyByteSlice(reference))
+		}
 
-	if err := verifyEqual(flatSource, referenceSource); err != nil {
-		t.Fatalf("sources not equal: %v\nflat:\n%sreference:\n%s", err, flat, prettyByteSlice(reference))
-	}
-
-	numCalls := 1 + rng.Intn(maxNumberOfCalls)
-	methods := make([]bytesMethod, 0, numCalls)
-	for i := 0; i < numCalls; i++ {
-		methods = append(methods, bytesMethods[rng.Intn(len(bytesMethods))])
-	}
-	if err := applyMethodsAndVerify(rng, flat, flatSource, reference, referenceSource, methods, selfReferencingSources); err != nil {
-		t.Fatal(err)
+		numCalls := 1 + rng.Intn(maxNumberOfCalls)
+		methods := make([]bytesMethod, 0, numCalls)
+		for i := 0; i < numCalls; i++ {
+			methods = append(methods, bytesMethods[rng.Intn(len(bytesMethods))])
+		}
+		if err := applyMethodsAndVerify(rng, flat, reference, methods, maxLength); err != nil {
+			t.Logf("nRun = %d\n", nRun)
+			t.Fatal(err)
+		}
 	}
 }
 
@@ -346,5 +311,23 @@ func TestBytes(t *testing.T) {
 		b1.CopySlice(b2, 0, 1, b2.Len())
 		require.Equal(t, 1, b1.Len())
 		require.Equal(t, "source two", string(b1.Get(0)))
+	})
+
+	t.Run("Slice", func(t *testing.T) {
+		b1 := NewBytes(0)
+		b1.AppendVal([]byte("one"))
+		b1.AppendVal([]byte("two"))
+		b1.AppendVal([]byte("three"))
+
+		s := b1.Slice(0, 3)
+		require.NotEqual(t, unsafe.Pointer(b1), unsafe.Pointer(s), "Bytes.Slice should create a new object")
+		b2 := b1.Slice(1, 2)
+		require.Equal(t, "one", string(b1.Get(0)))
+		require.Equal(t, "two", string(b1.Get(1)))
+		require.Equal(t, "two", string(b2.Get(0)))
+
+		b2.AppendVal([]byte("four"))
+		require.Equal(t, "four", string(b1.Get(2)), "appending to the slice of b1 should have updated b1")
+		require.Equal(t, "four", string(b2.Get(1)))
 	})
 }
