e3315fa4e1ab396033b9fcca55fdc1d02d4cb848
Bob Vawter <bob@cockroachlabs.com>
2019-10-29 10:29:12 -0400

    build: Upgrade to go 1.12.12
    This change upgrades the go runtime to 1.12.12 in order to pick up a [security
    fix](https://github.com/golang/go/issues/34960).
    
    Per the [checklist](build/README.md):
    * [X] Adjust version in Docker image
    * [X] Rebuild the Docker image and bump the version in builder.sh accordingly
    * [ ] ~Bump the version in go-version-check.sh~ (Patch release, not necessary)
    * [X] Bump the default installed version of Go in bootstrap-debian.sh
    
    Fixes: #41718
    
    Release note (build change): The go runtime has been upgraded to 1.12.12.

--- build/bootstrap/bootstrap-debian.sh
+++ build/bootstrap/bootstrap-debian.sh
@@ -38,9 +38,9 @@ echo '. ~/.bashrc_bootstrap' >> ~/.bashrc
 
 # Install Go.
 trap 'rm -f /tmp/go.tgz' EXIT
-curl https://dl.google.com/go/go1.12.10.linux-amd64.tar.gz > /tmp/go.tgz
+curl https://dl.google.com/go/go1.12.12.linux-amd64.tar.gz > /tmp/go.tgz
 sha256sum -c - <<EOF
-aaa84147433aed24e70b31da369bb6ca2859464a45de47c2a5023d8573412f6b  /tmp/go.tgz
+4cf11ac6a8fa42d26ab85e27a5d916ee171900a87745d9f7d4a29a21587d78fc  /tmp/go.tgz
 EOF
 sudo tar -C /usr/local -zxf /tmp/go.tgz
 
