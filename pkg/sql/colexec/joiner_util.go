6847f2d407a94cc81c921f513c8709d5c9ace348
Yahor Yuzefovich <yahor@cockroachlabs.com>
2019-11-01 16:58:55 -0700

    colexec: move joiner_util code out of the template
    The code no longer has any templating logic, so I simply copied over it
    into a regular .go file and removed all the related code generation
    stuff.
    
    Release note: None

--- pkg/sql/colexec/joiner_util.go
+++ pkg/sql/colexec/joiner_util.go
@@ -8,15 +8,6 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-// {{/*
-// +build execgen_template
-//
-// This file is the execgen template for joiner_util.eg.go. It's formatted in a
-// special way, so it's both valid Go and a valid text/template input. This
-// permits editing this file with editor support.
-//
-// */}}
-
 package colexec
 
 import (
@@ -26,15 +17,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/col/coltypes"
 	"github.com/cockroachdb/cockroach/pkg/sql/colexec/execerror"
 )
-
-// {{/*
-// Declarations to make the template compile properly.
-
-// _TYPES_T is the template type variable for coltypes.T. It will be replaced by
-// coltypes.Foo for each type Foo in the coltypes.T type.
-const _TYPES_T = coltypes.Unhandled
-
-// */}}
 
 // filterFeedOperator is used to feed the filter by manually setting the batch
 // to be returned on the first Next() call with zero-length batches returned on
