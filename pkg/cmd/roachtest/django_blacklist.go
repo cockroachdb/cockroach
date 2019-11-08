c2f24bd657b337d6ebd425713372ce6f96e550d4
Rafi Shamim <rafi@cockroachlabs.com>
2019-11-08 01:57:52 -0500

    roachtest: add new blacklists for 20.1
    The nightly ORM test need new blacklists for expected test failures now
    that work on 20.1 has started.
    
    Release note: none

--- pkg/cmd/roachtest/django_blacklist.go
+++ pkg/cmd/roachtest/django_blacklist.go
@@ -27,11 +27,61 @@ var enabledDjangoTests = []string{
 
 var djangoBlacklists = blacklistsForVersion{
 	{"v19.2", "djangoBlacklist19_2", djangoBlacklist19_2, "djangoIgnoreList19_2", djangoIgnoreList19_2},
+	{"v20.1", "djangoBlacklist20_1", djangoBlacklist20_1, "djangoIgnoreList20_1", djangoIgnoreList20_1},
 }
 
 // Maintain that this list is alphabetized.
 // TODO (rohany): The django tests are very flaky right now, so the blacklist
 //  doesn't stay consistent across runs.
+var djangoBlacklist20_1 = blacklist{
+	// Blacklist generated from running the tests above.
+	"app_loading.tests.GetModelsTest.test_get_models_only_returns_installed_models":    "unknown",
+	"apps.tests.AppsTests.test_models_not_loaded":                                      "unknown",
+	"backends.base.test_base.ExecuteWrapperTests.test_wrapper_connection_specific":     "unknown",
+	"backends.tests.DateQuotingTest.test_django_date_trunc":                            "unknown",
+	"backends.tests.FkConstraintsTests.test_check_constraints":                         "unknown",
+	"backends.tests.FkConstraintsTests.test_disable_constraint_checks_context_manager": "unknown",
+	"backends.tests.FkConstraintsTests.test_disable_constraint_checks_manually":        "unknown",
+	"backends.tests.ThreadTests.test_connections_thread_local":                         "unknown",
+	"update.tests.SimpleTest.test_nonempty_update_with_inheritance":                    "unknown",
+	// TODO (rohany): The postgres_tests suite within Django is not in a automatically
+	//  runnable state right now.
+	//"postgres_tests.test_aggregates.TestGeneralAggregate.test_bit_and_empty_result":                                 "41334",
+	//"postgres_tests.test_aggregates.TestGeneralAggregate.test_bit_and_general":                                      "41334",
+	//"postgres_tests.test_aggregates.TestGeneralAggregate.test_bit_and_on_only_false_values":                         "41334",
+	//"postgres_tests.test_aggregates.TestGeneralAggregate.test_bit_and_on_only_true_values":                          "41334",
+	//"postgres_tests.test_aggregates.TestGeneralAggregate.test_bit_or_empty_result":                                  "41334",
+	//"postgres_tests.test_aggregates.TestGeneralAggregate.test_bit_or_general":                                       "41334",
+	//"postgres_tests.test_aggregates.TestGeneralAggregate.test_bit_or_on_only_false_values":                          "41334",
+	//"postgres_tests.test_aggregates.TestGeneralAggregate.test_bit_or_on_only_true_values":                           "41334",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_corr_empty_result":                                 "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_corr_general":                                      "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_covar_pop_empty_result":                            "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_covar_pop_general":                                 "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_covar_pop_sample":                                  "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_covar_pop_sample_empty_result":                     "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_avgx_empty_result":                            "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_avgx_general":                                 "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_avgx_with_related_obj_and_number_as_argument": "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_avgy_empty_result":                            "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_avgy_general":                                 "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_count_empty_result":                           "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_count_general":                                "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_intercept_empty_result":                       "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_intercept_general":                            "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_r2_empty_result":                              "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_r2_general":                                   "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_slope_empty_result":                           "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_slope_general":                                "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_sxx_empty_result":                             "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_sxx_general":                                  "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_sxy_empty_result":                             "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_sxy_general":                                  "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_syy_empty_result":                             "41274",
+	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_syy_general":                                  "41274",
+	//"postgres_tests.test_array.TestOtherTypesExactQuerying.test_exact_decimals":                                     "23468",
+}
+
 var djangoBlacklist19_2 = blacklist{
 	// Blacklist generated from running the tests above.
 	"app_loading.tests.GetModelsTest.test_get_models_only_returns_installed_models":    "unknown",
@@ -80,5 +130,7 @@ var djangoBlacklist19_2 = blacklist{
 	//"postgres_tests.test_aggregates.TestStatisticsAggregate.test_regr_syy_general":                                  "41274",
 	//"postgres_tests.test_array.TestOtherTypesExactQuerying.test_exact_decimals":                                     "23468",
 }
+
+var djangoIgnoreList20_1 = blacklist{}
 
 var djangoIgnoreList19_2 = blacklist{}
