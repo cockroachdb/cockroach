cc9c02e2ebfbbe56c656f077fe2845a6a831335e
Jaewan Park <jaewan.huey.park@gmail.com>
2019-11-12 07:31:58 +0900

    storage/engine: support StorageConfig.MustExist for pebble
    Release note: None

--- pkg/storage/engine/engine.go
+++ pkg/storage/engine/engine.go
@@ -528,6 +528,7 @@ func NewEngine(
 			Opts:          DefaultPebbleOptions(),
 		}
 		pebbleConfig.Opts.Cache = pebble.NewCache(cacheSize)
+		pebbleConfig.Opts.ErrorIfNotExists = storageConfig.MustExist
 
 		return NewPebble(context.Background(), pebbleConfig)
 	case enginepb.EngineTypeRocksDB:
