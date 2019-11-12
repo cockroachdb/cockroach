cockroachdb/cockroach

commit 97788deb9c696ecf73ae4a45d8cc99966e9ad611

importccl: enabling saving of bad csv rows to a side file
When importing from large csv files it's common to have a few offending rows.
Currently `IMPORT` will abort at the first error which makes for a tedious
task of manually fixing ech problem and re-running. Instead users can specify
new option `WITH experimental_save_rejected` which will not stop on bad rows
but save them in a side file called `<original_csv_file>.rejected` and
continue. The user then can re-run the import command using `IMPORT INTO` using
the rejected file after fixing the problems in it.

Release note (sql change): enable skipping of faulty rows in IMPORT.

pkg/ccl/importccl/read_import_base.go
@@ -9,6 +9,7 @@
 package importccl
 
 import (
+	"bytes"
 	"compress/bzip2"
 	"compress/gzip"
 	"context"
@@ -26,7 +27,7 @@ import (
 	"github.com/cockroachdb/errors"
 )
 
-type readFileFunc func(context.Context, *fileReader, int32, string) error
+type readFileFunc func(context.Context, *fileReader, int32, string, chan string) error
 
 // readInputFile reads each of the passed dataFiles using the passed func. The
 // key part of dataFiles is the unique index of the data file among all files in
@@ -98,8 +99,59 @@ func readInputFiles(
 			defer decompressed.Close()
 			src.Reader = decompressed
 
-			if err := fileFunc(ctx, src, dataFileIndex, dataFile); err != nil {
-				return errors.Wrap(err, dataFile)
+			var rejected chan string
+			if format.Format == roachpb.IOFileFormat_MysqlOutfile && format.MysqlOut.SaveRejected {
+				rejected = make(chan string)
+			}
+			if rejected != nil {
+				grp := ctxgroup.WithContext(ctx)
+				grp.GoCtx(func(ctx context.Context) error {
+					var buf []byte
+					atFirstLine := true
+					for s := range rejected {
+						buf = append(buf, s...)
+						atFirstLine = false
+					}
+					if atFirstLine {
+						// no rejected rows
+						return nil
+					}
+					rejectedFile := dataFile
+					parsedURI, err := url.Parse(rejectedFile)
+					if err != nil {
+						return err
+					}
+					parsedURI.Path = parsedURI.Path + ".rejected"
+					conf, err := cloud.ExternalStorageConfFromURI(rejectedFile)
+					if err != nil {
+						return err
+					}
+					rejectedStorage, err := makeExternalStorage(ctx, conf)
+					if err != nil {
+						return err
+					}
+					defer rejectedStorage.Close()
+					if err := rejectedStorage.WriteFile(ctx, "", bytes.NewReader(buf)); err != nil {
+						return err
+					}
+					return nil
+				})
+
+				grp.GoCtx(func(ctx context.Context) error {
+					defer close(rejected)
+					if err := fileFunc(ctx, src, dataFileIndex, dataFile, rejected); err != nil {
+						return errors.Wrap(err, dataFile)
+					}
+					return nil
+				})
+
+				if err := grp.Wait(); err != nil {
+					return errors.Wrap(err, dataFile)
+				}
+			} else {
+				if err := fileFunc(ctx, src, dataFileIndex, dataFile, rejected); err != nil {
+					return errors.Wrap(err, dataFile)
+				}
 			}
 			return nil
 		}(); err != nil {

pkg/ccl/importccl/import_stmt.go
@@ -57,6 +57,7 @@ const (
 	importOptionOversample       = "oversample"
 	importOptionSkipFKs          = "skip_foreign_keys"
 	importOptionDisableGlobMatch = "disable_glob_matching"
+	importOptionSaveRejected     = "experimental_save_rejected"
 
 	pgCopyDelimiter = "delimiter"
 	pgCopyNull      = "nullif"
@@ -75,9 +76,10 @@ var importOptionExpectValues = map[string]sql.KVStringOptValidate{
 	mysqlOutfileEnclose:  sql.KVStringOptRequireValue,
 	mysqlOutfileEscape:   sql.KVStringOptRequireValue,
 
-	importOptionSSTSize:    sql.KVStringOptRequireValue,
-	importOptionDecompress: sql.KVStringOptRequireValue,
-	importOptionOversample: sql.KVStringOptRequireValue,
+	importOptionSSTSize:      sql.KVStringOptRequireValue,
+	importOptionDecompress:   sql.KVStringOptRequireValue,
+	importOptionOversample:   sql.KVStringOptRequireValue,
+	importOptionSaveRejected: sql.KVStringOptRequireNoValue,
 
 	importOptionSkipFKs:          sql.KVStringOptRequireNoValue,
 	importOptionDisableGlobMatch: sql.KVStringOptRequireNoValue,
@@ -310,6 +312,11 @@ func importPlanHook(
 			}
 			if override, ok := opts[csvNullIf]; ok {
 				format.MysqlOut.NullEncoding = &override
+			}
+			// TODO(spaskob): Refactor so that the save rejected option
+			// is passed in all import formats not just DELIMITED.
+			if _, ok := opts[importOptionSaveRejected]; ok {
+				format.MysqlOut.SaveRejected = true
 			}
 		case "MYSQLDUMP":
 			telemetry.Count("import.format.mysqldump")

pkg/ccl/importccl/import_stmt_test.go
@@ -45,6 +45,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/pkg/errors"
 	"golang.org/x/sync/errgroup"
 )
@@ -62,13 +63,14 @@ func TestImportData(t *testing.T) {
 	sqlDB.Exec(t, `SET CLUSTER SETTING kv.bulk_ingest.batch_size = '10KB'`)
 
 	tests := []struct {
-		name   string
-		create string
-		with   string
-		typ    string
-		data   string
-		err    string
-		query  map[string][][]string
+		name     string
+		create   string
+		with     string
+		typ      string
+		data     string
+		err      string
+		rejected string
+		query    map[string][][]string
 	}{
 		{
 			name: "duplicate unique index key",
@@ -236,33 +238,64 @@ d
 		},
 
 		// MySQL OUTFILE
+		// If err field is non-empty, the query filed specifies what expect
+		// to get from the rows that are parsed correctly (see option experimental_save_rejected).
 		{
-			name:   "too many imported columns",
-			create: `i int8`,
+			name:   "empty file",
+			create: `a string`,
 			typ:    "DELIMITED",
-			data:   "1\t2",
-			err:    "row 1: too many columns, expected 1",
+			data:   "",
+			query:  map[string][][]string{`SELECT * from t`: {}},
 		},
 		{
-			name:   "cannot parse data",
-			create: `i int8, j int8`,
-			typ:    "DELIMITED",
-			data:   "bad_int\t2",
-			err:    "row 1: parse",
-		},
-		{
-			name:   "unexpected number of columns",
+			name:   "empty field",
 			create: `a string, b string`,
 			typ:    "DELIMITED",
-			data:   "1,2",
-			err:    "row 1: unexpected number of columns, expected 2 got 1",
+			data:   "\t",
+			query:  map[string][][]string{`SELECT * from t`: {{"", ""}}},
 		},
 		{
-			name:   "unexpected number of columns in 1st row",
-			create: `a string, b string`,
+			name:   "empty line",
+			create: `a string`,
 			typ:    "DELIMITED",
-			data:   "1,2\n3\t4",
-			err:    "row 1: unexpected number of columns, expected 2 got 1",
+			data:   "\n",
+			query:  map[string][][]string{`SELECT * from t`: {{""}}},
+		},
+		{
+			name:     "too many imported columns",
+			create:   `i int8`,
+			typ:      "DELIMITED",
+			data:     "1\t2\n3",
+			err:      "row 1: too many columns, got 2 expected 1",
+			rejected: "1\t2\n",
+			query:    map[string][][]string{`SELECT * from t`: {{"3"}}},
+		},
+		{
+			name:     "cannot parse data",
+			create:   `i int8, j int8`,
+			typ:      "DELIMITED",
+			data:     "bad_int\t2\n3\t4",
+			err:      "row 1: parse",
+			rejected: "bad_int\t2\n",
+			query:    map[string][][]string{`SELECT * from t`: {{"3", "4"}}},
+		},
+		{
+			name:     "unexpected number of columns",
+			create:   `a string, b string`,
+			typ:      "DELIMITED",
+			data:     "1,2\n3\t4",
+			err:      "row 1: unexpected number of columns, expected 2 got 1",
+			rejected: "1,2\n",
+			query:    map[string][][]string{`SELECT * from t`: {{"3", "4"}}},
+		},
+		{
+			name:     "unexpected number of columns in 1st row",
+			create:   `a string, b string`,
+			typ:      "DELIMITED",
+			data:     "1,2\n3\t4",
+			err:      "row 1: unexpected number of columns, expected 2 got 1",
+			rejected: "1,2\n",
+			query:    map[string][][]string{`SELECT * from t`: {{"3", "4"}}},
 		},
 		{
 			name:   "field enclosure",
@@ -295,44 +328,54 @@ d
 			},
 		},
 		{
-			name:   "unmatched field enclosure",
-			create: `a string, b string`,
-			with:   `WITH fields_enclosed_by = '$'`,
-			typ:    "DELIMITED",
-			data:   "$foo\tnormal",
-			err:    "row 1: unmatched field enclosure at start of field",
+			name:     "unmatched field enclosure",
+			create:   `a string, b string`,
+			with:     `WITH fields_enclosed_by = '$'`,
+			typ:      "DELIMITED",
+			data:     "$foo\tnormal\nbaz\tbar",
+			err:      "row 1: unmatched field enclosure at start of field",
+			rejected: "$foo\tnormal\nbaz\tbar",
+			query:    map[string][][]string{`SELECT * from t`: {}},
 		},
 		{
-			name:   "unmatched field enclosure at end",
-			create: `a string, b string`,
-			with:   `WITH fields_enclosed_by = '$'`,
-			typ:    "DELIMITED",
-			data:   "foo$\tnormal",
-			err:    "row 1: unmatched field enclosure at end of field",
+			name:     "unmatched field enclosure at end",
+			create:   `a string, b string`,
+			with:     `WITH fields_enclosed_by = '$'`,
+			typ:      "DELIMITED",
+			data:     "foo$\tnormal\nbar\tbaz",
+			err:      "row 1: unmatched field enclosure at end of field",
+			rejected: "foo$\tnormal\n",
+			query:    map[string][][]string{`SELECT * from t`: {{"bar", "baz"}}},
 		},
 		{
-			name:   "unmatched field enclosure 2nd field",
-			create: `a string, b string`,
-			with:   `WITH fields_enclosed_by = '$'`,
-			typ:    "DELIMITED",
-			data:   "normal\t$foo",
-			err:    "row 1: unmatched field enclosure at start of field",
+			name:     "unmatched field enclosure 2nd field",
+			create:   `a string, b string`,
+			with:     `WITH fields_enclosed_by = '$'`,
+			typ:      "DELIMITED",
+			data:     "normal\t$foo",
+			err:      "row 1: unmatched field enclosure at start of field",
+			rejected: "normal\t$foo",
+			query:    map[string][][]string{`SELECT * from t`: {}},
 		},
 		{
-			name:   "unmatched field enclosure at end 2nd field",
-			create: `a string, b string`,
-			with:   `WITH fields_enclosed_by = '$'`,
-			typ:    "DELIMITED",
-			data:   "normal\tfoo$",
-			err:    "row 1: unmatched field enclosure at end of field",
+			name:     "unmatched field enclosure at end 2nd field",
+			create:   `a string, b string`,
+			with:     `WITH fields_enclosed_by = '$'`,
+			typ:      "DELIMITED",
+			data:     "normal\tfoo$",
+			err:      "row 1: unmatched field enclosure at end of field",
+			rejected: "normal\tfoo$",
+			query:    map[string][][]string{`SELECT * from t`: {}},
 		},
 		{
-			name:   "unmatched literal",
-			create: `i int8`,
-			with:   `WITH fields_escaped_by = '\'`,
-			typ:    "DELIMITED",
-			data:   `\`,
-			err:    "row 1: unmatched literal",
+			name:     "unmatched literal",
+			create:   `i int8`,
+			with:     `WITH fields_escaped_by = '\'`,
+			typ:      "DELIMITED",
+			data:     `\`,
+			err:      "row 1: unmatched literal",
+			rejected: `\`,
+			query:    map[string][][]string{`SELECT * from t`: {}},
 		},
 		{
 			name:   "escaped field enclosure",
@@ -366,20 +409,24 @@ d
 			},
 		},
 		{
-			name:   `\N with trailing char`,
-			create: `s STRING`,
-			with:   `WITH fields_escaped_by = '\'`,
-			typ:    "DELIMITED",
-			data:   "\\N1",
-			err:    "row 1: unexpected data after null encoding",
+			name:     `\N with trailing char`,
+			create:   `s STRING`,
+			with:     `WITH fields_escaped_by = '\'`,
+			typ:      "DELIMITED",
+			data:     "\\N1\nfoo",
+			err:      "row 1: unexpected data after null encoding",
+			rejected: "\\N1\n",
+			query:    map[string][][]string{`SELECT * from t`: {{"foo"}}},
 		},
 		{
-			name:   `double null`,
-			create: `s STRING`,
-			with:   `WITH fields_escaped_by = '\'`,
-			typ:    "DELIMITED",
-			data:   "\\N\\N",
-			err:    "row 1: unexpected null encoding",
+			name:     `double null`,
+			create:   `s STRING`,
+			with:     `WITH fields_escaped_by = '\'`,
+			typ:      "DELIMITED",
+			data:     `\N\N`,
+			err:      "row 1: unexpected null encoding",
+			rejected: `\N\N`,
+			query:    map[string][][]string{`SELECT * from t`: {}},
 		},
 		{
 			name:   `null and \N without escape`,
@@ -445,9 +492,7 @@ d
 			with:   `WITH fields_terminated_by = ',', skip = '4'`,
 			typ:    "DELIMITED",
 			data:   "a string, b string\nfoo,normal\nbar,baz",
-			query: map[string][][]string{
-				`SELECT * from t`: {},
-			},
+			query:  map[string][][]string{`SELECT * from t`: {}},
 		},
 		{
 			name:   "skip -1 lines",
@@ -829,10 +874,22 @@ COPY t (a, b, c) FROM stdin;
 		},
 	}
 
-	var dataString string
+	var mockRecorder struct {
+		syncutil.Mutex
+		dataString, rejectedString string
+	}
 	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mockRecorder.Lock()
+		defer mockRecorder.Unlock()
 		if r.Method == "GET" {
-			fmt.Fprint(w, dataString)
+			fmt.Fprint(w, mockRecorder.dataString)
+		}
+		if r.Method == "PUT" {
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				panic(err)
+			}
+			mockRecorder.rejectedString = string(body)
 		}
 	}))
 	defer srv.Close()
@@ -842,29 +899,57 @@ COPY t (a, b, c) FROM stdin;
 	sqlDB.Exec(t, `CREATE TABLE blah (i int8)`)
 	sqlDB.Exec(t, `DROP TABLE blah`)
 
-	for i, tc := range tests {
-		t.Run(fmt.Sprintf("%s: %s", tc.typ, tc.name), func(t *testing.T) {
-			dbName := fmt.Sprintf("d%d", i)
-			sqlDB.Exec(t, fmt.Sprintf(`CREATE DATABASE %s; USE %[1]s`, dbName))
-			defer sqlDB.Exec(t, fmt.Sprintf(`DROP DATABASE %s`, dbName))
-			var q string
-			if tc.create != "" {
-				q = fmt.Sprintf(`IMPORT TABLE t (%s) %s DATA ($1) %s`, tc.create, tc.typ, tc.with)
-			} else {
-				q = fmt.Sprintf(`IMPORT %s ($1) %s`, tc.typ, tc.with)
+	for _, saveRejected := range []bool{false, true} {
+		// this test is big and slow as is, so we can't afford to double it in race.
+		if util.RaceEnabled && saveRejected {
+			continue
+		}
+
+		for i, tc := range tests {
+			if tc.typ != "DELIMITED" && saveRejected {
+				continue
 			}
-			t.Log(q)
-			dataString = tc.data
-			sqlDB.ExpectErr(t, tc.err, q, srv.URL)
-			for query, res := range tc.query {
-				sqlDB.CheckQueryResults(t, query, res)
+			if saveRejected {
+				if tc.with == "" {
+					tc.with = "WITH experimental_save_rejected"
+				} else {
+					tc.with += ", experimental_save_rejected"
+				}
 			}
-		})
+			t.Run(fmt.Sprintf("%s/%s: save_rejected=%v", tc.typ, tc.name, saveRejected), func(t *testing.T) {
+				dbName := fmt.Sprintf("d%d", i)
+				sqlDB.Exec(t, fmt.Sprintf(`CREATE DATABASE %s; USE %[1]s`, dbName))
+				defer sqlDB.Exec(t, fmt.Sprintf(`DROP DATABASE %s`, dbName))
+				var q string
+				if tc.create != "" {
+					q = fmt.Sprintf(`IMPORT TABLE t (%s) %s DATA ($1) %s`, tc.create, tc.typ, tc.with)
+				} else {
+					q = fmt.Sprintf(`IMPORT %s ($1) %s`, tc.typ, tc.with)
+				}
+				t.Log(q, srv.URL, tc.data)
+				mockRecorder.dataString = tc.data
+				mockRecorder.rejectedString = ""
+				if !saveRejected || tc.rejected == "" {
+					sqlDB.ExpectErr(t, tc.err, q, srv.URL)
+				} else {
+					sqlDB.Exec(t, q, srv.URL)
+				}
+				if tc.err == "" || saveRejected {
+					for query, res := range tc.query {
+						sqlDB.CheckQueryResults(t, query, res)
+					}
+					if tc.rejected != mockRecorder.rejectedString {
+						t.Errorf("expected:\n<%v>\ngot:\n<%v>\n", tc.rejected,
+							mockRecorder.rejectedString)
+					}
+				}
+			})
+		}
 	}
 
 	t.Run("mysqlout multiple", func(t *testing.T) {
 		sqlDB.Exec(t, `CREATE DATABASE mysqlout; USE mysqlout`)
-		dataString = "1"
+		mockRecorder.dataString = "1"
 		sqlDB.Exec(t, `IMPORT TABLE t (s STRING) DELIMITED DATA ($1, $1)`, srv.URL)
 		sqlDB.CheckQueryResults(t, `SELECT * FROM t`, [][]string{{"1"}, {"1"}})
 	})

pkg/ccl/importccl/read_import_csv.go
@@ -103,7 +103,7 @@ func (c *csvInputReader) flushBatch(ctx context.Context, finished bool) error {
 }
 
 func (c *csvInputReader) readFile(
-	ctx context.Context, input *fileReader, inputIdx int32, inputName string,
+	ctx context.Context, input *fileReader, inputIdx int32, inputName string, rejected chan string,
 ) error {
 	cr := csv.NewReader(input)
 	if c.opts.Comma != 0 {

pkg/ccl/importccl/read_import_mysql.go
@@ -92,7 +92,7 @@ func (m *mysqldumpReader) readFiles(
 }
 
 func (m *mysqldumpReader) readFile(
-	ctx context.Context, input *fileReader, inputIdx int32, inputName string,
+	ctx context.Context, input *fileReader, inputIdx int32, inputName string, rejected chan string,
 ) error {
 	var inserts, count int64
 	r := bufio.NewReaderSize(input, 1024*64)

pkg/ccl/importccl/read_import_mysql_test.go
@@ -112,7 +112,7 @@ func TestMysqldumpDataReader(t *testing.T) {
 	defer in.Close()
 	wrapped := &fileReader{Reader: in, counter: byteCounter{r: in}}
 
-	if err := converter.readFile(ctx, wrapped, 1, ""); err != nil {
+	if err := converter.readFile(ctx, wrapped, 1, "", nil); err != nil {
 		t.Fatal(err)
 	}
 	converter.inputFinished(ctx)

pkg/ccl/importccl/read_import_mysqlout.go
@@ -21,6 +21,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/storage/cloud"
 	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 )
 
 type mysqloutfileReader struct {
@@ -63,13 +64,15 @@ func (d *mysqloutfileReader) readFiles(
 }
 
 func (d *mysqloutfileReader) readFile(
-	ctx context.Context, input *fileReader, inputIdx int32, inputName string,
+	ctx context.Context, input *fileReader, inputIdx int32, inputName string, rejected chan string,
 ) error {
 	d.conv.KvBatch.Source = inputIdx
 	d.conv.FractionFn = input.ReadFraction
 	var count int64 = 1
+	var countRejected int64
 
 	var row []tree.Datum
+	var savedRow []rune
 	// The current field being read needs to be a list to be able to undo
 	// field enclosures at end of field.
 	var fieldParts []rune
@@ -83,26 +86,62 @@ func (d *mysqloutfileReader) readFile(
 	// which means we do not look for separators until we see the end of the field
 	// as indicated by the matching enclosing char.
 	var readingField bool
+
 	// If we have just encountered a potential encloser symbol.
-	// That means if a end of field or line is next we should honor it.
+	// That means if an end of field or line is next we should honor it.
 	var gotEncloser bool
+
+	// If we are in the middle of processing a row that has errors in it.
+	// That means that we are going to try to guess where the row ends
+	// save it to be retried.
+	var gotOffendingRow bool
 
 	var gotNull bool
 
 	reader := bufio.NewReaderSize(input, 1024*64)
 	addField := func() error {
+		defer func() {
+			fieldParts = fieldParts[:0]
+			readingField = false
+			gotEncloser = false
+		}()
+		// Don't even try to add fields when we are in a bad row.
+		if gotOffendingRow {
+			return nil
+		}
+		if nextLiteral {
+			return makeRowErr(inputName, count, pgcode.Syntax, "unmatched literal")
+		}
+		// If previous symbol was field encloser it should be
+		// dropped as it only marks end of field. Otherwise
+		// throw an error since we don;t expect unmatched encloser.
+		if gotEncloser {
+			// If the encloser marked end of field
+			// drop it.
+			if readingField {
+				fieldParts = fieldParts[:len(fieldParts)-1]
+			} else {
+				// Unexpected since we did not see one at start of field.
+				gotEncloser = false
+				return makeRowErr(inputName, count, pgcode.Syntax,
+					"unmatched field enclosure at end of field")
+			}
+		} else if readingField {
+			return makeRowErr(inputName, count, pgcode.Syntax,
+				"unmatched field enclosure at start of field")
+		}
 		field := string(fieldParts)
 		if len(row) >= len(d.conv.VisibleCols) {
 			return makeRowErr(inputName, count, pgcode.Syntax,
-				"too many columns, expected %d: %#v", len(d.conv.VisibleCols), row)
+				"too many columns, got %d expected %d: %#v", len(row)+1, len(d.conv.VisibleCols), row)
 		}
 		if gotNull {
+			gotNull = false
 			if len(field) != 0 {
 				return makeRowErr(inputName, count, pgcode.Syntax,
 					"unexpected data after null encoding: %v", row)
 			}
 			row = append(row, tree.DNull)
-			gotNull = false
 		} else if !d.opts.HasEscape && (field == "NULL" || d.opts.NullEncoding != nil && field == *d.opts.NullEncoding) {
 			row = append(row, tree.DNull)
 		} else {
@@ -112,24 +151,39 @@ func (d *mysqloutfileReader) readFile(
 				return wrapRowErr(err, inputName, count, pgcode.Syntax,
 					"parse %q as %s", col.Name, col.Type.SQLString())
 			}
-
 			row = append(row, datum)
 		}
-		fieldParts = fieldParts[:0]
 		return nil
 	}
 	addRow := func() error {
+		defer func() {
+			if gotOffendingRow && d.opts.SaveRejected {
+				rejected <- string(savedRow)
+				countRejected++
+			}
+			savedRow = savedRow[:0]
+			gotOffendingRow = false
+			row = row[:0]
+			fieldParts = fieldParts[:0]
+		}()
+		if gotOffendingRow {
+			return nil
+		}
+		if err := addField(); err != nil {
+			gotOffendingRow = true
+			return err
+		}
 		if len(row) != len(d.conv.VisibleCols) {
+			gotOffendingRow = true
 			return makeRowErr(inputName, count, pgcode.Syntax,
 				"unexpected number of columns, expected %d got %d: %#v", len(d.conv.VisibleCols), len(row), row)
 		}
 		copy(d.conv.Datums, row)
 		if err := d.conv.Row(ctx, inputIdx, count); err != nil {
+			gotOffendingRow = true
 			return wrapRowErr(err, inputName, count, pgcode.Uncategorized, "")
 		}
 		count++
-
-		row = row[:0]
 		return nil
 	}
 
@@ -145,58 +199,44 @@ func (d *mysqloutfileReader) readFile(
 			i++
 		}
 	}
-	for {
-		c, w, err := reader.ReadRune()
-		finished := err == io.EOF
+	var c rune
+	var finished bool
+	// Main parsing loop body, returns true to indicate unrecoverable error.
+	// We are being conservative and treating most errors as unrecoverable for now.
+	loop := func() (bool, error) {
+		var err error
+		var w int
+		c, w, err = reader.ReadRune()
+		finished = err == io.EOF
 
 		// First check that if we're done and everything looks good.
 		if finished {
-			if nextLiteral {
-				return makeRowErr(inputName, count, pgcode.Syntax, "unmatched literal")
-			}
-			// If previous symbol was field encloser it should be
-			// dropped as it only marks end of field. Otherwise
-			// throw an error since we don;t expect unmatched encloser.
-			if gotEncloser {
-				if readingField {
-					fieldParts = fieldParts[:len(fieldParts)-1]
-				} else {
-					return makeRowErr(inputName, count, pgcode.Syntax,
-						"unmatched field enclosure at end of field")
-				}
-			} else if readingField {
-				return makeRowErr(inputName, count, pgcode.Syntax,
-					"unmatched field enclosure at start of field")
-			}
-			if len(fieldParts) > 0 {
-				if err := addField(); err != nil {
-					return err
-				}
-			}
-			// flush the last row if we have one.
-			if len(row) > 0 {
+			if len(savedRow) > 0 || (d.opts.SaveRejected && gotOffendingRow) {
+				// flush the last row.
 				if err := addRow(); err != nil {
-					return err
+					return false, err
 				}
 			}
-			break
+			return true, nil
 		}
+		savedRow = append(savedRow, c)
 
 		if err != nil {
-			return err
+			// error on ReadRune that is not io.EOF, we should fail hard.
+			return true, err
 		}
 
 		if c == unicode.ReplacementChar && w == 1 {
 			if err := reader.UnreadRune(); err != nil {
-				return err
+				return true, err
 			}
 			raw, err := reader.ReadByte()
 			if err != nil {
-				return err
+				return true, err
 			}
 			fieldParts = append(fieldParts, rune(raw))
 			gotEncloser = false
-			continue
+			return false, nil
 		}
 
 		// Do we need to check for escaping?
@@ -219,48 +259,39 @@ func (d *mysqloutfileReader) readFile(
 					fieldParts = append(fieldParts, rune(byte(26)))
 				case 'N':
 					if gotNull {
-						return makeRowErr(inputName, count, pgcode.Syntax, "unexpected null encoding")
+						gotNull = false
+						gotOffendingRow = true
+						return false, makeRowErr(inputName, count, pgcode.Syntax, "unexpected null encoding")
 					}
 					gotNull = true
 				default:
 					fieldParts = append(fieldParts, c)
 				}
 				gotEncloser = false
-				continue
+				return false, nil
 			}
 
 			if c == d.opts.Escape {
 				nextLiteral = true
 				gotEncloser = false
-				continue
+				return false, nil
 			}
 		}
 
 		// Are we done with the field, or even the whole row?
 		if (!readingField || gotEncloser) &&
 			(c == d.opts.FieldSeparator || c == d.opts.RowSeparator) {
-			if gotEncloser {
-				// If the encloser marked end of field
-				// drop it.
-				if readingField {
-					fieldParts = fieldParts[:len(fieldParts)-1]
-				} else {
-					// Unexpected since we did not see one at start of field.
-					return makeRowErr(inputName, count, pgcode.Syntax,
-						"unmatched field enclosure at end of field")
-				}
-			}
-			if err := addField(); err != nil {
-				return err
-			}
 			if c == d.opts.RowSeparator {
 				if err := addRow(); err != nil {
-					return err
+					return false, err
+				}
+			} else {
+				if err := addField(); err != nil {
+					gotOffendingRow = true
+					return false, err
 				}
 			}
-			readingField = false
-			gotEncloser = false
-			continue
+			return false, nil
 		}
 
 		if gotEncloser {
@@ -274,12 +305,36 @@ func (d *mysqloutfileReader) readFile(
 		if d.opts.Enclose != roachpb.MySQLOutfileOptions_Never && c == d.opts.Encloser {
 			if !readingField && len(fieldParts) == 0 {
 				readingField = true
-				continue
+				return false, nil
 			}
 			gotEncloser = true
 		}
 
 		fieldParts = append(fieldParts, c)
+		return false, nil
+	}
+	for {
+		br, err := loop()
+		if br {
+			if err != nil {
+				return err
+			}
+			break
+		}
+		if err != nil {
+			if d.opts.SaveRejected {
+				log.Error(ctx, err)
+				if countRejected > 1000 { // TODO(spaskob): turn the magic constant into an option
+					return makeRowErr(inputName, count, pgcode.Syntax,
+						"too many parsing errors encountered %d", countRejected)
+				}
+			} else {
+				return err
+			}
+		}
+		if finished {
+			break
+		}
 	}
 	return d.conv.SendBatch(ctx)
 }

pkg/ccl/importccl/read_import_pgcopy.go
@@ -257,7 +257,7 @@ func (c copyData) String() string {
 }
 
 func (d *pgCopyReader) readFile(
-	ctx context.Context, input *fileReader, inputIdx int32, inputName string,
+	ctx context.Context, input *fileReader, inputIdx int32, inputName string, rejected chan string,
 ) error {
 	s := bufio.NewScanner(input)
 	s.Split(bufio.ScanLines)

commit b0acf0d7d91d651c8ccc279ab971e425b10235fa

importccl: fix rejected file overwriting the original file
There was severe error introduced by #41430 that causes the original
IMPORT file to be overwritten by the rejected file.

Release note (bug fix): fix bug when using ‘experimental_save_rejected’
that would cause the rejected row file to overwrite the original input
file.

pkg/ccl/importccl/read_import_base.go
@@ -205,13 +205,11 @@ func readInputFiles(
 						// no rejected rows
 						return nil
 					}
-					rejectedFile := dataFile
-					parsedURI, err := url.Parse(rejectedFile)
+					rejFn, err := rejectedFilename(dataFile)
 					if err != nil {
 						return err
 					}
-					parsedURI.Path = parsedURI.Path + ".rejected"
-					conf, err := cloud.ExternalStorageConfFromURI(rejectedFile)
+					conf, err := cloud.ExternalStorageConfFromURI(rejFn)
 					if err != nil {
 						return err
 					}
@@ -248,6 +246,15 @@ func readInputFiles(
 		}
 	}
 	return nil
+}
+
+func rejectedFilename(datafile string) (string, error) {
+	parsedURI, err := url.Parse(datafile)
+	if err != nil {
+		return "", err
+	}
+	parsedURI.Path = parsedURI.Path + ".rejected"
+	return parsedURI.String(), nil
 }
 
 func decompressingReader(

pkg/ccl/importccl/read_import_base_test.go
@@ -1,0 +1,44 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package importccl
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+)
+
+func TestRejectedFilename(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	tests := []struct {
+		name     string
+		fname    string
+		rejected string
+	}{
+		{
+			name:     "http",
+			fname:    "http://127.0.0.1",
+			rejected: "http://127.0.0.1/.rejected",
+		},
+		{
+			name:     "nodelocal",
+			fname:    "nodelocal:///file.csv",
+			rejected: "nodelocal:///file.csv.rejected",
+		},
+	}
+	for _, tc := range tests {
+		rej, err := rejectedFilename(tc.fname)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if tc.rejected != rej {
+			t.Errorf("expected:\n%v\ngot:\n%v\n", tc.rejected, rej)
+		}
+	}
+}