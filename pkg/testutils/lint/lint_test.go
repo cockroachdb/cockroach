49f46ed59edf9ce4729e5212067e4049eed62126
Raphael 'kena' Poss <knz@thaumogen.net>
2019-11-18 15:03:10 +0100

    *: new `httputil.Client` and methods, & use them
    Previously, various parts of CockroachDB were using the `http`
    package's Get/Post/Head methods/functions directly. This was defective
    for two reasons:
    
    - it did not implement a request timeout, which could cause
      requests to hang forever if the server was inoperative.
    - it did not obey context cancellation, which would force
      the CockroachDB server to hang forever while trying to
      quit if some HTTP server was blocking.
    
    The last point in particular may cause excessive operator friction
    when restarting a server, by forcing a node to be killed and
    potentially disrupting operation.
    
    This patch fixes the issue by introducing a new `httputil.Client`
    class which implements a customizable timeout, see its constructor
    `NewClientWithTimeout`.  The default instance in
    `httputil.DefaultClient` uses a 3 second timeout.
    
    It also introduces context-aware variants of the `http`
    methods/functions `Get`, `Post` and `Head`.  Note that these variants
    do not _actually_ obey context cancellation when the code is compiled
    with Go 1.12 or prior. The new behavior only kicks in with Go 1.13 or
    later (it requires the new feature `NewRequestWithContext`). However,
    the usability problem should still be somewhat alleviated in Go 1.12
    builds thanks to the request timeout implemented by the new
    `httputil.Client`.
    
    The following components in CockroachDB are then adapted to use the
    new interface:
    
    - avro registration for changefeed encoders: timeout + cancellation
    - telemetry updates: timeout + cancellation
    - binfetcher: timeout + cancellation
    - cloudinfo: timeout + cancellation
    - docgen: timeout, no cancellation (interactive tool)
    - roachprod install: timeout, no cancellation (interactive tool)
    - test server: timeout, no cancellation (tests have timeouts already)
    
    Release note (bug fix): CockroachDB will now less likely hang in an
    inconvenient/inoperative state if it attempts to access an external
    HTTP server that blocks or is overloaded. A possible symptom of
    the bug is a node failing to shut down upon `cockroach quit`. This
    bug is present since at least version 2.0.

--- pkg/testutils/lint/lint_test.go
+++ pkg/testutils/lint/lint_test.go
@@ -330,6 +330,47 @@ func TestLint(t *testing.T) {
 		}
 	})
 
+	t.Run("TestHttputil", func(t *testing.T) {
+		t.Parallel()
+		for _, tc := range []struct {
+			re       string
+			excludes []string
+		}{
+			{re: `\bhttp\.(Get|Put|Head)\(`},
+		} {
+			cmd, stderr, filter, err := dirCmd(
+				pkgDir,
+				"git",
+				append([]string{
+					"grep",
+					"-nE",
+					tc.re,
+					"--",
+					"*.go",
+				}, tc.excludes...)...,
+			)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if err := cmd.Start(); err != nil {
+				t.Fatal(err)
+			}
+
+			if err := stream.ForEach(filter, func(s string) {
+				t.Errorf("\n%s <- forbidden; use 'httputil' instead", s)
+			}); err != nil {
+				t.Error(err)
+			}
+
+			if err := cmd.Wait(); err != nil {
+				if out := stderr.String(); len(out) > 0 {
+					t.Fatalf("err=%s, stderr=%s", err, out)
+				}
+			}
+		}
+	})
+
 	t.Run("TestEnvutil", func(t *testing.T) {
 		t.Parallel()
 		for _, tc := range []struct {
