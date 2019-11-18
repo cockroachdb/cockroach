f4801e3f79f73270d9b51f7ecf25b884955c8e64
Raphael 'kena' Poss <knz@thaumogen.net>
2019-11-18 18:59:30 +0100

    blobs, fileutil: properly handle cross-filesystem renames
    Prior to this patch, the `(*localStorage) WriteFile()` method was
    expecting to be able to use `os.Rename` to move a newly created file
    from `$TMPDIR` to the external I/O directory.
    
    This is bound to fail hard in most deployment scenarios, where
    typically `/tmp` is on a different filesystem (most often RAM-only).
    
    This patch fixes it by introducing a more correct `fileutil.Move()`
    function and using it from the blob store.
    
    Release note (bug fix): CockroachDB will now properly handle cases
    where the system's temporary directory lives on a different filesystem
    from the external I/O directory. Symptoms of the bug would be an error
    when trying to export data using a `nodelocal://` URL.

---
+++ pkg/util/fileutil/move_windows.go
@@ -0,0 +1,21 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+// +build windows
+
+package fileutil
+
+import "syscall"
+
+func isCrossDeviceLinkErrno(errno error) bool {
+	// 0x11 is Win32 Error Code ERROR_NOT_SAME_DEVICE
+	// See: https://msdn.microsoft.com/en-us/library/cc231199.aspx
+	return errno == syscall.Errno(0x11)
+}
