94bc9427072b3b9fabe2a6faba2cfd69a2ce858f
Radu Berinde <radu@cockroachlabs.com>
2019-11-19 11:28:19 -0500

    sql: scan only primary column for FK checks
    This change improves the legacy FK path to only scan the primary
    column family. The new path already does this (as long as the best
    plan is a lookup join). Adding a test for both paths.
    
    Fixes #30852.
    
    Release note: None

--- pkg/sql/row/fk_spans.go
+++ pkg/sql/row/fk_spans.go
@@ -11,6 +11,7 @@
 package row
 
 import (
+	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
@@ -19,12 +20,29 @@ import (
 // spanForValues produce access spans for a single FK constraint and a
 // tuple of columns.
 func (f fkExistenceCheckBaseHelper) spanForValues(values tree.Datums) (roachpb.Span, error) {
-	var key roachpb.Key
-	if values != nil {
-		span, _, err := sqlbase.EncodePartialIndexSpan(
-			f.searchTable.TableDesc(), f.searchIdx, f.prefixLen, f.ids, values, f.searchPrefix)
-		return span, err
+	if values == nil {
+		key := roachpb.Key(f.searchPrefix)
+		return roachpb.Span{Key: key, EndKey: key.PrefixEnd()}, nil
 	}
-	key = roachpb.Key(f.searchPrefix)
-	return roachpb.Span{Key: key, EndKey: key.PrefixEnd()}, nil
+
+	// If we are scanning the (entire) primary key, only scan family 0 (which is
+	// always present).
+	// TODO(radu): this logic will need to be improved when secondary indexes also
+	// conform to families.
+	if f.searchIdx.ID == f.searchTable.PrimaryIndex.ID && f.prefixLen == len(f.searchIdx.ColumnIDs) {
+		// This code is equivalent to calling EncodePartialIndexSpan followed by
+		// MakeFamilyKey but saves an unnecessary allocation.
+		key, _, err := sqlbase.EncodePartialIndexKey(
+			f.searchTable.TableDesc(), f.searchIdx, f.prefixLen, f.ids, values, f.searchPrefix,
+		)
+		if err != nil {
+			return roachpb.Span{}, err
+		}
+		key = keys.MakeFamilyKey(key, 0)
+		return roachpb.Span{Key: key, EndKey: roachpb.Key(key).PrefixEnd()}, nil
+	}
+
+	span, _, err := sqlbase.EncodePartialIndexSpan(
+		f.searchTable.TableDesc(), f.searchIdx, f.prefixLen, f.ids, values, f.searchPrefix)
+	return span, err
 }
