d49e62a1206fe4baf4fe972319e027aae73a4296
Peter Mattis <petermattis@gmail.com>
2019-11-20 13:16:38 -0500

    cli: fix Pebble {MustExist,ReadOnly} handling in OpenExistingStore
    Fix the handling of of `OpenEngineOptions.{MustExist,ReadOnly}` for
    Pebble. Fixes `TestOpenExistingStore` and `TestOpenReadOnlyStore` on
    Pebble.
    
    Release note: None

--- pkg/storage/engine/engine.go
+++ pkg/storage/engine/engine.go
@@ -526,7 +526,6 @@ func NewEngine(
 			Opts:          DefaultPebbleOptions(),
 		}
 		pebbleConfig.Opts.Cache = pebble.NewCache(cacheSize)
-		pebbleConfig.Opts.ErrorIfNotExists = storageConfig.MustExist
 
 		return NewPebble(context.Background(), pebbleConfig)
 	case enginepb.EngineTypeRocksDB:
