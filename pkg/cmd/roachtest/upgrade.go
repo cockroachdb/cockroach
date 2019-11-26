6730f6531e4e2151d6bf672d24eabe53e3b11c5e
Andrei Matei <andrei@cockroachlabs.com>
2019-11-26 15:52:50 -0500

    roachtest: use latest release in acceptance/version-upgrade
    Release note: None

--- pkg/cmd/roachtest/upgrade.go
+++ pkg/cmd/roachtest/upgrade.go
@@ -477,8 +477,7 @@ func runVersionUpgrade(ctx context.Context, t *test, c *cluster) {
 		binaryVersionUpgrade("v19.1.5", nodes),
 		clusterVersionUpgrade(""),
 
-		// TODO(andrei): Change to the final 19.2 version once released.
-		binaryVersionUpgrade("v19.2.0-rc.2", nodes),
+		binaryVersionUpgrade("v19.2.1", nodes),
 		clusterVersionUpgrade(""),
 
 		// Each new release has to be added here. When adding a new release, you'll
