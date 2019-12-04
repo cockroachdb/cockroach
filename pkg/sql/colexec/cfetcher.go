970d45fb21ece12405344d76e17d293d1b022e62
Yahor Yuzefovich <yahor@cockroachlabs.com>
2019-12-04 10:10:19 -0800

    colexec: fix handling of Unhandled type in cfetcher, ResetInternalBatch
    We recently merged a change to allow for 'unknown' vectors to be present
    among colvecs. However, we forgot to update the resetting of the batch
    - unknown columns do not have nulls and don't need to be reset. Now this
    is fixed.
    
    Also this commit removes an unused field in cfetcher.
    
    Release note: None

--- pkg/sql/colexec/cfetcher.go
+++ pkg/sql/colexec/cfetcher.go
@@ -57,9 +57,6 @@ type cTableInfo struct {
 	// schema changes.
 	cols []sqlbase.ColumnDescriptor
 
-	// The exec types corresponding to the table columns in cols.
-	typs []coltypes.T
-
 	// The ordered list of ColumnIDs that are required.
 	neededColsList []int
 
@@ -280,7 +277,6 @@ func (rf *cFetcher) Init(
 		index:            tableArgs.Index,
 		isSecondaryIndex: tableArgs.IsSecondaryIndex,
 		cols:             colDescriptors,
-		typs:             typs,
 
 		// These slice fields might get re-allocated below, so reslice them from
 		// the old table here in case they've got enough capacity already.
@@ -596,8 +592,10 @@ func (rf *cFetcher) NextBatch(ctx context.Context) (coldata.Batch, error) {
 			rf.machine.state[0] = stateDecodeFirstKVOfRow
 
 		case stateResetBatch:
-			for i := range rf.machine.colvecs {
-				rf.machine.colvecs[i].Nulls().UnsetNulls()
+			for _, colvec := range rf.machine.colvecs {
+				if colvec.Type() != coltypes.Unhandled {
+					colvec.Nulls().UnsetNulls()
+				}
 			}
 			rf.machine.batch.ResetInternalBatch()
 			rf.shiftState()
