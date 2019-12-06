d87cb5834fcd71894af9b0cd64ff0935e26f4bb7
Matt Jibson <matt.jibson@gmail.com>
2019-12-05 17:07:53 -0700

    cmd/skiperrs: add small cmd to skip erroring SQL
    I have wanted this program many times. We can reuse the sql stream code
    and most of the hard work is already done.
    
    Release note: None

--- pkg/cmd/cr2pg/main.go
+++ pkg/cmd/cr2pg/main.go
@@ -19,7 +19,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/cockroachdb/cockroach/pkg/sql/parser"
+	"github.com/cockroachdb/cockroach/pkg/cmd/cr2pg/sqlstream"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/pkg/errors"
 	"golang.org/x/sync/errgroup"
@@ -35,10 +35,9 @@ func main() {
 	// Divide up work between parsing, filtering, and writing.
 	g.Go(func() error {
 		defer close(readStmts)
-		const maxStatementSize = 1024 * 1024 * 32
-		stream := newSQLStream(os.Stdin, maxStatementSize)
+		stream := sqlstream.NewStream(os.Stdin)
 		for {
-			stmt, err := stream.next()
+			stmt, err := stream.Next()
 			if err == io.EOF {
 				break
 			}
@@ -136,62 +135,4 @@ func main() {
 	if err := g.Wait(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-// sqlStream converts an io.Reader into a stream of tree.Statement. Modified
-// from importccl/read_import_pgdump.go.
-type sqlStream struct {
-	s *bufio.Scanner
-}
-
-// newSQLStream returns a struct that can stream statements from an
-// io.Reader.
-func newSQLStream(r io.Reader, max int) *sqlStream {
-	s := bufio.NewScanner(r)
-	s.Buffer(make([]byte, 0, max), max)
-	p := &sqlStream{s: s}
-	s.Split(splitSQLSemicolon)
-	return p
-}
-
-// splitSQLSemicolon is a bufio.SplitFunc that splits on SQL semicolon tokens.
-func splitSQLSemicolon(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	if atEOF && len(data) == 0 {
-		return 0, nil, nil
-	}
-
-	if pos, ok := parser.SplitFirstStatement(string(data)); ok {
-		return pos, data[:pos], nil
-	}
-	// If we're at EOF, we have a final, non-terminated line. Return it.
-	if atEOF {
-		return len(data), data, nil
-	}
-	// Request more data.
-	return 0, nil, nil
-}
-
-func (p *sqlStream) next() (tree.Statement, error) {
-	for p.s.Scan() {
-		t := p.s.Text()
-		stmts, err := parser.Parse(t)
-		if err != nil {
-			return nil, err
-		}
-		switch len(stmts) {
-		case 0:
-			// Got whitespace or comments; try again.
-		case 1:
-			return stmts[0].AST, nil
-		default:
-			return nil, errors.Errorf("unexpected: got %d statements", len(stmts))
-		}
-	}
-	if err := p.s.Err(); err != nil {
-		if err == bufio.ErrTooLong {
-			err = errors.New("line too long")
-		}
-		return nil, err
-	}
-	return nil, io.EOF
 }
