d20f69f148eac21fff82a42fa059a6ccad24227a
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2019-12-08 16:10:04 -0500

    storage: don't block reads on lease transfer
    The decision to block reads on lease transfers has a long history. All lease
    changes began blocking reads in 2896b8c. This was later reduced to just lease
    transfers (not extensions) in 231089c. The reason for blocking reads on lease
    transfers was that "if we didn't block concurrent reads, there'd be a chance
    that reads could sneak in on a new lease holder between setting the lease and
    updating the low water mark [of the timestamp cache]."
    
    As the corresponding TODO mentions, it's not clear that this would actually
    cause any issues. It's actually much more likely that it would cause issues
    for writes, which actually consult the timestamp cache. However, none of this
    is a concern in practice because `Replica.leasePostApply` updates the timestamp
    cache before updating the Replica's lease, so such a race is not possible.
    This has been the case since c54d9b0.
    
    This commit addresses the TODO and stops blocking reads on lease transfers.
    
    Release note: None

--- pkg/storage/client_replica_test.go
+++ pkg/storage/client_replica_test.go
@@ -1152,13 +1152,9 @@ func TestLeaseNotUsedAfterRestart(t *testing.T) {
 }
 
 // Test that a lease extension (a RequestLeaseRequest that doesn't change the
-// lease holder) is not blocked by ongoing reads.
-// The test relies on two things:
-// 1) Lease extensions, unlike lease transfers, are not blocked by reads through
-//    their ReplicatedEvalResult.BlockReads.
-// 2) Requests such as RequestLeaseRequest don't declare to touch the whole key
-//    span of the range, and thus don't acquire latches that conflict with other
-//    reads.
+// lease holder) is not blocked by ongoing reads. The test relies on the fact
+// that RequestLeaseRequest does not declare to touch the whole key span of the
+// range, and thus don't conflict through the command queue with other reads.
 func TestLeaseExtensionNotBlockedByRead(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	readBlocked := make(chan struct{})
