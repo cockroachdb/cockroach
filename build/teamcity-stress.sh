bddc86981c48adf7be563adb760b232305b02960
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2019-12-10 13:49:43 +0100

    build: use -maxtime 1h in nightly unit test stress
    I noticed that some nightly stress builds were taking ~10h just for a
    single package. This is because of the `-race -p 2` flags under which
    some packages (sql, storage) take >5 minutes to complete. Getting
    through 100 iterations (while perfectly reasonable for smaller packages)
    does not make sense for these behemoths, so we want them to stop after
    one hour.
    
    The caveat here is that if the package doesn't even get through a single
    iteration -maxtime at the time of writing would call it a success
    anyway. That is being fixed in https://github.com/cockroachdb/stress/pull/12
    but also we're still adding `-timeout 40m` so we're safe anyway.
    
    Release note: None

--- build/teamcity-stress.sh
+++ build/teamcity-stress.sh
@@ -46,7 +46,7 @@ go install ./pkg/cmd/github-post
 # are test failures.
 # Use an `if` so that the `-e` option doesn't stop the script on error.
 if ! stdbuf -oL -eL \
-  make stress PKG="$PKG" TESTTIMEOUT=40m GOFLAGS="$GOFLAGS" TAGS="$TAGS" STRESSFLAGS="-maxruns 100 -maxfails 1 -stderr $STRESSFLAGS" 2>&1 \
+  make stress PKG="$PKG" TESTTIMEOUT=40m GOFLAGS="$GOFLAGS" TAGS="$TAGS" STRESSFLAGS="-maxruns 100 -maxtime 1h -maxfails 1 -stderr $STRESSFLAGS" 2>&1 \
   | tee artifacts/stress.log; then
   exit_status=${PIPESTATUS[0]}
   go tool test2json -t < artifacts/stress.log | github-post
