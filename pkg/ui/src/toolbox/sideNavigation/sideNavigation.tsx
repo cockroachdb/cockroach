3db3c1d5e6be1a7b7c763665383d0da22714e6e7
Andrii Vorobiov <and.vorobiov@gmail.com>
2019-12-10 17:59:33 +0200

    ui: Add active and disabled states for SideNavigation component
    Release note: None

--- pkg/ui/src/toolbox/sideNavigation/sideNavigation.tsx
+++ pkg/ui/src/toolbox/sideNavigation/sideNavigation.tsx
@@ -9,6 +9,7 @@
 // licenses/APL.txt.
 
 import * as React from "react";
+import cn from "classnames";
 
 import { Text, TextTypes } from "src/toolbox";
 
@@ -27,15 +28,34 @@ export interface NavigationItem {
 }
 
 export function NavigationItem(props: NavigationItem) {
-  const { children } = props;
+  const { children, isActive, disabled } = props;
+  let textType = TextTypes.Body;
+
+  if (isActive) {
+    textType = TextTypes.BodyStrong;
+  }
+
+  const classes = cn(
+    "side-navigation__navigation-item",
+    {
+      "side-navigation__navigation-item--active": isActive,
+      "side-navigation__navigation-item--disabled": disabled,
+    },
+  );
+
   return (
     <li
-      className="side-navigation__navigation-item">
-      <Text textType={TextTypes.Body}>
+      className={classes}>
+      <Text textType={textType}>
         {children}
       </Text>
     </li>
   );
+}
+
+NavigationItem.defaultProps = {
+  isActive: false,
+  disabled: false,
 }
 
 SideNavigation.Item = NavigationItem;
