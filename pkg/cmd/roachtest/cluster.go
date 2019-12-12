e4b453a6dc72ce4882d8da5e1fed7992c46b59d8
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2019-12-12 10:22:04 +0100

    roachtest: default Run{,E} to a per-invocation file
    A regular complaint about roachtest is that it is difficult to reason
    about the flow of the test given only the log files. This is in part
    because the main log often contains lenghty workload outputs that create
    large files with lots of non-timestamped sections where output is often
    even hard to assign to a specific process. Questions that often come up
    are "did this process already terminate at this point?", "Did it ever
    print output?", "What process does this log line belong to", "What is
    this test even doing? All I'm seeing are pages and pages of some bogus
    workload output",among many others.
    
    This commit changes the default behavior to creating a unique file per
    invocation (sorting in timestamp order) and streaming the output there
    and only there. Where streaming to stdout/stderr is desired, `RunL` can
    be used instead.
    
    Certainly a lot more can be done here, but this could already help quite
    a bit in keeping the main log manageable in size while also allowing us
    to better diagnose test failures.
    
    Release note: None

--- pkg/cmd/roachtest/cluster.go
+++ pkg/cmd/roachtest/cluster.go
@@ -1930,7 +1930,7 @@ func (c *cluster) Wipe(ctx context.Context, opts ...option) {
 
 // Run a command on the specified node.
 func (c *cluster) Run(ctx context.Context, node nodeListOption, args ...string) {
-	err := c.RunL(ctx, c.l, node, args...)
+	err := c.RunE(ctx, node, args...)
 	if err != nil {
 		c.t.Fatal(err)
 	}
@@ -1956,9 +1956,21 @@ func (c *cluster) Install(
 		append([]string{roachprod, "install", c.makeNodes(node), "--"}, args...)...)
 }
 
-// RunE runs a command on the specified node, returning an error.
+// RunE runs a command on the specified node, returning an error. The output will
+// be redirected to a file whose name will be logged by the cluster.
 func (c *cluster) RunE(ctx context.Context, node nodeListOption, args ...string) error {
-	return c.RunL(ctx, c.l, node, args...)
+	dest := fmt.Sprintf(`run_%s_%s_%s`,
+		timeutil.Now().Format(`15:04:05.000`),
+		c.makeNodes(node),
+		regexp.MustCompile(`[^a-z0-9_]+`).ReplaceAllString(strings.Join(args, "_"), "_"),
+	)
+	// NB: we set no prefix because it's only going to a file anyway.
+	l, err := c.l.ChildLogger(dest, quietStderr, quietStdout)
+	if err != nil {
+		return err
+	}
+	c.l.PrintfCtx(ctx, "redirecting output to artifact %s", dest)
+	return errors.Wrapf(c.RunL(ctx, l, node, args...), "output in %s", dest)
 }
 
 // RunL runs a command on the specified node, returning an error.
