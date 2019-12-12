dec8571ddb24b8dc44e15fca7521929cdf915b9c
Andrii Vorobiov <and.vorobiov@gmail.com>
2019-12-12 19:33:09 +0200

    ui: Redesign Navigation panel view and Main Navigation panel
    Release note: None

--- pkg/ui/src/views/app/containers/layout/index.tsx
+++ pkg/ui/src/views/app/containers/layout/index.tsx
@@ -16,8 +16,11 @@ import NavigationBar from "src/views/app/components/layoutSidebar";
 import TimeWindowManager from "src/views/app/containers/timewindow";
 import AlertBanner from "src/views/app/containers/alertBanner";
 import RequireLogin from "src/views/login/requireLogin";
+import { GlobalNavigation, CockroachLabsLockupIcon, Left, Right, TabNavigation, Text, TextTypes } from "src/toolbox";
+import LoginIndicator from "src/views/app/components/loginIndicator";
 
 import "./layout.styl";
+import "./layoutPanel.styl";
 
 /**
  * Defines the main layout of all admin ui pages. This includes static
@@ -29,12 +32,42 @@ export default class extends React.Component<RouterState, {}> {
   render() {
     return (
       <RequireLogin>
-        <Helmet titleTemplate="%s | Cockroach Console" defaultTitle="Cockroach Console" />
+        <Helmet
+          titleTemplate="%s | Cockroach Console"
+          defaultTitle="Cockroach Console"
+        />
         <TimeWindowManager/>
         <AlertBanner/>
-        <NavigationBar/>
-        <div className="page">
-          { this.props.children }
+        <div className="layout-panel">
+          <div className="layout-panel__header">
+            <GlobalNavigation>
+              <Left>
+                <CockroachLabsLockupIcon height={26} />
+              </Left>
+              <Right>
+                <LoginIndicator />
+              </Right>
+            </GlobalNavigation>
+          </div>
+          <div className="layout-panel__navigation-bar">
+            <TabNavigation>
+              <Text textType={TextTypes.Heading2}>Cluster - 01</Text>
+              <Text
+                textType={TextTypes.Body}
+                disabled
+              >
+                Cockroach version 19.2 / 12 regions / 54 machines
+              </Text>
+            </TabNavigation>
+          </div>
+          <div className="layout-panel__body">
+            <div className="layout-panel__sidebar">
+              <NavigationBar/>
+            </div>
+            <div className="layout-panel__content">
+              { this.props.children }
+            </div>
+          </div>
         </div>
       </RequireLogin>
     );
