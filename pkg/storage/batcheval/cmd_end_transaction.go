66255915b3a50b66e1b1e9ebbee393558dd18d2c
Andrei Matei <andrei@cockroachlabs.com>
2019-12-13 15:58:29 -0500

    storage: rationalize updates to br.Timestamp
    This patch fixes an incorrect comment on br.Timestamp that mistakenly
    indicated that the field is only relevant for non-transactional
    requests. It also makes the management of br.Timestamp in
    evaluateBatch() - it used to be pretty unscrutable.
    
    Release note: None

--- pkg/storage/batcheval/cmd_end_transaction.go
+++ pkg/storage/batcheval/cmd_end_transaction.go
@@ -272,6 +272,11 @@ func EndTransaction(
 		if retry, reason, extraMsg := IsEndTransactionTriggeringRetryError(reply.Txn, args); retry {
 			return result.Result{}, roachpb.NewTransactionRetryError(reason, extraMsg)
 		}
+		// Update the read timestamp in case we've essentially refreshed. This
+		// update is important because reply.Txn. ReadTimestamp will make its way
+		// into BatchResponse.Timestamp, which is used to update the timestamp
+		// cache.
+		reply.Txn.ReadTimestamp = reply.Txn.WriteTimestamp
 
 		// If the transaction needs to be staged as part of an implicit commit
 		// before being explicitly committed, write the staged transaction
