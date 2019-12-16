835be37c30b29a83e3a10236948b7e7058a36d5c
Andrii Vorobiov <and.vorobiov@gmail.com>
2019-12-16 12:30:48 +0200

    ui: Fix imports for design system components
    Release note: None

--- pkg/ui/src/views/app/containers/layout/index.tsx
+++ pkg/ui/src/views/app/containers/layout/index.tsx
@@ -16,7 +16,7 @@ import NavigationBar from "src/views/app/components/layoutSidebar";
 import TimeWindowManager from "src/views/app/containers/timewindow";
 import AlertBanner from "src/views/app/containers/alertBanner";
 import RequireLogin from "src/views/login/requireLogin";
-import { GlobalNavigation, CockroachLabsLockupIcon, Left, Right, TabNavigation, Text, TextTypes } from "src/toolbox";
+import { GlobalNavigation, CockroachLabsLockupIcon, Left, Right, TabNavigation, Text, TextTypes } from "src/components";
 import LoginIndicator from "src/views/app/components/loginIndicator";
 
 import "./layout.styl";
