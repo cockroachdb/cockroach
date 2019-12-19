b6b75ff81ee2cc48f95193863ff79350661804c8
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2019-12-19 17:43:38 -0500

    storage: don't use header timestamp to determine txn expiration
    This commit removes the use of a PushRequest's batch header timestamp
    to determine whether a pushee has expired or not. This is the first
    step in a refactor to remove cases where we mutate requests on the
    server before command evaluation.
    
    There is no migration needed for this change because it's impact
    is local to a single node - the leaseholder evaluating the request.
    
    Release note: None

--- pkg/storage/replica_test.go
+++ pkg/storage/replica_test.go
@@ -5555,11 +5555,18 @@ func TestPushTxnHeartbeatTimeout(t *testing.T) {
 			t.Fatalf("unexpected status: %v", test.status)
 		}
 
-		// Now, attempt to push the transaction with Now set to the txn start time + offset.
+		// Now, attempt to push the transaction.
 		args := pushTxnArgs(pusher, pushee, test.pushType)
-		args.PushTo = pushee.ReadTimestamp.Add(test.timeOffset, 0)
+		args.PushTo = pushee.ReadTimestamp.Add(0, 1)
+		h := roachpb.Header{Timestamp: args.PushTo}
 
-		reply, pErr := tc.SendWrappedWith(roachpb.Header{Timestamp: args.PushTo}, &args)
+		// Set the manual clock to the txn start time + offset. This is the time
+		// source used to detect transaction expiration. We make sure to set it
+		// above h.Timestamp to avoid it being updated by the request.
+		now := pushee.ReadTimestamp.Add(test.timeOffset, 0)
+		tc.manualClock.Set(now.WallTime)
+
+		reply, pErr := tc.SendWrappedWith(h, &args)
 		if !testutils.IsPError(pErr, test.expErr) {
 			t.Fatalf("%d: expected error %q; got %v, args=%+v, reply=%+v", i, test.expErr, pErr, args, reply)
 		}
