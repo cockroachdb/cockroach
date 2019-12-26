9e1dfff4ba9ed9a76b167accfcad3389daba4100
Daniel Harrison <daniel.harrison@gmail.com>
2019-12-26 14:14:29 -0800

    kvnemeses: begin scaffolding for a jepsen-style kv test
    Package kvnemeses exercises the KV api with random traffic and then
    validates that the observed behaviors are consistent with our
    guarantees.
    
    A set of Operations are generated which represent usage of the public KV
    api. These include both "workload" operations like Gets and Puts as well
    as "admin" operations like rebalances. These Operations can be handed to
    an Applier, which runs them against the KV api and records the results.
    
    Operations do allow for concurrency (this testing is much less
    interesting otherwise), which means that the state of the KV map is not
    recoverable from _only_ the input. TODO(dan): We can use RangeFeed to
    recover the exact KV history. This plus some Kyle magic can be used to
    check our transactional guarantees.
    
    TODO (in later commits)
    - Validate the log
    - CPut/InitPut/Increment/Delete
    - DeleteRange/ClearRange/RevertRange/Scan/ReverseScan
    - ChangeReplicas/TransferLease
    - ExportRequest
    - AddSSTable
    - Root and leaf transactions
    - GCRequest
    - Protected timestamps
    
    Release note: None

---
+++ pkg/kv/kvnemeses/engine.go
@@ -0,0 +1,86 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package kvnemeses
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/storage/engine"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/google/btree"
+)
+
+// Engine is a simplified version of engine.ReadWriter. It is a multi-version
+// key-value map, meaning that each read or write has an associated timestamp
+// and a read returns the write for the key with the highest timestamp (which is
+// not necessarily the most recently ingested write). Engine is not threadsafe.
+//
+// TODO(dan): If this gets any more complicated, it should probably just be
+// swapped for an in-mem engine.Engine.
+type Engine struct {
+	kvs *btree.BTree
+}
+
+// MakeEngine returns a new Engine.
+func MakeEngine() *Engine {
+	return &Engine{
+		kvs: btree.New(8),
+	}
+}
+
+// Get returns the value for this key with the highest timestamp <= ts. If no
+// such value exists, the returned value's RawBytes is nil.
+func (e *Engine) Get(key roachpb.Key, ts hlc.Timestamp) roachpb.Value {
+	var value roachpb.Value
+	e.kvs.AscendGreaterOrEqual(
+		btreeItem{Key: engine.MVCCKey{Key: key, Timestamp: ts}},
+		func(i btree.Item) bool {
+			if kv := i.(btreeItem); kv.Key.Key.Equal(key) {
+				value = roachpb.Value{
+					Timestamp: kv.Key.Timestamp,
+					RawBytes:  kv.Value,
+				}
+			}
+			return false
+		},
+	)
+	return value
+}
+
+// Put inserts a key/value/timestamp tuple. If an exact key/timestamp pair is
+// Put again, it overwrites the previous value.
+func (e *Engine) Put(key engine.MVCCKey, value []byte) {
+	e.kvs.ReplaceOrInsert(btreeItem{Key: key, Value: value})
+}
+
+// DebugPrint returns the entire contents of this Engine as a string for use in
+// debugging.
+func (e *Engine) DebugPrint(indent string) string {
+	var buf strings.Builder
+	e.kvs.Ascend(func(item btree.Item) bool {
+		if buf.Len() > 0 {
+			buf.WriteString("\n")
+		}
+		kv := item.(btreeItem)
+		fmt.Fprintf(&buf, "%s%s %s -> %s",
+			indent, kv.Key.Key, kv.Key.Timestamp, roachpb.Value{RawBytes: kv.Value}.PrettyPrint())
+		return true
+	})
+	return buf.String()
+}
+
+type btreeItem engine.MVCCKeyValue
+
+func (i btreeItem) Less(o btree.Item) bool {
+	return i.Key.Less(o.(btreeItem).Key)
+}
---
+++ pkg/kv/kvnemeses/operations.go
@@ -0,0 +1,179 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package kvnemeses
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/errors"
+)
+
+// Result returns the Result field of the given Operation.
+func (op Operation) Result() *Result {
+	switch o := op.GetValue().(type) {
+	case *GetOperation:
+		return &o.Result
+	case *PutOperation:
+		return &o.Result
+	case *SplitOperation:
+		return &o.Result
+	case *MergeOperation:
+		return &o.Result
+	case *BatchOperation:
+		return &o.Result
+	case *ClosureTxnOperation:
+		return &o.Result
+	default:
+		panic(errors.AssertionFailedf(`unknown operation: %T %v`, o, o))
+	}
+}
+
+type steps []Step
+
+func (s steps) After() hlc.Timestamp {
+	var ts hlc.Timestamp
+	for _, step := range s {
+		ts.Forward(step.After)
+	}
+	return ts
+}
+
+func (s Step) String() string {
+	fctx := formatCtx{receiver: `db`, indent: ``}
+	var buf strings.Builder
+	s.format(&buf, fctx)
+	return buf.String()
+}
+
+type formatCtx struct {
+	receiver string
+	indent   string
+	// TODO(dan): error handling.
+}
+
+func (s Step) format(w *strings.Builder, fctx formatCtx) {
+	w.WriteString("\n")
+	w.WriteString(fctx.indent)
+	s.Op.format(w, fctx)
+}
+
+func formatOps(w *strings.Builder, fctx formatCtx, ops []Operation) {
+	for _, op := range ops {
+		w.WriteString("\n")
+		w.WriteString(fctx.indent)
+		op.format(w, fctx)
+	}
+}
+
+func (op Operation) String() string {
+	fctx := formatCtx{receiver: `x`, indent: ``}
+	var buf strings.Builder
+	op.format(&buf, fctx)
+	return buf.String()
+}
+
+func (op Operation) format(w *strings.Builder, fctx formatCtx) {
+	switch o := op.GetValue().(type) {
+	case *GetOperation:
+		o.format(w, fctx)
+	case *PutOperation:
+		o.format(w, fctx)
+	case *SplitOperation:
+		o.format(w)
+	case *MergeOperation:
+		o.format(w)
+	case *BatchOperation:
+		newFctx := fctx
+		newFctx.indent = fctx.indent + `  `
+		newFctx.receiver = `b`
+		w.WriteString(`{`)
+		w.WriteString("\n")
+		w.WriteString(newFctx.indent)
+		w.WriteString(`b := &Batch{}`)
+		formatOps(w, newFctx, o.Ops)
+		w.WriteString("\n")
+		w.WriteString(newFctx.indent)
+		w.WriteString(fctx.receiver)
+		w.WriteString(`.Run(ctx, b)`)
+		o.Result.format(w)
+		w.WriteString("\n")
+		w.WriteString(fctx.indent)
+		w.WriteString(`}`)
+	case *ClosureTxnOperation:
+		txnName := `txn` + o.TxnID
+		newFctx := fctx
+		newFctx.indent = fctx.indent + `  `
+		newFctx.receiver = txnName
+		w.WriteString(fctx.receiver)
+		fmt.Fprintf(w, `.Txn(ctx, func(ctx context.Context, %s *client.Txn) error {`, txnName)
+		formatOps(w, newFctx, o.Ops)
+		w.WriteString("\n")
+		w.WriteString(newFctx.indent)
+		switch o.Type {
+		case ClosureTxnType_Commit:
+			w.WriteString(`return nil`)
+		case ClosureTxnType_Rollback:
+			w.WriteString(`return errors.New("rollback")`)
+		default:
+			panic(errors.AssertionFailedf(`unknown closure txn type: %s`, o.Type))
+		}
+		w.WriteString("\n")
+		w.WriteString(fctx.indent)
+		w.WriteString(`})`)
+		o.Result.format(w)
+	default:
+		fmt.Fprintf(w, "%v", op.GetValue())
+	}
+}
+
+func (op GetOperation) format(w *strings.Builder, fctx formatCtx) {
+	fmt.Fprintf(w, `%s.Get(ctx, %s)`, fctx.receiver, roachpb.Key(op.Key))
+	switch op.Result.Type {
+	case ResultType_Error:
+		err := errors.DecodeError(context.TODO(), *op.Result.Err)
+		fmt.Fprintf(w, ` // (nil, %s)`, err.Error())
+	case ResultType_Value:
+		v := `nil`
+		if len(op.Result.Value) > 0 {
+			v = `"` + string(op.Result.Value) + `"`
+		}
+		fmt.Fprintf(w, ` // (%s, nil)`, v)
+	}
+}
+
+func (op PutOperation) format(w *strings.Builder, fctx formatCtx) {
+	fmt.Fprintf(w, `%s.Put(ctx, %s, %s)`, fctx.receiver, roachpb.Key(op.Key), op.Value)
+	op.Result.format(w)
+}
+
+func (op SplitOperation) format(w *strings.Builder) {
+	fmt.Fprintf(w, `db.Split(ctx, %s)`, roachpb.Key(op.Key))
+	op.Result.format(w)
+}
+
+func (op MergeOperation) format(w *strings.Builder) {
+	fmt.Fprintf(w, `db.Merge(ctx, %s)`, roachpb.Key(op.Key))
+	op.Result.format(w)
+}
+
+func (r Result) format(w *strings.Builder) {
+	switch r.Type {
+	case ResultType_Error:
+		err := errors.DecodeError(context.TODO(), *r.Err)
+		fmt.Fprintf(w, ` // %s`, err.Error())
+	case ResultType_NoError:
+		fmt.Fprintf(w, ` // nil`)
+	}
+}
