bd298cba25824cae6c531b1810c488482fa20ba8
James H. Linder <jamesl@cockroachlabs.com>
2020-01-03 10:24:06 -0500

    roachprod: Make multiple set gcloud zones always geo-distribute nodes
    Before: if multiple zones were set for gcloud and --geo wasn't set, all hosts
    would be started in just one zone in one region.
    
    Why change? Because if multiple zones are set, the intention is that they be
    used.
    
    Now, for gcloud:
    
    1) when multiple zones are set in the CLI command, then --geo is assumed.
    2) if no zones are set in the CLI command, then the value of --geo is followed.
    
    Issue #38542.
    
    Release note: None

--- pkg/cmd/roachprod/vm/gce/gcloud.go
+++ pkg/cmd/roachprod/vm/gce/gcloud.go
@@ -194,6 +194,15 @@ type projectsVal struct {
 	opts                   *providerOpts
 }
 
+// defaultZones is the list of  zones used by default for cluster creation.
+// If the geo flag is specified, nodes are distributed between regions and
+// then zones.
+var defaultZones = []string{
+	"us-east1-b",
+	"us-west1-b",
+	"europe-west2-b",
+}
+
 // Set is part of the pflag.Value interface.
 func (v projectsVal) Set(projects string) error {
 	if projects == "" {
@@ -252,15 +261,17 @@ func (p *Provider) GetProjects() []string {
 func (o *providerOpts) ConfigureCreateFlags(flags *pflag.FlagSet) {
 	flags.StringVar(&o.MachineType, "machine-type", "n1-standard-4", "DEPRECATED")
 	_ = flags.MarkDeprecated("machine-type", "use "+ProviderName+"-machine-type instead")
-	flags.StringSliceVar(&o.Zones, "zones", []string{"us-east1-b", "us-west1-b", "europe-west2-b"}, "DEPRECATED")
+	flags.StringSliceVar(&o.Zones, "zones", []string{}, "DEPRECATED")
 	_ = flags.MarkDeprecated("zones", "use "+ProviderName+"-zones instead")
 
 	flags.StringVar(&o.ServiceAccount, ProviderName+"-service-account",
 		os.Getenv("GCE_SERVICE_ACCOUNT"), "Service account to use")
 	flags.StringVar(&o.MachineType, ProviderName+"-machine-type", "n1-standard-4",
 		"Machine type (see https://cloud.google.com/compute/docs/machine-types)")
-	flags.StringSliceVar(&o.Zones, ProviderName+"-zones",
-		[]string{"us-east1-b", "us-west1-b", "europe-west2-b"}, "Zones for cluster")
+	flags.StringSliceVar(&o.Zones, ProviderName+"-zones", []string{},
+		fmt.Sprintf("Zones for cluster. If > 1 zone specified, the cluster will be spread out evenly\n"+
+			"by region and then by zone within a region whether or not --geo is set (default\n[%s])",
+			strings.Join(defaultZones, ",")))
 	flags.StringVar(&o.Image, ProviderName+"-image", "ubuntu-1604-xenial-v20190122a",
 		"Image to use to create the vm, ubuntu-1904-disco-v20191008 is a more modern image")
 	flags.IntVar(&o.SSDCount, ProviderName+"-local-ssd-count", 1,
@@ -341,8 +352,12 @@ func (p *Provider) Create(names []string, opts vm.CreateOpts) error {
 			"`roachprod gc --gce-project=%s` cronjob\n", project)
 	}
 
-	if !opts.GeoDistributed {
-		p.opts.Zones = []string{p.opts.Zones[0]}
+	if len(p.opts.Zones) == 0 {
+		if opts.GeoDistributed {
+			p.opts.Zones = defaultZones
+		} else {
+			p.opts.Zones = []string{defaultZones[0]}
+		}
 	}
 
 	// Fixed args.
