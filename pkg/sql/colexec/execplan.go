e7568dc3a5bef999682968579fc04eb445d69654
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-01-06 14:27:48 -0800

    colexec: add simple implementation of an external sort
    This commit adds a simple implementation of external sort that works
    in two stages:
    1. it will use a combination of a newly introduced input partitioner
    and in-memory sorter to divide up all batches from the input into
    partitions, sort each partition in memory, and write sorted partitions
    to disk
    2. it will use OrderedSynchronizer to merge the partitions.
    
    Release note: None

--- pkg/sql/colexec/execplan.go
+++ pkg/sql/colexec/execplan.go
@@ -97,6 +97,9 @@ type NewColOperatorArgs struct {
 	// set to 'true' in tests.
 	UseStreamingMemAccountForBuffering bool
 	ProcessorConstructor               execinfra.ProcessorConstructor
+	// SpillingCallbackFn will be called when the spilling from an in-memory to
+	// disk-backed operator occurs. It should only be set in tests.
+	SpillingCallbackFn func()
 }
 
 // NewColOperatorResult is a helper struct that encompasses all of the return
@@ -557,7 +560,7 @@ func NewColOperator(
 			if needHash {
 				hashAggregatorMemAccount := streamingMemAccount
 				if !useStreamingMemAccountForBuffering {
-					hashAggregatorMemAccount = result.createBufferingMemAccount(ctx, flowCtx, "hash-aggregator-limited")
+					hashAggregatorMemAccount = result.createBufferingMemAccount(ctx, flowCtx, "hash-aggregator")
 				}
 				result.Op, err = NewHashAggregator(
 					NewAllocator(ctx, hashAggregatorMemAccount), inputs[0], typs, aggFns,
@@ -627,7 +630,7 @@ func NewColOperator(
 
 				hashJoinerMemAccount := streamingMemAccount
 				if !useStreamingMemAccountForBuffering {
-					hashJoinerMemAccount = result.createBufferingMemAccount(ctx, flowCtx, "hash-joiner-limited")
+					hashJoinerMemAccount = result.createBufferingMemAccount(ctx, flowCtx, "hash-joiner")
 				}
 				result.Op, err = NewEqHashJoinerOp(
 					NewAllocator(ctx, hashJoinerMemAccount),
@@ -708,7 +711,7 @@ func NewColOperator(
 				mergeJoinerMemAccount := streamingMemAccount
 				if !result.IsStreaming && !useStreamingMemAccountForBuffering {
 					// Whether the merge joiner is streaming is already set above.
-					mergeJoinerMemAccount = result.createBufferingMemAccount(ctx, flowCtx, "merge-joiner-limited")
+					mergeJoinerMemAccount = result.createBufferingMemAccount(ctx, flowCtx, "merge-joiner")
 				}
 				result.Op, err = NewMergeJoinOp(
 					NewAllocator(ctx, mergeJoinerMemAccount),
@@ -751,7 +754,7 @@ func NewColOperator(
 				if useStreamingMemAccountForBuffering {
 					sortChunksMemAccount = streamingMemAccount
 				} else {
-					sortChunksMemAccount = result.createBufferingMemAccount(ctx, flowCtx, "sort-chunks-limited")
+					sortChunksMemAccount = result.createBufferingMemAccount(ctx, flowCtx, "sort-chunks")
 				}
 				result.Op, err = NewSortChunks(
 					NewAllocator(ctx, sortChunksMemAccount), input, inputTypes,
@@ -769,7 +772,7 @@ func NewColOperator(
 				result.IsStreaming = true
 			} else {
 				// No optimizations possible. Default to the standard sort operator.
-				sorterMemMonitorName := fmt.Sprintf("sort-all-limited-%d", spec.ProcessorID)
+				sorterMemMonitorName := fmt.Sprintf("sort-all-%d", spec.ProcessorID)
 				var sorterMemAccount *mon.BoundAccount
 				if useStreamingMemAccountForBuffering {
 					sorterMemAccount = streamingMemAccount
@@ -788,8 +791,8 @@ func NewColOperator(
 				if useStreamingMemAccountForBuffering {
 					diskSpillerMemAccount = streamingMemAccount
 				} else {
-					diskSpillerMemAccount = result.createBufferingMemAccount(
-						ctx, flowCtx, "disk-spiller-sort-all-limited",
+					diskSpillerMemAccount = result.createBufferingUnlimitedMemAccount(
+						ctx, flowCtx, "disk-spiller-sort-all",
 					)
 				}
 				diskSpillerAllocator := NewAllocator(ctx, diskSpillerMemAccount)
@@ -798,8 +801,27 @@ func NewColOperator(
 					input, inMemorySorter.(bufferingInMemoryOperator),
 					sorterMemMonitorName,
 					func(input Operator) Operator {
-						return newExternalSorter(diskSpillerAllocator, input, inputTypes, orderingCols)
-					})
+						monitorNamePrefix := "external-sorter-"
+						// We are using an unlimited memory monitor here because external
+						// sort itself is responsible for making sure that we stay within
+						// the memory limit.
+						unlimitedAllocator := NewAllocator(
+							ctx, result.createBufferingUnlimitedMemAccount(
+								ctx, flowCtx, monitorNamePrefix,
+							))
+						diskQueuesUnlimitedAllocator := NewAllocator(
+							ctx, result.createBufferingUnlimitedMemAccount(
+								ctx, flowCtx, monitorNamePrefix+"disk-queues",
+							))
+						return newExternalSorter(
+							unlimitedAllocator,
+							input, inputTypes, core.Sorter.OutputOrdering,
+							execinfra.GetWorkMemLimit(flowCtx.Cfg),
+							diskQueuesUnlimitedAllocator,
+						)
+					},
+					args.SpillingCallbackFn,
+				)
 			}
 			result.ColumnTypes = spec.Input[0].ColumnTypes
 
@@ -821,7 +843,7 @@ func NewColOperator(
 				// which kind of partitioner to use should come from the optimizer.
 				windowSortingPartitionerMemAccount := streamingMemAccount
 				if !useStreamingMemAccountForBuffering {
-					windowSortingPartitionerMemAccount = result.createBufferingMemAccount(ctx, flowCtx, "window-sorting-partitioner-limited")
+					windowSortingPartitionerMemAccount = result.createBufferingMemAccount(ctx, flowCtx, "window-sorting-partitioner")
 				}
 				input, err = NewWindowSortingPartitioner(
 					NewAllocator(ctx, windowSortingPartitionerMemAccount), input, typs,
@@ -832,7 +854,7 @@ func NewColOperator(
 				if len(wf.Ordering.Columns) > 0 {
 					windowSorterMemAccount := streamingMemAccount
 					if !useStreamingMemAccountForBuffering {
-						windowSorterMemAccount = result.createBufferingMemAccount(ctx, flowCtx, "window-sorter-limited")
+						windowSorterMemAccount = result.createBufferingMemAccount(ctx, flowCtx, "window-sorter")
 					}
 					input, err = NewSorter(
 						NewAllocator(ctx, windowSorterMemAccount), input, typs,
@@ -1097,10 +1119,25 @@ func (r *NewColOperatorResult) createBufferingMemAccount(
 	ctx context.Context, flowCtx *execinfra.FlowCtx, name string,
 ) *mon.BoundAccount {
 	bufferingOpMemMonitor := execinfra.NewLimitedMonitor(
-		ctx, flowCtx.EvalCtx.Mon, flowCtx.Cfg, name,
+		ctx, flowCtx.EvalCtx.Mon, flowCtx.Cfg, name+"-limited",
 	)
 	r.BufferingOpMemMonitors = append(r.BufferingOpMemMonitors, bufferingOpMemMonitor)
 	bufferingMemAccount := bufferingOpMemMonitor.MakeBoundAccount()
+	r.BufferingOpMemAccounts = append(r.BufferingOpMemAccounts, &bufferingMemAccount)
+	return &bufferingMemAccount
+}
+
+// createBufferingUnlimitedMemAccount instantiates an unlimited memory monitor
+// and a memory account to be used with a buffering disk-backed Operator. The
+// receiver is updated to have references to both objects.
+func (r *NewColOperatorResult) createBufferingUnlimitedMemAccount(
+	ctx context.Context, flowCtx *execinfra.FlowCtx, name string,
+) *mon.BoundAccount {
+	bufferingOpUnlimitedMemMonitor := execinfra.NewMonitor(
+		ctx, flowCtx.EvalCtx.Mon, name+"-unlimited",
+	)
+	r.BufferingOpMemMonitors = append(r.BufferingOpMemMonitors, bufferingOpUnlimitedMemMonitor)
+	bufferingMemAccount := bufferingOpUnlimitedMemMonitor.MakeBoundAccount()
 	r.BufferingOpMemAccounts = append(r.BufferingOpMemAccounts, &bufferingMemAccount)
 	return &bufferingMemAccount
 }
