e793a19f87a7ca8eaa59cd9a0efae550bd488e8f
Justin Jaffray <justin@cockroachlabs.com>
2020-01-07 17:47:33 -0500

    opt: add first half of upsert FK checks
    An Upsert can be decomposed into an insert half and an update half, so
    we must emit the checks for both components. This commit introduces
    checks for the insert half.
    
    Release note (sql change): foreign key checks for insertions performed
    by upserts are now handled by the optimizer.

--- pkg/sql/opt/bench/stub_factory.go
+++ pkg/sql/opt/bench/stub_factory.go
@@ -275,6 +275,7 @@ func (f *stubFactory) ConstructUpsert(
 	returnCols exec.ColumnOrdinalSet,
 	checks exec.CheckOrdinalSet,
 	allowAutoCommit bool,
+	skipFKChecks bool,
 ) (exec.Node, error) {
 	return struct{}{}, nil
 }
--- pkg/sql/opt/exec/execbuilder/mutation.go
+++ pkg/sql/opt/exec/execbuilder/mutation.go
@@ -365,6 +365,7 @@ func (b *Builder) buildUpsert(ups *memo.UpsertExpr) (execPlan, error) {
 	updateColOrds := ordinalSetFromColList(ups.UpdateCols)
 	returnColOrds := ordinalSetFromColList(ups.ReturnCols)
 	checkOrds := ordinalSetFromColList(ups.CheckCols)
+	disableExecFKs := !ups.FKFallback
 	node, err := b.factory.ConstructUpsert(
 		input.root,
 		tab,
@@ -375,8 +376,13 @@ func (b *Builder) buildUpsert(ups *memo.UpsertExpr) (execPlan, error) {
 		returnColOrds,
 		checkOrds,
 		b.allowAutoCommit && len(ups.Checks) == 0,
+		disableExecFKs,
 	)
 	if err != nil {
+		return execPlan{}, err
+	}
+
+	if err := b.buildFKChecks(ups.Checks); err != nil {
 		return execPlan{}, err
 	}
 
