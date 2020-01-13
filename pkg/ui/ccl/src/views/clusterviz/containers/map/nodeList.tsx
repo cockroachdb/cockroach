fd843f679afd47109b749c0a5a738ef9dcb17747
Andrii Vorobiov <and.vorobiov@gmail.com>
2020-01-13 17:17:51 +0200

    ui: Reuse Dropdown component for Node view switcher (Node List / Node Map)
    Redesign of Nodes List view includes change of Node view switcher. Current
    changes reuse Dropdown component and adjust layout styles.
    
    Release note: None

--- pkg/ui/ccl/src/views/clusterviz/containers/map/nodeList.tsx
+++ pkg/ui/ccl/src/views/clusterviz/containers/map/nodeList.tsx
@@ -10,13 +10,17 @@ import React from "react";
 import {InjectedRouter, RouterState} from "react-router";
 
 import { NodesOverview } from "src/views/cluster/containers/nodesOverview";
-import { Select } from "src/components/select";
+import { Dropdown } from "src/components/dropdown";
+import { Button } from "src/components/button";
 
 import "./nodesList.styl";
 
-const Option = Select.Option;
-
 export default class NodeList extends React.Component<RouterState & { router: InjectedRouter }> {
+  readonly items = [
+    { value: "list", name: "Node List" },
+    { value: "map", name: "Node Map" },
+  ];
+
   handleMapTableToggle = (value: string) => {
     this.props.router.push(`/overview/${value}`);
   }
@@ -26,14 +30,14 @@ export default class NodeList extends React.Component<RouterState & { router: In
     return (
       <div className="fixed-panel">
         <div className="fixed-panel__panel-switcher">
-          <Select
-            defaultValue="list"
-            display="link"
+          <Dropdown
+            items={this.items}
             onChange={this.handleMapTableToggle}
           >
-            <Option value="list">Node List</Option>
-            <Option value="map">Node Map</Option>
-          </Select>
+            <Button type="flat" size="small">
+              Node List
+            </Button>
+          </Dropdown>
         </div>
         <div className="fixed-panel__content">
           <NodesOverview />
