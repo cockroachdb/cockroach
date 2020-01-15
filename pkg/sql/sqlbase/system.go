567193636e100b132b93a9bc0b1a3e608afc134a
richardjcai <caioftherichard@gmail.com>
2020-01-15 17:50:08 -0500

    Added role management via CREATEROLE privilege
    Currently, there is no "role management" in cockroach db.
    Created new CREATEROLE privilege to allow users to create roles and users.
    CREATEROLE privilege is tracked via the hasCreateRole column in the system.users table.
    Added migrations for adding hasCreateRole column to system.user and adding the new privilege to the admin user.
    Only users with CREATEROLE privilege are able to CREATE and ALTER roles and users. Granting roles remains unchanged.
    
    Fixes #41858
    
    Release note:
    Modified CREATE ROLE to add "CREATEROLE" option. Added ALTER ROLE command.
    Added CREATEROLE privilege - managed by choosing options when CREATING and ALTERING role.
    Example: CREATE ROLE "role" WITH CREATEROLE"

--- pkg/sql/sqlbase/system.go
+++ pkg/sql/sqlbase/system.go
@@ -69,7 +69,8 @@ CREATE TABLE system.descriptor (
 CREATE TABLE system.users (
   username         STRING PRIMARY KEY,
   "hashedPassword" BYTES,
-  "isRole"         BOOL NOT NULL DEFAULT false
+  "isRole"         BOOL NOT NULL DEFAULT false,
+  "hasCreateRole"  BOOL NOT NULL DEFAULT false
 );`
 
 	// Zone settings per DB/Table.
@@ -427,15 +428,17 @@ var (
 			{Name: "username", ID: 1, Type: *types.String},
 			{Name: "hashedPassword", ID: 2, Type: *types.Bytes, Nullable: true},
 			{Name: "isRole", ID: 3, Type: *types.Bool, DefaultExpr: &falseBoolString},
+			{Name: "hasCreateRole", ID: 4, Type: *types.Bool, DefaultExpr: &falseBoolString},
 		},
-		NextColumnID: 4,
+		NextColumnID: 5,
 		Families: []ColumnFamilyDescriptor{
 			{Name: "primary", ID: 0, ColumnNames: []string{"username"}, ColumnIDs: singleID1},
 			{Name: "fam_2_hashedPassword", ID: 2, ColumnNames: []string{"hashedPassword"}, ColumnIDs: []ColumnID{2}, DefaultColumnID: 2},
 			{Name: "fam_3_isRole", ID: 3, ColumnNames: []string{"isRole"}, ColumnIDs: []ColumnID{3}, DefaultColumnID: 3},
+			{Name: "fam_4_hasCreateRole", ID: 4, ColumnNames: []string{"hasCreateRole"}, ColumnIDs: []ColumnID{4}, DefaultColumnID: 4},
 		},
 		PrimaryIndex:   pk("username"),
-		NextFamilyID:   4,
+		NextFamilyID:   5,
 		NextIndexID:    2,
 		Privileges:     NewCustomSuperuserPrivilegeDescriptor(SystemAllowedPrivileges[keys.UsersTableID]),
 		FormatVersion:  InterleavedFormatVersion,
