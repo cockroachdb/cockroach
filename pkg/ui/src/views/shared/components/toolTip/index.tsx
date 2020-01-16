c9f6b2eba6f2c02f825fc670df1f8c01d52cc067
Vlad Los <carrott9@gmail.com>
2020-01-16 14:50:11 +0200

    ui: statements page
    restyled statement components
    
    Release note (ui): none

--- pkg/ui/src/views/shared/components/toolTip/index.tsx
+++ pkg/ui/src/views/shared/components/toolTip/index.tsx
@@ -8,22 +8,14 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
+import { Tooltip } from "antd";
 import React from "react";
-import classNames from "classnames";
-import Popper from "popper.js";
-
-import "./tooltip.styl";
 
 interface ToolTipWrapperProps {
   text: React.ReactNode;
   short?: boolean;
   children?: React.ReactNode;
 }
-
-interface ToolTipWrapperState {
-  hovered: boolean;
-}
-
 /**
  * ToolTipWrapper wraps its children with an area that detects mouseover events
  * and, when hovered, displays a floating tooltip to the immediate right of
@@ -33,64 +25,14 @@ interface ToolTipWrapperState {
  * such as "float" will render parent elements unable to properly wrap their
  * contents.
  */
-export class ToolTipWrapper extends React.Component<ToolTipWrapperProps, ToolTipWrapperState> {
-  popperInstance: Popper;
-  content: React.RefObject<HTMLDivElement> = React.createRef();
-  text: React.RefObject<HTMLDivElement> = React.createRef();
 
-  constructor(props?: ToolTipWrapperProps, context?: any) {
-    super(props, context);
-    this.state = {
-      hovered: false,
-    };
-  }
-
-  componentWillUnmount() {
-    if (this.popperInstance) {
-      this.popperInstance.destroy();
-    }
-  }
-
-  initPopper() {
-    // PopperOptions.eventsEnabled should be set to `false` to prevent
-    // performance issues on pages with a large number of tooltips
-    this.popperInstance = new Popper(this.content.current, this.text.current, {
-      placement: "auto",
-      eventsEnabled: false,
-    });
-  }
-
-  onMouseEnter = () => {
-    this.setState({hovered: true});
-    this.initPopper();
-  }
-
-  onMouseLeave = () => {
-    this.setState({hovered: false});
-  }
-
+export class ToolTipWrapper extends React.Component<ToolTipWrapperProps, {}> {
   render() {
-    const { text, short } = this.props;
-    const { hovered } = this.state;
-    const tooltipClassNames = classNames({
-      "hover-tooltip": true,
-      "hover-tooltip--hovered": hovered,
-      "hover-tooltip--short": short,
-    });
-
+    const { text, children } = this.props;
     return (
-      <div
-        className={tooltipClassNames}
-        onMouseEnter={this.onMouseEnter}
-        onMouseLeave={this.onMouseLeave}
-      >
-        <div className="hover-tooltip__content" ref={this.content}>
-          { this.props.children }
-        </div>
-        <div className="hover-tooltip__text" ref={this.text}>
-          { text }
-        </div>
-      </div>
+      <Tooltip title={ text } placement="bottom" overlayClassName="tooltip__preset--white">
+        {children}
+      </Tooltip>
     );
   }
 }
