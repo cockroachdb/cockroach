af8be22095318285683f78c3b404e46d25950a2d
Andrii Vorobiov <and.vorobiov@gmail.com>
2020-01-21 12:01:21 +0200

    ui: Fix usage of correct types and react warnings
    - Add `key` prop to react components.
    - Use 'ILiveness' interface across all app to avoid
    typing errors.
    - Remove unnecessary definitions: createLocation.d.ts,
    createPath.d.ts. They are already defined in 'react-router-dom'
    library.
    
    Release note: None

--- pkg/ui/ccl/src/views/clusterviz/containers/map/breadcrumbs.tsx
+++ pkg/ui/ccl/src/views/clusterviz/containers/map/breadcrumbs.tsx
@@ -36,7 +36,7 @@ export class Breadcrumbs extends React.Component<BreadcrumbsProps> {
           />
         {intersperse(
           paths.map((path, idx) => (
-            <span>
+            <span key={idx}>
               {idx === paths.length - 1
                 ? getLocalityLabel(path)
                 : <Link
