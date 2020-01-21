a7f429404980c20763a40a613b51e4ae6c1548a6
Andrii Vorobiov <and.vorobiov@gmail.com>
2020-01-21 12:15:37 +0200

    ui: Fix test data generation for statements tests
    Release note: None

--- pkg/ui/src/views/statements/statements.spec.tsx
+++ pkg/ui/src/views/statements/statements.spec.tsx
@@ -11,6 +11,8 @@
 import { assert } from "chai";
 import Long from "long";
 import moment from "moment";
+import { RouteComponentProps } from "react-router-dom";
+import * as H from "history";
 
 import "src/protobufInit";
 import * as protos from "src/js/protos";
@@ -43,7 +45,7 @@ describe("selectStatements", () => {
 
     const expectedFingerprints = [stmtA, stmtB, stmtC].map(stmt => stmt.key.key_data.query);
     expectedFingerprints.sort();
-    const actualFingerprints = result.map(stmt => stmt.label);
+    const actualFingerprints = result.map((stmt: any) => stmt.label);
     actualFingerprints.sort();
     assert.deepEqual(actualFingerprints, expectedFingerprints);
   });
@@ -444,13 +446,31 @@ function makeStateWithLastReset(lastReset: number) {
 }
 
 function makeRoutePropsWithParams(params: { [key: string]: string }) {
+  const history = H.createHashHistory();
   return {
-    params,
+    location: history.location,
+    history,
+    match: {
+      url: "",
+      path: history.location.pathname,
+      isExact: false,
+      params,
+    },
   };
 }
 
-function makeEmptyRouteProps() {
-  return makeRoutePropsWithParams({});
+function makeEmptyRouteProps(): RouteComponentProps<any> {
+  const history = H.createHashHistory();
+  return {
+    location: history.location,
+    history,
+    match: {
+      url: "",
+      path: history.location.pathname,
+      isExact: false,
+      params: {},
+    },
+  };
 }
 
 function makeRoutePropsWithApp(app: string) {
