e0a2e7d6b8d542a87f956d4c04e346d1b50840df
Vlad Los <carrott9@gmail.com>
2020-01-28 11:31:47 +0200

    ui: sql box
    Removed highlighting table name from sql box
    
    Release note (ui): none

--- pkg/ui/src/views/shared/components/sql/box.tsx
+++ pkg/ui/src/views/shared/components/sql/box.tsx
@@ -8,79 +8,20 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-import _ from "lodash";
 import React from "react";
 import { Highlight } from "./highlight";
 import "./sqlhighlight.styl";
-import TableInfo from "./tableInfo";
 
 interface SqlBoxProps {
   value: string;
 }
-interface SqlBoxState {
-  code: Array<string>;
-}
-
-const regExpTables = /((FROM|INTO|TABLE|UPDATE|CREATE)\s)([^\s([]+)/;
-const regExpList = /FROM|INTO|TABLE|UPDATE|CREATE/g;
-
-class SqlBox extends React.Component<SqlBoxProps, SqlBoxState> {
-  state = {
-    code: [""],
-  };
-
+class SqlBox extends React.Component<SqlBoxProps> {
   preNode: React.RefObject<HTMLPreElement> = React.createRef();
-
-  componentDidMount() {
-    this.replaceTableData();
-  }
-
-  getTableData = (value: string) => {
-    if (!regExpTables.test(value)) {
-      return value;
-    }
-    return value.match(regExpTables)[3];
-  }
-
-  replaceSystemData = (value: string) => {
-    if (!regExpList.test(value)) {
-    return <Highlight value={value} />;
-    } else {
-      const data = _.replace(value, this.getTableData(value), "");
-      // tslint:disable-next-line: no-shadowed-variable
-      const dataValues = _.replace(data, regExpList, (value: string) => `${value}splitIt`);
-      const code = dataValues.split(/splitIt /g);
-      return code.map((stringElement) => (
-        <React.Fragment>
-          <Highlight value={` ${stringElement} `} />
-          <span>
-            {regExpList.test(stringElement) && (
-              <TableInfo
-                title={this.getTableData(value)}
-                params={{
-                  database_name: this.getTableData(value).split(".")[0],
-                  table_name: this.getTableData(value).split(".")[this.getTableData(value).split(".").length - 1],
-                }}
-              />
-            )}
-          </span>
-        </React.Fragment>
-      ));
-    }
-  }
-
-  replaceTableData = () => {
-    let val = this.props.value;
-    val = _.replace(val, regExpList, (value: string) => `splitIt${value}`);
-    const code = val.split(/ splitIt/g);
-    this.setState({ code });
-  }
-
   render() {
-    const { code } = this.state;
+    const { value } = this.props;
     return (
       <div className="box-highlight">
-        {code && code.map((value: string) => this.replaceSystemData(value))}
+        <Highlight value={value} />
       </div>
     );
   }
