be87047d8e7e009e137d3f35778688547c8bb1e4
David Hartunian <davidh@cockroachlabs.com>
2020-01-28 16:48:26 -0500

    build-ui: Add ui-client packages to ui build
    - Adding 2 new packages as linked imports (node_modules just gets
      symlinked)
    - Adjusted webpack vendor build to bundle the new imports properly
      since they're not independently run through babel
    - Adjusted webpack vendor build to omit `ccl` package on ccl builds
    
    Release note: none

--- pkg/ui/webpack.vendor.js
+++ pkg/ui/webpack.vendor.js
@@ -18,48 +18,56 @@ const pkg = require("./package.json");
 const prodDependencies = Object.keys(pkg.dependencies);
 
 // tslint:disable:object-literal-sort-keys
-module.exports = {
-  entry: {
-    vendor: prodDependencies,
-  },
+module.exports = (env) => {
+  return {
+    entry: {
+      vendor: prodDependencies.filter((dep) => {
+        return (env.dist !== "oss" || dep !== "cockroachdb-protobuf-client-ccl")
+      }),
+    },
 
-  mode: "none",
+    output: {
+      filename: "vendor.oss.dll.js",
+      path: path.resolve(__dirname, "dist"),
+      library: "[name]_[hash]",
+    },
 
-  output: {
-    filename: "vendor.oss.dll.js",
-    path: path.resolve(__dirname, "dist"),
-    library: "[name]_[hash]",
-  },
+    mode: "none",
 
-  resolve: {
-    modules: [path.resolve(__dirname, "node_modules")],
-  },
+    resolve: {
+      modules: [path.resolve(__dirname, "node_modules")],
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        // analytics-node is an es6 module and we must run it through babel. As
-        // of 8/25/2017 there appears to be no better way to run babel only on
-        // the specific packages in node_modules that actually use ES6.
-        // https://github.com/babel/babel-loader/issues/171
-        exclude: /node_modules\/(?!analytics-node)/,
-        use: ["cache-loader", "thread-loader", "babel-loader"],
-      },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          // analytics-node is an es6 module and we must run it through babel. As
+          // of 8/25/2017 there appears to be no better way to run babel only on
+          // the specific packages in node_modules that actually use ES6.
+          // https://github.com/babel/babel-loader/issues/171
+          //
+          // davidh: Adding in the protobuf clients as of Jan/2020 to keep us from
+          // having to add too much build infra to the protobuf scripts. Once the
+          // Admin UI is managed separately we'll revisit this.
+          exclude: /node_modules\/(?!((analytics-node)|(cockroachdb-protobuf-client-ccl)|(cockroachdb-protobuf-client-oss)))/,
+          use: ["cache-loader", "thread-loader", "babel-loader"],
+        },
+      ],
+    },
+
+    plugins: [
+      new webpack.DllPlugin({
+        name: "[name]_[hash]",
+        path: path.resolve(__dirname, "vendor.oss.manifest.json"),
+      }),
     ],
-  },
 
-  plugins: [
-    new webpack.DllPlugin({
-      name: "[name]_[hash]",
-      path: path.resolve(__dirname, "vendor.oss.manifest.json"),
-    }),
-  ],
-
-  // Max size of is set to 4Mb to disable warning message and control
-  // the growing size of bundle over time.
-  performance: {
-    maxEntrypointSize: 4000000,
-    maxAssetSize: 4000000,
-  },
+    // Max size of is set to 4Mb to disable warning message and control
+    // the growing size of bundle over time.
+    performance: {
+      maxEntrypointSize: 4000000,
+      maxAssetSize: 4000000,
+    },
+  };
 };
