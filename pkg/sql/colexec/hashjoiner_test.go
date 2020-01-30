84f74659197cd6a1dac390ba245777b81b4de1d5
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-01-29 18:40:19 -0800

    colexec: add external hash joiner
    This commit adds external hash joiner that implements Grace hash join
    algorithm. It is done in two phases:
    1. we read the batches from both inputs and partition them according to
    the vectorized hash function initialized with 2's (unlike default value
    of 1)
    2. once we have exhausted both inputs, we proceed to join phase in which
    we reuse the in-memory hash joiner to actually perform a join operation
    on the tuples from the left and right sides that hashed to the same
    partition. Here, the in-memory hash joiner uses the default value of
    1 when initializing the hash values, so essentially it uses a different
    hash function.
    
    Current limitations:
    - we use fixed number of buckets (4096) in the partitioning phase
    - we do not support recursive partitioning, i.e. if we have a partition
    that will not fit into memory, we will just bail on the query.
    
    Release note: None

--- pkg/sql/colexec/hashjoiner_test.go
+++ pkg/sql/colexec/hashjoiner_test.go
@@ -875,6 +875,8 @@ func init() {
 	}
 }
 
+// createSpecForHashJoiner creates a hash join processor spec based on a test
+// case.
 func createSpecForHashJoiner(tc joinTestCase) *execinfrapb.ProcessorSpec {
 	hjSpec := &execinfrapb.HashJoinerSpec{
 		LeftEqColumns:        tc.leftEqCols,
@@ -905,6 +907,31 @@ func createSpecForHashJoiner(tc joinTestCase) *execinfrapb.ProcessorSpec {
 	}
 }
 
+// runHashJoinTestCase is a helper function that runs a single test case
+// against a hash join operator (either in-memory or disk-backed one) which is
+// created by the provided constructor.
+func runHashJoinTestCase(
+	t *testing.T, tc joinTestCase, hjOpConstructor func(sources []Operator) (Operator, error),
+) {
+	tc.init()
+	if !tc.onExpr.Empty() && tc.joinType != sqlbase.JoinType_INNER {
+		// Currently, onExpr is supported only for INNER join, so we skip all
+		// other cases.
+		return
+	}
+	inputs := []tuples{tc.leftTuples, tc.rightTuples}
+	typs := [][]coltypes.T{tc.leftTypes, tc.rightTypes}
+	var runner testRunner
+	if tc.skipAllNullsInjection {
+		// We're omitting all nulls injection test. See comments for each such
+		// test case.
+		runner = runTestsWithoutAllNullsInjection
+	} else {
+		runner = runTestsWithTyps
+	}
+	runner(t, inputs, typs, tc.expected, unorderedVerifier, hjOpConstructor)
+}
+
 func TestHashJoiner(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 
@@ -926,23 +953,7 @@ func TestHashJoiner(t *testing.T) {
 		}
 		for _, tcs := range [][]joinTestCase{hjTestCases, mjTestCases} {
 			for _, tc := range tcs {
-				tc.init()
-				if !tc.onExpr.Empty() && tc.joinType != sqlbase.JoinType_INNER {
-					// Currently, onExpr is supported only for INNER join, so we skip all
-					// other cases.
-					continue
-				}
-				inputs := []tuples{tc.leftTuples, tc.rightTuples}
-				typs := [][]coltypes.T{tc.leftTypes, tc.rightTypes}
-				var runner testRunner
-				if tc.skipAllNullsInjection {
-					// We're omitting all nulls injection test. See comments for each such
-					// test case.
-					runner = runTestsWithoutAllNullsInjection
-				} else {
-					runner = runTestsWithTyps
-				}
-				runner(t, inputs, typs, tc.expected, unorderedVerifier, func(sources []Operator) (Operator, error) {
+				runHashJoinTestCase(t, tc, func(sources []Operator) (Operator, error) {
 					spec := createSpecForHashJoiner(tc)
 					args := NewColOperatorArgs{
 						Spec:                spec,
@@ -1016,15 +1027,17 @@ func BenchmarkHashJoiner(b *testing.B) {
 										if fullOuter {
 											joinType = sqlbase.JoinType_FULL_OUTER
 										}
-										hj, err := NewHashJoiner(
-											testAllocator,
-											leftSource, rightSource,
+										hjSpec, err := makeHashJoinerSpec(
+											joinType,
 											[]uint32{0, 1}, []uint32{2, 3},
 											sourceTypes, sourceTypes,
 											rightDistinct,
-											joinType,
 										)
 										require.NoError(b, err)
+										hj := newHashJoiner(
+											testAllocator, hjSpec,
+											leftSource, rightSource,
+										)
 										hj.Init()
 
 										for i := 0; i < nBatches; i++ {
