909d01afda1561b2a3c25d52fdec769cfad6987f
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-02-01 19:02:14 -0800

    workload/tpch: unskip query 12
    Previously, query 12 was skipped because the results returned by lib/pq
    didn't match the expected output. The issue is that the driver returned
    []byte for decimals whereas we expected a string value and `fmt.Sprint`
    was giving us the ASCII codes of the digits instead of printing out the
    number. This is fixed by checking whether the returned value is []byte,
    and if so, converting it to string directly.
    
    Release note: None

--- pkg/workload/tpch/expected_rows.go
+++ pkg/workload/tpch/expected_rows.go
@@ -21,14 +21,6 @@ var (
 		`11`: true,
 		`16`: true,
 	}
-	// Note: if you modify queriesToSkip here, please adjust roachtest/tpchvec.go
-	// accordingly.
-	queriesToSkip = map[string]bool{
-		// TODO(yuzefovich): for some reason, second and third columns returned by
-		// query 12 are ASCII codes of the digits, i.e. we expect 6202 but get
-		// [54 50 48 50] instead.
-		`12`: true,
-	}
 )
 
 func init() {
