cf4b65df32ea1bb706c64885f70f8b362fe01fc4
Bilal Akhtar <bilal@cockroachlabs.com>
2020-02-02 16:08:10 -0500

    engine/metamorphic: Add engine restarts, compare across runs`
    This PR builds on top of #44458 (all commits before the last one
    are from that PR). It adds two things: one, it ensures that whenever
    successive engine types are tested, it does a comparison for matching
    outputs. This will ensure CI will fail if any change down the line
    causes an observed difference in behaviour between rocksdb and pebble.
    
    It also adds more MVCC operations that would be useful to test, such
    as MVCCFindSplitKey, MVCCDeleteRange, MVCCClearTimeRange,
    MVCCConditionalPut, etc.
    
    Furthermore, it adds a new kind of operation: restart. Restarts are
    special in that they're added after the specified n number of runs
    have happened; so a test run with 3 specified engine types
    (so 2 restarts), and n = 10000 will have 3 * 10000 = 30000 operations.
    A restart closes all open objects, then closes the engine, and starts
    up the next engine in the specified engine sequence. This, combined
    with the aforementioned checking functionality across different
    engine runs, lets us test for bidirectional compatibility across
    different engines.
    
    Fixes #43762 .
    
    Release note: None.

--- pkg/storage/engine/metamorphic/meta_test.go
+++ pkg/storage/engine/metamorphic/meta_test.go
@@ -18,48 +18,13 @@ import (
 	"math/rand"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
-	"github.com/cockroachdb/cockroach/pkg/base"
-	"github.com/cockroachdb/cockroach/pkg/roachpb"
-	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
-	"github.com/cockroachdb/cockroach/pkg/storage/engine"
-	"github.com/cockroachdb/cockroach/pkg/storage/engine/enginepb"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/util"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 )
-
-func makeStorageConfig(path string) base.StorageConfig {
-	return base.StorageConfig{
-		Attrs:           roachpb.Attributes{},
-		Dir:             path,
-		MustExist:       false,
-		MaxSize:         0,
-		Settings:        cluster.MakeTestingClusterSettings(),
-		UseFileRegistry: false,
-		ExtraOptions:    nil,
-	}
-}
-
-// createTestRocksDBEngine returns a new in-memory RocksDB engine with 1MB of
-// storage capacity.
-func createTestRocksDBEngine(path string) (engine.Engine, error) {
-	return engine.NewEngine(enginepb.EngineTypeRocksDB, 1<<20, makeStorageConfig(path))
-}
-
-// createTestPebbleEngine returns a new in-memory Pebble storage engine.
-func createTestPebbleEngine(path string) (engine.Engine, error) {
-	return engine.NewEngine(enginepb.EngineTypePebble, 1<<20, makeStorageConfig(path))
-}
-
-var mvccEngineImpls = []struct {
-	name   string
-	create func(path string) (engine.Engine, error)
-}{
-	{"rocksdb", createTestRocksDBEngine},
-	{"pebble", createTestPebbleEngine},
-}
 
 var (
 	keep  = flag.Bool("keep", false, "keep temp directories after test")
@@ -67,53 +32,105 @@ var (
 	seed  = flag.Int64("seed", 456, "specify seed to use for random number generator")
 )
 
-func runMetaTest(ctx context.Context, t *testing.T, seed int64, checkFile io.Reader) {
-	for _, engineImpl := range mvccEngineImpls {
-		t.Run(engineImpl.name, func(t *testing.T) {
-			tempDir, cleanup := testutils.TempDir(t)
+func runMetaTestForEngines(ctx context.Context, t *testing.T, seed int64, checkFile io.Reader, outputFile io.Writer, restarts bool, engineImpls []engineImpl) {
+	tempDir, cleanup := testutils.TempDir(t)
+	defer func() {
+		if !*keep {
+			cleanup()
+		}
+	}()
+
+	testRunner := metaTestRunner{
+		ctx:         ctx,
+		t:           t,
+		w:           outputFile,
+		seed:        seed,
+		restarts:    restarts,
+		engineImpls: engineImpls,
+		path:        filepath.Join(tempDir, "store"),
+	}
+
+	testRunner.init()
+	defer testRunner.closeAll()
+	if checkFile != nil {
+		testRunner.parseFileAndRun(checkFile)
+	} else {
+		// TODO(itsbilal): Make this configurable.
+		testRunner.generateAndRun(10000)
+	}
+}
+
+func runMetaTest(
+	ctx context.Context,
+	t *testing.T,
+	seed int64,
+	checkFile string,
+	restarts bool,
+	engineSequences [][]engineImpl,
+) {
+	outerTempDir, cleanup := testutils.TempDir(t)
+	defer func() {
+		if !*keep {
+			cleanup()
+		}
+	}()
+
+	// The test run with the first engine sequence writes its output to this file.
+	// All subsequent engine sequence runs compare their output against this file.
+	firstRunOutput := filepath.Join(outerTempDir, "output.meta")
+	firstRunExecuted := false
+	fmt.Printf("first run output file: %s\n", firstRunOutput)
+
+	for _, engineSequence := range engineSequences {
+		var engineNames []string
+		for _, engineImpl := range engineSequence {
+			engineNames = append(engineNames, engineImpl.name)
+		}
+
+		t.Run(strings.Join(engineNames, ","), func(t *testing.T) {
+			innerTempDir, cleanup := testutils.TempDir(t)
 			defer func() {
 				if !*keep {
 					cleanup()
 				}
 			}()
 
-			eng, err := engineImpl.create(filepath.Join(tempDir, engineImpl.name))
+			// If this is not the first sequence run and a "check" file was not passed
+			// in, use the first run's output file as the check file.
+			var checkFileReader io.ReadCloser
+			if checkFile == "" && firstRunExecuted {
+				checkFile = firstRunOutput
+			}
+			if checkFile != "" {
+				var err error
+				checkFileReader, err = os.Open(checkFile)
+				defer checkFileReader.Close()
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			var outputFileWriter io.WriteCloser
+			outputFile := firstRunOutput
+			if firstRunExecuted {
+				outputFile = filepath.Join(innerTempDir, "output.meta")
+			}
+			var err error
+			outputFileWriter, err = os.Create(outputFile)
+			defer outputFileWriter.Close()
 			if err != nil {
 				t.Fatal(err)
 			}
-			defer eng.Close()
-
-			outputFilePath := filepath.Join(tempDir, fmt.Sprintf("%s.meta", engineImpl.name))
-			fmt.Printf("output file path: %s\n", outputFilePath)
-
-			outputFile, err := os.Create(outputFilePath)
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer outputFile.Close()
-
-			testRunner := metaTestRunner{
-				ctx:    ctx,
-				t:      t,
-				w:      outputFile,
-				seed:   seed,
-				engine: eng,
-			}
-
-			testRunner.init()
-			defer testRunner.closeAll()
-			if checkFile != nil {
-				testRunner.parseFileAndRun(checkFile)
-			} else {
-				// TODO(itsbilal): Make this configurable.
-				testRunner.generateAndRun(10000)
-			}
+			fmt.Printf("check file = %s\noutput file = %s\n", checkFile, outputFile)
+			runMetaTestForEngines(ctx, t, seed, checkFileReader, outputFileWriter, restarts, engineSequence)
+			firstRunExecuted = true
 		})
 	}
 }
 
-// TestMeta runs the MVCC Metamorphic test suite.
-func TestMeta(t *testing.T) {
+// TestRocksPebbleEquivalence runs the MVCC Metamorphic test suite, and checks
+// for matching outputs by the test suite between RocksDB and Pebble.
+func TestRocksPebbleEquivalence(t *testing.T) {
 	defer leaktest.AfterTest(t)
 	ctx := context.Background()
 	if util.RaceEnabled {
@@ -124,23 +141,55 @@ func TestMeta(t *testing.T) {
 	// Have one fixed seed, one user-specified seed, and one random seed.
 	seeds := []int64{123, *seed, rand.Int63()}
 
-	if *check != "" {
-		t.Run("check", func(t *testing.T) {
-			if _, err := os.Stat(*check); os.IsNotExist(err) {
-				t.Fatal(err)
-			}
-			checkFile, err := os.Open(*check)
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer checkFile.Close()
-
-			runMetaTest(ctx, t, 0, checkFile)
-		})
-	}
 	for _, seed := range seeds {
 		t.Run(fmt.Sprintf("seed=%d", seed), func(t *testing.T) {
-			runMetaTest(ctx, t, seed, nil)
+			runMetaTest(ctx, t, seed, "", false, [][]engineImpl{
+				{engineImplRocksDB},
+				{engineImplPebble},
+			})
+		})
+	}
+}
+
+// TestRocksPebbleRestarts runs the MVCC Metamorphic test suite with restarts
+// enabled, and ensures that the output remains the same across different
+// engine sequences with restarts in between.
+func TestRocksPebbleRestarts(t *testing.T) {
+	defer leaktest.AfterTest(t)
+	ctx := context.Background()
+	if util.RaceEnabled {
+		// This test times out with the race detector enabled.
+		return
+	}
+
+	// Have one fixed seed, one user-specified seed, and one random seed.
+	seeds := []int64{123, *seed, rand.Int63()}
+
+	for _, seed := range seeds {
+		t.Run(fmt.Sprintf("seed=%d", seed), func(t *testing.T) {
+			runMetaTest(ctx, t, seed, "", true, [][]engineImpl{
+				{engineImplRocksDB},
+				{engineImplPebble},
+				{engineImplRocksDB, engineImplPebble},
+			})
+		})
+	}
+}
+
+// TestRocksPebbleCheck checks whether the output file specified with --check has
+// matching behaviour across rocks/pebble.
+func TestRocksPebbleCheck(t *testing.T) {
+	defer leaktest.AfterTest(t)
+	ctx := context.Background()
+
+	if *check != "" {
+		if _, err := os.Stat(*check); os.IsNotExist(err) {
+			t.Fatal(err)
+		}
+
+		runMetaTest(ctx, t, 0, *check, true, [][]engineImpl{
+			{engineImplRocksDB},
+			{engineImplPebble},
 		})
 	}
 }
