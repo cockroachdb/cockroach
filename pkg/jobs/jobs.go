d0fdbcda070b78940d30b2c300eb3bc9f5848574
Andrew Werner <ajwerner@cockroachlabs.com>
2020-02-04 17:31:12 -0500

    jobs: create a StartableJob.
    StartableJob isn't great but it's better than what we've got now.
    A StartableJob is a job which is created transactionally but also hooked in
    to a `resultCh` and can be started on the local gateway after the transaction
    commits.
    
    Currently we've got two main ways that jobs are kicked off "synchronously":
    
     1) Registry.Run()
    
    This doesn't currently do anything to kick off the jobs - it just waits for
    all of the relevant jobs to be started. This is used in conjunction with
    `planner.QueueJob` and `CreateJobWithTxn`. The idea here is that when a
    transaction creates a job we want, upon commit, for it to wait for the
    jobs it created to complete.
    
     2) Registry.CreateAndStartJob()
    
    This method has special powers that prevents the gateway node from adopting
    the job and also writes the job with a lease for the local node. It also allows
    the caller to pass a resultsCh which will recieve datums from the job.
    
    That resultCh is critical for IMPORT and BACKUP (?). Currently in both of those
    cases we create and run the job on a transaction outside of the caller's txn.
    
    This is particularly frustrating because the jobs package assigns IDs at
    creation time, making it difficult to transactionally associate a job with
    another resource while also retaining the ability to ensure that it starts
    locally and is connected to a resultsCh. This commit addresses that.
    
    The fact that `IMPORT` and `BACKUP` commit the job before committing the
    planner's txn feels dirty. I'm not planning on mucking with that stuff here in
    this PR but ultimately the work here seems to lay a possible path forward. In
    particular, instead of calling `Run()` with a slice of IDs we could call it
    with a slice of `StartableJob`s which may be hooked up to the channel. In that
    way we could unify CreateAndStartJob() and Run() with something better.
    
    Release note: None.

--- pkg/jobs/jobs.go
+++ pkg/jobs/jobs.go
@@ -13,6 +13,7 @@ package jobs
 import (
 	"context"
 	"fmt"
+	"sync/atomic"
 
 	"github.com/cockroachdb/cockroach/pkg/internal/client"
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
@@ -53,6 +54,18 @@ type Record struct {
 	Details       jobspb.Details
 	Progress      jobspb.ProgressDetails
 	RunningStatus RunningStatus
+}
+
+// StartableJob is a job created with a transaction to be started later.
+// See Registry.CreateStartableJob
+type StartableJob struct {
+	*Job
+	txn        *client.Txn
+	resumer    Resumer
+	resumerCtx context.Context
+	cancel     context.CancelFunc
+	resultsCh  chan<- tree.Datums
+	starts     int64 // used to detect multiple calls to Start()
 }
 
 // Status represents the status of a job in the system.jobs table.
@@ -722,4 +735,46 @@ func (j *Job) CurrentStatus(ctx context.Context) (Status, error) {
 		return "", err
 	}
 	return Status(statusString), nil
+}
+
+// Start will resume a resumable job. The transaction used to create the
+// StartableJob must be committed. If a non-nil error is returned, the job was
+// not started and nothing will be send on errCh. Clients must not start jobs
+// more than once.
+func (sj *StartableJob) Start(ctx context.Context) (errCh <-chan error, err error) {
+	if starts := atomic.AddInt64(&sj.starts, 1); starts != 1 {
+		return nil, errors.AssertionFailedf(
+			"StartableJob %d cannot be started more than once", *sj.ID())
+	}
+	defer func() {
+		if err != nil {
+			sj.registry.unregister(*sj.ID())
+		}
+	}()
+	if !sj.txn.IsCommitted() {
+		return nil, fmt.Errorf("cannot resume %T job which is not committed", sj.resumer)
+	}
+	if err := sj.Started(ctx); err != nil {
+		return nil, err
+	}
+	errCh, err = sj.registry.resume(sj.resumerCtx, sj.resumer, sj.resultsCh, sj.Job)
+	if err != nil {
+		return nil, err
+	}
+	return errCh, nil
+}
+
+// CleanupOnRollback will unregister the job in the case that the creating
+// transaction has been rolled back.
+func (sj *StartableJob) CleanupOnRollback(ctx context.Context) error {
+	if sj.txn.IsCommitted() {
+		return errors.AssertionFailedf(
+			"cannot call CleanupOnRollback for a StartableJob created by a committed transaction")
+	}
+	if !sj.txn.Sender().TxnStatus().IsFinalized() {
+		return errors.AssertionFailedf(
+			"cannot call CleanupOnRollback for a StartableJob with a non-finalized transaction")
+	}
+	sj.registry.unregister(*sj.ID())
+	return nil
 }
