bb1e3bb6a9048bf051229784b77c2dbd137c083b
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2020-02-03 20:12:42 -0500

    cluster: remove VersionQueryTxnTimestamp

--- pkg/settings/cluster/versionkey_string.go
+++ pkg/settings/cluster/versionkey_string.go
@@ -10,33 +10,32 @@ func _() {
 	var x [1]struct{}
 	_ = x[Version19_1-0]
 	_ = x[VersionStart19_2-1]
-	_ = x[VersionQueryTxnTimestamp-2]
-	_ = x[VersionStickyBit-3]
-	_ = x[VersionParallelCommits-4]
-	_ = x[VersionGenerationComparable-5]
-	_ = x[VersionLearnerReplicas-6]
-	_ = x[VersionTopLevelForeignKeys-7]
-	_ = x[VersionAtomicChangeReplicasTrigger-8]
-	_ = x[VersionAtomicChangeReplicas-9]
-	_ = x[VersionTableDescModificationTimeFromMVCC-10]
-	_ = x[VersionPartitionedBackup-11]
-	_ = x[Version19_2-12]
-	_ = x[VersionStart20_1-13]
-	_ = x[VersionContainsEstimatesCounter-14]
-	_ = x[VersionChangeReplicasDemotion-15]
-	_ = x[VersionSecondaryIndexColumnFamilies-16]
-	_ = x[VersionNamespaceTableWithSchemas-17]
-	_ = x[VersionProtectedTimestamps-18]
-	_ = x[VersionPrimaryKeyChanges-19]
-	_ = x[VersionAuthLocalAndTrustRejectMethods-20]
-	_ = x[VersionPrimaryKeyColumnsOutOfFamilyZero-21]
-	_ = x[VersionRootPassword-22]
-	_ = x[VersionNoExplicitForeignKeyIndexIDs-23]
+	_ = x[VersionStickyBit-2]
+	_ = x[VersionParallelCommits-3]
+	_ = x[VersionGenerationComparable-4]
+	_ = x[VersionLearnerReplicas-5]
+	_ = x[VersionTopLevelForeignKeys-6]
+	_ = x[VersionAtomicChangeReplicasTrigger-7]
+	_ = x[VersionAtomicChangeReplicas-8]
+	_ = x[VersionTableDescModificationTimeFromMVCC-9]
+	_ = x[VersionPartitionedBackup-10]
+	_ = x[Version19_2-11]
+	_ = x[VersionStart20_1-12]
+	_ = x[VersionContainsEstimatesCounter-13]
+	_ = x[VersionChangeReplicasDemotion-14]
+	_ = x[VersionSecondaryIndexColumnFamilies-15]
+	_ = x[VersionNamespaceTableWithSchemas-16]
+	_ = x[VersionProtectedTimestamps-17]
+	_ = x[VersionPrimaryKeyChanges-18]
+	_ = x[VersionAuthLocalAndTrustRejectMethods-19]
+	_ = x[VersionPrimaryKeyColumnsOutOfFamilyZero-20]
+	_ = x[VersionRootPassword-21]
+	_ = x[VersionNoExplicitForeignKeyIndexIDs-22]
 }
 
-const _VersionKey_name = "Version19_1VersionStart19_2VersionQueryTxnTimestampVersionStickyBitVersionParallelCommitsVersionGenerationComparableVersionLearnerReplicasVersionTopLevelForeignKeysVersionAtomicChangeReplicasTriggerVersionAtomicChangeReplicasVersionTableDescModificationTimeFromMVCCVersionPartitionedBackupVersion19_2VersionStart20_1VersionContainsEstimatesCounterVersionChangeReplicasDemotionVersionSecondaryIndexColumnFamiliesVersionNamespaceTableWithSchemasVersionProtectedTimestampsVersionPrimaryKeyChangesVersionAuthLocalAndTrustRejectMethodsVersionPrimaryKeyColumnsOutOfFamilyZeroVersionRootPasswordVersionNoExplicitForeignKeyIndexIDs"
+const _VersionKey_name = "Version19_1VersionStart19_2VersionStickyBitVersionParallelCommitsVersionGenerationComparableVersionLearnerReplicasVersionTopLevelForeignKeysVersionAtomicChangeReplicasTriggerVersionAtomicChangeReplicasVersionTableDescModificationTimeFromMVCCVersionPartitionedBackupVersion19_2VersionStart20_1VersionContainsEstimatesCounterVersionChangeReplicasDemotionVersionSecondaryIndexColumnFamiliesVersionNamespaceTableWithSchemasVersionProtectedTimestampsVersionPrimaryKeyChangesVersionAuthLocalAndTrustRejectMethodsVersionPrimaryKeyColumnsOutOfFamilyZeroVersionRootPasswordVersionNoExplicitForeignKeyIndexIDs"
 
-var _VersionKey_index = [...]uint16{0, 11, 27, 51, 67, 89, 116, 138, 164, 198, 225, 265, 289, 300, 316, 347, 376, 411, 443, 469, 493, 530, 569, 588, 623}
+var _VersionKey_index = [...]uint16{0, 11, 27, 43, 65, 92, 114, 140, 174, 201, 241, 265, 276, 292, 323, 352, 387, 419, 445, 469, 506, 545, 564, 599}
 
 func (i VersionKey) String() string {
 	if i < 0 || i >= VersionKey(len(_VersionKey_index)-1) {
