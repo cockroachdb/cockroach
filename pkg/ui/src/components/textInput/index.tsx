2e3536f500f4e1b7367b3316eabb8bc6345784e2
Andrii Vorobiov <and.vorobiov@gmail.com>
2020-02-05 12:54:49 +0200

    ui: Email subscription form
    - Subscription form is responsible to accept user email, validate it and
    submit an appropriate action.
    - InputText component is styled according to design system.
    
    Release note: None

---
+++ pkg/ui/src/components/textInput/index.tsx
@@ -0,0 +1,105 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import React from "react";
+import cn from "classnames";
+
+import { Text, TextTypes } from "src/components";
+import "./textInput.styl";
+
+interface TextInputProps {
+  onChange: (value: string) => void;
+  initialValue?: string;
+  placeholder?: string;
+  className?: string;
+  name?: string;
+  // validate function returns validation message
+  // in case validation failed or undefined if successful.
+  validate?: (value: string) => string | undefined;
+}
+
+interface TextInputState {
+  value: string;
+  validationMessage: string;
+  isValid: boolean;
+}
+
+export class TextInput extends React.Component<TextInputProps, TextInputState> {
+  static defaultProps = {
+    initialValue: "",
+    validate: () => true,
+  };
+
+  constructor(props: TextInputProps) {
+    super(props);
+
+    this.state = {
+      value: "",
+      isValid: true,
+      validationMessage: undefined,
+    };
+  }
+
+  handleOnTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    this.setState({
+      value,
+    });
+    this.props.onChange(value);
+  }
+
+  handleOnBlur = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    const { validate } = this.props;
+    const validationMessage = validate(value);
+
+    this.setState({
+      isValid: !Boolean(validationMessage),
+      validationMessage,
+    });
+  }
+
+  render() {
+    const { initialValue, placeholder, className, name } = this.props;
+    const { value, isValid, validationMessage } = this.state;
+    const textValue = value || initialValue;
+
+    const classes = cn(
+      className,
+      "crl-text-input",
+      {
+        "crl-text-input--invalid": !isValid,
+      },
+    );
+    return (
+      <div className="crl-text-input__wrapper">
+        <input
+          name={name}
+          type="text"
+          value={textValue}
+          placeholder={placeholder}
+          className={classes}
+          onChange={this.handleOnTextChange}
+          onBlur={this.handleOnBlur}
+          autoComplete="off"
+        />
+        {
+          !isValid &&
+          <Text
+            textType={TextTypes.Caption}
+            className="crl-text-input__error-message"
+          >
+            {validationMessage}
+          </Text>
+        }
+      </div>
+    );
+  }
+}
