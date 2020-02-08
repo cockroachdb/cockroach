39ece7203139bb64abe7805c68190a299c97c51a
Peter Mattis <petermattis@gmail.com>
2020-02-08 13:33:30 -0500

    cmd/roachtest: wait-for-full-replication before starting {sysbench,ycsb}
    The `sysbench` and `ycsb` roachtests were not waiting for full
    replication before starting the workload. The `sysbench` roachtests send
    traffic so fast and furious, that up-replication cannot keep up with
    splitting. I didn't observe this in `ycsb`, but presumably it had the
    same problem. Simply waiting for full replication at startup removes
    this problem, and brings `sysbench` and `ycsb` in line with the other
    perf roachtests such as `kv*` and `tpcc*`.
    
    Release note: None

--- pkg/cmd/roachtest/ycsb.go
+++ pkg/cmd/roachtest/ycsb.go
@@ -22,6 +22,7 @@ func registerYCSB(r *testRegistry) {
 		c.Put(ctx, cockroach, "./cockroach", c.Range(1, nodes))
 		c.Put(ctx, workload, "./workload", c.Node(nodes+1))
 		c.Start(ctx, t, c.Range(1, nodes))
+		waitForFullReplication(t, c.Conn(ctx, 1))
 
 		t.Status("running workload")
 		m := newMonitor(ctx, c, c.Range(1, nodes))
