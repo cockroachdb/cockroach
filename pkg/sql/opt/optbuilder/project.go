2d012b3a753db0fcf65763848d2d4d582334a25c
Radu Berinde <radu@cockroachlabs.com>
2020-02-07 23:22:49 -0500

    opt: fix deduplication of impure expressions
    We have the unexpected behavior of generating the same random value
    when doing something like
    `SELECT gen_random_uuid(), gen_random_uuid()`. This includes cases
    where the duplicate expressions come from default column values.
    
    This change fixes this by preventing deduplication if expressions have
    side-effects. The only exceptions are ORDER BY, where the order by
    column is supposed to refer to an existing projection if one exists;
    and GROUP BY where all instances of a grouping expression are
    collapsed - this is consistent with postgres.
    
    Fixes #44865.
    
    Release note (bug fix): fixed incorrect deduplication of impure
    expressions (like gen_random_uuid) in projections and default values.

--- pkg/sql/opt/optbuilder/project.go
+++ pkg/sql/opt/optbuilder/project.go
@@ -218,7 +218,7 @@ func (b *Builder) finishBuildScalar(
 	}
 
 	// Avoid synthesizing a new column if possible.
-	if col := outScope.findExistingCol(texpr); col != nil && col != outCol {
+	if col := outScope.findExistingCol(texpr, false /* allowSideEffects */); col != nil && col != outCol {
 		outCol.id = col.id
 		outCol.scalar = scalar
 		return scalar
@@ -267,7 +267,7 @@ func (b *Builder) finishBuildScalarRef(
 	// column id before projection.
 	if isOuterColumn {
 		// Avoid synthesizing a new column if possible.
-		existing := outScope.findExistingCol(col)
+		existing := outScope.findExistingCol(col, false /* allowSideEffects */)
 		if existing == nil || existing == outCol {
 			if outCol.name == "" {
 				outCol.name = col.name
