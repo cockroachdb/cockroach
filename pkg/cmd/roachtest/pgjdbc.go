c6d1ee4ab166157d20bddfbc5efb6ad3e047ea27
Adam Pantel <adam@cockroachlabs.com>
2020-02-10 16:30:04 -0500

    Pin pgjdbc roachtest to REL42.2.9
    Release note: none

--- pkg/cmd/roachtest/pgjdbc.go
+++ pkg/cmd/roachtest/pgjdbc.go
@@ -44,6 +44,8 @@ func registerPgjdbc(r *testRegistry) {
 		}
 
 		t.Status("cloning pgjdbc and installing prerequisites")
+		// Report the latest tag, but do not use it. The newest versions produces output that breaks our xml parser,
+		// and we want to pin to the working version for now.
 		latestTag, err := repeatGetLatestTag(
 			ctx, c, "pgjdbc", "pgjdbc", pgjdbcReleaseTagRegex,
 		)
@@ -80,7 +82,7 @@ func registerPgjdbc(r *testRegistry) {
 			c,
 			"https://github.com/pgjdbc/pgjdbc.git",
 			"/mnt/data1/pgjdbc",
-			latestTag,
+			"REL42.2.9",
 			node,
 		); err != nil {
 			t.Fatal(err)
