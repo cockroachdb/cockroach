3fee6cb259b3c4733afe961d3557384c788358c3
Vlad Los <carrott9@gmail.com>
2020-02-12 13:41:19 +0200

    ui: datepicker updates
    updated arrow position and controls design
    
    Resolves: #44422
    
    Release note(ui): none

---
+++ pkg/ui/assets/caret-left.tsx
@@ -0,0 +1,25 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import React from "react";
+
+// tslint:disable-next-line: variable-name
+const CuretLeft = () => (
+  <svg width={11} height={16} viewBox="0 0 11 16" fill="none">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9.704 2.968A1.286 1.286 0 008.01 1.032l-6.857 6a1.286 1.286 0 000 1.936l6.857 6a1.286 1.286 0 001.694-1.936L3.952 8l5.752-5.032z"
+      fill="#394455"
+    />
+  </svg>
+);
+
+export default CuretLeft;
---
+++ pkg/ui/assets/caret-right.tsx
@@ -0,0 +1,25 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import React from "react";
+
+// tslint:disable-next-line: variable-name
+const CuretRight = () => (
+  <svg width={11} height={16} viewBox="0 0 11 16" fill="none">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M1.153 13.032a1.286 1.286 0 101.694 1.936l6.857-6a1.286 1.286 0 000-1.936l-6.857-6a1.286 1.286 0 00-1.694 1.936L6.905 8l-5.752 5.032z"
+      fill="#394455"
+    />
+  </svg>
+);
+
+export default CuretRight;
