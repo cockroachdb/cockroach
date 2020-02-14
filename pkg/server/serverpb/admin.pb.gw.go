5a966afdb136ecaa525e7af1b71b80c397085919
Raphael 'kena' Poss <knz@thaumogen.net>
2020-02-14 17:42:48 +0100

    server: fix broken /health
    This patch fixes `/health` by ensuring it does not require
    authentication, does not perform KV operations and does not expose
    sensitive details.
    
    Reminder, for posterity:
    
    - `/_status/details` exposes health, readiness and node details,
      which is potentially privileged information. This requires an
      authenticated admin user. It also performs a KV operation for
      authentication and thus subject to cluster availability.
    
    - `/_admin/v1/health` only exposes health and readiness. It
      is non-privileged and does not require authentication. It does
      not perform KV operations and is thus not subject to cluster
      availability.
    
    - `/health` is now an alias for `/_admin/v1/health`.  (It used to be a
      non-authenticated alias for `/_status/details/local` which was both a
      UX and security bug. See release notes below for details.)
    
    - both `/health` and `/_admin/v1/health` accept a boolean flag e.g.
      via `?ready=1`. When `ready` is specified, a HTTP error is returned if
      the node is up but not able to accept client connections, or not live,
      or shutting down. When `ready` is *not* specified, a HTTP success is
      always returned if the node is up, even when it cannot accept client
      connections or is not live.
    
    Release justification: Category 3: Fixes for high-priority or high-severity bugs in existing functionality
    
    Release note (security update): The non-authenticated `/health` HTTP
    endpoint was previously exposing the private IP address of the node,
    which can be privileged information in some deployments. This has been
    corrected. Deployments using automation to retrieve a node build
    details, address details etc should use `/_status/details/local`
    instead and use a valid admin authentication cookie.
    
    Release note (bug fix): Accesses to `/health` using a non-root
    authentication token do not hang any more when a node is currently
    under load or if a system range is unavailable.
    
    Release justification: Bug fix to existing functionality

--- pkg/server/serverpb/admin.pb.gw.go
+++ pkg/server/serverpb/admin.pb.gw.go
@@ -232,9 +232,34 @@ func request_Admin_Settings_0(ctx context.Context, marshaler runtime.Marshaler, 
 
 }
 
+var (
+	filter_Admin_Health_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_Admin_Health_0(ctx context.Context, marshaler runtime.Marshaler, client AdminClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq HealthRequest
 	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_Admin_Health_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.Health(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+var (
+	filter_Admin_Health_1 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_Admin_Health_1(ctx context.Context, marshaler runtime.Marshaler, client AdminClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq HealthRequest
+	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_Admin_Health_1); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := client.Health(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -771,6 +796,35 @@ func RegisterAdminHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
+	mux.Handle("GET", pattern_Admin_Health_1, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Admin_Health_1(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Admin_Health_1(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_Admin_Liveness_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -1089,6 +1143,8 @@ var (
 
 	pattern_Admin_Health_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"_admin", "v1", "health"}, ""))
 
+	pattern_Admin_Health_1 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"health"}, ""))
+
 	pattern_Admin_Liveness_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"_admin", "v1", "liveness"}, ""))
 
 	pattern_Admin_Jobs_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"_admin", "v1", "jobs"}, ""))
@@ -1134,6 +1190,8 @@ var (
 	forward_Admin_Settings_0 = runtime.ForwardResponseMessage
 
 	forward_Admin_Health_0 = runtime.ForwardResponseMessage
+
+	forward_Admin_Health_1 = runtime.ForwardResponseMessage
 
 	forward_Admin_Liveness_0 = runtime.ForwardResponseMessage
 
