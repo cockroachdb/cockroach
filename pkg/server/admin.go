cockroachdb/cockroach

commit 04c947de72b38cf7f4e678232db5efcfee049ce9

server: authorize more requests
Previously, admin/status RPCs available over HTTP but that did not
perform SQL operations would not verify authorization from
clients (the only authorization was that performed by SQL privilege
checks).

This patch fixes it.

The following services are now blocked from non-admin users:

- `TableStats`. This is a temporary limitation, until this RPC learns
  how to verify privileges on the specific table for which stats are
  requested.
- `NonTableStats`
- `DataDistribution`
- `EnqueueRange`
- `RangeLog`
- `Gossip`
- `EngineStats`
- `Allocator`
- `AllocatorRange`
- `Certificates`
- `Details`
- `GetFiles`
- `LogFilesList`
- `LogFile`
- `Logs`
- `Stacks`
- `Profile`
- `RaftDebug`
- `Ranges`
- `HotRanges`
- `Range`
- `SpanStats`
- `Stores`

For reference, the following services were already properly authorized
by SQL privilege checks:

- `Databases`
- `DatabaseDetails`
- `TableDetails`
- `Users`
- `Events`
- `Jobs`
- `Locations`
- `QueryPlan`

The following were already authorized using a custom check:

- `ListSessions` (user must be either admin or can only list their own
  sessions)
- `ListLocalSessions` (ditto)

The following are now authorized using a custom check:

- `CancelSession` (user must be either admin or operate on their own sessions)
- `CancelQuery` (user must be either admin or operate on their own queries)

The following RPCs are not authorized but do not leak sensitive
information:

- `Health`
- `Liveness`
- `Settings`

Release note (admin ui change): Access to table statistics are
temporary blocked from access by non-admin users until further notice,
for security reasons.

Release note (admin ui change): Certain debug pages have been blocked
from non-admin users for security reasons.

pkg/server/admin.go
@@ -584,10 +584,13 @@ func generateTableSpan(tableID sqlbase.ID) roachpb.Span {
 func (s *adminServer) TableStats(
 	ctx context.Context, req *serverpb.TableStatsRequest,
 ) (*serverpb.TableStatsResponse, error) {
-	userName, err := userFromContext(ctx)
+	// TODO(someone): perform authorization based on the requesting user's
+	// SELECT privilege over the requested table.
+	userName, err := s.requireAdminUser(ctx)
 	if err != nil {
 		return nil, err
 	}
+
 	// Get table span.
 	path, err := s.queryDescriptorIDPath(
 		ctx, userName, []string{req.Database, req.Table},
@@ -606,6 +609,10 @@ func (s *adminServer) TableStats(
 func (s *adminServer) NonTableStats(
 	ctx context.Context, req *serverpb.NonTableStatsRequest,
 ) (*serverpb.NonTableStatsResponse, error) {
+	if _, err := s.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	timeSeriesStats, err := s.statsForSpan(ctx, roachpb.Span{
 		Key:    keys.TimeseriesPrefix,
 		EndKey: keys.TimeseriesPrefix.PrefixEnd(),
@@ -815,11 +822,7 @@ func (s *adminServer) Events(
 ) (*serverpb.EventsResponse, error) {
 	ctx = s.server.AnnotateCtx(ctx)
 
-	userName, err := userFromContext(ctx)
-	if err != nil {
-		return nil, err
-	}
-	isAdmin, err := s.hasAdminRole(ctx, userName)
+	userName, isAdmin, err := s.getUserAndRole(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -914,7 +917,8 @@ func (s *adminServer) RangeLog(
 ) (*serverpb.RangeLogResponse, error) {
 	ctx = s.server.AnnotateCtx(ctx)
 
-	userName, err := userFromContext(ctx)
+	// Range keys, even when pretty-printed, contain PII.
+	userName, err := s.requireAdminUser(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -1557,6 +1561,10 @@ func (s *adminServer) Decommission(
 func (s *adminServer) DataDistribution(
 	ctx context.Context, req *serverpb.DataDistributionRequest,
 ) (*serverpb.DataDistributionResponse, error) {
+	if _, err := s.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	resp := &serverpb.DataDistributionResponse{
 		DatabaseInfo: make(map[string]serverpb.DataDistributionResponse_DatabaseInfo),
 		ZoneConfigs:  make(map[string]serverpb.DataDistributionResponse_ZoneConfig),
@@ -1720,6 +1728,10 @@ func (s *adminServer) DataDistribution(
 func (s *adminServer) EnqueueRange(
 	ctx context.Context, req *serverpb.EnqueueRangeRequest,
 ) (*serverpb.EnqueueRangeResponse, error) {
+	if _, err := s.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	if !debug.GatewayRemoteAllowed(ctx, s.server.ClusterSettings()) {
 		return nil, remoteDebuggingErr
 	}
@@ -2194,6 +2206,28 @@ func (s *adminServer) dialNode(
 	return serverpb.NewAdminClient(conn), nil
 }
 
+func (s *adminServer) requireAdminUser(ctx context.Context) (userName string, err error) {
+	userName, isAdmin, err := s.getUserAndRole(ctx)
+	if err != nil {
+		return "", err
+	}
+	if !isAdmin {
+		return "", errInsufficientPrivilege
+	}
+	return userName, nil
+}
+
+func (s *adminServer) getUserAndRole(
+	ctx context.Context,
+) (userName string, isAdmin bool, err error) {
+	userName, err = userFromContext(ctx)
+	if err != nil {
+		return "", false, err
+	}
+	isAdmin, err = s.hasAdminRole(ctx, userName)
+	return userName, isAdmin, err
+}
+
 func (s *adminServer) hasAdminRole(ctx context.Context, sessionUser string) (bool, error) {
 	if sessionUser == security.RootUser {
 		// Shortcut.
@@ -2214,3 +2248,5 @@ func (s *adminServer) hasAdminRole(ctx context.Context, sessionUser string) (boo
 	}
 	return bool(dbDatum), nil
 }
+
+var errInsufficientPrivilege = errors.New("this operation requires admin privilege")

pkg/server/status.go
@@ -225,6 +225,9 @@ func (s *statusServer) dialNode(
 func (s *statusServer) Gossip(
 	ctx context.Context, req *serverpb.GossipRequest,
 ) (*gossip.InfoStatus, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
 	if !debug.GatewayRemoteAllowed(ctx, s.st) {
 		return nil, remoteDebuggingErr
 	}
@@ -250,6 +253,10 @@ func (s *statusServer) Gossip(
 func (s *statusServer) EngineStats(
 	ctx context.Context, req *serverpb.EngineStatsRequest,
 ) (*serverpb.EngineStatsResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	ctx = propagateGatewayMetadata(ctx)
 	ctx = s.AnnotateCtx(ctx)
 	nodeID, local, err := s.parseNodeID(req.NodeId)
@@ -291,6 +298,10 @@ func (s *statusServer) EngineStats(
 func (s *statusServer) Allocator(
 	ctx context.Context, req *serverpb.AllocatorRequest,
 ) (*serverpb.AllocatorResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	// TODO(a-robinson): It'd be nice to allow this endpoint and just avoid
 	// logging range start/end keys in the simulated allocator runs.
 	if !debug.GatewayRemoteAllowed(ctx, s.st) {
@@ -400,6 +411,10 @@ func recordedSpansToTraceEvents(spans []tracing.RecordedSpan) []*serverpb.TraceE
 func (s *statusServer) AllocatorRange(
 	ctx context.Context, req *serverpb.AllocatorRangeRequest,
 ) (*serverpb.AllocatorRangeResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	// TODO(a-robinson): It'd be nice to allow this endpoint and just avoid
 	// logging range start/end keys in the simulated allocator runs.
 	if !debug.GatewayRemoteAllowed(ctx, s.st) {
@@ -489,6 +504,10 @@ func (s *statusServer) AllocatorRange(
 func (s *statusServer) Certificates(
 	ctx context.Context, req *serverpb.CertificatesRequest,
 ) (*serverpb.CertificatesResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	ctx = propagateGatewayMetadata(ctx)
 	ctx = s.AnnotateCtx(ctx)
 	nodeID, local, err := s.parseNodeID(req.NodeId)
@@ -604,6 +623,10 @@ func extractCertFields(contents []byte, details *serverpb.CertificateDetails) er
 func (s *statusServer) Details(
 	ctx context.Context, req *serverpb.DetailsRequest,
 ) (*serverpb.DetailsResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	ctx = propagateGatewayMetadata(ctx)
 	ctx = s.AnnotateCtx(ctx)
 	nodeID, local, err := s.parseNodeID(req.NodeId)
@@ -658,6 +681,10 @@ func (s *statusServer) Details(
 func (s *statusServer) GetFiles(
 	ctx context.Context, req *serverpb.GetFilesRequest,
 ) (*serverpb.GetFilesResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	if !debug.GatewayRemoteAllowed(ctx, s.st) {
 		return nil, remoteDebuggingErr
 	}
@@ -726,6 +753,10 @@ func checkFilePattern(pattern string) error {
 func (s *statusServer) LogFilesList(
 	ctx context.Context, req *serverpb.LogFilesListRequest,
 ) (*serverpb.LogFilesListResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	ctx = propagateGatewayMetadata(ctx)
 	ctx = s.AnnotateCtx(ctx)
 	nodeID, local, err := s.parseNodeID(req.NodeId)
@@ -751,6 +782,10 @@ func (s *statusServer) LogFilesList(
 func (s *statusServer) LogFile(
 	ctx context.Context, req *serverpb.LogFileRequest,
 ) (*serverpb.LogEntriesResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	if !debug.GatewayRemoteAllowed(ctx, s.st) {
 		return nil, remoteDebuggingErr
 	}
@@ -826,6 +861,9 @@ func parseInt64WithDefault(s string, defaultValue int64) (int64, error) {
 func (s *statusServer) Logs(
 	ctx context.Context, req *serverpb.LogsRequest,
 ) (*serverpb.LogEntriesResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
 	if !debug.GatewayRemoteAllowed(ctx, s.st) {
 		return nil, remoteDebuggingErr
 	}
@@ -892,6 +930,10 @@ func (s *statusServer) Logs(
 func (s *statusServer) Stacks(
 	ctx context.Context, req *serverpb.StacksRequest,
 ) (*serverpb.JSONResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	ctx = propagateGatewayMetadata(ctx)
 	ctx = s.AnnotateCtx(ctx)
 	nodeID, local, err := s.parseNodeID(req.NodeId)
@@ -928,6 +970,10 @@ func (s *statusServer) Stacks(
 func (s *statusServer) Profile(
 	ctx context.Context, req *serverpb.ProfileRequest,
 ) (*serverpb.JSONResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	nodeID, local, err := s.parseNodeID(req.NodeId)
 	if err != nil {
 		return nil, grpcstatus.Errorf(codes.InvalidArgument, err.Error())
@@ -1074,6 +1120,10 @@ func (s *statusServer) Metrics(
 func (s *statusServer) RaftDebug(
 	ctx context.Context, req *serverpb.RaftDebugRequest,
 ) (*serverpb.RaftDebugResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	ctx = propagateGatewayMetadata(ctx)
 	ctx = s.AnnotateCtx(ctx)
 	nodes, err := s.Nodes(ctx, nil)
@@ -1174,6 +1224,10 @@ func (s *statusServer) handleVars(w http.ResponseWriter, r *http.Request) {
 func (s *statusServer) Ranges(
 	ctx context.Context, req *serverpb.RangesRequest,
 ) (*serverpb.RangesResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	ctx = propagateGatewayMetadata(ctx)
 	ctx = s.AnnotateCtx(ctx)
 	nodeID, local, err := s.parseNodeID(req.NodeId)
@@ -1332,6 +1386,10 @@ func (s *statusServer) Ranges(
 func (s *statusServer) HotRanges(
 	ctx context.Context, req *serverpb.HotRangesRequest,
 ) (*serverpb.HotRangesResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	ctx = propagateGatewayMetadata(ctx)
 	ctx = s.AnnotateCtx(ctx)
 
@@ -1418,6 +1476,10 @@ func (s *statusServer) localHotRanges(ctx context.Context) serverpb.HotRangesRes
 func (s *statusServer) Range(
 	ctx context.Context, req *serverpb.RangeRequest,
 ) (*serverpb.RangeResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	ctx = propagateGatewayMetadata(ctx)
 	ctx = s.AnnotateCtx(ctx)
 
@@ -1475,12 +1537,7 @@ func (s *statusServer) ListLocalSessions(
 		return nil, remoteDebuggingErr
 	}
 
-	sessionUser, err := userFromContext(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	isAdmin, err := s.admin.hasAdminRole(ctx, sessionUser)
+	sessionUser, isAdmin, err := s.admin.getUserAndRole(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -1602,11 +1659,11 @@ func (s *statusServer) iterateNodes(
 func (s *statusServer) ListSessions(
 	ctx context.Context, req *serverpb.ListSessionsRequest,
 ) (*serverpb.ListSessionsResponse, error) {
-	ctx = propagateGatewayMetadata(ctx)
 	if !debug.GatewayRemoteAllowed(ctx, s.st) {
 		return nil, remoteDebuggingErr
 	}
 
+	ctx = propagateGatewayMetadata(ctx)
 	ctx = s.AnnotateCtx(ctx)
 
 	response := &serverpb.ListSessionsResponse{
@@ -1643,6 +1700,16 @@ func (s *statusServer) ListSessions(
 func (s *statusServer) CancelSession(
 	ctx context.Context, req *serverpb.CancelSessionRequest,
 ) (*serverpb.CancelSessionResponse, error) {
+	sessionUser, isAdmin, err := s.admin.getUserAndRole(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if !isAdmin && sessionUser != req.Username {
+		// A user can only cancel their own sessions.
+		return nil, errInsufficientPrivilege
+	}
+
 	ctx = s.AnnotateCtx(ctx)
 	nodeID, local, err := s.parseNodeID(req.NodeId)
 
@@ -1674,6 +1741,16 @@ func (s *statusServer) CancelSession(
 func (s *statusServer) CancelQuery(
 	ctx context.Context, req *serverpb.CancelQueryRequest,
 ) (*serverpb.CancelQueryResponse, error) {
+	sessionUser, isAdmin, err := s.admin.getUserAndRole(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if !isAdmin && sessionUser != req.Username {
+		// A user can only cancel their own queries.
+		return nil, errInsufficientPrivilege
+	}
+
 	ctx = propagateGatewayMetadata(ctx)
 	ctx = s.AnnotateCtx(ctx)
 	nodeID, local, err := s.parseNodeID(req.NodeId)
@@ -1706,6 +1783,10 @@ func (s *statusServer) CancelQuery(
 func (s *statusServer) SpanStats(
 	ctx context.Context, req *serverpb.SpanStatsRequest,
 ) (*serverpb.SpanStatsResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	ctx = propagateGatewayMetadata(ctx)
 	ctx = s.AnnotateCtx(ctx)
 	nodeID, local, err := s.parseNodeID(req.NodeID)
@@ -1765,6 +1846,10 @@ func (s *statusServer) Diagnostics(
 func (s *statusServer) Stores(
 	ctx context.Context, req *serverpb.StoresRequest,
 ) (*serverpb.StoresResponse, error) {
+	if _, err := s.admin.requireAdminUser(ctx); err != nil {
+		return nil, err
+	}
+
 	ctx = propagateGatewayMetadata(ctx)
 	ctx = s.AnnotateCtx(ctx)
 	nodeID, local, err := s.parseNodeID(req.NodeId)

commit 33d3a6f5fea4b94a914b3fd049d1df402664eee1

server: make UI customizations work for non-admin users
Summary table:

| Time frame  | R/W of UI customization | different customizations per user |
|-------------|-------------------------|-----------------------------------|
| Pre-#42563  | all users               | broken (shared between users)     |
| Post-#42563 | admin only (regression) | broken                            |
| This commit | all users               | yes - each user has its own       |

The separation of customization between users is done by mixing the
username with the key with a `$` character when looking up rows in
`system.ui`.

This approach can look perhaps baroque when SQL would prefer us to
have two separate *columns* in the PK of `system.ui` and use that for
lookups and updates. Unfortunately PK changes are not yet supported,
and a PK change would incur a complex story in mixed-version clusters.

Additionally, this approach is suitable for backporting in 2.1, 19.1
and 19.2 clusters where the functionality was broken by #42563.

Release note (admin ui change): The display options are now saved
separately for each authenticated user. Note: when upgrading to a
version with this change, all current display customizations for admin
users are lost.

Release note (bug fix): Customizations of the Admin UI are again
properly saved across sessions.

pkg/server/admin.go
@@ -1066,7 +1066,7 @@ func (s *adminServer) getUIData(
 		if i != 0 {
 			query.Append(",")
 		}
-		query.Append("$", tree.NewDString(key))
+		query.Append("$", tree.NewDString(makeUIKey(userName, key)))
 	}
 	query.Append(");")
 	if err := query.Errors(); err != nil {
@@ -1074,7 +1074,7 @@ func (s *adminServer) getUIData(
 	}
 	rows, err := s.server.internalExecutor.QueryEx(
 		ctx, "admin-getUIData", nil, /* txn */
-		sqlbase.InternalExecutorSessionDataOverride{User: userName},
+		sqlbase.InternalExecutorSessionDataOverride{User: security.RootUser},
 		query.String(), query.QueryArguments()...,
 	)
 	if err != nil {
@@ -1088,6 +1088,9 @@ func (s *adminServer) getUIData(
 		if !ok {
 			return nil, s.serverErrorf("unexpected type for UI key: %T", row[0])
 		}
+		_, key := splitUIKey(string(dKey))
+		dKey = tree.DString(key)
+
 		dValue, ok := row[1].(*tree.DBytes)
 		if !ok {
 			return nil, s.serverErrorf("unexpected type for UI value: %T", row[1])
@@ -1103,6 +1106,21 @@ func (s *adminServer) getUIData(
 		}
 	}
 	return &resp, nil
+}
+
+// makeUIKey combines username and key to form a lookup key in
+// system.ui.
+// The username is combined to ensure that different users
+// can use different customizations.
+func makeUIKey(username, key string) string {
+	return username + "$" + key
+}
+
+// splitUIKey is the inverse of makeUIKey.
+// The caller must ensure that the value was produced by makeUIKey.
+func splitUIKey(combined string) (string, string) {
+	pair := strings.SplitN(combined, "$", 2)
+	return pair[0], pair[1]
 }
 
 // SetUIData is an endpoint that stores the given key/value pairs in the
@@ -1128,9 +1146,9 @@ func (s *adminServer) SetUIData(
 		rowsAffected, err := s.server.internalExecutor.ExecEx(
 			ctx, "admin-set-ui-data", nil, /* txn */
 			sqlbase.InternalExecutorSessionDataOverride{
-				User: userName,
+				User: security.RootUser,
 			},
-			query, key, val)
+			query, makeUIKey(userName, key), val)
 		if err != nil {
 			return nil, s.serverError(err)
 		}

pkg/server/admin_test.go
@@ -71,7 +71,16 @@ func getAdminJSONProtoWithAdminOption(
 func postAdminJSONProto(
 	ts serverutils.TestServerInterface, path string, request, response protoutil.Message,
 ) error {
-	return serverutils.PostJSONProto(ts, adminPrefix+path, request, response)
+	return postAdminJSONProtoWithAdminOption(ts, path, request, response, true)
+}
+
+func postAdminJSONProtoWithAdminOption(
+	ts serverutils.TestServerInterface,
+	path string,
+	request, response protoutil.Message,
+	isAdmin bool,
+) error {
+	return serverutils.PostJSONProtoWithAdminOption(ts, adminPrefix+path, request, response, isAdmin)
 }
 
 // getText fetches the HTTP response body as text in the form of a
@@ -1027,107 +1036,150 @@ func TestAdminAPISettings(t *testing.T) {
 	})
 }
 
+// TestAdminAPIUIData checks that UI customizations are properly
+// persisted for both admin and non-admin users.
 func TestAdminAPIUIData(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	s, _, _ := serverutils.StartServer(t, base.TestServerArgs{})
 	defer s.Stopper().Stop(context.TODO())
 
-	start := timeutil.Now()
+	testutils.RunTrueAndFalse(t, "isAdmin", func(t *testing.T, isAdmin bool) {
+		start := timeutil.Now()
 
-	mustSetUIData := func(keyValues map[string][]byte) {
-		if err := postAdminJSONProto(s, "uidata", &serverpb.SetUIDataRequest{
-			KeyValues: keyValues,
-		}, &serverpb.SetUIDataResponse{}); err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	expectKeyValues := func(expKeyValues map[string][]byte) {
-		var resp serverpb.GetUIDataResponse
-		queryValues := make(url.Values)
-		for key := range expKeyValues {
-			queryValues.Add("keys", key)
-		}
-		url := "uidata?" + queryValues.Encode()
-		if err := getAdminJSONProto(s, url, &resp); err != nil {
-			t.Fatal(err)
-		}
-		// Do a two-way comparison. We can't use reflect.DeepEqual(), because
-		// resp.KeyValues has timestamps and expKeyValues doesn't.
-		for key, actualVal := range resp.KeyValues {
-			if a, e := actualVal.Value, expKeyValues[key]; !bytes.Equal(a, e) {
-				t.Fatalf("key %s: value = %v, expected = %v", key, a, e)
-			}
-		}
-		for key, expVal := range expKeyValues {
-			if a, e := resp.KeyValues[key].Value, expVal; !bytes.Equal(a, e) {
-				t.Fatalf("key %s: value = %v, expected = %v", key, a, e)
+		mustSetUIData := func(keyValues map[string][]byte) {
+			if err := postAdminJSONProtoWithAdminOption(s, "uidata", &serverpb.SetUIDataRequest{
+				KeyValues: keyValues,
+			}, &serverpb.SetUIDataResponse{}, isAdmin); err != nil {
+				t.Fatal(err)
 			}
 		}
 
-		// Sanity check LastUpdated.
-		for _, val := range resp.KeyValues {
-			now := timeutil.Now()
-			if val.LastUpdated.Before(start) {
-				t.Fatalf("val.LastUpdated %s < start %s", val.LastUpdated, start)
+		expectKeyValues := func(expKeyValues map[string][]byte) {
+			var resp serverpb.GetUIDataResponse
+			queryValues := make(url.Values)
+			for key := range expKeyValues {
+				queryValues.Add("keys", key)
 			}
-			if val.LastUpdated.After(now) {
-				t.Fatalf("val.LastUpdated %s > now %s", val.LastUpdated, now)
+			url := "uidata?" + queryValues.Encode()
+			if err := getAdminJSONProtoWithAdminOption(s, url, &resp, isAdmin); err != nil {
+				t.Fatal(err)
+			}
+			// Do a two-way comparison. We can't use reflect.DeepEqual(), because
+			// resp.KeyValues has timestamps and expKeyValues doesn't.
+			for key, actualVal := range resp.KeyValues {
+				if a, e := actualVal.Value, expKeyValues[key]; !bytes.Equal(a, e) {
+					t.Fatalf("key %s: value = %v, expected = %v", key, a, e)
+				}
+			}
+			for key, expVal := range expKeyValues {
+				if a, e := resp.KeyValues[key].Value, expVal; !bytes.Equal(a, e) {
+					t.Fatalf("key %s: value = %v, expected = %v", key, a, e)
+				}
+			}
+
+			// Sanity check LastUpdated.
+			for _, val := range resp.KeyValues {
+				now := timeutil.Now()
+				if val.LastUpdated.Before(start) {
+					t.Fatalf("val.LastUpdated %s < start %s", val.LastUpdated, start)
+				}
+				if val.LastUpdated.After(now) {
+					t.Fatalf("val.LastUpdated %s > now %s", val.LastUpdated, now)
+				}
 			}
 		}
-	}
 
-	expectValueEquals := func(key string, expVal []byte) {
-		expectKeyValues(map[string][]byte{key: expVal})
-	}
-
-	expectKeyNotFound := func(key string) {
-		var resp serverpb.GetUIDataResponse
-		url := "uidata?keys=" + key
-		if err := getAdminJSONProto(s, url, &resp); err != nil {
-			t.Fatal(err)
+		expectValueEquals := func(key string, expVal []byte) {
+			expectKeyValues(map[string][]byte{key: expVal})
 		}
-		if len(resp.KeyValues) != 0 {
-			t.Fatal("key unexpectedly found")
+
+		expectKeyNotFound := func(key string) {
+			var resp serverpb.GetUIDataResponse
+			url := "uidata?keys=" + key
+			if err := getAdminJSONProtoWithAdminOption(s, url, &resp, isAdmin); err != nil {
+				t.Fatal(err)
+			}
+			if len(resp.KeyValues) != 0 {
+				t.Fatal("key unexpectedly found")
+			}
 		}
-	}
 
-	// Basic tests.
-	var badResp serverpb.GetUIDataResponse
-	const errPattern = "400 Bad Request"
-	if err := getAdminJSONProto(s, "uidata", &badResp); !testutils.IsError(err, errPattern) {
-		t.Fatalf("unexpected error: %v\nexpected: %s", err, errPattern)
-	}
+		// Basic tests.
+		var badResp serverpb.GetUIDataResponse
+		const errPattern = "400 Bad Request"
+		if err := getAdminJSONProtoWithAdminOption(s, "uidata", &badResp, isAdmin); !testutils.IsError(err, errPattern) {
+			t.Fatalf("unexpected error: %v\nexpected: %s", err, errPattern)
+		}
 
-	mustSetUIData(map[string][]byte{"k1": []byte("v1")})
-	expectValueEquals("k1", []byte("v1"))
+		mustSetUIData(map[string][]byte{"k1": []byte("v1")})
+		expectValueEquals("k1", []byte("v1"))
 
-	expectKeyNotFound("NON_EXISTENT_KEY")
+		expectKeyNotFound("NON_EXISTENT_KEY")
 
-	mustSetUIData(map[string][]byte{
-		"k2": []byte("v2"),
-		"k3": []byte("v3"),
+		mustSetUIData(map[string][]byte{
+			"k2": []byte("v2"),
+			"k3": []byte("v3"),
+		})
+		expectValueEquals("k2", []byte("v2"))
+		expectValueEquals("k3", []byte("v3"))
+		expectKeyValues(map[string][]byte{
+			"k2": []byte("v2"),
+			"k3": []byte("v3"),
+		})
+
+		mustSetUIData(map[string][]byte{"k2": []byte("v2-updated")})
+		expectKeyValues(map[string][]byte{
+			"k2": []byte("v2-updated"),
+			"k3": []byte("v3"),
+		})
+
+		// Write a binary blob with all possible byte values, then verify it.
+		var buf bytes.Buffer
+		for i := 0; i < 997; i++ {
+			buf.WriteByte(byte(i % 256))
+		}
+		mustSetUIData(map[string][]byte{"bin": buf.Bytes()})
+		expectValueEquals("bin", buf.Bytes())
 	})
-	expectValueEquals("k2", []byte("v2"))
-	expectValueEquals("k3", []byte("v3"))
-	expectKeyValues(map[string][]byte{
-		"k2": []byte("v2"),
-		"k3": []byte("v3"),
-	})
+}
 
-	mustSetUIData(map[string][]byte{"k2": []byte("v2-updated")})
-	expectKeyValues(map[string][]byte{
-		"k2": []byte("v2-updated"),
-		"k3": []byte("v3"),
-	})
+// TestAdminAPIUISeparateData check that separate users have separate customizations.
+func TestAdminAPIUISeparateData(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	s, _, _ := serverutils.StartServer(t, base.TestServerArgs{})
+	defer s.Stopper().Stop(context.TODO())
 
-	// Write a binary blob with all possible byte values, then verify it.
-	var buf bytes.Buffer
-	for i := 0; i < 997; i++ {
-		buf.WriteByte(byte(i % 256))
+	// Make a setting for an admin user.
+	if err := postAdminJSONProtoWithAdminOption(s, "uidata",
+		&serverpb.SetUIDataRequest{KeyValues: map[string][]byte{"k": []byte("v1")}},
+		&serverpb.SetUIDataResponse{},
+		true /*isAdmin*/); err != nil {
+		t.Fatal(err)
 	}
-	mustSetUIData(map[string][]byte{"bin": buf.Bytes()})
-	expectValueEquals("bin", buf.Bytes())
+
+	// Make a setting for a non-admin user.
+	if err := postAdminJSONProtoWithAdminOption(s, "uidata",
+		&serverpb.SetUIDataRequest{KeyValues: map[string][]byte{"k": []byte("v2")}},
+		&serverpb.SetUIDataResponse{},
+		false /*isAdmin*/); err != nil {
+		t.Fatal(err)
+	}
+
+	var resp serverpb.GetUIDataResponse
+	url := "uidata?keys=k"
+
+	if err := getAdminJSONProtoWithAdminOption(s, url, &resp, true /* isAdmin */); err != nil {
+		t.Fatal(err)
+	}
+	if len(resp.KeyValues) != 1 || !bytes.Equal(resp.KeyValues["k"].Value, []byte("v1")) {
+		t.Fatalf("unexpected admin values: %+v", resp.KeyValues)
+	}
+	if err := getAdminJSONProtoWithAdminOption(s, url, &resp, false /* isAdmin */); err != nil {
+		t.Fatal(err)
+	}
+	if len(resp.KeyValues) != 1 || !bytes.Equal(resp.KeyValues["k"].Value, []byte("v2")) {
+		t.Fatalf("unexpected non-admin values: %+v", resp.KeyValues)
+	}
 }
 
 func TestClusterAPI(t *testing.T) {

pkg/testutils/serverutils/test_server_shim.go
@@ -246,10 +246,13 @@ func GetJSONProtoWithAdminOption(
 	return httputil.GetJSON(httpClient, ts.AdminURL()+path, response)
 }
 
-// PostJSONProto uses the supplied client to POST request to the URL specified by
-// the parameters and unmarshals the result into response.
-func PostJSONProto(ts TestServerInterface, path string, request, response protoutil.Message) error {
-	httpClient, err := ts.GetAdminAuthenticatedHTTPClient()
+// PostJSONProtoWithAdminOption is like PostJSONProto but the caller
+// can customize whether the request is performed with admin
+// privilege.
+func PostJSONProtoWithAdminOption(
+	ts TestServerInterface, path string, request, response protoutil.Message, isAdmin bool,
+) error {
+	httpClient, err := ts.GetAuthenticatedHTTPClient(isAdmin)
 	if err != nil {
 		return err
 	}