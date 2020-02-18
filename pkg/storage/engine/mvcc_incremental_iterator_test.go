ca2cb24d93ad047d5b1575bfc084d55434fb8c74
Paul Bardea <pbardea@gmail.com>
2020-02-17 23:35:17 -0500

    storage: re-add MVCC incremental iterator tests
    These tests exercise the Go MVCCIncrementalIterator which is no longer
    used directly in CockroachDB. However, the MVCCIncrementalIterator is
    used as an oracle to test the C++ implementation. Therefore, it would
    still be beneficial to have these tests present as long as we plan to
    keep these 2 implementations in sync.
    
    Release note: None

--- pkg/storage/engine/mvcc_incremental_iterator_test.go
+++ pkg/storage/engine/mvcc_incremental_iterator_test.go
@@ -13,9 +13,12 @@ package engine
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"math"
+	"path/filepath"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/storage/engine/enginepb"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
@@ -23,6 +26,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -406,6 +410,311 @@ func TestMVCCIncrementalIteratorIntentRewrittenConcurrently(t *testing.T) {
 			if err := g.Wait(); err != nil {
 				t.Fatal(err)
 			}
+		})
+	}
+}
+
+// TestMVCCIncrementalIteratorIntentDeletion checks a workaround in
+// MVCCIncrementalIterator for a bug in time-bound iterators, where an intent
+// has been deleted, but the time-bound iterator doesn't see the deletion.
+func TestMVCCIncrementalIteratorIntentDeletion(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	txn := func(key roachpb.Key, ts hlc.Timestamp) *roachpb.Transaction {
+		return &roachpb.Transaction{
+			TxnMeta: enginepb.TxnMeta{
+				Key:            key,
+				ID:             uuid.MakeV4(),
+				Epoch:          1,
+				WriteTimestamp: ts,
+			},
+			ReadTimestamp: ts,
+		}
+	}
+	intent := func(txn *roachpb.Transaction) roachpb.LockUpdate {
+		intent := roachpb.MakeLockUpdate(txn, roachpb.Span{Key: txn.Key})
+		intent.Status = roachpb.COMMITTED
+		return intent
+	}
+
+	ctx := context.Background()
+	kA := roachpb.Key("kA")
+	vA1 := roachpb.MakeValueFromString("vA1")
+	vA2 := roachpb.MakeValueFromString("vA2")
+	vA3 := roachpb.MakeValueFromString("vA3")
+	kB := roachpb.Key("kB")
+	vB1 := roachpb.MakeValueFromString("vB1")
+	kC := roachpb.Key("kC")
+	vC1 := roachpb.MakeValueFromString("vC1")
+	ts0 := hlc.Timestamp{WallTime: 0}
+	ts1 := hlc.Timestamp{WallTime: 1}
+	ts2 := hlc.Timestamp{WallTime: 2}
+	ts3 := hlc.Timestamp{WallTime: 3}
+	txnA1 := txn(kA, ts1)
+	txnA3 := txn(kA, ts3)
+	txnB1 := txn(kB, ts1)
+	txnC1 := txn(kC, ts1)
+
+	db := NewInMem(ctx, DefaultStorageEngine, roachpb.Attributes{}, 10<<20)
+	defer db.Close()
+
+	// Set up two sstables very specifically:
+	//
+	// sst1 (time-bound metadata ts1->ts1)
+	// kA -> (intent)
+	// kA:1 -> vA1
+	// kB -> (intent)
+	// kB:1 -> vB1
+	// kC -> (intent)
+	// kC:1 -> vC1
+	//
+	// sst2 (time-bound metadata ts2->ts3) the intent deletions are for the
+	// intents at ts1, but there's no way know that when constructing the
+	// metadata (hence the time-bound iterator bug)
+	// kA -> (intent) [NB this overwrites the intent deletion]
+	// kA:3 -> vA3
+	// kA:2 -> vA2
+	// kB -> (intent deletion)
+	require.NoError(t, MVCCPut(ctx, db, nil, kA, txnA1.ReadTimestamp, vA1, txnA1))
+	require.NoError(t, MVCCPut(ctx, db, nil, kB, txnB1.ReadTimestamp, vB1, txnB1))
+	require.NoError(t, MVCCPut(ctx, db, nil, kC, txnC1.ReadTimestamp, vC1, txnC1))
+	require.NoError(t, db.Flush())
+	require.NoError(t, db.Compact())
+	_, err := MVCCResolveWriteIntent(ctx, db, nil, intent(txnA1))
+	require.NoError(t, err)
+	_, err = MVCCResolveWriteIntent(ctx, db, nil, intent(txnB1))
+	require.NoError(t, err)
+	require.NoError(t, MVCCPut(ctx, db, nil, kA, ts2, vA2, nil))
+	require.NoError(t, MVCCPut(ctx, db, nil, kA, txnA3.WriteTimestamp, vA3, txnA3))
+	require.NoError(t, db.Flush())
+
+	if rocks, ok := db.(*RocksDB); ok {
+		// Double-check that we've created the SSTs we intended to.
+		userProps, err := rocks.GetUserProperties()
+		require.NoError(t, err)
+		require.Len(t, userProps.Sst, 2)
+		require.Equal(t, userProps.Sst[0].TsMin, &ts1)
+		require.Equal(t, userProps.Sst[0].TsMax, &ts1)
+		require.Equal(t, userProps.Sst[1].TsMin, &ts2)
+		require.Equal(t, userProps.Sst[1].TsMax, &ts3)
+	}
+
+	// The kA ts1 intent has been resolved. There's now a new intent on kA, but
+	// the timestamp (ts3) is too new so it should be ignored.
+	kvs, err := slurpKVsInTimeRange(db, kA, ts0, ts1)
+	require.NoError(t, err)
+	require.Equal(t, []MVCCKeyValue{
+		{Key: MVCCKey{Key: kA, Timestamp: ts1}, Value: vA1.RawBytes},
+	}, kvs)
+	// kA has a value at ts2. Again the intent is too new (ts3), so ignore.
+	kvs, err = slurpKVsInTimeRange(db, kA, ts0, ts2)
+	require.NoError(t, err)
+	require.Equal(t, []MVCCKeyValue{
+		{Key: MVCCKey{Key: kA, Timestamp: ts2}, Value: vA2.RawBytes},
+		{Key: MVCCKey{Key: kA, Timestamp: ts1}, Value: vA1.RawBytes},
+	}, kvs)
+	// At ts3, we should see the new intent
+	_, err = slurpKVsInTimeRange(db, kA, ts0, ts3)
+	require.EqualError(t, err, `conflicting intents on "kA"`)
+
+	// Similar to the kA ts1 check, but there is no newer intent. We expect to
+	// pick up the intent deletion and it should cancel out the intent, leaving
+	// only the value at ts1.
+	kvs, err = slurpKVsInTimeRange(db, kB, ts0, ts1)
+	require.NoError(t, err)
+	require.Equal(t, []MVCCKeyValue{
+		{Key: MVCCKey{Key: kB, Timestamp: ts1}, Value: vB1.RawBytes},
+	}, kvs)
+
+	// Sanity check that we see the still unresolved intent for kC ts1.
+	_, err = slurpKVsInTimeRange(db, kC, ts0, ts1)
+	require.EqualError(t, err, `conflicting intents on "kC"`)
+}
+
+func TestMVCCIncrementalIteratorIntentStraddlesSStables(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	// Create a DB containing 2 keys, a and b, where b has an intent. We use the
+	// regular MVCCPut operation to generate these keys, which we'll later be
+	// copying into manually created sstables.
+	ctx := context.Background()
+	db1 := NewInMem(ctx, DefaultStorageEngine, roachpb.Attributes{}, 10<<20 /* 10 MB */)
+	defer db1.Close()
+
+	put := func(key, value string, ts int64, txn *roachpb.Transaction) {
+		v := roachpb.MakeValueFromString(value)
+		if err := MVCCPut(
+			ctx, db1, nil, roachpb.Key(key), hlc.Timestamp{WallTime: ts}, v, txn,
+		); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	put("a", "a value", 1, nil)
+	put("b", "b value", 2, &roachpb.Transaction{
+		TxnMeta: enginepb.TxnMeta{
+			Key:            roachpb.Key("b"),
+			ID:             uuid.MakeV4(),
+			Epoch:          1,
+			WriteTimestamp: hlc.Timestamp{WallTime: 2},
+		},
+		ReadTimestamp: hlc.Timestamp{WallTime: 2},
+	})
+
+	// Create a second DB in which we'll create a specific SSTable structure: the
+	// first SSTable contains 2 KVs where the first is a regular versioned key
+	// and the second is the MVCC metadata entry (i.e. an intent). The next
+	// SSTable contains the provisional value for the intent. The effect is that
+	// the metadata entry is separated from the entry it is metadata for.
+	//
+	//   SSTable 1:
+	//     a@1
+	//     b@<meta>
+	//
+	//   SSTable 2:
+	//     b@2
+	db2 := NewInMem(ctx, DefaultStorageEngine, roachpb.Attributes{}, 10<<20 /* 10 MB */)
+	defer db2.Close()
+
+	ingest := func(it Iterator, count int) {
+		sst, err := MakeRocksDBSstFileWriter()
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer sst.Close()
+
+		for i := 0; i < count; i++ {
+			ok, err := it.Valid()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !ok {
+				t.Fatal("expected key")
+			}
+			if err := sst.Put(it.Key(), it.Value()); err != nil {
+				t.Fatal(err)
+			}
+			it.Next()
+		}
+		sstContents, err := sst.Finish()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := db2.WriteFile(`ingest`, sstContents); err != nil {
+			t.Fatal(err)
+		}
+		if err := db2.IngestExternalFiles(ctx, []string{`ingest`}); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	{
+		// Iterate over the entries in the first DB, ingesting them into SSTables
+		// in the second DB.
+		it := db1.NewIterator(IterOptions{
+			UpperBound: keys.MaxKey,
+		})
+		defer it.Close()
+		it.SeekGE(MVCCKey{Key: keys.MinKey})
+		ingest(it, 2)
+		ingest(it, 1)
+	}
+
+	{
+		// Use an incremental iterator to simulate an incremental backup from (1,
+		// 2]. Note that incremental iterators are exclusive on the start time and
+		// inclusive on the end time. The expectation is that we'll see a write
+		// intent error.
+		it := NewMVCCIncrementalIterator(db2, MVCCIncrementalIterOptions{
+			IterOptions: IterOptions{UpperBound: keys.MaxKey},
+			StartTime:   hlc.Timestamp{WallTime: 1},
+			EndTime:     hlc.Timestamp{WallTime: 2},
+		})
+		defer it.Close()
+		for it.SeekGE(MVCCKey{Key: keys.MinKey}); ; it.Next() {
+			ok, err := it.Valid()
+			if err != nil {
+				if _, ok = err.(*roachpb.WriteIntentError); ok {
+					// This is the write intent error we were expecting.
+					return
+				}
+				t.Fatalf("%T: %s", err, err)
+			}
+			if !ok {
+				break
+			}
+		}
+		t.Fatalf("expected write intent error, but found success")
+	}
+}
+
+func TestMVCCIterateTimeBound(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	dir, cleanupFn := testutils.TempDir(t)
+	defer cleanupFn()
+
+	const numKeys = 1000
+	const numBatches = 10
+	const batchTimeSpan = 10
+	const valueSize = 8
+
+	eng, err := loadTestData(filepath.Join(dir, "mvcc_data"),
+		numKeys, numBatches, batchTimeSpan, valueSize)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer eng.Close()
+
+	for _, testCase := range []struct {
+		start hlc.Timestamp
+		end   hlc.Timestamp
+	}{
+		// entire time range
+		{hlc.Timestamp{WallTime: 0, Logical: 0}, hlc.Timestamp{WallTime: 110, Logical: 0}},
+		// one SST
+		{hlc.Timestamp{WallTime: 10, Logical: 0}, hlc.Timestamp{WallTime: 19, Logical: 0}},
+		// one SST, plus the min of the following SST
+		{hlc.Timestamp{WallTime: 10, Logical: 0}, hlc.Timestamp{WallTime: 20, Logical: 0}},
+		// one SST, plus the max of the preceding SST
+		{hlc.Timestamp{WallTime: 9, Logical: 0}, hlc.Timestamp{WallTime: 19, Logical: 0}},
+		// one SST, plus the min of the following and the max of the preceding SST
+		{hlc.Timestamp{WallTime: 9, Logical: 0}, hlc.Timestamp{WallTime: 21, Logical: 0}},
+		// one SST, not min or max
+		{hlc.Timestamp{WallTime: 17, Logical: 0}, hlc.Timestamp{WallTime: 18, Logical: 0}},
+		// one SST's max
+		{hlc.Timestamp{WallTime: 18, Logical: 0}, hlc.Timestamp{WallTime: 19, Logical: 0}},
+		// one SST's min
+		{hlc.Timestamp{WallTime: 19, Logical: 0}, hlc.Timestamp{WallTime: 20, Logical: 0}},
+		// random endpoints
+		{hlc.Timestamp{WallTime: 32, Logical: 0}, hlc.Timestamp{WallTime: 78, Logical: 0}},
+	} {
+		t.Run(fmt.Sprintf("%s-%s", testCase.start, testCase.end), func(t *testing.T) {
+			defer leaktest.AfterTest(t)()
+
+			var expectedKVs []MVCCKeyValue
+			iter := eng.NewIterator(IterOptions{UpperBound: roachpb.KeyMax})
+			defer iter.Close()
+			iter.SeekGE(MVCCKey{})
+			for {
+				ok, err := iter.Valid()
+				if err != nil {
+					t.Fatal(err)
+				} else if !ok {
+					break
+				}
+				ts := iter.Key().Timestamp
+				if (ts.Less(testCase.end) || testCase.end == ts) && testCase.start.Less(ts) {
+					expectedKVs = append(expectedKVs, MVCCKeyValue{Key: iter.Key(), Value: iter.Value()})
+				}
+				iter.Next()
+			}
+			if len(expectedKVs) < 1 {
+				t.Fatalf("source of truth had no expected KVs; likely a bug in the test itself")
+			}
+
+			fn := (*MVCCIncrementalIterator).NextKey
+			assertEqualKVs(eng, fn, keys.MinKey, keys.MaxKey, testCase.start, testCase.end, expectedKVs)(t)
 		})
 	}
 }
