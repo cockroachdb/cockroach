f3a1872828a1fbc8abfbd8cc925cadb17f816088
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2020-02-19 18:05:47 -0500

    storage/concurrency: move TestConcurrencyManagerBasic into test pkg
    This not only ensures that we're testing the external interface of
    concurrency.Manager, but it also ensures that we don't hit cyclic
    dependency issues in later commits.

--- pkg/storage/concurrency/lock_table_test.go
+++ pkg/storage/concurrency/lock_table_test.go
@@ -14,6 +14,8 @@ import (
 	"context"
 	"fmt"
 	"runtime"
+	"strconv"
+	"strings"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -368,6 +370,72 @@ func TestLockTableBasic(t *testing.T) {
 			}
 		})
 	})
+}
+
+func nextUUID(counter *uint128.Uint128) uuid.UUID {
+	*counter = counter.Add(1)
+	return uuid.FromUint128(*counter)
+}
+
+func scanTimestamp(t *testing.T, d *datadriven.TestData) hlc.Timestamp {
+	var ts hlc.Timestamp
+	var tsS string
+	d.ScanArgs(t, "ts", &tsS)
+	parts := strings.Split(tsS, ",")
+
+	// Find the wall time part.
+	tsW, err := strconv.ParseInt(parts[0], 10, 64)
+	if err != nil {
+		d.Fatalf(t, "%v", err)
+	}
+	ts.WallTime = tsW
+
+	// Find the logical part, if there is one.
+	var tsL int64
+	if len(parts) > 1 {
+		tsL, err = strconv.ParseInt(parts[1], 10, 32)
+		if err != nil {
+			d.Fatalf(t, "%v", err)
+		}
+	}
+	ts.Logical = int32(tsL)
+	return ts
+}
+
+func getSpan(t *testing.T, d *datadriven.TestData, str string) roachpb.Span {
+	parts := strings.Split(str, ",")
+	span := roachpb.Span{Key: roachpb.Key(parts[0])}
+	if len(parts) > 2 {
+		d.Fatalf(t, "incorrect span format: %s", str)
+	} else if len(parts) == 2 {
+		span.EndKey = roachpb.Key(parts[1])
+	}
+	return span
+}
+
+func scanSpans(t *testing.T, d *datadriven.TestData, ts hlc.Timestamp) *spanset.SpanSet {
+	spans := &spanset.SpanSet{}
+	var spansStr string
+	d.ScanArgs(t, "spans", &spansStr)
+	parts := strings.Split(spansStr, "+")
+	for _, p := range parts {
+		if len(p) < 2 || p[1] != '@' {
+			d.Fatalf(t, "incorrect span with access format: %s", p)
+		}
+		c := p[0]
+		p = p[2:]
+		var sa spanset.SpanAccess
+		switch c {
+		case 'r':
+			sa = spanset.SpanReadOnly
+		case 'w':
+			sa = spanset.SpanReadWrite
+		default:
+			d.Fatalf(t, "incorrect span access: %c", c)
+		}
+		spans.AddMVCC(sa, getSpan(t, d, p), ts)
+	}
+	return spans
 }
 
 type workItem struct {
