5e691bce19b973c992e8753e9dd9831a594ba09e
David Hartunian <davidh@cockroachlabs.com>
2020-02-21 14:32:22 -0500

    ui: refactor; extract jobStatusDropdown
    Release note: none

---
+++ pkg/ui/src/views/jobs/jobStatusOptions.ts
@@ -0,0 +1,9 @@
+export const statusOptions = [
+  {value: "", label: "All"},
+  {value: "pending", label: "Pending"},
+  {value: "running", label: "Running"},
+  {value: "paused", label: "Paused"},
+  {value: "canceled", label: "Canceled"},
+  {value: "succeeded", label: "Succeeded"},
+  {value: "failed", label: "Failed"},
+];
