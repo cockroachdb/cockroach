d10d875a8c202deeafd09fe571f3e472b9cc3cdb
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-02-24 16:08:11 -0800

    colexec: fix a couple of issues with memory accounting
    This commit fixes an issue of not correctly accounting for the selection
    vector in `RetainBatch` / `ReleaseBatch` methods when the selection is
    set to `false`.
    
    Also, it fixes an issue with memory accounting in external sort when
    we're dividing the input into partitions. Previously, we used
    `RetainBatch` method to track the memory usage. However, that method
    looks at the capacities of the vectors in the batch whereas in tests we
    often have full-sizes vectors with the length of the batch being very
    small. This caused external sorter to create a partition for every tuple
    (if `batchSize` of 1 is used) which caused some tests to be extremely
    slow. This is now fixed by accounting for the "portion" of the batch's
    memory footprint proportional to the length of the batch.
    
    Release note: None

--- pkg/sql/colexec/external_sort_test.go
+++ pkg/sql/colexec/external_sort_test.go
@@ -123,7 +123,6 @@ func TestExternalSort(t *testing.T) {
 
 func TestExternalSortRandomized(t *testing.T) {
 	defer leaktest.AfterTest(t)()
-	t.Skip("https://github.com/cockroachdb/cockroach/issues/45322")
 	ctx := context.Background()
 	st := cluster.MakeTestingClusterSettings()
 	evalCtx := tree.MakeTestingEvalContext(st)
