4c86748782c646301d21840d38dfc020d4d96278
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2020-02-26 16:36:43 +0100

    gc: paginate GC of local key ranges
    Touches #40815.
    
    I'm not calling it fixed yet because there are two remaining issues:
    
    1. GC will not trigger automatically based on a large abort span. This
       means that manual intervention is needed in such a case.
    2. The local key ranges are processed after the user key ranges.
       Even if no user keys are GC'able, all of it has to be scanned
       regardless (to arrive at that conclusion). With a large enough
       range size, this scan could theoretically consume the entirety
       of the time budget.
    
    However, in practice we expect operators to be able to "fix" any
    range via a manual GC once this PR is merged. Follow-up work needs
    to be done to address 1) and 2) above.
    
    Release note (bug fix): Improved the ability of garbage collection
    process to make process through ranges exhibiting abnormally large
    numbers of transaction records and/or abort span entries.

--- pkg/storage/gc/gc_test.go
+++ pkg/storage/gc/gc_test.go
@@ -11,11 +11,15 @@
 package gc
 
 import (
+	"bytes"
+	"context"
 	"testing"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/config/zonepb"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/stretchr/testify/require"
 )
 
@@ -32,4 +36,56 @@ func TestCalculateThreshold(t *testing.T) {
 		policy := zonepb.GCPolicy{TTLSeconds: c.ttlSeconds}
 		require.Equal(t, c.ts, TimestampForThreshold(CalculateThreshold(c.ts, policy), policy))
 	}
+}
+
+type collectingGCer struct {
+	keys [][]roachpb.GCRequest_GCKey
+}
+
+func (c *collectingGCer) GC(_ context.Context, keys []roachpb.GCRequest_GCKey) error {
+	c.keys = append(c.keys, keys)
+	return nil
+}
+
+func TestBatchingInlineGCer(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	ctx := context.Background()
+	c := &collectingGCer{}
+	m := makeBatchingInlineGCer(c, func(err error) { t.Error(err) })
+	if m.max == 0 {
+		t.Fatal("did not init max")
+	}
+	m.max = 10 // something reasonable for this unit test
+
+	long := roachpb.GCRequest_GCKey{
+		Key: bytes.Repeat([]byte("x"), m.max-1),
+	}
+	short := roachpb.GCRequest_GCKey{
+		Key: roachpb.Key("q"),
+	}
+
+	m.FlushingAdd(ctx, long.Key)
+	require.Nil(t, c.keys) // no flush
+
+	m.FlushingAdd(ctx, short.Key)
+	// Flushed long and short.
+	require.Len(t, c.keys, 1)
+	require.Len(t, c.keys[0], 2)
+	require.Equal(t, long, c.keys[0][0])
+	require.Equal(t, short, c.keys[0][1])
+	// Reset itself properly.
+	require.Nil(t, m.gcKeys)
+	require.Zero(t, m.size)
+
+	m.FlushingAdd(ctx, short.Key)
+	require.Len(t, c.keys, 1) // no flush
+
+	m.Flush(ctx)
+	require.Len(t, c.keys, 2) // flushed
+	require.Len(t, c.keys[1], 1)
+	require.Equal(t, short, c.keys[1][0])
+	// Reset itself properly.
+	require.Nil(t, m.gcKeys)
+	require.Zero(t, m.size)
 }
