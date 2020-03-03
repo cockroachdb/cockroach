feb3d45b8235fb53f426606916da4db3ec1f4051
David Hartunian <davidh@cockroachlabs.com>
2020-03-02 22:53:52 -0500

    sql: add API for accessing statement diagnostics
    Added 3 new RPC endpoints under `_status` for manipulating Statement Diagnostics
    Requests:
    
    1. `CreateStatementDiagnosticsRequest`
    Creates a new request for a given statement fingerprint
    
    2. `StatementDiagnosticsRequests`
    Fetches all statement diagnostics requests
    
    3. `StatementDiagnostics`
    Fetches a specific statement diagnostics request that has been completed and
    includes the JSON payload of the trace
    
    Basic implementations for these endpoints have been added to the
    `apiReducers.tsx` and `api.ts` files for use by the Admin UI.
    
    A minor bug where completed requests did not have `completedAt` timestamp set
    was fixed.

--- pkg/ui/src/util/api.ts
+++ pkg/ui/src/util/api.ts
@@ -110,6 +110,15 @@ export type EnqueueRangeResponseMessage = protos.cockroach.server.serverpb.Enque
 export type MetricMetadataRequestMessage = protos.cockroach.server.serverpb.MetricMetadataRequest;
 export type MetricMetadataResponseMessage = protos.cockroach.server.serverpb.MetricMetadataResponse;
 
+export type StatementDiagnosticsReportsRequestMessage = protos.cockroach.server.serverpb.StatementDiagnosticsReportsRequest;
+export type StatementDiagnosticsReportsResponseMessage = protos.cockroach.server.serverpb.StatementDiagnosticsReportsResponse;
+
+export type CreateStatementDiagnosticsReportRequestMessage = protos.cockroach.server.serverpb.CreateStatementDiagnosticsReportRequest;
+export type CreateStatementDiagnosticsReportResponseMessage = protos.cockroach.server.serverpb.CreateStatementDiagnosticsReportResponse;
+
+export type StatementDiagnosticsRequestMessage = protos.cockroach.server.serverpb.StatementDiagnosticsRequest;
+export type StatementDiagnosticsResponseMessage = protos.cockroach.server.serverpb.StatementDiagnosticsResponse;
+
 // API constants
 
 export const API_PREFIX = "_admin/v1";
@@ -347,6 +356,18 @@ export function getStores(req: StoresRequestMessage, timeout?: moment.Duration):
 // getStatements returns statements the cluster has recently executed, and some stats about them.
 export function getStatements(timeout?: moment.Duration): Promise<StatementsResponseMessage> {
   return timeoutFetch(serverpb.StatementsResponse, `${STATUS_PREFIX}/statements`, null, timeout);
+}
+
+export function getStatementDiagnosticsReports(timeout?: moment.Duration): Promise<StatementDiagnosticsReportsResponseMessage> {
+  return timeoutFetch(serverpb.StatementDiagnosticsReportsResponse, `${STATUS_PREFIX}/stmtdiagreports`, null, timeout);
+}
+
+export function createStatementDiagnosticsReport(req: CreateStatementDiagnosticsReportRequestMessage, timeout?: moment.Duration): Promise<CreateStatementDiagnosticsReportResponseMessage> {
+  return timeoutFetch(serverpb.CreateStatementDiagnosticsReportResponse, `${STATUS_PREFIX}/stmtdiagreports`, req as any, timeout);
+}
+
+export function getStatementDiagnostics(req: StatementDiagnosticsRequestMessage, timeout?: moment.Duration): Promise<StatementDiagnosticsResponseMessage> {
+  return timeoutFetch(serverpb.StatementDiagnosticsResponse, `${STATUS_PREFIX}/stmtdiag/${req.statement_diagnostics_id}`, null, timeout);
 }
 
 // getDataDistribution returns information about how replicas are distributed across nodes.
