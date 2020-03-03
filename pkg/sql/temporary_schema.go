abe224f6f6f90bb05f85451552517b8a739f92d1
Oliver Tan <otan@cockroachlabs.com>
2020-03-03 11:11:45 -0800

    sql: implement a background task for temp object cleanup
    This PR introduces a `TempObjectCleaner`, which is a background task
    that runs periodically on every node, but will only be executed by the
    node that has the meta1 leaseholder. This task will cleanup dangling
    temporary schemas that could not have been cleaned up by the connection.
    The job also comes with logging and metrics. A simple test mocking the
    ticking mechanism has also been included.
    
    Also refactored the existing temporary schema deletion to use ExecEx,
    which overrides the SearchPath / User executing the deletion at a lower
    layer rather than using `SetSessionExecutor`.
    
    Release note (sql change): Introduced a temporary table cleanup job that
    runs once every periodically per cluster that will remove any temporary
    schemas and their related objects that did not get removed cleanly when
    the connection closed. This period can be changed by the cluster setting
    `sql.temp_object_cleaner.cleanup_interval`, which defaults to 30min-ly.

--- pkg/sql/temporary_schema.go
+++ pkg/sql/temporary_schema.go
@@ -13,18 +13,72 @@ package sql
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/kv"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/security"
+	"github.com/cockroachdb/cockroach/pkg/server/serverpb"
+	"github.com/cockroachdb/cockroach/pkg/server/telemetry"
+	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql/schema"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqltelemetry"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlutil"
 	"github.com/cockroachdb/cockroach/pkg/util"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/metric"
+	"github.com/cockroachdb/cockroach/pkg/util/retry"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/cockroachdb/cockroach/pkg/util/uint128"
+	"github.com/cockroachdb/errors"
+	io_prometheus_client "github.com/prometheus/client_model/go"
+)
+
+// TempObjectCleanupInterval is a ClusterSetting controlling how often
+// temporary objects get cleaned up.
+var TempObjectCleanupInterval = settings.RegisterPublicDurationSetting(
+	"sql.temp_object_cleaner.cleanup_interval",
+	"how often to clean up orphaned temporary objects",
+	30*time.Minute,
+)
+
+var (
+	temporaryObjectCleanerActiveCleanersMetric = metric.Metadata{
+		Name:        "sql.temp_object_cleaner.active_cleaners",
+		Help:        "number of cleaner t®asks currently running on this node",
+		Measurement: "Count",
+		Unit:        metric.Unit_COUNT,
+		MetricType:  io_prometheus_client.MetricType_GAUGE,
+	}
+	temporaryObjectCleanerSchemasToDeleteMetric = metric.Metadata{
+		Name:        "sql.temp_object_cleaner.schemas_to_delete",
+		Help:        "number of schemas to be deleted by the temp object cleaner on this node",
+		Measurement: "Count",
+		Unit:        metric.Unit_COUNT,
+		MetricType:  io_prometheus_client.MetricType_COUNTER,
+	}
+	temporaryObjectCleanerSchemasDeletionErrorMetric = metric.Metadata{
+		Name:        "sql.temp_object_cleaner.schemas_deletion_error",
+		Help:        "number of errored schema deletions by the temp object cleaner on this node",
+		Measurement: "Count",
+		Unit:        metric.Unit_COUNT,
+		MetricType:  io_prometheus_client.MetricType_COUNTER,
+	}
+	temporaryObjectCleanerSchemasDeletionSuccessMetric = metric.Metadata{
+		Name:        "sql.temp_object_cleaner.schemas_deletion_success",
+		Help:        "number of successful schema deletions by the temp object cleaner on this node",
+		Measurement: "Count",
+		Unit:        metric.Unit_COUNT,
+		MetricType:  io_prometheus_client.MetricType_COUNTER,
+	}
 )
 
 func createTempSchema(params runParams, sKey sqlbase.DescriptorKey) (sqlbase.ID, error) {
@@ -60,6 +114,26 @@ func temporarySchemaName(sessionID ClusterWideID) string {
 	return fmt.Sprintf("pg_temp_%d_%d", sessionID.Hi, sessionID.Lo)
 }
 
+// temporarySchemaSessionID returns the sessionID of the given temporary schema.
+func temporarySchemaSessionID(scName string) (bool, ClusterWideID, error) {
+	if !strings.HasPrefix(scName, "pg_temp_") {
+		return false, ClusterWideID{}, nil
+	}
+	parts := strings.Split(scName, "_")
+	if len(parts) != 4 {
+		return false, ClusterWideID{}, errors.Errorf("malformed temp schema name %s", scName)
+	}
+	hi, err := strconv.ParseUint(parts[2], 10, 64)
+	if err != nil {
+		return false, ClusterWideID{}, err
+	}
+	lo, err := strconv.ParseUint(parts[3], 10, 64)
+	if err != nil {
+		return false, ClusterWideID{}, err
+	}
+	return true, ClusterWideID{uint128.Uint128{Hi: hi, Lo: lo}}, nil
+}
+
 // getTemporaryObjectNames returns all the temporary objects under the
 // temporary schema of the given dbID.
 func getTemporaryObjectNames(
@@ -81,16 +155,15 @@ func getTemporaryObjectNames(
 
 // cleanupSessionTempObjects removes all temporary objects (tables, sequences,
 // views, temporary schema) created by the session.
-func cleanupSessionTempObjects(ctx context.Context, server *Server, sessionID ClusterWideID) error {
+func cleanupSessionTempObjects(
+	ctx context.Context,
+	settings *cluster.Settings,
+	db *kv.DB,
+	ie sqlutil.InternalExecutor,
+	sessionID ClusterWideID,
+) error {
 	tempSchemaName := temporarySchemaName(sessionID)
-	sd := &sessiondata.SessionData{
-		SearchPath:    sqlbase.DefaultSearchPath.WithTemporarySchemaName(tempSchemaName),
-		User:          security.RootUser,
-		SequenceState: &sessiondata.SequenceState{},
-	}
-	ie := MakeInternalExecutor(ctx, server, MemoryMetrics{}, server.cfg.Settings)
-	ie.SetSessionData(sd)
-	return server.cfg.DB.Txn(ctx, func(ctx context.Context, txn *kv.Txn) error {
+	return db.Txn(ctx, func(ctx context.Context, txn *kv.Txn) error {
 		// We are going to read all database descriptor IDs, then for each database
 		// we will drop all the objects under the temporary schema.
 		dbIDs, err := GetAllDatabaseDescriptorIDs(ctx, txn)
@@ -98,7 +171,14 @@ func cleanupSessionTempObjects(ctx context.Context, server *Server, sessionID Cl
 			return err
 		}
 		for _, id := range dbIDs {
-			if err := cleanupSchemaObjects(ctx, server.cfg.Settings, ie.Exec, txn, id, tempSchemaName); err != nil {
+			if err := cleanupSchemaObjects(
+				ctx,
+				settings,
+				txn,
+				ie,
+				id,
+				tempSchemaName,
+			); err != nil {
 				return err
 			}
 			// Even if no objects were found under the temporary schema, the schema
@@ -113,24 +193,12 @@ func cleanupSessionTempObjects(ctx context.Context, server *Server, sessionID Cl
 	})
 }
 
-// TestingCleanupSchemaObjects is a wrapper around cleanupSchemaObjects, used for testing only.
-func TestingCleanupSchemaObjects(
-	ctx context.Context,
-	settings *cluster.Settings,
-	execQuery func(context.Context, string, *kv.Txn, string, ...interface{}) (int, error),
-	txn *kv.Txn,
-	dbID sqlbase.ID,
-	schemaName string,
-) error {
-	return cleanupSchemaObjects(ctx, settings, execQuery, txn, dbID, schemaName)
-}
-
 // cleanupSchemaObjects removes all objects that is located within a dbID and schema.
 func cleanupSchemaObjects(
 	ctx context.Context,
 	settings *cluster.Settings,
-	execQuery func(context.Context, string, *kv.Txn, string, ...interface{}) (int, error),
 	txn *kv.Txn,
+	ie sqlutil.InternalExecutor,
 	dbID sqlbase.ID,
 	schemaName string,
 ) error {
@@ -139,6 +207,12 @@ func cleanupSchemaObjects(
 		return err
 	}
 	a := UncachedPhysicalAccessor{}
+
+	searchPath := sqlbase.DefaultSearchPath.WithTemporarySchemaName(schemaName)
+	override := sqlbase.InternalExecutorSessionDataOverride{
+		SearchPath: &searchPath,
+		User:       security.RootUser,
+	}
 
 	// TODO(andrei): We might want to accelerate the deletion of this data.
 	var tables sqlbase.IDs
@@ -227,10 +301,11 @@ func cleanupSchemaObjects(
 								tree.Name(schema),
 								tree.Name(dTableDesc.Name),
 							)
-							_, err = execQuery(
+							_, err = ie.ExecEx(
 								ctx,
 								"delete-temp-dependent-col",
 								txn,
+								override,
 								fmt.Sprintf(
 									"ALTER TABLE %s ALTER COLUMN %s DROP DEFAULT",
 									tbName.FQString(),
@@ -269,11 +344,219 @@ func cleanupSchemaObjects(
 				query.WriteString(tbName.FQString())
 			}
 			query.WriteString(" CASCADE")
-			_, err = execQuery(ctx, "delete-temp-"+toDelete.typeName, txn, query.String())
+			_, err = ie.ExecEx(ctx, "delete-temp-"+toDelete.typeName, txn, override, query.String())
 			if err != nil {
 				return err
 			}
 		}
 	}
 	return nil
+}
+
+// isMeta1LeaseholderFunc helps us avoid an import into pkg/storage.
+type isMeta1LeaseholderFunc func(hlc.Timestamp) (bool, error)
+
+// TemporaryObjectCleaner is a background thread job that periodically
+// cleans up orphaned temporary objects by sessions which did not close
+// down cleanly.
+type TemporaryObjectCleaner struct {
+	settings                         *cluster.Settings
+	db                               *kv.DB
+	makeSessionBoundInternalExecutor sqlutil.SessionBoundInternalExecutorFactory
+	statusServer                     serverpb.StatusServer
+	isMeta1LeaseholderFunc           isMeta1LeaseholderFunc
+	testingKnobs                     ExecutorTestingKnobs
+	metrics                          *temporaryObjectCleanerMetrics
+}
+
+// temporaryObjectCleanerMetrics are the metrics for TemporaryObjectCleaner
+type temporaryObjectCleanerMetrics struct {
+	ActiveCleaners         *metric.Gauge
+	SchemasToDelete        *metric.Counter
+	SchemasDeletionError   *metric.Counter
+	SchemasDeletionSuccess *metric.Counter
+}
+
+var _ metric.Struct = (*temporaryObjectCleanerMetrics)(nil)
+
+// MetricStruct implements the metrics.Struct interface.
+func (m *temporaryObjectCleanerMetrics) MetricStruct() {}
+
+// NewTemporaryObjectCleaner initializes the TemporaryObjectCleaner with the
+// required arguments, but does not start it.
+func NewTemporaryObjectCleaner(
+	settings *cluster.Settings,
+	db *kv.DB,
+	makeSessionBoundInternalExecutor sqlutil.SessionBoundInternalExecutorFactory,
+	statusServer serverpb.StatusServer,
+	isMeta1LeaseholderFunc isMeta1LeaseholderFunc,
+	testingKnobs ExecutorTestingKnobs,
+) *TemporaryObjectCleaner {
+	return &TemporaryObjectCleaner{
+		settings:                         settings,
+		db:                               db,
+		makeSessionBoundInternalExecutor: makeSessionBoundInternalExecutor,
+		statusServer:                     statusServer,
+		isMeta1LeaseholderFunc:           isMeta1LeaseholderFunc,
+		testingKnobs:                     testingKnobs,
+		metrics:                          makeTemporaryObjectCleanerMetrics(),
+	}
+}
+
+// makeTemporaryObjectCleanerMetrics makes the metrics for the TemporaryObjectCleaner.
+func makeTemporaryObjectCleanerMetrics() *temporaryObjectCleanerMetrics {
+	return &temporaryObjectCleanerMetrics{
+		ActiveCleaners:         metric.NewGauge(temporaryObjectCleanerActiveCleanersMetric),
+		SchemasToDelete:        metric.NewCounter(temporaryObjectCleanerSchemasToDeleteMetric),
+		SchemasDeletionError:   metric.NewCounter(temporaryObjectCleanerSchemasDeletionErrorMetric),
+		SchemasDeletionSuccess: metric.NewCounter(temporaryObjectCleanerSchemasDeletionSuccessMetric),
+	}
+}
+
+// doTemporaryObjectCleanup performs the actual cleanup.
+func (c *TemporaryObjectCleaner) doTemporaryObjectCleanup(ctx context.Context) error {
+	// Wrap the retry functionality with the default arguments.
+	retryFunc := func(ctx context.Context, do func() error) error {
+		return retry.WithMaxAttempts(
+			ctx,
+			retry.Options{InitialBackoff: 1 * time.Second, MaxBackoff: 1 * time.Minute, Multiplier: 2},
+			5, // maxAttempts
+			func() error {
+				err := do()
+				if err != nil {
+					log.Warningf(ctx, "error during schema cleanup, retrying: %v", err)
+				}
+				return err
+			},
+		)
+	}
+
+	// We only want to perform the cleanup if we are holding the meta1 lease.
+	// This ensures only one server can perform the job at a time.
+	isLeaseholder, err := c.isMeta1LeaseholderFunc(c.db.Clock().Now())
+	if err != nil {
+		return err
+	}
+	if !isLeaseholder {
+		log.Infof(ctx, "skipping temporary object cleanup run as it is not the leaseholder")
+		return nil
+	}
+
+	c.metrics.ActiveCleaners.Inc(1)
+	defer c.metrics.ActiveCleaners.Dec(1)
+
+	log.Infof(ctx, "running temporary object cleanup background job")
+	txn := kv.NewTxn(ctx, c.db, 0)
+
+	// Build a set of all session IDs with temporary objects.
+	var dbIDs []sqlbase.ID
+	if err := retryFunc(ctx, func() error {
+		var err error
+		dbIDs, err = GetAllDatabaseDescriptorIDs(ctx, txn)
+		return err
+	}); err != nil {
+		return err
+	}
+	sessionIDs := make(map[ClusterWideID]struct{})
+	for _, dbID := range dbIDs {
+		var schemaNames map[sqlbase.ID]string
+		if err := retryFunc(ctx, func() error {
+			var err error
+			schemaNames, err = schema.GetForDatabase(ctx, txn, dbID)
+			return err
+		}); err != nil {
+			return err
+		}
+		for _, scName := range schemaNames {
+			isTempSchema, sessionID, err := temporarySchemaSessionID(scName)
+			if err != nil {
+				// This should not cause an error.
+				log.Warningf(ctx, "could not parse %q as temporary schema name", scName)
+				continue
+			}
+			if isTempSchema {
+				sessionIDs[sessionID] = struct{}{}
+			}
+		}
+	}
+	log.Infof(ctx, "found %d temporary schemas", len(sessionIDs))
+
+	// Get active sessions.
+	var response *serverpb.ListSessionsResponse
+	if err := retryFunc(ctx, func() error {
+		var err error
+		response, err = c.statusServer.ListSessions(
+			ctx,
+			&serverpb.ListSessionsRequest{},
+		)
+		return err
+	}); err != nil {
+		return err
+	}
+	activeSessions := make(map[uint128.Uint128]struct{})
+	for _, session := range response.Sessions {
+		activeSessions[uint128.FromBytes(session.ID)] = struct{}{}
+	}
+
+	// Clean up temporary data for inactive sessions.
+	ie := c.makeSessionBoundInternalExecutor(ctx, &sessiondata.SessionData{})
+	for sessionID := range sessionIDs {
+		if _, ok := activeSessions[sessionID.Uint128]; !ok {
+			log.Eventf(ctx, "cleaning up temporary object for session %q", sessionID)
+			c.metrics.SchemasToDelete.Inc(1)
+
+			// Reset the session data with the appropriate sessionID such that we can resolve
+			// the given schema correctly.
+			if err := retryFunc(ctx, func() error {
+				return cleanupSessionTempObjects(
+					ctx,
+					c.settings,
+					c.db,
+					ie,
+					sessionID,
+				)
+			}); err != nil {
+				// Log error but continue trying to delete the rest.
+				log.Warningf(ctx, "failed to clean temp objects under session %q: %v", sessionID, err)
+				c.metrics.SchemasDeletionError.Inc(1)
+			} else {
+				c.metrics.SchemasDeletionSuccess.Inc(1)
+				telemetry.Inc(sqltelemetry.TempObjectCleanerDeletionCounter)
+			}
+		} else {
+			log.Eventf(ctx, "not cleaning up %q as session is still active", sessionID)
+		}
+	}
+
+	log.Infof(ctx, "completed temporary object cleanup job")
+	return nil
+}
+
+// Start initializes the background thread which periodically cleans up leftover temporary objects.
+func (c *TemporaryObjectCleaner) Start(ctx context.Context, stopper *stop.Stopper) {
+	stopper.RunWorker(ctx, func(ctx context.Context) {
+		nextTick := timeutil.Now()
+		for {
+			nextTickCh := time.After(nextTick.Sub(timeutil.Now()))
+			if c.testingKnobs.TempObjectsCleanupCh != nil {
+				nextTickCh = c.testingKnobs.TempObjectsCleanupCh
+			}
+
+			select {
+			case <-nextTickCh:
+				if err := c.doTemporaryObjectCleanup(ctx); err != nil {
+					log.Warningf(ctx, "failed to clean temp objects: %v", err)
+				}
+			case <-stopper.ShouldQuiesce():
+				return
+			case <-ctx.Done():
+				return
+			}
+			if c.testingKnobs.OnTempObjectsCleanupDone != nil {
+				c.testingKnobs.OnTempObjectsCleanupDone()
+			}
+			nextTick = nextTick.Add(TempObjectCleanupInterval.Get(&c.settings.SV))
+			log.Infof(ctx, "temporary object cleaner next scheduled to run at %s", nextTick)
+		}
+	})
 }
