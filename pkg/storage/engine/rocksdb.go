45a1ac17b6f7a1c7e70afe20264c766fc8e6b8f5
Raphael 'kena' Poss <knz@thaumogen.net>
2020-03-04 12:09:28 +0100

    util/log: make SecondaryLogger a Closer and do the right thing
    Previously, secondary loggers would leak in the global registry.
    A previous patch of mine tried to improve the situation by
    asynchronously de-registering loggers, but that was not good enough.
    This patch fixes that by adding a `Close()` method to do the job.
    
    Release note: None

--- pkg/storage/engine/rocksdb.go
+++ pkg/storage/engine/rocksdb.go
@@ -84,10 +84,12 @@ var rocksdbLogger *log.SecondaryLogger
 
 // InitRocksDBLogger initializes the logger to use for RocksDB log messages. If
 // not called, WARNING, ERROR, and FATAL logs will be output to the normal
-// CockroachDB log.
-func InitRocksDBLogger(ctx context.Context) {
+// CockroachDB log. The caller is responsible for ensuring the
+// Close() method is eventually called on the new logger.
+func InitRocksDBLogger(ctx context.Context) *log.SecondaryLogger {
 	rocksdbLogger = log.NewSecondaryLogger(ctx, nil, "rocksdb",
 		true /* enableGC */, false /* forceSyncWrites */, false /* enableMsgCount */)
+	return rocksdbLogger
 }
 
 //export rocksDBLog
