142a9e5ed078b70e4ee0d0515183ac124f2a23db
Raphael 'kena' Poss <knz@thaumogen.net>
2020-03-04 20:01:38 +0100

    pgwire/hba: preserve the original configuration line
    Previously the original HBA configuration text was discarded during
    parsing and normalization.
    
    This patch changes the code to retain the original text. This is
    useful to preserve any explanatory comments a user may have placed at
    the end of each line. This will be also necessary/useful for
    authentication logging.
    
    Release note: None

--- pkg/sql/pgwire/hba_conf.go
+++ pkg/sql/pgwire/hba_conf.go
@@ -14,7 +14,6 @@ import (
 	"context"
 	"net"
 	"net/http"
-	"reflect"
 	"sort"
 	"strings"
 
@@ -220,7 +219,7 @@ func ParseAndNormalize(val string) (*hba.Conf, error) {
 		return conf, err
 	}
 
-	if len(conf.Entries) == 0 || !reflect.DeepEqual(conf.Entries[0], rootEntry) {
+	if len(conf.Entries) == 0 || !conf.Entries[0].Equivalent(rootEntry) {
 		entries := make([]hba.Entry, 1, len(conf.Entries)+1)
 		entries[0] = rootEntry
 		entries = append(entries, conf.Entries...)
@@ -248,6 +247,7 @@ var rootEntry = hba.Entry{
 	User:     []hba.String{{Value: security.RootUser, Quoted: false}},
 	Address:  hba.AnyAddr{},
 	Method:   hba.String{Value: "cert-password"},
+	Input:    "host  all root all cert-password # CockroachDB mandatory rule",
 }
 
 // DefaultHBAConfig is used when the stored HBA configuration string
@@ -255,8 +255,8 @@ var rootEntry = hba.Entry{
 var DefaultHBAConfig = func() *hba.Conf {
 	loadDefaultMethods()
 	conf, err := ParseAndNormalize(`
-host      all all  all cert-password
-local     all all      password
+host  all all  all cert-password # built-in CockroachDB default
+local all all      password      # built-in CockroachDB default
 `)
 	if err != nil {
 		panic(err)
