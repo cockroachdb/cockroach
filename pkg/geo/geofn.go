7d9e617670e4ffb7437641aa92a2a74b2bde34ce
Daniel Harrison <daniel.harrison@gmail.com>
2020-03-05 15:10:14 -0800

    geo: begin a library for spatial data operations
    Included:
    - A roundtrip-able long term storage format (implemented for a small
      number of the shapes we'll support)
    - Conversion between GEOMETRY and GEOGRAPHY
    - The very beginnings of SRID support
    
    Release note: None

---
+++ pkg/geo/geofn.go
@@ -0,0 +1,41 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package geo
+
+import (
+	"github.com/cockroachdb/errors"
+	"github.com/golang/geo/s2"
+	"github.com/twpayne/go-geom"
+)
+
+// STAreaGeometry exactly matches the semantics of ST_Area.
+func STAreaGeometry(g *Geometry) (float64, error) {
+	switch s := g.Shape.(type) {
+	case *geom.Point:
+		return s.Area(), nil
+	default:
+		return 0.0, errors.Errorf(`unhandled geom type %T: %s`, s, s)
+	}
+}
+
+// STAreaGeography exactly matches the semantics of ST_Area.
+func STAreaGeography(g *Geography) (float64, error) {
+	var area float64
+	for _, shape := range g.Shape {
+		switch s := shape.(type) {
+		case s2.Point:
+			// No-op.
+		default:
+			return 0.0, errors.Errorf(`unhandled geog type %T: %s`, s, s)
+		}
+	}
+	return area, nil
+}
