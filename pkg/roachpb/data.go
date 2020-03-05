6b5a01d705846ef6c27089ba5b3c742ea318af9a
Andrei Matei <andrei@cockroachlabs.com>
2020-03-04 22:15:27 -0500

    kv, roachpb: move seq num manipulation under Transaction
    It belongs better there than as a standalone function, particularly
    since we need to give special consideration to transaction fields (see
    next commit).
    
    Release note: None

--- pkg/roachpb/data.go
+++ pkg/roachpb/data.go
@@ -1162,6 +1162,52 @@ func (t *Transaction) GetObservedTimestamp(nodeID NodeID) (hlc.Timestamp, bool) 
 	return s.get(nodeID)
 }
 
+// AddIgnoredSeqNumRange adds the given range to the transaction's list of
+// ignored seqnum ranges.
+//
+// The following invariants are assumed to hold and are preserved:
+// - the list contains no overlapping ranges
+// - the list contains no contiguous ranges
+// - the list is sorted, with larger seqnums at the end
+//
+// Additionally, the caller must ensure:
+//
+// 1) if the new range overlaps with some range in the list, then it
+//    also overlaps with every subsequent range in the list.
+//
+// 2) the new range's "end" seqnum is larger or equal to the "end"
+//    seqnum of the last element in the list.
+//
+// For example:
+//     current list [3 5] [10 20] [22 24]
+//     new item:    [8 26]
+//     final list:  [3 5] [8 26]
+//
+//     current list [3 5] [10 20] [22 24]
+//     new item:    [28 32]
+//     final list:  [3 5] [10 20] [22 24] [28 32]
+//
+// This corresponds to savepoints semantics:
+//
+// - Property 1 says that a rollback to an earlier savepoint
+//   rolls back over all writes following that savepoint.
+// - Property 2 comes from that the new range's 'end' seqnum is the
+//   current write seqnum and thus larger than or equal to every
+//   previously seen value.
+func (t *Transaction) AddIgnoredSeqNumRange(newRange enginepb.IgnoredSeqNumRange) {
+	// Truncate the list at the last element not included in the new range.
+	list := t.IgnoredSeqNums
+	i := 0
+	for ; i < len(list); i++ {
+		if list[i].End < newRange.Start {
+			continue
+		}
+		break
+	}
+	list = list[:i]
+	t.IgnoredSeqNums = append(list, newRange)
+}
+
 // AsRecord returns a TransactionRecord object containing only the subset of
 // fields from the receiver that must be persisted in the transaction record.
 func (t *Transaction) AsRecord() TransactionRecord {
