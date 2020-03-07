506c887adcb7a6b7baafd154d1904f3ee1a20a2f
richardjcai <caioftherichard@gmail.com>
2020-03-06 19:14:52 -0500

    Make SHOW USERS/SHOW ROLES match psql \du command.
    Release note (sql change): Make SHOW USERS and SHOW ROLES the same since USERS=ROLES.
    Make SHOW USERS and SHOW ROLES match psql \du command. SHOW ROLES displays three columns, username, options, member_of.
    
    Release justification: Low risk change, only changing underlying select command for SHOW ROLES/USERS.

--- pkg/sql/delegate/show_roles.go
+++ pkg/sql/delegate/show_roles.go
@@ -17,7 +17,17 @@ import (
 
 // delegateShowRoles implements SHOW ROLES which returns all the roles.
 // Privileges: SELECT on system.users.
-func (d *delegator) delegateShowRoles(n *tree.ShowRoles) (tree.Statement, error) {
+func (d *delegator) delegateShowRoles() (tree.Statement, error) {
 	sqltelemetry.IncrementShowCounter(sqltelemetry.Roles)
-	return parse(`SELECT username AS role_name FROM system.users WHERE "isRole" = true ORDER BY 1`)
+	return parse(`
+SELECT
+	u.username,
+	IFNULL(string_agg(o.option || COALESCE('=' || o.value, ''), ', ' ORDER BY o.option), '') AS options,
+	ARRAY (SELECT role FROM system.role_members AS rm WHERE rm.member = u.username ORDER BY 1) AS member_of
+FROM
+	system.users AS u LEFT JOIN system.role_options AS o ON u.username = o.username
+GROUP BY
+	u.username
+ORDER BY 1;
+`)
 }
