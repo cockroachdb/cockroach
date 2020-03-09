f7aff0d1191797c9a4723878eb9a3b5719957847
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2020-03-09 12:50:10 +0100

    kv: fix various go generate invocations
    Needed to adjust relative paths.
    
    Release note: None

--- pkg/kv/bulk/main_test.go
+++ pkg/kv/bulk/main_test.go
@@ -22,7 +22,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 )
 
-//go:generate ../../../util/leaktest/add-leaktest.sh *_test.go
+//go:generate ../../util/leaktest/add-leaktest.sh *_test.go
 
 func init() {
 	security.SetAssetLoader(securitytest.EmbeddedAssets)
