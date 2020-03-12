b539a0ef8c0fea2a2621b47de0a7ae01d3ae0925
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2020-03-12 12:08:37 +0100

    kvserver: comment on propBuf locking
    Release justification: comment-only change
    Release note: None

--- pkg/kv/kvserver/replica.go
+++ pkg/kv/kvserver/replica.go
@@ -305,6 +305,10 @@ type Replica struct {
 		// evaluation and is consumed by the Raft processing thread. Once
 		// consumed, commands are proposed through Raft and moved to the
 		// proposals map.
+		//
+		// Access to proposalBuf must occur *without* holding the mutex.
+		// Instead, the buffer internally holds a reference to mu and will use
+		// it appropriately.
 		proposalBuf propBuf
 		// proposals stores the Raft in-flight commands which originated at
 		// this Replica, i.e. all commands for which propose has been called,
