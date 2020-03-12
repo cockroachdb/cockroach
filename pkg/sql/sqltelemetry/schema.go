b83169418facbd2c0cb77a4e452eaed2aac2edc0
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-03-12 15:57:04 -0700

    sql: add telemetry for ORDER BY and INTERLEAVE
    Release justification: reporting changes only.
    
    This commit adds the reporting of telemetry on the usage of INTERLEAVE
    (in `createTableNode.startExec`) and of ORDER BY (by marking
    `opt.SortOp` with "Telemetry" flag in the optbuilder and reusing the
    same "infrastructure" as relational operators do). Also it adds a logic
    test for the telemetry collection on the window functions' usage.
    
    Release note: None

--- pkg/sql/sqltelemetry/schema.go
+++ pkg/sql/sqltelemetry/schema.go
@@ -31,6 +31,10 @@ func SchemaNewTypeCounter(t string) telemetry.Counter {
 }
 
 var (
+	// CreateInterleavedTableCounter is to be incremented every time an
+	// interleaved table is being created.
+	CreateInterleavedTableCounter = telemetry.GetCounterOnce("sql.schema.create_interleaved_table")
+
 	// CreateTempTableCounter is to be incremented every time a TEMP TABLE
 	// has been created.
 	CreateTempTableCounter = telemetry.GetCounterOnce("sql.schema.create_temp_table")
