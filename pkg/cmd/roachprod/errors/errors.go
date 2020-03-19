c537fc85b98413e4eaf28337fa251e1dd13b3df8
James H. Linder <jamesl@cockroachlabs.com>
2020-03-19 10:45:59 -0400

    roachprod: distinguish infra and crdb errors
    Before: there are many reasons why roachprod might fail for a given command
    and it didn't differentiate between the different types of errors.
    
    Why change?
    
    - The lack of detail for why an error happened made debugging problems harder
      than needed.
    
    - We couldn't use automated methods to aggregate error reporting or direct
      errors to the right team when it's used by roachtest.
    
    - We want to gather metrics on specific types of errors and it's wasn't
      possible without more granular error reporting from roachprod.
    
    Now:
    
    - roachprod exits with different codes depending on type of error:
    
        0: all OK
        1: an unclassified roachprod error
        10: a problem with an SSH connection to a server in the cluster
        20: a problem running a non-cockroach command on a remote server in the
           cluster
        30: a problem running a cockroach command on a remote server in the cluster
    
    - for each error type, an easily-searchable string is emitted by roachprod at
      or near the end of output. The string will also be emitted for each error
      that happens during an ssh connection to a remote cluster node. The strings
      for each error code are:
    
        1:  UNCLASSIFIED_PROBLEM
        10: SSH_PROBLEM
        20: COMMAND_PROBLEM
        30: DEAD_ROACH_PROBLEM
    
    - Refined error handling for remote ssh connections only for commands that use
      SyncedCluster.Run().
    
    Release justification: This dosn't affect CRDB itself, only internal tools.
    
    Release note: None

---
+++ pkg/cmd/roachprod/errors/errors.go
@@ -0,0 +1,233 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package errors
+
+import (
+	"fmt"
+	"os/exec"
+
+	crdberrors "github.com/cockroachdb/errors"
+)
+
+// Error is an interface for error types used by the main.wrap() function
+// to output correctly classified log messages and exit codes.
+type Error interface {
+	error
+
+	// The exit code for the error when exiting roachprod.
+	ExitCode() int
+}
+
+// Exit codes for the errors
+const (
+	cmdExitCode          = 20
+	cockroachExitCode    = 30
+	sshExitCode          = 10
+	unclassifiedExitCode = 1
+)
+
+// Cmd wraps errors that result from a non-cockroach command run against
+// the cluster.
+//
+// For errors coming from a cockroach command, use Cockroach.
+type Cmd struct {
+	Err error
+}
+
+func (e Cmd) Error() string {
+	return fmt.Sprintf("COMMAND_PROBLEM: %s", e.Err.Error())
+}
+
+// ExitCode gives the process exit code to return for non-cockroach command
+// errors.
+func (e Cmd) ExitCode() int {
+	return cmdExitCode
+}
+
+// Format passes formatting responsibilities to cockroachdb/errors
+func (e Cmd) Format(s fmt.State, verb rune) {
+	crdberrors.FormatError(e, s, verb)
+}
+
+// Unwrap the wrapped the non-cockroach command error.
+func (e Cmd) Unwrap() error {
+	return e.Err
+}
+
+// Cockroach wraps errors that result from a cockroach command run against the cluster.
+//
+// For non-cockroach commands, use Cmd.
+type Cockroach struct {
+	Err error
+}
+
+func (e Cockroach) Error() string {
+	return fmt.Sprintf("DEAD_ROACH_PROBLEM: %s", e.Err.Error())
+}
+
+// ExitCode gives the process exit code to return for cockroach errors.
+func (e Cockroach) ExitCode() int {
+	return cockroachExitCode
+}
+
+// Format passes formatting responsibilities to cockroachdb/errors
+func (e Cockroach) Format(s fmt.State, verb rune) {
+	crdberrors.FormatError(e, s, verb)
+}
+
+// Unwrap the wrapped cockroach error.
+func (e Cockroach) Unwrap() error {
+	return e.Err
+}
+
+// SSH wraps ssh-specific errors from connections to remote hosts.
+type SSH struct {
+	Err error
+}
+
+func (e SSH) Error() string {
+	return fmt.Sprintf("SSH_PROBLEM: %s", e.Err.Error())
+}
+
+// ExitCode gives the process exit code to return for SSH errors.
+func (e SSH) ExitCode() int {
+	return sshExitCode
+}
+
+// Format passes formatting responsibilities to cockroachdb/errors
+func (e SSH) Format(s fmt.State, verb rune) {
+	crdberrors.FormatError(e, s, verb)
+}
+
+// Unwrap the wrapped SSH error.
+func (e SSH) Unwrap() error {
+	return e.Err
+}
+
+// Unclassified wraps roachprod and unclassified errors.
+type Unclassified struct {
+	Err error
+}
+
+func (e Unclassified) Error() string {
+	return fmt.Sprintf("UNCLASSIFIED_PROBLEM: %s", e.Err.Error())
+}
+
+// ExitCode gives the process exit code to return for unclassified errors.
+func (e Unclassified) ExitCode() int {
+	return unclassifiedExitCode
+}
+
+// Format passes formatting responsibilities to cockroachdb/errors
+func (e Unclassified) Format(s fmt.State, verb rune) {
+	crdberrors.FormatError(e, s, verb)
+}
+
+// Unwrap the wrapped unclassified error.
+func (e Unclassified) Unwrap() error {
+	return e.Err
+}
+
+// ClassifyCmdError classifies an error received while executing a
+// non-cockroach command remotely over an ssh connection to the right Error
+// type.
+func ClassifyCmdError(err error) Error {
+	if err == nil {
+		return nil
+	}
+
+	if exitErr, ok := asExitError(err); ok {
+		if exitErr.ExitCode() == 255 {
+			return SSH{err}
+		}
+		return Cmd{err}
+	}
+
+	return Unclassified{err}
+}
+
+// ClassifyCockroachError classifies an error received while executing a
+// cockroach command remotely over an ssh connection to the right Error type.
+func ClassifyCockroachError(err error) Error {
+	if err == nil {
+		return nil
+	}
+
+	if exitErr, ok := asExitError(err); ok {
+		if exitErr.ExitCode() == 255 {
+			return SSH{err}
+		}
+		return Cockroach{err}
+	}
+
+	return Unclassified{err}
+}
+
+// Extract the an ExitError from err's error tree or (nil, false) if none exists.
+func asExitError(err error) (*exec.ExitError, bool) {
+	if exitErr, ok := crdberrors.If(err, func(err error) (interface{}, bool) {
+		if err, ok := err.(*exec.ExitError); ok {
+			return err, true
+		}
+		return nil, false
+	}); ok {
+		return exitErr.(*exec.ExitError), true
+	}
+	return nil, false
+}
+
+// AsError extracts the Error from err's error tree or (nil, false) if none exists.
+func AsError(err error) (Error, bool) {
+	if rpErr, ok := crdberrors.If(err, func(err error) (interface{}, bool) {
+		if rpErr, ok := err.(Error); ok {
+			return rpErr, true
+		}
+		return nil, false
+	}); ok {
+		return rpErr.(Error), true
+	}
+	return nil, false
+}
+
+// SelectPriorityError selects an error from the list in this priority order:
+//
+// - the Error with the highest exit code
+// - one of the `error`s
+// - nil
+func SelectPriorityError(errors []error) error {
+	var result Error
+	for _, err := range errors {
+		if err == nil {
+			continue
+		}
+
+		rpErr, _ := AsError(err)
+		if result == nil {
+			result = rpErr
+			continue
+		}
+
+		if rpErr.ExitCode() > result.ExitCode() {
+			result = rpErr
+		}
+	}
+
+	if result != nil {
+		return result
+	}
+
+	for _, err := range errors {
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
