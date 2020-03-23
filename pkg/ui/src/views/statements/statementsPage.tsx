a443ff0c2c977b497bc95cb27a688cc313374aeb
Vlad Los <carrott9@gmail.com>
2020-03-23 14:31:46 +0200

    ui: Sortable sort per page
    moved pagination logic to `Sortedtable` component for fixed an issue with sort settings when sorting is worked out for one page
    
    Resolves: #46173
    
    Release justification: bug fix
    
    Release note (ui): none

--- pkg/ui/src/views/statements/statementsPage.tsx
+++ pkg/ui/src/views/statements/statementsPage.tsx
@@ -163,16 +163,6 @@ export class StatementsPage extends React.Component<StatementsPageProps & RouteC
     this.setState({ pagination: { ...pagination, current } });
     this.trackPaginate(current);
   }
-
-  getStatementsData = () => {
-    const { pagination: { current, pageSize } } = this.state;
-    const currentDefault = current - 1;
-    const start = (currentDefault * pageSize);
-    const end = (currentDefault * pageSize + pageSize);
-    const data = this.filteredStatementsData().slice(start, end);
-    return data;
-  }
-
   onSubmitSearchField = (search: string) => {
     this.setState({ pagination: { ...this.state.pagination, current: 1 }, search });
     this.syncHistory({
@@ -269,7 +259,7 @@ export class StatementsPage extends React.Component<StatementsPageProps & RouteC
     const selectedApp = appAttrValue || "";
     const appOptions = [{ value: "", label: "All" }];
     this.props.apps.forEach(app => appOptions.push({ value: app, label: app }));
-    const data = this.getStatementsData();
+    const data = this.filteredStatementsData();
     return (
       <div>
         <PageConfig>
@@ -321,6 +311,7 @@ export class StatementsPage extends React.Component<StatementsPageProps & RouteC
                 sortSetting={this.state.sortSetting}
                 onChangeSortSetting={this.changeSortSetting}
                 renderNoResult={this.noStatementResult()}
+                pagination={pagination}
               />
             </div>
           )}
@@ -330,7 +321,7 @@ export class StatementsPage extends React.Component<StatementsPageProps & RouteC
           itemRender={this.renderPage as (page: number, type: "page" | "prev" | "next" | "jump-prev" | "jump-next") => React.ReactNode}
           pageSize={pagination.pageSize}
           current={pagination.current}
-          total={this.filteredStatementsData().length}
+          total={data.length}
           onChange={this.onChangePage}
           hideOnSinglePage
         />
