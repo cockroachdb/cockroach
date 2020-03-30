16641810bd4597c9c68bda58a1ee0ffd3d9b4b98
Radu Berinde <radu@cockroachlabs.com>
2020-03-30 11:10:57 -0700

    opt: improved telemetry for joins
    Currently, we emit telemetry for each join operator in the final plan (e.g.
    `sql.plan.node.merge-join`. This data is hard to work with because it doesn't
    cleanly separate the algorithm from the join type. In particular, merge/lookup
    joins emit the same telemetry regardless of join type.
    
    This change removes the existing counters and replaces them with two sets of
    counters, one for the join type and one for the join algorithm (which now
    includes hash and cross).
    
    Fixes #46188.
    
    Release note: None

--- pkg/sql/opt/telemetry.go
+++ pkg/sql/opt/telemetry.go
@@ -13,6 +13,7 @@ package opt
 import (
 	"github.com/cockroachdb/cockroach/pkg/server/telemetry"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqltelemetry"
+	"github.com/cockroachdb/errors"
 )
 
 // OpTelemetryCounters stores telemetry counters for operators marked with the
@@ -22,5 +23,24 @@ var OpTelemetryCounters [NumOperators]telemetry.Counter
 func init() {
 	for _, op := range TelemetryOperators {
 		OpTelemetryCounters[op] = sqltelemetry.OptNodeCounter(op.String())
+	}
+}
+
+// JoinTypeToUseCounter returns the JoinTypeXyzUseCounter for the given join
+// operator.
+func JoinTypeToUseCounter(op Operator) telemetry.Counter {
+	switch op {
+	case InnerJoinOp:
+		return sqltelemetry.JoinTypeInnerUseCounter
+	case LeftJoinOp, RightJoinOp:
+		return sqltelemetry.JoinTypeLeftUseCounter
+	case FullJoinOp:
+		return sqltelemetry.JoinTypeFullUseCounter
+	case SemiJoinOp:
+		return sqltelemetry.JoinTypeSemiUseCounter
+	case AntiJoinOp:
+		return sqltelemetry.JoinTypeAntiUseCounter
+	default:
+		panic(errors.AssertionFailedf("unhandled join op %s", op))
 	}
 }
