864322d07719d9a640ea3893fda6814e56c5e229
David Hartunian <davidh@cockroachlabs.com>
2020-03-31 15:11:59 -0400

    ui: improve wording in storage tooltips
    Some existing tooltips discussion storage metrics
    needed adjustment to improve accuracy.
    
    Changes affect:
    
    * Overview page: Capacity graph
    * Replication page: Logical Bytes per Store
    * Storage page: Capacity and Live Bytes
    
    * Database summary box: "Database Size"
    
    Some tooltips now include links to documentation
    where database operators can get much more detail
    on how these metrics are calculated and how to
    use them.
    
    Release justification: low risk documentation edit
    
    Release note (admin ui change): improved tooltips
    for existing capacity and storage metrics.

---
+++ pkg/ui/src/views/cluster/containers/nodeGraphs/dashboards/graphTooltips.tsx
@@ -0,0 +1,53 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import React from "react";
+
+import * as docsURL from "src/util/docs";
+import {Anchor} from "src/components";
+import {ReactNode} from "react";
+
+export const CapacityGraphTooltip: React.FunctionComponent<{tooltipSelection: ReactNode}> =
+  ({tooltipSelection}) => (<div>
+    <dl>
+      <dt>Capacity</dt>
+      <dd>
+        <p>
+          Usage of disk space {tooltipSelection}.
+        </p>
+        <p>
+          <Anchor href={docsURL.howAreCapacityMetricsCalculated}>
+            How are these metrics calculated?
+          </Anchor>
+        </p>
+      </dd>
+    </dl>
+  </div>);
+
+export const LogicalBytesGraphTooltip: React.FunctionComponent =
+  () => (<div>
+    <dl>
+      <dt>Logical Bytes per Store</dt>
+      <dd>
+        <p>
+          Number of logical bytes stored in
+          {" "}
+          <Anchor href={docsURL.keyValuePairs}>
+            key-value pairs
+          </Anchor>
+          {" "}
+          on each node.
+        </p>
+        <p>
+          This includes historical and deleted data.
+        </p>
+      </dd>
+    </dl>
+  </div>);
