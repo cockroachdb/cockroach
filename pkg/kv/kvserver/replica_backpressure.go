8f5b0d150322addc8486b407492b0641ed0f67e4
Andrew Werner <ajwerner@cockroachlabs.com>
2020-04-01 22:43:34 -0400

    kvserver: prioritize splitting ranges which are currently backpressuring
    This commit is a further mitigation to the decrease of range size. It is
    important to split range which are too big. It is especially important to
    split ranges which would block writes.
    
    Release note: None

--- pkg/kv/kvserver/replica_backpressure.go
+++ pkg/kv/kvserver/replica_backpressure.go
@@ -50,15 +50,13 @@ var backpressureRangeSizeMultiplier = settings.RegisterValidatedFloatSetting(
 // is reduced by roughly exactly the multiplier then we'd potentially have
 // lots of ranges in this state.
 //
-// We additionally mitigate this situation further by:
+// We additionally mitigate this situation further by doing the following:
 //
 //  1) We store in-memory on each replica the largest zone configuration range
 //     size (largestPreviousMaxRangeBytes) we've seen and we do not backpressure
 //     if the current range size is less than that. That value is cleared when
 //     a range splits or runs GC such that the range size becomes smaller than
 //     the current max range size.
-//
-// TODO(ajwerner): We could mitigate this even further by:
 //
 //  2) We assign a higher priority in the snapshot queue to ranges which are
 //     currently backpressuring than ranges which are larger but are not
