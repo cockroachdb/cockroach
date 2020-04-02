bb4c0e57c807f007d3060010eb0799f9dd48c14f
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2020-04-02 14:35:25 -0400

    ui: Added track call for collapsing node on network diagnostics
    Added a track call when users toggle the checkbox to collapse nodes on
    the network diagnostics view (network latency matrix)
    
    Release note: None

--- pkg/ui/src/util/analytics/index.ts
+++ pkg/ui/src/util/analytics/index.ts
@@ -18,3 +18,4 @@ export { default as trackDownloadDiagnosticsBundle } from "./trackDownloadDiagno
 export { default as trackDocsLink} from "./trackDocsLink";
 export { default as trackFilter } from "./trackFilter";
 export { default as trackNetworkSort } from "./trackNetworkSort";
+export { default as trackCollapseNodes } from "./trackCollapseNodes";
