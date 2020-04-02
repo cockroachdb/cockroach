55562c28e2f0989b823b1b1dfd7ff4c2d46b4eba
Oliver Tan <otan@cockroachlabs.com>
2020-04-02 11:03:24 -0700

    geo: initial framework to parse WKT from GEOS
    This PR introduces the ability to parse WKT using the GEOS library. It
    is the only library I can tell that can decode WKT, so everything will
    have to go through that (for now). We're planning to store everything in
    EWKB, and everything can be parsed off WKB which is why I've made
    that the base type for now.
    
    In future, the geos package will take in WKBs in a new package `geomfn`,
    and transform into GEOSGeometry for operations without ever returning
    *GEOSGeometry back to the user. This is necessary as we have no control
    on the lifetime of a Geometry object.
    
    Release note: None

---
+++ pkg/geo/geopb/types.go
@@ -0,0 +1,20 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package geopb
+
+// WKT is the Well Known Text form of a spatial object.
+type WKT string
+
+// WKB is the Well Known Bytes form of a spatial object.
+type WKB []byte
+
+// EWKB is the Extended Well Known Bytes form of a spatial object.
+type EWKB []byte
