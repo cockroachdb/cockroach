1872ff85fcff454919bc3cb0df51761306909f18
Peter Mattis <petermattis@gmail.com>
2020-04-03 15:26:32 -0400

    geo/geos: clang-format the C++ code
    Copy the clang-format config from `c-deps/libroach`.
    
    Release note: None

--- pkg/geo/geos/geos_unix.cc
+++ pkg/geo/geos/geos_unix.cc
@@ -18,9 +18,9 @@
 namespace {
 
 // Data Types adapted from `capi/geos_c.h.in` in GEOS.
-typedef void *CR_GEOS_Handle;
-typedef void *CR_GEOS_WKTReader;
-typedef void *CR_GEOS_WKBWriter;
+typedef void* CR_GEOS_Handle;
+typedef void* CR_GEOS_WKTReader;
+typedef void* CR_GEOS_WKBWriter;
 
 // Function declarations from `capi/geos_c.h.in` in GEOS.
 typedef CR_GEOS_Handle (*CR_GEOS_init_r)();
@@ -29,28 +29,24 @@ typedef void (*CR_GEOS_finish_r)(CR_GEOS_Handle);
 typedef void (*CR_GEOS_Geom_destroy_r)(CR_GEOS_Handle, CR_GEOS_Geometry);
 
 typedef CR_GEOS_WKTReader (*CR_GEOS_WKTReader_create_r)(CR_GEOS_Handle);
-typedef CR_GEOS_Geometry (*CR_GEOS_WKTReader_read_r)(CR_GEOS_Handle,
-                                                     CR_GEOS_WKTReader,
-                                                     const char *);
+typedef CR_GEOS_Geometry (*CR_GEOS_WKTReader_read_r)(CR_GEOS_Handle, CR_GEOS_WKTReader,
+                                                     const char*);
 typedef void (*CR_GEOS_WKTReader_destroy_r)(CR_GEOS_Handle, CR_GEOS_WKTReader);
 
 typedef CR_GEOS_WKBWriter (*CR_GEOS_WKBWriter_create_r)(CR_GEOS_Handle);
-typedef char *(*CR_GEOS_WKBWriter_write_r)(CR_GEOS_Handle, CR_GEOS_WKBWriter,
-                                           CR_GEOS_Geometry, size_t *);
-typedef void (*CR_GEOS_WKBWriter_setByteOrder_r)(CR_GEOS_Handle,
-                                                 CR_GEOS_WKBWriter, int);
+typedef char* (*CR_GEOS_WKBWriter_write_r)(CR_GEOS_Handle, CR_GEOS_WKBWriter, CR_GEOS_Geometry,
+                                           size_t*);
+typedef void (*CR_GEOS_WKBWriter_setByteOrder_r)(CR_GEOS_Handle, CR_GEOS_WKBWriter, int);
 typedef void (*CR_GEOS_WKBWriter_destroy_r)(CR_GEOS_Handle, CR_GEOS_WKBWriter);
 
-std::string ToString(CR_GEOS_String goStr) {
-  return std::string(goStr.data, goStr.len);
-}
+std::string ToString(CR_GEOS_String goStr) { return std::string(goStr.data, goStr.len); }
 
-const char *dlopenFailError = "failed to execute dlopen";
+const char* dlopenFailError = "failed to execute dlopen";
 
-} // namespace
+}  // namespace
 
 struct CR_GEOS {
-  void *dlHandle;
+  void* dlHandle;
 
   CR_GEOS_init_r GEOS_init_r;
   CR_GEOS_finish_r GEOS_finish_r;
@@ -66,9 +62,7 @@ struct CR_GEOS {
   CR_GEOS_WKBWriter_setByteOrder_r GEOSWKBWriter_setByteOrder_r;
   CR_GEOS_WKBWriter_write_r GEOSWKBWriter_write_r;
 
-  CR_GEOS(void *h)
-      : dlHandle(h) {
-  }
+  CR_GEOS(void* h) : dlHandle(h) {}
 
   ~CR_GEOS() {
     if (dlHandle != NULL) {
@@ -77,13 +71,13 @@ struct CR_GEOS {
   }
 
   char* Init() {
-#define INIT(x)                     \
-    do {                            \
-      auto error = InitSym(&x, #x); \
-      if (error != nullptr) {       \
-        return error;               \
-      }                             \
-    } while (0)
+#define INIT(x)                                                                                    \
+  do {                                                                                             \
+    auto error = InitSym(&x, #x);                                                                  \
+    if (error != nullptr) {                                                                        \
+      return error;                                                                                \
+    }                                                                                              \
+  } while (0)
 
     INIT(GEOS_init_r);
     INIT(GEOS_finish_r);
@@ -100,18 +94,17 @@ struct CR_GEOS {
 #undef INIT
   }
 
-  template <typename T>
-  char *InitSym(T *ptr, const char *symbol) {
+  template <typename T> char* InitSym(T* ptr, const char* symbol) {
     *ptr = reinterpret_cast<T>(dlsym(dlHandle, symbol));
     return dlerror();
   }
 };
 
-char *CR_GEOS_Init(CR_GEOS_String loc, CR_GEOS **lib) {
+char* CR_GEOS_Init(CR_GEOS_String loc, CR_GEOS** lib) {
   auto locStr = ToString(loc);
-  void *dlHandle = dlopen(locStr.c_str(), RTLD_LAZY);
+  void* dlHandle = dlopen(locStr.c_str(), RTLD_LAZY);
   if (!dlHandle) {
-    return (char *)dlopenFailError;
+    return (char*)dlopenFailError;
   }
 
   std::unique_ptr<CR_GEOS> ret(new CR_GEOS(dlHandle));
@@ -124,7 +117,7 @@ char *CR_GEOS_Init(CR_GEOS_String loc, CR_GEOS **lib) {
   return NULL;
 }
 
-CR_GEOS_String CR_GEOS_WKTToWKB(CR_GEOS *lib, CR_GEOS_String wktString) {
+CR_GEOS_String CR_GEOS_WKTToWKB(CR_GEOS* lib, CR_GEOS_String wktString) {
   CR_GEOS_String result = {.data = NULL, .len = 0};
 
   auto handle = lib->GEOS_init_r();
@@ -136,8 +129,7 @@ CR_GEOS_String CR_GEOS_WKTToWKB(CR_GEOS *lib, CR_GEOS_String wktString) {
   if (geom != NULL) {
     auto wkbWriter = lib->GEOSWKBWriter_create_r(handle);
     lib->GEOSWKBWriter_setByteOrder_r(handle, wkbWriter, 1);
-    result.data =
-        lib->GEOSWKBWriter_write_r(handle, wkbWriter, geom, &result.len);
+    result.data = lib->GEOSWKBWriter_write_r(handle, wkbWriter, geom, &result.len);
     lib->GEOSWKBWriter_destroy_r(handle, wkbWriter);
     lib->GEOSGeom_destroy_r(handle, geom);
   }
