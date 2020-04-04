886e0b624302ec7e2b2e9a81155f4cba17148e2e
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-04-04 10:52:55 -0700

    rowexec: fix error propagation of countRows processor
    Previously, we would incorrectly propagate an error if such occurs
    during rendering the single output row of countRows processor - we would
    append to trailing meta and then would return `nil, nil` (for `row,
    meta`) which would indicate to the consumer that countRows is fully done
    (i.e. it has been drained) before it is actually done. This would result
    in not propagating the error and is now fixed.
    
    Release note (bug fix): Previously, instead of returning a parsing error
    in queries with `count(*)` CockroachDB could incorrectly return no
    output (when the query was executed via row-by-row engine), and this is
    now fixed.

--- pkg/sql/rowexec/countrows.go
+++ pkg/sql/rowexec/countrows.go
@@ -90,8 +90,13 @@ func (ag *countAggregator) Next() (sqlbase.EncDatumRow, *execinfrapb.ProducerMet
 			ret := make(sqlbase.EncDatumRow, 1)
 			ret[0] = sqlbase.EncDatum{Datum: tree.NewDInt(tree.DInt(ag.count))}
 			rendered, _, err := ag.Out.ProcessRow(ag.Ctx, ret)
-			// We're done as soon as we process our one output row.
-			ag.MoveToDraining(err)
+			// We're done as soon as we process our one output row, so we
+			// transition into draining state. We will, however, return non-nil
+			// error (if such occurs during rendering) separately below.
+			ag.MoveToDraining(nil /* err */)
+			if err != nil {
+				return nil, &execinfrapb.ProducerMetadata{Err: err}
+			}
 			return rendered, nil
 		}
 		ag.count++
