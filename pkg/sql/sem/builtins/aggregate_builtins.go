a1108e983b00ce04deb1504f44efc9ab539f93f9
Drew Kimball <andrewekimball@gmail.com>
2020-04-03 20:17:33 -0700

    sql: add bit support for bit_or aggregate function
    Previously, the bit_or aggregate function only worked with INT
    types. This commit adds support for BIT and VARBIT types.
    
    Fixes #45841
    
    Release note (sql change): bit_or aggregate function now supports
    BIT and VARBIT data types.

--- pkg/sql/sem/builtins/aggregate_builtins.go
+++ pkg/sql/sem/builtins/aggregate_builtins.go
@@ -139,7 +139,9 @@ var aggregates = map[string]builtinDefinition{
 	),
 
 	"bit_or": makeBuiltin(aggProps(),
-		makeAggOverload([]*types.T{types.Int}, types.Int, newBitOrAggregate,
+		makeAggOverload([]*types.T{types.Int}, types.Int, newIntBitOrAggregate,
+			"Calculates the bitwise OR of all non-null input values, or null if none."),
+		makeAggOverload([]*types.T{types.VarBit}, types.VarBit, newBitBitOrAggregate,
 			"Calculates the bitwise OR of all non-null input values, or null if none."),
 	),
 
@@ -448,7 +450,8 @@ var _ tree.AggregateFunc = &intXorAggregate{}
 var _ tree.AggregateFunc = &jsonAggregate{}
 var _ tree.AggregateFunc = &intBitAndAggregate{}
 var _ tree.AggregateFunc = &bitBitAndAggregate{}
-var _ tree.AggregateFunc = &bitOrAggregate{}
+var _ tree.AggregateFunc = &intBitOrAggregate{}
+var _ tree.AggregateFunc = &bitBitOrAggregate{}
 
 const sizeOfArrayAggregate = int64(unsafe.Sizeof(arrayAggregate{}))
 const sizeOfAvgAggregate = int64(unsafe.Sizeof(avgAggregate{}))
@@ -480,7 +483,8 @@ const sizeOfIntXorAggregate = int64(unsafe.Sizeof(intXorAggregate{}))
 const sizeOfJSONAggregate = int64(unsafe.Sizeof(jsonAggregate{}))
 const sizeOfIntBitAndAggregate = int64(unsafe.Sizeof(intBitAndAggregate{}))
 const sizeOfBitBitAndAggregate = int64(unsafe.Sizeof(bitBitAndAggregate{}))
-const sizeOfBitOrAggregate = int64(unsafe.Sizeof(bitOrAggregate{}))
+const sizeOfIntBitOrAggregate = int64(unsafe.Sizeof(intBitOrAggregate{}))
+const sizeOfBitBitOrAggregate = int64(unsafe.Sizeof(bitBitOrAggregate{}))
 
 // singleDatumAggregateBase is a utility struct that helps aggregate builtins
 // that store a single datum internally track their memory usage related to
@@ -974,17 +978,19 @@ func (a *bitBitAndAggregate) Size() int64 {
 	return sizeOfBitBitAndAggregate
 }
 
-type bitOrAggregate struct {
+type intBitOrAggregate struct {
 	sawNonNull bool
 	result     int64
 }
 
-func newBitOrAggregate(_ []*types.T, _ *tree.EvalContext, _ tree.Datums) tree.AggregateFunc {
-	return &bitOrAggregate{}
+func newIntBitOrAggregate(_ []*types.T, _ *tree.EvalContext, _ tree.Datums) tree.AggregateFunc {
+	return &intBitOrAggregate{}
 }
 
 // Add inserts one value into the running bitwise OR.
-func (a *bitOrAggregate) Add(_ context.Context, datum tree.Datum, otherArgs ...tree.Datum) error {
+func (a *intBitOrAggregate) Add(
+	_ context.Context, datum tree.Datum, otherArgs ...tree.Datum,
+) error {
 	if datum == tree.DNull {
 		return nil
 	}
@@ -1002,7 +1008,7 @@ func (a *bitOrAggregate) Add(_ context.Context, datum tree.Datum, otherArgs ...t
 }
 
 // Result returns the bitwise OR.
-func (a *bitOrAggregate) Result() (tree.Datum, error) {
+func (a *intBitOrAggregate) Result() (tree.Datum, error) {
 	if !a.sawNonNull {
 		return tree.DNull, nil
 	}
@@ -1010,17 +1016,74 @@ func (a *bitOrAggregate) Result() (tree.Datum, error) {
 }
 
 // Reset implements tree.AggregateFunc interface.
-func (a *bitOrAggregate) Reset(context.Context) {
+func (a *intBitOrAggregate) Reset(context.Context) {
 	a.sawNonNull = false
 	a.result = 0
 }
 
 // Close is part of the tree.AggregateFunc interface.
-func (a *bitOrAggregate) Close(context.Context) {}
+func (a *intBitOrAggregate) Close(context.Context) {}
 
 // Size is part of the tree.AggregateFunc interface.
-func (a *bitOrAggregate) Size() int64 {
-	return sizeOfBitOrAggregate
+func (a *intBitOrAggregate) Size() int64 {
+	return sizeOfIntBitOrAggregate
+}
+
+type bitBitOrAggregate struct {
+	sawNonNull bool
+	result     bitarray.BitArray
+}
+
+func newBitBitOrAggregate(_ []*types.T, _ *tree.EvalContext, _ tree.Datums) tree.AggregateFunc {
+	return &bitBitOrAggregate{}
+}
+
+// Add inserts one value into the running bitwise OR.
+func (a *bitBitOrAggregate) Add(
+	_ context.Context, datum tree.Datum, otherArgs ...tree.Datum,
+) error {
+	if datum == tree.DNull {
+		return nil
+	}
+	bits := &tree.MustBeDBitArray(datum).BitArray
+	if !a.sawNonNull {
+		// This is the first non-null datum, so we simply store
+		// the provided value for the aggregation.
+		a.result = *bits
+		a.sawNonNull = true
+		return nil
+	}
+	// If the length of the current bit array is different from that of the
+	// stored value, we return an error.
+	if a.result.BitLen() != bits.BitLen() {
+		return tree.NewCannotMixBitArraySizesError("OR")
+	}
+	// This is not the first non-null datum, so we actually OR it with the
+	// aggregate so far.
+	a.result = bitarray.Or(a.result, *bits)
+	return nil
+}
+
+// Result returns the bitwise OR.
+func (a *bitBitOrAggregate) Result() (tree.Datum, error) {
+	if !a.sawNonNull {
+		return tree.DNull, nil
+	}
+	return &tree.DBitArray{BitArray: a.result}, nil
+}
+
+// Reset implements tree.AggregateFunc interface.
+func (a *bitBitOrAggregate) Reset(context.Context) {
+	a.sawNonNull = false
+	a.result = bitarray.BitArray{}
+}
+
+// Close is part of the tree.AggregateFunc interface.
+func (a *bitBitOrAggregate) Close(context.Context) {}
+
+// Size is part of the tree.AggregateFunc interface.
+func (a *bitBitOrAggregate) Size() int64 {
+	return sizeOfBitBitOrAggregate
 }
 
 type boolAndAggregate struct {
