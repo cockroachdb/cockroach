b6d384673b06442045b63d4e472308fdf7b2ad59
Andrii Vorobiov <and.vorobiov@gmail.com>
2020-04-08 18:36:16 +0300

    ui: Add storybook script to package.json
    Storybook is aimed to be run locally during
    development so there is no needs to define it
    in Makefile, instead of that it can be run as
    NPM script.
    
    Release note: None

--- pkg/ui/src/components/tooltip/tooltip.tsx
+++ pkg/ui/src/components/tooltip/tooltip.tsx
@@ -12,6 +12,7 @@ import * as React from "react";
 import { default as AntTooltip, TooltipProps as AntTooltipProps } from "antd/es/tooltip";
 import cn from "classnames";
 
+import "antd/es/tooltip/style/css";
 import "./tooltip.styl";
 
 export interface TooltipProps {
