c9759fc1c02cc54deea292c23c33476c6cac64b9
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2020-04-08 22:50:44 -0400

    kv: don't mix prefix and non-prefix iters when collecting intents
    Fixes #47219.
    
    This commit addresses the bug diagnosed and explained in #47219. In that
    issue, we saw an assertion failure all the way up in the concurrency
    manager because a READ_UNCOMMITTED scan was hitting a WriteIntentError,
    which should not be possible. The root cause of this issue was that
    READ_UNCOMMITTED scans were mixing prefix and non-prefix iterators
    pulled from a read-only engine between the time that they were
    collecting intent keys and they were returning to fetch the provisional
    values for those keys. This mixing of iterators did not guarantee that
    the two stages of the operation would observe a consistent snapshot of
    the underlying engine, and because the READ_UNCOMMITTED scans also did
    not acquire latches, writes were able to slip in and change the intent
    while the scan wasn't looking. This caused the scan to throw a
    WriteIntentError for the new intent transaction, which badly confused
    other parts of the system (rightfully so).
    
    This commit fixes this issue in a few different ways:
    1. it ensures that we always use the same iterator type (prefix or non-prefix)
       when retrieving the provisional values for a collection of intents retrieved
       by an earlier scan during READ_UNCOMMITTED operations.
    2. it adds an assertion inside of batcheval.CollectIntentRows that the
       function never returns a WriteIntentError. This would have caught the bug
       much more easily, especially back before we had the concurrency manager
       assertion and this bug could have materialized as stuck range lookups and
       potentially even deadlocked splits due to the dependency cycle between
       those two operations.
    3. it documents the limited guarantees that read-only engines provide with
       respect to consistent engine snapshots across iterator instances.
    
    We'll want to backport this fix as far back as possible. It won't crash
    earlier releases of Cockroach, but as stated above, it might cause even
    more disastrous results. REMINDER: when backporting, remember to change
    the release note.
    
    Release notes (bug fix): a bug that could cause Cockroach processes to
    crash due to an assertion failure with the text "expected latches held,
    found none" has been fixed.
    
    Release justification: fixes a high-priority bug in existing
    functionality. The bug became louder (now crashes servers) due to recent
    changes that added new assertions into the code.

--- pkg/kv/kvserver/batcheval/intent.go
+++ pkg/kv/kvserver/batcheval/intent.go
@@ -17,39 +17,91 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/concurrency/lock"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/storage"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 )
 
-// CollectIntentRows collects the key-value pairs for each intent provided. It
-// also verifies that the ReturnIntents option is allowed.
+// CollectIntentRows collects the provisional key-value pairs for each intent
+// provided.
 //
-// TODO(nvanbenschoten): mvccGetInternal should return the intent values directly
-// when ReturnIntents is true. Since this will initially only be used for
-// RangeLookups and since this is how they currently collect intent values, this
-// is ok for now.
+// The method accepts a reader and flag indicating whether a prefix iterator
+// should be used when creating an iterator from the reader. This flexibility
+// works around a limitation of the Engine.NewReadOnly interface where prefix
+// iterators and non-prefix iterators pulled from the same read-only engine are
+// not guaranteed to provide a consistent snapshot of the underlying engine.
+// This function expects to be able to retrieve the corresponding provisional
+// value for each of the provided intents. As such, it is critical that it
+// observes the engine in the same state that it was in when the intent keys
+// were originally collected. Because of this, callers are tasked with
+// indicating whether the intents were originally collected using a prefix
+// iterator or not.
+//
+// TODO(nvanbenschoten): remove the usePrefixIter complexity when we're fully on
+// Pebble and can guarantee that all iterators created from a read-only engine
+// are consistent.
+//
+// TODO(nvanbenschoten): mvccGetInternal should return the intent values
+// directly when reading at the READ_UNCOMMITTED consistency level. Since this
+// is only currently used for range lookups and when watching for a merge (both
+// of which are off the hot path), this is ok for now.
 func CollectIntentRows(
-	ctx context.Context, reader storage.Reader, cArgs CommandArgs, intents []roachpb.Intent,
+	ctx context.Context, reader storage.Reader, usePrefixIter bool, intents []roachpb.Intent,
 ) ([]roachpb.KeyValue, error) {
 	if len(intents) == 0 {
 		return nil, nil
 	}
 	res := make([]roachpb.KeyValue, 0, len(intents))
-	for _, intent := range intents {
+	for i := range intents {
+		kv, err := readProvisionalVal(ctx, reader, usePrefixIter, &intents[i])
+		if err != nil {
+			switch t := err.(type) {
+			case *roachpb.WriteIntentError:
+				log.Fatalf(ctx, "unexpected %T in CollectIntentRows: %+v", t, t)
+			case *roachpb.ReadWithinUncertaintyIntervalError:
+				log.Fatalf(ctx, "unexpected %T in CollectIntentRows: %+v", t, t)
+			}
+			return nil, err
+		}
+		if kv.Value.IsPresent() {
+			res = append(res, kv)
+		}
+	}
+	return res, nil
+}
+
+// readProvisionalVal retrieves the provisional value for the provided intent
+// using the reader and the specified access method (i.e. with or without the
+// use of a prefix iterator). The function returns an empty KeyValue if the
+// intent is found to contain a deletion tombstone as its provisional value.
+func readProvisionalVal(
+	ctx context.Context, reader storage.Reader, usePrefixIter bool, intent *roachpb.Intent,
+) (roachpb.KeyValue, error) {
+	if usePrefixIter {
 		val, _, err := storage.MVCCGetAsTxn(
 			ctx, reader, intent.Key, intent.Txn.WriteTimestamp, intent.Txn,
 		)
 		if err != nil {
-			return nil, err
+			return roachpb.KeyValue{}, err
 		}
 		if val == nil {
 			// Intent is a deletion.
-			continue
+			return roachpb.KeyValue{}, nil
 		}
-		res = append(res, roachpb.KeyValue{
-			Key:   intent.Key,
-			Value: *val,
-		})
+		return roachpb.KeyValue{Key: intent.Key, Value: *val}, nil
 	}
-	return res, nil
+	res, err := storage.MVCCScanAsTxn(
+		ctx, reader, intent.Key, intent.Key.Next(), intent.Txn.WriteTimestamp, intent.Txn,
+	)
+	if err != nil {
+		return roachpb.KeyValue{}, err
+	}
+	if len(res.KVs) > 1 {
+		log.Fatalf(ctx, "multiple key-values returned from single-key scan: %+v", res.KVs)
+	} else if len(res.KVs) == 0 {
+		// Intent is a deletion.
+		return roachpb.KeyValue{}, nil
+	}
+	return res.KVs[0], nil
+
 }
 
 // acquireUnreplicatedLocksOnKeys adds an unreplicated lock acquisition by the
