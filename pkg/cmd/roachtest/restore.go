5abcbe73b90f3d190ea203958f86c18c6d057c86
Raphael 'kena' Poss <knz@cockroachlabs.com>
2020-04-09 17:38:27 +0200

    deps: upgrade cockroachdb/errors
    This picks up all the incremental improvements without the upgrade
    from raven-go to sentry-go.
    
    It also fixes the implementation of
    `neutil.InitialHeartbeatFailedError` which was incorrect, and upgrades
    `roachpb.IsRangeNotFoundError` to use `errors.HasType` as originally
    intended by @ajwerner.
    
    Finally it also fixes the roachtest error string comparisons which
    were relying on non-documented behavior.
    
    Release note: None

--- pkg/cmd/roachtest/restore.go
+++ pkg/cmd/roachtest/restore.go
@@ -130,7 +130,7 @@ func (hc *HealthChecker) Runner(ctx context.Context) (err error) {
 
 		if elapsed := timeutil.Since(tBegin); elapsed > 10*time.Second {
 			err := errors.Errorf("health check against node %d took %s", nodeIdx, elapsed)
-			logger.Printf(err.Error() + "\n")
+			logger.Printf("%+v", err)
 			// TODO(tschottdorf): see method comment.
 			// return err
 		}
