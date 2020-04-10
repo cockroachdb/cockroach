450830114fd4176ce21a24123b12414e0d1d71cf
Anthony Huang <anthony.huang@affirm.com>
2020-04-10 12:15:26 -0400

    sql: support MATERIALIZED syntax in CTEs
    The MATERIALIZED/NOT MATERIALIZED syntax, introduced in PostgresSQL 12,
    allows the user to override the optimizer's default decision to
    not materialize/materialize a table, respectively.
    
    For example, the following query is non-recursive and side-effect-free
    so the optimizer will not materialize the table by default:
    ```
    WITH cte AS (SELECT 1) SELECT * FROM cte
    ```
    
    To force materialization, `MATERIALIZED` can be specified explcitly:
    ```
    WITH cte AS MATERIALIZED (SELECT 1) SELECT * FROM cte
    ```
    
    Similarly, the opposite can be done:
    ```
    WITH cte AS NOT MATERIALIZED (SELECT 1/0) SELECT * FROM cte
    ```
    
    Or even for multiple different CTEs:
    ```
    WITH cte (x) AS MATERIALIZED (INSERT INTO abc VALUES (1, 2)), cte2 (y)
      AS NOT MATERIALIZED (SELECT x + 1 FROM cte) SELECT * FROM cte, cte2
    ```
    
    In the case of recursive CTEs, we will still respect the user's decision
    to materialize or not materialize the CTEs.
    
    Release note (sql change): CTEs were extended to support
    the MATERIALIZED/NOT MATERIALIZED syntax added in
    PostgreSQL 12.

--- pkg/sql/sem/tree/with.go
+++ pkg/sql/sem/tree/with.go
@@ -19,7 +19,17 @@ type With struct {
 // CTE represents a common table expression inside of a WITH clause.
 type CTE struct {
 	Name AliasClause
+	Mtr  MaterializeClause
 	Stmt Statement
+}
+
+// MaterializeClause represents a materialize clause inside of a WITH clause.
+type MaterializeClause struct {
+	// Set controls whether to use the Materialize bool instead of the default.
+	Set bool
+
+	// Materialize overrides the default materialization behavior.
+	Materialize bool
 }
 
 // Format implements the NodeFormatter interface.
@@ -36,7 +46,14 @@ func (node *With) Format(ctx *FmtCtx) {
 			ctx.WriteString(", ")
 		}
 		ctx.FormatNode(&cte.Name)
-		ctx.WriteString(" AS (")
+		ctx.WriteString(" AS ")
+		if cte.Mtr.Set {
+			if !cte.Mtr.Materialize {
+				ctx.WriteString("NOT ")
+			}
+			ctx.WriteString("MATERIALIZED ")
+		}
+		ctx.WriteString("(")
 		ctx.FormatNode(cte.Stmt)
 		ctx.WriteString(")")
 	}
