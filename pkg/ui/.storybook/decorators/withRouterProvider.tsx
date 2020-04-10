e433ea7078310c5082609b88ea50a7203836dc6d
Andrii Vorobiov <and.vorobiov@gmail.com>
2020-04-10 17:47:00 +0300

    ui: Add Statements page storybook
    To test styles isolation for statements page
    we need storybook which displays entire
    Statements screen only.
    
    To make it work, RouterProvider decorator
    is added which connects router to dummy (empty)
    store.
    
    `statementsPage.fixture.ts` file contains snapshot
    of required props for StatementsPage component
    
    Release note: None

---
+++ pkg/ui/.storybook/decorators/withRouterProvider.tsx
@@ -0,0 +1,31 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import React from "react";
+import {Provider} from "react-redux";
+import {ConnectedRouter, connectRouter} from "connected-react-router";
+import {createMemoryHistory} from "history";
+import { createStore, combineReducers } from "redux";
+import {RenderFunction} from "storybook__react";
+
+const history = createMemoryHistory();
+const routerReducer = connectRouter(history);
+
+const store = createStore(combineReducers({
+  router: routerReducer,
+}));
+
+export const withRouterProvider = (storyFn: RenderFunction) => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      { storyFn() }
+    </ConnectedRouter>
+  </Provider>
+);
