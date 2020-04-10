d9b1bd10458dfe800746d6152c088a43bbd051d8
Jordan Lewis <jordanthelewis@gmail.com>
2020-04-10 16:19:35 -0400

    sql: improve UPDATE on inverted indexes
    Previously, updating a table that had an inverted index on one of the
    columns would always delete all of the keys for the old row, and then
    re-add all of the keys for the new row, even if there were some keys
    that didn't change in the update.
    
    For example, updating an inverted indexed array column with value
    `[0,1]` to `[0,2]` would delete the key for `0` and `1`, and add the key
    for `0` and `2`, instead of just deleting `1` and adding `2`.
    
    Release note (performance improvement): improve performance of UPDATE on
    tables with inverted indexed columns.

--- pkg/util/unique/unique.go
+++ pkg/util/unique/unique.go
@@ -12,6 +12,7 @@ package unique
 
 import (
 	"bytes"
+	"reflect"
 	"sort"
 )
 
@@ -39,4 +40,76 @@ func UniquifyByteSlices(slices [][]byte) [][]byte {
 	}
 	slices = slices[:lastUniqueIdx+1]
 	return slices
+}
+
+// UniquifyAcrossSlices removes things that are duplicated across both of the
+// slices from both of the slices. It assumes that both slices are already free
+// of duplicates internally. It returns the slices, which will have also been
+// sorted as a side effect.
+// left and right have to be pointers to pre-sorted slices. The slices must be
+// the same types, and they must be pre-sorted along the same metric that
+// cmpFunc checks.
+// cmpFunc compares the lth index of left to the rth index of right.
+// leftSet sets the ith index of left to the jth index of left.
+// rightSet sets the ith index of right to the jth index of right.
+func UniquifyAcrossSlices(
+	left interface{},
+	right interface{},
+	cmpFunc func(l, r int) int,
+	leftSet func(i, j int),
+	rightSet func(i, j int),
+) {
+
+	leftSlice := reflect.ValueOf(left).Elem()
+	rightSlice := reflect.ValueOf(right).Elem()
+
+	leftLen := leftSlice.Len()
+	rightLen := rightSlice.Len()
+
+	var lIn, lOut int
+	var rIn, rOut int
+
+	// Remove entries that are duplicated across both entry lists.
+	// This loop walks through both lists using a merge strategy. Two pointers per
+	// list are maintained. One is the "input pointer", which is always the ith
+	// element of the input list. One is the "output pointer", which is the index
+	// after the most recent unique element in the list. Every time we bump the
+	// input pointer, we also set the element at the output pointer to that at
+	// the input pointer, so we don't have to use extra space - we're
+	// deduplicating in-place.
+	for {
+		if rIn >= rightLen && lIn >= leftLen {
+			break
+		} else if rIn >= rightLen {
+			leftSet(lOut, lIn)
+			lIn++
+			lOut++
+			continue
+		} else if lIn >= leftLen {
+			rightSet(rOut, rIn)
+			rIn++
+			rOut++
+			continue
+		}
+		cmp := cmpFunc(lIn, rIn)
+		switch cmp {
+		case -1:
+			leftSet(lOut, lIn)
+			lIn++
+			lOut++
+		case 1:
+			rightSet(rOut, rIn)
+			rIn++
+			rOut++
+		case 0:
+			// Elements are identical - we want to remove them from the list. So
+			// we increment our input indices without touching our output indices.
+			// Next time through the loop, we'll shift the next element back to
+			// the last output index which is now lagging behind the input index.
+			lIn++
+			rIn++
+		}
+	}
+	leftSlice.SetLen(lOut)
+	rightSlice.SetLen(rOut)
 }
