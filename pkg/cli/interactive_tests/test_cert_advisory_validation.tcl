d16ffd11db768f438a80faa8e2c42b4a19f36f27
Peter Mattis <petermattis@gmail.com>
2020-04-13 16:44:33 -0400

    cli: add --cert-principal-map to client commands
    Add support for the `--cert-principal-map` flag to the certs and client
    commands. Anywhere we were accepting the `--certs-dir` flag, we now also
    accept the `--cert-principal-map` flag.
    
    Fixes #47300
    Fixes #47754
    Fixes #48116
    
    Release note (cli change): Support the `--cert-principal-map` flag in
    the `cert *` and "client" commands such as `sql`, `init`, and `quit`.

--- pkg/cli/interactive_tests/test_cert_advisory_validation.tcl
+++ pkg/cli/interactive_tests/test_cert_advisory_validation.tcl
@@ -77,3 +77,21 @@ send "$argv cert list --certs-dir=$certs_dir --cert-principal-map=foo.bar:node\r
 eexpect "Certificate directory:"
 expect $prompt
 end_test
+
+start_test "Check that 'cert create-client' can utilize cert principal map."
+send "$argv cert create-client root --certs-dir=$certs_dir --ca-key=$certs_dir/ca.key --cert-principal-map=foo.bar:node\r"
+eexpect $prompt
+end_test
+
+start_test "Check that the client commands can use cert principal map."
+system "$argv start-single-node --certs-dir=$certs_dir --cert-principal-map=foo.bar:node --advertise-addr=localhost --background >>expect-cmd.log 2>&1"
+send "$argv sql --certs-dir=$certs_dir --cert-principal-map=foo.bar:node -e \"select 'hello'\"\r"
+eexpect "hello"
+expect $prompt
+send "$argv node ls --certs-dir=$certs_dir --cert-principal-map=foo.bar:node\r"
+eexpect "1 row"
+expect $prompt
+send "$argv quit --certs-dir=$certs_dir --cert-principal-map=foo.bar:node\r"
+eexpect "ok"
+expect $prompt
+end_test
