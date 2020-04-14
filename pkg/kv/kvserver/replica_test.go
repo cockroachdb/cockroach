cockroachdb/cockroach

commit 0e4fac5856eac91d8789d74359f9b049d0cd3ec5

storage: integrate Concurrency Manager into Replica
Related to #41720.
Related to #44976.

This commit integrates the new concurrency package into the storage
package. Each Replica is given a concurrency manager, which replaces
its existing latch manager and txn wait queue. The change also uses
the concurrency manager to simplify the role of the intent resolver.
The intent resolver no longer directly handles WriteIntentErrors. As
a result, we are able to delete the contention queue entirely.

With this change, all requests are now sequenced through the concurrency
manager. When sequencing, latches are acquired and conflicting locks are
detected. If any locks are found, the requests wait in lock wait-queues
for the locks to be resolved. This is a major deviation from how things
currently work because today, even with the contention queue, requests
end up waiting for conflicting transactions to commit/abort in the
txnWaitQueue after at least one RPC. Now, requests wait directly next
to the intents/locks that they are waiting on and react directly to the
resolution of these intents/locks.

Once requests are sequenced by the concurrency manager, they are
theoretically fully isolated from all conflicting requests. However,
this is not strictly true today because we have not yet pulled all
replicated locks into the concurrency manager's lock table. We will
do so in a future change. Until then, the concurrency manager maintains
a notion of "intent discovery", which is integrated into the Replica-level
concurrency retry loop.

Performance numbers will be published shortly. This will be followed
by performance numbers using the SELECT FOR UPDATE locking (#40205)
improvements that this change enables.

pkg/storage/replica_raft.go
@@ -20,8 +20,8 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/storage/apply"
+	"github.com/cockroachdb/cockroach/pkg/storage/concurrency"
 	"github.com/cockroachdb/cockroach/pkg/storage/engine"
-	"github.com/cockroachdb/cockroach/pkg/storage/spanset"
 	"github.com/cockroachdb/cockroach/pkg/storage/stateloader"
 	"github.com/cockroachdb/cockroach/pkg/storage/storagebase"
 	"github.com/cockroachdb/cockroach/pkg/storage/storagepb"
@@ -64,25 +64,14 @@ func makeIDKey() storagebase.CmdIDKey {
 // - any error obtained during the creation or proposal of the command, in
 //   which case the other returned values are zero.
 func (r *Replica) evalAndPropose(
-	ctx context.Context,
-	lease *roachpb.Lease,
-	ba *roachpb.BatchRequest,
-	spans *spanset.SpanSet,
-	ec endCmds,
-) (_ chan proposalResult, _ func(), _ int64, pErr *roachpb.Error) {
+	ctx context.Context, ba *roachpb.BatchRequest, g *concurrency.Guard, lease *roachpb.Lease,
+) (chan proposalResult, func(), int64, *concurrency.Guard, *roachpb.Error) {
 	idKey := makeIDKey()
-	proposal, pErr := r.requestToProposal(ctx, idKey, ba, spans)
+	proposal, pErr := r.requestToProposal(ctx, idKey, ba, g.LatchSpans())
 	log.Event(proposal.ctx, "evaluated request")
 
-	// Attach the endCmds to the proposal. This moves responsibility of
-	// releasing latches to "below Raft" machinery. However, we make sure
-	// we clean up this resource if the proposal doesn't make it to Raft.
-	proposal.ec = ec.move()
-	defer func() {
-		if pErr != nil {
-			proposal.ec.done(ctx, ba, nil /* br */, pErr)
-		}
-	}()
+	// Attach the endCmds to the proposal.
+	proposal.ec = endCmds{repl: r}
 
 	// Pull out proposal channel to return. proposal.doneCh may be set to
 	// nil if it is signaled in this function.
@@ -106,7 +95,7 @@ func (r *Replica) evalAndPropose(
 			EndTxns:            endTxns,
 		}
 		proposal.finishApplication(ctx, pr)
-		return proposalCh, func() {}, 0, nil
+		return proposalCh, func() {}, 0, g, nil
 	}
 
 	// If the request requested that Raft consensus be performed asynchronously,
@@ -117,7 +106,7 @@ func (r *Replica) evalAndPropose(
 			// Disallow async consensus for commands with EndTxnIntents because
 			// any !Always EndTxnIntent can't be cleaned up until after the
 			// command succeeds.
-			return nil, nil, 0, roachpb.NewErrorf("cannot perform consensus asynchronously for "+
+			return nil, nil, 0, g, roachpb.NewErrorf("cannot perform consensus asynchronously for "+
 				"proposal with EndTxnIntents=%v; %v", ets, ba)
 		}
 
@@ -137,6 +126,10 @@ func (r *Replica) evalAndPropose(
 		// Continue with proposal...
 	}
 
+	// Assume responsibility for releasing the concurrency guard
+	// if the proposal makes it to Raft.
+	proposal.ec.g = g
+
 	// Attach information about the proposer to the command.
 	proposal.command.ProposerLeaseSequence = lease.Sequence
 
@@ -154,14 +147,14 @@ func (r *Replica) evalAndPropose(
 	// behavior.
 	quotaSize := uint64(proposal.command.Size())
 	if maxSize := uint64(MaxCommandSize.Get(&r.store.cfg.Settings.SV)); quotaSize > maxSize {
-		return nil, nil, 0, roachpb.NewError(errors.Errorf(
+		return nil, nil, 0, g, roachpb.NewError(errors.Errorf(
 			"command is too large: %d bytes (max: %d)", quotaSize, maxSize,
 		))
 	}
 	var err error
 	proposal.quotaAlloc, err = r.maybeAcquireProposalQuota(ctx, quotaSize)
 	if err != nil {
-		return nil, nil, 0, roachpb.NewError(err)
+		return nil, nil, 0, g, roachpb.NewError(err)
 	}
 	// Make sure we clean up the proposal if we fail to insert it into the
 	// proposal buffer successfully. This ensures that we always release any
@@ -180,13 +173,13 @@ func (r *Replica) evalAndPropose(
 			Req:   *ba,
 		}
 		if pErr := filter(filterArgs); pErr != nil {
-			return nil, nil, 0, pErr
+			return nil, nil, 0, g, pErr
 		}
 	}
 
 	maxLeaseIndex, pErr := r.propose(ctx, proposal)
 	if pErr != nil {
-		return nil, nil, 0, pErr
+		return nil, nil, 0, g, pErr
 	}
 	// Abandoning a proposal unbinds its context so that the proposal's client
 	// is free to terminate execution. However, it does nothing to try to
@@ -208,7 +201,7 @@ func (r *Replica) evalAndPropose(
 		// We'd need to make sure the span is finished eventually.
 		proposal.ctx = r.AnnotateCtx(context.TODO())
 	}
-	return proposalCh, abandon, maxLeaseIndex, nil
+	return proposalCh, abandon, maxLeaseIndex, nil, nil
 }
 
 // propose encodes a command, starts tracking it, and proposes it to raft. The

commit d0c960b54a7c2f6906c84758de905cd643e966f1

kv: release latches before semi-synchronous intent resolution
Fixes #47187.
Fixes #47186.

This commit addresses the deadlock described in #47187. In that issue, we saw
that a single batch deadlocked while performing synchronous intent resolution
while continuing to hold latches over the spans that it was trying to resolve.
This cascaded into a full workload deadlock because all other requests piled up
in the latch manager.

This deadlock was introduced in 0e4fac5. That change made it possible for
semi-synchronous intent resolution to begin on the read-write path before a
request had released its latches. Specifically, in that issue we saw an EndTxn
that hit a TransactionAbortedError run into this bug. This case is now tested
in `TestEndTxnWithErrorAndSyncIntentResolution`. Before this commit, the test
successfully reproduced the deadlock.

It turns out that this has also been very close to a bug in the read-only path
for years. In fact, I believe it was broken or almost broken from the very first
commit that introduced this semi-synchronous intent resolution: 9858253. Even in
that commit, we see that latches (at the time, the CommandQueue) are held during
the call to (poorly named) `processIntentsAsync` in `addReadOnlyCmd`. I believe
the saving grace here was that we only seem to populate the Intents field on the
LocalResult for INCONSISTENT reads, which don't hold latches and therefore can't
create the deadlock. But if that's really the case then I don't understand
https://github.com/cockroachdb/cockroach/blob/29c0efdcc5edb5d100449a093b25df107f1df2d6/pkg/kv/kvserver/replica_read.go#L144.
Either way, this prevented me from hitting a deadlock here or writing a test
for this specific case.

I've tested this with 20 iterations of `cdc/tpcc-1000/rangefeed=true` and it has
been stable throughout. I'll spin up another 30 tonight.

Release notes (bug fix): a bug that could cause a workload to stall under heavy

pkg/kv/kvserver/replica_raft.go
@@ -51,27 +51,37 @@ func makeIDKey() storagebase.CmdIDKey {
 // parameter if the command requires a lease; nil otherwise. It then evaluates
 // the command and proposes it to Raft on success.
 //
+// The method accepts a concurrency guard, which it assumes responsibility for
+// if it succeeds in proposing a command into Raft. If the method does not
+// return an error, the guard is guaranteed to be eventually freed and the
+// caller should relinquish all ownership of it. If it does return an error, the
+// caller retains full ownership over the guard.
+//
 // Return values:
 // - a channel which receives a response or error upon application
 // - a closure used to attempt to abandon the command. When called, it unbinds
 //   the command's context from its Raft proposal. The client is then free to
 //   terminate execution, although it is given no guarantee that the proposal
 //   won't still go on to commit and apply at some later time.
-// - a callback to undo quota acquisition if the attempt to propose the batch
-//   request to raft fails. This also cleans up the command sizes stored for
-//   the corresponding proposal.
 // - the MaxLeaseIndex of the resulting proposal, if any.
 // - any error obtained during the creation or proposal of the command, in
 //   which case the other returned values are zero.
 func (r *Replica) evalAndPropose(
 	ctx context.Context, ba *roachpb.BatchRequest, g *concurrency.Guard, lease *roachpb.Lease,
-) (chan proposalResult, func(), int64, *concurrency.Guard, *roachpb.Error) {
+) (chan proposalResult, func(), int64, *roachpb.Error) {
 	idKey := makeIDKey()
 	proposal, pErr := r.requestToProposal(ctx, idKey, ba, g.LatchSpans())
 	log.Event(proposal.ctx, "evaluated request")
 
-	// Attach the endCmds to the proposal.
-	proposal.ec = endCmds{repl: r}
+	// If the request hit a server-side concurrency retry error, immediately
+	// proagate the error. Don't assume ownership of the concurrency guard.
+	if isConcurrencyRetryError(pErr) {
+		return nil, nil, 0, pErr
+	}
+
+	// Attach the endCmds to the proposal and assume responsibility for
+	// releasing the concurrency guard if the proposal makes it to Raft.
+	proposal.ec = endCmds{repl: r, g: g}
 
 	// Pull out proposal channel to return. proposal.doneCh may be set to
 	// nil if it is signaled in this function.
@@ -95,7 +105,7 @@ func (r *Replica) evalAndPropose(
 			EndTxns:            endTxns,
 		}
 		proposal.finishApplication(ctx, pr)
-		return proposalCh, func() {}, 0, g, nil
+		return proposalCh, func() {}, 0, nil
 	}
 
 	// If the request requested that Raft consensus be performed asynchronously,
@@ -106,7 +116,7 @@ func (r *Replica) evalAndPropose(
 			// Disallow async consensus for commands with EndTxnIntents because
 			// any !Always EndTxnIntent can't be cleaned up until after the
 			// command succeeds.
-			return nil, nil, 0, g, roachpb.NewErrorf("cannot perform consensus asynchronously for "+
+			return nil, nil, 0, roachpb.NewErrorf("cannot perform consensus asynchronously for "+
 				"proposal with EndTxnIntents=%v; %v", ets, ba)
 		}
 
@@ -126,10 +136,6 @@ func (r *Replica) evalAndPropose(
 		// Continue with proposal...
 	}
 
-	// Assume responsibility for releasing the concurrency guard
-	// if the proposal makes it to Raft.
-	proposal.ec.g = g
-
 	// Attach information about the proposer to the command.
 	proposal.command.ProposerLeaseSequence = lease.Sequence
 
@@ -147,14 +153,14 @@ func (r *Replica) evalAndPropose(
 	// behavior.
 	quotaSize := uint64(proposal.command.Size())
 	if maxSize := uint64(MaxCommandSize.Get(&r.store.cfg.Settings.SV)); quotaSize > maxSize {
-		return nil, nil, 0, g, roachpb.NewError(errors.Errorf(
+		return nil, nil, 0, roachpb.NewError(errors.Errorf(
 			"command is too large: %d bytes (max: %d)", quotaSize, maxSize,
 		))
 	}
 	var err error
 	proposal.quotaAlloc, err = r.maybeAcquireProposalQuota(ctx, quotaSize)
 	if err != nil {
-		return nil, nil, 0, g, roachpb.NewError(err)
+		return nil, nil, 0, roachpb.NewError(err)
 	}
 	// Make sure we clean up the proposal if we fail to insert it into the
 	// proposal buffer successfully. This ensures that we always release any
@@ -173,13 +179,13 @@ func (r *Replica) evalAndPropose(
 			Req:   *ba,
 		}
 		if pErr := filter(filterArgs); pErr != nil {
-			return nil, nil, 0, g, pErr
+			return nil, nil, 0, pErr
 		}
 	}
 
 	maxLeaseIndex, pErr := r.propose(ctx, proposal)
 	if pErr != nil {
-		return nil, nil, 0, g, pErr
+		return nil, nil, 0, pErr
 	}
 	// Abandoning a proposal unbinds its context so that the proposal's client
 	// is free to terminate execution. However, it does nothing to try to
@@ -201,7 +207,7 @@ func (r *Replica) evalAndPropose(
 		// We'd need to make sure the span is finished eventually.
 		proposal.ctx = r.AnnotateCtx(context.TODO())
 	}
-	return proposalCh, abandon, maxLeaseIndex, nil, nil
+	return proposalCh, abandon, maxLeaseIndex, nil
 }
 
 // propose encodes a command, starts tracking it, and proposes it to raft. The

pkg/kv/kvserver/replica_read.go
@@ -65,17 +65,50 @@ func (r *Replica) executeReadOnlyBatch(
 
 	var result result.Result
 	br, result, pErr = r.executeReadOnlyBatchWithServersideRefreshes(ctx, rw, rec, ba, spans)
-	if err := r.handleReadOnlyLocalEvalResult(ctx, ba, result.Local); err != nil {
-		pErr = roachpb.NewError(err)
+
+	// If the request hit a server-side concurrency retry error, immediately
+	// proagate the error. Don't assume ownership of the concurrency guard.
+	if isConcurrencyRetryError(pErr) {
+		return nil, g, pErr
 	}
-	r.updateTimestampCache(ctx, ba, br, pErr)
+
+	// Handle any local (leaseholder-only) side-effects of the request.
+	intents := result.Local.DetachEncounteredIntents()
+	if pErr == nil {
+		pErr = r.handleReadOnlyLocalEvalResult(ctx, ba, result.Local)
+	}
+
+	// Otherwise, update the timestamp cache and release the concurrency guard.
+	ec, g := endCmds{repl: r, g: g}, nil
+	ec.done(ctx, ba, br, pErr)
+
+	// Semi-synchronously process any intents that need resolving here in
+	// order to apply back pressure on the client which generated them. The
+	// resolution is semi-synchronous in that there is a limited number of
+	// outstanding asynchronous resolution tasks allowed after which
+	// further calls will block.
+	if len(intents) > 0 {
+		log.Eventf(ctx, "submitting %d intents to asynchronous processing", len(intents))
+		// We only allow synchronous intent resolution for consistent requests.
+		// Intent resolution is async/best-effort for inconsistent requests.
+		//
+		// An important case where this logic is necessary is for RangeLookup
+		// requests. In their case, synchronous intent resolution can deadlock
+		// if the request originated from the local node which means the local
+		// range descriptor cache has an in-flight RangeLookup request which
+		// prohibits any concurrent requests for the same range. See #17760.
+		allowSyncProcessing := ba.ReadConsistency == roachpb.CONSISTENT
+		if err := r.store.intentResolver.CleanupIntentsAsync(ctx, intents, allowSyncProcessing); err != nil {
+			log.Warning(ctx, err)
+		}
+	}
 
 	if pErr != nil {
 		log.VErrEvent(ctx, 3, pErr.String())
 	} else {
 		log.Event(ctx, "read completed")
 	}
-	return br, g, pErr
+	return br, nil, pErr
 }
 
 // executeReadOnlyBatchWithServersideRefreshes invokes evaluateBatch and retries
@@ -115,7 +148,7 @@ func (r *Replica) executeReadOnlyBatchWithServersideRefreshes(
 
 func (r *Replica) handleReadOnlyLocalEvalResult(
 	ctx context.Context, ba *roachpb.BatchRequest, lResult result.LocalResult,
-) error {
+) *roachpb.Error {
 	// Fields for which no action is taken in this method are zeroed so that
 	// they don't trigger an assertion at the end of the method (which checks
 	// that all fields were handled).
@@ -128,7 +161,7 @@ func (r *Replica) handleReadOnlyLocalEvalResult(
 		// to block all traffic until the merge either commits or aborts. See
 		// docs/tech-notes/range-merges.md.
 		if err := r.maybeWatchForMerge(ctx); err != nil {
-			return err
+			return roachpb.NewError(err)
 		}
 		lResult.MaybeWatchForMerge = false
 	}
@@ -139,22 +172,6 @@ func (r *Replica) handleReadOnlyLocalEvalResult(
 			r.concMgr.OnLockAcquired(ctx, &lResult.AcquiredLocks[i])
 		}
 		lResult.AcquiredLocks = nil
-	}
-
-	if intents := lResult.DetachEncounteredIntents(); len(intents) > 0 {
-		log.Eventf(ctx, "submitting %d intents to asynchronous processing", len(intents))
-		// We only allow synchronous intent resolution for consistent requests.
-		// Intent resolution is async/best-effort for inconsistent requests.
-		//
-		// An important case where this logic is necessary is for RangeLookup
-		// requests. In their case, synchronous intent resolution can deadlock
-		// if the request originated from the local node which means the local
-		// range descriptor cache has an in-flight RangeLookup request which
-		// prohibits any concurrent requests for the same range. See #17760.
-		allowSyncProcessing := ba.ReadConsistency == roachpb.CONSISTENT
-		if err := r.store.intentResolver.CleanupIntentsAsync(ctx, intents, allowSyncProcessing); err != nil {
-			log.Warning(ctx, err)
-		}
 	}
 
 	if !lResult.IsZero() {

pkg/kv/kvserver/replica_send.go
@@ -137,6 +137,11 @@ func (r *Replica) sendWithRangeID(
 //    their Raft entry has been committed but before it has been applied to
 //    the replicated state machine. In all of these cases, responsibility
 //    for releasing the concurrency guard is handed to Raft.
+//
+// However, this option is not permitted if the function returns a "server-side
+// concurrency retry error" (see isConcurrencyRetryError for more details). If
+// the function returns one of these errors, it must also pass ownership of the
+// concurrency guard back to the caller.
 type batchExecutionFn func(
 	*Replica, context.Context, *roachpb.BatchRequest, storagepb.LeaseStatus, *concurrency.Guard,
 ) (*roachpb.BatchResponse, *concurrency.Guard, *roachpb.Error)
@@ -240,49 +245,68 @@ func (r *Replica) executeBatchWithConcurrencyRetries(
 		}
 
 		br, g, pErr = fn(r, ctx, ba, status, g)
-		switch t := pErr.GetDetail().(type) {
-		case nil:
+		if pErr == nil {
 			// Success.
 			return br, nil
+		} else if !isConcurrencyRetryError(pErr) {
+			// Propagate error.
+			return nil, pErr
+		}
+
+		// The batch execution func returned a server-side concurrency retry
+		// error. It must have also handed back ownership of the concurrency
+		// guard without having already released the guard's latches.
+		g.AssertLatches()
+		switch t := pErr.GetDetail().(type) {
 		case *roachpb.WriteIntentError:
 			// Drop latches, but retain lock wait-queues.
-			g.AssertLatches()
 			if g, pErr = r.handleWriteIntentError(ctx, ba, g, pErr, t); pErr != nil {
 				return nil, pErr
 			}
-			// Retry...
 		case *roachpb.TransactionPushError:
 			// Drop latches, but retain lock wait-queues.
-			g.AssertLatches()
 			if g, pErr = r.handleTransactionPushError(ctx, ba, g, pErr, t); pErr != nil {
 				return nil, pErr
 			}
-			// Retry...
 		case *roachpb.IndeterminateCommitError:
 			// Drop latches and lock wait-queues.
-			g.AssertLatches()
 			r.concMgr.FinishReq(g)
 			g = nil
 			// Then launch a task to handle the indeterminate commit error.
 			if pErr = r.handleIndeterminateCommitError(ctx, ba, pErr, t); pErr != nil {
 				return nil, pErr
 			}
-			// Retry...
 		case *roachpb.MergeInProgressError:
 			// Drop latches and lock wait-queues.
-			g.AssertLatches()
 			r.concMgr.FinishReq(g)
 			g = nil
 			// Then listen for the merge to complete.
 			if pErr = r.handleMergeInProgressError(ctx, ba, pErr, t); pErr != nil {
 				return nil, pErr
 			}
-			// Retry...
 		default:
-			// Propagate error.
-			return nil, pErr
+			log.Fatalf(ctx, "unexpected concurrency retry error %T", t)
 		}
+		// Retry...
 	}
+}
+
+// isConcurrencyRetryError returns whether or not the provided error is a
+// "server-side concurrency retry error" that will be captured and retried by
+// executeBatchWithConcurrencyRetries. Server-side concurrency retry errors are
+// handled by dropping a request's latches, waiting for and/or ensuring that the
+// condition which caused the error is handled, re-sequencing through the
+// concurrency manager, and executing the request again.
+func isConcurrencyRetryError(pErr *roachpb.Error) bool {
+	switch pErr.GetDetail().(type) {
+	case *roachpb.WriteIntentError:
+	case *roachpb.TransactionPushError:
+	case *roachpb.IndeterminateCommitError:
+	case *roachpb.MergeInProgressError:
+	default:
+		return false
+	}
+	return true
 }
 
 func (r *Replica) handleWriteIntentError(

pkg/kv/kvserver/replica_write.go
@@ -130,7 +130,7 @@ func (r *Replica) executeWriteBatch(
 	// If the command is proposed to Raft, ownership of and responsibility for
 	// the concurrency guard will be assumed by Raft, so provide the guard to
 	// evalAndPropose.
-	ch, abandon, maxLeaseIndex, g, pErr := r.evalAndPropose(ctx, ba, g, &st.Lease)
+	ch, abandon, maxLeaseIndex, pErr := r.evalAndPropose(ctx, ba, g, &st.Lease)
 	if pErr != nil {
 		if maxLeaseIndex != 0 {
 			log.Fatalf(
@@ -140,6 +140,8 @@ func (r *Replica) executeWriteBatch(
 		}
 		return nil, g, pErr
 	}
+	g = nil // ownership passed to Raft, prevent misuse
+
 	// A max lease index of zero is returned when no proposal was made or a lease was proposed.
 	// In both cases, we don't need to communicate a MLAI. Furthermore, for lease proposals we
 	// cannot communicate under the lease's epoch. Instead the code calls EmitMLAI explicitly
@@ -198,7 +200,7 @@ func (r *Replica) executeWriteBatch(
 					log.Warning(ctx, err)
 				}
 			}
-			return propResult.Reply, g, propResult.Err
+			return propResult.Reply, nil, propResult.Err
 		case <-slowTimer.C:
 			slowTimer.Read = true
 			r.store.metrics.SlowRaftRequests.Inc(1)
@@ -211,14 +213,14 @@ func (r *Replica) executeWriteBatch(
 			abandon()
 			log.VEventf(ctx, 2, "context cancellation after %0.1fs of attempting command %s",
 				timeutil.Since(startTime).Seconds(), ba)
-			return nil, g, roachpb.NewError(roachpb.NewAmbiguousResultError(ctx.Err().Error()))
+			return nil, nil, roachpb.NewError(roachpb.NewAmbiguousResultError(ctx.Err().Error()))
 		case <-shouldQuiesce:
 			// If shutting down, return an AmbiguousResultError, which indicates
 			// to the caller that the command may have executed.
 			abandon()
 			log.VEventf(ctx, 2, "shutdown cancellation after %0.1fs of attempting command %s",
 				timeutil.Since(startTime).Seconds(), ba)
-			return nil, g, roachpb.NewError(roachpb.NewAmbiguousResultError("server shutdown"))
+			return nil, nil, roachpb.NewError(roachpb.NewAmbiguousResultError("server shutdown"))
 		}
 	}
 }

pkg/kv/kvserver/replica_test.go
@@ -606,7 +606,7 @@ func sendLeaseRequest(r *Replica, l *roachpb.Lease) error {
 	ba.Timestamp = r.store.Clock().Now()
 	ba.Add(&roachpb.RequestLeaseRequest{Lease: *l})
 	exLease, _ := r.GetLease()
-	ch, _, _, _, pErr := r.evalAndPropose(context.TODO(), &ba, allSpansGuard(), &exLease)
+	ch, _, _, pErr := r.evalAndPropose(context.TODO(), &ba, allSpansGuard(), &exLease)
 	if pErr == nil {
 		// Next if the command was committed, wait for the range to apply it.
 		// TODO(bdarnell): refactor this to a more conventional error-handling pattern.
@@ -1387,7 +1387,7 @@ func TestReplicaLeaseRejectUnknownRaftNodeID(t *testing.T) {
 	ba := roachpb.BatchRequest{}
 	ba.Timestamp = tc.repl.store.Clock().Now()
 	ba.Add(&roachpb.RequestLeaseRequest{Lease: *lease})
-	ch, _, _, _, pErr := tc.repl.evalAndPropose(context.Background(), &ba, allSpansGuard(), &exLease)
+	ch, _, _, pErr := tc.repl.evalAndPropose(context.Background(), &ba, allSpansGuard(), &exLease)
 	if pErr == nil {
 		// Next if the command was committed, wait for the range to apply it.
 		// TODO(bdarnell): refactor to a more conventional error-handling pattern.
@@ -4239,14 +4239,16 @@ func TestEndTxnWithErrors(t *testing.T) {
 			existTxn.Epoch = test.existEpoch
 			existTxnRecord := existTxn.AsRecord()
 			txnKey := keys.TransactionKey(test.key, txn.ID)
-			if err := storage.MVCCPutProto(ctx, tc.repl.store.Engine(), nil, txnKey, hlc.Timestamp{},
-				nil, &existTxnRecord); err != nil {
+			if err := storage.MVCCPutProto(
+				ctx, tc.repl.store.Engine(), nil, txnKey, hlc.Timestamp{}, nil, &existTxnRecord,
+			); err != nil {
 				t.Fatal(err)
 			}
 
 			// End the transaction, verify expected error.
 			txn.Key = test.key
 			args, h := endTxnArgs(txn, true)
+			args.LockSpans = []roachpb.Span{{Key: txn.Key}}
 			args.Sequence = 2
 
 			if _, pErr := tc.SendWrappedWith(h, &args); !testutils.IsPError(pErr, test.expErrRegexp) {
@@ -4257,6 +4259,41 @@ func TestEndTxnWithErrors(t *testing.T) {
 			}
 		})
 	}
+}
+
+// TestEndTxnWithErrorAndSyncIntentResolution verifies that an EndTransaction
+// request that hits an error and then is forced to perform intent resolution
+// synchronously does not deadlock on itself. This is a regression test against
+// #47187.
+func TestEndTxnWithErrorAndSyncIntentResolution(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	tc := testContext{}
+	ctx := context.Background()
+	stopper := stop.NewStopper()
+	defer stopper.Stop(ctx)
+	cfg := TestStoreConfig(nil)
+	cfg.TestingKnobs.IntentResolverKnobs.ForceSyncIntentResolution = true
+	tc.StartWithStoreConfig(t, stopper, cfg)
+
+	txn := newTransaction("test", roachpb.Key("a"), 1, tc.Clock())
+
+	// Establish existing txn state by writing directly to range engine.
+	existTxn := txn.Clone()
+	existTxn.Status = roachpb.ABORTED
+	existTxnRec := existTxn.AsRecord()
+	txnKey := keys.TransactionKey(txn.Key, txn.ID)
+	err := storage.MVCCPutProto(ctx, tc.repl.store.Engine(), nil, txnKey, hlc.Timestamp{}, nil, &existTxnRec)
+	require.NoError(t, err)
+
+	// End the transaction, verify expected error, shouldn't deadlock.
+	args, h := endTxnArgs(txn, true)
+	args.LockSpans = []roachpb.Span{{Key: txn.Key}}
+	args.Sequence = 2
+
+	_, pErr := tc.SendWrappedWith(h, &args)
+	require.Error(t, pErr.GetDetail(), "TransactionAbortedError(ABORT_REASON_ABORTED_RECORD_FOUND)")
+	require.NotNil(t, pErr.GetTxn())
+	require.Equal(t, txn.ID, pErr.GetTxn().ID)
 }
 
 // TestEndTxnRollbackAbortedTransaction verifies that no error is returned when
@@ -7647,7 +7684,7 @@ func TestReplicaCancelRaftCommandProgress(t *testing.T) {
 				Key: roachpb.Key(fmt.Sprintf("k%d", i)),
 			},
 		})
-		ch, _, idx, _, err := repl.evalAndPropose(ctx, &ba, allSpansGuard(), &lease)
+		ch, _, idx, err := repl.evalAndPropose(ctx, &ba, allSpansGuard(), &lease)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -7716,7 +7753,7 @@ func TestReplicaBurstPendingCommandsAndRepropose(t *testing.T) {
 				Key: roachpb.Key(fmt.Sprintf("k%d", i)),
 			},
 		})
-		ch, _, idx, _, err := tc.repl.evalAndPropose(ctx, &ba, allSpansGuard(), &lease)
+		ch, _, idx, err := tc.repl.evalAndPropose(ctx, &ba, allSpansGuard(), &lease)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -9034,7 +9071,7 @@ func TestErrorInRaftApplicationClearsIntents(t *testing.T) {
 	}
 
 	exLease, _ := repl.GetLease()
-	ch, _, _, _, pErr := repl.evalAndPropose(context.Background(), &ba, allSpansGuard(), &exLease)
+	ch, _, _, pErr := repl.evalAndPropose(context.Background(), &ba, allSpansGuard(), &exLease)
 	if pErr != nil {
 		t.Fatal(pErr)
 	}
@@ -9079,7 +9116,7 @@ func TestProposeWithAsyncConsensus(t *testing.T) {
 
 	atomic.StoreInt32(&filterActive, 1)
 	exLease, _ := repl.GetLease()
-	ch, _, _, _, pErr := repl.evalAndPropose(context.Background(), &ba, allSpansGuard(), &exLease)
+	ch, _, _, pErr := repl.evalAndPropose(context.Background(), &ba, allSpansGuard(), &exLease)
 	if pErr != nil {
 		t.Fatal(pErr)
 	}
@@ -9142,7 +9179,7 @@ func TestApplyPaginatedCommittedEntries(t *testing.T) {
 
 	atomic.StoreInt32(&filterActive, 1)
 	exLease, _ := repl.GetLease()
-	_, _, _, _, pErr := repl.evalAndPropose(ctx, &ba, allSpansGuard(), &exLease)
+	_, _, _, pErr := repl.evalAndPropose(ctx, &ba, allSpansGuard(), &exLease)
 	if pErr != nil {
 		t.Fatal(pErr)
 	}
@@ -9160,7 +9197,7 @@ func TestApplyPaginatedCommittedEntries(t *testing.T) {
 		ba2.Timestamp = tc.Clock().Now()
 
 		var pErr *roachpb.Error
-		ch, _, _, _, pErr = repl.evalAndPropose(ctx, &ba, allSpansGuard(), &exLease)
+		ch, _, _, pErr = repl.evalAndPropose(ctx, &ba, allSpansGuard(), &exLease)
 		if pErr != nil {
 			t.Fatal(pErr)
 		}
@@ -12050,7 +12087,7 @@ func TestProposalNotAcknowledgedOrReproposedAfterApplication(t *testing.T) {
 	// the proposal map. Entries are only removed from that map underneath raft.
 	tc.repl.RaftLock()
 	tracedCtx, cleanup := tracing.EnsureContext(ctx, cfg.AmbientCtx.Tracer, "replica send")
-	ch, _, _, _, pErr := tc.repl.evalAndPropose(tracedCtx, &ba, allSpansGuard(), &lease)
+	ch, _, _, pErr := tc.repl.evalAndPropose(tracedCtx, &ba, allSpansGuard(), &lease)
 	if pErr != nil {
 		t.Fatal(pErr)
 	}
@@ -12146,7 +12183,7 @@ func TestLaterReproposalsDoNotReuseContext(t *testing.T) {
 	// Go out of our way to enable recording so that expensive logging is enabled
 	// for this context.
 	tracing.StartRecording(sp, tracing.SingleNodeRecording)
-	ch, _, _, _, pErr := tc.repl.evalAndPropose(tracedCtx, &ba, allSpansGuard(), &lease)
+	ch, _, _, pErr := tc.repl.evalAndPropose(tracedCtx, &ba, allSpansGuard(), &lease)
 	if pErr != nil {
 		t.Fatal(pErr)
 	}