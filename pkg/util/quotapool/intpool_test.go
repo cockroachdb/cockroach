cockroachdb/cockroach

commit 755c634261bb936bfdebcab12379654b660a077d

util/stopper: adopt quotapool in place of channel for RunLimitedAsyncTask
This commit replaces the channel-based semaphore with a quotapool.

Release note: None

pkg/util/quotapool/intpool.go
@@ -17,7 +17,7 @@ import (
 	"sync"
 	"sync/atomic"
 
-	"github.com/pkg/errors"
+	"github.com/cockroachdb/errors"
 )
 
 // IntPool manages allocating integer units of quota to clients.
@@ -189,6 +189,17 @@ type IntRequestFunc func(ctx context.Context, p PoolInfo) (took uint64, err erro
 // again once there's new resources.
 var ErrNotEnoughQuota = fmt.Errorf("not enough quota available")
 
+// HasErrClosed returns true if this error is or contains an ErrClosed error.
+func HasErrClosed(err error) bool {
+	_, hasErrClosed := errors.If(err, func(err error) (unwrapped interface{}, ok bool) {
+		if _, hasErrClosed := err.(*ErrClosed); hasErrClosed {
+			return err, hasErrClosed
+		}
+		return nil, false
+	})
+	return hasErrClosed
+}
+
 // PoolInfo represents the information that the IntRequestFunc gets about the current quota pool conditions.
 type PoolInfo struct {
 	// Available is the amount of quota available to be consumed. This is the
@@ -263,6 +274,22 @@ func (p *IntPool) ApproximateQuota() (q uint64) {
 // Safe for concurrent use.
 func (p *IntPool) Close(reason string) {
 	p.qp.Close(reason)
+}
+
+// IntPoolCloser implements stop.Closer.
+type IntPoolCloser struct {
+	reason string
+	p      *IntPool
+}
+
+// Close makes the IntPoolCloser a stop.Closer.
+func (ipc IntPoolCloser) Close() {
+	ipc.p.Close(ipc.reason)
+}
+
+// Closer returns a struct which implements stop.Closer.
+func (p *IntPool) Closer(reason string) IntPoolCloser {
+	return IntPoolCloser{p: p, reason: reason}
 }
 
 var intAllocSyncPool = sync.Pool{

pkg/util/quotapool/intpool_test.go
@@ -17,6 +17,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/quotapool"
@@ -24,6 +25,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+// logSlowAcquisition is an Option to log slow acquisitions.
+var logSlowAcquisition = quotapool.OnSlowAcquisition(base.SlowRequestThreshold, quotapool.LogSlowAcquisition)
 
 // TestQuotaPoolBasic tests the minimal expected behavior of the quota pool
 // with different sized quota pool and a varying number of goroutines, each
@@ -561,7 +565,7 @@ func TestIntpoolRelease(t *testing.T) {
 func TestLen(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 
-	qp := quotapool.NewIntPool("test", 1, quotapool.LogSlowAcquisition)
+	qp := quotapool.NewIntPool("test", 1, logSlowAcquisition)
 	ctx := context.Background()
 	allocCh := make(chan *quotapool.IntAlloc)
 	doAcquire := func(ctx context.Context) {

pkg/kv/dist_sender.go
@@ -29,6 +29,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/metric"
+	"github.com/cockroachdb/cockroach/pkg/util/quotapool"
 	"github.com/cockroachdb/cockroach/pkg/util/retry"
 	"github.com/cockroachdb/cockroach/pkg/util/tracing"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
@@ -190,7 +191,7 @@ type DistSender struct {
 	rpcContext       *rpc.Context
 	nodeDialer       *nodedialer.Dialer
 	rpcRetryOptions  retry.Options
-	asyncSenderSem   chan struct{}
+	asyncSenderSem   *quotapool.IntPool
 	// clusterID is used to verify access to enterprise features.
 	// It is copied out of the rpcContext at construction time and used in
 	// testing.
@@ -282,7 +283,8 @@ func NewDistSender(cfg DistSenderConfig, g *gossip.Gossip) *DistSender {
 	}
 	ds.clusterID = &cfg.RPCContext.ClusterID
 	ds.nodeDialer = cfg.NodeDialer
-	ds.asyncSenderSem = make(chan struct{}, defaultSenderConcurrency)
+	ds.asyncSenderSem = quotapool.NewIntPool("dist sender concurrency", defaultSenderConcurrency)
+	ds.rpcContext.Stopper.AddCloser(ds.asyncSenderSem.Closer("stopper"))
 	ds.rangeIteratorGen = func() *RangeIterator { return NewRangeIterator(ds) }
 
 	if g != nil {

pkg/server/status.go
@@ -53,6 +53,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/contextutil"
 	"github.com/cockroachdb/cockroach/pkg/util/httputil"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/quotapool"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
@@ -1642,7 +1643,9 @@ func (s *statusServer) iterateNodes(
 	}
 
 	// Issue the requests concurrently.
-	sem := make(chan struct{}, maxConcurrentRequests)
+	sem := quotapool.NewIntPool("node status", maxConcurrentRequests)
+	ctx, cancel := s.stopper.WithCancelOnStop(ctx)
+	defer cancel()
 	for nodeID := range nodeStatuses {
 		nodeID := nodeID // needed to ensure the closure below captures a copy.
 		if err := s.stopper.RunLimitedAsyncTask(

pkg/sql/lease.go
@@ -35,6 +35,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlutil"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/quotapool"
 	"github.com/cockroachdb/cockroach/pkg/util/retry"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
@@ -1394,7 +1395,10 @@ type LeaseManager struct {
 	testingKnobs LeaseManagerTestingKnobs
 	ambientCtx   log.AmbientContext
 	stopper      *stop.Stopper
+	sem          *quotapool.IntPool
 }
+
+const leaseConcurrencyLimit = 5
 
 // NewLeaseManager creates a new LeaseManager.
 //
@@ -1432,8 +1436,9 @@ func NewLeaseManager(
 		},
 		ambientCtx: ambientCtx,
 		stopper:    stopper,
+		sem:        quotapool.NewIntPool("lease manager", leaseConcurrencyLimit),
 	}
-
+	lm.stopper.AddCloser(lm.sem.Closer("stopper"))
 	lm.mu.tables = make(map[sqlbase.ID]*tableState)
 
 	lm.draining.Store(false)
@@ -1860,13 +1865,12 @@ func (m *LeaseManager) refreshSomeLeases(ctx context.Context) {
 	}
 	m.mu.Unlock()
 	// Limit the number of concurrent lease refreshes.
-	sem := make(chan struct{}, 5)
 	var wg sync.WaitGroup
 	for i := range ids {
 		id := ids[i]
 		wg.Add(1)
 		if err := m.stopper.RunLimitedAsyncTask(
-			ctx, fmt.Sprintf("refresh table:%d lease", id), sem, true /*wait*/, func(ctx context.Context) {
+			ctx, fmt.Sprintf("refresh table:%d lease", id), m.sem, true /*wait*/, func(ctx context.Context) {
 				defer wg.Done()
 				if _, err := acquireNodeLease(ctx, m, id); err != nil {
 					log.Infof(ctx, "refreshing table: %d lease failed: %s", id, err)
@@ -1914,18 +1918,11 @@ SELECT "descID", version, expiration FROM system.public.lease AS OF SYSTEM TIME 
 			log.Warningf(ctx, "unable to read orphaned leases: %+v", err)
 			return
 		}
-		// Limit the number of concurrent lease releases.
-		sem := make(chan struct{}, 5)
+
 		var wg sync.WaitGroup
 		defer wg.Wait()
 		for i := range rows {
 			// Early exit?
-			select {
-			case <-m.stopper.ShouldQuiesce():
-				return
-			default:
-			}
-
 			row := rows[i]
 			wg.Add(1)
 			lease := storedTableLease{
@@ -1934,7 +1931,7 @@ SELECT "descID", version, expiration FROM system.public.lease AS OF SYSTEM TIME 
 				expiration: tree.MustBeDTimestamp(row[2]),
 			}
 			if err := m.stopper.RunLimitedAsyncTask(
-				ctx, fmt.Sprintf("release table lease %+v", lease), sem, true /*wait*/, func(ctx context.Context) {
+				ctx, fmt.Sprintf("release table lease %+v", lease), m.sem, true /*wait*/, func(ctx context.Context) {
 					m.LeaseStore.release(ctx, m.stopper, &lease)
 					log.Infof(ctx, "released orphaned table lease: %+v", lease)
 					wg.Done()

pkg/storage/intentresolver/intent_resolver.go
@@ -29,6 +29,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/contextutil"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/quotapool"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
@@ -124,8 +125,8 @@ type IntentResolver struct {
 	stopper      *stop.Stopper
 	testingKnobs storagebase.IntentResolverTestingKnobs
 	ambientCtx   log.AmbientContext
-	sem          chan struct{}    // Semaphore to limit async goroutines.
-	contentionQ  *contentionQueue // manages contention on individual keys
+	sem          *quotapool.IntPool // Semaphore to limit async goroutines.
+	contentionQ  *contentionQueue   // manages contention on individual keys
 
 	rdc kvbase.RangeDescriptorCache
 
@@ -186,13 +187,14 @@ func New(c Config) *IntentResolver {
 		clock:        c.Clock,
 		db:           c.DB,
 		stopper:      c.Stopper,
-		sem:          make(chan struct{}, c.TaskLimit),
+		sem:          quotapool.NewIntPool("intent resolver", uint64(c.TaskLimit)),
 		contentionQ:  newContentionQueue(c.Clock, c.DB),
 		every:        log.Every(time.Minute),
 		Metrics:      makeMetrics(),
 		rdc:          c.RangeDescriptorCache,
 		testingKnobs: c.TestingKnobs,
 	}
+	c.Stopper.AddCloser(ir.sem.Closer("stopper"))
 	ir.mu.inFlightPushes = map[uuid.UUID]int{}
 	ir.mu.inFlightTxnCleanups = map[uuid.UUID]struct{}{}
 	gcBatchSize := gcBatchSize

pkg/storage/queue.go
@@ -29,6 +29,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/metric"
+	"github.com/cockroachdb/cockroach/pkg/util/quotapool"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
@@ -407,9 +408,9 @@ type baseQueue struct {
 	queueConfig
 	incoming         chan struct{} // Channel signaled when a new replica is added to the queue.
 	processSem       chan struct{}
-	addOrMaybeAddSem chan struct{} // for {Maybe,}AddAsync
-	addLogN          log.EveryN    // avoid log spam when addSem, addOrMaybeAddSemSize are maxed out
-	processDur       int64         // accessed atomically
+	addOrMaybeAddSem *quotapool.IntPool // for {Maybe,}AddAsync
+	addLogN          log.EveryN         // avoid log spam when addSem, addOrMaybeAddSemSize are maxed out
+	processDur       int64              // accessed atomically
 	mu               struct {
 		syncutil.Mutex                                    // Protects all variables in the mu struct
 		replicas       map[roachpb.RangeID]*replicaItem   // Map from RangeID to replicaItem
@@ -460,7 +461,7 @@ func newBaseQueue(
 		queueConfig:      cfg,
 		incoming:         make(chan struct{}, 1),
 		processSem:       make(chan struct{}, cfg.maxConcurrency),
-		addOrMaybeAddSem: make(chan struct{}, cfg.addOrMaybeAddSemSize),
+		addOrMaybeAddSem: quotapool.NewIntPool("queue-add", uint64(cfg.addOrMaybeAddSemSize)),
 		addLogN:          log.Every(5 * time.Second),
 		getReplica: func(id roachpb.RangeID) (replicaInQueue, error) {
 			repl, err := store.GetReplica(id)

pkg/storage/store.go
@@ -59,6 +59,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/metric"
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
+	"github.com/cockroachdb/cockroach/pkg/util/quotapool"
 	"github.com/cockroachdb/cockroach/pkg/util/retry"
 	"github.com/cockroachdb/cockroach/pkg/util/shuffle"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
@@ -988,7 +989,8 @@ func (s *Store) SetDraining(drain bool) {
 	ctx := logtags.AddTag(context.Background(), "drain", nil)
 	transferAllAway := func() int {
 		// Limit the number of concurrent lease transfers.
-		sem := make(chan struct{}, 100)
+		const leaseTransferConcurrency = 100
+		sem := quotapool.NewIntPool("Store.SetDraining", leaseTransferConcurrency)
 		// Incremented for every lease or Raft leadership transfer attempted. We try
 		// to send both the lease and the Raft leaders away, but this may not
 		// reliably work. Instead, we run the surrounding retry loop until there are

commit 4c305bffce6dc1bcee2fb728d1a6ecb04c253774

quotapool: return ErrNotEnoughQuota on acquisition from zero-capacity pool
This commit updates the `IntPool` library to return an ErrNotEnoughQuota
error on any non-noop acquisition when a pool has a capacity of zero.

This revives the behavior of `IntentResolverTestingKnobs.ForceSyncIntentResolution`,
which had been accidentally broken by 755c634.

pkg/kv/kvserver/storagebase/knobs.go
@@ -25,9 +25,11 @@ type BatchEvalTestingKnobs struct {
 	// to one of the other filters. See #10493
 	// TODO(andrei): Provide guidance on what to use instead for trapping reads.
 	TestingEvalFilter ReplicaCommandFilter
+
 	// NumKeysEvaluatedForRangeIntentResolution is set by the stores to the
 	// number of keys evaluated for range intent resolution.
 	NumKeysEvaluatedForRangeIntentResolution *int64
+
 	// RecoverIndeterminateCommitsOnFailedPushes will propagate indeterminate
 	// commit errors to trigger transaction recovery even if the push that
 	// discovered the indeterminate commit was going to fail. This increases

pkg/util/quotapool/intpool.go
@@ -139,6 +139,9 @@ func NewIntPool(name string, capacity uint64, options ...Option) *IntPool {
 // Acquisitions of 0 return immediately with no error, even if the IntPool is
 // closed.
 //
+// Acquisitions of more than 0 from a pool with 0 capacity always returns an
+// ErrNotEnoughQuota.
+//
 // Safe for concurrent use.
 func (p *IntPool) Acquire(ctx context.Context, v uint64) (*IntAlloc, error) {
 	return p.acquireMaybeWait(ctx, v, true /* wait */)
@@ -154,6 +157,10 @@ func (p *IntPool) acquireMaybeWait(ctx context.Context, v uint64, wait bool) (*I
 	// Special case acquisitions of size 0.
 	if v == 0 {
 		return p.newIntAlloc(0), nil
+	}
+	// Special case capacity of 0.
+	if p.Capacity() == 0 {
+		return nil, ErrNotEnoughQuota
 	}
 	// The maximum capacity is math.MaxInt64 so we can always truncate requests
 	// to that value.

pkg/util/quotapool/intpool_test.go
@@ -305,6 +305,33 @@ func TestQuotaPoolCappedAcquisition(t *testing.T) {
 	}
 }
 
+// TestQuotaPoolZeroCapacity verifies that a non-noop acquisition request on a
+// pool with zero capacity is immediately rejected, regardless of whether the
+// request is permitted to wait or not.
+func TestQuotaPoolZeroCapacity(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	const quota = 0
+	qp := quotapool.NewIntPool("test", quota)
+	ctx := context.Background()
+
+	failed, err := qp.Acquire(ctx, 1)
+	require.Equal(t, quotapool.ErrNotEnoughQuota, err)
+	require.Nil(t, failed)
+
+	failed, err = qp.TryAcquire(ctx, 1)
+	require.Equal(t, quotapool.ErrNotEnoughQuota, err)
+	require.Nil(t, failed)
+
+	acq1, err := qp.Acquire(ctx, 0)
+	require.NoError(t, err)
+	acq1.Release()
+
+	acq2, err := qp.TryAcquire(ctx, 0)
+	require.NoError(t, err)
+	acq2.Release()
+}
+
 func TestOnAcquisition(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 