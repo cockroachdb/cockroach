825d8801632064430a3c4af18a0119070325841f
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2020-04-20 18:56:26 -0400

    kvserver: re-order NodeLiveness.StartHeartbeat arguments
    It's standard for the callback argument to be the final arg.

--- pkg/kv/kvserver/node_liveness.go
+++ pkg/kv/kvserver/node_liveness.go
@@ -438,7 +438,7 @@ func (nl *NodeLiveness) IsLive(nodeID roachpb.NodeID) (bool, error) {
 // liveness. The slice of engines will be written to before each heartbeat to
 // avoid maintaining liveness in the presence of disk stalls.
 func (nl *NodeLiveness) StartHeartbeat(
-	ctx context.Context, stopper *stop.Stopper, alive HeartbeatCallback, engines []storage.Engine,
+	ctx context.Context, stopper *stop.Stopper, engines []storage.Engine, alive HeartbeatCallback,
 ) {
 	log.VEventf(ctx, 1, "starting liveness heartbeat")
 	retryOpts := base.DefaultRetryOptions()
