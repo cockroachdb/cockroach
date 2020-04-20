6ca26976be6fdc11e248f858439dc91669b2e3d4
Oliver Tan <otan@cockroachlabs.com>
2020-04-20 07:43:59 -0700

    geo: add encoding functionality to different formats
    This commit adds encoding for KML, GeoJSON, WKT, WKB and WKBHex. Note EWKB
    and EWKBHex is already provided by the identity function and String()
    respectively. These will be plugged in at a later date.
    
    Release note: None

---
+++ pkg/geo/encode_test.go
@@ -0,0 +1,140 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package geo
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/geo/geopb"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEWKBToWKT(t *testing.T) {
+	testCases := []struct {
+		ewkt     geopb.EWKT
+		expected geopb.WKT
+	}{
+		{"POINT(1.0 1.0)", "POINT (1 1)"},
+		{"SRID=4;POINT(1.0 1.0)", "POINT (1 1)"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(string(tc.ewkt), func(t *testing.T) {
+			ewkb, err := ParseEWKT(tc.ewkt, geopb.DefaultGeometrySRID)
+			require.NoError(t, err)
+			encoded, err := EWKBToWKT(ewkb)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, encoded)
+		})
+	}
+}
+
+func TestEWKBToEWKT(t *testing.T) {
+	testCases := []struct {
+		ewkt     geopb.EWKT
+		expected geopb.EWKT
+	}{
+		{"POINT(1.0 1.0)", "POINT (1 1)"},
+		{"SRID=4;POINT(1.0 1.0)", "SRID=4;POINT (1 1)"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(string(tc.ewkt), func(t *testing.T) {
+			ewkb, err := ParseEWKT(tc.ewkt, geopb.DefaultGeometrySRID)
+			require.NoError(t, err)
+			encoded, err := EWKBToEWKT(ewkb)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, encoded)
+		})
+	}
+}
+
+func TestEWKBToWKB(t *testing.T) {
+	testCases := []struct {
+		ewkt     geopb.EWKT
+		expected geopb.WKB
+	}{
+		{"POINT(1.0 1.0)", []byte("\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0\x3f\x00\x00\x00\x00\x00\x00\xf0\x3f")},
+		{"SRID=4;POINT(1.0 1.0)", []byte("\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0\x3f\x00\x00\x00\x00\x00\x00\xf0\x3f")},
+	}
+
+	for _, tc := range testCases {
+		t.Run(string(tc.ewkt), func(t *testing.T) {
+			ewkb, err := ParseEWKT(tc.ewkt, geopb.DefaultGeometrySRID)
+			require.NoError(t, err)
+			encoded, err := EWKBToWKB(ewkb)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, encoded)
+		})
+	}
+}
+
+func TestEWKBToGeoJSON(t *testing.T) {
+	testCases := []struct {
+		ewkt     geopb.EWKT
+		expected string
+	}{
+		{"POINT(1.0 1.0)", `{"type":"Feature","geometry":{"type":"Point","coordinates":[1,1]},"properties":null}`},
+		{"SRID=4;POINT(1.0 1.0)", `{"type":"Feature","geometry":{"type":"Point","coordinates":[1,1]},"properties":null}`},
+	}
+
+	for _, tc := range testCases {
+		t.Run(string(tc.ewkt), func(t *testing.T) {
+			ewkb, err := ParseEWKT(tc.ewkt, geopb.DefaultGeometrySRID)
+			require.NoError(t, err)
+			encoded, err := EWKBToGeoJSON(ewkb)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, string(encoded))
+		})
+	}
+}
+
+func TestEWKBToWKBHex(t *testing.T) {
+	testCases := []struct {
+		ewkt     geopb.EWKT
+		expected string
+	}{
+		{"POINT(1.0 1.0)", "0101000000000000000000F03F000000000000F03F"},
+		{"SRID=4;POINT(1.0 1.0)", "0101000000000000000000F03F000000000000F03F"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(string(tc.ewkt), func(t *testing.T) {
+			ewkb, err := ParseEWKT(tc.ewkt, geopb.DefaultGeometrySRID)
+			require.NoError(t, err)
+			encoded, err := EWKBToWKBHex(ewkb)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, encoded)
+		})
+	}
+}
+
+func TestEWKBToKML(t *testing.T) {
+	testCases := []struct {
+		ewkt     geopb.EWKT
+		expected string
+	}{
+		{"POINT(1.0 1.0)", `<?xml version="1.0" encoding="UTF-8"?>
+<Point><coordinates>1,1</coordinates></Point>`},
+		{"SRID=4;POINT(1.0 1.0)", `<?xml version="1.0" encoding="UTF-8"?>
+<Point><coordinates>1,1</coordinates></Point>`},
+	}
+
+	for _, tc := range testCases {
+		t.Run(string(tc.ewkt), func(t *testing.T) {
+			ewkb, err := ParseEWKT(tc.ewkt, geopb.DefaultGeometrySRID)
+			require.NoError(t, err)
+			encoded, err := EWKBToKML(ewkb)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, encoded)
+		})
+	}
+}
