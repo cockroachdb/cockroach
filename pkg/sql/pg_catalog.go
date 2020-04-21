43257218de68d3fe922eea5e015a45c0ae0d0c6e
Owen Qian <owenq@cockroachlabs.com>
2020-04-21 02:27:21 -0400

    sql: add empty pg_event_trigger
    The event trigger table is currently empty, but will be populated in the future
    to improve compatibility with postgres.
    
    Release note: None.
    
    Fixes #47442.
    
    Rewrite logic test

--- pkg/sql/pg_catalog.go
+++ pkg/sql/pg_catalog.go
@@ -211,6 +211,7 @@ var pgCatalog = virtualSchema{
 		sqlbase.PgCatalogDescriptionTableID:         pgCatalogDescriptionTable,
 		sqlbase.PgCatalogSharedDescriptionTableID:   pgCatalogSharedDescriptionTable,
 		sqlbase.PgCatalogEnumTableID:                pgCatalogEnumTable,
+		sqlbase.PgCatalogEventTriggerTableID:        pgCatalogEventTriggerTable,
 		sqlbase.PgCatalogExtensionTableID:           pgCatalogExtensionTable,
 		sqlbase.PgCatalogForeignDataWrapperTableID:  pgCatalogForeignDataWrapperTable,
 		sqlbase.PgCatalogForeignServerTableID:       pgCatalogForeignServerTable,
@@ -1350,6 +1351,24 @@ CREATE TABLE pg_catalog.pg_enum (
 )`,
 	populate: func(_ context.Context, p *planner, _ *DatabaseDescriptor, addRow func(...tree.Datum) error) error {
 		// Enum types are not currently supported.
+		return nil
+	},
+}
+
+var pgCatalogEventTriggerTable = virtualSchemaTable{
+	comment: `event triggers (empty - feature does not exist)
+https://www.postgresql.org/docs/9.6/catalog-pg-event-trigger.html`,
+	schema: `
+CREATE TABLE pg_catalog.pg_event_trigger (
+	evtname NAME,
+	evtevent NAME,
+	evtowner OID,
+	evtfoid OID,
+	evtenabled CHAR,
+	evttags TEXT[]
+)`,
+	populate: func(_ context.Context, p *planner, _ *DatabaseDescriptor, addRow func(...tree.Datum) error) error {
+		// Event triggers are not currently supported.
 		return nil
 	},
 }
