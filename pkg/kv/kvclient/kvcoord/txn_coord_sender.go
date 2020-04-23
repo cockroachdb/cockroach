3933c99c8674bb82855a2f8968066e1be879886e
Andrei Matei <andrei@cockroachlabs.com>
2020-04-23 19:21:47 -0400

    kvclient: crash on unexpectedly-committed txn
    This patch elevates a log.Error() to a log.Fatal(). It deals with a
    transaction being unexpectedly-finalized. We've hit this in a
    mixed-version cluster mysteriously in #47609.
    
    Touches #47609
    
    Release note: None

--- pkg/kv/kvclient/kvcoord/txn_coord_sender.go
+++ pkg/kv/kvclient/kvcoord/txn_coord_sender.go
@@ -795,12 +795,21 @@ func (tc *TxnCoordSender) updateStateLocked(
 
 	// Update our transaction with any information the error has.
 	if errTxn := pErr.GetTxn(); errTxn != nil {
-		tc.mu.txn.Update(errTxn)
+		// Sanity checks. Finalized transactions are not supposed to get here.
 		if errTxn.Status != roachpb.PENDING {
-			// We only expect TransactionAbortedError to carry an aborted txn.
-			log.Errorf(ctx, "programming error: ABORTED txn in error: %s. txn: %s", pErr, errTxn)
-			tc.cleanupTxnLocked(ctx)
+			if errTxn.Status == roachpb.COMMITTED {
+				// Finding out about our transaction being committed indicates a serious
+				// bug. Requests are not supposed to be sent on transactions after they
+				// are committed.
+				log.Fatalf(ctx, "transaction unexpectedly committed: %s. ba: %s. txn: %s.", pErr, ba, errTxn)
+			}
+			// We only expect TransactionAbortedError to carry an aborted txn. In
+			// particular, the heartbeater doesn't like running when the transaction
+			// is know to be aborted.
+			log.Fatalf(ctx, "unexpected error with ABORTED txn: (%T) %s. ba: %s. txn: %s.", pErr.GoError(), pErr, ba, errTxn)
 		}
+
+		tc.mu.txn.Update(errTxn)
 	}
 	return pErr
 }
