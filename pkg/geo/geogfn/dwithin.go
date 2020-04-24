efcec5d1da18ee174814d57440c1e44116b132c6
Oliver Tan <otan@cockroachlabs.com>
2020-04-24 16:23:09 -0700

    geo/geogfn: implement distance and dwithin for spheres using S2
    This commit adds support for Distance and DWithin using spheres. This is
    a wrapper around the S2 library, using their fancy ShapeIndex and
    MinDistance edge finders to do the dirty work.
    
    (I wish they supported giving me the two closest points so we can
    completely re-use it for spheroids, but they do not.)
    
    Release note: None

---
+++ pkg/geo/geogfn/dwithin.go
@@ -0,0 +1,70 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package geogfn
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/geo"
+	"github.com/golang/geo/s1"
+	"github.com/golang/geo/s2"
+)
+
+// DWithin returns whether a is within distance d of b, i.e. Distance(a, b) <= d.
+func DWithin(a *geo.Geography, b *geo.Geography, distance float64) (bool, error) {
+	aRegions, err := a.AsS2()
+	if err != nil {
+		return false, err
+	}
+	bRegions, err := b.AsS2()
+	if err != nil {
+		return false, err
+	}
+
+	aShapeIndex, aPoints, err := s2RegionsToPointsAndShapeIndexes(aRegions)
+	if err != nil {
+		return false, err
+	}
+	bShapeIndex, bPoints, err := s2RegionsToPointsAndShapeIndexes(bRegions)
+	if err != nil {
+		return false, err
+	}
+
+	// Find the successor to the chord angle to signify <= distance when using `IsDistanceLess`.
+	chordAngle := s1.ChordAngleFromAngle(s1.Angle(distance / wgs84SphereRadiusMeters)).Successor()
+	if aShapeIndex.Len() > 0 {
+		aQuery := s2.NewClosestEdgeQuery(aShapeIndex, nil)
+
+		if bShapeIndex.Len() > 0 {
+			if aQuery.IsDistanceLess(s2.NewMinDistanceToShapeIndexTarget(bShapeIndex), chordAngle) {
+				return true, nil
+			}
+		}
+		for _, bPoint := range bPoints {
+			if aQuery.IsDistanceLess(s2.NewMinDistanceToPointTarget(bPoint), chordAngle) {
+				return true, nil
+			}
+		}
+	}
+
+	for _, aPoint := range aPoints {
+		if bShapeIndex.Len() > 0 {
+			bQuery := s2.NewClosestEdgeQuery(bShapeIndex, nil)
+			if bQuery.IsDistanceLess(s2.NewMinDistanceToPointTarget(aPoint), chordAngle) {
+				return true, nil
+			}
+		}
+		for _, bPoint := range bPoints {
+			if s2.ChordAngleBetweenPoints(aPoint, bPoint) <= chordAngle {
+				return true, nil
+			}
+		}
+	}
+	return false, nil
+}
