bbce18042dc651bae42b7a6d59dd3a05792da5f9
Darin <darinp@gmail.com>
2020-04-24 14:18:20 -0700

    ui: do not report errors for ranges with untrusted raft log size
    Previosuly a raft log size that exceeded several times the truncation
    threshold will result in a "Raft too large" reported for each replica even
    though the Log Size is set to not be trusted.
    This lead to confusion from the customers as it seemed as if there is a problem.
    With this fix, the "Raft too large" is only reported if the raft log size is trusted.
    In the cases that it isn't trusted, the raft log size is now displayed grayed for each replica.
    
    Fixes #47569
    
    Release note (admin ui change): Fixed a bug where "Raft log too large" was reported incorrectly for replicas
    for which the raft log size is not to be trusted.

--- pkg/ui/src/views/reports/containers/range/rangeTable.tsx
+++ pkg/ui/src/views/reports/containers/range/rangeTable.tsx
@@ -153,11 +153,18 @@ export default class RangeTable extends React.Component<RangeTableProps, {}> {
     };
   }
 
-  contentBytes(bytes: Long): RangeTableCellContent {
+  contentBytes(bytes: Long, className: string = null): RangeTableCellContent {
     const humanized = Bytes(bytes.toNumber());
+    if (_.isNull(className)) {
+      return {
+        value: [humanized],
+        title: [humanized, bytes.toString()],
+      };
+    }
     return {
       value: [humanized],
       title: [humanized, bytes.toString()],
+      className: [className],
     };
   }
 
@@ -499,7 +506,10 @@ export default class RangeTable extends React.Component<RangeTableProps, {}> {
         applied: this.contentIf(!dormant, () => this.createContent(FixLong(info.raft_state.applied))),
         commit: this.contentIf(!dormant, () => this.createContent(FixLong(info.raft_state.hard_state.commit))),
         lastIndex: this.createContent(FixLong(info.state.last_index)),
-        logSize: this.contentBytes(FixLong(info.state.raft_log_size)),
+        logSize: this.contentBytes(
+          FixLong(info.state.raft_log_size),
+          info.state.raft_log_size_trusted ? "" : "range-table__cell--dormant",
+        ),
         logSizeTrusted: this.createContent(info.state.raft_log_size_trusted.toString()),
         leaseHolderQPS: leaseHolder ? this.createContent(info.stats.queries_per_second.toFixed(4)) : rangeTableEmptyContent,
         keysWrittenPS: this.createContent(info.stats.writes_per_second.toFixed(4)),
