cf9c2c847f29471d2a1586b27fa82f74377ab280
Raphael 'kena' Poss <knz@thaumogen.net>
2020-04-27 21:57:34 +0200

    cli/zip: skip over decommissioned nodes
    Release note (cli change): `debug zip` now properly skips over fully
    decommissioned nodes.

--- pkg/cli/zip_test.go
+++ pkg/cli/zip_test.go
@@ -26,7 +26,9 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/rpc"
 	"github.com/cockroachdb/cockroach/pkg/server"
+	"github.com/cockroachdb/cockroach/pkg/server/serverpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
@@ -35,6 +37,8 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
+	"github.com/cockroachdb/errors"
+	"github.com/pmezard/go-difflib/difflib"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -150,7 +154,8 @@ retrieving SQL data for crdb_internal.kv_store_status... writing: debug/crdb_int
 retrieving SQL data for crdb_internal.schema_changes... writing: debug/crdb_internal.schema_changes.txt
 retrieving SQL data for crdb_internal.partitions... writing: debug/crdb_internal.partitions.txt
 retrieving SQL data for crdb_internal.zones... writing: debug/crdb_internal.zones.txt
-requesting nodes... writing: debug/nodes/1/status.json
+requesting nodes... writing: debug/nodes.json
+writing: debug/nodes/1/status.json
 using SQL connection URL for node 1: postgresql://...
 retrieving SQL data for crdb_internal.feature_usage... writing: debug/nodes/1/crdb_internal.feature_usage.txt
 retrieving SQL data for crdb_internal.gossip_alerts... writing: debug/nodes/1/crdb_internal.gossip_alerts.txt
@@ -398,21 +403,12 @@ func TestUnavailableZip(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// Strip any non-deterministic messages:
-	re := regexp.MustCompile(`(?m)postgresql://.*$`)
-	out = re.ReplaceAllString(out, `postgresql://...`)
-	re = regexp.MustCompile(`(?m)SQL address: .*$`)
-	out = re.ReplaceAllString(out, `SQL address: ...`)
-	re = regexp.MustCompile(`(?m)log file.*$`)
-	out = re.ReplaceAllString(out, `log file ...`)
-	re = regexp.MustCompile(`(?m)RPC connection to .*$`)
-	out = re.ReplaceAllString(out, `RPC connection to ...`)
-	re = regexp.MustCompile(`(?m)\^- resulted in.*$`)
-	out = re.ReplaceAllString(out, `^- resulted in ...`)
+	// Strip any non-deterministic messages.
+	out = eraseNonDeterministicZipOutput(out)
 
 	// In order to avoid non-determinism here, we erase the output of
 	// the range retrieval.
-	re = regexp.MustCompile(`(?m)^(requesting ranges.*found|writing: debug/nodes/\d+/ranges).*\n`)
+	re := regexp.MustCompile(`(?m)^(requesting ranges.*found|writing: debug/nodes/\d+/ranges).*\n`)
 	out = re.ReplaceAllString(out, ``)
 
 	const expected = `debug zip ` + os.DevNull + ` --timeout=.5s
@@ -452,7 +448,7 @@ retrieving SQL data for crdb_internal.partitions... writing: debug/crdb_internal
   ^- resulted in ...
 retrieving SQL data for crdb_internal.zones... writing: debug/crdb_internal.zones.txt.err.txt
   ^- resulted in ...
-requesting nodes... writing: debug/nodes.err.txt
+requesting nodes... writing: debug/nodes.json.err.txt
   ^- resulted in ...
 writing: debug/nodes/1/status.json
 using SQL connection URL for node 1: postgresql://...
@@ -485,6 +481,20 @@ requesting list of SQL databases... writing: debug/schema.err.txt
   ^- resulted in ...
 `
 	assert.Equal(t, expected, out)
+}
+
+func eraseNonDeterministicZipOutput(out string) string {
+	re := regexp.MustCompile(`(?m)postgresql://.*$`)
+	out = re.ReplaceAllString(out, `postgresql://...`)
+	re = regexp.MustCompile(`(?m)SQL address: .*$`)
+	out = re.ReplaceAllString(out, `SQL address: ...`)
+	re = regexp.MustCompile(`(?m)log file.*$`)
+	out = re.ReplaceAllString(out, `log file ...`)
+	re = regexp.MustCompile(`(?m)RPC connection to .*$`)
+	out = re.ReplaceAllString(out, `RPC connection to ...`)
+	re = regexp.MustCompile(`(?m)\^- resulted in.*$`)
+	out = re.ReplaceAllString(out, `^- resulted in ...`)
+	return out
 }
 
 // This tests the operation of zip over partial clusters.
@@ -527,17 +537,8 @@ func TestPartialZip(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// Strip any non-deterministic messages:
-	re := regexp.MustCompile(`(?m)postgresql://.*$`)
-	out = re.ReplaceAllString(out, `postgresql://...`)
-	re = regexp.MustCompile(`(?m)SQL address: .*$`)
-	out = re.ReplaceAllString(out, `SQL address: ...`)
-	re = regexp.MustCompile(`(?m)log file.*$`)
-	out = re.ReplaceAllString(out, `log file ...`)
-	re = regexp.MustCompile(`(?m)RPC connection to .*$`)
-	out = re.ReplaceAllString(out, `RPC connection to ...`)
-	re = regexp.MustCompile(`(?m)\^- resulted in.*$`)
-	out = re.ReplaceAllString(out, `^- resulted in ...`)
+	// Strip any non-deterministic messages.
+	out = eraseNonDeterministicZipOutput(out)
 
 	const expected = `debug zip ` + os.DevNull + `
 establishing RPC connection to ...
@@ -564,7 +565,8 @@ retrieving SQL data for crdb_internal.kv_store_status... writing: debug/crdb_int
 retrieving SQL data for crdb_internal.schema_changes... writing: debug/crdb_internal.schema_changes.txt
 retrieving SQL data for crdb_internal.partitions... writing: debug/crdb_internal.partitions.txt
 retrieving SQL data for crdb_internal.zones... writing: debug/crdb_internal.zones.txt
-requesting nodes... writing: debug/nodes/1/status.json
+requesting nodes... writing: debug/nodes.json
+writing: debug/nodes/1/status.json
 using SQL connection URL for node 1: postgresql://...
 retrieving SQL data for crdb_internal.feature_usage... writing: debug/nodes/1/crdb_internal.feature_usage.txt
 retrieving SQL data for crdb_internal.gossip_alerts... writing: debug/nodes/1/crdb_internal.gossip_alerts.txt
@@ -769,6 +771,228 @@ requesting table details for system.web_sessions... writing: debug/schema/system
 requesting table details for system.zones... writing: debug/schema/system/zones.json
 `
 	assert.Equal(t, expected, out)
+
+	// Now mark the stopped node as decommissioned, and check that zip
+	// skips over it.
+	s := tc.Server(0)
+	conn, err := s.RPCContext().GRPCDialNode(s.ServingRPCAddr(), s.NodeID(),
+		rpc.DefaultClass).Connect(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	as := serverpb.NewAdminClient(conn)
+	req := &serverpb.DecommissionRequest{
+		NodeIDs:         []roachpb.NodeID{2},
+		Decommissioning: true,
+	}
+	if _, err := as.Decommission(context.Background(), req); err != nil {
+		t.Fatal(err)
+	}
+	kvserver.TimeUntilStoreDead.Override(&s.ClusterSettings().SV, kvserver.TestTimeUntilStoreDead)
+
+	testutils.SucceedsSoon(t, func() error {
+		out, err = c.RunWithCapture("debug zip " + os.DevNull)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Strip any non-deterministic messages.
+		out = eraseNonDeterministicZipOutput(out)
+
+		const expected2 = `debug zip ` + os.DevNull + `
+establishing RPC connection to ...
+retrieving the node status to get the SQL address...
+using SQL address: ...
+using SQL connection URL: postgresql://...
+writing ` + os.DevNull + `
+requesting data for debug/events... writing: debug/events.json
+requesting data for debug/rangelog... writing: debug/rangelog.json
+requesting data for debug/liveness... writing: debug/liveness.json
+requesting data for debug/settings... writing: debug/settings.json
+requesting data for debug/reports/problemranges... writing: debug/reports/problemranges.json
+retrieving SQL data for crdb_internal.cluster_queries... writing: debug/crdb_internal.cluster_queries.txt
+retrieving SQL data for crdb_internal.cluster_sessions... writing: debug/crdb_internal.cluster_sessions.txt
+retrieving SQL data for crdb_internal.cluster_settings... writing: debug/crdb_internal.cluster_settings.txt
+retrieving SQL data for crdb_internal.cluster_transactions... writing: debug/crdb_internal.cluster_transactions.txt
+retrieving SQL data for crdb_internal.jobs... writing: debug/crdb_internal.jobs.txt
+retrieving SQL data for system.jobs... writing: debug/system.jobs.txt
+retrieving SQL data for system.descriptor... writing: debug/system.descriptor.txt
+retrieving SQL data for system.namespace... writing: debug/system.namespace.txt
+retrieving SQL data for system.namespace2... writing: debug/system.namespace2.txt
+retrieving SQL data for crdb_internal.kv_node_status... writing: debug/crdb_internal.kv_node_status.txt
+retrieving SQL data for crdb_internal.kv_store_status... writing: debug/crdb_internal.kv_store_status.txt
+retrieving SQL data for crdb_internal.schema_changes... writing: debug/crdb_internal.schema_changes.txt
+retrieving SQL data for crdb_internal.partitions... writing: debug/crdb_internal.partitions.txt
+retrieving SQL data for crdb_internal.zones... writing: debug/crdb_internal.zones.txt
+requesting nodes... writing: debug/nodes.json
+writing: debug/nodes/1/status.json
+using SQL connection URL for node 1: postgresql://...
+retrieving SQL data for crdb_internal.feature_usage... writing: debug/nodes/1/crdb_internal.feature_usage.txt
+retrieving SQL data for crdb_internal.gossip_alerts... writing: debug/nodes/1/crdb_internal.gossip_alerts.txt
+retrieving SQL data for crdb_internal.gossip_liveness... writing: debug/nodes/1/crdb_internal.gossip_liveness.txt
+retrieving SQL data for crdb_internal.gossip_network... writing: debug/nodes/1/crdb_internal.gossip_network.txt
+retrieving SQL data for crdb_internal.gossip_nodes... writing: debug/nodes/1/crdb_internal.gossip_nodes.txt
+retrieving SQL data for crdb_internal.leases... writing: debug/nodes/1/crdb_internal.leases.txt
+retrieving SQL data for crdb_internal.node_build_info... writing: debug/nodes/1/crdb_internal.node_build_info.txt
+retrieving SQL data for crdb_internal.node_metrics... writing: debug/nodes/1/crdb_internal.node_metrics.txt
+retrieving SQL data for crdb_internal.node_queries... writing: debug/nodes/1/crdb_internal.node_queries.txt
+retrieving SQL data for crdb_internal.node_runtime_info... writing: debug/nodes/1/crdb_internal.node_runtime_info.txt
+retrieving SQL data for crdb_internal.node_sessions... writing: debug/nodes/1/crdb_internal.node_sessions.txt
+retrieving SQL data for crdb_internal.node_statement_statistics... writing: debug/nodes/1/crdb_internal.node_statement_statistics.txt
+retrieving SQL data for crdb_internal.node_transactions... writing: debug/nodes/1/crdb_internal.node_transactions.txt
+retrieving SQL data for crdb_internal.node_txn_stats... writing: debug/nodes/1/crdb_internal.node_txn_stats.txt
+requesting data for debug/nodes/1/details... writing: debug/nodes/1/details.json
+requesting data for debug/nodes/1/gossip... writing: debug/nodes/1/gossip.json
+requesting data for debug/nodes/1/enginestats... writing: debug/nodes/1/enginestats.json
+requesting stacks for node 1... writing: debug/nodes/1/stacks.txt
+requesting threads for node 1... writing: debug/nodes/1/threads.txt
+requesting heap profile for node 1... writing: debug/nodes/1/heap.pprof
+requesting heap files for node 1... writing: debug/nodes/1/heapprof.err.txt
+  ^- resulted in ...
+requesting goroutine files for node 1... writing: debug/nodes/1/goroutines.err.txt
+  ^- resulted in ...
+requesting log file ...
+requesting ranges... 32 found
+writing: debug/nodes/1/ranges/1.json
+writing: debug/nodes/1/ranges/2.json
+writing: debug/nodes/1/ranges/3.json
+writing: debug/nodes/1/ranges/4.json
+writing: debug/nodes/1/ranges/5.json
+writing: debug/nodes/1/ranges/6.json
+writing: debug/nodes/1/ranges/7.json
+writing: debug/nodes/1/ranges/8.json
+writing: debug/nodes/1/ranges/9.json
+writing: debug/nodes/1/ranges/10.json
+writing: debug/nodes/1/ranges/11.json
+writing: debug/nodes/1/ranges/12.json
+writing: debug/nodes/1/ranges/13.json
+writing: debug/nodes/1/ranges/14.json
+writing: debug/nodes/1/ranges/15.json
+writing: debug/nodes/1/ranges/16.json
+writing: debug/nodes/1/ranges/17.json
+writing: debug/nodes/1/ranges/18.json
+writing: debug/nodes/1/ranges/19.json
+writing: debug/nodes/1/ranges/20.json
+writing: debug/nodes/1/ranges/21.json
+writing: debug/nodes/1/ranges/22.json
+writing: debug/nodes/1/ranges/23.json
+writing: debug/nodes/1/ranges/24.json
+writing: debug/nodes/1/ranges/25.json
+writing: debug/nodes/1/ranges/26.json
+writing: debug/nodes/1/ranges/27.json
+writing: debug/nodes/1/ranges/28.json
+writing: debug/nodes/1/ranges/29.json
+writing: debug/nodes/1/ranges/30.json
+writing: debug/nodes/1/ranges/31.json
+writing: debug/nodes/1/ranges/32.json
+writing: debug/nodes/3/status.json
+using SQL connection URL for node 3: postgresql://...
+retrieving SQL data for crdb_internal.feature_usage... writing: debug/nodes/3/crdb_internal.feature_usage.txt
+retrieving SQL data for crdb_internal.gossip_alerts... writing: debug/nodes/3/crdb_internal.gossip_alerts.txt
+retrieving SQL data for crdb_internal.gossip_liveness... writing: debug/nodes/3/crdb_internal.gossip_liveness.txt
+retrieving SQL data for crdb_internal.gossip_network... writing: debug/nodes/3/crdb_internal.gossip_network.txt
+retrieving SQL data for crdb_internal.gossip_nodes... writing: debug/nodes/3/crdb_internal.gossip_nodes.txt
+retrieving SQL data for crdb_internal.leases... writing: debug/nodes/3/crdb_internal.leases.txt
+retrieving SQL data for crdb_internal.node_build_info... writing: debug/nodes/3/crdb_internal.node_build_info.txt
+retrieving SQL data for crdb_internal.node_metrics... writing: debug/nodes/3/crdb_internal.node_metrics.txt
+retrieving SQL data for crdb_internal.node_queries... writing: debug/nodes/3/crdb_internal.node_queries.txt
+retrieving SQL data for crdb_internal.node_runtime_info... writing: debug/nodes/3/crdb_internal.node_runtime_info.txt
+retrieving SQL data for crdb_internal.node_sessions... writing: debug/nodes/3/crdb_internal.node_sessions.txt
+retrieving SQL data for crdb_internal.node_statement_statistics... writing: debug/nodes/3/crdb_internal.node_statement_statistics.txt
+retrieving SQL data for crdb_internal.node_transactions... writing: debug/nodes/3/crdb_internal.node_transactions.txt
+retrieving SQL data for crdb_internal.node_txn_stats... writing: debug/nodes/3/crdb_internal.node_txn_stats.txt
+requesting data for debug/nodes/3/details... writing: debug/nodes/3/details.json
+requesting data for debug/nodes/3/gossip... writing: debug/nodes/3/gossip.json
+requesting data for debug/nodes/3/enginestats... writing: debug/nodes/3/enginestats.json
+requesting stacks for node 3... writing: debug/nodes/3/stacks.txt
+requesting threads for node 3... writing: debug/nodes/3/threads.txt
+requesting heap profile for node 3... writing: debug/nodes/3/heap.pprof
+requesting heap files for node 3... writing: debug/nodes/3/heapprof.err.txt
+  ^- resulted in ...
+requesting goroutine files for node 3... writing: debug/nodes/3/goroutines.err.txt
+  ^- resulted in ...
+requesting log file ...
+requesting ranges... 32 found
+writing: debug/nodes/3/ranges/1.json
+writing: debug/nodes/3/ranges/2.json
+writing: debug/nodes/3/ranges/3.json
+writing: debug/nodes/3/ranges/4.json
+writing: debug/nodes/3/ranges/5.json
+writing: debug/nodes/3/ranges/6.json
+writing: debug/nodes/3/ranges/7.json
+writing: debug/nodes/3/ranges/8.json
+writing: debug/nodes/3/ranges/9.json
+writing: debug/nodes/3/ranges/10.json
+writing: debug/nodes/3/ranges/11.json
+writing: debug/nodes/3/ranges/12.json
+writing: debug/nodes/3/ranges/13.json
+writing: debug/nodes/3/ranges/14.json
+writing: debug/nodes/3/ranges/15.json
+writing: debug/nodes/3/ranges/16.json
+writing: debug/nodes/3/ranges/17.json
+writing: debug/nodes/3/ranges/18.json
+writing: debug/nodes/3/ranges/19.json
+writing: debug/nodes/3/ranges/20.json
+writing: debug/nodes/3/ranges/21.json
+writing: debug/nodes/3/ranges/22.json
+writing: debug/nodes/3/ranges/23.json
+writing: debug/nodes/3/ranges/24.json
+writing: debug/nodes/3/ranges/25.json
+writing: debug/nodes/3/ranges/26.json
+writing: debug/nodes/3/ranges/27.json
+writing: debug/nodes/3/ranges/28.json
+writing: debug/nodes/3/ranges/29.json
+writing: debug/nodes/3/ranges/30.json
+writing: debug/nodes/3/ranges/31.json
+writing: debug/nodes/3/ranges/32.json
+requesting list of SQL databases... 3 found
+requesting database details for defaultdb... writing: debug/schema/defaultdb@details.json
+0 tables found
+requesting database details for postgres... writing: debug/schema/postgres@details.json
+0 tables found
+requesting database details for system... writing: debug/schema/system@details.json
+26 tables found
+requesting table details for system.comments... writing: debug/schema/system/comments.json
+requesting table details for system.descriptor... writing: debug/schema/system/descriptor.json
+requesting table details for system.eventlog... writing: debug/schema/system/eventlog.json
+requesting table details for system.jobs... writing: debug/schema/system/jobs.json
+requesting table details for system.lease... writing: debug/schema/system/lease.json
+requesting table details for system.locations... writing: debug/schema/system/locations.json
+requesting table details for system.namespace... writing: debug/schema/system/namespace.json
+requesting table details for system.namespace2... writing: debug/schema/system/namespace2.json
+requesting table details for system.protected_ts_meta... writing: debug/schema/system/protected_ts_meta.json
+requesting table details for system.protected_ts_records... writing: debug/schema/system/protected_ts_records.json
+requesting table details for system.rangelog... writing: debug/schema/system/rangelog.json
+requesting table details for system.replication_constraint_stats... writing: debug/schema/system/replication_constraint_stats.json
+requesting table details for system.replication_critical_localities... writing: debug/schema/system/replication_critical_localities.json
+requesting table details for system.replication_stats... writing: debug/schema/system/replication_stats.json
+requesting table details for system.reports_meta... writing: debug/schema/system/reports_meta.json
+requesting table details for system.role_members... writing: debug/schema/system/role_members.json
+requesting table details for system.role_options... writing: debug/schema/system/role_options.json
+requesting table details for system.settings... writing: debug/schema/system/settings.json
+requesting table details for system.statement_bundle_chunks... writing: debug/schema/system/statement_bundle_chunks.json
+requesting table details for system.statement_diagnostics... writing: debug/schema/system/statement_diagnostics.json
+requesting table details for system.statement_diagnostics_requests... writing: debug/schema/system/statement_diagnostics_requests.json
+requesting table details for system.table_statistics... writing: debug/schema/system/table_statistics.json
+requesting table details for system.ui... writing: debug/schema/system/ui.json
+requesting table details for system.users... writing: debug/schema/system/users.json
+requesting table details for system.web_sessions... writing: debug/schema/system/web_sessions.json
+requesting table details for system.zones... writing: debug/schema/system/zones.json
+`
+		if out != expected2 {
+			diff, _ := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
+				A:        difflib.SplitLines(expected2),
+				B:        difflib.SplitLines(out),
+				FromFile: "Expected",
+				FromDate: "",
+				ToFile:   "Actual",
+				ToDate:   "",
+				Context:  1,
+			})
+			return errors.Newf("Diff:\n%s", diff)
+		}
+		return nil
+	})
 }
 
 // This test the operation of zip over secure clusters.
