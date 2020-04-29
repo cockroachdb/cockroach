bde12bbe54b4eb61d653b88cad0d718f43222da7
Raphael 'kena' Poss <knz@thaumogen.net>
2020-04-29 15:14:36 +0200

    cli/zip: makes it possible to include/exclude specific nodes
    Release note (cli change): `cockroach debug zip` now supports two
    command-line parameters `--nodes` and `--exclude-nodes`. When
    specified, they control which nodes are inspected when gathering the
    data. This makes it possible to focus on a group of nodes of interest
    in a large cluster, or to exclude nodes that `debug zip` would have
    trouble reaching otherwise. Both flags accept a list of individual
    node IDs or ranges of node IDs, e.g. `--nodes=1,10,13-15`.

--- pkg/cli/flags.go
+++ pkg/cli/flags.go
@@ -552,6 +552,13 @@ func init() {
 		}
 	}
 
+	// Zip command.
+	{
+		f := debugZipCmd.Flags()
+		VarFlag(f, &zipCtx.nodes.inclusive, cliflags.ZipNodes)
+		VarFlag(f, &zipCtx.nodes.exclusive, cliflags.ZipExcludeNodes)
+	}
+
 	// Decommission command.
 	VarFlag(decommissionNodeCmd.Flags(), &nodeCtx.nodeDecommissionWait, cliflags.Wait)
 
