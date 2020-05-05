30c26061e20d9682172481185f9e6b0692c0b176
Oliver Tan <otan@cockroachlabs.com>
2020-05-05 07:53:38 -0700

    geo/geogfn: implement DWithin, Distance for spheroids
    This commit introduces calculating Distance and DWithin for spheroids.
    This is rolled out by hand as the S2 primitives were not available to do
    this for us. Some minor optimizations have been made, but there are some
    TODOs left in there for a future date.
    
    It is important to note this is not the true minimum distance, but is
    the minimum distance that is in line with the PostGIS definition. This
    is commented on the `minDistance` function.
    
    Also made a minor speed boost improvement for geographiclib, only
    initializing the spheroid once.
    
    Release note: None

--- pkg/geo/geographiclib/geographiclib.go
+++ pkg/geo/geographiclib/geographiclib.go
@@ -14,7 +14,7 @@ package geographiclib
 // #cgo CXXFLAGS: -std=c++14
 // #cgo LDFLAGS: -lm
 //
-// #include "geographiclib.h"
+// #include "geodesic.h"
 import "C"
 
 import "github.com/golang/geo/s2"
@@ -27,22 +27,31 @@ var (
 // Spheroid is an object that can perform geodesic operations
 // on a given spheroid.
 type Spheroid struct {
-	Radius     float64
-	Flattening float64
+	cRepr        C.struct_geod_geodesic
+	Radius       float64
+	Flattening   float64
+	SphereRadius float64
 }
 
 // MakeSpheroid creates a spheroid from a radius and flattening.
 func MakeSpheroid(radius float64, flattening float64) Spheroid {
-	return Spheroid{Radius: radius, Flattening: flattening}
+	minorAxis := radius - radius*flattening
+	s := Spheroid{
+		Radius:       radius,
+		Flattening:   flattening,
+		SphereRadius: (radius*2 + minorAxis) / 3,
+	}
+	C.geod_init(&s.cRepr, C.double(radius), C.double(flattening))
+	return s
 }
 
-// Inverse solves the inverse geodesic problem on the given spheroid.
+// Inverse solves the geodetic inverse problem on the given spheroid
+// (https://en.wikipedia.org/wiki/Geodesy#Geodetic_problems).
 // Returns s12 (distance in meters), az1 (azimuth to point 1) and az2 (azimuth to point 2).
-func (s Spheroid) Inverse(a, b s2.LatLng) (s12, az1, az2 float64) {
+func (s *Spheroid) Inverse(a, b s2.LatLng) (s12, az1, az2 float64) {
 	var retS12, retAZ1, retAZ2 C.double
-	C.CR_GEOGRAPHICLIB_Inverse(
-		C.double(s.Radius),
-		C.double(s.Flattening),
+	C.geod_inverse(
+		&s.cRepr,
 		C.double(a.Lat.Degrees()),
 		C.double(a.Lng.Degrees()),
 		C.double(b.Lat.Degrees()),
