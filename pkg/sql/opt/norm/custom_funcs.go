53948bb14b97e2a59fa8655021f88722d510193d
Drew Kimball <andrewekimball@gmail.com>
2020-05-07 18:15:11 -0700

    opt: modify ConvertZipArraysToValues to handle json_array_elements
    Previously, ConvertZipArraysToValues only handled cases where the unnest
    function was called on either an ArrayExpr or a ConstExpr wrapping a
    DArray.
    
    This patch allows ConvertZipArraysToValues to also handle the
    json_array_elements and jsonb_array_elements functions if they have
    a single ConstExpr parameter that wraps a DJSON datum.
    
    A query like this:
    ```
    SELECT unnest(ARRAY[1,2,3,4,5]), json_array_elements('[{"a": "b"}, {"x": "y"}]');
    ```
    Will now be unnested into this:
    ```
    values
     ├── columns: unnest:1!null json_array_elements:2
     ├── cardinality: [5 - 5]
     ├── (1, '{"a": "b"}')
     ├── (2, '{"x": "y"}')
     ├── (3, NULL)
     ├── (4, NULL)
     └── (5, NULL)
    ```
    
    Release note: None

--- pkg/sql/opt/norm/custom_funcs.go
+++ pkg/sql/opt/norm/custom_funcs.go
@@ -1165,7 +1165,7 @@ func (c *CustomFuncs) ProjectExtraCol(
 //
 // 	3. There is at least one row.
 //
-// 	4. All tuples in the single column are either TupleExpr's or ConstExpr's
+// 	4. All tuples in the single column are either TupleExprs or ConstExprs
 //     that wrap DTuples, as opposed to dynamically generated tuples.
 //
 func (c *CustomFuncs) CanUnnestTuplesFromValues(expr memo.RelExpr) bool {
@@ -1702,22 +1702,34 @@ func (c *CustomFuncs) ZipOuterCols(zip memo.ZipExpr) opt.ColSet {
 	return colSet
 }
 
+// unnestFuncs maps function names that are supported by
+// ConvertZipArraysToValues
+var unnestFuncs = map[string]struct{}{
+	"unnest":               {},
+	"json_array_elements":  {},
+	"jsonb_array_elements": {},
+}
+
 // CanConstructValuesFromZips takes in an input ZipExpr and returns true if the
 // ProjectSet to which the zip belongs can be converted to an InnerJoinApply
 // with a Values operator on the right input.
 func (c *CustomFuncs) CanConstructValuesFromZips(zip memo.ZipExpr) bool {
-	for _, zipItem := range zip {
-		fn, ok := zipItem.Fn.(*memo.FunctionExpr)
-		if !ok || fn.Name != "unnest" {
-			// Not an unnest function.
+	for i := range zip {
+		fn, ok := zip[i].Fn.(*memo.FunctionExpr)
+		if !ok {
+			// Not a FunctionExpr.
+			return false
+		}
+		if _, ok := unnestFuncs[fn.Name]; !ok {
+			// Not a supported function.
 			return false
 		}
 		if len(fn.Args) != 1 {
-			// Unnest has more than one argument.
+			// Function has more than one argument.
 			return false
 		}
-		if !c.IsStaticArray(fn.Args[0]) {
-			// Unnest argument is not an ArrayExpr or ConstExpr wrapping a DArray.
+		if !c.IsStaticArray(fn.Args[0]) && !c.WrapsJSONArray(fn.Args[0]) {
+			// Argument is not an ArrayExpr or ConstExpr wrapping a DArray or DJSON.
 			return false
 		}
 	}
@@ -1725,12 +1737,14 @@ func (c *CustomFuncs) CanConstructValuesFromZips(zip memo.ZipExpr) bool {
 }
 
 // ConstructValuesFromZips constructs a Values operator with the elements from
-// the given ArrayExpr(s) (or ConstExpr(s) that wrap a DArray) in the given
-// ZipExpr.
+// the given ArrayExpr(s) or the ConstExpr(s) that wrap a DArray or DJSON in the
+// given ZipExpr.
 //
-// The functions contained in the ZipExpr must be unnest functions with a single
-// parameter each. The parameters of the unnest functions must be either
-// ArrayExpr's or ConstExpr's wrapping DArrays.
+// The functions contained in the ZipExpr must be unnest, json_array_elements or
+// jsonb_array_elements functions with a single parameter each. The parameters
+// of the unnest functions must be either ArrayExprs or ConstExprs wrapping
+// DArrays. The parameters of the json_array_elements functions must be
+// ConstExprs wrapping DJSON datums.
 func (c *CustomFuncs) ConstructValuesFromZips(zip memo.ZipExpr) memo.RelExpr {
 	numCols := len(zip)
 	outColTypes := make([]*types.T, numCols)
@@ -1738,17 +1752,27 @@ func (c *CustomFuncs) ConstructValuesFromZips(zip memo.ZipExpr) memo.RelExpr {
 	var outRows []memo.ScalarListExpr
 
 	// Get type and ColumnID of each column.
-	for i, zipItem := range zip {
-		arrExpr := zipItem.Fn.(*memo.FunctionExpr).Args[0]
-		outColTypes[i] = arrExpr.DataType().ArrayContents()
-		outColIDs[i] = zipItem.Cols[0]
+	for i := range zip {
+		function := zip[i].Fn.(*memo.FunctionExpr)
+		expr := function.Args[0]
+		switch function.Name {
+		case "unnest":
+			outColTypes[i] = expr.DataType().ArrayContents()
+
+		case "json_array_elements", "jsonb_array_elements":
+			outColTypes[i] = types.Jsonb
+
+		default:
+			panic(errors.AssertionFailedf("invalid function name: %v", function.Name))
+		}
+		outColIDs[i] = zip[i].Cols[0]
 	}
 
 	// addValToOutRows inserts a value into outRows at the given index.
 	addValToOutRows := func(expr opt.ScalarExpr, rIndex, cIndex int) {
 		if rIndex >= len(outRows) {
 			// If this is the largest column encountered so far, make a new row and
-			// fill with NullExpr's.
+			// fill with NullExprs.
 			outRows = append(outRows, make(memo.ScalarListExpr, numCols))
 			for i := 0; i < numCols; i++ {
 				outRows[rIndex][i] = c.f.ConstructNull(outColTypes[cIndex])
@@ -1758,24 +1782,51 @@ func (c *CustomFuncs) ConstructValuesFromZips(zip memo.ZipExpr) memo.RelExpr {
 	}
 
 	// Fill outRows with values from the arrays in the ZipExpr.
-	for i, zipItem := range zip {
-		arrExpr := zipItem.Fn.(*memo.FunctionExpr).Args[0]
-		switch t := arrExpr.(type) {
+	for i := range zip {
+		function := zip[i].Fn.(*memo.FunctionExpr)
+		param := function.Args[0]
+		switch t := param.(type) {
 		case *memo.ArrayExpr:
 			for j, val := range t.Elems {
 				addValToOutRows(val, j, i)
 			}
 
 		case *memo.ConstExpr:
-			dArray := t.Value.(*tree.DArray)
-			for j, elem := range dArray.Array {
-				val := c.f.ConstructConstVal(elem, dArray.ParamTyp)
+			// Use a ValueGenerator to retrieve values from the datums wrapped
+			// in the ConstExpr. These generators are used at runtime to unnest
+			// values from regular and JSON arrays.
+			generator, err := function.Overload.Generator(c.f.evalCtx, tree.Datums{t.Value})
+			if err != nil {
+				panic(errors.AssertionFailedf("generator retrieval failed: %v", err))
+			}
+			if err = generator.Start(c.f.evalCtx.Context, c.f.evalCtx.Txn); err != nil {
+				panic(errors.AssertionFailedf("generator.Start failed: %v", err))
+			}
+
+			for j := 0; ; j++ {
+				hasNext, err := generator.Next(c.f.evalCtx.Context)
+				if err != nil {
+					panic(errors.AssertionFailedf("generator.Next failed: %v", err))
+				}
+				if !hasNext {
+					break
+				}
+
+				vals, err := generator.Values()
+				if err != nil || len(vals) != 1 {
+					panic(errors.AssertionFailedf("failed to retrieve values: %v", err))
+				}
+				val := c.f.ConstructConstVal(vals[0], vals[0].ResolvedType())
 				addValToOutRows(val, j, i)
 			}
+			generator.Close()
+
+		default:
+			panic(errors.AssertionFailedf("invalid parameter type"))
 		}
 	}
 
-	// Convert outRows (a slice of ScalarListExpr's) into a ScalarListExpr
+	// Convert outRows (a slice of ScalarListExprs) into a ScalarListExpr
 	// containing a tuple for each row.
 	tuples := make(memo.ScalarListExpr, len(outRows))
 	for i, row := range outRows {
@@ -2197,6 +2248,17 @@ func (c *CustomFuncs) AllowNullArgs(op opt.Operator, left, right opt.ScalarExpr)
 func (c *CustomFuncs) IsJSONScalar(value opt.ScalarExpr) bool {
 	v := value.(*memo.ConstExpr).Value.(*tree.DJSON)
 	return v.JSON.Type() != json.ObjectJSONType && v.JSON.Type() != json.ArrayJSONType
+}
+
+// WrapsJSONArray returns true if the given ScalarExpr is a ConstExpr that wraps
+// a DJSON datum that contains a JSON array.
+func (c *CustomFuncs) WrapsJSONArray(expr opt.ScalarExpr) bool {
+	if constExpr, ok := expr.(*memo.ConstExpr); ok {
+		if dJSON, ok := constExpr.Value.(*tree.DJSON); ok {
+			return dJSON.JSON.Type() == json.ArrayJSONType
+		}
+	}
+	return false
 }
 
 // MakeSingleKeyJSONObject returns a JSON object with one entry, mapping key to value.
