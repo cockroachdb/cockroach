cockroachdb/cockroach

commit 4047d45256b8cc7291d0167916115066d4f09f90

sql: populate pg_catalog.pg_aggregate table
Fixes https://github.com/cockroachdb/cockroach/issues/44616

This commit added virtual Schema and populated pg_catalog.pg_aggregate table,
along with its respective testcases.

Release justification: low-risk functionality addition.

Release note (sql change): This PR adds virtual Schema and populate
pg_catalog.pg_aggregate table.

pkg/sql/pg_catalog.go
@@ -228,6 +228,7 @@ var pgCatalog = virtualSchema{
 		sqlbase.PgCatalogPreparedStatementsTableID:  pgCatalogPreparedStatementsTable,
 		sqlbase.PgCatalogPreparedXactsTableID:       pgCatalogPreparedXactsTable,
 		sqlbase.PgCatalogProcTableID:                pgCatalogProcTable,
+		sqlbase.PgCatalogAggregateTableID:           pgCatalogAggregateTable,
 		sqlbase.PgCatalogRangeTableID:               pgCatalogRangeTable,
 		sqlbase.PgCatalogRewriteTableID:             pgCatalogRewriteTable,
 		sqlbase.PgCatalogRolesTableID:               pgCatalogRolesTable,
@@ -2936,6 +2937,104 @@ CREATE TABLE pg_catalog.pg_views (
 					tree.DNull,                      // viewowner
 					tree.NewDString(desc.ViewQuery), // definition
 				)
+			})
+	},
+}
+
+var pgCatalogAggregateTable = virtualSchemaTable{
+	comment: `aggregated built-in functions (incomplete)
+https://www.postgresql.org/docs/9.6/catalog-pg-aggregate.html`,
+	schema: `
+CREATE TABLE pg_catalog.pg_aggregate (
+	aggfnoid REGPROC,
+	aggkind  CHAR,
+	aggnumdirectargs INT2,
+	aggtransfn REGPROC,
+	aggfinalfn REGPROC,
+	aggcombinefn REGPROC,
+	aggserialfn REGPROC,
+	aggdeserialfn REGPROC,
+	aggmtransfn REGPROC,
+	aggminvtransfn REGPROC,
+	aggmfinalfn REGPROC,
+	aggfinalextra BOOL,
+	aggmfinalextra BOOL,
+	aggsortop OID,
+	aggtranstype OID,
+	aggtransspace INT4,
+	aggmtranstype OID,
+	aggmtransspace INT4,
+	agginitval TEXT,
+	aggminitval TEXT
+)
+`,
+	populate: func(ctx context.Context, p *planner, dbContext *DatabaseDescriptor, addRow func(...tree.Datum) error) error {
+		h := makeOidHasher()
+		return forEachDatabaseDesc(ctx, p, dbContext, false, /* requiresPrivileges */
+			func(db *DatabaseDescriptor) error {
+				for _, name := range builtins.AllAggregateBuiltinNames {
+					if name == builtins.AnyNotNull {
+						// any_not_null is treated as a special case.
+						continue
+					}
+					_, overloads := builtins.GetBuiltinProperties(name)
+					for _, overload := range overloads {
+						params, _ := tree.GetParamsAndReturnType(overload)
+						sortOperatorOid := oidZero
+						aggregateKind := tree.NewDString("n")
+						aggNumDirectArgs := zeroVal
+						if params.Length() != 0 {
+							argType := tree.NewDOid(tree.DInt(params.Types()[0].Oid()))
+							returnType := tree.NewDOid(tree.DInt(oid.T_bool))
+							switch name {
+							// Cases to determine sort operator.
+							case "max", "bool_or":
+								sortOperatorOid = h.OperatorOid(">", argType, argType, returnType)
+							case "min", "bool_and", "every":
+								sortOperatorOid = h.OperatorOid("<", argType, argType, returnType)
+
+							// Cases to determine aggregate kind.
+							case "rank", "percent_rank", "cume_dict", "dense_rank":
+								aggregateKind = tree.NewDString("h")
+								aggNumDirectArgs = tree.NewDInt(1)
+							case "mode":
+								aggregateKind = tree.NewDString("o")
+							default:
+								if strings.HasPrefix(name, "percentile_") {
+									aggregateKind = tree.NewDString("o")
+									aggNumDirectArgs = tree.NewDInt(1)
+								}
+							}
+						}
+						regprocForZeroOid := oidZero.AsRegProc("-")
+						err := addRow(
+							h.BuiltinOid(name, &overload).AsRegProc(name), // aggfnoid
+							aggregateKind,     // aggkind
+							aggNumDirectArgs,  // aggnumdirectargs
+							regprocForZeroOid, // aggtransfn
+							regprocForZeroOid, // aggfinalfn
+							regprocForZeroOid, // aggcombinefn
+							regprocForZeroOid, // aggserialfn
+							regprocForZeroOid, // aggdeserialfn
+							regprocForZeroOid, // aggmtransfn
+							regprocForZeroOid, // aggminvtransfn
+							regprocForZeroOid, // aggmfinalfn
+							tree.DBoolFalse,   // aggfinalextra
+							tree.DBoolFalse,   // aggmfinalextra
+							sortOperatorOid,   // aggsortop
+							tree.DNull,        // aggtranstype
+							tree.DNull,        // aggtransspace
+							tree.DNull,        // aggmtranstype
+							tree.DNull,        // aggmtransspace
+							tree.DNull,        // agginitval
+							tree.DNull,        // aggminitval
+						)
+						if err != nil {
+							return err
+						}
+					}
+				}
+				return nil
 			})
 	},
 }

pkg/sql/logictest/testdata/logic_test/grant_table
@@ -95,6 +95,7 @@ test           information_schema  user_privileges                    public   S
 test           information_schema  views                              public   SELECT
 test           pg_catalog          NULL                               admin    ALL
 test           pg_catalog          NULL                               root     ALL
+test           pg_catalog          pg_aggregate                       public   SELECT
 test           pg_catalog          pg_am                              public   SELECT
 test           pg_catalog          pg_attrdef                         public   SELECT
 test           pg_catalog          pg_attribute                       public   SELECT

pkg/sql/logictest/testdata/logic_test/information_schema
@@ -272,6 +272,7 @@ information_schema  table_privileges
 information_schema  tables
 information_schema  user_privileges
 information_schema  views
+pg_catalog          pg_aggregate
 pg_catalog          pg_am
 pg_catalog          pg_attrdef
 pg_catalog          pg_attribute
@@ -415,6 +416,7 @@ table_privileges
 tables
 user_privileges
 views
+pg_aggregate
 pg_am
 pg_attrdef
 pg_attribute
@@ -565,6 +567,7 @@ system         information_schema  table_privileges                   SYSTEM VIE
 system         information_schema  tables                             SYSTEM VIEW  NO                  1
 system         information_schema  user_privileges                    SYSTEM VIEW  NO                  1
 system         information_schema  views                              SYSTEM VIEW  NO                  1
+system         pg_catalog          pg_aggregate                       SYSTEM VIEW  NO                  1
 system         pg_catalog          pg_am                              SYSTEM VIEW  NO                  1
 system         pg_catalog          pg_attrdef                         SYSTEM VIEW  NO                  1
 system         pg_catalog          pg_attribute                       SYSTEM VIEW  NO                  1
@@ -1562,6 +1565,7 @@ NULL     public   system         information_schema  table_privileges           
 NULL     public   system         information_schema  tables                             SELECT          NULL          YES
 NULL     public   system         information_schema  user_privileges                    SELECT          NULL          YES
 NULL     public   system         information_schema  views                              SELECT          NULL          YES
+NULL     public   system         pg_catalog          pg_aggregate                       SELECT          NULL          YES
 NULL     public   system         pg_catalog          pg_am                              SELECT          NULL          YES
 NULL     public   system         pg_catalog          pg_attrdef                         SELECT          NULL          YES
 NULL     public   system         pg_catalog          pg_attribute                       SELECT          NULL          YES
@@ -1906,6 +1910,7 @@ NULL     public   system         information_schema  table_privileges           
 NULL     public   system         information_schema  tables                             SELECT          NULL          YES
 NULL     public   system         information_schema  user_privileges                    SELECT          NULL          YES
 NULL     public   system         information_schema  views                              SELECT          NULL          YES
+NULL     public   system         pg_catalog          pg_aggregate                       SELECT          NULL          YES
 NULL     public   system         pg_catalog          pg_am                              SELECT          NULL          YES
 NULL     public   system         pg_catalog          pg_attrdef                         SELECT          NULL          YES
 NULL     public   system         pg_catalog          pg_attribute                       SELECT          NULL          YES

pkg/sql/logictest/testdata/logic_test/orms
@@ -72,8 +72,8 @@ GROUP BY i.relname,
 ORDER BY i.relname
 ----
 name              primary  unique  indkey  column_indexes  column_names  definition
-customers_id_idx  false    false   2       {1,2}           {name,id}     CREATE INDEX customers_id_idx ON test.public.customers USING btree (id ASC)
-primary           true     true    1       {1,2}           {name,id}     CREATE UNIQUE INDEX "primary" ON test.public.customers USING btree (name ASC)
+customers_id_idx  false    false   2       {2,1}           {id,name}     CREATE INDEX customers_id_idx ON test.public.customers USING btree (id ASC)
+primary           true     true    1       {2,1}           {id,name}     CREATE UNIQUE INDEX "primary" ON test.public.customers USING btree (name ASC)
 
 
 query TT colnames

pkg/sql/sqlbase/constants.go
@@ -109,6 +109,7 @@ const (
 	InformationSchemaViewsTableID
 	InformationSchemaUserPrivilegesID
 	PgCatalogID
+	PgCatalogAggregateTableID
 	PgCatalogAmTableID
 	PgCatalogAttrDefTableID
 	PgCatalogAttributeTableID

commit 16d11d76e2e93a4d2ee8fe3f6e09ec18a81ed4eb

sql: bugfix to pg_aggregate
Commit 4047d45256b8cc7291d0167916115066d4f09f90 introduced a bug which
caused reads from pg_aggregate to mistakenly change the 0 OID to have a
"-" name alias, causing some inconsistent behavior in system tables.

Release note: None

pkg/sql/pg_catalog.go
@@ -3007,7 +3007,7 @@ CREATE TABLE pg_catalog.pg_aggregate (
 								}
 							}
 						}
-						regprocForZeroOid := oidZero.AsRegProc("-")
+						regprocForZeroOid := tree.NewDOidWithName(tree.DInt(0), types.RegProc, "-")
 						err := addRow(
 							h.BuiltinOid(name, &overload).AsRegProc(name), // aggfnoid
 							aggregateKind,     // aggkind