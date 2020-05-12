cockroachdb/cockroach

commit 408c5807a26f83bb551f191cf7ea137b15b8a953

lint: properly parallelize TestVet/TestVet
The `shadow` and `printfuncs` testvet invocations are
not amenable to parallelization well.

This patch fixes it.

Release note: None

pkg/testutils/lint/lint_test.go
@@ -1221,7 +1221,6 @@ func TestLint(t *testing.T) {
 	})
 
 	t.Run("TestVet", func(t *testing.T) {
-		t.Parallel()
 		runVet := func(t *testing.T, args ...string) {
 			args = append(append([]string{"vet"}, args...), pkgScope)
 			vetCmd(t, crdb.Dir, "go", args, []stream.Filter{
@@ -1289,7 +1288,7 @@ func TestLint(t *testing.T) {
 		//    A function may be a Printf or Print wrapper if its last argument is ...interface{}.
 		//    If the next-to-last argument is a string, then this may be a Printf wrapper.
 		//    Otherwise it may be a Print wrapper.
-		runVet(t, "-all", "-printfuncs", printfuncs)
+		t.Run("vet", func(t *testing.T) { runVet(t, "-all", "-printfuncs", printfuncs) })
 	})
 
 	// TODO(tamird): replace this with errcheck.NewChecker() when

commit b2fc0547bd925e4589f6d66e9da12210fa55e4a4

lint: fix nightly lint build for release-19.2
Seems broken by `408c5807a26f83bb551f191cf7ea137b15b8a953`. Add the
correct variables to make the nightly lint build work for release-19.2

Release note: None

pkg/testutils/lint/nightly_lint_test.go
@@ -14,16 +14,18 @@ package lint
 
 import (
 	"bytes"
+	"go/build"
 	"os"
 	"os/exec"
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/cmd/urlcheck/lib/urlcheck"
 	sqlparser "github.com/cockroachdb/cockroach/pkg/sql/parser"
+	"github.com/ghemawat/stream"
 )
 
 func TestNightlyLint(t *testing.T) {
-	_, pkgSpecified := os.LookupEnv("PKG")
+	pkgVar, pkgSpecified := os.LookupEnv("PKG")
 
 	// RoachLint is expensive memory-wise and thus should not run with t.Parallel().
 	//
@@ -31,6 +33,16 @@ func TestNightlyLint(t *testing.T) {
 	// until issue https://github.com/cockroachdb/cockroach/issues/42594
 	// has been addressed.
 	t.Run("TestRoachLint", func(t *testing.T) {
+		crdb, err := build.Import(cockroachDB, "", build.FindOnly)
+		if err != nil {
+			t.Skip(err)
+		}
+
+		pkgScope := pkgVar
+		if !pkgSpecified {
+			pkgScope = "./pkg/..."
+		}
+
 		vetCmd(t, crdb.Dir, "roachlint", []string{pkgScope}, []stream.Filter{
 			// Ignore generated files.
 			stream.GrepNot(`pkg/.*\.pb\.go:`),