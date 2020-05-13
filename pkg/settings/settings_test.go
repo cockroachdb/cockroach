cockroachdb/cockroach

commit ac3c72339b82d9f85fd8112977a542b87fb5e712

server: separate SQL statistics from being refreshed with telemetry
This PR fixes the issue that SQL statement statistics
are refreshed when telemetry is reported.

To do this, this PR introduces a new statistics
pool for statistics to be reported to telemetry.
Whenever the SQL stats are reset, they are first
dumped into the reporting pool. The telemetry
reporter then takes the statistics that have been
aggregated so far and reports them whenever it executes.
This has the effect of telemetry only reporting SQL
stats at the rate of max(sql_refresh_rate, telemetry_report_rate).

Release note (sql change): This PR introduces a new cluster
setting to control the rate at which SQL statement statistics
are refreshed, `diagnostics.sql_stat_reset.interval`. Additionally
it renames the setting `diagnostics.forced_stat_reset.interval` to
`diagnostics.forced_sql_stat_reset_interval`.

pkg/settings/duration.go
@@ -115,6 +115,24 @@ func RegisterPublicNonNegativeDurationSetting(
 	return s
 }
 
+// RegisterPublicNonNegativeDurationSettingWithMaximum defines a new setting with
+// type duration, makes it public, and sets a maximum value.
+func RegisterPublicNonNegativeDurationSettingWithMaximum(
+	key, desc string, defaultValue time.Duration, maxValue time.Duration,
+) *DurationSetting {
+	s := RegisterValidatedDurationSetting(key, desc, defaultValue, func(v time.Duration) error {
+		if v < 0 {
+			return errors.Errorf("cannot set %s to a negative duration: %s", key, v)
+		}
+		if v >= maxValue {
+			return errors.Errorf("cannot set %s to a value larger than %s", key, maxValue)
+		}
+		return nil
+	})
+	s.SetVisibility(Public)
+	return s
+}
+
 // RegisterNonNegativeDurationSetting defines a new setting with type duration.
 func RegisterNonNegativeDurationSetting(
 	key, desc string, defaultValue time.Duration,

docs/generated/settings/settings.html
@@ -6,9 +6,10 @@
 <tr><td><code>cloudstorage.timeout</code></td><td>duration</td><td><code>10m0s</code></td><td>the timeout for import/export storage operations</td></tr>
 <tr><td><code>cluster.organization</code></td><td>string</td><td><code></code></td><td>organization name</td></tr>
 <tr><td><code>cluster.preserve_downgrade_option</code></td><td>string</td><td><code></code></td><td>disable (automatic or manual) cluster version upgrade from the specified version until reset</td></tr>
-<tr><td><code>diagnostics.forced_stat_reset.interval</code></td><td>duration</td><td><code>2h0m0s</code></td><td>interval after which pending diagnostics statistics should be discarded even if not reported</td></tr>
+<tr><td><code>diagnostics.forced_sql_stat_reset.interval</code></td><td>duration</td><td><code>2h0m0s</code></td><td>interval after which SQL statement statistics are refreshed even if not collected (should be more than diagnostics.sql_stat_reset.interval). It has a max value of 24H.</td></tr>
 <tr><td><code>diagnostics.reporting.enabled</code></td><td>boolean</td><td><code>true</code></td><td>enable reporting diagnostic metrics to cockroach labs</td></tr>
-<tr><td><code>diagnostics.reporting.interval</code></td><td>duration</td><td><code>1h0m0s</code></td><td>interval at which diagnostics data should be reported (should be shorter than diagnostics.forced_stat_reset.interval)</td></tr>
+<tr><td><code>diagnostics.reporting.interval</code></td><td>duration</td><td><code>1h0m0s</code></td><td>interval at which diagnostics data should be reported</td></tr>
+<tr><td><code>diagnostics.sql_stat_reset.interval</code></td><td>duration</td><td><code>1h0m0s</code></td><td>interval controlling how often SQL statement statistics should be reset (should be less than diagnostics.forced_sql_stat_reset.interval). It has a max value of 24H.</td></tr>
 <tr><td><code>enterprise.license</code></td><td>string</td><td><code></code></td><td>the encoded cluster license</td></tr>
 <tr><td><code>external.graphite.endpoint</code></td><td>string</td><td><code></code></td><td>if nonempty, push server metrics to the Graphite or Carbon server at the specified host:port</td></tr>
 <tr><td><code>external.graphite.interval</code></td><td>duration</td><td><code>10s</code></td><td>the interval at which metrics are pushed to Graphite (if enabled)</td></tr>

pkg/roachpb/app_stats.go
@@ -10,7 +10,11 @@
 
 package roachpb
 
-import "github.com/cockroachdb/cockroach/pkg/util/log"
+import (
+	"math"
+
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+)
 
 // GetVariance retrieves the variance of the values.
 func (l *NumericStat) GetVariance(count int64) float64 {
@@ -29,6 +33,12 @@ func (l *NumericStat) Record(count int64, val float64) {
 // Add combines b into this derived statistics.
 func (l *NumericStat) Add(b NumericStat, countA, countB int64) {
 	*l = AddNumericStats(*l, b, countA, countB)
+}
+
+// AlmostEqual compares two NumericStats between a window of size eps.
+func (l *NumericStat) AlmostEqual(b NumericStat, eps float64) bool {
+	return math.Abs(l.Mean-b.Mean) <= eps &&
+		math.Abs(l.SquaredDiffs-b.SquaredDiffs) <= eps
 }
 
 // AddNumericStats combines derived statistics.
@@ -53,4 +63,55 @@ func (si SensitiveInfo) GetScrubbedCopy() SensitiveInfo {
 	output.LastErr = log.Redact(si.LastErr)
 	// Not copying over MostRecentPlanDescription until we have an algorithm to scrub plan nodes.
 	return output
+}
+
+// Add combines other into this TxnStats.
+func (s *TxnStats) Add(other TxnStats) {
+	s.TxnTimeSec.Add(other.TxnTimeSec, s.TxnCount, other.TxnCount)
+	s.TxnCount += other.TxnCount
+	s.ImplicitCount += other.ImplicitCount
+	s.CommittedCount += other.CommittedCount
+}
+
+// Add combines other into this StatementStatistics.
+func (s *StatementStatistics) Add(other *StatementStatistics) {
+	s.FirstAttemptCount += other.FirstAttemptCount
+	if other.MaxRetries > s.MaxRetries {
+		s.MaxRetries = other.MaxRetries
+	}
+	s.NumRows.Add(other.NumRows, s.Count, other.Count)
+	s.ParseLat.Add(other.ParseLat, s.Count, other.Count)
+	s.PlanLat.Add(other.PlanLat, s.Count, other.Count)
+	s.RunLat.Add(other.RunLat, s.Count, other.Count)
+	s.ServiceLat.Add(other.ServiceLat, s.Count, other.Count)
+	s.OverheadLat.Add(other.OverheadLat, s.Count, other.Count)
+
+	if other.SensitiveInfo.LastErr != "" {
+		s.SensitiveInfo.LastErr = other.SensitiveInfo.LastErr
+	}
+
+	if s.SensitiveInfo.MostRecentPlanTimestamp.Before(other.SensitiveInfo.MostRecentPlanTimestamp) {
+		s.SensitiveInfo = other.SensitiveInfo
+	}
+
+	s.BytesRead += other.BytesRead
+	s.RowsRead += other.RowsRead
+	s.Count += other.Count
+}
+
+// AlmostEqual compares two StatementStatistics and their contained NumericStats
+// objects within an window of size eps.
+func (s *StatementStatistics) AlmostEqual(other *StatementStatistics, eps float64) bool {
+	return s.Count == other.Count &&
+		s.FirstAttemptCount == other.FirstAttemptCount &&
+		s.MaxRetries == other.MaxRetries &&
+		s.NumRows.AlmostEqual(other.NumRows, eps) &&
+		s.ParseLat.AlmostEqual(other.ParseLat, eps) &&
+		s.PlanLat.AlmostEqual(other.PlanLat, eps) &&
+		s.RunLat.AlmostEqual(other.RunLat, eps) &&
+		s.ServiceLat.AlmostEqual(other.ServiceLat, eps) &&
+		s.OverheadLat.AlmostEqual(other.OverheadLat, eps) &&
+		s.SensitiveInfo.Equal(other.SensitiveInfo) &&
+		s.BytesRead == other.BytesRead &&
+		s.RowsRead == other.RowsRead
 }

pkg/roachpb/app_stats.pb.go
@@ -26,6 +26,9 @@ var _ = time.Kitchen
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion2 // please upgrade the proto package
 
+// N.B. When fields are added to this struct, make sure to update
+// (*StatementStatistics).Add and (*StatementStatistics).AlmostEqual
+// in app_stats.go.
 type StatementStatistics struct {
 	// Count is the total number of times this statement was executed
 	// since the begin of the reporting period.
@@ -80,7 +83,7 @@ func (m *StatementStatistics) Reset()         { *m = StatementStatistics{} }
 func (m *StatementStatistics) String() string { return proto.CompactTextString(m) }
 func (*StatementStatistics) ProtoMessage()    {}
 func (*StatementStatistics) Descriptor() ([]byte, []int) {
-	return fileDescriptor_app_stats_78f287cf05837cfd, []int{0}
+	return fileDescriptor_app_stats_f212112aafb1969f, []int{0}
 }
 func (m *StatementStatistics) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -119,7 +122,7 @@ func (m *SensitiveInfo) Reset()         { *m = SensitiveInfo{} }
 func (m *SensitiveInfo) String() string { return proto.CompactTextString(m) }
 func (*SensitiveInfo) ProtoMessage()    {}
 func (*SensitiveInfo) Descriptor() ([]byte, []int) {
-	return fileDescriptor_app_stats_78f287cf05837cfd, []int{1}
+	return fileDescriptor_app_stats_f212112aafb1969f, []int{1}
 }
 func (m *SensitiveInfo) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -144,6 +147,8 @@ func (m *SensitiveInfo) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_SensitiveInfo proto.InternalMessageInfo
 
+// N.B. When this changes, make sure to update (*NumericStat).AlmostEqual
+// in app_stats.go.
 type NumericStat struct {
 	// NumericStat keeps track of two running values --- the running mean and
 	// the running sum of squared differences from the mean. Using this along
@@ -159,7 +164,7 @@ func (m *NumericStat) Reset()         { *m = NumericStat{} }
 func (m *NumericStat) String() string { return proto.CompactTextString(m) }
 func (*NumericStat) ProtoMessage()    {}
 func (*NumericStat) Descriptor() ([]byte, []int) {
-	return fileDescriptor_app_stats_78f287cf05837cfd, []int{2}
+	return fileDescriptor_app_stats_f212112aafb1969f, []int{2}
 }
 func (m *NumericStat) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -197,7 +202,7 @@ func (m *StatementStatisticsKey) Reset()         { *m = StatementStatisticsKey{}
 func (m *StatementStatisticsKey) String() string { return proto.CompactTextString(m) }
 func (*StatementStatisticsKey) ProtoMessage()    {}
 func (*StatementStatisticsKey) Descriptor() ([]byte, []int) {
-	return fileDescriptor_app_stats_78f287cf05837cfd, []int{3}
+	return fileDescriptor_app_stats_f212112aafb1969f, []int{3}
 }
 func (m *StatementStatisticsKey) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -232,7 +237,7 @@ func (m *CollectedStatementStatistics) Reset()         { *m = CollectedStatement
 func (m *CollectedStatementStatistics) String() string { return proto.CompactTextString(m) }
 func (*CollectedStatementStatistics) ProtoMessage()    {}
 func (*CollectedStatementStatistics) Descriptor() ([]byte, []int) {
-	return fileDescriptor_app_stats_78f287cf05837cfd, []int{4}
+	return fileDescriptor_app_stats_f212112aafb1969f, []int{4}
 }
 func (m *CollectedStatementStatistics) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -272,7 +277,7 @@ func (m *ExplainTreePlanNode) Reset()         { *m = ExplainTreePlanNode{} }
 func (m *ExplainTreePlanNode) String() string { return proto.CompactTextString(m) }
 func (*ExplainTreePlanNode) ProtoMessage()    {}
 func (*ExplainTreePlanNode) Descriptor() ([]byte, []int) {
-	return fileDescriptor_app_stats_78f287cf05837cfd, []int{5}
+	return fileDescriptor_app_stats_f212112aafb1969f, []int{5}
 }
 func (m *ExplainTreePlanNode) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -306,7 +311,7 @@ func (m *ExplainTreePlanNode_Attr) Reset()         { *m = ExplainTreePlanNode_At
 func (m *ExplainTreePlanNode_Attr) String() string { return proto.CompactTextString(m) }
 func (*ExplainTreePlanNode_Attr) ProtoMessage()    {}
 func (*ExplainTreePlanNode_Attr) Descriptor() ([]byte, []int) {
-	return fileDescriptor_app_stats_78f287cf05837cfd, []int{5, 0}
+	return fileDescriptor_app_stats_f212112aafb1969f, []int{5, 0}
 }
 func (m *ExplainTreePlanNode_Attr) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -332,6 +337,8 @@ func (m *ExplainTreePlanNode_Attr) XXX_DiscardUnknown() {
 var xxx_messageInfo_ExplainTreePlanNode_Attr proto.InternalMessageInfo
 
 // TxnStats contains statistics about transactions of one application.
+// N.B. When field are added to this struct, make sure to updated
+// (*TxnStats).Add in app_stats.go.
 type TxnStats struct {
 	TxnCount       int64       `protobuf:"varint,1,opt,name=txn_count,json=txnCount" json:"txn_count"`
 	TxnTimeSec     NumericStat `protobuf:"bytes,2,opt,name=txn_time_sec,json=txnTimeSec" json:"txn_time_sec"`
@@ -343,7 +350,7 @@ func (m *TxnStats) Reset()         { *m = TxnStats{} }
 func (m *TxnStats) String() string { return proto.CompactTextString(m) }
 func (*TxnStats) ProtoMessage()    {}
 func (*TxnStats) Descriptor() ([]byte, []int) {
-	return fileDescriptor_app_stats_78f287cf05837cfd, []int{6}
+	return fileDescriptor_app_stats_f212112aafb1969f, []int{6}
 }
 func (m *TxnStats) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -377,6 +384,103 @@ func init() {
 	proto.RegisterType((*ExplainTreePlanNode)(nil), "cockroach.sql.ExplainTreePlanNode")
 	proto.RegisterType((*ExplainTreePlanNode_Attr)(nil), "cockroach.sql.ExplainTreePlanNode.Attr")
 	proto.RegisterType((*TxnStats)(nil), "cockroach.sql.TxnStats")
+}
+func (this *SensitiveInfo) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	that1, ok := that.(*SensitiveInfo)
+	if !ok {
+		that2, ok := that.(SensitiveInfo)
+		if ok {
+			that1 = &that2
+		} else {
+			return false
+		}
+	}
+	if that1 == nil {
+		return this == nil
+	} else if this == nil {
+		return false
+	}
+	if this.LastErr != that1.LastErr {
+		return false
+	}
+	if !this.MostRecentPlanDescription.Equal(&that1.MostRecentPlanDescription) {
+		return false
+	}
+	if !this.MostRecentPlanTimestamp.Equal(that1.MostRecentPlanTimestamp) {
+		return false
+	}
+	return true
+}
+func (this *ExplainTreePlanNode) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	that1, ok := that.(*ExplainTreePlanNode)
+	if !ok {
+		that2, ok := that.(ExplainTreePlanNode)
+		if ok {
+			that1 = &that2
+		} else {
+			return false
+		}
+	}
+	if that1 == nil {
+		return this == nil
+	} else if this == nil {
+		return false
+	}
+	if this.Name != that1.Name {
+		return false
+	}
+	if len(this.Attrs) != len(that1.Attrs) {
+		return false
+	}
+	for i := range this.Attrs {
+		if !this.Attrs[i].Equal(that1.Attrs[i]) {
+			return false
+		}
+	}
+	if len(this.Children) != len(that1.Children) {
+		return false
+	}
+	for i := range this.Children {
+		if !this.Children[i].Equal(that1.Children[i]) {
+			return false
+		}
+	}
+	return true
+}
+func (this *ExplainTreePlanNode_Attr) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	that1, ok := that.(*ExplainTreePlanNode_Attr)
+	if !ok {
+		that2, ok := that.(ExplainTreePlanNode_Attr)
+		if ok {
+			that1 = &that2
+		} else {
+			return false
+		}
+	}
+	if that1 == nil {
+		return this == nil
+	} else if this == nil {
+		return false
+	}
+	if this.Key != that1.Key {
+		return false
+	}
+	if this.Value != that1.Value {
+		return false
+	}
+	return true
 }
 func (m *StatementStatistics) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
@@ -2311,67 +2415,67 @@ var (
 	ErrIntOverflowAppStats   = fmt.Errorf("proto: integer overflow")
 )
 
-func init() { proto.RegisterFile("roachpb/app_stats.proto", fileDescriptor_app_stats_78f287cf05837cfd) }
+func init() { proto.RegisterFile("roachpb/app_stats.proto", fileDescriptor_app_stats_f212112aafb1969f) }
 
-var fileDescriptor_app_stats_78f287cf05837cfd = []byte{
-	// 931 bytes of a gzipped FileDescriptorProto
+var fileDescriptor_app_stats_f212112aafb1969f = []byte{
+	// 944 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x56, 0xcd, 0x6e, 0xdb, 0x46,
-	0x10, 0x36, 0x2b, 0xc9, 0xa2, 0x86, 0x92, 0x83, 0xae, 0xdb, 0x84, 0x15, 0x0c, 0xd9, 0x55, 0x11,
-	0xc4, 0x41, 0x5b, 0x19, 0x30, 0x7a, 0x69, 0x0b, 0x07, 0xb0, 0x9d, 0x1c, 0x82, 0x1a, 0x41, 0x4b,
-	0xfb, 0xd4, 0x0b, 0xb1, 0x26, 0x47, 0xf2, 0xc2, 0xe4, 0x2e, 0xbd, 0xbb, 0x74, 0xa4, 0x57, 0xe8,
-	0x29, 0x2f, 0xd0, 0xf7, 0x31, 0x50, 0x14, 0xc8, 0x31, 0xa7, 0xfe, 0xd8, 0xb7, 0x3e, 0x44, 0x51,
-	0xec, 0x92, 0x54, 0x29, 0x47, 0x45, 0x75, 0x93, 0x67, 0xbe, 0x6f, 0x66, 0x67, 0xe6, 0x9b, 0xa1,
-	0xe1, 0x91, 0x14, 0x34, 0xba, 0xc8, 0xce, 0xf7, 0x68, 0x96, 0x85, 0x4a, 0x53, 0xad, 0x46, 0x99,
-	0x14, 0x5a, 0x90, 0x5e, 0x24, 0xa2, 0x4b, 0xeb, 0x1c, 0xa9, 0xab, 0xa4, 0xff, 0xd1, 0x44, 0x4c,
-	0x84, 0xf5, 0xec, 0x99, 0x5f, 0x05, 0xa8, 0xbf, 0x3d, 0x11, 0x62, 0x92, 0xe0, 0x9e, 0xfd, 0xeb,
-	0x3c, 0x1f, 0xef, 0x69, 0x96, 0xa2, 0xd2, 0x34, 0xcd, 0x0a, 0xc0, 0xf0, 0xa7, 0x75, 0xd8, 0x3c,
-	0xd5, 0x54, 0x63, 0x8a, 0x5c, 0x9b, 0x1f, 0x4c, 0x69, 0x16, 0x29, 0xd2, 0x87, 0x56, 0x24, 0x72,
-	0xae, 0x7d, 0x67, 0xc7, 0xd9, 0x6d, 0x1c, 0x35, 0x6f, 0x7e, 0xdb, 0x5e, 0x0b, 0x0a, 0x13, 0xf9,
-	0x0a, 0x36, 0xc7, 0x4c, 0x2a, 0x1d, 0x52, 0xad, 0x31, 0xcd, 0x74, 0x58, 0x20, 0x3f, 0xa8, 0x21,
-	0x3f, 0xb4, 0x80, 0xc3, 0xc2, 0x7f, 0x6c, 0x59, 0x8f, 0xc1, 0x4b, 0xe9, 0x34, 0x94, 0xa8, 0x25,
-	0x43, 0xe5, 0x37, 0x6a, 0x68, 0x48, 0xe9, 0x34, 0x28, 0xec, 0xe4, 0x0b, 0x78, 0x90, 0xe0, 0x84,
-	0x46, 0xb3, 0x30, 0xa1, 0x4a, 0x87, 0x28, 0xa5, 0xdf, 0xdc, 0x71, 0x76, 0x3b, 0x25, 0xb4, 0x57,
-	0x38, 0x4f, 0xa8, 0xd2, 0x2f, 0xa4, 0x24, 0xdf, 0x82, 0xcb, 0xf3, 0x34, 0x94, 0xe2, 0xb5, 0xf2,
-	0x5b, 0x3b, 0xce, 0xae, 0xb7, 0xdf, 0x1f, 0x2d, 0xf4, 0x65, 0xf4, 0x2a, 0x4f, 0x51, 0xb2, 0xc8,
-	0x94, 0x56, 0x86, 0x68, 0xf3, 0x3c, 0x0d, 0xc4, 0x6b, 0x45, 0x0e, 0xa0, 0x93, 0x51, 0xa9, 0x30,
-	0x4c, 0xa8, 0xf6, 0xd7, 0x57, 0x64, 0xbb, 0x96, 0x72, 0x42, 0xb5, 0xc9, 0x9d, 0x25, 0x94, 0x5b,
-	0x76, 0x7b, 0xd5, 0xdc, 0x86, 0x61, 0xc8, 0x5f, 0x43, 0x5b, 0xe6, 0x05, 0xd7, 0x5d, 0x91, 0xbb,
-	0x2e, 0x73, 0x4b, 0x3d, 0x04, 0x4f, 0xa1, 0xbc, 0x66, 0x51, 0xf1, 0xf0, 0xce, 0x8a, 0x74, 0x28,
-	0x49, 0x26, 0xc4, 0x31, 0x74, 0xc5, 0x35, 0xca, 0x0b, 0xa4, 0xb1, 0x8d, 0x01, 0x2b, 0xc6, 0xf0,
-	0x2a, 0x96, 0x09, 0x72, 0x00, 0xfe, 0xbd, 0x49, 0x85, 0x12, 0x63, 0x1a, 0x69, 0x8c, 0x7d, 0xaf,
-	0x36, 0xb2, 0x8f, 0x17, 0x46, 0x16, 0x94, 0x10, 0xf2, 0x12, 0x36, 0x14, 0x72, 0xc5, 0x34, 0xbb,
-	0xc6, 0x90, 0xf1, 0xb1, 0xf0, 0xbb, 0xf6, 0x15, 0x5b, 0xf7, 0x5e, 0x71, 0x5a, 0x81, 0x5e, 0xf2,
-	0xb1, 0xa8, 0x54, 0xa0, 0xea, 0x46, 0xf2, 0x19, 0xc0, 0xf9, 0x4c, 0xa3, 0x0a, 0x25, 0xd2, 0xd8,
-	0xef, 0xd5, 0x94, 0xd5, 0xb1, 0xf6, 0x00, 0x69, 0x4c, 0x3e, 0x85, 0x8e, 0x91, 0x49, 0x81, 0xd9,
-	0xa8, 0x61, 0x5c, 0x63, 0x36, 0x90, 0xe1, 0xdf, 0x0e, 0xf4, 0x16, 0xd2, 0x91, 0x6d, 0x70, 0xe7,
-	0x32, 0x74, 0x6a, 0x35, 0xb5, 0x93, 0x52, 0x80, 0x0c, 0xb6, 0x52, 0xa1, 0x74, 0x28, 0x31, 0x42,
-	0xae, 0x43, 0x2b, 0x88, 0x18, 0x55, 0x24, 0x59, 0xa6, 0x99, 0xe0, 0x76, 0x29, 0xbc, 0xfd, 0xe1,
-	0xbd, 0x9a, 0x5e, 0x4c, 0xb3, 0x84, 0x32, 0x7e, 0x26, 0x11, 0xbf, 0x4f, 0x28, 0x7f, 0x25, 0x62,
-	0x2c, 0x03, 0x7f, 0x62, 0xa2, 0x05, 0x36, 0x98, 0xf1, 0x3c, 0xff, 0x37, 0x14, 0xa1, 0xd0, 0x7f,
-	0x2f, 0xd5, 0x7c, 0x9d, 0xed, 0x3e, 0x99, 0x11, 0x16, 0x0b, 0x3f, 0xaa, 0x16, 0x7e, 0x74, 0x56,
-	0x21, 0x8e, 0x5c, 0x93, 0xe0, 0xcd, 0xef, 0xdb, 0x4e, 0xf0, 0x68, 0x31, 0xc9, 0x1c, 0x32, 0x0c,
-	0xc0, 0xab, 0x0d, 0x9d, 0xf8, 0xd0, 0x4c, 0x91, 0x72, 0x5b, 0xb9, 0x53, 0x3e, 0xd0, 0x5a, 0xc8,
-	0x53, 0xe8, 0xa9, 0xab, 0x9c, 0x4a, 0x8c, 0xc3, 0x98, 0x8d, 0xc7, 0xca, 0xd6, 0x59, 0x41, 0xba,
-	0xa5, 0xeb, 0xb9, 0xf1, 0x0c, 0x7f, 0x71, 0xe0, 0xe1, 0x92, 0x0b, 0xf3, 0x1d, 0xce, 0xcc, 0x91,
-	0xb9, 0xca, 0x51, 0xce, 0x16, 0x5a, 0x5b, 0x98, 0xc8, 0x43, 0x68, 0xd0, 0x2c, 0xb3, 0x71, 0x2b,
-	0x8f, 0x31, 0x90, 0x01, 0xb4, 0x63, 0xa6, 0xf4, 0xe9, 0x0f, 0x27, 0xb6, 0x64, 0xb7, 0x1a, 0x48,
-	0x69, 0x24, 0x5b, 0xb0, 0x3e, 0xa6, 0x2c, 0xc1, 0xd8, 0x9e, 0x8d, 0xca, 0x5d, 0xda, 0x4c, 0x54,
-	0x91, 0x69, 0x7b, 0x2a, 0x2a, 0x97, 0x31, 0x90, 0x27, 0xd0, 0x65, 0x69, 0x96, 0xb0, 0x88, 0xe9,
-	0x50, 0x4f, 0xb9, 0xbd, 0x06, 0x15, 0xc0, 0xab, 0x3c, 0x67, 0x53, 0x3e, 0xfc, 0xd9, 0x81, 0xad,
-	0x63, 0x91, 0x24, 0x68, 0x34, 0xbc, 0xec, 0x70, 0x1e, 0x40, 0xe3, 0x12, 0x8b, 0x8a, 0xbc, 0xfd,
-	0xc7, 0xf7, 0xb5, 0xbc, 0xb4, 0x0f, 0xd5, 0x43, 0x2e, 0x71, 0x46, 0x9e, 0x41, 0xcb, 0x1e, 0xf9,
-	0xff, 0x10, 0xce, 0x92, 0x00, 0x55, 0xdb, 0x2c, 0x6d, 0xf8, 0x97, 0x03, 0x9b, 0x4b, 0xd4, 0x65,
-	0x46, 0xc9, 0x69, 0x8a, 0x0b, 0x9d, 0xb6, 0x16, 0x72, 0x00, 0x2d, 0xaa, 0xb5, 0x34, 0x19, 0x1b,
-	0xbb, 0xde, 0xfe, 0x93, 0xff, 0x97, 0xea, 0xe8, 0x50, 0x6b, 0x19, 0x14, 0x2c, 0xf2, 0x0c, 0xdc,
-	0xe8, 0x82, 0x25, 0xb1, 0x44, 0xee, 0x37, 0x6c, 0x84, 0x15, 0xc4, 0x1e, 0xcc, 0x39, 0xfd, 0x6f,
-	0xa0, 0x69, 0xc2, 0x99, 0xc9, 0x54, 0x7d, 0xeb, 0xd4, 0x1b, 0xd2, 0x87, 0xd6, 0x35, 0x4d, 0x72,
-	0x5c, 0x50, 0x42, 0x61, 0x1a, 0xfe, 0xea, 0x80, 0x7b, 0x36, 0xe5, 0xa6, 0x17, 0xca, 0xec, 0xb7,
-	0x9e, 0xf2, 0xf0, 0xfd, 0xaf, 0x96, 0xab, 0xa7, 0xbc, 0xf8, 0x04, 0x1d, 0x41, 0xd7, 0x40, 0xcc,
-	0xd2, 0x84, 0x0a, 0xa3, 0xb2, 0xc7, 0x2b, 0x9c, 0x4e, 0x3d, 0xb5, 0x4b, 0x72, 0x8a, 0x11, 0xf9,
-	0x12, 0x1e, 0x44, 0x22, 0x4d, 0x99, 0xd6, 0x18, 0x97, 0xc9, 0xea, 0x9f, 0xb2, 0x8d, 0xb9, 0xb3,
-	0x48, 0xf9, 0x39, 0x6c, 0xcc, 0x85, 0x55, 0xa0, 0x9b, 0x35, 0x74, 0xaf, 0xf2, 0x59, 0xf0, 0xd1,
-	0xd3, 0x9b, 0x3f, 0x07, 0x6b, 0x37, 0xb7, 0x03, 0xe7, 0xed, 0xed, 0xc0, 0x79, 0x77, 0x3b, 0x70,
-	0xfe, 0xb8, 0x1d, 0x38, 0x6f, 0xee, 0x06, 0x6b, 0x6f, 0xef, 0x06, 0x6b, 0xef, 0xee, 0x06, 0x6b,
-	0x3f, 0xb6, 0xcb, 0xff, 0x06, 0xfe, 0x09, 0x00, 0x00, 0xff, 0xff, 0x54, 0xef, 0x88, 0x58, 0x17,
-	0x08, 0x00, 0x00,
+	0x10, 0x36, 0x23, 0xd9, 0xa2, 0x86, 0x96, 0x83, 0xae, 0xdb, 0x84, 0x15, 0x0c, 0xc9, 0x55, 0x11,
+	0xc4, 0x41, 0x5b, 0x19, 0x30, 0x7a, 0x69, 0x0b, 0x07, 0xb5, 0x9d, 0x1c, 0x82, 0x1a, 0x41, 0x4b,
+	0xfb, 0xd4, 0x0b, 0xb1, 0x26, 0x47, 0xf2, 0xc2, 0xe4, 0x2e, 0xbd, 0xbb, 0x74, 0xa4, 0x7b, 0x4f,
+	0x3d, 0xe5, 0x05, 0x0a, 0xf4, 0x71, 0x0c, 0x14, 0x05, 0x72, 0xcc, 0xa9, 0x3f, 0xf6, 0xa5, 0x4f,
+	0xd0, 0x73, 0xb1, 0x4b, 0x52, 0xa5, 0x1c, 0x15, 0xd5, 0x4d, 0x9e, 0xf9, 0xbe, 0x99, 0x9d, 0x99,
+	0x6f, 0x86, 0x86, 0x87, 0x52, 0xd0, 0xe8, 0x3c, 0x3b, 0xdb, 0xa5, 0x59, 0x16, 0x2a, 0x4d, 0xb5,
+	0x1a, 0x66, 0x52, 0x68, 0x41, 0x3a, 0x91, 0x88, 0x2e, 0xac, 0x73, 0xa8, 0x2e, 0x93, 0xee, 0xfb,
+	0x63, 0x31, 0x16, 0xd6, 0xb3, 0x6b, 0x7e, 0x15, 0xa0, 0x6e, 0x7f, 0x2c, 0xc4, 0x38, 0xc1, 0x5d,
+	0xfb, 0xd7, 0x59, 0x3e, 0xda, 0xd5, 0x2c, 0x45, 0xa5, 0x69, 0x9a, 0x15, 0x80, 0xc1, 0x8f, 0x6b,
+	0xb0, 0x79, 0xa2, 0xa9, 0xc6, 0x14, 0xb9, 0x36, 0x3f, 0x98, 0xd2, 0x2c, 0x52, 0xa4, 0x0b, 0xab,
+	0x91, 0xc8, 0xb9, 0xf6, 0x9d, 0x6d, 0x67, 0xa7, 0x71, 0xd8, 0xbc, 0xfe, 0xad, 0xbf, 0x12, 0x14,
+	0x26, 0xf2, 0x39, 0x6c, 0x8e, 0x98, 0x54, 0x3a, 0xa4, 0x5a, 0x63, 0x9a, 0xe9, 0xb0, 0x40, 0xde,
+	0xab, 0x21, 0xdf, 0xb3, 0x80, 0x83, 0xc2, 0x7f, 0x64, 0x59, 0x8f, 0xc0, 0x4b, 0xe9, 0x24, 0x94,
+	0xa8, 0x25, 0x43, 0xe5, 0x37, 0x6a, 0x68, 0x48, 0xe9, 0x24, 0x28, 0xec, 0xe4, 0x53, 0xb8, 0x9f,
+	0xe0, 0x98, 0x46, 0xd3, 0x30, 0xa1, 0x4a, 0x87, 0x28, 0xa5, 0xdf, 0xdc, 0x76, 0x76, 0xda, 0x25,
+	0xb4, 0x53, 0x38, 0x8f, 0xa9, 0xd2, 0xcf, 0xa5, 0x24, 0x5f, 0x81, 0xcb, 0xf3, 0x34, 0x94, 0xe2,
+	0x95, 0xf2, 0x57, 0xb7, 0x9d, 0x1d, 0x6f, 0xaf, 0x3b, 0x9c, 0xeb, 0xcb, 0xf0, 0x65, 0x9e, 0xa2,
+	0x64, 0x91, 0x29, 0xad, 0x0c, 0xd1, 0xe2, 0x79, 0x1a, 0x88, 0x57, 0x8a, 0xec, 0x43, 0x3b, 0xa3,
+	0x52, 0x61, 0x98, 0x50, 0xed, 0xaf, 0x2d, 0xc9, 0x76, 0x2d, 0xe5, 0x98, 0x6a, 0x93, 0x3b, 0x4b,
+	0x28, 0xb7, 0xec, 0xd6, 0xb2, 0xb9, 0x0d, 0xc3, 0x90, 0xbf, 0x80, 0x96, 0xcc, 0x0b, 0xae, 0xbb,
+	0x24, 0x77, 0x4d, 0xe6, 0x96, 0x7a, 0x00, 0x9e, 0x42, 0x79, 0xc5, 0xa2, 0xe2, 0xe1, 0xed, 0x25,
+	0xe9, 0x50, 0x92, 0x4c, 0x88, 0x23, 0x58, 0x17, 0x57, 0x28, 0xcf, 0x91, 0xc6, 0x36, 0x06, 0x2c,
+	0x19, 0xc3, 0xab, 0x58, 0x26, 0xc8, 0x3e, 0xf8, 0x77, 0x26, 0x15, 0x4a, 0x8c, 0x69, 0xa4, 0x31,
+	0xf6, 0xbd, 0xda, 0xc8, 0x3e, 0x98, 0x1b, 0x59, 0x50, 0x42, 0xc8, 0x0b, 0xd8, 0x50, 0xc8, 0x15,
+	0xd3, 0xec, 0x0a, 0x43, 0xc6, 0x47, 0xc2, 0x5f, 0xb7, 0xaf, 0xd8, 0xba, 0xf3, 0x8a, 0x93, 0x0a,
+	0xf4, 0x82, 0x8f, 0x44, 0xa5, 0x02, 0x55, 0x37, 0x92, 0x8f, 0x01, 0xce, 0xa6, 0x1a, 0x55, 0x28,
+	0x91, 0xc6, 0x7e, 0xa7, 0xa6, 0xac, 0xb6, 0xb5, 0x07, 0x48, 0x63, 0xf2, 0x11, 0xb4, 0x8d, 0x4c,
+	0x0a, 0xcc, 0x46, 0x0d, 0xe3, 0x1a, 0xb3, 0x81, 0x0c, 0x7e, 0xb8, 0x07, 0x9d, 0xb9, 0x74, 0xa4,
+	0x0f, 0xee, 0x4c, 0x86, 0x4e, 0xad, 0xa6, 0x56, 0x52, 0x0a, 0x90, 0xc1, 0x56, 0x2a, 0x94, 0x0e,
+	0x25, 0x46, 0xc8, 0x75, 0x68, 0x05, 0x11, 0xa3, 0x8a, 0x24, 0xcb, 0x34, 0x13, 0xdc, 0x2e, 0x85,
+	0xb7, 0x37, 0xb8, 0x53, 0xd3, 0xf3, 0x49, 0x96, 0x50, 0xc6, 0x4f, 0x25, 0xe2, 0xb7, 0x09, 0xe5,
+	0x2f, 0x45, 0x8c, 0x65, 0xe0, 0x0f, 0x4d, 0xb4, 0xc0, 0x06, 0x33, 0x9e, 0x67, 0xff, 0x86, 0x22,
+	0x14, 0xba, 0xef, 0xa4, 0x9a, 0xad, 0xb3, 0xdd, 0x27, 0x33, 0xc2, 0x62, 0xe1, 0x87, 0xd5, 0xc2,
+	0x0f, 0x4f, 0x2b, 0xc4, 0xa1, 0x6b, 0x12, 0xbc, 0xfe, 0xbd, 0xef, 0x04, 0x0f, 0xe7, 0x93, 0xcc,
+	0x20, 0x5f, 0x36, 0xff, 0xfa, 0xb9, 0xef, 0x0c, 0x02, 0xf0, 0x6a, 0xa3, 0x27, 0x3e, 0x34, 0x53,
+	0xa4, 0xdc, 0xd6, 0xef, 0x94, 0xcf, 0xb4, 0x16, 0xf2, 0x04, 0x3a, 0xea, 0x32, 0xa7, 0x12, 0xe3,
+	0x30, 0x66, 0xa3, 0x91, 0xb2, 0xd5, 0x56, 0x90, 0xf5, 0xd2, 0xf5, 0xcc, 0x78, 0x06, 0xbf, 0x38,
+	0xf0, 0x60, 0xc1, 0x9d, 0xf9, 0x06, 0xa7, 0xe6, 0xd4, 0x5c, 0xe6, 0x28, 0xa7, 0x73, 0x0d, 0x2e,
+	0x4c, 0xe4, 0x01, 0x34, 0x68, 0x96, 0xd9, 0xb8, 0x95, 0xc7, 0x18, 0x48, 0x0f, 0x5a, 0x31, 0x53,
+	0xfa, 0xe4, 0xbb, 0x63, 0x5b, 0xb8, 0x5b, 0x8d, 0xa5, 0x34, 0x92, 0x2d, 0x58, 0x1b, 0x51, 0x96,
+	0x60, 0x6c, 0x8f, 0x47, 0xe5, 0x2e, 0x6d, 0x26, 0xaa, 0xc8, 0xb4, 0x3d, 0x18, 0x95, 0xcb, 0x18,
+	0xc8, 0x63, 0x58, 0x67, 0x69, 0x96, 0xb0, 0x88, 0xe9, 0x50, 0x4f, 0xb8, 0xbd, 0x09, 0x15, 0xc0,
+	0xab, 0x3c, 0xa7, 0x13, 0x3e, 0xf8, 0xc9, 0x81, 0xad, 0x23, 0x91, 0x24, 0x68, 0x94, 0xbc, 0xe8,
+	0x7c, 0xee, 0x43, 0xe3, 0x02, 0x8b, 0x8a, 0xbc, 0xbd, 0x47, 0x77, 0x15, 0xbd, 0xb0, 0x0f, 0xd5,
+	0x43, 0x2e, 0x70, 0x4a, 0x9e, 0xc2, 0xaa, 0x3d, 0xf5, 0xff, 0x21, 0x9f, 0x05, 0x01, 0xaa, 0xb6,
+	0x59, 0xda, 0xe0, 0x6f, 0x07, 0x36, 0x17, 0x68, 0xcc, 0x8c, 0x92, 0xd3, 0x14, 0xe7, 0x3a, 0x6d,
+	0x2d, 0x64, 0x1f, 0x56, 0xa9, 0xd6, 0xd2, 0x64, 0x6c, 0xec, 0x78, 0x7b, 0x8f, 0xff, 0x5f, 0xb0,
+	0xc3, 0x03, 0xad, 0x65, 0x50, 0xb0, 0xc8, 0x53, 0x70, 0xa3, 0x73, 0x96, 0xc4, 0x12, 0xb9, 0xdf,
+	0xb0, 0x11, 0x96, 0x90, 0x7c, 0x30, 0xe3, 0x74, 0xbf, 0x86, 0xa6, 0x09, 0x67, 0x26, 0x53, 0xf5,
+	0xad, 0x5d, 0x6f, 0x48, 0x17, 0x56, 0xaf, 0x68, 0x92, 0xe3, 0x9c, 0x12, 0x0a, 0x53, 0x21, 0xda,
+	0x52, 0xba, 0xbf, 0x3a, 0xe0, 0x9e, 0x4e, 0xb8, 0xe9, 0x8b, 0x32, 0x1b, 0xaf, 0x27, 0x3c, 0x7c,
+	0xf7, 0x3b, 0xe6, 0xea, 0x09, 0x2f, 0x3e, 0x4a, 0x87, 0xb0, 0x6e, 0x20, 0x66, 0x8d, 0x42, 0x85,
+	0x51, 0xd9, 0xef, 0x25, 0x8e, 0xa9, 0x9e, 0xd8, 0xb5, 0x39, 0xc1, 0x88, 0x7c, 0x06, 0xf7, 0x23,
+	0x91, 0xa6, 0x4c, 0x6b, 0x8c, 0xcb, 0x64, 0xf5, 0x8f, 0xdb, 0xc6, 0xcc, 0x59, 0xa4, 0xfc, 0x04,
+	0x36, 0x66, 0x22, 0x2b, 0xd0, 0xcd, 0x1a, 0xba, 0x53, 0xf9, 0x2c, 0xf8, 0xf0, 0xc9, 0xf5, 0x9f,
+	0xbd, 0x95, 0xeb, 0x9b, 0x9e, 0xf3, 0xe6, 0xa6, 0xe7, 0xbc, 0xbd, 0xe9, 0x39, 0x7f, 0xdc, 0xf4,
+	0x9c, 0xd7, 0xb7, 0xbd, 0x95, 0x37, 0xb7, 0xbd, 0x95, 0xb7, 0xb7, 0xbd, 0x95, 0xef, 0x5b, 0xe5,
+	0xff, 0x07, 0xff, 0x04, 0x00, 0x00, 0xff, 0xff, 0x38, 0xbd, 0x98, 0x45, 0x29, 0x08, 0x00, 0x00,
 }

commit 41055e9a9bd35edfc6d621ee096b75aa4b0bb3a6

settings: fix RegisterPublicNonNegativeDurationSettingWithMaximum
`RegisterPublicNonNegativeDurationSettingWithMaximum` should include the
maximum. Seems broken by `ac3c72339b82d9f85fd8112977a542b87fb5e712`.

Release note (bug fix): Re-allow
`diagnostics.forced_sql_stat_reset.interval`,
`diagnostics.sql_stat_reset.interval` and `external.graphite.interval`
to set to their maximum values (24hr, 24hr and 15min respectively). This
previously only excluded these values to be allowed.

pkg/settings/duration.go
@@ -117,6 +117,7 @@ func RegisterPublicNonNegativeDurationSetting(
 
 // RegisterPublicNonNegativeDurationSettingWithMaximum defines a new setting with
 // type duration, makes it public, and sets a maximum value.
+// The maximum value is an allowed value.
 func RegisterPublicNonNegativeDurationSettingWithMaximum(
 	key, desc string, defaultValue time.Duration, maxValue time.Duration,
 ) *DurationSetting {
@@ -124,7 +125,7 @@ func RegisterPublicNonNegativeDurationSettingWithMaximum(
 		if v < 0 {
 			return errors.Errorf("cannot set %s to a negative duration: %s", key, v)
 		}
-		if v >= maxValue {
+		if v > maxValue {
 			return errors.Errorf("cannot set %s to a value larger than %s", key, maxValue)
 		}
 		return nil

pkg/settings/settings_test.go
@@ -146,6 +146,7 @@ var i1A = settings.RegisterIntSetting("i.1", "desc", 0)
 var i2A = settings.RegisterIntSetting("i.2", "desc", 5)
 var fA = settings.RegisterFloatSetting("f", "desc", 5.4)
 var dA = settings.RegisterDurationSetting("d", "desc", time.Second)
+var _ = settings.RegisterPublicNonNegativeDurationSettingWithMaximum("d_with_maximum", "desc", time.Second, time.Hour)
 var eA = settings.RegisterEnumSetting("e", "desc", "foo", map[int64]string{1: "foo", 2: "bar", 3: "baz"})
 var byteSize = settings.RegisterByteSizeSetting("zzz", "desc", mb)
 var mA = settings.RegisterStateMachineSettingImpl("statemachine", "foo", &dummyTransformer{})
@@ -180,6 +181,26 @@ var iVal = settings.RegisterValidatedIntSetting(
 		}
 		return nil
 	})
+
+func TestValidation(t *testing.T) {
+	sv := &settings.Values{}
+	sv.Init(settings.TestOpaque)
+
+	u := settings.NewUpdater(sv)
+	t.Run("d_with_maximum", func(t *testing.T) {
+		err := u.Set("d_with_maximum", "1h", "d")
+		require.NoError(t, err)
+		err = u.Set("d_with_maximum", "0h", "d")
+		require.NoError(t, err)
+		err = u.Set("d_with_maximum", "30m", "d")
+		require.NoError(t, err)
+
+		err = u.Set("d_with_maximum", "-1m", "d")
+		require.Error(t, err)
+		err = u.Set("d_with_maximum", "1h1s", "d")
+		require.Error(t, err)
+	})
+}
 
 func TestCache(t *testing.T) {
 	ctx := context.Background()