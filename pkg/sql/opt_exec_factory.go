ea66cd921bdfc1abb2c7aaea9040c1172b7f47e4
Rafi Shamim <rafi@cockroachlabs.com>
2020-05-12 21:41:24 -0400

    sql/pgwire: populate type modifier in RowDescription
    Previously, the TypeModifier would always be -1. Now it is populated
    with the value from pg_attribute.atttypmod when available.
    
    Release note (sql change): The RowDescription message of the pgwire
    protocol now contains the type modifier for each column in the result
    set. This corresponds to pg_attribute.atttypmod. If it is not available,
    the value is -1, as it was before.

--- pkg/sql/opt_exec_factory.go
+++ pkg/sql/opt_exec_factory.go
@@ -285,9 +285,9 @@ func (ef *execFactory) ConstructSimpleProject(
 	for i, col := range cols {
 		v := rb.r.ivarHelper.IndexedVar(int(col))
 		if colNames == nil {
-			rb.addExpr(v, inputCols[col].Name, inputCols[col].TableID, inputCols[col].PGAttributeNum)
+			rb.addExpr(v, inputCols[col].Name, inputCols[col].TableID, inputCols[col].PGAttributeNum, inputCols[col].GetTypeModifier())
 		} else {
-			rb.addExpr(v, colNames[i], 0 /* tableID */, 0 /* pgAttributeNum */)
+			rb.addExpr(v, colNames[i], 0 /* tableID */, 0 /* pgAttributeNum */, v.ResolvedType().TypeModifier())
 		}
 	}
 	return rb.res, nil
@@ -312,7 +312,7 @@ func (ef *execFactory) ConstructRender(
 	rb.init(n, reqOrdering, len(exprs))
 	for i, expr := range exprs {
 		expr = rb.r.ivarHelper.Rebind(expr, false /* alsoReset */, true /* normalizeToNonNil */)
-		rb.addExpr(expr, colNames[i], 0 /* tableID */, 0 /* pgAttributeNum */)
+		rb.addExpr(expr, colNames[i], 0 /* tableID */, 0 /* pgAttributeNum */, -1 /* typeModifier */)
 	}
 	return rb.res, nil
 }
@@ -2046,7 +2046,11 @@ func (rb *renderBuilder) init(n exec.Node, reqOrdering exec.OutputOrdering, cap 
 
 // addExpr adds a new render expression with the given name.
 func (rb *renderBuilder) addExpr(
-	expr tree.TypedExpr, colName string, tableID sqlbase.ID, pgAttributeNum sqlbase.ColumnID,
+	expr tree.TypedExpr,
+	colName string,
+	tableID sqlbase.ID,
+	pgAttributeNum sqlbase.ColumnID,
+	typeModifier int32,
 ) {
 	rb.r.render = append(rb.r.render, expr)
 	rb.r.columns = append(
@@ -2056,6 +2060,7 @@ func (rb *renderBuilder) addExpr(
 			Typ:            expr.ResolvedType(),
 			TableID:        tableID,
 			PGAttributeNum: pgAttributeNum,
+			TypeModifier:   typeModifier,
 		},
 	)
 }
