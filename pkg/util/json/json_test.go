80d306b9813955e47ddd1b978b865bedc949b0fa
Artem Barger <bartem@il.ibm.com>
2020-05-14 03:13:23 +0300

    sql: implement JSON{,B}_{OBJECT,}_AGG
    This commit adds implementation of json{,b}_object_agg built in
    function, by adding a new construction which augments constructions
    and allows to keep track on memory usage. Links json{,b}_object_agg
    built in function to enable support of queries which aggregates
    name/value pairs into JSON object.
    
    Signed-off-by: Artem Barger <bartem@il.ibm.com>
    
    Release note (sql change): enable aggregate queries which collect
    key/value pairs into JSON object.

--- pkg/util/json/json_test.go
+++ pkg/util/json/json_test.go
@@ -443,6 +443,60 @@ func TestArrayBuilderWithCounter(t *testing.T) {
 	}
 }
 
+func TestNewObjectBuilderWithCounter(t *testing.T) {
+	json := jsonTestShorthand
+
+	testCases := []struct {
+		input    [][]interface{}
+		expected JSON
+	}{
+		{
+			input:    [][]interface{}{},
+			expected: json(`{}`),
+		},
+		{
+			input:    [][]interface{}{{"key1", "val1"}},
+			expected: json(`{"key1": "val1"}`),
+		},
+		{
+			input:    [][]interface{}{{"key1", "val1"}, {"key2", "val2"}},
+			expected: json(`{"key1": "val1", "key2": "val2"}`),
+		},
+		{
+			input:    [][]interface{}{{"key1", []interface{}{1, 2, 3, 4}}},
+			expected: json(`{"key1": [1, 2, 3, 4]}`),
+		},
+		{
+			input:    [][]interface{}{{"key1", nil}, {"key2", 1}, {"key3", -1.27}, {"key4", "abcd"}},
+			expected: json(`{"key1": null, "key2": 1, "key3": -1.27, "key4": "abcd"}`),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("keys %+v", tc.input), func(t *testing.T) {
+			builder := NewObjectBuilderWithCounter()
+			for _, pair := range tc.input {
+				j, err := MakeJSON(pair[1])
+				if err != nil {
+					t.Fatal(err)
+				}
+				builder.Add(pair[0].(string), j)
+			}
+			result := builder.Build()
+			c, err := result.Compare(tc.expected)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if c != 0 {
+				t.Fatalf("expected %v to equal %v", result, tc.expected)
+			}
+			if builder.Size() != result.Size() {
+				t.Fatalf("expected %v to equal %v", builder.Size(), result.Size())
+			}
+		})
+	}
+}
+
 func TestBuildJSONObject(t *testing.T) {
 	testCases := []struct {
 		input []string
