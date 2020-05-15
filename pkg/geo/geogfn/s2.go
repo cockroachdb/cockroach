02c168ccbf77b27dd78b1b96263325f9d0dde547
Oliver Tan <otan@cockroachlabs.com>
2020-05-14 17:23:20 -0700

    geo/geomfn: implement DWithin for geometry types
    Moved the core of the logic for distance calculation operations to the
    `geodist` package, with required methods interface-d into the package
    for geometry/geography calculations. The code is mostly the same as it
    was in geogfn/distance.go, just the spheroid/sphere calculations mvoed
    out.
    
    The geometry based operations are contained in geomfn/distance.go. All
    math is annotated with links as well.
    
    Changed MinDistance to use the self-made implementation. The tests still
    call GEOS to make sure the return values are similar.
    
    Needed to bring some vendored packages in from go-geom for this to work.
    
    Release note (sql change): Implemented DWithin for Geometry types.

--- pkg/geo/geogfn/s2.go
+++ pkg/geo/geogfn/s2.go
@@ -46,28 +46,3 @@ func minChordAngle(a s1.ChordAngle, b s1.ChordAngle) s1.ChordAngle {
 	}
 	return b
 }
-
-// maybeClosestPointToEdge projects the point onto the infinite line represented
-// by the edge. This will return the point on the line closest to the edge.
-// It will return the closest point on the line, as well as a bool representing
-// whether the point that is projected lies directly on the edge as a segment.
-//
-// For visualization and more, see: Section 6 / Figure 4 of
-// "Projective configuration theorems: old wine into new wineskins", Tabachnikov, Serge, 2016/07/16
-func maybeClosestPointToEdge(edge s2.Edge, point s2.Point) (s2.Point, bool) {
-	// Project the point onto the normal of the edge. A great circle passing through
-	// the normal and the point will intersect with the great circle represented
-	// by the given edge.
-	normal := edge.V0.Vector.Cross(edge.V1.Vector).Normalize()
-	// To find the point where the great circle represented by the edge and the
-	// great circle represented by (normal, point), we project the point
-	// onto the normal.
-	normalScaledToPoint := normal.Mul(normal.Dot(point.Vector))
-	// The difference between the point and the projection of the normal when normalized
-	// should give us a point on the great circle which contains the vertexes of the edge.
-	closestPoint := s2.Point{Vector: point.Vector.Sub(normalScaledToPoint).Normalize()}
-	// We then check whether the given point lies on the geodesic of the edge,
-	// as the above algorithm only generates a point on the great circle
-	// represented by the edge.
-	return closestPoint, (&s2.Polyline{edge.V0, edge.V1}).IntersectsCell(s2.CellFromPoint(closestPoint))
-}
---
+++ pkg/geo/geomfn/distance.go
@@ -0,0 +1,421 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package geomfn
+
+import (
+	"math"
+
+	"github.com/cockroachdb/cockroach/pkg/geo"
+	"github.com/cockroachdb/cockroach/pkg/geo/geodist"
+	"github.com/cockroachdb/errors"
+	"github.com/twpayne/go-geom"
+	"github.com/twpayne/go-geom/xy/lineintersector"
+)
+
+// MinDistance returns the minimum distance between geometries A and B.
+func MinDistance(a *geo.Geometry, b *geo.Geometry) (float64, error) {
+	if a.SRID() != b.SRID() {
+		return 0, geo.NewMismatchingSRIDsError(a, b)
+	}
+	return minDistanceInternal(a, b, 0)
+}
+
+// DWithin determines if any part of geometry A is within D units of geometry B.
+func DWithin(a *geo.Geometry, b *geo.Geometry, d float64) (bool, error) {
+	if a.SRID() != b.SRID() {
+		return false, geo.NewMismatchingSRIDsError(a, b)
+	}
+	if d < 0 {
+		return false, errors.Newf("dwithin distance cannot be less than zero")
+	}
+	dist, err := minDistanceInternal(a, b, d)
+	if err != nil {
+		return false, err
+	}
+	return dist <= d, nil
+}
+
+// minDistanceInternal finds the minimum distance between two geometries.
+// This implementation is done in-house, as compared to using GEOS.
+func minDistanceInternal(a *geo.Geometry, b *geo.Geometry, stopAfterLE float64) (float64, error) {
+	u := newGeomMinDistanceUpdater(stopAfterLE)
+	c := &geomDistanceCalculator{updater: u}
+	return distanceInternal(a, b, c)
+}
+
+// distanceInternal calculates the distance between two geometries using
+// the DistanceCalculator operator.
+func distanceInternal(
+	a *geo.Geometry, b *geo.Geometry, c geodist.DistanceCalculator,
+) (float64, error) {
+	aGeoms, err := flattenGeometry(a)
+	if err != nil {
+		return 0, err
+	}
+	bGeoms, err := flattenGeometry(b)
+	if err != nil {
+		return 0, err
+	}
+	for _, aGeom := range aGeoms {
+		aGeodist, err := geomToGeodist(aGeom)
+		if err != nil {
+			return 0, err
+		}
+		for _, bGeom := range bGeoms {
+			bGeodist, err := geomToGeodist(bGeom)
+			if err != nil {
+				return 0, err
+			}
+			earlyExit, err := geodist.ShapeDistance(c, aGeodist, bGeodist)
+			if err != nil {
+				return 0, err
+			}
+			if earlyExit {
+				return c.DistanceUpdater().Distance(), nil
+			}
+		}
+	}
+	return c.DistanceUpdater().Distance(), nil
+}
+
+// geomToGeodist converts a given geom object to a geodist shape.
+func geomToGeodist(g geom.T) (geodist.Shape, error) {
+	switch g := g.(type) {
+	case *geom.Point:
+		return &geomGeodistPoint{Coord: g.Coords()}, nil
+	case *geom.LineString:
+		return &geomGeodistLineString{LineString: g}, nil
+	case *geom.Polygon:
+		return &geomGeodistPolygon{Polygon: g}, nil
+	}
+	return nil, errors.Newf("could not find shape: %T", g)
+}
+
+// geomGeodistPoint implements geodist.Point.
+type geomGeodistPoint struct {
+	geom.Coord
+}
+
+var _ geodist.Point = (*geomGeodistPoint)(nil)
+
+// IsShape implements the geodist.Point interface.
+func (*geomGeodistPoint) IsShape() {}
+
+// Point implements the geodist.Point interface.
+func (*geomGeodistPoint) IsPoint() {}
+
+// geomGeodistLineString implements geodist.LineString.
+type geomGeodistLineString struct {
+	*geom.LineString
+}
+
+var _ geodist.LineString = (*geomGeodistLineString)(nil)
+
+// IsShape implements the geodist.LineString interface.
+func (*geomGeodistLineString) IsShape() {}
+
+// LineString implements the geodist.LineString interface.
+func (*geomGeodistLineString) IsLineString() {}
+
+// Edge implements the geodist.LineString interface.
+func (g *geomGeodistLineString) Edge(i int) geodist.Edge {
+	return geodist.Edge{
+		V0: &geomGeodistPoint{Coord: g.LineString.Coord(i)},
+		V1: &geomGeodistPoint{Coord: g.LineString.Coord(i + 1)},
+	}
+}
+
+// NumEdges implements the geodist.LineString interface.
+func (g *geomGeodistLineString) NumEdges() int {
+	return g.LineString.NumCoords() - 1
+}
+
+// Vertex implements the geodist.LineString interface.
+func (g *geomGeodistLineString) Vertex(i int) geodist.Point {
+	return &geomGeodistPoint{Coord: g.LineString.Coord(i)}
+}
+
+// NumVertexes implements the geodist.LineString interface.
+func (g *geomGeodistLineString) NumVertexes() int {
+	return g.LineString.NumCoords()
+}
+
+// geomGeodistLinearRing implements geodist.LinearRing.
+type geomGeodistLinearRing struct {
+	*geom.LinearRing
+}
+
+var _ geodist.LinearRing = (*geomGeodistLinearRing)(nil)
+
+// IsShape implements the geodist.LinearRing interface.
+func (*geomGeodistLinearRing) IsShape() {}
+
+// LinearRing implements the geodist.LinearRing interface.
+func (*geomGeodistLinearRing) IsLinearRing() {}
+
+// Edge implements the geodist.LinearRing interface.
+func (g *geomGeodistLinearRing) Edge(i int) geodist.Edge {
+	return geodist.Edge{
+		V0: &geomGeodistPoint{Coord: g.LinearRing.Coord(i)},
+		V1: &geomGeodistPoint{Coord: g.LinearRing.Coord(i + 1)},
+	}
+}
+
+// NumEdges implements the geodist.LinearRing interface.
+func (g *geomGeodistLinearRing) NumEdges() int {
+	return g.LinearRing.NumCoords() - 1
+}
+
+// Vertex implements the geodist.LinearRing interface.
+func (g *geomGeodistLinearRing) Vertex(i int) geodist.Point {
+	return &geomGeodistPoint{Coord: g.LinearRing.Coord(i)}
+}
+
+// NumVertexes implements the geodist.LinearRing interface.
+func (g *geomGeodistLinearRing) NumVertexes() int {
+	return g.LinearRing.NumCoords()
+}
+
+// geomGeodistPolygon implements geodist.Polygon.
+type geomGeodistPolygon struct {
+	*geom.Polygon
+}
+
+var _ geodist.Polygon = (*geomGeodistPolygon)(nil)
+
+// IsShape implements the geodist.Polygon interface.
+func (*geomGeodistPolygon) IsShape() {}
+
+// Polygon implements the geodist.Polygon interface.
+func (*geomGeodistPolygon) IsPolygon() {}
+
+// LinearRing implements the geodist.Polygon interface.
+func (g *geomGeodistPolygon) LinearRing(i int) geodist.LinearRing {
+	return &geomGeodistLinearRing{LinearRing: g.Polygon.LinearRing(i)}
+}
+
+// NumLinearRings implements the geodist.Polygon interface.
+func (g *geomGeodistPolygon) NumLinearRings() int {
+	return g.Polygon.NumLinearRings()
+}
+
+// geomGeodistCrosser implements geodist.Crosser.
+type geomGeodistCrosser struct {
+	strategy   lineintersector.Strategy
+	edgeV0     geom.Coord
+	edgeV1     geom.Coord
+	nextEdgeV0 geom.Coord
+}
+
+var _ geodist.Crosser = (*geomGeodistCrosser)(nil)
+
+// ChainCrossing implements geodist.Crosser.
+func (c *geomGeodistCrosser) ChainCrossing(p geodist.Point) bool {
+	nextEdgeV1 := p.(*geomGeodistPoint).Coord
+	result := lineintersector.LineIntersectsLine(
+		c.strategy,
+		c.edgeV0,
+		c.edgeV1,
+		c.nextEdgeV0,
+		nextEdgeV1,
+	)
+	c.nextEdgeV0 = nextEdgeV1
+	return result.HasIntersection()
+}
+
+// geomMinDistanceUpdater finds the minimum distance using geom calculations.
+// Methods will return early if it finds a minimum distance <= stopAfterLE.
+type geomMinDistanceUpdater struct {
+	currentValue float64
+	stopAfterLE  float64
+}
+
+var _ geodist.DistanceUpdater = (*geomMinDistanceUpdater)(nil)
+
+// newGeomMinDistanceUpdater returns a new geomMinDistanceUpdater with the
+// correct arguments set up.
+func newGeomMinDistanceUpdater(stopAfterLE float64) *geomMinDistanceUpdater {
+	return &geomMinDistanceUpdater{
+		currentValue: math.MaxFloat64,
+		stopAfterLE:  stopAfterLE,
+	}
+}
+
+// Distance implements the DistanceUpdater interface.
+func (u *geomMinDistanceUpdater) Distance() float64 {
+	return u.currentValue
+}
+
+// Update implements the geodist.DistanceUpdater interface.
+func (u *geomMinDistanceUpdater) Update(aInterface geodist.Point, bInterface geodist.Point) bool {
+	a := aInterface.(*geomGeodistPoint).Coord
+	b := bInterface.(*geomGeodistPoint).Coord
+
+	dist := coordNorm(coordSub(a, b))
+	if dist < u.currentValue {
+		u.currentValue = dist
+		return dist <= u.stopAfterLE
+	}
+	return false
+}
+
+// OnIntersects implements the geodist.DistanceUpdater interface.
+func (u *geomMinDistanceUpdater) OnIntersects() bool {
+	u.currentValue = 0
+	return true
+}
+
+// geomDistanceCalculator implements geodist.DistanceCalculator
+type geomDistanceCalculator struct {
+	updater *geomMinDistanceUpdater
+}
+
+var _ geodist.DistanceCalculator = (*geomDistanceCalculator)(nil)
+
+// DistanceUpdater implements geodist.DistanceCalculator.
+func (c *geomDistanceCalculator) DistanceUpdater() geodist.DistanceUpdater {
+	return c.updater
+}
+
+// NewCrosser implements geodist.DistanceCalculator.
+func (c *geomDistanceCalculator) NewCrosser(
+	edge geodist.Edge, startPoint geodist.Point,
+) geodist.Crosser {
+	return &geomGeodistCrosser{
+		strategy:   &lineintersector.NonRobustLineIntersector{},
+		edgeV0:     edge.V0.(*geomGeodistPoint).Coord,
+		edgeV1:     edge.V1.(*geomGeodistPoint).Coord,
+		nextEdgeV0: startPoint.(*geomGeodistPoint).Coord,
+	}
+}
+
+// inBoundingCross returns whether a point is between a given edge.
+// This includes the boundary.
+// It only checks whether X _or_ Y is within the range of values covered by the edge.
+func (c *geomDistanceCalculator) inBoundingCross(
+	p geom.Coord, eV0 geom.Coord, eV1 geom.Coord,
+) bool {
+	return (eV0.X() <= p.X() && p.X() < eV1.X()) || (eV1.X() <= p.X() && p.X() < eV0.X()) ||
+		(eV0.Y() <= p.Y() && p.Y() < eV1.Y()) || (eV1.Y() <= p.Y() && p.Y() < eV0.Y())
+}
+
+// side corresponds to the side in which a point is relative to a line.
+type pointSide int
+
+const (
+	pointSideLeft  pointSide = -1
+	pointSideOn    pointSide = 0
+	pointSideRight pointSide = 1
+)
+
+// findPointSide finds which side a point is relative to the infinite line
+// given by the edge.
+// Note this side is relative to the orientation of the line.
+func (c *geomDistanceCalculator) findPointSide(
+	p geom.Coord, eV0 geom.Coord, eV1 geom.Coord,
+) pointSide {
+	// This is the equivalent of using the point-gradient formula
+	// and determining the sign, i.e. the sign of
+	// d = (x-x1)(y2-y1) - (y-y1)(x2-x1)
+	// where (x1,y1) and (x2,y2) is the edge and (x,y) is the point
+	sign := (p.X()-eV0.X())*(eV1.Y()-eV0.Y()) - (eV1.X()-eV0.X())*(p.Y()-eV0.Y())
+	switch {
+	case sign == 0:
+		return pointSideOn
+	case sign > 0:
+		return pointSideRight
+	default:
+		return pointSideLeft
+	}
+}
+
+// PointInLinearRing implements geodist.DistanceCalculator.
+func (c *geomDistanceCalculator) PointInLinearRing(
+	point geodist.Point, polygon geodist.LinearRing,
+) bool {
+	// This is done using the winding number algorithm, also known as the
+	// "non-zero rule".
+	// See: https://en.wikipedia.org/wiki/Point_in_polygon
+	// See also: https://en.wikipedia.org/wiki/Winding_number
+	// See also: https://en.wikipedia.org/wiki/Nonzero-rule
+	windingNumber := 0
+	p := point.(*geomGeodistPoint).Coord
+	for edgeIdx := 0; edgeIdx < polygon.NumEdges(); edgeIdx++ {
+		e := polygon.Edge(edgeIdx)
+		eV0 := e.V0.(*geomGeodistPoint).Coord
+		eV1 := e.V1.(*geomGeodistPoint).Coord
+		yMin := math.Min(eV0.Y(), eV1.Y())
+		yMax := math.Max(eV0.Y(), eV1.Y())
+		// If the edge isn't on the same level as Y, this edge isn't worth considering.
+		if p.Y() > yMax || p.Y() < yMin {
+			continue
+		}
+		side := c.findPointSide(p, eV0, eV1)
+		// If the point is on the line, there is a covering.
+		if side == pointSideOn && c.inBoundingCross(p, eV0, eV1) {
+			return true
+		}
+		// If the point is left of the segment and the line is rising
+		// we have a circle going CCW, so increment.
+		if side == pointSideLeft && eV0.Y() <= p.Y() && p.Y() < eV1.Y() {
+			windingNumber++
+		}
+		// If the line is to the right of the segment and the
+		// line is falling, we a have a circle going CW so decrement.
+		if side == pointSideRight && eV1.Y() <= p.Y() && p.Y() < eV0.Y() {
+			windingNumber--
+		}
+	}
+	return windingNumber != 0
+}
+
+// ClosestPointToEdge implements geodist.DistanceCalculator.
+func (c *geomDistanceCalculator) ClosestPointToEdge(
+	edge geodist.Edge, pointInterface geodist.Point,
+) (geodist.Point, bool) {
+	eV0 := edge.V0.(*geomGeodistPoint).Coord
+	eV1 := edge.V1.(*geomGeodistPoint).Coord
+	p := pointInterface.(*geomGeodistPoint).Coord
+
+	// From http://www.faqs.org/faqs/graphics/algorithms-faq/, section 1.02
+	//
+	//  Let the point be C (Cx,Cy) and the line be AB (Ax,Ay) to (Bx,By).
+	//  Let P be the point of perpendicular projection of C on AB.  The parameter
+	//  r, which indicates P's position along AB, is computed by the dot product
+	//  of AC and AB divided by the square of the length of AB:
+	//
+	//  (1)     AC dot AB
+	//      r = ---------
+	//          ||AB||^2
+	//
+	//  r has the following meaning:
+	//
+	//      r=0      P = A
+	//      r=1      P = B
+	//      r<0      P is on the backward extension of AB
+	//      r>1      P is on the forward extension of AB
+	//      0<r<1    P is interior to AB
+	if coordEqual(p, eV0) {
+		return pointInterface, true
+	}
+	if coordEqual(p, eV1) {
+		return pointInterface, true
+	}
+
+	ac := coordSub(p, eV0)
+	ab := coordSub(eV1, eV0)
+
+	r := coordDot(ac, ab) / coordNorm2(ab)
+	if r < 0 || r > 1 {
+		return pointInterface, false
+	}
+	return &geomGeodistPoint{Coord: coordAdd(eV0, coordMul(ab, r))}, true
+}
