9dc894c5c9f454b700fe3d7c8cd5fdfbbe11c23f
Matt Jibson <matt.jibson@gmail.com>
2020-05-18 11:21:55 -0600

    sql: enable simple stats for all types
    Improve datum fingerprinting (used by rowexec/sampler.go) to work
    correctly for all types. Previously, due to the hardcoded assumption that
    JSON was the only non-key encodable type, it would error if presented
    a geo/geom type.
    
    Use MustBeValueEncoded when fingerprinting and determining if a type can
    create a histogram. This should be future proof if we add new types or
    teach types how to key encode.
    
    Fixes #35844
    Informs #48219
    
    Release note: None

--- pkg/sql/create_stats.go
+++ pkg/sql/create_stats.go
@@ -31,7 +31,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/stats"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util"
-	"github.com/cockroachdb/cockroach/pkg/util/errorutil/unimplemented"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/errors"
@@ -207,19 +206,14 @@ func (n *createStatsNode) makeJobRecord(ctx context.Context) (*jobs.Record, erro
 
 		columnIDs := make([]sqlbase.ColumnID, len(columns))
 		for i := range columns {
-			if columns[i].Type.Family() == types.JsonFamily {
-				return nil, unimplemented.NewWithIssuef(35844,
-					"CREATE STATISTICS is not supported for JSON columns")
-			}
 			columnIDs[i] = columns[i].ID
 		}
-		colStats = []jobspb.CreateStatsDetails_ColStat{{ColumnIDs: columnIDs, HasHistogram: false}}
-		if len(columnIDs) == 1 && columns[0].Type.Family() != types.ArrayFamily {
+		colStats = []jobspb.CreateStatsDetails_ColStat{{
+			ColumnIDs: columnIDs,
 			// By default, create histograms on all explicitly requested column stats
-			// with a single column. (We cannot create histograms on array columns
-			// because we do not support key encoding arrays.)
-			colStats[0].HasHistogram = true
-		}
+			// with a single column.
+			HasHistogram: len(columnIDs) == 1 && canHistogramType(columns[0].Type),
+		}}
 	}
 
 	// Evaluate the AS OF time, if any.
@@ -261,6 +255,10 @@ func (n *createStatsNode) makeJobRecord(ctx context.Context) (*jobs.Record, erro
 	}, nil
 }
 
+func canHistogramType(t *types.T) bool {
+	return !sqlbase.ColumnTypeIsInvertedIndexable(t)
+}
+
 // maxNonIndexCols is the maximum number of non-index columns that we will use
 // when choosing a default set of column statistics.
 const maxNonIndexCols = 100
@@ -293,48 +291,56 @@ func createStatsDefaultColumns(
 			break
 		}
 
+		colIDs := desc.PrimaryIndex.ColumnIDs[: i+1 : i+1]
+
 		// Remember the requested stats so we don't request duplicates.
-		key := makeColStatKey(desc.PrimaryIndex.ColumnIDs[: i+1 : i+1])
+		key := makeColStatKey(colIDs)
 		requestedStats[key] = struct{}{}
 
+		col, err := desc.FindColumnByID(colIDs[i])
+		if err != nil {
+			return nil, err
+		}
 		colStats = append(colStats, jobspb.CreateStatsDetails_ColStat{
-			ColumnIDs:    desc.PrimaryIndex.ColumnIDs[: i+1 : i+1],
-			HasHistogram: i == 0,
+			ColumnIDs:    colIDs,
+			HasHistogram: i == 0 && canHistogramType(col.Type),
 		})
 	}
 
 	// Add column stats for each secondary index.
 	for i := range desc.Indexes {
-		if desc.Indexes[i].Type == sqlbase.IndexDescriptor_INVERTED {
-			// We don't yet support stats on inverted indexes.
-			continue
-		}
 		for j := range desc.Indexes[i].ColumnIDs {
 			if j != 0 && !multiColEnabled {
 				break
 			}
 
+			colIDs := desc.Indexes[i].ColumnIDs[: j+1 : j+1]
+
 			// Check for existing stats and remember the requested stats.
-			key := makeColStatKey(desc.Indexes[i].ColumnIDs[: j+1 : j+1])
+			key := makeColStatKey(colIDs)
 			if _, ok := requestedStats[key]; ok {
 				continue
 			}
 			requestedStats[key] = struct{}{}
 
+			col, err := desc.FindColumnByID(colIDs[j])
+			if err != nil {
+				return nil, err
+			}
 			colStats = append(colStats, jobspb.CreateStatsDetails_ColStat{
-				ColumnIDs:    desc.Indexes[i].ColumnIDs[: j+1 : j+1],
-				HasHistogram: j == 0,
+				ColumnIDs:    colIDs,
+				HasHistogram: j == 0 && canHistogramType(col.Type),
 			})
 		}
 	}
 
-	// Add all remaining non-json columns in the table, up to maxNonIndexCols.
+	// Add all remaining columns in the table, up to maxNonIndexCols.
 	nonIdxCols := 0
 	for i := 0; i < len(desc.Columns) && nonIdxCols < maxNonIndexCols; i++ {
 		col := &desc.Columns[i]
 		colList := []sqlbase.ColumnID{col.ID}
 		key := makeColStatKey(colList)
-		if _, ok := requestedStats[key]; !ok && col.Type.Family() != types.JsonFamily {
+		if _, ok := requestedStats[key]; !ok {
 			colStats = append(colStats, jobspb.CreateStatsDetails_ColStat{
 				ColumnIDs:    colList,
 				HasHistogram: col.Type.Family() == types.BoolFamily || col.Type.Family() == types.EnumFamily,
