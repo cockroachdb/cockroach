e6edb834ce78f7b57cf31207916d6ec2e11ae881
Oliver Tan <otan@cockroachlabs.com>
2020-05-18 10:02:16 -0700

    geo/geomfn: implement DWithin for geometry types
    Moved the core of the logic for distance calculation operations to the
    `geodist` package, with required methods interface-d into the package
    for geometry/geography calculations. The code is mostly the same as it
    was in geogfn/distance.go, just the spheroid/sphere calculations mvoed
    out.
    
    The geometry based operations are contained in geomfn/distance.go. All
    math is annotated with links as well.
    
    Changed MinDistance to use the self-made implementation. The tests still
    call GEOS to make sure the return values are similar.
    
    Needed to bring some vendored packages in from go-geom for this to work.
    
    Release note (sql change): Implemented DWithin for Geometry types.

--- pkg/geo/geomfn/geomfn.go
+++ pkg/geo/geomfn/geomfn.go
@@ -10,3 +10,55 @@
 
 // Package geomfn contains functions that are used for geometry-based builtins.
 package geomfn
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/geo"
+	"github.com/cockroachdb/errors"
+	"github.com/twpayne/go-geom"
+)
+
+// flattenGeometry flattens a geo.Geometry object.
+func flattenGeometry(g *geo.Geometry) ([]geom.T, error) {
+	f, err := g.AsGeomT()
+	if err != nil {
+		return nil, err
+	}
+	return flattenGeomT(f)
+}
+
+// flattenGeomT decomposes geom.T collections to individual geom.T components.
+func flattenGeomT(g geom.T) ([]geom.T, error) {
+	switch g := g.(type) {
+	case *geom.Point:
+		return []geom.T{g}, nil
+	case *geom.LineString:
+		return []geom.T{g}, nil
+	case *geom.Polygon:
+		return []geom.T{g}, nil
+	case *geom.MultiPoint:
+		ret := make([]geom.T, g.NumPoints())
+		for i := 0; i < g.NumPoints(); i++ {
+			ret[i] = g.Point(i)
+		}
+		return ret, nil
+	case *geom.MultiLineString:
+		ret := make([]geom.T, g.NumLineStrings())
+		for i := 0; i < g.NumLineStrings(); i++ {
+			ret[i] = g.LineString(i)
+		}
+		return ret, nil
+	case *geom.MultiPolygon:
+		ret := make([]geom.T, g.NumPolygons())
+		for i := 0; i < g.NumPolygons(); i++ {
+			ret[i] = g.Polygon(i)
+		}
+		return ret, nil
+	case *geom.GeometryCollection:
+		ret := make([]geom.T, g.NumGeoms())
+		for i := 0; i < g.NumGeoms(); i++ {
+			ret[i] = g.Geom(i)
+		}
+		return ret, nil
+	}
+	return nil, errors.Newf("unknown geom: %T", g)
+}
