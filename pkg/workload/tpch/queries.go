852a2b397bb89383822a26e2cd65cdbecdb6d6ee
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-05-21 12:26:41 -0700

    workload: switch TPCH queries mapping from using string to int
    It also exports `NumQueries` and `QueriesByNumber` fields of `tpch`
    package to be used by `tpchvec` roachtest.
    
    Release note: None

--- pkg/workload/tpch/queries.go
+++ pkg/workload/tpch/queries.go
@@ -10,30 +10,37 @@
 
 package tpch
 
-var queriesByName = map[string]string{
-	`1`:  query1,
-	`2`:  query2,
-	`3`:  query3,
-	`4`:  query4,
-	`5`:  query5,
-	`6`:  query6,
-	`7`:  query7,
-	`8`:  query8,
-	`9`:  query9,
-	`10`: query10,
-	`11`: query11,
-	`12`: query12,
-	`13`: query13,
-	`14`: query14,
-	`15`: query15,
-	`16`: query16,
-	`17`: query17,
-	`18`: query18,
-	`19`: query19,
-	`20`: query20,
-	`21`: query21,
-	`22`: query22,
-}
+var (
+	// QueriesByNumber is a mapping from the number of a TPC-H query to the actual
+	// query.
+	QueriesByNumber = map[int]string{
+		1:  query1,
+		2:  query2,
+		3:  query3,
+		4:  query4,
+		5:  query5,
+		6:  query6,
+		7:  query7,
+		8:  query8,
+		9:  query9,
+		10: query10,
+		11: query11,
+		12: query12,
+		13: query13,
+		14: query14,
+		15: query15,
+		16: query16,
+		17: query17,
+		18: query18,
+		19: query19,
+		20: query20,
+		21: query21,
+		22: query22,
+	}
+
+	// NumQueries specifies the number of queries in TPC-H benchmark.
+	NumQueries = len(QueriesByNumber)
+)
 
 const (
 	query1 = `
