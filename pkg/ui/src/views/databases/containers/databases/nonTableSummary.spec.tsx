f587bfada740580f980cda804c14aa8614d0954c
Andrii Vorobiov <and.vorobiov@gmail.com>
2020-05-27 13:19:24 +0300

    ui: Error message for non-table data on Databases page
    Previously, error messages received on request for
    non-table data on Databases page is not handled and not
    displayed for users.
    
    Now, default error message is shown in response on errors.
    
    Release note (admin ui change): Show default error message
    about restricted permissions for non-admin users on
    Databases page.

---
+++ pkg/ui/src/views/databases/containers/databases/nonTableSummary.spec.tsx
@@ -0,0 +1,72 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import React from "react";
+import { shallow } from "enzyme";
+import { noop } from "lodash";
+import { assert } from "chai";
+import Long from "long";
+
+import "src/enzymeInit";
+import { NonTableSummary } from "./nonTableSummary";
+import { refreshNonTableStats } from "src/redux/apiReducers";
+import { cockroach } from "src/js/protos";
+import Loading from "src/views/shared/components/loading";
+import NonTableStatsResponse = cockroach.server.serverpb.NonTableStatsResponse;
+
+describe("NonTableSummary", () => {
+  describe("Loading data", () => {
+    it("successfully loads data", () => {
+      const tableStatsData = new NonTableStatsResponse({
+        internal_use_stats: {
+          approximate_disk_bytes: Long.fromNumber(1),
+          missing_nodes: [],
+          node_count: Long.fromNumber(1),
+          range_count: Long.fromNumber(1),
+          replica_count: Long.fromNumber(1),
+          stats: null,
+        },
+        time_series_stats: {
+          approximate_disk_bytes: Long.fromNumber(1),
+          missing_nodes: [],
+          node_count: Long.fromNumber(1),
+          range_count: Long.fromNumber(1),
+          replica_count: Long.fromNumber(1),
+          stats: null,
+        },
+      });
+      const wrapper = shallow(<NonTableSummary
+        nonTableStats={tableStatsData}
+        nonTableStatsValid={true}
+        refreshNonTableStats={noop as typeof refreshNonTableStats}
+        lastError={undefined} />);
+      const loadingWrapper = wrapper.find(Loading).dive();
+      assert.isTrue(loadingWrapper.find(".database-summary-table").exists());
+    });
+
+    it("shows error message when failed request", () => {
+      const error = {
+        name: "Forbidden",
+        message: "Insufficient privileges to view this resource",
+      };
+      const wrapper = shallow(<NonTableSummary
+        nonTableStats={null}
+        nonTableStatsValid={true}
+        refreshNonTableStats={noop as typeof refreshNonTableStats}
+        lastError={error} />);
+
+      const loadingWrapper = wrapper.find(Loading).dive();
+      assert.isTrue(loadingWrapper.exists());
+
+      const errorText = loadingWrapper.find("li > b").at(0).text();
+      assert.equal(errorText, error.message);
+    });
+  });
+});
