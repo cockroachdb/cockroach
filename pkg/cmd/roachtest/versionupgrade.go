642c03e01ca571c570fb448d7936807fde546048
Spas Bojanov <spas@cockroachlabs.com>
2020-05-27 09:26:45 -0400

    roachtest/schemachange: specify concurrency
    This is a small refactor so that the roachtest can specify
    the concurrency in addition to the max_ops for the
    schemachange workload
    
    Release note: none.

--- pkg/cmd/roachtest/versionupgrade.go
+++ pkg/cmd/roachtest/versionupgrade.go
@@ -99,7 +99,7 @@ func runVersionUpgrade(ctx context.Context, t *test, c *cluster, buildVersion ve
 	}
 
 	testFeaturesStep := versionUpgradeTestFeatures.step(c.All())
-	schemaChangeStep := runSchemaChangeWorkloadStep(10 /* maxOps */)
+	schemaChangeStep := runSchemaChangeWorkloadStep(c.All().randNode()[0], 10 /* maxOps */, 2 /* concurrency */)
 
 	// The steps below start a cluster at predecessorVersion (from a fixture),
 	// then start an upgrade that is rolled back, and finally start and finalize
