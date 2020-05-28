4a4bca7771dd9b2bac13e44afa5c9a04608d3a3e
Marcus Gartner <marcus@cockroachlabs.com>
2020-05-28 09:43:28 -0700

    sql: create schemaexpr package
    This commit adds a new package for dealing with expressions within table
    schemas, such as check constraints, computed columns, and partial index
    predicates.
    
    The utility provided previously by the following functions have been moved
    and refactored into this new package:
    
      dequalifyColumnRefs - Now provided by DequalifyColumnRefs.
    
      makeCheckConstraint - Now provided by CheckConstraintBuilder.
    
      validateComputedColumn - Now provided by ComputedColumnValidator.
    
      validateIndexPredicate - Now provided by IndexPredicateValidator.
    
    In addition, several helper functions were refactored into the new
    package, including generateMaybeDuplicateNameForCheckConstraint,
    generateNameForCheckConstraint, iterColDescriptorsInExpr, and
    replaceVars. dummyColumnItems were also moved into the new package.
    
    Release note: None

---
+++ pkg/sql/schemaexpr/column.go
@@ -0,0 +1,186 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package schemaexpr
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgcode"
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
+	"github.com/cockroachdb/cockroach/pkg/sql/types"
+)
+
+// DequalifyColumnRefs returns an expression with database nad table names
+// stripped from qualified column names.
+//
+// For example:
+//
+//   tab.a > 0 AND db.tab.b = 'foo'
+//   =>
+//   a > 0 AND b = 'foo'
+//
+// This dequalification is necessary when CHECK constraints, computed columns,
+// or partial index predicates are created. If the table name was not stripped,
+// these expressions would become invalid if the table is renamed.
+func DequalifyColumnRefs(
+	ctx context.Context, source *sqlbase.DataSourceInfo, expr tree.Expr,
+) (tree.Expr, error) {
+	resolver := sqlbase.ColumnResolver{Source: source}
+	return tree.SimpleVisit(
+		expr,
+		func(expr tree.Expr) (recurse bool, newExpr tree.Expr, err error) {
+			if vBase, ok := expr.(tree.VarName); ok {
+				v, err := vBase.NormalizeVarName()
+				if err != nil {
+					return false, nil, err
+				}
+				if c, ok := v.(*tree.ColumnItem); ok {
+					_, err := c.Resolve(ctx, &resolver)
+					if err != nil {
+						return false, nil, err
+					}
+					colIdx := resolver.ResolverState.ColIdx
+					col := source.SourceColumns[colIdx]
+					return false, &tree.ColumnItem{ColumnName: tree.Name(col.Name)}, nil
+				}
+			}
+			return true, expr, err
+		},
+	)
+}
+
+// iterColDescriptors iterates over the expression's variable columns and
+// calls f on each.
+//
+// If the expression references a column that does not exist in the table
+// descriptor, iterColDescriptors errs with pgcode.UndefinedColumn.
+func iterColDescriptors(
+	desc *sqlbase.MutableTableDescriptor, rootExpr tree.Expr, f func(*sqlbase.ColumnDescriptor) error,
+) error {
+	_, err := tree.SimpleVisit(rootExpr, func(expr tree.Expr) (recurse bool, newExpr tree.Expr, err error) {
+		vBase, ok := expr.(tree.VarName)
+		if !ok {
+			// Not a VarName, don't do anything to this node.
+			return true, expr, nil
+		}
+
+		v, err := vBase.NormalizeVarName()
+		if err != nil {
+			return false, nil, err
+		}
+
+		c, ok := v.(*tree.ColumnItem)
+		if !ok {
+			return true, expr, nil
+		}
+
+		col, dropped, err := desc.FindColumnByName(c.ColumnName)
+		if err != nil || dropped {
+			return false, nil, pgerror.Newf(pgcode.UndefinedColumn,
+				"column %q does not exist, referenced in %q", c.ColumnName, rootExpr.String())
+		}
+
+		if err := f(col); err != nil {
+			return false, nil, err
+		}
+		return false, expr, err
+	})
+
+	return err
+}
+
+// dummyColumn represents a variable column that can type-checked. It is used
+// in validating check constraint and partial index predicate expressions. This
+// validation requires that the expression can be both both typed-checked and
+// examined for variable expressions.
+type dummyColumn struct {
+	typ *types.T
+	// name is only used for error-reporting.
+	name tree.Name
+}
+
+// String implements the Stringer interface.
+func (d *dummyColumn) String() string {
+	return tree.AsString(d)
+}
+
+// Format implements the NodeFormatter interface.
+func (d *dummyColumn) Format(ctx *tree.FmtCtx) {
+	d.name.Format(ctx)
+}
+
+// Walk implements the Expr interface.
+func (d *dummyColumn) Walk(_ tree.Visitor) tree.Expr {
+	return d
+}
+
+// TypeCheck implements the Expr interface.
+func (d *dummyColumn) TypeCheck(
+	_ context.Context, _ *tree.SemaContext, desired *types.T,
+) (tree.TypedExpr, error) {
+	return d, nil
+}
+
+// Eval implements the TypedExpr interface.
+func (*dummyColumn) Eval(_ *tree.EvalContext) (tree.Datum, error) {
+	panic("dummyColumnItem.Eval() is undefined")
+}
+
+// ResolvedType implements the TypedExpr interface.
+func (d *dummyColumn) ResolvedType() *types.T {
+	return d.typ
+}
+
+// replaceVars replaces the occurrences of column names in an expression with
+// dummyColumns containing their type, so that they may be type-checked. It
+// returns this new expression tree alongside a set containing the ColumnID of
+// each column seen in the expression.
+//
+// If the expression references a column that does not exist in the table
+// descriptor, replaceVars errs with pgcode.UndefinedColumn.
+// TODO(mgartner): The set should be a util.FastIntSet.
+func replaceVars(
+	desc *sqlbase.MutableTableDescriptor, rootExpr tree.Expr,
+) (tree.Expr, map[sqlbase.ColumnID]struct{}, error) {
+	colIDs := make(map[sqlbase.ColumnID]struct{})
+
+	newExpr, err := tree.SimpleVisit(rootExpr, func(expr tree.Expr) (recurse bool, newExpr tree.Expr, err error) {
+		vBase, ok := expr.(tree.VarName)
+		if !ok {
+			// Not a VarName, don't do anything to this node.
+			return true, expr, nil
+		}
+
+		v, err := vBase.NormalizeVarName()
+		if err != nil {
+			return false, nil, err
+		}
+
+		c, ok := v.(*tree.ColumnItem)
+		if !ok {
+			return true, expr, nil
+		}
+
+		col, dropped, err := desc.FindColumnByName(c.ColumnName)
+		if err != nil || dropped {
+			return false, nil, pgerror.Newf(pgcode.UndefinedColumn,
+				"column %q does not exist, referenced in %q", c.ColumnName, rootExpr.String())
+		}
+		colIDs[col.ID] = struct{}{}
+
+		// Convert to a dummyColumn of the correct type.
+		return false, &dummyColumn{typ: col.Type, name: c.ColumnName}, nil
+	})
+
+	return newExpr, colIDs, err
+}
---
+++ pkg/sql/schemaexpr/partial_index_test.go
@@ -0,0 +1,140 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package schemaexpr
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/parser"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/builtins"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
+	"github.com/cockroachdb/cockroach/pkg/sql/types"
+)
+
+func TestIndexPredicateValidator_Validate(t *testing.T) {
+	ctx := context.Background()
+	semaCtx := tree.MakeSemaContext()
+
+	// Trick to get the init() for the builtins package to run.
+	_ = builtins.AllBuiltinNames
+
+	database := tree.Name("foo")
+	table := tree.Name("bar")
+	tn := tree.MakeTableName(database, table)
+
+	desc := sqlbase.MutableTableDescriptor{
+		TableDescriptor: sqlbase.TableDescriptor{
+			Name: string(table),
+			ID:   1,
+			Columns: []sqlbase.ColumnDescriptor{
+				{
+					Name: "a",
+					ID:   1,
+					Type: types.Bool,
+				},
+				{
+					Name: "b",
+					ID:   2,
+					Type: types.Int,
+				},
+			},
+			Mutations: []sqlbase.DescriptorMutation{
+				{
+					Descriptor_: &sqlbase.DescriptorMutation_Column{
+						Column: &sqlbase.ColumnDescriptor{
+							Name: "c",
+							ID:   3,
+							Type: types.String,
+						},
+					},
+					Direction: sqlbase.DescriptorMutation_ADD,
+				},
+			},
+		},
+	}
+
+	validator := NewIndexPredicateValidator(ctx, tn, &desc, &semaCtx)
+
+	testData := []struct {
+		expr          string
+		expectedValid bool
+		expectedExpr  string
+	}{
+		// Allow expressions that result in a bool.
+		{"a", true, "a"},
+		{"b = 0", true, "b = 0"},
+		{"a AND b = 0", true, "a AND (b = 0)"},
+		{"a IS NULL", true, "a IS NULL"},
+		{"b IN (1, 2)", true, "b IN (1:::INT8, 2:::INT8)"},
+
+		// Allow immutable functions.
+		{"abs(b) > 0", true, "abs(b) > 0"},
+		{"c || c = 'foofoo'", true, "(c || c) = 'foofoo'"},
+		{"lower(c) = 'bar'", true, "lower(c) = 'bar'"},
+
+		// Disallow references to columns not in the table.
+		{"d", false, ""},
+		{"t.a", false, ""},
+
+		// Disallow expressions that do not result in a bool.
+		{"b", false, ""},
+		{"abs(b)", false, ""},
+		{"lower(c)", false, ""},
+
+		// Disallow subqueries.
+		{"exists(select 1)", false, ""},
+		{"b IN (select 1)", false, ""},
+
+		// Disallow mutable, aggregate, window, and set returning functions.
+		{"b > random()", false, ""},
+		{"sum(b) > 10", false, ""},
+		{"row_number() OVER () > 1", false, ""},
+		{"generate_series(1, 1) > 2", false, ""},
+
+		// Dequalify column names.
+		{"bar.a", true, "a"},
+		{"foo.bar.a", true, "a"},
+		{"bar.b = 0", true, "b = 0"},
+		{"foo.bar.b = 0", true, "b = 0"},
+		{"bar.a AND foo.bar.b = 0", true, "a AND (b = 0)"},
+	}
+
+	for _, d := range testData {
+		t.Run(d.expr, func(t *testing.T) {
+			expr, err := parser.ParseExpr(d.expr)
+			if err != nil {
+				t.Fatalf("%s: unexpected error: %s", d.expr, err)
+			}
+
+			r, err := validator.Validate(expr)
+
+			if !d.expectedValid {
+				if err == nil {
+					t.Fatalf("%s: expected invalid expression, but was valid", d.expr)
+				}
+				// The input expression is invalid so there is no need to check
+				// the output expression r.
+				return
+			}
+
+			if err != nil {
+				t.Fatalf("%s: expected valid expression, but found error: %s", d.expr, err)
+			}
+
+			s := tree.Serialize(r)
+			if s != d.expectedExpr {
+				t.Errorf("%s: expected %q, got %q", d.expr, d.expectedExpr, s)
+			}
+		})
+	}
+}
