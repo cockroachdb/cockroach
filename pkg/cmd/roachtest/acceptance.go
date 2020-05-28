c896531c23fd79ef48c382a9e85aef3a138a2388
Spas Bojanov <spas@cockroachlabs.com>
2020-05-28 15:16:30 -0400

    roachtest/version-upgrade: don't run schema change workload on 19.2 releases
    Fixes #47024.
    
    Release note (bug fix):
    The schema change workload is meant for testing the behavior of schema
    changes on clusters with nodes with min version 19.2. It will deadlock
    on earlier versions.

--- pkg/cmd/roachtest/acceptance.go
+++ pkg/cmd/roachtest/acceptance.go
@@ -51,11 +51,7 @@ func registerAcceptance(r *testRegistry) {
 		{
 			name: "version-upgrade",
 			fn: func(ctx context.Context, t *test, c *cluster) {
-				predV, err := PredecessorVersion(r.buildVersion)
-				if err != nil {
-					t.Fatal(err)
-				}
-				runVersionUpgrade(ctx, t, c, predV)
+				runVersionUpgrade(ctx, t, c, r.buildVersion)
 			},
 			// This test doesn't like running on old versions because it upgrades to
 			// the latest released version and then it tries to "head", where head is
