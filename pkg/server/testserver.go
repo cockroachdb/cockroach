317d80e3d79184d9388874e0774407f8bcdd8d55
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2020-05-29 15:45:35 +0200

    server: use unique fake node ID in StartTenant
    We had previously hard-coded a NodeID of 1 (matching the underlying
    TestServer's NodeID) to make "things work" for SQL tenants. The
    preceding commits lifted this restriction, so we now use a (static)
    NodeID which is highly unlikely to match any NodeID from the KV
    layer.
    
    Release note: None

--- pkg/server/testserver.go
+++ pkg/server/testserver.go
@@ -424,7 +424,7 @@ func (d dummyProtectedTSProvider) Protect(context.Context, *kv.Txn, *ptpb.Record
 // TODO(asubiotto): Jobs don't play well with a weird node ID in a multitenant
 //  environment, so a node ID of 1 is used here to get tests to pass. Fixing
 //  this is tracked in https://github.com/cockroachdb/cockroach/issues/47892.
-const fakeNodeID = roachpb.NodeID(1)
+const fakeNodeID = roachpb.NodeID(123456789)
 
 func testSQLServerArgs(ts *TestServer) sqlServerArgs {
 	stopper := ts.Stopper()
