444216e6afc32b7f5bfd8e50f002929ba2eae8bb
Drew Kimball <andrewekimball@gmail.com>
2020-05-28 19:17:12 -0700

    opt: add rule to split a constrained scan under a limit into a union of Scans
    This patch adds a rule that replaces a constrained Scan under a limit
    with a union of Scans with hard limits. Each new Scan scans over a
    single key from the original constraint. For now, this rule only
    matches when there are 16 or less keys in the original Scan constraint.
    
    Making this replacement can lend a significant speedup to queries that
    take a max or min over a column that is part of an index, but not the
    first index column.
    
    ```
    CREATE TABLE partitioned_data (
    	region STRING,
    	data INT NOT NULL,
    	INDEX (region, data)
    );
    SELECT max(data) FROM partitioned_data WHERE region='US_WEST' OR region='US_EAST';
    ```
    This query previously produced this plan:
    ```
    scalar-group-by
     ├── scan partitioned_data@secondary
     │    └── constraint: /1/2/3
     │         ├── [/'US_EAST' - /'US_EAST']
     │         └── [/'US_WEST' - /'US_WEST']
     └── aggregations
          └── max
               └── data
    ```
    In this example, the max value of "data" for any given "region" value
    is located in the last row that has that "region" value. This rule
    creates two scans, each of which would only scan a single row, which
    would be the max for that "region" value. From there, the two rows
    are sorted and and another limit grabs the row with the max value for
    "data":
    ```
    scalar-group-by
     ├── limit
     │    ├── sort
     │    │    └── union
     │    │         ├── scan partitioned_data@secondary
     │    │         │    ├── constraint: /1/2/3: (/'US_EAST' - /'US_EAST')
     │    │         │    └── limit: 1
     │    │         └── scan partitioned_data@secondary
     │    │              ├── constraint: /5/6/7: (/'US_WEST' - /'US_WEST')
     │    │              └── limit: 1
     │    └── 1
     └── aggregations
          └── const-agg
               └── data
    ```
    This takes the query from potentially scanning thousands of rows to
    only scanning two.
    
    Release note: None

--- pkg/sql/opt/constraint/spans.go
+++ pkg/sql/opt/constraint/spans.go
@@ -149,6 +149,77 @@ func (s *Spans) SortAndMerge(keyCtx *KeyContext) {
 	s.Truncate(n + 1)
 }
 
+// KeyCount returns the number of distinct keys contained in the spans. See
+// span.KeyCount for conditions under which this is possible. Example:
+//
+//    KeyCount('[/'ASIA' - /'ASIA'] [/0/1 - /0/4]') == 5
+//
+func (s *Spans) KeyCount(keyCtx *KeyContext) (int64, bool) {
+	keyCount := int64(0)
+	for i, cnt := 0, s.Count(); i < cnt; i++ {
+		cnt, ok := s.Get(i).KeyCount(keyCtx)
+		if !ok {
+			return 0, false
+		}
+		keyCount += cnt
+		if keyCount < 0 {
+			// Overflow.
+			return 0, false
+		}
+	}
+	return keyCount, true
+}
+
+// ExtractSingleKeySpans returns a new Spans struct containing a span for each
+// key in the given spans. Returns nil and false if one of the following is
+// true:
+//   1. The number of new spans exceeds the given limit value.
+//   2. The spans don't all have the same key length (length as in
+//      length(/3/'two'/1) == 3).
+//   3. span.Split is unsuccessful for any of the spans.
+//
+// Example:
+//
+//    ExtractSingleKeySpans('[/'ASIA' - /'ASIA'] [/0/1 - /0/4]')
+//    =>
+//    '[/'ASIA' - /'ASIA'] [/0/1 - /0/1] [/0/2 - /0/2] [/0/3 - /0/3] [/0/4 - /0/4]'
+//
+func (s *Spans) ExtractSingleKeySpans(keyCtx *KeyContext, limit int64) (*Spans, bool) {
+	// Ensure that the number of keys in the spans does not exceed the limit.
+	keyCount, ok := s.KeyCount(keyCtx)
+	if !ok || keyCount > limit {
+		return nil, false
+	}
+
+	// Ensure that the key lengths are consistent between spans.
+	keyLength := -1
+	for i, spanCnt := 0, s.Count(); i < spanCnt; i++ {
+		span := s.Get(i)
+		if keyLength == -1 {
+			// Initialize keyLength.
+			keyLength = span.StartKey().Length()
+		}
+		if span.StartKey().Length() != keyLength {
+			// The spans don't all have the same key length.
+			return nil, false
+		}
+	}
+
+	// Construct the new single-key spans.
+	newSpans := Spans{}
+	for i, spanCnt := 0, s.Count(); i < spanCnt; i++ {
+		splitSpans, ok := s.Get(i).Split(keyCtx, limit-int64(newSpans.Count()))
+		if !ok {
+			// The span could not be split into single key spans.
+			return nil, false
+		}
+		for i, cnt := 0, splitSpans.Count(); i < cnt; i++ {
+			newSpans.Append(splitSpans.Get(i))
+		}
+	}
+	return &newSpans, true
+}
+
 type spanSorter struct {
 	keyCtx KeyContext
 	spans  *Spans
