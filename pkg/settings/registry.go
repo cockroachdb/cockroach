b7f8f5627d7d00e9035c5f8dfced8759f63fce36
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-05-29 18:25:31 -0700

    settings: retire two settings removed in 20.1
    Release note: None

--- pkg/settings/registry.go
+++ pkg/settings/registry.go
@@ -62,6 +62,8 @@ var retiredSettings = map[string]struct{}{
 	// removed as of 20.1.
 	"schemachanger.lease.duration":       {},
 	"schemachanger.lease.renew_fraction": {},
+	"sql.distsql.temp_storage.joins":     {},
+	"sql.distsql.temp_storage.sorts":     {},
 	// removes as of 20.2.
 	"rocksdb.ingest_backpressure.pending_compaction_threshold": {},
 }
