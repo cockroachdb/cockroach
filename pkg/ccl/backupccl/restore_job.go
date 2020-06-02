c1072973b8a72ff9e88eae5a16e0b0ced8d10840
anzoteh96 <anzot@cockroachlabs.com>
2020-06-02 15:09:45 -0400

    backupccl: move Statistics field outside of BackupManifest and store it separately.
    Currently, backup manifests contain the Statistics field, which is an array
    of TableStatisticProto objects (histograms and other tables). Given the
    size of the field, and the fact that backup manifests and frequently
    downloaded (e.g. during incremental backup stage), the Statistics field
    becomes a significant overhead to backup manifest files.
    
    This PR moves this field into separate storage of these information, keeping backup
    manifest files in a reasonable amount of size. This TableStatisticsProto array is
    then retrieved only when necessary.
    
    Release note: None

--- pkg/ccl/backupccl/restore_job.go
+++ pkg/ccl/backupccl/restore_job.go
@@ -756,6 +756,8 @@ func restore(
 // can't be restored because the necessary tables are missing are omitted; if
 // skip_missing_foreign_keys was set, we should have aborted the RESTORE and
 // returned an error prior to this.
+// TODO: this method returns two things: the sqlbase.Descriptor and the backup manifest.
+// Ideally, this should be broken down into two methods.
 func loadBackupSQLDescs(
 	ctx context.Context,
 	p sql.PlanHookState,
@@ -796,6 +798,29 @@ type restoreResumer struct {
 	execCfg            *sql.ExecutorConfig
 }
 
+// getStatisticsFromBackup retrieves Statistics from backup manifest,
+// either through the Statistics field or from the files.
+func getStatisticsFromBackup(
+	ctx context.Context,
+	exportStore cloud.ExternalStorage,
+	encryption *roachpb.FileEncryptionOptions,
+	backup BackupManifest,
+) []*stats.TableStatisticProto {
+	if backup.Statistics != nil {
+		return backup.Statistics
+	}
+	tableStatistics := make([]*stats.TableStatisticProto, 0, len(backup.Statistics))
+	for _, fname := range backup.StatisticsFilenames {
+		myStatsTable, err := readTableStatistics(ctx, exportStore, fname, encryption)
+		if err != nil {
+			return tableStatistics
+		}
+		tableStatistics = append(tableStatistics, myStatsTable.Statistics...)
+	}
+
+	return tableStatistics
+}
+
 // remapRelevantStatistics changes the table ID references in the stats
 // from those they had in the backed up database to what they should be
 // in the restored database.
@@ -803,12 +828,11 @@ type restoreResumer struct {
 // being restored. If the tableRewrites can re-write the table ID, then that
 // table is being restored.
 func remapRelevantStatistics(
-	backup BackupManifest, tableRewrites TableRewriteMap,
+	tableStatistics []*stats.TableStatisticProto, tableRewrites TableRewriteMap,
 ) []*stats.TableStatisticProto {
-	relevantTableStatistics := make([]*stats.TableStatisticProto, 0, len(backup.Statistics))
+	relevantTableStatistics := make([]*stats.TableStatisticProto, 0, len(tableStatistics))
 
-	for i := range backup.Statistics {
-		stat := backup.Statistics[i]
+	for _, stat := range tableStatistics {
 		tableRewrite, ok := tableRewrites[stat.TableID]
 		if !ok {
 			// Table re-write not present, so statistic should not be imported.
@@ -951,9 +975,20 @@ func (r *restoreResumer) Resume(
 	details := r.job.Details().(jobspb.RestoreDetails)
 	p := phs.(sql.PlanHookState)
 
+	// The lastInd information is needed to know the index of latestBackUpManifest
+	// in order to retrieve the corresponding URI.
 	backupManifests, latestBackupManifest, sqlDescs, err := loadBackupSQLDescs(
 		ctx, p, details, details.Encryption,
 	)
+	lastInd := getBackupIndAtTime(backupManifests, details.EndTime)
+	if err != nil {
+		return err
+	}
+	defaultConf, err := cloud.ExternalStorageConfFromURI(details.URIs[lastInd])
+	if err != nil {
+		return errors.Wrapf(err, "creating external store configuration")
+	}
+	defaultStore, err := p.ExecCfg().DistSQLSrv.ExternalStorage(ctx, defaultConf)
 	if err != nil {
 		return err
 	}
@@ -966,7 +1001,8 @@ func (r *restoreResumer) Resume(
 	r.descriptorCoverage = details.DescriptorCoverage
 	r.databases = databases
 	r.execCfg = p.ExecCfg()
-	r.latestStats = remapRelevantStatistics(latestBackupManifest, details.TableRewrites)
+	backupStats := getStatisticsFromBackup(ctx, defaultStore, details.Encryption, latestBackupManifest)
+	r.latestStats = remapRelevantStatistics(backupStats, details.TableRewrites)
 
 	if len(r.tables) == 0 {
 		// We have no tables to restore (we are restoring an empty DB).
