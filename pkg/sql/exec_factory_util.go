1071544e75450be5d3742923fc5b58f0b4da8d0e
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-06-02 15:07:48 -0700

    sql: implement ConstructPlan by new factory
    This commit extracts the logic of `execFactory.ConstructPlan` into the
    function to be used by both factories. It also extracts out another
    small helper.
    
    Release note: None

---
+++ pkg/sql/exec_factory_util.go
@@ -0,0 +1,78 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package sql
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/exec"
+	"github.com/cockroachdb/cockroach/pkg/sql/rowexec"
+	"github.com/cockroachdb/errors"
+)
+
+func constructPlan(
+	planner *planner,
+	root exec.Node,
+	subqueries []exec.Subquery,
+	cascades []exec.Cascade,
+	checks []exec.Node,
+	usesPlanNodeRepresentation bool,
+) (exec.Plan, error) {
+	res := &planTop{
+		// TODO(radu): these fields can be modified by planning various opaque
+		// statements. We should have a cleaner way of plumbing these.
+		avoidBuffering:  planner.curPlan.avoidBuffering,
+		auditEvents:     planner.curPlan.auditEvents,
+		instrumentation: planner.curPlan.instrumentation,
+	}
+	assignPlan := func(plan *planMaybePhysical, node exec.Node) {
+		if usesPlanNodeRepresentation {
+			plan.planNode = node.(planNode)
+		} else {
+			*plan = node.(planMaybePhysical)
+		}
+	}
+	assignPlan(&res.main, root)
+	if len(subqueries) > 0 {
+		res.subqueryPlans = make([]subquery, len(subqueries))
+		for i := range subqueries {
+			in := &subqueries[i]
+			out := &res.subqueryPlans[i]
+			out.subquery = in.ExprNode
+			switch in.Mode {
+			case exec.SubqueryExists:
+				out.execMode = rowexec.SubqueryExecModeExists
+			case exec.SubqueryOneRow:
+				out.execMode = rowexec.SubqueryExecModeOneRow
+			case exec.SubqueryAnyRows:
+				out.execMode = rowexec.SubqueryExecModeAllRowsNormalized
+			case exec.SubqueryAllRows:
+				out.execMode = rowexec.SubqueryExecModeAllRows
+			default:
+				return nil, errors.Errorf("invalid SubqueryMode %d", in.Mode)
+			}
+			out.expanded = true
+			assignPlan(&out.plan, in.Root)
+		}
+	}
+	if len(cascades) > 0 {
+		res.cascades = make([]cascadeMetadata, len(cascades))
+		for i := range cascades {
+			res.cascades[i].Cascade = cascades[i]
+		}
+	}
+	if len(checks) > 0 {
+		res.checkPlans = make([]checkPlan, len(checks))
+		for i := range checks {
+			assignPlan(&res.checkPlans[i].plan, checks[i])
+		}
+	}
+
+	return res, nil
+}
--- pkg/sql/opt_exec_factory.go
+++ pkg/sql/opt_exec_factory.go
@@ -26,7 +26,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/constraint"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/exec"
 	"github.com/cockroachdb/cockroach/pkg/sql/row"
-	"github.com/cockroachdb/cockroach/pkg/sql/rowexec"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/builtins"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/span"
@@ -131,10 +130,8 @@ func (ef *execFactory) ConstructScan(
 	scan.isFull = len(scan.spans) == 1 && scan.spans[0].EqualValue(
 		scan.desc.IndexSpan(ef.planner.ExecCfg().Codec, scan.index.ID),
 	)
-	for i := range reqOrdering {
-		if reqOrdering[i].ColIdx >= len(colCfg.wantedColumns) {
-			return nil, errors.Errorf("invalid reqOrdering: %v", reqOrdering)
-		}
+	if err = colCfg.assertValidReqOrdering(reqOrdering); err != nil {
+		return nil, err
 	}
 	scan.reqOrdering = ReqOrdering(reqOrdering)
 	scan.estimatedRowCount = uint64(rowCount)
@@ -1048,50 +1045,7 @@ func (ef *execFactory) ConstructPlan(
 	if spool, ok := root.(*spoolNode); ok {
 		root = spool.source
 	}
-	res := &planTop{
-		// TODO(radu): these fields can be modified by planning various opaque
-		// statements. We should have a cleaner way of plumbing these.
-		avoidBuffering:  ef.planner.curPlan.avoidBuffering,
-		auditEvents:     ef.planner.curPlan.auditEvents,
-		instrumentation: ef.planner.curPlan.instrumentation,
-	}
-	res.main.planNode = root.(planNode)
-	if len(subqueries) > 0 {
-		res.subqueryPlans = make([]subquery, len(subqueries))
-		for i := range subqueries {
-			in := &subqueries[i]
-			out := &res.subqueryPlans[i]
-			out.subquery = in.ExprNode
-			switch in.Mode {
-			case exec.SubqueryExists:
-				out.execMode = rowexec.SubqueryExecModeExists
-			case exec.SubqueryOneRow:
-				out.execMode = rowexec.SubqueryExecModeOneRow
-			case exec.SubqueryAnyRows:
-				out.execMode = rowexec.SubqueryExecModeAllRowsNormalized
-			case exec.SubqueryAllRows:
-				out.execMode = rowexec.SubqueryExecModeAllRows
-			default:
-				return nil, errors.Errorf("invalid SubqueryMode %d", in.Mode)
-			}
-			out.expanded = true
-			out.plan.planNode = in.Root.(planNode)
-		}
-	}
-	if len(cascades) > 0 {
-		res.cascades = make([]cascadeMetadata, len(cascades))
-		for i := range cascades {
-			res.cascades[i].Cascade = cascades[i]
-		}
-	}
-	if len(checks) > 0 {
-		res.checkPlans = make([]checkPlan, len(checks))
-		for i := range checks {
-			res.checkPlans[i].plan.planNode = checks[i].(planNode)
-		}
-	}
-
-	return res, nil
+	return constructPlan(ef.planner, root, subqueries, cascades, checks, true /* usesPlanNodeRepresentation */)
 }
 
 // urlOutputter handles writing strings into an encoded URL for EXPLAIN (OPT,
