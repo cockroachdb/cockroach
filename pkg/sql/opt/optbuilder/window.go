c8dfb20dc08e2bfdec158762369de22e40398f99
richardjcai <caioftherichard@gmail.com>
2020-06-02 23:39:33 -0400

    sql: track fine-grained view dependencies Fixes #29021 Only create view dependencies on columns if the column is referenced in the view query.
    Release note (sql change): Views now only create a dependency on a table's column if the
    column is referenced in the view definition. Previously, all columns were added as a dependency
    meaning if a table was referenced in a view, all columns regardless of if the column was actually
    referenced would be added to the view's dependencies.

--- pkg/sql/opt/optbuilder/window.go
+++ pkg/sql/opt/optbuilder/window.go
@@ -346,6 +346,9 @@ func (b *Builder) buildWindowArgs(
 	argList := make(memo.ScalarListExpr, len(argExprs))
 	for j, a := range argExprs {
 		col := outScope.findExistingCol(a, false /* allowSideEffects */)
+		if col != nil {
+			b.TrackReferencedColumnForViews(col)
+		}
 		if col == nil {
 			col = b.synthesizeColumn(
 				outScope,
@@ -374,6 +377,9 @@ func (b *Builder) buildWindowPartition(
 	cols := flattenTuples(partition)
 	for j, e := range cols {
 		col := outScope.findExistingCol(e, false /* allowSideEffects */)
+		if col != nil {
+			b.TrackReferencedColumnForViews(col)
+		}
 		if col == nil {
 			col = b.synthesizeColumn(
 				outScope,
@@ -400,6 +406,9 @@ func (b *Builder) buildWindowOrdering(
 
 		for _, e := range cols {
 			col := outScope.findExistingCol(e, false /* allowSideEffects */)
+			if col != nil {
+				b.TrackReferencedColumnForViews(col)
+			}
 			if col == nil {
 				col = b.synthesizeColumn(
 					outScope,
