62bf6a2c21ea506002d35efbfb5e5b61139be500
richardjcai <caioftherichard@gmail.com>
2020-06-02 23:39:33 -0400

    sql: track fine-grained view dependencies Fixes #29021 Only create view dependencies on columns if the column is referenced in the view query.
    Release note (sql change): Views now only create a dependency on a table's column if the
    column is referenced in the view definition. Previously, all columns were added as a dependency
    meaning if a table was referenced in a view, all columns regardless of if the column was actually
    referenced would be added to the view's dependencies.

--- pkg/sql/opt/optbuilder/scope.go
+++ pkg/sql/opt/optbuilder/scope.go
@@ -574,8 +574,14 @@ func findExistingColInList(
 // findExistingCol finds the given expression among the bound variables in this
 // scope. Returns nil if the expression is not found (or an expression is found
 // but it has side-effects and allowSideEffects is false).
+// If a column is found and we are tracking view dependencies, we add the column
+// to the view dependencies since it means this column is being referenced.
 func (s *scope) findExistingCol(expr tree.TypedExpr, allowSideEffects bool) *scopeColumn {
-	return findExistingColInList(expr, s.cols, allowSideEffects)
+	col := findExistingColInList(expr, s.cols, allowSideEffects)
+	if col != nil {
+		s.builder.trackReferencedColumnForViews(col)
+	}
+	return col
 }
 
 // startAggFunc is called when the builder starts building an aggregate
