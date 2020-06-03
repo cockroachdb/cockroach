96411bf1b36aef2b9685e4c637c47af7c20bf9b1
Andrew Werner <ajwerner@cockroachlabs.com>
2020-06-03 01:40:05 -0400

    sqlbase: introduce DatabaseDescriptorInterface and ImmutableDatabaseDescriptor
    Next we'll stop having the raw descriptor implement that interface.
    
    Release note: None

--- pkg/sql/sqlbase/database.go
+++ pkg/sql/sqlbase/database.go
@@ -9,14 +9,3 @@
 // licenses/APL.txt.
 
 package sqlbase
-
-import "github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
-
-// MakeDatabaseDesc constructs a DatabaseDescriptor from an AST node.
-func MakeDatabaseDesc(id ID, p *tree.CreateDatabase) DatabaseDescriptor {
-	return DatabaseDescriptor{
-		ID:         id,
-		Name:       string(p.Name),
-		Privileges: NewDefaultPrivilegeDescriptor(),
-	}
-}
