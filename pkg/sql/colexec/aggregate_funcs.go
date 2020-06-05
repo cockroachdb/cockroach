8ba895ca23ad3f64ff152b1ad35b53a147053216
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-06-05 13:03:12 -0700

    colexec: support min and max on int2 and int4
    Previously, `min` and `max` were disabled on INT2 and INT4 columns
    because there was a mismatch between the logical expected type (INT8)
    and the actual physical type (the type of the argument). Now this is
    fixed by performing the cast and some extra templating.
    
    Additionally, this commit cleans up the way we populate the output types
    of the aggregate functions as well as removes some redundant return
    values.
    
    Release note (sql change): Vectorized execution engine now support `MIN`
    and `MAX` aggregate functions on columns of INT2 and INT4 types.

--- pkg/sql/colexec/aggregate_funcs.go
+++ pkg/sql/colexec/aggregate_funcs.go
@@ -139,9 +139,9 @@ func newAggregateFuncsAlloc(
 		case execinfrapb.AggregatorSpec_COUNT:
 			funcAllocs[i] = newCountAggAlloc(allocator, allocSize)
 		case execinfrapb.AggregatorSpec_MIN:
-			funcAllocs[i], err = newMinAggAlloc(allocator, aggTyps[i][0], allocSize)
+			funcAllocs[i] = newMinAggAlloc(allocator, aggTyps[i][0], allocSize)
 		case execinfrapb.AggregatorSpec_MAX:
-			funcAllocs[i], err = newMaxAggAlloc(allocator, aggTyps[i][0], allocSize)
+			funcAllocs[i] = newMaxAggAlloc(allocator, aggTyps[i][0], allocSize)
 		case execinfrapb.AggregatorSpec_BOOL_AND:
 			funcAllocs[i] = newBoolAndAggAlloc(allocator, allocSize)
 		case execinfrapb.AggregatorSpec_BOOL_OR:
@@ -150,7 +150,7 @@ func newAggregateFuncsAlloc(
 		// function, make sure to account for the memory under that struct in
 		// its constructor.
 		default:
-			return nil, errors.Errorf("unsupported columnar aggregate function %s", aggFns[i].String())
+			return nil, errors.AssertionFailedf("didn't find aggregateFuncAlloc for %s", aggFns[i].String())
 		}
 
 		if err != nil {
@@ -190,32 +190,12 @@ func (a *aggregateFuncsAlloc) makeAggregateFuncs() []aggregateFunc {
 func makeAggregateFuncsOutputTypes(
 	aggTyps [][]*types.T, aggFns []execinfrapb.AggregatorSpec_Func,
 ) ([]*types.T, error) {
+	var err error
 	outTyps := make([]*types.T, len(aggFns))
-	for i := range aggFns {
-		// Set the output type of the aggregate.
-		if len(aggTyps[i]) > 0 {
-			// By default, the output type is the input type (if there is one).
-			outTyps[i] = aggTyps[i][0]
-		}
-		switch aggFns[i] {
-		case execinfrapb.AggregatorSpec_COUNT_ROWS, execinfrapb.AggregatorSpec_COUNT:
-			// TODO(jordan): this is a somewhat of a hack. The aggregate functions
-			// should come with their own output types, somehow.
-			outTyps[i] = types.Int
-		case execinfrapb.AggregatorSpec_AVG:
-			if typeconv.TypeFamilyToCanonicalTypeFamily(aggTyps[i][0].Family()) == types.IntFamily {
-				// Average of integers is a decimal.
-				outTyps[i] = types.Decimal
-			}
-		case execinfrapb.AggregatorSpec_SUM:
-			if typeconv.TypeFamilyToCanonicalTypeFamily(aggTyps[i][0].Family()) == types.IntFamily {
-				// Summation of integers ('sum', not 'sum_int') is a decimal.
-				outTyps[i] = types.Decimal
-			}
-		case execinfrapb.AggregatorSpec_SUM_INT:
-			// Integer summation of integers ('sum_int', not 'sum') is always
-			// INT8.
-			outTyps[i] = types.Int
+	for i, aggFn := range aggFns {
+		_, outTyps[i], err = execinfrapb.GetAggregateInfo(aggFn, aggTyps[i]...)
+		if err != nil {
+			return nil, err
 		}
 	}
 	return outTyps, nil
@@ -234,47 +214,25 @@ func extractAggTypes(aggCols [][]uint32, typs []*types.T) [][]*types.T {
 	return aggTyps
 }
 
-// isAggregateSupported returns whether the aggregate function that operates on
+// isAggregateSupported checks whether the aggregate function that operates on
 // columns of types 'inputTypes' (which can be empty in case of COUNT_ROWS) is
-// supported.
-func isAggregateSupported(
-	allocator *colmem.Allocator, aggFn execinfrapb.AggregatorSpec_Func, inputTypes []*types.T,
-) (bool, error) {
-	// We're only interested in resolving the aggregate functions and will not
-	// be actually creating them with the alloc, so we use 0 as the allocation
-	// size.
-	_, err := newAggregateFuncsAlloc(
-		allocator,
-		[][]*types.T{inputTypes},
-		[]execinfrapb.AggregatorSpec_Func{aggFn},
-		0, /* allocSize */
-	)
-	if err != nil {
-		return false, err
+// supported and returns an error if it isn't.
+func isAggregateSupported(aggFn execinfrapb.AggregatorSpec_Func, inputTypes []*types.T) error {
+	supported := false
+	for _, supportedAggFn := range SupportedAggFns {
+		if aggFn == supportedAggFn {
+			supported = true
+			break
+		}
 	}
-	outputTypes, err := makeAggregateFuncsOutputTypes(
+	if !supported {
+		return errors.Errorf("unsupported columnar aggregate function %s", aggFn.String())
+	}
+	_, err := makeAggregateFuncsOutputTypes(
 		[][]*types.T{inputTypes},
 		[]execinfrapb.AggregatorSpec_Func{aggFn},
 	)
-	if err != nil {
-		return false, err
-	}
-	_, retType, err := execinfrapb.GetAggregateInfo(aggFn, inputTypes...)
-	if err != nil {
-		return false, err
-	}
-	// The columnar aggregates will return the same physical output type as their
-	// input. However, our current builtin resolution might say that the return
-	// type is the canonical for the family (for example, MAX on INT4 is said to
-	// return INT8), so we explicitly check whether the type the columnar
-	// aggregate returns and the type the planning code will expect it to return
-	// are the same. If they are not, we fallback to row-by-row engine.
-	if !retType.Identical(outputTypes[0]) {
-		// TODO(yuzefovich): support this case through vectorize. Probably it needs
-		// to be done at the same time as #38845.
-		return false, errors.Newf("aggregates with different input and output types are not supported")
-	}
-	return true, nil
+	return err
 }
 
 type aggAllocBase struct {
@@ -348,5 +306,63 @@ func newSumIntAggAlloc(
 		}
 	default:
 		return nil, errors.Errorf("unsupported sum_int agg type %s", t.Name())
+	}
+}
+
+func newMinAggAlloc(allocator *colmem.Allocator, t *types.T, allocSize int64) aggregateFuncAlloc {
+	allocBase := aggAllocBase{allocator: allocator, allocSize: allocSize}
+	switch typeconv.TypeFamilyToCanonicalTypeFamily(t.Family()) {
+	case types.BoolFamily:
+		return &minBoolAggAlloc{aggAllocBase: allocBase}
+	case types.BytesFamily:
+		return &minBytesAggAlloc{aggAllocBase: allocBase}
+	case types.DecimalFamily:
+		return &minDecimalAggAlloc{aggAllocBase: allocBase}
+	case types.IntFamily:
+		switch t.Width() {
+		case 16:
+			return &minInt16AggAlloc{aggAllocBase: allocBase}
+		case 32:
+			return &minInt32AggAlloc{aggAllocBase: allocBase}
+		default:
+			return &minInt64AggAlloc{aggAllocBase: allocBase}
+		}
+	case types.FloatFamily:
+		return &minFloat64AggAlloc{aggAllocBase: allocBase}
+	case types.TimestampTZFamily:
+		return &minTimestampAggAlloc{aggAllocBase: allocBase}
+	case types.IntervalFamily:
+		return &minIntervalAggAlloc{aggAllocBase: allocBase}
+	default:
+		return &minDatumAggAlloc{aggAllocBase: allocBase}
+	}
+}
+
+func newMaxAggAlloc(allocator *colmem.Allocator, t *types.T, allocSize int64) aggregateFuncAlloc {
+	allocBase := aggAllocBase{allocator: allocator, allocSize: allocSize}
+	switch typeconv.TypeFamilyToCanonicalTypeFamily(t.Family()) {
+	case types.BoolFamily:
+		return &maxBoolAggAlloc{aggAllocBase: allocBase}
+	case types.BytesFamily:
+		return &maxBytesAggAlloc{aggAllocBase: allocBase}
+	case types.DecimalFamily:
+		return &maxDecimalAggAlloc{aggAllocBase: allocBase}
+	case types.IntFamily:
+		switch t.Width() {
+		case 16:
+			return &maxInt16AggAlloc{aggAllocBase: allocBase}
+		case 32:
+			return &maxInt32AggAlloc{aggAllocBase: allocBase}
+		default:
+			return &maxInt64AggAlloc{aggAllocBase: allocBase}
+		}
+	case types.FloatFamily:
+		return &maxFloat64AggAlloc{aggAllocBase: allocBase}
+	case types.TimestampTZFamily:
+		return &maxTimestampAggAlloc{aggAllocBase: allocBase}
+	case types.IntervalFamily:
+		return &maxIntervalAggAlloc{aggAllocBase: allocBase}
+	default:
+		return &maxDatumAggAlloc{aggAllocBase: allocBase}
 	}
 }
