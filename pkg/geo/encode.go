066ce124ee6ca25aeb06fe28ed2a1b75b0bbaccc
Oliver Tan <otan@cockroachlabs.com>
2020-06-08 13:22:20 -0700

    geo: fix GeoJSON and allow options in ST_AsGeoJSON
    There was a bug in GeoJSON where we used the high level name:"Feature"
    field, as opposed to the inner Geometry. This has been fixed.
    
    We also support more options for ST_AsGeoJSON now that these changes are
    in twpayne/go-geom.
    
    Release note (sql change): Implement ST_AsGeoJSON with options to show
    bbox and CRS information.

--- pkg/geo/encode.go
+++ pkg/geo/encode.go
@@ -17,6 +17,9 @@ import (
 	"strings"
 
 	"github.com/cockroachdb/cockroach/pkg/geo/geopb"
+	"github.com/cockroachdb/cockroach/pkg/geo/geoprojbase"
+	"github.com/cockroachdb/errors"
+	"github.com/twpayne/go-geom"
 	"github.com/twpayne/go-geom/encoding/ewkb"
 	"github.com/twpayne/go-geom/encoding/geojson"
 	"github.com/twpayne/go-geom/encoding/kml"
@@ -71,17 +74,86 @@ func EWKBToWKB(b geopb.EWKB, byteOrder binary.ByteOrder) (geopb.WKB, error) {
 	return geopb.WKB(ret), err
 }
 
+// EWKBToGeoJSONFlag maps to the ST_AsGeoJSON flags for PostGIS.
+type EWKBToGeoJSONFlag int
+
+// These should be kept with ST_AsGeoJSON in PostGIS.
+// 0: means no option
+// 1: GeoJSON BBOX
+// 2: GeoJSON Short CRS (e.g EPSG:4326)
+// 4: GeoJSON Long CRS (e.g urn:ogc:def:crs:EPSG::4326)
+// 8: GeoJSON Short CRS if not EPSG:4326 (default)
+const (
+	EWKBToGeoJSONFlagIncludeBBox EWKBToGeoJSONFlag = 1 << (iota)
+	EWKBToGeoJSONFlagShortCRS
+	EWKBToGeoJSONFlagLongCRS
+	EWKBToGeoJSONFlagShortCRSIfNot4326
+
+	EWKBToGeoJSONFlagZero = 0
+)
+
+// geomToGeoJSONCRS converts a geom to its CRS GeoJSON form.
+func geomToGeoJSONCRS(t geom.T, long bool) (*geojson.CRS, error) {
+	projection, ok := geoprojbase.Projection(geopb.SRID(t.SRID()))
+	if !ok {
+		return nil, errors.Newf("unknown SRID: %d", t.SRID())
+	}
+	var prop string
+	if long {
+		prop = fmt.Sprintf("urn:ogc:def:crs:%s::%d", projection.AuthName, projection.AuthSRID)
+	} else {
+		prop = fmt.Sprintf("%s:%d", projection.AuthName, projection.AuthSRID)
+	}
+	crs := &geojson.CRS{
+		Type: "name",
+		Properties: map[string]interface{}{
+			"name": prop,
+		},
+	}
+	return crs, nil
+}
+
 // EWKBToGeoJSON transforms a given EWKB to GeoJSON.
-func EWKBToGeoJSON(b geopb.EWKB) ([]byte, error) {
+func EWKBToGeoJSON(b geopb.EWKB, maxDecimalDigits int, flag EWKBToGeoJSONFlag) ([]byte, error) {
 	t, err := ewkb.Unmarshal([]byte(b))
 	if err != nil {
 		return nil, err
 	}
-	f := geojson.Feature{
-		// TODO(otan): add features once we have spatial_ref_sys.
-		Geometry: t,
+	options := []geojson.EncodeGeometryOption{
+		geojson.EncodeGeometryWithMaxDecimalDigits(maxDecimalDigits),
 	}
-	return f.MarshalJSON()
+	if flag&EWKBToGeoJSONFlagIncludeBBox != 0 {
+		options = append(
+			options,
+			geojson.EncodeGeometryWithBBox(),
+		)
+	}
+	// Take CRS flag in order of precedence.
+	if t.SRID() != 0 {
+		if flag&EWKBToGeoJSONFlagLongCRS != 0 {
+			crs, err := geomToGeoJSONCRS(t, true /* long */)
+			if err != nil {
+				return nil, err
+			}
+			options = append(options, geojson.EncodeGeometryWithCRS(crs))
+		} else if flag&EWKBToGeoJSONFlagShortCRS != 0 {
+			crs, err := geomToGeoJSONCRS(t, false /* long */)
+			if err != nil {
+				return nil, err
+			}
+			options = append(options, geojson.EncodeGeometryWithCRS(crs))
+		} else if flag&EWKBToGeoJSONFlagShortCRSIfNot4326 != 0 {
+			if t.SRID() != 4326 {
+				crs, err := geomToGeoJSONCRS(t, false /* long */)
+				if err != nil {
+					return nil, err
+				}
+				options = append(options, geojson.EncodeGeometryWithCRS(crs))
+			}
+		}
+	}
+
+	return geojson.Marshal(t, options...)
 }
 
 // EWKBToWKBHex transforms a given EWKB to WKBHex.
