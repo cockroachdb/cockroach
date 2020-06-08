5cbdb0dcc42320f8a1e0b0183a33ceea0b00eb05
Jaewan Park <jaewan.huey.park@gmail.com>
2020-06-09 06:54:19 +0900

    geo/geogfn: implement ST_Project
    Fixes #48402
    
    Release note (sql change): This PR implement adds the ST_Project({geography,float8,float8})

--- pkg/geo/geographiclib/geographiclib_test.go
+++ pkg/geo/geographiclib/geographiclib_test.go
@@ -11,8 +11,10 @@
 package geographiclib
 
 import (
+	"math"
 	"testing"
 
+	"github.com/golang/geo/s1"
 	"github.com/golang/geo/s2"
 	"github.com/stretchr/testify/require"
 )
@@ -98,6 +100,33 @@ func TestAreaAndPerimeter(t *testing.T) {
 			area, perimeter := tc.spheroid.AreaAndPerimeter(tc.points)
 			require.Equal(t, tc.area, area)
 			require.Equal(t, tc.perimeter, perimeter)
+		})
+	}
+}
+
+func TestProject(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		spheroid Spheroid
+		point    s2.LatLng
+		distance float64
+		azimuth  float64
+		project  s2.LatLng
+	}{
+		{
+			desc:     "{0,0} project to 100000, radians(45.0) on WGS84Spheroid",
+			spheroid: WGS84Spheroid,
+			point:    s2.LatLng{Lat: 0, Lng: 0},
+			distance: 100000,
+			azimuth:  45 * math.Pi / 180.0,
+			project:  s2.LatLng{Lat: 0.011160897716439782, Lng: 0.011086872969072624},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			project := tc.spheroid.Project(tc.point, tc.distance, s1.Angle(tc.azimuth))
+			require.Equal(t, tc.project, project)
 		})
 	}
 }
