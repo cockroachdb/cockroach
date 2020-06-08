8b6401cdb3dc8c7925f28f597a3d4dea9759f79a
Andrei Matei <andrei@cockroachlabs.com>
2020-06-08 19:02:20 -0400

    kvclient: improve testing util
    We had a test transport that was returning empty replica descriptors,
    instead of replicas from the descriptor that it was provided. That
    worked only by accident, and will stop working.
    
    Release note: None

--- pkg/kv/kvclient/kvcoord/send_test.go
+++ pkg/kv/kvclient/kvcoord/send_test.go
@@ -110,7 +110,7 @@ func (f *firstNErrorTransport) NextInternalClient(
 }
 
 func (f *firstNErrorTransport) NextReplica() roachpb.ReplicaDescriptor {
-	return roachpb.ReplicaDescriptor{}
+	return f.replicas[f.numSent].ReplicaDescriptor
 }
 
 func (*firstNErrorTransport) MoveToFront(roachpb.ReplicaDescriptor) {
