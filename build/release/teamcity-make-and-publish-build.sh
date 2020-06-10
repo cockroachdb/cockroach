de236cbc0c740c14646e803059b82556497ead28
James H. Linder <jamesl@cockroachlabs.com>
2020-06-10 12:27:46 -0400

    Script for the MakeAndPublishBuild TC build configuration
    Before: the script wasn't implemented.
    
    Now:
    
    Part of the new release process, this script:
    
    - tags the selected SHA to the contents of `.buildinfo/tag`
    - compiles the binaries and archive and uploads them to S3 as the tagged
      name
    - makes and uploads the docker image to our internal GCR repository
    - pushes the git tag to the cockroachlabs/release-staging repo
    - pushes the latest-vX.Y-build docker image tag to GCR
    
    Release note: None

---
+++ build/release/teamcity-make-and-publish-build.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+source "$(dirname "${0}")/teamcity-support.sh"
+
+export BUILDER_HIDE_GOPATH_SRC=1
+
+build/builder.sh make .buildinfo/tag
+build_name="${TAG_NAME-$(cat .buildinfo/tag)}"
+release_branch="$(echo "$build_name" | grep -Eo "^v[0-9]+\.[0-9]+")"
+
+# TODO: What's the bucket name?
+#bucket="${BUCKET-cockroach-prerelease}"
+bucket="${BUCKET-james-cockroach-test}"
+
+
+tc_start_block "Tag the release"
+git tag "$build_name"
+tc_end_block "Tag the release"
+
+
+tc_start_block "Compile publish-provisional-artifacts"
+build/builder.sh go install ./pkg/cmd/publish-provisional-artifacts
+tc_end_block "Compile publish-provisional-artifacts"
+
+
+tc_start_block "Compile and publish S3 artifacts"
+build/builder.sh env \
+  AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+  AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
+  TC_BUILD_BRANCH="$build_name" \
+  publish-provisional-artifacts -provisional -release -bucket "$bucket"
+tc_end_block "Compile and publish S3 artifacts"
+
+
+tc_start_block "Make and push docker image"
+log_into_gcloud
+configure_docker_creds
+
+# TODO: Settle on the desired repo name
+#gcr_repository=gcr.io/cockroach-managed-staging/cockroach
+gcr_repository=gcr.io/jamesl-test/cockroach-pre-release
+
+# TODO: Where are the binaries deposited locally?
+cp cockroach build/deploy/cockroach
+
+docker build --no-cache --tag="${gcr_repository}:${build_name}" build/deploy
+docker push "${gcr_repository}:${build_name}"
+tc_end_block "Make and push docker image"
+
+
+tc_start_block "Push release tag to github.com/cockroachdb/cockroach"
+git remote add release-staging git@github.com:cockroachlabs/release-staging.git
+git push ssh://git@github.com/cockroachlabs/release-staging.git "$build_name"
+tc_end_block "Push release tag to github.com/cockroachdb/cockroach"
+
+
+tc_start_block "Tag docker image as latest-build"
+# Only tag the image as "latest-vX.Y-build" if the tag is on a release branch
+# (or master for the alphas for the next major release).
+if [[ -n "${release_branch}" ]] ; then
+  gcloud container images add-tag "${gcr_repository}:${build_name}" "${gcr_repository}:latest-${release_branch}-build"
+fi
+tc_end_block "Tag docker image as latest-build"
--- build/release/teamcity-support.sh
+++ build/release/teamcity-support.sh
@@ -15,3 +15,33 @@ tc_start_block() {
 tc_end_block() {
   echo "##teamcity[blockClosed name='$1']"
 }
+
+log_into_gcloud() {
+  # Set up Google credentials.
+  if [[ "${GOOGLE_EPHEMERAL_CREDENTIALS}" ]]; then
+    echo "${GOOGLE_EPHEMERAL_CREDENTIALS}" > creds.json
+    gcloud auth activate-service-account --key-file=creds.json
+  else
+    echo 'warning: GOOGLE_EPHEMERAL_CREDENTIALS not set' >&2
+  fi
+}
+
+log_into_dockerhub() {
+  echo "${DOCKER_AUTH}" | docker login --username "${DOCKER_ID}" --password-stdin
+}
+
+configure_docker_creds() {
+  # Configures creds for dockerhub and gcr
+  cat << EOF > ~/.docker/config.json
+{
+  "credsStore" : "",
+  "auths": {
+    "https://index.docker.io/v1/" : {
+    }
+  },
+  "credHelpers": {
+    "gcr.io": "gcloud"
+  }
+}
+EOF
+}
