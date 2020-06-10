863e9700176e5f65d517a0ed9e48c05430c0998a
richardjcai <caioftherichard@gmail.com>
2020-06-10 18:02:29 -0400

    sql: Support ALTER COLUMN TYPE with a user provided expression.
    Fixes #47706
    
    Release note (sql change): Support ALTER COLUMN TYPE ... USING EXPRESSION
    to allow users to optionally provide an expression to convert a column to a new type.

--- pkg/sql/schemaexpr/partial_index.go
+++ pkg/sql/schemaexpr/partial_index.go
@@ -56,19 +56,10 @@ func NewIndexPredicateValidator(
 //     functions.
 //
 func (v *IndexPredicateValidator) Validate(expr tree.Expr) (tree.Expr, error) {
-	// Replace the column variables with dummyColumns so that they can be
-	// type-checked.
-	replacedExpr, _, err := replaceVars(v.desc, expr)
-	if err != nil {
-		return nil, err
-	}
-
-	// Check that the type of the expression is a types.Bool and that there are
-	// no variable expressions (besides dummyColumnItems) and no impure
-	// functions.
-	_, err = sqlbase.SanitizeVarFreeExpr(
+	_, _, err := ReplaceColumnVarsAndSanitizeExpr(
 		v.ctx,
-		replacedExpr,
+		v.desc,
+		expr,
 		types.Bool,
 		"index predicate",
 		v.semaCtx,
