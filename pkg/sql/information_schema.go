ff029f99c37cb8e56d47e65fb9297b98137a34ac
gorjunov <gorjunov.sergei@gmail.com>
2020-06-16 23:27:40 +0300

    sql: populate role valid until fields in pg_catalog tables
    Release note (bug fix): Correctly populate rolvaliduntil value for roles in pg_roles and pg_authid.

--- pkg/sql/information_schema.go
+++ pkg/sql/information_schema.go
@@ -15,6 +15,7 @@ import (
 	"fmt"
 	"sort"
 	"strconv"
+	"time"
 	"unicode/utf8"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
@@ -1817,11 +1818,13 @@ func forEachColumnInIndex(
 }
 
 func forEachRole(
-	ctx context.Context, p *planner, fn func(username string, isRole bool, noLogin bool) error,
+	ctx context.Context,
+	p *planner,
+	fn func(username string, isRole bool, noLogin bool, rolValidUntil *time.Time) error,
 ) error {
 	query := `
 SELECT
-	username,
+	u.username,
 	"isRole",
 	EXISTS(
 		SELECT
@@ -1831,9 +1834,13 @@ SELECT
 		WHERE
 			r.username = u.username AND option = 'NOLOGIN'
 	)
-		AS nologin
+		AS nologin,
+	ro.value::TIMESTAMPTZ AS rolvaliduntil
 FROM
-	system.users AS u;
+	system.users AS u
+	LEFT JOIN system.role_options AS ro ON
+			ro.username = u.username
+			AND option = 'VALID UNTIL';
 `
 	rows, err := p.ExtendedEvalContext().ExecCfg.InternalExecutor.Query(
 		ctx, "read-roles", p.txn, query,
@@ -1851,9 +1858,16 @@ FROM
 		}
 		noLogin, ok := row[2].(*tree.DBool)
 		if !ok {
-			return errors.Errorf("noLogin should be a boolean value, found %s instead", row[1].ResolvedType())
+			return errors.Errorf("noLogin should be a boolean value, found %s instead", row[2].ResolvedType())
 		}
-		if err := fn(string(username), bool(*isRole), bool(*noLogin)); err != nil {
+		var rolValidUntil *time.Time
+		if rolValidUntilDatum, ok := row[3].(*tree.DTimestampTZ); ok {
+			rolValidUntil = &rolValidUntilDatum.Time
+		} else if row[3] != tree.DNull {
+			return errors.Errorf("rolValidUntil should be a timestamp or null value, found %s instead", row[3].ResolvedType())
+		}
+
+		if err := fn(string(username), bool(*isRole), bool(*noLogin), rolValidUntil); err != nil {
 			return err
 		}
 	}
