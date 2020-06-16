f9ca71dfa43b6d54fa19a0148baba79c5038da01
Alfonso Subiotto Marques <alfonso@cockroachlabs.com>
2020-06-16 13:48:00 +0200

    colexec: make ParallelUnorderedSynchronizer implement DrainMeta
    Previously, any components that would call DrainMeta downstream of the
    ParallelUnorderedSynchronizer would call into an upstream goroutine. This
    commit makes it so that any component downstream of the
    ParallelUnorderedSynchronizer that requires metadata from one of the input
    goroutines can now call DrainMeta on the ParallelUnorderedSynchronizer, who
    will take care of returning DrainMeta accumulated from any input goroutines.
    
    This commit also simplifies the other synchronizers so multiple metadata
    sources can be collapsed into one. This is not necessary for DrainMeta and Next
    to be called from a single goroutine but it does simplify the planning model.
    
    Release note: None (no user-visible changes)

--- pkg/sql/colexec/serial_unordered_synchronizer_test.go
+++ pkg/sql/colexec/serial_unordered_synchronizer_test.go
@@ -17,9 +17,11 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/col/coldata"
 	"github.com/cockroachdb/cockroach/pkg/col/coldatatestutils"
 	"github.com/cockroachdb/cockroach/pkg/sql/colexecbase"
+	"github.com/cockroachdb/cockroach/pkg/sql/execinfrapb"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
+	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/require"
 )
 
@@ -32,18 +34,29 @@ func TestSerialUnorderedSynchronizer(t *testing.T) {
 	const numBatches = 4
 
 	typs := []*types.T{types.Int}
-	inputs := make([]colexecbase.Operator, numInputs)
+	inputs := make([]SynchronizerInput, numInputs)
 	for i := range inputs {
 		batch := coldatatestutils.RandomBatch(testAllocator, rng, typs, coldata.BatchSize(), 0 /* length */, rng.Float64())
 		source := colexecbase.NewRepeatableBatchSource(testAllocator, batch, typs)
 		source.ResetBatchesToReturn(numBatches)
-		inputs[i] = source
+		inputIdx := i
+		inputs[i] = SynchronizerInput{
+			Op: source,
+			MetadataSources: []execinfrapb.MetadataSource{
+				execinfrapb.CallbackMetadataSource{
+					DrainMetaCb: func(_ context.Context) []execinfrapb.ProducerMetadata {
+						return []execinfrapb.ProducerMetadata{{Err: errors.Errorf("input %d test-induced metadata", inputIdx)}}
+					},
+				},
+			},
+		}
 	}
-	s := NewSerialUnorderedSynchronizer(inputs, typs)
+	s := NewSerialUnorderedSynchronizer(inputs)
 	resultBatches := 0
 	for {
 		b := s.Next(ctx)
 		if b.Length() == 0 {
+			require.Equal(t, len(inputs), len(s.DrainMeta(ctx)))
 			break
 		}
 		resultBatches++
