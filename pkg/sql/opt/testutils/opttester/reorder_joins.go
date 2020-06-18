59853711027c348137d0baec72559e837a083bc9
Drew Kimball <drewk@cockroachlabs.com>
2020-06-17 21:54:06 -0600

    opt: add rule to efficiently enumerate join orderings
    Previously, the optimizer used associative and commutative rules
    to reorder join trees. This method is limited in the number of
    joins that can be reordered without prohibitively long planning
    time. The long planning time is largely a result of two factors:
    1. Applying these rules to every memo expression produces many
       duplicate plans. While the duplicates aren't actually added to
       the memo, they are still produced often enough to increase
       planning time.
    2. Associative and commutative rules produce join orders in which
       cross joins are created which did not previously exist. These
       plans are rarely the best, since cross join output cardinalities
       are large except when the input cardinalities are very small.
    
    As an example of the second problem, take the following queries:
    ```
    SELECT * FROM xy INNER JOIN ab ON x = a INNER JOIN uv ON a = u;
    SELECT * FROM xy INNER JOIN uv ON True INNER JOIN ab ON x = a AND a = u;
    ```
    These queries are equivalent, but the second one is most likely
    slower than the first.
    
    This patch adds a library that constructs a graph representing the
    join tree to be reordered. The graph is then used to enumerate all
    valid join orderings all at once without duplicates, excluding those
    that involve creating cross joins. Only the first expression of each
    memo group is reordered. This addresses the above two problems,
    which significantly improves the time complexity of join reordering.
    Because of the planning time decrease, the default reorder_joins_limit
    can be increased.
    
    In addition, transitive closure is ensured for the join filters
    during contruction of the join graph. In the example query above, the
    x = a and a = u filters imply an x = u filter. This will become part
    of the join graph and will be used in constructing join trees, so an
    order like the following will now be possible without introducing a
    cross join:
    ```
    SELECT * FROM xy INNER JOIN uv ON x = u INNER JOIN ab ON x = a AND a = u;
    ```
    Finally, the method used to enumerate valid join orderings is extensible
    to non-inner joins. This will be left for a future PR.
    
    Release note: None

---
+++ pkg/sql/opt/testutils/opttester/reorder_joins.go
@@ -0,0 +1,161 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package opttester
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/opt"
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/memo"
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/xform"
+	"github.com/cockroachdb/errors"
+)
+
+// ReorderJoins optimizes the given query and outputs intermediate steps taken
+// during join enumeration. For each call to joinOrderBuilder.Reorder, the
+// output is as follows:
+//   1. The original join tree that is used to form the join graph.
+//   2. The vertexes of the join graph (as well as compact aliases that will be
+//      used to output joins added to the memo).
+//   3. The edges of the join graph.
+//   4. The joins which joinOrderBuilder attempts to add to the memo. An output
+//      like 'AB CD' means a join tree containing relations A and B is being
+//      joined to a join tree containing relations C and D. There is also a
+//      'refs' field containing all relations that are referenced by the join's
+//      ON condition.
+// The final optimized plan is then output.
+func (ot *OptTester) ReorderJoins() (string, error) {
+	ot.builder.Reset()
+	o := ot.makeOptimizer()
+
+	// Map from the first ColumnID of each base relation to its assigned name.
+	// this ensures that aliases are consistent across different calls to Reorder.
+	names := map[opt.ColumnID]string{}
+
+	// joinsConsidered counts the number of joins which joinOrderBuilder attempts
+	// to add to the memo during each call to Reorder.
+	var joinsConsidered int
+	var treeNum = 1
+
+	o.JoinOrderBuilder().NotifyOnReorder(
+		func(join memo.RelExpr, vertexes []memo.RelExpr, edges memo.FiltersExpr) {
+			if treeNum > 1 {
+				// This isn't the first Reorder call. Output the number of joins added to
+				// the memo by the last call to Reorder.
+				ot.builder.WriteString(fmt.Sprintf("\nJoins Considered: %v\n", joinsConsidered))
+				joinsConsidered = 0
+			}
+			ot.builder.WriteString(separator("-"))
+			ot.builder.WriteString(fmt.Sprintf("----Join Tree #%v----\n", treeNum))
+			ot.builder.WriteString(o.FormatExpr(join, memo.ExprFmtHideAll))
+			ot.builder.WriteString(fmt.Sprintf("\n----Vertexes----\n"))
+			ot.builder.WriteString(outputVertexes(vertexes, names, o))
+			ot.builder.WriteString(fmt.Sprintf("----Edges----\n"))
+			for i := range edges {
+				ot.builder.WriteString(o.FormatExpr(&edges[i], memo.ExprFmtHideAll))
+			}
+			ot.builder.WriteString("\n")
+			treeNum++
+		})
+
+	var relsJoinedLast string
+	o.JoinOrderBuilder().NotifyOnAddJoin(func(left, right, all, refs []memo.RelExpr) {
+		relsToJoin := outputRels(all, names)
+		if relsToJoin != relsJoinedLast {
+			ot.builder.WriteString(
+				fmt.Sprintf(
+					"----Joining %s----\n",
+					relsToJoin,
+				),
+			)
+			relsJoinedLast = relsToJoin
+		}
+		ot.builder.WriteString(
+			fmt.Sprintf(
+				"%s %s    refs [%s]\n",
+				outputRels(left, names),
+				outputRels(right, names),
+				outputRels(refs, names),
+			),
+		)
+		joinsConsidered++
+	})
+
+	expr, err := ot.optimizeExpr(o)
+	ot.builder.WriteString(fmt.Sprintf("\nJoins Considered: %v\n", joinsConsidered))
+	ot.builder.WriteString(separator("-"))
+	ot.builder.WriteString("----Final Plan----\n")
+	ot.builder.WriteString(ot.FormatExpr(expr))
+	ot.builder.WriteString(separator("-"))
+	return ot.builder.String(), err
+}
+
+// outputVertexes outputs each base relation in the vertexes slice along with
+// its alias.
+func outputVertexes(
+	vertexes []memo.RelExpr, names map[opt.ColumnID]string, o *xform.Optimizer,
+) string {
+	buf := bytes.Buffer{}
+	for i := range vertexes {
+		firstCol, ok := vertexes[i].Relational().OutputCols.Next(0)
+		if !ok {
+			panic(errors.AssertionFailedf("failed to retrieve column from %v", vertexes[i].Op()))
+		}
+		name, ok := names[firstCol]
+		if !ok {
+			name = getRelationName(len(names))
+			names[firstCol] = name
+		}
+		buf.WriteString(
+			fmt.Sprintf(
+				"%s:\n%s",
+				name,
+				o.FormatExpr(vertexes[i], memo.ExprFmtHideAll),
+			),
+		)
+		buf.WriteString("\n")
+	}
+	return buf.String()
+}
+
+// outputRels returns a string with the aliases of the given base relations
+// concatenated together. Panics if there is no alias for a base relation.
+func outputRels(baseRels []memo.RelExpr, names map[opt.ColumnID]string) string {
+	buf := bytes.Buffer{}
+	for i := range baseRels {
+		firstCol, ok := baseRels[i].Relational().OutputCols.Next(0)
+		if !ok {
+			panic(errors.AssertionFailedf("failed to retrieve column from %v", baseRels[i].Op()))
+		}
+		buf.WriteString(names[firstCol])
+	}
+	return buf.String()
+}
+
+// getRelationName returns a simple alias for a base relation given the number
+// of names generated so far.
+func getRelationName(nameCount int) string {
+	const lenAlphabet = 26
+	name := string(int('A') + (nameCount % lenAlphabet))
+	number := nameCount / lenAlphabet
+	if number > 0 {
+		// Names will follow the pattern: A, B, ..., Z, A1, B1, etc.
+		name += strconv.Itoa(number)
+	}
+	return name
+}
+
+func separator(sep string) string {
+	return fmt.Sprintf("%s\n", strings.Repeat(sep, 80))
+}
