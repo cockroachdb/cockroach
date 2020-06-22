0a12ed7ace66aa7f64f7a5cd119e8c166cedfb02
Raphael 'kena' Poss <knz@thaumogen.net>
2020-06-22 22:37:06 +0200

    importccl: use TestLogScope
    This greatly reduce the length of the build log
    and aims to overall reduce the time spent by TeamCity parsing log
    outputs.
    
    Release note: None

--- pkg/ccl/importccl/client_import_test.go
+++ pkg/ccl/importccl/client_import_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/errors"
 	"github.com/lib/pq"
 	"github.com/stretchr/testify/require"
@@ -31,6 +32,7 @@ import (
 // more details.
 func TestDropDatabaseCascadeDuringImportsFails(t *testing.T) {
 	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
