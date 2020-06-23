6e1af02f44e5284425c429ffd0e574d35bd22d55
arulajmani <arulajmani@gmail.com>
2020-06-23 12:43:40 -0400

    sql: add a new observer statement to show last query statistics
    Adds a new observer statement `SHOW LAST QUERY STATISTICS` which
    returns service, latency, planning, and parsing timigns for the last
    executed statement. By exposing these timings this PR builds a bridge
    towards providing more in-depth metrics to improve timings displayed by
    the CLI.
    
    Note that this new statement should probably not be exposed to users,
    and should only be used for internal use (and therefore I'm leaving
    the release note blank here). This is because the CLI issues some
    "under the hood" queries (eg. retriving the current db so that it can
    be displayed). If a user would use this statement without insight into
    these "under the hood" queries it could prove to be confusing.
    
    Informs #49450
    
    Release note: none

--- pkg/sql/parser/help_test.go
+++ pkg/sql/parser/help_test.go
@@ -320,6 +320,7 @@ func TestContextualHelp(t *testing.T) {
 		{`SHOW SYNTAX ??`, `SHOW SYNTAX`},
 		{`SHOW SYNTAX 'foo' ??`, `SHOW SYNTAX`},
 		{`SHOW SAVEPOINT STATUS ??`, `SHOW SAVEPOINT`},
+		{`SHOW LAST QUERY STATISTICS ??`, `SHOW LAST QUERY STATISTICS`},
 
 		{`SHOW RANGE ??`, `SHOW RANGE`},
 
