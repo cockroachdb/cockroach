b076aaba6bd75f1218255ee994d7b7d952764ac4
Rohan Yadav <rohany@alumni.cmu.edu>
2020-06-24 11:46:47 -0400

    sql: implement the CREATE SCHEMA command
    Built off of #49523.
    
    This PR adds the ability to create schemas. Note that we don't support
    qualification in the `CREATE SCHEMA` statement, and this is in line with
    Postgres.
    
    Release note: None

--- pkg/sql/create_schema.go
+++ pkg/sql/create_schema.go
@@ -13,10 +13,13 @@ package sql
 import (
 	"context"
 
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
+	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catalogkv"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgcode"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
-	"github.com/cockroachdb/cockroach/pkg/util/errorutil/unimplemented"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 )
 
 type createSchemaNode struct {
@@ -24,25 +27,96 @@ type createSchemaNode struct {
 }
 
 func (n *createSchemaNode) startExec(params runParams) error {
-	var schemaExists bool
-	if n.n.Schema == tree.PublicSchema {
-		schemaExists = true
-	} else {
-		for _, virtualSchema := range virtualSchemas {
-			if n.n.Schema == virtualSchema.name {
-				schemaExists = true
-				break
-			}
+	return params.p.createUserDefinedSchema(params, n.n)
+}
+
+func (p *planner) schemaExists(
+	ctx context.Context, parentID sqlbase.ID, schema string,
+) (bool, error) {
+	// Check statically known schemas.
+	if schema == tree.PublicSchema {
+		return true, nil
+	}
+	for _, vs := range virtualSchemas {
+		if schema == vs.name {
+			return true, nil
 		}
 	}
-	if !schemaExists {
-		return unimplemented.NewWithIssuef(26443,
-			"new schemas are unsupported")
+	// Now lookup in the namespace for other schemas.
+	exists, _, err := sqlbase.LookupObjectID(ctx, p.txn, p.ExecCfg().Codec, parentID, keys.RootNamespaceID, schema)
+	if err != nil {
+		return false, err
 	}
-	if n.n.IfNotExists {
-		return nil
+	return exists, nil
+}
+
+func (p *planner) createUserDefinedSchema(params runParams, n *tree.CreateSchema) error {
+	// Users can't create a schema without being connected to a DB.
+	if p.CurrentDatabase() == "" {
+		return pgerror.New(pgcode.UndefinedDatabase,
+			"cannot create schema without being connected to a database")
 	}
-	return pgerror.Newf(pgcode.DuplicateSchema, "schema %q already exists", n.n.Schema)
+
+	db, err := p.ResolveUncachedDatabaseByName(params.ctx, p.CurrentDatabase(), true /* required */)
+	if err != nil {
+		return err
+	}
+
+	// Users cannot create schemas within the system database.
+	if db.ID == keys.SystemDatabaseID {
+		return pgerror.New(pgcode.InvalidObjectDefinition, "cannot create schemas in the system database")
+	}
+
+	// Ensure there aren't any name collisions.
+	exists, err := p.schemaExists(params.ctx, db.ID, n.Schema)
+	if err != nil {
+		return err
+	}
+
+	if exists {
+		if n.IfNotExists {
+			return nil
+		}
+		return pgerror.Newf(pgcode.DuplicateSchema, "schema %q already exists", n.Schema)
+	}
+
+	// Ensure that the cluster version is high enough to create the schema.
+	if !params.p.ExecCfg().Settings.Version.IsActive(params.ctx, clusterversion.VersionUserDefinedSchemas) {
+		return pgerror.Newf(pgcode.ObjectNotInPrerequisiteState,
+			`creating schemas requires all nodes to be upgraded to %s`,
+			clusterversion.VersionByKey(clusterversion.VersionUserDefinedSchemas))
+	}
+
+	// Check that creation of schemas is enabled.
+	if !p.EvalContext().SessionData.UserDefinedSchemasEnabled {
+		return pgerror.Newf(pgcode.FeatureNotSupported,
+			"session variable experimental_enable_user_defined_schemas is set to false, cannot create a schema")
+	}
+
+	// Create the ID.
+	id, err := catalogkv.GenerateUniqueDescID(params.ctx, p.ExecCfg().DB, p.ExecCfg().Codec)
+	if err != nil {
+		return err
+	}
+
+	// Create the SchemaDescriptor.
+	desc := sqlbase.NewMutableCreatedSchemaDescriptor(sqlbase.SchemaDescriptor{
+		ParentID: db.ID,
+		Name:     n.Schema,
+		ID:       id,
+		// Inherit the parent privileges.
+		Privileges: db.GetPrivileges(),
+	})
+
+	// Finally create the schema on disk.
+	return p.createDescriptorWithID(
+		params.ctx,
+		sqlbase.NewSchemaKey(db.ID, n.Schema).Key(p.ExecCfg().Codec),
+		id,
+		desc,
+		params.ExecCfg().Settings,
+		tree.AsStringWithFQNames(n, params.Ann()),
+	)
 }
 
 func (*createSchemaNode) Next(runParams) (bool, error) { return false, nil }
