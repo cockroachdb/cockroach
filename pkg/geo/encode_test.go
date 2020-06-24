6c654a497f55158aaf5fd04e58cfbe4e7145f3dd
Juan Carlos <juanjcsr@gmail.com>
2020-06-23 22:31:56 -0500

    builtins: implement ST_GeoHash for Geometry and Geography types
    This PR adds a function to calculate the geohash representation,
    imitating the PostGIS funcitonality. If no precision is
    specified, the function calculates the geohash according
    to the feature. For points, it uses a default precision
    of 20 characters. For larger features, it estimates a
    precision according to the size of the feature.
    
    Release note (sql change): Implements ST_GeoHash for
    geometry and geography types.

--- pkg/geo/encode_test.go
+++ pkg/geo/encode_test.go
@@ -156,3 +156,166 @@ func TestSpatialObjectToKML(t *testing.T) {
 		})
 	}
 }
+
+func TestSpatialObjectToGeoHash(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		a        string
+		p        int
+		expected string
+	}{
+		{
+			desc:     "POINT EMPTY",
+			a:        "POINT EMPTY",
+			p:        0,
+			expected: "",
+		},
+		{
+			desc:     "POLYGON EMPTY",
+			a:        "POLYGON EMPTY",
+			p:        0,
+			expected: "",
+		},
+		{
+			desc:     "Point at 0,0",
+			a:        "POINT(0.0 0.0)",
+			p:        16,
+			expected: "s000000000000000",
+		},
+		{
+			"Point at 90, 0",
+			"POINT(90.0 0.0)",
+			16,
+			"w000000000000000",
+		},
+		{
+			"Point at a random location",
+			"SRID=4004;POINT(20.012345 -20.012345)",
+			15,
+			"kkqnpkue9ktbpe5",
+		},
+		{
+			"GeoHash from a MultiPolygon",
+			"POLYGON((-71.1776585052917 42.3902909739571,-71.1776820268866 42.3903701743239, -71.1776063012595 42.3903825660754,-71.1775826583081 42.3903033653531,-71.1776585052917 42.3902909739571))",
+			20,
+			"drt3hkfj8gw86nz6tbx7",
+		},
+		{
+			"Point at a random location",
+			"SRID=4004;POINT(20.0123451111111111 -20.012345111111111)",
+			20,
+			"kkqnpkue9kqp6mbe5c6b",
+		},
+		{
+			"Polygon to check automatic precision",
+			"POLYGON((-8.359375000000018 34.36143956369891,-3.4375000000000178 34.36143956369891,-3.4375000000000178 30.8077684261472,-8.359375000000018 30.8077684261472,-8.359375000000018 34.36143956369891))",
+			-1,
+			"e",
+		},
+		{
+			"Polygon to check manual precision",
+			"POLYGON((-8.359375000000018 34.36143956369891,-3.4375000000000178 34.36143956369891,-3.4375000000000178 30.8077684261472,-8.359375000000018 30.8077684261472,-8.359375000000018 34.36143956369891))",
+			5,
+			"evgc3",
+		},
+		{
+			"Polygon to check automatic precision",
+			"POLYGON((-99.18139024416594 19.420811187791617,-99.17177720705656 19.433762205907612,-99.16903062502531 19.424372820694032,-99.17589708010344 19.415306692500074,-99.19134660402922 19.409802010814566,-99.17795701662688 19.40526860361888,-99.21709581057219 19.40624005865604,-99.18139024416594 19.420811187791617))",
+			-1,
+			"9g3q",
+		},
+		{
+			"Polygon to check full precision",
+			"POLYGON((-99.18139024416594 19.420811187791617,-99.17177720705656 19.433762205907612,-99.16903062502531 19.424372820694032,-99.17589708010344 19.415306692500074,-99.19134660402922 19.409802010814566,-99.17795701662688 19.40526860361888,-99.21709581057219 19.40624005865604,-99.18139024416594 19.420811187791617))",
+			20,
+			"9g3qqz1yfh51x7uke7fz",
+		},
+		{
+			"GeoHash from a LineString",
+			"LineString(-99.22962622216731 19.468542204204024,-99.2289395766595 19.46902774319579)",
+			-1,
+			"9g3qvbp",
+		},
+		{
+			"GeoHash from a LineString full precision",
+			"LineString(-99.22962622216731 19.468542204204024,-99.2289395766595 19.46902774319579)",
+			20,
+			"9g3qvbpmyhefh1ecdhpw",
+		},
+		{
+			"GeoHash of LineString crossing DateLine",
+			"LINESTRING(-179 0, 179 0)",
+			GeoHashAutoPrecision,
+			"",
+		},
+		{
+			"GeoHash of LineString crossing DateLine",
+			"LINESTRING(-179 0, 179 0)",
+			5,
+			"s0000",
+		},
+		{
+			"GEOMTRYCOLLECTION EMPTY",
+			"SRID=4326;GEOMETRYCOLLECTION EMPTY",
+			20,
+			"",
+		},
+	}
+
+	t.Run("geometry", func(t *testing.T) {
+		for _, tc := range testCases {
+			t.Run(tc.desc, func(t *testing.T) {
+				a, err := ParseGeometry(tc.a)
+				require.NoError(t, err)
+				geohash, err := SpatialObjectToGeoHash(a.SpatialObject(), tc.p)
+				require.NoError(t, err)
+				require.Equal(t, tc.expected, geohash)
+			})
+		}
+	})
+	t.Run("geography", func(t *testing.T) {
+		for _, tc := range testCases {
+			t.Run(tc.desc, func(t *testing.T) {
+				a, err := ParseGeography(tc.a)
+				require.NoError(t, err)
+				geohash, err := SpatialObjectToGeoHash(a.SpatialObject(), tc.p)
+				require.NoError(t, err)
+				require.Equal(t, tc.expected, geohash)
+			})
+		}
+	})
+
+	t.Run("crossing the date line", func(t *testing.T) {
+		a, err := ParseGeography("LINESTRING(179 0, -179 0)")
+		require.NoError(t, err)
+		geohash, err := SpatialObjectToGeoHash(a.SpatialObject(), GeoHashAutoPrecision)
+		require.NoError(t, err)
+		require.Equal(t, "", geohash)
+	})
+
+	t.Run("geohashes errors with invalid bounds", func(t *testing.T) {
+		for _, tc := range []struct {
+			desc string
+			a    string
+			p    int
+		}{
+			{
+				"Point at 90, 181",
+				"POINT(90.0 181.0)",
+				16,
+			},
+			{
+				"Point at 90, 181",
+				"POINT(-990 181)",
+				16,
+			},
+		} {
+			t.Run(tc.desc, func(t *testing.T) {
+				a, err := ParseGeometry(tc.a)
+				require.NoError(t, err)
+				_, err = SpatialObjectToGeoHash(a.SpatialObject(), tc.p)
+				require.Error(t, err)
+			})
+		}
+	})
+}
