d775ec7673bf5af3635103a699e900e7762c78f4
anzoteh96 <anzot@cockroachlabs.com>
2020-07-01 20:53:41 -0400

    importccl: support `unique_rowid()` as default expression for IMPORT INTO
    The PR #50295 supports non-targeted columns with constant expression.
    This PR is a follow up to that in adding support to `unique_rowid()`.
    This is done by assigning the same value of `rowid` that was generated
    at the `IMPORT stage (row converter) as the default value.
    
    Release note (general change): IMPORT INTO now supports `unique_rowid()`
    as a default expression.

--- pkg/sql/row/row_converter.go
+++ pkg/sql/row/row_converter.go
@@ -22,21 +22,117 @@ import (
 	"github.com/cockroachdb/errors"
 )
 
+// For some functions (specifically the volatile ones), we do
+// not want to use the provided builtin. Instead, we opt for
+// our own function definition, which produces deterministic results.
+func makeBuiltinOverride(
+	builtin *tree.FunctionDefinition, overloads ...tree.Overload,
+) *tree.FunctionDefinition {
+	props := builtin.FunctionProperties
+	return tree.NewFunctionDefinition(
+		"import."+builtin.Name, &props, overloads)
+}
+
+// These constants are addresses used to store relevant information
+// in the Annotation field of evalCtx when evaluating expressions.
+const (
+	cellInfoAddr tree.AnnotationIdx = iota + 1
+)
+
+type cellInfoAnnot struct {
+	sourceID         int32
+	rowID            int64
+	uniqueRowIDInst  int
+	uniqueRowIDTotal int
+}
+
+// We don't want to call unique_rowid() for columns with such default expressions
+// because it is not idempotent and has unfortunate overlapping of output
+// spans since it puts the uniqueness-ensuring per-generator part (nodeID)
+// in the low-bits. Instead, make our own IDs that attempt to keep each
+// generator (sourceID) writing to its own key-space with sequential
+// rowIndexes mapping to sequential unique IDs. This is done by putting the
+// following as the lower bits, in order to handle the case where there are
+// multiple columns with default as `unique_rowid`:
+//
+// #default_rowid_cols * rowIndex + colPosition (among those with default unique_rowid)
+//
+// To avoid collisions with the SQL-genenerated IDs (at least for a
+// very long time) we also flip the top bit to 1.
+//
+// Producing sequential keys in non-overlapping spans for each source yields
+// observed improvements in ingestion performance of ~2-3x and even more
+// significant reductions in required compactions during IMPORT.
+//
+// TODO(dt): Note that currently some callers (e.g. CSV IMPORT, which can be
+// used on a table more than once) offset their rowIndex by a wall-time at
+// which their overall job is run, so that subsequent ingestion jobs pick
+// different row IDs for the i'th row and don't collide. However such
+// time-offset rowIDs mean each row imported consumes some unit of time that
+// must then elapse before the next IMPORT could run without colliding e.g.
+// a 100m row file would use 10µs/row or ~17min worth of IDs. For now it is
+// likely that IMPORT's write-rate is still the limiting factor, but this
+// scheme means rowIndexes are very large (1 yr in 10s of µs is about 2^42).
+// Finding an alternative scheme for avoiding collisions (like sourceID *
+// fileIndex*desc.Version) could improve on this. For now, if this
+// best-effort collision avoidance scheme doesn't work in some cases we can
+// just recommend an explicit PK as a workaround.
+//
+// TODO(anzoteh96): As per the issue in #51004, having too many columns with
+// default expression unique_rowid() could cause collisions when IMPORTs are run
+// too close to each other. It will therefore be nice to fix this problem.
+func importUniqueRowID(evalCtx *tree.EvalContext, args tree.Datums) (tree.Datum, error) {
+	c := (*evalCtx.Annotations).Get(cellInfoAddr).(cellInfoAnnot)
+	avoidCollisionsWithSQLsIDs := uint64(1 << 63)
+	shiftedIndex := int64(c.uniqueRowIDTotal)*c.rowID + int64(c.uniqueRowIDInst)
+	returnIndex := (uint64(c.sourceID) << rowIDBits) ^ uint64(shiftedIndex)
+	c.uniqueRowIDInst++
+	(*evalCtx.Annotations).Set(cellInfoAddr, c)
+	return tree.NewDInt(tree.DInt(avoidCollisionsWithSQLsIDs | returnIndex)), nil
+}
+
+// Besides overriding, there are also counters that we want to keep track
+// of as we walk through the expressions in a row (at datumRowConverter creation
+// time). This will be handled by the sideEffect field: it will be called with an
+// annotation passed in that changes the counter. In the case of unique_rowid, for
+// example, we want to keep track of the total number of unique_rowid occurences
+// in a row.
+type customFunc struct {
+	sideEffect func(annotations *tree.Annotations)
+	override   *tree.FunctionDefinition
+}
+
 // Given that imports can be retried and resumed, we want to
 // ensure that the default functions return the same value given
 // the same arguments, even on retries. Therfore we decide to support
 // only a limited subset of non-immutable functions, which are
 // all listed here.
-var supportedImportFunctions = map[string]struct{}{
+var supportedImportFuncOverrides = map[string]*customFunc{
 	// These methods can be supported given that we set the statement
 	// and transaction timestamp to be equal, i.e. the write timestamp.
-	"current_date":          {},
-	"current_timestamp":     {},
-	"localtimestamp":        {},
-	"now":                   {},
-	"statement_timestamp":   {},
-	"timeofday":             {},
-	"transaction_timestamp": {},
+	"current_date":          nil,
+	"current_timestamp":     nil,
+	"localtimestamp":        nil,
+	"now":                   nil,
+	"statement_timestamp":   nil,
+	"timeofday":             nil,
+	"transaction_timestamp": nil,
+	"unique_rowid": {
+		sideEffect: func(annot *tree.Annotations) {
+			c := annot.Get(cellInfoAddr).(cellInfoAnnot)
+			c.uniqueRowIDTotal++
+			annot.Set(cellInfoAddr, c)
+		},
+		override: makeBuiltinOverride(
+			tree.FunDefs["unique_rowid"],
+			tree.Overload{
+				Types:      tree.ArgTypes{},
+				ReturnType: tree.FixedReturnType(types.Int),
+				Fn:         importUniqueRowID,
+				Info:       "Returns a unique rowid based on row position and time",
+			},
+		),
+	},
 }
 
 func unsafeExpressionError(err error, msg string, expr string) error {
@@ -62,7 +158,11 @@ func (e *unsafeErrExpr) Eval(_ *tree.EvalContext) (tree.Datum, error) {
 // visitor walks the tree and ensures that any expression in the tree
 // that's not immutable is what we explicitly support.
 type importDefaultExprVisitor struct {
-	err error
+	err      error
+	ctx      context.Context
+	annot    *tree.Annotations
+	semaCtx  *tree.SemaContext
+	volatile bool
 }
 
 // VisitPre implements tree.Visitor interface.
@@ -79,8 +179,30 @@ func (v *importDefaultExprVisitor) VisitPost(expr tree.Expr) (newExpr tree.Expr)
 	case *tree.FuncExpr:
 		if fn.ResolvedOverload().Volatility > tree.VolatilityImmutable {
 			resolvedFnName := fn.Func.FunctionReference.(*tree.FunctionDefinition).Name
-			if _, ok := supportedImportFunctions[resolvedFnName]; !ok {
+			if custom, isSafe := supportedImportFuncOverrides[resolvedFnName]; !isSafe {
 				v.err = errors.Newf(`function %s unsupported by IMPORT INTO`, resolvedFnName)
+			} else {
+				// No override exists, means it's okay to use the definitions given in builtin.go.
+				if custom == nil {
+					return expr
+				}
+				// Override exists, so we turn the volatile flag of the visitor to true.
+				// In addition, the sideEffect function needs to be called to update any
+				// relevant counter (e.g. the total number of occurrences of the unique_rowid
+				// function in an expression).
+				v.volatile = true
+				custom.sideEffect(v.annot)
+				funcExpr := &tree.FuncExpr{
+					Func:  tree.ResolvableFunctionReference{FunctionReference: custom.override},
+					Type:  fn.Type,
+					Exprs: fn.Exprs,
+				}
+				// The override must have appropriate overload defined.
+				overrideExpr, err := funcExpr.TypeCheck(v.ctx, v.semaCtx, fn.ResolvedType())
+				if err != nil {
+					v.err = errors.Wrapf(err, "error overloading function")
+				}
+				return overrideExpr
 			}
 		}
 	}
@@ -91,28 +213,28 @@ func (v *importDefaultExprVisitor) VisitPost(expr tree.Expr) (newExpr tree.Expr)
 // for import.
 func SanitizeExprsForImport(
 	ctx context.Context, evalCtx *tree.EvalContext, expr tree.Expr, targetType *types.T,
-) (tree.TypedExpr, error) {
+) (tree.TypedExpr, bool, error) {
 	semaCtx := tree.MakeSemaContext()
 
 	// If we have immutable expressions, then we can just return it right away.
 	typedExpr, err := sqlbase.SanitizeVarFreeExpr(
 		ctx, expr, targetType, "import_default", &semaCtx, tree.VolatilityImmutable)
 	if err == nil {
-		return typedExpr, nil
+		return typedExpr, false, nil
 	}
 	// Now that the expressions are not immutable, we first check that they
 	// are of the correct type before checking for any unsupported functions
 	// for import.
 	typedExpr, err = tree.TypeCheck(ctx, expr, &semaCtx, targetType)
 	if err != nil {
-		return nil, unsafeExpressionError(err, "type checking error", expr.String())
+		return nil, false, unsafeExpressionError(err, "type checking error", expr.String())
 	}
-	v := &importDefaultExprVisitor{}
+	v := &importDefaultExprVisitor{annot: evalCtx.Annotations}
 	newExpr, _ := tree.WalkExpr(v, typedExpr)
 	if v.err != nil {
-		return nil, unsafeExpressionError(v.err, "expr walking error", expr.String())
+		return nil, false, unsafeExpressionError(v.err, "expr walking error", expr.String())
 	}
-	return newExpr.(tree.TypedExpr), nil
+	return newExpr.(tree.TypedExpr), v.volatile, nil
 }
 
 // KVInserter implements the putter interface.
@@ -290,7 +412,6 @@ type DatumRowConverter struct {
 	IsTargetCol map[int]struct{}
 
 	// The rest of these are derived from tableDesc, just cached here.
-	hidden                int
 	ri                    Inserter
 	EvalCtx               *tree.EvalContext
 	cols                  []sqlbase.ColumnDescriptor
@@ -396,43 +517,42 @@ func NewDatumRowConverter(
 		_, ok := isTargetColID[col.ID]
 		return ok
 	}
-	c.hidden = -1
+	hidden := -1
+	annot := make(tree.Annotations, 1)
+	annot.Set(cellInfoAddr, cellInfoAnnot{uniqueRowIDInst: 0})
+	c.EvalCtx.Annotations = &annot
 	for i := range cols {
 		col := &cols[i]
 		if col.Hidden {
-			if col.DefaultExpr == nil || *col.DefaultExpr != "unique_rowid()" || c.hidden != -1 {
+			if col.DefaultExpr == nil || *col.DefaultExpr != "unique_rowid()" || hidden != -1 {
 				return nil, errors.New("unexpected hidden column")
 			}
-			c.hidden = i
-			c.Datums = append(c.Datums, nil)
-		} else {
-			if col.DefaultExpr != nil {
-				// Placeholder for columns with default values that will be evaluated when
-				// each import row is being created.
-				c.defaultCache[i], err = SanitizeExprsForImport(ctx, evalCtx, defaultExprs[i], col.Type)
-				if err != nil {
-					// This expression may not be safe for import but we don't want to
-					// call the user out at this stage: targeted columns may not have
-					// been identified now (e.g. "IMPORT PGDUMP...") and we want to
-					// throw an error only at the "Row" stage when the targeted columns
-					// have been identified.
-					c.defaultCache[i] = &unsafeErrExpr{
-						err: errors.Wrapf(err, "default expression %s unsafe for import", defaultExprs[i].String()),
-					}
-				} else {
-					// TODO (anzoteh96): currently, all the functions we support are immutable
-					// or stable, so it's okay for defaultCache to be evaluated here. However,
-					// when we add support for unique_rowid, it changes for every row and
-					// therefore can no longer be evaluated right away. We therefore
-					// need a flag to determine when we can evaluate here.
+			hidden = i
+		}
+		if col.DefaultExpr != nil {
+			// Placeholder for columns with default values that will be evaluated when
+			// each import row is being created.
+			typedExpr, volatile, err := SanitizeExprsForImport(ctx, evalCtx, defaultExprs[i], col.Type)
+			if err != nil {
+				// This expression may not be safe for import but we don't want to
+				// call the user out at this stage: targeted columns may not have
+				// been identified now (e.g. "IMPORT PGDUMP...") and we want to
+				// throw an error only at the "Row" stage when the targeted columns
+				// have been identified.
+				c.defaultCache[i] = &unsafeErrExpr{
+					err: errors.Wrapf(err, "default expression %s unsafe for import", defaultExprs[i].String()),
+				}
+			} else {
+				c.defaultCache[i] = typedExpr
+				if !volatile {
 					c.defaultCache[i], err = c.defaultCache[i].Eval(evalCtx)
 					if err != nil {
 						return nil, errors.Wrapf(err, "error evaluating default expression")
 					}
 				}
-				if !isTargetCol(col) {
-					c.Datums = append(c.Datums, nil)
-				}
+			}
+			if !isTargetCol(col) {
+				c.Datums = append(c.Datums, nil)
 			}
 		}
 	}
@@ -456,41 +576,18 @@ const rowIDBits = 64 - builtins.NodeIDBits
 // Row inserts kv operations into the current kv batch, and triggers a SendBatch
 // if necessary.
 func (c *DatumRowConverter) Row(ctx context.Context, sourceID int32, rowIndex int64) error {
-	if c.hidden >= 0 {
-		// We don't want to call unique_rowid() for the hidden PK column because it
-		// is not idempotent and has unfortunate overlapping of output spans since
-		// it puts the uniqueness-ensuring per-generator part (nodeID) in the
-		// low-bits. Instead, make our own IDs that attempt to keep each generator
-		// (sourceID) writing to its own key-space with sequential rowIndexes
-		// mapping to sequential unique IDs, by putting the rowID in the lower
-		// bits. To avoid collisions with the SQL-genenerated IDs (at least for a
-		// very long time) we also flip the top bit to 1.
-		//
-		// Producing sequential keys in non-overlapping spans for each source yields
-		// observed improvements in ingestion performance of ~2-3x and even more
-		// significant reductions in required compactions during IMPORT.
-		//
-		// TODO(dt): Note that currently some callers (e.g. CSV IMPORT, which can be
-		// used on a table more than once) offset their rowIndex by a wall-time at
-		// which their overall job is run, so that subsequent ingestion jobs pick
-		// different row IDs for the i'th row and don't collide. However such
-		// time-offset rowIDs mean each row imported consumes some unit of time that
-		// must then elapse before the next IMPORT could run without colliding e.g.
-		// a 100m row file would use 10µs/row or ~17min worth of IDs. For now it is
-		// likely that IMPORT's write-rate is still the limiting factor, but this
-		// scheme means rowIndexes are very large (1 yr in 10s of µs is about 2^42).
-		// Finding an alternative scheme for avoiding collisions (like sourceID *
-		// fileIndex*desc.Version) could improve on this. For now, if this
-		// best-effort collision avoidance scheme doesn't work in some cases we can
-		// just recommend an explicit PK as a workaround.
-		avoidCollisionsWithSQLsIDs := uint64(1 << 63)
-		rowID := (uint64(sourceID) << rowIDBits) ^ uint64(rowIndex)
-		c.Datums[c.hidden] = tree.NewDInt(tree.DInt(avoidCollisionsWithSQLsIDs | rowID))
+	isTargetCol := func(i int) bool {
+		_, ok := c.IsTargetCol[i]
+		return ok
 	}
-
+	annot := c.EvalCtx.Annotations.Get(cellInfoAddr).(cellInfoAnnot)
+	annot.uniqueRowIDInst = 0
+	annot.sourceID = sourceID
+	annot.rowID = rowIndex
+	c.EvalCtx.Annotations.Set(cellInfoAddr, annot)
 	for i := range c.cols {
 		col := &c.cols[i]
-		if _, ok := c.IsTargetCol[i]; !ok && !col.Hidden && col.DefaultExpr != nil {
+		if !isTargetCol(i) && col.DefaultExpr != nil {
 			datum, err := c.defaultCache[i].Eval(c.EvalCtx)
 			if err != nil {
 				return errors.Wrapf(
