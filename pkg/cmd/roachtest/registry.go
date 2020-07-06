68264941a2984f09f0be97c89fad829a637ec62c
irfan sharif <irfanmahmoudsharif@gmail.com>
2020-07-05 23:58:16 -0400

    roachtest: add `decommission/mixed-versions`
    Add a roachtest stressing randomized `cockroach node
    {decommission,recommission}` usage in multi-version clusters.
    
    Release note: None

--- pkg/cmd/roachtest/registry.go
+++ pkg/cmd/roachtest/registry.go
@@ -27,6 +27,7 @@ func registerTests(r *testRegistry) {
 	registerClockMonotonicTests(r)
 	registerCopy(r)
 	registerDecommission(r)
+	registerDecommissionMixedVersion(r)
 	registerDiskFull(r)
 	registerDiskStalledDetection(r)
 	registerDjango(r)
