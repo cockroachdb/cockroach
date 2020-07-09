fd29de43934629f638b97d54b5ffba550854274e
arulajmani <arulajmani@gmail.com>
2020-07-09 18:32:05 -0400

    sql: allow drops on tables/sequences that have invalid ownership states
    Previously, when dropping a sequence or a table that had an ownership
    relationship, we would lookup corresponding table descriptors to unlink
    the relationship. In the case of tables, the owned sequence needed to
    be dropped as well, so we would lookup the sequence descriptor. If the
    corresponding descriptor was not found/had already been dropped, it
    would result in an error -- thereby making it impossible to drop the
    object.
    
    This wasn't an issue, because you don't expect descriptors to be
    dropped/not found if an ownership relationship still exists. However,
    this integrity constraint was violated by a couple of sequence
    ownership bugs. See #51170 for more details.
    
    It should be possible to drop tables/sequences that have descriptors
    in such invalid state. This PR adds support for this by swallowing
    specific errors that users may find themselves in due to invalid
    descriptors. It also adds tests to simulate these invalid states users
    may find themselves in.
    
    closes #51170
    
    Release note (bug fix): Previously users who found themselves with
    descriptors in an invalid state due to the ownership issues linked in
    that contained them. This is now fixed.

--- pkg/sql/sequence_test.go
+++ pkg/sql/sequence_test.go
@@ -12,6 +12,9 @@ package sql
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
+	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
@@ -19,6 +22,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/kv"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 )
 
@@ -187,5 +191,275 @@ func assertColumnOwnsSequences(
 				tableDesc.GetID(), ownerTableID, col.ID, ownerColID,
 			)
 		}
+	}
+}
+
+// Tests for allowing drops on sequence descriptors in a bad state due to
+// ownership bugs. It should be possible to drop tables/sequences that have
+// descriptors in an invalid state. See tracking issue #51770 for more details.
+// Relevant sub-issues are referenced in test names/inline comments.
+func TestInvalidOwnedDescriptorsAreDroppable(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	ctx := context.Background()
+	params := base.TestServerArgs{}
+	s, sqlConn, kvDB := serverutils.StartServer(t, params)
+	sqlDB := sqlutils.MakeSQLRunner(sqlConn)
+	defer s.Stopper().Stop(ctx)
+
+	// setup function (used by most sub-tests). Creates sequence/table objects
+	// that can be invalidated differently by the sub-tests to ensure things can
+	// be dropped even in the invalid state. Caller must cleanup using the
+	// returned function after.
+	setup :=  func() (cleanup func()) {
+		 _, err := sqlConn.Exec(`
+CREATE DATABASE t;
+CREATE TABLE t.test(a INT PRIMARY KEY, b INT);
+CREATE SEQUENCE t.seq OWNED BY t.test.a;
+CREATE SEQUENCE t.useq OWNED BY t.test.a;
+CREATE SEQUENCE t.valid_seq OWNED BY t.test.a`)
+		require.NoError(t, err)
+
+		return func() {
+			sqlConn.Exec("DROP DATABASE IF EXISTS t CASCADE")
+		}
+	}
+
+	// Tests simulating #50711 by breaking the invariant that sequences are owned
+	// by at most one column at a time.
+
+	// Dropping the table should work when the table descriptor is in an invalid
+	// state. The owned sequence should also be dropped.
+	t.Run("#50711 drop table", func(t *testing.T) {
+		cleanup := setup()
+		defer cleanup()
+		addOwnedSequence(t, kvDB, "t", "test", 0, "seq")
+		addOwnedSequence(t, kvDB, "t", "test", 1, "seq")
+
+		_, err := sqlConn.Exec("DROP TABLE t.test")
+		require.NoError(t, err)
+
+		// The sequence should have been dropped as well.
+		sqlDB.ExpectErr(t, `pq: relation "t.seq" does not exist`, "SELECT * FROM t.seq")
+		// The valid sequence should have also been dropped.
+		sqlDB.ExpectErr(t, `pq: relation "t.valid_seq" does not exist`, "SELECT * FROM t.valid_seq")
+	})
+
+	// Same setup as above, except now we drop the sequence first and then
+	// the table.
+	t.Run("#50711 drop sequence followed by drop table", func(t *testing.T) {
+		cleanup := setup()
+		defer cleanup()
+		addOwnedSequence(t, kvDB, "t", "test", 0, "seq")
+		addOwnedSequence(t, kvDB, "t", "test", 1, "seq")
+
+		 _, err := sqlConn.Exec("DROP SEQUENCE t.seq")
+		 require.NoError(t, err)
+
+		_, err = sqlConn.Exec("SELECT * FROM t.valid_seq")
+			require.NoError(t, err)
+
+		 _, err = sqlConn.Exec("DROP TABLE t.test")
+		 require.NoError(t, err)
+
+		// The valid sequence should have also been dropped.
+		sqlDB.ExpectErr(t, `pq: relation "t.valid_seq" does not exist`, "SELECT * FROM t.valid_seq")
+	})
+
+	// This test invalidates both seq and useq as DROP DATABASE CASCADE operates
+	// on objects lexicographically -- owned sequences can be dropped both as a
+	// regular sequence drop and as a side effect of the owner table being dropped.
+	t.Run("#50711 drop database cascade", func(t *testing.T) {
+		cleanup := setup()
+		defer cleanup()
+
+		addOwnedSequence(t, kvDB, "t", "test", 0, "seq")
+		addOwnedSequence(t, kvDB, "t", "test", 1, "seq")
+
+		addOwnedSequence(t, kvDB, "t", "test", 0, "useq")
+		addOwnedSequence(t, kvDB, "t", "test", 1, "useq")
+
+		_, err := sqlConn.Exec("DROP DATABASE t CASCADE")
+		require.NoError(t, err)
+	})
+
+	// Tests simulating #50781 by modifying the sequence's owner to a table that
+	// doesn't exist and column's `ownsSequenceIDs` to sequences that don't exist.
+
+	t.Run("#50781 drop table followed by drop sequence", func(t *testing.T) {
+		cleanup := setup()
+		defer cleanup()
+
+		breakOwnershipMapping(t, sqlConn, kvDB, "t", "test", "seq")
+
+		_, err := sqlConn.Exec("DROP TABLE t.test")
+		require.NoError(t, err)
+
+		// The valid sequence should have also been dropped.
+		sqlDB.ExpectErr(t, `pq: relation "t.valid_seq" does not exist`, "SELECT * FROM t.valid_seq")
+
+		_, err = sqlConn.Exec("DROP SEQUENCE t.seq")
+		require.NoError(t, err)
+	})
+
+	t.Run("#50781 drop sequence followed by drop table", func(t *testing.T) {
+		cleanup := setup()
+		defer cleanup()
+
+		breakOwnershipMapping(t, sqlConn, kvDB, "t", "test", "seq")
+
+		 _, err := sqlConn.Exec("DROP SEQUENCE t.seq")
+			require.NoError(t, err)
+
+		 _, err = sqlConn.Exec("DROP TABLE t.test")
+		 require.NoError(t, err)
+
+		// The valid sequence should have also been dropped.
+		sqlDB.ExpectErr(t, `pq: relation "t.valid_seq" does not exist`, "SELECT * FROM t.valid_seq")
+	})
+
+	// This test invalidates both seq and useq as DROP DATABASE CASCADE operates
+	// on objects lexicographically -- owned sequences can be dropped both as a
+	// regular sequence drop and as a side effect of the owner table being dropped.
+	t.Run("#50781 drop database cascade", func(t *testing.T) {
+		cleanup := setup()
+		defer cleanup()
+
+		breakOwnershipMapping(t, sqlConn, kvDB, "t", "test", "seq")
+		breakOwnershipMapping(t, sqlConn, kvDB, "t", "test", "useq")
+
+		_, err := sqlConn.Exec("DROP DATABASE t CASCADE")
+		require.NoError(t, err)
+	})
+
+	// Combine both #50711 and #50781.
+	t.Run("combined #50711 #50781 drop table followed by sequence", func(t *testing.T) {
+		cleanup := setup()
+		defer cleanup()
+
+		addOwnedSequence(t, kvDB, "t", "test", 0, "seq")
+		addOwnedSequence(t, kvDB, "t", "test", 1, "seq")
+		breakOwnershipMapping(t, sqlConn, kvDB, "t", "test", "seq")
+
+		_, err := sqlConn.Exec("DROP TABLE t.test")
+		require.NoError(t, err)
+
+		// The valid sequence should have also been dropped.
+		sqlDB.ExpectErr(t, `pq: relation "t.valid_seq" does not exist`, "SELECT * FROM t.valid_seq")
+
+		_, err = sqlConn.Exec("DROP SEQUENCE t.seq")
+		require.NoError(t, err)
+	})
+
+	t.Run("combined #50711 #50781 drop sequence followed by table", func(t *testing.T) {
+		cleanup := setup()
+		defer cleanup()
+
+		addOwnedSequence(t, kvDB, "t", "test", 0, "seq")
+		addOwnedSequence(t, kvDB, "t", "test", 1, "seq")
+		breakOwnershipMapping(t, sqlConn, kvDB, "t", "test", "seq")
+
+		_, err := sqlConn.Exec("DROP SEQUENCE t.seq")
+		require.NoError(t, err)
+
+		 _, err = sqlConn.Exec("DROP TABLE t.test")
+		 require.NoError(t, err)
+
+		// The valid sequence should have also been dropped.
+		sqlDB.ExpectErr(t, `pq: relation "t.valid_seq" does not exist`, "SELECT * FROM t.valid_seq")
+	})
+
+	// This test invalidates both seq and useq as DROP DATABASE CASCADE operates
+	// on objects lexicographically -- owned sequences can be dropped both as a
+	// regular sequence drop and as a side effect of the owner table being dropped.
+	t.Run("combined #50711 #50781 drop database cascade", func(t *testing.T) {
+		cleanup := setup()
+		defer cleanup()
+
+		addOwnedSequence(t, kvDB, "t", "test", 0, "seq")
+		addOwnedSequence(t, kvDB, "t", "test", 1, "seq")
+		breakOwnershipMapping(t, sqlConn, kvDB, "t", "test", "seq")
+
+		addOwnedSequence(t, kvDB, "t", "test", 0, "useq")
+		addOwnedSequence(t, kvDB, "t", "test", 1, "useq")
+		breakOwnershipMapping(t, sqlConn, kvDB, "t", "test", "useq")
+
+		_, err := sqlConn.Exec("DROP DATABASE t CASCADE")
+		require.NoError(t, err)
+	})
+}
+
+// addOwnedSequence adds the sequence referenced by seqName to the
+// ownsSequenceIDs of the column referenced by (dbName, tableName, colIdx).
+func addOwnedSequence(
+	t *testing.T, kvDB *kv.DB, dbName string, tableName string , colIdx int, seqName string,
+) {
+	seqDesc := sqlbase.TestingGetTableDescriptor(kvDB, keys.SystemSQLCodec, dbName, seqName)
+	tableDesc := sqlbase.TestingGetMutableExistingTableDescriptor(
+		kvDB, keys.SystemSQLCodec, dbName, tableName)
+
+	tableDesc.GetColumns()[colIdx].OwnsSequenceIds = append(
+		tableDesc.GetColumns()[colIdx].OwnsSequenceIds, seqDesc.ID)
+
+	err := kvDB.Put(
+		context.Background(),
+		sqlbase.MakeDescMetadataKey(keys.SystemSQLCodec, tableDesc.GetID()),
+		tableDesc.DescriptorProto(),
+	)
+	require.NoError(t, err)
+}
+
+// breakOwnershipMapping simulates #50781 by setting the sequence's owner table
+// to a non-existent tableID and setting the column's `ownsSequenceID` to a
+// non-existent sequenceID.
+func breakOwnershipMapping(
+	t *testing.T, conn *sql.DB, kvDB *kv.DB, dbName string, tableName string, seqName string,
+) {
+	seqDesc := sqlbase.TestingGetTableDescriptor(kvDB, keys.SystemSQLCodec, dbName, seqName)
+	tableDesc := sqlbase.TestingGetMutableExistingTableDescriptor(
+		kvDB, keys.SystemSQLCodec, dbName, tableName)
+
+	// Go through all the columns of the table and set the sequenceID owned by
+	// the column to seqID + 10 if the seqID refers to the given seqNam if the
+	// seqID refers to the given seqName.
+	for colIdx := range tableDesc.GetColumns() {
+		for i := range tableDesc.GetColumns()[colIdx].OwnsSequenceIds {
+			if tableDesc.GetColumns()[colIdx].OwnsSequenceIds[i] == seqDesc.ID {
+				tableDesc.GetColumns()[colIdx].OwnsSequenceIds[i] = getNextUnusedID(t, conn, seqDesc.ID)
+			}
+		}
+	}
+	// Set the sequence's owner table ID to tableID + 20.
+	seqDesc.SequenceOpts.SequenceOwner.OwnerTableID = getNextUnusedID(
+		t, conn, seqDesc.SequenceOpts.SequenceOwner.OwnerTableID)
+
+	err := kvDB.Put(
+		context.Background(),
+		sqlbase.MakeDescMetadataKey(keys.SystemSQLCodec, tableDesc.GetID()),
+		tableDesc.DescriptorProto(),
+	)
+	require.NoError(t, err)
+
+	err = kvDB.Put(
+		context.Background(),
+		sqlbase.MakeDescMetadataKey(keys.SystemSQLCodec, seqDesc.GetID()),
+		seqDesc.DescriptorProto(),
+	)
+	require.NoError(t, err)
+}
+
+// getNextUnusedID returns the next ID that is not used by any descriptor by
+// checking against the namespace table.
+func getNextUnusedID(t *testing.T, conn *sql.DB, id sqlbase.ID) sqlbase.ID {
+	unusedID := id + 1
+	for {
+		rows, err := conn.Query(fmt.Sprintf(
+			"SELECT * FROM system.namespace WHERE id=%d", unusedID),
+		)
+		require.NoError(t, err)
+		if !rows.Next() {
+			return unusedID
+		}
+		unusedID += 1
 	}
 }
