88129f6ec94d3403553beadacb33df33fe3b9369
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-07-10 23:24:11 -0700

    colexec: refactor hash aggregator
    This commit refactors the hash aggregator to use the vectorized hash
    table instead of Go's map and tightly couples the hash table with the
    actual aggregation. The hash table is used in a somewhat similar mode to
    how unordered distinct uses it with some crucial differences. Now the
    algorithm for online aggregation is as follows:
    1. read batch from the input
    2. group all tuples from the batch into "equality chains" (this is done
    by "probing" the batch against itself - similar to unordered distinct
    - but instead of "head" tuples into the hash table, we populate special
    "equality" selection vectors as well as a separate selection vector that
    contains heads of the equality chains)
    3. probe the heads of the equality chains against already existing
    buckets in the hash table
    4. if there are any matches, it means that all tuples in the
    corresponding equality chains belong to existing groups and are
    aggregated.
    5. all unmatched equality chains form new groups, so we create
    a separate bucket for each and aggregate the tuples into it.
    
    The crucial observation here is that we're maintaining a 1-to-1 mapping
    between the "heads" of the aggregation groups that are stored in the
    hash table and the "bucket" of aggregate function in `buckets` slice.
    
    This fundamental change to the hash aggregator's algorithm shows 4-5x
    speedups when the group sizes are small and has tolerable 30-40% hit
    when the group sizes are big. Such tradeoff is acceptable since the
    absolute speed in the latter case is still very high.
    
    Release note: None

--- pkg/sql/colexec/execgen/cmd/execgen/hash_aggregator_gen.go
+++ pkg/sql/colexec/execgen/cmd/execgen/hash_aggregator_gen.go
@@ -11,41 +11,15 @@
 package main
 
 import (
+	"fmt"
 	"io"
-	"strings"
-	"text/template"
-
-	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 )
 
 const hashAggTmpl = "pkg/sql/colexec/hash_aggregator_tmpl.go"
 
 func genHashAggregator(inputFileContents string, wr io.Writer) error {
-	r := strings.NewReplacer(
-		"_CANONICAL_TYPE_FAMILY", "{{.CanonicalTypeFamilyStr}}",
-		"_TYPE_WIDTH", typeWidthReplacement,
-		"TemplateType", "{{.VecMethod}}",
-	)
-	s := r.Replace(inputFileContents)
-
-	s = replaceManipulationFuncsAmbiguous(".Global", s)
-
-	assignCmpRe := makeFunctionRegex("_ASSIGN_NE", 6)
-	s = assignCmpRe.ReplaceAllString(s, makeTemplateFunctionCall("Global.Assign", 6))
-
-	populateSels := makeFunctionRegex("_POPULATE_SELS", 3)
-	s = populateSels.ReplaceAllString(s, `{{template "populateSels" buildDict "Global" . "BatchHasSelection" $3}}`)
-
-	matchLoop := makeFunctionRegex("_MATCH_LOOP", 8)
-	s = matchLoop.ReplaceAllString(
-		s, `{{template "matchLoop" buildDict "Global" . "LhsMaybeHasNulls" $7 "RhsMaybeHasNulls" $8}}`)
-
-	tmpl, err := template.New("hash_aggregator").Funcs(template.FuncMap{"buildDict": buildDict}).Parse(s)
-	if err != nil {
-		return err
-	}
-
-	return tmpl.Execute(wr, sameTypeComparisonOpToOverloads[tree.NE])
+	_, err := fmt.Fprint(wr, inputFileContents)
+	return err
 }
 
 func init() {
