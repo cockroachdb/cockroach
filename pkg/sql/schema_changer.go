76a42a5b6f67c643b43f18b53d8640bf4c426874
Rohan Yadav <rohany@alumni.cmu.edu>
2020-07-12 23:58:38 -0400

    sql: drop types as part of a `DROP DATABASE` statement
    Fixes #49940.
    
    This commit adds logic to drop all types as part of a `DROP DATABASE`
    cascade.
    
    Release note: None

--- pkg/sql/schema_changer.go
+++ pkg/sql/schema_changer.go
@@ -1777,17 +1777,29 @@ func (r schemaChangeResumer) Resume(
 
 	// For an empty database, the zone config for it was already GC'ed and there's
 	// nothing left to do.
-	if details.DroppedDatabaseID != sqlbase.InvalidID && len(details.DroppedTables) == 0 {
+	if details.DroppedDatabaseID != sqlbase.InvalidID && len(details.DroppedTables) == 0 && len(details.DroppedTypes) == 0 {
 		return nil
 	}
 
 	// If a database is being dropped, handle this separately by draining names
-	// for all the tables.
+	// for all the tables and types.
 	//
 	// This also covers other cases where we have a leftover 19.2 job that drops
 	// multiple tables in a single job (e.g., TRUNCATE on multiple tables), so
 	// it's possible for DroppedDatabaseID to be unset.
 	if details.DroppedDatabaseID != sqlbase.InvalidID || len(details.DroppedTables) > 1 {
+		// Drop all of the types in the database.
+		for i := range details.DroppedTypes {
+			ts := &typeSchemaChanger{
+				typeID:  details.DroppedTypes[i],
+				execCfg: p.ExecCfg(),
+			}
+			if err := ts.execWithRetry(ctx); err != nil {
+				return err
+			}
+		}
+
+		// Drop the tables now.
 		for i := range details.DroppedTables {
 			droppedTable := &details.DroppedTables[i]
 			if err := execSchemaChange(droppedTable.ID, sqlbase.InvalidMutationID, details.DroppedDatabaseID); err != nil {
