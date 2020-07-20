2643e24406f558b71db05b5da287c24915103feb
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-07-20 11:32:51 -0700

    sql: fix pagination in UPSERT
    `optTableUpserter` was incorrectly using `curBatchSize` method
    implementation of `tableUpserterBase` which returns the number of rows
    in `insertRows` row container, but that container is not actually used
    `optTableUpserter`. As a result, `curBatchSize` was always considered
    0, and we didn't perform the pagination on the UPSERTs when driven by
    the optimizer.
    
    Release note (bug fix): Previously, CockroachDB could hit a "command is
    too large" error when performing UPSERT operation with many values.
    Internally, we attempt to perform such operation by splitting it into
    "batches", but the batching mechanism was broken.

--- pkg/sql/tablewriter_upsert_opt.go
+++ pkg/sql/tablewriter_upsert_opt.go
@@ -92,7 +92,7 @@ func (tu *optTableUpserter) init(txn *client.Txn, evalCtx *tree.EvalContext) err
 		tu.rowsUpserted = rowcontainer.NewRowContainer(
 			evalCtx.Mon.MakeBoundAccount(),
 			sqlbase.ColTypeInfoFromColDescs(tu.returnCols),
-			tu.insertRows.Len(),
+			0, /* rowCapacity */
 		)
 	}
 
@@ -159,6 +159,11 @@ func (tu *optTableUpserter) atBatchEnd(ctx context.Context, traceKV bool) error 
 	// Nothing to do, because the row method does everything.
 	return nil
 }
+
+// curBatchSize is part of the extendedTableWriter interface. Note that we need
+// to override tableUpserterBase.curBatchSize because the optimizer-driven
+// UPSERTs do not store the insert rows in insertRows row container.
+func (tu *optTableUpserter) curBatchSize() int { return tu.batchSize }
 
 // insertNonConflictingRow inserts the given source row into the table when
 // there was no conflict. If the RETURNING clause was specified, then the
