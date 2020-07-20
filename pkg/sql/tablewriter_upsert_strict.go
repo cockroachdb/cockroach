ba2e7b3c8430b08b7159aa6a511a1d98b0a47b09
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-07-20 11:32:51 -0700

    sql: fix pagination in UPSERT
    `optTableUpserter` and `fastTableUpserter` were incorrectly using
    `curBatchSize` method implementation of `tableUpserterBase` which
    returns the number of rows in `insertRows` row container, but that \
    container is not actually used by those two upserters. As a result,
    `curBatchSize` was always considered 0, and we didn't perform the
    pagination on the UPSERTs when driven by the optimizer. This is now
    fixed by keeping `tableWriterBase` implementation of `curBatchSize`
    method and overriding it by `tableUpserter` and `strictTableUpserter`
    since those two actually use a row container.
    
    Release note (bug fix): Previously, CockroachDB could hit a "command is
    too large" error when performing UPSERT operation with many values.
    Internally, we attempt to perform such operation by splitting it into
    "batches", but the batching mechanism was broken.

--- pkg/sql/tablewriter_upsert_strict.go
+++ pkg/sql/tablewriter_upsert_strict.go
@@ -101,6 +101,11 @@ func (tu *strictTableUpserter) atBatchEnd(ctx context.Context, traceKV bool) err
 	return nil
 }
 
+// curBatchSize is part of the extendedTableWriter interface. Note that we need
+// to override tableWriterBase.curBatchSize because strictTableUpserter stores
+// the insert rows not in the batch but in insertRows row container.
+func (tu *strictTableUpserter) curBatchSize() int { return tu.insertRows.Len() }
+
 // Get all unique indexes and store them in tu.ConflictIndexes.
 func (tu *strictTableUpserter) getUniqueIndexes() (err error) {
 	tableDesc := tu.tableDesc()
