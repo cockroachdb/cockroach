d39631c85d5194c507ea95f111376ed2e6e10e3a
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2020-07-21 11:48:51 -0400

    teamcity-trigger: stress kvnemesis for the full 1h
    This commit reworks the stress invocation logic in teamcity-trigger to
    remove the `-maxruns 100` flag when stressing `pkg/kv/kvnemesis`. This
    allows the package to run for the full 1h each night.
    
    The commit does this by pulling these flags out of teamcity-stress.sh
    and into teamcity-trigger/main.go, which gives us more flexibility for
    this kind of conditional invocation, which we were already using for
    `pkg/sql/logictest`.

--- build/teamcity-stress.sh
+++ build/teamcity-stress.sh
@@ -47,7 +47,7 @@ go install ./pkg/cmd/github-post
 # are test failures.
 # Use an `if` so that the `-e` option doesn't stop the script on error.
 if ! stdbuf -oL -eL \
-  make stress PKG="$PKG" TESTTIMEOUT=40m GOFLAGS="$GOFLAGS" TAGS="$TAGS" STRESSFLAGS="-maxruns 100 -maxtime 1h -maxfails 1 -stderr $STRESSFLAGS" 2>&1 \
+  make stress PKG="$PKG" TESTTIMEOUT=40m GOFLAGS="$GOFLAGS" TAGS="$TAGS" STRESSFLAGS="-stderr $STRESSFLAGS" 2>&1 \
   | tee artifacts/stress.log; then
   exit_status=${PIPESTATUS[0]}
   go tool test2json -t -p "${PKG}" < artifacts/stress.log | github-post
