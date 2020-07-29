7ba27cb68292aa36ea6be8ea9292aaa4c29aa614
Oliver Tan <otan@cockroachlabs.com>
2020-07-28 21:07:12 -0700

    teams: add TEAMS.yaml to repo
    This commit adds a concept of TEAMS.yaml to the repo, allowing us to
    attribute Github aliases to team metadata. This will be useful in the
    times to come as we attribute ownership, and map that to contact
    information as well as Github board items.
    
    Release note: None

---
+++ pkg/internal/team/team.go
@@ -0,0 +1,67 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+// Package team involves processing team information based on a yaml
+// file containing team metadata.
+package team
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+// DefaultTeamsYAMLLocation is the default location for the TEAMS.yaml file.
+var DefaultTeamsYAMLLocation string
+
+func init() {
+	DefaultTeamsYAMLLocation = filepath.Join(
+		os.Getenv("GOPATH"),
+		"src/github.com/cockroachdb/cockroach/TEAMS.yaml",
+	)
+}
+
+// Alias is the name of a team.
+type Alias string
+
+// Team is a team in the CockroachDB repo.
+type Team struct {
+	// Alias is the name of the team.
+	// It is populated from using the key of the yaml file.
+	Alias Alias `yaml:"-"`
+	// Email is the email address for this team.
+	Email string `yaml:"email"`
+	// Slack is the slack channel for this team.
+	Slack string `yaml:"slack"`
+	// TriageColumnID is the Github Column ID to assign issues to.
+	TriageColumnID int `yaml:"triage_column_id"`
+}
+
+// LoadTeams loads the teams from an io input.
+// It is expected the input is in YAML format.
+func LoadTeams(f io.Reader) (map[Alias]Team, error) {
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	var ret map[Alias]Team
+	if err := yaml.UnmarshalStrict(b, &ret); err != nil {
+		return nil, err
+	}
+	// Populate the Alias value of each team.
+	for k, v := range ret {
+		v.Alias = k
+		ret[k] = v
+	}
+	return ret, nil
+}
