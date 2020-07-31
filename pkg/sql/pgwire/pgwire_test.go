38cc2986edde9a0678107a1a2ed7d7bc0f8bd96d
Eugene Kalinin <e.v.kalinin@gmail.com>
2020-07-31 23:53:38 +0300

    sql: improve SHOW TABLES to show row count
    This change adds column "estimated_row_count" into result of the "SHOW TABLES"
    to show estimated (not real) number of rows.
    
    Besides that the body of the `delegator.delegateShowTables` was a bit
    simplified (we do not need two almost similar versions of the query) and
    new virtual table `crdb_internal.table_row_statistics` was added (to
    show stats for non-root users).
    
    Release note (sql change): This change modifies SHOW TABLES to return
    estimates number of rows. New column's name is `estimated_row_count`.
    Number of rows is taken from `system.table_statistics` table (via
    `crdb_internal.table_row_statistics` which shows only tables accessible
    for current user).

--- pkg/sql/pgwire/pgwire_test.go
+++ pkg/sql/pgwire/pgwire_test.go
@@ -553,7 +553,7 @@ func TestPGPreparedQuery(t *testing.T) {
 			baseTest.Results("users", "primary", false, 1, "username", "ASC", false, false),
 		}},
 		{"SHOW TABLES FROM system", []preparedQueryTest{
-			baseTest.Results("public", "comments", "table").Others(27),
+			baseTest.Results("public", "comments", "table", gosql.NullString{}).Others(27),
 		}},
 		{"SHOW SCHEMAS FROM system", []preparedQueryTest{
 			baseTest.Results("crdb_internal").Others(4),
