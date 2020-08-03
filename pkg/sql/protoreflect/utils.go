87cade7e03bb6de05c7793c5c6e4a1de4ced0587
Yevgeniy Miretskiy <yevgeniy@cockroachlabs.com>
2020-08-03 11:34:01 -0400

    sql: Implement `crdb_internal.json_to_pb`
    `crdb_internal.json_to_pb` is an internal function that
    can serialize any protocol message linked into binary,
    from its JSONB representation to native protocol byte representation.
    
    The function takes two arguments: a fully qualified protocol
    message name, and its JSONB representation, and returns
    protocol message byte representation.
    
    ```
    SELECT crdb_internal.json_to_pb(
       'cockroach.sql.sqlbase.Descriptor',
        crdb_internal.pb_to_json('cockroach.sql.sqlbase.Descriptor', descriptor)
    ) FROM system.descriptor WHERE id = 1
    ```
    
    Release Notes: None

--- pkg/sql/protoreflect/utils.go
+++ pkg/sql/protoreflect/utils.go
@@ -11,8 +11,10 @@
 package protoreflect
 
 import (
-	"encoding/json"
+	json_encoding "encoding/json"
 	"reflect"
+	"strconv"
+	"strings"
 
 	jsonb "github.com/cockroachdb/cockroach/pkg/util/json"
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
@@ -21,9 +23,9 @@ import (
 	"github.com/gogo/protobuf/proto"
 )
 
-// DecodeMessage decodes protocol message specified as its fully
-// qualified name, and it's marshaled data, into a protoutil.Message.
-func DecodeMessage(name string, data []byte) (protoutil.Message, error) {
+// NewMessage creates a new protocol message object, given its fully
+// qualified name.
+func NewMessage(name string) (protoutil.Message, error) {
 	// Get the reflected type of the protocol message.
 	rt := proto.MessageType(name)
 	if rt == nil {
@@ -37,9 +39,7 @@ func DecodeMessage(name string, data []byte) (protoutil.Message, error) {
 	}
 
 	// Construct message of appropriate type, through reflection.
-	rt = rt.Elem()
-	rv := reflect.New(rt)
-
+	rv := reflect.New(rt.Elem())
 	msg, ok := rv.Interface().(protoutil.Message)
 
 	if !ok {
@@ -47,6 +47,49 @@ func DecodeMessage(name string, data []byte) (protoutil.Message, error) {
 		return nil, errors.AssertionFailedf(
 			"unexpected proto type for %s; expected protoutil.Message, got %T",
 			name, rv.Interface())
+	}
+	return msg, nil
+}
+
+// makeJSON exists so that the encoding produced by MessageToJSON is round
+// trip-able: that is, we can take that encoding and convert it back to the
+// protocol message representation.
+// See encoding comment in MessageToJSON below.
+func makeJSON(i interface{}) (jsonb.JSON, error) {
+	switch val := i.(type) {
+	case float64:
+		return jsonb.FromString(strconv.FormatFloat(val, 'f', -1, 64)), nil
+	case map[string]interface{}:
+		b := jsonb.NewObjectBuilder(len(val))
+		for k, v := range val {
+			j, err := makeJSON(v)
+			if err != nil {
+				return nil, err
+			}
+			b.Add(k, j)
+		}
+		return b.Build(), nil
+	case []interface{}:
+		b := jsonb.NewArrayBuilder(len(val))
+		for _, v := range val {
+			j, err := makeJSON(v)
+			if err != nil {
+				return nil, err
+			}
+			b.Add(j)
+		}
+		return b.Build(), nil
+	default:
+		return jsonb.MakeJSON(val)
+	}
+}
+
+// DecodeMessage decodes protocol message specified as its fully
+// qualified name, and it's marshaled data, into a protoutil.Message.
+func DecodeMessage(name string, data []byte) (protoutil.Message, error) {
+	msg, err := NewMessage(name)
+	if err != nil {
+		return nil, err
 	}
 
 	// Now, parse data as our proto message.
@@ -59,7 +102,7 @@ func DecodeMessage(name string, data []byte) (protoutil.Message, error) {
 // MessageToJSON converts a protocol message into a JSONB object.
 func MessageToJSON(msg protoutil.Message) (jsonb.JSON, error) {
 	// Convert to json.
-	jsonEncoder := jsonpb.Marshaler{}
+	jsonEncoder := jsonpb.Marshaler{EmitDefaults: false}
 	msgJSON, err := jsonEncoder.MarshalToString(msg)
 	if err != nil {
 		return nil, errors.Wrapf(err, "marshaling %s; msg=%+v", proto.MessageName(msg), msg)
@@ -68,19 +111,47 @@ func MessageToJSON(msg protoutil.Message) (jsonb.JSON, error) {
 	// We need to take encoded json object and, unfortunately, unmarshal
 	// it again, this time as a string->value map, so that we can construct
 	// JSONB object to return.
+	//
+	// NB: this unfortunate conversation to map[string]interface{} also
+	// creates a problem if we want to take JSONB returned by this function,
+	// and convert it back to protocol message.
+	// json_encoding.Unmarshal encodes *all* numeric values as float64.
+	// This has the unfortunate effect that when we construct our JSONB object
+	// below, those numbers will be encoded using scientific notation
+	// due to how APD library formats float64 (e.g. instead of "50" for table id,
+	// we will emit "5E1").  This, in turn causes problems if we attempt
+	// to unmarshal this json as a protocol message.
+	// TODO(yevgeniy): It would be nice to be able to pass formatting context to APD.
+	// TODO(yevgeniy): It would be even better if we didn't have to convert
+	//    from msgJSON to map[] and instead, be able to build jsonb object directly.
 	var msgMap map[string]interface{}
-	if err := json.Unmarshal([]byte(msgJSON), &msgMap); err != nil {
+	if err := json_encoding.Unmarshal([]byte(msgJSON), &msgMap); err != nil {
 		return nil, errors.Wrap(err, "unmarshaling to map")
 	}
 
 	// Build JSONB.
 	builder := jsonb.NewObjectBuilder(len(msgMap))
 	for k, v := range msgMap {
-		jv, err := jsonb.MakeJSON(v)
+		jv, err := makeJSON(v)
 		if err != nil {
 			return nil, errors.Wrapf(err, "encoding json value for %s", k)
 		}
 		builder.Add(k, jv)
 	}
 	return builder.Build(), nil
+}
+
+// JSONBMarshalToMessage initializes the target protocol message with
+// the data in the input JSONB object.
+// Returns serialized byte representation of the protocol message.
+func JSONBMarshalToMessage(input jsonb.JSON, target protoutil.Message) ([]byte, error) {
+	json := &jsonpb.Unmarshaler{}
+	if err := json.Unmarshal(strings.NewReader(input.String()), target); err != nil {
+		return nil, errors.Wrapf(err, "unmarshaling json to %s", proto.MessageName(target))
+	}
+	data, err := protoutil.Marshal(target)
+	if err != nil {
+		return nil, errors.Wrapf(err, "marshaling to proto %s", proto.MessageName(target))
+	}
+	return data, nil
 }
