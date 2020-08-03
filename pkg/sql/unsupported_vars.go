88fd7b40d27aa9ca91c16f9816f4799b5e238985
Oliver Tan <otan@cockroachlabs.com>
2020-08-03 11:40:44 -0700

    sql: support synchronous_commit and enable_seqscan as dummy no-op
    These vars are set by `osm2pgsql` and `ogr2ogr` respectively. These
    default to the ON state, the OFF state affects performance but not
    correctness.
    
    Release note (sql change): Support the setting and getting of the
    `synchronous_commit` and `enable_seqscan` variables, which do not affect
    any performance characteristics. These are no-ops enabled to allow
    certain tools to work.

--- pkg/sql/unsupported_vars.go
+++ pkg/sql/unsupported_vars.go
@@ -10,6 +10,35 @@
 
 package sql
 
+import "github.com/cockroachdb/cockroach/pkg/settings"
+
+// DummyVars contains a list of dummy vars we do not support that
+// PostgreSQL does, but are required as an easy fix to make certain
+// tooling/ORMs work. These vars should not affect the correctness
+// of results.
+var DummyVars = map[string]sessionVar{
+	"enable_seqscan": makeDummyBooleanSessionVar(
+		"enable_seqscan",
+		func(evalCtx *extendedEvalContext) string {
+			return formatBoolAsPostgresSetting(evalCtx.SessionData.EnableSeqScan)
+		},
+		func(m *sessionDataMutator, v bool) {
+			m.SetEnableSeqScan(v)
+		},
+		func(sv *settings.Values) string { return "on" },
+	),
+	"synchronous_commit": makeDummyBooleanSessionVar(
+		"synchronous_commit",
+		func(evalCtx *extendedEvalContext) string {
+			return formatBoolAsPostgresSetting(evalCtx.SessionData.SynchronousCommit)
+		},
+		func(m *sessionDataMutator, v bool) {
+			m.SetSynchronousCommit(v)
+		},
+		func(sv *settings.Values) string { return "on" },
+	),
+}
+
 // UnsupportedVars contains the set of PostgreSQL session variables
 // and client parameters that are not supported in CockroachDB.
 // These are used to produce error messages and telemetry.
@@ -70,7 +99,7 @@ var UnsupportedVars = func(ss ...string) map[string]struct{} {
 	"enable_material",
 	"enable_mergejoin",
 	"enable_nestloop",
-	"enable_seqscan",
+	// "enable_seqscan",
 	"enable_sort",
 	"enable_tidscan",
 	"escape_string_warning",
@@ -135,8 +164,8 @@ var UnsupportedVars = func(ss ...string) map[string]struct{} {
 	// "ssl_renegotiation_limit",
 	// "standard_conforming_strings",
 	// "statement_timeout",
-	//	"synchronize_seqscans",
-	"synchronous_commit",
+	// "synchronize_seqscans",
+	// "synchronous_commit",
 	"tcp_keepalives_count",
 	"tcp_keepalives_idle",
 	"tcp_keepalives_interval",
