47e0b4da4f3e469ce99b27c6397f64f996e8ca62
Andrei Matei <andrei@cockroachlabs.com>
2020-08-04 15:53:23 -0400

    sql: drop the "experimental" in experimental_follower_read_timestamp
    The known badness with follower reads was fixed - most recently #49391, #44053
    and #44878.
    
    At this point, the "experimental" prefix doesn't serve any purpose, and
    it unnecessarily raises questions.
    
    The previous name stays as an alias for backwards compatibility.
    
    Fixes #45243
    
    Release note: The experimental_follower_read_timestamp() function was
    renamed to follower_read_timestamp(), signifying more confidence in
    CRDB's follower read implementation. The previous name remains a
    supported alias.

--- pkg/sql/sem/tree/as_of.go
+++ pkg/sql/sem/tree/as_of.go
@@ -28,7 +28,7 @@ import (
 // FollowerReadTimestampFunctionName is the name of the function which can be
 // used with AOST clauses to generate a timestamp likely to be safe for follower
 // reads.
-const FollowerReadTimestampFunctionName = "experimental_follower_read_timestamp"
+const FollowerReadTimestampFunctionName = "follower_read_timestamp"
 
 var errInvalidExprForAsOf = errors.Errorf("AS OF SYSTEM TIME: only constant expressions or " +
 	FollowerReadTimestampFunctionName + " are allowed")
@@ -55,7 +55,8 @@ func EvalAsOfTimestamp(
 		if err != nil {
 			return hlc.Timestamp{}, errInvalidExprForAsOf
 		}
-		if def.Name != FollowerReadTimestampFunctionName {
+		if def.Name != FollowerReadTimestampFunctionName &&
+			def.Name != "experimental_"+FollowerReadTimestampFunctionName {
 			return hlc.Timestamp{}, errInvalidExprForAsOf
 		}
 		if te, err = fe.TypeCheck(ctx, semaCtx, types.TimestampTZ); err != nil {
