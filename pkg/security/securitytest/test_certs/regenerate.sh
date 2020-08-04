84403bf85259fbd35aefd4b474d3ef48beefa63e
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2020-08-04 11:16:23 +0200

    test_certs: remove stale comment
    Release note: None

--- pkg/security/securitytest/test_certs/regenerate.sh
+++ pkg/security/securitytest/test_certs/regenerate.sh
@@ -17,13 +17,4 @@ done
 ./cockroach mt cert --certs-dir="${dir_n}" --ca-key="${dir_n}/ca-server-tenant.key" create-tenant-server-ca
 ./cockroach mt cert --certs-dir="${dir_n}" --ca-key="${dir_n}/ca-server-tenant.key" create-tenant-server 127.0.0.1 ::1 localhost *.local
 
-# HACK(tbg): at the time of writing, the KV layer does not accept tenant client
-# certs yet, but tenants do use them. We overwrite the embedded tenant client
-# certs with those the internal node user would use to bridge this gap.
-# This can be used during local development.
-#
-# cat "${dir_n}/node.crt" > "${dir_n}/client-tenant.123456789.crt"
-# cat "${dir_n}/node.key" > "${dir_n}/client-tenant.123456789.key"
-# cat "${dir_n}/ca.crt" > "${dir_n}/ca-server-tenant.crt"
-
 make generate PKG=./pkg/security/securitytest
