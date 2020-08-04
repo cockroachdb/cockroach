1a3d7446292a6f9f7b6113f31ff0641b3778f43e
Spas Bojanov <spas@cockroachlabs.com>
2020-08-04 19:05:42 +0300

    sqlliveness: refactor code for testing and add tests
    Release note: none.

---
+++ pkg/sql/sqlliveness/slstorage/test_helpers.go
@@ -0,0 +1,72 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package slstorage
+
+import (
+	"context"
+	"sync"
+
+	"github.com/cockroachdb/cockroach/pkg/kv"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlliveness"
+	"github.com/cockroachdb/errors"
+)
+
+// FakeStorage implements the sqlliveness.Storage interface.
+type FakeStorage struct {
+	mu       sync.Mutex
+	sessions map[string]sqlliveness.Session
+}
+
+func NewFakeStorage() *FakeStorage {
+	fs := &FakeStorage{}
+	fs.sessions = make(map[string]sqlliveness.Session)
+	return fs
+}
+
+func (s *FakeStorage) Start(_ context.Context) {}
+
+func (s *FakeStorage) IsAlive(
+	_ context.Context, _ *kv.Txn, sid sqlliveness.SessionID,
+) (alive bool, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	_, ok := s.sessions[string(sid)]
+	return ok, nil
+}
+
+func (s *FakeStorage) Insert(_ context.Context, session sqlliveness.Session) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	key := string(session.ID())
+	if _, ok := s.mu.sessions[key]; ok {
+		return errors.Errorf("Session %s already exists", session.ID())
+	}
+	s.sessions[key] = session
+	return nil
+}
+
+func (s *FakeStorage) Update(_ context.Context, session sqlliveness.Session) (bool, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	key := string(session.ID())
+	if _, ok := s.mu.sessions[key]; !ok {
+		return false, nil
+	}
+	s.sessions[key] = session
+	return true, nil
+}
+
+func (s *FakeStorage) Delete(_ context.Context, session sqlliveness.Session) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.sessions, string(session.ID()))
+	return nil
+}
