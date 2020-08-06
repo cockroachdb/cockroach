1dfdf0d373de608d864b9b01ed0d8132347a9981
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2020-08-06 17:59:01 +0200

    cliccl: add ./cockroach mt start-proxy
    For end-to-end testing purposes, we want a dummy implementation of the
    sql proxy in the public repo. This commit adds it; you can now run the
    demo below (note the `./cockroach mt start-proxy` part towards the end).
    Note that the point is not that this replicates "exactly" the internal
    proxy we are planning to deploy, but that it exercises the main
    mechanism by which the tenant identifier is read from the connection
    parameters sufficiently well to give us confidence that the drivers and
    ORMs we test against will work (Examples-ORMs is the main anticipated
    user of this).
    
    ```bash
    
    set -euxo pipefail
    
    killall -9 cockroach || true
    
    rm -rf certs-{sql,kv,user} cockroach-data
    mkdir -p certs-{sql,kv,user}
    sql="certs-sql"
    kv="certs-kv"
    
    ./cockroach cert --certs-dir="${kv}" --ca-key="${kv}/ca.key" create-ca
    ./cockroach cert --certs-dir="${kv}" --ca-key="${kv}/ca.key" create-node 127.0.0.1 ::1 localhost *.local
    ./cockroach cert --certs-dir="${kv}" --ca-key="${kv}/ca.key" create-client root
    
    ./cockroach mt cert --certs-dir="${kv}" --ca-key="${kv}/ca-server-tenant.key" create-tenant-server-ca
    cp "${kv}/ca-server-tenant.crt" "${sql}"
    ./cockroach mt cert --certs-dir="${kv}" --ca-key="${kv}/ca-server-tenant.key" create-tenant-server 127.0.0.1 ::1 localhost *.local
    
    rm "${kv}"/ca*.key
    
    ./cockroach cert --certs-dir="${sql}" --ca-key="${sql}/ca.key" create-ca
    ./cockroach cert --certs-dir="${sql}" --ca-key="${sql}/ca.key" create-node 127.0.0.1 ::1 localhost *.local
    
    ./cockroach cert --certs-dir="${sql}" --ca-key="${sql}/ca.key" create-client root
    mv "${sql}"/client.root.* certs-user
    cp "${sql}/ca.crt" certs-user
    
    ./cockroach mt cert --certs-dir="${sql}" --ca-key="${sql}/ca-client-tenant.key" create-tenant-client-ca
    cp "${sql}/ca-client-tenant.crt" "${kv}"
    ./cockroach mt cert --certs-dir="${sql}" --ca-key="${sql}/ca-client-tenant.key" create-tenant-client 10
    
    rm "${sql}"/ca*.key
    
    ./cockroach start-single-node --tenant-addr 127.0.0.1:36257 --certs-dir certs-kv --host 127.0.0.1 --background
    ./cockroach sql --certs-dir certs-kv --host 127.0.0.1 -e 'select crdb_internal.create_tenant(10);'
    rm certs-kv/client.root.*
    
    ./cockroach mt start-sql --certs-dir certs-sql --tenant-id 10 --kv-addrs 127.0.0.1:36257 --sql-addr 127.0.0.1:46257 &
    sleep 1
    
    ./cockroach sql --url 'postgres://root@127.0.0.1:46257?sslmode=verify-ca&sslkey=certs-user/client.root.key&sslcert=certs-user/client.root.crt&sslrootcert=certs-user/ca.crt' -e "CREATE USER bob WITH PASSWORD 'foo';"
    
    ./cockroach mt start-proxy --listen-addr 127.0.0.1:56257 --target-addr 127.0.0.1:46257 --listen-cert certs-sql/node.crt --listen-key certs-sql/node.key &
    sleep 1
    
    ./cockroach sql --url 'postgres://bob:foo@127.0.0.1:56257/prancing-pony.defaultdb?sslmode=verify-ca&sslrootcert=certs-user/ca.crt' -e "SELECT 'this worked!';"
    
    ./cockroach sql --url 'postgres://bob:foo@127.0.0.1:56257?options=prancing-pony&sslmode=verify-ca&sslrootcert=certs-user/ca.crt'
    
    ```
    
    Release note: None

--- pkg/cli/mt.go
+++ pkg/cli/mt.go
@@ -12,6 +12,11 @@ package cli
 
 import "github.com/spf13/cobra"
 
+// AddMTCommand adds a subcommand to `./cockroach mt`.
+func AddMTCommand(cmd *cobra.Command) {
+	mtCmd.AddCommand(cmd)
+}
+
 func init() {
 	cockroachCmd.AddCommand(mtCmd)
 	mtCmd.AddCommand(mtStartSQLCmd)
