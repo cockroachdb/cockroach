2cf3582215d28f92a00332bd49e54d5ec07494ea
Spas Bojanov <spas@cockroachlabs.com>
2020-08-11 14:40:32 +0300

    cli: add debug doctor command
    This commit intorduces two new debug commands:
    
    `/cockroach debug doctor cluster --url=<cluster_conn_string>`
    and
    `/cockroach debug doctor zipdir <debug-dir>`
    
    which validate the descriptors read from a live cluster or from an
    unzipped debug zip directory. The commands print out all descriptor ids
    that were inspected and the problems with specific ids if found.
    
    Fixes #52077.
    Informs #51153.
    
    Release note: none.

---
+++ pkg/cli/doctor.go
@@ -0,0 +1,166 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package cli
+
+import (
+	"bufio"
+	"database/sql/driver"
+	hx "encoding/hex"
+	"io"
+	"os"
+	"path"
+	"strconv"
+	"strings"
+
+	"github.com/cockroachdb/apd/v2"
+	"github.com/cockroachdb/cockroach/pkg/sql/doctor"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/builtins"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/cockroachdb/errors"
+	"github.com/spf13/cobra"
+)
+
+var debugDoctorCmd = &cobra.Command{
+	Use:   "doctor [command]",
+	Short: "run a cockroach doctor tool command",
+	Long: `
+Runs various consistency checks over cockroach internal system tables read from
+a live cluster or a unzipped debug zip.
+`,
+}
+
+var debugDoctorCmds = []*cobra.Command{
+	doctorZipDirCmd,
+	doctorClusterCmd,
+}
+
+var doctorZipDirCmd = &cobra.Command{
+	Use:   "zipdir <debug_zip_dir>",
+	Short: "run doctor tool on data from a directory unzipped from debug.zip",
+	Long: `
+Run doctor tool on system data from directory	created by unzipping debug.zip.
+`,
+	Args: cobra.ExactArgs(1),
+	RunE: RunZipDirDoctor,
+}
+
+var doctorClusterCmd = &cobra.Command{
+	Use:   "cluster --url=<cluster connection string>",
+	Short: "run doctor tool on live cockroach cluster",
+	Long: `
+Run doctor tool reading system data from a live cluster specified by --url.
+`,
+	Args: cobra.NoArgs,
+	RunE: MaybeDecorateGRPCError(RunClusterDoctor),
+}
+
+// RunClusterDoctor runs the doctors tool reading data from a live cluster.
+func RunClusterDoctor(cmd *cobra.Command, args []string) (retErr error) {
+	sqlConn, err := makeSQLClient("cockroach doctor", useSystemDb)
+	if err != nil {
+		return errors.Wrap(err, "could not establish connection to cluster")
+	}
+	defer sqlConn.Close()
+
+	rows, err := sqlConn.Query(`
+SELECT id, descriptor, crdb_internal_mvcc_timestamp AS mod_time_logical
+FROM system.descriptor 
+ORDER BY id`,
+		nil,
+	)
+	if err != nil {
+		return errors.Wrap(err, "could not read system.descriptor")
+	}
+
+	descTable := make([]doctor.DescriptorTableRow, 0)
+	vals := make([]driver.Value, 3)
+	for {
+		if err := rows.Next(vals); err == io.EOF {
+			break
+		}
+		var row doctor.DescriptorTableRow
+		if id, ok := vals[0].(int64); ok {
+			row.ID = id
+		} else {
+			return errors.Errorf("unexpected value: %T of %v", vals[0], vals[0])
+		}
+		if descBytes, ok := vals[1].([]byte); ok {
+			row.DescBytes = descBytes
+		} else {
+			return errors.Errorf("unexpected value: %T of %v", vals[1], vals[1])
+		}
+		if vals[2] == nil {
+			row.ModTime = hlc.Timestamp{WallTime: timeutil.Now().UnixNano()}
+		} else if mt, ok := vals[2].([]byte); ok {
+			decimal, _, err := apd.NewFromString(string(mt))
+			if err != nil {
+				return err
+			}
+			ts, err := tree.DecimalToHLC(decimal)
+			if err != nil {
+				return err
+			}
+			row.ModTime = ts
+		} else {
+			return errors.Errorf("unexpected value: %T of %v", vals[2], vals[2])
+		}
+		descTable = append(descTable, row)
+	}
+
+	// TODO(spaskob): add --verbose flag.
+	if err := doctor.Examine(descTable, false); err != nil {
+		return errors.Wrap(err, "examine failed")
+	}
+	return nil
+}
+
+// RunZipDirDoctor runs the doctors tool reading data from a debug zip dir.
+func RunZipDirDoctor(cmd *cobra.Command, args []string) (retErr error) {
+	// To make parsing user functions code happy.
+	_ = builtins.AllBuiltinNames
+
+	file, err := os.Open(path.Join(args[0], "system.descriptor.txt"))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	descTable := make([]doctor.DescriptorTableRow, 0)
+	sc := bufio.NewScanner(file)
+	firstLine := true
+	for sc.Scan() {
+		if firstLine {
+			firstLine = false
+			continue
+		}
+		fields := strings.Fields(sc.Text())
+		last := len(fields) - 1
+		i, err := strconv.Atoi(fields[0])
+		if err != nil {
+			return errors.Errorf("failed to parse descriptor id %s: %v", fields[0], err)
+		}
+
+		descBytes, err := hx.DecodeString(fields[last])
+		if err != nil {
+			return errors.Errorf("failed to decode hex descriptor %d: %v", i, err)
+		}
+		ts := hlc.Timestamp{WallTime: timeutil.Now().UnixNano()}
+		descTable = append(descTable, doctor.DescriptorTableRow{ID: int64(i), DescBytes: descBytes, ModTime: ts})
+	}
+
+	// TODO(spaskob): add --verbose flag.
+	if err := doctor.Examine(descTable, false); err != nil {
+		return errors.Wrap(err, "examine failed")
+	}
+	return nil
+}
--- pkg/sql/sqlbase/table_desc.go
+++ pkg/sql/sqlbase/table_desc.go
@@ -14,7 +14,6 @@ import (
 	"context"
 
 	"github.com/cockroachdb/cockroach/pkg/keys"
-	"github.com/cockroachdb/cockroach/pkg/kv"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
@@ -72,7 +71,7 @@ type TableDescriptor interface {
 		getType func(descpb.ID) (TypeDescriptor, error),
 	) (descpb.IDs, error)
 
-	Validate(ctx context.Context, txn *kv.Txn, codec keys.SQLCodec) error
+	Validate(context.Context, protoGetter, keys.SQLCodec) error
 	IsVirtualTable() bool
 }
 
