dc826d7989b9e1f188a521f9e22406306d2ea927
Andrew Werner <ajwerner@cockroachlabs.com>
2020-08-12 12:59:53 -0400

    sqlliveness: wait for version to be activated in subsystems
    Prior to this commit we'd hit some errors which would get retried
    if the sqlmigration was slow. Better to just wait for it to happen.
    
    Also sneaks in some changes to use the context rather than quiescence
    channel because it makes the waiting cleaner.
    
    Release note: None

--- pkg/sql/sqlliveness/slinstance/slinstance_test.go
+++ pkg/sql/sqlliveness/slinstance/slinstance_test.go
@@ -15,6 +15,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlliveness"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlliveness/slinstance"
@@ -35,7 +36,10 @@ func TestSQLInstance(t *testing.T) {
 
 	mClock := hlc.NewManualClock(hlc.UnixNano())
 	clock := hlc.NewClock(mClock.UnixNano, time.Nanosecond)
-	settings := cluster.MakeClusterSettings()
+	settings := cluster.MakeTestingClusterSettingsWithVersions(
+		clusterversion.TestingBinaryVersion,
+		clusterversion.TestingBinaryMinSupportedVersion,
+		true /* initializeVersion */)
 	slinstance.DefaultTTL.Override(&settings.SV, 2*time.Microsecond)
 	slinstance.DefaultHeartBeat.Override(&settings.SV, time.Microsecond)
 
