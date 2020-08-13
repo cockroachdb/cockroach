04efbea410fdfeca52d64f5ba7a2843f5af09c50
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2020-08-13 09:26:04 -0400

    Replacing get() with Typescript optional chain operator
    As of TypeScript 3.7, the optional chaining operator provides roughly
    the same purpose as lodash/get (with better syntax in my opinion). I am
    removing get() and instead depending on an optional chain.
    
    There was also concern about the correctness of displaying
    `gc.ttlseconds` as a null value, which is valid. The error I am seeking
    to prevent here is that `gc` is null or undefined, and so if there is no
    `gc` value then I am instead choosing not to display a value at all.
    
    Release note (bug fix): don't display a value for gc.ttlseconds if not
    set

--- pkg/ui/src/views/shared/components/sql/highlight.tsx
+++ pkg/ui/src/views/shared/components/sql/highlight.tsx
@@ -11,7 +11,6 @@
 import * as hljs from "highlight.js";
 import React from "react";
 import * as protos from "src/js/protos";
-import get from "lodash/get";
 
 interface SqlBoxProps {
   value: string;
@@ -50,9 +49,13 @@ export class Highlight extends React.Component<SqlBoxProps> {
         <span className="hljs-label">range_max_bytes = </span>
         <span className="hljs-built_in">{`${String(zoneConfig.range_max_bytes)},`}</span>
         <br />
-        <span className="hljs-label">gc.ttlseconds = </span>
-        <span className="hljs-built_in">{`${get(zoneConfig, "gc.ttl_seconds", null)},`}</span>
-        <br />
+        {zoneConfig.gc?.ttl_seconds && (
+          <>
+            <span className="hljs-label">gc.ttlseconds = </span>
+            <span className="hljs-built_in">{`${zoneConfig.gc.ttl_seconds},`}</span>
+            <br />
+          </>
+        )}
         <span className="hljs-label">num_replicas = </span>
         <span className="hljs-built_in">{`${zoneConfig.num_replicas},`}</span>
         <br />
