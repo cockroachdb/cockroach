48c134fd9b3a4db64b94b55b4e8fee7166e050a6
Raphael 'kena' Poss <knz@thaumogen.net>
2020-08-13 13:41:58 +0200

    tests: skip distsql/TestAggregatorAgainstProcessor
    See #52742.
    
    Release note: None

--- pkg/sql/distsql/columnar_operators_test.go
+++ pkg/sql/distsql/columnar_operators_test.go
@@ -27,6 +27,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
+	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/stretchr/testify/require"
@@ -37,6 +38,9 @@ const randTypesProbability = 0.5
 
 func TestAggregatorAgainstProcessor(t *testing.T) {
 	defer leaktest.AfterTest(t)()
+
+	skip.WithIssue(t, 52742, "flaky")
+
 	st := cluster.MakeTestingClusterSettings()
 	evalCtx := tree.MakeTestingEvalContext(st)
 	defer evalCtx.Stop(context.Background())
