f7601602712b94b5fa37f179649e9d4ef0b676a2
Aditya Maru <adityamaru@gmail.com>
2020-08-17 14:02:58 -0400

    backupccl: add protected ts to BACKUPs run in DETACHED mode
    Fixes: #51067
    
    Release note: None

--- pkg/ccl/utilccl/jobutils.go
+++ pkg/ccl/utilccl/jobutils.go
@@ -9,11 +9,6 @@
 package utilccl
 
 import (
-	"context"
-
-	"github.com/cockroachdb/cockroach/pkg/jobs"
-	"github.com/cockroachdb/cockroach/pkg/sql"
-	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 )
@@ -33,19 +28,4 @@ var BulkJobExecutionResultHeader = sqlbase.ResultColumns{
 // job executes in detached mode (i.e. the caller doesn't wait for job completion).
 var DetachedJobExecutionResultHeader = sqlbase.ResultColumns{
 	{Name: "job_id", Typ: types.Int},
-}
-
-// StartAsyncJob starts running the job without blocking and waiting for job completion.
-func StartAsyncJob(
-	ctx context.Context, p sql.PlanHookState, jr *jobs.Record, resultsCh chan<- tree.Datums,
-) error {
-	// When running inside explicit transaction, we simply create the job record.
-	// We do not wait for the job to finish.
-	j, err := p.ExecCfg().JobRegistry.CreateAdoptableJobWithTxn(
-		ctx, *jr, p.ExtendedEvalContext().Txn)
-	if err != nil {
-		return err
-	}
-	resultsCh <- tree.Datums{tree.NewDInt(tree.DInt(*j.ID()))}
-	return nil
 }
