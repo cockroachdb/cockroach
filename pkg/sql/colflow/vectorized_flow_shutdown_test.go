570aec6b3470beed3b60082d5fa01daf7d00ec3f
Alfonso Subiotto Marques <alfonso@cockroachlabs.com>
2020-08-17 14:50:09 +0200

    colexec: remove internal cancellation behavior from unordered synchronizer
    Previously, the unordered synchronizer would cancel all inputs if one of the
    inputs encountered an error. This would result in possible context cancellation
    errors racing with the original error and would sometimes cause the original
    error to be overwritten (according to priority) in the distsql receiver (the
    root of a query).
    
    This behavior was incorrect, because what should happen is that the original
    error should be propagated followed by a call to DrainMeta by the caller to
    drain and close the remaining inputs. This commit removes internal context
    cancellation in favor of this behavior.
    
    Release note (bug fix): unexpected context cancellation errors could sometimes
    be returned in the vectorized execution engine. This is now fixed.

--- pkg/sql/colflow/vectorized_flow_shutdown_test.go
+++ pkg/sql/colflow/vectorized_flow_shutdown_test.go
@@ -385,10 +385,11 @@ func TestVectorizedFlowShutdown(t *testing.T) {
 						require.NotNil(t, meta.Err)
 						id, err := strconv.Atoi(meta.Err.Error())
 						require.NoError(t, err)
-						require.False(t, receivedMetaFromID[id])
 						receivedMetaFromID[id] = true
 					}
-					require.Equal(t, streamID, metaCount, fmt.Sprintf("received metadata from Outbox %+v", receivedMetaFromID))
+					for id, received := range receivedMetaFromID {
+						require.True(t, received, "did not receive metadata from Outbox %d", id)
+					}
 				case consumerClosed:
 					materializer.ConsumerClosed()
 				}
