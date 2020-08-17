a1c19fbfd516480e9111ce445ca3e1aaa023e0d8
Rohan Yadav <rohany@alumni.cmu.edu>
2020-08-17 11:12:29 -0400

    sqlbase: split ranges on materialized views
    Ensure that we split ranges on materialied views like normal tables.
    
    Release note: None

--- pkg/sql/sqlbase/system.go
+++ pkg/sql/sqlbase/system.go
@@ -34,7 +34,7 @@ func ShouldSplitAtDesc(rawDesc *roachpb.Value) bool {
 	}
 	switch t := desc.GetUnion().(type) {
 	case *descpb.Descriptor_Table:
-		if t.Table.IsView() {
+		if t.Table.IsView() && !t.Table.IsMaterializedView {
 			return false
 		}
 		return true
