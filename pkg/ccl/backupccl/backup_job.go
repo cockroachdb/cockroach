7cc55cfcb6c9d31f7cdde5ac3f7a31b91a1652e3
Paul Bardea <pbardea@gmail.com>
2020-08-18 10:22:36 -0400

    backupccl: write checkpoint in resuming instead of planning
    Previously, BACKUP would write its CHECKPOINT file during planning. This
    means that DETACHED BACKUPS would have the side-effect of leaving behind
    a checkpoint file. This is particularly problematic if the transaction
    in which the DETACHED BACKUP was created is rolledback or not yet
    committed.
    
    This change keeps the check for any existing backup manifests (both
    complete and checkpoint) inside planning, but moves the writing of the
    checkpoint file to only happen when the job starts. This makes an
    existing race between reading and writing this checkpoint file more
    evident, although not new:
    - BACKUP 1 checks for the checkpoint, doesn't find it
    - BACKUP 2 checks for the checkpoint, doesn't find it
    - BACKUP 1 writes their checkpoint
    - BACKUP 2 writes their checkpoint
    
    Release note (bug fix): Fix bug where non-committed DETACHED BACKUPS
    left files which falsely indicated that a BACKUP was in-progress.

--- pkg/ccl/backupccl/backup_job.go
+++ pkg/ccl/backupccl/backup_job.go
@@ -406,6 +406,24 @@ func (b *backupResumer) Resume(
 	details := b.job.Details().(jobspb.BackupDetails)
 	p := phs.(sql.PlanHookState)
 
+	// For all backups, partitioned or not, the main BACKUP manifest is stored at
+	// details.URI.
+	defaultConf, err := cloudimpl.ExternalStorageConfFromURI(details.URI, p.User())
+	if err != nil {
+		return errors.Wrapf(err, "export configuration")
+	}
+	defaultStore, err := p.ExecCfg().DistSQLSrv.ExternalStorage(ctx, defaultConf)
+	if err != nil {
+		return errors.Wrapf(err, "make storage")
+	}
+	defer defaultStore.Close()
+
+	if err := writeBackupManifest(
+		ctx, p.ExecCfg().Settings, defaultStore, BackupManifestCheckpointName, details.Encryption, &BackupManifest{},
+	); err != nil {
+		return errors.Wrapf(err, "writing checkpoint to %s", details.URI)
+	}
+
 	ptsID := details.ProtectedTimestampRecord
 	if ptsID != nil && !b.testingKnobs.ignoreProtectedTimestamps {
 		if err := p.ExecCfg().ProtectedTimestampProvider.Verify(ctx, *ptsID); err != nil {
@@ -428,17 +446,7 @@ func (b *backupResumer) Resume(
 		return pgerror.Wrapf(err, pgcode.DataCorrupted,
 			"unmarshal backup descriptor")
 	}
-	// For all backups, partitioned or not, the main BACKUP manifest is stored at
-	// details.URI.
-	defaultConf, err := cloudimpl.ExternalStorageConfFromURI(details.URI, p.User())
-	if err != nil {
-		return errors.Wrapf(err, "export configuration")
-	}
-	defaultStore, err := p.ExecCfg().DistSQLSrv.ExternalStorage(ctx, defaultConf)
-	if err != nil {
-		return errors.Wrapf(err, "make storage")
-	}
-	defer defaultStore.Close()
+
 	storageByLocalityKV := make(map[string]*roachpb.ExternalStorage)
 	for kv, uri := range details.URIsByLocalityKV {
 		conf, err := cloudimpl.ExternalStorageConfFromURI(uri, p.User())
