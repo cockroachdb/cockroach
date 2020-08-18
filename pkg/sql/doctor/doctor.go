b0f03cc3bf90061dcaa05647e8fe06cc4a50472c
Spas Bojanov <spas@cockroachlabs.com>
2020-08-18 17:10:39 +0300

    doctor: refactor for testing
    Release note: none.

--- pkg/sql/doctor/doctor.go
+++ pkg/sql/doctor/doctor.go
@@ -8,11 +8,14 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
+// Package doctor provides utilities for checking the consistency of cockroach
+// internal persisted metadata.
 package doctor
 
 import (
 	"context"
 	"fmt"
+	"io"
 
 	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
@@ -52,36 +55,35 @@ func NewProtoGetter(rows []DescriptorTableRow) (*sqlbase.MapProtoGetter, error) 
 }
 
 // Examine runs a suite of consistency checks over the descriptor table.
-func Examine(descTable []DescriptorTableRow, verbose bool) error {
-	fmt.Printf("Examining %d descriptors...\n", len(descTable))
+func Examine(descTable []DescriptorTableRow, verbose bool, stdout io.Writer) (ok bool, err error) {
+	fmt.Fprintf(stdout, "Examining %d descriptors...\n", len(descTable))
 	protoGetter, err := NewProtoGetter(descTable)
 	if err != nil {
-		return err
+		return false, err
 	}
 	var problemsFound bool
 	for _, row := range descTable {
 		desc := protoGetter.Protos[string(key(row.ID))].(*descpb.Descriptor)
 		t := sqlbase.TableFromDescriptor(desc, hlc.Timestamp{})
 		if t == nil {
+			// So far we only examine table descriptors. We may add checks for other
+			// descriptors in later versions.
 			continue
 		}
 		ts := hlc.Timestamp{WallTime: timeutil.Now().UnixNano()}
 		sqlbase.MaybeSetDescriptorModificationTimeFromMVCCTimestamp(context.Background(), desc, ts)
 		table := sqlbase.NewImmutableTableDescriptor(*sqlbase.TableFromDescriptor(desc, ts))
 		if int64(table.ID) != row.ID {
-			fmt.Printf("Table %3d: different id in the descriptor: %d", row.ID, table.ID)
+			fmt.Fprintf(stdout, "Table %3d: different id in the descriptor: %d", row.ID, table.ID)
 			problemsFound = true
 			continue
 		}
 		if err := table.Validate(context.Background(), protoGetter, keys.SystemSQLCodec); err != nil {
 			problemsFound = true
-			fmt.Printf("Table %3d: %s\n", table.ID, err.Error())
+			fmt.Fprintf(stdout, "Table %3d: %s\n", table.ID, err)
 		} else if verbose {
-			fmt.Printf("Table %3d: validated\n", table.ID)
+			fmt.Fprintf(stdout, "Table %3d: validated\n", table.ID)
 		}
 	}
-	if !problemsFound {
-		fmt.Println("No problems found!")
-	}
-	return nil
+	return !problemsFound, nil
 }
