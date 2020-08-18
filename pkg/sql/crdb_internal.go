e8e9d8c848107be64750430930f89d21117b21b4
Radu Berinde <radu@cockroachlabs.com>
2020-08-17 21:42:55 -0400

    sql: don't key statement statistics on distribution/vectorization
    The statement statistics code buckets statements separately depending on the
    distribution and vectorization flags. This is not very useful: we normally don't
    expect these flags to change for the same statement; and the UI doesn't show
    them separately anyway. In addition, it poses a problem to switching the explain
    tree (presented to UI) to use the new explain infrastructure: we need to know if
    we will need the tree upfront (before execbuild).
    
    This change removes these flags from the key. The flags are still populated in
    the `StatementStatisticsKey`.
    
    Release note: None

--- pkg/sql/crdb_internal.go
+++ pkg/sql/crdb_internal.go
@@ -778,10 +778,17 @@ CREATE TABLE crdb_internal.node_statement_statistics (
 				if s.data.SensitiveInfo.LastErr != "" {
 					errString = tree.NewDString(s.data.SensitiveInfo.LastErr)
 				}
+				var flags string
+				if s.distSQLUsed {
+					flags = "+"
+				}
+				if stmtKey.failed {
+					flags = "!" + flags
+				}
 				err := addRow(
 					tree.NewDInt(tree.DInt(nodeID)),
 					tree.NewDString(appName),
-					tree.NewDString(stmtKey.flags()),
+					tree.NewDString(flags),
 					tree.NewDString(stmtKey.stmt),
 					anonymized,
 					tree.NewDInt(tree.DInt(s.data.Count)),
