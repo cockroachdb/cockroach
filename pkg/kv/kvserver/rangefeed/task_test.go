bc2f9742b8cd10c9a4c195634be7877b0b78eb61
Andrei Matei <andrei@cockroachlabs.com>
2020-08-20 11:04:06 -0400

    rangefeed: don't delete txn records from the rngfeed pusher
    Before this patch, the rangefeed txn pushed was GC'ing the records of
    the transactions that it found to be aborted or committed when it
    PUSH_TOUCHED's them. Deleting the txn record is not a nice thing to do
    if the txn's coordinator might still be running, and so this patch
    terminates the practice. If the coordinator is still running and it had
    STAGED the txn record, a PUSH_TOUCH might cause a transition to ABORTED
    or COMMITTED. If such a transition is followed by the GC of the txn
    record, this causes an ambigous commit error for the coordinator, which
    can't tell whether the transaction committed (implicitly) or was rolled
    back.
    
    As the code stands, this patch doesn't actually help eliminate the
    ambiguity in the ABORTED case (it does in the COMMIT case though),
    because the coordinator doesn't distinguish between an ABORTED record
    and a missing record; it treats both the same, as ambiguous. But we're
    going to improve that, and the coordinator is gonna consider an existing
    record as non-ambiguous. The GC'ed case is going to remain the ambiguous
    one, and the idea is to avoid that as much as possible by only doing GC
    from the gc-queue (after an hour) and from the coordinator.
    
    Touches #52566
    
    Release note (bug fix): Some rare AmbiguousCommitErrors happening when
    CDC was used were eliminated.

--- pkg/kv/kvserver/rangefeed/task_test.go
+++ pkg/kv/kvserver/rangefeed/task_test.go
@@ -249,8 +249,8 @@ func TestInitResolvedTSScan(t *testing.T) {
 }
 
 type testTxnPusher struct {
-	pushTxnsFn               func([]enginepb.TxnMeta, hlc.Timestamp) ([]*roachpb.Transaction, error)
-	cleanupTxnIntentsAsyncFn func([]*roachpb.Transaction) error
+	pushTxnsFn       func([]enginepb.TxnMeta, hlc.Timestamp) ([]*roachpb.Transaction, error)
+	resolveIntentsFn func(ctx context.Context, intents []roachpb.LockUpdate) error
 }
 
 func (tp *testTxnPusher) PushTxns(
@@ -259,10 +259,8 @@ func (tp *testTxnPusher) PushTxns(
 	return tp.pushTxnsFn(txns, ts)
 }
 
-func (tp *testTxnPusher) CleanupTxnIntentsAsync(
-	ctx context.Context, txns []*roachpb.Transaction,
-) error {
-	return tp.cleanupTxnIntentsAsyncFn(txns)
+func (tp *testTxnPusher) ResolveIntents(ctx context.Context, intents []roachpb.LockUpdate) error {
+	return tp.resolveIntentsFn(ctx, intents)
 }
 
 func (tp *testTxnPusher) mockPushTxns(
@@ -271,41 +269,79 @@ func (tp *testTxnPusher) mockPushTxns(
 	tp.pushTxnsFn = fn
 }
 
-func (tp *testTxnPusher) mockCleanupTxnIntentsAsync(fn func([]*roachpb.Transaction) error) {
-	tp.cleanupTxnIntentsAsyncFn = fn
+func (tp *testTxnPusher) mockResolveIntentsFn(
+	fn func(context.Context, []roachpb.LockUpdate) error,
+) {
+	tp.resolveIntentsFn = fn
+}
+
+func (tp *testTxnPusher) intentsToTxns(intents []roachpb.LockUpdate) []enginepb.TxnMeta {
+	txns := make([]enginepb.TxnMeta, 0)
+	txnIDs := make(map[uuid.UUID]struct{})
+	for _, intent := range intents {
+		txn := intent.Txn
+		if _, ok := txnIDs[txn.ID]; ok {
+			continue
+		}
+		txns = append(txns, txn)
+		txnIDs[txn.ID] = struct{}{}
+	}
+	return txns
 }
 
 func TestTxnPushAttempt(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 
 	// Create a set of transactions.
-	txn1, txn2, txn3 := uuid.MakeV4(), uuid.MakeV4(), uuid.MakeV4()
-	ts1, ts2, ts3 := hlc.Timestamp{WallTime: 1}, hlc.Timestamp{WallTime: 2}, hlc.Timestamp{WallTime: 3}
+	txn1, txn2, txn3, txn4 := uuid.MakeV4(), uuid.MakeV4(), uuid.MakeV4(), uuid.MakeV4()
+	ts1, ts2, ts3, ts4 := hlc.Timestamp{WallTime: 1}, hlc.Timestamp{WallTime: 2}, hlc.Timestamp{WallTime: 3}, hlc.Timestamp{WallTime: 4}
+	txn2LockSpans := []roachpb.Span{
+		{Key: roachpb.Key("a"), EndKey: roachpb.Key("b")},
+		{Key: roachpb.Key("c"), EndKey: roachpb.Key("d")},
+	}
+	txn4LockSpans := []roachpb.Span{
+		{Key: roachpb.Key("e"), EndKey: roachpb.Key("f")},
+		{Key: roachpb.Key("g"), EndKey: roachpb.Key("h")},
+	}
 	txn1Meta := enginepb.TxnMeta{ID: txn1, Key: keyA, WriteTimestamp: ts1, MinTimestamp: ts1}
 	txn2Meta := enginepb.TxnMeta{ID: txn2, Key: keyB, WriteTimestamp: ts2, MinTimestamp: ts2}
 	txn3Meta := enginepb.TxnMeta{ID: txn3, Key: keyC, WriteTimestamp: ts3, MinTimestamp: ts3}
+	txn4Meta := enginepb.TxnMeta{ID: txn4, Key: keyC, WriteTimestamp: ts3, MinTimestamp: ts4}
 	txn1Proto := &roachpb.Transaction{TxnMeta: txn1Meta, Status: roachpb.PENDING}
-	txn2Proto := &roachpb.Transaction{TxnMeta: txn2Meta, Status: roachpb.COMMITTED}
+	txn2Proto := &roachpb.Transaction{TxnMeta: txn2Meta, Status: roachpb.COMMITTED, LockSpans: txn2LockSpans}
 	txn3Proto := &roachpb.Transaction{TxnMeta: txn3Meta, Status: roachpb.ABORTED}
+	// txn4 has its LockSpans populated, simulated a transaction that has been
+	// rolled back by its coordinator (which populated the LockSpans), but then
+	// not GC'ed for whatever reason.
+	txn4Proto := &roachpb.Transaction{TxnMeta: txn4Meta, Status: roachpb.ABORTED, LockSpans: txn4LockSpans}
 
 	// Run a txnPushAttempt.
 	var tp testTxnPusher
 	tp.mockPushTxns(func(txns []enginepb.TxnMeta, ts hlc.Timestamp) ([]*roachpb.Transaction, error) {
-		require.Equal(t, 3, len(txns))
+		require.Equal(t, 4, len(txns))
 		require.Equal(t, txn1Meta, txns[0])
 		require.Equal(t, txn2Meta, txns[1])
 		require.Equal(t, txn3Meta, txns[2])
+		require.Equal(t, txn4Meta, txns[3])
 		require.Equal(t, hlc.Timestamp{WallTime: 15}, ts)
 
-		// Return all three protos. The PENDING txn is pushed.
+		// Return all four protos. The PENDING txn is pushed.
 		txn1ProtoPushed := txn1Proto.Clone()
 		txn1ProtoPushed.WriteTimestamp = ts
-		return []*roachpb.Transaction{txn1ProtoPushed, txn2Proto, txn3Proto}, nil
+		return []*roachpb.Transaction{txn1ProtoPushed, txn2Proto, txn3Proto, txn4Proto}, nil
 	})
-	tp.mockCleanupTxnIntentsAsync(func(txns []*roachpb.Transaction) error {
+	tp.mockResolveIntentsFn(func(ctx context.Context, intents []roachpb.LockUpdate) error {
+		require.Len(t, intents, 4)
+		require.Equal(t, txn2LockSpans[0], intents[0].Span)
+		require.Equal(t, txn2LockSpans[1], intents[1].Span)
+		require.Equal(t, txn4LockSpans[0], intents[2].Span)
+		require.Equal(t, txn4LockSpans[1], intents[3].Span)
+		txns := tp.intentsToTxns(intents)
 		require.Equal(t, 2, len(txns))
-		require.Equal(t, txn2Proto, txns[0])
-		require.Equal(t, txn3Proto, txns[1])
+		require.Equal(t, txn2Meta, txns[0])
+		// Note that we don't expect intents for txn3 to be resolved since that txn
+		// doesn't have its LockSpans populated.
+		require.Equal(t, txn4Meta, txns[1])
 		return nil
 	})
 
@@ -313,7 +349,7 @@ func TestTxnPushAttempt(t *testing.T) {
 	p := Processor{eventC: make(chan event, 100)}
 	p.TxnPusher = &tp
 
-	txns := []enginepb.TxnMeta{txn1Meta, txn2Meta, txn3Meta}
+	txns := []enginepb.TxnMeta{txn1Meta, txn2Meta, txn3Meta, txn4Meta}
 	doneC := make(chan struct{})
 	pushAttempt := newTxnPushAttempt(&p, txns, hlc.Timestamp{WallTime: 15}, doneC)
 	pushAttempt.Run(context.Background())
@@ -325,6 +361,7 @@ func TestTxnPushAttempt(t *testing.T) {
 			updateIntentOp(txn1, hlc.Timestamp{WallTime: 15}),
 			updateIntentOp(txn2, hlc.Timestamp{WallTime: 2}),
 			abortTxnOp(txn3),
+			abortTxnOp(txn4),
 		}},
 	}
 	require.Equal(t, len(expEvents), len(p.eventC))
