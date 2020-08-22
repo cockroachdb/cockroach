c50ccc998440a888620ff005388bfb623a911ef6
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-08-21 19:34:05 -0700

    colexec: fix LEFT ANTI hash join when right eq cols are key
    Release note (bug fix): Previously, CockroachDB could return incorrect
    results when performing LEFT ANTI hash join when right equality columns
    form a key when it was executed via the vectorized engine, and now this
    has been fixed.

--- pkg/sql/colexec/hashjoiner.go
+++ pkg/sql/colexec/hashjoiner.go
@@ -667,6 +667,11 @@ func MakeHashJoinerSpec(
 		// conditions just yet. When we do, we'll have a separate case for that.
 		rightDistinct = true
 	case descpb.LeftAntiJoin:
+		// LEFT ANTI joins currently rely on the fact that
+		// ht.probeScratch.headID is populated in order to perform the
+		// matching. However, headID is only populated when the right side is
+		// considered to be non-distinct, so we override that information here.
+		rightDistinct = false
 	case descpb.IntersectAllJoin:
 	case descpb.ExceptAllJoin:
 	default:
