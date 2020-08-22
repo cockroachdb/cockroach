295a5d49f625cf3ab60ac0314d8c689a4bb3dc8b
Erik Grinaker <erik@grinaker.org>
2020-08-22 18:14:43 +0200

    builtins: implement ST_Multi and ST_Collection(Extract|Homogenize)
    Release note (sql change): Implement the geometry builtins `ST_Multi`,
    `ST_CollectionExtract`, and `ST_CollectionHomogenize`.

---
+++ pkg/geo/geomfn/collections.go
@@ -0,0 +1,313 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package geomfn
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/geo"
+	"github.com/cockroachdb/cockroach/pkg/geo/geopb"
+	"github.com/cockroachdb/errors"
+	"github.com/twpayne/go-geom"
+)
+
+// CollectionExtract returns a (multi-)geometry consisting only of the specified type.
+// The type can only be point, line, or polygon.
+func CollectionExtract(g *geo.Geometry, shapeType geopb.ShapeType) (*geo.Geometry, error) {
+	switch shapeType {
+	case geopb.ShapeType_Point, geopb.ShapeType_LineString, geopb.ShapeType_Polygon:
+	default:
+		return nil, errors.Newf("only point, linestring and polygon may be extracted (got %s)",
+			shapeType)
+	}
+
+	// If the input is already of the correct (multi-)type, just return it before
+	// decoding the geom.T below.
+	if g.ShapeType() == shapeType || g.ShapeType() == shapeType.MultiType() {
+		return g, nil
+	}
+
+	t, err := g.AsGeomT()
+	if err != nil {
+		return nil, err
+	}
+
+	switch t := t.(type) {
+	// If the input is not a collection then return an empty geometry of the expected type.
+	case *geom.Point, *geom.LineString, *geom.Polygon:
+		switch shapeType {
+		case geopb.ShapeType_Point:
+			return geo.NewGeometryFromGeomT(geom.NewPointEmpty(t.Layout()).SetSRID(t.SRID()))
+		case geopb.ShapeType_LineString:
+			return geo.NewGeometryFromGeomT(geom.NewLineString(t.Layout()).SetSRID(t.SRID()))
+		case geopb.ShapeType_Polygon:
+			return geo.NewGeometryFromGeomT(geom.NewPolygon(t.Layout()).SetSRID(t.SRID()))
+		default:
+			return nil, errors.AssertionFailedf("unexpected shape type %v", shapeType.String())
+		}
+
+	// If the input is a multitype then return an empty multi-geometry of the expected type.
+	case *geom.MultiPoint, *geom.MultiLineString, *geom.MultiPolygon:
+		switch shapeType.MultiType() {
+		case geopb.ShapeType_MultiPoint:
+			return geo.NewGeometryFromGeomT(geom.NewMultiPoint(t.Layout()).SetSRID(t.SRID()))
+		case geopb.ShapeType_MultiLineString:
+			return geo.NewGeometryFromGeomT(geom.NewMultiLineString(t.Layout()).SetSRID(t.SRID()))
+		case geopb.ShapeType_MultiPolygon:
+			return geo.NewGeometryFromGeomT(geom.NewMultiPolygon(t.Layout()).SetSRID(t.SRID()))
+		default:
+			return nil, errors.AssertionFailedf("unexpected shape type %v", shapeType.MultiType().String())
+		}
+
+	// If the input is a collection, recursively gather geometries of the right type.
+	case *geom.GeometryCollection:
+		// Empty geos.GeometryCollection has NoLayout, while PostGIS uses XY. Returned
+		// multi-geometries cannot have NoLayout.
+		layout := t.Layout()
+		if layout == geom.NoLayout && t.Empty() {
+			layout = geom.XY
+		}
+		iter := geo.NewGeomTIterator(t, geo.EmptyBehaviorOmit)
+		srid := t.SRID()
+
+		var (
+			multi geom.T
+			err   error
+		)
+		switch shapeType {
+		case geopb.ShapeType_Point:
+			multi, err = collectionExtractPoints(iter, layout, srid)
+		case geopb.ShapeType_LineString:
+			multi, err = collectionExtractLineStrings(iter, layout, srid)
+		case geopb.ShapeType_Polygon:
+			multi, err = collectionExtractPolygons(iter, layout, srid)
+		default:
+			return nil, errors.AssertionFailedf("unexpected shape type %v", shapeType.String())
+		}
+		if err != nil {
+			return nil, err
+		}
+		return geo.NewGeometryFromGeomT(multi)
+
+	default:
+		return nil, errors.AssertionFailedf("unexpected shape type: %T", t)
+	}
+}
+
+// collectionExtractPoints extracts points from an iterator.
+func collectionExtractPoints(
+	iter geo.GeomTIterator, layout geom.Layout, srid int,
+) (*geom.MultiPoint, error) {
+	points := geom.NewMultiPoint(layout).SetSRID(srid)
+	for {
+		if next, hasNext, err := iter.Next(); err != nil {
+			return nil, err
+		} else if !hasNext {
+			break
+		} else if point, ok := next.(*geom.Point); ok {
+			if err = points.Push(point); err != nil {
+				return nil, err
+			}
+		}
+	}
+	return points, nil
+}
+
+// collectionExtractLineStrings extracts line strings from an iterator.
+func collectionExtractLineStrings(
+	iter geo.GeomTIterator, layout geom.Layout, srid int,
+) (*geom.MultiLineString, error) {
+	lineStrings := geom.NewMultiLineString(layout).SetSRID(srid)
+	for {
+		if next, hasNext, err := iter.Next(); err != nil {
+			return nil, err
+		} else if !hasNext {
+			break
+		} else if lineString, ok := next.(*geom.LineString); ok {
+			if err = lineStrings.Push(lineString); err != nil {
+				return nil, err
+			}
+		}
+	}
+	return lineStrings, nil
+}
+
+// collectionExtractPolygons extracts polygons from an iterator.
+func collectionExtractPolygons(
+	iter geo.GeomTIterator, layout geom.Layout, srid int,
+) (*geom.MultiPolygon, error) {
+	polygons := geom.NewMultiPolygon(layout).SetSRID(srid)
+	for {
+		if next, hasNext, err := iter.Next(); err != nil {
+			return nil, err
+		} else if !hasNext {
+			break
+		} else if polygon, ok := next.(*geom.Polygon); ok {
+			if err = polygons.Push(polygon); err != nil {
+				return nil, err
+			}
+		}
+	}
+	return polygons, nil
+}
+
+// CollectionHomogenize returns the simplest representation of a collection.
+func CollectionHomogenize(g *geo.Geometry) (*geo.Geometry, error) {
+	t, err := g.AsGeomT()
+	if err != nil {
+		return nil, err
+	}
+	srid := t.SRID()
+	t, err = collectionHomogenizeGeomT(t)
+	if err != nil {
+		return nil, err
+	}
+	if srid != 0 {
+		geo.AdjustGeomTSRID(t, geopb.SRID(srid))
+	}
+	return geo.NewGeometryFromGeomT(t)
+}
+
+// collectionHomogenizeGeomT homogenizes a geom.T collection.
+func collectionHomogenizeGeomT(t geom.T) (geom.T, error) {
+	switch t := t.(type) {
+	case *geom.Point, *geom.LineString, *geom.Polygon:
+		return t, nil
+
+	case *geom.MultiPoint:
+		if t.NumPoints() == 1 {
+			return t.Point(0), nil
+		}
+		return t, nil
+
+	case *geom.MultiLineString:
+		if t.NumLineStrings() == 1 {
+			return t.LineString(0), nil
+		}
+		return t, nil
+
+	case *geom.MultiPolygon:
+		if t.NumPolygons() == 1 {
+			return t.Polygon(0), nil
+		}
+		return t, nil
+
+	case *geom.GeometryCollection:
+		layout := t.Layout()
+		if layout == geom.NoLayout && t.Empty() {
+			layout = geom.XY
+		}
+		points := geom.NewMultiPoint(layout)
+		linestrings := geom.NewMultiLineString(layout)
+		polygons := geom.NewMultiPolygon(layout)
+		iter := geo.NewGeomTIterator(t, geo.EmptyBehaviorOmit)
+		for {
+			next, hasNext, err := iter.Next()
+			if err != nil {
+				return nil, err
+			}
+			if !hasNext {
+				break
+			}
+			switch next := next.(type) {
+			case *geom.Point:
+				err = points.Push(next)
+			case *geom.LineString:
+				err = linestrings.Push(next)
+			case *geom.Polygon:
+				err = polygons.Push(next)
+			default:
+				err = errors.AssertionFailedf("encountered unexpected geometry type: %T", next)
+			}
+			if err != nil {
+				return nil, err
+			}
+		}
+		homog := geom.NewGeometryCollection()
+		switch points.NumPoints() {
+		case 0:
+		case 1:
+			if err := homog.Push(points.Point(0)); err != nil {
+				return nil, err
+			}
+		default:
+			if err := homog.Push(points); err != nil {
+				return nil, err
+			}
+		}
+		switch linestrings.NumLineStrings() {
+		case 0:
+		case 1:
+			if err := homog.Push(linestrings.LineString(0)); err != nil {
+				return nil, err
+			}
+		default:
+			if err := homog.Push(linestrings); err != nil {
+				return nil, err
+			}
+		}
+		switch polygons.NumPolygons() {
+		case 0:
+		case 1:
+			if err := homog.Push(polygons.Polygon(0)); err != nil {
+				return nil, err
+			}
+		default:
+			if err := homog.Push(polygons); err != nil {
+				return nil, err
+			}
+		}
+
+		if homog.NumGeoms() == 1 {
+			return homog.Geom(0), nil
+		}
+		return homog, nil
+
+	default:
+		return nil, errors.AssertionFailedf("unknown geometry type: %T", t)
+	}
+}
+
+// Multi converts the given geometry into a new multi-geometry.
+func Multi(g *geo.Geometry) (*geo.Geometry, error) {
+	t, err := g.AsGeomT() // implicitly clones the input
+	if err != nil {
+		return nil, err
+	}
+	switch t := t.(type) {
+	case *geom.MultiPoint, *geom.MultiLineString, *geom.MultiPolygon, *geom.GeometryCollection:
+		return geo.NewGeometryFromGeomT(t)
+	case *geom.Point:
+		multi := geom.NewMultiPoint(t.Layout()).SetSRID(t.SRID())
+		if !t.Empty() {
+			if err = multi.Push(t); err != nil {
+				return nil, err
+			}
+		}
+		return geo.NewGeometryFromGeomT(multi)
+	case *geom.LineString:
+		multi := geom.NewMultiLineString(t.Layout()).SetSRID(t.SRID())
+		if !t.Empty() {
+			if err = multi.Push(t); err != nil {
+				return nil, err
+			}
+		}
+		return geo.NewGeometryFromGeomT(multi)
+	case *geom.Polygon:
+		multi := geom.NewMultiPolygon(t.Layout()).SetSRID(t.SRID())
+		if !t.Empty() {
+			if err = multi.Push(t); err != nil {
+				return nil, err
+			}
+		}
+		return geo.NewGeometryFromGeomT(multi)
+	default:
+		return nil, errors.AssertionFailedf("unknown geometry type: %T", t)
+	}
+}
---
+++ pkg/geo/geomfn/collections_test.go
@@ -0,0 +1,221 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package geomfn
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/geo"
+	"github.com/cockroachdb/cockroach/pkg/geo/geopb"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCollectionExtract(t *testing.T) {
+	mixedWithDupes := `GEOMETRYCOLLECTION(
+		POINT (1 1),
+		MULTIPOINT (2 2, 3 3),
+		LINESTRING (1 1, 2 2),
+		MULTILINESTRING ((3 3, 4 4), (5 5, 6 6)),
+		POLYGON ((1 2, 3 4, 5 6, 1 2)),
+		GEOMETRYCOLLECTION(
+			POINT (3 3),
+			MULTIPOINT (4 4, 5 5),
+			LINESTRING (3 3, 4 4),
+			MULTILINESTRING ((7 7, 8 8), (9 9, 0 0)),
+			MULTIPOLYGON (((1 2, 3 4, 5 6, 1 2)), ((3 4, 4 5, 5 6, 3 4)))
+		)
+	)`
+
+	testCases := []struct {
+		wkt       string
+		shapeType geopb.ShapeType
+		expected  string
+	}{
+		{"POINT EMPTY", geopb.ShapeType_Point, "POINT EMPTY"},
+		{"POINT EMPTY", geopb.ShapeType_LineString, "LINESTRING EMPTY"},
+		{"POINT EMPTY", geopb.ShapeType_Polygon, "POLYGON EMPTY"},
+		{"POINT (1 2)", geopb.ShapeType_Point, "POINT (1 2)"},
+		{"POINT (1 2)", geopb.ShapeType_LineString, "LINESTRING EMPTY"},
+		{"POINT (1 2)", geopb.ShapeType_Polygon, "POLYGON EMPTY"},
+		{"LINESTRING EMPTY", geopb.ShapeType_Point, "POINT EMPTY"},
+		{"LINESTRING EMPTY", geopb.ShapeType_LineString, "LINESTRING EMPTY"},
+		{"LINESTRING EMPTY", geopb.ShapeType_Polygon, "POLYGON EMPTY"},
+		{"LINESTRING (1 2, 3 4)", geopb.ShapeType_Point, "POINT EMPTY"},
+		{"LINESTRING (1 2, 3 4)", geopb.ShapeType_LineString, "LINESTRING (1 2, 3 4)"},
+		{"LINESTRING (1 2, 3 4)", geopb.ShapeType_Polygon, "POLYGON EMPTY"},
+		{"POLYGON EMPTY", geopb.ShapeType_Point, "POINT EMPTY"},
+		{"POLYGON EMPTY", geopb.ShapeType_LineString, "LINESTRING EMPTY"},
+		{"POLYGON EMPTY", geopb.ShapeType_Polygon, "POLYGON EMPTY"},
+		{"POLYGON ((1 2, 3 4, 5 6, 1 2))", geopb.ShapeType_Point, "POINT EMPTY"},
+		{"POLYGON ((1 2, 3 4, 5 6, 1 2))", geopb.ShapeType_LineString, "LINESTRING EMPTY"},
+		{"POLYGON ((1 2, 3 4, 5 6, 1 2))", geopb.ShapeType_Polygon, "POLYGON ((1 2, 3 4, 5 6, 1 2))"},
+		{"MULTIPOINT EMPTY", geopb.ShapeType_Point, "MULTIPOINT EMPTY"},
+		{"MULTIPOINT EMPTY", geopb.ShapeType_LineString, "MULTILINESTRING EMPTY"},
+		{"MULTIPOINT EMPTY", geopb.ShapeType_Polygon, "MULTIPOLYGON EMPTY"},
+		{"MULTIPOINT (1 2, 3 4)", geopb.ShapeType_Point, "MULTIPOINT (1 2, 3 4)"},
+		{"MULTIPOINT (1 2, 3 4)", geopb.ShapeType_LineString, "MULTILINESTRING EMPTY"},
+		{"MULTIPOINT (1 2, 3 4)", geopb.ShapeType_Polygon, "MULTIPOLYGON EMPTY"},
+		{"MULTILINESTRING EMPTY", geopb.ShapeType_Point, "MULTIPOINT EMPTY"},
+		{"MULTILINESTRING EMPTY", geopb.ShapeType_LineString, "MULTILINESTRING EMPTY"},
+		{"MULTILINESTRING EMPTY", geopb.ShapeType_Polygon, "MULTIPOLYGON EMPTY"},
+		{"MULTILINESTRING ((1 2, 3 4), (5 6, 7 8))", geopb.ShapeType_Point, "MULTIPOINT EMPTY"},
+		{"MULTILINESTRING ((1 2, 3 4), (5 6, 7 8))", geopb.ShapeType_LineString, "MULTILINESTRING ((1 2, 3 4), (5 6, 7 8))"},
+		{"MULTILINESTRING ((1 2, 3 4), (5 6, 7 8))", geopb.ShapeType_Polygon, "MULTIPOLYGON EMPTY"},
+		{"MULTIPOLYGON EMPTY", geopb.ShapeType_Point, "MULTIPOINT EMPTY"},
+		{"MULTIPOLYGON EMPTY", geopb.ShapeType_LineString, "MULTILINESTRING EMPTY"},
+		{"MULTIPOLYGON EMPTY", geopb.ShapeType_Polygon, "MULTIPOLYGON EMPTY"},
+		{"MULTIPOLYGON (((0 1, 2 3, 4 5, 0 1)), ((5 6, 7 8, 9 0, 5 6)))", geopb.ShapeType_Point, "MULTIPOINT EMPTY"},
+		{"MULTIPOLYGON (((0 1, 2 3, 4 5, 0 1)), ((5 6, 7 8, 9 0, 5 6)))", geopb.ShapeType_LineString, "MULTILINESTRING EMPTY"},
+		{"MULTIPOLYGON (((0 1, 2 3, 4 5, 0 1)), ((5 6, 7 8, 9 0, 5 6)))", geopb.ShapeType_Polygon, "MULTIPOLYGON (((0 1, 2 3, 4 5, 0 1)), ((5 6, 7 8, 9 0, 5 6)))"},
+		{"GEOMETRYCOLLECTION EMPTY", geopb.ShapeType_Point, "MULTIPOINT EMPTY"},
+		{"GEOMETRYCOLLECTION EMPTY", geopb.ShapeType_LineString, "MULTILINESTRING EMPTY"},
+		{"GEOMETRYCOLLECTION EMPTY", geopb.ShapeType_Polygon, "MULTIPOLYGON EMPTY"},
+		{"GEOMETRYCOLLECTION(GEOMETRYCOLLECTION EMPTY)", geopb.ShapeType_Point, "MULTIPOINT EMPTY"},
+		{"GEOMETRYCOLLECTION(GEOMETRYCOLLECTION EMPTY)", geopb.ShapeType_LineString, "MULTILINESTRING EMPTY"},
+		{"GEOMETRYCOLLECTION(GEOMETRYCOLLECTION EMPTY)", geopb.ShapeType_Polygon, "MULTIPOLYGON EMPTY"},
+		{mixedWithDupes, geopb.ShapeType_Point, "MULTIPOINT (1 1, 2 2, 3 3, 3 3, 4 4, 5 5)"},
+		{mixedWithDupes, geopb.ShapeType_LineString, "MULTILINESTRING ((1 1, 2 2), (3 3, 4 4), (5 5, 6 6), (3 3, 4 4), (7 7, 8 8), (9 9, 0 0))"},
+		{mixedWithDupes, geopb.ShapeType_Polygon, "MULTIPOLYGON (((1 2, 3 4, 5 6, 1 2)), ((1 2, 3 4, 5 6, 1 2)), ((3 4, 4 5, 5 6, 3 4)))"},
+	}
+	errorTestCases := []struct {
+		shapeType geopb.ShapeType
+	}{
+		{geopb.ShapeType_Unset},
+		{geopb.ShapeType_MultiPoint},
+		{geopb.ShapeType_MultiLineString},
+		{geopb.ShapeType_MultiPolygon},
+		{geopb.ShapeType_Geometry},
+		{geopb.ShapeType_GeometryCollection},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.wkt, func(t *testing.T) {
+			srid := geopb.SRID(4000)
+			g, err := geo.ParseGeometryFromEWKT(geopb.EWKT(tc.wkt), srid, true)
+			require.NoError(t, err)
+
+			multi, err := CollectionExtract(g, tc.shapeType)
+			require.NoError(t, err)
+			wkt, err := geo.SpatialObjectToWKT(multi.SpatialObject(), 0)
+			require.NoError(t, err)
+			require.EqualValues(t, tc.expected, wkt)
+			require.EqualValues(t, srid, multi.SRID())
+		})
+	}
+
+	t.Run("errors on wrong shape type", func(t *testing.T) {
+		for _, tc := range errorTestCases {
+			t.Run(tc.shapeType.String(), func(t *testing.T) {
+				g, err := geo.ParseGeometry("POINT EMPTY")
+				require.NoError(t, err)
+				_, err = CollectionExtract(g, tc.shapeType)
+				require.Error(t, err)
+			})
+		}
+	})
+}
+
+func TestCollectionHomogenize(t *testing.T) {
+	testCases := []struct {
+		wkt      string
+		expected string
+	}{
+		{"POINT EMPTY", "POINT EMPTY"},
+		{"POINT (1 2)", "POINT (1 2)"},
+		{"MULTIPOINT EMPTY", "MULTIPOINT EMPTY"},
+		{"MULTIPOINT (1 2)", "POINT (1 2)"},
+		{"MULTIPOINT (1 2, 3 4)", "MULTIPOINT (1 2, 3 4)"},
+		{"LINESTRING EMPTY", "LINESTRING EMPTY"},
+		{"LINESTRING (1 2, 3 4)", "LINESTRING (1 2, 3 4)"},
+		{"MULTILINESTRING EMPTY", "MULTILINESTRING EMPTY"},
+		{"MULTILINESTRING ((1 2, 3 4))", "LINESTRING (1 2, 3 4)"},
+		{"MULTILINESTRING ((1 2, 3 4), (5 6, 7 8))", "MULTILINESTRING ((1 2, 3 4), (5 6, 7 8))"},
+		{"POLYGON EMPTY", "POLYGON EMPTY"},
+		{"POLYGON ((1 2, 3 4, 5 6, 1 2))", "POLYGON ((1 2, 3 4, 5 6, 1 2))"},
+		{"MULTIPOLYGON EMPTY", "MULTIPOLYGON EMPTY"},
+		{"MULTIPOLYGON (((1 2, 3 4, 5 6, 1 2)))", "POLYGON ((1 2, 3 4, 5 6, 1 2))"},
+		{"MULTIPOLYGON (((1 2, 3 4, 5 6, 1 2)), ((7 8, 9 0, 1 2, 7 8)))", "MULTIPOLYGON (((1 2, 3 4, 5 6, 1 2)), ((7 8, 9 0, 1 2, 7 8)))"},
+		{"GEOMETRYCOLLECTION EMPTY", "GEOMETRYCOLLECTION EMPTY"},
+		{"GEOMETRYCOLLECTION (GEOMETRYCOLLECTION EMPTY)", "GEOMETRYCOLLECTION EMPTY"},
+		{"GEOMETRYCOLLECTION (GEOMETRYCOLLECTION(MULTIPOINT (1 1)))", "POINT (1 1)"},
+		{
+			`GEOMETRYCOLLECTION (
+				LINESTRING(1 1, 2 2),
+				POINT(1 1),
+				GEOMETRYCOLLECTION(
+					MULTIPOINT(2 2, 3 3),
+					GEOMETRYCOLLECTION EMPTY,
+					POINT(1 1),
+					MULTIPOLYGON(((1 2, 2 3, 3 4, 1 2))),
+					LINESTRING(3 3, 4 4),
+					GEOMETRYCOLLECTION(
+						POINT(4 4),
+						POINT(5 5)
+					)
+				),
+				MULTIPOINT EMPTY
+			)`,
+			"GEOMETRYCOLLECTION (MULTIPOINT (1 1, 2 2, 3 3, 1 1, 4 4, 5 5), MULTILINESTRING ((1 1, 2 2), (3 3, 4 4)), POLYGON ((1 2, 2 3, 3 4, 1 2)))",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.wkt, func(t *testing.T) {
+			srid := geopb.SRID(4000)
+			g, err := geo.ParseGeometryFromEWKT(geopb.EWKT(tc.wkt), srid, true)
+			require.NoError(t, err)
+
+			result, err := CollectionHomogenize(g)
+			require.NoError(t, err)
+			wkt, err := geo.SpatialObjectToWKT(result.SpatialObject(), 0)
+			require.NoError(t, err)
+			require.EqualValues(t, tc.expected, wkt)
+			require.EqualValues(t, srid, result.SRID())
+		})
+	}
+}
+
+func TestMulti(t *testing.T) {
+	testCases := []struct {
+		wkt      string
+		expected string
+	}{
+		{"POINT EMPTY", "MULTIPOINT EMPTY"},
+		{"POINT (1 2)", "MULTIPOINT (1 2)"},
+		{"MULTIPOINT EMPTY", "MULTIPOINT EMPTY"},
+		{"MULTIPOINT (1 2, 3 4)", "MULTIPOINT (1 2, 3 4)"},
+		{"LINESTRING EMPTY", "MULTILINESTRING EMPTY"},
+		{"LINESTRING (1 2, 3 4, 5 6)", "MULTILINESTRING ((1 2, 3 4, 5 6))"},
+		{"MULTILINESTRING EMPTY", "MULTILINESTRING EMPTY"},
+		{"MULTILINESTRING ((1 2, 3 4), (5 6, 7 8))", "MULTILINESTRING ((1 2, 3 4), (5 6, 7 8))"},
+		{"POLYGON EMPTY", "MULTIPOLYGON EMPTY"},
+		{"POLYGON ((1 2, 3 4, 5 6, 1 2))", "MULTIPOLYGON (((1 2, 3 4, 5 6, 1 2)))"},
+		{"MULTIPOLYGON (((1 2, 3 4, 5 6, 1 2)), ((0 0, 1 1, 2 2, 0 0)))", "MULTIPOLYGON (((1 2, 3 4, 5 6, 1 2)), ((0 0, 1 1, 2 2, 0 0)))"},
+		{"GEOMETRYCOLLECTION EMPTY", "GEOMETRYCOLLECTION EMPTY"},
+		{"GEOMETRYCOLLECTION (POINT (1 2), LINESTRING (1 2, 3 4))", "GEOMETRYCOLLECTION (POINT (1 2), LINESTRING (1 2, 3 4))"},
+		{"GEOMETRYCOLLECTION (GEOMETRYCOLLECTION (POINT (1 2), LINESTRING (1 2, 3 4)))", "GEOMETRYCOLLECTION (GEOMETRYCOLLECTION (POINT (1 2), LINESTRING (1 2, 3 4)))"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.wkt, func(t *testing.T) {
+			srid := geopb.SRID(4000)
+			g, err := geo.ParseGeometryFromEWKT(geopb.EWKT(tc.wkt), srid, true)
+			require.NoError(t, err)
+
+			multi, err := Multi(g)
+			require.NoError(t, err)
+			wkt, err := geo.SpatialObjectToWKT(multi.SpatialObject(), 0)
+			require.NoError(t, err)
+			require.EqualValues(t, tc.expected, wkt)
+			require.EqualValues(t, srid, multi.SRID())
+		})
+	}
+}
