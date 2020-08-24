ade8c6bb4beb1f643f9830b81f12cd65d5063f81
Rebecca Taft <becca@cockroachlabs.com>
2020-08-24 11:37:49 -0500

    opt: add rules to convert semi-join to inner-join
    This commit adds two new rules to convert semi joins to inner
    joins: ConvertSemiToInnerJoin and ConvertSemiToInnerJoinAddKey.
    These rules are similar to CommuteSemiJoin, but apply in the cases
    where CommuteSemiJoin does not: when the ON condition is not a
    simple equality. In this case, we must perform a DisinctOn operation
    *after* the inner join, followed by a project to remove the
    right-side columns.
    
    Similar to CommuteSemiJoin, these rules allow semi joins to be
    commuted. These rules are also useful because they allow us to
    generate lookup joins and inverted lookup joins for cases where the
    index is not covering.
    
    ConvertSemiToInnerJoin applies when the left side already has a key,
    and ConvertSemiToInnerJoinAddKey applies when we need to add a key to
    the left side.
    
    Informs #48223
    
    Release note (performance improvement): The optimizer can now
    convert semi joins to inner joins in more cases, which may allow
    the optimizer to produce more efficient query plans.

--- pkg/sql/opt/xform/custom_funcs.go
+++ pkg/sql/opt/xform/custom_funcs.go
@@ -1945,8 +1945,9 @@ func (c *CustomFuncs) GenerateInvertedJoins(
 			// covering" here means that not all columns in the ON condition are
 			// available.
 			//
-			// TODO(rytaft): We could create a semi/anti join on top of an inner/left
-			//  join if we wrap it with a DISTINCT ON the key of the left side.
+			// For semi joins, we may still be able to generate an inverted join
+			// by converting it to an inner join using the ConvertSemiToInnerJoin
+			// rule. But in that case, we would already have an inner join here.
 			continue
 		}
 
@@ -2623,11 +2624,11 @@ func (c *CustomFuncs) ConvertIndexToLookupJoinPrivate(
 	}
 
 	return &memo.LookupJoinPrivate{
-		JoinType:              opt.InnerJoinOp,
-		Table:                 indexPrivate.Table,
-		Index:                 cat.PrimaryIndex,
-		KeyCols:               lookupCols,
-		Cols:                  outCols,
+		JoinType: opt.InnerJoinOp,
+		Table:    indexPrivate.Table,
+		Index:    cat.PrimaryIndex,
+		KeyCols:  lookupCols,
+		Cols:     outCols,
 		LookupColsAreTableKey: true,
 		ConstFilters:          nil,
 		JoinPrivate:           memo.JoinPrivate{},
