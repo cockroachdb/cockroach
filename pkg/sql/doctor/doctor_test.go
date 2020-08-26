2e51ce43bd6a3a7b71ff80086d3df0b12f85a03f
Spas Bojanov <spas@cockroachlabs.com>
2020-08-26 11:18:50 +0300

    cli: add namespace consistency check to doctor tool
    So far the doctor tool would run `Validate` on all table descriptors
    and report any validation errors. In this PR we ingest the namespace
    table as well (from `sysstem.namespace2.txt or from a live cluster).
    The namespace entries are then validated against the descriptors:
    - each descriptor that is not dropped should be pointed to by at least
    one namespace row.
    - the name of the table should be in found in a namespace row that points
    to the table descriptor.
    - no NULL values should be found in the namespace table.
    - every namespace row should point to an existing descriptor.
    
    Touches #51153.
    
    Release note: none.
    
    Release justification: this is a debug tool that examines a debug zip or
    a cluster for problems. There are no changes to the DB itself.

--- pkg/sql/doctor/doctor_test.go
+++ pkg/sql/doctor/doctor_test.go
@@ -12,11 +12,16 @@ package doctor_test
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/security"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/tabledesc"
 	"github.com/cockroachdb/cockroach/pkg/sql/doctor"
+	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
@@ -27,48 +32,187 @@ func TestExamine(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 
-	toBytes := func(tableDesc *descpb.TableDescriptor) []byte {
-		desc := descpb.Descriptor{
-			Union: &descpb.Descriptor_Table{
-				Table: tableDesc,
-			},
-		}
-		res, err := protoutil.Marshal(&desc)
+	toBytes := func(desc *descpb.Descriptor) []byte {
+		res, err := protoutil.Marshal(desc)
 		require.NoError(t, err)
 		return res
 	}
 
 	tests := []struct {
-		descTable []doctor.DescriptorTableRow
-		valid     bool
-		errStr    string
-		expected  string
+		descTable      doctor.DescriptorTable
+		namespaceTable doctor.NamespaceTable
+		valid          bool
+		errStr         string
+		expected       string
 	}{
 		{
 			valid:    true,
-			expected: "Examining 0 descriptors...\n",
+			expected: "Examining 0 descriptors and 0 namespace entries...\n",
 		},
 		{
-			descTable: []doctor.DescriptorTableRow{{ID: 1, DescBytes: []byte("#$@#@#$#@#")}},
+			descTable: doctor.DescriptorTable{{ID: 1, DescBytes: []byte("#$@#@#$#@#")}},
 			errStr:    "failed to unmarshal descriptor",
-			expected:  "Examining 1 descriptors...\n",
+			expected:  "Examining 1 descriptors and 0 namespace entries...\n",
 		},
 		{
-			descTable: []doctor.DescriptorTableRow{{ID: 1, DescBytes: toBytes(&descpb.TableDescriptor{ID: 2})}},
-			errStr:    "",
-			expected:  "Examining 1 descriptors...\nTable   1: different id in the descriptor: 2",
-		},
-		{
-			descTable: []doctor.DescriptorTableRow{
-				{ID: 1, DescBytes: toBytes(&descpb.TableDescriptor{Name: "foo", ID: 1})},
+			descTable: doctor.DescriptorTable{
+				{
+					ID: 1,
+					DescBytes: toBytes(&descpb.Descriptor{Union: &descpb.Descriptor_Table{
+						Table: &descpb.TableDescriptor{ID: 2},
+					}}),
+				},
 			},
-			expected: "Examining 1 descriptors...\nTable   1: invalid parent ID 0\n",
+			expected: `Examining 1 descriptors and 0 namespace entries...
+   Table   2: ParentID   0, ParentSchemaID 29, Name '': different id in descriptor table: 1
+`,
+		},
+		{
+			descTable: doctor.DescriptorTable{
+				{
+					ID: 1,
+					DescBytes: toBytes(&descpb.Descriptor{Union: &descpb.Descriptor_Table{
+						Table: &descpb.TableDescriptor{Name: "foo", ID: 1, State: descpb.DescriptorState_DROP},
+					}}),
+				},
+			},
+			expected: `Examining 1 descriptors and 0 namespace entries...
+   Table   1: ParentID   0, ParentSchemaID 29, Name 'foo': invalid parent ID 0
+`,
+		},
+		{
+			descTable: doctor.DescriptorTable{
+				{
+					ID: 1,
+					DescBytes: toBytes(&descpb.Descriptor{Union: &descpb.Descriptor_Table{
+						Table: &descpb.TableDescriptor{Name: "foo", ID: 1},
+					}}),
+				},
+			},
+			namespaceTable: doctor.NamespaceTable{
+				{NameInfo: descpb.NameInfo{ParentSchemaID: 29, Name: "foo"}, ID: 1},
+			},
+			expected: `Examining 1 descriptors and 1 namespace entries...
+   Table   1: ParentID   0, ParentSchemaID 29, Name 'foo': invalid parent ID 0
+`,
+		},
+		{
+			descTable: doctor.DescriptorTable{
+				{
+					ID: 1,
+					DescBytes: toBytes(&descpb.Descriptor{Union: &descpb.Descriptor_Database{
+						Database: &descpb.DatabaseDescriptor{Name: "db", ID: 1},
+					}}),
+				},
+			},
+			expected: `Examining 1 descriptors and 0 namespace entries...
+Database   1: ParentID   0, ParentSchemaID  0, Name 'db': not being dropped but no namespace entry found
+`,
+		},
+		{
+			descTable: doctor.DescriptorTable{
+				{
+					ID: 1,
+					DescBytes: toBytes(&descpb.Descriptor{Union: &descpb.Descriptor_Table{
+						Table: &descpb.TableDescriptor{
+							Name: "t", ID: 1, ParentID: 2,
+							Columns: []descpb.ColumnDescriptor{
+								{Name: "col", ID: 1, Type: types.Int},
+							},
+							NextColumnID: 2,
+							Families: []descpb.ColumnFamilyDescriptor{
+								{ID: 0, Name: "f", ColumnNames: []string{"col"}, ColumnIDs: []descpb.ColumnID{1}, DefaultColumnID: 1},
+							},
+							NextFamilyID: 1,
+							PrimaryIndex: descpb.IndexDescriptor{
+								Name:             tabledesc.PrimaryKeyIndexName,
+								ID:               1,
+								Unique:           true,
+								ColumnNames:      []string{"col"},
+								ColumnDirections: []descpb.IndexDescriptor_Direction{descpb.IndexDescriptor_ASC},
+								ColumnIDs:        []descpb.ColumnID{1},
+								Version:          descpb.SecondaryIndexFamilyFormatVersion,
+							},
+							NextIndexID: 2,
+							Privileges: descpb.NewCustomSuperuserPrivilegeDescriptor(
+								descpb.SystemAllowedPrivileges[keys.SqllivenessID], security.NodeUser),
+							FormatVersion:  descpb.InterleavedFormatVersion,
+							NextMutationID: 1,
+						},
+					}}),
+				},
+				{
+					ID: 2,
+					DescBytes: toBytes(&descpb.Descriptor{Union: &descpb.Descriptor_Database{
+						Database: &descpb.DatabaseDescriptor{Name: "db", ID: 2},
+					}}),
+				},
+			},
+			namespaceTable: doctor.NamespaceTable{
+				{NameInfo: descpb.NameInfo{ParentSchemaID: 29, Name: "t"}, ID: 1},
+				{NameInfo: descpb.NameInfo{Name: "db"}, ID: 2},
+			},
+			expected: `Examining 2 descriptors and 2 namespace entries...
+   Table   1: ParentID   2, ParentSchemaID 29, Name 't': could not find name in namespace table
+`,
+		},
+		{
+			descTable: doctor.DescriptorTable{
+				{
+					ID: 1,
+					DescBytes: toBytes(&descpb.Descriptor{Union: &descpb.Descriptor_Schema{
+						Schema: &descpb.SchemaDescriptor{Name: "schema", ID: 1, ParentID: 2},
+					}}),
+				},
+			},
+			namespaceTable: doctor.NamespaceTable{
+				{NameInfo: descpb.NameInfo{ParentID: 2, Name: "schema"}, ID: 1},
+			},
+			expected: `Examining 1 descriptors and 1 namespace entries...
+  Schema   1: ParentID   2, ParentSchemaID  0, Name 'schema': invalid parent id 2
+`,
+		},
+		{
+			descTable: doctor.DescriptorTable{
+				{
+					ID: 1,
+					DescBytes: toBytes(&descpb.Descriptor{Union: &descpb.Descriptor_Type{
+						Type: &descpb.TypeDescriptor{Name: "type", ID: 1},
+					}}),
+				},
+			},
+			namespaceTable: doctor.NamespaceTable{
+				{NameInfo: descpb.NameInfo{Name: "type"}, ID: 1},
+			},
+			expected: `Examining 1 descriptors and 1 namespace entries...
+    Type   1: ParentID   0, ParentSchemaID  0, Name 'type': invalid parentID 0
+`,
+		},
+		{
+			namespaceTable: doctor.NamespaceTable{
+				{NameInfo: descpb.NameInfo{Name: "foo"}, ID: keys.PublicSchemaID},
+				{NameInfo: descpb.NameInfo{Name: "bar"}, ID: keys.PublicSchemaID},
+				{NameInfo: descpb.NameInfo{Name: "pg_temp_foo"}, ID: 1},
+				{NameInfo: descpb.NameInfo{Name: "causes_error"}, ID: 2},
+			},
+			expected: `Examining 0 descriptors and 4 namespace entries...
+Descriptor 2: has namespace row(s) [{ParentID:0 ParentSchemaID:0 Name:causes_error}] but no descriptor
+`,
+		},
+		{
+			namespaceTable: doctor.NamespaceTable{
+				{NameInfo: descpb.NameInfo{Name: "null"}, ID: int64(descpb.InvalidID)},
+			},
+			expected: `Examining 0 descriptors and 1 namespace entries...
+Row(s) [{ParentID:0 ParentSchemaID:0 Name:null}]: NULL value found
+`,
 		},
 	}
 
 	for i, test := range tests {
 		var buf bytes.Buffer
-		valid, err := doctor.Examine(test.descTable, false, &buf)
+		valid, err := doctor.Examine(
+			context.Background(), test.descTable, test.namespaceTable, false, &buf)
 		msg := fmt.Sprintf("Test %d failed!", i+1)
 		if test.errStr != "" {
 			require.Containsf(t, err.Error(), test.errStr, msg)
