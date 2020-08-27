d0b83eda7b31653ccd978d9563a425f83f49a4e7
Rafi Shamim <rafi@cockroachlabs.com>
2020-08-27 10:14:50 -0400

    roachtest: add flaky test for activerecord
    Release justification: test-only change
    Release note: None

--- pkg/cmd/roachtest/activerecord_blocklist.go
+++ pkg/cmd/roachtest/activerecord_blocklist.go
@@ -30,7 +30,9 @@ var activeRecordBlockList20_2 = blocklist{}
 
 var activeRecordBlockList20_1 = blocklist{}
 
-var activeRecordIgnoreList20_2 = blocklist{}
+var activeRecordIgnoreList20_2 = blocklist{
+	"FixturesTest#test_create_fixtures": "flaky - FK constraint violated sometimes when loading all fixture data",
+}
 
 var activeRecordIgnoreList20_1 = blocklist{
 	"FixturesTest#test_create_fixtures": "flaky - FK constraint violated sometimes when loading all fixture data",
