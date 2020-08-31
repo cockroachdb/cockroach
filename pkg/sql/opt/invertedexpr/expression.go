0c02c57338637237ad8ae7b1416177175471bd9c
sumeerbhola <sumeer@cockroachlabs.com>
2020-08-31 19:12:37 -0400

    geoindex,invertedidx,rowexec,encoding,...: add bounding box to inverted column
    The bounding box is encoded after the cellid, and used to perform
    "pre-filtering", i.e., filtering after the inverted row is fetched
    but before it is added to various set expressions.
    
    The fundamental weakness with the cell covering
    approach is that the coverings for convex shapes are typically
    worse than a bounding box and we pay significant cost in doing
    a lookup join to eliminate the false positives. This attempts
    to reduce those false positives for the lookup join, and part
    of the inverted join (the invertedJoiner can avoid adding a
    row to the RowContainer).
    
    For the canonical nyc st_intersects and st_dwithin queries
    the joinReader would see a much higher input row count than
    the output due to false positives. Specifically,
    - st_intersects: input 3110, output 490
    - st_dwithin: input 23183, output 8606
    With this change the input counts are 1064 and 11177. There
    is the additional cost of reading extra bytes and applying
    the bounding box filter, but it is more than offset by the
    gains (see comparison numbers below).
    
    This is a format change for the index, so is easier to do
    before the launch of spatial features.
    
    The encoding here is trivial, and consumes 17 bytes for points
    and 33 bytes for others. An experiment with a more complicated
    encoding that converted to integers while bounding the
    relative error did not show any improvement wrt the following
    benchmarks.
    
    There are TODOs for followup PRs:
    - Do such pre-filtering for invertedFilterer. This will need
      a spec change.
    - Support pre-filtering for expressions containing more than
      one function.
    
    Comparison numbers:
    
    old is master
    name                                   old ms     new ms     delta
    Test/nyc/nyc-intersects                80.0 ±23%  69.0 ±30%  -13.76%  (p=0.000 n=49+48)
    Test/nyc/nyc-dwithin                    225 ±13%   169 ± 8%  -24.88%  (p=0.000 n=46+45)
    Test/nyc/nyc-coveredby                 84.1 ±11%  69.0 ±14%  -17.96%  (p=0.000 n=50+49)
    Test/nyc/nyc-expand-blocks             85.7 ± 6%  84.6 ± 8%   -1.22%  (p=0.030 n=45+47)
    Test/nyc/nyc-dwithin-and-dfullywithin   195 ± 7%   195 ± 6%     ~     (p=1.000 n=46+49)
    Test/postgis_geometry_tutorial/11.2b   1.66 ±24%  1.53 ±45%   -7.87%  (p=0.006 n=50+50)
    Test/postgis_geometry_tutorial/11.6    1.61 ±19%  1.66 ±32%     ~     (p=0.329 n=46+50)
    Test/postgis_geometry_tutorial/12.1.2  0.89 ±26%  0.91 ±26%     ~     (p=0.532 n=46+50)
    Test/postgis_geometry_tutorial/12.2.3  1.33 ±20%  1.38 ±36%     ~     (p=0.308 n=49+49)
    Test/postgis_geometry_tutorial/12.2.4  1.30 ±27%  1.29 ±18%     ~     (p=0.746 n=49+45)
    Test/postgis_geometry_tutorial/13.0    1.49 ±36%  1.55 ±33%     ~     (p=0.141 n=49+50)
    Test/postgis_geometry_tutorial/13.1a    233 ± 7%   197 ± 7%  -15.45%  (p=0.000 n=48+47)
    Test/postgis_geometry_tutorial/13.1c   24.9 ±15%  19.4 ±18%  -22.17%  (p=0.000 n=50+48)
    Test/postgis_geometry_tutorial/13.2     309 ± 4%   257 ± 7%  -16.92%  (p=0.000 n=43+47)
    Test/postgis_geometry_tutorial/14.1a   1.51 ±15%  1.70 ±16%  +12.93%  (p=0.000 n=48+44)
    Test/postgis_geometry_tutorial/14.2b   6.28 ±22%  6.07 ±19%     ~     (p=0.050 n=50+45)
    Test/postgis_geometry_tutorial/14.2c   3.70 ±14%  3.47 ±10%   -5.98%  (p=0.000 n=48+46)
    Test/postgis_geometry_tutorial/14.3c   31.9 ±11%  25.9 ±12%  -18.60%  (p=0.000 n=48+49)
    Test/postgis_geometry_tutorial/15.0    1.50 ±20%  1.50 ±20%     ~     (p=0.860 n=48+49)
    
    Release justification: This is an incompatible format change,
    and discussions with engineering and product indicated a
    preference to do this now before we launch spatial features
    
    Release note: None

--- pkg/sql/opt/invertedexpr/expression.go
+++ pkg/sql/opt/invertedexpr/expression.go
@@ -27,9 +27,30 @@ import (
 // SpanExpressionProto.SpansToRead to determine which spans to read from the
 // inverted index. Then it computes a set expression on the scanned rows as
 // defined by the SpanExpressionProto.Node.
+// For a subset of expressions, it can additionally perform pre-filtering,
+// which is filtering a row before evaluating the set expression (but after
+// the row is retrieved). This pre-filtering looks at additional state encoded
+// in the inverted column.
 type DatumsToInvertedExpr interface {
-	// Convert uses the lookup column to construct an inverted expression.
-	Convert(context.Context, rowenc.EncDatumRow) (*SpanExpressionProto, error)
+	// CanPreFilter returns true iff this DatumsToInvertedExpr can pre-filter.
+	CanPreFilter() bool
+
+	// Convert uses the lookup column to construct an inverted expression. When
+	// CanPreFilter() is true, and the returned *SpanExpressionProto is non-nil,
+	// the interface{} returned represents an opaque pre-filtering state for
+	// this lookup column.
+	Convert(context.Context, rowenc.EncDatumRow) (*SpanExpressionProto, interface{}, error)
+
+	// PreFilter is used for pre-filtering a looked up row whose inverted column is
+	// represented as enc. The caller has determined the candidate
+	// expressions for whom this row is relevant, and preFilters contains the
+	// pre-filtering state for those expressions. The result slice must be the
+	// same length as preFilters and will contain true when the row is relevant
+	// and false otherwise. It returns true iff there is at least one result
+	// index that has a true value. PreFilter must only be called when CanPreFilter()
+	// is true. This batching of pre-filter application for a looked up row allows
+	// enc to be decoded once.
+	PreFilter(enc EncInvertedVal, preFilters []interface{}, result []bool) (bool, error)
 }
 
 // EncInvertedVal is the encoded form of a value in the inverted column.
