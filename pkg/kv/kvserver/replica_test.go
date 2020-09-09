c73ef3c4eefdd203a9053c6b0b014dcb6968fdd2
Andrei Matei <andrei@cockroachlabs.com>
2020-09-09 18:51:57 -0400

    kvserver: plumb a ctx
    ... to a method manipulating range descs. I've needed it when debugging.
    
    Release note: None

--- pkg/kv/kvserver/replica_test.go
+++ pkg/kv/kvserver/replica_test.go
@@ -403,7 +403,7 @@ func (tc *testContext) addBogusReplicaToRangeDesc(
 		Header: roachpb.Header{Timestamp: tc.Clock().Now()},
 	}
 	descKey := keys.RangeDescriptorKey(oldDesc.StartKey)
-	if err := updateRangeDescriptor(&ba, descKey, dbDescKV.Value.TagAndDataBytes(), &newDesc); err != nil {
+	if err := updateRangeDescriptor(ctx, &ba, descKey, dbDescKV.Value.TagAndDataBytes(), &newDesc); err != nil {
 		return roachpb.ReplicaDescriptor{}, err
 	}
 	if err := tc.store.DB().Run(ctx, &ba); err != nil {
