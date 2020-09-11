e3a7b1172b4ba3e03f8693bf5534717440a90aac
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2020-09-10 21:09:03 -0400

    roachtest: reduce throughput requirements for kv/contention/nodes=4
    With the previous fix, the test now passes about 95% of the time. The
    remaining 5% are close to passing but dip below the 100 qps threshold a
    little too often. Having seen two of these, I would still consider them
    passing the goal of the test – there were no stalls and all distributed
    deadlocks were resolved without latencies ever blowing way up. Since the
    intention of the test is to catch these kinds of stalls and not to make
    any real throughput guarantees, I feel ok making the requirements a
    little easier so that this has a lower chance of flaking.

--- pkg/cmd/roachtest/kv.go
+++ pkg/cmd/roachtest/kv.go
@@ -249,9 +249,9 @@ func registerKVContention(r *testRegistry) {
 
 				// Assert that the average throughput stayed above a certain
 				// threshold. In this case, assert that max throughput only
-				// dipped below 100 qps for 5% of the time.
-				const minQPS = 100
-				verifyTxnPerSecond(ctx, c, t, c.Node(1), start, end, minQPS, 0.05)
+				// dipped below 50 qps for 10% of the time.
+				const minQPS = 50
+				verifyTxnPerSecond(ctx, c, t, c.Node(1), start, end, minQPS, 0.1)
 				return nil
 			})
 			m.Wait()
