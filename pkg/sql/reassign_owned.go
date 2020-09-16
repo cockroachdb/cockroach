7cb47c17a00757f6aecc62bdbb763acdb3b37fea
angelapwen <angelaw@cockroachlabs.com>
2020-09-16 10:33:37 -0700

    sql: allow parsing of REASSIGN OWNED BY ... TO ...
    Add parsing for REASSIGN OWNED BY ... TO ... command. Plan to follow up with
    implementation of the command for feature parity with PostgresQL.
    
    Release note: None

---
+++ pkg/sql/reassign_owned.go
@@ -0,0 +1,27 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package sql
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/util/errorutil/unimplemented"
+)
+
+// ReassignOwnedNode represents a REASSIGN OWNED BY <name> TO <name> statement.
+type reassignOwnedNode struct {
+	// TODO(angelaw): to implement
+}
+
+func (p *planner) ReassignOwned(ctx context.Context, n *tree.ReassignOwned) (planNode, error) {
+	return nil, unimplemented.NewWithIssue(52022, "reassign owned by is not yet implemented")
+}
