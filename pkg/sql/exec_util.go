95cae0ad7dfb804a7e7bc115b73e2803bf1fdfa7
Radu Berinde <radu@cockroachlabs.com>
2020-09-16 16:52:09 -0400

    sql: disallow cross-database views
    This change disallows views from referring to tables in other
    databases (except `system`) and adds a cluster setting to allow them
    (false by default).
    
    Informs #54126.
    
    Release note (sql change): creating views that refer to tables in
    other databases is now disallowed (and can be re-enabled via a cluster
    setting).

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -129,6 +129,14 @@ var allowCrossDatabaseFKs = settings.RegisterPublicBoolSetting(
 	false,
 )
 
+const allowCrossDatabaseViewsSetting = "sql.cross_db_views.enabled"
+
+var allowCrossDatabaseViews = settings.RegisterPublicBoolSetting(
+	allowCrossDatabaseViewsSetting,
+	"if true, creating views that refer to other databases is allowed",
+	false,
+)
+
 // traceTxnThreshold can be used to log SQL transactions that take
 // longer than duration to complete. For example, traceTxnThreshold=1s
 // will log the trace for any transaction that takes 1s or longer. To
