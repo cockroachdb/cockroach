2e241e8782f25b7577c392b282bed8148c994af8
Oliver Tan <otan@cockroachlabs.com>
2020-09-16 21:40:04 -0700

    sql: add information_schema.type_privileges
    This is a custom table as the PostgreSQL data_type_privileges table
    contains different information and has different semantics. This is
    needed for SHOW GRANTS delegate to work as appropriate.
    
    Release note (sql change): Add a custom
    information_schema.type_privileges table that displays type privileges
    for each supported type.

--- pkg/sql/catalog/catconstants/constants.go
+++ pkg/sql/catalog/catconstants/constants.go
@@ -102,6 +102,7 @@ const (
 	InformationSchemaTableConstraintTableID
 	InformationSchemaTablePrivilegesID
 	InformationSchemaTablesTableID
+	InformationSchemaTypePrivilegesID
 	InformationSchemaViewsTableID
 	InformationSchemaUserPrivilegesID
 	PgCatalogID
