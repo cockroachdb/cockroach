7d59fe7b52ec4a273604f9c4a7be7967dfca2304
Rebecca Taft <becca@cockroachlabs.com>
2020-09-23 16:43:37 -0500

    workload: add back injection of stats for TPC-C tables at start of workload
    This commit adds back the injection of statistics for some tables at the
    start of the TPC-C workload that had been removed by mistake in #35349.
    The removal caused a regression in TPC-C since it caused some plans to change
    due to the lack of statistics at the beginning of the benchmark. Adding the
    stats back should fix the regression.
    
    Fixes #54702
    
    Release note: None

--- pkg/workload/tpcc/tpcc.go
+++ pkg/workload/tpcc/tpcc.go
@@ -428,6 +428,7 @@ func (w *tpcc) Tables() []workload.Table {
 				return []interface{}{(i + 1) * numWarehousesPerRange}
 			},
 		)),
+		Stats: w.tpccWarehouseStats(),
 	}
 	district := workload.Table{
 		Name: `district`,
@@ -446,6 +447,7 @@ func (w *tpcc) Tables() []workload.Table {
 				return []interface{}{(i + 1) * numWarehousesPerRange, 0}
 			},
 		)),
+		Stats: w.tpccDistrictStats(),
 	}
 	customer := workload.Table{
 		Name: `customer`,
@@ -477,6 +479,7 @@ func (w *tpcc) Tables() []workload.Table {
 				return []interface{}{(i + 1) * numWarehousesPerRange}
 			},
 		)),
+		Stats: w.tpccHistoryStats(),
 	}
 	order := workload.Table{
 		Name: `order`,
@@ -489,6 +492,7 @@ func (w *tpcc) Tables() []workload.Table {
 			NumBatches: numOrdersPerWarehouse * w.warehouses,
 			FillBatch:  w.tpccOrderInitialRowBatch,
 		},
+		Stats: w.tpccOrderStats(),
 	}
 	newOrder := workload.Table{
 		Name:   `new_order`,
