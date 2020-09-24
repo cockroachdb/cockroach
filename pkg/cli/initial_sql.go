13085c14f9cb5a1bbb141dbcdd270fd776616c93
Raphael 'kena' Poss <knz@thaumogen.net>
2020-09-24 15:55:40 +0200

    cli/demo: provision an initial non-`root` "demo" user
    Release note (cli change): `cockroach demo` now pre-creates a `demo`
    user account with a random password, instead of letting (and
    encouraging) the user to use the `root` account directly.
    The `demo` account is currently granted the `admin` role.

--- pkg/cli/initial_sql.go
+++ pkg/cli/initial_sql.go
@@ -14,6 +14,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/cockroachdb/cockroach/pkg/security"
 	"github.com/cockroachdb/cockroach/pkg/server"
 	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
@@ -23,22 +24,62 @@ import (
 // runInitialSQL concerns itself with running "initial SQL" code when
 // a cluster is started for the first time.
 //
-// The "startSingleNode" argument is true for `start-single-node`
-// and `cockroach demo` with 2 nodes or less.
-func runInitialSQL(ctx context.Context, s *server.Server, startSingleNode bool) error {
-	if startSingleNode && s.InitialStart() {
+// The "startSingleNode" argument is true for `start-single-node`,
+// and `cockroach demo` with 2 nodes or fewer.
+// If adminUser is non-empty, an admin user with that name is
+// created upon initialization. Its password is then also returned.
+func runInitialSQL(
+	ctx context.Context, s *server.Server, startSingleNode bool, adminUser string,
+) (adminPassword string, err error) {
+	newCluster := s.InitialStart() && s.NodeID() == server.FirstNodeID
+	if !newCluster {
+		// The initial SQL code only runs the first time the cluster is initialized.
+		return "", nil
+	}
+
+	if startSingleNode {
 		// For start-single-node, set the default replication factor to
-		// 1 so as to avoid a warning message and unnecessary rebalance
+		// 1 so as to avoid warning messages and unnecessary rebalance
 		// churn.
 		if err := cliDisableReplication(ctx, s); err != nil {
 			log.Errorf(ctx, "could not disable replication: %v", err)
-			return err
+			return "", err
 		}
 		log.Infof(ctx, "Replication was disabled for this cluster.\n"+
 			"When/if adding nodes in the future, update zone configurations to increase the replication factor.")
 	}
 
-	return nil
+	if adminUser != "" && !s.Insecure() {
+		adminPassword, err = createAdminUser(ctx, s, adminUser)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return adminPassword, nil
+}
+
+// createAdminUser creates an admin user with the given name.
+func createAdminUser(
+	ctx context.Context, s *server.Server, adminUser string,
+) (adminPassword string, err error) {
+	adminPassword, err = security.GenerateRandomPassword()
+	if err != nil {
+		return "", err
+	}
+	if err := s.RunLocalSQL(ctx,
+		func(ctx context.Context, ie *sql.InternalExecutor) error {
+			_, err := ie.Exec(ctx, "admin-user", nil, "CREATE USER $1 WITH PASSWORD $2", adminUser, adminPassword)
+			if err != nil {
+				return err
+			}
+			// TODO(knz): Demote the admin user to an operator privilege with fewer options.
+			_, err = ie.Exec(ctx, "admin-user", nil, fmt.Sprintf("GRANT admin TO %s", tree.Name(adminUser)))
+			return err
+		}); err != nil {
+		return "", err
+	}
+	return adminPassword, nil
 }
 
 // cliDisableReplication changes the replication factor on
