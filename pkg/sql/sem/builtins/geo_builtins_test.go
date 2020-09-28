40a8872f8f99bb3cc4bd585409b4db24f791fd80
Oliver Tan <otan@cockroachlabs.com>
2020-09-28 13:48:27 -0700

    builtins: add test that checks all spatial functions handle POINT EMPTY
    SQLSmith can take a while to surface some things, so let's add this test
    that catches a lot of people offguard immediately.
    
    Release note: None

--- pkg/sql/sem/builtins/geo_builtins_test.go
+++ pkg/sql/sem/builtins/geo_builtins_test.go
@@ -11,11 +11,15 @@
 package builtins
 
 import (
+	"math/rand"
 	"strconv"
 	"strings"
 	"testing"
 	"unicode"
 
+	"github.com/cockroachdb/cockroach/pkg/sql/rowenc"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/stretchr/testify/require"
 )
@@ -30,6 +34,60 @@ func TestGeoBuiltinsInfo(t *testing.T) {
 					infoFirstLine := strings.Trim(strings.Split(overload.Info, "\n\n")[0], "\t\n ")
 					require.True(t, infoFirstLine[len(infoFirstLine)-1] == '.', "first line of info must end with a `.` character")
 					require.True(t, unicode.IsUpper(rune(infoFirstLine[0])), "first character of info start with an uppercase letter.")
+				})
+			}
+		})
+	}
+}
+
+// TestGeoBuiltinsPointEmptyArgs tests POINT EMPTY arguments do not cause panics.
+func TestGeoBuiltinsPointEmptyArgs(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	emptyGeometry, err := tree.ParseDGeometry("POINT EMPTY")
+	require.NoError(t, err)
+	emptyGeography, err := tree.ParseDGeography("POINT EMPTY")
+	require.NoError(t, err)
+
+	rng := rand.New(rand.NewSource(0))
+	for k, builtin := range geoBuiltins {
+		t.Run(k, func(t *testing.T) {
+			for i, overload := range builtin.overloads {
+				t.Run("overload_"+strconv.Itoa(i+1), func(t *testing.T) {
+					for overloadIdx := 0; overloadIdx < overload.Types.Length(); overloadIdx++ {
+						switch overload.Types.GetAt(overloadIdx).Family() {
+						case types.GeometryFamily, types.GeographyFamily:
+							t.Run("idx_"+strconv.Itoa(overloadIdx), func(t *testing.T) {
+								var datums tree.Datums
+								for i := 0; i < overload.Types.Length(); i++ {
+									if i == overloadIdx {
+										switch overload.Types.GetAt(i).Family() {
+										case types.GeometryFamily:
+											datums = append(datums, emptyGeometry)
+										case types.GeographyFamily:
+											datums = append(datums, emptyGeography)
+										default:
+											panic("unexpected condition")
+										}
+									} else {
+										datums = append(datums, rowenc.RandDatum(rng, overload.Types.GetAt(i), false))
+									}
+								}
+								var call strings.Builder
+								call.WriteString(k)
+								call.WriteByte('(')
+								for i, arg := range datums {
+									if i > 0 {
+										call.WriteString(", ")
+									}
+									call.WriteString(arg.String())
+								}
+								call.WriteByte(')')
+								t.Logf("calling: %s", call.String())
+								_, _ = overload.Fn(&tree.EvalContext{}, datums)
+							})
+						}
+					}
 				})
 			}
 		})
