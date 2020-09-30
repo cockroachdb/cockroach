191062f69da5067471a54bdf41ae0d03a2e80485
Raphael 'kena' Poss <knz@thaumogen.net>
2020-09-30 11:45:30 +0200

    cli/flags: remove `doctor` from the list of timeout-supporting commands
    This was unintentionally added - doctor is not meant to support
    configurable timeouts (just yet).
    
    Release note: None

--- pkg/cli/flags.go
+++ pkg/cli/flags.go
@@ -581,7 +581,6 @@ func init() {
 		statusNodeCmd,
 		lsNodesCmd,
 		debugZipCmd,
-		doctorClusterCmd,
 		// If you add something here, make sure the actual implementation
 		// of the command uses `cmdTimeoutContext(.)` or it will ignore
 		// the timeout.
