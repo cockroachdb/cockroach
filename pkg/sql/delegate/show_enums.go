5f531afe78c56d1ae941204d6548437f8bc18fa1
Oliver Tan <otan@cockroachlabs.com>
2020-10-01 14:55:45 -0700

    delegate: modify SHOW ENUMS and SHOW TYPES to display empty ENUMS
    Release note (bug fix): Fix a bug where empty enums did not show up for
    SHOW ENUMS or SHOW TYPES.

--- pkg/sql/delegate/show_enums.go
+++ pkg/sql/delegate/show_enums.go
@@ -13,28 +13,31 @@ package delegate
 import "github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 
 func (d *delegator) delegateShowEnums() (tree.Statement, error) {
+	// We can't query pg_enum directly as there are no rows in
+	// pg_enum if we create an empty enum (e.g. CREATE TYPE x AS ENUM()).
+	// Instead, use a CTE to aggregate enums, and use pg_type with an
+	// enum filter to LEFT JOIN against the aggregated enums to ensure
+	// we include these rows.
 	query := `
+WITH enums(enumtypid, values) AS (
+	SELECT
+		enums.enumtypid AS enumtypid,
+		string_agg(enums.enumlabel, '|') WITHIN GROUP (ORDER BY (enumsortorder)) AS values
+	FROM pg_catalog.pg_enum AS enums
+	GROUP BY enumtypid
+)
 SELECT
-	schema, name, string_agg(label, '|') AS values, owner
+	nsp.nspname AS schema,
+	types.typname AS name,
+	values,
+	rl.rolname AS owner
 FROM
-	(
-		SELECT
-			nsp.nspname AS schema,
-			type.typname AS name,
-			enum.enumlabel AS label,
-      rl.rolname AS owner
-		FROM
-			pg_catalog.pg_enum AS enum
-			JOIN pg_catalog.pg_type AS type ON (type.oid = enum.enumtypid)
-      LEFT JOIN pg_catalog.pg_roles AS rl on (type.typowner = rl.oid)
-			JOIN pg_catalog.pg_namespace AS nsp ON (type.typnamespace = nsp.oid)
-		ORDER BY
-			(enumtypid, enumsortorder)
-	)
-GROUP BY
-	(schema, name, owner)
-ORDER BY
-	(schema, name, owner);
+	pg_catalog.pg_type AS types
+	LEFT JOIN enums ON (types.oid = enums.enumtypid)
+	LEFT JOIN pg_catalog.pg_roles AS rl on (types.typowner = rl.oid)
+	JOIN pg_catalog.pg_namespace AS nsp ON (types.typnamespace = nsp.oid)
+WHERE types.typtype = 'e'
+ORDER BY (nsp.nspname, types.typname)
 `
 	return parse(query)
 }
