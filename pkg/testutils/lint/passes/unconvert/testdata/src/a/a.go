5852fa90614740bfd0fbe54210b46ca7d9b8c987
Andrew Werner <ajwerner@cockroachlabs.com>
2020-10-05 00:06:19 -0400

    testutils/lint/passes/unconvert: add support for nolint comments
    Adopts the earlier commit to add support for nolint comments to defeat the
    linter.
    
    Release note: None

--- pkg/testutils/lint/passes/unconvert/testdata/src/a/a.go
+++ pkg/testutils/lint/passes/unconvert/testdata/src/a/a.go
@@ -14,4 +14,94 @@ var (
 	f  float64 = -1
 	ff         = float64(f) // want `unnecessary conversion`
 	fi         = int(f)
+
+	// nolint:unconvert
+	fff = float64(f)
+
+	// nolint:unconvert
+	_ = float64(f) +
+		float64(f)
+
+	_ = 1 +
+		// nolint:unconvert
+		float64(f) +
+		2
+
+	_ = 1 +
+		2 +
+		float64(f) // nolint:unconvert
+
+	_ = 1 +
+		float64(f) + // nolint:unconvert
+		2
+
+	_ = 1 +
+		float64(f) + 2 // nolint:unconvert
+
+	_ = 1 +
+		float64(f) + 2 + 3 // nolint:unconvert
+
+	_ = 1 +
+		(float64(f) + 2 +
+			3) // nolint:unconvert
+
+	_ = 1 +
+		2 +
+		3 +
+		// nolint:unconvert
+		float64(f)
+
+	// nolint:unconvert
+	_ = 1 +
+		2 +
+		3 +
+		float64(f)
+
+	_ = 1 +
+		(float64(f) /* nolint:unconvert */ + 2 +
+			3)
+
+	// Here the comment does not cover the conversion.
+
+	_ = 1 +
+		// nolint:unconvert
+		2 +
+		3 +
+		float64(f) // want `unnecessary conversion`
+
+	_ = 1 +
+		// nolint:unconvert
+		2 +
+		float64(f) // want `unnecessary conversion`
+
+	_ = 1 +
+		(float64(f) + 2 + // want `unnecessary conversion`
+			3 /* nolint:unconvert */)
+
+	_ = 1 +
+		(float64(f) + 2 /* nolint:unconvert */ + // want `unnecessary conversion`
+			3)
+
+	_ = 1 +
+		(float64(f) + 2 + /* nolint:unconvert */ // want `unnecessary conversion`
+			3)
+
+	_ = 1 +
+		(float64(f) + 2 + // nolint:unconvert // want `unnecessary conversion`
+			3)
+
+	_ = 1 +
+		2 + // nolint:unconvert
+		float64(f) // want `unnecessary conversion`
 )
+
+func foo() {
+	// nolint:unconvert
+	if fff := float64(f); fff > 0 {
+		panic("foo")
+	}
+
+	if fff := float64(f); fff > 0 { // want `unnecessary conversion`
+		panic("foo")
+	}
+}
