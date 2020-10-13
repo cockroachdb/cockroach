d614cec8141fc244e2b28e83d04f8ba038f60aef
Monica Xu <monicax@cockroachlabs.com>
2020-10-13 11:11:27 -0400

    importccl: moved bundle import logic from planning phase to job execution phase.
    Previously, the logic for parsing schemas of Bundle forms such as postgresql dumps and mysql
    dumps was located in the planning phase for the import statement.
    
    This was inadequate because for large bundle imports where schema parsing takes
    up to an hour, there was no signal to the user that something was taking place.
    
    To address this, the logic for schema parsing for bundle formats was moved from the
    planning phase to the job execution phase. In the job execution phase, a RunningStatus
    was added to indicate to the user that the schema is being parsed.
    
    Release note: None

--- pkg/ccl/importccl/import_stmt.go
+++ pkg/ccl/importccl/import_stmt.go
@@ -94,6 +94,10 @@ const (
 	// as either an inline JSON schema, or an external schema URI.
 	avroSchema    = "schema"
 	avroSchemaURI = "schema_uri"
+
+	// RunningStatusImportBundleParseSchema indicates to the user that a bundle format
+	// schema is being parsed
+	RunningStatusImportBundleParseSchema jobs.RunningStatus = "parsing schema on Import Bundle"
 )
 
 var importOptionExpectValues = map[string]sql.KVStringOptValidate{
@@ -343,7 +347,6 @@ func importPlanHook(
 		}
 
 		table := importStmt.Table
-
 		var parentID, parentSchemaID descpb.ID
 		if table != nil {
 			// TODO: As part of work for #34240, we should be operating on
@@ -688,47 +691,7 @@ func importPlanHook(
 		} else {
 			seqVals := make(map[descpb.ID]int64)
 
-			if importStmt.Bundle {
-				store, err := p.ExecCfg().DistSQLSrv.ExternalStorageFromURI(ctx, files[0], p.User())
-				if err != nil {
-					return err
-				}
-				defer store.Close()
-
-				raw, err := store.ReadFile(ctx, "")
-				if err != nil {
-					return err
-				}
-				defer raw.Close()
-				reader, err := decompressingReader(raw, files[0], format.Compression)
-				if err != nil {
-					return err
-				}
-				defer reader.Close()
-
-				var match string
-				if table != nil {
-					match = table.ObjectName.String()
-				}
-
-				fks := fkHandler{skip: skipFKs, allowed: true, resolver: make(fkResolver)}
-				switch format.Format {
-				case roachpb.IOFileFormat_Mysqldump:
-					evalCtx := &p.ExtendedEvalContext().EvalContext
-					tableDescs, err = readMysqlCreateTable(ctx, reader, evalCtx, p, defaultCSVTableID, parentID, match, fks, seqVals)
-				case roachpb.IOFileFormat_PgDump:
-					evalCtx := &p.ExtendedEvalContext().EvalContext
-					tableDescs, err = readPostgresCreateTable(ctx, reader, evalCtx, p, match, parentID, walltime, fks, int(format.PgDump.MaxRowSize))
-				default:
-					return errors.Errorf("non-bundle format %q does not support reading schemas", format.Format.String())
-				}
-				if err != nil {
-					return err
-				}
-				if tableDescs == nil && table != nil {
-					return errors.Errorf("table definition not found for %q", table.ObjectName.String())
-				}
-			} else {
+			if !importStmt.Bundle {
 				if table == nil {
 					return errors.Errorf("non-bundle format %q should always have a table name", importStmt.FileFormat)
 				}
@@ -795,6 +758,7 @@ func importPlanHook(
 					IsNew:  true,
 				}
 			}
+
 		}
 
 		telemetry.CountBucketed("import.files", int64(len(files)))
@@ -823,27 +787,45 @@ func importPlanHook(
 		// connExecutor somehow.
 
 		importDetails := jobspb.ImportDetails{
-			URIs:       files,
-			Format:     format,
-			ParentID:   parentID,
-			Tables:     tableDetails,
-			SSTSize:    sstSize,
-			Oversample: oversample,
-			SkipFKs:    skipFKs,
+			URIs:              files,
+			Format:            format,
+			ParentID:          parentID,
+			Tables:            tableDetails,
+			SSTSize:           sstSize,
+			Oversample:        oversample,
+			SkipFKs:           skipFKs,
+			ParseBundleSchema: importStmt.Bundle,
+			Walltime:          walltime,
 		}
 
+		// TODO(mokaixu) move the below logic where match is assigned
+		if importStmt.Bundle {
+
+			if table != nil {
+				tableDetails = make([]jobspb.ImportDetails_Table, 1)
+				tableDetails[0] = jobspb.ImportDetails_Table{
+					Name:  table.ObjectName.String(),
+					IsNew: true,
+				}
+			}
+		}
 		// Prepare the protected timestamp record.
 		var spansToProtect []roachpb.Span
 		codec := p.(sql.PlanHookState).ExecCfg().Codec
-		for i := range tableDetails {
-			if td := &tableDetails[i]; !td.IsNew {
-				spansToProtect = append(spansToProtect, tableDescs[i].TableSpan(codec))
+
+		if tableDescs != nil {
+
+			for i := range tableDetails {
+				if td := &tableDetails[i]; !td.IsNew {
+					spansToProtect = append(spansToProtect, tableDescs[i].TableSpan(codec))
+				}
+			}
+			if len(spansToProtect) > 0 {
+				protectedtsID := uuid.MakeV4()
+				importDetails.ProtectedTimestampRecord = &protectedtsID
 			}
 		}
-		if len(spansToProtect) > 0 {
-			protectedtsID := uuid.MakeV4()
-			importDetails.ProtectedTimestampRecord = &protectedtsID
-		}
+
 		jr := jobs.Record{
 			Description: jobDesc,
 			Username:    p.User(),
@@ -888,7 +870,10 @@ func importPlanHook(
 }
 
 func parseAvroOptions(
-	ctx context.Context, opts map[string]string, p sql.PlanHookState, format *roachpb.IOFileFormat,
+	ctx context.Context,
+	opts map[string]string,
+	p sql.PlanHookState,
+	format *roachpb.IOFileFormat,
 ) error {
 	format.Format = roachpb.IOFileFormat_Avro
 	// Default input format is OCF.
@@ -1192,12 +1177,123 @@ func (r *importResumer) prepareTableDescsForIngestion(
 	return err
 }
 
+// parseAndCreateBundleTableDescs parses and creates the table
+// descriptors for bundle formats. The logic was previously in
+func parseAndCreateBundleTableDescs(
+	ctx context.Context,
+	p sql.PlanHookState,
+	tableDescs []*tabledesc.Mutable,
+	details jobspb.ImportDetails,
+	tableName string,
+	seqVals map[descpb.ID]int64,
+	skipFKs bool,
+	parentID descpb.ID,
+	files []string,
+	format roachpb.IOFileFormat,
+	walltime int64,
+) error {
+
+	store, err := p.ExecCfg().DistSQLSrv.ExternalStorageFromURI(ctx, files[0], p.User())
+	if err != nil {
+		return err
+	}
+	defer store.Close()
+
+	raw, err := store.ReadFile(ctx, "")
+	if err != nil {
+		return err
+	}
+	defer raw.Close()
+	reader, err := decompressingReader(raw, files[0], format.Compression)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
+	fks := fkHandler{skip: skipFKs, allowed: true, resolver: make(fkResolver)}
+	switch format.Format {
+	case roachpb.IOFileFormat_Mysqldump:
+		evalCtx := &p.ExtendedEvalContext().EvalContext
+		tableDescs, err = readMysqlCreateTable(ctx, reader, evalCtx, p, defaultCSVTableID, parentID, tableName, fks, seqVals)
+	case roachpb.IOFileFormat_PgDump:
+		evalCtx := &p.ExtendedEvalContext().EvalContext
+		tableDescs, err = readPostgresCreateTable(ctx, reader, evalCtx, p, tableName, parentID, walltime, fks, int(format.PgDump.MaxRowSize))
+	default:
+		return errors.Errorf("non-bundle format %q does not support reading schemas", format.Format.String())
+	}
+
+	return err
+
+}
+
 // Resume is part of the jobs.Resumer interface.
 func (r *importResumer) Resume(
 	ctx context.Context, phs interface{}, resultsCh chan<- tree.Datums,
 ) error {
 	details := r.job.Details().(jobspb.ImportDetails)
 	p := phs.(sql.PlanHookState)
+	seqVals := make(map[descpb.ID]int64)
+	skipFKs := details.SkipFKs
+	parentID := details.ParentID
+	files := details.URIs
+	format := details.Format
+	walltime := details.Walltime
+	var tableDescs []*tabledesc.Mutable
+	var tableName string
+
+	// Import Details table has 1 element when we are importing
+	// a targetted table
+	if len(details.Tables) > 0 {
+		tableName = details.Tables[0].Name
+	}
+
+	if details.ParseBundleSchema {
+
+		// Indicate to the user that schema parsing is taking place for bundle formats
+		if err := r.job.RunningStatus(ctx, func(_ context.Context, _ jobspb.Details) (jobs.RunningStatus, error) {
+			return RunningStatusImportBundleParseSchema, nil
+		}); err != nil {
+			return errors.Wrapf(err, "failed to update running status of job %d", errors.Safe(*r.job.ID()))
+		}
+
+		// Use information from planning phase to create and populate table descriptors
+		if err := parseAndCreateBundleTableDescs(
+			ctx, p, tableDescs, details, tableName, seqVals, skipFKs, parentID, files, format, walltime); err != nil {
+			return err
+		}
+
+		if tableDescs == nil && tableName != "" {
+			return errors.Errorf("table definition not found for %q", tableName)
+		}
+
+		tableDetails := make([]jobspb.ImportDetails_Table, len(tableDescs))
+		for i := range tableDescs {
+			tableDetails[i] = jobspb.ImportDetails_Table{
+				Desc:   tableDescs[i].TableDesc(),
+				SeqVal: seqVals[tableDescs[i].ID],
+				IsNew:  true,
+			}
+		}
+		details.Tables = tableDetails
+
+		for _, tbl := range tableDescs {
+			// For reasons relating to #37691, we disallow user defined types in
+			// the standard IMPORT case.
+			for _, col := range tbl.Columns {
+				if col.Type.UserDefined() {
+					return errors.Newf("IMPORT cannot be used with user defined types; use IMPORT INTO instead")
+				}
+			}
+		}
+
+		// We do not want to re-create existing table descriptors if job is re-resumed
+		details.ParseBundleSchema = false
+		if err := r.job.WithTxn(nil).SetDetails(ctx, details); err != nil {
+			return err
+		}
+
+	}
+
 	ptsID := details.ProtectedTimestampRecord
 	if ptsID != nil && !r.testingKnobs.ignoreProtectedTimestamps {
 		if err := p.ExecCfg().ProtectedTimestampProvider.Verify(ctx, *ptsID); err != nil {
@@ -1268,10 +1364,6 @@ func (r *importResumer) Resume(
 			return err
 		}
 	}
-
-	walltime := details.Walltime
-	files := details.URIs
-	format := details.Format
 
 	res, err := sql.DistIngest(ctx, p, r.job, tables, files, format, walltime, r.testingKnobs.alwaysFlushJobProgress)
 	if err != nil {
