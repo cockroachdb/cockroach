ca9439be3ab21d33d508e052e88c85461dac0cb1
Tobias Grieger <tobias.b.grieger@gmail.com>
2020-10-14 15:46:38 +0200

    server: fix and test restarting cluster with added stores
    We need to bootstrap additional stores asynchronously to avoid a
    deadlock when restarting the members of the quorum for the range
    that has the store ID allocator together.
    
    Fixes #55350.
    
    Release note: None

--- pkg/server/connectivity_test.go
+++ pkg/server/connectivity_test.go
@@ -30,6 +30,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 	"github.com/cockroachdb/errors"
+	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
 )
 
@@ -352,21 +353,26 @@ func TestAddNewStoresToExistingNodes(t *testing.T) {
 
 	numNodes := 3
 	tcArgs := base.TestClusterArgs{
-		ReplicationMode: base.ReplicationManual, // saves time
 		ServerArgsPerNode: map[int]base.TestServerArgs{
-			1: base.TestServerArgs{
-				StoreSpecs: []base.StoreSpec{base.StoreSpec{Path: n1s1}},
-			},
-			2: base.TestServerArgs{
-				StoreSpecs: []base.StoreSpec{base.StoreSpec{Path: n2s1}},
-			},
-			3: base.TestServerArgs{
-				StoreSpecs: []base.StoreSpec{base.StoreSpec{Path: n3s1}},
-			},
+			// NB: on my local (beefy) machine, upreplication
+			// takes ~6s. This is pretty hefty compared to ~1s
+			// with ephemeral stores. But - we need the real
+			// stores here. At the time of writing, we perform
+			// ~100 change replicas txns, all in all, and
+			// 0.06s for a replication change does seem ok.
+			0: {StoreSpecs: []base.StoreSpec{{Path: n1s1}}},
+			1: {StoreSpecs: []base.StoreSpec{{Path: n2s1}}},
+			2: {StoreSpecs: []base.StoreSpec{{Path: n3s1}}},
 		},
 	}
 
 	tc := testcluster.StartTestCluster(t, numNodes, tcArgs)
+	// NB: it's important that this test wait for full replication. Otherwise,
+	// with only a single voter on the range that allocates store IDs, it can
+	// pass erroneously. StartTestCluster already calls it, but we call it
+	// again explicitly.
+	tc.WaitForFullReplication()
+	clusterID := tc.Server(0).ClusterID()
 	tc.Stopper().Stop(ctx)
 
 	// Add an additional store to each node.
@@ -376,25 +382,28 @@ func TestAddNewStoresToExistingNodes(t *testing.T) {
 	defer n2cleanup2()
 	n3s2, n3cleanup2 := testutils.TempDir(t)
 	defer n3cleanup2()
+
 	tcArgs = base.TestClusterArgs{
+		// We need ParallelStart since this is an existing cluster. If
+		// we started sequentially, then the first node would hang forever
+		// waiting for the KV layer to become available, but that only
+		// happens when the second node also starts.
+		ParallelStart:   true,
 		ReplicationMode: base.ReplicationManual, // saves time
 		ServerArgsPerNode: map[int]base.TestServerArgs{
-			1: base.TestServerArgs{
+			0: {
 				StoreSpecs: []base.StoreSpec{
-					base.StoreSpec{Path: n1s1},
-					base.StoreSpec{Path: n1s2},
+					{Path: n1s1}, {Path: n1s2},
 				},
 			},
-			2: base.TestServerArgs{
+			1: {
 				StoreSpecs: []base.StoreSpec{
-					base.StoreSpec{Path: n2s1},
-					base.StoreSpec{Path: n2s2},
+					{Path: n2s1}, {Path: n2s2},
 				},
 			},
-			3: base.TestServerArgs{
+			2: {
 				StoreSpecs: []base.StoreSpec{
-					base.StoreSpec{Path: n3s1},
-					base.StoreSpec{Path: n3s2},
+					{Path: n3s1}, {Path: n3s2},
 				},
 			},
 		},
@@ -403,6 +412,12 @@ func TestAddNewStoresToExistingNodes(t *testing.T) {
 	// Start all nodes with additional stores.
 	tc = testcluster.StartTestCluster(t, numNodes, tcArgs)
 	defer tc.Stopper().Stop(ctx)
+
+	// Sanity check that we're testing what we wanted to test and didn't accidentally
+	// bootstrap three single-node clusters (who knows).
+	for _, srv := range tc.Servers {
+		require.Equal(t, clusterID, srv.ClusterID())
+	}
 
 	// Ensure all nodes have 2 stores available.
 	testutils.SucceedsSoon(t, func() error {
