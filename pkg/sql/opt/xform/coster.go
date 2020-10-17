13bc3ee305e28b42af33ab88a2e049554999bf91
Rebecca Taft <becca@cockroachlabs.com>
2020-10-17 09:41:22 -0500

    sql,opt: add hint for INVERTED join
    This commit adds the syntax and support for hinting an inverted
    join, similar to how it is already possible to hint a hash, merge,
    or lookup join. This will make it easier to test inverted joins
    and allow users to ensure that the optimizer plans an inverted join
    for queries where the hint is specified (and the other requirements
    for planning an inverted join are met).
    
    Release note (sql change): It is now possible to hint to the optimizer
    that it should plan an inverted join by using the syntax `... INNER
    INVERTED JOIN ...` or `... LEFT INVERTED JOIN ...`. If the hint is
    provided and it is possible to plan an inverted join, the optimizer
    will now plan an inverted join, even if it estimates that a different
    plan would have a lower cost. If the hint is provided but it is not
    possible to plan an inverted join because there is no inverted index
    on the right side table or the join condition is not a valid inverted
    join condition, the database will return an error.

--- pkg/sql/opt/xform/coster.go
+++ pkg/sql/opt/xform/coster.go
@@ -672,6 +672,9 @@ func (c *coster) computeHashJoinCost(join memo.RelExpr) memo.Cost {
 }
 
 func (c *coster) computeMergeJoinCost(join *memo.MergeJoinExpr) memo.Cost {
+	if join.MergeJoinPrivate.Flags.Has(memo.DisallowMergeJoin) {
+		return hugeCost
+	}
 	leftRowCount := join.Left.Relational().Stats.RowCount
 	rightRowCount := join.Right.Relational().Stats.RowCount
 
@@ -710,6 +713,9 @@ func (c *coster) computeIndexJoinCost(
 func (c *coster) computeLookupJoinCost(
 	join *memo.LookupJoinExpr, required *physical.Required,
 ) memo.Cost {
+	if join.LookupJoinPrivate.Flags.Has(memo.DisallowLookupJoinIntoRight) {
+		return hugeCost
+	}
 	return c.computeIndexLookupJoinCost(
 		join,
 		required,
@@ -801,6 +807,9 @@ func (c *coster) computeIndexLookupJoinCost(
 func (c *coster) computeInvertedJoinCost(
 	join *memo.InvertedJoinExpr, required *physical.Required,
 ) memo.Cost {
+	if join.InvertedJoinPrivate.Flags.Has(memo.DisallowInvertedJoinIntoRight) {
+		return hugeCost
+	}
 	lookupCount := join.Input.Relational().Stats.RowCount
 
 	// Take into account that the "internal" row count is higher, according to
--- pkg/sql/opt/xform/general_funcs.go
+++ pkg/sql/opt/xform/general_funcs.go
@@ -1868,7 +1868,7 @@ func (c *CustomFuncs) GenerateInvertedJoins(
 	on memo.FiltersExpr,
 	joinPrivate *memo.JoinPrivate,
 ) {
-	if joinPrivate.Flags.Has(memo.DisallowLookupJoinIntoRight) {
+	if joinPrivate.Flags.Has(memo.DisallowInvertedJoinIntoRight) {
 		return
 	}
 
