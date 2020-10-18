abd3c790d38c736c8a34cc3eb05530a5935b17bf
Rebecca Taft <becca@cockroachlabs.com>
2020-10-18 16:10:15 -0500

    sql,opt: add support for inverted joins with JSON and Array columns
    This commit adds support for planning and executing an invertedJoin
    when there is a contains (@>) predicate between two JSON or array columns,
    and the first one has an inverted index. Similar to spatial columns,
    the optimizer may be able to plan an inverted join for JSON and Array
    columns even if the join condition is complex, with multiple predicates
    combined with AND and OR expressions.
    
    Informs #35137
    
    Release note (performance improvement): The optimizer may now plan an
    inverted join if two tables are joined on JSON or Array columns using
    a contains predicate (e.g., `WHERE a @> b`), and the first column has
    an inverted index. The inverted join will be chosen if the optimizer
    expects it to be more efficient than any alternative plan. For queries
    in which the only alternative is a cartesian product followed by a
    filter, the inverted join will likely result in a performance improvement.

--- pkg/sql/span/span_builder.go
+++ pkg/sql/span/span_builder.go
@@ -421,12 +421,17 @@ func (s *Builder) SpansFromInvertedSpans(
 func (s *Builder) generateInvertedSpanKey(
 	enc []byte, scratchRow rowenc.EncDatumRow,
 ) (roachpb.Key, error) {
-	// Pretend that the encoded inverted val is an EncDatum. This isn't always
-	// true, since JSON inverted columns use a custom encoding. But since we
-	// are providing an already encoded Datum, the following will eventually
-	// fall through to EncDatum.Encode() which will reuse the encoded bytes.
-	encDatum := rowenc.EncDatumFromEncoded(descpb.DatumEncoding_ASCENDING_KEY, enc)
-	scratchRow[len(scratchRow)-1] = encDatum
-	span, _, err := s.SpanFromEncDatums(scratchRow, len(scratchRow))
+	prefixLen := len(scratchRow) - 1
+	scratchRow = scratchRow[:prefixLen]
+	if len(enc) > 0 {
+		// Pretend that the encoded inverted val is an EncDatum. This isn't always
+		// true, since JSON inverted columns use a custom encoding. But since we
+		// are providing an already encoded Datum, the following will eventually
+		// fall through to EncDatum.Encode() which will reuse the encoded bytes.
+		encDatum := rowenc.EncDatumFromEncoded(descpb.DatumEncoding_ASCENDING_KEY, enc)
+		scratchRow = append(scratchRow, encDatum)
+		prefixLen++
+	}
+	span, _, err := s.SpanFromEncDatums(scratchRow, prefixLen)
 	return span.Key, err
 }
