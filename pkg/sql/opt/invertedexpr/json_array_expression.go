f2d48cb739bae524a4dfb8b91437520119938d02
Rebecca Taft <becca@cockroachlabs.com>
2020-10-18 16:10:15 -0500

    sql,opt: add support for inverted joins with JSON and Array columns
    This commit adds support for planning and executing an invertedJoin
    when there is a contains (@>) predicate between two JSON or array columns,
    and the first one has an inverted index. Similar to spatial columns,
    the optimizer may be able to plan an inverted join for JSON and Array
    columns even if the join condition is complex, with multiple predicates
    combined with AND and OR expressions.
    
    Informs #35137
    
    Release note (performance improvement): The optimizer may now plan an
    inverted join if two tables are joined on JSON or Array columns using
    a contains predicate (e.g., `WHERE a @> b`), and the first column has
    an inverted index. The inverted join will be chosen if the optimizer
    expects it to be more efficient than any alternative plan. For queries
    in which the only alternative is a cartesian product followed by a
    filter, the inverted join will likely result in a performance improvement.

---
+++ pkg/sql/opt/invertedexpr/json_array_expression.go
@@ -0,0 +1,67 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package invertedexpr
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/rowenc"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/errors"
+)
+
+// JSONOrArrayToSpanExpr converts a JSON or Array datum to a SpanExpression if
+// possible. If not possible, returns nil. If the provided datum is not a JSON
+// or Array, returns an error.
+func JSONOrArrayToSpanExpr(
+	evalCtx *tree.EvalContext, d tree.Datum,
+) (_ *SpanExpression, tight bool, err error) {
+	var b []byte
+	spansSlice, tight, err := rowenc.EncodeContainingInvertedIndexSpans(
+		evalCtx, d, b, descpb.EmptyArraysInInvertedIndexesVersion,
+	)
+	if err != nil {
+		return nil, false, err
+	}
+	if len(spansSlice) == 0 {
+		return nil, false, errors.AssertionFailedf("trying to use null key in index lookup")
+	}
+
+	// The spans returned by EncodeInvertedIndexTableSpans represent the
+	// intersection of unions. So the below logic is performing a union on the
+	// inner loop and an intersection on the outer loop. See the comment
+	// above EncodeInvertedIndexTableSpans for details.
+	var invExpr InvertedExpression
+	for _, spans := range spansSlice {
+		var invExprLocal InvertedExpression
+		for _, span := range spans {
+			invSpan := InvertedSpan{Start: EncInvertedVal(span.Key), End: EncInvertedVal(span.EndKey)}
+			if invSpan.End == nil {
+				invSpan.End = EncInvertedVal(span.Key.PrefixEnd())
+			}
+			spanExpr := ExprForInvertedSpan(invSpan, true /* tight */)
+			if invExprLocal == nil {
+				invExprLocal = spanExpr
+			} else {
+				invExprLocal = Or(invExprLocal, spanExpr)
+			}
+		}
+		if invExpr == nil {
+			invExpr = invExprLocal
+		} else {
+			invExpr = And(invExpr, invExprLocal)
+		}
+	}
+
+	if spanExpr, ok := invExpr.(*SpanExpression); ok {
+		return spanExpr, tight, nil
+	}
+	return nil, false, nil
+}
