be8d0024adaa746627038991e89f25ee5bc50b2c
Oliver Tan <otan@cockroachlabs.com>
2020-10-19 11:43:41 -0700

    sql: add stubs for multiregion DATABASE and SHOW REGIONS
    I've allowed the plural forms for REGION(S) for CREATE/ALTER
    DATABASE...REGIONS, which I think makes it nice. Not too hesitant on
    committing this as is, because we can always change our minds later.
    
    Release note (sql change): Added stubs for the following syntaxes:
     * CREATE DATABASE d ... REGIONS regiona, regionb, ....
     * CREATE DATABASE d ... SURVIVE {AVAILABILITY ZONE FAILURE / REGION
       FAILURE / DEFAULT}
     * SHOW REGIONS / SHOW REGIONS FOR DATABASE d
     * ALTER DATABASE d ADD/DROP REGION(S) regiona, regionb, ...
     * ALTER DATABASE d SURVIVE ....

--- pkg/sql/opaque.go
+++ pkg/sql/opaque.go
@@ -51,6 +51,12 @@ func buildOpaque(
 	switch n := stmt.(type) {
 	case *tree.AlterDatabaseOwner:
 		plan, err = p.AlterDatabaseOwner(ctx, n)
+	case *tree.AlterDatabaseAddRegion:
+		plan, err = p.AlterDatabaseAddRegion(ctx, n)
+	case *tree.AlterDatabaseDropRegion:
+		plan, err = p.AlterDatabaseDropRegion(ctx, n)
+	case *tree.AlterDatabaseSurvive:
+		plan, err = p.AlterDatabaseSurvive(ctx, n)
 	case *tree.AlterIndex:
 		plan, err = p.AlterIndex(ctx, n)
 	case *tree.AlterSchema:
@@ -185,7 +191,10 @@ func buildOpaque(
 
 func init() {
 	for _, stmt := range []tree.Statement{
+		&tree.AlterDatabaseAddRegion{},
+		&tree.AlterDatabaseDropRegion{},
 		&tree.AlterDatabaseOwner{},
+		&tree.AlterDatabaseSurvive{},
 		&tree.AlterIndex{},
 		&tree.AlterSchema{},
 		&tree.AlterTable{},
