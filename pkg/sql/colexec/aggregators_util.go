3a26074e482d4d8a8332684acc47e693102dd36e
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-10-20 13:43:37 -0700

    colexec: fix DISTINCT ordered aggregation
    We're handling the DISTINCT aggregations by populating new selection
    vector to include only the tuples that are distinct to be aggregated by
    a particular function. Previously, we would convert the arguments to
    default aggregate function in case of ordered aggregation "with
    deselection" step (this is a minor optimization). However, combining
    these two things makes it very hard to know at which position in the
    converted []tree.Datum vectors the arguments of a tuple actually live
    which would result in us using the wrong ones. As a result, when
    actually performing the aggregation we could see that there is
    a selection vector (populated by the distinctness helper), yet there was
    no original selection vector on the batch itself, so we would use
    incorrect indices to look up the arguments in the converted []tree.Datum
    slices.
    
    Now this is fixed by following the pattern of the hash aggregator -
    converting the input argument vectors without the deselection. In order
    for the bug to be triggered we need to have at least two aggregate
    functions with one of them handled by the default aggregate function (i.e.
    it doesn't have an optimized implementation) with DISTINCT clause (if
    the projection contains only that function, then the distinct operation
    is pushed out of the aggregation into a distinct stage).
    
    Release note (bug fix): CockroachDB could previously incorrectly compute
    some aggregate functions with DISTINCT clauses when the query projects
    other columns/functions and the vectorized engine was used. Only 20.2
    versions are susceptible to this.

--- pkg/sql/colexec/aggregators_util.go
+++ pkg/sql/colexec/aggregators_util.go
@@ -354,6 +354,10 @@ func (b *distinctAggregatorHelperBase) selectDistinctTuples(
 			}
 		}
 		for _, colIdx := range inputIdxs {
+			// Note that we don't need to explicitly unset ed because encoded
+			// field is never set during fingerprinting - we'll compute the
+			// encoding and return it without updating the EncDatum; therefore,
+			// simply setting Datum field to the argument is sufficient.
 			b.scratch.ed.Datum = b.aggColsConverter.GetDatumColumn(int(colIdx))[tupleIdx]
 			// We know that we have tree.Datum, so there will definitely be no
 			// need to decode b.scratch.ed for fingerprinting, so we pass in
