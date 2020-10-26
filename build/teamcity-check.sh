990e8edafe49a9685f0fde9be854a57484ec1a73
Oliver Tan <otan@cockroachlabs.com>
2020-10-26 16:51:37 -0700

    build: add instructions to rectify code that is not regenerated
    Make it easier for the developer to know how to fix un-generated code by
    specifying the required commands.
    
    Release note: None

--- build/teamcity-check.sh
+++ build/teamcity-check.sh
@@ -12,6 +12,8 @@ function check_clean() {
   # The workspace is clean iff `git status --porcelain` produces no output. Any
   # output is either an error message or a listing of an untracked/dirty file.
   if [[ "$(git status --porcelain 2>&1)" != "" ]]; then
+    echo "Some automatically generated code is not up to date."
+    echo $1
     git status >&2 || true
     git diff -a >&2 || true
     exit 1
@@ -35,22 +37,23 @@ fi
 tc_start_block "Ensure generated code is up-to-date"
 # Buffer noisy output and only print it on failure.
 run build/builder.sh make generate &> artifacts/generate.log || (cat artifacts/generate.log && false)
-run build/builder.sh make buildshort &> artifacts/buildshort.log || (cat artifacts/buildshort.log && false)
 rm artifacts/generate.log
+check_clean "Run \`make generate\` to automatically fix these. For only BUILD.bazel issues, run \`bazel run //:gazelle\`."
+run build/builder.sh make buildshort &> artifacts/buildshort.log || (cat artifacts/buildshort.log && false)
 rm artifacts/buildshort.log
-check_clean
+check_clean "Run \`make buildshort\` to automatically fix these."
 tc_end_block "Ensure generated code is up-to-date"
 
 # generated code can generate new dependencies; check dependencies after generated code.
 tc_start_block "Ensure dependencies are up-to-date"
 # Run go mod tidy and `make -k vendor_rebuild` and ensure nothing changes.
 run build/builder.sh go mod tidy
-check_clean
+check_clean "Run \`go mod tidy\` and \`make vendor_rebuild\` to automatically fix these."
 run build/builder.sh make -k vendor_rebuild
 cd vendor
-check_clean
+check_clean "Run \`make vendor_rebuild\` to automatically fix these."
 cd ..
-check_clean
+check_clean "Run \`make vendor_rebuild\` to automatically fix these."
 tc_end_block "Ensure dependencies are up-to-date"
 
 tc_start_block "Lint"
