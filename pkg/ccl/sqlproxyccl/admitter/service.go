0f1411807a0a1827e912b1c392ea0ff70713a866
Spas Bojanov <spas@cockroachlabs.com>
2020-11-04 13:40:08 -0500

    sqlproxy/admitter:
    Add a new admitter package which performs basic connection admission
    control to rate limit connection attempts. Connection attempts are
    rate limited based on source IP. Admission control is currently
    purely a local decision, but the package this interface defines could
    easily become the client-side for connection to a centralized service.
    
    Note that this is a fork of the admitter package in the console repo.
    
    Release note: none.

---
+++ pkg/ccl/sqlproxyccl/admitter/service.go
@@ -0,0 +1,25 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package admitter
+
+import "time"
+
+// Service provides the interface for performing admission checks before
+// allowing requests into sqlproxy.
+type Service interface {
+	// AllowRequest determines whether a request should be allowed to proceed. It
+	// rate limits requests from IP addresses regardless of tenant id.
+	AllowRequest(ipAddress string, now time.Time) error
+
+	// RequestSuccess records the result of a successful request.
+	RequestSuccess(ipAddress string)
+
+	// KnownClient checks if this client has connected successfully before.
+	KnownClient(ipAddress string, tenID uint64) bool
+}
