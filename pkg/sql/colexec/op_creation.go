a266317bf17aa0534d580f3f4209f3825663871f
Yahor Yuzefovich <yahor@cockroachlabs.com>
2020-11-06 10:31:56 -1000

    colconv,colfetcher: pool allocations of converters and cTableInfos
    This commit pools the allocations of the converters when they are used
    in the materializers as well as `cTableInfo` structs used by the
    ColBatchScan. Additionally, this commit reuses the same ColumnFactory
    when setting up the whole flow and removes a copy of a processor spec
    when checking whether it is supported.
    
    Another notable change is reusing the same global empty post-processing
    spec when creating the materializers. It is thread-safe because the
    object is never modified.
    
    Release note: None

--- pkg/sql/colexec/op_creation.go
+++ pkg/sql/colexec/op_creation.go
@@ -14,6 +14,7 @@ import (
 	"context"
 	"sync"
 
+	"github.com/cockroachdb/cockroach/pkg/col/coldata"
 	"github.com/cockroachdb/cockroach/pkg/sql/colcontainer"
 	"github.com/cockroachdb/cockroach/pkg/sql/colexecbase"
 	"github.com/cockroachdb/cockroach/pkg/sql/execinfra"
@@ -39,6 +40,7 @@ type NewColOperatorArgs struct {
 	DiskQueueCfg         colcontainer.DiskQueueCfg
 	FDSemaphore          semaphore.Semaphore
 	ExprHelper           *ExprHelper
+	Factory              coldata.ColumnFactory
 	TestingKnobs         struct {
 		// UseStreamingMemAccountForBuffering specifies whether to use
 		// StreamingMemAccount when creating buffering operators and should only be
