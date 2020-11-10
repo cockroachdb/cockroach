cockroachdb/cockroach

commit ecf9e9c59d7a0aea73aad438f66f50e7b0f90a0a

liveness: introduce package for node liveness
NodeLiveness is a relatively isolated component and makes for a good
"should be a standalone package" candidate. We tease it out here, mostly
through mechanical code movements, in the hope that it'll force us to
clarify API boundaries going forward. Some intended changes to these
APIs, like introducing a way to read liveness records directly from KV,
will be added to this new package in other PRs. (These APIs were
originally proposed in #56107, and will be pulled out into stand alone
PRs after this one.)

While here, we also move out the liveness proto definition into this new
package. The "changes" made in this PR are the following:

  pkg/kv/kvserver/{node liveness files}               => pkg/kv/kvserver/liveness
  pkg/kv/kvserver/liveness/{node liveness protos}     => pkg/kv/kvserver/liveness/livenesspb
  pkg/kv/kvserver/liveness/node_liveness_test.go      => pkg/kv/kvserver/liveness/client_test.go
  pkg/kv/kvserver/liveness/node_liveness_unit_test.go => pkg/kv/kvserver/liveness/liveness_test.go

Separately, we've had to initialize server factory in TestMain as the
package internal tests depend on it. This was previously done within
pkg/kv/kvserver/main_test.go.

We've also renamed a few exported symbols to avoid stuttering. This was
needed to make our linters happy.

---

I've left the existing node liveness tests that make use of
multiTestContext in the kvserver package, seeing as how we're slowly
upgrading them to use TestCluster instead (#8299). The expectation is
that as we migrate each test, they'll move into this new package.

Release note: None

pkg/kv/kvserver/liveness/main_test.go
@@ -1,0 +1,24 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package liveness_test
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/server"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
+)
+
+func TestMain(m *testing.M) {
+	randutil.SeedForTests()
+	serverutils.InitTestServerFactory(server.TestServerFactory)
+}

commit 320b983a81f226162c00e4e28ad615e25e21564c

liveness: actually run tests
Broken in ecf9e9c59d7a0aea73aad438f66f50e7b0f90a0a.

pkg/kv/kvserver/liveness/main_test.go
@@ -11,14 +11,21 @@
 package liveness_test
 
 import (
+	"os"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/security"
+	"github.com/cockroachdb/cockroach/pkg/security/securitytest"
 	"github.com/cockroachdb/cockroach/pkg/server"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 )
 
 func TestMain(m *testing.M) {
+	security.SetAssetLoader(securitytest.EmbeddedAssets)
 	randutil.SeedForTests()
 	serverutils.InitTestServerFactory(server.TestServerFactory)
+	serverutils.InitTestClusterFactory(testcluster.TestClusterFactory)
+	os.Exit(m.Run())
 }