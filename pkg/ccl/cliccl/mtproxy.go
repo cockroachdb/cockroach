9349bd405a095ab8eefe9320b513ed892db69725
Spas Bojanov <spas@cockroachlabs.com>
2020-11-10 12:36:25 -0500

    [CC-3025] sqlproxy: add hook to rate limit connections
    We need a mechanism for sqlproxy users to provide admission control to
    clients that is enforced in the proxy. To that end we expand
    `BackendConfigFromParams` to provide a signal whether a connection
    should be refused and a callback to record the success of a connection.
    The callback can implement caching of known connections for example so
    that they are not rate limited on further connection requests.
    
    Release note: none.

--- pkg/ccl/cliccl/mtproxy.go
+++ pkg/ccl/cliccl/mtproxy.go
@@ -148,16 +148,22 @@ Uuwb2FVdh76ZK0AVd3Jh3KJs4+hr2u9syHaa7UPKXTcZsFWlGwZuu6X5A+0SO0S2
 		IncomingTLSConfig: &tls.Config{
 			Certificates: []tls.Certificate{cer},
 		},
-		BackendFromParams: func(params map[string]string) (addr string, conf *tls.Config, clientErr error) {
+		BackendConfigFromParams: func(
+			params map[string]string, ipAddress string,
+		) (config *sqlproxyccl.BackendConfig, clientErr error) {
 			const magic = "prancing-pony"
+			cfg := &sqlproxyccl.BackendConfig{
+				OutgoingAddress: sqlProxyTargetAddr,
+				TLSConf:         outgoingConf,
+			}
 			if strings.HasPrefix(params["database"], magic+".") {
 				params["database"] = params["database"][len(magic)+1:]
-				return sqlProxyTargetAddr, outgoingConf, nil
+				return cfg, nil
 			}
 			if params["options"] == "--cluster="+magic {
-				return sqlProxyTargetAddr, outgoingConf, nil
+				return cfg, nil
 			}
-			return "", nil, errors.Errorf("client failed to pass '%s' via database or options", magic)
+			return nil, errors.Errorf("client failed to pass '%s' via database or options", magic)
 		},
 	})
 
