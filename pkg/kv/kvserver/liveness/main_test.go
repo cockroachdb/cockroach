cockroachdb/cockroach

commit bec011918527bf35a10ca5f89625022864155140

liveness: introduce package for node liveness
NodeLiveness is a relatively isolated component and makes for a good
"should be a standalone package" candidate. We tease it out here, mostly
through mechanical code movements, in the hope that it'll force us to
clarify API boundaries going forward. Some intended changes to these
APIs, like introducing a way to read liveness records directly from KV,
will be added to this new package in other PRs. (These APIs were
originally proposed in #56107, and will be pulled out into stand alone
PRs after this one.)

While here, we also move out the liveness proto definition into this new
package. The "changes" made in this PR are the following:

  pkg/kv/kvserver/{node liveness files}               => pkg/kv/kvserver/liveness
  pkg/kv/kvserver/liveness/{node liveness protos}     => pkg/kv/kvserver/liveness/livenesspb
  pkg/kv/kvserver/liveness/node_liveness_test.go      => pkg/kv/kvserver/liveness/client_test.go
  pkg/kv/kvserver/liveness/node_liveness_unit_test.go => pkg/kv/kvserver/liveness/liveness_test.go

Separately, we've had to initialize server factory in TestMain as the
package internal tests depend on it. This was previously done within
pkg/kv/kvserver/main_test.go.

We've also renamed a few exported symbols to avoid stuttering. This was
needed to make our linters happy.

---

I've left the existing node liveness tests that make use of
multiTestContext in the kvserver package, seeing as how we're slowly
upgrading them to use TestCluster instead (#8299). The expectation is
that as we migrate each test, they'll move into this new package.

Release note: None

pkg/kv/kvserver/liveness/BUILD.bazel
@@ -1,0 +1,132 @@
+load("@io_bazel_rules_go//go:def.bzl", "go_library", "go_test")
+
+go_library(
+    name = "nodeliveness",
+    srcs = ["node_liveness.go"],
+    importpath = "github.com/cockroachdb/cockroach/pkg/kv/kvserver/nodeliveness",
+    visibility = ["//visibility:public"],
+    deps = [
+        "//pkg/base",
+        "//pkg/gossip",
+        "//pkg/keys",
+        "//pkg/kv",
+        "//pkg/kv/kvserver/closedts",
+        "//pkg/kv/kvserver/closedts/ctpb",
+        "//pkg/kv/kvserver/nodeliveness/nodelivenesspb",
+        "//pkg/roachpb",
+        "//pkg/settings/cluster",
+        "//pkg/storage",
+        "//pkg/util/contextutil",
+        "//pkg/util/hlc",
+        "//pkg/util/log",
+        "//pkg/util/metric",
+        "//pkg/util/retry",
+        "//pkg/util/stop",
+        "//pkg/util/syncutil",
+        "//pkg/util/timeutil",
+        "//pkg/util/tracing",
+        "//vendor/github.com/cockroachdb/errors",
+        "//vendor/github.com/cockroachdb/redact",
+    ],
+)
+
+go_test(
+    name = "nodeliveness_test",
+    srcs = [
+        "node_liveness_test.go",
+        "node_liveness_unit_test.go",
+    ],
+    embed = [":nodeliveness"],
+    deps = [
+        "//pkg/base",
+        "//pkg/config",
+        "//pkg/config/zonepb",
+        "//pkg/keys",
+        "//pkg/kv/kvserver",
+        "//pkg/kv/kvserver/nodeliveness/nodelivenesspb",
+        "//pkg/roachpb",
+        "//pkg/rpc",
+        "//pkg/server",
+        "//pkg/server/serverpb",
+        "//pkg/testutils",
+        "//pkg/testutils/skip",
+        "//pkg/testutils/testcluster",
+        "//pkg/util/hlc",
+        "//pkg/util/leaktest",
+        "//pkg/util/log",
+        "//pkg/util/syncutil",
+        "//vendor/github.com/cockroachdb/errors",
+        "//vendor/github.com/cockroachdb/logtags",
+        "//vendor/github.com/gogo/protobuf/proto",
+        "//vendor/github.com/kr/pretty",
+        "//vendor/github.com/stretchr/testify/assert",
+        "//vendor/github.com/stretchr/testify/require",
+    ],
+)
+
+go_library(
+    name = "liveness",
+    srcs = ["liveness.go"],
+    importpath = "github.com/cockroachdb/cockroach/pkg/kv/kvserver/liveness",
+    visibility = ["//visibility:public"],
+    deps = [
+        "//pkg/base",
+        "//pkg/gossip",
+        "//pkg/keys",
+        "//pkg/kv",
+        "//pkg/kv/kvserver/closedts",
+        "//pkg/kv/kvserver/closedts/ctpb",
+        "//pkg/kv/kvserver/liveness/livenesspb",
+        "//pkg/roachpb",
+        "//pkg/settings/cluster",
+        "//pkg/storage",
+        "//pkg/util/contextutil",
+        "//pkg/util/hlc",
+        "//pkg/util/log",
+        "//pkg/util/metric",
+        "//pkg/util/retry",
+        "//pkg/util/stop",
+        "//pkg/util/syncutil",
+        "//pkg/util/timeutil",
+        "//pkg/util/tracing",
+        "//vendor/github.com/cockroachdb/errors",
+        "//vendor/github.com/cockroachdb/redact",
+    ],
+)
+
+go_test(
+    name = "liveness_test",
+    srcs = [
+        "client_test.go",
+        "liveness_test.go",
+        "main_test.go",
+    ],
+    embed = [":liveness"],
+    deps = [
+        "//pkg/base",
+        "//pkg/config",
+        "//pkg/config/zonepb",
+        "//pkg/keys",
+        "//pkg/kv/kvserver",
+        "//pkg/kv/kvserver/liveness/livenesspb",
+        "//pkg/roachpb",
+        "//pkg/rpc",
+        "//pkg/server",
+        "//pkg/server/serverpb",
+        "//pkg/testutils",
+        "//pkg/testutils/serverutils",
+        "//pkg/testutils/skip",
+        "//pkg/testutils/testcluster",
+        "//pkg/util/hlc",
+        "//pkg/util/leaktest",
+        "//pkg/util/log",
+        "//pkg/util/randutil",
+        "//pkg/util/syncutil",
+        "//vendor/github.com/cockroachdb/errors",
+        "//vendor/github.com/cockroachdb/logtags",
+        "//vendor/github.com/gogo/protobuf/proto",
+        "//vendor/github.com/kr/pretty",
+        "//vendor/github.com/stretchr/testify/assert",
+        "//vendor/github.com/stretchr/testify/require",
+    ],
+)

pkg/kv/kvserver/liveness/main_test.go
@@ -1,0 +1,24 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package liveness_test
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/server"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
+)
+
+func TestMain(m *testing.M) {
+	randutil.SeedForTests()
+	serverutils.InitTestServerFactory(server.TestServerFactory)
+}

pkg/cli/BUILD.bazel
@@ -94,7 +94,7 @@ go_library(
         "//pkg/keys",
         "//pkg/kv/kvserver",
         "//pkg/kv/kvserver/gc",
-        "//pkg/kv/kvserver/kvserverpb",
+        "//pkg/kv/kvserver/liveness/livenesspb",
         "//pkg/kv/kvserver/rditer",
         "//pkg/kv/kvserver/stateloader",
         "//pkg/roachpb",
@@ -287,7 +287,7 @@ go_test(
         "//pkg/keys",
         "//pkg/kv",
         "//pkg/kv/kvserver",
-        "//pkg/kv/kvserver/kvserverpb",
+        "//pkg/kv/kvserver/liveness/livenesspb",
         "//pkg/kv/kvserver/stateloader",
         "//pkg/roachpb",
         "//pkg/rpc",

pkg/jobs/BUILD.bazel
@@ -26,7 +26,7 @@ go_library(
         "//pkg/jobs/jobspb",
         "//pkg/keys",
         "//pkg/kv",
-        "//pkg/kv/kvserver/kvserverpb",
+        "//pkg/kv/kvserver/liveness/livenesspb",
         "//pkg/roachpb",
         "//pkg/scheduledjobs",
         "//pkg/security",

pkg/kv/kvserver/BUILD.bazel
@@ -16,7 +16,6 @@ go_library(
         "log.go",
         "merge_queue.go",
         "metrics.go",
-        "node_liveness.go",
         "queue.go",
         "queue_helpers_testutil.go",
         "raft.go",
@@ -121,6 +120,8 @@ go_library(
         "//pkg/kv/kvserver/intentresolver",
         "//pkg/kv/kvserver/kvserverbase",
         "//pkg/kv/kvserver/kvserverpb",
+        "//pkg/kv/kvserver/liveness",
+        "//pkg/kv/kvserver/liveness/livenesspb",
         "//pkg/kv/kvserver/protectedts",
         "//pkg/kv/kvserver/protectedts/ptpb",
         "//pkg/kv/kvserver/raftentry",
@@ -226,7 +227,6 @@ go_test(
         "merge_queue_test.go",
         "metrics_test.go",
         "node_liveness_test.go",
-        "node_liveness_unit_test.go",
         "queue_concurrency_test.go",
         "queue_test.go",
         "raft_log_queue_test.go",
@@ -295,6 +295,8 @@ go_test(
         "//pkg/kv/kvserver/intentresolver",
         "//pkg/kv/kvserver/kvserverbase",
         "//pkg/kv/kvserver/kvserverpb",
+        "//pkg/kv/kvserver/liveness",
+        "//pkg/kv/kvserver/liveness/livenesspb",
         "//pkg/kv/kvserver/protectedts",
         "//pkg/kv/kvserver/protectedts/ptpb",
         "//pkg/kv/kvserver/protectedts/ptstorage",

pkg/kv/kvserver/kvserverpb/BUILD.bazel
@@ -4,8 +4,6 @@ go_library(
     name = "kvserverpb",
     srcs = [
         "lease_status.pb.go",
-        "liveness.go",
-        "liveness.pb.go",
         "log.go",
         "log.pb.go",
         "proposer_kv.go",
@@ -15,15 +13,13 @@ go_library(
     importpath = "github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvserverpb",
     visibility = ["//visibility:public"],
     deps = [
+        "//pkg/kv/kvserver/liveness/livenesspb",
         "//pkg/roachpb",
         "//pkg/storage/enginepb",
         "//pkg/util/hlc",
-        "//pkg/util/timeutil",
         "//pkg/util/uuid",
         "//vendor/github.com/gogo/protobuf/proto",
         "//vendor/github.com/gogo/protobuf/sortkeys",
         "//vendor/github.com/gogo/protobuf/types",
-        "//vendor/google.golang.org/grpc/codes",
-        "//vendor/google.golang.org/grpc/status",
     ],
 )

pkg/kv/kvserver/liveness/livenesspb/BUILD.bazel
@@ -1,0 +1,37 @@
+load("@io_bazel_rules_go//go:def.bzl", "go_library")
+
+go_library(
+    name = "nodelivenesspb",
+    srcs = [
+        "liveness.go",
+        "liveness.pb.go",
+    ],
+    importpath = "github.com/cockroachdb/cockroach/pkg/kv/kvserver/nodeliveness/nodelivenesspb",
+    visibility = ["//visibility:public"],
+    deps = [
+        "//pkg/roachpb",
+        "//pkg/util/hlc",
+        "//pkg/util/timeutil",
+        "//vendor/github.com/gogo/protobuf/proto",
+        "//vendor/google.golang.org/grpc/codes",
+        "//vendor/google.golang.org/grpc/status",
+    ],
+)
+
+go_library(
+    name = "livenesspb",
+    srcs = [
+        "liveness.go",
+        "liveness.pb.go",
+    ],
+    importpath = "github.com/cockroachdb/cockroach/pkg/kv/kvserver/liveness/livenesspb",
+    visibility = ["//visibility:public"],
+    deps = [
+        "//pkg/roachpb",
+        "//pkg/util/hlc",
+        "//pkg/util/timeutil",
+        "//vendor/github.com/gogo/protobuf/proto",
+        "//vendor/google.golang.org/grpc/codes",
+        "//vendor/google.golang.org/grpc/status",
+    ],
+)

pkg/kv/kvserver/reports/BUILD.bazel
@@ -18,6 +18,7 @@ go_library(
         "//pkg/keys",
         "//pkg/kv",
         "//pkg/kv/kvserver",
+        "//pkg/kv/kvserver/liveness",
         "//pkg/roachpb",
         "//pkg/security",
         "//pkg/settings",

commit 44e79f0d0bbd02ded3d2f313db4a553e544d0823

liveness: actually run tests
Broken in bec011918527bf35a10ca5f89625022864155140.

pkg/kv/kvserver/liveness/BUILD.bazel
@@ -47,6 +47,8 @@ go_test(
         "//pkg/kv/kvserver/liveness/livenesspb",
         "//pkg/roachpb",
         "//pkg/rpc",
+        "//pkg/security",
+        "//pkg/security/securitytest",
         "//pkg/server",
         "//pkg/server/serverpb",
         "//pkg/testutils",

pkg/kv/kvserver/liveness/main_test.go
@@ -11,14 +11,21 @@
 package liveness_test
 
 import (
+	"os"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/security"
+	"github.com/cockroachdb/cockroach/pkg/security/securitytest"
 	"github.com/cockroachdb/cockroach/pkg/server"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 )
 
 func TestMain(m *testing.M) {
+	security.SetAssetLoader(securitytest.EmbeddedAssets)
 	randutil.SeedForTests()
 	serverutils.InitTestServerFactory(server.TestServerFactory)
+	serverutils.InitTestClusterFactory(testcluster.TestClusterFactory)
+	os.Exit(m.Run())
 }