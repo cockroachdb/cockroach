04dfa10ec5f2643036b4a4c206ac509946ea2250
Andrew Werner <ajwerner@cockroachlabs.com>
2020-11-13 10:34:27 -0500

    sql/catalog/descs: cache resolved mutable descriptor objects upon resolution
    When resolving descriptors, the collection checks to see if an "uncommitted"
    descriptor had been previously added. Such descriptors are added explicitly
    by users with the `AddUncommittedDescriptor()` method. However, prior to this
    PR, that method could only be called with a "modified" descriptor (one with an
    incremented version) and thus was not called when a mutable descriptor is
    resolved (only when it is added). This can be problematic as a mutable
    descriptor may be resolved more than once during the execution of a statement
    prior to the point where that descriptor is written to the database and added.
    
    In some places, to deal with this, we would increment the version of the
    descriptor and add it to the collection prior to performing any changes in
    order to ensure that we'd get back the same in-memory object if the same
    descriptor was subsequently resolved by name or ID. This was to fix bugs.
    We've seen more bugs of this type lately (#56387).
    
    The PR changes the behavior such that once you resolve a mutable descriptor
    once, subsequent resolutions will return the same object. This should make
    the behavior more obvious. It also caches the mutable descriptor object upon
    the first uncached read from the store. This ends up being valuable because it
    means that when resolving a mutable object, its parents are pre-fetched and
    cached.
    
    This fixes an incidental bug in `AddGeometryColumn` whereby multiple
    invocations would clobber eachother because they'd resolve different mutable
    objects at planning time. cc @rytaft.
    
    Release note (bug fix): Fixed a bug where multiple invocations of
    `AddGeometryColumn` affecting the same table would result in only the last
    invocation applying.

--- pkg/ccl/backupccl/backup_test.go
+++ pkg/ccl/backupccl/backup_test.go
@@ -6645,15 +6645,15 @@ CREATE TYPE sc.typ AS ENUM ('hello');
 
 		sqlDB.ExpectErr(t, `database "d" is offline: restoring`, `USE d`)
 
-		sqlDB.ExpectErr(t, `target database or schema does not exist`, `SHOW TABLES FROM d`)
-		sqlDB.ExpectErr(t, `target database or schema does not exist`, `SHOW TABLES FROM d.sc`)
+		sqlDB.ExpectErr(t, `database "d" is offline: restoring`, `SHOW TABLES FROM d`)
+		sqlDB.ExpectErr(t, `database "d" is offline: restoring`, `SHOW TABLES FROM d.sc`)
 
-		sqlDB.ExpectErr(t, `relation "d.sc.tb" does not exist`, `SELECT * FROM d.sc.tb`)
-		sqlDB.ExpectErr(t, `relation "d.sc.tb" does not exist`, `ALTER TABLE d.sc.tb ADD COLUMN b INT`)
+		sqlDB.ExpectErr(t, `database "d" is offline: restoring`, `SELECT * FROM d.sc.tb`)
+		sqlDB.ExpectErr(t, `database "d" is offline: restoring`, `ALTER TABLE d.sc.tb ADD COLUMN b INT`)
 
-		sqlDB.ExpectErr(t, `type "d.sc.typ" does not exist`, `ALTER TYPE d.sc.typ RENAME TO typ2`)
+		sqlDB.ExpectErr(t, `database "d" is offline: restoring`, `ALTER TYPE d.sc.typ RENAME TO typ2`)
 
-		sqlDB.ExpectErr(t, `cannot create "d.sc.other" because the target database or schema does not exist`, `CREATE TABLE d.sc.other()`)
+		sqlDB.ExpectErr(t, `database "d" is offline: restoring`, `CREATE TABLE d.sc.other()`)
 
 		close(continueNotif)
 		require.NoError(t, g.Wait())
@@ -6741,7 +6741,7 @@ CREATE TYPE sc.typ AS ENUM ('hello');
 			{"public", security.AdminRole},
 		})
 
-		sqlDB.ExpectErr(t, `target database or schema does not exist`, `SHOW TABLES FROM newdb.sc`)
+		sqlDB.ExpectErr(t, `schema "sc" is offline: restoring`, `SHOW TABLES FROM newdb.sc`)
 
 		sqlDB.ExpectErr(t, `schema "sc" is offline: restoring`, `SELECT * FROM newdb.sc.tb`)
 		sqlDB.ExpectErr(t, `schema "sc" is offline: restoring`, `SELECT * FROM sc.tb`)
