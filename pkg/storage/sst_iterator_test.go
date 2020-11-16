ee2ff613c15c5920bc77da6862c06e201a22b1c0
sumeerbhola <sumeer@cockroachlabs.com>
2020-11-16 14:35:29 -0500

    storage: change the comparer and time bounds collector to handle EngineKeys
    - Rename MVCCComparer to PebbleComparer. PebbleComparer is changed to
      handle EngineKeys.
    - Add GetKeyPartFromEngineKey for callers that don't care about the
      version.
    - Fix pebbleTimeBoundPropCollector to handle EngineKeys.
    
    Informs #41720
    
    Release note: None

--- pkg/storage/sst_iterator_test.go
+++ pkg/storage/sst_iterator_test.go
@@ -142,13 +142,13 @@ func TestCockroachComparer(t *testing.T) {
 		Timestamp: hlc.Timestamp{WallTime: 2},
 	}
 
-	if x := MVCCComparer.Compare(EncodeKey(keyAMetadata), EncodeKey(keyA1)); x != -1 {
+	if x := EngineComparer.Compare(EncodeKey(keyAMetadata), EncodeKey(keyA1)); x != -1 {
 		t.Errorf("expected key metadata to sort first got: %d", x)
 	}
-	if x := MVCCComparer.Compare(EncodeKey(keyA2), EncodeKey(keyA1)); x != -1 {
+	if x := EngineComparer.Compare(EncodeKey(keyA2), EncodeKey(keyA1)); x != -1 {
 		t.Errorf("expected higher timestamp to sort first got: %d", x)
 	}
-	if x := MVCCComparer.Compare(EncodeKey(keyA2), EncodeKey(keyB2)); x != -1 {
+	if x := EngineComparer.Compare(EncodeKey(keyA2), EncodeKey(keyB2)); x != -1 {
 		t.Errorf("expected lower key to sort first got: %d", x)
 	}
 }
