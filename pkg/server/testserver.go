b288508f237ba4ffbb243a7df0a1de2acf831c01
Raphael 'kena' Poss <knz@thaumogen.net>
2020-11-18 20:27:41 +0100

    testserver: properly report errors when constructing servers
    Release note: None

--- pkg/server/testserver.go
+++ pkg/server/testserver.go
@@ -1291,7 +1291,7 @@ type testServerFactoryImpl struct{}
 var TestServerFactory = testServerFactoryImpl{}
 
 // New is part of TestServerFactory interface.
-func (testServerFactoryImpl) New(params base.TestServerArgs) interface{} {
+func (testServerFactoryImpl) New(params base.TestServerArgs) (interface{}, error) {
 	cfg := makeTestConfigFromParams(params)
 	ts := &TestServer{Cfg: &cfg, params: params}
 
@@ -1306,13 +1306,15 @@ func (testServerFactoryImpl) New(params base.TestServerArgs) interface{} {
 	// Needs to be called before NewServer to ensure resolvers are initialized.
 	ctx := context.Background()
 	if err := ts.Cfg.InitNode(ctx); err != nil {
-		return err
+		params.Stopper.Stop(ctx)
+		return nil, err
 	}
 
 	var err error
 	ts.Server, err = NewServer(*ts.Cfg, params.Stopper)
 	if err != nil {
-		return err
+		params.Stopper.Stop(ctx)
+		return nil, err
 	}
 
 	// Create a breaker which never trips and never backs off to avoid
@@ -1326,5 +1328,5 @@ func (testServerFactoryImpl) New(params base.TestServerArgs) interface{} {
 	// Our context must be shared with our server.
 	ts.Cfg = &ts.Server.cfg
 
-	return ts
+	return ts, nil
 }
--- pkg/testutils/testcluster/testcluster.go
+++ pkg/testutils/testcluster/testcluster.go
@@ -398,7 +398,11 @@ func (tc *TestCluster) AddServer(serverArgs base.TestServerArgs) (*server.TestSe
 		serverArgs.Addr = serverArgs.Listener.Addr().String()
 	}
 
-	s := serverutils.NewServer(serverArgs).(*server.TestServer)
+	srv, err := serverutils.NewServer(serverArgs)
+	if err != nil {
+		return nil, err
+	}
+	s := srv.(*server.TestServer)
 
 	tc.Servers = append(tc.Servers, s)
 	tc.serverArgs = append(tc.serverArgs, serverArgs)
