b5b1eb463a04c81a75f9d4607a743b026b5362e9
Radu Berinde <radu@cockroachlabs.com>
2020-11-25 13:42:20 -0800

    opt: don't hold on to evalCtx from detached Memo
    This change adds more "cleanup" code when detaching a Memo (a detached
    memo is stored in the query cache and is reused later in a "read-only"
    fashion). In particular, we clear the EvalContext in
    logicalPropsBuilder which can lead to inadvertently holding on to a
    lot of memory.
    
    Fixes #57059.
    
    Release note: None

--- pkg/sql/opt/norm/factory.go
+++ pkg/sql/opt/norm/factory.go
@@ -114,11 +114,11 @@ func (f *Factory) Init(evalCtx *tree.EvalContext, catalog cat.Catalog) {
 // placeholders are assigned. If there are no placeholders, there is no need
 // for column statistics, since the memo is already fully optimized.
 func (f *Factory) DetachMemo() *memo.Memo {
-	f.mem.ClearColStats(f.mem.RootExpr())
-	detach := f.mem
+	m := f.mem
 	f.mem = nil
+	m.Detach()
 	f.Init(f.evalCtx, nil /* catalog */)
-	return detach
+	return m
 }
 
 // DisableOptimizations disables all transformation rules. The unaltered input
