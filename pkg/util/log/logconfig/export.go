bf4c97d679da6e4f92243f4087c60a2626340f30
Raphael 'kena' Poss <knz@thaumogen.net>
2020-11-26 12:19:32 +0100

    util/log: new experimental integration with Fluentd
    Release note (cli change): It is now possible to redirect logging to
    [Fluentd](https://www.fluentd.org)-compatible network collectors. See
    the reference sink documentation for details. This is an alpha-quality
    feature.  Note that Fluent-enabled configuration only provide
    minimal event buffering, and log entries are dropped if the
    logging server becomes unavailable or network errors are
    encountered. This is a known limitation and will be likely improved in
    a later version.

--- pkg/util/log/logconfig/export.go
+++ pkg/util/log/logconfig/export.go
@@ -171,6 +171,33 @@ func (c *Config) Export(onlyChans ChannelList) (string, string) {
 		links = append(links, "stray --> stderr")
 	}
 
+	// Collect the network servers.
+	//
+	// servers maps each server to its box declaration.
+	servers := map[string]string{}
+	for _, fn := range c.Sinks.sortedServerNames {
+		fc := c.Sinks.FluentServers[fn]
+		if fc.Filter == logpb.Severity_NONE {
+			continue
+		}
+		skey := fmt.Sprintf("s__%s", fc.serverName)
+		target, thisprocs, thislinks := process(skey, fc.CommonSinkConfig)
+		hasLink := false
+		for _, ch := range fc.Channels.Channels {
+			if !chanSel.HasChannel(ch) {
+				continue
+			}
+			hasLink = true
+			links = append(links, fmt.Sprintf("%s --> %s", ch, target))
+		}
+		if hasLink {
+			processing = append(processing, thisprocs...)
+			links = append(links, thislinks...)
+			servers[fc.serverName] = fmt.Sprintf("queue %s as \"fluent: %s:%s\"",
+				skey, fc.Net, fc.Address)
+		}
+	}
+
 	// Export the stderr redirects.
 	if c.Sinks.Stderr.Filter != logpb.Severity_NONE {
 		target, thisprocs, thislinks := process("stderr", c.Sinks.Stderr.CommonSinkConfig)
@@ -206,6 +233,15 @@ func (c *Config) Export(onlyChans ChannelList) (string, string) {
 				fmt.Fprintf(&buf, "  %s\n", fdecl)
 			}
 			buf.WriteString(" }\n")
+		}
+		buf.WriteString("}\n")
+	}
+
+	// Represent the network servers, if any.
+	if len(c.Sinks.sortedServerNames) > 0 {
+		buf.WriteString("cloud network {\n")
+		for _, s := range c.Sinks.sortedServerNames {
+			fmt.Fprintf(&buf, " %s\n", servers[s])
 		}
 		buf.WriteString("}\n")
 	}
