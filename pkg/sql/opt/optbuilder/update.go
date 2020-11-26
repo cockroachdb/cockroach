f06aeaa421c06dd8c5172633594a049e4c7d36e6
Marcus Gartner <marcus@cockroachlabs.com>
2020-11-25 17:25:26 -0800

    opt: fix FK cascades to child tables with partial indexes
    Previously, the optimizer was not synthesizing partial index DEL columns
    for FK cascading updates and deletes. As a result, a cascading `UPDATE`
    could corrupt a child table's partial index, ultimately resulting in
    incorrect query results. A cascading `DELETE` would not corrupt
    partial indexes, but unnecessary `DEL` operations would be issued on
    the partial index.
    
    The optbuilder has been refactored so that these columns are correctly
    projected. Both PUT and DEL columns are now projected in the same
    function, `mutationBuilder.projectPartialIndexCols`. This function is
    called from principal functions in the optbuilder where CHECK constraint
    columns are also projected, like `mutationBuilder.buildUpdate`. In
    theory this should make it harder in the future to omit these necessary
    projections.
    
    Additionally, the execution engine was unable to handle extraneous
    columns that can be added as input to FK cascading updates. These
    extraneous columns would be incorrectly interpreted as synthesized
    partial index columns. This commit works around this issue by slicing
    the source values with an upper bound in `upateNode.processSourceRow`.
    The longer term fix is to not produce these columns (see issue #57097).
    
    Fixes #57085
    Fixes #57084
    
    Release justification: This is a critical bug fix to a new feature,
    partial indexes.
    
    Release note (bug fix): A bug has been fixed that caused errors or
    corrupted partial indexes of child tables in foreign key relationships
    with cascading `UPDATE`s and `DELETE`s. The corrupt partial indexes
    could result in incorrect query results. Any partial indexes on child
    tables of foreign key relationships with `ON DELETE CASCADE` or `ON
    UPDATE CASCADE` actions may be corrupt and should be dropped and
    re-created. This bug was introduce in version 20.2.

--- pkg/sql/opt/optbuilder/update.go
+++ pkg/sql/opt/optbuilder/update.go
@@ -345,8 +345,8 @@ func (mb *mutationBuilder) buildUpdate(returning tree.ReturningExprs) {
 
 	mb.addCheckConstraintCols()
 
-	// Add partial index put boolean columns to the input.
-	mb.projectPartialIndexPutCols(preCheckScope)
+	// Project partial index PUT and DEL boolean columns.
+	mb.projectPartialIndexPutAndDelCols(preCheckScope, mb.fetchScope)
 
 	mb.buildFKChecksForUpdate()
 
