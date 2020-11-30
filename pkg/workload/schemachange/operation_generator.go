c07156dead75c0c9e545cff5c7e3abca2d89924b
Jayant Shrivastava <jayants@cockroachlabs.com>
2020-11-30 11:36:41 -0500

    workload/schemachange: use regex to fetch tables
    Previously, the clause `LIKE 'table%'` was used to fetch tables.
    This pattern was capabable of fetching system tables, such as
    information_schema.tables. Generally, this workload should not
    alter the system tables, and should only test using the tables it
    creates. Tables created by the workload can be fetched using
    the clause `~ 'table[0-9]+'`, which is the word 'table' followed
    by a sequence number.
    
    Release note: None

--- pkg/workload/schemachange/operation_generator.go
+++ pkg/workload/schemachange/operation_generator.go
@@ -1832,7 +1832,7 @@ func (og *operationGenerator) randTable(
 		q := fmt.Sprintf(`
 		  SELECT table_name
 		    FROM [SHOW TABLES]
-		   WHERE table_name LIKE 'table%%'
+		   WHERE table_name ~ 'table[0-9]+'
 				 AND schema_name = '%s'
 		ORDER BY random()
 		   LIMIT 1;
@@ -1870,7 +1870,7 @@ func (og *operationGenerator) randTable(
 	const q = `
   SELECT schema_name, table_name
     FROM [SHOW TABLES]
-   WHERE table_name LIKE 'table%'
+   WHERE table_name ~ 'table[0-9]+'
 ORDER BY random()
    LIMIT 1;
 `
