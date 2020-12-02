87d3c5f3fb875b338fe95c98a2367042529cac1b
Bilal Akhtar <bilal@cockroachlabs.com>
2020-12-02 13:41:39 -0500

    cli: Set GOMAXPROCS on start if in a CPU-limited cgroup
    Having a GOMAXPROCS value defaulting to the Go default
    (numCPUs) is inefficient if there's a lower CPU limit
    set for the cgroup the cockroach process is in. The
    Go runtime could be scheduling multiple OS-level
    threads, not all of which would get to run concurrently.
    
    This change sees if the GOMAXPROCS env variable
    was set, denoting an overriden value. If it isn't
    set, and we're inside a cgroup, the value of
    GOMAXPROCS is now lowered to the maximum
    
    This is an implementation of part of the advice given
    in https://github.com/cockroachdb/docs/issues/9001 .
    The CPU requests/shares part of that equation cannot
    be implemented in Cockroach, so the docs issue remains
    unchanged.
    
    Release note (general change): Run fewer threads in parallel if
    running inside a container with a CPU limit.

--- pkg/util/cgroups/cgroups.go
+++ pkg/util/cgroups/cgroups.go
@@ -13,6 +13,7 @@ package cgroups
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"fmt"
 	"io/ioutil"
 	"math"
@@ -22,6 +23,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/errors"
 )
 
@@ -443,4 +445,21 @@ func getCgroupCPU(root string) (CPUUsage, error) {
 	}
 
 	return res, nil
+}
+
+// AdjustMaxProcs sets GOMAXPROCS (if not overridden by env variables) to be
+// the CPU limit of the current cgroup, if running inside a cgroup with a cpu
+// limit lower than runtime.NumCPU(). This is preferable to letting it fall back
+// to Go default, which is runtime.NumCPU(), as the Go scheduler would be
+// running more OS-level threads than can ever be concurrently scheduled.
+func AdjustMaxProcs(ctx context.Context) {
+	if _, set := os.LookupEnv("GOMAXPROCS"); !set {
+		if cpuInfo, err := GetCgroupCPU(); err == nil {
+			numCPUToUse := int(math.Ceil(cpuInfo.CPUShares()))
+			if numCPUToUse < runtime.NumCPU() && numCPUToUse > 0 {
+				log.Infof(ctx, "running in a container; setting GOMAXPROCS to %d", numCPUToUse)
+				runtime.GOMAXPROCS(numCPUToUse)
+			}
+		}
+	}
 }
