ed3240f5790d6d6e010d57a88592d21889bc3215
Paul Bardea <pbardea@gmail.com>
2020-12-10 13:55:32 -0500

    streaming: add mock streaming client
    This commit adds a mock implementation of the most basic version of a
    streaming client for cluster to cluster streaming in new `streamingccl`
    and `stream_client` packages.
    
    This change is intended to get the foundations in place to define the
    v0.1 version of the interface between components that reads the stream
    with the job that will ingest the data.
    
    Release note: None

---
+++ pkg/ccl/streamingccl/streamclient/client_test.go
@@ -0,0 +1,80 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package streamclient
+
+import (
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/stretchr/testify/require"
+)
+
+type mockStreamClient struct{}
+
+var _ Client = mockStreamClient{}
+
+// GetTopology implements the Client interface.
+func (sc mockStreamClient) GetTopology(_ StreamAddress) (Topology, error) {
+	return Topology{Partitions: []PartitionAddress{
+		"s3://my_bucket/my_stream/partition_1",
+		"s3://my_bucket/my_stream/partition_2",
+	}}, nil
+}
+
+// ConsumePartition implements the Client interface.
+func (sc mockStreamClient) ConsumePartition(_ PartitionAddress, _ time.Time) (chan Event, error) {
+	sampleKV := roachpb.KeyValue{
+		Key: []byte("key_1"),
+		Value: roachpb.Value{
+			RawBytes:  []byte("value 1"),
+			Timestamp: hlc.Timestamp{WallTime: 1},
+		},
+	}
+
+	events := make(chan Event, 100)
+	events <- MakeKVEvent(sampleKV)
+	events <- MakeCheckpointEvent(timeutil.Now())
+	close(events)
+
+	return events, nil
+}
+
+// TestExampleClientUsage serves as documentation to indicate how a stream
+// client could be used.
+func TestExampleClientUsage(t *testing.T) {
+	client := mockStreamClient{}
+	sa := StreamAddress("s3://my_bucket/my_stream")
+	topology, err := client.GetTopology(sa)
+	require.NoError(t, err)
+
+	startTimestamp := timeutil.Now()
+	numReceivedEvents := 0
+
+	for _, partition := range topology.Partitions {
+		eventCh, err := client.ConsumePartition(partition, startTimestamp)
+		require.NoError(t, err)
+
+		// This example looks for the closing of the channel to terminate the test,
+		// but an ingestion job should look for another event such as the user
+		// cutting over to the new cluster to move to the next stage.
+		for {
+			_, ok := <-eventCh
+			if !ok {
+				break
+			}
+			numReceivedEvents++
+		}
+	}
+
+	// We expect 4 events, 2 from each partition.
+	require.Equal(t, 4, numReceivedEvents)
+}
