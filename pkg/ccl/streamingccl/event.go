86373a1dc9e62f57f27a0090f20c0304bc7aa8f5
Paul Bardea <pbardea@gmail.com>
2020-12-10 13:55:32 -0500

    streaming: add mock streaming client
    This commit adds a mock implementation of the most basic version of a
    streaming client for cluster to cluster streaming in new `streamingccl`
    and `stream_client` packages.
    
    This change is intended to get the foundations in place to define the
    v0.1 version of the interface between components that reads the stream
    with the job that will ingest the data.
    
    Release note: None

---
+++ pkg/ccl/streamingccl/event.go
@@ -0,0 +1,62 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package streamingccl
+
+import (
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+)
+
+// YYY: This is nearly identical to the KV feed events we emit. These events
+// will most likely eventually be protobufs to serialize the entire event over
+// the stream connection.
+
+// EventType enumerates all possible events emitted over a cluster stream.
+type EventType int
+
+const (
+	// KVEvent indicates that the KV field of an event holds an updated KV which
+	// needs to be ingested.
+	KVEvent EventType = iota
+	// ResolvedEvent indicates that the Resolved field of an event holds a
+	// timestamp indicating that all events for this partition up to this
+	// timestamp have been emitted.
+	ResolvedEvent
+)
+
+// Event describes an event emitted by a cluster to cluster stream.
+// Its EventType field indicates which other fields are meaningful.
+type Event struct {
+	// EventType indicates which fields on the event will be meaningful.
+	EventType EventType
+
+	// KV is populated when eventType is KVEvent.
+	KV roachpb.KeyValue
+	// Resolved is populated when eventType is ResolvedEvent.
+	Resolved time.Time
+}
+
+// MakeKVEvent creates a KV typed Event.
+func MakeKVEvent(kv roachpb.KeyValue) Event {
+	return Event{
+		EventType: KVEvent,
+		KV:        kv,
+	}
+}
+
+// MakeResolvedEvent makes a resolved timestamp Event.
+func MakeResolvedEvent(resolvedTimestamp time.Time) Event {
+	return Event{
+		EventType: ResolvedEvent,
+		Resolved:  resolvedTimestamp,
+	}
+}
