6d38ec77b76cd2425a5b97c3900c5f7cb3ae4a43
Marius Posta <marius@cockroachlabs.com>
2020-12-16 17:57:08 -0500

    sql: change type hierarchy of TableDescriptor implementations
    Previously, it was possible to make changes to Indexes or Mutations in
    a descpb.TableDescriptor struct embedded in an Immutable itself embedded
    in a Mutable, without those changes being reflected in the Immutable's
    publicAndNonPublicIndexes field which is used as a cache by methods such
    as WritableIndexes(). Such methods could thus return outdated results.
    
    This patch addresses this source of bugs by instead embedding a new
    type, fully implementing the TableDescriptor interface, in both Mutable
    and Immutable. Immutable can now override some of these methods with
    cached implementations with no adverse repercussions on Mutable.
    
    Release note: None

--- pkg/sql/catalog/tabledesc/table_desc.go
+++ pkg/sql/catalog/tabledesc/table_desc.go
@@ -21,12 +21,177 @@ import (
 var _ catalog.TableDescriptor = (*Immutable)(nil)
 var _ catalog.TableDescriptor = (*Mutable)(nil)
 var _ catalog.MutableDescriptor = (*Mutable)(nil)
+var _ catalog.TableDescriptor = (*wrapper)(nil)
+
+// wrapper is the base implementation of the catalog.Descriptor
+// interface, which is overloaded by Immutable and Mutable.
+type wrapper struct {
+	descpb.TableDescriptor
+	postDeserializationChanges PostDeserializationTableDescriptorChanges
+}
+
+// NameResolutionResult implements the tree.NameResolutionResult interface.
+func (*wrapper) NameResolutionResult() {}
+
+// IsUncommittedVersion implements the catalog.Descriptor interface.
+func (*wrapper) IsUncommittedVersion() bool {
+	return false
+}
+
+// GetPostDeserializationChanges returns the set of changes which occurred to
+// this descriptor post deserialization.
+func (desc *wrapper) GetPostDeserializationChanges() PostDeserializationTableDescriptorChanges {
+	return desc.postDeserializationChanges
+}
+
+// PartialIndexOrds returns a set containing the ordinal of each partial index
+// defined on the table.
+func (desc *wrapper) PartialIndexOrds() util.FastIntSet {
+	var partialIndexOrds util.FastIntSet
+	for i, idx := range desc.DeletableIndexes() {
+		if idx.IsPartial() {
+			partialIndexOrds.Add(i)
+		}
+	}
+	return partialIndexOrds
+}
+
+// mutationIndexes returns all non-public indexes in the specified state.
+func (desc *wrapper) mutationIndexes(
+	mutationState descpb.DescriptorMutation_State,
+) []descpb.IndexDescriptor {
+	if len(desc.Mutations) == 0 {
+		return nil
+	}
+	indexes := make([]descpb.IndexDescriptor, 0, len(desc.Mutations))
+	for _, m := range desc.Mutations {
+		if m.State != mutationState {
+			continue
+		}
+		if idx := m.GetIndex(); idx != nil {
+			indexes = append(indexes, *idx)
+		}
+	}
+	return indexes
+}
+
+// DeleteOnlyIndexes returns a list of delete-only mutation indexes.
+func (desc *wrapper) DeleteOnlyIndexes() []descpb.IndexDescriptor {
+	return desc.mutationIndexes(descpb.DescriptorMutation_DELETE_ONLY)
+}
+
+// WritableIndexes returns a list of public and write-only mutation indexes.
+func (desc *wrapper) WritableIndexes() []descpb.IndexDescriptor {
+	if len(desc.Mutations) == 0 {
+		return desc.Indexes
+	}
+	indexes := make([]descpb.IndexDescriptor, 0, len(desc.Indexes)+len(desc.Mutations))
+	// Add all public indexes.
+	indexes = append(indexes, desc.Indexes...)
+	// Add all non-public writable indexes.
+	indexes = append(indexes, desc.mutationIndexes(descpb.DescriptorMutation_DELETE_AND_WRITE_ONLY)...)
+	return indexes
+}
+
+// DeletableIndexes implements the catalog.Descriptor interface.
+func (desc *wrapper) DeletableIndexes() []descpb.IndexDescriptor {
+	if len(desc.Mutations) == 0 {
+		return desc.Indexes
+	}
+	indexes := make([]descpb.IndexDescriptor, 0, len(desc.Indexes)+len(desc.Mutations))
+	// Add all writable indexes.
+	indexes = append(indexes, desc.WritableIndexes()...)
+	// Add all delete-only indexes.
+	indexes = append(indexes, desc.DeleteOnlyIndexes()...)
+	return indexes
+}
+
+// mutationColumns returns all non-public writable columns in the specified state.
+func (desc *wrapper) mutationColumns(
+	mutationState descpb.DescriptorMutation_State,
+) []descpb.ColumnDescriptor {
+	if len(desc.Mutations) == 0 {
+		return nil
+	}
+	columns := make([]descpb.ColumnDescriptor, 0, len(desc.Mutations))
+	for _, m := range desc.Mutations {
+		if m.State != mutationState {
+			continue
+		}
+		if col := m.GetColumn(); col != nil {
+			columns = append(columns, *col)
+		}
+	}
+	return columns
+}
+
+// DeletableColumns returns a list of public and non-public columns.
+func (desc *wrapper) DeletableColumns() []descpb.ColumnDescriptor {
+	if len(desc.Mutations) == 0 {
+		return desc.Columns
+	}
+	columns := make([]descpb.ColumnDescriptor, 0, len(desc.Columns)+len(desc.Mutations))
+	// Add writable columns.
+	columns = append(columns, desc.WritableColumns()...)
+	// Add delete-only columns.
+	columns = append(columns, desc.mutationColumns(descpb.DescriptorMutation_DELETE_ONLY)...)
+	return columns
+}
+
+// WritableColumns returns a list of public and write-only mutation columns.
+func (desc *wrapper) WritableColumns() []descpb.ColumnDescriptor {
+	if len(desc.Mutations) == 0 {
+		return desc.Columns
+	}
+	columns := make([]descpb.ColumnDescriptor, 0, len(desc.Columns)+len(desc.Mutations))
+	// Add public columns.
+	columns = append(columns, desc.Columns...)
+	// Add writable non-public columns.
+	columns = append(columns, desc.mutationColumns(descpb.DescriptorMutation_DELETE_AND_WRITE_ONLY)...)
+	return columns
+}
+
+// MutationColumns returns a list of mutation columns.
+func (desc *wrapper) MutationColumns() []descpb.ColumnDescriptor {
+	if len(desc.Mutations) == 0 {
+		return nil
+	}
+	columns := make([]descpb.ColumnDescriptor, 0, len(desc.Mutations))
+	// Add all writable non-public columns.
+	columns = append(columns, desc.mutationColumns(descpb.DescriptorMutation_DELETE_AND_WRITE_ONLY)...)
+	// Add all delete-only non-public columns.
+	columns = append(columns, desc.mutationColumns(descpb.DescriptorMutation_DELETE_ONLY)...)
+	return columns
+}
+
+// ActiveChecks returns a list of all check constraints that should be enforced
+// on writes (including constraints being added/validated). The columns
+// referenced by the returned checks are writable, but not necessarily public.
+func (desc *wrapper) ActiveChecks() []descpb.TableDescriptor_CheckConstraint {
+	checks := make([]descpb.TableDescriptor_CheckConstraint, len(desc.Checks))
+	for i, c := range desc.Checks {
+		checks[i] = *c
+	}
+	return checks
+}
+
+// GetColumnOrdinalsWithUserDefinedTypes returns a slice of column ordinals
+// of columns that contain user defined types.
+func (desc *wrapper) GetColumnOrdinalsWithUserDefinedTypes() []int {
+	var ords []int
+	for ord, col := range desc.DeletableColumns() {
+		if col.Type != nil && col.Type.UserDefined() {
+			ords = append(ords, ord)
+		}
+	}
+	return ords
+}
 
 // Immutable is a custom type for TableDescriptors
 // It holds precomputed values and the underlying TableDescriptor
 // should be const.
 type Immutable struct {
-	descpb.TableDescriptor
+	wrapper
 
 	// publicAndNonPublicCols is a list of public and non-public columns.
 	// It is partitioned by the state of the column: public, write-only, delete-only
@@ -44,17 +209,15 @@ type Immutable struct {
 	// partialIndexOrds contains the ordinal of each partial index.
 	partialIndexOrds util.FastIntSet
 
-	// ReadableColumns is a list of columns (including those undergoing a schema change)
+	// readableColumns is a list of columns (including those undergoing a schema change)
 	// which can be scanned. Columns in the process of a schema change
 	// are all set to nullable while column backfilling is still in
 	// progress, as mutation columns may have NULL values.
-	ReadableColumns []descpb.ColumnDescriptor
+	readableColumns []descpb.ColumnDescriptor
 
 	// columnsWithUDTs is a set of indexes into publicAndNonPublicCols containing
 	// indexes of columns that contain user defined types.
 	columnsWithUDTs []int
-
-	postDeserializationChanges PostDeserializationTableDescriptorChanges
 
 	// isUncommittedVersion is set to true if this descriptor was created from
 	// a copy of a Mutable with an uncommitted version.
@@ -65,44 +228,46 @@ type Immutable struct {
 	// outboundFKs []*ForeignKeyConstraint
 }
 
-// NameResolutionResult implements the tree.NameResolutionResult interface.
-func (*Immutable) NameResolutionResult() {}
-
 // IsUncommittedVersion implements the Descriptor interface.
 func (desc *Immutable) IsUncommittedVersion() bool {
 	return desc.isUncommittedVersion
 }
 
 // DescriptorProto prepares desc for serialization.
-func (desc *Immutable) DescriptorProto() *descpb.Descriptor {
+func (desc *wrapper) DescriptorProto() *descpb.Descriptor {
 	return &descpb.Descriptor{
 		Union: &descpb.Descriptor_Table{Table: &desc.TableDescriptor},
 	}
 }
 
 // GetPrimaryIndexID returns the ID of the primary index.
-func (desc *Immutable) GetPrimaryIndexID() descpb.IndexID {
+func (desc *wrapper) GetPrimaryIndexID() descpb.IndexID {
 	return desc.PrimaryIndex.ID
 }
 
 // GetPublicNonPrimaryIndexes returns the public non-primary indexes of the descriptor.
-func (desc *Immutable) GetPublicNonPrimaryIndexes() []descpb.IndexDescriptor {
-	return desc.GetIndexes()
+func (desc *wrapper) GetPublicNonPrimaryIndexes() []descpb.IndexDescriptor {
+	return desc.Indexes
 }
 
 // IsTemporary returns true if this is a temporary table.
-func (desc *Immutable) IsTemporary() bool {
+func (desc *wrapper) IsTemporary() bool {
 	return desc.GetTemporary()
 }
 
 // GetPublicColumns return the public columns in the descriptor.
-func (desc *Immutable) GetPublicColumns() []descpb.ColumnDescriptor {
+func (desc *wrapper) GetPublicColumns() []descpb.ColumnDescriptor {
 	return desc.Columns
 }
 
 // GetColumnAtIdx returns the column at the specified index.
-func (desc *Immutable) GetColumnAtIdx(idx int) *descpb.ColumnDescriptor {
+func (desc *wrapper) GetColumnAtIdx(idx int) *descpb.ColumnDescriptor {
 	return &desc.Columns[idx]
+}
+
+// ReadableColumns implements the catalog.TableDescriptor interface
+func (desc *Immutable) ReadableColumns() []descpb.ColumnDescriptor {
+	return desc.readableColumns
 }
 
 // ImmutableCopy implements the MutableDescriptor interface.
