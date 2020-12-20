c1349c390ce6910c4c0ed5ed26821dbe63270482
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2020-12-20 17:41:25 -0500

    roachtest/tpce: create a larger TPC-E test with 4 SSDs per node
    This commit introduces a new `tpce/c=100000/nodes=5` roachtest
    which uses a 5 node cluster of n1-standard-32 instances with 4
    local SSDs each.

--- pkg/cmd/roachtest/tpce.go
+++ pkg/cmd/roachtest/tpce.go
@@ -20,56 +20,85 @@ import (
 )
 
 func registerTPCE(r *testRegistry) {
-	const customers = 5000
-	const nodes = 3
-	const duration = 1 * time.Hour
-	r.Add(testSpec{
-		Name:    fmt.Sprintf("tpce/c=%d/nodes=%d", customers, nodes),
-		Owner:   OwnerKV,
-		Cluster: makeClusterSpec(nodes+1, cpu(4)),
-		Run: func(ctx context.Context, t *test, c *cluster) {
-			roachNodes := c.Range(1, nodes)
-			loadNode := c.Node(nodes + 1)
-			racks := nodes
+	type tpceOptions struct {
+		customers int
+		nodes     int
+		cpus      int
+		ssds      int
+	}
 
-			t.Status("installing cockroach")
-			c.Put(ctx, cockroach, "./cockroach", roachNodes)
-			c.Start(ctx, t, roachNodes, startArgs(fmt.Sprintf("--racks=%d", racks)))
+	runTPCE := func(ctx context.Context, t *test, c *cluster, opts tpceOptions) {
+		roachNodes := c.Range(1, opts.nodes)
+		loadNode := c.Node(opts.nodes + 1)
+		racks := opts.nodes
 
-			t.Status("installing docker")
-			if err := c.Install(ctx, t.l, loadNode, "docker"); err != nil {
+		t.Status("installing cockroach")
+		c.Put(ctx, cockroach, "./cockroach", roachNodes)
+		c.Start(ctx, t, roachNodes, startArgs(
+			fmt.Sprintf("--racks=%d", racks),
+			fmt.Sprintf("--store-count=%d", opts.ssds),
+		))
+
+		t.Status("installing docker")
+		if err := c.Install(ctx, t.l, loadNode, "docker"); err != nil {
+			t.Fatal(err)
+		}
+
+		// Configure to increase the speed of the import.
+		func() {
+			db := c.Conn(ctx, 1)
+			defer db.Close()
+			if _, err := db.ExecContext(
+				ctx, "SET CLUSTER SETTING kv.bulk_io_write.concurrent_addsstable_requests = $1", 4*opts.ssds,
+			); err != nil {
 				t.Fatal(err)
 			}
+		}()
 
-			m := newMonitor(ctx, c, roachNodes)
-			m.Go(func(ctx context.Context) error {
-				const dockerRun = `sudo docker run cockroachdb/tpc-e:latest`
+		m := newMonitor(ctx, c, roachNodes)
+		m.Go(func(ctx context.Context) error {
+			const dockerRun = `sudo docker run cockroachdb/tpc-e:latest`
 
-				roachNodeIPs := c.InternalIP(ctx, roachNodes)
-				roachNodeIPFlags := make([]string, len(roachNodeIPs))
-				for i, ip := range roachNodeIPs {
-					roachNodeIPFlags[i] = fmt.Sprintf("--hosts=%s", ip)
-				}
+			roachNodeIPs := c.InternalIP(ctx, roachNodes)
+			roachNodeIPFlags := make([]string, len(roachNodeIPs))
+			for i, ip := range roachNodeIPs {
+				roachNodeIPFlags[i] = fmt.Sprintf("--hosts=%s", ip)
+			}
 
-				t.Status("preparing workload")
-				c.Run(ctx, loadNode, fmt.Sprintf("%s --customers=%d --racks=%d --init %s",
-					dockerRun, customers, racks, roachNodeIPFlags[0]))
+			t.Status("preparing workload")
+			c.Run(ctx, loadNode, fmt.Sprintf("%s --customers=%d --racks=%d --init %s",
+				dockerRun, opts.customers, racks, roachNodeIPFlags[0]))
 
-				t.Status("running workload")
-				out, err := c.RunWithBuffer(ctx, t.l, loadNode,
-					fmt.Sprintf("%s --customers=%d --racks=%d --duration=%s %s",
-						dockerRun, customers, racks, duration, strings.Join(roachNodeIPFlags, " ")))
-				if err != nil {
-					t.Fatalf("%v\n%s", err, out)
-				}
-				outStr := string(out)
-				t.l.Printf("workload output:\n%s\n", outStr)
-				if strings.Contains(outStr, "Reported tpsE :    --   (not between 80% and 100%)") {
-					return errors.New("invalid tpsE fraction")
-				}
-				return nil
-			})
-			m.Wait()
-		},
-	})
+			t.Status("running workload")
+			const duration = 1 * time.Hour
+			out, err := c.RunWithBuffer(ctx, t.l, loadNode,
+				fmt.Sprintf("%s --customers=%d --racks=%d --duration=%s %s",
+					dockerRun, opts.customers, racks, duration, strings.Join(roachNodeIPFlags, " ")))
+			if err != nil {
+				t.Fatalf("%v\n%s", err, out)
+			}
+			outStr := string(out)
+			t.l.Printf("workload output:\n%s\n", outStr)
+			if strings.Contains(outStr, "Reported tpsE :    --   (not between 80% and 100%)") {
+				return errors.New("invalid tpsE fraction")
+			}
+			return nil
+		})
+		m.Wait()
+	}
+
+	for _, opts := range []tpceOptions{
+		{customers: 5_000, nodes: 3, cpus: 4, ssds: 1},
+		{customers: 100_000, nodes: 5, cpus: 32, ssds: 4},
+	} {
+		opts := opts
+		r.Add(testSpec{
+			Name:    fmt.Sprintf("tpce/c=%d/nodes=%d", opts.customers, opts.nodes),
+			Owner:   OwnerKV,
+			Cluster: makeClusterSpec(opts.nodes+1, cpu(opts.cpus), ssd(opts.ssds)),
+			Run: func(ctx context.Context, t *test, c *cluster) {
+				runTPCE(ctx, t, c, opts)
+			},
+		})
+	}
 }
