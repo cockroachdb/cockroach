cockroachdb/cockroach

commit 95b836dabc8e3dbf148b2c869b7db543647ec7a0

storage: implement iterator to interleave separated intents
intentInterleavingIter allows for both physically separate
and physically interleaved intents, and makes both look
like interleaved intents. It uses two underlying iterators,
a MVCCIterator and an EngineIterator, that are kept
synchronized.

Informs #41720

Release note: None

pkg/storage/pebble_iterator.go
@@ -399,6 +399,12 @@ func IsValidSplitKey(key roachpb.Key) bool {
 func (p *pebbleIterator) FindSplitKey(
 	start, end, minSplitKey roachpb.Key, targetSize int64,
 ) (MVCCKey, error) {
+	return findSplitKeyUsingIterator(p, start, end, minSplitKey, targetSize)
+}
+
+func findSplitKeyUsingIterator(
+	iter MVCCIterator, start, end, minSplitKey roachpb.Key, targetSize int64,
+) (MVCCKey, error) {
 	const timestampLen = 12
 
 	sizeSoFar := int64(0)
@@ -424,12 +430,16 @@ func (p *pebbleIterator) FindSplitKey(
 	// terminate iteration because the iterator's upper bound has already been
 	// set to end.
 	mvccMinSplitKey := MakeMVCCMetadataKey(minSplitKey)
-	p.SeekGE(MakeMVCCMetadataKey(start))
-	for ; p.iter.Valid(); p.iter.Next() {
-		mvccKey, err := DecodeMVCCKey(p.iter.Key())
+	iter.SeekGE(MakeMVCCMetadataKey(start))
+	for ; ; iter.Next() {
+		valid, err := iter.Valid()
 		if err != nil {
 			return MVCCKey{}, err
 		}
+		if !valid {
+			break
+		}
+		mvccKey := iter.UnsafeKey()
 
 		diff := targetSize - sizeSoFar
 		if diff < 0 {
@@ -470,7 +480,7 @@ func (p *pebbleIterator) FindSplitKey(
 			bestSplitKey.Key = append(bestSplitKey.Key[:0], prevKey.Key...)
 		}
 
-		sizeSoFar += int64(len(p.iter.Value()))
+		sizeSoFar += int64(len(iter.Value()))
 		if mvccKey.IsValue() && bytes.Equal(prevKey.Key, mvccKey.Key) {
 			// We only advanced timestamps, but not new mvcc keys.
 			sizeSoFar += timestampLen

pkg/keys/keys.go
@@ -393,22 +393,28 @@ func QueueLastProcessedKey(key roachpb.RKey, queue string) roachpb.Key {
 }
 
 // LockTableSingleKey creates a key under which all single-key locks for the
-// given key can be found. Note that there can be multiple locks for the given
-// key, but those are distinguished using the "version" which is not in scope
-// of the keys package.
+// given key can be found. buf is used as scratch-space to avoid allocations
+// -- its contents will be overwritten and not appended to.
+// Note that there can be multiple locks for the given key, but those are
+// distinguished using the "version" which is not in scope of the keys
+// package.
 // For a scan [start, end) the corresponding lock table scan is
 // [LTSK(start), LTSK(end)).
-func LockTableSingleKey(key roachpb.Key) roachpb.Key {
+func LockTableSingleKey(key roachpb.Key, buf []byte) (roachpb.Key, []byte) {
+	// The +3 accounts for the bytesMarker and terminator.
+	keyLen := len(LocalRangeLockTablePrefix) + len(LockTableSingleKeyInfix) + len(key) + 3
+	if cap(buf) < keyLen {
+		buf = make([]byte, 0, keyLen)
+	} else {
+		buf = buf[:0]
+	}
 	// Don't unwrap any local prefix on key using Addr(key). This allow for
 	// doubly-local lock table keys. For example, local range descriptor keys can
 	// be locked during split and merge transactions.
-	// The +3 account for the bytesMarker and terminator.
-	buf := make(roachpb.Key, 0,
-		len(LocalRangeLockTablePrefix)+len(LockTableSingleKeyInfix)+len(key)+3)
 	buf = append(buf, LocalRangeLockTablePrefix...)
 	buf = append(buf, LockTableSingleKeyInfix...)
 	buf = encoding.EncodeBytesAscending(buf, key)
-	return buf
+	return buf, buf
 }
 
 // DecodeLockTableSingleKey decodes the single-key lock table key to return the key
@@ -446,6 +452,12 @@ func DecodeLockTableSingleKey(key roachpb.Key) (lockedKey roachpb.Key, err error
 // claimed by a related (but not identical) concept.
 func IsLocal(k roachpb.Key) bool {
 	return bytes.HasPrefix(k, localPrefix)
+}
+
+// IsLocalStoreKey performs a cheap check that returns true iff the parameter
+// is a local store key.
+func IsLocalStoreKey(k roachpb.Key) bool {
+	return bytes.HasPrefix(k, localStorePrefix)
 }
 
 // Addr returns the address for the key, used to lookup the range containing the

pkg/keys/keys_test.go
@@ -718,7 +718,7 @@ func TestLockTableKeyEncodeDecode(t *testing.T) {
 	}
 	for _, test := range testCases {
 		t.Run("", func(t *testing.T) {
-			ltKey := LockTableSingleKey(test.key)
+			ltKey, _ := LockTableSingleKey(test.key, nil)
 			require.True(t, bytes.HasPrefix(ltKey, expectedPrefix))
 			k, err := DecodeLockTableSingleKey(ltKey)
 			require.NoError(t, err)

pkg/storage/BUILD.bazel
@@ -14,6 +14,7 @@ go_library(
         "error.go",
         "file_util.go",
         "in_mem.go",
+        "intent_interleaving_iter.go",
         "multi_iterator.go",
         "mvcc.go",
         "mvcc_incremental_iterator.go",
@@ -81,6 +82,7 @@ go_library(
         "//pkg/settings/cluster",
         "//pkg/storage/enginepb",
         "//pkg/storage/fs",
+        "//pkg/util",
         "//pkg/util/bufalloc",
         "//pkg/util/encoding",
         "//pkg/util/envutil",
@@ -116,6 +118,7 @@ go_test(
         "disk_map_test.go",
         "engine_key_test.go",
         "engine_test.go",
+        "intent_interleaving_iter_test.go",
         "main_test.go",
         "multi_iterator_test.go",
         "mvcc_history_test.go",
@@ -147,6 +150,7 @@ go_test(
         "//pkg/testutils",
         "//pkg/testutils/skip",
         "//pkg/testutils/zerofields",
+        "//pkg/util",
         "//pkg/util/caller",
         "//pkg/util/encoding",
         "//pkg/util/fileutil",

pkg/storage/engine.go
@@ -60,7 +60,8 @@ type SimpleMVCCIterator interface {
 	// NextKey advances the iterator to the next MVCC key. This operation is
 	// distinct from Next which advances to the next version of the current key
 	// or the next key if the iterator is currently located at the last version
-	// for a key.
+	// for a key. NextKey must not be used to switch iteration direction from
+	// reverse iteration to forward iteration.
 	NextKey()
 	// UnsafeKey returns the same value as Key, but the memory is invalidated on
 	// the next call to {Next,NextKey,Prev,SeekGE,SeekLT,Close}.
@@ -125,7 +126,8 @@ type MVCCIterator interface {
 	// and the encoded SST data specified, within the provided key range. Returns
 	// stats on skipped KVs, or an error if a collision is found.
 	CheckForKeyCollisions(sstData []byte, start, end roachpb.Key) (enginepb.MVCCStats, error)
-	// SetUpperBound installs a new upper bound for this iterator.
+	// SetUpperBound installs a new upper bound for this iterator. The caller can modify
+	// the parameter after this function returns.
 	SetUpperBound(roachpb.Key)
 	// Stats returns statistics about the iterator.
 	Stats() IteratorStats
@@ -294,7 +296,8 @@ type Reader interface {
 	NewMVCCIterator(iterKind MVCCIterKind, opts IterOptions) MVCCIterator
 	// NewEngineIterator returns a new instance of an EngineIterator over this
 	// engine. The caller must invoke EngineIterator.Close() when finished
-	// with the iterator to free resources.
+	// with the iterator to free resources. The caller can change IterOptions
+	// after this function returns.
 	NewEngineIterator(opts IterOptions) EngineIterator
 }
 

pkg/storage/engine_key.go
@@ -251,8 +251,9 @@ func (lk LockTableKey) ToEngineKey() EngineKey {
 	if lk.Strength != lock.Exclusive {
 		panic("unsupported lock strength")
 	}
+	ltKey, _ := keys.LockTableSingleKey(lk.Key, nil)
 	k := EngineKey{
-		Key:     keys.LockTableSingleKey(lk.Key),
+		Key:     ltKey,
 		Version: make([]byte, engineKeyVersionLockTableLen),
 	}
 	k.Version[0] = byte(lk.Strength)

pkg/storage/intent_interleaving_iter_test.go
@@ -1,0 +1,295 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package storage
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/concurrency/lock"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/storage/enginepb"
+	"github.com/cockroachdb/cockroach/pkg/util"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
+	"github.com/cockroachdb/cockroach/pkg/util/uint128"
+	"github.com/cockroachdb/cockroach/pkg/util/uuid"
+	"github.com/cockroachdb/datadriven"
+)
+
+// TODO(sumeer):
+// - randomized test with random intents that are interleaved and
+//   compare with no interleaved intents without using intentInterleavingIter.
+// - microbenchmarks to compare with non-interleaved intents.
+
+func scanRoachKey(t *testing.T, td *datadriven.TestData, field string) roachpb.Key {
+	var k string
+	td.ScanArgs(t, field, &k)
+	rk := roachpb.Key(k)
+	if strings.HasPrefix(k, "L") {
+		return append(keys.LocalRangePrefix, rk[1:]...)
+	}
+	return rk
+}
+
+func makePrintableKey(k MVCCKey) MVCCKey {
+	if bytes.HasPrefix(k.Key, keys.LocalRangePrefix) {
+		k.Key = append([]byte("L"), k.Key[len(keys.LocalRangePrefix):]...)
+	}
+	return k
+}
+
+func scanSeekKey(t *testing.T, td *datadriven.TestData) MVCCKey {
+	key := MVCCKey{Key: scanRoachKey(t, td, "k")}
+	if td.HasArg("ts") {
+		var ts int
+		td.ScanArgs(t, "ts", &ts)
+		key.Timestamp.WallTime = int64(ts)
+	}
+	return key
+}
+
+func checkAndOutputIter(iter MVCCIterator, b *strings.Builder) {
+	valid, err := iter.Valid()
+	if err != nil {
+		fmt.Fprintf(b, "output: err: %s\n", err)
+		return
+	}
+	if !valid {
+		fmt.Fprintf(b, "output: .\n")
+		return
+	}
+	k1 := makePrintableKey(iter.UnsafeKey())
+	k2 := makePrintableKey(iter.Key())
+	if !k1.Equal(k2) {
+		fmt.Fprintf(b, "output: key: %s != %s\n", k1, k2)
+		return
+	}
+	v1 := iter.UnsafeValue()
+	v2 := iter.Value()
+	if !bytes.Equal(v1, v2) {
+		fmt.Fprintf(b, "output: value: %x != %x\n", v1, v2)
+		return
+	}
+	if k1.Timestamp.IsEmpty() {
+		var meta enginepb.MVCCMetadata
+		if err := protoutil.Unmarshal(v1, &meta); err != nil {
+			fmt.Fprintf(b, "output: meta parsing: %s\n", err)
+			return
+		}
+		if meta.Timestamp.ToTimestamp().IsEmpty() {
+			fmt.Fprintf(b, "output: meta k=%s\n", string(k1.Key))
+		} else {
+			uuid := meta.Txn.ID.ToUint128()
+			var hiStr string
+			if uuid.Hi != 0 {
+				hiStr = fmt.Sprintf("%d,", uuid.Hi)
+			}
+			fmt.Fprintf(b, "output: meta k=%s ts=%d txn=%s%d\n",
+				string(k1.Key), meta.Timestamp.WallTime, hiStr, uuid.Lo)
+		}
+		return
+	}
+	fmt.Fprintf(b, "output: value k=%s ts=%d v=%s\n",
+		string(k1.Key), k1.Timestamp.WallTime, string(v1))
+}
+
+// TestIntentInterleavingIter is a datadriven test consisting of two commands:
+// - define: defines key-value pairs in the lock table and MVCC key spaces.
+//   Intents can be in both key spaces, and inline meta and MVCC values in
+//   the latter.
+//   meta k=<key> ts=<ts> txn=<txn>  defines an intent
+//   meta k=<key>                    defines an inline meta
+//   value k=<key> ts=<ts> v=<value> defines an MVCC value
+//   It is acceptable to define intents without provisional values to test
+//   out error checking code paths.
+// - iter: for iterating, is defined as
+//   iter [lower=<lower>] [upper=<upper>] [prefix=<true|false>]
+//   followed by newline separated sequence of operations:
+//     next, prev, seek-lt, seek-ge, set-upper, next-key
+//
+// A key starting with L is interpreted as a local-range key.
+func TestIntentInterleavingIter(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	var eng Engine
+	defer func() {
+		if eng != nil {
+			eng.Close()
+		}
+	}()
+
+	datadriven.Walk(t, "testdata/intent_interleaving_iter", func(t *testing.T, path string) {
+		if (util.RaceEnabled && strings.HasSuffix(path, "race_off")) ||
+			(!util.RaceEnabled && strings.HasSuffix(path, "race")) {
+			return
+		}
+		datadriven.RunTest(t, path, func(t *testing.T, d *datadriven.TestData) string {
+			switch d.Cmd {
+			case "define":
+				if eng != nil {
+					eng.Close()
+				}
+				eng = createTestPebbleEngine()
+				batch := eng.NewBatch()
+				var locksSection bool
+				var mvccSection bool
+				// pos is the original <file>:<lineno> prefix computed by
+				// datadriven. It points to the top "define" command itself.
+				// We editing d.Pos in-place below by extending `pos` upon
+				// each new line.
+				pos := d.Pos
+				for i, line := range strings.Split(d.Input, "\n") {
+					switch line {
+					case "locks":
+						locksSection = true
+						mvccSection = false
+						continue
+					case "mvcc":
+						locksSection = false
+						mvccSection = true
+						continue
+					}
+					// Compute a line prefix, to clarify error message. We
+					// prefix a newline character because some text editor do
+					// not know how to jump to the location of an error if
+					// there are multiple file:line prefixes on the same line.
+					d.Pos = fmt.Sprintf("\n%s: (+%d)", pos, i+1)
+					if !locksSection && !mvccSection {
+						t.Fatalf("%s: not a locks or mvcc section", d.Pos)
+					}
+					var err error
+					if d.Cmd, d.CmdArgs, err = datadriven.ParseLine(line); err != nil {
+						t.Fatalf("%s: %s", d.Pos, err)
+					}
+					switch d.Cmd {
+					case "meta":
+						key := scanRoachKey(t, d, "k")
+						// We don't bother populating most fields in the proto.
+						var meta enginepb.MVCCMetadata
+						var txnUUID uuid.UUID
+						if locksSection || d.HasArg("ts") {
+							var ts, txn int
+							d.ScanArgs(t, "ts", &ts)
+							meta.Timestamp.WallTime = int64(ts)
+							d.ScanArgs(t, "txn", &txn)
+							txnUUID = uuid.FromUint128(uint128.FromInts(0, uint64(txn)))
+							meta.Txn = &enginepb.TxnMeta{ID: txnUUID}
+						}
+						val, err := protoutil.Marshal(&meta)
+						if err != nil {
+							return err.Error()
+						}
+						if mvccSection {
+							// This is an abuse of PutUnversioned, but we know the
+							// implementation and can be sure that it will not change the key.
+							if err := batch.PutUnversioned(key, val); err != nil {
+								return err.Error()
+							}
+						} else {
+							ltKey := LockTableKey{Key: key, Strength: lock.Exclusive, TxnUUID: txnUUID[:]}
+							if err := batch.PutEngineKey(ltKey.ToEngineKey(), val); err != nil {
+								return err.Error()
+							}
+						}
+					case "value":
+						if locksSection {
+							t.Fatalf("%s: value in locks section", d.Pos)
+						}
+						key := scanRoachKey(t, d, "k")
+						var ts int
+						d.ScanArgs(t, "ts", &ts)
+						var value string
+						d.ScanArgs(t, "v", &value)
+						mvccKey := MVCCKey{Key: key, Timestamp: hlc.Timestamp{WallTime: int64(ts)}}
+						if err := batch.PutMVCC(mvccKey, []byte(value)); err != nil {
+							return err.Error()
+						}
+					}
+				}
+				d.Pos = pos
+				if err := batch.Commit(true); err != nil {
+					return err.Error()
+				}
+				return ""
+
+			case "iter":
+				var opts IterOptions
+				if d.HasArg("lower") {
+					opts.LowerBound = scanRoachKey(t, d, "lower")
+				}
+				if d.HasArg("upper") {
+					opts.UpperBound = scanRoachKey(t, d, "upper")
+				}
+				if d.HasArg("prefix") {
+					d.ScanArgs(t, "prefix", &opts.Prefix)
+				}
+				iter := newIntentInterleavingIterator(eng, opts)
+				var b strings.Builder
+				defer iter.Close()
+				// pos is the original <file>:<lineno> prefix computed by
+				// datadriven. It points to the top "define" command itself.
+				// We editing d.Pos in-place below by extending `pos` upon
+				// each new line.
+				pos := d.Pos
+				for i, line := range strings.Split(d.Input, "\n") {
+					// Compute a line prefix, to clarify error message. We
+					// prefix a newline character because some text editor do
+					// not know how to jump to the location of an error if
+					// there are multiple file:line prefixes on the same line.
+					d.Pos = fmt.Sprintf("\n%s: (+%d)", pos, i+1)
+					var err error
+					if d.Cmd, d.CmdArgs, err = datadriven.ParseLine(line); err != nil {
+						t.Fatalf("%s: %s", d.Pos, err)
+					}
+					switch d.Cmd {
+					case "seek-ge":
+						key := scanSeekKey(t, d)
+						iter.SeekGE(key)
+						fmt.Fprintf(&b, "seek-ge %s: ", makePrintableKey(key))
+						checkAndOutputIter(iter, &b)
+					case "next":
+						iter.Next()
+						fmt.Fprintf(&b, "next: ")
+						checkAndOutputIter(iter, &b)
+					case "seek-lt":
+						key := scanSeekKey(t, d)
+						iter.SeekLT(key)
+						fmt.Fprintf(&b, "seek-lt %s: ", makePrintableKey(key))
+						checkAndOutputIter(iter, &b)
+					case "prev":
+						iter.Prev()
+						fmt.Fprintf(&b, "prev: ")
+						checkAndOutputIter(iter, &b)
+					case "next-key":
+						iter.NextKey()
+						fmt.Fprintf(&b, "next-key: ")
+						checkAndOutputIter(iter, &b)
+					case "set-upper":
+						k := scanRoachKey(t, d, "k")
+						iter.SetUpperBound(k)
+						fmt.Fprintf(&b, "set-upper %s\n", string(makePrintableKey(MVCCKey{Key: k}).Key))
+					default:
+						fmt.Fprintf(&b, "unknown command: %s\n", d.Cmd)
+					}
+				}
+				d.Pos = pos
+				return b.String()
+			default:
+				return fmt.Sprintf("unknown command: %s", d.Cmd)
+			}
+		})
+	})
+}

commit 4731e0e6e4a95251f332d7abb2c3edb7c215cb0c

storage: avoid heap allocation per value in pebbleIterator.FindSplitKey
This was fallout from 95b836d.

```
name                                     old time/op    new time/op    delta
MVCCFindSplitKey_Pebble/valueSize=32-16    97.0ms ± 5%    76.5ms ±15%   -21.08%  (p=0.000 n=9+10)

name                                     old speed      new speed      delta
MVCCFindSplitKey_Pebble/valueSize=32-16   693MB/s ± 5%   881MB/s ±13%   +27.19%  (p=0.000 n=9+10)

name                                     old alloc/op   new alloc/op   delta
MVCCFindSplitKey_Pebble/valueSize=32-16    27.8MB ± 0%     0.0MB ±27%   -99.99%  (p=0.000 n=10+10)

name                                     old allocs/op  new allocs/op  delta
MVCCFindSplitKey_Pebble/valueSize=32-16      580k ± 0%        0k ± 0%  -100.00%  (p=0.000 n=10+9)
```

At the end of a 4 hour, 2.2 TB IMPORT of TPC-E, this was responsible for
15.96% of all heap allocations (by object).

pkg/storage/pebble_iterator.go
@@ -495,7 +495,7 @@ func findSplitKeyUsingIterator(
 			bestSplitKey.Key = append(bestSplitKey.Key[:0], prevKey.Key...)
 		}
 
-		sizeSoFar += int64(len(iter.Value()))
+		sizeSoFar += int64(len(iter.UnsafeValue()))
 		if mvccKey.IsValue() && bytes.Equal(prevKey.Key, mvccKey.Key) {
 			// We only advanced timestamps, but not new mvcc keys.
 			sizeSoFar += timestampLen