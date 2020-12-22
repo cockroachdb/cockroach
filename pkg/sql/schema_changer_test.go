51c2cda02ec80f6563273b0d0f8dcb362a8fd54e
Jayant Shrivastava <jayants@cockroachlabs.com>
2020-12-22 10:47:35 -0500

    sql: fix DDL after write error message
    Previously, the error message said "schema change statement
    cannot follow a statement that has written in the same
    transaction". This message is incorrect because DDLs after
    writes are allowed if the first schema change in a txn occurs
    before the first write. This change updates the error message
    to reflect the latter.
    
    Release note: None

--- pkg/sql/schema_changer_test.go
+++ pkg/sql/schema_changer_test.go
@@ -3579,7 +3579,7 @@ INSERT INTO t.kv VALUES ('a', 'b');
 			name:        `insert-create`,
 			firstStmt:   `INSERT INTO t.kv VALUES ('e', 'f')`,
 			secondStmt:  `CREATE INDEX foo2 ON t.kv (v)`,
-			expectedErr: `schema change statement cannot follow a statement that has written in the same transaction`,
+			expectedErr: `the first schema change statement in a transaction must precede any writes`,
 		},
 		// schema change at the end of a read only transaction.
 		{
