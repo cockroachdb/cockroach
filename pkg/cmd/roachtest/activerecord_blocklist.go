fd1326cc9f7dd6ae23c59e5d694cfc91c5925f8a
Rafi Shamim <rafi@cockroachlabs.com>
2020-12-22 15:43:50 -0500

    roachtest: test rails 6 in activerecord test
    This is the new version of the ActiveRecord adapter. It enables some
    tests that are not expected to pass on 20.1
    
    Release note: None

--- pkg/cmd/roachtest/activerecord_blocklist.go
+++ pkg/cmd/roachtest/activerecord_blocklist.go
@@ -31,7 +31,19 @@ var activeRecordBlockList21_1 = blocklist{}
 
 var activeRecordBlockList20_2 = blocklist{}
 
-var activeRecordBlockList20_1 = blocklist{}
+var activeRecordBlockList20_1 = blocklist{
+	"ActiveRecord::ConnectionAdapters::PostgreSQLAdapterTest#test_partial_index":          "9683",
+	"ActiveRecord::Migration::CompatibilityTest#test_migration_does_remove_unnamed_index": "9683",
+	"PostgresqlActiveSchemaTest#test_add_index":                                           "9683",
+	"PostgresqlEnumTest#test_assigning_enum_to_nil":                                       "24873",
+	"PostgresqlEnumTest#test_column":                                                      "24873",
+	"PostgresqlEnumTest#test_enum_defaults":                                               "24873",
+	"PostgresqlEnumTest#test_enum_mapping":                                                "24873",
+	"PostgresqlEnumTest#test_enum_type_cast":                                              "24873",
+	"PostgresqlEnumTest#test_invalid_enum_update":                                         "24873",
+	"PostgresqlEnumTest#test_no_oid_warning":                                              "24873",
+	"PostgresqlUUIDTest#test_add_column_with_default_array":                               "55320",
+}
 
 var activeRecordIgnoreList21_1 = blocklist{
 	"FixturesTest#test_create_fixtures": "flaky - FK constraint violated sometimes when loading all fixture data",
