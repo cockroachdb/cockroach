f446841d8ee06abb3948d547163b0b40948f8b36
David Taylor <tinystatemachine@gmail.com>
2020-12-22 15:20:51 +0000

    storageccl: add chunked-stream encrypted file support
    This adds a support for reading and writing encrypted files that
    rather than being a single message sealed with GCM, are chunked
    into a sequence of fixed-size messages which are each individually
    sealed.
    
    This is done to allow a reader to decrypt and process chunks in a
    streaming fashion rather than needing to read the entire file before
    decrypting it. Chunks are fixed size to allow for seeking to an offset
    in the clear-text, as its chunk can be computed deterministically.
    
    A chunk size of 1MB should add minimal overhead, such as the GCM tag
    that is appended to each chunk, while also providing a reasonable
    bound on the size of the buffer that a reader has to hold before it
    can decrypt and process.
    
    This patch only introduces support for the new encoding and extends the
    tests to cover it -- it does not change any existing callers to use it.
    
    Also while the approach was chosen with the intent to allow a seek
    within the reader, this initial patch does not yet implement the
    seek logic.
    
    Release note: none.

--- pkg/ccl/storageccl/encryption.go
+++ pkg/ccl/storageccl/encryption.go
@@ -14,6 +14,9 @@ import (
 	"crypto/cipher"
 	crypto_rand "crypto/rand"
 	"crypto/sha256"
+	"encoding/binary"
+	"io"
+	"io/ioutil"
 
 	"github.com/cockroachdb/errors"
 	"golang.org/x/crypto/pbkdf2"
@@ -33,7 +36,22 @@ import (
 var encryptionPreamble = []byte("encrypt")
 
 const encryptionSaltSize = 16
+
+// v1 is just the IV and then one sealed GCM message.
 const encryptionVersionIVPrefix = 1
+
+// v2 is an IV followed by 1 or more sealed GCM messages, representing chunks
+// of the original input. All but the last is of length 1<<20 in cleartext, and
+// that plus the gcm tag in the ciphertext.
+const encryptionVersionChunk1MB = 2
+
+// encryptionChunkSizeV2 is the chunk-size used by v2, i.e. 1mb, which should
+// minimize overhead while still while still limiting the size of buffers and
+// allowing seeks to mid-file.
+var encryptionChunkSizeV2 = 1 << 20  // 1MB
+const nonceSize = 12                 // GCM standard nonce
+const headerSize = 7 + 1 + nonceSize // preamble + version + iv
+const tagSize = 16                   // GCM standard tag
 
 // GenerateSalt generates a 16 byte random salt.
 func GenerateSalt() ([]byte, error) {
@@ -56,14 +74,18 @@ func AppearsEncrypted(text []byte) bool {
 }
 
 // EncryptFile encrypts a file with the supplied key and a randomly chosen IV
-// which is prepended in a header on the returned ciphertext. It is intended for
-// use on collections of separate data files that are all encrypted/decrypted
-// with the same key, such as BACKUP data files, and notably does _not_ include
-// information for key derivation as that is _not_ done for individual files in
-// such cases. See EncryptFileStoringSalt for a function that produces a
-// ciphertext that also includes the salt and thus supports decrypting with only
-// the passphrase (at the cost in including key derivation).
+// which is prepended in a header on the returned ciphertext.
 func EncryptFile(plaintext, key []byte) ([]byte, error) {
+	return encryptFile(plaintext, key, false)
+}
+
+// EncryptFileChunked is like EncryptFile but chunks the file into fixed-size
+// messages encrypted individually, allowing streaming (by-chunk) decryption.
+func EncryptFileChunked(plaintext, key []byte) ([]byte, error) {
+	return encryptFile(plaintext, key, true)
+}
+
+func encryptFile(plaintext, key []byte, chunked bool) ([]byte, error) {
 	gcm, err := aesgcm(key)
 	if err != nil {
 		return nil, err
@@ -71,56 +93,155 @@ func EncryptFile(plaintext, key []byte) ([]byte, error) {
 
 	// Allocate our output buffer: preamble + 1B version + iv, plus additional
 	// pre-allocated capacity for the ciphertext.
-	headerSize := len(encryptionPreamble) + 1 + gcm.NonceSize()
-	ciphertext := make([]byte, headerSize, headerSize+len(plaintext)+gcm.Overhead())
+
+	numChunks := 1
+	var version byte
+	// testing may set
+	if chunked {
+		version = encryptionVersionChunk1MB
+		numChunks = (len(plaintext) / encryptionChunkSizeV2) + 1
+	} else {
+		version = encryptionVersionIVPrefix
+	}
+
+	ciphertext := make([]byte, headerSize, headerSize+len(plaintext)+numChunks*gcm.Overhead())
 
 	// Write our header (preamble+version+IV) to the ciphertext buffer.
 	copy(ciphertext, encryptionPreamble)
-	ciphertext[len(encryptionPreamble)] = encryptionVersionIVPrefix
+	ciphertext[len(encryptionPreamble)] = version
+
 	// Pick a unique IV for this file and write it in the header.
-	iv := ciphertext[len(encryptionPreamble)+1:]
-	if _, err := crypto_rand.Read(iv); err != nil {
+	ivStart := len(encryptionPreamble) + 1
+	if _, err := crypto_rand.Read(ciphertext[ivStart : ivStart+nonceSize]); err != nil {
 		return nil, err
 	}
+	// Make a copy of the IV to increment for each chunk.
+	iv := append([]byte{}, ciphertext[ivStart:ivStart+nonceSize]...)
 
-	// Generate and write the actual ciphertext.
-	return gcm.Seal(ciphertext, iv, plaintext, nil), nil
+	for i := 0; i < len(plaintext); {
+		plainChunk := plaintext[i:]
+		if chunked && i+encryptionChunkSizeV2 < len(plaintext) {
+			plainChunk = plaintext[i : i+encryptionChunkSizeV2]
+		}
+		i += len(plainChunk)
+		ciphertext = gcm.Seal(ciphertext, iv, plainChunk, nil)
+		binary.BigEndian.PutUint64(iv[4:], binary.BigEndian.Uint64(iv[4:])+1)
+	}
+
+	return ciphertext, nil
 }
 
 // DecryptFile decrypts a file encrypted by EncryptFile, using the supplied key
 // and reading the IV from a prefix of the file. See comments on EncryptFile
 // for intended usage, and see DecryptFile
 func DecryptFile(ciphertext, key []byte) ([]byte, error) {
-	if !AppearsEncrypted(ciphertext) {
-		return nil, errors.New("file does not appear to be encrypted")
+	r, err := DecryptingReader(bytes.NewReader(ciphertext), key)
+	if err != nil {
+		return nil, err
 	}
-	ciphertext = ciphertext[len(encryptionPreamble):]
+	return ioutil.ReadAll(r)
+}
 
-	if len(ciphertext) < 1 {
-		return nil, errors.New("invalid encryption header")
-	}
-	version := ciphertext[0]
-	ciphertext = ciphertext[1:]
+type decryptReader struct {
+	ciphertext io.Reader
+	g          cipher.AEAD
+	fileIV     []byte
 
-	if version != encryptionVersionIVPrefix {
-		return nil, errors.Errorf("unexpected encryption scheme/config version %d", version)
-	}
+	ivScratch []byte
+	buf       []byte
+	pos       int
+	chunk     int64
+}
+
+// DecryptingReader returns a reader that decrypts on the fly with the given
+// key.
+func DecryptingReader(ciphertext io.Reader, key []byte) (io.Reader, error) {
 	gcm, err := aesgcm(key)
 	if err != nil {
 		return nil, err
 	}
 
-	ivSize := gcm.NonceSize()
-	if len(ciphertext) < ivSize {
-		return nil, errors.New("invalid encryption header: missing IV")
+	header := make([]byte, headerSize)
+	_, readHeaderErr := io.ReadFull(ciphertext, header)
+
+	// Verify that the read data does indeed look like an encrypted file and has
+	// a encoding version we can decode.
+	if !AppearsEncrypted(header) {
+		return nil, errors.New("file does not appear to be encrypted")
 	}
-	iv := ciphertext[:ivSize]
-	ciphertext = ciphertext[ivSize:]
-	plaintext, err := gcm.Open(ciphertext[:0], iv, ciphertext, nil)
-	if err != nil {
-		err = errors.Wrap(err, "failed to decrypt — maybe incorrect key")
+	if readHeaderErr != nil {
+		return nil, errors.Wrap(readHeaderErr, "invalid encryption header")
 	}
-	return plaintext, err
+
+	version := header[len(encryptionPreamble)]
+	if version < encryptionVersionIVPrefix || version > encryptionVersionChunk1MB {
+		return nil, errors.Errorf("unexpected encryption scheme/config version %d", version)
+	}
+	iv := header[len(encryptionPreamble)+1:]
+
+	// If this version is not chunked, need read it all to open it, and can just
+	// return a bytes.Reader over it.
+	if version == encryptionVersionIVPrefix {
+		buf, err := ioutil.ReadAll(ciphertext)
+		if err != nil {
+			return nil, err
+		}
+
+		buf, err = gcm.Open(buf[:0], iv, buf, nil)
+		return bytes.NewReader(buf), err
+	}
+	buf := make([]byte, nonceSize, int(encryptionChunkSizeV2)+tagSize+nonceSize)
+	ivScratch := buf[:nonceSize]
+	buf = buf[nonceSize:]
+	r := &decryptReader{g: gcm, fileIV: iv, ivScratch: ivScratch, ciphertext: ciphertext, buf: buf, chunk: -1}
+	return r, err
+}
+
+func (r *decryptReader) fill() error {
+	r.pos = 0
+	r.buf = r.buf[:cap(r.buf)]
+	r.chunk++
+	var read int
+	for read < len(r.buf) {
+		n, err := r.ciphertext.Read(r.buf[read:])
+		read += n
+
+		// If we've reached end and have read some ciphertext, need to decrypt it.
+		if err == io.EOF && read > 0 {
+			break
+		}
+		if err != nil {
+			return err
+		}
+	}
+	var err error
+	r.buf, err = r.g.Open(r.buf[:0], r.chunkIV(r.chunk), r.buf[:read], nil)
+	return err
+}
+
+func (r *decryptReader) chunkIV(num int64) []byte {
+	r.ivScratch = append(r.ivScratch[:0], r.fileIV...)
+	binary.BigEndian.PutUint64(r.ivScratch[4:], binary.BigEndian.Uint64(r.ivScratch[4:])+uint64(num))
+	return r.ivScratch
+}
+
+func (r *decryptReader) Read(p []byte) (int, error) {
+	if r.pos >= len(r.buf) {
+		if err := r.fill(); err != nil {
+			r.chunk = -1
+			return 0, err
+		}
+	}
+	read := copy(p, r.buf[r.pos:])
+	r.pos += read
+	return read, nil
+}
+
+func (r *decryptReader) Close() error {
+	if closer, ok := r.ciphertext.(io.Closer); ok {
+		return closer.Close()
+	}
+	return nil
 }
 
 func aesgcm(key []byte) (cipher.AEAD, error) {
