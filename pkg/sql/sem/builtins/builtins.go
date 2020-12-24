db717e3b9d3339136b1a665876031c21f29afda0
Rafi Shamim <rafi@cockroachlabs.com>
2020-12-23 19:32:49 -0500

    sql: fix substring(byte[]) to stop trying to escape raw bytes
    Release note (bug fix): The substring(byte[]) functions were not able to
    interpret bytes that had the `\` character since it was treating it as
    the beginning of an escape sequence. This is now fixed.

--- pkg/sql/sem/builtins/builtins.go
+++ pkg/sql/sem/builtins/builtins.go
@@ -4701,7 +4701,7 @@ var substringImpls = makeBuiltin(tree.FunctionProperties{Category: categoryStrin
 			byteString := string(*args[0].(*tree.DBytes))
 			start := int(tree.MustBeDInt(args[1]))
 			substring := getSubstringFromIndexBytes(byteString, start)
-			return tree.ParseDByte(substring)
+			return tree.NewDBytes(tree.DBytes(substring)), nil
 		},
 		Info:       "Returns a byte subarray of `input` starting at `start_pos` (count starts at 1).",
 		Volatility: tree.VolatilityImmutable,
@@ -4722,7 +4722,7 @@ var substringImpls = makeBuiltin(tree.FunctionProperties{Category: categoryStrin
 			if err != nil {
 				return nil, err
 			}
-			return tree.ParseDByte(substring)
+			return tree.NewDBytes(tree.DBytes(substring)), nil
 		},
 		Info: "Returns a byte subarray of `input` starting at `start_pos` (count starts at 1) and " +
 			"including up to `length` characters.",
