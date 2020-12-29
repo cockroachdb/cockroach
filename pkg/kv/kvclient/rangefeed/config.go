afbb3bd912ae8586363e801e532b1cd1b075712e
Andrew Werner <ajwerner@cockroachlabs.com>
2020-12-29 00:29:40 -0500

    kvclient/rangefeed: introduce package for using rangefeeds
    This commit introduces a new package underneath kvclient to simplify
    using rangefeeds. Namely, it provides sane retry and shutdown logic as well
    as support for performing an initial data scan. It also hides the nasty
    unwrapping of the DistSender as performed elsewhere.
    
    A longer-term vision here would be to support multiple spans and utilize this
    layer in the `kvfeed` of `changefeedccl`.
    
    Release note: None

---
+++ pkg/kv/kvclient/rangefeed/config.go
@@ -0,0 +1,72 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package rangefeed
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/util/retry"
+)
+
+// Option configures a RangeFeed.
+type Option interface {
+	set(*config)
+}
+
+type config struct {
+	retryOptions      retry.Options
+	onInitialScanDone OnInitialScanDone
+	withInitialScan   bool
+	withDiff          bool
+}
+
+type optionFunc func(*config)
+
+func (o optionFunc) set(c *config) { o(c) }
+
+// OnInitialScanDone is called when an initial scan is finished before any rows
+// from the rangefeed are supplied.
+type OnInitialScanDone func(ctx context.Context)
+
+// WithInitialScan enables an initial scan of the data in the span. The rows of
+// an initial scan will be passed to the value function used to construct the
+// RangeFeed. Upon completion of the initial scan, the passed function (if
+// non-nil) will be called. The initial scan may be restarted and thus rows
+// may be observed multiple times. The caller cannot rely on rows being returned
+// in order.
+func WithInitialScan(f OnInitialScanDone) Option {
+	return optionFunc(func(c *config) {
+		c.withInitialScan = true
+		c.onInitialScanDone = f
+	})
+}
+
+// WithDiff makes an option to enable an initial scan which defaults to
+// false.
+func WithDiff() Option {
+	return optionFunc(func(c *config) {
+		c.withDiff = true
+	})
+}
+
+// WithRetry configures the retry options for the rangefeed.
+func WithRetry(options retry.Options) Option {
+	return optionFunc(func(c *config) {
+		c.retryOptions = options
+	})
+}
+
+func initConfig(c *config, options []Option) {
+	*c = config{} // the default config is its zero value
+	for _, o := range options {
+		o.set(c)
+	}
+}
