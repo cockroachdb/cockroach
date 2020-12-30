59488332dc5101e90c01a166389cea370cf00beb
Aditya Maru <adityamaru@gmail.com>
2020-12-30 10:26:19 -0500

    streamingccl: add ingestion job framework
    This change introduces a new StreamIngestionJob. It does not do much
    more than laying out the general outline of the job, which is very
    similar to other bulk jobs such as changefeed, backup etc.
    
    More precisely:
    - Introduces StreamIngestionDetails job details proto
    - Hooks up the dependancy to a mock stream client
    - Introduces a StreamIngestionProcessorSpec
    - Sets up a simple DistSQL flow which round robin assigns the partitions
      to the processors.
    
    Most notable TODOs in job land which will be addressed in follow up PRs:
    - StreamIngestionPlanHook to create this job. Will involve figuring out
      SQL syntax.
    - Introducing a ts watermark in both the job and processors. This
      watermark will represent the lowest resolved ts which all processors
    have ingested till. Iron out semantics on job start and resumption.
    - Introducing a StreamIngestionFrontier processor which will slurp the
      results from the StreamIngestionProcessors, and use them to keep track
    of the minimum resolved ts across all processors.
    
    Release note: None

---
+++ pkg/ccl/streamingccl/streamclient/stream_client.go
@@ -0,0 +1,33 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package streamclient
+
+import "time"
+
+// client is a mock stream client.
+type client struct{}
+
+var _ Client = &client{}
+
+// NewStreamClient returns a new mock stream client.
+func NewStreamClient() Client {
+	return &client{}
+}
+
+// GetTopology implements the Client interface.
+func (m *client) GetTopology(address StreamAddress) (Topology, error) {
+	panic("unimplemented mock method")
+}
+
+// ConsumePartition implements the Client interface.
+func (m *client) ConsumePartition(
+	address PartitionAddress, startTime time.Time,
+) (chan Event, error) {
+	panic("unimplemented mock method")
+}
