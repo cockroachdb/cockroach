5797332b012da21d287efa143b77d22f76d8fb1b
Aditya Maru <adityamaru@gmail.com>
2020-12-30 10:26:19 -0500

    streamingccl: add ingestion job framework
    This change introduces a new StreamIngestionJob. It does not do much
    more than laying out the general outline of the job, which is very
    similar to other bulk jobs such as changefeed, backup etc.
    
    More precisely:
    - Introduces StreamIngestionDetails job details proto
    - Hooks up the dependancy to a mock stream client
    - Introduces a StreamIngestionProcessorSpec
    - Sets up a simple DistSQL flow which round robin assigns the partitions
      to the processors.
    
    Most notable TODOs in job land which will be addressed in follow up PRs:
    - StreamIngestionPlanHook to create this job. Will involve figuring out
      SQL syntax.
    - Introducing a ts watermark in both the job and processors. This
      watermark will represent the lowest resolved ts which all processors
    have ingested till. Iron out semantics on job start and resumption.
    - Introducing a StreamIngestionFrontier processor which will slurp the
      results from the StreamIngestionProcessors, and use them to keep track
    of the minimum resolved ts across all processors.
    
    Release note: None

--- pkg/ccl/streamingccl/stream_ingestion_job.go
+++ pkg/ccl/streamingccl/stream_ingestion_job.go
@@ -8,6 +8,89 @@
 
 package streamingccl
 
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/ccl/streamingccl/streamclient"
+	"github.com/cockroachdb/cockroach/pkg/jobs"
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/sql"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+)
+
+type streamIngestionResumer struct {
+	job *jobs.Job
+}
+
+func ingest(
+	ctx context.Context,
+	execCtx sql.JobExecContext,
+	streamAddress streamclient.PartitionAddress,
+	job *jobs.Job,
+) error {
+	// Initialize a stream client and resolve topology.
+	client := streamclient.NewStreamClient()
+	sa := streamclient.StreamAddress(streamAddress)
+	topology, err := client.GetTopology(sa)
+	if err != nil {
+		return err
+	}
+
+	evalCtx := execCtx.ExtendedEvalContext()
+	dsp := execCtx.DistSQLPlanner()
+
+	planCtx, nodes, err := dsp.SetupAllNodesPlanning(ctx, evalCtx, execCtx.ExecCfg())
+	if err != nil {
+		return err
+	}
+
+	// Construct stream ingestion processor specs.
+	streamIngestionSpecs, err := distStreamIngestionPlanSpecs(topology, nodes)
+	if err != nil {
+		return err
+	}
+
+	// Plan and run the DistSQL flow.
+	err = distStreamIngest(ctx, execCtx, nodes, planCtx, dsp, streamIngestionSpecs)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Resume is part of the jobs.Resumer interface.
+func (s *streamIngestionResumer) Resume(
+	ctx context.Context, execCtx interface{}, resultsCh chan<- tree.Datums,
+) error {
+	details := s.job.Details().(jobspb.StreamIngestionDetails)
+	p := execCtx.(sql.JobExecContext)
+
+	err := ingest(ctx, p, streamclient.PartitionAddress(details.StreamAddress), s.job)
+	if err != nil {
+		return err
+	}
+
+	// TODO(adityamaru): We probably want to use the resultsCh to indicate that
+	// the processors have completed setup. We can then return the job ID in the
+	// plan hook similar to how changefeeds do it.
+
+	return nil
+}
+
+// OnFailOrCancel is part of the jobs.Resumer interface.
+func (s *streamIngestionResumer) OnFailOrCancel(ctx context.Context, execCtx interface{}) error {
+	return nil
+}
+
+var _ jobs.Resumer = &streamIngestionResumer{}
+
 func init() {
-	// TODO: Implement me.
+	jobs.RegisterConstructor(
+		jobspb.TypeStreamIngestion,
+		func(job *jobs.Job,
+			settings *cluster.Settings) jobs.Resumer {
+			return &streamIngestionResumer{job: job}
+		})
 }
