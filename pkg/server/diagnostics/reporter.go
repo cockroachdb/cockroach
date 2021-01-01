c6227110522c8745e6e0875459bffdcdbd7d7e6f
Andrew Kimball <andyk@cockroachlabs.com>
2021-01-01 09:10:07 -0800

    server: add SQLInstanceInfo to diagnosticspb package
    This commit contains preparatory changes for supporting tenants in our
    diagnostics reporting code. Tenants run one or more SQL-only instances
    rather than full CRDB nodes.
    
    This commit is intended to be backported to a 20.2 patch release, so it
    isolates changes that might affect other production code that's unrelated
    to tenants. That includes adding an optional field to the DiagnosticsReport
    proto and updating BuildReportingURL to use it. It also renames sqlServer
    to SQLServer, so that it can be returned from the exported StartTenant
    method.
    
    Release note: None

---
+++ pkg/server/diagnostics/reporter.go
@@ -0,0 +1,24 @@
+// Copyright 2016 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package diagnostics
+
+import (
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/settings"
+)
+
+var ReportFrequency = settings.RegisterDurationSetting(
+	"diagnostics.reporting.interval",
+	"interval at which diagnostics data should be reported",
+	time.Hour,
+	settings.NonNegativeDuration,
+).WithPublic()
