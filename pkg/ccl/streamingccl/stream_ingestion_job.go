d8d05cad49f6f2e1b05adb38f4f0b306b98c6599
Aditya Maru <adityamaru@gmail.com>
2021-01-04 13:31:46 -0500

    streamingccl: add IngestionFrontierProcessor
    The IngestionFrontierProcessor (IFP) is fed rows from the IngestionProcessor.
    Each row represents a partition and the latest resolved ts event
    received for that partition. The IFP uses the existing util
    `spanFrontier` (used by changefeeds) to keep track of all the rows
    returned by the several ingestion procs, and resolves the minimum
    resolved ts for a particular partition across all procs. This is then
    written to the ingestion job progress indicating that all processors
    have ingested a stream upto a particular resolved ts.
    
    Release note: None

--- pkg/ccl/streamingccl/stream_ingestion_job.go
+++ pkg/ccl/streamingccl/stream_ingestion_job.go
@@ -30,7 +30,8 @@ func ingest(
 	ctx context.Context,
 	execCtx sql.JobExecContext,
 	streamAddress streamclient.PartitionAddress,
-	job *jobs.Job,
+	progress jobspb.Progress,
+	jobID int64,
 ) error {
 	// Initialize a stream client and resolve topology.
 	client := streamclient.NewStreamClient()
@@ -39,6 +40,12 @@ func ingest(
 	if err != nil {
 		return err
 	}
+
+	// TODO(adityamaru): If the job is being resumed it is possible that it has
+	// check-pointed a resolved ts up to which all of its processors had ingested
+	// KVs. We can skip to ingesting after this resolved ts. Plumb the
+	// initialHighwatermark to the ingestion processor spec based on what we read
+	// from the job progress.
 
 	evalCtx := execCtx.ExtendedEvalContext()
 	dsp := execCtx.DistSQLPlanner()
@@ -49,7 +56,8 @@ func ingest(
 	}
 
 	// Construct stream ingestion processor specs.
-	streamIngestionSpecs, err := distStreamIngestionPlanSpecs(topology, nodes)
+	streamIngestionSpecs, streamIngestionFrontierSpec, err := distStreamIngestionPlanSpecs(topology,
+		nodes, jobID)
 	if err != nil {
 		return err
 	}
@@ -57,7 +65,8 @@ func ingest(
 	// Plan and run the DistSQL flow.
 	// TODO: Updates from this flow need to feed back into the job to update the
 	// progress.
-	err = distStreamIngest(ctx, execCtx, nodes, planCtx, dsp, streamIngestionSpecs)
+	err = distStreamIngest(ctx, execCtx, nodes, jobID, planCtx, dsp, streamIngestionSpecs,
+		streamIngestionFrontierSpec)
 	if err != nil {
 		return err
 	}
@@ -72,7 +81,8 @@ func (s *streamIngestionResumer) Resume(
 	details := s.job.Details().(jobspb.StreamIngestionDetails)
 	p := execCtx.(sql.JobExecContext)
 
-	err := ingest(ctx, p, streamclient.PartitionAddress(details.StreamAddress), s.job)
+	err := ingest(ctx, p, streamclient.PartitionAddress(details.StreamAddress), s.job.Progress(),
+		*s.job.ID())
 	if err != nil {
 		return err
 	}
