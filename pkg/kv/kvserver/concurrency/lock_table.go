943829e56407762a7a7936f3b40dbad859f09ffc
Tobias Grieger <tobias.b.grieger@gmail.com>
2021-01-05 11:50:36 +0100

    concurrency: emit structured contention information to trace
    This change attaches a protobuf payload to the current Span whenever a
    request conflicts with another transaction. The payload contains the
    contending txn (i.e. the pushee) at the time at which it was first
    encountered, the key on which the conflict occurred (note that this is
    not necessarily the key at which the pushee is anchored) and the time
    spent waiting on the conflict (excluding intent resolution).
    
    This enables #57114.
    
    Touches #55583. I am not closing that issue yet because we also want
    to potentially track the outcome of the conflict.
    
    Release note: None

--- pkg/kv/kvserver/concurrency/lock_table.go
+++ pkg/kv/kvserver/concurrency/lock_table.go
@@ -80,7 +80,7 @@ type waitingState struct {
 	// Represents who the request is waiting for. The conflicting
 	// transaction may be a lock holder of a conflicting lock or a
 	// conflicting request being sequenced through the same lockTable.
-	txn  *enginepb.TxnMeta // always non-nil
+	txn  *enginepb.TxnMeta // always non-nil in waitFor{,Distinguished}
 	key  roachpb.Key       // the key of the conflict
 	held bool              // is the conflict a held lock?
 
