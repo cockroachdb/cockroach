e643b58b60a1a2f5ff600d1b689f4595cdff1839
Tobias Grieger <tobias.b.grieger@gmail.com>
2021-01-05 14:43:49 +0100

    kvnemesis: skip under race
    Closes #58443.
    
    Release note: None

--- pkg/kv/kvnemesis/kvnemesis_test.go
+++ pkg/kv/kvnemesis/kvnemesis_test.go
@@ -19,6 +19,7 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/kv"
+	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
@@ -29,6 +30,8 @@ import (
 
 func TestKVNemesisSingleNode(t *testing.T) {
 	defer leaktest.AfterTest(t)()
+	skip.UnderRace(t)
+
 	defer log.Scope(t).Close(t)
 
 	ctx := context.Background()
@@ -52,6 +55,8 @@ func TestKVNemesisSingleNode(t *testing.T) {
 
 func TestKVNemesisMultiNode(t *testing.T) {
 	defer leaktest.AfterTest(t)()
+	skip.UnderRace(t)
+
 	defer log.Scope(t).Close(t)
 
 	// 4 nodes so we have somewhere to move 3x replicated ranges to.
