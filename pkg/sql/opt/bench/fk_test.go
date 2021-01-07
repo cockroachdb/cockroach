a628983b38898daf417da3ed380fb565150b4865
Marcus Gartner <marcus@cockroachlabs.com>
2021-01-06 16:07:06 -0800

    opt: suppress logs in benchmarks
    As of #57134 passing `-logtostderr=false` as a `TESTFLAG` in benchmarks
    errs: `flag provided but not defined: -logtostderr`. The preferred
    method for suppressing logs in tests and benchmarks to is add
    `defer log.Scope(t).Close(t)` to the top of the test/benchmark
    (see #57979).
    
    This commit uses this new method to suppress logs in optimizer
    benchmarks.
    
    Release note: None

--- pkg/sql/opt/bench/fk_test.go
+++ pkg/sql/opt/bench/fk_test.go
@@ -19,6 +19,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 )
 
 func runFKBench(
@@ -53,6 +54,8 @@ func runFKBench(
 }
 
 func BenchmarkFKInsert(b *testing.B) {
+	defer log.Scope(b).Close(b)
+
 	const parentRows = 1000
 	setup := func(b *testing.B, r *sqlutils.SQLRunner, setupFKs bool) {
 		r.Exec(b, "CREATE TABLE child (k int primary key, p int)")
