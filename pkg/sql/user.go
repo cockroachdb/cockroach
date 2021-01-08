4106e335e8a70447e1353fe40d91330667cca7c7
Rafi Shamim <rafi@cockroachlabs.com>
2021-01-08 15:00:28 -0500

    sql: fix performance regression in user authn
    The authn code needs to query system.users and system.role_options.
    These queries are run by the internal executor, which has a current DB
    of "". This causes the name to be resolved as "".system.users and
    "".system.role_options. The lookup for the "" DB always fails, but that
    result is not cached, so the lookup occurs on every authn attempt. There
    is fallback logic that then looks up the correct name.
    
    Now we specify the fully-qualified 3-part name for these two queries.
    
    To show that this fix is important, new benchmarks are added to the
    bench/ddl_analysis tests.
    
    Release note (bug fix): The user authentication flow no longer performs
    extraneous name lookups. This performance regression was present since
    v20.2.

--- pkg/sql/user.go
+++ pkg/sql/user.go
@@ -111,7 +111,8 @@ func retrieveUserAndPassword(
 
 	// Perform the lookup with a timeout.
 	err = runFn(func(ctx context.Context) error {
-		const getHashedPassword = `SELECT "hashedPassword" FROM system.users ` +
+		// Use fully qualified table name to avoid looking up "".system.users.
+		const getHashedPassword = `SELECT "hashedPassword" FROM system.public.users ` +
 			`WHERE username=$1`
 		values, err := ie.QueryRowEx(
 			ctx, "get-hashed-pwd", nil, /* txn */
@@ -131,7 +132,8 @@ func retrieveUserAndPassword(
 			return nil
 		}
 
-		getLoginDependencies := `SELECT option, value FROM system.role_options ` +
+		// Use fully qualified table name to avoid looking up "".system.role_options.
+		getLoginDependencies := `SELECT option, value FROM system.public.role_options ` +
 			`WHERE username=$1 AND option IN ('NOLOGIN', 'VALID UNTIL')`
 
 		loginDependencies, err := ie.QueryEx(
