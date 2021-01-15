aaa8340612cde4099a2e2850b44f9e1fa3bedec4
Marius Posta <marius@cockroachlabs.com>
2021-01-15 13:51:09 -0500

    sql: set running status for GC jobs
    Previously GC jobs didn't populate their RunningStatus leaving users
    confused. This change addresses this by setting it to
    sql.RunningStatusWaitingGC, as it was pre-20.1
    
    Fixes #57826.
    
    Release note (bug fix): GC jobs now populate the running_status column
    for SHOW JOBS. This bug has been present since v20.1.

--- pkg/sql/schema_changer.go
+++ pkg/sql/schema_changer.go
@@ -1854,6 +1854,7 @@ func CreateGCJobRecord(
 		DescriptorIDs: descriptorIDs,
 		Details:       details,
 		Progress:      jobspb.SchemaChangeGCProgress{},
+		RunningStatus: RunningStatusWaitingGC,
 		NonCancelable: true,
 	}
 }
