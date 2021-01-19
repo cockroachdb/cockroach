4027db3cc197f9425ff9f027297ba1ed1ab77211
Rebecca Taft <becca@cockroachlabs.com>
2021-01-19 11:04:24 -0600

    opt: fix bug where ON CONFLICT DO NOTHING ignores some input
    Prior to this patch, it was possible for some valid input to an
    INSERT ... ON CONFLICT DO NOTHING to be discarded. For example,
    consider the following example:
    
    CREATE TABLE uniq (
      k INT PRIMARY KEY,
      v INT UNIQUE,
      w INT UNIQUE,
      x INT,
      y INT DEFAULT 5,
      UNIQUE (x, y)
    );
    
    INSERT INTO uniq VALUES (1, 1, 1, 1, 1);
    
    INSERT INTO uniq VALUES (1, 20, 20, 20, 20),
                            (20, 1, 20, 20, 20),
                            (20, 20, 20, 20, 20)
    ON CONFLICT DO NOTHING;
    
    Since row (20, 20, 20, 20, 20) does not conflict with the existing
    row in uniq, it should be inserted. However, prior to this patch, all
    three rows in the second INSERT statement were discarded.
    
    This commit fixes the problem by applying the upsert-distinct-on
    operators for each index after all conflicting rows are removed by
    anti-joins.
    
    Fixes #59125
    
    Release note (bug fix): Fixed a bug in which some non-conflicting rows
    provided as input to an INSERT ... ON CONFLICT DO NOTHING statement could
    be discarded, and not inserted. This could happen in cases where the
    table had one or more unique indexes in addition to the primary index,
    and some of the rows in the input conflicted with existing values in one
    or more unique index. This scenario could cause the rows that did not
    conflict to be erroneously discarded. This has now been fixed.

--- pkg/sql/opt/optbuilder/insert.go
+++ pkg/sql/opt/optbuilder/insert.go
@@ -763,6 +763,19 @@ func (mb *mutationBuilder) buildInputForDoNothing(
 			on,
 			memo.EmptyJoinPrivate,
 		)
+	}
+
+	// Loop over each arbiter index, creating an upsert-distinct-on for each one.
+	// This must happen after all conflicting rows are removed with the anti-joins
+	// created above, to avoid removing valid rows (see #59125).
+	for idx, idxCount := 0, mb.tab.IndexCount(); idx < idxCount; idx++ {
+		// Skip non-arbiter indexes.
+		if !arbiterIndexes.Contains(idx) {
+			continue
+		}
+
+		index := mb.tab.Index(idx)
+		_, isPartial := index.Predicate()
 
 		// If the index is a partial index, project a new column that allows the
 		// UpsertDistinctOn to only de-duplicate insert rows that satisfy the
