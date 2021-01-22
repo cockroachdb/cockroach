00b4f10605d5f84f4daacb05e50dfdd1db021195
Andrew Werner <ajwerner@cockroachlabs.com>
2021-01-21 21:42:59 -0500

    workload/schemachange: stop swallowing all errors during op generation
    Transaction restart errors must be rolled all the way back. Not doing that
    means the workload totally fails. Also, we want to treat uncategorized errors
    in generation as a real problem.
    
    Release note: None

--- pkg/workload/schemachange/schemachange.go
+++ pkg/workload/schemachange/schemachange.go
@@ -340,22 +340,18 @@ func (w *schemaChangeWorker) runInTxn(tx *pgx.Tx) error {
 			break
 		}
 
-		op, noops, err := w.opGen.randOp(tx)
-		if w.logger.verbose >= 2 {
-			for _, noop := range noops {
-				w.logger.writeLog(noop)
-			}
-		}
+		op, err := w.opGen.randOp(tx)
 
-		w.logger.addExpectedErrors(w.opGen.expectedExecErrors, w.opGen.expectedCommitErrors)
-
-		if err != nil {
+		if pgErr := (pgx.PgError{}); errors.As(err, &pgErr) && pgcode.MakeCode(pgErr.Code) == pgcode.SerializationFailure {
+			return errors.Mark(err, errRunInTxnRbkSentinel)
+		} else if err != nil {
 			return errors.Mark(
 				errors.Wrap(err, "***UNEXPECTED ERROR; Failed to generate a random operation"),
 				errRunInTxnFatalSentinel,
 			)
 		}
 
+		w.logger.addExpectedErrors(w.opGen.expectedExecErrors, w.opGen.expectedCommitErrors)
 		w.logger.writeLog(op)
 		if !w.dryRun {
 			start := timeutil.Now()
