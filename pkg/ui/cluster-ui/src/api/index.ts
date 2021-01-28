a35b0c6ca6b2a88dd767e1055baec5761c316508
Andrii Vorobiov <undeclarede@gmail.com>
2021-01-28 10:37:06 +0200

    Revert "cluster-ui: nodes redux functionality"
    This reverts commit 1fba624507dcce2892ece8b7da80c123dd7b2111.

--- pkg/ui/cluster-ui/src/api/index.ts
+++ pkg/ui/cluster-ui/src/api/index.ts
@@ -2,4 +2,3 @@ export * from "./fetchData";
 export * from "./statementDiagnosticsApi";
 export * from "./statementsApi";
 export * from "./basePath";
-export * from "./nodesApi";
--- pkg/ui/cluster-ui/src/store/reducers.ts
+++ pkg/ui/cluster-ui/src/store/reducers.ts
@@ -5,15 +5,11 @@ import {
   StatementDiagnosticsState,
   reducer as statementDiagnostics,
 } from "./statementDiagnostics";
-import { NodesState, reducer as nodes } from "./nodes";
-import { LivenessState, reducer as liveness } from "./liveness";
 
 export type AdminUiState = {
   statements: StatementsState;
   statementDiagnostics: StatementDiagnosticsState;
   localStorage: LocalStorageState;
-  nodes: NodesState;
-  liveness: LivenessState;
 };
 
 export type AppState = {
@@ -24,6 +20,4 @@ export const rootReducer = combineReducers<AdminUiState>({
   localStorage,
   statementDiagnostics,
   statements,
-  nodes,
-  liveness,
 });
