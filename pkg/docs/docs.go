03798d0093d64cd0d38a804e2628354a6fb4a525
arulajmani <arulajmani@gmail.com>
2021-01-28 13:36:02 -0500

    sql: add link to deprecation docs for cross db references in the error
    Closes #57782
    
    Release note (sql change): error messages for cross-database links now
    include a hint directing to the user to the deprecation docs. An
    example message looks like:
    
    ```
    ERROR: the view cannot refer to other databases; (see the 'sql.cross_db_views.enabled' cluster setting)
    SQLSTATE: 0A000
    HINT: Note that cross database references will be removed in future releases. See: https://www.cockroachlabs.com/docs/releases/v21.1.0.html#deprecations
    ```

--- pkg/docs/docs.go
+++ pkg/docs/docs.go
@@ -10,12 +10,25 @@
 
 package docs
 
-import "github.com/cockroachdb/cockroach/pkg/build"
+import (
+	"fmt"
+
+	"github.com/cockroachdb/cockroach/pkg/build"
+)
 
 // URLBase is the root URL for the version of the docs associated with this
 // binary.
 var URLBase = "https://www.cockroachlabs.com/docs/" + build.VersionPrefix()
 
+// URLReleaseNotesBase is the root URL for the release notes for the .0 patch
+// release associated with this binary.
+var URLReleaseNotesBase = fmt.Sprintf("https://www.cockroachlabs.com/docs/releases/%s.0.html",
+	build.VersionPrefix())
+
 // URL generates the URL to pageName in the version of the docs associated
 // with this binary.
 func URL(pageName string) string { return URLBase + "/" + pageName }
+
+// ReleaseNotesURL generates the URL to pageName in the .0 patch release notes
+// docs associated with this binary.
+func ReleaseNotesURL(pageName string) string { return URLReleaseNotesBase + pageName }
