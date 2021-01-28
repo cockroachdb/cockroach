5db31519fb61a8192face620c9f969b59423629d
Ricky Stewart <ricky@cockroachlabs.com>
2021-01-28 17:00:51 -0600

    build: upgrade to ubuntu focal in builder images
    This necessitated a few cascading changes:
    
    * `crosstool-ng` v1.23 doesn't build out of the box on the latest
      Ubuntu, so I had to update to 1.24.
    * The `DEFCONFIG` format isn't back-compatible between minor versions of
      `crosstool-ng`, so I had to deal with that as well. Luckily,
      `crosstool-ng` has an upgrade path. I used the *old* builder image
      with v1.23 to run `ct-ng defconfig` to generate `.config` files for
      the different build configurations, and then used the *new* version to
      run `ct-ng upgradeconfig` to produce updated `.config`s. I then
      updated the contents of the `.defconfig`s in-tree and renamed the
      files accordingly. As a result we no longer have to run `defconfig`
      during the build. If we need to update to a newer `crosstool-ng` at
      some point in the future, we'll have to follow a similar procedure
      (i.e. we'll have to run `ct-ng upgradeconfig` once for each of these
      configs).
      Ref: http://crosstool-ng.github.io/docs/configuration/
    * Upgrade to  `clang` 10, and to a compatible `llvm` for the `msan`
      build. The directory structure has changed slightly, so update some
      of the build commands to account for that. (There's still a build
      breakage with v11: https://github.com/shirou/gopsutil/issues/976)
    
    Also, make corresponding changes in `build/bazelbuilder` so there isn't
    skew between the two images.
    
    Fixed #59432.
    
    Release note: None

--- build/teamcity-bazel.sh
+++ build/teamcity-bazel.sh
@@ -15,5 +15,5 @@ docker run -i ${tty-} --rm --init \
        --workdir="/go/src/github.com/cockroachdb/cockroach" \
        -v "$root:/go/src/github.com/cockroachdb/cockroach:ro" \
        -v "$TMPDIR:/artifacts" \
-       cockroachdb/bazel:20210126-114949 bazelbuild.sh
+       cockroachdb/bazel:20210201-174432 bazelbuild.sh
 tc_end_block "Run Bazel build"
