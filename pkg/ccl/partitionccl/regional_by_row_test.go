298e651a6a1318c7a3727e88b7d8961a7f06fb0e
Oliver Tan <otan@cockroachlabs.com>
2021-02-02 18:13:21 +1100

    sql: add test for ALTER TABLE ... LOCALITY REGIONAL BY ROW / CANCEL
    Since LOCALITY REGIONAL BY ROW requires CCL, we have to move some test
    utilities to sqltestutils and test from a CCL package. This commit
    covers the relevant moves as well as adding the new test.
    
    Release note: None

---
+++ pkg/ccl/partitionccl/regional_by_row_test.go
@@ -0,0 +1,136 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package partitionccl_test
+
+import (
+	"context"
+	gosql "database/sql"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/jobs"
+	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/sql"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catalogkv"
+	"github.com/cockroachdb/cockroach/pkg/sql/execinfra"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqltestutils"
+	"github.com/cockroachdb/cockroach/pkg/sql/tests"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/util"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/errors"
+)
+
+// TestAlterToRegionalByRowChangeWithCancel tests an alteration to REGIONAL
+// BY ROW AS <col> which gets cancelled.
+// It is defined in partitionccl as it requires CCL to operate.
+func TestAlterToRegionalByRowAsChangeWithCancel(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	// Decrease the adopt loop interval so that retries happen quickly.
+	defer sqltestutils.SetTestJobsAdoptInterval()()
+
+	var chunkSize int64 = 100
+	var maxValue = 4000
+	if util.RaceEnabled {
+		// Race builds are a lot slower, so use a smaller number of rows.
+		maxValue = 200
+		chunkSize = 5
+	}
+
+	ctx := context.Background()
+	var db *gosql.DB
+	shouldCancel := false
+	params, _ := tests.CreateTestServerParams()
+	params.Locality.Tiers = []roachpb.Tier{
+		{Key: "region", Value: "ajstorm-1"},
+	}
+	params.Knobs = base.TestingKnobs{
+		SQLSchemaChanger: &sql.SchemaChangerTestingKnobs{
+			BackfillChunkSize: chunkSize,
+		},
+		DistSQL: &execinfra.TestingKnobs{
+			RunBeforeBackfillChunk: func(sp roachpb.Span) error {
+				if !shouldCancel {
+					return nil
+				}
+				if _, err := db.Exec(`CANCEL JOB (
+					SELECT job_id FROM [SHOW JOBS]
+					WHERE
+						job_type = 'SCHEMA CHANGE' AND
+						status = $1 AND
+						description NOT LIKE 'ROLL BACK%'
+				)`, jobs.StatusRunning); err != nil {
+					t.Error(err)
+				}
+				return nil
+			},
+		},
+	}
+	s, sqlDB, kvDB := serverutils.StartServer(t, params)
+	db = sqlDB
+	defer s.Stopper().Stop(ctx)
+
+	// Disable strict GC TTL enforcement because we're going to shove a zero-value
+	// TTL into the system with AddImmediateGCZoneConfig.
+	defer sqltestutils.DisableGCTTLStrictEnforcement(t, sqlDB)()
+
+	if _, err := sqlDB.Exec(`
+CREATE DATABASE t PRIMARY REGION "ajstorm-1";
+CREATE TABLE t.test (k INT NOT NULL, v INT) LOCALITY GLOBAL;
+`); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := sqltestutils.BulkInsertIntoTable(sqlDB, maxValue); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := sqlDB.Exec(`
+		ALTER TABLE t.test ADD COLUMN cr t.crdb_internal_region
+		NOT NULL
+		DEFAULT gateway_region()::t.crdb_internal_region
+	`); err != nil {
+		t.Fatal(err)
+	}
+	// This will fail, so we don't want to check the error.
+	shouldCancel = true
+	_, _ = sqlDB.Exec(`ALTER TABLE t.test SET LOCALITY REGIONAL BY ROW AS cr`)
+
+	// Ensure that the mutations corresponding to the primary key change are cleaned up and
+	// that the job did not succeed even though it was canceled.
+	testutils.SucceedsSoon(t, func() error {
+		tableDesc := catalogkv.TestingGetTableDescriptor(kvDB, keys.SystemSQLCodec, "t", "test")
+		if len(tableDesc.GetMutations()) != 0 {
+			return errors.Errorf("expected 0 mutations after cancellation, found %d", len(tableDesc.GetMutations()))
+		}
+		if tableDesc.GetPrimaryIndex().NumColumns() != 1 || tableDesc.GetPrimaryIndex().GetColumnName(0) != "rowid" {
+			return errors.Errorf("expected primary key change to not succeed after cancellation")
+		}
+		if !tableDesc.IsLocalityGlobal() {
+			return errors.Errorf("expected locality to be global")
+		}
+		return nil
+	})
+
+	// Stop any further attempts at cancellation, so the GC jobs don't fail.
+	shouldCancel = false
+	tableDesc := catalogkv.TestingGetTableDescriptor(kvDB, keys.SystemSQLCodec, "t", "test")
+	if _, err := sqltestutils.AddImmediateGCZoneConfig(db, tableDesc.GetID()); err != nil {
+		t.Fatal(err)
+	}
+	// Ensure that the writes from the partial new indexes are cleaned up.
+	testutils.SucceedsSoon(t, func() error {
+		return sqltestutils.CheckTableKeyCount(ctx, kvDB, 1, maxValue)
+	})
+}
