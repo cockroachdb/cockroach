8e4af7308278cb505e1d59e06360f818d2fcb055
Ricky Stewart <ricky@cockroachlabs.com>
2021-02-04 14:48:00 -0600

    bazel: don't unconditionally run tests with `-test.v`
    In a follow-up discussion on #59736, it was determined that this was an
    unacceptable UX regression for dev scenarios. Instead, turn this on only
    in CI.
    
    Release note: None

--- build/teamcity-bazel.sh
+++ build/teamcity-bazel.sh
@@ -10,6 +10,12 @@ tc_prepare
 export TMPDIR=$root/artifacts
 mkdir -p "$TMPDIR"
 
+# Sets `-test.v` in Go tests. (This is a no-op for now since we're not
+# running tests with Bazel in CI, but we shortly will, and we'll want to
+# make sure this is included when we do.)
+# Ref: https://github.com/bazelbuild/rules_go/pull/2456
+echo 'test --test_env=GO_TEST_WRAP_TESTV=1' > $root/.bazelrc.user
+
 tc_start_block "Run Bazel build"
 docker run -i ${tty-} --rm --init \
        --workdir="/go/src/github.com/cockroachdb/cockroach" \
