785c119abfcffd03515e149f91f8295d38dbd59b
Yahor Yuzefovich <yahor@cockroachlabs.com>
2021-02-05 12:39:23 -0800

    sql: close the last right side plan in apply join if panic occurs
    If a panic occurs when running the right side plan in the apply join, we
    might not close that plan correctly. In the row-by-row engine this
    doesn't matter because the server would crash, but in the vectorized
    engine we might catch the panic, and if the plan is not closed
    accordingly, we could then crash later since some resources (like memory
    accounting) are not cleared. This is now fixed.
    
    Release note: None

--- pkg/sql/apply_join.go
+++ pkg/sql/apply_join.go
@@ -50,6 +50,10 @@ type applyJoinNode struct {
 
 	planRightSideFn exec.ApplyJoinPlanRightSideFn
 
+	// lastRightSidePlan is the last plan created by planRightSideFn and will be
+	// closed when the applyJoinNode is closed if set.
+	lastRightSidePlan *planComponents
+
 	run struct {
 		// emptyRight is a cached, all-NULL slice that's used for left outer joins
 		// in the case of finding no match on the left.
@@ -214,11 +218,14 @@ func (a *applyJoinNode) Next(params runParams) (bool, error) {
 		if err != nil {
 			return false, err
 		}
-		plan := p.(*planComponents)
+		a.lastRightSidePlan = p.(*planComponents)
 
-		if err := a.runRightSidePlan(params, plan); err != nil {
+		if err := a.runRightSidePlan(params, a.lastRightSidePlan); err != nil {
 			return false, err
 		}
+		// runRightSidePlan call succeeded, so the plan has already been closed
+		// automatically.
+		a.lastRightSidePlan = nil
 
 		// We've got fresh right rows. Continue along in the loop, which will deal
 		// with joining the right plan's output with our left row.
@@ -308,6 +315,10 @@ func (a *applyJoinNode) Values() tree.Datums {
 }
 
 func (a *applyJoinNode) Close(ctx context.Context) {
+	if a.lastRightSidePlan != nil {
+		a.lastRightSidePlan.close(ctx)
+		a.lastRightSidePlan = nil
+	}
 	a.input.plan.Close(ctx)
 	a.run.rightRows.close(ctx)
 	if a.run.rightRowsIterator != nil {
--- pkg/sql/apply_join_test.go
+++ pkg/sql/apply_join_test.go
@@ -72,3 +72,58 @@ FROM
 		require.True(t, strings.Contains(err.Error(), "internal error"))
 	}
 }
+
+// TestApplyJoinPanic is a regression test for not closing the planNode tree in
+// the apply join that resulted in a panic that was caught by the vectorized
+// engine (#59871). At the moment of writing it's unclear why the type mismatch
+// causing the panic in the first place occurs.
+func TestApplyJoinPanic(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	testClusterArgs := base.TestClusterArgs{
+		ReplicationMode: base.ReplicationAuto,
+	}
+	tc := testcluster.StartTestCluster(t, 1, testClusterArgs)
+	ctx := context.Background()
+	defer tc.Stopper().Stop(ctx)
+
+	conn := tc.Conns[0]
+
+	_, err := conn.Exec(`
+WITH
+  with_2675 AS (SELECT * FROM (VALUES ('', (SELECT ''))) AS tab_8229 (col_14424, col_14425))
+SELECT
+  (
+    SELECT
+      cte_ref_793.col_14424
+    WHERE
+      EXISTS(
+        SELECT
+          NULL
+        WHERE
+          ''
+          IN (
+              (
+                SELECT
+                  NULL
+                FROM
+                  (VALUES (NULL), ((SELECT B'101100110111011110011100101001110000111001110' FROM (VALUES (0)) AS tab_8238 (col_14434))))
+                    AS tab_8239 (col_14436)
+                ORDER BY
+                  tab_8239.col_14436 DESC
+                LIMIT
+                  1
+              ),
+              ''
+            )
+      )
+  ),
+  cte_ref_793.col_14425
+FROM
+  with_2675 AS cte_ref_793;
+`)
+	// We expect that an internal error is returned.
+	require.Error(t, err)
+	require.True(t, strings.Contains(err.Error(), "invalid datum type given"))
+}
