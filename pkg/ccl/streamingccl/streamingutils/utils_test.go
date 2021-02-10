287eba0ef913492cb1eaac5f7d9c13140f35de1f
Aditya Maru <adityamaru@gmail.com>
2021-02-09 20:12:52 -0500

    builtins: add builtin to signal complete to stream ingestion job
    This change adds a builtin to signal to a running stream ingestion job
    to complete its ingestion. It does this by setting a bool on the jobs
    progress that will be polled periodically by the stream ingestion job.
    
    The builtin does not wait until the job has performed the necessary cleanup
    to get the ingested cluster to a consistent state. It returns the job ID
    of the job that has been signalled and can be used in conjunction with
    SHOW JOBS WHEN COMPLETE.
    
    Note, this change does not hookup the builtin with the stream ingestion
    job. That will be done in a follow up PR.
    
    Release note: None

---
+++ pkg/ccl/streamingccl/streamingutils/utils_test.go
@@ -0,0 +1,77 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package streamingutils
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	_ "github.com/cockroachdb/cockroach/pkg/ccl/streamingccl/streamingest"
+	"github.com/cockroachdb/cockroach/pkg/jobs"
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/security"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCutoverBuiltin(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	ctx := context.Background()
+	defer jobs.TestingSetAdoptAndCancelIntervals(100*time.Millisecond, 100*time.Millisecond)()
+
+	tc := testcluster.StartTestCluster(t, 1, base.TestClusterArgs{})
+	defer tc.Stopper().Stop(ctx)
+	registry := tc.Server(0).JobRegistry().(*jobs.Registry)
+	sqlDB := sqlutils.MakeSQLRunner(tc.ServerConn(0))
+	db := sqlDB.DB
+
+	startTimestamp := hlc.Timestamp{WallTime: timeutil.Now().UnixNano()}
+	streamIngestJobRecord := jobs.Record{
+		Description: "test stream ingestion",
+		Username:    security.RootUserName(),
+		Details: jobspb.StreamIngestionDetails{
+			StreamAddress: "some://address/here",
+			Span:          roachpb.Span{Key: keys.LocalMax, EndKey: keys.LocalMax.Next()},
+			StartTime:     startTimestamp,
+		},
+		Progress: jobspb.StreamIngestionProgress{},
+	}
+	job, err := registry.CreateAndStartJob(ctx, nil, streamIngestJobRecord)
+	require.NoError(t, err)
+
+	// Check that sentinel is not set.
+	progress := job.Progress()
+	sp, ok := progress.GetDetails().(*jobspb.Progress_StreamIngest)
+	require.True(t, ok)
+	require.False(t, sp.StreamIngest.MarkedForCompletion)
+
+	var jobID int
+	err = db.QueryRowContext(
+		ctx,
+		`SELECT crdb_internal.complete_stream_ingestion_job($1)`,
+		*job.ID()).Scan(&jobID)
+	require.NoError(t, err)
+	require.Equal(t, *job.ID(), int64(jobID))
+
+	// Check that sentinel is set on the job progress.
+	sj, err := registry.LoadJob(ctx, *job.ID())
+	require.NoError(t, err)
+	progress = sj.Progress()
+	sp, ok = progress.GetDetails().(*jobspb.Progress_StreamIngest)
+	require.True(t, ok)
+	require.True(t, sp.StreamIngest.MarkedForCompletion)
+}
