7c55798b09b2afa94193cd8ea9e82e8eb44ce5b1
Bilal Akhtar <bilal@cockroachlabs.com>
2021-02-11 15:36:50 -0500

    server: Migrate nodes, {hot-,}ranges, health endpoints to v2 API
    This change adds API v2 compatible versions of the node list, range info
    per node, ranges info across nodes, hot ranges, and health endpoints.
    These endpoints all support API v2 header-based authentication,
    pagination (if applicable), and only return relevant information
    in the response payloads.
    
    Release note (api change): Add these new HTTP API endpoints:
     - `/api/v2/nodes/`: Lists all nodes in the cluster
     - `/api/v2/nodes/<node_id>/ranges`: Lists all ranges on the specified node
     - `/api/v2/ranges/hot/`: Lists hot ranges in the cluster
     - `/api/v2/ranges/<range_id>/`: Describes range in more detail
     - `/api/v2/health/`: Returns an HTTP 200 response if node is healthy.
    
    Release justification: Adds more HTTP API endpoints in parallel
     that do not touch existing code.

--- pkg/server/status_test.go
+++ pkg/server/status_test.go
@@ -14,11 +14,9 @@ import (
 	"bytes"
 	"context"
 	gosql "database/sql"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"math"
-	"net/http"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -2006,103 +2004,6 @@ func TestListContentionEventsSecurity(t *testing.T) {
 		t.Errorf("unexpected failure listing contention events; error: %v; response errors: %v",
 			err, resp.Errors)
 	}
-}
-
-func TestListSessionsV2(t *testing.T) {
-	defer leaktest.AfterTest(t)()
-	defer log.Scope(t).Close(t)
-
-	testCluster := serverutils.StartNewTestCluster(t, 3, base.TestClusterArgs{})
-	ctx := context.Background()
-	defer testCluster.Stopper().Stop(ctx)
-
-	ts1 := testCluster.Server(0)
-
-	var sqlConns []*gosql.Conn
-	for i := 0; i < 15; i++ {
-		serverConn := testCluster.ServerConn(i % 3)
-		conn, err := serverConn.Conn(ctx)
-		require.NoError(t, err)
-		sqlConns = append(sqlConns, conn)
-	}
-
-	defer func() {
-		for _, conn := range sqlConns {
-			_ = conn.Close()
-		}
-	}()
-
-	doSessionsRequest := func(client http.Client, limit int, start string) listSessionsResponse {
-		req, err := http.NewRequest("GET", ts1.AdminURL()+apiV2Path+"sessions/", nil)
-		require.NoError(t, err)
-		query := req.URL.Query()
-		if limit > 0 {
-			query.Add("limit", strconv.Itoa(limit))
-		}
-		if len(start) > 0 {
-			query.Add("start", start)
-		}
-		req.URL.RawQuery = query.Encode()
-		resp, err := client.Do(req)
-		require.NoError(t, err)
-		require.NotNil(t, resp)
-		bytesResponse, err := ioutil.ReadAll(resp.Body)
-		require.NoError(t, err)
-		require.NoError(t, resp.Body.Close())
-
-		var sessionsResponse listSessionsResponse
-		if resp.StatusCode != 200 {
-			t.Fatal(string(bytesResponse))
-		}
-		require.NoError(t, json.Unmarshal(bytesResponse, &sessionsResponse))
-		return sessionsResponse
-	}
-
-	time.Sleep(500 * time.Millisecond)
-	adminClient, err := ts1.GetAdminAuthenticatedHTTPClient()
-	require.NoError(t, err)
-	sessionsResponse := doSessionsRequest(adminClient, 0, "")
-	require.LessOrEqual(t, 15, len(sessionsResponse.Sessions))
-	require.Equal(t, 0, len(sessionsResponse.Errors))
-	allSessions := sessionsResponse.Sessions
-	sort.Slice(allSessions, func(i, j int) bool {
-		return allSessions[i].Start.Before(allSessions[j].Start)
-	})
-
-	// Test the paginated version is identical to the non-paginated one.
-	for limit := 1; limit <= 15; limit++ {
-		var next string
-		var paginatedSessions []serverpb.Session
-		for {
-			sessionsResponse := doSessionsRequest(adminClient, limit, next)
-			paginatedSessions = append(paginatedSessions, sessionsResponse.Sessions...)
-			next = sessionsResponse.Next
-			require.LessOrEqual(t, len(sessionsResponse.Sessions), limit)
-			if len(sessionsResponse.Sessions) < limit {
-				break
-			}
-		}
-		sort.Slice(paginatedSessions, func(i, j int) bool {
-			return paginatedSessions[i].Start.Before(paginatedSessions[j].Start)
-		})
-		// Sometimes there can be a transient session that pops up in one of the two
-		// calls. Exclude it by only comparing the first 15 sessions.
-		require.Equal(t, paginatedSessions[:15], allSessions[:15])
-	}
-
-	// A non-admin user cannot see sessions at all.
-	nonAdminClient, err := ts1.GetAuthenticatedHTTPClient(false)
-	require.NoError(t, err)
-	req, err := http.NewRequest("GET", ts1.AdminURL()+apiV2Path+"sessions/", nil)
-	require.NoError(t, err)
-	resp, err := nonAdminClient.Do(req)
-	require.NoError(t, err)
-	require.NotNil(t, resp)
-	bytesResponse, err := ioutil.ReadAll(resp.Body)
-	require.NoError(t, err)
-	require.NoError(t, resp.Body.Close())
-	require.Equal(t, http.StatusForbidden, resp.StatusCode)
-	require.Contains(t, string(bytesResponse), "not allowed")
 }
 
 func TestCreateStatementDiagnosticsReport(t *testing.T) {
