3dad718867e9bd2a0d8d11d81c634ea897efa1a0
Ricky Stewart <ricky@cockroachlabs.com>
2021-02-12 11:54:21 -0600

    bazel: generate contents of `pkg/BUILD.bazel` in `make bazel-generate`
    Previously, the `ALL_TESTS` list was hardcoded, leaving a possibility
    that it would fall out of sync with the rest of tree. Now we use `bazel
    query` to enumerate all the tests in the repo generate the contents of
    `pkg/BUILD.bazel` accordingly. Since this is part of `make
    bazel-generate`, CI should ensure that this never falls out of sync
    again.
    
    Release note: None

---
+++ build/bazelutil/bazel-generate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+bazel run //:gazelle -- update-repos -from_file=go.mod -build_file_proto_mode=disable_global -to_macro=DEPS.bzl%go_deps
+bazel run //:gazelle
+bazel run //pkg/cmd/generate-test-suites --run_under="cd $PWD && " > pkg/BUILD.bazel
