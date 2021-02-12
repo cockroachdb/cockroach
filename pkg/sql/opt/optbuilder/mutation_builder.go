6b0b3f9f1e080b911fc984b4b6f4d93d360f18c6
Marcus Gartner <marcus@cockroachlabs.com>
2021-02-12 08:38:39 -0800

    sql: support INSERT with partial UNIQUE WITHOUT INDEX constraints
    Uniqueness checks on INSERTs are now performed for partial UNIQUE
    WITHOUT INDEX constraints. The semi-join filters in the uniqueness
    checks filter out rows that do not satisfy the predicate. Rows on both
    sides of the join are filtered. This is required to prevent duplicate
    key violations from occurring for rows that do not satisfy the predicate
    of the partial unique constraint.
    
    The WithScan and Scan of the uniqueness check now produce all ordinary
    table columns, rather than just the PK and unique columns, because
    predicates may refer any columns in the table.
    
    Informs #59195
    
    There is no release note because these constraints are gated behind the
    experimental_enable_unique_without_index_constraints session variable.
    
    Release note: None

--- pkg/sql/opt/optbuilder/mutation_builder.go
+++ pkg/sql/opt/optbuilder/mutation_builder.go
@@ -159,6 +159,11 @@ type mutationBuilder struct {
 	// reuse.
 	parsedIndexExprs []tree.Expr
 
+	// parsedUniqueConstraintExprs is a cached set of parsed partial unique
+	// constraint predicate expressions from the table schema. These are parsed
+	// once and cached for reuse.
+	parsedUniqueConstraintExprs []tree.Expr
+
 	// uniqueChecks contains unique check queries; see buildUnique* methods.
 	uniqueChecks memo.UniqueChecksExpr
 
@@ -1193,6 +1198,35 @@ func (mb *mutationBuilder) parsePartialIndexPredicateExpr(idx cat.IndexOrdinal) 
 	return expr
 }
 
+// parseUniqueConstraintPredicateExpr parses the predicate of the given partial
+// unique constraint and caches it for reuse. This function panics if the unique
+// constraint at the given ordinal is not partial.
+func (mb *mutationBuilder) parseUniqueConstraintPredicateExpr(idx cat.UniqueOrdinal) tree.Expr {
+	uniqueConstraint := mb.tab.Unique(idx)
+
+	predStr, isPartial := uniqueConstraint.Predicate()
+	if !isPartial {
+		panic(errors.AssertionFailedf("unique constraint at ordinal %d is not a partial unique constraint", idx))
+	}
+
+	if mb.parsedUniqueConstraintExprs == nil {
+		mb.parsedUniqueConstraintExprs = make([]tree.Expr, mb.tab.UniqueCount())
+	}
+
+	// Return expression from the cache, if it was already parsed previously.
+	if mb.parsedUniqueConstraintExprs[idx] != nil {
+		return mb.parsedUniqueConstraintExprs[idx]
+	}
+
+	expr, err := parser.ParseExpr(predStr)
+	if err != nil {
+		panic(err)
+	}
+
+	mb.parsedUniqueConstraintExprs[idx] = expr
+	return expr
+}
+
 // getIndexLaxKeyOrdinals returns the ordinals of all lax key columns in the
 // given index. A column's ordinal is the ordered position of that column in the
 // owning table.
@@ -1314,7 +1348,10 @@ func (mb *mutationBuilder) buildCheckInputScan(
 		}
 
 		// Synthesize a new output column for the input column, using the name
-		// of the column in the underlying table.
+		// of the column in the underlying table. The table's column names are
+		// used because partial unique constraint checks must filter the
+		// WithScan rows with a predicate expression that references the table's
+		// columns.
 		tableCol := mb.b.factory.Metadata().Table(mb.tabID).Column(tabOrd)
 		outCol := mb.md.AddColumn(string(tableCol.ColName()), tableCol.DatumType())
 		withScanScope.cols[i] = scopeColumn{
