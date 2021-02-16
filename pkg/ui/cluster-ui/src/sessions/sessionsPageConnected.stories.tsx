821b9fd9240aade2830a6c0a98c8e4087e7c5e6c
Vlad Los <carrott9@gmail.com>
2021-02-16 18:15:35 +0200

    cluster-ui: sessions redux functionality
    minor cleanups + recreated redux layer for sessions page,
    sessions details page, terminate query modal.
    
    notification message support after success\error on terminate
    query modal flow will be done as separate pr, due to very
    different approach of working with notifications in `dbconsole`
    and `managed-service`.

---
+++ pkg/ui/cluster-ui/src/sessions/sessionsPageConnected.stories.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { createMemoryHistory } from "history";
+import createSagaMiddleware from "redux-saga";
+import { Provider } from "react-redux";
+import {
+  ConnectedRouter,
+  connectRouter,
+  routerMiddleware,
+} from "connected-react-router";
+import {
+  applyMiddleware,
+  combineReducers,
+  compose,
+  createStore,
+  Store,
+} from "redux";
+import { SessionsPageConnected } from "./sessionsPageConnected";
+import { AppState, rootReducer, sagas } from "../store";
+
+const history = createMemoryHistory();
+const routerReducer = connectRouter(history);
+const sagaMiddleware = createSagaMiddleware();
+
+const store: Store<AppState> = createStore(
+  combineReducers({
+    router: routerReducer,
+    adminUI: rootReducer,
+  }),
+  compose(
+    applyMiddleware(sagaMiddleware, routerMiddleware(history)),
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+      (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
+  ),
+);
+
+sagaMiddleware.run(sagas);
+
+storiesOf("Sessions Page Connected", module)
+  .addDecorator(storyFn => (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>{storyFn()}</ConnectedRouter>
+    </Provider>
+  ))
+  .addDecorator(storyFn => (
+    <div style={{ backgroundColor: "#F5F7FA" }}>{storyFn()}</div>
+  ))
+  .add("with data", () => <SessionsPageConnected />);
