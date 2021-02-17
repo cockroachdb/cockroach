909aca7b08edb9bc64b37ef74ca22fce16a49be1
Tobias Grieger <tobias.b.grieger@gmail.com>
2021-02-17 12:45:47 +0100

    sql: work around contention registry errors
    See https://github.com/cockroachdb/cockroach/issues/60669.
    
    Release note: None

--- pkg/sql/distsql_running.go
+++ pkg/sql/distsql_running.go
@@ -670,7 +670,9 @@ func (r *DistSQLReceiver) Push(
 						r.contendedQueryMetric = nil
 					}
 					if err := r.contentionRegistry.AddContentionEvent(ev); err != nil {
-						r.resultWriter.SetError(errors.Wrap(err, "unable to add contention event to registry"))
+						// TODO(asubiotto): see https://github.com/cockroachdb/cockroach/issues/60669
+						// r.resultWriter.SetError(errors.Wrap(err, "unable to add contention event to registry"))
+						_ = err
 					}
 				})
 			}
