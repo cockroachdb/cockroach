aba51bddeeff12c756893cff87be1e213cf39c42
Yahor Yuzefovich <yahor@cockroachlabs.com>
2021-02-17 12:11:53 -0800

    contention: merge serialized representations of registries
    This commit adds a method to merge the serialized representation of
    contention registries into one which will be needed to get a global
    contention view.
    
    Note that the merge operation respects the same constants that limit the
    size of the registry. Additionally, the serialized representations now
    respect the following orderings:
    - on the highest level, all IndexContentionEvents objects are ordered
      according to their importance (achieved by an explicit sort)
    - on the middle level, all SingleKeyContention objects are ordered by their
      keys (achieved by using the ordered cache)
    - on the lowest level, all SingleTxnContention objects are ordered by the
      frequency of the occurrence (achieved by an explicit sort).
    The same guarantees are kept for the merge operation too.
    
    Release note: None

--- pkg/sql/contention/registry.go
+++ pkg/sql/contention/registry.go
@@ -11,6 +11,9 @@
 package contention
 
 import (
+	"bytes"
+	"fmt"
+	"sort"
 	"strings"
 	"time"
 	"unsafe"
@@ -50,7 +53,7 @@ type Registry struct {
 	indexMap *indexMap
 }
 
-const (
+var (
 	// indexMapMaxSize specifies the maximum number of indexes a Registry should
 	// keep track of contention events for.
 	indexMapMaxSize = 50
@@ -70,11 +73,11 @@ var _ = GetRegistryEstimatedMaxMemoryFootprintInBytes
 // estimatedAverageKeySize bytes.
 // Serves to reserve a reasonable amount of memory for this object.
 // Around ~4.5MB at the time of writing.
-func GetRegistryEstimatedMaxMemoryFootprintInBytes() uintptr {
+func GetRegistryEstimatedMaxMemoryFootprintInBytes() int {
 	const estimatedAverageKeySize = 64
-	txnsMapSize := maxNumTxns * (unsafe.Sizeof(uuid.UUID{}) + unsafe.Sizeof(int(0)))
-	orderedKeyMapSize := orderedKeyMapMaxSize * ((unsafe.Sizeof(comparableKey{}) * estimatedAverageKeySize) + txnsMapSize)
-	return indexMapMaxSize * (unsafe.Sizeof(indexMapKey{}) + unsafe.Sizeof(indexMapValue{}) + orderedKeyMapSize)
+	txnsMapSize := maxNumTxns * int(unsafe.Sizeof(uuid.UUID{})+unsafe.Sizeof(int(0)))
+	orderedKeyMapSize := orderedKeyMapMaxSize * (int(unsafe.Sizeof(comparableKey{})*estimatedAverageKeySize) + txnsMapSize)
+	return indexMapMaxSize * (int(unsafe.Sizeof(indexMapKey{})+unsafe.Sizeof(indexMapValue{})) + orderedKeyMapSize)
 }
 
 var orderedKeyMapCfg = cache.Config{
@@ -218,7 +221,14 @@ func (r *Registry) AddContentionEvent(c roachpb.ContentionEvent) error {
 	return nil
 }
 
-// Serialize returns the serialized representation of the registry.
+// Serialize returns the serialized representation of the registry. In this
+// representation the following orderings are maintained:
+// - on the highest level, all IndexContentionEvents objects are ordered
+//   according to their importance (achieved by an explicit sort)
+// - on the middle level, all SingleKeyContention objects are ordered by their
+//   keys (achieved by using the ordered cache)
+// - on the lowest level, all SingleTxnContention objects are ordered by the
+//   frequency of the occurrence (achieved by an explicit sort).
 func (r *Registry) Serialize() []contentionpb.IndexContentionEvents {
 	r.globalLock.Lock()
 	defer r.globalLock.Unlock()
@@ -245,12 +255,34 @@ func (r *Registry) Serialize() []contentionpb.IndexContentionEvents {
 				skc.Txns[txnCount].Count = e.Value.(uint64)
 				txnCount++
 			})
+			sortSingleTxnContention(skc.Txns)
 			skcCount++
 			return false
 		})
 		iceCount++
 	})
+	sortIndexContentionEvents(resp)
 	return resp
+}
+
+// sortIndexContentionEvents sorts all of the index contention events in-place
+// according to their importance (as defined by the total number of contention
+// events).
+func sortIndexContentionEvents(ice []contentionpb.IndexContentionEvents) {
+	sort.Slice(ice, func(i, j int) bool {
+		if ice[i].NumContentionEvents != ice[j].NumContentionEvents {
+			return ice[i].NumContentionEvents > ice[j].NumContentionEvents
+		}
+		return ice[i].CumulativeContentionTime > ice[j].CumulativeContentionTime
+	})
+}
+
+// sortSingleTxnContention sorts the transactions in-place according to the
+// frequency of their occurrence in DESC order.
+func sortSingleTxnContention(txns []contentionpb.SingleKeyContention_SingleTxnContention) {
+	sort.Slice(txns, func(i, j int) bool {
+		return txns[i].Count > txns[j].Count
+	})
 }
 
 // String returns a string representation of the Registry.
@@ -261,4 +293,135 @@ func (r *Registry) String() string {
 		b.WriteString(serialized[i].String())
 	}
 	return b.String()
+}
+
+// MergeSerializedRegistries merges the serialized representations of two
+// Registries into one. first is modified in-place.
+//
+// The result will contain at most indexMapMaxSize number of objects with the
+// most important objects (as defined by the total number of contention events)
+// kept from both arguments. Other constants (orderedKeyMapMaxSize and
+// maxNumTxns) are also respected by the internal slices.
+//
+// The returned representation has the same ordering guarantees as described for
+// Serialize above.
+func MergeSerializedRegistries(
+	first, second []contentionpb.IndexContentionEvents,
+) []contentionpb.IndexContentionEvents {
+	for s := range second {
+		found := false
+		for f := range first {
+			if first[f].TableID == second[s].TableID && first[f].IndexID == second[s].IndexID {
+				first[f] = mergeIndexContentionEvents(first[f], second[s])
+				found = true
+				break
+			}
+		}
+		if !found {
+			first = append(first, second[s])
+		}
+	}
+	// Sort all of the index contention events so that more frequent ones are at
+	// the front and then truncate if needed.
+	sortIndexContentionEvents(first)
+	if len(first) > indexMapMaxSize {
+		first = first[:indexMapMaxSize]
+	}
+	return first
+}
+
+// mergeIndexContentionEvents merges two lists of contention events that
+// occurred on the same index. It will panic if the indexes are different.
+//
+// The result will contain at most orderedKeyMapMaxSize number of single key
+// contention events (ordered by the keys).
+func mergeIndexContentionEvents(
+	first contentionpb.IndexContentionEvents, second contentionpb.IndexContentionEvents,
+) contentionpb.IndexContentionEvents {
+	if first.TableID != second.TableID || first.IndexID != second.IndexID {
+		panic(fmt.Sprintf("attempting to merge contention events from different indexes\n%s%s", first.String(), second.String()))
+	}
+	var result contentionpb.IndexContentionEvents
+	result.TableID = first.TableID
+	result.IndexID = first.IndexID
+	result.NumContentionEvents = first.NumContentionEvents + second.NumContentionEvents
+	result.CumulativeContentionTime = first.CumulativeContentionTime + second.CumulativeContentionTime
+	// Go over the events from both inputs and merge them so that we stay under
+	// the limit. We take advantage of the fact that events for both inputs are
+	// already ordered by their keys.
+	maxNumEvents := len(first.Events) + len(second.Events)
+	if maxNumEvents > orderedKeyMapMaxSize {
+		maxNumEvents = orderedKeyMapMaxSize
+	}
+	result.Events = make([]contentionpb.SingleKeyContention, maxNumEvents)
+	resultIdx, firstIdx, secondIdx := 0, 0, 0
+	// Iterate while we haven't taken enough events and at least one input is
+	// not exhausted.
+	for resultIdx < maxNumEvents && (firstIdx < len(first.Events) || secondIdx < len(second.Events)) {
+		var cmp int
+		if firstIdx == len(first.Events) {
+			// We've exhausted the list of events from the first input, so we
+			// will need to take from the second input.
+			cmp = 1
+		} else if secondIdx == len(second.Events) {
+			// We've exhausted the list of events from the second input, so we
+			// will need to take from the first input.
+			cmp = -1
+		} else {
+			cmp = first.Events[firstIdx].Key.Compare(second.Events[secondIdx].Key)
+		}
+		if cmp == 0 {
+			// The keys are the same, so we're merging the events from both
+			// inputs.
+			f := first.Events[firstIdx]
+			s := second.Events[secondIdx]
+			result.Events[resultIdx].Key = f.Key
+			result.Events[resultIdx].Txns = mergeSingleKeyContention(f.Txns, s.Txns)
+			firstIdx++
+			secondIdx++
+		} else if cmp < 0 {
+			// The first event is smaller, so we will take it as is.
+			result.Events[resultIdx] = first.Events[firstIdx]
+			firstIdx++
+		} else {
+			// The second event is smaller, so we will take it as is.
+			result.Events[resultIdx] = second.Events[secondIdx]
+			secondIdx++
+		}
+		resultIdx++
+	}
+	// We might have merged some events so that we allocated more than
+	// necessary, so we need to truncate if that's the case.
+	result.Events = result.Events[:resultIdx]
+	return result
+}
+
+// mergeSingleKeyContention merges two lists of contention events that occurred
+// on the same key updating first in-place. Objects are ordered by the count
+// (after merge when applicable) in DESC order.
+//
+// The result will contain at most maxNumTxns number of transactions.
+func mergeSingleKeyContention(
+	first, second []contentionpb.SingleKeyContention_SingleTxnContention,
+) []contentionpb.SingleKeyContention_SingleTxnContention {
+	for s := range second {
+		found := false
+		for f := range first {
+			if bytes.Equal(first[f].TxnID.GetBytes(), second[s].TxnID.GetBytes()) {
+				first[f].Count += second[s].Count
+				found = true
+				break
+			}
+		}
+		if !found {
+			first = append(first, second[s])
+		}
+	}
+	// Sort all of the transactions so that more frequent ones are at the front
+	// and then truncate if needed.
+	sortSingleTxnContention(first)
+	if len(first) > maxNumTxns {
+		first = first[:maxNumTxns]
+	}
+	return first
 }
