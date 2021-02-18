61f348d5cb3c1c6bb3de14d80c4a463e11c46a2f
Alfonso Subiotto Marques <alfonso@cockroachlabs.com>
2021-02-18 17:25:42 +0100

    execinfrapb: panic in test builds when encountering malformed metadata
    We've seen cases where no metadata fields are set, causing a panic because we
    assume that at least one field is set. This commit adds a panic in test builds
    if this is the case and adds some protection against malformed metadata by
    ignoring it if no fields are set.
    
    Release note: None

--- pkg/sql/execinfrapb/data.go
+++ pkg/sql/execinfrapb/data.go
@@ -12,11 +12,13 @@ package execinfrapb
 
 import (
 	"context"
+	"fmt"
 	"sync"
 
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/colinfo"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/util"
 	"github.com/cockroachdb/cockroach/pkg/util/encoding"
 	"github.com/cockroachdb/cockroach/pkg/util/tracing/tracingpb"
 	"github.com/cockroachdb/errors"
@@ -308,10 +310,12 @@ func LocalMetaToRemoteProducerMeta(
 		rpm.Value = &RemoteProducerMetadata_Metrics_{
 			Metrics: meta.Metrics,
 		}
-	} else {
+	} else if meta.Err != nil {
 		rpm.Value = &RemoteProducerMetadata_Error{
 			Error: NewError(ctx, meta.Err),
 		}
+	} else if util.CrdbTestBuild {
+		panic(fmt.Sprintf("unhandled field in local meta or all fields are nil"))
 	}
 	return rpm
 }
