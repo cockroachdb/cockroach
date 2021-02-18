b46b0279a24277db60f79259f89c92785b48022f
irfan sharif <irfanmahmoudsharif@gmail.com>
2021-02-18 14:22:08 -0500

    tracing: improve helpers to assert on trace structures
    The earlier implementation was a bit difficult to follow, and didn't
    assert on the structure of recordings (events from parent and child
    spans could appear at the same level). The helpers here are a bit more
    powerful, and ergonomic enough to allow authors to use arbitrary
    top-level indentation (as long as internal indentation is consistent).
    This lets us write tests of the form:
    
            if err := TestingCheckRecordedSpans(Span.GetRecording(), `
                span: root
                    event: a
                    span: child
                        event: [ambient] b
                        event: c
            `); err != nil {
                t.Fatal(err)
            }
    
    Release note: None

--- pkg/util/log/ambient_context_test.go
+++ pkg/util/log/ambient_context_test.go
@@ -60,13 +60,13 @@ func TestAnnotateCtxSpan(t *testing.T) {
 	sp1.Finish()
 
 	if err := tracing.TestingCheckRecordedSpans(sp1.GetRecording(), `
-		Span root:
+		span: root
 			tags: _verbose=1
 			event: a
 			event: c
-		Span child:
-			tags: _verbose=1 ambient=
-			event: [ambient] b
+			span: child
+				tags: _verbose=1 ambient=
+				event: [ambient] b
 	`); err != nil {
 		t.Fatal(err)
 	}
