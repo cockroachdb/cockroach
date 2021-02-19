8369cdae7a05c66b8ada4b241923638a4c3e14ee
Rebecca Taft <becca@cockroachlabs.com>
2021-02-18 20:19:42 -0700

    sql,opt: add cluster and session settings for locality optimized search
    This commit adds a new session setting locality_optimized_search and
    corresponding cluster default setting
    sql.defaults.locality_optimized_search.enabled. In future commits, these
    settings will be used to enable or disable locality optimized search.
    If enabled, the optimizer will try to plan scans and lookup joins in
    which local nodes are searched for matching rows before remote nodes,
    in the hope that the execution engine can avoid visiting remote nodes.
    
    Informs #55185
    
    Release note (sql change): Added a new session setting
    locality_optimized_search and corresponding cluster default setting
    sql.defaults.locality_optimized_search.enabled. Both are currently
    disabled by default, and are currently unused. In the future, these
    settings will be used to enable or disable locality optimized search.
    If enabled, the optimizer will try to search locally for rows in
    REGIONAL BY ROW tables before searching remote nodes.

--- pkg/sql/vars.go
+++ pkg/sql/vars.go
@@ -674,6 +674,25 @@ var varGen = map[string]sessionVar{
 	},
 
 	// CockroachDB extension.
+	`locality_optimized_search`: {
+		GetStringVal: makePostgresBoolGetStringValFn(`locality_optimized_search`),
+		Set: func(_ context.Context, m *sessionDataMutator, s string) error {
+			b, err := paramparse.ParseBoolVar(`locality_optimized_search`, s)
+			if err != nil {
+				return err
+			}
+			m.SetLocalityOptimizedSearch(b)
+			return nil
+		},
+		Get: func(evalCtx *extendedEvalContext) string {
+			return formatBoolAsPostgresSetting(evalCtx.SessionData.LocalityOptimizedSearch)
+		},
+		GlobalDefault: func(sv *settings.Values) string {
+			return formatBoolAsPostgresSetting(localityOptimizedSearchMode.Get(sv))
+		},
+	},
+
+	// CockroachDB extension.
 	`enable_implicit_select_for_update`: {
 		GetStringVal: makePostgresBoolGetStringValFn(`enable_implicit_select_for_update`),
 		Set: func(_ context.Context, m *sessionDataMutator, s string) error {
