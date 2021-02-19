062579558acc3176a61644ef7e59f900f4e20793
Rafi Shamim <rafi@cockroachlabs.com>
2021-02-19 11:21:14 -0500

    sql: include virtual table names in crash reports
    This uses the functionality that we already had in place for telemetry
    data. This might potentially make some crash reports more useful without
    compromizing privacy.
    
    Release note (general change): Crash reports that are sent to Cockroach Labs
    now no longer redact the names of builtin virtual tables from the
    crdb_internal, information_schema, and pg_catalog schemas.

--- pkg/sql/conn_executor.go
+++ pkg/sql/conn_executor.go
@@ -793,7 +793,8 @@ func (ex *connExecutor) closeWrapper(ctx context.Context, recovered interface{})
 
 			// Embed the statement in the error object for the telemetry
 			// report below. The statement gets anonymized.
-			panicErr = WithAnonymizedStatement(panicErr, ex.curStmtAST)
+			vt := ex.planner.extendedEvalCtx.VirtualSchemas
+			panicErr = WithAnonymizedStatement(panicErr, ex.curStmtAST, vt)
 		}
 
 		// Report the panic to telemetry in any case.
@@ -2818,6 +2819,6 @@ func init() {
 			return ""
 		}
 		// Anonymize the statement for reporting.
-		return anonymizeStmtAndConstants(stmt)
+		return anonymizeStmtAndConstants(stmt, nil /* VirtualTabler */)
 	})
 }
