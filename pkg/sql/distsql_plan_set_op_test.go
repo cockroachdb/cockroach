cockroachdb/cockroach

commit a76ee31e6a055842466335d3ee2141ac0a61e80e

sql: add ResultTypes to processor spec
This commit introduces a new field to `execinfrapb.ProcessorSpec` in
order to propagate the result types of the processors and the
corresponding field has been removed from `PhysicalPlan` (and was
replaced by a getter method). I believe it is a cleaner abstraction than
what we had previously (implicit type schema expectation by the output
stage that is passed into InputSyncSpec), and this change is
a prerequisite to refactor of the vectorized flow setup - with this
change it'll be now very easy to make sure that the desired type schema
is produced after any vectorized operator (without this plumbing we
would need to attempt to deduce the desired schema, and in some cases it
is not possible - e.g. when we have a remote table reader that is
feeding into an outbox).

Release note: None

pkg/sql/distsql_physical_planner.go
@@ -1319,7 +1319,7 @@ func (dsp *DistSQLPlanner) addSorters(
 			},
 		},
 		execinfrapb.PostProcessSpec{},
-		p.ResultTypes,
+		p.GetResultTypes(),
 		outputOrdering,
 	)
 }
@@ -1406,7 +1406,7 @@ func (dsp *DistSQLPlanner) planAggregators(
 		aggType = execinfrapb.AggregatorSpec_SCALAR
 	}
 
-	inputTypes := p.ResultTypes
+	inputTypes := p.GetResultTypes()
 
 	groupCols := make([]uint32, len(info.groupCols))
 	for i, idx := range info.groupCols {
@@ -1461,7 +1461,7 @@ func (dsp *DistSQLPlanner) planAggregators(
 			}
 			// Add distinct processors local to each existing current result
 			// processor.
-			p.AddNoGroupingStage(distinctSpec, execinfrapb.PostProcessSpec{}, p.ResultTypes, p.MergeOrdering)
+			p.AddNoGroupingStage(distinctSpec, execinfrapb.PostProcessSpec{}, inputTypes, p.MergeOrdering)
 		}
 	}
 
@@ -1865,20 +1865,22 @@ func (dsp *DistSQLPlanner) planAggregators(
 		// somewhat arbitrary decision; we could have a different number of nodes
 		// working on the final stage.
 		pIdxStart := physicalplan.ProcessorIdx(len(p.Processors))
+		prevStageResultTypes := p.GetResultTypes()
 		for _, resultProc := range p.ResultRouters {
 			proc := physicalplan.Processor{
 				Node: p.Processors[resultProc].Node,
 				Spec: execinfrapb.ProcessorSpec{
 					Input: []execinfrapb.InputSyncSpec{{
 						// The other fields will be filled in by mergeResultStreams.
-						ColumnTypes: p.ResultTypes,
+						ColumnTypes: prevStageResultTypes,
 					}},
 					Core: execinfrapb.ProcessorCoreUnion{Aggregator: &finalAggsSpec},
 					Post: finalAggsPost,
 					Output: []execinfrapb.OutputRouterSpec{{
 						Type: execinfrapb.OutputRouterSpec_PASS_THROUGH,
 					}},
-					StageID: stageID,
+					StageID:     stageID,
+					ResultTypes: finalOutTypes,
 				},
 			}
 			p.AddProcessor(proc)
@@ -1895,7 +1897,6 @@ func (dsp *DistSQLPlanner) planAggregators(
 			p.ResultRouters[i] = pIdxStart + physicalplan.ProcessorIdx(i)
 		}
 
-		p.ResultTypes = finalOutTypes
 		p.SetMergeOrdering(dsp.convertOrdering(info.reqOrdering, p.PlanToStreamColMap))
 	}
 
@@ -2054,7 +2055,8 @@ func mappingHelperForLookupJoins(
 	// the corresponding projection.
 	// The internal schema of the join reader is:
 	//    <input columns>... <table columns>...[continuation col]
-	numLeftCols := len(plan.ResultTypes)
+	inputTypes := plan.GetResultTypes()
+	numLeftCols := len(inputTypes)
 	numOutCols := numLeftCols + len(table.cols)
 	if addContinuationCol {
 		numOutCols++
@@ -2065,7 +2067,7 @@ func mappingHelperForLookupJoins(
 	outTypes = make([]*types.T, numOutCols)
 
 	for i := 0; i < numLeftCols; i++ {
-		outTypes[i] = plan.ResultTypes[i]
+		outTypes[i] = inputTypes[i]
 		post.OutputColumns[i] = uint32(i)
 	}
 	for i := range table.cols {
@@ -2106,7 +2108,7 @@ func makeIndexVarMapForLookupJoins(
 	// starting from numInputNodeCols.
 	indexVarMap = makePlanToStreamColMap(numInputNodeCols + len(table.cols))
 	copy(indexVarMap, plan.PlanToStreamColMap)
-	numLeftCols := len(plan.ResultTypes)
+	numLeftCols := len(plan.GetResultTypes())
 	for i := range table.cols {
 		indexVarMap[numInputNodeCols+i] = int(post.OutputColumns[numLeftCols+i])
 	}
@@ -2328,7 +2330,7 @@ func (dsp *DistSQLPlanner) createPlanForInvertedFilter(
 			execinfrapb.ProcessorCoreUnion{
 				InvertedFilterer: invertedFiltererSpec,
 			},
-			execinfrapb.PostProcessSpec{}, plan.ResultTypes)
+			execinfrapb.PostProcessSpec{}, plan.GetResultTypes())
 		return plan, nil
 	}
 	// Must be distributable.
@@ -2339,7 +2341,8 @@ func (dsp *DistSQLPlanner) createPlanForInvertedFilter(
 	// Instantiate one inverted filterer for every stream.
 	plan.AddNoGroupingStage(
 		execinfrapb.ProcessorCoreUnion{InvertedFilterer: invertedFiltererSpec},
-		execinfrapb.PostProcessSpec{}, plan.ResultTypes, execinfrapb.Ordering{})
+		execinfrapb.PostProcessSpec{}, plan.GetResultTypes(), execinfrapb.Ordering{},
+	)
 	// De-duplicate the PKs. Note that the inverted filterer output includes
 	// the inverted column always set to NULL, so we exclude it from the
 	// distinct columns.
@@ -2355,7 +2358,9 @@ func (dsp *DistSQLPlanner) createPlanForInvertedFilter(
 		execinfrapb.ProcessorCoreUnion{
 			Distinct: &execinfrapb.DistinctSpec{DistinctColumns: distinctColumns},
 		},
-		execinfrapb.PostProcessSpec{}, plan.ResultTypes)
+		execinfrapb.PostProcessSpec{},
+		plan.GetResultTypes(),
+	)
 	return plan, nil
 }
 
@@ -2407,7 +2412,7 @@ func (dsp *DistSQLPlanner) createPlanForJoin(
 	helper := &joinPlanningHelper{
 		numLeftOutCols:          n.pred.numLeftCols,
 		numRightOutCols:         n.pred.numRightCols,
-		numAllLeftCols:          len(leftPlan.ResultTypes),
+		numAllLeftCols:          len(leftPlan.GetResultTypes()),
 		leftPlanToStreamColMap:  leftMap,
 		rightPlanToStreamColMap: rightMap,
 	}
@@ -2500,13 +2505,12 @@ func (dsp *DistSQLPlanner) planJoiners(
 	p.AddJoinStage(
 		nodes, info.makeCoreSpec(), info.post,
 		info.leftEqCols, info.rightEqCols,
-		info.leftPlan.ResultTypes, info.rightPlan.ResultTypes,
+		info.leftPlan.GetResultTypes(), info.rightPlan.GetResultTypes(),
 		info.leftMergeOrd, info.rightMergeOrd,
-		leftRouters, rightRouters,
+		leftRouters, rightRouters, info.joinResultTypes,
 	)
 
 	p.PlanToStreamColMap = info.joinToStreamColMap
-	p.ResultTypes = info.joinResultTypes
 
 	// Joiners may guarantee an ordering to outputs, so we ensure that
 	// ordering is propagated through the input synchronizer of the next stage.
@@ -2655,7 +2659,7 @@ func (dsp *DistSQLPlanner) createPhysPlanForPlanNode(
 				}
 			},
 			execinfrapb.PostProcessSpec{},
-			plan.ResultTypes,
+			plan.GetResultTypes(),
 			plan.MergeOrdering,
 		)
 	}
@@ -2744,7 +2748,7 @@ func (dsp *DistSQLPlanner) wrapPlan(planCtx *PlanningCtx, n planNode) (*Physical
 		// We found a DistSQL-plannable subtree - create an input spec for it.
 		input = []execinfrapb.InputSyncSpec{{
 			Type:        execinfrapb.InputSyncSpec_UNORDERED,
-			ColumnTypes: p.ResultTypes,
+			ColumnTypes: p.GetResultTypes(),
 		}}
 	}
 	name := nodeName(n)
@@ -2762,12 +2766,11 @@ func (dsp *DistSQLPlanner) wrapPlan(planCtx *PlanningCtx, n planNode) (*Physical
 				Type: execinfrapb.OutputRouterSpec_PASS_THROUGH,
 			}},
 			// This stage consists of a single processor planned on the gateway.
-			StageID: p.NewStage(false /* containsRemoteProcessor */),
+			StageID:     p.NewStage(false /* containsRemoteProcessor */),
+			ResultTypes: wrapper.outputTypes,
 		},
 	}
 	pIdx := p.AddProcessor(proc)
-	p.ResultTypes = wrapper.outputTypes
-	p.PlanToStreamColMap = identityMapInPlace(make([]int, len(p.ResultTypes)))
 	if firstNotWrapped != nil {
 		// If we found a DistSQL-plannable subtree, we need to add a result stream
 		// between it and the physicalPlan we're creating here.
@@ -2783,6 +2786,7 @@ func (dsp *DistSQLPlanner) wrapPlan(planCtx *PlanningCtx, n planNode) (*Physical
 		p.ResultRouters = p.ResultRouters[:1]
 	}
 	p.ResultRouters[0] = pIdx
+	p.PlanToStreamColMap = identityMapInPlace(make([]int, len(p.GetResultTypes())))
 	return p, nil
 }
 
@@ -2810,12 +2814,12 @@ func (dsp *DistSQLPlanner) createValuesPlan(
 			// TODO: find a better node to place processor at
 			Node: dsp.gatewayNodeID,
 			Spec: execinfrapb.ProcessorSpec{
-				Core:   execinfrapb.ProcessorCoreUnion{Values: &s},
-				Output: []execinfrapb.OutputRouterSpec{{Type: execinfrapb.OutputRouterSpec_PASS_THROUGH}},
+				Core:        execinfrapb.ProcessorCoreUnion{Values: &s},
+				Output:      []execinfrapb.OutputRouterSpec{{Type: execinfrapb.OutputRouterSpec_PASS_THROUGH}},
+				ResultTypes: resultTypes,
 			},
 		}},
 		ResultRouters: []physicalplan.ProcessorIdx{0},
-		ResultTypes:   resultTypes,
 		GatewayNodeID: dsp.gatewayNodeID,
 		Distribution:  physicalplan.LocalPlan,
 	}
@@ -2946,7 +2950,7 @@ func (dsp *DistSQLPlanner) addDistinctProcessors(
 	}()
 
 	// Add distinct processors local to each existing current result processor.
-	plan.AddNoGroupingStage(distinctSpec, execinfrapb.PostProcessSpec{}, plan.ResultTypes, plan.MergeOrdering)
+	plan.AddNoGroupingStage(distinctSpec, execinfrapb.PostProcessSpec{}, plan.GetResultTypes(), plan.MergeOrdering)
 	if !plan.IsLastStageDistributed() {
 		return
 	}
@@ -2954,8 +2958,8 @@ func (dsp *DistSQLPlanner) addDistinctProcessors(
 	nodes := getNodesOfRouters(plan.ResultRouters, plan.Processors)
 	plan.AddStageOnNodes(
 		nodes, distinctSpec, execinfrapb.PostProcessSpec{},
-		distinctSpec.Distinct.DistinctColumns, plan.ResultTypes,
-		plan.MergeOrdering, plan.ResultRouters,
+		distinctSpec.Distinct.DistinctColumns, plan.GetResultTypes(),
+		plan.GetResultTypes(), plan.MergeOrdering, plan.ResultRouters,
 	)
 }
 
@@ -2971,8 +2975,8 @@ func (dsp *DistSQLPlanner) createPlanForOrdinality(
 		Ordinality: &execinfrapb.OrdinalitySpec{},
 	}
 
-	plan.PlanToStreamColMap = append(plan.PlanToStreamColMap, len(plan.ResultTypes))
-	outputTypes := append(plan.ResultTypes, types.Int)
+	plan.PlanToStreamColMap = append(plan.PlanToStreamColMap, len(plan.GetResultTypes()))
+	outputTypes := append(plan.GetResultTypes(), types.Int)
 
 	// WITH ORDINALITY never gets distributed so that the gateway node can
 	// always number each row in order.
@@ -3021,7 +3025,7 @@ func (dsp *DistSQLPlanner) createPlanForProjectSet(
 func (dsp *DistSQLPlanner) addProjectSet(
 	plan *PhysicalPlan, planCtx *PlanningCtx, info *projectSetPlanningInfo,
 ) error {
-	numResults := len(plan.ResultTypes)
+	numResults := len(plan.GetResultTypes())
 
 	// Create the project set processor spec.
 	projectSetSpec, err := createProjectSetSpec(planCtx, info, plan.PlanToStreamColMap)
@@ -3037,7 +3041,7 @@ func (dsp *DistSQLPlanner) addProjectSet(
 	// cases in the future where this is non-optimal (perhaps if its output is
 	// filtered), we could try to detect these cases and use AddNoGroupingStage
 	// instead.
-	outputTypes := append(plan.ResultTypes, projectSetSpec.GeneratedColumns...)
+	outputTypes := append(plan.GetResultTypes(), projectSetSpec.GeneratedColumns...)
 	plan.AddSingleGroupStage(dsp.gatewayNodeID, spec, execinfrapb.PostProcessSpec{}, outputTypes)
 
 	// Add generated columns to PlanToStreamColMap.
@@ -3166,7 +3170,7 @@ func (dsp *DistSQLPlanner) createPlanForSetOp(
 			if !dsp.isOnlyOnGateway(plan) {
 				// TODO(solon): We could skip this stage if there is a strong key on
 				// the result columns.
-				plan.AddNoGroupingStage(*distinctSpec, execinfrapb.PostProcessSpec{}, plan.ResultTypes, distinctOrds[side])
+				plan.AddNoGroupingStage(*distinctSpec, execinfrapb.PostProcessSpec{}, plan.GetResultTypes(), distinctOrds[side])
 				plan.AddProjection(streamCols)
 			}
 		}
@@ -3179,7 +3183,7 @@ func (dsp *DistSQLPlanner) createPlanForSetOp(
 	p.PlanToStreamColMap = planToStreamColMap
 
 	// Merge the plans' result types and merge ordering.
-	resultTypes, err := physicalplan.MergeResultTypes(leftPlan.ResultTypes, rightPlan.ResultTypes)
+	resultTypes, err := physicalplan.MergeResultTypes(leftPlan.GetResultTypes(), rightPlan.GetResultTypes())
 	if err != nil {
 		return nil, err
 	}
@@ -3209,7 +3213,6 @@ func (dsp *DistSQLPlanner) createPlanForSetOp(
 		// the left and right output routers.
 		p.ResultRouters = append(leftRouters, rightRouters...)
 
-		p.ResultTypes = resultTypes
 		p.SetMergeOrdering(mergeOrdering)
 
 		if !n.all {
@@ -3219,7 +3222,7 @@ func (dsp *DistSQLPlanner) createPlanForSetOp(
 			distinctSpec := execinfrapb.ProcessorCoreUnion{
 				Distinct: &execinfrapb.DistinctSpec{DistinctColumns: streamCols},
 			}
-			p.AddSingleGroupStage(dsp.gatewayNodeID, distinctSpec, execinfrapb.PostProcessSpec{}, p.ResultTypes)
+			p.AddSingleGroupStage(dsp.gatewayNodeID, distinctSpec, execinfrapb.PostProcessSpec{}, resultTypes)
 		} else {
 			// With UNION ALL, we can end up with multiple streams on the same node.
 			// We don't want to have unnecessary routers and cross-node streams, so
@@ -3280,16 +3283,16 @@ func (dsp *DistSQLPlanner) createPlanForSetOp(
 		if n.all {
 			p.AddJoinStage(
 				nodes, core, post, eqCols, eqCols,
-				leftPlan.ResultTypes, rightPlan.ResultTypes,
+				leftPlan.GetResultTypes(), rightPlan.GetResultTypes(),
 				leftPlan.MergeOrdering, rightPlan.MergeOrdering,
-				leftRouters, rightRouters,
+				leftRouters, rightRouters, resultTypes,
 			)
 		} else {
 			p.AddDistinctSetOpStage(
 				nodes, core, distinctSpecs[:], post, eqCols,
-				leftPlan.ResultTypes, rightPlan.ResultTypes,
+				leftPlan.GetResultTypes(), rightPlan.GetResultTypes(),
 				leftPlan.MergeOrdering, rightPlan.MergeOrdering,
-				leftRouters, rightRouters,
+				leftRouters, rightRouters, resultTypes,
 			)
 		}
 
@@ -3298,7 +3301,6 @@ func (dsp *DistSQLPlanner) createPlanForSetOp(
 			mergeOrdering = execinfrapb.Ordering{}
 		}
 
-		p.ResultTypes = resultTypes
 		p.SetMergeOrdering(mergeOrdering)
 	}
 
@@ -3338,8 +3340,8 @@ func (dsp *DistSQLPlanner) createPlanForWindow(
 			WindowFns:   make([]execinfrapb.WindowerSpec_WindowFn, len(samePartitionFuncs)),
 		}
 
-		newResultTypes := make([]*types.T, len(plan.ResultTypes)+len(samePartitionFuncs))
-		copy(newResultTypes, plan.ResultTypes)
+		newResultTypes := make([]*types.T, len(plan.GetResultTypes())+len(samePartitionFuncs))
+		copy(newResultTypes, plan.GetResultTypes())
 		for windowFnSpecIdx, windowFn := range samePartitionFuncs {
 			windowFnSpec, outputType, err := windowPlanState.createWindowFnSpec(windowFn)
 			if err != nil {
@@ -3395,6 +3397,7 @@ func (dsp *DistSQLPlanner) createPlanForWindow(
 			// SourceRouterSlot - namely, position in which
 			// a node appears in nodes.
 			prevStageRouters := plan.ResultRouters
+			prevStageResultTypes := plan.GetResultTypes()
 			plan.ResultRouters = make([]physicalplan.ProcessorIdx, 0, len(nodes))
 			for bucket, nodeID := range nodes {
 				proc := physicalplan.Processor{
@@ -3402,14 +3405,15 @@ func (dsp *DistSQLPlanner) createPlanForWindow(
 					Spec: execinfrapb.ProcessorSpec{
 						Input: []execinfrapb.InputSyncSpec{{
 							Type:        execinfrapb.InputSyncSpec_UNORDERED,
-							ColumnTypes: plan.ResultTypes,
+							ColumnTypes: prevStageResultTypes,
 						}},
 						Core: execinfrapb.ProcessorCoreUnion{Windower: &windowerSpec},
 						Post: execinfrapb.PostProcessSpec{},
 						Output: []execinfrapb.OutputRouterSpec{{
 							Type: execinfrapb.OutputRouterSpec_PASS_THROUGH,
 						}},
-						StageID: stageID,
+						StageID:     stageID,
+						ResultTypes: newResultTypes,
 					},
 				}
 				pIdx := plan.AddProcessor(proc)
@@ -3424,15 +3428,13 @@ func (dsp *DistSQLPlanner) createPlanForWindow(
 				}
 				plan.ResultRouters = append(plan.ResultRouters, pIdx)
 			}
-
-			plan.ResultTypes = newResultTypes
 		}
 	}
 
 	// We definitely added columns throughout all the stages of windowers, so we
 	// need to update PlanToStreamColMap. We need to update the map before adding
 	// rendering or projection because it is used there.
-	plan.PlanToStreamColMap = identityMap(plan.PlanToStreamColMap, len(plan.ResultTypes))
+	plan.PlanToStreamColMap = identityMap(plan.PlanToStreamColMap, len(plan.GetResultTypes()))
 
 	// windowers do not guarantee maintaining the order at the moment, so we
 	// reset MergeOrdering. There shouldn't be an ordering here, but we reset it
@@ -3445,10 +3447,10 @@ func (dsp *DistSQLPlanner) createPlanForWindow(
 		return nil, err
 	}
 
-	if len(plan.ResultTypes) != len(plan.PlanToStreamColMap) {
+	if len(plan.GetResultTypes()) != len(plan.PlanToStreamColMap) {
 		// We added/removed columns while rendering or projecting, so we need to
 		// update PlanToStreamColMap.
-		plan.PlanToStreamColMap = identityMap(plan.PlanToStreamColMap, len(plan.ResultTypes))
+		plan.PlanToStreamColMap = identityMap(plan.PlanToStreamColMap, len(plan.GetResultTypes()))
 	}
 
 	return plan, nil
@@ -3527,7 +3529,7 @@ func (dsp *DistSQLPlanner) FinalizePlan(planCtx *PlanningCtx, plan *PhysicalPlan
 
 	// Add a final projection so that DistSQLReceiver gets the rows of the
 	// desired schema.
-	projection := make([]uint32, 0, len(plan.ResultTypes))
+	projection := make([]uint32, 0, len(plan.GetResultTypes()))
 	for _, outputCol := range plan.PlanToStreamColMap {
 		if outputCol >= 0 {
 			projection = append(projection, uint32(outputCol))
@@ -3546,7 +3548,7 @@ func (dsp *DistSQLPlanner) FinalizePlan(planCtx *PlanningCtx, plan *PhysicalPlan
 				},
 			},
 			execinfrapb.PostProcessSpec{},
-			plan.ResultTypes,
+			plan.GetResultTypes(),
 		)
 	}
 

pkg/ccl/backupccl/restore_processor_planning.go
@@ -21,6 +21,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/execinfrapb"
 	"github.com/cockroachdb/cockroach/pkg/sql/physicalplan"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/storage/cloud"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/errors"
@@ -159,7 +160,8 @@ func distRestore(
 						RangeRouterSpec: rangeRouterSpec,
 					},
 				},
-				StageID: splitAndScatterStageID,
+				StageID:     splitAndScatterStageID,
+				ResultTypes: splitAndScatterOutputTypes,
 			},
 		}
 		pIdx := p.AddProcessor(proc)
@@ -176,10 +178,11 @@ func distRestore(
 				Input: []execinfrapb.InputSyncSpec{
 					{ColumnTypes: splitAndScatterOutputTypes},
 				},
-				Core:    execinfrapb.ProcessorCoreUnion{RestoreData: &restoreDataSpec},
-				Post:    execinfrapb.PostProcessSpec{},
-				Output:  []execinfrapb.OutputRouterSpec{{Type: execinfrapb.OutputRouterSpec_PASS_THROUGH}},
-				StageID: restoreDataStageID,
+				Core:        execinfrapb.ProcessorCoreUnion{RestoreData: &restoreDataSpec},
+				Post:        execinfrapb.PostProcessSpec{},
+				Output:      []execinfrapb.OutputRouterSpec{{Type: execinfrapb.OutputRouterSpec_PASS_THROUGH}},
+				StageID:     restoreDataStageID,
+				ResultTypes: []*types.T{},
 			},
 		}
 		pIdx := p.AddProcessor(proc)

pkg/sql/colexec/colbuilder/execplan.go
@@ -1164,7 +1164,8 @@ func (r opResult) wrapPostProcessSpec(
 		Core: execinfrapb.ProcessorCoreUnion{
 			Noop: &execinfrapb.NoopCoreSpec{},
 		},
-		Post: *post,
+		Post:        *post,
+		ResultTypes: args.Spec.ResultTypes,
 	}
 	return r.createAndWrapRowSource(
 		ctx, flowCtx, args, []colexecbase.Operator{r.Op}, [][]*types.T{r.ColumnTypes},

pkg/sql/colexec/external_sort_test.go
@@ -336,6 +336,7 @@ func createDiskBackedSorter(
 		Post: execinfrapb.PostProcessSpec{
 			Limit: uint64(k),
 		},
+		ResultTypes: typs,
 	}
 	args := &NewColOperatorArgs{
 		Spec:                spec,

pkg/sql/colexec/hashjoiner_test.go
@@ -942,6 +942,14 @@ func createSpecForHashJoiner(tc *joinTestCase) *execinfrapb.ProcessorSpec {
 	for _, outCol := range tc.rightOutCols {
 		projection = append(projection, rColOffset+outCol)
 	}
+	resultTypes := make([]*types.T, 0, len(projection))
+	for _, i := range projection {
+		if int(i) < len(tc.leftTypes) {
+			resultTypes = append(resultTypes, tc.leftTypes[i])
+		} else {
+			resultTypes = append(resultTypes, tc.rightTypes[i-rColOffset])
+		}
+	}
 	return &execinfrapb.ProcessorSpec{
 		Input: []execinfrapb.InputSyncSpec{
 			{ColumnTypes: tc.leftTypes},
@@ -954,6 +962,7 @@ func createSpecForHashJoiner(tc *joinTestCase) *execinfrapb.ProcessorSpec {
 			Projection:    true,
 			OutputColumns: projection,
 		},
+		ResultTypes: resultTypes,
 	}
 }
 
@@ -1173,6 +1182,7 @@ func TestHashJoinerProjection(t *testing.T) {
 			// from the left and from the right are intertwined.
 			OutputColumns: []uint32{3, 1, 0, 5, 4, 2},
 		},
+		ResultTypes: []*types.T{types.Int, types.Int, types.Bool, types.Decimal, types.Float, types.Bytes},
 	}
 
 	leftSource := newOpTestInput(1, leftTuples, leftTypes)

pkg/sql/colexec/is_null_ops_test.go
@@ -228,14 +228,16 @@ func TestIsNullSelOp(t *testing.T) {
 	for _, c := range testCases {
 		log.Infof(ctx, "%s", c.desc)
 		opConstructor := func(input []colexecbase.Operator) (colexecbase.Operator, error) {
+			typs := []*types.T{types.Int}
 			spec := &execinfrapb.ProcessorSpec{
-				Input: []execinfrapb.InputSyncSpec{{ColumnTypes: []*types.T{types.Int}}},
+				Input: []execinfrapb.InputSyncSpec{{ColumnTypes: typs}},
 				Core: execinfrapb.ProcessorCoreUnion{
 					Noop: &execinfrapb.NoopCoreSpec{},
 				},
 				Post: execinfrapb.PostProcessSpec{
 					Filter: execinfrapb.Expression{Expr: fmt.Sprintf("@1 %s", c.selExpr)},
 				},
+				ResultTypes: typs,
 			}
 			args := &NewColOperatorArgs{
 				Spec:                spec,

commit 4208a2598fa291027f4fd4eb2e07e92d3a9e8a4e

sql: propagate result types to inputs in set ops with nulls and tuples
The physical planning for set operations is special because it allows
for the result types from its inputs to be of different type family in
some cases. Namely, it is ok if one input has `types.Unknown` whereas
the other input is of some "known" type. In order to handle such case
the execbuilder plans casts; however, currently it is not possible to
plan casts to `Tuple` type. As a result, the vectorized engine is not
able to execute the query because it expects the data coming from both
inputs to be of the same type.

This commit modifies the logic of reconciling the result types from both
inputs to additionally update unknown types on the inputs if the other
input is of a Tuple type. Such behavior is acceptable because in the
original plan we only expected to have NULL values, and the Tuple type
is able to handle such case too.

The problem can also be reproduced in a logic test with the row-by-row
engine in the `fakedist` setting. This bug was introduced in
a76ee31e6a055842466335d3ee2141ac0a61e80e, and before that change we had
the following behavior: overwrite `plan.ResultTypes` to the merged types
and then plan a stage of distinct processors if we have UNION. This
resulted in the input spec for the input stream to distinct having
correctly set types. After the change, that was no longer the case.

Before that change the vectorized engine couldn't handle such queries
due to type mismatch during `SupportsVectorized` check, so we fell back
to the row-by-row engine. However, this commit makes it so that the
vectorized engine is able to execute such queries.

Release note (bug fix): CockroachDB previously could encounter an
internal error when performing UNION operation when the first input
resulted only in NULL values and consequent inputs produce tuples, and
this is now fixed. Only 21.1 alpha versions are affected.

pkg/sql/distsql_physical_planner.go
@@ -3268,7 +3268,7 @@ func (dsp *DistSQLPlanner) createPlanForSetOp(
 	p.PlanToStreamColMap = planToStreamColMap
 
 	// Merge the plans' result types and merge ordering.
-	resultTypes, err := mergeResultTypes(leftPlan.GetResultTypes(), rightPlan.GetResultTypes())
+	resultTypes, err := mergeResultTypesForSetOp(leftPlan, rightPlan)
 	if err != nil {
 		return nil, err
 	}

pkg/sql/distsql_plan_set_op.go
@@ -15,11 +15,14 @@ import (
 	"github.com/cockroachdb/errors"
 )
 
-// mergeResultTypes reconciles the ResultTypes between two plans. It enforces
-// that each pair of ColumnTypes must either match or be null, in which case the
-// non-null type is used. This logic is necessary for cases like
-// SELECT NULL UNION SELECT 1.
-func mergeResultTypes(left, right []*types.T) ([]*types.T, error) {
+// mergeResultTypesForSetOp reconciles the ResultTypes between two plans. It
+// enforces that each pair of ColumnTypes must either match or be null, in which
+// case the non-null type is used. This logic is necessary for cases like SELECT
+// NULL UNION SELECT 1.
+//
+// This method is intended to be used only for planning of set operations.
+func mergeResultTypesForSetOp(leftPlan, rightPlan *PhysicalPlan) ([]*types.T, error) {
+	left, right := leftPlan.GetResultTypes(), rightPlan.GetResultTypes()
 	if len(left) != len(right) {
 		return nil, errors.Errorf("ResultTypes length mismatch: %d and %d", len(left), len(right))
 	}
@@ -30,19 +33,34 @@ func mergeResultTypes(left, right []*types.T) ([]*types.T, error) {
 			merged[i] = leftType
 		} else if leftType.Family() == types.UnknownFamily {
 			merged[i] = rightType
-		} else if equivalentTypes(leftType, rightType) {
+		} else if leftType.Equivalent(rightType) {
+			// The types are equivalent for the purpose of UNION. Precision,
+			// Width, Oid, etc. do not affect the merging of values.
 			merged[i] = leftType
 		} else {
 			return nil, errors.Errorf(
 				"conflicting ColumnTypes: %s and %s", leftType.DebugString(), rightType.DebugString())
 		}
 	}
+	updateUnknownTypesForSetOp(leftPlan, merged)
+	updateUnknownTypesForSetOp(rightPlan, merged)
 	return merged, nil
 }
 
-// equivalentType checks whether a column type is equivalent to another for the
-// purpose of UNION. Precision, Width, Oid, etc. do not affect the merging of
-// values.
-func equivalentTypes(c, other *types.T) bool {
-	return c.Equivalent(other)
+// updateUnknownTypesForSetOp modifies plan's output types of the
+// types.UnknownFamily type family to be of the corresponding Tuple type coming
+// from the merged types. This is needed because at the moment the execbuilder
+// is not able to plan casts to tuples.
+//
+// This method is intended to be used only for planning of set operations.
+// TODO(yuzefovich): remove this once the execbuilder plans casts to tuples.
+func updateUnknownTypesForSetOp(plan *PhysicalPlan, merged []*types.T) {
+	currentTypes := plan.GetResultTypes()
+	for i := range merged {
+		if merged[i].Family() == types.TupleFamily && currentTypes[i].Family() == types.UnknownFamily {
+			for _, procIdx := range plan.ResultRouters {
+				plan.Processors[procIdx].Spec.ResultTypes[i] = merged[i]
+			}
+		}
+	}
 }

pkg/sql/distsql_plan_set_op_test.go
@@ -14,18 +14,22 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/physicalplan"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 )
 
-func TestMergeResultTypes(t *testing.T) {
+func TestMergeResultTypesForSetOp(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 
 	empty := []*types.T{}
 	null := []*types.T{types.Unknown}
 	typeInt := []*types.T{types.Int}
+	typeTuple := []*types.T{types.MakeTuple(typeInt)}
 
 	testData := []struct {
 		name     string
@@ -41,10 +45,32 @@ func TestMergeResultTypes(t *testing.T) {
 		{"left null", null, typeInt, &typeInt, false},
 		{"right null", typeInt, null, &typeInt, false},
 		{"both int", typeInt, typeInt, &typeInt, false},
+		{"left null, right tuple", null, typeTuple, &typeTuple, false},
+		{"right null, left tuple", typeTuple, null, &typeTuple, false},
 	}
+	checkUnknownTypesUpdate := func(plan PhysicalPlan, orig, merged []*types.T) {
+		for i, typ := range plan.GetResultTypes() {
+			if orig[i].Family() == types.UnknownFamily {
+				if typ.Family() == types.UnknownFamily && merged[i].Family() == types.TupleFamily {
+					t.Fatal("should have updated types NULL to tuple type on the original plan")
+				}
+				if typ.Family() != types.UnknownFamily && merged[i].Family() != types.TupleFamily {
+					t.Fatal("should have NOT updated types NULL to tuple type on the original plan")
+				}
+			}
+		}
+	}
+	infra := physicalplan.MakePhysicalInfrastructure(uuid.FastMakeV4(), roachpb.NodeID(1))
+	var leftPlan, rightPlan PhysicalPlan
+	leftPlan.PhysicalInfrastructure = &infra
+	rightPlan.PhysicalInfrastructure = &infra
+	leftPlan.ResultRouters = []physicalplan.ProcessorIdx{infra.AddProcessor(physicalplan.Processor{})}
+	rightPlan.ResultRouters = []physicalplan.ProcessorIdx{infra.AddProcessor(physicalplan.Processor{})}
 	for _, td := range testData {
 		t.Run(td.name, func(t *testing.T) {
-			result, err := mergeResultTypes(td.left, td.right)
+			leftPlan.Processors[0].Spec.ResultTypes = td.left
+			rightPlan.Processors[1].Spec.ResultTypes = td.right
+			result, err := mergeResultTypesForSetOp(&leftPlan, &rightPlan)
 			if td.err {
 				if err == nil {
 					t.Fatalf("expected error, got %+v", result)
@@ -57,6 +83,8 @@ func TestMergeResultTypes(t *testing.T) {
 			if !reflect.DeepEqual(*td.expected, result) {
 				t.Fatalf("expected %+v, got %+v", *td.expected, result)
 			}
+			checkUnknownTypesUpdate(leftPlan, td.left, result)
+			checkUnknownTypesUpdate(rightPlan, td.right, result)
 		})
 	}
 }

pkg/sql/logictest/testdata/logic_test/union
@@ -366,3 +366,27 @@ SELECT b FROM ab UNION SELECT a FROM ab
 ----
 1
 2
+
+# Regression test for the vectorized engine not being able to handle a UNION
+# between NULL and a tuple (#59611).
+statement ok
+CREATE TABLE t59611 (a INT);
+INSERT INTO t59611 VALUES (1)
+
+query T
+WITH
+  cte (cte_col)
+    AS (
+      SELECT
+        *
+      FROM
+        (VALUES ((SELECT NULL FROM t59611 LIMIT 1:::INT8)))
+      UNION SELECT * FROM (VALUES ((1, 2)))
+    )
+SELECT
+  NULL
+FROM
+  cte
+----
+NULL
+NULL