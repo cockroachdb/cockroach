0f490728f85f8356cdce100853eebf431a36add2
elliebababa <ellie24.huang@gmail.com>
2021-02-25 13:36:05 -0800

    cliccl/load: add `load show` args to display subset of backup metadata
    Previously, `cockroach load show <backup_path>` is used for debugging purposes,
    and displays all the metadata of a single backup manifest file.
    
    With this PR, we extend `load show` with more options to display subset of data.
    Also, we add support of "nodelocal://0/" access.
    
    Release justification: Non-production code changes
    Release note (cli change): Add `load show` args to display subset of backup metadata.
    Users can display subsets of metadata of a single manifest by using `load show files/descriptors/metadata/spans <backup_url>`.

--- pkg/ccl/cliccl/load.go
+++ pkg/ccl/cliccl/load.go
@@ -11,15 +11,16 @@ package cliccl
 import (
 	"context"
 	"fmt"
-	"strings"
-	"time"
-
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/blobs"
 	"github.com/cockroachdb/cockroach/pkg/ccl/backupccl"
 	"github.com/cockroachdb/cockroach/pkg/cli"
+	"github.com/cockroachdb/cockroach/pkg/cli/cliflags"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/security"
+	"github.com/cockroachdb/cockroach/pkg/server"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catalogkv"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/storage/cloud"
 	"github.com/cockroachdb/cockroach/pkg/storage/cloudimpl"
@@ -28,13 +29,26 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/errors"
 	"github.com/spf13/cobra"
+	"path/filepath"
+	"strings"
+	"time"
 )
+
+const (
+	descriptors = "descriptors"
+	files = "files"
+	spans = "spans"
+	metadata ="metadata"
+)
+
+var externalIODir string
 
 func init() {
 	loadShowCmd := &cobra.Command{
-		Use:   "show <basepath>",
+		Use:   "show [descriptors|files|spans|metadata] <backup_path>",
 		Short: "show backups",
-		Long:  "Shows information about a SQL backup.",
+		Long:  "Shows subset(s) of meta information about a SQL backup.",
+		Args: cobra.MinimumNArgs(1),
 		RunE:  cli.MaybeDecorateGRPCError(runLoadShow),
 	}
 
@@ -46,35 +60,117 @@ func init() {
 			return cmd.Usage()
 		},
 	}
+
+	f := loadCmds.Flags()
+	f.StringVarP(
+		&externalIODir,
+		cliflags.ExternalIODir.Name,
+		cliflags.ExternalIODir.Shorthand,
+		""/*value*/,
+		cliflags.ExternalIODir.Usage())
+
 	cli.AddCmd(loadCmds)
 	loadCmds.AddCommand(loadShowCmd)
 }
 
+func newBlobFactory(ctx context.Context, dialing roachpb.NodeID) (blobs.BlobClient, error) {
+	if dialing != 0 {
+			return nil, errors.Errorf(`only support nodelocal (0/self) under offline inspection`)
+		}
+	if externalIODir == "" {
+		externalIODir = filepath.Join(server.DefaultStorePath, "extern")
+	}
+	return blobs.NewLocalClient(externalIODir)
+}
+
+func parseShowArgs(args [] string) (options map[string]bool, path string, err error) {
+	options = make(map[string]bool)
+	for _, arg := range args {
+		switch strings.ToLower(arg) {
+		case descriptors:
+			options[descriptors] = true
+		case files:
+			options[files] = true
+		case spans:
+			options[spans] = true
+		case metadata:
+			options[metadata] = true
+		default:
+			if path != "" {
+				return nil, "", errors.New("more than one path is specifiied")
+			}
+			path = arg
+		}
+	}
+
+	if len(options) == 0 {
+		options[descriptors] = true
+		options[files] = true
+		options[spans] = true
+		options[metadata] = true
+	}
+
+	if len(args) == len(options) {
+			return nil, "", errors.New("backup_path argument is required")
+	}
+	return options, path, nil
+}
+
 func runLoadShow(cmd *cobra.Command, args []string) error {
-	if len(args) != 1 {
-		return errors.New("basepath argument is required")
+
+	var options map[string]bool
+	var path string
+	var err error
+	if options, path, err = parseShowArgs(args); err!=nil {
+		return err
+	}
+
+	var showHeaders bool
+	if len(options) > 1 {
+		showHeaders = true
 	}
 
 	ctx := context.Background()
-	basepath := args[0]
-	if !strings.Contains(basepath, "://") {
-		basepath = cloudimpl.MakeLocalStorageURI(basepath)
+	if !strings.Contains(path, "://") {
+		path = cloudimpl.MakeLocalStorageURI(path)
 	}
 
 	externalStorageFromURI := func(ctx context.Context, uri string,
 		user security.SQLUsername) (cloud.ExternalStorage, error) {
 		return cloudimpl.ExternalStorageFromURI(ctx, uri, base.ExternalIODirConfig{},
-			cluster.NoSettings, blobs.TestEmptyBlobClientFactory, user, nil, nil)
+			cluster.NoSettings, newBlobFactory, user, nil/*Internal Executor*/, nil/*kvDB*/)
 	}
+
 	// This reads the raw backup descriptor (with table descriptors possibly not
 	// upgraded from the old FK representation, or even older formats). If more
 	// fields are added to the output, the table descriptors may need to be
 	// upgraded.
-	desc, err := backupccl.ReadBackupManifestFromURI(ctx, basepath, security.RootUserName(),
+	desc, err := backupccl.ReadBackupManifestFromURI(ctx, path, security.RootUserName(),
 		externalStorageFromURI, nil)
 	if err != nil {
 		return err
 	}
+
+	if _, ok := options[metadata]; ok {
+		showMeta(desc)
+	}
+
+	if _, ok := options[spans]; ok {
+		showSpans(desc, showHeaders)
+	}
+
+	if _, ok := options[files]; ok {
+		showFiles(desc, showHeaders)
+	}
+
+	if _, ok := options[descriptors]; ok {
+		showDescriptor(desc)
+	}
+
+	return nil
+}
+
+func showMeta(desc backupccl.BackupManifest) {
 	start := timeutil.Unix(0, desc.StartTime.WallTime).Format(time.RFC3339Nano)
 	end := timeutil.Unix(0, desc.EndTime.WallTime).Format(time.RFC3339Nano)
 	fmt.Printf("StartTime: %s (%s)\n", start, desc.StartTime)
@@ -86,32 +182,60 @@ func runLoadShow(cmd *cobra.Command, args []string) error {
 	fmt.Printf("ClusterID: %s\n", desc.ClusterID)
 	fmt.Printf("NodeID: %s\n", desc.NodeID)
 	fmt.Printf("BuildInfo: %s\n", desc.BuildInfo.Short())
-	fmt.Printf("Spans:\n")
+}
+
+func showSpans(desc backupccl.BackupManifest, showHeaders bool) {
+	tabfmt:= ""
+	if showHeaders {
+		fmt.Printf("Spans:\n")
+		tabfmt = "\t"
+	}
 	for _, s := range desc.Spans {
-		fmt.Printf("	%s\n", s)
+		fmt.Printf("%s%s\n", tabfmt, s)
 	}
-	fmt.Printf("Files:\n")
+}
+
+func showFiles(desc backupccl.BackupManifest, showHeaders bool) {
+	tabfmt:= ""
+	if showHeaders {
+		fmt.Printf("Files:\n")
+		tabfmt = "\t"
+	}
 	for _, f := range desc.Files {
-		fmt.Printf("	%s:\n", f.Path)
-		fmt.Printf("		Span: %s\n", f.Span)
-		fmt.Printf("		Sha512: %0128x\n", f.Sha512)
-		fmt.Printf("		DataSize: %d (%s)\n", f.EntryCounts.DataSize, humanizeutil.IBytes(f.EntryCounts.DataSize))
-		fmt.Printf("		Rows: %d\n", f.EntryCounts.Rows)
-		fmt.Printf("		IndexEntries: %d\n", f.EntryCounts.IndexEntries)
+		fmt.Printf("%s%s:\n", tabfmt, f.Path)
+		fmt.Printf("	Span: %s\n", f.Span)
+		fmt.Printf("	Sha512: %0128x\n", f.Sha512)
+		fmt.Printf("	DataSize: %d (%s)\n", f.EntryCounts.DataSize, humanizeutil.IBytes(f.EntryCounts.DataSize))
+		fmt.Printf("	Rows: %d\n", f.EntryCounts.Rows)
+		fmt.Printf("	IndexEntries: %d\n", f.EntryCounts.IndexEntries)
 	}
+}
+
+func showDescriptor(desc backupccl.BackupManifest) {
 	// Note that these descriptors could be from any past version of the cluster,
 	// in case more fields need to be added to the output.
-	fmt.Printf("Descriptors:\n")
+	dbIDToName := make(map[descpb.ID]string)
 	for i := range desc.Descriptors {
 		d := &desc.Descriptors[i]
-		if desc := descpb.TableFromDescriptor(d, hlc.Timestamp{}); desc != nil {
-			fmt.Printf("	%d: %s (table)\n",
-				descpb.GetDescriptorID(d), descpb.GetDescriptorName(d))
-		}
-		if desc := d.GetDatabase(); desc != nil {
-			fmt.Printf("	%d: %s (database)\n",
-				descpb.GetDescriptorID(d), descpb.GetDescriptorName(d))
+		id := descpb.GetDescriptorID(d)
+		if d.GetDatabase() != nil {
+			dbIDToName[id] = descpb.GetDescriptorName(d)
 		}
 	}
-	return nil
+
+	fmt.Printf("Databases:\n")
+	for i := range dbIDToName {
+		fmt.Printf("	%s\n",
+			dbIDToName[i])
+	}
+
+	fmt.Printf("Tables:\n")
+	for i := range desc.Descriptors {
+		d := &desc.Descriptors[i]
+		if descpb.TableFromDescriptor(d, hlc.Timestamp{}) != nil {
+			desc := catalogkv.UnwrapDescriptorRaw(nil, d)
+			fmt.Printf("	%s (%s) \n",
+				descpb.GetDescriptorName(d), dbIDToName[desc.GetParentID()])
+		}
+	}
 }
