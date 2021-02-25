569def4566f47122b6f79a9e629b18ae92e93762
Alfonso Subiotto Marques <alfonso@cockroachlabs.com>
2021-02-25 14:51:01 -0500

    colexec: call StartInternal in Columnarizer
    Release justification: fixes a possible crash.
    
    Release note (bug fix): fixed an npe observed with a SpanFromContext call in
    the stack trace.

--- pkg/sql/colexec/columnarizer.go
+++ pkg/sql/colexec/columnarizer.go
@@ -132,6 +132,7 @@ func (c *Columnarizer) Init() {
 	// we have this check in place.
 	if c.initStatus == colexecop.OperatorNotInitialized {
 		c.accumulatedMeta = make([]execinfrapb.ProducerMetadata, 0, 1)
+		c.ctx = c.StartInternal(c.ctx, "columnarizer")
 		c.input.Start(c.ctx)
 		c.initStatus = colexecop.OperatorInitialized
 	}
@@ -235,7 +236,7 @@ func (c *Columnarizer) DrainMeta(ctx context.Context) []execinfrapb.ProducerMeta
 
 // Close is part of the Operator interface.
 func (c *Columnarizer) Close(ctx context.Context) error {
-	c.input.ConsumerClosed()
+	c.InternalClose()
 	return nil
 }
 
