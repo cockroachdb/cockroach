e6bc656204db7b8062e3ace1b981ddff9f63f3b5
Paul Bardea <pbardea@gmail.com>
2021-03-05 18:35:13 -0500

    backupccl,backupbase: change backup base to backupresolved
    Backupbase was a bit too generic and had quite of a bit of backup
    specific info that didn't need to be shared outside of backupccl. It was
    only this way due to some unfortunate dependencies. This commit cleans
    up those dependencies, and backuptargets is a better suited name to
    describe it's functionality.
    
    Release justification: low risk (just code-movement) change, which no
    expected impact. Merging in this release will make future backports
    easier.
    Release note: None

--- pkg/ccl/backupccl/targets.go
+++ pkg/ccl/backupccl/targets.go
@@ -12,7 +12,7 @@ import (
 	"context"
 	"sort"
 
-	"github.com/cockroachdb/cockroach/pkg/ccl/backupccl/backupbase"
+	"github.com/cockroachdb/cockroach/pkg/ccl/backupccl/backupresolver"
 	"github.com/cockroachdb/cockroach/pkg/ccl/storageccl"
 	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/kv"
@@ -21,7 +21,9 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catalogkeys"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catalogkv"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/dbdesc"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/systemschema"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgcode"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
@@ -89,7 +91,7 @@ func getRelevantDescChanges(
 	}
 
 	if !startTime.IsEmpty() {
-		starting, err := backupbase.LoadAllDescs(ctx, codec, db, startTime)
+		starting, err := backupresolver.LoadAllDescs(ctx, codec, db, startTime)
 		if err != nil {
 			return nil, err
 		}
@@ -340,7 +342,7 @@ func CheckObjectExists(
 func fullClusterTargetsRestore(
 	allDescs []catalog.Descriptor, lastBackupManifest BackupManifest,
 ) ([]catalog.Descriptor, []catalog.DatabaseDescriptor, []descpb.TenantInfo, error) {
-	fullClusterDescs, fullClusterDBs, err := backupbase.FullClusterTargets(allDescs)
+	fullClusterDescs, fullClusterDBs, err := fullClusterTargets(allDescs)
 	if err != nil {
 		return nil, nil, nil, err
 	}
@@ -361,6 +363,65 @@ func fullClusterTargetsRestore(
 	tenants := lastBackupManifest.Tenants
 
 	return filteredDescs, filteredDBs, tenants, nil
+}
+
+// fullClusterTargets returns all of the tableDescriptors to be included in a
+// full cluster backup, and all the user databases.
+func fullClusterTargets(
+	allDescs []catalog.Descriptor,
+) ([]catalog.Descriptor, []*dbdesc.Immutable, error) {
+	fullClusterDescs := make([]catalog.Descriptor, 0, len(allDescs))
+	fullClusterDBs := make([]*dbdesc.Immutable, 0)
+
+	systemTablesToBackup := GetSystemTablesToIncludeInClusterBackup()
+
+	for _, desc := range allDescs {
+		switch desc := desc.(type) {
+		case catalog.DatabaseDescriptor:
+			dbDesc := dbdesc.NewImmutable(*desc.DatabaseDesc())
+			fullClusterDescs = append(fullClusterDescs, desc)
+			if dbDesc.GetID() != systemschema.SystemDB.GetID() {
+				// The only database that isn't being fully backed up is the system DB.
+				fullClusterDBs = append(fullClusterDBs, dbDesc)
+			}
+		case catalog.TableDescriptor:
+			if desc.GetParentID() == keys.SystemDatabaseID {
+				// Add only the system tables that we plan to include in a full cluster
+				// backup.
+				if _, ok := systemTablesToBackup[desc.GetName()]; ok {
+					fullClusterDescs = append(fullClusterDescs, desc)
+				}
+			} else {
+				// Add all user tables that are not in a DROP state.
+				if desc.GetState() != descpb.DescriptorState_DROP {
+					fullClusterDescs = append(fullClusterDescs, desc)
+				}
+			}
+		case catalog.SchemaDescriptor:
+			fullClusterDescs = append(fullClusterDescs, desc)
+		case catalog.TypeDescriptor:
+			fullClusterDescs = append(fullClusterDescs, desc)
+		}
+	}
+	return fullClusterDescs, fullClusterDBs, nil
+}
+
+// fullClusterTargetsBackup returns the same descriptors referenced in
+// fullClusterTargets, but rather than returning the entire database
+// descriptor as the second argument, it only returns their IDs.
+func fullClusterTargetsBackup(
+	allDescs []catalog.Descriptor,
+) ([]catalog.Descriptor, []descpb.ID, error) {
+	fullClusterDescs, fullClusterDBs, err := fullClusterTargets(allDescs)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	fullClusterDBIDs := make([]descpb.ID, 0)
+	for _, desc := range fullClusterDBs {
+		fullClusterDBIDs = append(fullClusterDBIDs, desc.GetID())
+	}
+	return fullClusterDescs, fullClusterDBIDs, nil
 }
 
 func selectTargets(
@@ -388,7 +449,7 @@ func selectTargets(
 		return nil, nil, nil, errors.Errorf("tenant %d not in backup", targets.Tenant.ToUint64())
 	}
 
-	matched, err := backupbase.DescriptorsMatchingTargets(ctx,
+	matched, err := backupresolver.DescriptorsMatchingTargets(ctx,
 		p.CurrentDatabase(), p.CurrentSearchPath(), allDescs, targets)
 	if err != nil {
 		return nil, nil, nil, err
