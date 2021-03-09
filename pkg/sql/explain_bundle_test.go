3a4aa211389f507f501a900d82942bc06d432043
Rafi Shamim <rafi@cockroachlabs.com>
2021-03-09 17:18:40 -0500

    sql/*: stop defaulting to public schema in various places
    In addition to the release note below, this also fixes the error message
    that would appear when trying to drop a role that owns a table in a
    user-defined schema.
    
    Release note (bug fix): Using EXPLAIN (OPT, ENV) on a query that
    references a table in a user-defined schema would fail previously. This
    is now fixed.

--- pkg/sql/explain_bundle_test.go
+++ pkg/sql/explain_bundle_test.go
@@ -16,6 +16,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"math/rand"
 	"regexp"
 	"sort"
@@ -41,7 +42,9 @@ func TestExplainAnalyzeDebug(t *testing.T) {
 	srv, godb, _ := serverutils.StartServer(t, base.TestServerArgs{Insecure: true})
 	defer srv.Stopper().Stop(ctx)
 	r := sqlutils.MakeSQLRunner(godb)
-	r.Exec(t, "CREATE TABLE abc (a INT PRIMARY KEY, b INT, c INT UNIQUE)")
+	r.Exec(t, `CREATE TABLE abc (a INT PRIMARY KEY, b INT, c INT UNIQUE);
+CREATE SCHEMA s;
+CREATE TABLE s.a (a INT PRIMARY KEY);`)
 
 	base := "statement.txt trace.json trace.txt trace-jaeger.json env.sql"
 	plans := "schema.sql opt.txt opt-v.txt opt-vv.txt plan.txt"
@@ -56,7 +59,7 @@ func TestExplainAnalyzeDebug(t *testing.T) {
 	t.Run("basic", func(t *testing.T) {
 		rows := r.QueryStr(t, "EXPLAIN ANALYZE (DEBUG) SELECT * FROM abc WHERE c=1")
 		checkBundle(
-			t, fmt.Sprint(rows),
+			t, fmt.Sprint(rows), "public.abc",
 			base, plans, "stats-defaultdb.public.abc.sql", "distsql.html",
 		)
 	})
@@ -65,8 +68,16 @@ func TestExplainAnalyzeDebug(t *testing.T) {
 	t.Run("subqueries", func(t *testing.T) {
 		rows := r.QueryStr(t, "EXPLAIN ANALYZE (DEBUG) SELECT EXISTS (SELECT * FROM abc WHERE c=1)")
 		checkBundle(
-			t, fmt.Sprint(rows),
+			t, fmt.Sprint(rows), "public.abc",
 			base, plans, "stats-defaultdb.public.abc.sql", "distsql-2-main-query.html distsql-1-subquery.html",
+		)
+	})
+
+	t.Run("user-defined schema", func(t *testing.T) {
+		rows := r.QueryStr(t, "EXPLAIN ANALYZE (DEBUG) SELECT * FROM s.a WHERE a=1")
+		checkBundle(
+			t, fmt.Sprint(rows), "s.a",
+			base, plans, "stats-defaultdb.s.a.sql", "distsql.html",
 		)
 	})
 
@@ -79,7 +90,7 @@ func TestExplainAnalyzeDebug(t *testing.T) {
 		// The bundle url is inside the error detail.
 		var pqErr *pq.Error
 		_ = errors.As(err, &pqErr)
-		checkBundle(t, fmt.Sprintf("%+v", pqErr.Detail), base)
+		checkBundle(t, fmt.Sprintf("%+v", pqErr.Detail), "", base)
 	})
 
 	// Verify that we can issue the statement with prepare (which can happen
@@ -104,7 +115,7 @@ func TestExplainAnalyzeDebug(t *testing.T) {
 			rowsBuf.WriteByte('\n')
 		}
 		checkBundle(
-			t, rowsBuf.String(),
+			t, rowsBuf.String(), "public.abc",
 			base, plans, "stats-defaultdb.public.abc.sql", "distsql.html",
 		)
 	})
@@ -114,7 +125,7 @@ func TestExplainAnalyzeDebug(t *testing.T) {
 // bundle contains the expected files. The expected files are passed as an
 // arbitrary number of strings; each string contains one or more filenames
 // separated by a space.
-func checkBundle(t *testing.T, text string, expectedFiles ...string) {
+func checkBundle(t *testing.T, text, tableName string, expectedFiles ...string) {
 	t.Helper()
 	reg := regexp.MustCompile("http://[a-zA-Z0-9.:]*/_admin/v1/stmtbundle/[0-9]*")
 	url := reg.FindString(text)
@@ -143,6 +154,25 @@ func checkBundle(t *testing.T, text string, expectedFiles ...string) {
 			t.Fatalf("file %s is empty", f.Name)
 		}
 		files = append(files, f.Name)
+
+		if f.Name == "schema.sql" {
+			r, err := f.Open()
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer r.Close()
+			contents, err := ioutil.ReadAll(r)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !strings.Contains(string(contents), tableName) {
+				t.Errorf(
+					"expected table name to appear in schema.sql. tableName: %s\nfile contents:\n%s",
+					tableName,
+					string(contents),
+				)
+			}
+		}
 	}
 
 	var expList []string
