bf953d407731936466808b6b01e0f7ddda2b8487
Radu Berinde <radu@cockroachlabs.com>
2021-03-10 18:00:00 -0500

    sql: show time since table stats were collected in EXPLAIN
    Example:
    ```
    EXPLAIN SELECT * FROM rides JOIN vehicles ON rides.vehicle_id = vehicles.id;
                                             info
    --------------------------------------------------------------------------------------
      distribution: full
      vectorized: true
    
      • hash join
      │ estimated row count: 500
      │ equality: (vehicle_id) = (id)
      │
      ├── • scan
      │     estimated row count: 500 (100% of the table; stats collected 15 seconds ago)
      │     table: rides@primary
      │     spans: FULL SCAN
      │
      └── • scan
            estimated row count: 15 (100% of the table; stats collected 11 seconds ago)
            table: vehicles@primary
            spans: FULL SCAN
    (16 rows)
    ```
    
    Durations are rounded to the nearest unit (second, minute, hour, day).
    
    Fixes #57517.
    
    Release note (sql change): EXPLAIN and EXPLAIN ANALYZE now show how
    long ago table statistics were collected.

--- pkg/util/humanizeutil/duration_test.go
+++ pkg/util/humanizeutil/duration_test.go
@@ -52,3 +52,34 @@ func TestDuration(t *testing.T) {
 		})
 	}
 }
+
+func TestLongDuration(t *testing.T) {
+	testCases := []struct {
+		val time.Duration
+		exp string
+	}{
+		{val: 0, exp: "0 seconds"},
+		{val: time.Second, exp: "1 second"},
+		{val: time.Second + 500*time.Millisecond, exp: "2 seconds"},
+		{val: 50 * time.Second, exp: "50 seconds"},
+		{val: time.Minute, exp: "1 minute"},
+		{val: time.Minute + 20*time.Second, exp: "1 minute"},
+		{val: time.Minute + 30*time.Second, exp: "2 minutes"},
+		{val: 50 * time.Minute, exp: "50 minutes"},
+		{val: time.Hour, exp: "1 hour"},
+		{val: time.Hour + 10*time.Minute, exp: "1 hour"},
+		{val: time.Hour + 30*time.Minute, exp: "2 hours"},
+		{val: 24 * time.Hour, exp: "1 day"},
+		{val: 24*time.Hour + 10*time.Hour, exp: "1 day"},
+		{val: 24*time.Hour + 12*time.Hour, exp: "2 days"},
+		{val: 1234 * 24 * time.Hour, exp: "1234 days"},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.val.String(), func(t *testing.T) {
+			res := LongDuration(tc.val)
+			if res != tc.exp {
+				t.Errorf("expected '%s', got '%s'", tc.exp, res)
+			}
+		})
+	}
+}
