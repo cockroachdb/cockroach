ad95856a7be6e4f1b277aaa3345a6867b941c045
Rafi Shamim <rafi@cockroachlabs.com>
2021-03-18 11:52:48 -0400

    mutations: don't use inverted indexes for postgres
    Release note: None

--- pkg/sql/mutations/mutations.go
+++ pkg/sql/mutations/mutations.go
@@ -641,18 +641,19 @@ func postgresCreateTableMutator(
 			for _, def := range stmt.Defs {
 				switch def := def.(type) {
 				case *tree.IndexTableDef:
-					// Postgres doesn't support
-					// indexes in CREATE TABLE,
-					// so split them out to their
-					// own statement.
-					mutated = append(mutated, &tree.CreateIndex{
-						Name:     def.Name,
-						Table:    stmt.Table,
-						Inverted: def.Inverted,
-						Columns:  def.Columns,
-						Storing:  def.Storing,
-					})
-					changed = true
+					// Postgres doesn't support indexes in CREATE TABLE, so split them out
+					// to their own statement. Postgres also doesn't support inverted
+					// indexes at all.
+					if !def.Inverted {
+						mutated = append(mutated, &tree.CreateIndex{
+							Name:     def.Name,
+							Table:    stmt.Table,
+							Inverted: def.Inverted,
+							Columns:  def.Columns,
+							Storing:  def.Storing,
+						})
+						changed = true
+					}
 				case *tree.UniqueConstraintTableDef:
 					if def.PrimaryKey {
 						// Postgres doesn't support descending PKs.
