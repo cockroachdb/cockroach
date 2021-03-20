ade28bed23633c186c42e87b1c108952ab624a3f
Raphael 'kena' Poss <knz@thaumogen.net>
2021-03-20 17:13:59 +0100

    cli/sql: properly format different times and floats
    Prior to this patch, the SQL shell would improperly format
    variants of timestamps (time, date) and mistakenly
    use the same format for 32-bit and 64-bit float.
    This patch corrects this.
    
    Release note (cli change): The SQL shell (`cockroach demo`, `cockroach
    sql`) now attempts to better format values that are akin to time/date,
    as well as floating-point numbers.

--- pkg/util/timeutil/timeutil.go
+++ pkg/util/timeutil/timeutil.go
@@ -13,3 +13,18 @@ package timeutil
 // FullTimeFormat is the time format used to display any timestamp
 // with date, time and time zone data.
 const FullTimeFormat = "2006-01-02 15:04:05.999999-07:00:00"
+
+// TimestampWithoutTZFormat is the time format used to display
+// timestamps without a time zone offset.
+const TimestampWithoutTZFormat = "2006-01-02 15:04:05.999999"
+
+// TimeWithTZFormat is the time format used to display a time
+// with a time zone offset.
+const TimeWithTZFormat = "15:04:05.999999-07:00:00"
+
+// TimeWithoutTZFormat is the time format used to display a time
+// without a time zone offset.
+const TimeWithoutTZFormat = "15:04:05.999999"
+
+// DateFormat is the time format used to display a date.
+const DateFormat = "2006-01-02"
