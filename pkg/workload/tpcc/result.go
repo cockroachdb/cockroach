84c01369dd942b31f409aba8730c1e0219dcc463
Tobias Grieger <tobias.b.grieger@gmail.com>
2021-03-22 16:45:28 +0100

    roachtest: handle VM overload under tpccbench
    See https://github.com/cockroachdb/cockroach/pull/62039.
    
    `tpccbench`, by design, pushes CRDB into overload territory. However,
    tpccbench was not handling the resulting conditions at all. Any node
    death or stalling VM would fail the whole test run, rather than retry
    again with a lower warehouse count.
    
    This commit makes most errors "recoverable" in the sense that they
    will simply treat the run as failing, but continue the line search.
    Exceptions are communicated via `t.Fatal`, which will abort the
    whole run instead.
    
    We also make the `c.Stop()` step at the beginning of each search
    step resilient to VMs browning out under memory pressure (from
    the previous run), by patiently retrying for a few minutes.
    
    The hope is that this will allow the search to run to completion, even
    in the face of overload-imposed temporary VM outages. It is not expected
    to do this perfectly - after all, VMs don't generally return from
    brown-out within any fixed time period - but at least anecdotally most
    VMs seem to come back a few minutes in.
    
    Release note: None

--- pkg/workload/tpcc/result.go
+++ pkg/workload/tpcc/result.go
@@ -55,7 +55,6 @@ var passing90ThPercentile = map[string]time.Duration{
 
 // Result represents the outcome of a TPCC run.
 type Result struct {
-
 	// ActiveWarehouses is the number of warehouses used in the TPC-C run.
 	ActiveWarehouses int
 
@@ -155,7 +154,11 @@ func NewResultWithSnapshots(
 // TpmC returns a tpmC value with a warehouse factor of 12.86.
 // TpmC will panic if r does not contain a "newOrder" histogram in Cumulative.
 func (r *Result) TpmC() float64 {
-	return float64(r.Cumulative["newOrder"].TotalCount()) / (r.Elapsed.Seconds() / 60)
+	no := r.Cumulative["newOrder"]
+	if no == nil {
+		return 0
+	}
+	return float64(no.TotalCount()) / (r.Elapsed.Seconds() / 60)
 }
 
 // Efficiency returns the efficiency of a TPC-C run.
@@ -181,7 +184,7 @@ func (r *Result) FailureError() error {
 	var err error
 	if eff := r.Efficiency(); eff < PassingEfficiency {
 		err = errors.CombineErrors(err,
-			errors.Errorf("efficiency value of %v is below ppassing threshold of %v",
+			errors.Errorf("efficiency value of %v is below passing threshold of %v",
 				eff, PassingEfficiency))
 	}
 	for query, max90th := range passing90ThPercentile {
