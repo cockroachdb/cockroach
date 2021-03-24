79c74e9519605a8603fa4a6602b441de2ebbdbde
Andrii Vorobiov <and.vorobiov@gmail.com>
2021-03-24 10:56:01 +0200

    cluster-ui: re-export uiConfiguration actions
    cluster-ui package has to re-export uiConfig actions to allow external apps
    to change the reducers state in type-safe way - with action creator and
    provided types.

--- pkg/ui/cluster-ui/src/store/index.ts
+++ pkg/ui/cluster-ui/src/store/index.ts
@@ -1,3 +1,4 @@
 export { sagas } from "./sagas";
 export { notificationAction } from "./notifications";
+export { actions as uiConfigActions, UIConfigState } from "./uiConfig";
 export { rootReducer, AppState, rootActions } from "./reducers";
