102f97d8ddb81c9585d90dd528eb20ad5410a40f
Rafi Shamim <rafi@cockroachlabs.com>
2021-03-29 00:32:52 -0400

    mutations: allow spatial types for Postgres
    Release note: None

--- pkg/sql/mutations/mutations.go
+++ pkg/sql/mutations/mutations.go
@@ -686,25 +686,6 @@ func postgresCreateTableMutator(
 						Storing:  def.Storing,
 					})
 					changed = true
-				case *tree.ColumnTableDef:
-					colType := tree.MustBeStaticallyKnownType(def.Type)
-					switch colType.Family() {
-					case types.GeometryFamily, types.GeographyFamily, types.Box2DFamily:
-						// PostgreSQL does not have any spatial types. Turn them into
-						// String types.
-						def.Type = types.String
-						mutated = append(mutated, &tree.AlterTable{
-							Table: stmt.Table.ToUnresolvedObjectName(),
-							Cmds: tree.AlterTableCmds{
-								&tree.AlterTableAlterColumnType{
-									Column: def.Name,
-									ToType: types.String,
-								},
-							},
-						})
-						changed = true
-					}
-					newdefs = append(newdefs, def)
 				default:
 					newdefs = append(newdefs, def)
 				}
