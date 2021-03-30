ff9533c86f6f8ac3530a3ecf9adcc42efddd5db4
Oliver Tan <otan@cockroachlabs.com>
2021-03-31 10:01:00 +1100

    sql: add crdb_internal.validate_multi_region_zone_configs builtin
    In addition to the release note, added some pgcodes to validation errors
    as well as minor refactoring to make it work and error nicely with the
    "overriding" error message.
    
    Also fix up some of the string escaping to use tree.Name.
    
    Release note (sql change): Add a new builtin
    `crdb_internal.validate_multi_region_zone_configs` which validates all
    zone configurations in the current database are correctly configured.

--- pkg/ccl/importccl/import_table_creation.go
+++ pkg/ccl/importccl/import_table_creation.go
@@ -223,6 +223,13 @@ func (so *importSequenceOperators) CurrentDatabaseRegionConfig(
 	return nil, errors.WithStack(errSequenceOperators)
 }
 
+// ValidateAllMultiRegionZoneConfigsInCurrentDatabase is part of the tree.EvalDatabase interface.
+func (so *importSequenceOperators) ValidateAllMultiRegionZoneConfigsInCurrentDatabase(
+	_ context.Context,
+) error {
+	return errors.WithStack(errSequenceOperators)
+}
+
 // Implements the tree.EvalDatabase interface.
 func (so *importSequenceOperators) ParseQualifiedTableName(sql string) (*tree.TableName, error) {
 	name, err := parser.ParseTableName(sql)
