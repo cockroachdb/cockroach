38156c0209b83e87b96ffda7c529476f69ff04e1
Tobias Grieger <tobias.b.grieger@gmail.com>
2021-03-30 19:01:47 +0200

    repro script
    Release note (<category, see below>): <what> <show> <why>

---
+++ roachstress.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euxo pipefail
+
+sha=$(git rev-parse HEAD)
+
+if [ ! -f roachtest.$sha ]; then
+	./build/builder.sh mkrelease amd64-linux-gnu bin/{roach{prod,test},workload}
+	mv -f bin.docker_amd64/roachprod roachprod.$sha
+	mv -f bin.docker_amd64/workload workload.$sha
+	mv -f bin.docker_amd64/roachtest roachtest.$sha
+fi
+
+if [ ! -f cockroach.$sha ]; then
+	git clean -xffd ./pkg
+	./build/builder.sh mkrelease amd64-linux-gnu
+	mv cockroach-linux-2.6.32-gnu-amd64 cockroach.$sha
+fi
+
+TEST=hotspotsplits/nodes=4
+time caffeinate -- ./roachtest.$sha run "${TEST}" --port 8080 --debug --count 800 --cpu-quota 1600 --roachprod roachprod.${sha} --workload workload.${sha} --cockroach ./cockroach.$sha --artifacts artifacts.$sha
+
