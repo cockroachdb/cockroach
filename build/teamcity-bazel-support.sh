af4b94e9d1107bed6cdd0503256da8ff67268137
Ricky Stewart <ricky@cockroachlabs.com>
2021-04-01 17:40:05 -0500

    bazel: refactor bazel support code for teamcity
    Until now we've been doing ad-hoc calls to `docker run` to perform Bazel
    builds; this isn't scalable as any significant breaking changes have
    required updating all call sites. Refactor all the common logic for our
    Bazel builds into a common script, `teamcity-bazel-support.sh`. Notes:
    
    * run_bazel() is a new Bash function which all Bazel builds should go
      through.
    * run_bazel() sets up the `artifacts/` directory and all the Docker
      container's volumes, unless the `TEAMCITY_BAZEL_SUPPORT_LINT` variable
      is set, in which case we invoke `docker run` with slightly different
      arguments that are more appropriate for lints.
    * run_bazel() always mounts the Bazel cache in the same location, so now
      we're caching the results of Bazel builds on a per-agent basis(!!!)
    * No longer include `bazel{build,test}.sh` in the `cockroachdb/bazel`
      Docker image -- these files can just live in tree, and we don't want
      updates to them to require updating the Docker image.
    
    Now that we've standardized our Bazel builds, it'll be that much easier
    to make changes across all our Bazel builds, as we need to for testing
    UX improvements that are coming.
    
    Release note: None

--- build/teamcity-bazel-support.sh
+++ build/teamcity-bazel-support.sh
@@ -1,1 +1,38 @@
-BAZEL_IMAGE=cockroachdb/bazel:20210315-115417
+BAZEL_IMAGE=cockroachdb/bazel:20210401-180049
+
+# Call `run_bazel $NAME_OF_SCRIPT` to start an appropriately-configured Docker
+# container with the `cockroachdb/bazel` image running the given script.
+#
+# Set the TEAMCITY_BAZEL_SUPPORT_LINT variable for lints -- this will mount the
+# workspace as writeable in the Docker container, will avoid setting up the
+# artifacts directory, etc.
+run_bazel() {
+    if [ -z "${root:-}" ]
+    then
+        echo '$root is not set; please source teamcity-support.sh'
+        exit 1
+    fi
+
+    # Bazel configuration for CI.
+    cp $root/.bazelrc.ci $root/.bazelrc.user
+
+    # Set up volumes.
+    vols="--volume /home/agent/.bazelcache:/root/.cache/bazel"
+
+    workspace_vol="--volume ${root}:/go/src/github.com/cockroachdb/cockroach"
+    if [ -z "${TEAMCITY_BAZEL_SUPPORT_LINT:-}" ]
+    then
+        artifacts_dir=$root/artifacts
+        mkdir -p "$artifacts_dir"
+
+        vols="${vols} ${workspace_vol}:ro"
+        vols="${vols} --volume ${artifacts_dir}:/artifacts"
+    else
+        vols="${vols} ${workspace_vol}"
+    fi
+
+    docker run -i ${tty-} --rm --init \
+        --workdir="/go/src/github.com/cockroachdb/cockroach" \
+        ${vols} \
+        $BAZEL_IMAGE "$@"
+}
