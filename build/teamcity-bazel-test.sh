ea54d1640d90c4f833436f0c1866830fb4e7d218
Ricky Stewart <ricky@cockroachlabs.com>
2021-04-01 17:40:05 -0500

    bazel: refactor bazel support code for teamcity
    Until now we've been doing ad-hoc calls to `docker run` to perform Bazel
    builds; this isn't scalable as any significant breaking changes have
    required updating all call sites. Refactor all the common logic for our
    Bazel builds into a common script, `teamcity-bazel-support.sh`. Notes:
    
    * run_bazel() is a new Bash function which all Bazel builds should go
      through.
    * run_bazel() sets up the `artifacts/` directory and all the Docker
      container's volumes, unless the `TEAMCITY_BAZEL_SUPPORT_LINT` variable
      is set, in which case we invoke `docker run` with slightly different
      arguments that are more appropriate for lints.
    * run_bazel() always mounts the Bazel cache in the same location, so now
      we're caching the results of Bazel builds on a per-agent basis(!!!)
    * No longer include `bazel{build,test}.sh` in the `cockroachdb/bazel`
      Docker image -- these files can just live in tree, and we don't want
      updates to them to require updating the Docker image.
    
    Now that we've standardized our Bazel builds, it'll be that much easier
    to make changes across all our Bazel builds, as we need to for testing
    UX improvements that are coming.
    
    Release note: None

--- build/teamcity-bazel-test.sh
+++ build/teamcity-bazel-test.sh
@@ -2,22 +2,11 @@
 
 set -euo pipefail
 
-source "$(dirname "${0}")/teamcity-support.sh"
-source "$(dirname "${0}")/teamcity-bazel-support.sh"  # For BAZEL_IMAGE
+source "$(dirname "${0}")/teamcity-support.sh"  # For $root
+source "$(dirname "${0}")/teamcity-bazel-support.sh"  # For run_bazel
 
 tc_prepare
 
-# NB: $root is set by teamcity-support.sh.
-export TMPDIR=$root/artifacts
-mkdir -p "$TMPDIR"
-
-# Bazel configuration for CI.
-cp $root/.bazelrc.ci $root/.bazelrc.user
-
 tc_start_block "Run Bazel test"
-docker run -i ${tty-} --rm --init \
-       --workdir="/go/src/github.com/cockroachdb/cockroach" \
-       -v "$root:/go/src/github.com/cockroachdb/cockroach:ro" \
-       -v "$TMPDIR:/artifacts" \
-       $BAZEL_IMAGE bazeltest.sh
+run_bazel build/bazelutil/bazeltest.sh
 tc_end_block "Run Bazel test"
