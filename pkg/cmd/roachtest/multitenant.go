350cb1eb2457234a99ccf63c604ae641e2c8f533
Andrew Werner <awerner32@gmail.com>
2021-04-05 04:08:00 -0400

    roachtest: add multitenant version upgrade test
    This test exercises the upgrade path for tenants from v20.2 to v21.1.
    Along the way it changes config to make it easy to find the mt logs. It uses a
    subdir of the logdir rather than parent.
    
    Release note: None

--- pkg/cmd/roachtest/multitenant.go
+++ pkg/cmd/roachtest/multitenant.go
@@ -12,10 +12,7 @@ package main
 
 import (
 	"context"
-	gosql "database/sql"
-	"net/url"
-	"strings"
-	"time"
+	"path/filepath"
 
 	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/require"
@@ -26,82 +23,52 @@ func runAcceptanceMultitenant(ctx context.Context, t *test, c *cluster) {
 
 	c.Start(ctx, t, c.All())
 
-	_, err := c.Conn(ctx, 1).Exec(`SELECT crdb_internal.create_tenant(123)`)
-	require.NoError(t, err)
+	const tenantID = 123
+	{
+		_, err := c.Conn(ctx, 1).Exec(`SELECT crdb_internal.create_tenant($1)`, tenantID)
+		require.NoError(t, err)
+	}
 
 	kvAddrs := c.ExternalAddr(ctx, c.All())
 
 	tenantCtx, cancel := context.WithCancel(ctx)
 	defer cancel()
-	errCh := make(chan error, 1)
-	go func() {
-		errCh <- c.RunE(tenantCtx, c.Node(1),
-			"./cockroach", "mt", "start-sql",
-			// TODO(tbg): make this test secure.
-			// "--certs-dir", "certs",
-			"--insecure",
-			"--tenant-id", "123",
-			"--http-addr", "127.0.0.1:8081",
-			"--kv-addrs", strings.Join(kvAddrs, ","),
-			// Don't bind to external interfaces when running locally.
-			"--sql-addr", ifLocal("127.0.0.1", "0.0.0.0")+":36257",
-			// Ensure that log files get created.
-			"--log='file-defaults: {dir: .}'",
-		)
-		close(errCh)
-	}()
-	u, err := url.Parse(c.ExternalPGUrl(ctx, c.Node(1))[0])
-	require.NoError(t, err)
-	u.Host = c.ExternalIP(ctx, c.Node(1))[0] + ":36257"
-	url := u.String()
-	c.l.Printf("sql server should be running at %s", url)
-
-	time.Sleep(time.Second)
-
-	select {
-	case err := <-errCh:
-		t.Fatal(err)
-	default:
-	}
+	const (
+		tenantHTTPPort = 8081
+		tenantSQLPort  = 36258
+	)
+	const tenantNode = 1
+	tenant := createTenantNode(tenantCtx, t, c, "./cockroach", kvAddrs,
+		tenantID, tenantNode, tenantHTTPPort, tenantSQLPort)
 
 	t.Status("checking that a client can connect to the tenant server")
 
-	db, err := gosql.Open("postgres", url)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer db.Close()
-	_, err = db.Exec(`CREATE TABLE foo (id INT PRIMARY KEY, v STRING)`)
-	require.NoError(t, err)
-
-	_, err = db.Exec(`INSERT INTO foo VALUES($1, $2)`, 1, "bar")
-	require.NoError(t, err)
-
-	var id int
-	var v string
-	require.NoError(t, db.QueryRow(`SELECT * FROM foo LIMIT 1`).Scan(&id, &v))
-	require.Equal(t, 1, id)
-	require.Equal(t, "bar", v)
+	verifySQL(t, tenant.pgURL,
+		mkStmt(`CREATE TABLE foo (id INT PRIMARY KEY, v STRING)`),
+		mkStmt(`INSERT INTO foo VALUES($1, $2)`, 1, "bar"),
+		mkStmt(`SELECT * FROM foo LIMIT 1`).
+			withResults([][]string{{"1", "bar"}}))
 
 	t.Status("stopping the server ahead of checking for the tenant server")
 
 	// Stop the server, which also ensures that log files get flushed.
 	cancel()
-	<-errCh
+	<-tenant.errCh
 
 	t.Status("checking log file contents")
 
 	// Check that the server identifiers are present in the tenant log file.
+	logFile := filepath.Join(tenant.logDir(), "cockroach.log")
 	if err := c.RunE(ctx, c.Node(1),
-		"grep", "-q", "'\\[config\\] .* clusterID:'", "cockroach.log"); err != nil {
+		"grep", "-q", "'\\[config\\] .* clusterID:'", logFile); err != nil {
 		t.Fatal(errors.Wrap(err, "cluster ID not found in log file"))
 	}
 	if err := c.RunE(ctx, c.Node(1),
-		"grep", "-q", "'\\[config\\] .* tenantID:'", "cockroach.log"); err != nil {
+		"grep", "-q", "'\\[config\\] .* tenantID:'", logFile); err != nil {
 		t.Fatal(errors.Wrap(err, "tenant ID not found in log file"))
 	}
 	if err := c.RunE(ctx, c.Node(1),
-		"grep", "-q", "'\\[config\\] .* instanceID:'", "cockroach.log"); err != nil {
+		"grep", "-q", "'\\[config\\] .* instanceID:'", logFile); err != nil {
 		t.Fatal(errors.Wrap(err, "SQL instance ID not found in log file"))
 	}
 }
