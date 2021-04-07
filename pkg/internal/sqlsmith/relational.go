9eee4ffe7adb8a676ac1ab750ab1302a1a8ddfcf
Marcus Gartner <marcus@cockroachlabs.com>
2021-04-07 13:18:58 -0700

    sqlsmith: improve INSERT validity
    This commit makes several changes to sqlsmith and the random table
    generator to make it more likely that a randomly generated `INSERT`
    statement will successfully insert a row. This is important to maintain
    the efficacy of sqlsmith. See #63190 for additional context.
    
      1. `Smither.makeInsert` now has a significant chance of generating a
         simple `INSERT ... VALUES` statement. This is more likely to insert
         a row than the previously common alternative `INSERT ... SELECT`
         which pulled values from existing, often empty tables and
         frequently applied filters resulting in an empty set of rows.
      2. `rowenc.randComputedColumnTableDef` now considers the nullability
         of dependent columns when building computed columns. Previously, it
         was common for this function to generate a `NOT NULL` computed
         column dependent on a `NULL` column, making many inserts fail. It
         was also possible to generate a `NOT NULL` computed column with an
         expression like `(a + NULL)`, making inserts impossible.
    
    A regression test has been added to catch bugs in sqlsmith or the random
    table generator that might cause many or all `INSERT` statements to
    fail. I'm hesitant about this test because there is a non-zero chance it
    will produce a false-negative. But I don't think there is another way to
    catch these types of regressions. I stressed the test for 2500+ runs
    over 4 minutes without a false-negative, which provides some assurance
    that a false-negative is very unlikely. I also verified that this test
    catches the issues fixed by (1) and (2) after a few hundred runs, and it
    almost instantly catches the issue fixed in #63190.
    
    Release note: None

--- pkg/internal/sqlsmith/relational.go
+++ pkg/internal/sqlsmith/relational.go
@@ -11,6 +11,7 @@
 package sqlsmith
 
 import (
+	"github.com/cockroachdb/cockroach/pkg/sql/rowenc"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 )
@@ -950,13 +951,14 @@ func (s *Smither) makeInsert(refs colRefs) (*tree.Insert, *tableRef, bool) {
 
 	// Use DEFAULT VALUES only sometimes. A nil insert.Rows.Select indicates
 	// DEFAULT VALUES.
-	if s.d9() != 1 {
-		var desiredTypes []*types.T
+	if s.d9() == 1 {
+		return insert, tableRef, true
+	}
+
+	// Use a simple INSERT...VALUES statement some of the time.
+	if s.coin() {
 		var names tree.NameList
-
-		unnamed := s.coin()
-
-		// Grab some subset of the columns of the table to attempt to insert into.
+		var row tree.Exprs
 		for _, c := range tableRef.Columns {
 			// We *must* write a column if it's writable and non-nullable.
 			// We *can* write a column if it's writable and nullable.
@@ -964,22 +966,55 @@ func (s *Smither) makeInsert(refs colRefs) (*tree.Insert, *tableRef, bool) {
 			if c.Computed.Computed || c.Hidden {
 				continue
 			}
-			if unnamed || c.Nullable.Nullability == tree.NotNull || s.coin() {
-				desiredTypes = append(desiredTypes, tree.MustBeStaticallyKnownType(c.Type))
+			notNull := c.Nullable.Nullability == tree.NotNull
+			if notNull || s.coin() {
 				names = append(names, c.Name)
+				row = append(row, rowenc.RandDatum(s.rnd, tree.MustBeStaticallyKnownType(c.Type), !notNull))
 			}
 		}
-		if len(desiredTypes) == 0 {
-			return nil, nil, false
+		insert.Columns = names
+		insert.Rows = &tree.Select{
+			Select: &tree.ValuesClause{
+				Rows: []tree.Exprs{row},
+			},
 		}
-		if !unnamed {
-			insert.Columns = names
-		}
+		return insert, tableRef, true
+	}
 
-		insert.Rows, _, ok = s.makeSelect(desiredTypes, refs)
-		if !ok {
-			return nil, nil, false
+	// Otherwise, build a more complex INSERT with a SELECT.
+	// Grab some subset of the columns of the table to attempt to insert into.
+	var desiredTypes []*types.T
+	var names tree.NameList
+	unnamed := s.coin()
+	for _, c := range tableRef.Columns {
+		// We *must* write a column if it's writable and non-nullable.
+		// We *can* write a column if it's writable and nullable.
+		// We *cannot* write a column if it's computed or hidden.
+		//
+		// We include all non-computed, non-hidden columns if we are building an
+		// unnamed insert. If a column is omitted in an unnamed insert, it would
+		// be unlikely that column types of the insert values match the column
+		// types of the table.
+		if c.Computed.Computed || c.Hidden {
+			continue
 		}
+		if unnamed || c.Nullable.Nullability == tree.NotNull || s.coin() {
+			names = append(names, c.Name)
+			desiredTypes = append(desiredTypes, tree.MustBeStaticallyKnownType(c.Type))
+		}
+	}
+
+	if len(desiredTypes) == 0 {
+		return nil, nil, false
+	}
+
+	if !unnamed {
+		insert.Columns = names
+	}
+
+	insert.Rows, _, ok = s.makeSelect(desiredTypes, refs)
+	if !ok {
+		return nil, nil, false
 	}
 
 	return insert, tableRef, true
