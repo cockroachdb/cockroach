ab855da72a868df454bba34c9667b852de6bb675
Yahor Yuzefovich <yahor@cockroachlabs.com>
2021-04-07 09:22:36 -0700

    coldata: do not allocate wasteful memory for UUIDs
    Previously, whenever we're allocating a new `Bytes` vector, we would use
    64 bytes as an average element size to decide how large of a flat byte
    slice to allocate initially. This is wasteful in case of Uuids because
    then we know that each element will be exactly of 16 bytes. This commit
    uses that knowledge to remove wasteful allocations.
    
    Release note: None

--- pkg/col/coldata/vec.go
+++ pkg/col/coldata/vec.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/col/typeconv"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
+	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 )
 
 // Column is an interface that represents a raw array of a Go native type.
@@ -162,6 +163,9 @@ func (cf *defaultColumnFactory) MakeColumn(t *types.T, length int) Column {
 	case types.BoolFamily:
 		return make(Bools, length)
 	case types.BytesFamily:
+		if t.Family() == types.UuidFamily {
+			return NewBytesWithAvgLength(length, uuid.Size)
+		}
 		return NewBytes(length)
 	case types.IntFamily:
 		switch t.Width() {
