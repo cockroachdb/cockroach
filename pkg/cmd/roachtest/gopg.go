532f060700dc97bb0432fd4a0a7e12f45b24c605
richardjcai <caioftherichard@gmail.com>
2021-04-08 16:51:42 -0400

    roachtest: add roachtest for postgres nodejs driver
    Release note: None

--- pkg/cmd/roachtest/gopg.go
+++ pkg/cmd/roachtest/gopg.go
@@ -46,12 +46,14 @@ func registerGopg(r *testRegistry) {
 		t.Status("setting up cockroach")
 		c.Put(ctx, cockroach, "./cockroach", c.All())
 		c.Start(ctx, t, c.All())
-		version, err := fetchCockroachVersion(ctx, c, node[0])
+		version, err := fetchCockroachVersion(ctx, c, node[0], nil)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		if err := alterZoneConfigAndClusterSettings(ctx, version, c, node[0]); err != nil {
+		if err := alterZoneConfigAndClusterSettings(
+			ctx, version, c, node[0], nil,
+		); err != nil {
 			t.Fatal(err)
 		}
 
