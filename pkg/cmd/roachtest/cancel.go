100608cfd893c4d93c904080ecdc79e8f9f1bbde
Yahor Yuzefovich <yahor@cockroachlabs.com>
2021-04-09 14:51:32 -0700

    roachtest: rename SQLExec to SQLQueries
    Release note: None

--- pkg/cmd/roachtest/cancel.go
+++ pkg/cmd/roachtest/cancel.go
@@ -113,7 +113,7 @@ func registerCancel(r *testRegistry) {
 
 	r.Add(testSpec{
 		Name:    fmt.Sprintf("cancel/tpcc/distsql/w=%d,nodes=%d", warehouses, numNodes),
-		Owner:   OwnerSQLExec,
+		Owner:   OwnerSQLQueries,
 		Cluster: makeClusterSpec(numNodes),
 		Run: func(ctx context.Context, t *test, c *cluster) {
 			runCancel(ctx, t, c, queries, warehouses, true /* useDistsql */)
@@ -122,7 +122,7 @@ func registerCancel(r *testRegistry) {
 
 	r.Add(testSpec{
 		Name:    fmt.Sprintf("cancel/tpcc/local/w=%d,nodes=%d", warehouses, numNodes),
-		Owner:   OwnerSQLExec,
+		Owner:   OwnerSQLQueries,
 		Cluster: makeClusterSpec(numNodes),
 		Run: func(ctx context.Context, t *test, c *cluster) {
 			runCancel(ctx, t, c, queries, warehouses, false /* useDistsql */)
