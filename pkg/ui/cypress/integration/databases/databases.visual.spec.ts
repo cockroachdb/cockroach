2611f87a22f83219336cedab83282dbebc071fcb
Nandu Pokhrel <nandup@cockroachlabs.com>
2021-04-09 15:39:00 -0400

    ui: e2e tests, improved cypress test for transactions and database page
    Previously, only visual regression tests were written
    
    Wrote cypress tests using the cypress-testing-library
    
    Release note: None

--- pkg/ui/cypress/integration/databases/databases.visual.spec.ts
+++ pkg/ui/cypress/integration/databases/databases.visual.spec.ts
@@ -8,9 +8,29 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-describe("Databases - visual regression tests", () => {
-  it("renders the same view for databases/tables view", () => {
+describe("Databases - show databases without any tables", () => {
+  it("renders default view", () => {
     cy.visit("#/databases/tables");
-    cy.matchImageSnapshot();
+    cy.findAllByText("Databases").should("exist");
+    cy.findAllByText("defaultdb").should("exist");
+    cy.findAllByText("postgres").should("exist");
+  });
+});
+
+describe("Databases - show databases with tables", () => {
+  it("renders the view for databases/tables view", () => {
+    cy.visit("#/databases/tables");
+    cy.exec('cockroach sql --insecure --execute="create table test (id int)";');
+    cy.exec(
+      'cockroach sql --insecure --execute="create table test1 (id int)";'
+    );
+    cy.exec(
+      'cockroach sql --insecure --execute="create table test2 (id int)";'
+    );
+    cy.reload();
+    cy.log("check whether the tables exit in the database");
+    cy.findAllByText("public.test").should("exist");
+    cy.findAllByText("public.test1").should("exist");
+    cy.findAllByText("public.test2").should("exist");
   });
 });
