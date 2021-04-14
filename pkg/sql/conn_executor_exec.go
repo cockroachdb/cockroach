dbcda1f3cba98f5001aa38c2a3ece76744568cba
Faizan Qazi <faizan@cockroachlabs.com>
2021-04-14 11:24:22 -0400

    kv,sql: transaction commit deadlines due to leases should be late-bound
    Previously, SQL statements, during their execution, lease
    tables from the lease.Manager indirectly through the
    connExecutor's descs.Collection. Whenever a lease is used,
    the deadline of that version of that lease is applied to
    the transaction as a commit deadline. This had the unfortunate
    side effect that renewals would not be properly picked up, so
    if a transaction took longer then the 4 to 5 minute validity
    period, then we can get into problematic scenarios.
    
    To address, this is patch, adds lease tracking through the
    the table collections, so that we are aware when extension
    occurs, and uses that to figure out the latest time a given
    transaction can commit.
    
    Fixes #51042
    
    Release note (bug fix): Fixed a bug that prevent transactions which lasted
    longer than 5 minutes and then performed writes from committing.

--- pkg/sql/conn_executor_exec.go
+++ pkg/sql/conn_executor_exec.go
@@ -251,6 +251,13 @@ func (ex *connExecutor) execStmtInOpenState(
 
 	var stmt Statement
 	queryID := ex.generateID()
+	// Update the deadline on the transaction based on the collections.
+	if ex.state.mu.txn != nil {
+		err := ex.extraTxnState.descCollection.MaybeUpdateDeadline(ctx, ex.state.mu.txn)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
 	if prepared != nil {
 		stmt = makeStatementFromPrepared(prepared, queryID)
 	} else {
