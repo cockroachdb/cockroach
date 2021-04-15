d08b462e4ff09be5ef62ff9822861f62d7b74674
Alex Lunev <alexl@cockroachlabs.com>
2021-04-14 20:51:26 -0700

    TransferLeaseTarget and ShouldTransferLease use Constraints to filter down the list of available stores to calculate the mean leases across all the potential stores that could hold a replica. With addition of non-voter replicas, we should be pairing that list down to only the replicas that could hold voters. This commit adds filtering by VoterConstraints to the lease transfer logic.
    Release note: None

--- pkg/kv/kvserver/allocator_test.go
+++ pkg/kv/kvserver/allocator_test.go
@@ -1350,6 +1350,94 @@ func TestAllocatorTransferLeaseTarget(t *testing.T) {
 	}
 }
 
+func TestAllocatorTransferLeaseTargetConstraints(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	stopper, g, _, a, _ := createTestAllocator(10, true /* deterministic */)
+	defer stopper.Stop(context.Background())
+
+	// 6 stores with the following setup
+	// 1 | locality=dc=1 | lease_count=10
+	// 2 | locality=dc=0 | lease_count=0
+	// 3 | locality=dc=1 | lease_count=30
+	// 4 | locality=dc=0 | lease_count=0
+	// 5 | locality=dc=1 | lease_count=50
+	// 6 | locality=dc=0 | lease_count=0
+	var stores []*roachpb.StoreDescriptor
+	for i := 1; i <= 6; i++ {
+		stores = append(stores, &roachpb.StoreDescriptor{
+			StoreID: roachpb.StoreID(i),
+			Node: roachpb.NodeDescriptor{
+				NodeID: roachpb.NodeID(i),
+				Locality: roachpb.Locality{
+					Tiers: []roachpb.Tier{
+						{Key: "dc", Value: strconv.FormatInt(int64(i%2), 10)},
+					},
+				},
+			},
+			Capacity: roachpb.StoreCapacity{LeaseCount: int32(100 * i * (i % 2))},
+		})
+	}
+	sg := gossiputil.NewStoreGossiper(g)
+	sg.GossipStores(stores, t)
+
+	existing := replicas(1, 3, 5)
+
+	constraint := func(value string) []zonepb.ConstraintsConjunction {
+		return []zonepb.ConstraintsConjunction{
+			{
+				Constraints: []zonepb.Constraint{
+					{Key: "dc", Value: value, Type: zonepb.Constraint_REQUIRED},
+				},
+			},
+		}
+	}
+
+	constraints := func(value string) *zonepb.ZoneConfig {
+		return &zonepb.ZoneConfig{
+			NumReplicas: proto.Int32(1),
+			Constraints: constraint(value),
+		}
+	}
+
+	voterConstraints := func(value string) *zonepb.ZoneConfig {
+		return &zonepb.ZoneConfig{
+			NumReplicas:      proto.Int32(1),
+			VoterConstraints: constraint(value),
+		}
+	}
+
+	testCases := []struct {
+		existing    []roachpb.ReplicaDescriptor
+		leaseholder roachpb.StoreID
+		expected    roachpb.StoreID
+		zone        *zonepb.ZoneConfig
+	}{
+		{existing: existing, leaseholder: 5, expected: 1, zone: constraints("1")},
+		{existing: existing, leaseholder: 5, expected: 1, zone: voterConstraints("1")},
+		{existing: existing, leaseholder: 5, expected: 0, zone: constraints("0")},
+		{existing: existing, leaseholder: 5, expected: 0, zone: voterConstraints("0")},
+		{existing: existing, leaseholder: 5, expected: 1, zone: zonepb.EmptyCompleteZoneConfig()},
+	}
+	for _, c := range testCases {
+		t.Run("", func(t *testing.T) {
+			target := a.TransferLeaseTarget(
+				context.Background(),
+				c.zone,
+				c.existing,
+				c.leaseholder,
+				nil, /* replicaStats */
+				true,
+				true,  /* checkCandidateFullness */
+				false, /* alwaysAllowDecisionWithoutStats */
+			)
+			if c.expected != target.StoreID {
+				t.Fatalf("expected %d, but found %d", c.expected, target.StoreID)
+			}
+		})
+	}
+}
+
 // TestAllocatorTransferLeaseTargetDraining verifies that the allocator will
 // not choose to transfer leases to a store that is draining.
 func TestAllocatorTransferLeaseTargetDraining(t *testing.T) {
