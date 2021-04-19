c8d001e68e17a4f8f41d96116c61c9487d6ecf59
Alex Lunev <alexl@cockroachlabs.com>
2021-04-18 22:15:01 -0700

    kvserver: add a concept of suspect nodes to the allocator
    Touches #57093
    
    A flaky node that goes in and out of liveness can cause
    lease thrashing, as live nodes steal leases from the
    failing node and then when it heartbeats the allocator
    puts them back to maintain mean counts. This could be very
    disruptive to the cluster as the leases ping-pong back and
    forth. To prevent this problem we introduce a new concept
    of a suspect node to the allocator. When a node fails to
    hearbeat liveness it's considered suspect until it can reliably
    heartbeat liveness for an extended period of time (60s by default).
    
    Release note: None

--- pkg/kv/kvserver/store_rebalancer.go
+++ pkg/kv/kvserver/store_rebalancer.go
@@ -200,7 +200,7 @@ func (sr *StoreRebalancer) Start(ctx context.Context, stopper *stop.Stopper) {
 				continue
 			}
 
-			storeList, _, _ := sr.rq.allocator.storePool.getStoreList(storeFilterNone)
+			storeList, _, _ := sr.rq.allocator.storePool.getStoreList(storeFilterSuspect)
 			sr.rebalanceStore(ctx, mode, storeList)
 		}
 	})
@@ -948,7 +948,7 @@ func (sr *StoreRebalancer) shouldNotMoveTo(
 	// about node liveness.
 	targetNodeID := candidateStore.Node.NodeID
 	if targetNodeID != sr.rq.store.Ident.NodeID {
-		if !sr.rq.store.cfg.StorePool.isNodeReadyForRoutineReplicaTransfer(ctx, targetNodeID) {
+		if !sr.rq.allocator.storePool.isStoreReadyForRoutineReplicaTransfer(ctx, candidateStore.StoreID) {
 			log.VEventf(ctx, 3,
 				"refusing to transfer replica to n%d/s%d", targetNodeID, candidateStore.StoreID)
 			return true
