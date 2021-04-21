f61925984ef1beeba87ae8ae2215fcd43e45a892
Abby Hersh <abbychersh@gmail.com>
2021-04-21 10:55:54 -0400

    roachtest: update version map
    Release note: None

--- pkg/cmd/roachtest/test_runner.go
+++ pkg/cmd/roachtest/test_runner.go
@@ -1194,7 +1194,7 @@ func PredecessorVersion(buildVersion version.Version) (string, error) {
 	// map.
 	verMap := map[string]string{
 		"21.1": "20.2.6",
-		"20.2": "20.1.13",
+		"20.2": "20.1.14",
 		"20.1": "19.2.11",
 		"19.2": "19.1.11",
 		"19.1": "2.1.9",
