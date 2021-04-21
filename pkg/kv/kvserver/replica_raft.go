ed47b6098c6d0f04650a2133103dea8c5ce80e6a
Tobias Grieger <tobias.b.grieger@gmail.com>
2021-04-21 17:48:51 +0200

    kvserver: enqueue update check after Campaign()
    I noticed that TestSplitBurstWithSlowFollower would average only
    <10 splits per second even when no lagging replica is introduced (i.e.
    the `time.Sleep` commented out). Investigating the raft chatter
    suggested that after campaigning, the leaseholder of the right-hand
    side would not be processed by its Store for a `Ready` handling cycle
    until after ~50ms (the coalesced heartbeat timeout) in the test had
    passed, and a similar delay was observed when it was sending out its
    votes. Adding a call to `enqueueRaftUpdateCheck` fixes both, we end
    up at ~100 splits per second.
    
    Release note: None

--- pkg/kv/kvserver/replica_raft.go
+++ pkg/kv/kvserver/replica_raft.go
@@ -1536,6 +1536,7 @@ func (r *Replica) maybeCampaignOnWakeLocked(ctx context.Context) {
 		if err := r.mu.internalRaftGroup.Campaign(); err != nil {
 			log.VEventf(ctx, 1, "failed to campaign: %s", err)
 		}
+		r.store.enqueueRaftUpdateCheck(r.RangeID)
 	}
 }
 
