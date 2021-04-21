f01fb820618c96d1c94a3190b41a6c6d5f0db253
Paul Bardea <pbardea@gmail.com>
2021-04-21 13:40:44 +0000

    backupccl: don't include empty import entries during restore
    This commit filters out importSpans that have no associated data.
    
    File spans' start keys are set to be the .Next() of the EndKey of the
    previous file span. This was at least the case on the backup that is
    currently being run against restore2TB. This lead to a lot of importSpan
    entries that contained no files and would cover an empty key range. The
    precense of these import spans did not effect the performance of
    restores that much, but they did cause unnecessary splits and scatters
    which further contributed to the elevated NLHEs that were seen.
    
    This commit generally calms down the leaseholder errors, but do not
    eliminate them entirely. Future investigation is still needed. It also
    does not seem to have any performance impact on RESTORE performance (in
    terms of speed of the job).
    
    Release note: None

--- pkg/ccl/backupccl/restore_job.go
+++ pkg/ccl/backupccl/restore_job.go
@@ -269,6 +269,17 @@ rangeLoop:
 					return nil, hlc.Timestamp{}, err
 				}
 			}
+			if len(files) == 0 {
+				// There may be import entries that refer to no data, and hence
+				// no files. These are caused because file spans start at a
+				// specific key. E.g. consider the first file backing up data
+				// from table 51. It will cover span ‹/Table/51/1/0/0› -
+				// ‹/Table/51/1/3273›. When merged with the backup span:
+				// ‹/Table/51› - ‹/Table/52›, we get an empty span with no
+				// files: ‹/Table/51› - ‹/Table/51/1/0/0›. We should ignore
+				// these to avoid thrashing during restore's split and scatter.
+				continue
+			}
 			// If needed is false, we have data backed up that is not necessary
 			// for this restore. Skip it.
 			requestEntries = append(requestEntries, execinfrapb.RestoreSpanEntry{
@@ -636,6 +647,10 @@ func restore(
 		highWaterMark, errOnMissingRange)
 	if err != nil {
 		return emptyRowCount, errors.Wrapf(err, "making import requests for %d backups", len(backupManifests))
+	}
+	if len(importSpans) == 0 {
+		// There are no files to restore.
+		return emptyRowCount, nil
 	}
 
 	for i := range importSpans {
