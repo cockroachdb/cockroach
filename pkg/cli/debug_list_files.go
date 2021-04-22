7db236d57a68c67f4c3163f5d509e04aa1274d1a
Raphael 'kena' Poss <knz@thaumogen.net>
2021-04-22 17:14:06 +0200

    cli/debug: new sub-command `list-files`
    This is meant to assist the human operator select appropriate
    flags for the `debug zip` command.
    
    Release note (cli change): The new command `debug list-files`
    show the list of files that can be retrieved via the `debug zip`
    command. It supports the `--nodes` and `--exclude-nodes` parameters
    in the same way as `debug zip`.

---
+++ pkg/cli/debug_list_files.go
@@ -0,0 +1,137 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package cli
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/server/serverpb"
+	"github.com/cockroachdb/cockroach/pkg/server/status/statuspb"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/log/logpb"
+	"github.com/spf13/cobra"
+)
+
+var debugListFilesCmd = &cobra.Command{
+	Use:   "list-files",
+	Short: "list files available for retrieval via 'debug zip'",
+	RunE:  MaybeDecorateGRPCError(runDebugListFiles),
+}
+
+func runDebugListFiles(cmd *cobra.Command, _ []string) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Connect to the node pointed to in the command line.
+	conn, _, finish, err := getClientGRPCConn(ctx, serverCfg)
+	if err != nil {
+		return err
+	}
+	defer finish()
+
+	status := serverpb.NewStatusClient(conn)
+
+	// Retrieve the details for the head node.
+	firstNodeDetails, err := status.Details(ctx, &serverpb.DetailsRequest{NodeId: "local"})
+	if err != nil {
+		return err
+	}
+
+	// Retrieve the list of all nodes.
+	nodes, err := status.Nodes(ctx, &serverpb.NodesRequest{})
+	if err != nil {
+		log.Warningf(ctx, "cannot retrieve node list: %v", err)
+	}
+	// In case nodes came up back empty (the Nodes() RPC failed), we
+	// still want to inspect the per-node endpoints on the head
+	// node. As per the above, we were able to connect at least to
+	// that.
+	inputNodeList := []statuspb.NodeStatus{{Desc: roachpb.NodeDescriptor{
+		NodeID:     firstNodeDetails.NodeID,
+		Address:    firstNodeDetails.Address,
+		SQLAddress: firstNodeDetails.SQLAddress,
+	}}}
+	if nodes != nil {
+		// If the nodes were found, use that instead.
+		inputNodeList = nodes.Nodes
+	}
+
+	// Filter the list of nodes according to configuration.
+	var nodeList []roachpb.NodeID
+	for _, n := range inputNodeList {
+		if zipCtx.nodes.isIncluded(n.Desc.NodeID) {
+			nodeList = append(nodeList, n.Desc.NodeID)
+		}
+	}
+
+	// Determine the list of non-log file types.
+	fileTypes := make([]int, 0, len(serverpb.FileType_value))
+	for _, v := range serverpb.FileType_value {
+		fileTypes = append(fileTypes, int(v))
+	}
+	sort.Ints(fileTypes)
+
+	// The log files for each node.
+	logFiles := make(map[roachpb.NodeID][]logpb.FileInfo)
+	// The non-log files on each node. The int32 is the file type.
+	otherFiles := make(map[roachpb.NodeID]map[int32][]*serverpb.File)
+
+	// Retrieve the files.
+	for _, nodeID := range nodeList {
+		nodeIDs := fmt.Sprintf("%d", nodeID)
+		nodeLogs, err := status.LogFilesList(ctx, &serverpb.LogFilesListRequest{NodeId: nodeIDs})
+		if err != nil {
+			log.Warningf(ctx, "cannot retrieve log file list from node %d: %v", nodeID, err)
+		} else {
+			logFiles[nodeID] = nodeLogs.Files
+		}
+
+		otherFiles[nodeID] = make(map[int32][]*serverpb.File)
+		for _, fileTypeI := range fileTypes {
+			fileType := int32(fileTypeI)
+			nodeFiles, err := status.GetFiles(ctx, &serverpb.GetFilesRequest{
+				NodeId:   nodeIDs,
+				ListOnly: true,
+				Type:     serverpb.FileType(fileType),
+				Patterns: []string{"*"},
+			})
+			if err != nil {
+				log.Warningf(ctx, "cannot retrieve %s file list from node %d: %v", serverpb.FileType_name[fileType], nodeID, err)
+			} else {
+				otherFiles[nodeID][fileType] = nodeFiles.Files
+			}
+		}
+	}
+
+	// Print them out.
+	fileTableHeaders := []string{"node_id", "type", "file_name", "size"}
+	alignment := "lllr"
+	var rows [][]string
+	for _, nodeID := range nodeList {
+		nodeIDs := fmt.Sprintf("%d", nodeID)
+		for _, logFile := range logFiles[nodeID] {
+			rows = append(rows, []string{nodeIDs, "log", logFile.Name, fmt.Sprintf("%d", logFile.SizeBytes)})
+		}
+		for _, ft := range fileTypes {
+			fileType := int32(ft)
+			for _, other := range otherFiles[nodeID][fileType] {
+				rows = append(rows, []string{nodeIDs, strings.ToLower(serverpb.FileType_name[fileType]), other.Name, fmt.Sprintf("%d", other.FileSize)})
+			}
+		}
+	}
+
+	return printQueryOutput(os.Stdout, fileTableHeaders, newRowSliceIter(rows, alignment))
+}
--- pkg/cli/flags.go
+++ pkg/cli/flags.go
@@ -555,6 +555,7 @@ func init() {
 		debugGossipValuesCmd,
 		debugTimeSeriesDumpCmd,
 		debugZipCmd,
+		debugListFilesCmd,
 		doctorExamineClusterCmd,
 		doctorExamineFallbackClusterCmd,
 		doctorRecreateClusterCmd,
@@ -621,11 +622,15 @@ func init() {
 	// Zip command.
 	{
 		f := debugZipCmd.Flags()
-		varFlag(f, &zipCtx.nodes.inclusive, cliflags.ZipNodes)
-		varFlag(f, &zipCtx.nodes.exclusive, cliflags.ZipExcludeNodes)
 		boolFlag(f, &zipCtx.redactLogs, cliflags.ZipRedactLogs)
 		durationFlag(f, &zipCtx.cpuProfDuration, cliflags.ZipCPUProfileDuration)
 		intFlag(f, &zipCtx.concurrency, cliflags.ZipConcurrency)
+	}
+	// List-nodes + Zip commands.
+	for _, cmd := range []*cobra.Command{debugZipCmd, debugListFilesCmd} {
+		f := cmd.Flags()
+		varFlag(f, &zipCtx.nodes.inclusive, cliflags.ZipNodes)
+		varFlag(f, &zipCtx.nodes.exclusive, cliflags.ZipExcludeNodes)
 	}
 
 	// Decommission command.
