3560827fe85d166862389a214de8abf7aac0772c
Andrii Vorobiov <and.vorobiov@gmail.com>
2021-04-22 14:39:13 +0300

    ui: bazel build for UI
    - build `cluster-ui` JS package
    - build `db-console` package
    - build `crdb-protobufjs-client` package
    - embed JS assets into `distccl` go's binary
    
    Release note: None
    
    Release justification: non-production code changes

--- pkg/ui/workspaces/cluster-ui/webpack.config.js
+++ pkg/ui/workspaces/cluster-ui/webpack.config.js
@@ -13,123 +13,16 @@ const webpack = require("webpack");
 const WebpackBar = require("webpackbar");
 const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.ts",
-
-  output: {
-    path: path.resolve(__dirname, "dist/js"),
-    filename: "main.js",
-    library: "adminUI",
-    libraryTarget: "umd",
-  },
-
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
-
-  resolve: {
-    modules: [
-      'node_modules',
-      path.join(__dirname, 'src/fonts'),
-    ],
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".less"],
-    alias: {
-      src: path.resolve(__dirname, "src"),
-    },
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: true,
-          }
-        },
-      },
-      // Styles in current project use SCSS preprocessing language with CSS modules.
-      // They have to follow file naming convention: [filename].module.scss
-      {
-        test: /\.module\.scss$/,
-        exclude: /node_modules/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: {
-                localIdentName: "[local]--[hash:base64:5]",
-              }
-            },
-          },
-          "sass-loader"
-        ],
-      },
-      // Ant design styles defined as global styles with .scss files which don't follow
-      // internal convention to have .module.scss extension. That's why we need one more sass
-      // loader which matches SCSS files without .module suffix. Note, it doesn't exclude node_modules
-      // dir so it can access external dependencies.
-      {
-        test: /(?<!\.module)\.scss/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(ts|js)x?$/,
-        use: [
-          "babel-loader",
-          {
-            loader: "astroturf/loader",
-            options: { extension: ".module.scss" },
-          },
-        ],
-        exclude: /node_modules/,
-      },
-      // Preprocess LESS styles required by external components
-      // (react-select)
-      {
-        use: [
-          "style-loader",
-          "css-loader",
-          {
-            loader: "less-loader",
-            options: {
-              lessOptions: {
-                javascriptEnabled: true
-              }
-            }
-          },
-        ],
-        test: /\.less$/,
-      },
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader",
-      },
-      { test: /\.css$/, use: [ "style-loader", "css-loader" ] },
-    ],
-  },
-
-  plugins: [
-    new WebpackBar({
-      name: "cluster-ui",
-      color: "cyan",
-      profile: true,
-    }),
-    new MomentLocalesPlugin(),
-    new webpack.NormalModuleReplacementPlugin(
-      /node_modules\/antd\/lib\/style\/index\.less/,
-      path.resolve(__dirname, "src/core/antd-patch.less"),
-    ),
-  ],
+// tslint:disable:object-literal-sort-keys
+module.exports = (env, argv) => {
+  console.log(env)
+  const isBazelBuild = env && env.is_bazel_build;
 
   // When importing a module whose path matches one of the following, just
   // assume a corresponding global variable exists and use that instead.
   // This is important because it allows us to avoid bundling all of our
   // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
+  const externals = {
     protobufjs: "protobufjs",
     react: {
       commonjs: "react",
@@ -147,5 +40,133 @@ module.exports = {
     "react-redux": "react-redux",
     "redux-saga": "redux-saga",
     "redux": "redux",
-  },
-};
+  };
+
+  if (isBazelBuild) {
+    // TODO (koorosh): commented to avoid bazel build failure.
+    // should be fixed to consume react-router-dom as peer dependency
+    delete externals["react-router-dom"];
+  }
+
+  const config = {
+    entry: path.resolve(__dirname, "./src/index.ts"),
+
+    output: {
+      path: path.resolve(__dirname, "dist/js"),
+      filename: "main.js",
+      library: "adminUI",
+      libraryTarget: "umd",
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+      modules: [
+        'node_modules',
+        path.join(__dirname, 'src/fonts'),
+      ],
+      extensions: [".ts", ".tsx", ".js", ".jsx", ".less"],
+      alias: {
+        src: path.resolve(__dirname, "src"),
+      },
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+          use: {
+            loader: "url-loader",
+            options: {
+              limit: true,
+            }
+          },
+        },
+        // Styles in current project use SCSS preprocessing language with CSS modules.
+        // They have to follow file naming convention: [filename].module.scss
+        {
+          test: /\.module\.scss$/,
+          exclude: /node_modules/,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: {
+                  localIdentName: "[local]--[hash:base64:5]",
+                }
+              },
+            },
+            "sass-loader"
+          ],
+        },
+        // Ant design styles defined as global styles with .scss files which don't follow
+        // internal convention to have .module.scss extension. That's why we need one more sass
+        // loader which matches SCSS files without .module suffix. Note, it doesn't exclude node_modules
+        // dir so it can access external dependencies.
+        {
+          test: /(?<!\.module)\.scss/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.(ts|js)x?$/,
+          use: [
+            "babel-loader",
+            {
+              loader: "astroturf/loader",
+              options: {extension: ".module.scss"},
+            },
+          ],
+          exclude: /node_modules/,
+        },
+        // Preprocess LESS styles required by external components
+        // (react-select)
+        {
+          use: [
+            "style-loader",
+            "css-loader",
+            {
+              loader: "less-loader",
+              options: {
+                lessOptions: {
+                  javascriptEnabled: true
+                }
+              }
+            },
+          ],
+          test: /\.less$/,
+        },
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          loader: "source-map-loader",
+          exclude: /node_modules/,
+        },
+        {test: /\.css$/, use: ["style-loader", "css-loader"]},
+      ],
+    },
+
+    plugins: [
+      new WebpackBar({
+        name: "cluster-ui",
+        color: "cyan",
+        profile: true,
+      }),
+      new MomentLocalesPlugin(),
+      new webpack.NormalModuleReplacementPlugin(
+        /node_modules\/antd\/lib\/style\/index\.less/,
+        path.resolve(__dirname, "src/core/antd-patch.less"),
+      ),
+    ],
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: externals,
+  };
+
+  return config;
+}
