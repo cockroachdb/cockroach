069085e8e25407e4cccb5f19a702b196cdefb0f9
elkmaster <carrott9@gmail.com>
2021-04-22 18:05:20 +0300

    ui-components: tooltip component updates (#290)
    provide new style for table title usage (it have same color
    scheme as default but different typographic style)
    add ability to hover on tooltip for clickable elements inside
    other minor style updates and fixes
    as side effect of this work there is a fix of dev process,
    updates of storybook and webpack config was needed to run build
    of cluster-ui with linked local ui-components

--- pkg/ui/cluster-ui/.storybook/main.js
+++ pkg/ui/cluster-ui/.storybook/main.js
@@ -6,13 +6,6 @@ module.exports = {
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
   webpackFinal: async config => {
     config.module.rules = [
-      {
-        test: /\.(ts|tsx)$/,
-        loader: require.resolve('babel-loader'),
-        options: {
-          presets: [['react-app', { flow: false, typescript: true }]],
-        },
-      },
       ...appConfig.module.rules,
     ]
     config.resolve.extensions.push('.ts', '.tsx');
