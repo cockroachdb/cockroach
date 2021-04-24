429cdeeaa4bcfefd56749ac3dbe298f09f6c9575
Aayush Shah <aayush.shah15@gmail.com>
2021-04-23 21:12:08 -0400

    roachtest: add link to clusters' admin ui from the roachtest webui
    This commit makes the roachtest web ui link to respective clusters'
    admin UI.
    
    Release note: None

--- pkg/cmd/roachtest/test_runner.go
+++ pkg/cmd/roachtest/test_runner.go
@@ -1091,17 +1091,21 @@ func (r *testRunner) serveHTTP(wr http.ResponseWriter, req *http.Request) {
 				clusterReused = "no"
 			}
 		}
-		var clusterName string
+		var clusterName, clusterAdminUIAddr string
 		if w.Cluster() != nil {
 			clusterName = w.Cluster().name
+			clusterAdminUIAddr = w.Cluster().ExternalAdminUIAddr(
+				context.Background(),
+				w.Cluster().Node(1),
+			)[0]
 		}
 		t := w.Test()
 		testStatus := "N/A"
 		if t != nil {
 			testStatus = t.GetStatus()
 		}
-		fmt.Fprintf(wr, "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n",
-			w.name, w.Status(), testName, clusterName, clusterReused, testStatus)
+		fmt.Fprintf(wr, "<tr><td>%s</td><td>%s</td><td>%s</td><td><a href='//%s'>%s</a></td><td>%s</td><td>%s</td></tr>\n",
+			w.name, w.Status(), testName, clusterAdminUIAddr, clusterName, clusterReused, testStatus)
 	}
 	fmt.Fprintf(wr, "</table>")
 
