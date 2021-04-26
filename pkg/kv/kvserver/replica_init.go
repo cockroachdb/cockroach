03061a8507df6049f242c7816c1dcb2985197d39
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2021-04-26 02:17:38 -0400

    kv: rationalize load-based range merging
    Closes #62700.
    Re-addresses #41317.
    
    This commit reworks how queries-per-second measurements are used when
    determining whether to merge two ranges together. At a high-level, the change
    moves from a scheme where the QPS over the last second on the LHS and RHS ranges
    are combined and compared against a threshold (half the load-based split
    threshold) to a scheme where the maximum QPS measured over the past 5 minutes
    (configurable) on the LHS and RHS ranges are combined and compared against said
    threshold.
    
    The commit makes this change to avoid thrashing and to avoid overreacting to
    temporary fluctuations in load. These overreactions lead to general instability
    in clusters, as we saw in #41317. Worse, the overreactions compound and can lead
    to cluster-wide meltdowns where a transient slowdown can trigger a wave of range
    merges, which can slow the cluster down further, which can lead to more merges,
    etc. This is what we saw in #62700. This behavior is bad on small clusters and
    it is even worse on large ones, where range merges don't just interrupt traffic,
    but also result in a centralization of load in a previously well-distributed
    dataset, undoing all of the hard work of load-based splitting and rebalancing
    and creating serious hotspots.
    
    The commit improves this situation by introducing a form of memory into the
    load-based split `Decider`. This is the object which was previously only
    responsible for measuring queries-per-second on a range and triggering the
    process of finding a load-based split point. The object is now given an
    additional role of taking the second-long QPS samples that it measures and
    aggregating them together to track the maximum historical QPS over a
    configurable retention period. This maximum QPS measurement can be used to
    prevent load-based splits from being merged away until the resulting ranges have
    consistently remained below a certain QPS threshold for a sufficiently long
    period of time.
    
    The `mergeQueue` is taught how to use this new source of information. It is also
    taught that it should be conservative about imprecision in this QPS tracking,
    opting to skip a merge rather than perform one when the maximum QPS measurement
    has not been tracked for long enough. This means that range merges will
    typically no longer fire within 5 minutes of a lease transfer. This seems fine,
    as there are almost never situations where a range merge is desperately needed
    and we should risk making a bad decision in order to perform one.
    
    I've measured this change on the `clearrange` roachtest that we made heavy use
    of in #62700. As expected, it has the same effect as bumping up the
    `kv.range_split.by_load_merge_delay` high enough such that ranges never merge on
    the active table. Here's a screenshot of a recent run. We still see a period of
    increased tail latency and reduced throughput, which has a strong correlation
    with Pebble compactions. However, we no longer see the subsequent cluster outage
    that used to follow, where ranges on the active table would begin to merge and
    throughput would fall to 0 and struggle to recover, bottoming out repeatedly.
    
    <todo insert images>
    
    Release note (performance improvement): Range merges are no longer considered if
    a range has seen significant load over the previous 5 minutes, instead of being
    considered as long as a range has low load over the last second. This improves
    stability, as load-based splits will no longer rapidly disappear during transient
    throughput dips.

--- pkg/kv/kvserver/replica_init.go
+++ pkg/kv/kvserver/replica_init.go
@@ -93,6 +93,8 @@ func newUnloadedReplica(
 	r.mu.replicaID = replicaID
 	split.Init(&r.loadBasedSplitter, rand.Intn, func() float64 {
 		return float64(SplitByLoadQPSThreshold.Get(&store.cfg.Settings.SV))
+	}, func() time.Duration {
+		return SplitByLoadMergeDelay.Get(&store.cfg.Settings.SV)
 	})
 	r.mu.proposals = map[kvserverbase.CmdIDKey]*ProposalData{}
 	r.mu.checksums = map[uuid.UUID]ReplicaChecksum{}
