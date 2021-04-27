8deb705334a810bcc3b279d1fb8ca0ab1410ad3d
Darin Peshev <darinp@gmail.com>
2021-04-27 15:20:37 -0700

    ccl/sqlproxyccl: add basic login rate limiting
    Moves from the CC repo a package which performs basic login admission
    control to rate limit login attempts. Login attempts are rate limited
    based solely on source IP. Admission control is currently purely a
    local decision, but the package this interface defines could easily
    become the client-side for connection to a centralized service.
    
    Release note: None

---
+++ pkg/ccl/sqlproxyccl/admitter/service.go
@@ -0,0 +1,23 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+// Package admitter provides admission checks functionality. Rate limiting currently.
+package admitter
+
+import "time"
+
+//go:generate mockgen -package=admitter -destination=mocks_generated.go -source=service.go . Service
+
+// Service provides the interface for performing admission checks before
+// allowing requests into the managed service system.
+type Service interface {
+	// LoginCheck determines whether a login request should be allowed to
+	// proceed. It rate limits login attempts from IP addresses and it rate
+	// limits login attempts to individual accounts.
+	LoginCheck(ipAddress string, now time.Time) error
+}
