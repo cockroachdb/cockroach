87f776a9132215d08feae93bd17f1f64ae25b310
sumeerbhola <sumeer@cockroachlabs.com>
2021-04-27 22:35:02 -0400

    admission: add admission control package
    The current code is intended for node-level admission
    control of various kinds of work and is intended for
    both regular clusters and serverless settings (both
    KV and SQL nodes).
    The goals encompass overload protection, and performance
    isolation between tenants and within a tenant (for work
    of different importance within a tenant).
    
    It generalizes the code structure used in the successful
    experiments from
    https://github.com/sumeerbhola/cockroach/tree/admission
    This generalization is meant to allow tweaking of
    heuristics without requiring extensive changes to the
    framework, and to allow integration of both IO and memory
    overload signals.
    
    There are a number of TODOs that are for exploration in
    later, more sophisticated, experiments.
    
    Release note: None

---
+++ pkg/util/admission/admission_queue.go
@@ -0,0 +1,508 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package admission
+
+import (
+	"container/heap"
+	"context"
+	"math"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/util/metric"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/errors"
+)
+
+type WorkPriority int8
+
+const (
+	LowPri    WorkPriority = math.MinInt8
+	NormalPri WorkPriority = 0
+	HighPri   WorkPriority = math.MaxInt8
+)
+
+// WorkInfo provides information that is used to order work within an
+// AdmissionQueue. The WorkKind is not included as a field since an
+// AdmissionQueue deals with a single WorkKind.
+type WorkInfo struct {
+	// TenantID is the id of the tenant. For non-serverless, this will always be
+	// the SystemTenantID.
+	TenantID roachpb.TenantID
+	// Priority is utilized within a tenant.
+	Priority WorkPriority
+	// CreateTime is equivalent to Time.UnixNano() at the creation time of this
+	// work or a parent work (e.g. could be the start time of the transaction,
+	// if this work was created as part of a transaction). It is used to order
+	// work within a (TenantID, Priority) pair -- earlier CreateTime is given
+	// preference.
+	CreateTime int64
+	// BypassAdmission allows the work to bypass admission control, but allows
+	// for it to be accounted for. Ignored unless TenantID is the
+	// SystemTenantID. It should be used for high-priority intra-KV work, and
+	// when KV work generates other KV work (to avoid deadlock). Currently only
+	// supported for AdmissionQueues that are using slots (not tokens).
+	BypassAdmission bool
+
+	// Optional information specified only for AdmissionQueues where the work is
+	// tied to a range. This allows queued work to return early as soon as the
+	// range is no longer in a relevant state at this node. Currently only
+	// KVWork is tied to a range.
+	// TODO(sumeer): use these in the AdmissionQueue implementation.
+
+	// RangeID is the range at which this work must be performed. Optional (see
+	// comment above).
+	RangeID roachpb.RangeID
+	// RequiresLeaseholder is true iff the work requires the leaseholder.
+	// Optional (see comment above).
+	RequiresLeaseholder bool
+}
+
+// AdmissionQueue maintains a queue of work waiting to be admitted. Ordering
+// of work is achieved via 2 heaps: a tenant heap orders the tenants with
+// waiting work in increasing order of used slots or tokens. Within each
+// tenant, the waiting work is ordered based on priority and create time.
+// Tenants with non-zero values of used slots or tokens are tracked even if
+// they have no more waiting work. Token usage is reset to zero every second.
+// The choice of 1 second of memory for token distribution fairness is
+// somewhat arbitrary. The same 1 second interval is also used to garbage
+// collect tenants who have no waiting requests and no used slots or tokens.
+type AdmissionQueue struct {
+	workKind    WorkKind
+	granter     granter
+	usesTokens  bool
+	tiedToRange bool
+
+	// Lock ordering is admitMu < mu.
+
+	// Prevents more than one caller to be in Admit and adding to queue, so can
+	// release mu before calling tryGet and be assured that not competing with
+	// another Admit.
+	admitMu syncutil.Mutex
+	mu      struct {
+		syncutil.Mutex
+		// Tenants with waiting work.
+		tenantHeap tenantHeap
+		// All tenants, including those without waiting work. Periodically cleaned.
+		tenants map[uint64]*tenantInfo
+	}
+	metrics AdmissionQueueMetrics
+}
+
+var _ requester = &AdmissionQueue{}
+
+func MakeAdmissionQueue(
+	workKind WorkKind, granter granter, usesTokens bool, tiedToRange bool,
+) *AdmissionQueue {
+	q := &AdmissionQueue{
+		workKind:    workKind,
+		granter:     granter,
+		usesTokens:  usesTokens,
+		tiedToRange: tiedToRange,
+		metrics:     MakeAdmissionQueueMetrics(string(workKindString(workKind))),
+	}
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		for {
+			<-ticker.C
+			q.gcTenantsAndResetTokens()
+		}
+	}()
+	return q
+}
+
+// TODO(sumeer): reduce allocations by using a pool for tenantInfo, waitingWork,
+// waitingWork.ch.
+
+var neverClosedCh = make(chan struct{})
+
+func (q *AdmissionQueue) Admit(ctx context.Context, info WorkInfo) error {
+	q.metrics.Requested.Inc(1)
+	tenantID := info.TenantID.ToUint64()
+
+	// The code in this method does not use defer to unlock the mutexes because
+	// it needs the flexibility of selectively unlocking one of these on a
+	// certain code path. When changing the code, be careful in making sure the
+	// mutexes are properly unlocked on all code paths.
+	q.admitMu.Lock()
+	q.mu.Lock()
+	tenant, ok := q.mu.tenants[tenantID]
+	if !ok {
+		tenant = makeTenantInfo(tenantID)
+		q.mu.tenants[tenantID] = tenant
+	}
+	if info.BypassAdmission && roachpb.IsSystemTenantID(tenantID) {
+		tenant.used++
+		if len(tenant.waitingWorkHeap) > 0 {
+			q.mu.tenantHeap.fix(tenant)
+		}
+		q.mu.Unlock()
+		q.admitMu.Unlock()
+		q.granter.tookWithoutPermission()
+		q.metrics.Admitted.Inc(1)
+		return nil
+	}
+
+	if len(q.mu.tenantHeap) == 0 {
+		// Fast-path. Try to grab token/slot.
+		// Optimistically update used to avoid locking again.
+		tenant.used++
+		q.mu.Unlock()
+		if q.granter.tryGet() {
+			q.admitMu.Unlock()
+			q.metrics.Admitted.Inc(1)
+			return nil
+		}
+		// Did not get token/slot.
+		//
+		// There is a race here: before q.mu is acquired, the granter could
+		// experiences a reduction in load and call
+		// AdmissionQueue.hasWaitingRequests to see if it should grant, but since
+		// there is nothing in the queue that method will return false. Then the
+		// work here queues up even though granter has spare capacity. We could
+		// add additional synchronization (and complexity to the granter
+		// interface) to deal with this, by keeping the granter's lock
+		// (GrantCoordinator.mu) locked when returning from tryGrant and call
+		// granter again to release that lock after this work has been queued. But
+		// it has the downside of extending the scope of GrantCoordinator.mu.
+		// Instead we tolerate this race in the knowledge that GrantCoordinator
+		// will periodically, at a high frequency, look at the state of the
+		// requesters to see if there is any queued work that can be granted
+		// admission.
+		q.mu.Lock()
+		tenant.used--
+	}
+	// Check deadline.
+	startTime := time.Now()
+	deadline, ok := ctx.Deadline()
+	var doneCh <-chan struct{}
+	if ok {
+		doneCh = ctx.Done()
+		if doneCh == nil {
+			panic(errors.AssertionFailedf("have deadline but no done channel"))
+		}
+		select {
+		case _, ok := <-doneCh:
+			if !ok {
+				// Already cancelled. More likely to happen if cpu starvation is
+				// causing entering into the admission queue to be delayed.
+				q.mu.Unlock()
+				q.admitMu.Unlock()
+				q.metrics.Errored.Inc(1)
+				return errors.Newf("work %s deadline already expired: deadline: %v, now: %v",
+					workKindString(q.workKind), deadline, startTime)
+			}
+		default:
+		}
+	}
+	// Push onto heap(s).
+	work := makeWaitingWork(info.Priority, info.CreateTime)
+	heap.Push(&tenant.waitingWorkHeap, work)
+	if len(tenant.waitingWorkHeap) == 1 {
+		heap.Push(&q.mu.tenantHeap, tenant)
+	}
+	// Else already in tenantHeap.
+	q.metrics.WaitQueueLength.Inc(1)
+
+	if doneCh == nil {
+		doneCh = neverClosedCh
+	}
+	// Release all locks and start waiting.
+	q.mu.Unlock()
+	q.admitMu.Unlock()
+	select {
+	case <-doneCh:
+		q.mu.Lock()
+		if work.heapIndex == -1 {
+			// No longer in heap. Raced with token/slot grant.
+			tenant.used--
+			q.mu.Unlock()
+			q.granter.returnGrant()
+			q.granter.continueGrantChain()
+		} else {
+			tenant.waitingWorkHeap.remove(work)
+			if len(tenant.waitingWorkHeap) == 0 {
+				q.mu.tenantHeap.remove(tenant)
+			}
+			q.mu.Unlock()
+		}
+		q.metrics.Errored.Inc(1)
+		waitDur := time.Now().Sub(startTime)
+		q.metrics.WaitDurationSum.Inc(waitDur.Microseconds())
+		q.metrics.WaitDurations.RecordValue(waitDur.Nanoseconds())
+		q.metrics.WaitQueueLength.Dec(1)
+		return errors.Newf("work %s deadline expired while waiting: deadline: %v, start: %v, dur: %v",
+			workKindString(q.workKind), deadline, startTime, waitDur)
+	case _, ok := <-work.ch:
+		if !ok {
+			panic(errors.AssertionFailedf("channel should not be closed"))
+		}
+		q.metrics.Admitted.Inc(1)
+		waitDur := time.Now().Sub(startTime)
+		q.metrics.WaitDurationSum.Inc(waitDur.Microseconds())
+		q.metrics.WaitDurations.RecordValue(waitDur.Nanoseconds())
+		q.metrics.WaitQueueLength.Dec(1)
+		if work.heapIndex != -1 {
+			panic(errors.AssertionFailedf("grantee should be removed from heap"))
+		}
+		q.granter.continueGrantChain()
+		return nil
+	}
+}
+
+func (q *AdmissionQueue) AdmittedWorkDone(tenantID roachpb.TenantID) {
+	if q.usesTokens {
+		panic(errors.AssertionFailedf("tokens should not be returned"))
+	}
+	q.mu.Lock()
+	tenant, ok := q.mu.tenants[tenantID.ToUint64()]
+	if !ok {
+		panic(errors.AssertionFailedf("tenant not found"))
+	}
+	tenant.used--
+	if len(tenant.waitingWorkHeap) > 0 {
+		q.mu.tenantHeap.fix(tenant)
+	}
+	q.mu.Unlock()
+	q.granter.returnGrant()
+}
+
+func (q *AdmissionQueue) hasWaitingRequests() bool {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	return len(q.mu.tenantHeap) > 0
+}
+
+func (q *AdmissionQueue) granted() bool {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	if len(q.mu.tenantHeap) == 0 {
+		return false
+	}
+	tenant := q.mu.tenantHeap[0]
+	item := heap.Pop(&tenant.waitingWorkHeap).(*waitingWork)
+	item.ch <- struct{}{}
+	tenant.used++
+	if len(tenant.waitingWorkHeap) > 0 {
+		q.mu.tenantHeap.fix(tenant)
+	} else {
+		q.mu.tenantHeap.remove(tenant)
+	}
+	return true
+}
+
+func (q *AdmissionQueue) gcTenantsAndResetTokens() {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	for id, info := range q.mu.tenants {
+		if info.used == 0 && len(info.waitingWorkHeap) == 0 {
+			delete(q.mu.tenants, id)
+		}
+		if q.usesTokens {
+			info.used = 0
+			// All the heap members will reset used=0, so no need to change heap
+			// ordering.
+		}
+	}
+}
+
+// tenantInfo is the per-tenant information in the tenantHeap.
+type tenantInfo struct {
+	id uint64
+	// used can be the currently used slots, or the tokens granted within the last
+	// interval.
+	used            uint64
+	waitingWorkHeap waitingWorkHeap
+
+	// The heapIndex is maintained by the heap.Interface methods, and represents
+	// the heapIndex of the item in the heap.
+	heapIndex int
+}
+
+// tenantHeap is a heap of tenants with waiting work, ordered in increasing
+// order of tenantInfo.used. That is, we prefer tenants that are using less.
+type tenantHeap []*tenantInfo
+
+var _ heap.Interface = (*tenantHeap)(nil)
+
+func makeTenantInfo(id uint64) *tenantInfo {
+	return &tenantInfo{id: id, heapIndex: -1}
+}
+
+func (th *tenantHeap) fix(item *tenantInfo) {
+	heap.Fix(th, item.heapIndex)
+}
+
+func (th *tenantHeap) remove(item *tenantInfo) {
+	heap.Remove(th, item.heapIndex)
+}
+
+func (th *tenantHeap) Len() int {
+	return len(*th)
+}
+
+func (th *tenantHeap) Less(i, j int) bool {
+	return (*th)[i].used < (*th)[j].used
+}
+
+func (th *tenantHeap) Swap(i, j int) {
+	(*th)[i], (*th)[j] = (*th)[j], (*th)[i]
+	(*th)[i].heapIndex = i
+	(*th)[j].heapIndex = j
+}
+
+func (th *tenantHeap) Push(x interface{}) {
+	n := len(*th)
+	item := x.(*tenantInfo)
+	item.heapIndex = n
+	*th = append(*th, item)
+}
+
+func (th *tenantHeap) Pop() interface{} {
+	old := *th
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	item.heapIndex = -1
+	*th = old[0 : n-1]
+	return item
+}
+
+// waitingWork is the per-work information in the waitingWorkHeap.
+type waitingWork struct {
+	priority   WorkPriority
+	createTime int64
+	ch         chan struct{}
+	// The heapIndex is maintained by the heap.Interface methods, and represents
+	// the heapIndex of the item in the heap. -1 when not in the heap.
+	heapIndex int
+}
+
+// waitingWorkHeap is a heap of waiting work within a tenant. It is ordered in
+// decreasing order of priority, and within the same priority in increasing
+// order of createTime (to prefer older work).
+type waitingWorkHeap []*waitingWork
+
+var _ heap.Interface = (*waitingWorkHeap)(nil)
+
+func makeWaitingWork(priority WorkPriority, createTime int64) *waitingWork {
+	return &waitingWork{
+		priority:   priority,
+		createTime: createTime,
+		ch:         make(chan struct{}, 1),
+		heapIndex:  -1,
+	}
+}
+
+func (wwh *waitingWorkHeap) remove(item *waitingWork) {
+	heap.Remove(wwh, item.heapIndex)
+}
+
+func (wwh *waitingWorkHeap) Len() int { return len(*wwh) }
+
+func (wwh *waitingWorkHeap) Less(i, j int) bool {
+	if (*wwh)[i].priority == (*wwh)[j].priority {
+		return (*wwh)[i].createTime < (*wwh)[j].createTime
+	}
+	return (*wwh)[i].priority > (*wwh)[j].priority
+}
+
+func (wwh *waitingWorkHeap) Swap(i, j int) {
+	(*wwh)[i], (*wwh)[j] = (*wwh)[j], (*wwh)[i]
+	(*wwh)[i].heapIndex = i
+	(*wwh)[j].heapIndex = j
+}
+
+func (wwh *waitingWorkHeap) Push(x interface{}) {
+	n := len(*wwh)
+	item := x.(*waitingWork)
+	item.heapIndex = n
+	*wwh = append(*wwh, item)
+}
+
+func (wwh *waitingWorkHeap) Pop() interface{} {
+	old := *wwh
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	item.heapIndex = -1
+	*wwh = old[0 : n-1]
+	return item
+}
+
+var (
+	requestedMeta = metric.Metadata{
+		Name:        "admission.requested.",
+		Help:        "Number of requests",
+		Measurement: "Requests",
+		Unit:        metric.Unit_COUNT,
+	}
+	admittedMeta = metric.Metadata{
+		Name:        "admission.admitted.",
+		Help:        "Number of requests admitted",
+		Measurement: "Requests",
+		Unit:        metric.Unit_COUNT,
+	}
+	erroredMeta = metric.Metadata{
+		Name:        "admission.errored.",
+		Help:        "Number of requests not admitted due to error",
+		Measurement: "Requests",
+		Unit:        metric.Unit_COUNT,
+	}
+	waitDurationSumMeta = metric.Metadata{
+		Name:        "admission.wait_sum.",
+		Help:        "Total wait time in micros",
+		Measurement: "Microseconds",
+		Unit:        metric.Unit_COUNT,
+	}
+	waitDurationsMeta = metric.Metadata{
+		Name:        "admission.wait_durations.",
+		Help:        "Wait time durations for requests that waited",
+		Measurement: "Wait time Duration",
+		Unit:        metric.Unit_NANOSECONDS,
+	}
+	waitQueueLengthMeta = metric.Metadata{
+		Name:        "admission.wait_queue_length.",
+		Help:        "Length of wait queue",
+		Measurement: "Requests",
+		Unit:        metric.Unit_COUNT,
+	}
+)
+
+func addName(name string, meta metric.Metadata) metric.Metadata {
+	rv := meta
+	rv.Name = rv.Name + name
+	return rv
+}
+
+type AdmissionQueueMetrics struct {
+	Requested       *metric.Counter
+	Admitted        *metric.Counter
+	Errored         *metric.Counter
+	WaitDurationSum *metric.Counter
+	WaitDurations   *metric.Histogram
+	WaitQueueLength *metric.Gauge
+}
+
+func (AdmissionQueueMetrics) MetricStruct() {}
+
+func MakeAdmissionQueueMetrics(name string) AdmissionQueueMetrics {
+	return AdmissionQueueMetrics{
+		Requested:       metric.NewCounter(addName(name, requestedMeta)),
+		Admitted:        metric.NewCounter(addName(name, admittedMeta)),
+		Errored:         metric.NewCounter(addName(name, erroredMeta)),
+		WaitDurationSum: metric.NewCounter(addName(name, waitDurationSumMeta)),
+		WaitDurations: metric.NewLatency(
+			addName(name, waitDurationsMeta), base.DefaultHistogramWindowInterval()),
+		WaitQueueLength: metric.NewGauge(addName(name, waitQueueLengthMeta)),
+	}
+}
