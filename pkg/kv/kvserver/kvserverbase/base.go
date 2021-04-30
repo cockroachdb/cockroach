bce7047b8304fe6aa9ef26198d8d04c4a8a6510a
Andrew Werner <awerner32@gmail.com>
2021-04-29 23:52:11 -0400

    kvserver: move `SplitByLoadMergeDelay` to kvserverbase
    This setting was being imported from `sql` which created
    an illegal dependency.
    
    Release note: None

--- pkg/kv/kvserver/kvserverbase/base.go
+++ pkg/kv/kvserver/kvserverbase/base.go
@@ -205,3 +205,11 @@ func IntersectSpan(
 	}
 	return
 }
+
+// SplitByLoadMergeDelay wraps "kv.range_split.by_load_merge_delay".
+var SplitByLoadMergeDelay = settings.RegisterDurationSetting(
+	"kv.range_split.by_load_merge_delay",
+	"the delay that range splits created due to load will wait before considering being merged away",
+	5*time.Minute,
+	settings.NonNegativeDuration,
+)
