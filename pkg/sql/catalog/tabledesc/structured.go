eaa624584ea6188a9ebf9eb2c5d7e22f82aec2a5
Marius Posta <marius@cockroachlabs.com>
2021-05-04 10:01:47 -0400

    tabledesc: prevent panic when removing constraint from Checks slice
    Recently, a run of the TestRandomSyntaxSchemaChangeColumn test failed
    with a panic in tabledesc.MakeMutationComplete, see #63944. The panic
    was triggered by removing multiple elements from the Checks slice in the
    descriptor. While the underlying issue of _how_ this slice could
    possibly contain two constraints sharing the same name has not yet been
    resolved, deleting only the first constraint matching the target name
    will prevent future panics and perhaps help diagnose the underlying
    issue.
    
    Release note: None

--- pkg/sql/catalog/tabledesc/structured.go
+++ pkg/sql/catalog/tabledesc/structured.go
@@ -1572,6 +1572,7 @@ func (desc *Mutable) MakeMutationComplete(m descpb.DescriptorMutation) error {
 				for i, c := range desc.Checks {
 					if c.Name == t.Constraint.Check.Name {
 						desc.Checks = append(desc.Checks[:i], desc.Checks[i+1:]...)
+						break
 					}
 				}
 				col, err := desc.FindColumnWithID(t.Constraint.NotNullColumn)
