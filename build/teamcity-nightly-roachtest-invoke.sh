e7c6dcb22aae9070cac4a79f493fa2889619eda2
Tobias Grieger <tobias.b.grieger@gmail.com>
2021-05-05 14:33:05 +0200

    build: treat exit 10 from roachtest as success
    This will, in the future, allow us to set up notifications for nonzero
    exit status of the roachtest nightly jobs, which would fire only when
    problems with roachtest itself (as opposed to failing tests) occur.
    
    We can't actually fully reconfigure the builds until we have
    https://github.com/cockroachdb/dev-inf/issues/304, as per-build
    notifications will only be available after a TeamCity upgrade.
    However an interim step is possible, where the build will be
    green in the typical case (roachtest ok, but some tests failed)
    and red when anything truly unexpected happens.
    
    Release note: None

--- build/teamcity-nightly-roachtest-invoke.sh
+++ build/teamcity-nightly-roachtest-invoke.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+set +e
 bin/roachtest run \
   --cloud="${CLOUD}" \
   --artifacts="${ARTIFACTS}" \
@@ -17,3 +18,16 @@ bin/roachtest run \
   --slack-token="${SLACK_TOKEN}" \
   --cluster-id="${TC_BUILD_ID}" \
   "${TESTS}"
+code=$?
+set -e
+
+if [[ ${code} -eq 10 ]]; then
+  # Exit code 10 indicates that some tests failed, but that roachtest
+  # as a whole passed. We want to exit zero in this case so that we
+  # can let TeamCity report failing tests without also failing the
+  # build. That way, build failures can be used to notify about serious
+  # problems that prevent tests from being invoked in the first place.
+  code=0
+fi
+
+exit ${code}
