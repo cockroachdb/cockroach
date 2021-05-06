bf30c8812ae511f1e1b382e2595301578d2c8bde
Tobias Grieger <tobias.b.grieger@gmail.com>
2021-05-06 11:47:35 +0200

    roachtest: update election-after-restart
    The recently added in-process test TestElectionAfterRestart was not able
    to reproduce the problems in this test. It is possible that including
    the time for the cluster to boot up after restarting (and connecting to
    each other, exchanging initial gossip, etc) was the reason for its
    spurious failures. In its updated form, this test measures the duration
    in isolation, and it has been made much more aggressive (restarting
    the cluster 100 times, etc). It passes, with the majority duration being
    in the 2s range, rarely ever reaching 4s. I looked at a >3s trace and
    found nothing suspicious, it simply takes a little while to go through
    all of the ranges with continuous progress being made.
    
    Release note: None

--- pkg/cmd/roachtest/election.go
+++ pkg/cmd/roachtest/election.go
@@ -15,13 +15,13 @@ import (
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/stretchr/testify/require"
 )
 
 func registerElectionAfterRestart(r *testRegistry) {
 	r.Add(testSpec{
 		Name:    "election-after-restart",
 		Owner:   OwnerKV,
-		Skip:    "https://github.com/cockroachdb/cockroach/issues/54246",
 		Cluster: makeClusterSpec(3),
 		Run: func(ctx context.Context, t *test, c *cluster) {
 			t.Status("starting up")
@@ -36,6 +36,8 @@ func registerElectionAfterRestart(r *testRegistry) {
 
 			t.Status("creating table and splits")
 			c.Run(ctx, c.Node(1), `./cockroach sql --insecure -e "
+				CREATE TABLE timing(ts TIMESTAMP);
+				INSERT INTO timing VALUES(now());
         CREATE DATABASE IF NOT EXISTS test;
         CREATE TABLE test.kv (k INT PRIMARY KEY, v INT);
         -- Prevent the merge queue from immediately discarding our splits.
@@ -55,42 +57,50 @@ func registerElectionAfterRestart(r *testRegistry) {
 			// settle before restarting.
 			time.Sleep(3 * time.Second)
 
-			t.Status("restarting")
-			c.Stop(ctx)
-			c.Start(ctx, t)
+			for i := 0; i < 100; i++ {
+				t.Status("restarting")
+				c.Stop(ctx)
+				c.Start(ctx, t)
 
-			// Each of the 100 ranges in this table must elect a leader for
-			// this query to complete. In naive raft, each of these
-			// elections would require waiting for a 3-second timeout, one
-			// at a time. This test verifies that our mechanisms to speed
-			// this up are working (we trigger elections eagerly, but not so
-			// eagerly that multiple elections conflict with each other).
-			start = timeutil.Now()
-			// Use a large CONNECT_TIMEOUT so that if the initial connection
-			// takes ages (perhaps due to some cli-internal query taking a
-			// very long time), we fail with the duration check below and
-			// not an opaque error from the cli.
-			buf, err := c.RunWithBuffer(ctx, t.l, c.Node(1), `COCKROACH_CONNECT_TIMEOUT=240 ./cockroach sql --insecure -e "
+				// Each of the 100 ranges in this table must elect a leader for
+				// this query to complete. In naive raft, each of these
+				// elections would require waiting for a 3-second timeout, one
+				// at a time. This test verifies that our mechanisms to speed
+				// this up are working (we trigger elections eagerly, but not so
+				// eagerly that multiple elections conflict with each other).
+				//
+				// Use a large CONNECT_TIMEOUT so that if the initial connection
+				// takes ages (perhaps due to some cli-internal query taking a
+				// very long time), we fail with the duration check below and
+				// not an opaque error from the cli.
+				buf, err := c.RunWithBuffer(ctx, t.l, c.Node(1), `COCKROACH_CONNECT_TIMEOUT=240 ./cockroach sql --insecure -e "
 SET TRACING = on;
-SELECT * FROM test.kv;
+UPDATE timing SET ts = now() WHERE true;
+UPDATE timing SET ts = now() WHERE true;
+SELECT count(1) FROM test.kv;
 SET TRACING = off;
 SHOW TRACE FOR SESSION;
 "`)
-			if err != nil {
-				t.Fatalf("%s\n\n%s", buf, err)
-			}
-			duration = timeutil.Since(start)
-			t.l.Printf("post-restart, query took %s\n", duration)
-			if expected := 15 * time.Second; duration > expected {
-				// In the happy case, this query runs in around 250ms. Prior
-				// to the introduction of this test, a bug caused most
-				// elections to fail and the query would take over 100
-				// seconds. There are still issues that can cause a few
-				// elections to fail (the biggest one as I write this is
-				// #26448), so we must use a generous timeout here. We may be
-				// able to tighten the bounds as we make more improvements.
-				t.l.Printf("%s\n", buf)
-				t.Fatalf("expected query to succeed in less than %s, took %s", expected, duration)
+				if err != nil {
+					t.Fatalf("%s\n\n%s", buf, err)
+				}
+
+				var duration time.Duration
+				require.NoError(t, c.Conn(ctx, 1).QueryRow(
+					`SELECT 1000*1000*extract(milliseconds FROM (now()-ts))::int FROM timing`,
+				).Scan(&duration))
+				t.l.Printf("post-restart, query took %s\n", duration)
+				if expected := 10 * time.Second; duration > expected {
+					// In the happy case, this query runs in around 250ms. Prior
+					// to the introduction of this test, a bug caused most
+					// elections to fail and the query would take over 100
+					// seconds. There are still issues that can cause a few
+					// elections to fail (the biggest one as I write this is
+					// #26448), so we must use a generous timeout here. We may be
+					// able to tighten the bounds as we make more improvements.
+					t.l.Printf("%s\n", buf)
+					t.Fatalf("expected query to succeed in less than %s, took %s", expected, duration)
+				}
 			}
 		},
 	})
