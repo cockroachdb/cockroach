0f65a610bd48ab43767b64f3d8a09e04f2734b09
Ye Ji <ye@cockroachlabs.com>
2021-05-10 14:51:11 -0400

    sqlproxyccl: add support for expiring a denylist entry
    Previously, there's no easy way to specify an expiration
    time for each entry. This PR adds a new denylist config format
    that allows encoding of expiration time in the config file.
    
    Additionally, the config file used to be "free-form", and the parsing is
    offloaded to viper. This PR fixes the spec of the config file in yaml.
    
    Release note: None

--- pkg/ccl/sqlproxyccl/denylist/local_file_test.go
+++ pkg/ccl/sqlproxyccl/denylist/local_file_test.go
@@ -16,8 +16,37 @@ import (
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/stretchr/testify/require"
 )
+
+func TestViperForwardCompatibility(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	file := File{
+		Seq: 3,
+		Denylist: []*DenyEntry{
+			{DenyEntity{"1.1.1.1", IPAddrType}, timeutil.Now().Add(time.Hour), "reason"},
+		},
+	}
+
+	cfgFile, err := ioutil.TempFile("", "*_denylist.yml")
+	require.NoError(t, err)
+	defer func() { _ = os.Remove(cfgFile.Name()) }()
+
+	raw, err := file.Serialize()
+	require.NoError(t, err)
+	err = ioutil.WriteFile(cfgFile.Name(), raw, 0777)
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// make sure old parser won't break on new config format
+	_, err = NewViperDenyListFromFile(ctx, cfgFile.Name(),
+		time.Minute)
+	require.NoError(t, err)
+}
 
 func TestViperDenyList(t *testing.T) {
 	defer leaktest.AfterTest(t)()
@@ -32,14 +61,14 @@ func TestViperDenyList(t *testing.T) {
 	dl, err := NewViperDenyListFromFile(ctx, cfgFile.Name(), time.Millisecond)
 	require.NoError(t, err)
 
-	e, err := dl.Denied("123")
+	e, err := dl.Denied(DenyEntity{"123", ClusterType})
 	require.NoError(t, err)
 	require.True(t, e == nil)
 
 	_, err = cfgFile.Write([]byte("456: denied"))
 	require.NoError(t, err)
 	time.Sleep(50 * time.Millisecond)
-	e, err = dl.Denied("456")
+	e, err = dl.Denied(DenyEntity{"456", ClusterType})
 	require.NoError(t, err)
 	require.Equal(t, &Entry{Reason: "denied"}, e)
 }
--- pkg/ccl/sqlproxyccl/denylist/service.go
+++ pkg/ccl/sqlproxyccl/denylist/service.go
@@ -8,6 +8,8 @@
 
 package denylist
 
+import "strings"
+
 //go:generate mockgen -package=denylist -destination=mocks_generated.go -source=service.go . Service
 
 // Entry records the reason for putting an item on the denylist.
@@ -16,11 +18,71 @@ type Entry struct {
 	Reason string
 }
 
+// DenyEntity represent one denied entity.
+// This also serves as the spec for the config format.
+type DenyEntity struct {
+	Item string `yaml:"item"`
+	Type Type   `yaml:"type"`
+}
+
+// Type is the type of the denied entity
+type Type int
+
+// Enum values for Type
+const (
+	IPAddrType Type = iota + 1
+	ClusterType
+	UnknonwType
+)
+
+var strToTypeMap = map[string]Type{
+	"ip":      IPAddrType,
+	"cluster": ClusterType,
+}
+
+var typeToStrMap = map[Type]string{
+	IPAddrType:  "ip",
+	ClusterType: "cluster",
+}
+
+// UnmarshalYAML implements yaml.Unmarshaler interface for type
+func (typ *Type) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var raw string
+	err := unmarshal(&raw)
+	if err != nil {
+		return err
+	}
+
+	normalized := strings.ToLower(raw)
+	t, ok := strToTypeMap[normalized]
+	if !ok {
+		*typ = UnknonwType
+	} else {
+		*typ = t
+	}
+
+	return nil
+}
+
+// MarshalYAML implements yaml.Marshaler interface for type
+func (typ Type) MarshalYAML() (interface{}, error) {
+	return typ.String(), nil
+}
+
+// String() implements Stringer interface for type
+func (typ Type) String() string {
+	s, ok := typeToStrMap[typ]
+	if !ok {
+		return "UNKNOWN"
+	}
+	return s
+}
+
 // Service provides an interface for checking if an id has been denied access.
 type Service interface {
 	// Denied returns a non-nil Entry if the id is denied. The reason for the
 	// denial will be in Entry.
-	Denied(id string) (*Entry, error)
+	Denied(entity DenyEntity) (*Entry, error)
 
 	// TODO(spaskob): add API for registering listeners to be notified of any
 	// updates (inclusion/exclusion) to the denylist.
