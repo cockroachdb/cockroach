67a3392c358414fb71587177b34fef625ea9c8d7
Aditya Maru <adityamaru@gmail.com>
2021-05-14 11:41:15 +0100

    jobs: save job trace ID in job payload
    Previously, a job on creation would create a new context
    and wrap it with a "forked span" from the parent ctx. This
    meant that the span would inherit the traceID from its
    remote parent. When the job began executing, we would create
    yet another span. This span would be a child of the "forked
    span" mentioned above, if the job was being started on the
    same registry that created it. If the job was being resumed
    by a new registry, we would create a root span.
    
    This change deletes the root span created during job creation,
    and always creates a root span when the job begins executing.
    We did not use the span created on job creation for anything
    and therefore this seems like an appropriate simplification.
    
    Creating a root span allows us to identify only those spans linked
    to job execution using the trace_id assigned to this root span.
    This will be helpful when we build tooling to improve job
    observability that will pull all the relevant spans.
    
    This change also stores the trace_id associated with the current
    job resumer in the jobs' payload. This allows us to use it at
    a later time to inspect what a job is doing at a given point in
    time!
    
    Release note: None

--- pkg/jobs/jobs.go
+++ pkg/jobs/jobs.go
@@ -29,7 +29,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
-	"github.com/cockroachdb/cockroach/pkg/util/tracing"
 	"github.com/cockroachdb/errors"
 	"github.com/cockroachdb/redact"
 )
@@ -86,10 +85,19 @@ type StartableJob struct {
 	resumer    Resumer
 	resumerCtx context.Context
 	cancel     context.CancelFunc
-	span       *tracing.Span
 	execDone   chan struct{}
 	execErr    error
 	starts     int64 // used to detect multiple calls to Start()
+}
+
+// TraceableJob is associated with a Job object, and can be used to configure
+// the root tracing span that will be tied to the jobs' execution.
+// By default, a job will create a `noop` root span that will discard all
+// recordings.
+type TraceableJob interface {
+	// ForceRealSpan forces the registry to create a real Span instead of a
+	// low-overhead non-recordable noop span.
+	ForceRealSpan() bool
 }
 
 func init() {
@@ -835,23 +843,14 @@ func (sj *StartableJob) Start(ctx context.Context) (err error) {
 		return fmt.Errorf("cannot resume %T job which is not committed", sj.resumer)
 	}
 
-	finishSpan := func() {
-		if sj.span != nil {
-			sj.span.Finish()
-		}
-	}
-
 	if err := sj.started(ctx, nil /* txn */); err != nil {
-		finishSpan()
 		return err
 	}
 
 	if err := sj.registry.stopper.RunAsyncTask(ctx, sj.taskName(), func(ctx context.Context) {
 		sj.execErr = sj.registry.runJob(sj.resumerCtx, sj.resumer, sj.Job, StatusRunning, sj.taskName())
 		close(sj.execDone)
-		finishSpan()
 	}); err != nil {
-		finishSpan()
 		return err
 	}
 
@@ -907,9 +906,6 @@ func (sj *StartableJob) CleanupOnRollback(ctx context.Context) error {
 	// Given that, proceed to clean up regardless.
 
 	sj.registry.unregister(sj.ID())
-	if sj.span != nil {
-		sj.span.Finish()
-	}
 	if sj.cancel != nil {
 		sj.cancel()
 	}
