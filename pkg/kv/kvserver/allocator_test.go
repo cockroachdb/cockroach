568a5c6732ee1596978458ed1b8366ac689f1833
Aayush Shah <aayush.shah15@gmail.com>
2021-05-16 19:20:56 -0400

    kvserver: disallow replica rebalancing to dead nodes
    The previously existing logic to prevent replica rebalances to dead nodes was
    totally broken, and it was never caught because the accompanying unit test was
    also broken.
    
    The previous logic had an early return clause during the computation of
    diversity stats for _existing replicas_ rather than during the computation of
    stats for _potential candidates_. Additionally, the scenario that was
    previously created by this unit test was never going to trigger a replica
    rebalance, and the unit test would never actually execute any of its checks in
    the face of an empty result set.
    
    This commit fixes the check to actually prevent rebalancing to dead nodes and
    also re-structures the test.
    
    /cc @cockroachdb/kv
    
    Release note (bug fix): Replica rebalancing could previously sometimes
    rebalance to stores on dead nodes. This bug is now fixed.

--- pkg/kv/kvserver/allocator_test.go
+++ pkg/kv/kvserver/allocator_test.go
@@ -2582,6 +2582,8 @@ func TestAllocateCandidatesExcludeNonReadyNodes(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 
+	// NB: These stores are ordered from least likely to most likely to receive a
+	// replica.
 	stores := []*roachpb.StoreDescriptor{
 		{
 			StoreID:  1,
@@ -2591,17 +2593,17 @@ func TestAllocateCandidatesExcludeNonReadyNodes(t *testing.T) {
 		{
 			StoreID:  2,
 			Node:     roachpb.NodeDescriptor{NodeID: 2},
-			Capacity: roachpb.StoreCapacity{Capacity: 200, Available: 100, RangeCount: 600},
+			Capacity: roachpb.StoreCapacity{Capacity: 200, Available: 100, RangeCount: 450},
 		},
 		{
 			StoreID:  3,
 			Node:     roachpb.NodeDescriptor{NodeID: 3},
-			Capacity: roachpb.StoreCapacity{Capacity: 200, Available: 100, RangeCount: 600},
+			Capacity: roachpb.StoreCapacity{Capacity: 200, Available: 100, RangeCount: 300},
 		},
 		{
 			StoreID:  4,
 			Node:     roachpb.NodeDescriptor{NodeID: 4},
-			Capacity: roachpb.StoreCapacity{Capacity: 200, Available: 100, RangeCount: 600},
+			Capacity: roachpb.StoreCapacity{Capacity: 200, Available: 100, RangeCount: 150},
 		},
 	}
 
@@ -2630,6 +2632,11 @@ func TestAllocateCandidatesExcludeNonReadyNodes(t *testing.T) {
 			[]roachpb.StoreID{1},
 			[]roachpb.StoreID{2, 3, 4},
 			[]roachpb.StoreID{},
+		},
+		{
+			[]roachpb.StoreID{1},
+			[]roachpb.StoreID{2, 4},
+			[]roachpb.StoreID{3},
 		},
 	}
 
@@ -2676,7 +2683,7 @@ func TestAllocateCandidatesExcludeNonReadyNodes(t *testing.T) {
 		})
 
 		t.Run(fmt.Sprintf("%d/rebalance", testIdx), func(t *testing.T) {
-			results := rankedCandidateListForRebalancing(
+			rebalanceOpts := rankedCandidateListForRebalancing(
 				context.Background(),
 				sl,
 				removalConstraintsChecker,
@@ -2687,24 +2694,20 @@ func TestAllocateCandidatesExcludeNonReadyNodes(t *testing.T) {
 				a.storePool.isNodeReadyForRoutineReplicaTransfer,
 				a.scorerOptions(),
 			)
-
-			for i := range results {
-				if !expectedStoreIDsMatch(tc.existing, results[i].existingCandidates) {
-					t.Errorf(
-						"results[%d]: expected existing candidates %v, got %v",
-						i,
-						tc.existing,
-						results[i].existingCandidates,
-					)
+			if len(tc.expected) > 0 {
+				require.Len(t, rebalanceOpts, 1)
+				candidateStores := make([]roachpb.StoreID, len(rebalanceOpts[0].candidates))
+				for i, cand := range rebalanceOpts[0].candidates {
+					candidateStores[i] = cand.store.StoreID
 				}
-				if !expectedStoreIDsMatch(tc.expected, results[i].candidates) {
-					t.Errorf(
-						"results[%d]: expected candidates %v, got %v",
-						i,
-						tc.expected,
-						results[i].candidates,
-					)
+				require.ElementsMatch(t, tc.expected, candidateStores)
+				existingStores := make([]roachpb.StoreID, len(rebalanceOpts[0].existingCandidates))
+				for i, cand := range rebalanceOpts[0].existingCandidates {
+					existingStores[i] = cand.store.StoreID
 				}
+				require.ElementsMatch(t, existingStores, tc.existing)
+			} else {
+				require.Len(t, rebalanceOpts, 0)
 			}
 		})
 	}
