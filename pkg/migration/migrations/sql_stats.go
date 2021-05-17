0a1e719268dfbac5474ec298ce4a438019826ac4
Azhng <archer.xn@gmail.com>
2021-05-17 18:50:37 -0400

    sql: create system tables for SQL stats
    This PR creates two new system tables: system.statement_stats
    and system.transaction_stats per RFC #63752.
    
    This is the initial step that addresses #64743.
    
    Release note: None

---
+++ pkg/migration/migrations/sql_stats.go
@@ -0,0 +1,36 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package migrations
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
+	"github.com/cockroachdb/cockroach/pkg/migration"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/systemschema"
+	"github.com/cockroachdb/cockroach/pkg/sqlmigrations"
+)
+
+func sqlStatementStatsTableMigration(
+	ctx context.Context, _ clusterversion.ClusterVersion, d migration.TenantDeps,
+) error {
+	return sqlmigrations.CreateSystemTable(
+		ctx, d.DB, d.Codec, d.Settings, systemschema.StatementStatisticsTable,
+	)
+}
+
+func sqlTransactionStatsTableMigration(
+	ctx context.Context, _ clusterversion.ClusterVersion, d migration.TenantDeps,
+) error {
+	return sqlmigrations.CreateSystemTable(
+		ctx, d.DB, d.Codec, d.Settings, systemschema.TransactionStatisticsTable,
+	)
+}
