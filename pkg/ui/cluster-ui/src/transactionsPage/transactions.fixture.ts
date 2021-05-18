ffe2b684f885f5a28508d3cdf5841d0fedd4ee60
Andrii Vorobiov <and.vorobiov@gmail.com>
2021-05-18 12:58:32 +0300

    ui: add copyright headers to `cluster-ui` files
    Files that were moved as part of cluster-ui package from `ui` repository
    didn't have copyright headers that are required in current repo to
    comply with lint rules.
    
    This change appends copyright headers to all files in the package.
    
    Release note: none

--- pkg/ui/cluster-ui/src/transactionsPage/transactions.fixture.ts
+++ pkg/ui/cluster-ui/src/transactionsPage/transactions.fixture.ts
@@ -1,3 +1,13 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
 /* eslint-disable @typescript-eslint/camelcase */
 import { createMemoryHistory } from "history";
 import { cockroach } from "@cockroachlabs/crdb-protobuf-client";
