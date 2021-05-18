71524926d65ea452442e40fc04b90c928ac79e29
Andrii Vorobiov <and.vorobiov@gmail.com>
2021-05-18 12:58:32 +0300

    ui: add copyright headers to `cluster-ui` files
    Files that were moved as part of cluster-ui package from `ui` repository
    didn't have copyright headers that are required in current repo to
    comply with lint rules.
    
    This change appends copyright headers to all files in the package.
    
    Release note: none

--- pkg/ui/cluster-ui/src/store/nodes/nodes.sagas.ts
+++ pkg/ui/cluster-ui/src/store/nodes/nodes.sagas.ts
@@ -1,3 +1,13 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
 import { all, call, put, delay, takeLatest } from "redux-saga/effects";
 import { getNodes } from "src/api/nodesApi";
 import { actions } from "./nodes.reducer";
--- pkg/ui/cluster-ui/src/store/utils/noopReducer.ts
+++ pkg/ui/cluster-ui/src/store/utils/noopReducer.ts
@@ -1,3 +1,13 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
 /**
  * noopReducer is a stub function to use with `createSlice` (@redux-toolkit) as a definition
  * for reducer case which should not change state but it has to define an action which might be
