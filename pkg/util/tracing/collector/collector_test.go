4b4c2090dfdafec83be791407d7e3f4973a93657
Aditya Maru <adityamaru@gmail.com>
2021-05-20 16:38:47 +0100

    tracing: adds a trace service to pull clusterwide trace spans
    Previously, every node in the cluster had a local inflight span
    registry that was aware of all the spans that were rooted on that
    particular node. Child spans of a given traceID executing on a remote
    node would only become visible to the local registry once execution
    completes, and the span pushes its recordings over gRPC to the
    "client" node.
    
    This change introduces a `collector` and `service` package
    that together contain a gRPC service to be used for
    remote inflight span access.
    
    It is used for pulling inflight spans from all CockroachDB nodes.
    Each node will run a trace service, which serves the inflight spans from the
    local span registry on that node. Each node will also have a trace collector
    which uses the nodedialer to connect to another node's trace service,
    and access its inflight spans.
    
    The spans for a traceID are sorted by `StartTime` before they are
    returned. The per node trace dialer has yet to be hooked up to an
    appropriate location depending on where we intend to use it.
    
    Release note: None

---
+++ pkg/util/tracing/collector/collector_test.go
@@ -0,0 +1,167 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package collector_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/liveness"
+	"github.com/cockroachdb/cockroach/pkg/rpc/nodedialer"
+	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/tracing"
+	"github.com/cockroachdb/cockroach/pkg/util/tracing/collector"
+	"github.com/gogo/protobuf/types"
+	"github.com/stretchr/testify/require"
+)
+
+// testStructuredImpl is a testing implementation of Structured event.
+type testStructuredImpl struct {
+	*types.StringValue
+}
+
+var _ tracing.Structured = &testStructuredImpl{}
+
+func (t *testStructuredImpl) String() string {
+	return fmt.Sprintf("structured=%s", t.Value)
+}
+
+func newTestStructured(i string) *testStructuredImpl {
+	return &testStructuredImpl{
+		&types.StringValue{Value: i},
+	}
+}
+
+// setupTraces takes two tracers (potentially on different nodes), and creates
+// two span hierarchies as depicted below. The method returns the traceIDs for
+// both these span hierarchies, along with a cleanup method to Finish() all the
+// opened spans.
+//
+// Trace for t1:
+// -------------
+// root													<-- traceID1
+// 		root.child								<-- traceID1
+// root2.child.remotechild 			<-- traceID2
+// root2.child.remotechild2 		<-- traceID2
+//
+// Trace for t2:
+// -------------
+// root.child.remotechild				<-- traceID1
+// root.child.remotechilddone		<-- traceID1
+// root2												<-- traceID2
+// 		root2.child								<-- traceID2
+func setupTraces(t1, t2 *tracing.Tracer) (uint64, uint64, func()) {
+	// Start a root span on "node 1".
+	root := t1.StartSpan("root", tracing.WithForceRealSpan())
+	root.SetVerbose(true)
+	root.RecordStructured(newTestStructured("root"))
+
+	time.Sleep(10 * time.Millisecond)
+
+	// Start a child span on "node 1".
+	child := t1.StartSpan("root.child", tracing.WithParentAndAutoCollection(root))
+
+	// Sleep a bit so that everything that comes afterwards has higher timestamps
+	// than the one we just assigned. Otherwise the sorting is not deterministic.
+	time.Sleep(10 * time.Millisecond)
+
+	// Start a remote child span on "node 2".
+	childRemoteChild := t2.StartSpan("root.child.remotechild", tracing.WithParentAndManualCollection(child.Meta()))
+	childRemoteChild.RecordStructured(newTestStructured("root.child.remotechild"))
+
+	time.Sleep(10 * time.Millisecond)
+
+	// Start another remote child span on "node 2" that we finish.
+	childRemoteChildFinished := t2.StartSpan("root.child.remotechilddone", tracing.WithParentAndManualCollection(child.Meta()))
+	childRemoteChildFinished.Finish()
+	child.ImportRemoteSpans(childRemoteChildFinished.GetRecording())
+
+	// Start a root span on "node 2".
+	root2 := t2.StartSpan("root2", tracing.WithForceRealSpan())
+	root2.SetVerbose(true)
+	root2.RecordStructured(newTestStructured("root2"))
+
+	// Start a child span on "node 2".
+	child2 := t2.StartSpan("root2.child", tracing.WithParentAndAutoCollection(root2))
+	// Start a remote child span on "node 1".
+	child2RemoteChild := t1.StartSpan("root2.child.remotechild", tracing.WithParentAndManualCollection(child2.Meta()))
+
+	time.Sleep(10 * time.Millisecond)
+
+	// Start another remote child span on "node 1".
+	anotherChild2RemoteChild := t1.StartSpan("root2.child.remotechild2", tracing.WithParentAndManualCollection(child2.Meta()))
+	return root.TraceID(), root2.TraceID(), func() {
+		for _, span := range []*tracing.Span{root, child, childRemoteChild, root2, child2,
+			child2RemoteChild, anotherChild2RemoteChild} {
+			span.Finish()
+		}
+	}
+}
+
+func TestTracingCollectorGetSpanRecordings(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	args := base.TestClusterArgs{}
+	tc := testcluster.StartTestCluster(t, 2 /* nodes */, args)
+	defer tc.Stopper().Stop(ctx)
+
+	localTracer := tc.Server(0).Tracer().(*tracing.Tracer)
+	remoteTracer := tc.Server(1).Tracer().(*tracing.Tracer)
+
+	traceCollector := collector.New(
+		tc.Server(0).NodeDialer().(*nodedialer.Dialer),
+		tc.Server(0).NodeLiveness().(*liveness.NodeLiveness), localTracer)
+	localTraceID, remoteTraceID, cleanup := setupTraces(localTracer, remoteTracer)
+	defer cleanup()
+
+	t.Run("fetch-local-recordings", func(t *testing.T) {
+		recordedSpan, err := traceCollector.GetSpanRecordingsFromCluster(ctx, localTraceID)
+		require.NoError(t, err)
+
+		require.NoError(t, tracing.TestingCheckRecordedSpans(recordedSpan, `
+			span: root
+				tags: _unfinished=1 _verbose=1
+				event: structured=root
+				span: root.child
+					tags: _unfinished=1 _verbose=1
+					span: root.child.remotechild
+						tags: _unfinished=1 _verbose=1
+						event: structured=root.child.remotechild
+					span: root.child.remotechilddone
+						tags: _verbose=1
+`))
+	})
+
+	// The traceCollector is running on node 1, so most of the recordings for this
+	// subtest will be passed back by node 2 over RPC.
+	t.Run("fetch-remote-recordings", func(t *testing.T) {
+		recordedSpan, err := traceCollector.GetSpanRecordingsFromCluster(ctx, remoteTraceID)
+		require.NoError(t, err)
+
+		require.NoError(t, tracing.TestingCheckRecordedSpans(recordedSpan, `
+			span: root2
+				tags: _unfinished=1 _verbose=1
+				event: structured=root2
+				span: root2.child
+					tags: _unfinished=1 _verbose=1
+					span: root2.child.remotechild
+						tags: _unfinished=1 _verbose=1
+					span: root2.child.remotechild2
+						tags: _unfinished=1 _verbose=1
+`))
+	})
+}
