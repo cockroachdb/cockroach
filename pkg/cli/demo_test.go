5ffa1cc833a28d3de2b5d822f34f92c3115189f6
Raphael 'kena' Poss <knz@thaumogen.net>
2021-05-23 00:17:49 +0200

    cli/start,demo: produce JDBC-compatible connection URLs
    Some client drivers do not recognize user/password information passed
    before the "@" sign in the connection URL, most notably the JDBC
    driver.
    
    Example when a new server starts:
    
    Before:
    ```
    sql:        postgresql://demo:demo123@kenax:26257/defaultdb?sslmode=disable
    ```
    
    After:
    ```
    sql:        postgresql://demo:demo123@kenax:26257/defaultdb?sslmode=disable
    sql (JDBC): postgresql://kenax:26257/defaultdb?sslmode=disable&user=demo&password=demo123
    ```
    
    Release note (cli change): The connection URLs spelled out by
    `cockroach start`, `start-single-node`, `demo` in various outputs now
    include a variant suitable for use with JDBC client apps.

--- pkg/cli/demo_test.go
+++ pkg/cli/demo_test.go
@@ -279,7 +279,7 @@ func TestTransientClusterSimulateLatencies(t *testing.T) {
 		t.Run(tc.desc, func(t *testing.T) {
 			url, err := c.getNetworkURLForServer(ctx, tc.nodeIdx, true /* includeAppName */)
 			require.NoError(t, err)
-			conn := makeSQLConn(url)
+			conn := makeSQLConn(url.ToPQ().String())
 			defer conn.Close()
 			// Find the maximum latency in the cluster from the current node.
 			var maxLatency time.Duration
