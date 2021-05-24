dcfd99a7c004da932f24e730bb04a4b4e8d1ae40
Andrii Vorobiov <and.vorobiov@gmail.com>
2021-05-24 14:49:38 +0300

    ui: add copyright headers to `cluster-ui` files
    Files that were moved as part of cluster-ui package from `ui` repository
    didn't have copyright headers that are required in current repo to
    comply with lint rules.
    
    This change appends copyright headers to all files in the package.
    
    Release note: none

--- pkg/ui/cluster-ui/src/store/liveness/liveness.sagas.spec.ts
+++ pkg/ui/cluster-ui/src/store/liveness/liveness.sagas.spec.ts
@@ -1,3 +1,13 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
 import { expectSaga } from "redux-saga-test-plan";
 import { throwError } from "redux-saga-test-plan/providers";
 import * as matchers from "redux-saga-test-plan/matchers";
