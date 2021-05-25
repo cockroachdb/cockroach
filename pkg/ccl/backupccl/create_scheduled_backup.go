a6aced0d823611516126ee96c17d40387ab9b4a9
Anne Zhu <anne.zhu@cockroachlabs.com>
2021-05-25 11:53:35 -0400

    backupccl: fix include interleaved tables option for scheduled backup
    This commit fixes the bug that causes error when creating scheduled backup with interleaved tables, when the `include_deprecated_interleaves` option is included, the option gets ignored.
    
    Resolves: #65558
    
    Release note (bug fix): scheduled backup with interleaved tables can now be created with the `include_deprecated_interleaves` option.

--- pkg/ccl/backupccl/create_scheduled_backup.go
+++ pkg/ccl/backupccl/create_scheduled_backup.go
@@ -231,8 +231,9 @@ func doCreateBackupSchedules(
 	// Prepare backup statement (full).
 	backupNode := &tree.Backup{
 		Options: tree.BackupOptions{
-			CaptureRevisionHistory: eval.BackupOptions.CaptureRevisionHistory,
-			Detached:               true,
+			CaptureRevisionHistory:       eval.BackupOptions.CaptureRevisionHistory,
+			Detached:                     true,
+			IncludeDeprecatedInterleaves: eval.BackupOptions.IncludeDeprecatedInterleaves,
 		},
 		Nested:         true,
 		AppendToLatest: false,
