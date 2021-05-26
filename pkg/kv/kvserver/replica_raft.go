8d740d951b1d4090cee64992d2e21c891a5f94fe
Tobias Grieger <tobias.b.grieger@gmail.com>
2021-05-26 10:48:33 +0200

    kvserver: make wrapper around rawnode.Campaign
    Release note: None

--- pkg/kv/kvserver/replica_raft.go
+++ pkg/kv/kvserver/replica_raft.go
@@ -889,7 +889,9 @@ func (r *Replica) handleRaftReadyRaftMuLocked(
 			//
 			// NB: this must be called after Advance() above since campaigning is
 			// a no-op in the presence of unapplied conf changes.
-			maybeCampaignAfterConfChange(ctx, r.store.StoreID(), r.descRLocked(), raftGroup)
+			if maybeCampaignAfterConfChange(ctx, r.store.StoreID(), r.descRLocked(), raftGroup) {
+				r.campaignLocked(ctx)
+			}
 		}
 
 		// If the Raft group still has more to process then we immediately
@@ -1605,12 +1607,16 @@ func (r *Replica) maybeCampaignOnWakeLocked(ctx context.Context) {
 	raftStatus := r.mu.internalRaftGroup.BasicStatus()
 	livenessMap, _ := r.store.livenessMap.Load().(liveness.IsLiveMap)
 	if shouldCampaignOnWake(leaseStatus, r.store.StoreID(), raftStatus, livenessMap, r.descRLocked(), r.requiresExpiringLeaseRLocked()) {
-		log.VEventf(ctx, 3, "campaigning")
-		if err := r.mu.internalRaftGroup.Campaign(); err != nil {
-			log.VEventf(ctx, 1, "failed to campaign: %s", err)
-		}
-		r.store.enqueueRaftUpdateCheck(r.RangeID)
+		r.campaignLocked(ctx)
 	}
+}
+
+func (r *Replica) campaignLocked(ctx context.Context) {
+	log.VEventf(ctx, 3, "campaigning")
+	if err := r.mu.internalRaftGroup.Campaign(); err != nil {
+		log.VEventf(ctx, 1, "failed to campaign: %s", err)
+	}
+	r.store.enqueueRaftUpdateCheck(r.RangeID)
 }
 
 // a lastUpdateTimesMap is maintained on the Raft leader to keep track of the
@@ -1908,7 +1914,7 @@ func maybeCampaignAfterConfChange(
 	storeID roachpb.StoreID,
 	desc *roachpb.RangeDescriptor,
 	raftGroup *raft.RawNode,
-) {
+) bool {
 	// If a config change was carried out, it's possible that the Raft
 	// leader was removed. Verify that, and if so, campaign if we are
 	// the first remaining voter replica. Without this, the range will
@@ -1921,21 +1927,22 @@ func maybeCampaignAfterConfChange(
 	if st.Lead == 0 {
 		// Leader unknown. This isn't what we expect in steady state, so we
 		// don't do anything.
-		return
+		return false
 	}
 	if !desc.IsInitialized() {
 		// We don't have an initialized, so we can't figure out who is supposed
 		// to campaign. It's possible that it's us and we're waiting for the
 		// initial snapshot, but it's hard to tell. Don't do anything.
-		return
+		return false
 	}
 	// If the leader is no longer in the descriptor but we are the first voter,
 	// campaign.
 	_, leaderStillThere := desc.GetReplicaDescriptorByID(roachpb.ReplicaID(st.Lead))
 	if !leaderStillThere && storeID == desc.Replicas().VoterDescriptors()[0].StoreID {
-		log.VEventf(ctx, 3, "leader got removed by conf change; campaigning")
-		_ = raftGroup.Campaign()
+		log.VEventf(ctx, 3, "leader got removed by conf change")
+		return true
 	}
+	return false
 }
 
 func getNonDeterministicFailureExplanation(err error) string {
