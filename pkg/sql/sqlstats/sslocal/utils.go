64fd75b351dc8872b8cb60c4d673191406049db2
Azhng <archer.xn@gmail.com>
2021-05-25 20:57:58 -0400

    sql: refactor SQL stats collection to its own package
    Previously, SQL stats collection is implemented within the sql
    package. However, this is not ideal because it introduces a
    high-level coupling between the SQLServer and rest of subsystems
    within CRDB. As we move to persisted SQL stats, the SQL stats
    collection will experience an increase of complexity. Hence,
    refactoring SQL stats collection into its own subsystem abstracts
    away that complexity behind a set of interfaces.
    
    Release note: None
    
    Closes #65699

---
+++ pkg/sql/sqlstats/sslocal/utils.go
@@ -0,0 +1,39 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package sslocal
+
+import "github.com/cockroachdb/cockroach/pkg/sql/sqlstats"
+
+type stmtList []stmtKey
+
+func (s stmtList) Len() int {
+	return len(s)
+}
+func (s stmtList) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s stmtList) Less(i, j int) bool {
+	return s[i].anonymizedStmt < s[j].anonymizedStmt
+}
+
+type txnList []sqlstats.TransactionFingerprintID
+
+func (t txnList) Len() int {
+	return len(t)
+}
+
+func (t txnList) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+func (t txnList) Less(i, j int) bool {
+	return t[i] < t[j]
+}
