f3af604692be6d30c498b921a3857f9ba5ab21cc
Paul Bardea <pbardea@gmail.com>
2021-05-27 15:55:58 -0400

    backupccl: fix cluster restore progress logging
    This commit fixes a bug in cluster RESTORE's progress logging. When
    RESTORE was updated to support several phases, it made sure that the
    highwatermark of the restore was only updated during the main portion of
    the restore. However, there was a bug where the "fraction progressed"
    metric was updated by the non-main bundle of data to restore.
    
    This change makes is so that fraction progressed is only updated during
    the main data bundle.
    
    Release note (bug fix): Fixes a bug introduces in 21.1 where cluster
    restores would report inaccurate progress, showing 100% progress
    erroneously.

--- pkg/ccl/backupccl/restore_job.go
+++ pkg/ccl/backupccl/restore_job.go
@@ -666,11 +666,6 @@ func restore(
 		func(progressedCtx context.Context, details jobspb.ProgressDetails) {
 			switch d := details.(type) {
 			case *jobspb.Progress_Restore:
-				if !dataToRestore.isMainBundle() {
-					// We only update the progress for the primary data bundle (of which there
-					// can only be one).
-					return
-				}
 				mu.Lock()
 				if mu.highWaterMark >= 0 {
 					d.Restore.HighWater = importSpans[mu.highWaterMark].Span.Key
@@ -707,6 +702,12 @@ func restore(
 	jobProgressLoop := func(ctx context.Context) error {
 		ctx, progressSpan := tracing.ChildSpan(ctx, "progress-log")
 		defer progressSpan.Finish()
+		if !dataToRestore.isMainBundle() {
+			// We only update the progress for the primary data bundle (of which there
+			// can only be one).
+			return nil
+		}
+
 		return progressLogger.Loop(ctx, requestFinishedCh)
 	}
 
