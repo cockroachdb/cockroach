cockroachdb/cockroach

commit 200d1db58e5f6b47ad0b8637a0eec45e31851dd0

colbuilder: fix casting behavior for actual types mismatch
We have recently merged a change that enforces that the colbuilder
produces an operator chain that outputs batches with the desired type
schema. This is enforced by planning casts when there is a mismatch.
Previously, we would only try planning a vectorized cast because the
assumption was that only integers of different widths will need to be
cast in some cases, but as it turns out types of string family also
might need to be cast (e.g. `string` and `"char"` aren't identical).
This is now fixed by falling back to row-execution casting when
a vectorized cast isn't supported.

Release note: None

pkg/sql/colexec/colbuilder/execplan.go
@@ -1104,7 +1104,7 @@ func NewColOperator(
 			result.resetToState(ctx, resultPreSpecPlanningStateShallowCopy)
 			err = result.createAndWrapRowSource(ctx, flowCtx, args, inputs, inputTypes, spec, factory, err)
 		} else {
-			err = result.wrapPostProcessSpec(ctx, flowCtx, args, post, factory, err)
+			err = result.wrapPostProcessSpec(ctx, flowCtx, args, post, args.Spec.ResultTypes, factory, err)
 		}
 	} else {
 		// The result can be updated with the post process result.
@@ -1123,14 +1123,31 @@ func NewColOperator(
 	for i := range args.Spec.ResultTypes {
 		expected, actual := args.Spec.ResultTypes[i], r.ColumnTypes[i]
 		if !actual.Identical(expected) {
+			input := r.Op
+			castedIdx := len(r.ColumnTypes)
+			resultTypes := appendOneType(r.ColumnTypes, expected)
 			r.Op, err = colexec.GetCastOperator(
-				streamingAllocator, r.Op, i, len(r.ColumnTypes), actual, expected,
+				streamingAllocator, input, i, castedIdx, actual, expected,
 			)
 			if err != nil {
-				return r, err
+				// We don't support a native vectorized cast between these
+				// types, so we will plan a noop row-execution processor to
+				// handle it with a post-processing spec that simply passes
+				// through all of the columns from the input and appends the
+				// result of the cast to the end of the schema.
+				post := &execinfrapb.PostProcessSpec{}
+				post.RenderExprs = make([]execinfrapb.Expression, castedIdx+1)
+				for j := 0; j < castedIdx; j++ {
+					post.RenderExprs[j].Expr = fmt.Sprintf("@%d", j+1)
+				}
+				post.RenderExprs[castedIdx].Expr = fmt.Sprintf("@%d::%s", i+1, expected.SQLStandardName())
+				result.Op = input
+				if err = result.wrapPostProcessSpec(ctx, flowCtx, args, post, resultTypes, factory, err); err != nil {
+					return r, err
+				}
 			}
-			projection[i] = uint32(len(r.ColumnTypes))
-			r.ColumnTypes = appendOneType(r.ColumnTypes, expected)
+			r.ColumnTypes = resultTypes
+			projection[i] = uint32(castedIdx)
 		} else {
 			projection[i] = uint32(i)
 		}
@@ -1172,7 +1189,7 @@ func (r opResult) planAndMaybeWrapOnExprAsFilter(
 		}
 
 		onExprAsFilter := &execinfrapb.PostProcessSpec{Filter: onExpr}
-		return r.wrapPostProcessSpec(ctx, flowCtx, args, onExprAsFilter, factory, err)
+		return r.wrapPostProcessSpec(ctx, flowCtx, args, onExprAsFilter, args.Spec.ResultTypes, factory, err)
 	}
 	r.updateWithPostProcessResult(ppr)
 	return nil
@@ -1188,6 +1205,7 @@ func (r opResult) wrapPostProcessSpec(
 	flowCtx *execinfra.FlowCtx,
 	args *colexec.NewColOperatorArgs,
 	post *execinfrapb.PostProcessSpec,
+	resultTypes []*types.T,
 	factory coldata.ColumnFactory,
 	causeToWrap error,
 ) error {
@@ -1196,7 +1214,7 @@ func (r opResult) wrapPostProcessSpec(
 			Noop: &execinfrapb.NoopCoreSpec{},
 		},
 		Post:        *post,
-		ResultTypes: args.Spec.ResultTypes,
+		ResultTypes: resultTypes,
 	}
 	return r.createAndWrapRowSource(
 		ctx, flowCtx, args, []colexecbase.Operator{r.Op}, [][]*types.T{r.ColumnTypes},

pkg/sql/logictest/testdata/logic_test/vectorize_types
@@ -113,3 +113,8 @@ CREATE TABLE t47131_0(c0 INT2 UNIQUE); INSERT INTO t47131_0 VALUES(1)
 query I
 SELECT * FROM t47131_0 WHERE (t47131_0.c0 + t47131_0.c0::INT4) = 0
 ----
+
+query T
+SELECT 'a'::"char" FROM t47131_0
+----
+a

commit 34d9b646a88a9e3f0aac7a50cf95557dd6766a10

sql: add logic test for previously fixed casting bug
This commit adds a regression test for a correctness bug introduced in
`200d1db`. The bug was inadvertently fixed in `3a31d88`.

Fixes #66067

Release note: None

pkg/sql/logictest/testdata/logic_test/cast
@@ -60,3 +60,25 @@ SELECT _int8::INT4 FROM t64429
 
 statement error integer out of range for type int2
 SELECT _int4::INT2 FROM t64429
+
+# Regression test for #66067. Ensure that there is no correctness bug due to
+# improper casting of CHAR and VARCHAR.
+statement ok
+CREATE TABLE t66067_a (
+  a INT,
+  c CHAR(26),
+  CONSTRAINT c UNIQUE (c)
+);
+CREATE TABLE t66067_b (
+  a INT,
+  v VARCHAR(40)
+);
+INSERT INTO t66067_a VALUES (1, 'foo');
+INSERT INTO t66067_b VALUES (1, 'bar');
+
+query ITIT
+SELECT * FROM t66067_b b
+INNER LOOKUP JOIN t66067_a a ON b.a = a.a
+WHERE b.v = 'bar' AND a.c = 'foo'
+----
+1  bar  1  foo