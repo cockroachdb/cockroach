bab7d79c3c622f11d6a58963e27ab6625ecc38fb
Raphael 'kena' Poss <knz@thaumogen.net>
2021-06-04 20:43:30 +0200

    util/log: make `logpb.Entry` slightly more able to represent crdb-v2 entries
    Prior to this change, we did not have a public data structure able to
    represent the various fields available in a `crdb-v2` log entry: the
    existing `logpb.Entry` was not able to distinguish structured and
    non-structured entries, and did not have the ability to delimit the
    message and the stack trace.
    
    This patch extends `logpb.Entry` to make it more able to represent
    `crdb-v2` entries, at least for the purpose of extending `debug
    merge-log` towards conversion between logging formats.
    
    Additionally, the patch adds a *best effort* attempt at populating
    the new fields in the `crdb-v1` entry parser. This is best effort
    because the crdb-v1 parser is lossy so we cannot faithfully
    parse its entries reliably.
    
    Release note: None

--- pkg/util/log/format_crdb_v1.go
+++ pkg/util/log/format_crdb_v1.go
@@ -471,6 +471,27 @@ func (d *EntryDecoder) Decode(entry *logpb.Entry) error {
 		r = d.sensitiveEditor(r)
 		entry.Message = string(r.msg)
 		entry.Redactable = r.redactable
+
+		if strings.HasPrefix(entry.Message, structuredEntryPrefix+"{") || /* crdb-v1 prefix */
+			strings.HasPrefix(entry.Message, " ={") /* crdb-v2 prefix, happens when using the v1 parser against a v2 file */ {
+			if entry.Message[0] == ' ' {
+				entry.StructuredStart = 2
+			} else {
+				entry.StructuredStart = uint32(len(structuredEntryPrefix))
+			}
+			if nl := strings.IndexByte(entry.Message, '\n'); nl != -1 {
+				entry.StructuredEnd = uint32(nl)
+				entry.StackTraceStart = uint32(nl + 1)
+			} else {
+				entry.StructuredEnd = uint32(len(entry.Message))
+			}
+		}
+		// Note: we only know how to populate entry.StackTraceStart upon
+		// parse if the entry was structured (see above). If it is not
+		// structured, we cannot distinguish where the message ends and
+		// where the stack trace starts. This is another reason why the
+		// crdb-v1 format is lossy.
+
 		return nil
 	}
 }
