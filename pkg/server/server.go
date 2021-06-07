2451d7c2564a0be86f669989eb1aa6a773fbab47
Oleg Afanasyev <oleg@cockroachlabs.com>
2021-06-07 10:14:12 +0100

    server: return FailedPrecondition when talking to decom node
    Previously, when server detected that outgoing call would go to a
    decommissioned node it would shortcut with codes.PermissionDenied
    to skip attempt. That would result in retry loop immediately aborting
    without trying another replica or refreshing leaseholder. One of the
    consequences of that is that writes to system.eventlog will lose
    events if trying to reach decommissioned node.
    To address that, server will return codes.FailedPrecondition which
    will result in this replica being evicted and call retried.
    
    Release note: None

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -283,7 +283,7 @@ func NewServer(cfg Config, stopper *stop.Stopper) (*Server, error) {
 	stopper.AddCloser(&engines)
 
 	nodeTombStorage := &nodeTombstoneStorage{engs: engines}
-	checkPingFor := func(ctx context.Context, nodeID roachpb.NodeID) error {
+	checkPingFor := func(ctx context.Context, nodeID roachpb.NodeID, errorCode codes.Code) error {
 		ts, err := nodeTombStorage.IsDecommissioned(ctx, nodeID)
 		if err != nil {
 			// An error here means something very basic is not working. Better to terminate
@@ -292,7 +292,7 @@ func NewServer(cfg Config, stopper *stop.Stopper) (*Server, error) {
 		}
 		if !ts.IsZero() {
 			// The node was decommissioned.
-			return grpcstatus.Errorf(codes.PermissionDenied,
+			return grpcstatus.Errorf(errorCode,
 				"n%d was permanently removed from the cluster at %s; it is not allowed to rejoin the cluster",
 				nodeID, ts,
 			)
@@ -309,10 +309,16 @@ func NewServer(cfg Config, stopper *stop.Stopper) (*Server, error) {
 		Stopper:    stopper,
 		Settings:   cfg.Settings,
 		OnOutgoingPing: func(req *rpc.PingRequest) error {
-			return checkPingFor(ctx, req.TargetNodeID)
+			// Outgoing ping will block requests with codes.FailedPrecondition to
+			// notify caller that this replica is decommissioned but others could
+			// still be tried as caller node is valid, but not the destination.
+			return checkPingFor(ctx, req.TargetNodeID, codes.FailedPrecondition)
 		},
 		OnIncomingPing: func(req *rpc.PingRequest) error {
-			return checkPingFor(ctx, req.OriginNodeID)
+			// Incoming ping will reject requests with codes.PermissionDenied to
+			// signal remote node that it is not considered valid anymore and
+			// operations should fail immediately.
+			return checkPingFor(ctx, req.OriginNodeID, codes.PermissionDenied)
 		}}
 	if knobs := cfg.TestingKnobs.Server; knobs != nil {
 		serverKnobs := knobs.(*TestingKnobs)
