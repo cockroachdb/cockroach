bc99e502db2a3fd005acc00f4599b37700742a28
Peyton Walters <peyton.walters@cockroachlabs.com>
2021-06-07 14:17:46 -0400

    sql: add ddl_analysis tests for multi-region
    Previously, ddl_analysis only tested round trips for single-node setups.
    This is inadequate with the addition of multi-region tables and
    databases.
    This patch adds CCL tests for multi-region DDL statements.
    
    Release note: None

--- pkg/bench/ddl_analysis/grant_revoke_bench_test.go
+++ pkg/bench/ddl_analysis/grant_revoke_bench_test.go
@@ -15,28 +15,28 @@ import "testing"
 func BenchmarkGrant(b *testing.B) {
 	tests := []RoundTripBenchTestCase{
 		{
-			name: "grant all on 1 table",
-			setup: `CREATE USER TEST; 
+			Name: "grant all on 1 table",
+			Setup: `CREATE USER TEST; 
 CREATE TABLE t0();`,
-			stmt:  "GRANT ALL ON * TO TEST",
-			reset: "DROP ROLE TEST",
+			Stmt:  "GRANT ALL ON * TO TEST",
+			Reset: "DROP ROLE TEST",
 		},
 		{
-			name: "grant all on 2 tables",
-			setup: `CREATE USER TEST; 
+			Name: "grant all on 2 tables",
+			Setup: `CREATE USER TEST; 
 CREATE TABLE t0();
 CREATE TABLE t1();`,
-			stmt:  "GRANT ALL ON * TO TEST",
-			reset: "DROP ROLE TEST",
+			Stmt:  "GRANT ALL ON * TO TEST",
+			Reset: "DROP ROLE TEST",
 		},
 		{
-			name: "grant all on 3 tables",
-			setup: `CREATE USER TEST; 
+			Name: "grant all on 3 tables",
+			Setup: `CREATE USER TEST; 
 CREATE TABLE t0();
 CREATE TABLE t1();
 CREATE TABLE t2();`,
-			stmt:  "GRANT ALL ON * TO TEST",
-			reset: "DROP ROLE TEST",
+			Stmt:  "GRANT ALL ON * TO TEST",
+			Reset: "DROP ROLE TEST",
 		},
 	}
 
@@ -46,31 +46,31 @@ CREATE TABLE t2();`,
 func BenchmarkRevoke(b *testing.B) {
 	tests := []RoundTripBenchTestCase{
 		{
-			name: "revoke all on 1 table",
-			setup: `CREATE USER TEST; 
+			Name: "revoke all on 1 table",
+			Setup: `CREATE USER TEST; 
 CREATE TABLE t0();
 GRANT ALL ON * TO TEST;`,
-			stmt:  "REVOKE ALL ON * FROM TEST",
-			reset: "DROP ROLE TEST",
+			Stmt:  "REVOKE ALL ON * FROM TEST",
+			Reset: "DROP ROLE TEST",
 		},
 		{
-			name: "revoke all on 2 tables",
-			setup: `CREATE USER TEST; 
+			Name: "revoke all on 2 tables",
+			Setup: `CREATE USER TEST; 
 CREATE TABLE t0();
 CREATE TABLE t1();
 GRANT ALL ON * TO TEST;`,
-			stmt:  "REVOKE ALL ON * FROM TEST",
-			reset: "DROP ROLE TEST",
+			Stmt:  "REVOKE ALL ON * FROM TEST",
+			Reset: "DROP ROLE TEST",
 		},
 		{
-			name: "revoke all on 3 tables",
-			setup: `CREATE USER TEST; 
+			Name: "revoke all on 3 tables",
+			Setup: `CREATE USER TEST; 
 CREATE TABLE t0();
 CREATE TABLE t1();
 CREATE TABLE t2();
 GRANT ALL ON * TO TEST;`,
-			stmt:  "REVOKE ALL ON * FROM TEST",
-			reset: "DROP ROLE TEST",
+			Stmt:  "REVOKE ALL ON * FROM TEST",
+			Reset: "DROP ROLE TEST",
 		},
 	}
 
