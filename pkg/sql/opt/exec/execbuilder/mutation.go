a50b9a427f3e20ec07264f8412477899d03dfe2d
Marcus Gartner <marcus@cockroachlabs.com>
2021-06-08 19:02:19 -0700

    execbuilder: fix column mapping for insert fast path FK violations
    An insert fast path is built in `execbuilder` when FK checks are
    performed with WithScan and a simple LookupJoin. It is possible that the
    LookupJoin is planned on a unique index with columns in a different
    order than the FK columns. When an FK violation is detected during
    execution, the LookupJoin key values must be shuffled to match the FK
    column ordering to produce the correct error message.
    
    Previously, this shuffling would produce an internal error when the
    mutation's input contained duplicate column IDs. The shuffling relied on
    the Insert and WithScan column mappings. A duplicate column ID in these
    mappings created ambiguity which caused the shuffle to fail.
    
    For example, consider:
    
        CREATE TABLE p (x INT PRIMARY KEY)
        CREATE TABLE c (a INT, b INT REFERENCES p(x))
        INSERT INTO c (a, b) SELECT 1, 1
    
        FK Cols:              (b)
        Insert Mapping:       (i, i) => (a, b)
        WithScan Mapping:     (i) => (x)
        Lookup Join Key Cols: (x)
    
    By considering just these mappings, it is impossible to determine if the
    LookupJoin key column `x` corresponds to `a` or `b`, because `i`
    corresponds to both `a` and `b` in the insert mappings.
    
    As another example, consider:
    
        CREATE TABLE p (x INT, y INT, PRIMARY KEY (x, y))
        CREATE TABLE c (a INT, b INT, FOREIGN KEY (b, a) REFERENCES p(x, y))
        INSERT INTO c (a, b) SELECT 1, 1
    
        FK Cols:              (b, a)
        Insert Mapping:       (i, i) => (a, b)
        WithScan Mapping:     (i, i) => (x, y)
        Lookup Join Key Cols: (x, y)
    
    With these mappings, we cannot determine whether `(a, b)` corresponds to
    `(x, y)` or `(b, a)` corresponds to `(x, y)`.
    
    This issue is fixed by using the FK and unique index column ordinals to
    perform the shuffling, rather than the Insert and WithScan column
    mappings. This more detailed information eliminates any ambiguity and
    prevents the shuffling from failing.
    
    Fixes #65890
    
    Release note (bug fix): Previously, an INSERT causing a foreign key
    violation could result in an internal error in rare cases. The bug only
    affected the error response; any affected INSERTs, which must have been
    FK violations, did not succeeded. This bug, which was present since
    version 21.1.0, has been fixed.

--- pkg/sql/opt/exec/execbuilder/mutation.go
+++ pkg/sql/opt/exec/execbuilder/mutation.go
@@ -222,10 +222,11 @@ func (b *Builder) tryBuildFastPathInsert(ins *memo.InsertExpr) (_ execPlan, ok b
 			// the FK reference and the index we're looking up. We have to reshuffle
 			// the values to fix that.
 			fkVals := make(tree.Datums, len(values))
-			for i, ordinal := range out.InsertCols {
-				for j := range out.InsertCols {
-					if fk.OriginColumnOrdinal(tab, j) == int(ordinal) {
-						fkVals[j] = values[i]
+			for i := range fkVals {
+				parentOrd := fk.ReferencedColumnOrdinal(out.ReferencedTable, i)
+				for j := 0; j < out.ReferencedIndex.KeyColumnCount(); j++ {
+					if out.ReferencedIndex.Column(j).Ordinal() == parentOrd {
+						fkVals[i] = values[j]
 						break
 					}
 				}
