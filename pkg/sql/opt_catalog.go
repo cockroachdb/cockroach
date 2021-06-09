1c47bdbfc90c473e08f0513eaa485039b682034b
Marcus Gartner <marcus@cockroachlabs.com>
2021-06-09 15:34:52 -0700

    opt: only explore zig zag joins with two or more fixed columns
    `GenerateZigzagJoins` requires at least two fixed columns to generate a
    zigzag join. This commit adds a check early in the function to ensure
    there are enough fixed columns. This prevents doing unnecessary work in
    some cases.
    
    The end-to-end benchmarks highlight the improvement for a simple query:
    
        name                                          old time/op  new time/op  delta
        EndToEnd/kv-read/Simple                        226µs ± 2%   226µs ± 4%    ~     (p=0.841 n=5+5)
        EndToEnd/kv-read/Prepared                      139µs ± 2%   140µs ± 2%    ~     (p=1.000 n=5+5)
        EndToEnd/kv-read-const/Simple                  163µs ± 1%   162µs ± 2%    ~     (p=0.310 n=5+5)
        EndToEnd/kv-read-const/Prepared                139µs ± 2%   140µs ± 3%    ~     (p=0.548 n=5+5)
        EndToEnd/tpcc-new-order/Simple                 274µs ± 6%   273µs ± 5%    ~     (p=1.000 n=5+5)
        EndToEnd/tpcc-new-order/Prepared               169µs ± 5%   164µs ± 1%    ~     (p=0.056 n=5+5)
        EndToEnd/tpcc-delivery/Simple                  343µs ±12%   326µs ± 4%    ~     (p=0.095 n=5+5)
        EndToEnd/tpcc-delivery/Prepared                217µs ± 3%   216µs ± 1%    ~     (p=0.841 n=5+5)
        EndToEnd/tpcc-stock-level/Simple               670µs ± 3%   674µs ± 3%    ~     (p=0.095 n=5+5)
        EndToEnd/tpcc-stock-level/Prepared             577µs ±23%   573µs ±19%    ~     (p=1.000 n=5+5)
        EndToEnd/many-columns-and-indexes-a/Simple    1.40ms ± 2%  1.30ms ± 0%  -6.87%  (p=0.016 n=5+4)
        EndToEnd/many-columns-and-indexes-a/Prepared  1.30ms ± 1%  1.23ms ± 1%  -5.43%  (p=0.008 n=5+5)
        EndToEnd/many-columns-and-indexes-b/Simple    1.42ms ± 1%  1.42ms ± 3%    ~     (p=1.000 n=5+5)
        EndToEnd/many-columns-and-indexes-b/Prepared  1.33ms ± 5%  1.33ms ± 3%    ~     (p=0.841 n=5+5)
    
    Informs #66226
    
    Release note (performance improvement): The optimizer is now more
    efficient when planning simple queries on tables that have many columns
    and indexes.

--- pkg/sql/opt_catalog.go
+++ pkg/sql/opt_catalog.go
@@ -1316,7 +1316,7 @@ func (oi *optIndex) NonInvertedPrefixColumnCount() int {
 func (oi *optIndex) Column(i int) cat.IndexColumn {
 	ord := oi.columnOrds[i]
 	// Only key columns have a direction.
-	descending := i < oi.idx.NumKeyColumns() && oi.idx.GetKeyColumnDirection(i) == descpb.IndexDescriptor_DESC
+	descending := i < len(oi.desc.ColumnIDs) && oi.desc.ColumnDirections[i] == descpb.IndexDescriptor_DESC
 	return cat.IndexColumn{
 		Column:     oi.tab.Column(ord),
 		Descending: descending,
