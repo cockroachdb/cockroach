7a68bbdf5b4424379e83288a87af93e5b72d7174
Ricky Stewart <ricky@cockroachlabs.com>
2021-06-11 16:14:53 -0500

    bazel: get cross-compiler for windows working
    * Add some platform definitions for the Windows cross-compile build.
    * Fix some issues that were breaking the Windows build (bad targets in
      `BUILD.bazel`, mainly)
    * Refactor `.bazelrc`. Now we have `--config` options for the various
      configurations (today, that's `ci`, `crosslinux`, and `crosswindows`).
    
    Part of #66209. This isn't being exercised in CI yet.
    
    Release note: None

--- build/bazelutil/bazelbuild.sh
+++ build/bazelutil/bazelbuild.sh
@@ -2,5 +2,12 @@
 
 set -xeuo pipefail
 
-bazel build //pkg/cmd/bazci
-$(bazel info bazel-bin)/pkg/cmd/bazci/bazci_/bazci build //pkg/cmd/cockroach-short
+# TODO(ricky): switching configurations to build `bazci` for the host before
+# building the actual `cockroach` binary introduces some delays. It doesn't
+# cause bazel to throw away its entire cache or anything, but it definitely
+# rebuilds more than is technically necessary. This merits more investigation.
+# TODO(ricky): I think it might be necessary to teach `bazci` about the
+# configurations instead of asking `bazci` to just pass the configuration flags
+# down to Bazel unchanged, but for now it seems fine.
+bazel build //pkg/cmd/bazci --config=ci
+$(bazel info bazel-bin)/pkg/cmd/bazci/bazci_/bazci build //pkg/cmd/cockroach-short -- --config=ci --config=crosslinux
