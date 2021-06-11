eab6a9474376cb6466afc64ae9d70fd57537e550
Andrew Werner <awerner32@gmail.com>
2021-06-10 23:36:56 -0400

    pkg/sql/catalog: rename descriptortree to nstree
    This ends up being a better name for where this is going.
    
    Release note: None

--- pkg/sql/catalog/nstree/tree.go
+++ pkg/sql/catalog/nstree/tree.go
@@ -8,9 +8,9 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-// Package descriptortree provides a data structure for storing and retrieving
-// descriptors.
-package descriptortree
+// Package nstree provides a data structure for storing and retrieving
+// descriptors namespace entry-like data.
+package nstree
 
 import (
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
