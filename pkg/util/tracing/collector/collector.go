daeafa7c8f08446fef740f2bafa2370e534d38e2
Aditya Maru <adityamaru@gmail.com>
2021-06-11 12:22:06 -0400

    tracing: switch trace collector to an iterator pattern
    Previously, the collector would return span recordings from all
    nodes in the cluster.
    
    This change switches the trace collector to an iterator pattern.
    The trace collector can now be used in the following way:
    
    ```
    var iter *collector.TraceCollector
    for iter := tcollector.Start(); iter.Valid(); iter.Next() {
       recSpan = iter.Value() // tracingpb.RecordedSpan
    }
    if err := iter.Error(); err != nil {
      return err
    }
    ```
    
    The underlying iterator buffers the recorded spans of a single
    node at a time, and serves Value() calls from that buffer until
    it needs to poll another node in the cluster for its recorded spans.
    
    Release note: None

--- pkg/util/tracing/collector/collector.go
+++ pkg/util/tracing/collector/collector.go
@@ -18,10 +18,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/rpc"
 	"github.com/cockroachdb/cockroach/pkg/rpc/nodedialer"
-	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
-	"github.com/cockroachdb/cockroach/pkg/util/quotapool"
-	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/cockroach/pkg/util/tracing"
 	"github.com/cockroachdb/cockroach/pkg/util/tracing/tracingpb"
 	"github.com/cockroachdb/cockroach/pkg/util/tracing/tracingservicepb"
@@ -53,59 +50,125 @@ func New(
 	}
 }
 
-// GetSpanRecordingsFromCluster returns the inflight span recordings from all
-// nodes in the cluster.
+// TraceCollectorIterator can be used to return RecordedSpans from all live
+// nodes in the cluster, in a streaming manner. The iterator buffers the
+// RecordedSpans of one node at a time.
+type TraceCollectorIterator struct {
+	collector *TraceCollector
+
+	ctx context.Context
+
+	traceID uint64
+
+	// liveNodes represents all the nodes in the cluster that are considered live,
+	// and will be contacted for inflight trace spans by the iterator.
+	liveNodes []roachpb.NodeID
+
+	// curNodeIndex maintains the node from which the iterator has pulled inflight
+	// span recordings and buffered them in `recordedSpans` for consumption via
+	// the iterator.
+	curNodeIndex int
+
+	// recordedSpanIndex maintains the current position of of the iterator in the
+	// list of recorded spans. The recorded spans that the iterator points to are
+	// buffered in `recordedSpans`.
+	recordedSpanIndex int
+
+	// recordedSpans represents all recorded spans for a given node currently
+	// accessed by the iterator.
+	recordedSpans []tracingpb.RecordedSpan
+
+	iterErr error
+}
+
+// Start fetches the live nodes in the cluster, and configures the underlying
+// iterator that is used to access recorded spans in a streaming fashion.
+func (t *TraceCollector) Start(ctx context.Context, traceID uint64) *TraceCollectorIterator {
+	tc := &TraceCollectorIterator{ctx: ctx, traceID: traceID, collector: t, curNodeIndex: -1}
+	tc.liveNodes, tc.iterErr = nodesFromNodeLiveness(ctx, t.nodeliveness)
+	return tc
+}
+
+// Valid returns whether the iterator is in a valid state, and can return a
+// RecordedSpan.
+// If we have exhausted the buffered recordedSpans, the method will poll other
+// live nodes for inflight spans.
+func (i *TraceCollectorIterator) Valid() bool {
+	if i.iterErr != nil {
+		return false
+	}
+
+	// If recordedSpanIndex is within recordedSpans and there are some buffered
+	// recordedSpans, then we can return them when Next() is called.
+	if i.recordedSpans != nil && i.recordedSpanIndex < len(i.recordedSpans) {
+		return true
+	}
+
+	// Reset buffer variables.
+	i.recordedSpans = nil
+	i.recordedSpanIndex = 0
+
+	// Otherwise, either there are no more spans or we have exhausted the
+	// recordings from the current node, and we need to pull the inflight
+	// recordings from another node.
+	// Keep searching for recordings from all live nodes in the cluster.
+	for i.recordedSpans == nil {
+		i.curNodeIndex++
+		// No more spans to return from any of the live nodes in the cluster.
+		if !(i.curNodeIndex < len(i.liveNodes)) {
+			return false
+		}
+		i.recordedSpans, i.iterErr = i.collector.getTraceSpanRecordingsForNode(i.ctx, i.traceID,
+			i.liveNodes[i.curNodeIndex])
+		// TODO(adityamaru): We might want to consider not failing if a single node
+		// fails to return span recordings.
+		if i.iterErr != nil {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Next sets the iterator index to point to the next buffered RecordedSpan.
+func (i *TraceCollectorIterator) Next() {
+	i.recordedSpanIndex++
+}
+
+// Value returns the RecordedSpan pointed to by the iterator.
+func (i *TraceCollectorIterator) Value() tracingpb.RecordedSpan {
+	return i.recordedSpans[i.recordedSpanIndex]
+}
+
+// Error returns the error encountered by the iterator when fetching inflight
+// span recordings.
+func (i *TraceCollectorIterator) Error() error {
+	return i.iterErr
+}
+
+// getTraceSpanRecordingsForNode returns the inflight span recordings for traces
+// with traceID from the node with nodeID. The span recordings are sorted by
+// StartTime.
 // This method does not distinguish between requests for local and remote
 // inflight spans, and relies on gRPC short circuiting local requests.
-func (t *TraceCollector) GetSpanRecordingsFromCluster(
-	ctx context.Context, traceID uint64,
+func (t *TraceCollector) getTraceSpanRecordingsForNode(
+	ctx context.Context, traceID uint64, nodeID roachpb.NodeID,
 ) ([]tracingpb.RecordedSpan, error) {
-	var res []tracingpb.RecordedSpan
-	ns, err := nodesFromNodeLiveness(ctx, t.nodeliveness)
+	log.Infof(ctx, "executing GetSpanRecordings on nodes %s", nodeID.String())
+	conn, err := t.dialer.Dial(ctx, nodeID, rpc.DefaultClass)
 	if err != nil {
-		return res, err
+		return nil, err
+	}
+	traceClient := tracingservicepb.NewTracingClient(conn)
+	resp, err := traceClient.GetSpanRecordings(ctx,
+		&tracingservicepb.SpanRecordingRequest{TraceID: traceID})
+	if err != nil {
+		return nil, err
 	}
 
-	// Collect spans from all clients.
-	// We'll want to rate limit outgoing RPCs (limit pulled out of thin air).
-	var mu syncutil.Mutex
-	qp := quotapool.NewIntPool("every-node", 25)
-	log.Infof(ctx, "executing GetSpanRecordings on nodes %s", ns)
-	grp := ctxgroup.WithContext(ctx)
-
-	for _, node := range ns {
-		id := node.id // copy out of the loop variable
-		alloc, err := qp.Acquire(ctx, 1)
-		if err != nil {
-			return res, err
-		}
-
-		grp.GoCtx(func(ctx context.Context) error {
-			defer alloc.Release()
-
-			conn, err := t.dialer.Dial(ctx, id, rpc.DefaultClass)
-			if err != nil {
-				return err
-			}
-			traceClient := tracingservicepb.NewTracingClient(conn)
-			resp, err := traceClient.GetSpanRecordings(ctx,
-				&tracingservicepb.SpanRecordingRequest{TraceID: traceID})
-			if err != nil {
-				return err
-			}
-			mu.Lock()
-			res = append(res, resp.SpanRecordings...)
-			mu.Unlock()
-			return nil
-		})
-	}
-	if err := grp.Wait(); err != nil {
-		return res, err
-	}
-
-	sort.SliceStable(res, func(i, j int) bool {
-		return res[i].StartTime.Before(res[j].StartTime)
+	sort.SliceStable(resp.SpanRecordings, func(i, j int) bool {
+		return resp.SpanRecordings[i].StartTime.Before(resp.SpanRecordings[j].StartTime)
 	})
 
-	return res, nil
+	return resp.SpanRecordings, nil
 }
