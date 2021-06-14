82663f9e6ea99316433d39569deec7a3cd38dd97
Raphael 'kena' Poss <knz@thaumogen.net>
2021-06-14 15:56:05 +0200

    util/log,cli: new TELEMETRY channel
    See discussion in #63815: we would like to leverage the logging
    subsystem, in particular network logging, as a new way
    to channel diagnostic reports.
    
    This work will operate in multiple phases. This commit is only
    about the first phase: creating a new logging channel TELEMETRY.
    
    Release note (cli change): CockroachDB now supports a new logging
    channel called TELEMETRY. This will be used in later versions to
    report diagnostic events useful to Cockroach Labs for product
    analytics. By default, this channel is connected to file output, with
    a maximum retention of 1MiB. To redirect the diagnostic output, one
    can define a new sink that captures this channel.
    
    For example, to see diagnostics reports on the standard
    error, one can use: `--log='sinks: {stderr: {channels: TELEMETRY, filter: INFO}}'`
    
    (At the time of this writing, no events are defined for the TELEMETRY
    channel yet.)

--- pkg/util/log/eventpb/json_encode_generated.go
+++ pkg/util/log/eventpb/json_encode_generated.go
@@ -2071,6 +2071,14 @@ func (m *DropView) AppendJSONFields(printComma bool, b redact.RedactableBytes) (
 }
 
 // AppendJSONFields implements the EventPayload interface.
+func (m *Dummy) AppendJSONFields(printComma bool, b redact.RedactableBytes) (bool, redact.RedactableBytes) {
+
+	printComma, b = m.CommonEventDetails.AppendJSONFields(printComma, b)
+
+	return printComma, b
+}
+
+// AppendJSONFields implements the EventPayload interface.
 func (m *FinishSchemaChange) AppendJSONFields(printComma bool, b redact.RedactableBytes) (bool, redact.RedactableBytes) {
 
 	printComma, b = m.CommonEventDetails.AppendJSONFields(printComma, b)
