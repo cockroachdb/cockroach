eb1c76e2e415f918740d46c7b48f90f53877974c
richardjcai <caioftherichard@gmail.com>
2021-06-16 16:24:49 -0400

    Add SQL Grammar and tree Node for ALTER DEFAULT PRIVILEGES
    Grammar/tree commit only, no logic. Will populate release note
    in commit implementing logic.
    
    Release note: None

---
+++ pkg/sql/alter_default_privileges.go
@@ -0,0 +1,24 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package sql
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+)
+
+func (p *planner) alterDefaultPrivileges(
+	ctx context.Context, n *tree.AlterDefaultPrivileges,
+) (planNode, error) {
+	// TODO: implement this.
+	return nil, nil
+}
