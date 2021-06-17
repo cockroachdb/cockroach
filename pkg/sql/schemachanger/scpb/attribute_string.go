5333a8d5d4694a6dd1a129811e28be9444f30a6b
Andrew Werner <awerner32@gmail.com>
2021-06-16 21:47:20 -0400

    sql/schemachanger/scpb: rework attribute interfaces
    Did this need to be optimized? No. Nevetherless, I felt bad about how much
    we were going to allocate to get the nice abstraction.
    
    More importantly, this change more deeply hides the implementation of the
    Attribute concept and refocuses the functionality on comparing elements
    in various ways.
    
    `benchstat` if you're curious:
    
    ```
    name                old time/op    new time/op    delta
    CompareElements-16     588ns ± 1%      71ns ± 4%   -87.97%  (p=0.000 n=36+39)
    
    name                old alloc/op   new alloc/op   delta
    CompareElements-16      407B ± 0%        0B       -100.00%  (p=0.000 n=33+40)
    
    name                old allocs/op  new allocs/op  delta
    CompareElements-16      10.0 ± 0%       0.0       -100.00%  (p=0.000 n=40+40)
    ```
    
    Release note: None

--- pkg/sql/schemachanger/scpb/attribute_string.go
+++ pkg/sql/schemachanger/scpb/attribute_string.go
@@ -8,12 +8,12 @@ func _() {
 	// An "invalid array index" compiler error signifies that the constant values have changed.
 	// Re-run the stringer command to generate them again.
 	var x [1]struct{}
-	_ = x[AttributeType-1]
-	_ = x[AttributeDescID-2]
-	_ = x[AttributeDepID-3]
-	_ = x[AttributeColumnID-4]
-	_ = x[AttributeElementName-5]
-	_ = x[AttributeIndexID-6]
+	_ = x[AttributeType-0]
+	_ = x[AttributeDescID-1]
+	_ = x[AttributeDepID-2]
+	_ = x[AttributeColumnID-3]
+	_ = x[AttributeElementName-4]
+	_ = x[AttributeIndexID-5]
 }
 
 const _Attribute_name = "TypeDescIDDepIDColumnIDElementNameIndexID"
@@ -21,9 +21,8 @@ const _Attribute_name = "TypeDescIDDepIDColumnIDElementNameIndexID"
 var _Attribute_index = [...]uint8{0, 4, 10, 15, 23, 34, 41}
 
 func (i Attribute) String() string {
-	i -= 1
 	if i < 0 || i >= Attribute(len(_Attribute_index)-1) {
-		return "Attribute(" + strconv.FormatInt(int64(i+1), 10) + ")"
+		return "Attribute(" + strconv.FormatInt(int64(i), 10) + ")"
 	}
 	return _Attribute_name[_Attribute_index[i]:_Attribute_index[i+1]]
 }
--- pkg/sql/schemachanger/scpb/elements_test.go
+++ pkg/sql/schemachanger/scpb/elements_test.go
@@ -13,7 +13,33 @@ package scpb
 import (
 	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
+
+// TestElementAttributeValueTypesMatch ensure that for all elements which
+// have a given Attribute, that the values all have the same type.
+func TestElementAttributeValueTypesMatch(t *testing.T) {
+	typ := reflect.TypeOf((*ElementProto)(nil)).Elem()
+	attributeMap := make(map[Attribute]reflect.Type)
+	for i := 0; i < typ.NumField(); i++ {
+		f := typ.Field(i)
+		elem := reflect.New(f.Type.Elem()).Interface().(Element)
+		for i := 0; i < numAttributes; i++ {
+			attr := attributeOrder[i]
+			av := elem.getAttribute(attr)
+			if av == nil {
+				continue
+			}
+			avt := reflect.TypeOf(av)
+			if exp, ok := attributeMap[attr]; ok {
+				require.Equalf(t, exp, avt, "%v", attr)
+			} else {
+				attributeMap[attr] = avt
+			}
+		}
+	}
+}
 
 func TestGetElement(t *testing.T) {
 	typ := reflect.TypeOf((*ElementProto)(nil)).Elem()
