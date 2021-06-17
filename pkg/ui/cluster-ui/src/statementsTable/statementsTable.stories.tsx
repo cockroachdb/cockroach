0ea33c8f4a3b76068f80c1c02ed75e38200c87f9
Thomas Hardy <thomas.hardy@cockroachlabs.com>
2021-06-17 11:35:47 -0400

    ui: fixed transaction tool tips in db console
    Prior to this change, some of the tooltips in on the Transactions and
    Transactions Details pages (and a single tooltip on the Statements page) in DB
    console where mislabelled as statements (e.g. "Average time statements...") or
    were not explicitly clear (e.g. "Average time statements with this
    fingerprint...", not clear whether the tooltip is referring to a statement
    fingerprint or a transaction fingerprint). This change corrects these
    mislabelled tooltips.
    
    Resolves: #65286, #64973
    
    Release note (ui change): Fixed mislabelled tooltips on the Transactions and
    Transactions Details  pages in DB console.

--- pkg/ui/cluster-ui/src/statementsTable/statementsTable.stories.tsx
+++ pkg/ui/cluster-ui/src/statementsTable/statementsTable.stories.tsx
@@ -31,6 +31,7 @@ storiesOf("StatementsSortedTable", module)
         "(internal)",
         calculateTotalWorkload(statements),
         { "1": "gcp-europe-west1", "2": "gcp-us-east1", "3": "gcp-us-west1" },
+        "statement",
       )}
       sortSetting={{
         ascending: false,
