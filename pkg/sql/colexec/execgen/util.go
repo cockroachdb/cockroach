cbe754631b2f643e26f19f5c38ad64478d13c5d7
Jordan Lewis <jordanthelewis@gmail.com>
2021-06-19 00:00:17 -0300

    execgen: execgen:instantiate; runtime-to-template
    execgen:instantiate provides a way to inform execgen about the possible
    inputs to a template function, besides providing a concrete callsite.
    
    This allows us to solve the problem of how to convert a runtime value
    into a template value without having to write out a big switch/case
    statement by hand.
    
    For example, consider an integer template like this:
    
        // execgen:template<i>
        func b(i int) {
          switch i {
            case 1:
              fmt.Println("found a 1")
            case 2:
              fmt.Println("found a 2")
            case 3:
              fmt.Println("found a 3")
          }
        }
    
        func a() {
          b(1)
          b(2)
        }
    
    Previously, this template would be instantiated for all unique callsites
    of the template, in this case, 1 and 2.
    
    But what if we had a *runtime* value that we wanted to pass into the `b`
    function? How would we do that? Previously, we'd have to have another
    trivial case statement, to link up the dynamic value with an
    explicit call to the template:
    
        func a(input int) {
          if input == 1 {
            b(1)
          } else if input == 2 {
            b(2)
          }
        }
    
    Of course this code looks really silly, but without it, we can't
    specialize the `b` function properly, since we don't have the
    compile-time values 1 and 2 available to do so.
    
    This commit solves this problem by introducing a new directive, called
    execgen:instantiate, which provides a list of the possible arguments to
    a template function.
    
    With execgen:instantiate, our example can be rewritten like this:
    
        // execgen:instantiate<1>
        // execgen:instantiate<2>
        // execgen:instantiate<3>
        // execgen:template<i>
        func b(i int) {
          switch i {
            case 1:
              fmt.Println("found a 1")
            case 2:
              fmt.Println("found a 2")
            case 3:
              fmt.Println("found a 3")
          }
        }
    
        func a(input int) {
            b(input)
        }
    
    Given this input, execgen will generate variants for all of the
    instantiate annotations, and create a runtime-to-template switch
    function that allows us to simply call b(input), magically turning the
    runtime input value into an invocation of a compiled, specialized
    function:
    
        func b_runtime_to_template(i int) {
    	    switch i {
    	    case 1:
    		    b_1()
    	    case 2:
    		    b_2()
    	    case 3:
    		    b_3()
    	    default:
    		    panic(fmt.Sprintf("unknown value %s", i))
    	    }
        }
    
        func a(input int) {
    	    b_runtime_to_template(input)
        }
    
        func b_1() {
    	    fmt.Println("found a 1")
        }
    
        func b_2() {
    	    fmt.Println("found a 2")
        }
    
        func b_3() {
    	    fmt.Println("found a 3")
        }
    
    Release note: None

--- pkg/sql/colexec/execgen/util.go
+++ pkg/sql/colexec/execgen/util.go
@@ -11,6 +11,7 @@
 package execgen
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/dave/dst"
@@ -51,4 +52,32 @@ func prettyPrintExprs(exprs ...dst.Expr) string {
 		stmts[i] = &dst.ExprStmt{X: exprs[i]}
 	}
 	return prettyPrintStmts(stmts...)
+}
+
+func parseExpr(expr string) (dst.Expr, error) {
+	stmt, err := parseStmt(expr)
+	if err != nil {
+		return nil, err
+	}
+	return stmt.(*dst.ExprStmt).X, nil
+}
+
+func parseStmt(stmt string) (dst.Stmt, error) {
+	f, err := decorator.Parse(fmt.Sprintf(
+		`package main
+func test() {
+	%s
+}`, stmt))
+	if err != nil {
+		return nil, err
+	}
+	return f.Decls[0].(*dst.FuncDecl).Body.List[0], nil
+}
+
+func mustParseStmt(stmt string) dst.Stmt {
+	ret, err := parseStmt(stmt)
+	if err != nil {
+		panic(err)
+	}
+	return ret
 }
