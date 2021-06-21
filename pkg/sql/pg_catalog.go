edcff6b83f7f869227c98db44bd6e3230a8ea096
Marcus Gartner <marcus@cockroachlabs.com>
2021-06-21 10:00:06 -0700

    sql: add AccessibleColumns method to TableDescriptor interface
    The `TableDescriptor` interface now includes an `AccessibleColumns`
    method that returns all public column with `Inaccessible=false`. This
    method simplifies code that needs to access only accessible columns.
    
    Release note: None

--- pkg/sql/pg_catalog.go
+++ pkg/sql/pg_catalog.go
@@ -392,10 +392,7 @@ https://www.postgresql.org/docs/12/catalog-pg-attribute.html`,
 		}
 
 		// Columns for table.
-		for _, column := range table.PublicColumns() {
-			if column.IsInaccessible() {
-				continue
-			}
+		for _, column := range table.AccessibleColumns() {
 			tableID := tableOid(table.GetID())
 			if err := addColumn(column, tableID, column.GetPGAttributeNum()); err != nil {
 				return err
@@ -545,12 +542,6 @@ https://www.postgresql.org/docs/9.5/catalog-pg-class.html`,
 			relPersistence = relPersistenceTemporary
 		}
 		namespaceOid := h.NamespaceOid(db.GetID(), scName)
-		relnatts := 0
-		for _, column := range table.PublicColumns() {
-			if !column.IsInaccessible() {
-				relnatts++
-			}
-		}
 		if err := addRow(
 			tableOid(table.GetID()),        // oid
 			tree.NewDName(table.GetName()), // relname
@@ -566,12 +557,12 @@ https://www.postgresql.org/docs/9.5/catalog-pg-class.html`,
 			zeroVal,                        // relallvisible
 			oidZero,                        // reltoastrelid
 			tree.MakeDBool(tree.DBool(table.IsPhysicalTable())), // relhasindex
-			tree.DBoolFalse,                   // relisshared
-			relPersistence,                    // relPersistence
-			tree.DBoolFalse,                   // relistemp
-			relKind,                           // relkind
-			tree.NewDInt(tree.DInt(relnatts)), // relnatts
-			tree.NewDInt(tree.DInt(len(table.GetChecks()))), // relchecks
+			tree.DBoolFalse, // relisshared
+			relPersistence,  // relPersistence
+			tree.DBoolFalse, // relistemp
+			relKind,         // relkind
+			tree.NewDInt(tree.DInt(len(table.AccessibleColumns()))), // relnatts
+			tree.NewDInt(tree.DInt(len(table.GetChecks()))),         // relchecks
 			tree.DBoolFalse, // relhasoids
 			tree.MakeDBool(tree.DBool(table.IsPhysicalTable())), // relhaspkey
 			tree.DBoolFalse, // relhasrules
