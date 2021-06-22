3504e8bbf36bf7228bfe01ab5edab5d3f4ebe447
Steven Danna <danna@cockroachlabs.com>
2021-06-22 15:21:09 +0100

    ccl: add stub-schema-registry
    Occasionally it is nice to be able to start a "good enough" schema
    registry to manually test avro-formatted changefeeds.
    
    Release note: None

---
+++ pkg/ccl/cmdccl/stub-schema-registry/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/cockroachdb/cockroach/pkg/ccl/changefeedccl/cdctest"
+)
+
+func main() {
+	reg := cdctest.StartTestSchemaRegistry()
+	defer reg.Close()
+
+	fmt.Printf("Stub Schema Registry listening at %s\n", reg.URL())
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	sig := <-c
+	fmt.Printf("Shutting down on signal: %s\n", sig)
+}
