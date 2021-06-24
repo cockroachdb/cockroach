5f7d48b013f267e1ffc2e6a4407a9220d92c2f4c
Sajjad Rizvi <sajjad@cockroachlabs.com>
2021-06-24 16:20:16 -0400

    jobs: retry failed jobs with exponential-backoff
    Failed jobs were being retried with a constant interval in the previous
    implementation. This commit enables jobs to be retried with exponentially
    increasing delays with an upper bound. This change enables to retry the jobs
    that are not currently retried when they fail due to transient problems.
    
    Release note: None
    
    Fixes: #44594

--- pkg/jobs/update.go
+++ pkg/jobs/update.go
@@ -15,7 +15,9 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"time"
 
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
 	"github.com/cockroachdb/cockroach/pkg/kv"
 	"github.com/cockroachdb/cockroach/pkg/security"
@@ -36,12 +38,19 @@ import (
 // changes will be ignored unless JobUpdater is used).
 type UpdateFn func(txn *kv.Txn, md JobMetadata, ju *JobUpdater) error
 
+// RunStats consists of job-run statistics: num of runs and last-run timestamp.
+type RunStats struct {
+	LastRun time.Time
+	NumRuns int
+}
+
 // JobMetadata groups the job metadata values passed to UpdateFn.
 type JobMetadata struct {
 	ID       jobspb.JobID
 	Status   Status
 	Payload  *jobspb.Payload
 	Progress *jobspb.Progress
+	RunStats *RunStats
 }
 
 // CheckRunningOrReverting returns an InvalidStatusError if md.Status is not
@@ -78,6 +87,13 @@ func (ju *JobUpdater) UpdateProgress(progress *jobspb.Progress) {
 
 func (ju *JobUpdater) hasUpdates() bool {
 	return ju.md != JobMetadata{}
+}
+
+func (ju *JobUpdater) UpdateRunStats(numRuns int, lastRun time.Time) {
+	ju.md.RunStats = &RunStats{
+		NumRuns: numRuns,
+		LastRun: lastRun,
+	}
 }
 
 // UpdateHighwaterProgressed updates job updater progress with the new high water mark.
@@ -119,18 +135,27 @@ func (j *Job) Update(ctx context.Context, txn *kv.Txn, updateFn UpdateFn) error 
 	var payload *jobspb.Payload
 	var progress *jobspb.Progress
 	if err := j.runInTxn(ctx, txn, func(ctx context.Context, txn *kv.Txn) error {
-		stmt := "SELECT status, payload, progress FROM system.jobs WHERE id = $1 FOR UPDATE"
+		sessionIDClause := ""
 		if j.sessionID != "" {
-			stmt = "SELECT status, payload, progress, claim_session_id FROM system." +
-				"jobs WHERE id = $1 FOR UPDATE"
+			sessionIDClause = ", claim_session_id"
 		}
+		// Retrieve run stats if the jobs table version supports it.
+		statsClause := ""
+		if j.registry.settings.Version.IsActive(ctx, clusterversion.RetryJobsWithExponentialBackoff) {
+			statsClause = ", COALESCE(last_run, created), COALESCE(num_runs, 0)"
+		}
+		stmt := fmt.Sprintf("SELECT status, payload, progress%s%s FROM system.jobs WHERE id = $1 FOR UPDATE",
+			sessionIDClause, statsClause)
+
 		var err error
 		var row tree.Datums
-		row, err = j.registry.ex.QueryRowEx(
-			ctx, "log-job", txn, sessiondata.InternalExecutorOverride{User: security.RootUserName()},
+		if row, err = j.registry.ex.QueryRowEx(
+			ctx,
+			"log-job",
+			txn,
+			sessiondata.InternalExecutorOverride{User: security.RootUserName()},
 			stmt, j.ID(),
-		)
-		if err != nil {
+		); err != nil {
 			return err
 		}
 		if row == nil {
@@ -142,6 +167,7 @@ func (j *Job) Update(ctx context.Context, txn *kv.Txn, updateFn UpdateFn) error 
 			return errors.AssertionFailedf("job %d: expected string status, but got %T", j.ID(), statusString)
 		}
 
+		status := Status(*statusString)
 		if j.sessionID != "" {
 			if row[3] == tree.DNull {
 				return errors.Errorf(
@@ -155,8 +181,6 @@ func (j *Job) Update(ctx context.Context, txn *kv.Txn, updateFn UpdateFn) error 
 					j.ID(), statusString, j.sessionID, storedSession)
 			}
 		}
-
-		status := Status(*statusString)
 		if payload, err = UnmarshalPayload(row[1]); err != nil {
 			return err
 		}
@@ -170,6 +194,28 @@ func (j *Job) Update(ctx context.Context, txn *kv.Txn, updateFn UpdateFn) error 
 			Payload:  payload,
 			Progress: progress,
 		}
+
+		if j.registry.settings.Version.IsActive(ctx, clusterversion.RetryJobsWithExponentialBackoff) {
+			offset := 0
+			if j.sessionID != "" {
+				offset = 1
+			}
+			var lastRun *tree.DTimestamp
+			lastRun, ok = row[3+offset].(*tree.DTimestamp)
+			if !ok {
+				return errors.AssertionFailedf("job %d: expected timestamp last_run, but got %T", j.ID(), lastRun)
+			}
+			var numRuns *tree.DInt
+			numRuns, ok = row[4+offset].(*tree.DInt)
+			if !ok {
+				return errors.AssertionFailedf("job %d: expected int num_runs, but got %T", j.ID(), numRuns)
+			}
+			md.RunStats = &RunStats{
+				NumRuns: int(*numRuns),
+				LastRun: lastRun.Time,
+			}
+		}
+
 		var ju JobUpdater
 		if err := updateFn(txn, md, &ju); err != nil {
 			return err
@@ -179,6 +225,7 @@ func (j *Job) Update(ctx context.Context, txn *kv.Txn, updateFn UpdateFn) error 
 				return err
 			}
 		}
+
 		if !ju.hasUpdates() {
 			return nil
 		}
@@ -222,6 +269,11 @@ func (j *Job) Update(ctx context.Context, txn *kv.Txn, updateFn UpdateFn) error 
 				return err
 			}
 			addSetter("progress", progressBytes)
+		}
+
+		if ju.md.RunStats != nil {
+			addSetter("last_run", ju.md.RunStats.LastRun)
+			addSetter("num_runs", ju.md.RunStats.NumRuns)
 		}
 
 		updateStmt := fmt.Sprintf(
