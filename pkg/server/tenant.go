43439c1a2f45ac9f77b212c8731e7bf6df530cab
rimadeodhar <rima@cockroachlabs.com>
2021-06-25 14:06:21 -0700

    sqlinstance: Create provider API for sqlinstance
    In this commit, I create and implement the provider
    API to create and manage instance information
    corresponding to invidual SQL pods. The provider takes
    care of initializing a unique instance ID using the
    sql_instances system table. It also propagates SQL
    address and session information corresponding to a SQL
    pod using the sql_instances system table.
    This system is dependent on the sqlliveness subsystem.
    
    Release note: None

--- pkg/server/tenant.go
+++ pkg/server/tenant.go
@@ -59,20 +59,7 @@ func StartTenant(
 	if err != nil {
 		return nil, "", "", err
 	}
-	s, err := newSQLServer(ctx, args)
-	if err != nil {
-		return nil, "", "", err
-	}
-
-	s.execCfg.SQLStatusServer = newTenantStatusServer(
-		baseCfg.AmbientCtx, &adminPrivilegeChecker{ie: args.circularInternalExecutor},
-		args.sessionRegistry, args.contentionRegistry, args.flowScheduler, baseCfg.Settings, s,
-	)
-
-	// TODO(asubiotto): remove this. Right now it is needed to initialize the
-	// SpanResolver.
-	s.execCfg.DistSQLPlanner.SetNodeInfo(roachpb.NodeDescriptor{NodeID: 0})
-
+	args.ValidateAddrs(ctx)
 	connManager := netutil.MakeServer(
 		args.stopper,
 		// The SQL server only uses connManager.ServeWith. The both below
@@ -122,9 +109,22 @@ func StartTenant(
 			return nil, "", "", err
 		}
 	}
-
 	pgLAddr := pgL.Addr().String()
 	httpLAddr := httpL.Addr().String()
+	args.addr = pgLAddr
+	s, err := newSQLServer(ctx, args)
+	if err != nil {
+		return nil, "", "", err
+	}
+
+	s.execCfg.SQLStatusServer = newTenantStatusServer(
+		baseCfg.AmbientCtx, &adminPrivilegeChecker{ie: args.circularInternalExecutor},
+		args.sessionRegistry, args.contentionRegistry, args.flowScheduler, baseCfg.Settings, s,
+	)
+
+	// TODO(asubiotto): remove this. Right now it is needed to initialize the
+	// SpanResolver.
+	s.execCfg.DistSQLPlanner.SetNodeInfo(roachpb.NodeDescriptor{NodeID: 0})
 	args.recorder.AddNode(
 		args.registry,
 		roachpb.NodeDescriptor{},
@@ -169,8 +169,6 @@ func StartTenant(
 	}); err != nil {
 		return nil, "", "", err
 	}
-
-	s.execCfg.DistSQLPlanner.SetNodeInfo(roachpb.NodeDescriptor{NodeID: roachpb.NodeID(args.nodeIDContainer.SQLInstanceID())})
 
 	if err := s.preStart(ctx,
 		args.stopper,
@@ -320,9 +318,6 @@ func makeTenantSQLServerArgs(
 
 	recorder := status.NewMetricsRecorder(clock, nil, rpcContext, nil, st)
 
-	const sqlInstanceID = base.SQLInstanceID(10001)
-	idContainer := base.NewSQLIDContainer(sqlInstanceID, nil /* nodeID */)
-
 	runtime := status.NewRuntimeStatSampler(context.Background(), clock)
 	registry.AddMetricStruct(runtime)
 
@@ -358,9 +353,11 @@ func makeTenantSQLServerArgs(
 			isMeta1Leaseholder: func(_ context.Context, _ hlc.ClockTimestamp) (bool, error) {
 				return false, errors.New("isMeta1Leaseholder is not available to secondary tenants")
 			},
-			nodeIDContainer:        idContainer,
 			externalStorage:        externalStorage,
 			externalStorageFromURI: externalStorageFromURI,
+			// Set instance ID to 0 and node ID to nil to indicate
+			// that the instance ID will be bound later during preStart.
+			nodeIDContainer: base.NewSQLIDContainer(0, nil),
 		},
 		sqlServerOptionalTenantArgs: sqlServerOptionalTenantArgs{
 			tenantConnect: tenantConnect,
---
+++ pkg/sql/sqlinstance/instanceprovider/test_helpers.go
@@ -0,0 +1,34 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package instanceprovider
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlinstance"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlinstance/instancestorage"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlliveness"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
+)
+
+// NewTestInstanceProvider initializes a instanceprovider.provider
+// for test purposes
+func NewTestInstanceProvider(
+	stopper *stop.Stopper, session sqlliveness.Instance, addr string,
+) sqlinstance.Provider {
+	storage := instancestorage.NewFakeStorage()
+	p := &provider{
+		storage: storage,
+		stopper: stopper,
+	}
+	p.session = session
+	p.instanceAddr = addr
+	p.initialized = make(chan struct{})
+	return p
+}
