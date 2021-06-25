5cc5cb544309835341ac1c6be305bf7fbbc8dc5e
Ricky Stewart <ricky@cockroachlabs.com>
2021-06-25 18:50:28 -0500

    build: add script for build config to cross-compile to macos
    Also generalize some scripts that were unnecessarily repetitive.
    
    Closes #66210
    
    Release note: None

--- build/bazelutil/bazelbuild.sh
+++ build/bazelutil/bazelbuild.sh
@@ -2,8 +2,14 @@
 
 set -xeuo pipefail
 
+if [ -z "$1" ]
+then
+    echo 'Usage: bazelbuild.sh CONFIG'
+    exit 1
+fi
+
 bazel build //pkg/cmd/bazci --config=ci
 $(bazel info bazel-bin)/pkg/cmd/bazci/bazci_/bazci --compilation_mode opt \
 		       --config ci \
-		       --config crosslinux \
+		       --config "$1" \
 		       build //pkg/cmd/cockroach-short
