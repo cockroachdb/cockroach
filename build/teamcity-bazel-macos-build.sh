8333dd9623389efa25c518145dd14dcec0850296
Ricky Stewart <ricky@cockroachlabs.com>
2021-06-25 18:50:28 -0500

    build: add script for build config to cross-compile to macos
    Also generalize some scripts that were unnecessarily repetitive.
    
    Closes #66210
    
    Release note: None

--- build/teamcity-bazel-macos-build.sh
+++ build/teamcity-bazel-macos-build.sh
@@ -8,5 +8,5 @@ source "$(dirname "${0}")/teamcity-bazel-support.sh"  # For run_bazel
 tc_prepare
 
 tc_start_block "Run Bazel build"
-run_bazel build/bazelutil/bazelbuild.sh crosslinux
+run_bazel build/bazelutil/bazelbuild.sh crossmacos
 tc_end_block "Run Bazel build"
