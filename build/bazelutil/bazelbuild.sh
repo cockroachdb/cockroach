8ca63ee5c1f945b866d0a251e71b53c80a1ec981
Ricky Stewart <ricky@cockroachlabs.com>
2021-07-06 10:42:56 -0500

    bazel: correctly report target machine when stamping
    Add functionality in `stamp.sh` to allow specifying or inferring the
    build target from `cc -dumpmachine`.
    
    Closes #66853.
    
    Release note: None

--- build/bazelutil/bazelbuild.sh
+++ build/bazelutil/bazelbuild.sh
@@ -10,6 +10,5 @@ fi
 
 bazel build //pkg/cmd/bazci --config=ci
 $(bazel info bazel-bin)/pkg/cmd/bazci/bazci_/bazci --compilation_mode opt \
-		       --config ci \
 		       --config "$1" \
 		       build //pkg/cmd/cockroach-short
--- build/bazelutil/stamp.sh
+++ build/bazelutil/stamp.sh
@@ -19,17 +19,16 @@ GIT_COMMIT=$(git rev-parse HEAD)
 GIT_TAG=$(git describe --tags --dirty --match=v[0-9]* 2> /dev/null || git rev-parse --short HEAD;)
 GIT_UTCTIME=$(date -u '+%Y/%m/%d %H:%M:%S')
 
-# TODO(alanmas): So far HOST_TRIPLE is "hardcoded" but
-# we need to ensure it gets set correctly as we continue to port things to Bazel.
-# TODO(alanmas): As we don’t have a release pipeline set up for Bazel-built cockroach binaries yet
-# we are not taking care of:
+if [ -z "${1+x}" ]
+then
+    TARGET_TRIPLE=$(cc -dumpmachine)
+else
+    TARGET_TRIPLE="$1"
+fi
+
+# TODO(ricky): Also provide a way to stamp the following variables:
 # - github.com/cockroachdb/cockroach/pkg/build.channel
 # - github.com/cockroachdb/cockroach/pkg/util/log.crashReportEnv
-# we need to keep this on track to work on them as soon as we release our pipeline.
-
-HOST_TRIPLE="x86_64-pc-linux-gnu"
-
-TARGET_TRIPLE=${HOST_TRIPLE}
 
 # Prefix with STABLE_ so that these values are saved to stable-status.txt
 # instead of volatile-status.txt.
