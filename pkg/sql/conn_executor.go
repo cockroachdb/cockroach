70815a23ffc5ac16b94e4e124c2ed3361620c9c2
Azhng <archer.xn@gmail.com>
2021-07-08 14:11:00 -0400

    sql: replace the sqlStats provider in the sql.Server with a persisted version
    Previously, sql.Server use a node-local in-memory sqlStats provider to
    store SQL stats, which is cleared every hour.
    This commit replaces that provider with a persisted version that was introduced
    in the previous commit to enable all SQL stats to be persisted in the
    system tables.
    
    Release note (sql change): SQL stats now can be persisted into
    system.statement_statistics and system.transaction_statistics
    tables by enabling the sql.stats.flush.enable cluster setting.
    The interval of persistence is determined by the new
    sql.stats.flush.interval cluster setting which defaults to 1 hour.

--- pkg/sql/conn_executor.go
+++ pkg/sql/conn_executor.go
@@ -49,6 +49,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sessionphase"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlerrors"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlstats"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlstats/persistedsqlstats"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlstats/sslocal"
 	"github.com/cockroachdb/cockroach/pkg/sql/stmtdiagnostics"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
@@ -309,7 +310,7 @@ func NewServer(cfg *ExecutorConfig, pool *mon.BytesMonitor) *Server {
 		nil, /* resetInterval */
 		nil, /* reportedProvider */
 	)
-	sqlStats := sslocal.New(
+	memSQLStats := sslocal.New(
 		cfg.Settings,
 		sqlstats.MaxMemSQLStatsStmtFingerprints,
 		sqlstats.MaxMemSQLStatsTxnFingerprints,
@@ -319,15 +320,29 @@ func NewServer(cfg *ExecutorConfig, pool *mon.BytesMonitor) *Server {
 		sqlstats.SQLStatReset,
 		reportedSQLStats,
 	)
-	return &Server{
+	s := &Server{
 		cfg:             cfg,
 		Metrics:         metrics,
 		InternalMetrics: makeMetrics(cfg, true /* internal */),
 		pool:            pool,
-		sqlStats:        sqlStats,
 		reportedStats:   reportedSQLStats,
 		reCache:         tree.NewRegexpCache(512),
 	}
+
+	sqlStatsInternalExecutor := MakeInternalExecutor(context.Background(), s, MemoryMetrics{}, cfg.Settings)
+	persistedSQLStats := persistedsqlstats.New(&persistedsqlstats.Config{
+		Settings:         s.cfg.Settings,
+		InternalExecutor: &sqlStatsInternalExecutor,
+		KvDB:             cfg.DB,
+		SQLIDContainer:   cfg.NodeID,
+		Knobs:            cfg.SQLStatsTestingKnobs,
+		FlushCounter:     metrics.StatsMetrics.SQLStatsFlushStarted,
+		FailureCounter:   metrics.StatsMetrics.SQLStatsFlushFailure,
+		FlushDuration:    metrics.StatsMetrics.SQLStatsFlushDuration,
+	}, memSQLStats)
+
+	s.sqlStats = persistedSQLStats
+	return s
 }
 
 func makeMetrics(cfg *ExecutorConfig, internal bool) Metrics {
@@ -373,7 +388,15 @@ func makeMetrics(cfg *ExecutorConfig, internal bool) Metrics {
 			),
 			ReportedSQLStatsMemoryCurBytesCount: metric.NewGauge(
 				getMetricMeta(MetaReportedSQLStatsMemCurBytes, internal)),
-			DiscardedStatsCount: metric.NewCounter(getMetricMeta(MetaDiscardedSQLStats, internal)),
+			DiscardedStatsCount:  metric.NewCounter(getMetricMeta(MetaDiscardedSQLStats, internal)),
+			SQLStatsFlushStarted: metric.NewCounter(getMetricMeta(MetaSQLStatsFlushStarted, internal)),
+			SQLStatsFlushFailure: metric.NewCounter(getMetricMeta(MetaSQLStatsFlushFailure, internal)),
+			SQLStatsFlushDuration: metric.NewHistogram(
+				getMetricMeta(MetaSQLStatsFlushDuration, internal),
+				cfg.HistogramWindowInterval,
+				log10int64times1000,
+				3, /* sigFigs */
+			),
 		},
 	}
 }
@@ -387,6 +410,11 @@ func (s *Server) Start(ctx context.Context, stopper *stop.Stopper) {
 	// in case the telemetry worker fails.
 	s.sqlStats.Start(ctx, stopper)
 	s.reportedStats.Start(ctx, stopper)
+}
+
+// GetSQLStatsProvider returns the provider for the sqlstats subsystem.
+func (s *Server) GetSQLStatsProvider() sqlstats.Provider {
+	return s.sqlStats
 }
 
 // ResetSQLStats resets the executor's collected sql statistics.
