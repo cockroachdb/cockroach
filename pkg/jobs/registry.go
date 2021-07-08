9bc0995aed956c960c04f36b6934f33720d54be1
Aditya Maru <adityamaru@gmail.com>
2021-07-08 13:15:48 -0400

    server,jobs: add a TraceDumper to dump inflight trace zips
    This change does two things:
    
    1. It adds a TraceDumper that can be used to dump a trace zip
    for a particular trace id. The dumper uses the tracing.zipper
    to generate a zip of all inflight traces in the cluster.
    The zip is appropriately named (including a created at timestamp
    for the purpose of GC) and is stored in a subdirectory
    of the log directory.
    
    2. Introduces a cluster setting `jobs.dumptrace.mode` that allows
    users to configure Traceable jobs to dump their traces on failure, success
    or never.
    
    Release note: None

--- pkg/jobs/registry.go
+++ pkg/jobs/registry.go
@@ -25,6 +25,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/security"
 	"github.com/cockroachdb/cockroach/pkg/server/telemetry"
+	"github.com/cockroachdb/cockroach/pkg/server/tracedumper"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/builtins"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
@@ -97,6 +98,7 @@ type Registry struct {
 	settings *cluster.Settings
 	execCtx  jobExecCtxMaker
 	metrics  Metrics
+	td       *tracedumper.TraceDumper
 	knobs    TestingKnobs
 
 	// adoptionChan is used to nudge the registry to resume claimed jobs and
@@ -174,6 +176,7 @@ func MakeRegistry(
 	histogramWindowInterval time.Duration,
 	execCtxFn jobExecCtxMaker,
 	preventAdoptionFile string,
+	td *tracedumper.TraceDumper,
 	knobs *TestingKnobs,
 ) *Registry {
 	r := &Registry{
@@ -187,6 +190,7 @@ func MakeRegistry(
 		settings:            settings,
 		execCtx:             execCtxFn,
 		preventAdoptionFile: preventAdoptionFile,
+		td:                  td,
 		adoptionCh:          make(chan adoptionNotice),
 	}
 	if knobs != nil {
@@ -1194,36 +1198,4 @@ func (r *Registry) unregister(jobID jobspb.JobID) {
 		aj.cancel()
 		delete(r.mu.adoptedJobs, jobID)
 	}
-}
-
-// TestingNudgeAdoptionQueue is used by tests to tell the registry that there is
-// a job to be adopted.
-func (r *Registry) TestingNudgeAdoptionQueue() {
-	r.adoptionCh <- claimAndResumeClaimedJobs
-}
-
-// TestingCreateAndStartJob creates and asynchronously starts a job from record.
-// An error is returned if the job type has not been registered with
-// RegisterConstructor. The ctx passed to this function is not the context the
-// job will be started with (canceling ctx will not cause the job to cancel).
-func TestingCreateAndStartJob(
-	ctx context.Context, r *Registry, db *kv.DB, record Record,
-) (*StartableJob, error) {
-	var rj *StartableJob
-	jobID := r.MakeJobID()
-	if err := db.Txn(ctx, func(ctx context.Context, txn *kv.Txn) (err error) {
-		return r.CreateStartableJobWithTxn(ctx, &rj, jobID, txn, record)
-	}); err != nil {
-		if rj != nil {
-			if cleanupErr := rj.CleanupOnRollback(ctx); cleanupErr != nil {
-				log.Warningf(ctx, "failed to cleanup StartableJob: %v", cleanupErr)
-			}
-		}
-		return nil, err
-	}
-	err := rj.Start(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return rj, nil
 }
