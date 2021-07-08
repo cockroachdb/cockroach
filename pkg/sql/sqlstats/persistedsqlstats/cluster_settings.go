014b7550d8b16695c5ac0e2cf95258a1d82fd0e4
Azhng <archer.xn@gmail.com>
2021-07-08 14:10:30 -0400

    sql: implement persistedsqlstats flush logic
    This commit implements the initial flush logic of the
    persisted sql stats subsystem.
    
    Release note: None

---
+++ pkg/sql/sqlstats/persistedsqlstats/cluster_settings.go
@@ -0,0 +1,52 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package persistedsqlstats
+
+import (
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/settings"
+	"github.com/cockroachdb/errors"
+)
+
+// SQLStatsFlushInterval is the cluster setting that controls how often the SQL
+// stats are flushed to system table.
+var SQLStatsFlushInterval = settings.RegisterDurationSetting(
+	"sql.stats.flush.interval",
+	"the interval at which SQL execution statistics are flushed to disk",
+	time.Hour,
+	settings.NonNegativeDurationWithMaximum(time.Hour*24),
+).WithPublic()
+
+// SQLStatsFlushEnabled is the cluster setting that controls if the sqlstats
+// subsystem persists the statistics into system table.
+var SQLStatsFlushEnabled = settings.RegisterBoolSetting(
+	"sql.stats.flush.enabled",
+	"if set, SQL execution statistics are periodically flushed to disk",
+	true, /* defaultValue */
+).WithPublic()
+
+// SQLStatsFlushJitter specifies the jitter fraction on the interval between
+// attempts to flush SQL Stats.
+//
+// [(1 - SQLStatsFlushJitter) * SQLStatsFlushInterval),
+//  (1 + SQLStatsFlushJitter) * SQLStatsFlushInterval)]
+var SQLStatsFlushJitter = settings.RegisterFloatSetting(
+	"sql.stats.flush.jitter",
+	"jitter fraction on the duration between sql stats flushes",
+	0.15,
+	func(f float64) error {
+		if f < 0 || f > 1 {
+			return errors.Newf("%f is not in [0, 1]", f)
+		}
+		return nil
+	},
+)
