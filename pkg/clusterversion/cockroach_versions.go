2d284024239e13b61823e8b4c4a2a654cede9c51
Azhng <archer.xn@gmail.com>
2021-07-09 15:52:52 -0400

    sql: create sql stats compaction scheduled job
    This commit introduces SQL Stats Compaction Scheduled job, where it runs
    in an hourly schedule and invokes the SQL Stats Compaction Job that was
    created in the previous commit.
    
    Release justification: category 4
    
    Release note: None

--- pkg/clusterversion/cockroach_versions.go
+++ pkg/clusterversion/cockroach_versions.go
@@ -307,6 +307,11 @@ const (
 	// GeneratedAsIdentity is the syntax support for `GENERATED {ALWAYS | BY
 	// DEFAULT} AS IDENTITY` under `CREATE TABLE` syntax.
 	GeneratedAsIdentity
+	// SQLStatsCompactionScheduledJob creates a ScheduledJob for SQL Stats
+	// compaction on cluster startup and ensures that there is only one entry for
+	// the schedule.
+	SQLStatsCompactionScheduledJob
+
 	// Step (1): Add new versions here.
 )
 
@@ -525,6 +530,10 @@ var versionsSingleton = keyedVersions{
 	{
 		Key:     GeneratedAsIdentity,
 		Version: roachpb.Version{Major: 21, Minor: 1, Internal: 150},
+	},
+	{
+		Key:     SQLStatsCompactionScheduledJob,
+		Version: roachpb.Version{Major: 21, Minor: 1, Internal: 152},
 	},
 
 	// Step (2): Add new versions here.
