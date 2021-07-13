aeac9c9246aae1fd64d74994c96d65e0e14abcef
e-mbrown <ebsonari@gmail.com>
2021-07-13 11:38:05 -0400

    sql: add sql shell telemetry counter
    This change allows us to keep track of
    connections made using our internal SQl shell.
    
    Release note: None

--- pkg/sql/telemetry_test.go
+++ pkg/sql/telemetry_test.go
@@ -11,12 +11,22 @@
 package sql_test
 
 import (
+	"context"
+	gosql "database/sql"
+	"fmt"
+	"net/url"
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/ccl/kvccl/kvtenantccl"
+	"github.com/cockroachdb/cockroach/pkg/server"
+	"github.com/cockroachdb/cockroach/pkg/server/diagnostics"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqltestutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/diagutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 )
@@ -35,4 +45,61 @@ func TestTelemetry(t *testing.T) {
 		[]base.TestServerArgs{{}},
 		true, /* testTenant */
 	)
+}
+
+func TestTelemetryRecordCockroachShell(t *testing.T) {
+
+	diagSrv := diagutils.NewServer()
+
+	tempExternalIODir, tempDirCleanup := testutils.TempDir(t)
+	defer tempDirCleanup()
+
+	diagSrvURL := diagSrv.URL()
+	mapServerArgs := make(map[int]base.TestServerArgs)
+	serverArgs := []base.TestServerArgs{{}}
+	for i, v := range serverArgs {
+		v.Knobs.Server = &server.TestingKnobs{
+			DiagnosticsTestingKnobs: diagnostics.TestingKnobs{
+				OverrideReportingURL: &diagSrvURL,
+			},
+		}
+		v.ExternalIODir = tempExternalIODir
+		mapServerArgs[i] = v
+	}
+	cluster := serverutils.StartNewTestCluster(
+		t,
+		len(serverArgs),
+		base.TestClusterArgs{ServerArgsPerNode: mapServerArgs},
+	)
+
+	pgUrl, cleanupFn := sqlutils.PGUrl(
+		t,
+		cluster.Server(0).ServingSQLAddr(),
+		"TestTelemetryRecordCockroachShell",
+		url.User("root"),
+	)
+	defer cleanupFn()
+	q := pgUrl.Query()
+
+	q.Add("application_name", "$ cockroach internal test")
+	pgUrl.RawQuery = q.Encode()
+	fmt.Println(pgUrl)
+
+	db, err := gosql.Open("postgres", pgUrl.String())
+	if err != nil {
+		//return nil, err
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	var app string
+	db.QueryRow("SHOW application_name").Scan(&app)
+
+	reporter := cluster.Server(0).DiagnosticsReporter().(*diagnostics.Reporter)
+	reporter.ReportDiagnostics(context.Background())
+
+	last := diagSrv.LastRequestData()
+	fmt.Printf("%v", last)
+	//require.Equal(t, appName, last.)
+
 }
