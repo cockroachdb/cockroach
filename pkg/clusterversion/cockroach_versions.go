74a2aaa6eb74319c482b10ebd7f146b2d5bf10da
Cameron Nunez <cameron@cockroachlabs.com>
2021-07-14 15:19:56 -0400

    auth: set up periodic deletion of expired sessions in the web_sessions system table
    Expired sessions are not cleaned up in the web_sessions system
    table quickly enough. The table should be kept from growing
    indefinitely in the long run. This patch sets up periodic
    deletion of these expired sessions.
    
    Release note (security update): Old authentication web session
    rows in the system.web_sessions table no longer accumulate
    indefinitely in the long run. These rows are periodically
    deleted. Refer to the reference docs for details about the
    new cluster settings for system.web_sessions.

--- pkg/clusterversion/cockroach_versions.go
+++ pkg/clusterversion/cockroach_versions.go
@@ -271,6 +271,9 @@ const (
 	SQLInstancesTable
 	// Can return new retryable rangefeed errors without crashing the client
 	NewRetryableRangefeedErrors
+	// AlterSystemWebSessionsCreateIndexes creates indexes on the columns revokedAt and
+	// lastUsedAt for the system.web_sessions table.
+	AlterSystemWebSessionsCreateIndexes
 
 	// Step (1): Add new versions here.
 )
@@ -439,6 +442,10 @@ var versionsSingleton = keyedVersions{
 	{
 		Key:     NewRetryableRangefeedErrors,
 		Version: roachpb.Version{Major: 21, Minor: 1, Internal: 124},
+	},
+	{
+		Key:     AlterSystemWebSessionsCreateIndexes,
+		Version: roachpb.Version{Major: 21, Minor: 1, Internal: 126},
 	},
 
 	// Step (2): Add new versions here.
