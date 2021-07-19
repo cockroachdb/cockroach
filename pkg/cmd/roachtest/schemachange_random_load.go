bc62035d18fcbbb193f17d922e860efd0ed4bd69
Adam Storm <storm@cockroachlabs.com>
2021-07-19 16:25:58 -0400

    sql: Skip schemachange/random-load before 21.2
    The schemachange/random-load roachtest is broken in 20.2 and 21.1.
    Rather than backport the (rather invovled) changes required to make the
    test run clean in these releases, we're skipping it with the hopes of
    getting it clean in 21.2.
    
    Release note: None

--- pkg/cmd/roachtest/schemachange_random_load.go
+++ pkg/cmd/roachtest/schemachange_random_load.go
@@ -27,6 +27,7 @@ func registerSchemaChangeRandomLoad(r *testRegistry) {
 		Name:       "schemachange/random-load",
 		Owner:      OwnerSQLSchema,
 		Cluster:    makeClusterSpec(3),
+		Skip:       "Skipped until 21.2 due to stability issues",
 		MinVersion: "v20.1.0",
 		Run: func(ctx context.Context, t *test, c *cluster) {
 			maxOps := 5000
