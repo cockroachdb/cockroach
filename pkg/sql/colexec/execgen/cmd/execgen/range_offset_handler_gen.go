091e322caed2c565c6350537e605c40a227389a3
Drew Kimball <drewk@cockroachlabs.com>
2021-07-19 00:13:16 -0600

    colexec: add range check for binary operations with timestamps
    This commit adds a range check after additions or subtractions that
    produce a timestamp result. This ensures that the vectorized engine
    produces the same error as the row engine when the result of the
    operation is out of range.
    
    Release note: None

--- pkg/sql/colexec/execgen/cmd/execgen/range_offset_handler_gen.go
+++ pkg/sql/colexec/execgen/cmd/execgen/range_offset_handler_gen.go
@@ -303,23 +303,28 @@ func typeName(typeFamily types.Family, typeWidth int32) string {
 	return toVecMethod(typeconv.TypeFamilyToCanonicalTypeFamily(typeFamily), typeWidth)
 }
 
+// This format string should be used with the left operand of the binary
+// operation as the first argument, and the desired result variable name as the
+// second argument.
+const dateToTimeCastStr = `
+		d_casted, err := pgdate.MakeDateFromUnixEpoch(%s)
+		if err != nil {
+			colexecerror.ExpectedError(err)
+		}
+		%s, err := d_casted.ToTime()
+		if err != nil {
+			colexecerror.ExpectedError(err)
+		}
+`
+
 func dateAssignFunc(
 	op *lastArgWidthOverload, targetElem, leftElem, rightElem, targetCol, leftCol, rightCol string,
 ) string {
 	// Date rows are stored as int64s representing the number of days since the
 	// unix epoch. We have to convert to timestamps before executing the binary
 	// operator.
-	const castVarName = "t"
-	castStr := fmt.Sprintf(`
-		d, err := pgdate.MakeDateFromUnixEpoch(%s)
-		if err != nil {
-			colexecerror.ExpectedError(err)
-		}
-		%s, err := d.ToTime()
-		if err != nil {
-			colexecerror.ExpectedError(err)
-		}
-		`, leftElem, castVarName)
+	const castVarName = "t_casted"
+	castStr := fmt.Sprintf(dateToTimeCastStr, leftElem, castVarName)
 	var o timestampIntervalCustomizer
 	return castStr + o.getBinOpAssignFunc()(
 		op, targetElem, castVarName, rightElem, targetCol, leftCol, rightCol)
@@ -329,17 +334,8 @@ func dateCmpFunc(targetElem, leftElem, rightElem, leftCol, rightCol string) stri
 	// Date rows are stored as int64s representing the number of days since the
 	// unix epoch. We have to convert to timestamps before executing the
 	// comparison operator.
-	const castVarName = "t"
-	castStr := fmt.Sprintf(`
-		d, err := pgdate.MakeDateFromUnixEpoch(%s)
-		if err != nil {
-			colexecerror.InternalError(err)
-		}
-		%s, err := d.ToTime()
-		if err != nil {
-			colexecerror.InternalError(err)
-		}
-		`, leftElem, castVarName)
+	const castVarName = "t_casted"
+	castStr := fmt.Sprintf(dateToTimeCastStr, leftElem, castVarName)
 	var o timestampCustomizer
 	return castStr + o.getCmpOpCompareFunc()(targetElem, castVarName, rightElem, leftCol, rightCol)
 }
