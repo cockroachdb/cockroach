8cc783c06e01ed914e1b8d44244233b103ea74e2
Radu Berinde <radu@cockroachlabs.com>
2021-07-19 11:27:45 -0700

    sql: tenant resource limits configuration API
    This commit adds a `crdb_internal.update_tenant_resource_limits`
    internal SQL function (to be used by the system tenant) which updates
    the token bucket configuration for a specific tenant.
    
    Release note: None

---
+++ pkg/ccl/multitenantccl/tenantcostserver/configure.go
@@ -0,0 +1,63 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package tenantcostserver
+
+import (
+	"context"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/kv"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgcode"
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/cockroachdb/errors"
+)
+
+// ReconfigureTokenBucket updates a tenant's token bucket settings. It is part
+// of the TenantUsageServer interface; see that for more details.
+func (s *instance) ReconfigureTokenBucket(
+	ctx context.Context,
+	txn *kv.Txn,
+	tenantID roachpb.TenantID,
+	availableRU float64,
+	refillRate float64,
+	maxBurstRU float64,
+	asOf time.Time,
+	asOfConsumedRequestUnits float64,
+) error {
+	row, err := s.executor.QueryRowEx(
+		ctx, "check-tenant", txn, sessiondata.NodeUserSessionDataOverride,
+		`SELECT active FROM system.tenants WHERE id = $1`, tenantID.ToUint64(),
+	)
+	if err != nil {
+		return err
+	}
+	if row == nil {
+		return pgerror.Newf(pgcode.UndefinedObject, "tenant %q does not exist", tenantID)
+	}
+	if active := *row[0].(*tree.DBool); !active {
+		return errors.Errorf("tenant %q is not active", tenantID)
+	}
+	state, err := readTenantUsageState(ctx, s.executor, txn, tenantID)
+	if err != nil {
+		return err
+	}
+	state.Seq++
+	state.Bucket.Reconfigure(
+		availableRU, refillRate, maxBurstRU, asOf, asOfConsumedRequestUnits,
+		timeutil.Now(), state.Consumption.RU,
+	)
+	if err := updateTenantUsageState(ctx, s.executor, txn, tenantID, state); err != nil {
+		return err
+	}
+	return nil
+}
