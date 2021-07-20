6b49dd6c4c525e4014bc1fe34eedf112f6b02e73
Marius Posta <marius@cockroachlabs.com>
2021-07-20 14:47:52 -0400

    clusterversion: remove ForeignKeyRepresentationMigration
    Partially addresses #66546 by removing a cluster version and its
    associated dependencies which, for any cluster whose version is at
    least 21.1, is certain to be active.
    
    Release note: None

--- pkg/clusterversion/key_string.go
+++ pkg/clusterversion/key_string.go
@@ -28,26 +28,25 @@ func _() {
 	_ = x[MultiRegionFeatures-17]
 	_ = x[ClosedTimestampsRaftTransport-18]
 	_ = x[ChangefeedsSupportPrimaryIndexChanges-19]
-	_ = x[ForeignKeyRepresentationMigration-20]
-	_ = x[PriorReadSummaries-21]
-	_ = x[NonVotingReplicas-22]
-	_ = x[ProtectedTsMetaPrivilegesMigration-23]
-	_ = x[V21_1-24]
-	_ = x[Start21_1PLUS-25]
-	_ = x[Start21_2-26]
-	_ = x[JoinTokensTable-27]
-	_ = x[AcquisitionTypeInLeaseHistory-28]
-	_ = x[SerializeViewUDTs-29]
-	_ = x[ExpressionIndexes-30]
-	_ = x[DeleteDeprecatedNamespaceTableDescriptorMigration-31]
-	_ = x[FixDescriptors-32]
-	_ = x[SQLStatsTable-33]
-	_ = x[DatabaseRoleSettings-34]
+	_ = x[PriorReadSummaries-20]
+	_ = x[NonVotingReplicas-21]
+	_ = x[ProtectedTsMetaPrivilegesMigration-22]
+	_ = x[V21_1-23]
+	_ = x[Start21_1PLUS-24]
+	_ = x[Start21_2-25]
+	_ = x[JoinTokensTable-26]
+	_ = x[AcquisitionTypeInLeaseHistory-27]
+	_ = x[SerializeViewUDTs-28]
+	_ = x[ExpressionIndexes-29]
+	_ = x[DeleteDeprecatedNamespaceTableDescriptorMigration-30]
+	_ = x[FixDescriptors-31]
+	_ = x[SQLStatsTable-32]
+	_ = x[DatabaseRoleSettings-33]
 }
 
-const _Key_name = "Start20_2NodeMembershipStatusMinPasswordLengthAbortSpanBytesCreateLoginPrivilegeHBAForNonTLSV20_2Start21_1CPutInlineReplicaVersionsreplacedTruncatedAndRangeAppliedStateMigrationreplacedPostTruncatedAndRangeAppliedStateMigrationLongRunningMigrationsTruncatedAndRangeAppliedStateMigrationPostTruncatedAndRangeAppliedStateMigrationSeparatedIntentsTracingVerbosityIndependentSemanticsMultiRegionFeaturesClosedTimestampsRaftTransportChangefeedsSupportPrimaryIndexChangesForeignKeyRepresentationMigrationPriorReadSummariesNonVotingReplicasProtectedTsMetaPrivilegesMigrationV21_1Start21_1PLUSStart21_2JoinTokensTableAcquisitionTypeInLeaseHistorySerializeViewUDTsExpressionIndexesDeleteDeprecatedNamespaceTableDescriptorMigrationFixDescriptorsSQLStatsTableDatabaseRoleSettings"
+const _Key_name = "Start20_2NodeMembershipStatusMinPasswordLengthAbortSpanBytesCreateLoginPrivilegeHBAForNonTLSV20_2Start21_1CPutInlineReplicaVersionsreplacedTruncatedAndRangeAppliedStateMigrationreplacedPostTruncatedAndRangeAppliedStateMigrationLongRunningMigrationsTruncatedAndRangeAppliedStateMigrationPostTruncatedAndRangeAppliedStateMigrationSeparatedIntentsTracingVerbosityIndependentSemanticsMultiRegionFeaturesClosedTimestampsRaftTransportChangefeedsSupportPrimaryIndexChangesPriorReadSummariesNonVotingReplicasProtectedTsMetaPrivilegesMigrationV21_1Start21_1PLUSStart21_2JoinTokensTableAcquisitionTypeInLeaseHistorySerializeViewUDTsExpressionIndexesDeleteDeprecatedNamespaceTableDescriptorMigrationFixDescriptorsSQLStatsTableDatabaseRoleSettings"
 
-var _Key_index = [...]uint16{0, 9, 29, 46, 60, 80, 92, 97, 106, 116, 131, 177, 227, 248, 286, 328, 344, 380, 399, 428, 465, 498, 516, 533, 567, 572, 585, 594, 609, 638, 655, 672, 721, 735, 748, 768}
+var _Key_index = [...]uint16{0, 9, 29, 46, 60, 80, 92, 97, 106, 116, 131, 177, 227, 248, 286, 328, 344, 380, 399, 428, 465, 483, 500, 534, 539, 552, 561, 576, 605, 622, 639, 688, 702, 715, 735}
 
 func (i Key) String() string {
 	if i < 0 || i >= Key(len(_Key_index)-1) {
