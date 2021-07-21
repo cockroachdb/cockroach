0aa1d66ee8f1e4f112db907c580db9d40a539a24
Rafi Shamim <rafi@cockroachlabs.com>
2021-07-21 01:32:27 -0400

    catalog/migration: add system.database_role_settings
    This table will be used to hold per-database and per-role defaults for
    session variables. It is analagous and similar in structure to the
    Postgres pg_db_role_setting table. There is no release note since the
    table is empty and unused right now.
    
    Release note: None

--- pkg/clusterversion/cockroach_versions.go
+++ pkg/clusterversion/cockroach_versions.go
@@ -309,6 +309,9 @@ const (
 	// SQLStatsTable adds the system tables for storing persisted SQL statistics
 	// for statements and transactions.
 	SQLStatsTable
+	// DatabaseRoleSettings adds the system table for storing per-user and
+	// per-role default session settings.
+	DatabaseRoleSettings
 
 	// Step (1): Add new versions here.
 )
@@ -529,6 +532,10 @@ var versionsSingleton = keyedVersions{
 	{
 		Key:     SQLStatsTable,
 		Version: roachpb.Version{Major: 21, Minor: 1, Internal: 116},
+	},
+	{
+		Key:     DatabaseRoleSettings,
+		Version: roachpb.Version{Major: 21, Minor: 1, Internal: 118},
 	},
 	// Step (2): Add new versions here.
 }
--- pkg/clusterversion/key_string.go
+++ pkg/clusterversion/key_string.go
@@ -54,11 +54,12 @@ func _() {
 	_ = x[DeleteDeprecatedNamespaceTableDescriptorMigration-43]
 	_ = x[FixDescriptors-44]
 	_ = x[SQLStatsTable-45]
+	_ = x[DatabaseRoleSettings-46]
 }
 
-const _Key_name = "Start20_2GeospatialTypeAlterColumnTypeGeneralUserDefinedSchemasNoOriginFKIndexesNodeMembershipStatusMinPasswordLengthAbortSpanBytesMaterializedViewsBox2DTypeCreateLoginPrivilegeHBAForNonTLSV20_2Start21_1EmptyArraysInInvertedIndexesUniqueWithoutIndexConstraintsVirtualComputedColumnsCPutInlineReplicaVersionsreplacedTruncatedAndRangeAppliedStateMigrationreplacedPostTruncatedAndRangeAppliedStateMigrationNewSchemaChangerLongRunningMigrationsTruncatedAndRangeAppliedStateMigrationPostTruncatedAndRangeAppliedStateMigrationSeparatedIntentsTracingVerbosityIndependentSemanticsSequencesRegclassImplicitColumnPartitioningMultiRegionFeaturesClosedTimestampsRaftTransportChangefeedsSupportPrimaryIndexChangesForeignKeyRepresentationMigrationPriorReadSummariesNonVotingReplicasProtectedTsMetaPrivilegesMigrationV21_1Start21_1PLUSStart21_2JoinTokensTableAcquisitionTypeInLeaseHistorySerializeViewUDTsExpressionIndexesDeleteDeprecatedNamespaceTableDescriptorMigrationFixDescriptorsSQLStatsTable"
+const _Key_name = "Start20_2GeospatialTypeAlterColumnTypeGeneralUserDefinedSchemasNoOriginFKIndexesNodeMembershipStatusMinPasswordLengthAbortSpanBytesMaterializedViewsBox2DTypeCreateLoginPrivilegeHBAForNonTLSV20_2Start21_1EmptyArraysInInvertedIndexesUniqueWithoutIndexConstraintsVirtualComputedColumnsCPutInlineReplicaVersionsreplacedTruncatedAndRangeAppliedStateMigrationreplacedPostTruncatedAndRangeAppliedStateMigrationNewSchemaChangerLongRunningMigrationsTruncatedAndRangeAppliedStateMigrationPostTruncatedAndRangeAppliedStateMigrationSeparatedIntentsTracingVerbosityIndependentSemanticsSequencesRegclassImplicitColumnPartitioningMultiRegionFeaturesClosedTimestampsRaftTransportChangefeedsSupportPrimaryIndexChangesForeignKeyRepresentationMigrationPriorReadSummariesNonVotingReplicasProtectedTsMetaPrivilegesMigrationV21_1Start21_1PLUSStart21_2JoinTokensTableAcquisitionTypeInLeaseHistorySerializeViewUDTsExpressionIndexesDeleteDeprecatedNamespaceTableDescriptorMigrationFixDescriptorsSQLStatsTableDatabaseRoleSettings"
 
-var _Key_index = [...]uint16{0, 9, 23, 45, 63, 80, 100, 117, 131, 148, 157, 177, 189, 194, 203, 231, 260, 282, 292, 307, 353, 403, 419, 440, 478, 520, 536, 572, 589, 615, 634, 663, 700, 733, 751, 768, 802, 807, 820, 829, 844, 873, 890, 907, 956, 970, 983}
+var _Key_index = [...]uint16{0, 9, 23, 45, 63, 80, 100, 117, 131, 148, 157, 177, 189, 194, 203, 231, 260, 282, 292, 307, 353, 403, 419, 440, 478, 520, 536, 572, 589, 615, 634, 663, 700, 733, 751, 768, 802, 807, 820, 829, 844, 873, 890, 907, 956, 970, 983, 1003}
 
 func (i Key) String() string {
 	if i < 0 || i >= Key(len(_Key_index)-1) {
