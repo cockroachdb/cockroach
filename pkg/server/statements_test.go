2ceab239ffa75ea96e2c5adf0835f48e1ce45718
David Hartunian <davidh@cockroachlabs.com>
2021-07-22 14:59:42 -0400

    server: pod-to-pod fanout for statements api on tenant
    Previously, the Statements endpoint implementation was purely local in
    its execution and reported only in-memory data on the current tenant.
    This change initializes a gRPC and gRPC-gateway server on the tenant,
    much like we do on the hosts already, and uses the newly added
    InstanceID subsystem for tenants to implement a gRPC-based fanout for
    the Statements endpoint implementation.
    
    The fanout is done much in the same manner as on hosts and we
    expose the status server endpoints via HTTP on the tenant as well.
    
    This change is necessary to support serverless observability features
    and provide our UIs access to the Statements endpoint. Future work may
    move this API to SQL-only
    
    Resolves #64477
    
    Release note (api change): tenant pods now expose the Statements API at
    `/_status/statements` on their HTTP port.

---
+++ pkg/server/statements_test.go
@@ -0,0 +1,62 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package server
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/rpc"
+	"github.com/cockroachdb/cockroach/pkg/server/serverpb"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+)
+
+// TestStatements ensures that the Statements endpoint is accessible
+// via gRPC and returns information reflecting recently run SQL
+// queries.
+func TestStatements(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	ctx := context.Background()
+
+	testServer, db, _ := serverutils.StartServer(t, base.TestServerArgs{})
+	defer testServer.Stopper().Stop(ctx)
+
+	conn, err := testServer.RPCContext().GRPCDialNode(
+		testServer.RPCAddr(), testServer.NodeID(), rpc.DefaultClass,
+	).Connect(ctx)
+	require.NoError(t, err)
+	defer func(conn *grpc.ClientConn) {
+		_ = conn.Close()
+	}(conn)
+
+	client := serverpb.NewStatusClient(conn)
+
+	testQuery := "CREATE TABLE foo (id INT8)"
+	_, err = db.Exec(testQuery)
+	require.NoError(t, err)
+
+	resp, err := client.Statements(ctx, &serverpb.StatementsRequest{NodeID: "local"})
+	require.NoError(t, err)
+	require.NotEmpty(t, resp.Statements)
+
+	queries := make([]string, len(resp.Statements))
+	for _, s := range resp.Statements {
+		queries = append(queries, s.Key.KeyData.Query)
+	}
+	require.Contains(t, queries, testQuery)
+}
