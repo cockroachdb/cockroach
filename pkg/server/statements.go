73090bd82ead796cdd220762b842f7338d38142c
David Hartunian <davidh@cockroachlabs.com>
2021-07-22 14:59:42 -0400

    server: pod-to-pod fanout for statements api on tenant
    Previously, the Statements endpoint implementation was purely local in
    its execution and reported only in-memory data on the current tenant.
    This change initializes a gRPC and gRPC-gateway server on the tenant,
    much like we do on the hosts already, and uses the newly added
    InstanceID subsystem for tenants to implement a gRPC-based fanout for
    the Statements endpoint implementation.
    
    The fanout is done much in the same manner as on hosts and we
    expose the status server endpoints via HTTP on the tenant as well.
    
    This change is necessary to support serverless observability features
    and provide our UIs access to the Statements endpoint. Future work may
    move this API to SQL-only
    
    Resolves #64477
    
    Release note (api change): tenant pods now expose the Statements API at
    `/_status/statements` on their HTTP port.
    
    REVIEWER NOTE: This change is based on #66600 which is still in
    progress, please only review the final commit. Once #66600 is merged,
    only the final commit will remain on rebase.

--- pkg/server/statements.go
+++ pkg/server/statements.go
@@ -14,7 +14,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/server/serverpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catconstants"
@@ -33,6 +32,10 @@ func (s *statusServer) Statements(
 		return nil, err
 	}
 
+	if s.gossip.NodeID.Get() == 0 {
+		return nil, status.Errorf(codes.Unavailable, "nodeID not set")
+	}
+
 	response := &serverpb.StatementsResponse{
 		Statements:            []serverpb.StatementsResponse_CollectedStatementStatistics{},
 		LastReset:             timeutil.Now(),
@@ -49,7 +52,7 @@ func (s *statusServer) Statements(
 			return nil, status.Errorf(codes.InvalidArgument, err.Error())
 		}
 		if local {
-			return statementsLocal(ctx, s.gossip.NodeID, s.admin.server.sqlServer)
+			return statementsLocal(ctx, s.gossip.NodeID.Get(), s.admin.server.sqlServer)
 		}
 		status, err := s.dialNode(ctx, requestedNodeID)
 		if err != nil {
@@ -89,7 +92,7 @@ func (s *statusServer) Statements(
 }
 
 func statementsLocal(
-	ctx context.Context, nodeID *base.NodeIDContainer, sqlServer *SQLServer,
+	ctx context.Context, nodeID roachpb.NodeID, sqlServer *SQLServer,
 ) (*serverpb.StatementsResponse, error) {
 	stmtStats, err := sqlServer.pgServer.SQLServer.GetUnscrubbedStmtStats(ctx)
 	if err != nil {
@@ -113,7 +116,7 @@ func statementsLocal(
 	for i, txn := range txnStats {
 		resp.Transactions[i] = serverpb.StatementsResponse_ExtendedCollectedTransactionStatistics{
 			StatsData: txn,
-			NodeID:    nodeID.Get(),
+			NodeID:    nodeID,
 		}
 	}
 
@@ -121,7 +124,7 @@ func statementsLocal(
 		resp.Statements[i] = serverpb.StatementsResponse_CollectedStatementStatistics{
 			Key: serverpb.StatementsResponse_ExtendedStatementStatisticsKey{
 				KeyData: stmt.Key,
-				NodeID:  nodeID.Get(),
+				NodeID:  nodeID,
 			},
 			ID:    stmt.ID,
 			Stats: stmt.Stats,
