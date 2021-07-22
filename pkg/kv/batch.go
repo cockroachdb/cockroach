419c6639ad4405fc9d293a4627ce4e0fff8726cc
Michael Erickson <michae2@cockroachlabs.com>
2021-07-22 10:22:21 -0700

    kv: set Batch.pErr during Batch.prepare
    If we fail to construct a Batch (e.g. fail to marshal a key or value)
    then an error will be placed in the resultsBuf and the batch will not
    actually be sent to the layers below. In this case we still need to set
    Batch.pErr, so that Batch.MustPErr is able to return a roachpb.Error to
    higher layers.
    
    I imagine in practice we never fail to marshal the key or value, so we
    have never seen this panic in the wild.
    
    Release note (bug fix): Fix an extremely rare panic in Batch.MustPErr.
    
    Release justification: Bug fix.

--- pkg/kv/batch.go
+++ pkg/kv/batch.go
@@ -95,10 +95,11 @@ func (b *Batch) MustPErr() *roachpb.Error {
 }
 
 func (b *Batch) prepare() error {
-	for _, r := range b.Results {
-		if r.Err != nil {
-			return r.Err
+	if err := b.resultErr(); err != nil {
+		if b.pErr == nil {
+			b.pErr = roachpb.NewError(err)
 		}
+		return err
 	}
 	return nil
 }
@@ -298,9 +299,9 @@ func (b *Batch) fillResults(ctx context.Context) {
 // resultErr walks through the result slice and returns the first error found,
 // if one exists.
 func (b *Batch) resultErr() error {
-	for i := range b.Results {
-		if err := b.Results[i].Err; err != nil {
-			return err
+	for _, r := range b.Results {
+		if r.Err != nil {
+			return r.Err
 		}
 	}
 	return nil
