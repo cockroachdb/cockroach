8009590f17d404f9ced79669a03aad0754f76683
Jordan Lewis <jordanthelewis@gmail.com>
2021-07-23 20:44:42 -0300

    debug: add all-plan generator for statement-bundle
    This commit adds a way to generate all possible plans for a statement
    bundle, by replacing each placeholder in the bundle's statement with all
    of the different histogram values for the placeholder's column, and
    doing this for all combinations with all of the placeholders.
    
    Each possible plan is run in the demo cluster via explain, and
    deduplicated, so that the result is all unique plans for all of the
    combinations above.
    
    To use this, you must pass in the mapping from placeholder number to
    fully-qualified column name that is available in the statistics files of
    the statement bundle.
    
    Here is an example of the usage:
    
    ```
    ./cockroach debug statement-bundle recreate bundledir --placeholder='1=public.a.a' --placeholder='2=public.a.b'
    ```
    
    This usage will replace each usage of the placeholders 1 and 2 with all
    combinations of the values in the histograms for the columns public.a.a
    and public.a.b respectively.
    
    Each of the possible plans is printed to standard out, and the user is
    dropped into the demo environment.
    
    Release note: None

--- pkg/cli/statement_bundle.go
+++ pkg/cli/statement_bundle.go
@@ -11,13 +11,21 @@
 package cli
 
 import (
+	"bytes"
 	"context"
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"io"
 	"io/fs"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 
+	"github.com/cockroachdb/cockroach/pkg/cli/clisqlclient"
 	"github.com/cockroachdb/cockroach/pkg/cli/democluster"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
@@ -45,10 +53,16 @@ and stats in an unzipped statement bundle directory.
 	Args: cobra.ExactArgs(1),
 }
 
+var placeholderPairs []string
+
 func init() {
 	statementBundleRecreateCmd.RunE = MaybeDecorateGRPCError(func(cmd *cobra.Command, args []string) error {
 		return runBundleRecreate(cmd, args)
 	})
+
+	statementBundleRecreateCmd.Flags().StringArrayVar(&placeholderPairs, "placeholder", nil,
+		"pass in a map of placeholder id to fully-qualified table column to get the program to produce all combinations"+
+			" of explain plans with each of the histogram values for each column replaced in its placeholder.")
 }
 
 type statementBundle struct {
@@ -154,6 +168,174 @@ func runBundleRecreate(cmd *cobra.Command, args []string) error {
 # %s
 `, zipdir, bundle.statement)
 
+	if placeholderPairs != nil {
+		if err := runExplainCombinations(cliCtx, conn, bundle); err != nil {
+			return err
+		}
+	}
+
 	sqlCtx.ShellCtx.DemoCluster = c
 	return sqlCtx.Run(conn)
+}
+
+var placeholderRe = regexp.MustCompile(`(\d+): (.*)`)
+
+var statsRe = regexp.MustCompile(`ALTER TABLE ([\w.]+) INJECT STATISTICS '`)
+
+func runExplainCombinations(ctx cliContext, conn clisqlclient.Conn, bundle *statementBundle) error {
+	placeholderMap := make(map[int]string)
+	for _, placeholderPairStr := range placeholderPairs {
+		fmt.Println(placeholderPairStr)
+		pair := strings.Split(placeholderPairStr, "=")
+		if len(pair) != 2 {
+			return errors.New("use --placeholder='1=schema.table.col 2=schema.table.col...'")
+		}
+		n, err := strconv.Atoi(pair[0])
+		if err != nil {
+			return err
+		}
+		placeholderMap[n] = pair[1]
+	}
+
+	stmtComponents := strings.Split(string(bundle.statement), "Arguments:")
+	statement := strings.TrimSpace(stmtComponents[0])
+	placeholders := stmtComponents[1]
+
+	var stmtPlaceholders []int
+	for _, line := range strings.Split(placeholders, "\n") {
+		if matches := placeholderRe.FindStringSubmatch(line); len(matches) == 3 {
+			n, err := strconv.Atoi(matches[1])
+			if err != nil {
+				return err
+			}
+			stmtPlaceholders = append(stmtPlaceholders, n)
+		}
+	}
+
+	for _, n := range stmtPlaceholders {
+		if placeholderMap[n] == "" {
+			return errors.Errorf("specify --placeholder= for placeholder %d", n)
+		}
+	}
+
+	// map from fully-qualified column name to list of histogram upper_bound
+	// values with unique bucket attributes.
+	statsMap := make(map[string][]string)
+	for _, statsBytes := range bundle.stats {
+		statsStr := string(statsBytes)
+		matches := statsRe.FindStringSubmatch(statsStr)
+		if len(matches) != 2 {
+			return errors.Errorf("invalid stats file %s", statsStr)
+		}
+		tableName := matches[1]
+		// Find the first instance of ', which is the beginning of the JSON payload.
+		idx := bytes.IndexByte(statsBytes, '\'')
+		var statsJSON []map[string]interface{}
+
+		// Snip off the last 3 characters, which are ';\n, the end of the JSON payload.
+		data := statsBytes[idx+1 : len(statsBytes)-3]
+		if err := json.Unmarshal(data, &statsJSON); err != nil {
+			return err
+		}
+
+		// map a bucket key (a bucket without upper bound) to an upper bound sample.
+		// This deduplicates identical buckets.
+		for _, stat := range statsJSON {
+			bucketMap := make(map[bucketKey]string)
+			columns := stat["columns"].([]interface{})
+			if len(columns) > 1 {
+				// Ignore multi-col stats.
+				continue
+			}
+			col := columns[0]
+
+			buckets := stat["histo_buckets"].([]interface{})
+			for _, b := range buckets {
+				bucket := b.(map[string]interface{})
+				key := bucketKey{
+					NumEq:         int64(bucket["num_eq"].(float64)),
+					NumRange:      int64(bucket["num_range"].(float64)),
+					DistinctRange: bucket["distinct_range"].(float64),
+				}
+				bucketMap[key] = bucket["upper_bound"].(string)
+			}
+			upperBounds := make([]string, 0, len(bucketMap))
+			for _, upperBound := range bucketMap {
+				upperBounds = append(upperBounds, upperBound)
+			}
+			fqColName := fmt.Sprintf("%s.%s", tableName, col)
+			statsMap[fqColName] = upperBounds
+		}
+	}
+
+	combinations := getPlaceholderCombinations(stmtPlaceholders, placeholderMap, statsMap)
+
+	// uniqueExplains maps explain output to the list of placeholders that
+	// produced it.
+	uniqueExplains := make(map[string][]string)
+	for _, values := range combinations {
+		// Run an explain for each possible combination.
+		stmt := statement
+		for i, n := range stmtPlaceholders {
+			stmt = strings.ReplaceAll(stmt, fmt.Sprintf("$%d", n), values[i])
+		}
+
+		dvals := make([]driver.Value, len(values))
+		for i := range values {
+			dvals[i] = values[i]
+		}
+
+		query := "EXPLAIN(SHAPE) " + statement
+		cliCtx.PrintfUnlessEmbedded("> %s (%s)\n", query, dvals)
+		rows, err := conn.Query(query, dvals)
+		if err != nil {
+			return err
+		}
+		row := []driver.Value{""}
+		var explainStr = strings.Builder{}
+		for err = rows.Next(row); err == nil; err = rows.Next(row) {
+			fmt.Fprintln(&explainStr, row[0])
+		}
+		if err != io.EOF {
+			return err
+		}
+		rows.Close()
+
+		uniqueExplains[explainStr.String()] = values
+	}
+
+	cliCtx.PrintfUnlessEmbedded("found %d unique explains:\n\n", len(uniqueExplains))
+	for explain, vals := range uniqueExplains {
+		cliCtx.PrintfUnlessEmbedded("Values %s: \n%s\n----\n\n", vals, explain)
+	}
+
+	return nil
+}
+
+// getPlaceholderCombinations returns a list of lists, which each inner list is
+// a possible set of placeholders that can be inserted into the statement, where
+// each possible value for each placeholder is taken from the input statsMap.
+func getPlaceholderCombinations(
+	remainingPlaceholders []int, placeholderMap map[int]string, statsMap map[string][]string,
+) [][]string {
+	placeholder := remainingPlaceholders[0]
+	fqColName := placeholderMap[placeholder]
+	var rest = [][]string{nil}
+	if len(remainingPlaceholders) > 1 {
+		// Recurse to get the rest of the combinations.
+		rest = getPlaceholderCombinations(remainingPlaceholders[1:], placeholderMap, statsMap)
+	}
+	var ret [][]string
+	for _, val := range statsMap[fqColName] {
+		for _, inner := range rest {
+			ret = append(ret, append([]string{val}, inner...))
+		}
+	}
+	return ret
+}
+
+type bucketKey struct {
+	NumEq         int64
+	NumRange      int64
+	DistinctRange float64
 }
