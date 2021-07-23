03f64792434356685f01d9b44478a5f15125c4d5
Andrei Matei <andrei@cockroachlabs.com>
2021-07-23 14:01:15 -0400

    kvserver: simplify a test
    The new TestRangefeedCheckpointsRecoverFromLeaseExpiration was using
    unnecessary scaffolding. This change also makes it easier to backport to
    20.2.
    
    Release note: None

--- pkg/kv/kvserver/replica_rangefeed_test.go
+++ pkg/kv/kvserver/replica_rangefeed_test.go
@@ -1058,8 +1058,7 @@ func TestRangefeedCheckpointsRecoverFromLeaseExpiration(t *testing.T) {
 			},
 		},
 	}
-	tci, _, _ := setupClusterForClosedTSTesting(ctx, t, testingTargetDuration,
-		testingCloseFraction, cargs, "cttest", "kv")
+	tci := serverutils.StartNewTestCluster(t, 2, cargs)
 	tc := tci.(*testcluster.TestCluster)
 	defer tc.Stopper().Stop(ctx)
 
@@ -1070,9 +1069,8 @@ func TestRangefeedCheckpointsRecoverFromLeaseExpiration(t *testing.T) {
 
 	sqlDB := sqlutils.MakeSQLRunner(tc.ServerConn(0))
 	sqlDB.Exec(t, `SET CLUSTER SETTING kv.rangefeed.enabled = true`)
-	// Drop the target closedts duration. This was set to testingTargetDuration
-	// above, but this is higher then it needs to be now that cluster and schema
-	// setup is complete.
+	// Drop the target closedts duration in order to speed up the rangefeed
+	// "nudging" once the range loses its lease.
 	sqlDB.Exec(t, `SET CLUSTER SETTING kv.closed_timestamp.target_duration = '10ms'`)
 
 	n1 := tc.Server(0)
