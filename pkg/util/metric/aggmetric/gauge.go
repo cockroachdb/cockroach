59e53008dae12ca8bebf815b19df74066a435ca3
Radu Berinde <radu@cockroachlabs.com>
2021-07-26 10:03:34 -0700

    util: add AggGaugeFloat64
    This commit adds a float aggregate gauge. We also add support for
    incrementing a GaugeFloat64 (required for the aggregate gauge).
    
    Release note: None

--- pkg/util/metric/aggmetric/gauge.go
+++ pkg/util/metric/aggmetric/gauge.go
@@ -11,6 +11,7 @@
 package aggmetric
 
 import (
+	"math"
 	"sync/atomic"
 
 	"github.com/cockroachdb/cockroach/pkg/util/metric"
@@ -109,7 +110,7 @@ func (g *Gauge) ToPrometheusMetric() *io_prometheus_client.Metric {
 // Destroy is used to destroy the gauge associated with this child. The value of
 // the Gauge will be decremented from its parent.
 func (g *Gauge) Destroy() {
-	g.parent.g.Dec(atomic.SwapInt64(&g.value, 0))
+	g.Update(0)
 	g.parent.remove(g)
 }
 
@@ -127,4 +128,116 @@ func (g *Gauge) Inc(i int64) {
 // Dec decrements the gauge's value.
 func (g *Gauge) Dec(i int64) {
 	g.Inc(-i)
+}
+
+// Update sets the gauge's value.
+func (g *Gauge) Update(val int64) {
+	old := atomic.SwapInt64(&g.value, val)
+	g.parent.g.Inc(val - old)
+}
+
+// AggGaugeFloat64 maintains a value as the sum of its children. The gauge will
+// report to crdb-internal time series only the aggregate sum of all of its
+// children, while its children are additionally exported to prometheus via the
+// PrometheusIterable interface.
+type AggGaugeFloat64 struct {
+	g metric.GaugeFloat64
+	childSet
+}
+
+var _ metric.Iterable = (*AggGaugeFloat64)(nil)
+var _ metric.PrometheusIterable = (*AggGaugeFloat64)(nil)
+var _ metric.PrometheusExportable = (*AggGaugeFloat64)(nil)
+
+// NewGaugeFloat64 constructs a new AggGaugeFloat64.
+func NewGaugeFloat64(metadata metric.Metadata, childLabels ...string) *AggGaugeFloat64 {
+	g := &AggGaugeFloat64{g: *metric.NewGaugeFloat64(metadata)}
+	g.init(childLabels)
+	return g
+}
+
+// GetName is part of the metric.Iterable interface.
+func (g *AggGaugeFloat64) GetName() string { return g.g.GetName() }
+
+// GetHelp is part of the metric.Iterable interface.
+func (g *AggGaugeFloat64) GetHelp() string { return g.g.GetHelp() }
+
+// GetMeasurement is part of the metric.Iterable interface.
+func (g *AggGaugeFloat64) GetMeasurement() string { return g.g.GetMeasurement() }
+
+// GetUnit is part of the metric.Iterable interface.
+func (g *AggGaugeFloat64) GetUnit() metric.Unit { return g.g.GetUnit() }
+
+// GetMetadata is part of the metric.Iterable interface.
+func (g *AggGaugeFloat64) GetMetadata() metric.Metadata { return g.g.GetMetadata() }
+
+// Inspect is part of the metric.Iterable interface.
+func (g *AggGaugeFloat64) Inspect(f func(interface{})) { f(g) }
+
+// GetType is part of the metric.PrometheusExportable interface.
+func (g *AggGaugeFloat64) GetType() *io_prometheus_client.MetricType {
+	return g.g.GetType()
+}
+
+// GetLabels is part of the metric.PrometheusExportable interface.
+func (g *AggGaugeFloat64) GetLabels() []*io_prometheus_client.LabelPair {
+	return g.g.GetLabels()
+}
+
+// ToPrometheusMetric is part of the metric.PrometheusExportable interface.
+func (g *AggGaugeFloat64) ToPrometheusMetric() *io_prometheus_client.Metric {
+	return g.g.ToPrometheusMetric()
+}
+
+// Value returns the aggregate sum of all of its current children.
+func (g *AggGaugeFloat64) Value() float64 {
+	return g.g.Value()
+}
+
+// AddChild adds a GaugeFloat64 to this AggGaugeFloat64. This method panics if a GaugeFloat64
+// already exists for this set of labelVals.
+func (g *AggGaugeFloat64) AddChild(labelVals ...string) *GaugeFloat64 {
+	child := &GaugeFloat64{
+		parent:           g,
+		labelValuesSlice: labelValuesSlice(labelVals),
+	}
+	g.add(child)
+	return child
+}
+
+// GaugeFloat64 is a child of a AggGaugeFloat64. When it is incremented or
+// decremented, so too is the parent. When metrics are collected by prometheus,
+// each of the children will appear with a distinct label, however, when
+// cockroach internally collects metrics, only the parent is collected.
+type GaugeFloat64 struct {
+	labelValuesSlice
+	parent *AggGaugeFloat64
+	bits   uint64
+}
+
+// ToPrometheusMetric constructs a prometheus metric for this GaugeFloat64.
+func (g *GaugeFloat64) ToPrometheusMetric() *io_prometheus_client.Metric {
+	return &io_prometheus_client.Metric{
+		Gauge: &io_prometheus_client.Gauge{
+			Value: proto.Float64(g.Value()),
+		},
+	}
+}
+
+// Destroy is used to destroy the gauge associated with this child. The value of
+// the GaugeFloat64 will be decremented from its parent.
+func (g *GaugeFloat64) Destroy() {
+	g.Update(0)
+	g.parent.remove(g)
+}
+
+// Value returns the gauge's current value.
+func (g *GaugeFloat64) Value() float64 {
+	return math.Float64frombits(atomic.LoadUint64(&g.bits))
+}
+
+// Update sets the gauge's value.
+func (g *GaugeFloat64) Update(v float64) {
+	oldBits := atomic.SwapUint64(&g.bits, math.Float64bits(v))
+	g.parent.g.Inc(v - math.Float64frombits(oldBits))
 }
