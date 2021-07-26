0ab2237178268b43292af5809384c3d533e05dc2
Peyton Walters <peyton.walters@cockroachlabs.com>
2021-07-26 12:02:55 -0400

    sql: add syntax for experimental domiciling
    This commit adds the PLACEMENT [RESTRICTED | DEFAULT] flag to CREATE
    DATABASE and adds the ALTER DATABASE PLACEMENT statement. These will
    be used to support #59756.
    
    Release note: None

---
+++ pkg/sql/sem/tree/data_placement.go
@@ -0,0 +1,41 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package tree
+
+import "github.com/cockroachdb/errors"
+
+// DataPlacement represents the desired data placement strategy for a given
+// database.
+type DataPlacement uint32
+
+const (
+	// DataPlacementUnspecified indicates an unspecified placement policy.
+	// This will get translated to the default value when persisted.
+	DataPlacementUnspecified DataPlacement = iota
+	// DataPlacementDefault indicates specified default data placement policy,
+	DataPlacementDefault
+	// DataPlacementRestricted indicates the database will not use non-voters for
+	// REGIONAL BY [TABLE | ROW] tables.
+	DataPlacementRestricted
+)
+
+// Format implements the NodeFormatter interface.
+func (node *DataPlacement) Format(ctx *FmtCtx) {
+	switch *node {
+	case DataPlacementRestricted:
+		ctx.WriteString("PLACEMENT RESTRICTED")
+	case DataPlacementDefault:
+		ctx.WriteString("PLACEMENT DEFAULT")
+	case DataPlacementUnspecified:
+	default:
+		panic(errors.AssertionFailedf("unknown data placement strategy: %d", *node))
+	}
+}
