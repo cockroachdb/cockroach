684eec0ddecda784749594c80d650cb2e4f9ade2
Tobias Grieger <tobias.b.grieger@gmail.com>
2021-07-26 17:36:58 +0200

    cli: skip TestRemoveDeadReplicas
    Refs: #50977
    
    Reason: flaky test
    
    Generated by bin/skip-test.
    
    Release justification: non-production code changes
    
    Release note: None

--- pkg/cli/debug_test.go
+++ pkg/cli/debug_test.go
@@ -140,6 +140,7 @@ func TestOpenReadOnlyStore(t *testing.T) {
 
 func TestRemoveDeadReplicas(t *testing.T) {
 	defer leaktest.AfterTest(t)()
+	skip.WithIssue(t, 50977, "flaky test")
 	defer log.Scope(t).Close(t)
 
 	// This test is pretty slow under race (200+ cpu-seconds) because it
