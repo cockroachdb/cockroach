584fb971e3bbdaa750b54ad6425748e9673058b7
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2021-07-26 01:40:33 -0400

    kv: grab raftMu during no-op writes with local gossip triggers
    Fixes #68011.
    
    As of 9f8c019, it is now possible to have no-op writes that do not go through
    Raft but do set one of the gossip triggers. These gossip triggers require the
    raftMu to be held, so we were running into trouble when handling the local
    eval results above Raft.
    
    For instance, we see this case when a transaction sets the system config
    trigger and then performs a delete range over an empty span before
    committing. In this case, the transaction will have no intents to
    remove, so it can auto-GC its record during an EndTxn. If its record was
    never written in the first place, this is a no-op (as of 9f8c019).
    
    There appear to be three ways we could solve this:
    1. we can avoid setting gossip triggers on transactions that don't perform
       any writes.
    2. we can force EndTxn requests with gossip triggers to go through Raft even
       if they are otherwise no-ops.
    3. we can properly handle gossip triggers on the above Raft local eval result
       path.
    
    This commit opts for the third option.

--- pkg/kv/kvserver/replica_raft.go
+++ pkg/kv/kvserver/replica_raft.go
@@ -115,7 +115,7 @@ func (r *Replica) evalAndPropose(
 	if proposal.command == nil {
 		intents := proposal.Local.DetachEncounteredIntents()
 		endTxns := proposal.Local.DetachEndTxns(pErr != nil /* alwaysOnly */)
-		r.handleReadWriteLocalEvalResult(ctx, *proposal.Local)
+		r.handleReadWriteLocalEvalResult(ctx, *proposal.Local, false /* raftMuHeld */)
 
 		pr := proposalResult{
 			Reply:              proposal.Local.Reply,
