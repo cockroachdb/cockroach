0a29cdbd11e9b77922fd781d47d1e25222d47717
Radu Berinde <radu@cockroachlabs.com>
2021-07-25 18:54:40 -0700

    tenantcostserver: use the tenant_usage system table
    This change implements most of the interaction with the tenant_usage
    system table, with the exception of dead instances detection and
    clean-up.
    
    We currently tolerate an empty table, but it would be cleaner to
    initialize the tenant state (instance_id=0 row) at tenant creation
    time (+ implement a migration). I will explore this in a future
    change, when we add some configurable defaults for the refill rate
    etc.
    
    Release note: None

--- pkg/ccl/multitenantccl/tenantcostclient/tenant_side.go
+++ pkg/ccl/multitenantccl/tenantcostclient/tenant_side.go
@@ -61,6 +61,8 @@ func (c *tenantSideCostController) mainLoop(ctx context.Context, stopper *stop.S
 		select {
 		case <-ticker.C:
 			req := roachpb.TokenBucketRequest{
+				// TODO(radu): populate instance ID.
+				InstanceID: 1,
 				ConsumptionSinceLastRequest: roachpb.TokenBucketRequest_Consumption{
 					// Report a dummy 1 RU consumption each time.
 					RU:               1,
