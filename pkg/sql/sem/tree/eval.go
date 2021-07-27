0882c69f2667f4e39ebfc44195ae94645b277637
Oliver Tan <otan@cockroachlabs.com>
2021-07-27 14:28:17 +1000

    sql: allow YMD, DMY for DateStyle as a session variable
    This commit enables YMD and DMY to be set as DateStyle arguments. Note
    this does not (yet) have an effect.
    
    Also give an unimplemented issue for SQL, Postgres and German styles.
    
    Release note: None

--- pkg/sql/sem/tree/eval.go
+++ pkg/sql/sem/tree/eval.go
@@ -47,6 +47,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/mon"
 	"github.com/cockroachdb/cockroach/pkg/util/timeofday"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil/pgdate"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 	"github.com/cockroachdb/errors"
 	"github.com/lib/pq/oid"
@@ -3786,6 +3787,11 @@ func (ctx *EvalContext) GetLocation() *time.Location {
 // GetIntervalStyle returns the session interval style.
 func (ctx *EvalContext) GetIntervalStyle() duration.IntervalStyle {
 	return ctx.SessionData.GetIntervalStyle()
+}
+
+// GetDateStyle returns the session date style.
+func (ctx *EvalContext) GetDateStyle() pgdate.DateStyle {
+	return ctx.SessionData.GetDateStyle()
 }
 
 // Ctx returns the session's context.
