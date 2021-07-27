f510482e0fa4e45124994904594f493a19f63383
Oliver Tan <otan@cockroachlabs.com>
2021-07-27 13:25:17 +1000

    pgdate: add library for storing and parsing DateStyles
    Prepare a protobuf enum for DateStyle, to be used for storing later.
    Also add a library that is able to parse and format the DateStyle with
    the newly parsed data.
    
    Release note: None

--- pkg/util/timeutil/pgdate/pgdate.go
+++ pkg/util/timeutil/pgdate/pgdate.go
@@ -251,3 +251,19 @@ func (d Date) SubDays(days int64) (Date, error) {
 	}
 	return MakeDateFromPGEpoch(n)
 }
+
+// SQLString formats the Style into a SQL string.
+func (s Style) SQLString() string {
+	switch s {
+	case Style_POSTGRES:
+		return "Postgres"
+	case Style_GERMAN:
+		return "German"
+	}
+	return s.String()
+}
+
+// SQLString formats DateStyle into a SQL format.
+func (ds DateStyle) SQLString() string {
+	return fmt.Sprintf("%s, %s", ds.Style.SQLString(), ds.Order.String())
+}
