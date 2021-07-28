fa65da6c058c7bd96ac73a8496dae27e460dacea
Cat J <87989074+catj-cockroach@users.noreply.github.com>
2021-07-28 11:35:41 -0400

    security: add support for root owned certificates
    The certificate loader previously verified that certificate private
    keys were only accessible by the owning user. This is adequate for
    instances where CockroachDB is run as root, or run in a system where
    the certificate private key is manually updated.
    
    Where this breaks down is in Kubernetes, as volume mounts (such as
    Secrets) are mounted as owned by the root user. To allow non-root
    processes to interact with volumes, they can be given a owning group ID
    and have custom permissions set. This means in Kubernetes, when running
    CockroachDB as a regular user, we should allow root owned keys,
    provided the group is correct and permissions are still restricted.
    
    This change adds an exception to the permission checks that allow the
    certificate loader to read a key that is owned by root but has a group
    ID that matches the process's group ID (the group ID of the user
    running the process) and has updated permissions to match.
    
    Release note (security update): The certificate loader now allows the
    loading of root owned private keys, provided the owning group matches
    the group of the (non-root) user running CockroachDB.

---
+++ pkg/security/permission_check_test.go
@@ -0,0 +1,82 @@
+// Copyright 2017 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+// +build !windows,!plan9
+
+package security
+
+import (
+	"os"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+)
+
+type keyFile struct {
+	uid   uint32
+	gid   uint32
+	mode  os.FileMode
+	valid bool
+}
+
+// we're not testing any code that interacts with this so let's lie
+func (keyFile keyFile) Mode() os.FileMode            { return keyFile.mode }
+func (keyFile keyFile) UID() uint64                  { return uint64(keyFile.uid) }
+func (keyFile keyFile) GID() uint64                  { return uint64(keyFile.gid) }
+func (keyFile keyFile) IsOwnedByUID(uid uint64) bool { return keyFile.UID() == uid }
+func (keyFile keyFile) IsOwnedByGID(gid uint64) bool { return keyFile.GID() == gid }
+
+func TestCheckFilePermission(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	processGID := uint32(1000)
+
+	tests := []keyFile{
+		{
+			uid:   0,
+			gid:   0,
+			mode:  0700,
+			valid: true,
+		},
+		{
+			uid:   0,
+			gid:   processGID,
+			mode:  0740,
+			valid: true,
+		},
+		{
+			uid:   0,
+			gid:   0,
+			mode:  0700,
+			valid: true,
+		},
+		{
+			uid:   0,
+			gid:   0,
+			mode:  0770,
+			valid: false,
+		},
+		{
+			uid:   0,
+			gid:   processGID,
+			mode:  0640,
+			valid: true,
+		},
+	}
+
+	for i, testKeyFile := range tests {
+		err := checkFilePermissions(int(processGID), "example file", testKeyFile)
+		if testKeyFile.valid && err != nil {
+			t.Errorf("key permission check %d failed when it should have passed with error: %s", i, err)
+		} else if !testKeyFile.valid && err == nil {
+			t.Errorf("key permission check %d should have failed but did not (permissions %s, uid %d, gid %d)", i, testKeyFile.Mode(), testKeyFile.uid, testKeyFile.gid)
+		}
+	}
+}
