e34411c29e1b9db0f50e14bbe302156a8ae77a6b
Cat J <87989074+catj-cockroach@users.noreply.github.com>
2021-07-28 11:35:41 -0400

    security: add support for root owned certificates
    The certificate loader previously verified that certificate private
    keys were only accessible by the owning user. This is adequate for
    instances where CockroachDB is run as root, or run in a system where
    the certificate private key is manually updated.
    
    Where this breaks down is in Kubernetes, as volume mounts (such as
    Secrets) are mounted as owned by the root user. To allow non-root
    processes to interact with volumes, they can be given a owning group ID
    and have custom permissions set. This means in Kubernetes, when running
    CockroachDB as a regular user, we should allow root owned keys,
    provided the group is correct and permissions are still restricted.
    
    This change adds an exception to the permission checks that allow the
    certificate loader to read a key that is owned by root but has a group
    ID that matches the process's group ID (the group ID of the user
    running the process) and has updated permissions to match.
    
    Release note (security update): The certificate loader now allows the
    loading of root owned private keys, provided the owning group matches
    the group of the (non-root) user running CockroachDB.

---
+++ pkg/util/sysutil/acl_unix.go
@@ -0,0 +1,71 @@
+// Copyright 2017 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+// +build !windows
+// +build !plan9
+
+package sysutil
+
+import (
+	"os"
+	"syscall"
+)
+
+type unixACLInfo struct {
+	uid  uint64
+	gid  uint64
+	mode os.FileMode
+}
+
+func (acl *unixACLInfo) UID() uint64 {
+	return acl.uid
+}
+
+func (acl *unixACLInfo) GID() uint64 {
+	return acl.gid
+}
+
+func (acl *unixACLInfo) IsOwnedByUID(uid uint64) bool {
+	return acl.uid == uid
+}
+
+func (acl *unixACLInfo) IsOwnedByGID(gid uint64) bool {
+	return acl.gid == gid
+}
+
+func (acl *unixACLInfo) Mode() os.FileMode {
+	return acl.mode
+}
+
+// GetFileACLInfo returns an ACLInfo that has the UID and GID populated from
+// the system specific file information.
+func GetFileACLInfo(info os.FileInfo) ACLInfo {
+	sysInfo := info.Sys()
+	if nil != sysInfo {
+		if statTInfo, ok := sysInfo.(*syscall.Stat_t); ok {
+			// we use uint64 because a process should never have a
+			// GID that's less than zero, and syscall.Stat_t may
+			// use a uint16 or uint32 for GID, since it's often a
+			// C struct under the hood.
+			return &unixACLInfo{
+				uid:  uint64(statTInfo.Uid),
+				gid:  uint64(statTInfo.Gid),
+				mode: info.Mode().Perm(),
+			}
+		}
+	}
+
+	// if we don't know who owns a file, assume that root owns it.
+	return &unixACLInfo{
+		uid:  uint64(0),
+		gid:  uint64(0),
+		mode: info.Mode().Perm(),
+	}
+}
