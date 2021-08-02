4236115c85dea51af71211c387a9ca3a3208b33d
Erik Grinaker <grinaker@cockroachlabs.com>
2021-08-02 11:10:09 +0000

    kvserver: add `TargetBytesAllowEmpty` option
    The `Header.TargetBytes` request limit will be overshot if the first
    result exceeds it, to ensure the client can make progress. However, an
    upcoming [parallel get/scan streaming library][1] needs to be able to
    disable this behavior to avoid exceeding memory budgets when dispatching
    parallel requests.
    
    This patch adds a field `TargetBytesAllowEmpty` which will allow an
    empty response if the first result exceeds `TargetBytes`. This only has
    an effect for `Get` and `Scan` requests, and only on 22.1 clusters since
    21.2 `DistSender` logic relies on the limit always being overshot. It
    is not supported for `Export` requests where it will return an error.
    
    [1]: https://github.com/cockroachdb/cockroach/blob/master/docs/RFCS/20210617_index_lookups_memory_limits.md
    
    Release note: None

--- pkg/kv/kvserver/batcheval/cmd_get.go
+++ pkg/kv/kvserver/batcheval/cmd_get.go
@@ -62,8 +62,16 @@ func Get(
 		return result.Result{}, err
 	}
 	if val != nil {
+		// NB: This calculation is different from Scan, since Scan responses include
+		// the key/value pair while Get only includes the value.
+		numBytes := int64(len(val.RawBytes))
+		if h.TargetBytes > 0 && h.TargetBytesAllowEmpty && numBytes > h.TargetBytes {
+			reply.ResumeSpan = &roachpb.Span{Key: args.Key}
+			reply.ResumeReason = roachpb.RESUME_BYTE_LIMIT
+			return result.Result{}, nil
+		}
 		reply.NumKeys = 1
-		reply.NumBytes = int64(len(val.RawBytes))
+		reply.NumBytes = numBytes
 	}
 	var intents []roachpb.Intent
 	if intent != nil {
