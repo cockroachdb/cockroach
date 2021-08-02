7d4ba513d5dae6a1f4fe89db1b34f44726961749
Paul Bardea <pbardea@gmail.com>
2021-08-02 16:37:20 -0400

    importccl: use unique ID for export file naming
    Previously, EXPORT would name its files based on the node ID the export
    processor was running. This could be problematic if multiple exports are
    running on the same node (and writing to the same directory). This
    commit replaces the node ID with a unique identifier (generated based on
    the instance ID).
    
    As a side-effect this also enables tenants to use EXPORT.
    
    Release note (bug fix): Fix a bug in EXPORT where concurrent exports
    could overwrite eachother.

--- pkg/ccl/importccl/exportcsv.go
+++ pkg/ccl/importccl/exportcsv.go
@@ -21,6 +21,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/execinfrapb"
 	"github.com/cockroachdb/cockroach/pkg/sql/rowenc"
 	"github.com/cockroachdb/cockroach/pkg/sql/rowexec"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/builtins"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util/encoding/csv"
@@ -172,6 +173,9 @@ func (sp *csvWriter) Run(ctx context.Context) {
 	ctx, span := tracing.ChildSpan(ctx, "csvWriter")
 	defer span.Finish()
 
+	instanceID := sp.flowCtx.EvalCtx.NodeID.SQLInstanceID()
+	uniqueID := builtins.GenerateUniqueInt(instanceID)
+
 	err := func() error {
 		typs := sp.input.OutputTypes()
 		sp.input.Start(ctx)
@@ -252,12 +256,7 @@ func (sp *csvWriter) Run(ctx context.Context) {
 			}
 			defer es.Close()
 
-			nodeID, err := sp.flowCtx.EvalCtx.NodeID.OptionalNodeIDErr(47970)
-			if err != nil {
-				return err
-			}
-
-			part := fmt.Sprintf("n%d.%d", nodeID, chunk)
+			part := fmt.Sprintf("n%d.%d", uniqueID, chunk)
 			chunk++
 			filename := writer.FileName(sp.spec, part)
 			// Close writer to ensure buffer and any compression footer is flushed.
