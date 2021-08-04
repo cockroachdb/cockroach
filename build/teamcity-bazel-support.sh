766196c596f39611be44de7f5a2aff52572521c8
Ricky Stewart <ricky@cockroachlabs.com>
2021-08-04 12:19:30 -0500

    ci: separate out "check generated files up-to-date" into separate ci job
    This splits the lint build configuration roughly in half. The lint build
    config is one of the longer-running ones we see in CI, so this should
    save quite a lot of time.
    
    Release note: None

--- build/teamcity-bazel-support.sh
+++ build/teamcity-bazel-support.sh
@@ -6,9 +6,9 @@ BAZEL_IMAGE=cockroachdb/bazel:20210729-154657
 # Call `run_bazel $NAME_OF_SCRIPT` to start an appropriately-configured Docker
 # container with the `cockroachdb/bazel` image running the given script.
 #
-# Set the TEAMCITY_BAZEL_SUPPORT_LINT variable for lints -- this will mount the
-# workspace as writeable in the Docker container, will avoid setting up the
-# artifacts directory, etc.
+# Set the TEAMCITY_BAZEL_SUPPORT_GENERATE variable for scripts that generate
+# files in the workspace -- this will mount the workspace as writeable in the
+# Docker container, will avoid setting up the artifacts directory, etc.
 run_bazel() {
     if [ -z "${root:-}" ]
     then
@@ -20,7 +20,7 @@ run_bazel() {
     vols="--volume /home/agent/.bazelcache:/root/.cache/bazel"
 
     workspace_vol="--volume ${root}:/go/src/github.com/cockroachdb/cockroach"
-    if [ -z "${TEAMCITY_BAZEL_SUPPORT_LINT:-}" ]
+    if [ -z "${TEAMCITY_BAZEL_SUPPORT_GENERATE:-}" ]
     then
         artifacts_dir=$root/artifacts
         mkdir -p "$artifacts_dir"
