1d7da0dfa5e913ad58c2f5afd0094a30e77e059e
Faizan Qazi <faizan@cockroachlabs.com>
2021-08-04 00:26:34 -0400

    sql: remove table backrefs when dropping views in new schema changer
    Previously, when dropping a view in the new schema changer
    the table back refs weren't properly cleaned up. This was
    inadequate because we left back references to views inside
    table descriptors, which could lead to errors when dropping.
    To address this, this patch adds logic to clean up view back
    references to tables.
    
    Release note: None

--- pkg/sql/schemachanger/scbuild/view.go
+++ pkg/sql/schemachanger/scbuild/view.go
@@ -42,10 +42,21 @@ func (b *buildContext) maybeDropViewAndDependents(
 		DependedOnBy: make([]descpb.ID, 0, len(view.GetDependedOnBy())),
 		DependsOn:    make([]descpb.ID, 0, len(view.TableDesc().DependsOn)),
 	}
-	// Add dependencies in a order manner for reliable
+	// Add dependencies in an order manner for reliable
 	// unit testing.
 	for _, dep := range view.GetDependedOnBy() {
 		viewNode.DependedOnBy = append(viewNode.DependedOnBy, dep.ID)
+	}
+	// Clean up any back references to the tables.
+	for _, dep := range view.GetDependsOn() {
+		tableBackRef := &scpb.RelationDependedOnBy{
+			TableID:      dep,
+			DependedOnBy: viewNode.TableID,
+		}
+		if exists, _ := b.checkIfNodeExists(scpb.Target_DROP, tableBackRef); !exists {
+			b.addNode(scpb.Target_DROP,
+				tableBackRef)
+		}
 	}
 	viewNode.DependsOn = append(viewNode.DependsOn, view.TableDesc().DependsOn...)
 	sort.SliceStable(viewNode.DependsOn, func(i, j int) bool {
