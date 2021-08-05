ae6eb78ed69ecefc2b3d9cf056777b272e47fd0a
Ricky Stewart <ricky@cockroachlabs.com>
2021-08-05 16:04:07 -0500

    dev: teach dev how to lint
    Closes #68547.
    
    Release note: None

--- pkg/cmd/dev/lint.go
+++ pkg/cmd/dev/lint.go
@@ -10,14 +10,13 @@
 
 package main
 
-import (
-	"github.com/cockroachdb/errors"
-	"github.com/spf13/cobra"
-)
+import "github.com/spf13/cobra"
+
+const shortFlag = "short"
 
 // makeLintCmd constructs the subcommand used to run the specified linters.
 func makeLintCmd(runE func(cmd *cobra.Command, args []string) error) *cobra.Command {
-	return &cobra.Command{
+	lintCmd := &cobra.Command{
 		Use:   "lint",
 		Short: `Run the specified linters`,
 		Long:  `Run the specified linters.`,
@@ -26,9 +25,33 @@ func makeLintCmd(runE func(cmd *cobra.Command, args []string) error) *cobra.Comm
 		Args: cobra.NoArgs,
 		RunE: runE,
 	}
+	addCommonTestFlags(lintCmd)
+	lintCmd.Flags().Bool(shortFlag, false, "run only short lints")
+	return lintCmd
 }
 
-func (*dev) lint(cmd *cobra.Command, args []string) error {
-	// TODO(irfansharif): Flesh out the example usage patterns.
-	return errors.New("unimplemented")
+func (d *dev) lint(cmd *cobra.Command, _ []string) error {
+	ctx := cmd.Context()
+	filter := mustGetFlagString(cmd, filterFlag)
+	timeout := mustGetFlagDuration(cmd, timeoutFlag)
+	short := mustGetFlagBool(cmd, shortFlag)
+
+	var args []string
+	// NOTE the --config=test here. It's very important we compile the test binary with the
+	// appropriate stuff (gotags, etc.)
+	args = append(args, "run", "--color=yes", "--config=test", "//build/bazelutil:lint")
+	args = append(args, getConfigFlags()...)
+	args = append(args, mustGetRemoteCacheArgs(remoteCacheAddr)...)
+	args = append(args, "--", "-test.v")
+	if short {
+		args = append(args, "-test.short")
+	}
+	if timeout > 0 {
+		args = append(args, "-test.timeout", timeout.String())
+	}
+	if filter != "" {
+		args = append(args, "-test.run", filter)
+	}
+
+	return d.exec.CommandContextInheritingStdStreams(ctx, "bazel", args...)
 }
