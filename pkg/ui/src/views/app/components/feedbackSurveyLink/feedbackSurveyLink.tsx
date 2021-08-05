75ae91de37d7ffe7901ce428d2627e60482bd35b
Santamaura <santamaura@cockroachlabs.com>
2021-08-05 18:22:23 +0000

    Use short circuit evaluation in render

--- pkg/ui/src/views/app/components/feedbackSurveyLink/feedbackSurveyLink.tsx
+++ pkg/ui/src/views/app/components/feedbackSurveyLink/feedbackSurveyLink.tsx
@@ -1,3 +1,13 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
 import React from "react";
 import { useSelector } from "react-redux";
 
@@ -15,8 +25,10 @@ const FeedBackSurveyLink = () => {
   const feedbackLink = new URL("https://www.cockroachlabs.com/survey/");
   feedbackLink.searchParams.append("clusterId", clusterId);
   feedbackLink.searchParams.append("clusterVersion", singleVersion);
-
-  return clusterId && singleVersion ? (
+  if (!clusterId || !singleVersion) {
+    return <></>;
+  }
+  return (
     <div
       className="feedback-survey-link"
       onClick={() => window.open(feedbackLink.toString())}
@@ -28,8 +40,6 @@ const FeedBackSurveyLink = () => {
       />
       <div className="feedback-survey-link__title">Share feedback</div>
     </div>
-  ) : (
-    <></>
   );
 };
 
