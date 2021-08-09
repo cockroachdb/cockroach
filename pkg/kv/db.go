bc493cab2daf979e081ae71e72299310afa36b89
Alex Sarkesian <sarkesian@cockroachlabs.com>
2021-08-09 19:19:54 -0400

    kv: add option to return keys deleted in DB.DelRange operations
    Previously, there was an option to return deleted keys on Batch.DelRange
    operations, but not on DB.DelRange operations.  This change extends the
    feature to DB.DelRange operations by adding a `returnKeys` parameter,
    which will return a slice of the keys deleted if true.
    
    Release note: None
    
    Release justification: Low-risk update to new functionality.

--- pkg/kv/db.go
+++ pkg/kv/db.go
@@ -531,13 +531,17 @@ func (db *DB) Del(ctx context.Context, keys ...interface{}) error {
 
 // DelRange deletes the rows between begin (inclusive) and end (exclusive).
 //
-// TODO(pmattis): Perhaps the result should return which rows were deleted.
+// The returned []roachpb.Key will contain the keys deleted if the returnKeys
+// parameter is true, or will be nil if the parameter is false.
 //
 // key can be either a byte slice or a string.
-func (db *DB) DelRange(ctx context.Context, begin, end interface{}) error {
+func (db *DB) DelRange(
+	ctx context.Context, begin, end interface{}, returnKeys bool,
+) ([]roachpb.Key, error) {
 	b := &Batch{}
-	b.DelRange(begin, end, false)
-	return getOneErr(db.Run(ctx, b), b)
+	b.DelRange(begin, end, returnKeys)
+	r, err := getOneResult(db.Run(ctx, b), b)
+	return r.Keys, err
 }
 
 // AdminMerge merges the range containing key and the subsequent range. After
