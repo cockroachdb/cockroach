565664c78be51dc68cfc65ebd5159138365c7248
Josh Imhoff <josh@cockroachlabs.com>
2021-08-10 09:36:07 -0400

    keys: add a range-local key for probing
    This commit introduces a range-local key for probing. The key will
    only be used by probing components like kvprober. This means no
    contention with user-traffic or other CRDB components. This key also provides
    a safe place to write to in order to test write availabilty. A kvprober that
    does writes is coming soon.
    
    Release note: None.

--- pkg/keys/printer_test.go
+++ pkg/keys/printer_test.go
@@ -87,6 +87,7 @@ func TestPrettyPrint(t *testing.T) {
 		{keys.MakeRangeKeyPrefix(roachpb.RKey(tenSysCodec.TablePrefix(42))), `/Local/Range/Table/42`, revertSupportUnknown},
 		{keys.RangeDescriptorKey(roachpb.RKey(tenSysCodec.TablePrefix(42))), `/Local/Range/Table/42/RangeDescriptor`, revertSupportUnknown},
 		{keys.TransactionKey(tenSysCodec.TablePrefix(42), txnID), fmt.Sprintf(`/Local/Range/Table/42/Transaction/%q`, txnID), revertSupportUnknown},
+		{keys.RangeProbeKey(roachpb.RKey(tenSysCodec.TablePrefix(42))), `/Local/Range/Table/42/RangeProbe`, revertSupportUnknown},
 		{keys.QueueLastProcessedKey(roachpb.RKey(tenSysCodec.TablePrefix(42)), "foo"), `/Local/Range/Table/42/QueueLastProcessed/"foo"`, revertSupportUnknown},
 		{lockTableKey(keys.RangeDescriptorKey(roachpb.RKey(tenSysCodec.TablePrefix(42)))), `/Local/Lock/Intent/Local/Range/Table/42/RangeDescriptor`, revertSupportUnknown},
 		{lockTableKey(tenSysCodec.TablePrefix(111)), "/Local/Lock/Intent/Table/111", revertSupportUnknown},
