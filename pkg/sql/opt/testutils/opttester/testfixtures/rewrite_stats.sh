6dcd39083af9b1d985097162401dd3a2228715fe
Rebecca Taft <becca@cockroachlabs.com>
2021-08-10 21:40:49 -0400

    opt: add script to facilitate rewriting stats testfixtures
    This commit adds a script to make it easier to rewrite statistics
    in the testfixtures directory.
    
    Release note: None

---
+++ pkg/sql/opt/testutils/opttester/testfixtures/rewrite_stats.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -euo pipefail
+
+if [[ $# < 3 || $# > 4 ]]; then
+    cat 1>&2 <<EOF
+$0 rewrites the given testfixtures statistics file by running SHOW STATISTICS USING JSON
+for each table in the specified database.
+
+Note that this tool assumes a cockroach cluster is up and running in insecure mode, the
+specified database is already loaded, and statistics have been collected on all tables.
+
+Usage: $0 path/to/cockroach/binary database filename
+EOF
+    exit 1
+fi
+
+COCKROACH_BINARY=$1
+DATABASE=$2
+FILENAME=$3
+
+tables=($($COCKROACH_BINARY sql --insecure --format=tsv \
+ -e "USE $DATABASE; SELECT table_name FROM [SHOW TABLES] ORDER BY table_name;"))
+tables=("${tables[@]:3}")
+
+echo "Writing statistics to $FILENAME"
+echo "# Statistics for $DATABASE" > $FILENAME
+echo "" >> $FILENAME
+
+for table in "${tables[@]}"
+do
+    echo "exec-ddl" >> $FILENAME
+    echo -n "ALTER TABLE \"$table\" INJECT STATISTICS '" >> $FILENAME
+    # This command selects the statistics in JSON form for the current table. The output
+    # is passed through the following sed commands before being written to the file:
+    # 1. Remove the first two lines to leave only the JSON value.
+    # 2. Replace duplicate double quotes with one double quote.
+    # 3. Remove the double quote at the beginning of the JSON.
+    # 4. Remove the double quote at the end of the JSON.
+    # 5. Append '; to the last line.
+    $COCKROACH_BINARY sql --insecure --format=tsv \
+     -e "USE $DATABASE; SELECT jsonb_pretty(statistics) FROM [SHOW STATISTICS USING JSON FOR TABLE \"$table\"];" \
+     | sed '1,2d' | sed 's/""/"/g' | sed 's/^"\[/\[/g' | sed 's/\]"$/\]/g' | sed '$ s/$/'\'';/' >> $FILENAME
+    echo "----" >> $FILENAME
+    echo "" >> $FILENAME
+done
