c3307e085ee66fc341c32958e09388aff508ae68
Oleg Afanasyev <oleg@cockroachlabs.com>
2021-08-13 12:52:40 +0100

    storage: refactor export request params into struct
    Number of arguments to ExportMVCCToSst is too large. This diff moves
    them into a struct to improve readability.
    
    Release note: None
    
    Release justification:

--- pkg/storage/pebble_batch.go
+++ pkg/storage/pebble_batch.go
@@ -128,15 +128,7 @@ func (p *pebbleBatch) Closed() bool {
 
 // ExportMVCCToSst is part of the engine.Reader interface.
 func (p *pebbleBatch) ExportMVCCToSst(
-	ctx context.Context,
-	startKey, endKey roachpb.Key,
-	startTS, endTS hlc.Timestamp,
-	firstKeyTS hlc.Timestamp,
-	exportAllRevisions bool,
-	targetSize, maxSize uint64,
-	stopMidKey bool,
-	useTBI bool,
-	dest io.Writer,
+	ctx context.Context, exportOptions ExportOptions, dest io.Writer,
 ) (roachpb.BulkOpSummary, roachpb.Key, hlc.Timestamp, error) {
 	panic("unimplemented")
 }
