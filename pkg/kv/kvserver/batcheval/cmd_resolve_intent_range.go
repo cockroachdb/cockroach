239a22206c2b1a34c2781d562fb0d7709da40103
Erik Grinaker <grinaker@cockroachlabs.com>
2021-08-14 13:34:45 +0000

    kvserver: add `RESUME_BYTE_LIMIT` and `RESUME_INTENT_LIMIT`
    The `roachpb.ResumeReason` enum only contained the single reason
    `RESUME_KEY_LIMIT` (in addition to the `RESUME_UNKNOWN` zero value).
    This was outdated since iteration can be halted by byte limits and
    intent limits as well.
    
    This patch adds the new resume reasons `RESUME_BYTE_LIMIT` and
    `RESUME_INTENT_LIMIT`, adds `MVCCScanResult.ResumeReason` to plumb it
    through to the scan iteration, and updates relevant code.
    
    Release note: None

--- pkg/kv/kvserver/batcheval/cmd_resolve_intent_range.go
+++ pkg/kv/kvserver/batcheval/cmd_resolve_intent_range.go
@@ -58,6 +58,10 @@ func ResolveIntentRange(
 	if resumeSpan != nil {
 		update.EndKey = resumeSpan.Key
 		reply.ResumeSpan = resumeSpan
+		// The given MaxSpanRequestKeys really specifies the number of intents
+		// resolved, not the number of keys scanned. We could return
+		// RESUME_INTENT_LIMIT here, but since the given limit is a key limit we
+		// return RESUME_KEY_LIMIT for symmetry.
 		reply.ResumeReason = roachpb.RESUME_KEY_LIMIT
 	}
 
