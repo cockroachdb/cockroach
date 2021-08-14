11089a8b99420c784f952de1b5747e8182fb5f90
Erik Grinaker <grinaker@cockroachlabs.com>
2021-08-14 17:40:26 +0000

    kvserver: add `ResponseHeader.ResumeNextBytes`
    This patch adds the field `ResponseHeader.ResumeNextBytes` which will be
    poplated with the size of the next result in the `ResumeSpan`. This will
    be needed by an upcoming [parallel get/scan streaming library][1] to
    better manage its memory budget.
    
    In order to avoid additional quadratic reads when evaluating batches
    that exceed their limits and pass `-1` limits for the remaining
    requests, an additional field `AllowEmptyResumeNextBytes` is added to
    `BatchRequest.Header` to signal that it can be omitted once the first
    resume span has been generated.
    
    [1]: https://github.com/cockroachdb/cockroach/blob/master/docs/RFCS/20210617_index_lookups_memory_limits.md
    
    Release note: None
    
    Release justification: low risk, high benefit changes to existing functionality

--- pkg/kv/kvserver/batcheval/cmd_get_test.go
+++ pkg/kv/kvserver/batcheval/cmd_get_test.go
@@ -15,7 +15,9 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/storage"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/stretchr/testify/require"
@@ -47,39 +49,54 @@ func TestGetResumeSpan(t *testing.T) {
 	require.NoError(t, err)
 
 	testCases := []struct {
-		maxKeys      int64
-		targetBytes  int64
-		allowEmpty   bool
-		expectResume bool
-		expectReason roachpb.ResumeReason
+		maxKeys             int64
+		targetBytes         int64
+		allowEmpty          bool
+		avoidExcess         bool
+		allowEmptyNextBytes bool
+		expectResume        bool
+		expectReason        roachpb.ResumeReason
+		expectNextBytes     int64
 	}{
-		{maxKeys: -1, expectResume: true, expectReason: roachpb.RESUME_KEY_LIMIT},
+		{maxKeys: -1, expectResume: true, expectReason: roachpb.RESUME_KEY_LIMIT, expectNextBytes: 11},
+		{maxKeys: -1, allowEmptyNextBytes: true, expectResume: true, expectReason: roachpb.RESUME_KEY_LIMIT, expectNextBytes: 0},
 		{maxKeys: 0, expectResume: false},
 		{maxKeys: 1, expectResume: false},
 		{maxKeys: 1, allowEmpty: true, expectResume: false},
 
-		{targetBytes: -1, expectResume: true, expectReason: roachpb.RESUME_BYTE_LIMIT},
+		{targetBytes: -1, expectResume: true, expectReason: roachpb.RESUME_BYTE_LIMIT, expectNextBytes: 11},
+		{targetBytes: -1, allowEmptyNextBytes: true, expectResume: true, expectReason: roachpb.RESUME_BYTE_LIMIT, expectNextBytes: 0},
 		{targetBytes: 0, expectResume: false},
 		{targetBytes: 1, expectResume: false},
 		{targetBytes: 11, expectResume: false},
 		{targetBytes: 12, expectResume: false},
-		{targetBytes: 1, allowEmpty: true, expectResume: true, expectReason: roachpb.RESUME_BYTE_LIMIT},
-		{targetBytes: 11, allowEmpty: true, expectResume: false},
-		{targetBytes: 12, allowEmpty: true, expectResume: false},
+		{targetBytes: 1, allowEmpty: true, avoidExcess: true, expectResume: true, expectReason: roachpb.RESUME_BYTE_LIMIT, expectNextBytes: 11},
+		{targetBytes: 11, allowEmpty: true, avoidExcess: true, expectResume: false},
+		{targetBytes: 12, allowEmpty: true, avoidExcess: true, expectResume: false},
+		{targetBytes: 1, allowEmpty: true, avoidExcess: false, expectResume: false},
 
-		{maxKeys: -1, targetBytes: -1, expectResume: true, expectReason: roachpb.RESUME_KEY_LIMIT},
+		{maxKeys: -1, targetBytes: -1, expectResume: true, expectReason: roachpb.RESUME_KEY_LIMIT, expectNextBytes: 11},
+		{maxKeys: -1, targetBytes: -1, allowEmptyNextBytes: true, expectResume: true, expectReason: roachpb.RESUME_KEY_LIMIT, expectNextBytes: 0},
 		{maxKeys: 10, targetBytes: 100, expectResume: false},
 	}
 	for _, tc := range testCases {
-		name := fmt.Sprintf("maxKeys=%d targetBytes=%d allowEmpty=%t", tc.maxKeys, tc.targetBytes, tc.allowEmpty)
+		name := fmt.Sprintf("maxKeys=%d targetBytes=%d allowEmpty=%t avoidExcess=%t allowEmptyNextBytes=%t",
+			tc.maxKeys, tc.targetBytes, tc.allowEmpty, tc.avoidExcess, tc.allowEmptyNextBytes)
 		t.Run(name, func(t *testing.T) {
+			version := clusterversion.TestingBinaryVersion
+			if !tc.avoidExcess {
+				version = clusterversion.ByKey(clusterversion.AvoidExcessTargetBytes - 1)
+			}
+			settings := cluster.MakeTestingClusterSettingsWithVersions(version, clusterversion.TestingBinaryMinSupportedVersion, true)
+
 			resp := roachpb.GetResponse{}
 			_, err := Get(ctx, db, CommandArgs{
-				EvalCtx: (&MockEvalCtx{}).EvalContext(),
+				EvalCtx: (&MockEvalCtx{ClusterSettings: settings}).EvalContext(),
 				Header: roachpb.Header{
-					MaxSpanRequestKeys:    tc.maxKeys,
-					TargetBytes:           tc.targetBytes,
-					TargetBytesAllowEmpty: tc.allowEmpty,
+					MaxSpanRequestKeys:        tc.maxKeys,
+					TargetBytes:               tc.targetBytes,
+					TargetBytesAllowEmpty:     tc.allowEmpty,
+					AllowEmptyResumeNextBytes: tc.allowEmptyNextBytes,
 				},
 				Args: &roachpb.GetRequest{
 					RequestHeader: roachpb.RequestHeader{Key: key},
@@ -91,6 +108,7 @@ func TestGetResumeSpan(t *testing.T) {
 				require.NotNil(t, resp.ResumeSpan)
 				require.Equal(t, &roachpb.Span{Key: key}, resp.ResumeSpan)
 				require.Equal(t, tc.expectReason, resp.ResumeReason)
+				require.Equal(t, tc.expectNextBytes, resp.ResumeNextBytes)
 				require.Nil(t, resp.Value)
 			} else {
 				require.Nil(t, resp.ResumeSpan)
--- pkg/kv/kvserver/batcheval/cmd_reverse_scan.go
+++ pkg/kv/kvserver/batcheval/cmd_reverse_scan.go
@@ -36,6 +36,22 @@ func ReverseScan(
 	h := cArgs.Header
 	reply := resp.(*roachpb.ReverseScanResponse)
 
+	if h.AllowEmptyResumeNextBytes && (h.MaxSpanRequestKeys < 0 || h.TargetBytes < 0) {
+		// Receipt of a ScanRequest with negative MaxSpanRequestKeys or TargetBytes
+		// indicates that the request was part of a batch that has already exhausted
+		// its limit, which means that we should *not* serve the request and return
+		// a ResumeSpan for this ScanRequest.
+		//
+		// We can only do this if the caller allows us to not populate ResumeNextBytes.
+		reply.ResumeSpan = &roachpb.Span{Key: args.Key, EndKey: args.EndKey}
+		if h.MaxSpanRequestKeys < 0 {
+			reply.ResumeReason = roachpb.RESUME_KEY_LIMIT
+		} else if h.TargetBytes < 0 {
+			reply.ResumeReason = roachpb.RESUME_BYTE_LIMIT
+		}
+		return result.Result{}, nil
+	}
+
 	var res result.Result
 	var scanRes storage.MVCCScanResult
 	var err error
@@ -78,6 +94,7 @@ func ReverseScan(
 	if scanRes.ResumeSpan != nil {
 		reply.ResumeSpan = scanRes.ResumeSpan
 		reply.ResumeReason = scanRes.ResumeReason
+		reply.ResumeNextBytes = scanRes.ResumeNextBytes
 	}
 
 	if h.ReadConsistency == roachpb.READ_UNCOMMITTED {
