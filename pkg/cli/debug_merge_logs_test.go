ed02a5b7e0df8dc6240ff7512492d1b6402955ad
Cameron Nunez <cameron@cockroachlabs.com>
2021-08-16 17:44:01 -0400

    replace a panic with an error in merge-logs

--- pkg/cli/debug_merge_logs_test.go
+++ pkg/cli/debug_merge_logs_test.go
@@ -191,7 +191,12 @@ func (c testCase) run(t *testing.T) {
 			t.Fatalf("Failed to set flag to default: %v", err)
 		}
 	})
+
 	debugMergeLogsCmd.SetOut(&outBuf)
+	// Ensure that the original writer is restored when the test
+	// completes. Otherwise, subsequent tests may not see their output.
+	defer debugMergeLogsCmd.SetOut(nil)
+
 	if err := debugMergeLogsCmd.ParseFlags(c.flags); err != nil {
 		t.Fatalf("Failed to set flags: %v", err)
 	}
@@ -234,4 +239,15 @@ func TestCrdbV1DebugMergeLogs(t *testing.T) {
 	for _, c := range cases {
 		t.Run(c.name, c.run)
 	}
+}
+
+func Example_format_error() {
+	c := NewCLITest(TestCLIParams{NoServer: true})
+	defer c.Cleanup()
+
+	c.RunWithArgs([]string{"debug", "merge-logs", "testdata/merge_logs_v1/missing_format/*"})
+
+	// Output:
+	// debug merge-logs testdata/merge_logs_v1/missing_format/*
+	// ERROR: decoding format: failed to extract log file format from the log
 }
