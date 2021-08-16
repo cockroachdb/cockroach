8f5850f444476dd3358097d9c5b9183a33496028
Xin Hao Zhang <xzhang@cockroachlabs.com>
2021-08-16 13:52:40 -0400

    server: create api to query persisted stats by date range
    Release note (api change):
    
    Create _status/persistedstms endpoint to retrieve persisted
    statements from crdb_internal.statement_statistics by
    aggregated_ts range. The request expects query string
    parameters start and end, which are the date range
    in unix time.
    
    The response returned is currently the response expected
    by the get all statements api for frontend compatibility.
    
    Release justification:

---
+++ pkg/server/persisted_statement_stats.go
@@ -0,0 +1,213 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package server
+
+import (
+	"context"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/security"
+	"github.com/cockroachdb/cockroach/pkg/server/serverpb"
+	"github.com/cockroachdb/cockroach/pkg/sql"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catconstants"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlstats/persistedsqlstats/sqlstatsutil"
+	"github.com/cockroachdb/cockroach/pkg/util/encoding"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+)
+
+func (s *statusServer) CombinedStatementStats(
+	ctx context.Context, req *serverpb.CombinedStatementsStatsRequest,
+) (*serverpb.StatementsResponse, error) {
+	ctx = propagateGatewayMetadata(ctx)
+	ctx = s.AnnotateCtx(ctx)
+
+	if _, err := s.privilegeChecker.requireViewActivityPermission(ctx); err != nil {
+		return nil, err
+	}
+
+	startTime := timeutil.Unix(req.Start, 0)
+	endTime := timeutil.Unix(req.End, 0)
+
+	statements, err := collectCombinedStatements(ctx, s.internalExecutor, startTime, endTime)
+
+	transactions, err := collectCombinedTransactions(ctx, s.internalExecutor, startTime, endTime)
+
+	if err != nil {
+		return nil, err
+	}
+
+	response := &serverpb.StatementsResponse{
+		Statements:            statements,
+		Transactions:          transactions,
+		LastReset:             timeutil.Now(),
+		InternalAppNamePrefix: catconstants.InternalAppNamePrefix,
+	}
+
+	return response, nil
+}
+
+func collectCombinedStatements(
+	ctx context.Context, ie *sql.InternalExecutor, start time.Time, end time.Time,
+) ([]serverpb.StatementsResponse_CollectedStatementStatistics, error) {
+	query :=
+		`SELECT
+				fingerprint_id,
+				statistics,
+				metadata
+			FROM crdb_internal.statement_statistics
+			WHERE
+				aggregated_ts >= $1 AND aggregated_ts <= $2`
+
+	it, err := ie.QueryIteratorEx(ctx, "combined-stmts-by-interval", nil,
+		sessiondata.InternalExecutorOverride{
+			User: security.NodeUserName(),
+		}, query,
+		start,
+		end)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var statements []serverpb.StatementsResponse_CollectedStatementStatistics
+	var ok bool
+	for ok, err = it.Next(ctx); ok; ok, err = it.Next(ctx) {
+		row := it.Cur()
+
+		statementFingerprintID, err := datumToStmtFingerprintId(row[0])
+
+		if err != nil {
+			return nil, err
+		}
+
+		var stats roachpb.StatementStatistics
+		statsJson := tree.MustBeDJSON(row[1]).JSON
+		err = sqlstatsutil.DecodeStmtStatsStatisticsJSON(statsJson, &stats)
+		if err != nil {
+			return nil, err
+		}
+
+		var metadata roachpb.CollectedStatementStatistics
+		metadataJson := tree.MustBeDJSON(row[2]).JSON
+		err = sqlstatsutil.DecodeStmtStatsMetadataJSON(metadataJson, &metadata)
+		if err != nil {
+			return nil, err
+		}
+
+		stmt := serverpb.StatementsResponse_CollectedStatementStatistics{
+			Key: serverpb.StatementsResponse_ExtendedStatementStatisticsKey{
+				KeyData: metadata.Key,
+				NodeID:  roachpb.NodeID(0),
+			},
+			ID:    statementFingerprintID,
+			Stats: stats,
+		}
+
+		statements = append(statements, stmt)
+
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return statements, nil
+}
+
+func collectCombinedTransactions(
+	ctx context.Context, ie *sql.InternalExecutor, start time.Time, end time.Time,
+) ([]serverpb.StatementsResponse_ExtendedCollectedTransactionStatistics, error) {
+
+	query :=
+		`SELECT
+				app_name,
+				aggregated_ts,
+				statistics,
+				metadata
+			FROM crdb_internal.transaction_statistics
+			WHERE
+				aggregated_ts >= $1 AND aggregated_ts <= $2`
+
+	it, err := ie.QueryIteratorEx(ctx, "combined-txns-by-interval", nil,
+		sessiondata.InternalExecutorOverride{
+			User: security.NodeUserName(),
+		}, query,
+		start,
+		end)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var transactions []serverpb.StatementsResponse_ExtendedCollectedTransactionStatistics
+	var ok bool
+	for ok, err = it.Next(ctx); ok; ok, err = it.Next(ctx) {
+		row := it.Cur()
+
+		if err != nil {
+			return nil, err
+		}
+
+		app := string(tree.MustBeDString(row[0]))
+
+		aggregatedTs := tree.MustBeDTimestampTZ(row[1]).Time
+
+		var stats roachpb.TransactionStatistics
+		statsJson := tree.MustBeDJSON(row[2]).JSON
+		err = sqlstatsutil.DecodeTxnStatsStatisticsJSON(statsJson, &stats)
+
+		if err != nil {
+			return nil, err
+		}
+
+		var metadata roachpb.CollectedTransactionStatistics
+		metadataJson := tree.MustBeDJSON(row[3]).JSON
+		err = sqlstatsutil.DecodeTxnStatsMetadataJSON(metadataJson, &metadata)
+
+		if err != nil {
+			return nil, err
+		}
+
+		stmt := serverpb.StatementsResponse_ExtendedCollectedTransactionStatistics{
+			NodeID: roachpb.NodeID(0),
+			StatsData: roachpb.CollectedTransactionStatistics{
+				StatementFingerprintIDs: metadata.StatementFingerprintIDs,
+				App:                     app,
+				Stats:                   stats,
+				AggregatedTs:            aggregatedTs,
+			},
+		}
+
+		transactions = append(transactions, stmt)
+
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return transactions, nil
+}
+
+func datumToStmtFingerprintId(datum tree.Datum) (roachpb.StmtFingerprintID, error) {
+	b := []byte(tree.MustBeDBytes(datum))
+
+	_, statementFingerprintID, err := encoding.DecodeUint64Ascending(b)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return roachpb.StmtFingerprintID(statementFingerprintID), nil
+}
