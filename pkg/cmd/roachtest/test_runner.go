c1d4ca40efe84fb01a865dd6f064db27be018845
absterr08 <abby@cockroachlabs.com>
2021-08-16 16:25:34 -0400

    release: update 21.1 mapping in PredecessorVersion()
    This updates the version mapping of the `21.1` branch to point to the
    latest version (`20.2.14`).

--- pkg/cmd/roachtest/test_runner.go
+++ pkg/cmd/roachtest/test_runner.go
@@ -1217,7 +1217,7 @@ func PredecessorVersion(buildVersion version.Version) (string, error) {
 	// (see runVersionUpgrade). The same is true for adding a new key to this
 	// map.
 	verMap := map[string]string{
-		"21.1": "20.2.11",
+		"21.1": "20.2.14",
 		"20.2": "20.1.13",
 		"20.1": "19.2.11",
 		"19.2": "19.1.11",
