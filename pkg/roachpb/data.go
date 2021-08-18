dc148a4129f839260023107513eb43d0afb64272
Oleg Afanasyev <oleg@cockroachlabs.com>
2021-08-18 16:39:55 +0100

    kvclient: when retrying transaction bump ts to now
    Previously, when transaction failed with retryable error its timestamp
    was bumped by whatever event caused it like a push or a closed ts.
    Upon retry it would read at closed timestamp and will most likely
    has to make a refresh again for its writes to succeed. If transaction
    uses fixed timestamp, needs non negligible time to retry or hits any
    contention it could get into infinite retry loop. This behaviour is
    more pronounced with a faster closed timestamp refresh that is in
    place since 21.1.
    To prevent such outcome, this patch changes restart timestamp to now.
    
    Release note: None

--- pkg/roachpb/data.go
+++ pkg/roachpb/data.go
@@ -1457,9 +1457,28 @@ func PrepareTransactionForRetry(
 		txn.WriteTimestamp.Forward(tErr.PusheeTxn.WriteTimestamp)
 		txn.UpgradePriority(tErr.PusheeTxn.Priority - 1)
 	case *TransactionRetryError:
-		// Nothing to do. Transaction.Timestamp has already been forwarded to be
-		// ahead of any timestamp cache entries or newer versions which caused
-		// the restart.
+		// Transaction.Timestamp has already been forwarded to be ahead of any
+		// timestamp cache entries or newer versions which caused the restart.
+		if tErr.Reason == RETRY_SERIALIZABLE {
+			// For RETRY_SERIALIZABLE case, we want to bump timestamp further than
+			// timestamp cache.
+			// This helps transactions that had their commit timestamp fixed (See
+			// roachpb.Transaction.CommitTimestampFixed for details on when it happens)
+			// or transactions that hit read-write contention and can't bump
+			// read timestamp because of later writes.
+			// Upon retry, we want those transactions to restart on now() instead of
+			// closed ts to give them some time to complete without a need to refresh
+			// read spans yet again and possibly fail.
+			// The tradeoff here is that transactions that failed because they were
+			// waiting on locks or were slowed down in their first epoch for any other
+			// reason (e.g. lease transfers, network congestion, node failure, etc.)
+			// would have a chance to retry and succeed, but transactions that are
+			// just slow would still retry indefinitely and delay transactions that
+			// try to write to the keys this transaction reads because reads are not
+			// in the past anymore.
+			now := clock.Now()
+			txn.WriteTimestamp.Forward(now)
+		}
 	case *WriteTooOldError:
 		// Increase the timestamp to the ts at which we've actually written.
 		txn.WriteTimestamp.Forward(writeTooOldRetryTimestamp(tErr))
