d46cae0f9c6c331d52b67473108cb79df937e380
Oliver Tan <otan@cockroachlabs.com>
2021-08-18 14:08:52 +1000

    sql: introduce SET ROLE
    Release note (sql change): This commit introduces `SET ROLE`, which
    allows users with certain permissions to assume the identity of another
    user. It is worth noting that due to cross-version compatibility,
    `session_user` will always return the same as `current_user` until
    v22.1. Instead, use `session_user()` if you require this information.

--- pkg/sql/sessiondata/session_data.go
+++ pkg/sql/sessiondata/session_data.go
@@ -14,6 +14,7 @@ import (
 	"net"
 	"time"
 
+	"github.com/cockroachdb/cockroach/pkg/security"
 	"github.com/cockroachdb/cockroach/pkg/sql/sessiondatapb"
 	"github.com/cockroachdb/cockroach/pkg/util/duration"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
@@ -123,6 +124,14 @@ func (s *SessionData) GetDateStyle() pgdate.DateStyle {
 		return pgdate.DefaultDateStyle()
 	}
 	return s.DataConversionConfig.DateStyle
+}
+
+// SessionUser retrieves the session user.
+func (s *SessionData) SessionUser() security.SQLUsername {
+	if s.SessionUserProto == "" {
+		return s.User()
+	}
+	return s.SessionUserProto.Decode()
 }
 
 // LocalUnmigratableSessionData contains session parameters that cannot
