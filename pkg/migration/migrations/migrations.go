789748bbd880754f4856f2dcccb8783343d8701a
arulajmani <arulajmani@gmail.com>
2021-08-18 14:47:06 -0400

    sql: add `system.zones` for secondary tenants
    This patch bootstraps `system.zones` for secondary tenants and adds a
    migration for existing tenants. We also seed secondary tenants with the
    RANGE DEFAULT zone configuration.
    
    Release note: None

--- pkg/migration/migrations/migrations.go
+++ pkg/migration/migrations/migrations.go
@@ -121,6 +121,12 @@ var migrations = []migration.Migration{
 		interleavedTablesRemovedMigration,
 		interleavedTablesRemovedMigration,
 	),
+	migration.NewTenantMigration(
+		"add system.zones table for secondary tenants",
+		toCV(clusterversion.ZonesTableForSecondaryTenants),
+		NoPrecondition,
+		zonesTableForSecondaryTenants,
+	),
 }
 
 func init() {
