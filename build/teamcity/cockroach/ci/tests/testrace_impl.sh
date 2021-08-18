ff51a0fd56caa0be8b013d6f93d9101d14b5f032
Ricky Stewart <ricky@cockroachlabs.com>
2021-08-18 15:31:58 -0500

    bazel: add bazel `testrace` CI job
    Also some light refactoring: add a `--config race` and use that instead
    of directly referencing the `--@io_bazel_rules_go//go/config:race` flag.
    Also add `GORACE=halt_on_error=1` to the test environment when running
    under `--config race` -- this makes the behavior match up more closely
    to the `Makefile`.
    
    Closes #67145.
    
    Release note: None

---
+++ build/teamcity/cockroach/ci/tests/testrace_impl.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+# Usage: testrace_impl.sh PKG1 [PKG2 PKG3 PKG4...]
+# packages are expected to be formatted as go-style, e.g. ./pkg/cmd/bazci.
+
+bazel build //pkg/cmd/bazci --config=ci
+for pkg in "$@"
+do
+    # Query to list all affected tests.
+    pkg=${pkg#"./"}
+    if [[ $(basename $pkg) != ... ]]
+    then
+        pkg="$pkg:all"
+    fi
+    tests=$(bazel query "kind(go_test, $pkg)" --output=label)
+
+    # Run affected tests.
+    for test in "$tests"
+    do
+        $(bazel info bazel-bin)/pkg/cmd/bazci/bazci_/bazci --config ci --config race test "$test" -- \
+                               --test_env=COCKROACH_LOGIC_TESTS_SKIP=true \
+                               --test_env=GOMAXPROCS=8
+    done
+done
+
