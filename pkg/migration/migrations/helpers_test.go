a62fe103f85c6b334f5457c7c266af2a17aab7a9
Cameron Nunez <cameron@cockroachlabs.com>
2021-08-19 13:48:43 -0400

    migration,startupmigrations: make AlterSystemWebSessionsCreateIndexes migration async
    AlterSystemWebSessionsCreateIndexes is a startup migration yet it creates indexes
    on potentially large tables. This might cause significant time delays and lead to
    other issues. This patch makes this migration long-running.
    
    Release note (bug fix): The AlterSystemWebSessionsCreateIndexes migration is now
    async in case there are large tables it creates indexes over.
    
    Release justification: bug fix

--- pkg/migration/migrations/helpers_test.go
+++ pkg/migration/migrations/helpers_test.go
@@ -10,15 +10,140 @@
 
 package migrations
 
-import "github.com/cockroachdb/cockroach/pkg/sql/catalog"
+import (
+	"context"
+	gosql "database/sql"
+	"testing"
 
-func HasBackoffCols(storedTable, expectedTable catalog.TableDescriptor, col string) (bool, error) {
-	return hasColumn(storedTable, expectedTable, col)
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
+	"github.com/cockroachdb/cockroach/pkg/kv"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descs"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/tabledesc"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlutil"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	HasColumn = hasColumn
+	HasIndex  = hasIndex
+)
+
+type Schema struct {
+	// Schema name.
+	Name string
+	// Function that validates the schema.
+	ValidationFn func(catalog.TableDescriptor, catalog.TableDescriptor, string) (bool, error)
 }
 
-func HasBackoffIndex(jobsTable, expectedTable catalog.TableDescriptor, index string) (bool, error) {
-	return hasIndex(jobsTable, expectedTable, index)
+// Migrate runs cluster migration by changing the 'version' cluster setting.
+func Migrate(
+	t *testing.T, sqlDB *gosql.DB, key clusterversion.Key, done chan struct{}, expectError bool,
+) {
+	defer func() {
+		if done != nil {
+			done <- struct{}{}
+		}
+	}()
+	_, err := sqlDB.Exec(`SET CLUSTER SETTING version = $1`,
+		clusterversion.ByKey(key).String())
+	if expectError {
+		assert.Error(t, err)
+		return
+	}
+	assert.NoError(t, err)
 }
 
-const AddColsQuery = addColsQuery
-const AddIndexQuery = addIndexQuery
+// InjectLegacyTable overwrites the existing table descriptor with the previous table descriptor.
+func InjectLegacyTable(
+	ctx context.Context,
+	t *testing.T,
+	s serverutils.TestServerInterface,
+	table catalog.TableDescriptor,
+	getDeprecatedDescriptor func() *descpb.TableDescriptor,
+) {
+	err := s.CollectionFactory().(*descs.CollectionFactory).Txn(
+		ctx,
+		s.InternalExecutor().(sqlutil.InternalExecutor),
+		s.DB(),
+		func(ctx context.Context, txn *kv.Txn, descriptors *descs.Collection) error {
+			id := table.GetID()
+			tab, err := descriptors.GetMutableTableByID(ctx, txn, id, tree.ObjectLookupFlagsWithRequired())
+			if err != nil {
+				return err
+			}
+			builder := tabledesc.NewBuilder(getDeprecatedDescriptor())
+			require.NoError(t, builder.RunPostDeserializationChanges(ctx, nil))
+			tab.TableDescriptor = builder.BuildCreatedMutableTable().TableDescriptor
+			tab.Version = tab.ClusterVersion.Version + 1
+			return descriptors.WriteDesc(ctx, false /* kvTrace */, tab, txn)
+		})
+	require.NoError(t, err)
+}
+
+// ValidateSchemaExists validates whether the schema changes of the system table exist or not.
+func ValidateSchemaExists(
+	ctx context.Context,
+	t *testing.T,
+	s serverutils.TestServerInterface,
+	sqlDB *gosql.DB,
+	storedTableID descpb.ID,
+	expectedTable catalog.TableDescriptor,
+	stmts []string,
+	schemas []Schema,
+	expectExists bool,
+) {
+	// First validate by reading the columns and the index.
+	for _, stmt := range stmts {
+		_, err := sqlDB.Exec(stmt)
+		if expectExists {
+			require.NoError(
+				t, err, "expected schema to exist, but unable to query it, using statement: %s", stmt,
+			)
+		} else {
+			require.Error(
+				t, err, "expected schema to not exist, but queried it successfully, using statement: %s", stmt,
+			)
+		}
+	}
+
+	// Manually verify the table descriptor.
+	storedTable := GetTable(ctx, t, s, storedTableID)
+	str := "not have"
+	if expectExists {
+		str = "have"
+	}
+	for _, schema := range schemas {
+		updated, err := schema.ValidationFn(storedTable, expectedTable, schema.Name)
+		require.NoError(t, err)
+		require.Equal(t, expectExists, updated,
+			"expected table to %s %s", str, schema)
+	}
+}
+
+// GetTable returns the system table descriptor, reading it from storage.
+func GetTable(
+	ctx context.Context, t *testing.T, s serverutils.TestServerInterface, tableID descpb.ID,
+) catalog.TableDescriptor {
+	var table catalog.TableDescriptor
+	// Retrieve the table.
+	err := s.CollectionFactory().(*descs.CollectionFactory).Txn(
+		ctx,
+		s.InternalExecutor().(sqlutil.InternalExecutor),
+		s.DB(),
+		func(ctx context.Context, txn *kv.Txn, descriptors *descs.Collection) (err error) {
+			table, err = descriptors.GetImmutableTableByID(ctx, txn, tableID, tree.ObjectLookupFlags{
+				CommonLookupFlags: tree.CommonLookupFlags{
+					AvoidCached: true,
+					Required:    true,
+				},
+			})
+			return err
+		})
+	require.NoError(t, err)
+	return table
+}
