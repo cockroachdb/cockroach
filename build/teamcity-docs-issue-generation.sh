dd4232bfd6951ebdfaead47f5348bdac69f094ac
mikeCRL <mike@cockroachlabs.com>
2021-08-19 15:36:15 -0500

    Minor updates per review
    Release note: None
    
    Release note (<category, see below>): <what> <show> <why>

--- build/teamcity-docs-issue-generation.sh
+++ build/teamcity-docs-issue-generation.sh
@@ -8,20 +8,20 @@
 # GITHUB_API_TOKEN="replace_with_your_github_api_token"
 
 remove_files_on_exit() {
-rm -f commits.json match.json message messages.txt prnums valid
+  rm -f commits.json match.json message messages.txt prnums valid
 }
 trap remove_files_on_exit EXIT
 
 # A merge may contain multiple PRs due to craig(bot) activity
 # Get PR numbers for current merge
-curl -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/search/issues?q=sha:"$BUILD_VCS_NUMBER"+repo:cockroachdb/cockroach | jq .items[].number > prnums
+curl -s --retry 5 -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/search/issues?q=sha:"$BUILD_VCS_NUMBER"+repo:cockroachdb/cockroach | jq .items[].number > prnums
 
 # For each PR, process its commit(s)
 process_pr_commits() {
   # Get commits given PR number
-  commits=$(curl -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/repos/cockroachdb/cockroach/pulls/"$1"/commits)
+  commits=$(curl -s --retry 5 -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/repos/cockroachdb/cockroach/pulls/"$1"/commits)
   echo "$commits" > commits.json
-  pr_title=$(curl -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/repos/cockroachdb/cockroach/pulls/"$1" | jq .title)
+  pr_title=$(curl -s --retry 5 -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/repos/cockroachdb/cockroach/pulls/"$1" | jq .title)
   commitcount=$( jq length commits.json )
   commitctr=0
 
@@ -30,14 +30,14 @@ process_pr_commits() {
   do
     jq -r --arg i "$commitctr" '.[$i|tonumber].commit.message' commits.json > message
     # For each commit message, check whether it contains at least one release note
-    bool=$( jq -r --arg i "$commitctr" '.[$i|tonumber].commit.message|test("[rR]elease [nN]ote")' commits.json )
+    hasReleaseNotes=$( jq -r --arg i "$commitctr" '.[$i|tonumber].commit.message|test("[rR]elease [nN]ote")' commits.json )
     # Extract the type(s) of release note(s)
     jq -r --arg i "$commitctr" '.[$i|tonumber].commit.message|match("[rR]elease [nN]ote (?<type>.*): .*")' commits.json > match.json
     # Check whether there is at least one release note type that is not none" or "bug"-related
     jq -r '.captures | .[] | .string|test("[nN]one|[bB]ug")' match.json > valid
 
     # If conditions fulfilled, format the release note(s) and create a docs issue
-    if [ "$bool" = true ] && grep -q false valid
+    if [ "$hasReleaseNotes" = true ] && grep -q false valid
     then
       echo "https://github.com/cockroachdb/cockroach/pull/$1" > messages.txt
       echo "---" >> messages.txt
@@ -56,10 +56,10 @@ process_pr_commits() {
       # Escape double quotes
       body="${body//$'\"'/'\''"'}"
       # Get merge branch
-      merge_branch=$(curl -s -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/repos/cockroachdb/cockroach/pulls/"$1" | jq .base.ref)
+      merge_branch=$(curl -s --retry 5 -s -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/repos/cockroachdb/cockroach/pulls/"$1" | jq .base.ref)
       # Create issue
       echo "Creating issue for PR $prnum"
-      curl -X POST -H "Authorization: token $GITHUB_API_TOKEN" \
+      curl -s --retry 5 -X POST -H "Authorization: token $GITHUB_API_TOKEN" \
         -d '{"title": '"$pr_title"', "labels": ["C-product-change", '"$merge_branch"'], "body": "'"$body"'"}' \
         https://api.github.com/repos/cockroachdb/docs/issues
     fi
