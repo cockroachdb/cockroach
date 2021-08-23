2b3468789cc23d87d87c2ea7f6476bcd02734693
Jackson Owens <jackson@cockroachlabs.com>
2021-08-23 11:55:28 -0400

    sysutil: fallback to naive implementation if fallocate unsupported
    Update the Linux implementation of ResizeLargeFile to fallback to the
    naive implementation if fallocate is unsupported. Some older filesystems
    do not support fallocate, including older verisons of ZFS.
    
    Release justification: bug fix to new functionality (automatic ballast)
    Release note: None

--- pkg/util/sysutil/large_file_nonlinux.go
+++ pkg/util/sysutil/large_file_nonlinux.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The Cockroach Authors.
+// Copyright 2021 The Cockroach Authors.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt.
@@ -12,12 +12,6 @@
 
 package sysutil
 
-import (
-	"os"
-
-	"github.com/cockroachdb/errors"
-)
-
 // ResizeLargeFile resizes the file at the given path to be the provided
 // length in bytes. If no file exists at path, ResizeLargeFile creates a file.
 // All disk blocks within the new file are allocated, and there are no sparse
@@ -27,21 +21,5 @@ import (
 // On other platforms, it naively writes the specified number of bytes, which
 // can take a long time.
 func ResizeLargeFile(path string, bytes int64) error {
-	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	sixtyFourMB := make([]byte, 64<<20)
-	for bytes > 0 {
-		z := sixtyFourMB
-		if bytes < int64(len(z)) {
-			z = sixtyFourMB[:bytes]
-		}
-		if _, err := f.Write(z); err != nil {
-			return errors.Wrap(err, "write")
-		}
-		bytes -= int64(len(z))
-	}
-	return errors.Wrap(f.Sync(), "fsync")
+	return resizeLargeFileNaive(path, bytes)
 }
