7b4a042be9eea88152544c07b71527392c3b7566
Santamaura <santamaura@cockroachlabs.com>
2021-08-24 09:11:12 -0700

    wip setup basic redux flow to old hot ranges rpc

---
+++ pkg/ui/src/redux/hotRanges/hotRangesSagas.ts
@@ -0,0 +1,40 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import { cockroach } from "src/js/protos";
+import { getHotRanges } from "src/util/api";
+import { all, call, put, takeEvery } from "redux-saga/effects";
+import {
+  getHotRangesFailedAction,
+  getHotRangesSucceededAction,
+  GET_HOT_RANGES,
+} from "./hotRangesActions";
+
+import HotRangesRequest = cockroach.server.serverpb.HotRangesRequest;
+import HotRangesResponse = cockroach.server.serverpb.HotRangesResponse;
+import IHotRangesRequest = cockroach.server.serverpb.IHotRangesRequest;
+
+import { PayloadAction } from "oss/src/interfaces/action";
+
+export function* getHotRangesSaga(action: PayloadAction<IHotRangesRequest>) {
+  const hotRangesRequest = action.payload
+    ? new HotRangesRequest(action.payload)
+    : null;
+  try {
+    const resp: HotRangesResponse = yield call(getHotRanges, hotRangesRequest);
+    yield put(getHotRangesSucceededAction(resp));
+  } catch (e) {
+    yield put(getHotRangesFailedAction(e));
+  }
+}
+
+export function* hotRangesSaga() {
+  yield all([takeEvery(GET_HOT_RANGES, getHotRangesSaga)]);
+}
