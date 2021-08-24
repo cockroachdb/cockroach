f70a5d56b8c80703f4fff5dd77833329357c0e60
Oliver Tan <otan@cockroachlabs.com>
2021-08-25 06:19:25 +1000

    tests: capture more diagnostics when TestRandomSyntaxFuncCommon panics
    Refs: #63940
    
    Release justification: test only change
    
    Release note: None

--- pkg/sql/tests/rsg_test.go
+++ pkg/sql/tests/rsg_test.go
@@ -127,8 +127,21 @@ func (db *verifyFormatDB) exec(t *testing.T, ctx context.Context, sql string) er
 func (db *verifyFormatDB) execWithTimeout(
 	t *testing.T, ctx context.Context, sql string, duration time.Duration,
 ) error {
-	if err := verifyFormat(sql); err != nil {
-		db.verifyFormatErr = err
+	if err := func() (retErr error) {
+		defer func() {
+			if err := recover(); err != nil {
+				retErr = errors.CombineErrors(
+					errors.AssertionFailedf("panic executing %s: err %v", sql, err),
+					retErr,
+				)
+			}
+		}()
+		if err := verifyFormat(sql); err != nil {
+			db.verifyFormatErr = err
+			return err
+		}
+		return nil
+	}(); err != nil {
 		return err
 	}
 
