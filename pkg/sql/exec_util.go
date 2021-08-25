00a246484e40bf0209c41be158f42c2ba3b66267
Oliver Tan <otan@cockroachlabs.com>
2021-08-26 09:16:20 +1000

    sql: implement SET LOCAL on unnested transactions
    This implements SET LOCAL on simple transactions (i.e. BEGIN ...
    COMMIT/ROLLBACK, no SAVEPOINTs).
    
    Release justification: high pri feature
    
    Release note (sql change): Introduce SET LOCAL, which sets a session
    variable for the duration of the transaction.

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -2460,6 +2460,14 @@ func (f *sessionDataMutatorIterator) SetSessionDefaultIntSize(size int32) {
 	})
 }
 
+// applyOnTopMutator applies the given function on the mutator for the top
+// element on the sessiondata Stack only.
+func (f *sessionDataMutatorIterator) applyOnTopMutator(
+	applyFunc func(m *sessionDataMutator) error,
+) error {
+	return applyFunc(f.mutator(f.sds.Top()))
+}
+
 // forEachMutator iterates over each mutator over all SessionData elements
 // in the stack and applies the given function to them.
 // It is the equivalent of SET SESSION x = y.
