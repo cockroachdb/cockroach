8b7b41c039d2a2e4cb887c088b741330a4747c11
Andrei Matei <andrei@cockroachlabs.com>
2021-08-26 16:58:04 -0400

    Revert "server: dehumanize fields of the runtime stats event"
    This reverts commit 516c171638697ae36cdb8138dc29c72b892ffd33.
    
    Reverting to human-readable runtime stats log messages.
    
    Release note: Invalidates release note from https://github.com/cockroachdb/cockroach/pull/65024
    Release justification: "fix" for new functionality

--- pkg/server/status/runtime_stats_test.go
+++ pkg/server/status/runtime_stats_test.go
@@ -13,18 +13,17 @@ package status_test
 import (
 	"context"
 	"encoding/json"
-	"math"
-	"regexp"
-	"strings"
-	"testing"
-	"time"
-
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/log/eventpb"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"math"
+	"regexp"
+	"strings"
+	"testing"
+	"time"
 )
 
 var cmLogRe = regexp.MustCompile(`event_log\.go`)
@@ -58,9 +57,7 @@ func TestStructuredEventLogging(t *testing.T) {
 			continue
 		}
 		foundEntry = true
-		// TODO(knz): Remove this when crdb-v2 becomes the new format.
 		e.Message = strings.TrimPrefix(e.Message, "Structured entry:")
-		// crdb-v2 starts json with an equal sign.
 		e.Message = strings.TrimPrefix(e.Message, "=")
 		jsonPayload := []byte(e.Message)
 		var ev eventpb.RuntimeStats
