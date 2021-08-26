86ea78ecd5e7a1a6c747082dc1787f38781de18d
Oliver Tan <otan@cockroachlabs.com>
2021-08-26 20:11:53 +1000

    sql: implement SET LOCAL on unnested transactions
    This implements SET LOCAL on simple transactions (i.e. BEGIN ...
    COMMIT/ROLLBACK, no SAVEPOINTs).
    
    Release justification: high pri feature
    
    Release note (sql change): Introduce SET LOCAL, which sets a session
    variable for the duration of the transaction.

--- pkg/sql/conn_executor_exec.go
+++ pkg/sql/conn_executor_exec.go
@@ -514,7 +514,7 @@ func (ex *connExecutor) execStmtInOpenState(
 
 	case *tree.RollbackTransaction:
 		// RollbackTransaction is executed fully here; there's no plan for it.
-		ev, payload := ex.rollbackSQLTransaction(ctx)
+		ev, payload := ex.rollbackSQLTransaction(ctx, s)
 		return ev, payload, nil
 
 	case *tree.Savepoint:
@@ -816,6 +816,7 @@ func (ex *connExecutor) commitSQLTransaction(
 		return ex.makeErrEvent(err, ast)
 	}
 	ex.phaseTimes.SetSessionPhaseTime(sessionphase.SessionEndTransactionCommit, timeutil.Now())
+	ex.sessionDataStack.PopAll()
 	return eventTxnFinishCommitted{}, nil
 }
 
@@ -873,10 +874,13 @@ func (ex *connExecutor) createJobs(ctx context.Context) error {
 
 // rollbackSQLTransaction executes a ROLLBACK statement: the KV transaction is
 // rolled-back and an event is produced.
-func (ex *connExecutor) rollbackSQLTransaction(ctx context.Context) (fsm.Event, fsm.EventPayload) {
+func (ex *connExecutor) rollbackSQLTransaction(
+	ctx context.Context, stmt tree.Statement,
+) (fsm.Event, fsm.EventPayload) {
 	if err := ex.state.mu.txn.Rollback(ctx); err != nil {
 		log.Warningf(ctx, "txn rollback failed: %s", err)
 	}
+	ex.sessionDataStack.PopAll()
 	// We're done with this txn.
 	return eventTxnFinishAborted{}, nil
 }
@@ -1250,6 +1254,7 @@ func (ex *connExecutor) execStmtInNoTxnState(
 		if err != nil {
 			return ex.makeErrEvent(err, s)
 		}
+		ex.sessionDataStack.PushTopClone()
 		return eventTxnStart{ImplicitTxn: fsm.False},
 			makeEventTxnStartPayload(
 				ex.txnPriorityWithSessionDefault(s.Modes.UserPriority),
@@ -1309,7 +1314,7 @@ func (ex *connExecutor) execStmtInAbortedState(
 			// Note: Postgres replies to COMMIT of failed txn with "ROLLBACK" too.
 			res.ResetStmtType((*tree.RollbackTransaction)(nil))
 		}
-		return ex.rollbackSQLTransaction(ctx)
+		return ex.rollbackSQLTransaction(ctx, s)
 
 	case *tree.RollbackToSavepoint:
 		return ex.execRollbackToSavepointInAbortedState(ctx, s)
