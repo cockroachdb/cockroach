16ee7e24154bd66c7b6a4dcf10a0e4479723d827
Andrei Matei <andrei@cockroachlabs.com>
2021-08-27 14:43:51 -0400

    contextutil: record actual duration in TimeoutError
    Before this patch, a TimeoutError would report what the timeout was, but
    not how long the operation actually ran for. This is confusing when the
    operation actually ran for much longer than the timeout (because nobody
    was watching the ctx). This patch makes the error also report the actual
    duration.
    
    Release note: None

--- pkg/util/contextutil/timeout_error_test.go
+++ pkg/util/contextutil/timeout_error_test.go
@@ -21,13 +21,31 @@ import (
 )
 
 func TestEncodeDecode(t *testing.T) {
-	origErr := &TimeoutError{
-		operation: "hello",
-		duration:  3 * time.Minute,
-		cause:     fmt.Errorf("woo")}
-	enc := errbase.EncodeError(context.Background(), origErr)
-	newErr := errbase.DecodeError(context.Background(), enc)
+	ctx := context.Background()
+	{
+		origErr := &TimeoutError{
+			operation: "hello",
+			timeout:   3 * time.Minute,
+			cause:     fmt.Errorf("woo"),
+		}
+		enc := errbase.EncodeError(ctx, origErr)
+		newErr := errbase.DecodeError(ctx, enc)
 
-	assert.Equal(t, origErr.Error(), newErr.Error())
-	assert.Equal(t, origErr, newErr)
+		assert.Equal(t, origErr.Error(), newErr.Error())
+		assert.Equal(t, origErr, newErr)
+	}
+
+	{
+		origErr := &TimeoutError{
+			operation: "hello",
+			timeout:   3 * time.Minute,
+			took:      4 * time.Minute,
+			cause:     fmt.Errorf("woo"),
+		}
+		enc := errbase.EncodeError(ctx, origErr)
+		newErr := errbase.DecodeError(ctx, enc)
+
+		assert.Equal(t, origErr.Error(), newErr.Error())
+		assert.Equal(t, origErr, newErr)
+	}
 }
