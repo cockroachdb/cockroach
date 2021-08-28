e9eec09cb9cbaf02b491de530d03d2360ebca23b
Andrei Matei <andrei@cockroachlabs.com>
2021-08-27 20:02:40 -0400

    kvserver: cleanup liveness retry code
    This patch improves the code around retries of liveness heartbeat.
    Before this patch, if a heartbeat was canceled, it might have resulted
    in an AmbiguousResultError, which was turned into an explicitly
    retriable error, which was causing a retry but the retry was explicitly
    short-circuited on canceled ctx. The convoluted logic was
    mis-interpreted by me a fellow on first read. Also the error returned in
    this case was an uninformative "context canceled".
    
    This patch simplifies the logic by making an AmbiguousResultError caused
    by a canceled ctx not result in a retryable error in the first place.
    As a nice side-effect, this results in the original error propagating to
    the Heartbeat() caller, rather than a vanilla "context canceled"
    before.
    
    The patch also adds a test about the behavior on this kind of error,
    which was a bug in the past but not tested when it was fixed.
    
    Release note: None

--- pkg/kv/kvserver/liveness/liveness.go
+++ pkg/kv/kvserver/liveness/liveness.go
@@ -1222,12 +1222,7 @@ func (nl *NodeLiveness) RegisterCallback(cb IsLiveCallback) {
 func (nl *NodeLiveness) updateLiveness(
 	ctx context.Context, update livenessUpdate, handleCondFailed func(actual Record) error,
 ) (Record, error) {
-	for {
-		// Before each attempt, ensure that the context has not expired.
-		if err := ctx.Err(); err != nil {
-			return Record{}, err
-		}
-
+	for r := retry.StartWithCtx(ctx, base.DefaultRetryOptions()); r.Next(); {
 		nl.mu.RLock()
 		engines := nl.mu.engines
 		nl.mu.RUnlock()
@@ -1250,6 +1245,10 @@ func (nl *NodeLiveness) updateLiveness(
 		}
 		return written, nil
 	}
+	if err := ctx.Err(); err != nil {
+		return Record{}, err
+	}
+	panic("unreachable; should retry until ctx canceled")
 }
 
 func (nl *NodeLiveness) updateLivenessAttempt(
@@ -1320,6 +1319,13 @@ func (nl *NodeLiveness) updateLivenessAttempt(
 			}
 			return Record{}, handleCondFailed(Record{Liveness: actualLiveness, raw: tErr.ActualValue.TagAndDataBytes()})
 		} else if errors.HasType(err, (*roachpb.AmbiguousResultError)(nil)) {
+			// We generally want to retry ambiguous errors immediately, except if the
+			// ctx is canceled - in which case the ambiguous error is probably caused
+			// by the cancellation (and in any case it's pointless to retry with a
+			// canceled ctx).
+			if ctx.Err() != nil {
+				return Record{}, err
+			}
 			return Record{}, &errRetryLiveness{err}
 		} else if errors.HasType(err, (*roachpb.TransactionStatusError)(nil)) {
 			// 21.2 nodes can return a TransactionStatusError when they should have
