301d42059a28d640bff576947925c3d4f0f182c8
Yahor Yuzefovich <yahor@cockroachlabs.com>
2021-08-30 14:39:48 -0700

    sql: loosen up the physical planning of parallel scans
    This commit makes it so that in case we try to plan parallel
    TableReaders but encounter an error during planning, we fallback to
    having a single TableReader.
    
    Release note: None
    
    Release justification: bug fix to new functionality.

--- pkg/sql/distsql_physical_planner.go
+++ pkg/sql/distsql_physical_planner.go
@@ -1350,10 +1350,7 @@ var localScansConcurrencyLimit = settings.RegisterIntSetting(
 // for the local flow that would benefit (and is safe) to parallelize.
 func (dsp *DistSQLPlanner) maybeParallelizeLocalScans(
 	planCtx *PlanningCtx, info *tableReaderPlanningInfo,
-) (spanPartitions []SpanPartition, parallelizeLocal bool, err error) {
-	if !planCtx.isLocal {
-		return spanPartitions, parallelizeLocal, errors.AssertionFailedf("maybeParallelizeLocalScans called for a distributed plan")
-	}
+) (spanPartitions []SpanPartition, parallelizeLocal bool) {
 	// For local plans, if:
 	// - there is no required ordering,
 	// - the scan is safe to parallelize, and
@@ -1378,10 +1375,15 @@ func (dsp *DistSQLPlanner) maybeParallelizeLocalScans(
 		// Temporarily unset isLocal so that PartitionSpans divides all spans
 		// according to the respective leaseholders.
 		planCtx.isLocal = false
+		var err error
 		spanPartitions, err = dsp.PartitionSpans(planCtx, info.spans)
 		planCtx.isLocal = true
 		if err != nil {
-			return spanPartitions, parallelizeLocal, err
+			// For some reason we couldn't partition the spans - fallback to
+			// having a single TableReader.
+			spanPartitions = []SpanPartition{{dsp.gatewayNodeID, info.spans}}
+			parallelizeLocal = false
+			return spanPartitions, parallelizeLocal
 		}
 		for i := range spanPartitions {
 			spanPartitions[i].Node = dsp.gatewayNodeID
@@ -1444,7 +1446,7 @@ func (dsp *DistSQLPlanner) maybeParallelizeLocalScans(
 	} else {
 		spanPartitions = []SpanPartition{{dsp.gatewayNodeID, info.spans}}
 	}
-	return spanPartitions, parallelizeLocal, err
+	return spanPartitions, parallelizeLocal
 }
 
 func (dsp *DistSQLPlanner) planTableReaders(
@@ -1456,10 +1458,7 @@ func (dsp *DistSQLPlanner) planTableReaders(
 		err              error
 	)
 	if planCtx.isLocal {
-		spanPartitions, parallelizeLocal, err = dsp.maybeParallelizeLocalScans(planCtx, info)
-		if err != nil {
-			return err
-		}
+		spanPartitions, parallelizeLocal = dsp.maybeParallelizeLocalScans(planCtx, info)
 	} else if info.post.Limit == 0 {
 		// No hard limit - plan all table readers where their data live. Note
 		// that we're ignoring soft limits for now since the TableReader will
