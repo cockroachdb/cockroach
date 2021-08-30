68b88585e0d66be85720e9a10b432630c473cdb3
Oliver Tan <otan@cockroachlabs.com>
2021-08-30 12:17:33 +1000

    sql: make rewind work correctly with session variables
    Release justification: bug fix for old bug / new features.
    
    Release note (bug fix): Previously, doing SET in a transaction and
    having the transaction retry internally could result in the previous
    session variable being unused. For example:
    ```
    SET intervalstyle = 'postgres';
    BEGIN;
    do something with interval -- (1)
    SET intervalstyle = 'iso_8601';
    do something with interval -- (2)
    COMMIT;
    ```
    If the transaction retries at COMMIT, when attempting to re-run (1), we
    would have an interval style 'iso_8601' instead of the original
    'postgres' value. This has not been resolved.

--- pkg/sql/conn_executor.go
+++ pkg/sql/conn_executor.go
@@ -1207,10 +1207,13 @@ type connExecutor struct {
 
 		// savepoints maintains the stack of savepoints currently open.
 		savepoints savepointStack
-		// savepointsAtTxnRewindPos is a snapshot of the savepoints stack before
-		// processing the command at position txnRewindPos. When rewinding, we're
-		// going to restore this snapshot.
-		savepointsAtTxnRewindPos savepointStack
+		// rewindPosSnapshot is a snapshot of the savepoints and sessionData stack
+		// before processing the command at position txnRewindPos. When rewinding,
+		// we're going to restore this snapshot.
+		rewindPosSnapshot struct {
+			savepoints       savepointStack
+			sessionDataStack *sessiondata.Stack
+		}
 
 		// transactionStatementFingerprintIDs tracks all statement IDs that make up the current
 		// transaction. It's length is bound by the TxnStatsNumStmtFingerprintIDsToRecord
@@ -1889,7 +1892,10 @@ func (ex *connExecutor) execCmd(ctx context.Context) error {
 		}
 	case rewind:
 		ex.rewindPrepStmtNamespace(ctx)
-		ex.extraTxnState.savepoints = ex.extraTxnState.savepointsAtTxnRewindPos
+		ex.extraTxnState.savepoints = ex.extraTxnState.rewindPosSnapshot.savepoints
+		// Note we use the Replace function instead of reassigning, as there are
+		// copies of the ex.sessionDataStack in the iterators and extendedEvalContext.
+		ex.sessionDataStack.Replace(ex.extraTxnState.rewindPosSnapshot.sessionDataStack)
 		advInfo.rewCap.rewindAndUnlock(ctx)
 	case stayInPlace:
 		// Nothing to do. The same statement will be executed again.
@@ -2031,7 +2037,8 @@ func (ex *connExecutor) setTxnRewindPos(ctx context.Context, pos CmdPos) {
 	ex.extraTxnState.txnRewindPos = pos
 	ex.stmtBuf.Ltrim(ctx, pos)
 	ex.commitPrepStmtNamespace(ctx)
-	ex.extraTxnState.savepointsAtTxnRewindPos = ex.extraTxnState.savepoints.clone()
+	ex.extraTxnState.rewindPosSnapshot.savepoints = ex.extraTxnState.savepoints.clone()
+	ex.extraTxnState.rewindPosSnapshot.sessionDataStack = ex.sessionDataStack.Clone()
 }
 
 // stmtDoesntNeedRetry returns true if the given statement does not need to be
