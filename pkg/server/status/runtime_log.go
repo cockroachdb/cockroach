87072840520e725e991573479ab801ddd0d7c282
Raphael 'kena' Poss <knz@thaumogen.net>
2021-09-01 20:20:01 +0200

    status: also produce runtime stats on the DEV channel
    We found out during support cases that it is useful to see a human
    version of the runtime stats payload in the HEALTH log events.
    
    Release note: None

---
+++ pkg/server/status/runtime_log.go
@@ -0,0 +1,48 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package status
+
+import (
+	"context"
+	"strings"
+	"text/template"
+
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/log/eventpb"
+	"github.com/cockroachdb/redact"
+	humanize "github.com/dustin/go-humanize"
+)
+
+// statsTemplate formats an event of type eventpb.RuntimeStats into a
+// user-facing string.
+// TODO(knz): It may be beneficial to make this configurable at run-time.
+var statsTemplate = template.Must(template.New("runtime stats").Funcs(template.FuncMap{
+	"iBytes": humanize.IBytes,
+}).Parse(`{{iBytes .MemRSSBytes}} RSS, {{.GoroutineCount}} goroutines (stacks: {{iBytes .MemStackSysBytes}}), ` +
+	`{{iBytes .GoAllocBytes}}/{{iBytes .GoTotalBytes}} Go alloc/total{{if .GoStatsStaleness}}(stale){{end}} ` +
+	`(heap fragmentation: {{iBytes .HeapFragmentBytes}}, heap reserved: {{iBytes .HeapReservedBytes}}, heap released: {{iBytes .HeapReleasedBytes}}), ` +
+	`{{iBytes .CGoAllocBytes}}/{{iBytes .CGoTotalBytes}} CGO alloc/total ({{printf "%.1f" .CGoCallRate}} CGO/sec), ` +
+	`{{printf "%.1f" .CPUUserPercent}}/{{printf "%.1f" .CPUSysPercent}} %(u/s)time, {{printf "%.1f" .GCPausePercent}} %gc ({{.GCRunCount}}x), ` +
+	`{{iBytes .NetHostRecvBytes}}/{{iBytes .NetHostSendBytes}} (r/w)net`))
+
+func logStats(ctx context.Context, stats *eventpb.RuntimeStats) {
+	// In any case, log the structured event to its native channel (HEALTH).
+	log.StructuredEvent(ctx, stats)
+
+	// Also, log a formatted version of the structured event on the HEALTH channel,
+	// for use by humans while troubleshooting from log files.
+	var buf strings.Builder
+	if err := statsTemplate.Execute(&buf, stats); err != nil {
+		log.Warningf(ctx, "failed to render runtime stats: %v", err)
+		return
+	}
+	log.Health.Infof(ctx, "runtime stats: %s", redact.SafeString(buf.String()))
+}
