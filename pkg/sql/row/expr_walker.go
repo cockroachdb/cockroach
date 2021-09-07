d0d70dcc52426c4fa642ff8c46aa81070a629e07
Aditya Maru <adityamaru@gmail.com>
2021-09-07 16:21:01 -0700

    importccl: add support for IMPORT INTO RBR table
    This change overrides the `default_to_database_primary_region`
    and `gateway_region` to always return the primary region of the
    database of the table being imported into. This allows for
    IMPORT INTO an RBR table.
    
    Since IMPORT is a job, it does not have an associated session data
    and so it cannot rely on the planners' implementation of the regional
    operator. This change also implements the relevant methods in the
    `importRegionOperator` to allow resolution of the primary region
    of the database being imported into.
    
    Fixes: #69616
    
    Release note (sql change): IMPORT INTO regional by row tables
    is supported.
    
    Release justification: fixes for high-priority or high-severity bugs in existing functionality

--- pkg/sql/row/expr_walker.go
+++ pkg/sql/row/expr_walker.go
@@ -502,6 +502,25 @@ func importNextValByID(evalCtx *tree.EvalContext, args tree.Datums) (tree.Datum,
 	return importNextValHelper(evalCtx, c, seqMetadata)
 }
 
+// importDefaultToDatabasePrimaryRegion returns the primary region of the
+// database being imported into.
+func importDefaultToDatabasePrimaryRegion(
+	evalCtx *tree.EvalContext, _ tree.Datums,
+) (tree.Datum, error) {
+	regionConfig, err := evalCtx.Regions.CurrentDatabaseRegionConfig(evalCtx.Context)
+	if err != nil {
+		return nil, err
+	}
+	primaryRegion := regionConfig.PrimaryRegionString()
+	return tree.NewDString(primaryRegion), nil
+}
+
+// importGatewayRegion returns the primary region of the database being imported
+// into.
+func importGatewayRegion(evalCtx *tree.EvalContext, args tree.Datums) (tree.Datum, error) {
+	return importDefaultToDatabasePrimaryRegion(evalCtx, args)
+}
+
 func importNextValHelper(
 	evalCtx *tree.EvalContext, c *CellInfoAnnotation, seqMetadata *SequenceMetadata,
 ) (tree.Datum, error) {
@@ -639,6 +658,28 @@ var supportedImportFuncOverrides = map[string]*customFunc{
 				ReturnType: tree.FixedReturnType(types.Int),
 				Info:       "Advances the value of the sequence and returns the final value.",
 				Fn:         importNextValByID,
+			},
+		),
+	},
+	"default_to_database_primary_region": {
+		override: makeBuiltinOverride(
+			tree.FunDefs["default_to_database_primary_region"],
+			tree.Overload{
+				Types:      tree.ArgTypes{{"val", types.String}},
+				ReturnType: tree.FixedReturnType(types.String),
+				Info:       "Returns the primary region of the database.",
+				Fn:         importDefaultToDatabasePrimaryRegion,
+			},
+		),
+	},
+	"gateway_region": {
+		override: makeBuiltinOverride(
+			tree.FunDefs["gateway_region"],
+			tree.Overload{
+				Types:      tree.ArgTypes{},
+				ReturnType: tree.FixedReturnType(types.String),
+				Info:       "Returns the primary region of the database.",
+				Fn:         importGatewayRegion,
 			},
 		),
 	},
