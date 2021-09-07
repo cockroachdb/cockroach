697eace7618901d0cafc51551724f34b70b9e8cc
Radu Berinde <radu@cockroachlabs.com>
2021-09-07 12:36:50 -0700

    tenantcostclient: tenant-side throttling
    This commit implements the bulk of the tenant-side throttling library.
    
    High level overview of how it works (more details are in the RFC):
     - we start with an initial number of RUs (1000) that we can use
       immediately; in the meantime, we make an initial request to the
       bucket.
    
     - we request the tokens we estimate we'll need over the next 10s,
       according to an exponentially-moving average of recent consumption.
       We also factor in any token bucket debt or waiting requests.
    
     - if we are granted tokens immediately, we request more tokens when
       we have less than 10% of the granted tokens (i.e. 1s worth of
       usage) remaining.
    
     - if we are granted tokens over a period of time (i.e. a rate), we
       request more when we get within 1s of the "deadline" (unless we
       accumulate unused tokens (more than 10% of last granted).
    
    Other than the datadriven test, I did a manual end-to-end test with a
    KV workload and verified that we can throttle and un-throttle the
    workload using `crdb_internal.update_tenant_resource_limits`.
    
    The biggest things not yet done:
    
     - reasonable "backup" behavior when we can't perform token bucket
       requests (e.g. `tenant_usage` table is unavailable); I have a plan
       to return a backup rate from the server that would allow reasonable
       behavior for both free tenants and tenants with lots of paid RUs.
    
     - an end-to-end roachtest;
    
     - we account for CPU usage once per second, and take out the RUs from
       the buckets immediately. This has the potential of blocking out
       everything for a fraction of each second. This could be improved by
       increasing the sampling rate or implementing a debt-repayment
       mechanism where the debt is repaid continuously over the next
       second rather than instantly.
    
    Release note: None
    
    Release justification: multi-tenant-only code.

---
+++ pkg/ccl/multitenantccl/tenantcostclient/limiter.go
@@ -0,0 +1,162 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package tenantcostclient
+
+import (
+	"context"
+	"math"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/multitenant/tenantcostmodel"
+	"github.com/cockroachdb/cockroach/pkg/util/quotapool"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+)
+
+// limiter is used to rate-limit KV requests according to a local token bucket.
+//
+// The Wait() method is called when a KV request requires RUs. The other methods
+// are used to adjust/reconfigure/replenish the local token bucket.
+type limiter struct {
+	timeSource timeutil.TimeSource
+	tb         tokenBucket
+	qp         *quotapool.AbstractPool
+
+	// Total (rounded) RU needed for all currently waiting requests (or requests
+	// that are in the process of being fulfilled).
+	waitingRU int64
+}
+
+const initialRate = 100
+const initialRUs = 1000
+
+func (l *limiter) Init(timeSource timeutil.TimeSource, notifyChan chan<- struct{}) {
+	*l = limiter{
+		timeSource: timeSource,
+	}
+
+	l.tb.Init(timeSource.Now(), notifyChan, initialRate, initialRUs)
+
+	onWaitStartFn := func(ctx context.Context, poolName string, r quotapool.Request) {
+		atomic.AddInt64(&l.waitingRU, r.(*waitRequest).neededCeil())
+	}
+	onWaitFinishFn := func(
+		ctx context.Context, poolName string, r quotapool.Request, start time.Time,
+	) {
+		atomic.AddInt64(&l.waitingRU, -r.(*waitRequest).neededCeil())
+	}
+	l.qp = quotapool.New(
+		"tenant-side-limiter", l,
+		quotapool.WithTimeSource(timeSource),
+		quotapool.OnWaitStart(onWaitStartFn),
+		quotapool.OnWaitFinish(onWaitFinishFn),
+	)
+}
+
+// Wait removes the needed RUs from the bucket, waiting as necessary until it is
+// possible.
+func (l *limiter) Wait(ctx context.Context, needed tenantcostmodel.RU) error {
+	r := newWaitRequest(needed)
+	defer putWaitRequest(r)
+
+	return l.qp.Acquire(ctx, r)
+}
+
+// Adjust adds or removes tokens from the bucket. Tokens are added when we
+// receive more tokens from the host cluster. Tokens are removed when
+// consumption has occurred without Wait(): accounting for CPU usage and the
+// number of read bytes.
+func (l *limiter) AdjustTokens(now time.Time, delta tenantcostmodel.RU) {
+	if delta == 0 {
+		return
+	}
+	l.qp.Update(func(res quotapool.Resource) (shouldNotify bool) {
+		l.tb.AdjustTokens(now, delta)
+		// We notify the head of the queue if we added RUs, in which case that
+		// request might be allowed to go through earlier.
+		return delta > 0
+	})
+}
+
+// Reconfigure is used to call tokenBucket.Reconfigure under the pool's lock.
+func (l *limiter) Reconfigure(now time.Time, args tokenBucketReconfigureArgs) {
+	l.qp.Update(func(quotapool.Resource) (shouldNotify bool) {
+		l.tb.Reconfigure(now, args)
+		// Notify the head of the queue; the new configuration might allow that
+		// request to go through earlier.
+		return true
+	})
+}
+
+// AvailableTokens returns the current number of available RUs. This can be
+// negative if we accumulated debt or we have waiting requests.
+func (l *limiter) AvailableTokens(now time.Time) tenantcostmodel.RU {
+	var result tenantcostmodel.RU
+	l.qp.Update(func(quotapool.Resource) (shouldNotify bool) {
+		result = l.tb.AvailableTokens(now)
+		return false
+	})
+	// Subtract the RUs for currently waiting requests.
+	result -= tenantcostmodel.RU(atomic.LoadInt64(&l.waitingRU))
+	return result
+}
+
+// Update accounts for the passing of time; used to potentially trigger
+// a notification. See tokenBucket.Update.
+func (l *limiter) Update(now time.Time) {
+	l.qp.Update(func(quotapool.Resource) (shouldNotify bool) {
+		l.tb.Update(now)
+		// Nothing changed.
+		return false
+	})
+}
+
+// waitRequest is used to wait for adequate resources in the tokenBucket.
+type waitRequest struct {
+	needed tenantcostmodel.RU
+}
+
+var _ quotapool.Request = (*waitRequest)(nil)
+
+var waitRequestSyncPool = sync.Pool{
+	New: func() interface{} { return new(waitRequest) },
+}
+
+// newWaitRequest allocates a waitRequest from the sync.Pool.
+// It should be returned with putWaitRequest.
+func newWaitRequest(needed tenantcostmodel.RU) *waitRequest {
+	r := waitRequestSyncPool.Get().(*waitRequest)
+	*r = waitRequest{needed: needed}
+	return r
+}
+
+func putWaitRequest(r *waitRequest) {
+	*r = waitRequest{}
+	waitRequestSyncPool.Put(r)
+}
+
+// neededCeil returns the amount of needed RUs, rounded up to an integer.
+func (req *waitRequest) neededCeil() int64 {
+	return int64(math.Ceil(float64(req.needed)))
+}
+
+// Acquire is part of quotapool.Request.
+func (req *waitRequest) Acquire(
+	ctx context.Context, res quotapool.Resource,
+) (fulfilled bool, tryAgainAfter time.Duration) {
+	l := res.(*limiter)
+	now := l.timeSource.Now()
+	return l.tb.TryToFulfill(now, req.needed)
+}
+
+// ShouldWait is part of quotapool.Request.
+func (req *waitRequest) ShouldWait() bool {
+	return true
+}
