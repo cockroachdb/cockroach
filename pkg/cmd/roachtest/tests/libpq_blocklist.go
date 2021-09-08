5f913499fe96564a7f86a36b2503bf3e2124bc89
Rafi Shamim <rafi@cockroachlabs.com>
2021-09-08 14:07:00 -0400

    roachtest: add passing tests for lib/pq and pgjdbc
    Release justification: test only change
    Release note: None

--- pkg/cmd/roachtest/tests/libpq_blocklist.go
+++ pkg/cmd/roachtest/tests/libpq_blocklist.go
@@ -16,7 +16,40 @@ var libPQBlocklists = blocklistsForVersion{
 	{"v21.2", "libPQBlocklist21_2", libPQBlocklist21_2, "libPQIgnorelist21_2", libPQIgnorelist21_2},
 }
 
-var libPQBlocklist21_2 = libPQBlocklist21_1
+var libPQBlocklist21_2 = blocklist{
+	"pq.ExampleConnectorWithNoticeHandler":           "unknown",
+	"pq.TestBinaryByteSliceToInt":                    "41547",
+	"pq.TestBinaryByteSlicetoUUID":                   "41547",
+	"pq.TestConnListen":                              "41522",
+	"pq.TestConnUnlisten":                            "41522",
+	"pq.TestConnUnlistenAll":                         "41522",
+	"pq.TestConnectorWithNoticeHandler_Simple":       "unknown",
+	"pq.TestConnectorWithNotificationHandler_Simple": "unknown",
+	"pq.TestContextCancelBegin":                      "41335",
+	"pq.TestContextCancelExec":                       "41335",
+	"pq.TestContextCancelQuery":                      "41335",
+	"pq.TestCopyFromError":                           "5807",
+	"pq.TestCopyInRaiseStmtTrigger":                  "5807",
+	"pq.TestCopyInTypes":                             "5807",
+	"pq.TestCopyRespLoopConnectionError":             "5807",
+	"pq.TestEncodeAndParseTs":                        "41563",
+	"pq.TestInfinityTimestamp":                       "41564",
+	"pq.TestIssue186":                                "41558",
+	"pq.TestIssue196":                                "41689",
+	"pq.TestIssue282":                                "12137",
+	"pq.TestListenerFailedQuery":                     "41522",
+	"pq.TestListenerListen":                          "41522",
+	"pq.TestListenerReconnect":                       "41522",
+	"pq.TestListenerUnlisten":                        "41522",
+	"pq.TestListenerUnlistenAll":                     "41522",
+	"pq.TestNotifyExtra":                             "41522",
+	"pq.TestPing":                                    "35897",
+	"pq.TestQueryRowBugWorkaround":                   "5807",
+	"pq.TestReconnect":                               "35897",
+	"pq.TestRowsColumnTypes":                         "41688",
+	"pq.TestRuntimeParameters":                       "12137",
+	"pq.TestStringWithNul":                           "26366",
+}
 
 var libPQBlocklist21_1 = libPQBlocklist20_2
 
