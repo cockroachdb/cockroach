f9a8de94833be8db5ecc0f6b05b85b7dd878f3ed
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2021-09-07 22:29:18 -0400

    sql: implement pg_is_other_temp_schema
    Needed for #69010.
    
    This commit implements the `pg_is_other_temp_schema` builtin function.
    `pg_is_other_temp_schema` returns true if the given OID is the OID of another
    session's temporary schema. This can be useful, for example, to exclude other
    sessions' temporary tables from a catalog display.
    
    Release note (sql change): The pg_is_other_temp_schema builtin function is now
    supported, which returns whether the given OID is the OID of another session's
    temporary schema.
    
    Release justification: None, waiting for v22.1.

--- pkg/bench/rttanalysis/orm_queries_bench_test.go
+++ pkg/bench/rttanalysis/orm_queries_bench_test.go
@@ -197,6 +197,22 @@ WHERE
               CREATE TEMP TABLE t(a int primary key, b int)`,
 			Stmt: `SELECT pg_my_temp_schema() FROM generate_series(1, 10)`,
 		},
+
+		{
+			Name: "pg_is_other_temp_schema",
+			Setup: `SET experimental_enable_temp_tables = true;
+              CREATE TEMP TABLE t(a int primary key, b int)`,
+			Stmt: `SELECT nspname, pg_is_other_temp_schema(oid) FROM
+               (SELECT * FROM pg_namespace WHERE nspname = 'public') n`,
+		},
+
+		{
+			Name: "pg_is_other_temp_schema multiple times",
+			Setup: `SET experimental_enable_temp_tables = true;
+              CREATE TEMP TABLE t(a int primary key, b int)`,
+			Stmt: `SELECT nspname, pg_is_other_temp_schema(oid) FROM
+               (SELECT * FROM pg_namespace LIMIT 5) n`,
+		},
 	}
 
 	RunRoundTripBenchmark(b, tests)
