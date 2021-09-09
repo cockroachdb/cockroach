608ce381a210c11d8561ed705adc9ecc5b74a8be
Raphael 'kena' Poss <knz@thaumogen.net>
2021-09-09 14:51:34 +0200

    util/log: make it possible to customize the log config in unit tests
    Prior to this patch, the log configuration used in unit tests was
    fixed and could not be customized.
    
    This patch changes this and introduces a new command-line flag
    `-test-log-config` that enables the person running a test to override
    the unit test log config.
    
    This feature is limited though: it does not support setting redaction
    markers on the stderr sink. This is because test configurations
    always force panics and other go runtime errors to go to stderr, and
    this is incompatible with redaction marker.
    
    Here is an example run:
    
    ```
    make test PKG=./pkg/sql TESTS=TestAsOfTime \
      TESTFLAGS='-v -show-logs -test-log-config="sinks: {stderr: {filter: WARNING}}"'
    ```
    
    Note that certain tests require and force a specific log
    configuration, and thus will ignore `-test-log-config`. An example is
    sql.TestTelemetryLogging.
    
    Release justification: non-production code changes
    
    Release note: None

---
+++ pkg/util/log/test_log_scope_test.go
@@ -0,0 +1,162 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package log
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/util/log/channel"
+	"github.com/cockroachdb/cockroach/pkg/util/log/logconfig"
+	"github.com/cockroachdb/cockroach/pkg/util/log/severity"
+	"github.com/stretchr/testify/require"
+)
+
+// TestDefaultTestLogConfig checks the logging configuration used in tests.
+func TestDefaultTestLogConfig(t *testing.T) {
+	t.Run("no-dir", func(t *testing.T) {
+		// No directory.
+		testConfig, err := getTestConfig(nil)
+		require.NoError(t, err)
+
+		// Capturing fd2 writes is disabled.
+		require.False(t, testConfig.CaptureFd2.Enable)
+		// Test configs without a directory send all channels at severity INFO to stderr.
+		for _, c := range logconfig.SelectAllChannels() {
+			require.True(t, testConfig.Sinks.Stderr.Channels.AllChannels.HasChannel(c))
+			require.Equal(t, severity.INFO, testConfig.Sinks.Stderr.Channels.ChannelFilters[c])
+		}
+		// Output to files disabled.
+		require.Equal(t, 0, len(testConfig.Sinks.FileGroups))
+
+		// Redaction markers disabled on stderr.
+		require.False(t, *testConfig.Sinks.Stderr.Redactable)
+	})
+	t.Run("with-dir", func(t *testing.T) {
+		fakeDir := "/foo"
+		testConfig, err := getTestConfig(&fakeDir)
+		require.NoError(t, err)
+
+		// Capturing fd2 writes is disabled in any case, because we want
+		// panic in tests to show up on the test's stderr.
+		require.False(t, testConfig.CaptureFd2.Enable)
+		// Test configs without a directory send all channels at severity ERROR to stderr.
+		for _, c := range logconfig.SelectAllChannels() {
+			require.True(t, testConfig.Sinks.Stderr.Channels.AllChannels.HasChannel(c))
+			require.Equal(t, severity.ERROR, testConfig.Sinks.Stderr.Channels.ChannelFilters[c])
+		}
+		// Redaction markers disabled on stderr.
+		require.False(t, *testConfig.Sinks.Stderr.Redactable)
+
+		// Output to files enabled, with just 1 file sink for all channels at severity INFO.
+		require.Equal(t, 1, len(testConfig.Sinks.FileGroups))
+		def, ok := testConfig.Sinks.FileGroups["default"]
+		require.True(t, ok)
+		require.Equal(t, fakeDir, *def.Dir)
+		for _, c := range logconfig.SelectAllChannels() {
+			require.True(t, def.Channels.AllChannels.HasChannel(c))
+			require.Equal(t, severity.INFO, def.Channels.ChannelFilters[c])
+		}
+		require.True(t, *def.Redactable)
+	})
+}
+
+// TestDefaultTestLogConfig checks that the logging configuration used
+// in tests can be overidden using logging.testLogConfig.
+func TestOverrideTestLogConfig(t *testing.T) {
+	const fakeConf = `
+file-defaults:
+   redactable: false
+sinks:
+  file-groups:
+     storage-errors:
+        channels: STORAGE
+        filter: ERROR
+     force-file:
+        dir: /force-dir
+        channels: HEALTH
+  stderr:
+      channels: STORAGE
+      filter: WARNING
+`
+	t.Run("no-dir", func(t *testing.T) {
+		defer func(prev string) { logging.testLogConfig = prev }(logging.testLogConfig)
+		logging.testLogConfig = fakeConf
+		testConfig, err := getTestConfig(nil)
+		require.NoError(t, err)
+
+		// Capturing fd2 writes is disabled by default.
+		// This is different from the default config for servers, where
+		// fd2 capture is enabled by default.
+		require.False(t, testConfig.CaptureFd2.Enable)
+
+		// Check the forced stderr config.
+		require.Equal(t, 1, len(testConfig.Sinks.Stderr.Channels.ChannelFilters))
+		require.Equal(t, severity.WARNING, testConfig.Sinks.Stderr.Channels.ChannelFilters[channel.STORAGE])
+
+		// Since the file default directory is undefined in this case,
+		// only the force-file group remains. The other one was elided
+		// because it misses a directory.
+		require.Equal(t, 1, len(testConfig.Sinks.FileGroups))
+		fc, ok := testConfig.Sinks.FileGroups["force-file"]
+		require.True(t, ok)
+		require.Equal(t, "/force-dir", *fc.Dir)
+		require.Equal(t, 1, len(fc.Channels.ChannelFilters))
+		require.Equal(t, severity.INFO, fc.Channels.ChannelFilters[channel.HEALTH])
+		require.False(t, *fc.Redactable) // check inheritance
+	})
+
+	t.Run("with-dir", func(t *testing.T) {
+		defer func(prev string) { logging.testLogConfig = prev }(logging.testLogConfig)
+		logging.testLogConfig = fakeConf
+		fakeDir := "/foo"
+		testConfig, err := getTestConfig(&fakeDir)
+		require.NoError(t, err)
+
+		// Capturing fd2 writes is disabled by default.
+		// This is different from the default config for servers, where
+		// fd2 capture is enabled by default.
+		require.False(t, testConfig.CaptureFd2.Enable)
+
+		// Check the forced stderr config.
+		require.Equal(t, 1, len(testConfig.Sinks.Stderr.Channels.ChannelFilters))
+		require.Equal(t, severity.WARNING, testConfig.Sinks.Stderr.Channels.ChannelFilters[channel.STORAGE])
+
+		// We have a file directory so both sinks are active.
+		// We also get the implicit default target for the DEV channel, since
+		// our config did not define it.
+		require.Equal(t, 3, len(testConfig.Sinks.FileGroups))
+		fc1, ok := testConfig.Sinks.FileGroups["force-file"]
+		require.True(t, ok)
+		require.Equal(t, 1, len(fc1.Channels.ChannelFilters))
+		require.Equal(t, severity.INFO, fc1.Channels.ChannelFilters[channel.HEALTH])
+		require.False(t, *fc1.Redactable) // check inheritance
+		require.Equal(t, "/force-dir", *fc1.Dir)
+		fc2, ok := testConfig.Sinks.FileGroups["storage-errors"]
+		require.True(t, ok)
+		require.Equal(t, 1, len(fc2.Channels.ChannelFilters))
+		require.Equal(t, severity.ERROR, fc2.Channels.ChannelFilters[channel.STORAGE])
+		require.False(t, *fc1.Redactable) // check inheritance
+		require.Equal(t, "/foo", *fc2.Dir)
+		def, ok := testConfig.Sinks.FileGroups["default"]
+		require.True(t, ok)
+		require.Equal(t, fakeDir, *def.Dir)
+		for _, c := range logconfig.SelectAllChannels() {
+			if c == channel.HEALTH || c == channel.STORAGE {
+				// These two have been selected away by the two other sinks already.
+				require.False(t, def.Channels.AllChannels.HasChannel(c))
+			} else {
+				require.True(t, def.Channels.AllChannels.HasChannel(c))
+				require.Equal(t, severity.INFO, def.Channels.ChannelFilters[c])
+			}
+		}
+		require.False(t, *def.Redactable)
+	})
+}
