cockroachdb/cockroach

commit 56685ab9a09d46916a08bc1664edae70dccdf40e

sql: fix portals after exhausting rows
Previously, we would erroneously restart the execution from the very
beginning of empty, unclosed portals after they have been fully
exhausted when we should be returning no rows or an error in such
scenarios. This is now fixed by tracking whether a portal is exhausted
or not and intercepting the calls to `execStmt` when the conn executor
state machine is in an open state.

Note that the current solution has known deviations from Postgres:
- when attempting to execute portals of statements that don't return row
sets, on the second and consequent attempt PG returns an error while we
are silently doing nothing (meaning we do not run the statement at all
and return 0 rows)
- we incorrectly populate "command tag" field of pgwire messages of some
rows-returning statements after the portal suspension (for example,
a suspended UPDATE RETURNING in PG will return the total count of "rows
affected" while we will return the count since the last suspension).

These deviations are deemed acceptable since this commit fixes a much
worse problem - re-executing an exhausted portal (which could be
a mutation meaning, previously, we could have executed a mutation
multiple times).

The reasons for why this commit does not address these deviations are:
- Postgres has a concept of "portal strategy"
(see https://github.com/postgres/postgres/blob/2f9661311b83dc481fc19f6e3bda015392010a40/src/include/utils/portal.h#L89).
- Postgres has a concept of "command" type (these are things like
SELECTs, UPDATEs, INSERTs, etc,
see https://github.com/postgres/postgres/blob/1aac32df89eb19949050f6f27c268122833ad036/src/include/nodes/nodes.h#L672).

CRDB doesn't have these concepts, and without them any attempt to
simulate Postgres results in a very error-prone and brittle code.

Release note (bug fix): Previously, CockroachDB would erroneously
restart the execution of empty, unclosed portals after they have been
fully exhausted, and this has been fixed.

pkg/sql/conn_executor.go
@@ -632,12 +632,13 @@ func (s *Server) newConnExecutor(
 	ex.phaseTimes[sessionInit] = timeutil.Now()
 	ex.extraTxnState.prepStmtsNamespace = prepStmtNamespace{
 		prepStmts: make(map[string]*PreparedStatement),
-		portals:   make(map[string]*PreparedPortal),
+		portals:   make(map[string]PreparedPortal),
 	}
 	ex.extraTxnState.prepStmtsNamespaceAtTxnRewindPos = prepStmtNamespace{
 		prepStmts: make(map[string]*PreparedStatement),
-		portals:   make(map[string]*PreparedPortal),
+		portals:   make(map[string]PreparedPortal),
 	}
+	ex.extraTxnState.prepStmtsNamespaceMemAcc = ex.sessionMon.MakeBoundAccount()
 	ex.extraTxnState.descCollection = descs.MakeCollection(s.cfg.LeaseManager,
 		s.cfg.Settings, s.dbCache.getDatabaseCache(), s.dbCache)
 	ex.extraTxnState.txnRewindPos = -1
@@ -854,8 +855,13 @@ func (ex *connExecutor) close(ctx context.Context, closeType closeType) {
 
 	if closeType != panicClose {
 		// Close all statements and prepared portals.
-		ex.extraTxnState.prepStmtsNamespace.resetTo(ctx, prepStmtNamespace{})
-		ex.extraTxnState.prepStmtsNamespaceAtTxnRewindPos.resetTo(ctx, prepStmtNamespace{})
+		ex.extraTxnState.prepStmtsNamespace.resetTo(
+			ctx, prepStmtNamespace{}, &ex.extraTxnState.prepStmtsNamespaceMemAcc,
+		)
+		ex.extraTxnState.prepStmtsNamespaceAtTxnRewindPos.resetTo(
+			ctx, prepStmtNamespace{}, &ex.extraTxnState.prepStmtsNamespaceMemAcc,
+		)
+		ex.extraTxnState.prepStmtsNamespaceMemAcc.Close(ctx)
 	}
 
 	if ex.sessionTracing.Enabled() {
@@ -989,6 +995,12 @@ type connExecutor struct {
 		// txnRewindPos is advanced. Prepared statements are shared between the two
 		// collections, but these collections are periodically reconciled.
 		prepStmtsNamespaceAtTxnRewindPos prepStmtNamespace
+
+		// prepStmtsNamespaceMemAcc is the memory account that is shared
+		// between prepStmtsNamespace and prepStmtsNamespaceAtTxnRewindPos. It
+		// tracks the memory usage of portals and should be closed upon
+		// connExecutor's closure.
+		prepStmtsNamespaceMemAcc mon.BoundAccount
 
 		// onTxnFinish (if non-nil) will be called when txn is finished (either
 		// committed or aborted). It is set when txn is started but can remain
@@ -1136,7 +1148,7 @@ type prepStmtNamespace struct {
 	// session.
 	prepStmts map[string]*PreparedStatement
 	// portals contains the portals currently available on the session.
-	portals map[string]*PreparedPortal
+	portals map[string]PreparedPortal
 }
 
 func (ns prepStmtNamespace) String() string {
@@ -1156,13 +1168,15 @@ func (ns prepStmtNamespace) String() string {
 // references are release and all the to's references are duplicated.
 //
 // An empty `to` can be passed in to deallocate everything.
-func (ns *prepStmtNamespace) resetTo(ctx context.Context, to prepStmtNamespace) {
+func (ns *prepStmtNamespace) resetTo(
+	ctx context.Context, to prepStmtNamespace, prepStmtsNamespaceMemAcc *mon.BoundAccount,
+) {
 	for name, p := range ns.prepStmts {
 		p.decRef(ctx)
 		delete(ns.prepStmts, name)
 	}
 	for name, p := range ns.portals {
-		p.decRef(ctx)
+		p.decRef(ctx, prepStmtsNamespaceMemAcc, name)
 		delete(ns.portals, name)
 	}
 
@@ -1193,7 +1207,7 @@ func (ex *connExecutor) resetExtraTxnState(
 
 	// Close all portals.
 	for name, p := range ex.extraTxnState.prepStmtsNamespace.portals {
-		p.decRef(ctx)
+		p.decRef(ctx, &ex.extraTxnState.prepStmtsNamespaceMemAcc, name)
 		delete(ex.extraTxnState.prepStmtsNamespace.portals, name)
 	}
 
@@ -1398,10 +1412,11 @@ func (ex *connExecutor) execCmd(ctx context.Context) error {
 		// ExecPortal is handled like ExecStmt, except that the placeholder info
 		// is taken from the portal.
 
-		portal, ok := ex.extraTxnState.prepStmtsNamespace.portals[tcmd.Name]
+		portalName := tcmd.Name
+		portal, ok := ex.extraTxnState.prepStmtsNamespace.portals[portalName]
 		if !ok {
 			err := pgerror.Newf(
-				pgcode.InvalidCursorName, "unknown portal %q", tcmd.Name)
+				pgcode.InvalidCursorName, "unknown portal %q", portalName)
 			ev = eventNonRetriableErr{IsCommit: fsm.False}
 			payload = eventNonRetriableErrPayload{err: err}
 			res = ex.clientComm.CreateErrorResult(pos)
@@ -1441,18 +1456,11 @@ func (ex *connExecutor) execCmd(ctx context.Context) error {
 			pos, portal.OutFormats,
 			ex.sessionData.DataConversion,
 			tcmd.Limit,
-			tcmd.Name,
+			portalName,
 			ex.implicitTxn(),
 		)
 		res = stmtRes
-		curStmt := Statement{
-			Statement:     portal.Stmt.Statement,
-			Prepared:      portal.Stmt,
-			ExpectedTypes: portal.Stmt.Columns,
-			AnonymizedStr: portal.Stmt.AnonymizedStr,
-		}
-		stmtCtx := withStatement(ctx, ex.curStmt)
-		ev, payload, err = ex.execStmt(stmtCtx, curStmt, stmtRes, pinfo)
+		ev, payload, err = ex.execPortal(ctx, portal, portalName, stmtRes, pinfo)
 		if err != nil {
 			return err
 		}
@@ -1858,14 +1866,16 @@ func (ex *connExecutor) generateID() ClusterWideID {
 // prepStmtsNamespaceAtTxnRewindPos that's not part of prepStmtsNamespace.
 func (ex *connExecutor) commitPrepStmtNamespace(ctx context.Context) {
 	ex.extraTxnState.prepStmtsNamespaceAtTxnRewindPos.resetTo(
-		ctx, ex.extraTxnState.prepStmtsNamespace)
+		ctx, ex.extraTxnState.prepStmtsNamespace, &ex.extraTxnState.prepStmtsNamespaceMemAcc,
+	)
 }
 
 // commitPrepStmtNamespace deallocates everything in prepStmtsNamespace that's
 // not part of prepStmtsNamespaceAtTxnRewindPos.
 func (ex *connExecutor) rewindPrepStmtNamespace(ctx context.Context) {
 	ex.extraTxnState.prepStmtsNamespace.resetTo(
-		ctx, ex.extraTxnState.prepStmtsNamespaceAtTxnRewindPos)
+		ctx, ex.extraTxnState.prepStmtsNamespaceAtTxnRewindPos, &ex.extraTxnState.prepStmtsNamespaceMemAcc,
+	)
 }
 
 // getRewindTxnCapability checks whether rewinding to the position previously
@@ -2580,7 +2590,9 @@ func (ps connExPrepStmtsAccessor) Delete(ctx context.Context, name string) bool 
 
 // DeleteAll is part of the preparedStatementsAccessor interface.
 func (ps connExPrepStmtsAccessor) DeleteAll(ctx context.Context) {
-	ps.ex.extraTxnState.prepStmtsNamespace.resetTo(ctx, prepStmtNamespace{})
+	ps.ex.extraTxnState.prepStmtsNamespace.resetTo(
+		ctx, prepStmtNamespace{}, &ps.ex.extraTxnState.prepStmtsNamespaceMemAcc,
+	)
 }
 
 // contextStatementKey is an empty type for the handle associated with the

pkg/sql/conn_executor_prepare.go
@@ -358,9 +358,7 @@ func (ex *connExecutor) execBind(
 	}
 
 	// Create the new PreparedPortal.
-	if err := ex.addPortal(
-		ctx, portalName, bindCmd.PreparedStatementName, ps, qargs, columnFormatCodes,
-	); err != nil {
+	if err := ex.addPortal(ctx, portalName, ps, qargs, columnFormatCodes); err != nil {
 		return retErr(err)
 	}
 
@@ -379,7 +377,6 @@ func (ex *connExecutor) execBind(
 func (ex *connExecutor) addPortal(
 	ctx context.Context,
 	portalName string,
-	psName string,
 	stmt *PreparedStatement,
 	qargs tree.QueryArguments,
 	outFormats []pgwirebase.FormatCode,
@@ -388,13 +385,24 @@ func (ex *connExecutor) addPortal(
 		panic(errors.AssertionFailedf("portal already exists: %q", portalName))
 	}
 
-	portal, err := ex.newPreparedPortal(ctx, portalName, stmt, qargs, outFormats)
+	portal, err := ex.makePreparedPortal(ctx, portalName, stmt, qargs, outFormats)
 	if err != nil {
 		return err
 	}
 
 	ex.extraTxnState.prepStmtsNamespace.portals[portalName] = portal
 	return nil
+}
+
+// exhaustPortal marks a portal with the provided name as "exhausted" and
+// panics if there is no portal with such name.
+func (ex *connExecutor) exhaustPortal(portalName string) {
+	portal, ok := ex.extraTxnState.prepStmtsNamespace.portals[portalName]
+	if !ok {
+		panic(errors.AssertionFailedf("portal %s doesn't exist", portalName))
+	}
+	portal.exhausted = true
+	ex.extraTxnState.prepStmtsNamespace.portals[portalName] = portal
 }
 
 func (ex *connExecutor) deletePreparedStmt(ctx context.Context, name string) {
@@ -411,7 +419,7 @@ func (ex *connExecutor) deletePortal(ctx context.Context, name string) {
 	if !ok {
 		return
 	}
-	portal.decRef(ctx)
+	portal.decRef(ctx, &ex.extraTxnState.prepStmtsNamespaceMemAcc, name)
 	delete(ex.extraTxnState.prepStmtsNamespace.portals, name)
 }
 

pkg/sql/prepared_stmt.go
@@ -39,7 +39,7 @@ const (
 // PreparedStatement is a SQL statement that has been parsed and the types
 // of arguments and results have been determined.
 //
-// Note that PreparedStatemts maintain a reference counter internally.
+// Note that PreparedStatements maintain a reference counter internally.
 // References need to be registered with incRef() and de-registered with
 // decRef().
 type PreparedStatement struct {
@@ -111,7 +111,7 @@ type preparedStatementsAccessor interface {
 	// The method returns true if statement with that name was found and removed,
 	// false otherwise.
 	Delete(ctx context.Context, name string) bool
-	// DeleteAll removes all prepared statements and portals from the coolection.
+	// DeleteAll removes all prepared statements and portals from the collection.
 	DeleteAll(ctx context.Context)
 }
 
@@ -129,36 +129,35 @@ type PreparedPortal struct {
 
 	// refCount keeps track of the number of references to this PreparedStatement.
 	// New references are registered through incRef().
-	// Once refCount hits 0 (through calls to decRef()), the following memAcc is
-	// closed.
 	// Most references are being held by portals created from this prepared
 	// statement.
 	refCount int
 
-	memAcc mon.BoundAccount
+	// exhausted tracks whether this portal has already been fully exhausted,
+	// meaning that any additional attempts to execute it should return no
+	// rows.
+	exhausted bool
 }
 
-// newPreparedPortal creates a new PreparedPortal.
+// makePreparedPortal creates a new PreparedPortal.
 //
 // incRef() doesn't need to be called on the result.
 // When no longer in use, the PreparedPortal needs to be decRef()d.
-func (ex *connExecutor) newPreparedPortal(
+func (ex *connExecutor) makePreparedPortal(
 	ctx context.Context,
 	name string,
 	stmt *PreparedStatement,
 	qargs tree.QueryArguments,
 	outFormats []pgwirebase.FormatCode,
-) (*PreparedPortal, error) {
-	portal := &PreparedPortal{
+) (PreparedPortal, error) {
+	portal := PreparedPortal{
 		Stmt:       stmt,
 		Qargs:      qargs,
 		OutFormats: outFormats,
-		memAcc:     ex.sessionMon.MakeBoundAccount(),
 		refCount:   1,
 	}
-	sz := int64(uintptr(len(name)) + unsafe.Sizeof(*portal))
-	if err := portal.memAcc.Grow(ctx, sz); err != nil {
-		return nil, err
+	if err := ex.extraTxnState.prepStmtsNamespaceMemAcc.Grow(ctx, portal.size(name)); err != nil {
+		return PreparedPortal{}, err
 	}
 	// The portal keeps a reference to the PreparedStatement, so register it.
 	stmt.incRef(ctx)
@@ -167,19 +166,27 @@ func (ex *connExecutor) newPreparedPortal(
 
 func (p *PreparedPortal) incRef(ctx context.Context) {
 	if p.refCount <= 0 {
-		log.Fatal(ctx, "corrupt PreparedStatement refcount")
+		log.Fatal(ctx, "corrupt PreparedPortal refcount")
 	}
 	p.refCount++
 }
 
-func (p *PreparedPortal) decRef(ctx context.Context) {
+// decRef decrements the number of references to this portal. If the refCount
+// reaches 0, then the memory account is shrunk accordingly.
+func (p *PreparedPortal) decRef(
+	ctx context.Context, prepStmtsNamespaceMemAcc *mon.BoundAccount, portalName string,
+) {
 	if p.refCount <= 0 {
-		log.Fatal(ctx, "corrupt PreparedPrepared refcount")
+		log.Fatal(ctx, "corrupt PreparedPortal refcount")
 	}
 	p.refCount--
 
 	if p.refCount == 0 {
-		p.memAcc.Close(ctx)
+		prepStmtsNamespaceMemAcc.Shrink(ctx, p.size(portalName))
 		p.Stmt.decRef(ctx)
 	}
+}
+
+func (p PreparedPortal) size(portalName string) int64 {
+	return int64(uintptr(len(portalName)) + unsafe.Sizeof(p))
 }

pkg/sql/conn_executor_exec.go
@@ -137,6 +137,57 @@ func (ex *connExecutor) recordFailure() {
 	ex.metrics.EngineMetrics.FailureCount.Inc(1)
 }
 
+// execPortal executes a prepared statement. It is a "wrapper" around execStmt
+// method that is performing additional work to track portal's state.
+func (ex *connExecutor) execPortal(
+	ctx context.Context,
+	portal PreparedPortal,
+	portalName string,
+	stmtRes CommandResult,
+	pinfo *tree.PlaceholderInfo,
+) (ev fsm.Event, payload fsm.EventPayload, err error) {
+	curStmt := Statement{
+		Statement:     portal.Stmt.Statement,
+		Prepared:      portal.Stmt,
+		ExpectedTypes: portal.Stmt.Columns,
+		AnonymizedStr: portal.Stmt.AnonymizedStr,
+	}
+	stmtCtx := withStatement(ctx, ex.curStmt)
+	switch ex.machine.CurState().(type) {
+	case stateOpen:
+		// We're about to execute the statement in an open state which
+		// could trigger the dispatch to the execution engine. However, it
+		// is possible that we're trying to execute an already exhausted
+		// portal - in such a scenario we should return no rows, but the
+		// execution engine is not aware of that and would run the
+		// statement as if it was running it for the first time. In order
+		// to prevent such behavior, we check whether the portal has been
+		// exhausted and execute the statement only if it hasn't. If it has
+		// been exhausted, then we do not dispatch the query for execution,
+		// but connExecutor will still perform necessary state transitions
+		// which will emit CommandComplete messages and alike (in a sense,
+		// by not calling execStmt we "execute" the portal in such a way
+		// that it returns 0 rows).
+		// Note that here we deviate from Postgres which returns an error
+		// when attempting to execute an exhausted portal which has a
+		// StatementType() different from "Rows".
+		if !portal.exhausted {
+			ev, payload, err = ex.execStmt(stmtCtx, curStmt, stmtRes, pinfo)
+			// Portal suspension is supported via a "side" state machine
+			// (see pgwire.limitedCommandResult for details), so when
+			// execStmt returns, we know for sure that the portal has been
+			// executed to completion, thus, it is exhausted.
+			// Note that the portal is considered exhausted regardless of
+			// the fact whether an error occurred or not - if it did, we
+			// still don't want to re-execute the portal from scratch.
+			ex.exhaustPortal(portalName)
+		}
+	default:
+		ev, payload, err = ex.execStmt(stmtCtx, curStmt, stmtRes, pinfo)
+	}
+	return
+}
+
 // execStmtInOpenState executes one statement in the context of the session's
 // current transaction.
 // It handles statements that affect the transaction state (BEGIN, COMMIT)
@@ -476,10 +527,10 @@ func (ex *connExecutor) execStmtInOpenState(
 	// is re-configured, re-set etc without using NewTxnWithSteppingEnabled().
 	//
 	// Manually hunting them down and calling ConfigureStepping() each
-	// time would be error prone (and increase the change that a future
+	// time would be error prone (and increase the chance that a future
 	// change would forget to add the call).
 	//
-	// TODO(andrei): really the code should be re-architectued to ensure
+	// TODO(andrei): really the code should be rearchitected to ensure
 	// that all uses of SQL execution initialize the client.Txn using a
 	// single/common function. That would be where the stepping mode
 	// gets enabled once for all SQL statements executed "underneath".

pkg/sql/conn_executor_internal_test.go
@@ -38,8 +38,8 @@ import (
 )
 
 // Test portal implicit destruction. Unless destroying a portal is explicitly
-// requested, portals live until the end of the transaction in which
-// they'recreated. If they're created outside of a transaction, they live until
+// requested, portals live until the end of the transaction in which they're
+// created. If they're created outside of a transaction, they live until
 // the next transaction completes (so until the next statement is executed,
 // which statement is expected to be the execution of the portal that was just
 // created).

commit 397f0817d60583a1374eff0bac60a87227c153db

sql: fix some issues around the portal state management
This commit fixes some issues around the handling of portals in the
prepared statements namespace. Namely:
- it fixes the reference counting that was broken in 56685ab (where we
switched to storing the portal by value instead of by pointer, but the
refcounts need to be shared between all references which wasn't true
in that commit) by entirely removing the ref counting
- it fixes the missing memory accounting for a copy of the portal
- it deletes all of the portals from `prepStmtsNamespaceAtTxnRewindPos`
in `resetExtraTxnState` whenever a txn commits or rolls back
- it adjusts a comment on the method deleting a prepared statement to
match the implementation.

Release note: None.

Release justification: fix to a long-standing bug.

pkg/sql/conn_executor.go
@@ -1020,11 +1020,11 @@ func (ex *connExecutor) close(ctx context.Context, closeType closeType) {
 
 	if closeType != panicClose {
 		// Close all statements and prepared portals.
-		ex.extraTxnState.prepStmtsNamespace.resetTo(
-			ctx, prepStmtNamespace{}, &ex.extraTxnState.prepStmtsNamespaceMemAcc,
+		ex.extraTxnState.prepStmtsNamespace.resetToEmpty(
+			ctx, &ex.extraTxnState.prepStmtsNamespaceMemAcc,
 		)
-		ex.extraTxnState.prepStmtsNamespaceAtTxnRewindPos.resetTo(
-			ctx, prepStmtNamespace{}, &ex.extraTxnState.prepStmtsNamespaceMemAcc,
+		ex.extraTxnState.prepStmtsNamespaceAtTxnRewindPos.resetToEmpty(
+			ctx, &ex.extraTxnState.prepStmtsNamespaceMemAcc,
 		)
 		ex.extraTxnState.prepStmtsNamespaceMemAcc.Close(ctx)
 	}
@@ -1414,7 +1414,9 @@ type prepStmtNamespace struct {
 	// prepStmts contains the prepared statements currently available on the
 	// session.
 	prepStmts map[string]*PreparedStatement
-	// portals contains the portals currently available on the session.
+	// portals contains the portals currently available on the session. Note
+	// that PreparedPortal.accountForCopy needs to be called if a copy of a
+	// PreparedPortal is retained.
 	portals map[string]PreparedPortal
 }
 
@@ -1437,19 +1439,30 @@ func (ns prepStmtNamespace) String() string {
 	return sb.String()
 }
 
+// resetToEmpty deallocates the prepStmtNamespace.
+func (ns *prepStmtNamespace) resetToEmpty(
+	ctx context.Context, prepStmtsNamespaceMemAcc *mon.BoundAccount,
+) {
+	// No errors could occur since we're releasing the resources.
+	_ = ns.resetTo(ctx, prepStmtNamespace{}, prepStmtsNamespaceMemAcc)
+}
+
 // resetTo resets a namespace to equate another one (`to`). All the receiver's
-// references are release and all the to's references are duplicated.
+// references are released and all the to's references are duplicated.
 //
 // An empty `to` can be passed in to deallocate everything.
+//
+// It can only return an error if we've reached the memory limit and had to make
+// a copy of portals.
 func (ns *prepStmtNamespace) resetTo(
 	ctx context.Context, to prepStmtNamespace, prepStmtsNamespaceMemAcc *mon.BoundAccount,
-) {
+) error {
 	for name, p := range ns.prepStmts {
 		p.decRef(ctx)
 		delete(ns.prepStmts, name)
 	}
 	for name, p := range ns.portals {
-		p.decRef(ctx, prepStmtsNamespaceMemAcc, name)
+		p.close(ctx, prepStmtsNamespaceMemAcc, name)
 		delete(ns.portals, name)
 	}
 
@@ -1458,9 +1471,12 @@ func (ns *prepStmtNamespace) resetTo(
 		ns.prepStmts[name] = ps
 	}
 	for name, p := range to.portals {
-		p.incRef(ctx)
+		if err := p.accountForCopy(ctx, prepStmtsNamespaceMemAcc, name); err != nil {
+			return err
+		}
 		ns.portals[name] = p
 	}
+	return nil
 }
 
 // resetExtraTxnState resets the fields of ex.extraTxnState when a transaction
@@ -1480,12 +1496,16 @@ func (ex *connExecutor) resetExtraTxnState(ctx context.Context, ev txnEvent) err
 
 	// Close all portals.
 	for name, p := range ex.extraTxnState.prepStmtsNamespace.portals {
-		p.decRef(ctx, &ex.extraTxnState.prepStmtsNamespaceMemAcc, name)
+		p.close(ctx, &ex.extraTxnState.prepStmtsNamespaceMemAcc, name)
 		delete(ex.extraTxnState.prepStmtsNamespace.portals, name)
 	}
 
 	switch ev {
 	case txnCommit, txnRollback:
+		for name, p := range ex.extraTxnState.prepStmtsNamespaceAtTxnRewindPos.portals {
+			p.close(ctx, &ex.extraTxnState.prepStmtsNamespaceMemAcc, name)
+			delete(ex.extraTxnState.prepStmtsNamespaceAtTxnRewindPos.portals, name)
+		}
 		ex.extraTxnState.savepoints.clear()
 		// After txn is finished, we need to call onTxnFinish (if it's non-nil).
 		if ex.extraTxnState.onTxnFinish != nil {
@@ -1900,7 +1920,9 @@ func (ex *connExecutor) execCmd(ctx context.Context) error {
 			return err
 		}
 	case rewind:
-		ex.rewindPrepStmtNamespace(ctx)
+		if err := ex.rewindPrepStmtNamespace(ctx); err != nil {
+			return err
+		}
 		ex.extraTxnState.savepoints = ex.extraTxnState.rewindPosSnapshot.savepoints
 		// Note we use the Replace function instead of reassigning, as there are
 		// copies of the ex.sessionDataStack in the iterators and extendedEvalContext.
@@ -1975,7 +1997,9 @@ func (ex *connExecutor) updateTxnRewindPosMaybe(
 				"unexpected advance code when starting a txn: %s",
 				errors.Safe(advInfo.code))
 		}
-		ex.setTxnRewindPos(ctx, nextPos)
+		if err := ex.setTxnRewindPos(ctx, nextPos); err != nil {
+			return err
+		}
 	} else {
 		// See if we can advance the rewind point even if this is not the point
 		// where the transaction started. We can do that after running a special
@@ -2027,7 +2051,9 @@ func (ex *connExecutor) updateTxnRewindPosMaybe(
 				panic(errors.AssertionFailedf("unsupported cmd: %T", cmd))
 			}
 			if canAdvance {
-				ex.setTxnRewindPos(ctx, pos+1)
+				if err := ex.setTxnRewindPos(ctx, pos+1); err != nil {
+					return err
+				}
 			}
 		}
 	}
@@ -2038,16 +2064,16 @@ func (ex *connExecutor) updateTxnRewindPosMaybe(
 //
 // All statements with lower position in stmtBuf (if any) are removed, as we
 // won't ever need them again.
-func (ex *connExecutor) setTxnRewindPos(ctx context.Context, pos CmdPos) {
+func (ex *connExecutor) setTxnRewindPos(ctx context.Context, pos CmdPos) error {
 	if pos <= ex.extraTxnState.txnRewindPos {
 		panic(errors.AssertionFailedf("can only move the  txnRewindPos forward. "+
 			"Was: %d; new value: %d", ex.extraTxnState.txnRewindPos, pos))
 	}
 	ex.extraTxnState.txnRewindPos = pos
 	ex.stmtBuf.Ltrim(ctx, pos)
-	ex.commitPrepStmtNamespace(ctx)
 	ex.extraTxnState.rewindPosSnapshot.savepoints = ex.extraTxnState.savepoints.clone()
 	ex.extraTxnState.rewindPosSnapshot.sessionDataStack = ex.sessionDataStack.Clone()
+	return ex.commitPrepStmtNamespace(ctx)
 }
 
 // stmtDoesntNeedRetry returns true if the given statement does not need to be
@@ -2196,16 +2222,16 @@ func (ex *connExecutor) generateID() ClusterWideID {
 
 // commitPrepStmtNamespace deallocates everything in
 // prepStmtsNamespaceAtTxnRewindPos that's not part of prepStmtsNamespace.
-func (ex *connExecutor) commitPrepStmtNamespace(ctx context.Context) {
-	ex.extraTxnState.prepStmtsNamespaceAtTxnRewindPos.resetTo(
+func (ex *connExecutor) commitPrepStmtNamespace(ctx context.Context) error {
+	return ex.extraTxnState.prepStmtsNamespaceAtTxnRewindPos.resetTo(
 		ctx, ex.extraTxnState.prepStmtsNamespace, &ex.extraTxnState.prepStmtsNamespaceMemAcc,
 	)
 }
 
 // commitPrepStmtNamespace deallocates everything in prepStmtsNamespace that's
 // not part of prepStmtsNamespaceAtTxnRewindPos.
-func (ex *connExecutor) rewindPrepStmtNamespace(ctx context.Context) {
-	ex.extraTxnState.prepStmtsNamespace.resetTo(
+func (ex *connExecutor) rewindPrepStmtNamespace(ctx context.Context) error {
+	return ex.extraTxnState.prepStmtsNamespace.resetTo(
 		ctx, ex.extraTxnState.prepStmtsNamespaceAtTxnRewindPos, &ex.extraTxnState.prepStmtsNamespaceMemAcc,
 	)
 }
@@ -3173,8 +3199,8 @@ func (ps connExPrepStmtsAccessor) Delete(ctx context.Context, name string) bool 
 
 // DeleteAll is part of the preparedStatementsAccessor interface.
 func (ps connExPrepStmtsAccessor) DeleteAll(ctx context.Context) {
-	ps.ex.extraTxnState.prepStmtsNamespace.resetTo(
-		ctx, prepStmtNamespace{}, &ps.ex.extraTxnState.prepStmtsNamespaceMemAcc,
+	ps.ex.extraTxnState.prepStmtsNamespace.resetToEmpty(
+		ctx, &ps.ex.extraTxnState.prepStmtsNamespaceMemAcc,
 	)
 }
 

pkg/sql/conn_executor_prepare.go
@@ -488,7 +488,7 @@ func (ex *connExecutor) deletePortal(ctx context.Context, name string) {
 	if !ok {
 		return
 	}
-	portal.decRef(ctx, &ex.extraTxnState.prepStmtsNamespaceMemAcc, name)
+	portal.close(ctx, &ex.extraTxnState.prepStmtsNamespaceMemAcc, name)
 	delete(ex.extraTxnState.prepStmtsNamespace.portals, name)
 }
 

pkg/sql/prepared_stmt.go
@@ -115,23 +115,14 @@ type preparedStatementsAccessor interface {
 	DeleteAll(ctx context.Context)
 }
 
-// PreparedPortal is a PreparedStatement that has been bound with query arguments.
-//
-// Note that PreparedPortals maintain a reference counter internally.
-// References need to be registered with incRef() and de-registered with
-// decRef().
+// PreparedPortal is a PreparedStatement that has been bound with query
+// arguments.
 type PreparedPortal struct {
 	Stmt  *PreparedStatement
 	Qargs tree.QueryArguments
 
 	// OutFormats contains the requested formats for the output columns.
 	OutFormats []pgwirebase.FormatCode
-
-	// refCount keeps track of the number of references to this PreparedStatement.
-	// New references are registered through incRef().
-	// Most references are being held by portals created from this prepared
-	// statement.
-	refCount int
 
 	// exhausted tracks whether this portal has already been fully exhausted,
 	// meaning that any additional attempts to execute it should return no
@@ -141,8 +132,7 @@ type PreparedPortal struct {
 
 // makePreparedPortal creates a new PreparedPortal.
 //
-// incRef() doesn't need to be called on the result.
-// When no longer in use, the PreparedPortal needs to be decRef()d.
+// accountForCopy() doesn't need to be called on the prepared statement.
 func (ex *connExecutor) makePreparedPortal(
 	ctx context.Context,
 	name string,
@@ -154,37 +144,25 @@ func (ex *connExecutor) makePreparedPortal(
 		Stmt:       stmt,
 		Qargs:      qargs,
 		OutFormats: outFormats,
-		refCount:   1,
 	}
-	if err := ex.extraTxnState.prepStmtsNamespaceMemAcc.Grow(ctx, portal.size(name)); err != nil {
-		return PreparedPortal{}, err
-	}
-	// The portal keeps a reference to the PreparedStatement, so register it.
-	stmt.incRef(ctx)
-	return portal, nil
+	return portal, portal.accountForCopy(ctx, &ex.extraTxnState.prepStmtsNamespaceMemAcc, name)
 }
 
-func (p *PreparedPortal) incRef(ctx context.Context) {
-	if p.refCount <= 0 {
-		log.Fatal(ctx, "corrupt PreparedPortal refcount")
-	}
-	p.refCount++
+// accountForCopy updates the state to account for the copy of the
+// PreparedPortal (p is the copy).
+func (p PreparedPortal) accountForCopy(
+	ctx context.Context, prepStmtsNamespaceMemAcc *mon.BoundAccount, portalName string,
+) error {
+	p.Stmt.incRef(ctx)
+	return prepStmtsNamespaceMemAcc.Grow(ctx, p.size(portalName))
 }
 
-// decRef decrements the number of references to this portal. If the refCount
-// reaches 0, then the memory account is shrunk accordingly.
-func (p *PreparedPortal) decRef(
+// close closes this portal.
+func (p PreparedPortal) close(
 	ctx context.Context, prepStmtsNamespaceMemAcc *mon.BoundAccount, portalName string,
 ) {
-	if p.refCount <= 0 {
-		log.Fatal(ctx, "corrupt PreparedPortal refcount")
-	}
-	p.refCount--
-
-	if p.refCount == 0 {
-		prepStmtsNamespaceMemAcc.Shrink(ctx, p.size(portalName))
-		p.Stmt.decRef(ctx)
-	}
+	prepStmtsNamespaceMemAcc.Shrink(ctx, p.size(portalName))
+	p.Stmt.decRef(ctx)
 }
 
 func (p PreparedPortal) size(portalName string) int64 {