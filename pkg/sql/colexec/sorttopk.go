d0dfb187e5c08bf22e467c221d47558435fbc53c
rharding6373 <rharding6373@users.noreply.github.com>
2021-09-13 09:05:52 -0700

    Templatize TopKSorter
    Release justification:
    Release note (<category, see below>): <what> <show> <why>

--- pkg/sql/colexec/sorttopk.go
+++ pkg/sql/colexec/sorttopk.go
@@ -11,7 +11,6 @@
 package colexec
 
 import (
-	"container/heap"
 	"context"
 
 	"github.com/cockroachdb/cockroach/pkg/col/coldata"
@@ -71,6 +70,8 @@ func NewTopKSorter(
 		}
 	}
 	return base, nil
+
+	//return newTopKSorter(allocator, input, inputTypes, orderingCols, matchLen, k, maxOutputBatchMemSize)
 }
 
 var _ colexecop.BufferingInMemoryOperator = &topKSorter{}
@@ -186,125 +187,6 @@ func (t *topKSorter) Reset(ctx context.Context) {
 	}
 }
 
-//gcassert:inline
-func (t *topKSorter) nextBatch() {
-	t.inputBatch = t.Input.Next()
-	if t.hasPartialOrder {
-		t.distincterInput.SetBatch(t.inputBatch)
-		t.distincter.Next()
-	}
-	t.firstUnprocessedTupleIdx = 0
-}
-
-// spool reads in the entire input, always storing the top K rows it has seen so
-// far in o.topK. This is done by maintaining a max heap of indices into o.topK.
-// Whenever we encounter a row which is smaller than the max row in the heap,
-// we replace the max with that row.
-//
-// After all the input has been read, we pop everything off the heap to
-// determine the final output ordering. This is used in emit() to output the rows
-// in sorted order.
-func (t *topKSorter) spool() {
-	// Fill up t.topK by spooling up to K rows from the input.
-	// We don't need to check for distinct groups until after we have filled
-	// t.topK.
-	// TODO(harding): We could emit the first N < K rows if the N rows are in one
-	// or more distinct and complete groups, and then use a K-N size heap to find
-	// the remaining top K-N rows.
-	t.nextBatch()
-	remainingRows := t.k
-	groupId := 0
-	for remainingRows > 0 && t.inputBatch.Length() > 0 {
-		fromLength := t.inputBatch.Length()
-		if remainingRows < uint64(t.inputBatch.Length()) {
-			// t.topK will be full after this batch.
-			fromLength = int(remainingRows)
-		}
-		// Find the group id for each tuple just added to topK.
-		if t.hasPartialOrder {
-			sel := t.inputBatch.Selection()
-			for i, k := 0, t.topK.Length(); i < fromLength; i, k = i+1, k+1 {
-				idx := i
-				if sel != nil {
-					idx = sel[i]
-				}
-				if t.distinctOutput[idx] {
-					groupId++
-				}
-				t.group[k] = groupId
-			}
-		}
-		t.firstUnprocessedTupleIdx = fromLength
-		t.topK.AppendTuples(t.inputBatch, 0 /* startIdx */, fromLength)
-		remainingRows -= uint64(fromLength)
-		if fromLength == t.inputBatch.Length() {
-			t.nextBatch()
-		}
-	}
-	t.updateComparators(topKVecIdx, t.topK)
-	// Initialize the heap.
-	if cap(t.heap) < t.topK.Length() {
-		t.heap = make([]int, t.topK.Length())
-	} else {
-		t.heap = t.heap[:t.topK.Length()]
-	}
-	for i := range t.heap {
-		t.heap[i] = i
-	}
-	heap.Init(t)
-	// Read the remainder of the input. Whenever a row is less than the heap max,
-	// swap it in. When we find the end of the group, we can finish reading the
-	// input.
-	groupDone := false
-	for t.inputBatch.Length() > 0 {
-		t.updateComparators(inputVecIdx, t.inputBatch)
-		sel := t.inputBatch.Selection()
-		t.allocator.PerformOperation(
-			t.topK.ColVecs(),
-			func() {
-				for i := t.firstUnprocessedTupleIdx; i < t.inputBatch.Length(); i++ {
-					idx := i
-					if sel != nil {
-						idx = sel[i]
-					}
-					// If this is a distinct group, we have already found the top K input,
-					// so we can stop comparing the rest of this and subsequent batches.
-					if t.hasPartialOrder && t.distinctOutput[idx] {
-						groupDone = true
-						return
-					}
-					maxIdx := t.heap[0]
-					groupMaxIdx := 0
-					if t.hasPartialOrder {
-						groupMaxIdx = t.group[maxIdx]
-					}
-					if t.compareRow(inputVecIdx, topKVecIdx, idx, maxIdx, groupId, groupMaxIdx) < 0 {
-						for j := range t.inputTypes {
-							t.comparators[j].set(inputVecIdx, topKVecIdx, idx, maxIdx)
-						}
-						if t.hasPartialOrder {
-							t.group[maxIdx] = groupId
-						}
-						heap.Fix(t, 0)
-					}
-				}
-				t.firstUnprocessedTupleIdx = t.inputBatch.Length()
-			},
-		)
-		if groupDone {
-			break
-		}
-		t.nextBatch()
-	}
-	// t.topK now contains the top K rows unsorted. Create a selection vector
-	// which specifies the rows in sorted order by popping everything off the
-	// heap. Note that it's a max heap so we need to fill the selection vector in
-	// reverse.
-	t.sel = make([]int, t.topK.Length())
-	for i := 0; i < t.topK.Length(); i++ {
-		t.sel[len(t.sel)-i-1] = heap.Pop(t).(int)
-	}
-}
 func (t *topKSorter) emit() coldata.Batch {
 	toEmit := t.topK.Length() - t.emitted
 	if toEmit == 0 {
@@ -335,30 +217,7 @@ func (t *topKSorter) emit() coldata.Batch {
 	t.emitted += toEmit
 	return t.output
 }
-func (t *topKSorter) compareRow(
-	vecIdx1, vecIdx2 int, rowIdx1, rowIdx2 int, groupIdx1, groupIdx2 int,
-) int {
-	for i := range t.orderingCols {
-		if t.hasPartialOrder && i < t.matchLen && groupIdx1 == groupIdx2 {
-			// If the tuples being compared are in the same group, we only need to
-			// compare the columns that are not already ordered.
-			continue
-		}
-		info := t.orderingCols[i]
-		res := t.comparators[info.ColIdx].compare(vecIdx1, vecIdx2, rowIdx1, rowIdx2)
-		if res != 0 {
-			switch d := info.Direction; d {
-			case execinfrapb.Ordering_Column_ASC:
-				return res
-			case execinfrapb.Ordering_Column_DESC:
-				return -res
-			default:
-				colexecerror.InternalError(errors.AssertionFailedf("unexpected direction value %d", d))
-			}
-		}
-	}
-	return 0
-}
+
 func (t *topKSorter) updateComparators(vecIdx int, batch coldata.Batch) {
 	for i := range t.inputTypes {
 		t.comparators[i].setVec(vecIdx, batch.ColVec(i))
@@ -396,17 +255,6 @@ func (t *topKSorter) ExportBuffered(colexecop.Operator) coldata.Batch {
 // Len is part of heap.Interface and is only meant to be used internally.
 func (t *topKSorter) Len() int {
 	return len(t.heap)
-}
-
-// Less is part of heap.Interface and is only meant to be used internally.
-func (t *topKSorter) Less(i, j int) bool {
-	groupi := 0
-	groupj := 0
-	if t.hasPartialOrder {
-		groupi = t.group[t.heap[i]]
-		groupj = t.group[t.heap[j]]
-	}
-	return t.compareRow(topKVecIdx, topKVecIdx, t.heap[i], t.heap[j], groupi, groupj) > 0
 }
 
 // Swap is part of heap.Interface and is only meant to be used internally.
