3e83ba4117dbb40505f4a2c4f97b334a15e175b0
Alex Sarkesian <sarkesian@cockroachlabs.com>
2021-09-13 17:52:11 -0400

    storage: update mvcc metadata to conform to SafeFormatter interface
    This change updates our previous `MVCCMetadata` formatting methods from
    using `fmt.Formatter` and `errors.SafeMessage` to a more streamlined and
    redactable output following the `SafeFormatter` interface.
    
    Release note: None

--- pkg/storage/enginepb/mvcc_test.go
+++ pkg/storage/enginepb/mvcc_test.go
@@ -11,13 +11,13 @@
 package enginepb_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/storage/enginepb"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
+	"github.com/cockroachdb/redact"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -68,7 +68,7 @@ func TestFormatMVCCMetadata(t *testing.T) {
 		` ts=0,1 del=false klen=123 vlen=456 raw=/BYTES/foo ih={{11 /BYTES/bar}{22 /BYTES/baz}}` +
 		` mergeTs=<nil> txnDidNotUpdateMeta=true`
 
-	if str := fmt.Sprintf("%+v", meta); str != expV {
+	if str := redact.Sprintf("%+v", meta).StripMarkers(); str != expV {
 		t.Errorf(
 			"expected meta: %s\n"+
 				"got:           %s",
