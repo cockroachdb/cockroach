eaeb57428e6f5ba9a94e6a3dd61898653b6493e0
Bob Vawter <bob@vawter.org>
2021-09-15 12:13:06 -0400

    pgwire: Add dynamic user identity mapping
    This commit adds support for mapping incoming system identities (e.g.: GSSAPI
    or X.509 principals) to database usernames. The implementation follows in the
    same pattern as the HBA configuration. Namely, there is a new cluster setting
    server.identity_map.configuration which contains data compatible with the
    pg_ident.conf file.
    
    When a new connection is made, the relevant HBA configuration line is selected
    and the "map" option is used to select an identity-map ruleset. The system
    identity is mapped to a database username and authentication proceeds using the
    HBA entry method.
    
    CockroachDB explicitly disallows the use of the "map" entry with the "password"
    HBA method. When using the "cert-password" method, an identity mapping is
    applied only for incoming connections that present a client certificate;
    password-based connections will not be subject to any identity remapping.
    
    The root user is never subject to identity remapping and cannot be locked out
    by a bad identity-map configuration. The effective HBA configuration always has
    a system-defined rule that matches the root user as the first rule. This
    baked-in rule does not contain a "map" option, thus bypassing the mapper code
    entirely.
    
    Users must already have access to the ADMIN role in order to change cluster
    settings, so this change does not, in and of itself, present
    privilege-escalation concerns. Futhermore, if the identity mapping results in
    the "root", "node", or other reserved usernames, an error will be returned to
    the client.
    
    For pedantry's sake, the term "identity" is preferred over "username" within
    the code, since not all identities are necessarily what would be considered
    usernames.
    
    Fixes: #47196
    
    See also: https://www.postgresql.org/docs/13/auth-username-maps.html
    
    Release note (security update): The server.identity_map.configuration cluster
    setting allows a pg_ident.conf file to be uploaded to support dynamically
    remapping system identities (e.g.: Kerberos or X.509 principals) to database
    usernames. This supports use-cases where X.509 certificates must conform to
    organizational standards that mandate the use of Common Names that are not
    valid SQL usernames (e.g.: CN=carl@example.com => carl). Mapping rules that
    result in the root, node, or other reserved usernames will result in an error
    when the client attempts to connect.
    
    Release note (security update): The client_authentication_info structured log
    message provides a new "SystemIdentity" field with the client-provided system
    identity. The existing "User" field will be populated after any Host-Based
    Authentication rules have been selected and applied, which may include a
    system-identity to database-username mapping.
    
    Release note (security update): GSSAPI-based authentication can now use either
    the HBA "map" option or "include_realm=0" to map the incoming princpal to a
    database username. Existing configurations will operate unchanged, however
    operators are encouraged to migrate from "include_realm=0" to "map" to avoid
    ambiguity in deployments where multiple realms are present.
    
    Release note (security update): Incoming system identities are normalized to
    lower-case before they are evaluated against any active identity-mapping HBA
    configuration. For example, an incoming GSSAPI principal "carl@EXAMPLE.COM"
    would only be matched by rules such as "example carl@example.com carl" or
    "example /^(.*)@example.com$ \1".

---
+++ pkg/sql/pgwire/auth_behaviors.go
@@ -0,0 +1,110 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package pgwire
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/security"
+	"github.com/cockroachdb/errors"
+)
+
+// AuthBehaviors encapsulates the per-connection behaviors that may be
+// configured. This type is returned by AuthMethod implementations.
+//
+// Callers should call the AuthBehaviors.ConnClose method once the
+// associated network connection has been terminated to allow external
+// resources to be released.
+type AuthBehaviors struct {
+	authenticator       Authenticator
+	connClose           func()
+	replacementIdentity security.SQLUsername
+	replacedIdentity    bool
+	roleMapper          RoleMapper
+}
+
+// Ensure that an AuthBehaviors is easily composable with itself.
+var _ Authenticator = (*AuthBehaviors)(nil).Authenticate
+var _ func() = (*AuthBehaviors)(nil).ConnClose
+var _ RoleMapper = (*AuthBehaviors)(nil).MapRole
+
+// This is a hack for the unused-symbols linter. These two functions
+// are, at present, only called by the GSSAPI integration. The code
+// is guarded by a build tag, which is ignored by the linter.
+// TODO(#dev-inf): Update the linter to include the "gss" build tag.
+var _ = (*AuthBehaviors)(nil).SetConnClose
+var _ = (*AuthBehaviors)(nil).SetReplacementIdentity
+
+// Authenticate delegates to the Authenticator passed to
+// SetAuthenticator or returns an error if SetAuthenticator has not been
+// called.
+func (b *AuthBehaviors) Authenticate(
+	ctx context.Context,
+	systemIdentity security.SQLUsername,
+	clientConnection bool,
+	pwRetrieveFn PasswordRetrievalFn,
+) error {
+	if found := b.authenticator; found != nil {
+		return found(ctx, systemIdentity, clientConnection, pwRetrieveFn)
+	}
+	return errors.New("no Authenticator provided to AuthBehaviors")
+}
+
+// SetAuthenticator updates the Authenticator to be used.
+func (b *AuthBehaviors) SetAuthenticator(a Authenticator) {
+	b.authenticator = a
+}
+
+// ConnClose delegates to the function passed to SetConnClose to release
+// any resources associated with the connection. This method is a no-op
+// if SetConnClose has not been called or was called with nil.
+func (b *AuthBehaviors) ConnClose() {
+	if fn := b.connClose; fn != nil {
+		fn()
+	}
+}
+
+// SetConnClose updates the connection-close callback.
+func (b *AuthBehaviors) SetConnClose(fn func()) {
+	b.connClose = fn
+}
+
+// ReplacementIdentity returns an optional replacement for the
+// client-provided identity when validating the incoming connection.
+// This allows "ambient" authentication mechanisms, such as GSSAPI, to
+// provide replacement values. This method will return ok==false if
+// SetReplacementIdentity has not been called.
+func (b *AuthBehaviors) ReplacementIdentity() (_ security.SQLUsername, ok bool) {
+	return b.replacementIdentity, b.replacedIdentity
+}
+
+// SetReplacementIdentity allows the AuthMethod to override the
+// client-reported system identity.
+func (b *AuthBehaviors) SetReplacementIdentity(id security.SQLUsername) {
+	b.replacementIdentity = id
+	b.replacedIdentity = true
+}
+
+// MapRole delegates to the RoleMapper passed to SetRoleMapper or
+// returns an error if SetRoleMapper has not been called.
+func (b *AuthBehaviors) MapRole(
+	ctx context.Context, systemIdentity security.SQLUsername,
+) ([]security.SQLUsername, error) {
+	if found := b.roleMapper; found != nil {
+		return found(ctx, systemIdentity)
+	}
+	return nil, errors.New("no RoleMapper provided to AuthBehaviors")
+}
+
+// SetRoleMapper updates the RoleMapper to be used.
+func (b *AuthBehaviors) SetRoleMapper(m RoleMapper) {
+	b.roleMapper = m
+}
