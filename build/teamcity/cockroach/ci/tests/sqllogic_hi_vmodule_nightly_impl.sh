e5170f69afcb4827daa11cc7f1f6327527d36d94
Rail Aliiev <rail@iqchoice.com>
2021-09-16 12:09:15 +0300

    ci: add bazel SQL Logic Test High VModule Nightly job for CI
    Adds wrappers to run Bazel-based SQL Logic Test High VModule Nightly CI
    job.
    
    Fixes #67150
    
    Release note: None

---
+++ build/teamcity/cockroach/ci/tests/sqllogic_hi_vmodule_nightly_impl.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+bazel build //pkg/cmd/bazci --config=ci
+BAZEL_BIN=$(bazel info bazel-bin --config=ci)
+
+$BAZEL_BIN/pkg/cmd/bazci/bazci_/bazci --config=ci \
+    test //pkg/sql/logictest:logictest_test -- \
+    --test_arg=--vmodule=*=10 \
+    --test_arg=-show-sql \
+    --test_arg=-test.v \
+    --test_arg=-test.timeout=2h \
+    --test_filter='^TestLogic$' \
+    --test_timeout=7200
