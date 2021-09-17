bb1b1bb4816e2a8e522cc8077cde11949d340e05
Jack Wu <jack.wu@cockroachlabs.com>
2021-09-17 11:42:39 -0400

    sql: return an error message when trying to show grants for an invalid user
    Release justification: None, waiting for 22.1
    Release note: None

--- pkg/sql/opt_catalog.go
+++ pkg/sql/opt_catalog.go
@@ -12,6 +12,7 @@ package sql
 
 import (
 	"context"
+	"github.com/cockroachdb/cockroach/pkg/security"
 	"math"
 	"time"
 
@@ -368,6 +369,11 @@ func (oc *optCatalog) fullyQualifiedNameWithTxn(
 			scName,
 			tree.Name(desc.GetName())),
 		nil
+}
+
+// RoleExists is part of the cat.Catalog interface.
+func (oc *optCatalog) RoleExists(ctx context.Context, role security.SQLUsername) (bool, error) {
+	return RoleExists(ctx, oc.planner.ExecCfg(), oc.planner.Txn(), role)
 }
 
 // dataSourceForDesc returns a data source wrapper for the given descriptor.
