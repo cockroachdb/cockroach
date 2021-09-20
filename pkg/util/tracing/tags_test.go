90cd3bed2b78f1367c36f81bf7c82996139238a8
Andrei Matei <andrei@cockroachlabs.com>
2021-09-20 18:37:30 -0400

    util/tracing: move some test utilities to test file
    Release note: None

--- pkg/util/tracing/tags_test.go
+++ pkg/util/tracing/tags_test.go
@@ -31,7 +31,7 @@ func TestLogTags(t *testing.T) {
 	sp1 := tr.StartSpan("foo", WithForceRealSpan(), WithLogTags(l))
 	sp1.SetVerbose(true)
 	sp1.Finish()
-	require.NoError(t, TestingCheckRecordedSpans(sp1.GetRecording(), `
+	require.NoError(t, CheckRecordedSpans(sp1.GetRecording(), `
 		span: foo
 			tags: _verbose=1 tag1=val1 tag2=val2
 	`))
@@ -51,7 +51,7 @@ func TestLogTags(t *testing.T) {
 	sp2 := tr.StartSpan("bar", WithForceRealSpan(), WithLogTags(l))
 	sp2.SetVerbose(true)
 	sp2.Finish()
-	require.NoError(t, TestingCheckRecordedSpans(sp2.GetRecording(), `
+	require.NoError(t, CheckRecordedSpans(sp2.GetRecording(), `
 		span: bar
 			tags: _verbose=1 one=val1 two=val2
 	`))
@@ -69,7 +69,7 @@ func TestLogTags(t *testing.T) {
 	sp3 := tr.StartSpan("baz", WithLogTags(l), WithForceRealSpan())
 	sp3.SetVerbose(true)
 	sp3.Finish()
-	require.NoError(t, TestingCheckRecordedSpans(sp3.GetRecording(), `
+	require.NoError(t, CheckRecordedSpans(sp3.GetRecording(), `
 		span: baz
 			tags: _verbose=1 one=val1 two=val2
 	`))
