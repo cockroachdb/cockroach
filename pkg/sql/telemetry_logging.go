60760ddcfc08ec76aa5620aae2f259d207276c31
Xin Hao Zhang <xzhang@cockroachlabs.com>
2021-09-24 10:17:30 -0400

    sql: use adaptive sampling rate for telemetry logging
    Resolves #70553
    
    Previously, telemetry logging used a configurable QPS threshold
    and sampling rate, for which we would log all statements if we
    were under the QPS threshold, and then start sampling at the
    given rate once at the threshold. Using this technique meant
    that we will often see a sharp decreaes in telemetry logging
    once the sampling rate increases, as sampling rates would typically
    need to be at low values to accomodate a high QPS.
    
    This commit replaces the above technique with an adaptive sampling
    rate which merely logs events to telemetry at a maximum frequency.
    Rather than relying on QPS, we will simply track when we have
    last logged to the telemtry channel, and decide whether or not to
    log a given event accordingly.
    
    Release note (sql change): The cluster setting
    `sql.telemetry.query_sampling.qps_threshold`, and
    `sql.telemetry.query_sampling.sample_rate` have been removed.
    A new setting, `sql.telemetry.query_sampling.max_event_frequency`
    has been introduced, with default value of 10 events per second.

--- pkg/sql/telemetry_logging.go
+++ pkg/sql/telemetry_logging.go
@@ -14,25 +14,32 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 )
 
-// TelemetryLoggingMetrics keeps track of a rolling interval of previous query
-// counts with timestamps. The rolling interval of query counts + timestamps is
-// used in combination with the smoothing alpha value in an exponential
-// smoothing function to approximate cluster QPS.
+// Default value used to designate the maximum frequency at which events
+// are logged to the telemetry channel.
+const defaultMaxEventFrequency = 10
+
+var telemetryMaxEventFrequency = settings.RegisterIntSetting(
+	"sql.telemetry.query_sampling.max_event_frequency",
+	"the max event frequency at which we sample queries for telemetry",
+	defaultMaxEventFrequency,
+	settings.NonNegativeInt,
+)
+
+// TelemetryLoggingMetrics keeps track of the last time at which an event
+// was logged to the telemetry channel, and the number of skipped queries
+// since the last logged event.
 type TelemetryLoggingMetrics struct {
 	mu struct {
 		syncutil.RWMutex
-		rollingQueryCounts queryCountCircularBuffer
-		// MovingQPS is used in ExpSmoothQPS(), where we calculate a smoothed QPS
-		// value.
-		MovingQPS []int64
+		// The timestamp of the last emitted telemetry event.
+		lastEmittedTime time.Time
 	}
-	smoothingAlpha  float64
-	rollingInterval int
-	Knobs           *TelemetryLoggingTestingKnobs
+	Knobs *TelemetryLoggingTestingKnobs
 
 	// skippedQueryCount is used to produce the count of non-sampled queries.
 	skippedQueryCount uint64
@@ -40,10 +47,6 @@ type TelemetryLoggingMetrics struct {
 
 // TelemetryLoggingTestingKnobs provides hooks and knobs for unit tests.
 type TelemetryLoggingTestingKnobs struct {
-	// getRollingIntervalLength allows tests to override the rolling
-	// interval cluster setting.
-	getRollingIntervalLength func() int64
-
 	// getTimeNow allows tests to override the timeutil.Now() function used
 	// when updating rolling query counts.
 	getTimeNow func() time.Time
@@ -52,20 +55,6 @@ type TelemetryLoggingTestingKnobs struct {
 // ModuleTestingKnobs implements base.ModuleTestingKnobs interface.
 func (*TelemetryLoggingTestingKnobs) ModuleTestingKnobs() {}
 
-// NewTelemetryLoggingMetrics returns a new TelemetryLoggingMetrics object.
-func NewTelemetryLoggingMetrics(alpha float64, interval int64) *TelemetryLoggingMetrics {
-	t := TelemetryLoggingMetrics{
-		smoothingAlpha:  alpha,
-		rollingInterval: int(interval),
-	}
-	t.mu.rollingQueryCounts = queryCountCircularBuffer{queryCounts: make([]queryCountAndTime, interval)}
-	// MovingQPS calculates the QPS values between the query counts in
-	// rollingQueryCounts. Consequently, MovingQPS can only have interval - 1
-	// values.
-	t.mu.MovingQPS = make([]int64, interval-1)
-	return &t
-}
-
 func (t *TelemetryLoggingMetrics) timeNow() time.Time {
 	if t.Knobs != nil && t.Knobs.getTimeNow != nil {
 		return t.Knobs.getTimeNow()
@@ -73,36 +62,22 @@ func (t *TelemetryLoggingMetrics) timeNow() time.Time {
 	return timeutil.Now()
 }
 
-func (t *TelemetryLoggingMetrics) getInterval() int {
-	if t.Knobs != nil && t.Knobs.getRollingIntervalLength != nil {
-		return int(t.Knobs.getRollingIntervalLength())
-	}
-	return t.rollingInterval
-}
-
-// updateRollingQueryCounts appends a new queryCountAndTime to the
-// list of query counts in the telemetry logging metrics. Old
-// queryCountAndTime values are removed from the slice once the size
-// of the slice has exceeded the rollingInterval.
-func (t *TelemetryLoggingMetrics) updateRollingQueryCounts() {
+// maybeUpdateLastEmittedTime updates the lastEmittedTime if the amount of time
+// elapsed between lastEmittedTime and newTime is greater than requiredSecondsElapsed.
+func (t *TelemetryLoggingMetrics) maybeUpdateLastEmittedTime(
+	newTime time.Time, requiredSecondsElapsed float64,
+) bool {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 
-	currentTime := t.timeNow()
+	lastEmittedTime := t.mu.lastEmittedTime
 
-	// Get the latest entry.
-	// If the time since the latest entry was less than a second, bucket the
-	// current query into the previous timestamp.
-	if currentTime.Sub(t.mu.rollingQueryCounts.lastQueryCount().timestamp) < time.Second {
-		t.mu.rollingQueryCounts.lastQueryCount().incrementCount()
-		return
+	if float64(newTime.Sub(lastEmittedTime))*1e-9 >= requiredSecondsElapsed {
+		t.mu.lastEmittedTime = newTime
+		return true
 	}
 
-	newLatest := queryCountAndTime{
-		currentTime,
-		1,
-	}
-	t.mu.rollingQueryCounts.insert(newLatest)
+	return false
 }
 
 func (t *TelemetryLoggingMetrics) resetSkippedQueryCount() (res uint64) {
@@ -111,82 +86,4 @@ func (t *TelemetryLoggingMetrics) resetSkippedQueryCount() (res uint64) {
 
 func (t *TelemetryLoggingMetrics) incSkippedQueryCount() {
 	atomic.AddUint64(&t.skippedQueryCount, 1)
-}
-
-// queryCountCircularBuffer is a circular buffer of queryCountAndTime objects.
-// As part of the TelemetryLoggingMetrics object, queryCountCircularBuffer
-// should be accessed and written to via read/write locks.
-type queryCountCircularBuffer struct {
-	queryCounts []queryCountAndTime
-	end         int
-}
-
-// insert a queryCountAndTime object into the circular buffer. If the
-// buffer is full, the oldest value is overwritten.
-// Write lock needs to be acquired to call this method from a
-// TelemetryLoggingMetrics object.
-func (b *queryCountCircularBuffer) insert(val queryCountAndTime) {
-	// Increment the end pointer to the next index.
-	// Update the value of the next index.
-	b.end = b.nextIndex(b.end)
-	b.queryCounts[b.end] = val
-}
-
-// getQueryCount returns the query count at the given index.
-// Read lock needs to be acquired to call this method from a
-// TelemetryLoggingMetrics object.
-func (b *queryCountCircularBuffer) getQueryCount(n int) *queryCountAndTime {
-	return &b.queryCounts[n]
-}
-
-// lastQueryCount returns the latest query count.
-// Read lock needs to be acquired to call this method from a
-// TelemetryLoggingMetrics object.
-func (b *queryCountCircularBuffer) lastQueryCount() *queryCountAndTime {
-	return &b.queryCounts[b.end]
-}
-
-// endPointer returns the index of the buffer's end pointer.
-// Read lock needs to be acquired to call this method from a
-// TelemetryLoggingMetrics object.
-func (b *queryCountCircularBuffer) endPointer() int {
-	return b.end
-}
-
-// prevIndex returns the index previous to the given index 'n'.
-// Read lock needs to be acquired to call this method from a
-// TelemetryLoggingMetrics object.
-func (b *queryCountCircularBuffer) prevIndex(n int) int {
-	// Do not need mutex access, length of queryCounts never changes.
-	return (n + len(b.queryCounts) - 1) % len(b.queryCounts)
-}
-
-// nextIndex returns the index after the given index 'n'.
-// Read lock needs to be acquired to call this method from a
-// TelemetryLoggingMetrics object.
-func (b *queryCountCircularBuffer) nextIndex(n int) int {
-	// Do not need mutex access, length of queryCounts never changes.
-	return (n + 1) % len(b.queryCounts)
-}
-
-// queryCountAndTime keeps a count of user initiated statements,
-// and the timestamp at the latest count change.
-// queryCountAndTime objects are used as part of the queryCountCircularBuffer,
-// which in turn is used concurrently as part of the TelemetryLoggingMetrics
-// object. As such, queryCountAndTime objects are to be accessed and written to
-// via locks.
-type queryCountAndTime struct {
-	// No lock needs to be acquired to access this field as the timestamp of never
-	// changes.
-	timestamp time.Time
-	// Read lock needs to be acquired to access this field from a
-	// TelemetryLoggingMetrics object.
-	count int64
-}
-
-// incrementCount increments a query count by 1.
-// Write lock needs to be acquired to call this method from a
-// TelemetryLoggingMetrics object.
-func (q *queryCountAndTime) incrementCount() {
-	q.count++
 }
