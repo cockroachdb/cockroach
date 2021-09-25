5281f7912dba3e58e4ecb2acf4b6c5632c9efaba
Erik Grinaker <grinaker@cockroachlabs.com>
2021-09-25 17:46:58 +0000

    rangecache: improve tracing of range descriptor lookups
    Release note: None

--- pkg/kv/bulk/sst_batcher_test.go
+++ pkg/kv/bulk/sst_batcher_test.go
@@ -163,12 +163,13 @@ func runTestImport(t *testing.T, batchSizeValue int64) {
 			// splits to exercise that codepath, but we also want to make sure we
 			// still handle an unexpected split, so we make our own range cache and
 			// only populate it with one of our two splits.
-			mockCache := rangecache.NewRangeCache(s.ClusterSettings(), nil, func() int64 { return 2 << 10 }, s.Stopper())
+			ds := s.DistSenderI().(*kvcoord.DistSender)
+			mockCache := rangecache.NewRangeCache(s.ClusterSettings(), nil, func() int64 { return 2 << 10 }, s.Stopper(), ds.AmbientContext)
 			addr, err := keys.Addr(key(0))
 			if err != nil {
 				t.Fatal(err)
 			}
-			tok, err := s.DistSenderI().(*kvcoord.DistSender).RangeDescriptorCache().LookupWithEvictionToken(
+			tok, err := ds.RangeDescriptorCache().LookupWithEvictionToken(
 				ctx, addr, rangecache.EvictionToken{}, false)
 			if err != nil {
 				t.Fatal(err)
