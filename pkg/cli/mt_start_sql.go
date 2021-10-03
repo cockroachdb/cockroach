44f27b3cc2c9a60c84ed0fd41d046db9c87277c2
Jay <jay@cockroachlabs.com>
2021-10-03 01:00:43 -0400

    cli: support SQL disk spilling in tenants
    Previously, temp storage for SQL tenants was configured to point to memory
    with a limit of 100MB. As a result, all ephemeral data when processing large
    queries end up going to memory, and there was no way to configure this to
    point to disk. This commit changes that behavior, and the default temp storage
    for SQL tenants is now the same as SQL for dedicated, i.e. disk, with a limit
    of 32GB. The operator can configure this through the `--max-disk-temp-storage`
    flag.
    
    Release note (cli change): `cockroach mt start-sql` will now support the
    following flags to configure ephemeral storage for SQL when processing large
    queries: `--store`, `--temp-dir`, and `--max-disk-temp-storage`.
    
    Release note (sql change): SQL tenants will now spill to disk by default
    when processing large queries, instead of memory.

--- pkg/cli/mt_start_sql.go
+++ pkg/cli/mt_start_sql.go
@@ -15,7 +15,6 @@ import (
 	"os"
 	"os/signal"
 
-	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/cli/clierrorplus"
 	"github.com/cockroachdb/cockroach/pkg/clusterversion"
 	"github.com/cockroachdb/cockroach/pkg/server"
@@ -63,13 +62,6 @@ func runStartSQL(cmd *cobra.Command, args []string) error {
 	ctx := context.Background()
 	const clusterName = ""
 
-	// Remove the default store, which avoids using it to set up logging.
-	// Instead, we'll default to logging to stderr unless --log-dir is
-	// specified. This makes sense since the standalone SQL server is
-	// at the time of writing stateless and may not be provisioned with
-	// suitable storage.
-	serverCfg.Stores.Specs = nil
-
 	stopper, err := setupAndInitializeLoggingAndProfiling(ctx, cmd, false /* isServerCmd */)
 	if err != nil {
 		return err
@@ -94,20 +86,11 @@ func runStartSQL(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	tempStorageMaxSizeBytes := int64(base.DefaultInMemTempStorageMaxSizeBytes)
-	if err := diskTempStorageSizeValue.Resolve(
-		&tempStorageMaxSizeBytes, memoryPercentResolver,
+	if serverCfg.SQLConfig.TempStorageConfig, err = initTempStorageConfig(
+		ctx, serverCfg.Settings, stopper, serverCfg.Stores,
 	); err != nil {
 		return err
 	}
-
-	serverCfg.SQLConfig.TempStorageConfig = base.TempStorageConfigFromEnv(
-		ctx,
-		st,
-		base.StoreSpec{InMemory: true},
-		"", // parentDir
-		tempStorageMaxSizeBytes,
-	)
 
 	initGEOS(ctx)
 
