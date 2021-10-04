04bb2ce105061c7174ff395571c1d839a5ad8c6c
Neha George <neha.george@cockroachlabs.com>
2021-10-04 12:48:09 -0400

    opt, parser: add support for nulls first, nulls last
    Previously, we only supported the default Postgres ordering for
    ORDER BY. That is, NULLS come first for ascending order and NULLS
    come last for descending order. Postgres also supports the keywords
    "NULLS FIRST" and "NULLS LAST", which explicitly places all the NULLS
    first or last, respectively. This PR adds support for this non-default
    ordering.
    
    Release note: None

--- pkg/sql/parser/parse_test.go
+++ pkg/sql/parser/parse_test.go
@@ -509,9 +509,6 @@ func TestUnimplementedSyntax(t *testing.T) {
 		{`CREATE INDEX a ON b(c gin_trgm_ops)`, 41285, `index using gin_trgm_ops`, ``},
 		{`CREATE INDEX a ON b(c gist_trgm_ops)`, 41285, `index using gist_trgm_ops`, ``},
 		{`CREATE INDEX a ON b(c bobby)`, 47420, ``, ``},
-		{`CREATE INDEX a ON b(a NULLS LAST)`, 6224, ``, ``},
-		{`CREATE INDEX a ON b(a ASC NULLS LAST)`, 6224, ``, ``},
-		{`CREATE INDEX a ON b(a DESC NULLS FIRST)`, 6224, ``, ``},
 
 		{`INSERT INTO foo(a, a.b) VALUES (1,2)`, 27792, ``, ``},
 		{`INSERT INTO foo VALUES (1,2) ON CONFLICT ON CONSTRAINT a DO NOTHING`, 28161, ``, ``},
