789fbd68fc039a65b27dba60531773601c55f10a
Ricky Stewart <ricky@cockroachlabs.com>
2021-10-04 14:14:30 -0500

    bazel,ui: compress assets generated by bazel build
    Up until now building `cockroach` has resulted in the following warning:
    
        WARNING: /data/home/kena/src/go/src/github.com/cockroachdb/cockroach/pkg/ui/distccl/BUILD.bazel:4:8: output 'pkg/ui/distccl/assets' of //pkg/ui/distccl:genassets is a directory; dependency checking of directories is unsound
    
    The [Bazel documentation](https://docs.bazel.build/versions/main/be/general.html#general-advice)
    has this to say:
    
    > Avoid creating symlinks and directories. Bazel doesn't copy over the
    > directory/symlink structure created by genrules and its dependency
    > checking of directories is unsound.
    
    So we're getting this error because the
    `pkg/ui/{distccl,distoss}:genassets` rules produced an output directory
    rather than a discrete set of output files. Since the contents of the
    `assets` directories have unpredictable names, we can't fix this by
    naming the individual files. However, we can compress the `assets`
    directory into a single file and use that instead.
    
    Here we make a helper tool at `pkg/util/targz/gen` that basically just
    compresses the `assets` directory into an `assets.tar.gz` (just as `tar`
    would, but we don't assume that the user machine has `tar` installed).
    We also add a helper library `pkg/util/targz` that exposes the `.tar.gz`
    file as an `fs.FS`, which is enough for the downstream consumers of the
    `assets`.
    
    Closes #70348.
    
    Release note: None

--- pkg/ui/distoss/distoss_no_bazel.go
+++ pkg/ui/distoss/distoss_no_bazel.go
@@ -11,27 +11,26 @@
 // Package distoss embeds the assets for the OSS version of the web UI into the
 // Cockroach binary.
 
-//go:build bazel
-// +build bazel
+//go:build !bazel
+// +build !bazel
 
 package distoss
 
 import (
-	"bytes"
-	_ "embed"
+	"embed"
+	"io/fs"
 
 	"github.com/cockroachdb/cockroach/pkg/ui"
-	"github.com/cockroachdb/cockroach/pkg/util/targz"
 )
 
-//go:embed assets.tar.gz
-var assets []byte
+//go:embed assets/*
+var assets embed.FS
 
 func init() {
-	fs, err := targz.AsFS(bytes.NewBuffer(assets))
+	var err error
+	ui.Assets, err = fs.Sub(assets, "assets")
 	if err != nil {
 		panic(err)
 	}
-	ui.Assets = fs
 	ui.HaveUI = true
 }
