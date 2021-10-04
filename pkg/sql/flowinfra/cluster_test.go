6999e5fded43f59eb5839dc8b943fd1e2a33a3fd
Andrei Matei <andrei@cockroachlabs.com>
2021-10-04 14:29:18 -0400

    *: don't access Tracer through Settings
    Our Tracer is unfortunately tangled with the Settings, for no good
    reason. Creating a Settings also creates a Tracer, and the Settings also
    exposes a Tracer field. Random code was using that field because they
    needed a Tracer and they happened to have a Settings around.
    
    This coupling is unfortunate because it's in the way of tests
    controlling the creation of the Tracer. Also, it doesn't make any sense.
    This patch moves the majority of users of Settings.Tracer to get a
    Tracer in various other ways. The next patch will remove the coupling.
    
    Release note: None

--- pkg/sql/flowinfra/cluster_test.go
+++ pkg/sql/flowinfra/cluster_test.go
@@ -35,6 +35,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
 	"github.com/cockroachdb/cockroach/pkg/util/encoding"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
@@ -52,7 +53,8 @@ func TestClusterFlow(t *testing.T) {
 	const numRows = 100
 
 	args := base.TestClusterArgs{ReplicationMode: base.ReplicationManual}
-	tc := serverutils.StartNewTestCluster(t, 3, args)
+	tci := serverutils.StartNewTestCluster(t, 3, args)
+	tc := tci.(*testcluster.TestCluster)
 	defer tc.Stopper().Stop(context.Background())
 
 	sumDigitsFn := func(row int) tree.Datum {
@@ -90,7 +92,7 @@ func TestClusterFlow(t *testing.T) {
 		// that doesn't matter for the purposes of this test.
 
 		// Start a span (useful to look at spans using Lightstep).
-		sp := tc.Server(0).ClusterSettings().Tracer.StartSpan("cluster test")
+		sp := tc.ServerTyped(0).Tracer().StartSpan("cluster test")
 		ctx := tracing.ContextWithSpan(context.Background(), sp)
 		defer sp.Finish()
 
