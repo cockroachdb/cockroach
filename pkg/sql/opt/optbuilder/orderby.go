375786abd5420ce688cc73ccb23abb5ca504be27
Neha George <neha.george@cockroachlabs.com>
2021-10-04 12:48:09 -0400

    opt, parser: add support for nulls first, nulls last
    Previously, we only supported the default Postgres ordering for
    ORDER BY. That is, NULLS come first for ascending order and NULLS
    come last for descending order. Postgres also supports the keywords
    "NULLS FIRST" and "NULLS LAST", which explicitly places all the NULLS
    first or last, respectively. This PR adds support for this non-default
    ordering.
    
    Informs #6224.
    
    Release note (sql change): NULLS FIRST and NULLS LAST specifiers now
    supported for ORDER BY.

--- pkg/sql/opt/optbuilder/orderby.go
+++ pkg/sql/opt/optbuilder/orderby.go
@@ -11,6 +11,8 @@
 package optbuilder
 
 import (
+	"fmt"
+
 	"github.com/cockroachdb/cockroach/pkg/sql/opt"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/cat"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgcode"
@@ -114,9 +116,7 @@ func (b *Builder) addOrderByOrDistinctOnColumn(
 
 // analyzeOrderByIndex appends to the orderByScope a column for each indexed
 // column in the specified index, including the implicit primary key columns.
-func (b *Builder) analyzeOrderByIndex(
-	order *tree.Order, inScope, projectionsScope, orderByScope *scope,
-) {
+func (b *Builder) analyzeOrderByIndex(order *tree.Order, inScope, orderByScope *scope) {
 	tab, tn := b.resolveTable(&order.Table, privilege.SELECT)
 	index, err := b.findIndexByName(tab, order.Index)
 	if err != nil {
@@ -154,13 +154,22 @@ func (b *Builder) analyzeOrderByArg(
 	order *tree.Order, inScope, projectionsScope, orderByScope *scope,
 ) {
 	if order.OrderType == tree.OrderByIndex {
-		b.analyzeOrderByIndex(order, inScope, projectionsScope, orderByScope)
+		b.analyzeOrderByIndex(order, inScope, orderByScope)
 		return
+	}
+
+	// Set NULL order. The default order is nulls first for ascending order and
+	// nulls last for descending order.
+	nullsDefaultOrder := true
+	if order.NullsOrder != tree.DefaultNullsOrder &&
+		((order.NullsOrder == tree.NullsFirst && order.Direction == tree.Descending) ||
+			(order.NullsOrder == tree.NullsLast && order.Direction != tree.Descending)) {
+		nullsDefaultOrder = false
 	}
 
 	// Analyze the ORDER BY column(s).
 	start := len(orderByScope.cols)
-	b.analyzeExtraArgument(order.Expr, inScope, projectionsScope, orderByScope)
+	b.analyzeExtraArgument(order.Expr, inScope, projectionsScope, orderByScope, nullsDefaultOrder)
 	for i := start; i < len(orderByScope.cols); i++ {
 		col := &orderByScope.cols[i]
 		col.descending = order.Direction == tree.Descending
@@ -184,9 +193,12 @@ func (b *Builder) buildOrderByArg(
 
 // analyzeExtraArgument analyzes a single ORDER BY or DISTINCT ON argument.
 // Typically this is a single column, with the exception of qualified star
-// (table.*). The resulting typed expression(s) are added to extraColsScope.
+// (table.*). The resulting typed expression(s) are added to extraColsScope. The
+// nullsDefaultOrder bool determines whether an extra ordering column is
+// required to explicitly place nulls first or nulls last (when
+// nullsDefaultOrder is false).
 func (b *Builder) analyzeExtraArgument(
-	expr tree.Expr, inScope, projectionsScope, extraColsScope *scope,
+	expr tree.Expr, inScope, projectionsScope, extraColsScope *scope, nullsDefaultOrder bool,
 ) {
 	// Unwrap parenthesized expressions like "((a))" to "a".
 	expr = tree.StripParens(expr)
@@ -248,6 +260,13 @@ func (b *Builder) analyzeExtraArgument(
 	for _, e := range exprs {
 		// Ensure we can order on the given column(s).
 		ensureColumnOrderable(e)
+		if !nullsDefaultOrder {
+			metadataName := fmt.Sprintf("nulls_ordering_%s", e.String())
+			extraColsScope.addColumn(
+				scopeColName("").WithMetadataName(metadataName),
+				tree.NewTypedIsNullExpr(e),
+			)
+		}
 		extraColsScope.addColumn(scopeColName(""), e)
 	}
 }
