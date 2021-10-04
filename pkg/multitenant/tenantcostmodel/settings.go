147f85188e3b814e18d2349eed7f3f01ddefe623
Radu Berinde <radu@cockroachlabs.com>
2021-10-04 10:26:47 -0700

    multitenant: only charge for pgwire egress
    This commit changes the tenant cost accounting for pgwire bytes to
    only take into account egress; cloud providers don't charge for
    ingress. In the future, we can plumb the ingress bytes separately if
    needed - as of now we don't plan to charge for ingress.
    
    Release note: None
    
    Release justification: Necessary fix for the distributed rate limiting
    functionality, which is vital for the upcoming Serverless MVP release.
    It allows CRDB to throttle clusters that have run out of free or paid
    request units (which measure CPU and I/O usage). This functionality is
    only enabled in multi-tenant scenarios and should have no impact on
    our dedicated customers.

--- pkg/multitenant/tenantcostmodel/settings.go
+++ pkg/multitenant/tenantcostmodel/settings.go
@@ -61,8 +61,8 @@ var (
 		settings.PositiveFloat,
 	)
 
-	pgwireCostPerMB = settings.RegisterFloatSetting(
-		"tenant_cost_model.pgwire_cost_per_megabyte",
+	pgwireEgressCostPerMB = settings.RegisterFloatSetting(
+		"tenant_cost_model.pgwire_egress_cost_per_megabyte",
 		"cost of client <-> SQL ingress/egress per MB",
 		585.0,
 		settings.PositiveFloat,
@@ -75,7 +75,7 @@ var (
 		writeRequestCost,
 		writeCostPerMB,
 		podCPUSecondCost,
-		pgwireCostPerMB,
+		pgwireEgressCostPerMB,
 	}
 )
 
@@ -84,12 +84,12 @@ const perMBToPerByte = float64(1) / (1024 * 1024)
 // ConfigFromSettings constructs a Config using the cluster setting values.
 func ConfigFromSettings(sv *settings.Values) Config {
 	return Config{
-		KVReadRequest:  RU(readRequestCost.Get(sv)),
-		KVReadByte:     RU(readCostPerMB.Get(sv) * perMBToPerByte),
-		KVWriteRequest: RU(writeRequestCost.Get(sv)),
-		KVWriteByte:    RU(writeCostPerMB.Get(sv) * perMBToPerByte),
-		PodCPUSecond:   RU(podCPUSecondCost.Get(sv)),
-		PGWireByte:     RU(pgwireCostPerMB.Get(sv) * perMBToPerByte),
+		KVReadRequest:    RU(readRequestCost.Get(sv)),
+		KVReadByte:       RU(readCostPerMB.Get(sv) * perMBToPerByte),
+		KVWriteRequest:   RU(writeRequestCost.Get(sv)),
+		KVWriteByte:      RU(writeCostPerMB.Get(sv) * perMBToPerByte),
+		PodCPUSecond:     RU(podCPUSecondCost.Get(sv)),
+		PGWireEgressByte: RU(pgwireEgressCostPerMB.Get(sv) * perMBToPerByte),
 	}
 }
 
@@ -97,12 +97,12 @@ func ConfigFromSettings(sv *settings.Values) Config {
 // setting values.
 func DefaultConfig() Config {
 	return Config{
-		KVReadRequest:  RU(readRequestCost.Default()),
-		KVReadByte:     RU(readCostPerMB.Default() * perMBToPerByte),
-		KVWriteRequest: RU(writeRequestCost.Default()),
-		KVWriteByte:    RU(writeCostPerMB.Default() * perMBToPerByte),
-		PodCPUSecond:   RU(podCPUSecondCost.Default()),
-		PGWireByte:     RU(pgwireCostPerMB.Default() * perMBToPerByte),
+		KVReadRequest:    RU(readRequestCost.Default()),
+		KVReadByte:       RU(readCostPerMB.Default() * perMBToPerByte),
+		KVWriteRequest:   RU(writeRequestCost.Default()),
+		KVWriteByte:      RU(writeCostPerMB.Default() * perMBToPerByte),
+		PodCPUSecond:     RU(podCPUSecondCost.Default()),
+		PGWireEgressByte: RU(pgwireEgressCostPerMB.Default() * perMBToPerByte),
 	}
 }
 
