9f6cc9cf1384e70be01496b5c9560ece6a50c8a7
Marius Posta <marius@cockroachlabs.com>
2021-10-04 16:13:13 -0400

    sql: fix FK check bug in ALTER COLUMN TYPE
    This commit fixes an implementation bug when checking that the table's
    foreign keys didn't hold a reference to the altered column.
    
    Fixes #71089.
    
    Release note (bug fix): fixes a bug which caused ALTER COLUMN TYPE
    statements to fail when they shouldn't have.

--- pkg/sql/alter_column_type.go
+++ pkg/sql/alter_column_type.go
@@ -203,9 +203,18 @@ func alterColumnTypeGeneral(
 	// Disallow ALTER COLUMN TYPE general for columns that have a foreign key
 	// constraint.
 	for _, fk := range tableDesc.AllActiveAndInactiveForeignKeys() {
-		for _, id := range append(fk.OriginColumnIDs, fk.ReferencedColumnIDs...) {
-			if col.GetID() == id {
-				return colWithConstraintNotSupportedErr
+		if fk.OriginTableID == tableDesc.GetID() {
+			for _, id := range fk.OriginColumnIDs {
+				if col.GetID() == id {
+					return colWithConstraintNotSupportedErr
+				}
+			}
+		}
+		if fk.ReferencedTableID == tableDesc.GetID() {
+			for _, id := range fk.ReferencedColumnIDs {
+				if col.GetID() == id {
+					return colWithConstraintNotSupportedErr
+				}
 			}
 		}
 	}
