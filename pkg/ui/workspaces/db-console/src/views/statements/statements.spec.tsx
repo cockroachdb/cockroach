11019890b3d392d8922b799f3906146f2e122d78
Xin Hao Zhang <xzhang@cockroachlabs.com>
2021-10-05 13:55:37 -0400

    ui/cluster-ui: make app name a query search parameter in stmts page
    Fixes: #70790
    
    Previously, the selected app was derived from a route param on the
    statements page. All other filters are derived from query search
    parameters on the page. This commit makes the app name a query search
    parameter, as is the case in the transactions page.
    
    Release note: None

--- pkg/ui/workspaces/db-console/src/views/statements/statements.spec.tsx
+++ pkg/ui/workspaces/db-console/src/views/statements/statements.spec.tsx
@@ -439,7 +439,8 @@ describe("selectStatement", () => {
 
     assert.equal(result.statement, stmtA.key.key_data.query);
     assert.equal(result.stats.count.toNumber(), stmtA.stats.count.toNumber());
-    assert.deepEqual(result.app, [stmtA.key.key_data.app]);
+    // Statements with internal app prefix should have "(internal)" as app name
+    assert.deepEqual(result.app, ["(internal)"]);
     assert.deepEqual(result.distSQL, { numerator: 0, denominator: 1 });
     assert.deepEqual(result.vec, { numerator: 0, denominator: 1 });
     assert.deepEqual(result.failed, { numerator: 0, denominator: 1 });
@@ -588,6 +589,21 @@ function makeRoutePropsWithParams(params: { [key: string]: string }) {
   };
 }
 
+function makeRoutePropsWithSearchParams(params: { [key: string]: string }) {
+  const history = H.createHashHistory();
+  history.location.search = new URLSearchParams(params).toString();
+  return {
+    location: history.location,
+    history,
+    match: {
+      url: "",
+      path: history.location.pathname,
+      isExact: false,
+      params: {},
+    },
+  };
+}
+
 function makeEmptyRouteProps(): RouteComponentProps<any> {
   const history = H.createHashHistory();
   return {
@@ -603,7 +619,7 @@ function makeEmptyRouteProps(): RouteComponentProps<any> {
 }
 
 function makeRoutePropsWithApp(app: string) {
-  return makeRoutePropsWithParams({
+  return makeRoutePropsWithSearchParams({
     [appAttr]: app,
   });
 }
