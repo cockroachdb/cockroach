926e2c98004590edf7d9d4932af2394109c4b6cd
David Hartunian <davidh@cockroachlabs.com>
2021-10-05 11:24:08 -0400

    server: add ui-only status endpoint for node info
    Previously, a change was made to the `_status/nodes` endpoint to secure
    it behind an Admin role in order to limit the spread of sensitive
    information in node payloads to non-Admins. This included data such as
    crdb args, env variables, and addresses. That previous change created a
    regression in the DB Console because the cluster overview page relies
    heavily on information gleaned from the nodes endpoint. The page became
    useless for non-Admins since it couldn't receive any response from the
    endpoint.
    
    This change creates a new gRPC endpoint and payload types to mirror
    those found in the `roachpb` package for the purpose of continuing to
    expose node information to the DB Console. These endpoints are under the
    `_status/nodes_ui` prefix and convert internal `roachpb` payloads to the
    external facing ones (currently the payload types match exactly, but now
    they can change freely), adding in sensitive data only when the caller
    has the Admin role.
    
    The DB Console has been modified to only use the new `nodes_ui` endpoint
    which will dynamically return more data when the logged-in user is an
    Admin.
    
    non-UI callers on the `_status/nodes` gRPC endpoint such as the debug
    zip commands and others have not been modified.
    
    Resolves #70702
    
    Release note (ui change): Non-Admin users of the DB Console have
    regained the ability to view the cluster overview page. Users without
    the Admin role will still see most data about their nodes but
    information such as command line arguments, environment variables, and
    IP addresses and DNS names of nodes will be hidden.

--- pkg/ui/src/util/proto.ts
+++ pkg/ui/src/util/proto.ts
@@ -11,6 +11,8 @@
 import _ from "lodash";
 
 import * as protos from "src/js/protos";
+import { cockroach } from "src/js/protos";
+import INodeResponse = cockroach.server.serverpb.INodeResponse;
 
 export type INodeStatus = protos.cockroach.server.status.statuspb.INodeStatus;
 const nodeStatus: INodeStatus = null;
@@ -42,11 +44,8 @@ export function AccumulateMetrics(
  * metrics collection of the supplied NodeStatus object. This is convenient
  * for all current usages of NodeStatus in the UI.
  */
-export function RollupStoreMetrics(ns: INodeStatus): void {
-  AccumulateMetrics(
-    ns.metrics,
-    ..._.map(ns.store_statuses, (ss) => ss.metrics),
-  );
+export function RollupStoreMetrics(ns: INodeResponse): void {
+  AccumulateMetrics(ns.metrics, ..._.map(ns.store_statuses, ss => ss.metrics));
 }
 
 /**
