d0d3245b7d449eec9d82488eab5af2cf37042be9
irfan sharif <irfanmahmoudsharif@gmail.com>
2021-10-06 12:04:35 -0400

    rangefeedbuffer: introduce a rangefeed buffer
    In both #69614 and #69661 we find ourselves reaching for a thin,
    memory-bounded buffer to sit on top a rangefeed. We want to be able to
    accumulate raw rangefeed events, and when the rangefeed frontier is
    bumped, flush everything out en-masse in timestamp sorted order. Package
    rangefeedbuffer introduces such a datastructure.
    
    If we're past the configured memory limit before having observed a
    frontier bump, we have little recourse -- since we receive checkpoints and
    rangefeed values on the same stream, we're waiting for a future
    checkpoint to flush out the accumulated memory. Given this, we simply
    error out to the caller (expecting them to re-establish the rangefeed).
    
    Release note: None
    
    Co-authored-by: Arul Ajmani <arula@cockroachlabs.com>

---
+++ pkg/kv/kvclient/rangefeed/rangefeedbuffer/buffer_test.go
@@ -0,0 +1,131 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package rangefeedbuffer_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvclient/rangefeed/rangefeedbuffer"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/stretchr/testify/require"
+)
+
+// TestBuffer ensures that buffer addition and flushing semantics work as
+// expected.
+func TestBuffer(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	ts := func(nanos int) hlc.Timestamp {
+		return hlc.Timestamp{WallTime: int64(nanos)}
+	}
+
+	ctx := context.Background()
+	const limit = 25
+	buffer := rangefeedbuffer.New(limit)
+
+	{ // Sanity check the newly initialized rangefeed buffer.
+		events := buffer.Flush(ctx, ts(0))
+		require.True(t, len(events) == 0)
+	}
+
+	{ // Ensure that checkpoint events are discarded.
+		events := buffer.Flush(ctx, ts(5))
+		require.True(t, len(events) == 0)
+	}
+
+	{ // Bumping the frontier timestamp without existing events should behave expectedly.
+		events := buffer.Flush(ctx, ts(10))
+		require.True(t, len(events) == 0)
+	}
+
+	{ // Flushing at a timestamp lower than buffered events should return nothing.
+		require.NoError(t, buffer.Add(ctx, makeEvent("a", ts(13)))) // a@13
+		require.NoError(t, buffer.Add(ctx, makeEvent("b", ts(11)))) // b@11
+		require.NoError(t, buffer.Add(ctx, makeEvent("c", ts(15)))) // c@15
+		require.NoError(t, buffer.Add(ctx, makeEvent("d", ts(12)))) // d@12
+		require.NoError(t, buffer.Add(ctx, makeEvent("e", ts(18)))) // e@18
+
+		events := buffer.Flush(ctx, ts(10))
+		require.True(t, len(events) == 0)
+	}
+
+	{ // Flushing should capture only entries with timestamps <= the frontier, in sorted order.
+		events := buffer.Flush(ctx, ts(14))
+
+		require.True(t, len(events) == 3)
+		require.Equal(t, events[0].(*testEvent).data, "b") // b@11
+		require.Equal(t, events[1].(*testEvent).data, "d") // d@12
+		require.Equal(t, events[2].(*testEvent).data, "a") // a@13
+	}
+
+	{ // Incremental advances should only surface the events until the given timestamp.
+		events := buffer.Flush(ctx, ts(15))
+
+		require.True(t, len(events) == 1)
+		require.Equal(t, events[0].(*testEvent).data, "c") // c@15
+	}
+
+	{ // Adding events with timestamps <= the last flush are discarded.
+		require.NoError(t, buffer.Add(ctx, makeEvent("a", ts(13)))) // a@13
+		require.NoError(t, buffer.Add(ctx, makeEvent("b", ts(11)))) // b@11
+		require.NoError(t, buffer.Add(ctx, makeEvent("c", ts(15)))) // c@15
+		require.NoError(t, buffer.Add(ctx, makeEvent("d", ts(12)))) // d@12
+
+		events := buffer.Flush(ctx, ts(15))
+		require.True(t, len(events) == 0)
+	}
+
+	{ // Additional events are flushed out at appropriate points.
+		require.NoError(t, buffer.Add(ctx, makeEvent("f", ts(19)))) // f@19
+		require.NoError(t, buffer.Add(ctx, makeEvent("g", ts(21)))) // g@21
+
+		events := buffer.Flush(ctx, ts(20))
+		require.True(t, len(events) == 2)
+		require.Equal(t, events[0].(*testEvent).data, "e") // e@18
+		require.Equal(t, events[1].(*testEvent).data, "f") // f@19
+	}
+
+	{ // Ensure that a timestamp greater than any previous event flushes everything.
+		events := buffer.Flush(ctx, ts(100))
+		require.True(t, len(events) == 1)
+		require.Equal(t, events[0].(*testEvent).data, "g") // g@21
+	}
+
+	{ // Sanity check that there are no events left over.
+		events := buffer.Flush(ctx, ts(100))
+		require.True(t, len(events) == 0)
+	}
+
+	{ // Ensure that buffer limits are respected.
+		for i := 0; i < limit; i++ {
+			require.NoError(t, buffer.Add(ctx, makeEvent("x", ts(101)))) // x@101
+		}
+
+		err := buffer.Add(ctx, makeEvent("x", ts(101)))
+		require.ErrorIs(t, err, rangefeedbuffer.ErrBufferLimitExceeded)
+	}
+}
+
+type testEvent struct {
+	data string
+	ts   hlc.Timestamp
+}
+
+func (t *testEvent) Timestamp() hlc.Timestamp {
+	return t.ts
+}
+
+var _ rangefeedbuffer.Event = &testEvent{}
+
+func makeEvent(data string, ts hlc.Timestamp) rangefeedbuffer.Event {
+	return &testEvent{data: data, ts: ts}
+}
