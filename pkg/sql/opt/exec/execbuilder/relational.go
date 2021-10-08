a3426579c913172527ae03acc6b77f0f55d03180
Rebecca Taft <becca@cockroachlabs.com>
2021-10-08 08:53:09 -0500

    opt: update coster to avoid full scans with disallow_full_table_scans
    Updated the coster so that full table scans and full scans of non-partial
    indexes are given a "huge cost" if disallow_full_table_scans is true and
    stats are unavailable or the estimated row count is greater than
    large_full_scan_rows. Since disallow_full_table_scans and
    large_full_scan_rows can now affect the chosen plan, the optimizer now tracks
    these settings in the memo to ensure cached plans are not stale.
    
    Release note (sql change): The optimizer has been updated so that if
    disallow_full_table_scans is true, it will never plan a full table scan with
    an estimated row count greater than large_full_scan_rows. If no alternative
    plan is possible, an error will be returned, just as it was before. However,
    cases where an alternative plan is possible will no longer produce an error,
    since the alternative plan will be chosen. As a result, users should see
    fewer errors due to disallow_full_table_scans.
    
    A side effect of this change is that if disallow_full_table_scans is set along
    with statement-level hints such as an index hint, the optimizer will try to
    avoid a full scan while also respecting the index hint. If this is not
    possible, the optimizer will return an error and might not log the attempted
    full table scan or update the sql.guardrails.full_scan_rejected.count metric.
    If no index hint is used, the full scan will be logged and the metric updated.

--- pkg/sql/opt/exec/execbuilder/relational.go
+++ pkg/sql/opt/exec/execbuilder/relational.go
@@ -534,8 +534,13 @@ func (b *Builder) scanParams(
 				idx.Name(),
 			)
 		default:
-			// This should never happen.
 			err = fmt.Errorf("index \"%s\" cannot be used for this query", idx.Name())
+			if b.evalCtx.SessionData().DisallowFullTableScans &&
+				(b.ContainsLargeFullTableScan || b.ContainsLargeFullIndexScan) {
+				err = errors.WithHint(err,
+					"try overriding the `disallow_full_table_scans` or increasing the `large_full_scan_rows` cluster/session settings",
+				)
+			}
 		}
 
 		return exec.ScanParams{}, opt.ColMap{}, err
@@ -645,21 +650,6 @@ func (b *Builder) buildScan(scan *memo.ScanExpr) (execPlan, error) {
 		telemetry.Inc(sqltelemetry.PartialIndexScanUseCounter)
 	}
 
-	params, outputCols, err := b.scanParams(tab, &scan.ScanPrivate, scan.Relational(), scan.RequiredPhysical())
-	if err != nil {
-		return execPlan{}, err
-	}
-	res := execPlan{outputCols: outputCols}
-	root, err := b.factory.ConstructScan(
-		tab,
-		tab.Index(scan.Index),
-		params,
-		res.reqOrdering(scan),
-	)
-	if err != nil {
-		return execPlan{}, err
-	}
-
 	isUnfiltered := scan.IsUnfiltered(md)
 	// Save if we planned a full table/index scan on the builder so that the
 	// planner can be made aware later. We only do this for non-virtual tables.
@@ -673,6 +663,21 @@ func (b *Builder) buildScan(scan *memo.ScanExpr) (execPlan, error) {
 			b.ContainsFullIndexScan = true
 			b.ContainsLargeFullIndexScan = b.ContainsLargeFullIndexScan || large
 		}
+	}
+
+	params, outputCols, err := b.scanParams(tab, &scan.ScanPrivate, scan.Relational(), scan.RequiredPhysical())
+	if err != nil {
+		return execPlan{}, err
+	}
+	res := execPlan{outputCols: outputCols}
+	root, err := b.factory.ConstructScan(
+		tab,
+		tab.Index(scan.Index),
+		params,
+		res.reqOrdering(scan),
+	)
+	if err != nil {
+		return execPlan{}, err
 	}
 
 	res.root = root
