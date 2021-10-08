0904e92d8191adb561bdaa630abd1091d247dd21
Andrei Matei <andrei@cockroachlabs.com>
2021-10-08 14:57:29 -0400

    sql: fix leaks on canceled flows
    Before this patch, the span representing a flow that was canceled while
    blocked in the flow scheduler queue was never finished. Also, the
    construction of vectorized flows sometimes allocate tracked memory. This
    tracking was also leaking.
    
    Release note: None

--- pkg/sql/rowflow/row_based_flow.go
+++ pkg/sql/rowflow/row_based_flow.go
@@ -440,6 +440,11 @@ func (f *rowBasedFlow) Cleanup(ctx context.Context) {
 	f.Release()
 }
 
+// CleanupBeforeRun is part of the Flow interface.
+func (f *rowBasedFlow) CleanupBeforeRun(ctx context.Context) {
+	f.Cleanup(ctx)
+}
+
 type copyingRowReceiver struct {
 	execinfra.RowReceiver
 	alloc rowenc.EncDatumRowAlloc
