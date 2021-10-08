2eb99e1c5df5df35a8d7e25814e66892b0d3968a
Lindsey Jin <lindsey.jin@cockroachlabs.com>
2021-10-08 14:20:22 -0400

    sql: add summarized query formats for select, insert, and update statements
    Resolves #27021
    
    Previously, statements on the statements page hid too much information.
    There were complaints that it was difficult to disambiguate between
    statements without having to view the full query on the tooltips.
    
    To address this, this commit adds in new formatting functions for
    creating summarized queries. Currently, this only applies to SELECT,
    INSERT, and UPDATE statement types. We have also added a new FmtFlag,
    FmtSummary, a new field, StmtSummary, in the Statement struct, and a
    new field, QuerySummary, in app stats. A later commit will implement
    these changes in the frontend.
    
    Release note (sql change): Add new formatting functions to create
    summarized queries for SELECT, INSERT, and UPDATE statements. Also
    add new metadata fields, which will later be used to pass this
    information to the front-end Statements page.

--- pkg/sql/statement.go
+++ pkg/sql/statement.go
@@ -20,6 +20,7 @@ type Statement struct {
 	parser.Statement
 
 	StmtNoConstants string
+	StmtSummary     string
 	QueryID         ClusterWideID
 
 	ExpectedTypes colinfo.ResultColumns
@@ -41,6 +42,7 @@ func makeStatement(parserStmt parser.Statement, queryID ClusterWideID) Statement
 	return Statement{
 		Statement:       parserStmt,
 		StmtNoConstants: formatStatementHideConstants(parserStmt.AST),
+		StmtSummary:     formatStatementSummary(parserStmt.AST),
 		QueryID:         queryID,
 	}
 }
