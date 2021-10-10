c3b4b61b2b14c21d1fa18a48c5a7f611c36ecedb
Yevgeniy Miretskiy <yevgeniy@cockroachlabs.com>
2021-10-10 08:10:09 -0400

    changefeedccl: Fix a deadlock when closing blocking buffer.
    A deadlock can occur during shutdown when we attempt
    to release buffered events.  In such situation, we could
    wind up notifying blocking buffer of an out of quota situation,
    and that lead to a deadlock.
    
    In addition to not needing to release event resources, this change
    also identified and fixed a bug where buffered events were not released
    to the buffer pool.
    
    Fixes #71364
    
    Release Justification: low danger logic/correctness bug fix (category 4).
    Release Notes: None

--- pkg/ccl/changefeedccl/kvevent/blocking_buffer_test.go
+++ pkg/ccl/changefeedccl/kvevent/blocking_buffer_test.go
@@ -21,7 +21,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/randgen"
 	"github.com/cockroachdb/cockroach/pkg/sql/rowenc"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
-	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
 	"github.com/cockroachdb/cockroach/pkg/util/encoding"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
@@ -65,7 +64,6 @@ func getBoundAccountWithBudget(budget int64) (account mon.BoundAccount, cleanup 
 func TestBlockingBuffer(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
-	skip.UnderDeadlockWithIssue(t, 71364)
 
 	metrics := kvevent.MakeMetrics(time.Minute)
 	ba, release := getBoundAccountWithBudget(4096)
@@ -117,7 +115,6 @@ func TestBlockingBuffer(t *testing.T) {
 func TestBlockingBufferNotifiesConsumerWhenOutOfMemory(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
-	skip.UnderDeadlockWithIssue(t, 71364)
 
 	metrics := kvevent.MakeMetrics(time.Minute)
 	ba, release := getBoundAccountWithBudget(4096)
