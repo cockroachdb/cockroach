fb8f98e7a62d01b39ea397323563bc9a15405a70
Josh Soref <jsoref@users.noreply.github.com>
2021-10-11 07:45:30 -0400

    spelling: degradation
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- pkg/util/interval/btree_based_interval.go
+++ pkg/util/interval/btree_based_interval.go
@@ -862,7 +862,7 @@ type copyOnWriteContext struct {
 // The internal tree structure of b is marked read-only and shared between t and
 // t2.  Writes to both t and t2 use copy-on-write logic, creating new nodes
 // whenever one of b's original nodes would have been modified.  Read operations
-// should have no performance degredation.  Write operations for both t and t2
+// should have no performance degradation.  Write operations for both t and t2
 // will initially experience minor slow-downs caused by additional allocs and
 // copies due to the aforementioned copy-on-write logic, but should converge to
 // the original performance characteristics of the original tree.
