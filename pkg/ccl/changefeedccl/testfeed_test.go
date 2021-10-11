f479d27e00c89df1f8a5546351f5c2b7a377dd89
Josh Soref <jsoref@users.noreply.github.com>
2021-10-11 08:01:12 -0400

    spelling: waiter
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- pkg/ccl/changefeedccl/testfeed_test.go
+++ pkg/ccl/changefeedccl/testfeed_test.go
@@ -381,7 +381,7 @@ func (f *jobFeed) Close() error {
 // with the sink operations.
 type sinkSynchronizer struct {
 	syncutil.Mutex
-	waitor  chan struct{}
+	waiter  chan struct{}
 	flushed bool
 }
 
@@ -396,7 +396,7 @@ func (s *sinkSynchronizer) eventReady() chan struct{} {
 		close(ready)
 		s.flushed = false
 	} else {
-		s.waitor = ready
+		s.waiter = ready
 	}
 
 	return ready
@@ -406,9 +406,9 @@ func (s *sinkSynchronizer) addFlush() {
 	s.Lock()
 	defer s.Unlock()
 	s.flushed = true
-	if s.waitor != nil {
-		close(s.waitor)
-		s.waitor = nil
+	if s.waiter != nil {
+		close(s.waiter)
+		s.waiter = nil
 		s.flushed = false
 	}
 }
