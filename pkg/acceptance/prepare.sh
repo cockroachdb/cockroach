e3406a4c00f733bff28274627cd61d4b3b616e50
Josh Soref <jsoref@users.noreply.github.com>
2021-10-11 12:45:07 -0400

    spelling: libstdc
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- pkg/acceptance/prepare.sh
+++ pkg/acceptance/prepare.sh
@@ -6,6 +6,6 @@ set -euxo pipefail
 rm -f cockroach-linux-2.6.32-gnu-amd64 pkg/acceptance/acceptance.test
 
 # We must make a release build here because the binary needs to work in both
-# the builder image and the postgres-test image, which have different libstc++
+# the builder image and the postgres-test image, which have different libstdc++
 # versions.
 build/builder.sh mkrelease linux-gnu
