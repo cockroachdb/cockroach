67147f8d6f76587d6655b1dff8d3a1fdfb731f64
Radu Berinde <radu@cockroachlabs.com>
2021-10-11 06:38:02 -0700

    opt: fix implicit lateral for SRFs when using JOIN syntax
    When SRFs are used as a table, they imply that the join is lateral.
    This works for the multiple table syntax, but not for the JOIN syntax.
    This commit addresses this omission.
    
    Fixes #70342.
    
    Release note (bug fix): Fixes an incorrect "no data source matches
    prefix" error for queries that use a set-returning function on the
    right-hand side of a `JOIN` (unless `LATERAL` is explicitly
    specified).

--- pkg/sql/opt/optbuilder/select.go
+++ pkg/sql/opt/optbuilder/select.go
@@ -1204,6 +1204,7 @@ func (b *Builder) exprIsLateral(t tree.TableExpr) bool {
 	}
 	// Expressions which explicitly use the LATERAL keyword are lateral.
 	if ate.Lateral {
+		telemetry.Inc(sqltelemetry.LateralJoinUseCounter)
 		return true
 	}
 	// SRFs are always lateral.
