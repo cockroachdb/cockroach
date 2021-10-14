bbb69fe61966bdcdc84805af26b8e2dd85a0706c
David Taylor <tinystatemachine@gmail.com>
2021-10-14 22:38:39 +0000

    cli,backup,import,sql: add cli flag to allow non-admin node-based access
    Typically when running statements like BACKUP, IMPORT, or EXPORT, we
    only allow an admin user to direct that operation to interact with some
    remote resource that might be reachable only by virtue of being accessed
    by the node running the operation.
    
    Specifically, access to s3 or google cloud storage using 'implicit'
    auth, which uses the respective SDK methods to use the running machine's
    role or machine account, is typically only available to admin users, as
    is the ability to specify arbitrary network URIs using http:// or the
    custom-endpoint parameters to the other storage providers. This is
    because in these cases, the fact that the node is accessing that s3
    bucket or network URI may allow a degree of access the user might not
    otherwise have, and thus we restrict this to highly trusted admin users.
    
    This change introduces a flag,
    --external-io-enable-non-admin-implicit-access, that removes this
    restriction, allowing any SQL user who can invoke IMPORT, BACKUP or
    EXPORT commands to point those operations at URIs that would otherwise
    be restricted to admin-only users as described above. This may be
    desirable in an environment where network access is already controlled,
    for example at the container level, or where all users, not just admin
    users, are trusted enough to have the node make network requests on
    their behalf.
    
    Release note (security update): add a new flag --external-io-enable-non-admin-implicit-access
    that can remove the admin-only restriction on interacting with arbitrary network endpoint and
    using 'implicit' auth in operations such as BACKUP, IMPORT or EXPORT.

--- pkg/ccl/backupccl/restore_planning.go
+++ pkg/ccl/backupccl/restore_planning.go
@@ -1646,8 +1646,7 @@ func checkPrivilegesForRestore(
 				"only users with the CREATEDB privilege can restore databases")
 		}
 	}
-	knobs := p.ExecCfg().BackupRestoreTestingKnobs
-	if knobs != nil && knobs.AllowImplicitAccess {
+	if p.ExecCfg().ExternalIODirConfig.EnableNonAdminImplicitAndArbitraryOutbound {
 		return nil
 	}
 	// Check that none of the sources rely on implicit access.
