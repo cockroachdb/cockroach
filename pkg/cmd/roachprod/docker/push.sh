5efc10d5eef3865550840c6e6e0b91bbadbd6ed5
Rail Aliiev <rail@iqchoice.com>
2021-10-14 21:30:15 -0400

    roachprod: fix roachprod gc docker build
    Previously, the roachprod garbage collector docker image build process
    was using the `go get` approach to build roachprod.
    
    Currently, this method doesn't work, because it doesn't use any pinning,
    so the build ends up with all kind of deprecation warnings and failures.
    
    * Use multi-stage docker build in order to separate build and runtime.
      It also reduces the image size from 1.9G to 700M.
    * Build roachprod using the checked out commit SHA.
    * Use the Bazel build image we use in CI to build roachprod.
    * Use Bazel to build roachprod.
    * Added `cloudbuild.yaml` to publish the docker image to GCR and use a
      beefier instance type.
    * Modify the entrypoint script to set the default region, required by
      the AWS Go SDK library.
    * Add `push.sh` to script deployment.
    
    Release note: None

---
+++ pkg/cmd/roachprod/docker/push.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+# root is the absolute path to the root directory of the repository.
+root="$(cd ../../../../ &> /dev/null && pwd)"
+source "$root/build/teamcity-bazel-support.sh"  # For BAZEL_IMAGE
+
+if [[ -z ${OWNER+x} ]]; then
+    OWNER=cockroachdb
+fi
+if [[ -z ${REPO+x} ]]; then
+    REPO=cockroach
+fi
+
+SHA=$(git rev-parse --short HEAD)
+gcloud --project cockroach-dev-inf builds submit \
+  --substitutions=_BAZEL_IMAGE=$BAZEL_IMAGE,_SHA=$SHA,_OWNER=$OWNER,_REPO=$REPO \
+  --timeout=30m
+
+# Patch the existing cronjob configuration only for official builds
+if [[ "$OWNER" == "cockroachdb" && "$REPO" == "cockroach" ]]; then
+  kubectl set image cronjob/roachprod-gc-cronjob roachprod-gc-cronjob=gcr.io/cockroach-dev-inf/cockroachlabs/roachprod:$SHA
+fi
