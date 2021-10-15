d90efc17157511c1c215653a25d7cb97cd9cc3e8
Rail Aliiev <rail@iqchoice.com>
2021-10-14 21:30:15 -0400

    roachprod: fix roachprod gc docker build
    Previously, the roachprod garbage collector docker image build process
    was using the `go get` approach to build roachprod.
    
    Currently, this method doesn't work, because it doesn't use any pinning,
    so the build ends up with all kind of deprecation warnings and failures.
    
    * Use multi-stage docker build in order to separate build and runtime.
      It also reduces the image size from 1.9G to 700M.
    * Build roachprod using the checked out commit SHA.
    * Use the Bazel build image we use in CI to build roachprod.
    * Use Bazel to build roachprod.
    * Added `cloudbuild.yaml` to publish the docker image to GCR and use a
      beefier instance type.
    * Modify the entrypoint script to set the default region, required by
      the AWS Go SDK library.
    * Add `push.sh` to script deployment.
    
    Release note: None

--- pkg/cmd/roachprod/docker/entrypoint.sh
+++ pkg/cmd/roachprod/docker/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
-# Unpack all of the keys, configs, etc. and then start roachperf
+# Unpack all of the keys, configs, etc. and then run roachprod
 gcloud auth activate-service-account --key-file /secrets/gcloud.json
 aws configure set aws_access_key_id $(cat /secrets/aws_access_key_id)
 aws configure set aws_secret_access_key $(cat /secrets/aws_secret_access_key)
+# The default profile has to contain a region name in order for the AWS Go SDK
+# library to work
+aws configure set region us-east-1
 az login --service-principal -u $(cat /secrets/azure_user_id) -p $(cat /secrets/azure_password) -t $(cat /secrets/azure_tenant_id)
-exec roachprod $@
+exec /usr/local/bin/roachprod $@
