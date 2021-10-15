4c1e3238a38242fcd756992a922d439a5f35f163
Jordan Lewis <jordanthelewis@gmail.com>
2021-10-15 19:55:13 -0300

    randgen: add random DEFAULT, ON UPDATE exprs
    This commit updates randgen to sometimes generate random DEFAULT and ON
    UPDATE exprs when creating random table column definitions.
    
    The expression creation logic is extracted from the function that's used
    to create random partial index expressions. We pass in a random datum
    which is sometimes wrapped in further same-type scalar expressions.
    
    Release note: None

--- pkg/sql/randgen/expr.go
+++ pkg/sql/randgen/expr.go
@@ -122,12 +122,12 @@ func randAndOrExpr(rng *rand.Rand, left, right tree.Expr) tree.Expr {
 	}
 }
 
-// randExpr produces a random expression that refers to columns in
+// randColumnExpr produces a random expression that refers to columns in
 // normalColDefs. It can be used to generate random computed columns and
 // expression indexes. The return type is the type of the expression. The
 // returned nullability is NotNull if all columns referenced in the expression
 // have a NotNull nullability.
-func randExpr(
+func randColumnExpr(
 	rng *rand.Rand, normalColDefs []*tree.ColumnTableDef, nullOk bool,
 ) (tree.Expr, *types.T, tree.Nullability) {
 	nullability := tree.NotNull
@@ -191,34 +191,98 @@ func randExpr(
 	nullability = x.Nullable.Nullability
 	nullOk = nullOk && nullability != tree.NotNull
 
-	var expr tree.Expr
-	var typ *types.T
-	switch xTyp.Family() {
+	expr, typ := randExpr(rng, tree.NewUnresolvedName(string(x.Name)), xTyp, true, /* canChangeType */
+		true /* needsImmutable */, nullOk)
+	return expr, typ, nullability
+}
+
+// randExpr generates a random expression that includes the input expr that has
+// the given type exprType. If canChangeType is true, we may cast the input expr
+// to a different type. nullOk controls whether generating NULL is allowed.
+// If needsImmutable is true, we won't generate any scalar expressions that
+// are not immutable (always returning the same value given the same inputs).
+func randExpr(
+	rng *rand.Rand,
+	expr tree.Expr,
+	exprType *types.T,
+	canChangeType bool,
+	needsImmutable bool,
+	nullOk bool,
+) (tree.Expr, *types.T) {
+	typ := exprType
+	switch exprType.Family() {
 	case types.IntFamily, types.FloatFamily, types.DecimalFamily:
-		typ = xTyp
 		expr = &tree.BinaryExpr{
 			Operator: tree.MakeBinaryOperator(tree.Plus),
-			Left:     tree.NewUnresolvedName(string(x.Name)),
-			Right:    RandDatum(rng, xTyp, nullOk),
+			Left:     expr,
+			Right:    RandDatum(rng, exprType, nullOk),
 		}
 
 	case types.StringFamily:
-		typ = types.String
 		expr = &tree.FuncExpr{
 			Func:  tree.WrapFunction("lower"),
-			Exprs: tree.Exprs{tree.NewUnresolvedName(string(x.Name))},
+			Exprs: tree.Exprs{expr},
+		}
+
+	case types.IntervalFamily:
+		if !needsImmutable && rng.Intn(3) == 0 {
+			// Use now() just to make sure it's executable by adding the result of
+			// now()-now() to the input.
+			expr = &tree.BinaryExpr{
+				Operator: tree.MakeBinaryOperator(tree.Plus),
+				Left:     expr,
+				Right: &tree.BinaryExpr{
+					Operator: tree.MakeBinaryOperator(tree.Minus),
+					Left: &tree.FuncExpr{
+						Func: tree.WrapFunction("now"),
+					},
+					Right: &tree.FuncExpr{
+						Func: tree.WrapFunction("now"),
+					},
+				},
+			}
+		}
+
+	case types.TimestampFamily, types.DateFamily, types.TimestampTZFamily:
+		if !needsImmutable {
+			switch rng.Intn(6) {
+			case 0:
+				// Just use now() directly, no references.
+				expr = &tree.FuncExpr{
+					Func: tree.WrapFunction("now"),
+				}
+			case 1, 2:
+				// Use now() just to make sure it's executable by adding the result of
+				// now()-now() to the input.
+				expr = &tree.BinaryExpr{
+					Operator: tree.MakeBinaryOperator(tree.Plus),
+					Left:     expr,
+					Right: &tree.BinaryExpr{
+						Operator: tree.MakeBinaryOperator(tree.Minus),
+						Left: &tree.FuncExpr{
+							Func: tree.WrapFunction("now"),
+						},
+						Right: &tree.FuncExpr{
+							Func: tree.WrapFunction("now"),
+						},
+					},
+				}
+			}
 		}
 
 	default:
-		volatility, ok := tree.LookupCastVolatility(xTyp, types.String, nil /* sessionData */)
+		if !canChangeType {
+			return expr, exprType
+		}
+		volatility, ok := tree.LookupCastVolatility(exprType, types.String, nil /* sessionData */)
+		typ = types.String
 		if ok && volatility <= tree.VolatilityImmutable {
 			// We can cast to string; use lower(x::string)
-			typ = types.String
 			expr = &tree.FuncExpr{
 				Func: tree.WrapFunction("lower"),
 				Exprs: tree.Exprs{
 					&tree.CastExpr{
-						Expr: tree.NewUnresolvedName(string(x.Name)),
+						Expr: expr,
 						Type: types.String,
 					},
 				},
@@ -226,12 +290,11 @@ func randExpr(
 		} else {
 			// We cannot cast this type to string in a computed column expression.
 			// Use CASE WHEN x IS NULL THEN 'foo' ELSE 'bar'.
-			typ = types.String
 			expr = &tree.CaseExpr{
 				Whens: []*tree.When{
 					{
 						Cond: &tree.IsNullExpr{
-							Expr: tree.NewUnresolvedName(string(x.Name)),
+							Expr: expr,
 						},
 						Val: RandDatum(rng, types.String, nullOk),
 					},
@@ -240,6 +303,5 @@ func randExpr(
 			}
 		}
 	}
-
-	return expr, typ, nullability
+	return expr, typ
 }
