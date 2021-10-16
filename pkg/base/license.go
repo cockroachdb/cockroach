159c571c23a746822e82ac9a0131e5ca22e25ec2
David Hartunian <davidh@cockroachlabs.com>
2021-10-15 20:06:58 -0400

    server: create gauge metric for license expiry tracking
    Previously, the license expiry metric was manually appended to the
    Prometheus output resulting in some unexpected behavior. First, the
    expected Prometheus metadata that accompanies other metrics did not
    accompany this metric. Second, the metric was unavailable in the DB
    Console for viewing in custom graphs.
    
    This change updates the license expiry metric to use the standard
    `Metric` types and API. The metric is updated on the same cadence as our
    CPU and RAM metrics (every 10s by default).
    
    Resolves: #71525
    
    Release note (ops change): The license expiry metric is now available in
    the DB Console and includes the expected `HELP` and `TYPE` annotations
    in the promtheus output on `_status/vars`.

--- pkg/base/license.go
+++ pkg/base/license.go
@@ -15,6 +15,8 @@ import (
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/metric"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 	"github.com/cockroachdb/errors"
 )
@@ -30,6 +32,18 @@ var CheckEnterpriseEnabled = func(_ *cluster.Settings, _ uuid.UUID, org, feature
 	return errEnterpriseNotEnabled // nb: this is squarely in the hot path on OSS builds
 }
 
+var LicenseTTLMetadata = metric.Metadata{
+	// This metric name isn't namespaced for backwards
+	// compatibility. The prior version of this metric was manually
+	// inserted into the prometheus output
+	Name:        "seconds_until_enterprise_license_expiry",
+	Help:        "Seconds until enterprise license expiry (0 if no license present or running without enterprise features)",
+	Measurement: "Seconds",
+	Unit:        metric.Unit_SECONDS,
+}
+
+var LicenseTTL = metric.NewGauge(LicenseTTLMetadata)
+
 // TimeToEnterpriseLicenseExpiry returns a duration object that measures the time until
 // the currently set enterprise license expires starting from the 3rd argument
 // passed in.
@@ -39,6 +53,15 @@ var TimeToEnterpriseLicenseExpiry = func(
 	ctx context.Context, _ *cluster.Settings, _ time.Time,
 ) (time.Duration, error) {
 	return 0, nil
+}
+
+func UpdateTimeToLicenseExpiry(ctx context.Context, sv *cluster.Settings, asOf time.Time) {
+	durationToExpiry, err := TimeToEnterpriseLicenseExpiry(ctx, sv, asOf)
+	if err != nil {
+		log.Warningf(ctx, "unable to update license expiry gauge: %v", err)
+	}
+	secondsToExpiry := int64(durationToExpiry / time.Second)
+	LicenseTTL.Update(secondsToExpiry)
 }
 
 // LicenseType returns what type of license the cluster is running with, or
