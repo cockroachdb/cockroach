38c0529e1e3a35453d14a8e8f717cc7f94ef189f
David Hartunian <davidh@cockroachlabs.com>
2021-10-15 20:06:58 -0400

    server: create gauge metric for license expiry tracking
    Previously, the license expiry metric was manually appended to the
    Prometheus output resulting in some unexpected behavior. First, the
    expected Prometheus metadata that accompanies other metrics did not
    accompany this metric. Second, the metric was unavailable in the DB
    Console for viewing in custom graphs.
    
    This change updates the license expiry metric to use the standard
    `Metric` types and API. The metric is updated every minute using an
    async job initialized in the `UpdateMetricOnLicenseChange` which is only
    used in CCL clusters.
    
    Resolves: #71525
    
    Release note (ops change): The license expiry metric is now available in
    the DB Console and includes the expected `HELP` and `TYPE` annotations
    in the promtheus output on `_status/vars`.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -403,6 +403,12 @@ func NewServer(cfg Config, stopper *stop.Stopper) (*Server, error) {
 	runtimeSampler := status.NewRuntimeStatSampler(ctx, clock)
 	registry.AddMetricStruct(runtimeSampler)
 
+	registry.AddMetric(base.LicenseTTL)
+	err = base.UpdateMetricOnLicenseChange(ctx, cfg.Settings, base.LicenseTTL, timeutil.DefaultTimeSource{}, stopper)
+	if err != nil {
+		log.Errorf(ctx, "unable to initialize periodic license metric update: %v", err)
+	}
+
 	// A custom RetryOptions is created which uses stopper.ShouldQuiesce() as
 	// the Closer. This prevents infinite retry loops from occurring during
 	// graceful server shutdown
@@ -2660,6 +2666,7 @@ func startSampleEnvironment(ctx context.Context, cfg sampleEnvironmentCfg) error
 					curStats := goMemStats.Load().(*status.GoMemStats)
 					cgoStats := status.GetCGoMemStats(ctx)
 					cfg.runtime.SampleEnvironment(ctx, curStats, cgoStats)
+
 					if goroutineDumper != nil {
 						goroutineDumper.MaybeDump(ctx, cfg.st, cfg.runtime.Goroutines.Value())
 					}
