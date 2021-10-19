3d0a5aa523ac4be0d9a351b2eaccceca0169db91
Rui Hu <rui@cockroachlabs.com>
2021-10-19 15:19:37 -0400

    sql: add delete preserving index encoding, use for primary and secondary index
    As part of the transition for Bulk Ops to use only MVCC operations, the index
    backfiller must be rewritten. Part of the rewrite is to introduce a temporary
    index at the beginning of the backfill encoded with a new delete-preserving
    index encoding.
    
    The delete-preserving encoding results in index values being
    encoded with an additional bit that indicates whether or not the value has
    been deleted, which is necessary to preserve the delete history during the
    backfill.
    The entire RFC is here: https://github.com/cockroachdb/cockroach/blob/master/docs/RFCS/20211004_incremental_index_backfiller.md#new-index-encoding-for-deletions-vs-mvcc
    
    This patch adds this encoding and uses the encoding for writes
    to indices configured with this encoding.
    
    Release note: None

--- pkg/sql/row/deleter.go
+++ pkg/sql/row/deleter.go
@@ -129,10 +129,9 @@ func (rd *Deleter) DeleteRow(
 			return err
 		}
 		for _, e := range entries {
-			if traceKV {
-				log.VEventf(ctx, 2, "Del %s", keys.PrettyPrint(rd.Helper.secIndexValDirs[i], e.Key))
+			if err := rd.Helper.deleteIndexEntry(ctx, b, rd.Helper.Indexes[i], rd.Helper.secIndexValDirs[i], &e, traceKV); err != nil {
+				return err
 			}
-			b.Del(&e.Key)
 		}
 	}
 
@@ -185,10 +184,9 @@ func (rd *Deleter) DeleteIndexRow(
 	}
 
 	for _, entry := range secondaryIndexEntry {
-		if traceKV {
-			log.VEventf(ctx, 2, "Del %s", entry.Key)
+		if err := rd.Helper.deleteIndexEntry(ctx, b, idx, nil, &entry, traceKV); err != nil {
+			return err
 		}
-		b.Del(entry.Key)
 	}
 	return nil
 }
