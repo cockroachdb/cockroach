4fd3e1d02ad8a27ad084d467959e4286acddedef
Faizan Qazi <faizan@cockroachlabs.com>
2021-10-19 00:12:03 -0400

    sql: new schema changer does not properly use phases for drops
    Fixes: #66613
    
    Previously, when drop operations were added into the new schema
    changer we added all operations into the statement phase. This
    was inadequate because if an explicit transaction dropped
    a table and then most of the work would be applied before
    the transaction was committed potentially influencing
    other transactions. To address this, this patch splits the drop
    into between statement and pre-commit phases properly. In
    the pre-commit phase, a synthetic descriptor marks the
    object as dropped, so that only the transaction sees the
    drop. The pre-commit phase which executes before committing
    then marks the descriptor as dropped.
    
    Release note: None

--- pkg/sql/catalog/descs/synthetic_descriptors.go
+++ pkg/sql/catalog/descs/synthetic_descriptors.go
@@ -20,13 +20,24 @@ type syntheticDescriptors struct {
 	descs nstree.Map
 }
 
+func (sd *syntheticDescriptors) add(desc catalog.Descriptor) {
+	if mut, ok := desc.(catalog.MutableDescriptor); ok {
+		desc = mut.ImmutableCopy()
+		sd.descs.Upsert(desc)
+	} else {
+		// Already an immutable object.
+		sd.descs.Upsert(desc)
+	}
+}
+
+func (sd *syntheticDescriptors) remove(id descpb.ID) {
+	sd.descs.Remove(id)
+}
+
 func (sd *syntheticDescriptors) set(descs []catalog.Descriptor) {
 	sd.descs.Clear()
 	for _, desc := range descs {
-		if mut, ok := desc.(catalog.MutableDescriptor); ok {
-			desc = mut.ImmutableCopy()
-		}
-		sd.descs.Upsert(desc)
+		sd.add(desc)
 	}
 }
 
