a263c0ae406480b8367d00d7dbff166779af5a6f
Michael Butler <butler@cockroachlabs.com>
2021-10-20 14:11:55 -0400

    create export parquet processor
    This commit adds a new processor for exporting a relation as a parquet file,
    and adds a test for exporting a relation with bools, ints, floats, and strings.
    This commit also updates the fraugster parquet-go vendor dependency.
    Future work must ensure that all crdb data types, including arrays, can be
    exported properly.
    
    The fraugster/parquet-go vendor was specifically chosen because it allows us
    to  define the parquet schema at runtime. Specifically, we can write rows
    encoded as a dynamically created map[string]interface{}.
    
    Release note (sql change): adds ability to EXPORT PARQUET for relations with
    float, int, string column types.

---
+++ pkg/ccl/importccl/exportparquet.go
@@ -0,0 +1,416 @@
+// Copyright 2018 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package importccl
+
+import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/cockroachdb/cockroach/pkg/cloud"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/colinfo"
+	"github.com/cockroachdb/cockroach/pkg/sql/execinfra"
+	"github.com/cockroachdb/cockroach/pkg/sql/execinfrapb"
+	"github.com/cockroachdb/cockroach/pkg/sql/rowenc"
+	"github.com/cockroachdb/cockroach/pkg/sql/rowexec"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/builtins"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/types"
+	"github.com/cockroachdb/cockroach/pkg/util/tracing"
+	"github.com/cockroachdb/errors"
+	goparquet "github.com/fraugster/parquet-go"
+	"github.com/fraugster/parquet-go/parquet"
+	"github.com/fraugster/parquet-go/parquetschema"
+	"github.com/lib/pq/oid"
+)
+
+// parquetExporter data structure to augment the compression
+// and csv writer, encapsulating the internals to make
+// exporting oblivious for the consumers.
+type parquetExporter struct {
+	compressor     *gzip.Writer
+	buf            *bytes.Buffer
+	parquetWriter  *goparquet.FileWriter
+	parquetColumns []parquetColumn
+}
+
+// Write append record to parquet file.
+func (c *parquetExporter) Write(record map[string]interface{}) error {
+	return c.parquetWriter.AddData(record)
+}
+
+// Flush append record to parquet file.
+func (c *parquetExporter) Flush() error {
+	return nil
+}
+
+// Close closes the parquet writer
+func (c *parquetExporter) Close() error {
+	c.parquetWriter.Close()
+	return nil
+}
+
+// Bytes results in the slice of bytes with compressed content.
+func (c *parquetExporter) Bytes() []byte { // IDENTICAL TO CSV
+	return c.buf.Bytes()
+}
+
+func (c *parquetExporter) ResetBuffer() { // IDENTICAL
+	c.buf.Reset()
+}
+
+// Len returns length of the buffer with content. //IDENTICAL
+func (c *parquetExporter) Len() int {
+	return c.buf.Len()
+}
+
+func (c *parquetExporter) FileName(spec execinfrapb.ParquetWriterSpec, part string) string {
+	pattern := exportFilePatternDefault
+	if spec.NamePattern != "" {
+		pattern = spec.NamePattern
+	}
+
+	fileName := strings.Replace(pattern, exportFilePatternPart, part, -1)
+
+	return fileName
+}
+
+// parquetColumn contains the relevant data to map crdb table column to a parquet table column
+type parquetColumn struct {
+	name       string
+	crbdType   *types.T
+
+	//definition contains all relevant information around the parquet type for the table column
+	definition *parquetschema.ColumnDefinition
+
+	//encodeFn converts crbd table column value to a native go type. It's called by the processor.
+	encodeFn func(datum tree.Datum) (interface{}, error)
+}
+
+// newParquetExporter allocates a buffer, creates a new parquet file writer,
+// defines the parquet file schema, and packs all this info into a new parquetExporter
+func newParquetExporter(
+	sp execinfrapb.ParquetWriterSpec, typs []*types.T,
+) (*parquetExporter, error) {
+
+	parquetColumns := make([]parquetColumn, len(typs))
+	for i := 0; i < len(typs); i++ {
+		name := fmt.Sprintf("Col%d", i) // TODO(mbutler): figure out how to pass column names to export processor
+		parquetCol, err := newParquetColumn(typs[i], name)
+		if err != nil {
+			return nil, err
+		}
+		parquetColumns[i] = parquetCol
+	}
+
+	schema := newParquetSchema(parquetColumns)
+
+	var exporter *parquetExporter
+
+	buf := bytes.NewBuffer([]byte{})
+	pw := goparquet.NewFileWriter(buf,
+		goparquet.WithCompressionCodec(parquet.CompressionCodec_SNAPPY),
+		goparquet.WithSchemaDefinition(schema),
+	)
+
+	switch sp.CompressionCodec {
+	default:
+		{
+			exporter = &parquetExporter{
+				buf:            buf,
+				parquetWriter:  pw,
+				parquetColumns: parquetColumns,
+			}
+		}
+	}
+	return exporter, nil
+}
+
+// newParquetColumn populates a parquetColumn by finding the right parquet type and defining the
+// encodeFn
+func newParquetColumn(typ *types.T, name string) (parquetColumn, error) {
+	col := parquetColumn{}
+	col.definition = new(parquetschema.ColumnDefinition)
+	col.definition.SchemaElement = parquet.NewSchemaElement()
+	col.name = name
+	col.crbdType = typ
+
+	col.definition.SchemaElement.RepetitionType = parquet.FieldRepetitionTypePtr(parquet.FieldRepetitionType_REQUIRED)
+	col.definition.SchemaElement.Name = col.name
+
+	switch typ.Family() {
+	case types.BoolFamily:
+		col.definition.SchemaElement.Type = parquet.TypePtr(parquet.Type_BOOLEAN)
+		col.encodeFn = func(d tree.Datum) (interface{}, error) {
+			return bool(*d.(*tree.DBool)), nil
+		}
+
+	case types.StringFamily:
+		col.definition.SchemaElement.Type = parquet.TypePtr(parquet.Type_BYTE_ARRAY)
+		col.definition.SchemaElement.LogicalType = parquet.NewLogicalType()
+		col.definition.SchemaElement.LogicalType.STRING = parquet.NewStringType()
+		col.definition.SchemaElement.ConvertedType = parquet.ConvertedTypePtr(parquet.ConvertedType_UTF8)
+		col.encodeFn = func(d tree.Datum) (interface{}, error) {
+			return []byte(*d.(*tree.DString)), nil
+		}
+
+	case types.IntFamily:
+		if typ.Oid() == oid.T_int8 {
+			col.definition.SchemaElement.Type = parquet.TypePtr(parquet.Type_INT64)
+			col.encodeFn = func(d tree.Datum) (interface{}, error) {
+				return int64(*d.(*tree.DInt)), nil
+			}
+		} else {
+			col.definition.SchemaElement.Type = parquet.TypePtr(parquet.Type_INT32)
+			col.encodeFn = func(d tree.Datum) (interface{}, error) {
+				return int32(*d.(*tree.DInt)), nil
+			}
+		}
+
+	case types.FloatFamily:
+		if typ.Oid() == oid.T_float8 {
+			col.definition.SchemaElement.Type = parquet.TypePtr(parquet.Type_DOUBLE)
+			col.encodeFn = func(d tree.Datum) (interface{}, error) {
+				return float64(*d.(*tree.DFloat)), nil
+			}
+		} else {
+			col.definition.SchemaElement.Type = parquet.TypePtr(parquet.Type_FLOAT)
+			col.encodeFn = func(d tree.Datum) (interface{}, error) {
+				return float32(*d.(*tree.DFloat)), nil
+			}
+		}
+
+	case types.ArrayFamily:
+		// TODO(mb): Figure out how to modify the encodeFn for arrays.
+		// One possibility: recurse on type within array, define encodeFn elsewhere
+		// col.definition.Children[0] =  newParquetColumn(typ.ArrayContents(), name)
+		return col, errors.Errorf("parquet export does not support array type yet")
+
+	default:
+		return col, errors.Errorf("parquet export does not support type yet")
+	}
+
+	return col, nil
+}
+
+// newParquetSchema creates the schema for the parquet file,
+// see example schema:
+//     https://github.com/fraugster/parquet-go/issues/18#issuecomment-946013210
+// see docs here:
+//     https://pkg.go.dev/github.com/fraugster/parquet-go/parquetschema#SchemaDefinition
+func newParquetSchema(parquetFields []parquetColumn) *parquetschema.SchemaDefinition {
+
+	schemaDefinition := new(parquetschema.SchemaDefinition)
+	schemaDefinition.RootColumn = new(parquetschema.ColumnDefinition)
+	schemaDefinition.RootColumn.SchemaElement = parquet.NewSchemaElement()
+
+	for i := 0; i < len(parquetFields); i++ {
+		schemaDefinition.RootColumn.Children = append(schemaDefinition.RootColumn.Children,
+			parquetFields[i].definition)
+		schemaDefinition.RootColumn.SchemaElement.Name = "root"
+	}
+	return schemaDefinition
+}
+
+// MB note: basically the same as a csv writer processor
+func newParquetWriterProcessor(
+	flowCtx *execinfra.FlowCtx,
+	processorID int32,
+	spec execinfrapb.ParquetWriterSpec,
+	input execinfra.RowSource,
+	output execinfra.RowReceiver,
+) (execinfra.Processor, error) {
+	c := &parquetWriterProcessor{
+		flowCtx:     flowCtx,
+		processorID: processorID,
+		spec:        spec,
+		input:       input,
+		output:      output,
+	}
+	semaCtx := tree.MakeSemaContext()
+	if err := c.out.Init(&execinfrapb.PostProcessSpec{}, c.OutputTypes(), &semaCtx, flowCtx.NewEvalCtx()); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+type parquetWriterProcessor struct {
+	flowCtx     *execinfra.FlowCtx
+	processorID int32
+	spec        execinfrapb.ParquetWriterSpec
+	input       execinfra.RowSource
+	out         execinfra.ProcOutputHelper
+	output      execinfra.RowReceiver
+}
+
+var _ execinfra.Processor = &parquetWriterProcessor{}
+
+// MB: just copying this from csv exporter, not sure if it's correct
+func (sp *parquetWriterProcessor) OutputTypes() []*types.T {
+	res := make([]*types.T, len(colinfo.ExportColumns))
+	for i := range res {
+		res[i] = colinfo.ExportColumns[i].Typ
+	}
+	return res
+}
+
+// MB: copying from csv exporter. not sure what this does.
+func (sp *parquetWriterProcessor) MustBeStreaming() bool {
+	return false
+}
+
+func (sp *parquetWriterProcessor) Run(ctx context.Context) {
+	ctx, span := tracing.ChildSpan(ctx, "parquetWriter")
+	defer span.Finish()
+
+	instanceID := sp.flowCtx.EvalCtx.NodeID.SQLInstanceID()
+	uniqueID := builtins.GenerateUniqueInt(instanceID)
+
+	err := func() error {
+		typs := sp.input.OutputTypes()
+		sp.input.Start(ctx)
+		input := execinfra.MakeNoMetadataRowSource(sp.input, sp.output)
+		alloc := &rowenc.DatumAlloc{}
+
+		// TODO: Deal with NULL VALUES pt 1!!!
+		/*var nullsAs string
+		  if sp.spec.Options.NullEncoding != nil {
+		  	nullsAs = *sp.spec.Options.NullEncoding
+		  }*/
+
+		parquetRow := make(map[string]interface{}, len(typs))
+		chunk := 0
+		done := false
+		for {
+			var rows int64
+			// MB: a big difference from csv: for parquet, it's easiest to create a
+			// new ParquetExporter each time the processor creates a new parquet file,
+			// as the ParquetExporter contains metadata specific to that file
+			exporter, err := newParquetExporter(sp.spec, typs)
+			if err != nil {
+				return err
+			}
+
+			for {
+				// If the bytes.Buffer sink exceeds the target size of a Parquet file, we
+				// flush before exporting any additional rows.
+				if int64(exporter.buf.Len()) >= sp.spec.ChunkSize {
+					break
+				}
+				if sp.spec.ChunkRows > 0 && rows >= sp.spec.ChunkRows {
+					break
+				}
+				row, err := input.NextRow()
+				if err != nil {
+					return err
+				}
+				if row == nil {
+					done = true
+					break
+				}
+				rows++
+
+				for i, ed := range row {
+					// TODO: Deal with NULL VALUES pt 2!!!
+					/*if ed.IsNull() {
+						if sp.spec.Options.NullEncoding != nil {
+							parquetRow[colname] = &nullsAs // slightly diff from csv
+							continue
+						} else {
+							return errors.New("NULL value encountered during EXPORT, " +
+								"use `WITH nullas` to specify the string representation of NULL")
+						}
+					}*/
+					if err := ed.EnsureDecoded(typs[i], alloc); err != nil {
+						return err
+					}
+					edNative, err := exporter.parquetColumns[i].encodeFn(ed.Datum)
+					if err != nil {
+						return err
+					}
+					parquetRow[exporter.parquetColumns[i].name] = edNative
+				}
+				if err := exporter.Write(parquetRow); err != nil {
+					return err
+				}
+			}
+			if rows < 1 {
+				break
+			}
+			if err := exporter.Flush(); err != nil {
+				return errors.Wrap(err, "failed to flush parquet exporter")
+			}
+
+			conf, err := cloud.ExternalStorageConfFromURI(sp.spec.Destination, sp.spec.User())
+			if err != nil {
+				return err
+			}
+			es, err := sp.flowCtx.Cfg.ExternalStorage(ctx, conf)
+			if err != nil {
+				return err
+			}
+			defer es.Close()
+
+			part := fmt.Sprintf("n%d.%d", uniqueID, chunk)
+			chunk++
+			filename := exporter.FileName(sp.spec, part)
+
+			// Close exporter to ensure buffer and any compression footer is flushed.
+			err = exporter.Close()
+			if err != nil {
+				return errors.Wrapf(err, "failed to close exporting exporter")
+			}
+
+			size := exporter.Len()
+
+			if err := cloud.WriteFile(ctx, es, filename, bytes.NewReader(exporter.Bytes())); err != nil {
+				return err
+			}
+			res := rowenc.EncDatumRow{
+				rowenc.DatumToEncDatum(
+					types.String,
+					tree.NewDString(filename),
+				),
+				rowenc.DatumToEncDatum(
+					types.Int,
+					tree.NewDInt(tree.DInt(rows)),
+				),
+				rowenc.DatumToEncDatum(
+					types.Int,
+					tree.NewDInt(tree.DInt(size)),
+				),
+			}
+
+			cs, err := sp.out.EmitRow(ctx, res, sp.output)
+			if err != nil {
+				return err
+			}
+			if cs != execinfra.NeedMoreRows {
+				// TODO(dt): presumably this is because our recv already closed due to
+				// another error... so do we really need another one?
+				return errors.New("unexpected closure of consumer")
+			}
+			if done {
+				break
+			}
+		}
+
+		return nil
+	}()
+
+	// TODO(dt): pick up tracing info in trailing meta
+	execinfra.DrainAndClose(
+		ctx, sp.output, err, func(context.Context) {} /* pushTrailingMeta */, sp.input)
+}
+
+func init() {
+	rowexec.NewParquetWriterProcessor = newParquetWriterProcessor
+}
