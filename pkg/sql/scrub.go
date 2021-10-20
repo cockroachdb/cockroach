ac476c31e83f9c97bd6359d230d4fe845bad42fa
Radu Berinde <radu@cockroachlabs.com>
2021-10-20 13:40:10 -0700

    sql: capitalize rowContainerHelper API
    This change capitalizes the API of `rowContainerHelper` and
    `rowContainerIterator` to make it more clear what functions are not
    internal to the structure.
    
    Release note: None

--- pkg/sql/scrub.go
+++ pkg/sql/scrub.go
@@ -475,7 +475,7 @@ func scrubRunDistSQL(
 	ctx context.Context, planCtx *PlanningCtx, p *planner, plan *PhysicalPlan, columnTypes []*types.T,
 ) (*rowContainerHelper, error) {
 	var rowContainer rowContainerHelper
-	rowContainer.init(columnTypes, &p.extendedEvalCtx, "scrub" /* opName */)
+	rowContainer.Init(columnTypes, &p.extendedEvalCtx, "scrub" /* opName */)
 	rowResultWriter := NewRowResultWriter(&rowContainer)
 	recv := MakeDistSQLReceiver(
 		ctx,
@@ -496,10 +496,10 @@ func scrubRunDistSQL(
 		planCtx, p.txn, plan, recv, &evalCtxCopy, nil, /* finishedSetupFn */
 	)()
 	if rowResultWriter.Err() != nil {
-		rowContainer.close(ctx)
+		rowContainer.Close(ctx)
 		return nil, rowResultWriter.Err()
-	} else if rowContainer.len() == 0 {
-		rowContainer.close(ctx)
+	} else if rowContainer.Len() == 0 {
+		rowContainer.Close(ctx)
 		return nil, nil
 	}
 
