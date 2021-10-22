b545e81e954dfe1158a57c8a05d6a60829291d4d
Oleg Afanasyev <oleg@cockroachlabs.com>
2021-10-22 16:15:40 +0100

    cli: commands to perform unsafe recovery on quorum loss
    Previously, if range quorum was lost because too many replicas becoming
    permanently unavailable, cockroach could either be recovered from
    a backup, or debug-unsafe-remove-replicas command applied on each node
    to recover to a potentially inconsistent state.
    This command has some limitations on how it decides which replica is
    the most up to date one and in not usually recommended.
    This diff introduces the new process for the recovery. It doesn't
    guarantee consistency after recovery, but it provides additional checks
    and lays the foundation for further recovery work.
    
    Release note: None

---
+++ pkg/kv/kvserver/loqrecovery/recovery_test.go
@@ -0,0 +1,103 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package loqrecovery
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/loqrecovery/loqrecoverypb"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/datadriven"
+	"github.com/stretchr/testify/require"
+)
+
+func TestQuorumRecovery(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	datadriven.Walk(t, "testdata", func(t *testing.T, path string) {
+		env := quorumRecoveryEnv{}
+		datadriven.RunTest(t, path, func(t *testing.T, d *datadriven.TestData) string {
+			return env.Handle(t, *d)
+		})
+		env.cleanup()
+	})
+}
+
+func TestValidatePlanReplicaSet(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	desc := func(storeID int, replicaStores ...int) (result loqrecoverypb.LocalRangeInfo) {
+		result.StoreID = roachpb.StoreID(storeID)
+		result.NodeID = roachpb.NodeID(storeID)
+		var replicas []roachpb.ReplicaDescriptor
+		for i, rs := range replicaStores {
+			replicas = append(replicas, roachpb.ReplicaDescriptor{
+				NodeID: roachpb.NodeID(rs), StoreID: roachpb.StoreID(rs), ReplicaID: roachpb.ReplicaID(i)})
+		}
+		result.Desc = roachpb.RangeDescriptor{InternalReplicas: replicas}
+		return
+	}
+
+	node1 := []loqrecoverypb.LocalRangeInfo{desc(1, 1, 2, 3), desc(1, 3, 2, 1)}
+	node2 := []loqrecoverypb.LocalRangeInfo{desc(2, 1, 2, 3), desc(2, 2, 3, 1)}
+	node3 := []loqrecoverypb.LocalRangeInfo{desc(3, 3, 2, 1), desc(3, 2, 3, 1)}
+
+	cluster := func(nodes ...[]loqrecoverypb.LocalRangeInfo) (result []loqrecoverypb.LocalRangeInfo) {
+		for _, node := range nodes {
+			result = append(result, node...)
+		}
+		return
+	}
+
+	storeSet := func(storeIDs ...int) storeIDSet {
+		result := make(storeIDSet)
+		for _, storeID := range storeIDs {
+			result[roachpb.StoreID(storeID)] = struct{}{}
+		}
+		return result
+	}
+
+	stores := func(storeIDs ...int) (result []roachpb.StoreID) {
+		for _, storeID := range storeIDs {
+			result = append(result, roachpb.StoreID(storeID))
+		}
+		return
+	}
+
+	for _, td := range []struct {
+		name string
+		// Test data.
+		replicas   []loqrecoverypb.LocalRangeInfo
+		deadStores []roachpb.StoreID
+		// Outcomes.
+		liveStores      storeIDSet
+		missingStoreIDs storeIDSet
+		err             bool
+	}{
+		{name: "no_dead_stores", replicas: cluster(node1, node2, node3), deadStores: nil, liveStores: storeSet(1, 2, 3), missingStoreIDs: storeSet()},
+		{name: "no_explicit_dead_stores", replicas: cluster(node1, node2), deadStores: nil, liveStores: storeSet(1, 2), missingStoreIDs: storeSet(3)},
+		{name: "explicit_dead_stores_match_replicas", replicas: cluster(node2, node3), deadStores: stores(1), liveStores: storeSet(2, 3), missingStoreIDs: storeSet(1)},
+		{name: "explicit_dead_stores_with_replica_info", replicas: cluster(node1, node2), deadStores: stores(1), err: true},
+		{name: "explicit_dead_stores_mismatch", replicas: cluster(node1), deadStores: stores(2), err: true},
+	} {
+		t.Run(td.name, func(t *testing.T) {
+			liveStoreIDs, missingStoreIDs, err := validateReplicaSets(td.replicas, td.deadStores)
+			if td.err {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, td.liveStores, liveStoreIDs)
+				require.Equal(t, td.missingStoreIDs, missingStoreIDs)
+			}
+		})
+	}
+}
