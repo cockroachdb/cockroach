f702c63bcdf197df5972c5374c7f0dadfa7c2091
Oleg Afanasyev <oleg@cockroachlabs.com>
2021-10-22 16:15:40 +0100

    cli: commands to perform unsafe recovery on quorum loss
    Previously, if range quorum was lost because too many replicas becoming
    permanently unavailable, cockroach could either be recovered from
    a backup, or debug-unsafe-remove-replicas command applied on each node
    to recover to a potentially inconsistent state.
    This command has some limitations on how it decides which replica is
    the most up to date one and in not usually recommended.
    This diff introduces the new process for the recovery. It doesn't
    guarantee consistency after recovery, but it provides additional checks
    and lays the foundation for further recovery work.
    
    Release note: None

---
+++ pkg/kv/kvserver/loqrecovery/utils.go
@@ -0,0 +1,96 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package loqrecovery
+
+import (
+	"context"
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/storage"
+)
+
+type storeIDSet map[roachpb.StoreID]struct{}
+
+// storeListFromSet unwraps map to a sorted list of StoreIDs.
+func storeListFromSet(set storeIDSet) []roachpb.StoreID {
+	storeIDs := make([]roachpb.StoreID, 0, len(set))
+	for k := range set {
+		storeIDs = append(storeIDs, k)
+	}
+	sort.Slice(storeIDs, func(i, j int) bool {
+		return storeIDs[i] < storeIDs[j]
+	})
+	return storeIDs
+}
+
+// Make a string of stores 'set' in ascending order.
+func joinStoreIDs(storeIDs storeIDSet) string {
+	storeNames := make([]string, 0, len(storeIDs))
+	for _, id := range storeListFromSet(storeIDs) {
+		storeNames = append(storeNames, fmt.Sprintf("s%d", id))
+	}
+	return strings.Join(storeNames, ", ")
+}
+
+// UpdatableStore is a wrapper around storage.Engine providing ability
+// to lazily create batches and perform lifecycle management of the store
+// and the batch.
+type UpdatableStore struct {
+	storage storage.Engine
+	batch   storage.Batch
+}
+
+// NewUpdatableStore creates new UpdatableStore from storage.Engine
+func NewUpdatableStore(storage storage.Engine) *UpdatableStore {
+	return &UpdatableStore{storage: storage}
+}
+
+// GetStoreID reads roachpb.StoreIdent from underlying storage.
+// Hides underlying operation details from loqrecovery users.
+func (u *UpdatableStore) GetStoreID(ctx context.Context) (roachpb.StoreIdent, error) {
+	return kvserver.ReadStoreIdent(ctx, u.storage)
+}
+
+// Batch creates or returns existing batch to update storage.
+func (u *UpdatableStore) Batch() storage.Batch {
+	if u.batch == nil {
+		u.batch = u.storage.NewBatch()
+	}
+	return u.batch
+}
+
+// commit commits pending changes if available, otherwise does nothing.
+// returns true, nil if any changes were written
+// returns false, nil if store had no changes pending
+// returns false, err if pending changes failed to commit due to underlying
+// store problems.
+func (u *UpdatableStore) commit() (bool, error) {
+	if u.batch == nil || u.batch.Empty() {
+		return false, nil
+	}
+	if err := u.batch.Commit(true); err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+// Close closes storage and corresponding update batch if it was
+// open.
+func (u *UpdatableStore) Close() {
+	if u.batch != nil {
+		u.batch.Close()
+	}
+	u.storage.Close()
+}
