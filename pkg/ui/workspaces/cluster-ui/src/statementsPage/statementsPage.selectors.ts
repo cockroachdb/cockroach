92ff0bbde92cb4d54df5bf8b93862ead9c408ac3
Marylia Gutierrez <marylia@cockroachlabs.com>
2021-10-25 18:36:01 -0400

    ui: default filter on Transaction and Statement page exclude internals
    Previosuly, the default value when no App filter was
    selected on Transactions and Statements page, we were showing
    all data, now we're excluding internal transaction/statements.
    
    Fixes #70544
    
    Release note (ui change): The default value when no App is selected
    on Transactions and Statements filter is now excluding internal
    Transactions and Statements.

--- pkg/ui/workspaces/cluster-ui/src/statementsPage/statementsPage.selectors.ts
+++ pkg/ui/workspaces/cluster-ui/src/statementsPage/statementsPage.selectors.ts
@@ -169,6 +169,11 @@ export const selectStatements = createSelector(
           (showInternal && isInternal(statement)) ||
           criteria.includes(statement.app),
       );
+    } else {
+      // We don't want to show internal statements by default.
+      statements = statements.filter(
+        (statement: ExecutionStatistics) => !isInternal(statement),
+      );
     }
 
     const statsByStatementKey: {
