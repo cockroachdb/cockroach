87f79da6ab14a27b6f4faa9e9a47bc88671686f7
Radu Berinde <radu@cockroachlabs.com>
2021-10-25 10:20:18 -0700

    sql: enable EXPLAIN ANALYZE (DEBUG) for tenants
    EXPLAIN ANALYZE (DEBUG) was disabled for tenants because the returned URL
    was unusable.
    
    We can now download bundles through the SQL shell, so we can reenable
    EXPLAIN ANALYZE (DEBUG). For non-system tenants, we hide the URL and
    only show the SQL shell and cli options.
    
    As part of this change, we also add tenant support to the interactive
    cli test infrastructure and we implement the `--background` and
    `--pid-file` flags for `mt start-sql`.
    
    Fixes #70931.
    
    Release note (sql change): EXPLAIN ANALYZE (DEBUG) can now be used by
    tenants.

--- pkg/cli/interactive_tests/test_explain_analyze_debug.tcl
+++ pkg/cli/interactive_tests/test_explain_analyze_debug.tcl
@@ -47,4 +47,45 @@ eexpect eof
 
 end_test
 
+start_test "Ensure that EXPLAIN ANALYZE (DEBUG) works for a tenant"
+
+start_tenant 5 $argv
+
+spawn $argv sql --port [tenant_port 5]
+
+set client_spawn_id $spawn_id
+eexpect root@
+
+
+send "EXPLAIN ANALYZE (DEBUG) SELECT 1;\r"
+eexpect "Statement diagnostics bundle generated."
+expect -re "SQL shell: \\\\statement-diag download (\\d+)" {
+  set id $expect_out(1,string)
+}
+
+expect {
+  "warning: pq: unexpected DataRow in simple query execution" {
+    puts "Error: unexpected DataRow in simple query execution"
+    exit 1
+  }
+  "connection lost" {
+    puts "Error: connection lost"
+    exit 1
+  }
+  "root@" {
+  }
+}
+
+send "\\statement-diag download $id\r"
+eexpect "Bundle saved to"
+
+file_exists "stmt-bundle-$id.zip"
+
+interrupt
+eexpect eof
+
+stop_tenant 5 $argv
+
+end_test
+
 stop_server $argv
