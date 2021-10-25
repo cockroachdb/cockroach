1c00077526bc7ffb1be928d00e68d1dc473ebd34
Andrew Werner <awerner32@gmail.com>
2021-10-25 00:02:54 -0400

    server: adopt for settings rangefeed-backed settingswatcher, remove gossip
    This commit removes the code which connected the settings to their backing
    table via the gossipped system config. Instead it unconditionally enables the
    rangefeed-backed `settingswatcher` which was developed to support tenants.
    
    Note that it is rather tested code that has been used in multi-tenant sql
    pods for about a year now and all the existing tests still pass.
    
    Release note: None

--- pkg/server/settings_cache.go
+++ pkg/server/settings_cache.go
@@ -15,11 +15,25 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/server/settingswatcher"
+	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/storage"
 	"github.com/cockroachdb/cockroach/pkg/storage/enginepb"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
+	"github.com/cockroachdb/errors"
 )
+
+type settingsCacheWriter struct {
+	eng storage.Engine
+}
+
+func (s settingsCacheWriter) WriteKVs(ctx context.Context, kvs []roachpb.KeyValue) error {
+	return storeCachedSettingsKVs(ctx, s.eng, kvs)
+}
+
+var _ settingswatcher.Storage = (*settingsCacheWriter)(nil)
 
 // storeCachedSettingsKVs stores or caches node's settings locally.
 // This helps in restoring the node restart with the at least the same settings with which it died.
@@ -63,4 +77,23 @@ func loadCachedSettingsKVs(_ context.Context, eng storage.Engine) ([]roachpb.Key
 		return nil, err
 	}
 	return settingsKVs, nil
+}
+
+func initializeCachedSettings(
+	ctx context.Context, codec keys.SQLCodec, updater settings.Updater, kvs []roachpb.KeyValue,
+) error {
+	dec := settingswatcher.MakeRowDecoder(codec)
+	for _, kv := range kvs {
+		settings, val, valType, _, err := dec.DecodeRow(kv)
+		if err != nil {
+			return errors.Wrap(err, `while decoding settings data
+-this likely indicates the settings table structure or encoding has been altered;
+-skipping settings updates`)
+		}
+		if err := updater.Set(ctx, settings, val, valType); err != nil {
+			log.Warningf(ctx, "setting %q to %q failed: %+v", settings, val, err)
+		}
+	}
+	updater.ResetRemaining(ctx)
+	return nil
 }
