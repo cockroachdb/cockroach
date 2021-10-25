6afdf2b590417a4677b5ee4b5e30f6e4f7487427
Andrew Werner <awerner32@gmail.com>
2021-10-25 00:12:33 -0400

    server/settingswatcher: minor refactor
    Release note: None

--- pkg/server/settingswatcher/settings_watcher.go
+++ pkg/server/settingswatcher/settings_watcher.go
@@ -39,6 +39,9 @@ type SettingsWatcher struct {
 	f        *rangefeed.Factory
 	stopper  *stop.Stopper
 	dec      RowDecoder
+
+	// Running state, access underneath the rangefeed callback.
+	updater settings.Updater
 }
 
 // New constructs a new SettingsWatcher.
@@ -59,6 +62,50 @@ func New(
 	}
 }
 
+func (s *SettingsWatcher) onEntry(ctx context.Context, kv *roachpb.RangeFeedValue) {
+	k, val, valType, tombstone, err := s.dec.DecodeRow(roachpb.KeyValue{
+		Key:   kv.Key,
+		Value: kv.Value,
+	})
+	if err != nil {
+		log.Warningf(ctx, "failed to decode settings row %v: %v", kv.Key, err)
+	}
+	// This event corresponds to a deletion.
+	if tombstone {
+		s, ok := settings.Lookup(k, settings.LookupForLocalAccess)
+		if !ok {
+			log.Warningf(ctx, "failed to find setting %s, skipping update",
+				log.Safe(k))
+			return
+		}
+		ws, ok := s.(settings.NonMaskedSetting)
+		if !ok {
+			log.Fatalf(ctx, "expected writable setting, got %T", s)
+		}
+		val, valType = ws.EncodedDefault(), ws.Typ()
+	}
+
+	// The system tenant (i.e. the KV layer) does not use the SettingsWatcher
+	// to propagate cluster version changes (it uses the BumpClusterVersion
+	// RPC). However, non-system tenants (i.e. SQL pods) (asynchronously) get
+	// word of the new cluster version below.
+	const versionSettingKey = "version"
+	if k == versionSettingKey && !s.codec.ForSystemTenant() {
+		var v clusterversion.ClusterVersion
+		if err := protoutil.Unmarshal([]byte(val), &v); err != nil {
+			log.Warningf(ctx, "failed to set cluster version: %v", err)
+		} else if err := s.settings.Version.SetActiveVersion(ctx, v); err != nil {
+			log.Warningf(ctx, "failed to set cluster version: %v", err)
+		} else {
+			log.Infof(ctx, "set cluster version to: %v", v)
+		}
+	} else if err := s.updater.Set(ctx, k, val, valType); err != nil {
+		log.Warningf(ctx, "failed to set setting %s to %s: %v",
+			log.Safe(k), val, err)
+	}
+
+}
+
 // Start will start the SettingsWatcher. It returns after the initial settings
 // have been retrieved. An error will be returned if the context is canceled or
 // the stopper is stopped prior to the initial data being retrieved.
@@ -68,76 +115,45 @@ func (s *SettingsWatcher) Start(ctx context.Context) error {
 		Key:    settingsTablePrefix,
 		EndKey: settingsTablePrefix.PrefixEnd(),
 	}
-	now := s.clock.Now()
-	u := s.settings.MakeUpdater()
-	initialScanDone := make(chan struct{})
-	var initialScanErr error
-	rf, err := s.f.RangeFeed(ctx, "settings", []roachpb.Span{settingsTableSpan}, now, func(
-		ctx context.Context, kv *roachpb.RangeFeedValue,
-	) {
-		k, val, valType, tombstone, err := s.dec.DecodeRow(roachpb.KeyValue{
-			Key:   kv.Key,
-			Value: kv.Value,
-		})
-		if err != nil {
-			log.Warningf(ctx, "failed to decode settings row %v: %v", kv.Key, err)
-		}
-		// This event corresponds to a deletion.
-		if tombstone {
-			s, ok := settings.Lookup(k, settings.LookupForLocalAccess)
-			if !ok {
-				log.Warningf(ctx, "failed to find setting %s, skipping update",
-					log.Safe(k))
-				return
-			}
-			ws, ok := s.(settings.NonMaskedSetting)
-			if !ok {
-				log.Fatalf(ctx, "expected writable setting, got %T", s)
-			}
-			val, valType = ws.EncodedDefault(), ws.Typ()
-		}
+	s.updater = s.settings.MakeUpdater()
 
-		// The system tenant (i.e. the KV layer) does not use the SettingsWatcher
-		// to propagate cluster version changes (it uses the BumpClusterVersion
-		// RPC). However, non-system tenants (i.e. SQL pods) (asynchronously) get
-		// word of the new cluster version below.
-		const versionSettingKey = "version"
-		if k == versionSettingKey && !s.codec.ForSystemTenant() {
-			var v clusterversion.ClusterVersion
-			if err := protoutil.Unmarshal([]byte(val), &v); err != nil {
-				log.Warningf(ctx, "failed to set cluster version: %v", err)
-			} else if err := s.settings.Version.SetActiveVersion(ctx, v); err != nil {
-				log.Warningf(ctx, "failed to set cluster version: %v", err)
-			} else {
-				log.Infof(ctx, "set cluster version to: %v", v)
+	var (
+		initialScanDoneCh = make(chan struct{})
+		initialScanFunc   = func(ctx context.Context) {
+			s.updater.ResetRemaining(ctx)
+			close(initialScanDoneCh)
+		}
+		initialScanErr       error
+		initialScanErrorFunc = func(
+			ctx context.Context, err error,
+		) (shouldFail bool) {
+			// TODO(ajwerner): Consider if there are other errors which we want to
+			// treat as permanent.
+			if grpcutil.IsAuthError(err) ||
+				// This is a hack around the fact that we do not get properly structured
+				// errors out of gRPC. See #56208.
+				strings.Contains(err.Error(), "rpc error: code = Unauthenticated") {
+				initialScanErr = err
+				close(initialScanDoneCh)
+				shouldFail = true
 			}
-		} else if err := u.Set(ctx, k, val, valType); err != nil {
-			log.Warningf(ctx, "failed to set setting %s to %s: %v",
-				log.Safe(k), val, err)
+			return shouldFail
 		}
-	}, rangefeed.WithInitialScan(func(ctx context.Context) {
-		u.ResetRemaining(ctx)
-		close(initialScanDone)
-	}), rangefeed.WithOnInitialScanError(func(
-		ctx context.Context, err error,
-	) (shouldFail bool) {
-		// TODO(ajwerner): Consider if there are other errors which we want to
-		// treat as permanent.
-		if grpcutil.IsAuthError(err) ||
-			// This is a hack around the fact that we do not get properly structured
-			// errors out of gRPC. See #56208.
-			strings.Contains(err.Error(), "rpc error: code = Unauthenticated") {
-			initialScanErr = err
-			close(initialScanDone)
-			shouldFail = true
-		}
-		return shouldFail
-	}))
+	)
+	opts := []rangefeed.Option{
+		rangefeed.WithInitialScan(initialScanFunc),
+		rangefeed.WithOnInitialScanError(initialScanErrorFunc),
+	}
+
+	now := s.clock.Now()
+	rf, err := s.f.RangeFeed(
+		ctx, "settings", []roachpb.Span{settingsTableSpan}, now, s.onEntry, opts...,
+	)
 	if err != nil {
 		return err
 	}
 	select {
-	case <-initialScanDone:
+	case <-initialScanDoneCh:
 		if initialScanErr != nil {
 			return initialScanErr
 		}
