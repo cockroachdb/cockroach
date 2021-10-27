70f3919e1218b85cb1c2aff8f72cd0f770769a71
Alex Wong <alex.wong@cockroachlabs.com>
2021-10-27 16:31:00 -0400

    WIP pubsub implementation
    <what was there before: Previously, ...>
    <why it needed to change: This was inadequate because ...>
    <what you did about it: To address this, this patch ...>
    
    Release note (<category, see below>): <what> <show> <why>

--- pkg/ccl/changefeedccl/sink_pubsub.go
+++ pkg/ccl/changefeedccl/sink_pubsub.go
@@ -7,20 +7,16 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/ccl/changefeedccl/kvevent"
 	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
-	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"golang.org/x/oauth2/google"
+	"hash/crc32"
+	"net/url"
 	"path"
 	"regexp"
 	"strings"
-	"hash/crc32"
-
-	//"github.com/cockroachdb/cockroach/pkg/util/syncutil"
-	"net/url"
 
 	_ "cloud.google.com/go/pubsub"
 	gcpClient "cloud.google.com/go/pubsub/apiv1"
 	"github.com/cockroachdb/cockroach/pkg/ccl/changefeedccl/changefeedbase"
-	//"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/errors"
 	"gocloud.dev/pubsub"
 	"gocloud.dev/pubsub/gcppubsub"
@@ -38,6 +34,7 @@ var (
 	shortTopicPathRE = regexp.MustCompile("^[^/]+/[^/]+$")
 )
 
+//isPubsubSInk returns true if url contains scheme with valid pubsub sink
 func isPubsubSink(u *url.URL) bool {
 	switch u.Scheme {
 	case gcpScheme, memScheme:
@@ -47,11 +44,13 @@ func isPubsubSink(u *url.URL) bool {
 	}
 }
 
+//payload struct is sent to the sink
 type payload struct {
 	Key json.RawMessage `json:"key"`
 	Value json.RawMessage `json:"value"`
 }
 
+//pubsubMessage is sent to worker channels for workers to consume
 type pubsubMessage struct {
 	alloc kvevent.Alloc
 	message payload
@@ -97,7 +96,7 @@ type pubsubSink struct {
 	workerGroup ctxgroup.Group
 
 	exitWorkers func() // Signaled to shut down all workers.
-	eventsChans []chan pubsubMessage
+	eventsChans []chan pubsubMessage //channel where messages are consumed and sent out
 
 	// flushDone channel signaled when flushing completes.
 	flushDone chan struct{}
@@ -107,6 +106,7 @@ type pubsubSink struct {
 
 }
 
+//getGCPCredentials returns gcp credentials parsed out from url
 func getGCPCredentials(q *url.Values, ctx context.Context) (*google.Credentials, error){
 	var credsBase64 string
 	if credsBase64 = q.Get(credentialsParam); credsBase64 == "" {
@@ -140,6 +140,8 @@ func parseGCPURL(u *url.URL) (string, error) {
 	return "", errors.Errorf("could not parse project and topic from %s", fullPath)
 }
 
+
+//MakePubsubSink returns the corresponding pubsub sink based on the url given
 func MakePubsubSink(ctx context.Context, u *url.URL, opts map[string]string) (Sink, error) {
 
 	switch changefeedbase.FormatType(opts[changefeedbase.OptFormat]) {
@@ -163,11 +165,14 @@ func MakePubsubSink(ctx context.Context, u *url.URL, opts map[string]string) (Si
 	q := u.Query()
 	ctx, cancel := context.WithCancel(ctx)
 	p := &pubsubSink{workerCtx: ctx, url: u, numWorkers: 10, exitWorkers: cancel}
+	p.setupWorkers()
 
+	//creates custom pubsub object based on scheme
 	switch u.Scheme {
 	case gcpScheme:
 		creds, err := getGCPCredentials(&q, ctx)
 		if err != nil {
+			_ = p.Close()
 			return nil, err
 		}
 		g := &GcpPubsubSink{pubsubDesc: p, creds: creds}
@@ -176,22 +181,27 @@ func MakePubsubSink(ctx context.Context, u *url.URL, opts map[string]string) (Si
 		m := &MemPubsubSink{pubsubDesc: p}
 		return m, nil
 	default:
+		_ = p.Close()
 		return nil, errors.Errorf("unknown scheme: %s", u.Scheme)
 	}
 }
 
+//getWorkerCtx returns workerCtx
 func (p *pubsubSink)getWorkerCtx() context.Context{
 	return p.workerCtx
 }
 
+//getUrl returns url
 func (p *pubsubSink)getUrl() *url.URL{
 	return p.url
 }
 
+//setTopic sets the topic with the argument passed in
 func (p *pubsubSink)setTopic(topic *pubsub.Topic){
 	p.topic = topic
 }
 
+//EmitRow pushes a message to event channel where it is consumed by workers
 func (p *pubsubSink) 	EmitRow(
 	ctx context.Context,
 	_ TopicDescriptor,
@@ -199,11 +209,11 @@ func (p *pubsubSink) 	EmitRow(
 	_ hlc.Timestamp,
 	alloc kvevent.Alloc,
 ) error {
-	log.Warning(ctx, "emitting row")
 	m := pubsubMessage{alloc: alloc, isFlush: false, message: payload{
 		Key: key,
 		Value: value,
 	}}
+	// calculate index by hashing key
 	i := p.workerIndex(key)
 	select {
 	// check the webhook sink context in case workers have been terminated
@@ -214,12 +224,15 @@ func (p *pubsubSink) 	EmitRow(
 	case <-ctx.Done():
 		return ctx.Err()
 	case err := <-p.errChan:
+		// check if there are any errors with sink
 		return err
 	case p.eventsChans[i] <- m:
+		// send message to event channel
 	}
 	return nil
 }
 
+//EmitResolvedTimestamp sends resolved timestamp message
 func (p *pubsubSink) EmitResolvedTimestamp(ctx context.Context, encoder Encoder, resolved hlc.Timestamp) error {
 	payload, err := encoder.EncodeResolvedTimestamp(ctx, "", resolved)
 	if err != nil {
@@ -232,8 +245,8 @@ func (p *pubsubSink) EmitResolvedTimestamp(ctx context.Context, encoder Encoder,
 	return nil
 }
 
+//Flush blocks until all messages in the event channels are sent
 func (p *pubsubSink) Flush(ctx context.Context) error {
-	log.Warning(ctx, "starting flush")
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
@@ -245,7 +258,6 @@ func (p *pubsubSink) Flush(ctx context.Context) error {
 			return err
 		}
 	}
-	log.Warning(ctx, "done sending flushes to all channels")
 
 	select {
 	case <-ctx.Done():
@@ -258,6 +270,7 @@ func (p *pubsubSink) Flush(ctx context.Context) error {
 
 }
 
+//Close closes all the channels and shutdowns the topic
 func (p *pubsubSink) Close() error {
 	_ = p.workerGroup.Wait()
 	close(p.errChan)
@@ -272,6 +285,7 @@ func (p *pubsubSink) Close() error {
 	return nil
 }
 
+//sendMessage sends a message to the topic
 func (p *pubsubSink) sendMessage(m []byte) error {
 	err := p.topic.Send(p.workerCtx, &pubsub.Message{
 		Body: m,
@@ -282,6 +296,7 @@ func (p *pubsubSink) sendMessage(m []byte) error {
 	return nil
 }
 
+//setupWorkers sets up the channels used by the sink and starts a goroutine for every worker
 func (p *pubsubSink) setupWorkers() {
 	// setup events channels to send to workers and the worker group
 	p.eventsChans = make([]chan pubsubMessage, p.numWorkers)
@@ -291,9 +306,10 @@ func (p *pubsubSink) setupWorkers() {
 	p.errChan = make(chan error, 1)
 
 	// flushDone notified when flush completes.
-	p.flushDone = make(chan struct{})
+	p.flushDone = make(chan struct{}, 1)
 
 	for i := 0; i < p.numWorkers; i++ {
+		//initialize worker goroutine and channel for worker
 		p.eventsChans[i] = make(chan pubsubMessage)
 		j := i
 		p.workerGroup.GoCtx(func(ctx context.Context) error {
@@ -303,6 +319,7 @@ func (p *pubsubSink) setupWorkers() {
 	}
 }
 
+//workerLoop consumes any message sent to the channel corresponding to the worker index
 func (p *pubsubSink) workerLoop(workerIndex int) {
 	for {
 		select {
@@ -313,48 +330,54 @@ func (p *pubsubSink) workerLoop(workerIndex int) {
 				// Signals a flush request, makes sure that the messages in eventsChans are finished sending
 				continue
 			}
-			m := payload{Key: msg.message.Key, Value: msg.message.Value}
-			b, err := json.Marshal(m)
-			if err != nil {
-				p.exitWorkersWithError(err)
-			}
-			err = p.sendMessage(b)
-			if err != nil {
-				p.exitWorkersWithError(err)
-			}
 
-			msg.alloc.Release(p.workerCtx)
+			//inline func to trigger the defer alloc release when it loops
+			func(){
+				defer msg.alloc.Release(p.workerCtx)
+				m := payload{Key: msg.message.Key, Value: msg.message.Value}
+				b, err := json.Marshal(m)
+				if err != nil {
+					p.exitWorkersWithError(err)
+				}
+				err = p.sendMessage(b)
+				if err != nil {
+					p.exitWorkersWithError(err)
+				}
+			}()
 		}
 	}
 }
 
+//exitWorkersWithError sends an error to the sink error channel
 func (p *pubsubSink) exitWorkersWithError(err error) {
 	// errChan has buffer size 1, first error will be saved to the buffer and
 	// subsequent errors will be ignored
 	select {
 	case p.errChan <- err:
-		log.Warning(p.workerCtx, "exiting workers")
 		p.exitWorkers()
 	default:
 	}
 }
 
+//sinkError checks if there is an error in the error channel
 func (p *pubsubSink) sinkError() error {
 	select {
 	case err := <-p.errChan:
 		return err
 	default:
-		return nil
 	}
+	return nil
 }
 
+//workerIndex hashes key to return a worker index
 func (p *pubsubSink) workerIndex(key []byte) uint32 {
 	return crc32.ChecksumIEEE(key) % uint32(p.numWorkers)
 }
 
+//flushWorkers sends a flush message to every worker channel and then signals sink that flush is done
 func (p *pubsubSink) flushWorkers() error{
 	for i := 0; i < p.numWorkers; i++{
-		log.Warning(p.workerCtx, "flushing worker")
+		//flush message will be blocked until all the messages in the channel are processed
 		select {
 		case <-p.workerCtx.Done():
 			return p.workerCtx.Err()
@@ -362,16 +385,16 @@ func (p *pubsubSink) flushWorkers() error{
 		}
 	}
 	select {
+	//signals sink that flush is complete
 	case <-p.workerCtx.Done():
 		return p.workerCtx.Err()
 	case p.flushDone <- struct{}{}:
-		log.Warning(p.workerCtx, "sending flush done")
 		return nil
 	}
 }
 
+//Dial connects to gcp client and opens a topic
 func (p *GcpPubsubSink)Dial() error{
-	p.pubsubDesc.setupWorkers()
 	// Open a gRPC connection to the GCP Pub/Sub API.
 	conn, cleanup, err := gcppubsub.Dial(p.pubsubDesc.getWorkerCtx(), p.creds.TokenSource)
 	if err != nil {
@@ -404,6 +427,7 @@ func (p *GcpPubsubSink)Dial() error{
 	return nil
 }
 
+//EmitRow calls the pubsubDesc EmitRow
 func (p *GcpPubsubSink) 	EmitRow(
 	ctx context.Context,
 	topic TopicDescriptor,
@@ -418,14 +442,17 @@ func (p *GcpPubsubSink) 	EmitRow(
 	return nil
 }
 
+//EmitResolvedTimestamp calls the pubsubDesc EmitResolvedTimestamp
 func (p *GcpPubsubSink) EmitResolvedTimestamp(ctx context.Context, encoder Encoder, resolved hlc.Timestamp) error {
 	return p.pubsubDesc.EmitResolvedTimestamp(ctx, encoder, resolved)
 }
 
+//Flush calls the pubsubDesc Flush
 func (p *GcpPubsubSink) Flush(ctx context.Context) error {
 	return p.pubsubDesc.Flush(ctx)
 }
 
+//Close calls the pubsubDesc Close and closes the client and connection
 func (p *GcpPubsubSink) Close() error {
 	if p.pubsubDesc != nil {
 		err := p.pubsubDesc.Close()
@@ -435,15 +462,9 @@ func (p *GcpPubsubSink) Close() error {
 	}
 	if p.client != nil {
 		p.client.Close()
-		//if err != nil {
-		//	return errors.Wrapf(err, "closing gcp client")
-		//}
 	}
 	if p.conn != nil {
 		p.conn.Close()
-		//if err != nil {
-		//	return errors.Wrapf(err, "closing gcp connection")
-		//}
 	}
 	if p.cleanup != nil {
 		p.cleanup()
@@ -451,6 +472,7 @@ func (p *GcpPubsubSink) Close() error {
 	return nil
 }
 
+//Dial opens topic using url
 func (p *MemPubsubSink)Dial() error{
 	topic, err := pubsub.OpenTopic(p.pubsubDesc.getWorkerCtx(), p.pubsubDesc.getUrl().String())
 	if err != nil {
@@ -461,6 +483,7 @@ func (p *MemPubsubSink)Dial() error{
 	return nil
 }
 
+//EmitRow calls the pubsubDesc EmitRow
 func (p *MemPubsubSink) 	EmitRow(
 	ctx context.Context,
 	topic TopicDescriptor,
@@ -475,14 +498,17 @@ func (p *MemPubsubSink) 	EmitRow(
 	return nil
 }
 
+//EmitResolvedTimestamp calls the pubsubDesc EmitResolvedTimestamp
 func (p *MemPubsubSink) EmitResolvedTimestamp(ctx context.Context, encoder Encoder, resolved hlc.Timestamp) error {
 	return p.EmitResolvedTimestamp(ctx, encoder, resolved)
 }
 
+//Flush calls the pubsubDesc Flush
 func (p *MemPubsubSink) Flush(ctx context.Context) error {
 	return p.pubsubDesc.Flush(ctx)
 }
 
+//Close calls the pubsubDesc Close
 func (p *MemPubsubSink) Close() error {
 	if p.pubsubDesc != nil {
 		err := p.pubsubDesc.Close()
