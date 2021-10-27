71f424aa13c3876db75eaa612e5523fdd244d163
Andrew Werner <awerner32@gmail.com>
2021-10-27 13:50:27 -0400

    jobs: add mechanism to communicate job completion in-memory locally
    This change does two things. Firstly, when local transactions create jobs
    which are pre-claimed by the current registry, there's no need for the
    registry to go search to find these job IDs. Instead, it can just directly
    attempt to resume them. This nicely avoids a bunch of contention. Then,
    when we wait for the jobs to complete, we can avoid polling the status
    of the jobs table in the common case and instead wait for an in-memory
    notification. This is beneficial because it reduces contention on the
    job record of the running job.
    
    Release note (performance improvement): Improved job performance in the
    face of concurrent schema changes by reducing contention.

--- pkg/sql/schema_changer.go
+++ pkg/sql/schema_changer.go
@@ -491,8 +491,8 @@ func startGCJob(
 	}); err != nil {
 		return err
 	}
-	log.Infof(ctx, "starting GC job %d", jobID)
-	jobRegistry.NotifyToAdoptJobs()
+	log.Infof(ctx, "created GC job %d", jobID)
+	jobRegistry.NotifyToResume(ctx, jobID)
 	return nil
 }
 
@@ -886,7 +886,7 @@ func (sc *SchemaChanger) rollbackSchemaChange(ctx context.Context, err error) er
 		return err
 	}
 	log.Infof(ctx, "starting GC job %d", gcJobID)
-	sc.jobRegistry.NotifyToAdoptJobs()
+	sc.jobRegistry.NotifyToResume(ctx, gcJobID)
 	return nil
 }
 
@@ -1003,6 +1003,7 @@ func (sc *SchemaChanger) createIndexGCJob(
 		return err
 	}
 	log.Infof(ctx, "created index GC job %d", jobID)
+	sc.jobRegistry.NotifyToResume(ctx, jobID)
 	return nil
 }
 
@@ -1018,13 +1019,14 @@ func WaitToUpdateLeases(
 		MaxBackoff:     time.Second,
 		Multiplier:     1.5,
 	}
+	start := timeutil.Now()
 	log.Infof(ctx, "waiting for a single version...")
 	desc, err := leaseMgr.WaitForOneVersion(ctx, descID, retryOpts)
 	var version descpb.DescriptorVersion
 	if desc != nil {
 		version = desc.GetVersion()
 	}
-	log.Infof(ctx, "waiting for a single version... done (at v %d)", version)
+	log.Infof(ctx, "waiting for a single version... done (at v %d), took %v", version, timeutil.Since(start))
 	return desc, err
 }
 
@@ -1040,9 +1042,12 @@ func (sc *SchemaChanger) done(ctx context.Context) error {
 	// descriptor updates are published.
 	var didUpdate bool
 	var depMutationJobs []jobspb.JobID
+	var otherJobIDs []jobspb.JobID
 	err := sc.execCfg.CollectionFactory.Txn(ctx, sc.execCfg.InternalExecutor, sc.db, func(
 		ctx context.Context, txn *kv.Txn, descsCol *descs.Collection,
 	) error {
+		depMutationJobs = depMutationJobs[:0]
+		otherJobIDs = otherJobIDs[:0]
 		var err error
 		scTable, err := descsCol.GetMutableTableVersionByID(ctx, sc.descID, txn)
 		if err != nil {
@@ -1222,8 +1227,12 @@ func (sc *SchemaChanger) done(ctx context.Context) error {
 
 				// If we performed MakeMutationComplete on a PrimaryKeySwap mutation, then we need to start
 				// a job for the index deletion mutations that the primary key swap mutation added, if any.
-				if err := sc.queueCleanupJobs(ctx, scTable, txn); err != nil {
+				jobID, err := sc.queueCleanupJob(ctx, scTable, txn)
+				if err != nil {
 					return err
+				}
+				if jobID > 0 {
+					depMutationJobs = append(depMutationJobs, jobID)
 				}
 			}
 
@@ -1235,8 +1244,12 @@ func (sc *SchemaChanger) done(ctx context.Context) error {
 				// If we performed MakeMutationComplete on a computed column swap, then
 				// we need to start a job for the column deletion that the swap mutation
 				// added if any.
-				if err := sc.queueCleanupJobs(ctx, scTable, txn); err != nil {
+				jobID, err := sc.queueCleanupJob(ctx, scTable, txn)
+				if err != nil {
 					return err
+				}
+				if jobID > 0 {
+					depMutationJobs = append(depMutationJobs, jobID)
 				}
 			}
 			didUpdate = true
@@ -1252,10 +1265,11 @@ func (sc *SchemaChanger) done(ctx context.Context) error {
 
 		// Check any jobs that we need to depend on for the current
 		// job to be successful.
-		depMutationJobs, err = sc.getDependentMutationsJobs(ctx, scTable, committedMutations)
+		existingDepMutationJobs, err := sc.getDependentMutationsJobs(ctx, scTable, committedMutations)
 		if err != nil {
 			return err
 		}
+		depMutationJobs = append(depMutationJobs, existingDepMutationJobs...)
 
 		for i, g := range scTable.MutationJobs {
 			if g.MutationID == sc.mutationID {
@@ -1332,13 +1346,11 @@ func (sc *SchemaChanger) done(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	// Notify the job registry to start jobs, in case we started any.
-	sc.jobRegistry.NotifyToAdoptJobs()
 
 	// If any operations was skipped because a mutation was made
 	// redundant due to a column getting dropped later on then we should
 	// wait for those jobs to complete before returning our result back.
-	if err := sc.jobRegistry.WaitForJobs(ctx, sc.execCfg.InternalExecutor, depMutationJobs); err != nil {
+	if err := sc.jobRegistry.Run(ctx, sc.execCfg.InternalExecutor, depMutationJobs); err != nil {
 		return errors.Wrap(err, "A dependent transaction failed for this schema change")
 	}
 
@@ -2312,11 +2324,11 @@ func init() {
 	jobs.RegisterConstructor(jobspb.TypeSchemaChange, createResumerFn)
 }
 
-// queueCleanupJobs checks if the completed schema change needs to start a
+// queueCleanupJob checks if the completed schema change needs to start a
 // child job to clean up dropped schema elements.
-func (sc *SchemaChanger) queueCleanupJobs(
+func (sc *SchemaChanger) queueCleanupJob(
 	ctx context.Context, scDesc *tabledesc.Mutable, txn *kv.Txn,
-) error {
+) (jobspb.JobID, error) {
 	// Create jobs for dropped columns / indexes to be deleted.
 	mutationID := scDesc.ClusterVersion.NextMutationID
 	span := scDesc.PrimaryIndexSpan(sc.execCfg.Codec)
@@ -2330,6 +2342,7 @@ func (sc *SchemaChanger) queueCleanupJobs(
 	}
 	// Only start a job if spanList has any spans. If len(spanList) == 0, then
 	// no mutations were enqueued by the primary key change.
+	var jobID jobspb.JobID
 	if len(spanList) > 0 {
 		jobRecord := jobs.Record{
 			Description:   fmt.Sprintf("CLEANUP JOB for '%s'", sc.job.Payload().Description),
@@ -2346,9 +2359,9 @@ func (sc *SchemaChanger) queueCleanupJobs(
 			Progress:      jobspb.SchemaChangeProgress{},
 			NonCancelable: true,
 		}
-		jobID := sc.jobRegistry.MakeJobID()
+		jobID = sc.jobRegistry.MakeJobID()
 		if _, err := sc.jobRegistry.CreateJobWithTxn(ctx, jobRecord, jobID, txn); err != nil {
-			return err
+			return 0, err
 		}
 		log.Infof(ctx, "created job %d to drop previous columns and indexes", jobID)
 		scDesc.MutationJobs = append(scDesc.MutationJobs, descpb.TableDescriptor_MutationJob{
@@ -2356,7 +2369,7 @@ func (sc *SchemaChanger) queueCleanupJobs(
 			JobID:      int64(jobID),
 		})
 	}
-	return nil
+	return jobID, nil
 }
 
 func (sc *SchemaChanger) applyZoneConfigChangeForMutation(
