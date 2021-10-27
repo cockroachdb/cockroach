7b41f8f394f9dcfe08064a97385e9733d6e8bd9d
Xun He <xun@cockroachlabs.com>
2021-10-26 20:22:19 -0700

    Add label to roachprod

--- pkg/roachprod/vm/vm.go
+++ pkg/roachprod/vm/vm.go
@@ -26,6 +26,26 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+const (
+	// TagCluster is cluster name tag const.
+	TagCluster = "cluster"
+	// TagCreated is created time tag const, RFC3339-formatted timestamp.
+	TagCreated = "created"
+	// TagLifetime is lifetime tag const.
+	TagLifetime = "lifetime"
+	// TagRoachprod is roachprod tag const, value is true & false.
+	TagRoachprod = "roachprod"
+)
+
+// GetDefaultLabelMap returns a label map for a common set of labels.
+func GetDefaultLabelMap(opts CreateOpts) map[string]string {
+	return map[string]string{
+		TagCluster:   opts.ClusterName,
+		TagLifetime:  opts.Lifetime.String(),
+		TagRoachprod: "true",
+	}
+}
+
 // A VM is an abstract representation of a specific machine instance.  This type is used across
 // the various cloud providers supported by roachprod.
 type VM struct {
@@ -33,8 +53,9 @@ type VM struct {
 	CreatedAt time.Time `json:"created_at"`
 	// If non-empty, indicates that some or all of the data in the VM instance
 	// is not present or otherwise invalid.
-	Errors   []error       `json:"errors"`
-	Lifetime time.Duration `json:"lifetime"`
+	Errors   []error           `json:"errors"`
+	Lifetime time.Duration     `json:"lifetime"`
+	Labels   map[string]string `json:"labels"`
 	// The provider-internal DNS name for the VM instance
 	DNS string `json:"dns"`
 	// The name of the cloud provider that hosts the VM instance
@@ -151,8 +172,10 @@ const (
 
 // CreateOpts is the set of options when creating VMs.
 type CreateOpts struct {
-	ClusterName    string
-	Lifetime       time.Duration
+	ClusterName  string
+	Lifetime     time.Duration
+	CustomLabels map[string]string
+
 	GeoDistributed bool
 	VMProviders    []string
 	SSDOpts        struct {
