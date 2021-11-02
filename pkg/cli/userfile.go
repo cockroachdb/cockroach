5114db5c84dbee7566300483bd0e19df26b03f5f
Steven Danna <danna@cockroachlabs.com>
2021-11-02 08:46:52 +0000

    cli: ignore lint SA1019
    This lint checks for the use of context-aware APIs that aren't
    supported by DriverConn.
    
    Release note: None

--- pkg/cli/userfile.go
+++ pkg/cli/userfile.go
@@ -563,7 +563,7 @@ func renameUserFile(
 			_, _ = ex.ExecContext(ctx, `ROLLBACK`, nil)
 		}
 	}()
-
+	//lint:ignore SA1019 DriverConn doesn't support go 1.8 API
 	_, err = stmt.Exec([]driver.Value{newFilename, oldFilename})
 	if err != nil {
 		return err
@@ -652,6 +652,7 @@ func uploadUserFile(
 		if n > 0 {
 			// TODO(adityamaru): Switch to StmtExecContext once the copyin driver
 			// supports it.
+			//lint:ignore SA1019 DriverConn doesn't support go 1.8 API
 			_, err = stmt.Exec([]driver.Value{string(send[:n])})
 			if err != nil {
 				return "", err
