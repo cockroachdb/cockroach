2ccf86bf28fe1eaad7ab04910dc66f87ab30a1c7
Rafi Shamim <rafi@cockroachlabs.com>
2021-11-02 16:49:13 -0400

    sql/*: fix improperly wrapped errors
    I'm working on a linter that detects errors that are not wrapped
    correctly, and it discovered these.
    
    Release note: None

--- pkg/sql/opt/optgen/cmd/optgen/main.go
+++ pkg/sql/opt/optgen/cmd/optgen/main.go
@@ -218,7 +218,7 @@ func (g *optgen) generate(compiled *lang.CompiledExpr, genFunc genFunc) error {
 			// Write out incorrect source for easier debugging.
 			b = buf.Bytes()
 			out := g.cmdLine.Lookup("out").Value.String()
-			err = fmt.Errorf("code formatting failed with Go parse error\n%s:%s", out, err)
+			err = errors.Wrapf(err, "code formatting failed with Go parse error\n%s", out)
 		}
 	} else {
 		b = buf.Bytes()
