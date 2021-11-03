b944cffbc598191aa4ee4f0d038083467fae1c81
Tobias Grieger <tobias.b.grieger@gmail.com>
2021-11-03 16:31:04 +0100

    kvserver: resuscitate and fix up TestRaftSSTableSideloading
    This test originally verified that SSTs that would be sent as part of
    the raft log in a raft snapshot would be correctly inlined. But it has
    been fooling itself for a long time, since snapshots haven't had a log
    (in this test) for 1-2 years at this point. The test ended up using a
    for loop that never went anywhere and a resulting index of zero and
    just happened to never return a failing result.
    
    Replace it with a more useful test verifying that we can propose and
    read back a sideloaded proposal, the main bit of logic I wanted to
    preserve is a test for an idiosyncracy of `entries()` when no sideloaded
    storage is provided, in which case it can't add to the raft entry cache
    for fear of putting a non-inlined entry there.
    
    As a little additional upshot, there was no reason for this test to
    use an on-disk engine (anymore), since the sideloaded storage is
    backed by the pebble instance as well.
    
    Release note: None

--- pkg/kv/kvserver/replica_sideload_test.go
+++ pkg/kv/kvserver/replica_sideload_test.go
@@ -27,6 +27,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvserverbase"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvserverpb"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/raftentry"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/stateloader"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/storage"
@@ -676,6 +677,99 @@ func newOnDiskEngine(t *testing.T) (func(), storage.Engine) {
 		t.Fatal(err)
 	}
 	return cleanup, eng
+}
+
+// This test verifies that sideloaded proposals are
+// inlined correctly and can be read back.
+func TestRaftSSTableSideloading(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	defer SetMockAddSSTable()()
+
+	ctx := context.Background()
+	tc := testContext{}
+
+	stopper := stop.NewStopper()
+	defer stopper.Stop(ctx)
+	tc.Start(t, stopper)
+
+	// Disable log truncation to make sure our proposal stays in the log.
+	tc.store.SetRaftLogQueueActive(false)
+
+	var ba roachpb.BatchRequest
+	ba.RangeID = tc.repl.RangeID
+
+	// Put a sideloaded proposal on the Range.
+	key, val := "don't", "care"
+	origSSTData, _ := MakeSSTable(key, val, hlc.Timestamp{}.Add(0, 1))
+	{
+
+		var addReq roachpb.AddSSTableRequest
+		addReq.Data = origSSTData
+		addReq.Key = roachpb.Key(key)
+		addReq.EndKey = addReq.Key.Next()
+		ba.Add(&addReq)
+
+		_, pErr := tc.store.Send(ctx, ba)
+		if pErr != nil {
+			t.Fatal(pErr)
+		}
+	}
+
+	// Check the `entries()` method which has special handling to accommodate
+	// `term()`: when an empty sideload storage is passed in, `entries()` should
+	// not inline, and in turn also not populate the entries cache (since its
+	// contents must always be fully inlined).
+	tc.repl.raftMu.Lock()
+	defer tc.repl.raftMu.Unlock()
+	tc.repl.mu.Lock()
+	defer tc.repl.mu.Unlock()
+	testutils.RunTrueAndFalse(t, "withSS", func(t *testing.T, withSS bool) {
+		rsl := stateloader.Make(tc.repl.RangeID)
+		lo := tc.repl.mu.state.TruncatedState.Index + 1
+		hi := tc.repl.mu.lastIndex + 1
+
+		var ss SideloadStorage
+		if withSS {
+			ss = tc.repl.raftMu.sideloaded
+		}
+
+		tc.store.raftEntryCache.Clear(tc.repl.RangeID, hi)
+		ents, err := entries(
+			ctx, rsl, tc.store.Engine(), tc.repl.RangeID, tc.store.raftEntryCache,
+			ss, lo, hi, math.MaxUint64,
+		)
+		require.NoError(t, err)
+		require.Len(t, ents, int(hi-lo))
+
+		// Raft entry cache only gets populated when sideloaded storage provided.
+		_, okLo := tc.store.raftEntryCache.Get(tc.repl.RangeID, lo)
+		_, okHi := tc.store.raftEntryCache.Get(tc.repl.RangeID, hi-1)
+		if withSS {
+			require.True(t, okLo)
+			require.True(t, okHi)
+		} else {
+			require.False(t, okLo)
+			require.False(t, okHi)
+		}
+
+		// The rest of the test is the same in both cases. We find the sideloaded entry
+		// and check the sideloaded storage for the payload.
+		var idx int
+		for idx = 0; idx < len(ents); idx++ {
+			// Get the SST back from the raft log.
+			if !sniffSideloadedRaftCommand(ents[idx].Data) {
+				continue
+			}
+			ent, err := maybeInlineSideloadedRaftCommand(ctx, tc.repl.RangeID, ents[idx], tc.repl.raftMu.sideloaded, tc.store.raftEntryCache)
+			require.NoError(t, err)
+			sst, err := tc.repl.raftMu.sideloaded.Get(ctx, ent.Index, ent.Term)
+			require.NoError(t, err)
+			require.Equal(t, origSSTData, sst)
+			break
+		}
+		require.Less(t, idx, len(ents)) // there was an SST
+	})
 }
 
 func TestRaftSSTableSideloadingTruncation(t *testing.T) {
