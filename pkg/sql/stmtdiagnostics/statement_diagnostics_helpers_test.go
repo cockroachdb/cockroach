9a084f6c8b2fd445537a03b42b6620042c6957b8
Yahor Yuzefovich <yahor@cockroachlabs.com>
2021-11-05 09:37:35 -0700

    stmtdiagnostics: add min exec latency condition
    This commit extends the statement diagnostics to support conditional
    collection: namely, it introduces `minExecutionLatency` parameter so
    that we could collect bundles on the slow outliers if necessary. Using
    the zero value for that parameter will give us the previous behavior
    (collecting the bundle on the next query fingerprint execution).
    
    In order for some diagnostics requests not to stay "alive" forever, this
    commit also adds an `expiresAfter` interval. It'll be provided by the
    user when the conditional collection is desired. The zero value is
    treated as "never". Expired and not completed requests are not explicitly
    deleted from the system table and are being simply ignored once they
    expire.
    
    This commit then adds a migration to the `statement_diagnostics_requests`
    system table to add these two additional columns as well as to update
    the secondary index to store these columns as well. All requests created
    prior to the migration will never expire and won't have the min execution
    latency conditional.
    
    If a diagnostics request already exists for a fingerprint in the system
    table, a new one (even with different conditionals / expiration
    interval) cannot be created by the API - an error is returned. The
    follow-up work will extend an API to make it possible to cancel the
    existing request so that a new one could be created.
    
    This API is not hooked up to the DB Console yet, thus there is no release
    note.
    
    Release note: None

--- pkg/sql/stmtdiagnostics/statement_diagnostics_helpers_test.go
+++ pkg/sql/stmtdiagnostics/statement_diagnostics_helpers_test.go
@@ -10,12 +10,17 @@
 
 package stmtdiagnostics
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 // InsertRequestInternal exposes the form of insert which returns the request ID
 // as an int64 to tests in this package.
-func (r *Registry) InsertRequestInternal(ctx context.Context, fprint string) (int64, error) {
-	id, err := r.insertRequestInternal(ctx, fprint)
+func (r *Registry) InsertRequestInternal(
+	ctx context.Context, fprint string, minExecutionLatency time.Duration, expiresAfter time.Duration,
+) (int64, error) {
+	id, err := r.insertRequestInternal(ctx, fprint, minExecutionLatency, expiresAfter)
 	return int64(id), err
 }
 
