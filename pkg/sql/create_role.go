077f48525ed809c7af1c637c0d0fe800df2109d7
Raphael 'kena' Poss <knz@thaumogen.net>
2021-11-09 18:55:08 +0100

    sql: delete some dead code
    Release note: None

--- pkg/sql/create_role.go
+++ pkg/sql/create_role.go
@@ -143,15 +143,6 @@ func (n *CreateRoleNode) startExec(params runParams) error {
 		}
 	}
 
-	if hashedPassword == nil {
-		// v20.1 and below crash during authentication if they find a NULL value
-		// in system.users.hashedPassword. v20.2 and above handle this correctly,
-		// but we need to maintain mixed version compatibility for at least one
-		// release.
-		// TODO(nvanbenschoten): remove this for v21.1.
-		hashedPassword = []byte{}
-	}
-
 	// Check if the user/role exists.
 	row, err := params.extendedEvalCtx.ExecCfg.InternalExecutor.QueryRowEx(
 		params.ctx,
