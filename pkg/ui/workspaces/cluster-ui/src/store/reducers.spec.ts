d5f9d6092c9d055c7bb4e8c39df272c6f42c0f0a
Azhng <archer.xn@gmail.com>
2021-11-09 18:51:34 -0500

    ui: unify cluster-ui stores for Statement and Transaction Page
    Previously, on CC Conosle, Statement Page and Transaction Page were backed by
    their own Redux stores and their own Redux Sagas. However, both Statement
    and Transaction Page were backed by a single API endpoint, namely
    `/_status/statements`. This resulted in unnecessary API calls and bugs
    like #72009, which was caused by Reset SQL Stats sagas failing to
    reset the Transaction Page Redux store.
    
    This commit unifies the following Redux store and sagas into a single
    store / sagas:
    1. Statement Page store / sagas
    2. Transaciton Page store / sagas
    3. Reset SQL Stats store / sagas
    
    This greatly removed the code duplication and test duplication and simplify
    the logic. Statement Page and Transaction Page can reuse the same store
    by their own selectors.
    
    Resolves #72009
    
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/store/reducers.spec.ts
+++ pkg/ui/workspaces/cluster-ui/src/store/reducers.spec.ts
@@ -11,22 +11,22 @@
 import { assert } from "chai";
 import { createStore } from "redux";
 import { rootActions, rootReducer } from "./reducers";
-import { actions as statementsActions } from "./statements";
+import { actions as sqlStatsActions } from "./sqlStats";
 
 describe("rootReducer", () => {
   it("resets redux state on RESET_STATE action", () => {
     const store = createStore(rootReducer);
     const initState = store.getState();
     const error = new Error("oops!");
-    store.dispatch(statementsActions.failed(error));
+    store.dispatch(sqlStatsActions.failed(error));
     const changedState = store.getState();
     store.dispatch(rootActions.resetState());
     const resetState = store.getState();
 
     assert.deepEqual(initState, resetState);
     assert.notDeepEqual(
-      resetState.statements.lastError,
-      changedState.statements.lastError,
+      resetState.sqlStats.lastError,
+      changedState.sqlStats.lastError,
     );
   });
 });
--- pkg/ui/workspaces/cluster-ui/src/store/sqlStats/sqlStats.sagas.ts
+++ pkg/ui/workspaces/cluster-ui/src/store/sqlStats/sqlStats.sagas.ts
@@ -8,22 +8,107 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-import { all, call, put, takeEvery } from "redux-saga/effects";
+import { PayloadAction } from "@reduxjs/toolkit";
+import {
+  all,
+  call,
+  put,
+  delay,
+  takeLatest,
+  takeEvery,
+} from "redux-saga/effects";
+import Long from "long";
+import { cockroach } from "@cockroachlabs/crdb-protobuf-client";
+import {
+  getStatements,
+  getCombinedStatements,
+  StatementsRequest,
+} from "src/api/statementsApi";
 import { resetSQLStats } from "src/api/sqlStatsApi";
-import { actions as statementActions } from "src/store/statements";
-import { actions as sqlStatsActions } from "./sqlStats.reducer";
+import { actions as localStorageActions } from "src/store/localStorage";
+import {
+  actions as sqlStatsActions,
+  UpdateDateRangePayload,
+} from "./sqlStats.reducer";
+import { rootActions } from "../reducers";
 
-export function* resetSQLStatsSaga(): any {
+import { CACHE_INVALIDATION_PERIOD, throttleWithReset } from "src/store/utils";
+
+export function* refreshSQLStatsSaga(
+  action?: PayloadAction<StatementsRequest>,
+) {
+  yield put(sqlStatsActions.request(action?.payload));
+}
+
+export function* requestSQLStatsSaga(
+  action?: PayloadAction<StatementsRequest>,
+): any {
   try {
-    const response = yield call(resetSQLStats);
-    yield put(sqlStatsActions.received(response));
-    yield put(statementActions.invalidated());
-    yield put(statementActions.refresh());
+    const result = yield action?.payload?.combined
+      ? call(getCombinedStatements, action.payload)
+      : call(getStatements);
+    yield put(sqlStatsActions.received(result));
   } catch (e) {
     yield put(sqlStatsActions.failed(e));
   }
 }
 
-export function* sqlStatsSaga() {
-  yield all([takeEvery(sqlStatsActions.request, resetSQLStatsSaga)]);
+export function* receivedSQLStatsSaga(delayMs: number) {
+  yield delay(delayMs);
+  yield put(sqlStatsActions.invalidated());
+}
+
+export function* updateSQLStatsDateRangeSaga(
+  action: PayloadAction<UpdateDateRangePayload>,
+) {
+  const { start, end } = action.payload;
+  yield put(
+    // TODO(azhng): do we want to rename this into dataRange/SQLActivity?
+    localStorageActions.update({
+      key: "dateRange/StatementsPage",
+      value: { start, end },
+    }),
+  );
+  yield put(sqlStatsActions.invalidated());
+  const req = new cockroach.server.serverpb.StatementsRequest({
+    combined: true,
+    start: Long.fromNumber(start),
+    end: Long.fromNumber(end),
+  });
+  yield put(sqlStatsActions.refresh(req));
+}
+
+export function* resetSQLStatsSaga() {
+  try {
+    yield call(resetSQLStats);
+    yield put(sqlStatsActions.invalidated());
+    yield put(sqlStatsActions.refresh());
+  } catch (e) {
+    yield put(sqlStatsActions.failed(e));
+  }
+}
+
+export function* sqlStatsSaga(
+  cacheInvalidationPeriod: number = CACHE_INVALIDATION_PERIOD,
+) {
+  yield all([
+    throttleWithReset(
+      cacheInvalidationPeriod,
+      sqlStatsActions.refresh,
+      [
+        sqlStatsActions.invalidated,
+        sqlStatsActions.failed,
+        rootActions.resetState,
+      ],
+      refreshSQLStatsSaga,
+    ),
+    takeLatest(sqlStatsActions.request, requestSQLStatsSaga),
+    takeLatest(
+      sqlStatsActions.received,
+      receivedSQLStatsSaga,
+      cacheInvalidationPeriod,
+    ),
+    takeLatest(sqlStatsActions.updateDateRange, updateSQLStatsDateRangeSaga),
+    takeEvery(sqlStatsActions.reset, resetSQLStatsSaga),
+  ]);
 }
