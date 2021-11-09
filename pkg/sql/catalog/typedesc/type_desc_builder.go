e0c82d9a6f00c96dab1f132ee07049351e52a168
Jack Wu <jack.wu@cockroachlabs.com>
2021-11-09 16:24:48 -0500

    sql: migration and mixed cluster version roachtest for grant option
    The roachtest ensures that the validation check for
    grant options that is performed on a grant/revoke only runs when all nodes on
    a cluster are upgraded to 22.1. The migration scans through existing users
    and sets their grant option bits equal to their privilege bits if they
    have GRANT or ALL privileges (otherwise, it would have been impossible
    for them to grant anyways, so we do not do anything).
    
    Release note: None

--- pkg/sql/catalog/typedesc/type_desc_builder.go
+++ pkg/sql/catalog/typedesc/type_desc_builder.go
@@ -53,16 +53,18 @@ func (tdb *typeDescriptorBuilder) DescriptorType() catalog.DescriptorType {
 // interface.
 func (tdb *typeDescriptorBuilder) RunPostDeserializationChanges() {
 	tdb.maybeModified = protoutil.Clone(tdb.original).(*descpb.TypeDescriptor)
-	tdb.changed = catprivilege.MaybeFixPrivileges(
+	fixedPrivileges := catprivilege.MaybeFixPrivileges(
 		&tdb.maybeModified.Privileges,
 		tdb.maybeModified.GetParentID(),
 		tdb.maybeModified.GetParentSchemaID(),
 		privilege.Type,
 		tdb.maybeModified.GetName(),
 	)
+	addedGrantOptions := catprivilege.MaybeUpdateGrantOptions(tdb.maybeModified.Privileges)
+	tdb.changed = fixedPrivileges || addedGrantOptions
 }
 
-// RunPostRestoreChanges implements the catalog.DescriptorBuilder interface.
+// RunRestoreChanges implements the catalog.DescriptorBuilder interface.
 func (tdb *typeDescriptorBuilder) RunRestoreChanges(_ func(id descpb.ID) catalog.Descriptor) error {
 	return nil
 }
