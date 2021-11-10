1338c464d2576150dab35b836225428e0892400f
Marylia Gutierrez <marylia@cockroachlabs.com>
2021-11-10 13:26:52 -0500

    ui: save sort on cache for Statement page
    Previously, a sort selection was not maintained when
    the page change (e.g. coming back from Statement details).
    This commits saves the selected value to be used.
    
    Partially adressed #68199
    
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/statementsPage/statementsPage.selectors.ts
+++ pkg/ui/workspaces/cluster-ui/src/statementsPage/statementsPage.selectors.ts
@@ -240,3 +240,8 @@ export const selectDateRange = createSelector(
       Moment,
     ],
 );
+
+export const selectSortSetting = createSelector(
+  localStorageSelector,
+  localStorage => localStorage["sortSetting/StatementsPage"],
+);
