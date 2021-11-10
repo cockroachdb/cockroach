1248fae967db34a1ce20000a81dcb7c94f6a02fb
Andrei Matei <andrei@cockroachlabs.com>
2021-11-10 15:46:53 -0500

    rowexec: fix bulkRowWriter summary
    This processor was entering the draining state too early, which was
    causing it to not output its summary.
    
    Release note: None

--- pkg/sql/rowexec/bulk_row_writer.go
+++ pkg/sql/rowexec/bulk_row_writer.go
@@ -78,7 +78,9 @@ func (sp *bulkRowWriter) Start(ctx context.Context) {
 	ctx = sp.StartInternal(ctx, "bulkRowWriter")
 	sp.input.Start(ctx)
 	err := sp.work(ctx)
-	sp.MoveToDraining(err)
+	if err != nil {
+		sp.MoveToDraining(err)
+	}
 }
 
 // Next is part of the RowSource interface.
