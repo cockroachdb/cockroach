558831d630a003cf98bf2afe4a8b30560b163771
Azhng <archer.xn@gmail.com>
2021-11-11 15:07:58 -0500

    sql: introduce Transaction ID Cache
    Previously, it was impossible to correlate an individual execution
    of a transaction (identified via transaction ID) to this historical
    execution statistics (identified via transaction fingerprint ID).
    
    This commit introduces Transaction ID Cache (TxnIDCache), a FIFO
    cache that stores the mapping from transaction ID to transaction
    fingerprint ID. This buffer records the mapping at the
    end of the transaction execution. The oldest entry in the buffer
    will be evicted through FIFO policy. The default size of this
    Transaction ID Cache is capped at 64 MB and it is configurable via
    the sql.contention.txn_id_cache.max_size cluster setting.
    
    Release note (sql change): Transaction ID to Transaction Fingerprint
    ID mapping is now stored in the new Transaction ID Cache, a FIFO
    unordered in-memory buffer. The size of the buffer is 64 MB by default
    and configurable via sql.contention.txn_id_cache.max_size cluster
    setting. Consequentially, two additioanl metrics are introduced:
    * sql.contention.txn_id_cache.size: tracks the current memory usage
      of transaction ID Cache
    * sql.contention.txn_id_cache.discarded_count: number of resolved
      transaction IDs that are dropped due to memory constraints.

---
+++ pkg/sql/contention/txnidcache/provider.go
@@ -0,0 +1,74 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package txnidcache
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
+	"github.com/cockroachdb/cockroach/pkg/util/uuid"
+)
+
+// Provider is the main interface for txnidcache.
+type Provider interface {
+	// Start starts all background tasks for the txnidcache.Provider.
+	Start(ctx context.Context, stopper *stop.Stopper)
+
+	reader
+	messageSink
+	writerPool
+}
+
+// Writer is the interface that can be used to write to txnidcache.
+type Writer interface {
+	// Record writes a pair of transactionID and transaction fingerprint ID
+	// into a temporary buffer. This buffer will eventually be flushed into
+	// the transaction ID cache asynchronously.
+	Record(resolvedTxnID ResolvedTxnID)
+
+	// Flush starts the flushing process of writer's temporary buffer.
+	Flush()
+
+	// Close closes the Writer and flushes any pending data. The Writer should
+	// not be used after its closed.
+	Close()
+}
+
+type reader interface {
+	// Lookup returns the corresponding transaction fingerprint ID for a given txnID,
+	// if the given txnID has no entry in the Cache, the returned "found" boolean
+	// will be false.
+	Lookup(txnID uuid.UUID) (result roachpb.TransactionFingerprintID, found bool)
+
+	// TODO(azhng): we eventually want to implement a batch-lookup API.
+}
+
+type writerPool interface {
+	// GetWriter returns a Writer to the caller. After returned, the Writer
+	// is attached to the writerPool and a message sink, and can be disconnected
+	// using the disconnector interface.
+	GetWriter() Writer
+
+	disconnector
+}
+
+type messageSink interface {
+	// push allows a messageBlock to be pushed into the pusher.
+	push(messageBlock)
+
+	disconnector
+}
+
+type disconnector interface {
+	// disconnect allows a Writer to be disconnected from its attached target.
+	disconnect(Writer)
+}
---
+++ pkg/sql/contention/txnidcache/writer_pool.go
@@ -0,0 +1,59 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package txnidcache
+
+import "github.com/cockroachdb/cockroach/pkg/util/syncutil"
+
+type writerPoolImpl struct {
+	mu struct {
+		syncutil.Mutex
+		activeWriters map[Writer]struct{}
+	}
+
+	new func() Writer
+}
+
+var _ writerPool = &writerPoolImpl{}
+
+func newWriterPoolImpl(new func() Writer) *writerPoolImpl {
+	w := &writerPoolImpl{
+		new: new,
+	}
+	w.mu.activeWriters = make(map[Writer]struct{})
+	return w
+}
+
+// GetWriter implements the writerPool interface.
+// TODO(azhng): current writerPollImpl is very primitive, it returns a
+//  new instance of Writer each time GetWriter() is called. This means that
+//  each connExecutor will have its own copy of Writer. The underlying
+//  implementation of Writer is ConcurrentWriteBuffer, which is capable of
+//  efficiently handle concurrent writes. We can improve Writer allocation
+//  by making multiple connExecutors share a single Writer.
+//  One possible idea is to pre-allocate a pool of Writers and store them
+//  in a min-heap sorted by their ref-count. Each time GetWriter() is called,
+//  we find a Writer from the min-heap with least ref-count and give it back to
+//  the connExecutor, and increment its ref-count. Once the connExecutor closes,
+//  the ref-count is decremented.
+func (w *writerPoolImpl) GetWriter() Writer {
+	writeBuffer := w.new()
+	w.mu.Lock()
+	w.mu.activeWriters[writeBuffer] = struct{}{}
+	w.mu.Unlock()
+	return writeBuffer
+}
+
+// disconnect implements the writerPool interface.
+func (w *writerPoolImpl) disconnect(writer Writer) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	delete(w.mu.activeWriters, writer)
+}
