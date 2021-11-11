7ab30f674bcc5c49e1c8ad9116037af7c141adce
richardjcai <caioftherichard@gmail.com>
2021-11-11 12:26:04 -0500

    backupccl: update backup to consider public schemas with descriptor
    Updated restore_job.go in backup CCL to remap public schemas.
    NB: Backups made in 21.2 and prior have "synthetic" public schemas
    meaning the public schemas did not have a descriptor and used ID 29.
    In restore job, we now create a real public schema and remap objects
    in that public schema to use the new public schema's ID.
    
    These changes are mostly updates to IDs, public schemas now being
    backed by descriptors causes IDs of certain objects to be
    bumped in new databases.
    
    Release note: None

--- pkg/ccl/importccl/read_import_mysql_test.go
+++ pkg/ccl/importccl/read_import_mysql_test.go
@@ -27,8 +27,10 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catformat"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/dbdesc"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/schemadesc"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/tabledesc"
 	"github.com/cockroachdb/cockroach/pkg/sql/execinfrapb"
+	"github.com/cockroachdb/cockroach/pkg/sql/privilege"
 	"github.com/cockroachdb/cockroach/pkg/sql/row"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
@@ -105,7 +107,8 @@ func TestMysqldumpDataReader(t *testing.T) {
 	}
 }
 
-const expectedParentID = 52
+const expectedParentID = 53
+const expectedParentSchemaID = 54
 
 func readFile(t *testing.T, name string) string {
 	body, err := ioutil.ReadFile(filepath.Join("testdata", "mysqldump", name))
@@ -128,7 +131,17 @@ func readMysqlCreateFrom(
 	expectedParent := dbdesc.NewInitial(
 		expectedParentID, "test", security.RootUserName(),
 	)
-	tbl, err := readMysqlCreateTable(context.Background(), f, testEvalCtx, nil, id, expectedParent, name, fks, map[descpb.ID]int64{}, security.RootUserName(), walltime)
+	publicSchemaPrivileges := descpb.NewBasePrivilegeDescriptor(security.RootUserName())
+	publicSchemaPrivileges.Grant(security.PublicRoleName(), privilege.List{privilege.CREATE, privilege.USAGE})
+	expectedParentSchema := schemadesc.NewBuilder(&descpb.SchemaDescriptor{
+		ParentID:   expectedParentID,
+		Name:       tree.PublicSchema,
+		ID:         expectedParentSchemaID,
+		Privileges: publicSchemaPrivileges,
+		Version:    1,
+	}).BuildCreatedMutableSchema()
+	tbl, err := readMysqlCreateTable(context.Background(), f, testEvalCtx, nil, id, expectedParent,
+		expectedParentSchema, name, fks, map[descpb.ID]int64{}, security.RootUserName(), walltime)
 	if err != nil {
 		t.Fatal(err)
 	}
