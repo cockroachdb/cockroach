abb7abe71441745befb9dca8c81e72d3721e292b
Azhng <archer.xn@gmail.com>
2021-11-11 15:07:58 -0500

    sql: introduce Transaction ID Cache
    Previously, it was impossible to correlate an individual execution
    of a transaction (identified via transaction ID) to this historical
    execution statistics (identified via transaction fingerprint ID).
    
    This commit introduces Transaction ID Cache (TxnIDCache), a FIFO
    cache that stores the mapping from transaction ID to transaction
    fingerprint ID. This buffer records the mapping at the
    end of the transaction execution. The oldest entry in the buffer
    will be evicted through FIFO policy. The default size of this
    Transaction ID Cache is capped at 64 MB and it is configurable via
    the sql.contention.txn_id_cache.max_size cluster setting.
    
    Release note (sql change): Transaction ID to Transaction Fingerprint
    ID mapping is now stored in the new Transaction ID Cache, a FIFO
    unordered in-memory buffer. The size of the buffer is 64 MB by default
    and configurable via sql.contention.txn_id_cache.max_size cluster
    setting. Consequentially, two additioanl metrics are introduced:
    * sql.contention.txn_id_cache.size: tracks the current memory usage
      of transaction ID Cache
    * sql.contention.txn_id_cache.discarded_count: number of resolved
      transaction IDs that are dropped due to memory constraints.

---
+++ pkg/sql/contention/txnidcache/txn_id_cache.go
@@ -0,0 +1,237 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package txnidcache
+
+import (
+	"context"
+	"sync"
+
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/util/cache"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/cockroach/pkg/util/uuid"
+)
+
+// Reader is the interface that can be used to query the transaction fingerprint
+// ID of a transaction ID.
+type Reader interface {
+	// Lookup returns the corresponding transaction fingerprint ID for a given txnID,
+	// if the given txnID has no entry in the Cache, the returned "found" boolean
+	// will be false.
+	// TODO(azhng): we eventually want to implement a batch-lookup API.
+	Lookup(txnID uuid.UUID) (result roachpb.TransactionFingerprintID, found bool)
+}
+
+// Writer is the interface that can be used to write to txnidcache.
+type Writer interface {
+	// Record writes a pair of transactionID and transaction fingerprint ID
+	// into a temporary buffer. This buffer will eventually be flushed into
+	// the transaction ID cache asynchronously.
+	Record(resolvedTxnID ResolvedTxnID)
+
+	// Flush starts the flushing process of writer's temporary buffer.
+	Flush()
+}
+
+type messageSink interface {
+	// push allows a messageBlock to be pushed into the pusher.
+	push(*messageBlock)
+}
+
+const channelSize = 128
+
+// Cache stores the mapping from the Transaction IDs (UUID) of recently
+// executed transactions to their corresponding Transaction Fingerprint ID (uint64).
+// The size of Cache is controlled via sql.contention.txn_id_cache.max_size
+// cluster setting, and it follows FIFO eviction policy once the cache size
+// reaches the limit defined by the cluster setting.
+//
+// Cache's overall architecture is as follows:
+//   +------------------------------------------------------------+
+//   | connExecutor  --------*                                    |
+//   |                       |  writes resolvedTxnID to Writer    |
+//   |                       v                                    |
+//   |      +---------------------------------------------------+ |
+//   |      | Writer                                            | |
+//   |      |                                                   | |
+//   |      |  Writer contains multiple shards of concurrent    | |
+//   |      |  write buffer. Each incoming resolvedTxnID is     | |
+//   |      |  first hashed to a corresponding shard, and then  | |
+//   |      |  is written to the concurrent write buffer        | |
+//   |      |  backing that shard. Once the concurrent write    | |
+//   |      |  buffer is full, a flush is performed and the     | |
+//   |      |  content of the buffer is send into the channel.  | |
+//   |      |                                                   | |
+//   |      | +------------+                                    | |
+//   |      | | shard1     |                                    | |
+//   |      | +------------+                                    | |
+//   |      | | shard2     |                                    | |
+//   |      | +------------+                                    | |
+//   |      | | shard3     |                                    | |
+//   |      | +------------+                                    | |
+//   |      | | .....      |                                    | |
+//   |      | | .....      |                                    | |
+//   |      | +------------+                                    | |
+//   |      | | shard128   |                                    | |
+//   |      | +------------+                                    | |
+//   |      |                                                   | |
+//   |      +-----+---------------------------------------------+ |
+//   +------------|-----------------------------------------------+
+//                |
+//                |
+//                V
+//               channel
+//                ^
+//                |
+//               Cache polls the channel using a goroutine and push the
+//                |   messageBlock into its storage.
+//                |
+//   +----------------------------------+
+//   |    Cache:                        |
+//   |     The cache contains a         |
+//   |     FIFO buffer backed by        |
+//   |     cache.UnorderedCache         |
+//   +----------------------------------+
+type Cache struct {
+	st *cluster.Settings
+
+	msgChan chan *messageBlock
+
+	mu struct {
+		syncutil.RWMutex
+
+		store *cache.UnorderedCache
+	}
+
+	messageBlockPool *sync.Pool
+
+	writer Writer
+
+	metrics *Metrics
+}
+
+var (
+	entrySize = int64(uuid.UUID{}.Size()) +
+		roachpb.TransactionFingerprintID(0).Size()
+)
+
+// ResolvedTxnID represents a TxnID that is resolved to its corresponding
+// TxnFingerprintID.
+type ResolvedTxnID struct {
+	TxnID            uuid.UUID
+	TxnFingerprintID roachpb.TransactionFingerprintID
+}
+
+func (r *ResolvedTxnID) valid() bool {
+	return r.TxnFingerprintID != roachpb.InvalidTransactionFingerprintID
+}
+
+var (
+	_ Reader      = &Cache{}
+	_ Writer      = &Cache{}
+	_ messageSink = &Cache{}
+)
+
+// NewTxnIDCache creates a new instance of Cache.
+func NewTxnIDCache(st *cluster.Settings, metrics *Metrics) *Cache {
+	t := &Cache{
+		st:      st,
+		metrics: metrics,
+		msgChan: make(chan *messageBlock, channelSize),
+	}
+
+	t.messageBlockPool = &sync.Pool{
+		New: func() interface{} {
+			return &messageBlock{}
+		},
+	}
+
+	t.mu.store = cache.NewUnorderedCache(cache.Config{
+		Policy: cache.CacheFIFO,
+		ShouldEvict: func(size int, _, _ interface{}) bool {
+			return int64(size)*entrySize > MaxSize.Get(&st.SV)
+		},
+		OnEvictedEntry: func(_ *cache.Entry) {
+			metrics.EvictedTxnIDCacheCount.Inc(1)
+		},
+	})
+
+	t.writer = newWriter(t, t.messageBlockPool)
+	return t
+}
+
+// Start implements the Provider interface.
+func (t *Cache) Start(ctx context.Context, stopper *stop.Stopper) {
+	_ = stopper.RunAsyncTask(ctx, "txn-id-cache-ingest", func(ctx context.Context) {
+		for {
+			select {
+			case msgBlock := <-t.msgChan:
+				func() {
+					t.mu.Lock()
+					defer t.mu.Unlock()
+					for blockIdx := range msgBlock {
+						if !msgBlock[blockIdx].valid() {
+							break
+						}
+						t.mu.store.Add(msgBlock[blockIdx].TxnID, msgBlock[blockIdx].TxnFingerprintID)
+					}
+				}()
+				// Zero out the block and put it back into the pool.
+				*msgBlock = messageBlock{}
+				t.messageBlockPool.Put(msgBlock)
+			case <-stopper.ShouldQuiesce():
+				return
+			}
+		}
+	})
+}
+
+// Lookup implements the Reader interface.
+func (t *Cache) Lookup(txnID uuid.UUID) (result roachpb.TransactionFingerprintID, found bool) {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+
+	txnFingerprintID, ok := t.mu.store.Get(txnID)
+	if !ok {
+		return roachpb.InvalidTransactionFingerprintID, false /* found */
+	}
+
+	return txnFingerprintID.(roachpb.TransactionFingerprintID), true /* found */
+}
+
+// Record implements the Writer interface.
+func (t *Cache) Record(resolvedTxnID ResolvedTxnID) {
+	t.writer.Record(resolvedTxnID)
+}
+
+// push implements the messageSink interface.
+func (t *Cache) push(msg *messageBlock) {
+	select {
+	case t.msgChan <- msg:
+		// noop.
+	default:
+		t.metrics.DiscardedResolvedTxnIDCount.Inc(messageBlockSize)
+	}
+}
+
+// Flush flushes the resolved txn IDs in the Writer into the Cache.
+func (t *Cache) Flush() {
+	t.writer.Flush()
+}
+
+// Size return the current size of the Cache.
+func (t *Cache) Size() int64 {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return int64(t.mu.store.Len()) * entrySize
+}
