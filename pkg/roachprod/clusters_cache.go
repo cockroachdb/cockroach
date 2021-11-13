cc81a27c2df5c140595cf87608845e1324d7abca
Radu Berinde <radu@cockroachlabs.com>
2021-11-12 21:05:53 -0800

    roachprod: remove install.Clusters
    We were using `SyncedCluster` in two "modes": it can be just a
    metadata holder (only Cluster initialized) or it can be a usable
    object (once `Prepare()` is called). This makes things harder to
    follow, especially when we modify entries in the `Clusters` map in
    place.
    
    This change removes `install.Clusters` and replaces it with a
    `syncedClusters` map which only stores the metadata. A `SyncedCluster`
    only exists when we intend to perform an operation on a cluster.
    
    Release note: None

--- pkg/roachprod/clusters_cache.go
+++ pkg/roachprod/clusters_cache.go
@@ -20,7 +20,6 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/roachprod/cloud"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/config"
-	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/vm"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/vm/local"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
@@ -34,6 +33,10 @@ import (
 // clusters. It is also used as the "source of truth" for the local cluster.
 //
 // Each cluster corresponds to a json file in this directory.
+
+// syncedClusters stores the synced clusters metadata, populated from the
+// clusters cache.
+var syncedClusters cloud.Clusters
 
 // InitDirs initializes the directories for storing cluster metadata and debug
 // logs.
@@ -111,8 +114,11 @@ func shouldIgnoreCluster(c *cloud.Cluster) bool {
 }
 
 // LoadClusters reads the cached cluster metadata from config.ClustersDir and
-// populates install.Clusters.
+// populates syncedClusters. It is assumed that this is called when the process
+// starts, before any roachprod operations.
 func LoadClusters() error {
+	syncedClusters = make(cloud.Clusters)
+
 	clusterNames, err := listClustersInCache()
 	if err != nil {
 		return err
@@ -131,15 +137,7 @@ func LoadClusters() error {
 			continue
 		}
 
-		sc := &install.SyncedCluster{
-			Cluster: *c,
-		}
-
-		for _, vm := range c.VMs {
-			sc.Localities = append(sc.Localities, vm.Locality())
-		}
-
-		install.Clusters[sc.Name] = sc
+		syncedClusters[c.Name] = c
 
 		if config.IsLocalClusterName(c.Name) {
 			// Add the local cluster to the local provider.
