a77a5feb03a30c36e82664bc71640eb73bd24a8a
Radu Berinde <radu@cockroachlabs.com>
2021-11-16 11:17:13 -0800

    roachprod: minor flags cleanup
    This change:
     - makes the formatting consistent when setting up flags;
     - reorganizes and simplifies the code that sets up flags for multiple
       commands at a time;
     - removes global values that are redundant.
    
    Release note: None

--- pkg/cmd/roachprod/flags.go
+++ pkg/cmd/roachprod/flags.go
@@ -43,21 +43,24 @@ var (
 		"COCKROACH_ENABLE_RPC_COMPRESSION=false",
 		"COCKROACH_UI_RELEASE_NOTES_SIGNUP_DISMISSED=true",
 	}
-	nodeArgs          []string
-	tag               string
-	external          = false
-	certsDir          string
-	adminurlOpen      = false
-	adminurlPath      = ""
-	adminurlIPs       = false
-	useTreeDist       = true
-	encrypt           = false
-	skipInit          = false
-	quiet             = false
-	sig               = 9
-	waitFlag          = false
-	createVMOpts      = vm.DefaultCreateOpts()
-	startOpts         = install.StartOptsType{}
+	nodeArgs     []string
+	tag          string
+	external     = false
+	certsDir     string
+	adminurlOpen = false
+	adminurlPath = ""
+	adminurlIPs  = false
+	useTreeDist  = true
+	quiet        = false
+	sig          = 9
+	waitFlag     = false
+	createVMOpts = vm.DefaultCreateOpts()
+	startOpts    = install.StartOptsType{
+		Encrypt:    false,
+		Sequential: true,
+		SkipInit:   false,
+		StoreCount: 1,
+	}
 	stageOS           string
 	stageDir          string
 	logsDir           string
@@ -79,18 +82,6 @@ func initFlags() {
 	rootCmd.PersistentFlags().IntVarP(&maxConcurrency, "max-concurrency", "", 32,
 		"maximum number of operations to execute on nodes concurrently, set to zero for infinite",
 	)
-	for _, cmd := range []*cobra.Command{createCmd, destroyCmd, extendCmd, logsCmd} {
-		cmd.Flags().StringVarP(&username, "username", "u", os.Getenv("ROACHPROD_USER"),
-			"Username to run under, detect if blank")
-	}
-
-	for _, cmd := range []*cobra.Command{statusCmd, monitorCmd, startCmd,
-		stopCmd, runCmd, wipeCmd, reformatCmd, installCmd, putCmd, getCmd,
-		sqlCmd, pgurlCmd, adminurlCmd, ipCmd,
-	} {
-		cmd.Flags().BoolVar(
-			&ssh.InsecureIgnoreHostKey, "insecure-ignore-host-key", true, "don't check ssh host keys")
-	}
 
 	createCmd.Flags().DurationVarP(&createVMOpts.Lifetime,
 		"lifetime", "l", 12*time.Hour, "Lifetime of the cluster")
@@ -100,17 +91,16 @@ func initFlags() {
 		"filesystem", vm.Ext4, "The underlying file system(ext4/zfs). ext4 is used by default.")
 	createCmd.Flags().BoolVar(&createVMOpts.SSDOpts.NoExt4Barrier,
 		"local-ssd-no-ext4-barrier", true,
-		`Mount the local SSD with the "-o nobarrier" flag. `+
-			`Ignored if --local-ssd=false is specified.`)
+		`Mount the local SSD with the "-o nobarrier" flag. Ignored if --local-ssd=false is specified.`)
 	createCmd.Flags().IntVarP(&numNodes,
 		"nodes", "n", 4, "Total number of nodes, distributed across all clouds")
-
 	createCmd.Flags().IntVarP(&createVMOpts.OsVolumeSize,
 		"os-volume-size", "", 10, "OS disk volume size in GB")
-
 	createCmd.Flags().StringSliceVarP(&createVMOpts.VMProviders,
 		"clouds", "c", []string{gce.ProviderName},
-		fmt.Sprintf("The cloud provider(s) to use when creating new vm instances: %s", vm.AllProviderNames()))
+		fmt.Sprintf(
+			"The cloud provider(s) to use when creating new vm instances: %s",
+			vm.AllProviderNames()))
 	createCmd.Flags().BoolVar(&createVMOpts.GeoDistributed,
 		"geo", false, "Create geo-distributed cluster")
 	createCmd.Flags().StringToStringVar(&createVMOpts.CustomLabels,
@@ -151,52 +141,106 @@ func initFlags() {
 	listCmd.Flags().StringVar(&listPattern,
 		"pattern", "", "Show only clusters matching the regex pattern. Empty string matches everything.")
 
-	adminurlCmd.Flags().BoolVar(
-		&adminurlOpen, `open`, false, `Open the url in a browser`)
-	adminurlCmd.Flags().StringVar(
-		&adminurlPath, `path`, "/", `Path to add to URL (e.g. to open a same page on each node)`)
-	adminurlCmd.Flags().BoolVar(
-		&adminurlIPs, `ips`, false, `Use Public IPs instead of DNS names in URL`)
+	adminurlCmd.Flags().BoolVar(&adminurlOpen, "open", false, "Open the url in a browser")
+	adminurlCmd.Flags().StringVar(&adminurlPath,
+		"path", "/", "Path to add to URL (e.g. to open a same page on each node)")
+	adminurlCmd.Flags().BoolVar(&adminurlIPs,
+		"ips", false, `Use Public IPs instead of DNS names in URL`)
 
-	gcCmd.Flags().BoolVarP(
-		&dryrun, "dry-run", "n", dryrun, "dry run (don't perform any actions)")
+	gcCmd.Flags().BoolVarP(&dryrun,
+		"dry-run", "n", dryrun, "dry run (don't perform any actions)")
 	gcCmd.Flags().StringVar(&config.SlackToken, "slack-token", "", "Slack bot token")
 
-	pgurlCmd.Flags().BoolVar(
-		&external, "external", false, "return pgurls for external connections")
-	pgurlCmd.Flags().StringVar(
-		&certsDir, "certs-dir", "./certs", "cert dir to use for secure connections")
+	pgurlCmd.Flags().BoolVar(&external,
+		"external", false, "return pgurls for external connections")
+	pgurlCmd.Flags().StringVar(&certsDir,
+		"certs-dir", "./certs", "cert dir to use for secure connections")
 
-	pprofCmd.Flags().DurationVar(
-		&pprofOptions.duration, "duration", 30*time.Second, "Duration of profile to capture")
-	pprofCmd.Flags().BoolVar(
-		&pprofOptions.heap, "heap", false, "Capture a heap profile instead of a CPU profile")
-	pprofCmd.Flags().BoolVar(
-		&pprofOptions.open, "open", false, "Open the profile using `go tool pprof -http`")
-	pprofCmd.Flags().IntVar(
-		&pprofOptions.startingPort, "starting-port", 9000, "Initial port to use when opening pprof's HTTP interface")
+	pprofCmd.Flags().DurationVar(&pprofOptions.duration,
+		"duration", 30*time.Second, "Duration of profile to capture")
+	pprofCmd.Flags().BoolVar(&pprofOptions.heap,
+		"heap", false, "Capture a heap profile instead of a CPU profile")
+	pprofCmd.Flags().BoolVar(&pprofOptions.open,
+		"open", false, "Open the profile using `go tool pprof -http`")
+	pprofCmd.Flags().IntVar(&pprofOptions.startingPort,
+		"starting-port", 9000, "Initial port to use when opening pprof's HTTP interface")
 
-	ipCmd.Flags().BoolVar(
-		&external, "external", false, "return external IP addresses")
+	ipCmd.Flags().BoolVar(&external,
+		"external", false, "return external IP addresses")
 
-	runCmd.Flags().BoolVar(
-		&secure, "secure", false, "use a secure cluster")
-	runCmd.Flags().StringVarP(
-		&extraSSHOptions, "ssh-options", "O", "", "extra args to pass to ssh")
+	runCmd.Flags().StringVarP(&extraSSHOptions,
+		"ssh-options", "O", "", "extra args to pass to ssh")
 
 	startCmd.Flags().IntVarP(&numRacks,
 		"racks", "r", 0, "the number of racks to partition the nodes into")
+	startCmd.Flags().BoolVar(&startOpts.Sequential,
+		"sequential", startOpts.Sequential, "start nodes sequentially so node IDs match hostnames")
+	startCmd.Flags().StringArrayVarP(&nodeArgs,
+		"args", "a", nil, "node arguments")
+	startCmd.Flags().StringArrayVarP(&nodeEnv,
+		"env", "e", nodeEnv, "node environment variables")
+	startCmd.Flags().BoolVar(&startOpts.Encrypt,
+		"encrypt", startOpts.Encrypt, "start nodes with encryption at rest turned on")
+	startCmd.Flags().BoolVar(&startOpts.SkipInit,
+		"skip-init", startOpts.SkipInit, "skip initializing the cluster")
+	startCmd.Flags().IntVar(&startOpts.StoreCount,
+		"store-count", startOpts.StoreCount, "number of stores to start each node with")
 
 	stopCmd.Flags().IntVar(&sig, "sig", sig, "signal to pass to kill")
 	stopCmd.Flags().BoolVar(&waitFlag, "wait", waitFlag, "wait for processes to exit")
 
 	wipeCmd.Flags().BoolVar(&wipePreserveCerts, "preserve-certs", false, "do not wipe certificates")
 
+	putCmd.Flags().BoolVar(&useTreeDist, "treedist", useTreeDist, "use treedist copy algorithm")
+
+	stageCmd.Flags().StringVar(&stageOS, "os", "", "operating system override for staged binaries")
+	stageCmd.Flags().StringVar(&stageDir, "dir", "", "destination for staged binaries")
+
+	stageURLCmd.Flags().StringVar(&stageOS, "os", "", "operating system override for staged binaries")
+
+	logsCmd.Flags().StringVar(&logsFilter,
+		"filter", "", "re to filter log messages")
+	logsCmd.Flags().Var(flagutil.Time(&logsFrom),
+		"from", "time from which to stream logs")
+	logsCmd.Flags().Var(flagutil.Time(&logsTo),
+		"to", "time to which to stream logs")
+	logsCmd.Flags().DurationVar(&logsInterval,
+		"interval", 200*time.Millisecond, "interval to poll logs from host")
+	logsCmd.Flags().StringVar(&logsDir,
+		"logs-dir", "logs", "path to the logs dir, if remote, relative to username's home dir, ignored if local")
+	logsCmd.Flags().StringVar(&logsProgramFilter,
+		"logs-program", "^cockroach$", "regular expression of the name of program in log files to search")
+
+	monitorCmd.Flags().BoolVar(&monitorIgnoreEmptyNodes,
+		"ignore-empty-nodes", false,
+		"Automatically detect the (subset of the given) nodes which to monitor "+
+			"based on the presence of a nontrivial data directory.")
+
+	monitorCmd.Flags().BoolVar(&monitorOneShot,
+		"oneshot", false,
+		"Report the status of all targeted nodes once, then exit. The exit "+
+			"status is nonzero if (and only if) any node was found not running.")
+
+	cachedHostsCmd.Flags().StringVar(&cachedHostsCluster,
+		"cluster", "", "print hosts matching cluster")
+
+	for _, cmd := range []*cobra.Command{createCmd, destroyCmd, extendCmd, logsCmd} {
+		cmd.Flags().StringVarP(&username, "username", "u", os.Getenv("ROACHPROD_USER"),
+			"Username to run under, detect if blank")
+	}
+
+	for _, cmd := range []*cobra.Command{statusCmd, monitorCmd, startCmd,
+		stopCmd, runCmd, wipeCmd, reformatCmd, installCmd, putCmd, getCmd,
+		sqlCmd, pgurlCmd, adminurlCmd, ipCmd,
+	} {
+		cmd.Flags().BoolVar(
+			&ssh.InsecureIgnoreHostKey, "insecure-ignore-host-key", true, "don't check ssh host keys")
+	}
+
 	for _, cmd := range []*cobra.Command{
 		startCmd, statusCmd, stopCmd, runCmd,
 	} {
-		cmd.Flags().StringVar(
-			&tag, "tag", "", "the process tag")
+		cmd.Flags().StringVar(&tag, "tag", "", "the process tag")
 	}
 
 	for _, cmd := range []*cobra.Command{
@@ -206,70 +250,12 @@ func initFlags() {
 		_ = cmd.Flags().MarkDeprecated("scp", "always true")
 	}
 
-	putCmd.Flags().BoolVar(&useTreeDist, "treedist", useTreeDist, "use treedist copy algorithm")
-
-	stageCmd.Flags().StringVar(&stageOS, "os", "", "operating system override for staged binaries")
-	stageCmd.Flags().StringVar(&stageDir, "dir", "", "destination for staged binaries")
-
-	stageURLCmd.Flags().StringVar(&stageOS, "os", "", "operating system override for staged binaries")
-
-	logsCmd.Flags().StringVar(
-		&logsFilter, "filter", "", "re to filter log messages")
-	logsCmd.Flags().Var(
-		flagutil.Time(&logsFrom), "from", "time from which to stream logs")
-	logsCmd.Flags().Var(
-		flagutil.Time(&logsTo), "to", "time to which to stream logs")
-	logsCmd.Flags().DurationVar(
-		&logsInterval, "interval", 200*time.Millisecond, "interval to poll logs from host")
-	logsCmd.Flags().StringVar(
-		&logsDir, "logs-dir", "logs", "path to the logs dir, if remote, relative to username's home dir, ignored if local")
-	logsCmd.Flags().StringVar(
-		&logsProgramFilter, "logs-program", "^cockroach$", "regular expression of the name of program in log files to search")
-
-	monitorCmd.Flags().BoolVar(
-		&monitorIgnoreEmptyNodes,
-		"ignore-empty-nodes",
-		false,
-		"Automatically detect the (subset of the given) nodes which to monitor "+
-			"based on the presence of a nontrivial data directory.")
-
-	monitorCmd.Flags().BoolVar(
-		&monitorOneShot,
-		"oneshot",
-		false,
-		"Report the status of all targeted nodes once, then exit. The exit "+
-			"status is nonzero if (and only if) any node was found not running.")
-
-	cachedHostsCmd.Flags().StringVar(&cachedHostsCluster, "cluster", "", "print hosts matching cluster")
-
-	for _, cmd := range []*cobra.Command{
-		getCmd, putCmd, runCmd, startCmd, statusCmd, stopCmd,
-		wipeCmd, pgurlCmd, adminurlCmd, sqlCmd, installCmd,
-	} {
-		switch cmd {
-		case startCmd:
-			cmd.Flags().BoolVar(
-				&startOpts.Sequential, "sequential", true,
-				"start nodes sequentially so node IDs match hostnames")
-			cmd.Flags().StringArrayVarP(
-				&nodeArgs, "args", "a", nil, "node arguments")
-			cmd.Flags().StringArrayVarP(
-				&nodeEnv, "env", "e", nodeEnv, "node environment variables")
-			cmd.Flags().BoolVar(
-				&startOpts.Encrypt, "encrypt", encrypt, "start nodes with encryption at rest turned on")
-			cmd.Flags().BoolVar(
-				&startOpts.SkipInit, "skip-init", skipInit, "skip initializing the cluster")
-			cmd.Flags().IntVar(
-				&startOpts.StoreCount, "store-count", 1, "number of stores to start each node with")
-			fallthrough
-		case sqlCmd:
-			cmd.Flags().StringVarP(
-				&config.Binary, "binary", "b", config.Binary,
-				"the remote cockroach binary to use")
-			fallthrough
-		case pgurlCmd, adminurlCmd:
-			cmd.Flags().BoolVar(
-				&secure, "secure", false, "use a secure cluster")
-		}
+	for _, cmd := range []*cobra.Command{startCmd, sqlCmd} {
+		cmd.Flags().StringVarP(&config.Binary,
+			"binary", "b", config.Binary, "the remote cockroach binary to use")
+	}
+	for _, cmd := range []*cobra.Command{startCmd, sqlCmd, pgurlCmd, adminurlCmd, runCmd} {
+		cmd.Flags().BoolVar(&secure,
+			"secure", false, "use a secure cluster")
 	}
 }
