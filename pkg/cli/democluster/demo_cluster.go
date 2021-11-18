6fe4c834a1a5f88825a3af270f1498e2e168953b
Andrei Matei <andrei@cockroachlabs.com>
2021-11-18 11:55:47 -0500

    democluster: don't intermix tracers when starting servers
    In a demo cluster, starting each server was done through the cluster's
    stopper, so each Server.Start(ctx) was running in a span created using
    the cluster's Tracer. Each Server, though, has its own Tracer that it's
    using to create child spans. This kind of mixing of Tracers is not going
    to be supported any more, since it's fairly broken. As far as our own
    Tracer is concerned, tracing across Tracers is not supported.
    
    This patch fixes it by not propagating the cluster's span into Servers.
    
    Release note: None

--- pkg/cli/democluster/demo_cluster.go
+++ pkg/cli/democluster/demo_cluster.go
@@ -603,7 +603,14 @@ func (c *transientCluster) startNodeAsync(
 	serv := c.servers[idx]
 	tag := fmt.Sprintf("start-n%d", idx+1)
 	return c.stopper.RunAsyncTask(ctx, tag, func(ctx context.Context) {
+		// We call Start() with context.Background() because we don't want the
+		// tracing span corresponding to the task started just above to leak into
+		// the new server. Server's have their own Tracers, different from the one
+		// used by this transientCluster, and so traces inside the Server can't be
+		// combined with traces from outside it.
+		ctx = logtags.WithTags(context.Background(), logtags.FromContext(ctx))
 		ctx = logtags.AddTag(ctx, tag, nil)
+
 		err := serv.Start(ctx)
 		if err != nil {
 			c.warnLog(ctx, "server %d failed to start: %v", idx, err)
