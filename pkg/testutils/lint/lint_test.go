a733859f8ef7b444c5b33a818c7a01650c1246e7
Raphael 'kena' Poss <knz@thaumogen.net>
2021-11-18 16:04:01 +0100

    lint: add a linter to prevent direct constructs of AmbientContext
    When code instantiates log.AmbientContext directly, it may not
    initialize the fields of the type properly or fully. Only the
    constructor functions know how to do this.
    
    To prevent mistakes, this commit adds a linter.
    
    Release note: None

--- pkg/testutils/lint/lint_test.go
+++ pkg/testutils/lint/lint_test.go
@@ -546,6 +546,39 @@ func TestLint(t *testing.T) {
 		}
 	})
 
+	t.Run("TestAmbientContextConstruction", func(t *testing.T) {
+		t.Parallel()
+		cmd, stderr, filter, err := dirCmd(
+			pkgDir,
+			"git",
+			"grep",
+			"-nE",
+			`[^[:alnum:]]log\.AmbientContext\{`,
+			"--",
+			"*.go",
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err := cmd.Start(); err != nil {
+			t.Fatal(err)
+		}
+
+		if err := stream.ForEach(filter, func(s string) {
+			t.Errorf("\n%s <- don't construct an AC directly; reuse an existing AC when possible, or use one of the constructor functions instead: log.MakeServerAC(), log.MakeClientAC() or testutils.MakeAC()\n"+
+				"For more details and information, see the context tech note.", s)
+		}); err != nil {
+			t.Error(err)
+		}
+
+		if err := cmd.Wait(); err != nil {
+			if out := stderr.String(); len(out) > 0 {
+				t.Fatalf("err=%s, stderr=%s", err, out)
+			}
+		}
+	})
+
 	t.Run("TestSQLTelemetryGetCounter", func(t *testing.T) {
 		t.Parallel()
 		cmd, stderr, filter, err := dirCmd(
