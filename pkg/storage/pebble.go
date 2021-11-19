9db095812aafd3f12adda919e5e3b71a6de9ec36
Arjun Nair <nair@cockroachlabs.com>
2021-11-19 16:26:39 -0500

    test

--- pkg/storage/pebble.go
+++ pkg/storage/pebble.go
@@ -1004,7 +1004,9 @@ func (p *Pebble) PutUnversioned(key roachpb.Key, value []byte) error {
 }
 
 // PutIntent implements the Engine interface.
-func (p *Pebble) PutIntent(ctx context.Context, key roachpb.Key, value []byte, txnUUID uuid.UUID) error {
+func (p *Pebble) PutIntent(
+	ctx context.Context, key roachpb.Key, value []byte, txnUUID uuid.UUID,
+) error {
 	_, err := p.wrappedIntentWriter.PutIntent(ctx, key, value, txnUUID, nil)
 	return err
 }
