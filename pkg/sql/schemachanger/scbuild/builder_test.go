0ec4fe07e98d5397dc6063c7ccb0aeacd9f357c2
Faizan Qazi <faizan@cockroachlabs.com>
2021-11-19 15:49:57 -0500

    sql: block partially implemented stmts for declarative schema changer
    Previously, flipping on the declarative schema changer enabled
    all statement types including partially implemented ones. This
    was inadequate because as we reimplement more features
    we want to get in the model of enabling them once they are
    fully tested. To address this, this patch modifies the session
    variable experimental_use_new_schema_changer, so that the on
    setting leaves not fully supported schema changes as disabled.
    Statement types can be individually promoted over time into
    a fully supported state. Additionally, a new unsafe setting is
    introduced for experimental_use_new_schema_changer setting
    that will enable *all* unsupported ones.
    
    Release note: None

--- pkg/sql/schemachanger/scbuild/builder_test.go
+++ pkg/sql/schemachanger/scbuild/builder_test.go
@@ -63,9 +63,7 @@ func TestBuilderAlterTable(t *testing.T) {
 			t.Run(depsType.name, func(t *testing.T) {
 				s, sqlDB, _ := serverutils.StartServer(t, base.TestServerArgs{})
 				defer s.Stopper().Stop(ctx)
-
 				tdb := sqlutils.MakeSQLRunner(sqlDB)
-
 				datadriven.RunTest(t, path, func(t *testing.T, d *datadriven.TestData) string {
 					return run(ctx, t, d, s, tdb, depsType.dependenciesWrapper)
 				})
