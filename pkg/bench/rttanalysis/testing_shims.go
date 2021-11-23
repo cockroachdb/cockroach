682c0583d72ee749ac806a0236fcf47ba6a14a73
Andrew Werner <awerner32@gmail.com>
2021-11-22 23:48:44 -0500

    bench/rttanalysis: make expectation validation fast and sane
    This commit reworks the way expectation validation is performed in the
    rttanalysis framework. Before this change, we relied on subprocesses to
    execute the benchmarks and then parsed the results. This was brittle and
    slow. It was also exposed to issues where we still could not parallelize
    sub-tests of individual benchmark categories.
    
    We can't rearrange the top-level benchmarks because we want continuity in
    the test names. Instead, we introduce a registry pattern which will store
    the benchmark cases and then the top-level benchmarks can just exec
    themselves from the registry. This does induce some strings, but it seems
    worth it. We assert that indeed the benchmark is registered.
    
    This gives us the ability to eschew most (but sadly, not all) of the
    subprocess complexity and still get cross-benchmark parallelism. The
    subprocess code which remains is about making sure that metamorphic
    constants are disabled. It does let us eliminate a bunch of complexity
    related to test pattern matching; we now delegate to the go testing
    framework to decide which benchmarks to run in the expectation test.
    This symmetry is pleasant.
    
    The key insight here is that we can use a testing.T and some additional
    state to stand in for the previously expected testing.B via an interface.
    This works out pretty well.
    
    Fixes #72068
    Fixes #71347
    Fixes #71107
    Fixes #73066
    
    Release note: None

---
+++ pkg/bench/rttanalysis/testing_shims.go
@@ -0,0 +1,86 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package rttanalysis
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+)
+
+type testingB interface {
+	testing.TB
+	N() int
+	ResetTimer()
+	StopTimer()
+	StartTimer()
+	ReportMetric(float64, string)
+	Run(string, func(testingB))
+
+	// logScope is used to wrap log.Scope and make it available only in
+	// appropriate contexts.
+	logScope() func()
+}
+
+type bShim struct {
+	*testing.B
+}
+
+var _ testingB = bShim{}
+
+func (b bShim) logScope() func() {
+	sc := log.Scope(b)
+	return func() { sc.Close(b) }
+}
+func (b bShim) N() int { return b.B.N }
+func (b bShim) Run(name string, f func(b testingB)) {
+	b.B.Run(name, func(b *testing.B) {
+		f(bShim{b})
+	})
+}
+
+// tShim is used by the expectation test's testing.T to appear as though
+// it is a testing.B and can be used capture the output. The object also
+// suppresses creation of a new log.Scope in order to permit parallel
+// execution.
+type tShim struct {
+	*testing.T
+	results *resultSet
+}
+
+var _ testingB = tShim{}
+
+func (ts tShim) logScope() func() {
+	return func() {}
+}
+func (ts tShim) N() int      { return 1 }
+func (ts tShim) ResetTimer() {}
+func (ts tShim) StopTimer()  {}
+func (ts tShim) StartTimer() {}
+func (ts tShim) ReportMetric(f float64, s string) {
+	if s == roundTripsMetric {
+		ts.results.add(benchmarkResult{
+			name:   ts.Name(),
+			result: int(f),
+		})
+	}
+}
+func (ts tShim) Name() string {
+	// Trim the name of the outermost test off the beginning of the name.
+	tn := ts.T.Name()
+	return tn[strings.Index(tn, "/")+1:]
+}
+func (ts tShim) Run(s string, f func(testingB)) {
+	ts.T.Run(s, func(t *testing.T) {
+		f(tShim{results: ts.results, T: t})
+	})
+}
