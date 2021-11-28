0d6ae2b394402edb8fb03bdf91d04e693ca48358
Jane Xing <zhouxing@uchicago.edu>
2021-11-28 00:36:34 -0600

    bazel: run docker image tests under Bazel
    This commit is to add a test infrastructure for docker image in TeamCity using
    bazel.
    It downloads the docker image tar from an upstream build configuration
    (not fully implemented yet, #72394 ), loads the docker image from the tar,
    builds a docker container based on it, and runs SQL queries inside that
    container.
    
    Release note: None

---
+++ build/teamcity/cockroach/ci/tests/docker_image.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+dir="$(dirname $(dirname $(dirname $(dirname $(dirname "${0}")))))"
+source "$dir/teamcity-support.sh"
+
+tc_prepare
+
+tc_start_block "Run docker image tests"
+bazel run \
+  //pkg/testutils/docker:docker_test \
+  --config=crosslinux --config=test \
+  --test_timeout=1800
+tc_end_block "Run docker image tests"
