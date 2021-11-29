db2a38b8013dd3c0ad750adc52592234aeb98f98
Yahor Yuzefovich <yahor@cockroachlabs.com>
2021-11-29 11:29:17 -0800

    sql: do not use GetRequests for reverse scans
    We recently extended the usage of GetRequests to secondary indexes with
    no stored columns. However, currently the DistSender doesn't support
    intertwining "forward" (like `Get`) and "reverse" (like `ReverseScan`)
    requests in a single batch when the limits are set. In such a scenario
    a CRDB node would crash. This commit disables the usage of the
    GetRequests if the spans will be scanned in the reverse order.
    
    Release note: None

--- pkg/sql/distsql_spec_exec_factory.go
+++ pkg/sql/distsql_spec_exec_factory.go
@@ -210,7 +210,7 @@ func (e *distSQLSpecExecFactory) ConstructScan(
 	if params.InvertedConstraint != nil {
 		spans, err = sb.SpansFromInvertedSpans(params.InvertedConstraint, params.IndexConstraint, nil /* scratch */)
 	} else {
-		spans, err = sb.SpansFromConstraint(params.IndexConstraint, params.NeededCols, false /* forDelete */)
+		spans, err = sb.SpansFromConstraint(params.IndexConstraint, params.NeededCols, false /* forDelete */, params.Reverse)
 	}
 	if err != nil {
 		return nil, err
