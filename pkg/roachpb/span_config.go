b84d21bb88784009823c328419c517d6809b7242
irfan sharif <irfanmahmoudsharif@gmail.com>
2021-12-02 18:20:06 -0500

    spanconfig/{sqltranslator,testcluster}: support multi-tenant tests
    We introduce a nimble testing framework for multi-tenant span config
    tests, and use it to power tests for spanconfig.SQLTranslator when run
    on behalf of secondary tenants. This framework will see further addition
    when testing spanconfig.Reconciler -- we'll maintain a handle on each
    tenant's reconciler, kvaccessor, and use both to determine what
    span config mutations are made after SQL changes.
    
    This commit also leverages the ExcludeDroppedDescriptorsFromLookup
    testing knob we introduced in an earlier commit -- it obviates the need
    for arbitrary sleeps (which are both fragile and slow). It also packages
    a PrintSpanConfigDiffedAgainstDefaults helper that we'll use in a future
    PR (#71994).
    
    Release note: None

--- pkg/roachpb/span_config.go
+++ pkg/roachpb/span_config.go
@@ -125,3 +125,23 @@ func TestingDefaultSpanConfig() SpanConfig {
 		NumReplicas: 3,
 	}
 }
+
+// TestingSystemSpanConfig exports the system span config for testing purposes.
+func TestingSystemSpanConfig() SpanConfig {
+	return SpanConfig{
+		RangeMinBytes: 128 << 20, // 128 MB
+		RangeMaxBytes: 512 << 20, // 512 MB
+		// Use 25 hours instead of the previous 24 to make users successful by
+		// default. Users desiring to take incremental backups every 24h may
+		// incorrectly assume that the previous default 24h was sufficient to do
+		// that. But the equation for incremental backups is:
+		// 	GC TTLSeconds >= (desired backup interval) + (time to perform incremental backup)
+		// We think most new users' incremental backups will complete within an
+		// hour, and larger clusters will have more experienced operators and will
+		// understand how to change these settings if needed.
+		GCPolicy: GCPolicy{
+			TTLSeconds: 25 * 60 * 60,
+		},
+		NumReplicas: 5,
+	}
+}
