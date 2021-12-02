c783e0708d971988fc2187bce8eaa568e62ec26a
irfan sharif <irfanmahmoudsharif@gmail.com>
2021-12-02 14:48:01 -0500

    server,kvaccessor: record span configs during tenant creation/gc
    For newly created tenants, we want to ensure hard splits on tenant
    boundaries. The source of truth for split points in the span configs
    subsystem is the contents of system.span_configurations. To ensure
    hard splits, we insert a single key record at the tenant prefix. In a
    future commit we'll introduce the spanconfig.Reconciler process, which
    runs per tenant and governs all config entries under each tenant's
    purview. This has implications for this initial record we're talking
    about (this record might get cleaned up for e.g.); we'll explore it in
    tests for the Reconciler itself.
    
    Creating a single key record is easy enough -- we could've written
    directly to system.span_configurations. When a tenant is GC-ed
    however, we need to clear out all tenant state transactionally. To that
    end we plumb in a txn-scoped KVAccessor into the planner where
    crdb_internal.destroy_tenant is executed. This lets us easily delete
    all abandoned tenant span config records.
    
    Note: We get rid of spanconfig.experimental_kvaccessor.enabled. Access
    to spanconfigs infrastructure is already sufficiently gated through
    the env var. Now that crdb_internal.create_tenant attempts to write
    through the KVAccessor, it's cumbersome to have to enable the setting
    manually in every multi-tenant test (increasingly the default) enabling
    some part of the span configs infrastructure.
    
    ---
    
    This commit also needs a migration -- for existing clusters with
    secondary tenants, when upgrading we need to install this initial record
    at the tenant prefix for all extant tenants (and make sure to continue
    doing so for subsequent newly created tenants). This is to preserve the
    hard-split-on-tenant-boundary invariant we wish to provide. It's
    possible for an upgraded multi-tenant cluster to have dormant sql pods
    that have never reconciled. If KV switches over to the span configs
    subsystem, splitting only on the contents of system.span_configurations,
    we'll fail to split on all tenant boundaries. To this end we introduce
    clusterversion.SeedTenantSpanConfigs, which allows us to seed span
    config data for secondary tenants. The associated migration seeds
    entries for existing tenants.
    
    Release note: None

--- pkg/clusterversion/cockroach_versions.go
+++ pkg/clusterversion/cockroach_versions.go
@@ -308,6 +308,12 @@ const (
 	// This version must be active before any ProbeRequest is issued on the
 	// cluster.
 	ProbeRequest
+	// SeedTenantSpanConfigs populates system.span_configurations with seed
+	// data for secondary tenants. This state is what ensures that we always
+	// split on tenant boundaries when using the span configs infrastructure.
+	// This version also comes with a migration to populate the same seed data
+	// for existing tenants.
+	SeedTenantSpanConfigs
 
 	// *************************************************
 	// Step (1): Add new versions here.
@@ -540,6 +546,10 @@ var versionsSingleton = keyedVersions{
 	{
 		Key:     ProbeRequest,
 		Version: roachpb.Version{Major: 21, Minor: 2, Internal: 26},
+	},
+	{
+		Key:     SeedTenantSpanConfigs,
+		Version: roachpb.Version{Major: 21, Minor: 2, Internal: 28},
 	},
 
 	// *************************************************
