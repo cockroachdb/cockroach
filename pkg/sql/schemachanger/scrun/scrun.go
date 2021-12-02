d792ff926d1dd1639acd5712dbf5e751b68d46ab
Andrew Werner <awerner32@gmail.com>
2021-12-02 13:00:21 -0500

    sql/schemachanger: narrow interface to checkpoint job state
    Release note: None

--- pkg/sql/schemachanger/scrun/scrun.go
+++ pkg/sql/schemachanger/scrun/scrun.go
@@ -157,12 +157,7 @@ func RunSchemaChangesInJob(
 			if err := executeStage(ctx, td, sc, i); err != nil {
 				return err
 			}
-			if err := td.UpdateSchemaChangeJob(ctx, func(md jobs.JobMetadata, ju JobProgressUpdater) error {
-				pg := md.Progress.GetNewSchemaChange()
-				pg.States = makeStatuses(stage.After)
-				ju.UpdateProgress(md.Progress)
-				return nil
-			}); err != nil {
+			if err := td.UpdateState(ctx, stage.After); err != nil {
 				return err
 			}
 			if isLastStage {
@@ -179,14 +174,6 @@ func RunSchemaChangesInJob(
 	}
 
 	return nil
-}
-
-func makeStatuses(next scpb.State) []scpb.Status {
-	states := make([]scpb.Status, len(next.Nodes))
-	for i := range next.Nodes {
-		states[i] = next.Nodes[i].Status
-	}
-	return states
 }
 
 func makeState(
