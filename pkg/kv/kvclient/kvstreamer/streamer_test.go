6f9ec58a3060e90bf558d4010458afcd5725121a
Yahor Yuzefovich <yahor@cockroachlabs.com>
2021-12-07 14:43:31 -0800

    sql,kv: introduce Streamer API and use it for index joins in some cases
    This commit introduces the Streamer API (see
    https://github.com/cockroachdb/cockroach/blob/master/docs/RFCS/20210617_index_lookups_memory_limits.md)
    as well as its implementation for the simplest case - when requests are
    unique and can be served in any order. It additionally hooks up the
    implementation to be used by the index joins in both execution engines.
    
    There are three main pieces that this commit adds:
    1. the `Streamer` interface itself. It is largely the same as described in
    the RFC. Some notable changes are:
    - `Cancel` is renamed to `Close` and is made blocking to ensure that all
    goroutines kicked off by the `Streamer` exit before `Close` returns.
    - `Shrink` has been removed from the `Budget` interface (see below for
    more details).
    - furthermore, the `budget` interface has been unexported and the
    `streamer` has been tightly coupled with the `budget`'s implementation.
    - the TODO about collecting DistSQL metadata is removed because we are
    collecting the LeafTxnFinalState already when using the LeafTxn.
    2. the limited `streamer` implementation - only `OutOfOrder` mode is
    supported when the requests are unique. Notably, buffering and caching
    of the results is not implemented yet.
    3. `TxnKVStreamer` component that sits below the SQL fetchers, uses the
    `Streamer`, and is an adapter from `BatchResponse`s to key/value pairs
    that fetchers understand. Although not used at the moment,
    `TxnKVStreamer` is written under the assumption that a single result can
    satisfy multiple requests.
    
    The memory budget of the `Streamer` is utilized lazily. The RFC was
    calling for making a large reservation upfront and then shrinking the
    budget if we see that we don't need that large reservation; however,
    while working on this I realized that lazy reservations are a better fit
    for this. The `Streamer` can reserve up to the specified limit
    (determined by `distsql_workmem` variable) against the root monitor. The
    reservation then never shrinks under the assumption that if the
    reservation has gotten large, it means it was needed for higher
    concurrency (or large responses), and it is likely to be needed for the
    same reasons in the future.
    
    The layout of the main components of the `streamer` implementation:
    - in `Enqueue` we have a logic similar to what DistSender does in order
    to split each request (that might span multiple ranges) into
    single-range requests. Those sub-requests are batched together to be
    evaluated by a single `BatchRequest`.
    - `workerCoordinator.mainLoop` is responsible for taking single-range
    batches, estimating the corresponding response size, and issuing
    requests to be evaluated in parallel while adhering to the provided
    memory budget.
    - `workerCoordinator.performRequestAsync` is responsible for populating
    the `BatchRequest` and then processing the results while updating the
    memory budget.
    
    Current known limitations that will be addressed in the follow-up work:
    - at the moment a single row can be split across multiple BatchResponses
    when TargetBytes limit is reached when the table has multiple column
    families; therefore, we use the streamer only for single column family
    cases. We will expand the KV API shortly to not split the rows in
    multiple column family cases.
    - manual refresh of spans when `ReadWithinUncertaintyIntervalError` is
    encountered by a single streamer in a single flow is not implemented. It
    is an optimization that is considered a must for the final
    implementation in order to not regress on simple cases in terms of
    retriable errors. This will be implemented shortly as a follow-up.
    - I'm thinking that eventually we probably want to disable the batch
    splitting done by the DistSender to eliminate unnecessary blocking when
    the streamer's splitting was incorrect. This would give us some
    performance improvements in face of range boundary changes, but it
    doesn't seem important enough for the initial implementation.
    
    Release note: None

---
+++ pkg/kv/kvclient/kvstreamer/streamer_test.go
@@ -0,0 +1,271 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package kvstreamer
+
+import (
+	"context"
+	"math"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/kv"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvclient/kvcoord"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/concurrency/lock"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/mon"
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
+	"github.com/stretchr/testify/require"
+)
+
+func getStreamer(
+	ctx context.Context, s serverutils.TestServerInterface, limitBytes int64, acc *mon.BoundAccount,
+) *Streamer {
+	return NewStreamer(
+		s.DistSenderI().(*kvcoord.DistSender),
+		s.Stopper(),
+		kv.NewTxn(ctx, s.DB(), s.NodeID()),
+		cluster.MakeTestingClusterSettings(),
+		lock.WaitPolicy(0),
+		limitBytes,
+		acc,
+	)
+}
+
+// TestStreamerLimitations verifies that the streamer panics or encounters
+// errors in currently unsupported or invalid scenarios.
+func TestStreamerLimitations(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	ctx := context.Background()
+	s, _, _ := serverutils.StartServer(t, base.TestServerArgs{})
+	defer s.Stopper().Stop(ctx)
+
+	getStreamer := func() *Streamer {
+		return getStreamer(ctx, s, math.MaxInt64, nil /* acc */)
+	}
+
+	t.Run("InOrder mode unsupported", func(t *testing.T) {
+		require.Panics(t, func() {
+			streamer := getStreamer()
+			streamer.Init(InOrder, Hints{UniqueRequests: true})
+		})
+	})
+
+	t.Run("non-unique requests unsupported", func(t *testing.T) {
+		require.Panics(t, func() {
+			streamer := getStreamer()
+			streamer.Init(OutOfOrder, Hints{UniqueRequests: false})
+		})
+	})
+
+	t.Run("invalid enqueueKeys", func(t *testing.T) {
+		streamer := getStreamer()
+		defer streamer.Close()
+		streamer.Init(OutOfOrder, Hints{UniqueRequests: true})
+		// Use a single request but two keys which is invalid.
+		reqs := []roachpb.RequestUnion{{Value: &roachpb.RequestUnion_Get{}}}
+		enqueueKeys := []int{0, 1}
+		require.Error(t, streamer.Enqueue(ctx, reqs, enqueueKeys))
+	})
+
+	t.Run("pipelining unsupported", func(t *testing.T) {
+		streamer := getStreamer()
+		defer streamer.Close()
+		streamer.Init(OutOfOrder, Hints{UniqueRequests: true})
+		get := roachpb.NewGet(roachpb.Key("key"), false /* forUpdate */)
+		reqs := []roachpb.RequestUnion{{
+			Value: &roachpb.RequestUnion_Get{
+				Get: get.(*roachpb.GetRequest),
+			},
+		}}
+		require.NoError(t, streamer.Enqueue(ctx, reqs, nil /* enqueueKeys */))
+		// It is invalid to enqueue more requests before the previous have been
+		// responded to.
+		require.Error(t, streamer.Enqueue(ctx, reqs, nil /* enqueueKeys */))
+	})
+}
+
+// TestLargeKeys verifies that the Streamer successfully completes the queries
+// when the keys to lookup are large (i.e. the enqueued requests themselves have
+// large memory footprint).
+func TestLargeKeys(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	skip.UnderStress(t, "the test inserts large blobs, and the machine can be overloaded when under stress")
+
+	rng, _ := randutil.NewTestRand()
+	s, db, _ := serverutils.StartServer(t, base.TestServerArgs{})
+	ctx := context.Background()
+	defer s.Stopper().Stop(ctx)
+
+	// Lower the distsql_workmem limit so that we can operate with smaller
+	// blobs. Note that the joinReader in the row-by-row engine will override
+	// the limit if it is lower than 8MiB, so we cannot go lower than that here.
+	_, err := db.Exec("SET distsql_workmem='8MiB'")
+	require.NoError(t, err)
+	// In both engines, the index joiner buffers input rows up to 4MiB in size,
+	// so we have a couple of interesting options for the blob size:
+	// - 3000000 is interesting because it doesn't exceed the buffer size, yet
+	// two rows with such blobs do exceed it. The index joiners are expected to
+	// to process each row on its own.
+	// - 5000000 is interesting because a single row already exceeds the buffer
+	// size.
+	for _, blobSize := range []int{3000000, 5000000} {
+		// onlyLarge determines whether only large blobs are inserted or a mix
+		// of large and small blobs.
+		for _, onlyLarge := range []bool{false, true} {
+			_, err = db.Exec("DROP TABLE IF EXISTS foo")
+			require.NoError(t, err)
+			// We set up such a table that contains two large columns, one of them
+			// being the primary key. The idea is that the query below will first
+			// read from the secondary index which would include only the PK blob,
+			// and that will be used to construct index join lookups (i.e. the PK
+			// blobs will be the enqueued requests for the Streamer) whereas the
+			// other blob will be part of the response.
+			_, err = db.Exec("CREATE TABLE foo (pk_blob STRING PRIMARY KEY, attribute INT, blob TEXT, INDEX(attribute))")
+			require.NoError(t, err)
+
+			// Insert a handful of rows.
+			numRows := rng.Intn(3) + 3
+			for i := 0; i < numRows; i++ {
+				letter := string(byte('a') + byte(i))
+				valueSize := blobSize
+				if !onlyLarge && rng.Float64() < 0.5 {
+					// If we're using a mix of large and small values, with 50%
+					// use a small value now.
+					valueSize = rng.Intn(10) + 1
+				}
+				_, err = db.Exec("INSERT INTO foo SELECT repeat($1, $2), 1, repeat($1, $2)", letter, valueSize)
+				require.NoError(t, err)
+			}
+
+			// Perform an index join so that the Streamer API is used.
+			query := "SELECT * FROM foo@foo_attribute_idx WHERE attribute=1"
+			testutils.RunTrueAndFalse(t, "vectorize", func(t *testing.T, vectorize bool) {
+				vectorizeMode := "off"
+				if vectorize {
+					vectorizeMode = "on"
+				}
+				_, err = db.Exec("SET vectorize = " + vectorizeMode)
+				require.NoError(t, err)
+				_, err = db.Exec(query)
+				require.NoError(t, err)
+			})
+		}
+	}
+}
+
+// TestStreamerBudgetErrorInEnqueue verifies the behavior of the Streamer in
+// Enqueue when its limit and/or root pool limit are exceeded. Additional tests
+// around the memory limit errors (when the responses exceed the limit) can be
+// found in TestMemoryLimit in pkg/sql.
+func TestStreamerBudgetErrorInEnqueue(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	ctx := context.Background()
+	s, db, _ := serverutils.StartServer(t, base.TestServerArgs{})
+	defer s.Stopper().Stop(ctx)
+
+	// Create a dummy table for which we know the encoding of valid keys.
+	_, err := db.Exec("CREATE TABLE foo (pk_blob STRING PRIMARY KEY, attribute INT, blob TEXT, INDEX(attribute))")
+	require.NoError(t, err)
+
+	// makeGetRequest returns a valid GetRequest that wants to lookup a key with
+	// value 'a' repeated keySize number of times in the primary index of table
+	// foo.
+	makeGetRequest := func(keySize int) roachpb.RequestUnion {
+		var res roachpb.RequestUnion
+		var get roachpb.GetRequest
+		var union roachpb.RequestUnion_Get
+		key := make([]byte, keySize+6)
+		key[0] = 190
+		key[1] = 137
+		key[2] = 18
+		for i := 0; i < keySize; i++ {
+			key[i+3] = 97
+		}
+		key[keySize+3] = 0
+		key[keySize+4] = 1
+		key[keySize+5] = 136
+		get.Key = key
+		union.Get = &get
+		res.Value = &union
+		return res
+	}
+
+	// Imitate a root SQL memory monitor with 1MiB size.
+	const rootPoolSize = 1 << 20 /* 1MiB */
+	rootMemMonitor := mon.NewMonitor(
+		"root", /* name */
+		mon.MemoryResource,
+		nil,           /* curCount */
+		nil,           /* maxHist */
+		-1,            /* increment */
+		math.MaxInt64, /* noteworthy */
+		cluster.MakeTestingClusterSettings(),
+	)
+	rootMemMonitor.Start(ctx, nil /* pool */, mon.MakeStandaloneBudget(rootPoolSize))
+	defer rootMemMonitor.Stop(ctx)
+
+	acc := rootMemMonitor.MakeBoundAccount()
+	defer acc.Close(ctx)
+
+	getStreamer := func(limitBytes int64) *Streamer {
+		acc.Clear(ctx)
+		s := getStreamer(ctx, s, limitBytes, &acc)
+		s.Init(OutOfOrder, Hints{UniqueRequests: true})
+		return s
+	}
+
+	t.Run("single key exceeds limit", func(t *testing.T) {
+		const limitBytes = 10
+		streamer := getStreamer(limitBytes)
+		defer streamer.Close()
+
+		// A single request that exceeds the limit should be allowed.
+		reqs := make([]roachpb.RequestUnion, 1)
+		reqs[0] = makeGetRequest(limitBytes + 1)
+		require.NoError(t, streamer.Enqueue(ctx, reqs, nil /* enqueueKeys */))
+	})
+
+	t.Run("single key exceeds root pool size", func(t *testing.T) {
+		const limitBytes = 10
+		streamer := getStreamer(limitBytes)
+		defer streamer.Close()
+
+		// A single request that exceeds the limit as well as the root SQL pool
+		// should be denied.
+		reqs := make([]roachpb.RequestUnion, 1)
+		reqs[0] = makeGetRequest(rootPoolSize + 1)
+		require.Error(t, streamer.Enqueue(ctx, reqs, nil /* enqueueKeys */))
+	})
+
+	t.Run("multiple keys exceed limit", func(t *testing.T) {
+		const limitBytes = 10
+		streamer := getStreamer(limitBytes)
+		defer streamer.Close()
+
+		// Create two requests which exceed the limit when combined.
+		reqs := make([]roachpb.RequestUnion, 2)
+		reqs[0] = makeGetRequest(limitBytes/2 + 1)
+		reqs[1] = makeGetRequest(limitBytes/2 + 1)
+		require.Error(t, streamer.Enqueue(ctx, reqs, nil /* enqueueKeys */))
+	})
+}
