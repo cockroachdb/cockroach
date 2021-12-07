9131ba2331604d5d77870377697b53e2e50d4184
richardjcai <caioftherichard@gmail.com>
2021-12-06 19:02:20 -0500

    sql: insert missing public schema namespace entry
    When restoring a database, a namespace entry for the public
    schema was not created.
    
    Release note: None

--- pkg/clusterversion/cockroach_versions.go
+++ pkg/clusterversion/cockroach_versions.go
@@ -291,6 +291,9 @@ const (
 	// MVCCAddSSTable supports MVCC-compliant AddSSTable requests via the new
 	// WriteAtRequestTimestamp and DisallowConflicts parameters.
 	MVCCAddSSTable
+	// InsertPublicSchemaNamespaceEntryOnRestore ensures all public schemas
+	// have an entry in system.namespace upon being restored.
+	InsertPublicSchemaNamespaceEntryOnRestore
 	// Public schema is backed by a descriptor.
 	PublicSchemasWithDescriptors
 	// UnsplitRangesInAsyncGCJobs moves ranges unsplitting from transaction of
@@ -522,7 +525,7 @@ var versionsSingleton = keyedVersions{
 		Version: roachpb.Version{Major: 21, Minor: 2, Internal: 16},
 	},
 	{
-		Key:     PublicSchemasWithDescriptors,
+		Key:     InsertPublicSchemaNamespaceEntryOnRestore,
 		Version: roachpb.Version{Major: 21, Minor: 2, Internal: 18},
 	},
 	{
@@ -540,6 +543,10 @@ var versionsSingleton = keyedVersions{
 	{
 		Key:     ProbeRequest,
 		Version: roachpb.Version{Major: 21, Minor: 2, Internal: 26},
+	},
+	{
+		Key:     PublicSchemasWithDescriptors,
+		Version: roachpb.Version{Major: 21, Minor: 2, Internal: 28},
 	},
 
 	// *************************************************
