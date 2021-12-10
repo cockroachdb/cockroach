276c6ecbacaa509ea7febf52da3e97e952d6f2df
Marius Posta <marius@cockroachlabs.com>
2021-12-09 23:35:03 -0500

    scstate: refactor BuildStages
    This commit breaks down the logic that was previously inside the
    BuildStages function's body into separate functions. Mainly, the closures
    have been made into methods on a buildCtx struct which basically is
    the closure's captured variables made explicit. This commit is strictly
    a refactor.
    
    Release note: None

--- pkg/sql/schemachanger/scplan/scstage/build.go
+++ pkg/sql/schemachanger/scplan/scstage/build.go
@@ -22,197 +22,217 @@ import (
 )
 
 // BuildStages builds the plan's stages.
-func BuildStages(init scpb.State, phase scop.Phase, g *scgraph.Graph) []Stage {
-	// Fetch the order of the graph, which will be used to
-	// evaluating edges in topological order.
-	nodeRanks, err := g.GetNodeRanks()
-	if err != nil {
-		panic(err)
-	}
-	// TODO(ajwerner): deal with the case where the target status was
-	// fulfilled by something that preceded the initial state.
-	cur := init
-	fulfilled := map[*scpb.Node]struct{}{}
-	filterUnsatisfiedEdgesStep := func(edges []*scgraph.OpEdge) ([]*scgraph.OpEdge, bool) {
-		candidates := make(map[*scpb.Node]*scgraph.OpEdge)
-		for _, e := range edges {
-			candidates[e.To()] = e
-		}
-		// Check to see if the current set of edges will have their dependencies met
-		// if they are all run. Any which will not must be pruned. This greedy
-		// algorithm works, but a justification is in order.
-		failed := map[*scgraph.OpEdge]struct{}{}
-		for _, e := range edges {
-			if !e.IsPhaseSatisfied(phase) {
-				failed[e] = struct{}{}
-			}
-		}
-		for _, e := range edges {
-			if err := g.ForEachDepEdgeFrom(e.To(), func(de *scgraph.DepEdge) error {
-				_, isFulfilled := fulfilled[de.To()]
-				_, isCandidate := candidates[de.To()]
-				if de.Kind() == scgraph.SameStage && isFulfilled {
-					// This is bad, we have a happens-after relationship, and it has
-					// already happened.
-					return errors.AssertionFailedf("failed to satisfy %v->%v (%s) dependency",
-						screl.NodeString(de.From()), screl.NodeString(de.To()), de.Name())
-				}
-				if isFulfilled || isCandidate {
-					return nil
-				}
-				failed[e] = struct{}{}
-				return iterutil.StopIteration()
-			}); err != nil {
-				panic(err)
-			}
-		}
-		// Ensure that all SameStage DepEdges are met appropriately.
-		for _, e := range edges {
-			if err := g.ForEachSameStageDepEdgeTo(e.To(), func(de *scgraph.DepEdge) error {
-				if _, isFulfilled := fulfilled[de.From()]; isFulfilled {
-					// This is bad, we have a happens-after relationship, and it has
-					// already happened.
-					return errors.AssertionFailedf("failed to satisfy %v->%v (%s) dependency",
-						screl.NodeString(de.From()), screl.NodeString(de.To()), de.Name())
-				}
-				fromCandidate, fromIsCandidate := candidates[de.From()]
-				if !fromIsCandidate {
-					failed[e] = struct{}{}
-					return iterutil.StopIteration()
-				}
-				_, fromIsFailed := failed[fromCandidate]
-				if fromIsFailed {
-					failed[e] = struct{}{}
-					return iterutil.StopIteration()
-				}
-				if _, eIsFailed := failed[e]; eIsFailed {
-					failed[fromCandidate] = struct{}{}
-				}
-				return nil
-			}); err != nil {
-				panic(err)
-			}
-		}
-		if len(failed) == 0 {
-			return edges, true
-		}
-		truncated := edges[:0]
-		for _, e := range edges {
-			if _, found := failed[e]; !found {
-				truncated = append(truncated, e)
-			}
-		}
-		return truncated, false
-	}
-	filterUnsatisfiedEdges := func(edges []*scgraph.OpEdge) ([]*scgraph.OpEdge, bool) {
-		for len(edges) > 0 {
-			if filtered, done := filterUnsatisfiedEdgesStep(edges); !done {
-				edges = filtered
-			} else {
-				return filtered, true
-			}
-		}
-		return edges, false
-	}
-	isRevertiblePreferred := true
-	buildStageType := func(edges []*scgraph.OpEdge) (Stage, bool) {
-		edges, ok := filterUnsatisfiedEdges(edges)
-		if !ok {
-			return Stage{}, false
-		}
-		sort.SliceStable(edges,
-			func(i, j int) bool {
-				// Higher ranked edges should go first.
-				return nodeRanks[edges[i].To()] > nodeRanks[edges[j].To()]
-			})
-
-		next := shallowCopy(cur)
-		var ops []scop.Op
-		var isStageRevertible bool
-		for _, isStageRevertible = range []bool{true, false} {
-			if isStageRevertible && !isRevertiblePreferred {
-				continue
-			}
-			hasTransitions := false
-			for _, e := range edges {
-				for i, ts := range cur.Nodes {
-					if e.From() == ts && (!isStageRevertible || e.Revertible()) {
-						next.Nodes[i] = e.To()
-						hasTransitions = true
-						// If this edge has been marked as a no-op, then a state transition
-						// can happen without executing anything.
-						if !g.IsOpEdgeOptimizedOut(e) {
-							ops = append(ops, e.Op()...)
-						}
-						break
-					}
-				}
-			}
-			if hasTransitions {
-				break
-			}
-		}
-		if isRevertiblePreferred && !isStageRevertible {
-			isRevertiblePreferred = false
-		}
-		return Stage{
-			Before:     cur,
-			After:      next,
-			Ops:        scop.MakeOps(ops...),
-			Revertible: isStageRevertible,
-			Phase:      phase,
-		}, true
-	}
-
+func BuildStages(init scpb.State, phase scop.Phase, g *scgraph.Graph) (stages []Stage) {
+	b := makeBuildContext(init, phase, g)
 	// Build stages for this phase and all subsequent phases.
-	var stages []Stage
-	for ; phase <= scop.PostCommitPhase; phase++ {
-		for {
-			// Note that the current nodes are fulfilled for the sake of dependency
-			// checking.
-			for _, ts := range cur.Nodes {
-				fulfilled[ts] = struct{}{}
-			}
+	for b.phase <= scop.PostCommitPhase {
+		// Note that the current nodes are fulfilled for the sake of dependency
+		// checking.
+		for _, ts := range b.state.Nodes {
+			b.fulfilled[ts] = struct{}{}
+		}
 
-			// Extract the set of op edges for the current stage.
-			var opEdges []*scgraph.OpEdge
-			for _, t := range cur.Nodes {
-				// TODO(ajwerner): improve the efficiency of this lookup.
-				// Look for an opEdge from this node. Then, for the other side
-				// of the opEdge, look for dependencies.
-				if oe, ok := g.GetOpEdgeFrom(t); ok {
-					opEdges = append(opEdges, oe)
-				}
+		// Extract the set of op edges for the current stage.
+		var opEdges []*scgraph.OpEdge
+		for _, t := range b.state.Nodes {
+			// TODO(ajwerner): improve the efficiency of this lookup.
+			// Look for an opEdge from this node. Then, for the other side
+			// of the opEdge, look for dependencies.
+			if oe, ok := b.g.GetOpEdgeFrom(t); ok {
+				opEdges = append(opEdges, oe)
 			}
+		}
 
-			// Group the op edges a per-type basis.
-			opTypes := make(map[scop.Type][]*scgraph.OpEdge)
-			for _, oe := range opEdges {
-				opTypes[oe.Type()] = append(opTypes[oe.Type()], oe)
-			}
-
-			// Greedily attempt to find a stage which can be executed. This is sane
-			// because once a dependency is met, it never becomes unmet.
-			var didSomething bool
-			var s Stage
-			for _, typ := range []scop.Type{
-				scop.MutationType,
-				scop.BackfillType,
-				scop.ValidationType,
-			} {
-				if s, didSomething = buildStageType(opTypes[typ]); didSomething {
-					break
-				}
-			}
-			if !didSomething {
-				break
-			}
-			stages = append(stages, s)
-			cur = s.After
+		if s := b.tryBuildStage(opEdges); s != nil {
+			stages = append(stages, *s)
+			b.state = s.After
+			b.isRevertiblePreferred = b.isRevertiblePreferred && s.Revertible
+		} else {
+			// No further progress is possible in this phase, move on to the next one.
+			b.phase++
 		}
 	}
 
 	return decorateStages(stages)
+}
+
+// buildCtx contains the necessary context for building a stage.
+// When building a stage, this struct is read-only.
+type buildCtx struct {
+	g                     *scgraph.Graph
+	nodeRanks             map[*scpb.Node]int
+	state                 scpb.State
+	phase                 scop.Phase
+	isRevertiblePreferred bool
+	fulfilled             map[*scpb.Node]struct{}
+}
+
+// makeBuildContext is the constructor for buildCtx.
+func makeBuildContext(init scpb.State, phase scop.Phase, g *scgraph.Graph) buildCtx {
+	// Fetch the ranks of the nodes, which will be used to evaluate edges
+	// in topological order.
+	nodeRanks, err := g.GetNodeRanks()
+	if err != nil {
+		panic(err)
+	}
+	return buildCtx{
+		g:                     g,
+		phase:                 phase,
+		isRevertiblePreferred: true,
+		nodeRanks:             nodeRanks,
+		// TODO(ajwerner): deal with the case where the target status was
+		// fulfilled by something that preceded the initial state.
+		state:     init,
+		fulfilled: make(map[*scpb.Node]struct{}, len(nodeRanks)),
+	}
+}
+
+// tryBuildStage tries to build a stage using the provided op-edges.
+// This is done on a best-effort basis, if not possible nil is returned.
+func (b buildCtx) tryBuildStage(edges []*scgraph.OpEdge) *Stage {
+	// Group the op edges a per-type basis.
+	opTypes := make(map[scop.Type][]*scgraph.OpEdge)
+	for _, oe := range edges {
+		opTypes[oe.Type()] = append(opTypes[oe.Type()], oe)
+	}
+
+	// Greedily attempt to find a stage which can be executed. This is sane
+	// because once a dependency is met, it never becomes unmet.
+	for _, typ := range []scop.Type{
+		scop.MutationType,
+		scop.BackfillType,
+		scop.ValidationType,
+	} {
+		if filteredEdges := b.filterUnsatisfiedEdges(opTypes[typ]); len(filteredEdges) > 0 {
+			s := b.makeStage(filteredEdges)
+			return &s
+		}
+	}
+	return nil
+}
+
+func (b buildCtx) filterUnsatisfiedEdges(edges []*scgraph.OpEdge) []*scgraph.OpEdge {
+	for len(edges) > 0 {
+		failed := b.collectFailedEdges(edges)
+		if len(failed) == 0 {
+			return edges
+		}
+
+		// Remove all failed edges from the edges slice.
+		filtered := edges[:0]
+		for _, e := range edges {
+			if _, found := failed[e]; !found {
+				filtered = append(filtered, e)
+			}
+		}
+		edges = filtered
+	}
+	return nil
+}
+
+func (b buildCtx) collectFailedEdges(
+	edges []*scgraph.OpEdge,
+) (failed map[*scgraph.OpEdge]struct{}) {
+	candidates := make(map[*scpb.Node]*scgraph.OpEdge)
+	for _, e := range edges {
+		candidates[e.To()] = e
+	}
+	// Check to see if the current set of edges will have their dependencies met
+	// if they are all run. Any which will not must be pruned. This greedy
+	// algorithm works, but a justification is in order.
+	failed = make(map[*scgraph.OpEdge]struct{}, len(edges))
+	for _, e := range edges {
+		if !e.IsPhaseSatisfied(b.phase) {
+			failed[e] = struct{}{}
+		}
+	}
+	for _, e := range edges {
+		if err := b.g.ForEachDepEdgeFrom(e.To(), func(de *scgraph.DepEdge) error {
+			_, isFulfilled := b.fulfilled[de.To()]
+			_, isCandidate := candidates[de.To()]
+			if de.Kind() == scgraph.SameStage && isFulfilled {
+				// This is bad, we have a happens-after relationship, and it has
+				// already happened.
+				return errors.AssertionFailedf("failed to satisfy %v->%v (%s) dependency",
+					screl.NodeString(de.From()), screl.NodeString(de.To()), de.Name())
+			}
+			if isFulfilled || isCandidate {
+				return nil
+			}
+			failed[e] = struct{}{}
+			return iterutil.StopIteration()
+		}); err != nil {
+			panic(err)
+		}
+	}
+	// Ensure that all SameStage DepEdges are met appropriately.
+	for _, e := range edges {
+		if err := b.g.ForEachSameStageDepEdgeTo(e.To(), func(de *scgraph.DepEdge) error {
+			if _, isFulfilled := b.fulfilled[de.From()]; isFulfilled {
+				// This is bad, we have a happens-after relationship, and it has
+				// already happened.
+				return errors.AssertionFailedf("failed to satisfy %v->%v (%s) dependency",
+					screl.NodeString(de.From()), screl.NodeString(de.To()), de.Name())
+			}
+			fromCandidate, fromIsCandidate := candidates[de.From()]
+			if !fromIsCandidate {
+				failed[e] = struct{}{}
+				return iterutil.StopIteration()
+			}
+			_, fromIsFailed := failed[fromCandidate]
+			if fromIsFailed {
+				failed[e] = struct{}{}
+				return iterutil.StopIteration()
+			}
+			if _, eIsFailed := failed[e]; eIsFailed {
+				failed[fromCandidate] = struct{}{}
+			}
+			return nil
+		}); err != nil {
+			panic(err)
+		}
+	}
+	return failed
+}
+
+func (b buildCtx) makeStage(filteredEdges []*scgraph.OpEdge) Stage {
+	sort.SliceStable(filteredEdges,
+		func(i, j int) bool {
+			// Higher ranked edges should go first.
+			return b.nodeRanks[filteredEdges[i].To()] > b.nodeRanks[filteredEdges[j].To()]
+		})
+
+	s := Stage{
+		Before: b.state,
+		After:  shallowCopy(b.state),
+		Phase:  b.phase,
+	}
+	var ops []scop.Op
+	for _, s.Revertible = range []bool{true, false} {
+		if s.Revertible && !b.isRevertiblePreferred {
+			continue
+		}
+		hasTransitions := false
+		for _, e := range filteredEdges {
+			for i, ts := range b.state.Nodes {
+				if e.From() == ts && (!s.Revertible || e.Revertible()) {
+					s.After.Nodes[i] = e.To()
+					hasTransitions = true
+					// If this edge has been marked as a no-op, then a state transition
+					// can happen without executing anything.
+					if !b.g.IsOpEdgeOptimizedOut(e) {
+						ops = append(ops, e.Op()...)
+					}
+					break
+				}
+			}
+		}
+		if hasTransitions {
+			break
+		}
+	}
+	s.Ops = scop.MakeOps(ops...)
+	return s
 }
 
 // shallowCopy creates a shallow copy of the passed state. Importantly, it
