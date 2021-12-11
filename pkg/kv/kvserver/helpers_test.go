95a7671a31e0935c4fd3ba20f9bc254a5f7c4ec1
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2021-12-11 12:53:52 -0500

    kv: rename unquiesceXYZ methods to maybeUnquiesceXYZ
    This commit renames the `unquiesceXYZ` methods to include a "maybe" prefix to
    indicate that they may not succeed in unquiescing the replica.
    
    The commit also introduces a boolean return value to these methods to inform
    the caller about whether the unquiesce attempt was successful.
    
    Finally, the commit uses this return value in `maybeMarkReplicaInitializedLockedReplLocked`
    to assert that after initializing a replica, it did unquiesce immediately.

--- pkg/kv/kvserver/helpers_test.go
+++ pkg/kv/kvserver/helpers_test.go
@@ -462,10 +462,10 @@ func (r *Replica) GetQueueLastProcessed(ctx context.Context, queue string) (hlc.
 	return r.getQueueLastProcessed(ctx, queue)
 }
 
-func (r *Replica) UnquiesceAndWakeLeader() {
+func (r *Replica) MaybeUnquiesceAndWakeLeader() bool {
 	r.mu.Lock()
 	defer r.mu.Unlock()
-	r.unquiesceAndWakeLeaderLocked()
+	return r.maybeUnquiesceAndWakeLeaderLocked()
 }
 
 func (r *Replica) ReadProtectedTimestamps(ctx context.Context) {
