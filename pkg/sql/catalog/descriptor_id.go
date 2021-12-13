822c416fb38e0ab5043d9473394f669d95db4acd
Andrew Werner <awerner32@gmail.com>
2021-12-13 10:04:04 -0500

    keys,sql/catalog: add interface to check whether an ID is in the system db
    This interface will be used to replace the functions which compare to
    constants.
    
    Release note: None

---
+++ pkg/sql/catalog/descriptor_id.go
@@ -0,0 +1,21 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package catalog
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+)
+
+// IsSystemID returns true if the ID is part of the system database.
+func IsSystemID(c keys.SystemIDChecker, id descpb.ID) bool {
+	return c.IsSystemID(uint32(id))
+}
