57f926d9588ef8b6985c7bc17142aba1228aed98
Tobias Grieger <tobias.b.grieger@gmail.com>
2021-12-15 10:33:11 +0100

    issues: improve TestPost and fix HelpCommand
    The help command for unit tests didn't render properly:
    
    ![before](https://user-images.githubusercontent.com/5076964/146162775-ee58e441-a41e-471c-a80a-65f4ad806368.png)
    
    It now does:
    
    ![after](https://user-images.githubusercontent.com/5076964/146163054-bd6ea3c9-d1dd-42ae-a755-28b0c7973377.png)
    
    This would've been a very small change, but I decided to take the
    opportunity to improve testing, as similar problems have cropped up
    repeatedly in the past. To this end, I adjusted `TestPost` to use
    `datadriven` (in particular giving it `-rewrite` support out of the box)
    and also made sure that it exercises the HelpCommand logic that is
    actually used to post the issues (it was exercising a stub before).
    
    Release note: None

--- pkg/cmd/github-post/main.go
+++ pkg/cmd/github-post/main.go
@@ -60,15 +60,11 @@ func defaultFormatter(ctx context.Context, f failure) (issues.IssueFormatter, is
 		}
 	}
 	return issues.UnitTestFormatter, issues.PostRequest{
-		TestName:    f.testName,
-		PackageName: f.packageName,
-		Message:     f.testMessage,
-		Artifacts:   "/", // best we can do for unit tests
-		HelpCommand: func(r *issues.Renderer) {
-			issues.ReproductionCommandFromString(repro)
-			r.Escaped("See also: ")
-			r.A("How To Investigate a Go Test Failure (internal)", "https://cockroachlabs.atlassian.net/l/c/HgfXfJgM")
-		},
+		TestName:        f.testName,
+		PackageName:     f.packageName,
+		Message:         f.testMessage,
+		Artifacts:       "/", // best we can do for unit tests
+		HelpCommand:     issues.UnitTestHelpCommand(repro),
 		MentionOnCreate: mentions,
 		ProjectColumnID: projColID,
 	}
