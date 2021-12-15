08f9e600f2a3f6fa393f8430be04076ae2ec62c4
irfan sharif <irfanmahmoudsharif@gmail.com>
2021-12-15 18:46:11 -0500

    spanconfig/testcluster: plumb in testing knobs for tenants
    We were previously using an empty one with no control at the caller to
    override specific values. This comes in handy when looking to control
    knobs for all tenants in these tests.
    
    Release note: None

--- pkg/ccl/spanconfigccl/spanconfigcomparedccl/datadriven_test.go
+++ pkg/ccl/spanconfigccl/spanconfigcomparedccl/datadriven_test.go
@@ -110,7 +110,7 @@ func TestDataDriven(t *testing.T) {
 			tdb.Exec(t, `SET CLUSTER SETTING kv.closed_timestamp.target_duration = '100ms'`)
 		}
 
-		spanConfigTestCluster := spanconfigtestcluster.NewHandle(t, tc)
+		spanConfigTestCluster := spanconfigtestcluster.NewHandle(t, tc, scKnobs)
 		defer spanConfigTestCluster.Cleanup()
 
 		kvSubscriber := tc.Server(0).SpanConfigKVSubscriber().(spanconfig.KVSubscriber)
