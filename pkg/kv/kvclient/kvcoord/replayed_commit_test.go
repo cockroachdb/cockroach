1de186557b787af930325aa380344d8a30178a10
Tobias Grieger <tobias.b.grieger@gmail.com>
2021-12-15 11:19:29 +0100

    kvcoord: simplify txn already committed assertion
    Also improves
    TestCommitSanityCheckAssertionFiresOnUndetectedAmbiguousCommit to use a
    testing knob as opposed to updating the env var, which is always going
    to carry some residual risk of a data race.
    
    Release note: None

--- pkg/kv/kvclient/kvcoord/replayed_commit_test.go
+++ pkg/kv/kvclient/kvcoord/replayed_commit_test.go
@@ -19,6 +19,8 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/kv"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvclient/kvcoord"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvserverbase"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/rpc/nodedialer"
 	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
@@ -53,43 +55,45 @@ func TestCommitSanityCheckAssertionFiresOnUndetectedAmbiguousCommit(t *testing.T
 
 	ctx := context.Background()
 	var args base.TestClusterArgs
-	args.ServerArgs.Knobs.KVClient = &kvcoord.ClientTestingKnobs{TransportFactory: func(
-		options kvcoord.SendOptions,
-		dialer *nodedialer.Dialer,
-		slice kvcoord.ReplicaSlice,
-	) (kvcoord.Transport, error) {
-		tf, err := kvcoord.GRPCTransportFactory(options, dialer, slice)
-		if err != nil {
-			return nil, err
-		}
-		return &interceptingTransport{
-			Transport: tf,
-			intercept: func(ctx context.Context, ba roachpb.BatchRequest, br *roachpb.BatchResponse, err error) (*roachpb.BatchResponse, error) {
-				if err != nil || ba.Txn == nil || br.Txn == nil ||
-					ba.Txn.Status != roachpb.PENDING || br.Txn.Status != roachpb.COMMITTED ||
-					!keys.ScratchRangeMin.Equal(br.Txn.Key) {
-					// Only want to inject error on successful commit for "our" txn.
-					return br, err
-				}
-				err = circuit.ErrBreakerOpen
-				assert.True(t, grpcutil.RequestDidNotStart(err)) // avoid Fatal on goroutine
+	args.ServerArgs.Knobs.Store = &kvserver.StoreTestingKnobs{
+		IntentResolverKnobs: kvserverbase.IntentResolverTestingKnobs{
+			// Disable async intent resolution, as it could possibly GC the txn record
+			// out from under us, leading to the retried commit taking a path
+			// different from the one we want to exercise in this test.
+			DisableAsyncIntentResolution: true,
+		},
+	}
+	args.ServerArgs.Knobs.KVClient = &kvcoord.ClientTestingKnobs{
+		TransportFactory: func(
+			options kvcoord.SendOptions,
+			dialer *nodedialer.Dialer,
+			slice kvcoord.ReplicaSlice,
+		) (kvcoord.Transport, error) {
+			tf, err := kvcoord.GRPCTransportFactory(options, dialer, slice)
+			if err != nil {
 				return nil, err
-			},
-		}, nil
-	},
+			}
+			return &interceptingTransport{
+				Transport: tf,
+				intercept: func(ctx context.Context, ba roachpb.BatchRequest, br *roachpb.BatchResponse, err error) (*roachpb.BatchResponse, error) {
+					if err != nil || ba.Txn == nil || br.Txn == nil ||
+						ba.Txn.Status != roachpb.PENDING || br.Txn.Status != roachpb.COMMITTED ||
+						!keys.ScratchRangeMin.Equal(br.Txn.Key) {
+						// Only want to inject error on successful commit for "our" txn.
+						return br, err
+					}
+					err = circuit.ErrBreakerOpen
+					assert.True(t, grpcutil.RequestDidNotStart(err)) // avoid Fatal on goroutine
+					return nil, err
+				},
+			}, nil
+		},
+		// Turn the assertion into an error returned via the txn.
+		DisableCommitSanityCheck: true,
 	}
 
 	tc := testcluster.StartTestCluster(t, 1, args)
 	defer tc.Stopper().Stop(ctx)
-
-	{
-		// Turn the assertion into an error.
-		prev := kvcoord.DisableCommitSanityCheck
-		kvcoord.DisableCommitSanityCheck = true
-		defer func() {
-			kvcoord.DisableCommitSanityCheck = prev
-		}()
-	}
 
 	k := tc.ScratchRange(t)
 	kNext := k.Next()
