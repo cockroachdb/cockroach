e260026c056c2ff749da495c066763f4eda505b6
Michael Butler <butler@cockroachlabs.com>
2021-12-15 21:18:24 -0500

    backupccl: allow user to compress parquet file from EXPORT PARQUET
    Previously, EXPORT PARQUET compressed all parquet files with snappy. Now EXPORT
    PARQUET defaults to exporting an uncompressed file and provides the
    'compression' option which can be either 'gzip' or 'snappy'.
    
    To implement this cleanly, the ParquetWriterProcessor now uses the
    CSVWriterSpec, deprecating the ParquetWriterSpec.
    
    Release note (sql change): EXPORT PARQUET has a new 'compression' option whose
    value can be 'gzip' or 'snappy'. An example query:
    
    `EXPORT INTO PARQUET 'nodelocal://0/compress_snappy' WITH compression = snappy
       FROM SELECT * FROM foo `
    
    By default, the parquet file will be uncompressed. With compression, the file
    name will be '[filename].parquet.gz' or '[filename].parquet.snappy'.

--- pkg/sql/execinfra/version.go
+++ pkg/sql/execinfra/version.go
@@ -39,17 +39,20 @@ import "github.com/cockroachdb/cockroach/pkg/sql/execinfrapb"
 //
 // ATTENTION: When updating these fields, add a brief description of what
 // changed to the version history below.
-const Version execinfrapb.DistSQLVersion = 58
+const Version execinfrapb.DistSQLVersion = 59
 
 // MinAcceptedVersion is the oldest version that the server is compatible with.
 // A server will not accept flows with older versions.
-const MinAcceptedVersion execinfrapb.DistSQLVersion = 58
+const MinAcceptedVersion execinfrapb.DistSQLVersion = 59
 
 /*
 
 **  VERSION HISTORY **
 
 Please add new entries at the top.
+
+- Version: 59 (MinAcceptedVersion: 59):
+ - Deprecated ExportWriterSpec and ParquetWriterSpec and merged them into ExportSpec
 
 - Version: 58 (MinAcceptedVersion: 58)
 	- TableReaderSpec now contains a specific list of column IDs and the internal
