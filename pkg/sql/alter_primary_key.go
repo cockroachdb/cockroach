39c807ace589d178cd9788db7bc79b9e7744e289
Chengxiong Ruan <chengxiongruan@gmail.com>
2021-12-16 11:56:20 -0500

    sql: primary key to allow virtual columns
    To use virtual column as shard column for hash index, we need to
    first allow primary key to accept virtual columns. This pr loosen
    the constraint on primary key, allow virtual columns in suffix cols
    of secondary index as long as they are in primary key. Also added
    extra logic to make sure secondary index is rewritten if it contains
    any virtual column not exists in new primary when altering primary
    key.
    
    Release not (sql change): Virtual columns are now allowed in primary
    keys.

--- pkg/sql/alter_primary_key.go
+++ pkg/sql/alter_primary_key.go
@@ -386,6 +386,25 @@ func (p *planner) AlterPrimaryKey(
 				}
 			}
 		}
+
+		//If there is any virtual column of the secondary index not a primary key
+		//column, we should rewrite the index as well because we don't allow
+		//non-primary key virtual column in secondary index's suffix columns, and
+		//this would be violated if we don't rewrite the index.
+		if !idx.Primary() {
+			newPrimaryKeyColIDs := catalog.MakeTableColSet(newPrimaryIndexDesc.KeyColumnIDs...)
+			for i := 0; i < idx.NumKeySuffixColumns(); i++ {
+				colID := idx.GetKeySuffixColumnID(i)
+				col, err := tableDesc.FindColumnWithID(colID)
+				if err != nil {
+					return false, err
+				}
+				if col.IsVirtual() && !newPrimaryKeyColIDs.Contains(colID) {
+					return true, err
+				}
+			}
+		}
+
 		return !idx.IsUnique() || idx.GetType() == descpb.IndexDescriptor_INVERTED, nil
 	}
 	var indexesToRewrite []catalog.Index
@@ -468,6 +487,10 @@ func (p *planner) AlterPrimaryKey(
 		swapArgs.LocalityConfigSwap = &alterPrimaryKeyLocalitySwap.localityConfigSwap
 	}
 	tableDesc.AddPrimaryKeySwapMutation(swapArgs)
+
+	if err := catalog.ValidateSelf(tableDesc); err != nil {
+		return err
+	}
 
 	// Mark the primary key of the table as valid.
 	{
@@ -612,4 +635,34 @@ func shouldCopyPrimaryKey(
 		desc.HasPrimaryKey() &&
 		!desc.IsPrimaryIndexDefaultRowID() &&
 		!idsAndDirsMatch(oldPK, newPK)
+}
+
+// addIndexMutationWithSpecificPrimaryKey adds an index mutation into the given
+// table descriptor, but sets up the index with KeySuffixColumnIDs from the
+// given index, rather than the table's primary key.
+func addIndexMutationWithSpecificPrimaryKey(
+	ctx context.Context,
+	table *tabledesc.Mutable,
+	toAdd *descpb.IndexDescriptor,
+	primary *descpb.IndexDescriptor,
+) error {
+	// Reset the ID so that a call to AllocateIDs will set up the index.
+	toAdd.ID = 0
+	if err := table.AddIndexMutation(toAdd, descpb.DescriptorMutation_ADD); err != nil {
+		return err
+	}
+	if err := table.AllocateIDsWithoutValidation(ctx); err != nil {
+		return err
+	}
+	// Use the columns in the given primary index to construct this indexes
+	// KeySuffixColumnIDs list.
+	presentColIDs := catalog.MakeTableColSet(toAdd.KeyColumnIDs...)
+	presentColIDs.UnionWith(catalog.MakeTableColSet(toAdd.StoreColumnIDs...))
+	toAdd.KeySuffixColumnIDs = nil
+	for _, colID := range primary.KeyColumnIDs {
+		if !presentColIDs.Contains(colID) {
+			toAdd.KeySuffixColumnIDs = append(toAdd.KeySuffixColumnIDs, colID)
+		}
+	}
+	return nil
 }
