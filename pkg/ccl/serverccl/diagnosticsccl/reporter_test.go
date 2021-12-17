3fc79bc81650beeefa2a31638527a7ff4d7d27f5
David Taylor <tinystatemachine@gmail.com>
2021-12-17 23:48:46 +0000

    server: hash tenantID into ClusterID used in SQL exec
    Prior to this patch, in a multi-tenant deployment the ClusterID value
    visible to tenans was the same as the ClusterID for the storage
    cluster.
    
    This original design was defective because it prevented the
    backup/restore subsystem from distinguishing backups made for
    different tenants.
    
    In this commit, we change the ClusterID for tenants to become a new
    UUID that is the storage cluster's ClusterID with a hash of the
    tenant's ID added to the lower 64 bits. This makes it so that two
    tenants on the same storage cluster will observe different values when
    calling ClusterID().
    
    Release note (bug fix): Separate tenants now use unique values for the
    internal field ClusterID. Previously separate tenants that
    were hosted on the same storage cluster would be assigned the same
    ClusterID.

--- pkg/ccl/serverccl/diagnosticsccl/reporter_test.go
+++ pkg/ccl/serverccl/diagnosticsccl/reporter_test.go
@@ -35,6 +35,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/system"
+	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/require"
 )
@@ -69,7 +70,10 @@ func TestTenantReport(t *testing.T) {
 	require.Equal(t, 1, rt.diagServer.NumRequests())
 
 	last := rt.diagServer.LastRequestData()
-	require.Equal(t, rt.server.ClusterID().String(), last.UUID)
+	lastUUID, err := uuid.FromString(last.UUID)
+	require.NoError(t, err)
+	require.Equal(t, rt.server.ClusterID().ToUint128().Hi, lastUUID.ToUint128().Hi,
+		"tenant cluster id should start with host cluster hi bits")
 	require.Equal(t, tenantArgs.TenantID.String(), last.TenantID)
 	require.Equal(t, "", last.NodeID)
 	require.Equal(t, tenant.SQLInstanceID().String(), last.SQLInstanceID)
