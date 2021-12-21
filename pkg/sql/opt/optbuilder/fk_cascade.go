2985351793dde11bf100c31817e2e2614351be5d
Marcus Gartner <marcus@cockroachlabs.com>
2021-12-21 18:44:19 -0500

    sql: add assignment cast for UPDATEs
    Fixes #70628
    
    Release note: None

--- pkg/sql/opt/optbuilder/fk_cascade.go
+++ pkg/sql/opt/optbuilder/fk_cascade.go
@@ -458,7 +458,7 @@ func (cb *onDeleteSetBuilder) Build(
 				updateExprs[i].Expr = tree.DefaultVal{}
 			}
 		}
-		mb.addUpdateCols(updateExprs)
+		mb.addUpdateCols(updateExprs, false /* isUpsert */)
 
 		// TODO(radu): consider plumbing a flag to prevent building the FK check
 		// against the parent we are cascading from. Need to investigate in which
@@ -687,7 +687,7 @@ func (cb *onUpdateCascadeBuilder) Build(
 				panic(errors.AssertionFailedf("unsupported action"))
 			}
 		}
-		mb.addUpdateCols(updateExprs)
+		mb.addUpdateCols(updateExprs, false /* isUpsert */)
 
 		mb.buildUpdate(nil /* returning */)
 		return mb.outScope.expr
