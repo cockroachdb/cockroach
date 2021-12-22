f87695ef749aff379a1fcee4b71b68b48169d195
Andrei Matei <andrei@cockroachlabs.com>
2021-12-21 19:32:52 -0500

    tracing: add /debug/tracez rendering the active spans
    /debug/tracez lets users take a snapshot of the active spans registry
    and render the new snapshot, or one of the previously taken snapshots.
    The Tracer can hold up to 10 snapshots in memory.
    
    The PR description has a screenshot.
    
    When visualizing a snapshot, the page lets you do a number of things:
    1. List all the spans.
    2. See the (current) stack trace for each span's goroutine (if the
       goroutine was still running at the time when the snapshot was
       captured). Stack traces can be toggled visible/hidden.
    3. Sort the spans by name or start time.
    4. Filter the span according to text search. The search works across
       the name and stack trace.
    5. See the full trace that a particular span is part of.
    
    For the table Javascript providing sorting and filtering, this patch
    embeds the library from https://listjs.com/ .
    
    Limitations:
    - for now, only the registry of the local node is snapshotted. In the
      fuiture I'll collect info from all nodes.
    - for now, the relationships between different spans are not represented
      in any way. I'll work on the ability to go from a span to the whole
      trace that the span is part of.
    - for now, tags and structured and unstructured log messages that a span
      might have are not displayed in any way.
    
    At the moment, span creation is not enabled in production by default
    (i.e. the Tracer is put in TracingModeOnDemand by default, instead of
    the required TracingModeActiveSpansRegistry). This patch does not change
    that, so in order to benefit from /debug/tracez in all its glory, one
    has to run with COCKROACH_REAL_SPANS=1 for now. Not for long, though.
    
    Release note: None

--- pkg/server/debug/server.go
+++ pkg/server/debug/server.go
@@ -32,6 +32,8 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/storage"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
+	"github.com/cockroachdb/cockroach/pkg/util/tracing"
+	"github.com/cockroachdb/cockroach/pkg/util/tracing/tracingui"
 	"github.com/cockroachdb/errors"
 	pebbletool "github.com/cockroachdb/pebble/tool"
 	metrics "github.com/rcrowley/go-metrics"
@@ -61,14 +63,18 @@ var _ = func() *settings.StringSetting {
 
 // Server serves the /debug/* family of tools.
 type Server struct {
-	st  *cluster.Settings
-	mux *http.ServeMux
-	spy logSpy
+	ambientCtx log.AmbientContext
+	st         *cluster.Settings
+	mux        *http.ServeMux
+	spy        logSpy
 }
 
 // NewServer sets up a debug server.
 func NewServer(
-	st *cluster.Settings, hbaConfDebugFn http.HandlerFunc, profiler pprofui.Profiler,
+	ambientContext log.AmbientContext,
+	st *cluster.Settings,
+	hbaConfDebugFn http.HandlerFunc,
+	profiler pprofui.Profiler,
 ) *Server {
 	mux := http.NewServeMux()
 
@@ -137,9 +143,10 @@ func NewServer(
 	})
 
 	return &Server{
-		st:  st,
-		mux: mux,
-		spy: spy,
+		ambientCtx: ambientContext,
+		st:         st,
+		mux:        mux,
+		spy:        spy,
 	}
 }
 
@@ -230,6 +237,12 @@ func (ds *Server) RegisterClosedTimestampSideTransport(
 			w.Header().Add("Content-type", "text/html")
 			fmt.Fprint(w, sender.HTML())
 		})
+}
+
+// RegisterTracez registers the /debug/tracez handler, which renders snapshots
+// of active spans.
+func (ds *Server) RegisterTracez(tr *tracing.Tracer) {
+	tracingui.RegisterHTTPHandlers(ds.ambientCtx, ds.mux, tr)
 }
 
 // ServeHTTP serves various tools under the /debug endpoint.
