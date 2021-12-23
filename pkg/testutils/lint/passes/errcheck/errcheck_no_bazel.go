ddf6f06e286d8fc49617add8034f792d9beb6c95
Ricky Stewart <ricky@cockroachlabs.com>
2021-12-23 15:50:26 -0600

    bazel: clean up stuff that doesn't build
    Now you can build all the `go_library`, `go_binary`, and `go_test`
    targets in `pkg/...` cleanly. Much of this is deleting stale library
    targets from moves (see #74261), but I also had to update some `go` code
    to build cleanly with `nogo`.
    
    Release note: None

--- pkg/testutils/lint/passes/errcheck/errcheck_no_bazel.go
+++ pkg/testutils/lint/passes/errcheck/errcheck_no_bazel.go
@@ -1,4 +1,4 @@
-// Copyright 2020 The Cockroach Authors.
+// Copyright 2022 The Cockroach Authors.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt.
@@ -12,24 +12,11 @@
 // of the errcheck Analyzer from upstream (github.com/kisielk/errcheck).
 // Specifically, we pass in our own list of excludes.
 
-//go:build bazel
-// +build bazel
+// Package errcheck is intentionally empty when not built under Bazel.
+
+//go:build !bazel
+// +build !bazel
 
 package errcheck
 
-import (
-	_ "embed"
-
-	"github.com/kisielk/errcheck/errcheck"
-)
-
-var Analyzer = errcheck.Analyzer
-
-//go:embed errcheck_excludes.txt
-var excludesContent string
-
-func init() {
-	if err := Analyzer.Flags.Set("excludes", excludesContent); err != nil {
-		panic(err)
-	}
-}
+// File intentionally empty.
