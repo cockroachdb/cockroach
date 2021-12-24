2f868a07f9481ea379dae88ebd13e22767310c73
David Taylor <tinystatemachine@gmail.com>
2021-12-24 05:26:26 +0000

    clusterversion,batcheval: remove TargetBytesAvoidExcess
    Release note: none.

--- pkg/clusterversion/key_string.go
+++ pkg/clusterversion/key_string.go
@@ -27,27 +27,26 @@ func _() {
 	_ = x[SQLStatsCompactionScheduledJob-16]
 	_ = x[V21_2-17]
 	_ = x[Start22_1-18]
-	_ = x[TargetBytesAvoidExcess-19]
-	_ = x[AvoidDrainingNames-20]
-	_ = x[DrainingNamesMigration-21]
-	_ = x[TraceIDDoesntImplyStructuredRecording-22]
-	_ = x[AlterSystemTableStatisticsAddAvgSizeCol-23]
-	_ = x[AlterSystemStmtDiagReqs-24]
-	_ = x[MVCCAddSSTable-25]
-	_ = x[InsertPublicSchemaNamespaceEntryOnRestore-26]
-	_ = x[UnsplitRangesInAsyncGCJobs-27]
-	_ = x[ValidateGrantOption-28]
-	_ = x[PebbleFormatBlockPropertyCollector-29]
-	_ = x[ProbeRequest-30]
-	_ = x[SelectRPCsTakeTracingInfoInband-31]
-	_ = x[PreSeedTenantSpanConfigs-32]
-	_ = x[SeedTenantSpanConfigs-33]
-	_ = x[PublicSchemasWithDescriptors-34]
+	_ = x[AvoidDrainingNames-19]
+	_ = x[DrainingNamesMigration-20]
+	_ = x[TraceIDDoesntImplyStructuredRecording-21]
+	_ = x[AlterSystemTableStatisticsAddAvgSizeCol-22]
+	_ = x[AlterSystemStmtDiagReqs-23]
+	_ = x[MVCCAddSSTable-24]
+	_ = x[InsertPublicSchemaNamespaceEntryOnRestore-25]
+	_ = x[UnsplitRangesInAsyncGCJobs-26]
+	_ = x[ValidateGrantOption-27]
+	_ = x[PebbleFormatBlockPropertyCollector-28]
+	_ = x[ProbeRequest-29]
+	_ = x[SelectRPCsTakeTracingInfoInband-30]
+	_ = x[PreSeedTenantSpanConfigs-31]
+	_ = x[SeedTenantSpanConfigs-32]
+	_ = x[PublicSchemasWithDescriptors-33]
 }
 
-const _Key_name = "V21_1Start21_1PLUSStart21_2RetryJobsWithExponentialBackoffAutoSpanConfigReconciliationJobDefaultPrivilegesZonesTableForSecondaryTenantsUseKeyEncodeForHashShardedIndexesDatabasePlacementPolicyGeneratedAsIdentityOnUpdateExpressionsSpanConfigurationsTableBoundedStalenessDateAndIntervalStyleTenantUsageSingleConsumptionColumnSQLStatsTablesSQLStatsCompactionScheduledJobV21_2Start22_1TargetBytesAvoidExcessAvoidDrainingNamesDrainingNamesMigrationTraceIDDoesntImplyStructuredRecordingAlterSystemTableStatisticsAddAvgSizeColAlterSystemStmtDiagReqsMVCCAddSSTableInsertPublicSchemaNamespaceEntryOnRestoreUnsplitRangesInAsyncGCJobsValidateGrantOptionPebbleFormatBlockPropertyCollectorProbeRequestSelectRPCsTakeTracingInfoInbandPreSeedTenantSpanConfigsSeedTenantSpanConfigsPublicSchemasWithDescriptors"
+const _Key_name = "V21_1Start21_1PLUSStart21_2RetryJobsWithExponentialBackoffAutoSpanConfigReconciliationJobDefaultPrivilegesZonesTableForSecondaryTenantsUseKeyEncodeForHashShardedIndexesDatabasePlacementPolicyGeneratedAsIdentityOnUpdateExpressionsSpanConfigurationsTableBoundedStalenessDateAndIntervalStyleTenantUsageSingleConsumptionColumnSQLStatsTablesSQLStatsCompactionScheduledJobV21_2Start22_1AvoidDrainingNamesDrainingNamesMigrationTraceIDDoesntImplyStructuredRecordingAlterSystemTableStatisticsAddAvgSizeColAlterSystemStmtDiagReqsMVCCAddSSTableInsertPublicSchemaNamespaceEntryOnRestoreUnsplitRangesInAsyncGCJobsValidateGrantOptionPebbleFormatBlockPropertyCollectorProbeRequestSelectRPCsTakeTracingInfoInbandPreSeedTenantSpanConfigsSeedTenantSpanConfigsPublicSchemasWithDescriptors"
 
-var _Key_index = [...]uint16{0, 5, 18, 27, 58, 89, 106, 135, 168, 191, 210, 229, 252, 268, 288, 322, 336, 366, 371, 380, 402, 420, 442, 479, 518, 541, 555, 596, 622, 641, 675, 687, 718, 742, 763, 791}
+var _Key_index = [...]uint16{0, 5, 18, 27, 58, 89, 106, 135, 168, 191, 210, 229, 252, 268, 288, 322, 336, 366, 371, 380, 398, 420, 457, 496, 519, 533, 574, 600, 619, 653, 665, 696, 720, 741, 769}
 
 func (i Key) String() string {
 	if i < 0 || i >= Key(len(_Key_index)-1) {
