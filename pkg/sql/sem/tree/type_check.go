0b8d89fc1c1ded635de9f3aa24c29b1203e2d9ad
Marcus Gartner <marcus@cockroachlabs.com>
2021-12-27 17:13:35 -0500

    sql: add intervalStyleAffected and dateStyleAffected to castMap
    This commit adds `intervalStyleAffected` and `dateStyleAffected` fields
    to `castMap` and sets them based on the behavior of the existing
    functions `isIntervalStyleCastAffected` and `isDateStyleCastAffected`.
    
    Release note: None

--- pkg/sql/sem/tree/type_check.go
+++ pkg/sql/sem/tree/type_check.go
@@ -492,10 +492,11 @@ func resolveCast(
 	default:
 		var v Volatility
 		var hint string
-		if cast, ok := lookupCast(castFrom.Oid(), castTo.Oid()); ok && cast.volatility != volatilityTODO {
+		c, ok := lookupCast(castFrom.Oid(), castTo.Oid(), intervalStyleEnabled, dateStyleEnabled)
+		if ok && c.volatility != volatilityTODO {
 			// If the volatility has been set in castMap, use it.
-			v = cast.volatility
-			hint = cast.volatilityHint
+			v = c.volatility
+			hint = c.volatilityHint
 		} else if cast := lookupCastInfo(fromFamily, toFamily, intervalStyleEnabled, dateStyleEnabled); cast != nil {
 			// Otherwise, fallback to the volatility in castInfo.
 			v = cast.volatility
