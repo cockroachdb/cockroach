db869c03c142c6f2eacbd43eb53830108f66c318
Raphael 'kena' Poss <knz@thaumogen.net>
2021-12-29 17:58:37 +0100

    sql,security: create a password hash abstraction
    Prior to this commit, password hashes loaded from storage
    were simply represented as byte arrays.
    
    The in-RAM credential cache was then simply storing these byte arrays.
    
    Keeping this simple encoding is unfortunate for the SCRAM
    authentication format, because it forces the password check code to
    re-parse the 4 parameters of the SCRAM credentials upon every
    authentication event.
    
    Instead, we want to pre-parse the parameters once, and store them
    pre-parsed in the credential cache.
    
    To achieve this, this commit introduces a new `security.PasswordHash`
    to replace `[]byte` to represent password hashes in memory.
    A stored byte array can be converted to a `security.PasswordHash`
    using the new `security.LoadPasswordHash()` function.
    
    Additionally, this commit performs the following cleanups:
    
    - it renames `security.CompareHashAndPassword` to
      `CompareHashAndCleartextPassword` to clarify the format
      the password is expected in.
    - it makes `CompareHashAndCleartextPassword()` return
      a boolean alongside the error. The function does not
      return an error any more if the hash is valid but the
      credentials don't match. Callers are now expected to
      propagate the returned error, if any.
    - it un-exports `security.ErrPasswordUserAuthFailed`, which
      was a formatting string, and replaces it by a new
      error constructor `NewErrPasswordUserAuthFailed` (this
      was meant to be used to construct errors originally).
    
    NB: this commit merely adds a new abstraction in the APIs.
    SCRAM hashes are still not used for authentication.
    
    Release note: None

--- pkg/security/password.go
+++ pkg/security/password.go
@@ -14,17 +14,21 @@ import (
 	"bytes"
 	"context"
 	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 	"regexp"
 	"runtime"
 	"strconv"
 	"sync"
+	"unsafe"
 
 	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/util/envutil"
+	"github.com/cockroachdb/cockroach/pkg/util/errorutil/unimplemented"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/quotapool"
 	"github.com/cockroachdb/errors"
+	"github.com/xdg-go/scram"
 	// Reserved import for PR #74301.
 	_ "github.com/xdg-go/stringprep"
 	"golang.org/x/crypto/bcrypt"
@@ -69,6 +73,139 @@ var ErrEmptyPassword = errors.New("empty passwords are not permitted")
 // that was too short according to policy.
 var ErrPasswordTooShort = errors.New("password too short")
 
+// ErrUnknownHashMethod is returned by LoadPasswordHash if the hash encoding
+// method is not supported.
+var ErrUnknownHashMethod = errors.New("unknown hash method")
+
+// HashMethod indicates which password hash method to use.
+type HashMethod int8
+
+const (
+	// HashInvalidMethod represents invalid hashes.
+	// This always fails authentication.
+	HashInvalidMethod HashMethod = iota
+	// HashMissingPassword represents a virtual hash when there was
+	// no password.  This too always fails authentication.
+	// We need a different method here than HashInvalidMethod because
+	// the authentication code distinguishes the two cases when reporting
+	// why authentication fails in audit logs.
+	HashMissingPassword
+	// HashBCrypt indicates CockroachDB's bespoke bcrypt-based method.
+	HashBCrypt
+	// HashSCRAMSHA256 indicates SCRAM-SHA-256.
+	HashSCRAMSHA256
+)
+
+// PasswordHash represents the type of a password hash loaded from a credential store.
+type PasswordHash interface {
+	fmt.Stringer
+	// Method report which hashing method was used.
+	Method() HashMethod
+	// Size is the size of the in-memory representation of this hash. This
+	// is used for memory accounting.
+	Size() int
+	// compareWithCleartextPassword checks a cleartext password against
+	// the hash.
+	compareWithCleartextPassword(ctx context.Context, cleartext string) (ok bool, err error)
+}
+
+var _ PasswordHash = emptyPassword{}
+var _ PasswordHash = invalidHash(nil)
+var _ PasswordHash = bcryptHash(nil)
+var _ PasswordHash = (*scramHash)(nil)
+
+// emptyPassword represents a virtual hash when there was no password
+// to start with.
+type emptyPassword struct{}
+
+// String implements fmt.Stringer.
+func (e emptyPassword) String() string { return "<missing>" }
+
+// Method is part of the PasswordHash interface.
+func (e emptyPassword) Method() HashMethod { return HashMissingPassword }
+
+// Size is part of the PasswordHash interface.
+func (e emptyPassword) Size() int { return 0 }
+
+// compareWithCleartextPassword is part of the PasswordHash interface.
+func (e emptyPassword) compareWithCleartextPassword(
+	ctx context.Context, cleartext string,
+) (ok bool, err error) {
+	return false, nil
+}
+
+// MissingPasswordHash represents the virtual hash when there is no password
+// to start with.
+var MissingPasswordHash PasswordHash = emptyPassword{}
+
+// invalidHash represents a byte slice that's in an unknown hash format.
+// We keep the byte slice around so that it can be passed through
+// and re-stored as-is.
+type invalidHash []byte
+
+// String implements fmt.Stringer.
+func (n invalidHash) String() string { return string(n) }
+
+// Method is part of the PasswordHash interface.
+func (n invalidHash) Method() HashMethod { return HashInvalidMethod }
+
+// Size is part of the PasswordHash interface.
+func (n invalidHash) Size() int { return len(n) }
+
+// compareWithCleartextPassword is part of the PasswordHash interface.
+func (n invalidHash) compareWithCleartextPassword(
+	ctx context.Context, cleartext string,
+) (ok bool, err error) {
+	return false, nil
+}
+
+// bcryptHash represents a bcrypt-based hashed password.
+// The type is simple since we're offloading the decoding
+// of the parameters to the go standard bcrypt package.
+type bcryptHash []byte
+
+// String implements fmt.Stringer.
+func (b bcryptHash) String() string { return string(b) }
+
+// Method is part of the PasswordHash interface.
+func (b bcryptHash) Method() HashMethod { return HashBCrypt }
+
+// Size is part of the PasswordHash interface.
+func (b bcryptHash) Size() int { return len(b) }
+
+// scramHash represents a SCRAM-SHA-256 password hash.
+type scramHash struct {
+	bytes   []byte
+	decoded scram.StoredCredentials
+}
+
+// String implements fmt.Stringer.
+func (s *scramHash) String() string { return string(s.bytes) }
+
+// Method is part of the PasswordHash interface.
+func (s *scramHash) Method() HashMethod { return HashSCRAMSHA256 }
+
+// Size is part of the PasswordHash interface.
+func (s *scramHash) Size() int {
+	return int(unsafe.Sizeof(*s)) + len(s.bytes) + len(s.decoded.Salt) + len(s.decoded.StoredKey) + len(s.decoded.ServerKey)
+}
+
+// LoadPasswordHash decodes a password hash loaded as bytes from a credential store.
+func LoadPasswordHash(ctx context.Context, storedHash []byte) (res PasswordHash) {
+	res = invalidHash(storedHash)
+	if len(storedHash) == 0 {
+		return emptyPassword{}
+	}
+	if isBcryptHash(storedHash, false /* strict */) {
+		return bcryptHash(storedHash)
+	}
+	if ok, parts := isSCRAMHash(storedHash); ok {
+		return makeSCRAMHash(storedHash, parts, res)
+	}
+	// Fallthrough: keep the hash, but mark the method as unknown.
+	return res
+}
+
 var sha256NewSum = sha256.New().Sum(nil)
 
 // TODO(mjibson): properly apply SHA-256 to the password. The current code
@@ -86,17 +223,41 @@ func appendEmptySha256(password string) []byte {
 	return append([]byte(password), sha256NewSum...)
 }
 
-// CompareHashAndPassword tests that the provided bytes are equivalent to the
-// hash of the supplied password. If they are not equivalent, returns an
-// error.
-func CompareHashAndPassword(ctx context.Context, hashedPassword []byte, password string) error {
+// CompareHashAndCleartextPassword tests that the provided bytes are equivalent to the
+// hash of the supplied password. If the hash is valid but the password does not match,
+// no error is returned but the ok boolean is false.
+// If an error was detected while using the hash, an error is returned.
+func CompareHashAndCleartextPassword(
+	ctx context.Context, hashedPassword PasswordHash, password string,
+) (ok bool, err error) {
+	return hashedPassword.compareWithCleartextPassword(ctx, password)
+}
+
+// compareWithCleartextPassword is part of the PasswordHash interface.
+func (b bcryptHash) compareWithCleartextPassword(
+	ctx context.Context, cleartext string,
+) (ok bool, err error) {
 	sem := getBcryptSem(ctx)
 	alloc, err := sem.Acquire(ctx, 1)
 	if err != nil {
-		return err
+		return false, err
 	}
 	defer alloc.Release()
-	return bcrypt.CompareHashAndPassword(hashedPassword, appendEmptySha256(password))
+	err = bcrypt.CompareHashAndPassword([]byte(b), appendEmptySha256(cleartext))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+// compareWithCleartextPassword is part of the PasswordHash interface.
+func (s *scramHash) compareWithCleartextPassword(
+	ctx context.Context, cleartext string,
+) (ok bool, err error) {
+	return false, unimplemented.NewWithIssue(42519, "cleartext comparison for SCRAM not supported yet")
 }
 
 // HashPassword takes a raw password and returns a bcrypt hashed password.
@@ -125,14 +286,25 @@ const crdbBcryptPrefix = "CRDB-BCRYPT"
 // format supported by CockroachDB. The base64 encoding of the hash
 // uses the alphabet used by the bcrypt package:
 // "./ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-var bcryptHashRe = regexp.MustCompile(`^` + crdbBcryptPrefix + `\$\d[a-z]?\$\d\d\$[0-9A-Za-z\./]{22}[0-9A-Za-z\./]+$`)
+var bcryptHashRe = regexp.MustCompile(`^(` + crdbBcryptPrefix + `)?\$\d[a-z]?\$\d\d\$[0-9A-Za-z\./]{22}[0-9A-Za-z\./]+$`)
 
-func isBcryptHash(inputPassword []byte) bool {
-	return bcryptHashRe.Match(inputPassword)
+// isBcryptHash determines whether hashedPassword is in the CockroachDB bcrypt format.
+// If the script parameter is true, then the special "CRDB-BCRYPT" prefix is required.
+// This is used e.g. when accepting password hashes in the SQL ALTER USER statement.
+// When loading a hash from storage, typically we do not enforce this so as to
+// support password hashes stored in earlier versions of CockroachDB.
+func isBcryptHash(inputPassword []byte, strict bool) bool {
+	if !bcryptHashRe.Match(inputPassword) {
+		return false
+	}
+	if strict && !bytes.HasPrefix(inputPassword, []byte(crdbBcryptPrefix+`$`)) {
+		return false
+	}
+	return true
 }
 
 func checkBcryptHash(inputPassword []byte) (ok bool, hashedPassword []byte, err error) {
-	if !isBcryptHash(inputPassword) {
+	if !isBcryptHash(inputPassword, true /* strict */) {
 		return false, nil, nil
 	}
 	// Trim the "CRDB-BCRYPT" prefix. We trim this because previous version
@@ -174,6 +346,36 @@ func checkSCRAMHash(inputPassword []byte) (ok bool, hashedPassword []byte, err e
 	return true, inputPassword, nil
 }
 
+func makeSCRAMHash(storedHash []byte, parts [][]byte, invalidHash PasswordHash) PasswordHash {
+	iters, err := strconv.Atoi(string(parts[1]))
+	if err != nil {
+		return invalidHash //nolint:returnerrcheck
+	}
+	salt, err := base64.StdEncoding.DecodeString(string(parts[2]))
+	if err != nil {
+		return invalidHash //nolint:returnerrcheck
+	}
+	storedKey, err := base64.StdEncoding.DecodeString(string(parts[3]))
+	if err != nil {
+		return invalidHash //nolint:returnerrcheck
+	}
+	serverKey, err := base64.StdEncoding.DecodeString(string(parts[4]))
+	if err != nil {
+		return invalidHash //nolint:returnerrcheck
+	}
+	return &scramHash{
+		bytes: storedHash,
+		decoded: scram.StoredCredentials{
+			KeyFactors: scram.KeyFactors{
+				Salt:  string(salt),
+				Iters: iters,
+			},
+			StoredKey: storedKey,
+			ServerKey: serverKey,
+		},
+	}
+}
+
 func isMD5Hash(hashedPassword []byte) bool {
 	// This logic is inspired from PostgreSQL's get_password_type() function.
 	return bytes.HasPrefix(hashedPassword, []byte("md5")) &&
@@ -206,7 +408,6 @@ func CheckPasswordHashValidity(
 		return true, true, 0, "crdb-bcrypt", hashedPassword, err
 	}
 	if ok, hashedPassword, err := checkSCRAMHash(inputPassword); ok {
-
 		return true, true, 0, "scram-sha-256", hashedPassword, err
 	}
 	if isMD5Hash(inputPassword) {
