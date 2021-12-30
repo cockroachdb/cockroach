c248b665c6c2bc7eed4014fa7cad7c55bd2de70e
Oleg Afanasyev <oleg@cockroachlabs.com>
2021-12-30 19:09:54 +0000

    kvserver/loqrecovery: write replica recovery events to rangelog
    Previously a fact of loss of quorum replica recovery was only written
    as a structured log entry. This information is local to the node and
    does not survive if node is decommissioned. It would be beneficial
    to preserve this information longer. Range log while being limited
    to 30 days still provide a good reference data for investigations if
    recovery wasn't performed too long ago.
    This patch adds entries to rangelog for every updated range first time
    node that holds survivor replica is started after recovery.
    
    Release note: None

---
+++ pkg/kv/kvserver/loqrecovery/record_test.go
@@ -0,0 +1,114 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package loqrecovery
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvserverpb"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/loqrecovery/loqrecoverypb"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/stretchr/testify/require"
+)
+
+// TestPublishRangeLogEvents verifies that inserting recovery events into
+// RangeLog handles sql execution errors and unexpected results by propagating
+// errors up. This is important as caller relies on errors to preserve events if
+// they were not reflected in RangeLog.
+// It also performs basic sanity check that inserted records have correct range
+// id and reason for update and a timestamp.
+func TestPublishRangeLogEvents(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	ctx := context.Background()
+
+	for _, td := range []struct {
+		name string
+
+		// Recovery event and function under test arguments.
+		rangeID        roachpb.RangeID
+		time           int64
+		useLegacyEvent bool
+
+		// Callback results returned to function under test.
+		returnedRowCount int
+		queryExecError   error
+
+		// Expectations in callback and call results.
+		expectSuccess bool
+		expectReason  kvserverpb.RangeLogEventType
+	}{
+		{
+			name:             "success",
+			rangeID:          7,
+			time:             1021,
+			returnedRowCount: 1,
+			expectSuccess:    true,
+			expectReason:     kvserverpb.RangeLogEventType_unsafe_quorum_recovery,
+		},
+		{
+			name:             "sql error",
+			rangeID:          7,
+			time:             1021,
+			returnedRowCount: 1,
+			queryExecError:   errors.New("stray sql error occurred"),
+			expectReason:     kvserverpb.RangeLogEventType_unsafe_quorum_recovery,
+		},
+		{
+			name:             "wrong row count",
+			rangeID:          7,
+			time:             1021,
+			returnedRowCount: 0,
+			expectSuccess:    false,
+			expectReason:     kvserverpb.RangeLogEventType_unsafe_quorum_recovery,
+		},
+		{
+			name:             "upgrading cluster",
+			rangeID:          7,
+			time:             1021,
+			useLegacyEvent:   true,
+			returnedRowCount: 1,
+			expectSuccess:    true,
+			expectReason:     kvserverpb.RangeLogEventType_add_voter,
+		},
+	} {
+		t.Run(td.name, func(t *testing.T) {
+			var actualArgs []interface{}
+			execFn := func(ctx context.Context, stmt string, args ...interface{}) (int, error) {
+				actualArgs = args
+				return td.returnedRowCount, td.queryExecError
+			}
+
+			event := loqrecoverypb.ReplicaRecoveryRecord{
+				Timestamp: td.time,
+				RangeID:   td.rangeID,
+				StartKey:  loqrecoverypb.RecoveryKey(roachpb.RKeyMin),
+				EndKey:    loqrecoverypb.RecoveryKey(roachpb.RKeyMax),
+			}
+
+			err := UpdateRangeLogWithRecovery(ctx, execFn, event, td.useLegacyEvent)
+			if td.expectSuccess {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+			}
+			require.Equal(t, 6, len(actualArgs), "not enough query args were provided")
+			require.Contains(t, actualArgs[5], "Performed unsafe range loss of quorum recovery")
+			require.Equal(t, td.rangeID, actualArgs[1], "RangeID query arg doesn't match event")
+			require.Equal(t, timeutil.Unix(0, td.time), actualArgs[0],
+				"timestamp query arg doesn't match event")
+			require.Equal(t, td.expectReason.String(), actualArgs[3], "incorrect RangeLog event type")
+		})
+	}
+}
