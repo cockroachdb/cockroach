cockroachdb/cockroach

commit a8ae1bf96c5900aecee4471ae89f35b8bbc6b6af

*: use errors.HasType / HasInterface / As instead of casts
Release note: None

pkg/sql/pgwire/conn.go
@@ -1562,7 +1562,7 @@ func (c *readTimeoutConn) Read(b []byte) (int, error) {
 		}
 		n, err := c.Conn.Read(b)
 		// Continue if the error is due to timing out.
-		if err, ok := err.(net.Error); ok && err.Timeout() {
+		if ne := (net.Error)(nil); errors.As(err, &ne) && ne.Timeout() {
 			continue
 		}
 		return n, err

commit 840ac219129d1dd1ee9b8f6b50f485369313802e

sql: avoid net.Error allocation on each readTimeoutConn.Read call
This commit updates `readTimeoutConn.Read` to only call `errors.As` if the error
from the Read of the wrapped `net.Conn` was non-nil. This avoids a heap
allocation of a `net.Error` on each call to `readTimeoutConn.Read`, which was
originally introduced in a8ae1bf.

```
name                   old time/op    new time/op    delta
KV/Scan/SQL/rows=1-10    93.9µs ± 6%    94.6µs ± 7%    ~     (p=0.631 n=10+10)

name                   old alloc/op   new alloc/op   delta
KV/Scan/SQL/rows=1-10    20.1kB ± 0%    20.1kB ± 0%    ~     (p=0.197 n=10+10)

name                   old allocs/op  new allocs/op  delta
KV/Scan/SQL/rows=1-10       245 ± 0%       244 ± 0%  -0.41%  (p=0.000 n=10+10)
```

pkg/sql/pgwire/conn.go
@@ -1759,9 +1759,11 @@ func (c *readTimeoutConn) Read(b []byte) (int, error) {
 			return 0, err
 		}
 		n, err := c.Conn.Read(b)
-		// Continue if the error is due to timing out.
-		if ne := (net.Error)(nil); errors.As(err, &ne) && ne.Timeout() {
-			continue
+		if err != nil {
+			// Continue if the error is due to timing out.
+			if ne := (net.Error)(nil); errors.As(err, &ne) && ne.Timeout() {
+				continue
+			}
 		}
 		return n, err
 	}