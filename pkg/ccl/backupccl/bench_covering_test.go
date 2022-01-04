8cd5d4c76990c82eefa5857c287f99d70f29713d
David Taylor <tinystatemachine@gmail.com>
2022-01-04 04:57:17 +0000

    backupccl: add benchmark of coverage checkers
    Results show the effects of increasing the number of backups, spans, and files.
    
    ```
    name                                                                        time/op
    CoverageChecks/numBackups=24/numSpans=20/numFiles=0/checkCoverage-10         931µs ± 3%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=0/makeImportSpans-10       967µs ± 1%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=10/checkCoverage-10        939µs ± 6%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=10/makeImportSpans-10     2.11ms ± 4%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=100/checkCoverage-10       921µs ± 2%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=100/makeImportSpans-10    11.3ms ± 2%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=1000/checkCoverage-10      926µs ± 1%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=1000/makeImportSpans-10    186ms ± 2%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=10000/checkCoverage-10     890µs ± 1%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=10000/makeImportSpans-10   2.58s ± 0%
    
    name                                                                        alloc/op
    CoverageChecks/numBackups=24/numSpans=20/numFiles=0/checkCoverage-10         415kB ± 0%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=0/makeImportSpans-10       385kB ± 0%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=10/checkCoverage-10        415kB ± 0%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=10/makeImportSpans-10      619kB ± 0%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=100/checkCoverage-10       415kB ± 0%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=100/makeImportSpans-10    2.16MB ± 0%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=1000/checkCoverage-10      415kB ± 0%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=1000/makeImportSpans-10   14.2MB ± 0%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=10000/checkCoverage-10     415kB ± 0%
    CoverageChecks/numBackups=24/numSpans=20/numFiles=10000/makeImportSpans-10   146MB ± 0%
    ```
    
    Treating `checkCoverage` as new and `makeImportSpans` as old:
    ```
    name                                                        old time/op    new time/op    delta
    CoverageChecks/numBackups=24/numSpans=20/numFiles=0-10         967µs ± 1%     931µs ± 3%    -3.71%  (p=0.000 n=10+10)
    CoverageChecks/numBackups=24/numSpans=20/numFiles=10-10       2.11ms ± 4%    0.94ms ± 6%   -55.46%  (p=0.000 n=10+10)
    CoverageChecks/numBackups=24/numSpans=20/numFiles=100-10      11.3ms ± 2%     0.9ms ± 2%   -91.88%  (p=0.000 n=10+10)
    CoverageChecks/numBackups=24/numSpans=20/numFiles=1000-10      186ms ± 2%       1ms ± 1%   -99.50%  (p=0.000 n=9+9)
    CoverageChecks/numBackups=24/numSpans=20/numFiles=10000-10     2.58s ± 0%     0.00s ± 1%   -99.97%  (p=0.009 n=3+9)
    
    name                                                        old alloc/op   new alloc/op   delta
    CoverageChecks/numBackups=24/numSpans=20/numFiles=0-10         385kB ± 0%     415kB ± 0%    +7.78%  (p=0.000 n=10+10)
    CoverageChecks/numBackups=24/numSpans=20/numFiles=10-10        619kB ± 0%     415kB ± 0%   -32.96%  (p=0.000 n=9+10)
    CoverageChecks/numBackups=24/numSpans=20/numFiles=100-10      2.16MB ± 0%    0.41MB ± 0%   -80.80%  (p=0.000 n=10+9)
    CoverageChecks/numBackups=24/numSpans=20/numFiles=1000-10     14.2MB ± 0%     0.4MB ± 0%   -97.09%  (p=0.000 n=10+10)
    CoverageChecks/numBackups=24/numSpans=20/numFiles=10000-10     146MB ± 0%       0MB ± 0%   -99.72%  (p=0.000 n=3+10)
    ```
    
    Release note: none.

---
+++ pkg/ccl/backupccl/bench_covering_test.go
@@ -0,0 +1,95 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package backupccl
+
+import (
+	"context"
+	fmt "fmt"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/util/encoding"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+)
+
+func BenchmarkCoverageChecks(b *testing.B) {
+	for _, numBackups := range []int{1, 7, 24, 24 * 4} {
+		b.Run(fmt.Sprintf("numBackups=%d", numBackups), func(b *testing.B) {
+			for _, numSpans := range []int{10, 20, 100} {
+				b.Run(fmt.Sprintf("numSpans=%d", numSpans), func(b *testing.B) {
+					for _, baseFiles := range []int{0, 10, 100, 1000, 10000} {
+						b.Run(fmt.Sprintf("numFiles=%d", baseFiles), func(b *testing.B) {
+							b.StopTimer()
+							backups := make([]BackupManifest, numBackups)
+							ts := hlc.Timestamp{WallTime: time.Second.Nanoseconds()}
+							for i := range backups {
+								backups[i].Spans = make(roachpb.Spans, numSpans)
+								for j := range backups[i].Spans {
+									backups[i].Spans[j] = makeTableSpan(uint32(100 + j + (i / 4)))
+								}
+								backups[i].EndTime = ts.Add(time.Minute.Nanoseconds()*int64(i), 0)
+								if i > 0 {
+									backups[i].StartTime = backups[i-1].EndTime
+									if i%4 == 0 {
+										backups[i].IntroducedSpans = roachpb.Spans{backups[i].Spans[numSpans-1]}
+									}
+								}
+
+								numFiles := baseFiles
+								if i == 0 {
+									backups[i].Files = make([]BackupManifest_File, numFiles)
+								} else {
+									numFiles = numFiles / 2
+									backups[i].Files = make([]BackupManifest_File, numFiles)
+								}
+
+								for f := range backups[i].Files {
+									backups[i].Files[f].Path = fmt.Sprintf("1234567890%d.sst", f)
+									backups[i].Files[f].Span.Key = encoding.EncodeVarintAscending(backups[i].Spans[f*numSpans/numFiles].Key, int64(f))
+									backups[i].Files[f].Span.EndKey = encoding.EncodeVarintAscending(backups[i].Spans[f*numSpans/numFiles].Key, int64(f+1))
+
+								}
+								backups[i].Dir = roachpb.ExternalStorage{S3Config: &roachpb.ExternalStorage_S3{
+									Bucket:    "some-string-name",
+									Prefix:    "some-string-path/to/some/file",
+									AccessKey: "some-access-key",
+									Secret:    "some-secret-key",
+								}}
+							}
+							b.ResetTimer()
+
+							ctx := context.Background()
+							b.Run("checkCoverage", func(b *testing.B) {
+								b.ResetTimer()
+								for i := 0; i < b.N; i++ {
+									if err := checkCoverage(ctx, backups[numBackups-1].Spans, backups); err != nil {
+										b.Fatal(err)
+									}
+								}
+							})
+							b.Run("makeImportSpans", func(b *testing.B) {
+								b.ResetTimer()
+								for i := 0; i < b.N; i++ {
+									_, ts, err := makeImportSpans(backups[numBackups-1].Spans, backups, nil, nil, errOnMissingRange)
+									if err != nil {
+										b.Fatal(err)
+									}
+									if got, expected := ts, backups[len(backups)-1].EndTime; !got.Equal(expected) {
+										b.Fatal(expected, got)
+									}
+								}
+							})
+						})
+					}
+				})
+			}
+		})
+	}
+}
