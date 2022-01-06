7f1a81471e7838e3d0083d9ae0b846362f3dacf3
Ricky Stewart <ricky@cockroachlabs.com>
2022-01-06 17:37:02 -0600

    ci: bring bazel ci up to parity with checks performed in old ci
    Up until this point, checking the release justification and whether all
    generated files were up to date was done only in GitHub CI rather than
    Bazel CI. We just copy those same checks into Bazel CI as well.
    
    Closes #68385.
    
    Release note: None

--- build/teamcity-check-genfiles.sh
+++ build/teamcity-check-genfiles.sh
@@ -5,22 +5,14 @@ set -euo pipefail
 source "$(dirname "${0}")/teamcity-support.sh"
 source "$(dirname "${0}")/teamcity-bazel-support.sh"  # For run_bazel
 
-function check_clean() {
-  # The workspace is clean iff `git status --porcelain` produces no output. Any
-  # output is either an error message or a listing of an untracked/dirty file.
-  if [[ "$(git status --porcelain 2>&1)" != "" ]]; then
-    git status >&2 || true
-    git diff -a >&2 || true
-    echo "====================================================" >&2
-    echo "Some automatically generated code is not up to date." >&2
-    echo $1 >&2
-    exit 1
-  fi
-}
-
 tc_prepare
 
 tc_start_block "Ensure generated code is up-to-date"
+
+# NOTE(ricky): Please make sure any changes to the Bazel-related checks here are
+# propagated to build/teamcity/cockroach/ci/tests/check_generated_code_impl.sh
+# as well.
+
 # Buffer noisy output and only print it on failure.
 if ! (run run_bazel build/bazelutil/check.sh &> artifacts/buildshort.log || (cat artifacts/buildshort.log && false)); then
     # The command will output instructions on how to fix the error.
@@ -37,23 +29,23 @@ if grep TODO DEPS.bzl; then
     echo "Missing TODO comment in DEPS.bzl. Did you run \`./dev generate bazel --mirror\`?"
     exit 1
 fi
-check_clean "Run \`./dev generate bazel\` to automatically regenerate these."
+check_workspace_clean "Run \`./dev generate bazel\` to automatically regenerate these."
 run build/builder.sh make generate &> artifacts/generate.log || (cat artifacts/generate.log && false)
 rm artifacts/generate.log
-check_clean "Run \`make generate\` to automatically regenerate these."
+check_workspace_clean "Run \`make generate\` to automatically regenerate these."
 run build/builder.sh make buildshort &> artifacts/buildshort.log || (cat artifacts/buildshort.log && false)
 rm artifacts/buildshort.log
-check_clean "Run \`make buildshort\` to automatically regenerate these."
+check_workspace_clean "Run \`make buildshort\` to automatically regenerate these."
 tc_end_block "Ensure generated code is up-to-date"
 
 # generated code can generate new dependencies; check dependencies after generated code.
 tc_start_block "Ensure dependencies are up-to-date"
 # Run go mod tidy and `make -k vendor_rebuild` and ensure nothing changes.
 run build/builder.sh go mod tidy
-check_clean "Run \`go mod tidy\` and \`make -k vendor_rebuild\` to automatically regenerate these."
+check_workspace_clean "Run \`go mod tidy\` and \`make -k vendor_rebuild\` to automatically regenerate these."
 run build/builder.sh make -k vendor_rebuild
 cd vendor
-check_clean "Run \`make -k vendor_rebuild\` to automatically regenerate these."
+check_workspace_clean "Run \`make -k vendor_rebuild\` to automatically regenerate these."
 cd ..
 tc_end_block "Ensure dependencies are up-to-date"
 
