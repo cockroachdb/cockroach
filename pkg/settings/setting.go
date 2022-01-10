b08695e9160a479e265eee5e7631df76107484eb
Radu Berinde <radu@cockroachlabs.com>
2022-01-10 10:25:31 -0800

    setting: prevent use of SystemOnly settings from tenant code
    This change implements the `system-only` semantics in the RFC
    (#73349).
    
    All SystemOnly setting values are now invisible from tenant code. If
    tenant code attempts to get or set a SystemOnly setting by handle, it
    results in panics in test builds; in non-test builds, these settings
    always report the default value.
    
    Release note (sql change): System-only cluster settings are no longer
    visible for non-system tenants.

--- pkg/settings/setting.go
+++ pkg/settings/setting.go
@@ -22,6 +22,12 @@ import (
 // multiple "instances" (values) for each setting (e.g. for multiple test
 // servers in the same process).
 type Setting interface {
+	// Class returns the scope of the setting in multi-tenant scenarios.
+	Class() Class
+
+	// Key returns the name of the specific cluster setting.
+	Key() string
+
 	// Typ returns the short (1 char) string denoting the type of setting.
 	Typ() string
 
@@ -29,9 +35,6 @@ type Setting interface {
 	// It's used when materializing results for `SHOW CLUSTER SETTINGS` or `SHOW
 	// CLUSTER SETTING <setting-name>`.
 	String(sv *Values) string
-
-	// Key returns the name of the specific cluster setting.
-	Key() string
 
 	// Description contains a helpful text explaining what the specific cluster
 	// setting is for.
@@ -41,9 +44,6 @@ type Setting interface {
 	// Reserved settings are still accessible to users, but they don't get listed
 	// out when retrieving all settings.
 	Visibility() Visibility
-
-	// Class returns the scope of the setting in multi-tenant scenarios.
-	Class() Class
 }
 
 // NonMaskedSetting is the exported interface of non-masked settings.
