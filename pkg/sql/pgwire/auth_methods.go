0d4ff5764ffee1c03671348d606bd670d7ed396e
Raphael 'kena' Poss <knz@thaumogen.net>
2022-01-10 22:21:17 +0100

    pgwire: auto-upgrade password authentication with SCRAM hashes
    Release note (security update): When using the HBA authentication
    method `cert-password` (the default) for SQL client connections, and
    the SQL client does not present a TLS client certificate to the
    server, CockroachDB now automatically upgrades the password handshake
    protocol to use SCRAM-SHA-256 if the user's stored password uses the
    SCRAM encoding.
    
    The previous behavior of requesting a cleartext password is still used
    if the stored password is encoded using the CRDB-BCRYPT format.
    
    An operator can force to _always_ request SCRAM-SHA-256 when a TLS
    client cert is not provided (so as to guarantee the various
    security benefits of SCRAM) using the authentication methods
    `cert-scram-sha-256` (either TLS client cert _or_ SCRAM-SHA-256); and
    `scram-sha-256` (only SCRAM-SHA-256).
    
    (As previously, mandatory cleartext password authentication
    can be requested, e.g. for debugging purposes, by using the HBA
    method `password`.)
    
    This automatic protocol upgrade can be manually disabled
    using the new cluster setting
    `server.user_login.cert_password_method.auto_scram_promotion.enable` (by
    setting it to `false`), for example if certain client drivers
    are found to not support SCRAM-SHA-256 authentication properly.
    
    Release note (sql change): When possible, CockroachDB will now
    automatically require the PostgreSQL-compatible SCRAM-SHA-256 protocol
    when performing password validation when SQL client log in. (Reminder:
    this mechanism is not used when SQL clients use TLS client certs,
    which is the recommended approach.)
    
    This assumes support for SCRAM-SHA-256 in client drivers.
    As of 2020, we have found this to be prevalent in the pg driver
    ecosystem. However, users should be mindful of the following possible
    behavior changes:
    
    - an application that tries to detect whether password verification
      has failed by checking server error messages, might observe
      different error messages with SCRAM-SHA-256. Those checks, if
      present, need to be updated.
    
    - if a client driver simply does not support SCRAM-SHA-256 at all,
      the operator retains the option to set the cluster setting
      `server.user_login.cert_password_method.auto_scram_promotion.enable`
      to `false` to force the previous password verification method instead.

--- pkg/sql/pgwire/auth_methods.go
+++ pkg/sql/pgwire/auth_methods.go
@@ -19,6 +19,7 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/clusterversion"
 	"github.com/cockroachdb/cockroach/pkg/security"
+	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/hba"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/identmap"
@@ -106,8 +107,6 @@ var _ AuthMethod = authReject
 // authPassword is the AuthMethod constructor for HBA method
 // "password": authenticate using a cleartext password received from
 // the client.
-// It is also the fallback constructor for HBA method "cert-password",
-// when the SQL client does not provide a TLS client certificate.
 func authPassword(
 	_ context.Context,
 	c AuthConn,
@@ -421,9 +420,13 @@ func authCert(
 
 // authCertPassword is the AuthMethod constructor for HBA method
 // "cert-password": authenticate EITHER using a TLS client cert OR a
-// valid cleartext password (if transmitted over TLS, the password is
-// encrypted by the TLS transport).
+// password exchange.
+//
 // TLS client cert authn is used iff the client presents a TLS client cert.
+// Otherwise, the password authentication protocol is chosen
+// depending on the format of the stored credentials: SCRAM is preferred
+// if possible, otherwise fallback to cleartext.
+// See the documentation for authAutoSelectPasswordProtocol() below.
 func authCertPassword(
 	ctx context.Context,
 	c AuthConn,
@@ -434,13 +437,73 @@ func authCertPassword(
 ) (*AuthBehaviors, error) {
 	var fn AuthMethod
 	if len(tlsState.PeerCertificates) == 0 {
-		c.LogAuthInfof(ctx, "no client certificate, proceeding with password authentication")
-		fn = authPassword
+		if AutoSelectPasswordAuth.Get(&execCfg.Settings.SV) {
+			// We don't call c.LogAuthInfo here; this is done in
+			// authAutoSelectPasswordProtocol() below.
+			fn = authAutoSelectPasswordProtocol
+		} else {
+			c.LogAuthInfof(ctx, "no client certificate, proceeding with password authentication")
+			fn = authPassword
+		}
 	} else {
 		c.LogAuthInfof(ctx, "client presented certificate, proceeding with certificate validation")
 		fn = authCert
 	}
 	return fn(ctx, c, tlsState, execCfg, entry, identMap)
+}
+
+// AutoSelectPasswordAuth determines whether CockroachDB automatically promotes the password
+// protocol when a SCRAM hash is detected in the stored credentials.
+//
+// It is exported for use in tests.
+var AutoSelectPasswordAuth = settings.RegisterBoolSetting(
+	settings.TenantWritable,
+	"server.user_login.cert_password_method.auto_scram_promotion.enabled",
+	"whether to automatically promote cert-password authentication to use SCRAM",
+	true,
+).WithPublic()
+
+// authAutoSelectPasswordProtocol is the AuthMethod constructor used
+// for HBA method "cert-password" when the SQL client does not provide
+// a TLS client certificate.
+//
+// It uses the effective format of the stored hash password to decide
+// the hash protocol: if the stored hash uses the SCRAM encoding,
+// SCRAM-SHA-256 is used (which is a safer handshake); otherwise,
+// cleartext password authentication is used.
+func authAutoSelectPasswordProtocol(
+	_ context.Context,
+	c AuthConn,
+	_ tls.ConnectionState,
+	execCfg *sql.ExecutorConfig,
+	_ *hba.Entry,
+	_ *identmap.Conf,
+) (*AuthBehaviors, error) {
+	b := &AuthBehaviors{}
+	b.SetRoleMapper(UseProvidedIdentity)
+	b.SetAuthenticator(func(
+		ctx context.Context,
+		systemIdentity security.SQLUsername,
+		clientConnection bool,
+		pwRetrieveFn PasswordRetrievalFn,
+	) error {
+		// Request information about the password hash.
+		expired, hashedPassword, err := pwRetrieveFn(ctx)
+		// The authenticator function needs a closure to retrieve the same information.
+		// Memoize it.
+		newpwfn := func(ctx context.Context) (bool, security.PasswordHash, error) { return expired, hashedPassword, err }
+
+		// Was the password using the SCRAM hash encoding?
+		if err == nil && hashedPassword.Method() == security.HashSCRAMSHA256 {
+			// Yes: use a SCRAM-SHA-256 exchange.
+			c.LogAuthInfof(ctx, "no client cert but SCRAM credentials found, proceeding with SCRAM-SHA-256")
+			return scramAuthenticator(ctx, systemIdentity, clientConnection, newpwfn, c, execCfg)
+		}
+		// No: use cleartext password authentication.
+		c.LogAuthInfof(ctx, "no client cert nor SCRAM credentials; requesting cleartext password")
+		return passwordAuthenticator(ctx, systemIdentity, clientConnection, newpwfn, c)
+	})
+	return b, nil
 }
 
 // authCertPassword is the AuthMethod constructor for HBA method
