873b84253002a9ba0e5bcc89ae621d1803c715de
Ricky Stewart <ricky@cockroachlabs.com>
2022-01-14 13:59:50 -0600

    bazci,ci,dev: update how `stress` is invoked
    We give both `bazci` and `dev` a subcommand to merge `test.xml` files,
    and update `dev` and the various `stress` invocations in CI accordingly
    to make use of the `-shardable-artifacts` feature.
    
    Closes #74325.
    
    Release note: None

--- build/teamcity/cockroach/nightlies/stress_impl.sh
+++ build/teamcity/cockroach/nightlies/stress_impl.sh
@@ -38,7 +38,7 @@ do
                                           --test_env=COCKROACH_NIGHTLY_STRESS=true \
                                           --test_env=GO_TEST_JSON_OUTPUT_FILE=$GO_TEST_JSON_OUTPUT_FILE \
                                           --test_timeout="$TESTTIMEOUTSECS" \
-                                          --run_under "@com_github_cockroachdb_stress//:stress $STRESSFLAGS" \
+                                          --run_under "@com_github_cockroachdb_stress//:stress -bazel -shardable-artifacts 'GO_TEST_JSON_OUTPUT_FILE=cat,XML_OUTPUT_FILE=$BAZEL_BIN/pkg/cmd/bazci/bazci_/bazci merge-test-xmls' $STRESSFLAGS" \
                                           --define "gotags=$TAGS" \
                                           --nocache_test_results \
                                           ${EXTRA_BAZEL_FLAGS} \
