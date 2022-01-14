870ff40b4ab40f6635e52968b04eb899596a3617
Rebecca Taft <becca@cockroachlabs.com>
2022-01-14 17:31:44 -0600

    opt: fix bug in histogram calculation for TimeTZ
    This commit fixes a bug in the histogram estimation code for TimeTZ
    that made the faulty assumption that TimeTZ values are ordered by
    TimeOfDay. This is incorrect since it does not take the OffsetSecs
    into account. As a result, it was possible to estimate that the size
    of a histogram bucket was negative, which caused problems in the
    statistics estimation code. This commit fixes the problem by taking
    into account both TimeOfDay and OffsetSecs when estimating the size of
    a bucket in a TimeTZ histogram.
    
    Fixes #74667
    
    Release note (bug fix): Fixed an internal error, "estimated row count must
    be non-zero", that could occur during planning for queries over a table
    with a TimeTZ column. This error was due to a faulty assumption in the
    statistics estimation code about ordering of TimeTZ values, which has now
    been fixed. The error could occur when TimeTZ values used in the query had
    a different time zone offset than the TimeTZ values stored in the table.

--- pkg/sql/opt/props/histogram.go
+++ pkg/sql/opt/props/histogram.go
@@ -17,6 +17,7 @@ import (
 	"io"
 	"math"
 	"sort"
+	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/sql/inverted"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt"
@@ -26,6 +27,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util/encoding"
+	"github.com/cockroachdb/cockroach/pkg/util/timetz"
 	"github.com/cockroachdb/errors"
 	"github.com/olekukonko/tablewriter"
 )
@@ -805,12 +807,23 @@ func getRangesBeforeAndAfter(
 		return rangeBefore, rangeAfter, true
 
 	case types.TimeTZFamily:
-		lowerBefore := beforeLowerBound.(*tree.DTimeTZ).TimeOfDay
-		upperBefore := beforeUpperBound.(*tree.DTimeTZ).TimeOfDay
-		lowerAfter := afterLowerBound.(*tree.DTimeTZ).TimeOfDay
-		upperAfter := afterUpperBound.(*tree.DTimeTZ).TimeOfDay
-		rangeBefore = float64(upperBefore) - float64(lowerBefore)
-		rangeAfter = float64(upperAfter) - float64(lowerAfter)
+		// timeTZOffsetSecsRange is the total number of possible values for offset.
+		timeTZOffsetSecsRange := timetz.MaxTimeTZOffsetSecs - timetz.MinTimeTZOffsetSecs + 1
+
+		// Find the ranges in microseconds based on the absolute times of the range
+		// boundaries.
+		lowerBefore := beforeLowerBound.(*tree.DTimeTZ)
+		upperBefore := beforeUpperBound.(*tree.DTimeTZ)
+		lowerAfter := afterLowerBound.(*tree.DTimeTZ)
+		upperAfter := afterUpperBound.(*tree.DTimeTZ)
+		rangeBefore = float64(upperBefore.ToTime().Sub(lowerBefore.ToTime()) / time.Microsecond)
+		rangeAfter = float64(upperAfter.ToTime().Sub(lowerAfter.ToTime()) / time.Microsecond)
+
+		// Account for the offset.
+		rangeBefore *= float64(timeTZOffsetSecsRange)
+		rangeAfter *= float64(timeTZOffsetSecsRange)
+		rangeBefore += float64(upperBefore.OffsetSecs - lowerBefore.OffsetSecs)
+		rangeAfter += float64(upperAfter.OffsetSecs - lowerAfter.OffsetSecs)
 		return rangeBefore, rangeAfter, true
 
 	case types.StringFamily, types.BytesFamily, types.UuidFamily, types.INetFamily:
