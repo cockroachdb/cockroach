5a18c3ab8f4d1d096bc3a2d98bbe712d547c9cd1
Aditya Maru <adityamaru@gmail.com>
2022-01-17 12:16:18 -0500

    spanconfigsqlwatcher: add a `system.protected_ts_records` decoder
    This change adds a decoder to decode rows from the
    `system.protected_ts_records` table. This will be used by the
    rangefeed we setup in the SQLWatcher, to decode rangefeed updates
    and subsequently feed them into the SQLTranslator. The `target` column
    of the system table is the only column of interest since it will
    determine whether we ask the translator to generate `SystemSpanConfig`s
    or regular `SpanConfigs`. More information about this will be added
    in follow up PRs that use this decoder.
    
    This change also instructs gogoproto to generate an equality method
    for `roachpb.TenantID`, to allow for equality checks in the decoder
    test.
    
    Informs: #73727
    
    Release note: None

---
+++ pkg/spanconfig/spanconfigsqlwatcher/protectedtsdecoder_test.go
@@ -0,0 +1,102 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package spanconfigsqlwatcher_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/jobs"
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobsprotectedts"
+	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/kv"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/protectedts"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/protectedts/ptpb"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/spanconfig/spanconfigsqlwatcher"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/distsql"
+	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+// TestProtectedTimestampDecoder verifies that we can decode a row stored in
+// system.protected_ts_records.
+func TestProtectedTimestampDecoder(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	ctx := context.Background()
+	tc := testcluster.StartTestCluster(t, 1, base.TestClusterArgs{
+		ServerArgs: base.TestServerArgs{
+			Knobs: base.TestingKnobs{
+				ProtectedTS: &protectedts.TestingKnobs{EnableProtectedTimestampForMultiTenant: true}},
+		},
+	})
+	defer tc.Stopper().Stop(ctx)
+
+	s0 := tc.Server(0)
+	ptp := s0.DistSQLServer().(*distsql.ServerImpl).ServerConfig.ProtectedTimestampProvider
+	jr := s0.JobRegistry().(*jobs.Registry)
+	k := keys.SystemSQLCodec.TablePrefix(keys.ProtectedTimestampsRecordsTableID)
+
+	for _, testCase := range []struct {
+		name   string
+		target *ptpb.Target
+	}{
+		{
+			name:   "cluster",
+			target: ptpb.MakeClusterTarget(),
+		},
+		{
+			name: "tenant",
+			target: ptpb.MakeTenantsTarget([]roachpb.TenantID{roachpb.MakeTenantID(1),
+				roachpb.MakeTenantID(2)}),
+		},
+		{
+			name:   "schema-object",
+			target: ptpb.MakeSchemaObjectsTarget([]descpb.ID{1, 2}),
+		},
+	} {
+		t.Run(testCase.name, func(t *testing.T) {
+			var rec *ptpb.Record
+			ts := s0.Clock().Now()
+			jobID := jr.MakeJobID()
+
+			require.NoError(t, s0.DB().Txn(ctx, func(ctx context.Context, txn *kv.Txn) (err error) {
+				rec = jobsprotectedts.MakeRecord(uuid.MakeV4(), int64(jobID), ts,
+					nil /* deprecatedSpans */, jobsprotectedts.Jobs, testCase.target)
+				return ptp.Protect(ctx, txn, rec)
+			}))
+
+			rows, err := tc.Server(0).DB().Scan(ctx, k, k.PrefixEnd(), 0 /* maxRows */)
+			require.NoError(t, err)
+			require.Equal(t, 1, len(rows))
+
+			last := rows[len(rows)-1]
+			got, err := spanconfigsqlwatcher.TestingProtectedTimestampDecoderFn()(
+				roachpb.KeyValue{
+					Key:   last.Key,
+					Value: *last.Value,
+				},
+			)
+			require.NoError(t, err)
+			require.Truef(t, rec.Target.Equal(got),
+				"expected target=%s, got target=%s", rec.Target.String(), got.String())
+
+			require.NoError(t, s0.DB().Txn(ctx, func(ctx context.Context, txn *kv.Txn) (err error) {
+				return ptp.Release(ctx, txn, rec.ID.GetUUID())
+			}))
+		})
+	}
+}
