8cdd16ebc0a1ad058813714164e65b5793088098
Marylia Gutierrez <marylia@cockroachlabs.com>
2022-01-17 19:23:02 -0500

    server: create new endpoint that return the roles of the sql user
    The commit creates a new endpoint `/sqlroles` that returns
    a list of roles of the SQL user logged in.
    
    Partially addresses #74817
    
    Release note (api change): Creation of new endpoint `/sqlroles` that
    returns a list of the SQL roles for the SQL user logged in.

--- pkg/server/tenant_status.go
+++ pkg/server/tenant_status.go
@@ -32,6 +32,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catconstants"
 	"github.com/cockroachdb/cockroach/pkg/sql/contention"
 	"github.com/cockroachdb/cockroach/pkg/sql/flowinfra"
+	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlinstance"
 	"github.com/cockroachdb/cockroach/pkg/util/contextutil"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
@@ -883,4 +884,54 @@ func (t *tenantStatusServer) TableIndexStats(
 
 	return getTableIndexUsageStats(ctx, req, t.sqlServer.pgServer.SQLServer.GetLocalIndexStatistics(),
 		t.sqlServer.internalExecutor)
+}
+
+// UserSQLRoles return a list of the logged in SQL user roles.
+func (t *tenantStatusServer) UserSQLRoles(
+	ctx context.Context, req *serverpb.UserSQLRolesRequest,
+) (_ *serverpb.UserSQLRolesResponse, retErr error) {
+	ctx = propagateGatewayMetadata(ctx)
+	ctx = t.AnnotateCtx(ctx)
+	username, err := userFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	it, err := t.sqlServer.internalExecutor.QueryIteratorEx(
+		ctx, "sqlroles", nil, /* txn */
+		sessiondata.InternalExecutorOverride{User: username},
+		"SELECT option FROM system.role_options WHERE username=$1", username,
+	)
+	if err != nil {
+		return nil, err
+	}
+	// We have to make sure to close the iterator since we might return from the
+	// for loop early (before Next() returns false).
+	defer func() { retErr = errors.CombineErrors(retErr, it.Close()) }()
+
+	ok, err := it.Next(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp serverpb.UserSQLRolesResponse
+	if !ok {
+		// The query returned 0 rows.
+		return &resp, nil
+	}
+	scanner := makeResultScanner(it.Types())
+	for ; ok; ok, err = it.Next(ctx) {
+		row := it.Cur()
+		var role string
+		err = scanner.ScanIndex(row, 0, &role)
+		if err != nil {
+			return nil, err
+		}
+		resp.Roles = append(resp.Roles, role)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	return &resp, nil
 }
