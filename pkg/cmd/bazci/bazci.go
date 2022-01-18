c39399274a543f35b8dadec3cb2d500e8cf8901b
Ricky Stewart <ricky@cockroachlabs.com>
2022-01-18 10:38:56 -0600

    bazci: move test XML logic into `build/util`
    We'll consume it from both `bazci` and `dev` so it should be in a common
    place.
    
    Release note: None

--- pkg/cmd/bazci/bazci.go
+++ pkg/cmd/bazci/bazci.go
@@ -18,6 +18,7 @@ import (
 	"strings"
 
 	"github.com/alessio/shellescape"
+	bazelutil "github.com/cockroachdb/cockroach/pkg/build/util"
 	"github.com/cockroachdb/errors"
 	"github.com/spf13/cobra"
 )
@@ -254,7 +255,7 @@ func mungeTestXMLs(args parsedArgs) error {
 			return err
 		}
 		var buf bytes.Buffer
-		err = mungeTestXML(contents, &buf)
+		err = bazelutil.MungeTestXML(contents, &buf)
 		if err != nil {
 			return err
 		}
