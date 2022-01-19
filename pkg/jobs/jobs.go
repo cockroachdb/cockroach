7aab87c0fcf3d6eae0251bc559d2630d565e0cab
Shiranka Miskin <shiranka.miskin@gmail.com>
2022-01-19 14:29:59 -0500

    jobs: enable marking jobs as idle for metrics
    In order to allow long-running / indefinite jobs to not continually
    block a tenant from being treated as inactive in a serverless
    environment, this change adds a MarkIdle API to the jobs registry which
    increases / decreases a counter of currently idle jobs. An idle job
    should be able to be shut down at any moment.  Through the CurrentlyIdle
    and CurrentlyRunning jobs numbers serverless can determine if all jobs
    are idle and the tenant can be safely shut down.  For now this is only
    saved in-memory in the registry object, however we may transition to
    saving in persisted job state as well in
    the future.
    
    Fixes #74747
    
    Release note: None

--- pkg/jobs/jobs.go
+++ pkg/jobs/jobs.go
@@ -756,6 +756,12 @@ func (j *Job) MakeSessionBoundInternalExecutor(
 	return j.registry.sessionBoundInternalExecutorFactory(ctx, sd)
 }
 
+// MarkIdle marks the job as Idle.  Idleness should not be toggled frequently
+// (no more than ~twice a minute) as the action is logged.
+func (j *Job) MarkIdle(isIdle bool) {
+	j.registry.MarkIdle(j, isIdle)
+}
+
 func (j *Job) runInTxn(
 	ctx context.Context, txn *kv.Txn, fn func(context.Context, *kv.Txn) error,
 ) error {
