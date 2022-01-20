4490ef18fbbba2158619acd1062e234dd8447536
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-01-20 22:38:51 +0100

    kvserver: use EncodedError in SnapshotResponse
    We were previously using a "Message" string to indicate details about an
    error. We can do so much better now and actually encode the error. This
    wasn't possible when this field was first added, but it is now, so let's
    use it. As always, there's a migration concern, which means the old
    field stays around & is populated as well as interpreted for one
    release.
    
    We then use this new-found freedom to improve which errors were marked
    as "failed snapshot" errors. Previously, any error coming in on a
    `SnapshotResponse` were considered snapshot errors and were considered
    retriable. This was causing `TestLearnerSnapshotFailsRollback` to run
    for 90s, as `TestCluster`'s replication changes use a [SucceedsSoon] to
    retry snapshot errors - but that test actually injects an error that it
    wants to fail-fast.  Now, since snapshot error marks propagate over the
    wire, we can do the marking on the *sender* of the SnapshotResponse, and
    we can only mark messages that correspond to an actual failure to apply
    the snapshot (as opposed to an injected error, or a hard error due to a
    malformed request). The test now takes around one second, for a rare 90x
    speed-up.
    
    As a drive-by, we're also removing `errMalformedSnapshot`, which became
    unused when we stopped sending the raft log in raft snaps a few releases
    back, and which had managed to hide from the `unused` lint.
    
    [SucceedsSoon]: https://github.com/cockroachdb/cockroach/blob/37175f77bf374d1bcb76bc39a65149788be06134/pkg/testutils/testcluster/testcluster.go#L628-L631
    
    Fixes #74621.
    
    Release note: None

--- pkg/kv/kvserver/kvserverpb/raft.go
+++ pkg/kv/kvserver/kvserverpb/raft.go
@@ -10,5 +10,33 @@
 
 package kvserverpb
 
+import (
+	"context"
+
+	"github.com/cockroachdb/errors"
+)
+
 // SafeValue implements the redact.SafeValue interface.
 func (SnapshotRequest_Type) SafeValue() {}
+
+// Error returns the error contained in the snapshot response, if any.
+//
+// The bool indicates whether this message uses the deprecated behavior of
+// encoding an error as a string.
+func (m *DelegateSnapshotResponse) Error() (deprecated bool, _ error) {
+	return m.SnapResponse.Error()
+}
+
+// Error returns the error contained in the snapshot response, if any.
+//
+// The bool indicates whether this message uses the deprecated behavior of
+// encoding an error as a string.
+func (m *SnapshotResponse) Error() (deprecated bool, _ error) {
+	if m.Status != SnapshotResponse_ERROR {
+		return false, nil
+	}
+	if m.EncodedError.IsSet() {
+		return false, errors.DecodeError(context.Background(), m.EncodedError)
+	}
+	return true, errors.Newf("%s", m.DeprecatedMessage)
+}
