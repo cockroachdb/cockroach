f5575df545c2927cebbfd18b8f77d7f2b9caa0dc
Faizan Qazi <faizan@cockroachlabs.com>
2022-01-21 13:36:14 -0500

    schemachanger: use old schema changer for dropping temp schemas
    Previously, we would try to clean up temporary schemas using
    the declarative schema changer and run into problems looking
    up schemas. This was inadequate because these temporary schemas
    can only be looked up by ID within the same session. To address this,
    this patch will revert to the existing schema changer for dropping
    these objects. Longer term we will enhance collections and other interfaces
    to help support this in the declarative schema changer.
    
    Release note: None

--- pkg/sql/schemachanger/scbuild/internal/scbuildstmt/drop_table.go
+++ pkg/sql/schemachanger/scbuild/internal/scbuildstmt/drop_table.go
@@ -16,6 +16,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgcode"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
 	"github.com/cockroachdb/cockroach/pkg/sql/privilege"
+	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scerrors"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 )
@@ -41,6 +42,10 @@ func DropTable(b BuildCtx, n *tree.DropTable) {
 		// Mutate the AST to have the fully resolved name from above, which will be
 		// used for both event logging and errors.
 		name.ObjectNamePrefix = prefix.NamePrefix()
+		// We don't support dropping temporary tables.
+		if tbl.IsTemporary() {
+			panic(scerrors.NotImplementedErrorf(n, "dropping a temporary table"))
+		}
 		// Only decompose the tables first into elements, next we will check for
 		// dependent objects, in case they are all dropped *together*.
 		newCtx := dropTableBasic(b, tbl)
