1eabfcb92ee364a1f7f3f38b7d3972ed371db0c3
Andrew Werner <awerner32@gmail.com>
2022-01-21 17:56:35 -0500

    partitionccl,logictestccl: rewrite flaky test
    The test in question would flake if the drop index job ran to completion
    before the type change ran. This was flakey. We instead make it a real go test
    and use a testing knob to prevent this from happening.
    
    Fixes #68395
    
    Release note: None

--- pkg/ccl/partitionccl/partition_test.go
+++ pkg/ccl/partitionccl/partition_test.go
@@ -15,14 +15,18 @@ import (
 	"fmt"
 	"math/rand"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
+	"github.com/cockroachdb/cockroach-go/v2/crdb"
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/ccl/importccl"
 	"github.com/cockroachdb/cockroach/pkg/ccl/utilccl"
 	"github.com/cockroachdb/cockroach/pkg/config"
 	"github.com/cockroachdb/cockroach/pkg/config/zonepb"
+	"github.com/cockroachdb/cockroach/pkg/jobs"
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
 	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
@@ -49,6 +53,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/cockroachdb/errors"
 	"github.com/gogo/protobuf/proto"
+	"github.com/stretchr/testify/require"
 	yaml "gopkg.in/yaml.v2"
 )
 
@@ -1536,4 +1541,86 @@ func TestRemovePartitioningExpiredLicense(t *testing.T) {
 	expectErr(`ALTER INDEX t@i PARTITION BY RANGE (a) (PARTITION p45 VALUES FROM (4) TO (5))`, partitionErr)
 	expectErr(`ALTER INDEX t@t_pkey CONFIGURE ZONE USING DEFAULT`, zoneErr)
 	expectErr(`ALTER INDEX t@i CONFIGURE ZONE USING DEFAULT`, zoneErr)
+}
+
+// Test that dropping an enum value fails if there's a concurrent index drop
+// for an index partitioned by that enum value. The reason is that it
+// would be bad if we rolled back the dropping of the index.
+func TestDropEnumValueWithConcurrentPartitionedIndexDrop(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	var s serverutils.TestServerInterface
+	var sqlDB *gosql.DB
+
+	// Use the dropCh to block any DROP INDEX job until the channel is closed.
+	dropCh := make(chan struct{})
+	var closeOnce sync.Once
+	s, sqlDB, _ = serverutils.StartServer(t, base.TestServerArgs{
+		Knobs: base.TestingKnobs{
+			SQLSchemaChanger: &sql.SchemaChangerTestingKnobs{
+				RunBeforeResume: func(jobID jobspb.JobID) error {
+					var isDropJob bool
+					if err := sqlDB.QueryRow(`
+SELECT count(*) > 0
+  FROM [SHOW JOB $1]
+ WHERE description LIKE 'DROP INDEX %'
+`, jobID).Scan(&isDropJob); err != nil {
+						return err
+					}
+					if isDropJob {
+						<-dropCh
+					}
+					return nil
+				},
+			},
+		},
+	})
+	ctx := context.Background()
+	defer s.Stopper().Stop(ctx)
+	defer closeOnce.Do(func() { close(dropCh) })
+	tdb := sqlutils.MakeSQLRunner(sqlDB)
+
+	// Set up the table to have an index which is partitioned by the enum value
+	// we're going to drop.
+	for _, stmt := range []string{
+		`CREATE TYPE t AS ENUM ('a', 'b', 'c')`,
+		`CREATE TABLE tbl (
+    i INT8, k t,
+    PRIMARY KEY (i, k),
+    INDEX idx (k)
+        PARTITION BY RANGE (k)
+            (PARTITION a VALUES FROM ('a') TO ('b'))
+)`,
+	} {
+		tdb.Exec(t, stmt)
+	}
+	// Run a transaction to drop the index and the enum value.
+	errCh := make(chan error)
+	go func() {
+		errCh <- crdb.ExecuteTx(ctx, sqlDB, nil, func(tx *gosql.Tx) error {
+			if _, err := tx.Exec("drop index tbl@idx;"); err != nil {
+				return err
+			}
+			_, err := tx.Exec("alter type t drop value 'a';")
+			return err
+		})
+	}()
+	// Wait until the dropping of the enum value has finished.
+	testutils.SucceedsSoon(t, func() error {
+		var done bool
+		tdb.QueryRow(t, `SELECT count(*) = 0
+  FROM [SHOW JOBS]
+ WHERE job_type = 'TYPEDESC SCHEMA CHANGE' AND status IN`+
+			jobs.NonTerminalStatusTupleString).Scan(&done)
+		if done {
+			return nil
+		}
+		return errors.Errorf("not done")
+	})
+	// Allow the dropping of the index to proceed.
+	closeOnce.Do(func() { close(dropCh) })
+	// Ensure we got the right error.
+	require.Regexp(t,
+		`could not remove enum value "a" as it is being used in the partitioning of index tbl@idx`,
+		<-errCh)
 }
