0c40ae52e1860ee689e6fc979af5aa4051662e1b
arulajmani <arulajmani@gmail.com>
2022-01-21 12:57:30 -0500

    kvserver: make AdminVerifyProtectedTimestamp a no-op
    As part of re-working the PTS subsystem to work with secondary tenants
    we are dropping verification of protection records. None of the
    existing users of `Verification` (currently only backup) would fail
    non-destructively without it; this allows us to circumvent the
    complexity involved in making `Verification` work in the new subsystem.
    
    This patch removes code that was previously used to serve the
    `AdminVerifyProtectedTimestamp` request; we instead vacuously
    return true. This is to account for the {21.2, 22.1} mixed version case
    where 22.1 binary nodes still need to be able to serve this request.
    This can happen if the request is initiated on a 21.2 node and the
    leaseholder of the range it is trying to verify is on a 22.1 node. By
    always returning true we ensure the (backup) job upstream doesn't fail.
    This is okay even if the PTS record being verified does not apply as
    the failure mode is non-destructive (which is also why we're okay
    removing the call to verification in 22.1).
    
    This patch is required in preperation for the `ProtectedTSReader`.
    
    Release note: None

--- pkg/kv/kvserver/client_replica_test.go
+++ pkg/kv/kvserver/client_replica_test.go
@@ -3579,6 +3579,17 @@ func TestStrictGCEnforcement(t *testing.T) {
 				r.ReadProtectedTimestamps(ctx)
 			}
 		}
+		refreshCacheAndUpdatePTSState = func(t *testing.T, nodeID roachpb.NodeID) {
+			for i := 0; i < tc.NumServers(); i++ {
+				if tc.Server(i).NodeID() != nodeID {
+					continue
+				}
+				ptp := tc.Server(i).ExecutorConfig().(sql.ExecutorConfig).ProtectedTimestampProvider
+				require.NoError(t, ptp.Refresh(ctx, tc.Server(i).Clock().Now()))
+				_, r := getFirstStoreReplica(t, tc.Server(i), tableKey)
+				r.ReadProtectedTimestamps(ctx)
+			}
+		}
 	)
 
 	{
@@ -3632,13 +3643,15 @@ func TestStrictGCEnforcement(t *testing.T) {
 		}))
 		assertScanRejected(t)
 
-		require.NoError(t, ptp.Verify(ctx, rec.ID.GetUUID()))
+		desc, err := tc.LookupRange(tableKey)
+		require.NoError(t, err)
+		target, err := tc.FindRangeLeaseHolder(desc, nil)
+		require.NoError(t, err)
+		refreshCacheAndUpdatePTSState(t, target.NodeID)
 		assertScanOk(t)
 
 		// Transfer the lease and demonstrate that the query succeeds because we're
 		// cautious in the face of lease transfers.
-		desc, err := tc.LookupRange(tableKey)
-		require.NoError(t, err)
 		require.NoError(t, tc.TransferRangeLease(desc, tc.Target(1)))
 		assertScanOk(t)
 	})
