02b9fbff315de5ad5d90b1b96c0cc2b8eda2edb4
Ricky Stewart <ricky@cockroachlabs.com>
2022-01-21 15:34:08 -0600

    packer: add packer template for arm linux build agent
    Release note: None
    Release justification: Non-production code changes

--- build/packer/teamcity-agent.sh
+++ build/packer/teamcity-agent.sh
@@ -14,6 +14,7 @@ EOF
 
 # Avoid saving any Bash history.
 HISTSIZE=0
+ARCH=$(uname -m)
 
 # Add third-party APT repositories.
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0EBFCD88
@@ -44,44 +45,70 @@ apt-get install --yes \
   jq \
   openjdk-11-jre-headless \
   pass \
+  python2 \
+  python3 \
   unzip
 
-curl -fsSL https://github.com/Kitware/CMake/releases/download/v3.20.3/cmake-3.20.3-linux-x86_64.tar.gz -o /tmp/cmake.tar.gz
+case $ARCH in
+    x86_64) WHICH=x86_64; SHASUM=97bf730372f9900b2dfb9206fccbcf92f5c7f3b502148b832e77451aa0f9e0e6 ;;
+    aarch64) WHICH=aarch64; SHASUM=77620f99e9d5f39cf4a49294c6a68c89a978ecef144894618974b9958efe3c2a ;;
+esac
+curl -fsSL https://github.com/Kitware/CMake/releases/download/v3.20.3/cmake-3.20.3-linux-$WHICH.tar.gz -o /tmp/cmake.tar.gz
 sha256sum -c - <<EOF
-97bf730372f9900b2dfb9206fccbcf92f5c7f3b502148b832e77451aa0f9e0e6 /tmp/cmake.tar.gz
+$SHASUM /tmp/cmake.tar.gz
 EOF
 tar --strip-components=1 -C /usr -xzf /tmp/cmake.tar.gz
 rm -f /tmp/cmake.tar.gz
 
-curl -fsSL https://dl.google.com/go/go1.18.4.linux-amd64.tar.gz > /tmp/go.tgz
-sha256sum -c - <<EOF
+if [ $ARCH = x86_64 ]; then
+    curl -fsSL https://dl.google.com/go/go1.18.4.linux-amd64.tar.gz > /tmp/go.tgz
+    sha256sum -c - <<EOF
 c9b099b68d93f5c5c8a8844a89f8db07eaa58270e3a1e01804f17f4cf8df02f5  /tmp/go.tgz
 EOF
-tar -C /usr/local -zxf /tmp/go.tgz && rm /tmp/go.tgz
+    tar -C /usr/local -zxf /tmp/go.tgz && rm /tmp/go.tgz
 
-# Install the older version in parallel in order to run the acceptance test on older branches
-# TODO: Remove this when 21.1 is EOL (2022-11-18, according to
-# https://www.cockroachlabs.com/docs/releases/release-support-policy.html)
-curl -fsSL https://dl.google.com/go/go1.15.14.linux-amd64.tar.gz > /tmp/go_old.tgz
-sha256sum -c - <<EOF
+    # Install the older version in parallel in order to run the acceptance test on older branches
+    # TODO: Remove this when 21.1 is EOL (2022-11-18, according to
+    # https://www.cockroachlabs.com/docs/releases/release-support-policy.html)
+    curl -fsSL https://dl.google.com/go/go1.15.14.linux-amd64.tar.gz > /tmp/go_old.tgz
+    sha256sum -c - <<EOF
 6f5410c113b803f437d7a1ee6f8f124100e536cc7361920f7e640fedf7add72d /tmp/go_old.tgz
 EOF
-mkdir -p /usr/local/go1.15
-tar -C /usr/local/go1.15 --strip-components=1 -zxf /tmp/go_old.tgz && rm /tmp/go_old.tgz
+    mkdir -p /usr/local/go1.15
+    tar -C /usr/local/go1.15 --strip-components=1 -zxf /tmp/go_old.tgz && rm /tmp/go_old.tgz
 
 # Explicitly symlink the pinned version to /usr/bin.
-for f in `ls /usr/local/go/bin`; do
-    ln -s /usr/local/go/bin/$f /usr/bin
-done
+    for f in `ls /usr/local/go/bin`; do
+        ln -s /usr/local/go/bin/$f /usr/bin
+    done
+fi
+
+case $ARCH in
+    x86_64) WHICH=amd64; SHASUM=4cb534c52cdd47a6223d4596d530e7c9c785438ab3b0a49ff347e991c210b2cd ;;
+    aarch64) WHICH=arm64; SHASUM=c1de6860dd4f8d5e2ec270097bd46d6a211b971a0b8b38559784bd051ea950a1 ;;
+esac
 
 # Install Bazelisk.
 # Keep this in sync with `build/bazelbuilder/Dockerfile` and `build/bootstrap/bootstrap-debian.sh`.
-curl -fsSL https://github.com/bazelbuild/bazelisk/releases/download/v1.10.1/bazelisk-linux-amd64 > /tmp/bazelisk
+curl -fsSL https://github.com/bazelbuild/bazelisk/releases/download/v1.10.1/bazelisk-linux-$WHICH > /tmp/bazelisk
 sha256sum -c - <<EOF
-4cb534c52cdd47a6223d4596d530e7c9c785438ab3b0a49ff347e991c210b2cd /tmp/bazelisk
+$SHASUM /tmp/bazelisk
 EOF
 chmod +x /tmp/bazelisk
 mv /tmp/bazelisk /usr/bin/bazel
+
+# gcloud won't be automatically installed for ARM machines (which run on AWS).
+if [ $ARCH = aarch64 ]; then
+    curl -fsSL https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-377.0.0-linux-arm.tar.gz > /tmp/gcloud.tar.gz
+    sha256sum -c - <<EOF
+42dd29714b052d3460b005b3d09faacdb0818e70edd60c20d447a1594fd6aa83 /tmp/gcloud.tar.gz
+EOF
+    tar -C /usr/local -zxf /tmp/gcloud.tar.gz
+    rm /tmp/gcloud.tar.gz
+    /usr/local/google-cloud-sdk/install.sh -q --usage-reporting false
+    ln -s /usr/local/google-cloud-sdk/bin/gcloud /usr/bin
+    ln -s /usr/local/google-cloud-sdk/bin/gsutil /usr/bin
+fi
 
 # Installing gnome-keyring prevents the error described in
 # https://github.com/moby/moby/issues/34048
@@ -97,12 +124,18 @@ usermod -a -G docker agent
 su - agent <<'EOF'
 set -euxo pipefail
 
+ARCH=$(uname -m)
+
 # Set the default branch name for git. (Out of an abundance of caution because
 # I don't know how well TC handles having a different default branch name, stick
 # with "master".)
 git config --global init.defaultBranch master
 
-echo 'export GOPATH="$HOME"/work/.go' >> .profile && source .profile
+if [ $ARCH = x86_64 ]; then
+    echo 'export GOPATH="$HOME"/work/.go' >> .profile && source .profile
+else
+    GOPATH="$HOME"/work/.go
+fi
 
 wget https://teamcity.cockroachdb.com/update/buildAgent.zip
 unzip buildAgent.zip
@@ -124,26 +157,28 @@ EOS
 # containers.
 repo="$GOPATH"/src/github.com/cockroachdb/cockroach
 git clone --shared system/git/cockroach.git "$repo"
-cd "$repo"
-# Work around a bug in the builder's git version (at the time of writing)
-# which would corrupt the submodule defs. Probably good to remove once the
-# builder uses Ubuntu 18.04 or higher.
-git submodule update --init --recursive
-for branch in $(git branch --all --list --sort=-committerdate 'origin/release-*' | head -n1) master
-do
-  # Clean out all non-checked-in files. This is because of the check-in of
-  # the generated execgen files. Once we are no longer building 20.1 builds,
-  # the `git clean -dxf` line can be removed.
-  git clean -dxf
+if [ $ARCH = x86_64 ]; then
+    cd "$repo"
+    # Work around a bug in the builder's git version (at the time of writing)
+    # which would corrupt the submodule defs. Probably good to remove once the
+    # builder uses Ubuntu 18.04 or higher.
+    git submodule update --init --recursive
+    for branch in $(git branch --all --list --sort=-committerdate 'origin/release-*' | head -n1) master
+    do
+        # Clean out all non-checked-in files. This is because of the check-in of
+        # the generated execgen files. Once we are no longer building 20.1 builds,
+        # the `git clean -dxf` line can be removed.
+        git clean -dxf
 
-  git checkout "$branch"
-  # Stupid submodules.
-  rm -rf vendor; git checkout vendor; git submodule update --init --recursive
-  COCKROACH_BUILDER_CCACHE=1 build/builder.sh make test testrace TESTTIMEOUT=45m TESTS=-
-  # TODO(benesch): store the acceptanceversion somewhere more accessible.
-  docker pull $(git grep cockroachdb/acceptance -- '*.go' | sed -E 's/.*"([^"]*).*"/\1/') || true
-done
-cd -
+        git checkout "$branch"
+        # Stupid submodules.
+        rm -rf vendor; git checkout vendor; git submodule update --init --recursive
+        COCKROACH_BUILDER_CCACHE=1 build/builder.sh make test testrace TESTTIMEOUT=45m TESTS=-
+        # TODO(benesch): store the acceptanceversion somewhere more accessible.
+       docker pull $(git grep cockroachdb/acceptance -- '*.go' | sed -E 's/.*"([^"]*).*"/\1/') || true
+    done
+    cd -
+fi
 EOF
 write_teamcity_config
 
