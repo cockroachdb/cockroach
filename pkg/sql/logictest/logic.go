7b0ff2d24980eeeb55340b86d8fc02158c7e5d14
Steven Danna <danna@cockroachlabs.com>
2022-01-26 18:02:29 +0000

    logictest: support rewriting even for skipped tests
    Previously, the results for skipped tests were not rewritten when the
    rewrite flag was passed. Now, we rewrite the expectedResults as is if
    the test was skipped.
    
    Release note: None

--- pkg/sql/logictest/logic.go
+++ pkg/sql/logictest/logic.go
@@ -2450,6 +2450,11 @@ func (t *logicTest) processSubtest(subtest subtestDetails, path string) error {
 					}
 				}
 			} else {
+				if *rewriteResultsInTestfiles {
+					for _, l := range query.expectedResultsRaw {
+						t.emit(l)
+					}
+				}
 				s.LogAndResetSkip(t)
 			}
 			repeat = 1
