001599ee64d62e10631642e10e68397d6cb5c36a
Lidor Carmel <lidor@cockroachlabs.com>
2022-01-27 10:05:16 -0800

    sql: skip TestDistSQLFlowsVirtualTables
    See #75208
    
    Failed a bunch of time this week.
    
    Release note: None

--- pkg/sql/crdb_internal_test.go
+++ pkg/sql/crdb_internal_test.go
@@ -41,6 +41,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/tests"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
 	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
@@ -517,6 +518,7 @@ UPDATE system.namespace SET id = 12345 WHERE id = %d;
 func TestDistSQLFlowsVirtualTables(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
+	skip.UnderRace(t)
 
 	const numNodes = 3
 	const gatewayNodeID = 0
