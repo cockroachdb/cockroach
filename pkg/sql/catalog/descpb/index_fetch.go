122c0326ec0f91e13510c59ce4b2672b525896f2
Radu Berinde <radu@cockroachlabs.com>
2022-01-31 10:59:11 -0800

    descpb: introduce IndexFetchSpec
    This commit introduces a new IndexFetchSpec proto which efficiently
    encodes the necessary metadata to fetch a set of columns from an
    index. The goal is to use this instead of serializing the entire
    TableDescriptor when we execute a distributed scan.
    
    Release note: None

---
+++ pkg/sql/catalog/descpb/index_fetch.go
@@ -0,0 +1,34 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package descpb
+
+// KeyColumns returns the key columns in the index, excluding any key suffix
+// columns.
+func (s *IndexFetchSpec) KeyColumns() []IndexFetchSpec_KeyColumn {
+	return s.KeyAndSuffixColumns[:len(s.KeyAndSuffixColumns)-int(s.NumKeySuffixColumns)]
+}
+
+// KeyFullColumns returns the key columns in the index, plus all key suffix
+// columns if that index is not a unique index. It parallels
+// TableDesciptor.IndexFullColumns.
+func (s *IndexFetchSpec) KeyFullColumns() []IndexFetchSpec_KeyColumn {
+	if s.IsUniqueIndex {
+		// For unique indexes, the suffix columns are not part of the key (except
+		// when the key columns contain a NULL).
+		return s.KeyColumns()
+	}
+	return s.KeyAndSuffixColumns
+}
+
+// KeySuffixColumns returns the key suffix columns.
+func (s *IndexFetchSpec) KeySuffixColumns() []IndexFetchSpec_KeyColumn {
+	return s.KeyAndSuffixColumns[len(s.KeyAndSuffixColumns)-int(s.NumKeySuffixColumns):]
+}
