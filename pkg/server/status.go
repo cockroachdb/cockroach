0ffc720da46640ab47b6c34614082de22b0a27d6
Santamaura <santamaura@cockroachlabs.com>
2022-02-01 14:48:37 -0500

    ui: dispay circuit breakers in problem ranges and range status
    This PR adds changes to the reports/problemranges and reports/range pages.
    Ranges with replicas that have a circuit breaker will show up as problem ranges and
    the circuit breaker error will show up as a row on the status page.
    
    Release note (ui change): display circuit breakers in problems ranges and range status

--- pkg/server/status.go
+++ pkg/server/status.go
@@ -1993,6 +1993,7 @@ func (s *statusServer) rangesHelper(
 				NoLease:                metrics.Leader && !metrics.LeaseValid && !metrics.Quiescent,
 				QuiescentEqualsTicking: raftStatus != nil && metrics.Quiescent == metrics.Ticking,
 				RaftLogTooLarge:        metrics.RaftLogTooLarge,
+				CircuitBreakerError:    len(state.CircuitBreakerError) > 0,
 			},
 			LeaseStatus:                 metrics.LeaseStatus,
 			Quiescent:                   metrics.Quiescent,
