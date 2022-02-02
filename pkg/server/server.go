59382bdcd583b78d50bf2229f0a707fe5a5f91c9
Raphael 'kena' Poss <knz@thaumogen.net>
2022-02-02 13:58:31 +0100

    server: make `/_status/load` available everywhere
    Release note (api change): The `/_status/load` endpoint, which
    delivers an instant measurement of CPU load, is now available
    for regular CockroachDB nodes and not just multitenant SQL-only
    servers.
    (This is arguably a bug fix - this endpoint should have been available
    from the start.)

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1404,6 +1404,7 @@ func (s *Server) PreStart(ctx context.Context) error {
 		s.authentication,       /* authnServer */
 		s.adminAuthzCheck,      /* adminAuthzCheck */
 		s.recorder,             /* metricSource */
+		s.runtime,              /* runtimeStatsSampler */
 		gwMux,                  /* handleRequestsUnauthenticated */
 		s.debug,                /* handleDebugUnauthenticated */
 		newAPIV2Server(ctx, s), /* apiServer */
