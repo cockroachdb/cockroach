bd576c81143113238d6bdc5683cc60ee2740f298
Austen McClernon <austen@cockroachlabs.com>
2022-02-03 23:23:51 +0000

    kvserver: account for AddSSTableRequests in QPS
    Previously, Queries-Per-Second (QPS) was calculated uniformly per
    `BatchRequest` as 1. This patch introduces variable QPS calculation
    for `AddSSTableRequest`, which use an order of magnitude more
    resources than other request types.
    
    This patch introduces the
    `kv.replica_stats.addsst_request_size_factor` cluster setting. This
    setting is used to attribute QPS to `AddSSTableRequest` sizes. The
    calculation is done as QPS = 1 + size(AddSSTableRequest) / factor.
    When `kv.replica_stats.addsst_request_size_factor` is less than 1, or
    no `AddSSTableRequest` exists within a `BatchRequest`, then QPS = 1;
    the current behavior today.
    
    resolves #7371
    
    Release note (performance improvement):
    Introduced `kv.replica_stats.addsst_request_size_factor`
    cluster setting. This setting is used to tune
    Queries-Per-Second (QPS) sensitivity to large imports.
    By default, this setting is disabled. When enabled,
    the size of any AddSSTableRequest will contribute to
    QPS in inverse relation to this settings magnitude.
    Experimentally, 20,000 has produced the best load balancing results.

--- pkg/kv/kvserver/replica_rankings_test.go
+++ pkg/kv/kvserver/replica_rankings_test.go
@@ -11,13 +11,20 @@
 package kvserver
 
 import (
+	"context"
+	"fmt"
 	"math/rand"
 	"reflect"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sstutil"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/stretchr/testify/require"
 )
 
 func TestReplicaRankings(t *testing.T) {
@@ -71,5 +78,93 @@ func TestReplicaRankings(t *testing.T) {
 		if !reflect.DeepEqual(repls, replsCopy) {
 			t.Errorf("got different replicas on second call to topQPS; first call: %v, second call: %v", repls, replsCopy)
 		}
+	}
+}
+
+// TestAddSSTQPSStat verifies that AddSSTableRequests are accounted for
+// differently, when present in a BatchRequest, with a divisor set.
+func TestAddSSTQPSStat(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	ctx := context.Background()
+
+	tc := serverutils.StartNewTestCluster(t, 1, base.TestClusterArgs{
+		ReplicationMode: base.ReplicationManual,
+	})
+
+	defer tc.Stopper().Stop(ctx)
+	ts := tc.Server(0)
+	db := ts.DB()
+	conn := tc.ServerConn(0)
+	sqlDB := sqlutils.MakeSQLRunner(conn)
+
+	// Construct an sst with 100 keys that will be reused with different divisors.
+	sstKeys := make([]sstutil.KV, 100)
+	for i := range sstKeys {
+		sstKeys[i] = sstutil.KV{fmt.Sprintf("%3d", i+1), 1, "value"}
+	}
+	sst, start, end := sstutil.MakeSST(t, sstKeys)
+	requestSize := float64(len(sst))
+
+	sstReq := &roachpb.AddSSTableRequest{
+		RequestHeader: roachpb.RequestHeader{Key: start, EndKey: end},
+		Data:          sst,
+		MVCCStats:     sstutil.ComputeStats(t, sst),
+	}
+
+	get := &roachpb.GetRequest{
+		RequestHeader: roachpb.RequestHeader{Key: roachpb.Key("001")},
+	}
+
+	addSSTBA := roachpb.BatchRequest{}
+	nonSSTBA := roachpb.BatchRequest{}
+	addSSTBA.Add(sstReq)
+	nonSSTBA.Add(get)
+
+	// When the factor is set to 0, it is disabled and we expect uniform 1 QPS.
+	// In all other cases, we expect 1 + the size of a
+	// AddSSTableRequest/factor. If no AddSStableRequest exists within the
+	// request, it should be cost 1, regardless of factor.
+	testCases := []struct {
+		addsstRequestFactor int
+		expectedQPS         float64
+		ba                  roachpb.BatchRequest
+	}{
+		{0, 1, addSSTBA},
+		{100, 1, nonSSTBA},
+		{1, 1 + requestSize, addSSTBA},
+		{10, 1 + requestSize/10, addSSTBA},
+		{50, 1 + requestSize/50, addSSTBA},
+		{100, 1 + requestSize/100, addSSTBA},
+	}
+
+	// Send an AddSSTRequest once to create the key range.
+	_, pErr := db.NonTransactionalSender().Send(ctx, addSSTBA)
+	require.Nil(t, pErr)
+
+	store, err := ts.GetStores().(*Stores).GetStore(ts.GetFirstStoreID())
+	require.NoError(t, err)
+
+	repl := store.LookupReplica(roachpb.RKey(start))
+	require.NotNil(t, repl)
+
+	for _, testCase := range testCases {
+		sqlDB.Exec(t, fmt.Sprintf(`SET CLUSTER setting kv.replica_stats.addsst_request_size_factor = %d`, testCase.addsstRequestFactor))
+
+		qpsBefore, durationBefore := repl.QueriesPerSecond()
+		queriesBefore := qpsBefore * durationBefore.Seconds()
+
+		_, pErr = db.NonTransactionalSender().Send(ctx, testCase.ba)
+		require.Nil(t, pErr)
+
+		qpsAfter, durationAfter := repl.QueriesPerSecond()
+		queriesAfter := qpsAfter * durationAfter.Seconds()
+		queriesIncrease := queriesAfter - queriesBefore
+
+		// If queries are correctly recorded, we should see increase in query count by the expected
+		// QPS. However, it is possible to to get a slightly higher or lower number, due to rounding
+		// or timing errors; the result is calculated using floating point division. We allow a
+		// tolerance of 4 to avoid flaking the test due to this inaccuracy.
+		require.InDelta(t, queriesIncrease, testCase.expectedQPS, 4)
 	}
 }
