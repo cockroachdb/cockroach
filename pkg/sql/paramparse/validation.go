686ab4122e788b7c637ab1d4507cf321dd33e07e
Chengxiong Ruan <chengxiongruan@gmail.com>
2022-02-03 13:40:59 -0500

    sql: enable storage parameter syntax for primary key
    This is a prerequisite to support `WITH (bucket_count=...)` syntax
    for hash sharded index.
    
    Release note: Previously, `WITH (param=value)` syntax is not allowed
    for primary key. However, that does not align with postgresql.
    Also, to support `WITH (bucket_count=...)` syntax for hash
    sharded index, we need to enable it as well. This pr also adds
    extra validation to prevent existing storage params from being
    applied to primary keys. Which means, even the syntax is accepted
    by the parser, none of existing storage param for inverted index
    is allowed on primary key. We don't actually set those params
    on top of PKs internally, but we need to tell user which param is
    supprted or not. Eventually, when we add support for `bucket_count`
    param, a param white list will be maintained for PKs.

---
+++ pkg/sql/paramparse/validation.go
@@ -0,0 +1,31 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package paramparse
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgcode"
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+)
+
+// ValidateUniqueConstraintParams checks if there is any storage parameters
+// invalid as a param for Unique Constraint.
+func ValidateUniqueConstraintParams(params tree.StorageParams, isPK bool) error {
+	// TODO (issue 75243): add `bucket_count` as a valid param. Current dummy
+	// implementation is just for a proof of concept and make golint happy.
+	if len(params) == 0 {
+		return nil
+	}
+	if isPK {
+		return pgerror.Newf(pgcode.InvalidParameterValue, "invalid storage param %q on Primary Key", params[0].Key)
+	}
+	return pgerror.Newf(pgcode.InvalidParameterValue, "invalid storage param %q on Unique Index", params[0].Key)
+}
