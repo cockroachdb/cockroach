cockroachdb/cockroach

commit ad3bd1355463cefdc07e995765fa82adfe391d05

roachprod: library-ifying roachprod (first pass)
In #71660, roachprod library was created under pkg/roachprod
by moving the logic under pkg/cmd/roachprod to pkg/roachprod and
pointing the binary subcommands to the library.

This patch updates the logic to make it more suitable for a library
than a binary and integrates those changes into related tools such
as pkg/cmd/roachprod and pkg/cmd/roachtest.

Release note: None

pkg/roachprod/install/cluster_synced.go
@@ -16,12 +16,15 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"io/fs"
 	"io/ioutil"
 	"math"
 	"os"
 	"os/exec"
 	"os/signal"
+	// "path"
 	"path/filepath"
+	"reflect"
 	"sort"
 	"strings"
 	"sync"
@@ -43,39 +46,6 @@ import (
 	"github.com/cockroachdb/errors"
 	"golang.org/x/sync/errgroup"
 )
-
-// ClusterSettings contains various knobs that affect operations on a cluster.
-type ClusterSettings struct {
-	Secure         bool
-	PGUrlCertsDir  string
-	Env            []string
-	Tag            string
-	UseTreeDist    bool
-	Quiet          bool
-	NumRacks       int
-	MaxConcurrency int // used in Parallel
-	// DebugDir is used to stash debug information.
-	DebugDir string
-}
-
-// DefaultClusterSettings returns the default settings.
-func DefaultClusterSettings() ClusterSettings {
-	return ClusterSettings{
-		Tag:           "",
-		PGUrlCertsDir: "./certs",
-		Secure:        false,
-		Quiet:         false,
-		UseTreeDist:   true,
-		Env: []string{
-			"COCKROACH_ENABLE_RPC_COMPRESSION=false",
-			"COCKROACH_UI_RELEASE_NOTES_SIGNUP_DISMISSED=true",
-		},
-		NumRacks:       0,
-		MaxConcurrency: 32,
-	}
-}
-
-var _ = DefaultClusterSettings
 
 // A SyncedCluster is created from the cluster metadata in the synced clusters
 // cache and is used as the target for installing and managing various software
@@ -410,6 +380,12 @@ type NodeMonitorInfo struct {
 	Err error
 }
 
+// MonitorOpts is used to pass the options needed by Monitor.
+type MonitorOpts struct {
+	OneShot          bool // Report the status of all targeted nodes once, then exit.
+	IgnoreEmptyNodes bool // Only monitor nodes with a nontrivial data directory.
+}
+
 // Monitor writes NodeMonitorInfo for the cluster nodes to the returned channel.
 // Infos sent to the channel always have the Index and exactly one of Msg or Err
 // set.
@@ -421,7 +397,7 @@ type NodeMonitorInfo struct {
 // If ignoreEmptyNodes is true, nodes on which no CockroachDB data is found
 // (in {store-dir}) will not be probed and single message, "skipped", will
 // be emitted for them.
-func (c *SyncedCluster) Monitor(ignoreEmptyNodes bool, oneShot bool) chan NodeMonitorInfo {
+func (c *SyncedCluster) Monitor(opts MonitorOpts) chan NodeMonitorInfo {
 	ch := make(chan NodeMonitorInfo)
 	nodes := c.TargetNodes()
 	var wg sync.WaitGroup
@@ -453,8 +429,8 @@ func (c *SyncedCluster) Monitor(ignoreEmptyNodes bool, oneShot bool) chan NodeMo
 				Port        int
 				Local       bool
 			}{
-				OneShot:     oneShot,
-				IgnoreEmpty: ignoreEmptyNodes,
+				OneShot:     opts.OneShot,
+				IgnoreEmpty: opts.IgnoreEmptyNodes,
 				Store:       c.NodeDir(nodes[i], 1 /* storeIndex */),
 				Port:        c.NodePort(nodes[i]),
 				Local:       c.IsLocal(),
@@ -571,6 +547,113 @@ done
 	return ch
 }
 
+// RunResultDetails holds details of the result of commands executed by Run().
+type RunResultDetails struct {
+	Node             Node
+	Stdout           string
+	Stderr           string
+	Err              error
+	RemoteExitStatus string
+}
+
+// OutputRunResults prints stdout and stderr of a given RunResultDetails.
+func OutputRunResults(results []RunResultDetails) {
+	for _, result := range results {
+		if !config.Quiet {
+			// print STDOUT/STDERR and return if the node returned an err
+			fmt.Printf("--------------------------\nNODE #%d\n--------------------------\n", result.Node)
+			fmt.Printf("EXIT STATUS: %s\n", result.RemoteExitStatus)
+			if result.Stdout != "" {
+				fmt.Println(result.Stdout)
+				fmt.Println("End of STDOUT")
+			}
+			if result.Stderr != "" {
+				fmt.Println(result.Stderr)
+				fmt.Println("End of STDERR")
+			}
+		}
+	}
+}
+
+func processStdout(stdout string) (string, string) {
+	retStdout := stdout
+	exitStatusPattern := "LAST EXIT STATUS: "
+	exitStatusIndex := strings.LastIndex(retStdout, exitStatusPattern)
+	remoteExitStatus := "-1"
+	// If exitStatusIndex is -1 then "echo LAST EXIT STATUS: $?" didn't run
+	// mostly due to an ssh error but avoid speculation and temporarily
+	// use "-1" for unknown error before checking if it's SSH related later.
+	if exitStatusIndex != -1 {
+		retStdout = stdout[:exitStatusIndex]
+		remoteExitStatus = strings.TrimSpace(stdout[exitStatusIndex+len(exitStatusPattern):])
+	}
+	return retStdout, remoteExitStatus
+}
+
+func runCmdOnSingleNode(c *SyncedCluster, node Node, cmd string) (RunResultDetails, error) {
+	var result RunResultDetails
+	result.Node = node
+	sess, err := c.newSession(node)
+	if err != nil {
+		return result, err
+	}
+	sess.SetWithExitStatus(true)
+	defer sess.Close()
+	var stdoutBuffer, stderrBuffer bytes.Buffer
+	sess.SetStdout(&stdoutBuffer)
+	sess.SetStderr(&stderrBuffer)
+
+	// Argument template expansion is node specific (e.g. for {store-dir}).
+	e := expander{
+		node: node,
+	}
+	expandedCmd, err := e.expand(c, cmd)
+	if err != nil {
+		return result, err
+	}
+
+	// Be careful about changing these command strings. In particular, we need
+	// to support running commands in the background on both local and remote
+	// nodes. For example:
+	//
+	//   roachprod run cluster -- "sleep 60 &> /dev/null < /dev/null &"
+	//
+	// That command should return immediately. And a "roachprod status" should
+	// reveal that the sleep command is running on the cluster.
+	nodeCmd := fmt.Sprintf(`export ROACHPROD=%s GOTRACEBACK=crash && bash -c %s`,
+		c.roachprodEnvValue(node), ssh.Escape1(expandedCmd))
+	if c.IsLocal() {
+		nodeCmd = fmt.Sprintf("cd %s; %s", c.localVMDir(node), nodeCmd)
+	}
+
+	err = sess.Run(nodeCmd)
+	result.Stderr = strings.TrimSpace(stderrBuffer.String())
+	result.Stdout, result.RemoteExitStatus = processStdout(stdoutBuffer.String())
+
+	if err != nil {
+		detailMsg := fmt.Sprintf("Node %d. Command with error:\n```\n%s\n```\n", node, cmd)
+		err = errors.WithDetail(err, detailMsg)
+		err = rperrors.ClassifyCmdError(err)
+		if reflect.TypeOf(err) == reflect.TypeOf(rperrors.SSH{}) {
+			result.RemoteExitStatus = "255"
+		}
+		result.Err = err
+	} else if result.RemoteExitStatus != "0" {
+		message := fmt.Sprintf("Exit Code: %s\nStdout:%s\nStderr:%s\n", result.RemoteExitStatus, result.Stdout, result.Stderr)
+		result.Err = &NonZeroExitCode{message}
+	}
+	return result, nil
+}
+
+// NonZeroExitCode is returned when a command executed by Run() exits with a non-zero status.
+type NonZeroExitCode struct {
+	message string
+}
+
+func (e *NonZeroExitCode) Error() string {
+	return e.message
+}
+
 // Run a command on >= 1 node in the cluster.
 //
 // When running on just one node, the command output is streamed to stdout.
@@ -658,6 +741,28 @@ func (c *SyncedCluster) Run(stdout, stderr io.Writer, nodes Nodes, title, cmd st
 		}
 	}
 	return rperrors.SelectPriorityError(errs)
+}
+
+// RunNew will replace Run but we are still migrating so both exist now.
+func (c *SyncedCluster) RunNew(nodes Nodes, title, cmd string) ([]RunResultDetails, error) {
+	display := fmt.Sprintf("%s: %s", c.Name, title)
+	results := make([]RunResultDetails, len(nodes))
+
+	failed, err := c.ParallelE(display, len(nodes), 0, func(i int) ([]byte, error) {
+		result, err := runCmdOnSingleNode(c, nodes[i], cmd)
+		if err != nil {
+			return nil, err
+		}
+		results[i] = result
+		return nil, nil
+	})
+	if err != nil {
+		for _, node := range failed {
+			results[node.Index].Err = node.Err
+			results[node.Index].Stdout = string(node.Out)
+		}
+	}
+	return results, nil
 }
 
 // Wait TODO(peter): document
@@ -1108,6 +1213,24 @@ func formatProgress(p float64) string {
 
 // Put TODO(peter): document
 func (c *SyncedCluster) Put(src, dest string) error {
+	// Check if source file exists and if it's a symlink.
+	var potentialSymlinkPath string
+	var err error
+	if potentialSymlinkPath, err = filepath.EvalSymlinks(src); err != nil {
+		return err
+	}
+	// Different paths imply it is a symlink.
+	if potentialSymlinkPath != src {
+		// Get target symlink access mode.
+		var symlinkTargetInfo fs.FileInfo
+		if symlinkTargetInfo, err = os.Stat(potentialSymlinkPath); err != nil {
+			return err
+		}
+		redColor, resetColor := "\033[31m", "\033[0m"
+		fmt.Println(redColor + "WARNING: Source file is a symlink.")
+		fmt.Printf("WARNING: Remote file will inherit the target permissions '%v'.\n"+resetColor, symlinkTargetInfo.Mode())
+	}
+
 	// NB: This value was determined with a few experiments. Higher values were
 	// not tested.
 	const treeDistFanout = 10
@@ -1262,7 +1385,7 @@ func (c *SyncedCluster) Put(src, dest string) error {
 
 	var writer ui.Writer
 	var ticker *time.Ticker
-	if !c.Quiet {
+	if !config.Quiet {
 		ticker = time.NewTicker(100 * time.Millisecond)
 	} else {
 		ticker = time.NewTicker(1000 * time.Millisecond)
@@ -1276,7 +1399,7 @@ func (c *SyncedCluster) Put(src, dest string) error {
 	for done := false; !done; {
 		select {
 		case <-ticker.C:
-			if c.Quiet {
+			if config.Quiet {
 				fmt.Printf(".")
 			}
 		case r, ok := <-results:
@@ -1292,7 +1415,7 @@ func (c *SyncedCluster) Put(src, dest string) error {
 				linesMu.Unlock()
 			}
 		}
-		if !c.Quiet {
+		if !config.Quiet {
 			linesMu.Lock()
 			for i := range lines {
 				fmt.Fprintf(&writer, "  %2d: ", c.Nodes[i])
@@ -1309,7 +1432,7 @@ func (c *SyncedCluster) Put(src, dest string) error {
 		}
 	}
 
-	if c.Quiet {
+	if config.Quiet {
 		fmt.Printf("\n")
 		linesMu.Lock()
 		for i := range lines {
@@ -1617,7 +1740,7 @@ func (c *SyncedCluster) Get(src, dest string) error {
 	}()
 
 	var ticker *time.Ticker
-	if !c.Quiet {
+	if config.Quiet {
 		ticker = time.NewTicker(100 * time.Millisecond)
 	} else {
 		ticker = time.NewTicker(1000 * time.Millisecond)
@@ -1631,7 +1754,7 @@ func (c *SyncedCluster) Get(src, dest string) error {
 	for done := false; !done; {
 		select {
 		case <-ticker.C:
-			if c.Quiet {
+			if config.Quiet {
 				fmt.Printf(".")
 			}
 		case r, ok := <-results:
@@ -1647,7 +1770,7 @@ func (c *SyncedCluster) Get(src, dest string) error {
 				linesMu.Unlock()
 			}
 		}
-		if !c.Quiet {
+		if !config.Quiet {
 			linesMu.Lock()
 			for i := range lines {
 				fmt.Fprintf(&writer, "  %2d: ", c.Nodes[i])
@@ -1664,7 +1787,7 @@ func (c *SyncedCluster) Get(src, dest string) error {
 		}
 	}
 
-	if c.Quiet {
+	if config.Quiet {
 		fmt.Printf("\n")
 		linesMu.Lock()
 		for i := range lines {
@@ -1819,7 +1942,7 @@ func (c *SyncedCluster) Parallel(
 //
 // ParallelE runs the user-defined functions on the first `count`
 // nodes in the cluster. It runs at most `concurrency` (or
-// `c.MaxConcurrency` if it is lower) in parallel. If `concurrency` is
+// `config.MaxConcurrency` if it is lower) in parallel. If `concurrency` is
 // 0, then it defaults to `count`.
 //
 // If err is non-nil, the slice of ParallelResults will contain the
@@ -1830,8 +1953,8 @@ func (c *SyncedCluster) ParallelE(
 	if concurrency == 0 || concurrency > count {
 		concurrency = count
 	}
-	if c.MaxConcurrency > 0 && concurrency > c.MaxConcurrency {
-		concurrency = c.MaxConcurrency
+	if config.MaxConcurrency > 0 && concurrency > config.MaxConcurrency {
+		concurrency = config.MaxConcurrency
 	}
 
 	results := make(chan ParallelResult, count)
@@ -1864,7 +1987,7 @@ func (c *SyncedCluster) ParallelE(
 	}
 
 	var ticker *time.Ticker
-	if !c.Quiet {
+	if !config.Quiet {
 		ticker = time.NewTicker(100 * time.Millisecond)
 	} else {
 		ticker = time.NewTicker(1000 * time.Millisecond)
@@ -1880,7 +2003,7 @@ func (c *SyncedCluster) ParallelE(
 	for done := false; !done; {
 		select {
 		case <-ticker.C:
-			if c.Quiet {
+			if config.Quiet {
 				fmt.Fprintf(out, ".")
 			}
 		case r, ok := <-results:
@@ -1896,7 +2019,7 @@ func (c *SyncedCluster) ParallelE(
 			}
 		}
 
-		if !c.Quiet {
+		if !config.Quiet {
 			fmt.Fprint(&writer, display)
 			var n int
 			for i := range complete {
@@ -1914,7 +2037,7 @@ func (c *SyncedCluster) ParallelE(
 		}
 	}
 
-	if c.Quiet {
+	if config.Quiet {
 		fmt.Fprintf(out, "\n")
 	}
 

build/teamcity-roachtest-invoke.sh
@@ -8,7 +8,7 @@ bin/roachtest run \
   --teamcity \
   --roachprod="${PWD}/bin/roachprod" \
   --workload="${PWD}/bin/workload" \
-  --create-args=--os-volume-size=32 \
+  --os-volume-size=32 \
   "$@"
 code=$?
 set -e

build/teamcity-roachtest-stress.sh
@@ -26,7 +26,7 @@ build/teamcity-roachtest-invoke.sh \
   --cpu-quota="${CPUQUOTA-1024}" \
   --cluster-id="${TC_BUILD_ID}" \
   --build-tag="${BUILD_TAG}" \
-  --create-args="--lifetime=36h" \
+  --lifetime="36h" \
   --cockroach="${PWD}/cockroach-linux-2.6.32-gnu-amd64" \
   --artifacts="${PWD}/artifacts" \
   --disable-issue \

pkg/BUILD.bazel
@@ -82,7 +82,6 @@ ALL_TESTS = [
     "//pkg/cmd/publish-artifacts:publish-artifacts_test",
     "//pkg/cmd/publish-provisional-artifacts:publish-provisional-artifacts_test",
     "//pkg/cmd/roachtest/prometheus:prometheus_test",
-    "//pkg/cmd/roachtest/spec:spec_test",
     "//pkg/cmd/roachtest/tests:tests_test",
     "//pkg/cmd/roachtest:roachtest_test",
     "//pkg/cmd/teamcity-trigger:teamcity-trigger_test",

pkg/cmd/roachprod/BUILD.bazel
@@ -18,14 +18,10 @@ go_library(
         "//pkg/roachprod/ssh",
         "//pkg/roachprod/ui",
         "//pkg/roachprod/vm",
-        "//pkg/roachprod/vm/aws",
-        "//pkg/roachprod/vm/azure",
         "//pkg/roachprod/vm/gce",
-        "//pkg/roachprod/vm/local",
         "//pkg/util/flagutil",
         "@com_github_cockroachdb_errors//:errors",
         "@com_github_spf13_cobra//:cobra",
-        "@org_golang_x_term//:term",
     ],
 )
 

pkg/cmd/roachprod/flags.go
@@ -15,6 +15,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/cockroachdb/cockroach/pkg/roachprod"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/config"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/ssh"
@@ -25,6 +26,7 @@ import (
 )
 
 var (
+	pprofOpts         roachprod.PprofOpts
 	numNodes          int
 	numRacks          int
 	username          string
@@ -50,7 +52,6 @@ var (
 	adminurlPath  = ""
 	adminurlIPs   = false
 	useTreeDist   = true
-	quiet         = false
 	sig           = 9
 	waitFlag      = false
 	createVMOpts  = vm.DefaultCreateOpts()
@@ -68,17 +69,14 @@ var (
 	logsFrom          time.Time
 	logsTo            time.Time
 	logsInterval      time.Duration
-	maxConcurrency    int
 
-	monitorIgnoreEmptyNodes bool
-	monitorOneShot          bool
-
+	monitorOpts        install.MonitorOpts
 	cachedHostsCluster string
 )
 
 func initFlags() {
-	rootCmd.PersistentFlags().BoolVarP(&quiet, "quiet", "q", false, "disable fancy progress output")
-	rootCmd.PersistentFlags().IntVarP(&maxConcurrency, "max-concurrency", "", 32,
+	rootCmd.PersistentFlags().BoolVarP(&config.Quiet, "quiet", "q", false, "disable fancy progress output")
+	rootCmd.PersistentFlags().IntVarP(&config.MaxConcurrency, "max-concurrency", "", 32,
 		"maximum number of operations to execute on nodes concurrently, set to zero for infinite",
 	)
 
@@ -155,13 +153,13 @@ func initFlags() {
 	pgurlCmd.Flags().StringVar(&pgurlCertsDir,
 		"certs-dir", "./certs", "cert dir to use for secure connections")
 
-	pprofCmd.Flags().DurationVar(&pprofOptions.duration,
+	pprofCmd.Flags().DurationVar(&pprofOpts.Duration,
 		"duration", 30*time.Second, "Duration of profile to capture")
-	pprofCmd.Flags().BoolVar(&pprofOptions.heap,
+	pprofCmd.Flags().BoolVar(&pprofOpts.Heap,
 		"heap", false, "Capture a heap profile instead of a CPU profile")
-	pprofCmd.Flags().BoolVar(&pprofOptions.open,
+	pprofCmd.Flags().BoolVar(&pprofOpts.Open,
 		"open", false, "Open the profile using `go tool pprof -http`")
-	pprofCmd.Flags().IntVar(&pprofOptions.startingPort,
+	pprofCmd.Flags().IntVar(&pprofOpts.StartingPort,
 		"starting-port", 9000, "Initial port to use when opening pprof's HTTP interface")
 
 	ipCmd.Flags().BoolVar(&external,
@@ -210,12 +208,12 @@ func initFlags() {
 	logsCmd.Flags().StringVar(&logsProgramFilter,
 		"logs-program", "^cockroach$", "regular expression of the name of program in log files to search")
 
-	monitorCmd.Flags().BoolVar(&monitorIgnoreEmptyNodes,
+	monitorCmd.Flags().BoolVar(&monitorOpts.IgnoreEmptyNodes,
 		"ignore-empty-nodes", false,
 		"Automatically detect the (subset of the given) nodes which to monitor "+
 			"based on the presence of a nontrivial data directory.")
 
-	monitorCmd.Flags().BoolVar(&monitorOneShot,
+	monitorCmd.Flags().BoolVar(&monitorOpts.OneShot,
 		"oneshot", false,
 		"Report the status of all targeted nodes once, then exit. The exit "+
 			"status is nonzero if (and only if) any node was found not running.")

pkg/cmd/roachprod/main.go
@@ -27,12 +27,8 @@ import (
 	rperrors "github.com/cockroachdb/cockroach/pkg/roachprod/errors"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/ui"
-	_ "github.com/cockroachdb/cockroach/pkg/roachprod/vm/aws"
-	_ "github.com/cockroachdb/cockroach/pkg/roachprod/vm/azure"
-	_ "github.com/cockroachdb/cockroach/pkg/roachprod/vm/local"
 	"github.com/cockroachdb/errors"
 	"github.com/spf13/cobra"
-	"golang.org/x/term"
 )
 
 var rootCmd = &cobra.Command{
@@ -81,20 +77,6 @@ func wrap(f func(cmd *cobra.Command, args []string) error) func(cmd *cobra.Comma
 	}
 }
 
-// clusterOpts returns the ClusterSettings according to the command-line flags.
-func clusterOpts() install.ClusterSettings {
-	return install.ClusterSettings{
-		Tag:            tag,
-		PGUrlCertsDir:  pgurlCertsDir,
-		Secure:         secure,
-		Quiet:          quiet || !term.IsTerminal(int(os.Stdout.Fd())),
-		UseTreeDist:    useTreeDist,
-		Env:            nodeEnv,
-		NumRacks:       numRacks,
-		MaxConcurrency: maxConcurrency,
-	}
-}
-
 var createCmd = &cobra.Command{
 	Use:   "create <cluster>",
 	Short: "create a cluster",
@@ -140,7 +122,8 @@ Local Clusters
 `,
 	Args: cobra.ExactArgs(1),
 	Run: wrap(func(cmd *cobra.Command, args []string) (retErr error) {
-		return roachprod.Create(numNodes, username, createVMOpts, args[0], clusterOpts())
+		createVMOpts.ClusterName = args[0]
+		return roachprod.Create(username, numNodes, createVMOpts)
 	}),
 }
 
@@ -159,7 +142,7 @@ if the user would like to update the keys on the remote hosts.
 
 	Args: cobra.ExactArgs(1),
 	Run: wrap(func(cmd *cobra.Command, args []string) (retErr error) {
-		return roachprod.SetupSSH(args[0], clusterOpts(), username)
+		return roachprod.SetupSSH(args[0])
 	}),
 }
 
@@ -180,7 +163,7 @@ directories inside ${HOME}/local directory are removed.
 `,
 	Args: cobra.ArbitraryArgs,
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		return roachprod.Destroy(args, clusterOpts(), destroyAllMine, destroyAllLocal, username)
+		return roachprod.Destroy(destroyAllMine, destroyAllLocal, args...)
 	}),
 }
 
@@ -257,7 +240,7 @@ hosts file.
 		if listJSON && listDetails {
 			return errors.New("'json' option cannot be combined with 'details' option")
 		}
-		filteredCloud, err := roachprod.List(quiet, listMine, listPattern)
+		filteredCloud, err := roachprod.List(listMine, listPattern)
 		if err != nil {
 			return err
 		}
@@ -329,7 +312,7 @@ var syncCmd = &cobra.Command{
 	Long:  ``,
 	Args:  cobra.NoArgs,
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		_, err := roachprod.Sync(quiet)
+		_, err := roachprod.Sync()
 		_ = rootCmd.GenBashCompletionFile(bashCompletion)
 		return err
 	}),
@@ -345,7 +328,7 @@ hourly by a cronjob so it is not necessary to run manually.
 `,
 	Args: cobra.NoArgs,
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		return roachprod.GC(dryrun, config.SlackToken)
+		return roachprod.GC(dryrun)
 	}),
 }
 
@@ -359,7 +342,7 @@ destroyed:
 `,
 	Args: cobra.ExactArgs(1),
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		return roachprod.Extend(args[0], clusterOpts(), extendLifetime)
+		return roachprod.Extend(args[0], extendLifetime)
 	}),
 }
 
@@ -403,7 +386,15 @@ cluster setting will be set to its value.
 `,
 	Args: cobra.ExactArgs(1),
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		return roachprod.Start(args[0], clusterOpts(), startOpts)
+		clusterSettingsOpts := []install.ClusterSettingOption{
+			install.TagOption(tag),
+			install.PGUrlCertsDirOption(pgurlCertsDir),
+			install.SecureOption(secure),
+			install.UseTreeDistOption(useTreeDist),
+			install.EnvOption(nodeEnv),
+			install.NumRacksOption(numRacks),
+		}
+		return roachprod.Start(args[0], startOpts, clusterSettingsOpts...)
 	}),
 }
 
@@ -432,7 +423,7 @@ signals.
 		if sig == 9 /* SIGKILL */ && !cmd.Flags().Changed("wait") {
 			wait = true
 		}
-		return roachprod.Stop(args[0], clusterOpts(), sig, wait)
+		return roachprod.Stop(args[0], tag, sig, wait)
 	}),
 }
 
@@ -447,7 +438,7 @@ default cluster settings. It's intended to be used in conjunction with
 `,
 	Args: cobra.ExactArgs(1),
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		return roachprod.Init(args[0], clusterOpts(), username)
+		return roachprod.Init(args[0])
 	}),
 }
 
@@ -467,7 +458,7 @@ The "status" command outputs the binary and PID for the specified nodes:
 `,
 	Args: cobra.ExactArgs(1),
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		return roachprod.Status(args[0], clusterOpts())
+		return roachprod.Status(args[0], tag)
 	}),
 }
 
@@ -492,7 +483,7 @@ into a single stream.
 		} else {
 			dest = args[0] + ".logs"
 		}
-		return roachprod.Logs(logsOpts, args[0], clusterOpts(), dest, username)
+		return roachprod.Logs(args[0], dest, username, logsOpts)
 	}),
 }
 
@@ -515,7 +506,7 @@ of nodes, outputting a line whenever a change is detected:
 `,
 	Args: cobra.ExactArgs(1),
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		return roachprod.Monitor(args[0], clusterOpts(), monitorIgnoreEmptyNodes, monitorOneShot)
+		return roachprod.Monitor(args[0], monitorOpts)
 	}),
 }
 
@@ -530,7 +521,7 @@ nodes.
 `,
 	Args: cobra.ExactArgs(1),
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		return roachprod.Wipe(args[0], clusterOpts(), wipePreserveCerts)
+		return roachprod.Wipe(args[0], wipePreserveCerts)
 	}),
 }
 
@@ -560,7 +551,7 @@ the 'zfs rollback' command:
 
 	Args: cobra.ExactArgs(2),
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		return roachprod.Reformat(args[0], clusterOpts(), args[1])
+		return roachprod.Reformat(args[0], args[1])
 	}),
 }
 
@@ -572,7 +563,7 @@ var runCmd = &cobra.Command{
 `,
 	Args: cobra.MinimumNArgs(1),
 	Run: wrap(func(_ *cobra.Command, args []string) error {
-		return roachprod.Run(args[0], clusterOpts(), extraSSHOptions, args[1:])
+		return roachprod.Run(args[0], extraSSHOptions, tag, secure, args[1:])
 	}),
 }
 
@@ -583,7 +574,7 @@ var resetCmd = &cobra.Command{
 environments and will fall back to a no-op.`,
 	Args: cobra.ExactArgs(1),
 	Run: wrap(func(cmd *cobra.Command, args []string) (retErr error) {
-		return roachprod.Reset(args[0], clusterOpts(), numNodes, username)
+		return roachprod.Reset(args[0])
 	}),
 }
 
@@ -596,7 +587,7 @@ var installCmd = &cobra.Command{
 `,
 	Args: cobra.MinimumNArgs(2),
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		return roachprod.Install(args[0], clusterOpts(), args[1:])
+		return roachprod.Install(args[0], args[1:])
 	}),
 }
 
@@ -611,7 +602,7 @@ var downloadCmd = &cobra.Command{
 		if len(args) == 4 {
 			dest = args[3]
 		}
-		return roachprod.Download(args[0], clusterOpts(), src, sha, dest)
+		return roachprod.Download(args[0], src, sha, dest)
 	}),
 }
 
@@ -672,7 +663,7 @@ Some examples of usage:
 		if len(args) == 3 {
 			versionArg = args[2]
 		}
-		return roachprod.Stage(args[0], clusterOpts(), stageOS, stageDir, args[1], versionArg)
+		return roachprod.Stage(args[0], stageOS, stageDir, args[1], versionArg)
 	}),
 }
 
@@ -686,7 +677,7 @@ start."
 `,
 	Args: cobra.ExactArgs(1),
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		return roachprod.DistributeCerts(args[0], clusterOpts())
+		return roachprod.DistributeCerts(args[0])
 	}),
 }
 
@@ -702,7 +693,7 @@ var putCmd = &cobra.Command{
 		if len(args) == 3 {
 			dest = args[2]
 		}
-		return roachprod.Put(args[0], clusterOpts(), src, dest)
+		return roachprod.Put(args[0], src, dest, useTreeDist)
 	}),
 }
 
@@ -719,7 +710,7 @@ multiple nodes the destination file name will be prefixed with the node number.
 		if len(args) == 3 {
 			dest = args[2]
 		}
-		return roachprod.Get(args[0], clusterOpts(), src, dest)
+		return roachprod.Get(args[0], src, dest)
 	}),
 }
 
@@ -729,7 +720,7 @@ var sqlCmd = &cobra.Command{
 	Long:  "Run `cockroach sql` on a remote cluster.\n",
 	Args:  cobra.MinimumNArgs(1),
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		return roachprod.SQL(args[0], clusterOpts(), args[1:])
+		return roachprod.SQL(args[0], secure, args[1:])
 	}),
 }
 
@@ -740,16 +731,14 @@ var pgurlCmd = &cobra.Command{
 `,
 	Args: cobra.ExactArgs(1),
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		return roachprod.PgURL(args[0], clusterOpts(), external)
+		urls, err := roachprod.PgURL(args[0], pgurlCertsDir, external, secure)
+		if err != nil {
+			return err
+		}
+		fmt.Println(strings.Join(urls, " "))
+		return nil
 	}),
 }
-
-var pprofOptions = struct {
-	heap         bool
-	open         bool
-	startingPort int
-	duration     time.Duration
-}{}
 
 var pprofCmd = &cobra.Command{
 	Use:     "pprof <cluster>",
@@ -771,9 +760,9 @@ Examples:
 `,
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
 		if cmd.CalledAs() == "pprof-heap" {
-			pprofOptions.heap = true
+			pprofOpts.Heap = true
 		}
-		return roachprod.Pprof(args[0], clusterOpts(), pprofOptions.duration, pprofOptions.heap, pprofOptions.open, pprofOptions.startingPort)
+		return roachprod.Pprof(args[0], pprofOpts)
 	}),
 }
 
@@ -785,7 +774,14 @@ var adminurlCmd = &cobra.Command{
 `,
 	Args: cobra.ExactArgs(1),
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		return roachprod.AdminURL(args[0], clusterOpts(), adminurlIPs, adminurlOpen, adminurlPath)
+		urls, err := roachprod.AdminURL(args[0], adminurlPath, adminurlIPs, adminurlOpen, secure)
+		if err != nil {
+			return err
+		}
+		for _, url := range urls {
+			fmt.Println(url)
+		}
+		return nil
 	}),
 }
 
@@ -796,7 +792,7 @@ var ipCmd = &cobra.Command{
 `,
 	Args: cobra.ExactArgs(1),
 	Run: wrap(func(cmd *cobra.Command, args []string) error {
-		ips, err := roachprod.IP(args[0], clusterOpts(), external)
+		ips, err := roachprod.IP(args[0], external)
 		if err != nil {
 			return err
 		}

pkg/cmd/roachtest/BUILD.bazel
@@ -25,6 +25,10 @@ go_library(
         "//pkg/cmd/roachtest/test",
         "//pkg/cmd/roachtest/tests:with-mocks",
         "//pkg/internal/team",
+        "//pkg/roachprod",
+        "//pkg/roachprod/config",
+        "//pkg/roachprod/install",
+        "//pkg/roachprod/vm",
         "//pkg/testutils/skip",
         "//pkg/util/contextutil",
         "//pkg/util/ctxgroup",
@@ -42,6 +46,7 @@ go_library(
         "@com_github_petermattis_goid//:goid",
         "@com_github_slack_go_slack//:slack",
         "@com_github_spf13_cobra//:cobra",
+        "@com_github_spf13_pflag//:pflag",
         "@org_golang_x_sync//errgroup",
     ],
 )

commit d516f79a1844708c113832826454ac5eb09c8a5d

roachprod: fix cluster creation output
Broke in ad3bd1355463cefdc07e995765fa82adfe391d05.

Release note: None

pkg/roachprod/install/cluster_synced.go
@@ -1385,7 +1385,7 @@ func (c *SyncedCluster) Put(ctx context.Context, l *logger.Logger, src, dest str
 	for done := false; !done; {
 		select {
 		case <-ticker.C:
-			if config.Quiet && l.File == nil {
+			if !config.Quiet && l.File == nil {
 				fmt.Printf(".")
 			}
 		case r, ok := <-results:
@@ -1418,7 +1418,7 @@ func (c *SyncedCluster) Put(ctx context.Context, l *logger.Logger, src, dest str
 		}
 	}
 
-	if config.Quiet && l.File != nil {
+	if !config.Quiet && l.File != nil {
 		l.Printf("\n")
 		linesMu.Lock()
 		for i := range lines {
@@ -1740,7 +1740,7 @@ func (c *SyncedCluster) Get(l *logger.Logger, src, dest string) error {
 	for done := false; !done; {
 		select {
 		case <-ticker.C:
-			if config.Quiet && l.File == nil {
+			if !config.Quiet && l.File == nil {
 				fmt.Printf(".")
 			}
 		case r, ok := <-results:
@@ -1773,7 +1773,7 @@ func (c *SyncedCluster) Get(l *logger.Logger, src, dest string) error {
 		}
 	}
 
-	if config.Quiet && l.File == nil {
+	if !config.Quiet && l.File == nil {
 		l.Printf("\n")
 		linesMu.Lock()
 		for i := range lines {
@@ -1977,7 +1977,7 @@ func (c *SyncedCluster) ParallelE(
 	}
 
 	var ticker *time.Ticker
-	if !config.Quiet {
+	if config.Quiet {
 		ticker = time.NewTicker(100 * time.Millisecond)
 	} else {
 		ticker = time.NewTicker(1000 * time.Millisecond)
@@ -1993,7 +1993,7 @@ func (c *SyncedCluster) ParallelE(
 	for done := false; !done; {
 		select {
 		case <-ticker.C:
-			if config.Quiet && l.File == nil {
+			if !config.Quiet && l.File == nil {
 				fmt.Fprintf(out, ".")
 			}
 		case r, ok := <-results:
@@ -2027,7 +2027,7 @@ func (c *SyncedCluster) ParallelE(
 		}
 	}
 
-	if config.Quiet && l.File == nil {
+	if !config.Quiet && l.File == nil {
 		fmt.Fprintf(out, "\n")
 	}
 