ec57fc4247611bafd80d39a6f45912acdf369c3e
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-02-05 16:35:51 +0100

    raftlog: introduce & use in loqrecovery, kvserver
    This commit introduces a new package `raftlog`. Aspirationally, this
    will at some point become the de facto way of encapsulating the raft log
    encoding and may play a role in programmatically constructing and
    inspecting raft logs (e.g.  for testing).
    
    For now, we introduce two concepts:
    
    - `raftlog.Entry`, which wraps a `raftpb.Entry` and all of the
      information derived from it, such as the command ID, the
    `kvserverpb.RaftCommand`, the configuration change (if any), etc.
    
    - `raftlog.Iterator`, a way to iterate over the raft log in terms of
      `raftlog.Entry` (as opposed to `raftpb.Entry` which requires lots of
    manual processing).
    
    Both are then applied across the codebase, concretely:
    
    - `loqrecovery` is simplified via `raftlog.Iterator` to pull commit
      triggers out of the raft log.
    - debug pretty-printing is simpler thanks to use of `raftlog.Entry`.
    - `decodedRaftEntry` is now structurally a `raftpb.Entry`, and again
      lots manual custom unmarshaling code evaporates.
    
    It's currently difficult to create "interesting" raft log entries if
    trying to stay away from manual population of large datastructures
    (which is prone to rotting), so there's zero unit testing of
    `raftlog.Iterator`. However, the code is not new, instead it was
    deduplicated from a few places, and is now tested through all of them;
    so I don't feel to bad about it. I still think it is a priority to be
    able to "comfortably" create at least "simple" raft logs, meaning we
    need to be able to string together `batcheval` and entry creation at
    least in a rudimentary fashion. I intend to look into this next and
    add comprehensive unit tests for `raftlog.{Entry,Iterator}`.
    
    Touches #75729.
    
    Release note: None

---
+++ pkg/kv/kvserver/raftlog/entry.go
@@ -0,0 +1,117 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package raftlog
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvserverbase"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvserverpb"
+	"github.com/cockroachdb/cockroach/pkg/storage"
+	"github.com/cockroachdb/cockroach/pkg/storage/enginepb"
+	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
+	"github.com/cockroachdb/errors"
+	"go.etcd.io/etcd/raft/v3/raftpb"
+)
+
+// Entry contains data related to a raft log entry. This is the raftpb.Entry
+// itself but also all encapsulated data relevant for command application.
+type Entry struct {
+	raftpb.Entry // must be pre-populated when calling Load
+	// These fields will all be populated by Load.
+	ID                kvserverbase.CmdIDKey         // always set
+	ConfChangeV1      *raftpb.ConfChange            // only set for config change
+	ConfChangeV2      *raftpb.ConfChangeV2          // only set for config change
+	ConfChangeContext *kvserverpb.ConfChangeContext // only set for config change
+	Cmd               kvserverpb.RaftCommand        // always set
+}
+
+// MakeEntry populates an Entry from the provided raftpb.Entry.
+func MakeEntry(ent raftpb.Entry) (Entry, error) {
+	e := Entry{Entry: ent}
+	if err := e.load(); err != nil {
+		return Entry{}, err
+	}
+	return e, nil
+}
+
+// MakeEntryFromRawValue populates an Entry from a raw value, i.e. from bytes
+// as stored on a storage.Engine.
+func MakeEntryFromRawValue(b []byte) (Entry, error) {
+	var meta enginepb.MVCCMetadata
+	if err := protoutil.Unmarshal(b, &meta); err != nil {
+		return Entry{}, errors.Wrap(err, "decoding raft log MVCCMetadata")
+	}
+
+	var e Entry
+
+	if err := storage.MakeValue(meta).GetProto(&e.Entry); err != nil {
+		return Entry{}, errors.Wrap(err, "unmarshalling raft Entry")
+	}
+
+	err := e.load()
+	if err != nil {
+		return Entry{}, err
+	}
+	return e, nil
+}
+
+func (e *Entry) load() error {
+	if len(e.Data) == 0 {
+		// Raft-proposed empty entry.
+		return nil
+	}
+
+	var payload []byte
+	switch e.Type {
+	case raftpb.EntryNormal:
+		e.ID, payload = kvserverbase.DecodeRaftCommand(e.Data)
+	case raftpb.EntryConfChange:
+		e.ConfChangeV1 = &raftpb.ConfChange{}
+		if err := protoutil.Unmarshal(e.Data, e.ConfChangeV1); err != nil {
+			return errors.Wrap(err, "unmarshalling ConfChange")
+		}
+		e.ConfChangeContext = &kvserverpb.ConfChangeContext{}
+		if err := protoutil.Unmarshal(e.ConfChangeV1.Context, e.ConfChangeContext); err != nil {
+			return errors.Wrap(err, "unmarshalling ConfChangeContext")
+		}
+		payload = e.ConfChangeContext.Payload
+		e.ID = kvserverbase.CmdIDKey(e.ConfChangeContext.CommandID)
+	case raftpb.EntryConfChangeV2:
+		e.ConfChangeV2 = &raftpb.ConfChangeV2{}
+		if err := protoutil.Unmarshal(e.Data, e.ConfChangeV2); err != nil {
+			return errors.Wrap(err, "unmarshalling ConfChangeV2")
+		}
+		e.ConfChangeContext = &kvserverpb.ConfChangeContext{}
+		if err := protoutil.Unmarshal(e.ConfChangeV2.Context, e.ConfChangeContext); err != nil {
+			return errors.Wrap(err, "unmarshalling ConfChangeContext")
+		}
+		payload = e.ConfChangeContext.Payload
+		e.ID = kvserverbase.CmdIDKey(e.ConfChangeContext.CommandID)
+	default:
+		return errors.AssertionFailedf("unknown entry type %d", e.Type)
+	}
+
+	// TODO(tbg): can len(payload)==0 if we propose an empty command to wake up leader?
+	// If so, is that a problem here?
+	return errors.Wrap(protoutil.Unmarshal(payload, &e.Cmd), "unmarshalling RaftCommand")
+}
+
+// ConfChange returns ConfChangeV1 or ConfChangeV2 as an interface, if set.
+// Otherwise, returns nil.
+func (e *Entry) ConfChange() raftpb.ConfChangeI {
+	if e.ConfChangeV1 != nil {
+		return e.ConfChangeV1
+	}
+	if e.ConfChangeV2 != nil {
+		return e.ConfChangeV2
+	}
+	// NB: nil != interface{}(nil).
+	return nil
+}
