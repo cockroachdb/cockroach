538cdeda8b939079cc1294ef9019c8c6157fb33d
Jay <jay@cockroachlabs.com>
2022-02-06 14:41:41 -0500

    sql: add SHOW TRANSFER STATE observer statement
    Informs #76000.
    
    This commit adds the SHOW TRANSFER STATE observer statement, as described in
    the sqlproxy connection migration RFC. This observer statement will be used
    whenever a connection is about to be migrated to retrieve the relevant values
    needed for the transfer process. A unique aspect to this statement is that
    serialization or token generation errors will be returned as a SQL value
    instead of an ErrorResponse. This will allow the sqlproxy to react accordingly,
    and to reduce ambiguity issues during transferring.
    
    This observer statement will only work on tenants (due to the need of token
    generation). Since this is meant to be used internally only, there is no
    release note.
    
    Release note: None
    
    Co-authored-by: Jay <jay@cockroachlabs.com>
    Co-authored-by: Rafi Shamim <rafi@cockroachlabs.com>

--- pkg/sql/session_revival_token.go
+++ pkg/sql/session_revival_token.go
@@ -11,11 +11,13 @@
 package sql
 
 import (
+	"github.com/cockroachdb/cockroach/pkg/security"
 	"github.com/cockroachdb/cockroach/pkg/security/sessionrevival"
 	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgcode"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
 )
 
 // AllowSessionRevival is true if the cluster is allowed to create session
@@ -30,22 +32,41 @@ var AllowSessionRevival = settings.RegisterBoolSetting(
 	false,
 )
 
+// CreateSessionRevivalToken is a wrapper for createSessionRevivalToken, and
+// uses the planner.
 func (p *planner) CreateSessionRevivalToken() (*tree.DBytes, error) {
-	if !AllowSessionRevival.Get(&p.ExecCfg().Settings.SV) || p.ExecCfg().Codec.ForSystemTenant() {
-		return nil, pgerror.New(pgcode.FeatureNotSupported, "session revival tokens are not supported on this cluster")
-	}
-	// Note that we use SessionUser here and not CurrentUser, since when the
-	// token is used to create a new session, it should be for the user who was
-	// originally authenticated. (Whereas CurrentUser could be a different user
-	// if SET ROLE had been used.)
-	user := p.SessionData().SessionUser()
-	if user.IsRootUser() {
-		return nil, pgerror.New(pgcode.InsufficientPrivilege, "cannot create token for root user")
-	}
 	cm, err := p.ExecCfg().RPCContext.SecurityContext.GetCertificateManager()
 	if err != nil {
 		return nil, err
 	}
+	return createSessionRevivalToken(
+		AllowSessionRevival.Get(&p.ExecCfg().Settings.SV) && !p.ExecCfg().Codec.ForSystemTenant(),
+		p.SessionData(),
+		cm,
+	)
+}
+
+// createSessionRevivalToken creates a session revival token for the current
+// user.
+//
+// NOTE: This is used within an observer statement directly, and should not rely
+// on the planner because those statements do not get planned.
+func createSessionRevivalToken(
+	allowSessionRevival bool, sd *sessiondata.SessionData, cm *security.CertificateManager,
+) (*tree.DBytes, error) {
+	if !allowSessionRevival {
+		return nil, pgerror.New(pgcode.FeatureNotSupported, "session revival tokens are not supported on this cluster")
+	}
+
+	// Note that we use SessionUser here and not CurrentUser, since when the
+	// token is used to create a new session, it should be for the user who was
+	// originally authenticated. (Whereas CurrentUser could be a different user
+	// if SET ROLE had been used.)
+	user := sd.SessionUser()
+	if user.IsRootUser() {
+		return nil, pgerror.New(pgcode.InsufficientPrivilege, "cannot create token for root user")
+	}
+
 	tokenBytes, err := sessionrevival.CreateSessionRevivalToken(cm, user)
 	if err != nil {
 		return nil, err
@@ -53,6 +74,7 @@ func (p *planner) CreateSessionRevivalToken() (*tree.DBytes, error) {
 	return tree.NewDBytes(tree.DBytes(tokenBytes)), nil
 }
 
+// ValidateSessionRevivalToken validates a session revival token.
 func (p *planner) ValidateSessionRevivalToken(token *tree.DBytes) (*tree.DBool, error) {
 	if !AllowSessionRevival.Get(&p.ExecCfg().Settings.SV) || p.ExecCfg().Codec.ForSystemTenant() {
 		return nil, pgerror.New(pgcode.FeatureNotSupported, "session revival tokens are not supported on this cluster")
