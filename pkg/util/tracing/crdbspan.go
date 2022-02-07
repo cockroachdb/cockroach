8d16d74f4c1cd8b1ded427372fea66f06b143bcb
Andrei Matei <andrei@cockroachlabs.com>
2022-02-07 15:21:31 -0500

    util/tracing: set ParentSpanID on spans with remote parents
    We weren't setting the parent id on spans with remote parents. This
    patch fixes that, because it's the right thing to do and also because
    there were comments that implied that we did.
    There were no major consequences of the fact that we didn't set it
    because the importing of a remote recording was overwriting the parent
    id of the first importeed span.
    
    The next patch has a test for this.
    
    Release note: None

--- pkg/util/tracing/crdbspan.go
+++ pkg/util/tracing/crdbspan.go
@@ -35,8 +35,12 @@ type crdbSpan struct {
 	// sp is Span that this crdbSpan is part of.
 	sp *Span
 
-	traceID      tracingpb.TraceID // probabilistically unique
-	spanID       tracingpb.SpanID  // probabilistically unique
+	traceID tracingpb.TraceID // probabilistically unique
+	spanID  tracingpb.SpanID  // probabilistically unique
+	// parentSpanID indicates the parent at the time when this span was created. 0
+	// if this span didn't have a parent. If crdbSpan.mu.parent is set,
+	// parentSpanID corresponds to it. However, if the parent finishes, or if the
+	// parent is a span from a remote node, crdbSpan.mu.parent will be nil.
 	parentSpanID tracingpb.SpanID
 	operation    string // name of operation associated with the span
 
