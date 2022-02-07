d95bef7081792e01adda6e8e51597d7633f1fddf
Cameron Nunez <cameron@cockroachlabs.com>
2022-02-07 11:47:18 -0500

    server: enhance comments surrounding draining
    This patch improves the comments related to the draining process,
    fixing incorrect statements and providing more details to make
    the process more easily understood.
    
    Release note: None

--- pkg/server/admin.go
+++ pkg/server/admin.go
@@ -1806,7 +1806,7 @@ func (s *adminServer) checkReadinessForHealthCheck(ctx context.Context) error {
 		return status.Errorf(codes.Unavailable, "node is shutting down")
 	}
 
-	if !s.server.sqlServer.acceptingClients.Get() {
+	if !s.server.sqlServer.isReady.Get() {
 		return status.Errorf(codes.Unavailable, "node is not accepting SQL clients")
 	}
 
