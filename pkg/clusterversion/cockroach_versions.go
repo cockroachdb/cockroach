f7fea0675e69e920f24cb90f2dc2e494f44ffa91
Erik Grinaker <grinaker@cockroachlabs.com>
2022-02-07 21:07:01 +0000

    batcheval: add range tombstone support for `DeleteRange`
    This patch adds a parameter `UseExperimentalRangeTombstone` for
    `DeleteRange`, which deletes the span using an MVCC range tombstone,
    backed by an `ExperimentalDeleteRangeUsingTombstone` MVCC function. It
    also adds an `ExperimentalMVCCRangeTombstones` version gate which must
    be checked before using it.
    
    This is an experimental implementation to allow writing range keys via
    the KV API for testing and development purposes. It has significant
    shortcomings, and will be fleshed out at a later time.
    
    Release note: None

--- pkg/clusterversion/cockroach_versions.go
+++ pkg/clusterversion/cockroach_versions.go
@@ -358,6 +358,9 @@ const (
 	// version is guaranteed to reside in a cluster where all nodes support range
 	// keys at the Pebble layer.
 	EnablePebbleFormatVersionRangeKeys
+	// ExperimentalMVCCRangeTombstones enables the use of highly experimental MVCC
+	// range tombstones.
+	ExperimentalMVCCRangeTombstones
 
 	// *************************************************
 	// Step (1): Add new versions here.
@@ -626,6 +629,10 @@ var versionsSingleton = keyedVersions{
 	{
 		Key:     EnablePebbleFormatVersionRangeKeys,
 		Version: roachpb.Version{Major: 22, Minor: 1, Internal: 1002},
+	},
+	{
+		Key:     ExperimentalMVCCRangeTombstones,
+		Version: roachpb.Version{Major: 22, Minor: 1, Internal: 1004},
 	},
 }
 
