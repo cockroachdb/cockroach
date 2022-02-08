af146a83dd3172295ea2cebe9e300e226b9eb033
Mark Sirek <sirek@cockroachlabs.com>
2022-02-08 12:08:36 -0800

    opt: split filtered SELECT..ORDER BY..LIMIT into unioned SELECTs
    This commit adds a cost-based optimizer rewrite of queries of the form
    ```
    SELECT * FROM t1 WHERE filter_expression ORDER BY secondIndexColumn LIMIT n;
    ```
    where `filter_expression` is any boolean predicate on t1.
    secondIndexColumn is the 2nd column defined in an index on t1, and `n`
    is a positive integer. Equality or range predicates must exist on the
    first index column, either explicitly in `filter_expression`, or
    implicitly in a NOT NULL CHECK constraint, e.g. `index_col1 IN (1,2,3)`.
    Given this example CHECK constraint, the rewritten query is of the form:
    ```
    SELECT * FROM (
        SELECT * FROM t1 WHERE index_col1 = 1 AND filter_expression LIMIT n
        UNION ALL
        SELECT * FROM t1 WHERE index_col1 = 2 AND filter_expression LIMIT n
        UNION ALL
        SELECT * FROM t1 WHERE index_col1 = 3 AND filter_expression LIMIT n
    ) ORDER BY secondIndexColumn LIMIT n;
    ```
    The ORDER BY in the rewritten query ends up getting removed by another
    optimization which takes advantage of the execution engine's ability to
    stitch together the UNION ALL branch results in order.
    
    This commit builds statistics from NOT NULL CHECK constraints on virtual
    columns which are of the form `column IN (const_1, const_2, ...)`. This
    is needed because statistics cannot currently be created on virtual
    columns, and the above query rewrite may not occur without these stats.
    
    This commit also changes the structure of the UNION ALL tree built by
    this and similar rewrites from left-deep to balanced, providing order of
    magnitude speedup for cases which have high numbers of scans, for
    example, hash-sharded primary key tables with a large number of buckets.
    
    Fixes #75288
    
    Release note (performance improvement): Queries of the form
    `SELECT * FROM t1 WHERE filter_expression ORDER BY secondIndexColumn LIMIT n;`
    where there is a NOT NULL CHECK constraint of the form:
    `CHECK (firstIndexColumn IN (const_1, const_2, const_3...)`
    may now be rewritten as a UNION ALL `skip scan` to avoid the
    previously-required sort operation.

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -3042,6 +3042,10 @@ func (m *sessionDataMutator) SetQualityOfService(val sessiondatapb.QoSLevel) {
 	m.data.DefaultTxnQualityOfService = val.Validate()
 }
 
+func (m *sessionDataMutator) SetOptSplitScanLimit(val int32) {
+	m.data.OptSplitScanLimit = val
+}
+
 func (m *sessionDataMutator) SetStreamReplicationEnabled(val bool) {
 	m.data.EnableStreamReplication = val
 }
