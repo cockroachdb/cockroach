2fbe8d816e861cf0f3371d8580c89ab6fe38e5bc
Oliver Tan <otan@cockroachlabs.com>
2022-02-08 15:31:29 +1100

    sql: handle adding TTL to a table during an ALTER statement
    This commit handles adding a TTL to a table using
    `ALTER TABLE ... SET ...`.
    
    To accomplish this, we needed to make add column finalization and adding
    the TTL struct / scheduled job atomic. We add a new `ModifyRowLevelTTL`
    mutation which the same mutation ID, with approprate rollback handling
    if, e.g. ADD COLUMN succeeds but setting the row-level TTL fails.
    
    Release note: None

--- pkg/sql/catalog/tabledesc/mutation.go
+++ pkg/sql/catalog/tabledesc/mutation.go
@@ -158,6 +158,17 @@ func (c constraintToUpdate) GetConstraintID() descpb.ConstraintID {
 	panic("unknown constraint type")
 }
 
+// modifyRowLevelTTL implements the catalog.ModifyRowLevelTTL interface.
+type modifyRowLevelTTL struct {
+	maybeMutation
+	desc *descpb.ModifyRowLevelTTL
+}
+
+// RowLevelTTL contains the row level TTL config to add or remove.
+func (c modifyRowLevelTTL) RowLevelTTL() *descpb.TableDescriptor_RowLevelTTL {
+	return c.desc.RowLevelTTL
+}
+
 // primaryKeySwap implements the catalog.PrimaryKeySwap interface.
 type primaryKeySwap struct {
 	maybeMutation
@@ -292,13 +303,14 @@ func (c materializedViewRefresh) TableWithNewIndexes(
 // mutation implements the
 type mutation struct {
 	maybeMutation
-	column          catalog.Column
-	index           catalog.Index
-	constraint      catalog.ConstraintToUpdate
-	pkSwap          catalog.PrimaryKeySwap
-	ccSwap          catalog.ComputedColumnSwap
-	mvRefresh       catalog.MaterializedViewRefresh
-	mutationOrdinal int
+	column            catalog.Column
+	index             catalog.Index
+	constraint        catalog.ConstraintToUpdate
+	pkSwap            catalog.PrimaryKeySwap
+	ccSwap            catalog.ComputedColumnSwap
+	mvRefresh         catalog.MaterializedViewRefresh
+	modifyRowLevelTTL catalog.ModifyRowLevelTTL
+	mutationOrdinal   int
 }
 
 // AsColumn returns the corresponding Column if the mutation is on a column,
@@ -323,6 +335,12 @@ func (m mutation) AsConstraint() catalog.ConstraintToUpdate {
 // is a primary key swap, nil otherwise.
 func (m mutation) AsPrimaryKeySwap() catalog.PrimaryKeySwap {
 	return m.pkSwap
+}
+
+// AsModifyRowLevelTTL returns the corresponding ModifyRowLevelTTL if the
+// mutation is a computed column swap, nil otherwise.
+func (m mutation) AsModifyRowLevelTTL() catalog.ModifyRowLevelTTL {
+	return m.modifyRowLevelTTL
 }
 
 // AsComputedColumnSwap returns the corresponding ComputedColumnSwap if the
@@ -366,6 +384,7 @@ func newMutationCache(desc *descpb.TableDescriptor) *mutationCache {
 	var pkSwaps []primaryKeySwap
 	var ccSwaps []computedColumnSwap
 	var mvRefreshes []materializedViewRefresh
+	var modifyRowLevelTTLs []modifyRowLevelTTL
 	for i, m := range desc.Mutations {
 		mm := maybeMutation{
 			mutationID:         m.MutationID,
@@ -415,6 +434,12 @@ func newMutationCache(desc *descpb.TableDescriptor) *mutationCache {
 				desc:          pb,
 			})
 			backingStructs[i].mvRefresh = &mvRefreshes[len(mvRefreshes)-1]
+		} else if pb := m.GetModifyRowLevelTTL(); pb != nil {
+			modifyRowLevelTTLs = append(modifyRowLevelTTLs, modifyRowLevelTTL{
+				maybeMutation: mm,
+				desc:          pb,
+			})
+			backingStructs[i].modifyRowLevelTTL = &modifyRowLevelTTLs[len(modifyRowLevelTTLs)-1]
 		}
 	}
 	// Populate the c.all slice with Mutation interfaces.
