e8e33a3e43be3fdb518de93146c76317b2e0d44f
Rajiv Sharma <rajshar@fb.com>
2022-02-08 00:04:14 +0000

    opt: index accelerate && (overlaps) expressions for array inverted indexes.
    Previously, we did not support index acceleration for queries involving
    array overlaps (&&).
    
    This change adds support for using the inverted index with && expressions on
    Array columns. When there is an inverted index available, a scan will be done on
    the Array column using the spans found from the constant value.
    
    Release note (performance improvement): Expressions using the overlaps (&&)
    operator for arrays now support index-acceleration for faster execution in
    some cases.

--- pkg/sql/rowenc/index_encoding_test.go
+++ pkg/sql/rowenc/index_encoding_test.go
@@ -734,3 +734,128 @@ func getColumnTypes(columns []catalog.Column) ([]*types.T, error) {
 	}
 	return outTypes, nil
 }
+
+func TestEncodeOverlapsArrayInvertedIndexSpans(t *testing.T) {
+	testCases := []struct {
+		indexedValue string
+		value        string
+		ok           bool
+		expected     bool
+	}{
+
+		// This test uses EncodeInvertedIndexTableKeys and EncodeOverlapsInvertedIndexSpans
+		// to determine if the spans produced from the second Array value will
+		// correctly overlap or be distinct from the first value.
+
+		// The expression is a union of spans, so unique should never be true
+
+		// First we test that the spans will include expected value.
+		{`{1}`, `{1}`, true, true},
+		{`{1, 2}`, `{1}`, true, true},
+		{`{2}`, `{1, 2}`, true, true},
+		{`{2, NULL}`, `{1, 2}`, true, true},
+		{`{1, 2}`, `{1, 2}`, true, true},
+		{`{1, 3}`, `{1, 2}`, true, true},
+		{`{2}`, `{2, 2}`, true, true},
+		{`{1, 2}`, `{1, 2, 1}`, true, true},
+		{`{1, 1, 2, 3}`, `{1, 2, 1}`, true, true},
+		{`{1, 2, 4}`, `{1, 2, 3}`, true, true},
+		{`{2}`, `{2, NULL}`, true, true},
+		{`{2, 3}`, `{2, NULL}`, true, true},
+		{`{1, NULL}`, `{1, 2, NULL}`, true, true},
+
+		// Then we test that the spans exclude results that should be excluded.
+		{`{}`, `{}`, false, false},
+		{`{}`, `{1}`, true, false},
+		{`{1}`, `{}`, false, false},
+		{`{}`, `{1, 2}`, true, false},
+		{`{NULL}`, `{}`, false, false},
+		{`{}`, `{NULL}`, false, false},
+		{`{}`, `{NULL, NULL}`, false, false},
+		{`{2}`, `{1}`, true, false},
+		{`{4, 3}`, `{2, 1}`, true, false},
+		{`{5}`, `{1, 2, 1}`, true, false},
+		{`{NULL, 3}`, `{1, 2, 1}`, true, false},
+		{`{NULL}`, `{NULL}`, false, false},
+		{`{NULL}`, `{1, NULL}`, true, false},
+		{`{1,NULL}`, `{NULL}`, false, false},
+		{`{2, NULL}`, `{1, NULL}`, true, false},
+	}
+
+	evalCtx := tree.MakeTestingEvalContext(cluster.MakeTestingClusterSettings())
+	parseArray := func(s string) tree.Datum {
+		arr, _, err := tree.ParseDArrayFromString(&evalCtx, s, types.Int)
+		if err != nil {
+			t.Fatalf("Failed to parse array %s: %v", s, err)
+		}
+		return arr
+	}
+
+	runTest := func(indexedValue, value tree.Datum, expected, ok bool) {
+		keys, err := EncodeInvertedIndexTableKeys(indexedValue, nil, descpb.LatestNonPrimaryIndexDescriptorVersion)
+		require.NoError(t, err)
+
+		invertedExpr, err := EncodeOverlapsInvertedIndexSpans(&evalCtx, value)
+		require.NoError(t, err)
+
+		spanExpr, conversionOk := invertedExpr.(*inverted.SpanExpression)
+		if ok && !conversionOk {
+			t.Fatalf("For (%s, %s), Expr %v is not an InvertedExpression contrary to expectation", indexedValue, value, invertedExpr)
+		} else if !ok && conversionOk {
+			t.Fatalf("For (%s, %s), Expr %v is an InvertedExpression contrary to expectation", indexedValue, value, invertedExpr)
+		} else if !ok && !conversionOk {
+			return
+		}
+
+		// Array spans for && are always tight.
+		if spanExpr.Tight != true {
+			t.Errorf("For (%s, %s), expected tight=true, but got false", indexedValue, value)
+		}
+
+		// Array spans for && are never unique.
+		if spanExpr.Unique {
+			t.Errorf("For (%s, %s), expected unique=false, but got true", indexedValue, value)
+		}
+
+		// Check if the indexedValue is included by the spans (i.e. Overlaps).
+		overlaps, err := spanExpr.ContainsKeys(keys)
+		require.NoError(t, err)
+
+		if overlaps != expected {
+			if expected {
+				t.Errorf("Expected spans of %s to overlap with %s but they did not", value, indexedValue)
+			} else {
+				t.Errorf("Expected spans of %s to not overlap with %s but they did", value, indexedValue)
+			}
+		}
+	}
+
+	// Run pre-defined test cases from above.
+	for _, c := range testCases {
+		indexedValue, value := parseArray(c.indexedValue), parseArray(c.value)
+		runTest(indexedValue, value, c.expected, c.ok)
+	}
+
+	// Run a set of randomly generated test cases.
+	rng, _ := randutil.NewTestRand()
+	for i := 0; i < 100; i++ {
+		typ := randgen.RandArrayType(rng)
+
+		// Generate two random arrays and evaluate the result of `left && right`.
+		// Using 1/9th as the Null Chance to generate arrays with a small
+		// number of NULLs added in between.
+		left := randgen.RandArray(rng, typ, 9 /* nullChance */)
+		right := randgen.RandArray(rng, typ, 9 /* nullChance */)
+
+		overlaps, err := tree.ArrayOverlaps(&evalCtx, right.(*tree.DArray), left.(*tree.DArray))
+		require.NoError(t, err)
+
+		rightArr, _ := right.(*tree.DArray)
+		// An inverted expression can only be generated if the value array is
+		// non-empty or contains atleast one non-NULL element.
+		ok := rightArr.Len() > 0 && rightArr.HasNonNulls
+
+		// Now check that we get the same result with the inverted index spans.
+		runTest(left, right, bool(*overlaps), ok)
+	}
+}
