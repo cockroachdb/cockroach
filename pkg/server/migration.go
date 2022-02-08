9ac3779d66aae5076881eee3997d46b1f189852e
Jackson Owens <jackson@cockroachlabs.com>
2022-02-08 16:09:03 -0500

    server: remove DeprecateBaseEncryptionRegistry
    Remove the migration server DeprecateBaseEncryptionRegistry RPC. It was
    used for the migration of the encryption-at-rest registry format in
    21.2 and is no longer relevant.
    
    Missed this in #74314.
    
    Release note: None

--- pkg/server/migration.go
+++ pkg/server/migration.go
@@ -210,37 +210,6 @@ func (m *migrationServer) PurgeOutdatedReplicas(
 	return resp, nil
 }
 
-// TODO(ayang): remove this RPC and associated request/response in 22.1
-func (m *migrationServer) DeprecateBaseEncryptionRegistry(
-	ctx context.Context, req *serverpb.DeprecateBaseEncryptionRegistryRequest,
-) (*serverpb.DeprecateBaseEncryptionRegistryResponse, error) {
-	const opName = "deprecate-base-encryption-registry"
-	ctx, span := m.server.AnnotateCtxWithSpan(ctx, opName)
-	defer span.Finish()
-	ctx = logtags.AddTag(ctx, opName, nil)
-
-	if err := m.server.stopper.RunTaskWithErr(ctx, opName, func(
-		ctx context.Context,
-	) error {
-		// Same as in SyncAllEngines, because stores can be added asynchronously, we
-		// need to ensure that the bootstrap process has happened.
-		m.server.node.waitForAdditionalStoreInit()
-
-		for _, eng := range m.server.engines {
-			if err := eng.SetMinVersion(*req.Version); err != nil {
-				return err
-			}
-		}
-
-		return nil
-	}); err != nil {
-		return nil, err
-	}
-
-	resp := &serverpb.DeprecateBaseEncryptionRegistryResponse{}
-	return resp, nil
-}
-
 // WaitForSpanConfigSubscription implements the MigrationServer interface.
 func (m *migrationServer) WaitForSpanConfigSubscription(
 	ctx context.Context, _ *serverpb.WaitForSpanConfigSubscriptionRequest,
