60b80c4c2d886b38def04b18246861c22a2c3087
Aayush Shah <aayush.shah15@gmail.com>
2022-02-08 14:23:34 -0500

    kvserver: make Allocator methods return ReplicationTargets
    Previously they would return `ReplicaDescriptor`s, which was a vestige. These
    return values were almost immediately getting cast into `ReplicationTarget` in
    every single case anyway.This makes the return types of these allocator methods
    a little more consistent.
    
    Release note: None

--- pkg/kv/kvserver/replica_command.go
+++ pkg/kv/kvserver/replica_command.go
@@ -2900,7 +2900,7 @@ func (s *Store) relocateOne(
 		}
 		candidateStoreList := makeStoreList(candidateDescs)
 
-		targetStore, _ := s.allocator.allocateTargetFromList(
+		additionTarget, _ = s.allocator.allocateTargetFromList(
 			ctx,
 			candidateStoreList,
 			conf,
@@ -2913,16 +2913,11 @@ func (s *Store) relocateOne(
 			true, /* allowMultipleReplsPerNode */
 			args.targetType,
 		)
-		if targetStore == nil {
+		if roachpb.EmptyReplicationTarget(additionTarget) {
 			return nil, nil, fmt.Errorf(
 				"none of the remaining %ss %v are legal additions to %v",
 				args.targetType, args.targetsToAdd(), desc.Replicas(),
 			)
-		}
-
-		additionTarget = roachpb.ReplicationTarget{
-			NodeID:  targetStore.Node.NodeID,
-			StoreID: targetStore.StoreID,
 		}
 
 		// Pretend the new replica is already there so that the removal logic below
@@ -2976,7 +2971,7 @@ func (s *Store) relocateOne(
 		targetStore, _, err := s.allocator.removeTarget(
 			ctx,
 			conf,
-			s.allocator.storeListForCandidates(args.targetsToRemove()),
+			s.allocator.storeListForTargets(args.targetsToRemove()),
 			existingVoters,
 			existingNonVoters,
 			args.targetType,
