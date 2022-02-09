bd3a729150a4d32ed52c8795fad54611dd29457b
Oliver Tan <otan@cockroachlabs.com>
2022-02-09 11:24:13 +1100

    sql: make RESET (ttl) drop the TTL column
    This commit makes `ALTER TABLE ... RESET (ttl)` drop the automatic TTL
    column. To do this, the `DROP COLUMN` logic is extracted into a separate
    function, then used along with a `ModifyRowLevelTTL` mutation.
    
    An extra check is added to the schema changer to ensure we aren't
    re-adding a schedule that doesn't yet exist.
    
    Release note: None

--- pkg/sql/schema_changer_test.go
+++ pkg/sql/schema_changer_test.go
@@ -7379,6 +7379,14 @@ func TestTTLAutomaticColumnSchemaChangeFailures(t *testing.T) {
 	id STRING NOT NULL,
 	CONSTRAINT test_pkey PRIMARY KEY (id ASC)
 )`
+
+		createTTLTable = `CREATE DATABASE t;
+CREATE TABLE t.test (id TEXT PRIMARY KEY) WITH (ttl_expire_after = '10 hours');`
+		expectTTLTable = `CREATE TABLE public.test (
+	id STRING NOT NULL,
+	crdb_internal_expiration TIMESTAMPTZ NOT VISIBLE NOT NULL DEFAULT current_timestamp():::TIMESTAMPTZ + '10:00:00':::INTERVAL ON UPDATE current_timestamp():::TIMESTAMPTZ + '10:00:00':::INTERVAL,
+	CONSTRAINT test_pkey PRIMARY KEY (id ASC)
+) WITH (ttl = 'on', ttl_automatic_column = 'on', ttl_expire_after = '10:00:00':::INTERVAL)`
 	)
 
 	testCases := []struct {
@@ -7419,6 +7427,36 @@ func TestTTLAutomaticColumnSchemaChangeFailures(t *testing.T) {
 			expectedShowCreateTable: expectNonTTLTable,
 			expectSchedule:          false,
 		},
+		{
+			desc:         "error during ALTER TABLE ... RESET (ttl) during delete column mutation",
+			setup:        createTTLTable,
+			schemaChange: `ALTER TABLE t.test RESET (ttl)`,
+			knobs: &sql.SchemaChangerTestingKnobs{
+				RunBeforeBackfill: failFunc,
+			},
+			expectedShowCreateTable: expectTTLTable,
+			expectSchedule:          true,
+		},
+		{
+			desc:         "error during ALTER TABLE ... SET (ttl_expire_after ...) during modify row-level-ttl mutation",
+			setup:        createTTLTable,
+			schemaChange: `ALTER TABLE t.test RESET (ttl)`,
+			knobs: &sql.SchemaChangerTestingKnobs{
+				RunBeforeModifyRowLevelTTL: failFunc,
+			},
+			expectedShowCreateTable: expectTTLTable,
+			expectSchedule:          true,
+		},
+		{
+			desc:         "error during ALTER TABLE ... SET (ttl_expire_after ...) when tied to another mutation which fails",
+			setup:        createTTLTable,
+			schemaChange: `BEGIN; ALTER TABLE t.test RESET (ttl); CREATE INDEX test_idx ON t.test(id); COMMIT`,
+			knobs: &sql.SchemaChangerTestingKnobs{
+				RunBeforeIndexValidation: failFunc,
+			},
+			expectedShowCreateTable: expectTTLTable,
+			expectSchedule:          true,
+		},
 	}
 
 	for _, tc := range testCases {
@@ -7452,7 +7490,7 @@ func TestTTLAutomaticColumnSchemaChangeFailures(t *testing.T) {
 			)
 			if tc.expectSchedule {
 				require.NotNil(t, desc.GetRowLevelTTL())
-				require.Greater(t, desc.GetRowLevelTTL().ScheduleID, 0)
+				require.Greater(t, desc.GetRowLevelTTL().ScheduleID, int64(0))
 
 				// Ensure there is only one schedule and that it belongs to the table.
 				var numSchedules int
