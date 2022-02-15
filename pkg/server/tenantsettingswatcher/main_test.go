600670463289278b007ad9cd6479b77b223f4b88
Radu Berinde <radu@cockroachlabs.com>
2022-02-15 11:06:09 -0800

    tenantsettingswatcher: implement watcher
    This commit implements the tenantsettingswatcher, which monitors the
    tenant_settings table and allows callers to access the current
    overrides and listen for changes.
    
    For now, we hardcode the tenant_settings system table ID to 50 (which
    will be the case in fresh clusters). We will fix this in a follow-up
    change.
    
    Release note: None

--- pkg/server/tenantsettingswatcher/main_test.go
+++ pkg/server/tenantsettingswatcher/main_test.go
@@ -1,4 +1,4 @@
-// Copyright 2021 The Cockroach Authors.
+// Copyright 2022 The Cockroach Authors.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt.
@@ -8,7 +8,7 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-package systemconfigwatcher_test
+package tenantsettingswatcher_test
 
 import (
 	"os"
