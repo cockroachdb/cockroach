0766b7c4c49003e2e97ef35e9aafd98df33fcd87
Celia La <celia@cockroachlabs.com>
2022-02-16 13:39:11 -0800

    add BUILD.bazel

--- build/teamcity/internal/cockroach/release/process/post_blockers.sh
+++ build/teamcity/internal/cockroach/release/process/post_blockers.sh
@@ -8,6 +8,6 @@ source "$dir/teamcity-support.sh"  # For $root
 source "$dir/teamcity-bazel-support.sh"  # For run_bazel
 
 tc_start_block "Run Post Release Blockers Phase"
-BAZEL_SUPPORT_EXTRA_DOCKER_ARGS="-e DRY_RUN -e GITHUB_TOKEN -e RELEASE_SERIES -e SMTP_PASSWORD -e SMTP_USER" \
+BAZEL_SUPPORT_EXTRA_DOCKER_ARGS="-e DRY_RUN -e GITHUB_TOKEN -e METADATA_PUBLISHER_GOOGLE_CREDENTIALS_DEV -e METADATA_PUBLISHER_GOOGLE_CREDENTIALS_PROD -e RELEASE_SERIES -e SMTP_PASSWORD -e SMTP_USER" \
   run_bazel build/teamcity/internal/cockroach/release/process/post_blockers_impl.sh
 tc_end_block "Run Post Release Blockers Phase"
