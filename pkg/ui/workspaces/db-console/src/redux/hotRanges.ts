7ced061669110991d4f269e9cd5fc7e322b33051
Andrii Vorobiov <and.vorobiov@gmail.com>
2022-02-16 17:46:59 +0200

    ui: use paginated hot ranges data
    Initially, HotRanges view relied on data that
    was fetched with single request that might
    cause performance issues on nodes.
    
    With this change, `CachedDataReducerWithPagination`
    is used to fetch hot ranges with server side
    pagination.
    
    Release note: None

--- pkg/ui/workspaces/db-console/src/redux/hotRanges.ts
+++ pkg/ui/workspaces/db-console/src/redux/hotRanges.ts
@@ -1,12 +1,56 @@
 import { AdminUIState } from "src/redux/state";
+import { createSelector } from "reselect";
+import { orderBy } from "lodash";
 
-export const hotRangesSelector = (state: AdminUIState) =>
-  Object.values(state.cachedData.hotRanges)
-    .filter(
-      v => v.valid && !v.inFlight && !v.lastError && v.data?.ranges.length > 0,
-    )
+const hotRangesState = (state: AdminUIState) =>
+  Object.values(state.cachedData.hotRanges);
+
+const validHotRanges = createSelector(hotRangesState, hotRanges =>
+  hotRanges.filter(v => v.valid && !v.inFlight),
+);
+
+export const hotRangesSelector = createSelector(validHotRanges, hotRanges =>
+  hotRanges
+    .filter(v => v.data?.ranges.length > 0)
     .map(v => v.data.ranges)
     .reduce((acc, v) => {
       acc.push(...v);
       return acc;
-    }, []);
+    }, []),
+);
+
+export const pagesWithErrorSelector = createSelector(
+  validHotRanges,
+  hotRanges => hotRanges.filter(v => !!v.lastError),
+);
+
+export const lastErrorSelector = createSelector(
+  pagesWithErrorSelector,
+  hotRanges => {
+    const orderedPages = orderBy(hotRanges, ["setAt"], ["desc"]);
+    if (orderedPages.length > 0) {
+      return orderedPages[0].lastError;
+    }
+    return null;
+  },
+);
+
+export const isValidSelector = createSelector(hotRangesState, hotRanges =>
+  hotRanges.every(v => v.valid),
+);
+
+export const lastSetAtSelector = createSelector(
+  pagesWithErrorSelector,
+  hotRanges => {
+    const orderedPages = orderBy(hotRanges, ["setAt"], ["desc"]);
+    if (orderedPages.length > 0) {
+      return orderedPages[0].setAt;
+    }
+    return null;
+  },
+);
+
+export const isLoadingSelector = createSelector(hotRangesState, hotRanges =>
+  hotRanges.some(v => v.inFlight),
+);
+
