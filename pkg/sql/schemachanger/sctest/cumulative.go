7c7822312acba84b30e4b1dcaf4898546bd82ae3
Andrew Werner <awerner32@gmail.com>
2022-02-16 17:50:22 -0500

    schemachanger,backupccl: support for backup and restore mid-change
    This commit touches the schema changer to encode some more metadata about the
    state of the schema change into the descriptors in order to better facilitate
    synthesizing a job at restore time.
    
    The major contribution, then, is to support the declarative schema changer in
    restore. Along the way, it picks up some catalog niceties like batches
    descriptor retreival and the nstree.Catalog.
    
    Finally, the code is tested with a suite of datadriven tests which run schema
    changes and then take the state and BACKUP/RESTORE it at each of the possible
    stages (including all of the rollback stages) and makes sure that the right
    outcome ultimately happens.
    
    Note that this is currently just tested for database-level BACKUP/RESTORE
    and that more testing is planned as follow-up.
    
    Release note: None

--- pkg/sql/schemachanger/sctest/cumulative.go
+++ pkg/sql/schemachanger/sctest/cumulative.go
@@ -23,15 +23,20 @@ import (
 
 	"github.com/cockroachdb/cockroach-go/v2/crdb"
 	"github.com/cockroachdb/cockroach/pkg/jobs"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
 	"github.com/cockroachdb/cockroach/pkg/sql/parser"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scop"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scplan"
+	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/screl"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scrun"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/datadriven"
-	"github.com/pkg/errors"
+	"github.com/cockroachdb/errors"
+	"github.com/lib/pq"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
 )
 
 // cumulativeTest is a foundational helper for building tests over the
@@ -39,8 +44,10 @@ import (
 // the passed function for each test directive in the file. The setup
 // statements passed to the function will be all statements from all
 // previous test and setup blocks combined.
-func cumulativeTest(t *testing.T, tf func(t *testing.T, setup, stmts []parser.Statement)) {
-	datadriven.Walk(t, testutils.TestDataPath(t), func(t *testing.T, path string) {
+func cumulativeTest(
+	t *testing.T, dir string, tf func(t *testing.T, setup, stmts []parser.Statement),
+) {
+	datadriven.Walk(t, dir, func(t *testing.T, path string) {
 		var setup []parser.Statement
 
 		datadriven.RunTest(t, path, func(t *testing.T, d *datadriven.TestData) string {
@@ -68,18 +75,23 @@ func cumulativeTest(t *testing.T, tf func(t *testing.T, setup, stmts []parser.St
 	})
 }
 
+// TODO(ajwerner): For all the non-rollback variants, we'd really actually
+// like them to run over each of the rollback stages too.
+
 // Rollback tests that the schema changer job rolls back properly.
 // This data-driven test uses the same input as EndToEndSideEffects
 // but ignores the expected output.
-func Rollback(t *testing.T, newCluster NewClusterFunc) {
+func Rollback(t *testing.T, dir string, newCluster NewClusterFunc) {
 	countRevertiblePostCommitStages := func(
 		t *testing.T, setup, stmts []parser.Statement,
 	) (n int) {
-		processPlanInPhase(t, newCluster, setup, stmts, scop.PostCommitPhase, func(
-			p scplan.Plan,
-		) {
-			n = len(p.StagesForCurrentPhase())
-		})
+		processPlanInPhase(
+			t, newCluster, setup, stmts, scop.PostCommitPhase,
+			func(p scplan.Plan) { n = len(p.StagesForCurrentPhase()) },
+			func(db *gosql.DB) {
+
+			},
+		)
 		return n
 	}
 	var testRollbackCase func(
@@ -101,18 +113,6 @@ func Rollback(t *testing.T, newCluster NewClusterFunc) {
 			}
 		}
 	}
-
-	const fetchDescriptorStateQuery = `
-SELECT
-	create_statement
-FROM
-	( 
-		SELECT descriptor_id, create_statement FROM crdb_internal.create_schema_statements
-		UNION ALL SELECT descriptor_id, create_statement FROM crdb_internal.create_statements
-		UNION ALL SELECT descriptor_id, create_statement FROM crdb_internal.create_type_statements
-	)
-ORDER BY
-	create_statement;`
 
 	testRollbackCase = func(
 		t *testing.T, setup, stmts []parser.Statement, ord int,
@@ -140,10 +140,6 @@ ORDER BY
 		beforeFunc := func() {
 			before = tdb.QueryStr(t, fetchDescriptorStateQuery)
 		}
-		resetTxnState := func() {
-			// Reset the counter in case the transaction is retried for whatever reason.
-			atomic.StoreUint32(&numInjectedFailures, 0)
-		}
 		onError := func(err error) error {
 			// If the statement execution failed, then we expect to end up in the same
 			// state as when we started.
@@ -151,7 +147,7 @@ ORDER BY
 			return err
 		}
 		err := executeSchemaChangeTxn(
-			context.Background(), t, setup, stmts, db, beforeFunc, resetTxnState, onError,
+			context.Background(), t, setup, stmts, db, beforeFunc, nil, onError,
 		)
 		if atomic.LoadUint32(&numInjectedFailures) == 0 {
 			require.NoError(t, err)
@@ -159,13 +155,27 @@ ORDER BY
 			require.Regexp(t, fmt.Sprintf("boom %d", ord), err)
 		}
 	}
-	cumulativeTest(t, testFunc)
+	cumulativeTest(t, dir, testFunc)
 }
+
+const fetchDescriptorStateQuery = `
+SELECT
+	create_statement
+FROM
+	( 
+		SELECT descriptor_id, create_statement FROM crdb_internal.create_schema_statements
+		UNION ALL SELECT descriptor_id, create_statement FROM crdb_internal.create_statements
+		UNION ALL SELECT descriptor_id, create_statement FROM crdb_internal.create_type_statements
+	)
+WHERE descriptor_id IN (SELECT id FROM system.namespace)
+ORDER BY
+	create_statement;`
 
 // Pause tests that the schema changer can handle being paused and resumed
 // correctly. This data-driven test uses the same input as EndToEndSideEffects
 // but ignores the expected output.
-func Pause(t *testing.T, newCluster NewClusterFunc) {
+func Pause(t *testing.T, dir string, newCluster NewClusterFunc) {
+	skip.UnderRace(t)
 	var postCommit, nonRevertible int
 	countStages := func(
 		t *testing.T, setup, stmts []parser.Statement,
@@ -175,7 +185,7 @@ func Pause(t *testing.T, newCluster NewClusterFunc) {
 		) {
 			postCommit = len(p.StagesForCurrentPhase())
 			nonRevertible = len(p.Stages) - postCommit
-		})
+		}, nil)
 	}
 	var testPauseCase func(
 		t *testing.T, setup, stmts []parser.Statement, ord int,
@@ -190,7 +200,7 @@ func Pause(t *testing.T, newCluster NewClusterFunc) {
 		t.Logf("test case has %d revertible post-commit stages", n)
 		for i := 1; i <= n; i++ {
 			if !t.Run(
-				fmt.Sprintf("rollback stage %d of %d", i, n),
+				fmt.Sprintf("pause stage %d of %d", i, n),
 				func(t *testing.T) { testPauseCase(t, setup, stmts, i) },
 			) {
 				return
@@ -199,10 +209,10 @@ func Pause(t *testing.T, newCluster NewClusterFunc) {
 	}
 	testPauseCase = func(t *testing.T, setup, stmts []parser.Statement, ord int) {
 		var numInjectedFailures uint32
-		resetTxnState := func() {
-			// Reset the counter in case the transaction is retried for whatever reason.
-			atomic.StoreUint32(&numInjectedFailures, 0)
-		}
+		// TODO(ajwerner): It'd be nice to assert something about the number of
+		// remaining stages before the pause and then after. It's not totally
+		// trivial, as we don't checkpoint during non-mutation stages, so we'd
+		// need to look back and find the last mutation phase.
 		db, cleanup := newCluster(t, &scrun.TestingKnobs{
 			BeforeStage: func(p scplan.Plan, stageIdx int) error {
 				if atomic.LoadUint32(&numInjectedFailures) > 0 {
@@ -222,8 +232,9 @@ func Pause(t *testing.T, newCluster NewClusterFunc) {
 		onError := func(err error) error {
 			// Check that it's a pause error, with a job.
 			// Resume the job and wait for the job.
-			//require.NoError(t, err)
-			re := regexp.MustCompile(`job (\d+) was paused before it completed with reason: boom (\d+)`)
+			re := regexp.MustCompile(
+				`job (\d+) was paused before it completed with reason: boom (\d+)`,
+			)
 			match := re.FindStringSubmatch(err.Error())
 			require.NotNil(t, match)
 			idx, err := strconv.Atoi(match[2])
@@ -239,10 +250,247 @@ func Pause(t *testing.T, newCluster NewClusterFunc) {
 			return nil
 		}
 		require.NoError(t, executeSchemaChangeTxn(
-			context.Background(), t, setup, stmts, db, nil, resetTxnState, onError,
+			context.Background(), t, setup, stmts, db, nil, nil, onError,
 		))
+		require.Equal(t, uint32(1), atomic.LoadUint32(&numInjectedFailures))
 	}
-	cumulativeTest(t, testFunc)
+	cumulativeTest(t, dir, testFunc)
+}
+
+// Backup tests that the schema changer can handle being backed up and
+// restored correctly. This data-driven test uses the same input as
+// EndToEndSideEffects but ignores the expected output. Note that the
+// cluster constructor needs to provide a cluster with CCL BACKUP/RESTORE
+// functionality enabled.
+func Backup(t *testing.T, dir string, newCluster NewClusterFunc) {
+	skip.UnderRace(t)
+	skip.UnderStress(t)
+	var after [][]string
+	var dbName string
+	countStages := func(
+		t *testing.T, setup, stmts []parser.Statement,
+	) (postCommit, nonRevertible int) {
+		var pl scplan.Plan
+		processPlanInPhase(t, newCluster, setup, stmts, scop.PostCommitPhase,
+			func(p scplan.Plan) {
+				pl = p
+				postCommit = len(p.StagesForCurrentPhase())
+				nonRevertible = len(p.Stages) - postCommit
+			}, func(db *gosql.DB) {
+				tdb := sqlutils.MakeSQLRunner(db)
+				var ok bool
+				dbName, ok = maybeGetDatabaseForIDs(t, tdb, screl.GetDescIDs(pl.TargetState))
+				if ok {
+					tdb.Exec(t, fmt.Sprintf("USE %q", dbName))
+				}
+				after = tdb.QueryStr(t, fetchDescriptorStateQuery)
+			})
+		return postCommit, nonRevertible
+	}
+	var testBackupRestoreCase func(
+		t *testing.T, setup, stmts []parser.Statement, ord int,
+	)
+	testFunc := func(t *testing.T, setup, stmts []parser.Statement) {
+		postCommit, nonRevertible := countStages(t, setup, stmts)
+		if nonRevertible > 0 {
+			postCommit++
+		}
+		n := postCommit
+		t.Logf("test case has %d revertible post-commit stages", n)
+		for i := 1; i <= n; i++ {
+			if !t.Run(
+				fmt.Sprintf("backup/restore stage %d of %d", i, n),
+				func(t *testing.T) { testBackupRestoreCase(t, setup, stmts, i) },
+			) {
+				return
+			}
+		}
+	}
+	type stage struct {
+		p        scplan.Plan
+		stageIdx int
+		resume   chan error
+	}
+	mkStage := func(p scplan.Plan, stageIdx int) stage {
+		return stage{p: p, stageIdx: stageIdx, resume: make(chan error)}
+	}
+	testBackupRestoreCase = func(
+		t *testing.T, setup, stmts []parser.Statement, ord int,
+	) {
+		stageChan := make(chan stage)
+		ctx, cancel := context.WithCancel(context.Background())
+		db, cleanup := newCluster(t, &scrun.TestingKnobs{
+			BeforeStage: func(p scplan.Plan, stageIdx int) error {
+				if p.Stages[stageIdx].Phase < scop.PostCommitPhase {
+					return nil
+				}
+				if stageChan != nil {
+					s := mkStage(p, stageIdx)
+					select {
+					case stageChan <- s:
+					case <-ctx.Done():
+						return ctx.Err()
+					}
+					select {
+					case err := <-s.resume:
+						return err
+					case <-ctx.Done():
+						return ctx.Err()
+					}
+				}
+				return nil
+			},
+		})
+
+		// Start with full database backup/restore.
+		defer cleanup()
+		defer cancel()
+
+		conn, err := db.Conn(ctx)
+		require.NoError(t, err)
+		tdb := sqlutils.MakeSQLRunner(conn)
+		tdb.Exec(t, "create database backups")
+		var g errgroup.Group
+		var before [][]string
+		beforeFunc := func() {
+			tdb.Exec(t, fmt.Sprintf("USE %q", dbName))
+			before = tdb.QueryStr(t, fetchDescriptorStateQuery)
+		}
+		g.Go(func() error {
+			return executeSchemaChangeTxn(
+				context.Background(), t, setup, stmts, db, beforeFunc, nil, nil,
+			)
+		})
+		type backup struct {
+			name       string
+			isRollback bool
+			url        string
+			s          stage
+		}
+		var backups []backup
+		var done bool
+		var rollbackStage int
+		for i := 0; !done; i++ {
+			// We want to let the stages up to ord continue unscathed. Then, we'll
+			// start taking backups at ord. If ord corresponds to a revertible
+			// stage, we'll inject an error, forcing the schema change to revert.
+			// At each subsequent stage, we also take a backup. At the very end,
+			// we'll have one backup where things should succeed and N backups
+			// where we're reverting. In each case, we want to have the end state
+			// of the restored set of descriptors match what we have in the original
+			// cluster.
+			//
+			// Lastly, we'll hit an ord corresponding to the first non-revertible
+			// stage. At this point, we'll take a backup for each non-revertible
+			// stage and confirm that restoring them and letting the jobs run
+			// leaves the database in the right state.
+			s := <-stageChan
+			shouldFail := ord == i &&
+				s.p.Stages[s.stageIdx].Phase != scop.PostCommitNonRevertiblePhase &&
+				!s.p.InRollback
+			done = len(s.p.Stages) == s.stageIdx+1 && !shouldFail
+			t.Logf("stage %d/%d in %v (rollback=%v) %d %q %v",
+				s.stageIdx+1, len(s.p.Stages), s.p.Stages[s.stageIdx].Phase, s.p.InRollback, ord, dbName, done)
+
+			// If the database has been dropped, there is nothing for
+			// us to do here.
+			var exists bool
+			tdb.QueryRow(t,
+				`SELECT count(*) > 0 FROM system.namespace WHERE "parentID" = 0 AND name = $1`,
+				dbName).Scan(&exists)
+			if !exists || (i < ord && !done) {
+				close(s.resume)
+				continue
+			}
+
+			// This test assumes that all the descriptors being modified in the
+			// transaction are in the same database.
+			//
+			// TODO(ajwerner): Deal with trying to restore just some of the tables.
+			backupURL := fmt.Sprintf("userfile://backups.public.userfiles_$user/data%d", i)
+			tdb.Exec(t, fmt.Sprintf(
+				"BACKUP DATABASE %s INTO '%s'", dbName, backupURL))
+			backups = append(backups, backup{
+				name:       dbName,
+				isRollback: rollbackStage > 0,
+				url:        backupURL,
+				s:          s,
+			})
+
+			if s.p.InRollback {
+				rollbackStage++
+			}
+			if shouldFail {
+				s.resume <- errors.Newf("boom %d", i)
+			} else {
+				close(s.resume)
+			}
+		}
+		if err := g.Wait(); rollbackStage > 0 {
+			require.Regexp(t, fmt.Sprintf("boom %d", ord), err)
+		} else {
+			require.NoError(t, err)
+		}
+		stageChan = nil // allow the restored jobs to proceed
+		t.Logf("finished")
+
+		for i, b := range backups {
+			t.Run("", func(t *testing.T) {
+				t.Logf("testing backup %d %v", i, b.isRollback)
+				tdb.Exec(t, fmt.Sprintf("DROP DATABASE IF EXISTS %q CASCADE", dbName))
+				tdb.Exec(t, "SET experimental_use_new_schema_changer = 'off'")
+				tdb.Exec(t, fmt.Sprintf("RESTORE DATABASE %s FROM LATEST IN '%s'", dbName, b.url))
+				tdb.Exec(t, fmt.Sprintf("USE %q", dbName))
+				waitForSchemaChangesToFinish(t, tdb)
+				afterRestore := tdb.QueryStr(t, fetchDescriptorStateQuery)
+				if b.isRollback {
+					require.Equal(t, before, afterRestore)
+				} else {
+					require.Equal(t, after, afterRestore)
+				}
+				// Hack to deal with corrupt userfiles tables due to #76764.
+				const validateQuery = `
+SELECT * FROM crdb_internal.invalid_objects WHERE database_name != 'backups'
+`
+				tdb.CheckQueryResults(t, validateQuery, [][]string{})
+				tdb.Exec(t, fmt.Sprintf("DROP DATABASE %q CASCADE", dbName))
+				tdb.Exec(t, "USE backups")
+				tdb.CheckQueryResults(t, validateQuery, [][]string{})
+			})
+		}
+	}
+	cumulativeTest(t, dir, testFunc)
+}
+
+func maybeGetDatabaseForIDs(
+	t *testing.T, tdb *sqlutils.SQLRunner, ids catalog.DescriptorIDSet,
+) (dbName string, exists bool) {
+	err := tdb.DB.QueryRowContext(context.Background(), `
+SELECT name
+  FROM system.namespace
+ WHERE id
+       IN (
+            SELECT DISTINCT
+                   COALESCE(
+                    d->'database'->>'id',
+                    d->'schema'->>'parentId',
+                    d->'type'->>'parentId',
+                    d->'table'->>'parentId'
+                   )::INT8
+              FROM (
+                    SELECT crdb_internal.pb_to_json('desc', descriptor) AS d
+                      FROM system.descriptor
+                     WHERE id IN (SELECT * FROM ROWS FROM (unnest($1::INT8[])))
+                   )
+        )
+`, pq.Array(ids.Ordered())).
+		Scan(&dbName)
+	if errors.Is(err, gosql.ErrNoRows) {
+		return "", false
+	}
+
+	require.NoError(t, err)
+	return dbName, true
 }
 
 // processPlanInPhase will call processFunc with the plan as of the first
@@ -253,6 +501,7 @@ func processPlanInPhase(
 	setup, stmt []parser.Statement,
 	phaseToProcess scop.Phase,
 	processFunc func(p scplan.Plan),
+	after func(db *gosql.DB),
 ) {
 	var processOnce sync.Once
 	db, cleanup := newCluster(t, &scrun.TestingKnobs{
@@ -267,6 +516,9 @@ func processPlanInPhase(
 	require.NoError(t, executeSchemaChangeTxn(
 		context.Background(), t, setup, stmt, db, nil, nil, nil,
 	))
+	if after != nil {
+		after(db)
+	}
 }
 
 // executeSchemaChangeTxn spins up a test cluster, executes the setup
@@ -291,7 +543,7 @@ func executeSchemaChangeTxn(
 	for _, stmt := range setup {
 		tdb.Exec(t, stmt.SQL)
 	}
-	waitForSchemaChangesToComplete(t, tdb)
+	waitForSchemaChangesToSucceed(t, tdb)
 	if before != nil {
 		before()
 	}
@@ -299,11 +551,34 @@ func executeSchemaChangeTxn(
 	// Execute the tested statements with the declarative schema changer and fail
 	// the test if it all takes too long. This prevents the test suite from
 	// hanging when a regression is introduced.
-	tdb.Exec(t, "SET experimental_use_new_schema_changer = 'unsafe_always'")
 	{
 		c := make(chan error, 1)
 		go func() {
-			c <- crdb.ExecuteTx(ctx, db, nil, func(tx *gosql.Tx) error {
+			conn, err := db.Conn(ctx)
+			if err != nil {
+				c <- err
+				return
+			}
+			defer func() { _ = conn.Close() }()
+			c <- crdb.Execute(func() (err error) {
+				_, err = conn.ExecContext(
+					ctx, "SET experimental_use_new_schema_changer = 'unsafe_always'",
+				)
+				if err != nil {
+					return err
+				}
+				var tx *gosql.Tx
+				tx, err = conn.BeginTx(ctx, nil)
+				if err != nil {
+					return err
+				}
+				defer func() {
+					if err != nil {
+						err = errors.WithSecondaryError(err, tx.Rollback())
+					} else {
+						err = tx.Commit()
+					}
+				}()
 				if txnStartCallback != nil {
 					txnStartCallback()
 				}
@@ -334,6 +609,6 @@ func executeSchemaChangeTxn(
 	}
 
 	// Ensure we're really done here.
-	waitForSchemaChangesToComplete(t, tdb)
+	waitForSchemaChangesToSucceed(t, tdb)
 	return nil
 }
