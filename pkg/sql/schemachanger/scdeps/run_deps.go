64abaf28edb74345d1b7e7a2acc121c00f0236de
Faizan Qazi <faizan@cockroachlabs.com>
2022-02-16 22:03:20 -0500

    schemachanger: add support for KV tracing
    Previously, the declarative schema changer did not
    support KV tracing. This was inadequate because the
    legacy schema changer supported this functionality
    which is needed for supportability of the product.
    To address this, this patch will add support for
    KV tracing inside the declarative schema changer.
    
    Release note: None

--- pkg/sql/schemachanger/scdeps/run_deps.go
+++ pkg/sql/schemachanger/scdeps/run_deps.go
@@ -43,6 +43,7 @@ func NewJobRunDependencies(
 	testingKnobs *scrun.TestingKnobs,
 	statements []string,
 	sessionData *sessiondata.SessionData,
+	kvTrace bool,
 ) scrun.JobRunDependencies {
 	return &jobExecutionDeps{
 		collectionFactory:     collectionFactory,
@@ -60,6 +61,7 @@ func NewJobRunDependencies(
 		indexValidator:        indexValidator,
 		commentUpdaterFactory: commentUpdaterFactory,
 		sessionData:           sessionData,
+		kvTrace:               kvTrace,
 	}
 }
 
@@ -73,6 +75,7 @@ type jobExecutionDeps struct {
 	rangeCounter          RangeCounter
 	jobRegistry           *jobs.Registry
 	job                   *jobs.Job
+	kvTrace               bool
 
 	indexValidator scexec.IndexValidator
 
@@ -105,6 +108,7 @@ func (d *jobExecutionDeps) WithTxnInJob(ctx context.Context, fn scrun.JobTxnFunc
 				indexValidator:     d.indexValidator,
 				eventLogger:        d.eventLoggerFactory(txn),
 				schemaChangerJobID: d.job.ID(),
+				kvTrace:            d.kvTrace,
 			},
 			backfiller: d.backfiller,
 			backfillTracker: newBackfillTracker(d.codec,
