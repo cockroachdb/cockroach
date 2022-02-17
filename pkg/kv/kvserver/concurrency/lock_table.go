3fef904c6e119198bec6a7f4c80e68936e1955e9
Alex Sarkesian <sarkesian@cockroachlabs.com>
2022-02-16 20:18:49 -0500

    kv, kvserver: add query locks command
    This change adds a new KV API, `QueryLocks`, allowing a client to query
    the state of a replica's lock table over a provided key span.  This
    exposes the in-memory locks currently managed by Concurrency Control,
    and is part of the work needed to implement #75541.
    
    Resolves #74833.
    
    Release note: None
    
    Release justification: Low-risk new functionality.

--- pkg/kv/kvserver/concurrency/lock_table.go
+++ pkg/kv/kvserver/concurrency/lock_table.go
@@ -2909,6 +2909,7 @@ func (t *lockTableImpl) QueryLockTableState(
 		resumeState.ResumeNextBytes = nextByteSize
 		resumeState.ResumeSpan = &roachpb.Span{Key: nextKey, EndKey: span.EndKey}
 	}
+	resumeState.TotalBytes = numBytes
 
 	return lockTableState, resumeState
 }
