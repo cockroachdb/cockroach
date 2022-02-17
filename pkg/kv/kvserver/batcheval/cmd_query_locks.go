49d856658356b36e54f3c3105ce8ce37ac1e0736
Alex Sarkesian <sarkesian@cockroachlabs.com>
2022-02-16 20:18:49 -0500

    kv, kvserver: add query locks command
    This change adds a new KV API, `QueryLocks`, allowing a client to query
    the state of a replica's lock table over a provided key span.  This
    exposes the in-memory locks currently managed by Concurrency Control,
    and is part of the work needed to implement #75541.
    
    Resolves #74833.
    
    Release note: None
    
    Release justification: Low-risk new funtionality.

---
+++ pkg/kv/kvserver/batcheval/cmd_query_locks.go
@@ -0,0 +1,71 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package batcheval
+
+import (
+	"context"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/batcheval/result"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/concurrency"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/spanset"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/storage"
+)
+
+func init() {
+	RegisterReadOnlyCommand(roachpb.QueryLocks, declareKeysQueryLocks, QueryLocks)
+}
+
+func declareKeysQueryLocks(
+	rs ImmutableRangeState,
+	_ *roachpb.Header,
+	_ roachpb.Request,
+	latchSpans, _ *spanset.SpanSet,
+	_ time.Duration,
+) {
+	// Latch on the range descriptor during evaluation of query locks.
+	latchSpans.AddNonMVCC(spanset.SpanReadOnly, roachpb.Span{Key: keys.RangeDescriptorKey(rs.GetStartKey())})
+}
+
+func QueryLocks(
+	ctx context.Context, reader storage.Reader, cArgs CommandArgs, resp roachpb.Response,
+) (result.Result, error) {
+	args := cArgs.Args.(*roachpb.QueryLocksRequest)
+	h := cArgs.Header
+	reply := resp.(*roachpb.QueryLocksResponse)
+
+	concurrencyManager := cArgs.EvalCtx.GetConcurrencyManager()
+	keyScope := spanset.SpanGlobal
+	if keys.IsLocal(args.Key) {
+		keyScope = spanset.SpanLocal
+	}
+	opts := concurrency.QueryLockTableOptions{
+		KeyScope:           keyScope,
+		MaxLocks:           h.MaxSpanRequestKeys,
+		TargetBytes:        h.TargetBytes,
+		IncludeUncontended: args.IncludeUncontended,
+	}
+
+	// Collect all LockStateInfo objects from the requested key span, up to the
+	// target byte and max key limits specified in the request header.
+	lockInfos, resumeState := concurrencyManager.QueryLockTableState(ctx, args.Span(), opts)
+
+	// Set the results along with any resume reason/span for the client to
+	// continue where this request met its limits.
+	reply.Locks = lockInfos
+	reply.ResumeReason = resumeState.ResumeReason
+	reply.ResumeSpan = resumeState.ResumeSpan
+	reply.ResumeNextBytes = resumeState.ResumeNextBytes
+
+	return result.Result{}, nil
+}
