9ecc61993311296d370786e975eda63cc44fdf93
Nick Travers <travers@cockroachlabs.com>
2022-02-18 07:47:47 -0800

    clusterversion,storage: support Pebblev2 table format
    The `Pebblev2` SSTable format adds support for range keys. Add two new
    cluster versions to provide the upgrade path - the first version for
    bumping the store, the second for use as a feature gate.
    
    Rework the table format inference for new SSTable writers to take a more
    conservative approach. By default, assume version `RocksDBv2`, and only
    bump up to a newer version if a cluster supports it. Previously a newer
    version was assumed and bumped down if the cluster didn't support it.
    
    Update `storage.MakeBackupSSTWriter` to take in a context and cluster
    settings which can be used to infer which table version to use. Enable
    range keys for backups and ingest, if the cluster supports it.
    
    Release note: None

--- pkg/clusterversion/cockroach_versions.go
+++ pkg/clusterversion/cockroach_versions.go
@@ -291,6 +291,16 @@ const (
 	// EnableLeaseHolderRemoval enables removing a leaseholder and transferring the lease
 	// during joint configuration, including to VOTER_INCOMING replicas.
 	EnableLeaseHolderRemoval
+	// EnsurePebbleFormatVersionRangeKeys is the first step of a two-part
+	// migration that bumps Pebble's format major version to a version that
+	// supports range keys.
+	EnsurePebbleFormatVersionRangeKeys
+	// EnablePebbleFormatVersionRangeKeys is the second of a two-part migration
+	// and is used as the feature gate for use of range keys. Any node at this
+	// version is guaranteed to reside in a cluster where all nodes support range
+	// keys at the Pebble layer.
+	EnablePebbleFormatVersionRangeKeys
+
 	// *************************************************
 	// Step (1): Add new versions here.
 	// Do not add new versions to a patch release.
@@ -466,6 +476,15 @@ var versionsSingleton = keyedVersions{
 		Key:     EnableLeaseHolderRemoval,
 		Version: roachpb.Version{Major: 21, Minor: 2, Internal: 70},
 	},
+	{
+		Key:     EnsurePebbleFormatVersionRangeKeys,
+		Version: roachpb.Version{Major: 21, Minor: 2, Internal: 72},
+	},
+	{
+		Key:     EnablePebbleFormatVersionRangeKeys,
+		Version: roachpb.Version{Major: 21, Minor: 2, Internal: 74},
+	},
+
 	// *************************************************
 	// Step (2): Add new versions here.
 	// Do not add new versions to a patch release.
