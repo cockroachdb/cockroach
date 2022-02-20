939e32f33a4f501f3c0be67cdb59954463d0b08d
David Taylor <tinystatemachine@gmail.com>
2022-02-20 17:50:48 +0000

    importccl: split ccl-only tests into own package
    This moves the tests from improtccl that depend on CCL deatures (tenants, multi-region, etc)
    into their own separate package that can depend on importccl, soon to be renamed sql/importer,
    so that the remaining importccl tests do not require ccl features.
    
    Release note: none.

--- pkg/ccl/importerccl/main_test.go
+++ pkg/ccl/importerccl/main_test.go
@@ -6,12 +6,14 @@
 //
 //     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
 
-package importccl
+package importerccl
 
 import (
 	"os"
 	"testing"
 
+	_ "github.com/cockroachdb/cockroach/pkg/ccl/importccl"
+	"github.com/cockroachdb/cockroach/pkg/ccl/utilccl"
 	"github.com/cockroachdb/cockroach/pkg/security"
 	"github.com/cockroachdb/cockroach/pkg/security/securitytest"
 	"github.com/cockroachdb/cockroach/pkg/server"
@@ -21,6 +23,7 @@ import (
 )
 
 func TestMain(m *testing.M) {
+	defer utilccl.TestingEnableEnterprise()()
 	security.SetAssetLoader(securitytest.EmbeddedAssets)
 	randutil.SeedForTests()
 	serverutils.InitTestServerFactory(server.TestServerFactory)
