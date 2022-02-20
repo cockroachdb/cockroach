c6bd50d0534e196216918e0fe41a4d081f883758
David Taylor <tinystatemachine@gmail.com>
2022-02-20 19:35:03 +0000

    importccl: inline workloadccl/sstable.ToTableDesc
    This is the only user.
    
    Release note: none.

--- pkg/ccl/importccl/bench_test.go
+++ pkg/ccl/importccl/bench_test.go
@@ -10,13 +10,17 @@ package importccl_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/ccl/importccl"
-	"github.com/cockroachdb/cockroach/pkg/ccl/workloadccl/format"
 	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/bootstrap"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/parser"
 	"github.com/cockroachdb/cockroach/pkg/sql/row"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
@@ -25,9 +29,10 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/cockroach/pkg/workload"
 	"github.com/cockroachdb/cockroach/pkg/workload/tpcc"
+	"github.com/cockroachdb/errors"
 )
 
-//BenchmarkConvertToKVs/tpcc/warehouses=1-8         1        3558824936 ns/op          22.46 MB/s
+// BenchmarkConvertToKVs/tpcc/warehouses=1-8         1        3558824936 ns/op          22.46 MB/s
 func BenchmarkConvertToKVs(b *testing.B) {
 	skip.UnderShort(b, "skipping long benchmark")
 
@@ -35,6 +40,32 @@ func BenchmarkConvertToKVs(b *testing.B) {
 	b.Run(`tpcc/warehouses=1`, func(b *testing.B) {
 		benchmarkConvertToKVs(b, tpccGen)
 	})
+}
+
+func toTableDescriptor(
+	t workload.Table, tableID descpb.ID, ts time.Time,
+) (catalog.TableDescriptor, error) {
+	ctx := context.Background()
+	semaCtx := tree.MakeSemaContext()
+	stmt, err := parser.ParseOne(fmt.Sprintf(`CREATE TABLE "%s" %s`, t.Name, t.Schema))
+	if err != nil {
+		return nil, err
+	}
+	createTable, ok := stmt.AST.(*tree.CreateTable)
+	if !ok {
+		return nil, errors.Errorf("expected *tree.CreateTable got %T", stmt)
+	}
+	// We need to assign a valid parent database ID to the table descriptor, but
+	// the value itself doesn't matter, so we arbitrarily pick the system database
+	// ID because we know it's valid.
+	parentID := descpb.ID(keys.SystemDatabaseID)
+	testSettings := cluster.MakeTestingClusterSettings()
+	tableDesc, err := importccl.MakeTestingSimpleTableDescriptor(
+		ctx, &semaCtx, testSettings, createTable, parentID, keys.PublicSchemaID, tableID, importccl.NoFKs, ts.UnixNano())
+	if err != nil {
+		return nil, err
+	}
+	return tableDesc.ImmutableCopy().(catalog.TableDescriptor), nil
 }
 
 func benchmarkConvertToKVs(b *testing.B, g workload.Generator) {
@@ -45,7 +76,7 @@ func benchmarkConvertToKVs(b *testing.B, g workload.Generator) {
 	var bytes int64
 	b.ResetTimer()
 	for _, t := range g.Tables() {
-		tableDesc, err := format.ToTableDescriptor(t, tableID, ts)
+		tableDesc, err := toTableDescriptor(t, tableID, ts)
 		if err != nil {
 			b.Fatal(err)
 		}
