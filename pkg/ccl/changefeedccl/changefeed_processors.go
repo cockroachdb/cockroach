497cf5c039773c5e535d70606fb48dc2903bb82f
Aaron Zinger <zinger@cockroachlabs.com>
2022-02-21 15:52:04 -0500

    changefeedccl: enable changefeeds on multiple column families
    Changes the behavior when starting a changefeed on a table
    with multiple column families. Previously, this would error.
    Now, we create a feed that will emit individual messages per
    column family. Primary key columns will appear in the key
    for all column families, but only in the value in the families
    they are in. Avro schema names will include the family name.
    
    For UX simplicity, adding a column family to a table that
    previously had only one will cause the feed to error out
    as before. Since we don't have a good way right now to warn
    when making a schema change on a table with a running feed,
    the resulting behavior change would potentially be too
    surprising, presenting like dropped or incomplete messages.
    
    Release justification: The major changes required for this feature have already been merged in. This change is additive and fairly contained.
    
    Release note (enterprise change): Changefeeds can now be created on tables with more than one column family. Previously, this would error. Now, we create a feed that will emit individual messages per column family. Primary key columns will appear in the key for all column families, but in the value only in the families they are in. For example, if a table foo has families "primary" containing the primary key and a string column, and "secondary" containing a different string column, you'll see two messages for an insert that will look like 0 -> {id: 0, s1: "val1"}, 0 -> {s2: "val2"}. If an update then only affects one family, you'll see only one message (e.g. 0 -> {s2: "newval"}). This behavior reflects CockroachDB's internal treatment of column families: writes are processed and stored separately, with only the ordering and atomicity guarantees that would apply to updates to two different tables within a single transaction. Avro schema names will include the family name concatenated to the table name. Note that if family names are not specified in the CREATE or ALTER TABLE statement, the default family names will either be "primary" or of the form "fam_<zero-indexed family id>_<underscore-delimited list of columns>.
    
    Release note (sql change): Core changefeeds can now be created on tables with more than one column family. Previously, this would error. Now, we create a feed that will emit individual messages per column family. Primary key columns will appear in the key for all column families, but in the value only in the families they are in. For example, if a table foo has families "primary" containing the primary key and a string column, and "secondary" containing a different string column, you'll see two messages for an insert that will look like 0 -> {id: 0, s1: "val1"}, 0 -> {s2: "val2"}. If an update then only affects one family, you'll see only one message (e.g. 0 -> {s2: "newval"}). This behavior reflects CockroachDB's inte
    rnal treatment of column families: writes are processed and stored separately, with only the ordering and atomicity guarantees that would apply to
     updates to two different tables within a single transaction.

--- pkg/ccl/changefeedccl/changefeed_processors.go
+++ pkg/ccl/changefeedccl/changefeed_processors.go
@@ -351,7 +351,7 @@ func (ca *changeAggregator) makeKVFeedCfg(
 		sf = schemafeed.DoNothingSchemaFeed
 	} else {
 		sf = schemafeed.New(ctx, cfg, schemaChangeEvents, AllTargets(ca.spec.Feed),
-			initialHighWater, &ca.metrics.SchemaFeedMetrics)
+			initialHighWater, &ca.metrics.SchemaFeedMetrics, ca.spec.Feed.Opts)
 	}
 
 	return kvfeed.Config{
@@ -768,13 +768,15 @@ func (c *kvEventToRowConsumer) eventToRow(
 		prevSchemaTimestamp = schemaTimestamp.Prev()
 	}
 
-	desc, err := c.rfCache.TableDescForKey(ctx, event.KV().Key, schemaTimestamp)
+	desc, family, err := c.rfCache.TableDescForKey(ctx, event.KV().Key, schemaTimestamp)
 	if err != nil {
 		return r, err
 	}
 
 	r.tableDesc = desc
-	rf, err := c.rfCache.RowFetcherForTableDesc(desc)
+	r.familyID = family
+	var rf *row.Fetcher
+	rf, err = c.rfCache.RowFetcherForColumnFamily(desc, family)
 	if err != nil {
 		return r, err
 	}
@@ -818,17 +820,20 @@ func (c *kvEventToRowConsumer) eventToRow(
 	if withDiff {
 		prevRF := rf
 		r.prevTableDesc = r.tableDesc
+		r.prevFamilyID = r.familyID
 		if prevSchemaTimestamp != schemaTimestamp {
 			// If the previous value is being interpreted under a different
 			// version of the schema, fetch the correct table descriptor and
 			// create a new row.Fetcher with it.
-			prevDesc, err := c.rfCache.TableDescForKey(ctx, event.KV().Key, prevSchemaTimestamp)
+			prevDesc, family, err := c.rfCache.TableDescForKey(ctx, event.KV().Key, prevSchemaTimestamp)
+
 			if err != nil {
 				return r, err
 			}
-
 			r.prevTableDesc = prevDesc
-			prevRF, err = c.rfCache.RowFetcherForTableDesc(prevDesc)
+			r.prevFamilyID = family
+			prevRF, err = c.rfCache.RowFetcherForColumnFamily(prevDesc, family)
+
 			if err != nil {
 				return r, err
 			}
