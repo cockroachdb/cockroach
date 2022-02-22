3737ccd448208dd9f865f7b78463fda2fc533314
Radu Berinde <radu@cockroachlabs.com>
2022-02-22 10:56:52 -0800

    span: use KeyColumns in span.Builder
    This commit refactors span.Builder to use
    `IndexFetchSpec_KeyColumn`s instead of descriptors. This is in
    preparation for replacing the JoinReader descriptor with an
    `IndexFetchSpec`.
    
    Release note: None

--- pkg/sql/split.go
+++ pkg/sql/split.go
@@ -97,7 +97,8 @@ func getRowKey(
 		colMap.Set(index.GetKeyColumnID(i), i)
 	}
 	prefix := rowenc.MakeIndexKeyPrefix(codec, tableDesc.GetID(), index.GetID())
-	key, _, err := rowenc.EncodePartialIndexKey(index, len(values), colMap, values, prefix)
+	keyCols := tableDesc.IndexFetchSpecKeyAndSuffixColumns(index)
+	key, _, err := rowenc.EncodePartialIndexKey(keyCols[:len(values)], colMap, values, prefix)
 	if err != nil {
 		return nil, err
 	}
