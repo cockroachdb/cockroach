c3c0aeb6d9894af033b0fee3741b9af418e0c21b
Santamaura <santamaura@cockroachlabs.com>
2022-02-22 17:38:13 -0500

    ui: add alert banner on overview list page for staggered node versions
    Users wish to see more info related to how the progress of a cluster upgrade
    is going. Since we do not have a cluster upgrade status to use we are
    instead showing an alert banner on the overview page when there are more
    than one node versions detected. This alert banner lists the node versions
    detected and how many nodes are on each version. This is a non-critical
    alert and can be dismissed.
    
    Resolves: #67330
    
    Release justification: Low risk ui changes with improved QoL results
    
    Release note (ui change): add alert banner on overview list page for
    staggered node versions
    
    Release justification:

---
+++ pkg/ui/workspaces/db-console/src/views/shared/containers/alerts/overviewListAlerts.tsx
@@ -0,0 +1,63 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import React from "react";
+import _ from "lodash";
+import { Dispatch, Action, bindActionCreators } from "redux";
+import { connect } from "react-redux";
+
+import { AlertBox } from "src/views/shared/components/alertBox";
+import { AdminUIState } from "src/redux/state";
+import { Alert, overviewListAlertsSelector } from "src/redux/alerts";
+
+interface AlertSectionProps {
+  /**
+   * List of alerts to display in the alert section.
+   */
+  alerts: Alert[];
+  /**
+   * Raw dispatch method for the current store, will be used to dispatch
+   * alert dismissal callbacks.
+   */
+  dispatch: Dispatch<Action>;
+}
+
+class OverviewAlertListSection extends React.Component<AlertSectionProps, {}> {
+  render() {
+    const { alerts, dispatch } = this.props;
+    if (alerts.length === 0) {
+      return null;
+    }
+    return (
+      <section className="section">
+        {_.map(alerts, (a, i) => {
+          const { dismiss, ...alertProps } = a;
+          const boundDismiss = bindActionCreators(() => a.dismiss, dispatch);
+          return <AlertBox key={i} dismiss={boundDismiss} {...alertProps} />;
+        })}
+      </section>
+    );
+  }
+}
+
+const overviewAlertListSectionConnected = connect(
+  (state: AdminUIState) => {
+    return {
+      alerts: overviewListAlertsSelector(state),
+    };
+  },
+  dispatch => {
+    return {
+      dispatch: dispatch,
+    };
+  },
+)(OverviewAlertListSection);
+
+export default overviewAlertListSectionConnected;
