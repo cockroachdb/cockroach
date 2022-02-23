1431c4c63769059edcee1ad1cb6ec1934e9d913d
Ricky Stewart <ricky@cockroachlabs.com>
2022-02-23 17:56:00 -0600

    ci: a couple tweaks to bazel CI
    * Run `go mod tidy` in Bazel CI.
    * When running unit tests, write the profile file to `/artifacts` so it
      can be inspected later if necessary.
    
    Release note: None

--- build/teamcity/cockroach/ci/tests/unit_tests_impl.sh
+++ build/teamcity/cockroach/ci/tests/unit_tests_impl.sh
@@ -4,4 +4,5 @@ set -xeuo pipefail
 
 bazel build //pkg/cmd/bazci --config=ci
 $(bazel info bazel-bin --config=ci)/pkg/cmd/bazci/bazci_/bazci --config=ci \
-		       test //pkg:small_tests //pkg:medium_tests //pkg:large_tests //pkg:enormous_tests
+		                   test //pkg:small_tests //pkg:medium_tests //pkg:large_tests //pkg:enormous_tests -- \
+                                   --profile=/artifacts/profile.gz
