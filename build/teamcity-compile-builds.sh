fded0f24f6c9fcf5ccdc98eadfdc879c3023ecea
Ricky Stewart <ricky@cockroachlabs.com>
2022-02-23 10:57:16 -0600

    ci: delete `pkg/cmd/compile-build`
    Going forward with the Bazel migration the dependency on `pkg/release`
    from `pkg/cmd/compile-build` only complicates things and the binary
    doesn't really serve a purpose any more. Instead we have
    `build/teamcity-compile-builds.sh` just directly call into `mkrelease`.
    
    `build/teamcity-compile-build.sh` appears unused.
    
    Release note: None
    Release justification: Non-production code changes

--- build/teamcity-compile-builds.sh
+++ build/teamcity-compile-builds.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 export BUILDER_HIDE_GOPATH_SRC=1
 
-build/builder.sh go install ./pkg/cmd/compile-build
-build/builder.sh env \
-  compile-build --all --buildtype=development
+build/builder.sh mkrelease linux-gnu SUFFIX=.linux-2.6.32-gnu-amd64
+build/builder.sh mkrelease darwin    SUFFIX=.darwin-10.9-amd64
+build/builder.sh mkrelease windows   SUFFIX=.windows-6.2-amd64.exe
 cp cockroach.* artifacts
