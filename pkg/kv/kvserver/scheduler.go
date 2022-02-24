a11f4ebd7e322b7ec3bb5fc418ca2c7e993d29d3
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-02-24 16:57:06 +0100

    kvserver: disable assertion in SetPriorityID
    Merge-to-master is currently pretty red, and this has popped up once or
    twice. Disable the assertion (which is fine for tests, for now) while
    we investigate.
    
    Touches https://github.com/cockroachdb/cockroach/issues/75939
    
    Release note: None

--- pkg/kv/kvserver/scheduler.go
+++ pkg/kv/kvserver/scheduler.go
@@ -121,7 +121,10 @@ func (q *rangeIDQueue) Len() int {
 }
 
 func (q *rangeIDQueue) SetPriorityID(id roachpb.RangeID) {
-	if q.priorityID != 0 && q.priorityID != id {
+	if q.priorityID != 0 && q.priorityID != id &&
+		// This assertion is temporarily disabled, see:
+		// https://github.com/cockroachdb/cockroach/issues/75939
+		false {
 		panic(fmt.Sprintf(
 			"priority range ID already set: old=%d, new=%d",
 			q.priorityID, id))
