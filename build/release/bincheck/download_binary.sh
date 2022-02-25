ba16cf76a2fcab4408c0bbd970fc62dbd33cdcc3
Rail Aliiev <rail@iqchoice.com>
2022-02-24 22:28:04 -0500

    release: enable bincheck for tag pushes
    Previously, in order to run `bincheck` as part of the release process,
    we would need to follow hands-on instructions at
    https://github.com/cockroachdb/bincheck/blob/master/README.md.
    
    This process requires some time and error prone.
    
    This patch enables GitHub actions on tag push events and automates the
    task.
    
    Release note: None

--- build/release/bincheck/download_binary.sh
+++ build/release/bincheck/download_binary.sh
@@ -2,39 +2,22 @@
 
 set -euo pipefail
 
-binary_suffix=$1
-binary_source=""
-cockroach_version=""
-while read line; do
-  # Trailing \r on Windows sneaks into the variables, let's nuke it
-  line=$(echo $line | tr -d '\r')
-  case "$line" in
-  \#*|"") continue ;;
-  test:*)
-    binary_source="https://binaries-test.cockroachdb.com"
-    parts=(${line//:/ })
-    export COCKROACH_VERSION=${parts[1]}
-    ;;
-  v*)
-    binary_source="https://binaries.cockroachdb.com"
-    export COCKROACH_VERSION=$line
-    ;;
-  *)
-    export COCKROACH_SHA=$line
-  esac
-done < VERSION
+download_and_extract() {
+  cockroach_version=$1
+  binary_suffix=$2
+  binary_source="https://binaries.cockroachdb.com"
+  binary_url="${binary_source}/cockroach-${cockroach_version}.${binary_suffix}"
 
-binary_url="${binary_source}/cockroach-$COCKROACH_VERSION.${binary_suffix}"
+  mkdir -p mnt
 
-mkdir -p mnt
+  # Check if this is a tarball or zip.
+  if [[ "${binary_suffix}" == *.tgz ]]; then
+    curl -sSfL "${binary_url}" > cockroach.tar.gz
+    tar zxf cockroach.tar.gz -C mnt --strip-components=1
+  else
+    curl -sSfL "${binary_url}" > cockroach.zip
+    7z e -omnt cockroach.zip
+  fi
 
-# Check if this is a tarball or zip.
-if [[ "${binary_suffix}" == *.tgz ]]; then
-  curl -sSfL "${binary_url}" > cockroach.tar.gz
-  tar zxf cockroach.tar.gz -C mnt --strip-components=1
-else
-  curl -sSfL "${binary_url}" > cockroach.zip
-  7z e -omnt cockroach.zip
-fi
-
-echo "Downloaded ${binary_url}"
+  echo "Downloaded ${binary_url}"
+}
