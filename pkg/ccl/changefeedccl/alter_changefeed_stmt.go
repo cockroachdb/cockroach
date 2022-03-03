896213f19585538e5e4dfb662668d7d3ce2aa76a
Sherman Grewal <sherman.grewal@cockroachlabs.com>
2022-03-03 14:31:30 -0500

    changefeedccl: allow users to perform initial scans on newly added targets in the ALTER CHANGEFEED statement
    Currently, when a new target is added to an existing
    changefeed through the ALTER CHANGEFEED statement,
    no initial scan is performed on the newly added
    target. With this change, users can indicate that
    they want an initial scan by executing the following
    statement:
    
    ALTER CHANGEFEED <job_id> ADD <targets> WITH initial_scan
    
    Users can also explicitly ask for no initial scan by
    replacing 'initial_scan' with 'no_initial_scan'.
    The default behavior is to perform no initial scans
    when new targets are added.
    
    Release note (enterprise change): Users can now
    perform initial scans on newly added targets
    by executing the following statement:
    
    ALTER CHANGEFEED <job_id> ADD <targets> WITH initial_scan
    
    The default behavior is to perform no initial scans
    on newly added targets, but users can explicitly
    request this by replacing the 'initial_scan' with
    'no_initial_scan'.
    
    Release justification: low danger enhancement to newly
    implemented functionality.

--- pkg/ccl/changefeedccl/alter_changefeed_stmt.go
+++ pkg/ccl/changefeedccl/alter_changefeed_stmt.go
@@ -18,6 +18,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
 	"github.com/cockroachdb/cockroach/pkg/kv"
 	"github.com/cockroachdb/cockroach/pkg/sql"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/colinfo"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/resolver"
@@ -74,166 +75,9 @@ func alterChangefeedPlanHook(
 			return errors.Errorf(`job %d is not paused`, jobID)
 		}
 
-		// this CREATE CHANGEFEED node will be used to update the existing changefeed
-		newChangefeedStmt := &tree.CreateChangefeed{
-			SinkURI: tree.NewDString(prevDetails.SinkURI),
-		}
-
-		optionsMap := make(map[string]tree.KVOption, len(prevDetails.Opts))
-
-		// pull the options that are set for the existing changefeed
-		for key, value := range prevDetails.Opts {
-			// There are some options (e.g. topics) that we set during the creation of
-			// a changefeed, but we do not allow these options to be set by the user.
-			// Hence, we can not include these options in our new CREATE CHANGEFEED
-			// statement.
-			if _, ok := changefeedbase.ChangefeedOptionExpectValues[key]; !ok {
-				continue
-			}
-			existingOpt := tree.KVOption{Key: tree.Name(key)}
-			if len(value) > 0 {
-				existingOpt.Value = tree.NewDString(value)
-			}
-			optionsMap[key] = existingOpt
-		}
-
-		statementTime := hlc.Timestamp{
-			WallTime: p.ExtendedEvalContext().GetStmtTimestamp().UnixNano(),
-		}
-
-		allDescs, err := backupresolver.LoadAllDescs(ctx, p.ExecCfg(), statementTime)
+		newChangefeedStmt, nonInitialScanDescs, err := generateNewChangefeedStmt(ctx, p, alterChangefeedStmt.Cmds, jobID, prevDetails)
 		if err != nil {
 			return err
-		}
-		descResolver, err := backupresolver.NewDescriptorResolver(allDescs)
-		if err != nil {
-			return err
-		}
-
-		newDescs := make(map[descpb.ID]*tree.UnresolvedName)
-
-		for _, target := range AllTargets(prevDetails) {
-			desc := descResolver.DescByID[target.TableID]
-			newDescs[target.TableID] = tree.NewUnresolvedName(desc.GetName())
-		}
-
-		for _, cmd := range alterChangefeedStmt.Cmds {
-			switch v := cmd.(type) {
-			case *tree.AlterChangefeedAddTarget:
-				for _, targetPattern := range v.Targets.Tables {
-					targetName, err := getTargetName(targetPattern)
-					if err != nil {
-						return err
-					}
-					found, _, desc, err := resolver.ResolveExisting(
-						ctx,
-						targetName.ToUnresolvedObjectName(),
-						descResolver,
-						tree.ObjectLookupFlags{},
-						p.CurrentDatabase(),
-						p.CurrentSearchPath(),
-					)
-					if err != nil {
-						return err
-					}
-					if !found {
-						return pgerror.Newf(pgcode.InvalidParameterValue, `target %q does not exist`, tree.ErrString(targetPattern))
-					}
-					newDescs[desc.GetID()] = tree.NewUnresolvedName(desc.GetName())
-				}
-			case *tree.AlterChangefeedDropTarget:
-				for _, targetPattern := range v.Targets.Tables {
-					targetName, err := getTargetName(targetPattern)
-					if err != nil {
-						return err
-					}
-					found, _, desc, err := resolver.ResolveExisting(
-						ctx,
-						targetName.ToUnresolvedObjectName(),
-						descResolver,
-						tree.ObjectLookupFlags{},
-						p.CurrentDatabase(),
-						p.CurrentSearchPath(),
-					)
-					if err != nil {
-						return err
-					}
-					if !found {
-						return pgerror.Newf(pgcode.InvalidParameterValue, `target %q does not exist`, tree.ErrString(targetPattern))
-					}
-					delete(newDescs, desc.GetID())
-				}
-			case *tree.AlterChangefeedSetOptions:
-				optsFn, err := p.TypeAsStringOpts(ctx, v.Options, changefeedbase.AlterChangefeedOptionExpectValues)
-				if err != nil {
-					return err
-				}
-
-				opts, err := optsFn()
-				if err != nil {
-					return err
-				}
-
-				for key, value := range opts {
-					if _, ok := changefeedbase.AlterChangefeedUnsupportedOptions[key]; ok {
-						return pgerror.Newf(pgcode.InvalidParameterValue, `cannot alter option %q`, key)
-					}
-					if key == changefeedbase.OptSink {
-						newSinkURI, err := url.Parse(value)
-						if err != nil {
-							return err
-						}
-
-						prevSinkURI, err := url.Parse(prevDetails.SinkURI)
-						if err != nil {
-							return err
-						}
-
-						if newSinkURI.Scheme != prevSinkURI.Scheme {
-							return pgerror.Newf(
-								pgcode.InvalidParameterValue,
-								`New sink type %q does not match original sink type %q. Altering the sink type of a changefeed is disallowed, consider creating a new changefeed instead.`,
-								newSinkURI.Scheme,
-								prevSinkURI.Scheme,
-							)
-						}
-
-						newChangefeedStmt.SinkURI = tree.NewDString(value)
-					} else {
-						opt := tree.KVOption{Key: tree.Name(key)}
-						if len(value) > 0 {
-							opt.Value = tree.NewDString(value)
-						}
-						optionsMap[key] = opt
-					}
-				}
-			case *tree.AlterChangefeedUnsetOptions:
-				optKeys := v.Options.ToStrings()
-				for _, key := range optKeys {
-					if key == changefeedbase.OptSink {
-						return pgerror.Newf(pgcode.InvalidParameterValue, `cannot unset option %q`, key)
-					}
-					if _, ok := changefeedbase.ChangefeedOptionExpectValues[key]; !ok {
-						return pgerror.Newf(pgcode.InvalidParameterValue, `invalid option %q`, key)
-					}
-					if _, ok := changefeedbase.AlterChangefeedUnsupportedOptions[key]; ok {
-						return pgerror.Newf(pgcode.InvalidParameterValue, `cannot alter option %q`, key)
-					}
-					delete(optionsMap, key)
-				}
-			}
-		}
-
-		if len(newDescs) == 0 {
-			return pgerror.Newf(pgcode.InvalidParameterValue, "cannot drop all targets for changefeed job %d", jobID)
-		}
-
-		for _, targetName := range newDescs {
-			newChangefeedStmt.Targets.Tables = append(newChangefeedStmt.Targets.Tables, targetName)
-		}
-
-		for _, val := range optionsMap {
-			newChangefeedStmt.Options = append(newChangefeedStmt.Options, val)
 		}
 
 		sinkURIFn, err := p.TypeAsString(ctx, newChangefeedStmt.SinkURI, `ALTER CHANGEFEED`)
@@ -256,6 +100,19 @@ func alterChangefeedPlanHook(
 			return err
 		}
 
+		// obtain the targets and tables that we will not apply an initial scan on
+		targets, tables, err := getTargetsAndTables(ctx, p, nonInitialScanDescs, opts)
+		if err != nil {
+			return err
+		}
+
+		// skipInitialScanTargetDetails contains all targets that the changefeed
+		// will not perform an initial scan on
+		skipInitialScanTargetDetails := jobspb.ChangefeedDetails{
+			TargetSpecifications: targets,
+			Tables:               tables,
+		}
+
 		jobRecord, err := createChangefeedJobRecord(
 			ctx,
 			p,
@@ -269,11 +126,40 @@ func alterChangefeedPlanHook(
 			return errors.Wrap(err, `failed to alter changefeed`)
 		}
 
+		// Note that the StatementTime of the job record details will be the current
+		// statement timestamp as per the evaluation context. Since we set the
+		// details of the job payload to newDetails, the StatementTime will be
+		// updated to this new StatementTime provided by the job record
 		newDetails := jobRecord.Details.(jobspb.ChangefeedDetails)
 
-		// We need to persist the statement time that was generated during the
-		// creation of the changefeed
-		newDetails.StatementTime = prevDetails.StatementTime
+		var newProgress *jobspb.Progress = nil
+
+		// If there are newly added targets that we would like to perform an initial scan on,
+		// we need to alter the progress of the job so that we can ignore targets that do not
+		// need to be backfilled and ensure that backfills are performed on the requested targets
+		{
+			changefeedSpans, err := fetchSpansForTargets(ctx, p.ExecCfg(), AllTargets(skipInitialScanTargetDetails), newDetails.StatementTime)
+			if err != nil {
+				return err
+			}
+
+			// The progress defined below will be used to update the progress of the
+			// changefeed job. First, we reset the progress of the job so that initial
+			// scans can be performed on the desired targets. However, we need to
+			// ensure that no initial scans are performed on the remaining targets. We
+			// achieve this by creating a new checkpoint that includes the spans of
+			// the targets that we do not want to perform an initial scan on.
+			newProgress = &jobspb.Progress{
+				Progress: &jobspb.Progress_HighWater{},
+				Details: &jobspb.Progress_Changefeed{
+					Changefeed: &jobspb.ChangefeedProgress{
+						Checkpoint: &jobspb.ChangefeedProgress_Checkpoint{
+							Spans: changefeedSpans,
+						},
+					},
+				},
+			}
+		}
 
 		newPayload := job.Payload()
 		newPayload.Details = jobspb.WrapPayloadDetails(newDetails)
@@ -284,6 +170,9 @@ func alterChangefeedPlanHook(
 			txn *kv.Txn, md jobs.JobMetadata, ju *jobs.JobUpdater,
 		) error {
 			ju.UpdatePayload(&newPayload)
+			if newProgress != nil {
+				ju.UpdateProgress(newProgress)
+			}
 			return nil
 		})
 
@@ -316,4 +205,218 @@ func getTargetName(targetPattern tree.TablePattern) (*tree.TableName, error) {
 	}
 
 	return targetName, nil
+}
+
+// generateNewChangefeedStmt creates a new CREATE CHANGEFEED statement that includes the
+// alterations that were requested by the user. Furthermore, this function also returns
+// all of the targets that will not perform an initial scan
+func generateNewChangefeedStmt(
+	ctx context.Context,
+	p sql.PlanHookState,
+	alterCmds tree.AlterChangefeedCmds,
+	jobID jobspb.JobID,
+	details jobspb.ChangefeedDetails,
+) (*tree.CreateChangefeed, []catalog.Descriptor, error) {
+	// this CREATE CHANGEFEED node will be used to update the existing changefeed
+	newChangefeedStmt := &tree.CreateChangefeed{
+		SinkURI: tree.NewDString(details.SinkURI),
+	}
+
+	optionsMap := make(map[string]tree.KVOption, len(details.Opts))
+
+	// pull the options that are set for the existing changefeed
+	for key, value := range details.Opts {
+		// There are some options (e.g. topics) that we set during the creation of
+		// a changefeed, but we do not allow these options to be set by the user.
+		// Hence, we can not include these options in our new CREATE CHANGEFEED
+		// statement.
+		if _, ok := changefeedbase.ChangefeedOptionExpectValues[key]; !ok {
+			continue
+		}
+		existingOpt := tree.KVOption{Key: tree.Name(key)}
+		if len(value) > 0 {
+			existingOpt.Value = tree.NewDString(value)
+		}
+		optionsMap[key] = existingOpt
+	}
+
+	statementTime := hlc.Timestamp{
+		WallTime: p.ExtendedEvalContext().GetStmtTimestamp().UnixNano(),
+	}
+
+	allDescs, err := backupresolver.LoadAllDescs(ctx, p.ExecCfg(), statementTime)
+	if err != nil {
+		return nil, nil, err
+	}
+	descResolver, err := backupresolver.NewDescriptorResolver(allDescs)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	type targetInfo struct {
+		Desc        catalog.Descriptor
+		InitialScan bool
+	}
+
+	newTargets := make(map[descpb.ID]targetInfo)
+
+	for _, target := range AllTargets(details) {
+		desc := descResolver.DescByID[target.TableID]
+
+		// ensure that we do not perform initial scans on existing targets
+		newTargets[target.TableID] = targetInfo{
+			Desc:        desc,
+			InitialScan: false,
+		}
+	}
+
+	for _, cmd := range alterCmds {
+		switch v := cmd.(type) {
+		case *tree.AlterChangefeedAddTarget:
+			targetOptsFn, err := p.TypeAsStringOpts(ctx, v.Options, changefeedbase.AlterChangefeedTargetOptions)
+			if err != nil {
+				return nil, nil, err
+			}
+			targetOpts, err := targetOptsFn()
+			if err != nil {
+				return nil, nil, err
+			}
+
+			_, withInitialScan := targetOpts[changefeedbase.OptInitialScan]
+			_, noInitialScan := targetOpts[changefeedbase.OptNoInitialScan]
+			if withInitialScan && noInitialScan {
+				return nil, nil, pgerror.Newf(
+					pgcode.InvalidParameterValue,
+					`cannot specify both %q and %q`, changefeedbase.OptInitialScan,
+					changefeedbase.OptNoInitialScan,
+				)
+			}
+
+			for _, targetPattern := range v.Targets.Tables {
+				targetName, err := getTargetName(targetPattern)
+				if err != nil {
+					return nil, nil, err
+				}
+				found, _, desc, err := resolver.ResolveExisting(
+					ctx,
+					targetName.ToUnresolvedObjectName(),
+					descResolver,
+					tree.ObjectLookupFlags{},
+					p.CurrentDatabase(),
+					p.CurrentSearchPath(),
+				)
+				if err != nil {
+					return nil, nil, err
+				}
+				if !found {
+					return nil, nil, pgerror.Newf(pgcode.InvalidParameterValue, `target %q does not exist`, tree.ErrString(targetPattern))
+				}
+				// By default, we will not perform an initial scan on newly added
+				// targets. Hence, the user must explicitly state that they want an
+				// initial scan performed on the new targets
+				newTargets[desc.GetID()] = targetInfo{
+					Desc:        desc,
+					InitialScan: withInitialScan,
+				}
+			}
+		case *tree.AlterChangefeedDropTarget:
+			for _, targetPattern := range v.Targets.Tables {
+				targetName, err := getTargetName(targetPattern)
+				if err != nil {
+					return nil, nil, err
+				}
+				found, _, desc, err := resolver.ResolveExisting(
+					ctx,
+					targetName.ToUnresolvedObjectName(),
+					descResolver,
+					tree.ObjectLookupFlags{},
+					p.CurrentDatabase(),
+					p.CurrentSearchPath(),
+				)
+				if err != nil {
+					return nil, nil, err
+				}
+				if !found {
+					return nil, nil, pgerror.Newf(pgcode.InvalidParameterValue, `target %q does not exist`, tree.ErrString(targetPattern))
+				}
+				delete(newTargets, desc.GetID())
+			}
+		case *tree.AlterChangefeedSetOptions:
+			optsFn, err := p.TypeAsStringOpts(ctx, v.Options, changefeedbase.AlterChangefeedOptionExpectValues)
+			if err != nil {
+				return nil, nil, err
+			}
+
+			opts, err := optsFn()
+			if err != nil {
+				return nil, nil, err
+			}
+
+			for key, value := range opts {
+				if _, ok := changefeedbase.AlterChangefeedUnsupportedOptions[key]; ok {
+					return nil, nil, pgerror.Newf(pgcode.InvalidParameterValue, `cannot alter option %q`, key)
+				}
+				if key == changefeedbase.OptSink {
+					newSinkURI, err := url.Parse(value)
+					if err != nil {
+						return nil, nil, err
+					}
+
+					prevSinkURI, err := url.Parse(details.SinkURI)
+					if err != nil {
+						return nil, nil, err
+					}
+
+					if newSinkURI.Scheme != prevSinkURI.Scheme {
+						return nil, nil, pgerror.Newf(
+							pgcode.InvalidParameterValue,
+							`New sink type %q does not match original sink type %q. Altering the sink type of a changefeed is disallowed, consider creating a new changefeed instead.`,
+							newSinkURI.Scheme,
+							prevSinkURI.Scheme,
+						)
+					}
+
+					newChangefeedStmt.SinkURI = tree.NewDString(value)
+				} else {
+					opt := tree.KVOption{Key: tree.Name(key)}
+					if len(value) > 0 {
+						opt.Value = tree.NewDString(value)
+					}
+					optionsMap[key] = opt
+				}
+			}
+		case *tree.AlterChangefeedUnsetOptions:
+			optKeys := v.Options.ToStrings()
+			for _, key := range optKeys {
+				if key == changefeedbase.OptSink {
+					return nil, nil, pgerror.Newf(pgcode.InvalidParameterValue, `cannot unset option %q`, key)
+				}
+				if _, ok := changefeedbase.ChangefeedOptionExpectValues[key]; !ok {
+					return nil, nil, pgerror.Newf(pgcode.InvalidParameterValue, `invalid option %q`, key)
+				}
+				if _, ok := changefeedbase.AlterChangefeedUnsupportedOptions[key]; ok {
+					return nil, nil, pgerror.Newf(pgcode.InvalidParameterValue, `cannot alter option %q`, key)
+				}
+				delete(optionsMap, key)
+			}
+		}
+	}
+
+	if len(newTargets) == 0 {
+		return nil, nil, pgerror.Newf(pgcode.InvalidParameterValue, "cannot drop all targets for changefeed job %d", jobID)
+	}
+
+	for _, val := range optionsMap {
+		newChangefeedStmt.Options = append(newChangefeedStmt.Options, val)
+	}
+
+	var nonInitialScanDescs []catalog.Descriptor
+	for _, target := range newTargets {
+		newChangefeedStmt.Targets.Tables = append(newChangefeedStmt.Targets.Tables, tree.NewUnresolvedName(target.Desc.GetName()))
+		if !target.InitialScan {
+			nonInitialScanDescs = append(nonInitialScanDescs, target.Desc)
+		}
+	}
+
+	return newChangefeedStmt, nonInitialScanDescs, nil
 }
--- pkg/ccl/changefeedccl/alter_changefeed_test.go
+++ pkg/ccl/changefeedccl/alter_changefeed_test.go
@@ -15,13 +15,25 @@ import (
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/ccl/changefeedccl/cdctest"
+	"github.com/cockroachdb/cockroach/pkg/ccl/changefeedccl/changefeedbase"
 	"github.com/cockroachdb/cockroach/pkg/jobs"
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/kv"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/server"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/desctestutils"
+	"github.com/cockroachdb/cockroach/pkg/sql/execinfra"
 	"github.com/cockroachdb/cockroach/pkg/sql/tests"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
+	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/require"
 )
 
@@ -223,6 +235,16 @@ func TestAlterChangefeedErrors(t *testing.T) {
 			`cannot unset option "sink"`,
 			fmt.Sprintf(`ALTER CHANGEFEED %d UNSET sink`, feed.JobID()),
 		)
+
+		sqlDB.ExpectErr(t,
+			`pq: invalid option "diff"`,
+			fmt.Sprintf(`ALTER CHANGEFEED %d ADD bar WITH diff`, feed.JobID()),
+		)
+
+		sqlDB.ExpectErr(t,
+			`pq: cannot specify both "initial_scan" and "no_initial_scan"`,
+			fmt.Sprintf(`ALTER CHANGEFEED %d ADD bar WITH initial_scan, no_initial_scan`, feed.JobID()),
+		)
 	}
 
 	t.Run(`kafka`, kafkaTest(testFn))
@@ -372,4 +394,233 @@ func TestAlterChangefeedChangeSinkURI(t *testing.T) {
 	}
 
 	t.Run(`kafka`, kafkaTest(testFn))
+}
+
+func TestAlterChangefeedInitialScan(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	testFn := func(t *testing.T, db *gosql.DB, f cdctest.TestFeedFactory) {
+		sqlDB := sqlutils.MakeSQLRunner(db)
+		sqlDB.Exec(t, `CREATE TABLE foo (a INT PRIMARY KEY)`)
+
+		testFeed := feed(t, f, `CREATE CHANGEFEED FOR foo WITH resolved = '1s'`)
+		defer closeFeed(t, testFeed)
+
+		feed, ok := testFeed.(cdctest.EnterpriseTestFeed)
+		require.True(t, ok)
+
+		sqlDB.Exec(t, `PAUSE JOB $1`, feed.JobID())
+		waitForJobStatus(sqlDB, t, feed.JobID(), `paused`)
+
+		sqlDB.Exec(t, `CREATE TABLE bar (a INT PRIMARY KEY)`)
+		sqlDB.Exec(t, `INSERT INTO bar VALUES (1), (2), (3)`)
+
+		sqlDB.Exec(t, fmt.Sprintf(`ALTER CHANGEFEED %d ADD bar WITH initial_scan`, feed.JobID()))
+
+		sqlDB.Exec(t, fmt.Sprintf(`RESUME JOB %d`, feed.JobID()))
+		waitForJobStatus(sqlDB, t, feed.JobID(), `running`)
+
+		assertPayloads(t, testFeed, []string{
+			`bar: [1]->{"after": {"a": 1}}`,
+			`bar: [2]->{"after": {"a": 2}}`,
+			`bar: [3]->{"after": {"a": 3}}`,
+		})
+
+		sqlDB.Exec(t, `INSERT INTO bar VALUES (4)`)
+		assertPayloads(t, testFeed, []string{
+			`bar: [4]->{"after": {"a": 4}}`,
+		})
+	}
+
+	t.Run(`kafka`, kafkaTest(testFn))
+}
+
+func TestAlterChangefeedNoInitialScan(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	testFn := func(t *testing.T, db *gosql.DB, f cdctest.TestFeedFactory) {
+		sqlDB := sqlutils.MakeSQLRunner(db)
+		sqlDB.Exec(t, `CREATE TABLE foo (a INT PRIMARY KEY)`)
+
+		testFeed := feed(t, f, `CREATE CHANGEFEED FOR foo WITH resolved = '1s'`)
+		defer closeFeed(t, testFeed)
+
+		feed, ok := testFeed.(cdctest.EnterpriseTestFeed)
+		require.True(t, ok)
+
+		sqlDB.Exec(t, `PAUSE JOB $1`, feed.JobID())
+		waitForJobStatus(sqlDB, t, feed.JobID(), `paused`)
+
+		sqlDB.Exec(t, `CREATE TABLE bar (a INT PRIMARY KEY)`)
+		sqlDB.Exec(t, `INSERT INTO bar VALUES (1), (2), (3)`)
+
+		sqlDB.Exec(t, fmt.Sprintf(`ALTER CHANGEFEED %d ADD bar WITH no_initial_scan`, feed.JobID()))
+
+		sqlDB.Exec(t, fmt.Sprintf(`RESUME JOB %d`, feed.JobID()))
+		waitForJobStatus(sqlDB, t, feed.JobID(), `running`)
+
+		expectResolvedTimestamp(t, testFeed)
+
+		sqlDB.Exec(t, `INSERT INTO bar VALUES (4)`)
+		assertPayloads(t, testFeed, []string{
+			`bar: [4]->{"after": {"a": 4}}`,
+		})
+	}
+
+	t.Run(`kafka`, kafkaTest(testFn))
+}
+
+func TestAlterChangefeedAddTargetsDuringBackfill(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	skip.UnderRace(t)
+
+	rnd, _ := randutil.NewTestRand()
+	var maxCheckpointSize int64 = 100
+
+	testFn := func(t *testing.T, db *gosql.DB, f cdctest.TestFeedFactory) {
+		sqlDB := sqlutils.MakeSQLRunner(db)
+		sqlDB.Exec(t, `CREATE TABLE foo(val INT PRIMARY KEY)`)
+		sqlDB.Exec(t, `INSERT INTO foo (val) SELECT * FROM generate_series(0, 999)`)
+
+		fooDesc := desctestutils.TestingGetPublicTableDescriptor(
+			f.Server().DB(), keys.SystemSQLCodec, "d", "foo")
+		fooTableSpan := fooDesc.PrimaryIndexSpan(keys.SystemSQLCodec)
+
+		knobs := f.Server().(*server.TestServer).Cfg.TestingKnobs.
+			DistSQL.(*execinfra.TestingKnobs).
+			Changefeed.(*TestingKnobs)
+
+		// Ensure Scan Requests are always small enough that we receive multiple
+		// resolvedFoo events during a backfill
+		knobs.FeedKnobs.BeforeScanRequest = func(b *kv.Batch) {
+			b.Header.MaxSpanRequestKeys = 1 + rnd.Int63n(100)
+		}
+
+		// Emit resolvedFoo events for majority of spans.  Be extra paranoid and ensure that
+		// we have at least 1 span for which we don't emit resolvedFoo timestamp (to force checkpointing).
+		haveGaps := false
+		knobs.ShouldSkipResolved = func(r *jobspb.ResolvedSpan) bool {
+			if r.Span.Equal(fooTableSpan) {
+				// Do not emit resolvedFoo events for the entire table span.
+				// We "simulate" large table by splitting single table span into many parts, so
+				// we want to resolve those sub-spans instead of the entire table span.
+				// However, we have to emit something -- otherwise the entire changefeed
+				// machine would not work.
+				r.Span.EndKey = fooTableSpan.Key.Next()
+				return false
+			}
+			if haveGaps {
+				return rnd.Intn(10) > 7
+			}
+			haveGaps = true
+			return true
+		}
+
+		// Checkpoint progress frequently, and set the checkpoint size limit.
+		changefeedbase.FrontierCheckpointFrequency.Override(
+			context.Background(), &f.Server().ClusterSettings().SV, 1)
+		changefeedbase.FrontierCheckpointMaxBytes.Override(
+			context.Background(), &f.Server().ClusterSettings().SV, maxCheckpointSize)
+
+		registry := f.Server().JobRegistry().(*jobs.Registry)
+		testFeed := feed(t, f, `CREATE CHANGEFEED FOR foo WITH resolved = '100ms'`)
+
+		// Kafka feeds are not buffered, so we have to consume messages.
+		g := ctxgroup.WithContext(context.Background())
+		g.Go(func() error {
+			expectedValues := make([]string, 1000)
+			for j := 0; j <= 999; j++ {
+				expectedValues[j] = fmt.Sprintf(`foo: [%d]->{"after": {"val": %d}}`, j, j)
+			}
+			err := assertPayloadsBaseErr(testFeed, expectedValues, false, false)
+			if err != nil {
+				return err
+			}
+
+			for j := 0; j <= 999; j++ {
+				expectedValues[j] = fmt.Sprintf(`bar: [%d]->{"after": {"val": %d}}`, j, j)
+			}
+			err = assertPayloadsBaseErr(testFeed, expectedValues, false, false)
+			return err
+		})
+
+		defer func() {
+			require.NoError(t, g.Wait())
+			closeFeed(t, testFeed)
+		}()
+
+		jobFeed := testFeed.(cdctest.EnterpriseTestFeed)
+		loadProgress := func() jobspb.Progress {
+			jobID := jobFeed.JobID()
+			job, err := registry.LoadJob(context.Background(), jobID)
+			require.NoError(t, err)
+			return job.Progress()
+		}
+
+		// Wait for non-nil checkpoint.
+		testutils.SucceedsSoon(t, func() error {
+			progress := loadProgress()
+			if p := progress.GetChangefeed(); p != nil && p.Checkpoint != nil && len(p.Checkpoint.Spans) > 0 {
+				return nil
+			}
+			return errors.New("waiting for checkpoint")
+		})
+
+		// Pause the job and read and verify the latest checkpoint information.
+		require.NoError(t, jobFeed.Pause())
+		progress := loadProgress()
+		require.NotNil(t, progress.GetChangefeed())
+		h := progress.GetHighWater()
+		noHighWater := h == nil || h.IsEmpty()
+		require.True(t, noHighWater)
+
+		jobCheckpoint := progress.GetChangefeed().Checkpoint
+		require.Less(t, 0, len(jobCheckpoint.Spans))
+		var checkpoint roachpb.SpanGroup
+		checkpoint.Add(jobCheckpoint.Spans...)
+
+		sqlDB.Exec(t, `CREATE TABLE bar(val INT PRIMARY KEY)`)
+		sqlDB.Exec(t, `INSERT INTO bar (val) SELECT * FROM generate_series(0, 999)`)
+
+		sqlDB.Exec(t, `CREATE TABLE baz(val INT PRIMARY KEY)`)
+		sqlDB.Exec(t, `INSERT INTO baz (val) SELECT * FROM generate_series(0, 999)`)
+
+		sqlDB.Exec(t, fmt.Sprintf(`ALTER CHANGEFEED %d ADD bar WITH initial_scan ADD baz WITH no_initial_scan`, jobFeed.JobID()))
+
+		// Collect spans we attempt to resolve after when we resume.
+		var resolvedFoo []roachpb.Span
+		knobs.ShouldSkipResolved = func(r *jobspb.ResolvedSpan) bool {
+			if !r.Span.Equal(fooTableSpan) {
+				resolvedFoo = append(resolvedFoo, r.Span)
+			}
+			return false
+		}
+
+		require.NoError(t, jobFeed.Resume())
+
+		// Wait for the high water mark to be non-zero.
+		testutils.SucceedsSoon(t, func() error {
+			prog := loadProgress()
+			if p := prog.GetHighWater(); p != nil && !p.IsEmpty() {
+				return nil
+			}
+			return errors.New("waiting for highwater")
+		})
+
+		// At this point, highwater mark should be set, and previous checkpoint should be gone.
+		progress = loadProgress()
+		require.NotNil(t, progress.GetChangefeed())
+		require.Equal(t, 0, len(progress.GetChangefeed().Checkpoint.Spans))
+
+		// Verify that none of the resolvedFoo spans after resume were checkpointed.
+		for _, sp := range resolvedFoo {
+			require.Falsef(t, checkpoint.Contains(sp.Key), "span should not have been resolved: %s", sp)
+		}
+	}
+
+	t.Run(`kafka`, kafkaTest(testFn, feedTestNoTenants))
 }
