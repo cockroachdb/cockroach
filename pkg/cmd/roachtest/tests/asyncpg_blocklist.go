4c14db8d77f2774f97113ac6eda76a86bac4c212
richardjcai <caioftherichard@gmail.com>
2022-03-03 18:08:41 -0500

    orm: update asyncpg_blocklist
    Release justification: ORM test only change
    
    Release note: None

--- pkg/cmd/roachtest/tests/asyncpg_blocklist.go
+++ pkg/cmd/roachtest/tests/asyncpg_blocklist.go
@@ -23,11 +23,7 @@ var asyncpgBlocklists = blocklistsForVersion{
 // Please keep these lists alphabetized for easy diffing.
 // After a failed run, an updated version of this blocklist should be available
 // in the test log.
-var asyncpgBlocklist22_1 = asyncpgBlocklist21_2
-
-var asyncpgBlocklist21_2 = blocklist{}
-
-var asyncpgIgnoreList22_1 = blocklist{
+var asyncpgBlocklist22_1 = blocklist{
 	"test_cache_invalidation.TestCacheInvalidation.test_prepare_cache_invalidation_in_pool":               "unknown",
 	"test_cache_invalidation.TestCacheInvalidation.test_prepare_cache_invalidation_in_transaction":        "unknown",
 	"test_cache_invalidation.TestCacheInvalidation.test_prepare_cache_invalidation_silent":                "unknown",
@@ -68,6 +64,7 @@ var asyncpgIgnoreList22_1 = blocklist{
 	"test_copy.TestCopyTo.test_copy_records_to_table_async":                                               "unknown",
 	"test_copy.TestCopyTo.test_copy_to_table_basics":                                                      "unknown",
 	"test_cursor.TestCursor.test_cursor_02":                                                               "unknown",
+	"test_cursor.TestCursor.test_cursor_03":                                                               "unknown",
 	"test_cursor.TestCursor.test_cursor_04":                                                               "unknown",
 	"test_cursor.TestIterableCursor.test_cursor_iterable_02":                                              "unknown",
 	"test_exceptions.TestExceptions.test_exceptions_str":                                                  "unknown",
@@ -124,10 +121,10 @@ var asyncpgIgnoreList22_1 = blocklist{
 	"test_pool.TestPool.test_pool_handles_transaction_exit_in_asyncgen_2":                                 "unknown",
 	"test_listeners.TestListeners.test_dangling_listener_warns":                                           "unknown",
 	"test_pool.TestPool.test_pool_01":                                                                     "unknown",
+	"test_pool.TestPool.test_pool_11":                                                                     "unknown",
 	"test_pool.TestPool.test_pool_init_and_use_race":                                                      "unknown",
 }
-
-var asyncpgIgnoreList21_2 = blocklist{
+var asyncpgBlocklist21_2 = blocklist{
 	"test_cache_invalidation.TestCacheInvalidation.test_prepare_cache_invalidation_in_pool":               "unknown",
 	"test_cache_invalidation.TestCacheInvalidation.test_prepare_cache_invalidation_in_transaction":        "unknown",
 	"test_cache_invalidation.TestCacheInvalidation.test_prepare_cache_invalidation_silent":                "unknown",
@@ -221,3 +218,6 @@ var asyncpgIgnoreList21_2 = blocklist{
 	"test_utils.TestUtils.test_mogrify_multiple":                                                          "unknown",
 	"test_utils.TestUtils.test_mogrify_simple":                                                            "unknown",
 }
+
+var asyncpgIgnoreList22_1 = asyncpgIgnoreList21_2
+var asyncpgIgnoreList21_2 = blocklist{}
