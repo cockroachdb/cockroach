d74a79aff190d128ed495e4e9add062941bef424
Rajiv Sharma <rajshar.email@gmail.com>
2022-03-06 23:16:32 +0000

    opt: index accelerate && (overlaps) expressions for array inverted indexes
    Previously, we did not support index acceleration for queries involving
    array overlaps (&&).
    
    This change adds support for using the inverted index with && expressions on
    Array columns. When there is an inverted index available, a scan will be done on
    the Array column using the spans found from the constant value.
    
    Fixes: #75477
    
    Release note (performance improvement): Expressions using the overlaps (&&)
    operator for arrays now support index-acceleration for faster execution in
    some cases.
    
    Release justification: low risk, high benefit changes to existing
    functionality.

--- pkg/sql/opt/invertedidx/json_array_test.go
+++ pkg/sql/opt/invertedidx/json_array_test.go
@@ -223,7 +223,7 @@ func TestTryFilterJsonOrArrayIndex(t *testing.T) {
 
 	tc := testcat.New()
 	if _, err := tc.ExecuteDDL(
-		"CREATE TABLE t (j JSON, a INT[], INVERTED INDEX (j), INVERTED INDEX (a))",
+		"CREATE TABLE t (j JSON, a INT[], str STRING[], INVERTED INDEX (j), INVERTED INDEX (a), INVERTED INDEX (str))",
 	); err != nil {
 		t.Fatal(err)
 	}
@@ -705,6 +705,105 @@ func TestTryFilterJsonOrArrayIndex(t *testing.T) {
 			unique:           false,
 			remainingFilters: "",
 		},
+		{
+			// Overlaps is supported for arrays.
+			// Overlaps with a single element array produces
+			// unique inverted span expression.
+			filters:          "a && '{1}'",
+			indexOrd:         arrayOrd,
+			ok:               true,
+			tight:            true,
+			unique:           true,
+			remainingFilters: "",
+		},
+		{
+			// Overlaps with an empty array produces a non-inverted
+			// expression.
+			filters:  "a && '{}'",
+			indexOrd: arrayOrd,
+			ok:       false,
+		},
+		{
+			// Overlaps with conjunction of two tight expressions with
+			// same variable produces a tight expression.
+			// Overlaps with conjunction of two expressions over same
+			// variable producing unique span expressions results in a
+			// unique span expression.
+			filters:          "a && '{1}' AND a && '{2}'",
+			indexOrd:         arrayOrd,
+			ok:               true,
+			tight:            true,
+			unique:           true,
+			remainingFilters: "",
+		},
+		{
+			// Overlaps with disjunction of two tight expressions with
+			// same variable produces a tight expression.
+			// Overlaps with disjunction of two expressions over same
+			// variable produces non-unique span expression.
+			filters:          "a && '{1}' OR a && '{2}'",
+			indexOrd:         arrayOrd,
+			ok:               true,
+			tight:            true,
+			unique:           false,
+			remainingFilters: "",
+		},
+		{
+			// When operations affecting two different variables are AND-ed,
+			// the first index gets constrained. If the two expressions
+			// produce unique results, the AND-ed result is also unique.
+			filters:          "a && '{1}' AND str && '{hello}'",
+			indexOrd:         arrayOrd,
+			ok:               true,
+			tight:            false,
+			unique:           true,
+			remainingFilters: "str && '{hello}'",
+		},
+		{
+			// When operations affecting two different variables are OR-ed, we cannot
+			// constrain either index.
+			filters:  "a && '{1}' OR str && '{hello}'",
+			indexOrd: arrayOrd,
+			ok:       false,
+		},
+		{
+			filters:  "a && '{1}' OR str <@ '{hello}'",
+			indexOrd: arrayOrd,
+			ok:       false,
+		},
+		{
+			filters:  "a && '{1}' OR str @> '{hello}'",
+			indexOrd: arrayOrd,
+			ok:       false,
+		},
+		{
+			// If the two expressions produce unique results,
+			// the AND-ed result is also unique.
+			filters:          "a && '{1}' AND str <@ '{hello}'",
+			indexOrd:         arrayOrd,
+			ok:               true,
+			tight:            false,
+			unique:           true,
+			remainingFilters: "str <@ '{hello}'",
+		},
+		{
+			filters:          "str <@ '{hello}' AND a && '{1}'",
+			indexOrd:         arrayOrd,
+			ok:               true,
+			tight:            false,
+			unique:           true,
+			remainingFilters: "str <@ '{hello}'",
+		},
+		{
+			// If all the expressions in a conjunction produce unique results,
+			// then the AND-ed result is also unique.
+			filters:          "str <@ '{hello}' AND a && '{1}' AND str @> '{hello}'",
+			indexOrd:         arrayOrd,
+			ok:               true,
+			tight:            false,
+			unique:           true,
+			remainingFilters: "str <@ '{hello}' AND str @> '{hello}'",
+		},
 	}
 
 	for _, tc := range testCases {
@@ -732,24 +831,24 @@ func TestTryFilterJsonOrArrayIndex(t *testing.T) {
 		}
 
 		if tc.tight != spanExpr.Tight {
-			t.Fatalf("expected tight=%v, but got %v", tc.tight, spanExpr.Tight)
+			t.Fatalf("For (%s), expected tight=%v, but got %v", tc.filters, tc.tight, spanExpr.Tight)
 		}
 		if tc.unique != spanExpr.Unique {
-			t.Fatalf("expected unique=%v, but got %v", tc.unique, spanExpr.Unique)
+			t.Fatalf("For (%s), expected unique=%v, but got %v", tc.filters, tc.unique, spanExpr.Unique)
 		}
 
 		if remainingFilters == nil {
 			if tc.remainingFilters != "" {
-				t.Fatalf("expected remainingFilters=%s, got <nil>", tc.remainingFilters)
+				t.Fatalf("For (%s), expected remainingFilters=%s, got <nil>", tc.filters, tc.remainingFilters)
 			}
 			continue
 		}
 		if tc.remainingFilters == "" {
-			t.Fatalf("expected remainingFilters=<nil>, got %v", remainingFilters)
+			t.Fatalf("For (%s), expected remainingFilters=<nil>, got %v", tc.filters, remainingFilters)
 		}
 		expRemainingFilters := testutils.BuildFilters(t, &f, &semaCtx, evalCtx, tc.remainingFilters)
 		if remainingFilters.String() != expRemainingFilters.String() {
-			t.Errorf("expected remainingFilters=%v, got %v", expRemainingFilters, remainingFilters)
+			t.Errorf("For (%s), expected remainingFilters=%v, got %v", tc.filters, expRemainingFilters, remainingFilters)
 		}
 	}
 }
