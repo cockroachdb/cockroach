ef05e995585836d51fe8800a899aedc75c19621d
Faizan Qazi <faizan@cockroachlabs.com>
2022-03-07 12:19:51 -0500

    sql/schemachanger: block implicit record types in expressions
    Previously, the declarative schema changer did not
    properly detect implicit record types properly, so
    certain add column operations were not blocked properly.
    This was inadequate because it could lead to scenarios,
    where we were incorrectly accessing table descriptors as
    type descriptors. To address this, this patch
    will update the evaluate logic to parse expressions
    appropriately and detect types which resolve to
    tables.
    
    Release note: None

--- pkg/sql/schemachanger/scbuild/builder_state.go
+++ pkg/sql/schemachanger/scbuild/builder_state.go
@@ -319,6 +319,12 @@ func newTypeT(t *types.T) scpb.TypeT {
 
 // WrapExpression implements the scbuildstmt.TableHelpers interface.
 func (b *builderState) WrapExpression(expr tree.Expr) *scpb.Expression {
+	// Implicit table record references cannot be detected properly unless,
+	// we reparse the expression after serializing.
+	expr, err := parser.ParseExpr(tree.Serialize(expr))
+	if err != nil {
+		panic(err)
+	}
 	if expr == nil {
 		return nil
 	}
@@ -336,7 +342,14 @@ func (b *builderState) WrapExpression(expr tree.Expr) *scpb.Expression {
 				panic(err)
 			}
 			b.ensureDescriptor(id)
-			typ, err := catalog.AsTypeDescriptor(b.descCache[id].desc)
+			desc := b.descCache[id].desc
+			// Implicit record types will lead to table references, which will be
+			// disallowed.
+			if desc.DescriptorType() == catalog.Table {
+				panic(pgerror.Newf(pgcode.DependentObjectsStillExist,
+					"cannot modify table record type %q", desc.GetName()))
+			}
+			typ, err := catalog.AsTypeDescriptor(desc)
 			if err != nil {
 				panic(err)
 			}
