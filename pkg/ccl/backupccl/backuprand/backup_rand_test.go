a4a70f39506935be24cf4645b69c231745499314
Michael Butler <butler@cockroachlabs.com>
2022-03-09 20:08:02 -0500

    backupccl: add random data to tables in TestBackupRestoreDataRoundtrips
    Previously, TestBackupRestoreDataRoundtrips did not test if data in the
    randomly created tables were preserved after a backup restore roundtrip.
    This patch tries to add random data to each table and tests that the data is
    preserved across the roundtrip. In addition, this pr moves this test
    to its own package because the test takes a long time to run.
    
    Release note: None

--- pkg/ccl/backupccl/backuprand/backup_rand_test.go
+++ pkg/ccl/backupccl/backuprand/backup_rand_test.go
@@ -1,4 +1,4 @@
-// Copyright 2021 The Cockroach Authors.
+// Copyright 2022 The Cockroach Authors.
 //
 // Licensed as a CockroachDB Enterprise file under the Cockroach Community
 // License (the "License"); you may not use this file except in compliance with
@@ -6,7 +6,7 @@
 //
 //     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
 
-package backupccl
+package backuprand
 
 import (
 	"context"
@@ -15,7 +15,10 @@ import (
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
+	_ "github.com/cockroachdb/cockroach/pkg/ccl"
+	"github.com/cockroachdb/cockroach/pkg/ccl/backupccl/backuputils"
 	"github.com/cockroachdb/cockroach/pkg/internal/sqlsmith"
+	"github.com/cockroachdb/cockroach/pkg/sql/randgen"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
@@ -26,10 +29,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TestBackupRestoreRandomDataRoundtrips creates random tables using SQLSmith
-// and backs up and restores them, ensuring that the schema is properly
-// preserved across the roundtrip. It tests that full database backup as well
-// as all subsets of per-table backup roundtrip properly.
+// TestBackupRestoreRandomDataRoundtrips conducts backup/restore roundtrips on
+// randomly generated tables and verifies their data and schema are preserved.
+// It tests that full database backup as well as all subsets of per-table backup
+// roundtrip properly.
 func TestBackupRestoreRandomDataRoundtrips(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
@@ -46,10 +49,12 @@ func TestBackupRestoreRandomDataRoundtrips(t *testing.T) {
 			ExternalIODir:            dir,
 		},
 	}
+	const localFoo = "nodelocal://0/foo/"
+
 	ctx := context.Background()
-	tc := testcluster.StartTestCluster(t, singleNode, params)
+	tc := testcluster.StartTestCluster(t, 1, params)
 	defer tc.Stopper().Stop(ctx)
-	InitManualReplication(tc)
+	tc.ToggleReplicateQueues(false)
 	sqlDB := sqlutils.MakeSQLRunner(tc.Conns[0])
 	sqlDB.Exec(t, "CREATE DATABASE rand")
 
@@ -59,6 +64,7 @@ func TestBackupRestoreRandomDataRoundtrips(t *testing.T) {
 			t.Fatal(err)
 		}
 	}
+	numInserts := 20
 
 	tables := sqlDB.Query(t, `SELECT name FROM crdb_internal.tables WHERE 
 database_name = 'rand' AND schema_name = 'public'`)
@@ -68,24 +74,21 @@ database_name = 'rand' AND schema_name = 'public'`)
 		if err := tables.Scan(&tableName); err != nil {
 			t.Fatal(err)
 		}
+		// Note: we do not care how many rows successfully populate
+		// the given table
+		if _, err := randgen.PopulateTableWithRandData(rng, tc.Conns[0], tableName,
+			numInserts); err != nil {
+			t.Fatal(err)
+		}
 		tableNames = append(tableNames, tableName)
 	}
 
-	getCreateStatementForTable := func(tableName string) string {
-		var ignored, createStatement string
-		table := sqlDB.QueryRow(t, fmt.Sprintf("SHOW CREATE TABLE %s", tableName))
-		table.Scan(&ignored, &createStatement)
-		return createStatement
-	}
-
 	expectedCreateTableStmt := make(map[string]string)
+	expectedData := make(map[string][][]string)
 	for _, tableName := range tableNames {
-		createStatement := getCreateStatementForTable(fmt.Sprintf("rand.%s", tableName))
-		expectedCreateTableStmt[tableName] = createStatement
+		expectedCreateTableStmt[tableName] = sqlDB.QueryStr(t, fmt.Sprintf(`SELECT create_statement FROM [SHOW CREATE TABLE %s]`, tableName))[0][0]
+		expectedData[tableName] = sqlDB.QueryStr(t, fmt.Sprintf(`SELECT * FROM %s`, tableName))
 	}
-
-	// TODO(jordan): we should insert random data using SQLSmith mutation
-	// statements here.
 
 	// Now that we've created our random tables, backup and restore the whole DB
 	// and compare all table descriptors for equality.
@@ -93,13 +96,12 @@ database_name = 'rand' AND schema_name = 'public'`)
 	dbBackup := localFoo + "wholedb"
 	tablesBackup := localFoo + "alltables"
 	dbBackups := []string{dbBackup, tablesBackup}
-
-	if err := verifyBackupRestoreStatementResult(
+	if err := backuputils.VerifyBackupRestoreStatementResult(
 		t, sqlDB, "BACKUP DATABASE rand TO $1", dbBackup,
 	); err != nil {
 		t.Fatal(err)
 	}
-	if err := verifyBackupRestoreStatementResult(
+	if err := backuputils.VerifyBackupRestoreStatementResult(
 		t, sqlDB, "BACKUP TABLE rand.* TO $1", tablesBackup,
 	); err != nil {
 		t.Fatal(err)
@@ -111,8 +113,12 @@ database_name = 'rand' AND schema_name = 'public'`)
 	verifyTables := func(t *testing.T, tableNames []string) {
 		for _, tableName := range tableNames {
 			t.Logf("Verifying table %s", tableName)
-			createStatement := getCreateStatementForTable("restoredb." + tableName)
-			assert.Equal(t, expectedCreateTableStmt[tableName], createStatement, "SHOW CREATE %s not equal after RESTORE", tableName)
+			restoreTable := "restoredb." + tableName
+			createStmt := sqlDB.QueryStr(t,
+				fmt.Sprintf(`SELECT create_statement FROM [SHOW CREATE TABLE %s]`, restoreTable))[0][0]
+			assert.Equal(t, expectedCreateTableStmt[tableName], createStmt,
+				"SHOW CREATE %s not equal after RESTORE", tableName)
+			sqlDB.CheckQueryResults(t, fmt.Sprintf(`SELECT * FROM %s`, tableName), expectedData[tableName])
 		}
 	}
 
@@ -122,7 +128,7 @@ database_name = 'rand' AND schema_name = 'public'`)
 	for _, backup := range dbBackups {
 		sqlDB.Exec(t, "DROP DATABASE IF EXISTS restoredb")
 		sqlDB.Exec(t, "CREATE DATABASE restoredb")
-		if err := verifyBackupRestoreStatementResult(
+		if err := backuputils.VerifyBackupRestoreStatementResult(
 			t, sqlDB, "RESTORE rand.* FROM $1 WITH OPTIONS (into_db='restoredb')", backup,
 		); err != nil {
 			t.Fatal(err)
@@ -130,7 +136,7 @@ database_name = 'rand' AND schema_name = 'public'`)
 		verifyTables(t, tableNames)
 		sqlDB.Exec(t, "DROP DATABASE IF EXISTS restoredb")
 
-		if err := verifyBackupRestoreStatementResult(
+		if err := backuputils.VerifyBackupRestoreStatementResult(
 			t, sqlDB, "RESTORE DATABASE rand FROM $1 WITH OPTIONS (new_db_name='restoredb')", backup,
 		); err != nil {
 			t.Fatal(err)
