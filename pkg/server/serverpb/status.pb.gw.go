1692b3fe0fd577728fa08cbe4633d0ccd9dc4c60
Alex Barganier <abarganier@cockroachlabs.com>
2022-03-11 13:32:30 -0400

    pkg/server: add pagination fields to TenantRangesRequest/Response protos
    The `(*statusServer).Ranges()` endpoint, up until recently, did not contain
    any offset pagination fields in its gRPC request protos even though pagination
    is supported under the hood.
    
    Now that `Ranges()` supports pagination for gRPC clients, we can extend
    that pagination functionality to the `TenantRanges` endpoint as well.
    
    The pagination for `TenantRanges()` will limit the number of results returned
    *by each node*. This means that a `limit = 100` value for a tenant whose
    ranges are replicated three times, will return 300 results (100 results for
    each node containing a Replica).
    
    Release note: none
    
    Release justification: Low risk, high benefit changes to existing functionality

--- pkg/server/serverpb/status.pb.gw.go
+++ pkg/server/serverpb/status.pb.gw.go
@@ -393,9 +393,20 @@ func local_request_Status_Ranges_0(ctx context.Context, marshaler runtime.Marsha
 
 }
 
+var (
+	filter_Status_TenantRanges_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_Status_TenantRanges_0(ctx context.Context, marshaler runtime.Marshaler, client StatusClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq TenantRangesRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Status_TenantRanges_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := client.TenantRanges(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -405,6 +416,13 @@ func request_Status_TenantRanges_0(ctx context.Context, marshaler runtime.Marsha
 func local_request_Status_TenantRanges_0(ctx context.Context, marshaler runtime.Marshaler, server StatusServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq TenantRangesRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Status_TenantRanges_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := server.TenantRanges(ctx, &protoReq)
 	return msg, metadata, err
