e4a7903b12f4f1e16686181f9e70508210015ff2
Faizan Qazi <faizan@cockroachlabs.com>
2022-03-13 23:14:00 -0400

    workload/schemachange: disable schemachange workload
    Previously, the schemachange workload was enabled on 21.2,
    but due to a number of test issues could lead to flaky
    failures. This was inadequate because it created tons of
    noise for developers to investigate. To address this,
    this patch will temporarily disable this test in 21.2,
    since fixes in the next release will improve the stability
    and eliminate noise.
    
    Release note: None

--- pkg/cmd/roachtest/tests/schemachange_random_load.go
+++ pkg/cmd/roachtest/tests/schemachange_random_load.go
@@ -89,6 +89,10 @@ func registerRandomLoadBenchSpec(r registry.Registry, b randomLoadBenchSpec) {
 		// fix (or bypass) minor schema change bugs that are discovered.
 		NonReleaseBlocker: true,
 		Run: func(ctx context.Context, t test.Test, c cluster.Cluster) {
+			// The schemachange workload is extremely noisy in 21.2, so we are going
+			// leave it disabled until the workload gets fully stabilized in then next
+			// release (via epic: CRDB-13725).
+			t.Skip("disabled in 21.2 since schemachange workload is not stable")
 			runSchemaChangeRandomLoad(ctx, t, c, b.Ops, b.Concurrency)
 		},
 	})
