4b894b3457327f6c021306d2f9e3f2a88abf1435
Erik Grinaker <grinaker@cockroachlabs.com>
2022-03-15 17:49:39 +0000

    storage: add `IterOptions.RangeKeyMaskingBelow`
    This patch adds an iterator option `RangeKeyMaskingBelow`, which will
    cause range keys at or below the given timestamp to mask any point keys
    below them. This can be used to optimize callers that do not care about
    deleted point keys, since Pebble will not emit these.
    
    Release note: None

--- pkg/storage/engine_test.go
+++ pkg/storage/engine_test.go
@@ -2027,8 +2027,9 @@ func TestEngineRangeKeysUnsupported(t *testing.T) {
 				require.False(t, r.SupportsRangeKeys())
 
 				iter := r.NewMVCCIterator(MVCCKeyAndIntentsIterKind, IterOptions{
-					KeyTypes:   keyType,
-					UpperBound: keys.MaxKey,
+					KeyTypes:             keyType,
+					UpperBound:           keys.MaxKey,
+					RangeKeyMaskingBelow: hlc.Timestamp{WallTime: 1}, // should get disabled when unsupported
 				})
 				defer iter.Close()
 
