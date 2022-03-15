33926a0af14a97498f13b590e740f85072146637
Erik Grinaker <grinaker@cockroachlabs.com>
2022-03-15 17:49:39 +0000

    storage: add `IterOptions.RangeKeyMaskingBelow`
    This patch adds an iterator option `RangeKeyMaskingBelow`, which will
    cause range keys at or below the given timestamp to mask any point keys
    below them. This can be used to optimize callers that do not care about
    deleted point keys, since Pebble will not emit these.
    
    Release note: None

--- pkg/storage/mvcc_history_test.go
+++ pkg/storage/mvcc_history_test.go
@@ -69,7 +69,7 @@ import (
 // get            [t=<name>] [ts=<int>[,<int>]]                         [resolve [status=<txnstatus>]] k=<key> [inconsistent] [tombstones] [failOnMoreRecent] [localUncertaintyLimit=<int>[,<int>]] [globalUncertaintyLimit=<int>[,<int>]]
 // scan           [t=<name>] [ts=<int>[,<int>]]                         [resolve [status=<txnstatus>]] k=<key> [end=<key>] [inconsistent] [tombstones] [reverse] [failOnMoreRecent] [localUncertaintyLimit=<int>[,<int>]] [globalUncertaintyLimit=<int>[,<int>]] [max=<max>] [targetbytes=<target>] [avoidExcess] [allowEmpty]
 //
-// iter           [k=<key>] [end=<key>] [kind=key|keyAndIntents] [types=pointsOnly|pointsWithRanges|pointsAndRanges|rangesOnly]
+// iter           [k=<key>] [end=<key>] [kind=key|keyAndIntents] [types=pointsOnly|pointsWithRanges|pointsAndRanges|rangesOnly] [maskBelow=<int>[,<int>]]
 // iter_seek_ge   k=<key> [ts=<int>[,<int>]]
 // iter_seek_lt   k=<key> [ts=<int>[,<int>]]
 // iter_seek_intent_ge k=<key> txn=<name>
@@ -969,6 +969,9 @@ func cmdIterNew(e *evalCtx) error {
 		default:
 			return errors.Errorf("unknown key type %s", arg)
 		}
+	}
+	if e.hasArg("maskBelow") {
+		opts.RangeKeyMaskingBelow = e.getTsWithName("maskBelow")
 	}
 
 	var r, closeReader Reader
