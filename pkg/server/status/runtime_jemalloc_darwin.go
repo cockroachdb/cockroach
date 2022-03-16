beb9d806dda8ba70c7bc0a7c986c84e32fec498f
Ricky Stewart <ricky@cockroachlabs.com>
2022-03-16 13:32:25 -0500

    build: integrate new macOS cross toolchains w/ Bazel
    Release note: None

--- pkg/server/status/runtime_jemalloc_darwin.go
+++ pkg/server/status/runtime_jemalloc_darwin.go
@@ -13,11 +13,11 @@
 
 package status
 
-// extern void je_zone_register();
+// extern void zone_register();
 import "C"
 
 func init() {
-	// On macOS, je_zone_register is run at init time to register jemalloc with
+	// On macOS, zone_register is run at init time to register jemalloc with
 	// the system allocator. Unfortunately, all the machinery for this is in a
 	// single file, and is not referenced elsewhere, causing the linker to omit
 	// the file's symbols. Manually force the presence of these symbols on macOS
@@ -29,5 +29,5 @@ func init() {
 	//
 	// See https://github.com/jemalloc/jemalloc/issues/708 and the references
 	// within.
-	C.je_zone_register()
+	C.zone_register()
 }
