fc90e7af58331edfe2b440b87fa69c8be9b00693
Aditya Maru <adityamaru@gmail.com>
2022-03-17 23:20:56 +0530

    backupccl: move restore OnFailOrCancel test to datadriven
    This change moves all the tests related to cleanup of restore
    on fail or cancel to the datadriven framework. It also adds more
    instructions that are supported by the DD framework to help writing
    these tests.
    
    Release note: None

--- pkg/ccl/backupccl/restore_job.go
+++ pkg/ccl/backupccl/restore_job.go
@@ -1348,6 +1348,10 @@ func (r *restoreResumer) doResume(ctx context.Context, execCtx interface{}) erro
 		}
 
 		p.ExecCfg().JobRegistry.NotifyToAdoptJobs()
+		if err := p.ExecCfg().JobRegistry.CheckPausepoint(
+			"restore.after_publishing_descriptors"); err != nil {
+			return err
+		}
 		if fn := r.testingKnobs.afterPublishingDescriptors; fn != nil {
 			if err := fn(); err != nil {
 				return err
@@ -1484,6 +1488,10 @@ func (r *restoreResumer) doResume(ctx context.Context, execCtx interface{}) erro
 		}
 	}
 
+	if err := p.ExecCfg().JobRegistry.CheckPausepoint(
+		"restore.after_publishing_descriptors"); err != nil {
+		return err
+	}
 	if fn := r.testingKnobs.afterPublishingDescriptors; fn != nil {
 		if err := fn(); err != nil {
 			return err
@@ -1715,6 +1723,11 @@ func (r *restoreResumer) publishDescriptors(
 	if details.DescriptorsPublished {
 		return nil
 	}
+
+	if err := execCfg.JobRegistry.CheckPausepoint("restore.before_publishing_descriptors"); err != nil {
+		return err
+	}
+
 	if fn := r.testingKnobs.beforePublishingDescriptors; fn != nil {
 		if err := fn(); err != nil {
 			return err
@@ -1932,6 +1945,8 @@ func emitRestoreJobEvent(
 // change stuff to delete the keys in the background.
 func (r *restoreResumer) OnFailOrCancel(ctx context.Context, execCtx interface{}) error {
 	p := execCtx.(sql.JobExecContext)
+	r.execCfg = p.ExecCfg()
+
 	// Emit to the event log that the job has started reverting.
 	emitRestoreJobEvent(ctx, p, jobs.StatusReverting, r.job)
 
