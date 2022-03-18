9731e5cc055673248e54ac8f09662b259ca5479c
rharding6373 <rharding6373@users.noreply.github.com>
2022-03-18 11:30:12 -0700

    sql: adds unique constraint checks to SCRUB
    The SCRUB tool validates table constraints. Before this change, it could
    only validate `CHECK` and foreign key constraints. This PR enhances
    SCRUB to also validate tables that contain `PRIMARY KEY`, `UNIQUE`, or
    the experimental `UNIQUE WITHOUT INDEX` keywords.
    
    Fixes: #73632
    
    Release note (sql change): This PR expands the capabilities of the experimental
    SCRUB tool to include checking unique constraints for primary keys,
    unique indexes, and unique columns without indexes. The usage and output
    of SCRUB is unchanged, but if there is a unique constraint violation,
    users will see the error message `unique_constraint_violation` for all
    rows that violate the constraint, along with information about the row.

---
+++ pkg/sql/scrub/scrubtestutils/scrub_test_helper.go
@@ -0,0 +1,84 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package scrubtestutils
+
+import (
+	gosql "database/sql"
+	"regexp"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+)
+
+type ExpectedScrubResult struct {
+	ErrorType    string
+	Database     string
+	Table        string
+	PrimaryKey   string
+	Repaired     bool
+	DetailsRegex string
+}
+
+func CheckScrubResult(t *testing.T, ress []sqlutils.ScrubResult, exps []ExpectedScrubResult) {
+	t.Helper()
+
+	for i := 0; i < len(exps); i++ {
+		res := ress[i]
+		exp := exps[i]
+		if res.ErrorType != exp.ErrorType {
+			t.Errorf("expected %q error, instead got: %s", exp.ErrorType, res.ErrorType)
+		}
+
+		if res.Database != exp.Database {
+			t.Errorf("expected database %q, got %q", exp.Database, res.Database)
+		}
+
+		if res.Table != exp.Table {
+			t.Errorf("expected table %q, got %q", exp.Table, res.Table)
+		}
+
+		if res.PrimaryKey != exp.PrimaryKey {
+			t.Errorf("expected primary key %q, got %q", exp.PrimaryKey, res.PrimaryKey)
+		}
+		if res.Repaired != exp.Repaired {
+			t.Fatalf("expected repaired %v, got %v", exp.Repaired, res.Repaired)
+		}
+
+		if matched, err := regexp.MatchString(exp.DetailsRegex, res.Details); err != nil {
+			t.Fatal(err)
+		} else if !matched {
+			t.Errorf("expected error details to contain `%s`, got `%s`", exp.DetailsRegex, res.Details)
+		}
+	}
+}
+
+// RunScrub runs a SCRUB statement and checks that it returns exactly one scrub
+// result and that it matches the expected result.
+func RunScrub(t *testing.T, db *gosql.DB, scrubStmt string, exp []ExpectedScrubResult) {
+	t.Helper()
+
+	// Run SCRUB and find the violation created.
+	rows, err := db.Query(scrubStmt)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	defer rows.Close()
+
+	results, err := sqlutils.GetScrubResultRows(rows)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	if len(results) != len(exp) {
+		t.Fatalf("expected %d results, got %d. got %#v", len(exp), len(results), results)
+	}
+	CheckScrubResult(t, results, exp)
+}
---
+++ pkg/sql/scrub_unique_constraint.go
@@ -0,0 +1,210 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package sql
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/scrub"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/catid"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+)
+
+// sqlUniqueConstraintCheckOperation is a check which validates a
+// UNIQUE constraint on a table.
+type sqlUniqueConstraintCheckOperation struct {
+	tableName  *tree.TableName
+	tableDesc  catalog.TableDescriptor
+	constraint *descpb.ConstraintDetail
+	cols       []catid.ColumnID
+	name       string
+	asOf       hlc.Timestamp
+	predicate  string
+
+	// columns is a list of the columns returned in the query result
+	// tree.Datums.
+	columns []catalog.Column
+	// primaryColIdxs maps PrimaryIndex.Columns to the row
+	// indexes in the query result tree.Datums.
+	primaryColIdxs []int
+
+	run sqlCheckConstraintCheckRun
+}
+
+// sqlCheckConstraintCheckRun contains the run-time state for
+// sqlUniqueConstraintCheckOperation during local execution.
+type sqlUniqueConstraintCheckRun struct {
+	started  bool
+	rows     []tree.Datums
+	rowIndex int
+}
+
+func newSQLUniqueConstraintCheckOperation(
+	tableName *tree.TableName,
+	tableDesc catalog.TableDescriptor,
+	constraint descpb.ConstraintDetail,
+	asOf hlc.Timestamp,
+) *sqlUniqueConstraintCheckOperation {
+	op := sqlUniqueConstraintCheckOperation{
+		tableName:  tableName,
+		tableDesc:  tableDesc,
+		constraint: &constraint,
+		asOf:       asOf,
+	}
+	if constraint.UniqueWithoutIndexConstraint != nil {
+		op.cols = constraint.UniqueWithoutIndexConstraint.ColumnIDs
+		op.name = constraint.UniqueWithoutIndexConstraint.Name
+		op.predicate = constraint.UniqueWithoutIndexConstraint.Predicate
+	} else if constraint.Index != nil {
+		op.cols = constraint.Index.KeyColumnIDs
+		// Partitioning columns are prepended to the index but are not part of the
+		// unique constraint, so we ignore them.
+		if constraint.Index.Partitioning.NumImplicitColumns > 0 {
+			op.cols = op.cols[constraint.Index.Partitioning.NumImplicitColumns:]
+		}
+		op.name = constraint.Index.Name
+		op.predicate = constraint.Index.Predicate
+	}
+	return &op
+}
+
+// Start implements the checkOperation interface.
+// It creates a SELECT expression and generates a plan from it, which
+// then runs in the distSQL execution engine.
+func (o *sqlUniqueConstraintCheckOperation) Start(params runParams) error {
+	ctx := params.ctx
+	// Create a query of the form:
+	// SELECT a,b,c FROM db.t AS tbl1 JOIN
+	//   (SELECT b, c FROM db.t GROUP BY b, c
+	//     WHERE b IS NOT NULL AND c IS NOT NULL [AND partial index predicate]
+	//     HAVING COUNT(*) > 1) as tbl2
+	//   ON tbl1.b = tbl2.b AND tbl1.c = tbl2.c;
+	// Where a, b, and c are all the public columns in table db.t and b and c are
+	// the unique columns. We select all public columns to provide detailed
+	// information if there are constraint violations.
+
+	// Collect all the columns.
+	o.columns = o.tableDesc.PublicColumns()
+
+	// Make a list of all the unique column names.
+	keyCols := make([]string, len(o.cols))
+	matchers := make([]string, len(o.cols))
+	for i := 0; i < len(o.cols); i++ {
+		col, err := o.tableDesc.FindColumnWithID(o.cols[i])
+		if err != nil {
+			return err
+		}
+		keyCols[i] = tree.NameString(col.GetName())
+		matchers[i] = fmt.Sprintf("tbl1.%[1]s=tbl2.%[1]s", keyCols[i])
+	}
+	// Make a list of all the public column names.
+	pCols := make([]string, len(o.columns))
+	for i := 0; i < len(o.columns); i++ {
+		pCols[i] = fmt.Sprintf("tbl1.%[1]s", tree.NameString(o.columns[i].GetName()))
+	}
+	asOf := ""
+	if o.asOf != hlc.MaxTimestamp {
+		asOf = fmt.Sprintf("AS OF SYSTEM TIME %[1]d",
+			o.asOf.WallTime)
+	}
+	tableName := fmt.Sprintf("%s.%s", o.tableName.Catalog(), o.tableName.Table())
+	dup, _, err := duplicateRowQuery(o.tableDesc, o.cols, o.predicate, false /* limitResults */)
+	if err != nil {
+		return err
+	}
+
+	sel := fmt.Sprintf(`SELECT %[1]s 
+FROM %[2]s AS tbl1 JOIN 
+(%[3]s) AS tbl2 
+ON %[4]s
+%[5]s `,
+		strings.Join(pCols, ","),        // 1
+		tableName,                       // 2
+		dup,                             // 3
+		strings.Join(matchers, " AND "), // 4
+		asOf,                            // 5
+	)
+
+	rows, err := params.extendedEvalCtx.ExecCfg.InternalExecutor.QueryBuffered(
+		ctx, "scrub-unique", params.p.txn, sel,
+	)
+	if err != nil {
+		return err
+	}
+
+	o.run.started = true
+	o.run.rows = rows
+	// Find the row indexes for all of the primary index columns.
+	o.primaryColIdxs, err = getPrimaryColIdxs(o.tableDesc, o.columns)
+	return nil
+}
+
+// Next implements the checkOperation interface.
+func (o *sqlUniqueConstraintCheckOperation) Next(params runParams) (tree.Datums, error) {
+	row := o.run.rows[o.run.rowIndex]
+	o.run.rowIndex++
+	timestamp, err := tree.MakeDTimestamp(
+		params.extendedEvalCtx.GetStmtTimestamp(),
+		time.Nanosecond,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	var primaryKeyDatums tree.Datums
+	for _, rowIdx := range o.primaryColIdxs {
+		primaryKeyDatums = append(primaryKeyDatums, row[rowIdx])
+	}
+
+	details := make(map[string]interface{})
+	rowDetails := make(map[string]interface{})
+	details["row_data"] = rowDetails
+	details["constraint_name"] = o.name
+	for rowIdx, col := range o.columns {
+		rowDetails[col.GetName()] = row[rowIdx].String()
+	}
+	detailsJSON, err := tree.MakeDJSON(details)
+	if err != nil {
+		return nil, err
+	}
+
+	return tree.Datums{
+		tree.DNull, /* job_uuid */
+		tree.NewDString(scrub.UniqueConstraintViolation),
+		tree.NewDString(o.tableName.Catalog()),
+		tree.NewDString(o.tableName.Table()),
+		tree.NewDString(primaryKeyDatums.String()),
+		timestamp,
+		tree.DBoolFalse,
+		detailsJSON,
+	}, nil
+}
+
+// Started implements the checkOperation interface.
+func (o *sqlUniqueConstraintCheckOperation) Started() bool {
+	return o.run.started
+}
+
+// Done implements the checkOperation interface.
+func (o *sqlUniqueConstraintCheckOperation) Done(ctx context.Context) bool {
+	return o.run.rows == nil || o.run.rowIndex >= len(o.run.rows)
+}
+
+// Close implements the checkOperation interface.
+func (o *sqlUniqueConstraintCheckOperation) Close(ctx context.Context) {
+	o.run.rows = nil
+}
