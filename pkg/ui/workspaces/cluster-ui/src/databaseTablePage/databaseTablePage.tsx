e3fec2edab0389ec2a2e39b4ad06276a09864891
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2022-03-22 11:12:50 -0400

    ui: Hide table stats collection setting for non-admins
    Release justification: low-risk change
    
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/databaseTablePage/databaseTablePage.tsx
+++ pkg/ui/workspaces/cluster-ui/src/databaseTablePage/databaseTablePage.tsx
@@ -91,7 +91,7 @@ export interface DatabaseTablePageData {
   stats: DatabaseTablePageDataStats;
   indexStats: DatabaseTablePageIndexStats;
   showNodeRegionsSection?: boolean;
-  automaticStatsCollectionEnabled: boolean;
+  automaticStatsCollectionEnabled?: boolean;
 }
 
 export interface DatabaseTablePageDataDetails {
@@ -372,27 +372,29 @@ export class DatabaseTablePage extends React.Component<
                         )}
                       />
                     )}
-                    <SummaryCardItemBoolSetting
-                      label="Auto Stats Collection"
-                      value={this.props.automaticStatsCollectionEnabled}
-                      toolTipText={
-                        <span>
-                          {" "}
-                          Automatic statistics can help improve query
-                          performance. Learn how to{" "}
-                          <Anchor
-                            href={tableStatsClusterSetting}
-                            target="_blank"
-                            className={booleanSettingCx(
-                              "crl-hover-text__link-text",
-                            )}
-                          >
-                            manage statistics collection
-                          </Anchor>
-                          .
-                        </span>
-                      }
-                    />
+                    {this.props.automaticStatsCollectionEnabled != null && (
+                      <SummaryCardItemBoolSetting
+                        label="Auto Stats Collection"
+                        value={this.props.automaticStatsCollectionEnabled}
+                        toolTipText={
+                          <span>
+                            {" "}
+                            Automatic statistics can help improve query
+                            performance. Learn how to{" "}
+                            <Anchor
+                              href={tableStatsClusterSetting}
+                              target="_blank"
+                              className={booleanSettingCx(
+                                "crl-hover-text__link-text",
+                              )}
+                            >
+                              manage statistics collection
+                            </Anchor>
+                            .
+                          </span>
+                        }
+                      />
+                    )}
                   </SummaryCard>
                 </Col>
 
