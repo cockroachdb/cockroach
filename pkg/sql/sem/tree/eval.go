84db765a5ca1283c7ee3862db94b151a0146f04d
Marcus Gartner <marcus@cockroachlabs.com>
2022-03-24 14:27:01 +0000

    sql: fix ColumnAccessExpr.Eval with NULL inner expression
    Previously, `ColumnAccessExpr.Eval` would panic if the
    `ColumnAccessExpr`'s inner expression evaluated to `NULL`, because it
    attempted to cast this `NULL` to a `DTuple`. Now, if the inner
    expression is `NULL`, `ColumnAccessExpr.Eval` returns `NULL`.
    
    Fixes #78159
    
    Release note (bug fix): A bug has been fixed that caused an internal
    error when the inner expression of a column access expression evaluated
    to `NULL`. For example, evaluation of the expression
    `(CASE WHEN b THEN ((ROW(1) AS a)) ELSE NULL END).a` would error when
    `b` is `false`. This bug has been present since version 19.1 or earlier.

--- pkg/sql/sem/tree/eval.go
+++ pkg/sql/sem/tree/eval.go
@@ -3927,6 +3927,9 @@ func (expr *ColumnAccessExpr) Eval(ctx *EvalContext) (Datum, error) {
 	if err != nil {
 		return nil, err
 	}
+	if d == DNull {
+		return d, nil
+	}
 	return d.(*DTuple).D[expr.ColIndex], nil
 }
 
