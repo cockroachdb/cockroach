37d3c8c7d388802f0547ec63b1b861d81c851138
Ricky Stewart <ricky@cockroachlabs.com>
2022-03-24 11:22:43 -0500

    ci: report failure to generate code as a test failure to teamcity
    Closes #78368.
    
    Release note: None

--- build/teamcity-check-genfiles.sh
+++ build/teamcity-check-genfiles.sh
@@ -9,6 +9,8 @@ tc_prepare
 
 tc_start_block "Ensure generated code is up-to-date"
 
+begin_check_generated_code_tests
+
 # NOTE(ricky): Please make sure any changes to the Bazel-related checks here are
 # propagated to build/teamcity/cockroach/ci/tests/check_generated_code_impl.sh
 # as well.
@@ -29,24 +31,26 @@ if grep TODO DEPS.bzl; then
     echo "Missing TODO comment in DEPS.bzl. Did you run \`./dev generate bazel --mirror\`?"
     exit 1
 fi
-check_workspace_clean "Run \`./dev generate bazel\` to automatically regenerate these."
+check_workspace_clean 'dev_generate_bazel' "Run \`./dev generate bazel\` to automatically regenerate these."
 run build/builder.sh make generate &> artifacts/generate.log || (cat artifacts/generate.log && false)
 rm artifacts/generate.log
-check_workspace_clean "Run \`make generate\` to automatically regenerate these."
+check_workspace_clean 'make_generate' "Run \`make generate\` to automatically regenerate these."
 run build/builder.sh make buildshort &> artifacts/buildshort.log || (cat artifacts/buildshort.log && false)
 rm artifacts/buildshort.log
-check_workspace_clean "Run \`make buildshort\` to automatically regenerate these."
+check_workspace_clean 'make_buildshort' "Run \`make buildshort\` to automatically regenerate these."
 tc_end_block "Ensure generated code is up-to-date"
 
 # generated code can generate new dependencies; check dependencies after generated code.
 tc_start_block "Ensure dependencies are up-to-date"
 # Run go mod tidy and `make -k vendor_rebuild` and ensure nothing changes.
 run build/builder.sh go mod tidy
-check_workspace_clean "Run \`go mod tidy\` and \`make -k vendor_rebuild\` to automatically regenerate these."
+check_workspace_clean 'go_mod_tidy' "Run \`go mod tidy\` and \`make -k vendor_rebuild\` to automatically regenerate these."
 run build/builder.sh make -k vendor_rebuild
 cd vendor
-check_workspace_clean "Run \`make -k vendor_rebuild\` to automatically regenerate these."
+check_workspace_clean 'vendor_rebuild' "Run \`make -k vendor_rebuild\` to automatically regenerate these."
 cd ..
+
+end_check_generated_code_tests
 tc_end_block "Ensure dependencies are up-to-date"
 
 tc_start_block "Test web UI"
