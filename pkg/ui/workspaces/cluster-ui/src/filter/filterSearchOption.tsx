9f846437296e90e87d441da3c7aeadb7879be6a2
Andrii Vorobiov <and.vorobiov@gmail.com>
2022-03-24 15:53:56 +0200

    ui: add FilterDropdown component
    Currently, `DbConsole` and `Cluster-UI` don't have any
    reusable filter component to use on new pages. Most
    relevant component is `Filter` from `cluster-ui` package
    that is designed specifically for Statements page and
    cannot be reused anywhere else.
    
    This change is a first attempt to add new component that
    represents basic building blocks for Dropdown Filters
    that are reusable. It will be used in Hot Ranges page.
    
    Release note: None

---
+++ pkg/ui/workspaces/cluster-ui/src/filter/filterSearchOption.tsx
@@ -0,0 +1,34 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import React from "react";
+import { Search } from "../search";
+import { filterLabel } from "../queryFilter/filterClasses";
+
+export type FilterSearchOptionProps = {
+  label: string;
+  onChanged?: (value: string) => void;
+  value?: string;
+};
+
+export const FilterSearchOption = (props: FilterSearchOptionProps) => {
+  const { label, onChanged, value } = props;
+  return (
+    <div>
+      <div className={filterLabel.margin}>{label}</div>
+      <Search
+        onChange={onChanged}
+        renderSuffix={false}
+        placeholder="Search"
+        value={value}
+      />
+    </div>
+  );
+};
