52605916a7e3a2d99e55cd7f671a4a9ea3c44b13
Matthew Todd <todd@cockroachlabs.com>
2022-03-28 12:14:07 -0400

    sql: optionally group statement statistics by transaction fingerprint ID
    Fixes #78338.
    
    We have seen some workloads where the same statements are executed
    across multiple ad-hoc transactions, each with a different transaction
    fingerprint ID, resulting in so many rows being flushed to the
    `system.statement_statistics` table that the cleanup job isn't able to
    keep up.
    
    This commit introduces a new cluster setting,
    `sql.stats.associate_stmt_with_txn_fingerprint.enabled`,
    that may be disabled to mitigate against the impact of these
    high-cardinality statistics.
    
    (Not filing a release note as this is not intended to be a public
    cluster setting.)
    
    Release note: None

---
+++ pkg/sql/sqlstats/sslocal/cluster_settings.go
@@ -0,0 +1,16 @@
+package sslocal
+
+import "github.com/cockroachdb/cockroach/pkg/settings"
+
+// AssociateStmtWithTxnFingerprint determines whether to segment
+// per-statment statistics by transaction fingerprint. While enabled by
+// default, it may be useful to disable for workloads that run the same
+// statements across many (ad-hoc) transaction fingerprints, producing
+// higher-cardinality data in the system.statement_statistics table than
+// the cleanup job is able to keep up with. See #78338.
+var AssociateStmtWithTxnFingerprint = settings.RegisterBoolSetting(
+	settings.TenantWritable,
+	"sql.stats.associate_stmt_with_txn_fingerprint.enabled",
+	"whether to segment per-statement query statistics by transaction fingerprint",
+	true,
+)
