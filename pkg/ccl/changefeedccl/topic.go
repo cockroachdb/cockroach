3ddf25b568f4a11a9cd3d4c77c66805d87ca63f1
Aaron Zinger <zinger@cockroachlabs.com>
2022-03-28 18:37:27 -0400

    changefeedccl: send different column families to different topics
    This change necessitated touching every sink, which was making me
    cranky, so this commit refactors to pull topic name generation
    out of sink and encoder code and into a new topic.go file.
    The goal here is that when we add another target type, sinks won't
    need to know about it.
    
    As a side effect, this also addresses an old TODO to implement
    full_table_name on all sinks. The other options that affect topic
    name generation logic are all at the sink level, so they haven't
    moved to global application by default, but it'd be easy to do.
    
    One interesting consequence of having different topics per family
    is that we lose the invariant enforced by some sinks that no new
    topics get created after initialization. For split_column_families,
    a topic is now created "lazily" when emitting to it. The topics shown
    in e.g. SHOW CHANGEFEED JOB will in the case of split_column_families
    be placeholders. A topic will display as foo.{family} when events
    for foo will actually be emitted to foo.primary, foo.family2, etc.
    For now, in Kafka, PubSub and SqlSink I've disallowed sending resolved
     timestamps with split_column_families since the change frontier
     doesn't have the full set of topics to emit them to. This is a solveable
    problem but not trivial because of schema changes.
    
    Arguably this ship already sailed with ALTER CHANGEFEED, but it
    still seems worth calling out, as we could still special-case back
    in the old behavior if there's still a need for it.
    
    Release note (enterprise change): Changefeed messages for tables with multiple column families will append the family name to the table name in the default topic. For example, `CREATE CHANGEFEED FOR TABLE foo, TABLE bar FAMILY primary` will emit to the topics "foo" and "bar.primary". A changefeed created with the `split_column_families` option will create new topics if new families are added to a watched table that already had multiple column families. In cloud storage sinks such as s3, the filename will include the family separated by a + (e.g. "bar+primary"). The full_table_name option is now supported for all sinks.

---
+++ pkg/ccl/changefeedccl/topic.go
@@ -0,0 +1,223 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package changefeedccl
+
+import (
+	"strings"
+
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/errors"
+)
+
+// TopicDescriptor describes topic emitted by the sink.
+type TopicDescriptor interface {
+	// GetNameComponents returns the names that should go into any string-based topic identifier.
+	// If the topic is a table, this will be the statement time name of the table, fully-qualified
+	// if that option was set when creating the changefeed.
+	GetNameComponents() []string
+	// GetTopicIdentifier returns a struct suitable for use as a unique key in a map containing
+	// all topics in a feed.
+	GetTopicIdentifier() TopicIdentifier
+	// GetVersion returns topic version.
+	// For example, the underlying data source (e.g. table) may change, in which case
+	// we may want to emit same Name/ID, but a different version number.
+	GetVersion() descpb.DescriptorVersion
+	// GetTargetSpecification() returns the target specification for this topic.
+	// Currently this is assumed to be 1:1, or to be many: 1 for EachColumnFamily topics.
+	GetTargetSpecification() jobspb.ChangefeedTargetSpecification
+}
+
+// TopicIdentifier is a minimal set of fields that
+// uniquely identifies a topic.
+type TopicIdentifier struct {
+	TableID  descpb.ID
+	FamilyID descpb.FamilyID
+}
+
+// TopicNamer generates and caches the strings used as topic keys by sinks,
+// using target specifications, options, and sink-specific string manipulation.
+type TopicNamer struct {
+	join       byte
+	prefix     string
+	singleName string
+	sanitize   func(string) string
+
+	// DisplayNames are initialized once from specs and may contain placeholder strings.
+	DisplayNames map[jobspb.ChangefeedTargetSpecification]string
+
+	// FullNames are generated whenever Name() is actually called (usually during sink.EmitRow).
+	// They do not contain placeholder strings.
+	FullNames map[TopicIdentifier]string
+
+	sliceCache []string
+}
+
+type topicNameOption interface {
+	set(*TopicNamer)
+}
+
+type optJoinByte byte
+
+func (o optJoinByte) set(tn *TopicNamer) {
+	tn.join = byte(o)
+}
+
+// WithJoinByte overrides the default '.' separator between a table and family name.
+func WithJoinByte(b byte) topicNameOption {
+	return optJoinByte(b)
+}
+
+type optPrefix string
+
+func (o optPrefix) set(tn *TopicNamer) {
+	tn.prefix = string(o)
+}
+
+// WithPrefix defines a prefix string for all topics named by this TopicNamer.
+func WithPrefix(s string) topicNameOption {
+	return optPrefix(s)
+}
+
+type optSingleName string
+
+func (o optSingleName) set(tn *TopicNamer) {
+	tn.singleName = string(o)
+}
+
+// WithSingleName causes all topics named by this TopicNamer to be the same,
+// overriding things like the table name but not options like WithPrefix.
+func WithSingleName(s string) topicNameOption {
+	return optSingleName(s)
+}
+
+type optSanitize func(string) string
+
+func (o optSanitize) set(tn *TopicNamer) {
+	tn.sanitize = o
+}
+
+// WithSanitizeFn defines a post-processor for all topic names.
+func WithSanitizeFn(fn func(string) string) topicNameOption {
+	return optSanitize(fn)
+}
+
+// MakeTopicNamer creates a TopicNamer.
+// specs are used to populate DisplayNames and the values iterated over in Each.
+// Add options using WithJoinByte, WithPrefix, WithSingleName, and/or WithSanitizeFn.
+func MakeTopicNamer(
+	specs []jobspb.ChangefeedTargetSpecification, opts ...topicNameOption,
+) (*TopicNamer, error) {
+	tn := &TopicNamer{
+		join:         '.',
+		DisplayNames: make(map[jobspb.ChangefeedTargetSpecification]string, len(specs)),
+		FullNames:    make(map[TopicIdentifier]string),
+	}
+	for _, opt := range opts {
+		opt.set(tn)
+	}
+	for _, s := range specs {
+		name, err := tn.makeDisplayName(s)
+		if err != nil {
+			return nil, err
+		}
+		tn.DisplayNames[s] = name
+	}
+
+	return tn, nil
+
+}
+
+const familyPlaceholder = "{family}"
+
+// Name generates (with caching) a sink's topic identifier string.
+func (tn *TopicNamer) Name(td TopicDescriptor) (string, error) {
+	if name, ok := tn.FullNames[td.GetTopicIdentifier()]; ok {
+		return name, nil
+	}
+	name, err := tn.makeName(td.GetTargetSpecification(), td)
+	tn.FullNames[td.GetTopicIdentifier()] = name
+	return name, err
+}
+
+// DisplayNamesSlice gives all topics that are going to be emitted to,
+// suitable for displaying to the user on feed creation.
+func (tn *TopicNamer) DisplayNamesSlice() []string {
+	if len(tn.sliceCache) > 0 {
+		return tn.sliceCache
+	}
+	for _, n := range tn.DisplayNames {
+		tn.sliceCache = append(tn.sliceCache, n)
+		if tn.singleName != "" {
+			return tn.sliceCache
+		}
+	}
+	return tn.sliceCache
+}
+
+// Each is a convenience method that iterates a function over DisplayNamesSlice.
+func (tn *TopicNamer) Each(fn func(string) error) error {
+	for _, name := range tn.DisplayNames {
+		err := fn(name)
+		if tn.singleName != "" || err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// A nil topic descriptor means we're building solely from the spec
+// and should use placeholders if necessary. Only necessary in the
+// EACH_FAMILY case as in the COLUMN_FAMILY case we know the name from
+// the spec.
+func (tn *TopicNamer) makeName(
+	s jobspb.ChangefeedTargetSpecification, td TopicDescriptor,
+) (string, error) {
+	switch s.Type {
+	case jobspb.ChangefeedTargetSpecification_PRIMARY_FAMILY_ONLY:
+		return tn.nameFromComponents(s.StatementTimeName), nil
+	case jobspb.ChangefeedTargetSpecification_COLUMN_FAMILY:
+		return tn.nameFromComponents(s.StatementTimeName, s.FamilyName), nil
+	case jobspb.ChangefeedTargetSpecification_EACH_FAMILY:
+		if td == nil {
+			return tn.nameFromComponents(s.StatementTimeName, familyPlaceholder), nil
+		}
+		return tn.nameFromComponents(td.GetNameComponents()...), nil
+	default:
+		return "", errors.AssertionFailedf("unrecognized type %s", s.Type)
+	}
+}
+
+func (tn *TopicNamer) makeDisplayName(s jobspb.ChangefeedTargetSpecification) (string, error) {
+	return tn.makeName(s, nil /* no topic descriptor yet, use placeholders if needed */)
+}
+
+func (tn *TopicNamer) nameFromComponents(components ...string) string {
+	// Use strings.Builder rather than strings.Join because the join
+	// character isn't used with the prefix, so we save a string copy this way.
+	var b strings.Builder
+	b.WriteString(tn.prefix)
+	if tn.singleName != "" {
+		b.WriteString(tn.singleName)
+	} else {
+		for i, c := range components {
+			if i > 0 {
+				b.WriteByte(tn.join)
+			}
+			b.WriteString(c)
+		}
+	}
+	str := b.String()
+
+	if tn.sanitize != nil {
+		return tn.sanitize(str)
+	}
+
+	return str
+}
