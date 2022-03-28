9e9bf6ec59dd2580c672d8f40fc84be96769a04a
Arjun Nair <nair@cockroachlabs.com>
2022-03-28 16:42:12 -0400

    pkg/server: move pebble metrics initialization
    Currently, we don't wait for all the stores to initialize before calling
    `SetPebbleMetricsProvider`. This may cause admission control to be unaware
    of some of the stores on the Node. This may also lead to the metrics provider
    printing a warning because it can later detect a store which it doesn't know
    about.
    
    Release note: None

--- pkg/util/admission/granter.go
+++ pkg/util/admission/granter.go
@@ -14,6 +14,7 @@ import (
 	"context"
 	"math"
 	"time"
+	"unsafe"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/settings"
@@ -1221,7 +1222,11 @@ type StoreGrantCoordinators struct {
 	// These metrics are shared by WorkQueues across stores.
 	workQueueMetrics WorkQueueMetrics
 
-	gcMap                 map[int32]*GrantCoordinator
+	gcMap syncutil.IntMap // map[int64(StoreID)]*GrantCoordinator
+	// numStores is used to track the number of stores which have been added
+	// to the gcMap. This is used because the IntMap doesn't expose a size
+	// api.
+	numStores             int
 	pebbleMetricsProvider PebbleMetricsProvider
 	closeCh               chan struct{}
 }
@@ -1234,13 +1239,18 @@ func (sgc *StoreGrantCoordinators) SetPebbleMetricsProvider(
 	if sgc.pebbleMetricsProvider != nil {
 		panic(errors.AssertionFailedf("SetPebbleMetricsProvider called more than once"))
 	}
-	sgc.gcMap = make(map[int32]*GrantCoordinator)
 	sgc.pebbleMetricsProvider = pmp
 	sgc.closeCh = make(chan struct{})
 	metrics := sgc.pebbleMetricsProvider.GetPebbleMetrics()
 	for _, m := range metrics {
 		gc := sgc.initGrantCoordinator(m.StoreID)
-		sgc.gcMap[m.StoreID] = gc
+		// Defensive call to LoadAndStore even though Store ought to be sufficient
+		// since SetPebbleMetricsProvider can only be called once. This code
+		// guards against duplication of stores returned by GetPebbleMetrics.
+		_, loaded := sgc.gcMap.LoadOrStore(int64(m.StoreID), unsafe.Pointer(gc))
+		if !loaded {
+			sgc.numStores++
+		}
 		gc.pebbleMetricsTick(startupCtx, m.Metrics)
 		gc.allocateIOTokensTick()
 	}
@@ -1258,12 +1268,13 @@ func (sgc *StoreGrantCoordinators) SetPebbleMetricsProvider(
 				ticks++
 				if ticks%adjustmentInterval == 0 {
 					metrics := sgc.pebbleMetricsProvider.GetPebbleMetrics()
-					if len(metrics) != len(sgc.gcMap) {
+					if len(metrics) != sgc.numStores {
 						log.Warningf(ctx,
-							"expected %d store metrics and found %d metrics", len(sgc.gcMap), len(metrics))
+							"expected %d store metrics and found %d metrics", sgc.numStores, len(metrics))
 					}
 					for _, m := range metrics {
-						if gc, ok := sgc.gcMap[m.StoreID]; ok {
+						if unsafeGc, ok := sgc.gcMap.Load(int64(m.StoreID)); ok {
+							gc := (*GrantCoordinator)(unsafeGc)
 							gc.pebbleMetricsTick(ctx, m.Metrics)
 						} else {
 							log.Warningf(ctx,
@@ -1271,9 +1282,13 @@ func (sgc *StoreGrantCoordinators) SetPebbleMetricsProvider(
 						}
 					}
 				}
-				for _, gc := range sgc.gcMap {
+				sgc.gcMap.Range(func(_ int64, unsafeGc unsafe.Pointer) bool {
+					gc := (*GrantCoordinator)(unsafeGc)
 					gc.allocateIOTokensTick()
-				}
+					// true indicates that iteration should continue after the
+					// current entry has been processed.
+					return true
+				})
 			case <-sgc.closeCh:
 				done = true
 			}
@@ -1333,7 +1348,8 @@ func (sgc *StoreGrantCoordinators) initGrantCoordinator(storeID int32) *GrantCoo
 // TryGetQueueForStore returns a WorkQueue for the given storeID, or nil if
 // the storeID is not known.
 func (sgc *StoreGrantCoordinators) TryGetQueueForStore(storeID int32) *WorkQueue {
-	if granter, ok := sgc.gcMap[storeID]; ok {
+	if unsafeGranter, ok := sgc.gcMap.Load(int64(storeID)); ok {
+		granter := (*GrantCoordinator)(unsafeGranter)
 		return granter.GetWorkQueue(KVWork)
 	}
 	return nil
@@ -1344,9 +1360,14 @@ func (sgc *StoreGrantCoordinators) close() {
 	if sgc.closeCh != nil {
 		close(sgc.closeCh)
 	}
-	for _, c := range sgc.gcMap {
-		c.Close()
-	}
+
+	sgc.gcMap.Range(func(_ int64, unsafeGc unsafe.Pointer) bool {
+		gc := (*GrantCoordinator)(unsafeGc)
+		gc.Close()
+		// true indicates that iteration should continue after the
+		// current entry has been processed.
+		return true
+	})
 }
 
 // GrantCoordinators holds a regular GrantCoordinator for all work, and a
