9b9d660a7631deda328310ea47ba849d57cd2451
Andrii Vorobiov <and.vorobiov@gmail.com>
2022-03-31 16:01:54 +0300

    ui: update Hot ranges page styles
    Current change introduces several minor UI changes
    on Hot Ranges page:
    - changed page description, not it isn't rendered as
    inline alert;
    - Data formatting is changed to use UTC;
    - Changed heading style to H3 to follow the same style
    as other pages;
    - QPS values are rounded to 2 decimals;
    
    Release note (ui change): minor styling changes on
    Hot Ranges page to follow the same style as other pages.

--- pkg/ui/workspaces/db-console/src/views/hotRanges/index.tsx
+++ pkg/ui/workspaces/db-console/src/views/hotRanges/index.tsx
@@ -8,7 +8,6 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-import moment from "moment";
 import { cockroach } from "src/js/protos";
 import { useDispatch, useSelector } from "react-redux";
 import React, { useEffect } from "react";
@@ -26,9 +25,8 @@ import {
   lastErrorSelector,
   lastSetAtSelector,
 } from "src/redux/hotRanges";
-import { InlineAlert } from "src/components";
-import { performanceBestPracticesHotSpots } from "src/util/docs";
 import { selectNodeLocalities } from "src/redux/localities";
+import { DATE_FORMAT_24_UTC } from "src/util/format";
 
 const cx = classNames.bind(styles);
 const HotRangesRequest = cockroach.server.serverpb.HotRangesRequest;
@@ -58,33 +56,20 @@ const HotRangesPage = () => {
     );
   }, [dispatch]);
 
-  const formatCurrentDateTime = (datetime: moment.Moment) => {
-    return (
-      datetime.format("MMM DD, YYYY") +
-      " at " +
-      datetime.format("h:mm A") +
-      " (UTC)"
-    );
-  };
-  // TODO(santamaura): add url to anchor once it's available
   return (
-    <div className="section">
+    <React.Fragment>
       <Helmet title="Hot Ranges" />
-      <h1 className="base-heading">Hot Ranges</h1>
-      <InlineAlert
-        title=""
-        message={
-          <Text className={cx("hotranges-description")}>
-            The Hot Ranges table shows ranges receiving a high number of reads
-            or writes. By default, the table is sorted by ranges with the
-            highest QPS (queries per second). <br /> Use this information to{" "}
-            <Anchor href={performanceBestPracticesHotSpots}>
-              find and reduce hot spots.
-            </Anchor>
-          </Text>
-        }
-        fullWidth
-      />
+      <h3 className="base-heading">Hot Ranges</h3>
+      <Text className={cx("hotranges-description")}>
+        The Hot Ranges table shows ranges receiving a high number of reads or
+        writes. By default, the table is sorted by ranges with the highest QPS
+        (queries per second). <br />
+        Use this information to
+        <Anchor href="" target="_blank">
+          {" "}
+          find and reduce hot spots.
+        </Anchor>
+      </Text>
       <ErrorBoundary>
         <Loading
           loading={isLoading}
@@ -92,14 +77,16 @@ const HotRangesPage = () => {
           render={() => (
             <HotRangesTable
               hotRangesList={hotRanges}
-              lastUpdate={lastSetAt && formatCurrentDateTime(lastSetAt?.utc())}
+              lastUpdate={
+                lastSetAt && lastSetAt?.utc().format(DATE_FORMAT_24_UTC)
+              }
               nodeIdToLocalityMap={nodeIdToLocalityMap}
             />
           )}
           page={undefined}
         />
       </ErrorBoundary>
-    </div>
+    </React.Fragment>
   );
 };
 
