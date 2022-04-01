333752345625c3446327edaece7585830e24caca
irfan sharif <irfanmahmoudsharif@gmail.com>
2022-04-01 16:15:33 -0400

    [fixup] spanconfig: address review comments
    TODO(irfansharif): Update top-level commit message when squashing.
    
    Release note: None

--- pkg/server/server_sql.go
+++ pkg/server/server_sql.go
@@ -575,6 +575,7 @@ func newSQLServer(ctx context.Context, cfg sqlServerArgs) (*SQLServer, error) {
 		leaseMgr,
 		virtualSchemas,
 		hydratedTablesCache,
+		spanConfig.splitter,
 		spanConfig.limiter,
 	)
 
