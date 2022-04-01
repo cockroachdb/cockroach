b5913e5cf22b4634f390a3ec7f41139549218f13
David Taylor <tinystatemachine@gmail.com>
2022-04-01 16:21:51 +0000

    sql: add helper to detect physical plan changes
    Release note: none.

--- pkg/sql/distsql_plan_bulk.go
+++ pkg/sql/distsql_plan_bulk.go
@@ -13,9 +13,11 @@ package sql
 import (
 	"context"
 	"math/rand"
+	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/server/serverpb"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/errors"
 )
 
@@ -94,4 +96,87 @@ func (dsp *DistSQLPlanner) setupAllNodesPlanningTenant(
 		sqlInstanceIDs[i], sqlInstanceIDs[j] = sqlInstanceIDs[j], sqlInstanceIDs[i]
 	})
 	return planCtx, sqlInstanceIDs, nil
+}
+
+// PhysicalPlanMaker describes a function that makes a physical plan.
+type PhysicalPlanMaker func(context.Context, *DistSQLPlanner) (*PhysicalPlan, *PlanningCtx, error)
+
+// CalculatePlanGrowth returns the number of new or updated sql instances in one
+// physical plan relative to an old one, as well as that number as a fraction of
+// the number in the old one.
+func CalculatePlanGrowth(before, after *PhysicalPlan) (int, float64) {
+	var changed int
+	beforeSpecs := before.GenerateFlowSpecs()
+	afterSpecs := after.GenerateFlowSpecs()
+
+	// How many nodes are in after that are not in before, or are in both but
+	// have changed their spec?
+	for n, afterSpec := range afterSpecs {
+		if beforeSpec, ok := beforeSpecs[n]; !ok {
+			changed++ // not in before at all
+		} else if len(beforeSpec.Processors) != len(afterSpec.Processors) {
+			changed++ // in before with different procs
+		} else {
+			for i := range beforeSpec.Processors {
+				// TODO(dt): add equality check method gen, use it.
+				if afterSpec.Processors[i].Size() != beforeSpec.Processors[i].Size() {
+					changed++ // procs have differnet specs
+					break
+				}
+			}
+		}
+	}
+
+	return changed, float64(changed) / float64(len(beforeSpecs))
+}
+
+// ErrPlanChanged is a sentinel marker error for use to signal a plan changed.
+var ErrPlanChanged = errors.New("physical plan has changed")
+
+// PhysicalPlanChangeChecker returns a function which will periodically call the
+// passed function at the requested interval until the returned channel is
+// closed and compare the plan it returns to the passed initial plan, returning
+// and error if it has changed (as defined by CalculatePlanGrowth) by more than
+// the passed threshold. A threshold of 0 disables it.
+func PhysicalPlanChangeChecker(
+	ctx context.Context,
+	name string,
+	initial *PhysicalPlan,
+	fn PhysicalPlanMaker,
+	execCtx JobExecContext,
+	freq time.Duration,
+	thresholdFn func() float64,
+) (func(context.Context) error, chan struct{}) {
+	stop := make(chan struct{})
+
+	return func(ctx context.Context) error {
+		tick := time.NewTicker(freq)
+		defer tick.Stop()
+		done := ctx.Done()
+		for {
+			select {
+			case <-stop:
+				return nil
+			case <-done:
+				return ctx.Err()
+			case <-tick.C:
+				dsp := execCtx.DistSQLPlanner()
+				p, _, err := fn(ctx, dsp)
+				if err != nil {
+					log.Warningf(ctx, "job replanning check failed to generate plan: %v", err)
+					continue
+				}
+				changed, growth := CalculatePlanGrowth(initial, p)
+				threshold := thresholdFn()
+				replan := threshold != 0.0 && growth > threshold
+				if growth > 0.1 || log.V(1) {
+					log.Infof(ctx, "Re-planned %s would add/alter flows on %d nodes / %.2f, threshold %.2f, replan %v",
+						changed, growth, threshold, replan)
+				}
+				if replan {
+					return ErrPlanChanged
+				}
+			}
+		}
+	}, stop
 }
