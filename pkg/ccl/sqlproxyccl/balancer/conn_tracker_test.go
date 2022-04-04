3028ef85dee6c5f50a23f21bb4ee83d142924982
Jay <jay@cockroachlabs.com>
2022-04-04 11:58:57 -0400

    ccl/sqlproxyccl: run rebalancer queue processor in the background
    The previous commit added a rebalancer queue. This commit connects the queue to
    the balancer, and runs the queue processor in the background. By the default,
    we limit up to 100 concurrent transfers at any point in time, and each transfer
    will be retried up to 3 times.
    
    Release note: None

--- pkg/ccl/sqlproxyccl/balancer/conn_tracker_test.go
+++ pkg/ccl/sqlproxyccl/balancer/conn_tracker_test.go
@@ -18,11 +18,13 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/stretchr/testify/require"
 )
 
 func TestConnTracker(t *testing.T) {
 	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
 
 	tracker := NewConnTracker()
 	makeConn := func(tenantID int, podAddr string) (roachpb.TenantID, *testTrackerConnHandle) {
