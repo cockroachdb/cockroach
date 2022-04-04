71636ce51d0a1b5db86e43203d14bc3a59a1c717
Jay <jay@cockroachlabs.com>
2022-04-04 15:58:57 +0000

    ccl/sqlproxyccl: run rebalancer queue processor in the background
    The previous commit added a rebalancer queue. This commit connects the queue to
    the balancer, and runs the queue processor in the background. By the default,
    we limit up to 100 concurrent transfers at any point in time, and each transfer
    will be retried up to 3 times.
    
    Release note: None

--- pkg/ccl/sqlproxyccl/connector_test.go
+++ pkg/ccl/sqlproxyccl/connector_test.go
@@ -22,6 +22,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/errors"
 	"github.com/jackc/pgproto3/v2"
@@ -480,13 +481,19 @@ func TestConnector_dialTenantCluster(t *testing.T) {
 func TestConnector_lookupAddr(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	ctx := context.Background()
+	stopper := stop.NewStopper()
+	defer stopper.Stop(ctx)
+
+	balancer, err := balancer.NewBalancer(ctx, stopper)
+	require.NoError(t, err)
 
 	t.Run("successful", func(t *testing.T) {
 		var lookupTenantPodsFnCount int
+
 		c := &connector{
 			ClusterName: "my-foo",
 			TenantID:    roachpb.MakeTenantID(10),
-			Balancer:    balancer.NewBalancer(),
+			Balancer:    balancer,
 		}
 		c.DirectoryCache = &testTenantDirectoryCache{
 			lookupTenantPodsFn: func(
@@ -525,7 +532,7 @@ func TestConnector_lookupAddr(t *testing.T) {
 		c := &connector{
 			ClusterName: "my-foo",
 			TenantID:    roachpb.MakeTenantID(10),
-			Balancer:    balancer.NewBalancer(),
+			Balancer:    balancer,
 		}
 		c.DirectoryCache = &testTenantDirectoryCache{
 			lookupTenantPodsFn: func(
@@ -588,7 +595,7 @@ func TestConnector_lookupAddr(t *testing.T) {
 		c := &connector{
 			ClusterName: "my-foo",
 			TenantID:    roachpb.MakeTenantID(10),
-			Balancer:    balancer.NewBalancer(),
+			Balancer:    balancer,
 		}
 		c.DirectoryCache = &testTenantDirectoryCache{
 			lookupTenantPodsFn: func(
@@ -613,7 +620,7 @@ func TestConnector_lookupAddr(t *testing.T) {
 		c := &connector{
 			ClusterName: "my-foo",
 			TenantID:    roachpb.MakeTenantID(10),
-			Balancer:    balancer.NewBalancer(),
+			Balancer:    balancer,
 		}
 		c.DirectoryCache = &testTenantDirectoryCache{
 			lookupTenantPodsFn: func(
@@ -638,7 +645,7 @@ func TestConnector_lookupAddr(t *testing.T) {
 		c := &connector{
 			ClusterName: "my-foo",
 			TenantID:    roachpb.MakeTenantID(10),
-			Balancer:    balancer.NewBalancer(),
+			Balancer:    balancer,
 		}
 		c.DirectoryCache = &testTenantDirectoryCache{
 			lookupTenantPodsFn: func(
