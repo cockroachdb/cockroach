462ad19ee781a33896fbdc5b17f3e9cfcebc179f
Yahor Yuzefovich <yahor@cockroachlabs.com>
2022-04-04 13:18:07 -0700

    logictest: randomize distsql_workmem in all configs
    This commit makes it so that `sql.distsql.temp_storage.workmem` cluster
    setting is randomized in `[10KiB, 100KiB)` range in every config with
    50% probability. This increases test coverage. Note that `*-disk`
    configs use a different knob, so they are still useful to keep. The
    randomization is only disabled for opt logic tests.
    
    This required changing the way we handle small limits in the windower
    (there, we used to return an error if there isn't enough workmem budget,
    and we silently bump it up to the minimum - this is what we already do
    for the join reader) as well as minor cleanups to the logic tests.
    
    Release note: None

--- pkg/sql/rowexec/windower.go
+++ pkg/sql/rowexec/windower.go
@@ -145,14 +145,8 @@ func newWindower(
 	// windower will overflow to disk if this limit is not enough.
 	limit := execinfra.GetWorkMemLimit(flowCtx)
 	if limit < memRequiredByWindower {
-		if !flowCtx.Cfg.TestingKnobs.ForceDiskSpill && flowCtx.Cfg.TestingKnobs.MemoryLimitBytes == 0 {
-			return nil, errors.Errorf(
-				"window functions require %d bytes of RAM but only %d are in the budget. "+
-					"Consider increasing sql.distsql.temp_storage.workmem cluster setting or distsql_workmem session variable",
-				memRequiredByWindower, limit)
-		}
-		// The limit is set very low by the tests, but the windower requires
-		// some amount of RAM, so we override the limit.
+		// The limit is set very low, likely by the tests, but the windower
+		// requires some amount of RAM, so we override the limit.
 		limit = memRequiredByWindower
 	}
 	limitedMon := mon.NewMonitorInheritWithLimit("windower-limited", limit, evalCtx.Mon)
