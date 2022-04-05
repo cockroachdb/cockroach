baed19317affef60f1642cb65ace748d89862a6f
Alex Barganier <abarganier@cockroachlabs.com>
2022-04-05 11:34:16 -0400

    log: drain buffered messages during final flush in bufferSink
    When the server shuts down, this is indicated to the accumulator via the
    context.Done() channel. When this happens, the accumulator previously consumed
    at most 1 of the log messages buffered so far on messageCh. If there were more
    messages there, they got lost.
    
    This patch updates the final flush logic to drain the log messages buffered
    in messageCh in its entirety before triggering the final flush, avoiding the
    case described above where messages were lost.
    
    Release note: none

--- pkg/util/log/buffer_sink_test.go
+++ pkg/util/log/buffer_sink_test.go
@@ -192,6 +192,28 @@ func TestBufferForceSync(t *testing.T) {
 	atomic.StoreInt32(&marker, 1)
 }
 
+func TestBufferCtxDoneFlushesRemainingMsgs(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	ctx, cancel := context.WithCancel(context.Background())
+	ctrl := gomock.NewController(t)
+	mock := NewMockLogSink(ctrl)
+	sink := newBufferSink(ctx, mock, 0 /*maxStaleness*/, 0 /*sizeTrigger*/, 2 /* maxInFlight */, nil /*errCallback*/)
+	defer ctrl.Finish()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	defer wg.Wait()
+
+	mock.EXPECT().
+		output(gomock.Eq([]byte("test1\ntest2\ntest3")), sinkOutputOptionsMatcher{extraFlush: gomock.Eq(true)}).
+		Do(addArgs(wg.Done))
+
+	require.NoError(t, sink.output([]byte("test1"), sinkOutputOptions{}))
+	require.NoError(t, sink.output([]byte("test2"), sinkOutputOptions{}))
+	require.NoError(t, sink.output([]byte("test3"), sinkOutputOptions{}))
+	cancel()
+}
+
 type sinkOutputOptionsMatcher struct {
 	extraFlush   gomock.Matcher
 	ignoreErrors gomock.Matcher
