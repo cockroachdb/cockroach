6b66286a86db18be1ca5b6950bcccff41e86fcb7
Alex Sarkesian <sarkesian@cockroachlabs.com>
2022-04-07 18:53:19 -0400

    sql: add virtual indexes to crdb_internal.cluster_locks virtual table
    This change adds virtual indexes on the `table_id`, `database_name`, and
    `table_name` columns of the `crdb_internal.cluster_locks` virtual table,
    so that when queried, the `kv.Batch`es with `QueryLocksRequest`s can be
    constrained to query only specific tables or databases. This allows the
    requests to be much more limited, rather than needing to request all of
    the ranges that comprise the key spans of all tables accessible by the
    user.
    
    Release note (sql change): Improved query performance for
    `crdb_internal.cluster_locks` when issued with constraints in the WHERE
    clause on `table_id`, `database_name`, or `table_name` columns.

--- pkg/sql/crdb_internal.go
+++ pkg/sql/crdb_internal.go
@@ -5839,9 +5839,9 @@ var crdbInternalClusterLocksTable = virtualSchemaTable{
 CREATE TABLE crdb_internal.cluster_locks (
     range_id            INT NOT NULL,
     table_id            INT NOT NULL,
-    database_name       STRING,
+    database_name       STRING NOT NULL,
     schema_name         STRING,
-    table_name          STRING,
+    table_name          STRING NOT NULL,
     index_name          STRING,
     lock_key            BYTES NOT NULL,
     lock_key_pretty     STRING NOT NULL,
@@ -5850,11 +5850,106 @@ CREATE TABLE crdb_internal.cluster_locks (
     lock_strength       STRING,
     durability          STRING,
     granted             BOOL,
-    contended           BOOL,
-    duration            INTERVAL
+    contended           BOOL NOT NULL,
+    duration            INTERVAL,
+    INDEX(table_id),
+    INDEX(database_name),
+    INDEX(table_name),
+    INDEX(contended)
 );`,
-	indexes: nil,
-	generator: func(ctx context.Context, p *planner, db catalog.DatabaseDescriptor, stopper *stop.Stopper) (virtualTableGenerator, cleanupFunc, error) {
+	indexes: []virtualIndex{
+		{
+			populate: func(
+				ctx context.Context,
+				unwrappedConstraint tree.Datum,
+				p *planner,
+				db catalog.DatabaseDescriptor,
+				addRow func(...tree.Datum) error,
+			) (matched bool, err error) {
+				var filters clusterLocksFilters
+				switch t := unwrappedConstraint.(type) {
+				case *tree.DInt:
+					filters.tableID = (*int64)(t)
+				default:
+					return false, errors.AssertionFailedf(
+						"unexpected type %T for table_id column in virtual table crdb_internal.cluster_locks", unwrappedConstraint)
+				}
+
+				return populateClusterLocksWithFilter(ctx, p, db, addRow, filters)
+			},
+		},
+		{
+			populate: func(
+				ctx context.Context,
+				unwrappedConstraint tree.Datum,
+				p *planner,
+				db catalog.DatabaseDescriptor,
+				addRow func(...tree.Datum) error,
+			) (matched bool, err error) {
+				var filters clusterLocksFilters
+				switch t := unwrappedConstraint.(type) {
+				case *tree.DString:
+					filters.databaseName = (*string)(t)
+				default:
+					return false, errors.AssertionFailedf(
+						"unexpected type %T for database_name column in virtual table crdb_internal.cluster_locks", unwrappedConstraint)
+				}
+				return populateClusterLocksWithFilter(ctx, p, db, addRow, filters)
+			},
+		},
+		{
+			populate: func(
+				ctx context.Context,
+				unwrappedConstraint tree.Datum,
+				p *planner,
+				db catalog.DatabaseDescriptor,
+				addRow func(...tree.Datum) error,
+			) (matched bool, err error) {
+				var filters clusterLocksFilters
+				switch t := unwrappedConstraint.(type) {
+				case *tree.DString:
+					filters.tableName = (*string)(t)
+				default:
+					return false, errors.AssertionFailedf(
+						"unexpected type %T for table_name column in virtual table crdb_internal.cluster_locks", unwrappedConstraint)
+				}
+				return populateClusterLocksWithFilter(ctx, p, db, addRow, filters)
+			},
+		},
+		{
+			populate: func(
+				ctx context.Context,
+				unwrappedConstraint tree.Datum,
+				p *planner,
+				db catalog.DatabaseDescriptor,
+				addRow func(...tree.Datum) error,
+			) (matched bool, err error) {
+				var filters clusterLocksFilters
+				switch t := unwrappedConstraint.(type) {
+				case *tree.DBool:
+					filters.contended = (*bool)(t)
+				default:
+					return false, errors.AssertionFailedf(
+						"unexpected type %T for contended column in virtual table crdb_internal.cluster_locks", unwrappedConstraint)
+				}
+				return populateClusterLocksWithFilter(ctx, p, db, addRow, filters)
+			},
+		},
+	},
+	generator: genClusterLocksGenerator(clusterLocksFilters{}),
+}
+
+type clusterLocksFilters struct {
+	tableID      *int64
+	databaseName *string
+	tableName    *string
+	contended    *bool
+}
+
+func genClusterLocksGenerator(
+	filters clusterLocksFilters,
+) func(ctx context.Context, p *planner, db catalog.DatabaseDescriptor, stopper *stop.Stopper) (virtualTableGenerator, cleanupFunc, error) {
+	return func(ctx context.Context, p *planner, _ catalog.DatabaseDescriptor, _ *stop.Stopper) (virtualTableGenerator, cleanupFunc, error) {
 		// TODO(sarkesian): remove gate for 22.2 release
 		if !p.ExecCfg().Settings.Version.IsActive(ctx, clusterversion.ClusterLocksVirtualTable) {
 			return nil, nil, pgerror.New(pgcode.FeatureNotSupported,
@@ -5904,6 +5999,15 @@ CREATE TABLE crdb_internal.cluster_locks (
 			}
 			switch desc := desc.(type) {
 			case catalog.TableDescriptor:
+				if filters.tableName != nil && *filters.tableName != desc.GetName() {
+					continue
+				}
+				if filters.tableID != nil && descpb.ID(*filters.tableID) != desc.GetID() {
+					continue
+				}
+				if filters.databaseName != nil && *filters.databaseName != dbNames[uint32(desc.GetParentID())] {
+					continue
+				}
 				spansToQuery = append(spansToQuery, desc.TableSpan(p.execCfg.Codec))
 			}
 		}
@@ -5935,6 +6039,10 @@ CREATE TABLE crdb_internal.cluster_locks (
 				},
 				IncludeUncontended: true,
 			}
+			if filters.contended != nil && *filters.contended {
+				queryLocksRequest.IncludeUncontended = false
+			}
+
 			b.AddRawRequest(queryLocksRequest)
 
 			b.Header.MaxSpanRequestKeys = int64(rowinfra.ProductionKVBatchSize)
@@ -6072,5 +6180,32 @@ CREATE TABLE crdb_internal.cluster_locks (
 			}, nil
 
 		}, nil, nil
-	},
+	}
+}
+
+func populateClusterLocksWithFilter(
+	ctx context.Context,
+	p *planner,
+	db catalog.DatabaseDescriptor,
+	addRow func(...tree.Datum) error,
+	filters clusterLocksFilters,
+) (matched bool, err error) {
+	var rowGenerator virtualTableGenerator
+	generator := genClusterLocksGenerator(filters)
+	rowGenerator, _, err = generator(ctx, p, db, nil /* stopper */)
+	if err != nil {
+		return false, err
+	}
+	var row tree.Datums
+	row, err = rowGenerator()
+	for row != nil && err == nil {
+		err = addRow(row...)
+		if err != nil {
+			break
+		}
+		matched = true
+
+		row, err = rowGenerator()
+	}
+	return matched, err
 }
