17af313d209df4f72caaa8481550b08cc7b355e3
Rafi Shamim <rafi@cockroachlabs.com>
2022-04-07 18:10:27 -0400

    roachtest: update libpq test
    This allows the query cancel tests to pass.
    
    Release note: None

--- pkg/cmd/roachtest/tests/libpq_blocklist.go
+++ pkg/cmd/roachtest/tests/libpq_blocklist.go
@@ -17,7 +17,37 @@ var libPQBlocklists = blocklistsForVersion{
 	{"v22.1", "libPQBlocklist22_1", libPQBlocklist22_1, "libPQIgnorelist22_1", libPQIgnorelist22_1},
 }
 
-var libPQBlocklist22_1 = libPQBlocklist21_2
+var libPQBlocklist22_1 = blocklist{
+	"pq.ExampleConnectorWithNoticeHandler":           "unknown",
+	"pq.TestBinaryByteSliceToInt":                    "41547",
+	"pq.TestBinaryByteSlicetoUUID":                   "41547",
+	"pq.TestConnListen":                              "41522",
+	"pq.TestConnUnlisten":                            "41522",
+	"pq.TestConnUnlistenAll":                         "41522",
+	"pq.TestConnectorWithNoticeHandler_Simple":       "unknown",
+	"pq.TestConnectorWithNotificationHandler_Simple": "unknown",
+	"pq.TestCopyFromError":                           "5807",
+	"pq.TestCopyInRaiseStmtTrigger":                  "5807",
+	"pq.TestCopyInTypes":                             "5807",
+	"pq.TestCopyRespLoopConnectionError":             "5807",
+	"pq.TestEncodeAndParseTs":                        "41563",
+	"pq.TestInfinityTimestamp":                       "41564",
+	"pq.TestIssue186":                                "41558",
+	"pq.TestIssue196":                                "41689",
+	"pq.TestIssue282":                                "12137",
+	"pq.TestListenerFailedQuery":                     "41522",
+	"pq.TestListenerListen":                          "41522",
+	"pq.TestListenerReconnect":                       "41522",
+	"pq.TestListenerUnlisten":                        "41522",
+	"pq.TestListenerUnlistenAll":                     "41522",
+	"pq.TestNotifyExtra":                             "41522",
+	"pq.TestPing":                                    "35897",
+	"pq.TestQueryRowBugWorkaround":                   "5807",
+	"pq.TestReconnect":                               "35897",
+	"pq.TestRowsColumnTypes":                         "41688",
+	"pq.TestRuntimeParameters":                       "12137",
+	"pq.TestStringWithNul":                           "26366",
+}
 
 var libPQBlocklist21_2 = blocklist{
 	"pq.ExampleConnectorWithNoticeHandler":           "unknown",
