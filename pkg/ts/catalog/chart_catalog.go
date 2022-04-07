b85bab1e3c8ac0b70a87d4fed87e2e47e68194f7
Azhng <archer.xn@gmail.com>
2022-04-07 19:00:07 +0000

    sql/pgwire: add sql.conn.failures metric
    Related to #73566
    
    This commit adds the the new sql.conn.failures counter metric that
    reflects the number of failed SQL connections.
    
    Release note (sql change): the new sql.conn.failures counter metric shows
    number of failed SQL connections.

--- pkg/ts/catalog/chart_catalog.go
+++ pkg/ts/catalog/chart_catalog.go
@@ -2270,6 +2270,13 @@ var charts = []sectionDescription{
 				AxisLabel: "Latency",
 			},
 			{
+				Title: "Connection Failures",
+				Metrics: []string{
+					"sql.conn.failures",
+				},
+				AxisLabel: "Failures",
+			},
+			{
 				Title: "Open Transactions",
 				Metrics: []string{
 					"sql.txns.open",
