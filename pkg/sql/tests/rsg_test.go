0575d5c20d399200edb324d9ca1b5f7bf8a1c9f0
Yahor Yuzefovich <yahor@cockroachlabs.com>
2022-04-07 14:40:13 -0700

    tests: don't swallow an error about no cast volatility
    The underlying issue #70831 has been fixed.
    
    Release note: None

--- pkg/sql/tests/rsg_test.go
+++ pkg/sql/tests/rsg_test.go
@@ -165,9 +165,7 @@ func (db *verifyFormatDB) execWithTimeout(
 					}
 				}
 			}
-			// TODO(yuzefovich): allow "no volatility for cast tuple" errors to
-			// fail once #70831 is resolved.
-			if es := err.Error(); (strings.Contains(es, "internal error") && !strings.Contains(es, "no volatility for cast tuple")) ||
+			if es := err.Error(); strings.Contains(es, "internal error") ||
 				strings.Contains(es, "driver: bad connection") ||
 				strings.Contains(es, "unexpected error inside CockroachDB") {
 				return &crasher{
