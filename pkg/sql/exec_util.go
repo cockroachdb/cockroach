7ab5ba889062d1bbb0424f9adec240e9e62cf480
Michael Erickson <michae2@cockroachlabs.com>
2022-04-08 17:21:30 +0000

    sql: session variable to allow multiple modification subqueries of table
    Add a new session variable, enable_multiple_modifications_of_table,
    which can be used instead of sql.multiple_modifications_of_table.enabled
    to allow execution of statements with multiple modification subqueries
    of the same table.
    
    Instead of making the original cluster setting the GlobalDefault of this
    new session setting, the original cluster setting is kept in the
    optbuilder logic. This is to avoid breaking applications that are
    already toggling the cluster setting mid-session to allow statements.
    
    Fixes: #76261
    
    Release note (sql change): Add a new session variable,
    enable_multiple_modifications_of_table, which can be used instead of
    cluster variable sql.multiple_modifications_of_table.enabled to allow
    statements containing multiple INSERT ON CONFLICT, UPSERT, UPDATE, or
    DELETE subqueries modifying the same table. Note that underlying issue
    70731 is not fixed. As with sql.multiple_modifications_of_table.enabled,
    be warned that with this session variable enabled there is nothing to
    prevent the table corruption seen in issue 70731 from occuring if the
    same row is modified multiple times by different subqueries of a single
    statment. It's best to rewrite these statements, but the session
    variable is provided as an aid if this is not possible.

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -3201,6 +3201,10 @@ func (m *sessionDataMutator) SetExpectAndIgnoreNotVisibleColumnsInCopy(val bool)
 	m.data.ExpectAndIgnoreNotVisibleColumnsInCopy = val
 }
 
+func (m *sessionDataMutator) SetMultipleModificationsOfTable(val bool) {
+	m.data.MultipleModificationsOfTable = val
+}
+
 // Utility functions related to scrubbing sensitive information on SQL Stats.
 
 // quantizeCounts ensures that the Count field in the
