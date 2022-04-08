315846bc117dc1c6e93d381e55775f3472ed782e
Andrew Werner <awerner32@gmail.com>
2022-04-08 16:01:47 -0400

    sql: disallow partial index creation referencing new column in predicate
    Release note (bug fix): Previously, when adding a column to a pre-existing
    table and adding a partial index referencing that column in the
    transaction, DML operations against the table while the schema change was
    ongoing would fail. Now these hazardous schema changes are not allowed.

--- pkg/sql/catalog/descriptor.go
+++ pkg/sql/catalog/descriptor.go
@@ -698,6 +698,12 @@ type TableDescriptor interface {
 	GetAutoStatsSettings() *catpb.AutoStatsSettings
 }
 
+// MutableTableDescriptor is both a MutableDescriptor and a TableDescriptor.
+type MutableTableDescriptor interface {
+	TableDescriptor
+	MutableDescriptor
+}
+
 // TypeDescriptor will eventually be called typedesc.Descriptor.
 // It is implemented by (Imm|M)utableTypeDescriptor.
 type TypeDescriptor interface {
