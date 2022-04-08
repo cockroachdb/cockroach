5ccd84b4a38c277b1fe051591a113fa5c5e09376
Aaron Zinger <zinger@cockroachlabs.com>
2022-04-08 15:13:32 -0400

    changefeedccl,roachtest: skip failing test
    This is failing every night with a timeout on the initial connection.
    Probably some provisioned account or something rotted while it was
    failing for a different reason.
    
    Release note: None

--- pkg/cmd/roachtest/tests/cdc.go
+++ pkg/cmd/roachtest/tests/cdc.go
@@ -813,7 +813,9 @@ func registerCDC(r registry.Registry) {
 			})
 		},
 	})
-	r.Add(registry.TestSpec{
+	// TODO(zinger): uncomment once manual acceptance testing passes
+	// and whatever connectivity/provisioning issue happening here is fixed.
+	/* r.Add(registry.TestSpec{
 		Name:            "cdc/pubsub-sink",
 		Owner:           `cdc`,
 		Cluster:         r.MakeClusterSpec(4, spec.CPU(16)),
@@ -829,8 +831,8 @@ func registerCDC(r registry.Registry) {
 				targetSteadyLatency:      time.Minute,
 			})
 		},
-	})
-	// TODO(ryan min): uncomment once connectivity issue is fixed,
+	}) */
+	// TODO(zinger): uncomment once connectivity issue is fixed,
 	// currently fails with "initial scan did not complete" because sink
 	// URI is set as localhost, need to expose it to the other nodes via IP
 	/*
