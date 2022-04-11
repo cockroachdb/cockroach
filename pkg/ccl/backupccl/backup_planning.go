cbbd9fa618a3af31792a6a7de87e7f943b0c97c8
Michael Butler <butler@cockroachlabs.com>
2022-04-11 16:13:38 -0400

    backupccl: add UX guardrails during backup subdirectory resolution
    Informs #79674, #79672
    
    The changes in this PR sprang out of discussions in #79447 which deprecated the
    ability to pass an explicit subdirectory in any backup command. This PR tweaks
    that deprecation warning and adds further UX guardrails.
    
    Release note (sql change): This patch introduces a few UX guardrails, including
    one breaking change:
    
    [breaking]: After further discussion, we realized explicit subdirectories were
    useful for running incremental backups, but not for full backups. To that end,
    this pr throws an error if: a) a user passes a subdirectory; b)
    there does not already exist a full backup in that subdirectory. The user can
    enable this deprecated syntax by switching the new
    bulkio.backup.deprecated_full_backup_with_subdir cluster setting to true.
    
    Discussion in #79447 also lead to a discovery of two
    bugs for backups with AS OF SYSTEM TIME:
    
    Previously, a user could run an AS OF SYSTEM TIME
    incremental backup with an end time earlier than the previous backup's end time
    , which could lead to an out of order incremental backup chain. This PR causes
    the incremental backup to fail if the AS OF SYSTEM TIME is less than the
    previous backup's end time.
    
    Previously, if a user ran `BACKUP INTO dest AS OF SYSTEM TIME t` and a full
    backup subdirectory already existed at t, the backup would mistakingly increment
    on that full backup instead of failing. Without the IN keyword, the user expects
    a full backup, not an incremental backup. In this patch, the full backup fails
    when it detects a full backup already exists at the resolved subdirectory.

--- pkg/ccl/backupccl/backup_planning.go
+++ pkg/ccl/backupccl/backup_planning.go
@@ -108,6 +108,17 @@ type (
 	}
 )
 
+// featureFullBackupUserSubdir, when true, will create a full backup at a user
+// specified subdirectory if no backup already exists at that subdirectory. As
+// of 22.1, this feature is default disabled, and will be totally disabled by 22.2.
+var featureFullBackupUserSubdir = settings.RegisterBoolSetting(
+	settings.TenantWritable,
+	"bulkio.backup.deprecated_full_backup_with_subdir.enabled",
+	"when true, a backup command with a user specified subdirectory will create a full backup at"+
+		" the subdirectory if no backup already exists at that subdirectory.",
+	false,
+).WithPublic()
+
 // getPublicIndexTableSpans returns all the public index spans of the
 // provided table.
 func getPublicIndexTableSpans(
@@ -699,16 +710,11 @@ func backupPlanHook(
 		if backupStmt.Nested {
 			if backupStmt.AppendToLatest {
 				initialDetails.Destination.Subdir = latestFileName
+				initialDetails.Destination.Exists = true
+
 			} else if subdir != "" {
 				initialDetails.Destination.Subdir = "/" + strings.TrimPrefix(subdir, "/")
-				// Deprecation notice for `BACKUP INTO` syntax with an explicit subdir.
-				// Remove this once the syntax is deleted in 22.2.
-				p.BufferClientNotice(ctx,
-					pgnotice.Newf("BACKUP commands with an explicitly specified"+
-						" subdirectory will be removed in a future release. Users can create a full backup via `BACKUP ... "+
-						"INTO <collectionURI>`, or an incremental backup on the latest full backup in their "+
-						"collection via `BACKUP ... INTO LATEST IN <collectionURI>`"))
-
+				initialDetails.Destination.Exists = true
 			} else {
 				initialDetails.Destination.Subdir = endTime.GoTime().Format(DateBasedIntoFolderName)
 			}
@@ -1460,6 +1466,13 @@ func getBackupDetailAndManifest(
 
 		if err := requireEnterprise(execCfg, "incremental"); err != nil {
 			return jobspb.BackupDetails{}, BackupManifest{}, err
+		}
+		lastEndTime := prevBackups[len(prevBackups)-1].EndTime
+		if lastEndTime.Compare(initialDetails.EndTime) > 0 {
+			return jobspb.BackupDetails{}, BackupManifest{},
+				errors.Newf("`AS OF SYSTEM TIME` %s must be greater than "+
+					"the previous backup's end time of %s.",
+					initialDetails.EndTime.GoTime(), lastEndTime.GoTime())
 		}
 	}
 
