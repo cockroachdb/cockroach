de901f8a6c0461086653abcb9d99ddd6a052839e
Yahor Yuzefovich <yahor@cockroachlabs.com>
2022-04-12 13:54:56 -0700

    sql: fix overflow for soft limits
    We're tracking `LimitHint` property as a float and later convert it to
    an int64. Previously, this could lead to overflows when the limit hint
    value exceeds the range of int64, and we now instead will use a value of
    0 (i.e. disable the soft limit).
    
    Release note (bug fix): Previously, CockroachDB could encounter an
    internal error when evaluating queries with OFFSET and LIMIT clauses
    when the addition of the `offset` and the `limit` value would be larger
    than `int64` range.

--- pkg/sql/distsql_physical_planner.go
+++ pkg/sql/distsql_physical_planner.go
@@ -2234,7 +2234,7 @@ func (dsp *DistSQLPlanner) createPlanForIndexJoin(
 		LockingStrength:   n.table.lockingStrength,
 		LockingWaitPolicy: n.table.lockingWaitPolicy,
 		MaintainOrdering:  len(n.reqOrdering) > 0,
-		LimitHint:         int64(n.limitHint),
+		LimitHint:         n.limitHint,
 	}
 
 	fetchColIDs := make([]descpb.ColumnID, len(n.cols))
@@ -2303,7 +2303,7 @@ func (dsp *DistSQLPlanner) createPlanForLookupJoin(
 		LeftJoinWithPairedJoiner:          n.isSecondJoinInPairedJoiner,
 		OutputGroupContinuationForLeftRow: n.isFirstJoinInPairedJoiner,
 		LookupBatchBytesLimit:             dsp.distSQLSrv.TestingKnobs.JoinReaderBatchBytesLimit,
-		LimitHint:                         int64(n.limitHint),
+		LimitHint:                         n.limitHint,
 	}
 
 	fetchColIDs := make([]descpb.ColumnID, len(n.table.cols))
