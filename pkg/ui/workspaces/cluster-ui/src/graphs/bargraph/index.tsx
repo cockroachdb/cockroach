b3a878982fffe4e4d9d1b3741f4e01d13635d308
Xin Hao Zhang <xzhang@cockroachlabs.com>
2022-04-12 16:53:21 -0400

    cluster-ui: create bar graph for time series data
    Closes #74516
    
    This commit creates a generic bar chart component in
    the cluster-ui package. The component is intended for
    use with time series data and should be wrapped in
    parent components that convert metrics responses from
    the server to the expected format.
    
    Release note: None

---
+++ pkg/ui/workspaces/cluster-ui/src/graphs/bargraph/index.tsx
@@ -0,0 +1,99 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import React, { useEffect, useRef } from "react";
+import classNames from "classnames/bind";
+import { getStackedBarOpts, stack } from "./bars";
+import uPlot, { AlignedData } from "uplot";
+import styles from "./bargraph.module.scss";
+import { Visualization } from "../visualization";
+import {
+  AxisUnits,
+  calculateXAxisDomainBarChart,
+  calculateYAxisDomain,
+} from "../utils/domain";
+import { Options } from "uplot";
+
+const cx = classNames.bind(styles);
+
+export type BarGraphTimeSeriesProps = {
+  alignedData?: AlignedData;
+  colourPalette?: string[]; // Series colour palette.
+  preCalcGraphSize?: boolean;
+  title: string;
+  tooltip?: React.ReactNode;
+  uPlotOptions: Partial<Options>;
+  yAxisUnits: AxisUnits;
+};
+
+// Currently this component only supports stacked multi-series bars.
+export const BarGraphTimeSeries: React.FC<BarGraphTimeSeriesProps> = ({
+  alignedData,
+  colourPalette,
+  preCalcGraphSize = true,
+  title,
+  tooltip,
+  uPlotOptions,
+  yAxisUnits,
+}) => {
+  const graphRef = useRef<HTMLDivElement>(null);
+  const samplingIntervalMillis = alignedData[0].length
+    ? alignedData[0][1] - alignedData[0][0]
+    : 0;
+
+  useEffect(() => {
+    if (!alignedData) return;
+
+    const xAxisDomain = calculateXAxisDomainBarChart(
+      alignedData[0][0], // startMillis
+      alignedData[0][alignedData[0].length - 1], // endMillis
+      samplingIntervalMillis,
+    );
+
+    const stackedData = stack(alignedData, () => false);
+
+    const allYDomainPoints: number[] = [];
+    stackedData.slice(1).forEach(points => allYDomainPoints.push(...points));
+    const yAxisDomain = calculateYAxisDomain(yAxisUnits, allYDomainPoints);
+
+    const opts = getStackedBarOpts(
+      alignedData,
+      uPlotOptions,
+      xAxisDomain,
+      yAxisDomain,
+      colourPalette,
+    );
+
+    const plot = new uPlot(opts, stackedData, graphRef.current);
+
+    return () => {
+      plot?.destroy();
+    };
+  }, [
+    alignedData,
+    colourPalette,
+    uPlotOptions,
+    yAxisUnits,
+    samplingIntervalMillis,
+  ]);
+
+  return (
+    <Visualization
+      title={title}
+      loading={!alignedData}
+      preCalcGraphSize={preCalcGraphSize}
+      tooltip={tooltip}
+    >
+      <div className={cx("bargraph")}>
+        <div ref={graphRef} />
+      </div>
+    </Visualization>
+  );
+};
