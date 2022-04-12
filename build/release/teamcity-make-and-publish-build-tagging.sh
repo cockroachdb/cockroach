1fa59ff8538c88933cbd7636057a00e6f0cda664
Rail Aliiev <rail@iqchoice.com>
2022-04-11 23:53:20 -0400

    ci: run Make and Publish Build without dependencies
    Previously, we ran Make and Publish Build at the end of the CI
    dependency graph, what slowed down availability of the binaries.
    
    This patch splits the scripts into two parts:
      * Make and Publish Build Artifacts: builds and publishes all binary
        artifacts to S3 and GCR.
      * Make and Publish Build: tags the GitHub repo and creates the latest
        tags for the docker images.
    
    Release note: None

--- build/release/teamcity-make-and-publish-build-tagging.sh
+++ build/release/teamcity-make-and-publish-build-tagging.sh
@@ -15,13 +15,11 @@ release_branch="$(echo "$build_name" | grep -Eo "^v[0-9]+\.[0-9]+" || echo"")"
 is_custom_build="$(echo "$TC_BUILD_BRANCH" | grep -Eo "^custombuild-" || echo "")"
 
 if [[ -z "${DRY_RUN}" ]] ; then
-  bucket="${BUCKET-cockroach-builds}"
   google_credentials=$GOOGLE_COCKROACH_CLOUD_IMAGES_COCKROACHDB_CREDENTIALS
   gcr_repository="us-docker.pkg.dev/cockroach-cloud-images/cockroachdb/cockroach"
   # Used for docker login for gcloud
   gcr_hostname="us-docker.pkg.dev"
 else
-  bucket="${BUCKET:-cockroach-builds-test}"
   google_credentials="$GOOGLE_COCKROACH_RELEASE_CREDENTIALS"
   gcr_repository="us.gcr.io/cockroach-release/cockroach-test"
   build_name="${build_name}.dryrun"
@@ -33,7 +31,6 @@ cat << EOF
   build_name:      $build_name
   release_branch:  $release_branch
   is_custom_build: $is_custom_build
-  bucket:          $bucket
   gcr_repository:  $gcr_repository
 
 EOF
@@ -43,36 +40,6 @@ tc_end_block "Variable Setup"
 tc_start_block "Tag the release"
 git tag "${build_name}"
 tc_end_block "Tag the release"
-
-
-tc_start_block "Compile publish-provisional-artifacts"
-build/builder.sh go install ./pkg/cmd/publish-provisional-artifacts
-tc_end_block "Compile publish-provisional-artifacts"
-
-
-tc_start_block "Compile and publish S3 artifacts"
-build/builder.sh env \
-  AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
-  AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
-  TC_BUILD_BRANCH="$build_name" \
-  publish-provisional-artifacts -provisional -release -bucket "$bucket"
-tc_end_block "Compile and publish S3 artifacts"
-
-
-tc_start_block "Make and push docker image"
-configure_docker_creds
-docker_login_with_google
-
-# TODO: update publish-provisional-artifacts with option to leave one or more cockroach binaries in the local filesystem
-# NB: tar usually stops reading as soon as it sees an empty block but that makes
-# curl unhappy, so passing `i` will cause it to read to the end.
-curl -f -s -S -o- "https://${bucket}.s3.amazonaws.com/cockroach-${build_name}.linux-amd64.tgz" | tar ixfz - --strip-components 1
-cp cockroach lib/libgeos.so lib/libgeos_c.so build/deploy
-cp -r licenses build/deploy/
-
-docker build --no-cache --tag="${gcr_repository}:${build_name}" build/deploy
-docker push "${gcr_repository}:${build_name}"
-tc_end_block "Make and push docker image"
 
 
 tc_start_block "Push release tag to github.com/cockroachdb/cockroach"
