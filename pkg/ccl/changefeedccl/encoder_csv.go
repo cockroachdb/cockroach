ee566a19ccbb2c079e25b2adf7269ca4ded874a1
Sherman Grewal <sherman.grewal@cockroachlabs.com>
2022-04-14 11:39:53 -0400

    changefeedccl: support a CSV format for changefeeds In this PR, we introduce a new CSV format for changefeeds. Note that this format is only supported with the initial_scan='only' option. For instance, one can now execute:
    CREATE CHANGEFEED FOR foo WITH format=csv, initial_scan='only'
    
    Release note (enterprise change): Support a CSV format for
    changefeeds. Only works with initial_scan='only', and
    does not work with diff/resolved options.

---
+++ pkg/ccl/changefeedccl/encoder_csv.go
@@ -0,0 +1,119 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package changefeedccl
+
+import (
+	"bytes"
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/ccl/changefeedccl/changefeedbase"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/util/encoding/csv"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/errors"
+)
+
+type csvEncoder struct {
+	alloc                   tree.DatumAlloc
+	virtualColumnVisibility string
+
+	buf    *bytes.Buffer
+	writer *csv.Writer
+
+	include map[descpb.ColumnID]struct{}
+	version descpb.DescriptorVersion
+}
+
+var _ Encoder = &csvEncoder{}
+
+func newCSVEncoder(opts map[string]string) *csvEncoder {
+	newBuf := bytes.NewBuffer([]byte{})
+	newEncoder := &csvEncoder{
+		virtualColumnVisibility: opts[changefeedbase.OptVirtualColumns],
+		buf:                     newBuf,
+		writer:                  csv.NewWriter(newBuf),
+	}
+	newEncoder.writer.SkipNewline = true
+	return newEncoder
+}
+
+func buildIncludeMap(row encodeRow) (map[descpb.ColumnID]struct{}, error) {
+	family, err := row.tableDesc.FindFamilyByID(row.familyID)
+	if err != nil {
+		return nil, err
+	}
+	include := make(map[descpb.ColumnID]struct{}, len(family.ColumnIDs))
+	var yes struct{}
+	for _, colID := range family.ColumnIDs {
+		include[colID] = yes
+	}
+
+	return include, nil
+}
+
+// EncodeKey implements the Encoder interface.
+func (e *csvEncoder) EncodeKey(_ context.Context, row encodeRow) ([]byte, error) {
+	return nil, nil
+}
+
+// EncodeValue implements the Encoder interface.
+func (e *csvEncoder) EncodeValue(_ context.Context, row encodeRow) ([]byte, error) {
+	if row.deleted {
+		return nil, errors.Errorf(`cannot encode deleted rows into CSV format`)
+	}
+
+	var err error
+	rowVersion := row.tableDesc.GetVersion()
+	if rowVersion != e.version {
+		e.include, err = buildIncludeMap(row)
+		if err != nil {
+			return nil, err
+		}
+		e.version = rowVersion
+	}
+
+	// TODO(sherman): Currently we store the values of the row into this csvRow
+	// array and then call the CSV write function with this array passed in. This
+	// could incur a loss in performance when the row is very large, as allocating
+	// new memory can be a costly operation. Ideally, we should just write each
+	// value in the row directly to the buffer, rather than storing each value in
+	// an array and then writing the entire array to the buffer. There are no
+	// methods in the CSV writer package that would support this, which is why
+	// this has not been done so far. Keep an eye out for the performance of CSV
+	// encoding for large rows and determine if it would be better to create our
+	// own CSV writer that writes values directly to the buffer.
+	var csvRow []string
+
+	for i, col := range row.tableDesc.PublicColumns() {
+		_, inFamily := e.include[col.GetID()]
+		virtual := col.IsVirtual() && e.virtualColumnVisibility == string(changefeedbase.OptVirtualColumnsNull)
+		if inFamily || virtual {
+			datum := row.datums[i]
+			if err := datum.EnsureDecoded(col.GetType(), &e.alloc); err != nil {
+				return nil, err
+			}
+			csvRow = append(csvRow, tree.AsString(datum.Datum))
+		}
+	}
+
+	e.buf.Reset()
+	if err := e.writer.Write(csvRow); err != nil {
+		return nil, err
+	}
+	e.writer.Flush()
+	return e.buf.Bytes(), nil
+}
+
+// EncodeResolvedTimestamp implements the Encoder interface.
+func (e *csvEncoder) EncodeResolvedTimestamp(
+	_ context.Context, _ string, resolved hlc.Timestamp,
+) ([]byte, error) {
+	return nil, errors.New("EncodeResolvedTimestamp is not supported with the CSV encoder")
+}
