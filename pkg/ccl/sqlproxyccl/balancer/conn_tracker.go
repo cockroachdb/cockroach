a31b6592b8b2c2b74da0c2ddca54ba0774b12d92
Jay <jay@cockroachlabs.com>
2022-04-14 01:24:18 -0400

    ccl/sqlproxyccl: update connection tracker to use the tenant cache
    This commit updates the connection tracker in the balancer package to use the
    tenant cache introduced in the previous commit. Each tenant entry holds onto
    a tenant cache, which gets refreshed with the current list of connections once
    every 15 seconds. This cache will be used by the pod selection algorithm in
    a future PR. To prevent thundering herd issues where connections are routed to
    the same pod due to a stale cache, the cache is also updated manually whenever
    a connection gets added or removed from the connection tracker.
    
    Release note: None

--- pkg/ccl/sqlproxyccl/balancer/conn_tracker.go
+++ pkg/ccl/sqlproxyccl/balancer/conn_tracker.go
@@ -10,28 +10,58 @@ package balancer
 
 import (
 	"context"
+	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/logtags"
 )
+
+// cacheRefreshInterval refers to the interval that the tenant caches are
+// periodically refreshed. Note that the cache is also updated manually whenever
+// a connection gets added to or removed from the connection tracker.
+const cacheRefreshInterval = 15 * time.Second
 
 // ConnTracker tracks all connection handles associated with each tenant, and
 // handles are grouped by SQL pods.
 type ConnTracker struct {
+	// timeSource is the source of the time, and uses timeutil.DefaultTimeSource
+	// by default. This is often replaced in tests.
+	timeSource timeutil.TimeSource
+
+	// mu synchronizes access to the list of tenants.
 	mu struct {
 		syncutil.Mutex
+
+		// tenants refer to a list of tenant entries.
 		tenants map[roachpb.TenantID]*tenantEntry
 	}
 }
 
 // NewConnTracker returns a new instance of the connection tracker. All exposed
 // methods on the connection tracker are thread-safe.
-func NewConnTracker() *ConnTracker {
-	t := &ConnTracker{}
+func NewConnTracker(
+	ctx context.Context, stopper *stop.Stopper, timeSource timeutil.TimeSource,
+) (*ConnTracker, error) {
+	// Ensure that ctx gets cancelled on stopper's quiescing.
+	ctx, _ = stopper.WithCancelOnQuiesce(ctx)
+
+	if timeSource == nil {
+		timeSource = timeutil.DefaultTimeSource{}
+	}
+
+	t := &ConnTracker{timeSource: timeSource}
 	t.mu.tenants = make(map[roachpb.TenantID]*tenantEntry)
-	return t
+
+	if err := stopper.RunAsyncTask(
+		ctx, "refresh-tenant-cache", t.refreshTenantCachesLoop,
+	); err != nil {
+		return nil, err
+	}
+	return t, nil
 }
 
 // OnConnect registers the connection handle for tracking under the given
@@ -69,6 +99,24 @@ func (t *ConnTracker) GetConnsMap(tenantID roachpb.TenantID) map[string][]Connec
 	return e.getConnsMap()
 }
 
+// GetTenantCache returns the given tenant's cache that contains cached data
+// about the tenant (e.g. number of active/idle connections). This cache gets
+// refreshed with the current data periodically, and updated whenever a new
+// connection gets added to or removed from the connection tracker.
+// GetTenantCache returns nil if the tenant does not exist in the connection
+// tracker.
+//
+// TODO(jaylim-crl): Consider moving the connection map returned from
+// GetConnsMap into the tenant cache for a cleaner API. We'll revisit this
+// decision once we add the rebalancing logic to the balancer.
+func (t *ConnTracker) GetTenantCache(tenantID roachpb.TenantID) TenantCache {
+	e := t.getEntry(tenantID, false /* allowCreate */)
+	if e == nil {
+		return nil
+	}
+	return e.getCache()
+}
+
 // GetTenantIDs returns a list of tenant IDs that have at least one connection
 // registered with the connection tracker.
 func (t *ConnTracker) GetTenantIDs() []roachpb.TenantID {
@@ -101,6 +149,38 @@ func (t *ConnTracker) getEntry(tenantID roachpb.TenantID, allowCreate bool) *ten
 	return entry
 }
 
+// refreshTenantCachesLoop runs on a background goroutine to continuously
+// refresh all tenant caches in the connection tracker, once every
+// cacheRefreshInterval.
+func (t *ConnTracker) refreshTenantCachesLoop(ctx context.Context) {
+	timer := t.timeSource.NewTimer()
+	defer timer.Stop()
+	for {
+		timer.Reset(cacheRefreshInterval)
+		select {
+		case <-ctx.Done():
+			return
+		case <-timer.Ch():
+			timer.MarkRead()
+			t.refreshTenantCaches()
+		}
+	}
+}
+
+// refreshTenantCaches refreshes the cache for all the tenants associated with
+// the connection tracker.
+func (t *ConnTracker) refreshTenantCaches() {
+	tenantIDs := t.GetTenantIDs()
+	for _, tenantID := range tenantIDs {
+		e := t.getEntry(tenantID, false /* allowCreate */)
+		if e != nil {
+			e.refreshCache()
+		}
+	}
+}
+
+// logTrackerEvent logs an event based on the logtags attached to the connection
+// goroutine.
 func logTrackerEvent(event string, handle ConnectionHandle) {
 	// The right approach would be for the caller to pass in a ctx object. For
 	// simplicity, we'll just use a background context here since it's only used
@@ -113,17 +193,27 @@ func logTrackerEvent(event string, handle ConnectionHandle) {
 // tenantEntry is a connection tracker entry that stores connection information
 // for a single tenant. All methods on tenantEntry are thread-safe.
 type tenantEntry struct {
+	// cache corresponds to the tenant cache that is used to cache the number of
+	// active/idle connections by pod. A cache is used here to avoid frequent
+	// computations of such numbers (which would require iterating all
+	// connections for the given tenant).
+	cache *tenantCache
+
 	// mu synchronizes access to the list of connections associated with this
 	// tenant.
 	mu struct {
 		syncutil.Mutex
+
+		// conns refers to a set of connection handles.
 		conns map[ConnectionHandle]struct{}
 	}
 }
 
 // newTenantEntry returns a new instance of tenantEntry.
 func newTenantEntry() *tenantEntry {
-	e := &tenantEntry{}
+	e := &tenantEntry{
+		cache: newTenantCache(),
+	}
 	e.mu.conns = make(map[ConnectionHandle]struct{})
 	return e
 }
@@ -131,7 +221,7 @@ func newTenantEntry() *tenantEntry {
 // addHandle adds the handle to the entry based on the handle's active server
 // remote address. This returns true if the operation was successful, and false
 // otherwise.
-func (e *tenantEntry) addHandle(handle ConnectionHandle) bool {
+func (e *tenantEntry) addHandle(handle ConnectionHandle) (success bool) {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 
@@ -140,12 +230,15 @@ func (e *tenantEntry) addHandle(handle ConnectionHandle) bool {
 		return false
 	}
 	e.mu.conns[handle] = struct{}{}
+
+	// New connections are always active.
+	e.cache.updateActiveCount(handle.ServerRemoteAddr(), 1)
 	return true
 }
 
 // removeHandle deletes the handle from the tenant entry. This returns true if
 // the operation was successful, and false otherwise.
-func (e *tenantEntry) removeHandle(handle ConnectionHandle) bool {
+func (e *tenantEntry) removeHandle(handle ConnectionHandle) (success bool) {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 
@@ -154,6 +247,17 @@ func (e *tenantEntry) removeHandle(handle ConnectionHandle) bool {
 		return false
 	}
 	delete(e.mu.conns, handle)
+
+	// We'll update the cache based on the data that we have now. It is possible
+	// that the cache was computed using a different state (e.g. cache does not
+	// include this connection, or cache counted this connection as active, but
+	// is idle now), resulting in us decrementing from a different map here.
+	// Regardless, since this is a cache, accuracy does not matter.
+	if handle.IsIdle() {
+		e.cache.updateIdleCount(handle.ServerRemoteAddr(), -1)
+	} else {
+		e.cache.updateActiveCount(handle.ServerRemoteAddr(), -1)
+	}
 	return true
 }
 
@@ -191,4 +295,31 @@ func (e *tenantEntry) getConnsCount() int {
 	defer e.mu.Unlock()
 
 	return len(e.mu.conns)
+}
+
+// getCache returns the cache for the given tenant.
+func (e *tenantEntry) getCache() TenantCache {
+	return e.cache
+}
+
+// refreshCache updates the cache associated with the tenant. This iterates
+// through all connections for the tenant, so use with care.
+func (e *tenantEntry) refreshCache() {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+
+	// Construct cache based on current data.
+	cacheData := newTenantCacheData()
+	for handle := range e.mu.conns {
+		// Connection has been closed.
+		if handle.Context().Err() != nil {
+			continue
+		}
+		if handle.IsIdle() {
+			cacheData.idleConnsCount[handle.ServerRemoteAddr()]++
+		} else {
+			cacheData.activeConnsCount[handle.ServerRemoteAddr()]++
+		}
+	}
+	e.cache.refreshData(cacheData)
 }
