8cb8fe07ee60a925be47ddbbda8c595cb7621bf4
nnaka2992 <nnaka2992@gmail.com>
2022-04-16 01:37:20 +0900

    sql: implement ALTER/CREATE SEQUENCE ... RESTART fixes #74496 what you did about it: Extended the ALTER SEQUENCE command to support restarting the sequence with and without value. The new command call nextval('sequence_name') internally as PostgreSQL does. At the same time this supports CREATE SEQUENCE sequence_name RESTART command for PostgreSQL compatibility.
    Release note (sql change): Add a new RESTART option to ALTER and
    CREATE SEQUENCE which sets the nextval to arbitary number or
    a value for START.

--- pkg/sql/alter_sequence.go
+++ pkg/sql/alter_sequence.go
@@ -141,8 +141,10 @@ func (n *alterSequenceNode) startExec(params runParams) error {
 		}
 	}
 	if opts.Restart != nil {
-		err := params.p.SetSequenceValueByID(params.ctx, uint32(desc.ID), *opts.Restart, false)
-		if err != nil {
+		if err := params.p.createdSequences.addCreatedSequence(desc.ID); err != nil {
+			return err
+		}
+		if err := params.p.SetSequenceValueByID(params.ctx, uint32(desc.ID), *opts.Restart, false); err != nil {
 			return err
 		}
 	}
