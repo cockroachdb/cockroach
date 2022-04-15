b3615277f96864115c5127f85bfd793ec4fef693
Sean Barag <barag@cockroachlabs.com>
2022-04-15 08:41:05 -0700

    ui: reintroduce end-to-end UI tests with cypress
    A recent commit [1] removed the stale, unused end-to-end tests. In their
    place, add newly-written tests that can run headlessly in TeamCity (via
    Docker) as a quick-failing health check and as a more exhaustive suite.
    
    [1] 3d171b234b (ui: remove unused end-to-end UI tests, 2022-07-25)
    
    Release note: None

---
+++ pkg/ui/workspaces/e2e-tests/prettier.config.js
@@ -0,0 +1,16 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+module.exports = {
+  trailingComma: "all",
+  // use default options from 1.19: https://prettier.io/blog/2020/03/21/2.0.0.html#breaking-changes
+  arrowParens: "avoid",
+  endOfLine: "auto",
+};
