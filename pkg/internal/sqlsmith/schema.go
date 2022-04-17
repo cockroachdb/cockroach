d7b90169309c43e50a408dd0589212dd154a66d7
Rebecca Taft <becca@cockroachlabs.com>
2022-04-17 19:12:47 -0400

    sqlsmith: skip crdb_internal.reset_multi_region_zone_configs_for_database
    Don't use crdb_internal.reset_multi_region_zone_configs_for_database when
    generating sqlsmith queries, since it currently causes an internal error.
    
    Informs #80023
    
    Release note: None

--- pkg/internal/sqlsmith/schema.go
+++ pkg/internal/sqlsmith/schema.go
@@ -502,6 +502,7 @@ var functions = func() map[tree.FunctionClass]map[oid.Oid][]function {
 			"crdb_internal.reset_index_usage_stats",
 			"crdb_internal.start_replication_stream",
 			"crdb_internal.replication_stream_progress",
+			"crdb_internal.reset_multi_region_zone_configs_for_database",
 		} {
 			skip = skip || strings.Contains(def.Name, substr)
 		}
