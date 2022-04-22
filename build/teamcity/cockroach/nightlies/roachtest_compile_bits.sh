5947a5d875f1546cad1c645255c06b5aca0d8558
Oliver Tan <otan@cockroachlabs.com>
2022-04-23 08:35:08 +1000

    nightlies: fix bazel compilation
    Release note: None

--- build/teamcity/cockroach/nightlies/roachtest_compile_bits.sh
+++ build/teamcity/cockroach/nightlies/roachtest_compile_bits.sh
@@ -2,7 +2,7 @@
 
 # Builds all bits needed for roachtests, stages them in bin/ and lib.docker_amd64/.
 
-bazel build --config crosslinux --config ci --config with_ui -c opt \
+bazel build --config crosslinux --config ci --config with_ui -c opt --config force_build_cdeps \
       //pkg/cmd/cockroach //pkg/cmd/workload //pkg/cmd/roachtest \
       //c-deps:libgeos
 BAZEL_BIN=$(bazel info bazel-bin --config crosslinux --config ci --config with_ui -c opt)
@@ -16,7 +16,7 @@ chmod a+w bin/cockroach bin/roachtest bin/workload
 # Stage the geos libs in the appropriate spot.
 mkdir -p lib.docker_amd64
 chmod o+rwx lib.docker_amd64
-cp $BAZEL_BIN/c-deps/libgeos/lib/libgeos.so   lib.docker_amd64
-cp $BAZEL_BIN/c-deps/libgeos/lib/libgeos_c.so lib.docker_amd64
+cp $BAZEL_BIN/c-deps/libgeos_foreign/lib/libgeos.so   lib.docker_amd64
+cp $BAZEL_BIN/c-deps/libgeos_foreign/lib/libgeos_c.so lib.docker_amd64
 chmod a+w lib.docker_amd64/libgeos.so lib.docker_amd64/libgeos_c.so
 ln -s lib.docker_amd64 lib
