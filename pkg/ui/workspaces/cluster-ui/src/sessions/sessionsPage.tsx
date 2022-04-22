57fc667c7bcf0455b0836705e40f1043b5cd4224
Gerardo Torres <gerardo.torrescastro@cockroachlabs.com>
2022-04-22 17:10:58 -0400

    ui: display closed sessions, add username and session status filter
    Fixes #67888, #79914.
    
    Previously, the sessions page UI did not support displaying closed
    sessions and did not support the ability to filter by username or
    session status. This commit adds the "Closed" session status to closed
    sessions and adds the ability to filter by username and session status.
    
    Release note (ui change): sessions overview and session details pages now
    display closed sessions; sessions overview page now has username and session
    status filters

--- pkg/ui/workspaces/cluster-ui/src/sessions/sessionsPage.tsx
+++ pkg/ui/workspaces/cluster-ui/src/sessions/sessionsPage.tsx
@@ -13,6 +13,7 @@ import { isNil, merge } from "lodash";
 
 import { syncHistory } from "src/util/query";
 import {
+  getStatusString,
   makeSessionsColumns,
   SessionInfo,
   SessionsSortedTable,
@@ -110,6 +111,20 @@ function getSessionAppFilterOptions(sessions: SessionInfo[]): string[] {
   );
 
   return Array.from(uniqueAppNames).sort();
+}
+
+function getSessionUsernameFilterOptions(sessions: SessionInfo[]): string[] {
+  const uniqueUsernames = new Set(sessions.map(s => s.session.username));
+
+  return Array.from(uniqueUsernames).sort();
+}
+
+function getSessionStatusFilterOptions(sessions: SessionInfo[]): string[] {
+  const uniqueStatuses = new Set(
+    sessions.map(s => getStatusString(s.session.status)),
+  );
+
+  return Array.from(uniqueStatuses).sort();
 }
 
 export class SessionsPage extends React.Component<
@@ -298,6 +313,20 @@ export class SessionsPage extends React.Component<
           "seconds",
         );
         return sessionTime >= timeValue || timeValue === "empty";
+      })
+      .filter((s: SessionInfo) => {
+        if (filters.username && filters.username != "All") {
+          const usernames = filters.username.split(",");
+          return usernames.includes(s.session.username);
+        }
+        return true;
+      })
+      .filter((s: SessionInfo) => {
+        if (filters.sessionStatus && filters.sessionStatus != "All") {
+          const statuses = filters.sessionStatus.split(",");
+          return statuses.includes(getStatusString(s.session.status));
+        }
+        return true;
       });
 
     return {
@@ -317,6 +346,8 @@ export class SessionsPage extends React.Component<
     } = this.getFilteredSessionsData();
 
     const appNames = getSessionAppFilterOptions(sessionsData);
+    const usernames = getSessionUsernameFilterOptions(sessionsData);
+    const sessionStatuses = getSessionStatusFilterOptions(sessionsData);
     const columns = makeSessionsColumns(
       "session",
       this.terminateSessionRef,
@@ -354,6 +385,10 @@ export class SessionsPage extends React.Component<
           <Filter
             onSubmitFilters={this.onSubmitFilters}
             appNames={appNames}
+            usernames={usernames}
+            showUsername={true}
+            showSessionStatus={true}
+            sessionStatuses={sessionStatuses}
             activeFilters={activeFilters}
             filters={filters}
             timeLabel={timeLabel}
