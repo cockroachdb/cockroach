47dff27edb941feb169b84db416792cabcc7f7d0
Yevgeniy Miretskiy <yevgeniy@cockroachlabs.com>
2022-04-22 17:14:10 -0400

    changefeedccl: Add a placeholder option.
    Add a cluster version gated placeholder option for
    potential backports of span constrained changefeeds.
    
    Release Notes: None
    Release Justification: low danger, no functionality change.

--- pkg/ccl/changefeedccl/changefeed_stmt.go
+++ pkg/ccl/changefeedccl/changefeed_stmt.go
@@ -876,6 +876,14 @@ func validateDetails(details jobspb.ChangefeedDetails) (jobspb.ChangefeedDetails
 			)
 		}
 	}
+	{
+		_, isSet := details.Opts[changefeedbase.OptPrimaryKeyFilter]
+		if isSet {
+			return jobspb.ChangefeedDetails{}, pgerror.Newf(pgcode.FeatureNotSupported,
+				"option %s is not currently supported", changefeedbase.OptPrimaryKeyFilter,
+			)
+		}
+	}
 	return details, nil
 }
 
