a8675565813898c3cf005d662076cb21fe6972eb
Yevgeniy Miretskiy <yevgeniy@cockroachlabs.com>
2022-04-22 16:36:32 -0400

    sql: Introduce `SpanConstrainer` interface.
    Add a small `SpanConstrainer` interface to enable direct
    usage of optimizer to constrain a span based on filtering
    expression.
    
    The users of this functionality, such as changefeeds (or rangefeed)
    would be able to leverage optimizer to constrain the span that
    they need to watch -- thus improving flexibility and efficiency.
    
    Release Notes: None

--- pkg/sql/job_exec_context.go
+++ pkg/sql/job_exec_context.go
@@ -11,10 +11,14 @@
 package sql
 
 import (
+	"context"
+
 	"github.com/cockroachdb/cockroach/pkg/kv"
 	"github.com/cockroachdb/cockroach/pkg/migration"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/security"
 	"github.com/cockroachdb/cockroach/pkg/spanconfig"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/lease"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
@@ -67,6 +71,17 @@ func (e *plannerJobExecContext) SpanConfigReconciler() spanconfig.Reconciler {
 }
 func (e *plannerJobExecContext) Txn() *kv.Txn { return e.p.Txn() }
 
+// ConstrainPrimaryIndexSpanByExpr implements SpanConstrainer
+func (e *plannerJobExecContext) ConstrainPrimaryIndexSpanByExpr(
+	ctx context.Context,
+	desc catalog.TableDescriptor,
+	evalCtx *tree.EvalContext,
+	semaCtx *tree.SemaContext,
+	filter tree.Expr,
+) ([]roachpb.Span, error) {
+	return e.p.ConstrainPrimaryIndexSpanByExpr(ctx, desc, evalCtx, semaCtx, filter)
+}
+
 // JobExecContext provides the execution environment for a job. It is what is
 // passed to the Resume/OnFailOrCancel/OnPauseRequested methods of a jobs's
 // Resumer to give that resumer access to things like ExecutorCfg, LeaseMgr,
@@ -77,6 +92,7 @@ func (e *plannerJobExecContext) Txn() *kv.Txn { return e.p.Txn() }
 // (though note that ExtendedEvalContext may transitively include methods that
 // close over/expect a txn so use it with caution).
 type JobExecContext interface {
+	SpanConstrainer
 	SemaCtx() *tree.SemaContext
 	ExtendedEvalContext() *extendedEvalContext
 	SessionData() *sessiondata.SessionData
