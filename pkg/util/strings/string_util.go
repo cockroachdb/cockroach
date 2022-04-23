13b671acab05a14ade21f449c195bd334b2c9734
Daniel Hix <danieljacobhix@gmail.com>
2022-04-22 21:13:08 -0500

    Add utility functions for cleaning strings
    This will be a common function so a util package with tests make sense for it.

---
+++ pkg/util/strings/string_util.go
@@ -0,0 +1,46 @@
+// Copyright 2012, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in licenses/BSD-vitess.txt.
+
+// Portions of this file are additionally subject to the following
+// license and copyright.
+//
+// Copyright 2017 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package strings
+
+import "strings"
+
+// CollapseDupeChar will take the given string and given character
+// and collapse any repeating instances of this character to a
+// single instance.
+// E.g. CollapseDupeChar("wwwhy hello there", 'w') returns "why hello there"
+func CollapseDupeChar(toCollapse string, collapseChar rune) string {
+	var builder strings.Builder
+	hadPrevious := false
+	for _, r := range toCollapse {
+		if r != collapseChar {
+			hadPrevious = false
+			builder.WriteRune(r)
+			continue
+		}
+
+		// Our character matches
+		if hadPrevious {
+			continue
+		}
+
+		hadPrevious = true
+		builder.WriteRune(r)
+	}
+
+	return builder.String()
+}
---
+++ pkg/util/strings/string_util_test.go
@@ -0,0 +1,42 @@
+// Copyright 2012, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in licenses/BSD-vitess.txt.
+
+// Portions of this file are additionally subject to the following
+// license and copyright.
+//
+// Copyright 2017 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package strings
+
+import "testing"
+
+type StringTest struct {
+	ToTest   string
+	DupeChar rune
+	Expected string
+}
+
+func TestCollapseDupeChar(t *testing.T) {
+	tests := []StringTest{
+		{"%%%%%%%%this is a test%%%%%%%%%", '%', "%this is a test%"},
+		{"%%%%%%test1%%%%%%%test2%%%%%test3%%%%", '%', "%test1%test2%test3%"},
+		{"I work on ddddddifferent characters", 'd', "I work on different characters"},
+	}
+
+	for _, test := range tests {
+		result := CollapseDupeChar(test.ToTest, test.DupeChar)
+
+		if result != test.Expected {
+			t.Errorf("expected %s but got %s", test.Expected, result)
+		}
+	}
+}
