3ef8afa957a4c75809603b3d4137092f57451f36
Jay <jay@cockroachlabs.com>
2022-04-23 14:47:11 -0400

    ccl/sqlproxyccl: introduce a new ServerAssignment struct
    This commit introduces a new ServerAssignment struct that will be tracked by
    the connection tracker through {register,unregister}Assignment. At the moment,
    those functions do nothing. The plan is to update the connection tracker to
    track these assignments instead of the forwarder instances. This helps in two
    ways:
    1. We can now update the counts map the moment a SQL pod is assigned to an
       incoming request, rather than waiting until a forwarder has been created.
    2. TenantCache can be removed since we can easily maintain an up-to-date count
       map. (Previously, with the forwarder approach, we would need to keep that
       updated everytime we move the connection around. With this approach, once
       an assignment is handed out, the destination does not change. A transfer
       operation just requests for a new assignment.)
    
    Release note: None

---
+++ pkg/ccl/sqlproxyccl/balancer/assignment.go
@@ -0,0 +1,56 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package balancer
+
+import (
+	"sync"
+
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+)
+
+// ServerAssignment represents an assignment to a SQL pod.
+type ServerAssignment struct {
+	owner   ConnectionHandle
+	addr    string
+	onClose struct {
+		sync.Once
+		closerFn func()
+	}
+}
+
+// NewServerAssignment returns a new instance of ServerAssignment. Instances of
+// ServerAssignment must be closed through Close once no longer in use.
+func NewServerAssignment(
+	tenantID roachpb.TenantID, tracker *ConnTracker, owner ConnectionHandle, addr string,
+) *ServerAssignment {
+	sa := &ServerAssignment{owner: owner, addr: addr}
+	sa.onClose.closerFn = func() {
+		tracker.unregisterAssignment(tenantID, sa)
+	}
+	tracker.registerAssignment(tenantID, sa)
+	return sa
+}
+
+// Owner returns the connection handle associated with the server assignment.
+// The connection handle may not be initialized yet, so callers will need to
+// check for that where necessary.
+func (sa *ServerAssignment) Owner() ConnectionHandle {
+	return sa.owner
+}
+
+// Addr returns the address of the server assignment.
+func (sa *ServerAssignment) Addr() string {
+	return sa.addr
+}
+
+// Close cleans up the server assignment and deregisters it from the connection
+// tracker. This is idempotent.
+func (sa *ServerAssignment) Close() {
+	sa.onClose.Do(sa.onClose.closerFn)
+}
---
+++ pkg/ccl/sqlproxyccl/balancer/assignment_test.go
@@ -0,0 +1,36 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package balancer
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
+	"github.com/stretchr/testify/require"
+)
+
+func TestServerAssignment(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	ctx := context.Background()
+	stopper := stop.NewStopper()
+	defer stopper.Stop(ctx)
+
+	tracker, err := NewConnTracker(ctx, stopper, nil /* timeSource */)
+	require.NoError(t, err)
+	tenantID, handle := makeConn(10, "foo-bar-baz")
+
+	sa := NewServerAssignment(tenantID, tracker, handle, "127.0.0.10")
+	require.Equal(t, handle, sa.Owner())
+	require.Equal(t, "127.0.0.10", sa.Addr())
+	// TODO(jaylim-crl): test Close once {register,unregister}Assignment gets
+	// implemented.
+}
