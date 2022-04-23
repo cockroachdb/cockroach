4f3c010da35a16a41c45c1967270d813cd2f63ef
Yevgeniy Miretskiy <yevgeniy@cockroachlabs.com>
2022-04-23 12:54:18 -0400

    changfeedccl: Support filter over primary key span.
    Extend changfeeds to support a filtering expression over primary key
    span via `primary_key_filter` option.
    
    When specified, this option is treated as an SQL filter expression
    over primary key columns.  This filter can then be used to constrain
    the spans that's watched by changefeed only to those that satisfy
    the expression.  The use of this filter restricted only to the
    changefeeds over a single table.
    
    Release Notes (enterprise change): `primary_key_filter` option can be used
    to restrict the span watched by changefeed only to the portion that
    satisfies the filtering predicate.

--- pkg/ccl/changefeedccl/changefeedbase/options.go
+++ pkg/ccl/changefeedccl/changefeedbase/options.go
@@ -71,6 +71,7 @@ const (
 	OptOnError                  = `on_error`
 	OptMetricsScope             = `metrics_label`
 	OptVirtualColumns           = `virtual_columns`
+	OptPrimaryKeyFilter         = `primary_key_filter`
 
 	OptVirtualColumnsOmitted VirtualColumnVisibility = `omitted`
 	OptVirtualColumnsNull    VirtualColumnVisibility = `null`
@@ -209,6 +210,7 @@ var ChangefeedOptionExpectValues = map[string]sql.KVStringOptValidate{
 	OptOnError:                  sql.KVStringOptRequireValue,
 	OptMetricsScope:             sql.KVStringOptRequireValue,
 	OptVirtualColumns:           sql.KVStringOptRequireValue,
+	OptPrimaryKeyFilter:         sql.KVStringOptRequireValue,
 }
 
 func makeStringSet(opts ...string) map[string]struct{} {
@@ -228,7 +230,7 @@ var CommonOptions = makeStringSet(OptCursor, OptEndTime, OptEnvelope,
 	OptSchemaChangeEvents, OptSchemaChangePolicy,
 	OptProtectDataFromGCOnPause, OptOnError,
 	OptInitialScan, OptNoInitialScan, OptInitialScanOnly,
-	OptMinCheckpointFrequency, OptMetricsScope, OptVirtualColumns, Topics)
+	OptMinCheckpointFrequency, OptMetricsScope, OptVirtualColumns, Topics, OptPrimaryKeyFilter)
 
 // SQLValidOptions is options exclusive to SQL sink
 var SQLValidOptions map[string]struct{} = nil
