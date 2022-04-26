2cc03c18f9eff311902bee3850d14cf9ec5d5562
sumeerbhola <sumeer@cockroachlabs.com>
2022-04-26 10:08:42 -0400

    rangefeed,storage: use time-bound iterator for with-diff catchup scan
    The MVCCIncrementalIterator already has support for selectively
    seeing versions beyond the time bounds, via the NextIgnoringTime
    method. This method is sufficient for with-diff catchup scans -- the
    changes to MVCCIncrementalIterator here are limited to comment
    improvements.
    
    To utilize NextIgnoringTime in the CatchUpIterator we need to
    distinguish between the reasons that a catchup scan wants to
    iterate to an older version of a key. These are now handled by
    different methods via the simpleCatchupIter interface. There
    is a simpleCatchupIterAdapter to provide a trivial implementation
    when the underlying iterator is a SimpleMVCCIterator.
    
    There is more testing of MVCCIncrementalIterator's NextIgnoringTime,
    and there is now a TestCatchupScan that tests with and without diff.
    BenchmarkCatchUpScan now also benchmarks with-diff. Results for
    linear-keys, where time-bound iteration shows an improvement:
    
    BenchmarkCatchUpScan/linear-keys/useTBI=true/withDiff=true/perc=0.00-16         	       3	 397238811 ns/op	253609173 B/op	 3006262 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=true/withDiff=true/perc=50.00-16        	       6	 192314659 ns/op	126847284 B/op	 1503167 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=true/withDiff=true/perc=75.00-16        	      12	  93243340 ns/op	63433929 B/op	  751606 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=true/withDiff=true/perc=95.00-16        	      66	  17636059 ns/op	12691501 B/op	  150356 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=true/withDiff=true/perc=99.00-16        	     296	   3566355 ns/op	 2560979 B/op	   30126 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=true/withDiff=false/perc=0.00-16        	       2	 587807376 ns/op	253689672 B/op	 3006770 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=true/withDiff=false/perc=50.00-16       	       4	 286110763 ns/op	126893106 B/op	 1503413 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=true/withDiff=false/perc=75.00-16       	       8	 140012384 ns/op	63458643 B/op	  751740 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=true/withDiff=false/perc=95.00-16       	      43	  26210520 ns/op	12695466 B/op	  150380 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=true/withDiff=false/perc=99.00-16       	     200	   5381206 ns/op	 2561444 B/op	   30133 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=false/withDiff=true/perc=0.00-16        	       3	 379387200 ns/op	253577896 B/op	 3006228 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=false/withDiff=true/perc=50.00-16       	       4	 294533788 ns/op	130799366 B/op	 1503677 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=false/withDiff=true/perc=75.00-16       	       4	 254943713 ns/op	69418826 B/op	  752487 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=false/withDiff=true/perc=95.00-16       	       5	 220269848 ns/op	20291392 B/op	  151411 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=false/withDiff=true/perc=99.00-16       	       5	 211813333 ns/op	10473760 B/op	   31231 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=false/withDiff=false/perc=0.00-16       	       3	 379575618 ns/op	253591349 B/op	 3006275 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=false/withDiff=false/perc=50.00-16      	       4	 282490494 ns/op	126852270 B/op	 1503388 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=false/withDiff=false/perc=75.00-16      	       5	 241938049 ns/op	63463176 B/op	  751985 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=false/withDiff=false/perc=95.00-16      	       5	 210848534 ns/op	12741640 B/op	  150944 allocs/op
    BenchmarkCatchUpScan/linear-keys/useTBI=false/withDiff=false/perc=99.00-16      	       6	 196175587 ns/op	 2602912 B/op	   30652 allocs/op
    
    Fixes #78974
    
    Release note: None

--- pkg/kv/kvserver/rangefeed/catchup_scan.go
+++ pkg/kv/kvserver/rangefeed/catchup_scan.go
@@ -24,9 +24,33 @@ import (
 
 // A CatchUpIterator is an iterator for catchUp-scans.
 type CatchUpIterator struct {
-	storage.SimpleMVCCIterator
+	simpleCatchupIter
 	close func()
 }
+
+// simpleCatchupIter is an extension of SimpleMVCCIterator that allows for the
+// primary iterator to be implemented using a regular MVCCIterator or a
+// (often) more efficient MVCCIncrementalIterator. When the caller wants to
+// iterate to see older versions of a key, the desire of the caller needs to
+// be expressed using one of two methods:
+//   - Next: when it wants to omit any versions that are not within the time
+//     bounds.
+//   - NextIgnoringTime: when it wants to see the next older version even if it
+//     is not within the time bounds.
+type simpleCatchupIter interface {
+	storage.SimpleMVCCIterator
+	NextIgnoringTime()
+}
+
+type simpleCatchupIterAdapter struct {
+	storage.SimpleMVCCIterator
+}
+
+func (i simpleCatchupIterAdapter) NextIgnoringTime() {
+	i.SimpleMVCCIterator.Next()
+}
+
+var _ simpleCatchupIter = simpleCatchupIterAdapter{}
 
 // NewCatchUpIterator returns a CatchUpIterator for the given Reader.
 // If useTBI is true, a time-bound iterator will be used if possible,
@@ -37,13 +61,8 @@ func NewCatchUpIterator(
 	ret := &CatchUpIterator{
 		close: closer,
 	}
-	// TODO(ssd): The withDiff option requires us to iterate over
-	// values arbitrarily in the past so that we can populate the
-	// previous value of a key. This is possible since the
-	// IncrementalIterator has a non-timebound iterator
-	// internally, but it is not yet implemented.
-	if useTBI && !args.WithDiff {
-		ret.SimpleMVCCIterator = storage.NewMVCCIncrementalIterator(reader, storage.MVCCIncrementalIterOptions{
+	if useTBI {
+		ret.simpleCatchupIter = storage.NewMVCCIncrementalIterator(reader, storage.MVCCIncrementalIterOptions{
 			EnableTimeBoundIteratorOptimization: true,
 			EndKey:                              args.Span.EndKey,
 			// StartTime is exclusive but args.Timestamp
@@ -63,9 +82,10 @@ func NewCatchUpIterator(
 			InlinePolicy: storage.MVCCIncrementalIterInlinePolicyEmit,
 		})
 	} else {
-		ret.SimpleMVCCIterator = reader.NewMVCCIterator(storage.MVCCKeyAndIntentsIterKind, storage.IterOptions{
+		iter := reader.NewMVCCIterator(storage.MVCCKeyAndIntentsIterKind, storage.IterOptions{
 			UpperBound: args.Span.EndKey,
 		})
+		ret.simpleCatchupIter = simpleCatchupIterAdapter{SimpleMVCCIterator: iter}
 	}
 
 	return ret
@@ -74,7 +94,7 @@ func NewCatchUpIterator(
 // Close closes the iterator and calls the instantiator-supplied close
 // callback.
 func (i *CatchUpIterator) Close() {
-	i.SimpleMVCCIterator.Close()
+	i.simpleCatchupIter.Close()
 	if i.close != nil {
 		i.close()
 	}
@@ -86,8 +106,8 @@ func (i *CatchUpIterator) Close() {
 type outputEventFn func(e *roachpb.RangeFeedEvent) error
 
 // CatchUpScan iterates over all changes for the given span of keys,
-// starting at catchUpTimestamp.  Keys and Values are emitted as
-// RangeFeedEvents passed to the given outputFn.
+// starting at catchUpTimestamp. Keys and Values are emitted as
+// RangeFeedEvents passed to the given outputFn. catchUpTimestamp is exclusive.
 func (i *CatchUpIterator) CatchUpScan(
 	startKey, endKey storage.MVCCKey,
 	catchUpTimestamp hlc.Timestamp,
@@ -107,6 +127,8 @@ func (i *CatchUpIterator) CatchUpScan(
 			if reorderBuf[l-1].Val.PrevValue.IsPresent() {
 				panic("RangeFeedValue.PrevVal unexpectedly set")
 			}
+			// TODO(sumeer): find out if it is deliberate that we are not populating
+			// PrevValue.Timestamp.
 			reorderBuf[l-1].Val.PrevValue.RawBytes = val
 		}
 	}
@@ -143,18 +165,23 @@ func (i *CatchUpIterator) CatchUpScan(
 			}
 			if !meta.IsInline() {
 				// This is an MVCCMetadata key for an intent. The catchUp scan
-				// only cares about committed values, so ignore this and skip
-				// past the corresponding provisional key-value. To do this,
-				// scan to the timestamp immediately before (i.e. the key
-				// immediately after) the provisional key.
-				//
-				// Make a copy since should not pass an unsafe key from the iterator
-				// that provided it, when asking it to seek.
-				a, unsafeKey.Key = a.Copy(unsafeKey.Key, 0)
-				i.SeekGE(storage.MVCCKey{
-					Key:       unsafeKey.Key,
-					Timestamp: meta.Timestamp.ToTimestamp().Prev(),
-				})
+				// only cares about committed values, so ignore this and skip past
+				// the corresponding provisional key-value. To do this, iterate to
+				// the provisional key-value, validate its timestamp, then iterate
+				// again. When using MVCCIncrementalIterator we know that the
+				// provisional value will also be within the time bounds so we use
+				// Next.
+				i.Next()
+				if ok, err := i.Valid(); err != nil {
+					return errors.Wrap(err, "iterating to provisional value for intent")
+				} else if !ok {
+					return errors.Errorf("expected provisional value for intent")
+				}
+				if !meta.Timestamp.ToTimestamp().EqOrdering(i.UnsafeKey().Timestamp) {
+					return errors.Errorf("expected provisional value for intent with ts %s, found %s",
+						meta.Timestamp, i.UnsafeKey().Timestamp)
+				}
+				i.Next()
 				continue
 			}
 
@@ -231,8 +258,15 @@ func (i *CatchUpIterator) CatchUpScan(
 			// Skip all the way to the next key.
 			i.NextKey()
 		} else {
-			// Move to the next version of this key.
-			i.Next()
+			// Move to the next version of this key (there may not be one, in which
+			// case it will move to the next key).
+			if withDiff {
+				// Need to see the next version even if it is older than the time
+				// bounds.
+				i.NextIgnoringTime()
+			} else {
+				i.Next()
+			}
 		}
 	}
 
