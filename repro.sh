25c16a9a07b53ddeefc43cfe4b4ea06a004c2c89
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-04-28 15:10:50 +0100

    toolbox to run against real cluster
    Release note: None

--- repro.sh
+++ repro.sh
@@ -2,23 +2,33 @@
 
 set -euo pipefail
 
-roachprod destroy local || true
-roachprod create -n 3 local
-roachprod put local ./cockroach
+# For real cluster: roachprod create -n 7 $USER-snaps and fill
+# in the name here
+clus=local
 
-roachprod start local:1
+# roachprod create -n 7 local
+# roachprod wipe $clus
+roachprod stop $clus:1-7 || true
+roachprod put $clus ./cockroach-repro ./cockroach
 
-./cockroach workload init kv
+roachprod start $clus:1-7
 
-# Wait for initial splits
-sleep 20
+# Wait for >=3x replication for all replicas
+# NB: this takes minutes, don't be alarmed.
+# Probably worth reusing the cluster (which is
+# why the `wipe` above is commented out).
+time roachprod sql $clus:1 -- -e "
+select crdb_internal.force_retry('3100ms') from crdb_internal.ranges_no_leases where array_length(replicas, 1) < 3;
+"
 
-./cockroach workload run kv --min-block-bytes 1024 --max-block-bytes 1024 --batch 1024 --max-ops 100 --sequential --read-percent 0
+# NB: this should be more like 1000 on a real cluster.
+roachprod run $clus:1 -- ./cockroach workload fixtures import tpcc --warehouses=10 --checks=false.
 
-# Should print 45
-./cockroach sql --insecure -e "select range_id from crdb_internal.ranges_no_leases where table_name = 'kv';"
+# Start moving replicas off n[12] to n[34567].
+# --wait makes sure we don't permanently remove the nodes & don't block here.
+roachprod run $clus:1 ./cockroach node decommission --insecure --wait=none 1 2
 
-roachprod start local:2-3
-
-(cat ~/local/*/logs/cockroach.log && tail -F ~/local/*/logs/cockroach.log) | grep --line-buffered -E 'XXX|YYY' | tee log.txt | grep -F r45/
+# Can run this to have replicas move back to n[12]:
+# Can then decommission other nodes, etc. This is to be run manually.
+# roachprod run $clus:1 -- ./cockroach node recommission --insecure 1 2
 
