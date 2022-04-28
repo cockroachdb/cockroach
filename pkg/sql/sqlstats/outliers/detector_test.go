4514ac6be46a0139a6b331b5b09cb705ad08a974
Matthew Todd <todd@cockroachlabs.com>
2022-04-28 12:52:29 -0400

    outliers: introduce a composite "any" detector
    This gives us a place to hang the upcoming histogram-based detector
    coming in #79451.
    
    Release note: None

--- pkg/sql/sqlstats/outliers/detector_test.go
+++ pkg/sql/sqlstats/outliers/detector_test.go
@@ -19,6 +19,50 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestAnyDetector(t *testing.T) {
+	t.Run("enabled is false without any detectors", func(t *testing.T) {
+		detector := &anyDetector{}
+		require.False(t, detector.enabled())
+	})
+
+	t.Run("enabled is false with all disabled detectors", func(t *testing.T) {
+		detector := &anyDetector{[]detector{&fakeDetector{}, &fakeDetector{}}}
+		require.False(t, detector.enabled())
+	})
+
+	t.Run("enabled is true with at least one enabled detector", func(t *testing.T) {
+		detector := &anyDetector{[]detector{&fakeDetector{stubEnabled: true}, &fakeDetector{}}}
+		require.True(t, detector.enabled())
+	})
+
+	t.Run("isOutlier is false without any detectors", func(t *testing.T) {
+		detector := &anyDetector{}
+		require.False(t, detector.isOutlier(&Outlier_Statement{}))
+	})
+
+	t.Run("isOutlier is false without any concerned detectors", func(t *testing.T) {
+		detector := &anyDetector{[]detector{&fakeDetector{}, &fakeDetector{}}}
+		require.False(t, detector.isOutlier(&Outlier_Statement{}))
+	})
+
+	t.Run("isOutlier is true with at least one concerned detector", func(t *testing.T) {
+		detector := &anyDetector{[]detector{&fakeDetector{stubIsOutlier: true}, &fakeDetector{}}}
+		require.True(t, detector.isOutlier(&Outlier_Statement{}))
+	})
+
+	t.Run("isOutlier consults all detectors without short-circuiting", func(t *testing.T) {
+		// Detector implementations may wish to observe all statements, to
+		// build up their baseline sense of what "usual" is. To short-circuit
+		// would deny them that chance.
+		d1 := &fakeDetector{stubIsOutlier: true}
+		d2 := &fakeDetector{stubIsOutlier: true}
+		detector := &anyDetector{[]detector{d1, d2}}
+		detector.isOutlier(&Outlier_Statement{})
+		require.True(t, d1.isOutlierCalled, "the first detector should be consulted")
+		require.True(t, d2.isOutlierCalled, "the second detector should be consulted")
+	})
+}
+
 func TestLatencyThresholdDetector(t *testing.T) {
 	t.Run("enabled false by default", func(t *testing.T) {
 		detector := latencyThresholdDetector{st: cluster.MakeTestingClusterSettings()}
@@ -51,3 +95,20 @@ func TestLatencyThresholdDetector(t *testing.T) {
 		require.True(t, detector.isOutlier(&Outlier_Statement{LatencyInSeconds: 1}))
 	})
 }
+
+type fakeDetector struct {
+	stubEnabled     bool
+	stubIsOutlier   bool
+	isOutlierCalled bool
+}
+
+func (f fakeDetector) enabled() bool {
+	return f.stubEnabled
+}
+
+func (f *fakeDetector) isOutlier(_ *Outlier_Statement) bool {
+	f.isOutlierCalled = true
+	return f.stubIsOutlier
+}
+
+var _ detector = &fakeDetector{}
