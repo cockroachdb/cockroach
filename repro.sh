f8353e4983fcbeed2f201a8a271ec13b0c64e6ea
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-04-28 14:56:10 +0100

    Check for 100mb range
    Looks like the chunky part doesn't go to L0.
    
    ```
    W220428 13:55:54.088449 603 kv/kvserver/pkg/kv/kvserver/replica_raftstorage.go:952 ⋮ [n3,s3,r45/2:{-}] 65  XXX ingestion into L0: {Bytes:129007245 ApproxIngestedIntoL0Bytes:6311}
    W220428 13:55:59.208826 200 kv/kvserver/pkg/kv/kvserver/replica_raftstorage.go:952 ⋮ [n2,s2,r45/3:{-}] 67  XXX ingestion into L0: {Bytes:129007393 ApproxIngestedIntoL0Bytes:6459}
    ```
    
    Release note: None

--- repro.sh
+++ repro.sh
@@ -6,7 +6,19 @@ roachprod destroy local || true
 roachprod create -n 3 local
 roachprod put local ./cockroach
 
-roachprod start local
+roachprod start local:1
 
-(cat ~/local/*/logs/cockroach.log && tail -F ~/local/*/logs/cockroach.log) | grep --line-buffered -E 'XXX|YYY' | tee log.txt
+./cockroach workload init kv
+
+# Wait for initial splits
+sleep 20
+
+./cockroach workload run kv --min-block-bytes 1024 --max-block-bytes 1024 --batch 1024 --max-ops 100 --sequential --read-percent 0
+
+# Should print 45
+./cockroach sql --insecure -e "select range_id from crdb_internal.ranges_no_leases where table_name = 'kv';"
+
+roachprod start local:2-3
+
+(cat ~/local/*/logs/cockroach.log && tail -F ~/local/*/logs/cockroach.log) | grep --line-buffered -E 'XXX|YYY' | tee log.txt | grep -F r45/
 
