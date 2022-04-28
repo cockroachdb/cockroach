a6cc1c4453916cc161b5d26067f846bc340e2770
Gerardo Torres <gerardo.torrescastro@cockroachlabs.com>
2022-04-28 11:36:31 -0400

    server, sql: collect additional session details
    Partially addresses #74257.
    
    Previously, the sql server did not provide additional session details
    such as total number of transactions executed, transaction fingerprint
    IDs, and total active time. This change adds the aforementioned session
    details to the `serverpb.Session` struct.
    
    The transaction fingerprint IDs are collected from the
    `ApplicationStats` for each connExecutor. The total number of
    transactions executed is accumulated using the `txnCounter` from the
    `extraTxnState` in `connExecutor`. The total active time is calculated by a
    `timeutil.StopWatch` which is started and stopped when a transaction is
    started, restarted, and finished.
    
    Release note: none

--- pkg/sql/delegate/show_sessions.go
+++ pkg/sql/delegate/show_sessions.go
@@ -16,7 +16,7 @@ import (
 )
 
 func (d *delegator) delegateShowSessions(n *tree.ShowSessions) (tree.Statement, error) {
-	const query = `SELECT node_id, session_id, status, user_name, client_address, application_name, active_queries, last_active_query, session_start, oldest_query_start FROM crdb_internal.`
+	const query = `SELECT node_id, session_id, status, user_name, client_address, application_name, active_queries, last_active_query, num_txns_executed, session_start, oldest_query_start FROM crdb_internal.`
 	table := `node_sessions`
 	if n.Cluster {
 		table = `cluster_sessions`
