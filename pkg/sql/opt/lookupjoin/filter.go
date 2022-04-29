101463921ee995549a5147db70da0bed0a512a64
Marcus Gartner <marcus@cockroachlabs.com>
2022-04-29 15:58:32 -0400

    opt: consolidate duplicate findJoinFilterConstants methods
    This commit consolidates duplicate methods
    `CustomFuncs.findJoinFilterConstants` and
    `ConstaintBuilder.findJoinFilterConstants` into the function
    `lookupjoin.FindJoinFilterConstants`.
    
    Also, a couple additional tests related to this function have been
    added.
    
    Release note: None

---
+++ pkg/sql/opt/lookupjoin/filter.go
@@ -0,0 +1,55 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package lookupjoin
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/sql/opt"
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/memo"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/eval"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+)
+
+// FindJoinFilterConstants tries to find a filter that is exactly equivalent to
+// constraining the given column to a constant value or a set of constant
+// values. If successful, the constant values and the index of the constraining
+// FiltersItem are returned. If multiple filters match, the one that minimizes
+// the number of returned values is chosen. Note that the returned constant
+// values do not contain NULL.
+func FindJoinFilterConstants(
+	filters memo.FiltersExpr, col opt.ColumnID, evalCtx *eval.Context,
+) (values tree.Datums, filterIdx int, ok bool) {
+	var bestValues tree.Datums
+	var bestFilterIdx int
+	for filterIdx := range filters {
+		props := filters[filterIdx].ScalarProps()
+		if props.TightConstraints {
+			constCol, constVals, ok := props.Constraints.HasSingleColumnConstValues(evalCtx)
+			if !ok || constCol != col {
+				continue
+			}
+			hasNull := false
+			for i := range constVals {
+				if constVals[i] == tree.DNull {
+					hasNull = true
+					break
+				}
+			}
+			if !hasNull && (bestValues == nil || len(bestValues) > len(constVals)) {
+				bestValues = constVals
+				bestFilterIdx = filterIdx
+			}
+		}
+	}
+	if bestValues == nil {
+		return nil, -1, false
+	}
+	return bestValues, bestFilterIdx, true
+}
