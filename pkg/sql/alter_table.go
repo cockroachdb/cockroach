de9193fe94194c814de1bce9f86c028d5b0358d7
Oliver Tan <otan@cockroachlabs.com>
2022-04-29 15:11:41 +1000

    tree: move HoistConstraints telemetry reference
    Release note: none

--- pkg/sql/alter_table.go
+++ pkg/sql/alter_table.go
@@ -98,7 +98,9 @@ func (p *planner) AlterTable(ctx context.Context, n *tree.AlterTable) (planNode,
 			tree.Name(tableDesc.GetName()), tree.Name(tableDesc.GetName()))
 	}
 
-	n.HoistAddColumnConstraints()
+	n.HoistAddColumnConstraints(func() {
+		telemetry.Inc(sqltelemetry.SchemaChangeAlterCounterWithExtra("table", "add_column.references"))
+	})
 
 	// See if there's any "inject statistics" in the query and type check the
 	// expressions.
