560fd41d5476fe51794aa3afd675bdd1c6c6f6b8
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-05-02 13:02:52 +0200

    admission: improve granter logs
    To the uninitiated, they were previously difficult to understand.
    Now it's clearer what each number means just from looking at the
    output.
    
    While rebasing this PR on top of #80480, I realized how hard it
    was (at least for me) to keep track of the many quantities involved. The
    way the updates to the state were computed (were the old struct is
    updated incrementally in-place) additionally made me worried
    that bugs could easily be introduced. Some quantities are cumulative,
    some are for the current interval but contain other quantities, etc - it
    all melted my brain with ease.
    
    I decided to at least try to make it easier for the next newcomer to
    this codebase to get their bearings by constructing all of the updates
    first, and then overwriting the old state. I additionally renamed all
    of the variables according to a fixed pattern so that it is now clear
    which ones are for the current interval or cumulative, and added
    commentary to (hopefully) clarify which quantities contain each other.
    Additionally, all quantities are now stored for posterity, so in
    principle it's now straightforward to write a test case on any of the
    intermediate quantities for any desired inputs.
    
    More could be done here but I feel that this leaves things in a good
    place. There's a chance that I have written equally intricate code but
    happen to like it more because I wrote it. I trust my reviewers to keep
    me honest.
    
    Release note: None

--- pkg/util/admission/work_queue.go
+++ pkg/util/admission/work_queue.go
@@ -1682,7 +1682,7 @@ func (q *StoreWorkQueue) AdmittedWorkDone(h StoreWorkHandle, ingestedIntoL0Bytes
 			q.mu.stats.admittedBytes += uint64(h.writeBytes)
 			if h.ingestRequest {
 				q.mu.stats.ingestedBytes += uint64(h.writeBytes)
-				q.mu.stats.ingestedIntoL0Bytes += uint64(ingestedIntoL0Bytes)
+				q.mu.stats.ingestedAccountedL0Bytes += uint64(ingestedIntoL0Bytes)
 			}
 		}
 		q.mu.Unlock()
