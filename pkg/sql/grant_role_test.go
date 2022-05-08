b991592e8a1b6e82c5c2465273b8743d40a7d616
Xiang Gu <xiang@cockroachlabs.com>
2022-05-07 20:02:30 -0400

    sql: No-op GRANT/ROVOKE/ALTER ROLE that are no-ops
    Previously, we schedule a schema change for every 'GRANT', 'REVOKE',
    or 'ALTER ROLE' statements even if they are no-ops, that is, if they
    don't actually change any
      1. privileges (via 'GRANT/REVOKE privileges ...'), or
      2. role memberships (via `GRANT/REVOKE role ...), or
      3. role options (via `ALTER ROLE ... WITH ...`), or
      4. default session vars values(via `ALTER ROLE ... SET/RESET ...`)
    
    Performing a schema change on such no-op statements will invalidate
    caches and hence incur overhead. This PR fixes this by detecting those
    no-ops and make them real no-op (i.e. no schema change will be
    performed) and consequently avoid unnecessary cache invalidation.
    
    Release note: None

---
+++ pkg/sql/grant_role_test.go
@@ -0,0 +1,57 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package sql_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/desctestutils"
+	"github.com/cockroachdb/cockroach/pkg/sql/tests"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/stretchr/testify/require"
+)
+
+// TestNoOpGrantRole tests that if we GRANT membership of a role to a user who
+// is already a member of that role will not cause any schema change to the
+// `system.role_members` table.
+func TestNoOpGrantRole(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	params, _ := tests.CreateTestServerParams()
+	s, sqlDB, kvDB := serverutils.StartServer(t, params)
+	defer s.Stopper().Stop(context.Background())
+	tdb := sqlutils.MakeSQLRunner(sqlDB)
+
+	tdb.Exec(t, "CREATE USER developer WITH CREATEDB")
+	tdb.Exec(t, "CREATE USER roach WITH PASSWORD NULL")
+
+	// Grant membership of role `developer` to user `roach`.
+	// Assert that user `roach` is now a member of role `developer` by peeking the `system.role_members` table.
+	tdb.Exec(t, "GRANT developer TO roach")
+	num := 0
+	tdb.QueryRow(t, "SELECT count(1) FROM system.role_members WHERE role = 'developer' AND member = 'roach'").Scan(&num)
+	require.Equal(t, 1, num)
+	roleMembersTableDesc := desctestutils.TestingGetTableDescriptor(kvDB, keys.SystemSQLCodec, "system", "public", "role_members")
+	roleMembersTableVersion := roleMembersTableDesc.GetVersion()
+
+	// Repeat the statement and assert that no schema change was performed to table `system.role_members`
+	// by checking the table's version remains the same.
+	tdb.Exec(t, "GRANT developer TO roach")
+	tdb.QueryRow(t, "SELECT count(1) FROM system.role_members WHERE role = 'developer' AND member = 'roach'").Scan(&num)
+	require.Equal(t, 1, num)
+	roleMembersTableDesc = desctestutils.TestingGetTableDescriptor(kvDB, keys.SystemSQLCodec, "system", "public", "role_members")
+	require.Equal(t, roleMembersTableVersion, roleMembersTableDesc.GetVersion())
+}
