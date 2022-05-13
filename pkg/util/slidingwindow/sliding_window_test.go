79597b402d696be3cddc751ddbee2f38ccb90f88
Austen McClernon <austen@cockroachlabs.com>
2022-05-13 22:17:49 +0000

    util: add sliding window aggregator
    This patch introduces a sliding window aggregator (swag) as a utility
    data structure.
    
    Swag represents a sliding window aggregator over a binary operation.
    The aggregator will aggregate recorded values in two ways:
    (1) Within each window, the binary operation is applied when recording a new
        value into the current window.
    (2) On Query, the binary operation is accumulated over every window, from
        most to least recent.
    The binary operator function must therefore be:
        associative :: binOp(binOp(a,b), c) = binOp(a,binOp(b,c))
    In order to have correct results. Note that this does not allow for a more
    general class of aggregators that are associative, such as geometric mean,
    bloom filters etc. These require special treatment with user defined
    functions for lift(e), lower(a) and combine(v1,v2)
    (https://dl.acm.org/doi/pdf/10.1145/3093742.3093925).
    
    query: O(k), append: O(k), space O(k), k = |windows|.
    The average case append is O(1), when no windows
    require rotating; the size requirement is 32 + 8k bytes.
    
    Release note: None

---
+++ pkg/util/slidingwindow/sliding_window_test.go
@@ -0,0 +1,120 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package slidingwindow
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+// TestSlidingWindow asserts that the sliding windows correctly rotate old
+// windows; report the correct aggregate query and duration.
+func TestSlidingWindow(t *testing.T) {
+	startTime := time.Date(1, 2, 3, 4, 5, 6, 7, time.UTC)
+	interval := time.Second * 1
+
+	max := func(a, b float64) float64 {
+		if a > b {
+			return a
+		}
+		return b
+	}
+	sum := func(a, b float64) float64 {
+		return a + b
+	}
+
+	testCases := []struct {
+		desc             string
+		recordedTicks    [][]float64
+		binOp            func(a, b float64) float64
+		expectedWindows  []float64
+		expectedAgg      float64
+		expectedDuration time.Duration
+	}{
+		{
+			desc:             "every window, no overlap max",
+			recordedTicks:    [][]float64{{1}, {2}, {3}, {4}},
+			binOp:            max,
+			expectedWindows:  []float64{4, 3, 2, 1},
+			expectedAgg:      4,
+			expectedDuration: interval * 4,
+		},
+		{
+			desc:             "every window, no overlap sum",
+			recordedTicks:    [][]float64{{1}, {2}, {3}, {4}},
+			binOp:            sum,
+			expectedWindows:  []float64{4, 3, 2, 1},
+			expectedAgg:      10,
+			expectedDuration: interval * 4,
+		},
+		{
+			desc:             "partial windows, overlap max",
+			recordedTicks:    [][]float64{{}, {1}, {}, {2}, {}, {3}, {}, {4}},
+			binOp:            max,
+			expectedWindows:  []float64{4, 0, 3, 0},
+			expectedAgg:      4,
+			expectedDuration: interval * 4,
+		},
+		{
+			desc:             "partial windows, overlap sum",
+			recordedTicks:    [][]float64{{}, {1}, {}, {2}, {}, {3}, {}, {4}},
+			binOp:            sum,
+			expectedWindows:  []float64{4, 0, 3, 0},
+			expectedAgg:      7,
+			expectedDuration: interval * 4,
+		},
+		{
+			desc:             "empty windows",
+			recordedTicks:    [][]float64{{}, {}, {}, {}, {}, {}, {}, {}},
+			binOp:            sum,
+			expectedWindows:  []float64{0, 0, 0, 0},
+			expectedAgg:      0,
+			expectedDuration: interval * 4,
+		},
+		{
+			desc:          "2 windows, max",
+			recordedTicks: [][]float64{{100, 200, 300}, {200, 400, 600}},
+			binOp:         max,
+			// We increment the query time by a second below, so we expect the
+			// duration to be rounded up to 3.
+			expectedWindows:  []float64{600, 300, 0},
+			expectedAgg:      600,
+			expectedDuration: interval * 3,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			swag := NewSwag(startTime, interval, 4, tc.binOp)
+
+			curTime := startTime
+			for _, records := range tc.recordedTicks {
+				curTime = curTime.Add(swag.rotateInterval)
+				for _, record := range records {
+					swag.Record(curTime, record)
+				}
+			}
+
+			// Add another interval to the query time, just shy of the rotate
+			// interval - to round up the duration returned from query and
+			// avoid nasty decimals.
+			curTime = curTime.Add(swag.rotateInterval - time.Microsecond)
+			agg, duration := swag.Query(curTime)
+
+			require.EqualValues(t, tc.expectedWindows, swag.Windows(curTime))
+			require.Equal(t, tc.expectedAgg, agg)
+			require.Equal(t, tc.expectedDuration, duration.Round(time.Second))
+		})
+	}
+
+}
