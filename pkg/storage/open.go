685e9cea1f2ad17c2a7b7c0e62950533f1227b86
Jackson Owens <jackson@cockroachlabs.com>
2022-05-17 13:40:54 -0400

    vendor: bump Pebble to e567fec84c6e
    ```
    e567fec8 db: ensure Open closes opened directories on error
    b8c9a560 internal/metamorphic: overwrite unused bounds buffers
    7c5f0cbb db: copy user-provided bounds and optimize unchanged bounds
    f8897076 *: Add IterOption to optionally read L6 filter blocks.
    d79f9617 vfs: extend disk-health checking to filesystem metadata operations
    5ae21746 db: remove newRangeKeyIter closure
    6d975489 db: add BenchmarkIteratorScan
    782d102e sstable: fix invariant check for sstable size estimation
    738a7f0b db: fix NewIter regression resulting in extra memtable levels
    37558663 *: Use keyspan.LevelIter for rangedels in compactions
    e6c60c71 db: use sublevel level iters for all compactions out of L0
    d8f63bef db: extend documentation on ingested file overlap; improve test cases
    b9e970a8 internal/keyspan: correct and document MergingIter key stability
    498177bb internal/keyspan: collapse fragmentBoundIterator into MergingIter
    ```
    
    Release note (bug fix): Fix a gap in disk-stall detection. Previously,
    disk stalls during filesystem metadata operations could go undetected,
    inducing deadlocks.  Now stalls during these types of operations will
    correctly fatal the process.

--- pkg/storage/open.go
+++ pkg/storage/open.go
@@ -164,11 +164,7 @@ type Location struct {
 func Filesystem(dir string) Location {
 	return Location{
 		dir: dir,
-		// fs is left nil intentionally, so that it will be left as the
-		// default of vfs.Default wrapped in vfs.WithDiskHealthChecks
-		// (initialized by DefaultPebbleOptions).
-		// TODO(jackson): Refactor to make it harder to accidentally remove
-		// disk health checks by setting your own VFS in a call to NewPebble.
+		fs:  vfs.Default,
 	}
 }
 
@@ -196,9 +192,7 @@ func Open(ctx context.Context, loc Location, opts ...ConfigOption) (*Pebble, err
 	var cfg engineConfig
 	cfg.Dir = loc.dir
 	cfg.Opts = DefaultPebbleOptions()
-	if loc.fs != nil {
-		cfg.Opts.FS = loc.fs
-	}
+	cfg.Opts.FS = loc.fs
 	for _, opt := range opts {
 		if err := opt(&cfg); err != nil {
 			return nil, err
