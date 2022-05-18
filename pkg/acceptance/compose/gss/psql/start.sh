b065031b749e92ea2d96e35efbf09234f0593395
Ricky Stewart <ricky@cockroachlabs.com>
2022-05-18 11:46:06 -0500

    release-21.1: acceptance: run `python`, `psql` containers as current uid
    Manual cherry-pick from #81460.
    
    `postgres`'s permission checking for certificates has gotten more
    rigorous since [this commit](https://git.postgresql.org/gitweb/?p=postgresql.git;a=commitdiff;h=a59c79564bdc209a5bc7b02d706f0d7352eb82fa).
    This has broken a couple `acceptance` tests which do not pin to any
    specific `postgres` version (see #81313, #81437).
    
    Here we attempt to solve the problem "once and for all" by ensuring that
    these containers run with a UID that is equal to the one that created
    the certificates.
    
    Release note: None

--- pkg/acceptance/compose/gss/psql/start.sh
+++ pkg/acceptance/compose/gss/psql/start.sh
@@ -4,4 +4,11 @@ set -e
 
 echo psql | kinit tester@MY.EX
 
+echo "Preparing SQL user ahead of test"
+env \
+    PGSSLKEY=/certs/client.root.key \
+    PGSSLCERT=/certs/client.root.crt \
+    psql -U root -c "ALTER USER root WITH PASSWORD rootpw"
+
+echo "Running test"
 ./gss.test
