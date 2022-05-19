5abe980b098ebb82103ee1676d6f1c63b446057f
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2022-05-18 21:55:18 -0400

    ui: Jest + TSConfig Complete
    Adjusted Jest config for DB Console
    -----------------------------------
    To account for the many types of assets in the DB Console application,
    added a `moduleNameMapper` block with entries for image files and css.
    These entries were taken and modified from the Cluster UI jest config to
    build on an already working solution. To account for the very
    specialized way that DB Console overlays paths for OSS and CCL builds
    (see "CCL Build" section of /pkg/ui/ README), I included the `paths`
    section of the `tsconfig.json` via a `pathsToModuleNameMapper` utility
    from `ts-jest`. To further accomodate the CCL source paths, this path
    was included in the `roots` property.
    
    Since `enzyme` is currently used for DOM testing, I set the
    `testEnvironment` to "enzyme" as well as included some enzyme related
    files to `setupFilesAfterEnv`.
    
    Jest does not use Webpack to build the app before running tests and so
    files need to be "transformed" before running on Node.js. The `transform`
    section of the config specifies which transformer should be used for
    which files. All .ts and .tsx files are transformed with `ts-jest` (this
    is the bulk of the app), and .js files are transformed with
    `babel-jest`. This is specifically for proto files in `src/js/protos`
    which are generated, but use `import` statements which are not supported
    by Node.js[1] in the same way we using them via babel.
    
    Adjusted TS Config for DB Console
    ---------------------------------
    I changed the `jsx` property from "preserve" to "react". Perhaps
    something else in our webpack config is transforming JSX besides the
    Typescript compiler, but for Jest test Node.js can't execute jsx. This
    property will have the Typescript compiler convert jsx syntax to
    `React.createElement` calls.
    
    Beyond some adjustments to the `paths` entry, the other addition here is
    setting `esModuleInterop` to "true". This allows namespace imports
    (example: `import * as React from "react"`) to be correctly transformed
    into CommonJS (import react = require("react"))
    
    Renaming .babelrc to babel.config.js
    ------------------------------------
    I thought at first that .babelrc had been deprecated in Babel version 7,
    but it turns out that Babel supports both formats, but treats them
    different in terms of priority when merging configurations[2]. It seems
    the convention is to use `babel.config.js` for project wide
    configuration and `.babelrc` for specific overrides for individual
    modules. I again took the babel.config.js from Cluster UI as a starting
    place. Besides needing some of the included plugins, Jest specifically
    needs the `@babel/plugin-transform-modules-commonjs`
    
    [1]: Node.js supports ECMAScript modules since version 14, but you must
    specify `"type": "module"` in the local package.json or use `*.mjs`
    extensions. Neither of these are practical when transpiling. By default
    Node.js still uses CommonJS modules.
    [2]: see https://babeljs.io/docs/en/configuration#print-effective-configs
    
    Release note: None

---
+++ pkg/ui/workspaces/db-console/babel.config.js
@@ -0,0 +1,35 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+/* globals module */
+
+const presets = [
+  [
+    "@babel/env",
+    {
+      modules: "commonjs",
+    },
+  ],
+  "@babel/react",
+];
+
+const plugins = [
+  "@babel/proposal-class-properties",
+  "@babel/proposal-object-rest-spread",
+  // @babel/plugin-transform-runtime is required to support dynamic loading of cluster-ui package
+  "@babel/plugin-transform-runtime",
+  ["import", { libraryName: "antd", style: true }],
+];
+
+const env = {
+  test: {
+    plugins: ["@babel/plugin-transform-modules-commonjs"],
+  },
+};
+module.exports = { presets, plugins, env };
