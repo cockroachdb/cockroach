01e5ea043dc87c4833bb465c17d71fedf708fa44
Renato Costa <renato@cockroachlabs.com>
2022-05-20 10:37:16 -0400

    roachtest: allow TC_BUILDTYPE_ID to be accessible by Docker
    In #81103, the process of generating TeamCity links in test failure
    reports started relying on the `TC_BUILDTYPE_ID` environment
    variable. While that variable was added to TeamCity builds, it was not
    being passed down to Docker where the tests actually run. As a result,
    links generated by the GitHub poster were broken (see, for example, #81572).
    
    This commit makes `TC_BUILDTYPE_ID` accessible by Docker for every
    build that was already passing `TC_BUILD_BRANCH`. This should be
    sufficient to cover all existing cases and more, in case having
    access to this variable becomes useful in the future.
    
    Release note: None

--- build/teamcity/cockroach/nightlies/random_syntax_tests.sh
+++ build/teamcity/cockroach/nightlies/random_syntax_tests.sh
@@ -8,6 +8,6 @@ source "$dir/teamcity-support.sh"  # For $root
 source "$dir/teamcity-bazel-support.sh"  # For run_bazel
 
 tc_start_block "Run random syntax tests"
-BAZEL_SUPPORT_EXTRA_DOCKER_ARGS="-e BUILD_VCS_NUMBER -e GITHUB_API_TOKEN -e GITHUB_REPO -e TC_BUILD_BRANCH -e TC_BUILD_ID -e TC_SERVER_URL" \
+BAZEL_SUPPORT_EXTRA_DOCKER_ARGS="-e BUILD_VCS_NUMBER -e GITHUB_API_TOKEN -e GITHUB_REPO -e TC_BUILDTYPE_ID -e TC_BUILD_BRANCH -e TC_BUILD_ID -e TC_SERVER_URL" \
                                run_bazel build/teamcity/cockroach/nightlies/random_syntax_tests_impl.sh
 tc_end_block "Run random syntax tests"
--- build/teamcity/cockroach/nightlies/sqllogic_hi_vmodule_nightly.sh
+++ build/teamcity/cockroach/nightlies/sqllogic_hi_vmodule_nightly.sh
@@ -8,6 +8,6 @@ source "$dir/teamcity-support.sh"  # For $root
 source "$dir/teamcity-bazel-support.sh"  # For run_bazel
 
 tc_start_block "Run SQL Logic Test High VModule"
-BAZEL_SUPPORT_EXTRA_DOCKER_ARGS="-e TC_BUILD_BRANCH -e GITHUB_API_TOKEN -e BUILD_VCS_NUMBER -e TC_BUILD_ID -e TC_SERVER_URL" \
+BAZEL_SUPPORT_EXTRA_DOCKER_ARGS="-e TC_BUILDTYPE_ID -e TC_BUILD_BRANCH -e GITHUB_API_TOKEN -e BUILD_VCS_NUMBER -e TC_BUILD_ID -e TC_SERVER_URL" \
   run_bazel build/teamcity/cockroach/nightlies/sqllogic_hi_vmodule_nightly_impl.sh
 tc_end_block "Run SQL Logic Test High VModule"
