df3f101b81a960644c3fc0deb09beeb1484261e2
richardjcai <caioftherichard@gmail.com>
2022-05-24 14:58:48 -0400

    sql: add new system.privileges table
    First step in upcoming privileges work.
    Explained in RFC: https://github.com/cockroachdb/cockroach/pull/80580
    
    Release note: None

--- pkg/clusterversion/cockroach_versions.go
+++ pkg/clusterversion/cockroach_versions.go
@@ -365,6 +365,8 @@ const (
 	// version is guaranteed to reside in a cluster where all nodes support range
 	// keys at the Pebble layer.
 	EnablePebbleFormatVersionRangeKeys
+	// SystemPrivilegesTable adds system.privileges table.
+	SystemPrivilegesTable
 
 	// *************************************************
 	// Step (1): Add new versions here.
@@ -637,6 +639,10 @@ var versionsSingleton = keyedVersions{
 	{
 		Key:     EnablePebbleFormatVersionRangeKeys,
 		Version: roachpb.Version{Major: 22, Minor: 1, Internal: 8},
+	},
+	{
+		Key:     SystemPrivilegesTable,
+		Version: roachpb.Version{Major: 22, Minor: 1, Internal: 10},
 	},
 
 	// *************************************************
