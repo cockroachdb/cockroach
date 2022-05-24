4d0e56406cc763175f335feef2f01d6ff915a2ac
Steven Danna <danna@cockroachlabs.com>
2022-05-24 16:40:41 +0100

    backupccl: reduce allocations in spansForAllTableIndexes
    The table descriptor builder API copies the descriptor and does a
    number of allocations so that it can answer any question one might
    have about the table.
    
    However, in this function, we only care about collecting the active
    indexes and constructing spans for them.  Doing this manually is
    straightforward and reduces the number of allocations considerably
    when processing a large number of revisions.
    
    ```
    BEFORE: BenchmarkSpansForAllTableIndexes/revisionCount=15000-16     16  62820672 ns/op  63661512 B/op     240618 allocs/op
    AFTER:  BenchmarkSpansForAllTableIndexes/revisionCount=15000-16     49  22688142 ns/op   7289864 B/op      75030 allocs/op
    ```
    
    This brings the benchmark case for a large number of revisions into
    line with the case of a single table that just happens to have a large
    number of indexes.
    
    Release note: None

--- pkg/ccl/backupccl/backup_test.go
+++ pkg/ccl/backupccl/backup_test.go
@@ -6332,8 +6332,7 @@ func TestPublicIndexTableSpans(t *testing.T) {
 		tableDesc := getMockTableDesc(test.tableID, test.pkIndex,
 			test.indexes, test.addingIndexes, test.droppingIndexes)
 		t.Run(fmt.Sprintf("%s:%s", "getPublicIndexTableSpans", test.name), func(t *testing.T) {
-			spans, err := getPublicIndexTableSpans(tableDesc, unusedMap, codec)
-			require.NoError(t, err)
+			spans := getPublicIndexTableSpans(tableDesc.TableDesc(), unusedMap, codec)
 			var unmergedSpans []string
 			for _, span := range spans {
 				unmergedSpans = append(unmergedSpans, span.String())
