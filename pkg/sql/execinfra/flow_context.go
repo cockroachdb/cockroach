0a3137c2d7de89a1e6b242c308e2afe2c54735f5
Andrew Werner <awerner32@gmail.com>
2022-05-25 15:54:28 -0400

    sql/sem/builtins: remove dependency of builtins on sql/catalog/descs
    builtins is a major bottleneck in the build graph. Reducing its deps helps
    move it to be earlier. This removes effectively all dependencies on the
    subtree under catalog. The introduction of the new package was required to
    eliminate the very upsetting method to return a descriptor as an opaque
    interface.
    
    Release note: None

--- pkg/sql/execinfra/flow_context.go
+++ pkg/sql/execinfra/flow_context.go
@@ -20,6 +20,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descs"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/tabledesc"
+	"github.com/cockroachdb/cockroach/pkg/sql/evalcatalog"
 	"github.com/cockroachdb/cockroach/pkg/sql/execinfrapb"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/eval"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
@@ -62,6 +63,11 @@ type FlowCtx struct {
 	// In the latter case, after the flow is complete, all descriptors leased from
 	// this object must be released.
 	Descriptors *descs.Collection
+
+	// EvalCatalogBuiltins is initialized if the flow context is remote and the
+	// above descs.Collection is non-nil. It is referenced in the eval.Context
+	// in order to provide catalog access to builtins.
+	EvalCatalogBuiltins evalcatalog.Builtins
 
 	// IsDescriptorsCleanupRequired is set if Descriptors needs to release the
 	// leases it acquired after the flow is complete.
