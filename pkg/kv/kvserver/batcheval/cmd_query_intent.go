1d25919baa4e7a43ad2ba75ddf8318abc1ab460d
Andrew Baptist <baptist@cockroachlabs.com>
2022-05-25 11:56:31 -0400

    kv: Only scan intents span for QueryIntent request
    Change QueryIntent to only read from the lock table.
    Previously the request required merging the read from the MVCC
    iterator with the lock table. This should improve performance.
    
    Also add unit testing for QueryIntent
    
    Fixes #69716
    
    Release note: None

--- pkg/kv/kvserver/batcheval/cmd_query_intent.go
+++ pkg/kv/kvserver/batcheval/cmd_query_intent.go
@@ -14,12 +14,14 @@ import (
 	"context"
 	"time"
 
+	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/batcheval/result"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/spanset"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/storage"
-	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/storage/enginepb"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
 	"github.com/cockroachdb/errors"
 )
 
@@ -37,6 +39,8 @@ func declareKeysQueryIntent(
 	// QueryIntent requests read the specified keys at the maximum timestamp in
 	// order to read any intent present, if one exists, regardless of the
 	// timestamp it was written at.
+	// TODO: Not sure this is still correct - this is the whole span, but does it
+	// include intents? What timestamp should it use?
 	latchSpans.AddNonMVCC(spanset.SpanReadOnly, req.Header().Span())
 }
 
@@ -74,24 +78,55 @@ func QueryIntent(
 			h.Timestamp, args.Txn.WriteTimestamp)
 	}
 
-	// Read at the specified key at the maximum timestamp. This ensures that we
-	// see an intent if one exists, regardless of what timestamp it is written
-	// at.
-	_, intent, err := storage.MVCCGet(ctx, reader, args.Key, hlc.MaxTimestamp, storage.MVCCGetOptions{
-		// Perform an inconsistent read so that intents are returned instead of
-		// causing WriteIntentErrors.
-		Inconsistent: true,
-		// Even if the request header contains a txn, perform the engine lookup
-		// without a transaction so that intents for a matching transaction are
-		// not returned as values (i.e. we don't want to see our own writes).
-		Txn: nil,
-	})
+	// Read from the lock table to see if an intent exists.
+	// Iterate over the lock key space with this key as a lower bound and prefix
+	// set to true there should only be a single result that comes back from the
+	// iteration
+	lbKey, _ := keys.LockTableSingleKey(args.Key, nil)
+	iter := reader.NewEngineIterator(storage.IterOptions{Prefix: true, LowerBound: lbKey})
+	// FIXME: what is the alternate approach in Go to creating an array of size 1
+	var intent roachpb.Intent
+	valid, err := iter.SeekEngineKeyGE(storage.EngineKey{Key: lbKey})
 	if err != nil {
 		return result.Result{}, err
 	}
+	if valid {
+		engineKey, err := iter.EngineKey()
+		if err != nil {
+			return result.Result{}, err
+		}
+		checkKey, err := keys.DecodeLockTableSingleKey(engineKey.Key)
+		if err != nil {
+			return result.Result{}, err
+		}
+		if !checkKey.Equal(args.Key) {
+			// should not happen since we did a prefix match
+			return result.Result{}, errors.AssertionFailedf("key does not match expected %v != %v", checkKey, args.Key)
+		}
+		var meta enginepb.MVCCMetadata
+		if err = protoutil.Unmarshal(iter.UnsafeValue(), &meta); err != nil {
+			return result.Result{}, err
+		}
+		intent = roachpb.MakeIntent(meta.Txn, checkKey)
+
+		hasNext, err := iter.NextEngineKey()
+		if err != nil {
+			// we expect false, but not an error
+			return result.Result{}, err
+		}
+		// should not get here - this was a prefix match on an intent, only 1 allowed
+		if hasNext {
+			engineKey, err := iter.EngineKey()
+			if err != nil {
+				return result.Result{}, err
+			}
+			return result.Result{}, errors.AssertionFailedf("unexpected additional key found %v while looking for %v", engineKey, args.Key)
+		}
+	}
 
 	var curIntentPushed bool
-	if intent != nil {
+
+	if valid {
 		// See comment on QueryIntentRequest.Txn for an explanation of this
 		// comparison.
 		// TODO(nvanbenschoten): Now that we have a full intent history,
@@ -103,12 +138,15 @@ func QueryIntent(
 
 		// If we found a matching intent, check whether the intent was pushed
 		// past its expected timestamp.
-		if reply.FoundIntent {
-			// If the request is querying an intent for its own transaction, forward
-			// the timestamp we compare against to the provisional commit timestamp
-			// in the batch header.
+		if !reply.FoundIntent {
+			log.Infof(ctx, "intent mismatch requires - %v == %v and %v == %v and %v <= %v",
+				args.Txn.ID, intent.Txn.ID, args.Txn.Epoch, intent.Txn.Epoch, args.Txn.Sequence, intent.Txn.Sequence)
+		} else {
 			cmpTS := args.Txn.WriteTimestamp
 			if ownTxn {
+				// If the request is querying an intent for its own transaction, forward
+				// the timestamp we compare against to the provisional commit timestamp
+				// in the batch header.
 				cmpTS.Forward(h.Txn.WriteTimestamp)
 			}
 			if cmpTS.Less(intent.Txn.WriteTimestamp) {
@@ -136,7 +174,7 @@ func QueryIntent(
 			// the txn use refresh spans more effectively.
 			return result.Result{}, roachpb.NewTransactionRetryError(roachpb.RETRY_SERIALIZABLE, "intent pushed")
 		}
-		return result.Result{}, roachpb.NewIntentMissingError(args.Key, intent)
+		return result.Result{}, roachpb.NewIntentMissingError(args.Key, &intent)
 	}
 	return result.Result{}, nil
 }
