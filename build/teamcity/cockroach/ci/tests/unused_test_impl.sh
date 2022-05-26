e3f8ee5b483f0bd7abe02bca114a196ebf070e4f
Ricky Stewart <ricky@cockroachlabs.com>
2022-05-26 09:53:33 -0500

    bazel: add unused lint
    1. Pull in `7336768b31d5af41cd46d5defb58560a93b3ba98` for `rules_go`
       which adds the `unused.out` file to the `.x` archive for each
       compiled library.
    2. Add a helper binary `build/bazelutil/unused_checker`. This binary
       takes a list of `.x` archives on the command-line, parses out the
       JSON in the `unused.out` file, and aggregates all this data producing
       a list of unused objects.
    3. Add a macro `get_x_data` that uses an aspect to enumerate all of the
       `.x` archives for each library/binary/etc.
    4. Update `pkg/cmd/generate-test-suites` to in addition to the list of
       `go_test` targets, produce the list of all `go_*` targets and
       enumerate all the `get_x_data` targets as well. Also have it use
       `buildozer` to create `get_x_data` targets wherever relevant.
    5. Add a macro `unused_test` that instantiates a `sh_binary` which when
       run invokes the `unused_checker` with the results from all
       `get_x_data` in the repo.
    
    Closes #76933.
    
    Release note: None

---
+++ build/teamcity/cockroach/ci/tests/unused_test_impl.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+# Get all the .x archives for everything in bazel-bin.
+bazel query 'kind("go_test|go_binary|go_transition_binary|go_transition_test|go_library", //pkg/...)' | \
+    xargs bazel build --config=ci --config=simplestamp --config=test
+
+bazel run //pkg:unused_checker --config=ci --config=simplestamp --config=test
