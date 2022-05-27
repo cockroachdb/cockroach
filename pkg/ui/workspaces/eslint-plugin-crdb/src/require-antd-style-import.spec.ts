34c2486030445c1629bb5cd341c1744f61ed7e00
Sean Barag <barag@cockroachlabs.com>
2022-05-27 22:29:07 +0000

    ui: add eslint-plugin-crdb package with custom eslint rules
    antd 3.x recommends the use of babel-plugin-import [1] to automatically
    add style imports for each component imported from the "antd" module,
    e.g.:
    
    ```
    import { Button } from "antd";
    ```
    
    becomes:
    
    ```
    import { Button } from "antd";
    import "antd/lib/button/style";
    ```
    
    In an environment in which Babel isn't available, this transformation
    can be applied statically and enforced with an eslint rule. Add a custom
    rule to ensure antd components always have their corresponding styles
    imported.
    
    [1] https://github.com/umijs/babel-plugin-import
    
    Release note: None

---
+++ pkg/ui/workspaces/eslint-plugin-crdb/src/require-antd-style-import.spec.ts
@@ -0,0 +1,322 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import { TSESLint } from "@typescript-eslint/utils";
+
+import rule from "./require-antd-style-import";
+
+const ruleTester = new TSESLint.RuleTester({
+  parser: require.resolve("@typescript-eslint/parser"),
+});
+
+ruleTester.run("require-antd-style-import", rule, {
+  valid: [
+    {
+      name: "single import statement",
+      code: `
+        import { Col } from "antd";
+        import "antd/lib/col/style";
+      `,
+    },
+    {
+      name: "multiple import statements",
+      code: `
+        import { Col } from "antd";
+        import "antd/lib/col/style";
+        import { Row } from "antd";
+        import "antd/lib/row/style";
+      `,
+    },
+    {
+      name: "multiple values in one import statement",
+      code: `
+        import { Col, Row } from "antd";
+        import "antd/lib/col/style";
+        import "antd/lib/row/style";
+      `,
+    },
+    {
+      name: "renamed import",
+      code: `
+        import { Col as AntdCol } from "antd";
+        import "antd/lib/col/style";
+      `,
+    },
+    {
+      name: "PascalCase import",
+      code: `
+        import { PageHeader } from "antd";
+        import "antd/lib/page-header/style";
+      `,
+    },
+    {
+      name: "PascalCase renamed import",
+      code: `
+        import { PageHeader as AntdPageHeader } from "antd";
+        import "antd/lib/page-header/style";
+      `,
+    },
+    {
+      name: "out of order imports",
+      code: `
+        import "antd/lib/col/style";
+        import { Table } from "antd";
+        import "antd/lib/row/style";
+        import { Row, Col as Column } from "antd";
+        import "antd/lib/table/style";
+      `,
+    },
+    {
+      name: "type-only import with no style",
+      code: `
+        import type { Foo } from "antd";
+      `,
+    },
+    {
+      name: "nested import with no style",
+      code: `
+        import { CheckboxProps } from "antd/lib/checkbox";
+      `,
+    },
+    {
+      name: "default antd imports",
+      code: `
+        import ant_design from "antd";
+      `,
+    },
+    {
+      name: "non-antd imports",
+      code: `
+        import { useEffect } from "react";
+      `,
+    },
+  ],
+  invalid: [
+    {
+      name: "missing single import",
+      code: `
+//  0   0   1   1   2   2
+//  5   9   3   7   1   5
+import { Row } from "antd";
+`.trim(),
+      errors: [
+        {
+          messageId: "missingImport",
+          data: {
+            imported: "Row",
+            style: "antd/lib/row/style",
+          },
+          line: 3,
+          column: 10,
+          endColumn: 13,
+        }
+      ],
+      output: `
+//  0   0   1   1   2   2
+//  5   9   3   7   1   5
+import { Row } from "antd";
+import "antd/lib/row/style";
+`.trim(),
+    },
+    {
+      name: "missing multiple import statements",
+      code: `
+//  0   0   1   1   2   2
+//  5   9   3   7   1   5
+import { Row } from "antd";
+import { Table } from "antd";
+`.trim(),
+      errors: [
+        {
+          messageId: "missingImport",
+          data: {
+            imported: "Row",
+            style: "antd/lib/row/style",
+          },
+          line: 3,
+          column: 10,
+          endColumn: 13,
+        },
+        {
+          messageId: "missingImport",
+          data: {
+            imported: "Table",
+            style: "antd/lib/table/style",
+          },
+          line: 4,
+          column: 10,
+          endColumn: 15,
+        }
+      ],
+      output: `
+//  0   0   1   1   2   2
+//  5   9   3   7   1   5
+import { Row } from "antd";
+import "antd/lib/row/style";
+import { Table } from "antd";
+import "antd/lib/table/style";
+`.trim(),
+    },
+    {
+      name: "missing import from multi-import",
+      code: `
+//  0   0   1   1   2   2
+//  5   9   3   7   1   5
+import { Row, Col } from "antd";
+`.trim(),
+      errors: [
+        {
+          messageId: "missingImport",
+          data: {
+            imported: "Row",
+            style: "antd/lib/row/style",
+          },
+          line: 3,
+          column: 10,
+          endColumn: 13,
+        },
+        {
+          messageId: "missingImport",
+          data: {
+            imported: "Col",
+            style: "antd/lib/col/style",
+          },
+          line: 3,
+          column: 15,
+          endColumn: 18,
+        }
+      ],
+      // eslint's RuleTester only applies a single pass of fixers for each AST node, so the second
+      // fix (for "Col") is intentionally not included here.
+      output: `
+//  0   0   1   1   2   2
+//  5   9   3   7   1   5
+import { Row, Col } from "antd";
+import "antd/lib/row/style";
+`.trim(),
+    },
+    {
+      name: "missing import from multi-import",
+      code: `
+//  0   0   1   1   2   2
+//  5   9   3   7   1   5
+import { Row, Col } from "antd";
+`.trim(),
+      errors: [
+        {
+          messageId: "missingImport",
+          data: {
+            imported: "Row",
+            style: "antd/lib/row/style",
+          },
+          line: 3,
+          column: 10,
+          endColumn: 13,
+        },
+        {
+          messageId: "missingImport",
+          data: {
+            imported: "Col",
+            style: "antd/lib/col/style",
+          },
+          line: 3,
+          column: 15,
+          endColumn: 18,
+        }
+      ],
+      // eslint's RuleTester only applies a single pass of fixers for each AST node, so the second
+      // fix (for "Col") is intentionally not included here.
+      output: `
+//  0   0   1   1   2   2
+//  5   9   3   7   1   5
+import { Row, Col } from "antd";
+import "antd/lib/row/style";
+`.trim(),
+    },
+    {
+      name: "missing renamed import",
+      code: `
+//  0   0   1   1   2   2
+//  5   9   3   7   1   5
+import { Row as AntdRow } from "antd";
+`.trim(),
+      errors: [
+        {
+          messageId: "missingImport",
+          data: {
+            imported: "Row",
+            style: "antd/lib/row/style",
+          },
+          line: 3,
+          column: 10,
+          endColumn: 24,
+        },
+      ],
+      output: `
+//  0   0   1   1   2   2
+//  5   9   3   7   1   5
+import { Row as AntdRow } from "antd";
+import "antd/lib/row/style";
+`.trim(),
+    },
+    {
+      name: "missing PascalCase import",
+      code: `
+//  0   0   1   1   2   2
+//  5   9   3   7   1   5
+import { PageHeader } from "antd";
+`.trim(),
+      errors: [
+        {
+          messageId: "missingImport",
+          data: {
+            imported: "PageHeader",
+            style: "antd/lib/page-header/style",
+          },
+          line: 3,
+          column: 10,
+          endColumn: 20,
+        },
+      ],
+      output: `
+//  0   0   1   1   2   2
+//  5   9   3   7   1   5
+import { PageHeader } from "antd";
+import "antd/lib/page-header/style";
+`.trim(),
+    },
+    {
+      name: "missing renamed PascalCase import",
+      code: `
+//  0   0   1   1   2   2   2
+//  5   9   3   7   1   5   9
+import { PageHeader as Header } from "antd";
+`.trim(),
+      errors: [
+        {
+          messageId: "missingImport",
+          data: {
+            imported: "PageHeader",
+            style: "antd/lib/page-header/style",
+          },
+          line: 3,
+          column: 10,
+          endColumn: 30,
+        },
+      ],
+      output: `
+//  0   0   1   1   2   2   2
+//  5   9   3   7   1   5   9
+import { PageHeader as Header } from "antd";
+import "antd/lib/page-header/style";
+`.trim(),
+    }
+  ],
+});
