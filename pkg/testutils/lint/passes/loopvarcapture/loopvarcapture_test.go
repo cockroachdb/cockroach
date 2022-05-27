4a6d009e577f7707321275c6ad7595d839f78d6e
Renato Costa <renato@cockroachlabs.com>
2022-05-27 10:29:52 -0400

    lint: add loopvarcapture linter
    This commit introduces a new linter: `loopvarcapture`. It reports
    uses of loop variables captured by reference in Go routines or defer
    statements, a common source of data races [1].
    
    `govet` currently has a similar linter [2]; however, that project
    prioritizes having no false positives at the expense of allowing
    false negatives. This linter, on the other hand, represents the
    opinion that loop variables should not be captured by reference in Go
    routines even when it's safe to do so. That behavior is confusing and
    concurrency added to related code over time  could lead to the
    introduction of data races, potentially manifesting as bugs in the
    product or flakiness in the tests. These issues are hard to debug and
    take a lot of developer time.
    
    Developers are still able to use their own judgement and disable this
    linter in specific instances by using a `nolint` comment.
    
    [1] A Study of Real-World Data Races in Golang: https://arxiv.org/pdf/2204.00764.pdf
    [2] https://github.com/golangci/govet/blob/44ddbe260190d79165f4150b828650780405d801/rangeloop.go#L36
    
    Resolves: #80803.
    
    Release note: None.

---
+++ pkg/testutils/lint/passes/loopvarcapture/loopvarcapture_test.go
@@ -0,0 +1,45 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package loopvarcapture_test
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/build/bazel"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/lint/passes/loopvarcapture"
+	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+var extraGoRoutineFunctions = []loopvarcapture.Function{
+	{Pkg: "example.org/concurrency", Type: "Group", Name: "Go"}, // test non-pointer receiver
+	{Pkg: "example.org/concurrency", Name: "Go"},                // test a package-level function
+	{Pkg: "example.org/concurrency", Name: "GoWithError"},       // test a function with a return value
+}
+
+func init() {
+	if bazel.BuiltWithBazel() {
+		bazel.SetGoEnv()
+	}
+}
+
+func TestAnalyzer(t *testing.T) {
+	skip.UnderStress(t)
+
+	originalGoRoutineFunctions := loopvarcapture.GoRoutineFunctions
+	loopvarcapture.GoRoutineFunctions = append(originalGoRoutineFunctions, extraGoRoutineFunctions...)
+	defer func() { loopvarcapture.GoRoutineFunctions = originalGoRoutineFunctions }()
+
+	testdata := testutils.TestDataPath(t)
+	analysistest.TestData = func() string { return testdata }
+	analysistest.Run(t, testdata, loopvarcapture.Analyzer, "p")
+}
