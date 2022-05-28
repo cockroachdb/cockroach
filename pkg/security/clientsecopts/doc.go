577e158f3017448bec8ef2a69f1430a0990e30e9
Raphael 'kena' Poss <knz@thaumogen.net>
2022-05-28 19:30:32 +0200

    rpc: move LoadSecurityOptions and PGURL to a new lightweight package
    This allows clarifies their scope as separate from the RPC logic.
    
    Release note: None

---
+++ pkg/security/clientsecopts/doc.go
@@ -0,0 +1,13 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+// Package clientsecopts defines the rules to attach TLS
+// authentication options to a client connection URL.
+package clientsecopts
