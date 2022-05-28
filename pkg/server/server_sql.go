b8ae22000efce54b1ceb2c5a34d347f69246495b
Raphael 'kena' Poss <knz@thaumogen.net>
2022-05-28 21:59:47 +0200

    clientconnurl: rename ClientOptions to ClientSecurityOptions
    to make room for a non-security ClientOptions.
    
    Release note: None

--- pkg/server/server_sql.go
+++ pkg/server/server_sql.go
@@ -677,7 +677,7 @@ func newSQLServer(ctx context.Context, cfg sqlServerArgs) (*SQLServer, error) {
 		sqlExecutorTestingKnobs = sql.ExecutorTestingKnobs{}
 	}
 
-	ccopts := clientsecopts.ClientOptions{
+	ccopts := clientsecopts.ClientSecurityOptions{
 		Insecure: cfg.Config.Insecure,
 		CertsDir: cfg.Config.SSLCertsDir,
 	}
