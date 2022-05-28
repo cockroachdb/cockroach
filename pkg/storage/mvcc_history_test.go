5a2ec4fd8cd63deaa5d15802581fca54f50e96b9
Erik Grinaker <grinaker@cockroachlabs.com>
2022-05-28 12:14:47 +0000

    storage: add MVCC point synthesizing iterator
    This patch adds `pointSynthesizingIter`, an MVCC iterator which wraps an
    arbitrary MVCC iterator and synthesizes point keys for range keys at
    their start key and where they overlap point keys. It can optionally
    synthesize around the SeekGE seek key too, which is useful for point
    operations like `MVCCGet` where we may want to return a synthetic
    tombstone for an MVCC range tombstone if there is no existing point key.
    
    This will primarily be used to handle MVCC range tombstones in MVCC
    scans and gets, as well as during MVCC conflict checks, which allows
    much of this logic to remain unchanged and simplified (in particular,
    `pebbleMVCCScanner` will not need any changes).
    
    However, this patch does not make use of the iterator yet, since both it
    and Pebble will need further performance optimizations for use in hot
    paths. For now, correctness is sufficient, and only basic attempts at
    performance optimization have been made.
    
    Release note: None

--- pkg/storage/mvcc_history_test.go
+++ pkg/storage/mvcc_history_test.go
@@ -69,7 +69,7 @@ import (
 // get            [t=<name>] [ts=<int>[,<int>]]                         [resolve [status=<txnstatus>]] k=<key> [inconsistent] [tombstones] [failOnMoreRecent] [localUncertaintyLimit=<int>[,<int>]] [globalUncertaintyLimit=<int>[,<int>]]
 // scan           [t=<name>] [ts=<int>[,<int>]]                         [resolve [status=<txnstatus>]] k=<key> [end=<key>] [inconsistent] [tombstones] [reverse] [failOnMoreRecent] [localUncertaintyLimit=<int>[,<int>]] [globalUncertaintyLimit=<int>[,<int>]] [max=<max>] [targetbytes=<target>] [avoidExcess] [allowEmpty]
 //
-// iter_new       [k=<key>] [end=<key>] [prefix] [kind=key|keyAndIntents] [types=pointsOnly|pointsWithRanges|pointsAndRanges|rangesOnly] [maskBelow=<int>[,<int>]]
+// iter_new       [k=<key>] [end=<key>] [prefix] [kind=key|keyAndIntents] [types=pointsOnly|pointsWithRanges|pointsAndRanges|rangesOnly] [pointSynthesis [emitOnSeekGE]] [maskBelow=<int>[,<int>]]
 // iter_seek_ge   k=<key> [ts=<int>[,<int>]]
 // iter_seek_lt   k=<key> [ts=<int>[,<int>]]
 // iter_seek_intent_ge k=<key> txn=<name>
@@ -1012,6 +1012,9 @@ func cmdIterNew(e *evalCtx) error {
 	e.iter = &iterWithCloseReader{
 		MVCCIterator: r.NewMVCCIterator(kind, opts),
 		closeReader:  closeReader,
+	}
+	if e.hasArg("pointSynthesis") {
+		e.iter = newPointSynthesizingIter(e.iter, e.hasArg("emitOnSeekGE"))
 	}
 	return nil
 }
