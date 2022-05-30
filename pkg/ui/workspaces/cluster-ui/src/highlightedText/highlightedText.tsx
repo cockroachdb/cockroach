11855299910b4b533df6a2a8889638ceea89a6fc
Marylia Gutierrez <marylia@cockroachlabs.com>
2022-05-30 14:50:09 -0400

    ui: dont add highlight for * and ^
    We don't highlight special characters that are normally
    used on regex, but the characters `*` and `^` were not on
    the list.
    This commit adds them on the list to be ignored to highlight.
    This commit also adds tests for the higlight function.
    
    Fixes #81695
    
    Release note (bug fix): properly handle special characters to
    not being highlighted.

--- pkg/ui/workspaces/cluster-ui/src/highlightedText/highlightedText.tsx
+++ pkg/ui/workspaces/cluster-ui/src/highlightedText/highlightedText.tsx
@@ -79,19 +79,14 @@ export function getHighlightedText(
   if (!highlight || highlight.length === 0) {
     return text;
   }
+
   highlight = highlight.replace(
-    /[°§%()\[\]{}\\?´`'#|;:+-]+/g,
+    /[°§%()\[\]{}\\?´`'#|;:+^*-]+/g,
     "highlightNotDefined",
   );
   const search = highlight
     .split(" ")
     .map(val => {
-      // When it's only a character we want to make sure to use the literal value (by adding []),
-      // this is added to handle cases where the highlighted term is a character used normally
-      // in regex, such as '*', '+', '?'
-      if (val.length == 1) {
-        return `[${val.toLowerCase()}]`;
-      }
       if (val.length > 0) {
         return val.toLowerCase();
       }
