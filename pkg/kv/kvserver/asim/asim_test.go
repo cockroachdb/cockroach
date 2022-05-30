5cd6d638d987a3010e7cab8c50f0ce904ab941d1
Austen McClernon <austen@cockroachlabs.com>
2022-05-30 21:04:12 +0000

    asim: add replicate queue
    This patch adds logic to simulate the replicate queue. The replicate
    queue considers replicas that it is given, enqueueing them if they meet
    the criteria. The criteria is currently whether there is a non noop
    allocator action for that replica. The replicate queue processes one
    replica at once and if a consider rebalance action is returned by the
    allocator, it will create a replica change to be applied to state.
    
    Release note: None

--- pkg/kv/kvserver/asim/asim_test.go
+++ pkg/kv/kvserver/asim/asim_test.go
@@ -12,12 +12,16 @@ package asim_test
 
 import (
 	"context"
+	"fmt"
+	"math"
 	"testing"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/state"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/workload"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRunAllocatorSimulator(t *testing.T) {
@@ -34,4 +38,48 @@ func TestRunAllocatorSimulator(t *testing.T) {
 
 	sim := asim.NewSimulator(start, end, interval, rwg, s, exchange, changer)
 	sim.RunSim(ctx)
+}
+
+// TestAllocatorSimulatorSpeed tests that the simulation runs at a rate of at
+// least 5 simulated minutes per wall clock second (1:600) for a 32 node
+// cluster, with 1000 replicas per node.
+// NB: In practice, on a single thread N2 GCP VM, this completes with a minimum
+// run of 100ms, approx 10x faster than what this test asserts. The limit is
+// set much higher due to --stress and inconsistent processor speeds.
+func TestAllocatorSimulatorSpeed(t *testing.T) {
+	ctx := context.Background()
+	rwg := make([]workload.Generator, 1)
+	rwg[0] = &workload.RandomGenerator{}
+	start := state.TestingStartTime()
+	end := start.Add(5 * time.Minute)
+	interval := 10 * time.Second
+
+	sample := func() int64 {
+		exchange := state.NewFixedDelayExhange(start, interval, interval)
+		changer := state.NewReplicaChanger()
+		replicaCounts := make(map[state.StoreID]int)
+		for i := 0; i < 32; i++ {
+			replicaCounts[state.StoreID(i+1)] = 1000
+		}
+		s, _ := state.NewTestStateReplCounts(replicaCounts)
+		sim := asim.NewSimulator(start, end, interval, rwg, s, exchange, changer)
+
+		startTime := timeutil.Now()
+		sim.RunSim(ctx)
+		return timeutil.Since(startTime).Nanoseconds()
+	}
+
+	// We sample 5 runs and take the minimum. The minimum is the cleanest
+	// estimate here of performance, as any additional time over the minimum is
+	// noise in a run.
+	minRunTime := int64(math.MaxInt64)
+	samples := 5
+	for i := 0; i < samples; i++ {
+		if sampledRun := sample(); sampledRun < minRunTime {
+			minRunTime = sampledRun
+		}
+	}
+
+	fmt.Println(time.Duration(minRunTime).Seconds())
+	require.Less(t, minRunTime, time.Second.Nanoseconds())
 }
---
+++ pkg/kv/kvserver/asim/replicate_queue.go
@@ -0,0 +1,228 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package asim
+
+import (
+	"container/heap"
+	"context"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/allocator/allocatorimpl"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/allocator/storepool"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/state"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+)
+
+// ReplicateQueue presents an interface to interact with a single consumer
+// queue, which processes replicas for replication updates if they meet the
+// criteria.
+type ReplicateQueue interface {
+	// MaybeAdd proposes a replica for inclusion into the ReplicateQueue, if it
+	// meets the criteria it is enqueued.
+	MaybeAdd(ctx context.Context, repl state.Replica, state state.State) bool
+	// Tick proceses updates in the ReplicateQueue. Only one replica is
+	// processed at a time and the duration taken to process a replica depends
+	// on the action taken. Replicas in the queue are processed in order of
+	// priority, then in FIFO order on ties.
+	Tick(ctx context.Context, tick time.Time, state state.State)
+}
+
+// replicaItem represents an item in the replica queue.
+type replicaItem struct {
+	rangeID   roachpb.RangeID
+	replicaID roachpb.ReplicaID
+	// Enforce FIFO order for equal priorities.
+	seq int
+	// Fields used when a replicaItem is enqueued in a priority queue.
+	priority float64
+	// The index of the item in the heap, maintained by the heap.Interface
+	// methods.
+	index int
+}
+
+// Len is part of the container.Heap interface.
+func (pq priorityQueue) Len() int { return len(pq.sl) }
+
+// Less is part of the container.Heap interface.
+func (pq priorityQueue) Less(i, j int) bool {
+	a, b := pq.sl[i], pq.sl[j]
+	if a.priority == b.priority {
+		// When priorities are equal, we want the lower sequence number to show
+		// up first (FIFO).
+		return a.seq < b.seq
+	}
+	// We want Pop to give us the highest, not lowest, priority so we use
+	// greater than here.
+	return a.priority > b.priority
+}
+
+// Swap is part of the container.Heap interface.
+func (pq priorityQueue) Swap(i, j int) {
+	pq.sl[i], pq.sl[j] = pq.sl[j], pq.sl[i]
+	pq.sl[i].index, pq.sl[j].index = i, j
+}
+
+// push is part of the container.Heap interface.
+func (pq *priorityQueue) Push(x interface{}) {
+	n := len(pq.sl)
+	item := x.(*replicaItem)
+	item.index = n
+	pq.seqGen++
+	item.seq = pq.seqGen
+	pq.sl = append(pq.sl, item)
+}
+
+// Pop is part of the container.Heap interface.
+func (pq *priorityQueue) Pop() interface{} {
+	old := pq.sl
+	n := len(old)
+	item := old[n-1]
+	item.index = -1 // for safety
+	old[n-1] = nil  // for gc
+	pq.sl = old[0 : n-1]
+	return item
+}
+
+type priorityQueue struct {
+	seqGen int
+	sl     []*replicaItem
+}
+
+// replicateQueue is an implementation of the ReplicateQueue interface.
+type replicateQueue struct {
+	priorityQueue
+	allocator    allocatorimpl.Allocator
+	storeID      state.StoreID
+	stateChanger state.Changer
+	next         time.Time
+	// TOOD(kvoli): Delay is used as a fixed delay we pass for all replica
+	// changes. In the future we should pass in a delay function which returns
+	// a duration given the size of the range, snapshot rates and overload.
+	delay time.Duration
+}
+
+// NewReplicateQueue returns a new replicate queue.
+func NewReplicateQueue(
+	storeID state.StoreID,
+	stateChanger state.Changer,
+	delay time.Duration,
+	allocator allocatorimpl.Allocator,
+) ReplicateQueue {
+	return &replicateQueue{
+		priorityQueue: priorityQueue{sl: make([]*replicaItem, 0, 1)},
+		allocator:     allocator,
+		storeID:       storeID,
+		stateChanger:  stateChanger,
+		delay:         delay,
+	}
+}
+
+// MaybeAdd proposes a replica for inclusion into the ReplicateQueue, if it
+// meets the criteria it is enqueued. The criteria is currently if the
+// allocator returns a non-noop, then the replica is added.
+func (rq *replicateQueue) MaybeAdd(
+	ctx context.Context, replica state.Replica, state state.State,
+) bool {
+	rng, ok := state.Range(replica.Range())
+	if !ok {
+		return false
+	}
+
+	action, priority := rq.allocator.ComputeAction(ctx, rng.SpanConfig(), rng.Descriptor())
+	if action != allocatorimpl.AllocatorNoop {
+		heap.Push(
+			rq,
+			&replicaItem{
+				rangeID:   roachpb.RangeID(replica.Range()),
+				replicaID: replica.Descriptor().ReplicaID,
+				priority:  priority,
+			},
+		)
+		return true
+	}
+	return false
+}
+
+// Tick proceses updates in the ReplicateQueue. Only one replica is
+// processed at a time and the duration taken to process a replica depends
+// on the action taken. Replicas in the queue are processed in order of
+// priority, then in FIFO order on ties. The Tick function currently only
+// supports processing ConsiderRebalance actions on replicas.
+// TODO(kvoli,lidorcarmel): Support taking additional actions, beyond consider rebalance.
+func (rq *replicateQueue) Tick(ctx context.Context, tick time.Time, s state.State) {
+	if tick.Before(rq.next) || rq.priorityQueue.Len() == 0 {
+		return
+	}
+
+	item := heap.Pop(rq).(*replicaItem)
+	if item == nil {
+		return
+	}
+
+	rng, ok := s.Range(state.RangeID(item.rangeID))
+	if !ok {
+		return
+	}
+
+	action, _ := rq.allocator.ComputeAction(ctx, rng.SpanConfig(), rng.Descriptor())
+
+	switch action {
+	case allocatorimpl.AllocatorConsiderRebalance:
+		rq.considerRebalance(ctx, tick, rng, s)
+	case allocatorimpl.AllocatorNoop:
+		return
+	default:
+		log.Infof(ctx, "allocator action %s for range %s is unsupported by the simulator "+
+			"replicate queue, ignoring.", action, rng)
+		return
+	}
+}
+
+// considerRebalance simulates the logic of the replicate queue when given a
+// considerRebalance action. It will first ask the allocator for add and remove
+// targets for a range. It will then enqueue the replica change into the state
+// changer and update the time to process the next replica, with the completion
+// time returned.
+func (rq *replicateQueue) considerRebalance(
+	ctx context.Context, tick time.Time, rng state.Range, s state.State,
+) {
+	add, remove, details, ok := rq.allocator.RebalanceVoter(
+		ctx,
+		rng.SpanConfig(),
+		nil, /* raftStatus */
+		rng.Descriptor().Replicas().VoterDescriptors(),
+		rng.Descriptor().Replicas().NonVoterDescriptors(),
+		s.UsageInfo(rng.RangeID()),
+		storepool.StoreFilterNone,
+		rq.allocator.ScorerOptions(ctx),
+	)
+
+	// We were unable to find a rebalance target for the range.
+	if !ok {
+		log.Warningf(ctx, "%s", details)
+		return
+	}
+
+	// Enqueue the change to be processed and update when the next replica
+	// processing can occur with the completion time of the change.
+	// 	NB: This limits concurrency to at most one change at a time per
+	// 	ReplicateQueue.
+	change := state.ReplicaChange{
+		RangeID: state.RangeID(rng.Descriptor().RangeID),
+		Add:     state.StoreID(add.StoreID),
+		Remove:  state.StoreID(remove.StoreID),
+		Wait:    rq.delay,
+	}
+	if completeAt, ok := rq.stateChanger.Push(tick, &change); ok {
+		rq.next = completeAt
+	}
+}
