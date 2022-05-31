fa2213ad3bf13e6cfea168f3f971b018b3f12bf9
Thomas Hardy <thardy@cockroachlabs.com>
2022-05-31 17:14:09 -0400

    cluster-ui: created a "connected" component for index details page
    This PR introduces an "indexDetailsConnected" component on cluster-ui.
    This allows us to reuse the cluster-ui redux store without replicating
    the same logic on CC console.
    
    Release note: None

---
+++ pkg/ui/workspaces/cluster-ui/src/indexDetailsPage/indexDetails.selectors.ts
@@ -0,0 +1,104 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import { createSelector } from "reselect";
+import { AppState } from "../store";
+import { RouteComponentProps } from "react-router";
+import {
+  databaseNameAttr,
+  generateTableID,
+  getMatchParamByName,
+  indexNameAttr,
+  longToInt,
+  schemaNameAttr,
+  tableNameAttr,
+  TimestampToMoment,
+} from "../util";
+import { cockroach } from "@cockroachlabs/crdb-protobuf-client";
+import { IndexDetailsPageData } from "./indexDetailsPage";
+import { selectIsTenant } from "../store/uiConfig";
+import { BreadcrumbItem } from "../breadcrumbs";
+const { RecommendationType } = cockroach.sql.IndexRecommendation;
+
+export const selectIndexDetails = createSelector(
+  (_state: AppState, props: RouteComponentProps): string =>
+    getMatchParamByName(props.match, databaseNameAttr),
+  (_state: AppState, props: RouteComponentProps): string =>
+    getMatchParamByName(props.match, schemaNameAttr),
+  (_state: AppState, props: RouteComponentProps): string =>
+    getMatchParamByName(props.match, tableNameAttr),
+  (_state: AppState, props: RouteComponentProps): string =>
+    getMatchParamByName(props.match, indexNameAttr),
+  (state: AppState) => state.adminUI.indexStats.cachedData,
+  (state: AppState) => selectIsTenant(state),
+  (database, schema, table, index, indexStats): IndexDetailsPageData => {
+    const stats = indexStats[generateTableID(database, table)];
+    const details = stats?.data?.statistics.filter(
+      stat => stat.index_name === index, // index names must be unique for a table
+    )[0];
+    const filteredIndexRecommendations =
+      stats?.data?.index_recommendations.filter(
+        indexRec => indexRec.index_id === details.statistics.key.index_id,
+      ) || [];
+    const indexRecommendations = filteredIndexRecommendations.map(indexRec => {
+      return {
+        type: RecommendationType[indexRec.type].toString(),
+        reason: indexRec.reason,
+      };
+    });
+
+    return {
+      databaseName: database,
+      tableName: table,
+      indexName: index,
+      breadcrumbItems: createManagedServiceBreadcrumbs(
+        database,
+        schema,
+        table,
+        index,
+      ),
+      details: {
+        loading: !!stats?.inFlight,
+        loaded: !!stats?.valid,
+        createStatement: details?.create_statement || "",
+        totalReads:
+          longToInt(details?.statistics?.stats?.total_read_count) || 0,
+        lastRead: TimestampToMoment(details?.statistics?.stats?.last_read),
+        lastReset: TimestampToMoment(stats?.data?.last_reset),
+        indexRecommendations,
+      },
+    };
+  },
+);
+
+// Note: if the managed-service routes to the index detail or the previous
+// database pages change, the breadcrumbs displayed here need to be updated.
+function createManagedServiceBreadcrumbs(
+  database: string,
+  schema: string,
+  table: string,
+  index: string,
+): BreadcrumbItem[] {
+  return [
+    { link: "/databases", name: "Databases" },
+    {
+      link: `/databases/${database}`,
+      name: "Tables",
+    },
+    {
+      link: `/databases/${database}/${schema}/${table}`,
+      name: `Table: ${table}`,
+    },
+    {
+      link: `/databases/${database}/${schema}/${table}/${index}`,
+      name: `Index: ${index}`,
+    },
+  ];
+}
--- pkg/ui/workspaces/cluster-ui/src/store/indexStats/index.ts
+++ pkg/ui/workspaces/cluster-ui/src/store/indexStats/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2021 The Cockroach Authors.
+// Copyright 2022 The Cockroach Authors.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt.
@@ -8,5 +8,5 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-export * from "./indexDetailsPage";
-export * from "./indexDetailsConnected";
+export * from "./indexStats.reducer";
+export * from "./indexStats.sagas";
