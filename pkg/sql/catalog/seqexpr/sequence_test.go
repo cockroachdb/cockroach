1b662de9c20c5a75071c480c4d2d07fd34915377
Xiang Gu <xiang@cockroachlabs.com>
2022-05-31 16:51:34 -0400

    ccl: upgrade by-name sequence reference to by-ID during restore
    In 20.2 and prior, sequences are referenced by-name. It was later
    changed to reference-by-ID to enable things like
    `ALTER SEQUENCE ... RENAME ...`.
    
    But if a backup is taken in 20.2 and prior, and then the backup is
    restored in a newer binary version (where sequence references should
    be by-ID), we will need to also be able to upgrade those sequence
    references from by-name to by-ID.
    
    Release note: None

--- pkg/sql/catalog/seqexpr/sequence_test.go
+++ pkg/sql/catalog/seqexpr/sequence_test.go
@@ -15,9 +15,11 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/parser"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetSequenceFromFunc(t *testing.T) {
@@ -119,7 +121,7 @@ func TestGetUsedSequences(t *testing.T) {
 }
 
 func TestReplaceSequenceNamesWithIDs(t *testing.T) {
-	namesToID := map[string]int64{
+	namesToID := map[string]descpb.ID{
 		"seq": 123,
 	}
 
@@ -154,4 +156,125 @@ func TestReplaceSequenceNamesWithIDs(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestMaybeUpgradeSequenceReference(t *testing.T) {
+	allUsedSeqNames := make(map[string]descpb.ID)
+	allUsedSeqNames["public.s1"] = 1
+	allUsedSeqNames["testdb.public.s2"] = 2
+	allUsedSeqNames["sc1.s3"] = 3
+	allUsedSeqNames["s3"] = 3
+	expr := "nextval('public.s1') + nextval('testdb.public.s2') + currval('sc1.s3') + nextval('s3')"
+	hasUpgraded, err := UpgradeSequenceReferenceInExpr(&expr, allUsedSeqNames)
+	require.NoError(t, err)
+	require.True(t, hasUpgraded)
+	require.Equal(t,
+		"((nextval(1:::REGCLASS) + nextval(2:::REGCLASS)) + currval(3:::REGCLASS)) + nextval(3:::REGCLASS)",
+		expr)
+}
+
+func TestSeqNameToIDMappingInExpr(t *testing.T) {
+	t.Run("t1", func(t *testing.T) {
+		// Test 1:
+		// 		seqIDToNameMapping = {23 : 'db.sc1.t', 25 : 'db.sc2.t'}
+		//		expr = "nextval('sc2.t')"
+		//		expected return =  {"sc2.t" : 25} (because `db.sc2.t` best-matches `sc2.t`)
+		seqIDToNameMapping := make(map[descpb.ID]*tree.TableName)
+		tbl1 := tree.MakeTableNameWithSchema("db", "sc1", "t")
+		tbl2 := tree.MakeTableNameWithSchema("db", "sc2", "t")
+		seqIDToNameMapping[23] = &tbl1
+		seqIDToNameMapping[25] = &tbl2
+		expr := "nextval('sc2.t')"
+		seqNameToIDmappingInExpr, err := SeqNameToIDMappingInExpr(expr, seqIDToNameMapping)
+		require.NoError(t, err)
+		require.Equal(t, 1, len(seqNameToIDmappingInExpr))
+		require.Equal(t, descpb.ID(25), seqNameToIDmappingInExpr["sc2.t"])
+	})
+
+	t.Run("t2", func(t *testing.T) {
+		// Test 2:
+		// 		seqIDToNameMapping = {23 : 'db.sc1.t', 25 : 'sc2.t'}
+		//		expr = "nextval('sc2.t')"
+		//		expected return =  {"sc2.t" : 25} (because `sc2.t` best-matches `sc2.t`)
+		seqIDToNameMapping := make(map[descpb.ID]*tree.TableName)
+		tbl1 := tree.MakeTableNameWithSchema("db", "sc1", "t")
+		tbl2 := tree.MakeTableNameWithSchema("", "sc2", "t")
+		seqIDToNameMapping[23] = &tbl1
+		seqIDToNameMapping[25] = &tbl2
+		expr := "nextval('sc2.t')"
+		seqNameToIDmappingInExpr, err := SeqNameToIDMappingInExpr(expr, seqIDToNameMapping)
+		require.NoError(t, err)
+		require.Equal(t, 1, len(seqNameToIDmappingInExpr))
+		require.Equal(t, descpb.ID(25), seqNameToIDmappingInExpr["sc2.t"])
+	})
+
+	t.Run("t3", func(t *testing.T) {
+		// Test 3:
+		// 		seqIDToNameMapping = {23 : 'db.sc1.t', 25 : 'sc2.t'}
+		//		expr = "nextval('db.sc2.t')"
+		//		expected return =  {"db.sc2.t" : 25} (because `sc2.t` best-matches `db.sc2.t`)
+		seqIDToNameMapping := make(map[descpb.ID]*tree.TableName)
+		tbl1 := tree.MakeTableNameWithSchema("db", "sc1", "t")
+		tbl2 := tree.MakeTableNameWithSchema("", "sc2", "t")
+		seqIDToNameMapping[23] = &tbl1
+		seqIDToNameMapping[25] = &tbl2
+		expr := "nextval('db.sc2.t')"
+		seqNameToIDmappingInExpr, err := SeqNameToIDMappingInExpr(expr, seqIDToNameMapping)
+		require.NoError(t, err)
+		require.Equal(t, 1, len(seqNameToIDmappingInExpr))
+		require.Equal(t, descpb.ID(25), seqNameToIDmappingInExpr["db.sc2.t"])
+	})
+
+	t.Run("t4", func(t *testing.T) {
+		// Test 4:
+		// 		seqIDToNameMapping = {23 : 'sc1.t', 25 : 'sc2.t'}
+		//		expr = "nextval('t')"
+		//		expected return = non-nil error (because both 'sc1.t' and 'sc2.t' are equally good matches
+		//				 		 					for 't' and we cannot decide)
+		seqIDToNameMapping := make(map[descpb.ID]*tree.TableName)
+		tbl1 := tree.MakeTableNameWithSchema("", "sc1", "t")
+		tbl2 := tree.MakeTableNameWithSchema("", "sc2", "t")
+		seqIDToNameMapping[23] = &tbl1
+		seqIDToNameMapping[25] = &tbl2
+		expr := "nextval('t')"
+		_, err := SeqNameToIDMappingInExpr(expr, seqIDToNameMapping)
+		require.Errorf(t, err, "more than 1 matches found for t")
+	})
+
+	t.Run("t5", func(t *testing.T) {
+		// Test 5:
+		// 		seqIDToNameMapping = {23 : 'sc1.t', 25 : 'sc2.t'}
+		//		expr = "nextval('t2')"
+		//		expected return = non-nil error (because neither 'sc1.t' nor 'sc2.t' matches 't2')
+		seqIDToNameMapping := make(map[descpb.ID]*tree.TableName)
+		tbl1 := tree.MakeTableNameWithSchema("", "sc1", "t")
+		tbl2 := tree.MakeTableNameWithSchema("", "sc2", "t")
+		seqIDToNameMapping[23] = &tbl1
+		seqIDToNameMapping[25] = &tbl2
+		expr := "nextval('t2')"
+		_, err := SeqNameToIDMappingInExpr(expr, seqIDToNameMapping)
+		require.Errorf(t, err, "no table name found to match input t2")
+	})
+
+	t.Run("t6", func(t *testing.T) {
+		// Test 6:
+		// 		seqIDToNameMapping = {23 : 'sc1.t', 25 : 'sc2.t', 27 : 't2'}
+		//		expr = "nextval('sc1.t') + currval('testdb.sc2.t') + nextval('t2') + nextval('sc2.t2')"
+		//		expected return = {'sc1.t' : 23, 'testdb.sc2.t' : 25, 't2' : 27, 'sc2.t2' : 27}
+		seqIDToNameMapping := make(map[descpb.ID]*tree.TableName)
+		tbl1 := tree.MakeTableNameWithSchema("", "sc1", "t")
+		tbl2 := tree.MakeTableNameWithSchema("", "sc2", "t")
+		tbl3 := tree.MakeTableNameWithSchema("", "", "t2")
+		seqIDToNameMapping[23] = &tbl1
+		seqIDToNameMapping[25] = &tbl2
+		seqIDToNameMapping[27] = &tbl3
+		expr := "nextval('sc1.t') + currval('testdb.sc2.t') + nextval('t2') + nextval('sc2.t2')"
+		seqNameToIDmappingInExpr, err := SeqNameToIDMappingInExpr(expr, seqIDToNameMapping)
+		require.NoError(t, err)
+		require.Equal(t, 4, len(seqNameToIDmappingInExpr))
+		require.Equal(t, descpb.ID(23), seqNameToIDmappingInExpr["sc1.t"])
+		require.Equal(t, descpb.ID(25), seqNameToIDmappingInExpr["testdb.sc2.t"])
+		require.Equal(t, descpb.ID(27), seqNameToIDmappingInExpr["t2"])
+		require.Equal(t, descpb.ID(27), seqNameToIDmappingInExpr["sc2.t2"])
+	})
 }
