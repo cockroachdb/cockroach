43f957666062373d91c5557ca2de9edfe1d096cb
Raphael 'kena' Poss <knz@thaumogen.net>
2022-05-31 14:26:53 +0200

    cli/flag: move the flag definition logic to cliflagcfg
    This ensures that the basic primitives are reusable out of package
    `cli`.
    
    Release note: None

--- pkg/cli/cliflagcfg/flags.go
+++ pkg/cli/cliflagcfg/flags.go
@@ -11,6 +11,9 @@
 package cliflagcfg
 
 import (
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/cli/cliflags"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
@@ -21,4 +24,52 @@ import (
 func FlagSetForCmd(cmd *cobra.Command) *pflag.FlagSet {
 	_ = cmd.LocalFlags() // force merge persistent+local flags
 	return cmd.Flags()
+}
+
+// StringFlag creates a string flag and registers it with the FlagSet.
+// The default value is taken from the variable pointed to by valPtr.
+// See cli/context.go to initialize defaults.
+func StringFlag(f *pflag.FlagSet, valPtr *string, flagInfo cliflags.FlagInfo) {
+	f.StringVarP(valPtr, flagInfo.Name, flagInfo.Shorthand, *valPtr, flagInfo.Usage())
+	RegisterEnvVarDefault(f, flagInfo)
+}
+
+// IntFlag creates an int flag and registers it with the FlagSet.
+// The default value is taken from the variable pointed to by valPtr.
+// See cli/context.go to initialize defaults.
+func IntFlag(f *pflag.FlagSet, valPtr *int, flagInfo cliflags.FlagInfo) {
+	f.IntVarP(valPtr, flagInfo.Name, flagInfo.Shorthand, *valPtr, flagInfo.Usage())
+	RegisterEnvVarDefault(f, flagInfo)
+}
+
+// BoolFlag creates a bool flag and registers it with the FlagSet.
+// The default value is taken from the variable pointed to by valPtr.
+// See cli/context.go to initialize defaults.
+func BoolFlag(f *pflag.FlagSet, valPtr *bool, flagInfo cliflags.FlagInfo) {
+	f.BoolVarP(valPtr, flagInfo.Name, flagInfo.Shorthand, *valPtr, flagInfo.Usage())
+	RegisterEnvVarDefault(f, flagInfo)
+}
+
+// DurationFlag creates a duration flag and registers it with the FlagSet.
+// The default value is taken from the variable pointed to by valPtr.
+// See cli/context.go to initialize defaults.
+func DurationFlag(f *pflag.FlagSet, valPtr *time.Duration, flagInfo cliflags.FlagInfo) {
+	f.DurationVarP(valPtr, flagInfo.Name, flagInfo.Shorthand, *valPtr, flagInfo.Usage())
+	RegisterEnvVarDefault(f, flagInfo)
+}
+
+// VarFlag creates a custom-variable flag and registers it with the FlagSet.
+// The default value is taken from the value's current value.
+// See cli/context.go to initialize defaults.
+func VarFlag(f *pflag.FlagSet, value pflag.Value, flagInfo cliflags.FlagInfo) {
+	f.VarP(value, flagInfo.Name, flagInfo.Shorthand, flagInfo.Usage())
+	RegisterEnvVarDefault(f, flagInfo)
+}
+
+// StringSliceFlag creates a string slice flag and registers it with the FlagSet.
+// The default value is taken from the value's current value.
+// See cli/context.go to initialize defaults.
+func StringSliceFlag(f *pflag.FlagSet, valPtr *[]string, flagInfo cliflags.FlagInfo) {
+	f.StringSliceVar(valPtr, flagInfo.Name, *valPtr, flagInfo.Usage())
+	RegisterEnvVarDefault(f, flagInfo)
 }
