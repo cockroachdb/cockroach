3fda1b2cc318f6da6d4543a12a6a2cfedba39d6d
Prashant Khoje <prashant.khoje@ibm.com>
2022-06-01 02:21:37 -0500

    ppc64le - fix geogfn and geomfn test failures
    geogfn
        - TestAzimuth/north_east
        - TestSegmentize/LINESTRING(1.0_1.0,_2.0_2.0,_3.0_3.0),_maximum_segment_length:_100000.000000
        - TestSegmentize/POLYGON((0.0_0.0,_1.0_0.0,_1.0_1.0,_0.0_0.0)),_maximum_segment_length:_100000.000000
        - TestSegmentize/POLYGON((0.0_0.0,_1.0_0.0,_1.0_1.0,_0.0_0.0),_(0.1_0.1,_0.2_0.1,_0.2_0.2,_0.1_0.1)),_maximum_segment_length:_50000.000000
        - TestSegmentize/MULTILINESTRING((1.0_1.0,_2.0_2.0,_3.0_3.0),_(6.0_6.0,_7.0_6.0)),_maximum_segment_length:_100000.000000
        - TestSegmentize/MULTILINESTRING_(EMPTY,_(1.0_1.0,_2.0_2.0,_3.0_3.0),_(6.0_6.0,_7.0_6.0)),_maximum_segment_length:_100000.000000
        - TestSegmentize/MULTIPOLYGON(((3.0_3.0,_4.0_3.0,_4.0_4.0,_3.0_3.0)),_((0.0_0.0,_1.0_0.0,_1.0_1.0,_0.0_0.0),_(0.1_0.1,_0.2_0.1,_0.2_0.2,_0.1_0.1))),_maximum_segment_length:_100000.000000
        - TestSegmentize/GEOMETRYCOLLECTION_(POINT_(40_10),LINESTRING_(10_10,_20_20,_10_40),POLYGON_((40_40,_20_45,_45_30,_40_40))),_maximum_segment_length:_1000000.000000
        - TestSegmentize/LINESTRING(0_0_25,_0_1_0,_2_5_100),_maximum_segment_length:_150000.000000
        - TestSegmentizeCoords/Coordinate(85,_85)_to_Coordinate(0,_0),_0.29502092024628396
        - TestSegmentizeCoords/Coordinate(0,_16,_23,_10)_to_Coordinate(1,_0,_-5,_0),_0.07848050723825097
        - TestProject/SRID
    
    geomfn - TestGenerateRandomPoints/Polygon_with_a_hole
    
    Cherry picks #66621
    
    Release note: None
    
    Co-authored-by: Oliver Tan <otan@cockroachlabs.com>

---
+++ pkg/geo/geotest/geotest.go
@@ -0,0 +1,86 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package geotest
+
+import (
+	"math"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/geo"
+	"github.com/cockroachdb/errors"
+	"github.com/stretchr/testify/require"
+	"github.com/twpayne/go-geom"
+)
+
+// Epsilon is the default epsilon to use in spatial tests.
+// This corresponds to ~1cm in lat/lng.
+const Epsilon = 0.000001
+
+// RequireGeographyInEpsilon and ensures the geometry shape and SRID are equal,
+// and that each coordinate is within the provided epsilon.
+func RequireGeographyInEpsilon(t *testing.T, expected, got geo.Geography, epsilon float64) {
+	expectedT, err := expected.AsGeomT()
+	require.NoError(t, err)
+	gotT, err := got.AsGeomT()
+	require.NoError(t, err)
+	RequireGeomTInEpsilon(t, expectedT, gotT, epsilon)
+}
+
+// RequireGeometryInEpsilon and ensures the geometry shape and SRID are equal,
+// and that each coordinate is within the provided epsilon.
+func RequireGeometryInEpsilon(t *testing.T, expected, got geo.Geometry, epsilon float64) {
+	expectedT, err := expected.AsGeomT()
+	require.NoError(t, err)
+	gotT, err := got.AsGeomT()
+	require.NoError(t, err)
+	RequireGeomTInEpsilon(t, expectedT, gotT, epsilon)
+}
+
+// FlatCoordsInEpsilon ensures the flat coords are within the expected epsilon.
+func FlatCoordsInEpsilon(t *testing.T, expected []float64, actual []float64, epsilon float64) {
+	require.Equal(t, len(expected), len(actual), "expected %#v, got %#v", expected, actual)
+	for i := range expected {
+		require.True(t, math.Abs(expected[i]-actual[i]) < epsilon, "expected %#v, got %#v (mismatching at position %d)", expected, actual, i)
+	}
+}
+
+// RequireGeomTInEpsilon ensures that the geom.T are equal, except for
+// coords which should be within the given epsilon.
+func RequireGeomTInEpsilon(t *testing.T, expectedT, gotT geom.T, epsilon float64) {
+	require.Equal(t, expectedT.SRID(), gotT.SRID())
+	require.Equal(t, expectedT.Layout(), gotT.Layout())
+	require.IsType(t, expectedT, gotT)
+	switch lhs := expectedT.(type) {
+	case *geom.Point, *geom.LineString:
+		FlatCoordsInEpsilon(t, expectedT.FlatCoords(), gotT.FlatCoords(), epsilon)
+	case *geom.MultiPoint, *geom.Polygon, *geom.MultiLineString:
+		require.Equal(t, expectedT.Ends(), gotT.Ends())
+		FlatCoordsInEpsilon(t, expectedT.FlatCoords(), gotT.FlatCoords(), epsilon)
+	case *geom.MultiPolygon:
+		require.Equal(t, expectedT.Ends(), gotT.Ends())
+		require.Equal(t, expectedT.Endss(), gotT.Endss())
+		FlatCoordsInEpsilon(t, expectedT.FlatCoords(), gotT.FlatCoords(), epsilon)
+	case *geom.GeometryCollection:
+		rhs, ok := gotT.(*geom.GeometryCollection)
+		require.True(t, ok)
+		require.Len(t, rhs.Geoms(), len(lhs.Geoms()))
+		for i := range lhs.Geoms() {
+			RequireGeomTInEpsilon(
+				t,
+				lhs.Geom(i),
+				rhs.Geom(i),
+				epsilon,
+			)
+		}
+	default:
+		panic(errors.AssertionFailedf("unknown geometry type: %T", expectedT))
+	}
+}
