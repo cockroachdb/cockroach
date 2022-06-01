d65fccf4423934eee886bedead31f9f7628f73d3
wenyihu3 <wenyi.hu@cockroachlabs.com>
2022-06-01 16:41:05 -0400

    indexrec: added geospatial index for spatial functions to indexrec
    indexrec: changed 2 test cases
    
    indexrec: added geospatial index support for BBox2d operations
    
    indexrec: added Box2D index to geospatial inverted index instead

--- pkg/sql/opt/indexrec/index_candidate_set.go
+++ pkg/sql/opt/indexrec/index_candidate_set.go
@@ -11,6 +11,7 @@
 package indexrec
 
 import (
+	"github.com/cockroachdb/cockroach/pkg/geo/geoindex"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/colinfo"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/cat"
@@ -165,6 +166,14 @@ func (ics *indexCandidateSet) categorizeIndexCandidates(expr opt.Expr) {
 	case *memo.ContainedByExpr:
 		ics.addVariableExprIndex(expr.Left, ics.overallCandidates)
 		ics.addVariableExprIndex(expr.Right, ics.overallCandidates)
+	case *memo.FunctionExpr:
+		ics.addGeoSpatialIndexes(expr, ics.overallCandidates)
+	case *memo.BBoxCoversExpr:
+		ics.addVariableExprIndex(expr.Left, ics.overallCandidates)
+		ics.addVariableExprIndex(expr.Right, ics.overallCandidates)
+	case *memo.BBoxIntersectsExpr:
+		ics.addVariableExprIndex(expr.Left, ics.overallCandidates)
+		ics.addVariableExprIndex(expr.Right, ics.overallCandidates)
 	}
 	for i, n := 0, expr.ChildCount(); i < n; i++ {
 		ics.categorizeIndexCandidates(expr.Child(i))
@@ -300,10 +309,12 @@ func (ics *indexCandidateSet) addVariableExprIndex(
 	switch expr := expr.(type) {
 	case *memo.VariableExpr:
 		col := expr.Col
-		if colinfo.ColumnTypeIsIndexable(ics.md.ColumnMeta(col).Type) {
-			ics.addSingleColumnIndex(col, false /* desc */, indexCandidates)
-		} else {
+		// Note: Box2D geospatial indexes is added to inverted candidates as a special case
+		// because ColumnTypeIsInvertedIndexable returns false for it
+		if (!colinfo.ColumnTypeIsIndexable(ics.md.ColumnMeta(col).Type)) || (expr.DataType().Family() == types.Box2DFamily) {
 			ics.addSingleColumnIndex(col, false /* desc */, ics.invertedCandidates)
+		} else {
+			ics.addSingleColumnIndex(col, false /* desc */, indexCandidates)
 		}
 	}
 }
@@ -379,15 +390,6 @@ func addIndexToCandidates(
 		return
 	}
 
-	// Do not add indexes on spatial columns.
-	// TODO(rytaft): Support spatial predicates like st_contains() etc.
-	for _, indexCol := range newIndex {
-		colFamily := indexCol.Column.DatumType().Family()
-		if colFamily == types.GeometryFamily || colFamily == types.GeographyFamily {
-			return
-		}
-	}
-
 	// Do not add duplicate indexes.
 	for _, existingIndex := range indexCandidates[currTable] {
 		if len(existingIndex) != len(newIndex) {
@@ -407,4 +409,21 @@ func addIndexToCandidates(
 	}
 	// Index does not exist already, so add it.
 	indexCandidates[currTable] = append(indexCandidates[currTable], newIndex)
+}
+
+// AddGeoSpatialIndexes is used to add single-column GIN indexes to inverted candidates for
+// spatial functions that can be index-accelerated.
+func (ics *indexCandidateSet) addGeoSpatialIndexes(expr opt.Expr, indexCandidates map[cat.Table][][]cat.IndexColumn) {
+	switch expr := expr.(type) {
+	case *memo.FunctionExpr:
+		// Ensure that the function is a spatial function AND can be index-accelerated
+		_, ok := geoindex.RelationshipMap[expr.Name]
+		if ok {
+			// Add arguments of the spatial function to inverted indexes
+			for i := range expr.Args {
+				var child = expr.Args.Child(i)
+				ics.addVariableExprIndex(child, indexCandidates)
+			}
+		}
+	}
 }
