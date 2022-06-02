d18bec4a95e16fcf293c89d7acbbb37575d6d9ca
Andrew Werner <awerner32@gmail.com>
2022-06-02 19:02:18 -0400

    changefeedccl/schemafeed: add datadriven test
    This change also refactors some of the event classifications
    
    Release note: None

---
+++ pkg/ccl/changefeedccl/schemafeed/helpers_test.go
@@ -0,0 +1,37 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package schemafeed
+
+import "strings"
+
+const TestingAllEventFilter = "testing"
+
+func init() {
+	schemaChangeEventFilters[TestingAllEventFilter] = tableEventFilter{
+		tableEventDropColumn:                  false,
+		tableEventAddColumnWithBackfill:       false,
+		tableEventAddColumnNoBackfill:         false,
+		tableEventUnknown:                     false,
+		tableEventPrimaryKeyChange:            false,
+		tableEventLocalityRegionalByRowChange: false,
+		tableEventAddHiddenColumn:             false,
+	}
+}
+
+var ClassifyEvent = classifyTableEvent
+
+func PrintTableEventType(t tableEventType) string {
+	var strs []string
+	for i := 0; i < 63; i++ {
+		if t&1<<i != 0 {
+			strs = append(strs, tableEventType(1<<i).String())
+		}
+	}
+	return strings.Join(strs, "|")
+}
