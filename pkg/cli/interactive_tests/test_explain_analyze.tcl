f548996e264f4dd9e3c7db2ea97a986bd1cb80fb
Rafi Shamim <rafi@cockroachlabs.com>
2022-06-07 10:14:06 -0400

    cli,sql: fix reported result columns for EXPLAIN ANALYZE
    This fixes an issue where EXPLAIN ANALYZE would report a RowDescription
    for both the EXPLAIN and for the statement being explained. If the
    statement had a different number of result columns, this would confuse
    the CLI.
    
    No release note, since this bug was not released.
    
    Release note: None

---
+++ pkg/cli/interactive_tests/test_explain_analyze.tcl
@@ -0,0 +1,26 @@
+#! /usr/bin/env expect -f
+
+source [file join [file dirname $argv0] common.tcl]
+
+start_server $argv
+
+start_test "Ensure that EXPLAIN ANALYZE works as expected in the sql shell"
+
+# Spawn a sql shell.
+spawn $argv sql
+set client_spawn_id $spawn_id
+eexpect root@
+
+# Check for a regression where the CLI would get confused when the statement
+# had a different number of result columns.
+send "EXPLAIN ANALYZE SELECT 1,2;\r"
+eexpect "info"
+eexpect "planning time"
+eexpect "actual row count"
+
+send_eof
+eexpect eof
+
+end_test
+
+stop_server $argv
