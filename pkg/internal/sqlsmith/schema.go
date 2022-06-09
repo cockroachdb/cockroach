6900fe209ee4ff0c39b5610fa6beb06523e0da5f
Yahor Yuzefovich <yahor@cockroachlabs.com>
2022-06-09 07:41:44 -0700

    sqlsmith: skip three builtins
    This commit skips using three builtins with
    `crdb_internal.revalidate_unique_constraint` prefix.
    
    Release note: None

--- pkg/internal/sqlsmith/schema.go
+++ pkg/internal/sqlsmith/schema.go
@@ -496,7 +496,8 @@ var functions = func() map[tree.FunctionClass]map[oid.Oid][]function {
 		if strings.Contains(def.Name, "crdb_internal.force_") ||
 			strings.Contains(def.Name, "crdb_internal.unsafe_") ||
 			strings.Contains(def.Name, "crdb_internal.create_join_token") ||
-			strings.Contains(def.Name, "crdb_internal.reset_multi_region_zone_configs_for_database") {
+			strings.Contains(def.Name, "crdb_internal.reset_multi_region_zone_configs_for_database")
+      strings.Contains(def.Name, "crdb_internal.revalidate_unique_constraint") {
 			continue
 		}
 		if _, ok := m[def.Class]; !ok {
