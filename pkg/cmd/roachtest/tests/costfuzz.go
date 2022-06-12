d34a6d55652aa35448a9b170ab651550c836678e
Mark Sirek <sirek@cockroachlabs.com>
2022-06-11 23:31:29 -0700

    sqlsmith: add DisableIndexHints option
    This commit adds a DisableIndexHints sqlsmith option which causes the
    Smither to avoid generation of index hints.
    
    Release note: none

--- pkg/cmd/roachtest/tests/costfuzz.go
+++ pkg/cmd/roachtest/tests/costfuzz.go
@@ -165,6 +165,7 @@ func runOneRoundCostFuzz(
 		sqlsmith.DisableMutations(), sqlsmith.DisableImpureFns(), sqlsmith.DisableLimits(),
 		sqlsmith.DisableConstantWhereClause(), sqlsmith.FavorInterestingData(),
 		sqlsmith.UnlikelyRandomNulls(), sqlsmith.DisableCrossJoins(),
+		sqlsmith.DisableIndexHints(),
 		sqlsmith.SetComplexity(.8),
 		sqlsmith.SetScalarComplexity(.3),
 	)
