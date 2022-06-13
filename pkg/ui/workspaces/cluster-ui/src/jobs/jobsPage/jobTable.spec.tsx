a946b2db0667f9f9064a51ce6948757d372a1c5f
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2022-06-13 14:50:05 -0400

    ui: refactor assertions in *.spec.tsx files
    When I ran jest-codemods before I only included `*.spec.ts` files. This
    change includes `*.spec.tsx` files with a single hand correction.
    
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/jobs/jobsPage/jobTable.spec.tsx
+++ pkg/ui/workspaces/cluster-ui/src/jobs/jobsPage/jobTable.spec.tsx
@@ -8,7 +8,6 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 import React from "react";
-import { assert } from "chai";
 import { shallow } from "enzyme";
 import { earliestRetainedTime } from "./jobsPage.fixture";
 import { JobTable, JobTableProps } from "./jobTable";
@@ -20,7 +19,7 @@ describe("<JobTable>", () => {
     };
     const jobTableProps: JobTableProps = {
       sort: { columnTitle: null, ascending: true },
-      setSort: () => {},
+      setSort: () => { },
       jobs: {
         jobs: [{}, {}, {}, {}],
         earliest_retained_time: earliestRetainedTime,
@@ -40,7 +39,7 @@ describe("<JobTable>", () => {
         isUsedFilter={jobTableProps.isUsedFilter}
       />,
     );
-    assert.equal(jobTable.state().pagination.current, 2);
+    expect(jobTable.state().pagination.current).toEqual(2);
     jobTable.setProps({
       jobs: {
         jobs: [{}, {}],
@@ -48,6 +47,6 @@ describe("<JobTable>", () => {
         toJSON: toJSON,
       },
     });
-    assert.equal(jobTable.state().pagination.current, 1);
+    expect(jobTable.state().pagination.current).toEqual(1);
   });
 });
