f1725f0d8ac034e86b440d52bbee383e91e63da4
Rail Aliiev <rail@iqchoice.com>
2022-06-13 08:40:44 -0400

    Upgrade Go to version 1.17.11
    * [x] Adjust version in Docker image
    * [x] Adjust version in the TeamCity agent image
    * [x] Rebuild and push the Docker image
    * [x] Download ALL the archives (`.tar.gz`, `.zip`) for the new Go
      version from https://golang.org/dl/ and mirror them in the
      `public-bazel-artifacts` bucket in the `Bazel artifacts` project in
      GCP (sub-directory `go`, next to the other Go SDK's).
    * [x] Bump the version in `WORKSPACE` under `go_download_sdk`. You may
      need to bump
      [rules_go](https://github.com/bazelbuild/rules_go/releases). Also edit
      the filenames listed in `sdks` and update all the hashes to match what
      you mirrored in the step above.
    * [x] Run `./dev generate bazel` to refresh `distdir_files.bzl`, then
      `bazel fetch @distdir//:archives` to ensure you've updated all hashes
      to the correct value.
    * [x] Bump the version in `builder.sh` accordingly
    * [x] Bump the default installed version of Go in `bootstrap-debian.sh`
    * [ ] Update the `builder.dockerImage` parameter in the TeamCity
      [`Cockroach`](https://teamcity.cockroachdb.com/admin/editProject.html?projectId=Cockroach&tab=projectParams)
      and
      [`Internal`](https://teamcity.cockroachdb.com/admin/editProject.html?projectId=Internal&tab=projectParams)
      projects.
    * [ ] Deploy new TeamCity agent images
    
    Partially addresses #82808
    
    Release note: None

--- build/packer/teamcity-agent.sh
+++ build/packer/teamcity-agent.sh
@@ -53,14 +53,15 @@ EOF
 tar --strip-components=1 -C /usr -xzf /tmp/cmake.tar.gz
 rm -f /tmp/cmake.tar.gz
 
-curl -fsSL https://dl.google.com/go/go1.17.10.linux-amd64.tar.gz > /tmp/go.tgz
+curl -fsSL https://dl.google.com/go/go1.17.11.linux-amd64.tar.gz > /tmp/go.tgz
 sha256sum -c - <<EOF
-87fc728c9c731e2f74e4a999ef53cf07302d7ed3504b0839027bd9c10edaa3fd /tmp/go.tgz
+d69a4fe2694f795d8e525c72b497ededc209cb7185f4c3b62d7a98dd6227b3fe  /tmp/go.tgz
 EOF
 tar -C /usr/local -zxf /tmp/go.tgz && rm /tmp/go.tgz
 
 # Install the older version in parallel in order to run the acceptance test on older branches
-# TODO: Remove this when 21.1 is EOL
+# TODO: Remove this when 21.1 is EOL (2022-11-18, according to
+# https://www.cockroachlabs.com/docs/releases/release-support-policy.html)
 curl -fsSL https://dl.google.com/go/go1.15.14.linux-amd64.tar.gz > /tmp/go_old.tgz
 sha256sum -c - <<EOF
 6f5410c113b803f437d7a1ee6f8f124100e536cc7361920f7e640fedf7add72d /tmp/go_old.tgz
