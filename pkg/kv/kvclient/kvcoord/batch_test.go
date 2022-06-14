91c897172cc0e6e3db8fa5208a9d4ecd85965ebf
Yahor Yuzefovich <yahor@cockroachlabs.com>
2022-06-13 19:19:11 -0700

    kvcoord: introduce batch truncation helper
    This commit introduces a batch truncation helper that encompasses logic
    of truncating requests to the boundaries of a single range as well as
    returning the next key to seek the range iterator to. The helper is now
    used both in the DistSender as well as in the Streamer. No modification
    to the actual logic of `Truncate` nor `Next`/`prev` functions has been
    done other than incorporating the return of the next seek key into
    `Truncate` function itself. This is needed since the following commit
    will tightly couple the truncation process with the next seek key
    determination in order to optimize it.
    
    The helper can be configured with a knob indicating whether `Truncate`
    needs to return requests in the original order. This behavior is
    necessary by the BatchRequests that contain writes since in several
    spots we rely on the ordering assumptions (e.g. of increasing values of
    `Sequence`).
    
    The following adjustments were made to the tests:
    - `BenchmarkTruncate` has been renamed to `BenchmarkTruncateLegacy`
    - `TestTruncate` has been refactored to exercise the new and the old
    code-paths
    - `TestBatchPrevNext` has been refactored to run through the new code
    path, also a few test cases have been adjusted slightly.
    
    This commit also introduces some unit tests for the new code path when
    it runs in a loop over multiple ranges as well as a corresponding
    benchmark.
    
    Release note: None

--- pkg/kv/kvclient/kvcoord/batch_test.go
+++ pkg/kv/kvclient/kvcoord/batch_test.go
@@ -13,7 +13,10 @@ package kvcoord
 import (
 	"bytes"
 	"fmt"
+	"math/rand"
 	"reflect"
+	"sort"
+	"strings"
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/keys"
@@ -27,7 +30,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TestBatchPrevNext tests prev() and next()
+// TestBatchPrevNext tests the seeking behavior of the
+// BatchTruncationHelper.Truncate.
 func TestBatchPrevNext(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
@@ -154,23 +158,27 @@ func TestBatchPrevNext(t *testing.T) {
 			expBW: "c\x00",
 		},
 		// Explanations below are an exercise for the reader, but it's very similar to above.
-		{spans: span(loc("b"), ""), key: "a",
+		{spans: span(loc("b"), loc("c")), key: "a",
 			expFW: "b",
 			expBW: min,
 		},
-		{spans: span(loc("b"), ""), key: "b",
+		{spans: span(loc("b"), loc("c")), key: "b",
 			expFW: "b",
 			expBW: min,
 		},
-		{spans: span(loc("b"), ""), key: "b\x00",
+		// Note that such a span results in an invalid ScanRequest, but it's ok
+		// for the purposes of this test.
+		{spans: span(loc("b"), loc("b")), key: "b\x00",
 			expFW: max,
 			// Handled `key >= b\x00`, so next we'll have to chip away at `[KeyMin, b\x00)`. Note
 			// how this doesn't return `b` which would be incorrect as `[KeyMin, b)` does not
 			// contain `loc(b)`.
 			expBW: "b\x00",
 		},
+		// Note that such a span results in an invalid ScanRequest, but it's ok
+		// for the purposes of this test.
 		{
-			spans: span(loc("a"), "", loc("b"), ""), key: "b",
+			spans: span(loc("a"), loc("a"), loc("b"), loc("b")), key: "b",
 			// We've dealt with any key that addresses to `< b`, and `loc(b)` is not
 			// covered by it. `loc(b)` lives between `b` and `b\x00`, so we start at
 			// `b`.
@@ -200,12 +208,25 @@ func TestBatchPrevNext(t *testing.T) {
 				args.Key, args.EndKey = span.Key, span.EndKey
 				ba.Add(args)
 			}
-			if next, err := Next(ba.Requests, roachpb.RKey(test.key)); err != nil {
+			const mustPreserveOrder = false
+			ascHelper, err := MakeBatchTruncationHelper(Ascending, ba.Requests, mustPreserveOrder)
+			require.NoError(t, err)
+			descHelper, err := MakeBatchTruncationHelper(Descending, ba.Requests, mustPreserveOrder)
+			require.NoError(t, err)
+			if _, _, next, err := ascHelper.Truncate(
+				roachpb.RSpan{
+					Key:    roachpb.RKeyMin,
+					EndKey: roachpb.RKey(test.key)},
+			); err != nil {
 				t.Error(err)
 			} else if !bytes.Equal(next, roachpb.Key(test.expFW)) {
 				t.Errorf("next: expected %q, got %q", test.expFW, next)
 			}
-			if prev, err := prev(ba.Requests, roachpb.RKey(test.key)); err != nil {
+			if _, _, prev, err := descHelper.Truncate(
+				roachpb.RSpan{
+					Key:    roachpb.RKey(test.key),
+					EndKey: roachpb.RKeyMax},
+			); err != nil {
 				t.Error(err)
 			} else if !bytes.Equal(prev, roachpb.Key(test.expBW)) {
 				t.Errorf("prev: expected %q, got %q", test.expBW, prev)
@@ -214,6 +235,28 @@ func TestBatchPrevNext(t *testing.T) {
 	}
 }
 
+type requestsWithPositions struct {
+	reqs      []roachpb.RequestUnion
+	positions []int
+}
+
+var _ sort.Interface = &requestsWithPositions{}
+
+func (r *requestsWithPositions) Len() int {
+	return len(r.positions)
+}
+
+func (r *requestsWithPositions) Less(i, j int) bool {
+	return r.positions[i] < r.positions[j]
+}
+
+func (r *requestsWithPositions) Swap(i, j int) {
+	r.reqs[i], r.reqs[j] = r.reqs[j], r.reqs[i]
+	r.positions[i], r.positions[j] = r.positions[j], r.positions[i]
+}
+
+// TestTruncate verifies the truncation logic of the BatchTruncationHelper over
+// a single range as well as truncateLegacy() function directly.
 func TestTruncate(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
@@ -316,72 +359,321 @@ func TestTruncate(t *testing.T) {
 			desc: [2]string{"a", "z"},
 		},
 	}
-
-	for i, test := range testCases {
-		goldenOriginal := roachpb.BatchRequest{}
-		for _, ks := range test.keys {
-			if len(ks[1]) > 0 {
-				goldenOriginal.Add(&roachpb.ResolveIntentRangeRequest{
-					RequestHeader: roachpb.RequestHeader{
-						Key: roachpb.Key(ks[0]), EndKey: roachpb.Key(ks[1]),
-					},
-					IntentTxn: enginepb.TxnMeta{ID: uuid.MakeV4()},
-				})
-			} else {
-				goldenOriginal.Add(&roachpb.GetRequest{
-					RequestHeader: roachpb.RequestHeader{Key: roachpb.Key(ks[0])},
-				})
+	for _, isLegacy := range []bool{false, true} {
+		for _, mustPreserveOrder := range []bool{false, true} {
+			if isLegacy && mustPreserveOrder {
+				// This config is meaningless because truncateLegacy() always
+				// preserves the ordering.
+				continue
 			}
-		}
+			for i, test := range testCases {
+				goldenOriginal := roachpb.BatchRequest{}
+				for _, ks := range test.keys {
+					if len(ks[1]) > 0 {
+						goldenOriginal.Add(&roachpb.ResolveIntentRangeRequest{
+							RequestHeader: roachpb.RequestHeader{
+								Key: roachpb.Key(ks[0]), EndKey: roachpb.Key(ks[1]),
+							},
+							IntentTxn: enginepb.TxnMeta{ID: uuid.MakeV4()},
+						})
+					} else {
+						goldenOriginal.Add(&roachpb.GetRequest{
+							RequestHeader: roachpb.RequestHeader{Key: roachpb.Key(ks[0])},
+						})
+					}
+				}
 
-		original := roachpb.BatchRequest{Requests: make([]roachpb.RequestUnion, len(goldenOriginal.Requests))}
-		for i, request := range goldenOriginal.Requests {
-			original.Requests[i].MustSetInner(request.GetInner().ShallowCopy())
-		}
+				original := roachpb.BatchRequest{Requests: make([]roachpb.RequestUnion, len(goldenOriginal.Requests))}
+				for i, request := range goldenOriginal.Requests {
+					original.Requests[i].MustSetInner(request.GetInner().ShallowCopy())
+				}
 
-		desc := &roachpb.RangeDescriptor{
-			StartKey: roachpb.RKey(test.desc[0]), EndKey: roachpb.RKey(test.desc[1]),
-		}
-		if len(desc.StartKey) == 0 {
-			desc.StartKey = roachpb.RKey(test.from)
-		}
-		if len(desc.EndKey) == 0 {
-			desc.EndKey = roachpb.RKey(test.to)
-		}
-		rs := roachpb.RSpan{Key: roachpb.RKey(test.from), EndKey: roachpb.RKey(test.to)}
-		rs, err := rs.Intersect(desc)
-		if err != nil {
-			t.Errorf("%d: intersection failure: %v", i, err)
-			continue
-		}
-		reqs, pos, err := Truncate(original.Requests, rs)
-		if err != nil || test.err != "" {
-			if !testutils.IsError(err, test.err) {
-				t.Errorf("%d: %v (expected: %q)", i, err, test.err)
+				var truncationHelper BatchTruncationHelper
+				if !isLegacy {
+					var err error
+					truncationHelper, err = MakeBatchTruncationHelper(Ascending, original.Requests, mustPreserveOrder)
+					if err != nil {
+						t.Errorf("%d: Init failure: %v", i, err)
+						continue
+					}
+					// We need to truncate all requests up to the start of the
+					// test range since this is assumed by Truncate().
+					truncateKey := roachpb.RKey(test.from)
+					if truncateKey.Less(roachpb.RKey(test.desc[0])) {
+						truncateKey = roachpb.RKey(test.desc[0])
+					}
+					_, _, _, err = truncationHelper.Truncate(
+						roachpb.RSpan{Key: roachpb.RKeyMin, EndKey: truncateKey},
+					)
+					if err != nil || test.err != "" {
+						if !testutils.IsError(err, test.err) {
+							t.Errorf("%d: %v (expected: %q)", i, err, test.err)
+						}
+						continue
+					}
+				}
+				desc := &roachpb.RangeDescriptor{
+					StartKey: roachpb.RKey(test.desc[0]), EndKey: roachpb.RKey(test.desc[1]),
+				}
+				if len(desc.StartKey) == 0 {
+					desc.StartKey = roachpb.RKey(test.from)
+				}
+				if len(desc.EndKey) == 0 {
+					desc.EndKey = roachpb.RKey(test.to)
+				}
+				rs := roachpb.RSpan{Key: roachpb.RKey(test.from), EndKey: roachpb.RKey(test.to)}
+				rs, err := rs.Intersect(desc)
+				if err != nil {
+					t.Errorf("%d: intersection failure: %v", i, err)
+					continue
+				}
+				reqs, pos, err := truncateLegacy(original.Requests, rs)
+				if isLegacy {
+					if err != nil || test.err != "" {
+						if !testutils.IsError(err, test.err) {
+							t.Errorf("%d: %v (expected: %q)", i, err, test.err)
+						}
+						continue
+					}
+				} else {
+					reqs, pos, _, err = truncationHelper.Truncate(rs)
+				}
+				if err != nil {
+					t.Errorf("%d: truncation failure: %v", i, err)
+					continue
+				}
+				if !isLegacy && !mustPreserveOrder {
+					// Truncate can return results in an arbitrary order, so we
+					// need to restore the order according to positions.
+					scratch := &requestsWithPositions{reqs: reqs, positions: pos}
+					sort.Sort(scratch)
+				}
+				var numReqs int
+				for j, arg := range reqs {
+					req := arg.GetInner()
+					if h := req.Header(); !bytes.Equal(h.Key, roachpb.Key(test.expKeys[j][0])) || !bytes.Equal(h.EndKey, roachpb.Key(test.expKeys[j][1])) {
+						t.Errorf("%d.%d: range mismatch: actual [%q,%q), wanted [%q,%q)", i, j,
+							h.Key, h.EndKey, roachpb.RKey(test.expKeys[j][0]), roachpb.RKey(test.expKeys[j][1]))
+					} else if len(h.Key) != 0 {
+						numReqs++
+					}
+				}
+				if num := len(pos); numReqs != num {
+					t.Errorf("%d: counted %d requests, but truncation indicated %d", i, numReqs, num)
+				}
+				if !reflect.DeepEqual(original, goldenOriginal) {
+					t.Errorf("%d: truncation mutated original:\nexpected: %s\nactual: %s",
+						i, goldenOriginal, original)
+				}
 			}
-			continue
-		}
-		var numReqs int
-		for j, arg := range reqs {
-			req := arg.GetInner()
-			if h := req.Header(); !bytes.Equal(h.Key, roachpb.Key(test.expKeys[j][0])) || !bytes.Equal(h.EndKey, roachpb.Key(test.expKeys[j][1])) {
-				t.Errorf("%d.%d: range mismatch: actual [%q,%q), wanted [%q,%q)", i, j,
-					h.Key, h.EndKey, test.expKeys[j][0], test.expKeys[j][1])
-			} else if len(h.Key) != 0 {
-				numReqs++
-			}
-		}
-		if num := len(pos); numReqs != num {
-			t.Errorf("%d: counted %d requests, but truncation indicated %d", i, reqs, num)
-		}
-		if !reflect.DeepEqual(original, goldenOriginal) {
-			t.Errorf("%d: truncation mutated original:\nexpected: %s\nactual: %s",
-				i, goldenOriginal, original)
 		}
 	}
 }
 
-func BenchmarkTruncate(b *testing.B) {
+// TestTruncateLoop verifies that using the BatchTruncationHelper in a loop over
+// multiple ranges works as expected.
+func TestTruncateLoop(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	makeGetRequest := func(key string) roachpb.RequestUnion {
+		var req roachpb.RequestUnion
+		req.MustSetInner(&roachpb.GetRequest{
+			RequestHeader: roachpb.RequestHeader{Key: roachpb.Key(key)},
+		})
+		return req
+	}
+	makeScanRequest := func(start, end string) roachpb.RequestUnion {
+		var req roachpb.RequestUnion
+		req.MustSetInner(&roachpb.ScanRequest{
+			RequestHeader: roachpb.RequestHeader{
+				Key: roachpb.Key(start), EndKey: roachpb.Key(end),
+			},
+		})
+		return req
+	}
+	rng, _ := randutil.NewTestRand()
+	const maxKeyLength = 8
+	randomKey := func() string {
+		keyLength := rng.Intn(maxKeyLength) + 1
+		return string(randomKey(rng, keyLength))
+	}
+
+	makeRandomTestCase := func() ([]roachpb.RequestUnion, []roachpb.RSpan) {
+		var requests []roachpb.RequestUnion
+		numRequests := rng.Intn(20) + 1
+		for i := 0; i < numRequests; i++ {
+			if rng.Float64() < 0.5 {
+				// With 50% chance, use a Get request; otherwise, a Scan
+				// request.
+				requests = append(requests, makeGetRequest(randomKey()))
+			} else {
+				req := [2]string{randomKey(), randomKey()}
+				for strings.Compare(req[0], req[1]) >= 0 {
+					req[1] = randomKey()
+				}
+				requests = append(requests, makeScanRequest(req[0], req[1]))
+			}
+		}
+
+		numRanges := rng.Intn(5) + 1
+		return requests, makeRanges(rng, numRanges, maxKeyLength)
+	}
+
+	for numRuns := 0; numRuns < 100; numRuns++ {
+		requests, ranges := makeRandomTestCase()
+		for _, scanDir := range []ScanDirection{Ascending, Descending} {
+			for _, mustPreserveOrder := range []bool{false, true} {
+				t.Run(fmt.Sprintf("run=%d/%s/order=%t", numRuns, scanDir, mustPreserveOrder), func(t *testing.T) {
+					helper, err := MakeBatchTruncationHelper(scanDir, requests, mustPreserveOrder)
+					require.NoError(t, err)
+					for i := 0; i < len(ranges); i++ {
+						curRangeRS := ranges[i]
+						if scanDir == Descending {
+							// Test cases are written assuming the Ascending
+							// scan direction, so we have to iterate in reverse
+							// here.
+							curRangeRS = ranges[len(ranges)-1-i]
+						}
+						actualReqs, actualPositions, actualSeekKey, err := helper.Truncate(curRangeRS)
+						require.NoError(t, err)
+						expectedReqs, expectedPositions, err := truncateLegacy(requests, curRangeRS)
+						require.NoError(t, err)
+						if !mustPreserveOrder {
+							// Since requests can be truncated in an arbitrary
+							// order, we will sort them according to their
+							// positions.
+							scratch := requestsWithPositions{reqs: actualReqs, positions: actualPositions}
+							sort.Sort(&scratch)
+						}
+						require.True(t, reflect.DeepEqual(expectedReqs, actualReqs))
+						require.True(t, reflect.DeepEqual(expectedPositions, actualPositions))
+						var expectedSeekKey roachpb.RKey
+						if scanDir == Ascending {
+							expectedSeekKey, err = nextLegacy(requests, curRangeRS.EndKey)
+						} else {
+							expectedSeekKey, err = prevLegacy(requests, curRangeRS.Key)
+						}
+						require.NoError(t, err)
+						require.Equal(t, expectedSeekKey, actualSeekKey)
+					}
+				})
+			}
+		}
+	}
+}
+
+func randomKeyByte(rng *rand.Rand) byte {
+	// Plus two is needed to skip local key space.
+	return byte(2 + rng.Intn(253))
+}
+
+func randomKey(rng *rand.Rand, keyLength int) []byte {
+	res := make([]byte, keyLength)
+	for i := range res {
+		res[i] = randomKeyByte(rng)
+	}
+	return res
+}
+
+// makeRanges returns a set of spans that describes the specified number of
+// ranges spanning the whole key space. The returned spans are ordered by the
+// start key and are non-overlapping.
+func makeRanges(rng *rand.Rand, numRanges int, keyLength int) []roachpb.RSpan {
+	splitPoints := make([][]byte, numRanges-1)
+	for i := range splitPoints {
+		splitPoints[i] = randomKey(rng, keyLength)
+	}
+	// Sort all the split points so that we can treat them as boundaries of
+	// consecutive ranges.
+	for i := range splitPoints {
+		for j := i + 1; j < len(splitPoints); j++ {
+			if bytes.Compare(splitPoints[i], splitPoints[j]) > 0 {
+				splitPoints[i], splitPoints[j] = splitPoints[j], splitPoints[i]
+			}
+		}
+	}
+	rangeSpans := make([]roachpb.RSpan, numRanges)
+	rangeSpans[0].Key = roachpb.RKeyMin
+	rangeSpans[numRanges-1].EndKey = roachpb.RKeyMax
+	for i := range splitPoints {
+		rangeSpans[i].EndKey = splitPoints[i]
+		rangeSpans[i+1].Key = splitPoints[i]
+	}
+	return rangeSpans
+}
+
+func BenchmarkTruncateLoop(b *testing.B) {
+	defer leaktest.AfterTest(b)()
+	defer log.Scope(b).Close(b)
+
+	rng, _ := randutil.NewTestRand()
+	const keyLength = 8
+	for _, scanDir := range []ScanDirection{Ascending, Descending} {
+		for _, mustPreserveOrder := range []bool{false, true} {
+			for _, numRequests := range []int{128, 16384} {
+				for _, numRanges := range []int{4, 64} {
+					// We'll split the whole key space into numRanges ranges
+					// using random numRanges-1 split points.
+					rangeSpans := makeRanges(rng, numRanges, keyLength)
+					if scanDir == Descending {
+						// Reverse all the range spans for the Descending scan
+						// direction.
+						for i, j := 0, numRanges-1; i < j; i, j = i+1, j-1 {
+							rangeSpans[i], rangeSpans[j] = rangeSpans[j], rangeSpans[i]
+						}
+					}
+					var orderStr string
+					if mustPreserveOrder {
+						orderStr = "/preserveOrder"
+					}
+					for _, requestType := range []string{"get", "scan"} {
+						b.Run(fmt.Sprintf(
+							"%s%s/reqs=%d/ranges=%d/type=%s",
+							scanDir, orderStr, numRequests, numRanges, requestType,
+						), func(b *testing.B) {
+							reqs := make([]roachpb.RequestUnion, numRequests)
+							switch requestType {
+							case "get":
+								for i := 0; i < numRequests; i++ {
+									var get roachpb.GetRequest
+									get.Key = randomKey(rng, keyLength)
+									reqs[i].MustSetInner(&get)
+								}
+							case "scan":
+								for i := 0; i < numRequests; i++ {
+									var scan roachpb.ScanRequest
+									startKey := randomKey(rng, keyLength)
+									endKey := randomKey(rng, keyLength)
+									for bytes.Equal(startKey, endKey) {
+										endKey = randomKey(rng, keyLength)
+									}
+									if bytes.Compare(startKey, endKey) > 0 {
+										startKey, endKey = endKey, startKey
+									}
+									scan.Key = startKey
+									scan.EndKey = endKey
+									reqs[i].MustSetInner(&scan)
+								}
+							}
+							b.ResetTimer()
+							for i := 0; i < b.N; i++ {
+								h, err := MakeBatchTruncationHelper(scanDir, reqs, mustPreserveOrder)
+								require.NoError(b, err)
+								for _, rs := range rangeSpans {
+									_, _, _, err := h.Truncate(rs)
+									require.NoError(b, err)
+								}
+							}
+						})
+					}
+				}
+			}
+		}
+	}
+}
+
+func BenchmarkTruncateLegacy(b *testing.B) {
 	defer leaktest.AfterTest(b)()
 	defer log.Scope(b).Close(b)
 
@@ -390,10 +682,6 @@ func BenchmarkTruncate(b *testing.B) {
 	// single-byte key space into three parts, and we'll be truncating the
 	// requests according to the middle part.
 	rs := roachpb.RSpan{Key: roachpb.RKey([]byte{85}), EndKey: roachpb.RKey([]byte{170})}
-	randomKeyByte := func() byte {
-		// Plus two is needed to skip local key space.
-		return byte(2 + rng.Intn(253))
-	}
 	for _, numRequests := range []int{1 << 5, 1 << 10, 1 << 15} {
 		for _, requestType := range []string{"get", "scan"} {
 			b.Run(fmt.Sprintf("reqs=%d/type=%s", numRequests, requestType), func(b *testing.B) {
@@ -402,14 +690,14 @@ func BenchmarkTruncate(b *testing.B) {
 				case "get":
 					for i := 0; i < numRequests; i++ {
 						var get roachpb.GetRequest
-						get.Key = []byte{randomKeyByte()}
+						get.Key = []byte{randomKeyByte(rng)}
 						reqs[i].MustSetInner(&get)
 					}
 				case "scan":
 					for i := 0; i < numRequests; i++ {
 						var scan roachpb.ScanRequest
-						startKey := randomKeyByte()
-						endKey := randomKeyByte()
+						startKey := randomKeyByte(rng)
+						endKey := randomKeyByte(rng)
 						if endKey < startKey {
 							startKey, endKey = endKey, startKey
 						}
@@ -420,7 +708,7 @@ func BenchmarkTruncate(b *testing.B) {
 				}
 				b.ResetTimer()
 				for i := 0; i < b.N; i++ {
-					_, _, err := Truncate(reqs, rs)
+					_, _, err := truncateLegacy(reqs, rs)
 					require.NoError(b, err)
 				}
 			})
