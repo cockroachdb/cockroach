37da8c2d2961c0b7ec077b87906356df6d52cbe1
Yahor Yuzefovich <yahor@cockroachlabs.com>
2022-06-21 10:57:22 -0700

    sqlsmith: skip crdb_internal.request_statement_bundle
    Release note: None

--- pkg/internal/sqlsmith/schema.go
+++ pkg/internal/sqlsmith/schema.go
@@ -505,6 +505,7 @@ var functions = func() map[tree.FunctionClass]map[oid.Oid][]function {
 			"crdb_internal.replication_stream_progress",
 			"crdb_internal.complete_replication_stream",
 			"crdb_internal.revalidate_unique_constraint",
+			"crdb_internal.request_statement_bundle",
 		} {
 			skip = skip || strings.Contains(def.Name, substr)
 		}
