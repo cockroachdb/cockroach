76f5a0921fe2731e7a12e5958bbc0af3a7004a0b
Steven Danna <danna@cockroachlabs.com>
2022-06-21 20:30:02 +0100

    backupccl: make checkpoint interval configurable
    This PR makes the interval between checkpoints configurable and also
    excludes the processing time of the checkpoint itself from that
    interval.
    
    The goal of this change is to potentially address issues we've seen in
    large clusters that we currently believe can be attributed to the
    backup process slowing down substantially once it takes a minute or
    longer to marshall, compress, and write the progress checkpoint.
    
    Release note (ops change): A new setting
    `bulkio.backup.checkpoint_interval` controls the minimum interval
    between writes of progress checkpoints to external storage.

--- pkg/ccl/backupccl/backup_job.go
+++ pkg/ccl/backupccl/backup_job.go
@@ -35,6 +35,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/scheduledjobs"
 	"github.com/cockroachdb/cockroach/pkg/security/username"
 	"github.com/cockroachdb/cockroach/pkg/server/telemetry"
+	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
@@ -57,18 +58,11 @@ import (
 
 // BackupCheckpointInterval is the interval at which backup progress is saved
 // to durable storage.
-var BackupCheckpointInterval = time.Minute
-
-// TestingShortBackupCheckpointInterval sets the BackupCheckpointInterval
-// to a shorter interval for testing purposes, so we can see multiple
-// checkpoints written without having extremely large backups. It returns
-// a function which resets the checkpoint interval to the old interval.
-func TestingShortBackupCheckpointInterval(oldInterval time.Duration) func() {
-	BackupCheckpointInterval = time.Millisecond * 10
-	return func() {
-		BackupCheckpointInterval = oldInterval
-	}
-}
+var BackupCheckpointInterval = settings.RegisterDurationSetting(
+	settings.TenantWritable,
+	"bulkio.backup.checkpoint_interval",
+	"the minimum time between writing progress checkpoints during a backup",
+	time.Minute)
 
 var forceReadBackupManifest = util.ConstantWithMetamorphicTestBool("backup-read-manifest", false)
 
@@ -240,14 +234,14 @@ func backup(
 			for i := int32(0); i < progDetails.CompletedSpans; i++ {
 				requestFinishedCh <- struct{}{}
 			}
-			if timeutil.Since(lastCheckpoint) > BackupCheckpointInterval {
+
+			interval := BackupCheckpointInterval.Get(&execCtx.ExecCfg().Settings.SV)
+			if timeutil.Since(lastCheckpoint) > interval {
 				resumerSpan.RecordStructured(&backuppb.BackupProgressTraceEvent{
 					TotalNumFiles:     numBackedUpFiles,
 					TotalEntryCounts:  backupManifest.EntryCounts,
 					RevisionStartTime: backupManifest.RevisionStartTime,
 				})
-
-				lastCheckpoint = timeutil.Now()
 
 				err := writeBackupManifestCheckpoint(
 					ctx, defaultURI, encryption, backupManifest, execCtx.ExecCfg(), execCtx.User(),
@@ -255,7 +249,7 @@ func backup(
 				if err != nil {
 					log.Errorf(ctx, "unable to checkpoint backup descriptor: %+v", err)
 				}
-
+				lastCheckpoint = timeutil.Now()
 				if execCtx.ExecCfg().TestingKnobs.AfterBackupCheckpoint != nil {
 					execCtx.ExecCfg().TestingKnobs.AfterBackupCheckpoint()
 				}
