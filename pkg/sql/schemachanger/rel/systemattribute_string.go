3787739c874002ff26a68d7d456e34d73c373412
Andrew Werner <awerner32@gmail.com>
2022-06-27 15:57:19 -0400

    sql/schemachanger/rel: increase ordinalSet size to 32-bit, test exceeding it
    Before this change, we were butting up against the 16-attribute limit for a
    schema. When it was exceeded, nothing happend except that ordinal values in
    the ordinalSet were dropped silently. We now validate that a schema does not
    have too many attributes when constructing the schema.
    
    Along the way, we also increase the limit from 14 to 30.
    
    Release note: None

--- pkg/sql/schemachanger/rel/systemattribute_string.go
+++ pkg/sql/schemachanger/rel/systemattribute_string.go
@@ -8,18 +8,18 @@ func _() {
 	// An "invalid array index" compiler error signifies that the constant values have changed.
 	// Re-run the stringer command to generate them again.
 	var x [1]struct{}
-	_ = x[Type-63]
-	_ = x[Self-62]
+	_ = x[Type-1]
+	_ = x[Self-2]
 }
 
-const _systemAttribute_name = "SelfType"
+const _systemAttribute_name = "TypeSelf"
 
 var _systemAttribute_index = [...]uint8{0, 4, 8}
 
 func (i systemAttribute) String() string {
-	i -= 62
+	i -= 1
 	if i < 0 || i >= systemAttribute(len(_systemAttribute_index)-1) {
-		return "systemAttribute(" + strconv.FormatInt(int64(i+62), 10) + ")"
+		return "systemAttribute(" + strconv.FormatInt(int64(i+1), 10) + ")"
 	}
 	return _systemAttribute_name[_systemAttribute_index[i]:_systemAttribute_index[i+1]]
 }
