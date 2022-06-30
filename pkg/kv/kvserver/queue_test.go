24649ef80c57fcc6919538dbd70b59caad833fd4
shralex <shralex@gmail.com>
2022-06-30 10:07:24 -0700

    kvserver: the timeout of queued items should consider the rates of all item types in the queue
    When the kv.snapshot_recovery.max_rate and kv.snapshot_rebalance.max_rate
    settings are given different values, if the recovery rate is high and the
    rebalance rate is low, recovery snapshots can have a lower timeout than the
    expected duration of a single rebalance snapshot. This means that any steady
    rebalance load can starve recovery snapshots. To mitigate the issue, this PR
    sets the timeout for a snapshot based on min(kv.snapshot_recovery.max_rate,
    kv.snapshot_rebalance.max_rate).
    
    Release note: None

--- pkg/kv/kvserver/queue_test.go
+++ pkg/kv/kvserver/queue_test.go
@@ -993,7 +993,7 @@ func TestQueueRateLimitedTimeoutFunc(t *testing.T) {
 			st := cluster.MakeTestingClusterSettings()
 			queueGuaranteedProcessingTimeBudget.Override(ctx, &st.SV, tc.guaranteedProcessingTime)
 			recoverySnapshotRate.Override(ctx, &st.SV, tc.rateLimit)
-			tf := makeRateLimitedTimeoutFunc(recoverySnapshotRate)
+			tf := makeRateLimitedTimeoutFunc(recoverySnapshotRate, rebalanceSnapshotRate)
 			repl := mvccStatsReplicaInQueue{
 				size: tc.replicaSize,
 			}
