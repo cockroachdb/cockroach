6de82a10457ec98d679423b9e0d534bdb2bfbdd1
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-07-01 15:36:09 -0400

    wip

---
+++ pkg/util/admission/admissionpb/io_threshold.go
@@ -0,0 +1,40 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+//
+
+package admissionpb
+
+import (
+	"math"
+
+	"github.com/cockroachdb/redact"
+	"github.com/cockroachdb/redact/interfaces"
+)
+
+// Score returns, as the second return value, whether IO admission control is
+// considering the Store overloaded. The first return value is a 1-normalized
+// float (i.e. 1.0 is the threshold at which the second value flips to true).
+func (iot *IOThreshold) Score() (float64, bool) {
+	f := math.Max(
+		float64(iot.L0NumFiles)/float64(iot.L0NumFilesThreshold),
+		float64(iot.L0NumSubLevels)/float64(iot.L0NumSubLevelsThreshold),
+	)
+	return f, f > 1.0
+}
+
+// SafeFormat implements redact.SafeFormatter.
+func (iot *IOThreshold) SafeFormat(s interfaces.SafePrinter, _ rune) {
+	sc, overload := iot.Score()
+	s.Printf("score=%.2f io-overload=%t", redact.SafeFloat(sc), redact.Safe(overload))
+}
+
+func (iot *IOThreshold) String() string {
+	return redact.StringWithoutMarkers(iot)
+}
