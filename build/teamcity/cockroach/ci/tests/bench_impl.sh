02946c03f7dd2b736d95ea21371c0ea3020b919c
healthy-pod <ahmad@cockroachlabs.com>
2022-07-05 10:42:52 -0700

    <pkg>: <short description - lowercase, no final period>
    <what was there before: Previously, ...>
    <why it needed to change: This was inadequate because ...>
    <what you did about it: To address this, this patch ...>
    
    Release note (<category, see below>): <what> <show> <why>

--- build/teamcity/cockroach/ci/tests/bench_impl.sh
+++ build/teamcity/cockroach/ci/tests/bench_impl.sh
@@ -14,14 +14,17 @@ targets=$(for pkg in $pkgs
               grep $pkg <<< $all_tests || true
           done | sort | uniq)
 
+task() {
+    tc_start_block "Bench $1"
+    # We need the `test_sharding_strategy` flag or else the benchmarks will
+    # fail to run sharded tests like //pkg/sql/importer:importer_test.
+    bazel run --config=test --config=crosslinux --config=ci --test_sharding_strategy=disabled $1 -- \
+          -test.bench=. -test.benchtime=1ns -test.short -test.run=-
+    tc_end_block "Bench $1" 
+}
+
 set -x
 # Run all tests serially.
 for target in $targets
-do
-    tc_start_block "Bench $target"
-    # We need the `test_sharding_strategy` flag or else the benchmarks will
-    # fail to run sharded tests like //pkg/sql/importer:importer_test.
-    bazel run --config=test --config=crosslinux --config=ci --test_sharding_strategy=disabled $target -- \
-          -test.bench=. -test.benchtime=1ns -test.short -test.run=-
-    tc_end_block "Bench $target"
+    do task "$target" &
 done
