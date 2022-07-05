11b9353ebcb655ab3615eaf4046f081139f400c2
Michael Erickson <michae2@cockroachlabs.com>
2022-07-04 23:31:54 -0700

    sql/stats: convert between histograms and quantile functions
    To predict histograms in statistics forecasts, we will use linear
    regression over quantile functions. (Quantile functions are another
    representation of histogram data, in a form more amenable to statistical
    manipulation.)
    
    This commit defines quantile functions and adds methods to convert
    between histograms and quantile functions.
    
    This code was originally part of #77070 but has been pulled out to
    simplify that PR. A few changes have been made:
    - Common code has been factored into closures.
    - More checks have been added for positive values.
    - In `makeQuantile` we now trim leading empty buckets as well as
      trailing empty buckets.
    - The logic in `quantile.toHistogram` to steal from `NumRange` if
      `NumEq` is zero now checks that `NumRange` will still be >= 1.
    - More tests have been added.
    
    Assists: #79872
    
    Release note: None

--- pkg/sql/stats/quantile_test.go
+++ pkg/sql/stats/quantile_test.go
@@ -12,15 +12,355 @@ package stats
 
 import (
 	"math"
+	"reflect"
 	"strconv"
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/cat"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/eval"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil/pgdate"
 )
+
+type testHistogram []testBucket
+
+type testBucket struct {
+	NumEq, NumRange, DistinctRange, UpperBound float64
+}
+
+func (th testHistogram) toHistogram() histogram {
+	if th == nil {
+		return histogram{}
+	}
+	h := histogram{buckets: make([]cat.HistogramBucket, len(th))}
+	for i := range th {
+		h.buckets[i].NumEq = th[i].NumEq
+		h.buckets[i].NumRange = th[i].NumRange
+		h.buckets[i].DistinctRange = th[i].DistinctRange
+		h.buckets[i].UpperBound = tree.NewDFloat(tree.DFloat(th[i].UpperBound))
+	}
+	return h
+}
+
+// Test conversions from histogram to quantile.
+func TestMakeQuantile(t *testing.T) {
+	// We use all floats here. TestToQuantileValue and TestFromQuantileValue test
+	// conversions to other datatypes.
+	testCases := []struct {
+		hist testHistogram
+		rows float64
+		qfun quantile
+		err  bool
+	}{
+		{
+			hist: nil,
+			rows: 0,
+			qfun: zeroQuantile,
+		},
+		{
+			hist: testHistogram{},
+			rows: 0,
+			qfun: zeroQuantile,
+		},
+		{
+			hist: testHistogram{{0, 0, 0, 0}},
+			rows: 0,
+			qfun: zeroQuantile,
+		},
+		{
+			hist: testHistogram{{0, 0, 0, 100}, {0, 0, 0, 200}},
+			rows: 10,
+			qfun: zeroQuantile,
+		},
+		{
+			hist: testHistogram{{1, 0, 0, 0}},
+			rows: 1,
+			qfun: zeroQuantile,
+		},
+		{
+			hist: testHistogram{{2, 0, 0, 0}},
+			rows: 2,
+			qfun: zeroQuantile,
+		},
+		{
+			hist: testHistogram{{1, 0, 0, 100}},
+			rows: 1,
+			qfun: quantile{{0, 100}, {1, 100}},
+		},
+		{
+			hist: testHistogram{{1, 0, 0, 100}, {0, 1, 1, 200}},
+			rows: 2,
+			qfun: quantile{{0, 100}, {0.5, 100}, {1, 200}},
+		},
+		{
+			hist: testHistogram{{1, 0, 0, 100}, {2, 1, 1, 200}},
+			rows: 4,
+			qfun: quantile{{0, 100}, {0.25, 100}, {0.5, 200}, {1, 200}},
+		},
+		{
+			hist: testHistogram{{0, 0, 0, 100}, {6, 2, 2, 200}},
+			rows: 8,
+			qfun: quantile{{0, 100}, {0.25, 200}, {1, 200}},
+		},
+		{
+			hist: testHistogram{{0, 0, 0, 100}, {6, 2, 2, 200}},
+			rows: 8,
+			qfun: quantile{{0, 100}, {0.25, 200}, {1, 200}},
+		},
+		{
+			hist: testHistogram{{2, 0, 0, 100}, {6, 2, 2, 200}, {2, 0, 0, 300}, {0, 4, 4, 400}},
+			rows: 16,
+			qfun: quantile{{0, 100}, {0.125, 100}, {0.25, 200}, {0.625, 200}, {0.625, 300}, {0.75, 300}, {1, 400}},
+		},
+		// Cases where we trim leading and trailing zero buckets.
+		{
+			hist: testHistogram{{0, 0, 0, 0}, {0, 0, 0, 100}, {2, 2, 2, 200}},
+			rows: 4,
+			qfun: quantile{{0, 100}, {0.5, 200}, {1, 200}},
+		},
+		{
+			hist: testHistogram{{0, 0, 0, 100}, {2, 6, 6, 200}, {0, 0, 0, 300}},
+			rows: 8,
+			qfun: quantile{{0, 100}, {0.75, 200}, {1, 200}},
+		},
+		{
+			hist: testHistogram{{0, 0, 0, 0}, {4, 0, 0, 100}, {1, 3, 3, 200}, {0, 0, 0, 300}},
+			rows: 8,
+			qfun: quantile{{0, 100}, {0.5, 100}, {0.875, 200}, {1, 200}},
+		},
+		// Cases where we clamp p to 1 to fix histogram errors.
+		{
+			hist: testHistogram{{2, 0, 0, 100}},
+			rows: 1,
+			qfun: quantile{{0, 100}, {1, 100}},
+		},
+		{
+			hist: testHistogram{{1, 0, 0, 100}, {0, 1, 1, 200}},
+			rows: 1,
+			qfun: quantile{{0, 100}, {1, 100}, {1, 200}},
+		},
+		// Error cases.
+		{
+			hist: testHistogram{},
+			rows: math.Inf(1),
+			err:  true,
+		},
+		{
+			hist: testHistogram{},
+			rows: math.NaN(),
+			err:  true,
+		},
+		{
+			hist: testHistogram{},
+			rows: -1,
+			err:  true,
+		},
+		{
+			hist: testHistogram{{0, 1, 1, 100}},
+			rows: 1,
+			err:  true,
+		},
+		{
+			hist: testHistogram{{-1, 0, 0, 100}},
+			rows: 1,
+			err:  true,
+		},
+		{
+			hist: testHistogram{{math.Inf(1), 0, 0, 100}},
+			rows: 1,
+			err:  true,
+		},
+		{
+			hist: testHistogram{{1, 0, 0, 100}, {1, 0, 0, 99}},
+			rows: 2,
+			err:  true,
+		},
+		{
+			hist: testHistogram{{1, 0, 0, 100}, {0, 1, 1, 100}},
+			rows: 2,
+			err:  true,
+		},
+	}
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			q, err := makeQuantile(tc.hist.toHistogram(), tc.rows)
+			if err != nil {
+				if !tc.err {
+					t.Errorf("test case %d unexpected makeQuantile err: %v", i, err)
+				}
+				return
+			}
+			if tc.err {
+				t.Errorf("test case %d expected makeQuantile err", i)
+				return
+			}
+			if !reflect.DeepEqual(q, tc.qfun) {
+				t.Errorf("test case %d incorrect quantile %v expected %v", i, q, tc.qfun)
+			}
+		})
+	}
+}
+
+// Test conversions from quantile to histogram.
+func TestQuantileToHistogram(t *testing.T) {
+	// We use all floats here. TestToQuantileValue and TestFromQuantileValue test
+	// conversions to other datatypes.
+	testCases := []struct {
+		qfun quantile
+		rows float64
+		hist testHistogram
+		err  bool
+	}{
+		{
+			qfun: zeroQuantile,
+			rows: 0,
+			hist: nil,
+		},
+		{
+			qfun: zeroQuantile,
+			rows: 1,
+			hist: testHistogram{{1, 0, 0, 0}},
+		},
+		{
+			qfun: zeroQuantile,
+			rows: 2,
+			hist: testHistogram{{2, 0, 0, 0}},
+		},
+		{
+			qfun: quantile{{0, 100}, {1, 100}},
+			rows: 1,
+			hist: testHistogram{{1, 0, 0, 100}},
+		},
+		{
+			qfun: quantile{{0, 0}, {0, 100}, {1, 100}},
+			rows: 1,
+			hist: testHistogram{{1, 0, 0, 100}},
+		},
+		{
+			qfun: quantile{{0, 100}, {1, 100}, {1, 100}},
+			rows: 1,
+			hist: testHistogram{{1, 0, 0, 100}},
+		},
+		{
+			qfun: quantile{{0, 100}, {1, 100}, {1, 200}},
+			rows: 1,
+			hist: testHistogram{{1, 0, 0, 100}},
+		},
+		{
+			qfun: quantile{{0, 0}, {1, 100}},
+			rows: 1,
+			hist: testHistogram{{0, 0, 0, 0}, {0, 1, 1, 100}},
+		},
+		{
+			qfun: quantile{{0, 0}, {0.5, 100}, {1, 100}},
+			rows: 2,
+			hist: testHistogram{{0, 0, 0, 0}, {1, 1, 1, 100}},
+		},
+		{
+			qfun: quantile{{0, 0}, {0.9, 100}, {1, 100}},
+			rows: 10,
+			hist: testHistogram{{0, 0, 0, 0}, {1, 9, 9, 100}},
+		},
+		{
+			qfun: quantile{{0, 100}, {0.25, 100}, {0.75, 200}, {1, 200}},
+			rows: 16,
+			hist: testHistogram{{4, 0, 0, 100}, {4, 8, 8, 200}},
+		},
+		{
+			qfun: quantile{{0, 100}, {0.25, 100}, {0.5, 200}, {0.75, 200}, {0.75, 300}, {1, 300}},
+			rows: 16,
+			hist: testHistogram{{4, 0, 0, 100}, {4, 4, 4, 200}, {4, 0, 0, 300}},
+		},
+		{
+			qfun: quantile{{0, 500}, {0.125, 500}, {0.25, 600}, {0.5, 600}, {0.75, 800}, {1, 800}},
+			rows: 16,
+			hist: testHistogram{{2, 0, 0, 500}, {4, 2, 2, 600}, {4, 4, 4, 800}},
+		},
+		{
+			qfun: quantile{{0, 300}, {0, 310}, {0.125, 310}, {0.125, 320}, {0.25, 320}, {0.25, 330}, {0.5, 330}, {0.5, 340}, {0.625, 340}, {0.625, 350}, {0.75, 350}, {0.75, 360}, {0.875, 360}, {0.875, 370}, {1, 370}},
+			rows: 32,
+			hist: testHistogram{{4, 0, 0, 310}, {4, 0, 0, 320}, {8, 0, 0, 330}, {4, 0, 0, 340}, {4, 0, 0, 350}, {4, 0, 0, 360}, {4, 0, 0, 370}},
+		},
+		// Cases where we steal a row from NumRange to give to NumEq.
+		{
+			qfun: quantile{{0, 0}, {1, 100}},
+			rows: 2,
+			hist: testHistogram{{0, 0, 0, 0}, {1, 1, 1, 100}},
+		},
+		{
+			qfun: quantile{{0, 100}, {0.5, 100}, {1, 200}, {1, 300}},
+			rows: 4,
+			hist: testHistogram{{2, 0, 0, 100}, {1, 1, 1, 200}},
+		},
+		{
+			qfun: quantile{{0, 0}, {0.875, 87.5}, {1, 100}},
+			rows: 8,
+			hist: testHistogram{{0, 0, 0, 0}, {1, 6, 6, 87.5}, {0, 1, 1, 100}},
+		},
+		{
+			qfun: quantile{{0, 400}, {0.5, 600}, {0.75, 700}, {1, 800}},
+			rows: 16,
+			hist: testHistogram{{0, 0, 0, 400}, {1, 7, 7, 600}, {1, 3, 3, 700}, {1, 3, 3, 800}},
+		},
+		// Error cases.
+		{
+			qfun: quantile{},
+			rows: 1,
+			err:  true,
+		},
+		{
+			qfun: quantile{{0, 0}},
+			rows: 1,
+			err:  true,
+		},
+		{
+			qfun: quantile{{1, 0}, {0, 0}},
+			rows: 1,
+			err:  true,
+		},
+		{
+			qfun: quantile{{0, 100}, {0, 200}},
+			rows: 1,
+			err:  true,
+		},
+		{
+			qfun: quantile{{0, 100}, {math.NaN(), 100}, {1, 100}},
+			rows: 1,
+			err:  true,
+		},
+		{
+			qfun: quantile{{0, 0}, {0.75, 25}, {0.25, 75}, {1, 100}},
+			rows: 1,
+			err:  true,
+		},
+		{
+			qfun: quantile{{0, 100}, {1, 99}},
+			rows: 1,
+			err:  true,
+		},
+	}
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			h, err := tc.qfun.toHistogram(types.Float, tc.rows)
+			if err != nil {
+				if !tc.err {
+					t.Errorf("test case %d unexpected quantile.toHistogram err: %v", i, err)
+				}
+				return
+			}
+			if tc.err {
+				t.Errorf("test case %d expected quantile.toHistogram err", i)
+				return
+			}
+			h2 := tc.hist.toHistogram()
+			if !reflect.DeepEqual(h, h2) {
+				t.Errorf("test case %d incorrect histogram %v expected %v", i, h, h2)
+			}
+		})
+	}
+}
 
 // Test conversions from datum to quantile value and back.
 func TestToQuantileValue(t *testing.T) {
@@ -258,15 +598,15 @@ func TestToQuantileValue(t *testing.T) {
 	evalCtx := eval.NewTestingEvalContext(cluster.MakeTestingClusterSettings())
 	for i, tc := range testCases {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
-			val, err := ToQuantileValue(tc.dat)
+			val, err := toQuantileValue(tc.dat)
 			if err != nil {
 				if !tc.err {
-					t.Errorf("test case %d (%v) unexpected ToQuantileValue err: %v", i, tc.typ.Name(), err)
+					t.Errorf("test case %d (%v) unexpected toQuantileValue err: %v", i, tc.typ.Name(), err)
 				}
 				return
 			}
 			if tc.err {
-				t.Errorf("test case %d (%v) expected ToQuantileValue err", i, tc.typ.Name())
+				t.Errorf("test case %d (%v) expected toQuantileValue err", i, tc.typ.Name())
 				return
 			}
 			if val != tc.val {
@@ -274,9 +614,9 @@ func TestToQuantileValue(t *testing.T) {
 				return
 			}
 			// Check that we can make the round trip.
-			res, err := FromQuantileValue(tc.typ, val)
+			res, err := fromQuantileValue(tc.typ, val)
 			if err != nil {
-				t.Errorf("test case %d (%v) unexpected FromQuantileValue err: %v", i, tc.typ.Name(), err)
+				t.Errorf("test case %d (%v) unexpected fromQuantileValue err: %v", i, tc.typ.Name(), err)
 				return
 			}
 			cmp, err := res.CompareError(evalCtx, tc.dat)
@@ -535,15 +875,15 @@ func TestFromQuantileValue(t *testing.T) {
 	evalCtx := eval.NewTestingEvalContext(cluster.MakeTestingClusterSettings())
 	for i, tc := range testCases {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
-			d, err := FromQuantileValue(tc.typ, tc.val)
+			d, err := fromQuantileValue(tc.typ, tc.val)
 			if err != nil {
 				if !tc.err {
-					t.Errorf("test case %d (%v) unexpected FromQuantileValue err: %v", i, tc.typ.Name(), err)
+					t.Errorf("test case %d (%v) unexpected fromQuantileValue err: %v", i, tc.typ.Name(), err)
 				}
 				return
 			}
 			if tc.err {
-				t.Errorf("test case %d (%v) expected FromQuantileValue err", i, tc.typ.Name())
+				t.Errorf("test case %d (%v) expected fromQuantileValue err", i, tc.typ.Name())
 				return
 			}
 			cmp, err := d.CompareError(evalCtx, tc.dat)
@@ -556,9 +896,9 @@ func TestFromQuantileValue(t *testing.T) {
 				return
 			}
 			// Check that we can make the round trip with the clamped value.
-			res, err := ToQuantileValue(d)
+			res, err := toQuantileValue(d)
 			if err != nil {
-				t.Errorf("test case %d (%v) unexpected ToQuantileValue err: %v", i, tc.typ.Name(), err)
+				t.Errorf("test case %d (%v) unexpected toQuantileValue err: %v", i, tc.typ.Name(), err)
 				return
 			}
 			if res != tc.res {
