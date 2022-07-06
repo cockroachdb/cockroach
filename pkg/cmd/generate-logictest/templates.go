b234c108dd48941ffeed6632faa767b8c44906ed
Ricky Stewart <ricky@cockroachlabs.com>
2022-07-06 15:32:50 -0500

    logictest: refactor monolithic `TestLogic` targets into many subtests
    The monolithic `TestLogic` test (and its corollaries, found in
    `pkg/ccl/logictestccl` and `pkg/sql/opt/exec/execbuilder`) is very prone
    to timing out under Bazel as the subtests cannot be parallelized by the
    Bazel test runner. Furthermore, it can be difficult to debug failures in
    individual subtests as the logs for each parallel subtest are all
    intermingled. To solve this problem we generate finer-grained tests
    for each of the subtests that would otherwise be run in the monolithic
    `TestLogic` test. To that end we do all of the following:
    
    1. Split out some common helper logic into the subpackage
       `pkg/sql/logictest/logictestbase`.
    2. Refactor the logic test machinery. Up until this point, the
       `RunLogicTest` function (and related functions) have dynamically
       enumerated the subtests from `testdata` and spawned a subtest with
       `t.Run()` for each. Instead, `RunLogicTest` now takes a specific
       configuration index and a path to one test file.
    3. Restructure the logictests as follows: split out the long-running
       sqlite logic tests into their own packages, `pkg/sql/sqlitelogictest`
       and `pkg/ccl/sqlitelogictestccl`; for each logictest configuration X,
       create a subpackage `tests/X` to house all tests for that
       configuration; for each logictest file Y, create a test
       `LogicTest_X_Y` that tests that one specific file. We do this not
       only for `pkg/sql/logictest` but also for `pkg/ccl/logictestccl` and
       `pkg/sql/opt/exec/execbuilder`.
    4. Create a helper binary that generates all of these test files and
       integrate into `bazel-generate.sh`.
    5. Update `Makefile` and `dev` to change how they execute logic tests.
    6. Update some TeamCity scripts to change how they execute logic tests.
       Update `bazci` accordingly.
    7. Delete now-irrelevant TeamCity scripts from `build`.
    
    This has some breaking changes for developers:
    1. Developers using `make testlogic` or `dev testlogic` should generally
       not be affected as these frontends are ported to the new test API's.
    2. Developers used to running
         `dev test pkg/sql/logictest -f TestLogic/config-name/file-name/subtest`
       or the equivalent
         `make test ./pkg/sql/logictest TESTS=TestLogic/config-name/file-name/subtest`
       will now instead have to run:
         `dev test pkg/sql/logictest/tests/config-name -f TestLogic_file_name/subtest`
       or
         `make test ./pkg/sql/logictest/tests/config-name TESTS=TestLogic_file_name/subtest`
       In particular we point to the following important differences:
       * In the `file-name`, replace - and . with an underscore (_). If the
         filename has a slash, omit it entirely.
       * Use underscores instead of / in between the function name prefix,
         and the file name. All of these are baked into the actual Go test
         (function) name so we need to use _.
       * Don't include the config-name in the test filter; it's redundant.
       Note that if you just want to test all the tests in a directory with
       a particular config, the test filter is no longer necessary, so you
       can just do e.g. `dev test pkg/sql/logictest/tests/local`. The filter
       is necessary only if you want to test a particular file or subtest.
    3. The same as above applies for logic tests in `pkg/ccl/logictestccl` and
       `pkg/sql/opt/exec/execbuilder`, as in
       `dev test pkg/sql/opt/exec/execbuilder/tests/local -f TestExecBuild_autocommit`.
    4. Sqlite logic tests are split out into their own directory
       (`pkg/sql/sqlitelogictest` and `pkg/ccl/sqlitelogictestccl`) with the
       same test layout as above, like
       `dev test pkg/sql/sqlitelogictest/tests/local ...`
    5. `dev testlogic` will now unconditionally regenerate these test files
       before running the logic test, and `dev test` will do the same if the
       test is a logic test target. This behavior can be skipped with
       `--no-gen`.
    
    Closes #77204.
    Closes #77456.
    
    Release note: None

---
+++ pkg/cmd/generate-logictest/templates.go
@@ -0,0 +1,259 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package main
+
+import "text/template"
+
+const templateText = `
+{{- define "cclHeader" -}}
+// Copyright 2022 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+{{- end }}
+
+{{- define "bslHeader" -}}
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+{{- end }}
+
+{{- define "header" }}
+{{- if .Ccl }}{{ template "cclHeader" }}
+{{- else }}{{ template "bslHeader" }}{{ end }}
+{{- end }}
+
+{{- define "declareTestdataPaths" }}
+{{- if .LogicTest }}var logicTestDir string
+{{ end }}{{ if .CclLogicTest }}var cclLogicTestDir string
+{{ end }}{{ if .ExecBuildLogicTest }}var execBuildLogicTestDir string
+{{ end }}{{ if .SqliteLogicTest }}var sqliteLogicTestDir string
+{{ end }}
+{{- end }}
+
+{{- define "runLogicTest" }}
+{{- if .LogicTest -}}
+func runLogicTest(t *testing.T, file string) {
+	skip.UnderDeadlock(t, "times out and/or hangs")
+	logictest.RunLogicTest(t, logictest.TestServerArgs{}, configIdx, filepath.Join(logicTestDir, file))
+}
+{{ end }}
+{{- end }}
+
+{{- define "runCCLLogicTest" }}
+{{- if .CclLogicTest -}}
+func runCCLLogicTest(t *testing.T, file string) {
+	skip.UnderDeadlock(t, "times out and/or hangs")
+	logictest.RunLogicTest(t, logictest.TestServerArgs{}, configIdx, filepath.Join(cclLogicTestDir, file))
+}
+{{ end }}
+{{- end }}
+
+{{- define "runExecBuildLogicTest" }}
+{{- if .ExecBuildLogicTest -}}
+func runExecBuildLogicTest(t *testing.T, file string) {
+	defer sql.TestingOverrideExplainEnvVersion("CockroachDB execbuilder test version")()
+	skip.UnderDeadlock(t, "times out and/or hangs")
+	serverArgs := logictest.TestServerArgs{
+		DisableWorkmemRandomization: true,{{ if .ForceProductionValues }}
+		ForceProductionValues:       true,{{end}}
+	}
+	logictest.RunLogicTest(t, serverArgs, configIdx, filepath.Join(execBuildLogicTestDir, file))
+}
+{{ end }}
+{{- end }}
+
+{{- define "runSqliteLogicTest" }}
+{{- if .SqliteLogicTest -}}
+func runSqliteLogicTest(t *testing.T, file string) {
+	skip.UnderDeadlock(t, "times out and/or hangs")
+	if !*logictest.Bigtest {
+		skip.IgnoreLint(t, "-bigtest flag must be specified to run this test")
+	}
+	// SQLLite logic tests can be very memory intensive, so we give them larger
+	// limit than other logic tests get.
+	serverArgs := logictest.TestServerArgs{
+		MaxSQLMemoryLimit: 512 << 20, // 512 MiB
+	}
+	logictest.RunLogicTest(t, serverArgs, configIdx, filepath.Join(sqliteLogicTestDir, file))
+}
+{{ end }}
+{{- end }}
+
+{{- define "declareTestdataSetupFunctions" }}
+{{- template "runLogicTest" . }}
+{{- template "runCCLLogicTest" . }}
+{{- template "runExecBuildLogicTest" . }}
+{{- template "runSqliteLogicTest" . }}
+{{- end }}
+
+{{- define "initLogicTest" }}
+{{- if .LogicTest }}	if bazel.BuiltWithBazel() {
+		var err error
+		logicTestDir, err = bazel.Runfile("pkg/sql/logictest/testdata/logic_test")
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		logicTestDir = "{{ .RelDir }}/sql/logictest/testdata/logic_test"
+	}
+{{ end }}
+{{- end }}
+
+{{- define "initCCLLogicTest" }}
+{{- if .CclLogicTest }}	if bazel.BuiltWithBazel() {
+		var err error
+		cclLogicTestDir, err = bazel.Runfile("pkg/ccl/logictestccl/testdata/logic_test")
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		cclLogicTestDir = "{{ .RelDir }}/ccl/logictestccl/testdata/logic_test"
+	}
+{{ end }}
+{{- end }}
+
+{{- define "initExecBuildLogicTest" }}
+{{- if .ExecBuildLogicTest }}	if bazel.BuiltWithBazel() {
+		var err error
+		execBuildLogicTestDir, err = bazel.Runfile("pkg/sql/opt/exec/execbuilder/testdata")
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		execBuildLogicTestDir = "{{ .RelDir }}/sql/opt/exec/execbuilder/testdata"
+	}
+{{ end }}
+{{- end }}
+
+{{- define "initSqliteLogicTest" }}
+{{- if .SqliteLogicTest }}	if *logictest.Bigtest {
+		if bazel.BuiltWithBazel() {
+			var err error
+			sqliteLogicTestDir, err = bazel.Runfile("external/com_github_cockroachdb_sqllogictest")
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			var err error
+			sqliteLogicTestDir, err = sqlitelogictest.FindLocalLogicTestClone()
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+{{ end }}
+{{- end }}
+
+{{- define "initFunc" }}
+func init() {
+{{ template "initLogicTest" . }}
+{{- template "initCCLLogicTest" . }}
+{{- template "initExecBuildLogicTest" . }}
+{{- template "initSqliteLogicTest" . }}
+}
+{{- end }}
+
+{{- template "header" . }}
+
+// Code generated by generate-logictest, DO NOT EDIT.
+
+package test{{ .Package }}
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/build/bazel"{{ if .Ccl }}
+	_ "github.com/cockroachdb/cockroach/pkg/ccl"
+	"github.com/cockroachdb/cockroach/pkg/ccl/utilccl"{{ end }}
+	"github.com/cockroachdb/cockroach/pkg/security/securityassets"
+	"github.com/cockroachdb/cockroach/pkg/security/securitytest"
+	"github.com/cockroachdb/cockroach/pkg/server"{{ if .ExecBuildLogicTest }}
+	"github.com/cockroachdb/cockroach/pkg/sql"{{ end }}
+	"github.com/cockroachdb/cockroach/pkg/sql/logictest"{{ if .SqliteLogicTest }}
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlitelogictest"{{ end }}
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
+	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
+)
+
+const configIdx = {{ .ConfigIdx }}
+
+{{ template "declareTestdataPaths" . }}
+{{- template "initFunc" . }}
+
+func TestMain(m *testing.M) {
+	{{ if .Ccl }}defer utilccl.TestingEnableEnterprise()()
+	{{ end }}securityassets.SetLoader(securitytest.EmbeddedAssets)
+	randutil.SeedForTests()
+	serverutils.InitTestServerFactory(server.TestServerFactory)
+	serverutils.InitTestClusterFactory(testcluster.TestClusterFactory)
+	os.Exit(m.Run())
+}
+
+{{ template "declareTestdataSetupFunctions" . }}`
+
+// There is probably room for optimization here. Among other things:
+// some tests may declare a testdata dependency they don't actually need, and
+// the sizes for some of these tests can probably be smaller than "enormous".
+const buildFileTemplate = `load("//build/bazelutil/unused_checker:unused.bzl", "get_x_data")
+load("@io_bazel_rules_go//go:def.bzl", "go_test")
+
+go_test(
+    name = "{{ .TestRuleName }}_test",
+    size = "enormous",
+    srcs = ["generated_test.go"],
+    data = [
+        "//c-deps:libgeos",  # keep{{ if .SqliteLogicTest }}
+        "@com_github_cockroachdb_sqllogictest//:testfiles",  # keep{{ end }}{{ if .CclLogicTest }}
+        "//pkg/ccl/logictestccl:testdata",  # keep{{ end }}{{ if .LogicTest }}
+        "//pkg/sql/logictest:testdata",  # keep{{ end }}{{ if .ExecBuildLogicTest }}
+        "//pkg/sql/opt/exec/execbuilder:testdata",  # keep{{ end }}
+    ],
+    shard_count = {{ if gt .TestCount 16 }}16{{ else }}{{ .TestCount }}{{end}},
+    deps = [
+        "//pkg/build/bazel",{{ if .Ccl }}
+        "//pkg/ccl",
+        "//pkg/ccl/utilccl",{{ end }}
+        "//pkg/security/securityassets",
+        "//pkg/security/securitytest",
+        "//pkg/server",{{ if .ExecBuildLogicTest }}
+        "//pkg/sql",{{ end }}
+        "//pkg/sql/logictest",{{ if .SqliteLogicTest }}
+        "//pkg/sql/sqlitelogictest",{{ end }}
+        "//pkg/testutils/serverutils",
+        "//pkg/testutils/skip",
+        "//pkg/testutils/testcluster",
+        "//pkg/util/leaktest",
+        "//pkg/util/randutil",
+    ],
+)
+
+get_x_data(name = "get_x_data")
+`
+
+var (
+	testFileTpl  = template.Must(template.New("source").Parse(templateText))
+	buildFileTpl = template.Must(template.New("source").Parse(buildFileTemplate))
+)
