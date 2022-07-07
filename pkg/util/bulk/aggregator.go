fc36b3cd6e63d50237bf8026be6de1586fbc96ae
Aditya Maru <adityamaru@gmail.com>
2022-07-07 17:54:41 -0400

    bulk, backupccl: introduce a Structured event Aggregator
    This change introduces an Aggregator object that is capable
    of listening for Structured events emitted in a recording,
    aggregating them and rendering them as LazyTags.
    We also introduce an AggregatorEvent interface that can be
    implemented by a Structured event thereby making it eligible
    for aggregation in the Aggregator.
    
    The first user of the Aggregator will be every backup data
    processor that is setup on the nodes in the cluster during
    a backup. The Aggregator lives as long as the processor, and
    listens for Aggregator events emitted by any span that is a child
    of the processors' span. This includes both local children as
    well as remote children whose recordings have been imported into
    a local span. The Aggregator stores running aggregates of each
    AggregatorEvent it is notified about, bucketed by the events'
    tag name. This aggregate will be rendered on the tracing span as a LazyTag.
    
    This change teaches every ExportRequest to emit an AggregatorEvent.
    Going forward we expect many more operations in bulk jobs to
    define and emit such events providing visibility into otherwise
    opaque operations.
    
    We cleanup some of the StructuredEvents that were previously
    added but have not proved useful, and also change some of the
    tracing span operation names to be more intuitive.
    
    To view these aggregated events once can navigate to the
    `/tracez` endpoint of the debug console to take a snapshot and search for either
    `BACKUP` or the job ID to filter for tracing spans on that node.
    The span associated with the backup processor will be decorated with
    tags that correspond to the fields in the introduced `ExportStats`
    proto message. Note these stats are only aggregated on a per node
    basis.
    
    Fixes: #80388
    
    Release note: None

---
+++ pkg/util/bulk/aggregator.go
@@ -0,0 +1,105 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package bulk
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/cockroach/pkg/util/tracing"
+)
+
+// AggregatorEvent describes an event that can be aggregated and stored by the
+// Aggregator. An AggregatorEvent also implements the tracing.LazyTag interface
+// to render its information on the associated tracing span.
+type AggregatorEvent interface {
+	tracing.LazyTag
+
+	// Identity returns an AggregatorEvent that when combined with another
+	// event returns the other AggregatorEvent unchanged.
+	Identity() AggregatorEvent
+	// Combine combines two AggregatorEvents together.
+	Combine(other AggregatorEvent)
+	// Tag returns a string used to identify the AggregatorEvent.
+	Tag() string
+}
+
+// An Aggregator can be used to aggregate and render AggregatorEvents that are
+// emitted as part of its tracing spans' recording.
+type Aggregator struct {
+	mu struct {
+		syncutil.Mutex
+		// aggregatedEvents is a mapping from the tag identifying the
+		// AggregatorEvent to the running aggregate of the AggregatorEvent.
+		aggregatedEvents map[string]AggregatorEvent
+		// sp is the tracing span managed by the Aggregator.
+		sp *tracing.Span
+		// closed is set to true if the Aggregator has already been closed.
+		closed bool
+	}
+}
+
+// Notify implements the tracing.EventListener interface.
+func (b *Aggregator) Notify(event tracing.Structured) {
+	bulkEvent, ok := event.(AggregatorEvent)
+	if !ok {
+		return
+	}
+
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	// If this is the first AggregatorEvent with this tag, set it as a LazyTag on
+	// the associated tracing span. This way the AggregatorEvent will be
+	// dynamically Render()ed everytime we pull the tracing for the associated
+	// span.
+	eventTag := bulkEvent.Tag()
+	if _, ok := b.mu.aggregatedEvents[bulkEvent.Tag()]; !ok {
+		b.mu.aggregatedEvents[eventTag] = bulkEvent.Identity()
+		b.mu.sp.SetLazyTag(eventTag, b.mu.aggregatedEvents[eventTag])
+	}
+	b.mu.aggregatedEvents[eventTag].Combine(bulkEvent)
+}
+
+// Close is responsible for finishing the Aggregators' tracing span.
+func (b *Aggregator) Close() {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	if !b.mu.closed {
+		b.mu.sp.Finish()
+		b.mu.closed = true
+	}
+}
+
+var _ tracing.EventListener = &Aggregator{}
+
+// MakeAggregatorWithSpan returns an instance of an Aggregator along with a
+// newly created child context. The Aggregator is registered as a
+// tracing.EventListener on the span associated with newly created context.
+//
+// The Aggregator instance is responsible for finishing the returned span, and
+// so the user must call Close().
+func MakeAggregatorWithSpan(
+	ctx context.Context, aggregatorName string,
+) (context.Context, *Aggregator) {
+	agg := &Aggregator{}
+	sp := tracing.SpanFromContext(ctx)
+
+	aggCtx, aggSpan := sp.Tracer().StartSpanCtx(ctx, aggregatorName,
+		tracing.WithEventListeners([]tracing.EventListener{agg}), tracing.WithParent(sp))
+
+	agg.mu.Lock()
+	defer agg.mu.Unlock()
+	agg.mu.aggregatedEvents = make(map[string]AggregatorEvent)
+	agg.mu.sp = aggSpan
+
+	return aggCtx, agg
+}
