9b2bb35b15500f4aa87dc6101ed39a66f195888b
Michael Butler <butler@cockroachlabs.com>
2022-07-07 06:34:10 -0400

    storageccl: use the new PebbleIterator in ExternalSSTReader
    This PR refactors all call sites of ExternalSSTReader(), to support using the
    new PebbleIterator, which has baked in range key support. Most notably, this
    PR replaces the multiIterator used in the restore data processor with the
    PebbleSSTIterator.
    
    This patch is apart of a larger effort to teach backup and restore about MVCC
    bulk operations. Next, the readAsOfIterator will need to learn how to
    deal with range keys.
    
    Informs #71155
    
    This PR addresses a bug created in #83984: loop variables in
    ExternalSSTReader were captured by reference, leading to roachtest failures
    (#84240, #84162).
    
    Informs #71155i
    
    Fixes: #84240, #84162, #84181
    
    Release note: none

--- pkg/ccl/storageccl/external_sst_reader_test.go
+++ pkg/ccl/storageccl/external_sst_reader_test.go
@@ -10,10 +10,24 @@ package storageccl
 
 import (
 	"bytes"
+	"context"
+	"strconv"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/blobs"
+	"github.com/cockroachdb/cockroach/pkg/cloud"
+	_ "github.com/cockroachdb/cockroach/pkg/cloud/nodelocal"
+	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/security/username"
+	"github.com/cockroachdb/cockroach/pkg/sql"
+	"github.com/cockroachdb/cockroach/pkg/storage"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/storageutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
 	"github.com/cockroachdb/cockroach/pkg/util/ioctx"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/stretchr/testify/require"
 )
 
@@ -71,4 +85,75 @@ func TestSSTReaderCache(t *testing.T) {
 	// Read at where prior non-suffix read finished does not make a new call.
 	_, _ = raw.ReadAt(discard, 10)
 	require.Equal(t, expectedOpenCalls, openCalls)
+}
+
+// TestNewExternalSSTReader ensures that ExternalSSTReader properly reads and
+// iterates through SSTs stored in different external storage base directories.
+func TestNewExternalSSTReader(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	ctx := context.Background()
+	dir, dirCleanupFn := testutils.TempDir(t)
+	defer dirCleanupFn()
+	args := base.TestServerArgs{ExternalIODir: dir}
+	tc := testcluster.StartTestCluster(t, 1, base.TestClusterArgs{ServerArgs: args})
+	defer tc.Stopper().Stop(ctx)
+	clusterSettings := tc.Server(0).ClusterSettings()
+
+	const localFoo = "nodelocal://1/foo"
+
+	subdirs := []string{"a", "b", "c"}
+	fileStores := make([]StoreFile, len(subdirs))
+
+	for i, subdir := range subdirs {
+
+		// Create a store rooted in the file's subdir
+		store, err := cloud.ExternalStorageFromURI(
+			ctx,
+			localFoo+subdir+"/",
+			base.ExternalIODirConfig{},
+			clusterSettings,
+			blobs.TestBlobServiceClient(dir),
+			username.RootUserName(),
+			tc.Servers[0].InternalExecutor().(*sql.InternalExecutor), tc.Servers[0].DB(), nil)
+		require.NoError(t, err)
+
+		fileStores[i].Store = store
+
+		// Create an SST and write it to external storage. Ensure that the SSTs do
+		// not span the same keyspace.
+		numKeys := 10000
+		kvs := make(storageutils.KVs, numKeys)
+		for j := 0; j < numKeys; j++ {
+			// Ensure each key is unique
+			kvs[j] = storageutils.PointKV(subdir+"a"+strconv.Itoa(i), numKeys-j, "1")
+		}
+
+		fileName := subdir + "DistinctFileName.sst"
+		fileStores[i].FilePath = fileName
+
+		sst, _, _ := storageutils.MakeSST(t, clusterSettings, kvs)
+
+		w, err := store.Writer(ctx, fileName)
+		require.NoError(t, err)
+		_, err = w.Write(sst)
+		require.NoError(t, err)
+		w.Close()
+	}
+
+	var iterOpts = storage.IterOptions{
+		KeyTypes:   storage.IterKeyTypePointsAndRanges,
+		LowerBound: keys.LocalMax,
+		UpperBound: keys.MaxKey,
+	}
+
+	iter, err := ExternalSSTReader(ctx, fileStores, nil, iterOpts)
+	require.NoError(t, err)
+	for iter.SeekGE(storage.MVCCKey{Key: keys.LocalMax}); ; iter.Next() {
+		ok, err := iter.Valid()
+		require.NoError(t, err)
+		if !ok {
+			break
+		}
+	}
 }
