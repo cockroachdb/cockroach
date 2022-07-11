5e9159b4291fcfdd068aed22f3b381e10f710c92
Sean Barag <barag@cockroachlabs.com>
2022-07-11 15:11:43 -0700

    deps: add testing-library and npm scripts
    Release note (<category, see below>): <what> <show> <why>

--- pkg/ui/workspaces/e2e-tests/cypress.config.ts
+++ pkg/ui/workspaces/e2e-tests/cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      config.baseUrl = "http://localhost:8084";
       // implement node event listeners here
     },
   },
--- pkg/ui/workspaces/e2e-tests/cypress/support/commands.ts
+++ pkg/ui/workspaces/e2e-tests/cypress/support/commands.ts
@@ -23,3 +23,4 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import "@testing-library/cypress/add-commands";
