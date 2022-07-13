e712f3e3b61909897eb904ac4e209c6f0b676ac2
Andrei Matei <andrei@cockroachlabs.com>
2022-07-13 15:02:01 -0400

    kvserver: cleanup 1PC code
    Some 1PC code was confusing. It gave the impression that a fatal
    assertion is possible when it wasn't.
    Before, evaluate1PC() suggested that it's possible for
    onePCResult{success: onePCFailed, pErr: pErr} to be returned. If that
    were to actually happen, the caller (evaluateWriteBatch) would fatal -
    it wants an error on onePCFailed. I believe that this condition could
    not have actually happened. This patch makes it clear that evaluate1PC()
    always returns an error for onePCFailed, and asserts that the condition
    that would have let to the no-err return previously is impossible.
    
    Release note: None

--- pkg/kv/kvserver/replica_test.go
+++ pkg/kv/kvserver/replica_test.go
@@ -5267,6 +5267,7 @@ func TestReplicaEndTxnWithRequire1PC(t *testing.T) {
 
 	key := roachpb.Key("a")
 	txn := newTransaction("test", key, 1, tc.Clock())
+	// Perform a write in the txn. This will prevent us from performing 1PC later.
 	var ba roachpb.BatchRequest
 	ba.Header = roachpb.Header{Txn: txn}
 	put := putArgs(key, []byte("value"))
@@ -5276,6 +5277,9 @@ func TestReplicaEndTxnWithRequire1PC(t *testing.T) {
 		t.Fatalf("unexpected error: %s", pErr)
 	}
 
+	// Now send the commit with the Require1PC flag set. This is non-sensical and
+	// we check that the server rejects it: since we've written before, this batch
+	// will not qualify for 1PC evaluation.
 	et, etH := endTxnArgs(txn, true)
 	et.Require1PC = true
 	ba = roachpb.BatchRequest{}
