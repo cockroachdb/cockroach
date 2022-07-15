04ebfe98b8ab11ae887cf554a8c1b65b7de1839a
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2022-07-15 09:18:43 -0400

    ui: Remove useless eslint global declaration
    Release note: None

--- pkg/ui/workspaces/db-console/src/redux/history.ts
+++ pkg/ui/workspaces/db-console/src/redux/history.ts
@@ -7,7 +7,6 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
-/* globals module */
 
 import { createHashHistory } from "history";
 
