36486a1079e3d28c7324507399215c0c68168598
Raphael 'kena' Poss <knz@thaumogen.net>
2022-07-17 01:01:25 +0200

    pgwire,sdnotify: shortcut to /tmp in tests when necessary
    This commit changes `TestAuthenticationAndHBARules` (pgwire) and
    `TestSDNotify` (sdnotify) to use `/tmp` as socket directory if the
    default temporary directory name is too long.
    
    Release note: None

--- pkg/util/sdnotify/sdnotify_unix_test.go
+++ pkg/util/sdnotify/sdnotify_unix_test.go
@@ -14,16 +14,32 @@
 package sdnotify
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	_ "github.com/cockroachdb/cockroach/pkg/util/log" // for flags
+	"github.com/stretchr/testify/require"
 )
 
 func TestSDNotify(t *testing.T) {
-	l, err := listen()
-	if err != nil {
-		t.Fatal(err)
+	tmpDir := os.TempDir()
+	// On BSD, binding to a socket is limited to a path length of 104 characters
+	// (including the NUL terminator). In glibc, this limit is 108 characters.
+	// macOS also has a tendency to produce very long temporary directory names.
+	if len(tmpDir) >= 104-1-len("sdnotify/notify.sock")-10 {
+		// Perhaps running inside a sandbox?
+		t.Logf("default temp dir name is too long: %s", tmpDir)
+		t.Logf("forcing use of /tmp instead")
+		// Note: Using /tmp may fail on some systems; this is why we
+		// prefer os.TempDir() by default.
+		tmpDir = "/tmp"
 	}
+	dir, err := ioutil.TempDir(tmpDir, "sdnotify")
+	require.NoError(t, err)
+
+	l, err := listen(dir)
+	require.NoError(t, err)
 	defer func() { _ = l.close() }()
 
 	ch := make(chan error)
