7377c2bd181bad5738de256ec4eaa1c1be445a2f
Xin Hao Zhang <xzhang@cockroachlabs.com>
2022-07-18 14:57:29 -0400

    cluster-ui: allow dynamic timescale window from key buttons
    fixes #77007
    
    Previously, controlling the time scale window using the arrow
    key buttons and 'Now' button would disable the dynamically moving
    window even when the end time = now. This commit makes it so that
    when the time window is changed to include the current time (i.e.
    endTime = now), the time window will be changed to dynamic to
    ensure the window is keep up to date with the current time.
    
    This commit also fixes the object construction of the time scale
    from session storage and corrects the 'Now' button tooltip label from
    'past 1 day' to 'Most recent interval', with the other labels in the
    control panel ('next time interval' and 'previous time interval'
    being capitalized.
    
    Release note (bug fix): changing the time window using arrow buttons
    and 'Now' button will now properly turn the timeframe into a moving
    window when endTime = now.

--- pkg/ui/workspaces/db-console/src/redux/globalTimeScale.ts
+++ pkg/ui/workspaces/db-console/src/redux/globalTimeScale.ts
@@ -8,13 +8,32 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
+import moment from "moment";
 import { LocalSetting } from "./localsettings";
 import { AdminUIState } from "./state";
 import { TimeScale, defaultTimeScaleSelected } from "@cockroachlabs/cluster-ui";
 
 const localSettingsSelector = (state: AdminUIState) => state.localSettings;
 
-export const globalTimeScaleLocalSetting = new LocalSetting<
-  AdminUIState,
-  TimeScale
->("timeScale/SQLActivity", localSettingsSelector, defaultTimeScaleSelected);
+const setting = new LocalSetting<AdminUIState, TimeScale>(
+  "timeScale/SQLActivity",
+  localSettingsSelector,
+  defaultTimeScaleSelected,
+);
+
+export const globalTimeScaleLocalSetting = {
+  ...setting,
+  selector: (state: AdminUIState): TimeScale => {
+    const val = setting.selector(state);
+    if (typeof val.windowSize !== "string") {
+      return val;
+    }
+    return {
+      key: val.key,
+      windowSize: val.windowSize && moment.duration(val.windowSize),
+      windowValid: val.windowValid && moment.duration(val.windowValid),
+      sampleSize: val.sampleSize && moment.duration(val.sampleSize),
+      fixedWindowEnd: val.fixedWindowEnd,
+    };
+  },
+};
