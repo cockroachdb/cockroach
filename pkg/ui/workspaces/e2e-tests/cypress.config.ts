322aeffed7d7e83f7bd294d5bea9d88b980fe617
Sean Barag <barag@cockroachlabs.com>
2022-07-18 20:20:09 +0000

    Add TC+Docker-specific config overrides
    Release note (<category, see below>): <what> <show> <why>

--- pkg/ui/workspaces/e2e-tests/cypress.config.ts
+++ pkg/ui/workspaces/e2e-tests/cypress.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from "cypress";
 
+const TEAMCITY_OVERRIDES: Partial<Cypress.UserConfigOptions> = {
+  // Don't remove other artifacts, in case other job phases included some already.
+  trashAssetsBeforeRuns: false,
+  downloadsFolder: "/artifacts/cypress/downloads",
+  screenshotsFolder: "/artifacts/cypress/screenshots",
+  videosFolder: "/artifacts/cypress/videos",
+};
+
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:8080",
     setupNodeEvents(on, config) {
       // Implement custom node event listeners here
     },
+    // Override some settings when running in TeamCity
+    ...(
+      process.env["TEAMCITY_VERSION"] && process.env["IS_DOCKER"]
+        ? TEAMCITY_OVERRIDES
+        : {}
+    ),
   },
 });
