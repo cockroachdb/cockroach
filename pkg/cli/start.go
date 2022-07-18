2c7bf96d8a53bfb664461dd554f1610f9ba138a0
Raphael 'kena' Poss <knz@thaumogen.net>
2022-07-18 21:48:29 +0200

    server: enabling instantiating secondary tenant servers in-memory
    Epic: CRDB-14537
    
    This commit lays the groundwork for the ability to run tenant servers
    in-memory. It introduces a new `serverController` with two roles:
    
    - it maps tenant names to server instances.
    - it only starts servers the first time the tenant name is referenced,
      and only if that tenant is marked active.
    
    As of this commit, no subsystem inside CockroachDB refers to this
    new server controller; this means there is no signal hooked up
    to start these servers automatically yet.
    
    For testing, a debug HTTP endpoint has been added:
    `/debug/tickle?name=<tenantname>`
    
    Example use:
    
    1. start a server. At this point no secondary tenant server is created yet.
    2. create a test tenant, e.g. via `select crdb_internal.create_tenant(123, 'hello');`.
       At this point, the secondary tenant server is still not running.
    3. Perform a HTTP request to the debug endpoint, e.g. to `/debug/tickle?name=hello`
    4. Observe (e.g. in logs): the secondary server has been started.
    
    One can also observe that the controller also serves the name `system`
    to refer to the system tenant.
    
    For now, the secondary servers created this way use separate network
    listeners for SQL/HTTP/RPC. NB: This mechanism will be superseded when
    https://github.com/cockroachdb/cockroach/issues/84604 is addressed.
    
    The port number is assigned randomly. To derive a predictable port
    number for testing (until issue #84604 is addressed), the operator can
    pass e.g. `--secondary-tenant-port-offset=100` to the start
    command (for 100 past the base port number).
    
    Release note: None

--- pkg/cli/start.go
+++ pkg/cli/start.go
@@ -22,7 +22,6 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
-	"text/tabwriter"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
@@ -41,10 +40,10 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/server/status"
 	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
-	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catalogkeys"
 	"github.com/cockroachdb/cockroach/pkg/storage"
 	"github.com/cockroachdb/cockroach/pkg/storage/enginepb"
 	"github.com/cockroachdb/cockroach/pkg/storage/fs"
+	"github.com/cockroachdb/cockroach/pkg/util"
 	"github.com/cockroachdb/cockroach/pkg/util/cgroups"
 	"github.com/cockroachdb/cockroach/pkg/util/envutil"
 	"github.com/cockroachdb/cockroach/pkg/util/grpcutil"
@@ -1038,24 +1037,6 @@ func startShutdownAsync(
 	}()
 }
 
-// makeServerOptionsForURL creates the input for MakeURLForServer().
-// Beware of not calling this too early; the server address
-// is finalized late in the network initialization sequence.
-func makeServerOptionsForURL(
-	serverCfg *server.Config,
-) (clientsecopts.ClientSecurityOptions, clientsecopts.ServerParameters) {
-	clientConnOptions := clientsecopts.ClientSecurityOptions{
-		Insecure: serverCfg.Config.Insecure,
-		CertsDir: serverCfg.Config.SSLCertsDir,
-	}
-	serverParams := clientsecopts.ServerParameters{
-		ServerAddr:      serverCfg.Config.SQLAdvertiseAddr,
-		DefaultPort:     base.DefaultPort,
-		DefaultDatabase: catalogkeys.DefaultDatabaseName,
-	}
-	return clientConnOptions, serverParams
-}
-
 // reportServerInfo prints out the server version and network details
 // in a standardized format.
 func reportServerInfo(
@@ -1077,7 +1058,7 @@ func reportServerInfo(
 	// (Re-)compute the client connection URL. We cannot do this
 	// earlier (e.g. above, in the runStart function) because
 	// at this time the address and port have not been resolved yet.
-	clientConnOptions, serverParams := makeServerOptionsForURL(serverCfg)
+	clientConnOptions, serverParams := server.MakeServerOptionsForURL(serverCfg.Config)
 	pgURL, err := clientsecopts.MakeURLForServer(clientConnOptions, serverParams, url.User(username.RootUser))
 	if err != nil {
 		log.Ops.Errorf(ctx, "failed computing the URL: %v", err)
@@ -1162,7 +1143,7 @@ func reportServerInfo(
 	}
 
 	// Collect the formatted string and show it to the user.
-	msg, err := expandTabsInRedactableBytes(buf.RedactableBytes())
+	msg, err := util.ExpandTabsInRedactableBytes(buf.RedactableBytes())
 	if err != nil {
 		return err
 	}
@@ -1173,22 +1154,6 @@ func reportServerInfo(
 	}
 
 	return nil
-}
-
-// expandTabsInRedactableBytes expands tabs in the redactable byte
-// slice, so that columns are aligned. The correctness of this
-// function depends on the assumption that the `tabwriter` does not
-// replace characters.
-func expandTabsInRedactableBytes(s redact.RedactableBytes) (redact.RedactableBytes, error) {
-	var buf bytes.Buffer
-	tw := tabwriter.NewWriter(&buf, 2, 1, 2, ' ', 0)
-	if _, err := tw.Write([]byte(s)); err != nil {
-		return nil, err
-	}
-	if err := tw.Flush(); err != nil {
-		return nil, err
-	}
-	return redact.RedactableBytes(buf.Bytes()), nil
 }
 
 func hintServerCmdFlags(ctx context.Context, cmd *cobra.Command) {
@@ -1433,7 +1398,7 @@ func reportReadinessExternally(ctx context.Context, cmd *cobra.Command, waitForI
 		// (Re-)compute the client connection URL. We cannot do this
 		// earlier (e.g. above, in the runStart function) because
 		// at this time the address and port have not been resolved yet.
-		clientConnOptions, serverParams := makeServerOptionsForURL(&serverCfg)
+		clientConnOptions, serverParams := server.MakeServerOptionsForURL(serverCfg.Config)
 		pgURL, err := clientsecopts.MakeURLForServer(clientConnOptions, serverParams, url.User(username.RootUser))
 		if err != nil {
 			log.Errorf(ctx, "failed computing the URL: %v", err)
