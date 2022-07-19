1e3b5b795600146ef33b3fa367593cf928532a4c
Yahor Yuzefovich <yahor@cockroachlabs.com>
2022-07-19 10:13:12 -0700

    WIP on disabling txn sampling

--- pkg/sql/instrumentation.go
+++ pkg/sql/instrumentation.go
@@ -45,7 +45,7 @@ var collectTxnStatsSampleRate = settings.RegisterFloatSetting(
 	settings.TenantWritable,
 	"sql.txn_stats.sample_rate",
 	"the probability that a given transaction will collect execution statistics (displayed in the DB Console)",
-	0.01,
+	0,
 	func(f float64) error {
 		if f < 0 || f > 1 {
 			return errors.New("value must be between 0 and 1 inclusive")
