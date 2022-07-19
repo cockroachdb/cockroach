4c8a34b280e234a09af7e44fe61a4b059c533107
DrewKimball <drewk@cockroachlabs.com>
2022-07-19 16:54:43 -0700

    opt: allow lookup joins to order on index columns
    It is possible for lookup joins to return the results of each lookup
    in the order of the lookup index. In the case when the input is ordered
    on a key, preserving the input ordering and then returning looked-up rows
    in index order is equivalent to performing a sort on the input ordering
    with the index columns appended.
    
    This patch teaches the optimizer that lookup joins can preserve the index
    ordering. This allows the optimizer to avoid sorting in some cases, which
    can significantly improve performance because sorts have to buffer all input
    rows. Due to implementation details of the lookup join, order can only be
    preserved when none of the index columns involved in the ordering are
    sorted in descending order.
    
    Fixes #84685
    
    Release note (performance improvement): The optimizer can now return the
    results of a join in sorted order in more cases. This can allow the
    optimizer to avoid expensive sorts that need to buffer all input rows.

--- pkg/sql/distsql_physical_planner.go
+++ pkg/sql/distsql_physical_planner.go
@@ -2333,6 +2333,22 @@ func (dsp *DistSQLPlanner) createPlanForLookupJoin(
 		return nil, err
 	}
 
+	// If any of the ordering columns originate from the lookup table, this is a
+	// case where we are ordering on a prefix of input columns followed by the
+	// lookup columns. We need to maintain the index ordering on each lookup.
+	var maintainLookupOrdering bool
+	numInputCols := len(plan.GetResultTypes())
+	for i := range n.reqOrdering {
+		if n.reqOrdering[i].ColIdx >= numInputCols {
+			maintainLookupOrdering = true
+			if n.reqOrdering[i].Direction == encoding.Descending {
+				// Validate that an ordering on lookup columns does not contain
+				// descending columns.
+				panic(errors.AssertionFailedf("ordering on a lookup index with descending columns"))
+			}
+		}
+	}
+
 	joinReaderSpec := execinfrapb.JoinReaderSpec{
 		Type:              n.joinType,
 		LockingStrength:   n.table.lockingStrength,
@@ -2341,6 +2357,7 @@ func (dsp *DistSQLPlanner) createPlanForLookupJoin(
 		// is late in the sense that the cost of this has not been taken into
 		// account. Make this decision earlier in CustomFuncs.GenerateLookupJoins.
 		MaintainOrdering:                  len(n.reqOrdering) > 0 || n.isFirstJoinInPairedJoiner,
+		MaintainLookupOrdering:            maintainLookupOrdering,
 		LeftJoinWithPairedJoiner:          n.isSecondJoinInPairedJoiner,
 		OutputGroupContinuationForLeftRow: n.isFirstJoinInPairedJoiner,
 		LookupBatchBytesLimit:             dsp.distSQLSrv.TestingKnobs.JoinReaderBatchBytesLimit,
