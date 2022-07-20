57ffb02ac4e8fda086e93d4f2e02dac52f2f492b
Sean Barag <barag@cockroachlabs.com>
2022-07-20 22:14:59 +0000

    tc: try using progress blocks
    Does this replace the opaque "command line" phase? Let's find out!
    
    Release note (<category, see below>): <what> <show> <why>

--- build/teamcity-cypress-health.sh
+++ build/teamcity-cypress-health.sh
@@ -6,32 +6,30 @@ source "$(dirname "${0}")/teamcity-support.sh"
 
 tc_prepare
 
-tc_start_block "Build Docker image"
+tc_start_progress_block "Build Docker image"
 # Buffer noisy output and only print it on failure.
-DOCKER_BUILDKIT=1 docker build \
+run DOCKER_BUILDKIT=1 docker build \
   -f ./pkg/ui/workspaces/e2e-tests/Dockerfile \
   -t cockroachdb/cockroach-ci-ui \
   --progress=plain \
   $PWD &> artifacts/docker-build.log || (cat artifacts/docker-build.log && false)
 rm artifacts/docker-build.log
-tc_end_block "Build Docker image"
+tc_end_progress_block "Build Docker image"
 
 # Expect the timeout to come from the TC environment.
 TESTTIMEOUT=${TESTTIMEOUT:-20m}
 
-tc_start_block "Prepare cockroach binary"
 # TeamCity doesn't restore permissions for files retrieved from artifact
 # dependencies, so ensure the cockroach binary is executable before running it
 # in a Docker container.
 chmod a+x upstream_artifacts/cockroach
-tc_end_block "Prepare cockroach binary"
 
-tc_start_block "Run Cypress health checks"
-docker run \
+tc_start_progress_block "Run Cypress health checks"
+run docker run \
   --rm \
   -v $PWD/upstream_artifacts:/upstream_artifacts \
   -v $PWD/artifacts:/artifacts \
   cockroachdb/cockroach-ci-ui \
   --reporter teamcity \
   --spec 'cypress/e2e/health-check/**'
-tc_end_block "Run Cypress health checks"
+tc_end_progress_block "Run Cypress health checks"
--- build/teamcity/util.sh
+++ build/teamcity/util.sh
@@ -7,3 +7,13 @@ tc_start_block() {
 tc_end_block() {
   echo "##teamcity[blockClosed name='$1']"
 }
+
+tc_start_progress_block() {
+  tc_start_block $1
+  echo "##teamcity[progressStart '$1']"
+}
+
+tc_end_progress() {
+  echo "##teamcity[progressEnd '$1']"
+  tc_end_block $1
+}
