6300c5703dfe6b60fbb2960a0ce482e0bb0e4ef0
wenyihu3 <wenyi.hu@cockroachlabs.com>
2022-07-20 21:01:56 -0400

    sql: adds is_visible to indexes info table
    This PR adds a new column `is_visible` to `crdb_internal.table_indexes` and
    `information_schema.statistics`. It also adds a new column `visible` to the
    output of following SQL statements:
    
    ```
    SHOW INDEX FROM (table_name)
    SHOW INDEXES FROM (table_name)
    SHOW KEYS FROM (table_name)
    SHOW INDEX FROM DATABASE (database_name)
    SHOW INDEXES FROM DATABASE (database_name)
    SHOW KEYS FROM DATABASE (database_name)
    ```
    
    Since the not visible index feature has not been introduced yet, it is expected
    for all test cases to output `true` for all `is_visible` or `visible` columns.
    
    See also: https://github.com/cockroachdb/cockroach/pull/83471
    
    Assists: https://github.com/cockroachdb/cockroach/issues/72576
    
    Release note (sql change): A new column `is_visible` has been added to the table
    `crdb_internal.table_indexes` and `information_schema.statistics`. A new column
    `visible` has also been added to the output of `SHOW INDEX`, `SHOW INDEXES`, and
    `SHOW KEYS`. The `is_visible` or `visible` column indicates whether the index is
    visible to the optimizer.

--- pkg/sql/crdb_internal.go
+++ pkg/sql/crdb_internal.go
@@ -2781,6 +2781,7 @@ CREATE TABLE crdb_internal.table_indexes (
   is_unique           BOOL NOT NULL,
   is_inverted         BOOL NOT NULL,
   is_sharded          BOOL NOT NULL,
+  is_visible          BOOL NOT NULL,
   shard_bucket_count  INT,
   created_at          TIMESTAMP
 )
@@ -2826,6 +2827,7 @@ CREATE TABLE crdb_internal.table_indexes (
 							tree.MakeDBool(tree.DBool(idx.IsUnique())),
 							tree.MakeDBool(idx.GetType() == descpb.IndexDescriptor_INVERTED),
 							tree.MakeDBool(tree.DBool(idx.IsSharded())),
+							tree.MakeDBool(tree.DBool(!idx.IsNotVisible())),
 							shardBucketCnt,
 							createdAt,
 						)
