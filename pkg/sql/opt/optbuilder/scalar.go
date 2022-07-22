a4be96d7ec8c71ed1e8d64fac21abba5e8dce681
Chengxiong Ruan <chengxiongruan@gmail.com>
2022-07-21 23:17:48 -0400

    sql/opt: optbuilder support for CREATE FUNCTION
    optbuilder is extended to accept the `CREATE FUNCTION` statement
    to resolve all referenced objects for dependencies tracking.
    A plan node is generated as the output.
    
    Release note: None.

--- pkg/sql/opt/optbuilder/scalar.go
+++ pkg/sql/opt/optbuilder/scalar.go
@@ -560,7 +560,7 @@ func (b *Builder) buildFunction(
 	}
 
 	// Add a dependency on sequences that are used as a string argument.
-	if b.trackViewDeps {
+	if b.trackSchemaDeps {
 		seqIdentifier, err := seqexpr.GetSequenceFromFunc(f, builtinsregistry.GetBuiltinProperties)
 		if err != nil {
 			panic(err)
@@ -569,7 +569,7 @@ func (b *Builder) buildFunction(
 			var ds cat.DataSource
 			if seqIdentifier.IsByID() {
 				flags := cat.Flags{
-					AvoidDescriptorCaches: b.insideViewDef,
+					AvoidDescriptorCaches: b.insideViewDef || b.insideFuncDef,
 				}
 				ds, _, err = b.catalog.ResolveDataSourceByID(b.ctx, flags, cat.StableID(seqIdentifier.SeqID))
 				if err != nil {
@@ -579,7 +579,7 @@ func (b *Builder) buildFunction(
 				tn := tree.MakeUnqualifiedTableName(tree.Name(seqIdentifier.SeqName))
 				ds, _, _ = b.resolveDataSource(&tn, privilege.SELECT)
 			}
-			b.viewDeps = append(b.viewDeps, opt.ViewDep{
+			b.schemaDeps = append(b.schemaDeps, opt.SchemaDep{
 				DataSource: ds,
 			})
 		}
