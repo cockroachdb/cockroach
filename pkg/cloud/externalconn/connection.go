e78fd921919537620a9347d43fe572861076ac78
Aditya Maru <adityamaru@gmail.com>
2022-07-22 17:23:24 -0400

    externalconn,nodelocal: add `external` ExternalStorage provider
    In #84310 we added the ability to create an external connection
    to represent a `nodelocal` endpoint. This diff is the second piece
    of the puzzle that allows systems in CRDB to interact with the
    external connection object.
    
    We introduce an `external` URI scheme to the `ExternalStorage`
    registry. URIs with an `external` scheme are required to contain a
    host component referring to the unique name of an existing extenral
    connection object. Optionally, the URI can also contain a path
    component that will be appended to the endpoint that was specified
    at the time the external connection object was created. This is necessary
    for operations such as backup and restore that read/write to subdirectories
    in the endpoint inputted by the user. A nice UX win is the abilility to
    have a single external connection object for the base bucket, and then
    interact with all the subdirectories without having to create an object
    for each directory. In the future we may want to clamp down on this, and
    allow the user to specify which objects permit subdirectory access.
    
    The `external://<object-name>/<optional-path>` URI is parsed and the
    underlying object is fetched from the `system.external_connections` table.
    The resource represented by the object is then `Dial()`ed to return an
    `ExternalStorage` handle that can be used to read, write, list etc. With
    this change all bulk operations and cdc are able to use external connections
    to represent a `nodelocal` endpoint. For example, a backup can now be run as:
    
    ```
    CREATE EXTERNAL CONNECTION foo AS 'nodelocal://1/foo';
    BACKUP INTO 'external://foo';
    RESTORE FROM LATEST IN 'external://foo';
    ```
    
    Gradually, we will add support for all other external storage endpoints
    as well.
    
    Note, we do not register limiter settings for the `external` provider
    `ExternalStorage` interface, nor do we wrap it with an ioRecorder. This is
    because the underlying resource of the external connection object will already
    have its registered limiters and recorder.
    
    Informs: #84753
    
    Release note (sql change): Bulk operations and CDC will accept an `external`
    scheme URI that points to a previously created External Connection object,
    These operations can then interact with the underlying resource represented by
    the object as they did before.

--- pkg/cloud/externalconn/connection.go
+++ pkg/cloud/externalconn/connection.go
@@ -14,19 +14,36 @@ import (
 	"context"
 	"net/url"
 
+	"github.com/cockroachdb/cockroach/pkg/cloud"
 	"github.com/cockroachdb/cockroach/pkg/cloud/externalconn/connectionpb"
 )
+
+// Connection is a marker interface for objects that support interaction with an
+// external resource.
+type Connection interface{}
 
 // ConnectionDetails is the interface to the external resource represented by an
 // External Connection object.
 type ConnectionDetails interface {
+	// Dial establishes a connection to the external resource.
+	//
+	// A non-empty subdir results in a `Connection` to the joined path of the
+	// endpoint represented by the external connection and subdir.
+	Dial(ctx context.Context, args cloud.ExternalStorageContext, subdir string) (Connection, error)
 	// ConnectionProto prepares the ConnectionDetails for serialization.
 	ConnectionProto() *connectionpb.ConnectionDetails
 	// ConnectionType returns the type of the connection.
 	ConnectionType() connectionpb.ConnectionType
 }
 
-// ConnectionDetailsFromURIFactory is the factory method that takes in an
-// endpoint URI for an external resource, and returns a ConnectionDetails
-// interface to interact with it.
-type ConnectionDetailsFromURIFactory func(ctx context.Context, uri *url.URL) (ConnectionDetails, error)
+// connectionParserFactory is the factory method that takes in an endpoint URI
+// for an external resource, and returns the ConnectionDetails proto
+// representation of that URI.
+type connectionParserFactory func(ctx context.Context,
+	uri *url.URL) (connectionpb.ConnectionDetails, error)
+
+// connectionDetailsFactory is the factory method that returns a
+// ConnectionDetails interface to interact with the underlying External
+// Connection object.
+type connectionDetailsFactory func(ctx context.Context,
+	details connectionpb.ConnectionDetails) ConnectionDetails
