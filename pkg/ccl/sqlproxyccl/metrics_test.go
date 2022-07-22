24211f08ed337098494d40d6e8f27dc513782834
Jeff <swenson@cockroachlabs.com>
2022-07-22 16:51:14 -0400

    sqlproxyccl: track which side broke the connection
    Previously, the sqlproxy considered an error when copying from the
    client->server as a client error and an error when copying from the
    server->client as a server error. This logic is incorrect as either side
    could be the source of the broken connection.
    
    Now, a wrapper is applied to the connection, and the errors returned by
    the wrapper are used to properly account for which side broke the
    connection.
    
    Release note: None

---
+++ pkg/ccl/sqlproxyccl/metrics_test.go
@@ -0,0 +1,59 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package sqlproxyccl
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/metric"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMetricsUpdateForError(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	m := makeProxyMetrics()
+	type testCase struct {
+		code     errorCode
+		counters []*metric.Counter
+	}
+	tests := []testCase{
+		{codeClientReadFailed, []*metric.Counter{m.ClientDisconnectCount}},
+		{codeClientWriteFailed, []*metric.Counter{m.ClientDisconnectCount}},
+		{codeClientDisconnected, []*metric.Counter{m.ClientDisconnectCount}},
+
+		{codeBackendDisconnected, []*metric.Counter{m.BackendDisconnectCount}},
+		{codeBackendReadFailed, []*metric.Counter{m.BackendDisconnectCount}},
+		{codeBackendWriteFailed, []*metric.Counter{m.BackendDisconnectCount}},
+
+		{codeExpiredClientConnection, []*metric.Counter{m.ExpiredClientConnCount}},
+
+		{codeProxyRefusedConnection, []*metric.Counter{m.RefusedConnCount, m.BackendDownCount}},
+
+		{codeParamsRoutingFailed, []*metric.Counter{m.RoutingErrCount, m.BackendDownCount}},
+		{codeUnavailable, []*metric.Counter{m.RoutingErrCount, m.BackendDownCount}},
+
+		{codeBackendDown, []*metric.Counter{m.BackendDownCount}},
+
+		{codeAuthFailed, []*metric.Counter{m.AuthFailedCount}},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.code.String(), func(t *testing.T) {
+			var before []int64
+			for _, counter := range tc.counters {
+				before = append(before, counter.Count())
+			}
+			m.updateForError(newErrorf(tc.code, "test error"))
+			for i, counter := range tc.counters {
+				require.Equal(t, counter.Count(), before[i]+1)
+			}
+		})
+	}
+}
