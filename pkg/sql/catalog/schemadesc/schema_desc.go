8b9abf12f690b6309bd3fc607c1c3896c3edefdf
Chengxiong Ruan <chengxiongruan@gmail.com>
2022-07-21 22:54:11 -0400

    sql: add Function descriptor proto and interfaces.
    This commit adding the new FunctionDescriptor proto and
    proper descriptor relevant interfaces to access descriptor
    attribute or make edit to the descriptor. Schema descriptor
    is also modified to hold function signatures.
    
    Release note: None.

--- pkg/sql/catalog/schemadesc/schema_desc.go
+++ pkg/sql/catalog/schemadesc/schema_desc.go
@@ -57,6 +57,16 @@ func (desc *immutable) SafeMessage() string {
 	return formatSafeMessage("schemadesc.immutable", desc)
 }
 
+func (desc *immutable) GetFunction(name string) (bool, descpb.SchemaDescriptor_Function) {
+	fn, found := desc.Functions[name]
+	return found, fn
+}
+
+// SkipNamespace implements the descriptor interface.
+func (desc *immutable) SkipNamespace() bool {
+	return false
+}
+
 // SafeMessage makes Mutable a SafeMessager.
 func (desc *Mutable) SafeMessage() string {
 	return formatSafeMessage("schemadesc.Mutable", desc)
