2db02f489e80dc2656d0ea2c74c6ca3d6047322f
Rafi Shamim <rafi@cockroachlabs.com>
2022-07-25 15:21:38 -0400

    sql: add is_grantable to SHOW DEFAULT PRIVILEGES
    Release note (sql change): The SHOW DEFAULT PRIVILEGES command now has a
    column that says if the default privilege will give the grant option to
    the grantee.

--- pkg/sql/delegate/show_default_privileges.go
+++ pkg/sql/delegate/show_default_privileges.go
@@ -35,7 +35,8 @@ func (d *delegator) delegateShowDefaultPrivileges(
 	}
 
 	query := fmt.Sprintf(
-		"SELECT role, for_all_roles, object_type, grantee, privilege_type FROM crdb_internal.default_privileges WHERE database_name = %s%s",
+		"SELECT role, for_all_roles, object_type, grantee, privilege_type, is_grantable "+
+			"FROM crdb_internal.default_privileges WHERE database_name = %s%s",
 		lexbase.EscapeSQLString(currentDatabase.Normalize()),
 		schemaClause,
 	)
