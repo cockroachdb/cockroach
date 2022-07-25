3a92ff116803892051ee4a4471c2a8abdd15115c
Marcus Gartner <marcus@cockroachlabs.com>
2022-07-25 14:47:41 -0400

    opt: add optbuilder support for multi-statement UDFs
    User-defined functions with multiple statements in the function body can
    now be built by optbuilder.
    
    Release note: None

--- pkg/sql/opt/memo/expr.go
+++ pkg/sql/opt/memo/expr.go
@@ -101,6 +101,9 @@ type RelExpr interface {
 	setNext(e RelExpr)
 }
 
+// RelListExpr is an ordered list of relational expressions.
+type RelListExpr []RelExpr
+
 // ScalarPropsExpr is implemented by scalar expressions which cache scalar
 // properties, like FiltersExpr and ProjectionsExpr. These expressions are also
 // tagged with the ScalarProps tag.
