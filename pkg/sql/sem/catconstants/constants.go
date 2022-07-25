d20769517cf45a5ee9e721b2328f9b8fd76cb80f
Matthew Todd <todd@cockroachlabs.com>
2022-07-25 10:32:38 -0400

    insights: crdb_internal.cluster_execution_insights
    Here we introduce a new virtual table for a cluster-wide view of
    "insights," a subsystem of sqlstats that is currently disabled by
    default but that will identify slow- and slower-than-usual statement
    executions, along with other potentially problematic behaviors we will
    be building support for.
    
    This table will back the upcoming insights UI over the new SQL-over-HTTP
    endpoint.
    
    Release note (sql change): A new crdb_internal virtual table,
    cluster_execution_insights, was introduced, offering a cluster-wide view
    of the same node-local information available in node_execution_insights.
    The insights subsystem is, as of this commit, still under development
    and disabled by default, so there will not yet be much to see here.

--- pkg/sql/sem/catconstants/constants.go
+++ pkg/sql/sem/catconstants/constants.go
@@ -97,6 +97,7 @@ const (
 	CrdbInternalClusterContendedTablesViewID
 	CrdbInternalClusterContentionEventsTableID
 	CrdbInternalClusterDistSQLFlowsTableID
+	CrdbInternalClusterExecutionInsightsTableID
 	CrdbInternalClusterLocksTableID
 	CrdbInternalClusterQueriesTableID
 	CrdbInternalClusterTransactionsTableID
