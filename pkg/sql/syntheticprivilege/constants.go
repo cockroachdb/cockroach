92f2e26cac0908b62afed4eb71ea965e446558ad
richardjcai <caioftherichard@gmail.com>
2022-07-26 19:10:53 -0400

    sql: add caching for synthetic privileges
    Also minor moving around of code
    
    Release note: None

---
+++ pkg/sql/syntheticprivilege/constants.go
@@ -0,0 +1,16 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package syntheticprivilege
+
+import "github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+
+// SystemPrivilegesTableName represents system.database_role_settings.
+var SystemPrivilegesTableName = tree.NewTableNameWithSchema("system", tree.PublicSchemaName, "privileges")
