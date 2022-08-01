fe819255a34eff2fa6ae4c73e30a75769cb53ce4
Yahor Yuzefovich <yahor@cockroachlabs.com>
2022-08-01 15:58:17 -0700

    colexec: fix recent accounting leak in the hash joiner when it spills
    This commit fixes yet another bug introduced in
    935090df5767cd7fe314a028246e465993e9b250 which would lead to a leak in
    the memory accounting system when the hash joiner spills to disk. That
    commit introduced accounting for a couple of internal slices, but we
    forgot that the hash joiner can be reused multiple times (when it is
    used by the external hash joiner), so we'd incorrectly grow the memory
    usage for each partition of the external hash joiner. This is now fixed.
    
    Release note: None

--- pkg/sql/colexec/colexecjoin/hashjoiner.go
+++ pkg/sql/colexec/colexecjoin/hashjoiner.go
@@ -229,6 +229,13 @@ type hashJoiner struct {
 		rowIdx int
 	}
 
+	// accountedFor tracks how much memory the hash joiner has accounted for so
+	// far related to some of the internal slices in the hash table.
+	accountedFor struct {
+		same    int64
+		visited int64
+	}
+
 	exportBufferedState struct {
 		hashTableReleased  bool
 		rightExported      int
@@ -327,18 +334,22 @@ func (hj *hashJoiner) build() {
 		// they have separate collectLeftAnti method.
 		hj.ht.Same = colexecutils.MaybeAllocateUint64Array(hj.ht.Same, hj.ht.Vals.Length()+1)
 		// At this point, we have fully built the hash table on the right side
-		// (meaning we have fully consumed the righ input), so it'd be a shame
+		// (meaning we have fully consumed the right input), so it'd be a shame
 		// to fallback to disk, thus, we use the unlimited allocator.
-		hj.outputUnlimitedAllocator.AdjustMemoryUsage(memsize.Uint64 * int64(cap(hj.ht.Same)))
+		newAccountedFor := memsize.Uint64 * int64(cap(hj.ht.Same))
+		hj.outputUnlimitedAllocator.AdjustMemoryUsage(newAccountedFor - hj.accountedFor.same)
+		hj.accountedFor.same = newAccountedFor
 	}
 	if !hj.spec.rightDistinct || hj.spec.JoinType.IsSetOpJoin() {
 		// visited slice is also used for set-operation joins, regardless of
 		// the fact whether the right side is distinct.
 		hj.ht.Visited = colexecutils.MaybeAllocateBoolArray(hj.ht.Visited, hj.ht.Vals.Length()+1)
 		// At this point, we have fully built the hash table on the right side
-		// (meaning we have fully consumed the righ input), so it'd be a shame
+		// (meaning we have fully consumed the right input), so it'd be a shame
 		// to fallback to disk, thus, we use the unlimited allocator.
-		hj.outputUnlimitedAllocator.AdjustMemoryUsage(memsize.Bool * int64(cap(hj.ht.Visited)))
+		newAccountedFor := memsize.Bool * int64(cap(hj.ht.Visited))
+		hj.outputUnlimitedAllocator.AdjustMemoryUsage(newAccountedFor - hj.accountedFor.visited)
+		hj.accountedFor.visited = newAccountedFor
 		// Since keyID = 0 is reserved for end of list, it can be marked as visited
 		// at the beginning.
 		hj.ht.Visited[0] = true
@@ -752,6 +763,10 @@ func (hj *hashJoiner) Reset(ctx context.Context) {
 		}
 	}
 	hj.state = hjBuilding
+	// Note that hj.ht.Reset() doesn't reset hj.ht.Same and hj.ht.Visited
+	// slices, but we'll reset them manually in hj.build(). We also keep
+	// references to those slices, so we don't release any of the memory we've
+	// accounted for.
 	hj.ht.Reset(ctx)
 	// Note that we don't zero out hj.probeState.buildIdx,
 	// hj.probeState.probeIdx, and hj.probeState.probeRowUnmatched because the
