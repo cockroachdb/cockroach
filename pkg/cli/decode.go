9b6bda53165fb4bf5c8ec2eb2b3e5bed36d9e97f
Andrei Matei <andrei@cockroachlabs.com>
2022-08-01 16:47:24 -0400

    cli: teach decode-proto to handle Chrome protos
    This patch teaches `cockroach debug decode-protos` to help with
    replaying HTTP network requests captured by Chrome through its "copy as
    cURL" feature. Chrome inexplicably seems to mess up when copying our
    requests that POST protos: it encodes the raw bytes as UTF-8, which
    makes the POST data fail to decode as a proto. This patch makes
    decode-proto recognize such an encoding.
    
    The new fields --single --binary --out=<file> are added to help the use
    case of decoding a single proto and writing it to a binary file that can
    then be read by curl.
    
    Developed with Werner.
    
    Release note: None

--- pkg/cli/decode.go
+++ pkg/cli/decode.go
@@ -12,29 +12,92 @@ package cli
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/base64"
 	gohex "encoding/hex"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/cockroachdb/cockroach/pkg/sql/protoreflect"
+	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
 	"github.com/cockroachdb/errors"
 	"github.com/mattn/go-isatty"
 	"github.com/spf13/cobra"
 )
 
 func runDebugDecodeProto(_ *cobra.Command, _ []string) error {
+	if debugDecodeProtoBinaryOutput && debugDecodeProtoOutputFile == "" {
+		return errors.Errorf("--out is required when --binary is specified. Redirecting stdout is not " +
+			"supported because that can introduce a trailing newline character.")
+	}
+
 	if isatty.IsTerminal(os.Stdin.Fd()) {
 		fmt.Fprintln(stderr,
 			`# Reading proto-encoded pieces of data from stdin.
 # Press Ctrl+C or Ctrl+D to terminate.`,
 		)
 	}
-	return streamMap(os.Stdout, os.Stdin,
+	out := os.Stdout
+	if debugDecodeProtoOutputFile != "" {
+		var err error
+		out, err = os.OpenFile(debugDecodeProtoOutputFile, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if debugDecodeProtoSingleProto {
+		buf := bytes.NewBuffer(nil)
+		_, err := buf.ReadFrom(os.Stdin)
+		if err != nil {
+			return err
+		}
+		msg := tryDecodeValue(string(buf.Bytes()), debugDecodeProtoName)
+		if msg == nil {
+			return errors.Errorf("decoding failed")
+		}
+		bytes, err := protoutil.Marshal(msg)
+		if err != nil {
+			return err
+		}
+
+		if debugDecodeProtoBinaryOutput {
+			_, err := out.Write(bytes)
+			if err != nil {
+				return err
+			}
+		} else {
+			j, err := protoreflect.MessageToJSON(msg, protoreflect.FmtFlags{EmitDefaults: debugDecodeProtoEmitDefaults})
+			if err != nil {
+				// Unexpected error: the data was valid protobuf, but does not
+				// reflect back to JSON. We report the protobuf struct in the
+				// error message nonetheless.
+				return errors.Wrapf(err, "while JSON-encoding %#v", msg)
+			}
+			fmt.Fprint(out, j)
+		}
+		return nil
+	}
+
+	return streamMap(out, os.Stdin,
 		func(s string) (bool, string, error) {
-			return tryDecodeValue(s, debugDecodeProtoName, debugDecodeProtoEmitDefaults)
+			msg := tryDecodeValue(s, debugDecodeProtoName)
+			if msg == nil {
+				return false, "", nil
+			}
+
+			j, err := protoreflect.MessageToJSON(msg, protoreflect.FmtFlags{EmitDefaults: debugDecodeProtoEmitDefaults})
+			if err != nil {
+				// Unexpected error: the data was valid protobuf, but does not
+				// reflect back to JSON. We report the protobuf struct in the
+				// error message nonetheless.
+				return false, "", errors.Wrapf(err, "while JSON-encoding %#v", msg)
+			}
+			return true, j.String(), nil
 		})
 }
 
@@ -63,27 +126,69 @@ func streamMap(out io.Writer, in io.Reader, fn func(string) (bool, string, error
 	return sc.Err()
 }
 
+// interpretString decodes s from one of a couple of supported encodings:
+// - hex
+// - base-64
+// - Go (or C) quoted string
+func interpretString(s string) ([]byte, bool) {
+	// Try hex.
+	bytes, err := gohex.DecodeString(s)
+	if err == nil {
+		return bytes, true
+	}
+	// Try base64.
+	bytes, err = base64.StdEncoding.DecodeString(s)
+	if err == nil {
+		return bytes, true
+	}
+	// Try quoted string.
+	s = strings.TrimSpace(s)
+	// Remove wrapping quotes, if any.
+	if (strings.HasPrefix(s, "'") && strings.HasSuffix(s, "'")) ||
+		(strings.HasPrefix(s, "\"") && strings.HasSuffix(s, "\"")) {
+		s = s[1 : len(s)-1]
+	}
+	// Add wrapping quotes; strconv.Unquote requires them.
+	s = fmt.Sprintf("\"%s\"", s)
+	unquoted, err := strconv.Unquote(s)
+	if err == nil {
+		return []byte(unquoted), true
+	}
+	return nil, false
+}
+
 // tryDecodeValue tries to decode the given string with the given proto name
 // reports ok=false if the data was not valid proto-encoded.
-func tryDecodeValue(s, protoName string, emitDefaults bool) (ok bool, val string, err error) {
-	bytes, err := gohex.DecodeString(s)
-	if err != nil {
-		b, err := base64.StdEncoding.DecodeString(s)
-		if err != nil {
-			return false, "", nil //nolint:returnerrcheck
-		}
-		bytes = b
+func tryDecodeValue(s, protoName string) protoutil.Message {
+	bytes, ok := interpretString(s)
+	if !ok {
+		return nil
 	}
 	msg, err := protoreflect.DecodeMessage(protoName, bytes)
-	if err != nil {
-		return false, "", nil //nolint:returnerrcheck
+	if err == nil {
+		return msg
 	}
-	j, err := protoreflect.MessageToJSON(msg, protoreflect.FmtFlags{EmitDefaults: emitDefaults})
-	if err != nil {
-		// Unexpected error: the data was valid protobuf, but does not
-		// reflect back to JSON. We report the protobuf struct in the
-		// error message nonetheless.
-		return false, "", errors.Wrapf(err, "while JSON-encoding %#v", msg)
+
+	bytes, ok = convertFromUTF8(bytes)
+	if !ok {
+		return nil
 	}
-	return true, j.String(), nil
+	msg, err = protoreflect.DecodeMessage(protoName, bytes)
+	return msg
+}
+
+func convertFromUTF8(bytes []byte) (out []byte, ok bool) {
+	for len(bytes) > 0 {
+		// We expect only one-byte runes, which encode to one or two UTF-8 bytes.
+		// That's sufficient for how (I think) Chrome treats the raw bytes that it
+		// encodes to UTF-8.
+		got, n := utf8.DecodeRune(bytes)
+		if got > 0xff || n > 2 {
+			// Unexpected multi-byte rune.
+			return nil, false
+		}
+		out = append(out, byte(got))
+		bytes = bytes[n:]
+	}
+	return out, true
 }
