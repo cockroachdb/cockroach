35d5ef125eee48152019cc183941549fd7e6fc1d
Andrei Matei <andrei@cockroachlabs.com>
2022-08-01 16:47:24 -0400

    cli: teach decode-proto to handle Chrome protos
    This patch teaches `cockroach debug decode-protos` to help with
    replaying HTTP network requests captured by Chrome through its "copy as
    cURL" feature. Chrome inexplicably seems to mess up when copying our
    requests that POST protos: it encodes the raw bytes as UTF-8, which
    makes the POST data fail to decode as a proto. This patch makes
    decode-proto recognize such an encoding.
    
    The new fields --single --binary --out=<file> are added to help the use
    case of decoding a single proto and writing it to a binary file that can
    then be read by curl.
    
    Developed with Werner.
    
    Release note: None

--- pkg/cli/decode_test.go
+++ pkg/cli/decode_test.go
@@ -73,7 +73,7 @@ func TestStreamMap(t *testing.T) {
 func TestTryDecodeValue(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 
-	protoName := "cockroach.sql.sqlbase.TableDescriptor"
+	const defaultProtoType = "cockroach.sql.sqlbase.TableDescriptor"
 	marshal := func(pb protoutil.Message) []byte {
 		s, err := protoutil.Marshal(pb)
 		require.NoError(t, err)
@@ -87,10 +87,11 @@ func TestTryDecodeValue(t *testing.T) {
 	tableDesc := &descpb.TableDescriptor{ID: 42, ParentID: 7, Name: "foo"}
 
 	tests := []struct {
-		name    string
-		s       string
-		wantOK  bool
-		wantVal string
+		name      string
+		protoType string
+		s         string
+		wantOK    bool
+		wantVal   string
 	}{
 		{
 			name:    "from hex",
@@ -116,13 +117,35 @@ func TestTryDecodeValue(t *testing.T) {
 			name: "base64 not proto",
 			s:    base64.StdEncoding.EncodeToString([]byte("@#$@#%$%@")),
 		},
+		{
+			// This is the POST data of an HTTP tsdb query taken from Chrome using
+			// "Copy as cURL". It is a quoted string, containing UTF-8 encoded bytes.
+			name:      "Chrome-encoded",
+			s:         `\u0008\u0080ì¿ùÛ\u008bù\u0083\u0017\u0010\u0080¬¢ÿ¾ôù\u0083\u0017\u001a \n\u0018cr.node.sql.select.count\u0010\u0001\u0018\u0002 \u0002\u001a \n\u0018cr.node.sql.update.count\u0010\u0001\u0018\u0002 \u0002\u001a \n\u0018cr.node.sql.insert.count\u0010\u0001\u0018\u0002 \u0002\u001a \n\u0018cr.node.sql.delete.count\u0010\u0001\u0018\u0002 \u0002\u001a*\n\u001fcr.node.sql.service.latency-p99\u0010\u0003\u0018\u0002 \u0000*\u00011\u001a3\n+cr.node.sql.distsql.contended_queries.count\u0010\u0001\u0018\u0002 \u0002\u001a\u001c\n\u0011cr.store.replicas\u0010\u0001\u0018\u0002 \u0000*\u00011\u001a\u0019\n\u0011cr.store.capacity\u0010\u0001\u0018\u0002 \u0000\u001a#\n\u001bcr.store.capacity.available\u0010\u0001\u0018\u0002 \u0000\u001a\u001e\n\u0016cr.store.capacity.used\u0010\u0001\u0018\u0002 \u0000 \u0080Ø\u008eáo`,
+			wantOK:    true,
+			protoType: "cockroach.ts.tspb.TimeSeriesQueryRequest",
+			wantVal:   `{"endNanos": "1659549679000000000", "queries": [{"derivative": "NON_NEGATIVE_DERIVATIVE", "downsampler": "AVG", "name": "cr.node.sql.select.count", "sourceAggregator": "SUM", "sources": []}, {"derivative": "NON_NEGATIVE_DERIVATIVE", "downsampler": "AVG", "name": "cr.node.sql.update.count", "sourceAggregator": "SUM", "sources": []}, {"derivative": "NON_NEGATIVE_DERIVATIVE", "downsampler": "AVG", "name": "cr.node.sql.insert.count", "sourceAggregator": "SUM", "sources": []}, {"derivative": "NON_NEGATIVE_DERIVATIVE", "downsampler": "AVG", "name": "cr.node.sql.delete.count", "sourceAggregator": "SUM", "sources": []}, {"derivative": "NONE", "downsampler": "MAX", "name": "cr.node.sql.service.latency-p99", "sourceAggregator": "SUM", "sources": ["1"]}, {"derivative": "NON_NEGATIVE_DERIVATIVE", "downsampler": "AVG", "name": "cr.node.sql.distsql.contended_queries.count", "sourceAggregator": "SUM", "sources": []}, {"derivative": "NONE", "downsampler": "AVG", "name": "cr.store.replicas", "sourceAggregator": "SUM", "sources": ["1"]}, {"derivative": "NONE", "downsampler": "AVG", "name": "cr.store.capacity", "sourceAggregator": "SUM", "sources": []}, {"derivative": "NONE", "downsampler": "AVG", "name": "cr.store.capacity.available", "sourceAggregator": "SUM", "sources": []}, {"derivative": "NONE", "downsampler": "AVG", "name": "cr.store.capacity.used", "sourceAggregator": "SUM", "sources": []}], "sampleNanos": "30000000000", "startNanos": "1659546079000000000"}`,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotOk, gotVal, err := tryDecodeValue(tt.s, protoName, true /* emitDefaults */)
-			require.Equal(t, tt.wantOK, gotOk)
+			protoType := tt.protoType
+			if protoType == "" {
+				protoType = defaultProtoType
+			}
+			msg := tryDecodeValue(tt.s, protoType)
+			if !tt.wantOK {
+				if msg != nil {
+					t.Fatal("decoding succeeded unexpectedly")
+				}
+				return
+			}
+			if msg == nil {
+				t.Fatal("decoding failed")
+			}
+			json, err := protoreflect.MessageToJSON(msg, protoreflect.FmtFlags{EmitDefaults: true})
 			require.NoError(t, err)
-			require.Equal(t, gotVal, tt.wantVal)
+			require.Equal(t, tt.wantVal, json.String())
 		})
 	}
 }
