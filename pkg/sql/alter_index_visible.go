5a57f902f9ad0178f5bbc1536e7891ab87e32e34
wenyihu3 <wenyi.hu@cockroachlabs.com>
2022-08-02 00:07:33 -0400

    sql: support ALTER INDEX … NOT VISIBLE
    This commit adds the actual execution for ALTER INDEX … [VISIBLE | NOT VISIBLE].
    
    Assists: https://github.com/cockroachdb/cockroach/issues/72576
    
    See also: https://github.com/cockroachdb/cockroach/pull/84783
    
    Release note (sql change): Altering an index to visible or not visible using
    ALTER INDEX … VISIBLE | NOT VISIBLE is now supported.

--- pkg/sql/alter_index_visible.go
+++ pkg/sql/alter_index_visible.go
@@ -3,10 +3,15 @@ package sql
 import (
 	"context"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/tabledesc"
-	"github.com/cockroachdb/cockroach/pkg/util/errorutil/unimplemented"
-
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgcode"
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgnotice"
+	"github.com/cockroachdb/cockroach/pkg/sql/privilege"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/util/log/eventpb"
+	"github.com/cockroachdb/errors"
 )
 
 type alterIndexVisibleNode struct {
@@ -16,9 +21,45 @@ type alterIndexVisibleNode struct {
 }
 
 func (p *planner) AlterIndexVisible(ctx context.Context, stmt *tree.AlterIndexVisible) (planNode, error) {
-	return nil, unimplemented.Newf(
-		"Not Visible Index",
-		"altering an index to visible or not visible is not supported yet")
+	if err := checkSchemaChangeEnabled(
+		ctx,
+		p.ExecCfg(),
+		"ALTER INDEX VISIBILITY",
+	); err != nil {
+		return nil, err
+	}
+
+	// Check if the table actually exists. expandMutableIndexName returns the
+	// underlying table for the index. If no table is found and IfExists is false,
+	// error is returned. Otherwise, tableDesc returned is nil.
+	_, tableDesc, err := expandMutableIndexName(ctx, p, stmt.Index, !stmt.IfExists /* requireTable */)
+	if err != nil {
+		// Error if no table is found and IfExists is false.
+		return nil, err
+	}
+
+	if tableDesc == nil {
+		// Nothing needed if no table is found and IfExists is true.
+		return newZeroNode(nil /* columns */), nil
+	}
+
+	// Now we know this table exists, check if the index exists. FindIndexWithName
+	// returns error if none was found in the tableDesc.
+	idx, err := tableDesc.FindIndexWithName(string(stmt.Index.Index))
+	if err != nil {
+		if stmt.IfExists {
+			// Nothing needed if no index exists and IfExists is true.
+			return newZeroNode(nil /* columns */), nil
+		}
+		// Error if no index exists and IfExists is not specified.
+		return nil, pgerror.WithCandidateCode(err, pgcode.UndefinedObject)
+	}
+
+	if err := p.CheckPrivilege(ctx, tableDesc, privilege.CREATE); err != nil {
+		return nil, err
+	}
+
+	return &alterIndexVisibleNode{n: stmt, tableDesc: tableDesc, index: idx}, nil
 }
 
 // TODO (wenyihu6): Double check on whether we should have ReadingOwnWrites. I
@@ -28,9 +69,57 @@ func (p *planner) AlterIndexVisible(ctx context.Context, stmt *tree.AlterIndexVi
 // func (n *alterIndexVisibleNode) ReadingOwnWrites() {}
 
 func (n *alterIndexVisibleNode) startExec(params runParams) error {
-	return unimplemented.Newf(
-		"Not Visible Index",
-		"altering an index to visible or not visible is not supported yet")
+	if n.n.NotVisible {
+		if n.index.Primary() {
+			return errors.WithHint(
+				pgerror.Newf(pgcode.FeatureNotSupported, "primary index cannot be invisible"),
+				"instead, use ALTER TABLE ... ALTER PRIMARY KEY or ADD CONSTRAINT ... PRIMARY KEY "+
+					"and change the secondary index invisible",
+			)
+		}
+	}
+
+	if n.index.IsNotVisible() == n.n.NotVisible {
+		// Nothing needed if the index is already what they want.
+		str := ""
+		if n.n.NotVisible {
+			str = "not "
+		}
+		params.p.BufferClientNotice(
+			params.ctx,
+			pgnotice.Newf("index is already "+str+"visible"),
+		)
+		return nil
+	}
+
+	n.index.IndexDesc().NotVisible = n.n.NotVisible
+
+	if err := validateDescriptor(params.ctx, params.p, n.tableDesc); err != nil {
+		return err
+	}
+
+	if err := params.p.writeSchemaChange(
+		params.ctx, n.tableDesc, descpb.InvalidMutationID, tree.AsStringWithFQNames(n.n, params.Ann()),
+	); err != nil {
+		return err
+	}
+
+	// Warn against dropping an index if there exists a NotVisible index that may
+	// be used for constraint check behind the scene.
+	if notVisibleIndexNotice := tabledesc.ValidateNotVisibleIndexWithinTable(n.tableDesc); notVisibleIndexNotice != nil {
+		params.p.BufferClientNotice(
+			params.ctx,
+			notVisibleIndexNotice,
+		)
+	}
+
+	return params.p.logEvent(params.ctx,
+		n.tableDesc.ID,
+		&eventpb.AlterIndexVisible{
+			TableName:  n.n.Index.Table.FQString(),
+			IndexName:  n.index.GetName(),
+			NotVisible: n.n.NotVisible,
+		})
 }
 func (n *alterIndexVisibleNode) Next(runParams) (bool, error) { return false, nil }
 func (n *alterIndexVisibleNode) Values() tree.Datums          { return tree.Datums{} }
