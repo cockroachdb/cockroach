cockroachdb/cockroach

commit 6535dc634606d4767b581f2b4cd74a0386b30d05

colexechash: improve memory accounting in the hash table
This commit improves the memory accounting in the hash table to be more
precise in the case when the `distsql_workmem` limit is exhausted.
Previously, we would allocate large slices first only to perform the
memory accounting after the fact, possibly running out of budget which
would result in a error being thrown. We'd end up in a situation where
the hash table is still referencing larger newly-allocated slices while
only the previous memory usage is accounted for. This commit makes it so
that we account for the needed capacity upfront, then perform the
allocation, and then reconcile the accounting if necessary. This way
we're much more likely to encounter the budget error before making the
large allocations.

Additionally, this commit accounts for some internal slices in the hash
table used only in the hash joiner case.

Also, now both the hash aggregator and the hash joiner eagerly release
references to these internal slices of the hash table when the spilling
to disk occurs (we cannot do the same for the unordered distinct because
there the hash table is actually used after the spilling too).

This required a minor change to the way the unordered distinct spills to
disk. Previously, the memory error could only occur in two spots (and
one of those would leave the hash table in an inconsistent state and we
were "smart" in how we repaired that). However, now the memory error
could occur in more spots (and we could have several different
inconsistent states), so this commit chooses a slight performance
regression of simply rebuilding the hash table from scratch, once, when
the unordered distinct spills to disk.

Release note: None

pkg/sql/colexec/colexecjoin/hashjoiner.go
@@ -21,6 +21,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/colexecerror"
 	"github.com/cockroachdb/cockroach/pkg/sql/colexecop"
 	"github.com/cockroachdb/cockroach/pkg/sql/colmem"
+	"github.com/cockroachdb/cockroach/pkg/sql/memsize"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util/buildutil"
 	"github.com/cockroachdb/errors"
@@ -170,9 +171,9 @@ type hashJoinerSourceSpec struct {
 type hashJoiner struct {
 	*joinHelper
 
-	// buildSideAllocator should be used when building the hash table from the
+	// hashTableAllocator should be used when building the hash table from the
 	// right input.
-	buildSideAllocator *colmem.Allocator
+	hashTableAllocator *colmem.Allocator
 	// outputUnlimitedAllocator should *only* be used when populating the
 	// output when we have already fully built the hash table from the right
 	// input and are only populating output one batch at a time. If we were to
@@ -229,6 +230,7 @@ type hashJoiner struct {
 	}
 
 	exportBufferedState struct {
+		hashTableReleased  bool
 		rightExported      int
 		rightWindowedBatch coldata.Batch
 	}
@@ -258,7 +260,7 @@ func (hj *hashJoiner) Init(ctx context.Context) {
 	const hashTableLoadFactor = 1.0
 	hj.ht = colexechash.NewHashTable(
 		ctx,
-		hj.buildSideAllocator,
+		hj.hashTableAllocator,
 		hashTableLoadFactor,
 		hj.hashTableInitialNumBuckets,
 		hj.spec.Right.SourceTypes,
@@ -268,7 +270,7 @@ func (hj *hashJoiner) Init(ctx context.Context) {
 		probeMode,
 	)
 
-	hj.exportBufferedState.rightWindowedBatch = hj.buildSideAllocator.NewMemBatchWithFixedCapacity(
+	hj.exportBufferedState.rightWindowedBatch = hj.outputUnlimitedAllocator.NewMemBatchWithFixedCapacity(
 		hj.spec.Right.SourceTypes, 0, /* size */
 	)
 	hj.state = hjBuilding
@@ -324,11 +326,19 @@ func (hj *hashJoiner) build() {
 		// We don't need same with LEFT ANTI and EXCEPT ALL joins because
 		// they have separate collectLeftAnti method.
 		hj.ht.Same = colexecutils.MaybeAllocateUint64Array(hj.ht.Same, hj.ht.Vals.Length()+1)
+		// At this point, we have fully built the hash table on the right side
+		// (meaning we have fully consumed the righ input), so it'd be a shame
+		// to fallback to disk, thus, we use the unlimited allocator.
+		hj.outputUnlimitedAllocator.AdjustMemoryUsage(memsize.Uint64 * int64(cap(hj.ht.Same)))
 	}
 	if !hj.spec.rightDistinct || hj.spec.JoinType.IsSetOpJoin() {
 		// visited slice is also used for set-operation joins, regardless of
 		// the fact whether the right side is distinct.
 		hj.ht.Visited = colexecutils.MaybeAllocateBoolArray(hj.ht.Visited, hj.ht.Vals.Length()+1)
+		// At this point, we have fully built the hash table on the right side
+		// (meaning we have fully consumed the righ input), so it'd be a shame
+		// to fallback to disk, thus, we use the unlimited allocator.
+		hj.outputUnlimitedAllocator.AdjustMemoryUsage(memsize.Bool * int64(cap(hj.ht.Visited)))
 		// Since keyID = 0 is reserved for end of list, it can be marked as visited
 		// at the beginning.
 		hj.ht.Visited[0] = true
@@ -675,7 +685,13 @@ func (hj *hashJoiner) ExportBuffered(input colexecop.Operator) coldata.Batch {
 		// point we haven't requested a single batch from the left.
 		return coldata.ZeroBatch
 	} else if hj.inputTwo == input {
+		if hj.exportBufferedState.hashTableReleased {
+			return coldata.ZeroBatch
+		}
 		if hj.exportBufferedState.rightExported == hj.ht.Vals.Length() {
+			// We no longer need the hash table, so we can release it.
+			hj.ht.Release()
+			hj.exportBufferedState.hashTableReleased = true
 			return coldata.ZeroBatch
 		}
 		newRightExported := hj.exportBufferedState.rightExported + coldata.BatchSize()
@@ -743,6 +759,14 @@ func (hj *hashJoiner) Reset(ctx context.Context) {
 	// hj.probeState.buildRowMatched is reset after building the hash table is
 	// complete in build() method.
 	hj.emittingRightState.rowIdx = 0
+	if buildutil.CrdbTestBuild {
+		if hj.exportBufferedState.hashTableReleased {
+			colexecerror.InternalError(errors.AssertionFailedf(
+				"the hash joiner is being reset after having spilled to disk",
+			))
+		}
+	}
+	hj.exportBufferedState.hashTableReleased = false
 	hj.exportBufferedState.rightExported = 0
 }
 
@@ -811,7 +835,7 @@ func MakeHashJoinerSpec(
 
 // NewHashJoiner creates a new equality hash join operator on the left and
 // right input tables.
-// buildSideAllocator should use a limited memory account and will be used for
+// hashTableAllocator should use a limited memory account and will be used for
 // the build side whereas outputUnlimitedAllocator should use an unlimited
 // memory account and will only be used when populating the output.
 // memoryLimit will limit the size of the batches produced by the hash joiner.
@@ -823,7 +847,7 @@ func NewHashJoiner(
 ) colexecop.ResettableOperator {
 	return &hashJoiner{
 		joinHelper:                 newJoinHelper(leftSource, rightSource),
-		buildSideAllocator:         buildSideAllocator,
+		hashTableAllocator:         buildSideAllocator,
 		outputUnlimitedAllocator:   outputUnlimitedAllocator,
 		spec:                       spec,
 		outputTypes:                spec.JoinType.MakeOutputTypes(spec.Left.SourceTypes, spec.Right.SourceTypes),

pkg/sql/colexec/colexecdisk/external_distinct.go
@@ -158,7 +158,7 @@ func (f *unorderedDistinctFilterer) Next() coldata.Batch {
 			//
 			// See https://github.com/cockroachdb/cockroach/pull/58006#pullrequestreview-565859919
 			// for all the gory details.
-			f.ud.Ht.MaybeRepairAfterDistinctBuild()
+			f.ud.Ht.RepairAfterDistinctBuild()
 			f.ud.MaybeEmitErrorOnDup(f.ud.LastInputBatchOrigLen, batch.Length())
 			f.seenBatch = true
 			return batch

pkg/sql/colexec/colexechash/hashtable.go
@@ -277,6 +277,9 @@ var _ colexecop.Resetter = &HashTable{}
 
 // NewHashTable returns a new HashTable.
 //
+// - allocator must be the allocator that is owned by the hash table and not
+// shared with any other components.
+//
 // - loadFactor determines the average number of tuples per bucket which, if
 // exceeded, will trigger resizing the hash table. This number can have a
 // noticeable effect on the performance, so every user of the hash table should
@@ -387,6 +390,22 @@ func (ht *HashTable) shouldResize(numTuples int) bool {
 	return float64(numTuples)/float64(ht.numBuckets) > ht.loadFactor
 }
 
+// probeBufferInternalMaxMemUsed returns the maximum memory used by the slices
+// of hashTableProbeBuffer that are limited by coldata.BatchSize() in size.
+func probeBufferInternalMaxMemUsed() int64 {
+	// probeBufferInternalMaxMemUsed accounts for:
+	// - five uint64 slices:
+	//   - hashTableProbeBuffer.hashChains.Next
+	//   - hashTableProbeBuffer.ToCheck
+	//   - hashTableProbeBuffer.ToCheckID
+	//   - hashTableProbeBuffer.HeadID
+	//   - hashTableProbeBuffer.HashBuffer
+	// - two bool slices:
+	//   - hashTableProbeBuffer.differs
+	//   - hashTableProbeBuffer.distinct.
+	return memsize.Uint64*int64(5*coldata.BatchSize()) + memsize.Bool*int64(2*coldata.BatchSize())
+}
+
 // accountForLimitedSlices checks whether we have already accounted for the
 // memory used by the slices that are limited by coldata.BatchSize() in size
 // and adjusts the allocator accordingly if we haven't.
@@ -394,9 +413,18 @@ func (p *hashTableProbeBuffer) accountForLimitedSlices(allocator *colmem.Allocat
 	if p.limitedSlicesAreAccountedFor {
 		return
 	}
-	internalMemMaxUsed := memsize.Int64*int64(5*coldata.BatchSize()) + memsize.Bool*int64(2*coldata.BatchSize())
-	allocator.AdjustMemoryUsage(internalMemMaxUsed)
+	allocator.AdjustMemoryUsage(probeBufferInternalMaxMemUsed())
 	p.limitedSlicesAreAccountedFor = true
+}
+
+func (ht *HashTable) accountForUnlimitedSlices(newUint64Count int64) {
+	ht.allocator.AdjustMemoryUsage(memsize.Uint64 * (newUint64Count - ht.unlimitedSlicesNumUint64AccountedFor))
+	ht.unlimitedSlicesNumUint64AccountedFor = newUint64Count
+}
+
+func (ht *HashTable) unlimitedSlicesCapacity() int64 {
+	// Note that if ht.ProbeScratch.First is nil, it'll have zero capacity.
+	return int64(cap(ht.BuildScratch.First) + cap(ht.ProbeScratch.First) + cap(ht.BuildScratch.Next))
 }
 
 // buildFromBufferedTuples builds the hash table from already buffered tuples
@@ -406,24 +434,45 @@ func (ht *HashTable) buildFromBufferedTuples() {
 	for ht.shouldResize(ht.Vals.Length()) {
 		ht.numBuckets *= 2
 	}
+	// Account for memory used by the internal auxiliary slices that are limited
+	// in size.
+	ht.ProbeScratch.accountForLimitedSlices(ht.allocator)
+	// Figure out the minimum capacities of the unlimited slices before actually
+	// allocating then.
+	needCapacity := int64(ht.numBuckets) + int64(ht.Vals.Length()+1) // ht.BuildScratch.First + ht.BuildScratch.Next
+	if ht.ProbeScratch.First != nil {
+		needCapacity += int64(ht.numBuckets) // ht.ProbeScratch.First
+	}
+	if needCapacity > ht.unlimitedSlicesCapacity() {
+		// We'll need to allocate larger slices then we currently have, so
+		// perform the memory accounting for the anticipated memory usage. Note
+		// that it might not be precise, so we'll reconcile after the
+		// allocations below.
+		ht.accountForUnlimitedSlices(needCapacity)
+	}
+	// Perform the actual build.
+	ht.buildFromBufferedTuplesNoAccounting()
+	// Now ensure that the accounting is precise (cap's of the slices might
+	// exceed len's that we've accounted for).
+	ht.accountForUnlimitedSlices(ht.unlimitedSlicesCapacity())
+}
+
+// buildFromBufferedTuples builds the hash table from already buffered tuples in
+// ht.Vals according to the current number of buckets. No memory accounting is
+// performed, so this function is guaranteed to not throw a memory error.
+func (ht *HashTable) buildFromBufferedTuplesNoAccounting() {
 	ht.BuildScratch.First = colexecutils.MaybeAllocateUint64Array(ht.BuildScratch.First, int(ht.numBuckets))
 	if ht.ProbeScratch.First != nil {
 		ht.ProbeScratch.First = colexecutils.MaybeAllocateUint64Array(ht.ProbeScratch.First, int(ht.numBuckets))
 	}
+	// ht.BuildScratch.Next is used to store the computed hash value of each key.
+	ht.BuildScratch.Next = colexecutils.MaybeAllocateUint64Array(ht.BuildScratch.Next, ht.Vals.Length()+1)
+
 	for i, keyCol := range ht.keyCols {
 		ht.Keys[i] = ht.Vals.ColVec(int(keyCol))
 	}
-	// ht.BuildScratch.Next is used to store the computed hash value of each key.
-	ht.BuildScratch.Next = colexecutils.MaybeAllocateUint64Array(ht.BuildScratch.Next, ht.Vals.Length()+1)
 	ht.ComputeBuckets(ht.BuildScratch.Next[1:], ht.Keys, ht.Vals.Length(), nil /* sel */)
 	ht.buildNextChains(ht.BuildScratch.First, ht.BuildScratch.Next, 1 /* offset */, uint64(ht.Vals.Length()))
-	// Account for memory used by the internal auxiliary slices that are
-	// limited in size.
-	ht.ProbeScratch.accountForLimitedSlices(ht.allocator)
-	// Note that if ht.ProbeScratch.First is nil, it'll have zero capacity.
-	newUint64Count := int64(cap(ht.BuildScratch.First) + cap(ht.ProbeScratch.First) + cap(ht.BuildScratch.Next))
-	ht.allocator.AdjustMemoryUsage(memsize.Int64 * (newUint64Count - ht.unlimitedSlicesNumUint64AccountedFor))
-	ht.unlimitedSlicesNumUint64AccountedFor = newUint64Count
 }
 
 // FullBuild executes the entirety of the hash table build phase using the input
@@ -688,20 +737,18 @@ func (ht *HashTable) AppendAllDistinct(batch coldata.Batch) {
 	}
 }
 
-// MaybeRepairAfterDistinctBuild checks whether the hash table built via
-// DistinctBuild is in an inconsistent state and repairs it if so.
-func (ht *HashTable) MaybeRepairAfterDistinctBuild() {
-	// BuildScratch.Next has an extra 0th element not used by the tuples
-	// reserved for the end of the chain.
-	if len(ht.BuildScratch.Next) < ht.Vals.Length()+1 {
-		// The hash table in such a state that some distinct tuples were
-		// appended to ht.Vals, but 'Next' and 'First' slices were not updated
-		// accordingly.
-		numConsistentTuples := len(ht.BuildScratch.Next) - 1
-		lastBatchNumDistinctTuples := ht.Vals.Length() - numConsistentTuples
-		ht.BuildScratch.Next = append(ht.BuildScratch.Next, ht.ProbeScratch.HashBuffer[:lastBatchNumDistinctTuples]...)
-		ht.buildNextChains(ht.BuildScratch.First, ht.BuildScratch.Next, uint64(numConsistentTuples)+1, uint64(lastBatchNumDistinctTuples))
-	}
+// RepairAfterDistinctBuild rebuilds the hash table populated via DistinctBuild
+// in the case a memory error was thrown.
+func (ht *HashTable) RepairAfterDistinctBuild() {
+	// Note that we don't try to be smart and "repair" the already built hash
+	// table in which only the most recently added tuples might need to be
+	// "built". We do it this way because the memory error could have occurred
+	// in several spots making it harder to reason about what are the
+	// "consistent" tuples and what are those that need to be repaired. This is
+	// a minor performance hit, but it is done only once throughout the lifetime
+	// of the unordered distinct when it just spilled to disk, so the regression
+	// is ok.
+	ht.buildFromBufferedTuplesNoAccounting()
 }
 
 // checkCols performs a column by column checkCol on the key columns.
@@ -948,4 +995,20 @@ func (ht *HashTable) Reset(_ context.Context) {
 		// so we make the length 1).
 		ht.BuildScratch.Next = ht.BuildScratch.Next[:1]
 	}
+}
+
+// Release releases all of the slices used by the hash table as well as the
+// corresponding memory reservations.
+//
+// The hash table becomes unusable, so any method calls after Release() will
+// result in an undefined behavior.
+//
+// Note that this happens to implement the execreleasable.Releasable interface,
+// but this method serves a different purpose (namely we want to release the
+// hash table mid-execution, when the spilling to disk occurs), so the hash
+// table is not added to the slice of objects that are released on the flow
+// cleanup.
+func (ht *HashTable) Release() {
+	ht.allocator.ReleaseMemory(ht.allocator.Used())
+	*ht = HashTable{}
 }

pkg/sql/colexec/colexecjoin/BUILD.bazel
@@ -28,6 +28,7 @@ go_library(
         "//pkg/sql/colmem",
         "//pkg/sql/execinfra/execopnode",
         "//pkg/sql/execinfrapb",
+        "//pkg/sql/memsize",
         "//pkg/sql/sem/eval",
         "//pkg/sql/sem/tree",  # keep
         "//pkg/sql/types",

pkg/sql/colexec/hash_aggregator.go
@@ -431,6 +431,12 @@ func (op *hashAggregator) onlineAgg(b coldata.Batch) {
 }
 
 func (op *hashAggregator) ExportBuffered(input colexecop.Operator) coldata.Batch {
+	if op.ht != nil {
+		// This is the first call to ExportBuffered - release the hash table
+		// since we no longer need it.
+		op.ht.Release()
+		op.ht = nil
+	}
 	if !op.inputTrackingState.zeroBatchEnqueued {
 		// Per the contract of the spilling queue, we need to append a
 		// zero-length batch.

pkg/sql/colexec/unordered_distinct.go
@@ -33,7 +33,7 @@ func NewUnorderedDistinct(
 ) colexecop.ResettableOperator {
 	return &UnorderedDistinct{
 		OneInputNode:         colexecop.NewOneInputNode(input),
-		allocator:            allocator,
+		hashTableAllocator:   allocator,
 		distinctCols:         distinctCols,
 		typs:                 typs,
 		nullsAreDistinct:     nullsAreDistinct,
@@ -50,10 +50,10 @@ type UnorderedDistinct struct {
 	colexecop.OneInputNode
 	colexecbase.UpsertDistinctHelper
 
-	allocator        *colmem.Allocator
-	distinctCols     []uint32
-	typs             []*types.T
-	nullsAreDistinct bool
+	hashTableAllocator *colmem.Allocator
+	distinctCols       []uint32
+	typs               []*types.T
+	nullsAreDistinct   bool
 
 	Ht *colexechash.HashTable
 	// lastInputBatch tracks the last input batch read from the input and not
@@ -80,7 +80,7 @@ func (op *UnorderedDistinct) Init(ctx context.Context) {
 	const hashTableNumBuckets = 128
 	op.Ht = colexechash.NewHashTable(
 		op.Ctx,
-		op.allocator,
+		op.hashTableAllocator,
 		hashTableLoadFactor,
 		hashTableNumBuckets,
 		op.typs,

commit a4b1b47e810b37f62fc84f15d4da7524c8b4add3

colexec: fix recent accounting leak in the hash joiner when it spills
This commit fixes yet another bug introduced in
6535dc634606d4767b581f2b4cd74a0386b30d05 which would lead to a leak in
the memory accounting system when the hash joiner spills to disk. That
commit introduced accounting for a couple of internal slices, but we
forgot that the hash joiner can be reused multiple times (when it is
used by the external hash joiner), so we'd incorrectly grow the memory
usage for each partition of the external hash joiner. This is now fixed.

Release note: None

pkg/sql/colexec/colexecjoin/hashjoiner.go
@@ -229,6 +229,15 @@ type hashJoiner struct {
 		rowIdx int
 	}
 
+	// accountedFor tracks how much memory the hash joiner has accounted for so
+	// far related to some of the internal slices in the hash table.
+	accountedFor struct {
+		// hashtableSame tracks the current memory usage of hj.ht.Same.
+		hashtableSame int64
+		// hashtableVisited tracks the current memory usage of hj.ht.Visited.
+		hashtableVisited int64
+	}
+
 	exportBufferedState struct {
 		hashTableReleased  bool
 		rightExported      int
@@ -327,18 +336,24 @@ func (hj *hashJoiner) build() {
 		// they have separate collectLeftAnti method.
 		hj.ht.Same = colexecutils.MaybeAllocateUint64Array(hj.ht.Same, hj.ht.Vals.Length()+1)
 		// At this point, we have fully built the hash table on the right side
-		// (meaning we have fully consumed the righ input), so it'd be a shame
+		// (meaning we have fully consumed the right input), so it'd be a shame
 		// to fallback to disk, thus, we use the unlimited allocator.
-		hj.outputUnlimitedAllocator.AdjustMemoryUsage(memsize.Uint64 * int64(cap(hj.ht.Same)))
+		newAccountedFor := memsize.Uint64 * int64(cap(hj.ht.Same))
+		// hj.ht.Same will never shrink, so the delta is non-negative.
+		hj.outputUnlimitedAllocator.AdjustMemoryUsage(newAccountedFor - hj.accountedFor.hashtableSame)
+		hj.accountedFor.hashtableSame = newAccountedFor
 	}
 	if !hj.spec.rightDistinct || hj.spec.JoinType.IsSetOpJoin() {
 		// visited slice is also used for set-operation joins, regardless of
 		// the fact whether the right side is distinct.
 		hj.ht.Visited = colexecutils.MaybeAllocateBoolArray(hj.ht.Visited, hj.ht.Vals.Length()+1)
 		// At this point, we have fully built the hash table on the right side
-		// (meaning we have fully consumed the righ input), so it'd be a shame
+		// (meaning we have fully consumed the right input), so it'd be a shame
 		// to fallback to disk, thus, we use the unlimited allocator.
-		hj.outputUnlimitedAllocator.AdjustMemoryUsage(memsize.Bool * int64(cap(hj.ht.Visited)))
+		newAccountedFor := memsize.Bool * int64(cap(hj.ht.Visited))
+		// hj.ht.Visited will never shrink, so the delta is non-negative.
+		hj.outputUnlimitedAllocator.AdjustMemoryUsage(newAccountedFor - hj.accountedFor.hashtableVisited)
+		hj.accountedFor.hashtableVisited = newAccountedFor
 		// Since keyID = 0 is reserved for end of list, it can be marked as visited
 		// at the beginning.
 		hj.ht.Visited[0] = true
@@ -752,6 +767,10 @@ func (hj *hashJoiner) Reset(ctx context.Context) {
 		}
 	}
 	hj.state = hjBuilding
+	// Note that hj.ht.Reset() doesn't reset hj.ht.Same and hj.ht.Visited
+	// slices, but we'll reset them manually in hj.build(). We also keep
+	// references to those slices, so we don't release any of the memory we've
+	// accounted for.
 	hj.ht.Reset(ctx)
 	// Note that we don't zero out hj.probeState.buildIdx,
 	// hj.probeState.probeIdx, and hj.probeState.probeRowUnmatched because the