8abc47851882ecf681438ae12c48930ab63e358e
Yahor Yuzefovich <yahor@cockroachlabs.com>
2022-08-01 22:00:45 -0700

    colmem: improve memory-limiting behavior of the accounting helpers
    This commit fixes an oversight in how we are allocating batches of the
    "dynamic" capacity. We have two related ways for reallocating batches,
    and both of them work by growing the capacity of the batch until the
    memory limit is exceeded, and then the batch would be reused until the
    end of the query execution. This is a reasonable heuristic under the
    assumption that all tuples in the data stream are roughly equal in size,
    but this might not be the case.
    
    In particular, consider an example when 10k small rows of 1KiB are
    followed by 10k large rows of 1MiB. According to our heuristic, we
    happily grow the batch until 1024 in capacity, and then we do not shrink
    the capacity of that batch, so once the large rows start appearing, we
    put 1GiB worth of data into a single batch, significantly exceeding our
    memory limit (usually 64MiB with the default `workmem` setting).
    
    This commit introduces a new heuristic as follows:
    - the first time a batch exceeds the memory limit, its capacity is memorized,
      and from now on that capacity will determine the upper bound on the
      capacities of the batches allocated through the helper;
    - if at any point in time a batch exceeds the memory limit by at least a
      factor of two, then that batch is discarded, and the capacity will never
      exceed half of the capacity of the discarded batch;
    - if the memory limit is not reached, then the behavior of the dynamic growth
      of the capacity provided by `Allocator.ResetMaybeReallocate` is still
      applicable (i.e. the capacities will grow exponentially until
      coldata.BatchSize()).
    
    Note that this heuristic does not have an ability to grow the maximum
    capacity once it's been set although it might make sense to do so (say,
    if after shrinking the capacity, the next five times we see that the
    batch is using less than half of the memory limit). This is an conscious
    omission since I want this change to be backported, and never growing
    seems like a safer choice. Thus, this improvement is left as a TODO.
    
    Also, we still might create batches that are too large in memory
    footprint in those places that don't use the SetAccountingHelper (e.g.
    in the columnarizer) since we perform the memory limit check at the
    batch granularity. However, this commit improves things there so that we
    don't reuse that batch on the next iteration and will use half of the
    capacity on the next iteration.
    
    Release note (bug fix): CockroachDB now more precisely respects the
    `distsql_workmem` setting which improves the stability of each node and
    makes OOMs less likely.

--- pkg/sql/colexec/colexecjoin/mergejoiner_inner.eg.go
+++ pkg/sql/colexec/colexecjoin/mergejoiner_inner.eg.go
@@ -10898,10 +10898,7 @@ func (o *mergeJoinInnerOp) buildFromBufferedGroup() (bufferedGroupComplete bool)
 }
 
 func (o *mergeJoinInnerOp) Next() coldata.Batch {
-	o.output, _ = o.unlimitedAllocator.ResetMaybeReallocate(
-		o.outputTypes, o.output, 1 /* minDesiredCapacity */, o.memoryLimit,
-		false, /* desiredCapacitySufficient */
-	)
+	o.output, _ = o.helper.ResetMaybeReallocate(o.outputTypes, o.output, 0 /* tuplesToBeSet */)
 	o.outputCapacity = o.output.Capacity()
 	o.bufferedGroup.helper.output = o.output
 	o.builderState.outCount = 0
