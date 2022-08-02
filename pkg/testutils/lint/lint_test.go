435642a792f2ab561e2d524d0985cbac1cb50415
Ricky Stewart <ricky@cockroachlabs.com>
2022-08-02 11:44:00 -0500

    acceptance: fix, un-skip `TestComposeGSS`
    * Add `go.mod` file for `pkg/acceptance/compose/gss/psql` for pinning.
    * Remove dependency on `cockroach` via
      `pkg/acceptance/compose/gss/psql`.
    
    Fixes #84981.
    
    Release note: None

--- pkg/testutils/lint/lint_test.go
+++ pkg/testutils/lint/lint_test.go
@@ -722,6 +722,7 @@ func TestLint(t *testing.T) {
 			`\btime\.(Now|Since|Unix|LoadLocation)\(`,
 			"--",
 			"*.go",
+			":!acceptance/compose/gss/psql/gss_test.go",
 			":!**/embedded.go",
 			":!util/timeutil/time.go",
 			":!util/timeutil/zoneinfo.go",
