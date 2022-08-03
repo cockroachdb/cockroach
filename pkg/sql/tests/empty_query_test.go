e74638342b6468307b455b89001cbf01b6515bdb
Andrew Werner <awerner32@gmail.com>
2022-08-03 17:53:20 +0000

    sql: short-circuit declarative schema changer logic if there is nothing to do
    Not having this short-circuit logic can cause problems if the connExecutor is
    in some surprising state. It'll only be in that surprising state if no
    statements have been executed. Also, there's no reason to build all of these
    dependencies if we're going to just return anyway. I think this should be a
    bit of a performance improvement.
    
    Fixes #85523
    Fixes #85516
    Fixes #85518
    
    Release note: None

---
+++ pkg/sql/tests/empty_query_test.go
@@ -0,0 +1,48 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package tests
+
+import (
+	"context"
+	"net/url"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/security/username"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/jackc/pgx/v4"
+	"github.com/stretchr/testify/require"
+)
+
+// TestEmptyQuery is a regression test to ensure that sending an empty
+// query to the database as the first query is safe.
+func TestEmptyQuery(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	s, _, _ := serverutils.StartServer(t, base.TestServerArgs{Insecure: false, UseDatabase: "defaultdb"})
+	defer s.Stopper().Stop(context.Background())
+
+	pgURL, cleanupFunc := sqlutils.PGUrl(
+		t, s.ServingSQLAddr(), "testConnClose" /* prefix */, url.User(username.RootUser),
+	)
+	defer cleanupFunc()
+
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, pgURL.String())
+	require.NoError(t, err)
+
+	err = conn.QueryRow(ctx, "").Scan()
+	require.Error(t, err)
+	require.Regexp(t, "no rows in result set", err)
+}
