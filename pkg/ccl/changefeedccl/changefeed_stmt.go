381c24ac1c97bda3739ec93bbe73e7b06e55fd35
Jayant Shrivastava <jayants@cockroachlabs.com>
2022-08-03 14:03:18 -0400

    changefeedccl: enforce stop policy for changefeed expressions
    Changefeeds created with expressions do unexpected
    things when schema changes happen. This change enforces
    schema_change_policy='stop' when expressions are used,
    so changefeeds are forced to stop. In the future, this
    restriction will be removed.
    
    See https://github.com/cockroachdb/cockroach/issues/85143.
    
    Release note (enterprise change):
    When creating a changefeed with 'AS SELECT ...',
    the option schema_change_policy='stop' will be required. This
    means that the changefeed will stop if schema changes occur.
    This restriction will be reverted in future releases, allowing
    schema changes again.

--- pkg/ccl/changefeedccl/changefeed_stmt.go
+++ pkg/ccl/changefeedccl/changefeed_stmt.go
@@ -429,6 +429,15 @@ func createChangefeedJobRecord(
 		// TODO: Set the default envelope to row here when using a sink and format
 		// that support it.
 		details.Select = cdceval.AsStringUnredacted(normalized.Clause())
+
+		// TODO(#85143): do not enforce schema_change_policy='stop' for changefeed expressions.
+		schemachangeOptions, err := opts.GetSchemaChangeHandlingOptions()
+		if err != nil {
+			return nil, err
+		}
+		if schemachangeOptions.Policy != changefeedbase.OptSchemaChangePolicyStop {
+			return nil, errors.Errorf(`using "AS SELECT" requires option schema_change_policy='stop'`)
+		}
 	}
 
 	// TODO(dan): In an attempt to present the most helpful error message to the
