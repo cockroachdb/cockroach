847fddd51e3d638b80941499de78eeb6d03d212d
Andrew Werner <awerner32@gmail.com>
2022-08-04 16:29:59 -0400

    util/log: rework eventpb so that log does not depend on it
    This commit reworks some code generation and dependencies such that eventpb
    now depends on logpb and log no longer needs to depend on eventpb. This is
    important so that we can import protocol buffers into eventpb from other
    protobuf packages like descpb and hlc which themselves import util/log.
    
    Realistically it's bad that those packages import log, but this was easier
    to break in a sense than was that, and this is also a sane depedendency to
    not have.
    
    Release note: None

--- pkg/kv/kvserver/loqrecovery/loqrecoverypb/recovery.go
+++ pkg/kv/kvserver/loqrecovery/loqrecoverypb/recovery.go
@@ -14,6 +14,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/util/keysutil"
 	"github.com/cockroachdb/cockroach/pkg/util/log/eventpb"
+	"github.com/cockroachdb/cockroach/pkg/util/log/logpb"
 	"github.com/cockroachdb/errors"
 	"github.com/gogo/protobuf/proto"
 )
@@ -77,7 +78,7 @@ func (m *ReplicaInfo) Replica() (roachpb.ReplicaDescriptor, error) {
 // AsStructuredLog creates a structured log entry from the record.
 func (m *ReplicaRecoveryRecord) AsStructuredLog() eventpb.DebugRecoverReplica {
 	return eventpb.DebugRecoverReplica{
-		CommonEventDetails: eventpb.CommonEventDetails{
+		CommonEventDetails: logpb.CommonEventDetails{
 			Timestamp: m.Timestamp,
 		},
 		CommonDebugEventDetails: eventpb.CommonDebugEventDetails{
