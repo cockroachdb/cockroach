1267eaaf4930eeb968b561acb50f2d6b987a2c54
Faizan Qazi <faizan@cockroachlabs.com>
2022-08-08 20:19:01 -0400

    sql/logictest: create_as fails intermittently
    Fixes: #85357
    
    Previously, when we added the CTAS test for validating
    tables with statements that exceed the raft command size,
    we did not take into account that the number of bytes for
    flushing can be adjusted in a metamorphic way. This was
    inadequate because with metamorphic testing the value
    could potentially be larger than the raft command size,
    which would cause rows to never get flushed. To address this,
    this patch moves the logic test into a non-metamorphic test
    for sanity testing.
    
    Release note: None

--- pkg/sql/logictest/tests/local/generated_test.go
+++ pkg/sql/logictest/tests/local/generated_test.go
@@ -450,6 +450,13 @@ func TestLogic_create_as(
 	runLogicTest(t, "create_as")
 }
 
+func TestLogic_create_as_non_metamorphic(
+	t *testing.T,
+) {
+	defer leaktest.AfterTest(t)()
+	runLogicTest(t, "create_as_non_metamorphic")
+}
+
 func TestLogic_create_index(
 	t *testing.T,
 ) {
