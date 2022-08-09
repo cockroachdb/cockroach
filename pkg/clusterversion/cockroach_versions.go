2574cee371d48bfc6ef478a25ce894660a712f8c
Celia La <celia@cockroachlabs.com>
2022-08-08 18:44:23 -0700

    clusterversion,kvserver: remove DontProposeWriteTimestampForLeaseTransfers
    Release note (<category, see below>): <what> <show> <why>

--- pkg/clusterversion/cockroach_versions.go
+++ pkg/clusterversion/cockroach_versions.go
@@ -181,10 +181,6 @@ const (
 	// EnableSpanConfigStore enables the use of the span configs infrastructure
 	// in KV.
 	EnableSpanConfigStore
-	// DontProposeWriteTimestampForLeaseTransfers stops setting the WriteTimestamp
-	// on lease transfer Raft proposals. New leaseholders now forward their clock
-	// directly to the new lease start time.
-	DontProposeWriteTimestampForLeaseTransfers
 	// EnablePebbleFormatVersionBlockProperties enables a new Pebble SSTable
 	// format version for block property collectors.
 	// NB: this cluster version is paired with PebbleFormatBlockPropertyCollector
@@ -370,10 +366,6 @@ var versionsSingleton = keyedVersions{
 	{
 		Key:     EnableSpanConfigStore,
 		Version: roachpb.Version{Major: 21, Minor: 2, Internal: 40},
-	},
-	{
-		Key:     DontProposeWriteTimestampForLeaseTransfers,
-		Version: roachpb.Version{Major: 21, Minor: 2, Internal: 60},
 	},
 	{
 		Key:     EnablePebbleFormatVersionBlockProperties,
