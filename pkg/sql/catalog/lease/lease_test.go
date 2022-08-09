e5143c6223982b60ad5e04c987205e93219b4a0d
Andrew Werner <awerner32@gmail.com>
2022-08-09 10:24:00 -0400

    lease: fix test flake
    We were asserting a transient, intermediate state. There was no need to assert it.
    
    Fixes #85513
    
    Release note: None

--- pkg/sql/catalog/lease/lease_test.go
+++ pkg/sql/catalog/lease/lease_test.go
@@ -1140,7 +1140,6 @@ INSERT INTO t.kv VALUES ('a', 'b');
 	}
 
 	tableSpan := tableDesc.TableSpan(keys.SystemSQLCodec)
-	tests.CheckKeyCount(t, kvDB, tableSpan, 4)
 
 	// Allow async schema change waiting for GC to complete (when dropping an
 	// index) and clear the index keys.
