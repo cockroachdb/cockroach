c96ef78f9dc9d002d57534c55e41b6561f21dfef
Celia La <celia@cockroachlabs.com>
2022-08-09 03:25:38 -0700

    clusterversion,sql: remove RowLevelTTL
    Release note: none

--- pkg/sql/alter_table.go
+++ pkg/sql/alter_table.go
@@ -1827,13 +1827,6 @@ func handleTTLStorageParamChange(
 	tableDesc *tabledesc.Mutable,
 	before, after *catpb.RowLevelTTL,
 ) error {
-
-	if before == nil && after != nil {
-		if err := checkTTLEnabledForCluster(params.ctx, params.p.ExecCfg().Settings); err != nil {
-			return err
-		}
-	}
-
 	// update existing config
 	if before != nil && after != nil {
 
