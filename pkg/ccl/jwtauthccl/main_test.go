32bc074590b10fc40b9e278a6560bc81398be625
Kyle Patron <kyle@cockroachlabs.com>
2022-08-09 09:22:36 -0400

    ccl/jwtauthccl: Implement JWT based auth for CRDB SSO
    In order to enable future work to allow customers to log into
    CRDB instances using SSO, this change implements a new type of
    auth based on JWTs. Its enablement and configuration is controlled
    by cluster settings instead of being added as an hba_conf option.
    
    In particular, this change introduces three cluster settings:
    server.jwt_authentication.enable, server.jwt_authentication.jwks,
    and server.jwt_authentication.issuers. If during login, the custom
    option `--crdb:jwt.auth=true` is sent, the JWT code path is activated
    and the contents of the "password" field are analyzed as a token,
    if the enabled cluster setting is set to true.
    
    The token signature is validated with the jwks cluster setting.
    The token's expiration time is validated, the subject field is
    matched to the username requested, the audience is field is
    matched against the cluster's ID and the issuer is matched to
    the value of the issuers cluster setting.
    
    Finally, if the token passes all of those checks, CRDB ensures
    that an enterprise license is active.
    
    Release justification: This change is safe for release because
    in order for this code to be active both the client and the server
    must opt-in to using CRDB SSO auth for a given connection.
    
    Release note (enterprise change): A new JWT based auth method
    has been introduced as an option. This work lays the ground
    work for users to have SSO based login into their
    CockroachDB Dedicated clusters.

---
+++ pkg/ccl/jwtauthccl/main_test.go
@@ -0,0 +1,23 @@
+package jwtauthccl
+
+import (
+	"os"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/ccl/utilccl"
+	"github.com/cockroachdb/cockroach/pkg/security/securityassets"
+	"github.com/cockroachdb/cockroach/pkg/security/securitytest"
+	"github.com/cockroachdb/cockroach/pkg/server"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
+)
+
+func TestMain(m *testing.M) {
+	defer utilccl.TestingEnableEnterprise()()
+	securityassets.SetLoader(securitytest.EmbeddedAssets)
+	randutil.SeedForTests()
+	serverutils.InitTestServerFactory(server.TestServerFactory)
+	serverutils.InitTestClusterFactory(testcluster.TestClusterFactory)
+	os.Exit(m.Run())
+}
