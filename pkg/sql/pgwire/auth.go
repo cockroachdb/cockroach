a14dcafab747c3df7212e6c1f62c26a78f61fa9d
Kyle Patron <kyle@cockroachlabs.com>
2022-08-09 09:22:36 -0400

    ccl/jwtauthccl: Implement JWT based auth for CRDB SSO
    In order to enable future work to allow customers to log into
    CRDB instances using SSO, this change implements a new type of
    auth based on JWTs. Its enablement and configuration is controlled
    by cluster settings instead of being added as an hba_conf option.
    
    In particular, this change introduces three cluster settings:
    server.jwt_authentication.enable, server.jwt_authentication.jwks,
    and server.jwt_authentication.issuers. If during login, the custom
    option `--crdb:jwt.auth=true` is sent, the JWT code path is activated
    and the contents of the "password" field are analyzed as a token,
    if the enabled cluster setting is set to true.
    
    The token signature is validated with the jwks cluster setting.
    The token's expiration time is validated, the subject field is
    matched to the username requested, the audience is field is
    matched against the cluster's ID and the issuer is matched to
    the value of the issuers cluster setting.
    
    Finally, if the token passes all of those checks, CRDB ensures
    that an enterprise license is active.
    
    Release note (enterprise change): A new JWT based auth method
    has been introduced as an option. This work lays the ground
    work for users to have SSO based login into their
    CockroachDB Dedicated clusters.

--- pkg/sql/pgwire/auth.go
+++ pkg/sql/pgwire/auth.go
@@ -254,6 +254,12 @@ func (c *conn) findAuthenticationMethod(
 		return
 	}
 
+	if flag, ok := c.sessionArgs.CustomOptionSessionDefaults["crdb:jwt.auth"]; ok && strings.ToLower(flag) == "true" {
+		methodFn = authJwtToken
+		hbaEntry = &jwtAuthEntry
+		return
+	}
+
 	// Look up the method from the HBA configuration.
 	var mi methodInfo
 	mi, hbaEntry, err = c.lookupAuthenticationMethodUsingRules(authOpt.connType, authOpt.auth)
