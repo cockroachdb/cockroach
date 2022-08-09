6a0fa5129c6e7cf5b09968257d417e12525ded1e
Celia La <celia@cockroachlabs.com>
2022-08-08 18:03:02 -0700

    clusterversion,sql/catalog: remove RemoveIncompatibleDatabasePrivileges
    Release note: None

--- pkg/clusterversion/cockroach_versions.go
+++ pkg/clusterversion/cockroach_versions.go
@@ -181,9 +181,6 @@ const (
 	// EnableSpanConfigStore enables the use of the span configs infrastructure
 	// in KV.
 	EnableSpanConfigStore
-	// RemoveIncompatibleDatabasePrivileges adds the migration which guarantees that
-	// databases do not have incompatible privileges
-	RemoveIncompatibleDatabasePrivileges
 	// AddRaftAppliedIndexTermMigration is a migration that causes each range
 	// replica to start populating RangeAppliedState.RaftAppliedIndexTerm field.
 	AddRaftAppliedIndexTermMigration
@@ -385,10 +382,6 @@ var versionsSingleton = keyedVersions{
 	{
 		Key:     EnableSpanConfigStore,
 		Version: roachpb.Version{Major: 21, Minor: 2, Internal: 40},
-	},
-	{
-		Key:     RemoveIncompatibleDatabasePrivileges,
-		Version: roachpb.Version{Major: 21, Minor: 2, Internal: 54},
 	},
 	{
 		Key:     AddRaftAppliedIndexTermMigration,
