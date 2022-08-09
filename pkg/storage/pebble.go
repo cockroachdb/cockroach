68cb1eb855e354dbb755de57d69913090063ee24
Celia La <celia@cockroachlabs.com>
2022-08-09 15:00:07 -0700

    clusterversion,storage,sql: remove 22.1 PebbleFormat version gates
    Release note: none

--- pkg/storage/pebble.go
+++ pkg/storage/pebble.go
@@ -1792,13 +1792,9 @@ func (p *Pebble) SetMinVersion(version roachpb.Version) error {
 		if formatVers < pebble.FormatMarkedCompacted {
 			formatVers = pebble.FormatMarkedCompacted
 		}
-	case !version.Less(clusterversion.ByKey(clusterversion.PebbleFormatSplitUserKeysMarked)):
+	case !version.Less(clusterversion.ByKey(clusterversion.TODOPreV22_1)):
 		if formatVers < pebble.FormatSplitUserKeysMarked {
 			formatVers = pebble.FormatSplitUserKeysMarked
-		}
-	case !version.Less(clusterversion.ByKey(clusterversion.PebbleFormatBlockPropertyCollector)):
-		if formatVers < pebble.FormatBlockPropertyCollector {
-			formatVers = pebble.FormatBlockPropertyCollector
 		}
 	case !version.Less(clusterversion.ByKey(clusterversion.TODOPreV21_2)):
 		if formatVers < pebble.FormatSetWithDelete {
