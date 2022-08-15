c8a4e784e43363f93bc339f34a051d6c624a7957
Aditya Maru <adityamaru@gmail.com>
2022-08-15 14:06:23 -0400

    externalconn: support `SHOW CREATE EXTERNAL CONNECTION`
    This change adds support for `SHOW CREATE EXTERNAL CONNECTION`
    and `SHOW CREATE ALL EXTERNAL CONNECTIONS` that displays the
    connection name and the statement used to create the external
    connection. This displays unredacted information of the underlying
    resource and is therefore restricted to admin only or by the
    owner of the external connection object.
    
    Note, synthetic privileges do not have a concept of external
    connections at the moment. So, the operations are limited to
    admin only until that functionality is added.
    
    Informs: #85905
    
    Release note (sql change): Add support for `SHOW CREATE EXTERNAL CONNECTION`
    and `SHOW CREATE ALL EXTERNAL CONNECTIONS` that displays the
    connection name and the unredacted query used to create the external
    connection. This can only be run by users of the admin role today.

--- pkg/ccl/cloudccl/externalconn/datadriven_test.go
+++ pkg/ccl/cloudccl/externalconn/datadriven_test.go
@@ -104,6 +104,12 @@ func TestDataDriven(t *testing.T) {
 				}
 
 			case "query-sql":
+				if d.HasArg("user") {
+					var user string
+					d.ScanArgs(t, "user", &user)
+					resetToRootUser := externalConnTestCluster.SetSQLDBForUser(tenantID, user)
+					defer resetToRootUser()
+				}
 				var rows *gosql.Rows
 				var err error
 				if rows, err = tenant.QueryWithErr(d.Input); err != nil {
