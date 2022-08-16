797699947dddabf60f50f10ca068ff16be91dbaa
Xiang Gu <xiang@cockroachlabs.com>
2022-08-16 14:13:35 -0400

    upgrades: Added an cluster upgrade from v22.1 to v22.2
    This upgrade attempts to update invalid column IDs in sequence back
    references, if any, on a best-effort basis.
    
    The context of the need for such an upgrade can be found in #82576.
    The summary there is bugs in prior versions might cause sequence
    descriptor corruption where their back references might contain a column
    ID 0. We ought to figure out what the actual column ID is and update
    such invalid column IDs.

--- pkg/clusterversion/key_string.go
+++ pkg/clusterversion/key_string.go
@@ -48,11 +48,12 @@ func _() {
 	_ = x[WaitedForDelRangeInGCJob-37]
 	_ = x[RangefeedUseOneStreamPerNode-38]
 	_ = x[NoNonMVCCAddSSTable-39]
+	_ = x[UpdateInvalidColumnIDsInSequenceBackReferences-40]
 }
 
-const _Key_name = "V21_2Start22_1ProbeRequestPublicSchemasWithDescriptorsEnsureSpanConfigReconciliationEnsureSpanConfigSubscriptionEnableSpanConfigStoreEnableNewStoreRebalancerV22_1Start22_2LocalTimestampsPebbleFormatSplitUserKeysMarkedCompactedEnsurePebbleFormatVersionRangeKeysEnablePebbleFormatVersionRangeKeysTrigramInvertedIndexesRemoveGrantPrivilegeMVCCRangeTombstonesUpgradeSequenceToBeReferencedByIDSampledStmtDiagReqsAddSSTableTombstonesSystemPrivilegesTableEnablePredicateProjectionChangefeedAlterSystemSQLInstancesAddLocalitySystemExternalConnectionsTableAlterSystemStatementStatisticsAddIndexRecommendationsRoleIDSequenceAddSystemUserIDColumnSystemUsersIDColumnIsBackfilledSetSystemUsersUserIDColumnNotNullSQLSchemaTelemetryScheduledJobsSchemaChangeSupportsCreateFunctionDeleteRequestReturnKeyPebbleFormatPrePebblev1MarkedRoleOptionsTableHasIDColumnRoleOptionsIDColumnIsBackfilledSetRoleOptionsUserIDColumnNotNullUseDelRangeInGCJobWaitedForDelRangeInGCJobRangefeedUseOneStreamPerNodeNoNonMVCCAddSSTable"
+const _Key_name = "V21_2Start22_1ProbeRequestPublicSchemasWithDescriptorsEnsureSpanConfigReconciliationEnsureSpanConfigSubscriptionEnableSpanConfigStoreEnableNewStoreRebalancerV22_1Start22_2LocalTimestampsPebbleFormatSplitUserKeysMarkedCompactedEnsurePebbleFormatVersionRangeKeysEnablePebbleFormatVersionRangeKeysTrigramInvertedIndexesRemoveGrantPrivilegeMVCCRangeTombstonesUpgradeSequenceToBeReferencedByIDSampledStmtDiagReqsAddSSTableTombstonesSystemPrivilegesTableEnablePredicateProjectionChangefeedAlterSystemSQLInstancesAddLocalitySystemExternalConnectionsTableAlterSystemStatementStatisticsAddIndexRecommendationsRoleIDSequenceAddSystemUserIDColumnSystemUsersIDColumnIsBackfilledSetSystemUsersUserIDColumnNotNullSQLSchemaTelemetryScheduledJobsSchemaChangeSupportsCreateFunctionDeleteRequestReturnKeyPebbleFormatPrePebblev1MarkedRoleOptionsTableHasIDColumnRoleOptionsIDColumnIsBackfilledSetRoleOptionsUserIDColumnNotNullUseDelRangeInGCJobWaitedForDelRangeInGCJobRangefeedUseOneStreamPerNodeNoNonMVCCAddSSTableUpdateInvalidColumnIDsInSequenceBackReferences"
 
-var _Key_index = [...]uint16{0, 5, 14, 26, 54, 84, 112, 133, 157, 162, 171, 186, 226, 260, 294, 316, 336, 355, 388, 407, 427, 448, 483, 517, 547, 600, 614, 635, 666, 699, 730, 764, 786, 815, 842, 873, 906, 924, 948, 976, 995}
+var _Key_index = [...]uint16{0, 5, 14, 26, 54, 84, 112, 133, 157, 162, 171, 186, 226, 260, 294, 316, 336, 355, 388, 407, 427, 448, 483, 517, 547, 600, 614, 635, 666, 699, 730, 764, 786, 815, 842, 873, 906, 924, 948, 976, 995, 1041}
 
 func (i Key) String() string {
 	if i < 0 || i >= Key(len(_Key_index)-1) {
