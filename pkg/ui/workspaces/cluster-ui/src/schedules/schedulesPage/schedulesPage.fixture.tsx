401f76610adc16043e93e4b8d744b5b95fe1f6ee
Ben Bardin <bardin@cockroachlabs.com>
2022-08-16 13:38:50 -0400

    ui: add page to view schedules in db console.
    Adds /schedules page in DB console. Structure is very similar to /jobs pa
    ge. The new /schedules page provides a bit more observability into
    what schedules are running, and their various states.
    
    Release note (ui change): Add page to view schedules in DB console.
    
    Release justification: Low-risk - new, read-only page.

---
+++ pkg/ui/workspaces/cluster-ui/src/schedules/schedulesPage/schedulesPage.fixture.tsx
@@ -0,0 +1,107 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+import * as protos from "@cockroachlabs/crdb-protobuf-client";
+import { createMemoryHistory } from "history";
+import Long from "long";
+import moment from "moment";
+import { SchedulesPageProps } from "./schedulesPage";
+
+import { Schedule } from "src/api/schedulesApi";
+
+const schedulesTimeoutErrorMessage =
+  "Unable to retrieve the Schedules table. To reduce the amount of data, try filtering the table.";
+
+const defaultScheduleProperties = {
+  owner: "root",
+  created: moment("15 Aug 2022"),
+  nextRun: moment("15 Aug 2122"),
+  jobsRunning: 1,
+  state: "doing some stuff",
+  recurrence: "@weekly",
+  command: "{}",
+};
+
+export const activeScheduleFixture = {
+  ...defaultScheduleProperties,
+  id: new Long(8136728577, 70289336),
+  label: "automatic SQL Stats compaction",
+  owner: "node",
+  status: "ACTIVE",
+};
+
+const pausedScheduleFixture = {
+  ...defaultScheduleProperties,
+  id: new Long(7003330561, 70312826),
+  label:
+    "ALTER TABLE movr.public.user_promo_codes ADD FOREIGN KEY (city, user_id) REFERENCES movr.public.users (city, id)",
+  status: "PAUSED",
+  error: "mock failure message",
+};
+
+export const allSchedulesFixture = [
+  activeScheduleFixture,
+  pausedScheduleFixture,
+];
+
+const history = createMemoryHistory({ initialEntries: ["/statements"] });
+
+const staticScheduleProps: Omit<
+  SchedulesPageProps,
+  "schedules" | "schedulesError" | "schedulesLoading" | "onFilterChange"
+> = {
+  history,
+  location: {
+    pathname: "/schedules",
+    search: "",
+    hash: "",
+    state: null,
+  },
+  match: {
+    path: "/schedules",
+    url: "/schedules",
+    isExact: true,
+    params: "{}",
+  },
+  sort: {
+    columnTitle: "creationTime",
+    ascending: false,
+  },
+  status: "",
+  show: "50",
+  setSort: () => {},
+  setStatus: () => {},
+  setShow: () => {},
+  refreshSchedules: () => null,
+};
+
+const getSchedulesPageProps = (
+  schedules: Array<Schedule>,
+  error: Error | null = null,
+  loading = false,
+): SchedulesPageProps => ({
+  ...staticScheduleProps,
+  schedules,
+  schedulesError: error,
+  schedulesLoading: loading,
+});
+
+export const withData: SchedulesPageProps =
+  getSchedulesPageProps(allSchedulesFixture);
+export const empty: SchedulesPageProps = getSchedulesPageProps([]);
+export const loading: SchedulesPageProps = getSchedulesPageProps(
+  allSchedulesFixture,
+  null,
+  true,
+);
+export const error: SchedulesPageProps = getSchedulesPageProps(
+  allSchedulesFixture,
+  new Error(schedulesTimeoutErrorMessage),
+  false,
+);
