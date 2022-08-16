caa21ad7332c69b8a73fe7fb93ea17ba694feded
e-mbrown <ebsonari@gmail.com>
2022-08-16 15:21:32 -0400

    sql: Implement Discard Temp
    Release note (sql change): We now support DISCARD TEMP/TEMPORARY, which drops
    all temporary tables created in the current session. The command does not drop
    temporary schemas.
    
    Release justification: Bug Fix

--- pkg/sql/discard.go
+++ pkg/sql/discard.go
@@ -12,10 +12,20 @@ package sql
 
 import (
 	"context"
+	"strings"
 
+	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/security/username"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descs"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/resolver"
+	"github.com/cockroachdb/cockroach/pkg/sql/clusterunique"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgcode"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/errors"
 )
 
@@ -40,6 +50,19 @@ func (p *planner) Discard(ctx context.Context, s *tree.Discard) (planNode, error
 
 		// DEALLOCATE ALL
 		p.preparedStatements.DeleteAll(ctx)
+
+		//	DISCARD TEMP
+		err := prepareDeleteTempTables(ctx, p)
+		if err != nil {
+			return nil, err
+		}
+
+	case tree.DiscardModeTemp:
+		err := prepareDeleteTempTables(ctx, p)
+		if err != nil {
+			return nil, err
+		}
+
 	default:
 		return nil, errors.AssertionFailedf("unknown mode for DISCARD: %d", s.Mode)
 	}
@@ -62,6 +85,148 @@ func resetSessionVar(ctx context.Context, m sessionDataMutator, varName string) 
 		if hasDefault {
 			if err := v.Set(ctx, m, defVal); err != nil {
 				return err
+			}
+		}
+	}
+	return nil
+}
+
+func getTempSchema(
+	ctx context.Context, p *planner, codec keys.SQLCodec, allDbDescs []catalog.DatabaseDescriptor,
+) (map[clusterunique.ID]struct{}, error) {
+
+	sessionIDs := make(map[clusterunique.ID]struct{})
+	for _, dbDesc := range allDbDescs {
+		var schemaEntries map[descpb.ID]resolver.SchemaEntryForDB
+		schemaEntries, err := resolver.GetForDatabase(ctx, p.txn, codec, dbDesc)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, scEntry := range schemaEntries {
+			isTempSchema, sessionID, err := temporarySchemaSessionID(scEntry.Name)
+			if err != nil {
+				// This should not cause an error.
+				log.Warningf(ctx, "could not parse %q as temporary schema name", scEntry)
+				continue
+			}
+			if isTempSchema {
+				sessionIDs[sessionID] = struct{}{}
+			}
+
+		}
+	}
+	log.Infof(ctx, "found %d temporary schemas", len(sessionIDs))
+	return sessionIDs, nil
+}
+
+func prepareDeleteTempTables(ctx context.Context, p *planner) error {
+	codec := p.execCfg.Codec
+	descCol := p.Descriptors()
+	allDbDescs, err := descCol.GetAllDatabaseDescriptors(ctx, p.Txn())
+	if err != nil {
+		return err
+	}
+	ie := p.execCfg.InternalExecutor
+	sessionIDs, err := getTempSchema(ctx, p, codec, allDbDescs)
+	if err != nil {
+		return err
+	}
+
+	for _, dbDesc := range allDbDescs {
+		err = removeTempTable(ctx, p, ie, descCol, dbDesc, sessionIDs)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func removeTempTable(
+	ctx context.Context,
+	p *planner,
+	ie *InternalExecutor,
+	descCol *descs.Collection,
+	dbDesc catalog.DatabaseDescriptor,
+	sessionIDs map[clusterunique.ID]struct{},
+) error {
+	for sessionID := range sessionIDs {
+		schemaName := temporarySchemaName(sessionID)
+		tbNames, tbIDs, err := descCol.GetObjectNamesAndIDs(
+			ctx,
+			p.txn,
+			dbDesc,
+			schemaName,
+			tree.DatabaseListFlags{CommonLookupFlags: tree.CommonLookupFlags{Required: false}},
+		)
+		if err != nil {
+			return err
+		}
+
+		databaseIDToTempSchemaID := make(map[uint32]uint32)
+		var tables descpb.IDs
+		tblDescsByID := make(map[descpb.ID]catalog.TableDescriptor, len(tbNames))
+		tblNamesByID := make(map[descpb.ID]tree.TableName, len(tbNames))
+		for i, tbName := range tbNames {
+			desc, err := descCol.Direct().MustGetTableDescByID(ctx, p.txn, tbIDs[i])
+			if err != nil {
+				return err
+			}
+
+			tblDescsByID[desc.GetID()] = desc
+			tblNamesByID[desc.GetID()] = tbName
+
+			databaseIDToTempSchemaID[uint32(desc.GetParentID())] = uint32(desc.GetParentSchemaID())
+
+			if !desc.IsSequence() {
+				tables = append(tables, desc.GetID())
+			}
+		}
+
+		searchPath := sessiondata.DefaultSearchPathForUser(username.RootUserName()).WithTemporarySchemaName(schemaName)
+		override := sessiondata.InternalExecutorOverride{
+			SearchPath:               &searchPath,
+			User:                     username.RootUserName(),
+			DatabaseIDToTempSchemaID: databaseIDToTempSchemaID,
+		}
+
+		for _, toDelete := range []struct {
+			// typeName is the type of table being deleted, e.g. view, table, sequence
+			typeName string
+			// ids represents which ids we wish to remove.
+			ids descpb.IDs
+			// preHook is used to perform any operations needed before calling
+			// delete on all the given ids.
+			preHook func(descpb.ID) error
+		}{
+			{"TABLE", tables, nil},
+		} {
+			if len(toDelete.ids) > 0 {
+				if toDelete.preHook != nil {
+					for _, id := range toDelete.ids {
+						if err := toDelete.preHook(id); err != nil {
+							return err
+						}
+					}
+				}
+
+				var query strings.Builder
+				query.WriteString("DROP ")
+				query.WriteString(toDelete.typeName)
+
+				for i, id := range toDelete.ids {
+					tbName := tblNamesByID[id]
+					if i != 0 {
+						query.WriteString(",")
+					}
+					query.WriteString(" ")
+					query.WriteString(tbName.FQString())
+				}
+				query.WriteString(" CASCADE")
+				_, err = ie.ExecEx(ctx, "delete-temp-"+toDelete.typeName, p.txn, override, query.String())
+				if err != nil {
+					return err
+				}
 			}
 		}
 	}
