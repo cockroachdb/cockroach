7dadfa3a6e90ceb1e6ab482c9ae28e47b48441c9
Faizan Qazi <faizan@cockroachlabs.com>
2022-08-15 22:16:27 -0400

    sql/schemachanger: add public methods for detecting supports stmts
    Previously, we had no way of determining if a given
    SQL statement would use the declarative schema changer
    without executing this. This PR adds the ability to detect
    if a statement is fully supported via API to help aid in
    adding new automated infrastructure for backwards and
    mixed version compatibility. To address this, this patch,
    will expose a new method.
    
    Release note: None

--- pkg/sql/schemachanger/scbuild/internal/scbuildstmt/process.go
+++ pkg/sql/schemachanger/scbuild/internal/scbuildstmt/process.go
@@ -26,19 +26,35 @@ type supportedStatement struct {
 	fn interface{}
 	// on indicates that this statement is on by default.
 	on bool
+	// extraChecks contains a function to determine whether the command is
+	// supported. These extraChecks are important to be able to avoid any
+	// extra round-trips to resolve the descriptor and its elements if we know
+	// that we cannot process the command.
+	extraChecks interface{}
 }
 
 // isFullySupported returns if this statement type is supported, where the
 // mode of the new schema changer can force unsupported statements to be
 // supported.
-func isFullySupported(onByDefault bool, mode sessiondatapb.NewSchemaChangerMode) bool {
+func isFullySupported(
+	n tree.Statement, onByDefault bool, extraFn interface{}, mode sessiondatapb.NewSchemaChangerMode,
+) bool {
 	// If the unsafe modes of the new schema changer are used then any implemented
 	// operation will be exposed.
 	if mode == sessiondatapb.UseNewSchemaChangerUnsafeAlways ||
-		mode == sessiondatapb.UseNewSchemaChangerUnsafe {
+		mode == sessiondatapb.UseNewSchemaChangerUnsafe ||
+		onByDefault {
+		// If a command is labeled as fully supported, it can still have additional,
+		// checks via callback function.
+		if extraFn != nil {
+			fn := reflect.ValueOf(extraFn)
+			in := []reflect.Value{reflect.ValueOf(n), reflect.ValueOf(mode)}
+			values := fn.Call(in)
+			return values[0].Bool()
+		}
 		return true
 	}
-	return onByDefault
+	return false
 }
 
 // Tracks operations which are fully supported when the declarative schema
@@ -48,7 +64,7 @@ var supportedStatements = map[reflect.Type]supportedStatement{
 	// Alter table will have commands individually whitelisted via the
 	// supportedAlterTableStatements list, so wwe will consider it fully supported
 	// here.
-	reflect.TypeOf((*tree.AlterTable)(nil)):          {fn: AlterTable, on: true},
+	reflect.TypeOf((*tree.AlterTable)(nil)):          {fn: AlterTable, on: true, extraChecks: alterTableIsSupported},
 	reflect.TypeOf((*tree.CreateIndex)(nil)):         {fn: CreateIndex, on: false},
 	reflect.TypeOf((*tree.DropDatabase)(nil)):        {fn: DropDatabase, on: true},
 	reflect.TypeOf((*tree.DropOwnedBy)(nil)):         {fn: DropOwnedBy, on: true},
@@ -70,23 +86,58 @@ var supportedStatements = map[reflect.Type]supportedStatement{
 func init() {
 	// Check function signatures inside the supportedStatements map.
 	for statementType, statementEntry := range supportedStatements {
-		callBackType := reflect.TypeOf(statementEntry.fn)
-		if callBackType.Kind() != reflect.Func {
+		// Validate main callback functions.
+		callBackFnType := reflect.TypeOf(statementEntry.fn)
+		if callBackFnType.Kind() != reflect.Func {
 			panic(errors.AssertionFailedf("%v entry for statement is "+
 				"not a function", statementType))
 		}
-		if callBackType.NumIn() != 2 ||
-			!callBackType.In(0).Implements(reflect.TypeOf((*BuildCtx)(nil)).Elem()) ||
-			callBackType.In(1) != statementType {
+		if callBackFnType.NumIn() != 2 ||
+			!callBackFnType.In(0).Implements(reflect.TypeOf((*BuildCtx)(nil)).Elem()) ||
+			callBackFnType.In(1) != statementType {
 			panic(errors.AssertionFailedf("%v entry for statement is "+
-				"does not have a valid signature got %v", statementType, callBackType))
+				"does not have a valid signature got %v", statementType, callBackFnType))
+		}
+		// Validate fully supported function callbacks.
+		if statementEntry.extraChecks != nil {
+			fullySupportedCallbackFn := reflect.TypeOf(statementEntry.extraChecks)
+			if fullySupportedCallbackFn.Kind() != reflect.Func {
+				panic(errors.AssertionFailedf("%v entry for statement is "+
+					"not a function", statementType))
+			}
+			if fullySupportedCallbackFn.NumIn() != 2 ||
+				fullySupportedCallbackFn.In(0) != statementType ||
+				fullySupportedCallbackFn.In(1) != reflect.TypeOf(sessiondatapb.UseNewSchemaChangerOff) {
+				panic(errors.AssertionFailedf("%v entry for statement is "+
+					"does not have a valid signature got %v", statementType, callBackFnType))
+
+			}
 		}
 	}
+}
+
+// CheckIfStmtIsSupported determines if the statement is supported by the declarative
+// schema changer.
+func CheckIfStmtIsSupported(n tree.Statement, mode sessiondatapb.NewSchemaChangerMode) bool {
+	// Check if an entry exists for the statement type, in which
+	// case it is either fully or partially supported.
+	info, ok := supportedStatements[reflect.TypeOf(n)]
+	if !ok {
+		return false
+	}
+	// Check if partially supported operations are allowed next. If an
+	// operation is not fully supported will not allow it to be run in
+	// the declarative schema changer until its fully supported.
+	return isFullySupported(n, info.on, info.extraChecks, mode)
 }
 
 // Process dispatches on the statement type to populate the BuilderState
 // embedded in the BuildCtx. Any error will be panicked.
 func Process(b BuildCtx, n tree.Statement) {
+	// If a statement is not supported throw a not implemented error.
+	if !CheckIfStmtIsSupported(n, b.EvalCtx().SessionData().NewSchemaChangerMode) {
+		panic(scerrors.NotImplementedError(n))
+	}
 	// Check if an entry exists for the statement type, in which
 	// case it is either fully or partially supported.
 	info, ok := supportedStatements[reflect.TypeOf(n)]
@@ -97,7 +148,7 @@ func Process(b BuildCtx, n tree.Statement) {
 	// operation is not fully supported will not allow it to be run in
 	// the declarative schema changer until its fully supported.
 	if !isFullySupported(
-		info.on, b.EvalCtx().SessionData().NewSchemaChangerMode,
+		n, info.on, info.extraChecks, b.EvalCtx().SessionData().NewSchemaChangerMode,
 	) {
 		panic(scerrors.NotImplementedError(n))
 	}
