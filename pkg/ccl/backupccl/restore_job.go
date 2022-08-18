329783de91c2d66f0eb0c5e01eee7dd96c31b46f
Michael Butler <butler@cockroachlabs.com>
2022-08-18 17:48:25 -0400

    backupccl: elide data from offline tables in RESTORE
    A previous PR (#83915) enabled BACKUP to target offline descriptors
    (including IMPORTing and RESTOREing tables), but didn't fully handle their
    rollback on RESTORE. This PR ensures RESTORE properly handles these offline
    tables:
    - An offline table created by RESTORE or IMPORT PGDUMP is fully discarded. The
      table does not exist in the restoring cluster.
    - An offline table created by an IMPORT INTO has all importing data elided in
      the restore processor and is restored online to its pre import state.
    
    If the RESTORE occurs AOST after the table returns online, the table is
    RESTOREd to it's post offline state.
    
    Currently, all tables that return online are fully re-backed up. For many
    situations above, this is unecessary. Future work should ensure that these
    tables are not re-backed up when we know all operations within the
    incremental backup interval on the table were MVCC.
    
    Release note (sql change): offline tables with in-progress IMPORT INTO imports
    (i.e. tables that existed before the import job began) now get
    backed up once the cluster has upgraded to v22.2. During a cluster/database
    Restore that executes AOST during the in-progress import, all data from the
    import is elided and leaves the table in its pre-import state (online, with
    pre-import data, if any). If the RESTORE executes AOST after the import
    completes, all the imported data ingests into the restored data.
    
    This patch only applies to database and cluster BACKUP and RESTORE, and a
    future PR should allow a user to BACKUP/RESTORE an existing offline table
    undergoing an IMPORT.
    
    Release justification: fixes bug where offline tables were not properly handled
    in restore.

--- pkg/ccl/backupccl/restore_job.go
+++ pkg/ccl/backupccl/restore_job.go
@@ -95,7 +95,7 @@ var restoreStatsInsertBatchSize = 10
 func rewriteBackupSpanKey(
 	codec keys.SQLCodec, kr *KeyRewriter, key roachpb.Key,
 ) (roachpb.Key, error) {
-	newKey, rewritten, err := kr.RewriteKey(append([]byte(nil), key...))
+	newKey, rewritten, err := kr.RewriteKey(append([]byte(nil), key...), 0)
 	if err != nil {
 		return nil, errors.NewAssertionErrorWithWrappedErrf(err,
 			"could not rewrite span start key: %s", key)
@@ -660,6 +660,27 @@ func shouldPreRestore(table *tabledesc.Mutable) bool {
 	return ok
 }
 
+// eligibleInProgressImport returns true if the descriptor represents a table with an in progress
+// import that started in a cluster finalized to version 22.2.
+func eligibleInProgressImport(
+	ctx context.Context, p sql.JobExecContext, desc catalog.Descriptor,
+) (bool, error) {
+	table, ok := desc.(catalog.TableDescriptor)
+	if !ok {
+		return false, nil
+	}
+
+	if table.GetInProgressImportStartTime() == 0 {
+		return false, nil
+	}
+	if !p.ExecCfg().Settings.Version.IsActive(ctx, clusterversion.Start22_2) {
+		return false, errors.AssertionFailedf(
+			`Only a finalized 22.2 cluster can set a descriptor's ImportStartTime; therefore, descriptor
+							%v should never get restored on a cluster that has not finalized to 22.2`, table.GetID())
+	}
+	return true, nil
+}
+
 // createImportingDescriptors creates the tables that we will restore into and returns up to three
 // configurations for separate restoration flows. The three restoration flows are
 //
@@ -708,6 +729,17 @@ func createImportingDescriptors(
 	preRestoreTables := make([]catalog.TableDescriptor, 0)
 
 	for _, desc := range sqlDescs {
+
+		// TODO (msbutler) remove the schema_change condition once the online schema changer
+		// doesn't rely on OFFLINE state
+		if desc.Offline() && desc.GetDeclarativeSchemaChangerState() == nil {
+			if eligible, err := eligibleInProgressImport(ctx, p, desc); err != nil {
+				return nil, nil, nil, err
+			} else if !eligible {
+				continue
+			}
+		}
+
 		switch desc := desc.(type) {
 		case catalog.TableDescriptor:
 			mut := tabledesc.NewBuilder(desc.TableDesc()).BuildCreatedMutableTable()
