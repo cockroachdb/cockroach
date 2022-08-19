a37581e99942dec7f8f26784194b4d216899b554
Rebecca Taft <becca@cockroachlabs.com>
2022-08-19 17:44:55 -0500

    opt: cache not-null columns in the metadata
    This commit adds a new table annotation for not null columns to avoid
    rediscovering them multiple times per table. This improves performance
    for queries over tables with a large number of columns and indexes.
    
    Benchmark results:
    
    name                         old time/op    new time/op    delta
    SlowQueries/slow-query-3-10     123ms ± 1%      63ms ± 2%  -48.62%  (p=0.000 n=10+9)
    SlowQueries/slow-query-1-10    20.3ms ± 1%    20.2ms ± 2%     ~     (p=0.353 n=10+10)
    SlowQueries/slow-query-2-10     312ms ± 2%     313ms ± 4%     ~     (p=0.842 n=10+9)
    
    name                         old alloc/op   new alloc/op   delta
    SlowQueries/slow-query-3-10    59.3MB ± 0%    49.0MB ± 0%  -17.38%  (p=0.000 n=10+10)
    SlowQueries/slow-query-1-10    12.3MB ± 0%    12.3MB ± 0%   -0.27%  (p=0.000 n=10+10)
    SlowQueries/slow-query-2-10     181MB ± 0%     181MB ± 0%     ~     (p=0.739 n=10+10)
    
    name                         old allocs/op  new allocs/op  delta
    SlowQueries/slow-query-3-10      542k ± 0%      381k ± 0%  -29.72%  (p=0.000 n=9+10)
    SlowQueries/slow-query-1-10      131k ± 0%      131k ± 0%   -0.39%  (p=0.000 n=10+10)
    SlowQueries/slow-query-2-10     1.65M ± 0%     1.65M ± 0%     ~     (p=0.345 n=9+10)
    
    Release justification: low risk, high benefit changes to existing
    functionality
    
    Release note (performance improvement): Planning time has been reduced
    for queries over tables with a large number of columns and/or indexes.

--- pkg/sql/opt/memo/logical_props_builder.go
+++ pkg/sql/opt/memo/logical_props_builder.go
@@ -86,7 +86,7 @@ func (b *logicalPropsBuilder) buildScanProps(scan *ScanExpr, rel *props.Relation
 	// Not Null Columns
 	// ----------------
 	// Initialize not-NULL columns from the table schema.
-	rel.NotNullCols = tableNotNullCols(md, scan.Table)
+	rel.NotNullCols = makeTableNotNullCols(md, scan.Table).Copy()
 	// Union not-NULL columns with not-NULL columns in the constraint.
 	if scan.Constraint != nil {
 		rel.NotNullCols.UnionWith(scan.Constraint.ExtractNotNullCols(b.evalCtx))
@@ -474,7 +474,7 @@ func (b *logicalPropsBuilder) buildIndexJoinProps(indexJoin *IndexJoinExpr, rel 
 	// ----------------
 	// Add not-NULL columns from the table schema, and filter out any not-NULL
 	// columns from the input that are not projected by the index join.
-	rel.NotNullCols = tableNotNullCols(md, indexJoin.Table)
+	rel.NotNullCols = makeTableNotNullCols(md, indexJoin.Table).Copy()
 	rel.NotNullCols.IntersectionWith(rel.OutputCols)
 
 	// Outer Columns
@@ -2095,7 +2095,7 @@ func ensureLookupJoinInputProps(join *LookupJoinExpr, sb *statisticsBuilder) *pr
 			}
 		}
 
-		relational.NotNullCols = tableNotNullCols(md, join.Table)
+		relational.NotNullCols = makeTableNotNullCols(md, join.Table).Copy()
 		relational.NotNullCols.IntersectionWith(relational.OutputCols)
 		relational.Cardinality = props.AnyCardinality
 		relational.FuncDeps.CopyFrom(MakeTableFuncDep(md, join.Table))
@@ -2112,7 +2112,7 @@ func ensureInvertedJoinInputProps(join *InvertedJoinExpr, sb *statisticsBuilder)
 	if relational.OutputCols.Empty() {
 		md := join.Memo().Metadata()
 		relational.OutputCols = join.Cols.Difference(join.Input.Relational().OutputCols)
-		relational.NotNullCols = tableNotNullCols(md, join.Table)
+		relational.NotNullCols = makeTableNotNullCols(md, join.Table).Copy()
 		relational.NotNullCols.IntersectionWith(relational.OutputCols)
 		relational.Cardinality = props.AnyCardinality
 
@@ -2162,7 +2162,7 @@ func ensureInputPropsForIndex(
 	if relProps.OutputCols.Empty() {
 		relProps.OutputCols = md.TableMeta(tabID).IndexColumns(indexOrd)
 		relProps.OutputCols.IntersectionWith(outputCols)
-		relProps.NotNullCols = tableNotNullCols(md, tabID)
+		relProps.NotNullCols = makeTableNotNullCols(md, tabID).Copy()
 		relProps.NotNullCols.IntersectionWith(relProps.OutputCols)
 		relProps.Cardinality = props.AnyCardinality
 		relProps.FuncDeps.CopyFrom(MakeTableFuncDep(md, tabID))
@@ -2171,9 +2171,17 @@ func ensureInputPropsForIndex(
 	}
 }
 
-// tableNotNullCols returns the set of not-NULL non-mutation columns from the given table.
-func tableNotNullCols(md *opt.Metadata, tabID opt.TableID) opt.ColSet {
-	cs := opt.ColSet{}
+// makeTableNotNullCols returns the set of not-NULL non-mutation columns from
+// the given table. The set is derived lazily and is cached in the metadata,
+// since it may be accessed multiple times during query optimization.
+func makeTableNotNullCols(md *opt.Metadata, tabID opt.TableID) opt.ColSet {
+	cs, ok := md.TableAnnotation(tabID, opt.NotNullAnnID).(opt.ColSet)
+	if ok {
+		// Already made.
+		return cs
+	}
+
+	cs = opt.ColSet{}
 	tab := md.Table(tabID)
 
 	// Only iterate over non-mutation columns, since even non-null mutation
@@ -2185,6 +2193,8 @@ func tableNotNullCols(md *opt.Metadata, tabID opt.TableID) opt.ColSet {
 			cs.Add(tabID.ColumnID(i))
 		}
 	}
+
+	md.SetTableAnnotation(tabID, opt.NotNullAnnID, cs)
 	return cs
 }
 
