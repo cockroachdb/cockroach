c4eb79e726b35cc9a3e6d20de0e342bdf76e3cc5
e-mbrown <ebsonari@gmail.com>
2022-08-22 11:39:05 -0400

    sql: Add no-op for UNLISTEN
    This commit makes UNLISTEN a no-op to fix issue with npgsql.
    
    Release justification: Low risk, high benefit change to existing functionality
    Release note: None

---
+++ pkg/sql/sem/tree/unlisten.go
@@ -0,0 +1,35 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package tree
+
+// Unlisten represents a UNLISTEN statement.
+type Unlisten struct {
+	TypeName *UnresolvedObjectName
+	Star     bool
+}
+
+var _ Statement = &Unlisten{}
+
+// Format implements the NodeFormatter interface.
+func (node *Unlisten) Format(ctx *FmtCtx) {
+	ctx.WriteString("UNLISTEN ")
+	if node.Star {
+		ctx.WriteString("* ")
+	}
+	if node.TypeName != nil {
+		ctx.FormatNode(node.TypeName)
+	}
+}
+
+// String implements the Statement interface.
+func (node *Unlisten) String() string {
+	return AsString(node)
+}
