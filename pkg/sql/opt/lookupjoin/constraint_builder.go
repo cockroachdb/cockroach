4807844e08adeca218960359f4fb5fe2ab576616
Mark Sirek <sirek@cockroachlabs.com>
2022-08-22 14:00:27 -0700

    xform: ignore derived hash bucket lookup join cols for selectivity estimate
    Fixes #85353
    
    Previously, a lookup join involving the first column of a hash-sharded
    index and a column from another table would use a derived equality
    condition between the invisible hash bucket column and an expression on
    the other table's column for selectivity estimation purposes. Since the
    derived join condition does not actually reduce the number of qualified
    rows, the optimizer would end up with an underestimated row count
    estimate for the join, and end up selecting it, when lower cost join
    methods existed.
    
    To address this, this patch remembers which left table key columns in
    the lookup join are synthesized for derived equijoin conditions on the
    lookup table hash bucket column, and ignores them when building the
    filter functional dependencies which are later used in
    `selectivityFromEquivalencies()` for calculating the join selectivity.
    
    Release justification: Low risk fix for costly lookup joins on
    hash-sharded indexes.
    
    Release note (bug fix): This patch fixes a bug in lookup join
    selectivity estimation involving hash-sharded indexes which may cause
    lookup joins to be selected by the optimizer in cases where other join
    methods are less expensive.

--- pkg/sql/opt/lookupjoin/constraint_builder.go
+++ pkg/sql/opt/lookupjoin/constraint_builder.go
@@ -42,6 +42,14 @@ type Constraint struct {
 	// in RightSideCols. It will be nil if LookupExpr is non-nil.
 	KeyCols opt.ColList
 
+	// DerivedEquivCols is the set of lookup join equijoin columns which are part
+	// of synthesized equality constraints based on another equality join
+	// condition and a computed index key column in the lookup table. Since these
+	// columns are not reducing the selectivity of the join, but are just added to
+	// facilitate index lookups, they should not be used in determining join
+	// selectivity.
+	DerivedEquivCols opt.ColSet
+
 	// RightSideCols is an ordered list of prefix index columns that are
 	// constrained by this constraint. It corresponds 1:1 with the columns in
 	// KeyCols if KeyCols is non-nil. Otherwise, it includes the prefix of index
@@ -177,6 +185,7 @@ func (b *ConstraintBuilder) Build(
 	numIndexKeyCols := index.LaxKeyColumnCount()
 
 	keyCols := make(opt.ColList, 0, numIndexKeyCols)
+	var derivedEquivCols opt.ColSet
 	rightSideCols := make(opt.ColList, 0, numIndexKeyCols)
 	var inputProjections memo.ProjectionsExpr
 	var lookupExpr memo.FiltersExpr
@@ -253,6 +262,8 @@ func (b *ConstraintBuilder) Build(
 			projection := b.f.ConstructProjectionsItem(b.f.RemapCols(expr, b.eqColMap), compEqCol)
 			inputProjections = append(inputProjections, projection)
 			addEqualityColumns(compEqCol, idxCol)
+			derivedEquivCols.Add(compEqCol)
+			derivedEquivCols.Add(idxCol)
 			foundEqualityCols = true
 			foundLookupCols = true
 			continue
@@ -364,6 +375,7 @@ func (b *ConstraintBuilder) Build(
 
 	c := Constraint{
 		KeyCols:          keyCols,
+		DerivedEquivCols: derivedEquivCols,
 		RightSideCols:    rightSideCols,
 		LookupExpr:       lookupExpr,
 		InputProjections: inputProjections,
