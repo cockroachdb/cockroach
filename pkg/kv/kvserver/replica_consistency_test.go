94e5107fc186b18b707dc36b0f1c856050cb487f
Pavel Kalinnikov <pavel@cockroachlabs.com>
2022-08-22 13:32:29 +0100

    kvserver: sync checksum computation with long poll
    Previously, the checsum computation would run until completion unconditionally.
    If the requestor cancels the request or dies, the computation continues. This
    may cause a build-up of running and pending checksum tasks.
    
    After this commit, the checksum computation task is synchronized with the
    checksum collection request. Both wait at most 5 seconds until the other party
    has joined. Once joined, the collection request handler gets a cancel function
    that it can use to stop the running computation task (for example, it took too
    long, and the request abandons the computation).
    
    This ensures that the checksum computation is:
    - never started if its result is not requested,
    - terminated promptly if the computation result is no longer needed.
    
    As a side effect, this commit simplifies the code by removing the unnecessary
    GC of the pending and completed computations.
    
    Release justification: performance and stability improvement
    
    Release note (bug fix): Consistency check computation is now synchronized with
    the computation result request. This ensures that the computation is started
    only if its result is requested, and promptly finished if its result is no
    longer needed.

--- pkg/kv/kvserver/replica_consistency_test.go
+++ pkg/kv/kvserver/replica_consistency_test.go
@@ -14,6 +14,7 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -23,6 +24,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/storage"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/echotest"
+	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
@@ -53,10 +55,10 @@ func TestReplicaChecksumVersion(t *testing.T) {
 		} else {
 			cc.Version = 1
 		}
-		tc.repl.computeChecksumPostApply(ctx, cc)
+		go tc.repl.computeChecksumPostApply(ctx, cc)
 		rc, err := tc.repl.getChecksum(ctx, cc.ChecksumID)
 		if !matchingVersion {
-			if !testutils.IsError(err, "no checksum found") {
+			if !testutils.IsError(err, "context deadline exceeded") {
 				t.Fatal(err)
 			}
 			require.Nil(t, rc.Checksum)
@@ -78,52 +80,42 @@ func TestGetChecksumNotSuccessfulExitConditions(t *testing.T) {
 	defer stopper.Stop(ctx)
 	tc.Start(ctx, t, stopper)
 
-	id := uuid.FastMakeV4()
-	notify := make(chan struct{})
-	close(notify)
-
 	// Simple condition, the checksum is notified, but not computed.
-	tc.repl.mu.Lock()
-	tc.repl.mu.checksums[id] = replicaChecksum{notify: notify}
-	tc.repl.mu.Unlock()
-	rc, err := tc.repl.getChecksum(ctx, id)
-	if !testutils.IsError(err, "no checksum found") {
-		t.Fatal(err)
-	}
-	require.Nil(t, rc.Checksum)
+	id := uuid.FastMakeV4()
+	rc := tc.repl.getReplicaChecksum(id)
+	close(rc.ready)
+	rc.complete(CollectChecksumResponse{})
+	resp, err := tc.repl.getChecksum(ctx, id)
+	require.True(t, testutils.IsError(err, "checksum computation failed to start"))
+	require.Nil(t, resp.Checksum)
+
 	// Next condition, the initial wait expires and checksum is not started,
 	// this will take 10ms.
 	id = uuid.FastMakeV4()
-	tc.repl.mu.Lock()
-	tc.repl.mu.checksums[id] = replicaChecksum{notify: make(chan struct{})}
-	tc.repl.mu.Unlock()
-	rc, err = tc.repl.getChecksum(ctx, id)
-	if !testutils.IsError(err, "checksum computation did not start") {
-		t.Fatal(err)
-	}
-	require.Nil(t, rc.Checksum)
-	// Next condition, initial wait expired and we found the started flag,
-	// so next step is for context deadline.
-	id = uuid.FastMakeV4()
-	tc.repl.mu.Lock()
-	tc.repl.mu.checksums[id] = replicaChecksum{stop: func() {}, notify: make(chan struct{})}
-	tc.repl.mu.Unlock()
-	rc, err = tc.repl.getChecksum(ctx, id)
-	if !testutils.IsError(err, "context deadline exceeded") {
-		t.Fatal(err)
-	}
-	require.Nil(t, rc.Checksum)
+	rc = tc.repl.getReplicaChecksum(id)
+	resp, err = tc.repl.getChecksum(ctx, id)
+	require.True(t, testutils.IsError(err, "joining the computation.* timed out"))
+	require.Nil(t, resp.Checksum)
 
-	// Need to reset the context, since we deadlined it above.
-	ctx, cancel = context.WithTimeout(context.Background(), 100*time.Millisecond)
-	defer cancel()
-	// Next condition, node should quiesce.
-	tc.repl.store.Stopper().Quiesce(ctx)
-	rc, err = tc.repl.getChecksum(ctx, uuid.FastMakeV4())
-	if !testutils.IsError(err, "store quiescing") {
-		t.Fatal(err)
-	}
-	require.Nil(t, rc.Checksum)
+	// Next condition, the computation has joined the request, but the request was
+	// canceled after it.
+	ctx, cancel = context.WithCancel(context.Background())
+	id = uuid.FastMakeV4()
+	rc = tc.repl.getReplicaChecksum(id)
+	// Signal
+	g := ctxgroup.WithContext(context.Background())
+	var taskStopped int32
+	g.GoCtx(func(ctx context.Context) error {
+		require.NoError(t, rc.start(ctx, func() {
+			atomic.AddInt32(&taskStopped, 1)
+		}))
+		cancel()
+		return nil
+	})
+	resp, err = tc.repl.getChecksum(ctx, id)
+	require.True(t, testutils.IsError(err, "context canceled"))
+	require.Nil(t, resp.Checksum)
+	require.Equal(t, int32(1), atomic.LoadInt32(&taskStopped))
 }
 
 // TestReplicaChecksumSHA512 checks that a given dataset produces the expected
