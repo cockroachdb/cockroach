833c95fad7156a58d71fab2b8be0ad3739bfe6eb
Marcus Gartner <marcus@cockroachlabs.com>
2022-08-24 09:31:30 -0400

    opt: return struct from index rec library instead of formatted string
    Previously, the `indexrec` package returned a formatted string as the
    final recommendation result. The string was formatted specificaly for
    the output of `EXPLAIN`. This had limited utility. To create index
    recommendations in the `statement_statistics` table, we were parsing the
    string output.
    
    This commit updates the `indexrec` package to return a slice of structs
    rather than a slice of strings that are formatted for `EXPLAIN` output.
    The recommendations can now be consumed in different contexts without
    having to parse a string.
    
    Fixes #85955
    
    Release justification: This is a minor change to new functionality.
    
    Release note: None

--- pkg/sql/explain_plan.go
+++ pkg/sql/explain_plan.go
@@ -137,10 +137,20 @@ func (e *explainPlanNode) startExec(params runParams) error {
 		}
 	}
 	// Add index recommendations to output, if they exist.
-	if params.p.instrumentation.indexRecommendations != nil {
+	if recs := params.p.instrumentation.indexRecs; recs != nil {
 		// First add empty row.
 		rows = append(rows, "")
-		rows = append(rows, params.p.instrumentation.indexRecommendations...)
+		rows = append(rows, fmt.Sprintf("index recommendations: %d", len(recs)))
+		for i := range recs {
+			plural := ""
+			recType := "index creation"
+			if recs[i].Replacement {
+				recType = "index replacement"
+				plural = "s"
+			}
+			rows = append(rows, fmt.Sprintf("%d. type: %s", i+1, recType))
+			rows = append(rows, fmt.Sprintf("   SQL command%s: %s", plural, recs[i].SQL))
+		}
 	}
 	v := params.p.newContainerValuesNode(colinfo.ExplainPlanColumns, 0)
 	datums := make([]tree.DString, len(rows))
--- pkg/sql/idxrecommendations/idx_recommendations.go
+++ pkg/sql/idxrecommendations/idx_recommendations.go
@@ -12,8 +12,8 @@ package idxrecommendations
 
 import (
 	"fmt"
-	"strings"
 
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/indexrec"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 )
 
@@ -34,36 +34,25 @@ type IdxRecommendations interface {
 	) []string
 }
 
-// FormatIdxRecommendations received a list with recommendations info, e.g.:
-//{
-//	"index recommendations: 2",
-//	"1. type: index replacement",
-//	"SQL commands: CREATE UNIQUE INDEX ON t1 (i) STORING (k); DROP INDEX t1@existing_t1_i;",
-//  "2. type: index creation",
-//  "SQL command: CREATE INDEX ON t2 (i) STORING (k);",
-//}
-// and returns a list of type and recommendations, e.g.:
-//{
-//		"replacement : CREATE UNIQUE INDEX ON t1 (i) STORING (k); DROP INDEX t1@existing_t1_i;",
-//    "creation : CREATE INDEX ON t2 (i) STORING (k);",
-//}
-func FormatIdxRecommendations(idxRec []string) []string {
-	recommendations := []string{}
-	if len(idxRec) == 0 {
-		return recommendations
+// FormatIdxRecommendations formats a list of index recommendations. The output
+// is in the format:
+//
+//   {
+//     "replacement : CREATE UNIQUE INDEX ON t1 (i) STORING (k); DROP INDEX t1@existing_t1_i;",
+//     "creation : CREATE INDEX ON t2 (i) STORING (k);",
+//   }
+//
+func FormatIdxRecommendations(recs []indexrec.Rec) []string {
+	if len(recs) == 0 {
+		return nil
 	}
-
-	var recType string
-	var recCommand string
-	var rec string
-
-	for i := 1; i < len(idxRec); i++ {
-		recType = strings.Split(idxRec[i], "type: index ")[1]
-		recCommand = strings.Replace(idxRec[i+1], "   SQL command: ", "", 1)
-		recCommand = strings.Replace(recCommand, "   SQL commands: ", "", 1)
-		rec = fmt.Sprintf("%s : %s", recType, recCommand)
-		recommendations = append(recommendations, rec)
-		i++
+	recommendations := make([]string, len(recs))
+	for i := range recs {
+		recType := "creation"
+		if recs[i].Replacement {
+			recType = "replacement"
+		}
+		recommendations[i] = fmt.Sprintf("%s : %s", recType, recs[i].SQL)
 	}
 
 	return recommendations
