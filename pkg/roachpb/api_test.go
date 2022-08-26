cockroachdb/cockroach

commit cf14049344e2c6878cba569b72cf708f16555da0

kv: pass explicit Now timestamp on BatchRequest, remove timestamp downcasting
This commit adds an explicit `ClockTimestamp` field called `Now` to the
`BatchRequest` header, which mirrors the `Now` field on the `BatchResponse`
header.

In doing so, it removes the last instance where we downcasted a `Timestamp` to a
`ClockTimestamp` using the `TryToClockTimestamp` method. With this change, MVCC
("operation") timestamps never flow back into HLC clocks as clock signals. This
was enabled by #80706 and sets the groundwork to remove synthetic timestamps in
v23.1 — the role they played in dynamic typing of clock timestamps is now
entirely fulfilled by statically typed `ClockTimestamp` channels.

This is an important step in separating out the MVCC timestamp domain from the
clock timestamp domain and clarifying the roles of the two layers. In turn, this
layering opens the door for CockroachDB to start thinking about dynamic clock
synchronization error bounds.

pkg/roachpb/api.proto
@@ -2258,6 +2258,12 @@ message Header {
   // request's uncertainty interval remains fixed across retries.
   util.hlc.Timestamp timestamp_from_server_clock = 27 [
     (gogoproto.casttype) = "github.com/cockroachdb/cockroach/pkg/util/hlc.ClockTimestamp"];
+  // now is a clock reading from the sender of the request. It can be used by
+  // the receiver to update its local HLC, which can help stabilize HLCs across
+  // the cluster. The receiver is NOT required to use the timestamp to update
+  // its local clock, and the sender is NOT required to attach a timestamp.
+  util.hlc.Timestamp now = 28 [(gogoproto.nullable) = false,
+    (gogoproto.casttype) = "github.com/cockroachdb/cockroach/pkg/util/hlc.ClockTimestamp"];
   // replica specifies the destination of the request.
   ReplicaDescriptor replica = 2 [(gogoproto.nullable) = false];
   // range_id specifies the ID of the Raft consensus group which the key

pkg/kv/kvclient/kvcoord/dist_sender.go
@@ -653,14 +653,18 @@ func (ds *DistSender) initAndVerifyBatch(
 	ctx context.Context, ba *roachpb.BatchRequest,
 ) *roachpb.Error {
 	// Attach the local node ID to each request.
-	if ba.Header.GatewayNodeID == 0 {
-		ba.Header.GatewayNodeID = ds.getNodeID()
+	if ba.GatewayNodeID == 0 {
+		ba.GatewayNodeID = ds.getNodeID()
 	}
+
+	// Attach a clock reading from the local node to help stabilize HLCs across
+	// the cluster. This is NOT required for correctness.
+	ba.Now = ds.clock.NowAsClockTimestamp()
 
 	// In the event that timestamp isn't set and read consistency isn't
 	// required, set the timestamp using the local clock.
 	if ba.ReadConsistency != roachpb.CONSISTENT && ba.Timestamp.IsEmpty() {
-		ba.Timestamp = ds.clock.Now()
+		ba.Timestamp = ba.Now.ToTimestamp()
 	}
 
 	if len(ba.Requests) < 1 {

pkg/kv/kvserver/client_replica_test.go
@@ -118,6 +118,9 @@ func TestReplicaClockUpdates(t *testing.T) {
 		// MaxOffset. Set the synthetic flag according to the test case.
 		reqTS := clocks[0].Now().Add(clocks[0].MaxOffset().Nanoseconds()/2, 0).WithSynthetic(synthetic)
 		h := roachpb.Header{Timestamp: reqTS}
+		if !reqTS.Synthetic {
+			h.Now = hlc.ClockTimestamp(reqTS)
+		}
 
 		// Execute the command.
 		var req roachpb.Request
@@ -178,8 +181,8 @@ func TestLeaseholdersRejectClockUpdateWithJump(t *testing.T) {
 	const numCmds = 3
 	clockOffset := s.Clock().MaxOffset() / numCmds
 	for i := int64(1); i <= numCmds; i++ {
-		ts := ts1.Add(i*clockOffset.Nanoseconds(), 0).WithSynthetic(false)
-		if _, err := kv.SendWrappedWith(context.Background(), store.TestSender(), roachpb.Header{Timestamp: ts}, incArgs); err != nil {
+		ts := hlc.ClockTimestamp(ts1.Add(i*clockOffset.Nanoseconds(), 0))
+		if _, err := kv.SendWrappedWith(context.Background(), store.TestSender(), roachpb.Header{Now: ts}, incArgs); err != nil {
 			t.Fatal(err)
 		}
 	}
@@ -190,8 +193,8 @@ func TestLeaseholdersRejectClockUpdateWithJump(t *testing.T) {
 	}
 
 	// Once the accumulated offset reaches MaxOffset, commands will be rejected.
-	tsFuture := ts1.Add(s.Clock().MaxOffset().Nanoseconds()+1, 0).WithSynthetic(false)
-	_, pErr := kv.SendWrappedWith(ctx, store.TestSender(), roachpb.Header{Timestamp: tsFuture}, incArgs)
+	tsFuture := hlc.ClockTimestamp(ts1.Add(s.Clock().MaxOffset().Nanoseconds()+1, 0))
+	_, pErr := kv.SendWrappedWith(ctx, store.TestSender(), roachpb.Header{Now: tsFuture}, incArgs)
 	if !testutils.IsPError(pErr, "remote wall time is too far ahead") {
 		t.Fatalf("unexpected error %v", pErr)
 	}

pkg/kv/kvserver/replica_closedts_internal_test.go
@@ -728,7 +728,8 @@ func TestQueryResolvedTimestampResolvesAbandonedIntents(t *testing.T) {
 	require.Nil(t, pErr)
 	require.True(t, intentExists())
 
-	// Inject a closed timestamp.
+	// Bump the clock and inject a closed timestamp.
+	tc.manualClock.AdvanceTo(ts20.GoTime())
 	tc.repl.mu.Lock()
 	tc.repl.mu.state.RaftClosedTimestamp = ts20
 	tc.repl.mu.Unlock()

pkg/kv/kvserver/replica_test.go
@@ -231,9 +231,7 @@ func (tc *testContext) Sender() kv.Sender {
 				tc.Fatal(err)
 			}
 		}
-		if baClockTS, ok := ba.Timestamp.TryToClockTimestamp(); ok {
-			tc.Clock().Update(baClockTS)
-		}
+		tc.Clock().Update(ba.Now)
 		return ba
 	})
 }
@@ -5996,10 +5994,6 @@ func TestPushTxnPushTimestamp(t *testing.T) {
 		if reply.PusheeTxn.Status != roachpb.PENDING {
 			t.Errorf("expected pushed txn to have status PENDING; got %s", reply.PusheeTxn.Status)
 		}
-
-		// Sanity check clock update, or lack thereof.
-		after := tc.Clock().Now()
-		require.Equal(t, synthetic, after.Less(expTS))
 	})
 }
 
@@ -8677,7 +8671,7 @@ func TestRefreshFromBelowGCThreshold(t *testing.T) {
 			t.Run(fmt.Sprintf("gcThreshold=%s", testCase.gc), func(t *testing.T) {
 				if !testCase.gc.IsEmpty() {
 					gcr := roachpb.GCRequest{Threshold: testCase.gc}
-					_, pErr := tc.SendWrapped(&gcr)
+					_, pErr := tc.SendWrappedWith(roachpb.Header{Timestamp: testCase.gc}, &gcr)
 					require.Nil(t, pErr)
 				}
 
@@ -8883,12 +8877,11 @@ func BenchmarkMVCCGCWithForegroundTraffic(b *testing.B) {
 	tc.Start(ctx, b, stopper)
 
 	key := roachpb.Key("test")
-	now := func() hlc.Timestamp { return hlc.Timestamp{WallTime: timeutil.Now().UnixNano()} }
 
 	// send sends the Request with a present-time batch timestamp.
 	send := func(args roachpb.Request) *roachpb.BatchResponse {
 		var header roachpb.Header
-		header.Timestamp = now()
+		header.Timestamp = tc.Clock().Now()
 		ba := roachpb.BatchRequest{}
 		ba.Header = header
 		ba.Add(args)
@@ -8928,7 +8921,7 @@ func BenchmarkMVCCGCWithForegroundTraffic(b *testing.B) {
 		go func() {
 			defer wg.Done()
 			for {
-				gc(key, now()) // NB: These are no-op GC requests.
+				gc(key, tc.Clock().Now()) // NB: These are no-op GC requests.
 				time.Sleep(10 * time.Microsecond)
 
 				select {

pkg/kv/kvserver/store_send.go
@@ -14,6 +14,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
 	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
@@ -88,7 +89,19 @@ func (s *Store) SendWithWriteBytes(
 	// Update our clock with the incoming request timestamp. This advances the
 	// local node's clock to a high water mark from all nodes with which it has
 	// interacted.
-	if baClockTS, ok := ba.Timestamp.TryToClockTimestamp(); ok {
+	baClockTS := ba.Now
+	if baClockTS.IsEmpty() && !s.ClusterSettings().Version.IsActive(ctx, clusterversion.LocalTimestamps) {
+		// TODO(nvanbenschoten): remove this in v23.1. v21.2 nodes will still send
+		// requests without a Now field. This is not necessary for correctness now
+		// that local timestamps pulled from the leaseholder's own HLC are used in
+		// conjunction with observed timestamps to prevent stale reads, but using
+		// this timestamp when available can help stabilize HLCs.
+		//
+		// NOTE: we version gate this so that no test hits this branch and relies on
+		// this behavior.
+		baClockTS, _ = ba.Timestamp.DeprecatedTryToClockTimestamp()
+	}
+	if !baClockTS.IsEmpty() {
 		if s.cfg.TestingKnobs.DisableMaxOffsetCheck {
 			s.cfg.Clock.Update(baClockTS)
 		} else {

pkg/kv/kvserver/store_test.go
@@ -1087,14 +1087,14 @@ func TestStoreSendUpdateTime(t *testing.T) {
 	defer stopper.Stop(ctx)
 	store, _ := createTestStore(ctx, t, testStoreOpts{createSystemRanges: true}, stopper)
 	args := getArgs([]byte("a"))
-	reqTS := store.cfg.Clock.Now().Add(store.cfg.Clock.MaxOffset().Nanoseconds(), 0).WithSynthetic(false)
-	_, pErr := kv.SendWrappedWith(ctx, store.TestSender(), roachpb.Header{Timestamp: reqTS}, &args)
+	now := hlc.ClockTimestamp(store.cfg.Clock.Now().Add(store.cfg.Clock.MaxOffset().Nanoseconds(), 0))
+	_, pErr := kv.SendWrappedWith(ctx, store.TestSender(), roachpb.Header{Now: now}, &args)
 	if pErr != nil {
 		t.Fatal(pErr)
 	}
 	ts := store.cfg.Clock.Now()
-	if ts.WallTime != reqTS.WallTime || ts.Logical <= reqTS.Logical {
-		t.Errorf("expected store clock to advance to %s; got %s", reqTS, ts)
+	if ts.WallTime != now.WallTime || ts.Logical <= now.Logical {
+		t.Errorf("expected store clock to advance to %s; got %s", now, ts)
 	}
 }
 
@@ -1135,8 +1135,8 @@ func TestStoreSendWithClockOffset(t *testing.T) {
 	store, _ := createTestStore(ctx, t, testStoreOpts{createSystemRanges: true}, stopper)
 	args := getArgs([]byte("a"))
 	// Set args timestamp to exceed max offset.
-	reqTS := store.cfg.Clock.Now().Add(store.cfg.Clock.MaxOffset().Nanoseconds()+1, 0).WithSynthetic(false)
-	_, pErr := kv.SendWrappedWith(ctx, store.TestSender(), roachpb.Header{Timestamp: reqTS}, &args)
+	now := hlc.ClockTimestamp(store.cfg.Clock.Now().Add(store.cfg.Clock.MaxOffset().Nanoseconds()+1, 0))
+	_, pErr := kv.SendWrappedWith(ctx, store.TestSender(), roachpb.Header{Now: now}, &args)
 	if !testutils.IsPError(pErr, "remote wall time is too far ahead") {
 		t.Errorf("unexpected error: %v", pErr)
 	}

pkg/util/hlc/timestamp.go
@@ -386,16 +386,19 @@ func (LegacyTimestamp) SafeValue() {}
 // ClockTimestamp is a Timestamp with the added capability of being able to
 // update a peer's HLC clock. It possesses this capability because the clock
 // timestamp itself is guaranteed to have come from an HLC clock somewhere in
-// the system. As such, a clock timestamp is an promise that some node in the
+// the system. As such, a clock timestamp is a promise that some node in the
 // system has a clock with a reading equal to or above its value.
 //
 // ClockTimestamp is the statically typed version of a Timestamp with its
 // Synthetic flag set to false.
 type ClockTimestamp Timestamp
 
-// TryToClockTimestamp attempts to downcast a Timestamp into a ClockTimestamp.
-// Returns the result and a boolean indicating whether the cast succeeded.
-func (t Timestamp) TryToClockTimestamp() (ClockTimestamp, bool) {
+// DeprecatedTryToClockTimestamp attempts to downcast a Timestamp into a
+// ClockTimestamp. Returns the result and a boolean indicating whether the cast
+// succeeded.
+// TODO(nvanbenschoten): remove this in v23.1 when we remove the synthetic
+// timestamp bit.
+func (t Timestamp) DeprecatedTryToClockTimestamp() (ClockTimestamp, bool) {
 	if t.Synthetic {
 		return ClockTimestamp{}, false
 	}

commit ad7c2f7c39c22bdacf113577c017ccc0d0341dd9

roachpb: reduce size of BatchRequest struct below 256 bytes
Resolves #86541.

This commit reduces the size of `BatchRequest` from 264 bytes to 232 bytes. It
does so by packing fields and making the `TraceInfo` field nullable. In doing
so, it resolves a sizable performance regression caused by cf14049.

It's not entirely clear why we see the performance cliff once the size of this
struct reaches or exceeds 256 bytes, but the cliff is very visible in macro
benchmarks (less so in micro benchmarks). A theory floated in #86541 is that is
has to do with heap allocated `BatchRequest`s falling into a different malloc
small object size class. That seems plausible, but I don't know if that explains
the regression when the struct is exactly 256.

Another theory is that it is related to passing such a large struct by value to
functions throughout the sender stack. Are function arguments above a certain
size treated differently than those below a certain size? Or perhaps this was
causing additional stack growth that changed the number of times that goroutine
stacks needed to grow and the locations in which they grew. I'm not sure, but I
do have a WIP branch that finally switches to passing BatchRequest by reference
which shows an additional performance win.

\### Macro benchmarks
```
name                           old ops/s    new ops/s    delta
kv95/enc=false/nodes=3/cpu=32    109k ±10%    120k ± 4%   +9.85%  (p=0.000 n=10+10)

name                           old avg(ms)  new avg(ms)  delta
kv95/enc=false/nodes=3/cpu=32    1.79 ±12%    1.60 ± 0%  -10.61%  (p=0.000 n=10+7)

name                           old p99(ms)  new p99(ms)  delta
kv95/enc=false/nodes=3/cpu=32    8.53 ±17%    7.79 ± 4%   -8.68%  (p=0.004 n=10+10)
```

\### Micro benchmarks
```
name                        old time/op    new time/op    delta
KV/Insert/Native/rows=1-10    41.5µs ± 2%    41.4µs ± 2%    ~     (p=0.393 n=10+10)
KV/Insert/SQL/rows=1-10        123µs ± 1%     123µs ± 1%    ~     (p=0.356 n=9+10)

pkg/roachpb/api.proto
@@ -2428,9 +2428,6 @@ message Header {
   // supported requests from max_span_request_keys apply to the target_bytes
   // option as well.
   int64 target_bytes = 15;
-  // If true, allow returning an empty result when the first result exceeds a
-  // limit (e.g. TargetBytes). Only supported by Get, Scan, and ReverseScan.
-  bool allow_empty = 23;
   // If positive, Scan and ReverseScan requests with limits (MaxSpanRequestKeys
   // or TargetBytes) will not return results with partial SQL rows at the end
   // (recall that SQL rows can span multiple keys). Such partial rows will be
@@ -2444,6 +2441,9 @@ message Header {
   //
   // Added in 22.1, callers must check the ScanWholeRows version gate first.
   int32 whole_rows_of_size = 26;
+  // If true, allow returning an empty result when the first result exceeds a
+  // limit (e.g. TargetBytes). Only supported by Get, Scan, and ReverseScan.
+  bool allow_empty = 23;
   // If true, DistSender returns partial non-empty results when encountering a
   // range boundary, with an appropriate resume span and reason
   // RESUME_RANGE_BOUNDARY. This will disable parallelism of DistSender
@@ -2472,18 +2472,6 @@ message Header {
   // EndTxnRequest. Currently set conservatively: a request might be
   // composed of distinct spans yet have this field set to false.
   bool distinct_spans = 9;
-  // client_range_info represents the kvclient's knowledge about the state of
-  // the range (i.e. of the range descriptor and lease). The kvserver checks
-  // whether the client's info is up to date and, if it isn't, it will return a
-  // RangeInfo with up-to-date information. Typically this entire field is set
-  // by the client's DistSender, however it will preserve the value of the field 
-  // `ExplicitlyRequested` so that requests passed to DistSender can request 
-  // `RangeInfos` if desired.
-  ClientRangeInfo client_range_info = 17 [(gogoproto.nullable) = false];
-  // gateway_node_id is the ID of the gateway node where the request originated.
-  // For requests from tenants, this is set to the NodeID of the KV node handling
-  // the BatchRequest.
-  int32 gateway_node_id = 11 [(gogoproto.customname) = "GatewayNodeID", (gogoproto.casttype) = "NodeID"];
   // If set, the request will return to the client before proposing the
   // request into Raft. All consensus processing will be performed
   // asynchronously. Because consensus may fail, this means that the
@@ -2501,6 +2489,18 @@ message Header {
   // current batch. When set, it allows the server to handle pushes and write
   // too old conditions locally.
   bool can_forward_read_timestamp = 16;
+  // gateway_node_id is the ID of the gateway node where the request originated.
+  // For requests from tenants, this is set to the NodeID of the KV node handling
+  // the BatchRequest.
+  int32 gateway_node_id = 11 [(gogoproto.customname) = "GatewayNodeID", (gogoproto.casttype) = "NodeID"];
+  // client_range_info represents the kvclient's knowledge about the state of
+  // the range (i.e. of the range descriptor and lease). The kvserver checks
+  // whether the client's info is up to date and, if it isn't, it will return a
+  // RangeInfo with up-to-date information. Typically this entire field is set
+  // by the client's DistSender, however it will preserve the value of the field
+  // `ExplicitlyRequested` so that requests passed to DistSender can request
+  // `RangeInfos` if desired.
+  ClientRangeInfo client_range_info = 17 [(gogoproto.nullable) = false];
   // bounded_staleness is set when a read-only batch is performing a bounded
   // staleness read and wants its timestamp to be chosen dynamically, based
   // on a resolved timestamp from its target replica(s).
@@ -2512,7 +2512,7 @@ message Header {
   // Requests with a non-zero timestamp are not allowed to set this field.
   BoundedStalenessHeader bounded_staleness = 22;
 
-  util.tracing.tracingpb.TraceInfo trace_info = 25 [(gogoproto.nullable) = false];
+  util.tracing.tracingpb.TraceInfo trace_info = 25;
 
   reserved 7, 10, 12, 14, 20;
 }

pkg/server/node.go
@@ -1156,7 +1156,7 @@ func setupSpanForIncomingRPC(
 		var remoteParent tracing.SpanMeta
 		if !ba.TraceInfo.Empty() {
 			ctx, newSpan = tr.StartSpanCtx(ctx, grpcinterceptor.BatchMethodName,
-				tracing.WithRemoteParentFromTraceInfo(&ba.TraceInfo),
+				tracing.WithRemoteParentFromTraceInfo(ba.TraceInfo),
 				tracing.WithServerSpanKind)
 		} else {
 			// For backwards compatibility with 21.2, if tracing info was passed as

pkg/sql/distsql/server.go
@@ -607,7 +607,7 @@ func (ds *ServerImpl) setupSpanForIncomingRPC(
 
 	if !req.TraceInfo.Empty() {
 		return tr.StartSpanCtx(ctx, grpcinterceptor.SetupFlowMethodName,
-			tracing.WithRemoteParentFromTraceInfo(&req.TraceInfo),
+			tracing.WithRemoteParentFromTraceInfo(req.TraceInfo),
 			tracing.WithServerSpanKind)
 	}
 	// For backwards compatibility with 21.2, if tracing info was passed as

pkg/sql/execinfrapb/api.proto
@@ -29,7 +29,7 @@ import "util/tracing/tracingpb/tracing.proto";
 message SetupFlowRequest {
   reserved 1, 2;
 
-  optional util.tracing.tracingpb.TraceInfo trace_info = 11 [(gogoproto.nullable) = false];
+  optional util.tracing.tracingpb.TraceInfo trace_info = 11;
 
   // LeafTxnInputState is the input parameter for the *client.Txn needed for
   // executing the flow.

pkg/util/tracing/span.go
@@ -875,9 +875,9 @@ func (sm SpanMeta) String() string {
 	return s.String()
 }
 
-// ToProto converts a SpanMeta to the TraceInfo proto.
-func (sm SpanMeta) ToProto() tracingpb.TraceInfo {
-	ti := tracingpb.TraceInfo{
+// ToProto converts a SpanMeta to the *TraceInfo proto.
+func (sm SpanMeta) ToProto() *tracingpb.TraceInfo {
+	ti := &tracingpb.TraceInfo{
 		TraceID:       sm.traceID,
 		ParentSpanID:  sm.spanID,
 		RecordingMode: sm.recordingType.ToProto(),

pkg/util/tracing/tracingpb/tracing.go
@@ -12,5 +12,5 @@ package tracingpb
 
 // Empty returns true if t does not have any tracing info in it.
 func (t *TraceInfo) Empty() bool {
-	return t.TraceID == 0
+	return t == nil || t.TraceID == 0
 }

pkg/roachpb/api_test.go
@@ -13,6 +13,7 @@ package roachpb
 import (
 	"reflect"
 	"testing"
+	"unsafe"
 
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/concurrency/lock"
 	"github.com/cockroachdb/cockroach/pkg/storage/enginepb"
@@ -405,4 +406,12 @@ func TestFlagCombinations(t *testing.T) {
 			}
 		}
 	}
+}
+
+// TestBatchRequestSize asserts that the size of BatchRequest remains below 256
+// bytes. In #86541, we found that once the size reaches or exceeds this size,
+// end-to-end benchmarks observe a large (~10%) performance regression.
+func TestBatchRequestSize(t *testing.T) {
+	size := int(unsafe.Sizeof(BatchRequest{}))
+	require.Less(t, size, 256)
 }

pkg/util/tracing/tracer_test.go
@@ -553,7 +553,7 @@ func TestSpanRecordingFinished(t *testing.T) {
 
 	tr2 := NewTracer()
 	childTraceInfo := child.Meta().ToProto()
-	remoteChildChild := tr2.StartSpan("root.child.remotechild", WithRemoteParentFromTraceInfo(&childTraceInfo))
+	remoteChildChild := tr2.StartSpan("root.child.remotechild", WithRemoteParentFromTraceInfo(childTraceInfo))
 	child.ImportRemoteRecording(remoteChildChild.GetRecording(tracingpb.RecordingVerbose))
 	remoteChildChild.Finish()
 