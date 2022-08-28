66273f3ad718ccf7c3652fa51c480ef8b1567469
Marylia Gutierrez <marylia@cockroachlabs.com>
2022-08-28 14:18:16 -0400

    ui: update sub-optimal label
    Update label from `Suboptimal Plan` to `Sub-Optimal Plan`.
    
    Release justification: low risk change
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/insights/types.ts
+++ pkg/ui/workspaces/cluster-ui/src/insights/types.ts
@@ -144,7 +144,7 @@ const suboptimalPlanInsight = (execType: InsightExecEnum): Insight => {
     `due to outdated statistics or missing indexes.`;
   return {
     name: InsightNameEnum.suboptimalPlan,
-    label: "Suboptimal Plan",
+    label: "Sub-Optimal Plan",
     description: description,
     tooltipDescription:
       description + ` Click the ${execType} execution ID to see more details.`,
