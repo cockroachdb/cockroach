b13cf32188eac0d4df7847a10aa74d2a79e55d21
Jayant Shrivastava <jayants@cockroachlabs.com>
2022-08-29 15:38:01 -0400

    do not merge: force parallel event consumer for testing
    This change forces 32 parallel event consumer to be used
    by changefeeds for testing purposes. Once CI passes, showing
    that existing functionality is maintained by parallel consumers,
    this commit can be dropped, reverting the number of consumers
    to be a metamorphic constant.
    
    Release justification: This will not be merged.
    Release note: None

--- pkg/ccl/changefeedccl/changefeedbase/settings.go
+++ pkg/ccl/changefeedccl/changefeedbase/settings.go
@@ -13,7 +13,6 @@ import (
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/settings"
-	"github.com/cockroachdb/cockroach/pkg/util"
 	"github.com/cockroachdb/errors"
 )
 
@@ -238,6 +237,6 @@ var EventConsumerMaxWorkers = settings.RegisterIntSetting(
 	settings.TenantWritable,
 	"changefeed.consumer_max_workers",
 	"the maximum number of workers to use when processing events; 0 disables",
-	int64(util.ConstantWithMetamorphicTestRange("changefeed.consumer_max_workers", 0, 0, 32)),
+	32,
 	settings.NonNegativeInt,
 ).WithPublic()
