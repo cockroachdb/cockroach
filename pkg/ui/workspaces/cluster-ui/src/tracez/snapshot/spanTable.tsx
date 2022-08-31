f011608df677046ebe3311d220f9cd42b37c7f5b
Ben Bardin <bardin@cockroachlabs.com>
2022-08-31 16:43:41 -0400

    pkg/ui: add first stage of redesigned tracez page
    Begins to implement
    
    https://www.figma.com/file/LUptGAxt5x1idq6eZLA50o/BulkIO-Traces?node-id=59%3A7958
    
    When complete, the new pages will offer:
    - A robust route structure and full redux integration for better
    navigation.
    - Denser information display.
    - More information display, i.e. start time/duration, and expanding mutliple
    tag groups.
    - A new span details page, which will include child span information.
    
    For now, this page is behind a new route with no entrypoint.
    It implements the core route structure, redux selectors and
    API calls.
    Left for later PRs are Take Snapshot, the Span Details page, pagination,
    search filtering, and View Raw JSON.
    
    Release note: None

---
+++ pkg/ui/workspaces/cluster-ui/src/tracez/snapshot/spanTable.tsx
@@ -0,0 +1,254 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+import moment from "moment";
+import React, { useState } from "react";
+import { Nodes, Caution, Plus, Minus } from "@cockroachlabs/icons";
+import { Span, Snapshot } from "src/api/tracezApi";
+import { EmptyTable } from "src/empty";
+import { ColumnDescriptor, SortSetting, SortedTable } from "src/sortedtable";
+
+import styles from "../snapshot.module.scss";
+import classNames from "classnames/bind";
+import { TimestampToMoment } from "src/util";
+import { cockroach } from "@cockroachlabs/crdb-protobuf-client";
+import ISpanTag = cockroach.server.serverpb.ISpanTag;
+import RecordingMode = cockroach.util.tracing.tracingpb.RecordingMode;
+import { CircleFilled } from "../../icon";
+import { Dropdown } from "src/dropdown";
+import "antd/lib/switch/style";
+const cx = classNames.bind(styles);
+
+class SpanSortedTable extends SortedTable<Span> {}
+
+interface TagRowProps {
+  t: ISpanTag;
+}
+const Tag = ({ t }: TagRowProps) => {
+  let highlight = null;
+  if (t.highlight) {
+    highlight = <Caution />;
+  }
+  let arrow = null;
+  if (t.inherited) {
+    arrow = " ↓";
+  } else if (t.copied_from_child) {
+    arrow = " ↑";
+  }
+  return (
+    <div className={cx("tag")}>
+      {highlight}
+      {t.key}
+      {arrow}
+      {t.val && ": "}
+      {t.val}
+    </div>
+  );
+};
+
+const TagCell = (props: { span: Span }) => {
+  const tagGroups = props.span.processed_tags.filter(
+    tag => tag?.children?.length,
+  );
+  const tags = props.span.processed_tags.filter(tag => !tag?.children?.length);
+
+  let hiddenTagGroup = null;
+  if (tagGroups.length && tagGroups[tagGroups.length - 1].key === "...") {
+    // This should always be present, and always be last, but wrap it in the check just in case.
+    hiddenTagGroup = tagGroups.pop();
+  }
+
+  return (
+    <div className={cx("tag-cell")}>
+      <div className={cx("single-tags-row")}>
+        {tags.map((t, i) => (
+          <Tag t={t} key={i} />
+        ))}
+        {hiddenTagGroup && <TagGroup tag={hiddenTagGroup} />}
+      </div>
+      {tagGroups.map((t, i) => (
+        <TagGroup tag={t} key={i} />
+      ))}
+    </div>
+  );
+};
+
+const TagGroup = (props: { tag: ISpanTag }) => {
+  const { tag } = props;
+  const [isExpanded, setExpanded] = useState(false);
+  const { children } = tag;
+  return (
+    <div className={cx("tag")}>
+      {!isExpanded && (
+        <div className={cx("tag-group")}>
+          <Plus
+            className={cx("plus-minus")}
+            onClick={() => {
+              setExpanded(!isExpanded);
+            }}
+          />
+          <div>
+            &nbsp;
+            {tag.key + " (" + tag.children.length + ")"}
+          </div>
+        </div>
+      )}
+      {isExpanded && (
+        <div className={cx("tag-group")}>
+          <Minus
+            className={cx("plus-minus")}
+            onClick={() => {
+              setExpanded(!isExpanded);
+            }}
+          />
+          <div>
+            &nbsp;
+            {tag.key + " (" + tag.children.length + ")"}
+            <table>
+              <tbody>
+                {children.map((child, i) => (
+                  <tr key={i}>
+                    <td>
+                      <strong>{child.key + ":"}</strong>&nbsp;
+                    </td>
+                    <td>{child.val}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const formatDuration = (d: moment.Duration): string => {
+  const hours = Math.floor(d.asHours());
+  const hourStr = hours ? hours + "h " : "";
+  const minutes = d.minutes();
+  let minuteStr = "";
+  if (hourStr) {
+    minuteStr = minutes.toFixed(0).padStart(2, "0") + "m ";
+  } else if (minutes) {
+    minuteStr = minutes + "m ";
+  }
+  const secondPadNum = hourStr || minuteStr ? 2 : 1;
+  const secondStr = d.seconds().toFixed(0).padStart(secondPadNum, "0");
+  const msStr = "." + d.milliseconds().toFixed(0).padEnd(3, "0") + "s";
+  return hourStr + minuteStr + secondStr + msStr;
+};
+const makeColumns = (
+  snapshot: Snapshot,
+  relativeTimeMode: boolean,
+  setRelativeTimeMode: (value: boolean) => void,
+): ColumnDescriptor<Span>[] => {
+  const startTime = TimestampToMoment(snapshot.captured_at);
+
+  const timeModes = [
+    {
+      name: "Start Time (UTC)",
+      value: false,
+    },
+    {
+      name: "Duration",
+      value: true,
+    },
+  ];
+  return [
+    {
+      name: "icons",
+      title: "",
+      cell: span => {
+        return span.current_recording_mode != RecordingMode.OFF ? (
+          // Use a viewbox to shrink the icon just a bit while leaving it centered.
+          <CircleFilled className={cx("icon-red")} viewBox={"-1 -1 12 12"} />
+        ) : span.current ? (
+          <CircleFilled className={cx("icon-green")} viewBox={"-1 -1 12 12"} />
+        ) : null;
+      },
+      className: cx("icon-cell"),
+    },
+    {
+      name: "span",
+      title: "Span",
+      titleAlign: "left",
+      cell: span => span.operation,
+      sort: span => span.operation,
+      hideTitleUnderline: true,
+      className: cx("operation-cell"),
+    },
+    {
+      name: "time",
+      title: (
+        <div
+          onClick={e => {
+            e.stopPropagation();
+          }}
+          className={cx("table-title-time")}
+        >
+          <Dropdown<boolean>
+            items={timeModes}
+            onChange={value => {
+              setRelativeTimeMode(value);
+            }}
+          >
+            {relativeTimeMode ? "Duration" : "Start Time (UTC)"}
+          </Dropdown>
+        </div>
+      ),
+      titleAlign: "right",
+      cell: span => {
+        return relativeTimeMode
+          ? formatDuration(
+              moment.duration(startTime.diff(TimestampToMoment(span.start))),
+            )
+          : TimestampToMoment(span.start).format("YYYY-MM-DD HH:mm:ss");
+      },
+      // This sort is backwards in duration mode, but toggling between sort keys within a column really trips things up.
+      sort: span => TimestampToMoment(span.start).unix(),
+      hideTitleUnderline: true,
+      className: cx("table-cell-time"),
+    },
+    {
+      name: "tags",
+      title: "Tags",
+      titleAlign: "left",
+      cell: span => <TagCell span={span} />,
+      hideTitleUnderline: true,
+      className: cx("table-cell"),
+    },
+  ];
+};
+
+export interface SpanTableProps {
+  sort: SortSetting;
+  setSort: (value: SortSetting) => void;
+  snapshot: Snapshot;
+}
+
+export const SpanTable: React.FC<SpanTableProps> = props => {
+  const { snapshot, sort, setSort } = props;
+  const spans = snapshot?.spans;
+
+  const [relativeTimeMode, setRelativeTimeMode] = useState(false);
+  if (!spans) {
+    return <EmptyTable title="No spans to show" icon={<Nodes />} />;
+  }
+
+  return (
+    <SpanSortedTable
+      data={spans}
+      sortSetting={sort}
+      onChangeSortSetting={setSort}
+      columns={makeColumns(snapshot, relativeTimeMode, setRelativeTimeMode)}
+      rowClass={() => cx("table-row")}
+    />
+  );
+};
