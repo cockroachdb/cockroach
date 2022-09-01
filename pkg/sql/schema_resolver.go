90b962a72e4c0c748dddedf3f2875473b44bdd8f
Chengxiong Ruan <chengxiongruan@gmail.com>
2022-09-01 16:18:46 -0400

    sql: check function dependencies when renaming databases
    Previously, we only check table dependencies when renaming
    a database. With UDF introduced, we need to handle function
    depencies as well.
    
    Release note: None.
    Release justification: necessary bug fix with UDF introduced.

--- pkg/sql/schema_resolver.go
+++ pkg/sql/schema_resolver.go
@@ -224,6 +224,33 @@ func (sr *schemaResolver) getQualifiedTableName(
 	return &tbName, nil
 }
 
+func (sr *schemaResolver) getQualifiedFunctionName(
+	ctx context.Context, fnDesc catalog.FunctionDescriptor,
+) (*tree.FunctionName, error) {
+	_, dbDesc, err := sr.descCollection.GetImmutableDatabaseByID(ctx, sr.txn, fnDesc.GetParentID(),
+		tree.DatabaseLookupFlags{
+			Required:       true,
+			IncludeOffline: true,
+			IncludeDropped: true,
+			AvoidLeased:    true,
+		})
+	if err != nil {
+		return nil, err
+	}
+	scDesc, err := sr.descCollection.GetImmutableSchemaByID(ctx, sr.txn, fnDesc.GetParentSchemaID(),
+		tree.SchemaLookupFlags{
+			IncludeOffline: true,
+			IncludeDropped: true,
+			AvoidLeased:    true,
+		})
+	if err != nil {
+		return nil, err
+	}
+
+	fnName := tree.MakeQualifiedFunctionName(dbDesc.GetName(), scDesc.GetName(), fnDesc.GetName())
+	return &fnName, nil
+}
+
 // ResolveType implements the tree.TypeReferenceResolver interface.
 func (sr *schemaResolver) ResolveType(
 	ctx context.Context, name *tree.UnresolvedObjectName,
