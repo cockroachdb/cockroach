b0093ea6475ffaf4115d8dceff6bb5bd75ccd22a
Pavel Kalinnikov <pavel@cockroachlabs.com>
2022-09-01 11:22:06 +0100

    kvserver: cancel checksum computation on quiescing
    The checksum computation can take long, so if the store quiesces, it's better
    to cancel it.
    
    Release justification: part of a performance improvement PR
    Release note: None

--- pkg/kv/kvserver/replica_consistency.go
+++ pkg/kv/kvserver/replica_consistency.go
@@ -747,7 +747,6 @@ func (*Replica) sha512(
 }
 
 func (r *Replica) computeChecksumPostApply(ctx context.Context, cc kvserverpb.ComputeChecksum) {
-	stopper := r.store.Stopper()
 	now := timeutil.Now()
 	r.mu.Lock()
 	var notify chan struct{}
@@ -800,7 +799,7 @@ func (r *Replica) computeChecksumPostApply(ctx context.Context, cc kvserverpb.Co
 	//
 	// Don't use the proposal's context for this, as it likely to be canceled very
 	// soon.
-	const taskName = "storage.Replica: computing checksum"
+	const taskName = "kvserver.Replica: computing checksum"
 	sem := r.store.consistencySem
 	if cc.Mode == roachpb.ChecksumMode_CHECK_STATS {
 		// Stats-only checks are cheap, and the DistSender parallelizes these across
@@ -808,11 +807,15 @@ func (r *Replica) computeChecksumPostApply(ctx context.Context, cc kvserverpb.Co
 		// they don't count towards the semaphore limit.
 		sem = nil
 	}
-	if err := stopper.RunAsyncTaskEx(r.AnnotateCtx(context.Background()), stop.TaskOpts{
+	stopper := r.store.Stopper()
+	taskCtx, taskCancel := stopper.WithCancelOnQuiesce(r.AnnotateCtx(context.Background()))
+	if err := stopper.RunAsyncTaskEx(taskCtx, stop.TaskOpts{
 		TaskName:   taskName,
 		Sem:        sem,
 		WaitForSem: false,
 	}, func(ctx context.Context) {
+		defer taskCancel()
+
 		if err := contextutil.RunWithTimeout(ctx, taskName, consistencyCheckAsyncTimeout,
 			func(ctx context.Context) error {
 				defer snap.Close()
@@ -877,6 +880,7 @@ A file preventing this node from restarting was placed at:
 		}
 
 	}); err != nil {
+		taskCancel()
 		defer snap.Close()
 		log.Errorf(ctx, "could not run async checksum computation (ID = %s): %v", cc.ChecksumID, err)
 		// Set checksum to nil.
