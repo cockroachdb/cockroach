cb2e8b4c2f25f49f4660af162d7431cfdd9dfe15
Mark Sirek <sirek@cockroachlabs.com>
2022-09-05 14:47:31 -0700

    logictestccl: Fix regional_by_row_query_behavior flake
    Fixes #86284
    
    This fixes a test flake in regional_by_row_query_behavior which is
    caused by 2 issues:
    
      1. Zone configs not becoming available in `SystemConfig` between
         table creation time and query execution time.
    
      2. Metamorphic testing modify the `coldata-batch-size` setting,
         causing a different number of kv reads to be done, which affects
         the kv trace results in this test.
    
    The flake can be reproduced via the command:
    ```
    ./dev testlogic ccl --files=regional_by_row_query_behavior \
                        --config=multiregion-9node-3region-3azs --stress
    ```
    For point 1, the `retry` logic test directive is enhanced to purge the
    zone config cache in the SystemConfig when a failing test is retried,
    allowing the updated zone config to be read.
    
    For point 2, the logic test blocking flag `!metamorphic` which resets
    batch size related settings that were modified in metamorphic mode back
    to their default production values, is updated to also cover the
    `coldata-batch-size` setting. The flag is renamed to
    `!metamorphic-batch-sizes` to better reflect its actual effect.
    
    Release justification: low risk fix for test flake
    
    Release note: None

--- pkg/sql/logictest/logictestbase/logictestbase.go
+++ pkg/sql/logictest/logictestbase/logictestbase.go
@@ -23,7 +23,6 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/build"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
-	"github.com/cockroachdb/cockroach/pkg/util"
 )
 
 var (
@@ -580,7 +579,7 @@ func (l stdlogger) Logf(format string, args ...interface{}) {
 // If the file doesn't contain a directive, the default config is returned.
 func ReadTestFileConfigs(
 	t logger, path string, defaults ConfigSet,
-) (_ ConfigSet, onlyNonMetamorphic bool) {
+) (_ ConfigSet, nonMetamorphicBatchSizes bool) {
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, false
@@ -604,8 +603,8 @@ func ReadTestFileConfigs(
 			if len(fields) == 2 {
 				t.Fatalf("%s: empty LogicTest directive", path)
 			}
-			cs, onlyNonMetamorphic := processConfigs(t, path, defaults, fields[2:])
-			return cs, onlyNonMetamorphic
+			cs, nonMetamorphicBatchSizes := processConfigs(t, path, defaults, fields[2:])
+			return cs, nonMetamorphicBatchSizes
 		}
 	}
 	// No directive found, return the default config.
@@ -631,10 +630,11 @@ func getBlocklistIssueNo(blocklistDirective string) (string, int) {
 
 // processConfigs, given a list of configNames, returns the list of
 // corresponding logicTestConfigIdxs as well as a boolean indicating whether
-// the test works only in non-metamorphic setting.
+// metamorphic settings related to batch sizes should be overridden with default
+// production values.
 func processConfigs(
 	t logger, path string, defaults ConfigSet, configNames []string,
-) (_ ConfigSet, onlyNonMetamorphic bool) {
+) (_ ConfigSet, nonMetamorphicBatchSizes bool) {
 	const blocklistChar = '!'
 	// blocklist is a map from a blocked config to a corresponding issue number.
 	// If 0, there is no associated issue.
@@ -662,12 +662,12 @@ func processConfigs(
 		}
 	}
 
-	if _, ok := blocklist["metamorphic"]; ok && util.IsMetamorphicBuild() {
-		onlyNonMetamorphic = true
+	if _, ok := blocklist["metamorphic-batch-sizes"]; ok {
+		nonMetamorphicBatchSizes = true
 	}
 	if len(blocklist) != 0 && allConfigNamesAreBlocklistDirectives {
 		// No configs specified, this blocklist applies to the default configs.
-		return applyBlocklistToConfigs(defaults, blocklist), onlyNonMetamorphic
+		return applyBlocklistToConfigs(defaults, blocklist), nonMetamorphicBatchSizes
 	}
 
 	var configs ConfigSet
@@ -696,7 +696,7 @@ func processConfigs(
 		}
 	}
 
-	return configs, onlyNonMetamorphic
+	return configs, nonMetamorphicBatchSizes
 }
 
 // applyBlocklistToConfigs applies the given blocklist to configs, returning the
