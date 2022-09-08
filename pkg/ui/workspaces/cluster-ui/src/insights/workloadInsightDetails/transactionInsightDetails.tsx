0d8f05815ba6128499b68c34894e9029d7cd3711
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2022-09-08 16:03:39 -0400

    ui: fix txn insight query bug, align summary card, remove contended keys in details page
    This commit fixes a small bug on the transaction insight details page
    that was incorrectly mapping the waiting transaction statement
    fingerprints to the blocking transaction statements. The commit also
    aligns the summary cards in the details page. The commit also removes
    the contended key from the details page while we look for a more user-
    friendly format to display row contention.
    
    Release note: None
    Release justification: bug fix

--- pkg/ui/workspaces/cluster-ui/src/insights/workloadInsightDetails/transactionInsightDetails.tsx
+++ pkg/ui/workspaces/cluster-ui/src/insights/workloadInsightDetails/transactionInsightDetails.tsx
@@ -156,7 +156,11 @@ export class TransactionInsightDetails extends React.Component<TransactionInsigh
             </Col>
           </Row>
           <Row gutter={24} className={tableCx("margin-bottom")}>
-            <InsightsSortedTable columns={insightsColumns} data={tableData} />
+            {/* TO DO (ericharmeling): We might want this table to span the entire page when other types of insights
+            are added*/}
+            <Col className="gutter-row"  span={12}>
+              <InsightsSortedTable columns={insightsColumns} data={tableData} />
+            </Col>
           </Row>
         </section>
         <section className={tableCx("section")}>
@@ -167,14 +171,12 @@ export class TransactionInsightDetails extends React.Component<TransactionInsigh
             tableName={insightDetails.tableName}
             indexName={insightDetails.indexName}
             databaseName={insightDetails.databaseName}
-            contendedKey={String(insightDetails.contendedKey)}
             waitTime={moment.duration(insightDetails.elapsedTime)}
             waitingExecutions={[]}
             blockingExecutions={[]}
           />
           <Row gutter={24}>
-            <Col>
-              <Row>
+            <Col className="gutter-row">
                 <Heading type="h5">
                   {WaitTimeInsightsLabels.BLOCKED_TXNS_TABLE_TITLE(
                     insightDetails.executionID,
@@ -187,7 +189,6 @@ export class TransactionInsightDetails extends React.Component<TransactionInsigh
                     execType={insightDetails.execType}
                   />
                 </div>
-              </Row>
             </Col>
           </Row>
         </section>
