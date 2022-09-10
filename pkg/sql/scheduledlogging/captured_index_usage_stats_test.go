5fe54713e79f0ccd7b36b169f67aadee10c6a2bb
Raphael 'kena' Poss <knz@thaumogen.net>
2022-09-10 10:43:43 +0200

    scheduledlogging: skip TestCaptureIndexUsageStats under short
    This test takes between 1 and 2 minutes to run. (#87772)
    
    Release note: None

--- pkg/sql/scheduledlogging/captured_index_usage_stats_test.go
+++ pkg/sql/scheduledlogging/captured_index_usage_stats_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
@@ -65,6 +66,9 @@ func (s *stubDurations) getOverlapDuration() time.Duration {
 
 func TestCaptureIndexUsageStats(t *testing.T) {
 	defer leaktest.AfterTest(t)()
+
+	skip.UnderShort(t, "#87772")
+
 	sc := log.ScopeWithoutShowLogs(t)
 	defer sc.Close(t)
 
