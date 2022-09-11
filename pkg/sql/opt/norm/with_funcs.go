8b9385b3365d99f30433753f26e649c7146a1e49
Mark Sirek <sirek@cockroachlabs.com>
2022-09-11 06:54:06 +0000

    norm: early inlining of WITH bound expression with VALUES
    Fixes https://github.com/cockroachlabs/support/issues/1769
    
    This commit implements early inlining of WITH clause bound expressions
    (at the time when the WithScanExpr is normalized) if the
    WITH bound expression is a ValuesExpr with all constant expressions or
    placeholders and it appears in an IN or NOT IN expression, for example:
    `column IN (VALUES(...))` or `column NOT IN(VALUES(...))`.
    Not doing early inlining can result in a superfluous join.
    
    Normally WITH clause inlining is done when the top-level WithExpr is
    normalized, but since normalization is done bottom-up, the WithExpr is
    processed last, making the inlining too late to trigger other
    normalization rules.
    
    Release note (performance improvement): This patch adds early inlining
    of VALUES clauses and unnested arrays in WITH queries in order to
    eliminate unnecessary joins.

--- pkg/sql/opt/norm/with_funcs.go
+++ pkg/sql/opt/norm/with_funcs.go
@@ -13,6 +13,7 @@ package norm
 import (
 	"github.com/cockroachdb/cockroach/pkg/sql/opt"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/memo"
+	"github.com/cockroachdb/errors"
 )
 
 // CanInlineWith returns whether or not it's valid to inline binding in expr.
@@ -60,4 +61,69 @@ func (c *CustomFuncs) InlineWith(binding, input memo.RelExpr, priv *memo.WithPri
 	}
 
 	return replace(input).(memo.RelExpr)
+}
+
+// CanInlineWithScan returns whether or not it's valid and heuristically cheaper
+// to inline a WithScanExpr with its bound expression from the memo. Currently
+// this only allows inlining leak-proof constant VALUES clauses of the form
+// `column IN (VALUES(...))` or `column NOT IN(VALUES(...))`, but could likely
+// be extended to handle other expressions in the future.
+func (c *CustomFuncs) CanInlineWithScan(private *memo.WithScanPrivate, scalar opt.ScalarExpr) bool {
+	if !private.CanInlineInPlace {
+		return false
+	}
+	// If we don't have `column IN(...)` or `column NOT IN(...)` or
+	// (col1, col2 ... coln) IN/NOT IN (...), it is not cheaper to inline because
+	// we wouldn't be avoiding one or more joins.
+	if tupleExpr, ok := scalar.(*memo.TupleExpr); ok {
+		for _, scalarExpr := range tupleExpr.Elems {
+			if scalarExpr.Op() != opt.VariableOp {
+				return false
+			}
+		}
+	} else if scalar.Op() != opt.VariableOp {
+		return false
+	}
+	expr := c.mem.Metadata().WithBinding(private.With)
+	var valuesExpr *memo.ValuesExpr
+	var ok bool
+	if valuesExpr, ok = expr.(*memo.ValuesExpr); !ok {
+		return false
+	}
+	if !valuesExpr.IsConstantsAndPlaceholders() {
+		return false
+	}
+	return true
+}
+
+// InlineWithScan replaces a WithScanExpr with its bound expression, mapped to
+// new output ColumnIDs.
+func (c *CustomFuncs) InlineWithScan(private *memo.WithScanPrivate) memo.RelExpr {
+	expr := c.mem.Metadata().WithBinding(private.With)
+	var valuesExpr *memo.ValuesExpr
+	var ok bool
+	valuesExpr.Op()
+	if valuesExpr, ok = expr.(*memo.ValuesExpr); !ok {
+		// Didn't find the expected VALUES.
+		panic(errors.AssertionFailedf("attempt to inline a WithScan which is not a VALUES clause; operator: %s",
+			expr.Op().String()))
+	}
+	projections := make(memo.ProjectionsExpr, len(private.InCols))
+	for i := range private.InCols {
+		projections[i] = c.f.ConstructProjectionsItem(
+			c.f.ConstructVariable(private.InCols[i]),
+			private.OutCols[i],
+		)
+	}
+	// Shallow copy the values in case this WITH binding is inlined more than
+	// once.
+	newRows := make(memo.ScalarListExpr, len(valuesExpr.Rows))
+	copy(newRows, valuesExpr.Rows)
+	newCols := make(opt.ColList, len(valuesExpr.ValuesPrivate.Cols))
+	copy(newCols, valuesExpr.ValuesPrivate.Cols)
+	newValuesExpr := c.f.ConstructValues(newRows, &memo.ValuesPrivate{
+		Cols: newCols,
+		ID:   c.f.Metadata().NextUniqueID(),
+	})
+	return c.f.ConstructProject(newValuesExpr, projections, opt.ColSet{})
 }
