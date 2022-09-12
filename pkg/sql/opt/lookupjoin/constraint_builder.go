878eecf977d663099a4cb82fd764d0b4fe484c2b
Marcus Gartner <marcus@cockroachlabs.com>
2022-09-12 14:56:23 -0400

    opt: reduce allocations in extracting join conditions
    This commit reduces allocations in join condition extract functions by
    only allocating slices when necessary.
    
    ```
    name                         old time/op    new time/op    delta
    SlowQueries/slow-query-1-24    36.6ms ± 0%    35.6ms ± 0%   -2.79%  (p=0.008 n=5+5)
    SlowQueries/slow-query-2-24     516ms ± 1%     506ms ± 0%   -1.87%  (p=0.008 n=5+5)
    SlowQueries/slow-query-3-24    99.5ms ± 1%    98.8ms ± 2%     ~     (p=0.310 n=5+5)
    
    name                         old alloc/op   new alloc/op   delta
    SlowQueries/slow-query-1-24    12.1MB ± 0%    11.9MB ± 0%   -2.22%  (p=0.008 n=5+5)
    SlowQueries/slow-query-2-24     156MB ± 0%     154MB ± 0%   -1.12%  (p=0.008 n=5+5)
    SlowQueries/slow-query-3-24    47.5MB ± 0%    47.5MB ± 0%   -0.01%  (p=0.008 n=5+5)
    
    name                         old allocs/op  new allocs/op  delta
    SlowQueries/slow-query-1-24      129k ± 0%      110k ± 0%  -14.70%  (p=0.008 n=5+5)
    SlowQueries/slow-query-2-24     1.24M ± 0%     1.14M ± 0%   -8.50%  (p=0.008 n=5+5)
    SlowQueries/slow-query-3-24      359k ± 0%      359k ± 0%   -0.12%  (p=0.008 n=5+5)
    ```
    
    Release note: None

--- pkg/sql/opt/lookupjoin/constraint_builder.go
+++ pkg/sql/opt/lookupjoin/constraint_builder.go
@@ -149,11 +149,11 @@ func (b *ConstraintBuilder) Build(
 	// eqFilterOrds calculated during Init would no longer be valid because the
 	// ordinals of the filters will have changed.
 	leftEq, rightEq, eqFilterOrds :=
-		memo.ExtractJoinEqualityColumns(b.leftCols, b.rightCols, onFilters)
+		memo.ExtractJoinEqualityColumnsWithFilterOrds(b.leftCols, b.rightCols, onFilters)
 	rightEqSet := rightEq.ToSet()
 
 	// Retrieve the inequality columns from onFilters.
-	_, rightCmp, inequalityFilterOrds := memo.ExtractJoinConditionColumns(
+	rightCmp, inequalityFilterOrds := memo.ExtractJoinConditionRightColumnsWithFilterOrds(
 		b.leftCols, b.rightCols, onFilters, true, /* inequality */
 	)
 
