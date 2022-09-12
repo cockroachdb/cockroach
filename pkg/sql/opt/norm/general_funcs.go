908679de4b4374b36baf18dc5083e266ba456a0c
Mark Sirek <sirek@cockroachlabs.com>
2022-09-12 00:22:27 -0700

    norm: do not fold floor division with unit denominator
    The floor division operator `//` is like division but drops the
    fractional portion of the result. Normalization rule FoldDivOne
    rewrites `(column // 1)` as `(column)` which is incorrect if the
    data in `column` has fractional digits, as those digits should be
    dropped.
    
    The solution is to only fold `(column // 1)` when `column` is one of
    the integer types.
    
    Release note (bug fix): This patch fixes incorrect results from
    the floor division operator, `//`, when the numerator is non-constant
    and the denominator is the constant 1.

--- pkg/sql/opt/norm/general_funcs.go
+++ pkg/sql/opt/norm/general_funcs.go
@@ -64,6 +64,12 @@ func (c *CustomFuncs) IsTimestampTZ(scalar opt.ScalarExpr) bool {
 	return scalar.DataType().Family() == types.TimestampTZFamily
 }
 
+// IsInt returns true if the given scalar expression is of one of the
+// integer types.
+func (c *CustomFuncs) IsInt(scalar opt.ScalarExpr) bool {
+	return scalar.DataType().Family() == types.IntFamily
+}
+
 // BoolType returns the boolean SQL type.
 func (c *CustomFuncs) BoolType() *types.T {
 	return types.Bool
