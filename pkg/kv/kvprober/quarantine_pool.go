c53370387adde9d49e1b39c06679aa8d96e2ede5
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-09-13 10:12:41 -0400

    fixup! kv: update kvprober with quarantine pool
    - use FunctionalGauge so that oldest time is reflected accurately
    - make quarantinePool actually pay attention to changes if size cluster setting
    - fix up some logging that would otherwise create noise at Error level

--- pkg/kv/kvprober/quarantine_pool.go
+++ pkg/kv/kvprober/quarantine_pool.go
@@ -24,59 +24,100 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/errors"
 )
 
 type quarantinePool struct {
-	steps        []Step
-	size         int64
-	entryTimeMap map[roachpb.RangeID]time.Time
+	size func() int64 // can change over time
+	mu   struct {
+		syncutil.Mutex
+		steps        []Step
+		size         int64
+		entryTimeMap map[roachpb.RangeID]time.Time
+	}
 }
 
 func newQuarantinePool(settings *cluster.Settings) *quarantinePool {
-	poolSize := quarantinePoolSize.Get(&settings.SV)
 	return &quarantinePool{
-		size:         poolSize,
-		entryTimeMap: make(map[roachpb.RangeID]time.Time),
-		steps:        make([]Step, poolSize),
+		size: func() int64 { return quarantinePoolSize.Get(&settings.SV) },
 	}
 }
 
-func (qp *quarantinePool) add(ctx context.Context, step Step) {
-	if int64(len(qp.steps)) >= qp.size-1 {
-		log.Health.Errorf(ctx, "cannot add range %s to quarantine pool, at capacity", step.RangeID.String())
-	} else {
-		qp.steps = append(qp.steps, step)
-		qp.entryTimeMap[step.RangeID] = timeutil.Now()
+func (qp *quarantinePool) maybeAdd(ctx context.Context, step Step) (added bool) {
+	qp.mu.Lock()
+	defer qp.mu.Unlock()
+	if _, ok := qp.mu.entryTimeMap[step.RangeID]; ok {
+		// Already in the pool.
+		return false
 	}
+
+	size := qp.size()
+
+	if int64(len(qp.mu.steps)) >= size {
+		// The pool is full. Note that we don't log, as we have a full pool of
+		// failing ranges, and it should thus be clear that the cluster is likely
+		// experiencing a widespread outage.
+		//
+		// Truncate slice in case size() got lowered.
+		qp.mu.steps = qp.mu.steps[:size]
+		return false
+	}
+	qp.mu.steps = append(qp.mu.steps, step)
+	if qp.mu.entryTimeMap == nil {
+		qp.mu.entryTimeMap = map[roachpb.RangeID]time.Time{}
+	}
+	qp.mu.entryTimeMap[step.RangeID] = timeutil.Now()
+	return true
 }
 
-func (qp *quarantinePool) remove(ctx context.Context, step Step) {
-	if len(qp.steps) < 1 {
-		log.Health.Errorf(ctx, "cannot remove range %s from quarantine pool, pool is empty", step.RangeID.String())
+func (qp *quarantinePool) oldestDuration() time.Duration {
+	qp.mu.Lock()
+	defer qp.mu.Unlock()
+
+	now := timeutil.Now()
+	var max time.Duration
+	for _, then := range qp.mu.entryTimeMap {
+		dur := now.Sub(then)
+		if dur > max {
+			max = dur
+		}
+	}
+	return max
+}
+
+func (qp *quarantinePool) maybeRemove(ctx context.Context, step Step) {
+	qp.mu.Lock()
+	defer qp.mu.Unlock()
+	if _, found := qp.mu.entryTimeMap[step.RangeID]; !found {
 		return
 	}
+	delete(qp.mu.entryTimeMap, step.RangeID)
 	idx := -1
-	for k, v := range qp.steps {
+	for k, v := range qp.mu.steps {
 		if v.RangeID == step.RangeID {
 			idx = k
 			break
 		}
 	}
 	if idx == -1 {
-		log.Health.Errorf(ctx, "cannot remove range %s from quarantine pool, not found", step.RangeID.String())
+		// This is a programming error. We had an entry in entryTimeMap, but can't
+		// find the corresponding step.
+		log.Health.Errorf(ctx, "inconsistent from quarantine pool: %s not found", step.RangeID)
 		return
 	}
-	// Expensive op if pool size is very large.
-	qp.steps = append(qp.steps[:idx], qp.steps[idx+1:]...)
-	delete(qp.entryTimeMap, step.RangeID)
+	qp.mu.steps = append(qp.mu.steps[:idx], qp.mu.steps[idx+1:]...)
 }
 
 func (qp *quarantinePool) next(ctx context.Context) (Step, error) {
-	if len(qp.steps) > 0 {
-		step := qp.steps[0]
-		return step, nil
+	qp.mu.Lock()
+	defer qp.mu.Unlock()
+
+	if len(qp.mu.steps) == 0 {
+		return Step{}, errors.New("quarantine pool is empty")
 	}
-	return Step{}, errors.New("there are no keys in quarantine")
+
+	step := qp.mu.steps[0]
+	return step, nil
 }
