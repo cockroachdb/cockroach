f1878cd8910c254eabcabad58ec4772a71d4fa2b
Yevgeniy Miretskiy <yevgeniy@cockroachlabs.com>
2022-09-15 11:42:01 -0400

    changefeedccl: Improve JSON encoder performance
    Rewrite JSON encoder to improve its performance.
    
    Prior to this change JSON encoder was very inefficient.
    This inefficiency had multiple underlying reasons:
      * New Go map objects were constructed for each event.
      * Underlying json conversion functions had inefficiencies
        (tracked in https://github.com/cockroachdb/cockroach/pull/87968)
      * Conversion of Go maps to JSON incurs the cost
        of sorting the keys -- for each row. Sorting,
        particularly when rows are wide, has significant cost.
      * Each conversion to JSON allocated new array builder
        (to encode keys) and new object builder; that too has cost.
      * Underlying code structure, while attempting to reuse
        code when constructing different "envelope" formats,
        cause the code to be more inefficient.
    
    This PR addresses all of the above.  In particular, since
    a schema version for the table is guaranteeed to have
    the same set of primary key and value columns, we can construct
    JSON builders once.  The expensive sort operation can be performed
    once per version; builders can be memoized and cached.
    
    The performance impact is significant:
      * Key encoding speed up is 5-30%, depending on the number of primary
        keys.
      * Value encoding 30% - 60% faster (slowest being "wrapped" envelope
        with diff -- which effectively encodes 2x values)
      * Byte allocations per row reduces by over 70%, with the number
        of allocations reduced similarly.
    
    Release note (enterprise change): Changefeed JSON encoder
    performance improved by 50%.
    Release justification: performance improvement

--- pkg/ccl/changefeedccl/sink_cloudstorage.go
+++ pkg/ccl/changefeedccl/sink_cloudstorage.go
@@ -408,7 +408,7 @@ func makeCloudStorageSink(
 			changefeedbase.OptEnvelope, encodingOpts.Envelope)
 	}
 
-	if !encodingOpts.KeyInValue {
+	if encodingOpts.Envelope != changefeedbase.OptEnvelopeBare && !encodingOpts.KeyInValue {
 		return nil, errors.Errorf(`this sink requires the WITH %s option`, changefeedbase.OptKeyInValue)
 	}
 
