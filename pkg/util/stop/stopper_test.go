2865f3f65b23c957691422966a2aa48b8bc5dd7e
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-09-19 15:29:20 +0200

    [dnm] unit test demonstrating weird promql p_max behavior
    See [^1].
    
    [^1]: https://cockroachlabs.slack.com/archives/C0228FLT53R/p1663594041343959?thread_ts=1663573770.592609&cid=C0228FLT53R
    
    Release note: None

--- pkg/util/stop/stopper_test.go
+++ pkg/util/stop/stopper_test.go
@@ -12,8 +12,13 @@ package stop_test
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"math"
+	"os"
 	"runtime"
+	"sort"
+	"strconv"
 	"sync"
 	"sync/atomic"
 	"testing"
@@ -767,4 +772,204 @@ func TestStopperRunAsyncTaskCreatesRootSpans(t *testing.T) {
 		},
 	))
 	require.NotNil(t, <-c)
+}
+
+func TestFoo(t *testing.T) {
+	r, err := os.Open("weird-pmax.json")
+	require.NoError(t, err)
+	defer r.Close()
+
+	dec := json.NewDecoder(r)
+	m := map[string]interface{}{}
+	require.NoError(t, dec.Decode(&m))
+	frames := m["response"].(map[string]interface{})["results"].(map[string]interface{})["A"].(map[string]interface{})["frames"].([]interface{})
+	var bs buckets
+	for _, frame := range frames {
+		fields := frame.(map[string]interface{})["schema"].(map[string]interface{})["fields"].([]interface{})
+		var leString string
+		for _, field := range fields {
+			f := field.(map[string]interface{})
+			if f["name"] != "Value" {
+				continue
+			}
+			leString = f["labels"].(map[string]interface{})["le"].(string)
+		}
+		require.NotZero(t, leString)
+		tup := frame.(map[string]interface{})["data"].(map[string]interface{})["values"].([]interface{})
+		ts, count := int64(tup[0].([]interface{})[0].(float64)), tup[1].([]interface{})[0].(float64)
+		le, err := strconv.ParseFloat(leString, 64)
+		require.NoError(t, err)
+		require.Equal(t, fmt.Sprint(le), leString) // ensure it round trips
+		_ = ts
+		bs = append(bs, bucket{
+			upperBound: le,
+			count:      count,
+		})
+	}
+	sort.Sort(bs)
+	if false {
+		// This helps a lot - brings the p100 to ~201ms which is the secondmost top bucket.
+		bs[len(bs)-1].count += 1.0
+	}
+	for i, b := range bs {
+		s := fmt.Sprint(time.Duration(b.upperBound))
+		if math.IsInf(b.upperBound, 1) {
+			s = fmt.Sprint(b.upperBound)
+		}
+		t.Logf("idx %d: <=%s: %f", i, s, b.count)
+	}
+	for _, q := range []float64{0.5, 0.9, 0.99, 0.999, 0.9999, 1.0} {
+		ns := time.Duration(bucketQuantile(t, q, bs))
+		t.Logf("p%.3f: %s", q*100, ns)
+	}
+}
+
+// bucketQuantile calculates the quantile 'q' based on the given buckets. The
+// buckets will be sorted by upperBound by this function (i.e. no sorting
+// needed before calling this function). The quantile value is interpolated
+// assuming a linear distribution within a bucket. However, if the quantile
+// falls into the highest bucket, the upper bound of the 2nd highest bucket is
+// returned. A natural lower bound of 0 is assumed if the upper bound of the
+// lowest bucket is greater 0. In that case, interpolation in the lowest bucket
+// happens linearly between 0 and the upper bound of the lowest bucket.
+// However, if the lowest bucket has an upper bound less or equal 0, this upper
+// bound is returned if the quantile falls into the lowest bucket.
+//
+// There are a number of special cases (once we have a way to report errors
+// happening during evaluations of AST functions, we should report those
+// explicitly):
+//
+// If 'buckets' has 0 observations, NaN is returned.
+//
+// If 'buckets' has fewer than 2 elements, NaN is returned.
+//
+// If the highest bucket is not +Inf, NaN is returned.
+//
+// If q==NaN, NaN is returned.
+//
+// If q<0, -Inf is returned.
+//
+// If q>1, +Inf is returned.
+func bucketQuantile(t *testing.T, q float64, buckets buckets) float64 {
+	if math.IsNaN(q) {
+		return math.NaN()
+	}
+	if q < 0 {
+		return math.Inf(-1)
+	}
+	if q > 1 {
+		return math.Inf(+1)
+	}
+	sort.Sort(buckets)
+	if !math.IsInf(buckets[len(buckets)-1].upperBound, +1) {
+		return math.NaN()
+	}
+
+	buckets = coalesceBuckets(buckets)
+	ensureMonotonic(buckets)
+
+	if len(buckets) < 2 {
+		return math.NaN()
+	}
+	observations := buckets[len(buckets)-1].count
+	if observations == 0 {
+		return math.NaN()
+	}
+	rank := q * observations
+	t.Logf("q=%f, observations=%f, rank=%f", q, observations, rank)
+	b := sort.Search(len(buckets)-1, func(i int) bool { return buckets[i].count >= rank })
+
+	if b == len(buckets)-1 {
+		return buckets[len(buckets)-2].upperBound
+	}
+	if b == 0 && buckets[0].upperBound <= 0 {
+		return buckets[0].upperBound
+	}
+	var (
+		bucketStart float64
+		bucketEnd   = buckets[b].upperBound
+		count       = buckets[b].count
+	)
+	if b > 0 {
+		bucketStart = buckets[b-1].upperBound
+		count -= buckets[b-1].count
+		rank -= buckets[b-1].count
+	}
+	return bucketStart + (bucketEnd-bucketStart)*(rank/count)
+}
+
+type bucket struct {
+	upperBound float64
+	count      float64
+}
+
+// buckets implements sort.Interface.
+type buckets []bucket
+
+func (b buckets) Len() int           { return len(b) }
+func (b buckets) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
+func (b buckets) Less(i, j int) bool { return b[i].upperBound < b[j].upperBound }
+
+// coalesceBuckets merges buckets with the same upper bound.
+//
+// The input buckets must be sorted.
+func coalesceBuckets(buckets buckets) buckets {
+	last := buckets[0]
+	i := 0
+	for _, b := range buckets[1:] {
+		if b.upperBound == last.upperBound {
+			last.count += b.count
+		} else {
+			buckets[i] = last
+			last = b
+			i++
+		}
+	}
+	buckets[i] = last
+	return buckets[:i+1]
+}
+
+// The assumption that bucket counts increase monotonically with increasing
+// upperBound may be violated during:
+//
+//   * Recording rule evaluation of histogram_quantile, especially when rate()
+//      has been applied to the underlying bucket timeseries.
+//   * Evaluation of histogram_quantile computed over federated bucket
+//      timeseries, especially when rate() has been applied.
+//
+// This is because scraped data is not made available to rule evaluation or
+// federation atomically, so some buckets are computed with data from the
+// most recent scrapes, but the other buckets are missing data from the most
+// recent scrape.
+//
+// Monotonicity is usually guaranteed because if a bucket with upper bound
+// u1 has count c1, then any bucket with a higher upper bound u > u1 must
+// have counted all c1 observations and perhaps more, so that c  >= c1.
+//
+// Randomly interspersed partial sampling breaks that guarantee, and rate()
+// exacerbates it. Specifically, suppose bucket le=1000 has a count of 10 from
+// 4 samples but the bucket with le=2000 has a count of 7 from 3 samples. The
+// monotonicity is broken. It is exacerbated by rate() because under normal
+// operation, cumulative counting of buckets will cause the bucket counts to
+// diverge such that small differences from missing samples are not a problem.
+// rate() removes this divergence.)
+//
+// bucketQuantile depends on that monotonicity to do a binary search for the
+// bucket with the φ-quantile count, so breaking the monotonicity
+// guarantee causes bucketQuantile() to return undefined (nonsense) results.
+//
+// As a somewhat hacky solution until ingestion is atomic per scrape, we
+// calculate the "envelope" of the histogram buckets, essentially removing
+// any decreases in the count between successive buckets.
+
+func ensureMonotonic(buckets buckets) {
+	max := buckets[0].count
+	for i := 1; i < len(buckets); i++ {
+		switch {
+		case buckets[i].count > max:
+			max = buckets[i].count
+		case buckets[i].count < max:
+			buckets[i].count = max
+		}
+	}
 }
