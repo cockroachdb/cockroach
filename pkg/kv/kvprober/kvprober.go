cd4e0598d583522c802df12cd4bf407787f99951
Santamaura <santamaura@cockroachlabs.com>
2022-09-19 17:29:49 -0400

    kv: update kvprober with quarantine pool
    addressing comments
    
    Release note: None

--- pkg/kv/kvprober/kvprober.go
+++ pkg/kv/kvprober/kvprober.go
@@ -20,6 +20,7 @@ package kvprober
 import (
 	"context"
 	"math/rand"
+	"reflect"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/kv"
@@ -296,6 +297,9 @@ func (p *Prober) Start(ctx context.Context, stopper *stop.Stopper) error {
 	if err := startLoop(ctx, "write probe loop", p.writeProbe, p.writePlanner, writeInterval); err != nil {
 		return err
 	}
+	// The purpose of the quarantine pool is to detect outages affecting a small number
+	// of ranges but at a high confidence. The quarantine pool does this by repeatedly
+	// probing ranges that are in the pool.
 	return startLoop(ctx, "quarantine write probe loop", p.quarantineProbe, p.quarantineWritePool, quarantineWriteInterval)
 }
 
@@ -372,6 +376,11 @@ func (p *Prober) writeProbeImpl(ctx context.Context, ops proberOpsI, txns prober
 	p.metrics.ProbePlanAttempts.Inc(1)
 
 	step, err := pl.next(ctx)
+	// In the case where the quarantine pool is empty don't record a planning failure since
+	// it isn't an actua; plan failure.
+	if reflect.DeepEqual(step, Step{}) && err == nil {
+		return
+	}
 	if err != nil {
 		log.Health.Errorf(ctx, "can't make a plan: %v", err)
 		p.metrics.ProbePlanFailures.Inc(1)
