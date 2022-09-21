b05415f058d7321874defff7a24eaa417cb46bc5
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2022-09-21 19:24:10 -0400

    ui: add connected components for insights
    This commit adds connected components for the workload and schema insights pages, for use
    in the CC Console.
    
    Fixes #87693.
    
    Release note: None

---
+++ pkg/ui/workspaces/cluster-ui/src/store/insights/transactionInsights/transactionInsights.sagas.ts
@@ -0,0 +1,53 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import { all, call, delay, put, takeLatest } from "redux-saga/effects";
+
+import { actions } from "./transactionInsights.reducer";
+import { getTransactionInsightEventState } from "src/api/insightsApi";
+import { throttleWithReset } from "src/store/utils";
+import { rootActions } from "src/store/reducers";
+
+export function* refreshTransactionInsightsSaga() {
+  yield put(actions.request());
+}
+
+export function* requestTransactionInsightsSaga(): any {
+  try {
+    const result = yield call(getTransactionInsightEventState);
+    yield put(actions.received(result));
+  } catch (e) {
+    yield put(actions.failed(e));
+  }
+}
+
+export function* receivedTransactionInsightsSaga(delayMs: number) {
+  yield delay(delayMs);
+  yield put(actions.invalidated());
+}
+
+export function* transactionInsightsSaga(
+  cacheInvalidationPeriod: number = 10 * 1000,
+) {
+  yield all([
+    throttleWithReset(
+      cacheInvalidationPeriod,
+      actions.refresh,
+      [actions.invalidated, rootActions.resetState],
+      refreshTransactionInsightsSaga,
+    ),
+    takeLatest(actions.request, requestTransactionInsightsSaga),
+    takeLatest(
+      actions.received,
+      receivedTransactionInsightsSaga,
+      cacheInvalidationPeriod,
+    ),
+  ]);
+}
