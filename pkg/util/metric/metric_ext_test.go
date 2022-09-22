f6c7b9a6835722b8f58310dda36607a8e096589a
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-09-22 12:16:44 +0200

    metric: front-port a regression test
    Front-ports some testing improvements from #88331.
    
    Note that 22.2 and master don't exhibit the bug fixed in that
    PR since we switched to using prometheus histograms in #86671.
    
    Release note: None

---
+++ pkg/util/metric/metric_ext_test.go
@@ -0,0 +1,36 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package metric_test
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/echotest"
+	"github.com/cockroachdb/cockroach/pkg/util/metric"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHistogramPrometheus(t *testing.T) {
+	// Regression test against https://github.com/cockroachdb/cockroach/pull/88331.
+	// The output includes buckets for which the upper bound equals the previous
+	// bucket's upper bound.
+	h := metric.NewHistogram(metric.Metadata{}, time.Second, []float64{1, 2, 3, 4, 5, 6, 10, 20, 30})
+	h.RecordValue(1)
+	h.RecordValue(5)
+	h.RecordValue(5)
+	h.RecordValue(10)
+	act, err := json.MarshalIndent(*h.ToPrometheusMetric().Histogram, "", "  ")
+	require.NoError(t, err)
+	echotest.Require(t, string(act), testutils.TestDataPath(t, "histogram.txt"))
+}
