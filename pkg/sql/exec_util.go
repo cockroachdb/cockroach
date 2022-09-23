cd35024e3a40cbd3445dbb9a2435e7d3ce47557b
Michael Butler <butler@cockroachlabs.com>
2022-09-23 10:20:09 -0400

    backupccl: reintroduce previously offline tables with manifest.DescriptorChanges
    getReintroducedSpans finds all tables included in the current and previous
    backup that may have undergone a non-mvcc operation. The current backup will
    then back up these tables' spans from ts = 0, as the previous backup may have
    missed certain non-mvcc written.
    
    To find these tables, getReintroducedSpans must find all tables covered in the
    previous backup that were in the offline state at previous backup start time.
    
    This function assumed that the previous backup's
    manifest.Descriptors field would contain all tables covered in the previous
    backup; however, while investigating #88042, we discovered that this assumption
    is not correct. During revision history backups, a table with an in-progress
    import (e.g. offline at backup time) can get backed up and included in
    manifest.DescriptorChanges but not in manifest.Descriptors. This implies that
    getReintroducedSpans missed reintroducing spans from this table, implying that
    backup chains have missed backing up some data.
    
    This patch fixes getReintroducedSpans to ensure it reintroduces tables included
    in manifest.DescriptorChanges whose last revision brought the table offline. In
    addition, this patch adds a check to restore planning which causes the restore
    to fail immedidiately if any restore target is missing an introduced span.
    
    Release note(bug fix): fix apart of Tech Advisory
    https://cockroachlabs.atlassian.net/browse/TSE-198
    
    Release justification: bug fix

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -1381,6 +1381,11 @@ type BackupRestoreTestingKnobs struct {
 	// testing. This is typically the bulk mem monitor if not
 	// specified here.
 	BackupMemMonitor *mon.BytesMonitor
+
+	// SkipDescriptorChangeIntroduction skips checking the
+	// backupManifest.DescriptorChanges when deciding which spans to reintroduce.
+	// This helps simulate the bug described in 88042.
+	SkipDescriptorChangeIntroduction bool
 }
 
 var _ base.ModuleTestingKnobs = &BackupRestoreTestingKnobs{}
@@ -2035,8 +2040,10 @@ func (st *SessionTracing) getRecording() []tracingpb.RecordedSpan {
 //
 // Args:
 // kvTracingEnabled: If set, the traces will also include "KV trace" messages -
-//   verbose messages around the interaction of SQL with KV. Some of the messages
-//   are per-row.
+//
+//	verbose messages around the interaction of SQL with KV. Some of the messages
+//	are per-row.
+//
 // showResults: If set, result rows are reported in the trace.
 func (st *SessionTracing) StartTracing(
 	recType tracing.RecordingType, kvTracingEnabled, showResults bool,
@@ -2257,11 +2264,11 @@ type traceRow [traceNumCols]tree.Datum
 
 // A regular expression to split log messages.
 // It has three parts:
-// - the (optional) code location, with at least one forward slash and a period
-//   in the file name:
-//   ((?:[^][ :]+/[^][ :]+\.[^][ :]+:[0-9]+)?)
-// - the (optional) tag: ((?:\[(?:[^][]|\[[^]]*\])*\])?)
-// - the message itself: the rest.
+//   - the (optional) code location, with at least one forward slash and a period
+//     in the file name:
+//     ((?:[^][ :]+/[^][ :]+\.[^][ :]+:[0-9]+)?)
+//   - the (optional) tag: ((?:\[(?:[^][]|\[[^]]*\])*\])?)
+//   - the message itself: the rest.
 var logMessageRE = regexp.MustCompile(
 	`(?s:^((?:[^][ :]+/[^][ :]+\.[^][ :]+:[0-9]+)?) *((?:\[(?:[^][]|\[[^]]*\])*\])?) *(.*))`)
 
