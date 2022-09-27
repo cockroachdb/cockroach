25fc90939623d6958a16e97b9325aae5d585f778
Kyle Patron <kyle@cockroachlabs.com>
2022-09-27 15:36:13 -0400

    ccl/jwtauthccl: add user mapping to JWT auth
    Previously, JWT auth wasn't optimized for third party token
    issuers. In order to better support customers running in
    public clouds, we decided we needed to better support this.
    
    To accomplish this there are 3 changes that are being made:
    * restrict JWT auth so it can't be used to login with root
    * allow multiple audiences for a cluster (since customers
    may not be able to control the audiences field for third
    party issued tokens)
    * allow customers to map the subjects of tokens to valid SQL
    users (since customers may not be able to control the subject
    field for third party issued tokens)
    
    The third part of this is the most involved.
    An example of why this is needed. If you wanted to use a JWT
    issued for a GCP service account, you previously couldn't
    because the subject is a user id like 113794436627259432046.
    Since that is not a valid sql username, we need to do some
    kind of mapping.
    
    We decided to reuse the existing identity map for this purpose.
    When a user logs in with a JWT, the cluster checks if there is
    a ident map with the map name equal to the issuer field of the
    token. If there is, it uses the map to translate the subject to
    a user.
    
    As an example, we can set the identity_map.configuration to:
    ```
    https://accounts.google.com    113794436627259432046    kyle
    https://accounts.google.com    /^([9-0]*)$              g_\1
    email_example                  /^([^@]+)@.*$            email_\1
    ```
    Then I can log in to the kyle or g_113794436627259432046 user
    using the following invocations (113794436627259432046  is the
    user id for kpatron-testing@kpatron-sandbox.iam.gserviceaccount.com):
    ```
    psql "port=26257 host=localhost user=kyle
    password=`gcloud auth print-identity-token
    --impersonate-service-account=
    "kpatron-testing@kpatron-sandbox.iam.gserviceaccount.com"
    --audiences="test_cluster"` options=--crdb:jwt_auth_enabled=true"
    ```
    ```
    psql "port=26257 host=localhost user=g_113794436627259432046
    password=`gcloud auth print-identity-token
    --impersonate-service-account=
    "kpatron-testing@kpatron-sandbox.iam.gserviceaccount.com"
    --audiences="test_cluster"` options=--crdb:jwt_auth_enabled=true"
    ```
    
    Release note (enterprise change): User mapping, and support for
    multiple audiences has been added to JWT auth in order to better
    integrate with third party JWT issuers.

--- pkg/sql/pgwire/identmap/ident_map.go
+++ pkg/sql/pgwire/identmap/ident_map.go
@@ -128,6 +128,14 @@ func (c *Conf) Empty() bool {
 	return c.data == nil || len(c.data) == 0
 }
 
+// HasMap returns true if there are entries for the given map name.
+func (c *Conf) HasMap(mapName string) bool {
+	if c.data == nil {
+		return false
+	}
+	return c.data[mapName] != nil
+}
+
 // Map returns the database usernames that a system identity maps to
 // within the named mapping. If there are no matching usernames, or if
 // mapName is unknown, nil will be returned. The returned list will be
