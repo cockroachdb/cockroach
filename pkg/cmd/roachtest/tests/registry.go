f6a1bed31261b5887ceb6575eab036967e500854
Oleg Afanasyev <oleg@cockroachlabs.com>
2022-09-27 10:50:44 +0100

    roachtest: roachtest testing gc functionaltiy
    Previously there were no roachtest that verified mvcc GC functionality.
    This commit adds a GC roachtest with an emphasis on range tombstone
    processing. This is a new area and involves feature flags and optional
    handling. Integration tests will ensure component wiring works as
    expected.
    
    Release note: None

--- pkg/cmd/roachtest/tests/registry.go
+++ pkg/cmd/roachtest/tests/registry.go
@@ -77,6 +77,7 @@ func RegisterTests(r registry.Registry) {
 	registerLibPQ(r)
 	registerLiquibase(r)
 	registerLoadSplits(r)
+	registerMVCCGC(r)
 	registerMultiTenantFairness(r)
 	registerMultiTenantTPCH(r)
 	registerMultiTenantUpgrade(r)
