2b1f72932f492cabf09b367b06d66d9417fef05d
Leon Fattakhov <leon.fattakhov@cockroachlabs.com>
2022-09-28 13:37:14 -0400

    metrics: expose pebble flush utilization
    Create a new `GaugeFloat64` metric for pebble’s flush utilization. This
    metric is not cumulative, rather, it is the metric over an interval.
    This interval is determined by the `interval` parameter of the
    `Node.startComputePeriodicMetrics` method.
    
    In order to compute the metric over an interval the previous value of
    the metric must be stored. As a result, a map is constructed that takes
    a pointer to a store and maps it to a pointer to storage metrics:
    `make(map[*kvserver.Store]*storage.Metrics)`. This map is passed to
    `node.computeMetricsPeriodically` which gets the store to calculate its
    metrics and then updates the previous metrics in the map.
    
    Refactor `store.go`'s metric calculation by separating
    `ComputeMetrics(ctx context.Context, tick int) error` into two methods:
    
    * `ComputeMetrics(ctx context.Context) error`
    * `ComputeMetricsPeriodically(ctx context.Context, prevMetrics
      *storage.Metrics, tick int) (m storage.Metrics, err error)`
    
    Both methods call the `computeMetrics` which contains the common code
    between the two calls. Before this, the process for retrieving metrics
    instantaneous was to pass a tick value such as `-1` or `0` to the
    `ComputeMetrics(ctx context.Context, tick int)` however it can be
    done with a call to `ComputeMetrics(ctx context.Context)`
    
    The `store.ComputeMetricsPeriodically` method will also return the
    latest storage metrics. These metrics are used to update the mapping
    between stores and metrics used for computing the metric delta over an
    interval.
    
    Release note: None

--- pkg/testutils/testcluster/testcluster.go
+++ pkg/testutils/testcluster/testcluster.go
@@ -1380,7 +1380,7 @@ func (tc *TestCluster) WaitForFullReplication() error {
 				if err := s.ForceReplicationScanAndProcess(); err != nil {
 					return err
 				}
-				if err := s.ComputeMetrics(context.TODO(), 0); err != nil {
+				if err := s.ComputeMetrics(context.TODO()); err != nil {
 					// This can sometimes fail since ComputeMetrics calls
 					// updateReplicationGauges which needs the system config gossiped.
 					log.Infof(context.TODO(), "%v", err)
