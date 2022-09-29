f1c335a17cdd70563963a8dee57be009a042e355
Alex Sarkesian <sarkesian@cockroachlabs.com>
2022-09-29 19:06:00 -0400

    kvserver: decrease verbosity of replicate queue trace logging
    While logging of traces on replicate queue errors was recently added,
    logging these traces at the `WARNING` level appears to be too high,
    causing noisy logs. This change decreases the verbosity of these logs to
    the `INFO` level.
    
    Release note: None

--- pkg/kv/kvserver/replicate_queue.go
+++ pkg/kv/kvserver/replicate_queue.go
@@ -814,7 +814,7 @@ func (rq *replicateQueue) processOneChangeWithTracing(
 		}
 
 		if err != nil {
-			log.KvDistribution.Warningf(ctx, "error processing replica: %v%s", err, traceOutput)
+			log.KvDistribution.Infof(ctx, "error processing replica: %v%s", err, traceOutput)
 		} else if exceededDuration {
 			log.KvDistribution.Infof(ctx, "processing replica took %s, exceeding threshold of %s%s",
 				processDuration, loggingThreshold, traceOutput)
