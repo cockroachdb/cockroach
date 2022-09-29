37df9a3894bda58c405dce3baf98ff4883ee1867
Erik Grinaker <grinaker@cockroachlabs.com>
2022-09-29 19:39:51 +0000

    sql/logictest: add metamorphic param for small engine blocks
    This patch adds a metamorphic param `logictest-small-engine-blocks`
    which configures Pebble with a block size of 1 byte. This places each
    key in a separate block, and serves to provoke bugs in time-bound
    iterators.
    
    Release note: None

--- pkg/sql/logictest/logic.go
+++ pkg/sql/logictest/logic.go
@@ -551,6 +551,11 @@ var (
 	useMVCCRangeTombstonesForPointDeletes = util.ConstantWithMetamorphicTestBool(
 		"logictest-use-mvcc-range-tombstones-for-point-deletes", false)
 
+	// smallEngineBlocks will configure Pebble with very small blocks of 1 byte,
+	// to provoke bugs in time-bound iterators.
+	smallEngineBlocks = util.ConstantWithMetamorphicTestBool(
+		"logictest-small-engine-blocks", false)
+
 	// BackupRestoreProbability is the environment variable for `3node-backup` config.
 	backupRestoreProbability = envutil.EnvOrDefaultFloat64("COCKROACH_LOGIC_TEST_BACKUP_RESTORE_PROBABILITY", 0.0)
 )
@@ -1243,6 +1248,7 @@ func (t *logicTest) newCluster(
 						UseRangeTombstonesForPointDeletes: supportsMVCCRangeTombstones &&
 							useMVCCRangeTombstonesForPointDeletes,
 					},
+					SmallEngineBlocks: smallEngineBlocks,
 				},
 				SQLEvalContext: &eval.TestingKnobs{
 					AssertBinaryExprReturnTypes:     true,
