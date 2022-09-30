a1522263b0f7db38597ab825c4f6f4d6fb3ae1e0
Yevgeniy Miretskiy <yevgeniy@cockroachlabs.com>
2022-09-30 17:19:19 -0400

    sql: Make `tree.ParseJSON` faster
    Switch `tree.ParseJSON` to use `json-iterator` library to convert input
    string directly into `tree.JSON`, without extra conversion to go native
    type.
    
    The change is a drop in replacement for standard `encoding/json`
    library.  Allmost all metrics (speed per op, throughput, allocations/op)
    are improved significantly.  The new implementation tends to allocate
    more when encoding very large strings (256KB or larger).
    
    This change should help for any insert query that converts strings to
    JSON -- including, large scale imports of JSON data.
    
    Release note (enterprise change): Signficantly speed up conversion of
    strings to JSONb.

--- pkg/util/json/random.go
+++ pkg/util/json/random.go
@@ -29,18 +29,67 @@ var staticStrings = []string{
 	"foobar",
 }
 
-// Random generates a random JSON value.
-func Random(complexity int, rng *rand.Rand) (JSON, error) {
-	return MakeJSON(doRandomJSON(complexity, rng))
+type randConfig struct {
+	strLen     int
+	complexity int
 }
 
-func randomJSONString(rng *rand.Rand) interface{} {
-	if rng.Intn(2) == 0 {
+// RandOption is an option to control generation of random json.
+type RandOption interface {
+	apply(*randConfig)
+}
+
+type funcOpt func(cfg *randConfig)
+
+func (fn funcOpt) apply(cfg *randConfig) {
+	fn(cfg)
+}
+
+// WithMaxStrLen returns an option to set maximum length of random JSON string objects.
+func WithMaxStrLen(l int) RandOption {
+	return funcOpt(func(cfg *randConfig) {
+		cfg.strLen = l
+	})
+}
+
+// WithComplexity returns an option to set maximum complexity of JSON objection.
+func WithComplexity(c int) RandOption {
+	return funcOpt(func(cfg *randConfig) {
+		cfg.complexity = c
+	})
+}
+
+func defaultRandConfig(complexity int) randConfig {
+	return randConfig{
+		strLen:     defaultRandStrLen,
+		complexity: complexity,
+	}
+}
+
+// RandGen generates a random JSON value configured with specified options.
+func RandGen(rng *rand.Rand, opts ...RandOption) (JSON, error) {
+	cfg := defaultRandConfig(20)
+	for _, opt := range opts {
+		opt.apply(&cfg)
+	}
+	return MakeJSON(doRandomJSON(rng, &cfg))
+}
+
+// Random generates a random JSON value.
+func Random(complexity int, rng *rand.Rand) (JSON, error) {
+	cfg := defaultRandConfig(complexity)
+	return MakeJSON(doRandomJSON(rng, &cfg))
+}
+
+const defaultRandStrLen = 10
+
+func randomJSONString(rng *rand.Rand, maxLen int) interface{} {
+	if maxLen <= defaultRandStrLen && rng.Intn(2) == 0 {
 		return staticStrings[rng.Intn(len(staticStrings))]
 	}
 	result := make([]byte, 0)
-	l := rng.Intn(10) + 3
-	for i := 0; i < l; i++ {
+	l := rng.Intn(maxLen) + 3
+	for i := 0; i < l+maxLen/4 && i < maxLen; i++ { // Bias to generate larger strings.
 		result = append(result, byte(rng.Intn(0x7f-0x20)+0x20))
 	}
 	return string(result)
@@ -50,11 +99,11 @@ func randomJSONNumber(rng *rand.Rand) interface{} {
 	return json.Number(fmt.Sprintf("%v", rng.ExpFloat64()))
 }
 
-func doRandomJSON(complexity int, rng *rand.Rand) interface{} {
-	if complexity <= 0 || rng.Intn(10) == 0 {
+func doRandomJSON(rng *rand.Rand, cfg *randConfig) interface{} {
+	if cfg.complexity <= 0 || rng.Intn(cfg.complexity) == 0 {
 		switch rng.Intn(5) {
 		case 0:
-			return randomJSONString(rng)
+			return randomJSONString(rng, cfg.strLen)
 		case 1:
 			return randomJSONNumber(rng)
 		case 2:
@@ -65,26 +114,26 @@ func doRandomJSON(complexity int, rng *rand.Rand) interface{} {
 			return nil
 		}
 	}
-	complexity--
+	cfg.complexity--
 	switch rng.Intn(3) {
 	case 0:
 		result := make([]interface{}, 0)
-		for complexity > 0 {
-			amount := 1 + rng.Intn(complexity)
-			complexity -= amount
-			result = append(result, doRandomJSON(amount, rng))
+		for cfg.complexity > 0 {
+			amount := 1 + rng.Intn(cfg.complexity)
+			cfg.complexity -= amount
+			result = append(result, doRandomJSON(rng, cfg))
 		}
 		return result
 	case 1:
 		result := make(map[string]interface{})
-		for complexity > 0 {
-			amount := 1 + rng.Intn(complexity)
-			complexity -= amount
-			result[randomJSONString(rng).(string)] = doRandomJSON(amount, rng)
+		for cfg.complexity > 0 {
+			amount := 1 + rng.Intn(cfg.complexity)
+			cfg.complexity -= amount
+			result[randomJSONString(rng, defaultRandStrLen).(string)] = doRandomJSON(rng, cfg)
 		}
 		return result
 	default:
-		j, _ := Random(complexity, rng)
+		j, _ := Random(cfg.complexity, rng)
 		encoding, _ := EncodeJSON(nil, j)
 		encoded, _ := newEncodedFromRoot(encoding)
 		return encoded
