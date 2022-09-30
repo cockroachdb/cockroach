266038249672fe19d823b9a769507f7ae4de354a
Erik Grinaker <grinaker@cockroachlabs.com>
2022-09-30 17:11:55 +0000

    kvnemesis: add support for `AddSSTable`
    This patch adds support for `AddSSTable` operations in kvnemesis. It
    generates SSTs of 1-16 point key/value pairs, using options that make
    the ingestion MVCC-compliant (`SSTTimestampToRequestTimestamp` and
    `DisallowConflicts`).
    
    It does not yet test ingestion of MVCC range tombstones, since this is
    not yet used in production code, and to avoid blocking this PR on any
    range key-related issues. A later PR will address this.
    
    `AddSSTable` requests cannot span multiple ranges, so the generator
    optimistically builds SSTs based on the currently known splits. This may
    race with concurrent splits causing the request to fail, but the
    majority of requests will succeed.
    
    Release note: None

--- pkg/kv/kvnemesis/operations_test.go
+++ pkg/kv/kvnemesis/operations_test.go
@@ -11,14 +11,21 @@
 package kvnemesis
 
 import (
+	"context"
 	"fmt"
 	"strings"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/storage"
+	"github.com/cockroachdb/cockroach/pkg/storage/enginepb"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/echotest"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/stretchr/testify/require"
 )
 
 var (
@@ -38,6 +45,27 @@ var (
 func TestOperationsFormat(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
+	ctx := context.Background()
+
+	var sstValueHeader enginepb.MVCCValueHeader
+	sstValueHeader.KVNemesisSeq.Set(1)
+	sstSpan := roachpb.Span{Key: roachpb.Key(k1), EndKey: roachpb.Key(k4)}
+	sstTS := hlc.Timestamp{WallTime: 1}
+	sstFile := &storage.MemFile{}
+	{
+		st := cluster.MakeTestingClusterSettings()
+		w := storage.MakeIngestionSSTWriter(ctx, st, sstFile)
+		defer w.Close()
+
+		require.NoError(t, w.PutMVCC(storage.MVCCKey{Key: roachpb.Key(k1), Timestamp: sstTS},
+			storage.MVCCValue{MVCCValueHeader: sstValueHeader, Value: roachpb.MakeValueFromString("v1")}))
+		require.NoError(t, w.PutMVCC(storage.MVCCKey{Key: roachpb.Key(k2), Timestamp: sstTS},
+			storage.MVCCValue{MVCCValueHeader: sstValueHeader}))
+		require.NoError(t, w.PutMVCCRangeKey(
+			storage.MVCCRangeKey{StartKey: roachpb.Key(k3), EndKey: roachpb.Key(k4), Timestamp: sstTS},
+			storage.MVCCValue{MVCCValueHeader: sstValueHeader}))
+		require.NoError(t, w.Finish())
+	}
 
 	tests := []struct {
 		step Step
@@ -52,6 +80,9 @@ func TestOperationsFormat(t *testing.T) {
 					delRange(k10, k11, 2),
 					put(k11, 3),
 				)),
+		},
+		{
+			step: step(addSSTable(sstFile.Data(), sstSpan, sstTS, sstValueHeader.KVNemesisSeq.Get(), true)),
 		},
 	}
 
