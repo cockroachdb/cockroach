b288e88dbc3288a47b70c6b7885f48b4d5753310
Andrii Vorobiov <and.vorobiov@gmail.com>
2022-10-05 20:16:22 +0300

    server, tenant: hot ranges structured logging for all cluster types
    This patch introduces periodic logging of hot ranges info (ie QPS and
    relevant DB objects info). It is done for both dedicated and
    serverless cluster types, and by default called every 4h.
    Stats collection can be enabled/disabled, or changed interval
    through cluster settings.
    
    Release note: None

---
+++ pkg/server/structlogging/hot_ranges_log_test.go
@@ -0,0 +1,102 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package structlogging_test
+
+import (
+	"context"
+	"math"
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/ccl"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/server/serverpb"
+	"github.com/cockroachdb/cockroach/pkg/server/structlogging"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/log/logcrash"
+	"github.com/cockroachdb/cockroach/pkg/util/log/logtestutils"
+	"github.com/cockroachdb/errors"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHotRangesStats(t *testing.T) {
+	ctx := context.Background()
+	defer leaktest.AfterTest(t)()
+	ccl.TestingEnableEnterprise()
+	defer ccl.TestingDisableEnterprise()
+	sc := log.ScopeWithoutShowLogs(t)
+	defer sc.Close(t)
+
+	cleanup := logtestutils.InstallTelemetryLogFileSink(sc, t)
+	defer cleanup()
+
+	s, _, _ := serverutils.StartServer(t, base.TestServerArgs{
+		StoreSpecs: []base.StoreSpec{
+			base.DefaultTestStoreSpec,
+			base.DefaultTestStoreSpec,
+			base.DefaultTestStoreSpec,
+		},
+		Knobs: base.TestingKnobs{
+			Store: &kvserver.StoreTestingKnobs{
+				DisableReplicaRebalancing: true,
+			},
+		},
+	})
+	defer s.Stopper().Stop(ctx)
+
+	logcrash.DiagnosticsReportingEnabled.Override(ctx, &s.ClusterSettings().SV, true)
+	structlogging.TelemetryHotRangesStatsEnabled.Override(ctx, &s.ClusterSettings().SV, true)
+	structlogging.TelemetryHotRangesStatsInterval.Override(ctx, &s.ClusterSettings().SV, 500*time.Millisecond)
+	structlogging.TelemetryHotRangesStatsLoggingDelay.Override(ctx, &s.ClusterSettings().SV, 10*time.Millisecond)
+
+	tenantID := roachpb.MustMakeTenantID(2)
+	tt, err := s.StartTenant(ctx, base.TestTenantArgs{
+		TenantID: tenantID,
+		Settings: s.ClusterSettings(),
+	})
+	require.NoError(t, err)
+
+	testutils.SucceedsSoon(t, func() error {
+		ss := tt.TenantStatusServer().(serverpb.TenantStatusServer)
+		resp, err := ss.HotRangesV2(ctx, &serverpb.HotRangesRequest{TenantID: tenantID.String()})
+		if err != nil {
+			return err
+		}
+		if len(resp.Ranges) == 0 {
+			return errors.New("waiting for hot ranges to be collected")
+		}
+		return nil
+	})
+
+	testutils.SucceedsWithin(t, func() error {
+		log.Flush()
+		entries, err := log.FetchEntriesFromFiles(
+			0,
+			math.MaxInt64,
+			10000,
+			regexp.MustCompile(`"EventType":"hot_ranges_stats"`),
+			log.WithMarkedSensitiveData,
+		)
+		if err != nil {
+			return err
+		}
+		if len(entries) == 0 {
+			return errors.New("waiting for logs")
+		}
+		return nil
+	}, 5*time.Second)
+}
