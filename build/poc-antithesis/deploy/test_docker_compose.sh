20686bf7dc0ec148a6976862902fd132ce633a65
Stan Rosenberg <stan@cockroachlabs.com>
2022-10-06 18:18:29 +0000

    ci: refactor build/deploy for Antithesis POC
    Previously, poc-antithesis branch had to be used in CI
    in order to build and deploy docker images and
    docker-compose configuration for running automated
    tests inside Antithesis' testbed.
    
    This commit moves all the boilerplate under build/poc-antithesis.
    It also adds patches for Dockerfiles used to build both
    instrumented and uninstrumented images. The README is now
    the source of truth for performing both manual and CI
    builds and deploys.
    
    Release note: None

--- build/poc-antithesis/deploy/test_docker_compose.sh
+++ build/poc-antithesis/deploy/test_docker_compose.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-export docker_registry=gcr.io/cockroach-testeng-infra/
-export build_tag="poc-antithesis-latest"
-
 docker-compose build --no-cache
 docker-compose up -d
 
 echo "Initializing cluster..."
 docker exec roach1 ./cockroach init --cluster-name=test --insecure
-
-./run_bank_workload.sh
