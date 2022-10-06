f7c9b72c22b3e873591eec8bcf3aa08c44c6cb7e
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-10-06 12:51:01 +0200

    echotest: add Walk
    `echotest` is often a good intermediate solution between table-driven
    and datadriven testing, when full datadriven testing may be
    counter-productive.
    
    However, `echotest.Require` is too centered on tests that make only a
    single (or handful) of assertions. For table-driven tests with lots of
    cases more automation is helpful. This is now provided by `Walk`.
    
    It would likely be useful to upstream that, but let's try it out here
    first.
    
    Release note: None

---
+++ pkg/testutils/echotest/echotest_test.go
@@ -0,0 +1,40 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+//
+
+package echotest
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
+)
+
+func TestWalk(t *testing.T) {
+	skip.IgnoreLint(t)
+	w := Walk(t, testutils.TestDataPath(t))
+	defer w.Check(t)
+
+	// NB: a desirable property here is that in Goland you can click on the
+	// subtest and it'll take you to the entry in the slice. This requires
+	// that `t.Run` is invoked directly with the test name (this is a trial
+	// and error finding).
+	for _, test := range []struct{ name string }{
+		{name: "foo"},
+		{name: "bar"},
+	} {
+		t.Run(test.name, w.Do(t, test.name, func(t *testing.T, path string) {
+			Require(t, fmt.Sprintf("hello, %s", test.name), path)
+		}))
+	}
+
+}
