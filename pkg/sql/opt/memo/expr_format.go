c004da8f499bb32d6e7fa1671c8dfc2b1cac0985
Marcus Gartner <marcus@cockroachlabs.com>
2022-10-06 22:15:31 +0000

    opt: do not plan insert fast path if expression contains UDF
    In general, it is not safe to perform the INSERT fast path if the VALUES
    clause invokes a UDF. The INSERT fast path always performs the FK checks
    before the INSERT. A UDF might mutate other tables (although we don't
    currently support this), so the FK checks must be performed after the
    INSERT is complete.
    
    Release note: None

--- pkg/sql/opt/memo/expr_format.go
+++ pkg/sql/opt/memo/expr_format.go
@@ -1110,6 +1110,9 @@ func (f *ExprFmtCtx) scalarPropsStrings(scalar opt.ScalarExpr) []string {
 			} else if scalarProps.HasSubquery {
 				emitProp("subquery")
 			}
+			if scalarProps.HasUDF {
+				emitProp("udf")
+			}
 		}
 
 		if !f.HasFlags(ExprFmtHideConstraints) {
