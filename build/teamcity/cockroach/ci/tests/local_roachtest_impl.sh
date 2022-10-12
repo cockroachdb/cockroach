4570753e35f94015c1c996a3205b5f88847b367b
Renato Costa <renato@cockroachlabs.com>
2022-10-12 17:10:57 -0400

    build: stop local cluster, if any, before running acceptance roachtests
    This stops the `local` cluster, if any, before attempting to run
    acceptance roachtests in CI. If a previous `roachtest` run on the same
    host was abruptly killed, the local cluster would remain active and
    cause every test to fail.
    
    Fixes #88757.
    
    Epic: None.
    
    Release note: None.

--- build/teamcity/cockroach/ci/tests/local_roachtest_impl.sh
+++ build/teamcity/cockroach/ci/tests/local_roachtest_impl.sh
@@ -10,9 +10,20 @@ fi
 
 bazel build --config=$CROSSLINUX_CONFIG --config=ci //pkg/cmd/cockroach-short \
       //pkg/cmd/roachtest \
+      //pkg/cmd/roachprod \
       //pkg/cmd/workload
 
 BAZEL_BIN=$(bazel info bazel-bin --config=$CROSSLINUX_CONFIG --config=ci)
+ROACHPROD=$BAZEL_BIN/pkg/cmd/roachprod/roachprod_/roachprod
+
+# if there is already a local cluster on this host, stop it before we
+# attempt to run acceptance tests. While this is generally not the
+# case, if a previous `roachtest` run was abruptly killed, the local
+# cluster would remain active and cause every test below to fail.
+if $ROACHPROD cached-hosts | grep -q '^local'; then
+  $ROACHPROD destroy --all-local
+fi
+
 $BAZEL_BIN/pkg/cmd/roachtest/roachtest_/roachtest run acceptance kv/splits cdc/bank \
   --local \
   --parallelism=1 \
