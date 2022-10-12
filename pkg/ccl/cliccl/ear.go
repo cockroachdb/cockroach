7784767e5da6c726aa9d309793fb388b31dbbacb
Nick Travers <travers@cockroachlabs.com>
2022-10-12 15:27:03 -0700

    cliccl: add `encryption-registry-list` command
    The existing `enc_util` package contains a tool that could be used to
    dump the files in an encryption registry. This command has been broken
    since the file registry format was updated.
    
    Add the `(*PebbleFileRegistry).List` function, that returns a map of
    files in the registry. Adapt existing test cases.
    
    Add a `debug encryption-registry-list` command that will print all files
    contained in the registry of an encrypted store. This is useful for
    debugging which store / data key was used to encrypt each file,
    replacing the equivalent functionality in `enc_util`.
    
    Touches: #89095.
    Epic: None.
    
    Release note (ops change): Adds a new command that can be used by an
    operator to list the files present in the Encryption-At-Rest file
    registry.

--- pkg/ccl/cliccl/ear.go
+++ pkg/ccl/cliccl/ear.go
@@ -9,14 +9,21 @@
 package cliccl
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"io"
 	"os"
+	"sort"
 
+	"github.com/cockroachdb/cockroach/pkg/ccl/storageccl/engineccl/enginepbccl"
 	"github.com/cockroachdb/cockroach/pkg/cli"
 	"github.com/cockroachdb/cockroach/pkg/storage"
+	"github.com/cockroachdb/cockroach/pkg/storage/enginepb"
+	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
 	"github.com/cockroachdb/errors"
+	"github.com/cockroachdb/pebble/vfs"
 	"github.com/spf13/cobra"
 )
 
@@ -53,6 +60,64 @@ func runDecrypt(_ *cobra.Command, args []string) (returnErr error) {
 	}
 	if _, err = io.Copy(outFile, f); err != nil {
 		return errors.Wrapf(err, "could not write to output file")
+	}
+
+	return nil
+}
+
+type fileEntry struct {
+	name     string
+	envType  enginepb.EnvType
+	settings enginepbccl.EncryptionSettings
+}
+
+func (f fileEntry) String() string {
+	var b bytes.Buffer
+	_, _ = fmt.Fprintf(
+		&b, "%s:\n  env type: %s, %s\n",
+		f.name, f.envType, f.settings.EncryptionType,
+	)
+	if f.settings.EncryptionType != enginepbccl.EncryptionType_Plaintext {
+		_, _ = fmt.Fprintf(
+			&b, "  keyID: %s\n  nonce: % x\n  counter: %d",
+			f.settings.KeyId, f.settings.Nonce, f.settings.Counter,
+		)
+	}
+	return b.String()
+}
+
+func runList(cmd *cobra.Command, args []string) error {
+	dir := args[0]
+
+	fr := &storage.PebbleFileRegistry{
+		FS:       vfs.Default,
+		DBDir:    dir,
+		ReadOnly: true,
+	}
+	if err := fr.Load(); err != nil {
+		return errors.Wrapf(err, "could not load file registry")
+	}
+	defer func() { _ = fr.Close() }()
+
+	// List files and print to stdout.
+	var entries []fileEntry
+	for name, entry := range fr.List() {
+		var encSettings enginepbccl.EncryptionSettings
+		settings := entry.EncryptionSettings
+		if err := protoutil.Unmarshal(settings, &encSettings); err != nil {
+			return errors.Wrapf(err, "could not unmarshal encryption settings for %s", name)
+		}
+		entries = append(entries, fileEntry{
+			name:     name,
+			envType:  entry.EnvType,
+			settings: encSettings,
+		})
+	}
+	sort.Slice(entries, func(i, j int) bool {
+		return entries[i].name < entries[j].name
+	})
+	for _, e := range entries {
+		_, _ = fmt.Fprintf(cmd.OutOrStdout(), "%s\n", e)
 	}
 
 	return nil
