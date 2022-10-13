6ed0eb148f6ef2162eccffa66439a78e1e69ae96
Andrew Werner <awerner32@gmail.com>
2022-10-13 13:17:06 -0400

    insights: pool memory usage in insights layer
    These allocation were 0.7% of all allocations in a ycsb/A run.
    
    Release note: None

--- pkg/sql/sqlstats/insights/registry.go
+++ pkg/sql/sqlstats/insights/registry.go
@@ -12,9 +12,11 @@ package insights
 
 import (
 	"context"
+	"sync"
 
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql/clusterunique"
+	"github.com/cockroachdb/cockroach/pkg/util"
 	"github.com/cockroachdb/cockroach/pkg/util/cache"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 )
@@ -31,7 +33,7 @@ type lockingRegistry struct {
 	// before enabling the insights subsystem by default.
 	mu struct {
 		syncutil.RWMutex
-		statements map[clusterunique.ID][]*Statement
+		statements map[clusterunique.ID]*statementBuf
 		insights   *cache.OrderedCache
 	}
 }
@@ -45,48 +47,77 @@ func (r *lockingRegistry) ObserveStatement(sessionID clusterunique.ID, statement
 	}
 	r.mu.Lock()
 	defer r.mu.Unlock()
-	r.mu.statements[sessionID] = append(r.mu.statements[sessionID], statement)
+	b, ok := r.mu.statements[sessionID]
+	if !ok {
+		b = statementsBufPool.Get().(*statementBuf)
+		r.mu.statements[sessionID] = b
+	}
+	b.append(statement)
+}
+
+type statementBuf []*Statement
+
+func (b *statementBuf) append(statement *Statement) {
+	*b = append(*b, statement)
+}
+
+func (b *statementBuf) release() {
+	for i, n := 0, len(*b); i < n; i++ {
+		(*b)[i] = nil
+	}
+	*b = (*b)[:0]
+	statementsBufPool.Put(b)
+}
+
+var statementsBufPool = sync.Pool{
+	New: func() interface{} {
+		return new(statementBuf)
+	},
+}
+
+var insightPool = sync.Pool{
+	New: func() interface{} {
+		return new(Insight)
+	},
 }
 
 func (r *lockingRegistry) ObserveTransaction(sessionID clusterunique.ID, transaction *Transaction) {
 	if !r.enabled() {
 		return
 	}
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	statements := r.mu.statements[sessionID]
-	delete(r.mu.statements, sessionID)
+	statements := r.popSessionStatements(sessionID)
+	defer statements.release()
 
-	slowStatements := make(map[clusterunique.ID]struct{})
-	for _, s := range statements {
+	var slowStatements util.FastIntSet
+	for i, s := range *statements {
 		if r.detector.isSlow(s) {
-			slowStatements[s.ID] = struct{}{}
+			slowStatements.Add(i)
 		}
 	}
-
-	if len(slowStatements) > 0 {
-		for _, s := range statements {
-			var p Problem
-			var c []Cause
-			if _, ok := slowStatements[s.ID]; ok {
-				switch s.Status {
-				case Statement_Completed:
-					p = Problem_SlowExecution
-					c = r.causes.examine(s)
-				case Statement_Failed:
-					// Note that we'll be building better failure support for 23.1.
-					// For now, we only mark failed statements that were also slow.
-					p = Problem_FailedExecution
-				}
-			}
-			r.mu.insights.Add(s.ID, &Insight{
-				Session:     &Session{ID: sessionID},
-				Transaction: transaction,
-				Statement:   s,
-				Problem:     p,
-				Causes:      c,
-			})
+	if slowStatements.Empty() {
+		return
+	}
+	// Note that we'll record insights for every statement, not just for
+	// the slow ones.
+	for i, s := range *statements {
+		insight := insightPool.Get().(*Insight)
+		*insight = Insight{
+			Session:     Session{ID: sessionID},
+			Transaction: transaction,
+			Statement:   s,
 		}
+		if slowStatements.Contains(i) {
+			switch s.Status {
+			case Statement_Completed:
+				insight.Problem = Problem_SlowExecution
+				insight.Causes = r.causes.examine(insight.Causes, s)
+			case Statement_Failed:
+				// Note that we'll be building better failure support for 23.1.
+				// For now, we only mark failed statements that were also slow.
+				insight.Problem = Problem_FailedExecution
+			}
+		}
+		r.addInsight(s.ID, insight)
 	}
 }
 
@@ -111,18 +142,38 @@ func (r *lockingRegistry) enabled() bool {
 	return r.detector.enabled()
 }
 
+func (r *lockingRegistry) popSessionStatements(sessionID clusterunique.ID) *statementBuf {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	statements := r.mu.statements[sessionID]
+	delete(r.mu.statements, sessionID)
+	return statements
+}
+
+func (r *lockingRegistry) addInsight(id clusterunique.ID, insight *Insight) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.mu.insights.Add(id, insight)
+}
+
 func newRegistry(st *cluster.Settings, detector detector) *lockingRegistry {
 	config := cache.Config{
 		Policy: cache.CacheFIFO,
 		ShouldEvict: func(size int, key, value interface{}) bool {
 			return int64(size) > ExecutionInsightsCapacity.Get(&st.SV)
 		},
+		OnEvicted: func(_, value interface{}) {
+			insight := value.(*Insight)
+			insight.Causes = insight.Causes[:0]
+			*insight = Insight{Causes: insight.Causes}
+			insightPool.Put(insight)
+		},
 	}
 	r := &lockingRegistry{
 		detector: detector,
 		causes:   &causes{st: st},
 	}
-	r.mu.statements = make(map[clusterunique.ID][]*Statement)
+	r.mu.statements = make(map[clusterunique.ID]*statementBuf)
 	r.mu.insights = cache.NewOrderedCache(config)
 	return r
 }
