dd84f35819a7f079e7da0df56c6b072b34d23861
Pavel Kalinnikov <pavel@cockroachlabs.com>
2022-10-13 19:44:09 +0100

    kvserver: deflake TestLogGrowthWhenRefreshingPendingCommands
    Fixes #89862
    
    Release justification: fixes flaky test
    Release note: None

--- pkg/kv/kvserver/client_raft_test.go
+++ pkg/kv/kvserver/client_raft_test.go
@@ -1614,6 +1614,7 @@ func TestLogGrowthWhenRefreshingPendingCommands(t *testing.T) {
 		}
 		propNode := tc.GetFirstStoreFromServer(t, propIdx).TestSender()
 		tc.TransferRangeLeaseOrFatal(t, *leaderRepl.Desc(), tc.Target(propIdx))
+		tc.WaitForLeaseUpgrade(ctx, t, *leaderRepl.Desc())
 		testutils.SucceedsSoon(t, func() error {
 			// Lease transfers may not be immediately observed by the new
 			// leaseholder. Wait until the new leaseholder is aware.
@@ -1625,7 +1626,9 @@ func TestLogGrowthWhenRefreshingPendingCommands(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			if lease, _ := repl.GetLease(); !lease.Replica.Equal(repDesc) {
+			lease, _ := repl.GetLease()
+			t.Logf("lease: %v", lease)
+			if !lease.Replica.Equal(repDesc) {
 				return errors.Errorf("lease not transferred yet; found %v", lease)
 			}
 			return nil
