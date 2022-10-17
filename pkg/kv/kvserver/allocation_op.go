84c38409f8b240a83b01465540fddda308785675
Austen McClernon <austen@cockroachlabs.com>
2022-10-17 22:44:18 +0000

    kvserver: separate repl q decision from action
    Previously, the replicate queue would both plan and apply changes for
    in-process replicas within the processOneChange function. This was
    problematic for simulation as it was not possible to call
    processOneChange directly to apply the simulated result, without
    blocking the goroutine.
    
    This patch separates `processOneChange` into planning (`PlanOneChange`), the
    application of the change (`applyChange`) and post application tracking
    (`TrackChangeOutcome`).
    
    resolves: #90533
    
    Release note: None

---
+++ pkg/kv/kvserver/allocation_op.go
@@ -0,0 +1,142 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package kvserver
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/allocator"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/allocator/storepool"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvserverpb"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+)
+
+// AllocationOp represents an atomic allocation operation to be applied against
+// a specific replica.
+//
+// TODO(kvoli): Add AllocationRelocateRangeOp and AllocationFinalizeChangeOp.
+type AllocationOp interface {
+	// trackSideEffects tracks the side effects that have been generated in
+	// creating this operation. This is used for updating context specific
+	// metrics.
+	trackSideEffects()
+	// applyImpact updates the given storepool to reflect the result of
+	// applying this operation.
+	applyImpact(storepool *storepool.StorePool)
+	// lhBeingRemoved returns true when the leaseholder is will be removed if
+	// this operation succeeds, otherwise false.
+	lhBeingRemoved() bool
+}
+
+// AllocationTransferLeaseOp represents an operation to transfer a range lease to another
+// store, from the current one.
+type AllocationTransferLeaseOp struct {
+	target, source     roachpb.StoreID
+	usage              allocator.RangeUsageInfo
+	bypassSafetyChecks bool
+	sideEffects        func()
+}
+
+// lhBeingRemoved returns true when the leaseholder is will be removed if this
+// operation succeeds, otherwise false. This is always true for lease
+// transfers.
+func (o AllocationTransferLeaseOp) lhBeingRemoved() bool {
+	return true
+}
+
+func (o AllocationTransferLeaseOp) applyImpact(storepool *storepool.StorePool) {
+	// TODO(kvoli): Currently the local storepool is updated directly in the
+	// lease transfer call, rather than in this function. Move the storepool
+	// tracking from rq.TransferLease to this function once #89771 is merged.
+}
+
+// trackSideEffects tracks the side effects that have been generated in
+// creating this operation. This is used for updating context specific
+// metrics.
+func (o AllocationTransferLeaseOp) trackSideEffects() {
+	if o.sideEffects != nil {
+		o.sideEffects()
+	}
+}
+
+// AllocationChangeReplicasOp represents an operation to execute a change
+// replicas txn.
+type AllocationChangeReplicasOp struct {
+	usage             allocator.RangeUsageInfo
+	lhStore           roachpb.StoreID
+	chgs              roachpb.ReplicationChanges
+	priority          kvserverpb.SnapshotRequest_Priority
+	allocatorPriority float64
+	reason            kvserverpb.RangeLogEventReason
+	details           string
+	sideEffects       func()
+}
+
+// lhBeingRemoved returns true when the voter removals for this change replicas
+// operation includes the leaseholder store.
+func (o AllocationChangeReplicasOp) lhBeingRemoved() bool {
+	for _, chg := range o.chgs.VoterRemovals() {
+		if chg.StoreID == o.lhStore {
+			return true
+		}
+	}
+	return false
+}
+
+// applyEstimatedImpact updates the given storepool to reflect the result
+// of applying this operation.
+func (o AllocationChangeReplicasOp) applyImpact(storepool *storepool.StorePool) {
+	for _, chg := range o.chgs {
+		storepool.UpdateLocalStoreAfterRebalance(chg.Target.StoreID, o.usage, chg.ChangeType)
+	}
+}
+
+// trackSideEffects tracks the side effects that have been generated in
+// creating this operation. This is used for updating context specific
+// metrics.
+func (o AllocationChangeReplicasOp) trackSideEffects() {
+	if o.sideEffects != nil {
+		o.sideEffects()
+	}
+}
+
+// AllocationFinalizeAtomicReplicationOp represents an operation to finalize an
+// atomic change replicas operation and remove any remaining learners.
+type AllocationFinalizeAtomicReplicationOp struct{}
+
+func (o AllocationFinalizeAtomicReplicationOp) lhBeingRemoved() bool                       { return false }
+func (o AllocationFinalizeAtomicReplicationOp) applyImpact(storepool *storepool.StorePool) {}
+func (o AllocationFinalizeAtomicReplicationOp) trackSideEffects()                          {}
+
+// AllocationNoop represents no operation.
+type AllocationNoop struct{}
+
+func (o AllocationNoop) lhBeingRemoved() bool                       { return false }
+func (o AllocationNoop) applyImpact(storepool *storepool.StorePool) {}
+func (o AllocationNoop) trackSideEffects()                          {}
+
+// effectBuilder is a utility struct to track a list of effects, which may be
+// used to construct a single effect function that in turn calls all tracked
+// effects.
+type effectBuilder struct {
+	e []func()
+}
+
+func (b effectBuilder) add(effect func()) effectBuilder {
+	b.e = append(b.e, effect)
+	return b
+}
+
+func (b effectBuilder) f() func() {
+	return func() {
+		for _, effect := range b.e {
+			effect()
+		}
+	}
+}
