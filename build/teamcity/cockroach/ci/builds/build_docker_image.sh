0045de9a4a531907c867b6a1b45694cdbfa64aad
Mari Lyndon <mari@cockroachlabs.com>
2022-10-17 21:18:13 -0400

    Add arm64 support to docker image creating tasks
    This changes the x86_64 image in nightly and final build steps to be prefixed
    with "amd64-", and adds an arm64 image which is prefixed with "arm64-".
    The unprefixed image, which was previously the x86_64 image, is now a
    multi-architecture image which points to the two prefixed images.
    
    This change also splits build_docker_image into build_docker_image_arm64
    and build_docker_image_x86_64. The Build and Test Docker TeamCity builds
    have been split accordingly, into arm64 and x86_64 variants for each.
    These builds do not build/test the combined image, only the individual
    platforms' images.
    
    Part of CRDB-3143
    
    Release note (general change): Cockroach Docker images are now multi
    architecture manifests supporting the x86_64 (amd64) and arm64
    architectures.

--- build/teamcity/cockroach/ci/builds/build_docker_image.sh
+++ build/teamcity/cockroach/ci/builds/build_docker_image.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# The first and only parameter is the name of the architecture the image is being built for.
+# This should be the format Docker expects for the platform flag minus linux/ - i.e., either
+# amd64 or arm64. Old TC configs will run this file directly and not supply a platform, so
+# default to amd64.
+build_arch=${1:-amd64}
+
 dir="$(dirname $(dirname $(dirname $(dirname $(dirname "${0}")))))"
 source "$dir/teamcity-support.sh"  # For $root
 
@@ -11,7 +17,7 @@ chmod o+rwx "${artifacts}"
 
 tc_start_block "Copy cockroach binary and dependency files to build/deploy"
 
-# Get the cockroach binary from Build (Linux x86_64)
+# Get the cockroach binary from Build (Linux ${arch})
 # Artifacts rules:
 # bazel-bin/pkg/cmd/cockroach/cockroach_/cockroach=>upstream_artifacts
 # bazel-bin/c-deps/libgeos/lib/libgeos.so=>upstream_artifacts
@@ -33,12 +39,15 @@ docker_image_tar_name="cockroach-docker-image.tar"
 
 docker_tag="cockroachdb/cockroach-ci"
 
+docker rmi "$(grep '^FROM ' build/deploy/Dockerfile | head -n1 | cut -d ' ' -f2)"
 docker build \
   --no-cache \
+  --platform=linux/${build_arch} \
   --tag="$docker_tag" \
   --memory 30g \
   --memory-swap -1 \
   build/deploy
+docker rmi "$(grep -e '^FROM ' build/deploy/Dockerfile | head -n1 | cut -d ' ' -f2)"
 
 docker save "$docker_tag" | gzip > "${artifacts}/${docker_image_tar_name}".gz
 
