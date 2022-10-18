f72db8cccf804b02ea6fc2c6ae9d88bf4b1f47d0
Ganeshprasad Rajashekhar Biradar <ganeshprasad.biradar@cockroachlabs.com>
2022-10-18 11:22:56 -0500

    cdc: add support for format=parquet
    Parquet is a columnar storage format with compression and the ability to
    access a subset of columns for a query being its main advantages:
    
    https://parquet.apache.org/docs/overview/
    
    This commit adds support to emit changefeeds in parquet format. Currently
    parquet format can only be used with initial scan only.
    
    Release note (enterprise change): CHANGEFEEDS now have limited support
    for the parquet format.
    
    Resolves: #59819

--- pkg/ccl/changefeedccl/encoder_test.go
+++ pkg/ccl/changefeedccl/encoder_test.go
@@ -1051,3 +1051,39 @@ func makeTestTypes(rng *rand.Rand, numKeyTypes, numColTypes int) (keyTypes, valT
 
 	return randTypes(numKeyTypes, true), randTypes(numColTypes, false)
 }
+
+func TestParquetEncoder(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	testFn := func(t *testing.T, s TestServer, f cdctest.TestFeedFactory) {
+		defer TestingSetIncludeParquetMetadata()()
+
+		sqlDB := sqlutils.MakeSQLRunner(s.DB)
+		sqlDB.Exec(t, `CREATE TABLE foo (i INT PRIMARY KEY, x STRING, y INT, z FLOAT NOT NULL, a BOOL)`)
+		sqlDB.Exec(t, `INSERT INTO foo VALUES (1, 'Alice', 3, 0.5032135844230652, true), (2, 'Bob',
+	2, CAST('nan' AS FLOAT),false),(3, NULL, NULL, 4.5, NULL)`)
+
+		foo := feed(t, f, fmt.Sprintf(`CREATE CHANGEFEED FOR foo `+
+			`WITH format=%s`, changefeedbase.OptFormatParquet))
+		defer closeFeed(t, foo)
+
+		assertPayloads(t, foo, []string{
+			`foo: [1]->{"after": {"a": true, "i": 1, "x": "Alice", "y": 3, "z": 0.5032135844230652}}`,
+			`foo: [2]->{"after": {"a": false, "i": 2, "x": "Bob", "y": 2, "z": "NaN"}}`,
+			`foo: [3]->{"after": {"a": null, "i": 3, "x": null, "y": null, "z": 4.5}}`,
+		})
+
+		sqlDB.Exec(t, `UPDATE foo SET x='wonderland' where i=1`)
+		assertPayloads(t, foo, []string{
+			`foo: [1]->{"after": {"a": true, "i": 1, "x": "wonderland", "y": 3, "z": 0.5032135844230652}}`,
+		})
+
+		sqlDB.Exec(t, `DELETE from foo where i=1`)
+		assertPayloads(t, foo, []string{
+			`foo: [1]->{"after": null}`,
+		})
+
+	}
+	cdcTest(t, testFn, feedTestForceSink("cloudstorage"))
+}
