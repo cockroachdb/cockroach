7c6c65dab72b559be4b0c7766d5fea76618aa3ad
Mari Lyndon <mari@cockroachlabs.com>
2022-10-19 00:09:53 -0400

    Add arm64 support to docker image creating tasks
    This changes the x86_64 image in nightly and final build steps to be prefixed
    with "amd64-", and adds an arm64 image which is prefixed with "arm64-".
    The unprefixed image, which was previously the x86_64 image, is now a
    multi-architecture image which points to the two prefixed images.
    
    This change also splits build_docker_image into build_docker_image_arm64
    and build_docker_image_x86_64. The Build and Test Docker TeamCity builds
    have been split accordingly, into arm64 and x86_64 variants for each.
    These builds do not build/test the combined image, only the individual
    platforms' images.
    
    Part of CRDB-3143
    
    Release note (general change): Cockroach Docker images are now multi
    architecture manifests supporting the x86_64 (amd64) and arm64
    architectures.

--- build/release/teamcity-publish-redhat-release.sh
+++ build/release/teamcity-publish-redhat-release.sh
@@ -47,23 +47,34 @@ tc_start_block "Configure docker"
 docker_login_with_redhat
 tc_end_block "Configure docker"
 
-tc_start_block "Rebuild docker image"
-sed \
-  -e "s,@repository@,${dockerhub_repository},g" \
-  -e "s,@tag@,${build_name},g" \
-  build/deploy-redhat/Dockerfile.in > build/deploy-redhat/Dockerfile
+tc_start_block "Rebuild per-arch docker images"
+declare -a combined_image_args
+for docker_arch in "${docker_archs[@]}"; do
+  cp --recursive build/deploy-redhat "build/deploy-redhat-${docker_arch}"
 
-cat build/deploy-redhat/Dockerfile
+  sed \
+    -e "s,@repository@,${dockerhub_repository},g" \
+    -e "s,@tag@,${docker_arch}-${build_name},g" \
+    "build/deploy-redhat-${docker_arch}/Dockerfile.in" > "build/deploy-redhat-${docker_arch}/Dockerfile"
 
-docker build --no-cache \
-  --label release=$rhel_release \
-  --tag=${rhel_repository}:${build_name} \
-  build/deploy-redhat
-tc_end_block "Rebuild docker image"
+  cat "build/deploy-redhat-${docker_arch}/Dockerfile"
 
-tc_start_block "Push RedHat docker image"
-retry docker push "${rhel_repository}:${build_name}"
-tc_end_block "Push RedHat docker image"
+  build_docker_tag="${rhel_repository}:${docker_arch}-${build_name}"
+  combined_image_args+=("--amend" "${build_docker_tag}")
+  docker build --no-cache \
+    --label=release=$rhel_release \
+    --platform="linux/${docker_arch}" \
+    --tag="${build_docker_tag}" \
+    --pull \
+    "build/deploy-redhat-${docker_arch}"
+  retry docker push "${build_docker_tag}"
+done
+tc_end_block "Rebuild per-arch docker images"
+
+tc_start_block "Push multiarch RedHat docker image"
+docker manifest create "${rhel_repository}:${build_name}" "${combined_image_args[@]}"
+retry docker manifest push "${rhel_repository}:${build_name}"
+tc_end_block "Push multiarch RedHat docker image"
 
 tc_start_block "Run preflight"
 mkdir -p artifacts
@@ -77,7 +88,7 @@ docker run \
   --env PFLT_PYXIS_API_TOKEN="$REDHAT_API_TOKEN" \
   --env PFLT_DOCKERCONFIG=/temp-authfile.json \
   --env DOCKER_CONFIG=/tmp/docker \
-  -v $PWD/artifacts:/artifacts \
+  -v "$PWD/artifacts:/artifacts" \
   -v ~/.docker/config.json:/temp-authfile.json:ro \
   -v ~/.docker/config.json:/tmp/docker/config.json:ro \
   quay.io/opdev/preflight:stable check container \
