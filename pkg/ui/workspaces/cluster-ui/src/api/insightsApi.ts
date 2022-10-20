71a836354ed9f0e8fa4ab99fdfcb5a9595f57f44
maryliag <marylia@cockroachlabs.com>
2022-10-20 14:55:25 -0400

    ui: link insights to fingerprint details
    Now from the Insights page, clicking on the statement
    or fingerprint ids, it will bring you to their respective
    details page.
    
    This commit also filter out transaction insights that didn't
    have their value set yet (meaning they're still 0).
    
    Finally, this commit fixes the start/end values being passed
    to the combined statement endpoint, to the correct rounded
    values, aligning what we say on the UI.
    
    Fixes #87750
    
    Release note (ui change): The fingerprint id values
    for statement and transactions on the insights pages are
    links that open the respective details page on the time period
    of the execution of that statement/transaction.
    
    Release note (bug fix): Sendind the proper start/end values
    to the endpoint used on SQL Activity page, now returning the
    full hour as described on the UI.

--- pkg/ui/workspaces/cluster-ui/src/api/insightsApi.ts
+++ pkg/ui/workspaces/cluster-ui/src/api/insightsApi.ts
@@ -74,6 +74,7 @@ const txnContentionQuery = `SELECT *
                                                max(collection_ts)       AS collection_ts,
                                                sum(contention_duration) AS total_contention_duration
                                         FROM crdb_internal.transaction_contention_events tce
+                                        WHERE waiting_txn_fingerprint_id != '\x0000000000000000'
                                         GROUP BY waiting_txn_id, waiting_txn_fingerprint_id),
                                        (SELECT txn_id FROM crdb_internal.cluster_execution_insights)
                                   WHERE total_contention_duration > threshold
@@ -570,6 +571,7 @@ type ExecutionInsightsResponseRow = {
   session_id: string;
   txn_id: string;
   txn_fingerprint_id: string; // hex string
+  implicit_txn: boolean;
   stmt_id: string;
   stmt_fingerprint_id: string; // hex string
   query: string;
@@ -608,6 +610,7 @@ function getStatementInsightsFromClusterExecutionInsightsResponse(
     return {
       transactionID: row.txn_id,
       transactionFingerprintID: row.txn_fingerprint_id,
+      implicitTxn: row.implicit_txn,
       query: row.query,
       startTime: start,
       endTime: end,
@@ -651,6 +654,7 @@ const statementInsightsQuery: InsightQuery<
       session_id,
       txn_id,
       encode(txn_fingerprint_id, 'hex')  AS txn_fingerprint_id,
+      implicit_txn,
       stmt_id,
       encode(stmt_fingerprint_id, 'hex') AS stmt_fingerprint_id,
       query AS non_prettified_query,
