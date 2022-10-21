ef5b5104a5aea0614183da524dc1e1a01a575112
Thomas Hardy <thardy@cockroachlabs.com>
2022-10-21 11:25:23 -0400

    cluster-ui: fetch databases using sql-over-http endpoint
    Resolves: #90257
    
    This commit introduces a function to cluster-ui that fetches database
    names using the sql-over-http endpoint. This removes the need to use the
    admin server's `/databases` endpoint.
    
    Release note: None

---
+++ pkg/ui/workspaces/cluster-ui/src/api/databasesApi.ts
@@ -0,0 +1,61 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import {
+  executeInternalSql,
+  SqlExecutionRequest,
+  sqlResultsAreEmpty,
+} from "./sqlApi";
+import { PROMISE_TIMEOUT, withTimeout } from "./util";
+import moment from "moment";
+
+export type DatabasesColumns = {
+  database_name: string;
+  owner: string;
+  primary_region: string;
+  secondary_region: string;
+  regions: string[];
+  survival_goal: string;
+};
+
+export type DatabasesListResponse = { databases: string[] };
+
+export const databasesRequest: SqlExecutionRequest = {
+  statements: [
+    {
+      sql: `SHOW DATABASES`,
+    },
+  ],
+  execute: true,
+};
+
+export function getDatabasesList(
+  timeout: moment.Duration = PROMISE_TIMEOUT,
+  ): Promise<DatabasesListResponse> {
+  return withTimeout(
+    executeInternalSql<DatabasesColumns>(databasesRequest),
+    timeout
+  ).then(result  => {
+    // If request succeeded but query failed, throw error (caught by saga/cacheDataReducer).
+    if (result.error) {
+      throw result.error;
+    }
+
+    if (sqlResultsAreEmpty(result)) {
+      return { databases: [] };
+    }
+
+    const dbNames: string[] = result.execution.txn_results[0].rows.map(
+      row => row.database_name,
+    );
+
+    return { databases: dbNames };
+  });
+}
