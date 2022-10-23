616c822d77ebf6bda889303d8e2a9b458ab8ce26
Raphael 'kena' Poss <knz@thaumogen.net>
2022-10-23 19:21:35 +0200

    server: split the tenant server creation into 3 stages
    The original motivation (and ultimate goal) for this commit is to
    split the tenant server initialization into three phases: `New()`,
    `PreStart()`, `AcceptClients()`, so as to reuse a common process startup
    logic in a separate (later) commit.
    
    To achieve this goal, this commit re-orders the initialization steps
    in `server.NewTenantServer` (previously known as
    `server.StartTenant`), and extracts many of them into a new method
    `(*SQLServerWrapper).PreStart()`.
    
    The specific order of the code in `NewTenantServer()` and
    `(*SQLServerWrapper).PreStart()` was chosen to mirror the order of
    things in `NewServer()` and `(*Server).PreStart()`.
    
    Reasons for using the same order:
    
    - it makes the review of this change easier: the reviewer can pull
      `server.go` and `tenant.go` and read them side-by-side, to satisfy
      themselves that the two implementations of
      `NewServer`/`NewTenantServer` and `PreStart` are equivalent.
    
    - it will make it easier for future maintainers to keep them in sync.
    
    - it helps us discover the fact that both sides share a lot of code.
      This opens an opportunity to merge them to a common implementation
      at a later stage.
    
    While doing this work, care was also taken to discover which steps of
    `(*Server).PreStart()` were *missing* from the tenant server
    initialization. We found the following:
    
    - the Sentry context enhancement (to report cluster ID, etc)
      was missing. This commit fixes that.
    
    - several log entries that report the server configuration
      to the OPS channel were not emitted. This commit fixes that.
    
    - the Graphite metric reporting was never enabled, even when
      configured. This commit fixes that.
    
    - the Obs Service testing knobs (TestingKnobs.EventExporter) were
      not configured on the ObsServer instance. This commit fixes that.
    
    - the `go.scheduler_latency` metric was not being measured.
      This commit fixes that.
    
    Additionally, two followup issues were filed for the following
    missing steps:
    
    - missing support for the special file that blocks background jobs. (#90524)
    - missing support for the system.eventlog cleanup loop. (#90521)
    
    Release note: None

--- pkg/server/tenant_status.go
+++ pkg/server/tenant_status.go
@@ -87,7 +87,6 @@ func newTenantStatusServer(
 	closedSessionCache *sql.ClosedSessionCache,
 	remoteFlowRunner *flowinfra.RemoteFlowRunner,
 	st *cluster.Settings,
-	sqlServer *SQLServer,
 	rpcCtx *rpc.Context,
 	stopper *stop.Stopper,
 ) *tenantStatusServer {
@@ -100,7 +99,6 @@ func newTenantStatusServer(
 			closedSessionCache: closedSessionCache,
 			remoteFlowRunner:   remoteFlowRunner,
 			st:                 st,
-			sqlServer:          sqlServer,
 			rpcCtx:             rpcCtx,
 			stopper:            stopper,
 		},
