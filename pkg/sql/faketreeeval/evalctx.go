4b743aa7a5fefb8dc1d1a9d151c6a2520112eab1
Rafi Shamim <rafi@cockroachlabs.com>
2022-10-25 15:54:35 -0400

    sql: fix pg_table_is_visible to handle indexes
    This uses the internal executor now to do the introspection using
    pg_class where everything can be looked up all at once.
    
    There's an increase in number of round trips, but it's a constant
    factor.
    
    Release note (bug fix): Fixed the pg_table_is_visible builtin function
    so it correctly reports visibility of indexes based on the current
    search_path.

--- pkg/sql/faketreeeval/evalctx.go
+++ pkg/sql/faketreeeval/evalctx.go
@@ -66,13 +66,6 @@ func (so *DummySequenceOperators) SchemaExists(
 	return false, errors.WithStack(errSequenceOperators)
 }
 
-// IsTableVisible is part of the eval.DatabaseCatalog interface.
-func (so *DummySequenceOperators) IsTableVisible(
-	ctx context.Context, curDB string, searchPath sessiondata.SearchPath, tableID oid.Oid,
-) (bool, bool, error) {
-	return false, false, errors.WithStack(errSequenceOperators)
-}
-
 // IsTypeVisible is part of the eval.DatabaseCatalog interface.
 func (so *DummySequenceOperators) IsTypeVisible(
 	ctx context.Context, curDB string, searchPath sessiondata.SearchPath, typeID oid.Oid,
@@ -362,13 +355,6 @@ func (ep *DummyEvalPlanner) ValidateAllMultiRegionZoneConfigsInCurrentDatabase(
 // SchemaExists is part of the eval.DatabaseCatalog interface.
 func (ep *DummyEvalPlanner) SchemaExists(ctx context.Context, dbName, scName string) (bool, error) {
 	return false, errors.WithStack(errEvalPlanner)
-}
-
-// IsTableVisible is part of the eval.DatabaseCatalog interface.
-func (ep *DummyEvalPlanner) IsTableVisible(
-	ctx context.Context, curDB string, searchPath sessiondata.SearchPath, tableID oid.Oid,
-) (bool, bool, error) {
-	return false, false, errors.WithStack(errEvalPlanner)
 }
 
 // IsTypeVisible is part of the eval.DatabaseCatalog interface.
