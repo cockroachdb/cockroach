bea29b8dd55f0c84d024e8e9cc422ee26ce1bd6b
Andrei Matei <andrei@cockroachlabs.com>
2022-10-27 12:56:19 -0400

    sqlliveness: improve slinstance.Instance
    This patch improves the code of slinstance.Instance and fixes some bugs.
    
    Before this patch, the background sqlliveness heartbeat loop was very
    inconsistent about what events it signaled on exit. Some exit paths
    would raise an event to the event consumer, some wouldn't. The ones that
    didn't are very dubious because future calls to Session() might block
    forever.
    Even for the code paths that did raise call into clearSession(), which
    is supposed to raise an event, the event would only be produced if the
    current session is not currently expired. Tying that event with the
    session expiration didn't make sense because, if the session is not
    currently expired and the hb loop exits, there will be no further
    opportunity to signal an event.
    
    This patch makes the raising of events clear and consistent: whenever
    the heartbeat loop exists, it arranges that future Session() calls error
    out, and it signals the event listener if there was currently a session.
    
    This patch also improves the shutdown process triggered in tenant
    servers when a session is found to have been deleted. This shutdown
    works through this event raised by the Instance whenever a session is
    deleted, giving the sqlServer the opportunity to initiate a shutdown.
    Before this patch, the event handler would trigger a shutdown
    asynchronously. While the shutdown trigger propagated, the sqlliveness
    Instance could create new sessions. I don't think there were clear bad
    consequences of this, but, at the very least, creating new sessions in a
    tenant server is confusing because the server is supposed to be bound to
    a single session throughout its lifetime - that's why the server shuts
    down when it finds out that the session has been deleted. This patch
    makes it so that the heartbeat loop that generally creates new session
    shuts down synchronously when the event handler tells it to do so.
    
    Release note: None

--- pkg/sql/sqlliveness/slinstance/slinstance.go
+++ pkg/sql/sqlliveness/slinstance/slinstance.go
@@ -16,6 +16,7 @@ package slinstance
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
@@ -97,8 +98,14 @@ func (s *session) setExpiration(exp hlc.Timestamp) {
 // listeners of some state changes.
 type SessionEventListener interface {
 	// OnSessionDeleted is called when the session liveness record is found to be
-	// missing.
-	OnSessionDeleted(context.Context)
+	// missing, or otherwise when the Instance background worker exits and thus
+	// the current session will not be heartbeated anymore.
+	//
+	// If OnSessionDeleted returns true, the sqlliveness code will continue to
+	// create another session in the background. If it returns false, the
+	// sqlliveness background worker shuts down without creating a new session;
+	// further calls to Instance.Session() will fail.
+	OnSessionDeleted(context.Context) (createAnotherSession bool)
 }
 
 // Instance implements the sqlliveness.Instance interface by storing the
@@ -118,19 +125,34 @@ type Instance struct {
 	testKnobs     sqlliveness.TestingKnobs
 	mu            struct {
 		syncutil.Mutex
-		startErr error
-		blockCh  chan struct{}
-		s        *session
+		// stopErr, if set, indicates that the heartbeat loop has stopped because of
+		// this error. Calls to Session() will return this error.
+		stopErr error
+		// s is the current session, if any.
+		s *session
+		// blockCh is set when s == nil && stopErr == nil. It is used to wait on
+		// updates to s.
+		blockCh chan struct{}
 	}
 }
 
-func (l *Instance) getSessionOrBlockCh() (*session, chan struct{}) {
+var _ sqlliveness.Instance = &Instance{}
+
+// getSessionOrBlockCh returns the Instance's session state: if there is
+// currently a session, it is returned. Otherwise, if the heartbeat loop is
+// working on creating a session, a channel to wait on is returned. Finally, if
+// the Instance is not creating sessions any more, an error is returned.
+func (l *Instance) getSessionOrBlockCh() (*session, chan struct{}, error) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
-	if l.mu.s != nil {
-		return l.mu.s, nil
+
+	if l.mu.stopErr != nil {
+		return nil, nil, l.mu.stopErr
 	}
-	return nil, l.mu.blockCh
+	if l.mu.s != nil {
+		return l.mu.s, nil, nil
+	}
+	return nil, l.mu.blockCh, nil
 }
 
 func (l *Instance) setSession(s *session) {
@@ -138,17 +160,37 @@ func (l *Instance) setSession(s *session) {
 	l.mu.s = s
 	// Notify all calls to Session that a non-nil session is available.
 	close(l.mu.blockCh)
+	l.mu.blockCh = nil
 	l.mu.Unlock()
 }
 
-func (l *Instance) clearSession(ctx context.Context) {
+// clearSession resets the Instance to a nil session, indicating that we no
+// longer have a usable, non-expired session. Further calls to Session() will
+// block for acquiring a new session.
+//
+// clearSession returns whether a new session should be created. clearSession
+// invokes the sessions events handler, if any, which has the power to say that
+// no new session should be created. In that case, the heartbeat loop will exit
+// and further Session() calls will result in errors.
+func (l *Instance) clearSession(ctx context.Context) (createNewSession bool) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
-	if expiration := l.mu.s.Expiration(); expiration.Less(l.clock.Now()) {
-		l.sessionEvents.OnSessionDeleted(ctx)
+	return l.clearSessionLocked(ctx)
+}
+
+func (l *Instance) clearSessionLocked(ctx context.Context) (createNewSession bool) {
+	if l.mu.s == nil {
+		panic("expected session to be set")
 	}
+	// When the session is set, blockCh should not be set.
+	if l.mu.blockCh != nil {
+		panic("unexpected blockCh")
+	}
+
 	l.mu.s = nil
 	l.mu.blockCh = make(chan struct{})
+
+	return l.sessionEvents.OnSessionDeleted(ctx)
 }
 
 // createSession tries until it can create a new session and returns an error
@@ -238,6 +280,25 @@ func (l *Instance) extendSession(ctx context.Context, s *session) (bool, error) 
 }
 
 func (l *Instance) heartbeatLoop(ctx context.Context) {
+	err := l.heartbeatLoopInner(ctx)
+	if err == nil {
+		panic("expected heartbeat to always terminate with an error")
+	}
+
+	log.Warning(ctx, "exiting heartbeat loop")
+
+	// Keep track of the fact that this Instance is not usable anymore. Further
+	// Session() calls will return errors.
+	l.mu.Lock()
+	if l.mu.s != nil {
+		_ = l.clearSessionLocked(ctx)
+	}
+	l.mu.stopErr = err
+	close(l.mu.blockCh)
+	l.mu.Unlock()
+}
+
+func (l *Instance) heartbeatLoopInner(ctx context.Context) error {
 	defer func() {
 		log.Warning(ctx, "exiting heartbeat loop")
 	}()
@@ -252,44 +313,43 @@ func (l *Instance) heartbeatLoop(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
-			l.clearSession(ctx)
-			return
+			return stop.ErrUnavailable
 		case <-t.C:
 			t.Read = true
-			s, _ := l.getSessionOrBlockCh()
+
+			var s *session
+			l.mu.Lock()
+			s = l.mu.s
+			l.mu.Unlock()
+
+			// If we don't currently have a session, create one.
 			if s == nil {
 				newSession, err := l.createSession(ctx)
 				if err != nil {
-					func() {
-						l.mu.Lock()
-						defer l.mu.Unlock()
-						l.mu.startErr = err
-						// There was an unrecoverable error when trying to
-						// create the session. Notify all calls to Session that
-						// the session failed.
-						close(l.mu.blockCh)
-					}()
-					l.sessionEvents.OnSessionDeleted(ctx)
-					return
+					return err
 				}
 				l.setSession(newSession)
 				t.Reset(l.hb())
 				continue
 			}
+
+			// Extend the session.
 			found, err := l.extendSession(ctx, s)
 			if err != nil {
-				l.clearSession(ctx)
-				return
+				// extendSession only ever returns an error on a canceled ctx.
+				return err
 			}
 			if !found {
-				l.clearSession(ctx)
-				// Start next loop iteration immediately to insert a new session.
+				// Someone deleted our session record. Let's clear our state and proceed
+				// to create a new session, unless the event handler wants to shut down
+				// instead.
+				if !l.clearSession(ctx) {
+					return errors.New("session record deleted")
+				}
 				t.Reset(0)
 				continue
 			}
-			if log.V(2) {
-				log.Infof(ctx, "extended SQL liveness session %s", s.ID())
-			}
+			log.VEventf(ctx, 2, "extended SQL liveness session %s", s.ID())
 			t.Reset(l.hb())
 		}
 	}
@@ -352,26 +412,22 @@ func (l *Instance) Session(ctx context.Context) (sqlliveness.Session, error) {
 	}
 
 	for {
-		s, ch := l.getSessionOrBlockCh()
+		s, ch, err := l.getSessionOrBlockCh()
+		if err != nil {
+			return nil, err
+		}
 		if s != nil {
 			return s, nil
 		}
 
+		// Wait for the background worker to create a new session.
 		select {
+		case <-ch:
+			continue
 		case <-l.stopper.ShouldQuiesce():
 			return nil, stop.ErrUnavailable
 		case <-ctx.Done():
 			return nil, ctx.Err()
-		case <-ch:
-			var err error
-			func() {
-				l.mu.Lock()
-				defer l.mu.Unlock()
-				err = l.mu.startErr
-			}()
-			if err != nil {
-				return nil, err
-			}
 		}
 	}
 }
@@ -381,4 +437,4 @@ type dummySessionEventListener struct{}
 var _ SessionEventListener = &dummySessionEventListener{}
 
 // OnSessionDeleted implements the slinstance.SessionEventListener interface.
-func (d *dummySessionEventListener) OnSessionDeleted(context.Context) {}
+func (d *dummySessionEventListener) OnSessionDeleted(context.Context) bool { return true }
