0b290922d1ca22fb0703706f4df45c5175e39208
Yahor Yuzefovich <yahor@cockroachlabs.com>
2022-10-28 12:16:04 -0700

    flowinfra: use the flow context in FlowStream RPC in outbox
    Previously, we used the background context when performing the
    `FlowStream` RPC in the row-based outbox rather than the flow context.
    This has been the case for like four years now, but I believe it is
    incorrect. In particular, if the flow context is canceled, this would
    not shutdown the gRPC stream, possibly leaking some of the goroutines
    (e.g. the goroutine listening for the drain signal from the inbox). This
    is now fixed by correctly using the flow context.
    
    This commit also fixes the outbox to satisfy the gRPC contract hidden in
    [1] where the listener goroutine would not `Recv` everything from the
    stream which could result in leakage of gRPC goroutines.
    
    This required the cleanup of the outbox infrastructure to bring it more
    in line with what we have in the vectorized engine (which has been
    updated some time ago with recent learnings). In particular, the
    following problems have been fixed:
    - `CloseSend` signal from the main goroutine of the outbox previously
    could have been omitted in some cases (I'm not sure what the impact of
    this is though)
    - the listener ("watchdog") goroutine is now tracked by the wait group
    of the flow which makes it so that `Flow.Wait` blocks until the watchdog
    exits. That goroutine now exits only after it `Recv`es an error from the
    server side (the inbox) of `FlowStream` RPC.
    
    [1] https://pkg.go.dev/google.golang.org/grpc#ClientConn.NewStream
    
    Epic: None
    
    Release note: None

--- pkg/sql/colflow/colrpc/outbox.go
+++ pkg/sql/colflow/colrpc/outbox.go
@@ -13,7 +13,6 @@ package colrpc
 import (
 	"bytes"
 	"context"
-	"io"
 	"sync/atomic"
 	"time"
 
@@ -26,6 +25,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/colmem"
 	"github.com/cockroachdb/cockroach/pkg/sql/execinfra"
 	"github.com/cockroachdb/cockroach/pkg/sql/execinfrapb"
+	"github.com/cockroachdb/cockroach/pkg/sql/flowinfra"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/tracing"
@@ -225,25 +225,6 @@ func (o *Outbox) Run(
 	log.VEvent(ctx, 2, "Outbox exiting")
 }
 
-// handleStreamErr is a utility method used to handle an error when calling
-// a method on a flowStreamClient. If err is an io.EOF, outboxCtxCancel is
-// called, for all other errors flowCtxCancel is. The given error is logged with
-// the associated opName.
-func handleStreamErr(
-	ctx context.Context,
-	opName redact.SafeString,
-	err error,
-	flowCtxCancel, outboxCtxCancel context.CancelFunc,
-) {
-	if err == io.EOF {
-		log.VEventf(ctx, 2, "Outbox calling outboxCtxCancel after %s EOF", opName)
-		outboxCtxCancel()
-	} else {
-		log.VEventf(ctx, 1, "Outbox calling flowCtxCancel after %s connection error: %+v", opName, err)
-		flowCtxCancel()
-	}
-}
-
 func (o *Outbox) moveToDraining(ctx context.Context, reason redact.RedactableString) {
 	if atomic.CompareAndSwapUint32(&o.draining, 0, 1) {
 		log.VEventf(ctx, 2, "Outbox moved to draining (%s)", reason)
@@ -321,7 +302,7 @@ func (o *Outbox) sendBatches(
 			// soon as the message is written to the control buffer. The message is
 			// marshaled (bytes are copied) before writing.
 			if err := stream.Send(o.scratch.msg); err != nil {
-				handleStreamErr(ctx, "Send (batches)", err, flowCtxCancel, outboxCtxCancel)
+				flowinfra.HandleStreamErr(ctx, "Send (batches)", err, flowCtxCancel, outboxCtxCancel)
 				return
 			}
 		}
@@ -399,12 +380,12 @@ func (o *Outbox) runWithStream(
 		for {
 			msg, err := stream.Recv()
 			if err != nil {
-				handleStreamErr(ctx, "watchdog Recv", err, flowCtxCancel, outboxCtxCancel)
+				flowinfra.HandleStreamErr(ctx, "watchdog Recv", err, flowCtxCancel, outboxCtxCancel)
 				break
 			}
 			switch {
 			case msg.Handshake != nil:
-				log.VEventf(ctx, 2, "Outbox received handshake: %v", msg.Handshake)
+				log.VEventf(ctx, 2, "Outbox received handshake: %s", msg.Handshake)
 			case msg.DrainRequest != nil:
 				log.VEventf(ctx, 2, "Outbox received drain request")
 				o.moveToDraining(ctx, "consumer requested draining" /* reason */)
@@ -423,14 +404,14 @@ func (o *Outbox) runWithStream(
 		}
 		o.moveToDraining(ctx, reason)
 		if err := o.sendMetadata(ctx, stream, errToSend); err != nil {
-			handleStreamErr(ctx, "Send (metadata)", err, flowCtxCancel, outboxCtxCancel)
+			flowinfra.HandleStreamErr(ctx, "Send (metadata)", err, flowCtxCancel, outboxCtxCancel)
 		} else {
 			// Close the stream. Note that if this block isn't reached, the stream
 			// is unusable.
 			// The receiver goroutine will read from the stream until any error
 			// is returned (most likely an io.EOF).
 			if err := stream.CloseSend(); err != nil {
-				handleStreamErr(ctx, "CloseSend", err, flowCtxCancel, outboxCtxCancel)
+				flowinfra.HandleStreamErr(ctx, "CloseSend", err, flowCtxCancel, outboxCtxCancel)
 			}
 		}
 	}
