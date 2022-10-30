77a9d52878a528b50833defd806cfbbb2af87398
Erik Grinaker <grinaker@cockroachlabs.com>
2022-10-30 12:58:31 +0000

    storage: fix point synthesis activation during reverse scans
    This patch fixes a bug where `pointSynthesizingIter` could appear to
    have skipped a synthetic point tombstone in the reverse direction when
    enabled dynamically by `pebbleMVCCScanner`.
    
    Consider the following case:
    
    ```
    REAL DATASET          SYNTHETIC DATASET
    3       [b3]          3      [b3]
    2    a2               2   a2
    1    [---)            1   x
         a   b                a   b
    ```
    
    Recall that `pebbleMVCCScanner` only enables `pointSynthesizingIter`
    when it encounters a range key. In the case above, during a reverse
    scan, the `[a-b)@1` range key will first become visible to
    `pebbleMVCCScanner` when it lands on `a@2`, so it enabled point
    synthesis positioned at the `a@2` point key. Notice how the iterator has
    now skipped over the synthetic point tombstone `a@1`.
    
    This is particularly problematic when combined with `pebbleMVCCScanner`
    peeking, which assumes that following a `iterPeekPrev()` call, an
    `iterNext()` call can step the parent iterator forward once to get back
    to the original position.  With the above bug, that is no longer true,
    as it instead lands on the synthetic point tombstone which was skipped
    during reverse iteration. During intent processing for `b@3`, such an
    `iterNext()` call is expected to land on the intent's provisional value
    at `b@3`, but it instead lands on the intent itself at `b@0`. This in
    turn caused a value checksum or decoding failure, where it was expecting
    the current key to be `b@3`, but the actual key was `b@0`.
    
    This patch fixes the bug by keeping track of the direction of the
    previous positioning operation in `pebbleMVCCScanner`, and then
    repositioning the `pointSynthesizingIter` as appropriate during dynamic
    activation in the reverse direction. This doesn't have a significant
    effect on performance (in particular, the point get path is never in
    reverse).
    
    Several alternative approaches were attempted but abandoned:
    
    * Don't synthesize points at a range key's start bound. While compelling
      for a range of reasons (complexity, performance, etc) this has two
      flaws: on a bare range key, the iterator still needs to know which
      direction to skip in, and we also need to know the timestamps of bare
      range keys for `FailOnMoreRecent` handling.
    
    * Only synthesize a single point at a range key's start bound. This
      would solve the problem at hand, but has rather strange semantics.
    
    Longer-term, it may be better to merge range key logic into
    `pebbleMVCCScanner` itself -- `pointSynthesizingIter` was intended to
    reduce complexity, but it's not clear that the overall complexity is
    actually reduced.
    
    Release note: None

--- pkg/storage/pebble_mvcc_scanner.go
+++ pkg/storage/pebble_mvcc_scanner.go
@@ -309,6 +309,11 @@ func extractResultKey(repr []byte) roachpb.Key {
 // by switching to a pointSynthesizingIter.
 type pebbleMVCCScanner struct {
 	parent MVCCIterator
+	// parentReverse is true if the previous parent positioning operation was a
+	// reverse operation (SeekLT or Prev). This is needed to correctly initialize
+	// pointIter when encountering an MVCC range tombstone in reverse.
+	// TODO(erikgrinaker): Consider adding MVCCIterator.IsReverse() instead.
+	parentReverse bool
 	// pointIter is a point synthesizing iterator that wraps and replaces parent
 	// when an MVCC range tombstone is encountered. A separate reference to it is
 	// kept in order to release it back to its pool when the scanner is done.
@@ -453,6 +458,7 @@ func (p *pebbleMVCCScanner) get(ctx context.Context) {
 		}
 	}
 
+	p.parentReverse = false
 	p.parent.SeekGE(MVCCKey{Key: p.start})
 	if !p.updateCurrent() {
 		return
@@ -963,6 +969,7 @@ func (p *pebbleMVCCScanner) advanceKeyAtEnd() bool {
 		// Iterating to the next key might have caused the iterator to reach the
 		// end of the key space. If that happens, back up to the very last key.
 		p.peeked = false
+		p.parentReverse = true
 		p.parent.SeekLT(MVCCKey{Key: p.end})
 		if !p.updateCurrent() {
 			return false
@@ -1217,7 +1224,7 @@ func (p *pebbleMVCCScanner) enablePointSynthesis() bool {
 				p.parent.UnsafeKey()))
 		}
 	}
-	pointIter, err := NewPointSynthesizingIterAtParent(p.parent)
+	pointIter, err := NewPointSynthesizingIterAtParent(p.parent, p.parentReverse)
 	if err != nil {
 		p.err = err
 		return false
@@ -1284,6 +1291,7 @@ func (p *pebbleMVCCScanner) iterValid() bool {
 
 // iterSeek seeks to the latest revision of the specified key (or a greater key).
 func (p *pebbleMVCCScanner) iterSeek(key MVCCKey) bool {
+	p.parentReverse = false
 	p.clearPeeked()
 	p.parent.SeekGE(key)
 	return p.updateCurrent()
@@ -1291,6 +1299,7 @@ func (p *pebbleMVCCScanner) iterSeek(key MVCCKey) bool {
 
 // iterSeekReverse seeks to the latest revision of the key before the specified key.
 func (p *pebbleMVCCScanner) iterSeekReverse(key MVCCKey) bool {
+	p.parentReverse = true
 	p.clearPeeked()
 	p.parent.SeekLT(key)
 	if !p.updateCurrent() {
@@ -1309,6 +1318,7 @@ func (p *pebbleMVCCScanner) iterSeekReverse(key MVCCKey) bool {
 
 // iterNext advances to the next MVCC key.
 func (p *pebbleMVCCScanner) iterNext() bool {
+	p.parentReverse = false
 	if p.reverse && p.peeked {
 		// If we have peeked at the previous entry, we need to advance the iterator
 		// twice.
@@ -1334,6 +1344,7 @@ func (p *pebbleMVCCScanner) iterNext() bool {
 
 // iterPrev advances to the previous MVCC Key.
 func (p *pebbleMVCCScanner) iterPrev() bool {
+	p.parentReverse = true
 	if p.peeked {
 		p.peeked = false
 		return p.updateCurrent()
@@ -1348,6 +1359,7 @@ func (p *pebbleMVCCScanner) iterPrev() bool {
 func (p *pebbleMVCCScanner) iterPeekPrev() ([]byte, bool) {
 	if !p.peeked {
 		p.peeked = true
+		p.parentReverse = true
 		// We need to save a copy of the current iterator key and value and adjust
 		// curRawKey, curKey and curValue to point to this saved data. We use a
 		// single buffer for this purpose: savedBuf.
