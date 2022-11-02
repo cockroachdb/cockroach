1acfcc8d512a66eefef148c6f1fbcb1421b62098
adityamaru <adityamaru@gmail.com>
2022-11-02 09:24:48 -0400

    builtins: add `crdb_internal.fingerprint` builtin
    This change adds a `crdb_internal.fingerprint` builtin
    that accepts a `startTime`, `endTime`, `startKey` and `endKey`
    to define the interval the user wants to fingerprint. The builtin
    is powered by sending an ExportRequest with the defined intervals
    but with the `ExportFingerprint` option set to true.
    
    Setting this option on the ExportRequest means that instead of
    writing all point and rangekeys to an SST and sending them back to
    the client, command evaluation will use the newly introduced
    `fingerprintWriter` (#90848) when exporting keys. This writer
    computes an `fnv64` hash of the key/timestamp, value for each point key
    and maintains a running XOR aggregate of all the point keys processed
    as part of the ExportRequest. Rangekeys are not fingerprinted during
    command evaluation, but instead returned to the client in a
    pebble SST. This is because range keys do not have a stable,
    discrete identity and so it is up to the caller to define a deterministic
    ingerprinting scheme across all returned range keys.
    
    The ExportRequest sent as part of this builtin does not set any DistSender
    limit, thereby allowing concurrent execution across ranges. We are not
    concerned about the ExportResponses growing too large since the SSTs
    will only contain rangekeys that should be few in number. If this assumption
    is proved incorrect in the future, we can revisit setting a `TargetBytes`
    to the header of the BatchRequest.
    
    Fixes: #89336
    
    Release note: None

--- pkg/storage/fingerprint_writer.go
+++ pkg/storage/fingerprint_writer.go
@@ -13,11 +13,13 @@ package storage
 import (
 	"context"
 	"hash"
+	"hash/fnv"
 	"io"
 
 	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/util/tracing"
 	"github.com/cockroachdb/errors"
 )
 
@@ -179,4 +181,110 @@ func (f *fingerprintWriter) stripTenantPrefix(key []byte) []byte {
 		return key
 	}
 	return remainder
+}
+
+// FingerprintRangekeys iterates over the provided SSTs, that are expected to
+// contain only rangekeys, and maintains a XOR aggregate of each rangekey's
+// fingerprint.
+func FingerprintRangekeys(
+	ctx context.Context, cs *cluster.Settings, opts MVCCExportFingerprintOptions, ssts [][]byte,
+) (uint64, error) {
+	ctx, sp := tracing.ChildSpan(ctx, "storage.FingerprintRangekeys")
+	defer sp.Finish()
+	_ = ctx // ctx is currently unused, but this new ctx should be used below in the future.
+
+	if len(ssts) == 0 {
+		return 0, nil
+	}
+
+	// Assert that the SSTs do not contain any point keys.
+	//
+	// NB: Combined point/range key iteration is usually a fair bit more expensive
+	// than iterating over them separately.
+	pointKeyIterOpts := IterOptions{
+		KeyTypes:   IterKeyTypePointsOnly,
+		UpperBound: keys.MaxKey,
+	}
+	pointKeyIter, err := NewMultiMemSSTIterator(ssts, false /* verify */, pointKeyIterOpts)
+	if err != nil {
+		return 0, err
+	}
+	defer pointKeyIter.Close()
+	for pointKeyIter.SeekGE(NilKey); ; pointKeyIter.Next() {
+		if valid, err := pointKeyIter.Valid(); !valid || err != nil {
+			if err != nil {
+				return 0, err
+			}
+			break
+		}
+		hasPoint, _ := pointKeyIter.HasPointAndRange()
+		if hasPoint {
+			return 0, errors.AssertionFailedf("unexpected point key; ssts should only contain range keys")
+		}
+	}
+
+	rangeKeyIterOpts := IterOptions{
+		KeyTypes:   IterKeyTypeRangesOnly,
+		LowerBound: keys.MinKey,
+		UpperBound: keys.MaxKey,
+	}
+	var fingerprint uint64
+	iter, err := NewMultiMemSSTIterator(ssts, true /* verify */, rangeKeyIterOpts)
+	if err != nil {
+		return fingerprint, err
+	}
+	defer iter.Close()
+
+	destFile := &MemFile{}
+	fw := makeFingerprintWriter(ctx, fnv.New64(), cs, destFile, opts)
+	defer fw.Close()
+	fingerprintRangeKey := func(stack MVCCRangeKeyStack) (uint64, error) {
+		defer fw.hasher.Reset()
+		if err := fw.hashKey(stack.Bounds.Key); err != nil {
+			return 0, err
+		}
+		if err := fw.hashKey(stack.Bounds.EndKey); err != nil {
+			return 0, err
+		}
+		for _, v := range stack.Versions {
+			fw.timestampBuf = EncodeMVCCTimestampToBuf(fw.timestampBuf, v.Timestamp)
+			if err := fw.hash(fw.timestampBuf); err != nil {
+				return 0, err
+			}
+			mvccValue, ok, err := tryDecodeSimpleMVCCValue(v.Value)
+			if !ok && err == nil {
+				mvccValue, err = decodeExtendedMVCCValue(v.Value)
+			}
+			if err != nil {
+				return 0, errors.Wrapf(err, "decoding mvcc value %s", v.Value)
+			}
+			if err := fw.hashValue(mvccValue.Value.RawBytes); err != nil {
+				return 0, err
+			}
+		}
+		return fw.hasher.Sum64(), nil
+	}
+
+	for iter.SeekGE(MVCCKey{Key: keys.MinKey}); ; iter.Next() {
+		if ok, err := iter.Valid(); err != nil {
+			return fingerprint, err
+		} else if !ok {
+			break
+		}
+		hasPoint, _ := iter.HasPointAndRange()
+		if hasPoint {
+			return fingerprint, errors.AssertionFailedf("unexpected point key; ssts should only contain range keys")
+		}
+		rangekeyFingerprint, err := fingerprintRangeKey(iter.RangeKeys())
+		if err != nil {
+			return fingerprint, err
+		}
+		fw.xorAgg.add(rangekeyFingerprint)
+	}
+
+	if len(destFile.Data()) != 0 {
+		return 0, errors.AssertionFailedf("unexpected data found in destFile")
+	}
+
+	return fw.Finish()
 }
