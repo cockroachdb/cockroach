3a2fc6aaca10b378b98ca8a72b01f9c9219f8bcc
Faizaan Madhani <fzmadhani@gmail.com>
2022-11-03 14:42:59 -0400

    sql: merge partial and full table statistics
    This commit adds support to the table stats
    cache to merge partial statistics with full table
    statistics when updating the stats cache, in an
    effort to keep more accurate table statistics that
    can be used by the optimizer.
    
    Additionally, if forecasting is enabled for the table, these
    merged statistics are used for the forecast.
    
    Epic: CRDB-19449
    
    Release note (sql change): The optimizer will
    now use table statistics that are merged combinations
    of the newest partial statistic and latest full
    statistic collection. And, if forecasting is enabled,
    the merged statistic will be used in the forecast.

---
+++ pkg/sql/stats/merge.go
@@ -0,0 +1,237 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package stats
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/cat"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/eval"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/errors"
+	"github.com/cockroachdb/redact"
+)
+
+// CreateMergedStatistics returns an array of table statistics that
+// are the merged combinations of the latest full statistic and the latest partial
+// statistic for a specific column set. If merging a partial stat with the full
+// stat is not possible, we don't include that statistic as part of the resulting array.
+func CreateMergedStatistics(
+	ctx context.Context, partialStatsList []*TableStatistic, fullStatsList []*TableStatistic,
+) []*TableStatistic {
+	// Iterate through partialTableStats and construct a map
+	// mapping the columnID set to the latest partial table stat.
+	// It relies on the fact that the first partial stat for that
+	// column is the latest one as they are sorted by descending
+	// createdAt time.
+	partialStatsMap := make(map[descpb.ColumnID]*TableStatistic)
+	for _, stat := range partialStatsList {
+		col := stat.ColumnIDs[0]
+		if _, ok := partialStatsMap[col]; !ok {
+			partialStatsMap[col] = stat
+		}
+	}
+	// Map the ColumnIDs to the latest full table statistic,
+	// and map the keys to the number of columns in the set.
+	// It relies on the fact that the first full statistic
+	// is the latest.
+	fullStatsMap := make(map[descpb.ColumnID]*TableStatistic)
+	for _, stat := range fullStatsList {
+		col := stat.ColumnIDs[0]
+		if len(stat.ColumnIDs) == 1 {
+			_, ok := fullStatsMap[col]
+			if !ok {
+				fullStatsMap[col] = stat
+			}
+		}
+	}
+
+	mergedStats := make([]*TableStatistic, 0, len(fullStatsMap))
+	for col, fullStat := range fullStatsMap {
+		var merged *TableStatistic
+		var err error
+		if val, ok := partialStatsMap[col]; ok && val.CreatedAt.After(fullStatsMap[col].CreatedAt) {
+			partialStat := partialStatsMap[col]
+			merged, err = mergeExtremesStatistic(fullStat, partialStat)
+			if err != nil {
+				log.VEventf(ctx, 2, "could not merge statistics for table %v columns %s: %v", fullStat.TableID, redact.Safe(col), err)
+				continue
+			}
+			mergedStats = append(mergedStats, merged)
+		}
+	}
+	return mergedStats
+}
+
+// mergeExtremesStatistic merges a full table statistic with a partial table
+// statistic and returns a new full table statistic. It does this by prepending
+// the partial histogram buckets with UpperBound less than the first bucket of
+// the full histogram and appending the partial histogram buckets with
+// UpperBounds that are greater than the last full histogram bucket to the end
+// of the full histogram. It then recalculates the counts for the new merged
+// statistic, using the new combined histogram. The createdAt time is set to
+// that of the latest partial statistic, and the statistic is named the string
+// assigned to jobspb.MergedStatsName.
+//
+// For example, consider this case:
+// Full Statistic: {row: 14, dist: 9, null: 4, size: 1}
+// CreatedAt: 2022-01-02
+// Histogram (format is: {NumEq, NumRange, DistinctRange, UpperBound}):
+// [{1, 0, 0, 2}, {1, 1, 0, 4}, {1, 1, 0, 6}]
+//
+// Partial Statistic: {row, 19, dist: 3, null: 0, size: 1}
+// CreatedAt: 2022-01-03
+// Histogram: [{2, 0, 0, 0}, {2, 0, 0, 1}, {2, 0, 0, 7}, {2, 0, 0, 8}]
+//
+// Merged Statistic: {row: 29, dist: 18, null: 0, size: 1}
+// CreatedAt: 2022-01-03
+// Histogram: [{2, 0, 0, 0}, {2, 0, 0, 1}, {1, 0, 0, 2}, {1, 1, 0, 4},
+// {1, 1, 0, 6}, {2, 0, 0, 7}, {2, 0, 0, 8}]
+//
+// Alternatively, consider this case where the new values are added at
+// the upper extreme of the index:
+// Full Statistic: {row: 12, dist: 6, null: 0, size: 3}
+// CreatedAt: 2021-02-01
+// Histogram: [{3, 3, 2, 8}, {3, 3, 2, 15}]
+//
+// Partial Statistic: {row: 18, dist: 11, null: 0, size: 8}
+// CreatedAt: 2022-02-02
+// Histogram: [{4, 4, 4, 19}, {5, 5, 5, 21}]
+//
+// Merged Statistic: {row: 30, dist: 17, null: 0, size: 4}
+// CreatedAt: 2022-02-03
+// Histogram: [{3, 3, 2, 8}, {3, 3, 2, 15}, {4, 4, 4, 19}, {5, 5, 5, 21}]
+//
+// In the case where there is no partial histogram, the full statistic
+// is returned but with the created_at time of the partial statistic,
+// with the statistic renamed to the string assigned to.
+// jobspb.MergedStatsName.
+func mergeExtremesStatistic(
+	fullStat *TableStatistic, partialStat *TableStatistic,
+) (*TableStatistic, error) {
+	fullStatColKey := MakeSortedColStatKey(fullStat.ColumnIDs)
+	partialStatColKey := MakeSortedColStatKey(partialStat.ColumnIDs)
+	if fullStatColKey != partialStatColKey {
+		return fullStat, errors.New("column sets for full table statistics and partial table statistics column sets do not match")
+	}
+
+	// Merge the histograms
+	// Currently, since we don't merge multi-column statistics, each
+	// statistic passed through should have a histogram.
+	// TODO (faizaanmadhani): Add support for multi-column partial statistics.
+	fullHistogram := fullStat.Histogram
+	partialHistogram := partialStat.Histogram
+
+	if len(fullHistogram) == 0 {
+		return fullStat, errors.New("the full statistic histogram does not exist")
+	}
+	// If the partial histogram was empty this means that there
+	// were no new values at the extremes. We update the
+	// createdAt time and rename it.
+	if len(partialHistogram) == 0 {
+		fullStat.Name = jobspb.MergedStatsName
+		fullStat.CreatedAt = partialStat.CreatedAt
+		return fullStat, nil
+	}
+
+	mergedHistogram := make([]cat.HistogramBucket, 0, len(fullHistogram)+len(partialHistogram))
+
+	// Remove the NULL bucket from the front
+	// of both histograms if it exists.
+	if fullHistogram[0].UpperBound == tree.DNull {
+		fullHistogram = fullHistogram[1:]
+	}
+	var partialNullCount uint64
+	if partialHistogram[0].UpperBound == tree.DNull {
+		partialNullCount = uint64(partialHistogram[0].NumEq)
+		partialHistogram = partialHistogram[1:]
+	}
+
+	var cmpCtx *eval.Context
+
+	var numOriginalRows uint64
+
+	i := 0
+	j := 0
+	// Merge partial stats to prior full statistics.
+	for i < len(partialHistogram) && j < len(fullHistogram) {
+		if val, err := partialHistogram[i].UpperBound.CompareError(cmpCtx, fullHistogram[j].UpperBound); err == nil {
+			if val == -1 {
+				mergedHistogram = append(mergedHistogram, partialHistogram[i])
+				numOriginalRows += uint64(partialHistogram[i].NumRange + partialHistogram[i].NumEq)
+				i++
+			} else {
+				mergedHistogram = append(mergedHistogram, fullHistogram[j])
+				numOriginalRows += uint64(fullHistogram[j].NumRange + fullHistogram[j].NumEq)
+				j++
+			}
+		} else {
+			return nil, err
+		}
+	}
+
+	// Iterate through the rest of the full histogram and append it.
+	for j < len(fullHistogram) {
+		mergedHistogram = append(mergedHistogram, fullHistogram[j])
+		numOriginalRows += uint64(fullHistogram[j].NumRange + fullHistogram[j].NumEq)
+		j++
+	}
+
+	// iterate through the remaining partial histogram and append it.
+	for i < len(partialHistogram) {
+		mergedHistogram = append(mergedHistogram, partialHistogram[i])
+		numOriginalRows += uint64(partialHistogram[i].NumRange + partialHistogram[i].NumEq)
+		i++
+	}
+
+	var mergedRowCount uint64
+	var mergedDistinctCount uint64
+	mergedNullCount := partialNullCount
+	for _, bucket := range mergedHistogram {
+		mergedRowCount += uint64(bucket.NumEq + bucket.NumRange)
+		mergedDistinctCount += uint64(bucket.DistinctRange)
+		if bucket.NumEq > 0 {
+			mergedDistinctCount += 1
+		}
+	}
+	mergedRowCount += mergedNullCount
+
+	mergedAvgSize := (partialStat.AvgSize*partialStat.RowCount + fullStat.AvgSize*numOriginalRows) / (partialStat.RowCount + numOriginalRows)
+
+	mergedTableStatistic := &TableStatistic{
+		TableStatisticProto: TableStatisticProto{
+			TableID:       fullStat.TableID,
+			StatisticID:   0, // TODO (faizaanmadhani): Add support for SHOW HISTOGRAM.
+			Name:          jobspb.MergedStatsName,
+			ColumnIDs:     fullStat.ColumnIDs,
+			CreatedAt:     partialStat.CreatedAt,
+			RowCount:      mergedRowCount,
+			DistinctCount: mergedDistinctCount,
+			NullCount:     mergedNullCount,
+			AvgSize:       mergedAvgSize,
+		},
+	}
+
+	hist := histogram{
+		buckets: mergedHistogram,
+	}
+	histData, err := hist.toHistogramData(fullStat.HistogramData.ColumnType)
+	if err != nil {
+		return nil, err
+	}
+	mergedTableStatistic.HistogramData = &histData
+	mergedTableStatistic.setHistogramBuckets(hist)
+
+	return mergedTableStatistic, nil
+}
