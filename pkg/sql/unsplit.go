ba69a05b2eaf0ea34b90be7e5a335fc74db4ca3c
Evan Wall <wall@cockroachlabs.com>
2022-11-05 11:05:53 -0400

    multitenant: move UNSPLIT AT tenant check from SQL to KV layer
    fixes #91390
    
    Move the UNSPLIT AT tenant check from the SQL to KV layer to prepare for tenant
    permissions work. This check will eventually be replaced by a tenant
    permissions lookup to determine if the tenant can perform the requested KV
    operation (AdminUnsplit in this case). `Codec.ForSystemTenant()` checks still
    exist in the SQL and will be handled in separate PRs.
    
    Release note: None

--- pkg/sql/unsplit.go
+++ pkg/sql/unsplit.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
+	"github.com/cockroachdb/cockroach/pkg/sql/oppurpose"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
 	"github.com/cockroachdb/errors"
@@ -49,7 +50,7 @@ func (n *unsplitNode) Next(params runParams) (bool, error) {
 		return false, err
 	}
 
-	if err := params.extendedEvalCtx.ExecCfg.DB.AdminUnsplit(params.ctx, rowKey); err != nil {
+	if err := params.extendedEvalCtx.ExecCfg.DB.AdminUnsplit(params.ctx, rowKey, oppurpose.UnsplitManual); err != nil {
 		ctx := params.p.EvalContext().FmtCtx(tree.FmtSimple)
 		row.Format(ctx)
 		return false, errors.Wrapf(err, "could not UNSPLIT AT %s", ctx)
@@ -130,7 +131,7 @@ func (n *unsplitAllNode) Next(params runParams) (bool, error) {
 	rowKey := n.run.keys[0]
 	n.run.keys = n.run.keys[1:]
 
-	if err := params.extendedEvalCtx.ExecCfg.DB.AdminUnsplit(params.ctx, rowKey); err != nil {
+	if err := params.extendedEvalCtx.ExecCfg.DB.AdminUnsplit(params.ctx, rowKey, oppurpose.UnsplitManual); err != nil {
 		return false, errors.Wrapf(err, "could not UNSPLIT AT %s", keys.PrettyPrint(nil /* valDirs */, rowKey))
 	}
 
