cockroachdb/cockroach

commit 2dde736d41367234049ef4dca2b83a5949308e61

roachpb: cleanup speculative leases returned by NotLeaseHolderErrors
Speculative leases are returned as part of NLHEs if the committed lease
is not known, but there is a guess as to who the leaseholder may be.
Previoulsy, there were two ways to return these -- either by populating
just the LeaseHolder field on the NLHE or by populating the Lease field
with an unset sequence number. This patch unifies this behavior, and
going forward, we expect the latter to be the canonical form to
represent speculative leases. To that effect, the LeaseHolder field in
the NLHE proto is prefixed as "deprecated". We should be able to remove
the thing entirely in v23.1.

Release note: None
Release justification: low risk change.

pkg/kv/kvserver/replica_range_lease.go
@@ -1023,10 +1023,26 @@ func newNotLeaseHolderError(
 		if stillMember {
 			err.Lease = new(roachpb.Lease)
 			*err.Lease = l
-			err.LeaseHolder = &err.Lease.Replica
 		}
 	}
 	return err
+}
+
+// newNotLeaseHolderErrorWithSpeculativeLease returns a NotLeaseHolderError
+// initialized with a speculative lease pointing to the supplied replica.
+// A NotLeaseHolderError may be constructed with a speculative lease if the
+// current lease is not known, but the error is being created by guessing who
+// the leaseholder may be.
+func newNotLeaseHolderErrorWithSpeculativeLease(
+	leaseHolder roachpb.ReplicaDescriptor,
+	proposerStoreID roachpb.StoreID,
+	rangeDesc *roachpb.RangeDescriptor,
+	msg string,
+) *roachpb.NotLeaseHolderError {
+	speculativeLease := roachpb.Lease{
+		Replica: leaseHolder,
+	}
+	return newNotLeaseHolderError(speculativeLease, proposerStoreID, rangeDesc, msg)
 }
 
 // newLeaseTransferRejectedBecauseTargetMayNeedSnapshotError return an error

pkg/kv/kvclient/kvcoord/dist_sender.go
@@ -2188,7 +2188,7 @@ func (ds *DistSender) sendToReplicas(
 				ds.metrics.NotLeaseHolderErrCount.Inc(1)
 				// If we got some lease information, we use it. If not, we loop around
 				// and try the next replica.
-				if tErr.Lease != nil || tErr.LeaseHolder != nil {
+				if tErr.Lease != nil || tErr.DeprecatedLeaseHolder != nil {
 					// Update the leaseholder in the range cache. Naively this would also
 					// happen when the next RPC comes back, but we don't want to wait out
 					// the additional RPC latency.
@@ -2196,8 +2196,10 @@ func (ds *DistSender) sendToReplicas(
 					var updatedLeaseholder bool
 					if tErr.Lease != nil {
 						updatedLeaseholder = routing.SyncTokenAndMaybeUpdateCache(ctx, tErr.Lease, &tErr.RangeDesc)
-					} else if tErr.LeaseHolder != nil {
-						updatedLeaseholder = routing.SyncTokenAndMaybeUpdateCacheWithSpeculativeLease(ctx, *tErr.LeaseHolder, &tErr.RangeDesc)
+					} else if tErr.DeprecatedLeaseHolder != nil {
+						updatedLeaseholder = routing.SyncTokenAndMaybeUpdateCacheWithSpeculativeLease(
+							ctx, *tErr.DeprecatedLeaseHolder, &tErr.RangeDesc,
+						)
 					}
 					// Move the new leaseholder to the head of the queue for the next
 					// retry. Note that the leaseholder might not be the one indicated by

pkg/kv/kvclient/kvcoord/dist_sender_test.go
@@ -601,11 +601,13 @@ func TestRetryOnNotLeaseHolderError(t *testing.T) {
 			expLease:       true,
 		},
 		{
+			// TODO(arul): This is only possible in 22.{1,2} mixed version clusters;
+			// remove once we get rid of the LeaseHolder field in 23.1.
 			name: "leaseholder in desc, no lease",
 			nlhe: roachpb.NotLeaseHolderError{
-				RangeID:     testUserRangeDescriptor3Replicas.RangeID,
-				LeaseHolder: &recognizedLeaseHolder,
-				RangeDesc:   testUserRangeDescriptor3Replicas,
+				RangeID:               testUserRangeDescriptor3Replicas.RangeID,
+				DeprecatedLeaseHolder: &recognizedLeaseHolder,
+				RangeDesc:             testUserRangeDescriptor3Replicas,
 			},
 			expLeaseholder: &recognizedLeaseHolder,
 			expLease:       false,
@@ -757,9 +759,8 @@ func TestBackoffOnNotLeaseHolderErrorDuringTransfer(t *testing.T) {
 			}
 			reply.Error = roachpb.NewError(
 				&roachpb.NotLeaseHolderError{
-					Replica:     repls[int(seq)%2],
-					LeaseHolder: &repls[(int(seq)+1)%2],
-					Lease:       lease,
+					Replica: repls[int(seq)%2],
+					Lease:   lease,
 				})
 			return reply, nil
 		}
@@ -840,9 +841,8 @@ func TestNoBackoffOnNotLeaseHolderErrorFromFollowerRead(t *testing.T) {
 		br := ba.CreateReply()
 		if ba.Replica != lease.Replica {
 			br.Error = roachpb.NewError(&roachpb.NotLeaseHolderError{
-				Replica:     ba.Replica,
-				LeaseHolder: &lease.Replica,
-				Lease:       &lease,
+				Replica: ba.Replica,
+				Lease:   &lease,
 			})
 		}
 		return br, nil
@@ -1123,8 +1123,11 @@ func TestDistSenderIgnoresNLHEBasedOnOldRangeGeneration(t *testing.T) {
 					Replica:  roachpb.ReplicaDescriptor{NodeID: 4, StoreID: 4, ReplicaID: 4},
 				}
 			} else {
-				// Speculative lease -- the NLHE only carries LeaseHolder information.
-				nlhe.LeaseHolder = &roachpb.ReplicaDescriptor{NodeID: 4, StoreID: 4, ReplicaID: 4}
+				// Speculative lease -- the NLHE only carries LeaseHolder information
+				// and the sequence number is unset.
+				nlhe.Lease = &roachpb.Lease{
+					Replica: roachpb.ReplicaDescriptor{NodeID: 4, StoreID: 4, ReplicaID: 4},
+				}
 			}
 
 			cachedLease := roachpb.Lease{
@@ -1703,7 +1706,10 @@ func TestEvictCacheOnUnknownLeaseHolder(t *testing.T) {
 		var err error
 		switch count {
 		case 0, 1:
-			err = &roachpb.NotLeaseHolderError{LeaseHolder: &roachpb.ReplicaDescriptor{NodeID: 99, StoreID: 999}}
+			err = &roachpb.NotLeaseHolderError{
+				Lease: &roachpb.Lease{
+					Replica: roachpb.ReplicaDescriptor{NodeID: 99, StoreID: 999}},
+			}
 		case 2:
 			err = roachpb.NewRangeNotFoundError(0, 0)
 		default:
@@ -5173,8 +5179,9 @@ func TestDistSenderNLHEFromUninitializedReplicaDoesNotCauseUnboundedBackoff(t *t
 			// will include a speculative lease that points to a replica that isn't
 			// part of the client's range descriptor. This is only possible in
 			// versions <= 22.1 as NLHE errors from uninitialized replicas no longer
-			// return speculative leases. Effectively, this acts as a mixed
-			// (22.1, 22.2) version test.
+			// return speculative leases by populating the (Deprecated)LeaseHolder
+			// field. Effectively, this acts as a mixed (22.1, 22.2) version test.
+			// TODO(arul): remove the speculative lease version of this test in 23.1.
 
 			clock := hlc.NewClockWithSystemTimeSource(time.Nanosecond /* maxOffset */)
 			rpcContext := rpc.NewInsecureTestingContext(ctx, clock, stopper)
@@ -5217,7 +5224,7 @@ func TestDistSenderNLHEFromUninitializedReplicaDoesNotCauseUnboundedBackoff(t *t
 						RangeDesc: roachpb.RangeDescriptor{},
 					}
 					if returnSpeculativeLease {
-						nlhe.LeaseHolder = &roachpb.ReplicaDescriptor{NodeID: 5, StoreID: 5, ReplicaID: 5}
+						nlhe.DeprecatedLeaseHolder = &roachpb.ReplicaDescriptor{NodeID: 5, StoreID: 5, ReplicaID: 5}
 					}
 					br.Error = roachpb.NewError(nlhe)
 				case 1:

pkg/kv/kvserver/client_raft_test.go
@@ -1282,8 +1282,9 @@ func TestRequestsOnLaggingReplica(t *testing.T) {
 	require.NotNil(t, pErr, "unexpected success")
 	nlhe := pErr.GetDetail().(*roachpb.NotLeaseHolderError)
 	require.NotNil(t, nlhe, "expected NotLeaseholderError, got: %s", pErr)
-	require.NotNil(t, nlhe.LeaseHolder, "expected NotLeaseholderError with a known leaseholder, got: %s", pErr)
-	require.Equal(t, leaderReplicaID, nlhe.LeaseHolder.ReplicaID)
+	require.False(t, nlhe.Lease.Empty())
+	require.NotNil(t, nlhe.Lease.Replica, "expected NotLeaseholderError with a known leaseholder, got: %s", pErr)
+	require.Equal(t, leaderReplicaID, nlhe.Lease.Replica.ReplicaID)
 }
 
 type fakeSnapshotStream struct {

pkg/kv/kvserver/client_replica_test.go
@@ -1617,9 +1617,9 @@ func TestLeaseExpirationBasedRangeTransfer(t *testing.T) {
 	if !ok {
 		t.Fatalf("expected %T, got %s", &roachpb.NotLeaseHolderError{}, pErr)
 	}
-	if !nlhe.LeaseHolder.Equal(&l.replica1Desc) {
+	if !nlhe.Lease.Replica.Equal(&l.replica1Desc) {
 		t.Fatalf("expected lease holder %+v, got %+v",
-			l.replica1Desc, nlhe.LeaseHolder)
+			l.replica1Desc, nlhe.Lease.Replica)
 	}
 
 	// Check that replica1 now has the lease.
@@ -1717,9 +1717,9 @@ func TestLeaseExpirationBasedDrainTransfer(t *testing.T) {
 	if !ok {
 		t.Fatalf("expected %T, got %s", &roachpb.NotLeaseHolderError{}, pErr)
 	}
-	if nlhe.LeaseHolder == nil || !nlhe.LeaseHolder.Equal(&l.replica1Desc) {
+	if nlhe.Lease.Empty() || !nlhe.Lease.Replica.Equal(&l.replica1Desc) {
 		t.Fatalf("expected lease holder %+v, got %+v",
-			l.replica1Desc, nlhe.LeaseHolder)
+			l.replica1Desc, nlhe.Lease.Replica)
 	}
 
 	// Check that replica1 now has the lease.

pkg/kv/kvserver/replica_gossip.go
@@ -144,8 +144,8 @@ func (r *Replica) getLeaseForGossip(ctx context.Context) (bool, *roachpb.Error) 
 				switch e := pErr.GetDetail().(type) {
 				case *roachpb.NotLeaseHolderError:
 					// NotLeaseHolderError means there is an active lease, but only if
-					// the lease holder is set; otherwise, it's likely a timeout.
-					if e.LeaseHolder != nil {
+					// the lease is non-empty; otherwise, it's likely a timeout.
+					if !e.Lease.Empty() {
 						pErr = nil
 					}
 				default:

pkg/kv/kvserver/replica_proposal_buf.go
@@ -1235,12 +1235,14 @@ func (rp *replicaProposer) rejectProposalWithRedirectLocked(
 	storeID := r.store.StoreID()
 	r.store.metrics.LeaseRequestErrorCount.Inc(1)
 	redirectRep, _ /* ok */ := rangeDesc.GetReplicaDescriptorByID(redirectTo)
-	speculativeLease := roachpb.Lease{
-		Replica: redirectRep,
-	}
 	log.VEventf(ctx, 2, "redirecting proposal to node %s; request: %s", redirectRep.NodeID, prop.Request)
-	rp.rejectProposalWithErrLocked(ctx, prop, roachpb.NewError(newNotLeaseHolderError(
-		speculativeLease, storeID, rangeDesc, "refusing to acquire lease on follower")))
+	rp.rejectProposalWithErrLocked(ctx, prop, roachpb.NewError(
+		newNotLeaseHolderErrorWithSpeculativeLease(
+			redirectRep,
+			storeID,
+			rangeDesc,
+			"refusing to acquire lease on follower"),
+	))
 }
 
 func (rp *replicaProposer) rejectProposalWithLeaseTransferRejectedLocked(

pkg/kv/kvserver/replica_test.go
@@ -739,7 +739,7 @@ func TestBehaviorDuringLeaseTransfer(t *testing.T) {
 			require.Error(t, err)
 			var lErr *roachpb.NotLeaseHolderError
 			require.True(t, errors.As(err, &lErr))
-			require.Equal(t, secondReplica.StoreID, lErr.LeaseHolder.StoreID)
+			require.Equal(t, secondReplica.StoreID, lErr.Lease.Replica.StoreID)
 		} else {
 			// Check that the replica doesn't use its lease, even though there's
 			// no longer a transfer in progress. This is because, even though

commit 15006b8f97f812742f4ce64797c5bc59d43b2332

kvserver: return DeprecatedLeaseHolder field in NLHEs
v22.1 binaries assume that the leaseholder is unknown when logging
NLHE errors if the (Deprecated)LeaseHolder field is unset -- regardless
of if the Lease is set or not. We broke this logging in 2dde736
(for mixed version clusters) when we stopped shipping back leaseholder
information (in favour of only shipping lease information) on NLHEs.
This patch fixes this by populating the (Deprecated)LeaseHolder field
when constructing NLHEs.

Release note: None

pkg/kv/kvserver/replica_range_lease.go
@@ -1036,6 +1036,10 @@ func newNotLeaseHolderError(
 		if stillMember {
 			err.Lease = new(roachpb.Lease)
 			*err.Lease = l
+			// TODO(arul): We only need to return this for the 22.1 <-> 22.2 mixed
+			// version state, as v22.1 use this field to log NLHE messages. We can
+			// get rid of this, and the field, in v23.1.
+			err.DeprecatedLeaseHolder = &err.Lease.Replica
 		}
 	}
 	return err