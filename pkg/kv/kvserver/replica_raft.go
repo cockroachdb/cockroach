8087255148e3c66fef79f01e90d0c4a97342dc35
Pavel Kalinnikov <pavel@cockroachlabs.com>
2022-11-11 13:35:06 +0000

    kvserver: factor out storing log entries in Replica
    Release note: None

--- pkg/kv/kvserver/replica_raft.go
+++ pkg/kv/kvserver/replica_raft.go
@@ -927,77 +927,10 @@ func (r *Replica) handleRaftReadyRaftMuLocked(
 	r.traceMessageSends(msgApps, "sending msgApp")
 	r.sendRaftMessagesRaftMuLocked(ctx, msgApps, pausedFollowers)
 
-	// Use a more efficient write-only batch because we don't need to do any
-	// reads from the batch. Any reads are performed on the underlying DB.
-	batch := r.store.Engine().NewUnindexedBatch(false /* writeOnly */)
-	defer batch.Close()
-
 	prevLastIndex := state.lastIndex
-	if len(rd.Entries) > 0 {
-		stats.tAppendBegin = timeutil.Now()
-		// All of the entries are appended to distinct keys, returning a new
-		// last index.
-		thinEntries, numSideloaded, sideLoadedEntriesSize, otherEntriesSize, err := maybeSideloadEntries(ctx, rd.Entries, r.raftMu.sideloaded)
-		if err != nil {
-			const expl = "during sideloading"
-			return stats, expl, errors.Wrap(err, expl)
-		}
-		state.byteSize += sideLoadedEntriesSize
-		if state, err = logAppend(
-			ctx, r.raftMu.stateLoader.RaftLogPrefix(), batch, state, thinEntries,
-		); err != nil {
-			const expl = "during append"
-			return stats, expl, errors.Wrap(err, expl)
-		}
-		stats.appendedRegularCount += len(thinEntries) - numSideloaded
-		stats.appendedRegularBytes += otherEntriesSize
-		stats.appendedSideloadedCount += numSideloaded
-		stats.appendedSideloadedBytes += sideLoadedEntriesSize
-		stats.tAppendEnd = timeutil.Now()
-	}
-
-	if !raft.IsEmptyHardState(rd.HardState) {
-		if !r.IsInitialized() && rd.HardState.Commit != 0 {
-			log.Fatalf(ctx, "setting non-zero HardState.Commit on uninitialized replica %s. HS=%+v", r, rd.HardState)
-		}
-		// NB: Note that without additional safeguards, it's incorrect to write
-		// the HardState before appending rd.Entries. When catching up, a follower
-		// will receive Entries that are immediately Committed in the same
-		// Ready. If we persist the HardState but happen to lose the Entries,
-		// assertions can be tripped.
-		//
-		// We have both in the same batch, so there's no problem. If that ever
-		// changes, we must write and sync the Entries before the HardState.
-		if err := r.raftMu.stateLoader.SetHardState(ctx, batch, rd.HardState); err != nil {
-			const expl = "during setHardState"
-			return stats, expl, errors.Wrap(err, expl)
-		}
-	}
-	// Synchronously commit the batch with the Raft log entries and Raft hard
-	// state as we're promising not to lose this data.
-	//
-	// Note that the data is visible to other goroutines before it is synced to
-	// disk. This is fine. The important constraints are that these syncs happen
-	// before Raft messages are sent and before the call to RawNode.Advance. Our
-	// regular locking is sufficient for this and if other goroutines can see the
-	// data early, that's fine. In particular, snapshots are not a problem (I
-	// think they're the only thing that might access log entries or HardState
-	// from other goroutines). Snapshots do not include either the HardState or
-	// uncommitted log entries, and even if they did include log entries that
-	// were not persisted to disk, it wouldn't be a problem because raft does not
-	// infer the that entries are persisted on the node that sends a snapshot.
-	stats.tPebbleCommitBegin = timeutil.Now()
-	stats.pebbleBatchBytes = int64(batch.Len())
-	sync := rd.MustSync && !disableSyncRaftLog.Get(&r.store.cfg.Settings.SV)
-	if err := batch.Commit(sync); err != nil {
-		const expl = "while committing batch"
-		return stats, expl, errors.Wrap(err, expl)
-	}
-	stats.sync = sync
-	stats.tPebbleCommitEnd = timeutil.Now()
-	if rd.MustSync {
-		r.store.metrics.RaftLogCommitLatency.RecordValue(
-			stats.tPebbleCommitEnd.Sub(stats.tPebbleCommitBegin).Nanoseconds())
+	if state, err = r.storeLogEntries(ctx, state, rd, &stats); err != nil {
+		const expl = "while storing log entries"
+		return stats, expl, err
 	}
 
 	if len(rd.Entries) > 0 {
@@ -1144,6 +1077,91 @@ func (r *Replica) handleRaftReadyRaftMuLocked(
 	// get blocked.
 	r.updateProposalQuotaRaftMuLocked(ctx, lastLeaderID)
 	return stats, "", nil
+}
+
+// storeLogEntries persists newly appended raft Entries to the log storage.
+// Accepts the state of the log before the operation, returns the state after.
+// Persists HardState atomically with, or strictly after Entries.
+//
+// TODO(pavelkalinnikov): raft.Ready combines multiple roles. For a stricter
+// separation between log and state storage, introduce a log-specific Ready,
+// consisting of committed Entries, HardState, and MustSync.
+func (r *Replica) storeLogEntries(
+	ctx context.Context, state raftLogState, rd raft.Ready, stats *handleRaftReadyStats,
+) (raftLogState, error) {
+	// TODO(pavelkalinnikov): Doesn't this comment contradict the code?
+	// Use a more efficient write-only batch because we don't need to do any
+	// reads from the batch. Any reads are performed on the underlying DB.
+	batch := r.store.Engine().NewUnindexedBatch(false /* writeOnly */)
+	defer batch.Close()
+
+	if len(rd.Entries) > 0 {
+		stats.tAppendBegin = timeutil.Now()
+		// All of the entries are appended to distinct keys, returning a new
+		// last index.
+		thinEntries, numSideloaded, sideLoadedEntriesSize, otherEntriesSize, err := maybeSideloadEntries(ctx, rd.Entries, r.raftMu.sideloaded)
+		if err != nil {
+			const expl = "during sideloading"
+			return raftLogState{}, errors.Wrap(err, expl)
+		}
+		state.byteSize += sideLoadedEntriesSize
+		if state, err = logAppend(
+			ctx, r.raftMu.stateLoader.RaftLogPrefix(), batch, state, thinEntries,
+		); err != nil {
+			const expl = "during append"
+			return raftLogState{}, errors.Wrap(err, expl)
+		}
+		stats.appendedRegularCount += len(thinEntries) - numSideloaded
+		stats.appendedRegularBytes += otherEntriesSize
+		stats.appendedSideloadedCount += numSideloaded
+		stats.appendedSideloadedBytes += sideLoadedEntriesSize
+		stats.tAppendEnd = timeutil.Now()
+	}
+
+	if !raft.IsEmptyHardState(rd.HardState) {
+		if !r.IsInitialized() && rd.HardState.Commit != 0 {
+			log.Fatalf(ctx, "setting non-zero HardState.Commit on uninitialized replica %s. HS=%+v", r, rd.HardState)
+		}
+		// NB: Note that without additional safeguards, it's incorrect to write
+		// the HardState before appending rd.Entries. When catching up, a follower
+		// will receive Entries that are immediately Committed in the same
+		// Ready. If we persist the HardState but happen to lose the Entries,
+		// assertions can be tripped.
+		//
+		// We have both in the same batch, so there's no problem. If that ever
+		// changes, we must write and sync the Entries before the HardState.
+		if err := r.raftMu.stateLoader.SetHardState(ctx, batch, rd.HardState); err != nil {
+			const expl = "during setHardState"
+			return raftLogState{}, errors.Wrap(err, expl)
+		}
+	}
+	// Synchronously commit the batch with the Raft log entries and Raft hard
+	// state as we're promising not to lose this data.
+	//
+	// Note that the data is visible to other goroutines before it is synced to
+	// disk. This is fine. The important constraints are that these syncs happen
+	// before Raft messages are sent and before the call to RawNode.Advance. Our
+	// regular locking is sufficient for this and if other goroutines can see the
+	// data early, that's fine. In particular, snapshots are not a problem (I
+	// think they're the only thing that might access log entries or HardState
+	// from other goroutines). Snapshots do not include either the HardState or
+	// uncommitted log entries, and even if they did include log entries that
+	// were not persisted to disk, it wouldn't be a problem because raft does not
+	// infer the that entries are persisted on the node that sends a snapshot.
+	stats.tPebbleCommitBegin = timeutil.Now()
+	stats.pebbleBatchBytes = int64(batch.Len())
+	sync := rd.MustSync && !disableSyncRaftLog.Get(&r.store.cfg.Settings.SV)
+	if err := batch.Commit(sync); err != nil {
+		const expl = "while committing batch"
+		return raftLogState{}, errors.Wrap(err, expl)
+	}
+	stats.sync = sync
+	stats.tPebbleCommitEnd = timeutil.Now()
+	if rd.MustSync {
+		r.store.metrics.RaftLogCommitLatency.RecordValue(
+			stats.tPebbleCommitEnd.Sub(stats.tPebbleCommitBegin).Nanoseconds())
+	}
+	return state, nil
 }
 
 // splitMsgApps splits the Raft message slice into two slices, one containing
