cockroachdb/cockroach

commit b555e2b7685b39f3a5a9767de5a45b0fada1b058

insights: pool memory usage in insights layer
These allocation were 0.7% of all allocations in a ycsb/A run.

Release note: None

pkg/sql/sqlstats/insights/registry.go
@@ -12,9 +12,11 @@ package insights
 
 import (
 	"context"
+	"sync"
 
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql/clusterunique"
+	"github.com/cockroachdb/cockroach/pkg/util"
 	"github.com/cockroachdb/cockroach/pkg/util/cache"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 )
@@ -31,7 +33,7 @@ type lockingRegistry struct {
 	// before enabling the insights subsystem by default.
 	mu struct {
 		syncutil.RWMutex
-		statements map[clusterunique.ID][]*Statement
+		statements map[clusterunique.ID]*statementBuf
 		insights   *cache.OrderedCache
 	}
 }
@@ -45,48 +47,77 @@ func (r *lockingRegistry) ObserveStatement(sessionID clusterunique.ID, statement
 	}
 	r.mu.Lock()
 	defer r.mu.Unlock()
-	r.mu.statements[sessionID] = append(r.mu.statements[sessionID], statement)
+	b, ok := r.mu.statements[sessionID]
+	if !ok {
+		b = statementsBufPool.Get().(*statementBuf)
+		r.mu.statements[sessionID] = b
+	}
+	b.append(statement)
+}
+
+type statementBuf []*Statement
+
+func (b *statementBuf) append(statement *Statement) {
+	*b = append(*b, statement)
+}
+
+func (b *statementBuf) release() {
+	for i, n := 0, len(*b); i < n; i++ {
+		(*b)[i] = nil
+	}
+	*b = (*b)[:0]
+	statementsBufPool.Put(b)
+}
+
+var statementsBufPool = sync.Pool{
+	New: func() interface{} {
+		return new(statementBuf)
+	},
+}
+
+var insightPool = sync.Pool{
+	New: func() interface{} {
+		return new(Insight)
+	},
 }
 
 func (r *lockingRegistry) ObserveTransaction(sessionID clusterunique.ID, transaction *Transaction) {
 	if !r.enabled() {
 		return
 	}
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	statements := r.mu.statements[sessionID]
-	delete(r.mu.statements, sessionID)
+	statements := r.popSessionStatements(sessionID)
+	defer statements.release()
 
-	slowStatements := make(map[clusterunique.ID]struct{})
-	for _, s := range statements {
+	var slowStatements util.FastIntSet
+	for i, s := range *statements {
 		if r.detector.isSlow(s) {
-			slowStatements[s.ID] = struct{}{}
+			slowStatements.Add(i)
 		}
 	}
-
-	if len(slowStatements) > 0 {
-		for _, s := range statements {
-			var p Problem
-			var c []Cause
-			if _, ok := slowStatements[s.ID]; ok {
-				switch s.Status {
-				case Statement_Completed:
-					p = Problem_SlowExecution
-					c = r.causes.examine(s)
-				case Statement_Failed:
-					// Note that we'll be building better failure support for 23.1.
-					// For now, we only mark failed statements that were also slow.
-					p = Problem_FailedExecution
-				}
-			}
-			r.mu.insights.Add(s.ID, &Insight{
-				Session:     &Session{ID: sessionID},
-				Transaction: transaction,
-				Statement:   s,
-				Problem:     p,
-				Causes:      c,
-			})
+	if slowStatements.Empty() {
+		return
+	}
+	// Note that we'll record insights for every statement, not just for
+	// the slow ones.
+	for i, s := range *statements {
+		insight := insightPool.Get().(*Insight)
+		*insight = Insight{
+			Session:     Session{ID: sessionID},
+			Transaction: transaction,
+			Statement:   s,
 		}
+		if slowStatements.Contains(i) {
+			switch s.Status {
+			case Statement_Completed:
+				insight.Problem = Problem_SlowExecution
+				insight.Causes = r.causes.examine(insight.Causes, s)
+			case Statement_Failed:
+				// Note that we'll be building better failure support for 23.1.
+				// For now, we only mark failed statements that were also slow.
+				insight.Problem = Problem_FailedExecution
+			}
+		}
+		r.addInsight(s.ID, insight)
 	}
 }
 
@@ -111,18 +142,38 @@ func (r *lockingRegistry) enabled() bool {
 	return r.detector.enabled()
 }
 
+func (r *lockingRegistry) popSessionStatements(sessionID clusterunique.ID) *statementBuf {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	statements := r.mu.statements[sessionID]
+	delete(r.mu.statements, sessionID)
+	return statements
+}
+
+func (r *lockingRegistry) addInsight(id clusterunique.ID, insight *Insight) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.mu.insights.Add(id, insight)
+}
+
 func newRegistry(st *cluster.Settings, detector detector) *lockingRegistry {
 	config := cache.Config{
 		Policy: cache.CacheFIFO,
 		ShouldEvict: func(size int, key, value interface{}) bool {
 			return int64(size) > ExecutionInsightsCapacity.Get(&st.SV)
 		},
+		OnEvicted: func(_, value interface{}) {
+			insight := value.(*Insight)
+			insight.Causes = insight.Causes[:0]
+			*insight = Insight{Causes: insight.Causes}
+			insightPool.Put(insight)
+		},
 	}
 	r := &lockingRegistry{
 		detector: detector,
 		causes:   &causes{st: st},
 	}
-	r.mu.statements = make(map[clusterunique.ID][]*Statement)
+	r.mu.statements = make(map[clusterunique.ID]*statementBuf)
 	r.mu.insights = cache.NewOrderedCache(config)
 	return r
 }

pkg/sql/sqlstats/insights/registry_test.go
@@ -28,7 +28,7 @@ import (
 func TestRegistry(t *testing.T) {
 	ctx := context.Background()
 
-	session := &Session{ID: clusterunique.IDFromBytes([]byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))}
+	session := Session{ID: clusterunique.IDFromBytes([]byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))}
 	transaction := &Transaction{ID: uuid.FastMakeV4()}
 	statement := &Statement{
 		ID:               clusterunique.IDFromBytes([]byte("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")),
@@ -136,7 +136,7 @@ func TestRegistry(t *testing.T) {
 	})
 
 	t.Run("buffering statements per session", func(t *testing.T) {
-		otherSession := &Session{ID: clusterunique.IDFromBytes([]byte("cccccccccccccccccccccccccccccccc"))}
+		otherSession := Session{ID: clusterunique.IDFromBytes([]byte("cccccccccccccccccccccccccccccccc"))}
 		otherTransaction := &Transaction{ID: uuid.FastMakeV4()}
 		otherStatement := &Statement{
 			ID:               clusterunique.IDFromBytes([]byte("dddddddddddddddddddddddddddddddd")),

pkg/sql/sqlstats/insights/BUILD.bazel
@@ -21,6 +21,7 @@ go_library(
         "//pkg/settings/cluster",
         "//pkg/sql/clusterunique",
         "//pkg/sql/contention/contentionutils",
+        "//pkg/util",
         "//pkg/util/cache",
         "//pkg/util/metric",
         "//pkg/util/quantile",

pkg/sql/sqlstats/insights/causes.go
@@ -16,7 +16,10 @@ type causes struct {
 	st *cluster.Settings
 }
 
-func (c *causes) examine(stmt *Statement) (result []Cause) {
+// examine will append all causes of the statement's problems to buf and
+// return the result. Buf allows the slice to be pooled.
+func (c *causes) examine(buf []Cause, stmt *Statement) (result []Cause) {
+	result = buf
 	if len(stmt.IndexRecommendations) > 0 {
 		result = append(result, Cause_SuboptimalPlan)
 	}
@@ -29,5 +32,5 @@ func (c *causes) examine(stmt *Statement) (result []Cause) {
 		result = append(result, Cause_HighRetryCount)
 	}
 
-	return
+	return result
 }

pkg/sql/sqlstats/insights/causes_test.go
@@ -56,7 +56,7 @@ func TestCauses(t *testing.T) {
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			require.ElementsMatch(t, tc.causes, p.examine(tc.statement))
+			require.ElementsMatch(t, tc.causes, p.examine(nil /* buf */, tc.statement))
 		})
 	}
 }

pkg/sql/sqlstats/insights/ingester.go
@@ -12,6 +12,7 @@ package insights
 
 import (
 	"context"
+	"sync"
 	"sync/atomic"
 	"time"
 
@@ -47,7 +48,11 @@ var _ Provider = &concurrentBufferIngester{}
 // Performance was deemed acceptable under 10,000 concurrent goroutines.
 const bufferSize = 8192
 
-type eventBuffer [bufferSize]*event
+type eventBuffer [bufferSize]event
+
+var eventBufferPool = sync.Pool{
+	New: func() interface{} { return new(eventBuffer) },
+}
 
 type event struct {
 	sessionID   clusterunique.ID
@@ -64,7 +69,8 @@ func (i *concurrentBufferIngester) Start(ctx context.Context, stopper *stop.Stop
 		for {
 			select {
 			case events := <-i.eventBufferCh:
-				i.ingest(events)
+				i.ingest(events) // note that injest clears the buffer
+				eventBufferPool.Put(events)
 			case <-stopper.ShouldQuiesce():
 				atomic.StoreUint64(&i.running, 0)
 				return
@@ -90,10 +96,10 @@ func (i *concurrentBufferIngester) Start(ctx context.Context, stopper *stop.Stop
 }
 
 func (i *concurrentBufferIngester) ingest(events *eventBuffer) {
-	for _, e := range events {
+	for idx, e := range events {
 		// Because an eventBuffer is a fixed-size array, rather than a slice,
 		// we do not know how full it is until we hit a nil entry.
-		if e == nil {
+		if e == (event{}) {
 			break
 		}
 		if e.statement != nil {
@@ -101,6 +107,7 @@ func (i *concurrentBufferIngester) ingest(events *eventBuffer) {
 		} else {
 			i.registry.ObserveTransaction(e.sessionID, e.transaction)
 		}
+		events[idx] = event{}
 	}
 }
 
@@ -125,7 +132,7 @@ func (i *concurrentBufferIngester) ObserveStatement(
 		return
 	}
 	i.guard.AtomicWrite(func(writerIdx int64) {
-		i.guard.eventBuffer[writerIdx] = &event{
+		i.guard.eventBuffer[writerIdx] = event{
 			sessionID: sessionID,
 			statement: statement,
 		}
@@ -139,7 +146,7 @@ func (i *concurrentBufferIngester) ObserveTransaction(
 		return
 	}
 	i.guard.AtomicWrite(func(writerIdx int64) {
-		i.guard.eventBuffer[writerIdx] = &event{
+		i.guard.eventBuffer[writerIdx] = event{
 			sessionID:   sessionID,
 			transaction: transaction,
 		}
@@ -158,7 +165,7 @@ func newConcurrentBufferIngester(registry *lockingRegistry) *concurrentBufferIng
 		registry:      registry,
 	}
 
-	i.guard.eventBuffer = &eventBuffer{}
+	i.guard.eventBuffer = eventBufferPool.Get().(*eventBuffer)
 	i.guard.ConcurrentBufferGuard = contentionutils.NewConcurrentBufferGuard(
 		func() int64 {
 			return bufferSize
@@ -167,7 +174,7 @@ func newConcurrentBufferIngester(registry *lockingRegistry) *concurrentBufferIng
 			if atomic.LoadUint64(&i.running) == 1 {
 				i.eventBufferCh <- i.guard.eventBuffer
 			}
-			i.guard.eventBuffer = &eventBuffer{}
+			i.guard.eventBuffer = eventBufferPool.Get().(*eventBuffer)
 		},
 	)
 	return i

pkg/sql/sqlstats/insights/ingester_test.go
@@ -123,7 +123,7 @@ func TestIngester_Disabled(t *testing.T) {
 	writer := ingester.Writer(false /* internal */)
 	writer.ObserveStatement(clusterunique.ID{}, &Statement{})
 	writer.ObserveTransaction(clusterunique.ID{}, &Transaction{})
-	require.Nil(t, ingester.guard.eventBuffer[0])
+	require.Equal(t, event{}, ingester.guard.eventBuffer[0])
 }
 
 func TestIngester_WriterIgnoresInternalExecutorObservations(t *testing.T) {
@@ -131,7 +131,7 @@ func TestIngester_WriterIgnoresInternalExecutorObservations(t *testing.T) {
 	writer := ingester.Writer(true /* internal */)
 	writer.ObserveStatement(clusterunique.ID{}, &Statement{})
 	writer.ObserveTransaction(clusterunique.ID{}, &Transaction{})
-	require.Nil(t, ingester.guard.eventBuffer[0])
+	require.Equal(t, event{}, ingester.guard.eventBuffer[0])
 }
 
 func TestIngester_DoesNotBlockWhenReceivingManyObservationsAfterShutdown(t *testing.T) {

pkg/sql/sqlstats/insights/insights.proto
@@ -103,7 +103,7 @@ message Statement {
 }
 
 message Insight {
-  Session session = 1;
+  Session session = 1 [(gogoproto.nullable) = false];
   Transaction transaction = 2;
   Statement statement = 3;
 

commit 9fd8749893c7b74034397318a128e4bed1ac60bc

insights: fix a nil pointer when observing txn with no stmts
The bug was introduced in b555e2b7685b39f3a5a9767de5a45b0fada1b058.

Release note: None

pkg/sql/sqlstats/insights/registry.go
@@ -66,7 +66,10 @@ func (r *lockingRegistry) ObserveTransaction(sessionID clusterunique.ID, transac
 	if !r.enabled() {
 		return
 	}
-	statements := r.statements[sessionID]
+	statements, ok := r.statements[sessionID]
+	if !ok {
+		return
+	}
 	delete(r.statements, sessionID)
 	defer statements.release()
 

pkg/sql/sqlstats/insights/registry_test.go
@@ -218,4 +218,10 @@ func TestRegistry(t *testing.T) {
 
 		require.Equal(t, expected, actual)
 	})
+
+	t.Run("txn with no stmts", func(t *testing.T) {
+		st := cluster.MakeTestingClusterSettings()
+		registry := newRegistry(st, &latencyThresholdDetector{st: st}, newStore(st))
+		require.NotPanics(t, func() { registry.ObserveTransaction(session.ID, transaction) })
+	})
 }