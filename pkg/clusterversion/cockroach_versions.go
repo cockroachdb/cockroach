6faf2b9638d21c0be2cfef48bd4211b82687497f
Celia La <celia@cockroachlabs.com>
2022-11-13 09:03:34 -0800

    clusterversion: remove SystemExternalConnectionsTable
    Removing gate introduced in 3d3b537a7c6.
    
    Release note: None.

--- pkg/clusterversion/cockroach_versions.go
+++ pkg/clusterversion/cockroach_versions.go
@@ -211,8 +211,6 @@ const (
 	// V22_2AlterSystemSQLInstancesAddLocality adds a locality column to the
 	// system.sql_instances table.
 	V22_2AlterSystemSQLInstancesAddLocality
-	// V22_2SystemExternalConnectionsTable adds system.external_connections table.
-	V22_2SystemExternalConnectionsTable
 
 	// V22_2 is CockroachDB v22.2. It's used for all v22.2.x patch releases.
 	V22_2
@@ -336,10 +334,6 @@ var rawVersionsSingleton = keyedVersions{
 	{
 		Key:     V22_2AlterSystemSQLInstancesAddLocality,
 		Version: roachpb.Version{Major: 22, Minor: 1, Internal: 28},
-	},
-	{
-		Key:     V22_2SystemExternalConnectionsTable,
-		Version: roachpb.Version{Major: 22, Minor: 1, Internal: 30},
 	},
 	{
 		Key:     V22_2,
