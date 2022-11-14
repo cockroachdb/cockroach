34cc6c2b61b95a9eaacc147a5e7317f235aa34fa
Yahor Yuzefovich <yahor@cockroachlabs.com>
2022-11-14 11:40:34 -0800

    sql: add tracing span on client side of SetupFlow RPC
    This commit restores the creation of the tracing span for the client
    side of the SetupFlow RPC when it is issued in parallel (by the DistSQL
    worker). This was lost in a recent refactor.
    
    Epic: None
    
    Release note: None

--- pkg/sql/distsql_running.go
+++ pkg/sql/distsql_running.go
@@ -474,11 +474,17 @@ func (dsp *DistSQLPlanner) setupFlows(
 	//   the tracing span;
 	// - now the client-side goroutine of the RPC is scheduled, and it attempts
 	//   to use the span from the context, but it has already been finished.
-	//
-	// We still want to be able to cancel the RPCs when either the local flow
-	// finishes or the local node is quiescing, so we derive a separate context
-	// with the cancellation ability.
-	runnerCtx, cancelRunnerCtx := dsp.stopper.WithCancelOnQuiesce(context.Background())
+	runnerCtx, cancelRunnerCtx := context.WithCancel(origCtx)
+	var runnerSpan *tracing.Span
+	// This span is necessary because it can outlive its parent.
+	runnerCtx, runnerSpan = evalCtx.Tracer.StartSpanCtx(
+		runnerCtx, "setup-flow-async", /* operationName*/
+		tracing.WithParent(tracing.SpanFromContext(origCtx)),
+	)
+	runnerCleanup := func() {
+		cancelRunnerCtx()
+		runnerSpan.Finish()
+	}
 	for nodeID, flowSpec := range flows {
 		if nodeID == thisNodeID {
 			// Skip this node.
@@ -500,12 +506,11 @@ func (dsp *DistSQLPlanner) setupFlows(
 		case dsp.runnerCoordinator.runnerChan <- runReq:
 			usedWorker = true
 		default:
-			// We can just use the "parent" context since we're executing the
-			// request in a blocking fashion in the current goroutine. This
-			// allows for the cancellation of the "parent" context be noticed
-			// sooner.
-			runReq.ctx = origCtx
+			// Use the context of the local flow since we're executing this
+			// SetupFlow RPC synchronously.
+			runReq.ctx = ctx
 			if err = runReq.run(); err != nil {
+				runnerCleanup()
 				return ctx, flow, err
 			}
 		}
@@ -514,6 +519,7 @@ func (dsp *DistSQLPlanner) setupFlows(
 	if !usedWorker {
 		// We executed all SetupFlow RPCs in the current goroutine, and all RPCs
 		// succeeded.
+		runnerCleanup()
 		return ctx, flow, nil
 	}
 
@@ -544,16 +550,18 @@ func (dsp *DistSQLPlanner) setupFlows(
 		cancelRunnerCtx()
 	})
 	_ = dsp.stopper.RunAsyncTask(origCtx, "distsql-remote-flows-setup-listener", func(ctx context.Context) {
+		defer runnerSpan.Finish()
+		var seenError bool
 		for i := 0; i < len(flows)-1; i++ {
 			res := <-resultChan
-			if res.err != nil {
+			if res.err != nil && !seenError {
+				seenError = true
 				// The setup of at least one remote flow failed.
 				cleanupCalledMu.Lock()
-				defer cleanupCalledMu.Unlock()
-				if cleanupCalledMu.called {
-					// We no longer care about the error nor do we need to
-					// cancel the flow.
-					return
+				skipCancel := cleanupCalledMu.called
+				cleanupCalledMu.Unlock()
+				if skipCancel {
+					continue
 				}
 				// First, we update the DistSQL receiver with the error to be
 				// returned to the client eventually.
@@ -565,9 +573,6 @@ func (dsp *DistSQLPlanner) setupFlows(
 				recv.setErrorWithoutStatusUpdate(res.err)
 				// Now explicitly cancel the local flow.
 				flow.Cancel()
-				// resultChan is buffered, so we can just ignore the remaining
-				// results of the RPCs.
-				return
 			}
 		}
 	})
