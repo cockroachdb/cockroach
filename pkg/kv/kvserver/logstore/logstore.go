fd52e56da48be15abc7761a3878e7696e9caf9dc
Pavel Kalinnikov <pavel@cockroachlabs.com>
2022-11-15 22:20:52 +0000

    kvserver,logstore: move LogStore type to logstore
    Release note: None

--- pkg/kv/kvserver/logstore/logstore.go
+++ pkg/kv/kvserver/logstore/logstore.go
@@ -16,12 +16,27 @@ import (
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/stateloader"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/settings"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/storage"
 	"github.com/cockroachdb/cockroach/pkg/storage/enginepb"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/util/metric"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/cockroachdb/errors"
 	"go.etcd.io/etcd/raft/v3"
 	"go.etcd.io/etcd/raft/v3/raftpb"
+)
+
+var disableSyncRaftLog = settings.RegisterBoolSetting(
+	settings.TenantWritable,
+	"kv.raft_log.disable_synchronization_unsafe",
+	"set to true to disable synchronization on Raft log writes to persistent storage. "+
+		"Setting to true risks data loss or data corruption on server crashes. "+
+		"The setting is meant for internal testing only and SHOULD NOT be used in production.",
+	false,
 )
 
 // Ready contains the log entries and state to be saved to stable storage. This
@@ -67,6 +82,97 @@ type AppendStats struct {
 	PebbleBytes int64
 
 	Sync bool
+}
+
+// Metrics contains metrics specific to the log storage.
+type Metrics struct {
+	RaftLogCommitLatency *metric.Histogram
+}
+
+// LogStore is a stub of a separated Raft log storage.
+type LogStore struct {
+	Engine      storage.Engine
+	Sideload    SideloadStorage
+	StateLoader stateloader.StateLoader
+	Settings    *cluster.Settings
+	Metrics     Metrics
+}
+
+// StoreEntries persists newly appended Raft log Entries to the log storage.
+// Accepts the state of the log before the operation, returns the state after.
+// Persists HardState atomically with, or strictly after Entries.
+func (s *LogStore) StoreEntries(
+	ctx context.Context, state RaftState, rd Ready, stats *AppendStats,
+) (RaftState, error) {
+	// TODO(pavelkalinnikov): Doesn't this comment contradict the code?
+	// Use a more efficient write-only batch because we don't need to do any
+	// reads from the batch. Any reads are performed on the underlying DB.
+	batch := s.Engine.NewUnindexedBatch(false /* writeOnly */)
+	defer batch.Close()
+
+	if len(rd.Entries) > 0 {
+		stats.Begin = timeutil.Now()
+		// All of the entries are appended to distinct keys, returning a new
+		// last index.
+		thinEntries, numSideloaded, sideLoadedEntriesSize, otherEntriesSize, err := MaybeSideloadEntries(ctx, rd.Entries, s.Sideload)
+		if err != nil {
+			const expl = "during sideloading"
+			return RaftState{}, errors.Wrap(err, expl)
+		}
+		state.ByteSize += sideLoadedEntriesSize
+		if state, err = LogAppend(
+			ctx, s.StateLoader.RaftLogPrefix(), batch, state, thinEntries,
+		); err != nil {
+			const expl = "during append"
+			return RaftState{}, errors.Wrap(err, expl)
+		}
+		stats.RegularEntries += len(thinEntries) - numSideloaded
+		stats.RegularBytes += otherEntriesSize
+		stats.SideloadedEntries += numSideloaded
+		stats.SideloadedBytes += sideLoadedEntriesSize
+		stats.End = timeutil.Now()
+	}
+
+	if !raft.IsEmptyHardState(rd.HardState) {
+		// NB: Note that without additional safeguards, it's incorrect to write
+		// the HardState before appending rd.Entries. When catching up, a follower
+		// will receive Entries that are immediately Committed in the same
+		// Ready. If we persist the HardState but happen to lose the Entries,
+		// assertions can be tripped.
+		//
+		// We have both in the same batch, so there's no problem. If that ever
+		// changes, we must write and sync the Entries before the HardState.
+		if err := s.StateLoader.SetHardState(ctx, batch, rd.HardState); err != nil {
+			const expl = "during setHardState"
+			return RaftState{}, errors.Wrap(err, expl)
+		}
+	}
+	// Synchronously commit the batch with the Raft log entries and Raft hard
+	// state as we're promising not to lose this data.
+	//
+	// Note that the data is visible to other goroutines before it is synced to
+	// disk. This is fine. The important constraints are that these syncs happen
+	// before Raft messages are sent and before the call to RawNode.Advance. Our
+	// regular locking is sufficient for this and if other goroutines can see the
+	// data early, that's fine. In particular, snapshots are not a problem (I
+	// think they're the only thing that might access log entries or HardState
+	// from other goroutines). Snapshots do not include either the HardState or
+	// uncommitted log entries, and even if they did include log entries that
+	// were not persisted to disk, it wouldn't be a problem because raft does not
+	// infer the that entries are persisted on the node that sends a snapshot.
+	stats.PebbleBegin = timeutil.Now()
+	stats.PebbleBytes = int64(batch.Len())
+	sync := rd.MustSync && !disableSyncRaftLog.Get(&s.Settings.SV)
+	if err := batch.Commit(sync); err != nil {
+		const expl = "while committing batch"
+		return RaftState{}, errors.Wrap(err, expl)
+	}
+	stats.Sync = sync
+	stats.PebbleEnd = timeutil.Now()
+	if rd.MustSync {
+		s.Metrics.RaftLogCommitLatency.RecordValue(stats.PebbleEnd.Sub(stats.PebbleBegin).Nanoseconds())
+	}
+	return state, nil
 }
 
 // LogAppend adds the given entries to the raft log. Takes the previous log
