3792404301725ef28b9e79bc7b32cfd025bf87ab
Andrew Werner <awerner32@gmail.com>
2022-11-15 21:52:15 -0500

    sql/catalog,multiregionccl: ensure system database can be modified
    This PR removes some logic which prevented the system database from being
    properly modified. It also adds a test which ensures that the changes happen
    as expected.
    
    Release note: None

---
+++ pkg/ccl/multiregionccl/regional_by_row_system_database_test.go
@@ -0,0 +1,79 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package multiregionccl
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/ccl/multiregionccl/multiregionccltestutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+)
+
+// This is a rather contrived test to demonstrate that one can successfully,
+// albeit with some arm-twisting, convince the sql layer to make regional-
+// by-row tables in the system database.
+func TestRegionalByRowTablesInTheSystemDatabase(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	ctx := context.Background()
+	tc, sqlDB, cleanup := multiregionccltestutils.TestingCreateMultiRegionCluster(t, 3, base.TestingKnobs{})
+	defer cleanup()
+	defer tc.Stopper().Stop(ctx)
+
+	tdb := sqlutils.MakeSQLRunner(sqlDB)
+	tdb.Exec(t, `
+BEGIN;
+ALTER DATABASE system CONFIGURE ZONE DISCARD;
+ALTER DATABASE system SET PRIMARY REGION "us-east1";
+COMMIT;
+`)
+	// Pick an arbitrary table that we don't interact with via KV to make
+	// REGIONAL BY ROW.
+	tdb.Exec(t, `ALTER TABLE system.web_sessions SET LOCALITY REGIONAL BY ROW`)
+	tdb.Exec(t, `ALTER TABLE system.namespace SET LOCALITY GLOBAL`)
+	tdb.CheckQueryResults(t, `
+SELECT create_statement FROM [SHOW CREATE DATABASE system]
+UNION ALL SELECT create_statement FROM [SHOW CREATE TABLE system.web_sessions]
+UNION ALL SELECT create_statement FROM [SHOW CREATE TABLE system.namespace]
+`, [][]string{
+		{`CREATE DATABASE system PRIMARY REGION "us-east1" REGIONS = "us-east1" SURVIVE ZONE FAILURE`},
+		{`CREATE TABLE public.web_sessions (
+	id INT8 NOT NULL DEFAULT unique_rowid(),
+	"hashedSecret" BYTES NOT NULL,
+	username STRING NOT NULL,
+	"createdAt" TIMESTAMP NOT NULL DEFAULT now():::TIMESTAMP,
+	"expiresAt" TIMESTAMP NOT NULL,
+	"revokedAt" TIMESTAMP NULL,
+	"lastUsedAt" TIMESTAMP NOT NULL DEFAULT now():::TIMESTAMP,
+	"auditInfo" STRING NULL,
+	crdb_region system.public.crdb_internal_region NOT VISIBLE NOT NULL DEFAULT default_to_database_primary_region(gateway_region())::system.public.crdb_internal_region,
+	CONSTRAINT "primary" PRIMARY KEY (id ASC),
+	INDEX "web_sessions_expiresAt_idx" ("expiresAt" ASC),
+	INDEX "web_sessions_createdAt_idx" ("createdAt" ASC),
+	INDEX "web_sessions_revokedAt_idx" ("revokedAt" ASC),
+	INDEX "web_sessions_lastUsedAt_idx" ("lastUsedAt" ASC),
+	FAMILY "fam_0_id_hashedSecret_username_createdAt_expiresAt_revokedAt_lastUsedAt_auditInfo" (id, "hashedSecret", username, "createdAt", "expiresAt", "revokedAt", "lastUsedAt", "auditInfo"),
+	FAMILY fam_9_crdb_region (crdb_region)
+) LOCALITY REGIONAL BY ROW`},
+		{`CREATE TABLE public.namespace (
+	"parentID" INT8 NOT NULL,
+	"parentSchemaID" INT8 NOT NULL,
+	name STRING NOT NULL,
+	id INT8 NULL,
+	CONSTRAINT "primary" PRIMARY KEY ("parentID" ASC, "parentSchemaID" ASC, name ASC),
+	FAMILY "primary" ("parentID", "parentSchemaID", name),
+	FAMILY fam_4_id (id)
+) LOCALITY GLOBAL`},
+	})
+}
