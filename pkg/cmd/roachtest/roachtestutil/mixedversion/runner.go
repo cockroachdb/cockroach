2b1080ab738109de2b480756c0e2ddd0116c2b10
Renato Costa <renato@cockroachlabs.com>
2022-11-16 22:06:01 -0500

    roachtest: introduce new mixed-version testing API
    This introduces a new mixed-version API for roachtests, located under
    `roachtestutil/mixedversion`. The goal of this API is to provide a
    higher level framework than the existing `versionUpgradeTest` set of
    functions.
    
    Currently, all mixed-version roachtests rely on teams having to
    specify, step by step, which release version nodes should start from,
    how and in what order they upgrade, whether they attempt a rollback,
    at what point the feature being tested is invoked, etc. All of this
    makes the process of writing mixed-version roachtests time consuming
    and takes the focus away from what is actually being tested in
    mixed-version. In addition, it's typically not feasible to test
    multiple combinations of event orderings in mixed-version tests, and
    as a consequence most tests only exercise functionality after all
    nodes have upgraded to the new version, potentially hiding bugs that
    are only exposed if _some_ nodes are running a newer binary, while
    others are running an older release.
    
    The new API has the goal of automatically exploring different
    orderings in each run. In the most common scenario, teams will only
    need to specify what they want tested in a mixed-version setting, and
    the framework will decide when that feature will be invoked: in some
    runs, it will happen when all nodes are migrated to the new
    binary (most common scenario in existing tests); in other runs, it
    will happen when only some nodes have upgraded; sometimes it will run
    while upgrade migrations are running in the background; etc. It's also
    possible to specify multiple features to be tested in mixed-version
    state, and the framework will randomly schedule them during the test,
    potentially running them concurrently. The randomness is controlled by
    a seed that is logged when the test is executed. By reusing the same
    seed, the same mixed-version test is generated which helps with
    reproducibility (but, of course, does not make the test
    deterministic).
    
    Mixed-version tests that use the `mixedversion` package can be broken
    down into two stages: _planning_ and _execution_. During planning, all
    mixed-version hooks will be scheduled and a test plan is
    generated. All randomness is introduced during test plan
    generation. The execution stage just takes care of running the plan,
    and does not introduce any other randomness. When the mixed-version
    test is run, the test plan is printed in a tree-like format, providing
    a high level overview of what the test is doing. In addition, each
    step of the test is assigned a numeric ID, which can be used to
    reference the output of that step during execution. Each step logs to
    a different file, and the artifacts containing the output of each
    step will live in `mixed-version-test/{ID}.log`.
    
    The new framework builds on top of the recently introduced
    `clusterupgrade` package, which provides basic, low-level fiunctions
    that are  also used by the existing mixed-version tests.
    
    Epic: CRDB-19321
    Release note: None

---
+++ pkg/cmd/roachtest/roachtestutil/mixedversion/runner.go
@@ -0,0 +1,280 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package mixedversion
+
+import (
+	"context"
+	gosql "database/sql"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/cluster"
+	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/option"
+	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/roachtestutil/clusterupgrade"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/roachprod/logger"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"golang.org/x/sync/errgroup"
+)
+
+type (
+	testRunner struct {
+		ctx       context.Context
+		plan      *TestPlan
+		cluster   cluster.Cluster
+		crdbNodes option.NodeListOption
+		seed      int64
+		logger    *logger.Logger
+
+		binaryVersions  []roachpb.Version
+		clusterVersions []roachpb.Version
+
+		connCache []*gosql.DB
+	}
+)
+
+var (
+	// everything that is not an alphanum or a few special characters
+	invalidChars = regexp.MustCompile(`[^a-zA-Z0-9 \-_\.]`)
+)
+
+func newTestRunner(
+	ctx context.Context,
+	plan *TestPlan,
+	l *logger.Logger,
+	c cluster.Cluster,
+	crdbNodes option.NodeListOption,
+	randomSeed int64,
+) *testRunner {
+	return &testRunner{
+		ctx:       ctx,
+		plan:      plan,
+		logger:    l,
+		cluster:   c,
+		crdbNodes: crdbNodes,
+		seed:      randomSeed,
+	}
+}
+
+// run implements the test running logic, which boils down to running
+// each step in sequence.
+func (tr *testRunner) run() error {
+	defer tr.closeConnections()
+
+	for _, step := range tr.plan.steps {
+		if err := tr.runStep(step); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// runStep contains the logic of running a single test step, called
+// recursively in the case of sequentialRunStep and concurrentRunStep.
+func (tr *testRunner) runStep(step testStep) error {
+	if ss, ok := step.(singleStep); ok && ss.ID() > 1 {
+		// if we are running a singleStep that is *not* the first step,
+		// we can initialize the database connections. This represents the
+		// assumption that the first step in the test plan is the one that
+		// sets up binaries and make the `cockroach` process available on
+		// the nodes.
+		if err := tr.maybeInitConnections(); err != nil {
+			return err
+		}
+		if err := tr.refreshBinaryVersions(); err != nil {
+			return err
+		}
+		if err := tr.refreshClusterVersions(); err != nil {
+			return err
+		}
+	}
+
+	switch s := step.(type) {
+	case sequentialRunStep:
+		for _, ss := range s.steps {
+			if err := tr.runStep(ss); err != nil {
+				return err
+			}
+		}
+		return nil
+
+	case concurrentRunStep:
+		group, _ := errgroup.WithContext(tr.ctx)
+		for _, cs := range s.delayedSteps {
+			cs := cs
+			group.Go(func() error {
+				return tr.runStep(cs)
+			})
+		}
+		return group.Wait()
+
+	case delayedStep:
+		time.Sleep(s.delay)
+		return tr.runStep(s.step)
+
+	default:
+		ss := s.(singleStep)
+		stepLogger, err := tr.loggerFor(ss)
+		if err != nil {
+			return err
+		}
+
+		tr.logStep("STARTING", ss, stepLogger)
+		start := timeutil.Now()
+		defer func() {
+			prefix := fmt.Sprintf("FINISHED [%s]", timeutil.Since(start))
+			tr.logStep(prefix, ss, stepLogger)
+		}()
+		if err := ss.Run(tr.ctx, stepLogger, tr.cluster, tr.conn); err != nil {
+			return tr.reportError(err, ss, stepLogger)
+		}
+
+		return nil
+	}
+}
+
+// reportError augments the error passed with extra
+// information. Specifically, the error message will include the ID of
+// the step that failed, the random seed used, the binary version on
+// each node when the error occurred, and the cluster version before
+// and after the step (in case the failure happened *while* the
+// cluster version was updating).
+func (tr *testRunner) reportError(err error, step singleStep, l *logger.Logger) error {
+	errMsg := fmt.Sprintf("mixed-version test failure while running step %d (%s): %s",
+		step.ID(), step.Description(), err,
+	)
+	debugInfo := func(label, value string) string {
+		return fmt.Sprintf("%-40s%s", label+":", value)
+	}
+	seedInfo := debugInfo("test random seed", strconv.FormatInt(tr.seed, 10))
+	binaryVersions := debugInfo("binary versions", formatVersions(tr.binaryVersions))
+	clusterVersionsBefore := debugInfo("cluster versions before failure", formatVersions(tr.clusterVersions))
+	var clusterVersionsAfter string
+	if err := tr.refreshClusterVersions(); err == nil {
+		clusterVersionsBefore += "\n"
+		clusterVersionsAfter = debugInfo("cluster versions after failure", formatVersions(tr.clusterVersions))
+	} else {
+		tr.logger.Printf("failed to fetch cluster versions after failure: %s", err)
+	}
+
+	if err := renameFailedLogger(l); err != nil {
+		tr.logger.Printf("could not rename failed step logger: %v", err)
+	}
+
+	return fmt.Errorf(
+		"%s\n%s\n%s\n%s%s",
+		errMsg, seedInfo, binaryVersions, clusterVersionsBefore, clusterVersionsAfter,
+	)
+}
+
+func (tr *testRunner) logStep(prefix string, step singleStep, l *logger.Logger) {
+	dashes := strings.Repeat("-", 10)
+	l.Printf("%[1]s %s (%d): %s %[1]s", dashes, prefix, step.ID(), step.Description())
+}
+
+// loggerFor creates a logger instance to be used by a test step. Logs
+// will be available under `mixed-version-test/{ID}.log`, making it
+// easy to go from the IDs displayed in the test plan to the
+// corresponding output of that step.
+func (tr *testRunner) loggerFor(step singleStep) (*logger.Logger, error) {
+	name := invalidChars.ReplaceAllString(strings.ToLower(step.Description()), "")
+	name = fmt.Sprintf("%d_%s", step.ID(), name)
+
+	prefix := fmt.Sprintf("%s/%s", logPrefix, name)
+	return prefixedLogger(tr.logger, prefix)
+}
+
+// refreshBinaryVersions updates the internal `binaryVersions` field
+// with the binary version running on each node of the cluster.
+func (tr *testRunner) refreshBinaryVersions() error {
+	tr.binaryVersions = make([]roachpb.Version, 0, len(tr.crdbNodes))
+	for _, node := range tr.crdbNodes {
+		bv, err := clusterupgrade.BinaryVersion(tr.conn(node))
+		if err != nil {
+			return fmt.Errorf("failed to get binary version for node %d: %w", node, err)
+		}
+		tr.binaryVersions = append(tr.binaryVersions, bv)
+	}
+
+	return nil
+}
+
+// refreshClusterVersions updates the internal `clusterVersions` field
+// with the current view of the cluster version in each of the nodes
+// of the cluster.
+func (tr *testRunner) refreshClusterVersions() error {
+	tr.clusterVersions = make([]roachpb.Version, 0, len(tr.crdbNodes))
+	for _, node := range tr.crdbNodes {
+		cv, err := clusterupgrade.ClusterVersion(tr.ctx, tr.conn(node))
+		if err != nil {
+			return fmt.Errorf("failed to get cluster version for node %d: %w", node, err)
+		}
+		tr.clusterVersions = append(tr.clusterVersions, cv)
+	}
+
+	return nil
+}
+
+// maybeInitConnections initialize connections if the connection cache
+// is empty.
+func (tr *testRunner) maybeInitConnections() error {
+	if tr.connCache != nil {
+		return nil
+	}
+
+	tr.connCache = make([]*gosql.DB, len(tr.crdbNodes))
+	for _, node := range tr.crdbNodes {
+		conn, err := tr.cluster.ConnE(tr.ctx, tr.logger, node)
+		if err != nil {
+			return fmt.Errorf("failed to connect to node %d: %w", node, err)
+		}
+
+		tr.connCache[node-1] = conn
+	}
+
+	return nil
+}
+
+// conn returns a database connection to the given node. Assumes the
+// connection cache has been previously initialized.
+func (tr *testRunner) conn(node int) *gosql.DB {
+	return tr.connCache[node-1]
+}
+
+func (tr *testRunner) closeConnections() {
+	for _, db := range tr.connCache {
+		if db != nil {
+			_ = db.Close()
+		}
+	}
+}
+
+func renameFailedLogger(l *logger.Logger) error {
+	currentFileName := l.File.Name()
+	newLogName := strings.TrimSuffix(currentFileName, filepath.Ext(currentFileName))
+	newLogName += "_FAILED.log"
+	return os.Rename(currentFileName, newLogName)
+}
+
+func formatVersions(versions []roachpb.Version) string {
+	var pairs []string
+	for idx, version := range versions {
+		pairs = append(pairs, fmt.Sprintf("%d: %s", idx+1, version))
+	}
+
+	return fmt.Sprintf("[%s]", strings.Join(pairs, ", "))
+}
