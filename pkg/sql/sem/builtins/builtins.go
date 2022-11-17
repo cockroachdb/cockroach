bb388daa5b99d49873b637b02c871ceba51f3a1c
Jeff <swenson@cockroachlabs.com>
2022-11-17 16:19:15 -0500

    multiregionccl: add unsafe_optimize_system_database
    crdb_internal.unsafe_optimize_system_database optimizes the system
    database for multi region deployments by configuring latency critical
    tables as global or regional by row. Tables that are sensitive to read
    latency were converted to global. Table that are sensitive to write
    latency were converted to regional by row.
    
    system.sqlliveness and system.sql_instances were reworked by (#90408)
    and (#92010) to have a primary key index that is binary compatible with
    regional by row tables. The conversion to regional by row is
    implemented as a built in function because it is not possible to use
    `ALTER TABLE ... SET LOCALITY REGIONAL BY ROW` to perform the conversion.
    Using alter table to convert the RBR tables has two challenges:
    1. There is no way to convert the crdb_region column from []byte to the
       region enum type.
    2. Changing the locality to RBR always rewrites the primary index. The
       sqlliveness and sql_instance subsystems use the raw kv API and expect a
       specific index ID. Rewriting the primary index changes the ID and breaks
       the subsystems.
    
    Release note: None
    
    Part of #85736

--- pkg/sql/sem/builtins/builtins.go
+++ pkg/sql/sem/builtins/builtins.go
@@ -4926,6 +4926,22 @@ value if you rely on the HLC for accuracy.`,
 		},
 	),
 
+	"crdb_internal.unsafe_optimize_system_database": makeBuiltin(
+		tree.FunctionProperties{
+			Category:     builtinconstants.CategoryMultiRegion,
+			Undocumented: true,
+		},
+		tree.Overload{
+			Types:      tree.ParamTypes{},
+			ReturnType: tree.FixedReturnType(types.Bool),
+			Fn: func(ctx context.Context, evalCtx *eval.Context, args tree.Datums) (tree.Datum, error) {
+				return tree.MakeDBool(tree.DBool(true)), evalCtx.Regions.OptimizeSystemDatabase(ctx)
+			},
+			Info:       "Configures the sytem database to reduce latency during start up",
+			Volatility: volatility.Volatile,
+		},
+	),
+
 	"crdb_internal.destroy_tenant": makeBuiltin(
 		tree.FunctionProperties{
 			Category:     builtinconstants.CategoryMultiTenancy,
