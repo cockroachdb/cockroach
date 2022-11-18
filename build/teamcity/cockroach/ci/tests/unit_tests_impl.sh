03d00dd015984b2bf0aa1fb6cd430e75dbd1bdd7
healthy-pod <ahmad@cockroachlabs.com>
2022-11-18 10:53:30 -0800

    bazci: testing an experimental remote cache
    Release note: None
    Epic: None

--- build/teamcity/cockroach/ci/tests/unit_tests_impl.sh
+++ build/teamcity/cockroach/ci/tests/unit_tests_impl.sh
@@ -5,4 +5,4 @@ set -xeuo pipefail
 bazel build //pkg/cmd/bazci --config=ci
 $(bazel info bazel-bin --config=ci)/pkg/cmd/bazci/bazci_/bazci -- test --config=cinolint --config=simplestamp -c fastbuild \
 		                  //pkg:small_tests //pkg:medium_tests //pkg:large_tests //pkg:enormous_tests \
-                                   --profile=/artifacts/profile.gz
+                                   --profile=/artifacts/profile.gz --remote_cache=https://storage.googleapis.com/experimental-ci-remote-cache
