77cae0d28429578b279e342d364ec8e3af22b0f7
Raphael 'kena' Poss <knz@thaumogen.net>
2022-11-20 15:25:31 +0100

    clisqlshell: don't process `\p` and `\r` in multi-line editors
    Release note: None

--- pkg/cli/clisqlshell/editor_bubbline.go
+++ pkg/cli/clisqlshell/editor_bubbline.go
@@ -97,6 +97,10 @@ func (b *bubblineReader) canPrompt() bool {
 	return true
 }
 
+func (b *bubblineReader) multilineEdit() bool {
+	return false
+}
+
 func (b *bubblineReader) setPrompt(prompt string) {
 	b.ins.Prompt = prompt
 }
