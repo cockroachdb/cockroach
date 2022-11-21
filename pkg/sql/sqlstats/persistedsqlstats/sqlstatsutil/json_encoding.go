875b6d2e1a5b58754c5b915a58c1231787d04302
maryliag <marylia@cockroachlabs.com>
2022-11-21 16:22:37 -0500

    sql, ui: add index used by statement
    Part Of #82615
    
    This commits collects the list of indexes used by the
    statement execution and and saves on `system.statement_statistics`
    and `crdb_internal.statement_statistics` under the statistics columns,
    inside the statistics key.
    To get the information
    ```
    SELECT statistics -> 'statistics' ->> 'indexes' from
    crdb_internal.statement_statistics
    ```
    
    The value is saved as an array of string with format `tableId@indexId`.
    
    Release note (sql change): Add list of indexes used by
    the query on the statistics column on system.statement_statistics
    and crdb_internal.statement_statistics tables. The format is
    `tableID@indexID`.

--- pkg/sql/sqlstats/persistedsqlstats/sqlstatsutil/json_encoding.go
+++ pkg/sql/sqlstats/persistedsqlstats/sqlstatsutil/json_encoding.go
@@ -84,6 +84,12 @@ func BuildStmtMetadataJSON(statistics *roachpb.CollectedStatementStatistics) (js
 //	      },
 //	      "required": ["mean", "sqDiff"]
 //	    },
+//	    "indexes": {
+//	      "type": "array",
+//	      "items": {
+//	        "type": "string",
+//	      },
+//	    },
 //	    "node_ids": {
 //	      "type": "array",
 //	      "items": {
@@ -103,10 +109,11 @@ func BuildStmtMetadataJSON(statistics *roachpb.CollectedStatementStatistics) (js
 //	        "svcLat":            { "$ref": "#/definitions/numeric_stats" },
 //	        "ovhLat":            { "$ref": "#/definitions/numeric_stats" },
 //	        "bytesRead":         { "$ref": "#/definitions/numeric_stats" },
-//	        "rowsRead":          { "$ref": "#/definitions/numeric_stats" }
+//	        "rowsRead":          { "$ref": "#/definitions/numeric_stats" },
 //	        "firstExecAt":       { "type": "string" },
 //	        "lastExecAt":        { "type": "string" },
 //	        "nodes":             { "type": "node_ids" },
+//	        "indexes":           { "type": "indexes" },
 //	      },
 //	      "required": [
 //	        "firstAttemptCnt",
@@ -120,7 +127,8 @@ func BuildStmtMetadataJSON(statistics *roachpb.CollectedStatementStatistics) (js
 //	        "ovhLat",
 //	        "bytesRead",
 //	        "rowsRead",
-//	        "nodes"
+//	        "nodes",
+//	        "indexes
 //	      ]
 //	    },
 //	    "execution_statistics": {
