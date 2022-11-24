cockroachdb/cockroach

commit bfb930b1350aa84be60fe01c30fac270dbc9ee8b

*: un-singletonize the cluster settings
Apologies to drop such a huge refactoring, but it was a bit of a coup de force
required (or at least strongly suggested) by my inflight PR on version
migration (#17411).

We were previously using our singleton-based `settings` package that originally
replaced the use of environment variables. Its use, however, has since
diversified, the cluster version setting being the most elaborate addition.

In #17411 it has turned out that with these singletons in place, a lot of
effort and hacks are required to even be able to run tests at the same version.
However, it would be nice to test nonidentical versions in cluster tests.

Consequently, this PR

- introduces a struct `cluster.Settings` which now holds all the settings
  previously scattered around the code base; and
- plumbs this struct everywhere it's needed.

There are some open FIXMEs but I'd like a reviewer to review the bulk that's
here in the initial commit (which should pass all tests). I will then address
the FIXMEs (which require more focused attention) in follow-up commits, before
merging this PR.

pkg/cli/start.go
@@ -52,7 +52,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
-	"github.com/cockroachdb/cockroach/pkg/util/tracing"
 )
 
 // jemallocHeapDump is an optional function to be called at heap dump time.
@@ -212,7 +211,7 @@ func initCPUProfile(ctx context.Context, dir string) {
 	}
 
 	go func() {
-		defer log.RecoverAndReportPanic(ctx)
+		defer log.RecoverAndReportPanic(ctx, serverCfg.Settings.ReportingSettings)
 
 		ctx := context.Background()
 
@@ -304,7 +303,7 @@ func runStart(cmd *cobra.Command, args []string) error {
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
-	tracer := tracing.NewTracer()
+	tracer := serverCfg.Settings.Tracer
 	sp := tracer.StartSpan("server start")
 	startCtx := opentracing.ContextWithSpan(context.Background(), sp)
 
@@ -334,7 +333,11 @@ func runStart(cmd *cobra.Command, args []string) error {
 	go func() {
 		// Ensure that the log files see the startup messages immediately.
 		defer log.Flush()
-		defer log.RecoverAndReportPanic(startCtx)
+		defer func() {
+			if s != nil {
+				log.RecoverAndReportPanic(startCtx, s.ClusterSettings().ReportingSettings)
+			}
+		}()
 		defer sp.Finish()
 		if err := func() error {
 			if err := serverCfg.InitNode(); err != nil {
@@ -638,7 +641,7 @@ func getClientGRPCConn() (*grpc.ClientConn, *hlc.Clock, *stop.Stopper, error) {
 	clock := hlc.NewClock(hlc.UnixNano, 0)
 	stopper := stop.NewStopper()
 	rpcContext := rpc.NewContext(
-		log.AmbientContext{},
+		log.AmbientContext{Tracer: serverCfg.Settings.Tracer},
 		serverCfg.Config,
 		clock,
 		stopper,

pkg/util/log/crash_reporting.go
@@ -25,37 +25,22 @@ import (
 	"golang.org/x/net/context"
 
 	"github.com/cockroachdb/cockroach/pkg/build"
-	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/util/caller"
 	"github.com/cockroachdb/cockroach/pkg/util/envutil"
 )
 
-// DiagnosticsReportingEnabled wraps "diagnostics.reporting.enabled".
-//
-// "diagnostics.reporting.enabled" enables reporting of metrics related to a
-// node's storage (number, size and health of ranges) back to CockroachDB.
-// Collecting this data from production clusters helps us understand and improve
-// how our storage systems behave in real-world use cases.
-//
-// Note: while the setting itself is actually defined with a default value of
-// `false`, it is usually automatically set to `true` when a cluster is created
-// (or is migrated from a earlier beta version). This can be prevented with the
-// env var COCKROACH_SKIP_ENABLING_DIAGNOSTIC_REPORTING.
-//
-// Doing this, rather than just using a default of `true`, means that a node
-// will not errantly send a report using a default before loading settings.
-var DiagnosticsReportingEnabled = settings.RegisterBoolSetting(
-	"diagnostics.reporting.enabled",
-	"enable reporting diagnostic metrics to cockroach labs",
-	false,
-)
+// ReportingSettings avoids a dependency on pkg/settings/cluster.
+type ReportingSettings interface {
+	HasDiagnosticsReportingEnabled() bool
+	HasCrashReportsEnabled() bool
+}
 
 // RecoverAndReportPanic can be invoked on goroutines that run with
 // stderr redirected to logs to ensure the user gets informed on the
 // real stderr a panic has occurred.
-func RecoverAndReportPanic(ctx context.Context) {
+func RecoverAndReportPanic(ctx context.Context, st ReportingSettings) {
 	if r := recover(); r != nil {
-		ReportPanic(ctx, r, 1)
+		ReportPanic(ctx, st, r, 1)
 		panic(r)
 	}
 }
@@ -78,7 +63,7 @@ func format(r interface{}) string {
 }
 
 // ReportPanic reports a panic has occurred on the real stderr.
-func ReportPanic(ctx context.Context, r interface{}, depth int) {
+func ReportPanic(ctx context.Context, st ReportingSettings, r interface{}, depth int) {
 	Shout(ctx, Severity_ERROR, "a panic has occurred!")
 
 	// TODO(dt,knz,sql-team): we need to audit all sprintf'ing of values into the
@@ -98,7 +83,7 @@ func ReportPanic(ctx context.Context, r interface{}, depth int) {
 		file, line, _ := caller.Lookup(depth + 3)
 		reportable = fmt.Sprintf("%s %s:%d", format(r), filepath.Base(file), line)
 	}
-	sendCrashReport(ctx, reportable, depth+3)
+	sendCrashReport(ctx, st, reportable, depth+3)
 
 	// Ensure that the logs are flushed before letting a panic
 	// terminate the server.
@@ -115,12 +100,6 @@ func ReportPanic(ctx context.Context, r interface{}, depth int) {
 		fmt.Fprintf(OrigStderr, "%v\n\n%s\n", r, debug.Stack())
 	}
 }
-
-var crashReports = settings.RegisterBoolSetting(
-	"diagnostics.reporting.send_crash_reports",
-	"send crash and panic reports",
-	true,
-)
 
 var crashReportURL = func() string {
 	var defaultURL string
@@ -153,10 +132,15 @@ func SetupCrashReporter(ctx context.Context, cmd string) {
 
 var crdbPaths = []string{"github.com/cockroachdb/cockroach"}
 
-func sendCrashReport(ctx context.Context, r interface{}, depth int) {
-	if !DiagnosticsReportingEnabled.Get() || !crashReports.Get() {
+func sendCrashReport(ctx context.Context, st ReportingSettings, r interface{}, depth int) {
+	if st == nil || !st.HasDiagnosticsReportingEnabled() || !st.HasCrashReportsEnabled() {
 		return // disabled via settings.
 	}
+	// FIXME(tschottdorf): this particular method would benefit from globals
+	// mirroring the server settings. Callers coming through log.Fatal will
+	// pass a nil ReportingSettings above, which is pretty bad. OTOH, perhaps
+	// they should just pass ReportingSettings directly through log.Fatal.
+
 	if raven.DefaultClient == nil {
 		return // disabled via empty URL env var.
 	}

commit 40606998b1ec4940ff901ba97f62407a0dc42f6c

cli/start: ensure startup panics are reported to stderr
This fixes a regression introduced in
bfb930b1350aa84be60fe01c30fac270dbc9ee8b (in 2017!)

The `recover()` call from `RecoverAndReportPanic` doesn't work unless
the function is called via `defer` directly. It can't be called from
another function e.g. a closure with `defer func() {
... myPanicCatcher() ... }`.

This commit clarifies this in the API docs for the `logcrash` package
and fixes the only call site that was incorrect.

Release note (bug fix): Server crashes that occur during startup are
now more clearly reported in logs and the standard error output.

pkg/cli/start.go
@@ -39,6 +39,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/security/username"
 	"github.com/cockroachdb/cockroach/pkg/server"
 	"github.com/cockroachdb/cockroach/pkg/server/status"
+	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catalogkeys"
 	"github.com/cockroachdb/cockroach/pkg/storage"
@@ -645,12 +646,16 @@ func createAndStartServerAsync(
 		// mechanism to intercept the panic and log the panic details to
 		// the error reporting server.
 		defer func() {
+			var sv *settings.Values
 			if s != nil {
-				// We only attempt to log the panic details if the server has
-				// actually been started successfully. If there's no server,
-				// we won't know enough to decide whether reporting is
-				// permitted.
-				logcrash.RecoverAndReportPanic(ctx, &s.ClusterSettings().SV)
+				sv = &s.ClusterSettings().SV
+			}
+			if r := recover(); r != nil {
+				// This ensures that the panic, if any, is also reported on stderr.
+				// The settings.Values, if available, determines whether a Sentry
+				// report should be sent. No Sentry report is sent if sv is nil.
+				logcrash.ReportPanic(ctx, sv, r, 1 /* depth */)
+				panic(r)
 			}
 		}()
 		// When the start up goroutine completes, so can the start up span.

pkg/util/log/logcrash/crash_reporting.go
@@ -118,6 +118,13 @@ func ReportPanicWithGlobalSettings(ctx context.Context, r interface{}, depth int
 // RecoverAndReportPanic can be invoked on goroutines that run with
 // stderr redirected to logs to ensure the user gets informed on the
 // real stderr a panic has occurred.
+// The argument sv may be nil; if it is, no Sentry report is sent.
+//
+// Note: this function _must_ be called with `defer log.RecoverAndReportPanic()`.
+// Do not use `defer func() { ... log.RecoverAndReportPanic() ...}` as this
+// will fail to actually catch the panic.
+// If you need more code around the call in your defer, use ReportPanic()
+// instead and do not forget to re-panic afterwards.
 func RecoverAndReportPanic(ctx context.Context, sv *settings.Values) {
 	if r := recover(); r != nil {
 		ReportPanic(ctx, sv, r, depthForRecoverAndReportPanic)
@@ -127,6 +134,13 @@ func RecoverAndReportPanic(ctx context.Context, sv *settings.Values) {
 
 // RecoverAndReportNonfatalPanic is an alternative RecoverAndReportPanic that
 // does not re-panic in Release builds.
+// The caller is responsible for ensuring that the argument sv is non-nil.
+//
+// Note: this function _must_ be called with `defer log.RecoverAndReportNonfatalPanic()`.
+// Do not use `defer func() { ... log.RecoverAndReportNonfatalPanic() ...}` as this
+// will fail to actually catch the panic.
+// If you need more code around the call in your defer, use ReportNonfatalPanic()
+// instead. The caller should not explicitly re-panic afterwards.
 func RecoverAndReportNonfatalPanic(ctx context.Context, sv *settings.Values) {
 	if r := recover(); r != nil {
 		ReportPanic(ctx, sv, r, depthForRecoverAndReportPanic)
@@ -141,7 +155,7 @@ func RecoverAndReportNonfatalPanic(ctx context.Context, sv *settings.Values) {
 // Note that ReportPanic() does not assume that the panic object
 // will be left uncaught to terminate the process. For example,
 // at the time of this writing, ReportPanic() is called from
-// RecoverAndReportNonfatalPanic() above.
+// ReportNonfatalPanic() above.
 func ReportPanic(ctx context.Context, sv *settings.Values, r interface{}, depth int) {
 	// Announce the panic has occurred to all places. The purpose
 	// of this call is threefold: