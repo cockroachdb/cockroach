412bb2423e94c9100de436a8e7d3db0172320aa3
healthy-pod <ahmad@cockroachlabs.com>
2022-11-24 17:03:26 -0800

    teamcity-trigger: remove make-related code and pass custom timeouts
    This code change removes make-related code from teamcity-trigger. It
    also allows us to customize the timeout used per test target by updating
    one source which is the `customTimeouts` map. The timeout will then be
    propagated to bazel's `test_timeout` and go's `-test.timeout` as needed.
    
    Release note: None
    Epic: None

--- build/teamcity/cockroach/nightlies/stress_impl.sh
+++ build/teamcity/cockroach/nightlies/stress_impl.sh
@@ -15,40 +15,24 @@ bazel build //pkg/cmd/bazci --config=ci
 BAZEL_BIN=$(bazel info bazel-bin --config=ci)
 ARTIFACTS_DIR=/artifacts
 
-# Query to list all affected tests.
-PKG=${PKG#"./"}
-if [[ $(basename $PKG) != ... ]]
+if [[ ! -z $(bazel query "attr(tags, \"broken_in_bazel\", $TARGET)") ]]
 then
-    PKG="$PKG:all"
+    echo "Skipping test $TARGET as it is broken in bazel"
+    exit 0
 fi
-tests=$(bazel query "kind(go_test, $PKG)" --output=label)
+if [[ ! -z $(bazel query "attr(tags, \"integration\", $TARGET)") ]]
+then
+    echo "Skipping test $TARGET as it is an integration test"
+    exit 0
+fi
 
-# Run affected tests.
-for test in $tests
-do
-    if [[ ! -z $(bazel query "attr(tags, \"broken_in_bazel\", $test)") ]]
-    then
-        echo "Skipping test $test as it is broken in bazel"
-        continue
-    fi
-    if [[ ! -z $(bazel query "attr(tags, \"integration\", $test)") ]]
-    then
-        echo "Skipping test $test as it is an integration test"
-        continue
-    fi
-    exit_status=0
-    $BAZEL_BIN/pkg/cmd/bazci/bazci_/bazci -- test --config=ci "$test" \
-                                          --test_env=COCKROACH_NIGHTLY_STRESS=true \
-                                          --test_timeout="$TESTTIMEOUTSECS" \
-                                          --run_under "@com_github_cockroachdb_stress//:stress -bazel -shardable-artifacts 'XML_OUTPUT_FILE=$BAZEL_BIN/pkg/cmd/bazci/bazci_/bazci merge-test-xmls' $STRESSFLAGS" \
-                                          --define "gotags=$TAGS" \
-                                          --nocache_test_results \
-                                          --test_output streamed \
-                                          ${EXTRA_BAZEL_FLAGS} \
-        || exit_status=$?
-    if [ $exit_status -ne 0 ]
-    then
-        exit $exit_status
-    fi
-done
-
+GOTESTTIMEOUTSECS=$(($TESTTIMEOUTSECS - 5))
+$BAZEL_BIN/pkg/cmd/bazci/bazci_/bazci -- test --config=ci "$TARGET" \
+                                        --test_env=COCKROACH_NIGHTLY_STRESS=true \
+                                        --test_timeout="$TESTTIMEOUTSECS" \
+                                        --test_arg=-test.timeout="${GOTESTTIMEOUTSECS}s"
+                                        --run_under "@com_github_cockroachdb_stress//:stress -bazel -shardable-artifacts 'XML_OUTPUT_FILE=$BAZEL_BIN/pkg/cmd/bazci/bazci_/bazci merge-test-xmls' $STRESSFLAGS" \
+                                        --define "gotags=$TAGS" \
+                                        --nocache_test_results \
+                                        --test_output streamed \
+                                        ${EXTRA_BAZEL_FLAGS} \
