e80257200d5aad256ebc28b5618054d52eeee8e3
healthy-pod <ahmad@cockroachlabs.com>
2022-11-24 17:03:26 -0800

    teamcity-trigger: remove make-related code and pass custom timeouts
    This code change removes make-related code from teamcity-trigger. It
    also allows us to customize the timeout used per test target by updating
    one source which is the `customTimeouts` map. The timeout will then be
    propagated to bazel's `test_timeout` and go's `-test.timeout` as needed.
    
    Release note: None
    Epic: None

--- build/teamcity/cockroach/nightlies/stress_impl.sh
+++ build/teamcity/cockroach/nightlies/stress_impl.sh
@@ -37,9 +37,11 @@ do
         continue
     fi
     exit_status=0
+    GOTESTTIMEOUTSECS=$(($TESTTIMEOUTSECS - 5))
     $BAZEL_BIN/pkg/cmd/bazci/bazci_/bazci -- test --config=ci "$test" \
                                           --test_env=COCKROACH_NIGHTLY_STRESS=true \
                                           --test_timeout="$TESTTIMEOUTSECS" \
+                                          --test_arg=-test.timeout="${GOTESTTIMEOUTSECS}s"
                                           --run_under "@com_github_cockroachdb_stress//:stress -bazel -shardable-artifacts 'XML_OUTPUT_FILE=$BAZEL_BIN/pkg/cmd/bazci/bazci_/bazci merge-test-xmls' $STRESSFLAGS" \
                                           --define "gotags=$TAGS" \
                                           --nocache_test_results \
