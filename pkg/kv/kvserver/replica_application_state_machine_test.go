92e1e5e894b09932057183a7bb5019f5516d5692
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-11-28 13:15:56 +0100

    kvserver: deflake TestReplicaStateMachineChangeReplicas
    This is very similar to
    https://github.com/cockroachdb/cockroach/pull/92375.
    
    Release note: None

--- pkg/kv/kvserver/replica_application_state_machine_test.go
+++ pkg/kv/kvserver/replica_application_state_machine_test.go
@@ -145,6 +145,14 @@ func TestReplicaStateMachineChangeReplicas(t *testing.T) {
 			require.Error(t, err)
 			require.IsType(t, &roachpb.RangeNotFoundError{}, err)
 		}
+		// Set a destroyStatus to make sure there won't be any raft processing once
+		// we release raftMu. We applied a command but not one from the raft log, so
+		// should there be a command in the raft log (i.e. some errant lease request
+		// or whatnot) this will fire assertions because it will conflict with the
+		// log index that we pulled out of thin air above.
+		r.mu.Lock()
+		defer r.mu.Unlock()
+		r.mu.destroyStatus.Set(errors.New("test done"), destroyReasonRemoved)
 	})
 }
 
