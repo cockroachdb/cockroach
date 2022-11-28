bd91a6de1b589ca6a361e6e2014f3d2a71252371
Faizaan Madhani <fzmadhani@gmail.com>
2022-11-28 11:42:02 -0500

    sql: Fix create partial stats test cases
    This commit modifies the create partial
    statistics test cases to ensure that the full table
    statistics that are to be used to create a partial
    statistic exist in the cache. Previously, this was
    not the case so certain tests had non-deterministic
    outputs causes failures in stress tests.
    
    Resolves: #92495
    
    Epic: CRDB-19449
    
    Release note: None

--- pkg/sql/alter_table.go
+++ pkg/sql/alter_table.go
@@ -1301,6 +1301,11 @@ func insertJSONStatistic(
 	}
 
 	if !settings.Version.IsActive(ctx, clusterversion.V23_1AddPartialStatisticsPredicateCol) {
+
+		if s.PartialPredicate != "" {
+			return pgerror.Newf(pgcode.ObjectNotInPrerequisiteState, "statistic for columns %v with collection time %s to insert is partial but cluster version is below 23.1", s.Columns, s.CreatedAt)
+		}
+
 		_ /* rows */, err := ie.Exec(
 			ctx,
 			"insert-stats",
@@ -1327,6 +1332,12 @@ func insertJSONStatistic(
 			histogram)
 		return err
 	}
+
+	var predicateValue interface{}
+	if s.PartialPredicate != "" {
+		predicateValue = s.PartialPredicate
+	}
+
 	_ /* rows */, err := ie.Exec(
 		ctx,
 		"insert-stats",
@@ -1352,7 +1363,7 @@ func insertJSONStatistic(
 		s.NullCount,
 		s.AvgSize,
 		histogram,
-		s.PartialPredicate,
+		predicateValue,
 	)
 	return err
 }
