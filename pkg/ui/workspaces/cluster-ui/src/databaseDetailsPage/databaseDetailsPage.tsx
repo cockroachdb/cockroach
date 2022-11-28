3c39728da264870fc5ee8c968df40b9da9504cbb
gtr <gerardo@cockroachlabs.com>
2022-11-28 11:58:50 -0500

    ui: Add search and filtering to the databases pages
    Previously, the databases and databases details pages did not contain
    search and filter components that the txns and stmts pages did. This
    change adds search and filter components to both the databases and
    databases details pages. The search box filters by database/table name
    while the filter allows filtering by node and region.
    
    Release note (ui change): the databases page and the databases details
    pages each now contain search and filter components, allowing the
    ability to search and filter through databases and their tables.

--- pkg/ui/workspaces/cluster-ui/src/databaseDetailsPage/databaseDetailsPage.tsx
+++ pkg/ui/workspaces/cluster-ui/src/databaseDetailsPage/databaseDetailsPage.tsx
@@ -28,7 +28,7 @@ import {
 } from "src/sortedtable";
 import * as format from "src/util/format";
 import { DATE_FORMAT } from "src/util/format";
-import { mvccGarbage, syncHistory } from "../util";
+import { mvccGarbage, syncHistory, unique } from "../util";
 
 import styles from "./databaseDetailsPage.module.scss";
 import sortableTableStyles from "src/sortedtable/sortedtable.module.scss";
@@ -41,6 +41,15 @@ import { Caution } from "@cockroachlabs/icons";
 import { Anchor } from "../anchor";
 import LoadingError from "../sqlActivity/errorComponent";
 import { Loading } from "../loading";
+import { Search } from "../search";
+import { 
+  Filter,
+  Filters,
+  defaultFilters,
+  calculateActiveFilters,
+} from "src/queryFilter";
+import { nodeRegions } from "src/transactionsPage/transactions.fixture";
+import { UIConfigState } from "src/store";
 
 const cx = classNames.bind(styles);
 const sortableTableCx = classNames.bind(sortableTableStyles);
@@ -92,6 +101,9 @@ export interface DatabaseDetailsPageData {
   tables: DatabaseDetailsPageDataTable[];
   sortSettingTables: SortSetting;
   sortSettingGrants: SortSetting;
+  search: string;
+  filters: Filters;
+  isTenant?: UIConfigState["isTenant"];
   viewMode: ViewMode;
   showNodeRegionsColumn?: boolean;
 }
@@ -124,6 +136,11 @@ export interface DatabaseDetailsPageDataTableStats {
   lastError: Error;
   replicationSizeInBytes: number;
   rangeCount: number;
+  // Array of node IDs used to unambiguously filter by node and region.
+  nodes?: number[];
+  // String of nodes grouped by region in alphabetical order, e.g.
+  // regionA(n1,n2), regionB(n3). Used for display in the table's
+  // "Regions/Nodes" column.
   nodesByRegionString?: string;
 }
 
@@ -131,6 +148,8 @@ export interface DatabaseDetailsPageActions {
   refreshDatabaseDetails: (database: string) => void;
   refreshTableDetails: (database: string, table: string) => void;
   refreshTableStats: (database: string, table: string) => void;
+  onFilterChange?: (value: Filters) => void;
+  onSearchComplete?: (query: string) => void;
   onSortingTablesChange?: (columnTitle: string, ascending: boolean) => void;
   onSortingGrantsChange?: (columnTitle: string, ascending: boolean) => void;
   onViewModeChange?: (viewMode: ViewMode) => void;
@@ -147,11 +166,34 @@ export enum ViewMode {
 
 interface DatabaseDetailsPageState {
   pagination: ISortedTablePagination;
+  filters?: Filters;
+  activeFilters?: number;
   lastStatsError: Error;
   lastDetailsError: Error;
 }
 
 class DatabaseSortedTable extends SortedTable<DatabaseDetailsPageDataTable> {}
+
+// filterBySearchQuery returns true if the search query matches the database name.
+function filterBySearchQuery(
+  table: DatabaseDetailsPageDataTable,
+  search: string,
+): boolean {
+  const matchString = table.name.toLowerCase();
+
+  if (search.startsWith('"') && search.endsWith('"')) {
+    search = search.substring(1, search.length - 1);
+
+    return matchString.includes(search)
+  }
+
+  const res = search
+    .toLowerCase()
+    .split(" ")
+    .every(val => matchString.includes(val));
+
+  return res;
+}
 
 export class DatabaseDetailsPage extends React.Component<
   DatabaseDetailsPageProps,
@@ -284,6 +326,110 @@ export class DatabaseDetailsPage extends React.Component<
     if (onSortingChange) {
       onSortingChange(ss.columnTitle, ss.ascending);
     }
+  };
+
+  onClearSearchField = (): void => {
+    if (this.props.onSearchComplete) {
+      this.props.onSearchComplete("");
+    }
+
+    syncHistory(
+      {
+        q: undefined,
+      },
+      this.props.history,
+    );
+  };
+
+  onSubmitSearchField = (search: string): void => {
+    if (this.props.onSearchComplete) {
+      this.props.onSearchComplete(search);
+    }
+
+    this.resetPagination();
+    syncHistory(
+      {
+        q: search,
+      },
+      this.props.history,
+    );
+  };
+
+  onSubmitFilters = (filters: Filters): void => {
+    if (this.props.onFilterChange) {
+      this.props.onFilterChange(filters);
+    }
+
+    this.setState({
+      filters: filters,
+      activeFilters: calculateActiveFilters(filters),
+    });
+
+    this.resetPagination();
+    syncHistory(
+      {
+        app: filters.app,
+        timeNumber: filters.timeNumber,
+        timeUnit: filters.timeUnit,
+        fullScan: filters.fullScan.toString(),
+        sqlType: filters.sqlType,
+        database: filters.database,
+        regions: filters.regions,
+        nodes: filters.nodes,
+      },
+      this.props.history,
+    );
+  };
+
+  resetPagination = (): void => {
+    this.setState(prevState => {
+      return {
+        pagination: {
+          current: 1,
+          pageSize: prevState.pagination.pageSize,
+        }
+      };
+    });
+  };
+
+  // Returns a list of database tables to the display based on input from the
+  // search box and the applied filters.
+  filteredDatabaseTables = (): DatabaseDetailsPageDataTable[] => {
+    const {
+      search,
+      tables,
+      filters,
+      isTenant,
+    } = this.props;
+
+    const regionsSelected = filters.regions.length > 0 ? filters.regions.split(",") : [];
+    const nodesSelected = filters.nodes.length > 0 ? filters.nodes.split(",") : [];
+
+    return tables
+      .filter(
+        table => search ? filterBySearchQuery(table, search) : true
+      )
+      .filter(
+        table => {
+          if (regionsSelected.length == 0 && nodesSelected.length == 0) return true;
+          if (isTenant) return true;
+
+          let foundRegion = regionsSelected.length == 0;
+          let foundNode = nodesSelected.length == 0;
+
+          table.stats.nodes?.forEach(node => {
+            if (foundRegion || regionsSelected.includes(nodeRegions[node.toString()])) {
+              foundRegion = true;
+            }
+            if (foundNode || nodesSelected.includes("n" + node.toString())) {
+              foundNode = true;
+            }
+            if (foundNode && foundRegion) return true;
+          });
+
+          return foundRegion && foundNode;
+        }
+      );
   };
 
   private changeViewMode(viewMode: ViewMode) {
@@ -594,6 +740,26 @@ export class DatabaseDetailsPage extends React.Component<
   }
 
   render(): React.ReactElement {
+    const {
+      search,
+      filters,
+      isTenant,
+    } = this.props;
+
+    const tablesToDisplay = this.filteredDatabaseTables();
+    const activeFilters = calculateActiveFilters(filters);
+
+    const nodes = isTenant
+      ? []
+      : Object.keys(nodeRegions)
+        .map(n => Number(n))
+        .sort();
+
+    const regions = isTenant
+      ? []
+      : unique(nodes
+        .map(node => nodeRegions[node.toString()]))
+
     const sortSetting =
       this.props.viewMode == ViewMode.Tables
         ? this.props.sortSettingTables
@@ -631,6 +797,27 @@ export class DatabaseDetailsPage extends React.Component<
               View: {this.props.viewMode}
             </Dropdown>
           </PageConfigItem>
+          <PageConfigItem>
+              <Search
+                onSubmit={this.onSubmitSearchField}
+                onClear={this.onClearSearchField}
+                defaultValue={search}
+                placeholder={"Search Tables"}
+              />
+          </PageConfigItem>
+          <PageConfigItem>
+            <Filter
+              hideAppNames={true}
+              regions={regions}
+              hideTimeLabel={true}
+              nodes={nodes.map(n => "n" + n.toString())}
+              activeFilters={activeFilters}
+              filters={defaultFilters}
+              onSubmitFilters={this.onSubmitFilters}
+              showNodes={nodes.length > 1}
+              showRegions={regions.length > 1}
+            />
+          </PageConfigItem>
         </PageConfig>
 
         <section className={sortableTableCx("cl-table-container")}>
@@ -652,7 +839,7 @@ export class DatabaseDetailsPage extends React.Component<
             render={() => (
               <DatabaseSortedTable
                 className={cx("database-table")}
-                data={this.props.tables}
+                data={tablesToDisplay}
                 columns={this.columns()}
                 sortSetting={sortSetting}
                 onChangeSortSetting={this.changeSortSetting}
