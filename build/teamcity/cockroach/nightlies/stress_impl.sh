e6b44954c8634a6345329df2a71eeae98b119a12
Stan Rosenberg <stan@cockroachlabs.com>
2022-12-01 17:45:09 +0000

    ci: fix regression in stress_impl
    Fixes previous backport which removed GO_TEST_JSON_OUTPUT_FILE
    by mistake during git conflict resolution.
    
    Release note: None

--- build/teamcity/cockroach/nightlies/stress_impl.sh
+++ build/teamcity/cockroach/nightlies/stress_impl.sh
@@ -33,6 +33,7 @@ do
         continue
     fi
     exit_status=0
+    GO_TEST_JSON_OUTPUT_FILE=$ARTIFACTS_DIR/$(echo "$test" | cut -d: -f2).test.json.txt
     $BAZEL_BIN/pkg/cmd/bazci/bazci_/bazci -- test --config=ci "$test" \
                                           --test_env=COCKROACH_NIGHTLY_STRESS=true \
                                           --test_env=GO_TEST_JSON_OUTPUT_FILE=$GO_TEST_JSON_OUTPUT_FILE \
