25cbd8e7a85259e65705481810a8a05c2cb1e903
Rafi Shamim <rafi@cockroachlabs.com>
2022-12-01 11:34:02 -0500

    tree: improve type-checking for placeholders with ambiguous type
    Release note (bug fix): Prepared statements that use type hints can now
    succeed type-checking in more cases when the placeholder type is
    ambiguous.

--- pkg/sql/sem/tree/placeholders.go
+++ pkg/sql/sem/tree/placeholders.go
@@ -134,7 +134,11 @@ func (p *PlaceholderTypesInfo) SetType(idx PlaceholderIdx, typ *types.T) error {
 				pgcode.DatatypeMismatch,
 				"placeholder %s already has type %s, cannot assign %s", idx, t, typ)
 		}
-		return nil
+		// If `t` is ambiguous, then we can set the type to `typ`, which is more
+		// specific.
+		if !t.IsAmbiguous() {
+			return nil
+		}
 	}
 	p.Types[idx] = typ
 	return nil
