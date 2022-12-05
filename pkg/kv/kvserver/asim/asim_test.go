c465964a9ae27fc6009aa375a78379bfa3f21f7b
Austen McClernon <austen@cockroachlabs.com>
2022-12-05 06:57:43 +0000

    asim: add e2e determinsim test
    This patch introduces `TestAllocatorSimulatorDeterministic` which
    asserts that multiple runs seeded with identical data produce an
    identical end state.
    
    Release note: None

--- pkg/kv/kvserver/asim/asim_test.go
+++ pkg/kv/kvserver/asim/asim_test.go
@@ -23,6 +23,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/config"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/state"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/workload"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/stretchr/testify/require"
@@ -157,4 +158,63 @@ func TestAllocatorSimulatorSpeed(t *testing.T) {
 
 	fmt.Println(time.Duration(minRunTime).Seconds())
 	require.Less(t, minRunTime, requiredRunTime)
+}
+
+func TestAllocatorSimulatorDeterministic(t *testing.T) {
+
+	start := state.TestingStartTime()
+	settings := config.DefaultSimulationSettings()
+
+	runs := 3
+	end := start.Add(15 * time.Minute)
+	bgInterval := 10 * time.Second
+	interval := 2 * time.Second
+	preGossipStart := start.Add(-settings.StateExchangeInterval - settings.StateExchangeDelay)
+
+	stores := 7
+	replsPerRange := 3
+	replicasPerStore := 100
+	// NB: We want 100 replicas per store, so the number of ranges required
+	// will be 1/3 of the total replicas.
+	ranges := (replicasPerStore * stores) / replsPerRange
+	// NB: In this test we are using a uniform workload and expect to see at
+	// most 3 splits occur due to range size, therefore the keyspace need not
+	// be larger than 3 keys per range.
+	keyspace := 3 * ranges
+	// Track the run to compare against for determinism.
+	var refRun []roachpb.StoreDescriptor
+
+	for run := 0; run < runs; run++ {
+		rwg := make([]workload.Generator, 1)
+		rwg[0] = testCreateWorkloadGenerator(start, stores, int64(keyspace))
+		exchange := state.NewFixedDelayExhange(preGossipStart, settings.StateExchangeInterval, settings.StateExchangeDelay)
+		changer := state.NewReplicaChanger()
+		m := asim.NewMetricsTracker() // no output
+		replicaDistribution := make([]float64, stores)
+
+		// NB: Here create half of the stores with equal replica counts, the
+		// other half have no replicas. This will lead to a flurry of activity
+		// rebalancing towards these stores, based on the replica count
+		// imbalance.
+		for i := 0; i < stores/2; i++ {
+			replicaDistribution[i] = 1.0 / float64(stores/2)
+		}
+		for i := stores / 2; i < stores; i++ {
+			replicaDistribution[i] = 0
+		}
+
+		s := state.NewTestStateReplDistribution(replicaDistribution, ranges, replsPerRange, keyspace)
+		testPreGossipStores(s, exchange, preGossipStart)
+		sim := asim.NewSimulator(start, end, interval, bgInterval, rwg, s, exchange, changer, settings, m)
+
+		ctx := context.Background()
+		sim.RunSim(ctx)
+		descs := s.StoreDescriptors()
+
+		if run == 0 {
+			refRun = descs
+			continue
+		}
+		require.Equal(t, refRun, descs)
+	}
 }
