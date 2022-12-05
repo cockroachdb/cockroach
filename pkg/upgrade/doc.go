33431136b1b5f19f6d1d2e86cfc6a032c80462c0
Andrei Matei <andrei@cockroachlabs.com>
2022-12-05 15:02:31 -0500

    upgrade: don't blindly run permanent migrations
    This patch makes the "primordial" permanent upgrades not run in clusters
    being upgraded from 22.2 or lower.
    
    Startup migrations were recently replaced with "permanent upgrades", in #91627.
    The permanent new upgrades were running on node startup regardless of
    whether it was a "new cluster" or an existing one - i.e. regardless of
    whether the previous startupmigrations had also run, on the argument
    that startupmigrations and upgrades are supposed to be idempotent
    anyway. But, alas, there's idempotency and then there's idempotency:
    startupmigrations/upgrades are supposed to be OK with running multiple
    times at the same BinaryVersion in a "new cluster", but not necessarily
    across BinaryVersions and in a cluster that has been in use. There are
    different things that can go wrong when one of these migrations runs
    multiple times in these general conditions, because the migration
    might makes implicit assumptions about the schema and data of the
    cluster it's running in:
    1. A migration might assume that all the data in a system table was
       created before a CRDB version changed some semantics. For example,
       the migration deleted in #92597 was assuming that `CREATEROLE` role
       option had a certain meaning for all the users who have it and was doing
       a backfill across all these users. Running this migration again at an
       arbitrary later point would affect unintended new users.
    2. A migration assumes the schema of system tables it operates on. As
       this schema (which is part of the bootstrap image for new cluster)
       changes with new versions, the code of the upgrade changes also. This
       means that the upgrade is no longer suitable for running against
       clusters that don't have the upgraded schema - i.e. clusters that have
       not yet been upgraded to the latest schema. This is a real problem
       because we're adding a column to system.users, and the addRootUser
       upgrade can no longer run against 22.2 clusters.
    
    This patch guards all upgrades on checking that the old corresponding
    startupmigration has not run, thus enabling the mentioned system schema
    change in 23.1.
    
    Release note: None
    Epic: None

---
+++ pkg/upgrade/doc.go
@@ -0,0 +1,74 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+/*
+Package upgrade contains infrastructure for migrating a cluster from a version
+to another. It contains "upgrades" associated to different cluster versions that
+are run when the cluster is upgraded: the upgrade for version v is run when the
+cluster version is bumped from a version < v to one >= v. The code for the
+upgrade corresponding to version v can be deleted when compatibility with
+version v is no longer needed.
+
+There is also a special category of upgrades called "permanent upgrades".
+Permanent upgrades are associated with a cluster version (just like the
+non-permanent ones), but, in addition to running when the cluster version is
+bumped above their version, also run when the cluster is created at a version >=
+v. They are called "permanent" because these upgrades cannot be deleted as soon
+as compatibility with version v is no longer needed. Permanent upgrades can be
+used to perform setup needed by the cluster, but that cannot be included in the
+cluster's bootstrap image. For example, let's say that we have upgrades
+associated to the following cluster versions:
+
+v0.0.2  - permanent
+v0.0.3  - permanent
+v23.1.1
+v23.1.5 - permanent
+v23.1.7
+v23.2.0
+v23.2.1 - permanent
+v23.2.2
+v23.2.3
+v24.1.0
+
+and let's futher say that we're creating the cluster at version v23.2.0. On
+cluster creation, we'll run all permanent upgrades <= v23.2.0, and none of the
+non-permanent ones. Then, let's say that all the nodes are upgraded to
+BinaryVersion=v24.1.0 binaries (and automatic version upgrades are turned off);
+the cluster logical version remains v23.2.0. No upgrades are run. Then automatic
+upgrades are turned on, and `SET CLUSTER VERSION 24.1.0` is run in the
+background. At this point, all permanent and non-permanent upgrades > 23.2.0 and
+<= 24.1.0 are run, in order.
+
+Upgrades are run inside jobs, with one job per upgrade. A single job is ever
+created for a particular upgrade, across the cluster - i.e. nodes coordinate
+with one another when creating these jobs. Once an upgrade finishes, a row is
+left in system.migrations.
+
+Upgrades need to be idempotent: they might be run multiple times as the jobs
+error or nodes crash in the middle of running one of the jobs. However, if an
+upgrade has been run successfully by a binary with BinaryVersion=b, it is not
+run again by a binary with a different BinaryVersion. This is a useful guarantee
+for permanent upgrades, as it allows the code for an upgrade to change between
+versions (for example in response to updated bootstrap schema), without needing
+to worry about making the upgrade work for cluster being upgraded. Consider the
+following example: say v23.1 adds column "foo" to table system.users. This is
+done by adding the column to the bootstrap schema, and by creating an upgrade
+corresponding to version 23.1.<foo> doing the `ALTER TABLE` for old cluster, and
+probably a backfill for the table rows. Say that a permanent upgrade like
+`addRootUser()` has code inserting into system.users. If it were not for this
+guarantee, the `addRootUser()` code could not be changed to do:
+   INSERT INTO system.users (..., foo) ....
+If this query was run in a cluster being upgraded from 22.2 to 23.1, it would
+fail because the column doesn't exist yet (as the upgrade adding it has not yet
+run). This guarantee says that, if addRootUser() runs, it can expect the
+bootstrap schema that has "foo".
+*/
+
+package upgrade
