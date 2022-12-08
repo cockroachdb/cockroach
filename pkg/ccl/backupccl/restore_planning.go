3a59e57637827602ac175c89d21479f5db2dc054
Andy Yang <yang@cockroachlabs.com>
2022-12-08 14:11:19 -0500

    upgrades: delete V22_2SystemPrivilegesTable upgrade and version gates
    This patch deletes the `V22_2SystemPrivilegesTable` upgrade since its
    associated unit test starts to fail when the bootstrap schema for the
    `system.privileges` table is updated. This is safe to do since the
    release engineering team has previously stated they will bump
    `binaryMinSupportedVersion` before cutting the branch for 23.1.
    
    Release note: None

--- pkg/ccl/backupccl/restore_planning.go
+++ pkg/ccl/backupccl/restore_planning.go
@@ -1229,38 +1229,26 @@ func checkPrivilegesForRestore(
 		requiresRestoreSystemPrivilege := restoreStmt.DescriptorCoverage == tree.AllDescriptors ||
 			restoreStmt.Targets.TenantID.IsSet()
 
-		var hasRestoreSystemPrivilege bool
-		if p.ExecCfg().Settings.Version.IsActive(ctx, clusterversion.V22_2SystemPrivilegesTable) {
-			err := p.CheckPrivilegeForUser(ctx, syntheticprivilege.GlobalPrivilegeObject,
-				privilege.RESTORE, p.User())
-			hasRestoreSystemPrivilege = err == nil
-		}
-
-		if requiresRestoreSystemPrivilege && hasRestoreSystemPrivilege {
+		if requiresRestoreSystemPrivilege {
+			if err := p.CheckPrivilegeForUser(
+				ctx, syntheticprivilege.GlobalPrivilegeObject, privilege.RESTORE, p.User(),
+			); err != nil {
+				return pgerror.Newf(pgcode.InsufficientPrivilege,
+					"only users with the admin role or the RESTORE system privilege are allowed to perform"+
+						" a cluster restore")
+			}
 			return checkRestoreDestinationPrivileges(ctx, p, from)
-		} else if requiresRestoreSystemPrivilege && !hasRestoreSystemPrivilege {
-			return pgerror.Newf(pgcode.InsufficientPrivilege,
-				"only users with the admin role or the RESTORE system privilege are allowed to perform"+
-					" a cluster restore")
 		}
 	}
 
-	var hasRestoreSystemPrivilege bool
 	// If running a database restore, check that the user has the `RESTORE` system
 	// privilege.
 	//
 	// TODO(adityamaru): In 23.1 a missing `RESTORE` privilege should return an
 	// error. In 22.2 we continue to check for old style privileges and role
 	// options.
-	if len(restoreStmt.Targets.Databases) > 0 {
-		if p.ExecCfg().Settings.Version.IsActive(ctx, clusterversion.V22_2SystemPrivilegesTable) {
-			err := p.CheckPrivilegeForUser(ctx, syntheticprivilege.GlobalPrivilegeObject,
-				privilege.RESTORE, p.User())
-			hasRestoreSystemPrivilege = err == nil
-		}
-	}
-
-	if hasRestoreSystemPrivilege {
+	if len(restoreStmt.Targets.Databases) > 0 &&
+		p.CheckPrivilegeForUser(ctx, syntheticprivilege.GlobalPrivilegeObject, privilege.RESTORE, p.User()) == nil {
 		return checkRestoreDestinationPrivileges(ctx, p, from)
 	}
 
