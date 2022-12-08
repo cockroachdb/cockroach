446f3fc7bd7011831fb04df863c831c1339bf5cf
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-12-08 17:05:48 +0100

    kvserver: remove redundant assertion
    This was checked already at this point, earlier in `c.Stage`.
    
    Epic: CRDB-220
    Release note: None

--- pkg/kv/kvserver/replica_app_batch.go
+++ pkg/kv/kvserver/replica_app_batch.go
@@ -522,9 +522,6 @@ func (b *replicaAppBatch) runPostAddTriggers(ctx context.Context, cmd *replicate
 func (b *replicaAppBatch) stageTrivialReplicatedEvalResult(
 	ctx context.Context, cmd *replicatedCmd,
 ) {
-	if cmd.Index() == 0 {
-		log.Fatalf(ctx, "raft entry with index 0")
-	}
 	b.state.RaftAppliedIndex = cmd.Index()
 	b.state.RaftAppliedIndexTerm = cmd.Term
 
