354885d6852ec02912c72927eb7afcc9db1c6c50
Faizan Qazi <faizan@cockroachlabs.com>
2022-12-08 20:58:53 +0000

    sql: do not auto-commit inside planner if DDL is executed
    Previously, we had an optimization that allowed the planner
    to auto-commit after operations like insert, etc.. This
    was normally fine, but if we executed any DDL, then during
    commit processing we may need to do additional work. So,
    letting DML commit the transaction isn't safe. To address this,
    this patch will disable auto-commit inside the planner if we
    executed any DDL statements earlier.
    
    Fixes: #93010
    
    Release note (bug fix): In Postgres extended protocol mode it was possible for
    auto-commits to not execute certain logic for DDL, when certain
    DML (insert/update/delete) and DDL were combined in an implicit transaction.

--- pkg/sql/conn_executor.go
+++ pkg/sql/conn_executor.go
@@ -1679,6 +1679,7 @@ func (ns *prepStmtNamespace) resetTo(
 // transaction event, resetExtraTxnState invokes corresponding callbacks
 // (e.g. onTxnFinish() and onTxnRestart()).
 func (ex *connExecutor) resetExtraTxnState(ctx context.Context, ev txnEvent) {
+	ex.extraTxnState.numDDL = 0
 	ex.extraTxnState.firstStmtExecuted = false
 	ex.extraTxnState.hasAdminRoleCache = HasAdminRoleCache{}
 
