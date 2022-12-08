954a96c6a7344121e79d683d49589c3b3d305c2e
Nick Vigilante <vigilante@cockroachlabs.com>
2022-12-08 10:49:27 -0500

    docs issue generation: Update TC configuration to use new parameters
    Part of #72910
    
    Release note: None

--- build/teamcity/cockroach/create-docs-issue.sh
+++ build/teamcity/cockroach/create-docs-issue.sh
@@ -9,7 +9,7 @@ dir="$(dirname $(dirname $(dirname "${0}")))"
 source "$dir/teamcity-support.sh"
 source "$dir/teamcity-bazel-support.sh"
 
-BAZEL_SUPPORT_EXTRA_DOCKER_ARGS="-e GITHUB_API_TOKEN -e BUILD_VCS_NUMBER" run_bazel << 'EOF'
+BAZEL_SUPPORT_EXTRA_DOCKER_ARGS="-e GITHUB_API_TOKEN -e DOCS_ISSUE_GEN_END_TIME -e DOCS_ISSUE_GEN_START_TIME -e DRY_RUN_DOCS_ISSUE_GEN" run_bazel << 'EOF'
 bazel build --config ci //pkg/cmd/docs-issue-generation
 BAZEL_BIN=$(bazel info bazel-bin --config ci)
 $BAZEL_BIN/pkg/cmd/docs-issue-generation/docs-issue-generation_/docs-issue-generation
