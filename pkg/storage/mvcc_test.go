b3bdfbda7f9b438123098897c4ffb8bc204d18d3
Steven Danna <danna@cockroachlabs.com>
2022-12-10 11:51:22 +0000

    storage: move elastic CPU test hook out of fast path
    Epic: none
    Release note: None

--- pkg/storage/mvcc_test.go
+++ pkg/storage/mvcc_test.go
@@ -6046,6 +6046,7 @@ func TestMVCCExportToSSTExhaustedAtStart(t *testing.T) {
 				EndTS:              limits.maxTimestamp,
 				ExportAllRevisions: true,
 			})
+			require.False(t, firstCall)
 		})
 	t.Run("elastic CPU limit always exhausted",
 		func(t *testing.T) {
