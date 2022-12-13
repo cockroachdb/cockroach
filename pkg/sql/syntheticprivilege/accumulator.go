271435278e8a02dee5f935809250dee477a331b9
Rafi Shamim <rafi@cockroachlabs.com>
2022-12-13 14:34:43 -0500

    syntheticprivilege: add accumulator to make priv descriptors
    Release note: None

---
+++ pkg/sql/syntheticprivilege/accumulator.go
@@ -0,0 +1,75 @@
+package syntheticprivilege
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/security/username"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/privilege"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/errors"
+)
+
+// Accumulator accumulates different rows of system.privileges
+// and combines them into one PrivilegeDescriptor.
+type Accumulator struct {
+	desc       *catpb.PrivilegeDescriptor
+	objectType privilege.ObjectType
+	path       string
+}
+
+// NewAccumulator initializes a Accumulator.
+func NewAccumulator(objectType privilege.ObjectType, path string) *Accumulator {
+	return &Accumulator{
+		desc:       &catpb.PrivilegeDescriptor{},
+		objectType: objectType,
+		path:       path,
+	}
+}
+
+// AddRow adds a row from system.privileges into the accumulator.
+func (s *Accumulator) AddRow(path, user tree.DString, privArr, grantOptionArr *tree.DArray) error {
+	if string(path) != s.path {
+		return errors.AssertionFailedf("mismatched path in Accumulator; expected %s, got %s", s.path, path)
+	}
+
+	var privilegeStrings []string
+	for _, elem := range privArr.Array {
+		privilegeStrings = append(privilegeStrings, string(tree.MustBeDString(elem)))
+	}
+
+	var grantOptionStrings []string
+	for _, elem := range grantOptionArr.Array {
+		grantOptionStrings = append(grantOptionStrings, string(tree.MustBeDString(elem)))
+	}
+	privs, err := privilege.ListFromStrings(privilegeStrings)
+	if err != nil {
+		return err
+	}
+	grantOptions, err := privilege.ListFromStrings(grantOptionStrings)
+	if err != nil {
+		return err
+	}
+	privsWithGrantOption := privilege.ListFromBitField(
+		privs.ToBitField()&grantOptions.ToBitField(),
+		s.objectType,
+	)
+	privsWithoutGrantOption := privilege.ListFromBitField(
+		privs.ToBitField()&^privsWithGrantOption.ToBitField(),
+		s.objectType,
+	)
+	s.desc.Grant(
+		username.MakeSQLUsernameFromPreNormalizedString(string(user)),
+		privsWithGrantOption,
+		true, /* withGrantOption */
+	)
+	s.desc.Grant(
+		username.MakeSQLUsernameFromPreNormalizedString(string(user)),
+		privsWithoutGrantOption,
+		false, /* withGrantOption */
+	)
+	return nil
+}
+
+// GetDescriptor returns the privilege descriptor.
+func (s *Accumulator) GetDescriptor() *catpb.PrivilegeDescriptor {
+	return s.desc
+}
