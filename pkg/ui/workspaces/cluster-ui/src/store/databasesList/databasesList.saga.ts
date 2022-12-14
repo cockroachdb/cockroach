a8d008efceedcaad2bee8574f665485c10955cf8
gtr <gerardo@cockroachlabs.com>
2022-12-14 18:17:56 -0500

    ui: Populate database filter dropdown in stmts page with `SHOW DATABASES` sql-over-http call
    Fixes: #70461.
    
    Previously, the databases filter dropdown was populated by the
    `StatementsResponse` API call. This would result in some databases for
    which we do not receive any stmts to be ignored. According to above
    issue, the database filter-drop down should always be populated with
    cluster databases even when there are no statements or transactions for
    them. This commit populates the database filter dropdown using the
    `getDatabasesList()` API call which itself executes the `SHOW DATABASES`
    SQL query.
    
    Release note (ui change): The databases filter dropdown in the stmts
    page now uses the `getDatabasesList()` API call, resulting in all
    cluster databases showing up.

---
+++ pkg/ui/workspaces/cluster-ui/src/store/databasesList/databasesList.saga.ts
@@ -0,0 +1,34 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import { all, call, put, takeLatest } from "redux-saga/effects";
+
+import { actions } from "./databasesList.reducers";
+import { getDatabasesList } from "src/api";
+
+export function* refreshDatabasesListSaga() {
+  yield put(actions.request());
+}
+
+export function* requestDatabasesListSaga(): any {
+  try {
+    const result = yield call(getDatabasesList);
+    yield put(actions.received(result));
+  } catch (e) {
+    yield put(actions.failed(e));
+  }
+}
+
+export function* databasesListSaga() {
+  yield all([
+    takeLatest(actions.refresh, refreshDatabasesListSaga),
+    takeLatest(actions.request, requestDatabasesListSaga),
+  ]);
+}
