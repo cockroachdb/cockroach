de726447776cd60c9dab4de773bff261901b9450
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-12-14 16:14:16 +0100

    wip

--- gen.sh
+++ gen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # wget https://github.com/google/flatbuffers/releases/download/v22.10.26/Mac.flatc.binary.zip
 
-./flatc --warnings-as-errors --go --grpc -o ./pkg/kv/kvserver ./pkg/kv/kvserver/rafttransport/raft.fbs
+./flatc --warnings-as-errors --go --grpc -o ./pkg/kv/kvserver ./pkg/kv/kvserver/raftfbs/transport.fbs
