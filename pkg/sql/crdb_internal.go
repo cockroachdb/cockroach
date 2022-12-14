4a637ffca0c0e5b0217d9230ca874abcd8aee01f
Marius Posta <marius@cockroachlabs.com>
2022-12-14 08:57:40 -0500

    catkv: remove GetCatalogUnvalidated from Direct
    This commit replaces it with the GetAllFromStorageUnvalidated
    method on descs.Collection
    
    Release note: None

--- pkg/sql/crdb_internal.go
+++ pkg/sql/crdb_internal.go
@@ -5192,7 +5192,7 @@ CREATE TABLE crdb_internal.invalid_objects (
 	) error {
 		// The internalLookupContext will only have descriptors in the current
 		// database. To deal with this, we fall through.
-		c, err := p.Descriptors().Direct().GetCatalogUnvalidated(ctx, p.txn)
+		c, err := p.Descriptors().GetAllFromStorageUnvalidated(ctx, p.txn)
 		if err != nil {
 			return err
 		}
@@ -5582,7 +5582,7 @@ CREATE TABLE crdb_internal.lost_descriptors_with_data (
 		}
 		// Get all descriptors which will be used to determine
 		// which ones are missing.
-		c, err := p.Descriptors().Direct().GetCatalogUnvalidated(ctx, p.txn)
+		c, err := p.Descriptors().GetAllFromStorageUnvalidated(ctx, p.txn)
 		if err != nil {
 			return err
 		}
