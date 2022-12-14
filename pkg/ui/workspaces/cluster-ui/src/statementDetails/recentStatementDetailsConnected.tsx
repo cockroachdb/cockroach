9f024e6511db22233efa40009242144ed07db6ab
Matthew Todd <todd@cockroachlabs.com>
2022-12-14 11:39:22 -0500

    ui: preserve backwards compatibility for managed-service
    These components were renamed in #91860, but the support in
    managed-service for loading multiple versions of cluster-ui doesn't like
    it when we do that; it expects changes to be purely additive.
    
    We choose here to provide aliases to the old names, rather than undoing
    the rename completely, so that we can eventually switch managed-service
    over to the new names once the old versions have fallen out of the
    support window.
    
    Epic: none
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/statementDetails/recentStatementDetailsConnected.tsx
+++ pkg/ui/workspaces/cluster-ui/src/statementDetails/recentStatementDetailsConnected.tsx
@@ -47,3 +47,11 @@ const mapDispatchToProps = (
 export const RecentStatementDetailsPageConnected = withRouter(
   connect(mapStateToProps, mapDispatchToProps)(RecentStatementDetails),
 );
+
+// Prior to 23.1, this component was called
+// ActiveStatementDetailsPageConnected. We keep the alias here to avoid
+// breaking the multi-version support in managed-service's console code.
+// When managed-service drops support for 22.2 (around the end of 2024?),
+// we can remove this code.
+export const ActiveStatementDetailsPageConnected =
+  RecentStatementDetailsPageConnected;
