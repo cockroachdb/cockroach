8fc9ec2e7d122b3431eedaaf8dc547dbef7bff00
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-12-15 14:17:56 +0100

    raftlog: clarify command en/decoding, replace DecodeRaftCommand
    `DecodeRaftCommand` was error-prone because it expected to be handed the
    `entry.Data` belonging to a `raftpb.Entry` of `Type` `raftpb.EntryNormal`.
    
    This wasn't enforced or clear, which caused potential problems in a few
    usages.
    
    A `raftpb.Entry` used by CockroachDB (is either empty or) always stores
    a `kvserverpb.RaftCommand`. However, that `kvserverpb.RaftCommand` is
    never "just" the `Entry.Data` slice. For "regular" log entries and also
    those which we optimize for SST ingestion, the `Entry.Data` slice is a
    byte tag followed by eight bytes (CmdIDKey) followed by the marshaled
    `RaftCommand`. For configuration changes, it's a
    `raftpb.ConfChange{,V2}` wrapping a `kvserverpb.ConfChangeContext`
    wrapping a `kvserverpb.RaftCommand`.
    
    This inhomogeneity was previously not encapsulated well. In this commit,
    we expand the set of recognized
    `kvserverbase.RaftCommandEncodingVersion`s to explicitly reflect empty
    entries and configuration changes.
    
    We move away from the previous pattern which could have made the
    impression that "all raftpb.Entry are encoded equally".
    
    Instead, the first operation is to detect the type of entry via a call
    to `raftlog.EncodingVersion(raftpb.Entry)` and then acting accordingly.
    It turns out that for all current callers, this is enough since they can
    then either decide to do nothing, or special case with a follow-up call
    to `DecomposeRaftVersion{Sideloaded,Standard}` in case the CmdIDKey is
    needed cheaply, or fully decode the Entry (in the case of conf changes).
    
    `DecodeRaftCommand` is now gone.
    
    This change highlighted a potential (but not actual) bug: in
    `MaybeSideloadEntries`, we were looking at the first byte of
    `(Entry).Data` to determine whether this was a sideloaded command.
    However, this was only valid when `Entry.Type` was `raftpb.EntryNormal`;
    the other types are used for Raft configuration changes and
    `(Entry).Data` in that case will represent a marshaled
    `raftpb.ConfChange` and not something created by
    `kvserverpb.EncodeRaftCommand`.
    
    Luckily, the first byte of a marshaled `raftpb.ConfChange{,V2}` is
    always `0x8`[^1] which doesn't match `RaftVersionSideloaded` (which
    equals one), so the code was "functionally correct". After this PR, it's
    also semantically correct.
    
    [^1]: https://github.com/etcd-io/raft/blob/eaa6808e1f7ab2247c13778250f70520b0527ff1/raftpb/raft.pb.go#L1102
    
    Epic: CRDB-220
    Release note: None

--- pkg/kv/kvserver/logstore/logstore.go
+++ pkg/kv/kvserver/logstore/logstore.go
@@ -325,7 +325,11 @@ func LoadTerm(
 		// sideloaded entries here to keep the term fetching cheap.
 		// TODO(pavelkalinnikov): consider not caching here, after measuring if it
 		// makes any difference.
-		if v, _, _ := kvserverbase.DecodeRaftCommand(entry.Data); v != kvserverbase.RaftVersionSideloaded {
+		typ, err := raftlog.EncodingVersion(entry)
+		if err != nil {
+			return 0, err
+		}
+		if typ != kvserverbase.RaftVersionSideloaded {
 			eCache.Add(rangeID, []raftpb.Entry{entry}, false /* truncate */)
 		}
 		return entry.Term, nil
@@ -398,7 +402,11 @@ func LoadEntries(
 		}
 		expectedIndex++
 
-		if v, _, _ := kvserverbase.DecodeRaftCommand(ent.Data); v == kvserverbase.RaftVersionSideloaded {
+		typ, err := raftlog.EncodingVersion(ent)
+		if err != nil {
+			return err
+		}
+		if typ == kvserverbase.RaftVersionSideloaded {
 			newEnt, err := MaybeInlineSideloadedRaftCommand(
 				ctx, rangeID, ent, sideloaded, eCache,
 			)
