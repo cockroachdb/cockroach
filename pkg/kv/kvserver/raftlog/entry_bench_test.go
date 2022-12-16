fa97947bf2e472bc717a774116225a4e0a501c1a
Tobias Grieger <tobias.b.grieger@gmail.com>
2022-12-16 13:51:06 +0100

    raftlog: add marshal/unmarshal/memcpy bench
    Wanted a benchmark to get an idea of how bad an allocation vs a
    non-allocation memcpy is, as our current raft entry encoding requires
    a few rounds of proto marshaling/unmarshaling that don't seem necessary.
    
    My take-away is that the first order of business is getting rid of the
    allocations (whose extra cost in GC isn't even tracked here), and that
    improvements from avoiding memcpy are likely smaller.
    
    Condensed output (M1 Pro)
    
    ```
    goos: darwin
    goarch: arm64
    pkg: github.com/cockroachdb/cockroach/pkg/kv/kvserver/raftlog
    
    slice,size=0_B-10                  2.064 ns/op         0 mb/s   0 allocs/op
    marshal,size=0_B-10                22.78 ns/op         0 mb/s   0 allocs/op
    unmarshal-no-reuse,size=0_B-10     60.75 ns/op         0 mb/s   1 allocs/op
    unmarshal-reuse,size=0_B-10        23.49 ns/op         0 mb/s   0 allocs/op
    slice,size=128_B-10                3.134 ns/op     38955 mb/s   0 allocs/op
    marshal,size=128_B-10              26.99 ns/op      4523 mb/s   0 allocs/op
    unmarshal-no-reuse,size=128_B-10   104.2 ns/op      1172 mb/s   2 allocs/op
    unmarshal-reuse,size=128_B-10      29.47 ns/op      4143 mb/s   0 allocs/op
    slice,size=512_B-10                7.771 ns/op     62831 mb/s   0 allocs/op
    marshal,size=512_B-10              31.21 ns/op     15646 mb/s   0 allocs/op
    unmarshal-no-reuse,size=512_B-10   164.0 ns/op      2977 mb/s   2 allocs/op
    unmarshal-reuse,size=512_B-10      34.65 ns/op     14091 mb/s   0 allocs/op
    slice,size=1.0_KiB-10              14.17 ns/op     68934 mb/s   0 allocs/op
    marshal,size=1.0_KiB-10            38.56 ns/op     25323 mb/s   0 allocs/op
    unmarshal-no-reuse,size=1.0_KiB-10 245.4 ns/op      3979 mb/s   2 allocs/op
    unmarshal-reuse,size=1.0_KiB-10    39.91 ns/op     24471 mb/s   0 allocs/op
    slice,size=8.0_KiB-10              107.6 ns/op     72596 mb/s   0 allocs/op
    marshal,size=8.0_KiB-10            128.4 ns/op     60841 mb/s   0 allocs/op
    unmarshal-no-reuse,size=8.0_KiB-10 1464 ns/op       5335 mb/s   2 allocs/op
    unmarshal-reuse,size=8.0_KiB-10    131.1 ns/op     59569 mb/s   0 allocs/op
    slice,size=16_KiB-10               205.2 ns/op     76133 mb/s   0 allocs/op
    marshal,size=16_KiB-10             235.1 ns/op     66451 mb/s   0 allocs/op
    unmarshal-no-reuse,size=16_KiB-10  2351 ns/op       6645 mb/s   2 allocs/op
    unmarshal-reuse,size=16_KiB-10     246.6 ns/op     63358 mb/s   0 allocs/op
    slice,size=32_KiB-10               451.0 ns/op     69285 mb/s   0 allocs/op
    marshal,size=32_KiB-10             493.0 ns/op     63383 mb/s   0 allocs/op
    unmarshal-no-reuse,size=32_KiB-10  4986 ns/op       6267 mb/s   2 allocs/op
    unmarshal-reuse,size=32_KiB-10     492.6 ns/op     63438 mb/s   0 allocs/op
    slice,size=64_KiB-10               759.1 ns/op     82330 mb/s   0 allocs/op
    marshal,size=64_KiB-10             836.3 ns/op     74727 mb/s   0 allocs/op
    unmarshal-no-reuse,size=64_KiB-10  9868 ns/op       6334 mb/s   2 allocs/op
    unmarshal-reuse,size=64_KiB-10     931.7 ns/op     67076 mb/s   0 allocs/op
    slice,size=128_KiB-10              1582 ns/op      78995 mb/s   0 allocs/op
    marshal,size=128_KiB-10            1719 ns/op      72701 mb/s   0 allocs/op
    unmarshal-no-reuse,size=128_KiB-10 17912 ns/op      6978 mb/s   2 allocs/op
    unmarshal-reuse,size=128_KiB-10    1813 ns/op      68962 mb/s   0 allocs/op
    slice,size=1.0_MiB-10              14821 ns/op     67468 mb/s   0 allocs/op
    marshal,size=1.0_MiB-10            16104 ns/op     62093 mb/s   0 allocs/op
    unmarshal-no-reuse,size=1.0_MiB-10 97271 ns/op     10279 mb/s   2 allocs/op
    unmarshal-reuse,size=1.0_MiB-10    21924 ns/op     45610 mb/s   0 allocs/op
    ```
    
    Epic: None
    Release note: None

--- pkg/kv/kvserver/raftlog/entry_bench_test.go
+++ pkg/kv/kvserver/raftlog/entry_bench_test.go
@@ -13,7 +13,12 @@ package raftlog
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvserverpb"
+	"github.com/cockroachdb/cockroach/pkg/util/humanizeutil"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 )
 
 func BenchmarkNewEntry(b *testing.B) {
@@ -38,6 +43,82 @@ func BenchmarkNewEntry(b *testing.B) {
 						e.Release()
 					}
 				}
+			})
+		}
+	}
+}
+
+func BenchmarkMarshalUnmarshal(b *testing.B) {
+	const kb = 1 << 10
+	for _, bytes := range []int64{
+		0,
+		128,
+		512,
+		1 * kb,
+		8 * kb,
+		16 * kb,
+		32 * kb,
+		64 * kb,
+		128 * kb,
+		1 * kb * kb, // 1mb
+	} {
+		for _, typ := range []string{
+			"slice",
+			"marshal",
+			"unmarshal-no-reuse",
+			"unmarshal-reuse",
+		} {
+			b.Run(fmt.Sprintf("type=%s,size=%s", typ, humanizeutil.IBytes(bytes)), func(b *testing.B) {
+				src := make([]byte, bytes)
+				_, _ = rand.Read(src)
+
+				var work func()
+				if typ == "slice" {
+					dst := make([]byte, bytes)
+					work = func() {
+						copy(dst, src)
+					}
+				} else {
+					cmd := &kvserverpb.RaftCommand{}
+					cmd.WriteBatch = &kvserverpb.WriteBatch{Data: src}
+					dst := make([]byte, cmd.Size())
+					marshal := func() {
+						if _, err := cmd.MarshalToSizedBuffer(dst); err != nil {
+							b.Fatal(err)
+						}
+					}
+					if typ == "marshal" {
+						work = marshal
+					} else {
+						marshal() // populate dst
+						var cmd2 kvserverpb.RaftCommand
+						reset := cmd2.Reset
+						if typ == "unmarshal-reuse" {
+							reset = func() {
+								if cmd2.WriteBatch != nil {
+									// Reuse as much memory as possible. Should not need to allocate.
+									cmd2.WriteBatch.Data = cmd2.WriteBatch.Data[:0]
+								}
+							}
+						}
+						work = func() {
+							reset()
+							if err := cmd2.Unmarshal(dst); err != nil {
+								b.Fatal(err)
+							}
+						}
+					}
+				}
+
+				b.ReportAllocs()
+				tBegin := timeutil.Now()
+				b.ResetTimer()
+				for i := 0; i < b.N; i++ {
+					work()
+				}
+				b.StopTimer()
+				dur := timeutil.Since(tBegin)
+				b.ReportMetric(float64(int64(b.N)*bytes)/(dur.Seconds()*kb*kb), "mb/s")
 			})
 		}
 	}
