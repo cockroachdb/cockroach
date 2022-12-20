c46630932de5b5d00b7171ff9f4c46edba2ce9c2
Lidor Carmel <lidor@cockroachlabs.com>
2022-12-20 14:02:02 -0800

    streamingest: add a new state for replicating tenant, INITIALIZING REPLICATION
    Until the initial scan is completed we cannot cutover to the secondary
    tenant - the state of that tenant will be, with this PR,
    `INITIALIZING REPLICATION`. The tenant can stay in that state for some
    time.
    
    Note that with this change the replicated tenant cannot be in the `ADD`
    state.
    
    TODO: we should add a `REPLICATION CUTOVER` state.
    
    Epic: CRDB-18752
    
    Release note: None

--- pkg/ccl/streamingccl/streamingest/stream_replication_e2e_test.go
+++ pkg/ccl/streamingccl/streamingest/stream_replication_e2e_test.go
@@ -1280,6 +1280,14 @@ func TestTenantStreamingShowTenant(t *testing.T) {
 	defer cleanup()
 	producerJobID, ingestionJobID := c.startStreamReplication(ctx)
 
+	rowStr := c.destSysSQL.QueryStr(t, fmt.Sprintf("SHOW TENANT %s WITH REPLICATION STATUS", args.destTenantName))
+	require.Equal(t, "2", rowStr[0][0])
+	require.Equal(t, "destination", rowStr[0][1])
+	if rowStr[0][3] == "NULL" {
+		// There is no source yet, therefore the replication is not fully initialized.
+		require.Equal(t, "INITIALIZING REPLICATION", rowStr[0][2])
+	}
+
 	jobutils.WaitForJobToRun(c.t, c.srcSysSQL, jobspb.JobID(producerJobID))
 	jobutils.WaitForJobToRun(c.t, c.destSysSQL, jobspb.JobID(ingestionJobID))
 	highWatermark := c.srcCluster.Server(0).Clock().Now()
