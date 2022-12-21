2824339fd8a19f8e582ac30fec0cce948ce38bf5
Sean Barag <barag@cockroachlabs.com>
2022-12-21 14:12:49 -0800

    ui: use NPM dependencies mirrored to GCS bucket instead of yarn-vendored
    Now that tooling exists to mirror NPM dependencies and rewrite the
    relevant yarn.lock entries [1], there's nothing stopping those
    dependencies from getting mirrored and being used. ./dev ui mirror-deps
    has already been run. Update the lockfiles, switch to online
    installation when fetching dependencies with Bazel, and remove both the
    "seed_yarn_cache" hack and the pkg/ui/yarn-vendor submodule.
    
    [1] https://github.com/cockroachdb/cockroach/pull/94102
    
    Part of: #85038
    Release note (build change): Required NPM dependencies are now fetched
    from a Google Cloud Storage bucket managed by Cockroach Labs instead of
    from a git submodule committed in-tree.

--- pkg/ui/workspaces/e2e-tests/build/compose-entrypoint.sh
+++ pkg/ui/workspaces/e2e-tests/build/compose-entrypoint.sh
@@ -9,14 +9,10 @@ mkdir /scratch
 cp --recursive --no-target-directory --no-preserve=owner /e2e /scratch
 cd /scratch
 
-# Write a minimal .yarnrc mirroring the one in cockroach.git/pkg/ui, to avoid
-# mounting that entire tree.
-echo "yarn-offline-mirror /yarn-vendor" > .yarnrc
-
 # Remove and reinstall any node_modules that may have been copied, since they're
 # potentially specific to the host platform.
 rm -rf node_modules/
-yarn install --force --offline
+yarn install --force --pure-lockfile
 
 # Run tests, passing extra CLI arguments through to Cypress
 yarn cy:run "$@"
