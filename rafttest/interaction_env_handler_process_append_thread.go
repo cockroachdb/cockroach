etcd-io/etcd

raft: support asynchronous storage writes
Fixes #12257.

This change adds opt-in support to raft to perform local storage writes
asynchronously from the raft state machine handling loop.

A new AsyncStorageWrites configuration instructs the raft node to write to its
local storage (raft log and state machine) using a request/response message
passing interface instead of the default `Ready`/`Advance` function call
interface. Local storage messages can be pipelined and processed asynchronously
(with respect to `Ready` iteration), facilitating reduced interference between
Raft proposals and increased batching of log appends and state machine
application. As a result, use of asynchronous storage writes can reduce
end-to-end commit latency and increase maximum throughput.

When AsyncStorageWrites is enabled, the `Ready.Message` slice will include new
`MsgStorageAppend` and `MsgStorageApply` messages. The messages will target a
`LocalAppendThread` and a `LocalApplyThread`, respectively. Messages to the same
target must be reliably processed in order. In other words, they can't be
dropped (like messages over the network) and those targeted at the same thread
can't be reordered. Messages to different targets can be processed in any order.

`MsgStorageAppend` carries Raft log entries to append, election votes to persist,
and snapshots to apply. All writes performed in response to a `MsgStorageAppend`
are expected to be durable. The message assumes the role of the Entries,
HardState, and Snapshot fields in Ready.

`MsgStorageApply` carries committed entries to apply. The message assumes
the role of the CommittedEntries field in Ready.

Local messages each carry one or more response messages which should be
delivered after the corresponding storage write has been completed. These
responses may target the same node or may target other nodes. The storage
threads are not responsible for understanding the response messages, only
for delivering them to the correct target after perfo

raft/rafttest/interaction_env_handler_process_append_thread.go
@@ -1,0 +1,95 @@
+// Copyright 2022 The etcd Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rafttest
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cockroachdb/datadriven"
+	"go.etcd.io/etcd/raft/v3"
+	"go.etcd.io/etcd/raft/v3/raftpb"
+)
+
+func (env *InteractionEnv) handleProcessAppendThread(t *testing.T, d datadriven.TestData) error {
+	idxs := nodeIdxs(t, d)
+	for _, idx := range idxs {
+		var err error
+		if len(idxs) > 1 {
+			fmt.Fprintf(env.Output, "> %d processing append thread\n", idx+1)
+			env.withIndent(func() { err = env.ProcessAppendThread(idx) })
+		} else {
+			err = env.ProcessAppendThread(idx)
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// ProcessAppendThread runs processes a single message on the "append" thread of
+// the node with the given index.
+func (env *InteractionEnv) ProcessAppendThread(idx int) error {
+	n := &env.Nodes[idx]
+	if len(n.AppendWork) == 0 {
+		return fmt.Errorf("no append work to perform")
+	}
+	m := n.AppendWork[0]
+	n.AppendWork = n.AppendWork[1:]
+
+	resps := m.Responses
+	m.Responses = nil
+	env.Output.WriteString("Processing:\n")
+	env.Output.WriteString(raft.DescribeMessage(m, defaultEntryFormatter) + "\n")
+	var st raftpb.HardState
+	if m.HardState != nil {
+		st = *m.HardState
+	}
+	var snap raftpb.Snapshot
+	if m.Snapshot != nil {
+		snap = *m.Snapshot
+	}
+	if err := processAppend(n, st, m.Entries, snap); err != nil {
+		return err
+	}
+
+	env.Output.WriteString("Responses:\n")
+	for _, m := range resps {
+		env.Output.WriteString(raft.DescribeMessage(m, defaultEntryFormatter) + "\n")
+	}
+	env.Messages = append(env.Messages, resps...)
+	return nil
+}
+
+func processAppend(n *Node, st raftpb.HardState, ents []raftpb.Entry, snap raftpb.Snapshot) error {
+	// TODO(tbg): the order of operations here is not necessarily safe. See:
+	// https://github.com/etcd-io/etcd/pull/10861
+	s := n.Storage
+	if !raft.IsEmptyHardState(st) {
+		if err := s.SetHardState(st); err != nil {
+			return err
+		}
+	}
+	if err := s.Append(ents); err != nil {
+		return err
+	}
+	if !raft.IsEmptySnap(snap) {
+		if err := s.ApplySnapshot(snap); err != nil {
+			return err
+		}
+	}
+	return nil
+}

raft/rafttest/interaction_env_handler_process_apply_thread.go
@@ -1,0 +1,109 @@
+// Copyright 2022 The etcd Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rafttest
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cockroachdb/datadriven"
+	"go.etcd.io/etcd/raft/v3"
+	"go.etcd.io/etcd/raft/v3/raftpb"
+)
+
+func (env *InteractionEnv) handleProcessApplyThread(t *testing.T, d datadriven.TestData) error {
+	idxs := nodeIdxs(t, d)
+	for _, idx := range idxs {
+		var err error
+		if len(idxs) > 1 {
+			fmt.Fprintf(env.Output, "> %d processing apply thread\n", idx+1)
+			env.withIndent(func() { err = env.ProcessApplyThread(idx) })
+		} else {
+			err = env.ProcessApplyThread(idx)
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// ProcessApplyThread runs processes a single message on the "apply" thread of
+// the node with the given index.
+func (env *InteractionEnv) ProcessApplyThread(idx int) error {
+	n := &env.Nodes[idx]
+	if len(n.ApplyWork) == 0 {
+		return fmt.Errorf("no apply work to perform")
+	}
+	m := n.ApplyWork[0]
+	n.ApplyWork = n.ApplyWork[1:]
+
+	resps := m.Responses
+	m.Responses = nil
+	env.Output.WriteString("Processing:\n")
+	env.Output.WriteString(raft.DescribeMessage(m, defaultEntryFormatter) + "\n")
+	if err := processApply(n, m.Entries); err != nil {
+		return err
+	}
+
+	env.Output.WriteString("Responses:\n")
+	for _, m := range resps {
+		env.Output.WriteString(raft.DescribeMessage(m, defaultEntryFormatter) + "\n")
+	}
+	env.Messages = append(env.Messages, resps...)
+	return nil
+}
+
+func processApply(n *Node, ents []raftpb.Entry) error {
+	for _, ent := range ents {
+		var update []byte
+		var cs *raftpb.ConfState
+		switch ent.Type {
+		case raftpb.EntryConfChange:
+			var cc raftpb.ConfChange
+			if err := cc.Unmarshal(ent.Data); err != nil {
+				return err
+			}
+			update = cc.Context
+			cs = n.RawNode.ApplyConfChange(cc)
+		case raftpb.EntryConfChangeV2:
+			var cc raftpb.ConfChangeV2
+			if err := cc.Unmarshal(ent.Data); err != nil {
+				return err
+			}
+			cs = n.RawNode.ApplyConfChange(cc)
+			update = cc.Context
+		default:
+			update = ent.Data
+		}
+
+		// Record the new state by starting with the current state and applying
+		// the command.
+		lastSnap := n.History[len(n.History)-1]
+		var snap raftpb.Snapshot
+		snap.Data = append(snap.Data, lastSnap.Data...)
+		// NB: this hard-codes an "appender" state machine.
+		snap.Data = append(snap.Data, update...)
+		snap.Metadata.Index = ent.Index
+		snap.Metadata.Term = ent.Term
+		if cs == nil {
+			sl := n.History
+			cs = &sl[len(sl)-1].Metadata.ConfState
+		}
+		snap.Metadata.ConfState = *cs
+		n.History = append(n.History, snap)
+	}
+	return nil
+}

cockroachdb/cockroach

rafttest/interaction_env_handler_process_append_thread.go
@@ -1,0 +1,97 @@
+// Copyright 2022 The etcd Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rafttest
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cockroachdb/datadriven"
+
+	"go.etcd.io/raft/v3"
+	"go.etcd.io/raft/v3/raftpb"
+)
+
+func (env *InteractionEnv) handleProcessAppendThread(t *testing.T, d datadriven.TestData) error {
+	idxs := nodeIdxs(t, d)
+	for _, idx := range idxs {
+		var err error
+		if len(idxs) > 1 {
+			fmt.Fprintf(env.Output, "> %d processing append thread\n", idx+1)
+			env.withIndent(func() { err = env.ProcessAppendThread(idx) })
+		} else {
+			err = env.ProcessAppendThread(idx)
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// ProcessAppendThread runs processes a single message on the "append" thread of
+// the node with the given index.
+func (env *InteractionEnv) ProcessAppendThread(idx int) error {
+	n := &env.Nodes[idx]
+	if len(n.AppendWork) == 0 {
+		env.Output.WriteString("no append work to perform")
+		return nil
+	}
+	m := n.AppendWork[0]
+	n.AppendWork = n.AppendWork[1:]
+
+	resps := m.Responses
+	m.Responses = nil
+	env.Output.WriteString("Processing:\n")
+	env.Output.WriteString(raft.DescribeMessage(m, defaultEntryFormatter) + "\n")
+	var st raftpb.HardState
+	if m.HardState != nil {
+		st = *m.HardState
+	}
+	var snap raftpb.Snapshot
+	if m.Snapshot != nil {
+		snap = *m.Snapshot
+	}
+	if err := processAppend(n, st, m.Entries, snap); err != nil {
+		return err
+	}
+
+	env.Output.WriteString("Responses:\n")
+	for _, m := range resps {
+		env.Output.WriteString(raft.DescribeMessage(m, defaultEntryFormatter) + "\n")
+	}
+	env.Messages = append(env.Messages, resps...)
+	return nil
+}
+
+func processAppend(n *Node, st raftpb.HardState, ents []raftpb.Entry, snap raftpb.Snapshot) error {
+	// TODO(tbg): the order of operations here is not necessarily safe. See:
+	// https://github.com/etcd-io/etcd/pull/10861
+	s := n.Storage
+	if !raft.IsEmptyHardState(st) {
+		if err := s.SetHardState(st); err != nil {
+			return err
+		}
+	}
+	if err := s.Append(ents); err != nil {
+		return err
+	}
+	if !raft.IsEmptySnap(snap) {
+		if err := s.ApplySnapshot(snap); err != nil {
+			return err
+		}
+	}
+	return nil
+}

rafttest/interaction_env_handler_process_apply_thread.go
@@ -1,0 +1,111 @@
+// Copyright 2022 The etcd Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rafttest
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cockroachdb/datadriven"
+
+	"go.etcd.io/raft/v3"
+	"go.etcd.io/raft/v3/raftpb"
+)
+
+func (env *InteractionEnv) handleProcessApplyThread(t *testing.T, d datadriven.TestData) error {
+	idxs := nodeIdxs(t, d)
+	for _, idx := range idxs {
+		var err error
+		if len(idxs) > 1 {
+			fmt.Fprintf(env.Output, "> %d processing apply thread\n", idx+1)
+			env.withIndent(func() { err = env.ProcessApplyThread(idx) })
+		} else {
+			err = env.ProcessApplyThread(idx)
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// ProcessApplyThread runs processes a single message on the "apply" thread of
+// the node with the given index.
+func (env *InteractionEnv) ProcessApplyThread(idx int) error {
+	n := &env.Nodes[idx]
+	if len(n.ApplyWork) == 0 {
+		env.Output.WriteString("no apply work to perform")
+		return nil
+	}
+	m := n.ApplyWork[0]
+	n.ApplyWork = n.ApplyWork[1:]
+
+	resps := m.Responses
+	m.Responses = nil
+	env.Output.WriteString("Processing:\n")
+	env.Output.WriteString(raft.DescribeMessage(m, defaultEntryFormatter) + "\n")
+	if err := processApply(n, m.Entries); err != nil {
+		return err
+	}
+
+	env.Output.WriteString("Responses:\n")
+	for _, m := range resps {
+		env.Output.WriteString(raft.DescribeMessage(m, defaultEntryFormatter) + "\n")
+	}
+	env.Messages = append(env.Messages, resps...)
+	return nil
+}
+
+func processApply(n *Node, ents []raftpb.Entry) error {
+	for _, ent := range ents {
+		var update []byte
+		var cs *raftpb.ConfState
+		switch ent.Type {
+		case raftpb.EntryConfChange:
+			var cc raftpb.ConfChange
+			if err := cc.Unmarshal(ent.Data); err != nil {
+				return err
+			}
+			update = cc.Context
+			cs = n.RawNode.ApplyConfChange(cc)
+		case raftpb.EntryConfChangeV2:
+			var cc raftpb.ConfChangeV2
+			if err := cc.Unmarshal(ent.Data); err != nil {
+				return err
+			}
+			cs = n.RawNode.ApplyConfChange(cc)
+			update = cc.Context
+		default:
+			update = ent.Data
+		}
+
+		// Record the new state by starting with the current state and applying
+		// the command.
+		lastSnap := n.History[len(n.History)-1]
+		var snap raftpb.Snapshot
+		snap.Data = append(snap.Data, lastSnap.Data...)
+		// NB: this hard-codes an "appender" state machine.
+		snap.Data = append(snap.Data, update...)
+		snap.Metadata.Index = ent.Index
+		snap.Metadata.Term = ent.Term
+		if cs == nil {
+			sl := n.History
+			cs = &sl[len(sl)-1].Metadata.ConfState
+		}
+		snap.Metadata.ConfState = *cs
+		n.History = append(n.History, snap)
+	}
+	return nil
+}