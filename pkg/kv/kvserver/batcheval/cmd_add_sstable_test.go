e31333c61059abd7f996b28ad4c692dd5572678b
Erik Grinaker <grinaker@cockroachlabs.com>
2022-12-21 13:13:06 +0000

    storage: fix `GCBytesAge` in `CheckSSTConflicts` for covered point tombstone
    When `CheckSSTConflicts` ingests a point key above a point tombstone
    covered by a range tombstone, it incorrectly considered the key to be
    deleted at the range tombstone, not at the point tombstone. This patch
    fixes this calculation.
    
    Release note: None

--- pkg/kv/kvserver/batcheval/cmd_add_sstable_test.go
+++ pkg/kv/kvserver/batcheval/cmd_add_sstable_test.go
@@ -419,6 +419,14 @@ func TestEvalAddSSTable(t *testing.T) {
 			sst:        kvs{pointKV("a", 3, "sst")},
 			expectErr:  "inline values are unsupported",
 		},
+		// Regression test for https://github.com/cockroachdb/cockroach/issues/94053.
+		"DisallowConflicts MVCC stats with point tombstone below range tombstone": {
+			noConflict:     true,
+			data:           kvs{rangeKV("a", "c", 3, ""), pointKV("b", 2, ""), pointKV("b", 1, "b1")},
+			sst:            kvs{pointKV("b", 5, "sst")},
+			expect:         kvs{rangeKV("a", "c", 3, ""), pointKV("b", 5, "sst"), pointKV("b", 2, ""), pointKV("b", 1, "b1")},
+			expectStatsEst: !storage.DisableCheckSSTRangeKeyMasking, // assert correct without masking
+		},
 
 		// DisallowShadowing
 		"DisallowShadowing errors above existing": {
