cockroachdb/cockroach

commit e532c656e60b3286e52dbaeb748938090fbb6b38

Update etcd/raft to 285e4437
This change imports etcd/raft at 285e4437, and fixes the usage of now nillable
Snapshot field.

Release note: none
Epic: none

pkg/kv/kvserver/raft.go
@@ -206,7 +206,7 @@ func raftDescribeMessage(m raftpb.Message, f raft.EntryFormatter) string {
 		}
 		fmt.Fprintf(&buf, "]")
 	}
-	if !raft.IsEmptySnap(m.Snapshot) {
+	if m.Snapshot != nil && !raft.IsEmptySnap(*m.Snapshot) {
 		snap := m.Snapshot
 		snap.Data = nil
 		fmt.Fprintf(&buf, " Snapshot:%v", snap)

DEPS.bzl
@@ -9001,20 +9001,10 @@ def go_deps():
         ],
         build_file_proto_mode = "default",
         importpath = "go.etcd.io/etcd/raft/v3",
-        sha256 = "fdd8f069bde875b9a7abaf71337eb7331ee2ca68ed9374b2bec1ad8969e114a2",
-        strip_prefix = "go.etcd.io/etcd/raft/v3@v3.6.0-alpha.0.0.20221108125646-2a1055c7f3a7",
+        sha256 = "04d1e7e39e2e1ffce70e063b12a0cd7ff7e7be9acab1678fb79c006a95712114",
+        strip_prefix = "go.etcd.io/etcd/raft/v3@v3.6.0-alpha.0.0.20221126025132-285e44378fa1",
         urls = [
-            "https://storage.googleapis.com/cockroach-godeps/gomod/go.etcd.io/etcd/raft/v3/io_etcd_go_etcd_raft_v3-v3.6.0-alpha.0.0.20221108125646-2a1055c7f3a7.zip",
-        ],
-    )
-    go_repository(
-        name = "io_etcd_go_raft_v3",
-        build_file_proto_mode = "disable_global",
-        importpath = "go.etcd.io/raft/v3",
-        sha256 = "61cb8ce8d354ee2f7f809b74b3f92aab0e1f6fbecdd751239368085a57ee004f",
-        strip_prefix = "go.etcd.io/raft/v3@v3.0.0-20221201111702-eaa6808e1f7a",
-        urls = [
-            "https://storage.googleapis.com/cockroach-godeps/gomod/go.etcd.io/raft/v3/io_etcd_go_raft_v3-v3.0.0-20221201111702-eaa6808e1f7a.zip",
+            "https://storage.googleapis.com/cockroach-godeps/gomod/go.etcd.io/etcd/raft/v3/io_etcd_go_etcd_raft_v3-v3.6.0-alpha.0.0.20221126025132-285e44378fa1.zip",
         ],
     )
     go_repository(

build/bazelutil/distdir_files.bzl
@@ -864,8 +864,7 @@ DISTDIR_FILES = {
     "https://storage.googleapis.com/cockroach-godeps/gomod/go.etcd.io/etcd/client/pkg/v3/io_etcd_go_etcd_client_pkg_v3-v3.5.0.zip": "c0ca209767c5734c6ed023888ba5be02aab5bd3c4d018999467f2bfa8bf65ee3",
     "https://storage.googleapis.com/cockroach-godeps/gomod/go.etcd.io/etcd/client/v2/io_etcd_go_etcd_client_v2-v2.305.0.zip": "91fcb507fe8c193844b56bfb6c8741aaeb6ffa11ee9043de2af0f141173679f3",
     "https://storage.googleapis.com/cockroach-godeps/gomod/go.etcd.io/etcd/io_etcd_go_etcd-v0.5.0-alpha.5.0.20200910180754-dd1b699fc489.zip": "d982ee501979b41b68625693bad77d15e4ae79ab9d0eae5f6028205f96a74e49",
-    "https://storage.googleapis.com/cockroach-godeps/gomod/go.etcd.io/etcd/raft/v3/io_etcd_go_etcd_raft_v3-v3.6.0-alpha.0.0.20221108125646-2a1055c7f3a7.zip": "fdd8f069bde875b9a7abaf71337eb7331ee2ca68ed9374b2bec1ad8969e114a2",
-    "https://storage.googleapis.com/cockroach-godeps/gomod/go.etcd.io/raft/v3/io_etcd_go_raft_v3-v3.0.0-20221201111702-eaa6808e1f7a.zip": "61cb8ce8d354ee2f7f809b74b3f92aab0e1f6fbecdd751239368085a57ee004f",
+    "https://storage.googleapis.com/cockroach-godeps/gomod/go.etcd.io/etcd/raft/v3/io_etcd_go_etcd_raft_v3-v3.6.0-alpha.0.0.20221126025132-285e44378fa1.zip": "04d1e7e39e2e1ffce70e063b12a0cd7ff7e7be9acab1678fb79c006a95712114",
     "https://storage.googleapis.com/cockroach-godeps/gomod/go.mongodb.org/mongo-driver/org_mongodb_go_mongo_driver-v1.5.1.zip": "446cff132e82c64af7ffcf48e268eb16ec81f694914aa6baecb06cbbae1be0d7",
     "https://storage.googleapis.com/cockroach-godeps/gomod/go.mozilla.org/pkcs7/org_mozilla_go_pkcs7-v0.0.0-20200128120323-432b2356ecb1.zip": "3c4c1667907ff3127e371d44696326bad9e965216d4257917ae28e8b82a9e08d",
     "https://storage.googleapis.com/cockroach-godeps/gomod/go.opencensus.io/io_opencensus_go-v0.23.0.zip": "81c78beb84872084d6d5ddc0a0bffc47294412898472c891a29cfcb66f3fa2d8",

go.mod
@@ -16,7 +16,7 @@ require (
 	github.com/google/btree v1.0.1
 	github.com/google/pprof v0.0.0-20210827144239-02619b876842
 	github.com/google/uuid v1.3.0
-	go.etcd.io/etcd/raft/v3 v3.6.0-alpha.0.0.20221108125646-2a1055c7f3a7
+	go.etcd.io/etcd/raft/v3 v3.6.0-alpha.0.0.20221126025132-285e44378fa1
 	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8
 	golang.org/x/exp v0.0.0-20220104160115-025e73f80486
 	golang.org/x/exp/typeparams v0.0.0-20220713135740-79cabaa25d75 // indirect

go.sum
@@ -2265,8 +2265,8 @@ go.etcd.io/etcd v0.5.0-alpha.5.0.20200910180754-dd1b699fc489/go.mod h1:yVHk9ub3C
 go.etcd.io/etcd/api/v3 v3.5.0/go.mod h1:cbVKeC6lCfl7j/8jBhAK6aIYO9XOjdptoxU/nLQcPvs=
 go.etcd.io/etcd/client/pkg/v3 v3.5.0/go.mod h1:IJHfcCEKxYu1Os13ZdwCwIUTUVGYTSAM3YSwc9/Ac1g=
 go.etcd.io/etcd/client/v2 v2.305.0/go.mod h1:h9puh54ZTgAKtEbut2oe9P4L/oqKCVB6xsXlzd7alYQ=
-go.etcd.io/etcd/raft/v3 v3.6.0-alpha.0.0.20221108125646-2a1055c7f3a7 h1:mE43O2IN+1AKeNpEt6M0bzwgn0ja1sDNDOTXWYUhcMM=
-go.etcd.io/etcd/raft/v3 v3.6.0-alpha.0.0.20221108125646-2a1055c7f3a7/go.mod h1:zA67K91XlNPihTwsx3RnnKQRRNmdZ38SH/9WvMPte1w=
+go.etcd.io/etcd/raft/v3 v3.6.0-alpha.0.0.20221126025132-285e44378fa1 h1:qmI5+rIGvX0Gp4tUJxSULKDl4VVU2GoiewA126MGMqE=
+go.etcd.io/etcd/raft/v3 v3.6.0-alpha.0.0.20221126025132-285e44378fa1/go.mod h1:mXtYCG30wr2Hn2qRurHKWQUkn5E4BWhcUpzACb3C9hw=
 go.mongodb.org/mongo-driver v1.0.3/go.mod h1:u7ryQJ+DOzQmeO7zB6MHyr8jkEQvC8vH7qLUO4lqsUM=
 go.mongodb.org/mongo-driver v1.1.1/go.mod h1:u7ryQJ+DOzQmeO7zB6MHyr8jkEQvC8vH7qLUO4lqsUM=
 go.mongodb.org/mongo-driver v1.1.2/go.mod h1:u7ryQJ+DOzQmeO7zB6MHyr8jkEQvC8vH7qLUO4lqsUM=

pkg/kv/kvserver/client_raft_test.go
@@ -1470,7 +1470,9 @@ func TestFailedSnapshotFillsReservation(t *testing.T) {
 			ToReplica:   rep2Desc,
 		},
 	}
-	header.RaftMessageRequest.Message.Snapshot.Data = uuid.UUID{}.GetBytes()
+	header.RaftMessageRequest.Message.Snapshot = &raftpb.Snapshot{
+		Data: uuid.UUID{}.GetBytes(),
+	}
 	// Cause this stream to return an error as soon as we ask it for something.
 	// This injects an error into HandleSnapshotStream when we try to send the
 	// "snapshot accepted" message.

pkg/kv/kvserver/replica_command.go
@@ -2853,7 +2853,7 @@ func (r *Replica) followerSendSnapshot(
 				From:     uint64(req.CoordinatorReplica.ReplicaID),
 				To:       uint64(req.RecipientReplica.ReplicaID),
 				Term:     req.Term,
-				Snapshot: snap.RaftSnap,
+				Snapshot: &snap.RaftSnap,
 			},
 		},
 		RangeSize:           rangeSize,

pkg/kv/kvserver/store_snapshot.go
@@ -1438,7 +1438,7 @@ func SendEmptySnapshot(
 			To:       uint64(to.ReplicaID),
 			From:     uint64(from.ReplicaID),
 			Term:     hs.Term,
-			Snapshot: outgoingSnap.RaftSnap,
+			Snapshot: &outgoingSnap.RaftSnap,
 		},
 	}
 

commit c8e4473dde1a84831eda81bd17aa0d33c43b3d48

kv: don't mutate raft snapshot when logging raft ready
This commit fixes a subtle bug that we introduced in e532c65 when we
picked up etcd/raft at 285e4437. With `Message.Snapshot` now a pointer,
logic in `raftDescribeMessage` began unintentionally mutating the
message's snapshot, instead of mutating a local copy.

This commit fixes the bug. We only hit this path when verbose raft
logging is enabled, so I doubt anyone hit this issue in the two weeks
that this issue has sat on master.

I confirmed that we didn't make the same mistake anywhere else.

Release note: None
Epic: None

pkg/kv/kvserver/raft.go
@@ -207,8 +207,8 @@ func raftDescribeMessage(m raftpb.Message, f raft.EntryFormatter) string {
 		}
 		fmt.Fprintf(&buf, "]")
 	}
-	if m.Snapshot != nil && !raft.IsEmptySnap(*m.Snapshot) {
-		snap := m.Snapshot
+	if m.Snapshot != nil {
+		snap := *m.Snapshot
 		snap.Data = nil
 		fmt.Fprintf(&buf, " Snapshot:%v", snap)
 	}