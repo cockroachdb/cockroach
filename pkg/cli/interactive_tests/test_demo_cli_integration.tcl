dd3ad7d3bc5f734264858b941c04c8900d2223bd
Raphael 'kena' Poss <knz@thaumogen.net>
2022-12-27 15:27:41 +0100

    cli/demo: simplify the TLS configuration
    TLDR: this patch ensures a separate SQL client can resume the
    connection to `cockroach demo` when it is restarted, without
    reconfiguring the connection URL. It also enables running `cockroach
    sql` against a `demo` session with fewer/simpler arguments.
    
    **Change 1 (enhancement): reuse directory**
    
    Prior to this patch, `cockroach demo` was auto-generating new TLS
    keys/certs on every run, and using a different directory every time.
    
    Especially the different directory made the tool difficult to use: it
    was forcing the connection URLs to change on every invocation.
    
    To fix this, this patch now reuses the same directory,
    `$HOME/.cockroach-demo`, for every invocation. Certificate/key files
    in that directory are reused if already present.
    
    This also makes it possible for SQL clients to simply reconnect
    (reusing the same connection string) when the `demo` session is
    restarted.
    
    NB: The reason why this change does not use `$HOME/.cockroach-certs`
    is that it should remain possible to run `cockroach demo` alongside a
    regular server, and the node certs for a regular server should contain
    different metadata than for a demo server (esp. relating to server
    addresses).
    
    **Change 2 (enhancement): also produce a TLS client cert for the `demo` user**
    
    Prior to this patch, `demo` would only write a TLS client cert for the
    `root` user in the certificate directory. This was preventing direct
    use of TLS client cert authentication for the `demo` user,
    even by those clients that support TLS client cert authn (such as
    `cockroach sql`).
    
    This patch fixes this by also producing the TLS client cert for the
    `demo` user.
    
    As a result, running `cockroach sql` against the `demo` cluster
    is now possible **without entering a password**.
    
    Passwordless access is also possible from other SQL clients by
    providing the `demo` client cert and key in the connection URL.
    This is now simplified thanks to change 1 above, as the certificate
    directory is now always the same.
    
    **Change 3 (enhancement): show how to use use `cockroach sql`**
    
    Prior to this patch, `demo` would only print out connection URLs on
    startup and `\demo ls`. Because the connection URLs are relatively
    long, it was making it complex and error-prone to copy-paste them
    properly for use in a separate `cockroach sql` session.
    
    This patch prints out an example `cockroach sql` invocation alongside
    the connection URLs.
    
    For example:
    ```
    ...
    (cli) cockroach sql --certs-dir=/data/home/kena/.cockroach-demo -u demo -p 26258
    (sql) postgresql://demo:demo99186@127.0.0.1:26258/defaultdb?sslmode=require&sslrootcert=%2Fdata%2Fhome%2Fkena%2F.cockroach-demo%2Fca.crt
    ...
    ```
    
    **Change 4 (enhancement): simpler multi-tenant TLS**
    
    Prior to this patch, `cockroach demo` was generating the tenant TLS
    configuration using a separate CA key/cert pair. This is not needed
    for a demo session, and was also forcing every SQL client that wants
    to connect to a secondary tenant server to specify the tenant CA cert
    in connection URLs.
    
    This patch simplifies this by reusing the same CA for simulating
    secondary tenants. This also enables running `cockroach sql` without
    extra arguments to connect to a secondary tenant server.
    
    Release note (cli change): The TLS parameters to connect to a
    `cockroach demo` cluster from SQL clients has been simplified.

---
+++ pkg/cli/interactive_tests/test_demo_cli_integration.tcl
@@ -0,0 +1,75 @@
+#! /usr/bin/env expect -f
+
+source [file join [file dirname $argv0] common.tcl]
+
+set ::env(COCKROACH_INSECURE) "false"
+
+spawn $argv demo --empty --no-line-editor --multitenant=true
+eexpect "Welcome"
+
+start_test "Check that the cli connect instructions get printed out."
+eexpect "Application tenant"
+eexpect "(cli)"
+eexpect "cockroach sql"
+eexpect " -u demo"
+end_test
+
+eexpect "defaultdb>"
+
+set demo_spawn_id $spawn_id
+
+set ::env(COCKROACH_CERTS_DIR) $env(HOME)/.cockroach-demo
+
+start_test "Check that a SQL shell can connect to the app tenant without special arguments as demo"
+spawn $argv sql --no-line-editor -u demo
+eexpect "Welcome"
+eexpect demo@
+eexpect "defaultdb>"
+send "table system.tenants;\r"
+eexpect "ERROR"
+eexpect "does not exist"
+eexpect "defaultdb>"
+send "\\q\r"
+eexpect eof
+end_test
+
+start_test "Check that a SQL shell can connect to the app tenant without special arguments as root"
+spawn $argv sql --no-line-editor -u root
+eexpect "Welcome"
+eexpect root@
+eexpect "defaultdb>"
+send "table system.tenants;\r"
+eexpect "ERROR"
+eexpect "does not exist"
+eexpect "defaultdb>"
+send "\\q\r"
+eexpect eof
+end_test
+
+start_test "Check that a SQL shell can connect to the system tenant without special arguments as demo"
+spawn $argv sql --no-line-editor -u demo -p 26258
+eexpect "Welcome"
+eexpect demo@
+eexpect "defaultdb>"
+send "table system.tenants;\r"
+eexpect "2 rows"
+eexpect "defaultdb>"
+send "\\q\r"
+eexpect eof
+end_test
+
+start_test "Check that a SQL shell can connect to the app tenant without special arguments as root"
+spawn $argv sql --no-line-editor -u root -p 26258
+eexpect "Welcome"
+eexpect root@
+eexpect "defaultdb>"
+send "table system.tenants;\r"
+eexpect "2 rows"
+eexpect "defaultdb>"
+send "\\q\r"
+eexpect eof
+end_test
+
+set spawn_id $demo_spawn_id
+send "\\q\r"
+eexpect eof
