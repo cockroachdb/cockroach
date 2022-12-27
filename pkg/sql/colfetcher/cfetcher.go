7d7a54689d16c919edf77f46e43fdb670de0f3da
Yahor Yuzefovich <yahor@cockroachlabs.com>
2022-12-27 11:00:56 -0800

    Revert "Revert "WIP on allocating new large batches in cFetcher""
    This reverts commit a2d9008fea22b80f126a54da3b06614369d5fba6.

--- pkg/sql/colfetcher/cfetcher.go
+++ pkg/sql/colfetcher/cfetcher.go
@@ -176,6 +176,9 @@ type cFetcherArgs struct {
 	// single set of spans. This allows the cFetcher to close itself eagerly,
 	// once it finishes the first fetch.
 	singleUse bool
+	// allocateFreshBatches indicates whether the cFetcher will allocate new
+	// batches on each Next invocation, or instead just reuse a single batch.
+	allocateFreshBatches bool
 	// TODO: comment.
 	allowNullsInNonNullableOnLastRowInBatch bool
 }
@@ -308,9 +311,16 @@ func (cf *cFetcher) resetBatch() {
 			tuplesToBeSet = int(cf.estimatedRowCount)
 		}
 	}
-	cf.machine.batch, reallocated = cf.accountingHelper.ResetMaybeReallocate(
-		cf.table.typs, cf.machine.batch, tuplesToBeSet,
-	)
+	if cf.allocateFreshBatches {
+		cf.machine.batch = cf.accountingHelper.AllocateNewBatch(
+			cf.table.typs, cf.machine.batch, tuplesToBeSet,
+		)
+		reallocated = true
+	} else {
+		cf.machine.batch, reallocated = cf.accountingHelper.ResetMaybeReallocate(
+			cf.table.typs, cf.machine.batch, tuplesToBeSet,
+		)
+	}
 	if reallocated {
 		cf.machine.colvecs.SetBatch(cf.machine.batch)
 		// Pull out any requested system column output vecs.
