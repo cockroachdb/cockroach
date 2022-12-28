5ea271cd7ba2fe54c239076eaa982e3aab81f2e5
Austen McClernon <austen@cockroachlabs.com>
2022-12-28 16:34:11 +0000

    asim: move exchange into gossip pkg
    This patch moves `exchange.go` and `exchange_test.go` into a separate
    pkg. These files were previously tracked under `asim/state`. This
    separation enforces stricter interaction between the state and gossip
    components within asim for the purpose of modularity.
    
    Release note: None

--- pkg/kv/kvserver/asim/op/controller_test.go
+++ pkg/kv/kvserver/asim/op/controller_test.go
@@ -18,6 +18,7 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/allocator/allocatorimpl"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/config"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/gossip"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/state"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/stretchr/testify/require"
@@ -283,7 +284,7 @@ func TestRelocateRangeOp(t *testing.T) {
 				}
 			}
 
-			exchange := state.NewFixedDelayExhange(
+			exchange := gossip.NewFixedDelayExhange(
 				start,
 				time.Second,
 				time.Second*0, /* no state update delay */
