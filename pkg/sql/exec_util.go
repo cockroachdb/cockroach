0581379e928320014c003c4c9b5d2205e3373ada
Marcus Gartner <marcus@cockroachlabs.com>
2022-12-28 15:48:45 -0500

    sql: add optimizer_use_improved_disjunction_stats session setting
    This commit adds the `optimizer_use_improved_disjunction_stats` session
    setting that defaults to `false`. When `true`, the improved disjunction
    logic added in #89358 is performed. When `false`, the logic reverts to
    the previous logic. This setting will allow use to backport the new
    logic without impacting query plans of running clusters by defaulting
    the setting to false.
    
    Release note: None

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -3344,6 +3344,10 @@ func (m *sessionDataMutator) SetVariableInequalityLookupJoinEnabled(val bool) {
 	m.data.VariableInequalityLookupJoinEnabled = val
 }
 
+func (m *sessionDataMutator) SetOptimizerUseImprovedDisjunctionStats(val bool) {
+	m.data.OptimizerUseImprovedDisjunctionStats = val
+}
+
 // Utility functions related to scrubbing sensitive information on SQL Stats.
 
 // quantizeCounts ensures that the Count field in the
