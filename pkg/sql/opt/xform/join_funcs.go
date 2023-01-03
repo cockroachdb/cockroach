75ced1fdc08ff6aa17dcf49278b84226792ff5c4
Mark Sirek <sirek@cockroachlabs.com>
2023-01-02 23:01:47 -0800

    sql: execution-time erroring of remote region access for enforce_home_region setting
    Informs #83819
    
    enforce_home_region phase 2
    ---------------------------
    This adds dynamic checking of whether locality-optimized searches
    and locality-optimized lookup joins must access rows in remote regions.
    If the `enforce_home_region` session setting is true, and remote access is
    required in these operations, the query errors out. Both vectorized and
    non-vectorized execution is handled.
    
    Locality-optimized search is handled by setting an exclusive upper bound
    on the inputs to the `SerialUnorderedSynchronizer` built for this operation,
    and erroring out if the synchronizer iterates to that input before the query
    finishes execution due to a LimitOp consumer.
    
    Locality-optimized joins besides antijoin are handled by erroring out if
    `joinReader.performLookup` calls `jr.strategy.generateRemoteSpans` and
    is about to issue a lookup to a remote region.
    
    Locality-optimized antijoins come as a pair, first the local lookup join,
    and then the output of that feeds into a remote lookup join. This PR adds
    tagging of that 2nd lookup join as having only lookups of remote rows so
    that any attempted lookup by that 2nd join errors out.
    
    Release note: None

--- pkg/sql/opt/xform/join_funcs.go
+++ pkg/sql/opt/xform/join_funcs.go
@@ -1286,6 +1286,18 @@ func (c *CustomFuncs) EmptyFiltersExpr() memo.FiltersExpr {
 	return memo.EmptyFiltersExpr
 }
 
+// CreateRemoteOnlyLookupJoinPrivate creates a new lookup join private from the
+// given private and replaces the LookupExpr with the given filter. It also
+// marks the private as locality optimized and as a join which only does lookups
+// into remote regions.
+func (c *CustomFuncs) CreateRemoteOnlyLookupJoinPrivate(
+	remoteLookupExpr memo.FiltersExpr, private *memo.LookupJoinPrivate,
+) *memo.LookupJoinPrivate {
+	newPrivate := c.CreateLocalityOptimizedLookupJoinPrivate(remoteLookupExpr, c.e.funcs.EmptyFiltersExpr(), private)
+	newPrivate.RemoteOnlyLookups = true
+	return newPrivate
+}
+
 // CreateLocalityOptimizedLookupJoinPrivate creates a new lookup join private
 // from the given private and replaces the LookupExpr and RemoteLookupExpr with
 // the given filters. It also marks the private as locality optimized.
@@ -2148,5 +2160,6 @@ func (c *CustomFuncs) mapInputSideOfLookupJoin(
 	constFilters := c.e.f.RemapCols(&lookupJoinExpr.ConstFilters, colMap).(*memo.FiltersExpr)
 	mappedLookupJoinExpr.ConstFilters = *constFilters
 	mappedLookupJoinExpr.Locking = lookupJoinExpr.Locking
+	mappedLookupJoinExpr.RemoteOnlyLookups = lookupJoinExpr.RemoteOnlyLookups
 	return mappedLookupJoinExpr
 }
