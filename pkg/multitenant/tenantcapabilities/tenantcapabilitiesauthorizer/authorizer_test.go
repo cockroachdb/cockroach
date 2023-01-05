1286dcc6eb5921324f91d42207dfc6db27069468
Arul Ajmani <arulajmani@gmail.com>
2023-01-05 09:40:34 -0500

    tenantcapabilities: introduce a Watcher over system.tenants
    This patch introduces three new interfaces -- a Watcher, a Reader,
    and Authorizer. They're not hooked up yet, but once they are, they'll
    work together to provide (in-memory) capability checks for incoming
    tenant requests.
    
    The Watcher establishes a rangefeed over `system.tenants` to
    incrementally (and transparently) maintain an in-memory view of the
    global tenant capability state. Publicly, it exposes a `Reader`
    interface.
    
    The `Reader` provides access to the global tenant capability state.
    The `Watcher` and `Authorizer` communicate with each other using the
    `Reader` interface.
    
    The `Authorizer` consults the global tenant capability state to perform
    authorization checks for incoming requests issued by tenants. Part of
    the motivation to structure the code as such is to expand the set of
    inputs the `Authorizer` uses to authorize requests. One could imagine
    other dependencies being injected into the `Authorizer` in the future.
    
    In this PR, we conservatively estimate a tenant capabilities proto to be
    50 bytes in size. Given the `Watcher` keeps the entire capabilities
    state in memory, this means that for 10,0000 tenants, we would have a
    very sane memory footprint of 500KB.
    
    Epic: CRDB-18503
    References: #94643
    
    Release note: None

---
+++ pkg/multitenant/tenantcapabilities/tenantcapabilitiesauthorizer/authorizer_test.go
@@ -0,0 +1,89 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package tenantcapabilitiesauthorizer
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/multitenant/tenantcapabilities"
+	"github.com/cockroachdb/cockroach/pkg/multitenant/tenantcapabilities/tenantcapabilitiespb"
+	"github.com/cockroachdb/cockroach/pkg/multitenant/tenantcapabilities/tenantcapabilitiestestutils"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/testutils/datapathutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/datadriven"
+)
+
+// TestDataDriven runs datadriven tests against the Authorizer interface. The
+// syntax is as follows:
+//
+// "update-state": updates the underlying global tenant capability state.
+// Example:
+//
+//	update-state
+//	upsert {ten=10}:{CanAdminSplit=true}
+//	delete {ten=15}
+//	----
+//
+// "has-capability-for-batch": performs a capability check, given a tenant and
+// batch request declaration. Example:
+//
+//	has-capability-for-batch
+//	{ten=10}
+//	split
+//	----
+func TestDataDriven(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	datadriven.Walk(t, datapathutils.TestDataPath(t), func(t *testing.T, path string) {
+		mockReader := mockReader(make(map[roachpb.TenantID]tenantcapabilitiespb.TenantCapabilities))
+		authorizer := New(mockReader)
+
+		datadriven.RunTest(t, path, func(t *testing.T, d *datadriven.TestData) string {
+			switch d.Cmd {
+			case "update-state":
+				updates := tenantcapabilitiestestutils.ParseTenantCapabilityUpdateStateArguments(t, d.Input)
+				mockReader.updateState(updates)
+
+			case "has-capability-for-batch":
+				tenID, ba := tenantcapabilitiestestutils.ParseBatchRequestString(t, d.Input)
+				hasCapability := authorizer.HasCapabilityForBatch(context.Background(), tenID, &ba)
+				return fmt.Sprintf("%t", hasCapability)
+
+			default:
+				return fmt.Sprintf("unknown command %s", d.Cmd)
+			}
+			return "ok"
+		})
+	})
+}
+
+type mockReader map[roachpb.TenantID]tenantcapabilitiespb.TenantCapabilities
+
+func (m mockReader) updateState(updates []tenantcapabilities.Update) {
+	for _, update := range updates {
+		if update.Deleted {
+			delete(m, update.TenantID)
+		} else {
+			m[update.TenantID] = update.TenantCapabilities
+		}
+	}
+}
+
+// GetCapabilities implements the tenantcapabilities.Reader interface.
+func (m mockReader) GetCapabilities(
+	id roachpb.TenantID,
+) (tenantcapabilitiespb.TenantCapabilities, bool) {
+	cp, found := m[id]
+	return cp, found
+}
