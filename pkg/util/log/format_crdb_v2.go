5f47225b2703b1721ff6db09dd87cc87995602bb
Alex Barganier <abarganier@cockroachlabs.com>
2023-01-06 12:24:26 -0500

    pkg/util/log: don't use "-" to represent empty tags when decoding v2
    Previously, a crdb_v2 log entry without any tags would use `[-]` in
    the log entry to indicate that the tags for the entry were empty.
    
    With recent changes ensuring that all logs are tagged with the
    tenant ID (crdb_v1, crdb_v2, json), we never expect a log entry
    with empty tags.
    
    When decoding the crdb_v2 format, we forgot to update the decoder
    to no longer set "-" in the tags field when no tags other than
    the tenant ID tag were available. This does not play nicely
    when re-formatting the logpb.Entry back into crdb_v1 format
    (commonly done in our debugging tools such as debug merge-logs)
    since it would create entries with tag strings such as:
    ```
    [T1,-]
    ```
    
    This patch updates the crdb_v2 format's decoder to no longer
    use `-` to denote empty tags. Instead, an empty string on the
    tags field is used which is much more compatible with writing
    the entry back into the crdb_v1 format.
    
    Note that the decoder still properly interprets `-` as an
    empty tags list, in case a newer CRDB version is used to
    decode crdb_v2 logs generated from an older version.
    
    Release note: none

--- pkg/util/log/format_crdb_v2.go
+++ pkg/util/log/format_crdb_v2.go
@@ -705,7 +705,7 @@ func (f entryDecoderV2Fragment) getTags(editor redactEditor) string {
 	case "":
 		fallthrough
 	case "-":
-		return "-"
+		return ""
 	default:
 		r := editor(redactablePackage{
 			msg:        []byte(tagsStr),
