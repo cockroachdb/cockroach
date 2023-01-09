ca00a9469aaeb85a130d8ad179893b1503abdf3a
Nick Vigilante <vigilante@cockroachlabs.com>
2023-01-09 12:47:05 -0500

    lint: Remove epic and issue ref linter from cockroach repo
    This PR is one of two PRs needed to move the epic and issue ref linter
    from the cockroach repo to Blathers.
    
    This PR should not be merged until cockroachlabs/blathers-bot#92 is
    merged.
    
    Release note: None
    
    Part of #90189

--- build/github/pr-epic-issue-ref-lint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-PR_NUMBER="$(echo "$GITHUB_REF" | awk -F '/' '{print $3}')"
-
-GITHUB_TOKEN="${GITHUB_TOKEN}" bin/bazel run //pkg/cmd/lint-epic-issue-refs:lint-epic-issue-refs "$PR_NUMBER"
