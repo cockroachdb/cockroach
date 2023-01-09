1f9879a9680b90d2b37e9c3830c0980494cf9bac
Rail Aliiev <rail@iqchoice.com>
2023-01-09 23:56:17 +0000

    release: split release process
    Previously, the release publishing step contained building, signing and
    other operations that can happen in advance.
    
    This PR splits the release process into 2 steps: build/sign and
    publishing. Artifacts generated during the build stage are published to
    a "staged" bucket and copied to the final bucket during the publishing
    phase.
    
    * Extracted docker verification into a function.
    * The version is read from a file now.
    * Use `gsutil` in MacOS signing in order to download from private repos.
    * Removed `PRE_RELEASE` variable set from TeamCity in favour of
      "guessing" it from the version.
    * Simplified dry-run logic to define version/build names, because the
      version is defined in the source code now.
    * The publishing phase updates docker image packages.
    
    Epic: RE-307
    Fixes: RE-211
    Release note: None

--- build/release/teamcity-support.sh
+++ build/release/teamcity-support.sh
@@ -23,6 +23,14 @@ docker_login_with_google() {
   echo "${google_credentials}" | docker login -u _json_key --password-stdin "https://${gcr_hostname}"
 }
 
+docker_login_gcr() {
+  local repo=$1
+  local credentials=$2
+  local hostname="${repo%%/*}"
+  # https://cloud.google.com/container-registry/docs/advanced-authentication#json-key
+  echo "${credentials}" | docker login -u _json_key --password-stdin "https://${hostname}"
+}
+
 docker_login() {
   echo "${DOCKER_ACCESS_TOKEN}" | docker login --username "${DOCKER_ID}" --password-stdin
 }
@@ -46,3 +54,54 @@ EOF
 docker_login_with_redhat() {
   echo "${REDHAT_REGISTRY_KEY}" | docker login --username unused --password-stdin $rhel_registry
 }
+
+verify_docker_image(){
+  local img=$1
+  local docker_platform=$2
+  local expected_sha=$3
+  local expected_build_tag=$4
+  local fips_build=$5
+  local error=0
+
+  docker rmi "$img" || true
+  docker pull --platform="$docker_platform" "$img"
+
+  local output=$(docker run --platform="$docker_platform" "$img" version)
+  build_type=$(grep "^Build Type:" <<< "$output" | cut -d: -f2 | sed 's/ //g')
+  sha=$(grep "^Build Commit ID:" <<< "$output" | cut -d: -f2 | sed 's/ //g')
+  build_tag=$(grep "^Build Tag:" <<< "$output" | cut -d: -f2 | sed 's/ //g')
+  go_version=$(grep "^Go Version:" <<< "$output" | cut -d: -f2 | sed 's/ //g')
+
+  # Build Type should always be "release"
+  if [ "$build_type" != "release" ]; then
+    echo "ERROR: Release type mismatch, expected 'release', got '$build_type'"
+    error=1
+  fi
+  if [ "$sha" != "$expected_sha" ]; then
+    echo "ERROR: SHA mismatch, expected '$expected_sha', got '$sha'"
+    error=1
+  fi
+  if [ "$build_tag" != "$expected_build_tag" ]; then
+    echo "ERROR: Build tag mismatch, expected '$expected_build_tag', got '$build_tag'"
+    error=1
+  fi
+
+  build_tag_output=$(docker run --platform="$docker_platform" "$img" version --build-tag)
+  if [ "$build_tag_output" != "$expected_build_tag" ]; then
+    echo "ERROR: Build tag from 'cockroach version --build-tag' mismatch, expected '$expected_build_tag', got '$build_tag_output'"
+    error=1
+  fi
+  if [[ $fips_build == true ]]; then
+    if [[ "$go_version" != *"fips" ]]; then
+      echo "ERROR: Go version '$go_version' does not contain 'fips'"
+      error=1
+    fi
+    openssl_version_output=$(docker run --platform="$docker_platform" "$img" shell -c "openssl version")
+    if [[ $openssl_version_output != *"FIPS"* ]]; then
+      echo "ERROR: openssl version '$openssl_version_output' does not contain 'FIPS'"
+      error=1
+    fi
+  fi
+  return $error
+}
+
