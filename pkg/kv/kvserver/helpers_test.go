3d51c79d527871333bb690220da2d0ac90fc793d
Pavel Kalinnikov <pavel@cockroachlabs.com>
2023-01-10 15:02:31 +0000

    kvserver: separate adding replicas to Store maps
    In this commit addReplicaInternalLocked now requires the replica to be in the
    replicasByRangeID map unconditionally, i.e. that addReplicaToRangeMapLocked has
    been previously called for this replica.
    
    This holds true in the production code because each Replica is either:
    - created and initialized in Store.Start, in which case it's added to both maps
    - created in getOrCreateReplica, and later initialized in a split trigger; in
      this case the additions to the map happen sequentially in two places
    
    Release note: none

--- pkg/kv/kvserver/helpers_test.go
+++ pkg/kv/kvserver/helpers_test.go
@@ -67,6 +67,9 @@ func (s *Store) FindTargetAndTransferLease(
 func (s *Store) AddReplica(repl *Replica) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
+	if err := s.addReplicaToRangeMapLocked(repl); err != nil {
+		return err
+	}
 	if err := s.addReplicaInternalLocked(repl); err != nil {
 		return err
 	}
