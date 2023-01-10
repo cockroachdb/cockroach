bd287409ce703e96953ba0a0d1b72d5ec8f229b8
Marius Posta <marius@cockroachlabs.com>
2023-01-10 16:20:53 -0500

    catalog: replace ColumnNamesForIDs method with function
    This commit refacts this method as a function.
    
    Informs #91924.
    
    Release note: None

--- pkg/sql/check.go
+++ pkg/sql/check.go
@@ -185,7 +185,7 @@ func nonMatchingRowQuery(
 	indexIDForValidation descpb.IndexID,
 	limitResults bool,
 ) (sql string, originColNames []string, _ error) {
-	originColNames, err := srcTbl.NamesForColumnIDs(fk.OriginColumnIDs)
+	originColNames, err := catalog.ColumnNamesForIDs(srcTbl, fk.OriginColumnIDs)
 	if err != nil {
 		return "", nil, err
 	}
@@ -215,7 +215,7 @@ func nonMatchingRowQuery(
 		qualifiedSrcCols[i] = fmt.Sprintf("s.%s", srcCols[i])
 	}
 
-	referencedColNames, err := targetTbl.NamesForColumnIDs(fk.ReferencedColumnIDs)
+	referencedColNames, err := catalog.ColumnNamesForIDs(targetTbl, fk.ReferencedColumnIDs)
 	if err != nil {
 		return "", nil, err
 	}
@@ -291,7 +291,7 @@ func validateForeignKey(
 ) error {
 	nCols := len(fk.OriginColumnIDs)
 
-	referencedColumnNames, err := targetTable.NamesForColumnIDs(fk.ReferencedColumnIDs)
+	referencedColumnNames, err := catalog.ColumnNamesForIDs(targetTable, fk.ReferencedColumnIDs)
 	if err != nil {
 		return err
 	}
@@ -378,7 +378,7 @@ func duplicateRowQuery(
 	indexIDForValidation descpb.IndexID,
 	limitResults bool,
 ) (sql string, colNames []string, _ error) {
-	colNames, err := srcTbl.NamesForColumnIDs(columnIDs)
+	colNames, err := catalog.ColumnNamesForIDs(srcTbl, columnIDs)
 	if err != nil {
 		return "", nil, err
 	}
