13a72ed04cf5c0f990ac91621c022d54be08f2de
Tobias Grieger <tobias.b.grieger@gmail.com>
2023-01-11 14:16:11 +0100

    rditer: revamp TestSelect
    It's now a bit less... quirky. Also, it now exercises the case of r1,
    where the StartKey is RKeyMin but should be LocalMax.
    
    Epic: none
    Release note: None

--- pkg/kv/kvserver/rditer/select_test.go
+++ pkg/kv/kvserver/rditer/select_test.go
@@ -24,60 +24,57 @@ import (
 func TestSelect(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 
-	type tc struct {
-		// Inputs.
+	w := echotest.NewWalker(t, datapathutils.TestDataPath(t, t.Name()))
+	for _, tc := range []struct {
 		name string
-		opts SelectOpts
-
-		// Outputs.
-		s []roachpb.Span
-	}
-
-	var tcs []tc
-
-	for _, span := range []roachpb.RSpan{
-		{},
+		sp   roachpb.RSpan
+	}{
 		{
-			Key:    roachpb.RKey("a"),
-			EndKey: roachpb.RKey("c"),
+			name: "no_span",
+		},
+		{
+			name: "r1",
+			sp: roachpb.RSpan{
+				// r1 is special - see https://github.com/cockroachdb/cockroach/issues/95055.
+				Key:    roachpb.RKeyMin,
+				EndKey: roachpb.RKey("c"),
+			},
+		},
+		{
+			name: "r2",
+			sp: roachpb.RSpan{
+				Key:    roachpb.RKey("a"),
+				EndKey: roachpb.RKey("c"),
+			},
 		},
 	} {
-		for _, replicatedByRangeID := range []bool{false, true} {
-			for _, unreplicatedByRangeID := range []bool{false, true} {
-				opts := SelectOpts{
-					ReplicatedBySpan:      span,
-					ReplicatedByRangeID:   replicatedByRangeID,
-					UnreplicatedByRangeID: unreplicatedByRangeID,
-				}
-				var name []string
-				if replicatedByRangeID {
-					name = append(name, "replicatedid")
-				}
-				if unreplicatedByRangeID {
-					name = append(name, "unreplicatedid")
-				}
-				if !opts.ReplicatedBySpan.Equal(roachpb.RSpan{}) {
-					name = append(name, "span")
-				}
-				if len(name) == 0 {
-					name = append(name, "zero")
-				}
-				tcs = append(tcs, tc{
-					name: strings.Join(name, "_"),
-					opts: opts,
-					s:    Select(roachpb.RangeID(123), opts),
-				})
-			}
-		}
-	}
-
-	w := echotest.NewWalker(t, datapathutils.TestDataPath(t, t.Name()))
-
-	for _, tc := range tcs {
 		t.Run(tc.name, w.Run(t, tc.name, func(t *testing.T) string {
 			var buf strings.Builder
-			for _, sp := range tc.s {
-				fmt.Fprintf(&buf, "%s\n", sp)
+			for _, replicatedByRangeID := range []bool{false, true} {
+				for _, unreplicatedByRangeID := range []bool{false, true} {
+					opts := SelectOpts{
+						ReplicatedBySpan:      tc.sp,
+						ReplicatedByRangeID:   replicatedByRangeID,
+						UnreplicatedByRangeID: unreplicatedByRangeID,
+					}
+					var description []string
+					if replicatedByRangeID {
+						description = append(description, "RangeID-replicated")
+					}
+					if unreplicatedByRangeID {
+						description = append(description, "RangeID-unreplicated")
+					}
+					if rs := opts.ReplicatedBySpan; !rs.Equal(roachpb.RSpan{}) {
+						description = append(description, rs.String())
+					}
+					if len(description) == 0 {
+						description = append(description, "nothing")
+					}
+					fmt.Fprintf(&buf, "Select(%s):\n", strings.Join(description, ", "))
+					for _, sp := range Select(roachpb.RangeID(123), opts) {
+						fmt.Fprintf(&buf, "  %s\n", sp)
+					}
+				}
 			}
 			return buf.String()
 		}))
