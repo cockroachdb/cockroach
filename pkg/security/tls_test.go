137279c55a5733634564a3d651b6a5f03b27c70f
Cat J <87989074+catj-cockroach@users.noreply.github.com>
2023-01-11 12:54:07 -0500

    security: add option to re-enable old cipher suites
    In https://github.com/cockroachdb/cockroach/pull/82362 we updated the
    list of cipher suites to match the "recommended" cipher suites defined
    in RFC 8447. As noted in that PR, very old clients may fail to connect.
    
    For systems using those very old clients, this PR adds a environment
    variable that can be used to re-enable the "old" cipher suites removed
    in that PR.
    
    Release note (security update): adds option to re-enable "old" cipher
    suites for use with very old clients.
    
    Fixes: https://github.com/cockroachlabs/support/issues/1989

--- pkg/security/tls_test.go
+++ pkg/security/tls_test.go
@@ -17,6 +17,7 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/security"
 	"github.com/cockroachdb/cockroach/pkg/security/certnames"
+	"github.com/cockroachdb/cockroach/pkg/util/envutil"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/stretchr/testify/require"
 )
@@ -25,19 +26,28 @@ func TestLoadTLSConfig(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	cm, err := security.NewCertificateManager(certnames.EmbeddedCertsDir, security.CommandTLSSettings{})
 	require.NoError(t, err)
-	config, err := cm.GetServerTLSConfig()
+	// We check that the UI Server tls.Config doesn't include the old cipher suites by default.
+	config, err := cm.GetUIServerTLSConfig()
+	require.NoError(t, err)
+	clientConfig, err := config.GetConfigForClient(&tls.ClientHelloInfo{})
 	require.NoError(t, err)
 	require.NotNil(t, config.GetConfigForClient)
-	config, err = config.GetConfigForClient(&tls.ClientHelloInfo{})
-	require.NoError(t, err)
-	if err != nil {
-		t.Fatalf("Failed to load TLS config: %v", err)
-	}
+	require.NotNil(t, clientConfig)
+	require.Equal(t, security.RecommendedCipherSuites(), clientConfig.CipherSuites)
 
-	if len(config.Certificates) != 1 {
-		t.Fatalf("config.Certificates should have 1 cert; found %d", len(config.Certificates))
+	// Next we check that the Server tls.Config is correctly generated.
+	config, err = cm.GetServerTLSConfig()
+	require.NoError(t, err)
+	require.NotNil(t, config.GetConfigForClient)
+
+	clientConfig, err = config.GetConfigForClient(&tls.ClientHelloInfo{})
+	require.NoError(t, err)
+	require.NotNil(t, clientConfig)
+	require.Equal(t, security.RecommendedCipherSuites(), clientConfig.CipherSuites)
+	if len(clientConfig.Certificates) != 1 {
+		t.Fatalf("clientConfig.Certificates should have 1 cert; found %d", len(clientConfig.Certificates))
 	}
-	cert := config.Certificates[0]
+	cert := clientConfig.Certificates[0]
 	asn1Data := cert.Certificate[0] // TODO Check len()
 
 	x509Cert, err := x509.ParseCertificate(asn1Data)
@@ -45,17 +55,48 @@ func TestLoadTLSConfig(t *testing.T) {
 		t.Fatalf("Couldn't parse test cert: %v", err)
 	}
 
-	if err = verifyX509Cert(x509Cert, "localhost", config.RootCAs); err != nil {
+	if err = verifyX509Cert(x509Cert, "localhost", clientConfig.RootCAs); err != nil {
 		t.Errorf("Couldn't verify test cert against server CA: %v", err)
 	}
 
-	if err = verifyX509Cert(x509Cert, "localhost", config.ClientCAs); err != nil {
+	if err = verifyX509Cert(x509Cert, "localhost", clientConfig.ClientCAs); err != nil {
 		t.Errorf("Couldn't verify test cert against client CA: %v", err)
 	}
 
-	if err = verifyX509Cert(x509Cert, "google.com", config.RootCAs); err == nil {
+	if err = verifyX509Cert(x509Cert, "google.com", clientConfig.RootCAs); err == nil {
 		t.Errorf("Verified test cert for wrong hostname")
 	}
+}
+
+func TestLoadTLSConfigWithOldCipherSuites(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	reset := envutil.TestSetEnv(t, security.OldCipherSuitesEnabledEnv, "true")
+	defer reset()
+
+	cm, err := security.NewCertificateManager(certnames.EmbeddedCertsDir, security.CommandTLSSettings{})
+	require.NoError(t, err)
+
+	recommendedAndOldCipherSuites := append(
+		security.RecommendedCipherSuites(),
+		security.OldCipherSuites()...,
+	)
+
+	// We check that the UI Server tls.Config now includes the old cipher suites after the existing cipher suites.
+	config, err := cm.GetUIServerTLSConfig()
+	require.NoError(t, err)
+	clientConfig, err := config.GetConfigForClient(&tls.ClientHelloInfo{})
+	require.NoError(t, err)
+	require.NotNil(t, clientConfig)
+	require.Equal(t, recommendedAndOldCipherSuites, clientConfig.CipherSuites)
+
+	// We check that the Server tls.Config now includes the old cipher suites after the existing cipher suites.
+	config, err = cm.GetServerTLSConfig()
+	require.NoError(t, err)
+	require.NotNil(t, config.GetConfigForClient)
+	clientConfig, err = config.GetConfigForClient(&tls.ClientHelloInfo{})
+	require.NoError(t, err)
+	require.NotNil(t, clientConfig)
+	require.Equal(t, recommendedAndOldCipherSuites, clientConfig.CipherSuites)
 }
 
 func verifyX509Cert(cert *x509.Certificate, dnsName string, roots *x509.CertPool) error {
