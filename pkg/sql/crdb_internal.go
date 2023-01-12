83fca32a5fac8c3b657f76fbe14f853553ffdc23
Rafi Shamim <rafi@cockroachlabs.com>
2023-01-12 13:00:32 -0500

    comprules: support completion for qualified functions
    The syntax completion engine now generates suggestion for user-defined
    functions. It restricts the suggestions to only functions in the schema
    name if one was provided, or else to the schemas in the search_path.
    
    No release note since this functionality is new in v23.1.
    
    Release note: None

--- pkg/sql/crdb_internal.go
+++ pkg/sql/crdb_internal.go
@@ -2721,17 +2721,25 @@ CREATE TABLE crdb_internal.builtin_functions (
   function  STRING NOT NULL,
   signature STRING NOT NULL,
   category  STRING NOT NULL,
-  details   STRING NOT NULL
+  details   STRING NOT NULL,
+  schema    STRING NOT NULL
 )`,
 	populate: func(ctx context.Context, _ *planner, _ catalog.DatabaseDescriptor, addRow func(...tree.Datum) error) error {
 		for _, name := range builtins.AllBuiltinNames() {
 			props, overloads := builtinsregistry.GetBuiltinProperties(name)
+			schema := catconstants.PgCatalogName
+			const crdbInternal = catconstants.CRDBInternalSchemaName + "."
+			if strings.HasPrefix(name, crdbInternal) {
+				name = name[len(crdbInternal):]
+				schema = catconstants.CRDBInternalSchemaName
+			}
 			for _, f := range overloads {
 				if err := addRow(
 					tree.NewDString(name),
 					tree.NewDString(f.Signature(false /* simplify */)),
 					tree.NewDString(props.Category),
 					tree.NewDString(f.Info),
+					tree.NewDString(schema),
 				); err != nil {
 					return err
 				}
