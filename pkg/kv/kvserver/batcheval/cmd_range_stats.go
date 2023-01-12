354c4b1d4a2702ca338a5fedabcf1c13f9afc34f
Austen McClernon <austen@cockroachlabs.com>
2023-01-12 18:18:27 +0000

    kvserver: remove deprecated last split qps
    This patch removes the deprecated 'lastSplitQPS' value throughout the
    split/merge code. This field was deprecated in 22.1 in favor or
    `maxSplitQPS` and stopped being consulted in 22.2.
    
    Now only `maxSplitQPS` is consulted and set in `RangeStatsResponse`.
    
    Release note: None

--- pkg/kv/kvserver/batcheval/cmd_range_stats.go
+++ pkg/kv/kvserver/batcheval/cmd_range_stats.go
@@ -44,14 +44,12 @@ func RangeStats(
 ) (result.Result, error) {
 	reply := resp.(*roachpb.RangeStatsResponse)
 	reply.MVCCStats = cArgs.EvalCtx.GetMVCCStats()
-	reply.DeprecatedLastQueriesPerSecond = cArgs.EvalCtx.GetLastSplitQPS(ctx)
 	if qps, ok := cArgs.EvalCtx.GetMaxSplitQPS(ctx); ok {
 		reply.MaxQueriesPerSecond = qps
 	} else {
 		// See comment on MaxQueriesPerSecond. -1 means !ok.
 		reply.MaxQueriesPerSecond = -1
 	}
-	reply.MaxQueriesPerSecondSet = true
 	reply.RangeInfo = cArgs.EvalCtx.GetRangeInfo(ctx)
 	return result.Result{}, nil
 }
