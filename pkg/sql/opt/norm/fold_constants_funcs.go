cockroachdb/cockroach

commit 5c9c34997fb405cf49e91cf245d1735fa030c52a

opt: replace FoldBinaryCheckOverflow with FoldBinary
Release note: None

pkg/sql/opt/norm/rules/comp.opt
@@ -47,11 +47,7 @@
         ) &
         (CanConstructBinary Minus $right $leftRight) &
         (Let
-            ($result $ok):(FoldBinaryCheckNull
-                Minus
-                $right
-                $leftRight
-            )
+            ($result $ok):(FoldBinary Minus $right $leftRight)
             $ok
         )
 )
@@ -77,12 +73,7 @@
         ) &
         (CanConstructBinary Plus $right $leftRight) &
         (Let
-            ($result $ok):(FoldBinaryCheckNull
-                Plus
-                $right
-                $leftRight
-            )
-            $ok
+            ($result $ok):(FoldBinary Plus $right $leftRight) $ok
         )
 )
 =>
@@ -107,12 +98,7 @@
         ) &
         (CanConstructBinary Minus $leftLeft $right) &
         (Let
-            ($result $ok):(FoldBinaryCheckNull
-                Minus
-                $leftLeft
-                $right
-            )
-            $ok
+            ($result $ok):(FoldBinary Minus $leftLeft $right) $ok
         )
 )
 =>

pkg/sql/opt/norm/comp_funcs.go
@@ -14,7 +14,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/opt"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/memo"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/builtins/builtinsregistry"
-	"github.com/cockroachdb/cockroach/pkg/sql/sem/eval"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/errors"
@@ -49,37 +48,6 @@ func (c *CustomFuncs) ArithmeticErrorsOnOverflow(left, right *types.T) bool {
 		return false
 	}
 	return true
-}
-
-// FoldBinaryCheckNull attempts to evaluate a binary expression with
-// constant inputs. The only operations supported are plus and minus. It returns
-// a constant expression if all the following criteria are met:
-//
-//  1. An overload function for the given operator and input types exists and
-//     has an appropriate volatility.
-//  2. There is no error when evaluating the binary expression.
-//
-// If any of these conditions are not met, it returns ok=false.
-func (c *CustomFuncs) FoldBinaryCheckNull(
-	op opt.Operator, left, right opt.ScalarExpr,
-) (_ opt.ScalarExpr, ok bool) {
-	o, ok := memo.FindBinaryOverload(op, left.DataType(), right.DataType())
-	if !ok || !c.CanFoldOperator(o.Volatility) {
-		return nil, false
-	}
-	lDatum, rDatum := memo.ExtractConstDatum(left), memo.ExtractConstDatum(right)
-	// TODO(mgartner): FoldBinaryCheckNull is similar to FoldBinary, except
-	// for this NULL check. The NULL check might not be necessary since we no
-	// longer use this function on TIME and INTERVAL types, so maybe we can
-	// reuse FoldBinary instead.
-	if lDatum == tree.DNull || rDatum == tree.DNull {
-		return nil, false
-	}
-	result, err := eval.BinaryOp(c.f.ctx, c.f.evalCtx, o.EvalOp, lDatum, rDatum)
-	if err != nil {
-		return nil, false
-	}
-	return c.f.ConstructConstVal(result, o.ReturnType), true
 }
 
 // NormalizeTupleEquality remaps the elements of two tuples compared for

commit 07efdc55133c0c4c58b3305f0360e2bfdf281255

norm: fix folding binary and comparison operators with NULLs
This commit fixes a recently introduced regression in
5c9c34997fb405cf49e91cf245d1735fa030c52a where we removed the non-null
check of arguments to binary and comparison operators prior to
evaluating them. That check is needed if `CalledOnNullInput` is set to
`false` for the corresponding operator - in all other places where we
`Eval` the operator we do have this non-null check. This commit updates
the applicable normalization rules that can fold operators to apply only
to `Const` expressions (excluding `ConstValue`s which include NULLS) as
well as adds the additional protection of having an explicit non-null
check.

However, this bug seems to occur rarely in practice - the original
reproduction requires that there is an integer out of range computation
prior to folding, and I couldn't really simplify that reproduction.
Thus, I decided to omit the release note.

Release note: None

pkg/sql/opt/norm/fold_constants_funcs.go
@@ -294,7 +294,13 @@ func (c *CustomFuncs) FoldBinary(
 	}
 
 	lDatum, rDatum := memo.ExtractConstDatum(left), memo.ExtractConstDatum(right)
-	result, err := eval.BinaryOp(c.f.ctx, c.f.evalCtx, o.EvalOp, lDatum, rDatum)
+	var result tree.Datum
+	var err error
+	if !o.CalledOnNullInput && (lDatum == tree.DNull || rDatum == tree.DNull) {
+		result = tree.DNull
+	} else {
+		result, err = eval.BinaryOp(c.f.ctx, c.f.evalCtx, o.EvalOp, lDatum, rDatum)
+	}
 	if err != nil {
 		return nil, false
 	}
@@ -511,7 +517,13 @@ func (c *CustomFuncs) FoldComparison(
 		lDatum, rDatum = rDatum, lDatum
 	}
 
-	result, err := eval.BinaryOp(c.f.ctx, c.f.evalCtx, o.EvalOp, lDatum, rDatum)
+	var result tree.Datum
+	var err error
+	if !o.CalledOnNullInput && (lDatum == tree.DNull || rDatum == tree.DNull) {
+		result = tree.DNull
+	} else {
+		result, err = eval.BinaryOp(c.f.ctx, c.f.evalCtx, o.EvalOp, lDatum, rDatum)
+	}
 	if err != nil {
 		return nil, false
 	}

pkg/sql/opt/norm/rules/comp.opt
@@ -39,8 +39,8 @@
 #       normalize.go either. We can add once we've proved it's OK to do so.
 [NormalizeCmpPlusConst, Normalize]
 (Eq | Ge | Gt | Le | Lt
-    (Plus $leftLeft:^(ConstValue) $leftRight:(ConstValue))
-    $right:(ConstValue) &
+    (Plus $leftLeft:^(ConstValue) $leftRight:(Const))
+    $right:(Const) &
         (ArithmeticErrorsOnOverflow
             (TypeOf $right)
             (TypeOf $leftRight)
@@ -65,8 +65,8 @@
 # See NormalizeCmpPlusConst for more details.
 [NormalizeCmpMinusConst, Normalize]
 (Eq | Ge | Gt | Le | Lt
-    (Minus $leftLeft:^(ConstValue) $leftRight:(ConstValue))
-    $right:(ConstValue) &
+    (Minus $leftLeft:^(ConstValue) $leftRight:(Const))
+    $right:(Const) &
         (ArithmeticErrorsOnOverflow
             (TypeOf $right)
             (TypeOf $leftRight)
@@ -90,8 +90,8 @@
 # See NormalizeCmpPlusConst for more details.
 [NormalizeCmpConstMinus, Normalize]
 (Eq | Ge | Gt | Le | Lt
-    (Minus $leftLeft:(ConstValue) $leftRight:^(ConstValue))
-    $right:(ConstValue) &
+    (Minus $leftLeft:(Const) $leftRight:^(ConstValue))
+    $right:(Const) &
         (ArithmeticErrorsOnOverflow
             (TypeOf $leftLeft)
             (TypeOf $right)

pkg/sql/opt/exec/execbuilder/testdata/scalar
@@ -1301,3 +1301,32 @@ vectorized: true
       estimated row count: 1,000 (missing stats)
       table: t0@t0_pkey
       spans: FULL SCAN
+
+# Regression tests for not checking whether arguments to a binary op are
+# non-NULL when folding (#94264).
+query T
+EXPLAIN SELECT 2-(9223372036854775807+436256318) < (CASE WHEN false THEN -1 END)
+----
+distribution: local
+vectorized: true
+·
+• values
+  size: 1 column, 1 row
+
+query T
+EXPLAIN SELECT (9223372036854775807+436256318)-2 < (CASE WHEN false THEN -1 END)
+----
+distribution: local
+vectorized: true
+·
+• values
+  size: 1 column, 1 row
+
+query T
+EXPLAIN SELECT (9223372036854775807+436256318)+2 < (CASE WHEN false THEN -1 END)
+----
+distribution: local
+vectorized: true
+·
+• values
+  size: 1 column, 1 row

pkg/sql/opt/norm/testdata/rules/bool
@@ -434,28 +434,11 @@ FROM
 WHERE
 	(NOT (t.g ~ t.g));
 ----
-project
- ├── columns: "?column?":2 g:1
- ├── cardinality: [0 - 1]
- ├── immutable
+values
+ ├── columns: "?column?":2!null g:1!null
+ ├── cardinality: [0 - 0]
  ├── key: ()
- ├── fd: ()-->(1,2)
- ├── select
- │    ├── columns: column1:1
- │    ├── cardinality: [0 - 1]
- │    ├── immutable
- │    ├── key: ()
- │    ├── fd: ()-->(1)
- │    ├── values
- │    │    ├── columns: column1:1
- │    │    ├── cardinality: [1 - 1]
- │    │    ├── key: ()
- │    │    ├── fd: ()-->(1)
- │    │    └── (NULL,)
- │    └── filters
- │         └── NOT (CAST(NULL AS GEOMETRY) ~ CAST(NULL AS GEOMETRY)) [immutable]
- └── projections
-      └── NULL [as="?column?":2]
+ └── fd: ()-->(1,2)
 
 # --------------------------------------------------
 # EliminateNot

pkg/sql/opt/norm/testdata/rules/comp
@@ -162,6 +162,18 @@ values
  ├── fd: ()-->(2)
  └── (true,)
 
+# Regression test for #94264. This rule should not apply when the argument to a
+# Plus or a Minus is NULL.
+norm expect-not=(NormalizeCmpPlusConst)
+SELECT (9223372036854775807+436256318)+2 < (CASE WHEN false THEN -1 END)
+----
+values
+ ├── columns: "?column?":1
+ ├── cardinality: [1 - 1]
+ ├── key: ()
+ ├── fd: ()-->(1)
+ └── (NULL,)
+
 # --------------------------------------------------
 # NormalizeCmpMinusConst
 # --------------------------------------------------
@@ -218,6 +230,19 @@ values
  ├── key: ()
  ├── fd: ()-->(2)
  └── (true,)
+
+# Regression test for #94264. This rule should not apply when the argument to a
+# Plus or a Minus is NULL.
+norm expect-not=(NormalizeCmpMinusConst)
+SELECT (9223372036854775807+436256318)-2 < (CASE WHEN false THEN -1 END)
+----
+values
+ ├── columns: "?column?":1
+ ├── cardinality: [1 - 1]
+ ├── key: ()
+ ├── fd: ()-->(1)
+ └── (NULL,)
+
 
 # --------------------------------------------------
 # NormalizeCmpConstMinus
@@ -280,6 +305,18 @@ select
  │    └── fd: (1)-->(2-6)
  └── filters
       └── ('2022-01-01' - s:4::TIME) >= '2022-01-01 01:00:00' [outer=(4), stable]
+
+# Regression test for #94264. This rule should not apply when the argument to a
+# Plus or a Minus is NULL.
+norm expect-not=(NormalizeCmpConstMinus)
+SELECT 2-(9223372036854775807+436256318) < (CASE WHEN false THEN -1 END)
+----
+values
+ ├── columns: "?column?":1
+ ├── cardinality: [1 - 1]
+ ├── key: ()
+ ├── fd: ()-->(1)
+ └── (NULL,)
 
 # --------------------------------------------------
 # NormalizeTupleEquality