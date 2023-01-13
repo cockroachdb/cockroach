df4833c608dbeafa637e81055d7a37c96720094d
j82w <jwilley@cockroachlabs.com>
2023-01-13 10:37:12 -0500

    sql: fixes database list with special characters
    The database name was not properly escaped which causes the
    query to fail if the database name has a special character.
    The endpoint still does not support names with `/`. This will
    not be an issue since all ui is being converted to sql-over-http.
    
    Example uri: `http://localhost:8080/_admin/v1/databases/my%20testdb`
    
    part of: #94328
    
    Release note (sql change): fixes databases list api when
    database name has special characters.

--- pkg/server/admin_test.go
+++ pkg/server/admin_test.go
@@ -58,6 +58,8 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
+	"github.com/cockroachdb/cockroach/pkg/util/randident"
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 	"github.com/cockroachdb/errors"
@@ -339,6 +341,18 @@ func TestAdminDebugRedirect(t *testing.T) {
 	}
 }
 
+func generateRandomName() string {
+	rand, _ := randutil.NewTestRand()
+	cfg := randident.DefaultNameGeneratorConfig()
+
+	ng := randident.NewNameGenerator(
+		&cfg,
+		rand,
+		"ab%s-c.d",
+	)
+	return ng.GenerateOne(42)
+}
+
 func TestAdminAPIDatabases(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
@@ -354,14 +368,15 @@ func TestAdminAPIDatabases(t *testing.T) {
 	ctx, span := ac.AnnotateCtxWithSpan(context.Background(), "test")
 	defer span.Finish()
 
-	const testdb = "test"
-	query := "CREATE DATABASE " + testdb
+	testDbName := generateRandomName()
+	testDbEscaped := tree.NameString(testDbName)
+	query := "CREATE DATABASE " + testDbEscaped
 	if _, err := db.Exec(query); err != nil {
 		t.Fatal(err)
 	}
 	// Test needs to revoke CONNECT on the public database to properly exercise
 	// fine-grained permissions logic.
-	if _, err := db.Exec(fmt.Sprintf("REVOKE CONNECT ON DATABASE %s FROM public", testdb)); err != nil {
+	if _, err := db.Exec(fmt.Sprintf("REVOKE CONNECT ON DATABASE %s FROM public", testDbEscaped)); err != nil {
 		t.Fatal(err)
 	}
 	if _, err := db.Exec("REVOKE CONNECT ON DATABASE defaultdb FROM public"); err != nil {
@@ -380,7 +395,7 @@ func TestAdminAPIDatabases(t *testing.T) {
 	query = fmt.Sprintf(
 		"GRANT %s ON DATABASE %s TO %s",
 		strings.Join(privileges, ", "),
-		testdb,
+		testDbEscaped,
 		authenticatedUserNameNoAdmin().SQLIdentifier(),
 	)
 	if _, err := db.Exec(query); err != nil {
@@ -400,8 +415,8 @@ func TestAdminAPIDatabases(t *testing.T) {
 		expectedDBs []string
 		isAdmin     bool
 	}{
-		{[]string{"defaultdb", "postgres", "system", testdb}, true},
-		{[]string{"postgres", testdb}, false},
+		{[]string{"defaultdb", "postgres", "system", testDbName}, true},
+		{[]string{"postgres", testDbName}, false},
 	} {
 		t.Run(fmt.Sprintf("isAdmin:%t", tc.isAdmin), func(t *testing.T) {
 			// Test databases endpoint.
@@ -419,6 +434,7 @@ func TestAdminAPIDatabases(t *testing.T) {
 				t.Fatalf("length of result %d != expected %d", a, e)
 			}
 
+			sort.Strings(tc.expectedDBs)
 			sort.Strings(resp.Databases)
 			for i, e := range tc.expectedDBs {
 				if a := resp.Databases[i]; a != e {
@@ -428,9 +444,11 @@ func TestAdminAPIDatabases(t *testing.T) {
 
 			// Test database details endpoint.
 			var details serverpb.DatabaseDetailsResponse
+			urlEscapeDbName := url.PathEscape(testDbName)
+
 			if err := getAdminJSONProtoWithAdminOption(
 				s,
-				"databases/"+testdb,
+				"databases/"+urlEscapeDbName,
 				&details,
 				tc.isAdmin,
 			); err != nil {
@@ -471,7 +489,7 @@ func TestAdminAPIDatabases(t *testing.T) {
 			}
 
 			// Verify Descriptor ID.
-			databaseID, err := ts.admin.queryDatabaseID(ctx, username.RootUserName(), testdb)
+			databaseID, err := ts.admin.queryDatabaseID(ctx, username.RootUserName(), testDbName)
 			if err != nil {
 				t.Fatal(err)
 			}
