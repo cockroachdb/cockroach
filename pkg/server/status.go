ab174c9b175efd9e27121a0449eb91a9de0b8914
Austen McClernon <austen@cockroachlabs.com>
2023-01-13 17:39:13 +0000

    kvserver,server: add range cpu time hotrange api
    Various range load statistics such as read(write) bytes(keys) exist on
    the hot ranges (v1) and range statistics api. This commit adds the
    additional statistic, `CPUTimePerSecond`. This statistic is the recent
    average cpu usage per second of the range in nanoseconds.
    
    resolves: #95384
    
    Release note: None

--- pkg/server/status.go
+++ pkg/server/status.go
@@ -2144,6 +2144,7 @@ func (s *systemStatusServer) rangesHelper(
 				ReadsPerSecond:      loadStats.ReadKeysPerSecond,
 				WriteBytesPerSecond: loadStats.WriteKeysPerSecond,
 				ReadBytesPerSecond:  loadStats.ReadBytesPerSecond,
+				CPUTimePerSecond:    loadStats.RaftCPUNanosPerSecond + loadStats.RequestCPUNanosPerSecond,
 			},
 			Problems: serverpb.RangeProblems{
 				Unavailable:            metrics.Unavailable,
@@ -2689,6 +2690,7 @@ func (s *systemStatusServer) localHotRanges(
 			storeResp.HotRanges[i].ReadsPerSecond = r.ReadKeysPerSecond
 			storeResp.HotRanges[i].WriteBytesPerSecond = r.WriteBytesPerSecond
 			storeResp.HotRanges[i].ReadBytesPerSecond = r.ReadBytesPerSecond
+			storeResp.HotRanges[i].CPUTimePerSecond = r.CPUTimePerSecond
 		}
 		resp.Stores = append(resp.Stores, storeResp)
 		return nil
