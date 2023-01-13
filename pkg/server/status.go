134463bd7aaf58df4b5480c7be3f18610061d251
Austen McClernon <austen@cockroachlabs.com>
2023-01-13 17:46:50 +0000

    server: add more load stats to hot ranges v2 api
    Previously, only QPS was included in the hot ranges v2 api. This commit
    adds the following range statistics to the API:
    
    ```
    WritesPerSecond
    ReadsPerSecond
    WriteBytesPerSecond
    ReadBytesPerSecond
    CPUTimePerSecond
    ```
    
    See the associated comments in `status.proto` for a description of each
    load statistic.
    
    These may be used to determine the hottest ranges. Note however that
    these are sorted by default by QPS. The ordering of ranges w.r.t the new
    load statistics has no guarantees. Likewise, ranges may be omitted which
    have high relative values for the new load statistics but relatively low
    QPS. In practice however, there usually exists a strong correlation that
    ensures that such ranges would not be omitted entirely.
    
    resolves: #95383
    
    Release note: None

--- pkg/server/status.go
+++ pkg/server/status.go
@@ -2585,16 +2585,21 @@ func (s *systemStatusServer) HotRangesV2(
 					}
 
 					ranges = append(ranges, &serverpb.HotRangesResponseV2_HotRange{
-						RangeID:           r.Desc.RangeID,
-						NodeID:            requestedNodeID,
-						QPS:               r.QueriesPerSecond,
-						TableName:         tableName,
-						SchemaName:        schemaName,
-						DatabaseName:      dbName,
-						IndexName:         indexName,
-						ReplicaNodeIds:    replicaNodeIDs,
-						LeaseholderNodeID: r.LeaseholderNodeID,
-						StoreID:           store.StoreID,
+						RangeID:             r.Desc.RangeID,
+						NodeID:              requestedNodeID,
+						QPS:                 r.QueriesPerSecond,
+						WritesPerSecond:     r.WritesPerSecond,
+						ReadsPerSecond:      r.ReadsPerSecond,
+						WriteBytesPerSecond: r.WriteBytesPerSecond,
+						ReadBytesPerSecond:  r.ReadBytesPerSecond,
+						CPUTimePerSecond:    r.CPUTimePerSecond,
+						TableName:           tableName,
+						SchemaName:          schemaName,
+						DatabaseName:        dbName,
+						IndexName:           indexName,
+						ReplicaNodeIds:      replicaNodeIDs,
+						LeaseholderNodeID:   r.LeaseholderNodeID,
+						StoreID:             store.StoreID,
 					})
 				}
 			}
