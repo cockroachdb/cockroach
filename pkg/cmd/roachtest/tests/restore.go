536d5193ec47c92820893cc01fb8c1194db62dd3
Michael Butler <butler@cockroachlabs.com>
2023-01-18 15:20:22 -0500

    roachtest: run cluster backup schedule by default
    This patch creates a cluster backup schedule by default on roachtests. A
    roachtest writer can override this default behavior by passing
    option.DefaultStartOptsNoBackups() at cluster.Start().
    
    Roachtests are supposed to mirror a customer env. Like a customer's cluster,
    roachtests currently run background jobs like gc, sql stats computation,
    rebalancing etc. But unlike most customer clusters, roachtests do not run
    scheduled backup jobs by default. Any workload on a roachtest should run just
    as well with or without with background jobs, but we currently do not test
    this, at least wrt to scheduled backups. This patch addresses this lack of test
    coverage.
    
    Fixes #86045
    
    Release note: None

--- pkg/cmd/roachtest/tests/restore.go
+++ pkg/cmd/roachtest/tests/restore.go
@@ -303,7 +303,7 @@ func registerRestoreNodeShutdown(r registry.Registry) {
 			gatewayNode := 2
 			nodeToShutdown := 3
 			c.Put(ctx, t.Cockroach(), "./cockroach")
-			c.Start(ctx, t.L(), option.DefaultStartOpts(), install.MakeClusterSettings())
+			c.Start(ctx, t.L(), option.DefaultStartOptsNoBackups(), install.MakeClusterSettings())
 
 			jobSurvivesNodeShutdown(ctx, t, c, nodeToShutdown, makeRestoreStarter(ctx, t, c, gatewayNode))
 		},
@@ -317,7 +317,7 @@ func registerRestoreNodeShutdown(r registry.Registry) {
 			gatewayNode := 2
 			nodeToShutdown := 2
 			c.Put(ctx, t.Cockroach(), "./cockroach")
-			c.Start(ctx, t.L(), option.DefaultStartOpts(), install.MakeClusterSettings())
+			c.Start(ctx, t.L(), option.DefaultStartOptsNoBackups(), install.MakeClusterSettings())
 
 			jobSurvivesNodeShutdown(ctx, t, c, nodeToShutdown, makeRestoreStarter(ctx, t, c, gatewayNode))
 		},
@@ -452,7 +452,7 @@ func registerRestore(r registry.Registry) {
 			EncryptionSupport: registry.EncryptionMetamorphic,
 			Run: func(ctx context.Context, t test.Test, c cluster.Cluster) {
 				c.Put(ctx, t.Cockroach(), "./cockroach")
-				c.Start(ctx, t.L(), option.DefaultStartOpts(), install.MakeClusterSettings())
+				c.Start(ctx, t.L(), option.DefaultStartOptsNoBackups(), install.MakeClusterSettings())
 				m := c.NewMonitor(ctx)
 
 				// Run the disk usage logger in the monitor to guarantee its
@@ -522,7 +522,7 @@ func registerRestore(r registry.Registry) {
 		Timeout: withPauseTimeout,
 		Run: func(ctx context.Context, t test.Test, c cluster.Cluster) {
 			c.Put(ctx, t.Cockroach(), "./cockroach")
-			c.Start(ctx, t.L(), option.DefaultStartOpts(), install.MakeClusterSettings())
+			c.Start(ctx, t.L(), option.DefaultStartOptsNoBackups(), install.MakeClusterSettings())
 			m := c.NewMonitor(ctx)
 
 			// Run the disk usage logger in the monitor to guarantee its
@@ -715,7 +715,7 @@ func registerRestore(r registry.Registry) {
 					t.Skip("test configured to run on %s", sp.hardware.cloud)
 				}
 				c.Put(ctx, t.Cockroach(), "./cockroach")
-				c.Start(ctx, t.L(), option.DefaultStartOpts(), install.MakeClusterSettings())
+				c.Start(ctx, t.L(), option.DefaultStartOptsNoBackups(), install.MakeClusterSettings())
 				m := c.NewMonitor(ctx)
 
 				// Run the disk usage logger in the monitor to guarantee its
