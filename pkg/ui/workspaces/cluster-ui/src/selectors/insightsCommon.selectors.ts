138b561bf78b757d59f283d3a383b79be4e13756
Xin Hao Zhang <xzhang@cockroachlabs.com>
2023-01-18 13:50:11 -0500

    ui: rename insightsApi.ts to txnInsightsApi.ts
    This rename is in preparation for splitting the insights
    api call into separate txn and stmt calls.
    
    Epic: none
    
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/selectors/insightsCommon.selectors.ts
+++ pkg/ui/workspaces/cluster-ui/src/selectors/insightsCommon.selectors.ts
@@ -8,7 +8,7 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-import { FlattenedStmtInsights } from "src/api/insightsApi";
+import { FlattenedStmtInsights } from "src/api/txnInsightsApi";
 import {
   mergeTxnInsightDetails,
   flattenTxnInsightsToStmts,
