45f14df8983ca42575854449e9e67c6a9cbf8843
Pavel Kalinnikov <pavel@cockroachlabs.com>
2023-01-19 17:53:49 +0000

    kvserver: fix race on Replica.tenantLimiter
    When Replica is being destroyed, it can set tenantLimiter to nil. However, all
    accessors assume synchronization through IsInitialized atomic check and that
    this field can't be modified after the replica is initialized.
    
    There is no particular need in nullifying this limiter, the replica is going
    away soon, so this commit removes this line in order to fix the data race.
    
    Epic: none
    Release note: none

--- pkg/kv/kvserver/replica_destroy.go
+++ pkg/kv/kvserver/replica_destroy.go
@@ -132,7 +132,6 @@ func (r *Replica) postDestroyRaftMuLocked(ctx context.Context, ms enginepb.MVCCS
 	// Unhook the tenant rate limiter if we have one.
 	if r.tenantLimiter != nil {
 		r.store.tenantRateLimiters.Release(r.tenantLimiter)
-		r.tenantLimiter = nil
 	}
 
 	return nil
