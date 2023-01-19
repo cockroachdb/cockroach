5cf0686229d732ab440a45a0fd3f0a42bb377a8f
Rail Aliiev <rail@iqchoice.com>
2023-01-19 11:50:56 -0500

    build: compile FIPS go archives
    Epic: none
    Release note: None

---
+++ build/teamcity/internal/release/build-and-publish-patched-go-fips.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+google_credentials="$GOOGLE_EPHEMERAL_CREDENTIALS"
+dir="$(dirname $(dirname $(dirname $(dirname "${0}"))))"
+source "$dir/teamcity-support.sh"  # for log_into_gcloud
+log_into_gcloud
+
+set -x
+
+this_dir="$(cd "$(dirname "${0}")"; pwd)"
+toplevel="$(dirname $(dirname $(dirname $(dirname $this_dir))))"
+
+mkdir -p "${toplevel}"/artifacts
+# TODO: pin docker image version
+DOCKER_IMAGE=registry.access.redhat.com/ubi8/go-toolset:latest
+
+tc_start_block "Build Go toolchains (linux/amd64)" 
+docker run --rm -i ${tty-} -v $this_dir/build-and-publish-patched-go:/bootstrap \
+  -v "${toplevel}"/artifacts:/artifacts \
+  --user root \
+  --platform linux/amd64 \
+  $DOCKER_IMAGE /bootstrap/impl-fips.sh
+tc_end_block "Build Go toolchains (linux/amd64)"
+
+# TODO: do we care to cross build without the qemu llayer? Last I tried, th
+# compiler was complaining about some missing headers.
+tc_start_block "Build Go toolchains (linux/arm64)" 
+docker run --rm -i ${tty-} -v $this_dir/build-and-publish-patched-go:/bootstrap \
+  -v "${toplevel}"/artifacts:/artifacts \
+  --user root \
+  --platform linux/arm64 \
+  $DOCKER_IMAGE /bootstrap/impl-fips.sh
+tc_end_block "Build Go toolchains (linux/arm64)"
+
+tc_start_block "Publish artifacts"
+loc=$(date +%Y%m%d-%H%M%S)
+for FILE in `find $root/artifacts -name '*.tar.gz'`; do
+    BASE=$(basename $FILE)
+    gsutil cp $FILE gs://public-bazel-artifacts/go-fips/$loc/$BASE
+done
+tc_end_block "Publish artifacts"
+
+tc_end_block "Print checksums"
+sha256sum $root/artifacts/*.tar.gz
+echo "urls = [\"https://storage.googleapis.com/public-bazel-artifacts/go-fips/$loc/{}\"],"
+tc_start_block "Print checksums"
