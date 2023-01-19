85dfba8b4c43f304af9a3eb2e5d5403502d5bafe
Rafi Shamim <rafi@cockroachlabs.com>
2023-01-18 21:38:45 -0500

    sql: add index expression to SHOW INDEXES
    Release note (sql change): SHOW INDEXES will now show the expression
    used to define an index, if one was used.

--- pkg/sql/delegate/show_database_indexes.go
+++ pkg/sql/delegate/show_database_indexes.go
@@ -31,9 +31,19 @@ func (d *delegator) delegateShowDatabaseIndexes(
 SELECT
 	table_name,
 	index_name,
+	index_schema,
 	non_unique::BOOL,
 	seq_in_index,
 	column_name,
+	CASE
+		-- array_positions(i.indkey, 0) returns the 1-based indexes of the indkey elements that are 0.
+		-- array_position(arr, seq_in_index) returns the 1-based index of the value seq_in_index in arr.
+		-- indkey is an int2vector, which is accessed with 0-based indexes.
+		-- indexprs is a string[], which is accessed with 1-based indexes.
+		-- To put this all together, for the k-th 0 value inside of indkey, this will find the k-th indexpr.
+		WHEN i.indkey[seq_in_index-1] = 0 THEN (indexprs::STRING[])[array_position(array_positions(i.indkey, 0), seq_in_index)]
+		ELSE column_name
+	END AS definition,
 	direction,
 	storing::BOOL,
 	implicit::BOOL,
@@ -41,18 +51,17 @@ SELECT
 
 	if n.WithComment {
 		getAllIndexesQuery += `,
-	obj_description(pg_class.oid) AS comment`
+	obj_description(c.oid) AS comment`
 	}
 
 	getAllIndexesQuery += `
 FROM
-	%s.information_schema.statistics`
-
-	if n.WithComment {
-		getAllIndexesQuery += `
-	LEFT JOIN pg_class ON
-		statistics.index_name = pg_class.relname`
-	}
+    %[1]s.information_schema.statistics AS s
+    JOIN %[1]s.pg_catalog.pg_class c ON c.relname = s.index_name
+    JOIN %[1]s.pg_catalog.pg_class c_table ON c_table.relname = s.table_name
+    JOIN %[1]s.pg_catalog.pg_namespace n ON c.relnamespace = n.oid AND c_table.relnamespace = n.oid AND n.nspname = s.index_schema
+    JOIN %[1]s.pg_catalog.pg_index i ON i.indexrelid = c.oid AND i.indrelid = c_table.oid
+`
 
 	getAllIndexesQuery += `
 ORDER BY 1, 2, 4`
