9491e609ef1d470907a7c0fc374c2f4d02b514a0
Andrei Matei <andrei@cockroachlabs.com>
2023-01-22 14:19:00 -0500

    obsservice: support embedding in CRDB nodes
    This patch adds a special value to the --obsservice-addr CRDB flag:
    --obsservice-addr=embed means that an the respective node will run the
    Obs Service RPC service internally and the events exporter will be
    configured to connect to this internal service.
    
    Release note: None
    Fixes: CRDB-19741
    Epic: CRDB-16791

--- pkg/server/tenant.go
+++ pkg/server/tenant.go
@@ -676,11 +676,12 @@ func (s *SQLServerWrapper) PreStart(ctx context.Context) error {
 	if instanceID == 0 {
 		log.Fatalf(ctx, "expected SQLInstanceID to be initialized after preStart")
 	}
-	if err := s.eventsExporter.Start(ctx, obs.NodeInfo{
+	s.eventsExporter.SetNodeInfo(obs.NodeInfo{
 		ClusterID:     clusterID,
 		NodeID:        int32(instanceID),
 		BinaryVersion: build.BinaryVersion(),
-	}, s.stopper); err != nil {
+	})
+	if err := s.eventsExporter.Start(ctx, s.stopper); err != nil {
 		return errors.Wrap(err, "failed to start the event exporter")
 	}
 
@@ -1049,7 +1050,13 @@ func makeTenantSQLServerArgs(
 	// cluster ID is known, with a Start() call.
 	var eventsExporter obs.EventsExporterInterface
 	if baseCfg.ObsServiceAddr != "" {
-		ee, err := obs.NewEventsExporter(
+		if baseCfg.ObsServiceAddr == base.ObsServiceEmbedFlagValue {
+			// TODO(andrei): Add support for this option for tenants - at least for
+			// shared-process tenants where the event exporting should be hooked up to
+			// the ingester running in the host process.
+			return sqlServerArgs{}, errors.New("--obsservice-addr=embed is not currently supported for tenants")
+		}
+		ee := obs.NewEventsExporter(
 			baseCfg.ObsServiceAddr,
 			timeutil.DefaultTimeSource{},
 			baseCfg.Tracer,
@@ -1058,13 +1065,8 @@ func makeTenantSQLServerArgs(
 			10*1<<20,                               // maxBufferSizeBytes - 10MB
 			monitorAndMetrics.rootSQLMemoryMonitor, // memMonitor - this is not "SQL" usage, but we don't have another memory pool
 		)
-		if err != nil {
-			log.Errorf(startupCtx, "failed to create events exporter: %s", err)
-		} else {
-			eventsExporter = ee
-		}
-	}
-	if eventsExporter == nil {
+		eventsExporter = ee
+	} else {
 		eventsExporter = &obs.NoopEventsExporter{}
 	}
 
