5782068284b5c1944767c8d586585e853aaef53b
Marius Posta <marius@cockroachlabs.com>
2023-01-23 10:35:58 -0500

    rules: remove FromHasPublicStatusIfFromIsTableAndToIsRowLevelTTL
    This is only necessary in 22.2 for compatibility with 22.1.
    The clause has therefore been moved to release_22_2.
    
    Release note: None

--- pkg/sql/schemachanger/scplan/internal/rules/helpers.go
+++ pkg/sql/schemachanger/scplan/internal/rules/helpers.go
@@ -524,33 +524,6 @@ var descriptorIsNotBeingDropped = screl.Schema.DefNotJoin1(
 	},
 )
 
-// FromHasPublicStatusIfFromIsTableAndToIsRowLevelTTL creates
-// a clause which leads to the outer clause failing to unify
-// if the passed element `from` is a Table, `to` is a RowLevelTTl,
-// and there does not exist a Node with the same Target as
-// `fromTarget` in PUBLIC status.
-// It is used to suppress rule "descriptor drop right before dependent element removal"
-// for the special case where we drop a rowLevelTTL table in mixed
-// version state for forward compatibility (issue #86672).
-var FromHasPublicStatusIfFromIsTableAndToIsRowLevelTTL = screl.Schema.DefNotJoin3(
-	"fromHasPublicStatusIfFromIsTableAndToIsRowLevelTTL",
-	"fromTarget", "fromEl", "toEl", func(fromTarget, fromEl, toEl rel.Var) rel.Clauses {
-		n := rel.Var("n")
-		return rel.Clauses{
-			fromEl.Type((*scpb.Table)(nil)),
-			toEl.Type((*scpb.RowLevelTTL)(nil)),
-			n.Type((*screl.Node)(nil)),
-			n.AttrEqVar(screl.Target, fromTarget),
-			screl.Schema.DefNotJoin1("nodeHasNoPublicStatus", "n", func(n rel.Var) rel.Clauses {
-				public := rel.Var("public")
-				return rel.Clauses{
-					public.Eq(scpb.Status_PUBLIC),
-					n.AttrEqVar(screl.CurrentStatus, public),
-				}
-			})(n),
-		}
-	})
-
 // notJoinOnNodeWithStatusIn is a cache to memoize getNotJoinOnNodeWithStatusIn.
 var notJoinOnNodeWithStatusIn = map[string]rel.Rule1{}
 
