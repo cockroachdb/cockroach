7baeb67a96e6e352274193a0aafdbef1f707a403
Lidor Carmel <lidor@cockroachlabs.com>
2023-01-24 12:38:13 -0800

    streamingest,jobspb,sql: adding a `pending cutover` state
    A replication tenant can have a cutover time in the future, in that
    case the state of that tenant would be `REPLICATION CUTTING OVER` before this
    pr. But the cutover does not start until the cutover time.
    
    This pr adds a `REPLICATION PENDING CUTOVER` state for a tenant that has a
    cutover time in the future.
    
    Epic: CRDB-18752
    
    Release note: None

--- pkg/sql/show_tenant.go
+++ pkg/sql/show_tenant.go
@@ -34,6 +34,7 @@ const (
 	initReplication   tenantStatus = "INITIALIZING REPLICATION"
 	replicating       tenantStatus = "REPLICATING"
 	replicationPaused tenantStatus = "REPLICATION PAUSED"
+	pendingCutover    tenantStatus = "REPLICATION PENDING CUTOVER"
 	cuttingOver       tenantStatus = "REPLICATION CUTTING OVER"
 	// Users should not see this status normally.
 	replicationUnknownFormat tenantStatus = "REPLICATION UNKNOWN (%s)"
@@ -153,7 +154,10 @@ func getTenantStatus(
 		} else {
 			progress := replicationInfo.IngestionProgress
 			if progress != nil && !progress.CutoverTime.IsEmpty() {
-				return cuttingOver
+				if progress.CutoverStarted {
+					return cuttingOver
+				}
+				return pendingCutover
 			} else {
 				return replicating
 			}
