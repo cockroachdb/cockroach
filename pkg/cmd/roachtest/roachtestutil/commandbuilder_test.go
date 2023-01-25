7ccf2d0a2ba3c53b3b5e925183bbdfe42073158a
Renato Costa <renato@cockroachlabs.com>
2023-01-25 08:50:12 -0500

    roachtestutil: introduce commandbuilder
    `commandbuilder.go` adds the `roachtestutil.Command` type, a thin
    wrapper that helps tests build complex commands to be run in a
    cluster. The way roachtests deal with this right now is by a
    combination of string concatenation and ad-hoc `fmt.Sprint`
    calls. This has at least two drawbacks:
    
    * it makes it difficult to piece together the command that is actually
    being run in a cluster, especially if conditionals and string
    formatters are involved;
    
    * if a function takes a command (string) as parameter, there's no easy
    way to either verify if a flag is present or to safely add a command
    line flag.
    
    The `roachtestutil.Command` type aims to solve both of the
    shortcomings. Over time, roachtests can move from raw strings to this
    type as needed. In addition, as `roachtest` starts to provide higher
    level APIs for test writers, having a more programmable interface to
    commands is desirable.
    
    Epic: CRDB-19321
    
    Release note: None

---
+++ pkg/cmd/roachtest/roachtestutil/commandbuilder_test.go
@@ -0,0 +1,77 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package roachtestutil
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCommand(t *testing.T) {
+	c := NewCommand("./cockroach")
+	require.Equal(t, "./cockroach", c.String())
+
+	c = NewCommand("./cockroach workload init")
+	require.Equal(t, "./cockroach workload init", c.String())
+
+	c = NewCommand("./cockroach").Arg("workload").Arg("init")
+	require.Equal(t, "./cockroach workload init", c.String())
+
+	baseCommand := NewCommand("./cockroach workload run bank").Arg("{pgurl:%d}", 1)
+
+	c = clone(baseCommand)
+	c.Flag("max-ops", 10).Flag("path", "/some/path")
+	require.Equal(t, "./cockroach workload run bank {pgurl:1} --max-ops 10 --path /some/path", c.String())
+
+	c = clone(baseCommand)
+	c.MaybeFlag(true, "max-ops", 10)     // included
+	c.MaybeFlag(false, "concurrency", 8) // not included
+	require.True(t, c.HasFlag("max-ops"))
+	require.False(t, c.HasFlag("concurrency"))
+	require.Equal(t, "./cockroach workload run bank {pgurl:1} --max-ops 10", c.String())
+
+	c = clone(baseCommand)
+	c.ITEFlag(true, "max-ops", 10, 20)
+	c.ITEFlag(false, "duration", 2*time.Hour, 10*time.Minute)
+	require.Equal(t, "./cockroach workload run bank {pgurl:1} --duration 10m0s --max-ops 10", c.String())
+
+	c = clone(baseCommand)
+	c.Option("local")
+	c.MaybeOption(true, "background") // included
+	c.MaybeOption(false, "dry-run")   // not included
+	require.True(t, c.HasFlag("local"))
+	require.True(t, c.HasFlag("background"))
+	require.False(t, c.HasFlag("dry-run"))
+	require.Equal(t, "./cockroach workload run bank {pgurl:1} --background --local", c.String())
+
+	c = clone(baseCommand)
+	c.Flag("c", 10)
+	c.MaybeFlag(true, "n", "8")    // included
+	c.MaybeFlag(false, "j", "yes") // not included
+	c.Option("x")
+	require.True(t, c.HasFlag("c"))
+	require.Equal(t, "./cockroach workload run bank {pgurl:1} -c 10 -n 8 -x", c.String())
+}
+
+func clone(cmd *Command) *Command {
+	flags := make(map[string]*string)
+	for k, v := range cmd.Flags {
+		flags[k] = v
+	}
+
+	return &Command{
+		Binary:    cmd.Binary,
+		Arguments: append([]string{}, cmd.Arguments...),
+		Flags:     flags,
+	}
+}
