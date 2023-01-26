ff2708c4c796417efa49b87a09b94952953b7395
Jackson Owens <jackson@cockroachlabs.com>
2023-01-25 21:10:07 -0500

    cmd/roachtest: adapt disk-stall detection roachtest
    Move the existing disk-stall/* roachtests under disk-stall/fuse/* (for the FUSE
    filesystem approach to stalling) and skip them for now. Currently, they're not
    capable of stalling the disk longer 50us (see #95886), which makes them
    unreliable at exercising stalls.
    
    Add two new roachtests, disk-stall/dmsetup and disk-stall/cgroup that use
    dmsetup and cgroup bandwidth restrctions respectively to reliably induce a
    write stall for an indefinite duration.
    
    Informs #94373.
    Epic: None
    Release note: None

--- pkg/cmd/roachtest/tests/disk_stall.go
+++ pkg/cmd/roachtest/tests/disk_stall.go
@@ -12,128 +12,301 @@ package tests
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"runtime"
-	"strings"
+	"strconv"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/cluster"
+	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/option"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/registry"
+	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/spec"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/test"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
+	"github.com/cockroachdb/cockroach/pkg/util/httputil"
+	"github.com/cockroachdb/cockroach/pkg/util/retry"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 )
 
 // registerDiskStalledDetection registers the disk stall test.
 func registerDiskStalledDetection(r registry.Registry) {
-	for _, affectsLogDir := range []bool{false, true} {
-		for _, affectsDataDir := range []bool{false, true} {
+	stallers := map[string]func(test.Test, cluster.Cluster) diskStaller{
+		"dmsetup": func(t test.Test, c cluster.Cluster) diskStaller { return &dmsetupDiskStaller{t: t, c: c} },
+		"cgroup":  func(t test.Test, c cluster.Cluster) diskStaller { return &cgroupDiskStaller{t: t, c: c} },
+	}
+	for name, makeStaller := range stallers {
+		name, makeStaller := name, makeStaller
+		r.Add(registry.TestSpec{
+			Name:    fmt.Sprintf("disk-stalled/%s", name),
+			Owner:   registry.OwnerStorage,
+			Cluster: r.MakeClusterSpec(1),
+			Timeout: 10 * time.Minute,
+			Run: func(ctx context.Context, t test.Test, c cluster.Cluster) {
+				runDiskStalledDetection(ctx, t, c, makeStaller(t, c))
+			},
+		})
+	}
+
+	for _, stallLogDir := range []bool{false, true} {
+		for _, stallDataDir := range []bool{false, true} {
 			// Grab copies of the args because we'll pass them into a closure.
 			// Everyone's favorite bug to write in Go.
-			affectsLogDir := affectsLogDir
-			affectsDataDir := affectsDataDir
+			stallLogDir := stallLogDir
+			stallDataDir := stallDataDir
 			r.Add(registry.TestSpec{
+				Skip:        "#95886",
+				SkipDetails: "The test current only induces a 50us disk-stall, which cannot be reliably detected.",
 				Name: fmt.Sprintf(
-					"disk-stalled/log=%t,data=%t",
-					affectsLogDir, affectsDataDir,
+					"disk-stalled/fuse/log=%t,data=%t",
+					stallLogDir, stallDataDir,
 				),
 				Owner:   registry.OwnerStorage,
 				Cluster: r.MakeClusterSpec(1),
 				Run: func(ctx context.Context, t test.Test, c cluster.Cluster) {
-					runDiskStalledDetection(ctx, t, c, affectsLogDir, affectsDataDir)
+					runDiskStalledDetection(ctx, t, c, &fuseDiskStaller{
+						t:         t,
+						c:         c,
+						stallLogs: stallLogDir,
+						stallData: stallDataDir,
+					})
 				},
 			})
 		}
 	}
 }
 
-func runDiskStalledDetection(
-	ctx context.Context, t test.Test, c cluster.Cluster, affectsLogDir bool, affectsDataDir bool,
-) {
-	if c.IsLocal() && runtime.GOOS != "linux" {
-		t.Fatalf("must run on linux os, found %s", runtime.GOOS)
-	}
+func runDiskStalledDetection(ctx context.Context, t test.Test, c cluster.Cluster, s diskStaller) {
+	const maxSyncDur = 10 * time.Second
+	startOpts := option.DefaultStartOpts()
+	// TODO(jackson): Configure --store to s.DataDir() and logging dir to
+	// s.LogDir()
+	startSettings := install.MakeClusterSettings()
+	startSettings.Env = append(startSettings.Env,
+		"COCKROACH_AUTO_BALLAST=false",
+		fmt.Sprintf("COCKROACH_LOG_MAX_SYNC_DURATION=%s", maxSyncDur),
+		fmt.Sprintf("COCKROACH_ENGINE_MAX_SYNC_DURATION_DEFAULT=%s", maxSyncDur))
 
-	n := c.Node(1)
+	t.Status("setting up disk staller")
+	s.Setup(ctx)
+	defer s.Cleanup(ctx)
 
 	c.Put(ctx, t.Cockroach(), "./cockroach")
-	c.Run(ctx, n, "sudo umount -f {store-dir}/faulty || true")
-	c.Run(ctx, n, "mkdir -p {store-dir}/{real,faulty} || true")
-	// Make sure the actual logs are downloaded as artifacts.
-	c.Run(ctx, n, "rm -f logs && ln -s {store-dir}/real/logs logs || true")
-
-	t.Status("setting up charybdefs")
-
-	if err := c.Install(ctx, t.L(), n, "charybdefs"); err != nil {
+	c.Start(ctx, t.L(), startOpts, startSettings, c.All())
+	defer c.Stop(ctx, t.L(), option.DefaultStopOpts(), c.All())
+	adminUIAddrs, err := c.ExternalAdminUIAddr(ctx, t.L(), c.All())
+	if err != nil {
 		t.Fatal(err)
 	}
-	c.Run(ctx, n, "sudo charybdefs {store-dir}/faulty -oallow_other,modules=subdir,subdir={store-dir}/real")
-	c.Run(ctx, n, "sudo mkdir -p {store-dir}/real/logs")
-	c.Run(ctx, n, "sudo chmod -R 777 {store-dir}/{real,faulty}")
-
-	errCh := make(chan install.RunResultDetails)
-
-	// NB: charybdefs' delay nemesis introduces 50ms per syscall. It would
-	// be nicer to introduce a longer delay, but this works.
-	tooShortSync := 40 * time.Millisecond
-
-	maxLogSync := time.Hour
-	logDir := "real/logs"
-	if affectsLogDir {
-		logDir = "faulty/logs"
-		maxLogSync = tooShortSync
-	}
-	maxDataSync := time.Hour
-	dataDir := "real"
-	if affectsDataDir {
-		maxDataSync = tooShortSync
-		dataDir = "faulty"
+	uptime, err := getUptime(ctx, adminUIAddrs[0])
+	if err != nil {
+		t.Fatal(err)
+	} else if uptime == 0 {
+		t.Fatal("uptime is zero")
 	}
 
-	tStarted := timeutil.Now()
-	dur := 10 * time.Minute
-	if !affectsDataDir && !affectsLogDir {
-		dur = 30 * time.Second
+	// TODO(jackson): Add a concurrent workload.
+
+	// Wait between [1m,5m) before stalling the disk.
+	pauseDur := time.Duration(rand.Intn(4))*time.Minute + time.Minute
+	pauseBeforeStall := time.After(pauseDur)
+	t.Status("pausing ", pauseDur, " before inducing write stall")
+	select {
+	case <-ctx.Done():
+		t.Fatalf("context done before stall: %s", ctx.Err())
+	case <-pauseBeforeStall:
 	}
 
-	go func() {
-		t.WorkerStatus("running server")
-		result, err := c.RunWithDetailsSingleNode(ctx, t.L(), n,
-			fmt.Sprintf("timeout --signal 9 %ds env "+
-				"COCKROACH_ENGINE_MAX_SYNC_DURATION_DEFAULT=%s "+
-				"COCKROACH_LOG_MAX_SYNC_DURATION=%s "+
-				"COCKROACH_AUTO_BALLAST=false "+
-				"./cockroach start-single-node --insecure --store {store-dir}/%s --log '{sinks: {stderr: {filter: INFO}}, file-defaults: {dir: \"{store-dir}/%s\"}}'",
-				int(dur.Seconds()), maxDataSync, maxLogSync, dataDir, logDir,
-			),
-		)
+	t.Status("inducing write stall")
+	stalledAt := timeutil.Now()
+	s.Stall(ctx, c.All())
+	defer s.Unstall(ctx, c.All())
+
+	t.Status("pausing ", 2*maxSyncDur, " before checking for termination")
+	select {
+	case <-ctx.Done():
+		t.Fatal(ctx.Err())
+	case <-time.After(2 * maxSyncDur):
+	}
+	uptime, err = getUptime(ctx, adminUIAddrs[0])
+	if err == nil && timeutil.Now().Add(-uptime).Before(stalledAt) {
+		t.Fatalf("node's uptime of %s indicates it's been up since before the stall at %s", uptime, stalledAt)
+	}
+}
+
+func getUptime(ctx context.Context, adminUIAddr string) (uptime time.Duration, err error) {
+	var nodeDiagnostics struct {
+		Node struct {
+			Uptime string `json:"uptime"`
+		} `json:"node"`
+	}
+	if err := retry.ForDuration(10*time.Second, func() error {
+		resp, err := httputil.Get(ctx, "http://"+adminUIAddr+"/_status/diagnostics/local")
 		if err != nil {
-			result.Err = err
+			return err
 		}
-		errCh <- result
-	}()
-
-	time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
-
-	t.Status("blocking storage")
-	c.Run(ctx, n, "charybdefs-nemesis --delay")
-
-	result := <-errCh
-	if result.Err == nil {
-		t.Fatalf("expected an error: %s", result.Stdout)
+		defer resp.Body.Close()
+		return json.NewDecoder(resp.Body).Decode(&nodeDiagnostics)
+	}); err != nil {
+		return 0, err
 	}
-
-	// This test can also run in sanity check mode to make sure it doesn't fail
-	// due to the aggressive env vars above.
-	expectMsg := affectsDataDir || affectsLogDir
-
-	if expectMsg != strings.Contains(result.Stderr, "disk stall detected") {
-		t.Fatalf("unexpected output: %v", result.Err)
-	} else if elapsed := timeutil.Since(tStarted); !expectMsg && elapsed < dur {
-		t.Fatalf("no disk stall injected, but process terminated too early after %s (expected >= %s)", elapsed, dur)
+	u, err := strconv.Atoi(nodeDiagnostics.Node.Uptime)
+	if err != nil {
+		return 0, err
 	}
+	return time.Second * time.Duration(u), nil
+}
 
-	c.Run(ctx, n, "charybdefs-nemesis --clear")
-	c.Run(ctx, n, "sudo umount {store-dir}/faulty")
+type diskStaller interface {
+	Setup(ctx context.Context)
+	Cleanup(ctx context.Context)
+	Stall(ctx context.Context, nodes option.NodeListOption)
+	Unstall(ctx context.Context, nodes option.NodeListOption)
+	DataDir() string
+	LogDir() string
+}
+
+type dmsetupDiskStaller struct {
+	t test.Test
+	c cluster.Cluster
+}
+
+var _ diskStaller = (*dmsetupDiskStaller)(nil)
+
+func (s *dmsetupDiskStaller) device() string { return getDevice(s.t, s.c.Spec()) }
+
+func (s *dmsetupDiskStaller) Setup(ctx context.Context) {
+	dev := s.device()
+	s.c.Run(ctx, s.c.All(), `sudo umount /mnt/data1`)
+	s.c.Run(ctx, s.c.All(), `sudo dmsetup remove_all`)
+	s.c.Run(ctx, s.c.All(), `echo "0 $(sudo blockdev --getsz `+dev+`) linear `+dev+` 0" | `+
+		`sudo dmsetup create data1`)
+	s.c.Run(ctx, s.c.All(), `sudo mount /dev/mapper/data1 /mnt/data1`)
+}
+
+func (s *dmsetupDiskStaller) Cleanup(ctx context.Context) {
+	s.c.Run(ctx, s.c.All(), `sudo umount /mnt/data1`)
+	s.c.Run(ctx, s.c.All(), `sudo dmsetup remove_all`)
+	s.c.Run(ctx, s.c.All(), `sudo mount /mnt/data1`)
+}
+
+func (s *dmsetupDiskStaller) Stall(ctx context.Context, nodes option.NodeListOption) {
+	s.c.Run(ctx, nodes, `sudo dmsetup suspend --noflush --nolockfs data1`)
+}
+
+func (s *dmsetupDiskStaller) Unstall(ctx context.Context, nodes option.NodeListOption) {
+	s.c.Run(ctx, nodes, `sudo dmsetup resume data1`)
+}
+
+func (s *dmsetupDiskStaller) DataDir() string { return "{store-dir}" }
+func (s *dmsetupDiskStaller) LogDir() string  { return "logs" }
+
+type cgroupDiskStaller struct {
+	t test.Test
+	c cluster.Cluster
+}
+
+var _ diskStaller = (*cgroupDiskStaller)(nil)
+
+func (s *cgroupDiskStaller) DataDir() string             { return "{store-dir}" }
+func (s *cgroupDiskStaller) LogDir() string              { return "logs" }
+func (s *cgroupDiskStaller) Setup(ctx context.Context)   {}
+func (s *cgroupDiskStaller) Cleanup(ctx context.Context) {}
+
+func (s *cgroupDiskStaller) Stall(ctx context.Context, nodes option.NodeListOption) {
+	s.setThroughput(ctx, nodes, "write", 1)
+}
+
+func (s *cgroupDiskStaller) Unstall(ctx context.Context, nodes option.NodeListOption) {
+	s.setThroughput(ctx, nodes, "write", 0)
+}
+
+func (s *cgroupDiskStaller) device() (major, minor int) {
+	// TODO(jackson): Programmatically determine the device major,minor numbers.
+	// eg,:
+	//    deviceName := getDevice(s.t, s.c.Spec())
+	//    `cat /proc/partitions` and find `deviceName`
+	switch s.c.Spec().Cloud {
+	case spec.GCE:
+		// ls -l /dev/nvme0n1
+		// brw-rw---- 1 root disk 259, 0 Jan 26 20:05 /dev/nvme0n1
+		return 259, 0
+	default:
+		s.t.Fatalf("unsupported cloud %q", s.c.Spec().Cloud)
+		return 0, 0
+	}
+}
+
+func (s *cgroupDiskStaller) setThroughput(
+	ctx context.Context, nodes option.NodeListOption, readOrWrite string, bytesPerSecond int,
+) {
+	major, minor := s.device()
+	s.c.Run(ctx, nodes, fmt.Sprintf(`echo %d:%d %d | sudo cat > /sys/fs/cgroup/blkio/blkio.throttle.%s_bps_device`,
+		major, minor, bytesPerSecond, readOrWrite))
+}
+
+// fuseDiskStaller uses a FUSE filesystem (charybdefs) to insert an artifical
+// delay on all I/O.
+type fuseDiskStaller struct {
+	t         test.Test
+	c         cluster.Cluster
+	stallLogs bool
+	stallData bool
+}
+
+var _ diskStaller = (*fuseDiskStaller)(nil)
+
+func (s *fuseDiskStaller) DataDir() string {
+	if s.stallData {
+		return "{store-dir}/faulty"
+	}
+	return "{store-dir}/real"
+}
+
+func (s *fuseDiskStaller) LogDir() string {
+	if s.stallLogs {
+		return "{store-dir}/faulty/logs"
+	}
+	return "{store-dir}/real/logs"
+}
+
+func (s *fuseDiskStaller) Setup(ctx context.Context) {
+	if s.c.IsLocal() && runtime.GOOS != "linux" {
+		s.t.Fatalf("must run on linux os, found %s", runtime.GOOS)
+	}
+	s.t.Status("setting up charybdefs")
+	if err := s.c.Install(ctx, s.t.L(), s.c.All(), "charybdefs"); err != nil {
+		s.t.Fatal(err)
+	}
+	s.c.Run(ctx, s.c.All(), "sudo umount -f {store-dir}/faulty || true")
+	s.c.Run(ctx, s.c.All(), "mkdir -p {store-dir}/{real,faulty} || true")
+	s.c.Run(ctx, s.c.All(), "rm -f logs && ln -s {store-dir}/real/logs logs || true")
+	s.c.Run(ctx, s.c.All(), "sudo charybdefs {store-dir}/faulty -oallow_other,modules=subdir,subdir={store-dir}/real")
+	s.c.Run(ctx, s.c.All(), "sudo mkdir -p {store-dir}/real/logs")
+	s.c.Run(ctx, s.c.All(), "sudo chmod -R 777 {store-dir}/{real,faulty}")
+}
+
+func (s *fuseDiskStaller) Cleanup(ctx context.Context) {
+	s.c.Run(ctx, s.c.All(), "sudo umount {store-dir}/faulty")
+}
+
+func (s *fuseDiskStaller) Stall(ctx context.Context, nodes option.NodeListOption) {
+	s.c.Run(ctx, nodes, "charybdefs-nemesis --delay")
+}
+
+func (s *fuseDiskStaller) Unstall(ctx context.Context, nodes option.NodeListOption) {
+	s.c.Run(ctx, nodes, "charybdefs-nemesis --clear")
+}
+
+func getDevice(t test.Test, s spec.ClusterSpec) string {
+	switch s.Cloud {
+	case spec.GCE:
+		return "/dev/nvme0n1"
+	case spec.AWS:
+		return "/dev/nvme1n1"
+	default:
+		t.Fatalf("unsupported cloud %q", s.Cloud)
+		return ""
+	}
 }
