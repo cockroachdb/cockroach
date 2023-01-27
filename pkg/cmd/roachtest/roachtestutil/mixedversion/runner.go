e70dac8ba14fc983bbc81ac1e33b6b9cc19602dd
Renato Costa <renato@cockroachlabs.com>
2023-01-27 11:54:38 -0500

    roachtest: support running steps in the background in `mixedversion`
    This adds a `BackgroundFunc` API to the `mixedversion` package in
    roachtest, allowing test writers to run tasks in the background during
    an upgrade test. The most common use-case for this functionality is
    running a workload while the cluster upgrades (other similar use-cases
    exist in a variety of tests); for this reason, a `Workload`
    convenience function is added that allows tests to add a workload to a
    mixed-version test with one function call.
    
    Currently, each test needs to devise their own mechanism to: spawn the
    background task; monitor its execution; and terminate the test on
    error.
    
    The current API aims to reduce copying and pasting of such logic,
    making for a more declarative test. In the future, the test planner
    itself could decide to run some steps in the background and it should
    be able to leverage the mechanisms introduced in this commit.
    
    Epic: CRDB-19321
    
    Release note: None

--- pkg/cmd/roachtest/roachtestutil/mixedversion/runner.go
+++ pkg/cmd/roachtest/roachtestutil/mixedversion/runner.go
@@ -16,10 +16,12 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"path"
 	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
+	"sync/atomic"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/cluster"
@@ -35,24 +37,48 @@ type (
 	// Helper is the struct passed to user-functions providing helper
 	// functions that mixed-version tests can use.
 	Helper struct {
-		ctx         context.Context
-		testContext *Context
-		conns       []*gosql.DB
-		crdbNodes   option.NodeListOption
+		ctx             context.Context
+		testContext     *Context
+		backgroundCount int64
+		runner          *testRunner
+		stepLogger      *logger.Logger
+	}
 
-		stepLogger *logger.Logger
+	// backgroundEvent is the struct sent by background steps when they
+	// finish (successfully or not).
+	backgroundEvent struct {
+		Name string
+		Err  error
+	}
+
+	backgroundRunner struct {
+		group  *errgroup.Group
+		ctx    context.Context
+		events chan backgroundEvent
+	}
+
+	testFailure struct {
+		displayed             bool
+		description           string
+		seed                  int64
+		binaryVersions        []roachpb.Version
+		clusterVersionsBefore []roachpb.Version
+		clusterVersionsAfter  []roachpb.Version
 	}
 
 	testRunner struct {
 		ctx       context.Context
+		cancel    context.CancelFunc
 		plan      *TestPlan
 		cluster   cluster.Cluster
 		crdbNodes option.NodeListOption
 		seed      int64
 		logger    *logger.Logger
 
-		binaryVersions  []roachpb.Version
-		clusterVersions []roachpb.Version
+		binaryVersions  atomic.Value
+		clusterVersions atomic.Value
+
+		background *backgroundRunner
 
 		connCache []*gosql.DB
 	}
@@ -65,6 +91,7 @@ var (
 
 func newTestRunner(
 	ctx context.Context,
+	cancel context.CancelFunc,
 	plan *TestPlan,
 	l *logger.Logger,
 	c cluster.Cluster,
@@ -72,12 +99,14 @@ func newTestRunner(
 	randomSeed int64,
 ) *testRunner {
 	return &testRunner{
-		ctx:       ctx,
-		plan:      plan,
-		logger:    l,
-		cluster:   c,
-		crdbNodes: crdbNodes,
-		seed:      randomSeed,
+		ctx:        ctx,
+		cancel:     cancel,
+		plan:       plan,
+		logger:     l,
+		cluster:    c,
+		crdbNodes:  crdbNodes,
+		background: newBackgroundRunner(ctx),
+		seed:       randomSeed,
 	}
 }
 
@@ -85,14 +114,39 @@ func newTestRunner(
 // each step in sequence.
 func (tr *testRunner) run() error {
 	defer tr.closeConnections()
+	defer func() {
+		tr.logger.Printf("canceling mixed-version test context")
+		tr.cancel()
+		// Wait for some time so that any background tasks are properly
+		// canceled and their cancelation messages are displayed in the
+		// logs accordingly.
+		time.Sleep(5 * time.Second)
+	}()
 
-	for _, step := range tr.plan.steps {
-		if err := tr.runStep(step); err != nil {
+	stepsErr := make(chan error)
+	go func() {
+		defer close(stepsErr)
+		for _, step := range tr.plan.steps {
+			if err := tr.runStep(step); err != nil {
+				stepsErr <- err
+				return
+			}
+		}
+	}()
+
+	for {
+		select {
+		case err := <-stepsErr:
 			return err
+		case event := <-tr.background.Events():
+			if event.Err == nil {
+				tr.logger.Printf("background step finished: %s", event.Name)
+				continue
+			}
+
+			return fmt.Errorf("background step `%s` returned error: %w", event.Name, event.Err)
 		}
 	}
-
-	return nil
 }
 
 // runStep contains the logic of running a single test step, called
@@ -153,53 +207,87 @@ func (tr *testRunner) runStep(step testStep) error {
 			return err
 		}
 
-		tr.logStep("STARTING", ss, stepLogger)
-		tr.logVersions(stepLogger)
-		start := timeutil.Now()
-		defer func() {
-			prefix := fmt.Sprintf("FINISHED [%s]", timeutil.Since(start))
-			tr.logStep(prefix, ss, stepLogger)
-		}()
-		if err := ss.Run(tr.ctx, stepLogger, tr.cluster, tr.newHelper(stepLogger)); err != nil {
-			return tr.reportError(err, ss, stepLogger)
+		if ss.Background() {
+			tr.startBackgroundStep(ss, stepLogger)
+			return nil
 		}
 
-		return nil
+		return tr.runSingleStep(tr.ctx, ss, stepLogger)
 	}
 }
 
-// reportError augments the error passed with extra
-// information. Specifically, the error message will include the ID of
-// the step that failed, the random seed used, the binary version on
-// each node when the error occurred, and the cluster version before
-// and after the step (in case the failure happened *while* the
-// cluster version was updating).
-func (tr *testRunner) reportError(err error, step singleStep, l *logger.Logger) error {
-	errMsg := fmt.Sprintf("mixed-version test failure while running step %d (%s): %s",
+// runSingleStep takes care of the logic of running a `singleStep`,
+// including logging start and finish times, wrapping the error (if
+// any) with useful information, and renaming the log file to indicate
+// failure. This logic is the same whether running a step in the
+// background or not.
+func (tr *testRunner) runSingleStep(ctx context.Context, ss singleStep, l *logger.Logger) error {
+	tr.logStep("STARTING", ss, l)
+	tr.logVersions(l)
+	start := timeutil.Now()
+	defer func() {
+		prefix := fmt.Sprintf("FINISHED [%s]", timeutil.Since(start))
+		tr.logStep(prefix, ss, l)
+	}()
+	if err := ss.Run(ctx, l, tr.cluster, tr.newHelper(l)); err != nil {
+		if isContextCanceled(err) {
+			l.Printf("step terminated (context canceled)")
+			return nil
+		}
+		return tr.stepError(err, ss, l)
+	}
+
+	return nil
+}
+
+func (tr *testRunner) startBackgroundStep(ss singleStep, l *logger.Logger) {
+	tr.background.Start(ss.Description(), func(ctx context.Context) error {
+		return tr.runSingleStep(ctx, ss, l)
+	})
+}
+
+// stepError augments generates a `testFailure` error by augmenting
+// the error passed with extra information. Specifically, the error
+// message will include the ID of the step that failed, the random
+// seed used, the binary version on each node when the error occurred,
+// and the cluster version before and after the step (in case the
+// failure happened *while* the cluster version was updating).
+func (tr *testRunner) stepError(err error, step singleStep, l *logger.Logger) error {
+	desc := fmt.Sprintf("mixed-version test failure while running step %d (%s): %s",
 		step.ID(), step.Description(), err,
 	)
-	debugInfo := func(label, value string) string {
-		return fmt.Sprintf("%-40s%s", label+":", value)
-	}
-	seedInfo := debugInfo("test random seed", strconv.FormatInt(tr.seed, 10))
-	binaryVersions := debugInfo("binary versions", formatVersions(tr.binaryVersions))
-	clusterVersionsBefore := debugInfo("cluster versions before failure", formatVersions(tr.clusterVersions))
-	var clusterVersionsAfter string
-	if err := tr.refreshClusterVersions(); err == nil {
-		clusterVersionsBefore += "\n"
-		clusterVersionsAfter = debugInfo("cluster versions after failure", formatVersions(tr.clusterVersions))
-	} else {
+
+	return tr.testFailure(desc, l)
+}
+
+// testFailure generates a `testFailure` with the given
+// description. It logs the error to the logger passed, and renames
+// the underlying file to include the "FAILED" prefix to help in
+// debugging.
+func (tr *testRunner) testFailure(desc string, l *logger.Logger) error {
+	clusterVersionsBefore := tr.clusterVersions
+	if err := tr.refreshClusterVersions(); err != nil {
 		tr.logger.Printf("failed to fetch cluster versions after failure: %s", err)
 	}
+	clusterVersionsAfter := tr.clusterVersions
+
+	tf := &testFailure{
+		description:           desc,
+		seed:                  tr.seed,
+		binaryVersions:        loadAtomicVersions(tr.binaryVersions),
+		clusterVersionsBefore: loadAtomicVersions(clusterVersionsBefore),
+		clusterVersionsAfter:  loadAtomicVersions(clusterVersionsAfter),
+	}
+
+	// print the test failure on the step's logger for convenience, and
+	// to reduce cross referencing of logs.
+	l.Printf("%v", tf)
 
 	if err := renameFailedLogger(l); err != nil {
 		tr.logger.Printf("could not rename failed step logger: %v", err)
 	}
 
-	return fmt.Errorf(
-		"%s\n%s\n%s\n%s%s",
-		errMsg, seedInfo, binaryVersions, clusterVersionsBefore, clusterVersionsAfter,
-	)
+	return tf
 }
 
 func (tr *testRunner) logStep(prefix string, step singleStep, l *logger.Logger) {
@@ -211,12 +299,15 @@ func (tr *testRunner) logStep(prefix string, step singleStep, l *logger.Logger) 
 // cluster versions on each node. The cached versions should exist for
 // all steps but the first one (when we start the cluster itself).
 func (tr *testRunner) logVersions(l *logger.Logger) {
-	if tr.binaryVersions == nil || tr.clusterVersions == nil {
+	binaryVersions := loadAtomicVersions(tr.binaryVersions)
+	clusterVersions := loadAtomicVersions(tr.clusterVersions)
+
+	if binaryVersions == nil || clusterVersions == nil {
 		return
 	}
 
-	l.Printf("binary versions: %s", formatVersions(tr.binaryVersions))
-	l.Printf("cluster versions: %s", formatVersions(tr.clusterVersions))
+	l.Printf("binary versions: %s", formatVersions(binaryVersions))
+	l.Printf("cluster versions: %s", formatVersions(clusterVersions))
 }
 
 // loggerFor creates a logger instance to be used by a test step. Logs
@@ -227,22 +318,25 @@ func (tr *testRunner) loggerFor(step singleStep) (*logger.Logger, error) {
 	name := invalidChars.ReplaceAllString(strings.ToLower(step.Description()), "")
 	name = fmt.Sprintf("%d_%s", step.ID(), name)
 
-	prefix := fmt.Sprintf("%s/%s", logPrefix, name)
+	prefix := path.Join(logPrefix, name)
 	return prefixedLogger(tr.logger, prefix)
 }
 
 // refreshBinaryVersions updates the internal `binaryVersions` field
-// with the binary version running on each node of the cluster.
+// with the binary version running on each node of the cluster. We use
+// the `atomic` package here as this function may be called by two
+// steps that are running concurrently.
 func (tr *testRunner) refreshBinaryVersions() error {
-	tr.binaryVersions = make([]roachpb.Version, 0, len(tr.crdbNodes))
+	newBinaryVersions := make([]roachpb.Version, 0, len(tr.crdbNodes))
 	for _, node := range tr.crdbNodes {
 		bv, err := clusterupgrade.BinaryVersion(tr.conn(node))
 		if err != nil {
 			return fmt.Errorf("failed to get binary version for node %d: %w", node, err)
 		}
-		tr.binaryVersions = append(tr.binaryVersions, bv)
+		newBinaryVersions = append(newBinaryVersions, bv)
 	}
 
+	tr.binaryVersions.Store(newBinaryVersions)
 	return nil
 }
 
@@ -250,15 +344,16 @@ func (tr *testRunner) refreshBinaryVersions() error {
 // with the current view of the cluster version in each of the nodes
 // of the cluster.
 func (tr *testRunner) refreshClusterVersions() error {
-	tr.clusterVersions = make([]roachpb.Version, 0, len(tr.crdbNodes))
+	newClusterVersions := make([]roachpb.Version, 0, len(tr.crdbNodes))
 	for _, node := range tr.crdbNodes {
 		cv, err := clusterupgrade.ClusterVersion(tr.ctx, tr.conn(node))
 		if err != nil {
 			return fmt.Errorf("failed to get cluster version for node %d: %w", node, err)
 		}
-		tr.clusterVersions = append(tr.clusterVersions, cv)
+		newClusterVersions = append(newClusterVersions, cv)
 	}
 
+	tr.clusterVersions.Store(newClusterVersions)
 	return nil
 }
 
@@ -294,8 +389,7 @@ func (tr *testRunner) maybeInitConnections() error {
 func (tr *testRunner) newHelper(l *logger.Logger) *Helper {
 	return &Helper{
 		ctx:        tr.ctx,
-		conns:      tr.connCache,
-		crdbNodes:  tr.crdbNodes,
+		runner:     tr,
 		stepLogger: l,
 	}
 }
@@ -314,10 +408,41 @@ func (tr *testRunner) closeConnections() {
 	}
 }
 
+func newBackgroundRunner(ctx context.Context) *backgroundRunner {
+	g, _ := errgroup.WithContext(ctx)
+	return &backgroundRunner{
+		group:  g,
+		ctx:    ctx,
+		events: make(chan backgroundEvent),
+	}
+}
+
+// Start will run the function `fn` in a goroutine. Any errors
+// returned by that function are observable by reading from the
+// channel returned by the `Events()` function.
+func (br *backgroundRunner) Start(name string, fn func(context.Context) error) {
+	br.group.Go(func() error {
+		err := fn(br.ctx)
+		br.events <- backgroundEvent{
+			Name: name,
+			Err:  err,
+		}
+		return err
+	})
+}
+
+func (br *backgroundRunner) Events() <-chan backgroundEvent {
+	return br.events
+}
+
+func (h *Helper) RandomNode(prng *rand.Rand, nodes option.NodeListOption) int {
+	return nodes[prng.Intn(len(nodes))]
+}
+
 // RandomDB returns a (nodeID, connection) tuple for a randomly picked
 // cockroach node according to the parameters passed.
 func (h *Helper) RandomDB(prng *rand.Rand, nodes option.NodeListOption) (int, *gosql.DB) {
-	node := nodes[prng.Intn(len(nodes))]
+	node := h.RandomNode(prng, nodes)
 	return node, h.Connect(node)
 }
 
@@ -325,7 +450,7 @@ func (h *Helper) RandomDB(prng *rand.Rand, nodes option.NodeListOption) (int, *g
 // database node. The query and the node picked are logged in the logs
 // of the step that calls this function.
 func (h *Helper) QueryRow(rng *rand.Rand, query string, args ...interface{}) *gosql.Row {
-	node, db := h.RandomDB(rng, h.crdbNodes)
+	node, db := h.RandomDB(rng, h.runner.crdbNodes)
 	h.stepLogger.Printf("running SQL statement:\n%s\nArgs: %v\nNode: %d", query, args, node)
 	return db.QueryRowContext(h.ctx, query, args...)
 }
@@ -334,14 +459,14 @@ func (h *Helper) QueryRow(rng *rand.Rand, query string, args ...interface{}) *go
 // The query and the node picked are logged in the logs of the step
 // that calls this function.
 func (h *Helper) Exec(rng *rand.Rand, query string, args ...interface{}) error {
-	node, db := h.RandomDB(rng, h.crdbNodes)
+	node, db := h.RandomDB(rng, h.runner.crdbNodes)
 	h.stepLogger.Printf("running SQL statement:\n%s\nArgs: %v\nNode: %d", query, args, node)
 	_, err := db.ExecContext(h.ctx, query, args...)
 	return err
 }
 
 func (h *Helper) Connect(node int) *gosql.DB {
-	return h.conns[node-1]
+	return h.runner.conn(node)
 }
 
 // SetContext should be called by steps that need access to the test
@@ -356,11 +481,97 @@ func (h *Helper) Context() *Context {
 	return h.testContext
 }
 
+// Background allows test writers to create functions that run in the
+// background in mixed-version hooks.
+func (h *Helper) Background(name string, fn func(context.Context, *logger.Logger) error) {
+	h.runner.background.Start(name, func(ctx context.Context) error {
+		bgLogger, err := h.loggerFor(name)
+		if err != nil {
+			return fmt.Errorf("failed to create logger for background function %q: %w", name, err)
+		}
+
+		err = fn(ctx, bgLogger)
+		if err != nil {
+			if isContextCanceled(err) {
+				bgLogger.Printf("background function terminated (context canceled)")
+				return nil
+			}
+
+			desc := fmt.Sprintf("error in background function: %s", err)
+			return h.runner.testFailure(desc, bgLogger)
+		}
+
+		return nil
+	})
+}
+
+// BackgroundCommand has the same semantics of `Background()`; the
+// command passed will run and the test will fail if the command is
+// not successful.
+func (h *Helper) BackgroundCommand(cmd string, nodes option.NodeListOption) {
+	desc := fmt.Sprintf("run command: %q", cmd)
+	h.Background(desc, func(ctx context.Context, l *logger.Logger) error {
+		l.Printf("running command `%s` on nodes %v in the background", cmd, nodes)
+		return h.runner.cluster.RunE(ctx, nodes, cmd)
+	})
+}
+
+// loggerFor creates a logger instance to be used by background
+// functions (created by calling `Background` on the helper
+// instance). It is similar to the logger instances created for
+// mixed-version steps, but with the `background_` prefix.
+func (h *Helper) loggerFor(name string) (*logger.Logger, error) {
+	atomic.AddInt64(&h.backgroundCount, 1)
+
+	fileName := invalidChars.ReplaceAllString(strings.ToLower(name), "")
+	fileName = fmt.Sprintf("background_%s_%d", fileName, h.backgroundCount)
+	fileName = path.Join(logPrefix, fileName)
+
+	return prefixedLogger(h.runner.logger, fileName)
+}
+
+func (tf *testFailure) Error() string {
+	if tf.displayed {
+		return tf.description
+	}
+
+	tf.displayed = true
+	debugInfo := func(label, value string) string {
+		return fmt.Sprintf("%-40s%s", label+":", value)
+	}
+	seedInfo := debugInfo("test random seed", strconv.FormatInt(tf.seed, 10))
+	binaryVersions := debugInfo("binary versions", formatVersions(tf.binaryVersions))
+	clusterVersionsBefore := debugInfo(
+		"cluster versions before failure",
+		formatVersions(tf.clusterVersionsBefore),
+	)
+	var clusterVersionsAfter string
+	if cv := tf.clusterVersionsAfter; cv != nil {
+		clusterVersionsBefore += "\n"
+		clusterVersionsAfter = debugInfo("cluster versions after failure", formatVersions(cv))
+	}
+
+	return fmt.Sprintf(
+		"%s\n%s\n%s\n%s%s",
+		tf.description, seedInfo, binaryVersions, clusterVersionsBefore, clusterVersionsAfter,
+	)
+}
+
 func renameFailedLogger(l *logger.Logger) error {
 	currentFileName := l.File.Name()
-	newLogName := strings.TrimSuffix(currentFileName, filepath.Ext(currentFileName))
-	newLogName += "_FAILED.log"
+	newLogName := path.Join(
+		filepath.Dir(currentFileName),
+		"FAILED_"+filepath.Base(currentFileName),
+	)
 	return os.Rename(currentFileName, newLogName)
+}
+
+func loadAtomicVersions(v atomic.Value) []roachpb.Version {
+	if v.Load() == nil {
+		return nil
+	}
+
+	return v.Load().([]roachpb.Version)
 }
 
 func formatVersions(versions []roachpb.Version) string {
@@ -370,4 +581,17 @@ func formatVersions(versions []roachpb.Version) string {
 	}
 
 	return fmt.Sprintf("[%s]", strings.Join(pairs, ", "))
+}
+
+// isContextCanceled returns a boolean indicating whether the error
+// given happened because some context was canceled.
+func isContextCanceled(err error) bool {
+	// TODO(renato): unfortunately, we have to resort to string
+	// comparison here. The most common use case for this function is
+	// detecting cluster commands that fail when the test context is
+	// canceled (after test success or failure), and roachtest does not
+	// return an error that wraps the context cancelation (in other
+	// words, `errors.Is` doesn't work). Once we fix this behavior, we
+	// should use structured errors here.
+	return strings.Contains(err.Error(), context.Canceled.Error())
 }
