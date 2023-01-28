2cfc5e2b97219864bef9179149c229d4305f17e1
Jackson Owens <jackson@cockroachlabs.com>
2023-01-28 13:57:22 -0500

    server: close listeners and all open connections on disk stall
    Disk stalls prevent a node from making progress. Any ranges for which the
    stalled node is leaseholder may also be prevented from making progress while
    the stalled node remains online but incapacitated. CockroachDB nodes detect
    stalls within their stores through timing all write filesystem operations.
    Previously, when a stall was detected, Cockroach would simply fatal the
    process. However, a process blocked on disk IO cannot be terminated. The
    process will enter the zombie state, but will be unable to be reaped.
    
    This commit adds a new step to disk stall handling, closing all open sockets.
    
    Epic: None
    Release note (bug fix): Fix a bug where a node with a disk stall would continue
    to accept new connections and preserve existing connections until the disk
    stall abated.

---
+++ pkg/util/log/exit_override_nonunix.go
@@ -0,0 +1,20 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+//go:build !darwin && !dragonfly && !freebsd && !linux && !netbsd && !openbsd && !solaris
+// +build !darwin,!dragonfly,!freebsd,!linux,!netbsd,!openbsd,!solaris
+
+package log
+
+import "errors"
+
+func forceCloseSocket(fd int) error {
+	return errors.New("unimplemented")
+}
