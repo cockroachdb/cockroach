ffca8882a3ff804c7a699da7f2611e7988ca2d14
Tommy Reilly <treilly@cockroachlabs.com>
2023-01-30 16:37:33 -0500

    kv: add simple bulk put routines for new vectorized insert
    These are simple analogs to existing APIs that do all the memory
    allocations in bulk.
    
    Epic: CRDB-18892
    Informs: #91831
    Release note: None

--- pkg/kv/db_test.go
+++ pkg/kv/db_test.go
@@ -857,3 +857,63 @@ func TestPreservingSteppingOnSenderReplacement(t *testing.T) {
 		require.Equal(t, expectedStepping, txn.ConfigureStepping(ctx, expectedStepping))
 	})
 }
+
+func TestBulkBatchAPI(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	s, db := setup(t)
+	defer s.Stopper().Stop(context.Background())
+	ctx := context.Background()
+
+	kys := []roachpb.Key{[]byte("a"), nil, []byte("b"), []byte("c")}
+	vals := [][]byte{[]byte("you"), nil, []byte("know"), []byte("me")}
+
+	type putter func(*kv.Batch)
+
+	clearKeys := func() {
+		txn := db.NewTxn(ctx, "bulk-test")
+		b := txn.NewBatch()
+		b.Del("a", "b", "c")
+		err := txn.CommitInBatch(ctx, b)
+		require.NoError(t, err)
+	}
+
+	verify := func() {
+		for i, k := range kys {
+			if k == nil {
+				continue
+			}
+			v, err := db.Get(ctx, k)
+			require.NoError(t, err)
+			raw := vals[i]
+			if tv, err := v.Value.GetTuple(); err == nil {
+				raw = tv
+			}
+			require.Equal(t, raw, vals[i])
+		}
+	}
+
+	testF := func(p putter) {
+		txn := db.NewTxn(ctx, "bulk-test")
+		b := txn.NewBatch()
+		p(b)
+		err := txn.CommitInBatch(ctx, b)
+		require.NoError(t, err)
+		verify()
+		clearKeys()
+	}
+
+	testF(func(b *kv.Batch) { b.PutBytes(kys, vals) })
+	testF(func(b *kv.Batch) { b.InitPutBytes(kys, vals) })
+	testF(func(b *kv.Batch) { b.InitPutTuples(kys, vals) })
+	testF(func(b *kv.Batch) { b.CPutTuples(kys, vals) })
+
+	values := make([]roachpb.Value, len(kys))
+	for i, v := range vals {
+		if kys[i] != nil {
+			values[i].InitChecksum(kys[i])
+			values[i].SetTuple(v)
+		}
+	}
+	testF(func(b *kv.Batch) { b.CPutValues(kys, values) })
+}
