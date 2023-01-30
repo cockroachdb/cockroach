1e495a3ee7f7bce3f724ac20be3c6140621ccfa3
Raphael 'kena' Poss <knz@thaumogen.net>
2023-01-30 10:00:15 +0100

    server: only sample process-wide metrics once
    Prior to this patch, the process-wide metrics were being sampled by
    each tenant server. This is unnecessary: they are process-wide so they
    can be sampled only once.
    
    Release note: None

--- pkg/ccl/serverccl/tenant_vars_test.go
+++ pkg/ccl/serverccl/tenant_vars_test.go
@@ -18,7 +18,9 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/base"
 	_ "github.com/cockroachdb/cockroach/pkg/ccl/kvccl"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/server"
 	"github.com/cockroachdb/cockroach/pkg/sql/tests"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
@@ -41,87 +43,100 @@ func TestTenantVars(t *testing.T) {
 	})
 	defer testCluster.Stopper().Stop(ctx)
 
-	server := testCluster.Server(0 /* idx */)
+	srv := testCluster.Server(0 /* idx */)
 
-	tenant, _ := serverutils.StartTenant(t, server, base.TestTenantArgs{
-		TenantID: roachpb.MustMakeTenantID(10 /* id */),
+	testutils.RunTrueAndFalse(t, "shared-process", func(t *testing.T, sharedProcess bool) {
+		var tenant serverutils.TestTenantInterface
+		if !sharedProcess {
+			tenant, _ = serverutils.StartTenant(t, srv, base.TestTenantArgs{
+				TenantID: roachpb.MustMakeTenantID(10 /* id */),
+			})
+		} else {
+			var err error
+			tenant, _, err = srv.(*server.TestServer).StartSharedProcessTenant(ctx,
+				base.TestSharedProcessTenantArgs{
+					TenantName: roachpb.TenantName("test"),
+					TenantID:   roachpb.MustMakeTenantID(20),
+				})
+			require.NoError(t, err)
+		}
+
+		startNowNanos := timeutil.Now().UnixNano()
+		url := tenant.AdminURL() + "/_status/load"
+		client := http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
+		}
+		resp, err := client.Get(url)
+		require.NoError(t, err)
+		defer resp.Body.Close()
+		require.Equal(t, 200, resp.StatusCode,
+			"invalid non-200 status code %v from tenant", resp.StatusCode)
+
+		var parser expfmt.TextParser
+		metrics, err := parser.TextToMetricFamilies(resp.Body)
+		require.NoError(t, err)
+
+		userCPU, found := metrics["sys_cpu_user_ns"]
+		require.True(t, found)
+		require.Len(t, userCPU.GetMetric(), 1)
+		require.Equal(t, io_prometheus_client.MetricType_GAUGE, userCPU.GetType())
+		cpuUserNanos := userCPU.Metric[0].GetGauge().GetValue()
+
+		sysCPU, found := metrics["sys_cpu_sys_ns"]
+		require.True(t, found)
+		require.Len(t, sysCPU.GetMetric(), 1)
+		require.Equal(t, io_prometheus_client.MetricType_GAUGE, sysCPU.GetType())
+		cpuSysNanos := sysCPU.Metric[0].GetGauge().GetValue()
+
+		now, found := metrics["sys_cpu_now_ns"]
+		require.True(t, found)
+		require.Len(t, now.GetMetric(), 1)
+		require.Equal(t, io_prometheus_client.MetricType_GAUGE, now.GetType())
+		nowNanos := now.Metric[0].GetGauge().GetValue()
+
+		// The values are between zero and whatever User/Sys time is observed after the get.
+		require.Positive(t, cpuUserNanos)
+		require.Positive(t, cpuSysNanos)
+		require.Positive(t, nowNanos)
+		cpuTime := gosigar.ProcTime{}
+		require.NoError(t, cpuTime.Get(os.Getpid()))
+		require.LessOrEqual(t, cpuUserNanos, float64(cpuTime.User)*1e6)
+		require.LessOrEqual(t, cpuSysNanos, float64(cpuTime.Sys)*1e6)
+		require.GreaterOrEqual(t, nowNanos, float64(startNowNanos))
+		require.LessOrEqual(t, nowNanos, float64(timeutil.Now().UnixNano()))
+
+		resp, err = client.Get(url)
+		require.NoError(t, err)
+		defer resp.Body.Close()
+		require.Equal(t, 200, resp.StatusCode,
+			"invalid non-200 status code %v from tenant", resp.StatusCode)
+
+		metrics, err = parser.TextToMetricFamilies(resp.Body)
+		require.NoError(t, err)
+
+		userCPU, found = metrics["sys_cpu_user_ns"]
+		require.True(t, found)
+		require.Len(t, userCPU.GetMetric(), 1)
+		require.Equal(t, io_prometheus_client.MetricType_GAUGE, userCPU.GetType())
+		cpuUserNanos2 := userCPU.Metric[0].GetGauge().GetValue()
+
+		sysCPU, found = metrics["sys_cpu_sys_ns"]
+		require.True(t, found)
+		require.True(t, found)
+		require.Len(t, sysCPU.GetMetric(), 1)
+		require.Equal(t, io_prometheus_client.MetricType_GAUGE, sysCPU.GetType())
+		cpuSysNanos2 := sysCPU.Metric[0].GetGauge().GetValue()
+
+		require.LessOrEqual(t, float64(cpuTime.User)*1e6, cpuUserNanos2)
+		require.LessOrEqual(t, float64(cpuTime.Sys)*1e6, cpuSysNanos2)
+
+		_, found = metrics["jobs_running_non_idle"]
+		require.True(t, found)
+		_, found = metrics["sql_query_count"]
+		require.True(t, found)
+		_, found = metrics["sql_conns"]
+		require.True(t, found)
 	})
-
-	startNowNanos := timeutil.Now().UnixNano()
-	url := tenant.AdminURL() + "/_status/load"
-	client := http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		},
-	}
-	resp, err := client.Get(url)
-	require.NoError(t, err)
-	defer resp.Body.Close()
-	require.Equal(t, 200, resp.StatusCode,
-		"invalid non-200 status code %v from tenant", resp.StatusCode)
-
-	var parser expfmt.TextParser
-	metrics, err := parser.TextToMetricFamilies(resp.Body)
-	require.NoError(t, err)
-
-	userCPU, found := metrics["sys_cpu_user_ns"]
-	require.True(t, found)
-	require.Len(t, userCPU.GetMetric(), 1)
-	require.Equal(t, io_prometheus_client.MetricType_GAUGE, userCPU.GetType())
-	cpuUserNanos := userCPU.Metric[0].GetGauge().GetValue()
-
-	sysCPU, found := metrics["sys_cpu_sys_ns"]
-	require.True(t, found)
-	require.Len(t, sysCPU.GetMetric(), 1)
-	require.Equal(t, io_prometheus_client.MetricType_GAUGE, sysCPU.GetType())
-	cpuSysNanos := sysCPU.Metric[0].GetGauge().GetValue()
-
-	now, found := metrics["sys_cpu_now_ns"]
-	require.True(t, found)
-	require.Len(t, now.GetMetric(), 1)
-	require.Equal(t, io_prometheus_client.MetricType_GAUGE, now.GetType())
-	nowNanos := now.Metric[0].GetGauge().GetValue()
-
-	// The values are between zero and whatever User/Sys time is observed after the get.
-	require.Positive(t, cpuUserNanos)
-	require.Positive(t, cpuSysNanos)
-	require.Positive(t, nowNanos)
-	cpuTime := gosigar.ProcTime{}
-	require.NoError(t, cpuTime.Get(os.Getpid()))
-	require.LessOrEqual(t, cpuUserNanos, float64(cpuTime.User)*1e6)
-	require.LessOrEqual(t, cpuSysNanos, float64(cpuTime.Sys)*1e6)
-	require.GreaterOrEqual(t, nowNanos, float64(startNowNanos))
-	require.LessOrEqual(t, nowNanos, float64(timeutil.Now().UnixNano()))
-
-	resp, err = client.Get(url)
-	require.NoError(t, err)
-	defer resp.Body.Close()
-	require.Equal(t, 200, resp.StatusCode,
-		"invalid non-200 status code %v from tenant", resp.StatusCode)
-
-	metrics, err = parser.TextToMetricFamilies(resp.Body)
-	require.NoError(t, err)
-
-	userCPU, found = metrics["sys_cpu_user_ns"]
-	require.True(t, found)
-	require.Len(t, userCPU.GetMetric(), 1)
-	require.Equal(t, io_prometheus_client.MetricType_GAUGE, userCPU.GetType())
-	cpuUserNanos2 := userCPU.Metric[0].GetGauge().GetValue()
-
-	sysCPU, found = metrics["sys_cpu_sys_ns"]
-	require.True(t, found)
-	require.True(t, found)
-	require.Len(t, sysCPU.GetMetric(), 1)
-	require.Equal(t, io_prometheus_client.MetricType_GAUGE, sysCPU.GetType())
-	cpuSysNanos2 := sysCPU.Metric[0].GetGauge().GetValue()
-
-	require.LessOrEqual(t, float64(cpuTime.User)*1e6, cpuUserNanos2)
-	require.LessOrEqual(t, float64(cpuTime.Sys)*1e6, cpuSysNanos2)
-
-	_, found = metrics["jobs_running_non_idle"]
-	require.True(t, found)
-	_, found = metrics["sql_query_count"]
-	require.True(t, found)
-	_, found = metrics["sql_conns"]
-	require.True(t, found)
 }
