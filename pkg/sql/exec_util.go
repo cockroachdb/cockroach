28f0d01a0d42ecd34a70fb5d8623ef9151174553
Evan Wall <wall@cockroachlabs.com>
2023-01-30 16:55:53 -0500

    sql: use RLock in connExecutor.CancelQuery and connExecutor.CancelActiveQueries
    Fixes #95994
    
    `connExecutor.CancelQuery` and `connExecutor.CancelActiveQueries` do not
    modify `mu.ActiveQueries` or the `*queryMetas` inside so they can safely
    use `RLock` instead of `Lock`.
    
    Release note: None

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -2034,12 +2034,6 @@ type queryMeta struct {
 	database string
 }
 
-// cancel cancels the query associated with this queryMeta, by closing the
-// associated stmt context.
-func (q *queryMeta) cancel() {
-	q.cancelQuery()
-}
-
 // SessionDefaults mirrors fields in Session, for restoring default
 // configuration values in SET ... TO DEFAULT (or RESET ...) statements.
 type SessionDefaults map[string]string
@@ -2138,9 +2132,8 @@ func (r *SessionRegistry) deregister(
 }
 
 type RegistrySession interface {
-	user() username.SQLUsername
-	// BaseSessionUser returns the base session's username.
-	BaseSessionUser() username.SQLUsername
+	// SessionUser returns the session user's username.
+	SessionUser() username.SQLUsername
 	hasQuery(queryID clusterunique.ID) bool
 	// CancelQuery cancels the query specified by queryID if it exists.
 	CancelQuery(queryID clusterunique.ID) bool
