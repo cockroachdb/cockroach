024d84935ff7bcd16dc6805b6c1dcbd5472e5397
Tommy Reilly <treilly@cockroachlabs.com>
2023-01-30 16:37:33 -0500

    kv: add simple bulk put routines for new vectorized insert
    These are simple analogs to existing APIs that do all the memory
    allocations in bulk.
    
    Epic: CRDB-18892
    Informs: #91831
    Release note: None

--- pkg/kv/db_test.go
+++ pkg/kv/db_test.go
@@ -857,3 +857,98 @@ func TestPreservingSteppingOnSenderReplacement(t *testing.T) {
 		require.Equal(t, expectedStepping, txn.ConfigureStepping(ctx, expectedStepping))
 	})
 }
+
+func TestBulkBatchAPI(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	s, db := setup(t)
+	defer s.Stopper().Stop(context.Background())
+	ctx := context.Background()
+
+	kys := []roachpb.Key{[]byte("a"), []byte("b"), []byte("c")}
+	vals := [][]byte{[]byte("you"), []byte("know"), []byte("me")}
+
+	type putter func(*kv.Batch)
+
+	clearKeys := func() {
+		txn := db.NewTxn(ctx, "bulk-test")
+		b := txn.NewBatch()
+		b.Del("a", "b", "c")
+		err := txn.CommitInBatch(ctx, b)
+		require.NoError(t, err)
+	}
+
+	verify := func() {
+		for i, k := range kys {
+			v, err := db.Get(ctx, k)
+			require.NoError(t, err)
+			raw := vals[i]
+			if tv, err := v.Value.GetTuple(); err == nil {
+				raw = tv
+			}
+			err = v.Value.Verify(k)
+			require.NoError(t, err)
+			require.Equal(t, raw, vals[i])
+		}
+	}
+
+	testF := func(p putter) {
+		txn := db.NewTxn(ctx, "bulk-test")
+		b := txn.NewBatch()
+		p(b)
+		err := txn.CommitInBatch(ctx, b)
+		require.NoError(t, err)
+		verify()
+		require.Greater(t, len(b.Results), 1)
+		r := b.Results[0]
+		require.Equal(t, len(r.Rows), len(kys))
+		require.NoError(t, r.Err)
+		clearKeys()
+	}
+
+	testF(func(b *kv.Batch) { b.PutBytes(kys, vals) })
+	testF(func(b *kv.Batch) { b.PutTuples(kys, vals) })
+	testF(func(b *kv.Batch) { b.InitPutBytes(kys, vals) })
+	testF(func(b *kv.Batch) { b.InitPutTuples(kys, vals) })
+	testF(func(b *kv.Batch) { b.CPutTuples(kys, vals) })
+
+	values := make([]roachpb.Value, len(kys))
+	for i, v := range vals {
+		if kys[i] != nil {
+			values[i].InitChecksum(kys[i])
+			values[i].SetTuple(v)
+		}
+	}
+	testF(func(b *kv.Batch) { b.CPutValues(kys, values) })
+}
+
+func TestGetResults(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	s, db := setup(t)
+	defer s.Stopper().Stop(context.Background())
+	ctx := context.Background()
+
+	kys1 := []roachpb.Key{[]byte("a"), []byte("b"), []byte("c")}
+	kys2 := []roachpb.Key{[]byte("d"), []byte("e"), []byte("f")}
+	vals := [][]byte{[]byte("you"), []byte("know"), []byte("me")}
+	txn := db.NewTxn(ctx, "bulk-test")
+	b := txn.NewBatch()
+	b.PutBytes(kys1, vals)
+	b.PutBytes(kys2, vals)
+	err := txn.CommitInBatch(ctx, b)
+	require.NoError(t, err)
+	for i := 0; i < len(kys1)+len(kys2); i++ {
+		res, row, err := b.GetResult(i)
+		require.Equal(t, res, &b.Results[i/3])
+		require.Equal(t, row, b.Results[i/3].Rows[i%3])
+		require.NoError(t, err)
+	}
+	// test EndTxn result
+	_, _, err = b.GetResult(len(kys1) + len(kys2))
+	require.NoError(t, err)
+
+	// test out of bounds
+	_, _, err = b.GetResult(len(kys1) + len(kys2) + 1)
+	require.Error(t, err)
+}
