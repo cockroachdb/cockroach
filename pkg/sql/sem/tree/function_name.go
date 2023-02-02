6c9468e36d5336349878bb4fa3362273105a003a
Chengxiong Ruan <chengxiongruan@gmail.com>
2023-02-01 23:37:38 -0500

    sql: implement function OID reference
    This commit implements `[FUNCTION xxxx]` OID references syntax
    of functions. With this change, functions can be called with
    the new OID numerical representation. For example
    `SELECT [FUNCTION 123]('helloworld')`. The intention of this
    syntax is only for internal serialization of references to UDFs.
    But it's general enough for builtin functions as well.
    
    A new implementation of the `ResolvableFunctionReference`
    interface, `OIDFunctionReference` is introduced for function
    resolution purpose for the new syntax.
    
    The `ResolveFunctionByOID` method is also refactored to return
    a qualified name.
    
    Fixes: #83231
    
    Release note: None

--- pkg/sql/sem/tree/function_name.go
+++ pkg/sql/sem/tree/function_name.go
@@ -57,7 +57,7 @@ type FunctionReferenceResolver interface {
 	// there is no function with the same oid.
 	ResolveFunctionByOID(
 		ctx context.Context, oid oid.Oid,
-	) (string, *Overload, error)
+	) (*FunctionName, *Overload, error)
 }
 
 // ResolvableFunctionReference implements the editable reference call of a
@@ -113,6 +113,17 @@ func (ref *ResolvableFunctionReference) Resolve(
 		}
 		ref.FunctionReference = fd
 		return fd, nil
+	case *OIDFunctionReference:
+		fnName, o, err := resolver.ResolveFunctionByOID(ctx, t.OID)
+		if err != nil {
+			return nil, err
+		}
+		fd := &ResolvedFunctionDefinition{
+			Name:      fnName.Object(),
+			Overloads: []QualifiedOverload{{Schema: fnName.Schema(), Overload: o}},
+		}
+		ref.FunctionReference = fd
+		return fd, nil
 	default:
 		return nil, errors.AssertionFailedf("unknown resolvable function reference type %s", t)
 	}
@@ -146,3 +157,16 @@ var _ FunctionReference = &ResolvedFunctionDefinition{}
 func (*UnresolvedName) functionReference()             {}
 func (*FunctionDefinition) functionReference()         {}
 func (*ResolvedFunctionDefinition) functionReference() {}
+func (*OIDFunctionReference) functionReference()       {}
+
+type OIDFunctionReference struct {
+	OID oid.Oid
+}
+
+func (o *OIDFunctionReference) String() string {
+	return AsString(o)
+}
+
+func (o *OIDFunctionReference) Format(ctx *FmtCtx) {
+	ctx.WriteString(fmt.Sprintf("[FUNCTION %d]", o.OID))
+}
