da5aef121d3b0beefb8325c4ea13c317f704dd19
Faizan Qazi <faizan@cockroachlabs.com>
2023-02-02 15:14:23 -0500

    sql/upgrades: add a declarative schema changer upgrade
    Informs: #92605
    
    Previously, we had no way of modifying declarative
    schema changer elements. So deprecating or modifying
    the element model was extremely difficult. This patch,
    adds an upgrade framework that will pause any
    on-going declarative schema change jobs, upgrade descriptors
    to the have the latest elements, and then resume the jobs.
    This model allows us to modify/remove the element model
    easily.
    
    Epic: none
    Release note: None

---
+++ pkg/upgrade/upgrades/schemachanger_elements_test.go
@@ -0,0 +1,206 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package upgrades_test
+
+import (
+	"context"
+	gosql "database/sql"
+	"sync/atomic"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
+	"github.com/cockroachdb/cockroach/pkg/jobs"
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/server"
+	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scexec"
+	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scop"
+	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scplan"
+	"github.com/cockroachdb/cockroach/pkg/sql/tests"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUpgradeSchemaChangerElements(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	ctx := context.Background()
+	const (
+		WaitForSchemaChangeCompletion = 1
+		ForceJobFirst                 = 2
+		NoWaitRequired                = 0
+	)
+	testCases := []struct {
+		name       string
+		after      func(t *testing.T, sqlDB *gosql.DB) error
+		run        func(t *testing.T, sqlDB *gosql.DB) error
+		unpauseJob bool
+		waitType   int
+	}{
+		{
+			"running schema change that will have a deprecated waitForSchemaChangerElementMigration element",
+			nil,
+			func(t *testing.T, sqlDB *gosql.DB) error {
+				_, err := sqlDB.Exec("SET sql_safe_updates=off")
+				require.NoError(t, err)
+				_, err = sqlDB.Exec("ALTER TABLE t DROP COLUMN b")
+				require.ErrorContains(t, err, "was paused before it completed with reason: pause point \"newschemachanger.before.exec\" hit")
+				return nil
+			},
+			true,
+			WaitForSchemaChangeCompletion,
+		},
+		{
+			"running schema change that will have no deprecated elements",
+			nil,
+			func(t *testing.T, sqlDB *gosql.DB) error {
+				_, err := sqlDB.Exec("SET sql_safe_updates=off")
+				require.NoError(t, err)
+				_, err = sqlDB.Exec("ALTER TABLE t ADD COLUMN j int")
+				require.ErrorContains(t, err, "was paused before it completed with reason: pause point \"newschemachanger.before.exec\" hit")
+				return nil
+			},
+			true,
+			ForceJobFirst | WaitForSchemaChangeCompletion, // ADD COLUMN will not have any deprecated elements, so no one will wait for the job.
+		},
+		{
+			"running schema change that will have a deprecated waitForSchemaChangerElementMigration element but is paused",
+			func(t *testing.T, sqlDB *gosql.DB) error {
+				_, err := sqlDB.Exec("SET CLUSTER SETTING jobs.debug.pausepoints=''")
+				require.NoError(t, err)
+				_, err = sqlDB.Exec("RESUME JOB (SELECT job_id FROM crdb_internal.jobs WHERE description LIKE 'ALTER TABLE%' AND status='paused' FETCH FIRST 1 ROWS ONLY);")
+				require.NoError(t, err)
+				testutils.SucceedsSoon(t, func() error {
+					row := sqlDB.QueryRow("SELECT job_id FROM crdb_internal.jobs WHERE description LIKE 'ALTER TABLE%' AND status='succeeded' FETCH FIRST 1 ROWS ONLY;")
+					var id int64
+					if err = row.Scan(&id); err != nil {
+						return err
+					}
+					return nil
+				})
+				return nil
+			},
+			func(t *testing.T, sqlDB *gosql.DB) error {
+				_, err := sqlDB.Exec("SET sql_safe_updates=off")
+				require.NoError(t, err)
+				_, err = sqlDB.Exec("ALTER TABLE t DROP COLUMN b")
+				require.ErrorContains(t, err, "was paused before it completed with reason: pause point \"newschemachanger.before.exec\" hit")
+				return nil
+			},
+			false,
+			NoWaitRequired,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			params, _ := tests.CreateTestServerParams()
+			params.Knobs.Server = &server.TestingKnobs{
+				DisableAutomaticVersionUpgrade: make(chan struct{}),
+				BinaryVersionOverride:          clusterversion.ByKey(clusterversion.V23_1_SchemaChangerDeprecatedIndexPredicates - 1),
+			}
+
+			var (
+				schemaChangeAllowedToComplete chan struct{}
+				waitedForJob                  chan struct{}
+				jobIsPaused                   chan struct{}
+				readyToQuery                  int64
+			)
+			scJobID := jobspb.InvalidJobID
+			params.Knobs.SQLDeclarativeSchemaChanger = &scexec.TestingKnobs{
+				BeforeStage: func(p scplan.Plan, stageIdx int) error {
+					if waitedForJob == nil {
+						return nil
+					}
+					if p.Stages[stageIdx].Phase == scop.PreCommitPhase {
+						scJobID = p.JobID
+					} else if p.Stages[stageIdx].Phase > scop.PreCommitPhase {
+						<-waitedForJob
+						waitedForJob = nil
+						schemaChangeAllowedToComplete <- struct{}{}
+					}
+					return nil
+				},
+			}
+			jobKnobs := jobs.NewTestingKnobsWithShortIntervals()
+			jobKnobs.IntervalOverrides.WaitForJobsInitialDelay = shortInterval()
+			jobKnobs.IntervalOverrides.WaitForJobsMaxDelay = shortInterval()
+			jobKnobs.BeforeWaitForJobsQuery = func(jobs []jobspb.JobID) {
+				if scJobID != jobspb.InvalidJobID {
+					for _, j := range jobs {
+						// The upgrade is waiting for the job...
+						if j == scJobID && atomic.LoadInt64(&readyToQuery) > 0 {
+							scJobID = jobspb.InvalidJobID
+							atomic.StoreInt64(&readyToQuery, 0)
+							waitedForJob <- struct{}{}
+						}
+					}
+
+				}
+			}
+			params.Knobs.JobsTestingKnobs = jobKnobs
+			s, sqlDB, _ := serverutils.StartServer(t, params)
+			defer s.Stopper().Stop(ctx)
+			_, err := sqlDB.Exec("CREATE TABLE t (pk INT PRIMARY KEY, b INT)")
+			require.NoError(t, err)
+			_, err = sqlDB.Exec("CREATE UNIQUE INDEX ON t (b) WHERE pk > 0")
+			require.NoError(t, err)
+			_, err = sqlDB.Exec("SET CLUSTER SETTING jobs.debug.pausepoints='newschemachanger.before.exec'")
+			require.NoError(t, err)
+
+			ctx := context.Background()
+			g := ctxgroup.WithContext(ctx)
+
+			schemaChangeAllowedToComplete = make(chan struct{})
+			waitedForJob = make(chan struct{})
+			jobIsPaused = make(chan struct{})
+			atomic.StoreInt64(&readyToQuery, 0)
+			g.GoCtx(func(ctx context.Context) error {
+				err := tc.run(t, sqlDB)
+				jobIsPaused <- struct{}{}
+				return err
+			})
+
+			g.GoCtx(func(ctx context.Context) error {
+				<-jobIsPaused
+				atomic.StoreInt64(&readyToQuery, 1)
+				if tc.unpauseJob {
+					_, err = sqlDB.Exec("SET CLUSTER SETTING jobs.debug.pausepoints=''")
+					require.NoError(t, err)
+					_, err = sqlDB.Exec("RESUME JOB $1", scJobID)
+					require.NoError(t, err)
+				}
+				_, err = sqlDB.Exec("SET CLUSTER SETTING version = crdb_internal.node_executable_version()")
+				return err
+			})
+
+			if tc.waitType&ForceJobFirst > 0 {
+				// Pretend that we waited for the job, if
+				// we actually wait again a hang will occur.
+				waitedForJob <- struct{}{}
+			}
+			if tc.waitType&WaitForSchemaChangeCompletion > 0 {
+				<-schemaChangeAllowedToComplete
+			}
+			require.NoError(t, g.Wait())
+			if tc.after != nil {
+				// Prevent any unintended waits on schema changes.
+				waitedForJob = nil
+				require.NoError(t, tc.after(t, sqlDB))
+			}
+		})
+	}
+}
