6d7107f00e0bee6d7fbf0594eaca034f2a6bf233
David Hartunian <davidh@cockroachlabs.com>
2023-02-06 17:50:18 -0500

    admin: statement diagnostics uses correct auth helpers
    Previously, the statement diagnostics HTTP handler was initialized using the
    incorrect `ctx` value. This caused the HTTP request context to not be correctly
    handed down to the handler. Furthermore, the call to
    `userFromIncomingRPCContext` was incorrect in this scenario since it relied on
    a gRPC context being populated with HTTP session information, which did not
    exist. That code sets a `root` user when context is missing because the gRPC
    handlers are used for inter-node communication *and* HTTP with the DB console
    and external tools.
    
    This commit attaches the request context to the diagnostics bundle handler
    correctly, and amends the authorization code to use
    `userFromHTTPAuthInfoContext` which correctly reads the session cookie info
    from the request (like many `api/v2` handlers do since those exist outside the
    gRPC infrastructure).
    
    Resolves #99049
    
    Epic: None
    
    Release note (security update): Previously, users could gain unauthorized
    access to statement diagnostic bundles they did not create if they requested
    the bundle through an HTTP request to `/_admin/v1/stmtbundle/<id>` and
    correctly guessed its (non-secret) ID.  This change locks down this endpoint
    behind the usual SQL gating that correctly uses the SQL user in the HTTP
    session as identified by their cookie.

--- pkg/server/admin.go
+++ pkg/server/admin.go
@@ -205,7 +205,15 @@ func (s *adminServer) RegisterGateway(
 			http.Error(w, "invalid id", http.StatusBadRequest)
 			return
 		}
-		s.getStatementBundle(ctx, id, w)
+		// Add default user when running in Insecure mode because we don't
+		// retrieve the user from gRPC metadata (which falls back to `root`)
+		// but from HTTP metadata (which does not).
+		if s.server.cfg.Insecure {
+			ctx := req.Context()
+			ctx = context.WithValue(ctx, webSessionUserKey{}, username.RootUser)
+			req = req.WithContext(ctx)
+		}
+		s.getStatementBundle(req.Context(), id, w)
 	})
 
 	// Register the endpoints defined in the proto.
@@ -2474,14 +2482,13 @@ func (s *adminServer) QueryPlan(
 // getStatementBundle retrieves the statement bundle with the given id and
 // writes it out as an attachment.
 func (s *adminServer) getStatementBundle(ctx context.Context, id int64, w http.ResponseWriter) {
-	sessionUser, err := userFromContext(ctx)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	// This endpoint uses `getSQLUsername` to get its user metadata
+	// due to the fact that it is implemented as a raw HTTP handler
+	// instead of a gRPC handler.
+	sqlUsername := getSQLUsername(ctx)
 	row, err := s.server.sqlServer.internalExecutor.QueryRowEx(
 		ctx, "admin-stmt-bundle", nil, /* txn */
-		sessiondata.InternalExecutorOverride{User: sessionUser},
+		sessiondata.InternalExecutorOverride{User: sqlUsername},
 		"SELECT bundle_chunks FROM system.statement_diagnostics WHERE id=$1 AND bundle_chunks IS NOT NULL",
 		id,
 	)
@@ -2500,7 +2507,7 @@ func (s *adminServer) getStatementBundle(ctx context.Context, id int64, w http.R
 	for _, chunkID := range chunkIDs {
 		chunkRow, err := s.server.sqlServer.internalExecutor.QueryRowEx(
 			ctx, "admin-stmt-bundle", nil, /* txn */
-			sessiondata.InternalExecutorOverride{User: sessionUser},
+			sessiondata.InternalExecutorOverride{User: sqlUsername},
 			"SELECT data FROM system.statement_bundle_chunks WHERE id=$1",
 			chunkID,
 		)
