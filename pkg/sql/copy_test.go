8693921710ce227b23860f16738c0c1f38e8450e
Oliver Tan <otan@cockroachlabs.com>
2023-02-06 15:55:13 +1100

    sql: implement COPY ... TO STDOUT on the conn executor
    Release note: None

--- pkg/sql/copy_test.go
+++ pkg/sql/copy_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016 The Cockroach Authors.
+// Copyright 2022 The Cockroach Authors.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt.
@@ -11,92 +11,190 @@
 package sql
 
 import (
+	"bytes"
+	"context"
+	"fmt"
+	"net/url"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/security/username"
+	"github.com/cockroachdb/cockroach/pkg/sql/tests"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
+	"github.com/jackc/pgx/v4"
+	"github.com/lib/pq"
+	"github.com/stretchr/testify/require"
 )
 
-func TestDecodeCopy(t *testing.T) {
+// TestCopyLogging verifies copy works with logging turned on.
+func TestCopyLogging(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 
-	tests := []struct {
-		in     string
-		expect string
-	}{
-		{
-			in:     "simple",
-			expect: "simple",
-		},
-		{
-			in:     `new\nline`,
-			expect: "new\nline",
-		},
-		{
-			in:     `\b\f\n\r\t\v\\`,
-			expect: "\b\f\n\r\t\v\\",
-		},
-		{
-			in:     `\0\12\123`,
-			expect: "\000\012\123",
-		},
-		{
-			in:     `\x1\xaf`,
-			expect: "\x01\xaf",
-		},
-		{
-			in:     `T\n\07\xEV\x0fA\xb2C\1`,
-			expect: "T\n\007\x0eV\x0fA\xb2C\001",
-		},
-		{
-			in:     `\\\"`,
-			expect: "\\\"",
-		},
-		{
-			in:     `\x`,
-			expect: "x",
-		},
-		{
-			in:     `\xg`,
-			expect: "xg",
-		},
-		{
-			in:     `\`,
-			expect: "\\",
-		},
-		{
-			in:     `\8`,
-			expect: "8",
-		},
-		{
-			in:     `\a`,
-			expect: "a",
-		},
-		{
-			in:     `\x\xg\8\xH\x32\s\`,
-			expect: "xxg8xH2s\\",
-		},
-	}
+	ctx := context.Background()
 
-	for _, test := range tests {
-		t.Run(test.in, func(t *testing.T) {
-			out := decodeCopy(test.in)
-			if out != test.expect {
-				t.Errorf("%q: got %q, expected %q", test.in, out, test.expect)
+	for _, strings := range [][]string{
+		{`SET CLUSTER SETTING sql.trace.log_statement_execute = true`},
+		{`SET CLUSTER SETTING sql.telemetry.query_sampling.enabled = true`},
+		{`SET CLUSTER SETTING sql.log.unstructured_entries.enabled = true`, `SET CLUSTER SETTING sql.trace.log_statement_execute = true`},
+		{`SET CLUSTER SETTING sql.log.admin_audit.enabled = true`},
+	} {
+		t.Run(strings[0], func(t *testing.T) {
+			params, _ := tests.CreateTestServerParams()
+			s, db, _ := serverutils.StartServer(t, params)
+			defer s.Stopper().Stop(context.Background())
+
+			_, err := db.Exec(`
+		CREATE TABLE t (
+			i INT PRIMARY KEY
+		);
+	`)
+			require.NoError(t, err)
+
+			for _, str := range strings {
+				_, err = db.Exec(str)
+				require.NoError(t, err)
 			}
+
+			pgURL, cleanupGoDB, err := sqlutils.PGUrlE(
+				s.ServingSQLAddr(),
+				"StartServer", /* prefix */
+				url.User(username.RootUser),
+			)
+			require.NoError(t, err)
+			s.Stopper().AddCloser(stop.CloserFn(func() { cleanupGoDB() }))
+			config, err := pgx.ParseConfig(pgURL.String())
+			require.NoError(t, err)
+
+			const val = 2
+
+			// We have to start a new connection every time to exercise all possible paths.
+			t.Run("success during COPY FROM", func(t *testing.T) {
+				db := serverutils.OpenDBConn(
+					t, s.ServingSQLAddr(), params.UseDatabase, params.Insecure, s.Stopper())
+				require.NoError(t, err)
+				txn, err := db.Begin()
+				require.NoError(t, err)
+				{
+					stmt, err := txn.Prepare(pq.CopyIn("t", "i"))
+					require.NoError(t, err)
+					_, err = stmt.Exec(val)
+					require.NoError(t, err)
+					require.NoError(t, stmt.Close())
+				}
+				require.NoError(t, txn.Commit())
+
+				var i int
+				require.NoError(t, db.QueryRow("SELECT i FROM t").Scan(&i))
+				require.Equal(t, val, i)
+			})
+
+			t.Run("success during COPY TO", func(t *testing.T) {
+				conn, err := pgx.ConnectConfig(ctx, config)
+				require.NoError(t, err)
+				defer func() {
+					require.NoError(t, conn.Close(ctx))
+				}()
+
+				var buf bytes.Buffer
+				_, err = conn.PgConn().CopyTo(ctx, &buf, "COPY t(i) TO STDOUT")
+				require.NoError(t, err)
+
+				require.Equal(t, fmt.Sprintf("%d\n", val), buf.String())
+			})
+
+			t.Run("error in statement", func(t *testing.T) {
+				db := serverutils.OpenDBConn(
+					t, s.ServingSQLAddr(), params.UseDatabase, params.Insecure, s.Stopper())
+				txn, err := db.Begin()
+				require.NoError(t, err)
+				{
+					_, err := txn.Prepare(pq.CopyIn("xxx", "yyy"))
+					require.Error(t, err)
+					require.ErrorContains(t, err, `relation "xxx" does not exist`)
+				}
+				require.NoError(t, txn.Rollback())
+			})
+
+			t.Run("error during COPY FROM", func(t *testing.T) {
+				db := serverutils.OpenDBConn(
+					t, s.ServingSQLAddr(), params.UseDatabase, params.Insecure, s.Stopper())
+				txn, err := db.Begin()
+				require.NoError(t, err)
+				{
+					stmt, err := txn.Prepare(pq.CopyIn("t", "i"))
+					require.NoError(t, err)
+					_, err = stmt.Exec("bob")
+					require.NoError(t, err)
+					err = stmt.Close()
+					require.Error(t, err)
+					require.ErrorContains(t, err, `could not parse "bob" as type int`)
+				}
+				require.NoError(t, txn.Rollback())
+			})
+
+			t.Run("error in statement during COPY FROM", func(t *testing.T) {
+				db := serverutils.OpenDBConn(
+					t, s.ServingSQLAddr(), params.UseDatabase, params.Insecure, s.Stopper())
+				txn, err := db.Begin()
+				require.NoError(t, err)
+				{
+					_, err := txn.Prepare(pq.CopyIn("xxx", "yyy"))
+					require.Error(t, err)
+					require.ErrorContains(t, err, `relation "xxx" does not exist`)
+				}
+				require.NoError(t, txn.Rollback())
+			})
+
+			t.Run("error during insert phase of COPY FROM", func(t *testing.T) {
+				db := serverutils.OpenDBConn(
+					t, s.ServingSQLAddr(), params.UseDatabase, params.Insecure, s.Stopper())
+				txn, err := db.Begin()
+				require.NoError(t, err)
+				{
+					stmt, err := txn.Prepare(pq.CopyIn("t", "i"))
+					require.NoError(t, err)
+					_, err = stmt.Exec("2")
+					require.NoError(t, err)
+					err = stmt.Close()
+					require.Error(t, err)
+					require.ErrorContains(t, err, `duplicate key value violates unique constraint "t_pkey"`)
+				}
+				require.NoError(t, txn.Rollback())
+			})
+
+			t.Run("error in statement during COPY TO", func(t *testing.T) {
+				conn, err := pgx.ConnectConfig(ctx, config)
+				require.NoError(t, err)
+				defer func() {
+					require.NoError(t, conn.Close(ctx))
+				}()
+
+				var buf bytes.Buffer
+				_, err = conn.PgConn().CopyTo(ctx, &buf, "COPY xxx(i) TO STDOUT")
+				require.Error(t, err)
+				require.ErrorContains(t, err, `relation "xxx" does not exist`)
+			})
+
+			t.Run("error during copy during COPY FROM", func(t *testing.T) {
+				db := serverutils.OpenDBConn(
+					t, s.ServingSQLAddr(), params.UseDatabase, params.Insecure, s.Stopper())
+				txn, err := db.Begin()
+				require.NoError(t, err)
+				{
+					stmt, err := txn.Prepare(pq.CopyIn("t", "i"))
+					require.NoError(t, err)
+					_, err = stmt.Exec("bob")
+					require.NoError(t, err)
+					err = stmt.Close()
+					require.Error(t, err)
+					require.ErrorContains(t, err, `could not parse "bob" as type int`)
+				}
+				require.NoError(t, txn.Rollback())
+			})
 		})
-	}
-}
-
-func BenchmarkDecodeCopySimple(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		decodeCopy("test string")
-	}
-}
-
-func BenchmarkDecodeCopyEscaped(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		decodeCopy(`string \x1 with escape`)
 	}
 }
---
+++ pkg/sql/copy_to.go
@@ -0,0 +1,194 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package sql
+
+import (
+	"bytes"
+	"context"
+	"io"
+
+	"github.com/cockroachdb/cockroach/pkg/kv"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/colinfo"
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgwirebase"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
+	"github.com/cockroachdb/cockroach/pkg/util/errorutil/unimplemented"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+)
+
+// copyToTranslater translates datums into the appropriate format for CopyTo.
+type copyToTranslater interface {
+	translateRow(datums tree.Datums, rcs colinfo.ResultColumns) ([]byte, error)
+}
+
+// textCopyToTranslater is the default text representation of COPY TO from postgres.
+type textCopyToTranslater struct {
+	copyOptions
+	rowBuffer bytes.Buffer
+	fmtCtx    *tree.FmtCtx
+}
+
+var _ copyToTranslater = (*textCopyToTranslater)(nil)
+
+func (t *textCopyToTranslater) translateRow(
+	datums tree.Datums, rcs colinfo.ResultColumns,
+) ([]byte, error) {
+	t.rowBuffer.Reset()
+	t.fmtCtx.Buffer.Reset()
+	for i, d := range datums {
+		if i > 0 {
+			t.rowBuffer.WriteByte(t.delimiter)
+		}
+		if d == tree.DNull {
+			t.rowBuffer.WriteString(t.null)
+			continue
+		}
+		t.fmtCtx.FormatNode(d)
+		if err := encodeCopy(&t.rowBuffer, t.fmtCtx.Buffer.Bytes(), t.delimiter); err != nil {
+			return nil, err
+		}
+		t.fmtCtx.Buffer.Reset()
+	}
+	t.rowBuffer.WriteByte('\n')
+	return t.rowBuffer.Bytes(), nil
+}
+
+func runCopyTo(
+	ctx context.Context, p *planner, txn *kv.Txn, cmd CopyOut,
+) (numOutputRows int, retErr error) {
+	copyOptions, err := processCopyOptions(ctx, p, cmd.Stmt.Options)
+	if err != nil {
+		return 0, err
+	}
+
+	wireFormat := pgwirebase.FormatText
+	var t copyToTranslater
+	switch cmd.Stmt.Options.CopyFormat {
+	case tree.CopyFormatBinary:
+		//wireFormat = pgwirebase.FormatBinary
+		return 0, unimplemented.NewWithIssue(
+			85571,
+			"binary format for COPY TO not implemented",
+		)
+	case tree.CopyFormatCSV:
+		return 0, unimplemented.NewWithIssue(85571, "CSV format for COPY TO not implemented")
+	default:
+		textTranslater := &textCopyToTranslater{
+			copyOptions: copyOptions,
+			fmtCtx:      p.EvalContext().FmtCtx(tree.FmtPgwireText),
+		}
+		t = textTranslater
+	}
+
+	var q string
+	if cmd.Stmt.Statement != nil {
+		q = cmd.Stmt.Statement.String()
+	} else {
+		// Construct a SELECT expression using the table name.
+		// The columns are * if not specified, or otherwise specific each column
+		// item in a SelectExpr.
+		selectExprs := tree.SelectExprs{tree.StarSelectExpr()}
+		if len(cmd.Stmt.Columns) > 0 {
+			selectExprs = make(tree.SelectExprs, len(cmd.Stmt.Columns))
+			for i, c := range cmd.Stmt.Columns {
+				selectExprs[i] = tree.SelectExpr{Expr: &tree.ColumnItem{ColumnName: c}}
+			}
+		}
+		q = (&tree.SelectClause{
+			Exprs: selectExprs,
+			From: tree.From{
+				Tables: tree.TableExprs{&cmd.Stmt.Table},
+			},
+		}).String()
+	}
+
+	it, err := p.execCfg.InternalDB.NewInternalExecutor(p.SessionData()).QueryIteratorEx(
+		ctx,
+		cmd.Stmt.String(),
+		txn,
+		sessiondata.NoSessionDataOverride,
+		q,
+	)
+	if err != nil {
+		return 0, err
+	}
+	defer func() {
+		if err := it.Close(); err != nil {
+			log.SqlExec.Errorf(ctx, "error closing iterator for %s: %+v", cmd, retErr)
+		}
+	}()
+
+	// Send the message describing the columns to the client.
+	if err := cmd.Conn.BeginCopyOut(ctx, it.Types(), wireFormat); err != nil {
+		return 0, err
+	}
+
+	// Send all the rows out to the client.
+	if err := func() error {
+		for {
+			next, err := it.Next(ctx)
+			if err != nil {
+				return err
+			}
+			if !next {
+				break
+			}
+			numOutputRows++
+			row, err := t.translateRow(it.Cur(), it.Types())
+			if err != nil {
+				return err
+			}
+			if err := cmd.Conn.SendCopyData(ctx, row); err != nil {
+				return err
+			}
+		}
+		return nil
+	}(); err != nil {
+		return 0, err
+	}
+	return numOutputRows, cmd.Conn.SendCopyDone(ctx)
+}
+
+var encodeMap = func() map[byte]byte {
+	ret := make(map[byte]byte, len(decodeMap))
+	for k, v := range decodeMap {
+		ret[v] = k
+	}
+	return ret
+}()
+
+// encodeCopy escapes a single COPY field.
+//
+// See: https://www.postgresql.org/docs/9.5/static/sql-copy.html#AEN74432
+// NOTE: we don't have to worry about hex in COPY TO.
+func encodeCopy(w io.Writer, in []byte, delimiter byte) error {
+	lastIndex := 0
+	for i, r := range in {
+		if escapeChar, ok := encodeMap[r]; ok || r == delimiter {
+			// Write everything up to this point.
+			if _, err := w.Write(in[lastIndex:i]); err != nil {
+				return err
+			}
+			lastIndex = i + 1
+
+			// Escape the current character, using the delimiter if needbe.
+			if escapeChar == 0 {
+				escapeChar = delimiter
+			}
+			if _, err := w.Write([]byte{'\\', escapeChar}); err != nil {
+				return err
+			}
+		}
+	}
+	// Flush the remaining bytes.
+	_, err := w.Write(in[lastIndex:])
+	return err
+}
