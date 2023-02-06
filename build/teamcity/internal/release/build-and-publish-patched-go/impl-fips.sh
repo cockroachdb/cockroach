19d1e87cc32e37156b9fc0b08ff5bd8c9edc8ea5
Rail Aliiev <rail@iqchoice.com>
2023-02-06 11:18:13 -0500

    more changes

--- build/teamcity/internal/release/build-and-publish-patched-go/impl-fips.sh
+++ build/teamcity/internal/release/build-and-publish-patched-go/impl-fips.sh
@@ -27,10 +27,11 @@ sed -i "s/go mod tidy/go mod tidy -go=1.16/g" scripts/create-secondary-patch.sh
 cd go/src
 # Apply the CRL patch
 patch -p2 < /bootstrap/diff.patch
+# add a special version modifier so we can explicitly use it in bazel
+sed -i 's/$/fips/' ../VERSION 
 ./make.bash -v
 cd ../..
 GOVERS=$(go/bin/go env GOVERSION)
 GOOS=$(go/bin/go env GOOS)
 GOARCH=$(go/bin/go env GOARCH)
-# Add the "-fips" suffix so we don't have the same basename with our regular Go toolchain.
-tar cf - go | gzip -9 > /artifacts/$GOVERS.$GOOS-$GOARCH-fips.tar.gz
+tar cf - go | gzip -9 > /artifacts/$GOVERS.$GOOS-$GOARCH.tar.gz
