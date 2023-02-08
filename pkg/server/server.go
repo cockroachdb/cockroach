1cdb1acf2f3ac8508219bbc596bd09c8ca828b00
zachlite <zachlite@gmail.com>
2023-02-08 18:04:54 -0500

    server: introduce CriticalNodes rpc
    Since #90016, we've had the ability to generate multi-tenant
    replication reports via `spanconfig.Reporter.SpanConfigConformance()`.
    
    This commit leverages #90016 to provide multi-tenant friendly
    observability of nodes whose replicas are unavailable or underreplicated,
    and are therefore considered critical nodes.
    
    A new `CriticalNodes` rpc is available via HTTP. The response
    includes a list of node descriptors that are considered critical, and the
    corresponding SpanConfigConformanceReport which includes details of
    non-conforming ranges contributing to the criticality.
    
    Epic: https://cockroachlabs.atlassian.net/browse/CRDB-10630
    Release note: None

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -900,6 +900,7 @@ func NewServer(cfg Config, stopper *stop.Stopper) (*Server, error) {
 		clock,
 		distSender,
 		rangestats.NewFetcher(db),
+		node,
 	)
 
 	keyVisualizerServer := &KeyVisualizerServer{
