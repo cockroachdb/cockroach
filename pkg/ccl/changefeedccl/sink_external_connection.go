9a049c0efdc9f02729e22bfd6e14c0bb482cee46
Jane Xing <zhouxing@uchicago.edu>
2023-02-08 17:43:16 -0500

    cloud,ccl,*: break sql -> cloud/externalconn/utils dependency
    This commit is to break `cloud/externalconn/utils/`'s dependency of the `sql`
    package, as `sql` is just used to get the `sql.ExecCfg` for the validation
    function for external connection uri. But in fact, only a few fields of `sql.ExecCfg`
    is used, hence it makes more sense to just use a lighter
    `exeternalconn.ExternalConnEnv`.
    
    Release note: None

--- pkg/ccl/changefeedccl/sink_external_connection.go
+++ pkg/ccl/changefeedccl/sink_external_connection.go
@@ -17,7 +17,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/cloud/externalconn/connectionpb"
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
 	"github.com/cockroachdb/cockroach/pkg/security/username"
-	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/execinfra"
 	"github.com/cockroachdb/cockroach/pkg/sql/isql"
 	"github.com/cockroachdb/errors"
@@ -66,30 +65,32 @@ func makeExternalConnectionSink(
 }
 
 func validateExternalConnectionSinkURI(
-	ctx context.Context, execCfg interface{}, user username.SQLUsername, uri string,
+	ctx context.Context, env externalconn.ExternalConnEnv, uri string,
 ) error {
 
-	serverCfg := execinfra.ServerConfig{ExternalStorageFromURI: func(ctx context.Context, uri string,
+	serverCfg := &execinfra.ServerConfig{ExternalStorageFromURI: func(ctx context.Context, uri string,
 		user username.SQLUsername, opts ...cloud.ExternalStorageOption) (cloud.ExternalStorage, error) {
 		return nil, nil
 	}}
 
 	// Pass through the server config, except for the WrapSink testing knob since that often assumes it's
 	// inside a job.
-	if actualExecCfg, ok := execCfg.(*sql.ExecutorConfig); ok {
-		serverCfg = actualExecCfg.DistSQLSrv.ServerConfig
-		if knobs, ok := serverCfg.TestingKnobs.Changefeed.(*TestingKnobs); ok && knobs.WrapSink != nil {
-			wrapSink := knobs.WrapSink
-			knobs.WrapSink = nil
-			defer func() { knobs.WrapSink = wrapSink }()
-		}
+
+	if s, ok := env.ServerCfg.(*execinfra.ServerConfig); ok && s != nil {
+		serverCfg = s
+	}
+
+	if knobs, ok := serverCfg.TestingKnobs.Changefeed.(*TestingKnobs); ok && knobs.WrapSink != nil {
+		wrapSink := knobs.WrapSink
+		knobs.WrapSink = nil
+		defer func() { knobs.WrapSink = wrapSink }()
 	}
 
 	// Validate the URI by creating a canary sink.
 	//
 	// TODO(adityamaru): When we add `CREATE EXTERNAL CONNECTION ... WITH` support
 	// to accept JSONConfig we should validate that here too.
-	_, err := getSink(ctx, &serverCfg, jobspb.ChangefeedDetails{SinkURI: uri}, nil, user,
+	_, err := getSink(ctx, serverCfg, jobspb.ChangefeedDetails{SinkURI: uri}, nil, env.Username,
 		jobspb.JobID(0), nil)
 	if err != nil {
 		return errors.Wrap(err, "invalid changefeed sink URI")
