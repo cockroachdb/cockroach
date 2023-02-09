51648a82456f1fc8032762bf4cbde8a1cd1222aa
David Hartunian <davidh@cockroachlabs.com>
2023-02-09 11:11:33 -0500

    servercontroller: serve http from default tenant on error
    Previously, when an HTTP request arrived with a tenant cookie attached, we
    would attempt to connect to that tenant and fail if the tenant couldn't be
    started or if it didn't exist. This causes a bad user-facing experience when
    someone's browser contains stale tenant cookies and they attempt to load DB
    Console. The error returns a 500 to the browser and user sees no UI, can't try
    and login again, and is basically stuck until they clear their cookies.
    
    This change falls back to serving HTTP from the default tenant when the
    requested one can't be reached. This ensures that *something* is served to
    ensure that unauthenticated endpoints can still be loaded in these scenarios
    and users can attempt to login again.
    
    Epic: CRDB-12100
    
    Release note: None

--- pkg/server/server_controller_http.go
+++ pkg/server/server_controller_http.go
@@ -64,7 +64,7 @@ func (c *serverController) httpMux(w http.ResponseWriter, r *http.Request) {
 	s, err := c.getServer(ctx, tenantName)
 	if err != nil {
 		log.Warningf(ctx, "unable to find server for tenant %q: %v", tenantName, err)
-		// Clear session and tenant cookies after all logouts have completed.
+		// Clear session and tenant cookies since it appears they reference invalid state.
 		http.SetCookie(w, &http.Cookie{
 			Name:     MultitenantSessionCookieName,
 			Value:    "",
@@ -79,7 +79,19 @@ func (c *serverController) httpMux(w http.ResponseWriter, r *http.Request) {
 			HttpOnly: false,
 			Expires:  timeutil.Unix(0, 0),
 		})
-		w.WriteHeader(http.StatusInternalServerError)
+		// Fall back to serving requests from the default tenant. This helps us serve
+		// the root path along with static assets even when the browser contains invalid
+		// tenant names or sessions (common during development). Otherwise the user can
+		// get into a state where they cannot load DB Console assets at all due to invalid
+		// cookies.
+		defaultTenantName := roachpb.TenantName(defaultTenantSelect.Get(&c.st.SV))
+		s, err = c.getServer(ctx, defaultTenantName)
+		if err != nil {
+			log.Warningf(ctx, "unable to find server for default tenant %q: %v", defaultTenantName, err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		s.getHTTPHandlerFn()(w, r)
 		return
 	}
 	s.getHTTPHandlerFn()(w, r)
