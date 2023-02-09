5667f318a68471554464b51c1fe1d054e94e808f
Rafi Shamim <rafi@cockroachlabs.com>
2023-02-09 18:20:06 +0000

    cacheutil: fix computation of entry size
    The previous way would always result in a fixed size for each entry,
    since it would just compute the size of a pointer. Now, the size of the
    entry must be specified when writing to the cache.
    
    Release note: None

--- pkg/sql/cacheutil/cache.go
+++ pkg/sql/cacheutil/cache.go
@@ -12,7 +12,6 @@ package cacheutil
 
 import (
 	"context"
-	"unsafe"
 
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
@@ -85,12 +84,16 @@ func (c *Cache) LoadValueOutsideOfCacheSingleFlight(
 // MaybeWriteBackToCache tries to put the key, value into the
 // cache, and returns true if it succeeded. If the underlying system
 // tables have been modified since they were read, the cache is not
-// updated.
+// updated. The entrySize should be the size in bytes of the key and value.
 // Note that reading from system tables may give us data from a newer table
 // version than the one we pass in here, that is okay since the cache will
 // be invalidated upon the next read.
 func (c *Cache) MaybeWriteBackToCache(
-	ctx context.Context, tableVersions []descpb.DescriptorVersion, key interface{}, entry interface{},
+	ctx context.Context,
+	tableVersions []descpb.DescriptorVersion,
+	key interface{},
+	value interface{},
+	entrySize int64,
 ) bool {
 	c.Lock()
 	defer c.Unlock()
@@ -104,14 +107,13 @@ func (c *Cache) MaybeWriteBackToCache(
 		}
 	}
 	// Table version remains the same: update map, unlock, return.
-	const sizeOfEntry = int(unsafe.Sizeof(entry))
-	if err := c.boundAccount.Grow(ctx, int64(sizeOfEntry)); err != nil {
+	if err := c.boundAccount.Grow(ctx, entrySize); err != nil {
 		// If there is no memory available to cache the entry, we can still
 		// proceed with authentication so that users are not locked out of
 		// the database.
 		log.Ops.Warningf(ctx, "no memory available to cache info: %v", err)
 	} else {
-		c.cache[key] = entry
+		c.cache[key] = value
 	}
 	return true
 }
