f050e58e8d471c3ab5a10a6a46e79ca766a584de
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-02-13 14:56:18 -0800

    cli: fix statement-bundle recreate for table names with quotes
    If a table name uses a reserved keyword, then it's name must be enclosed
    in double quotes. Previously, for such tables `debug statement-bundle recreate`
    command would fail since it wouldn't match the regex, and now it is
    fixed.
    
    Release note: None

---
+++ pkg/cli/testdata/explain-bundle/bundle/stats-defaultdb.public."order".sql
@@ -0,0 +1,13 @@
+ALTER TABLE public."order" INJECT STATISTICS '[
+    {
+        "columns": [
+            "id"
+        ],
+        "created_at": "2021-06-23 21:17:16.83267",
+        "distinct_count": 0,
+        "histo_col_type": "INT8",
+        "name": "__auto__",
+        "null_count": 0,
+        "row_count": 0
+    }
+]';
