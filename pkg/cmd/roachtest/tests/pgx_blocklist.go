351da21e78ac79187eeab53f5a0d76b4c1067825
Jane Xing <zhouxing@uchicago.edu>
2023-02-13 10:30:26 -0500

    roachtest: fix blocklist
    Update the blockList and ignoreList for roachtests so that there's only one for
    each version.
    
    fixes #96935
    
    Release note: None

--- pkg/cmd/roachtest/tests/pgx_blocklist.go
+++ pkg/cmd/roachtest/tests/pgx_blocklist.go
@@ -10,41 +10,12 @@
 
 package tests
 
-var pgxBlocklists = blocklistsForVersion{
-	{"v20.2", "pgxBlocklist20_2", pgxBlocklist20_2, "pgxIgnorelist20_2", pgxIgnorelist20_2},
-	{"v21.1", "pgxBlocklist21_1", pgxBlocklist21_1, "pgxIgnorelist21_1", pgxIgnorelist21_1},
-	{"v21.2", "pgxBlocklist21_2", pgxBlocklist21_2, "pgxIgnorelist21_2", pgxIgnorelist21_2},
-	{"v22.1", "pgxBlocklist22_1", pgxBlocklist22_1, "pgxIgnorelist22_1", pgxIgnorelist22_1},
-	{"v22.2", "pgxBlocklist22_2", pgxBlocklist22_2, "pgxIgnorelist22_2", pgxIgnorelist22_2},
-	{"v23.1", "pgxBlocklist23_1", pgxBlocklist23_1, "pgxIgnorelist23_1", pgxIgnorelist23_1},
-}
-
 // Please keep these lists alphabetized for easy diffing.
 // After a failed run, an updated version of this blocklist should be available
 // in the test log.
-var pgxBlocklist23_1 = blocklist{}
+var pgxBlocklist = blocklist{}
 
-var pgxBlocklist22_2 = blocklist{}
-
-var pgxBlocklist22_1 = blocklist{}
-
-var pgxBlocklist21_2 = blocklist{}
-
-var pgxBlocklist21_1 = blocklist{}
-
-var pgxBlocklist20_2 = blocklist{}
-
-var pgxIgnorelist23_1 = pgxIgnorelist22_2
-
-var pgxIgnorelist22_2 = pgxIgnorelist22_1
-
-var pgxIgnorelist22_1 = pgxIgnorelist21_2
-
-var pgxIgnorelist21_2 = pgxIgnorelist21_1
-
-var pgxIgnorelist21_1 = pgxIgnorelist20_2
-
-var pgxIgnorelist20_2 = blocklist{
+var pgxIgnorelist = blocklist{
 	"v4.TestBeginIsoLevels": "We don't support isolation levels",
 	"v4.TestConnCopyFromFailServerSideMidwayAbortsWithoutWaiting": "https://github.com/cockroachdb/cockroach/issues/69291#issuecomment-906898940",
 	"v4.TestQueryEncodeError":                                     "This test checks the exact error message",
