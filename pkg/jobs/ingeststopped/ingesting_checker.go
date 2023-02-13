0dde9e6808b0012c7120c0e2faeb7de802e03257
David Taylor <tinystatemachine@gmail.com>
2023-02-13 20:59:30 +0000

    jobs/ingeststopped: add helper to check all nodes for ingest activity
    This adds a new helper utility that can be used to check if a given job
    has indicated that it is currently performing ingestion work on any node
    using the job Registry's MarkAsIngesting API.
    
    This is done by running a distSQL flow on all nodes that invokes a new
    processor which simply reads this registry API, returning an error if
    the given job ID is ingesting and no error if it is not.
    
    This utility is intended for use when a job needs to positively verify
    that ingestion activity for a given job -- typically itself in a previous
    phase or retry -- has ceases before moving on to a new phase or retry.
    
    Typically, a job only moves on to a new phase after the flow running its
    ingestion activity returns after every ingesting processor exits. However
    in some rare edge cases, it is possible for this not to be the case: a
    very slow, delayed ingestion could still be flushing after the coordinator
    for the job terminated and the job was adopted on a different node. In
    such a case, that new execution of the job, potentially now in a reverting
    phase, could race with this delayed ingestion. In the case of a revert,
    such a race could lead to incomplete cleanup by the revert, as data may
    be added by the delayed ingestion after the reverting phase processes a
    given span.
    
    Integration of this new utility into jobs that require it is left to a
    future change.
    
    Release note: none.
    Epic: none.

---
+++ pkg/jobs/ingeststopped/ingesting_checker.go
@@ -0,0 +1,118 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package ingeststopped
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/jobs"
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/sql"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/execinfrapb"
+	"github.com/cockroachdb/cockroach/pkg/sql/physicalplan"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/types"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/retry"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/cockroachdb/cockroach/pkg/util/tracing"
+)
+
+// WaitForNoIngestingNodes contacts all nodes in the cluster and verifies that
+// they are not ingesting data for the given job ID, and continues to try to do
+// so for the specified duration until it succeeds or times out.
+func WaitForNoIngestingNodes(
+	ctx context.Context, execCtx sql.JobExecContext, job *jobs.Job, maxWait time.Duration,
+) error {
+	ctx, sp := tracing.ChildSpan(ctx, "WaitForNoIngestingNodes")
+	defer sp.Finish()
+
+	retries := retry.StartWithCtx(ctx, retry.Options{
+		InitialBackoff: time.Second,
+		MaxBackoff:     time.Second * 10,
+	})
+
+	started := timeutil.Now()
+
+	var lastStatusUpdate time.Time
+	const statusUpdateFrequency = time.Second * 30
+
+	for retries.Next() {
+		err := checkAllNodesForIngestingJob(ctx, execCtx, job.ID())
+		if err == nil {
+			break
+		}
+		log.Infof(ctx, "failed to verify job no longer importing on all nodes: %+v", err)
+
+		if timeutil.Since(started) > maxWait {
+			return err
+		}
+
+		if timeutil.Since(lastStatusUpdate) > statusUpdateFrequency {
+			if statusErr := job.NoTxn().RunningStatus(ctx, func(_ context.Context, _ jobspb.Details) (jobs.RunningStatus, error) {
+				return jobs.RunningStatus(fmt.Sprintf("waiting for all nodes to finish ingesting writing before proceeding: %s", err)), nil
+			}); statusErr != nil {
+				log.Warningf(ctx, "failed to update running status of job %d: %s", job.ID(), statusErr)
+			} else {
+				lastStatusUpdate = timeutil.Now()
+			}
+		}
+	}
+	return nil
+}
+
+func checkAllNodesForIngestingJob(
+	ctx context.Context, execCtx sql.JobExecContext, jobID catpb.JobID,
+) error {
+	dsp := execCtx.DistSQLPlanner()
+	evalCtx := execCtx.ExtendedEvalContext()
+
+	// TODO(dt): We should record which nodes were assigned ingestion processors
+	// and then ensure we're reaching out to them specifically here, in particular
+	// in the event a node that was importing is no longer in liveness but might
+	// still be off ingesting.
+	planCtx, sqlInstanceIDs, err := dsp.SetupAllNodesPlanning(ctx, evalCtx, execCtx.ExecCfg())
+	if err != nil {
+		return err
+	}
+
+	p := planCtx.NewPhysicalPlan()
+	corePlacement := make([]physicalplan.ProcessorCorePlacement, len(sqlInstanceIDs))
+	for i := range sqlInstanceIDs {
+		corePlacement[i].SQLInstanceID = sqlInstanceIDs[i]
+		corePlacement[i].Core.IngestStopped = &execinfrapb.IngestStoppedSpec{JobID: jobID}
+	}
+
+	p.AddNoInputStage(
+		corePlacement, execinfrapb.PostProcessSpec{}, []*types.T{}, execinfrapb.Ordering{},
+	)
+	sql.FinalizePlan(ctx, planCtx, p)
+
+	res := sql.NewMetadataOnlyMetadataCallbackWriter()
+
+	recv := sql.MakeDistSQLReceiver(
+		ctx,
+		res,
+		tree.Ack,
+		nil, /* rangeCache */
+		nil, /* txn - the flow does not read or write the database */
+		nil, /* clockUpdater */
+		evalCtx.Tracing,
+	)
+	defer recv.Release()
+
+	evalCtxCopy := *evalCtx
+	dsp.Run(ctx, planCtx, nil, p, recv, &evalCtxCopy, nil /* finishedSetupFn */)
+	return res.Err()
+}
