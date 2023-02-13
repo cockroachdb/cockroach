2674049e9c020782d19d89d39af03d1aed1e15af
Faizan Qazi <faizan@cockroachlabs.com>
2023-02-13 14:38:29 -0500

    cli: add debug command for declarative schema changer rules
    Previously, we had no way of dumping out the supported
    declarative schema changer rules on a given cockroach
    binary. This was inadequate because to support mixed version
    states we need to maintain the current and previous set of
    rules, but have no way of asserting the two are equivalent.
    This patch adds a debug command that will enable us to
    add this type of comparison using a future roachtest.
    
    Epic: none
    Release note: None

---
+++ pkg/cli/declarative_print_rules_test.go
@@ -0,0 +1,48 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package cli
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
+	"github.com/cockroachdb/cockroach/pkg/testutils/datapathutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/datadriven"
+)
+
+// This test doctoring a secure cluster.
+func TestDeclarativeRules(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	c := NewCLITest(TestCLIParams{T: t, NoServer: true})
+	defer c.Cleanup()
+
+	t.Run("declarative corpus validation standalone command", func(t *testing.T) {
+		version := clusterversion.TestingBinaryVersion
+		opOut, err := c.RunWithCapture(fmt.Sprintf("debug declarative-print-rules %s op", version.String()))
+		if err != nil {
+			t.Fatal(err)
+		}
+		depOut, err := c.RunWithCapture(fmt.Sprintf("debug declarative-print-rules %s dep", version.String()))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Using datadriven allows TESTFLAGS=-rewrite.
+		datadriven.RunTest(t, datapathutils.TestDataPath(t, "declarative-rules", "oprules"), func(t *testing.T, td *datadriven.TestData) string {
+			return opOut
+		})
+		datadriven.RunTest(t, datapathutils.TestDataPath(t, "declarative-rules", "deprules"), func(t *testing.T, td *datadriven.TestData) string {
+			return depOut
+		})
+	})
+}
