3749f3208ac91c910554f8298d09cbf4cb54efc9
Ricky Stewart <rickybstewart@gmail.com>
2023-02-13 16:06:25 -0600

    patched-go: sign for macOS
    Add two new TC jobs, "Sign patched Go for Mac" and "Publish Patched Go
    for Mac". The first grabs the Mac tarball that is cross-built and signs
    and notarizes it. The second grabs the new *signed* tarball and
    publishes it.
    
    Epic: none
    Release note: None

---
+++ build/teamcity/internal/release/sign-patched-go.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+KEYCHAIN_NAME=signing
+KEYCHAIN_PROFILE=notarization
+curr_dir=$(pwd)
+
+cleanup() {
+    security lock-keychain "${KEYCHAIN_NAME}"
+    rm -rf darwin.zip staging darwin-amd64 darwin-arm64 *.tar.gz TIMESTAMP.txt
+}
+trap cleanup EXIT
+
+mkdir artifacts
+mv TIMESTAMP.txt artifacts/TIMESTAMP.txt
+security unlock-keychain -p "${KEYCHAIN_PASSWORD}" "${KEYCHAIN_NAME}"
+
+sign() {
+    archive=$(find . -name go*.darwin-$1.tar.gz -d 1 | head -n1 | xargs basename)
+    mkdir darwin-$1
+    tar -xf $archive -C darwin-$1
+    rm $archive
+    for bin in go gofmt; do
+        codesign --timestamp --options=runtime -f --keychain "$KEYCHAIN_NAME" -s "$SIGNING_IDENTITY" darwin-$1/go/bin/$bin
+    done
+    tar cf - -C darwin-$1 go | gzip -9 > artifacts/$archive
+    mkdir staging
+    cp darwin-$1/go/bin/gofmt staging
+    cp darwin-$1/go/bin/go    staging
+    zip -r darwin.zip staging
+    rm -rf staging
+    xcrun notarytool submit darwin.zip --wait \
+          --team-id "$TEAM_ID" --keychain-profile "$KEYCHAIN_PROFILE" \
+          --apple-id "$APPLE_ID" --verbose \
+          --keychain "${HOME}/Library/Keychains/${KEYCHAIN_NAME}-db"
+}
+
+sign amd64
+sign arm64
