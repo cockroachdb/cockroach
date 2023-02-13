b034125fa3be806eb902b86449f049fe236a50e9
David Taylor <tinystatemachine@gmail.com>
2023-02-13 21:01:26 +0000

    sql/importer: wait for any pending ingestion to complete before rolling back
    Release note (bug fix): Fix a potential bug whereby a failed or
    cancelled IMPORT could in some cases leave some of the imported rows
    behind after it was cancelled, in the rare event that the writing
    processes were slow enough to continue writing after the cleanup process
    started. Epic: TODO.
    
    Fixes: TODO.

--- pkg/sql/importer/import_job.go
+++ pkg/sql/importer/import_job.go
@@ -19,6 +19,7 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/clusterversion"
 	"github.com/cockroachdb/cockroach/pkg/jobs"
+	"github.com/cockroachdb/cockroach/pkg/jobs/ingeststopped"
 	"github.com/cockroachdb/cockroach/pkg/jobs/joberror"
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
 	"github.com/cockroachdb/cockroach/pkg/kv"
@@ -57,6 +58,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/cockroach/pkg/util/tracing"
 	"github.com/cockroachdb/errors"
+	"github.com/cockroachdb/redact"
 )
 
 const importJobRecoveryEventType eventpb.RecoveryEventType = "import_job"
@@ -1408,6 +1410,20 @@ func (r *importResumer) OnFailOrCancel(
 	logutil.LogJobCompletion(ctx, importJobRecoveryEventType, r.job.ID(), false, jobErr, r.res.Rows)
 
 	addToFileFormatTelemetry(details.Format.Format.String(), "failed")
+
+	// If the import completed preparation and started writing, verify it has
+	// stopped writing before proceeding to revert it.
+	if details.PrepareComplete {
+		log.Infof(ctx, "need to verify that no nodes are still importing since job had started writing...")
+		const maxWait = time.Minute * 5
+		if err := ingeststopped.WaitForNoIngestingNodes(ctx, p, r.job, maxWait); err != nil {
+			log.Errorf(ctx, "unable to verify that attempted IMPORT had stopped writing before reverting after %s: %v", maxWait, err)
+		} else {
+			log.Infof(ctx, "verified no nodes still ingesting on behalf of job %d", redact.Safe(r.job.ID()))
+		}
+
+	}
+
 	cfg := execCtx.(sql.JobExecContext).ExecCfg()
 	var jobsToRunAfterTxnCommit []jobspb.JobID
 	if err := sql.DescsTxn(ctx, cfg, func(
