845ac51a73cd62ded24da92cfd2550e86efd587a
adityamaru <adityamaru@gmail.com>
2023-02-13 23:50:27 -0500

    jobs: introduces an InfoStorage wrapper
    This change introduces an InfoStorage wrapper that
    is initialized in the context of a Job and optionally a txn.
    This wrapper can be used to read/write rows in
    the system.job_info table that correspond to the
    Job.
    
    Additionally, the write path of InfoStorage checks that
    the caller updating the row is part of the currently running
    instance of the job. We do this by checking that the job's sqlliveness
    session is the same as the one persisted in the job record.
    
    Release note: None

---
+++ pkg/jobs/job_info_storage_test.go
@@ -0,0 +1,233 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package jobs_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/jobs"
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/keyvisualizer"
+	"github.com/cockroachdb/cockroach/pkg/security/username"
+	"github.com/cockroachdb/cockroach/pkg/sql/isql"
+	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/upgrade/upgradebase"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJobInfoAccessors(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	s, _, _ := serverutils.StartServer(t, base.TestServerArgs{})
+	ctx := context.Background()
+	defer s.Stopper().Stop(ctx)
+
+	idb := s.InternalDB().(isql.DB)
+	r := s.JobRegistry().(*jobs.Registry)
+
+	createJob := func(id jobspb.JobID) *jobs.Job {
+		defaultRecord := jobs.Record{
+			// Job does not accept an empty Details field, so arbitrarily provide
+			// ImportDetails.
+			Details:  jobspb.BackupDetails{},
+			Progress: jobspb.BackupProgress{},
+			Username: username.TestUserName(),
+		}
+
+		job, err := r.CreateJobWithTxn(ctx, defaultRecord, id, nil /* txn */)
+		require.NoError(t, err)
+		return job
+	}
+
+	job1 := createJob(1)
+	job2 := createJob(2)
+	job3 := createJob(3)
+	kPrefix, kA, kB, kC := []byte("🔑"), []byte("🔑A"), []byte("🔑B"), []byte("🔑C")
+	v1, v2 := []byte("val1"), []byte("val2")
+
+	// Key doesn't exist yet.
+	getJobInfo := func(j *jobs.Job, key []byte) (v []byte, ok bool, err error) {
+		err = idb.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+			infoStorage := j.InfoStorageWithTxn(txn)
+			v, ok, err = infoStorage.Get(ctx, key)
+			return err
+		})
+		return v, ok, err
+	}
+	_, ok, err := getJobInfo(job1, kA)
+	require.NoError(t, err)
+	require.False(t, ok)
+
+	// Write kA = v1.
+	require.NoError(t, idb.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+		infoStorage := job1.InfoStorageWithTxn(txn)
+		return infoStorage.Write(ctx, kA, v1)
+	}))
+
+	// Check that key is now found with value v1.
+	v, ok, err := getJobInfo(job1, kA)
+	require.NoError(t, err)
+	require.True(t, ok)
+	require.Equal(t, v1, v)
+
+	// Overwrite kA = v2.
+	require.NoError(t, idb.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+		infoStorage := job1.InfoStorageWithTxn(txn)
+		return infoStorage.Write(ctx, kA, v2)
+	}))
+
+	// Check that key is now v1.
+	v, ok, err = getJobInfo(job1, kA)
+	require.NoError(t, err)
+	require.True(t, ok)
+	require.Equal(t, v2, v)
+
+	// Verify a different is not found.
+	_, ok, err = getJobInfo(job1, kB)
+	require.NoError(t, err)
+	require.False(t, ok)
+
+	// Verify that the same key for a different job is not found.
+	_, ok, err = getJobInfo(job2, kB)
+	require.NoError(t, err)
+	require.False(t, ok)
+
+	// Write and revise some info keys a, b and c (out of order, just for fun).
+	require.NoError(t, idb.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+		infoStorage := job2.InfoStorageWithTxn(txn)
+		return infoStorage.Write(ctx, kB, v2)
+	}))
+	require.NoError(t, idb.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+		infoStorage := job2.InfoStorageWithTxn(txn)
+		return infoStorage.Write(ctx, kA, v1)
+	}))
+	require.NoError(t, idb.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+		infoStorage := job2.InfoStorageWithTxn(txn)
+		return infoStorage.Write(ctx, kC, v2)
+	}))
+	require.NoError(t, idb.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+		infoStorage := job2.InfoStorageWithTxn(txn)
+		return infoStorage.Write(ctx, kA, v2)
+	}))
+
+	// Iterate the common prefix of a, b and c.
+	var i int
+	require.NoError(t, idb.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+		infoStorage := job2.InfoStorageWithTxn(txn)
+		return infoStorage.Iterate(ctx, kPrefix, func(key, value []byte) error {
+			i++
+			switch i {
+			case 1:
+				require.Equal(t, key, kA)
+			case 2:
+				require.Equal(t, key, kB)
+			case 3:
+				require.Equal(t, key, kC)
+			}
+			require.Equal(t, v2, value)
+			return nil
+		})
+	}))
+	require.Equal(t, 3, i)
+
+	// Iterate the specific prefix of just a.
+	found := false
+	require.NoError(t, idb.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+		infoStorage := job2.InfoStorageWithTxn(txn)
+		return infoStorage.Iterate(ctx, kA, func(key, value []byte) error {
+			require.Equal(t, kA, key)
+			require.Equal(t, v2, value)
+			found = true
+			return nil
+		})
+	}))
+	require.True(t, found)
+
+	// Iterate a different job.
+	require.NoError(t, idb.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+		infoStorage := job3.InfoStorageWithTxn(txn)
+		return infoStorage.Iterate(ctx, kPrefix, func(key, value []byte) error {
+			t.Fatalf("unexpected record for job 3: %v = %v", key, value)
+			return nil
+		})
+	}))
+}
+
+func TestAccessorsWithWrongSQLLivenessSession(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	args := base.TestServerArgs{
+		Knobs: base.TestingKnobs{
+			// Avoiding jobs to be adopted.
+			JobsTestingKnobs: &jobs.TestingKnobs{
+				DisableAdoptions: true,
+			},
+			// DisableAdoptions needs this.
+			UpgradeManager: &upgradebase.TestingKnobs{
+				DontUseJobs:                       true,
+				SkipJobMetricsPollingJobBootstrap: true,
+			},
+			KeyVisualizer: &keyvisualizer.TestingKnobs{
+				SkipJobBootstrap: true,
+			},
+		},
+		DisableSpanConfigs: true,
+	}
+
+	ctx := context.Background()
+	s, _, _ := serverutils.StartServer(t, args)
+	defer s.Stopper().Stop(ctx)
+	ief := s.InternalDB().(isql.DB)
+
+	registry := s.JobRegistry().(*jobs.Registry)
+
+	defaultRecord := jobs.Record{
+		// Job does not accept an empty Details field, so arbitrarily provide
+		// ImportDetails.
+		Details:  jobspb.ImportDetails{},
+		Progress: jobspb.ImportProgress{},
+		Username: username.TestUserName(),
+	}
+
+	job, err := registry.CreateJobWithTxn(ctx, defaultRecord, registry.MakeJobID(), nil /* txn */)
+	require.NoError(t, err)
+	infoStorage := job.InfoStorageNoTxn()
+	require.NoError(t, infoStorage.Write(ctx, []byte("foo"), []byte("baz")))
+
+	// Change job's session id and check that writes are rejected.
+	updateClaimStmt := `UPDATE system.jobs SET claim_session_id = $1 WHERE id = $2`
+	require.NoError(t, ief.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+		_, err := txn.ExecEx(ctx, "update-claim", txn.KV(), sessiondata.NodeUserSessionDataOverride,
+			updateClaimStmt, "!@#!@$!$@#", job.ID())
+		return err
+	}))
+	require.True(t, testutils.IsError(infoStorage.Write(ctx, []byte("foo"), []byte("bar")),
+		"expected session.*but found"))
+
+	// A Get should still succeed even with an invalid session id.
+	val, exists, err := infoStorage.Get(ctx, []byte("foo"))
+	require.NoError(t, err)
+	require.True(t, exists)
+	require.Equal(t, val, []byte("baz"))
+
+	// Iterate should still succeed even with an invalid session id.
+	require.NoError(t, infoStorage.Iterate(ctx, []byte("foo"), func(infoKey, value []byte) error {
+		require.Equal(t, value, []byte("baz"))
+		return nil
+	}))
+}
