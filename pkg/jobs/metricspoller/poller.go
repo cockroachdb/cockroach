a788ed45a9e70472734989f2aec592629084f861
Yevgeniy Miretskiy <yevgeniy@cockroachlabs.com>
2023-02-14 10:27:29 -0500

    jobs: Extend job metric poller to collect PTS stats
    Prior PR https://github.com/cockroachdb/cockroach/pull/89752 added
    a metrics poller job which produces per job type stats on the
    number of paused jobs.
    
    This PR extends metrics poller to also collect stats related
    to protected timestamps created by jobs.
    Namely, two new metrics, per job type are added:
    
    * `jobs.<job type>.protected_record_count` -- keeps track of the number
      of protected timestamp records help by the jobs.
    * `jobs.<job type>.protected_age_sec` -- keeps track of the age
      of the oldest protected timestamp held by those jobs.
    
    The metrics improve observability into protected timestamp system,
    and allow operators to alert when protected timestamp records are
    too old since that prevents garbage collection from occuring
    (and if GC is not performed for too long, the cluster performance
    would degrade).
    
    Follow on work will also make this functionality available for
    schedules.
    
    Epic: CRDB-21953
    Fixes #78354
    
    Release note (enterprise change): Jobs that utilize protected timestamp
    system (BACKUP, CHANGEFEED, IMPORT, etc) now produce metrics that
    can be monitored to detect cases when job leaves stale protected
    timestamp, preventing garbage collection from occuring.

---
+++ pkg/jobs/metricspoller/poller.go
@@ -0,0 +1,110 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package metricspoller
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/jobs"
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/sql"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/metric"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/cockroachdb/logtags"
+	io_prometheus_client "github.com/prometheus/client_model/go"
+)
+
+// metricsPoller is a singleton job whose purpose is to poll various metrics
+// periodically.  These metrics are meant to be cluster wide metrics -- for
+// example, number of jobs currently paused in the cluster.  While such metrics
+// could be implemented locally by each node, doing so would result in the
+// metric being inflated by the number of nodes.  That's not ideal, and that's
+// what the purpose of this job is: namely, to provide a convenient way to query
+// various aspects of cluster state, and make that state available via correctly
+// counted metrics.
+
+type metricsPoller struct {
+	job *jobs.Job
+}
+
+var _ jobs.Resumer = &metricsPoller{}
+
+// OnFailOrCancel is a part of the Resumer interface.
+func (mp *metricsPoller) OnFailOrCancel(
+	ctx context.Context, execCtx interface{}, jobErr error,
+) error {
+	return nil
+}
+
+// Resume is part of the Resumer interface.
+func (mp *metricsPoller) Resume(ctx context.Context, execCtx interface{}) error {
+	// The metrics polling job is a forever running background job. It's always
+	// safe to wind the SQL pod down whenever it's running, something we
+	// indicate through the job's idle status.
+	mp.job.MarkIdle(true)
+
+	exec := execCtx.(sql.JobExecContext)
+	metrics := exec.ExecCfg().JobRegistry.MetricsStruct().JobSpecificMetrics[jobspb.TypePollJobsStats].(pollerMetrics)
+
+	t := timeutil.NewTimer()
+	defer t.Stop()
+
+	runTask := func(name string, task func(ctx context.Context, execCtx sql.JobExecContext) error) error {
+		ctx = logtags.AddTag(ctx, "task", name)
+		return task(ctx, exec)
+	}
+
+	for {
+		t.Reset(jobs.PollJobsMetricsInterval.Get(&exec.ExecCfg().Settings.SV))
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-t.C:
+			t.Read = true
+			if err := runTask("paused-jobs", updatePausedMetrics); err != nil {
+				log.Errorf(ctx, "Periodic stats collector task paused-jobs completed with error %s", err)
+				metrics.numErrors.Inc(1)
+			}
+			if err := runTask("pts-stats", updatePTSStats); err != nil {
+				log.Errorf(ctx, "Periodic stats collector task pts-stats completed with error %s", err)
+				metrics.numErrors.Inc(1)
+			}
+		}
+	}
+}
+
+type pollerMetrics struct {
+	numErrors *metric.Counter
+}
+
+func (m pollerMetrics) MetricStruct() {}
+
+func newPollerMetrics() metric.Struct {
+	return pollerMetrics{
+		numErrors: metric.NewCounter(metric.Metadata{
+			Name:        "jobs.metrics.task_failed",
+			Help:        "Number of metrics poller tasks that failed",
+			Measurement: "errors",
+			Unit:        metric.Unit_COUNT,
+			MetricType:  io_prometheus_client.MetricType_COUNTER,
+		}),
+	}
+}
+
+func init() {
+	createResumerFn := func(job *jobs.Job, settings *cluster.Settings) jobs.Resumer {
+		return &metricsPoller{job: job}
+	}
+	jobs.RegisterConstructor(jobspb.TypePollJobsStats, createResumerFn,
+		jobs.DisablesTenantCostControl, jobs.WithJobMetrics(newPollerMetrics()))
+}
