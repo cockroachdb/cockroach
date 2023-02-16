ff4eb5f3769bc94b33dcd9dd1063902981f80bcd
Tobias Grieger <tobias.b.grieger@gmail.com>
2023-02-16 10:09:39 +0100

    try in blasts

--- experiment.sh
+++ experiment.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -euxo pipefail
 ./dev build short
+
 roachprod wipe local
 roachprod put local cockroach
 roachprod start local
 roachprod sql local:1 -- -e 'set cluster setting kv.range_split.load_qps_threshold = 9999999'
 roachprod ssh local:1 -- ./cockroach workload init kv --splits 1
-roachprod ssh local:1 -- ./cockroach workload run kv --min-block-bytes 1024 --max-block-bytes 1024 --read-percent 0 {pgurl} --concurrency=4096 --tolerate-errors
+
+while true; do
+
+roachprod ssh local:1 -- ./cockroach workload run kv --min-block-bytes 1024 --max-block-bytes 1024 --read-percent 0 {pgurl} --concurrency=4096 --tolerate-errors --duration 30s
+roachprod monitor local --oneshot
+
+done
+
+
+
