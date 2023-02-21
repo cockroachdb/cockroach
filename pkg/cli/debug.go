c7afe68e8087a036f9cabe691a4c67af4881c9c2
Tobias Grieger <tobias.b.grieger@gmail.com>
2023-02-21 21:41:06 +0100

    cli: support OpenMetrics in tsdump
    Touches https://github.com/cockroachdb/cockroach/issues/97120.
    
        $ cockroach debug tsdump --insecure --format=openmetrics > tsdump.om
    
        $ tail -n 3 tsdump.om
        valcount{store="1"} 9841.000000 1677012190.0
        valcount{store="1"} 9847.000000 1677012200.0
        # EOF
    
        $ docker run -v $PWD/tsdump.om:/tsdump.om -v $PWD/data:/data --entrypoint /bin/promtool prom/prometheus tsdb create-blocks-from openmetrics /tsdump.om /data
        BLOCK ULID                  MIN TIME       MAX TIME       DURATION     NUM SAMPLES  NUM CHUNKS   NUM SERIES   SIZE
        01GSTVJG4QMBSQ4ACPK80FFDJ9  1676544540000  1676544940001  6m40.001s    10598        1514         1514         261097
        01GSTVJGWM2CX1AA56QSSZ9T2R  1677009150000  1677009590001  7m20.001s    68130        1514         1514         305952
        01GSTVJHT2TWTZGCPE52AE1T95  1677009600000  1677012200001  43m20.001s   394093       4542         1514         692053
    
        $ ls data
        01GSTVJG4QMBSQ4ACPK80FFDJ9	01GSTVJGWM2CX1AA56QSSZ9T2R	01GSTVJHT2TWTZGCPE52AE1T95
    
        $ docker run -p 9090:9090 -v $PWD/data:/data prom/prometheus --storage.tsdb.path=/data --web.enable-admin-api --config.file=/etc/prometheus/prometheus.yml
    
    <img width="1689" alt="image" src="https://user-images.githubusercontent.com/5076964/220455288-e29147d7-c2f6-49dc-bb1d-5be3b28d958f.png">
    
    It should be relatively straightforward to use docker-compose to also
    get a Grafana stuck, but I think where the money's at is putting this
    into a hosted instance. Unfortunately the `promtool` method is
    offline-first, so it doesn't lend itself readily to it.
    
    Epic: none
    Release note: None
    
    cli: allow converting raw tsdumps into openmetric ones
    
    `./cockroach debug tsdump --format=openmetrics tsdump.gob > tsdump.om`
    
    For demonstration purposes one can then:
    
    - clone dockprom[^1]
    - add `uid: v9Zz2K6nz` to the prometheus datasource[^2]
    - run:
    
      ```
      for d in grafana_data prometheus_data; do
        docker volume create --driver local --opt type=none --opt device=$PWD/$d--opt o=bind $d
      done
      promtool tsdb create-blocks-from openmetrics tsdump.om prometheus_data/
      docker-compose up -d
      ```
    - http://localhost:3000 admin/admin
    - import the L2 dashboard[^3]
    - view the tsdump![^4]
    
    Note that all of the histograms are missing because the internal timeseries
    data doesn't support histograms (records just a few quantiles).
    
    [^1]: https://github.com/stefanprodan/dockprom
    [^2]: grafana/provisioning/datasources/datasource.yml
    [^3]: https://grafana.testeng.crdb.io/d/CAvWxELVz/l2-drill-down?orgId=1&from=now-1h&to=now&refresh=1m&var-cluster=grinaker-231&var-instances=All
    [^4]: <img width="1664" alt="image" src="https://user-images.githubusercontent.com/5076964/233642565-175fec77-1bff-4a75-af98-b9bdc6748f78.png">
    
    Epic: none
    Release note: None
    
    cli: enable offline tsdump conversion
    
    This commit enables tsdump conversion without a running
    cluster. Prior to this commit, a grpc connection to a
    tsdb server was required to convert raw tsdump files to
    other supported formats.
    
    Release note: None
    
    cli: add more labels to tsdump openmetrics output
    
    This commit adds more labels to the openmetrics tsdump output,
    for parity with cloud prom metrics.
    
    Labels with values passed by the command line:
    - `cluster`
    
    Labels with values collected from datapoints:
    - `node_id`
    - `store`
    
    Labels with hard-coded values:
    - `cluster_type`
    - `job`
    - `region`
    
    Labels with zero values:
    - `instance`
    - `node`
    - `organization_id`
    - `organization_label`
    - `sla_type`
    - `tenant_id`
    
    Prior to this commit, only the `node` and `store` labels were
    reported.
    
    For details, see
    https://github.com/cockroachlabs/managed-service/pull/14740#issuecomment-1736134254.
    
    Release note: None

--- pkg/cli/debug.go
+++ pkg/cli/debug.go
@@ -1536,9 +1536,11 @@ func init() {
 	f.Var(&debugLogChanSel, "only-channels", "selection of channels to include in the output diagram.")
 
 	f = debugTimeSeriesDumpCmd.Flags()
-	f.Var(&debugTimeSeriesDumpOpts.format, "format", "output format (text, csv, tsv, raw)")
+	f.Var(&debugTimeSeriesDumpOpts.format, "format", "output format (text, csv, tsv, raw, openmetrics)")
 	f.Var(&debugTimeSeriesDumpOpts.from, "from", "oldest timestamp to include (inclusive)")
 	f.Var(&debugTimeSeriesDumpOpts.to, "to", "newest timestamp to include (inclusive)")
+	f.StringVar(&debugTimeSeriesDumpOpts.clusterLabel, "cluster-label",
+		"", "prometheus label for cluster name")
 
 	f = debugSendKVBatchCmd.Flags()
 	f.StringVar(&debugSendKVBatchContext.traceFormat, "trace", debugSendKVBatchContext.traceFormat,
