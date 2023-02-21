2bd4382feda7c0ad894067d58467f35cba0f1ab7
Shivam Saraf <shivam.saraf@cockroachlabs.com>
2023-02-21 13:19:45 -0500

    sql: support JSONB encoding and decoding for forward indexes
    Currently, it is not possible to create a primary and a secondary index
    on a JSON column in CRDB. This is because forward indexes on JSONB
    columns have not yet been implemented due to a lack of a valid
    lexicographic ordering.
    
    To address this, a key encoding strategy was developed for each JSON
    value. In order to maintain a lexicographical ordering of the encodings
    of JSON values, different marker bytes were defined in an order similar
    to the order defined for these JSON values. Encodings for primitive JSON
    values, such as Null, False and True, only consist of their marker
    bytes.
    
    e.g: To encode a JSON False:
    
    `enc(false::JSONB) = [JSONB_Null_Tag]`
    
    Encodings for JSON Strings and Numbers consist of a concatenation of
    their respective marker bytes, the encoding of the string or the number
    in consideration and a terminator byte to indicate that the encoding for
    the JSON value has ended.
    
    e.g: To encode a JSON String '"a"':
    
    `enc('"a"'::JSONB) = [JSONB_String_Tag, enc("a"), JSONB_Terminator_Tag]`
    
    Encodings for JSON Arrays and Objects consist of a concatenation of
    their respective marker bytes, the total number of elements/key-value
    pairs present within the container (in bytes), the encodings of the
    elements present in the container followed by a terminator tag to
    indicate the encoding for the given JSON container has ended.
    
    e.g: To encode a JSON array '["a"]':
    
    `enc('["a"]'::JSONB) = [JSONB_Array_Tag, enc(1), JSONB_String_Tag,
    enc(a), JSONB_Terminator_Tag, JSONB_Terminator_Tag]`
    
    Epic: CRDB-24501
    Fixes: #35706
    
    Release note: None

--- pkg/sql/catalog/systemschema/system.go
+++ pkg/sql/catalog/systemschema/system.go
@@ -2512,6 +2512,7 @@ var (
 				Version:             descpb.StrictIndexColumnIDGuaranteesVersion,
 				Type:                descpb.IndexDescriptor_INVERTED,
 				InvertedColumnKinds: []catpb.InvertedIndexColumnKind{catpb.InvertedIndexColumnKind_DEFAULT},
+				CompositeColumnIDs:  []descpb.ColumnID{13},
 			},
 		),
 		func(tbl *descpb.TableDescriptor) {
