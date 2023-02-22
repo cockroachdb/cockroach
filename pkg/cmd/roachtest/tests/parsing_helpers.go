0f1ff7aae8a351381771c8638a76b88dbd468033
Rafi Shamim <rafi@cockroachlabs.com>
2023-02-22 16:21:50 -0500

    roachtest: improve npgsql test parsing
    Parse an XML file rather than the plain text output.
    
    Release note: None

--- pkg/cmd/roachtest/tests/parsing_helpers.go
+++ pkg/cmd/roachtest/tests/parsing_helpers.go
@@ -15,32 +15,10 @@ import (
 	"bytes"
 	"fmt"
 	"regexp"
-	"strconv"
 )
-
-var dotNetUnitTestOutputRegex = regexp.MustCompile(`\s+(?P<result>Skipped|Failed)\s+(?P<name>.*) \[.+]`)
-var dotNetTestSummaryRegex = regexp.MustCompile(`Failed:\s+\d+,\s+Passed:\s+(?P<passes>\d+),\s+Skipped:\s+\d+,\s+Total:\s+\d+, Duration:`)
 
 var rustUnitTestOutputRegex = regexp.MustCompile(`(?P<type>.*) (?P<class>.*)::(?P<name>.*) \.\.\. (?P<result>.*)`)
 var pythonUnitTestOutputRegex = regexp.MustCompile(`(?P<name>.*) \((?P<class>.*)\) \.\.\. (?P<result>[^'"]*?)(?: u?['"](?P<reason>.*)['"])?$`)
-
-func (r *ormTestsResults) parseDotNetUnitTestOutput(
-	input []byte, expectedFailures blocklist, ignoredList blocklist,
-) {
-	r.parseUnitTestOutput(dotNetUnitTestOutputRegex, input, expectedFailures, ignoredList)
-	// Search the results one more time to get the total number of tests.
-	scanner := bufio.NewScanner(bytes.NewReader(input))
-	for scanner.Scan() {
-		match := dotNetTestSummaryRegex.FindStringSubmatch(scanner.Text())
-		if match != nil {
-			groups := map[string]string{}
-			for i, name := range match {
-				groups[dotNetTestSummaryRegex.SubexpNames()[i]] = name
-			}
-			r.passExpectedCount, _ = strconv.Atoi(groups["passes"])
-		}
-	}
-}
 
 func (r *ormTestsResults) parsePythonUnitTestOutput(
 	input []byte, expectedFailures blocklist, ignoredList blocklist,
