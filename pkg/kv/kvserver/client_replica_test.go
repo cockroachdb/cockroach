cockroachdb/cockroach

commit 7443a00e8ce01a9a01b61923611913f99f327769

kv: stop modifying MaxTimestamp, plumb local uncertainty limit
This commit replaces `LimitTxnMaxTimestamp` with `ComputeLocalUncertaintyLimit`.
Instead of updating the transaction proto's MaxTimestamp field, we now keep a
localUncertaintyLimit value on the stack during evaluation and use this to limit
uncertainty restarts. This makes things easier to understand, as we no longer
modify a request's transaction in-place.

In doing so, this also fixes a bug where an `mvccPutInternal` could return a
`ReadWithinUncertaintyInterval` error in cases where it should have returned a
`WriteTooOld` error.

pkg/kv/kvserver/client_replica_test.go
@@ -1124,16 +1124,16 @@ func TestLeaseExpirationBelowFutureTimeRequest(t *testing.T) {
 	})
 }
 
-// TestRangeLimitTxnMaxTimestamp verifies that on lease transfer, the
-// normal limiting of a txn's max timestamp to the first observed
-// timestamp on a node is extended to include the lease start
-// timestamp. This disallows the possibility that a write to another
-// replica of the range (on node n1) happened at a later timestamp
-// than the originally observed timestamp for the node which now owns
-// the lease (n2). This can happen if the replication of the write
-// doesn't make it from n1 to n2 before the transaction observes n2's
-// clock time.
-func TestRangeLimitTxnMaxTimestamp(t *testing.T) {
+// TestRangeLocalUncertaintyLimitAfterNewLease verifies that on lease
+// transfer, the normal limiting of a request's local uncertainty limit
+// to the first observed timestamp on a node is extended to include the
+// lease start timestamp. This disallows the possibility that a write to
+// another replica of the range (on node n1) happened at a later
+// timestamp than the originally observed timestamp for the node which
+// now owns the lease (n2). This can happen if the replication of the
+// write doesn't make it from n1 to n2 before the transaction observes
+// n2's clock time.
+func TestRangeLocalUncertaintyLimitAfterNewLease(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 
@@ -1177,7 +1177,7 @@ func TestRangeLimitTxnMaxTimestamp(t *testing.T) {
 
 	// Do a write on node1 to establish a key with its timestamp at now.
 	if _, pErr := kv.SendWrapped(
-		context.Background(), tc.Servers[0].DistSender(), putArgs(keyA, []byte("value")),
+		ctx, tc.Servers[0].DistSender(), putArgs(keyA, []byte("value")),
 	); pErr != nil {
 		t.Fatal(pErr)
 	}
@@ -1194,7 +1194,7 @@ func TestRangeLimitTxnMaxTimestamp(t *testing.T) {
 	}
 
 	testutils.SucceedsSoon(t, func() error {
-		if err := replica1.AdminTransferLease(context.Background(), replica2Desc.StoreID); err != nil {
+		if err := replica1.AdminTransferLease(ctx, replica2Desc.StoreID); err != nil {
 			t.Fatal(err)
 		}
 		lease, _ := replica2.GetLease()
@@ -1214,7 +1214,7 @@ func TestRangeLimitTxnMaxTimestamp(t *testing.T) {
 	// expect to see an uncertainty interval error.
 	h := roachpb.Header{Txn: &txn}
 	if _, pErr := kv.SendWrappedWith(
-		context.Background(), tc.Servers[0].DistSender(), h, getArgs(keyA),
+		ctx, tc.Servers[0].DistSender(), h, getArgs(keyA),
 	); !testutils.IsPError(pErr, "uncertainty") {
 		t.Fatalf("expected an uncertainty interval error; got %v", pErr)
 	}

pkg/kv/kvserver/batcheval/cmd_get.go
@@ -33,9 +33,10 @@ func Get(
 	reply := resp.(*roachpb.GetResponse)
 
 	val, intent, err := storage.MVCCGet(ctx, reader, args.Key, h.Timestamp, storage.MVCCGetOptions{
-		Inconsistent:     h.ReadConsistency != roachpb.CONSISTENT,
-		Txn:              h.Txn,
-		FailOnMoreRecent: args.KeyLocking != lock.None,
+		Inconsistent:          h.ReadConsistency != roachpb.CONSISTENT,
+		Txn:                   h.Txn,
+		FailOnMoreRecent:      args.KeyLocking != lock.None,
+		LocalUncertaintyLimit: cArgs.LocalUncertaintyLimit,
 	})
 	if err != nil {
 		return result.Result{}, err

pkg/kv/kvserver/batcheval/cmd_lease_transfer.go
@@ -33,7 +33,7 @@ func declareKeysTransferLease(
 	// and so it uses latches to wait for all in-flight requests to complete.
 	//
 	// Because of this, it declares a non-MVCC write over every addressable key
-	// in the range, even through the only key the TransferLease actually writes
+	// in the range, even though the only key the TransferLease actually writes
 	// to is the RangeLeaseKey. This guarantees that it conflicts with any other
 	// request because every request must declare at least one addressable key.
 	//

pkg/kv/kvserver/batcheval/cmd_scan.go
@@ -40,12 +40,13 @@ func Scan(
 	var err error
 
 	opts := storage.MVCCScanOptions{
-		Inconsistent:     h.ReadConsistency != roachpb.CONSISTENT,
-		Txn:              h.Txn,
-		MaxKeys:          h.MaxSpanRequestKeys,
-		TargetBytes:      h.TargetBytes,
-		FailOnMoreRecent: args.KeyLocking != lock.None,
-		Reverse:          false,
+		Inconsistent:          h.ReadConsistency != roachpb.CONSISTENT,
+		Txn:                   h.Txn,
+		LocalUncertaintyLimit: cArgs.LocalUncertaintyLimit,
+		MaxKeys:               h.MaxSpanRequestKeys,
+		TargetBytes:           h.TargetBytes,
+		FailOnMoreRecent:      args.KeyLocking != lock.None,
+		Reverse:               false,
 	}
 
 	switch args.ScanFormat {

pkg/kv/kvserver/batcheval/declare.go
@@ -17,6 +17,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/spanset"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/storage/enginepb"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 )
 
 // DefaultDeclareKeys is the default implementation of Command.DeclareKeys.
@@ -105,5 +106,6 @@ type CommandArgs struct {
 	Header  roachpb.Header
 	Args    roachpb.Request
 	// *Stats should be mutated to reflect any writes made by the command.
-	Stats *enginepb.MVCCStats
+	Stats                 *enginepb.MVCCStats
+	LocalUncertaintyLimit hlc.Timestamp
 }

pkg/kv/kvserver/observedts/BUILD.bazel
@@ -13,7 +13,6 @@ go_library(
         "//pkg/kv/kvserver/kvserverpb",
         "//pkg/roachpb",
         "//pkg/util/hlc",
-        "//pkg/util/log",
     ],
 )
 

pkg/kv/kvserver/observedts/doc.go
@@ -172,7 +172,7 @@ var D5 = roachpb.Transaction{}.ObservedTimestamps
 
 // D6 ————————————————————————————————————————————————
 //
-// LimitTxnMaxTimestamp
+// ComputeLocalUncertaintyLimit
 //
 // Observed timestamps allow transactions to avoid uncertainty related restarts
 // because they allow transactions to bound their "effective max timestamp" when
@@ -183,8 +183,10 @@ var D5 = roachpb.Transaction{}.ObservedTimestamps
 //
 // TODO(nvanbenschoten): update this documentation to discuss the local
 // uncertainty limit in place of the "effective max timestamp" once we start
-// returning this value instead of updating the max timestamp field.
-var D6 = LimitTxnMaxTimestamp
+// returning this value instead of updating the max timestamp field. Also,
+// update the documentation to reflect to rename from "max timestamp" to
+// "global uncertainty limit".
+var D6 = ComputeLocalUncertaintyLimit
 
 // Ignore unused warnings.
 var _, _, _, _, _, _ = D1, D2, D3, D4, D5, D6

pkg/kv/kvserver/observedts/limit.go
@@ -11,17 +11,18 @@
 package observedts
 
 import (
-	"context"
-
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvserverpb"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
-	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 )
 
-// LimitTxnMaxTimestamp limits the transaction's max timestamp so that it
-// respects any timestamp already observed on the leaseholder's node. This
-// prevents unnecessary uncertainty interval restarts caused by reading a value
-// written at a timestamp between txn.Timestamp and txn.MaxTimestamp.
+// ComputeLocalUncertaintyLimit returns a limited version of the the
+// transaction's global uncertainty limit so that it respects any timestamp
+// already observed on the leaseholder's node. This prevents unnecessary
+// uncertainty interval restarts caused by reading a value written at a
+// timestamp between txn.ReadTimestamp and txn.GlobalUncertaintyLimit. The
+// returned local uncertainty limit will either be empty (if observed
+// timestamps could not be used) or will be a clock timestamp.
 //
 // The lease's start time is also taken into consideration to ensure that a
 // lease transfer does not result in the observed timestamp for this node being
@@ -38,44 +39,37 @@ import (
 //     guaranteed to be greater than any write which occurred under
 //     the previous leaseholder.
 //
-func LimitTxnMaxTimestamp(
-	ctx context.Context, txn *roachpb.Transaction, status kvserverpb.LeaseStatus,
-) *roachpb.Transaction {
-	if txn == nil || status.State != kvserverpb.LeaseState_VALID {
-		return txn
+func ComputeLocalUncertaintyLimit(
+	txn *roachpb.Transaction, status kvserverpb.LeaseStatus,
+) hlc.Timestamp {
+	if txn == nil {
+		return hlc.Timestamp{}
 	}
+	if status.State != kvserverpb.LeaseState_VALID {
+		return hlc.Timestamp{}
+	}
+
 	// For calls that read data within a txn, we keep track of timestamps
 	// observed from the various participating nodes' HLC clocks. If we have a
 	// timestamp on file for the leaseholder's node which is smaller than
-	// MaxTimestamp, we can lower MaxTimestamp accordingly. If MaxTimestamp
-	// drops below ReadTimestamp, we effectively can't see uncertainty restarts
-	// anymore.
+	// GlobalUncertaintyLimit, we can lower localUncertaintyLimit accordingly.
+	// If GlobalUncertaintyLimit drops below ReadTimestamp, we effectively can't
+	// see uncertainty restarts anymore.
 	//
 	// Note that we care about an observed timestamp from the leaseholder's
 	// node, even if this is a follower read on a different node. See the
 	// comment in doc.go about "Follower Reads" for more.
-	obsClockTS, ok := txn.GetObservedTimestamp(status.Lease.Replica.NodeID)
+	obsTs, ok := txn.GetObservedTimestamp(status.Lease.Replica.NodeID)
 	if !ok {
-		return txn
+		return hlc.Timestamp{}
 	}
 	// If the lease is valid, we use the greater of the observed timestamp and
-	// the lease start time, up to the max timestamp. This ensures we avoid
-	// incorrect assumptions about when data was written, in absolute time on a
-	// different node, which held the lease before this replica acquired it.
-	obsClockTS.Forward(status.Lease.Start)
-	obsTS := obsClockTS.ToTimestamp()
-	// If the observed timestamp reduces the transaction's uncertainty interval,
-	// update the transacion proto.
-	if obsTS.Less(txn.MaxTimestamp) {
-		// Copy-on-write to protect others we might be sharing the Txn with.
-		txnClone := txn.Clone()
-		// The uncertainty window is [ReadTimestamp, maxTS), so if that window
-		// is empty, there won't be any uncertainty restarts.
-		if obsTS.LessEq(txn.ReadTimestamp) {
-			log.Event(ctx, "read has no clock uncertainty")
-		}
-		txnClone.MaxTimestamp.Backward(obsTS)
-		txn = txnClone
-	}
-	return txn
+	// the lease start time. This ensures we avoid incorrect assumptions about
+	// when data was written, in absolute time on a different node, which held
+	// the lease before this replica acquired it.
+	obsTs.Forward(status.Lease.Start)
+
+	localUncertaintyLimit := txn.MaxTimestamp
+	localUncertaintyLimit.Backward(obsTs.ToTimestamp())
+	return localUncertaintyLimit
 }

commit 1abbc05d686483b427ffdc7c9055eb65b8a64a6e

kv: configure transaction uncertainty interval for reverse scan
This commit configures the transaction uncertainty intervals for reverse scans.
This was initially missed in 7443a00, which broke the use of observed timestamps
for reverse scans. In e36a826, we started using this new plumbing path for the
entire uncertainty interval, so the pessimization became a correctness bug that
could lead to a loss of real-time ordering between a write and a reverse scan in
cases with moderate but bounded clock skew between nodes.

The commit adds testing for this case and the ScanRequest case. I should have
exercised these cases back in 09dfee3 when I noticed there was a complete
absence of end-to-end testing for uncertainty interval errors. Unfortunately,
that commit only added testing for the GetRequest case, allowing the bug to slip
in for ReverseScanRequest.

Release note (bug fix): Transaction uncertainty intervals are correctly
configured for reverse scans again, ensuring that reverse scans cannot serve
stale reads when clocks in a cluster are skewed.

Epic: None

pkg/kv/kvserver/batcheval/cmd_reverse_scan.go
@@ -43,6 +43,7 @@ func ReverseScan(
 		Inconsistent:          h.ReadConsistency != kvpb.CONSISTENT,
 		SkipLocked:            h.WaitPolicy == lock.WaitPolicy_SkipLocked,
 		Txn:                   h.Txn,
+		Uncertainty:           cArgs.Uncertainty,
 		MaxKeys:               h.MaxSpanRequestKeys,
 		MaxIntents:            storage.MaxIntentsPerWriteIntentError.Get(&cArgs.EvalCtx.ClusterSettings().SV),
 		TargetBytes:           h.TargetBytes,

pkg/kv/kvserver/client_replica_test.go
@@ -435,67 +435,88 @@ func TestTxnPutOutOfOrder(t *testing.T) {
 // timestamp is below the committed value's timestamp. As a result, the
 // transaction observes the committed value in its certain future, allowing it
 // to avoid the ReadWithinUncertaintyIntervalError.
+//
+// Each test variant is run using the three different forms of transactional
+// read-only operations: Get, Scan, and ReverseScan.
 func TestTxnReadWithinUncertaintyInterval(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 
 	testutils.RunTrueAndFalse(t, "observedTS", func(t *testing.T, observedTS bool) {
-		ctx := context.Background()
-		manual := hlc.NewHybridManualClock()
-		srv, _, _ := serverutils.StartServer(t, base.TestServerArgs{
-			Knobs: base.TestingKnobs{
-				Server: &server.TestingKnobs{
-					WallClock: manual,
-				},
-			},
+		readOps := []interface{}{"get", "scan", "revScan"}
+		testutils.RunValues(t, "readOp", readOps, func(t *testing.T, readOp interface{}) {
+			testTxnReadWithinUncertaintyInterval(t, observedTS, readOp.(string))
 		})
-		s := srv.(*server.TestServer)
-		defer s.Stopper().Stop(ctx)
-		store, err := s.Stores().GetStore(s.GetFirstStoreID())
-		require.NoError(t, err)
-
-		// Split off a scratch range.
-		key, err := s.ScratchRange()
-		require.NoError(t, err)
-
-		// Pause the server's clocks going forward.
-		manual.Pause()
-
-		// Create a new transaction.
-		now := s.Clock().Now()
-		maxOffset := s.Clock().MaxOffset().Nanoseconds()
-		require.NotZero(t, maxOffset)
-		txn := roachpb.MakeTransaction("test", key, 1, now, maxOffset, int32(s.SQLInstanceID()))
-		require.True(t, txn.ReadTimestamp.Less(txn.GlobalUncertaintyLimit))
-		require.Len(t, txn.ObservedTimestamps, 0)
-
-		// If the test variant wants an observed timestamp from the server at a
-		// timestamp below the value, collect one now.
-		if observedTS {
-			get := getArgs(key)
-			resp, pErr := kv.SendWrappedWith(ctx, store.TestSender(), kvpb.Header{Txn: &txn}, get)
-			require.Nil(t, pErr)
-			txn.Update(resp.Header().Txn)
-			require.Len(t, txn.ObservedTimestamps, 1)
-		}
-
-		// Perform a non-txn write. This will grab a timestamp from the clock.
-		put := putArgs(key, []byte("val"))
-		_, pErr := kv.SendWrapped(ctx, store.TestSender(), put)
-		require.Nil(t, pErr)
-
-		// Perform another read on the same key. Depending on whether or not the txn
-		// had collected an observed timestamp, it may or may not observe the value
-		// in its uncertainty interval and throw an error.
-		get := getArgs(key)
-		_, pErr = kv.SendWrappedWith(ctx, store.TestSender(), kvpb.Header{Txn: &txn}, get)
-		if observedTS {
-			require.Nil(t, pErr)
-		} else {
-			require.NotNil(t, pErr)
-			require.IsType(t, &kvpb.ReadWithinUncertaintyIntervalError{}, pErr.GetDetail())
-		}
 	})
+}
+
+func testTxnReadWithinUncertaintyInterval(t *testing.T, observedTS bool, readOp string) {
+	ctx := context.Background()
+	manual := hlc.NewHybridManualClock()
+	srv, _, _ := serverutils.StartServer(t, base.TestServerArgs{
+		Knobs: base.TestingKnobs{
+			Server: &server.TestingKnobs{
+				WallClock: manual,
+			},
+		},
+	})
+	s := srv.(*server.TestServer)
+	defer s.Stopper().Stop(ctx)
+	store, err := s.Stores().GetStore(s.GetFirstStoreID())
+	require.NoError(t, err)
+
+	// Split off a scratch range.
+	key, err := s.ScratchRange()
+	require.NoError(t, err)
+
+	// Pause the server's clocks going forward.
+	manual.Pause()
+
+	// Create a new transaction.
+	now := s.Clock().Now()
+	maxOffset := s.Clock().MaxOffset().Nanoseconds()
+	require.NotZero(t, maxOffset)
+	txn := roachpb.MakeTransaction("test", key, 1, now, maxOffset, int32(s.SQLInstanceID()))
+	require.True(t, txn.ReadTimestamp.Less(txn.GlobalUncertaintyLimit))
+	require.Len(t, txn.ObservedTimestamps, 0)
+
+	// If the test variant wants an observed timestamp from the server at a
+	// timestamp below the value, collect one now.
+	if observedTS {
+		get := getArgs(key)
+		resp, pErr := kv.SendWrappedWith(ctx, store.TestSender(), kvpb.Header{Txn: &txn}, get)
+		require.Nil(t, pErr)
+		txn.Update(resp.Header().Txn)
+		require.Len(t, txn.ObservedTimestamps, 1)
+	}
+
+	// Perform a non-txn write. This will grab a timestamp from the clock.
+	put := putArgs(key, []byte("val"))
+	_, pErr := kv.SendWrapped(ctx, store.TestSender(), put)
+	require.Nil(t, pErr)
+
+	// Perform another read on the same key. Depending on whether or not the txn
+	// had collected an observed timestamp, it may or may not observe the value
+	// in its uncertainty interval and throw an error.
+
+	var read kvpb.Request
+	switch readOp {
+	case "get":
+		read = getArgs(key)
+	case "scan":
+		read = scanArgs(key, key.Next())
+	case "revScan":
+		read = revScanArgs(key, key.Next())
+	default:
+		t.Fatalf("unknown op: %q", readOp)
+	}
+	_, pErr = kv.SendWrappedWith(ctx, store.TestSender(), kvpb.Header{Txn: &txn}, read)
+	if observedTS {
+		require.Nil(t, pErr)
+	} else {
+		require.NotNil(t, pErr)
+		require.IsType(t, &kvpb.ReadWithinUncertaintyIntervalError{}, pErr.GetDetail())
+	}
 }
 
 // TestTxnReadWithinUncertaintyIntervalAfterIntentResolution tests cases where a

pkg/kv/kvserver/client_test.go
@@ -37,12 +37,31 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// getArgs returns a GetRequest and GetResponse pair addressed to
-// the default replica for the specified key.
+// getArgs returns a GetRequest for the specified key.
 func getArgs(key roachpb.Key) *kvpb.GetRequest {
 	return &kvpb.GetRequest{
 		RequestHeader: kvpb.RequestHeader{
 			Key: key,
+		},
+	}
+}
+
+// scanArgs returns a ScanRequest for the specified key and end key.
+func scanArgs(key, endKey roachpb.Key) *kvpb.ScanRequest {
+	return &kvpb.ScanRequest{
+		RequestHeader: kvpb.RequestHeader{
+			Key:    key,
+			EndKey: endKey,
+		},
+	}
+}
+
+// revScanArgs returns a ReverseScanRequest for the specified key and end key.
+func revScanArgs(key, endKey roachpb.Key) *kvpb.ReverseScanRequest {
+	return &kvpb.ReverseScanRequest{
+		RequestHeader: kvpb.RequestHeader{
+			Key:    key,
+			EndKey: endKey,
 		},
 	}
 }