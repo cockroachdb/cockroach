d675f83b8969cf993d8e8127fbdaea34a94c38cf
Evan Wall <wall@cockroachlabs.com>
2023-02-22 09:14:26 -0500

    multitenant: reduce touch points required to add new capabilities
    Fixes #97652
    
    Currently a adding a new capability of an existing type requires modifying call
    sites in many files:
    ```
    1) pkg/multitenant/tenantcapabilities/tenantcapabilitiestestutils/testutils.go
    2) pkg/sql/crdb_internal.go
    3) pkg/sql/show_tenant.go
    4) pkg/sql/tenant_capability.go
    5) pkg/testutils/testcluster/testcluster.go
    ```
    
    This change allows new capabilities of existings types to be added without
    modifying these call sites - they only need to be modified if a new capability
    type is added.
    
    Additionally, a new interface is added to abstract away the capability models
    generated from protobufs:
    ```
    type TenantCapabilities interface {
    	// GetBoolCapability returns the value of the corresponding bool capability.
    	GetBoolCapability(BoolCapabilityName) bool
    	// SetBoolCapability sets the value of the corresponding bool capability.
    	SetBoolCapability(BoolCapabilityName, bool)
    }
    ```
    
    This PR #97828 demonstrates how another capability type can be added following
    this pattern.
    
    Release note: None

--- pkg/multitenant/tenantcapabilities/tenantcapabilitiespb/capabilities.go
+++ pkg/multitenant/tenantcapabilities/tenantcapabilitiespb/capabilities.go
@@ -10,56 +10,37 @@
 
 package tenantcapabilitiespb
 
-import "github.com/cockroachdb/errors"
+import (
+	"fmt"
 
-// TenantCapabilityName is a pseudo-enum of valid capability names.
-type TenantCapabilityName int32
-
-// valueOffset sets the iota offset to make sure the 0 value is not a valid
-// enum value.
-const valueOffset = 1
-
-// IsSet returns true if the capability name has a non-zero value.
-func (t TenantCapabilityName) IsSet() bool {
-	return t >= valueOffset
-}
-
-var stringToTenantCapabilityName = func() map[string]TenantCapabilityName {
-	numCapabilities := len(_TenantCapabilityName_index) - 1
-	m := make(map[string]TenantCapabilityName, numCapabilities)
-	for i := 0; i < numCapabilities; i++ {
-		startIndex := _TenantCapabilityName_index[i]
-		endIndex := _TenantCapabilityName_index[i+1]
-		s := _TenantCapabilityName_name[startIndex:endIndex]
-		m[s] = TenantCapabilityName(i + valueOffset)
-	}
-	return m
-}()
-
-// TenantCapabilityNameFromString converts a string to a TenantCapabilityName
-// or returns an error if no conversion is possible.
-func TenantCapabilityNameFromString(s string) (TenantCapabilityName, error) {
-	tenantCapabilityName, ok := stringToTenantCapabilityName[s]
-	if !ok {
-		return 0, errors.Newf("unknown capability: %q", s)
-	}
-	return tenantCapabilityName, nil
-}
-
-//go:generate stringer -type=TenantCapabilityName -linecomment
-const (
-	// CanAdminSplit if set to true, grants the tenant the ability to
-	// successfully perform `AdminSplit` requests.
-	CanAdminSplit TenantCapabilityName = iota + valueOffset // can_admin_split
-	// CanViewNodeInfo if set to true, grants the tenant the ability
-	// retrieve node-level observability data at endpoints such as `_status/nodes`
-	// and in the DB Console overview page.
-	CanViewNodeInfo // can_view_node_info
-	// CanViewTSDBMetrics if set to true, grants the tenant the ability to
-	// make arbitrary queries of the TSDB of the entire cluster. Currently,
-	// we do not store per-tenant metrics so this will surface system metrics
-	// to the tenant.
-	// TODO(davidh): Revise this once tenant-scoped metrics are implemented in
-	// https://github.com/cockroachdb/cockroach/issues/96438
-	CanViewTSDBMetrics // can_view_tsdb_metrics
+	"github.com/cockroachdb/cockroach/pkg/multitenant/tenantcapabilities/tenantcapabilitiesapi"
 )
+
+func (t *TenantCapabilities) getBoolFieldRef(
+	capabilityName tenantcapabilitiesapi.BoolCapabilityName,
+) *bool {
+	switch capabilityName {
+	case tenantcapabilitiesapi.CanAdminSplit:
+		return &t.CanAdminSplit
+	case tenantcapabilitiesapi.CanViewNodeInfo:
+		return &t.CanViewNodeInfo
+	case tenantcapabilitiesapi.CanViewTSDBMetrics:
+		return &t.CanViewTSDBMetrics
+	default:
+		panic(fmt.Sprintf("unknown capability: %q", capabilityName.String()))
+	}
+}
+
+// GetBoolCapability implements tenantcapabilitiesapi.TenantCapabilities.
+func (t *TenantCapabilities) GetBoolCapability(
+	capabilityName tenantcapabilitiesapi.BoolCapabilityName,
+) bool {
+	return *t.getBoolFieldRef(capabilityName)
+}
+
+// SetBoolCapability implements tenantcapabilitiesapi.TenantCapabilities.
+func (t *TenantCapabilities) SetBoolCapability(
+	capabilityName tenantcapabilitiesapi.BoolCapabilityName, capabilityValue bool,
+) {
+	*t.getBoolFieldRef(capabilityName) = capabilityValue
+}
