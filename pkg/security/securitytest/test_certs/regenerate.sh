c50bd93fe35e97cf00e2165520448cdc99bbf78a
Raphael 'kena' Poss <knz@thaumogen.net>
2023-02-23 20:18:31 +0100

    cli: don't scope TLS client certs to a specific tenant by default
    This commit changes the default for `--tenant-scope` from "only the
    system tenant" to "cert valid for all tenants".
    
    Note that the scoping is generally useful for security, and it is used
    in CockroachCloud. However, CockroachCloud does not use our CLI code
    to generate certs and sets its cert tenant scopes on its own.
    
    Given that our CLI code is provided for convenience and developer
    productivity, and we don't expect certs generated here to be used in
    multi-tenant deployments where tenants are adversarial to each other,
    defaulting to certs that are valid on every tenant is a good choice.
    
    Release note: None

--- pkg/security/securitytest/test_certs/regenerate.sh
+++ pkg/security/securitytest/test_certs/regenerate.sh
@@ -7,9 +7,9 @@ rm -f "${dir_n}"/*.{crt,key}
 ./cockroach cert --certs-dir="${dir_n}" --ca-key="${dir_n}/ca.key" create-node 127.0.0.1 ::1 localhost *.local
 
 # Create client certs with tenant scopes.
-./cockroach cert --certs-dir="${dir_n}" --ca-key="${dir_n}/ca.key" create-client root --tenant-scope 1,2,10,11,20
-./cockroach cert --certs-dir="${dir_n}" --ca-key="${dir_n}/ca.key" create-client testuser --tenant-scope 1,2,10,11,20
-./cockroach cert --certs-dir="${dir_n}" --ca-key="${dir_n}/ca.key" create-client testuser2 --tenant-scope 1,2,10,11,20
+./cockroach cert --certs-dir="${dir_n}" --ca-key="${dir_n}/ca.key" create-client root
+./cockroach cert --certs-dir="${dir_n}" --ca-key="${dir_n}/ca.key" create-client testuser
+./cockroach cert --certs-dir="${dir_n}" --ca-key="${dir_n}/ca.key" create-client testuser2
 
 # Tenant certs
 ./cockroach mt cert --certs-dir="${dir_n}" --ca-key="${dir_n}/ca-client-tenant.key" create-tenant-client-ca
