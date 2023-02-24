fd0f6927d11866bb66d08701709ac2be197c2f98
Xiang Gu <xiang@cockroachlabs.com>
2023-02-23 19:12:01 -0500

    sql: disallow dropping column referenced in partial index predicate
    Informs #96924
    
    Release note (bug fix/sql change): Columns referenced in partial index
    predicates and partial unique constraint predicates can no longer be
    dropped. The `ALTER TABLE .. DROP COLUMN` statement now returns an error
    with a hint suggesting to drop the indexes and constraints first. This
    is a temporary safe-guard to prevent users from hitting #96924. This
    restriction will be lifted when that bug is fixed.

--- pkg/sql/alter_table.go
+++ pkg/sql/alter_table.go
@@ -1653,7 +1653,7 @@ func dropColumnImpl(
 		containsThisColumn := idx.CollectKeyColumnIDs().Contains(colToDrop.GetID()) ||
 			idx.CollectKeySuffixColumnIDs().Contains(colToDrop.GetID()) ||
 			idx.CollectSecondaryStoredColumnIDs().Contains(colToDrop.GetID())
-		if !containsThisColumn && idx.IsPartial() {
+		if idx.IsPartial() {
 			expr, err := parser.ParseExpr(idx.GetPredicate())
 			if err != nil {
 				return nil, err
@@ -1666,6 +1666,7 @@ func dropColumnImpl(
 
 			if colIDs.Contains(colToDrop.GetID()) {
 				containsThisColumn = true
+				return nil, sqlerrors.NewColumnReferencedByPartialIndex(string(colToDrop.ColName()), idx.GetName())
 			}
 		}
 		// Perform the DROP.
@@ -1691,6 +1692,21 @@ func dropColumnImpl(
 
 	// Drop non-index-backed unique constraints which reference the column.
 	for _, uwoi := range tableDesc.EnforcedUniqueConstraintsWithoutIndex() {
+		if uwoi.IsPartial() {
+			expr, err := parser.ParseExpr(uwoi.GetPredicate())
+			if err != nil {
+				return nil, err
+			}
+
+			colIDs, err := schemaexpr.ExtractColumnIDs(tableDesc, expr)
+			if err != nil {
+				return nil, err
+			}
+
+			if colIDs.Contains(colToDrop.GetID()) {
+				return nil, sqlerrors.NewColumnReferencedByPartialUniqueWithoutIndexConstraint(string(colToDrop.ColName()), uwoi.GetName())
+			}
+		}
 		if uwoi.Dropped() || !uwoi.CollectKeyColumnIDs().Contains(colToDrop.GetID()) {
 			continue
 		}
