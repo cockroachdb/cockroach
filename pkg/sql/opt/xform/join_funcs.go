554b1ddc0714e32346e2b2e8bced5bc91ea1a70f
Rebecca Taft <becca@cockroachlabs.com>
2023-02-26 20:00:13 -0600

    opt: split disjunction in join conditions in more cases
    Prior to this commit, when a join condition included a disjunction
    (e.g. a OR b), in some cases we could remove the disjunction by splitting
    the join into a UNION of joins to create a more efficient plan. However,
    we were only performing this transformation if at least one side of the OR
    predicate contained an equijoin predicate (e.g., t1.col1 = t2.col1). There
    were other cases where we could have improved the plan by splitting the
    disjunction, but we did not do so.
    
    This commit improves our ability to optimize joins with disjunctions in
    the join condition when there is the possibility to push one or both sides
    of the disjunction below the join. This commit removes the requirement that
    the disjunction contains an equijoin predicate, and instead splits the
    disjunction in all cases where it is possible to do so, thus enabling
    more optimization opportunities.
    
    Fixes #97695
    
    Release note (performance improvement): if the session setting
    optimizer_use_improved_split_disjunction_for_joins is true, the optimizer
    now creates a better query plan in some cases where an inner, semi, or
    anti join contains a join predicate with a disjuction (OR condition).

--- pkg/sql/opt/xform/join_funcs.go
+++ pkg/sql/opt/xform/join_funcs.go
@@ -1480,16 +1480,13 @@ func (c *CustomFuncs) splitValues(
 }
 
 // splitDisjunctionForJoin finds the first disjunction in the ON clause that can
-// be split into an interesting pair of predicates. It returns the pair of
-// predicates and the Filters item they were a part of. If an "interesting"
-// disjunction is not found, ok=false is returned.
+// be split into a pair of predicates. It returns the pair of predicates and the
+// Filters item they were a part of. If a disjunction is not found, ok=false is
+// returned.
 //
 // It is expected that the left and right inputs to joinRel have been
 // pre-checked to be canonical scans, or Selects from canonical scans, and
 // origLeftScan and origRightScan refer to those scans.
-//
-// For details on what makes an "interesting" disjunction, see
-// findInterestingDisjunctionPairForJoin.
 func (c *CustomFuncs) splitDisjunctionForJoin(
 	joinRel memo.RelExpr,
 	filters memo.FiltersExpr,
@@ -1504,7 +1501,7 @@ func (c *CustomFuncs) splitDisjunctionForJoin(
 	for i := range filters {
 		if filters[i].Condition.Op() == opt.OrOp {
 			if leftPreds, rightPreds, ok =
-				c.findInterestingDisjunctionPairForJoin(joinRel, &filters[i], origLeftScan, origRightScan); ok {
+				c.findDisjunctionPairForJoin(joinRel, &filters[i], origLeftScan, origRightScan); ok {
 				itemToReplace = &filters[i]
 				return leftPreds, rightPreds, itemToReplace, true
 			}
@@ -1528,12 +1525,11 @@ func (c *CustomFuncs) makeFilteredSelectForJoin(
 	return newSelect
 }
 
-// SplitJoinWithEquijoinDisjuncts checks a join relation for a disjunction of
-// equijoin predicates in an InnerJoin, SemiJoin or AntiJoin. If present, and
-// the inputs to the join are canonical scans, or Selects from canonical scans,
-// it builds two new join relations of the same join type as the original, but
-// with one disjunct assigned to firstJoin and the remaining disjuncts assigned
-// to secondJoin.
+// SplitJoinWithDisjuncts checks a join relation for a disjunction of predicates
+// in an InnerJoin, SemiJoin or AntiJoin. If present, and the inputs to the join
+// are canonical scans, or Selects from canonical scans, it builds two new join
+// relations of the same join type as the original, but with one disjunct
+// assigned to firstJoin and the remaining disjuncts assigned to secondJoin.
 //
 // In the case of inner join, newRelationCols contains the column ids from the
 // original Scans in the left and right inputs plus primary key columns from
@@ -1544,9 +1540,10 @@ func (c *CustomFuncs) makeFilteredSelectForJoin(
 // aggCols contains the non-key columns of the left and right inputs.
 // groupingCols contains the primary key columns of the left and right inputs,
 // needed for deduplicating results.
-// If there is no disjunction of equijoin predicates, or the join type is not
-// one of the supported join types listed above, ok=false is returned.
-func (c *CustomFuncs) SplitJoinWithEquijoinDisjuncts(
+//
+// If there is no disjunction of predicates, or the join type is not one of the
+// supported join types listed above, ok=false is returned.
+func (c *CustomFuncs) SplitJoinWithDisjuncts(
 	joinRel memo.RelExpr, joinFilters memo.FiltersExpr,
 ) (
 	firstJoin memo.RelExpr,
@@ -1768,25 +1765,20 @@ func (c *CustomFuncs) CanHoistProjectInput(relation memo.RelExpr) (ok bool) {
 	return ok
 }
 
-// findInterestingDisjunctionPairForJoin groups disjunction subexpressions into
-// an "interesting" pair of join predicates.
+// findDisjunctionPairForJoin groups disjunction subexpressions into a pair of
+// join predicates.
 //
-// An "interesting" pair of predicates is one where one predicate is an
-// equality predicate which could enable more performant joins than cross join,
-// such as hash join or lookup join. At least one predicate in the disjunction
-// must be an equality join term referencing both input relations. When there
-// are more than two predicates, the deepest leaf node in the left depth OrExpr
-// tree is returned as "left" and the remaining predicates are built into a
-// brand new OrExpr chain and returned as "right".
+// When there are more than two predicates, the deepest leaf node in the left
+// depth OrExpr tree is returned as "left" and the remaining predicates are
+// built into a brand new OrExpr chain and returned as "right".
 //
 // It is expected that the left and right inputs to joinRel have been
 // pre-checked to be canonical scans, or Selects from canonical scans, and
 // leftScan and rightScan refer to those scans.
 //
-// findInterestingDisjunctionPairForJoin returns an ok=false if at least one of
-// the ORed predicates is not an equality join term referencing both input
-// relations, or if joinRel is not a join relation.
-func (c *CustomFuncs) findInterestingDisjunctionPairForJoin(
+// findDisjunctionPairForJoin returns ok=false if joinRel is not a join
+// relation.
+func (c *CustomFuncs) findDisjunctionPairForJoin(
 	joinRel memo.RelExpr, filter *memo.FiltersItem, leftScan *memo.ScanExpr, rightScan *memo.ScanExpr,
 ) (left opt.ScalarExpr, right opt.ScalarExpr, ok bool) {
 	if !opt.IsJoinOp(joinRel) {
@@ -1805,9 +1797,8 @@ func (c *CustomFuncs) findInterestingDisjunctionPairForJoin(
 	leftColSet := c.OutputCols(leftScan)
 	rightColSet := c.OutputCols(rightScan)
 
-	// An ANDed expression is interesting if it has at least one equality join
-	// predicate.
-	interesting := false
+	// hasJoinEquality returns true if an ANDed expression has at least one
+	// equality join predicate.
 	var hasJoinEquality func(opt.ScalarExpr) bool
 	hasJoinEquality = func(expr opt.ScalarExpr) bool {
 		switch t := expr.(type) {
@@ -1824,14 +1815,18 @@ func (c *CustomFuncs) findInterestingDisjunctionPairForJoin(
 	// Traverse all adjacent OrExpr.
 	var collect func(opt.ScalarExpr)
 	collect = func(expr opt.ScalarExpr) {
-		interesting = false
+		interesting := false
 		switch t := expr.(type) {
 		case *memo.OrExpr:
 			collect(t.Left)
 			collect(t.Right)
 			return
 		default:
-			interesting = hasJoinEquality(expr)
+			if c.e.evalCtx.SessionData().OptimizerUseImprovedSplitDisjunctionForJoins {
+				interesting = true
+			} else {
+				interesting = hasJoinEquality(expr)
+			}
 		}
 
 		if interesting && len(leftExprs) == 0 {
