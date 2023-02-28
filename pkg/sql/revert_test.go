be536a5b24099cc57daa2904e4847abfbab7229e
Steven Danna <danna@cockroachlabs.com>
2023-02-28 10:29:17 +0000

    sql: add RevertSpans and RevertSpansFanout
    This PR adds RevertSpans and RevertSpansFanout.
    
    RevertSpans de-duplicates some code between import rollback and
    streaming cutover.
    
    Along the way, it fixes a small bug that existed in RevertTables: Only
    a single RevertRangeRequest is permitted in a batch since
    RevertRangeRequest has the isAlone flag set. As a result, a future
    caller of RevertTables would have encountered a fatal error from KV.
    
    RevertSpansFanout uses DistSQL's PartitionSpans to manually fan out
    multiple RevertRange requests. Since all users of revert range request
    currently set a limit on the number of keys touched, dist sender
    doesn't fanout such request.
    
    Release note: None

--- pkg/sql/revert_test.go
+++ pkg/sql/revert_test.go
@@ -12,18 +12,23 @@ package sql_test
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/kv"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/security/username"
 	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/bootstrap"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/desctestutils"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/catid"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
@@ -104,6 +109,206 @@ func TestTableRollback(t *testing.T) {
 
 		db.CheckQueryResults(t, `SELECT count(*) FROM test`, beforeNumRows)
 	})
+}
+
+// TestTableRollbackMultiTable is a regression test for a previous bug
+// in RevertTables in which passing two tables to the function would
+// cause a log.Fatal error from KV.
+func TestTableRollbackMultiTable(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	s, sqlDB, kv := serverutils.StartServer(
+		t, base.TestServerArgs{UseDatabase: "test"})
+	defer s.Stopper().Stop(context.Background())
+	execCfg := s.ExecutorConfig().(sql.ExecutorConfig)
+
+	db := sqlutils.MakeSQLRunner(sqlDB)
+	db.Exec(t, "CREATE DATABASE IF NOT EXISTS test")
+	db.Exec(t, "CREATE TABLE test (k INT PRIMARY KEY)")
+	db.Exec(t, "CREATE TABLE test2 (k INT PRIMARY KEY)")
+
+	kvDB := s.DB()
+	desc1 := desctestutils.TestingGetPublicTableDescriptor(kvDB, execCfg.Codec, "test", "test")
+	desc2 := desctestutils.TestingGetPublicTableDescriptor(kv, keys.SystemSQLCodec, "test", "test2")
+
+	tableID1 := desc1.GetID()
+	tableID2 := desc2.GetID()
+
+	const numRows = 1000
+	db.Exec(t, `INSERT INTO test (k) SELECT generate_series(1, $1)`, numRows)
+	db.Exec(t, `INSERT INTO test2 (k) SELECT generate_series(1, $1)`, numRows)
+
+	before1, ts := fingerprintTableNoHistory(t, db, tableID1, "")
+	before2, _ := fingerprintTableNoHistory(t, db, tableID2, ts)
+
+	targetTime, err := hlc.ParseHLC(ts)
+	require.NoError(t, err)
+
+	// Delete some rows
+	db.Exec(t, `DELETE FROM test WHERE k % 5 = 2`)
+	db.Exec(t, `DELETE FROM test2 WHERE k % 5 = 2`)
+
+	afterDelete1, _ := fingerprintTableNoHistory(t, db, tableID1, "")
+	afterDelete2, _ := fingerprintTableNoHistory(t, db, tableID2, "")
+	require.NotEqual(t, before1, afterDelete1)
+	require.NotEqual(t, before2, afterDelete2)
+
+	aost1, _ := fingerprintTableNoHistory(t, db, tableID1, ts)
+	aost2, _ := fingerprintTableNoHistory(t, db, tableID2, ts)
+	require.Equal(t, before1, aost1)
+	require.Equal(t, before2, aost2)
+
+	const ignoreGC = false
+
+	// Bypass the offline check.
+	desc1.TableDesc().State = descpb.DescriptorState_OFFLINE
+	desc2.TableDesc().State = descpb.DescriptorState_OFFLINE
+
+	require.NoError(t, sql.RevertTables(context.Background(), kv, &execCfg, []catalog.TableDescriptor{desc1, desc2}, targetTime, ignoreGC, 10))
+
+	reverted1, _ := fingerprintTableNoHistory(t, db, tableID1, "")
+	reverted2, _ := fingerprintTableNoHistory(t, db, tableID2, "")
+	require.Equal(t, before1, reverted1, "expected reverted table after edits to match before")
+	require.Equal(t, before2, reverted2, "expected reverted table after edits to match before")
+}
+
+// TestRevertSpansFanout tests RevertSpansFanout using the same
+// sequence of modifications used in TestTableRollback.
+//
+// We use more splits and scatter the ranges.
+func TestRevertSpansFanout(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	ctx := context.Background()
+
+	tc := testcluster.StartTestCluster(t, 3, base.TestClusterArgs{
+		ServerArgs: base.TestServerArgs{UseDatabase: "test"},
+	})
+	defer tc.Stopper().Stop(context.Background())
+	s := tc.TenantOrServer(0)
+	sqlDB := tc.Conns[0]
+
+	execCfg := s.ExecutorConfig().(sql.ExecutorConfig)
+
+	db := sqlutils.MakeSQLRunner(sqlDB)
+	db.Exec(t, "CREATE DATABASE IF NOT EXISTS test")
+	db.Exec(t, "CREATE TABLE test (k INT PRIMARY KEY, rev INT DEFAULT 0, INDEX (rev))")
+
+	kvDB := s.DB()
+	desc := desctestutils.TestingGetPublicTableDescriptor(kvDB, execCfg.Codec, "test", "test")
+	span := desc.TableSpan(execCfg.Codec)
+	tableID := desc.GetID()
+
+	// Fill a table with some rows plus some revisions to those rows.
+	const (
+		initRows  = 1000
+		extraRows = 500
+		splits    = 100
+	)
+
+	db.Exec(t, "INSERT INTO test (k) SELECT generate_series(1, $1)", initRows)
+	db.Exec(t, "UPDATE test SET rev = 1 WHERE k % 3 = 0")
+	db.Exec(t, "DELETE FROM test WHERE k % 10 = 0")
+	before, ts := fingerprintTableNoHistory(t, db, tableID, "")
+	targetTime, err := hlc.ParseHLC(ts)
+	require.NoError(t, err)
+
+	beforeNumRows := db.QueryStr(t, "SELECT count(*) FROM test")
+
+	// Make some more edits: delete some rows and edit others, insert into some of
+	// the gaps made between previous rows, edit a large swath of rows and add a
+	// large swath of new rows as well.
+	db.Exec(t, "DELETE FROM test WHERE k % 5 = 2")
+	db.Exec(t, "INSERT INTO test (k, rev) SELECT generate_series(10, $1, 10), 10", initRows)
+	db.Exec(t, "INSERT INTO test (k, rev) SELECT generate_series($1+1, $1+$2, 1), $2", initRows, extraRows)
+	db.Exec(t, "UPDATE test SET rev = 2 WHERE k % 4 = 0")
+	db.Exec(t, "UPDATE test SET rev = 4 WHERE k > 150 and k < 350")
+
+	var splitPointsBuf strings.Builder
+	splitIncrement := (initRows + extraRows) / splits
+	for i := 0; i < 100; i++ {
+		splitPoint := splitIncrement * i
+		if i == 0 {
+			fmt.Fprintf(&splitPointsBuf, "(%d)", splitPoint)
+		} else {
+			fmt.Fprintf(&splitPointsBuf, ", (%d)", splitPoint)
+		}
+	}
+
+	// Split and scatter the table.
+	db.Exec(t, fmt.Sprintf("ALTER TABLE test SPLIT AT VALUES %s", splitPointsBuf.String()))
+	db.Exec(t, "ALTER TABLE test SCATTER")
+
+	rsCtx, close := sql.MakeJobExecContext("revert-spans", username.RootUserName(), &sql.MemoryMetrics{}, &execCfg)
+	defer close()
+
+	verifyRevert := func() {
+		reverted, _ := fingerprintTableNoHistory(t, db, tableID, "")
+		require.Equal(t, before, reverted, "expected reverted table after edits to match before")
+		db.CheckQueryResults(t, "SELECT count(*) FROM test", beforeNumRows)
+	}
+
+	t.Run("revert-fanout-with-callback", func(t *testing.T) {
+		require.NoError(t,
+			sql.RevertSpansFanout(ctx,
+				kvDB, rsCtx, []roachpb.Span{span}, targetTime, false, 10,
+				func(context.Context, roachpb.Span) error {
+					return nil
+				}))
+		verifyRevert()
+	})
+	t.Run("revert-without-callback", func(t *testing.T) {
+		db.Exec(t, "DELETE FROM test WHERE k % 5 = 2")
+		require.NoError(t,
+			sql.RevertSpansFanout(ctx,
+				kvDB, rsCtx, []roachpb.Span{span}, targetTime, false, 10, nil))
+		verifyRevert()
+	})
+	t.Run("revert-with-1-worker", func(t *testing.T) {
+		db.Exec(t, "DELETE FROM test WHERE k % 5 = 2")
+		db.Exec(t, "SET CLUSTER SETTING sql.revert.max_span_parallelism = 1")
+		defer db.Exec(t, "RESET CLUSTER SETTING sql.revert.max_span_parallelism")
+		require.NoError(t,
+			sql.RevertSpansFanout(ctx,
+				kvDB, rsCtx, []roachpb.Span{span}, targetTime, false, 10,
+				func(context.Context, roachpb.Span) error {
+					return nil
+				}))
+		verifyRevert()
+
+	})
+	t.Run("revert-with-failing-callback", func(t *testing.T) {
+		require.Error(t,
+			sql.RevertSpansFanout(ctx,
+				kvDB, rsCtx, []roachpb.Span{span}, targetTime, false, 10,
+				func(context.Context, roachpb.Span) error {
+					return errors.New("callback failed")
+				}), "callbackFailed")
+		// No use verifying, the revert failed and we didn't
+		// make any changes anyway.
+	})
+}
+
+// fingerprintTableNoHistory returns the fingerprint of the given
+// table as well as the time the fingerprint was taken at.
+//
+// If a non-empty ts string is specified, the fingerprint will be
+// taken at that timestamp.
+func fingerprintTableNoHistory(
+	t *testing.T, db *sqlutils.SQLRunner, tableID catid.DescID, ts string,
+) (int, string) {
+	t.Helper()
+	query := "SELECT cluster_logical_timestamp(), * FROM crdb_internal.fingerprint(crdb_internal.table_span($1), 0::timestamptz, false)"
+	if ts != "" {
+		query = fmt.Sprintf("%s AS OF SYSTEM TIME %s", query, ts)
+	}
+
+	var fingerprint int
+	var timestamp string
+	db.QueryRow(t, query, tableID).Scan(&timestamp, &fingerprint)
+	return fingerprint, timestamp
 }
 
 func TestRevertGCThreshold(t *testing.T) {
