6d303fa68a89f190de753a98a3eb823d2911e39a
e-mbrown <ebsonari@gmail.com>
2023-03-06 15:47:15 -0500

    sql: add plpgsql parser for telemetry purposes
    This commit  removes portions of the plpgparser
    that arent in use or could be shared with the sql
    parser/lexer.
    
    Release note: None

--- pkg/sql/parser/sql-gen.sh
+++ pkg/sql/parser/sql-gen.sh
@@ -1,23 +1,43 @@
 #!/usr/bin/env bash
 
-# This is used through bazel when generating sql.go. Look at BUILD.bazel for
+# This is used through bazel when generating sql.go and plpgsql.go.
+# Look at BUILD.bazel in pkg/sql/parser or pkg/plpgsql/parser for
 # usage.
 
 set -euo pipefail
 
-awk '/func.*sqlSymUnion/ {print $(NF - 1)}' $1 | \
-    sed -e 's/[]\/$*.^|[]/\\&/g' | \
-    sed -e "s/^/s_(type|token) <(/" | \
-    awk '{print $0")>_\\1 <union> /* <\\2> */_"}' > types_regex.tmp
 
-sed -E -f types_regex.tmp < $1 | \
-    awk -f $2 | \
-    sed -Ee 's,//.*$$,,g;s,/[*]([^*]|[*][^/])*[*]/, ,g;s/ +$$//g' > sql-gen.y
-rm types_regex.tmp
+if [ "$#" -eq 5 ]; then
+    awk '/func.*sqlSymUnion/ {print $(NF - 1)}' $1 | \
+        sed -e 's/[]\/$*.^|[]/\\&/g' | \
+        sed -e "s/^/s_(type|token) <(/" | \
+        awk '{print $0")>_\\1 <union> /* <\\2> */_"}' > types_regex.tmp
 
-ret=$($4 -p sql -o $3 sql-gen.y); \
-  if expr "$ret" : ".*conflicts" >/dev/null; then \
-    echo "$ret"; exit 1; \
-  fi;
-rm sql-gen.y
-$5 -w $3
+    sed -E -f types_regex.tmp < $1 | \
+          awk -f $2 | \
+        sed -Ee 's,//.*$$,,g;s,/[*]([^*]|[*][^/])*[*]/, ,g;s/ +$$//g' > sql-gen.y
+    rm types_regex.tmp
+
+    ret=$($4 -p sql -o $3 sql-gen.y); \
+      if expr "$ret" : ".*conflicts" >/dev/null; then \
+        echo "$ret"; exit 1; \
+      fi;
+    rm sql-gen.y
+    $5 -w $3
+else
+  awk '/func.*plpgsqlSymUnion/ {print $(NF - 1)}' $1 | \
+        sed -e 's/[]\/$*.^|[]/\\&/g' | \
+        sed -e "s/^/s_(type|token) <(/" | \
+        awk '{print $0")>_\\1 <union> /* <\\2> */_"}' > types_regex.tmp
+
+    sed -E -f types_regex.tmp < $1 | \
+        sed -Ee 's,//.*$$,,g;s,/[*]([^*]|[*][^/])*[*]/, ,g;s/ +$$//g' > plpgsql-gen.y
+    rm types_regex.tmp
+
+    ret=$($3 -p plpgsql -o $2 plpgsql-gen.y); \
+      if expr "$ret" : ".*conflicts" >/dev/null; then \
+        echo "$ret"; exit 1; \
+      fi;
+    rm plpgsql-gen.y
+    $4 -w $2
+fi
