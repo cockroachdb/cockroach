cb3468e8b61edbeec422a3d950898a0718fcbc96
David Taylor <tinystatemachine@gmail.com>
2023-03-06 20:46:05 +0000

    cloudcheck: add duration param to cloud connection tester
    Release note: none.
    Epic: none.

--- pkg/sql/sem/tree/show.go
+++ pkg/sql/sem/tree/show.go
@@ -162,7 +162,9 @@ type ShowBackupOptions struct {
 	// the full backup dir.
 	EncryptionInfoDir Expr
 	DebugMetadataSST  bool
-	TransferSize      Expr
+
+	CheckConnectionTransferSize Expr
+	CheckConnectionDuration     Expr
 }
 
 var _ NodeFormatter = &ShowBackupOptions{}
@@ -221,9 +223,16 @@ func (o *ShowBackupOptions) Format(ctx *FmtCtx) {
 		ctx.WriteString("debug_dump_metadata_sst")
 	}
 
-	if o.TransferSize != nil {
+	// The following are only used in connection-check SHOW.
+	if o.CheckConnectionTransferSize != nil {
+		maybeAddSep()
 		ctx.WriteString("TRANSFER = ")
-		ctx.FormatNode(o.TransferSize)
+		ctx.FormatNode(o.CheckConnectionTransferSize)
+	}
+	if o.CheckConnectionDuration != nil {
+		maybeAddSep()
+		ctx.WriteString("TIME = ")
+		ctx.FormatNode(o.CheckConnectionDuration)
 	}
 }
 
@@ -238,7 +247,8 @@ func (o ShowBackupOptions) IsDefault() bool {
 		o.Privileges == options.Privileges &&
 		o.DebugMetadataSST == options.DebugMetadataSST &&
 		o.EncryptionInfoDir == options.EncryptionInfoDir &&
-		o.TransferSize == options.TransferSize
+		o.CheckConnectionTransferSize == options.CheckConnectionTransferSize &&
+		o.CheckConnectionDuration == options.CheckConnectionDuration
 }
 
 func combineBools(v1 bool, v2 bool, label string) (bool, error) {
@@ -313,7 +323,19 @@ func (o *ShowBackupOptions) CombineWith(other *ShowBackupOptions) error {
 	if err != nil {
 		return err
 	}
-	// no need to combine TransferSize – it only is used standalone by CONNECTION.
+
+	o.CheckConnectionTransferSize, err = combineExpr(o.CheckConnectionTransferSize, other.CheckConnectionTransferSize,
+		"transfer")
+	if err != nil {
+		return err
+	}
+
+	o.CheckConnectionDuration, err = combineExpr(o.CheckConnectionDuration, other.CheckConnectionDuration,
+		"time")
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
