28daefb84c0dae570109901bb80f8a4974255b2d
Drew Kimball <drewk@cockroachlabs.com>
2023-03-06 23:09:41 -0800

    opt: track UDT references by name in the Metadata
    Previously, it was possible for invalid queries to be kept in the cache
    after a schema change, since user-defined types were tracked only by OID.
    This missed cases where the search path changed which object a name in
    the query resolved to (or whether it resolved at all).
    
    This patch fixes this behavior by tracking UDT references by name, similar
    to what was already done for data sources. This ensures that the query
    staleness check doesn't miss changes to the search path.
    
    Fixes #96674
    
    Release note (bug fix): Fixed a bug that could prevent a cached query with
    a user-defined type reference from being invalidated even after a schema
    change that should prevent the type from being resolved.

--- pkg/sql/opt/optbuilder/builder.go
+++ pkg/sql/opt/optbuilder/builder.go
@@ -514,7 +514,7 @@ func (o *optTrackingTypeResolver) ResolveType(
 	if err != nil {
 		return nil, err
 	}
-	o.metadata.AddUserDefinedType(typ)
+	o.metadata.AddUserDefinedType(typ, name)
 	return typ, nil
 }
 
@@ -526,6 +526,6 @@ func (o *optTrackingTypeResolver) ResolveTypeByOID(
 	if err != nil {
 		return nil, err
 	}
-	o.metadata.AddUserDefinedType(typ)
+	o.metadata.AddUserDefinedType(typ, nil /* name */)
 	return typ, nil
 }
