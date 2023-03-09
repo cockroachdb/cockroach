2a202fbd95f847f6078e2c17ba9f48c5a86528c9
Xiang Gu <xiang@cockroachlabs.com>
2023-03-09 16:04:49 -0500

    scbuild: Rewrite how we decide when a stmt is not supported
    Previously, we had accumulated complex code over time to answer one
    simple question "Is a stmt not supported in declarative schema changer".
    This allows us to "fail fast" and avoid going into the builder (to
    save us round trips). It is complicated and not easy to maintain (e.g.
    we need to consider mode, cluster version, etc.). This
    commit pulls all the checks into one place, the `checks` field in the
    stmt-support-map for each stmt. The one important property is that this
    check is always right about negatives, but could emit false positives.
    That is, it's possible that a stmt is not supported yet passes this
    check; this stmt will need to go into the builder, do some resolution,
    before we know it's not supported and a panic error is thrown out. To
    that regard, we name the function `IsStmtFullySupportedWithFalsePositive`.

--- pkg/sql/schemachanger/scbuild/internal/scbuildstmt/process.go
+++ pkg/sql/schemachanger/scbuild/internal/scbuildstmt/process.go
@@ -14,11 +14,9 @@ import (
 	"reflect"
 
 	"github.com/cockroachdb/cockroach/pkg/clusterversion"
-	"github.com/cockroachdb/cockroach/pkg/server/telemetry"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scerrors"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sessiondatapb"
-	"github.com/cockroachdb/cockroach/pkg/sql/sqltelemetry"
 	"github.com/cockroachdb/errors"
 )
 
@@ -27,57 +25,16 @@ import (
 type supportedStatement struct {
 	// fn is a function to perform a schema change.
 	fn interface{}
+	// checks contains a coarse-grained function to filter out most
+	// unsupported statements.
+	// It's possible for certain unsupported statements to pass it but will
+	// eventually be discovered in the builder and cause an unimplemented panic.
+	// It should only contain "simple" checks that does not require us to resolve
+	// the descriptor and its elements, so we can avoid unnecessary round-trips
+	// made in the builder.
+	checks interface{}
 	// on indicates that this statement is on by default.
 	on bool
-	// extraChecks contains a function to determine whether the command is
-	// supported. These extraChecks are important to be able to avoid any
-	// extra round-trips to resolve the descriptor and its elements if we know
-	// that we cannot process the command.
-	extraChecks interface{}
-	// minSupportedClusterVersion is the minimal binary version that supports this
-	// statement in the declarative schema changer.
-	minSupportedClusterVersion clusterversion.Key
-}
-
-// isFullySupported returns if this statement type is supported, where the
-// mode of the new schema changer can force unsupported statements to be
-// supported.
-func isFullySupported(
-	n tree.Statement, onByDefault bool, extraFn interface{}, mode sessiondatapb.NewSchemaChangerMode,
-) (ret bool) {
-	// If the unsafe modes of the new schema changer are used then any implemented
-	// operation will be exposed.
-	if mode == sessiondatapb.UseNewSchemaChangerUnsafeAlways ||
-		mode == sessiondatapb.UseNewSchemaChangerUnsafe ||
-		onByDefault {
-		ret = true
-		// If a command is labeled as fully supported, it can still have additional,
-		// checks via callback function.
-		if extraFn != nil {
-			fn := reflect.ValueOf(extraFn)
-			in := []reflect.Value{reflect.ValueOf(n), reflect.ValueOf(mode)}
-			values := fn.Call(in)
-			ret = values[0].Bool()
-		}
-		// If `n` is `ALTER TABLE`, then further check each individual command, as
-		// they might have their own extra checks.
-		if alterTableStmt, isAlterTable := n.(*tree.AlterTable); ret && isAlterTable {
-			// Hoist the constraints to separate clauses because other code assumes that
-			// that is how the commands will look.
-			alterTableStmt.HoistAddColumnConstraints(func() {
-				telemetry.Inc(sqltelemetry.SchemaChangeAlterCounterWithExtra("table", "add_column.references"))
-			})
-			for _, cmd := range alterTableStmt.Cmds {
-				info := supportedAlterTableStatements[reflect.TypeOf(cmd)]
-				if info.on && info.extraChecks != nil && !reflect.ValueOf(info.extraChecks).
-					Call([]reflect.Value{reflect.ValueOf(cmd)})[0].Bool() {
-					panic(scerrors.NotImplementedError(cmd))
-				}
-			}
-		}
-		return ret
-	}
-	return ret
 }
 
 // Tracks operations which are fully supported when the declarative schema
@@ -87,111 +44,125 @@ var supportedStatements = map[reflect.Type]supportedStatement{
 	// Alter table will have commands individually whitelisted via the
 	// supportedAlterTableStatements list, so wwe will consider it fully supported
 	// here.
-	reflect.TypeOf((*tree.AlterTable)(nil)):          {fn: AlterTable, on: true, extraChecks: alterTableIsSupported},
-	reflect.TypeOf((*tree.CreateIndex)(nil)):         {fn: CreateIndex, on: true, minSupportedClusterVersion: clusterversion.V23_1},
-	reflect.TypeOf((*tree.DropDatabase)(nil)):        {fn: DropDatabase, on: true, minSupportedClusterVersion: clusterversion.TODODelete_V22_1},
-	reflect.TypeOf((*tree.DropOwnedBy)(nil)):         {fn: DropOwnedBy, on: true, minSupportedClusterVersion: clusterversion.TODODelete_V22_2Start},
-	reflect.TypeOf((*tree.DropSchema)(nil)):          {fn: DropSchema, on: true, minSupportedClusterVersion: clusterversion.TODODelete_V22_1},
-	reflect.TypeOf((*tree.DropSequence)(nil)):        {fn: DropSequence, on: true, minSupportedClusterVersion: clusterversion.TODODelete_V22_1},
-	reflect.TypeOf((*tree.DropTable)(nil)):           {fn: DropTable, on: true, minSupportedClusterVersion: clusterversion.TODODelete_V22_1},
-	reflect.TypeOf((*tree.DropType)(nil)):            {fn: DropType, on: true, minSupportedClusterVersion: clusterversion.TODODelete_V22_1},
-	reflect.TypeOf((*tree.DropView)(nil)):            {fn: DropView, on: true, minSupportedClusterVersion: clusterversion.TODODelete_V22_1},
-	reflect.TypeOf((*tree.CommentOnDatabase)(nil)):   {fn: CommentOnDatabase, on: true, minSupportedClusterVersion: clusterversion.V22_2},
-	reflect.TypeOf((*tree.CommentOnSchema)(nil)):     {fn: CommentOnSchema, on: true, minSupportedClusterVersion: clusterversion.V22_2},
-	reflect.TypeOf((*tree.CommentOnTable)(nil)):      {fn: CommentOnTable, on: true, minSupportedClusterVersion: clusterversion.V22_2},
-	reflect.TypeOf((*tree.CommentOnColumn)(nil)):     {fn: CommentOnColumn, on: true, minSupportedClusterVersion: clusterversion.V22_2},
-	reflect.TypeOf((*tree.CommentOnIndex)(nil)):      {fn: CommentOnIndex, on: true, minSupportedClusterVersion: clusterversion.V22_2},
-	reflect.TypeOf((*tree.CommentOnConstraint)(nil)): {fn: CommentOnConstraint, on: true, minSupportedClusterVersion: clusterversion.V22_2},
-	reflect.TypeOf((*tree.DropIndex)(nil)):           {fn: DropIndex, on: true, minSupportedClusterVersion: clusterversion.V23_1},
-	reflect.TypeOf((*tree.DropFunction)(nil)):        {fn: DropFunction, on: true, minSupportedClusterVersion: clusterversion.V23_1},
-	reflect.TypeOf((*tree.CreateFunction)(nil)):      {fn: CreateFunction, on: true, minSupportedClusterVersion: clusterversion.V23_1},
+	reflect.TypeOf((*tree.AlterTable)(nil)):          {fn: AlterTable, on: true, checks: alterTableChecks},
+	reflect.TypeOf((*tree.CreateIndex)(nil)):         {fn: CreateIndex, on: true, checks: isV231Active},
+	reflect.TypeOf((*tree.DropDatabase)(nil)):        {fn: DropDatabase, on: true, checks: isV221Active},
+	reflect.TypeOf((*tree.DropOwnedBy)(nil)):         {fn: DropOwnedBy, on: true, checks: isV222Active},
+	reflect.TypeOf((*tree.DropSchema)(nil)):          {fn: DropSchema, on: true, checks: isV221Active},
+	reflect.TypeOf((*tree.DropSequence)(nil)):        {fn: DropSequence, on: true, checks: isV221Active},
+	reflect.TypeOf((*tree.DropTable)(nil)):           {fn: DropTable, on: true, checks: isV221Active},
+	reflect.TypeOf((*tree.DropType)(nil)):            {fn: DropType, on: true, checks: isV221Active},
+	reflect.TypeOf((*tree.DropView)(nil)):            {fn: DropView, on: true, checks: isV221Active},
+	reflect.TypeOf((*tree.CommentOnDatabase)(nil)):   {fn: CommentOnDatabase, on: true, checks: isV222Active},
+	reflect.TypeOf((*tree.CommentOnSchema)(nil)):     {fn: CommentOnSchema, on: true, checks: isV222Active},
+	reflect.TypeOf((*tree.CommentOnTable)(nil)):      {fn: CommentOnTable, on: true, checks: isV222Active},
+	reflect.TypeOf((*tree.CommentOnColumn)(nil)):     {fn: CommentOnColumn, on: true, checks: isV222Active},
+	reflect.TypeOf((*tree.CommentOnIndex)(nil)):      {fn: CommentOnIndex, on: true, checks: isV222Active},
+	reflect.TypeOf((*tree.CommentOnConstraint)(nil)): {fn: CommentOnConstraint, on: true, checks: isV222Active},
+	reflect.TypeOf((*tree.DropIndex)(nil)):           {fn: DropIndex, on: true, checks: isV231Active},
+	reflect.TypeOf((*tree.DropFunction)(nil)):        {fn: DropFunction, on: true, checks: isV231Active},
+	reflect.TypeOf((*tree.CreateFunction)(nil)):      {fn: CreateFunction, on: true, checks: isV231Active},
 }
 
 func init() {
+	boolType := reflect.TypeOf((*bool)(nil)).Elem()
 	// Check function signatures inside the supportedStatements map.
 	for statementType, statementEntry := range supportedStatements {
 		// Validate main callback functions.
-		callBackFnType := reflect.TypeOf(statementEntry.fn)
-		if callBackFnType.Kind() != reflect.Func {
+		callBackType := reflect.TypeOf(statementEntry.fn)
+		if callBackType.Kind() != reflect.Func {
 			panic(errors.AssertionFailedf("%v entry for statement is "+
 				"not a function", statementType))
 		}
-		if callBackFnType.NumIn() != 2 ||
-			!callBackFnType.In(0).Implements(reflect.TypeOf((*BuildCtx)(nil)).Elem()) ||
-			callBackFnType.In(1) != statementType {
-			panic(errors.AssertionFailedf("%v entry for statement is "+
-				"does not have a valid signature got %v", statementType, callBackFnType))
+		if callBackType.NumIn() != 2 ||
+			!callBackType.In(0).Implements(reflect.TypeOf((*BuildCtx)(nil)).Elem()) ||
+			callBackType.In(1) != statementType {
+			panic(errors.AssertionFailedf("%v entry for statement "+
+				"does not have a valid signature; got %v", statementType, callBackType))
 		}
 		// Validate fully supported function callbacks.
-		if statementEntry.extraChecks != nil {
-			fullySupportedCallbackFn := reflect.TypeOf(statementEntry.extraChecks)
-			if fullySupportedCallbackFn.Kind() != reflect.Func {
+		if statementEntry.checks != nil {
+			checks := reflect.TypeOf(statementEntry.checks)
+			if checks.Kind() != reflect.Func {
 				panic(errors.AssertionFailedf("%v entry for statement is "+
 					"not a function", statementType))
 			}
-			if fullySupportedCallbackFn.NumIn() != 2 ||
-				fullySupportedCallbackFn.In(0) != statementType ||
-				fullySupportedCallbackFn.In(1) != reflect.TypeOf(sessiondatapb.UseNewSchemaChangerOff) {
-				panic(errors.AssertionFailedf("%v entry for statement is "+
-					"does not have a valid signature got %v", statementType, callBackFnType))
-
+			if checks.NumIn() != 3 ||
+				(checks.In(0) != statementType && !statementType.Implements(checks.In(0))) ||
+				checks.In(1) != reflect.TypeOf(sessiondatapb.UseNewSchemaChangerOff) ||
+				checks.In(2) != reflect.TypeOf((*clusterversion.ClusterVersion)(nil)).Elem() ||
+				checks.NumOut() != 1 ||
+				checks.Out(0) != boolType {
+				panic(errors.AssertionFailedf("%v checks does not have a valid signature; got %v",
+					statementType, checks))
 			}
 		}
 	}
 }
 
-// CheckIfStmtIsSupported determines if the statement is supported by the declarative
-// schema changer.
-func CheckIfStmtIsSupported(
-	activeVersion clusterversion.ClusterVersion,
+// IsFullySupportedWithFalsePositive returns if this statement is
+// "fully supported" in the declarative schema changer under mode and active
+// cluster version.
+// It can return false positive but never false negative, because we only run
+// a few "simple" checks that we cannot totally eliminate unsupported stmts;
+// we will discover those in the builder (when we resolve descriptors and its
+// elements) and panic with an unimplemented error.
+func IsFullySupportedWithFalsePositive(
 	n tree.Statement,
+	activeVersion clusterversion.ClusterVersion,
 	mode sessiondatapb.NewSchemaChangerMode,
+) (ret bool) {
+	return isFullySupportedWithFalsePositiveInternal(supportedStatements, reflect.TypeOf(n),
+		reflect.ValueOf(n), mode, activeVersion)
+}
+
+// isFullySupportedWithFalsePositiveInternal determines whether a stmt is
+// fully supported, with false positive, in the declarative schema changer,
+// given mode the active cluster version.
+func isFullySupportedWithFalsePositiveInternal(
+	stmtSupportMap map[reflect.Type]supportedStatement,
+	stmtType reflect.Type,
+	stmtValue reflect.Value,
+	mode sessiondatapb.NewSchemaChangerMode,
+	activeVersion clusterversion.ClusterVersion,
 ) bool {
-	// Check if an entry exists for the statement type, in which
-	// case it is either fully or partially supported.
-	info, ok := supportedStatements[reflect.TypeOf(n)]
-	if !ok {
-		return false
-	}
-	// Check if partially supported operations are allowed next. If an
-	// operation is not fully supported will not allow it to be run in
-	// the declarative schema changer until its fully supported.
-	if !isFullySupported(n, info.on, info.extraChecks, mode) {
-		return false
-	}
-	// If a version handle is included, then this check can go further
-	// and confirm if the active version supports this statement.
-	if !stmtSupportedInCurrentClusterVersion(activeVersion, n) {
+	if mode == sessiondatapb.UseNewSchemaChangerOff {
 		return false
 	}
 
-	return true
+	info, ok := stmtSupportMap[stmtType]
+	if !ok {
+		return false
+	}
+
+	// If extraFn does not pass, then it's not supported, regardless of mode.
+	if info.checks != nil {
+		fn := reflect.ValueOf(info.checks)
+		in := []reflect.Value{stmtValue, reflect.ValueOf(mode), reflect.ValueOf(activeVersion)}
+		values := fn.Call(in)
+		if !values[0].Bool() {
+			return false
+		}
+	}
+
+	// We now can just return whether the statement is labelled as "on" or not,
+	// with the possibility of mode forcing "not on" to be on.
+	return info.on ||
+		mode == sessiondatapb.UseNewSchemaChangerUnsafeAlways ||
+		mode == sessiondatapb.UseNewSchemaChangerUnsafe
 }
 
 // Process dispatches on the statement type to populate the BuilderState
 // embedded in the BuildCtx. Any error will be panicked.
 func Process(b BuildCtx, n tree.Statement) {
-	// Check if an entry exists for the statement type, in which
-	// case it is either fully or partially supported.
-	info, ok := supportedStatements[reflect.TypeOf(n)]
-	if !ok {
-		panic(scerrors.NotImplementedError(n))
-	}
-	// Check if partially supported operations are allowed next. If an
-	// operation is not fully supported will not allow it to be run in
-	// the declarative schema changer until its fully supported.
-	if !isFullySupported(
-		n, info.on, info.extraChecks, b.EvalCtx().SessionData().NewSchemaChangerMode,
-	) {
+	// Run a few "quick checks" to see if the statement is not supported.
+	if !IsFullySupportedWithFalsePositive(n, b.EvalCtx().Settings.Version.ActiveVersion(b),
+		b.EvalCtx().SessionData().NewSchemaChangerMode) {
 		panic(scerrors.NotImplementedError(n))
 	}
 
-	// Check if the statement is supported in the current cluster version.
-	if !stmtSupportedInCurrentClusterVersion(b.EvalCtx().Settings.Version.ActiveVersion(b), n) {
-		panic(scerrors.NotImplementedError(n))
-	}
-
-	// Next invoke the callback function, with the concrete types.
+	// Invoke the callback function, with the concrete types.
+	info := supportedStatements[reflect.TypeOf(n)]
 	fn := reflect.ValueOf(info.fn)
 	in := []reflect.Value{reflect.ValueOf(b), reflect.ValueOf(n)}
 	// Check if the feature flag for it is enabled.
@@ -202,14 +173,14 @@ func Process(b BuildCtx, n tree.Statement) {
 	fn.Call(in)
 }
 
-// stmtSupportedInCurrentClusterVersion checks whether the statement is supported
-// in current cluster version.
-func stmtSupportedInCurrentClusterVersion(
-	activeVersion clusterversion.ClusterVersion, n tree.Statement,
-) bool {
-	if alterTableStmt, isAlterTable := n.(*tree.AlterTable); isAlterTable {
-		return alterTableAllCmdsSupportedInCurrentClusterVersion(activeVersion, alterTableStmt)
-	}
-	minSupportedClusterVersion := supportedStatements[reflect.TypeOf(n)].minSupportedClusterVersion
-	return activeVersion.IsActive(minSupportedClusterVersion)
+var isV221Active = func(_ tree.NodeFormatter, _ sessiondatapb.NewSchemaChangerMode, activeVersion clusterversion.ClusterVersion) bool {
+	return activeVersion.IsActive(clusterversion.TODODelete_V22_1)
+}
+
+var isV222Active = func(_ tree.NodeFormatter, _ sessiondatapb.NewSchemaChangerMode, activeVersion clusterversion.ClusterVersion) bool {
+	return activeVersion.IsActive(clusterversion.V22_2)
+}
+
+var isV231Active = func(_ tree.NodeFormatter, _ sessiondatapb.NewSchemaChangerMode, activeVersion clusterversion.ClusterVersion) bool {
+	return activeVersion.IsActive(clusterversion.V23_1)
 }
