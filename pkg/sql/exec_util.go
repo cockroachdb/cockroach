64ec0b98f573bd5802a822973488880497fa1ac9
Jane Xing <zhouxing@uchicago.edu>
2023-03-12 21:03:39 -0400

    sql/settings: add `sql.defaults.multiple_active_portals.enabled` cluster setting
    Release note (sql change): added a non-public `sql.defaults.multiple_active_portals.enabled`
    setting. This setting is only for a PREVIEWABLE feature.
    With it set true, all non-internal portals with read-only SELECT queries without
    sub/post queries can be paused and resumed in an interleaving manner, but are
    executed with local plan.

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -733,6 +733,14 @@ var overrideAlterPrimaryRegionInSuperRegion = settings.RegisterBoolSetting(
 	false,
 ).WithPublic()
 
+var enableMultipleActivePortals = settings.RegisterBoolSetting(
+	settings.TenantWritable,
+	"sql.defaults.multiple_active_portals.enabled",
+	"if true, portals with read-only SELECT query without sub/post queries "+
+		"can be executed in interleaving manner, but with local execution plan",
+	false,
+)
+
 var errNoTransactionInProgress = errors.New("there is no transaction in progress")
 var errTransactionInProgress = errors.New("there is already a transaction in progress")
 
