34639466e51d8c1c083ecb4718cf1dbf804e0a82
Michael Butler <butler@cockroachlabs.com>
2023-03-12 20:39:37 -0400

    roachtest: add note on monitor best practices
    Epic: none
    
    Release note: none

--- pkg/cmd/roachtest/cluster/monitor_interface.go
+++ pkg/cmd/roachtest/cluster/monitor_interface.go
@@ -13,6 +13,13 @@ package cluster
 import "context"
 
 // A Monitor watches the cluster for unexpected node deaths.
+//
+// NB: In a roachtest, it's best practice to spawn a new go routine via a
+// monitor, instead of directly in the testSpec.Run() closure because the
+// monitor's go routine has builtin recovery from panics. In other words, a
+// t.Fatal() call within monitor.Go() will lead to a graceful roachtest to
+// failure, just like when t.Fatal is called directly in testSpec.Run(). This
+// ensures proper roachprod cluster shutdown, for example.
 type Monitor interface {
 	ExpectDeath()
 	ExpectDeaths(count int32)
