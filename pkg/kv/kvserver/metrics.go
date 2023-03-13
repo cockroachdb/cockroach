c9db532aadc8e7cc2c4b77e76ed7ebc8e541d556
Austen McClernon <austen@cockroachlabs.com>
2023-03-13 13:56:28 +0000

    kvserver: add replica qps histogram metric
    This commit introduces a histogram tracking percentiles of replica QPS
    time. Previously, there was only point in time insight into replica QPS
    distribution via hot ranges. This change enables historical timeseries
    tracking via the metric `rebalancing.replicas.queriespersecond`.
    
    Part of: #98255
    
    Release note (ops change): The `rebalancing.replicas.queriespersecond`
    histogram metric is added, which provides insight into the distribution
    of queries per replica within a store.

--- pkg/kv/kvserver/metrics.go
+++ pkg/kv/kvserver/metrics.go
@@ -367,6 +367,13 @@ var (
 		Measurement: "Nanoseconds/Sec",
 		Unit:        metric.Unit_NANOSECONDS,
 	}
+	metaRecentReplicaQueriesPerSecond = metric.Metadata{
+		Name: "rebalancing.replicas.queriespersecond",
+		Help: "Histogram of average kv-level requests received per second by " +
+			"replicas on the store in the last 30 minutes.",
+		Measurement: "Queries/Sec",
+		Unit:        metric.Unit_COUNT,
+	}
 
 	// Metric for tracking follower reads.
 	metaFollowerReadsCount = metric.Metadata{
@@ -1907,14 +1914,19 @@ type StoreMetrics struct {
 	Reserved           *metric.Gauge
 
 	// Rebalancing metrics.
-	AverageQueriesPerSecond        *metric.GaugeFloat64
-	AverageWritesPerSecond         *metric.GaugeFloat64
-	AverageReadsPerSecond          *metric.GaugeFloat64
-	AverageRequestsPerSecond       *metric.GaugeFloat64
-	AverageWriteBytesPerSecond     *metric.GaugeFloat64
-	AverageReadBytesPerSecond      *metric.GaugeFloat64
-	AverageCPUNanosPerSecond       *metric.GaugeFloat64
+	AverageQueriesPerSecond    *metric.GaugeFloat64
+	AverageWritesPerSecond     *metric.GaugeFloat64
+	AverageReadsPerSecond      *metric.GaugeFloat64
+	AverageRequestsPerSecond   *metric.GaugeFloat64
+	AverageWriteBytesPerSecond *metric.GaugeFloat64
+	AverageReadBytesPerSecond  *metric.GaugeFloat64
+	AverageCPUNanosPerSecond   *metric.GaugeFloat64
+	// NB: Even though we could average the histogram in order to get
+	// AverageCPUNanosPerSecond from RecentReplicaCPUNanosPerSecond, we duplicate
+	// both for backwards compatibility since the cost of the gauge is small.
+	// This includes all replicas, including quiesced ones.
 	RecentReplicaCPUNanosPerSecond *metric.ManualWindowHistogram
+	RecentReplicaQueriesPerSecond  *metric.ManualWindowHistogram
 	// l0SublevelsWindowedMax doesn't get recorded to metrics itself, it maintains
 	// an ad-hoc history for gosipping information for allocator use.
 	l0SublevelsWindowedMax syncutil.AtomicFloat64
@@ -2496,6 +2508,11 @@ func newStoreMetrics(histogramWindow time.Duration) *StoreMetrics {
 		RecentReplicaCPUNanosPerSecond: metric.NewManualWindowHistogram(
 			metaRecentReplicaCPUNanosPerSecond,
 			metric.ReplicaCPUTimeBuckets,
+			true, /* withRotate */
+		),
+		RecentReplicaQueriesPerSecond: metric.NewManualWindowHistogram(
+			metaRecentReplicaQueriesPerSecond,
+			metric.ReplicaBatchRequestCountBuckets,
 			true, /* withRotate */
 		),
 
--- pkg/kv/kvserver/store.go
+++ pkg/kv/kvserver/store.go
@@ -2959,6 +2959,7 @@ func (s *Store) updateReplicationGauges(ctx context.Context) error {
 		replicaCPUNanosPerSecond := loadStats.RaftCPUNanosPerSecond + loadStats.RequestCPUNanosPerSecond
 		averageCPUNanosPerSecond += replicaCPUNanosPerSecond
 		s.metrics.RecentReplicaCPUNanosPerSecond.RecordValue(replicaCPUNanosPerSecond)
+		s.metrics.RecentReplicaQueriesPerSecond.RecordValue(loadStats.QueriesPerSecond)
 
 		locks += metrics.LockTableMetrics.Locks
 		totalLockHoldDurationNanos += metrics.LockTableMetrics.TotalLockHoldDurationNanos
@@ -3031,6 +3032,9 @@ func (s *Store) updateReplicationGauges(ctx context.Context) error {
 	}
 
 	if err := s.metrics.RecentReplicaCPUNanosPerSecond.Rotate(); err != nil {
+		return err
+	}
+	if err := s.metrics.RecentReplicaQueriesPerSecond.Rotate(); err != nil {
 		return err
 	}
 
