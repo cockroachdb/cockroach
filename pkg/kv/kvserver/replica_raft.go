bfcd7320ea808aaec9a2c0894acc77a052b74939
Tobias Grieger <tobias.b.grieger@gmail.com>
2023-03-13 11:58:29 +0100

    Revert "kvserver: improve reproposal assertions and documentation"
    Reverts #97973.
    
    This reverts commit dd698970ab38479b882f77db5f35326b3676d56c.

--- pkg/kv/kvserver/replica_raft.go
+++ pkg/kv/kvserver/replica_raft.go
@@ -317,45 +317,32 @@ func (r *Replica) evalAndPropose(
 
 // propose encodes a command, starts tracking it, and proposes it to Raft.
 //
-// On success, the method hands ownership of the command over to the Raft
-// machinery. After the method returns with a nil error, all access to the
-// command must be performed while holding Replica.mu and Replica.raftMu.
-// If the method returns with an error, the error is permanent for the
-// proposal, that is, the caller must notify the client that the proposal
-// failed and the client can retry, making a new proposal in the process.
+// The method hands ownership of the command over to the Raft machinery. After
+// the method returns, all access to the command must be performed while holding
+// Replica.mu and Replica.raftMu.
 //
-// propose takes ownership of the supplied token, even on error; the caller
-// should tok.Move() it into this method. It will be used to untrack the request
-// once it comes out of the proposal buffer. If the method returns with an error,
-// the token is released, since, as explained above, an error is permanent.
-//
-// The ProposalData must not be reproposed or reused should an error be returned
-// from this method. Its MaxLeaseIndex and encodedCommand fields must be empty.
-// Reproposals are a rich source of complexity. See the comment on `r.mu.proposals`
-// for details.
+// propose takes ownership of the supplied token; the caller should tok.Move()
+// it into this method. It will be used to untrack the request once it comes out
+// of the proposal buffer.
 func (r *Replica) propose(
 	ctx context.Context, p *ProposalData, tok TrackedRequestToken,
 ) (pErr *kvpb.Error) {
 	defer tok.DoneIfNotMoved(ctx)
 
-	defer func() {
-		// An error for this method
+	// If an error occurs reset the command's MaxLeaseIndex to its initial value.
+	// Failure to propose will propagate to the client. An invariant of this
+	// package is that proposals which are finished carry a raft command with a
+	// MaxLeaseIndex equal to the proposal command's max lease index.
+	defer func(prev uint64) {
 		if pErr != nil {
-			p.encodedCommand = nil
+			p.command.MaxLeaseIndex = prev
 		}
-	}()
+	}(p.command.MaxLeaseIndex)
 
-	if p.command.MaxLeaseIndex > 0 {
-		// MaxLeaseIndex should not be populated now. It is set only when the proposal buffer
-		// flushes this proposal into the local raft instance.
-		return kvpb.NewError(errors.AssertionFailedf("MaxLeaseIndex set: %+v", p))
-	}
-	if p.encodedCommand != nil {
-		// This indicates someone took an existing proposal and handed it to this method
-		// again. The caller needs to properly reset the proposal if they're going to do
-		// that.
-		return kvpb.NewError(errors.AssertionFailedf("encodedCommand set: %+v", p))
-	}
+	// Make sure the maximum lease index is unset. This field will be set in
+	// propBuf.Insert and its encoded bytes will be appended to the encoding
+	// buffer as a MaxLeaseFooter.
+	p.command.MaxLeaseIndex = 0
 
 	// Determine the encoding style for the Raft command.
 	prefix := true
