d820915e5c231adcf240602a341dd2a4d805e3a4
Raphael 'kena' Poss <knz@thaumogen.net>
2023-03-14 14:54:27 +0100

    sql: support tenant configuration templates
    This change introduces the LIKE clause to CREATE TENANT, which makes
    CREATE TENANT copy the parameters (but not the storage keyspace) from
    the tenant selected by LIKE.
    
    Also if LIKE is not specified, but the (new) cluster setting
    `sql.create_tenant.default_template` is not empty, the value of the
    cluster setting is used implicitly as LIKE clause.
    
    A proposed use of this is cluster-to-cluster replication, considering
    cutover as well. On the target (sink) cluster, the operator would do:
    
    ```
    CREATE TENANT replicated LIKE app_template
      FROM REPLICATION OF application ON ....
    ```
    
    And then cutover would look something like the following if they
    wanted the tenant to still be named "application"
    
    ```
    ALTER TENANT replicated CUTOVER TO LATEST;
    DROP TENANT application; -- if there's one already
    ALTER TENANT replicated RENAME TO application;
    ALTER TENANT application START SERVICE SHARED;
    ```
    
    Release note: None

--- pkg/sql/tenant_accessors.go
+++ pkg/sql/tenant_accessors.go
@@ -15,8 +15,10 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/clusterversion"
 	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
 	"github.com/cockroachdb/cockroach/pkg/multitenant/mtinfopb"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql/isql"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgcode"
@@ -186,4 +188,141 @@ func (p *planner) LookupTenantID(
 		return tid, err
 	}
 	return roachpb.MustMakeTenantID(rec.ID), nil
+}
+
+// GetExtendedTenantInfo hydrates a TenantInfoWithUsage with the
+// additional data beyond the TenantInfo record.
+func GetExtendedTenantInfo(
+	ctx context.Context, txn isql.Txn, info *mtinfopb.TenantInfo,
+) (*mtinfopb.TenantInfoWithUsage, error) {
+	res := &mtinfopb.TenantInfoWithUsage{
+		ProtoInfo: info.ProtoInfo,
+		SQLInfo:   info.SQLInfo,
+	}
+	rows, err := txn.QueryBufferedEx(ctx, "get-tenant-setting-overrides", txn.KV(), sessiondata.NodeUserSessionDataOverride,
+		`SELECT name, value, value_type, reason FROM system.tenant_settings WHERE tenant_id = $1`,
+		info.ID,
+	)
+	if err != nil {
+		return nil, err
+	}
+	for _, row := range rows {
+		override := &mtinfopb.SettingOverride{
+			Name:      string(tree.MustBeDString(row[0])),
+			Value:     string(tree.MustBeDString(row[1])),
+			ValueType: string(tree.MustBeDString(row[2])),
+		}
+		if row[3] != tree.DNull {
+			s := string(tree.MustBeDString(row[3]))
+			override.Reason = &s
+		}
+		res.SettingOverrides = append(res.SettingOverrides, override)
+	}
+
+	row, err := txn.QueryRowEx(ctx, "get-tenant-usage-config", txn.KV(), sessiondata.NodeUserSessionDataOverride,
+		`SELECT ru_current, ru_burst_limit, ru_refill_rate
+       FROM system.tenant_usage
+      WHERE tenant_id = $1 AND instance_id = 0`,
+		info.ID,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if row != nil {
+		res.Usage = &mtinfopb.UsageInfo{
+			RUCurrent:    float64(tree.MustBeDFloat(row[0])),
+			RUBurstLimit: float64(tree.MustBeDFloat(row[1])),
+			RURefillRate: float64(tree.MustBeDFloat(row[2])),
+		}
+	}
+
+	return res, nil
+}
+
+var defaultTenantConfigTemplate = func() *settings.StringSetting {
+	s := settings.RegisterStringSetting(
+		settings.SystemOnly,
+		"sql.create_tenant.default_template",
+		"tenant to use as configuration template when LIKE is not specified in CREATE TENANT",
+		// We use the empty string so that no template is used by default
+		// (i.e. empty proto, no setting overrides).
+		"",
+	)
+	s.SetReportable(true)
+	return s
+}()
+
+// GetTenantTemplate loads the tenant template corresponding to the
+// provided origin tenant. If info is nil, likeTenantID is zero and
+// likeTenantName is empty, the default template is returned.
+func GetTenantTemplate(
+	ctx context.Context,
+	settings *cluster.Settings,
+	txn isql.Txn,
+	info *mtinfopb.TenantInfo,
+	likeTenantID uint64,
+	likeTenantName string,
+) (res *mtinfopb.TenantInfoWithUsage, err error) {
+	if info != nil && (likeTenantID != 0 || likeTenantName != "") {
+		// Sanity check
+		return nil, errors.AssertionFailedf("programming error: cannot pass both default info struct and tenant reference")
+	}
+	if info == nil {
+		if likeTenantID == 0 && likeTenantName == "" {
+			// No LIKE at all. Do we have something in the cluster setting?
+			tmplName := defaultTenantConfigTemplate.Get(&settings.SV)
+			if tmplName == "" {
+				// No template at all - just use an empty protobuf.
+				return &mtinfopb.TenantInfoWithUsage{}, nil
+			}
+			// Use the template specified in the setting.
+			info, err = GetTenantRecordByName(ctx, settings, txn, roachpb.TenantName(tmplName))
+			if err != nil {
+				return nil, errors.Wrapf(err, "retrieving default tenant configuration template %q", tmplName)
+			}
+		} else {
+			if likeTenantID != 0 && likeTenantName != "" {
+				return nil, errors.AssertionFailedf("programming error: conflicting input tenant spec from caller")
+			}
+			// No pre-loaded info, but we have a LIKE clause. Is it by-ID or by-Name?
+			if likeTenantID != 0 {
+				// By-ID.
+				tid, err := roachpb.MakeTenantID(likeTenantID)
+				if err != nil {
+					return nil, errors.Wrap(err, "invalid LIKE tenant ID")
+				}
+				info, err = GetTenantRecordByID(ctx, txn, tid, settings)
+				if err != nil {
+					return nil, errors.Wrap(err, "retrieving LIKE tenant record")
+				}
+			} else {
+				// By-name.
+				info, err = GetTenantRecordByName(ctx, settings, txn, roachpb.TenantName(likeTenantName))
+				if err != nil {
+					return nil, errors.Wrap(err, "retrieving LIKE tenant record")
+				}
+			}
+		}
+	}
+
+	// Now we have our info field. Expand it.
+	tmplInfo, err := GetExtendedTenantInfo(ctx, txn, info)
+	if err != nil {
+		return nil, errors.Wrap(err, "retrieving tenant template details")
+	}
+
+	// Clear out the fields we can't reuse in a fresh tenant record.
+	tmplInfo.ID = 0
+	tmplInfo.Name = ""
+	tmplInfo.DataState = mtinfopb.DataStateReady
+	tmplInfo.ServiceMode = mtinfopb.ServiceModeNone
+	tmplInfo.DroppedName = ""
+	tmplInfo.DeprecatedID = 0
+	tmplInfo.DeprecatedDataState = 0
+	tmplInfo.TenantReplicationJobID = 0
+	if tmplInfo.Usage != nil {
+		tmplInfo.Usage.Consumption = kvpb.TenantConsumption{}
+	}
+
+	return tmplInfo, nil
 }
