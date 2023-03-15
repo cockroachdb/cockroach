a0cd57668744f7c8d822c13570520f080c446df9
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-03-14 21:55:07 -0700

    colflow: simplify the vectorized flow creator a bit
    This commit refactors the vectorized flow creator to directly embed two
    structs that implement interfaces used by the creator. This allows for
    those structs to not be allocated on the main code path. Also,
    `flowCreatorHelper` no longer implements the `Releasable` interface.
    
    Release note: None

--- pkg/sql/colflow/vectorized_flow_test.go
+++ pkg/sql/colflow/vectorized_flow_test.go
@@ -242,7 +242,7 @@ func TestDrainOnlyInputDAG(t *testing.T) {
 	}
 	var wg sync.WaitGroup
 	vfc := newVectorizedFlowCreator(
-		&vectorizedFlowCreatorHelper{f: f}, componentCreator, false, false, &wg, &execinfra.RowChannel{},
+		f, nil /* helper */, componentCreator, false, false, &wg, &execinfra.RowChannel{},
 		nil /* batchSyncFlowConsumer */, nil /* podNodeDialer */, execinfrapb.FlowID{}, colcontainer.DiskQueueCfg{},
 		nil /* fdSemaphore */, descs.DistSQLTypeResolver{}, admission.WorkInfo{},
 	)
