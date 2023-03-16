e8296275e37ad59ec02cfad31379b16f5461d2c5
Tobias Grieger <tobias.b.grieger@gmail.com>
2023-03-16 12:39:38 +0100

    scripts: use pprofme for pprof-post
    They've changed their endpoints and the script no longer works. But they
    now have a `cli`. Make the script a wrapper around the cli so that
    anyone who uses pprof-post will find out it exists.
    
    Epic: none
    Release note: None

--- scripts/pprof-post.sh
+++ scripts/pprof-post.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Usage: $0 "optional description, defaults to current date" < somefile.pb.gz
-out=$(curl -s 'https://api.polarsignals.com/api/share/v1/profiles' \
-	-X POST \
-	-F "description=${1-$(date -u)}" \
-	-F "profile=@-")
-echo -n "https://share.polarsignals.com/"
-grep -Eo '[0-9a-f]{4,}' <<< "${out}"
-	
+if ! which pprofme; then
+	echo "pprofme missing, setup instructions: https://github.com/polarsignals/pprofme#install"
+	exit 1
+fi
+
+pprofme upload "$@"
