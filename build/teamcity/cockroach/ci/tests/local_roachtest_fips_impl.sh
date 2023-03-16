cba39b746dce63d919c96cf92fefb1a0c555364e
Rail Aliiev <rail@iqchoice.com>
2023-03-16 19:49:10 -0400

    ci: add local roachtest wrappers
    This PR add wrappers to run FIPS local roachtests in CI.
    
    Epic: DEVINF-478
    Informs: #98824
    Release note: None

---
+++ build/teamcity/cockroach/ci/tests/local_roachtest_fips_impl.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -euox pipefail
+
+fips_enabled=$(cat /proc/sys/crypto/fips_enabled)
+
+if [[ $fips_enabled != "1" ]]; then
+  echo "FIPS mode is not enabled. Exiting."
+  exit 1
+fi
+
+export CROSSLINUX_CONFIG="crosslinuxfips"
+
+bazel build --config=$CROSSLINUX_CONFIG --config=ci //pkg/cmd/cockroach-short \
+      //pkg/cmd/roachtest \
+      //pkg/cmd/roachprod \
+      //pkg/cmd/workload
+
+BAZEL_BIN=$(bazel info bazel-bin --config=$CROSSLINUX_CONFIG --config=ci)
+
+# if there are any local clusters on this host, stop them before we
+# attempt to run acceptance tests. While this is generally not the
+# case, if a previous `roachtest` run was abruptly killed, the local
+# cluster would remain active and cause every test below to fail.
+$BAZEL_BIN/pkg/cmd/roachprod/roachprod_/roachprod destroy --all-local
+
+$BAZEL_BIN/pkg/cmd/roachtest/roachtest_/roachtest run acceptance kv/splits cdc/bank \
+  --local \
+  --parallelism=1 \
+  --cockroach "$BAZEL_BIN/pkg/cmd/cockroach-short/cockroach-short_/cockroach-short" \
+  --workload "$BAZEL_BIN/pkg/cmd/workload/workload_/workload" \
+  --artifacts /artifacts \
+  --teamcity
