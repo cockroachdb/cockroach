4709bc060cafb245bde69af2e304ac1c3733b87f
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-03-16 17:26:21 -0700

    sql: do not propagate contention events as processor metadata
    This commit switches the way contention events are obtained for the
    query level stats. Previously, every processor was responsible for
    accumulating the contention events it encountered and then attaching all
    of them to the `KVStats` part of the "execution metadata". This results
    in the contention events being propagated twice - once via the metadata,
    and the second time via the structured events of the trace. This commit
    makes it so that at the end of the query execution we analyze the trace
    to extract all contention events (i.e. to use the latter), so we can
    remove the former. This isn't a big deal on its own, but it seems
    cleaner this way.
    
    Epic: None
    
    Release note: None

--- pkg/sql/colflow/stats.go
+++ pkg/sql/colflow/stats.go
@@ -267,9 +267,7 @@ func (vsc *vectorizedStatsCollectorImpl) GetStats() *execinfrapb.ComponentStats 
 		s.KV.TuplesRead.Set(uint64(vsc.kvReader.GetRowsRead()))
 		s.KV.BytesRead.Set(uint64(vsc.kvReader.GetBytesRead()))
 		s.KV.BatchRequestsIssued.Set(uint64(vsc.kvReader.GetBatchRequestsIssued()))
-		totalContentionTime, events := vsc.kvReader.GetContentionInfo()
-		s.KV.ContentionTime.Set(totalContentionTime)
-		s.KV.ContentionEvents = events
+		s.KV.ContentionTime.Set(vsc.kvReader.GetContentionTime())
 		scanStats := vsc.kvReader.GetScanStats()
 		execstats.PopulateKVMVCCStats(&s.KV, &scanStats)
 		s.Exec.ConsumedRU.Set(scanStats.ConsumedRU)
