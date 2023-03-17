02ea66db12e8485396177f7aca1f6c844300cef5
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-03-17 15:05:32 -0700

    execinfra: plumb FlowCtx and processorID into ProcessorSpan method
    This commit is mostly a mechanical change to plumb `FlowCtx` object as
    well as the processor ID into `ProcessorSpan` method. The main goal of
    this work is to be able to know whether the method is called on the
    gateway node or not (this will be used by the follow-up commit), but
    this change also results in unification of how FlowID and ProcessoID
    tags are set on the tracing span.
    
    Release note: None

--- pkg/sql/colexec/ordered_synchronizer_tmpl.go
+++ pkg/sql/colexec/ordered_synchronizer_tmpl.go
@@ -57,7 +57,9 @@ const _TYPE_WIDTH = 0
 // stream are assumed to be ordered according to the same set of columns.
 type OrderedSynchronizer struct {
 	colexecop.InitHelper
-	span *tracing.Span
+	flowCtx     *execinfra.FlowCtx
+	processorID int32
+	span        *tracing.Span
 
 	accountingHelper      colmem.SetAccountingHelper
 	inputs                []colexecargs.OpWithMetaInfo
@@ -107,6 +109,8 @@ func (o *OrderedSynchronizer) Child(nth int, verbose bool) execopnode.OpNode {
 // - tuplesToMerge, if positive, indicates the total number of tuples that will
 // be emitted by all inputs, use 0 if unknown.
 func NewOrderedSynchronizer(
+	flowCtx *execinfra.FlowCtx,
+	processorID int32,
 	allocator *colmem.Allocator,
 	memoryLimit int64,
 	inputs []colexecargs.OpWithMetaInfo,
@@ -115,6 +119,8 @@ func NewOrderedSynchronizer(
 	tuplesToMerge int64,
 ) *OrderedSynchronizer {
 	os := &OrderedSynchronizer{
+		flowCtx:               flowCtx,
+		processorID:           processorID,
 		inputs:                inputs,
 		ordering:              ordering,
 		typs:                  typs,
@@ -232,7 +238,7 @@ func (o *OrderedSynchronizer) Init(ctx context.Context) {
 	if !o.InitHelper.Init(ctx) {
 		return
 	}
-	o.Ctx, o.span = execinfra.ProcessorSpan(o.Ctx, "ordered sync")
+	o.Ctx, o.span = execinfra.ProcessorSpan(o.Ctx, o.flowCtx, "ordered sync", o.processorID)
 	o.inputIndices = make([]int, len(o.inputs))
 	for i := range o.inputs {
 		o.inputs[i].Root.Init(o.Ctx)
@@ -252,7 +258,7 @@ func (o *OrderedSynchronizer) DrainMeta() []execinfrapb.ProducerMetadata {
 				o.span.RecordStructured(stats.GetStats())
 			}
 		}
-		if meta := execinfra.GetTraceDataAsMetadata(o.span); meta != nil {
+		if meta := execinfra.GetTraceDataAsMetadata(o.flowCtx, o.span); meta != nil {
 			bufferedMeta = append(bufferedMeta, *meta)
 		}
 	}
