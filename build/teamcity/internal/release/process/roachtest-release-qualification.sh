5f505893f2167f78b5902a94b4fb5a1bb513b64b
Rail Aliiev <rail@iqchoice.com>
2023-03-17 13:31:46 -0400

    roachtest: GCE/AWS nightly and release roachtest for FIPS
    Previously, roachtest nightly tests were limited to run on regular VMs.
    In order to check FIPS functionality, we need to run the tests on
    FIPS-enabled VMs.
    
    This PR adds a flag in order to select FIPS-enabled GCE or AWS VMs.
    
    * Added `--fips` flag.
    * Added wrapper scripts to selectively run FIPS-enabled tests.
    * Updated the AWS configs to have an alternative AMIs for FIPS.
    
    Epic: DEVINF-478
    Release note: None

--- build/teamcity/internal/release/process/roachtest-release-qualification.sh
+++ build/teamcity/internal/release/process/roachtest-release-qualification.sh
@@ -24,16 +24,26 @@ fi
 artifacts=$PWD/artifacts/$(date +"%%Y%%m%%d")-${TC_BUILD_ID}
 mkdir -p "$artifacts"
 
+if [[ ${FIPS_ENABLED:-0} == 1 ]]; then
+  platform=crosslinuxfips
+  tarball_platform="linux-amd64-fips"
+  fips_flag="--fips"
+else
+  platform=crosslinux
+  tarball_platform="linux-amd64"
+  fips_flag=""
+fi
+
 release_version=$(echo $TC_BUILD_BRANCH | sed -e 's/provisional_[[:digit:]]*_//')
-curl -f -s -S -o- "https://storage.googleapis.com/cockroach-builds-artifacts-prod/cockroach-${release_version}.linux-amd64.tgz" | tar ixfz - --strip-components 1
+curl -f -s -S -o- "https://storage.googleapis.com/cockroach-builds-artifacts-prod/cockroach-${release_version}.${tarball_platform}.tgz" | tar ixfz - --strip-components 1
 chmod +x cockroach
 
-run_bazel <<'EOF'
-bazel build --config ci --config crosslinux //pkg/cmd/workload //pkg/cmd/roachtest //pkg/cmd/roachprod
-BAZEL_BIN=$(bazel info bazel-bin --config ci --config crosslinux)
-cp $BAZEL_BIN/pkg/cmd/roachprod/roachprod_/roachprod bin
-cp $BAZEL_BIN/pkg/cmd/roachtest/roachtest_/roachtest bin
-cp $BAZEL_BIN/pkg/cmd/workload/workload_/workload    bin
+run_bazel <<EOF
+bazel build --config ci --config $platform //pkg/cmd/workload //pkg/cmd/roachtest //pkg/cmd/roachprod
+BAZEL_BIN=$(bazel info bazel-bin --config ci --config $platform)
+cp \$BAZEL_BIN/pkg/cmd/roachprod/roachprod_/roachprod bin
+cp \$BAZEL_BIN/pkg/cmd/roachtest/roachtest_/roachtest bin
+cp \$BAZEL_BIN/pkg/cmd/workload/workload_/workload    bin
 chmod a+w bin/roachprod bin/roachtest bin/workload
 EOF
 
@@ -55,4 +65,5 @@ timeout -s INT $((7800*60)) bin/roachtest run \
   --workload "$PWD/bin/workload" \
   --artifacts "$artifacts" \
   --parallelism 5 \
+  $fips_flag \
   --teamcity
