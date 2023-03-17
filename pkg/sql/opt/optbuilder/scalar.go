a8a3b75ba509a63192e7fdd9e65621f210baf326
Marcus Gartner <marcus@cockroachlabs.com>
2023-03-17 14:59:04 -0400

    sql: support array-flatten subqueries within UDFs
    Array-flatten subqueries (e.g., `ARRAY(SELECT a FROM t)`) are now
    supported within UDFs. They are now converted to a normal subquery with
    a ScalarGroupBy if they exist within a UDF, even if they are
    uncorrelated. This allows them to be executed without any changes to the
    execbuilder or the evaluation logic of `tree.Routine`.
    
    Fixes #98738
    
    Release note: None

--- pkg/sql/opt/optbuilder/scalar.go
+++ pkg/sql/opt/optbuilder/scalar.go
@@ -149,14 +149,7 @@ func (b *Builder) buildScalar(
 
 		inCol := s.cols[0].id
 
-		// This looks kind of arbitrary and strange, because it is:
-		// We cannot array_agg over some types, but we can only decorrelate via array_agg.
-		// Thus, we reject a query that is correlated and over a type that we can't array_agg.
 		typ := b.factory.Metadata().ColumnMeta(inCol).Type
-		if !s.outerCols.Empty() && !memo.AggregateOverloadExists(opt.ArrayAggOp, typ) {
-			panic(unimplementedWithIssueDetailf(35710, "", "can't execute a correlated ARRAY(...) over %s", typ))
-		}
-
 		if err := types.CheckArrayElementType(typ); err != nil {
 			panic(err)
 		}
@@ -169,6 +162,7 @@ func (b *Builder) buildScalar(
 			OriginalExpr: s.Subquery,
 			Ordering:     s.ordering,
 			RequestedCol: inCol,
+			WithinUDF:    b.insideUDF,
 		}
 		out = b.factory.ConstructArrayFlatten(s.node, &subqueryPrivate)
 
