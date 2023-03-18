b5da738bc2c3c941dc986b8e50c530130ec618d6
adityamaru <adityamaru@gmail.com>
2023-03-18 18:01:25 -0400

    roachtest: add `unsafe_restore_incompatible_version` to roachtests
    Some roachtests rely on fixtures generated before 22.2. To restore
    such fixtures that are outside our compatibility window we must run
    the restore with `unsafe_restore_incompatible_version`.
    
    Fixes: #98918
    
    Release note: None

--- pkg/cmd/roachtest/tests/tpc_utils.go
+++ pkg/cmd/roachtest/tests/tpc_utils.go
@@ -107,7 +107,7 @@ func loadTPCHDataset(
 	if _, err := db.ExecContext(ctx, `CREATE DATABASE IF NOT EXISTS tpch;`); err != nil {
 		return err
 	}
-	query := fmt.Sprintf(`RESTORE tpch.* FROM '%s' WITH into_db = 'tpch';`, tpchURL)
+	query := fmt.Sprintf(`RESTORE tpch.* FROM '%s' WITH into_db = 'tpch', unsafe_restore_incompatible_version;`, tpchURL)
 	_, err = db.ExecContext(ctx, query)
 	return err
 }
