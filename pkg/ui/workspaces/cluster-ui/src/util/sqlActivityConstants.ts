e2e6bd79eb74e931c2c53f00e396f0fca2853bab
Xin Hao Zhang <xzhang@cockroachlabs.com>
2023-03-21 16:52:08 -0400

    ui: change default sort to % of All Runtime for stmts
    Previously, the default sort sent to server for stmts was
    service lat. This commit changes it to '% Of All Runtime'
    since that is a more widely used option. Note that txns
    does not contain this column at the moment and keeps service
    lat as the default sort.
    
    We also now display the sort options in alphabetical order.
    
    Epic: none
    
    Release note (ui change): The default request sort for stmt
    fingerprint overview page is now '% of All Runtime'

--- pkg/ui/workspaces/cluster-ui/src/util/sqlActivityConstants.ts
+++ pkg/ui/workspaces/cluster-ui/src/util/sqlActivityConstants.ts
@@ -37,12 +37,16 @@ export function getSortLabel(sort: SqlStatsSortType): string {
   }
 }
 
-export const stmtRequestSortOptions = Object.values(SqlStatsSortOptions).map(
-  sortVal => ({
+export const stmtRequestSortOptions = Object.values(SqlStatsSortOptions)
+  .map(sortVal => ({
     value: sortVal as SqlStatsSortType,
     label: getSortLabel(sortVal as SqlStatsSortType),
-  }),
-);
+  }))
+  .sort((a, b) => {
+    if (a.label < b.label) return -1;
+    if (a.label > b.label) return 1;
+    return 0;
+  });
 
 export const txnRequestSortOptions = stmtRequestSortOptions.filter(
   option =>
