44e66d01cd6e967970a6b34bbad2f679de203700
Michael Butler <butler@cockroachlabs.com>
2023-03-22 17:41:27 +0000

    backupccl: scrub backup stmt from return errors
    This patch ensures that no unsanitized uris or secret keys get written to the
    jobs table if the backup fails.
    
    Informs #99145
    
    Release note: None

--- pkg/ccl/backupccl/schedule_exec.go
+++ pkg/ccl/backupccl/schedule_exec.go
@@ -147,10 +147,10 @@ func planBackup(
 ) (sql.PlanHookRowFn, error) {
 	fn, cols, _, _, err := backupPlanHook(ctx, backupStmt, p)
 	if err != nil {
-		return nil, errors.Wrapf(err, "backup eval: %q", tree.AsString(backupStmt))
+		return nil, errors.Wrapf(err, "failed to evaluate backup stmt")
 	}
 	if fn == nil {
-		return nil, errors.Newf("backup eval: %q", tree.AsString(backupStmt))
+		return nil, errors.Newf("failed to evaluate backup stmt")
 	}
 	if len(cols) != len(jobs.DetachedJobExecutionResultHeader) {
 		return nil, errors.Newf("unexpected result columns")
