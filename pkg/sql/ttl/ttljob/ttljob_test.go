11ab9e4e084fbb1ad82373ae1ac960440e9f02da
Shivam Saraf <shivam.saraf@cockroachlabs.com>
2023-03-23 17:17:44 -0400

    Fixing TestRowLevelTTLJobRandomEntries

--- pkg/sql/ttl/ttljob/ttljob_test.go
+++ pkg/sql/ttl/ttljob/ttljob_test.go
@@ -578,7 +578,8 @@ func TestRowLevelTTLJobRandomEntries(t *testing.T) {
 		// TODO(#76419): DateFamily has a broken `-infinity` case.
 		// TODO(#99432): JsonFamily has broken cases. This is because the test is wrapping JSON
 		// objects in multiple single quotes which causes parsing errors.
-		if colinfo.ColumnTypeIsIndexable(typ) && typ.Family() != types.DateFamily {
+		if colinfo.ColumnTypeIsIndexable(typ) && typ.Family() != types.DateFamily &&
+			typ.Family() != types.JsonFamily {
 			indexableTyps = append(indexableTyps, typ)
 		}
 	}
