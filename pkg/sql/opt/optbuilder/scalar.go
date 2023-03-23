44f57e2db8d8682189f8b79dc202030d856a9097
Drew Kimball <drewk@cockroachlabs.com>
2023-03-22 18:07:57 -0700

    opt: check UDF overloading a builtin invalidates the query cache
    It is possible to define a user-defined function with the same signature
    as a builtin function. Normally, an unqualified function call will resolve
    to the builtin function because its schema will be first in the search path.
    However, it is possible to modify the search path, so that the same function
    call can resolve to different functions on different executions. Example:
    ```
    CREATE FUNCTION public.abs(val INT) RETURNS INT CALLED ON NULL INPUT LANGUAGE SQL AS $$ SELECT val+100 $$;
    SELECT abs(1); --This should resolve to the builtin abs().
    SET search_path = public, pg_catalog;
    SELECT abs(1); --This should resolve to the udf abs().
    ```
    
    Fixes #97757
    
    Release note (bug fix): Fixed a bug existing from when user-defined
    functions were introduced that could cause a function call to resolve
    to the wrong function after changes to the schema search path.

--- pkg/sql/opt/optbuilder/scalar.go
+++ pkg/sql/opt/optbuilder/scalar.go
@@ -542,6 +542,7 @@ func (b *Builder) buildFunction(
 	if overload.IsUDF {
 		return b.buildUDF(f, def, inScope, outScope, outCol, colRefs)
 	}
+	b.factory.Metadata().AddBuiltin(f.Func.ReferenceByName)
 
 	if overload.Class == tree.AggregateClass {
 		panic(errors.AssertionFailedf("aggregate function should have been replaced"))
