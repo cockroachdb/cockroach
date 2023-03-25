317a9d84c7f8a570a75e2ea30a41423549009446
Renato Costa <renato@cockroachlabs.com>
2023-03-25 10:47:41 -0400

    roachtest: allow callers to cancel background steps in mixedversion
    This commit updates the signature of functions in the `mixedversion`
    package that create steps that run in the background; this includes
    functions in the `Helper` struct, as well as in the main
    `mixedversion.Test` struct. These functions now all return a
    `mixedversion.StopFunc` that test authors can call when they wish to
    stop a background function without causing the test to fail.
    
    When the stop function is called, the context passed to the background
    functions is canceled; however, that context cancelation is captured
    by the framework and logged as an expected termination. If the context
    is canceled through other means, the test will fail as usual.
    
    Epic: CRDB-19321
    
    Release note: None

--- pkg/cmd/roachtest/tests/versionupgrade.go
+++ pkg/cmd/roachtest/tests/versionupgrade.go
@@ -282,7 +282,9 @@ func uploadAndStartFromCheckpointFixture(nodes option.NodeListOption, v string) 
 		startOpts := option.DefaultStartOpts()
 		// NB: can't start sequentially since cluster already bootstrapped.
 		startOpts.RoachprodOpts.Sequential = false
-		clusterupgrade.StartWithBinary(ctx, t.L(), u.c, nodes, binary, startOpts)
+		if err := clusterupgrade.StartWithBinary(ctx, t.L(), u.c, nodes, binary, startOpts); err != nil {
+			t.Fatal(err)
+		}
 	}
 }
 
@@ -292,7 +294,9 @@ func uploadAndStart(nodes option.NodeListOption, v string) versionStep {
 		startOpts := option.DefaultStartOpts()
 		// NB: can't start sequentially since cluster already bootstrapped.
 		startOpts.RoachprodOpts.Sequential = false
-		clusterupgrade.StartWithBinary(ctx, t.L(), u.c, nodes, binary, startOpts)
+		if err := clusterupgrade.StartWithBinary(ctx, t.L(), u.c, nodes, binary, startOpts); err != nil {
+			t.Fatal(err)
+		}
 	}
 }
 
