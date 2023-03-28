c072906e8bbe05cd284c723cb1517dee4e5b0de6
adityamaru <adityamaru@gmail.com>
2023-03-28 14:46:18 -0400

    jobs: update jobs and job_info join queries
    This change updates the two places where we perform
    a join over the `system.jobs` and `system.job_info`
    table to use more performant and correct queries.
    
    The queries are more performant because of an additional
    predicate on the `job_id` in each of the CTE queries. This
    prevents a full table scan when we are only concerned with
    rows corresponding to a particular job. This has been verified
    by running both variants of the query through the EXPLAIN ANALYZE (plan)
    tool. This tool occassionally reports a full table scan in the CTE queries
    when there is no predicate involved.
    
    The queries are more correct because they take into consideration
    a job having >1 row for a particular info_key. This is possible
    during upgrades. Jobs start writing their payload and progress to
    the job_info table once V23_1CreateSystemJobInfoTable is active.
    If a job is still running when the cluster steps through
    V23_1JobInfoTableIsBackfilled then the job will have duplicate entries
    for the payload and prgress rows. This is because we blindly backfill
    the contents of the jobs table into the job_info table without
    clearing previous entries corresponding to the job.
    
    This change also adds a red-green regression test for the above
    correctness problem.
    
    Fixes: #99101
    Release note: None

--- pkg/jobs/update.go
+++ pkg/jobs/update.go
@@ -379,8 +379,8 @@ func getSelectStmtForJobUpdate(
 	const (
 		selectWithoutSession = `
 WITH
-	latestpayload AS (SELECT job_id, value FROM system.job_info AS payload WHERE info_key = 'legacy_payload'::BYTES),
-	latestprogress AS (SELECT job_id, value FROM system.job_info AS progress WHERE info_key = 'legacy_progress'::BYTES)
+	latestpayload AS (SELECT job_id, value FROM system.job_info AS payload WHERE info_key = 'legacy_payload'::BYTES AND job_id = $1 ORDER BY written DESC LIMIT 1),
+	latestprogress AS (SELECT job_id, value FROM system.job_info AS progress WHERE info_key = 'legacy_progress'::BYTES AND job_id = $1 ORDER BY written DESC LIMIT 1)
 	SELECT
 		status, payload.value AS payload, progress.value AS progress`
 		selectWithSession = selectWithoutSession + `, claim_session_id`
