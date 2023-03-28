5c7c63383de8f50d4893e70549d3369560b9345f
Tobias Grieger <tobias.b.grieger@gmail.com>
2023-03-28 11:48:36 +0200

    raftentry: add raft.entrycache.read_bytes
    This is counter tracking the bytes returned from the entry cache (via the `Get` and `Scan` methods).
    
    Epic: none
    Release note: None

--- pkg/ts/catalog/chart_catalog.go
+++ pkg/ts/catalog/chart_catalog.go
@@ -3988,6 +3988,7 @@ var charts = []sectionDescription{
 				Title: "raft.Storage.Engine Read bytes",
 				Metrics: []string{
 					"raft.storage.read_bytes",
+					"raft.entrycache.read_bytes",
 				},
 			},
 		},
