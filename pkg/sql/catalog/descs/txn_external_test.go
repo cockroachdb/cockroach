17a3e37fb380582157d6f4a0f736244a928a4397
ajwerner <awerner32@gmail.com>
2023-03-28 00:46:19 -0400

    sql: make deadline errors retryable
    If a transaction gets finds itself with a deadline before its current
    timestamp, something that can happen when there are schema changes or
    when the sqlliveness subsystem is unavailable, we ought to retry the
    transaction. Today, an assertion failure is sent. This is the wrong
    behavior.
    
    In order to achieve the desired goal, we detect the scenario in
    MaybeUpdateDeadline and we return an error which implements the
    appropriate interface to be interpreted as a retry error in the
    pgwire and sql layers.
    
    This change does a few other little things:
    
    1) It simplies the logic to check whether an error is retriable so that
       all errors which pgwire will treat as client-visible retries are treated
       as retriable internally. In addition, we retain certain additional checks
       for retriable errors the sql layer handles explicitly.
    2) It makes sure to reset the sqlliveness session in the descs.Collection
       when restarting transactions. Not doing this, on the one hand, was an
       oversight that meant that if the session turned over internally, then
       restarts wouldn't see it. However, it's not actually a bug today because
       only secondary tenants set the session, and they never acquire a new
       session.
    
    The test utilizes sessions rather than descriptors because the testing
    knob infrastructure is easier to manipulate.
    
    Fixes: #96336
    
    Backports will deal with #76727
    
    Release note (bug fix): In rare cases involving overload and schema changes,
    users could sometimes, transiently, see errors of the form "deadline below
    read timestamp is nonsensical; txn has would have no chance to commit". These
    errors carried and internal pgcode and could not be retried. This form of error
    is now classified as a retriable error and will be retried automatically either
    by the client or internally.

--- pkg/sql/catalog/descs/txn_external_test.go
+++ pkg/sql/catalog/descs/txn_external_test.go
@@ -12,15 +12,25 @@ package descs_test
 
 import (
 	"context"
+	gosql "database/sql"
+	"math/rand"
 	"testing"
 
+	"github.com/cockroachdb/cockroach-go/v2/crdb"
 	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descs"
 	"github.com/cockroachdb/cockroach/pkg/sql/isql"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlliveness"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlliveness/sqllivenesstestutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/errors"
+	"github.com/cockroachdb/logtags"
 	"github.com/stretchr/testify/require"
 )
 
@@ -68,4 +78,90 @@ func TestTxnWithStepping(t *testing.T) {
 		}
 		return nil
 	}, isql.SteppingEnabled()))
+}
+
+// TestLivenessSessionExpiredErrorResultsInRestartAtSQLLayer ensures that if
+// a transaction sees a deadline which is in the past of the current
+// transaction timestamp, it'll propagate a retriable error. This should only
+// happen in transient situations.
+//
+// Note that the sqlliveness session is not the only source of a deadline. In
+// fact, it's probably more common that schema changes lead to descriptor
+// deadlines kicking in. By default, at time of writing, sqlliveness sessions
+// only apply to secondary tenants. The test leverages them because they are
+// the easiest way to interact with the deadline at a session level.
+func TestLivenessSessionExpiredErrorResultsInRestartAtSQLLayer(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	ctx := context.Background()
+	var (
+		toFail = struct {
+			syncutil.Mutex
+			remaining int
+		}{}
+		shouldFail = func() bool {
+			toFail.Lock()
+			defer toFail.Unlock()
+			if toFail.remaining > 0 {
+				toFail.remaining--
+				return true
+			}
+			return false
+		}
+		checkFailed = func(t *testing.T) {
+			toFail.Lock()
+			defer toFail.Unlock()
+			require.Zero(t, toFail.remaining)
+		}
+		setRemaining = func(t *testing.T, n int) {
+			toFail.Lock()
+			defer toFail.Unlock()
+			toFail.remaining = n
+		}
+	)
+	s, sqlDB, _ := serverutils.StartServer(t, base.TestServerArgs{
+		Knobs: base.TestingKnobs{
+			SQLExecutor: &sql.ExecutorTestingKnobs{
+				ForceSQLLivenessSession: true,
+			},
+			SQLLivenessKnobs: &sqlliveness.TestingKnobs{
+				SessionOverride: func(ctx context.Context) (sqlliveness.Session, error) {
+					// Only client sessions have the client tag. We control the only
+					// client session.
+					tags := logtags.FromContext(ctx)
+					if _, hasClient := tags.GetTag("client"); hasClient && shouldFail() {
+						// This fake session is certainly expired.
+						return &sqllivenesstestutils.FakeSession{
+							ExpTS: hlc.Timestamp{WallTime: 1},
+						}, nil
+					}
+					return nil, nil
+				},
+			},
+		},
+	})
+	defer s.Stopper().Stop(ctx)
+
+	tdb := sqlutils.MakeSQLRunner(sqlDB)
+	tdb.Exec(t, "CREATE TABLE t (i INT PRIMARY KEY)")
+
+	// Ensure that the internal retry works seamlessly
+	t.Run("auto-retry", func(t *testing.T) {
+		setRemaining(t, rand.Intn(20))
+		tdb.Exec(t, `SELECT * FROM t`)
+		checkFailed(t)
+	})
+	t.Run("explicit transaction", func(t *testing.T) {
+		setRemaining(t, rand.Intn(20))
+		require.NoError(t, crdb.ExecuteTx(ctx, sqlDB, nil, func(tx *gosql.Tx) error {
+			_, err := tx.Exec("SELECT * FROM t")
+			if err != nil {
+				return err
+			}
+			_, err = tx.Exec(`INSERT INTO t VALUES (1)`)
+			return err
+		}))
+		checkFailed(t)
+	})
 }
