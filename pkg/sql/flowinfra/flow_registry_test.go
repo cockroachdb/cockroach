cockroachdb/cockroach

commit f4bcebd2bd62671946d490a1573a50af88cc04c8

flowinfra: fix a couple of rare bugs with flow cleanup
This commit fixes a couple of rare bugs around the flow cleanup that
were encountered when running `tpch_concurrency` roachtest at the
concurrency of 1000 or so (i.e. for the bugs to reproduce we needed
extremely overloaded nodes).

The setup for the bugs is as follows:
- we have some number of inbound streams on the node. The inbound
streams in this context are the server side of the `FlowStream` RPCs
(i.e. the inbox side)
- each inbound stream needs a separate goroutine (created by the gRPC
framework), and these goroutines are tracked against the wait group of
the flow
- the main goroutine of the flow registers the flow with the
`FlowRegistry` and gives it a timeout (10 seconds by default) to let the
inbound streams to arrive
- we have synchronization in place to block the inbound streams until
the flow is registered
- once the inbound stream finds its flow in the registry, it sends
a handshake RPC to the producer (i.e. to the outbox).

The first bug is about not decrementing the wait group of the flow in
some cases. In particular, if the inbound stream timeout occurs _while_
the inbound stream is performing the "handshake" RPC _and_ that RPC
results in an error, then the wait group wouldn't be decremented. This
was the case because
1. the "timeout" goroutine would observe that the stream is "connected",
so it would treat the timeout cancellation as a no-op
2. the inbound stream goroutine would get an error on the handshake, so
it would bubble the error up but would never call
`InboundStreamInfo.onFinish`.
This bug is now fixed by only marking the stream as "connected" once the
handshake succeeds. This bug was introduced about a year ago in 314d0fd0e31a9d61d5a83bc3663ccb2c364dd12a
when we refactored the locking around the flow registry.

The second bug is related. Previously, if `ConnectInboundStream`
resulted in an error, this could leave one of the inbox goroutines

pkg/sql/flowinfra/flow_registry.go
@@ -90,31 +90,6 @@ func NewInboundStreamInfo(
 	}
 }
 
-// connect marks s as connected. It is an error if s was already marked either
-// as connected or canceled. It should be called without holding any mutexes.
-func (s *InboundStreamInfo) connect(
-	flowID execinfrapb.FlowID, streamID execinfrapb.StreamID,
-) error {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	if s.mu.connected {
-		return errors.Errorf("flow %s: inbound stream %d already connected", flowID, streamID)
-	}
-	if s.mu.canceled {
-		return errors.Errorf("flow %s: inbound stream %d came too late", flowID, streamID)
-	}
-	s.mu.connected = true
-	return nil
-}
-
-// disconnect marks s as not connected. It should be called without holding any
-// mutexes.
-func (s *InboundStreamInfo) disconnect() {
-	s.mu.Lock()
-	s.mu.connected = false
-	s.mu.Unlock()
-}
-
 // finishLocked marks s as finished and calls onFinish. The mutex of s must be
 // held when calling this method.
 func (s *InboundStreamInfo) finishLocked() {
@@ -124,7 +99,6 @@ func (s *InboundStreamInfo) finishLocked() {
 	if s.mu.finished {
 		panic("double finish")
 	}
-
 	s.mu.finished = true
 	s.onFinish()
 }
@@ -484,11 +458,10 @@ func (fr *FlowRegistry) Drain(
 		}
 		// Now cancel all still running flows.
 		for _, f := range fr.flows {
-			if f.flow != nil && f.flow.ctxCancel != nil {
+			if f.flow != nil {
 				// f.flow might be nil when ConnectInboundStream() was
 				// called, but the consumer of that inbound stream hasn't
 				// been scheduled yet.
-				// f.flow.ctxCancel might be nil in tests.
 				f.flow.ctxCancel()
 			}
 		}
@@ -571,7 +544,7 @@ func (fr *FlowRegistry) ConnectInboundStream(
 	streamID execinfrapb.StreamID,
 	stream execinfrapb.DistSQL_FlowStreamServer,
 	timeout time.Duration,
-) (*FlowBase, InboundStreamHandler, func(), error) {
+) (_ *FlowBase, _ InboundStreamHandler, cleanup func(), retErr error) {
 	fr.Lock()
 	entry := fr.getEntryLocked(flowID)
 	flow := entry.flow
@@ -604,30 +577,52 @@ func (fr *FlowRegistry) ConnectInboundStream(
 		}
 	}
 
+	defer func() {
+		if retErr != nil {
+			// If any error is encountered below, we know that the distributed
+			// query execution will fail, so we cancel the flow on this node. If
+			// this node is the gateway, this might actually be required for
+			// proper shutdown of the whole distributed plan.
+			flow.ctxCancel()
+		}
+	}()
+
 	// entry.inboundStreams is safe to access without holding the mutex since
 	// the map is not modified after Flow.Setup.
 	s, ok := entry.inboundStreams[streamID]
 	if !ok {
 		return nil, nil, nil, errors.Errorf("flow %s: no inbound stream %d", flowID, streamID)
 	}
-	// We now mark the stream as connected but, if an error happens later
-	// because the handshake fails, we reset the state; we want the stream to be
-	// considered timed out when the moment comes just as if this connection
-	// attempt never happened.
-	if err := s.connect(flowID, streamID); err != nil {
-		return nil, nil, nil, err
-	}
 
-	if err := stream.Send(&execinfrapb.ConsumerSignal{
+	// Don't mark s as connected until after the handshake succeeds.
+	handshakeErr := stream.Send(&execinfrapb.ConsumerSignal{
 		Handshake: &execinfrapb.ConsumerHandshake{
 			ConsumerScheduled:  true,
 			Version:            execinfra.Version,
 			MinAcceptedVersion: execinfra.MinAcceptedVersion,
 		},
-	}); err != nil {
-		s.disconnect()
-		return nil, nil, nil, err
-	}
+	})
 
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if s.mu.canceled {
+		// Regardless of whether the handshake succeeded or not, this inbound
+		// stream has already been canceled and properly finished.
+		return nil, nil, nil, errors.Errorf("flow %s: inbound stream %d came too late", flowID, streamID)
+	}
+	if handshakeErr != nil {
+		// The handshake failed, so we're canceling this stream.
+		s.mu.canceled = true
+		s.finishLocked()
+		return nil, nil, nil, handshakeErr
+	}
+	if s.mu.connected {
+		// This is unexpected - the FlowStream RPC was issued twice by the
+		// outboxes for the same stream. We are processing the second RPC call
+		// right now, so there is another goroutine that will handle the
+		// cleanup, so we defer the cleanup to that goroutine.
+		return nil, nil, nil, errors.AssertionFailedf("flow %s: inbound stream %d already connected", flowID, streamID)
+	}
+	s.mu.connected = true
 	return flow, s.receiver, s.finish, nil
 }

pkg/sql/flowinfra/flow_registry_test.go
@@ -212,7 +212,7 @@ func TestStreamConnectionTimeout(t *testing.T) {
 	// Register a flow with a very low timeout. After it times out, we'll attempt
 	// to connect a stream, but it'll be too late.
 	id1 := execinfrapb.FlowID{UUID: uuid.MakeV4()}
-	f1 := &FlowBase{}
+	f1 := &FlowBase{ctxCancel: func() {}}
 	streamID1 := execinfrapb.StreamID(1)
 	consumer := &distsqlutils.RowBuffer{}
 	wg := &sync.WaitGroup{}
@@ -370,7 +370,7 @@ func TestFlowRegistryDrain(t *testing.T) {
 	ctx := context.Background()
 	reg := NewFlowRegistry()
 
-	flow := &FlowBase{}
+	flow := &FlowBase{ctxCancel: func() {}}
 	id := execinfrapb.FlowID{UUID: uuid.MakeV4()}
 	registerFlow := func(t *testing.T, id execinfrapb.FlowID) {
 		t.Helper()
@@ -653,5 +653,128 @@ func TestFlowCancelPartiallyBlocked(t *testing.T) {
 	_, meta = left.Next()
 	if !errors.Is(meta.Err, cancelchecker.QueryCanceledError) {
 		t.Fatal("expected query canceled, found", meta.Err)
+	}
+}
+
+// delayedErrorServerStream is a light wrapper around the server stream that
+// allows to block (on delayCh) Send() calls which always result in the
+// provided error.
+type delayedErrorServerStream struct {
+	execinfrapb.DistSQL_FlowStreamServer
+	// rpcCalledCh is sent on in the very beginning of every Send() call.
+	rpcCalledCh chan<- struct{}
+	delayCh     <-chan struct{}
+	err         error
+}
+
+func (s *delayedErrorServerStream) Send(*execinfrapb.ConsumerSignal) error {
+	s.rpcCalledCh <- struct{}{}
+	<-s.delayCh
+	return s.err
+}
+
+// TestErrorOnSlowHandshake is a regression test for #94113. In particular, it
+// verifies that the flow infra is shut down correctly when the inbound stream
+// times out while the handshake message is being sent to the producer and later
+// results in an error.
+//
+// Here is a more detailed sequence of events:
+//   - the flow, which has a single inbound stream, is registered with the flow
+//     registry; the flow is given some timeout (2s in the test) for that
+//     inbound stream to arrive
+//   - the inbound stream arrives, finds the flow in the registry and sends a
+//     handshake message
+//   - for whatever reason this "handshake" RPC is slow (in the test it is
+//     blocked, but in production it could be due to overloaded node), so the
+//     inbound stream timeout is reached
+//   - that timeout "cancels" the single pending flow; this cancellation results
+//     in the flow being marked as "canceled" and the wait group being
+//     decremented (as well as calling Timeout() on the receiver)
+//   - after the flow cancellation is performed, the "handshake" RPC results in
+//     an error which results in flow being properly canceled (by calling
+//     FlowBase.ctxCancel).
+//
+// Before #94113 was fixed, the flow would be incorrectly marked as "connected"
+// before the "handshake" RPC was issued, so the inbound stream timeout would
+// not actually call Timeout() on the receiver and the wait group would never be
+// decremented. Also, the flow wouldn't be properly canceled either, so this
+// setup could result in a deadlock.
+func TestErrorOnSlowHandshake(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	reg := NewFlowRegistry()
+	flowID := execinfrapb.FlowID{UUID: uuid.MakeV4()}
+	streamID := execinfrapb.StreamID(1)
+	cancelCh := make(chan struct{})
+	f := &FlowBase{ctxCancel: func() {
+		cancelCh <- struct{}{}
+	}}
+
+	serverStream, _ /* clientStream */, cleanup := createDummyStream(t)
+	defer cleanup()
+
+	rpcCalledCh, delayCh := make(chan struct{}), make(chan struct{})
+	serverStream = &delayedErrorServerStream{
+		DistSQL_FlowStreamServer: serverStream,
+		rpcCalledCh:              rpcCalledCh,
+		delayCh:                  delayCh,
+		err:                      errors.New("dummy error"),
+	}
+
+	var wg sync.WaitGroup
+	streamInfo := &InboundStreamInfo{
+		receiver: RowInboundStreamHandler{&distsqlutils.RowBuffer{}, nil /* types */},
+		onFinish: wg.Done,
+	}
+	wg.Add(1)
+	inboundStreams := map[execinfrapb.StreamID]*InboundStreamInfo{
+		streamID: streamInfo,
+	}
+	inboundStreamTimeout := 2 * time.Second
+	if err := reg.RegisterFlow(
+		context.Background(), flowID, f, inboundStreams, inboundStreamTimeout,
+	); err != nil {
+		t.Fatal(err)
+	}
+
+	errCh := make(chan error, 1)
+	go func() {
+		if _, _, _, err := reg.ConnectInboundStream(
+			// timeout here doesn't matter since the flow is already present in
+			// the registry.
+			context.Background(), flowID, streamID, serverStream, 0, /* timeout */
+		); err != nil {
+			errCh <- err
+		}
+	}()
+
+	// Wait for the concurrent goroutine to issue the first RPC (which is a
+	// handshake).
+	<-rpcCalledCh
+	// Wait out the inbound stream timeout.
+	time.Sleep(inboundStreamTimeout + time.Second)
+	// At this point we expect the inbound stream is canceled by
+	// flowEntry.streamTimer, so we can unblock the handshake RPC. (Note that
+	// we added an extra second to the sleep above in order to make it more
+	// likely that the timer fired.)
+	delayCh <- struct{}{}
+
+	// Make sure that the wait group is properly decremented (this must have
+	// been done by flowEntry.streamTimer too).
+	wg.Wait()
+	// Since the RPC resulted in an error, we expect that the flow is canceled.
+	<-cancelCh
+	err := <-errCh
+	if err == nil {
+		t.Fatal("expected ConnectInboundStream to result in an error")
+	}
+	if !testutils.IsError(err, "came too late") && !testutils.IsError(err, "dummy error") {
+		// Generally speaking, we expect the "came too late" error which
+		// indicates that the inbound stream was canceled while performing the
+		// RPC. However, under stress it might be possible for the injected
+		// error to be returned. This is acceptable since the main goal of the
+		// test is to ensure that no deadlocks happen, and we don't care that
+		// much about which particular error was returned.
+		t.Fatalf("unexpected error from ConnectInboundStream: %v", err)
 	}
 }

pkg/sql/colflow/colrpc/outbox.go
@@ -204,6 +204,8 @@ func (o *Outbox) Run(
 			return err
 		}
 
+		// TODO(yuzefovich): the row-based outbox sends the header as part of
+		// the first message with data, consider doing that here too.
 		log.VEvent(ctx, 2, "Outbox sending header")
 		// Send header message to establish the remote server (consumer).
 		if err := stream.Send(
@@ -218,7 +220,9 @@ func (o *Outbox) Run(
 		}
 		return nil
 	}(); err != nil {
-		// error during stream set up.
+		// An error during stream setup - the whole query will fail, so we might
+		// as well proactively cancel the flow on this node.
+		flowCtxCancel()
 		o.close(ctx)
 		return
 	}

pkg/sql/flowinfra/outbox.go
@@ -237,25 +237,33 @@ func (m *Outbox) mainLoop(ctx context.Context, wg *sync.WaitGroup) (retErr error
 		}
 	}
 
-	conn, err := execinfra.GetConnForOutbox(
-		ctx, m.flowCtx.Cfg.PodNodeDialer, m.sqlInstanceID, SettingFlowStreamTimeout.Get(&m.flowCtx.Cfg.Settings.SV),
-	)
-	if err != nil {
-		// Log any Dial errors. This does not have a verbosity check due to being
-		// a critical part of query execution: if this step doesn't work, the
-		// receiving side might end up hanging or timing out.
-		log.Infof(ctx, "outbox: connection dial error: %+v", err)
-		return err
-	}
-	client := execinfrapb.NewDistSQLClient(conn)
-	if log.V(2) {
-		log.Infof(ctx, "outbox: calling FlowStream")
-	}
-	m.stream, err = client.FlowStream(ctx)
-	if err != nil {
-		if log.V(1) {
-			log.Infof(ctx, "FlowStream error: %s", err)
+	if err := func() error {
+		conn, err := execinfra.GetConnForOutbox(
+			ctx, m.flowCtx.Cfg.PodNodeDialer, m.sqlInstanceID, SettingFlowStreamTimeout.Get(&m.flowCtx.Cfg.Settings.SV),
+		)
+		if err != nil {
+			// Log any Dial errors. This does not have a verbosity check due to being
+			// a critical part of query execution: if this step doesn't work, the
+			// receiving side might end up hanging or timing out.
+			log.Infof(ctx, "outbox: connection dial error: %+v", err)
+			return err
 		}
+		client := execinfrapb.NewDistSQLClient(conn)
+		if log.V(2) {
+			log.Infof(ctx, "outbox: calling FlowStream")
+		}
+		m.stream, err = client.FlowStream(ctx)
+		if err != nil {
+			if log.V(1) {
+				log.Infof(ctx, "FlowStream error: %s", err)
+			}
+			return err
+		}
+		return nil
+	}(); err != nil {
+		// An error during stream setup - the whole query will fail, so we might
+		// as well proactively cancel the flow on this node.
+		m.flowCtxCancel()
 		return err
 	}
 	if log.V(2) {

commit 0ec5c9f9b45087b14e3602fc735e4a4032e39eb3

flowinfra: fix recently introduced bug with row-based flow shutdown
This commit fixes a bug that was introduced in f4bcebd2bd62671946d490a1573a50af88cc04c8
that I believe to be the root cause of "slow quiesce" CI failures we've
seen on `fakedist-vec-off` logic test config. In particular, the bug is
as follows: that commit made it so that if the handshake with the remote
node (when connecting the inbound stream) fails, we now "cancel" the
stream by marking it as "canceled" and decrementing the wait group.
However, one important piece of the proper cancellation was missed - we
also need to push an error to the receiver. We do that when the inbound
stream timeout timer fires but that commit forgot to do so on the
handshake error, and this is now fixed.

The incorrect assumption in f4bcebd2bd62671946d490a1573a50af88cc04c8 was
that canceling the context of the flow is sufficient for the proper
shutdown, but this turns out to not be the case in the row-based flows.
We have some components (e.g. `execinfra.RowChannel`) that will block
until `ProducerDone` is not called, and with just cancelling the context
that could never happen. Now that we properly push the error it will.
The vectorized flows aren't affected by this because there we do listen
for context cancellation signals.

Release note: None

pkg/sql/flowinfra/flow_registry.go
@@ -104,16 +104,28 @@ func (s *InboundStreamInfo) finishLocked() {
 }
 
 // cancelIfNotConnected cancels and finishes s if it's not marked as connected,
-// finished, or canceled, and returns the pending receiver if so.
-func (s *InboundStreamInfo) cancelIfNotConnected() InboundStreamHandler {
+// finished, or canceled, and sends the provided error to the pending receiver
+// if so. A boolean indicating whether s is canceled is returned.
+func (s *InboundStreamInfo) cancelIfNotConnected(err error) bool {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	if s.mu.connected || s.mu.finished || s.mu.canceled {
-		return nil
+		return false
 	}
+	s.cancelLocked(err)
+	return true
+}
+
+// cancelLocked marks s as canceled, finishes it, and sends the given error to
+// the receiver. The mutex of s must be held when calling this method.
+func (s *InboundStreamInfo) cancelLocked(err error) {
+	s.mu.AssertHeld()
 	s.mu.canceled = true
 	s.finishLocked()
-	return s.receiver
+	// Since Timeout might block, we must send the error in a new goroutine.
+	go func() {
+		s.receiver.Timeout(err)
+	}()
 }
 
 // finish is the same as finishLocked when the mutex of s is not held already.
@@ -344,12 +356,8 @@ func (fr *FlowRegistry) cancelPendingStreams(
 	for _, is := range inboundStreams {
 		// Connected, non-finished inbound streams will get an error returned in
 		// ProcessInboundStream(). Handle non-connected streams here.
-		pendingReceiver := is.cancelIfNotConnected()
-		if pendingReceiver != nil {
+		if is.cancelIfNotConnected(pendingReceiverErr) {
 			numTimedOutReceivers++
-			go func(receiver InboundStreamHandler) {
-				receiver.Timeout(pendingReceiverErr)
-			}(pendingReceiver)
 		}
 	}
 	return numTimedOutReceivers
@@ -612,8 +620,7 @@ func (fr *FlowRegistry) ConnectInboundStream(
 	}
 	if handshakeErr != nil {
 		// The handshake failed, so we're canceling this stream.
-		s.mu.canceled = true
-		s.finishLocked()
+		s.cancelLocked(handshakeErr)
 		return nil, nil, nil, handshakeErr
 	}
 	if s.mu.connected {

pkg/sql/flowinfra/flow_registry_test.go
@@ -721,9 +721,10 @@ func TestErrorOnSlowHandshake(t *testing.T) {
 		err:                      errors.New("dummy error"),
 	}
 
+	receiver := &distsqlutils.RowBuffer{}
 	var wg sync.WaitGroup
 	streamInfo := &InboundStreamInfo{
-		receiver: RowInboundStreamHandler{&distsqlutils.RowBuffer{}, nil /* types */},
+		receiver: RowInboundStreamHandler{receiver, nil /* types */},
 		onFinish: wg.Done,
 	}
 	wg.Add(1)
@@ -776,5 +777,12 @@ func TestErrorOnSlowHandshake(t *testing.T) {
 		// test is to ensure that no deadlocks happen, and we don't care that
 		// much about which particular error was returned.
 		t.Fatalf("unexpected error from ConnectInboundStream: %v", err)
+	}
+	// We expect that "no inbound stream connection" error is pushed into the
+	// receiver.
+	if len(receiver.Mu.Records) != 1 {
+		t.Fatalf("expected a single meta object with an error, got %v", receiver.Mu.Records)
+	} else if r := receiver.Mu.Records[0]; !IsNoInboundStreamConnectionError(r.Meta.Err) {
+		t.Fatalf("unexpected error: %v", r)
 	}
 }