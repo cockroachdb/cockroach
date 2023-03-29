655141e273cd28a69c769e74e8e50051d8435912
Faizan Qazi <faizan@cockroachlabs.com>
2023-03-28 21:50:31 -0400

    sql/schemachanger: add DML injection tests for rollback scenarios
    Previously, our DML injection tests only covered happy paths
    for schema changes. We recently ran into a bug where the old
    primary index was corrupted after DML and left in a bad state
    visible only after a rollback. To address this, this patch
    adds tests for this specific scenario and adds the ability
    to test rollbacks with DML injection.
    
    Epic: none
    
    Release note: None

--- pkg/ccl/schemachangerccl/backup_base_mixed_generated_test.go
+++ pkg/ccl/schemachangerccl/backup_base_mixed_generated_test.go
@@ -38,6 +38,16 @@ func TestBackupMixedVersionElements_base_add_column_no_default(t *testing.T) {
 	defer log.Scope(t).Close(t)
 	sctest.BackupMixedVersionElements(t, "pkg/sql/schemachanger/testdata/end_to_end/add_column_no_default", newClusterMixed)
 }
+func TestBackupMixedVersionElements_base_add_column_with_stored(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	sctest.BackupMixedVersionElements(t, "pkg/sql/schemachanger/testdata/end_to_end/add_column_with_stored", newClusterMixed)
+}
+func TestBackupMixedVersionElements_base_add_column_with_stored_family(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	sctest.BackupMixedVersionElements(t, "pkg/sql/schemachanger/testdata/end_to_end/add_column_with_stored_family", newClusterMixed)
+}
 func TestBackupMixedVersionElements_base_alter_table_add_check_udf(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
