cockroachdb/cockroach

commit 75071db8e7bdfe7b0035a5d8647ad5313d57ab8e

sql: add a framework for lazy generation of virtual table rows
This PR adds a framework for lazily generating rows for virtual
tables. It also ports some virtual table generators that loop
over all table descriptors to use this framework.

Release note: None

pkg/sql/virtual_table.go
@@ -24,20 +24,136 @@ import (
 // nil). If there is an error, then return (nil, error).
 type virtualTableGenerator func() (tree.Datums, error)
 
+// cleanupFunc is a function to cleanup resources created by the generator.
+type cleanupFunc func()
+
+// rowPusher is an interface for lazy generators to push rows into
+// and then suspend until the next row has been requested.
+type rowPusher interface {
+	// pushRow pushes the input row to the receiver of the generator. It doesn't
+	// mutate the input row. It will block until the the data has been received
+	// and more data has been requested. Once pushRow returns, the caller is free
+	// to mutate the slice passed as input. The caller is not allowed to perform
+	// operations on a transaction while blocked on a call to pushRow.
+	// If pushRow returns an error, the caller must immediately return the error.
+	pushRow(...tree.Datum) error
+}
+
+// funcRowPusher implements rowPusher on functions.
+type funcRowPusher func(...tree.Datum) error
+
+func (f funcRowPusher) pushRow(datums ...tree.Datum) error {
+	return f(datums...)
+}
+
+type virtualTableGeneratorResponse struct {
+	datums tree.Datums
+	err    error
+}
+
+// setupGenerator takes in a worker that generates rows eagerly and transforms
+// it into a lazy row generator. It returns two functions:
+// * next: A handle that can be called to generate a row from the worker. Next
+//   cannot be called once cleanup has been called.
+// * cleanup: Performs all cleanup. This function must be called exactly once
+//   to ensure that resources are cleaned up.
+func setupGenerator(
+	ctx context.Context, worker func(pusher rowPusher) error,
+) (next virtualTableGenerator, cleanup cleanupFunc) {
+	var cancel func()
+	ctx, cancel = context.WithCancel(ctx)
+	cleanup = cancel
+
+	// comm is the channel to manage communication between the row receiver
+	// and the generator. The row receiver notifies the worker to begin
+	// computation through comm, and the generator places rows to consume
+	// back into comm.
+	comm := make(chan virtualTableGeneratorResponse)
+
+	addRow := func(datums ...tree.Datum) error {
+		select {
+		case <-ctx.Done():
+			return sqlbase.QueryCanceledError
+		case comm <- virtualTableGeneratorResponse{datums: datums}:
+		}
+
+		// Block until the next call to cleanup() or next(). This allows us to
+		// avoid issues with concurrent transaction usage if the worker is using
+		// a transaction. Otherwise, worker could proceed running operations after
+		// a call to next() has returned. That could result in the main operator
+		// chain using the transaction while the worker is also running. This
+		// makes it so that the worker can only run while next() is being called,
+		// which effectively gives ownership of the transaction usage over to the
+		// worker, and then back to the next() caller after it is done.
+		select {
+		case <-ctx.Done():
+			return sqlbase.QueryCanceledError
+		case <-comm:
+		}
+		return nil
+	}
+
+	go func() {
+		// We wait until a call to next before starting the worker. This prevents
+		// concurrent transaction usage during the startup phase. We also have to
+		// wait on done here if cleanup is called before any calls to next() to
+		// avoid leaking this goroutine. Lastly, we check if the context has
+		// been canceled before any rows are even requested.
+		select {
+		case <-ctx.Done():
+			return
+		case <-comm:
+		}
+		err := worker(funcRowPusher(addRow))
+		// If the query was canceled, next() will already return a
+		// QueryCanceledError, so just exit here.
+		if err == sqlbase.QueryCanceledError {
+			return
+		}
+
+		// Notify that we are done sending rows.
+		select {
+		case <-ctx.Done():
+			return
+		case comm <- virtualTableGeneratorResponse{err: err}:
+		}
+	}()
+
+	next = func() (tree.Datums, error) {
+		// Notify the worker to begin computing a row.
+		select {
+		case comm <- virtualTableGeneratorResponse{}:
+		case <-ctx.Done():
+			return nil, sqlbase.QueryCanceledError
+		}
+
+		// Wait for the row to be sent.
+		select {
+		case <-ctx.Done():
+			return nil, sqlbase.QueryCanceledError
+		case resp := <-comm:
+			return resp.datums, resp.err
+		}
+	}
+	return next, cleanup
+}
+
 // virtualTableNode is a planNode that constructs its rows by repeatedly
 // invoking a virtualTableGenerator function.
 type virtualTableNode struct {
 	columns    sqlbase.ResultColumns
 	next       virtualTableGenerator
+	cleanup    func()
 	currentRow tree.Datums
 }
 
 func (p *planner) newContainerVirtualTableNode(
-	columns sqlbase.ResultColumns, capacity int, next virtualTableGenerator,
+	columns sqlbase.ResultColumns, capacity int, next virtualTableGenerator, cleanup func(),
 ) *virtualTableNode {
 	return &virtualTableNode{
 		columns: columns,
 		next:    next,
+		cleanup: cleanup,
 	}
 }
 
@@ -59,4 +175,7 @@ func (n *virtualTableNode) Values() tree.Datums {
 }
 
 func (n *virtualTableNode) Close(ctx context.Context) {
+	if n.cleanup != nil {
+		n.cleanup()
+	}
 }

pkg/ccl/logictestccl/testdata/logic_test/crdb_internal
@@ -153,3 +153,10 @@ RANGE default  ALTER RANGE default CONFIGURE ZONE USING
                num_replicas = 3,
                constraints = '[]',
                lease_preferences = '[]'
+
+# This test checks that generator backed tables do not concurrently
+# access transactions. It does this by scanning two virtual tables at a time.
+# If the background generator functions were performing work when not allowed
+# to, the background generators would conflict with an error.
+statement ok
+SELECT a.* FROM crdb_internal.partitions AS a JOIN crdb_internal.partitions AS b ON a.table_id = b.table_id

pkg/sql/crdb_internal.go
@@ -218,86 +218,93 @@ CREATE TABLE crdb_internal.tables (
   audit_mode               STRING NOT NULL,
   schema_name              STRING NOT NULL
 )`,
-	populate: func(ctx context.Context, p *planner, _ *DatabaseDescriptor, addRow func(...tree.Datum) error) error {
-		descs, err := p.Tables().getAllDescriptors(ctx, p.txn)
-		if err != nil {
-			return err
-		}
-		dbNames := make(map[sqlbase.ID]string)
-		// Record database descriptors for name lookups.
-		for _, desc := range descs {
-			db, ok := desc.(*sqlbase.DatabaseDescriptor)
-			if ok {
-				dbNames[db.ID] = db.Name
-			}
-		}
-
-		addDesc := func(table *sqlbase.TableDescriptor, dbName tree.Datum, scName string) error {
-			leaseNodeDatum := tree.DNull
-			leaseExpDatum := tree.DNull
-			if table.Lease != nil {
-				leaseNodeDatum = tree.NewDInt(tree.DInt(int64(table.Lease.NodeID)))
-				leaseExpDatum = tree.MakeDTimestamp(
-					timeutil.Unix(0, table.Lease.ExpirationTime), time.Nanosecond,
-				)
-			}
-			dropTimeDatum := tree.DNull
-			if table.DropTime != 0 {
-				dropTimeDatum = tree.MakeDTimestamp(
-					timeutil.Unix(0, table.DropTime), time.Nanosecond,
-				)
-			}
-			return addRow(
-				tree.NewDInt(tree.DInt(int64(table.ID))),
-				tree.NewDInt(tree.DInt(int64(table.GetParentID()))),
-				tree.NewDString(table.Name),
-				dbName,
-				tree.NewDInt(tree.DInt(int64(table.Version))),
-				tree.TimestampToInexactDTimestamp(table.ModificationTime),
-				tree.TimestampToDecimal(table.ModificationTime),
-				tree.NewDString(table.FormatVersion.String()),
-				tree.NewDString(table.State.String()),
-				leaseNodeDatum,
-				leaseExpDatum,
-				dropTimeDatum,
-				tree.NewDString(table.AuditMode.String()),
-				tree.NewDString(scName))
-		}
-
-		// Note: we do not use forEachTableDesc() here because we want to
-		// include added and dropped descriptors.
-		for _, desc := range descs {
-			table, ok := desc.(*sqlbase.TableDescriptor)
-			if !ok || p.CheckAnyPrivilege(ctx, table) != nil {
-				continue
-			}
-			dbName := dbNames[table.GetParentID()]
-			if dbName == "" {
-				// The parent database was deleted. This is possible e.g. when
-				// a database is dropped with CASCADE, and someone queries
-				// this virtual table before the dropped table descriptors are
-				// effectively deleted.
-				dbName = fmt.Sprintf("[%d]", table.GetParentID())
-			}
-			if err := addDesc(table, tree.NewDString(dbName), "public"); err != nil {
+	generator: func(ctx context.Context, p *planner, dbDesc *DatabaseDescriptor) (virtualTableGenerator, cleanupFunc, error) {
+		row := make(tree.Datums, 14)
+		worker := func(pusher rowPusher) error {
+			descs, err := p.Tables().getAllDescriptors(ctx, p.txn)
+			if err != nil {
 				return err
 			}
-		}
+			dbNames := make(map[sqlbase.ID]string)
+			// Record database descriptors for name lookups.
+			for _, desc := range descs {
+				db, ok := desc.(*sqlbase.DatabaseDescriptor)
+				if ok {
+					dbNames[db.ID] = db.Name
+				}
+			}
 
-		// Also add all the virtual descriptors.
-		vt := p.getVirtualTabler()
-		vEntries := vt.getEntries()
-		for _, virtSchemaName := range vt.getSchemaNames() {
-			e := vEntries[virtSchemaName]
-			for _, tName := range e.orderedDefNames {
-				vTableEntry := e.defs[tName]
-				if err := addDesc(vTableEntry.desc, tree.DNull, virtSchemaName); err != nil {
+			addDesc := func(table *sqlbase.TableDescriptor, dbName tree.Datum, scName string) error {
+				leaseNodeDatum := tree.DNull
+				leaseExpDatum := tree.DNull
+				if table.Lease != nil {
+					leaseNodeDatum = tree.NewDInt(tree.DInt(int64(table.Lease.NodeID)))
+					leaseExpDatum = tree.MakeDTimestamp(
+						timeutil.Unix(0, table.Lease.ExpirationTime), time.Nanosecond,
+					)
+				}
+				dropTimeDatum := tree.DNull
+				if table.DropTime != 0 {
+					dropTimeDatum = tree.MakeDTimestamp(
+						timeutil.Unix(0, table.DropTime), time.Nanosecond,
+					)
+				}
+				row = row[:0]
+				row = append(row,
+					tree.NewDInt(tree.DInt(int64(table.ID))),
+					tree.NewDInt(tree.DInt(int64(table.GetParentID()))),
+					tree.NewDString(table.Name),
+					dbName,
+					tree.NewDInt(tree.DInt(int64(table.Version))),
+					tree.TimestampToInexactDTimestamp(table.ModificationTime),
+					tree.TimestampToDecimal(table.ModificationTime),
+					tree.NewDString(table.FormatVersion.String()),
+					tree.NewDString(table.State.String()),
+					leaseNodeDatum,
+					leaseExpDatum,
+					dropTimeDatum,
+					tree.NewDString(table.AuditMode.String()),
+					tree.NewDString(scName),
+				)
+				return pusher.pushRow(row...)
+			}
+
+			// Note: we do not use forEachTableDesc() here because we want to
+			// include added and dropped descriptors.
+			for _, desc := range descs {
+				table, ok := desc.(*sqlbase.TableDescriptor)
+				if !ok || p.CheckAnyPrivilege(ctx, table) != nil {
+					continue
+				}
+				dbName := dbNames[table.GetParentID()]
+				if dbName == "" {
+					// The parent database was deleted. This is possible e.g. when
+					// a database is dropped with CASCADE, and someone queries
+					// this virtual table before the dropped table descriptors are
+					// effectively deleted.
+					dbName = fmt.Sprintf("[%d]", table.GetParentID())
+				}
+				if err := addDesc(table, tree.NewDString(dbName), "public"); err != nil {
 					return err
 				}
 			}
-		}
 
-		return nil
+			// Also add all the virtual descriptors.
+			vt := p.getVirtualTabler()
+			vEntries := vt.getEntries()
+			for _, virtSchemaName := range vt.getSchemaNames() {
+				e := vEntries[virtSchemaName]
+				for _, tName := range e.orderedDefNames {
+					vTableEntry := e.defs[tName]
+					if err := addDesc(vTableEntry.desc, tree.DNull, virtSchemaName); err != nil {
+						return err
+					}
+				}
+			}
+			return nil
+		}
+		next, cleanup := setupGenerator(ctx, worker)
+		return next, cleanup, nil
 	},
 }
 
@@ -456,11 +463,11 @@ CREATE TABLE crdb_internal.jobs (
 	coordinator_id     		INT
 )`,
 	comment: `decoded job metadata from system.jobs (KV scan)`,
-	generator: func(ctx context.Context, p *planner, _ *DatabaseDescriptor) (virtualTableGenerator, error) {
+	generator: func(ctx context.Context, p *planner, _ *DatabaseDescriptor) (virtualTableGenerator, cleanupFunc, error) {
 		currentUser := p.SessionData().User
 		isAdmin, err := p.HasAdminRole(ctx)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 
 		// Beware: we're querying system.jobs as root; we need to be careful to filter
@@ -471,7 +478,7 @@ CREATE TABLE crdb_internal.jobs (
 			sqlbase.InternalExecutorSessionDataOverride{User: security.RootUser},
 			query)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 
 		// Attempt to account for the memory of the retrieved rows and the data
@@ -491,7 +498,7 @@ CREATE TABLE crdb_internal.jobs (
 			}
 		}
 		if err := ba.Grow(ctx, totalMem); err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 
 		// We'll reuse this container on each loop.
@@ -598,7 +605,7 @@ CREATE TABLE crdb_internal.jobs (
 				)
 				return container, nil
 			}
-		}, nil
+		}, nil, nil
 	},
 }
 
@@ -1587,32 +1594,40 @@ CREATE TABLE crdb_internal.table_columns (
   hidden           BOOL NOT NULL
 )
 `,
-	populate: func(ctx context.Context, p *planner, dbContext *DatabaseDescriptor, addRow func(...tree.Datum) error) error {
-		return forEachTableDescAll(ctx, p, dbContext, hideVirtual,
-			func(db *DatabaseDescriptor, _ string, table *TableDescriptor) error {
-				tableID := tree.NewDInt(tree.DInt(table.ID))
-				tableName := tree.NewDString(table.Name)
-				for i := range table.Columns {
-					col := &table.Columns[i]
-					defStr := tree.DNull
-					if col.DefaultExpr != nil {
-						defStr = tree.NewDString(*col.DefaultExpr)
+	generator: func(ctx context.Context, p *planner, dbContext *DatabaseDescriptor) (virtualTableGenerator, cleanupFunc, error) {
+		row := make(tree.Datums, 8)
+		worker := func(pusher rowPusher) error {
+			return forEachTableDescAll(ctx, p, dbContext, hideVirtual,
+				func(db *DatabaseDescriptor, _ string, table *TableDescriptor) error {
+					tableID := tree.NewDInt(tree.DInt(table.ID))
+					tableName := tree.NewDString(table.Name)
+					for i := range table.Columns {
+						col := &table.Columns[i]
+						defStr := tree.DNull
+						if col.DefaultExpr != nil {
+							defStr = tree.NewDString(*col.DefaultExpr)
+						}
+						row = row[:0]
+						row = append(row,
+							tableID,
+							tableName,
+							tree.NewDInt(tree.DInt(col.ID)),
+							tree.NewDString(col.Name),
+							tree.NewDString(col.Type.DebugString()),
+							tree.MakeDBool(tree.DBool(col.Nullable)),
+							defStr,
+							tree.MakeDBool(tree.DBool(col.Hidden)),
+						)
+						if err := pusher.pushRow(row...); err != nil {
+							return err
+						}
 					}
-					if err := addRow(
-						tableID,
-						tableName,
-						tree.NewDInt(tree.DInt(col.ID)),
-						tree.NewDString(col.Name),
-						tree.NewDString(col.Type.DebugString()),
-						tree.MakeDBool(tree.DBool(col.Nullable)),
-						defStr,
-						tree.MakeDBool(tree.DBool(col.Hidden)),
-					); err != nil {
-						return err
-					}
-				}
-				return nil
-			})
+					return nil
+				},
+			)
+		}
+		next, cleanup := setupGenerator(ctx, worker)
+		return next, cleanup, nil
 	},
 }
 
@@ -1632,39 +1647,49 @@ CREATE TABLE crdb_internal.table_indexes (
   is_inverted      BOOL NOT NULL
 )
 `,
-	populate: func(ctx context.Context, p *planner, dbContext *DatabaseDescriptor, addRow func(...tree.Datum) error) error {
+	generator: func(ctx context.Context, p *planner, dbContext *DatabaseDescriptor) (virtualTableGenerator, cleanupFunc, error) {
 		primary := tree.NewDString("primary")
 		secondary := tree.NewDString("secondary")
-		return forEachTableDescAll(ctx, p, dbContext, hideVirtual,
-			func(db *DatabaseDescriptor, _ string, table *TableDescriptor) error {
-				tableID := tree.NewDInt(tree.DInt(table.ID))
-				tableName := tree.NewDString(table.Name)
-				if err := addRow(
-					tableID,
-					tableName,
-					tree.NewDInt(tree.DInt(table.PrimaryIndex.ID)),
-					tree.NewDString(table.PrimaryIndex.Name),
-					primary,
-					tree.MakeDBool(tree.DBool(table.PrimaryIndex.Unique)),
-					tree.MakeDBool(table.PrimaryIndex.Type == sqlbase.IndexDescriptor_INVERTED),
-				); err != nil {
-					return err
-				}
-				for _, idx := range table.Indexes {
-					if err := addRow(
+		row := make(tree.Datums, 7)
+		worker := func(pusher rowPusher) error {
+			return forEachTableDescAll(ctx, p, dbContext, hideVirtual,
+				func(db *DatabaseDescriptor, _ string, table *TableDescriptor) error {
+					tableID := tree.NewDInt(tree.DInt(table.ID))
+					tableName := tree.NewDString(table.Name)
+					row = row[:0]
+					row = append(row,
 						tableID,
 						tableName,
-						tree.NewDInt(tree.DInt(idx.ID)),
-						tree.NewDString(idx.Name),
-						secondary,
-						tree.MakeDBool(tree.DBool(idx.Unique)),
-						tree.MakeDBool(idx.Type == sqlbase.IndexDescriptor_INVERTED),
-					); err != nil {
+						tree.NewDInt(tree.DInt(table.PrimaryIndex.ID)),
+						tree.NewDString(table.PrimaryIndex.Name),
+						primary,
+						tree.MakeDBool(tree.DBool(table.PrimaryIndex.Unique)),
+						tree.MakeDBool(table.PrimaryIndex.Type == sqlbase.IndexDescriptor_INVERTED),
+					)
+					if err := pusher.pushRow(row...); err != nil {
 						return err
 					}
-				}
-				return nil
-			})
+					for _, idx := range table.Indexes {
+						row = row[:0]
+						row = append(row,
+							tableID,
+							tableName,
+							tree.NewDInt(tree.DInt(idx.ID)),
+							tree.NewDString(idx.Name),
+							secondary,
+							tree.MakeDBool(tree.DBool(idx.Unique)),
+							tree.MakeDBool(idx.Type == sqlbase.IndexDescriptor_INVERTED),
+						)
+						if err := pusher.pushRow(row...); err != nil {
+							return err
+						}
+					}
+					return nil
+				},
+			)
+		}
+		next, cleanup := setupGenerator(ctx, worker)
+		return next, cleanup, nil
 	},
 }
 
@@ -2102,13 +2127,13 @@ CREATE TABLE crdb_internal.ranges_no_leases (
 	split_enforced_until TIMESTAMP
 )
 `,
-	generator: func(ctx context.Context, p *planner, _ *DatabaseDescriptor) (virtualTableGenerator, error) {
+	generator: func(ctx context.Context, p *planner, _ *DatabaseDescriptor) (virtualTableGenerator, cleanupFunc, error) {
 		if err := p.RequireAdminRole(ctx, "read crdb_internal.ranges_no_leases"); err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 		descs, err := p.Tables().getAllDescriptors(ctx, p.txn)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 		// TODO(knz): maybe this could use internalLookupCtx.
 		dbNames := make(map[uint64]string)
@@ -2134,13 +2159,13 @@ CREATE TABLE crdb_internal.ranges_no_leases (
 			EndKey: keys.MaxKey,
 		})
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 
 		// Map node descriptors to localities
 		descriptors, err := getAllNodeDescriptors(p)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 		nodeIDToLocality := make(map[roachpb.NodeID]roachpb.Locality)
 		for _, desc := range descriptors {
@@ -2226,7 +2251,7 @@ CREATE TABLE crdb_internal.ranges_no_leases (
 				learnersArr,
 				splitEnforcedUntil,
 			}, nil
-		}, nil
+		}, nil, nil
 	},
 }
 
@@ -2971,18 +2996,22 @@ CREATE TABLE crdb_internal.partitions (
 	subzone_id INT -- references a subzone id in the crdb_internal.zones table
 )
 	`,
-	populate: func(ctx context.Context, p *planner, dbContext *DatabaseDescriptor, addRow func(...tree.Datum) error) error {
+	generator: func(ctx context.Context, p *planner, dbContext *DatabaseDescriptor) (virtualTableGenerator, cleanupFunc, error) {
 		dbName := ""
 		if dbContext != nil {
 			dbName = dbContext.Name
 		}
-		return forEachTableDescAll(ctx, p, dbContext, hideVirtual, /* virtual tables have no partitions*/
-			func(db *DatabaseDescriptor, _ string, table *TableDescriptor) error {
-				return table.ForeachNonDropIndex(func(index *sqlbase.IndexDescriptor) error {
-					return addPartitioningRows(ctx, p, dbName, table, index, &index.Partitioning,
-						tree.DNull /* parentName */, 0 /* colOffset */, addRow)
+		worker := func(pusher rowPusher) error {
+			return forEachTableDescAll(ctx, p, dbContext, hideVirtual, /* virtual tables have no partitions*/
+				func(db *DatabaseDescriptor, _ string, table *TableDescriptor) error {
+					return table.ForeachNonDropIndex(func(index *sqlbase.IndexDescriptor) error {
+						return addPartitioningRows(ctx, p, dbName, table, index, &index.Partitioning,
+							tree.DNull /* parentName */, 0 /* colOffset */, pusher.pushRow)
+					})
 				})
-			})
+		}
+		next, cleanup := setupGenerator(ctx, worker)
+		return next, cleanup, nil
 	},
 }
 

pkg/sql/virtual_schema.go
@@ -74,7 +74,7 @@ type virtualSchemaTable struct {
 	// generator, if non-nil, is a function that is used when creating a
 	// virtualTableNode. This function returns a virtualTableGenerator function
 	// which generates the next row of the virtual table when called.
-	generator func(ctx context.Context, p *planner, db *DatabaseDescriptor) (virtualTableGenerator, error)
+	generator func(ctx context.Context, p *planner, db *DatabaseDescriptor) (virtualTableGenerator, cleanupFunc, error)
 }
 
 // virtualSchemaView represents a view within a virtualSchema
@@ -264,11 +264,11 @@ func (e virtualDefEntry) getPlanInfo() (sqlbase.ResultColumns, virtualTableConst
 			}
 
 			if def.generator != nil {
-				next, err := def.generator(ctx, p, dbDesc)
+				next, cleanup, err := def.generator(ctx, p, dbDesc)
 				if err != nil {
 					return nil, err
 				}
-				return p.newContainerVirtualTableNode(columns, 0, next), nil
+				return p.newContainerVirtualTableNode(columns, 0, next, cleanup), nil
 			}
 			v := p.newContainerValuesNode(columns, 0)
 

pkg/sql/virtual_table_test.go
@@ -1,0 +1,146 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package sql
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/errors"
+	"github.com/stretchr/testify/require"
+)
+
+func TestVirtualTableGenerators(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	t.Run("test cleanup", func(t *testing.T) {
+		ctx := context.Background()
+		worker := func(pusher rowPusher) error {
+			if err := pusher.pushRow(tree.NewDInt(1)); err != nil {
+				return err
+			}
+			if err := pusher.pushRow(tree.NewDInt(2)); err != nil {
+				return err
+			}
+			return nil
+		}
+
+		next, cleanup := setupGenerator(ctx, worker)
+		d, err := next()
+		if err != nil {
+			t.Fatal(err)
+		}
+		require.Equal(t, tree.Datums{tree.NewDInt(1)}, d)
+
+		// Check that we can safely cleanup in the middle of execution.
+		cleanup()
+	})
+
+	t.Run("test worker error", func(t *testing.T) {
+		// Test that if the worker returns an error we catch it.
+		ctx := context.Background()
+		worker := func(pusher rowPusher) error {
+			if err := pusher.pushRow(tree.NewDInt(1)); err != nil {
+				return err
+			}
+			if err := pusher.pushRow(tree.NewDInt(2)); err != nil {
+				return err
+			}
+			return errors.New("dummy error")
+		}
+		next, cleanup := setupGenerator(ctx, worker)
+		_, err := next()
+		require.NoError(t, err)
+		_, err = next()
+		require.NoError(t, err)
+		_, err = next()
+		require.Error(t, err)
+		cleanup()
+	})
+
+	t.Run("test no next", func(t *testing.T) {
+		ctx := context.Background()
+		// Test we don't leak anything if we call cleanup before next.
+		worker := func(pusher rowPusher) error {
+			return nil
+		}
+		_, cleanup := setupGenerator(ctx, worker)
+		cleanup()
+	})
+
+	t.Run("test context cancellation", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		// Test cancellation before asking for any rows.
+		worker := func(pusher rowPusher) error {
+			if err := pusher.pushRow(tree.NewDInt(1)); err != nil {
+				return err
+			}
+			if err := pusher.pushRow(tree.NewDInt(2)); err != nil {
+				return err
+			}
+			return nil
+		}
+		next, cleanup := setupGenerator(ctx, worker)
+		cancel()
+		_, err := next()
+		// There is a small chance that we race and don't return
+		// a query canceled here. So, only check the error if
+		// it is non-nil.
+		if err != nil {
+			require.Equal(t, sqlbase.QueryCanceledError, err)
+		}
+		cleanup()
+
+		// Test cancellation after asking for a row.
+		ctx, cancel = context.WithCancel(context.Background())
+		next, cleanup = setupGenerator(ctx, worker)
+		row, err := next()
+		require.NoError(t, err)
+		require.Equal(t, tree.Datums{tree.NewDInt(1)}, row)
+		cancel()
+		_, err = next()
+		require.Equal(t, sqlbase.QueryCanceledError, err)
+		cleanup()
+
+		// Test cancellation after asking for all the rows.
+		ctx, cancel = context.WithCancel(context.Background())
+		next, cleanup = setupGenerator(ctx, worker)
+		_, err = next()
+		require.NoError(t, err)
+		_, err = next()
+		require.NoError(t, err)
+		cancel()
+		cleanup()
+	})
+}
+
+func BenchmarkVirtualTableGenerators(b *testing.B) {
+	defer leaktest.AfterTest(b)()
+	ctx := context.Background()
+	worker := func(pusher rowPusher) error {
+		for {
+			if err := pusher.pushRow(tree.NewDInt(tree.DInt(1))); err != nil {
+				return err
+			}
+		}
+	}
+	b.Run("bench read", func(b *testing.B) {
+		next, cleanup := setupGenerator(ctx, worker)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			_, err := next()
+			require.NoError(b, err)
+		}
+		cleanup()
+	})
+}

commit 3ae41a41674d24db0bb99eb064ac581b2b1cb574

sql: fix virtual table generation in some cases
This commit fixes a long-standing bug which could lead to the generation
of the virtual table to hang indefinitely. The generation of a virtual
table involves two goroutines:
- one is the reader goroutine that is performing the virtual table scan
- another is the "pusher" ("worker") goroutine that is populating the
virtual table one row at a time and is pushing those rows to the reader.

We have tight synchronization between two goroutines so that at most one
goroutine is active at any point in time (this is needed to prevent
invalid concurrent usage of the txn). Previously, it was possible for
the reader to be blocked forever. This was the case when the worker
finishes by returning "QueryCanceledError" - in such a case the worker
would exit without unblocking the reader. The assumption was that such
an error was the result of the context cancellation (i.e. `ctx` in scope
of the reader), but that is not necessarily true. For example, the same
error could be returned when the node is quescing. There is really no
reason for the worker to skip notifying the reader, and we will now just
send all errors to the reader.

The bug has been present since the lazy virtual table generation was
introduced in 75071db8e7bdfe7b0035a5d8647ad5313d57ab8e, but it has been
exposed in fcd689492a20e6c8d73e7ac4f53c778c12c21e14. Perhaps the latter
commit made it much more likely for the assumption mentioned in the
paragraph to be violated. In particular, we now are more likely to have
multiple "nested" internally-executed queries that deals with virtual
tables.

Release note (bug fix): Previously, queries reading from virtual tables
(e.g. `crdb_internal.*` and `pg_catalog.*` tables) could hang
indefinitely in some rare cases when they result in an error, and this
is now fixed.

pkg/sql/virtual_table.go
@@ -125,11 +125,6 @@ func setupGenerator(
 			case <-comm:
 			}
 			err := worker(ctx, funcRowPusher(addRow))
-			// If the query was canceled, next() will already return a
-			// QueryCanceledError, so just exit here.
-			if errors.Is(err, cancelchecker.QueryCanceledError) {
-				return
-			}
 			// Notify that we are done sending rows.
 			select {
 			case <-ctx.Done():