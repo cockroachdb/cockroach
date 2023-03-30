5af18e586681e617ec2ae28d1f9602689b810a8a
Jane Xing <zhouxing@uchicago.edu>
2023-03-29 20:36:27 -0500

    sql: add session variable for multiple active portals
    This change introduces a new session variable for a preview feature. When set to `true`,
    all non-internal portals with read-only [`SELECT`](../v23.1/selection-queries.html)
    queries without sub-queries or post-queries can be paused and resumed in an interleaving
    manner, but are executed with a local plan.
    
    Release note (SQL change): Added the session variable `multiple_active_portals_enabled`.
    This setting is only for a preview feature. When set to `true`, it allows
    multiple portals to be open at the same time, with their execution interleaved
    with each other. In other words, these portals can be paused. The underlying
    statement for a pausable portal must be a read-only `SELECT` query without
    sub-queries or post-queries, and such a portal is always executed with a local
    plan.

--- pkg/server/server_sql.go
+++ pkg/server/server_sql.go
@@ -58,7 +58,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/server/settingswatcher"
 	"github.com/cockroachdb/cockroach/pkg/server/status"
 	"github.com/cockroachdb/cockroach/pkg/server/systemconfigwatcher"
-	"github.com/cockroachdb/cockroach/pkg/server/telemetry"
 	"github.com/cockroachdb/cockroach/pkg/server/tracedumper"
 	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
@@ -102,7 +101,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlliveness/slinstance"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlliveness/slprovider"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlstats"
-	"github.com/cockroachdb/cockroach/pkg/sql/sqltelemetry"
 	"github.com/cockroachdb/cockroach/pkg/sql/stats"
 	"github.com/cockroachdb/cockroach/pkg/sql/stmtdiagnostics"
 	"github.com/cockroachdb/cockroach/pkg/sql/syntheticprivilegecache"
@@ -1368,12 +1366,6 @@ func newSQLServer(ctx context.Context, cfg sqlServerArgs) (*SQLServer, error) {
 	}
 	vmoduleSetting.SetOnChange(&cfg.Settings.SV, fn)
 	fn(ctx)
-
-	sql.EnableMultipleActivePortals.SetOnChange(&cfg.Settings.SV, func(ctx context.Context) {
-		if sql.EnableMultipleActivePortals.Get(&cfg.Settings.SV) {
-			telemetry.Inc(sqltelemetry.MultipleActivePortalCounter)
-		}
-	})
 
 	return &SQLServer{
 		ambientCtx:                     cfg.BaseConfig.AmbientCtx,
