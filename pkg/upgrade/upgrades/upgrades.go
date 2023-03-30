1a2671c6365bf0f1d4d6d73f21caa8e078ac432c
maryliag <marylia@cockroachlabs.com>
2023-03-30 11:08:29 -0400

    sql: alter gc ttl on sql stats tables to 1h
    Fixes #100138
    
    Change the GC TTL value of SQL Stats table to
    be 1h.
    
    Release note (sql change): Changr GC TTL on SQL Stats table to 1h.

--- pkg/upgrade/upgrades/upgrades.go
+++ pkg/upgrade/upgrades/upgrades.go
@@ -304,6 +304,12 @@ var upgrades = []upgradebase.Upgrade{
 		upgrade.NoPrecondition,
 		systemStatisticsActivityTableMigration,
 	),
+	upgrade.NewPermanentTenantUpgrade(
+		"change TTL for SQL Stats system tables",
+		toCV(clusterversion.V23_1ChangeSQLStatsTTL),
+		sqlStatsTTLChange,
+		"change TTL for SQL Stats system tables",
+	),
 	upgrade.NewTenantUpgrade(
 		"stop writing payload and progress to system.jobs",
 		toCV(clusterversion.V23_2StopWritingPayloadAndProgressToSystemJobs),
