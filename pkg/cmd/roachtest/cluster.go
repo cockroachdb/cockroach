3cfd40cf78b8d43a84fc238fbc1f0126cd0015ef
Michael Butler <butler@cockroachlabs.com>
2023-03-30 23:55:39 +0000

    address lidor comments

--- pkg/cmd/roachtest/cluster.go
+++ pkg/cmd/roachtest/cluster.go
@@ -2423,7 +2423,9 @@ func (c *clusterImpl) Extend(ctx context.Context, d time.Duration, l *logger.Log
 // NewMonitor creates a monitor that can watch for unexpected crdb node deaths on m.Wait()
 // and provide roachtest safe goroutines.
 //
-// As a general rule, if the user has a workload node, do not monitor it.
+// As a general rule, if the user has a workload node, do not monitor it. A
+// monitor's semantics around handling expected node deaths breaks down if it's
+// monitoring a workload node.
 func (c *clusterImpl) NewMonitor(ctx context.Context, opts ...option.Option) cluster.Monitor {
 	return newMonitor(ctx, c.t, c, opts...)
 }
