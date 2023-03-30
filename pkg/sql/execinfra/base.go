7c56abf7a7dd8ae1b0fe0e0981c7c28ee333aa70
Jane Xing <zhouxing@uchicago.edu>
2023-03-29 20:36:27 -0500

    sql: add session variable for multiple active portals
    This change introduces a new session variable for a preview feature. When set to `true`,
    all non-internal portals with read-only [`SELECT`](../v23.1/selection-queries.html)
    queries without sub-queries or post-queries can be paused and resumed in an interleaving
    manner, but are executed with a local plan.
    
    Release note (SQL change): Added the session variable `multiple_active_portals_enabled`.
    This setting is only for a preview feature. When set to `true`, it allows
    multiple portals to be open at the same time, with their execution interleaved
    with each other. In other words, these portals can be paused. The underlying
    statement for a pausable portal must be a read-only `SELECT` query without
    sub-queries or post-queries, and such a portal is always executed with a local
    plan.

--- pkg/sql/execinfra/base.go
+++ pkg/sql/execinfra/base.go
@@ -43,8 +43,9 @@ const (
 	NeedMoreRows ConsumerStatus = iota
 	// SwitchToAnotherPortal indicates that the we received exec command for
 	// a different portal, and may come back to continue executing the current
-	// portal later. If the cluster setting sql.pgwire.multiple_active_portals.enabled
-	// is set to be true, we do nothing and return the control to the connExecutor.
+	// portal later. If the cluster setting session variable
+	// multiple_active_portals_enabled is set to be true, we do nothing and return
+	// the control to the connExecutor.
 	SwitchToAnotherPortal
 	// DrainRequested indicates that the consumer will not process any more data
 	// rows, but will accept trailing metadata from the producer.
