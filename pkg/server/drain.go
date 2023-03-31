ae9c682fbc771753fb55d2e31a11326b9a90926b
Raphael 'kena' Poss <knz@thaumogen.net>
2023-03-31 17:03:41 +0200

    server,sqlstats: shut down stats persistence tasks gracefully before SQL
    This commit ensures that the background tasks that manage the
    persistence of SQL stats are shut down during graceful drain, to
    reduce log spam at the end of the shutdown sequence and also to
    provide a sequence point where we are confident there is no more SQL
    activity.
    
    Release note: None

--- pkg/server/drain.go
+++ pkg/server/drain.go
@@ -381,7 +381,9 @@ func (s *drainServer) drainClients(
 	s.sqlServer.distSQLServer.Drain(ctx, queryMaxWait, reporter)
 
 	// Flush in-memory SQL stats into the statement stats system table.
-	s.sqlServer.pgServer.SQLServer.GetSQLStatsProvider().(*persistedsqlstats.PersistedSQLStats).Flush(ctx)
+	statsProvider := s.sqlServer.pgServer.SQLServer.GetSQLStatsProvider().(*persistedsqlstats.PersistedSQLStats)
+	statsProvider.Flush(ctx)
+	statsProvider.Stop(ctx)
 
 	// Inform the async tasks for table stats that the node is draining
 	// and wait for task shutdown.
