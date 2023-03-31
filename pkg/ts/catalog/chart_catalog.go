1931a968f5207e893b02818422f5defdd3272e58
Erik Grinaker <grinaker@cockroachlabs.com>
2023-03-31 11:40:42 +0000

    kvserver: add `leases.requests.latency` metric
    This patch adds a histogram of lease request latencies. It includes all
    request types (acquisitions, transfers, and extensions) and all
    outcomes (successes and errors), but only considers the coalesced lease
    requests regardless of the number of waiters and how long they have been
    waiting for.
    
    Epic: none
    Release note (ops change): Added a metric `leases.requests.latency`
    recording a histogram of lease request latencies.

--- pkg/ts/catalog/chart_catalog.go
+++ pkg/ts/catalog/chart_catalog.go
@@ -1744,6 +1744,10 @@ var charts = []sectionDescription{
 				Metrics: []string{"requests.slow.lease"},
 			},
 			{
+				Title:   "Lease Request Latency",
+				Metrics: []string{"leases.requests.latency"},
+			},
+			{
 				Title: "Succcess Rate",
 				Metrics: []string{
 					"leases.error",
