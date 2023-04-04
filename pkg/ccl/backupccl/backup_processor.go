cockroachdb/cockroach

commit 93e1e178b4d2f1df923c4399fc395672103aacd7

tracing: Notify EventListeners while holding the span's mutex
This commit addresses an oversight that was introduced in f6e0348b9500fc03d413e0763d730181b4b25708
where we made the notifications of the EventListeners done without
holding the span's mutex. However, the contract of `WithEventListeners`
implies that the callbacks are not called any more after `Finish()`
returns, so that commit introduced a bug that is now fixed in this
commit.

The fix done in this commit is suboptimal though - it introduces an
exported method `SetLazyTagLocked` which is not great. The proper fix
would be to refactor `TracingAggregator` to avoid setting the lazy tag
dynamically, in the event listener callback, but that seems like
a non-trivial change, and I'd like to get this commit backported. To
avoid introducing more callsites of this `SetLazyTagLocked` method it is
marked as "deprecated" and #100438 tracks addressing this piece of the
tech debt.

Release note: None

pkg/util/bulk/tracing_aggregator.go
@@ -62,7 +62,7 @@ func (b *TracingAggregator) Notify(event tracing.Structured) tracing.EventConsum
 	eventTag := bulkEvent.Tag()
 	if _, ok := b.mu.aggregatedEvents[bulkEvent.Tag()]; !ok {
 		b.mu.aggregatedEvents[eventTag] = bulkEvent.Identity()
-		b.mu.sp.SetLazyTag(eventTag, b.mu.aggregatedEvents[eventTag])
+		b.mu.sp.SetLazyTagLocked(eventTag, b.mu.aggregatedEvents[eventTag])
 	}
 	b.mu.aggregatedEvents[eventTag].Combine(bulkEvent)
 	return tracing.EventNotConsumed

pkg/kv/bulk/bulkpb/ingestion_performance_stats.go
@@ -271,5 +271,3 @@ type timing time.Duration
 
 func (t timing) String() string { return time.Duration(t).Round(time.Second).String() }
 func (t timing) SafeValue()     {}
-
-var _ bulk.TracingAggregatorEvent = &IngestionPerformanceStats{}

pkg/testutils/lint/lint_test.go
@@ -1799,6 +1799,8 @@ func TestLint(t *testing.T) {
 				stream.GrepNot(`pkg/sql/row/expr_walker_test.go:.* evalCtx.SetDeprecatedContext is deprecated: .*`),
 				stream.GrepNot(`pkg/sql/schemachanger/scbuild/tree_context_builder.go:.* evalCtx.SetDeprecatedContext is deprecated: .*`),
 				stream.GrepNot(`pkg/sql/schemachanger/scplan/internal/rules/.*/.*go:.* should not use dot imports \(ST1001\)`),
+				// TODO(yuzefovich): remove this exclusion (#100438).
+				stream.GrepNot(`pkg/util/bulk/tracing_aggregator.go:.*SetLazyTagLocked is deprecated: .*`),
 			), func(s string) {
 				t.Errorf("\n%s", s)
 			}); err != nil {

pkg/util/tracing/crdbspan.go
@@ -962,28 +962,28 @@ func (s *crdbSpan) getLazyTagLocked(key string) (interface{}, bool) {
 // convention between parents and children.
 func (s *crdbSpan) notifyEventListeners(item Structured) {
 	s.mu.Lock()
+	var unlocked bool
+	defer func() {
+		if !unlocked {
+			s.mu.Unlock()
+		}
+	}()
 
 	// Check if the span has been finished concurrently with this notify call.
 	// This can happen when the signal comes from a child span; in that case the
 	// child calls into the parent without holding the child's lock, so the call
 	// can race with parent.Finish().
 	if s.mu.finished {
-		s.mu.Unlock()
 		return
 	}
-	s.mu.Unlock()
 
 	// Notify s' eventListeners first, before passing the event to parent.
-	//
-	// We can operate on s' eventListeners without holding the mutex because the
-	// slice is only written to once during span creation.
 	for _, listener := range s.eventListeners {
 		if listener.Notify(item) == EventConsumed {
 			return
 		}
 	}
 
-	s.mu.Lock()
 	if s.mu.recording.notifyParentOnStructuredEvent {
 		parent := s.mu.parent.Span.i.crdb
 		// Take a reference of s' parent before releasing the mutex. This ensures
@@ -992,9 +992,8 @@ func (s *crdbSpan) notifyEventListeners(item Structured) {
 		parentRef := makeSpanRef(s.mu.parent.Span)
 		defer parentRef.release()
 		s.mu.Unlock()
+		unlocked = true
 		parent.notifyEventListeners(item)
-	} else {
-		s.mu.Unlock()
 	}
 }
 

pkg/util/tracing/span.go
@@ -520,6 +520,19 @@ func (sp *Span) SetLazyTag(key string, value interface{}) {
 	sp.i.SetLazyTag(key, value)
 }
 
+// SetLazyTagLocked is the same as SetLazyTag but assumes that the mutex of sp
+// is being held.
+//
+// Deprecated: this method should not be used because it's introduced only to go
+// around some tech debt (#100438). Once that issue is addressed, this method
+// should be removed.
+func (sp *Span) SetLazyTagLocked(key string, value interface{}) {
+	if sp.detectUseAfterFinish() {
+		return
+	}
+	sp.i.setLazyTagLocked(key, value)
+}
+
 // GetLazyTag returns the value of the tag with the given key. If that tag doesn't
 // exist, the bool retval is false.
 func (sp *Span) GetLazyTag(key string) (interface{}, bool) {
@@ -535,8 +548,7 @@ type EventListener interface {
 	// Notify is invoked on every Structured event recorded by the span and its
 	// children, recursively.
 	//
-	// Note that this method should not run for a long time as it will hold up the
-	// span recording Structured events during traced operations.
+	// The caller holds the mutex of the span.
 	//
 	// Notify will not be called concurrently on the same span.
 	Notify(event Structured) EventConsumptionStatus

pkg/util/tracing/span_inner.go
@@ -241,6 +241,14 @@ func (s *spanInner) SetLazyTag(key string, value interface{}) *spanInner {
 	return s
 }
 
+func (s *spanInner) setLazyTagLocked(key string, value interface{}) *spanInner {
+	if s.isNoop() {
+		return s
+	}
+	s.crdb.setLazyTagLocked(key, value)
+	return s
+}
+
 // GetLazyTag returns the value of the tag with the given key. If that tag doesn't
 // exist, the bool retval is false.
 func (s *spanInner) GetLazyTag(key string) (interface{}, bool) {

pkg/util/tracing/span_test.go
@@ -1310,16 +1310,12 @@ func TestWithRemoteParentFromTraceInfo(t *testing.T) {
 
 type mockEventListener struct {
 	eventsSeen int
-	notifyImpl func()
 	// If set, then the events are "consumed" by this listener.
 	consuming bool
 }
 
 func (f *mockEventListener) Notify(_ Structured) EventConsumptionStatus {
 	f.eventsSeen++
-	if f.notifyImpl != nil {
-		f.notifyImpl()
-	}
 	if f.consuming {
 		return EventConsumed
 	}
@@ -1390,25 +1386,6 @@ func TestEventListener(t *testing.T) {
 	childSp2.RecordStructured(&types.Int32Value{Value: 12})
 	require.Equal(t, 7, rootEventListener.eventsSeen)
 	childSp2.Finish()
-}
-
-func TestEventListenerNotifiedWithoutHoldingSpanMutex(t *testing.T) {
-	tr := NewTracer()
-	rootEventListener := &mockEventListener{}
-	sp := tr.StartSpan("root", WithRecording(tracingpb.RecordingStructured),
-		WithEventListeners(rootEventListener))
-	defer sp.Finish()
-
-	// Set the EventListeners Notify() method to acquire the span's mutex.
-	rootEventListener.notifyImpl = func() {
-		sp.i.crdb.mu.Lock()
-		defer sp.i.crdb.mu.Unlock()
-	}
-
-	// Record a StructuredEvent, if Notify() were called holding the span's mutex
-	// this would deadlock.
-	sp.RecordStructured(&types.Int32Value{Value: 5})
-	require.Equal(t, 1, rootEventListener.eventsSeen)
 }
 
 // TestFinishedChildrenMetadata tests that on Finish() the parent span's

commit 9e4e6083cbbd4d90993b33d117d46ad9d7835b21

util/bulk: fix locking order in TracingAggregator
This commit fixes an inconsistent locking order in TracingAggregator
that was recently introduced in 93e1e178b4d2f1df923c4399fc395672103aacd7.
In particular, now `EventListener.Notify` is called while holding the
span's mutex. However, `TracingAggregator.Close` was implemented in such
a fashion that its internal mutex was before `Finish` (which acquires
the span's mutex). This commit fixes this issue by getting rid of
unnecessary locking in `Close` - namely, it's now required that `Close`
is called exactly once, so we can just call `span.Finish`.

Release note: None

pkg/ccl/backupccl/backup_processor.go
@@ -217,8 +217,8 @@ func (bp *backupDataProcessor) Next() (rowenc.EncDatumRow, *execinfrapb.Producer
 
 func (bp *backupDataProcessor) close() {
 	bp.cancelAndWaitForWorker()
-	bp.agg.Close()
 	if bp.InternalClose() {
+		bp.agg.Close()
 		bp.memAcc.Close(bp.Ctx())
 	}
 }

pkg/util/bulk/tracing_aggregator.go
@@ -35,15 +35,13 @@ type TracingAggregatorEvent interface {
 // A TracingAggregator can be used to aggregate and render AggregatorEvents that
 // are emitted as part of its tracing spans' recording.
 type TracingAggregator struct {
+	// sp is the tracing span managed by the TracingAggregator.
+	sp *tracing.Span
 	mu struct {
 		syncutil.Mutex
 		// aggregatedEvents is a mapping from the tag identifying the
 		// TracingAggregatorEvent to the running aggregate of the TracingAggregatorEvent.
 		aggregatedEvents map[string]TracingAggregatorEvent
-		// sp is the tracing span managed by the TracingAggregator.
-		sp *tracing.Span
-		// closed is set to true if the TracingAggregator has already been closed.
-		closed bool
 	}
 }
 
@@ -62,20 +60,17 @@ func (b *TracingAggregator) Notify(event tracing.Structured) tracing.EventConsum
 	eventTag := bulkEvent.Tag()
 	if _, ok := b.mu.aggregatedEvents[bulkEvent.Tag()]; !ok {
 		b.mu.aggregatedEvents[eventTag] = bulkEvent.Identity()
-		b.mu.sp.SetLazyTagLocked(eventTag, b.mu.aggregatedEvents[eventTag])
+		b.sp.SetLazyTagLocked(eventTag, b.mu.aggregatedEvents[eventTag])
 	}
 	b.mu.aggregatedEvents[eventTag].Combine(bulkEvent)
 	return tracing.EventNotConsumed
 }
 
-// Close is responsible for finishing the Aggregators' tracing span.
+// Close is responsible for finishing the TracingAggregator's tracing span.
+//
+// NOTE: it must be called exactly once.
 func (b *TracingAggregator) Close() {
-	b.mu.Lock()
-	defer b.mu.Unlock()
-	if !b.mu.closed {
-		b.mu.sp.Finish()
-		b.mu.closed = true
-	}
+	b.sp.Finish()
 }
 
 var _ tracing.EventListener = &TracingAggregator{}
@@ -96,7 +91,7 @@ func MakeTracingAggregatorWithSpan(
 	agg.mu.Lock()
 	defer agg.mu.Unlock()
 	agg.mu.aggregatedEvents = make(map[string]TracingAggregatorEvent)
-	agg.mu.sp = aggSpan
+	agg.sp = aggSpan
 
 	return aggCtx, agg
 }