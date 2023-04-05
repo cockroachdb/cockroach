b7769949e9b77e19f6678129c1804122c056540c
Raphael 'kena' Poss <knz@thaumogen.net>
2023-04-05 22:50:26 +0200

    jobs: remove the redundant isql.DB reference
    Release note: None

--- pkg/server/server_sql.go
+++ pkg/server/server_sql.go
@@ -628,7 +628,6 @@ func newSQLServer(ctx context.Context, cfg sqlServerArgs) (*SQLServer, error) {
 			cfg.AmbientCtx,
 			cfg.stopper,
 			cfg.clock,
-			cfg.internalDB,
 			cfg.rpcContext.LogicalClusterID,
 			cfg.nodeIDContainer,
 			cfg.sqlLivenessProvider,
