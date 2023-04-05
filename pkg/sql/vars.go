d6e3993256af19b412cafbdbecdab9a5b691e5fe
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-04-05 16:20:16 -0700

    sql: add `unbounded_parallel_scans` session variable
    This commit introduces `unbounded_parallel_scans` session variable that
    controls whether the scans (i.e. served by the TableReader DistSQL
    processors) will be parallelized across ranges even if we cannot
    guarantee an upper bound on the number of rows scanned. Currently, we
    only parallelize the scan across ranges if
    1. there are no hard nor soft limits (meaning that the query cannot have
    the LIMIT clause)
    2. we know during the planning that less than 10k rows will be scanned.
    
    The first condition is needed in order to not scan too much data
    unnecessarily and is not affected by the new session variable. The
    rationale for the second condition is that all scanned rows will be
    buffered in memory by a single node, and since we cannot use
    BatchRequest's limits when we want cross-range parallelism (those limits
    disable the parallelism), the total footprint of rows can be arbitrarily
    large which could lead to OOMs of the nodes. Thus, we put this condition
    in place: if we know that less than 10k rows will be returned, then we
    take our chances and enable the parallelism.
    
    However, these two conditions are contradictory in nature which results
    in parallelizing scans rarely, which is especially detrimental in the
    multi-region setup. The long term solution to this problem is expanding
    the Streamer API to a) power the TableReaders, and b) to support LIMITs,
    but in the short term this commit introduces a session variable that
    effectively disables the second condition. This gives power to the users
    to choose the parallelism at their own risk.
    
    Release note (sql change): New session variable `unbounded_parallel_scans`
    has been introduced which controls whether scans will be parallelized across
    ranges in more cases. Note that using this feature can lead to increased
    likelihood of OOMs, so it should be used with care (namely when you
    expect that the scan should read "reasonable" number of rows - probably
    less than 10k). Also note that queries with LIMITs aren't affected by
    this variable - cross-range parallelism of scans continues to be
    disabled for those.

--- pkg/sql/vars.go
+++ pkg/sql/vars.go
@@ -2665,6 +2665,23 @@ var varGen = map[string]sessionVar{
 		},
 		GlobalDefault: displayPgBool(util.ConstantWithMetamorphicTestBool("multiple_active_portals_enabled", false)),
 	},
+
+	// CockroachDB extension.
+	`unbounded_parallel_scans`: {
+		GetStringVal: makePostgresBoolGetStringValFn(`unbounded_parallel_scans`),
+		Set: func(_ context.Context, m sessionDataMutator, s string) error {
+			b, err := paramparse.ParseBoolVar("unbounded_parallel_scans", s)
+			if err != nil {
+				return err
+			}
+			m.SetUnboundedParallelScans(b)
+			return nil
+		},
+		Get: func(evalCtx *extendedEvalContext, _ *kv.Txn) (string, error) {
+			return formatBoolAsPostgresSetting(evalCtx.SessionData().UnboundedParallelScans), nil
+		},
+		GlobalDefault: globalFalse,
+	},
 }
 
 // We want test coverage for this on and off so make it metamorphic.
