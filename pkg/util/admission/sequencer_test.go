aa4ab6a0c5cb7ae7e90b5f5035626172b3885b20
irfan sharif <irfanmahmoudsharif@gmail.com>
2023-04-06 16:55:46 -0400

    admission: move CreateTime-sequencing below-raft
    We move kvflowsequencer.Sequencer and its use in kvflowhandle.Handle
    (above-raft) to admission.sequencer, now used by
    admission.StoreWorkQueue (below-raft). This variant appeared in an
    earlier revision of #97599 where we first introduced monotonically
    increasing CreateTimes for a given raft group.
    
    In a subsequent commit, when integrating kvflowcontrol into the critical
    path for replication traffic, we'll observe that it's quite difficult to
    create sequencing CreateTimes[^1] above raft. This is because these sequence
    numbers are encoded as part of the raft proposal[^2], and at
    encode-time, we don't actually know what log position the proposal is
    going to end up in. It's hard to explicitly guarantee that a proposal
    with log-position P1 will get encoded before another with log position
    P2, where P1 < P2.
    
    Naively sequencing CreateTimes at proposal-encode-time could result in
    over-admission. This is because of how we return flow tokens -- up to
    some log index[^3], and how use these sequence numbers in below-raft
    WorkQueues. If P2 ends up with a lower sequence number/CreateTime, it
    would get admitted first, and when returning flow tokens by log
    position, in specifying up-to-P2, we'll early return P1's flow tokens
    despite it not being admitted. So we'd over-admit at the sender. This
    is all within a <tenant,priority> pair.
    
    [^1]: We use CreateTimes as "sequence numbers" in replication admission
          control. We want to assign each AC-queued work below-raft a
          "sequence number" for FIFO ordering within a <tenant,priority>.
          We ensure these timestamps are roughly monotonic with respect to
          log positions of replicated work by sequencing work in log
          position order.
    [^2]: In kvflowcontrolpb.RaftAdmissionMeta.
    [^3]: See kvflowcontrolpb.AdmittedRaftLogEntries.
    
    Release note: None

--- pkg/util/admission/sequencer_test.go
+++ pkg/util/admission/sequencer_test.go
@@ -8,16 +8,13 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-package kvflowsequencer
+package admission
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 	"testing"
 	"time"
 
-	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvflowcontrol/kvflowcontrolpb"
 	"github.com/cockroachdb/cockroach/pkg/testutils/datapathutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
@@ -30,13 +27,13 @@ func TestSequencer(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 
-	var sequencer *Sequencer
+	var seq *sequencer
 	var lastSeqNum int64
 	datadriven.RunTest(t, datapathutils.TestDataPath(t, "sequencer"),
 		func(t *testing.T, d *datadriven.TestData) string {
 			switch d.Cmd {
 			case "init":
-				sequencer = New()
+				seq = &sequencer{}
 				return ""
 
 			case "sequence":
@@ -49,8 +46,8 @@ func TestSequencer(t *testing.T) {
 
 				// Parse log-position=<int>/<int>.
 				logPosition := parseLogPosition(t, d)
-				_ = logPosition
-				sequenceNum := sequencer.Sequence(tzero.Add(dur)).UnixNano()
+				_ = logPosition // unused
+				sequenceNum := seq.sequence(tzero.Add(dur).UnixNano())
 				if lastSeqNum < sequenceNum {
 					movement = " (advanced)"
 				}
@@ -66,24 +63,4 @@ func TestSequencer(t *testing.T) {
 			}
 		},
 	)
-}
-
-// tzero represents the t=0, the earliest possible time. All other
-// create-time=<duration> is relative to this time.
-var tzero = timeutil.Unix(0, 0)
-
-func parseLogPosition(t *testing.T, d *datadriven.TestData) kvflowcontrolpb.RaftLogPosition {
-	// Parse log-position=<int>/<int>.
-	var arg string
-	d.ScanArgs(t, "log-position", &arg)
-	inner := strings.Split(arg, "/")
-	require.Len(t, inner, 2)
-	term, err := strconv.Atoi(inner[0])
-	require.NoError(t, err)
-	index, err := strconv.Atoi(inner[1])
-	require.NoError(t, err)
-	return kvflowcontrolpb.RaftLogPosition{
-		Term:  uint64(term),
-		Index: uint64(index),
-	}
 }
