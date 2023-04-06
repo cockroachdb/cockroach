41fc21457b4981f2c65ce93edee2f8db99960e09
Marcus Gartner <marcus@cockroachlabs.com>
2023-04-06 16:17:19 -0400

    opt: hoist uncorrelated equality subqueries
    Subqueries that are in equality expressions with a variable are now
    hoisted. When these expressions exist in a filter, hoisting the subquery
    can allow the main query to plan a lookup join, rather than an
    inefficient full-table scan.
    
    For example, consider the table and query:
    
        CREATE TABLE t (
          a INT,
          INDEX (a)
        );
    
        SELECT * FROM t WHERE a = (SELECT max(a) FROM t);
    
    Prior to this commit, the query plan for this query required a full
    table scan:
    
        select
         ├── columns: a:1
         ├── scan t@t_a_idx
         │    ├── columns: a:1
         │    └── constraint: /1/2: (/NULL - ]
         └── filters
              └── eq
                   ├── a:1
                   └── subquery
                        └── scalar-group-by
                             ├── columns: max:9
                             ├── scan t@t_a_idx,rev
                             │    ├── columns: a:5
                             │    ├── constraint: /5/6: (/NULL - ]
                             │    └── limit: 1(rev)
                             └── aggregations
                                  └── const-agg [as=max:9, outer=(5)]
                                       └── a:5
    
    By hoisting the subquery, the full table scan is replaced with a lookup
    join:
    
        project
         ├── columns: a:1
         └── inner-join (lookup t@t_a_idx)
              ├── columns: a:1 max:9
              ├── key columns: [9] = [1]
              ├── scalar-group-by
              │    ├── columns: max:9
              │    ├── scan t@t_a_idx,rev
              │    │    ├── columns: a:5
              │    │    ├── constraint: /5/6: (/NULL - ]
              │    │    └── limit: 1(rev)
              │    └── aggregations
              │         └── const-agg [as=max:9, outer=(5)]
              │              └── a:5
              └── filters (true)
    
    This hoisting is enabled by default, but can be disabled by setting the
    `optimizer_hoist_uncorrelated_equality_subqueries` session setting to
    `false`.
    
    Fixes #83392
    Informs #51820
    Informs #93829
    Informs #100855
    
    Release note (performance improvement): Queries that have subqueries in
    equality expressions are now more efficiently planned by the optimizer.

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -3514,6 +3514,10 @@ func (m *sessionDataMutator) SetOptimizerAlwaysUseHistograms(val bool) {
 	m.data.OptimizerAlwaysUseHistograms = val
 }
 
+func (m *sessionDataMutator) SetOptimizerHoistUncorrelatedEqualitySubqueries(val bool) {
+	m.data.OptimizerHoistUncorrelatedEqualitySubqueries = val
+}
+
 func (m *sessionDataMutator) SetEnableCreateStatsUsingExtremes(val bool) {
 	m.data.EnableCreateStatsUsingExtremes = val
 }
