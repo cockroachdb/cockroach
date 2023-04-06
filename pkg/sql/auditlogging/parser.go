7605568908adab0e803a4f0f75f24c582c691c38
Thomas Hardy <thardy@cockroachlabs.com>
2023-04-06 14:17:11 -0400

    sql: introduce role-level audit logging
    **Configuration**
    
    This change introduces role-level audit logging through a cluster setting `sql.log.user_audit`.
    
    Using the cluster setting, a user is able to set an audit logging configuration using a table-like syntax, like so:
    
    ```
    set cluster setting sql.log.user_audit = '
        test_role       ALL
        another_role    NONE
        ...
        ALL             NONE
    ';
    ```
    
    Each row in the configuration represents an _audit setting_ in the configuration. An audit setting is comprised of the following columns:
    - `USER/ROLE`: the name of the user/role we want to audit (i.e. `test_role` in the example above)
        - the _special_ user/role value `ALL` applies the audit setting to *all* users
    
    - `STATEMENT_FILTER`: the statement filter for the role (i.e. `ALL` or `NONE` in the example above)
        - `ALL` will enable audit logging for all SQL statements for the given audit setting
        - `NONE` will exclude audit logging for all SQL statements for the given audit setting
    
    As such, the example above can be read as:
    - users with the username/role `test_role` will emit audit logs for all statements they issue
    - users with the username/role `another_role` will not emit audit logs for any statements they issue (in principle, one can achieve this by simply omitting `another_role` from the configuration entirely, but we include it here as an example)
    - all remaining users will not emit audit logs for any statements they issue
    
    Internally, this configuration is stored as an object containing a list of audit settings. The configuration is stored as part of the executor config, so it can be shared amongst all SQL connections.
    
    **Matching**
    
    To determine whether a statement should be emitted to the audit log, we check whether there is a corresponding audit setting for the statement. This means:
    - the user who issued the statement must have a username or belong to a role that has a corresponding audit setting
    - for that audit setting, check if auditing is enabled (i.e. the statement filter must be `ALL`)
    
    To do this, we get the user's role memberships from the role membership cache and iterate through the config's audit settings, looking for a matching audit setting by role. *At the first audit setting match*, we stop iteration. We check if the auditing is enabled (i.e. statement filter must be `ALL`), if so, we add the statement to a collection of audit events to be logged later. If the statement filter is `NONE`, we stop there.
    
    It's worth re-iterating that the *order in which the audit settings in the configuration are specified is important*, as the *first matching audit setting* will apply first.
    
    Additionally, we make sure to filter out any statements issued by:
    - reserved users
    - internal planners
    - internal executors
    from the audit logs to avoid noise in the logs.
    
    Release note (sql change): introduce role-level audit logging

---
+++ pkg/sql/auditlogging/parser.go
@@ -0,0 +1,128 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package auditlogging
+
+import (
+	"strings"
+
+	"github.com/cockroachdb/cockroach/pkg/security/username"
+	"github.com/cockroachdb/cockroach/pkg/settings/rulebasedscanner"
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgcode"
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
+	"github.com/cockroachdb/errors"
+)
+
+// parse parses the provided audit logging configuration.
+func parse(input string) (*AuditConfig, error) {
+	tokens, err := rulebasedscanner.Tokenize(input)
+	if err != nil {
+		return nil, err
+	}
+
+	config := EmptyAuditConfig()
+	config.settings = make([]AuditSetting, len(tokens.Lines))
+	// settingsRoleMap keeps track of the roles we've already written in the config
+	settingsRoleMap := make(map[username.SQLUsername]interface{}, len(tokens.Lines))
+	for i, line := range tokens.Lines {
+		setting, err := parseAuditSetting(line)
+		if err != nil {
+			return nil, errors.Wrapf(
+				pgerror.WithCandidateCode(err, pgcode.ConfigFile),
+				"line %d", tokens.Linenos[i])
+		}
+		if _, exists := settingsRoleMap[setting.Role]; exists {
+			return nil, errors.Newf("duplicate role listed: %v", setting.Role)
+		}
+		settingsRoleMap[setting.Role] = i
+		config.settings[i] = setting
+		if setting.Role.Normalized() == allUserRole {
+			config.allRoleAuditSettingIdx = i
+		}
+	}
+	return config, nil
+}
+
+func parseAuditSetting(inputLine rulebasedscanner.Line) (setting AuditSetting, err error) {
+	fieldIdx := 0
+	expectedNumFields := 2
+	setting.input = inputLine.Input
+	line := inputLine.Tokens
+
+	if len(line) > expectedNumFields {
+		return setting, errors.WithHint(
+			errors.New("too many fields specified"),
+			"Expected only 2 fields (role, statement filter)")
+	}
+
+	// Read the user/role type.
+	if len(line[fieldIdx]) > 1 {
+		return setting, errors.WithHint(
+			errors.New("multiple values specified for role"),
+			"Specify exactly one role type per line.")
+	}
+	// Note we do not do any validation to ensure the input role exists as an actual role. This allows for
+	// input roles to be arbitrary string values.
+	setting.Role, err = username.MakeSQLUsernameFromUserInput(line[fieldIdx][0].Value, username.PurposeValidation)
+	if err != nil {
+		return setting, err
+	}
+	err = parseRole(setting.Role)
+	if err != nil {
+		return setting, err
+	}
+	// Parse statement filter
+	fieldIdx++
+	if fieldIdx >= len(line) {
+		return setting, errors.New("end-of-line before statement filter specification")
+	}
+	if len(line[fieldIdx]) > 1 {
+		return setting, errors.WithHint(
+			errors.New("multiple values specified for statement filter"),
+			"Specify exactly one statement filter per line.")
+	}
+	setting.IncludeStatements, err = parseStatementFilter(line[fieldIdx][0].Value)
+	return setting, err
+}
+
+func parseRole(role username.SQLUsername) error {
+	// Cannot use reserved role names.
+	if role.IsPublicRole() || role.IsNoneRole() {
+		return errors.Newf("cannot use reserved role name: '%s'", role.Normalized())
+	}
+	// Cannot use node user.
+	if role.IsNodeUser() {
+		return errors.Newf("cannot use reserved username: '%s'", role.Normalized())
+	}
+	// Cannot use 'pg_' prefix, reserved.
+	if strings.HasPrefix(role.Normalized(), "pg_") {
+		return errors.Newf("cannot use 'pg_' prefix in role name: '%s'", role.Normalized())
+	}
+	// Cannot use 'crdb_internal_' prefix, reserved.
+	if strings.HasPrefix(role.Normalized(), "crdb_internal_") {
+		return errors.Newf("cannot use 'crdb_internal_' prefix in role name: '%s'", role.Normalized())
+	}
+	return nil
+}
+
+// parseStatementFilter parses the statement filter field.
+func parseStatementFilter(stmtFilter string) (bool, error) {
+	val := strings.ToUpper(stmtFilter)
+	switch val {
+	case "ALL":
+		return true, nil
+	case "NONE":
+		return false, nil
+	default:
+		return false, errors.WithHint(errors.Newf(
+			`unknown statement filter: %q (valid filters include: "ALL", "NONE")`, stmtFilter,
+		), "Statement filter value is normalized (i.e. All, all are valid inputs for ALL)")
+	}
+}
