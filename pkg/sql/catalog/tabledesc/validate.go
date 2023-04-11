0570f0745941cb2f165d2bf27ef89866e670a9b1
Evan Wall <wall@cockroachlabs.com>
2023-04-11 15:31:15 -0400

    ttl: allow outbound foreign keys on TTL tables
    Fixes #101225
    
    This change allows TTL tables to have outbound foreign keys. Outbound foreign
    keys are unaffected by rows being deleted from the TTL table itself.
    
    Release note (sql change): Allow outbound foreign keys on TTL tables.

--- pkg/sql/catalog/tabledesc/validate.go
+++ pkg/sql/catalog/tabledesc/validate.go
@@ -196,23 +196,14 @@ func (desc *wrapper) ValidateForwardReferences(
 		}
 	}
 
-	// Row-level TTL is not compatible with foreign keys.
+	// Row-level TTL is not compatible with inbound foreign keys.
 	// This check should be in ValidateSelf but interferes with AllocateIDs.
-	if desc.HasRowLevelTTL() {
-		if len(desc.OutboundFKs) > 0 {
-			vea.Report(unimplemented.NewWithIssuef(
-				76407,
-				`foreign keys from table with TTL %q are not permitted`,
-				desc.GetName(),
-			))
-		}
-		if len(desc.InboundFKs) > 0 {
-			vea.Report(unimplemented.NewWithIssuef(
-				76407,
-				`foreign keys to table with TTL %q are not permitted`,
-				desc.GetName(),
-			))
-		}
+	if desc.HasRowLevelTTL() && len(desc.InboundFKs) > 0 {
+		vea.Report(unimplemented.NewWithIssuef(
+			76407,
+			`foreign keys to table with TTL %q are not permitted`,
+			desc.GetName(),
+		))
 	}
 
 	// Check foreign keys.
