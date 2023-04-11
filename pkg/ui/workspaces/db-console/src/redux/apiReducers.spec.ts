606b5a094358f89ae06fea3a58180e433aa9d2fc
Xin Hao Zhang <xzhang@cockroachlabs.com>
2023-04-11 15:38:52 -0400

    ui: beef up tests for sqlActivity utils, add  tests for `createSelectorForCachedDataField`
    Note that the `testUtils` file in the api folder is a forward port of
    testing utilities created for https://github.com/cockroachdb/cockroach/pull/99403
    
    This commit adds testing for functions in `sqlActivity/util.tsx` and
    adds some checks to those functions to make them more safe to use.
    
    The param `nodeRegions` is removed from `filteredStatementsData` as it
    was unused.
    
    Epic: none
    
    Release note: None

--- pkg/ui/workspaces/db-console/src/redux/apiReducers.spec.ts
+++ pkg/ui/workspaces/db-console/src/redux/apiReducers.spec.ts
@@ -12,8 +12,13 @@ import {
   generateTableID,
   databaseRequestPayloadToID,
   tableRequestToID,
+  createSelectorForCachedDataField,
 } from "./apiReducers";
 import { api as clusterUiApi } from "@cockroachlabs/cluster-ui";
+import { AdminUIState, createAdminUIStore } from "src/redux/state";
+import { createMemoryHistory } from "history";
+import { merge } from "lodash";
+import moment from "moment-timezone";
 
 describe("table id generator", function () {
   it("generates encoded db/table id", function () {
@@ -46,5 +51,71 @@ describe("request to string functions", function () {
     expect(tableRequestToID(tableRequest)).toEqual(
       generateTableID(database, table),
     );
+  });
+});
+
+describe("createSelectorForCachedDataField", () => {
+  const setAt = moment.utc();
+
+  function mockStoreState(): AdminUIState {
+    const store = createAdminUIStore(createMemoryHistory());
+    return merge(store.getState(), {
+      cachedData: {
+        statements: {
+          inFlight: true,
+          setAt,
+          reqAt: null,
+          data: null,
+          lastError: new Error("hello world"),
+          valid: true,
+        },
+        version: {
+          inFlight: false,
+          setAt: null,
+          reqAt: moment.utc(),
+          data: {
+            details: [
+              {
+                version: "hello",
+                detail: "world",
+              },
+            ],
+          },
+          lastError: null,
+          valid: false,
+        },
+      },
+    });
+  }
+  it("converts the fields of the cached data to RequestState", () => {
+    const state = mockStoreState();
+
+    const selectStmts = createSelectorForCachedDataField("statements");
+    const selectVersion = createSelectorForCachedDataField("version");
+
+    const stmtsState = selectStmts(state);
+    expect(stmtsState).toEqual({
+      inFlight: true,
+      lastUpdated: setAt,
+      data: null,
+      error: new Error("hello world"),
+      valid: true,
+    });
+
+    const versionState = selectVersion(state);
+    expect(versionState).toEqual({
+      inFlight: false,
+      lastUpdated: null,
+      data: {
+        details: [
+          {
+            version: "hello",
+            detail: "world",
+          },
+        ],
+      },
+      error: null,
+      valid: false,
+    });
   });
 });
