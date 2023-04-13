3fe4c9f3bf84fec0756321fe722d2ef1f5e1f24c
Xiang Gu <xiang@cockroachlabs.com>
2023-04-13 15:01:05 -0400

    storageparams: Introduce "schema_locked" which prevents schema changes
    Release note (sql change): We introduced a new table storage parameter
    "schema_locked", which, when set, prevents schema changes on that table.
    This parameter is only allowed to set/reset in a single-statement
    implicit transaction.
    Note that we whitelisted certain schema change stmts (e.g. GRANT, COMMENT)
    so that they are allowed even if this parameter is set.

--- pkg/sql/logictest/tests/local/generated_test.go
+++ pkg/sql/logictest/tests/local/generated_test.go
@@ -1682,6 +1682,13 @@ func TestLogic_schema_change_retry(
 	runLogicTest(t, "schema_change_retry")
 }
 
+func TestLogic_schema_locked(
+	t *testing.T,
+) {
+	defer leaktest.AfterTest(t)()
+	runLogicTest(t, "schema_locked")
+}
+
 func TestLogic_schema_repair(
 	t *testing.T,
 ) {
