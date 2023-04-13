fc8b1418e9f9fe17495562dff042517334155a16
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2023-04-13 10:34:59 -0400

    storage: don't return WriteTooOld on inline DeleteRange that hits non-inline value
    Fixes #101440.
    
    This commit resolves a condition where an inline DeleteRange request
    could return a WriteTooOld error when encountering a non-inline value,
    instead of returning the expected assertion error. This could trigger a
    fatal error in `tryBumpBatchTimestamp`. See the corresponding issue for
    more details.
    
    Note that this is not resolving any condition that could cause an inline
    DeleteRange request to hit a non-inline value, it's just ensuring that
    if corruption creates such a condition, we don't fatal.
    
    Release note: None

--- pkg/storage/mvcc_test.go
+++ pkg/storage/mvcc_test.go
@@ -1974,8 +1974,8 @@ func TestMVCCDeleteRangeInline(t *testing.T) {
 			const writeTooOldErrString = "WriteTooOldError"
 			if _, _, _, err := MVCCDeleteRange(
 				ctx, engine, nil, testKey6, keyMax, 1, hlc.Timestamp{Logical: 0}, nil, true,
-			); !testutils.IsError(err, writeTooOldErrString) {
-				t.Fatalf("got error %v, expected error with text '%s'", err, writeTooOldErrString)
+			); !testutils.IsError(err, inlineMismatchErrString) {
+				t.Fatalf("got error %v, expected error with text '%s'", err, inlineMismatchErrString)
 			}
 
 			// Attempt to delete inline keys in a transaction; should fail.
@@ -2797,7 +2797,9 @@ func TestMVCCReverseScanSeeksOverRepeatedKeys(t *testing.T) {
 // The bug happened in this scenario.
 // (1) reverse scan is positioned at the range's smallest key and calls `prevKey()`
 // (2) `prevKey()` peeks and sees newer versions of the same logical key
-//	   `iters_before_seek_-1` times, moving the iterator backwards each time
+//
+//	`iters_before_seek_-1` times, moving the iterator backwards each time
+//
 // (3) on the `iters_before_seek_`th peek, there are no previous keys found
 //
 // Then, the problem was `prevKey()` treated finding no previous key as if it had found a
