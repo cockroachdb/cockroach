3209d6a1baa8da9c473a59bcf7418947a5de1e89
Erik Grinaker <grinaker@cockroachlabs.com>
2023-04-16 11:36:25 +0000

    kvserver: skip correct `TestTruncateLog`
    5f0765f skipped the wrong `TestTruncateLog`.
    
    Epic: none
    Release note: none

--- pkg/kv/kvserver/batcheval/cmd_truncate_log_test.go
+++ pkg/kv/kvserver/batcheval/cmd_truncate_log_test.go
@@ -18,7 +18,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/storage"
-	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
@@ -56,8 +55,6 @@ func readTruncStates(
 func TestTruncateLog(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
-
-	skip.WithIssue(t, 79719)
 
 	ctx := context.Background()
 	const (
