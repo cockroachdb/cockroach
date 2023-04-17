5cc11e0ea872d6580a4bdccca088ebfab3ec8d11
Marcus Gartner <marcus@cockroachlabs.com>
2023-04-17 18:22:47 -0400

    opt: copy ordering choice columns
    This commit fixes a long-standing bug in `OrderingChoice.Copy` in which
    the `Group` columns were not being copied. As far as we now, this has
    caused no user-facing bugs, but it does not follow `Copy`'s description
    that the new `OrderingChoice` has a "private" version of the ordering
    column slice, and column sets shared between `OrderingChoice`s could
    lead to future bugs.
    
    Release note: None

--- pkg/sql/opt/props/ordering_choice.go
+++ pkg/sql/opt/props/ordering_choice.go
@@ -657,12 +657,15 @@ func (oc *OrderingChoice) AppendCol(id opt.ColumnID, descending bool) {
 }
 
 // Copy returns a complete copy of this instance, with a private version of the
-// ordering column array.
+// ordering column slice.
 func (oc *OrderingChoice) Copy() OrderingChoice {
 	var other OrderingChoice
 	other.Optional = oc.Optional.Copy()
 	other.Columns = make([]OrderingColumnChoice, len(oc.Columns))
 	copy(other.Columns, oc.Columns)
+	for i := range other.Columns {
+		other.Columns[i].Group = other.Columns[i].Group.Copy()
+	}
 	return other
 }
 
