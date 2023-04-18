5135fea5928b3928fed28bcc83a086a89b6a2512
Renato Costa <renato@cockroachlabs.com>
2023-04-18 11:04:58 -0400

    roachtest: SSH errors and cluster creation failure not release blockers
    This removes the automatic `release-blocker` label from errors
    related to SSH flakes or cluster creation failures. Every time we
    create a new release branch, we invariably hit one of these situations
    and have to manually remove the label after being pinged by the
    release team.
    
    Epic: none
    
    Release note: None

--- pkg/cmd/roachtest/github_test.go
+++ pkg/cmd/roachtest/github_test.go
@@ -163,9 +163,7 @@ func TestCreatePostRequest(t *testing.T) {
 		// !nonReleaseBlocker and issue is an SSH flake. Also ensure that
 		// in the event of a failed cluster creation, nil `vmOptions` and
 		// `clusterImpl` are not dereferenced
-		// expectedReleaseBlocker is true on this branch due to a backport, which doesn't include
-		// a change to exclude infra flakes from being labeled as release blockers.
-		{false, true, false, false, "", createFailure(rperrors.ErrSSH255), true, true,
+		{false, true, false, false, "", createFailure(rperrors.ErrSSH255), true, false,
 			prefixAll(map[string]string{
 				"cloud": "gce",
 				"ssd":   "0",
