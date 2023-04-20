6b05f7cf30f9047b39e4ca6e8730015fc96f0c38
rharding6373 <rharding6373@users.noreply.github.com>
2023-04-20 14:12:48 -0700

    sql: add enums to statement bundles
    Statement bundles now include the `CREATE` statements for enums in
    `schema.sql`.
    
    Epic: None
    Fixes: #100377
    
    Release note: None

--- pkg/cli/testdata/explain-bundle/bundle/schema.sql
+++ pkg/cli/testdata/explain-bundle/bundle/schema.sql
@@ -1,10 +1,14 @@
-CREATE TABLE public.a (
-	a INT8 NOT NULL,
-	b INT8 NULL,
-	CONSTRAINT "primary" PRIMARY KEY (a ASC),
-	FAMILY "primary" (a, b)
+CREATE TABLE public.a
+(
+  a      INT8 NOT NULL,
+  b      INT8 NULL,
+  CONSTRAINT "primary" PRIMARY KEY (a ASC),
+  FAMILY "primary" (a, b)
 );
 
-CREATE TABLE public."order" (
-    id INT8 PRIMARY KEY
+CREATE TABLE public."order"
+(
+  id INT8 PRIMARY KEY
 );
+
+CREATE TYPE public.d AS ENUM {'Monday', 'Tuesday'}
