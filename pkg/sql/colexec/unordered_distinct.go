63cd59cecc587286a203659ec6a84ae6614889bf
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-04-20 15:58:16 -0700

    colexec: fix hash table memory accounting for hash aggregator
    The hash table has multiple slices that are limited by the maximum
    length of the probing batch. Previously, we hard-coded that length as
    `coldata.BatchSize() = 1024`, but that is incorrect for the hash
    aggregator.  The hash aggregator (for performance reasons) buffers up
    to `coldata.MaxBatchSize = 4096` tuples before using the hash table.
    As a result, whenever we performed the hash aggregation on more than
    1024 tuples, we wouldn't account for those slices correctly, and in the
    worst case the under accounting would exceed 100KiB which could be
    non-trivial if hash aggregation queries are run with some concurrency.
    This commit fixes that oversight.
    
    Epic: None
    
    Release note: None

--- pkg/sql/colexec/unordered_distinct.go
+++ pkg/sql/colexec/unordered_distinct.go
@@ -83,6 +83,7 @@ func (op *UnorderedDistinct) Init(ctx context.Context) {
 	op.Ht = colexechash.NewHashTable(
 		op.Ctx,
 		op.hashTableAllocator,
+		coldata.BatchSize(),
 		hashTableLoadFactor,
 		op.hashTableNumBuckets,
 		op.typs,
