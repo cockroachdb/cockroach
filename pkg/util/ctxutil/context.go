9c3ade6cd7174e4392d1bc851ba2a1146a78a020
Yevgeniy Miretskiy <yevgeniy@cockroachlabs.com>
2023-04-20 07:16:03 -0400

    ctxutil: Add `ctxutil.WhenDone`
    Add `ctxutil.WhenDone` function which invokes a callback when
    parent context becomes done.
    
    ```
    parent, cancelParent := context.WithTimeout(....)
    ctxutil.WhenDone(parent, func() {....})
    ```
    
    Normally, in order to invoke a function when context becomes
    done, a go routine should be spun up that listens on
    `ctx.Done()` channel.  Sometimes, this is too heavy weight.
    `ctxutil.WhenDone` adds such a mechanism, which under most
    circumstances, simply invokes the function when context completes.
    
    When parent context becomes done, the funciton is invoked
    with the result (Err()) of the parent context.
    
    If the parent context never completes, `WhenDone` returns an error.
    
    Go versions 1.20 or higher also add ability to augment
    context error message with "cause" error.
    
    Epic: None
    
    Release note: None

---
+++ pkg/util/ctxutil/context.go
@@ -0,0 +1,53 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package ctxutil
+
+import (
+	"context"
+	_ "unsafe" // Must import unsafe to enable gross hack below.
+
+	"github.com/cockroachdb/errors"
+)
+
+// WhenDoneFunc is the callback invoked by context when it becomes done.
+// The callback is passed the error from the parent context.
+type WhenDoneFunc func(err error)
+
+// WhenDoneCauseFunc accepts context error (context.Err()) as well
+// as the cause for cancellation (cause is nil prior to go1.20).
+type WhenDoneCauseFunc func(err, cause error)
+
+// ErrNeverCompletes is an error indicating that the context never completes.
+var ErrNeverCompletes = errors.New("context never completes")
+
+// WhenDone arranges for the specified function to be invoked when
+// parent context becomes done.
+// If the context never becomes done, returns ErrNeverCompletes error.
+func WhenDone(parent context.Context, done WhenDoneFunc) error {
+	if parent.Done() == nil {
+		return ErrNeverCompletes
+	}
+	c := &whenDone{Context: parent, notify: func(err, cause error) { done(err) }}
+	context_propagateCancel(parent, c)
+	return nil
+}
+
+type whenDone struct {
+	context.Context
+	notify WhenDoneCauseFunc
+}
+
+func (c *whenDone) cancelWithCause(removeFromParent bool, err, cause error) {
+	c.notify(err, cause)
+	if removeFromParent {
+		context_removeChild(c.Context, c)
+	}
+}
