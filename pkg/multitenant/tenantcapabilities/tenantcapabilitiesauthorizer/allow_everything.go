b677dea3134c1005982f4e00c1e1af7ecd81f06a
Raphael 'kena' Poss <knz@thaumogen.net>
2023-04-20 15:11:27 +0200

    tenantcapabilitiesauthorizer: avoid using the word "noop"
    Prior to this patch, an implementation of the `Authorizer` interface
    was called "Noop". Unfortunately, "no-op" does not tell the reader
    clearly what an *authorization policy* does. What does it allow? What
    does it reject?
    
    This patch clarifies that situation by renaming it to
    "AllowEverythingAuthorizer".
    
    Additionally, the RPC context for tenant servers was configured
    with this authorizer implementation, which is conceptually confusing:
    it would conceptually allow RPC clients to solicit KV operations from
    it. Instead, this patch introduces a new "AllowNothingAuthorizer" for
    that case.
    
    (Note: this is purely conceptual since the RPC service for tenant
    server does not, in fact, offer the roachpb.Internal service.)
    
    Release note: None

--- pkg/multitenant/tenantcapabilities/tenantcapabilitiesauthorizer/allow_everything.go
+++ pkg/multitenant/tenantcapabilities/tenantcapabilitiesauthorizer/allow_everything.go
@@ -18,47 +18,51 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 )
 
-// NoopAuthorizer is a tenantcapabilities.Authorizer that simply no-ops.
-type NoopAuthorizer struct{}
+// AllowEverythingAuthorizer is a tenantcapabilities.Authorizer that
+// allows all operations
+type AllowEverythingAuthorizer struct{}
 
-var _ tenantcapabilities.Authorizer = &NoopAuthorizer{}
+var _ tenantcapabilities.Authorizer = &AllowEverythingAuthorizer{}
 
-// NewNoopAuthorizer constructs and returns a NoopAuthorizer.
-func NewNoopAuthorizer() *NoopAuthorizer {
-	return &NoopAuthorizer{}
+// NewAllowEverythingAuthorizer constructs and returns a AllowEverythingAuthorizer.
+func NewAllowEverythingAuthorizer() *AllowEverythingAuthorizer {
+	return &AllowEverythingAuthorizer{}
 }
 
 // HasCapabilityForBatch implements the tenantcapabilities.Authorizer interface.
-func (n *NoopAuthorizer) HasCapabilityForBatch(
+func (n *AllowEverythingAuthorizer) HasCapabilityForBatch(
 	context.Context, roachpb.TenantID, *kvpb.BatchRequest,
 ) error {
 	return nil
 }
 
 // BindReader implements the tenantcapabilities.Authorizer interface.
-func (n *NoopAuthorizer) BindReader(tenantcapabilities.Reader) {
-}
+func (n *AllowEverythingAuthorizer) BindReader(tenantcapabilities.Reader) {}
 
 // HasNodeStatusCapability implements the tenantcapabilities.Authorizer interface.
-func (n *NoopAuthorizer) HasNodeStatusCapability(
+func (n *AllowEverythingAuthorizer) HasNodeStatusCapability(
 	ctx context.Context, tenID roachpb.TenantID,
 ) error {
 	return nil
 }
 
 // HasTSDBQueryCapability implements the tenantcapabilities.Authorizer interface.
-func (n *NoopAuthorizer) HasTSDBQueryCapability(ctx context.Context, tenID roachpb.TenantID) error {
+func (n *AllowEverythingAuthorizer) HasTSDBQueryCapability(
+	ctx context.Context, tenID roachpb.TenantID,
+) error {
 	return nil
 }
 
 // HasNodelocalStorageCapability implements the tenantcapabilities.Authorizer interface.
-func (n *NoopAuthorizer) HasNodelocalStorageCapability(
+func (n *AllowEverythingAuthorizer) HasNodelocalStorageCapability(
 	ctx context.Context, tenID roachpb.TenantID,
 ) error {
 	return nil
 }
 
 // IsExemptFromRateLimiting implements the tenantcapabilities.Authorizer interface.
-func (n *NoopAuthorizer) IsExemptFromRateLimiting(context.Context, roachpb.TenantID) bool {
+func (n *AllowEverythingAuthorizer) IsExemptFromRateLimiting(
+	context.Context, roachpb.TenantID,
+) bool {
 	return true
 }
---
+++ pkg/multitenant/tenantcapabilities/tenantcapabilitiesauthorizer/allow_nothing.go
@@ -0,0 +1,67 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package tenantcapabilitiesauthorizer
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
+	"github.com/cockroachdb/cockroach/pkg/multitenant/tenantcapabilities"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/errors"
+)
+
+// AllowNothingAuthorizer is a tenantcapabilities.Authorizer that
+// allows all operations
+type AllowNothingAuthorizer struct{}
+
+var _ tenantcapabilities.Authorizer = &AllowNothingAuthorizer{}
+
+// NewAllowNothingAuthorizer constructs and returns a AllowNothingAuthorizer.
+func NewAllowNothingAuthorizer() *AllowNothingAuthorizer {
+	return &AllowNothingAuthorizer{}
+}
+
+// HasCapabilityForBatch implements the tenantcapabilities.Authorizer interface.
+func (n *AllowNothingAuthorizer) HasCapabilityForBatch(
+	context.Context, roachpb.TenantID, *kvpb.BatchRequest,
+) error {
+	return errors.New("operation blocked")
+}
+
+// BindReader implements the tenantcapabilities.Authorizer interface.
+func (n *AllowNothingAuthorizer) BindReader(tenantcapabilities.Reader) {}
+
+// HasNodeStatusCapability implements the tenantcapabilities.Authorizer interface.
+func (n *AllowNothingAuthorizer) HasNodeStatusCapability(
+	ctx context.Context, tenID roachpb.TenantID,
+) error {
+	return errors.New("operation blocked")
+}
+
+// HasTSDBQueryCapability implements the tenantcapabilities.Authorizer interface.
+func (n *AllowNothingAuthorizer) HasTSDBQueryCapability(
+	ctx context.Context, tenID roachpb.TenantID,
+) error {
+	return errors.New("operation blocked")
+}
+
+// HasNodelocalStorageCapability implements the tenantcapabilities.Authorizer interface.
+func (n *AllowNothingAuthorizer) HasNodelocalStorageCapability(
+	ctx context.Context, tenID roachpb.TenantID,
+) error {
+	return errors.New("operation blocked")
+}
+
+// IsExemptFromRateLimiting implements the tenantcapabilities.Authorizer interface.
+func (n *AllowNothingAuthorizer) IsExemptFromRateLimiting(context.Context, roachpb.TenantID) bool {
+	return false
+}
