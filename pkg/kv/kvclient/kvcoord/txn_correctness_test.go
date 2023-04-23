040c493f031b201d2784e18dadffb74f2c122995
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2023-04-23 15:24:46 -0400

    kv: add TestTxnDBDirtyReadAnomaly to txn correctness tests
    This commit adds testing of the "dirty read" anomaly to the txn
    correctness test suite.
    
    With dirty reads, a transaction writes to a key while a concurrent
    transaction reads from that key. The writing transaction then rolls
    back. If the reading transaction observes the write, either before or
    after the rollback, it has experienced a dirty read.
    
    Epic: None
    Release note: None

--- pkg/kv/kvclient/kvcoord/txn_correctness_test.go
+++ pkg/kv/kvclient/kvcoord/txn_correctness_test.go
@@ -1059,6 +1059,34 @@ func testTxnDBDirtyWriteAnomalyBothCommit(t *testing.T) {
 	checkConcurrency("dirty write", allLevels, []string{txn1, txn2}, verify, t)
 }
 
+// TestTxnDBDirtyReadAnomaly verifies that none of RC, SI, or SSI
+// isolation are subject to the dirty read anomaly.
+//
+// With dirty reads, a transaction writes to a key while a concurrent
+// transaction reads from that key. The writing transaction then rolls
+// back. If the reading transaction observes the write, either before or
+// after the rollback, it has experienced a dirty read.
+//
+// Dirty reads would typically fail with a history such as:
+//
+//	W1(A) R2(A) A1 C2
+func TestTxnDBDirtyReadAnomaly(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	txn1 := "W(A,1) A"
+	txn2 := "R(A) W(B,A) C"
+	verify := &verifier{
+		history: "R(B)",
+		checkFn: func(env map[string]int64) error {
+			if env["B"] != 0 {
+				return errors.Errorf("expected B=0, got %d", env["B"])
+			}
+			return nil
+		},
+	}
+	checkConcurrency("dirty read", allLevels, []string{txn1, txn2}, verify, t)
+}
+
 // TestTxnDBReadSkewAnomaly verifies that neither SI nor SSI isolation
 // are subject to the read skew anomaly, an example of a database
 // constraint violation known as inconsistent analysis[^1]. This
