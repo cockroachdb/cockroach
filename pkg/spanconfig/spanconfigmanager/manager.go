e78921c2e257f60f812bd065b4a60c9260853f96
David Taylor <tinystatemachine@gmail.com>
2023-04-24 18:29:55 +0000

    jobs: use job_type column for RunningJobExists queries
    Avoiding the virtual table avoids the join on infos.
    
    Release note: none.
    Epic: none.

--- pkg/spanconfig/spanconfigmanager/manager.go
+++ pkg/spanconfig/spanconfigmanager/manager.go
@@ -185,7 +185,7 @@ func (m *Manager) createAndStartJobIfNoneExists(ctx context.Context) (bool, erro
 
 	var job *jobs.Job
 	if err := m.db.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
-		exists, err := jobs.RunningJobExists(ctx, jobspb.InvalidJobID, txn,
+		exists, err := jobs.RunningJobExists(ctx, jobspb.InvalidJobID, txn, m.settings.Version,
 			jobspb.TypeAutoSpanConfigReconciliation)
 		if err != nil {
 			return err
