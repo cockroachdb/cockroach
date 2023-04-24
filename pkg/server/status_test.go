af1aec27b16c4bc50dc775cac10efed5288ee965
Andrii Vorobiov <and.vorobiov@gmail.com>
2023-04-24 15:07:59 +0300

    status, server: expose network connectivity endpoint
    This patch introduces new endpoint which aimed to provide information
    about network connection status between nodes rather than nodes statuses
    (that is done with node liveness api).
    
    `NetworkConnectivity` endpoint relies on gossip client to get all known
    nodes in the cluster and then checks `rpcCtx.ConnHealth` for every peer.
    It can tell us following:
    - connection is live if no error returned;
    - `ErrNoConnection` returned if node is attempting to connect
    to target node;
    - other errors indicate that network connection is unhealthy;
    
    This functionality should not care about whether node is decommissioned,
    shutdown or whatever.
    
    Later on, it'll be used in conjunction with node liveness statuses to
    distinguish more specific reasons why network connection is broken.
    For instance,
    - if node liveness status is DEAD and connection is failed,
    then it should not be considered as network issue.
    - if node liveness status is LIVE/DECOMMISSIONING and network
    connection is failed, then it is network partitioning;
    
    Release note: None

--- pkg/server/status_test.go
+++ pkg/server/status_test.go
@@ -1127,6 +1127,51 @@ func TestHotRanges2Response(t *testing.T) {
 	}
 }
 
+func TestNetworkConnectivity(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	numNodes := 3
+	testCluster := serverutils.StartNewTestCluster(t, numNodes, base.TestClusterArgs{
+		ReplicationMode: base.ReplicationManual,
+	})
+	ctx := context.Background()
+	defer testCluster.Stopper().Stop(ctx)
+	ts := testCluster.Server(0)
+
+	var resp serverpb.NetworkConnectivityResponse
+	// Should wait because endpoint relies on Gossip.
+	testutils.SucceedsSoon(t, func() error {
+		if err := getStatusJSONProto(ts, "connectivity", &resp); err != nil {
+			return err
+		}
+		if len(resp.ErrorsByNodeID) > 0 {
+			return errors.Errorf("expected no errors but got: %d", len(resp.ErrorsByNodeID))
+		}
+		if len(resp.Connections) < numNodes {
+			return errors.Errorf("expected results from %d nodes but got: %d", numNodes, len(resp.ErrorsByNodeID))
+		}
+		return nil
+	})
+	// Test when one node is stopped.
+	stoppedNodeID := testCluster.Server(1).NodeID()
+	testCluster.Server(1).Stopper().Stop(ctx)
+
+	testutils.SucceedsSoon(t, func() error {
+		if err := getStatusJSONProto(ts, "connectivity", &resp); err != nil {
+			return err
+		}
+		require.Equal(t, len(resp.Connections), numNodes-1)
+		fmt.Printf("got status: %s", resp.Connections[ts.NodeID()].Peers[stoppedNodeID].Status.String())
+		if resp.Connections[ts.NodeID()].Peers[stoppedNodeID].Status != serverpb.NetworkConnectivityResponse_ERROR {
+			return errors.New("waiting for connection state to be changed.")
+		}
+		if latency := resp.Connections[ts.NodeID()].Peers[stoppedNodeID].Latency; latency > 0 {
+			return errors.Errorf("expected latency to be 0 but got %s", latency.String())
+		}
+		return nil
+	})
+}
+
 func TestHotRanges2ResponseWithViewActivityOptions(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
