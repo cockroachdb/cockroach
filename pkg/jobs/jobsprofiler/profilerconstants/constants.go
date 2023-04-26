c887de2fc4d31c838cb3a0ec8a68a848f2550cff
adityamaru <adityamaru@gmail.com>
2023-04-25 23:23:04 -0400

    jobs, backupccl: persist per node, per processor backup progress
    This change teaches the coordinator node of the backup job to
    periodically persist the fraction progress per node and per processor
    on each node in the `system.job_info` table. This information will
    be surfaced in a future change via `SHOW JOB WITH EXECUTION DETAILS`
    to provide more observability into how much work each processor has
    remaining during a backup.
    
    This change also introduces and InfoKey interface that defines methods
    that should be implemented by all info keys used in the job_info table.
    This change does not go so far as changing the parameter type of the various
    job info storage methods to use the `InfoKey` interface. This will be done in
    a future change.
    
    Informs: #100488
    Release note: None

--- pkg/jobs/jobsprofiler/profilerconstants/constants.go
+++ pkg/jobs/jobsprofiler/profilerconstants/constants.go
@@ -10,6 +10,32 @@
 
 package profilerconstants
 
-// DSPDiagramInfoKeyPrefix is the prefix of the info key used for rows that
-// store the DistSQL plan diagram for a job.
-const DSPDiagramInfoKeyPrefix = "dsp-diag-url-"
+import (
+	"fmt"
+
+	"github.com/cockroachdb/errors"
+)
+
+const DSPDiagramInfoKeyPrefix = "~dsp-diag-url-"
+
+// MakeDSPDiagramInfoKey constructs an ephemeral DSP diagram info key.
+func MakeDSPDiagramInfoKey(parts ...any) (string, error) {
+	if len(parts) != 1 {
+		return "", errors.AssertionFailedf("expected 1 part got %d", len(parts))
+	}
+	return fmt.Sprintf("%s%d", DSPDiagramInfoKeyPrefix, parts[0]), nil
+}
+
+// NodeProcessorProgressInfoKeyPrefix is the prefix of the info key used for
+// rows that store the per node, per processor progress for a job.
+const NodeProcessorProgressInfoKeyPrefix = "~node-processor-progress-"
+
+// MakeNodeProcessorProgressInfoKey returns the info_key used for rows that
+// store the per node, per processor progress for a job.
+func MakeNodeProcessorProgressInfoKey(parts ...any) (string, error) {
+	if len(parts) != 3 {
+		return "", errors.AssertionFailedf("expected 3 parts got %d", len(parts))
+	}
+	// The info key is of the form: <prefix>-<flowID>,<instanceID>,<processorID>.
+	return fmt.Sprintf("%s%s,%s,%d", NodeProcessorProgressInfoKeyPrefix, parts[0], parts[1], parts[2]), nil
+}
