fef083b9e394d6bd8e1b5287ff8dde44a5951c8e
Evan Wall <wall@cockroachlabs.com>
2023-04-27 09:10:31 -0400

    ttl: rename span to bounds
    Release note: None

--- pkg/sql/ttl/ttljob/ttljob_processor.go
+++ pkg/sql/ttl/ttljob/ttljob_processor.go
@@ -181,16 +181,16 @@ func (t *ttlProcessor) work(ctx context.Context) error {
 		processorConcurrency = processorSpanCount
 	}
 	err := func() error {
-		spanChan := make(chan QueryBounds, processorConcurrency)
-		defer close(spanChan)
+		boundsChan := make(chan QueryBounds, processorConcurrency)
+		defer close(boundsChan)
 		for i := int64(0); i < processorConcurrency; i++ {
 			group.GoCtx(func(ctx context.Context) error {
-				for spanToProcess := range spanChan {
+				for bounds := range boundsChan {
 					start := timeutil.Now()
-					spanRowCount, err := t.runTTLOnSpan(
+					spanRowCount, err := t.runTTLOnQueryBounds(
 						ctx,
 						metrics,
-						spanToProcess,
+						bounds,
 						pkColNames,
 						pkColDirs,
 						relationName,
@@ -202,7 +202,7 @@ func (t *ttlProcessor) work(ctx context.Context) error {
 					if err != nil {
 						// Continue until channel is fully read.
 						// Otherwise, the keys input will be blocked.
-						for spanToProcess = range spanChan {
+						for bounds = range boundsChan {
 						}
 						return err
 					}
@@ -224,7 +224,7 @@ func (t *ttlProcessor) work(ctx context.Context) error {
 			if err != nil {
 				return errors.Wrapf(err, "decode endKey error key=%x", []byte(endKey))
 			}
-			spanChan <- QueryBounds{
+			boundsChan <- QueryBounds{
 				Start: startPK,
 				End:   endPK,
 			}
@@ -270,7 +270,7 @@ func (t *ttlProcessor) work(ctx context.Context) error {
 }
 
 // spanRowCount should be checked even if the function returns an error because it may have partially succeeded
-func (t *ttlProcessor) runTTLOnSpan(
+func (t *ttlProcessor) runTTLOnQueryBounds(
 	ctx context.Context,
 	metrics rowLevelTTLMetrics,
 	bounds QueryBounds,
