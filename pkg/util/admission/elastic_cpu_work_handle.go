5deca5f9979d2df62b67c2237d765f88ccf75267
irfan sharif <irfanmahmoudsharif@gmail.com>
2023-05-05 17:11:49 -0400

    admission: defend against severe elastic cpu token debt
    Speculative fix for #102817.
    Speculative fix for #103359.
    
    In experiments and in production clusters, we observed elastic CPU wait
    queue durations in the order of minutes, which can be long enough to
    fail entire backups. Separately, we saw that with the elastic CPU
    limiter enabled, backups can sometimes be extremely inefficient, likely
    exporting a single key per ExportRequest (#101685 will improve this
    observability). We suspect that happened because by the time we actually
    got to the mvccExportToSST loop, we'd already expended the 100ms
    CPU-time grant we were given resolving intents or doing conflict
    resolution. Now we instead start the CPU timer per request once actually
    in the mvccExportToWriter loop, not counting all the "pre-work" CPU time
    against our allotted budget. We still deduct CPU tokens for the pre-work
    to avoid risking over-admission, and export a metric for how much
    pre-work we're doing. As a defense-in-depth thing, we also periodically
    reset the elastic CPU token bucket periodically (every 30s), again
    permitting some amount of over-admission.
    
    While here, we export a few other metrics in the elastic CPU limiter
    stack to better diagnose issues:
    - admission.elastic_cpu.nanos_exhausted_duration, which is the total
      duration when elastic CPU tokens were exhausted, in micros. We have
      equivalents for {IO token, CPU slot} exhaustion.
    - admission.elastic_cpu.over_limit_durations, a latency histogram that
      surfaces exactly how much over the 100ms grants we go, including
      pre-work. This is quite high before/after this patch when
      ExportRequests can spend time resolving intents, pushing txns, doing
      conflict resolution. Except now we still give 100ms of on-CPU to to
      mvccExportToWriter.
    - admission.elastic_cpu.pre_work_nanos, the elastic CPU time spent doing
      pre-work. See commentary above.
    - admission.elastic_cpu.available_nanos, an instantaneous metric that
      surfaces exactly how many tokens are available. Useful to understand
      how much debt we're in.
    
    Release note (bug fix): In earlier patch releases of v23.1, it was
    possible for backups to be excessively slow, slower than they were in
    earlier releases. It was also possible for them to fail with errors of
    the following form: 'operation "Export Request for span ..." timed out
    after 5m0.001s'. At least one of the reasons for this behavior is now
    addressed. This problem also affected v22.2 clusters if using a
    hidden-by-default, default-as-disabled cluster setting
    'admission.elastic_cpu.enabled'.

--- pkg/util/admission/elastic_cpu_work_handle.go
+++ pkg/util/admission/elastic_cpu_work_handle.go
@@ -28,6 +28,11 @@ type ElasticCPUWorkHandle struct {
 	// before indicating that it's over limit. It measures the duration since
 	// cpuStart.
 	allotted time.Duration
+	// preWork measures how much on-CPU running time this request accrued before
+	// starting doing the actual work it intended to. We don't want the preWork
+	// duration to count against what it was allotted, but we still want to
+	// track to deduct an appropriate number of granter tokens.
+	preWork time.Duration
 
 	// This handle is used in tight loops that are sensitive to per-iteration
 	// overhead (checking against the running time too can have an effect). To
@@ -42,17 +47,35 @@ type ElasticCPUWorkHandle struct {
 	// between that running time and what this handle was allotted.
 	runningTimeAtLastCheck, differenceWithAllottedAtLastCheck time.Duration
 
-	testingOverrideRunningTime func() time.Duration
-
-	testingOverrideOverLimit func() (bool, time.Duration)
+	testingOverrideRunningTime func() time.Duration // overrides the time since cpuStart
+	testingOverrideOverLimit   func() (bool, time.Duration)
 }
 
 func newElasticCPUWorkHandle(allotted time.Duration) *ElasticCPUWorkHandle {
 	h := &ElasticCPUWorkHandle{allotted: allotted}
-	h.cpuStart = h.runningTime()
+	h.cpuStart = grunning.Time()
 	return h
 }
 
+// StartTimer is used to denote that we're just starting to do the actual on-CPU
+// work we acquired CPU tokens for. It's possible for requests to do
+// semi-unrelated pre-work up until this point, time spent that we don't want to
+// count against our allotted CPU time. But we still want to deduct granter
+// tokens for that time to avoid over-admission. StartTimer makes note of how
+// much pre-work was done and starts counting any subsequent on-CPU time against
+// what was allotted. If StartTimer is never invoked, all on-CPU time is counted
+// against what was allotted. It can only be called once.
+func (h *ElasticCPUWorkHandle) StartTimer() {
+	if h == nil {
+		return
+	}
+	h.preWork = h.runningTime()
+	h.cpuStart = grunning.Time()
+}
+
+// runningTime returns the time spent since cpuStart. cpuStart is captured when
+// the handle is first constructed, and optionally reset if callers make use of
+// StartTimer.
 func (h *ElasticCPUWorkHandle) runningTime() time.Duration {
 	if h == nil {
 		return time.Duration(0)
@@ -64,10 +87,19 @@ func (h *ElasticCPUWorkHandle) runningTime() time.Duration {
 }
 
 // OverLimit is used to check whether we're over the allotted elastic CPU
-// tokens. It also returns the absolute time difference between how long we ran
-// for and what was allotted. Integrated callers are expected to invoke this in
-// tight loops (we assume most callers are CPU-intensive and thus have tight
-// loops somewhere) and bail once done.
+// tokens. If StartTimer was invoked, we start measuring on-CPU time only after
+// the invocation. It also returns the total time difference between how long we
+// ran for and what was allotted. The difference includes pre-work, if any. If
+// the difference is positive we've exceeded what was allotted, and vice versa.
+// It's possible to not be OverLimit but still have exceeded the allotment
+// (because of excessive pre-work).
+//
+// Integrated callers are expected to invoke this in tight loops (we assume most
+// callers are CPU-intensive and thus have tight loops somewhere) and bail once
+// done.
+//
+// TODO(irfansharif): Non-test callers use one or the other return value, not
+// both. Split this API?
 func (h *ElasticCPUWorkHandle) OverLimit() (overLimit bool, difference time.Duration) {
 	if h == nil { // not applicable
 		return false, time.Duration(0)
@@ -76,7 +108,7 @@ func (h *ElasticCPUWorkHandle) OverLimit() (overLimit bool, difference time.Dura
 	// What we're effectively doing is just:
 	//
 	// 		runningTime := h.runningTime()
-	// 		return runningTime > h.allotted, grunning.Difference(runningTime, h.allotted)
+	// 		return runningTime > h.allotted, runningTime - h.allotted
 	//
 	// But since this is invoked in tight loops where we're sensitive to
 	// per-iteration overhead (the naive form described above causes a 5%
@@ -88,7 +120,7 @@ func (h *ElasticCPUWorkHandle) OverLimit() (overLimit bool, difference time.Dura
 	// adjust for it elsewhere by penalizing subsequent waiters.
 	h.itersSinceLastCheck++
 	if h.itersSinceLastCheck < h.itersUntilCheck {
-		return false, h.differenceWithAllottedAtLastCheck
+		return false, h.preWork + h.differenceWithAllottedAtLastCheck
 	}
 	return h.overLimitInner()
 }
@@ -100,7 +132,7 @@ func (h *ElasticCPUWorkHandle) overLimitInner() (overLimit bool, difference time
 
 	runningTime := h.runningTime()
 	if runningTime >= h.allotted {
-		return true, grunning.Difference(runningTime, h.allotted)
+		return true, h.preWork + (runningTime - h.allotted)
 	}
 
 	if h.itersUntilCheck == 0 {
@@ -112,9 +144,9 @@ func (h *ElasticCPUWorkHandle) overLimitInner() (overLimit bool, difference time
 		}
 	}
 
-	h.runningTimeAtLastCheck, h.differenceWithAllottedAtLastCheck = runningTime, grunning.Difference(runningTime, h.allotted)
+	h.runningTimeAtLastCheck, h.differenceWithAllottedAtLastCheck = runningTime, runningTime-h.allotted
 	h.itersSinceLastCheck = 0
-	return false, h.differenceWithAllottedAtLastCheck
+	return false, h.differenceWithAllottedAtLastCheck + h.preWork
 }
 
 // TestingOverrideOverLimit allows tests to override the behaviour of
@@ -151,9 +183,8 @@ func TestingNewElasticCPUHandle() *ElasticCPUWorkHandle {
 	return newElasticCPUWorkHandle(420 * time.Hour) // use a very high allotment
 }
 
-// TestingNewElasticCPUWithCallback constructs an
-// ElascticCPUWorkHandle with a testing override for the behaviour of
-// OverLimit().
+// TestingNewElasticCPUHandleWithCallback constructs an ElasticCPUWorkHandle
+// with a testing override for the behaviour of OverLimit().
 func TestingNewElasticCPUHandleWithCallback(cb func() (bool, time.Duration)) *ElasticCPUWorkHandle {
 	h := TestingNewElasticCPUHandle()
 	h.testingOverrideOverLimit = cb
