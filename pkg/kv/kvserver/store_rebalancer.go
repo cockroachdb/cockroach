518fd7e8f1d825d45565fa8e9f8d7ef9dc8a5ea1
Austen McClernon <austen@cockroachlabs.com>
2023-05-05 18:22:37 +0000

    kvserver: annotate rebalance ctx with objective
    Previously, the easiest method of determining the current rebalance
    objective from logs was to view the cluster setting and check for
    logging indicating a mixed version cluster - this was cumbersome.
    
    This commit annotates the ctx in the store rebalancer loop with an
    additional tag: `obj`. The `obj` tag indicates the current rebalance
    objective, either `cpu` or `qps` currently.
    
    resolves: #102812
    
    Release note: None

--- pkg/kv/kvserver/store_rebalancer.go
+++ pkg/kv/kvserver/store_rebalancer.go
@@ -276,6 +276,9 @@ func (sr *StoreRebalancer) Start(ctx context.Context, stopper *stop.Stopper) {
 				continue
 			}
 			objective := sr.RebalanceObjective()
+			sr.AddLogTag("obj", objective)
+			ctx = sr.AnnotateCtx(ctx)
+
 			hottestRanges := sr.replicaRankings.TopLoad(objective.ToDimension())
 			options := sr.scorerOptions(ctx, objective.ToDimension())
 			rctx := sr.NewRebalanceContext(ctx, options, hottestRanges, mode)
