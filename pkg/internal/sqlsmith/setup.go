df32fc9b9d8770bcf8b51f001ee8a90082b5e3ec
Michael Erickson <michae2@cockroachlabs.com>
2023-05-08 11:33:26 -0700

    sqlsmith: add a composite type column to the `seed` table
    Release note: None

--- pkg/internal/sqlsmith/setup.go
+++ pkg/internal/sqlsmith/setup.go
@@ -145,6 +145,7 @@ func randTablesN(r *rand.Rand, n int, prefix string) []string {
 const (
 	seedTable = `
 BEGIN; CREATE TYPE greeting AS ENUM ('hello', 'howdy', 'hi', 'good day', 'morning'); COMMIT;
+BEGIN; CREATE TYPE dimensions AS (length INT, width INT, height INT); COMMIT;
 BEGIN;
 CREATE TABLE IF NOT EXISTS seed AS
 	SELECT
@@ -164,7 +165,8 @@ CREATE TABLE IF NOT EXISTS seed AS
 		substring('00000000-0000-0000-0000-' || g::STRING || '00000000000', 1, 36)::UUID AS _uuid,
 		'0.0.0.0'::INET + g AS _inet,
 		g::STRING::JSONB AS _jsonb,
-		enum_range('hello'::greeting)[g] as _enum
+		enum_range('hello'::greeting)[g] as _enum,
+		(g, g, g)::dimensions AS _composite
 	FROM
 		generate_series(1, 5) AS g;
 COMMIT;
