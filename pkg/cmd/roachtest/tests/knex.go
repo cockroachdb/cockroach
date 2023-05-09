64a839b13f3701d096373934440956ea71c158fb
Celia La <celia@cockroachlabs.com>
2023-05-08 18:12:47 -0700

    codeowners, roachtest: merge SQL schema + sessions to SQL foundations
    The SQL Schema and Sessions teams are merging to form SQL
    Foundations. As described in DEVINFHD-867 we want to:
    
    - reroute old teams to new sql-foundations team
    - rename "SQL Schema" project to "SQL Foundations"
    - deprecate "SQL Sessions" project
    
    Release note: None

--- pkg/cmd/roachtest/tests/knex.go
+++ pkg/cmd/roachtest/tests/knex.go
@@ -128,7 +128,7 @@ func registerKnex(r registry.Registry) {
 
 	r.Add(registry.TestSpec{
 		Name:       "knex",
-		Owner:      registry.OwnerSQLSessions,
+		Owner:      registry.OwnerSQLFoundations,
 		Cluster:    r.MakeClusterSpec(1),
 		NativeLibs: registry.LibGEOS,
 		Tags:       registry.Tags(`default`, `orm`),
