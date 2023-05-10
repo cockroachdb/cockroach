a510291d883e06f6cd02ba8b50baf439f45b0369
Evan Wall <wall@cockroachlabs.com>
2023-05-10 12:43:30 -0400

    sql: deflake TestSerialNormalizationWithUniqueUnorderedID
    Fixes #102749
    
    This test has random input so retry failures to reduce chances of flaking.
    
    Release note: None

--- pkg/sql/sem/builtins/builtins_test.go
+++ pkg/sql/sem/builtins/builtins_test.go
@@ -31,7 +31,9 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/util/duration"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/retry"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/cockroachdb/errors"
 	"github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -98,17 +100,22 @@ func TestMapToUniqueUnorderedID(t *testing.T) {
 // by insertions guarantees a (somewhat) uniform distribution of the data.
 func TestSerialNormalizationWithUniqueUnorderedID(t *testing.T) {
 	defer leaktest.AfterTest(t)()
-	ctx := context.Background()
 
 	skip.UnderRace(t, "the test is too slow and the goodness of fit test "+
 		"assumes large N")
-	params := base.TestServerArgs{}
-	s, db, _ := serverutils.StartServer(t, params)
-	defer s.Stopper().Stop(ctx)
 
-	tdb := sqlutils.MakeSQLRunner(db)
-	// Create a new table with serial primary key i (unordered_rowid) and int j (index).
-	tdb.Exec(t, `
+	ctx := context.Background()
+	const attempts = 2
+	// This test can flake when the random data is not distributed evenly.
+	err := retry.WithMaxAttempts(ctx, retry.Options{}, attempts, func() error {
+
+		params := base.TestServerArgs{}
+		s, db, _ := serverutils.StartServer(t, params)
+		defer s.Stopper().Stop(ctx)
+
+		tdb := sqlutils.MakeSQLRunner(db)
+		// Create a new table with serial primary key i (unordered_rowid) and int j (index).
+		tdb.Exec(t, `
 SET serial_normalization TO 'unordered_rowid';
 CREATE DATABASE t;
 USE t;
@@ -117,27 +124,27 @@ CREATE TABLE t (
   j INT
 )`)
 
-	numberOfRows := 10000
+		numberOfRows := 10000
 
-	// Insert rows.
-	tdb.Exec(t, fmt.Sprintf(`
+		// Insert rows.
+		tdb.Exec(t, fmt.Sprintf(`
 INSERT INTO t(j) SELECT * FROM generate_series(1, %d);
 `, numberOfRows))
 
-	// Build an equi-width histogram over the key range. The below query will
-	// generate the key bounds for each high-order bit pattern as defined by
-	// prefixBits. For example, if this were 3, we'd get the following groups:
-	//
-	//            low         |        high
-	//  ----------------------+----------------------
-	//                      0 | 2305843009213693952
-	//    2305843009213693952 | 4611686018427387904
-	//    4611686018427387904 | 6917529027641081856
-	//    6917529027641081856 | 9223372036854775807
-	//
-	const prefixBits = 4
-	var keyCounts pq.Int64Array
-	tdb.QueryRow(t, `
+		// Build an equi-width histogram over the key range. The below query will
+		// generate the key bounds for each high-order bit pattern as defined by
+		// prefixBits. For example, if this were 3, we'd get the following groups:
+		//
+		//            low         |        high
+		//  ----------------------+----------------------
+		//                      0 | 2305843009213693952
+		//    2305843009213693952 | 4611686018427387904
+		//    4611686018427387904 | 6917529027641081856
+		//    6917529027641081856 | 9223372036854775807
+		//
+		const prefixBits = 4
+		var keyCounts pq.Int64Array
+		tdb.QueryRow(t, `
   WITH boundaries AS (
                      SELECT i << (64 - $1) AS p FROM ROWS FROM (generate_series(0, (1<<($1-1)) -1)) AS t (i)
                      UNION ALL SELECT (((1 << 62) - 1) << 1)+1 -- int63 max value
@@ -156,26 +163,31 @@ INSERT INTO t(j) SELECT * FROM generate_series(1, %d);
 SELECT array_agg(c)
   FROM counts;`, prefixBits).Scan(&keyCounts)
 
-	t.Log("Key counts in each split range")
-	for i, keyCount := range keyCounts {
-		t.Logf("range %d: %d\n", i, keyCount)
-	}
-	require.Len(t, keyCounts, 1<<(prefixBits-1))
+		t.Log("Key counts in each split range")
+		for i, keyCount := range keyCounts {
+			t.Logf("range %d: %d\n", i, keyCount)
+		}
+		require.Len(t, keyCounts, 1<<(prefixBits-1))
 
-	// To check that the distribution over ranges is not uniform, we use a
-	// chi-square goodness of fit statistic. We'll set our null hypothesis as
-	// 'each range in the distribution should have the same probability of getting
-	// a row inserted' and we'll check if we can reject the null hypothesis if
-	// chi-square is greater than the critical value we currently set as 35.2585,
-	// a deliberate choice that gives us a p-value of 0.00001 according to
-	// https://www.fourmilab.ch/rpkp/experiments/analysis/chiCalc.html. If we are
-	// able to reject the null hypothesis, then the distribution is not uniform,
-	// and we raise an error. This test has 7 degrees of freedom (categories - 1).
-	chiSquared := discreteUniformChiSquared(keyCounts)
-	criticalValue := 35.2585
-	require.Lessf(t, chiSquared, criticalValue, "chiSquared value of %f must be"+
-		" less than criticalVal %f to guarantee distribution is relatively uniform",
-		chiSquared, criticalValue)
+		// To check that the distribution over ranges is not uniform, we use a
+		// chi-square goodness of fit statistic. We'll set our null hypothesis as
+		// 'each range in the distribution should have the same probability of getting
+		// a row inserted' and we'll check if we can reject the null hypothesis if
+		// chi-square is greater than the critical value we currently set as 35.2585,
+		// a deliberate choice that gives us a p-value of 0.00001 according to
+		// https://www.fourmilab.ch/rpkp/experiments/analysis/chiCalc.html. If we are
+		// able to reject the null hypothesis, then the distribution is not uniform,
+		// and we raise an error. This test has 7 degrees of freedom (categories - 1).
+		chiSquared := discreteUniformChiSquared(keyCounts)
+		criticalValue := 35.2585
+		if chiSquared >= criticalValue {
+			return errors.Newf("chiSquared value of %f must be"+
+				" less than criticalVal %f to guarantee distribution is relatively uniform",
+				chiSquared, criticalValue)
+		}
+		return nil
+	})
+	require.NoError(t, err)
 }
 
 // discreteUniformChiSquared calculates the chi-squared statistic (ref:
