dcd149f343312591953ff76c2842d0437e4dea87
Raphael 'kena' Poss <knz@thaumogen.net>
2023-05-12 13:46:25 +0200

    sql: more redactable details about traces in logs
    Prior to this patch, the logging output for SQL statements / txns that
    exceeded the tracing threshold would mark the entire SQL statement
    syntax and the entire trace as redactable.
    
    This commit improves the situation by making the statement syntax and
    the trace more finely redactable, ensuring that more details are
    included after redaction.
    
    Release note (sql change): The logging output emitted when
    `sql.trace.stmt.enable_threshold` or `sql.trace.txn.enable_threshold`
    are set is now emited on the SQL_EXEC channel instead of DEV as
    previously.

--- pkg/sql/txn_state.go
+++ pkg/sql/txn_state.go
@@ -32,6 +32,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/tracing/tracingpb"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 	"github.com/cockroachdb/errors"
+	"github.com/cockroachdb/redact"
 	"go.opentelemetry.io/otel/attribute"
 )
 
@@ -270,7 +271,15 @@ func (ts *txnState) finishSQLTxn() (txnID uuid.UUID, commitTimestamp hlc.Timesta
 	}
 
 	if ts.recordingThreshold > 0 {
-		logTraceAboveThreshold(ts.Ctx, sp.GetRecording(sp.RecordingType()), "SQL txn", ts.recordingThreshold, timeutil.Since(ts.recordingStart))
+		if elapsed := timeutil.Since(ts.recordingStart); elapsed >= ts.recordingThreshold {
+			logTraceAboveThreshold(ts.Ctx,
+				sp.GetRecording(sp.RecordingType()), /* recording */
+				"SQL txn",                           /* opName */
+				redact.Sprint(redact.Safe(txnID)),   /* detail */
+				ts.recordingThreshold,               /* threshold */
+				elapsed,                             /* elapsed */
+			)
+		}
 	}
 
 	sp.Finish()
