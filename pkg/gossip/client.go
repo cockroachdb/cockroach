908924f5691633d196ad691c85a8af3203436a77
Tobias Grieger <tobias.b.grieger@gmail.com>
2023-05-15 10:15:48 +0200

    gossip: don't use deprecated breaker
    Epic: none
    Release note: None

--- pkg/gossip/client.go
+++ pkg/gossip/client.go
@@ -17,7 +17,6 @@ import (
 	"sync"
 	"time"
 
-	circuit "github.com/cockroachdb/circuitbreaker"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/rpc"
 	"github.com/cockroachdb/cockroach/pkg/util"
@@ -65,15 +64,13 @@ func newClient(ambient log.AmbientContext, addr net.Addr, nodeMetrics Metrics) *
 	}
 }
 
+var logFailedStartEvery = log.Every(5 * time.Second)
+
 // start dials the remote addr and commences gossip once connected. Upon exit,
 // the client is sent on the disconnected channel. This method starts client
 // processing in a goroutine and returns immediately.
 func (c *client) startLocked(
-	g *Gossip,
-	disconnected chan *client,
-	rpcCtx *rpc.Context,
-	stopper *stop.Stopper,
-	breaker *circuit.Breaker,
+	g *Gossip, disconnected chan *client, rpcCtx *rpc.Context, stopper *stop.Stopper,
 ) {
 	// Add a placeholder for the new outgoing connection because we may not know
 	// the ID of the node we're connecting to yet. This will be resolved in
@@ -98,23 +95,26 @@ func (c *client) startLocked(
 			disconnected <- c
 		}()
 
-		consecFailures := breaker.ConsecFailures()
-		var stream Gossip_GossipClient
-		if err := breaker.Call(func() error {
+		stream, err := func() (Gossip_GossipClient, error) {
 			// Note: avoid using `grpc.WithBlock` here. This code is already
 			// asynchronous from the caller's perspective, so the only effect of
 			// `WithBlock` here is blocking shutdown - at the time of this writing,
 			// that ends ups up making `kv` tests take twice as long.
 			conn, err := rpcCtx.GRPCUnvalidatedDial(c.addr.String()).Connect(ctx)
 			if err != nil {
-				return err
+				return nil, err
 			}
-			if stream, err = NewGossipClient(conn).Gossip(ctx); err != nil {
-				return err
+			stream, err := NewGossipClient(conn).Gossip(ctx)
+			if err != nil {
+				return nil, err
 			}
-			return c.requestGossip(g, stream)
-		}, 0); err != nil {
-			if consecFailures == 0 {
+			if err := c.requestGossip(g, stream); err != nil {
+				return nil, err
+			}
+			return stream, nil
+		}()
+		if err != nil {
+			if logFailedStartEvery.ShouldLog() {
 				log.Warningf(ctx, "failed to start gossip client to %s: %s", c.addr, err)
 			}
 			return
