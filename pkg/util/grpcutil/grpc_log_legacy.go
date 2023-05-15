f0acc77d431d9a49f2a1691d8ba911ad4e06d097
Tobias Grieger <tobias.b.grieger@gmail.com>
2023-05-15 13:53:41 +0200

    grpcutil: adopt DepthLoggerV2
    grpc has a `DepthLogger` interface in which the methods called get told about
    the number of stackframes to skip to get the true caller. This takes a lot of
    guesswork (which had since gone stale anyway) out of our grpc logger
    implementation.
    
    Now we'll (sort of) reliably get the log lines annotated with the stack frames
    from which they came.
    
    The old interface still needs to be supported, but it's no longer called, so
    I simplified it down to something that still logs but makes no attempt to get
    the "correct" caller.
    
    Since I was here I couldn't help but also massage the output a bit.
    Unfortunately gRPC gives us totally unstructured logging, so I can't add
    redaction or anything of that sort, but I at least removed newlines so that
    grpc logs are more compact and easier to grep out.
    
    I also improved vmodule so that you can opt into *only* gRPC warnings and
    errors, etc, on a per-file basis (3=INFO, 2=WARNING, 1=ERROR).
    
    Epic: none
    Release note: None
    
    squash gRPC logging rework
    
    - remove warning output filter
    - force messages into single line to avoid blobs
    - rework vmodule logging to make it more useful

---
+++ pkg/util/grpcutil/grpc_log_legacy.go
@@ -0,0 +1,71 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package grpcutil
+
+import "github.com/cockroachdb/redact"
+
+// gRPC logging is a real redirect maze but luckily it doesn't matter to get
+// this right since it actually uses the DepthLoggerV2 interface. The methods
+// here are provided as a fallback implementation in case there are any stray
+// callsites in gRPC using the old interface.
+
+func (l *grpcLogger) Info(args ...interface{}) {
+	l.InfoDepth(0, args...)
+}
+
+func (l *grpcLogger) Infoln(args ...interface{}) {
+	l.InfoDepth(0, args...)
+}
+
+func (l *grpcLogger) Infof(format string, args ...interface{}) {
+	l.InfoDepth(0, redact.Sprintf(format, args...))
+}
+
+func (l *grpcLogger) Warning(args ...interface{}) {
+	l.WarningDepth(0, args...)
+}
+
+func (l *grpcLogger) Warningln(args ...interface{}) {
+	l.WarningDepth(0, args...)
+}
+
+func (l *grpcLogger) Warningf(format string, args ...interface{}) {
+	l.WarningDepth(0, args...)
+}
+
+func (l *grpcLogger) Error(args ...interface{}) {
+	l.ErrorDepth(0, args...)
+}
+
+func (l *grpcLogger) Errorln(args ...interface{}) {
+	l.ErrorDepth(0, args...)
+}
+
+func (l *grpcLogger) Errorf(format string, args ...interface{}) {
+	l.ErrorDepth(0, redact.Sprintf(format, args...))
+}
+
+func (l *grpcLogger) Fatal(args ...interface{}) {
+	l.FatalDepth(0, args...)
+}
+
+func (l *grpcLogger) Fatalln(args ...interface{}) {
+	l.FatalDepth(0, args...)
+}
+
+func (l *grpcLogger) Fatalf(format string, args ...interface{}) {
+	l.FatalDepth(0, redact.Sprintf(format, args...))
+}
+
+func (l *grpcLogger) V(i int) bool {
+	// NB: V isn't used by gRPC despite being in the interface.
+	return l.vDepth(i, 0)
+}
