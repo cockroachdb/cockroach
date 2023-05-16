b8278382b4decd12a2f346efa1297cfacb8a8edf
Xin Hao Zhang <xzhang@cockroachlabs.com>
2023-05-16 13:59:26 -0400

    log: add buffer format options
    This commit adds an option to format the logging sink
    buffers on flush.
    
    New option (under `buffering` in the config file):
    - format: string that is either 'json-array' or 'newline',
    defaulting to 'newline'.
    
    Format options:
    - json-array: adds '[' at the start of the buffer output and ']'
    at the end. Separates entries in the buffer with ",".
    - newline (default): separates entries in the buffer with "\n".
    
    This change was motivated by the desire to send multiple
    JSON log entries to the DD API, which expects them to be in a
    JSON array.
    
    Epic: none
    Part of: #103477
    
    Release note (cli change): New log config option for the
    `buffering` field:
    - format: 'json-array' or 'newline'.
    Descriptions:
    - json-array: adds '[' at the start of the buffer output and ']'
    at the end. Separates entries in the buffer with ",".
    - newline (default): separates entries in the buffer with "\n".

--- pkg/util/log/logconfig/validate.go
+++ pkg/util/log/logconfig/validate.go
@@ -42,6 +42,7 @@ func (c *Config) Validate(defaultLogDir *string) (resErr error) {
 	defaultBufferedStaleness := 5 * time.Second
 	defaultFlushTriggerSize := ByteSize(1024 * 1024)   // 1mib
 	defaultMaxBufferSize := ByteSize(50 * 1024 * 1024) // 50mib
+	bufferFmt := BufferFmtNewline
 
 	baseCommonSinkConfig := CommonSinkConfig{
 		Filter:      logpb.Severity_INFO,
@@ -56,6 +57,7 @@ func (c *Config) Validate(defaultLogDir *string) (resErr error) {
 				MaxStaleness:     &zeroDuration,
 				FlushTriggerSize: &zeroByteSize,
 				MaxBufferSize:    &zeroByteSize,
+				Format:           &bufferFmt,
 			},
 		},
 	}
@@ -76,6 +78,7 @@ func (c *Config) Validate(defaultLogDir *string) (resErr error) {
 					MaxStaleness:     &zeroDuration,
 					FlushTriggerSize: &zeroByteSize,
 					MaxBufferSize:    &zeroByteSize,
+					Format:           &bufferFmt,
 				},
 			},
 		},
@@ -88,6 +91,7 @@ func (c *Config) Validate(defaultLogDir *string) (resErr error) {
 					MaxStaleness:     &defaultBufferedStaleness,
 					FlushTriggerSize: &defaultFlushTriggerSize,
 					MaxBufferSize:    &defaultMaxBufferSize,
+					Format:           &bufferFmt,
 				},
 			},
 		},
@@ -100,6 +104,7 @@ func (c *Config) Validate(defaultLogDir *string) (resErr error) {
 					MaxStaleness:     &defaultBufferedStaleness,
 					FlushTriggerSize: &defaultFlushTriggerSize,
 					MaxBufferSize:    &defaultMaxBufferSize,
+					Format:           &bufferFmt,
 				},
 			},
 		},
