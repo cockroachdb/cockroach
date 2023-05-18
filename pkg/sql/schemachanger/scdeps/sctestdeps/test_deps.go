77372a6bf30657d3f49c56b5c7978984fe90b5df
Xiang Gu <xiang@cockroachlabs.com>
2023-05-18 11:30:41 -0500

    schemachanger: Implement CREATE DATABASE
    This commit supports CREATE DATABASE in declarative schema changer.
    It falls back to legacy schema changer if the database is requested
    to be multi-region.
    
    Release note: None

--- pkg/sql/schemachanger/scdeps/sctestdeps/test_deps.go
+++ pkg/sql/schemachanger/scdeps/sctestdeps/test_deps.go
@@ -207,6 +207,21 @@ func (s *TestState) MemberOfWithAdminOption(
 	return nil, nil
 }
 
+// HasGlobalPrivilegeOrRoleOption implements the scbuild.AuthorizationAccessor interface.
+func (s *TestState) HasGlobalPrivilegeOrRoleOption(
+	ctx context.Context, privilege privilege.Kind,
+) (bool, error) {
+	s.LogSideEffectf("checking current user %q has system privilege %q or the corresponding"+
+		" legacy role option", s.User(), privilege.DisplayName())
+	return true, nil
+}
+
+// CheckRoleExists implements the scbuild.AuthorizationAccessor interface.
+func (s *TestState) CheckRoleExists(ctx context.Context, role username.SQLUsername) error {
+	s.LogSideEffectf("checking role/user %q exists", role)
+	return nil
+}
+
 // IndexPartitioningCCLCallback implements the scbuild.Dependencies interface.
 func (s *TestState) IndexPartitioningCCLCallback() scbuild.CreatePartitioningCCLCallback {
 	if ccl := scdeps.CreatePartitioningCCL; ccl != nil {
