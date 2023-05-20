588ab4bd7f6da8d363e81b1953ad541b0d932a19
Austen McClernon <austen@cockroachlabs.com>
2023-05-20 01:25:26 +0000

    kvserver: avoid load-based splitting between rows
    It was possible for a SQL row to be torn across two ranges due to the
    load-based splitter not rejecting potentially unsafe split keys. It is
    impossible to determine with keys sampled from response spans, whether a
    key is certainly unsafe or safe.
    
    This commit side steps this problem by re-using the
    `adminSplitWithDescriptor` command to find the first real key, after or
    at the provided `args.SplitKey`. This ensures that the split key will
    always be a real key whilst not requiring any checks in the splitter
    itself.
    
    The updated `adminSplitWithDescriptor` is local only and requires opting
    into finding the first safe key by setting `findFirstSafeKey` to `true`.
    
    As such, all safe split key checks are also removed from the `split`
    pkg, with a warning added that the any split key returned is unsafe.
    
    Resolves: https://github.com/cockroachdb/cockroach/issues/103483
    
    Release note (bug fix): It was possible for a SQL row to be split across
    two ranges. When this occurred, SQL queries could return unexpected
    errors. This bug is resolved by these changes, as we now inspect the real
    keys, rather than just request keys to determine load-based split points.

--- pkg/kv/kvserver/replica_split_load.go
+++ pkg/kv/kvserver/replica_split_load.go
@@ -254,11 +254,37 @@ func (r *Replica) recordBatchForLoadBasedSplitting(
 
 // loadSplitKey returns a suggested load split key for the range if it exists,
 // otherwise it returns nil. If there were any errors encountered when
-// validating the split key, the error is returned as well.
+// validating the split key, the error is returned as well. It is guaranteed
+// that the key returned, if non-nil, will be greater than the start key of the
+// range and also within the range bounds.
+//
+// NOTE: The returned split key CAN BE BETWEEN A SQL ROW, The split key
+// returned should only be used to engage a split via adminSplitWithDescriptor
+// where findFirstSafeKey is set to true.
 func (r *Replica) loadSplitKey(ctx context.Context, now time.Time) roachpb.Key {
-	splitKey := r.loadBasedSplitter.MaybeSplitKey(ctx, now)
+	var splitKey roachpb.Key
+	if overrideFn := r.store.cfg.TestingKnobs.LoadBasedSplittingOverrideKey; overrideFn != nil {
+		var useSplitKey bool
+		if splitKey, useSplitKey = overrideFn(r.GetRangeID()); useSplitKey {
+			return splitKey
+		}
+	} else {
+		splitKey = r.loadBasedSplitter.MaybeSplitKey(ctx, now)
+	}
+
 	if splitKey == nil {
 		return nil
+	}
+
+	// If the splitKey belongs to a Table range, try and shorten the key to just
+	// the row prefix. This allows us to check that splitKey doesn't map to the
+	// first key of the range here.
+	//
+	// NB: We handle unsafe split keys in replica.adminSplitWithDescriptor, so it
+	// isn't an issue if we return an unsafe key here. See the case where
+	// findFirstSafeKey is true.
+	if keyRowPrefix, err := keys.EnsureSafeSplitKey(splitKey); err == nil {
+		splitKey = keyRowPrefix
 	}
 
 	// We swallow the error here and instead log an event. It is currently
