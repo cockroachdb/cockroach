fbd48c82638aadbbc26f264d5701ce00fd78d2b9
Oleg Afanasyev <oleg@cockroachlabs.com>
2023-05-22 16:25:10 +0100

    kvserver/batcheval: fix mvcc stats computaion on split
    Previouly split trigger was using in-memory copy of mvccstats from replica.
    This value could diverge from store snapshot used by request in special cases
    of lease extensions that don't hold latches.
    This was causing stats for the replica to diverge.
    This PR changes base stats to be read from request reader which provides
    consistent stats value with content of splitted ranges.
    
    Touches: #101721
    
    Release note: None

--- pkg/kv/kvserver/batcheval/cmd_end_transaction.go
+++ pkg/kv/kvserver/batcheval/cmd_end_transaction.go
@@ -1012,13 +1012,29 @@ func splitTrigger(
 			"unable to compute range key stats delta for RHS")
 	}
 
+	// Retrieve MVCC Stats from the current batch instead of using stats from
+	// execution context. Stats in the context could diverge from storage snapshot
+	// of current request when lease extensions are applied. Lease expiration is
+	// a special case that updates stats without obtaining latches and thus can
+	// execute concurrently with splitTrigger. As a result we must not write
+	// absolute stats values for LHS based on this value, always produce a delta
+	// since underlying stats in storage could change. At the same time it is safe
+	// to write absolute RHS side stats since we hold lock for values, and
+	// "unprotected" lease key don't yet exist until this split operation creates
+	// RHS replica.
+	currentStats, err := MakeStateLoader(rec).LoadMVCCStats(ctx, batch)
+	if err != nil {
+		return enginepb.MVCCStats{}, result.Result{}, errors.Wrap(err,
+			"unable to fetch original range mvcc stats for split")
+	}
+
 	h := splitStatsHelperInput{
-		AbsPreSplitBothEstimated: rec.GetMVCCStats(),
-		DeltaBatchEstimated:      bothDeltaMS,
-		DeltaRangeKey:            rangeKeyDeltaMS,
-		AbsPostSplitLeftFn:       makeScanStatsFn(ctx, batch, ts, &split.LeftDesc, "left hand side"),
-		AbsPostSplitRightFn:      makeScanStatsFn(ctx, batch, ts, &split.RightDesc, "right hand side"),
-		ScanRightFirst:           splitScansRightForStatsFirst || emptyRHS,
+		AbsPreSplitBothStored: currentStats,
+		DeltaBatchEstimated:   bothDeltaMS,
+		DeltaRangeKey:         rangeKeyDeltaMS,
+		PostSplitScanLeftFn:   makeScanStatsFn(ctx, batch, ts, &split.LeftDesc, "left hand side"),
+		PostSplitScanRightFn:  makeScanStatsFn(ctx, batch, ts, &split.RightDesc, "right hand side"),
+		ScanRightFirst:        splitScansRightForStatsFirst || emptyRHS,
 	}
 	return splitTriggerHelper(ctx, rec, batch, h, split, ts)
 }
@@ -1031,7 +1047,7 @@ func splitTrigger(
 var splitScansRightForStatsFirst = util.ConstantWithMetamorphicTestBool(
 	"split-scans-right-for-stats-first", false)
 
-// makeScanStatsFn constructs a splitStatsScanFn for the provided post-split
+// makeScanStatsFn constructs a getStatsFn for the provided post-split
 // range descriptor which computes the range's statistics.
 func makeScanStatsFn(
 	ctx context.Context,
@@ -1039,7 +1055,7 @@ func makeScanStatsFn(
 	ts hlc.Timestamp,
 	sideDesc *roachpb.RangeDescriptor,
 	sideName string,
-) splitStatsScanFn {
+) getStatsFn {
 	return func() (enginepb.MVCCStats, error) {
 		sideMS, err := rditer.ComputeStatsForRange(sideDesc, reader, ts.WallTime)
 		if err != nil {
