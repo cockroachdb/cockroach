6f3ec46312d68c279bc4418f71a0f4e0938a8f60
Andrew Baptist <baptist@cockroachlabs.com>
2023-05-23 17:24:11 -0400

    storepool: Simplify liveness func
    Remove unnnecessary parameters from the liveness func. Now that liveness
    is mostly self-contined, the methods to call it are simpler.
    
    Epic: none
    
    Release note: None

--- pkg/kv/kvserver/allocator/storepool/override_store_pool.go
+++ pkg/kv/kvserver/allocator/storepool/override_store_pool.go
@@ -12,7 +12,6 @@ package storepool
 
 import (
 	"context"
-	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/allocator"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/liveness"
@@ -50,12 +49,11 @@ var _ AllocatorStorePool = &OverrideStorePool{}
 func OverrideNodeLivenessFunc(
 	overrides map[roachpb.NodeID]livenesspb.NodeLivenessStatus, realNodeLivenessFunc NodeLivenessFunc,
 ) NodeLivenessFunc {
-	return func(nid roachpb.NodeID, now hlc.Timestamp, timeUntilNodeDead time.Duration) livenesspb.NodeLivenessStatus {
+	return func(nid roachpb.NodeID) livenesspb.NodeLivenessStatus {
 		if override, ok := overrides[nid]; ok {
 			return override
 		}
-
-		return realNodeLivenessFunc(nid, now, timeUntilNodeDead)
+		return realNodeLivenessFunc(nid)
 	}
 }
 
