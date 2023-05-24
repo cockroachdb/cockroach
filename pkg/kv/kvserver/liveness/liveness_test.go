eb0b15ce626a7195aa2acdae36c7e933f23c996c
Andrew Baptist <baptist@cockroachlabs.com>
2023-05-24 11:00:00 -0400

    liveness: Add tests based on the store descriptor time
    Now that this method takes both the liveness expiration and the store
    descriptor update into account, test variations of both of them.
    
    Epic: none
    
    Release note: None

--- pkg/kv/kvserver/liveness/liveness_test.go
+++ pkg/kv/kvserver/liveness/liveness_test.go
@@ -143,12 +143,77 @@ func TestNodeLivenessLivenessStatus(t *testing.T) {
 	nl := NewNodeLiveness(NodeLivenessOptions{Clock: clock, Settings: cluster.MakeTestingClusterSettings()})
 	now := clock.Now()
 	threshold := TimeUntilNodeDead.Default()
+	suspectThreshold := TimeAfterNodeSuspect.Default()
 
 	for _, tc := range []struct {
-		name     string
-		liveness livenesspb.Liveness
-		expected livenesspb.NodeLivenessStatus
+		name           string
+		liveness       livenesspb.Liveness
+		descriptor     UpdateInfo
+		expectedAlive  bool
+		expectedStatus livenesspb.NodeLivenessStatus
 	}{
+		// Assume that we just haven't received a gossip update if we have a
+		// liveness record but no descriptor.
+		{
+			name: "No descriptor",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.AddDuration(threshold).ToLegacyTimestamp(),
+				Draining:   false,
+			},
+			descriptor:     UpdateInfo{},
+			expectedAlive:  true,
+			expectedStatus: livenesspb.NodeLivenessStatus_LIVE,
+		},
+		{
+			name: "Expired descriptor",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.AddDuration(threshold).ToLegacyTimestamp(),
+				Draining:   false,
+			},
+			descriptor:     UpdateInfo{lastUpdateTime: now.AddDuration(-threshold)},
+			expectedAlive:  false,
+			expectedStatus: livenesspb.NodeLivenessStatus_DEAD,
+		},
+		{
+			name: "Unavailable recent descriptor",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.AddDuration(threshold).ToLegacyTimestamp(),
+				Draining:   false,
+			},
+			descriptor:     UpdateInfo{lastUnavailableTime: now.AddDuration(-1 * time.Second), lastUpdateTime: now.AddDuration(-1 * time.Second)},
+			expectedAlive:  false,
+			expectedStatus: livenesspb.NodeLivenessStatus_UNAVAILABLE,
+		},
+		{
+			name: "Unavailable descriptor a long time ago",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.AddDuration(threshold).ToLegacyTimestamp(),
+				Draining:   false,
+			},
+			descriptor:     UpdateInfo{lastUnavailableTime: now.AddDuration(-suspectThreshold), lastUpdateTime: now},
+			expectedAlive:  true,
+			expectedStatus: livenesspb.NodeLivenessStatus_LIVE,
+		},
+		{
+			name: "Unavailable and Expired descriptor",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.AddDuration(threshold).ToLegacyTimestamp(),
+				Draining:   false,
+			},
+			descriptor:     UpdateInfo{lastUnavailableTime: now.AddDuration(-suspectThreshold), lastUpdateTime: now.AddDuration(-threshold)},
+			expectedAlive:  false,
+			expectedStatus: livenesspb.NodeLivenessStatus_DEAD,
+		},
 		// Valid status.
 		{
 			name: "Valid 5 min",
@@ -158,7 +223,9 @@ func TestNodeLivenessLivenessStatus(t *testing.T) {
 				Expiration: now.AddDuration(5 * time.Minute).ToLegacyTimestamp(),
 				Draining:   false,
 			},
-			expected: livenesspb.NodeLivenessStatus_LIVE,
+			descriptor:     UpdateInfo{lastUpdateTime: now},
+			expectedAlive:  true,
+			expectedStatus: livenesspb.NodeLivenessStatus_LIVE,
 		},
 		{
 			name: "Expires just slightly in the future",
@@ -168,17 +235,21 @@ func TestNodeLivenessLivenessStatus(t *testing.T) {
 				Expiration: now.AddDuration(time.Nanosecond).ToLegacyTimestamp(),
 				Draining:   false,
 			},
-			expected: livenesspb.NodeLivenessStatus_LIVE,
+			descriptor:     UpdateInfo{lastUpdateTime: now},
+			expectedAlive:  true,
+			expectedStatus: livenesspb.NodeLivenessStatus_LIVE,
 		},
 		{
-			name: "Expired status",
+			name: "Expired both",
 			liveness: livenesspb.Liveness{
 				NodeID:     1,
 				Epoch:      1,
 				Expiration: now.ToLegacyTimestamp(),
 				Draining:   false,
 			},
-			expected: livenesspb.NodeLivenessStatus_UNAVAILABLE,
+			descriptor:     UpdateInfo{lastUpdateTime: now.AddDuration(-suspectThreshold)},
+			expectedAlive:  false,
+			expectedStatus: livenesspb.NodeLivenessStatus_UNAVAILABLE,
 		},
 		{
 			name: "Max bound of expired",
@@ -188,7 +259,9 @@ func TestNodeLivenessLivenessStatus(t *testing.T) {
 				Expiration: now.AddDuration(-threshold).AddDuration(time.Nanosecond).ToLegacyTimestamp(),
 				Draining:   false,
 			},
-			expected: livenesspb.NodeLivenessStatus_UNAVAILABLE,
+			descriptor:     UpdateInfo{lastUpdateTime: now.AddDuration(-suspectThreshold)},
+			expectedAlive:  false,
+			expectedStatus: livenesspb.NodeLivenessStatus_UNAVAILABLE,
 		},
 		{
 			name: "Dead",
@@ -198,7 +271,9 @@ func TestNodeLivenessLivenessStatus(t *testing.T) {
 				Expiration: now.AddDuration(-threshold).ToLegacyTimestamp(),
 				Draining:   false,
 			},
-			expected: livenesspb.NodeLivenessStatus_DEAD,
+			descriptor:     UpdateInfo{lastUpdateTime: now.AddDuration(-threshold)},
+			expectedAlive:  false,
+			expectedStatus: livenesspb.NodeLivenessStatus_DEAD,
 		},
 		{
 			name: "Decommissioning",
@@ -209,7 +284,9 @@ func TestNodeLivenessLivenessStatus(t *testing.T) {
 				Membership: livenesspb.MembershipStatus_DECOMMISSIONING,
 				Draining:   false,
 			},
-			expected: livenesspb.NodeLivenessStatus_DECOMMISSIONING,
+			descriptor:     UpdateInfo{lastUpdateTime: now},
+			expectedAlive:  true,
+			expectedStatus: livenesspb.NodeLivenessStatus_DECOMMISSIONING,
 		},
 		{
 			name: "Decommissioning + expired",
@@ -220,7 +297,9 @@ func TestNodeLivenessLivenessStatus(t *testing.T) {
 				Membership: livenesspb.MembershipStatus_DECOMMISSIONING,
 				Draining:   false,
 			},
-			expected: livenesspb.NodeLivenessStatus_DECOMMISSIONED,
+			descriptor:     UpdateInfo{lastUpdateTime: now.AddDuration(-suspectThreshold)},
+			expectedAlive:  false,
+			expectedStatus: livenesspb.NodeLivenessStatus_DECOMMISSIONED,
 		},
 		{
 			name: "Decommissioning + live",
@@ -234,10 +313,15 @@ func TestNodeLivenessLivenessStatus(t *testing.T) {
 			// Despite having marked the node as fully decommissioned, through
 			// this NodeLivenessStatus API we still surface the node as
 			// "Decommissioning". See #50707 for more details.
-			expected: livenesspb.NodeLivenessStatus_DECOMMISSIONING,
+			descriptor: UpdateInfo{lastUpdateTime: now},
+			// Despite having marked the node as fully decommissioned, through
+			// this NodeLivenessStatus API we still surface the node as
+			// "Decommissioning". See #50707 for more details.
+			expectedAlive:  false,
+			expectedStatus: livenesspb.NodeLivenessStatus_DECOMMISSIONING,
 		},
 		{
-			name: "Decommissioning + expired",
+			name: "Decommissioned + expired",
 			liveness: livenesspb.Liveness{
 				NodeID:     1,
 				Epoch:      1,
@@ -245,7 +329,9 @@ func TestNodeLivenessLivenessStatus(t *testing.T) {
 				Membership: livenesspb.MembershipStatus_DECOMMISSIONED,
 				Draining:   false,
 			},
-			expected: livenesspb.NodeLivenessStatus_DECOMMISSIONED,
+			descriptor:     UpdateInfo{lastUpdateTime: now.AddDuration(-threshold)},
+			expectedAlive:  false,
+			expectedStatus: livenesspb.NodeLivenessStatus_DECOMMISSIONED,
 		},
 		{
 			name: "Draining",
@@ -255,7 +341,9 @@ func TestNodeLivenessLivenessStatus(t *testing.T) {
 				Expiration: now.AddDuration(5 * time.Minute).ToLegacyTimestamp(),
 				Draining:   true,
 			},
-			expected: livenesspb.NodeLivenessStatus_DRAINING,
+			descriptor:     UpdateInfo{lastUpdateTime: now},
+			expectedAlive:  true,
+			expectedStatus: livenesspb.NodeLivenessStatus_DRAINING,
 		},
 		{
 			name: "Decommissioning that is unavailable",
@@ -266,7 +354,9 @@ func TestNodeLivenessLivenessStatus(t *testing.T) {
 				Draining:   false,
 				Membership: livenesspb.MembershipStatus_DECOMMISSIONING,
 			},
-			expected: livenesspb.NodeLivenessStatus_UNAVAILABLE,
+			descriptor:     UpdateInfo{lastUpdateTime: now.AddDuration(-suspectThreshold)},
+			expectedAlive:  false,
+			expectedStatus: livenesspb.NodeLivenessStatus_UNAVAILABLE,
 		},
 		{
 			name: "Draining that is unavailable",
@@ -276,15 +366,23 @@ func TestNodeLivenessLivenessStatus(t *testing.T) {
 				Expiration: now.ToLegacyTimestamp(),
 				Draining:   true,
 			},
-			expected: livenesspb.NodeLivenessStatus_UNAVAILABLE,
+			descriptor:     UpdateInfo{lastUpdateTime: now.AddDuration(-suspectThreshold)},
+			expectedAlive:  false,
+			expectedStatus: livenesspb.NodeLivenessStatus_UNAVAILABLE,
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			// TODO(baptist): Remove this when LivenessStatus is removed. This is here
 			// to make sure we preserve existing behavior. Both the old and new
 			// mechanisms should return the same result.
-			//			require.Equal(t, tc.expected, storepool.LivenessStatus(tc.liveness, now, threshold))
-			require.Equal(t, tc.expected, nl.convertToNodeVitality(tc.liveness).LivenessStatus())
+			// Use the set node descriptor.
+			if (tc.descriptor != UpdateInfo{}) {
+				nl.cache.mu.lastNodeUpdate[1] = tc.descriptor
+			}
+
+			nv := nl.convertToNodeVitality(tc.liveness)
+			require.Equal(t, tc.expectedAlive, nv.IsLive(livenesspb.Rebalance))
+			require.Equal(t, tc.expectedStatus, nv.LivenessStatus())
 		})
 	}
 }
