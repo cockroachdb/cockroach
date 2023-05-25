b964151d3036db8bbd0d60c815545704d5cd7234
Andrew Baptist <baptist@cockroachlabs.com>
2023-05-25 17:47:31 -0400

    convert SQL and upgrade to NodeVitality
    Liveness is not intended to be used directly. This converts the usages
    in upgrade and the internal crdb tables that expose liveness to instead
    use NodeVitality. This makes it easier to change the IsLive condition.
    
    Epic: none
    
    Release note: None

--- pkg/kv/kvserver/liveness/livenesspb/liveness.go
+++ pkg/kv/kvserver/liveness/livenesspb/liveness.go
@@ -213,6 +213,7 @@ const (
 	Rebalance
 	Admin
 	DistSQL
+	Upgrade
 )
 
 func (nv NodeVitality) IsLive(usage VitalityUsage) bool {
@@ -224,7 +225,15 @@ func (nv NodeVitality) IsLive(usage VitalityUsage) bool {
 	case Rebalance:
 		return nv.isAlive()
 	case DistSQL:
+		// TODO(erikgrinaker): We may want to use IsAvailableNotDraining instead, to
+		// avoid scheduling long-running flows (e.g. rangefeeds or backups) on nodes
+		// that are being drained/decommissioned. However, these nodes can still be
+		// leaseholders, and preventing processor scheduling on them can cause a
+		// performance cliff for e.g. table reads that then hit the network.
 		return nv.isAliveAndConnected()
+	case Upgrade:
+		//FIXME
+		return nv.isAlive()
 	}
 	// TODO(baptist): Should be an assertion that we don't know this uasge.
 	return false
