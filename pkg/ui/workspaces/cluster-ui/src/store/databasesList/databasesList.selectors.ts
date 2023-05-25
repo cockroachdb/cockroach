680cdc0553d39499ad92fc46a70c666506828737
Thomas Hardy <thardy@cockroachlabs.com>
2023-05-25 03:31:45 -0400

    cluster-ui: databases page connected component with reusable combiner logic
    This PR adds a connected component for the databases page on cluster-ui
    to be used on CC console. Additionally, we move some common
    selector/combiner logic between the two connected components (for db/cc
    console) into cluster-ui for reuse.
    
    This PR also introduces the cluster settings API to cluster-ui, needed
    to capture the cluster's version (necessary for version gating) and
    whether automatic stats collection in enabled. Furthermore, we add the
    database details API as well to cluster-ui, making use of the existing
    databaseDetailsApi using sql-over-http.
    
    To be used in conjuction with: cockroachlabs/managed-service#13193
    
    *Demos* DB Console:
    https://www.loom.com/share/88900a20a57f4f14bd7faa95a165dfc5 CC Console:
    https://www.loom.com/share/906c9319eb9a4cc796a3fea5ef4ec90e
    
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/store/databasesList/databasesList.selectors.ts
+++ pkg/ui/workspaces/cluster-ui/src/store/databasesList/databasesList.selectors.ts
@@ -9,9 +9,25 @@
 // licenses/APL.txt.
 
 import { createSelector } from "reselect";
-import { adminUISelector } from "../utils/selectors";
+import { adminUISelector, localStorageSelector } from "../utils/selectors";
+import { LocalStorageKeys } from "../localStorage";
+import { SortSetting } from "../../sortedtable";
+import { AppState } from "../reducers";
+import { Filters } from "../../queryFilter";
 
 export const databasesListSelector = createSelector(
   adminUISelector,
   adminUiState => adminUiState.databasesList,
 );
+
+export const selectDatabasesSortSetting = (state: AppState): SortSetting => {
+  return localStorageSelector(state)[LocalStorageKeys.DB_SORT];
+};
+
+export const selectDatabasesFilters = (state: AppState): Filters => {
+  return localStorageSelector(state)[LocalStorageKeys.DB_FILTERS];
+};
+
+export const selectDatabasesSearch = (state: AppState): string => {
+  return localStorageSelector(state)[LocalStorageKeys.DB_SEARCH];
+};
