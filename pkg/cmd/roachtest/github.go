bb47305a792a40b548ef1d161ad99277c614ee0f
Miral Gadani <miral@cockroachlabs.com>
2023-05-25 15:26:34 +0000

    roachtest: categorise errors that occur during test teardown
    Epic: none
    Fixes: #98366
    
    Release note: None

--- pkg/cmd/roachtest/github.go
+++ pkg/cmd/roachtest/github.go
@@ -38,6 +38,7 @@ const (
 	otherErr issueCategory = iota
 	clusterCreationErr
 	sshErr
+	teardownErr
 )
 
 func newGithubIssues(disable bool, c *clusterImpl, vmCreateOpts *vm.CreateOpts) *githubIssues {
@@ -128,6 +129,8 @@ func (g *githubIssues) createPostRequest(
 		issueName = "ssh_problem"
 		messagePrefix = fmt.Sprintf("test %s failed due to ", t.Name())
 		infraFlake = true
+	} else if cat == teardownErr {
+		messagePrefix = fmt.Sprintf("test %s failed during teardown (see teardown.log) due to ", t.Name())
 	}
 
 	// Issues posted from roachtest are identifiable as such and
@@ -226,6 +229,8 @@ func (g *githubIssues) MaybePost(t *testImpl, l *logger.Logger, message string) 
 		cat = clusterCreationErr
 	} else if failureContainsError(firstFailure, rperrors.ErrSSH255) {
 		cat = sshErr
+	} else if failureContainsError(firstFailure, errDuringTeardown) {
+		cat = teardownErr
 	}
 
 	return g.issuePoster(
