3622ffebec7d37b5f895b032c1809f052e18c152
Erik Grinaker <grinaker@cockroachlabs.com>
2023-05-28 17:56:03 +0000

    roachtest: skip dead node checks in `failover/chaos`
    The disk stall dmsetup cleanup needs to kill the nodes first, so we have
    to disable the dead node validation.
    
    Epic: none
    Release note: None

--- pkg/cmd/roachtest/tests/failover.go
+++ pkg/cmd/roachtest/tests/failover.go
@@ -76,7 +76,7 @@ func registerFailover(r registry.Registry) {
 				Name:                "failover/chaos" + suffix,
 				Owner:               registry.OwnerKV,
 				Timeout:             60 * time.Minute,
-				Cluster:             r.MakeClusterSpec(10, spec.CPU(2), spec.PreferLocalSSD(false)), // uses disk stalls
+				Cluster:             r.MakeClusterSpec(10, spec.CPU(2), spec.PreferLocalSSD(false)),
 				Leases:              leases,
 				SkipPostValidations: registry.PostValidationNoDeadNodes, // cleanup kills nodes
 				Run: func(ctx context.Context, t test.Test, c cluster.Cluster) {
