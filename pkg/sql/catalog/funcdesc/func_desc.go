4bedeb8f9a7dccd7d97de388813f353286a92e8a
Drew Kimball <drewk@cockroachlabs.com>
2023-05-28 20:56:34 -0600

    opt: allow parsing for simple PL/pgSQL functions
    This patch takes advantage of functionality previously added to the PL/pgSQL
    parser and allows simple PL/pgSQL functions to be parsed, so that the
    unimplemented error is thrown during optbuilding rather than parsing. This
    paves the way for a future commit to add optbuilder support for PL/pgSQL.
    
    Epic: CRDB-799
    
    Release note: None

--- pkg/sql/catalog/funcdesc/func_desc.go
+++ pkg/sql/catalog/funcdesc/func_desc.go
@@ -686,6 +686,7 @@ func (desc *immutable) ToOverload() (ret *tree.Overload, err error) {
 		Body:       desc.FunctionBody,
 		IsUDF:      true,
 		Version:    uint64(desc.Version),
+		Language:   desc.getCreateExprLang(),
 	}
 
 	argTypes := make(tree.ParamTypes, 0, len(desc.Params))
@@ -784,6 +785,8 @@ func (desc *immutable) getCreateExprLang() tree.FunctionLanguage {
 	switch desc.Lang {
 	case catpb.Function_SQL:
 		return tree.FunctionLangSQL
+	case catpb.Function_PLPGSQL:
+		return tree.FunctionLangPLpgSQL
 	}
 	return tree.FunctionLangUnknown
 }
