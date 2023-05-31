cockroachdb/cockroach

commit a74ca72e09f2ad8d9a2e17deafa5b03ca8fbd716

row: refactor fetchers a bit in preparation of KV projection pushdown
This commit refactors different fetchers in `row` package in preparation
of introducing a new implementation of `storage.NextKVer` interface for
the KV projection pushdown work.

The main non-mechanical change is to how we're tracking observability
information. `GetBytesRead` and `GetBatchRequestsIssued` are now
introduced into `KVBatchFetcher` interface, and the implementation is
pushed one level lower. Consider the following chain of objects:
```
colfetcher.ColBatchScan -> colfetcher.cFetcher -> row.KVFetcher -> row.txnKVFetcher
```
Previously, we would track the number of bytes read in `row.KVFetcher`,
and now that responsibility is moved into `row.txnKVFetcher` (or
`row.txnKVStreamer` when using the streamer API). This is needed since
the upcoming work will introduce the following chain:
```
colfetcher.ColBatchDirectScan -> row.txnKVFetcher
```
A new helper struct is introduced to extract the common logic from
`row.txnKVFetcher` and `row.txnKVStreamer`.

Additionally, this commit makes several mechanical changes (like
exporting methods in `KVBatchFetcher` interface as well as
`KVBatchFetcherResponse` struct) which shouldn't be controversial.

Release note: None

pkg/sql/row/fetcher.go
@@ -53,27 +53,33 @@ const DebugRowFetch = false
 // part of the output.
 const noOutputColumn = -1
 
-// kvBatchFetcherResponse contains a response from the KVBatchFetcher.
-type kvBatchFetcherResponse struct {
-	// moreKVs indicates whether this response contains some keys from the
+// KVBatchFetcherResponse contains a response from the KVBatchFetcher.
+type KVBatchFetcherResponse struct {
+	// MoreKVs indicates whether this response contains some keys from the
 	// fetch.
 	//
 	// If true, then the fetch might (or might not) be complete at this point -
-	// another call to nextBatch() is needed to find out. If false, then the
+	// another call to NextBatch() is needed to find out. If false, then the
 	// fetch has already been completed and this response doesn't have any
 	// fetched data.
-	moreKVs bool
-	// Only one of kvs and batchResponse will be set. Which one is set depends
-	// on the server version. Both must be handled by calling code.
 	//
-	// kvs, if set, is a slice of roachpb.KeyValue, the deserialized kv pairs
+	// Note that it is possible that MoreKVs is true when neither KVs nor
+	// BatchResponse is set. This can occur when there was nothing to fetch for
+	// a Scan or a ReverseScan request, so the caller should just skip over such
+	// a response.
+	MoreKVs bool
+	// Only one of KVs and BatchResponse will be set. Which one is set depends
+	// on the request type (KVs is used for Gets and BatchResponse for Scans and
+	// ReverseScans). Both must be handled by calling code.
+	//
+	// KVs, if set, is a slice of roachpb.KeyValue, the deserialized kv pairs
 	// that were fetched.
-	kvs []roachpb.KeyValue
-	// batchResponse, if set, is a packed byte slice containing the keys and
-	// values. An empty batchResponse indicates that nothing was fetched for the
+	KVs []roachpb.KeyValue
+	// BatchResponse, if set, is a packed byte slice containing the keys and
+	// values. An empty BatchResponse indicates that nothing was fetched for the
 	// corresponding ScanRequest, and the caller is expected to skip over the
 	// response.
-	batchResponse []byte
+	BatchResponse []byte
 	// spanID is the ID associated with the span that generated this response.
 	spanID int
 }
@@ -89,11 +95,22 @@ type KVBatchFetcher interface {
 		firstBatchKeyLimit rowinfra.KeyLimit,
 	) error
 
-	// nextBatch returns the next batch of rows. See kvBatchFetcherResponse for
+	// NextBatch returns the next batch of rows. See KVBatchFetcherResponse for
 	// details on what is returned.
-	nextBatch(ctx context.Context) (kvBatchFetcherResponse, error)
+	NextBatch(ctx context.Context) (KVBatchFetcherResponse, error)
 
-	close(ctx context.Context)
+	// GetBytesRead returns the number of bytes read by this fetcher. It is safe
+	// for concurrent use and is able to handle a case of uninitialized fetcher.
+	GetBytesRead() int64
+
+	// GetBatchRequestsIssued returns the number of BatchRequests issued by this
+	// fetcher throughout its lifetime. It is safe for concurrent use and is
+	// able to handle a case of uninitialized fetcher.
+	GetBatchRequestsIssued() int64
+
+	// Close releases the resources of this KVBatchFetcher. Must be called once
+	// the fetcher is no longer in use.
+	Close(ctx context.Context)
 }
 
 type tableInfo struct {
@@ -379,21 +396,22 @@ func (rf *Fetcher) Init(ctx context.Context, args FetcherInitArgs) error {
 		}
 		rf.kvFetcher = args.StreamingKVFetcher
 	} else if !args.WillUseKVProvider {
-		fetcherArgs := kvBatchFetcherArgs{
+		var batchRequestsIssued int64
+		fetcherArgs := newTxnKVFetcherArgs{
 			reverse:                    args.Reverse,
 			lockStrength:               args.LockStrength,
 			lockWaitPolicy:             args.LockWaitPolicy,
 			lockTimeout:                args.LockTimeout,
 			acc:                        rf.kvFetcherMemAcc,
 			forceProductionKVBatchSize: args.ForceProductionKVBatchSize,
+			batchRequestsIssued:        &batchRequestsIssued,
 		}
-		var batchRequestsIssued int64
 		if args.Txn != nil {
-			fetcherArgs.sendFn = makeKVBatchFetcherDefaultSendFunc(args.Txn, &batchRequestsIssued)
+			fetcherArgs.sendFn = makeTxnKVFetcherDefaultSendFunc(args.Txn, &batchRequestsIssued)
 			fetcherArgs.requestAdmissionHeader = args.Txn.AdmissionHeader()
 			fetcherArgs.responseAdmissionQ = args.Txn.DB().SQLKVResponseAdmissionQ
 		}
-		rf.kvFetcher = newKVFetcher(newKVBatchFetcher(fetcherArgs), &batchRequestsIssued)
+		rf.kvFetcher = newKVFetcher(newTxnKVFetcherInternal(fetcherArgs))
 	}
 
 	return nil
@@ -408,8 +426,12 @@ func (rf *Fetcher) Init(ctx context.Context, args FetcherInitArgs) error {
 //   - allowing the caller to update the Fetcher to use the new txn. In this case,
 //     the caller should be careful since reads performed under different txns
 //     do not provide consistent view of the data.
+//
+// Note that this resets the number of batch requests issued by the Fetcher.
+// Consider using GetBatchRequestsIssued if that information is needed.
 func (rf *Fetcher) SetTxn(txn *kv.Txn) error {
-	sendFn := makeKVBatchFetcherDefaultSendFunc(txn, rf.kvFetcher.atomics.batchRequestsIssued)
+	var batchRequestsIssued int64
+	sendFn := makeTxnKVFetcherDefaultSendFunc(txn, &batchRequestsIssued)
 	return rf.setTxnAndSendFn(txn, sendFn)
 }
 
@@ -611,10 +633,7 @@ func (rf *Fetcher) ConsumeKVProvider(ctx context.Context, f *KVProvider) error {
 	if rf.kvFetcher != nil {
 		rf.kvFetcher.Close(ctx)
 	}
-	// We won't actually perform any KV reads, so we don't need to track the
-	// number of batch requests issued - the case of the KVProvider is handled
-	// separately in GetBatchRequestsIssued().
-	rf.kvFetcher = newKVFetcher(f, nil /* batchRequestsIssued */)
+	rf.kvFetcher = newKVFetcher(f)
 	return rf.startScan(ctx)
 }
 
@@ -1261,13 +1280,16 @@ func (rf *Fetcher) Key() roachpb.Key {
 
 // GetBytesRead returns total number of bytes read by the underlying KVFetcher.
 func (rf *Fetcher) GetBytesRead() int64 {
+	if rf == nil || rf.kvFetcher == nil {
+		return 0
+	}
 	return rf.kvFetcher.GetBytesRead()
 }
 
 // GetBatchRequestsIssued returns total number of BatchRequests issued by the
 // underlying KVFetcher.
 func (rf *Fetcher) GetBatchRequestsIssued() int64 {
-	if rf == nil || rf.args.WillUseKVProvider {
+	if rf == nil || rf.kvFetcher == nil || rf.args.WillUseKVProvider {
 		return 0
 	}
 	return rf.kvFetcher.GetBatchRequestsIssued()

pkg/sql/row/kv_batch_streamer.go
@@ -25,6 +25,7 @@ import (
 
 // txnKVStreamer handles retrieval of key/values.
 type txnKVStreamer struct {
+	kvBatchFetcherHelper
 	streamer   *kvstreamer.Streamer
 	keyLocking lock.Strength
 
@@ -49,13 +50,18 @@ var _ KVBatchFetcher = &txnKVStreamer{}
 
 // newTxnKVStreamer creates a new txnKVStreamer.
 func newTxnKVStreamer(
-	streamer *kvstreamer.Streamer, lockStrength descpb.ScanLockingStrength, acc *mon.BoundAccount,
+	streamer *kvstreamer.Streamer,
+	lockStrength descpb.ScanLockingStrength,
+	acc *mon.BoundAccount,
+	batchRequestsIssued *int64,
 ) KVBatchFetcher {
-	return &txnKVStreamer{
+	f := &txnKVStreamer{
 		streamer:   streamer,
 		keyLocking: getKeyLockingStrength(lockStrength),
 		acc:        acc,
 	}
+	f.kvBatchFetcherHelper.init(f.nextBatch, batchRequestsIssued)
+	return f
 }
 
 // SetupNextFetch implements the KVBatchFetcher interface.
@@ -113,10 +119,10 @@ func (f *txnKVStreamer) getSpanID(resultPosition int) int {
 // GetResponses).
 func (f *txnKVStreamer) proceedWithLastResult(
 	ctx context.Context,
-) (skip bool, _ kvBatchFetcherResponse, _ error) {
+) (skip bool, _ KVBatchFetcherResponse, _ error) {
 	result := f.lastResultState.Result
-	ret := kvBatchFetcherResponse{
-		moreKVs: true,
+	ret := KVBatchFetcherResponse{
+		MoreKVs: true,
 		spanID:  f.getSpanID(result.Position),
 	}
 	if get := result.GetResp; get != nil {
@@ -125,21 +131,21 @@ func (f *txnKVStreamer) proceedWithLastResult(
 		if get.Value == nil {
 			// Nothing found in this particular response, so we skip it.
 			f.releaseLastResult(ctx)
-			return true, kvBatchFetcherResponse{}, nil
+			return true, KVBatchFetcherResponse{}, nil
 		}
 		origSpan := f.spans[result.Position]
 		f.getResponseScratch[0] = roachpb.KeyValue{Key: origSpan.Key, Value: *get.Value}
-		ret.kvs = f.getResponseScratch[:]
+		ret.KVs = f.getResponseScratch[:]
 		return false, ret, nil
 	}
 	scan := result.ScanResp
 	if len(scan.BatchResponses) > 0 {
-		ret.batchResponse, f.lastResultState.remainingBatches = scan.BatchResponses[0], scan.BatchResponses[1:]
+		ret.BatchResponse, f.lastResultState.remainingBatches = scan.BatchResponses[0], scan.BatchResponses[1:]
 	}
 	// We're consciously ignoring scan.Rows argument since the Streamer
 	// guarantees to always produce Scan responses using BATCH_RESPONSE format.
 	//
-	// Note that ret.batchResponse might be nil when the ScanResponse is empty,
+	// Note that ret.BatchResponse might be nil when the ScanResponse is empty,
 	// and the caller (the KVFetcher) will skip over it.
 	return false, ret, nil
 }
@@ -149,15 +155,14 @@ func (f *txnKVStreamer) releaseLastResult(ctx context.Context) {
 	f.lastResultState.Result = kvstreamer.Result{}
 }
 
-// nextBatch implements the KVBatchFetcher interface.
-func (f *txnKVStreamer) nextBatch(ctx context.Context) (kvBatchFetcherResponse, error) {
+func (f *txnKVStreamer) nextBatch(ctx context.Context) (resp KVBatchFetcherResponse, _ error) {
 	// Check whether there are more batches in the current ScanResponse.
 	if len(f.lastResultState.remainingBatches) > 0 {
-		ret := kvBatchFetcherResponse{
-			moreKVs: true,
+		ret := KVBatchFetcherResponse{
+			MoreKVs: true,
 			spanID:  f.getSpanID(f.lastResultState.Result.Position),
 		}
-		ret.batchResponse, f.lastResultState.remainingBatches = f.lastResultState.remainingBatches[0], f.lastResultState.remainingBatches[1:]
+		ret.BatchResponse, f.lastResultState.remainingBatches = f.lastResultState.remainingBatches[0], f.lastResultState.remainingBatches[1:]
 		return ret, nil
 	}
 
@@ -188,7 +193,7 @@ func (f *txnKVStreamer) nextBatch(ctx context.Context) (kvBatchFetcherResponse, 
 	var err error
 	f.results, err = f.streamer.GetResults(ctx)
 	if len(f.results) == 0 || err != nil {
-		return kvBatchFetcherResponse{moreKVs: false}, err
+		return KVBatchFetcherResponse{MoreKVs: false}, err
 	}
 	return f.nextBatch(ctx)
 }
@@ -201,8 +206,8 @@ func (f *txnKVStreamer) reset(ctx context.Context) {
 	}
 }
 
-// close releases the resources of this txnKVStreamer.
-func (f *txnKVStreamer) close(ctx context.Context) {
+// Close releases the resources of this txnKVStreamer.
+func (f *txnKVStreamer) Close(ctx context.Context) {
 	f.reset(ctx)
 	f.streamer.Close(ctx)
 	*f = txnKVStreamer{}

pkg/sql/row/fetcher_test.go
@@ -711,6 +711,9 @@ func TestRowFetcherReset(t *testing.T) {
 	); err != nil {
 		t.Fatal(err)
 	}
+	// We need to nil out the kvFetchers since these are stored as pointers.
+	fetcher.kvFetcher = nil
+	resetFetcher.kvFetcher = nil
 
 	if !reflect.DeepEqual(resetFetcher, fetcher) {
 		t.Fatal("unequal before and after reset", resetFetcher, fetcher)

commit 42a2d6b55b37cfefda36569c21b45db8461dcdda

row: fix some stats for txnKVStreamer
This commit fixes the collection of "bytes read" and "gRPC calls" stats
by the txnKVStreamer that was broken in a74ca72e09f2ad8d9a2e17deafa5b03ca8fbd716.
In particular, that commit introduced a new helper that contained
counters for those two metrics, and that helper was being reset on
`Close` method (which is, somewhat unfortunately, can be called before
the counters are retrived vie `Get*` methods of the `KVBatchFetcher`
interface. The bug is fixed by preserving the helper state. The
corresponding testing has been extended. Note that txnKVFetcher isn't
affected that it (at least right now) doesn't deeply unset itself on
`Close`.

Release note (bug fix): The collection of `KV bytes read` and `KV gRPC
calls` execution statistics during EXPLAIN ANALYZE could previously be
incorrect (it would remain at zero) in some cases and is now fixed. The
bug was introduced in 23.1 release.

pkg/sql/row/fetcher.go
@@ -136,7 +136,8 @@ type KVBatchFetcher interface {
 	GetBatchRequestsIssued() int64
 
 	// Close releases the resources of this KVBatchFetcher. Must be called once
-	// the fetcher is no longer in use.
+	// the fetcher is no longer in use. Note that observability-related methods
+	// can still be safely called after Close.
 	Close(ctx context.Context)
 }
 

pkg/sql/row/kv_batch_streamer.go
@@ -213,5 +213,6 @@ func (f *txnKVStreamer) reset(ctx context.Context) {
 func (f *txnKVStreamer) Close(ctx context.Context) {
 	f.reset(ctx)
 	f.streamer.Close(ctx)
-	*f = txnKVStreamer{}
+	// Preserve observability-related fields.
+	*f = txnKVStreamer{kvBatchFetcherHelper: f.kvBatchFetcherHelper}
 }

pkg/sql/explain_test.go
@@ -323,11 +323,10 @@ func TestExplainStatsCollected(t *testing.T) {
 	}
 }
 
-// TestExplainMVCCSteps makes sure that the MVCC stats are properly collected
-// during explain analyze. This can't be a normal logic test because the MVCC
-// stats are marked as nondeterministic (they change depending on number of
-// nodes in the query).
-func TestExplainMVCCSteps(t *testing.T) {
+// TestExplainKVInfo makes sure that miscellaneous KV-level stats are properly
+// collected during EXPLAIN ANALYZE. This can't be a normal logic test because
+// KV-level stats are marked as non-deterministic.
+func TestExplainKVInfo(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 
@@ -345,50 +344,72 @@ func TestExplainMVCCSteps(t *testing.T) {
 		} else {
 			r.Exec(t, "SET vectorize = off")
 		}
-		r.Exec(t, "DROP TABLE IF EXISTS ab")
-		r.Exec(t, "DROP TABLE IF EXISTS bc")
-		r.Exec(t, "CREATE TABLE ab (a PRIMARY KEY, b) AS SELECT g, g FROM generate_series(1,1000) g(g)")
-		r.Exec(t, "CREATE TABLE bc (b PRIMARY KEY, c) AS SELECT g, g FROM generate_series(1,1000) g(g)")
+		for _, streamer := range []bool{true, false} {
+			if streamer {
+				r.Exec(t, "SET streamer_enabled = true")
+			} else {
+				r.Exec(t, "SET streamer_enabled = false")
+			}
+			r.Exec(t, "DROP TABLE IF EXISTS ab")
+			r.Exec(t, "DROP TABLE IF EXISTS bc")
+			r.Exec(t, "CREATE TABLE ab (a PRIMARY KEY, b) AS SELECT g, g FROM generate_series(1,1000) g(g)")
+			r.Exec(t, "CREATE TABLE bc (b PRIMARY KEY, c) AS SELECT g, g FROM generate_series(1,1000) g(g)")
 
-		scanQuery := "SELECT count(*) FROM ab"
-		expectedSteps, expectedSeeks := 1000, 1
-		foundSteps, foundSeeks := getMVCCStats(t, r, scanQuery)
+			scanQuery := "SELECT count(*) FROM ab"
+			info := getKVInfo(t, r, scanQuery)
 
-		assert.Equal(t, expectedSteps, foundSteps)
-		assert.Equal(t, expectedSeeks, foundSeeks)
-		assert.Equal(t, expectedSteps, foundSteps)
-		assert.Equal(t, expectedSeeks, foundSeeks)
+			assert.Equal(t, 1000, info.counters[rowsRead])
+			assert.LessOrEqual(t, 31 /* KiB */, info.counters[bytesRead])
+			assert.Equal(t, 1, info.counters[gRPCCalls])
+			assert.Equal(t, 1000, info.counters[stepCount])
+			assert.Equal(t, 1, info.counters[seekCount])
 
-		// Update all rows.
-		r.Exec(t, "UPDATE ab SET b=b+1 WHERE true")
+			lookupJoinQuery := "SELECT count(*) FROM ab INNER LOOKUP JOIN bc ON ab.b = bc.b"
+			info = getKVInfo(t, r, lookupJoinQuery)
 
-		expectedSteps, expectedSeeks = 1000, 1
-		foundSteps, foundSeeks = getMVCCStats(t, r, scanQuery)
-
-		assert.Equal(t, expectedSteps, foundSteps)
-		assert.Equal(t, expectedSeeks, foundSeeks)
-
-		// Check that the lookup join (which is executed via a row-by-row
-		// processor wrapped into the vectorized flow when vectorize=true)
-		// correctly propagates the scan stats.
-		lookupJoinQuery := "SELECT count(*) FROM ab INNER LOOKUP JOIN bc ON ab.b = bc.b"
-		foundSteps, foundSeeks = getMVCCStats(t, r, lookupJoinQuery)
-		// We're mainly interested in the fact whether the propagation takes
-		// place, so one of the values being positive is sufficient.
-		assert.Greater(t, foundSteps+foundSeeks, 0)
+			assert.Equal(t, 1000, info.counters[rowsRead])
+			assert.LessOrEqual(t, 13 /* KiB */, info.counters[bytesRead])
+			assert.Equal(t, 1, info.counters[gRPCCalls])
+			assert.Equal(t, 0, info.counters[stepCount])
+			assert.Equal(t, 1000, info.counters[seekCount])
+		}
 	}
 }
 
-// getMVCCStats returns the number of MVCC steps and seeks found in the EXPLAIN
-// ANALYZE of the given query from the top-most operator in the plan (i.e. if
-// there are multiple operators exposing the scan stats, then the first info
-// that appears in the EXPLAIN output is used).
-func getMVCCStats(t *testing.T, r *sqlutils.SQLRunner, query string) (foundSteps, foundSeeks int) {
-	rows := r.Query(t, "EXPLAIN ANALYZE(VERBOSE) "+query)
+const (
+	rowsRead = iota
+	bytesRead
+	gRPCCalls
+	stepCount
+	seekCount
+	numKVCounters
+)
+
+type kvInfo struct {
+	counters [numKVCounters]int
+}
+
+var patterns [numKVCounters]*regexp.Regexp
+
+func init() {
+	patterns[rowsRead] = regexp.MustCompile(`KV rows read: (\d+)`)
+	patterns[bytesRead] = regexp.MustCompile(`KV bytes read: (\d+) \w+`)
+	patterns[gRPCCalls] = regexp.MustCompile(`KV gRPC calls: (\d+)`)
+	patterns[stepCount] = regexp.MustCompile(`MVCC step count \(ext/int\): (\d+)/[\d+]`)
+	patterns[seekCount] = regexp.MustCompile(`MVCC seek count \(ext/int\): (\d+)/[\d+]`)
+}
+
+// getKVInfo returns miscellaneous KV-level stats found in the EXPLAIN ANALYZE
+// of the given query from the top-most operator in the plan (i.e. if there are
+// multiple operators exposing the scan stats, then the first info that appears
+// in the EXPLAIN output is used).
+func getKVInfo(t *testing.T, r *sqlutils.SQLRunner, query string) kvInfo {
+	rows := r.Query(t, "EXPLAIN ANALYZE (VERBOSE) "+query)
 	var output strings.Builder
 	var str string
 	var err error
-	var stepsSet, seeksSet bool
+	var info kvInfo
+	var counterSet [numKVCounters]bool
 	for rows.Next() {
 		if err := rows.Scan(&str); err != nil {
 			t.Fatal(err)
@@ -398,19 +419,17 @@ func getMVCCStats(t *testing.T, r *sqlutils.SQLRunner, query string) (foundSteps
 		str = strings.TrimSpace(str)
 		// Numbers are printed with commas to indicate 1000s places, remove them.
 		str = strings.ReplaceAll(str, ",", "")
-		stepRe := regexp.MustCompile(`MVCC step count \(ext/int\): (\d+)/(\d+)`)
-		stepMatches := stepRe.FindStringSubmatch(str)
-		if len(stepMatches) == 3 && !stepsSet {
-			foundSteps, err = strconv.Atoi(stepMatches[1])
-			assert.NoError(t, err)
-			stepsSet = true
-		}
-		seekRe := regexp.MustCompile(`MVCC seek count \(ext/int\): (\d+)/(\d+)`)
-		seekMatches := seekRe.FindStringSubmatch(str)
-		if len(seekMatches) == 3 && !seeksSet {
-			foundSeeks, err = strconv.Atoi(seekMatches[1])
-			assert.NoError(t, err)
-			seeksSet = true
+		for i := 0; i < numKVCounters; i++ {
+			if counterSet[i] {
+				continue
+			}
+			matches := patterns[i].FindStringSubmatch(str)
+			if len(matches) == 2 {
+				info.counters[i], err = strconv.Atoi(matches[1])
+				assert.NoError(t, err)
+				counterSet[i] = true
+				break
+			}
 		}
 	}
 	if err := rows.Close(); err != nil {
@@ -420,7 +439,7 @@ func getMVCCStats(t *testing.T, r *sqlutils.SQLRunner, query string) (foundSteps
 		fmt.Println("Explain output:")
 		fmt.Println(output.String())
 	}
-	return foundSteps, foundSeeks
+	return info
 }
 
 // TestExplainAnalyzeWarnings verifies that warnings are printed whenever the