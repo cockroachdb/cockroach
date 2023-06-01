749f10ddc17526b2391bfa74bcd8fc7e36a95c26
Rafi Shamim <rafi@cockroachlabs.com>
2023-06-01 13:00:35 -0400

    sql: prevent dropped user from accessing the cluster
    Release note (bug fix): Previously if a user was logged in while a
    different session dropped that user, the dropped user would still
    inherit privileges from the `public` role. Now, CockroachDB checks that
    the user exists before allowing it to inherit privileges from the
    `public` role.
    
    In addition, any active web sessions are now revoked when a user is dropped.

--- pkg/sql/authorization.go
+++ pkg/sql/authorization.go
@@ -175,6 +175,16 @@ func (p *planner) HasPrivilege(
 
 	// Check if the 'public' pseudo-role has privileges.
 	if privs.CheckPrivilege(username.PublicRoleName(), privilegeKind) {
+		// Before returning true, make sure the user actually exists.
+		// We only need to check for existence here, since a dropped user will not
+		// appear in the role hierarchy, so it cannot pass the privilege check made
+		// later in this function. The RoleExists call performs a system table
+		// lookup, so it's better not to do it in the general case.
+		if exists, err := p.RoleExists(ctx, user); err != nil {
+			return false, err
+		} else if !exists {
+			return false, pgerror.Newf(pgcode.UndefinedObject, "role %s was concurrently dropped", user)
+		}
 		return true, nil
 	}
 
