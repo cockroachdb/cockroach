926c4d2a81b81f4e39d698677d7b78555c94c176
Faizan Qazi <faizan@cockroachlabs.com>
2023-06-01 15:34:10 -0400

    sql/schemachanger: skip validation and backfill for added tables
    Previously, we would always generate operations to backfill
    and validate on newly created tables. This will prevent
    us from being able to generate single stage plans for
    creating relations. This patch removes those checks
    for added tables.
    
    Epic: none
    
    Release note: None

---
+++ pkg/sql/schemachanger/scexec/scmutationexec/sequence.go
@@ -0,0 +1,77 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package scmutationexec
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/tabledesc"
+	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scop"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/catid"
+)
+
+func (i *immediateVisitor) CreateSequenceDescriptor(
+	_ context.Context, op scop.CreateSequenceDescriptor,
+) error {
+	mut := tabledesc.NewBuilder(&descpb.TableDescriptor{
+		ParentID:      catid.InvalidDescID, // Set by `SchemaParent` element
+		Name:          "",                  // Set by `Namespace` element
+		ID:            op.SequenceID,
+		Privileges:    &catpb.PrivilegeDescriptor{Version: catpb.Version21_2}, // Populated by `UserPrivileges` elements and `Owner` element
+		Version:       1,
+		FormatVersion: descpb.InterleavedFormatVersion,
+	}).BuildCreatedMutable()
+	tabledDesc := mut.(*tabledesc.Mutable)
+	tabledDesc.State = descpb.DescriptorState_ADD
+	tabledDesc.SequenceOpts = &descpb.TableDescriptor_SequenceOpts{}
+	i.CreateDescriptor(mut)
+	return nil
+}
+
+func (i *immediateVisitor) CreateOrUpdateSequenceOptions(
+	ctx context.Context, op scop.CreateOrUpdateSequenceOptions,
+) error {
+	sc, err := i.checkOutTable(ctx, op.SequenceID)
+	if err != nil {
+		return err
+	}
+	sc.SequenceOpts.Increment = op.Increment
+	sc.SequenceOpts.MinValue = op.Min
+	sc.SequenceOpts.MaxValue = op.Max
+	sc.SequenceOpts.Start = op.Start
+	sc.SequenceOpts.CacheSize = op.CacheSize
+	return nil
+}
+
+func (i *immediateVisitor) InitSequence(ctx context.Context, op scop.InitSequence) error {
+	sc, err := i.checkOutTable(ctx, op.SequenceID)
+	if err != nil {
+		return err
+	}
+	startVal := sc.SequenceOpts.Start
+	if op.RestartWith != nil {
+		startVal = *op.RestartWith
+	}
+	startVal = startVal - sc.SequenceOpts.Increment
+	i.ImmediateMutationStateUpdater.InitSequence(sc.ID, startVal)
+	return nil
+}
+
+func (i *immediateVisitor) SetTableName(ctx context.Context, op scop.SetTableName) error {
+	sc, err := i.checkOutTable(ctx, op.TableID)
+	if err != nil {
+		return err
+	}
+	sc.SetName(op.Name)
+	return nil
+}
--- pkg/sql/schemachanger/scplan/internal/opgen/op_funcs.go
+++ pkg/sql/schemachanger/scplan/internal/opgen/op_funcs.go
@@ -14,6 +14,7 @@ import (
 	"reflect"
 
 	"github.com/cockroachdb/cockroach/pkg/clusterversion"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scop"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/screl"
@@ -49,6 +50,7 @@ func statementForDropJob(e scpb.Element, md *opGenContext) scop.StatementForDrop
 // lookup.
 type opGenContext struct {
 	scpb.TargetState
+	Current         []scpb.Status
 	ActiveVersion   clusterversion.ClusterVersion
 	elementToTarget map[scpb.Element]int
 	InRollback      bool
@@ -61,6 +63,7 @@ func makeOpgenContext(
 		ActiveVersion:   activeVersion,
 		InRollback:      cs.InRollback,
 		TargetState:     cs.TargetState,
+		Current:         cs.Current,
 		elementToTarget: make(map[scpb.Element]int),
 	}
 	for i := range cs.Targets {
@@ -163,4 +166,29 @@ func checkOpFunc(el scpb.Element, fn interface{}) (opType scop.Type, _ error) {
 			out, opInterfaceType)
 	}
 	return opType, nil
+}
+
+// checkIfDescriptorIsAdded given the context this will determine if
+// a descriptor is an added state, which will imply that certain
+// steps can be completely skipped (i.e. backfill and validatin).
+func checkIfDescriptorIsAdded(id descpb.ID, md *opGenContext) bool {
+	for idx, t := range md.Targets {
+		// Only descriptors transitioning from ABSENT headed
+		// towards PUBLI can be considered added.
+		if (md.Current[idx] != scpb.Status_ABSENT &&
+			md.Current[idx] != scpb.Status_DESCRIPTOR_ADDED) ||
+			t.TargetStatus != scpb.Status_PUBLIC {
+			continue
+		}
+		// Validate this is the descriptor we are
+		// looking for.
+		if screl.GetDescID(t.Element()) != id {
+			continue
+		}
+		switch t.Element().(type) {
+		case *scpb.Sequence, *scpb.Table, *scpb.View:
+			return true
+		}
+	}
+	return false
 }
