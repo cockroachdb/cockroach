0ffef284adfa3ebc7f7efaad65a1043a96af399a
Andrew Baptist <baptist@cockroachlabs.com>
2023-06-02 13:24:03 -0400

    server: remove unused LivenessStatus field
    This field was returned in the response to a DecommissionPreCheck, but
    the value was never read. In a seperate PR we are attempting to remove
    the usage of this field, so removing it here helps.
    
    Epic: none
    
    Release note: None

--- pkg/server/admin.go
+++ pkg/server/admin.go
@@ -2733,13 +2733,11 @@ func (s *systemAdminServer) DecommissionPreCheck(
 			resultsByNodeID[nID] = serverpb.DecommissionPreCheckResponse_NodeCheckResult{
 				NodeID:                nID,
 				DecommissionReadiness: serverpb.DecommissionPreCheckResponse_UNKNOWN,
-				LivenessStatus:        livenessStatus,
 			}
 		} else if livenessStatus == livenesspb.NodeLivenessStatus_DECOMMISSIONED {
 			resultsByNodeID[nID] = serverpb.DecommissionPreCheckResponse_NodeCheckResult{
 				NodeID:                nID,
 				DecommissionReadiness: serverpb.DecommissionPreCheckResponse_ALREADY_DECOMMISSIONED,
-				LivenessStatus:        livenessStatus,
 			}
 		} else {
 			nodesToCheck = append(nodesToCheck, nID)
@@ -2784,7 +2782,6 @@ func (s *systemAdminServer) DecommissionPreCheck(
 		resultsByNodeID[nID] = serverpb.DecommissionPreCheckResponse_NodeCheckResult{
 			NodeID:                nID,
 			DecommissionReadiness: readiness,
-			LivenessStatus:        livenessStatusByNodeID[nID],
 			ReplicaCount:          int64(numReplicas),
 			CheckedRanges:         rangeCheckErrsByNode[nID],
 		}
