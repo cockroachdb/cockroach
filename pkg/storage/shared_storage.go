617dfbf2963df6224530620881856298351b4e3c
Radu Berinde <radu@cockroachlabs.com>
2023-06-05 11:25:49 -0700

    storage: set LimitHint for pebble object reads
    Epic: none
    Release note: None

--- pkg/storage/shared_storage.go
+++ pkg/storage/shared_storage.go
@@ -38,6 +38,7 @@ var _ shared.ObjectReader = (*externalStorageReader)(nil)
 func (r *externalStorageReader) ReadAt(ctx context.Context, p []byte, offset int64) error {
 	reader, _, err := r.es.ReadFile(ctx, r.objName, cloud.ReadOptions{
 		Offset:     offset,
+		LengthHint: int64(len(p)),
 		NoFileSize: true,
 	})
 	if err != nil {
