0933849cfc0f3d15acb6ce370ae4b33aacf8cbfc
Faizan Qazi <faizan@cockroachlabs.com>
2023-06-06 11:36:18 -0400

    cli/doctor: handle quoted namespace entries
    Previously, when parsing the namespace entries
    we did not handle quoted table names properly.
    This patch addresses, this by unquoting entries
    properly.
    
    Fixes: #104347
    
    Release note: None

--- pkg/cli/doctor_test.go
+++ pkg/cli/doctor_test.go
@@ -68,6 +68,18 @@ func TestDoctorZipDir(t *testing.T) {
 		})
 	})
 
+	t.Run("examine", func(t *testing.T) {
+		out, err := c.RunWithCapture("debug doctor examine zipdir testdata/doctor/debugzip-with-quotes")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Using datadriven allows TESTFLAGS=-rewrite.
+		datadriven.RunTest(t, datapathutils.TestDataPath(t, "doctor", "test_examine_zipdir_with_quotes"), func(t *testing.T, td *datadriven.TestData) string {
+			return out
+		})
+	})
+
 	t.Run("recreate", func(t *testing.T) {
 		out, err := c.RunWithCapture("debug doctor recreate zipdir testdata/doctor/debugzip")
 		if err != nil {
