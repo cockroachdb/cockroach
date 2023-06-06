a009d22c26edc0007805d050f022df3d6c80e66e
David Taylor <tinystatemachine@gmail.com>
2023-06-06 19:47:09 +0000

    backupccl: add execution locality filter to RESTORE worker placement
    This mirrors the similar option to BACKUP and changefeeds, restricting the job coordinator and
    the worker processes planned by the job to executing on nodes which match the passed locality filter.
    
    Release note (sql change): RESTORE can now be passed a WITH EXECUTION LOCALITY option similar to BACKUP, to restrict execution of the job to nodes with matching localities.
    Epic: CRDB-28521.

--- pkg/ccl/backupccl/restore_processor_planning.go
+++ pkg/ccl/backupccl/restore_processor_planning.go
@@ -21,6 +21,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobsprofiler"
 	"github.com/cockroachdb/cockroach/pkg/kv"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catenumpb"
@@ -78,6 +79,7 @@ func distRestore(
 	spanFilter spanCoveringFilter,
 	numImportSpans int,
 	useSimpleImportSpans bool,
+	execLocality roachpb.Locality,
 	progCh chan *execinfrapb.RemoteProducerMetadata_BulkProcessorProgress,
 ) error {
 	defer close(progCh)
@@ -111,7 +113,10 @@ func distRestore(
 	memMonSSTs := memoryMonitorSSTs.Get(execCtx.ExecCfg().SV())
 	makePlan := func(ctx context.Context, dsp *sql.DistSQLPlanner) (*sql.PhysicalPlan, *sql.PlanningCtx, error) {
 
-		planCtx, sqlInstanceIDs, err := dsp.SetupAllNodesPlanning(ctx, execCtx.ExtendedEvalContext(), execCtx.ExecCfg())
+		planCtx, sqlInstanceIDs, err := dsp.SetupAllNodesPlanningWithOracle(
+			ctx, execCtx.ExtendedEvalContext(), execCtx.ExecCfg(),
+			physicalplan.DefaultReplicaChooser, execLocality,
+		)
 		if err != nil {
 			return nil, nil, err
 		}
