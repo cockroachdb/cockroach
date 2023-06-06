715c63fd71f572575dc5dfef5820d7c8464a6c42
Jeff <swenson@cockroachlabs.com>
2023-06-06 17:37:20 +0000

    tenantcostmodel: charge for cross region network traffic
    Previously the 'tenant_cost_model.region_multiplier_table' allowed
    configuring the cost of multi-region operations by applying a multiplier
    to the entire operation cost. There were three problems with this approach
    that prevented deploying it to production:
    
    1. There was a flat multiplier for reads and writes. Because the fixed
       overhead of a read is much lower, the cost impact of cross region
       network traffic is much larger when compared to writes. The break
       even read multiplier was an order of magnitude higher than the break
       even write multiplier.
    2. The correct multiplier depends heavily on how many bytes are in an
       operation. Large operations require a larger multiplier to break even
       than smaller operations. Again the break even multiplier for large
       operations needed to be an order of magnitude larger than the break
       even for small operations.
    3. The original setting assumed the cost of network traffic was
       symmetric between two different regions. That assumption is true for
       some cloud providers, but is not true for all cloud providers.
    
    Now, the 'tenant_cost_model.cross_region_network_cost' setting allows
    configuring the RU/byte cost of network traffic between region pairs.
    The cost is computed by the dist sender because computing the network
    cost requires locality information for the local node, the target
    replica, and all replica's in the range.
    
    The network cost only applies to logical byte traffic, it does not apply
    to RPC overhead. This is believed to be okay, because the fixed RU cost
    of each request and batch is high enough to cover the network cost of
    the operation overhead.
    
    This only accounts for network traffic incurred by reads or writes
    between the sql server or the kv server. It does not account for network
    traffic generated by zone configuration changes, replica rebalancing, or
    distsql.
    - Currently distsql is disabled in production Serverless and will only
      be reenabled once distsql properly accounts for network traffic.
      Distsql should use the same byte per RU cost table as sql server <->
      kv traffic.
    - Zone configuration is expected to generate a small enough amount of
      the overall network traffic that we do not need to charge for it. We
      can revise this decision in the future if production data shows that
      is not true.
    - Replica rebalancing is something controlled by CRDB. If rebalancing
      cost ends up being a problem, we could look into reducing the amount
      of cross-region rebalancing traffic or we could start charging for
      rebalancing traffic.
    
    This change is backwards incompatible and will be back ported to 23.1.
    This is safe because Serverless is the only user of the setting.
    
    Release Note: none
    
    Part of: https://cockroachlabs.atlassian.net/browse/CC-7140

--- pkg/multitenant/tenantcostmodel/settings_test.go
+++ pkg/multitenant/tenantcostmodel/settings_test.go
@@ -30,106 +30,62 @@ func TestParseRegionalCostMultiplierTableSetting(t *testing.T) {
 			input: "",
 		},
 		{
+			name:  "empty_json",
+			input: `{"regionPairs":[]}`,
+		},
+		{
+			name: "valid",
+			input: `{"regionPairs": [
+				{"fromRegion": "us-central1", "toRegion": "us-west1", "cost": 10},
+				{"fromRegion": "us-west1", "toRegion": "us-central1", "cost": 20}
+			]}`,
+		},
+		{
 			name:  "malformed",
 			input: "testing",
 			err:   "invalid character",
 		},
 		{
-			name:  "valid_json",
-			input: `{"regions":["asia-southeast1"],"matrix":[[1,1.5,2.6],[1,3.5],[1]]}`,
-			err:   "unexpected number of rows",
+			name: "entry_missing_from_region",
+			input: `{"regionPairs": [
+				{"toRegion": "us-central1", "cost": 0}
+			]}`,
+			err: "entry 0 is missing 'fromRegion'",
 		},
 		{
-			name:  "empty_json",
-			input: `{"regions":[],"matrix":[]}`,
+			name: "entry_missing_to_region",
+			input: `{"regionPairs": [
+				{"fromRegion": "us-central1", "cost": 0}
+			]}`,
+			err: "entry 0 is missing 'toRegion'",
 		},
 		{
-			name:  "valid",
-			input: `{"regions":["us-east1","eu-central1","asia-southeast1"],"matrix":[[1,1.5,2.6],[1,3.5],[1]]}`,
+			name: "loopback_entry",
+			input: `{"regionPairs": [
+				{"fromRegion": "us-central1", "toRegion": "us-central1", "cost": 0}
+			]}`,
+			err: "'us-central1' contains an entry for itself",
+		},
+		{
+			name: "entry_negative_cost",
+			input: `{"regionPairs": [
+				{"fromRegion": "us-central1", "toRegion": "us-west1", "cost": 10},
+				{"fromRegion": "us-west1", "toRegion": "us-central1", "cost": -20}
+			]}`,
+			err: "must not be negative",
+		},
+		{
+			name: "missing_region",
+			input: `{"regionPairs": [
+				{"fromRegion": "us-central1", "toRegion": "us-west1", "cost": 0}
+			]}`,
+			err: "missing from region 'us-west1' to region 'us-central1'",
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			table, err := parseRegionalCostMultiplierTableSetting(tc.input)
+			err := CrossRegionNetworkCostSetting.Validate(nil, tc.input)
 			if tc.err != "" {
 				require.Regexp(t, tc.err, err.Error())
-			} else {
-				require.NoError(t, err)
-				require.NotNil(t, table)
-			}
-		})
-	}
-}
-
-func TestRegionalCostMultiplierCompactTable_Validate(t *testing.T) {
-	defer leaktest.AfterTest(t)()
-
-	for _, tc := range []struct {
-		name  string
-		table *RegionalCostMultiplierCompactTable
-		err   string
-	}{
-		{
-			name: "mismatched_rows",
-			table: &RegionalCostMultiplierCompactTable{
-				Regions: []string{"a"},
-			},
-			err: "unexpected number of rows: found 1 regions, but 0 rows in matrix",
-		},
-		{
-			name: "invalid_matrix_start",
-			table: &RegionalCostMultiplierCompactTable{
-				Regions: []string{"a"},
-				Matrix: [][]RUMultiplier{
-					{1, 2},
-				},
-			},
-			err: "malformed cost multiplier matrix",
-		},
-		{
-			name: "invalid_matrix_end",
-			table: &RegionalCostMultiplierCompactTable{
-				Regions: []string{"a", "b", "c"},
-				Matrix: [][]RUMultiplier{
-					{1, 2, 3},
-					{2, 3},
-					{},
-				},
-			},
-			err: "malformed cost multiplier matrix",
-		},
-		{
-			name: "negative values",
-			table: &RegionalCostMultiplierCompactTable{
-				Regions: []string{"a", "b", "c"},
-				Matrix: [][]RUMultiplier{
-					{2, -3, 4},
-					{3, 4},
-					{4},
-				},
-			},
-			err: "values must be non-negative",
-		},
-		{
-			name: "valid",
-			table: &RegionalCostMultiplierCompactTable{
-				Regions: []string{"a", "b", "c", "d"},
-				Matrix: [][]RUMultiplier{
-					{1, 2, 3, 4},
-					{2, 3, 4},
-					{3, 4},
-					{0},
-				},
-			},
-		},
-		{
-			name:  "valid_empty",
-			table: &RegionalCostMultiplierCompactTable{},
-		},
-	} {
-		t.Run(tc.name, func(t *testing.T) {
-			err := tc.table.Validate()
-			if tc.err != "" {
-				require.EqualError(t, err, tc.err)
 			} else {
 				require.NoError(t, err)
 			}
@@ -140,317 +96,31 @@ func TestRegionalCostMultiplierCompactTable_Validate(t *testing.T) {
 func TestRegionalCostMultiplierTable_CostMultiplier(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 
-	for _, tc := range []struct {
-		name     string
-		table    *RegionalCostMultiplierTable
-		r1       string
-		r2       string
-		expected RUMultiplier
-	}{
-		{
-			name:     "nil_matrix",
-			table:    &RegionalCostMultiplierTable{},
-			r1:       "a",
-			r2:       "b",
-			expected: 1,
-		},
-		{
-			name: "no_row_key",
-			table: &RegionalCostMultiplierTable{
-				Matrix: map[string]map[string]RUMultiplier{},
-			},
-			r1:       "a",
-			r2:       "b",
-			expected: 1,
-		},
-		{
-			name: "nil_row",
-			table: &RegionalCostMultiplierTable{
-				Matrix: map[string]map[string]RUMultiplier{
-					"a": nil,
-				},
-			},
-			r1:       "a",
-			r2:       "b",
-			expected: 1,
-		},
-		{
-			name: "no_col_key",
-			table: &RegionalCostMultiplierTable{
-				Matrix: map[string]map[string]RUMultiplier{
-					"a": {},
-				},
-			},
-			r1:       "a",
-			r2:       "b",
-			expected: 1,
-		},
-		{
-			name: "found",
-			table: &RegionalCostMultiplierTable{
-				Matrix: map[string]map[string]RUMultiplier{
-					"a": {
-						"b": 100,
-					},
-				},
-			},
-			r1:       "a",
-			r2:       "b",
-			expected: 100,
-		},
-	} {
-		t.Run(tc.name, func(t *testing.T) {
-			require.Equal(t, tc.expected, tc.table.CostMultiplier(tc.r1, tc.r2))
-		})
-	}
-}
+	table, err := NewNetworkCostTable(`
+		{"regionPairs": [
+				{"fromRegion": "us-central1", "toRegion": "us-west1", "cost": 10},
+				{"fromRegion": "us-west1", "toRegion": "us-central1", "cost": 20}
+		]}
+	`)
+	require.NoError(t, err)
 
-func TestRegionalCostMultiplierTable_Validate(t *testing.T) {
-	defer leaktest.AfterTest(t)()
+	cost, found := table.Matrix[NetworkPath{
+		FromRegion: "us-central1",
+		ToRegion:   "us-west1",
+	}]
+	require.True(t, found)
+	require.Equal(t, cost, NetworkCost(10))
 
-	for _, tc := range []struct {
-		name  string
-		table *RegionalCostMultiplierTable
-		err   string
-	}{
-		{
-			name:  "nil_matrix",
-			table: &RegionalCostMultiplierTable{},
-		},
-		{
-			name: "nil_inner_matrix",
-			table: &RegionalCostMultiplierTable{
-				Matrix: map[string]map[string]RUMultiplier{
-					"a": nil,
-				},
-			},
-			err: "inner matrix cannot be nil",
-		},
-		{
-			name: "more_rows_than_cols",
-			table: &RegionalCostMultiplierTable{
-				Matrix: map[string]map[string]RUMultiplier{
-					"a": {
-						"b": 100,
-					},
-					"b": {
-						"a": 100,
-					},
-				},
-			},
-			err: "number of rows and columns do not match: rows=2, cols=1",
-		},
-		{
-			name: "more_cols_than_rows",
-			table: &RegionalCostMultiplierTable{
-				Matrix: map[string]map[string]RUMultiplier{
-					"a": {
-						"b": 100,
-						"c": 101,
-					},
-				},
-			},
-			err: "number of rows and columns do not match: rows=1, cols=2",
-		},
-		{
-			name: "not_symmetrical",
-			table: &RegionalCostMultiplierTable{
-				Matrix: map[string]map[string]RUMultiplier{
-					"a": {
-						"a": 10,
-						"b": 100,
-					},
-					"b": {
-						"a": 40,
-						"b": 10,
-					},
-				},
-			},
-			err: "table values are invalid",
-		},
-		{
-			name: "negative_values",
-			table: &RegionalCostMultiplierTable{
-				Matrix: map[string]map[string]RUMultiplier{
-					"a": {
-						"a": -10,
-						"b": 100,
-					},
-					"b": {
-						"a": 100,
-						"b": -20,
-					},
-				},
-			},
-			err: "table values must be non-negative",
-		},
-		{
-			name: "valid",
-			table: &RegionalCostMultiplierTable{
-				Matrix: map[string]map[string]RUMultiplier{
-					"a": {
-						"a": 10,
-						"b": 55,
-						"c": 66,
-					},
-					"b": {
-						"a": 55,
-						"b": 10,
-						"c": 77,
-					},
-					"c": {
-						"a": 66,
-						"b": 77,
-						"c": 10,
-					},
-				},
-			},
-		},
-	} {
-		t.Run(tc.name, func(t *testing.T) {
-			err := tc.table.Validate()
-			if tc.err != "" {
-				require.EqualError(t, err, tc.err)
-			} else {
-				require.NoError(t, err)
-			}
-		})
-	}
-}
+	cost, found = table.Matrix[NetworkPath{
+		FromRegion: "us-west1",
+		ToRegion:   "us-central1",
+	}]
+	require.True(t, found)
+	require.Equal(t, cost, NetworkCost(20))
 
-func TestMakeRegionalCostMultiplierTableFromCompact(t *testing.T) {
-	defer leaktest.AfterTest(t)()
-
-	t.Run("empty", func(t *testing.T) {
-		compact := &RegionalCostMultiplierCompactTable{}
-		require.NoError(t, compact.Validate())
-
-		table := MakeRegionalCostMultiplierTableFromCompact(compact)
-		expectedTable := RegionalCostMultiplierTable{
-			Matrix: map[string]map[string]RUMultiplier{},
-		}
-		require.Equal(t, expectedTable, table)
-		require.NoError(t, table.Validate())
-	})
-
-	t.Run("single region", func(t *testing.T) {
-		compact := &RegionalCostMultiplierCompactTable{
-			Regions: []string{"us-east1"},
-			Matrix:  [][]RUMultiplier{{1.9}},
-		}
-		require.NoError(t, compact.Validate())
-
-		table := MakeRegionalCostMultiplierTableFromCompact(compact)
-		expectedTable := RegionalCostMultiplierTable{
-			Matrix: map[string]map[string]RUMultiplier{
-				"us-east1": {
-					"us-east1": 1.9,
-				},
-			},
-		}
-		require.Equal(t, expectedTable, table)
-		require.NoError(t, table.Validate())
-	})
-
-	t.Run("multiple regions", func(t *testing.T) {
-		compact := &RegionalCostMultiplierCompactTable{
-			Regions: []string{"us-east1", "eu-central1", "asia-southeast1"},
-			Matrix: [][]RUMultiplier{
-				{1, 1.5, 2.6},
-				{1, 3.5},
-				{1},
-			},
-		}
-		require.NoError(t, compact.Validate())
-
-		table := MakeRegionalCostMultiplierTableFromCompact(compact)
-		expectedTable := RegionalCostMultiplierTable{
-			Matrix: map[string]map[string]RUMultiplier{
-				"us-east1": {
-					"us-east1":        1,
-					"eu-central1":     1.5,
-					"asia-southeast1": 2.6,
-				},
-				"eu-central1": {
-					"us-east1":        1.5,
-					"eu-central1":     1,
-					"asia-southeast1": 3.5,
-				},
-				"asia-southeast1": {
-					"us-east1":        2.6,
-					"eu-central1":     3.5,
-					"asia-southeast1": 1,
-				},
-			},
-		}
-		require.Equal(t, expectedTable, table)
-		require.NoError(t, table.Validate())
-	})
-}
-
-func TestMakeRegionalCostMultiplierCompactTableFromExpanded(t *testing.T) {
-	defer leaktest.AfterTest(t)()
-
-	t.Run("empty", func(t *testing.T) {
-		expanded := &RegionalCostMultiplierTable{}
-		require.NoError(t, expanded.Validate())
-
-		table := MakeRegionalCostMultiplierCompactTableFromExpanded(expanded)
-		expectedTable := RegionalCostMultiplierCompactTable{
-			Regions: []string{},
-			Matrix:  [][]RUMultiplier{},
-		}
-		require.Equal(t, expectedTable, table)
-		require.NoError(t, table.Validate())
-	})
-
-	t.Run("single region", func(t *testing.T) {
-		expanded := &RegionalCostMultiplierTable{
-			Matrix: map[string]map[string]RUMultiplier{
-				"eu-central1": {
-					"eu-central1": 42,
-				},
-			},
-		}
-		require.NoError(t, expanded.Validate())
-
-		table := MakeRegionalCostMultiplierCompactTableFromExpanded(expanded)
-		expectedTable := RegionalCostMultiplierCompactTable{
-			Regions: []string{"eu-central1"},
-			Matrix:  [][]RUMultiplier{{42}},
-		}
-		require.Equal(t, expectedTable, table)
-		require.NoError(t, table.Validate())
-	})
-
-	t.Run("multiple regions", func(t *testing.T) {
-		expanded := &RegionalCostMultiplierTable{
-			Matrix: map[string]map[string]RUMultiplier{
-				"us-east1": {
-					"us-east1":        1,
-					"eu-central1":     1.5,
-					"asia-southeast1": 2.6,
-				},
-				"eu-central1": {
-					"us-east1":        1.5,
-					"eu-central1":     1,
-					"asia-southeast1": 3.5,
-				},
-				"asia-southeast1": {
-					"us-east1":        2.6,
-					"eu-central1":     3.5,
-					"asia-southeast1": 1,
-				},
-			},
-		}
-		require.NoError(t, expanded.Validate())
-
-		table := MakeRegionalCostMultiplierCompactTableFromExpanded(expanded)
-		expectedTable := RegionalCostMultiplierCompactTable{
-			Regions: []string{"asia-southeast1", "eu-central1", "us-east1"},
-			Matrix:  [][]RUMultiplier{{1, 3.5, 2.6}, {1, 1.5}, {1}},
-		}
-		require.Equal(t, expectedTable, table)
-		require.NoError(t, table.Validate())
-	})
+	cost = table.Matrix[NetworkPath{
+		FromRegion: "does-not-exist",
+		ToRegion:   "also-does-not-exist",
+	}]
+	require.Equal(t, cost, NetworkCost(0))
 }
