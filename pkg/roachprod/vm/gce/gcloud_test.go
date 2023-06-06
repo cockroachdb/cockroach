5ad20cc74ab410c9b4f6e9656fbd4de7ac511316
Stan Rosenberg <stan.rosenberg@gmail.com>
2023-06-06 12:05:50 -0400

    roachtest: upgrade GCE to n2 and AWS to m6id and c6id
    Previously, roachtest used n1 in GCE, m5d and c6d in AWS.
    CockroachDB Cloud hardware now uses n2 in GCE, m6i in AWS [1].
    This change brings roachtest hardware into parity with
    CockroachDB Cloud; it is based on the draft PR in [2].
    
    [1] https://cockroachlabs.atlassian.net/wiki/spaces/MC/pages/2799501550/CockroachDB+Cloud+Hardware
    [2] https://github.com/cockroachdb/cockroach/pull/99991
    
    Epic: none
    
    Release note: None
    
    Co-authored-by: Nick Travers <travers@cockroachlabs.com>

---
+++ pkg/roachprod/vm/gce/gcloud_test.go
@@ -0,0 +1,61 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package gce
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAllowedLocalSSDCount(t *testing.T) {
+	for i, c := range []struct {
+		machineType string
+		expected    []int
+		unsupported bool
+	}{
+		// N1 has the same ssd counts for all cpu counts.
+		{"n1-standard-4", []int{1, 2, 3, 4, 5, 6, 7, 8, 16, 24}, false},
+		{"n1-highcpu-64", []int{1, 2, 3, 4, 5, 6, 7, 8, 16, 24}, false},
+		{"n1-higmem-96", []int{1, 2, 3, 4, 5, 6, 7, 8, 16, 24}, false},
+
+		{"n2-standard-4", []int{1, 2, 4, 8, 16, 24}, false},
+		{"n2-standard-8", []int{1, 2, 4, 8, 16, 24}, false},
+		{"n2-standard-16", []int{2, 4, 8, 16, 24}, false},
+		// N.B. n2-standard-30 doesn't exist, but we still get the ssd counts based on cpu count.
+		{"n2-standard-30", []int{4, 8, 16, 24}, false},
+		{"n2-standard-32", []int{4, 8, 16, 24}, false},
+		{"n2-standard-48", []int{8, 16, 24}, false},
+		{"n2-standard-64", []int{8, 16, 24}, false},
+		{"n2-standard-80", []int{8, 16, 24}, false},
+		{"n2-standard-96", []int{16, 24}, false},
+		{"n2-standard-128", []int{16, 24}, false},
+
+		{"c2-standard-4", []int{1, 2, 4, 8}, false},
+		{"c2-standard-8", []int{1, 2, 4, 8}, false},
+		{"c2-standard-16", []int{2, 4, 8}, false},
+		{"c2-standard-30", []int{4, 8}, false},
+		{"c2-standard-60", []int{8}, false},
+		// c2-standard-64 doesn't exist and exceed cpu count, so we expect an error.
+		{"c2-standard-64", nil, true},
+	} {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			actual, err := AllowedLocalSSDCount(c.machineType)
+			if c.unsupported {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.EqualValues(t, c.expected, actual)
+			}
+		})
+	}
+}
