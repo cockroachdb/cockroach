c6bc21293d4f484261fa9754e6f870a9b1d7c07a
Thomas Hardy <thardy@cockroachlabs.com>
2023-06-05 23:02:04 -0400

    auditloggingccl: migrate role-based audit logging as a CCL feature
    This change moves the existing role-based audit logging logic to be
    consumed as a CCL (enterprise) feature.
    
    The notable changes here are:
    - moving the audit logging cluster settings (i.e. `sql.log.user_audit`
      and `sql.log.user_audit.reduced_config.enabled`) to the `ccl` package.
    Consequently, these cluster settings will only exist when the use has a
    CCL license
    - gating the `sql.log.user_audit` cluster setting behind the enterprise
      CCL license. This was done by adding an enterprise CCL license check
    in the cluster setting's validation function. Users will be unable to
    change this cluster setting (and thereby will not be able to enable
    role-based audit logging) unless they have an enterprise CCL license
    -  the addition of function hooks to be used on CCL initialization,
       namely:
       - `ConfigureRoleBasedAuditClusterSettings`: used to add the
         `SetOnChange` cluster setting hook to the audit logging cluster
    setting
       - `UserAuditLogConfigEmpty`: used to check whether the audit logging
         cluster setting is empty
       - `UserAuditReducedConfigEnabled`: used to check whether the reduced
         audit configuration is enabled (note: regular users are still able
    to enable/disable this cluster setting, but it will take no effect as
    they will have no way to enable role-based audit logging)
       - `UserAuditEnterpriseParamsHook`: used to retrieve parameters
         necessary for enterprise license checks within the the
    `sql.log.user_audit` cluster setting validation function
    
    Release note (sql change): Role-based audit logging is now a CCL
    (enterprise) feature. Only enterprise CCL users will be able to
    configure role-based audit logging using the `sql.log.user_audit`
    cluster setting.

--- pkg/ccl/auditloggingccl/audit_log_config.go
+++ pkg/ccl/auditloggingccl/audit_log_config.go
@@ -1,32 +1,36 @@
 // Copyright 2023 The Cockroach Authors.
 //
-// Use of this software is governed by the Business Source License
-// included in the file licenses/BSL.txt.
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
 //
-// As of the Change Date specified in that file, in accordance with
-// the Business Source License, use of this software will be governed
-// by the Apache License, Version 2.0, included in the file
-// licenses/APL.txt.
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
 
-package auditlogging
+package auditloggingccl
 
 import (
 	"context"
 
+	"github.com/cockroachdb/cockroach/pkg/ccl/utilccl"
 	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/sql/auditlogging"
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgcode"
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/errors"
 )
+
+const auditConfigDefaultValue = ""
 
 // UserAuditLogConfig is a cluster setting that takes a user/role-based audit configuration.
 var UserAuditLogConfig = settings.RegisterValidatedStringSetting(
 	settings.TenantWritable,
 	"sql.log.user_audit",
 	"user/role-based audit logging configuration",
-	"",
+	auditConfigDefaultValue,
 	validateAuditLogConfig,
-).WithPublic()
+)
 
 // UserAuditEnableReducedConfig is a cluster setting that enables/disables a computed
 // reduced configuration. This allows us to compute the audit configuration once at
@@ -43,19 +47,28 @@ var UserAuditEnableReducedConfig = settings.RegisterBoolSetting(
 		"(user role memberships/cluster setting) are not reflected within session. "+
 		"Users will need to start a new session to see these changes in their auditing behaviour.",
 	false,
-).WithPublic()
+)
 
 func validateAuditLogConfig(_ *settings.Values, input string) error {
-	if input == "" {
+	if input == auditConfigDefaultValue {
 		// Empty config
 		return nil
 	}
-	// Ensure it can be parsed.
-	conf, err := parse(input)
+	st, clusterID, err := auditlogging.UserAuditEnterpriseParamsHook()
 	if err != nil {
 		return err
 	}
-	if len(conf.settings) == 0 {
+	enterpriseCheckErr := utilccl.CheckEnterpriseEnabled(st, clusterID, "role-based audit logging")
+	if enterpriseCheckErr != nil {
+		return pgerror.Wrap(enterpriseCheckErr,
+			pgcode.InsufficientPrivilege, "role-based audit logging requires enterprise license")
+	}
+	// Ensure it can be parsed.
+	conf, err := auditlogging.Parse(input)
+	if err != nil {
+		return err
+	}
+	if len(conf.Settings) == 0 {
 		// The string was not empty, but we were unable to parse any settings.
 		return errors.WithHint(errors.New("no entries"),
 			"To use the default configuration, assign the empty string ('').")
@@ -66,17 +79,41 @@ func validateAuditLogConfig(_ *settings.Values, input string) error {
 // UpdateAuditConfigOnChange initializes the local
 // node's audit configuration each time the cluster setting
 // is updated.
-func UpdateAuditConfigOnChange(ctx context.Context, acl *AuditConfigLock, st *cluster.Settings) {
+func UpdateAuditConfigOnChange(
+	ctx context.Context, acl *auditlogging.AuditConfigLock, st *cluster.Settings,
+) {
 	val := UserAuditLogConfig.Get(&st.SV)
-	config, err := parse(val)
+	config, err := auditlogging.Parse(val)
 	if err != nil {
 		// We encounter an error parsing (i.e. invalid config), fallback
 		// to an empty config.
 		log.Ops.Warningf(ctx, "invalid audit log config (sql.log.user_audit): %v\n"+
 			"falling back to empty audit config", err)
-		config = EmptyAuditConfig()
+		config = auditlogging.EmptyAuditConfig()
 	}
 	acl.Lock()
 	acl.Config = config
 	acl.Unlock()
+}
+
+var ConfigureRoleBasedAuditClusterSettings = func(ctx context.Context, acl *auditlogging.AuditConfigLock, st *cluster.Settings, sv *settings.Values) {
+	UserAuditLogConfig.SetOnChange(
+		sv, func(ctx context.Context) {
+			UpdateAuditConfigOnChange(ctx, acl, st)
+		})
+	UpdateAuditConfigOnChange(ctx, acl, st)
+}
+
+var UserAuditLogConfigEmpty = func(sv *settings.Values) bool {
+	return UserAuditLogConfig.Get(sv) == ""
+}
+
+var UserAuditReducedConfigEnabled = func(sv *settings.Values) bool {
+	return UserAuditEnableReducedConfig.Get(sv)
+}
+
+func init() {
+	auditlogging.ConfigureRoleBasedAuditClusterSettings = ConfigureRoleBasedAuditClusterSettings
+	auditlogging.UserAuditLogConfigEmpty = UserAuditLogConfigEmpty
+	auditlogging.UserAuditReducedConfigEnabled = UserAuditReducedConfigEnabled
 }
