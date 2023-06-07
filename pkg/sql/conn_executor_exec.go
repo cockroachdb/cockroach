331a40d513162136a36341520935bf3d21bd0f2b
Xin Hao Zhang <xzhang@cockroachlabs.com>
2023-06-07 15:32:30 -0400

    telemetry: add txn sampling option
    The telemetry logging channel currently logs statement executions based on
    whether or not enough time has passed between emitted query log events. We
    would also like to have a sampling strategy that is able to capture all
    queries in a transaction. This commit introduces the "transaction" sampling
    mode for telemetry, which enables us to sample transaction executions as a
    whole for telemetry. This will allow us to capture all queries for a sampled
    txn.
    
    The "transaction" mode works in the following way:
      - 'forced' stmt logging is still enabled (e.g. if the stmt is a non-DML type
        or the stmt has tracing on
      - Otherwise, an entire transaction is either logged or skipped. A txn is tracked
      - if at the time of logging the first stmt in the txn, there is enough capacity
       to track another transaction (according to the setting
       `sql.telemetry.txn_mode.tracking_limit`), and enough time has elapsed since
       starting tracking for a previous txn.
    
    New cluster settings (not documented currently):
    - sql.telemetry.query_sampling.mode: enum value of "statement" (default)
    or "transaction" that controls the execution type observed for sampling
    - sql.telemetry.txn_mode.tracking_limit: the maximum number of transactions
    tracked at one time for which we will send all statements to telemetry
    
    Release note: None
    
    Part of #103518

--- pkg/sql/conn_executor_exec.go
+++ pkg/sql/conn_executor_exec.go
@@ -2782,6 +2782,7 @@ func (ex *connExecutor) onTxnFinish(ctx context.Context, ev txnEvent) {
 				transactionFingerprintID,
 			)
 		}
+
 		err = ex.recordTransactionFinish(ctx, transactionFingerprintID, ev, implicit, txnStart)
 		if err != nil {
 			if log.V(1) {
@@ -2791,6 +2792,10 @@ func (ex *connExecutor) onTxnFinish(ctx context.Context, ev txnEvent) {
 		}
 		// If we have a commitTimestamp, we should use it.
 		ex.previousTransactionCommitTimestamp.Forward(ev.commitTimestamp)
+	}
+
+	if telemetryLoggingEnabled.Get(&ex.server.cfg.Settings.SV) {
+		ex.server.TelemetryLoggingMetrics.onTxnFinish(ev.txnID.String())
 	}
 }
 
--- pkg/sql/telemetry_logging.go
+++ pkg/sql/telemetry_logging.go
@@ -11,10 +11,12 @@
 package sql
 
 import (
+	"context"
 	"sync/atomic"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/settings"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql/execstats"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
@@ -28,9 +30,11 @@ const defaultMaxEventFrequency = 8
 var TelemetryMaxEventFrequency = settings.RegisterIntSetting(
 	settings.TenantWritable,
 	"sql.telemetry.query_sampling.max_event_frequency",
-	"the max event frequency at which we sample queries for telemetry, "+
-		"note that this value shares a log-line limit of 10 logs per second on the "+
-		"telemetry pipeline with all other telemetry events",
+	"the max event frequency at which we sample executions for telemetry, "+
+		"note that it is recommended that this value shares a log-line limit of 10 "+
+		" logs per second on the telemetry pipeline with all other telemetry events. "+
+		"If sampling mode is set to transaction, all statements associated with a single "+
+		"transaction are counted as 1 unit.",
 	defaultMaxEventFrequency,
 	settings.NonNegativeInt,
 )
@@ -42,19 +46,65 @@ var telemetryInternalQueriesEnabled = settings.RegisterBoolSetting(
 	false,
 ).WithPublic()
 
+const (
+	telemetryModeStatement = iota
+	telemetryModeTransaction
+)
+
+var telemetrySamplingMode = settings.RegisterEnumSetting(
+	settings.TenantWritable,
+	"sql.telemetry.query_sampling.mode",
+	"the execution level used for telemetry sampling. If set to 'statement', events "+
+		"are sampled at the statement execution level. If set to transaction, events are "+
+		"sampled at the txn execution level, i.e. all statements for a txn will be logged "+
+		"and are counted together as one sampled event (events are still emitted one per "+
+		"statement)",
+	"statement",
+	map[int64]string{
+		telemetryModeStatement:   "statement",
+		telemetryModeTransaction: "transaction",
+	},
+)
+
+var telemetryTrackedTxnsLimit = settings.RegisterIntSetting(
+	settings.TenantWritable,
+	"sql.telemetry.txn_mode.tracking_limit",
+	"the maximum number of transactions tracked at one time for which we will send "+
+		"all statements to telemetry",
+	10000,
+	settings.NonNegativeInt,
+)
+
 // TelemetryLoggingMetrics keeps track of the last time at which an event
 // was logged to the telemetry channel, and the number of skipped queries
 // since the last logged event.
 type TelemetryLoggingMetrics struct {
+	st *cluster.Settings
+
 	mu struct {
 		syncutil.RWMutex
 		// The timestamp of the last emitted telemetry event.
 		lastEmittedTime time.Time
+
+		// observedTxnExecutions is used to track txn executions that are currently
+		// being logged. When the sampling mode is set to txns, we must ensure we
+		// log all stmts for a txn. Txns are removed upon completing execution when
+		// all events have been captured.
+		observedTxnExecutions map[string]interface{}
 	}
+
 	Knobs *TelemetryLoggingTestingKnobs
 
 	// skippedQueryCount is used to produce the count of non-sampled queries.
 	skippedQueryCount uint64
+}
+
+func newTelemetryLoggingmetrics(
+	knobs *TelemetryLoggingTestingKnobs, st *cluster.Settings,
+) *TelemetryLoggingMetrics {
+	t := TelemetryLoggingMetrics{Knobs: knobs, st: st}
+	t.mu.observedTxnExecutions = make(map[string]interface{})
+	return &t
 }
 
 // TelemetryLoggingTestingKnobs provides hooks and knobs for unit tests.
@@ -81,6 +131,39 @@ func NewTelemetryLoggingTestingKnobs(
 	}
 }
 
+// registerOnTelemetrySamplingModeChange sets up the callback for when the
+// telemetry sampling mode is changed. When switching from txn to stmt, we
+// clear the txns we are currently tracking for logging.
+func (t *TelemetryLoggingMetrics) registerOnTelemetrySamplingModeChange(
+	settings *cluster.Settings,
+) {
+	telemetrySamplingMode.SetOnChange(&settings.SV, func(ctx context.Context) {
+		mode := telemetrySamplingMode.Get(&settings.SV)
+		t.mu.Lock()
+		defer t.mu.Unlock()
+		if mode == telemetryModeStatement {
+			// Clear currently observed txns.
+			t.mu.observedTxnExecutions = make(map[string]interface{})
+		}
+	})
+}
+
+func (t *TelemetryLoggingMetrics) onTxnFinish(txnExecutionID string) {
+	if telemetrySamplingMode.Get(&t.st.SV) != telemetryModeTransaction {
+		return
+	}
+
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	delete(t.mu.observedTxnExecutions, txnExecutionID)
+}
+
+func (t *TelemetryLoggingMetrics) getNumberTrackedTxns() int {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return len(t.mu.observedTxnExecutions)
+}
+
 // ModuleTestingKnobs implements base.ModuleTestingKnobs interface.
 func (*TelemetryLoggingTestingKnobs) ModuleTestingKnobs() {}
 
@@ -91,21 +174,57 @@ func (t *TelemetryLoggingMetrics) timeNow() time.Time {
 	return timeutil.Now()
 }
 
-// maybeUpdateLastEmittedTime updates the lastEmittedTime if the amount of time
-// elapsed between lastEmittedTime and newTime is greater than requiredSecondsElapsed.
-func (t *TelemetryLoggingMetrics) maybeUpdateLastEmittedTime(
-	newTime time.Time, requiredSecondsElapsed float64,
-) bool {
+// shouldEmitLog returns true if the stmt should be logged. The last emitted time
+// tracked by telemetry logging metrics will be updated to the given time if any of
+// the following are met:
+//   - The telemetry mode is set to "transaction" AND the stmt is the first in
+//     the txn AND the txn is not already being tracked AND the required amount
+//     of time has elapsed.
+//   - The telemetry mode is set to "statement" AND the required amount of time has elapsed
+//   - The txn is not being tracked and the stmt is being forced to log.
+func (t *TelemetryLoggingMetrics) shouldEmitLog(
+	newTime time.Time, txnExecutionID string, force bool, stmtPosInTxn int,
+) (shouldEmit bool) {
+	maxEventFrequency := TelemetryMaxEventFrequency.Get(&t.st.SV)
+	requiredTimeElapsed := time.Second / time.Duration(maxEventFrequency)
+	isTxnMode := telemetrySamplingMode.Get(&t.st.SV) == telemetryModeTransaction
+	txnsLimit := int(telemetryTrackedTxnsLimit.Get(&t.st.SV))
+
+	var enoughTimeElapsed, txnIsTracked, startTrackingTxn bool
+	// Avoid taking the full lock if we don't have to.
+	func() {
+		t.mu.RLock()
+		defer t.mu.RUnlock()
+
+		_, txnIsTracked = t.mu.observedTxnExecutions[txnExecutionID]
+		enoughTimeElapsed = newTime.Sub(t.mu.lastEmittedTime) >= requiredTimeElapsed
+		startTrackingTxn = isTxnMode && txnExecutionID != "" &&
+			stmtPosInTxn == 1 && len(t.mu.observedTxnExecutions) < txnsLimit
+	}()
+
+	if txnIsTracked || (!force && (!enoughTimeElapsed || (isTxnMode && !startTrackingTxn))) {
+		// If we are already tracking the txn, we don't want to update the last emitted time.
+		// Otherwise, we'll update if we are forcing the log or enough time has passed.
+		return txnIsTracked
+	}
+
 	t.mu.Lock()
 	defer t.mu.Unlock()
 
-	lastEmittedTime := t.mu.lastEmittedTime
-	if float64(newTime.Sub(lastEmittedTime))*1e-9 >= requiredSecondsElapsed {
-		t.mu.lastEmittedTime = newTime
-		return true
+	// The lastEmittedTime and tracked txns may have changed since releasing the Rlock.
+	txnLimitReached := len(t.mu.observedTxnExecutions) >= txnsLimit
+	if !force && (newTime.Sub(t.mu.lastEmittedTime) < requiredTimeElapsed ||
+		(startTrackingTxn && txnLimitReached)) {
+		return false
 	}
 
-	return false
+	// We could be forcing the log so we should only track its txn if it meets the criteria.
+	if startTrackingTxn && !txnLimitReached {
+		t.mu.observedTxnExecutions[txnExecutionID] = struct{}{}
+	}
+
+	t.mu.lastEmittedTime = newTime
+	return true
 }
 
 func (t *TelemetryLoggingMetrics) getQueryLevelStats(
