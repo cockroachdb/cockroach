488d936f7ec2a4303ac2dbde75d094d458a780f2
Tobias Grieger <tobias.b.grieger@gmail.com>
2023-06-07 09:06:52 +0200

    fixup! rpc: retain information about failed connections
    Move peerKey to peer_map.go.

--- pkg/rpc/peer_map.go
+++ pkg/rpc/peer_map.go
@@ -11,9 +11,36 @@
 package rpc
 
 import (
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/util/circuit"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/redact"
 )
+
+// peerKey is used as key in the Context.peers map.
+// Connections which carry a different class but share a target and nodeID
+// will always specify distinct connections. Different remote node IDs get
+// distinct *Connection objects to ensure that we don't mis-route RPC
+// requests in the face of address reuse. Gossip connections and other
+// non-Internal users of the Context are free to dial nodes without
+// specifying a node ID (see GRPCUnvalidatedDial()) however later calls to
+// Dial with the same target and class with a node ID will create a new
+// underlying connection which will not be reused by calls specifying the
+// NodeID.
+type peerKey struct {
+	TargetAddr string
+	// NodeID of remote node, 0 when unknown, non-zero to check with remote node.
+	// Never mutated.
+	NodeID roachpb.NodeID
+	Class  ConnectionClass
+}
+
+var _ redact.SafeFormatter = peerKey{}
+
+// SafeFormat implements the redact.SafeFormatter interface.
+func (c peerKey) SafeFormat(p redact.SafePrinter, _ rune) {
+	p.Printf("{n%d: %s (%v)}", c.NodeID, c.TargetAddr, c.Class)
+}
 
 type peerMap struct {
 	mu struct {
