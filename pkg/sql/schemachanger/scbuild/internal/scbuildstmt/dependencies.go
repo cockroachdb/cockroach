248f0901053bb6cbd87dbaa58cee8c0697ffa0fa
Faizan Qazi <faizan@cockroachlabs.com>
2023-06-08 11:52:42 -0400

    sql/schemachanger: add support for OWNED BY on CREATE SEQUENCE
    Previously, our CREATE SEQUENCE support for the declarative
    schema changer fell back if an OWNED BY clause is specified.
    To address this patch adds support for owned by clause.
    
    Epic: none
    
    Release note: None

--- pkg/sql/schemachanger/scbuild/internal/scbuildstmt/dependencies.go
+++ pkg/sql/schemachanger/scbuild/internal/scbuildstmt/dependencies.go
@@ -192,6 +192,10 @@ type SchemaFeatureChecker interface {
 	CanPerformDropOwnedBy(
 		ctx context.Context, role username.SQLUsername,
 	) (bool, error)
+
+	// CanCreateCrossDBSequenceOwnerRef returns if cross database sequence
+	// owner references are allowed.
+	CanCreateCrossDBSequenceOwnerRef() error
 }
 
 // PrivilegeChecker checks an element's privileges.
