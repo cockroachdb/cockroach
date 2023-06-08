59623f9ed75cc12e024b26e5c8d2a2dcaa15cc45
Andrii Vorobiov <and.vorobiov@gmail.com>
2023-06-08 22:45:46 +0300

    sql: create system.txn_exec_insights and stmt_exec_insights tables
    This change adds two new system tables to store statements and
    transaction execution insights.
    
    Before, this data was kept in memory only and tracked limited
    number of latest insights. These tables will be used to persist
    this data periodically.
    
    Tables allow to store the same information as in memory store without
    aggregation.
    
    Tables implement Row level TTL to delete old records.
    
    Release note (sql): added new system.txn_exec_insights and
    stmt_exec_insights tables to persist execution insights.

--- pkg/sql/catalog/bootstrap/metadata.go
+++ pkg/sql/catalog/bootstrap/metadata.go
@@ -472,6 +472,8 @@ func addSystemDescriptorsToSchema(target *MetadataSchema) {
 	target.AddDescriptor(systemschema.StatementActivityTable)
 	target.AddDescriptor(systemschema.TransactionActivityTable)
 	target.AddDescriptorForSystemTenant(systemschema.TenantIDSequence)
+	target.AddDescriptor(systemschema.TransactionExecInsightsTable)
+	target.AddDescriptor(systemschema.StatementExecInsightsTable)
 
 	// Adding a new system table? It should be added here to the metadata schema,
 	// and also created as a migration for older clusters.
@@ -485,7 +487,7 @@ func addSystemDescriptorsToSchema(target *MetadataSchema) {
 // NumSystemTablesForSystemTenant is the number of system tables defined on
 // the system tenant. This constant is only defined to avoid having to manually
 // update auto stats tests every time a new system table is added.
-const NumSystemTablesForSystemTenant = 51
+const NumSystemTablesForSystemTenant = 53
 
 // addSplitIDs adds a split point for each of the PseudoTableIDs to the supplied
 // MetadataSchema.
