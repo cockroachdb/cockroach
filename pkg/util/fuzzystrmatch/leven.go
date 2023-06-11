2b87fdea1f382116ea7d29025e4ba2ca46512d4c
Azanul <azanulhaque@gmail.com>
2023-06-11 13:50:55 +0530

    builtins: add levenshtein_less_than
    Release justification: low-risk update to new functionality
    Release note (sql change): Implemented levenshtein_less_than(text, text, int) int.
    
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- pkg/util/fuzzystrmatch/leven.go
+++ pkg/util/fuzzystrmatch/leven.go
@@ -10,6 +10,8 @@
 
 package fuzzystrmatch
 
+import "fmt"
+
 // LevenshteinDistanceWithCost calculates the Levenshtein distance between
 // source and target. The distance is calculated using the given cost metrics
 // for each of the three possible edit operations.
@@ -65,6 +67,210 @@ func LevenshteinDistanceWithCost(source, target string, insCost, delCost, subCos
 // insertion, deletion and substitution are all one.
 func LevenshteinDistance(source, target string) int {
 	return LevenshteinDistanceWithCost(source, target, 1, 1, 1)
+}
+
+// LevenshteinLessThanDistance is the standard Levenshtein distance where the cost of
+// insertion, deletion and substitution are all one and we care about distances less than
+// or equal to maxDist.
+func LevenshteinLessThanDistance(source, target string, maxD int) int {
+	return LevenshteinLessThanDistanceWithCost(source, target, 1, 1, 1, maxD)
+}
+
+// LevenshteinLessThanDistanceWithCost is the Levenshtein distance between the source and
+// the target with the given costs of insertion, deletion and substitution.
+func LevenshteinLessThanDistanceWithCost(source, target string, insCost, delCost, subCost, maxDist int) int {
+	// Converting to a slice of runes allows us to count multi-byte characters
+	// properly.
+	s, t := []rune(source), []rune(target)
+	lenS, lenT := len(s), len(t)
+	fmt.Println(s, len(s), lenS)
+	fmt.Println(t, len(t), lenT)
+
+	startColumn := 0
+	stopColumn := lenS + 1
+	var netInserts int
+
+	// If maxDist >= 0, determine whether the bound is impossibly tight.  If so,
+	// return maxDist + 1 immediately.  Otherwise, determine whether it's tight
+	// enough to limit the computation we must perform.  If so, figure out
+	// initial stop column.
+	if maxDist >= 0 {
+		netInserts = lenT - lenS
+		minTheoDist := netInserts * insCost
+		if netInserts < 0 {
+			minTheoDist = -netInserts * delCost
+		}
+		if minTheoDist > maxDist {
+			fmt.Println("xxx", minTheoDist)
+			return maxDist + 1
+		}
+
+		subCost = min2(subCost, insCost+delCost)
+		maxTheoDist := minTheoDist + subCost*min2(lenS, lenT)
+		if maxDist >= maxTheoDist {
+			maxDist = -1
+		} else if insCost+delCost > 0 {
+			// Figure out how much of the first row of the notional matrix we
+			// need to fill in.  If the string is growing, the theoretical
+			// minimum distance already incorporates the cost of deleting the
+			// number of characters necessary to make the two strings equal in
+			// length.  Each additional deletion forces another insertion, so
+			// the best-case total cost increases by insCost + delCost. If the
+			// string is shrinking, the minimum theoretical cost assumes no
+			// excess deletions; that is, we're starting no further right than
+			// column n - m.  If we do start further right, the best-case
+			// total cost increases by insCost + delCost for each move right.
+			slackDist := maxDist - minTheoDist
+			bestColumn := 0
+			if netInserts < 0 {
+				bestColumn = -netInserts
+			}
+			stopColumn = bestColumn + (slackDist / (insCost + delCost)) + 1
+			if stopColumn > lenS {
+				stopColumn = lenS + 1
+			}
+		}
+	}
+
+	// One more cell for initialization column and row.
+	lenS++
+	lenT++
+
+	// Initialize previous and current rows of notional array.
+	prev, curr := make([]int, lenS), make([]int, lenS)
+
+	// To transform the first i characters of s into the first 0 characters of
+	// t, we must perform i deletions.
+	for i := startColumn; i < stopColumn; i++ {
+		prev[i] = i * delCost
+	}
+
+	// Loop through rows of the notional array.
+	for i := 1; i < lenT; i++ {
+		fmt.Println(prev)
+
+		// In the best case, values percolate down the diagonal unchanged, so
+		// we must increment stopColumn unless it's already on the right end
+		// of the array.  The inner loop will read prev[stopColumn], so we
+		// have to initialize it even though it shouldn't affect the result.
+		if stopColumn < lenS {
+			prev[stopColumn] = maxDist + 1
+			stopColumn++
+		}
+
+		// The main loop fills in curr, but curr[0] needs a special case: to
+		// transform the first 0 characters of s into the first j characters
+		// of t, we must perform j insertions.  However, if startColumn > 0,
+		// this special case does not apply.
+		j := 1
+		if startColumn == 0 {
+			curr[0] = i * insCost
+		} else {
+			j = startColumn
+		}
+
+		// This inner loop is critical to performance, so we include a
+		// fast-path to handle the (fairly common) case where no multibyte
+		// characters are in the mix.  The fast-path is entitled to assume
+		// that if s_char_len is not initialized then BOTH strings contain
+		// only single-byte characters.
+		for ; j < stopColumn; j++ {
+			// Calculate costs for insertion, deletion, and substitution.
+			// When calculating cost for substitution, we compare the last
+			// character of each possibly-multibyte character first,
+			// because that's enough to rule out most mis-matches.  If we
+			// get past that test, then we compare the lengths and the
+			// remaining bytes.
+			ins := prev[j] + insCost
+			del := curr[j-1] + delCost
+			var sub int
+			if t[i-1] == s[j-1] {
+				sub = prev[j-1]
+			} else {
+				sub = prev[j-1] + subCost
+			}
+
+			// Take the one with minimum cost.
+			curr[j] = min3(del, ins, sub)
+		}
+
+		// Swap current row with previous row.
+		prev, curr = curr, prev
+
+		// This chunk of code represents a significant performance hit if used
+		// in the case where there is no maxDist bound.  This is probably not
+		// because the maxDist >= 0 test itself is expensive, but rather because
+		// the possibility of needing to execute this code prevents tight
+		// optimization of the loop as a whole.
+		if maxDist >= 0 {
+			// The "zero point" is the column of the current row where the
+			// remaining portions of the strings are of equal length.  There
+			// are (n - 1) characters in the target string, of which j have
+			// been transformed.  There are (m - 1) characters in the source
+			// string, so we want to find the value for zp where (n - 1) - j =
+			// (m - 1) - zp.
+			zp := i - (lenT - lenS)
+
+			// Check whether the stop column can slide left.
+			for stopColumn > 0 {
+				ii := stopColumn - 1
+				netInserts = ii - zp
+
+				oc := -netInserts * delCost
+				if netInserts > 0 {
+					oc = netInserts * insCost
+				}
+
+				if prev[ii]+oc <= maxDist {
+					break
+				}
+
+				stopColumn--
+			}
+
+			// Check whether the start column can slide right.
+			for startColumn < stopColumn {
+				netInserts = startColumn - zp
+
+				oc := -netInserts * delCost
+				if netInserts > 0 {
+					oc = -netInserts * insCost
+				}
+				if prev[startColumn]+oc <= maxDist {
+					break
+				}
+
+				// We'll never again update these values, so we must make sure
+				// there's nothing here that could confuse any future
+				// iteration of the outer loop.
+				prev[startColumn] = maxDist + 1
+				curr[startColumn] = maxDist + 1
+
+				if startColumn != 0 {
+					source += string(s[startColumn-1])
+				}
+				startColumn++
+			}
+
+			// If they cross, we're going to exceed the bound.
+			if startColumn >= stopColumn {
+				return maxDist + 1
+			}
+		}
+	}
+
+	// Because the final value was swapped from the previous row to the
+	// current row, that's where we'll find it.
+	fmt.Println(prev)
+	fmt.Println(startColumn, stopColumn)
+	return prev[lenS-1]
+}
+
+func min2(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func min3(a, b, c int) int {
