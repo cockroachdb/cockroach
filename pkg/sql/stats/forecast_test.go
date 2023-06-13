174e833debf59b122bfba867afacb4658e4dd2d4
Michael Erickson <michae2@cockroachlabs.com>
2023-06-13 20:29:10 +0000

    sql/stats: better sentry report for NumRange=0 assertion failure
    The "first bucket should have NumRange=0" assertion failure has become
    hot, so add a validation step to statistics forecasting that will catch
    it earlier, before it causes query planning to fail. As part of this
    validation step, produce a better sentry report that should help us
    track down the problem.
    
    Informs: #93892
    
    Release note: None

--- pkg/sql/stats/forecast_test.go
+++ pkg/sql/stats/forecast_test.go
@@ -617,7 +617,7 @@ func TestForecastColumnStatistics(t *testing.T) {
 			expected := tc.forecast.toTableStatistic(jobspb.ForecastStatsName, i, descpb.ColumnIDs{1}, fullStatID, partialStatID)
 			at := testStatTime(tc.at)
 
-			forecast, err := forecastColumnStatistics(ctx, observed, at, 1)
+			forecast, err := forecastColumnStatistics(ctx, nil /* sv */, observed, at, 1)
 			if err != nil {
 				if !tc.err {
 					t.Errorf("test case %d unexpected forecastColumnStatistics err: %v", i, err)
