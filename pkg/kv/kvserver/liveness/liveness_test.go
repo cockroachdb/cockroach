b9ceed24129101cefbcf343d0f9210593ac45147
Andrew Baptist <baptist@cockroachlabs.com>
2023-06-15 10:17:46 -0400

    Move the NodeLiveness method into the liveness package.
    This moves the test for the NodeLiveness function from the store_pool to
    livenesspb but keeps the identical behavior of the method (the test
    conditions are unchanged).
    
    Epic: none
    
    Release note: None

--- pkg/kv/kvserver/liveness/liveness_test.go
+++ pkg/kv/kvserver/liveness/liveness_test.go
@@ -13,12 +13,16 @@ package liveness
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/liveness/livenesspb"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/stretchr/testify/require"
 )
 
 func TestShouldReplaceLiveness(t *testing.T) {
@@ -128,6 +132,159 @@ func TestShouldReplaceLiveness(t *testing.T) {
 			if act := livenessChanged(test.old, test.new); act != test.exp {
 				t.Errorf("unexpected update: %+v", test)
 			}
+		})
+	}
+}
+
+func TestNodeLivenessLivenessStatus(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	clock := hlc.NewClockForTesting(timeutil.NewManualTime(timeutil.Unix(100000, 100000)))
+	nl := NewNodeLiveness(NodeLivenessOptions{Clock: clock, Settings: cluster.MakeTestingClusterSettings()})
+	now := clock.Now()
+	threshold := TimeUntilNodeDead.Default()
+
+	for _, tc := range []struct {
+		name     string
+		liveness livenesspb.Liveness
+		expected livenesspb.NodeLivenessStatus
+	}{
+		// Valid status.
+		{
+			name: "Valid 5 min",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.AddDuration(5 * time.Minute).ToLegacyTimestamp(),
+				Draining:   false,
+			},
+			expected: livenesspb.NodeLivenessStatus_LIVE,
+		},
+		{
+			name: "Expires just slightly in the future",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.AddDuration(time.Nanosecond).ToLegacyTimestamp(),
+				Draining:   false,
+			},
+			expected: livenesspb.NodeLivenessStatus_LIVE,
+		},
+		{
+			name: "Expired status",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.ToLegacyTimestamp(),
+				Draining:   false,
+			},
+			expected: livenesspb.NodeLivenessStatus_UNAVAILABLE,
+		},
+		{
+			name: "Max bound of expired",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.AddDuration(-threshold).AddDuration(time.Nanosecond).ToLegacyTimestamp(),
+				Draining:   false,
+			},
+			expected: livenesspb.NodeLivenessStatus_UNAVAILABLE,
+		},
+		{
+			name: "Dead",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.AddDuration(-threshold).ToLegacyTimestamp(),
+				Draining:   false,
+			},
+			expected: livenesspb.NodeLivenessStatus_DEAD,
+		},
+		{
+			name: "Decommissioning",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.AddDuration(time.Second).ToLegacyTimestamp(),
+				Membership: livenesspb.MembershipStatus_DECOMMISSIONING,
+				Draining:   false,
+			},
+			expected: livenesspb.NodeLivenessStatus_DECOMMISSIONING,
+		},
+		{
+			name: "Decommissioning + expired",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.AddDuration(-threshold).ToLegacyTimestamp(),
+				Membership: livenesspb.MembershipStatus_DECOMMISSIONING,
+				Draining:   false,
+			},
+			expected: livenesspb.NodeLivenessStatus_DECOMMISSIONED,
+		},
+		{
+			name: "Decommissioning + live",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.AddDuration(time.Second).ToLegacyTimestamp(),
+				Membership: livenesspb.MembershipStatus_DECOMMISSIONED,
+				Draining:   false,
+			},
+			// Despite having marked the node as fully decommissioned, through
+			// this NodeLivenessStatus API we still surface the node as
+			// "Decommissioning". See #50707 for more details.
+			expected: livenesspb.NodeLivenessStatus_DECOMMISSIONING,
+		},
+		{
+			name: "Decommissioning + expired",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.AddDuration(-threshold).ToLegacyTimestamp(),
+				Membership: livenesspb.MembershipStatus_DECOMMISSIONED,
+				Draining:   false,
+			},
+			expected: livenesspb.NodeLivenessStatus_DECOMMISSIONED,
+		},
+		{
+			name: "Draining",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.AddDuration(5 * time.Minute).ToLegacyTimestamp(),
+				Draining:   true,
+			},
+			expected: livenesspb.NodeLivenessStatus_DRAINING,
+		},
+		{
+			name: "Decommissioning that is unavailable",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.ToLegacyTimestamp(),
+				Draining:   false,
+				Membership: livenesspb.MembershipStatus_DECOMMISSIONING,
+			},
+			expected: livenesspb.NodeLivenessStatus_UNAVAILABLE,
+		},
+		{
+			name: "Draining that is unavailable",
+			liveness: livenesspb.Liveness{
+				NodeID:     1,
+				Epoch:      1,
+				Expiration: now.ToLegacyTimestamp(),
+				Draining:   true,
+			},
+			expected: livenesspb.NodeLivenessStatus_UNAVAILABLE,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			// TODO(baptist): Remove this when LivenessStatus is removed. This is here
+			// to make sure we preserve existing behavior. Both the old and new
+			// mechanisms should return the same result.
+			//			require.Equal(t, tc.expected, storepool.LivenessStatus(tc.liveness, now, threshold))
+			require.Equal(t, tc.expected, nl.convertToNodeVitality(tc.liveness).LivenessStatus())
 		})
 	}
 }
