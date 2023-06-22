678694baae820bd209f513ec39e952112ebfcdbd
Andrew Baptist <baptist@cockroachlabs.com>
2023-06-22 14:23:08 -0400

    liveness: remove unused GetIsLiveMap
    All the code has been changed to no longer require the GetIsLiveMap
    function anymore.
    
    Epic: none
    
    Release note: None

--- pkg/kv/kvserver/liveness/cache.go
+++ pkg/kv/kvserver/liveness/cache.go
@@ -205,28 +205,6 @@ func (c *cache) GetLiveness(nodeID roachpb.NodeID) (_ Record, ok bool) {
 	return Record{}, false
 }
 
-// GetIsLiveMap returns a map of nodeID to boolean liveness status of
-// each node. This excludes nodes that were removed completely (dead +
-// decommissioned)
-func (c *cache) GetIsLiveMap() livenesspb.IsLiveMap {
-	lMap := livenesspb.IsLiveMap{}
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	now := c.clock.Now()
-	for nID, l := range c.mu.nodes {
-		isLive := l.IsLive(now)
-		if l.Membership.Decommissioned() {
-			// This is a node that was completely removed. Skip over it.
-			continue
-		}
-		lMap[nID] = livenesspb.IsLiveMapEntry{
-			Liveness: l.Liveness,
-			IsLive:   isLive,
-		}
-	}
-	return lMap
-}
-
 // getAllLivenessEntries returns a copy of all the entries currently in the
 // liveness cache. Most places should avoid calling this method and instead just
 // get the entry they need. In a few places in the code it is more efficient to
