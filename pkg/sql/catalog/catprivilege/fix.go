300257d153758e24e59fa1647fef7266c5d26a9f
Rafi Shamim <rafi@cockroachlabs.com>
2023-06-22 12:01:40 -0400

    catprivilege: remove MaybeFixUsagePrivForTablesAndDBs
    This fix is no longer needed, since e75af68da08 added a 21.2 upgrade job
    that fixed this bug for all descriptors.
    
    Release note: None

--- pkg/sql/catalog/catprivilege/fix.go
+++ pkg/sql/catalog/catprivilege/fix.go
@@ -17,48 +17,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/privilege"
 )
 
-// MaybeFixUsagePrivForTablesAndDBs fixes cases where privilege descriptors
-// with ZONECONFIG were corrupted after upgrading from 20.1 to 20.2.
-// USAGE was mistakenly added in the privilege bitfield above ZONECONFIG
-// causing privilege descriptors with ZONECONFIG in 20.1 to have USAGE privilege
-// instead of ZONECONFIG.
-// Fortunately ZONECONFIG was only valid on TABLES/DB while USAGE is not valid
-// on either so we know if the descriptor was corrupted.
-func MaybeFixUsagePrivForTablesAndDBs(ptr **catpb.PrivilegeDescriptor) bool {
-	if *ptr == nil {
-		*ptr = &catpb.PrivilegeDescriptor{}
-	}
-	p := *ptr
-
-	if p.Version > catpb.InitialVersion {
-		// InitialVersion is for descriptors that were created in versions 20.1 and
-		// earlier. If the privilege descriptor was created after 20.1, then we
-		// do not have to fix it. Furthermore privilege descriptor versions are
-		// currently never updated so we're guaranteed to only have this issue
-		// on privilege descriptors that are on "InitialVersion".
-		return false
-	}
-
-	modified := false
-	for i := range p.Users {
-		// Users is a slice of values, we need pointers to make them mutable.
-		userPrivileges := &p.Users[i]
-		// Tables and Database should not have USAGE privilege in 20.2 onwards.
-		// The only reason they would have USAGE privilege is because they had
-		// ZoneConfig in 20.1 and upgrading to 20.2 where USAGE was added
-		// in the privilege bitfield where ZONECONFIG previously was.
-		if privilege.USAGE.Mask()&userPrivileges.Privileges != 0 {
-			// Remove USAGE privilege and add ZONECONFIG. The privilege was
-			// originally ZONECONFIG in 20.1 but got changed to USAGE in 20.2
-			// due to changing the bitfield values.
-			userPrivileges.Privileges = (userPrivileges.Privileges - privilege.USAGE.Mask()) | privilege.ZONECONFIG.Mask()
-			modified = true
-		}
-	}
-
-	return modified
-}
-
 // MaybeFixPrivileges fixes the privilege descriptor if needed, including:
 // * adding default privileges for the "admin" role
 // * fixing default privileges for the "root" user
@@ -109,10 +67,6 @@ func MaybeFixPrivileges(
 	// Check "root" user and "admin" role.
 	fixSuperUser(username.RootUserName())
 	fixSuperUser(username.AdminRoleName())
-
-	if objectType == privilege.Table || objectType == privilege.Database {
-		changed = MaybeFixUsagePrivForTablesAndDBs(&p) || changed
-	}
 
 	for i := range p.Users {
 		// Users is a slice of values, we need pointers to make them mutable.
