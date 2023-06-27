c114c7e9cb5b5938c0e9c5893ab3cd0b10bccff9
Marius Posta <marius@cockroachlabs.com>
2023-06-27 16:34:54 -0400

    upgrades: enforce specific upgrades for V??_?Start keys.
    Previously, we'd added the precondition check functionality to cluster
    upgrade steps in large part because we wanted to check for descriptor
    corruptions before actually proceeding with a cluster upgrade. Otherwise
    what might (and did) happen was that an unrelated upgrade step would get
    stuck and throw an error while the corruption was still present, leaving
    the cluster upgrade in limbo in some intermediate internal version.
    Descriptor corruption can fly under the radar for a long time in some
    cases (like dangling back-references) because these might not otherwise
    cause any problems until a DROP is performed.
    
    The problem is that this precondition check was tied to a specific
    cluster version key which has since been deleted as part of the regular
    release engineering process and there were no tests or any other
    mechanism that would ensure that cluster version upgrades would always
    include this precondition.
    
    This commit enforces the presence of upgrade steps for all V??_?Start
    cluster version keys, which have internal version 2 and succeed each
    supported pre-existing release for this binary. These upgrade steps
    will be the first to run anytime a cluster version upgrade is performed.
    This removes the need from having to remember adding this every time a
    major release is done.
    
    Presently, these have a precondition check that verifies that the
    invalid_objects vtable is empty. The upgrade step itself consists of
    replacing all entries in system.descriptor with protobufs having
    undergone post-deserialization changes when applicable. Future work
    will extend these metadata health checks and repairs appropriately.
    
    Fixes #104418.
    
    Release note (general change): henceforth, all cluster version upgrades
    perform a catalog health check and repair upgrade step before proceeding
    to other upgrade steps.

---
+++ pkg/upgrade/upgrades/upgrades_test.go
@@ -0,0 +1,82 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package upgrades
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/upgrade/upgradebase"
+	"github.com/stretchr/testify/require"
+)
+
+// TestUniqueVersions tests that the registered upgrades have unique versions.
+func TestUniqueVersions(t *testing.T) {
+	versions := make(map[roachpb.Version]upgradebase.Upgrade)
+	for _, m := range upgrades {
+		_, found := versions[m.Version()]
+		require.Falsef(t, found, "duplicate version %s", m.Version())
+		versions[m.Version()] = m
+	}
+}
+
+// TestFirstUpgradesAfterPreExistingRelease checks that the first internal
+// version following each supported pre-existing release has an upgrade
+// registered for it, which is also in firstUpgradesAfterPreExistingReleases,
+// and vice-versa.
+func TestFirstUpgradesAfterPreExistingRelease(t *testing.T) {
+	// Compute the set of pre-existing releases supported by this binary.
+	// This excludes the latest release if the binary version is a release.
+	preExistingReleases := make(map[roachpb.Version]struct{})
+	minBinaryVersion := clusterversion.ByKey(clusterversion.BinaryMinSupportedVersionKey)
+	binaryVersion := clusterversion.ByKey(clusterversion.BinaryVersionKey)
+	for _, v := range clusterversion.ListBetween(minBinaryVersion, binaryVersion) {
+		preExistingReleases[roachpb.Version{Major: v.Major, Minor: v.Minor}] = struct{}{}
+	}
+	if binaryVersion.Internal == 0 {
+		delete(preExistingReleases, binaryVersion)
+	}
+
+	require.NotEmpty(t, preExistingReleases)
+	// Check that the first internal version after each pre-existing release has
+	// an upgrade registered for it, and that that upgrade is in the
+	// firstUpgradesAfterPreExistingReleases slice.
+	for r := range preExistingReleases {
+		v := roachpb.Version{Major: r.Major, Minor: r.Minor, Internal: 2}
+		m, found := registry[v]
+		require.True(t, found, "missing upgrade for %s in registry", v)
+		require.Contains(t, firstUpgradesAfterPreExistingReleases, m,
+			"missing upgrade for %s in slice", v)
+	}
+	// Check that for each registered upgrade for a non-primordial version with
+	// internal version 2 is two internal versions ahead of a supported pre-existing
+	// release, and that the upgrade is in the firstUpgradesAfterPreExistingReleases
+	// slice.
+	for v, m := range registry {
+		if v.Major == 0 || v.Internal != 2 {
+			continue
+		}
+		r := roachpb.Version{Major: v.Major, Minor: v.Minor}
+		_, found := preExistingReleases[r]
+		require.True(t, found,
+			"registered upgrade for %s but %s is not a supported pre-existing release",
+			v, r)
+		require.Contains(t, firstUpgradesAfterPreExistingReleases, m,
+			"missing upgrade for %s in slice", v)
+	}
+	// Check that the firstUpgradesAfterPreExistingReleases slice has no upgrades
+	// which aren't registered. This implies there are no duplicates either.
+	for _, m := range firstUpgradesAfterPreExistingReleases {
+		_, found := registry[m.Version()]
+		require.Truef(t, found, "expected upgrade %s to be registered, but isn't", m.Version())
+	}
+}
