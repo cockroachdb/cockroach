32eb14730edc2ae693412c01c751828a15e3a4a0
Raphael 'kena' Poss <knz@thaumogen.net>
2023-06-29 23:25:49 +0200

    kvtenant: define a cross-version metadata protocol test
    This commit introduces a new test to check that old-version tenant
    clients can operate with a new-version KV server, and vice-versa.
    
    Release note: None

--- pkg/kv/kvclient/kvtenant/setting_overrides.go
+++ pkg/kv/kvclient/kvtenant/setting_overrides.go
@@ -50,6 +50,11 @@ func (c *connector) runTenantSettingsSubscription(ctx context.Context, startupCh
 			c.settingsMu.receivedFirstAllTenantOverrides = false
 			c.settingsMu.receivedFirstSpecificOverrides = false
 		}()
+		func() {
+			c.metadataMu.Lock()
+			defer c.metadataMu.Unlock()
+			c.metadataMu.receivedFirstMetadata = false
+		}()
 
 		for {
 			e, err := stream.Recv()
@@ -79,6 +84,12 @@ func (c *connector) runTenantSettingsSubscription(ctx context.Context, startupCh
 				case <-time.After(1 * time.Second):
 				}
 				continue
+			}
+
+			if c.testingEmulateOldVersionSettingsClient {
+				// The old version of the settings client does not understand anything
+				// else than setting events.
+				e.EventType = kvpb.TenantSettingsEvent_SETTING_EVENT
 			}
 
 			var reconnect bool
@@ -133,6 +144,7 @@ func (c *connector) processMetadataEvent(ctx context.Context, e *kvpb.TenantSett
 	c.metadataMu.Lock()
 	defer c.metadataMu.Unlock()
 
+	c.metadataMu.receivedFirstMetadata = true
 	c.metadataMu.capabilities = e.Capabilities
 	c.metadataMu.tenantName = e.Name
 	// TODO(knz): Remove the cast once we have proper typing in the
