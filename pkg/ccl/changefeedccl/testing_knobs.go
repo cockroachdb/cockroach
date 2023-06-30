1e35bb7dd272f2b9c3cfb20be59582fca34f6584
Yevgeniy Miretskiy <yevgeniy@cockroachlabs.com>
2023-06-30 11:59:07 -0400

    changefeedccl: Return an error from FilterSpansWithMutation
    Modify FilterSpansWithMutation testing knob to return an error
    in addition to filtering decision.
    
    This is needed since using `require.XXX` or similar calls inside
    that knob, which runs in the context of changefeed processor, is not
    safe -- if the require check is false, this would cause
    changefeed to panic (and in general, it's not safe to panic
    on another thread -- only the test that owns *testing.T should do that).
    
    Informs #104399
    
    Release note: None

--- pkg/ccl/changefeedccl/testing_knobs.go
+++ pkg/ccl/changefeedccl/testing_knobs.go
@@ -40,7 +40,7 @@ type TestingKnobs struct {
 	PubsubClientSkipClientCreation bool
 	// FilterSpanWithMutation is a filter returning true if the resolved span event should
 	// be skipped. This method takes a pointer in case resolved spans need to be mutated.
-	FilterSpanWithMutation func(resolved *jobspb.ResolvedSpan) bool
+	FilterSpanWithMutation func(resolved *jobspb.ResolvedSpan) (bool, error)
 	// FeedKnobs are kvfeed testing knobs.
 	FeedKnobs kvfeed.TestingKnobs
 	// NullSinkIsExternalIOAccounted controls whether we record
