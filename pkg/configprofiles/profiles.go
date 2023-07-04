1038f2f658a1a1281653d6a51d09042e8fd1e0d7
Raphael 'kena' Poss <knz@thaumogen.net>
2023-07-04 19:23:08 +0200

    sql: session var `disable_drop_virtual_cluster`
    This replaces `disable_drop_tenant`, together with the cluster setting
    `sql.drop_virtual_cluster.enabled` (replaces
    `sql.drop_tenant.enabled`).
    
    We are OK with this breaking change since the feature was not yet
    exposed to end-users.
    
    Release note: None

--- pkg/configprofiles/profiles.go
+++ pkg/configprofiles/profiles.go
@@ -91,8 +91,8 @@ var multitenantClusterInitTasks = []autoconfigpb.Task{
 			// to range coalescing have not been solved yet).
 			"SET CLUSTER SETTING spanconfig.storage_coalesce_adjacent.enabled = false",
 			"SET CLUSTER SETTING spanconfig.tenant_coalesce_adjacent.enabled = false",
-			// Make the operator double-check tenant deletions.
-			"SET CLUSTER SETTING sql.drop_tenant.enabled = false",
+			// Make the operator double-check virtual cluster deletions.
+			"SET CLUSTER SETTING sql.drop_virtual_cluster.enabled = false",
 		},
 		nil, /* txnSQL */
 	),
