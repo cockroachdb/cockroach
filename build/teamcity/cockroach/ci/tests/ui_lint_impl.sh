4db51afa57a382e0ec3b3e90199ae3061245502f
Sean Barag <barag@cockroachlabs.com>
2023-07-05 08:47:11 -0700

    ui: replace yarn with pnpm and reintroduce workspace support
    Bazel's rules_js requires a pnpm-lock.yaml file --- the dependency
    lockfile format for the pnpm package manager [1] --- to be committed.
    While it offers to translate yarn.lock files at build-time, the presence
    of multiple package managers' lockfiles is slightly confusing and
    increases noise in commit logs, pull requests, etc. Unlike the older
    rules_nodejs, rules_js *does* support package-manager workspaces,
    allowing for a single 'pnpm install' outside of bazel to install all
    dependencies in pkg/ui/ (like we did pre-rules_nodejs) and deduplicated
    dependency downloads across JS packages. Remove yarn and replace it with
    pnpm, since that's ultimately what rules_js mimicks.
    
    For posterity, this is the current timeline of JS package management
    in CRDB:
    
    * Jun 2015 [2]: npm added for JS package management
    * Oct 2016 [3]: npm replaced with yarn
    * Apr 2021 [4]: Bazel's rules_nodejs 3.7 configured
    * Jun 2021 [5]: yarn workspaces support introduced
    * Sep 2021 [6]: Bazel's rules_nodejs upgraded to 4.1
    * May 2022 [7]: Bazel's rules_nodejs upgraded to 4.5 and yarn workspaces
                    support removed
    * May 2023 [8]: Bazel's rules_nodejs replaced with rules_js
    * Jul 2023 [9]: yarn replaced with pnpm + pnpm workspaces
    
    [1] https://pnpm.io
    [2] 4379fc1f671 (Add node package management to the ui., 2015-06-29)
    [3] 8071dc50d91 (*: switch from npm to yarn, 2016-10-21)
    [4] 6cc3af3a42c (ui: bazel build for UI, 2021-04-22)
    [5] b8323204a3e (ui: use yarn workspaces, 2021-06-28)
    [6] c4594b82fa1 (update build_bazel_rules_nodejs build rules to version 4.1.0, 2021-09-16)
    [7] 5b6c271b1cd (bazel: upgrade to rules_nodejs 5.4.2, 2022-05-17)
    [8] 3d95f5fa79d (build,bazel: upgrade to `rules_js`, 2023-05-18)
    [9] This commit
    
    Release note: None
    Epic: None

--- build/teamcity/cockroach/ci/tests/ui_lint_impl.sh
+++ build/teamcity/cockroach/ci/tests/ui_lint_impl.sh
@@ -3,4 +3,9 @@
 set -xeuo pipefail
 
 bazel build //pkg/cmd/bazci --config=ci
+# Any missing dependencies will cause all of //pkg/ui:lint to fail during the
+# dependency-download phase, leading to cryptic failures.
+# Check for unmirrored dependencies before linting the UI to provide a clear
+# error message.
+$(bazel info bazel-bin --config=ci)/pkg/cmd/bazci/bazci_/bazci -- test --config=ci //pkg/cmd/mirror/npm:list_unmirrored_dependencies
 $(bazel info bazel-bin --config=ci)/pkg/cmd/bazci/bazci_/bazci -- test --config=ci //pkg/ui:lint
--- build/teamcity/cockroach/ci/tests/ui_test_impl.sh
+++ build/teamcity/cockroach/ci/tests/ui_test_impl.sh
@@ -3,4 +3,9 @@
 set -xeuo pipefail
 
 bazel build //pkg/cmd/bazci --config=ci
-$(bazel info bazel-bin --config=ci)/pkg/cmd/bazci/bazci_/bazci -- test --config=ci //pkg/ui/workspaces/db-console:jest //pkg/ui/workspaces/cluster-ui:jest
+# Any missing dependencies will cause all of //pkg/ui:test to fail during the
+# dependency-download phase, leading to cryptic failures.
+# Check for unmirrored dependencies before testing the UI to provide a clear
+# error message.
+$(bazel info bazel-bin --config=ci)/pkg/cmd/bazci/bazci_/bazci -- test --config=ci //pkg/cmd/mirror/npm:list_unmirrored_dependencies
+$(bazel info bazel-bin --config=ci)/pkg/cmd/bazci/bazci_/bazci -- test --config=ci //pkg/ui:test
