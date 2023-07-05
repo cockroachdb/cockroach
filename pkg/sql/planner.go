eaa7742048446a881013f18f23744816441dc049
Evan Wall <wall@cockroachlabs.com>
2023-07-05 14:01:37 -0400

    sql: fix CREATE AS sourcing pg_catalog.pg_cursors panic
    Fixes #106167
    
    Fixes a bug discovered in #105895 that caused a panic when running these
    statements because planner.sqlCursors was nil:
    CREATE TABLE t AS SELECT * FROM pg_catalog.pg_cursors;
    CREATE MATERIALIZED VIEW v AS SELECT * FROM pg_catalog.pg_cursors;
    
    Release note (bug fix): Fix panic executing CREATE AS with
    pg_catalog.cursors source. For example:
    CREATE TABLE t AS SELECT * FROM pg_catalog.pg_cursors;
    CREATE MATERIALIZED VIEW v AS SELECT * FROM pg_catalog.pg_cursors;

--- pkg/sql/planner.go
+++ pkg/sql/planner.go
@@ -457,6 +457,7 @@ func newInternalPlanner(
 
 	p.queryCacheSession.Init()
 	p.optPlanningCtx.init(p)
+	p.sqlCursors = emptySqlCursors{}
 	p.preparedStatements = emptyPreparedStatements{}
 	p.createdSequences = emptyCreatedSequences{}
 
