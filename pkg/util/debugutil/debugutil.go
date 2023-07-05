e40ff408fce1777142802d463efe2ff2e53e7470
Marius Posta <marius@cockroachlabs.com>
2023-07-05 09:25:17 -0400

    debugutil,scpb: add debugutil package, adopt in scpb
    This commit adds a debugutil package which exposes a flag which is set
    when the cockroach process is spawned by the delve debugger.
    
    This functionality is used by the scpb package to decorate data
    structures with useful but expensive debugging information on an
    as-needed basis.
    
    Release note: None

---
+++ pkg/util/debugutil/debugutil.go
@@ -0,0 +1,43 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package debugutil
+
+import (
+	"os"
+	"sync/atomic"
+
+	ps "github.com/mitchellh/go-ps"
+)
+
+// IsLaunchedByDebugger returns true in cases where the delve debugger
+// was used to launch the cockroach process.
+func IsLaunchedByDebugger() bool {
+	return isLaunchedByDebugger.Load()
+}
+
+var isLaunchedByDebugger atomic.Bool
+
+func init() {
+	isLaunchedByDebugger.Store(func(maybeDelvePID int) bool {
+		// We loop in case there were intermediary processes like the gopls language server.
+		for maybeDelvePID != 0 {
+			switch p, err := ps.FindProcess(maybeDelvePID); {
+			case err != nil:
+				return false
+			case p.Executable() == "dlv":
+				return true
+			default:
+				maybeDelvePID = p.PPid()
+			}
+		}
+		return false
+	}(os.Getppid()))
+}
