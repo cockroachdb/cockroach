18ac50e7fc8f3bbe3bbca24ee39d259d3d8e1539
Evan Wall <wall@cockroachlabs.com>
2023-07-05 14:52:00 -0400

    sql: remove TestCreateAsVTable brokenTables
    Informs #105895
    
    Remove the broken tables map now that it is empty.
    
    Release note: None

--- pkg/sql/create_as_test.go
+++ pkg/sql/create_as_test.go
@@ -31,10 +31,6 @@ import (
 func TestCreateAsVTable(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 
-	// These are the vtables that need to be fixed.
-	// The map should be empty if all vtables are supported.
-	brokenTables := map[string]struct{}{}
-
 	ctx := context.Background()
 	testCluster := serverutils.StartNewTestCluster(t, 1, base.TestClusterArgs{})
 	defer testCluster.Stopper().Stop(ctx)
@@ -78,10 +74,6 @@ func TestCreateAsVTable(t *testing.T) {
 			}
 
 			fqName := name.FQString()
-			if _, ok := brokenTables[fqName]; ok {
-				continue
-			}
-
 			// Filter by trace_id to prevent error when selecting from
 			// crdb_internal.cluster_inflight_traces:
 			// "pq: a trace_id value needs to be specified".
