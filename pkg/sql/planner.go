3abe8a6fd8165079ae861b6b80a9e310d6c62363
Evan Wall <wall@cockroachlabs.com>
2023-07-05 13:34:49 -0400

    sql: fix CREATE AS sourcing pg_catalog.pg_prepared_statements panic
    Fixes #106166
    
    Fixes a bug discovered in #105895 that caused a panic when running these
    statements because planner.preparedStatements was nil:
    CREATE TABLE t AS SELECT * FROM pg_catalog.pg_prepared_statements;
    CREATE MATERIALIZED VIEW v AS SELECT * FROM pg_catalog.pg_prepared_statements;
    
    Release note (bug fix): Fix panic executing CREATE AS with
    pg_catalog.pg_prepared_statements source. For example:
    CREATE TABLE t AS SELECT * FROM pg_catalog.pg_prepared_statements;
    CREATE MATERIALIZED VIEW v AS SELECT * FROM pg_catalog.pg_prepared_statements;

--- pkg/sql/planner.go
+++ pkg/sql/planner.go
@@ -425,6 +425,7 @@ func newInternalPlanner(
 
 	p.queryCacheSession.Init()
 	p.optPlanningCtx.init(p)
+	p.preparedStatements = emptyPreparedStatements{}
 	p.createdSequences = emptyCreatedSequences{}
 
 	p.schemaResolver.descCollection = p.Descriptors()
