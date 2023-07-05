bb49030f34c4173479b3006eb7e93a08c5fc64e0
Erik Grinaker <grinaker@cockroachlabs.com>
2023-07-05 20:06:06 +0000

    allocator/plan: place leases violating preferences in purgatory
    This patch places replicas in the replicate queue purgatory when
    it has a lease violating the lease preferences and it's unable to find a
    suitable target. This causes the replica to be retried more often.
    
    This will only trigger when replicas are eagerly enqueued (typically
    when we acquire a new lease that violates preferences), since we
    otherwise don't attempt to enqueue replicas when they don't have a valid
    lease transfer target.
    
    Epic: none
    Release note: None

--- pkg/kv/kvserver/allocator/plan/replicate.go
+++ pkg/kv/kvserver/allocator/plan/replicate.go
@@ -868,7 +868,7 @@ func (rp ReplicaPlanner) considerRebalance(
 		if !canTransferLeaseFrom(ctx, repl) {
 			return nil, stats, nil
 		}
-		return rp.shedLeaseTarget(
+		op, err := rp.shedLeaseTarget(
 			ctx,
 			repl,
 			desc,
@@ -878,8 +878,11 @@ func (rp ReplicaPlanner) considerRebalance(
 				ExcludeLeaseRepl:       false,
 				CheckCandidateFullness: true,
 			},
-		), stats, nil
-
+		)
+		if err != nil {
+			return nil, stats, err
+		}
+		return op, stats, nil
 	}
 
 	// If we have a valid rebalance action (ok == true) and we haven't
@@ -925,7 +928,7 @@ func (rp ReplicaPlanner) shedLeaseTarget(
 	desc *roachpb.RangeDescriptor,
 	conf roachpb.SpanConfig,
 	opts allocator.TransferLeaseOptions,
-) (op AllocationOp) {
+) (op AllocationOp, _ error) {
 	usage := repl.RangeUsageInfo()
 	// Learner replicas aren't allowed to become the leaseholder or raft leader,
 	// so only consider the `VoterDescriptors` replicas.
@@ -940,7 +943,16 @@ func (rp ReplicaPlanner) shedLeaseTarget(
 		opts,
 	)
 	if target == (roachpb.ReplicaDescriptor{}) {
-		return nil
+		// If we don't find a suitable target, but we own a lease violating the
+		// lease preferences, return an error to place the replica in purgatory and
+		// retry sooner. This typically happens when we've just acquired a violating
+		// lease and we eagerly enqueue the replica before we've received Raft
+		// leadership, which prevents us from finding appropriate lease targets
+		// since we can't determine if any are behind.
+		if repl.LeaseViolatesPreferences(ctx) {
+			return nil, CantTransferLeaseViolatingPreferencesError{RangeID: desc.RangeID}
+		}
+		return nil, nil
 	}
 
 	op = AllocationTransferLeaseOp{
@@ -949,7 +961,7 @@ func (rp ReplicaPlanner) shedLeaseTarget(
 		Usage:              usage,
 		bypassSafetyChecks: false,
 	}
-	return op
+	return op, nil
 }
 
 // maybeTransferLeaseAwayTarget is called whenever a replica on a given store
@@ -1015,3 +1027,26 @@ func (rp ReplicaPlanner) maybeTransferLeaseAwayTarget(
 
 	return op, nil
 }
+
+// CantTransferLeaseViolatingPreferencesError is an error returned when a lease
+// violates the lease preferences, but we couldn't find a valid target to
+// transfer the lease to. It indicates that the replica should be sent to
+// purgatory, to retry the transfer faster.
+type CantTransferLeaseViolatingPreferencesError struct {
+	RangeID roachpb.RangeID
+}
+
+var _ errors.SafeFormatter = CantTransferLeaseViolatingPreferencesError{}
+
+func (e CantTransferLeaseViolatingPreferencesError) Error() string { return fmt.Sprint(e) }
+
+func (e CantTransferLeaseViolatingPreferencesError) Format(s fmt.State, verb rune) {
+	errors.FormatError(e, s, verb)
+}
+
+func (e CantTransferLeaseViolatingPreferencesError) SafeFormatError(p errors.Printer) (next error) {
+	p.Printf("can't transfer r%d lease violating preferences, no suitable target", e.RangeID)
+	return nil
+}
+
+func (CantTransferLeaseViolatingPreferencesError) PurgatoryErrorMarker() {}
