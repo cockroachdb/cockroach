44ff00a83dbc654b097b97645429a1a5bf7113fa
Michael Butler <butler@cockroachlabs.com>
2023-07-07 17:28:50 -0400

    c2c: skip TestStreamingAutoReplan
    This patch also switches AutoReplanning default off.
    
    Informs #106451
    
    Release note: None

--- pkg/cmd/roachtest/tests/cluster_to_cluster.go
+++ pkg/cmd/roachtest/tests/cluster_to_cluster.go
@@ -1361,7 +1361,8 @@ func srcClusterSettings(t test.Test, db *sqlutils.SQLRunner) {
 
 func destClusterSettings(t test.Test, db *sqlutils.SQLRunner, additionalDuration time.Duration) {
 	db.ExecMultiple(t, `SET CLUSTER SETTING cross_cluster_replication.enabled = true;`,
-		`SET CLUSTER SETTING kv.rangefeed.enabled = true;`)
+		`SET CLUSTER SETTING kv.rangefeed.enabled = true;`,
+		`SET CLUSTER SETTING stream_replication.replan_flow_threshold = 0.1;`)
 
 	if additionalDuration != 0 {
 		replanFrequency := additionalDuration / 2
