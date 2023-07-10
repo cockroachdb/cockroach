33f5ea05bfbd023d73180964edf0c414d8732c47
Steven Danna <danna@cockroachlabs.com>
2023-07-10 11:55:16 +0100

    kv,sql: testing knob to randomly inject retryable errors
    Occasionally, we see bugs caused by mismangement of state inside a
    function that will be retried. Namely, the functions `(*kv.DB).Txn`
    and `(*sql.InternalExecutor).Txn` take funcs that must be suitably
    idempotent.
    
    This is testing knob provides cheap way to help us hunt down such
    bugs.
    
    In the long run, it would be nice to turn this on by default, but we
    should probably make sure TestServer runs reliably with this turned on
    before flipping the default.
    
    I also considered retrying _every_ transaction once rather than
    relying on randomness. However, that would require adding more state
    in kv.Txn, which I wasn't sure I really wanted to do for testing
    purposes.
    
    Informs #106417
    
    Epic: none
    
    Release note: None

--- pkg/kv/txn.go
+++ pkg/kv/txn.go
@@ -27,6 +27,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
@@ -1428,6 +1429,27 @@ func (txn *Txn) GenerateForcedRetryableErr(ctx context.Context, msg redact.Redac
 	defer txn.mu.Unlock()
 	now := txn.db.clock.NowAsClockTimestamp()
 	return txn.mu.sender.GenerateForcedRetryableErr(ctx, now.ToTimestamp(), msg)
+}
+
+var (
+	randRetryRngSource, _     = randutil.NewPseudoRand()
+	randomTxnRetryProbability = 0.1
+)
+
+// TestingShouldReturnRandomRetry returns true if we should generate a
+// random, retriable error for this transaction.
+func (txn *Txn) TestingShouldReturnRandomRetry() bool {
+	txn.mu.Lock()
+	defer txn.mu.Unlock()
+
+	if !txn.mu.sender.TestingRandomRetryableErrorsEnabled() {
+		return false
+	}
+
+	if txn.mu.sender.ClientFinalized() {
+		return false
+	}
+	return randRetryRngSource.Float64() < randomTxnRetryProbability
 }
 
 // IsSerializablePushAndRefreshNotPossible returns true if the transaction is
