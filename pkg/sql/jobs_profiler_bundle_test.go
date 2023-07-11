62038ac447b34c791697d539c9d835bbe977b0b6
adityamaru <adityamaru@gmail.com>
2023-07-11 17:32:54 -0400

    sql,server: add endpoint to list a job's execution details
    In https://github.com/cockroachdb/cockroach/pull/105384
    we added infrastructure to request and store execution details
    for a job. This currently only includes the DistSQL diagram
    generated during a job execution. Going forward this will
    include several files such as traces, goroutines, profiles etc.
    
    This change introduces an endpoint that allows listing all such
    files that are available for consumption. This list will be displayed
    on the job details page allowing the user to download any subset of
    the files collected during job execution.
    
    Informs: #105076
    Release note: None

--- pkg/sql/jobs_profiler_bundle_test.go
+++ pkg/sql/jobs_profiler_bundle_test.go
@@ -17,6 +17,7 @@ import (
 	"io"
 	"net/http"
 	"runtime/pprof"
+	"sort"
 	"strings"
 	"testing"
 	"time"
@@ -72,7 +73,7 @@ func TestReadWriteProfilerExecutionDetails(t *testing.T) {
 					infra := physicalplan.NewPhysicalInfrastructure(uuid.FastMakeV4(), base.SQLInstanceID(1))
 					p.PhysicalInfrastructure = infra
 					jobsprofiler.StorePlanDiagram(ctx, s.Stopper(), &p, s.InternalDB().(isql.DB), j.ID())
-					checkForPlanDiagram(ctx, t, s.InternalDB().(isql.DB), j.ID())
+					checkForPlanDiagrams(ctx, t, s.InternalDB().(isql.DB), j.ID(), 1)
 					return nil
 				},
 			}
@@ -113,6 +114,100 @@ func TestReadWriteProfilerExecutionDetails(t *testing.T) {
 		require.True(t, strings.Contains(string(goroutines), fmt.Sprintf("labels: {\"foo\":\"bar\", \"job\":\"IMPORT id=%d\", \"n\":\"1\"}", importJobID)))
 		require.True(t, strings.Contains(string(goroutines), "github.com/cockroachdb/cockroach/pkg/sql_test.fakeExecResumer.Resume"))
 	})
+}
+
+func TestListProfilerExecutionDetails(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	// Timeout the test in a few minutes if it hasn't succeeded.
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Minute*2)
+	defer cancel()
+
+	params, _ := tests.CreateTestServerParams()
+	params.Knobs.JobsTestingKnobs = jobs.NewTestingKnobsWithShortIntervals()
+	defer jobs.ResetConstructors()()
+	s, sqlDB, _ := serverutils.StartServer(t, params)
+	defer s.Stopper().Stop(ctx)
+
+	runner := sqlutils.MakeSQLRunner(sqlDB)
+	execCfg := s.ExecutorConfig().(sql.ExecutorConfig)
+
+	expectedDiagrams := 1
+	jobs.RegisterConstructor(jobspb.TypeImport, func(j *jobs.Job, _ *cluster.Settings) jobs.Resumer {
+		return fakeExecResumer{
+			OnResume: func(ctx context.Context) error {
+				p := sql.PhysicalPlan{}
+				infra := physicalplan.NewPhysicalInfrastructure(uuid.FastMakeV4(), base.SQLInstanceID(1))
+				p.PhysicalInfrastructure = infra
+				jobsprofiler.StorePlanDiagram(ctx, s.Stopper(), &p, s.InternalDB().(isql.DB), j.ID())
+				checkForPlanDiagrams(ctx, t, s.InternalDB().(isql.DB), j.ID(), expectedDiagrams)
+				if err := execCfg.JobRegistry.CheckPausepoint("fakeresumer.pause"); err != nil {
+					return err
+				}
+				return nil
+			},
+		}
+	}, jobs.UsesTenantCostControl)
+
+	runner.Exec(t, `CREATE TABLE t (id INT)`)
+	runner.Exec(t, `INSERT INTO t SELECT generate_series(1, 100)`)
+
+	t.Run("list execution detail files", func(t *testing.T) {
+		runner.Exec(t, `SET CLUSTER SETTING jobs.debug.pausepoints = 'fakeresumer.pause'`)
+		var importJobID int
+		runner.QueryRow(t, `IMPORT INTO t CSV DATA ('nodelocal://1/foo') WITH DETACHED`).Scan(&importJobID)
+		jobutils.WaitForJobToPause(t, runner, jobspb.JobID(importJobID))
+
+		runner.Exec(t, `SELECT crdb_internal.request_job_execution_details($1)`, importJobID)
+		files := listExecutionDetails(t, s, jobspb.JobID(importJobID))
+		require.Len(t, files, 2)
+		require.Regexp(t, "distsql\\..*\\.html", files[0])
+		require.Regexp(t, "goroutines\\..*\\.txt", files[1])
+
+		// Resume the job, so it can write another DistSQL diagram and goroutine
+		// snapshot.
+		runner.Exec(t, `SET CLUSTER SETTING jobs.debug.pausepoints = ''`)
+		expectedDiagrams = 2
+		runner.Exec(t, `RESUME JOB $1`, importJobID)
+		jobutils.WaitForJobToSucceed(t, runner, jobspb.JobID(importJobID))
+		runner.Exec(t, `SELECT crdb_internal.request_job_execution_details($1)`, importJobID)
+		files = listExecutionDetails(t, s, jobspb.JobID(importJobID))
+		require.Len(t, files, 4)
+		require.Regexp(t, "distsql\\..*\\.html", files[0])
+		require.Regexp(t, "distsql\\..*\\.html", files[1])
+		require.Regexp(t, "goroutines\\..*\\.txt", files[2])
+		require.Regexp(t, "goroutines\\..*\\.txt", files[3])
+	})
+}
+
+func listExecutionDetails(
+	t *testing.T, s serverutils.TestServerInterface, jobID jobspb.JobID,
+) []string {
+	t.Helper()
+
+	client, err := s.GetAdminHTTPClient()
+	require.NoError(t, err)
+
+	url := s.AdminURL().String() + fmt.Sprintf("/_status/list_job_profiler_execution_details/%d", jobID)
+	req, err := http.NewRequest("GET", url, nil)
+	require.NoError(t, err)
+
+	req.Header.Set("Content-Type", httputil.ProtoContentType)
+	resp, err := client.Do(req)
+	require.NoError(t, err)
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+
+	edResp := serverpb.ListJobProfilerExecutionDetailsResponse{}
+	require.NoError(t, protoutil.Unmarshal(body, &edResp))
+	sort.Slice(edResp.Files, func(i, j int) bool {
+		return edResp.Files[i] < edResp.Files[j]
+	})
+	return edResp.Files
 }
 
 func checkExecutionDetails(
