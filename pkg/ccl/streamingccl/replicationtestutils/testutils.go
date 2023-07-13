dafc9b606a72001fbc1b1cb940a84ea66967d093
Michael Butler <butler@cockroachlabs.com>
2023-07-13 14:23:08 -0400

    pass

--- pkg/ccl/streamingccl/replicationtestutils/testutils.go
+++ pkg/ccl/streamingccl/replicationtestutils/testutils.go
@@ -428,7 +428,11 @@ func CreateScatteredTable(t *testing.T, c *TenantStreamingClusters, numNodes int
 	c.SrcTenantSQL.Exec(t, "ALTER TABLE d.scattered SPLIT AT (SELECT * FROM generate_series($1::INT, $2::INT, $3::INT))",
 		rowsPerRange, (numRanges-1)*rowsPerRange, rowsPerRange)
 	c.SrcTenantSQL.Exec(t, "ALTER TABLE d.scattered SCATTER")
-	time.Sleep(5 * time.Second)
+	var leaseHolderCount int
+	c.SrcTenantSQL.QueryRow(t,
+		`SELECT count(DISTINCT lease_holder) FROM [SHOW RANGES FROM DATABASE d WITH DETAILS]`).
+		Scan(&leaseHolderCount)
+	require.Greater(t, leaseHolderCount, 0)
 }
 
 var defaultSrcClusterSetting = map[string]string{
