05d06e5c4cfdf79766b9ea32ec6514c313438847
Faizan Qazi <faizan@cockroachlabs.com>
2023-07-14 15:03:30 -0400

    sql/schemachanger: block dropping indexes that are the only source of a column
    Previously, because of a bug we could end up with secondary indexes
    being the only thing that stored a column. This could lead to data
    loss if that secondary index was dropped. To address this, this patch
    will block dropping such indexes with a link to the technical advisory.
    
    Informs: #106739
    
    Release note (bug fix): Block dropping of indexes impacted by
    technical advisory 99561 if data loss could occur.

--- pkg/sql/schemachanger/scbuild/internal/scbuildstmt/drop_index.go
+++ pkg/sql/schemachanger/scbuild/internal/scbuildstmt/drop_index.go
@@ -116,6 +116,43 @@ func maybeDropIndex(
 	return sie
 }
 
+// panicIfColumnsAreNotStoredElseWhere detects if dropping an index will lose a stored
+// column. Previously, we had an issue where new columns were accidentally
+// added into secondary indexes. This could lead to wrong results and data
+// loss, so for safety we are going to block dropping these secondary indexes.
+func panicIfColumnsAreNotStoredElseWhere(b BuildCtx, sie *scpb.SecondaryIndex) {
+	tblElts := b.QueryByID(sie.TableID)
+	secondaryIdxElts := tblElts.Filter(hasIndexIDAttrFilter(sie.IndexID))
+	_, _, indexName := scpb.FindIndexName(secondaryIdxElts)
+	_, _, pie := scpb.FindPrimaryIndex(b.QueryByID(sie.TableID))
+	primaryIdxElts := tblElts.Filter(hasIndexIDAttrFilter(pie.IndexID))
+
+	foundColumns := make(map[catid.ColumnID]struct{})
+	primaryIdxElts.ForEachElementStatus(func(current scpb.Status, target scpb.TargetStatus, e scpb.Element) {
+		idxColumn, ok := (e).(*scpb.IndexColumn)
+		if !ok {
+			return
+		}
+		foundColumns[idxColumn.ColumnID] = struct{}{}
+	})
+	secondaryIdxElts.ForEachElementStatus(func(current scpb.Status, target scpb.TargetStatus, e scpb.Element) {
+		idxColumn, ok := (e).(*scpb.IndexColumn)
+		if !ok {
+			return
+		}
+		columnElts := tblElts.Filter(hasColumnIDAttrFilter(idxColumn.ColumnID))
+		// Skip columns that are virtual.
+		_, _, columnType := scpb.FindColumnType(columnElts)
+		if columnType.IsVirtual {
+			return
+		}
+		// Check if the column is stored in primary index.
+		if _, found := foundColumns[idxColumn.ColumnID]; !found {
+			panic(sqlerrors.NewSecondaryIndexDataLossError(indexName.Name))
+		}
+	})
+}
+
 // dropSecondaryIndex is a helper to drop a secondary index which may be used
 // both in DROP INDEX and as a cascade from another operation.
 func dropSecondaryIndex(
@@ -125,6 +162,9 @@ func dropSecondaryIndex(
 	sie *scpb.SecondaryIndex,
 ) {
 	{
+		// Sanity: We had a pretty severe bug with a data loss risk, prevent
+		// dropping of any indexes that are the sole source for given data.
+		panicIfColumnsAreNotStoredElseWhere(b, sie)
 		next := b.WithNewSourceElementID()
 		// Maybe drop dependent views.
 		// If CASCADE and there are "dependent" views (i.e. views that use this
