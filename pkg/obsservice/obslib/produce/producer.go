1d831aed2f47b4e5308e1341be8d35835cecc595
Alex Barganier <abarganier@cockroachlabs.com>
2023-07-18 16:12:55 -0400

    obsservice: ability to produce events into event queues
    Once an event in the obsservice has been routed, it is ready
    to be transformed into its event-specific type, validated, and
    then produced into an event queue to await aggregation.
    
    While the individual implementations for how to transform,
    validate, and enqueue events varies across event types and
    event queue implementations, the common group of operations
    can be generalized into a common ProducerGroup implementation,
    with the transform, validate, and produce logic injected at
    initialization.
    
    This patch defines the necessary interfaces for this, as well
    as the common ProducerGroup logic.
    
    Release note: none

---
+++ pkg/obsservice/obslib/produce/producer.go
@@ -0,0 +1,101 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+package produce
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cockroachdb/cockroach/pkg/obsservice/obslib"
+	"github.com/cockroachdb/cockroach/pkg/obsservice/obslib/transform"
+	"github.com/cockroachdb/cockroach/pkg/obsservice/obslib/validate"
+	"github.com/cockroachdb/cockroach/pkg/obsservice/obspb"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/errors"
+)
+
+// EventProducer defines how to produce an event into an event-specific
+// queue to await aggregation by the obsservice.
+type EventProducer interface {
+	Produce(interface{}) error
+}
+
+// ProducerGroup is a modular, event-type-specific group of steps
+// designed to take a routed obspb.Event through the necessary procedure
+// to transform it into its event-specific type, validate that event,
+// and enqueue it to await aggregation by the obsservice. It can be
+// provided with a Transformer, multiple Validators (called sequentially),
+// and multiple Producers (called sequentially) at initialization.
+type ProducerGroup struct {
+	alias       string
+	transformer transform.EventTransformer
+	validators  []validate.Validator
+	producers   []EventProducer
+}
+
+var _ obslib.EventConsumer = (*ProducerGroup)(nil)
+
+func NewProducerGroup(
+	alias string,
+	transformer transform.EventTransformer,
+	validators []validate.Validator,
+	producers []EventProducer,
+) (*ProducerGroup, error) {
+	if transformer == nil {
+		return nil, errors.New("nil transformer provided")
+	}
+	if len(validators) == 0 {
+		return nil, errors.New("must provide at least one Validator")
+	}
+	if len(producers) == 0 {
+		return nil, errors.New("must provide at least one EventProducer")
+	}
+	return &ProducerGroup{
+		alias:       fmt.Sprintf("%s-producer", alias),
+		transformer: transformer,
+		validators:  validators,
+		producers:   producers,
+	}, nil
+}
+
+// TODO(abarganier): histogram for ProducerGroup latency.
+func (p *ProducerGroup) Consume(ctx context.Context, event *obspb.Event) error {
+	transformed, err := p.transformer.Transform(event)
+	if err != nil {
+		// TODO(abarganier): Counter metrics for transformation errors.
+		log.Errorf(ctx, "%s failed to transform event %v, err = %v",
+			p.alias, event, err)
+		return errors.Wrap(err, "transforming event")
+	}
+	var validationErrors error
+	for _, validator := range p.validators {
+		if err := validator.Validate(transformed); err != nil {
+			// TODO(abarganier): Counter metrics for validation errors.
+			validationErrors = errors.CombineErrors(err, validationErrors)
+		}
+	}
+	if validationErrors != nil {
+		log.Errorf(ctx, "%s validation failed for event %v, err = %v",
+			p.alias, transformed, err)
+		return validationErrors
+	}
+	var produceErrors error
+	for _, producer := range p.producers {
+		if err := producer.Produce(transformed); err != nil {
+			// TODO(abarganier): Counter metrics for producer errors.
+			produceErrors = errors.CombineErrors(err, produceErrors)
+		}
+	}
+	if produceErrors != nil {
+		log.Errorf(ctx, "%s failed to produce event %v, err = %v",
+			p.alias, transformed, err)
+		return produceErrors
+	}
+	return nil
+}
