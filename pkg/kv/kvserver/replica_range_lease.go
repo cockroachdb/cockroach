8ef4e36a2f12dde227624c9717376fe7785dea74
Austen McClernon <austen@cockroachlabs.com>
2023-07-21 20:16:14 +0000

    kvserver: add lease preference metrics
    There were no existing metrics to monitor the lease preference
    conformance. This commit adds two metric gauges:
    `leases.preferences.violating` and `leases.preferences.less-preferred`.
    These metrics are reported by the store.
    
    `leases.preferences.violating` indicates the number of valid leases a
    store owns, which satisfy none of the preferences applied.
    
    `leases.preferences.less-preferred` indicates the number of valid leases
    a store owns, which satisfy some of the preferences applied, but not the
    first one.
    
    For example, with a lease preference `'[[+zone=a],[+zone=b]]'`, the
    metric values with different leaseholders are:
    
    ```
    leaseholder_locality="zone=c"
    leases.preferences.less-preferred: 0
    leases.preferences.violating: 1
    ```
    
    ```
    leaseholder_locality="zone=b"
    leases.preferences.less-preferred: 1
    leases.preferences.violating: 0
    ```
    
    ```
    leaseholder_locality="zone=a"
    leases.preferences.less-preferred: 0
    leases.preferences.violating: 0
    ```
    
    When no preferences are applied, the lease is not counted in either
    metric.
    
    Epic: none
    
    Informs: #106100
    
    Release note (ops change): Introduce two new metrics to monitor lease
    range preference conformance. `leases.preferences.violating` indicates
    the number of valid leases a store owns, which satisfy none of the
    preferences applied. `leases.preferences.less-preferred` indicates the
    number of valid leases a store owns, which satisfy some of the
    preferences applied, but not the first one.

--- pkg/kv/kvserver/replica_range_lease.go
+++ pkg/kv/kvserver/replica_range_lease.go
@@ -1536,25 +1536,65 @@ func (r *Replica) hasCorrectLeaseTypeRLocked(lease roachpb.Lease) bool {
 	return hasExpirationLease == r.shouldUseExpirationLeaseRLocked()
 }
 
-// LeaseViolatesPreferences checks if current replica owns the lease and if it
-// violates the lease preferences defined in the span config. If there is an
-// error or no preferences defined then it will return false and consider that
-// to be in-conformance.
+// leasePreferencesStatus represents the state of satisfying lease preferences.
+type leasePreferencesStatus int
+
+const (
+	// leasePreferencesViolating indicates the leaseholder does not
+	// satisfy any lease preference applied.
+	leasePreferencesViolating leasePreferencesStatus = iota
+	// leasePreferencesLessPreferred indicates the leaseholder satisfies _some_
+	// preference, however not the most preferred.
+	leasePreferencesLessPreferred
+	// leasePreferencesOK indicates the lease satisfies the first
+	// preference, or no lease preferences are applied.
+	leasePreferencesOK
+)
+
+// LeaseViolatesPreferences checks if this replica owns the lease and if it
+// violates the lease preferences defined in the span config. If no preferences
+// are defined then it will return false and consider it to be in conformance.
 func (r *Replica) LeaseViolatesPreferences(ctx context.Context) bool {
-	storeDesc, err := r.store.Descriptor(ctx, true /* useCached */)
-	if err != nil {
-		log.Infof(ctx, "Unable to load the descriptor %v: cannot check if lease violates preference", err)
+	storeID := r.store.StoreID()
+	now := r.Clock().NowAsClockTimestamp()
+	r.mu.RLock()
+	leaseStatus := r.leaseStatusAtRLocked(ctx, now)
+	preferences := r.mu.conf.LeasePreferences
+	r.mu.RUnlock()
+
+	if !leaseStatus.IsValid() || !leaseStatus.Lease.OwnedBy(storeID) {
 		return false
 	}
-	conf := r.SpanConfig()
-	if len(conf.LeasePreferences) == 0 {
-		return false
+
+	storeAttrs := r.store.Attrs()
+	nodeAttrs := r.store.nodeDesc.Attrs
+	nodeLocality := r.store.nodeDesc.Locality
+	preferenceStatus := makeLeasePreferenceStatus(
+		leaseStatus, storeID, storeAttrs, nodeAttrs, nodeLocality, preferences)
+
+	return preferenceStatus == leasePreferencesViolating
+}
+
+func makeLeasePreferenceStatus(
+	leaseStatus kvserverpb.LeaseStatus,
+	storeID roachpb.StoreID,
+	storeAttrs, nodeAttrs roachpb.Attributes,
+	nodeLocality roachpb.Locality,
+	preferences []roachpb.LeasePreference,
+) leasePreferencesStatus {
+	if !leaseStatus.IsValid() || !leaseStatus.Lease.OwnedBy(storeID) {
+		return leasePreferencesOK
 	}
-	for _, preference := range conf.LeasePreferences {
-		if constraint.CheckStoreConjunction(*storeDesc, preference.Constraints) {
-			return false
+	if len(preferences) == 0 {
+		return leasePreferencesOK
+	}
+	for i, preference := range preferences {
+		if constraint.CheckConjunction(storeAttrs, nodeAttrs, nodeLocality, preference.Constraints) {
+			if i > 0 {
+				return leasePreferencesLessPreferred
+			}
+			return leasePreferencesOK
 		}
 	}
-	// We have at lease one preference set up, but we don't satisfy any.
-	return true
+	return leasePreferencesViolating
 }
