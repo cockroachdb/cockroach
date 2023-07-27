c75554983cca891e05a44bcac5ce2018f5504690
Oleg Afanasyev <oleg@cockroachlabs.com>
2023-07-27 18:05:55 +0100

    rangefeed: add transaction pusher to scheduler rangefeed
    This commit adds slow transaction pusher to scheduler based rangefeed.
    Store will trigger bulk enqueueing of relevant push events to
    eligible processors.
    
    Epic: none
    
    Release note: None

--- pkg/kv/kvclient/rangefeed/rangefeed_external_test.go
+++ pkg/kv/kvclient/rangefeed/rangefeed_external_test.go
@@ -58,7 +58,11 @@ type feedProcessorType struct {
 }
 
 func (t feedProcessorType) String() string {
-	return fmt.Sprintf("mux=%t_scheduler=%t", t.feed == rangefeed.MuxRangefeedEnabled, t.useScheduler)
+	if t.feed == rangefeed.MuxRangefeedEnabled {
+		return fmt.Sprintf("mux/scheduler=%t", t.useScheduler)
+	} else {
+		return fmt.Sprintf("single/scheduler=%t", t.useScheduler)
+	}
 }
 
 var procTypes = []feedProcessorType{
