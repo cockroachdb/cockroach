35728b4a3ab826b039b27a8c689e4f6e887a4771
Marcus Gartner <marcus@cockroachlabs.com>
2023-07-27 16:50:45 -0700

    sql: prevent panic when using UDF as EXECUTE argument
    Fixes #99008
    
    Release note (bug fix): A bug has been fixed that caused nodes to crash
    when attempting to `EXECUTE` a prepared statement with an argument that
    referenced a user-defined function. This bug was present since
    user-defined functions were introduced in version 22.2.

--- pkg/sql/logictest/tests/local-legacy-schema-changer/generated_test.go
+++ pkg/sql/logictest/tests/local-legacy-schema-changer/generated_test.go
@@ -2172,6 +2172,13 @@ func TestLogic_udf_plpgsql(
 	runLogicTest(t, "udf_plpgsql")
 }
 
+func TestLogic_udf_prepare(
+	t *testing.T,
+) {
+	defer leaktest.AfterTest(t)()
+	runLogicTest(t, "udf_prepare")
+}
+
 func TestLogic_udf_privileges(
 	t *testing.T,
 ) {
