c6166bcf7921f33d45622bbfda1194094599a088
wenyihu6 <wenyi.hu@cockroachlabs.com>
2023-07-26 21:39:44 -0400

    asim: add tests for constraints validation, asim determinism
    This patch adds an asim datadriven test case which sets up zone constraints and
    ensures its proper application and validation of these constraints.
    
    Part Of: https://github.com/cockroachdb/cockroach/issues/105904
    Release Note: None

--- pkg/kv/kvserver/asim/asim_test.go
+++ pkg/kv/kvserver/asim/asim_test.go
@@ -18,10 +18,14 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/config"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/event"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/gen"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/metrics"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/state"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/workload"
+	"github.com/cockroachdb/cockroach/pkg/spanconfig/spanconfigtestutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/stretchr/testify/require"
 )
 
@@ -39,17 +43,18 @@ func TestRunAllocatorSimulator(t *testing.T) {
 	sim.RunSim(ctx)
 }
 
-func TestAllocatorSimulatorDeterministic(t *testing.T) {
-	skip.WithIssue(t, 105904, "asim is non-deterministic")
+func TestAsimDeterministic(t *testing.T) {
+	skip.UnderRace(t, 105904, "asim is non-deterministic under race")
 	settings := config.DefaultSimulationSettings()
 
 	runs := 3
 	duration := 15 * time.Minute
 	settings.TickInterval = 2 * time.Second
 
-	stores := 7
+	stores := 21
 	replsPerRange := 3
-	replicasPerStore := 100
+	replicasPerStore := 600
+
 	// NB: We want 100 replicas per store, so the number of ranges required
 	// will be 1/3 of the total replicas.
 	ranges := (replicasPerStore * stores) / replsPerRange
@@ -84,6 +89,109 @@ func TestAllocatorSimulatorDeterministic(t *testing.T) {
 		sim.RunSim(ctx)
 		history := sim.History()
 
+		if run == 0 {
+			refRun = history
+			continue
+		}
+		require.Equal(t, refRun.Recorded, history.Recorded)
+	}
+}
+
+func TestAsimDeterministicZoneConf(t *testing.T) {
+	const defaultSeed = 42
+	const defaultKeyspace = 10000
+	settingsGen := gen.StaticSettings{Settings: config.DefaultSimulationSettings()}
+	duration := 30 * time.Minute
+	clusterGen := gen.LoadedCluster{
+		Info: state.MultiRegionConfig,
+	}
+	rangeGen := gen.BasicRanges{
+		BaseRanges: gen.BaseRanges{
+			Ranges:            200,
+			ReplicationFactor: 3,
+			KeySpace:          defaultKeyspace,
+		},
+		PlacementType: gen.Uniform,
+	}
+
+	loadGen := gen.BasicLoad{}
+	eventGen := gen.StaticEvents{DelayedEvents: event.DelayedEventList{}}
+	span := spanconfigtestutils.ParseSpan(t, "[0,9999999999)")
+	conf := spanconfigtestutils.ParseZoneConfig(t, "num_replicas=3 constraints={'+region=US_East'}").AsSpanConfig()
+	eventGen.DelayedEvents = append(eventGen.DelayedEvents, event.DelayedEvent{
+		EventFn: func(ctx context.Context, tick time.Time, s state.State) {
+			s.SetSpanConfig(span, conf)
+		},
+		At: settingsGen.Settings.StartTime,
+	})
+
+	num := 3
+	var refRun asim.History
+	for run := 0; run < num; run++ {
+		simulator := gen.GenerateSimulation(duration, clusterGen, rangeGen, loadGen, settingsGen, eventGen, defaultSeed)
+		simulator.RunSim(context.Background())
+		history := simulator.History()
+		if run == 0 {
+			refRun = history
+			continue
+		}
+		require.Equal(t, refRun.Recorded, history.Recorded)
+	}
+}
+
+func TestAsimDeterministicDiskFull(t *testing.T) {
+	const defaultSeed = 42
+	settingsGen := gen.StaticSettings{Settings: config.DefaultSimulationSettings()}
+	duration := 30 * time.Minute
+	defaultKeyspace := 10000
+	clusterGen := gen.BasicCluster{
+		Nodes:         5,
+		StoresPerNode: 1,
+	}
+	rangeGen := gen.BasicRanges{
+		BaseRanges: gen.BaseRanges{
+			Ranges:            500,
+			ReplicationFactor: 3,
+			KeySpace:          defaultKeyspace,
+			Bytes:             300000000,
+		},
+		PlacementType: gen.Uniform,
+	}
+
+	var rwRatio = 0.0
+	var minKey, maxKey = int64(1), int64(defaultKeyspace)
+	var accessSkew bool
+	loadGen := gen.BasicLoad{
+		RWRatio:      rwRatio,
+		Rate:         500,
+		SkewedAccess: accessSkew,
+		MinBlockSize: 128000,
+		MaxBlockSize: 128000,
+		MinKey:       minKey,
+		MaxKey:       maxKey,
+	}
+
+	capacityOverride := state.NewCapacityOverride()
+	capacityOverride.Capacity = 45000000000
+	capacityOverride.Available = -1
+	store := 5
+
+	var delay time.Duration
+	eventGen := gen.StaticEvents{DelayedEvents: event.DelayedEventList{}}
+	eventGen.DelayedEvents = append(eventGen.DelayedEvents, event.DelayedEvent{
+		EventFn: func(ctx context.Context, tick time.Time, s state.State) {
+			log.Infof(ctx, "setting capacity override %+v", capacityOverride)
+			s.SetCapacityOverride(state.StoreID(store), capacityOverride)
+		},
+		At: settingsGen.Settings.StartTime.Add(delay),
+	})
+
+	num := 10
+	var refRun asim.History
+	for run := 0; run < num; run++ {
+		simulator := gen.GenerateSimulation(duration, clusterGen, rangeGen, loadGen, settingsGen, eventGen, defaultSeed)
+		simulator.RunSim(context.Background())
+		history := simulator.History()
 		if run == 0 {
 			refRun = history
 			continue
