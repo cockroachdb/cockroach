4f74c92a4bed0ff1d43e0922487c00b7d5249bb3
zachlite <zachlite@gmail.com>
2023-07-28 10:31:53 -0400

    ui, server: add a timeout per node while collecting hot ranges
    Requests for hot ranges are serviced by a cluster wide fan-out,
    where non-trivial work is done on each node to provide a response.
    For each store, and for each hot range, we start a transaction with KV to look
    up descriptor info.
    
    Previously, there was no upper-bound set on the time a node could take
    to provide a response. This commit introduces a per-node timeout
    in the pagination logic, and is configurable with the new cluster setting
    server.hot_ranges.node.timeout. A value of 0 will disable the timeout.
    
    Error behavior and semantics are preserved. If a particular node times out,
    The fan-out continues as before, as if a node failed to provide a response.
    
    Informs https://github.com/cockroachdb/cockroach/issues/104269
    Resolves https://github.com/cockroachdb/cockroach/issues/107627
    Epic: none
    Release note (ops change): Added a new cluster setting named
    server.hot_ranges.node.timeout, with a default value of 5 minutes.
    The setting controls the maximum amount of time that a hot ranges request
    will spend waiting for a node to provide a response.
    Set to 0 to disable timeouts.

---
+++ pkg/server/hot_ranges.go
@@ -0,0 +1,25 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package server
+
+import (
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/settings"
+)
+
+var HotRangesNodeTimeout = settings.RegisterDurationSetting(
+	settings.TenantWritable,
+	"server.hot_ranges.node.timeout",
+	"the duration allowed for a single node to return hot range data before the request is cancelled; if set to 0, there is no timeout",
+	time.Minute*5,
+	settings.NonNegativeDuration,
+).WithPublic()
