5107cff82ac54c1e9c244a1f5d3bdd2ff2e64e43
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2023-07-31 10:40:21 -0400

    kv: don't let pusher win when pushing STAGING txn with equal priority
    Fixes #105330.
    Fixes #101721.
    
    This commit updates the transaction push logic to stop pushes from completing
    successfully when the pushee is STAGING and the pusher has equal priority. Prior
    to this change, the pusher would win in these cases when using a PUSH_TIMESTAMP
    if at least one of the two transactions involved used a weak isolation level.
    
    This had two undesirable effects:
    - if the pushee was still possibly committable and requiring recovery
      (`!(knownHigherTimestamp || knownHigherEpoch)` in the code) then the pusher
      would immediately begin parallel commit recovery, attempting to disrupt the
      commit and abort the pushee. This is undesirable because the pushee may still
      be in progress and in cases of equal priority, we'd like to wait for the
      parallel commit to complete before kicking off recovery, deferring recovery to
      only the cases where the pushee/committers's heartbeat has expired.
    - if the pushee was known too be uncommittable (`knownHigherTimestamp || knownHigherEpoch`
      in the code), then txn recovery was not kicked off but the pusher still could
      not perform the PUSH_TIMESTAMP (see e40c1b4e), so it would return a
      `TransactionPushError`. This confused logic in `handleTransactionPushError`,
      allowing the error to escape to the client.
    
    This commit resolves both issues by considering the pushee's transaction status
    in `txnwait.ShouldPushImmediately`.
    
    Release note: None

--- pkg/kv/kvnemesis/validator.go
+++ pkg/kv/kvnemesis/validator.go
@@ -1232,16 +1232,6 @@ func (v *validator) failIfError(
 ) (ambiguous, hasError bool) {
 	exceptions = append(exceptions[:len(exceptions):len(exceptions)], func(err error) bool {
 		return errors.Is(err, errInjected)
-	}, func(err error) bool {
-		// Work-around for [1].
-		//
-		// TODO(arul): find out why we (as of [2]) sometimes leaking
-		// *TransactionPushError (wrapped in `UnhandledRetryableError`) from
-		// `db.Get`, `db.Scan`, etc.
-		//
-		// [1]: https://github.com/cockroachdb/cockroach/issues/105330
-		// [2]: https://github.com/cockroachdb/cockroach/pull/97779
-		return errors.HasType(err, (*kvpb.UnhandledRetryableError)(nil))
 	})
 	switch r.Type {
 	case ResultType_Unknown:
