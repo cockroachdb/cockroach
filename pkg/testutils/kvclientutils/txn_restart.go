0c1aac7be873881c7d8e1543fd40a5d20e83de6c
Evan Wall <wall@cockroachlabs.com>
2023-08-01 07:11:00 -0400

    schema: allow transaction retry in schema changer backfill
    Fixes #106696
    
    This change adds a test to simulate transaction retries during the schema
    changer backfill.
    
    Some retries caused `ingested key collides with an existing one` errors
    during addSSTable because the schema change's source data changed.
    For example: `CREATE TABLE t AS SELECT nextval('seq');` - `nextval` is
    non-transactional so each time it is selected, the value changes.
    
    This change also allows the schema changer to specify that addSSTable is
    allowed to shadow (replace) keys' values if they are different to handle
    this case.
    
    Release note: None

--- pkg/testutils/kvclientutils/txn_restart.go
+++ pkg/testutils/kvclientutils/txn_restart.go
@@ -11,8 +11,12 @@
 package kvclientutils
 
 import (
+	"strings"
+
 	"github.com/cockroachdb/cockroach/pkg/kv"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 )
 
 var randRetryRngSource, _ = randutil.NewLockedPseudoRand()
@@ -21,4 +25,34 @@ func RandomTransactionRetryFilter() func(*kv.Txn) bool {
 	return func(*kv.Txn) bool {
 		return randRetryRngSource.Float64() < kv.RandomTxnRetryProbability
 	}
+}
+
+func PrefixTransactionRetryFilter(
+	t testutils.TB, prefix string, maxCount int,
+) (func(*kv.Txn) bool, func()) {
+	var count int
+	var mu syncutil.Mutex
+	verifyFunc := func() {
+		mu.Lock()
+		defer mu.Unlock()
+		if count == 0 {
+			t.Errorf("expected at least 1 transaction to match prefix %q", prefix)
+		}
+	}
+	filterFunc := func(txn *kv.Txn) bool {
+		// Use DebugNameLocked because txn is locked by the caller.
+		if !strings.HasPrefix(txn.DebugNameLocked(), prefix) {
+			return false
+		}
+
+		mu.Lock()
+		defer mu.Unlock()
+		if count < maxCount {
+			count++
+			return true
+		}
+		return false
+	}
+
+	return filterFunc, verifyFunc
 }
