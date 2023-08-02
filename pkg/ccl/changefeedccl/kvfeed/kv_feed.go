0f3637a3c982879136691a44c0cefb3c49a66310
Yevgeniy Miretskiy <yevgeniy@cockroachlabs.com>
2023-08-02 15:13:58 -0400

    changefeedccl: Release allocation when skipping events
    The changefeed (or KV feed to be precise) may skip
    some events when "scan boundary" is reached.
    Scan boundary is a timestamp when certain event occurs --
    usually a schema change.  But, it may also occur
    when the `end_time` option is set.
    
    The KV feed ignores events that have MVCC timestamp
    greater or equal to the scan boundary event.
    
    Unfortunately, due to a long outstanding bug, the memory
    allocation associated with the event would not be released
    when KV feed decides to skip the event.
    
    Because of this, allocated memory was "leaked" and not reclaimed.
    If enough additional events arrive, those leaked events may
    account for all of the memory budget, thus leading to inability
    for additional events to be added.
    
    This bug impacts any changefeeds running with the `end_time`
    option set.  It might also impact changefeeds that observe
    normal schema change event, though this situation is highly unlikely
    (the same transaction that perform schema change had to have
    modified sufficient number of rows in the table to fill
    up all of the memory budget).
    
    Fixes #108040
    
    Release note (enterprise change): Fix a potential "deadlock" when
    running changefeed with `end_time` option set.

--- pkg/ccl/changefeedccl/kvfeed/kv_feed.go
+++ pkg/ccl/changefeedccl/kvfeed/kv_feed.go
@@ -501,12 +501,24 @@ func copyFromSourceToDestUntilTableEvent(
 		}
 	}
 	var (
-		scanBoundary         errBoundaryReached
+		scanBoundary errBoundaryReached
+		endTimeIsSet = !endTime.IsEmpty()
+
+		// checkForScanBoundary takes in a new event's timestamp (event generated
+		// from rangefeed), and asks "Is some type of 'boundary' reached
+		// at 'ts'?"
+		// Here a boundary is reached either
+		// - table event(s) occurred at timestamp at or before `ts`, or
+		// - endTime reached at or before `ts`.
 		checkForScanBoundary = func(ts hlc.Timestamp) error {
 			// If the scanBoundary is not nil, it either means that there is a table
 			// event boundary set or a boundary for the end time. If the boundary is
 			// for the end time, we should keep looking for table events.
-			_, isEndTimeBoundary := scanBoundary.(*errEndTimeReached)
+			isEndTimeBoundary := false
+			if endTimeIsSet {
+				_, isEndTimeBoundary = scanBoundary.(*errEndTimeReached)
+			}
+
 			if scanBoundary != nil && !isEndTimeBoundary {
 				return nil
 			}
@@ -521,7 +533,7 @@ func copyFromSourceToDestUntilTableEvent(
 			// precedence to table events.
 			if len(nextEvents) > 0 {
 				scanBoundary = &errTableEventReached{nextEvents[0]}
-			} else if !endTime.IsEmpty() && scanBoundary == nil {
+			} else if endTimeIsSet && scanBoundary == nil {
 				scanBoundary = &errEndTimeReached{
 					endTime: endTime,
 				}
@@ -584,6 +596,17 @@ func copyFromSourceToDestUntilTableEvent(
 			if err != nil {
 				return err
 			}
+
+			if skipEntry || scanBoundaryReached {
+				// We will skip this entry or outright terminate kvfeed (if boundary reached).
+				// Regardless of the reason, we must release this event memory allocation
+				// since other ranges might not have reached scan boundary yet.
+				// Failure to release this event allocation may prevent other events from being
+				// enqueued in the blocking buffer due to memory limit.
+				a := e.DetachAlloc()
+				a.Release(ctx)
+			}
+
 			if scanBoundaryReached {
 				// All component rangefeeds are now at the boundary.
 				// Break out of the ctxgroup by returning the sentinel error.
@@ -595,7 +618,6 @@ func copyFromSourceToDestUntilTableEvent(
 			return addEntry(e)
 		}
 	)
-
 	for {
 		e, err := source.Get(ctx)
 		if err != nil {
