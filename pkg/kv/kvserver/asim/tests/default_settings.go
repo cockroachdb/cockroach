836222c141822d929bc6e126a822e6ef8d1563c0
wenyihu6 <wenyi.hu@cockroachlabs.com>
2023-08-02 15:16:34 -0400

    asim: enable user-defined repliFactor, placement in rand range_gen
    This patch introduces two additional options for randomized range generations,
    letting users define  replication factor and placement type. Although some
    aspects of ranges configs are randomly generated (ranges and keyspace), these
    two configurations are not randomized. Once set by the user, the configuration
    will persist across iterations.
    
    Release Note: none
    Part Of: #106311

--- pkg/kv/kvserver/asim/tests/default_settings.go
+++ pkg/kv/kvserver/asim/tests/default_settings.go
@@ -108,24 +108,3 @@ func defaultPlotSettings() plotSettings {
 		width:  defaultWidth,
 	}
 }
-
-type rangeGenSettings struct {
-	rangeKeyGenType generatorType
-	keySpaceGenType generatorType
-	weightedRand    []float64
-}
-
-const (
-	defaultRangeKeyGenType = uniformGenerator
-	defaultKeySpaceGenType = uniformGenerator
-)
-
-var defaultWeightedRand []float64
-
-func defaultRangeGenSettings() rangeGenSettings {
-	return rangeGenSettings{
-		rangeKeyGenType: defaultRangeKeyGenType,
-		keySpaceGenType: defaultKeySpaceGenType,
-		weightedRand:    defaultWeightedRand,
-	}
-}
