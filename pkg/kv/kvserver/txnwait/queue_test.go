572ec055700417f49e9feaf8547a6bd6e298892f
Mira Radeva <mira@cockroachlabs.com>
2023-08-02 13:25:10 -0400

    kv: Allow WaitPolicy_Error requests to push with priority
    Previously, a request with WaitPolicy_Error could only push a
    transaction using PUSH_TOUCH to check that the transaction is not
    abandoned. However, sometimes it is useful for a WaitPolicy_Error
    request to be able to push the timestamp of a transaction with a lower
    priority (see #104728).
    
    As part of implementing the above, this change also simplifies the
    handling of WaitPolicy_Error by removing the use of PUSH_TOUCH in the
    lock table waiter, and using a regular (PUSH_TIMESTAMP or PUSH_ABORT)
    push instead. The wait policy is passed as part of the push request and
    handled at the server/pushee side to ensure that if the push fails the
    request does not wait in the txn wait queue.
    
    Fixes: #104728
    Epic: CRDB-28698
    
    Release note: None

--- pkg/kv/kvserver/txnwait/queue_test.go
+++ pkg/kv/kvserver/txnwait/queue_test.go
@@ -21,8 +21,10 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/kv"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/concurrency/isolation"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/concurrency/lock"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/storage/enginepb"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
@@ -39,131 +41,93 @@ func TestShouldPushImmediately(t *testing.T) {
 	mid1 := enginepb.TxnPriority(1)
 	mid2 := enginepb.TxnPriority(2)
 	testCases := []struct {
-		force      bool
 		typ        kvpb.PushTxnType
 		pusherPri  enginepb.TxnPriority
 		pusheePri  enginepb.TxnPriority
 		shouldPush bool
 	}{
-		{false, kvpb.PUSH_ABORT, min, min, false},
-		{false, kvpb.PUSH_ABORT, min, mid1, false},
-		{false, kvpb.PUSH_ABORT, min, mid2, false},
-		{false, kvpb.PUSH_ABORT, min, max, false},
-		{false, kvpb.PUSH_ABORT, mid1, min, true},
-		{false, kvpb.PUSH_ABORT, mid1, mid1, false},
-		{false, kvpb.PUSH_ABORT, mid1, mid2, false},
-		{false, kvpb.PUSH_ABORT, mid1, max, false},
-		{false, kvpb.PUSH_ABORT, mid2, min, true},
-		{false, kvpb.PUSH_ABORT, mid2, mid1, false},
-		{false, kvpb.PUSH_ABORT, mid2, mid2, false},
-		{false, kvpb.PUSH_ABORT, mid2, max, false},
-		{false, kvpb.PUSH_ABORT, max, min, true},
-		{false, kvpb.PUSH_ABORT, max, mid1, true},
-		{false, kvpb.PUSH_ABORT, max, mid2, true},
-		{false, kvpb.PUSH_ABORT, max, max, false},
-		{false, kvpb.PUSH_TIMESTAMP, min, min, false},
-		{false, kvpb.PUSH_TIMESTAMP, min, mid1, false},
-		{false, kvpb.PUSH_TIMESTAMP, min, mid2, false},
-		{false, kvpb.PUSH_TIMESTAMP, min, max, false},
-		{false, kvpb.PUSH_TIMESTAMP, mid1, min, true},
-		{false, kvpb.PUSH_TIMESTAMP, mid1, mid1, false},
-		{false, kvpb.PUSH_TIMESTAMP, mid1, mid2, false},
-		{false, kvpb.PUSH_TIMESTAMP, mid1, max, false},
-		{false, kvpb.PUSH_TIMESTAMP, mid2, min, true},
-		{false, kvpb.PUSH_TIMESTAMP, mid2, mid1, false},
-		{false, kvpb.PUSH_TIMESTAMP, mid2, mid2, false},
-		{false, kvpb.PUSH_TIMESTAMP, mid2, max, false},
-		{false, kvpb.PUSH_TIMESTAMP, max, min, true},
-		{false, kvpb.PUSH_TIMESTAMP, max, mid1, true},
-		{false, kvpb.PUSH_TIMESTAMP, max, mid2, true},
-		{false, kvpb.PUSH_TIMESTAMP, max, max, false},
-		{false, kvpb.PUSH_TOUCH, min, min, true},
-		{false, kvpb.PUSH_TOUCH, min, mid1, true},
-		{false, kvpb.PUSH_TOUCH, min, mid2, true},
-		{false, kvpb.PUSH_TOUCH, min, max, true},
-		{false, kvpb.PUSH_TOUCH, mid1, min, true},
-		{false, kvpb.PUSH_TOUCH, mid1, mid1, true},
-		{false, kvpb.PUSH_TOUCH, mid1, mid2, true},
-		{false, kvpb.PUSH_TOUCH, mid1, max, true},
-		{false, kvpb.PUSH_TOUCH, mid2, min, true},
-		{false, kvpb.PUSH_TOUCH, mid2, mid1, true},
-		{false, kvpb.PUSH_TOUCH, mid2, mid2, true},
-		{false, kvpb.PUSH_TOUCH, mid2, max, true},
-		{false, kvpb.PUSH_TOUCH, max, min, true},
-		{false, kvpb.PUSH_TOUCH, max, mid1, true},
-		{false, kvpb.PUSH_TOUCH, max, mid2, true},
-		{false, kvpb.PUSH_TOUCH, max, max, true},
-		// Force pushes always push immediately.
-		{true, kvpb.PUSH_ABORT, min, min, true},
-		{true, kvpb.PUSH_ABORT, min, mid1, true},
-		{true, kvpb.PUSH_ABORT, min, mid2, true},
-		{true, kvpb.PUSH_ABORT, min, max, true},
-		{true, kvpb.PUSH_ABORT, mid1, min, true},
-		{true, kvpb.PUSH_ABORT, mid1, mid1, true},
-		{true, kvpb.PUSH_ABORT, mid1, mid2, true},
-		{true, kvpb.PUSH_ABORT, mid1, max, true},
-		{true, kvpb.PUSH_ABORT, mid2, min, true},
-		{true, kvpb.PUSH_ABORT, mid2, mid1, true},
-		{true, kvpb.PUSH_ABORT, mid2, mid2, true},
-		{true, kvpb.PUSH_ABORT, mid2, max, true},
-		{true, kvpb.PUSH_ABORT, max, min, true},
-		{true, kvpb.PUSH_ABORT, max, mid1, true},
-		{true, kvpb.PUSH_ABORT, max, mid2, true},
-		{true, kvpb.PUSH_ABORT, max, max, true},
-		{true, kvpb.PUSH_TIMESTAMP, min, min, true},
-		{true, kvpb.PUSH_TIMESTAMP, min, mid1, true},
-		{true, kvpb.PUSH_TIMESTAMP, min, mid2, true},
-		{true, kvpb.PUSH_TIMESTAMP, min, max, true},
-		{true, kvpb.PUSH_TIMESTAMP, mid1, min, true},
-		{true, kvpb.PUSH_TIMESTAMP, mid1, mid1, true},
-		{true, kvpb.PUSH_TIMESTAMP, mid1, mid2, true},
-		{true, kvpb.PUSH_TIMESTAMP, mid1, max, true},
-		{true, kvpb.PUSH_TIMESTAMP, mid2, min, true},
-		{true, kvpb.PUSH_TIMESTAMP, mid2, mid1, true},
-		{true, kvpb.PUSH_TIMESTAMP, mid2, mid2, true},
-		{true, kvpb.PUSH_TIMESTAMP, mid2, max, true},
-		{true, kvpb.PUSH_TIMESTAMP, max, min, true},
-		{true, kvpb.PUSH_TIMESTAMP, max, mid1, true},
-		{true, kvpb.PUSH_TIMESTAMP, max, mid2, true},
-		{true, kvpb.PUSH_TIMESTAMP, max, max, true},
-		{true, kvpb.PUSH_TOUCH, min, min, true},
-		{true, kvpb.PUSH_TOUCH, min, mid1, true},
-		{true, kvpb.PUSH_TOUCH, min, mid2, true},
-		{true, kvpb.PUSH_TOUCH, min, max, true},
-		{true, kvpb.PUSH_TOUCH, mid1, min, true},
-		{true, kvpb.PUSH_TOUCH, mid1, mid1, true},
-		{true, kvpb.PUSH_TOUCH, mid1, mid2, true},
-		{true, kvpb.PUSH_TOUCH, mid1, max, true},
-		{true, kvpb.PUSH_TOUCH, mid2, min, true},
-		{true, kvpb.PUSH_TOUCH, mid2, mid1, true},
-		{true, kvpb.PUSH_TOUCH, mid2, mid2, true},
-		{true, kvpb.PUSH_TOUCH, mid2, max, true},
-		{true, kvpb.PUSH_TOUCH, max, min, true},
-		{true, kvpb.PUSH_TOUCH, max, mid1, true},
-		{true, kvpb.PUSH_TOUCH, max, mid2, true},
-		{true, kvpb.PUSH_TOUCH, max, max, true},
+		{kvpb.PUSH_ABORT, min, min, false},
+		{kvpb.PUSH_ABORT, min, mid1, false},
+		{kvpb.PUSH_ABORT, min, mid2, false},
+		{kvpb.PUSH_ABORT, min, max, false},
+		{kvpb.PUSH_ABORT, mid1, min, true},
+		{kvpb.PUSH_ABORT, mid1, mid1, false},
+		{kvpb.PUSH_ABORT, mid1, mid2, false},
+		{kvpb.PUSH_ABORT, mid1, max, false},
+		{kvpb.PUSH_ABORT, mid2, min, true},
+		{kvpb.PUSH_ABORT, mid2, mid1, false},
+		{kvpb.PUSH_ABORT, mid2, mid2, false},
+		{kvpb.PUSH_ABORT, mid2, max, false},
+		{kvpb.PUSH_ABORT, max, min, true},
+		{kvpb.PUSH_ABORT, max, mid1, true},
+		{kvpb.PUSH_ABORT, max, mid2, true},
+		{kvpb.PUSH_ABORT, max, max, false},
+		{kvpb.PUSH_TIMESTAMP, min, min, false},
+		{kvpb.PUSH_TIMESTAMP, min, mid1, false},
+		{kvpb.PUSH_TIMESTAMP, min, mid2, false},
+		{kvpb.PUSH_TIMESTAMP, min, max, false},
+		{kvpb.PUSH_TIMESTAMP, mid1, min, true},
+		{kvpb.PUSH_TIMESTAMP, mid1, mid1, false},
+		{kvpb.PUSH_TIMESTAMP, mid1, mid2, false},
+		{kvpb.PUSH_TIMESTAMP, mid1, max, false},
+		{kvpb.PUSH_TIMESTAMP, mid2, min, true},
+		{kvpb.PUSH_TIMESTAMP, mid2, mid1, false},
+		{kvpb.PUSH_TIMESTAMP, mid2, mid2, false},
+		{kvpb.PUSH_TIMESTAMP, mid2, max, false},
+		{kvpb.PUSH_TIMESTAMP, max, min, true},
+		{kvpb.PUSH_TIMESTAMP, max, mid1, true},
+		{kvpb.PUSH_TIMESTAMP, max, mid2, true},
+		{kvpb.PUSH_TIMESTAMP, max, max, false},
+		{kvpb.PUSH_TOUCH, min, min, true},
+		{kvpb.PUSH_TOUCH, min, mid1, true},
+		{kvpb.PUSH_TOUCH, min, mid2, true},
+		{kvpb.PUSH_TOUCH, min, max, true},
+		{kvpb.PUSH_TOUCH, mid1, min, true},
+		{kvpb.PUSH_TOUCH, mid1, mid1, true},
+		{kvpb.PUSH_TOUCH, mid1, mid2, true},
+		{kvpb.PUSH_TOUCH, mid1, max, true},
+		{kvpb.PUSH_TOUCH, mid2, min, true},
+		{kvpb.PUSH_TOUCH, mid2, mid1, true},
+		{kvpb.PUSH_TOUCH, mid2, mid2, true},
+		{kvpb.PUSH_TOUCH, mid2, max, true},
+		{kvpb.PUSH_TOUCH, max, min, true},
+		{kvpb.PUSH_TOUCH, max, mid1, true},
+		{kvpb.PUSH_TOUCH, max, mid2, true},
+		{kvpb.PUSH_TOUCH, max, max, true},
 	}
 	for _, test := range testCases {
-		t.Run("", func(t *testing.T) {
-			req := kvpb.PushTxnRequest{
-				Force:    test.force,
-				PushType: test.typ,
-				PusherTxn: roachpb.Transaction{
-					TxnMeta: enginepb.TxnMeta{
-						// NOTE: different pusher isolation levels tested below.
-						IsoLevel: isolation.Serializable,
-						Priority: test.pusherPri,
+		testutils.RunTrueAndFalse(t, "forcePush", func(t *testing.T, forcePush bool) {
+			testutils.RunTrueAndFalse(t, "waitPolicyError", func(t *testing.T, waitPolicyError bool) {
+				req := kvpb.PushTxnRequest{
+					Force:    forcePush,
+					PushType: test.typ,
+					PusherTxn: roachpb.Transaction{
+						TxnMeta: enginepb.TxnMeta{
+							// NOTE: different pusher isolation levels tested below.
+							IsoLevel: isolation.Serializable,
+							Priority: test.pusherPri,
+						},
 					},
-				},
-				PusheeTxn: enginepb.TxnMeta{
-					// NOTE: different pushee isolation levels tested below.
-					IsoLevel: isolation.Serializable,
-					Priority: test.pusheePri,
-				},
-			}
-			pusheeStatus := roachpb.PENDING
-			shouldPush := ShouldPushImmediately(&req, pusheeStatus)
-			require.Equal(t, test.shouldPush, shouldPush)
+					PusheeTxn: enginepb.TxnMeta{
+						// NOTE: different pushee isolation levels tested below.
+						IsoLevel: isolation.Serializable,
+						Priority: test.pusheePri,
+					},
+				}
+				var wp lock.WaitPolicy
+				if waitPolicyError {
+					wp = lock.WaitPolicy_Error
+				} else {
+					wp = lock.WaitPolicy_Block
+				}
+				pusheeStatus := roachpb.PENDING
+				shouldPush := ShouldPushImmediately(&req, pusheeStatus, wp)
+				if waitPolicyError || forcePush {
+					require.True(t, shouldPush)
+				} else {
+					require.Equal(t, test.shouldPush, shouldPush)
+				}
+			})
 		})
 	}
 }
@@ -576,7 +540,7 @@ func TestMaybeWaitForPushWithContextCancellation(t *testing.T) {
 	waitingRes := make(chan *kvpb.Error)
 	go func() {
 		req := kvpb.PushTxnRequest{PusheeTxn: txn.TxnMeta, PushType: kvpb.PUSH_ABORT}
-		_, err := q.MaybeWaitForPush(ctx, &req)
+		_, err := q.MaybeWaitForPush(ctx, &req, lock.WaitPolicy_Block)
 		waitingRes <- err
 	}()
 
@@ -680,7 +644,7 @@ func TestPushersReleasedAfterAnyQueryTxnFindsAbortedTxn(t *testing.T) {
 			defer wg.Done()
 			ctx := context.Background()
 			req := kvpb.PushTxnRequest{PusheeTxn: txn.TxnMeta, PushType: kvpb.PUSH_ABORT}
-			res, err := q.MaybeWaitForPush(ctx, &req)
+			res, err := q.MaybeWaitForPush(ctx, &req, lock.WaitPolicy_Block)
 			require.Nil(t, err)
 			require.NotNil(t, res)
 			require.Equal(t, roachpb.ABORTED, res.PusheeTxn.Status)
