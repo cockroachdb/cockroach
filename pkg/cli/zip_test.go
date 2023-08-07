e762b4fe0d8ffc2c8b8bf7675ff9ab07dfbce4ca
David Hartunian <davidh@cockroachlabs.com>
2023-08-07 15:40:32 -0400

    cli: debug zip uses read-only range probe
    To reduce the chance of corruption issues when run against older
    clusters, the recently added use of 'crdb_internal.probe_ranges` is
    modified to use a `read` probe instead of a `write` probe.
    
    See discussion in #107720
    
    Release note: None
    Epic: None

--- pkg/cli/zip_test.go
+++ pkg/cli/zip_test.go
@@ -121,7 +121,7 @@ ORDER BY name ASC`)
 		assert.NoError(t, rows.Scan(&table))
 		tables = append(tables, table)
 	}
-	tables = append(tables, "crdb_internal.probe_ranges_1s_write_limit_100")
+	tables = append(tables, "crdb_internal.probe_ranges_1s_read_limit_100")
 	sort.Strings(tables)
 
 	var exp []string
