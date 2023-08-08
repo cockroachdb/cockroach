10a02cc724afdd9d192948213222dbbe7bcfeb2a
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-08-07 20:05:59 -0700

    sql: retry all "rpc error" distributed errors as local
    This commit includes all errors that contain `rpc error` substring to be
    retried-as-local. In particular, this allows us to avoid problems with
    DistSQL using no-longer-live SQL pod after that pod is shutdown. (This
    usage of the downed pod is currently expected given that the cache of
    live instances isn't updated when the pod is shutdown.)
    
    Release note: None

--- pkg/sql/sqlerrors/errors.go
+++ pkg/sql/sqlerrors/errors.go
@@ -12,6 +12,8 @@
 package sqlerrors
 
 import (
+	"strings"
+
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/security/username"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
@@ -410,4 +412,23 @@ func errHasCode(err error, code ...pgcode.Code) bool {
 		}
 	}
 	return false
+}
+
+// IsDistSQLRetryableError returns true if the supplied error, or any of its parent
+// causes is an rpc error.
+// This is an unfortunate implementation that should be looking for a more
+// specific error.
+func IsDistSQLRetryableError(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	// TODO(knz): this is a bad implementation. Make it go away
+	// by avoiding string comparisons.
+
+	errStr := err.Error()
+	// When a crdb node dies, any DistSQL flows with processors scheduled on
+	// it get an error with "rpc error" in the message from the call to
+	// `(*DistSQLPlanner).Run`.
+	return strings.Contains(errStr, `rpc error`)
 }
