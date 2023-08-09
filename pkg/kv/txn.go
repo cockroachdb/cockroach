274fffb5ad81230cf463bedfc1c4521462043c3b
Evan Wall <wall@cockroachlabs.com>
2023-08-09 11:03:25 -0400

    testing: replace EnableRandomTransactionRetryErrors with TransactionRetryFilter
    Adds TransactionRetryFilter which allows the retry behavior to be customized.
    
    EnableRandomTransactionRetryErrors has been refactored to a
    TransactionRetryFilter implementation that randomly retries.
    
    Release note: None

--- pkg/kv/txn.go
+++ pkg/kv/txn.go
@@ -27,7 +27,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
-	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 	"github.com/cockroachdb/errors"
@@ -1426,25 +1425,19 @@ func (txn *Txn) GenerateForcedRetryableError(
 	return txn.mu.sender.PrepareRetryableError(ctx, msg)
 }
 
-var (
-	randRetryRngSource, _     = randutil.NewPseudoRand()
-	randomTxnRetryProbability = 0.1
-)
+const RandomTxnRetryProbability = 0.1
 
-// TestingShouldReturnRandomRetry returns true if we should generate a
+// TestingShouldRetry returns true if we should generate a
 // random, retriable error for this transaction.
-func (txn *Txn) TestingShouldReturnRandomRetry() bool {
+func (txn *Txn) TestingShouldRetry() bool {
 	txn.mu.Lock()
 	defer txn.mu.Unlock()
-
-	if !txn.mu.sender.TestingRandomRetryableErrorsEnabled() {
-		return false
-	}
 
 	if txn.mu.sender.ClientFinalized() {
 		return false
 	}
-	return randRetryRngSource.Float64() < randomTxnRetryProbability
+
+	return txn.mu.sender.TestingShouldRetry(txn)
 }
 
 // IsSerializablePushAndRefreshNotPossible returns true if the transaction is
