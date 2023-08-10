31a09ce2867e7a9c343fb9b904426cc5d1714f1f
DarrylWong <darryl@cockroachlabs.com>
2023-08-10 13:47:48 -0400

    roachtest: pass artifacts-literal flag to local roachtests
    Previously, the --artifacts-literal flag was not passed in for local roachtests. In the event of a test failure,
    roachtest should immediately publish artifacts so they are available to the TeamCity UI before the entire build finishes.
    Without explicitly passing in the artifacts directory, roachtest is unable to tell TeamCity where to collect
    a test's artifacts and the immediate publish fails. To address this, an artifacts-literal flag is now passed for local roachtests.
    
    Fixes: #103778
    
    Epic: None
    
    Release note: None

--- build/teamcity/cockroach/ci/tests/local_roachtest.sh
+++ build/teamcity/cockroach/ci/tests/local_roachtest.sh
@@ -8,6 +8,6 @@ source "$dir/teamcity-support.sh"  # For $root
 source "$dir/teamcity-bazel-support.sh"  # For run_bazel
 
 tc_start_block "Run local roachtests"
-BAZEL_SUPPORT_EXTRA_DOCKER_ARGS="-e COCKROACH_DEV_LICENSE -e BUILD_VCS_NUMBER -e TC_BUILD_ID -e TC_BUILD_BRANCH" \
+BAZEL_SUPPORT_EXTRA_DOCKER_ARGS="-e LITERAL_ARTIFACTS_DIR=$root/artifacts -e COCKROACH_DEV_LICENSE -e BUILD_VCS_NUMBER -e TC_BUILD_ID -e TC_BUILD_BRANCH" \
   run_bazel build/teamcity/cockroach/ci/tests/local_roachtest_impl.sh
 tc_end_block "Run local roachtests"
