eb5f4fad437ff8109b839bfe1e24f16268abd9be
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2023-08-12 16:51:34 -0400

    kv: don't step read timestamp for read committed AOST txns
    Fixes #100154.
    
    This commit updates the stepping logic in the TxnCoordSender to not step a read
    committed transaction's read timestamp if the transaction's read timestamp has
    been fixed. This fixes AS OF SYSTEM TIME transactions and, in particular,
    follower reads in read committed transactions.
    
    Once this lands, I'd like to follow it up with an enhancement to the
    `follower-reads` roachtest suite to run follower reads under weak isolation
    transactions. This is tracked in #108671.
    
    Release note: None

--- pkg/kv/kvclient/kvcoord/txn_test.go
+++ pkg/kv/kvclient/kvcoord/txn_test.go
@@ -381,7 +381,19 @@ func TestTxnReadCommittedPerStatementReadSnapshot(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 
-	run := func(t *testing.T, isoLevel isolation.Level, mode kv.SteppingMode, step, expObserveExternalWrites bool) {
+	run := func(
+		t *testing.T,
+		// The transaction's isolation level.
+		isoLevel isolation.Level,
+		// Is the transaction's read timestamp fixed?
+		fixedReadTs bool,
+		// Is manual stepping enabled?
+		mode kv.SteppingMode,
+		// If manual stepping is enabled, is the transaction actually stepped?
+		step bool,
+		// Do we expect the transaction to observe intermediate, external writes?
+		expObserveExternalWrites bool,
+	) {
 		s := createTestDB(t)
 		defer s.Stop()
 		ctx := context.Background()
@@ -396,6 +408,10 @@ func TestTxnReadCommittedPerStatementReadSnapshot(t *testing.T) {
 		// Begin the test's transaction.
 		txn1 := s.DB.NewTxn(ctx, "txn1")
 		require.NoError(t, txn1.SetIsoLevel(isoLevel))
+		initReadTs := txn1.ReadTimestamp()
+		if fixedReadTs {
+			require.NoError(t, txn1.SetFixedTimestamp(ctx, initReadTs))
+		}
 		txn1.ConfigureStepping(ctx, mode)
 
 		// In a loop, increment the key outside the transaction, then read it in the
@@ -433,27 +449,35 @@ func TestTxnReadCommittedPerStatementReadSnapshot(t *testing.T) {
 			expVals = []int64{1, 1, 1}
 		}
 		require.Equal(t, expVals, readVals)
+
+		// Check whether the transaction's read timestamp changed.
+		sameReadTs := initReadTs == txn1.ReadTimestamp()
+		require.Equal(t, expObserveExternalWrites, !sameReadTs)
 	}
 
 	isolation.RunEachLevel(t, func(t *testing.T, isoLevel isolation.Level) {
-		testutils.RunTrueAndFalse(t, "steppingMode", func(t *testing.T, modeBool bool) {
-			mode := kv.SteppingMode(modeBool)
-			if mode == kv.SteppingEnabled {
-				// If stepping is enabled, run a variant of the test where the
-				// transaction is stepped between reads and a variant of the test
-				// where it is not.
-				testutils.RunTrueAndFalse(t, "step", func(t *testing.T, step bool) {
+		testutils.RunTrueAndFalse(t, "fixedReadTs", func(t *testing.T, fixedReadTs bool) {
+			testutils.RunTrueAndFalse(t, "steppingMode", func(t *testing.T, modeBool bool) {
+				mode := kv.SteppingMode(modeBool)
+				if mode == kv.SteppingEnabled {
+					// If stepping is enabled, run a variant of the test where the
+					// transaction is stepped between reads and a variant of the test
+					// where it is not.
+					testutils.RunTrueAndFalse(t, "step", func(t *testing.T, step bool) {
+						// Expect a new read snapshot on each kv operation if the
+						// transaction is read committed, its read timestamp is not
+						// fixed, and it is manually stepped.
+						expObserveExternalWrites := isoLevel == isolation.ReadCommitted && !fixedReadTs && step
+						run(t, isoLevel, fixedReadTs, mode, step, expObserveExternalWrites)
+					})
+				} else {
 					// Expect a new read snapshot on each kv operation if the
-					// transaction is read committed and is manually stepped.
-					expObserveExternalWrites := isoLevel == isolation.ReadCommitted && step
-					run(t, isoLevel, mode, step, expObserveExternalWrites)
-				})
-			} else {
-				// Expect a new read snapshot on each kv operation if the
-				// transaction is read committed.
-				expObserveExternalWrites := isoLevel == isolation.ReadCommitted
-				run(t, isoLevel, mode, false, expObserveExternalWrites)
-			}
+					// transaction is read committed and its read timestamp is not
+					// fixed.
+					expObserveExternalWrites := isoLevel == isolation.ReadCommitted && !fixedReadTs
+					run(t, isoLevel, fixedReadTs, mode, false, expObserveExternalWrites)
+				}
+			})
 		})
 	})
 }
