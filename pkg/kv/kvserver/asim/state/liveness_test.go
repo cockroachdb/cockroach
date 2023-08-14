f1bb7381edbec276a4fe0ccb512604b9cb4da80a
wenyihu6 <wenyi.hu@cockroachlabs.com>
2023-08-14 14:49:01 -0400

    asim: deflake TestAsimDeterministic
    Previously, the allocator simulator exhibits non-deterministic behavior
    because it sets the liveness expiration record using the real current time
    plus one minute. In addition, the liveness record was not updated between
    ticks. While this was typically acceptable, it could lead to dead nodes in
    deadlock test scenarios where tests took longer.
    
    To address this, the following changes were made in this patch:
    1. Remove the usage of TestNodeVitality as the simulator uses a simulated clock
    rather than a real-time clock.
    2. Replace the existing TestNodeVitality mapping with an enum map for Liveness
    Status. The majority of the code changes just revert to the state before this
    [commit](https://github.com/cockroachdb/cockroach/commit/4a52da5c0d90e40c6e1d6e2b26f4a72dd04527ef).
    3. To hook the liveness component with the liveness status mapping, this patch
    also introduces a conversion method which constructs node vitality in a manner
    that respects all node liveness status properties.
    
    Fixes: #105904, #108092
    Release Note: None

---
+++ pkg/kv/kvserver/asim/state/liveness_test.go
@@ -0,0 +1,75 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package state
+
+import (
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/liveness/livenesspb"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMockLiveness(t *testing.T) {
+	clock := &ManualSimClock{nanos: time.Date(2022, 03, 21, 11, 0, 0, 0, time.UTC).UnixNano()}
+	now := hlc.NewClockForTesting(clock).Now()
+	testCases := []struct {
+		status           livenesspb.NodeLivenessStatus
+		IsAlive          bool
+		MembershipStatus livenesspb.MembershipStatus
+	}{
+		{
+			status:           livenesspb.NodeLivenessStatus_UNKNOWN,
+			IsAlive:          false,
+			MembershipStatus: livenesspb.MembershipStatus_ACTIVE,
+		},
+		{
+			status:           livenesspb.NodeLivenessStatus_DEAD,
+			IsAlive:          false,
+			MembershipStatus: livenesspb.MembershipStatus_ACTIVE,
+		},
+		{
+			status:           livenesspb.NodeLivenessStatus_UNAVAILABLE,
+			IsAlive:          false,
+			MembershipStatus: livenesspb.MembershipStatus_ACTIVE,
+		},
+		{
+			status:           livenesspb.NodeLivenessStatus_DECOMMISSIONED,
+			IsAlive:          false,
+			MembershipStatus: livenesspb.MembershipStatus_DECOMMISSIONED,
+		},
+		{
+			status:           livenesspb.NodeLivenessStatus_DECOMMISSIONING,
+			IsAlive:          true,
+			MembershipStatus: livenesspb.MembershipStatus_DECOMMISSIONING,
+		},
+		{
+			status:           livenesspb.NodeLivenessStatus_LIVE,
+			IsAlive:          true,
+			MembershipStatus: livenesspb.MembershipStatus_ACTIVE,
+		},
+		{
+			status:           livenesspb.NodeLivenessStatus_DRAINING,
+			IsAlive:          true,
+			MembershipStatus: livenesspb.MembershipStatus_ACTIVE,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.status.String(), func(t *testing.T) {
+			nv := convertNodeStatusToNodeVitality(roachpb.NodeID(1), tc.status, now)
+			require.Equal(t, tc.IsAlive, nv.IsLive(livenesspb.Rebalance))
+			require.Equal(t, tc.MembershipStatus, nv.MembershipStatus())
+			require.Equal(t, tc.status, nv.LivenessStatus())
+		})
+	}
+}
