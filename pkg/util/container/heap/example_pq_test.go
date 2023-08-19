3fb160fb6e10d408fbf03e08027b321f9c86f043
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2023-08-19 17:48:40 -0400

    util: introduce generic containers package
    Package container provides three container types: heap, list, and ring.
    
    The package is a direct copy of the Go standard library's `container` package,
    but with interfaces replaced with generics. No other changes have been made,
    even at the expense of ergonomics in a few places, and no other changes should
    be made. The intention is to delete this library when the Go standard library
    adds generics support to these packages.
    
    The use of generics over (empty) interfaces for the contained value in these
    containers provides three main benefits:
    
      - type safety: generics replace dynamic type assertions with static type
        checks. This ensures that code with type errors cannot compile, rather than
        panicking at runtime.
    
      - performance: generics allow values to be stored directly in the container,
        rather than boxed in an interface{}. This reduces allocations, eliminates
        runtime type assertions, and improves cache locality.
    
      - code documentation: generics require the container to be parameterized by
        the type of value it stores. This serves as a helpful form of documentation
        that describes the intended use of the container.
    
    Container types are one of the quintessential application of generics, so much
    of the benefit of generics that was described in the original design proposal[1]
    is realized by this package.
    
    [1]: https://go.googlesource.com/proposal/+/HEAD/design/43651-type-parameters.md
    
    Epic: None
    Release note: None

---
+++ pkg/util/container/heap/example_pq_test.go
@@ -0,0 +1,109 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This example demonstrates a priority queue built using the heap interface.
+package heap_test
+
+import (
+	"fmt"
+
+	"github.com/cockroachdb/cockroach/pkg/util/container/heap"
+)
+
+// An Item is something we manage in a priority queue.
+type Item struct {
+	value    string // The value of the item; arbitrary.
+	priority int    // The priority of the item in the queue.
+	// The index is needed by update and is maintained by the heap.Interface methods.
+	index int // The index of the item in the heap.
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	return pq[i].priority > pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x *Item) {
+	n := len(*pq)
+	item := x
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() *Item {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update modifies the priority and value of an Item in the queue.
+func (pq *PriorityQueue) update(item *Item, value string, priority int) {
+	item.value = value
+	item.priority = priority
+	heap.Fix[*Item](pq, item.index)
+}
+
+// This example creates a PriorityQueue with some items, adds and manipulates an item,
+// and then removes the items in priority order.
+func Example_priorityQueue() {
+	// Some items and their priorities.
+	items := map[string]int{
+		"banana": 3, "apple": 2, "pear": 4,
+	}
+
+	// Create a priority queue, put the items in it, and
+	// establish the priority queue (heap) invariants.
+	pq := make(PriorityQueue, len(items))
+	i := 0
+	for value, priority := range items {
+		pq[i] = &Item{
+			value:    value,
+			priority: priority,
+			index:    i,
+		}
+		i++
+	}
+	heap.Init[*Item](&pq)
+
+	// Insert a new item and then modify its priority.
+	item := &Item{
+		value:    "orange",
+		priority: 1,
+	}
+	heap.Push[*Item](&pq, item)
+	pq.update(item, item.value, 5)
+
+	// Take the items out; they arrive in decreasing priority order.
+	for pq.Len() > 0 {
+		item := heap.Pop[*Item](&pq)
+		fmt.Printf("%.2d:%s ", item.priority, item.value)
+	}
+	// Output:
+	// 05:orange 04:pear 03:banana 02:apple
+}
