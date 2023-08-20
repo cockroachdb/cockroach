4723150908788f595fe45adaf9b173e6f015dc6e
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2023-08-20 17:36:07 -0400

    kv: rename HandleWriterIntentError to HandleLockConflictError
    Rename and update comments.
    
    Epic: None
    Release note: None

--- pkg/kv/kvserver/replica_send.go
+++ pkg/kv/kvserver/replica_send.go
@@ -756,7 +756,7 @@ func (r *Replica) handleLockConflictError(
 		return g, pErr
 	}
 	// g's latches will be dropped, but it retains its spot in lock wait-queues.
-	return r.concMgr.HandleWriterIntentError(ctx, g, t.LeaseSequence, t)
+	return r.concMgr.HandleLockConflictError(ctx, g, t.LeaseSequence, t)
 }
 
 func (r *Replica) handleTransactionPushError(
