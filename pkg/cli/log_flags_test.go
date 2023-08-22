7657f58203a4dba472ac464b4771a09dd3b380a2
Alex Barganier <abarganier@cockroachlabs.com>
2023-08-22 14:06:46 -0400

    pkg/util/log: change default httpSink timeout to 60s
    Previously, the default timeout for the httpSink was
    for there to be no timeout at all.
    
    This means that the first call to `output()` on where
    the http target was unavailable would hang forever.
    This would deadlock the calling goroutine, whether
    that's the bufferedSink flush goroutine, or (even worse)
    a server goroutine in the event that the httpSink is not
    buffered.
    
    Our default timeout should not deadlock in the worst case
    scenario. Admittedly, `60s` would also cause a significant
    performance degradation in the event that the httpSink was
    unbuffered, but it would at least be able to emit logs
    indicating the timeout as the cause. Previously, the deadlocks
    due to no timeout being set by default meant that no
    indication was ever given that the httpSink was unable
    to reach the http target.
    
    Release note (ops change): The default value of `timeout`
    for `http-servers` logging sinks has been changed from
    "no timeout" to `60s`. This will be reflected in the
    `http-defaults` section of the log configuration. Users
    still maintain the ability to override the timeout, or
    disable it by explicitly setting it to `0` (e.g. `timeout: 0`).

--- pkg/cli/log_flags_test.go
+++ pkg/cli/log_flags_test.go
@@ -51,7 +51,7 @@ func TestSetupLogging(t *testing.T) {
 	const defaultHTTPConfig = `http-defaults: {` +
 		`method: POST, ` +
 		`unsafe-tls: false, ` +
-		`timeout: 0s, ` +
+		`timeout: 1m0s, ` +
 		`disable-keep-alives: false, ` +
 		`compression: gzip, ` +
 		`filter: INFO, ` +
--- pkg/util/log/logconfig/validate.go
+++ pkg/util/log/logconfig/validate.go
@@ -111,8 +111,11 @@ func (c *Config) Validate(defaultLogDir *string) (resErr error) {
 		UnsafeTLS:         &bf,
 		DisableKeepAlives: &bf,
 		Method:            func() *HTTPSinkMethod { m := HTTPSinkMethod(http.MethodPost); return &m }(),
-		Timeout:           &zeroDuration,
-		Compression:       &GzipCompression,
+		Timeout: func() *time.Duration {
+			oneMinute := 60 * time.Second
+			return &oneMinute
+		}(),
+		Compression: &GzipCompression,
 	}
 
 	propagateCommonDefaults(&baseFileDefaults.CommonSinkConfig, baseCommonSinkConfig)
