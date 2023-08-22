cockroachdb/cockroach

commit 0da9146f7b039cc442cba02755c21882d5497c95

concurrency: stop storing txn meta by reference in txnLock
Previously, we would store a reference to the txn meta in the txnLock
struct. This isn't required, as this field must now always be set. This
patch switches over to storing the txnMeta struct directly. Doing so
means we don't hold on to the entire `roachpb.LockAcquisition` or
`roachpb.LockUpdate` struct in a few places.

Epic: none

Release note: None

pkg/kv/kvserver/concurrency/lock_table.go
@@ -705,7 +705,7 @@ func (g *lockTableGuardImpl) IsKeyLockedByConflictingTxn(
 			lockHolderTxn, _ := tl.getLockHolder()
 			if !g.isSameTxn(lockHolderTxn) &&
 				lock.Conflicts(tl.getLockMode(), makeLockMode(str, g.txn, g.ts), &g.lt.settings.SV) {
-				return true, tl.txn, nil // the key is locked by some other transaction; return it
+				return true, &tl.txn, nil // the key is locked by some other transaction; return it
 			}
 		}
 		// We can be in either of 2 cases at this point:
@@ -1197,10 +1197,7 @@ type txnLock struct {
 	// As a result, the TxnMeta stored here may not correspond to the latest
 	// call to acquire/update the lock (if the call was made using a TxnMeta
 	// with an older epoch).
-	//
-	// TODO(arul): Now that we expect this to always be set, let's store the
-	// entire txnMeta here instead of storing it by reference.
-	txn *enginepb.TxnMeta
+	txn enginepb.TxnMeta
 
 	// INVARIANT: At least one of (and possibly both of) unreplicatedInfo and
 	// replicatedInfo must be set to track lock holder information.
@@ -1224,7 +1221,7 @@ type txnLock struct {
 // newTxnLock constructs and returns a new txnLock.
 func newTxnLock(txn *enginepb.TxnMeta, clock *hlc.Clock) *txnLock {
 	tl := &txnLock{}
-	tl.txn = txn
+	tl.txn = *txn
 	tl.unreplicatedInfo.init()
 	tl.startTime = clock.PhysicalTime()
 	return tl
@@ -1251,7 +1248,7 @@ func (tl *txnLock) getLockHolder() (*enginepb.TxnMeta, hlc.Timestamp) {
 	} else {
 		ts = tl.replicatedInfo.ts
 	}
-	return tl.txn, ts
+	return &tl.txn, ts
 }
 
 // isHeldReplicated returns true if the receiver is held as a replicated lock.
@@ -1433,9 +1430,9 @@ func (tl *txnLock) reacquireLock(acq *roachpb.LockAcquisition) error {
 		// unreplicatedLockInfo.
 		assert(acq.Durability == lock.Replicated, "the unreplicated case should have been handled above")
 	case tl.txn.Epoch == acq.Txn.Epoch: // lock is being acquired at the same epoch
-		tl.txn = &acq.Txn
+		tl.txn = acq.Txn
 	case tl.txn.Epoch < acq.Txn.Epoch: // lock is being acquired at a newer epoch
-		tl.txn = &acq.Txn
+		tl.txn = acq.Txn
 		// The txn meta tracked here corresponds to unreplicated locks. When we
 		// learn about a newer epoch during lock acquisition of a replicated lock,
 		// we clear out the unreplicatedLockInfo state being tracked from the
@@ -1714,10 +1711,10 @@ func (l *lockState) lockStateInfo(now time.Time) roachpb.LockStateInfo {
 
 	durability := lock.Unreplicated
 	if l.isLocked() {
-		// TODO(arul): This doesn't work with multiple lock holders; file an issue
-		// about this observability gap.
+		// This doesn't work with multiple lock holders. See
+		// https://github.com/cockroachdb/cockroach/issues/109081.
 		tl := l.holders.Front().Value.(*txnLock)
-		txnHolder = tl.txn
+		txnHolder = &tl.txn
 		if tl.isHeldReplicated() {
 			durability = lock.Replicated
 		}
@@ -1916,7 +1913,7 @@ func (l *lockState) claimantTxn() (_ *enginepb.TxnMeta, held bool) {
 		// necessitates it to change. So we always return the first lock holder,
 		// ensuring all requests consider the same transaction to have claimed a
 		// key.
-		return l.holders.Front().Value.(*txnLock).txn, true
+		return &l.holders.Front().Value.(*txnLock).txn, true
 	}
 	if l.queuedWriters.Len() == 0 {
 		panic("no queued writers or lock holder; no one should be waiting on the lock")
@@ -1927,6 +1924,7 @@ func (l *lockState) claimantTxn() (_ *enginepb.TxnMeta, held bool) {
 
 // releaseWritersFromTxn removes all waiting writers for the lockState that are
 // part of the specified transaction.
+//
 // REQUIRES: l.mu is locked.
 func (l *lockState) releaseWritersFromTxn(txn *enginepb.TxnMeta) {
 	for e := l.queuedWriters.Front(); e != nil; {
@@ -2030,7 +2028,7 @@ func (l *lockState) isAnyLockHeldReplicated() (bool, *enginepb.TxnMeta) {
 	for e := l.holders.Front(); e != nil; e = e.Next() {
 		tl := e.Value.(*txnLock)
 		if tl.isHeldReplicated() {
-			return true, tl.txn
+			return true, &tl.txn
 		}
 	}
 	return false, nil
@@ -2665,7 +2663,7 @@ func (l *lockState) isNonConflictingLock(g *lockTableGuardImpl) bool {
 
 	for e := l.holders.Front(); e != nil; e = e.Next() {
 		tl := e.Value.(*txnLock)
-		if g.isSameTxn(tl.txn) {
+		if g.isSameTxn(&tl.txn) {
 			// NB: Unlike the pessimistic (normal) evaluation code path, we do
 			// not need to check the lock's strength if it is already held by
 			// this transaction -- it's non-conflicting. There's two cases to
@@ -2992,7 +2990,7 @@ func (l *lockState) tryUpdateLockLocked(up roachpb.LockUpdate) (heldByTxn, gc bo
 
 	e := l.heldBy[up.Txn.ID]
 	tl := e.Value.(*txnLock)
-	txn := &up.Txn
+	txn := up.Txn
 	ts := up.Txn.WriteTimestamp
 	_, beforeTs := tl.getLockHolder()
 	advancedTs := beforeTs.Less(ts)

pkg/kv/kvserver/concurrency/lock_table_test.go
@@ -1897,7 +1897,7 @@ func TestLockStateSafeFormat(t *testing.T) {
 	}
 	holder := &txnLock{}
 	l.holders.PushFront(holder)
-	holder.txn = &enginepb.TxnMeta{ID: uuid.NamespaceDNS}
+	holder.txn = enginepb.TxnMeta{ID: uuid.NamespaceDNS}
 	// TODO(arul): add something about replicated locks here too.
 	holder.unreplicatedInfo.init()
 	holder.unreplicatedInfo.ts = hlc.Timestamp{WallTime: 123, Logical: 7}

commit 1dd111942c98a558448cc3adc409431d98deb09c

concurrency: fix data race
This patch fixes  a data race that was introduced in 0da9146 between
the a transaction that holds a lock (`txnLocks.txn`) and a lock table
waiter waiting on that transaction (`waitingState.txn`). We do so by
storing a copy of the transaction being waited on, as opposed to sharing
a reference between these two objects. This is required because
transaction pushes and lock updates aren't synchronized -- this was the
case previously as well, except the structure of the code didn't make
this super obvious.

Closes #109271

Release note: None

pkg/kv/kvserver/concurrency/lock_table.go
@@ -96,11 +96,11 @@ type waitingState struct {
 	// Represents who the request is waiting for. The conflicting
 	// transaction may be a lock holder of a conflicting lock or a
 	// conflicting request being sequenced through the same lockTable.
-	txn           *enginepb.TxnMeta // always non-nil in waitFor{,Distinguished,Self} and waitElsewhere
-	key           roachpb.Key       // the key of the conflict
-	held          bool              // is the conflict a held lock?
-	queuedWriters int               // how many writers are waiting?
-	queuedReaders int               // how many readers are waiting?
+	txn           enginepb.TxnMeta // only set for waitFor{,Distinguished,Self} and waitElsewhere
+	key           roachpb.Key      // the key of the conflict
+	held          bool             // is the conflict a held lock?
+	queuedWriters int              // how many writers are waiting?
+	queuedReaders int              // how many readers are waiting?
 
 	// Represents the lock strength of the action that the request was trying to
 	// perform when it hit the conflict. E.g. was it trying to perform a (possibly
@@ -647,7 +647,12 @@ func (g *lockTableGuardImpl) updateWaitingStateLocked(newState waitingState) {
 func (g *lockTableGuardImpl) canElideWaitingStateUpdate(newState waitingState) bool {
 	// Note that we don't need to check newState.guardStrength as it's
 	// automatically assigned when updating the state.
-	return g.mu.state.kind == newState.kind && g.mu.state.txn == newState.txn &&
+	return g.mu.state.kind == newState.kind &&
+		// NB: The only field of interest to the pusher is the pushee transaction's
+		// ID. Other fields on the pushee's TxnMeta, such as its read timestamp or
+		// epoch, may be arbitrarily stale without being of material consequence.
+		// This allows us to elide updates just to these fields.
+		g.mu.state.txn.ID == newState.txn.ID &&
 		g.mu.state.key.Equal(newState.key) && g.mu.state.held == newState.held
 }
 
@@ -1804,7 +1809,9 @@ func (kl *keyLocks) informActiveWaiters() {
 	// However, if we naively plugged things into the current structure, it would
 	// either sit tight (because its waiting for itself) or, worse yet, push a
 	// transaction it's actually compatible with!
-	waitForState.txn, waitForState.held = kl.claimantTxn()
+	var claimantTxn *enginepb.TxnMeta
+	claimantTxn, waitForState.held = kl.claimantTxn()
+	waitForState.txn = *claimantTxn // store a copy of the claimant txn
 	findDistinguished := false
 	// We need to find a (possibly new) distinguished waiter if either:
 	//   There isn't one for this lock.
@@ -1818,7 +1825,7 @@ func (kl *keyLocks) informActiveWaiters() {
 		// be held by it. This is because if it were, this request would no longer
 		// be waiting in lock wait queues (via a call to releaseWritersFromTxn).
 		// This is asserted below.
-		kl.distinguishedWaiter.isSameTxn(waitForState.txn) {
+		kl.distinguishedWaiter.isSameTxn(&waitForState.txn) {
 		// Ensure that if we're trying to find a new distinguished waiter because
 		// all waiters on the lock are waiting on the (old) distinguished waiter,
 		// the lock is not held.
@@ -1861,7 +1868,7 @@ func (kl *keyLocks) informActiveWaiters() {
 		}
 		g := qg.guard
 		state := waitForState
-		if g.isSameTxn(waitForState.txn) {
+		if g.isSameTxn(&waitForState.txn) {
 			if waitForState.held {
 				panic("writer from the lock holder txn should not be waiting in a wait queue")
 			}
@@ -2234,8 +2241,8 @@ func (kl *keyLocks) constructWaitingState(g *lockTableGuardImpl) waitingState {
 	}
 	txn, held := kl.claimantTxn()
 	waitForState.held = held
-	waitForState.txn = txn
-	if g.isSameTxn(waitForState.txn) {
+	waitForState.txn = *txn // store a copy of the claimant txn.
+	if g.isSameTxn(&waitForState.txn) {
 		waitForState.kind = waitSelf
 	} else if kl.distinguishedWaiter == g {
 		waitForState.kind = waitForDistinguished
@@ -2905,7 +2912,7 @@ func (kl *keyLocks) tryClearLock(force bool) bool {
 			// themselves.
 			waitState := waitingState{
 				kind: waitElsewhere,
-				txn:  replicatedLockHolderTxn,
+				txn:  *replicatedLockHolderTxn, // store a copy of the replicated lock holder's txn
 				key:  kl.key,
 				held: true,
 			}

pkg/kv/kvserver/concurrency/lock_table_waiter.go
@@ -517,7 +517,7 @@ func (w *lockTableWaiterImpl) pushLockTxn(
 		log.Fatalf(ctx, "unexpected WaitPolicy: %v", req.WaitPolicy)
 	}
 
-	pusheeTxn, err := w.ir.PushTransaction(ctx, ws.txn, h, pushType)
+	pusheeTxn, err := w.ir.PushTransaction(ctx, &ws.txn, h, pushType)
 	if err != nil {
 		// If pushing with an Error WaitPolicy and the push fails, then the lock
 		// holder is still active. Transform the error into a LockConflictError.
@@ -699,7 +699,7 @@ func (w *lockTableWaiterImpl) pushRequestTxn(
 	pushType := kvpb.PUSH_ABORT
 	log.VEventf(ctx, 3, "pushing txn %s to detect request deadlock", ws.txn.Short())
 
-	_, err := w.ir.PushTransaction(ctx, ws.txn, h, pushType)
+	_, err := w.ir.PushTransaction(ctx, &ws.txn, h, pushType)
 	if err != nil {
 		return err
 	}
@@ -1183,7 +1183,7 @@ func (tag *contentionTag) notify(ctx context.Context, s waitingState) *kvpb.Cont
 		res := tag.generateEventLocked()
 		tag.mu.waiting = true
 		tag.mu.curStateKey = s.key
-		tag.mu.curStateTxn = s.txn
+		tag.mu.curStateTxn = &s.txn
 		// Accumulate the wait time.
 		now := tag.clock.PhysicalTime()
 		if !tag.mu.waitStart.IsZero() {
@@ -1261,7 +1261,7 @@ func newLockConflictErr(req Request, ws waitingState, reason kvpb.LockConflictEr
 	// TODO(nvanbenschoten): we should use the correct Lock strength of the lock
 	// holder, once we have it.
 	err := kvpb.NewError(&kvpb.LockConflictError{
-		Locks:  []roachpb.Lock{roachpb.MakeIntent(ws.txn, ws.key).AsLock()},
+		Locks:  []roachpb.Lock{roachpb.MakeIntent(&ws.txn, ws.key).AsLock()},
 		Reason: reason,
 	})
 	// TODO(nvanbenschoten): setting an error index can assist the KV client in
@@ -1279,10 +1279,6 @@ func newLockConflictErr(req Request, ws waitingState, reason kvpb.LockConflictEr
 }
 
 func canPushWithPriority(req Request, s waitingState) bool {
-	if s.txn == nil {
-		// Can't push a non-transactional request.
-		return false
-	}
 	var pushType kvpb.PushTxnType
 	if s.guardStrength == lock.None {
 		pushType = kvpb.PUSH_TIMESTAMP

pkg/kv/kvserver/concurrency/lock_table_waiter_test.go
@@ -319,7 +319,7 @@ func testWaitPush(t *testing.T, k waitKind, makeReq func() Request, expPushTS hl
 			req := makeReq()
 			g.state = waitingState{
 				kind:          k,
-				txn:           &pusheeTxn.TxnMeta,
+				txn:           pusheeTxn.TxnMeta,
 				key:           keyA,
 				held:          lockHeld,
 				guardStrength: lock.None,
@@ -404,7 +404,7 @@ func testWaitNoopUntilDone(t *testing.T, k waitKind, makeReq func() Request) {
 	txn := makeTxnProto("noop-wait-txn")
 	g.state = waitingState{
 		kind: k,
-		txn:  &txn.TxnMeta,
+		txn:  txn.TxnMeta,
 	}
 	g.notify()
 	defer notifyUntilDone(t, g)()
@@ -511,7 +511,7 @@ func testErrorWaitPush(
 			req := makeReq()
 			g.state = waitingState{
 				kind:          k,
-				txn:           &pusheeTxn.TxnMeta,
+				txn:           pusheeTxn.TxnMeta,
 				key:           keyA,
 				held:          lockHeld,
 				guardStrength: lock.None,
@@ -671,7 +671,7 @@ func testWaitPushWithTimeout(t *testing.T, k waitKind, makeReq func() Request) {
 				req := makeReq()
 				g.state = waitingState{
 					kind:          k,
-					txn:           &pusheeTxn.TxnMeta,
+					txn:           pusheeTxn.TxnMeta,
 					key:           keyA,
 					held:          lockHeld,
 					guardStrength: lock.Intent,
@@ -805,7 +805,7 @@ func TestLockTableWaiterIntentResolverError(t *testing.T) {
 		lockHeld := sync
 		g.state = waitingState{
 			kind:          waitForDistinguished,
-			txn:           &pusheeTxn.TxnMeta,
+			txn:           pusheeTxn.TxnMeta,
 			key:           keyA,
 			held:          lockHeld,
 			guardStrength: lock.Intent,
@@ -993,7 +993,7 @@ func TestContentionEventTracer(t *testing.T) {
 	h.notify(ctx, waitingState{
 		kind: waitForDistinguished,
 		key:  roachpb.Key("a"),
-		txn:  &txn1.TxnMeta,
+		txn:  txn1.TxnMeta,
 	})
 	require.Zero(t, h.tag.mu.lockWait)
 	require.NotZero(t, h.tag.mu.waitStart)
@@ -1023,7 +1023,7 @@ func TestContentionEventTracer(t *testing.T) {
 	h.notify(ctx, waitingState{
 		kind: waitFor,
 		key:  roachpb.Key("a"),
-		txn:  &txn1.TxnMeta,
+		txn:  txn1.TxnMeta,
 	})
 	require.Empty(t, events)
 	require.Zero(t, h.tag.mu.lockWait)
@@ -1035,7 +1035,7 @@ func TestContentionEventTracer(t *testing.T) {
 	h.notify(ctx, waitingState{
 		kind: waitFor,
 		key:  roachpb.Key("a"),
-		txn:  &txn2.TxnMeta,
+		txn:  txn2.TxnMeta,
 	})
 	require.Equal(t, time.Duration(21) /* 10+11 */, h.tag.mu.lockWait)
 	require.Len(t, events, 1)
@@ -1050,7 +1050,7 @@ func TestContentionEventTracer(t *testing.T) {
 	h.notify(ctx, waitingState{
 		kind: waitForDistinguished,
 		key:  roachpb.Key("b"),
-		txn:  &txn2.TxnMeta,
+		txn:  txn2.TxnMeta,
 	})
 	require.Equal(t, time.Duration(33) /* 10+11+12 */, h.tag.mu.lockWait)
 	require.Len(t, events, 2)
@@ -1067,7 +1067,7 @@ func TestContentionEventTracer(t *testing.T) {
 	h.notify(ctx, waitingState{
 		kind: waitFor,
 		key:  roachpb.Key("b"),
-		txn:  &txn1.TxnMeta,
+		txn:  txn1.TxnMeta,
 	})
 	manual.Advance(14)
 	h.notify(ctx, waitingState{