c0c971a44a722a6a0b106fe5a0e5029ed38ab0a2
sumeerbhola <sumeer@cockroachlabs.com>
2023-08-22 19:18:26 -0400

    admission: incorporate L0 size into overload score
    Small ingests happening at a high rate can elevate the sub-level count
    when small memtables need to be flushed (as observed in a clearrange
    roachtest). This causes admission control to restrict user-facing
    traffic, even when the bytes in L0 are modest and can all be compacted
    away very quickly.
    
    This PR updates the scoring logic to add a max and min sub-level bound.
    The max is the L0 bytes divided by admission.l0_min_size_per_sub_level,
    where the latter defaults to 5MB. The min is the sub-level count
    divided by 3.
    
    Fixes #109207
    
    Epic: none
    
    Release note (ops change): Cluster setting
    admission.l0_min_size_per_sub_level is introduced, which reduces the
    probability of admission control throttling when there is a sequence
    of small memtable flushes or tiny files ingested into L0.

---
+++ pkg/util/admission/admissionpb/io_threshold_test.go
@@ -0,0 +1,83 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package admissionpb
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIOThreshold(t *testing.T) {
+	var testCases = []struct {
+		iot   IOThreshold
+		score float64
+	}{
+		{
+			// Score is 30/20.
+			iot: IOThreshold{
+				L0NumSubLevels:           30,
+				L0NumSubLevelsThreshold:  20,
+				L0NumFiles:               200,
+				L0NumFilesThreshold:      1000,
+				L0Size:                   50,
+				L0MinimumSizePerSubLevel: 0,
+			},
+			score: 1.5,
+		},
+		// Score is 2500/1000.
+		{
+			iot: IOThreshold{
+				L0NumSubLevels:           30,
+				L0NumSubLevelsThreshold:  20,
+				L0NumFiles:               2500,
+				L0NumFilesThreshold:      1000,
+				L0Size:                   50,
+				L0MinimumSizePerSubLevel: 0,
+			},
+			score: 2.5,
+		},
+		// Max sub-levels is 5000/1000=5, and min sub-levels is 30/3=10. Actual
+		// sub-levels is 30. So we use 10 sub-levels and score is 10/20.
+		{
+			iot: IOThreshold{
+				L0NumSubLevels:           30,
+				L0NumSubLevelsThreshold:  20,
+				L0NumFiles:               250,
+				L0NumFilesThreshold:      1000,
+				L0Size:                   5000,
+				L0MinimumSizePerSubLevel: 1000,
+			},
+			score: 0.5,
+		},
+		// Max sub-levels is 5000/1000=5, and min sub-levels is 13/3=4. Actual
+		// sub-levels is 13. So we use 5 sub-levels and score is 5/20.
+		{
+			iot: IOThreshold{
+				L0NumSubLevels:           13,
+				L0NumSubLevelsThreshold:  20,
+				L0NumFiles:               250,
+				L0NumFilesThreshold:      1000,
+				L0Size:                   5000,
+				L0MinimumSizePerSubLevel: 1000,
+			},
+			score: 0.25,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%+v", tc.iot), func(t *testing.T) {
+			score, overloaded := tc.iot.Score()
+			require.Equal(t, tc.score > 1.0, overloaded)
+			require.Equal(t, tc.score, score)
+		})
+	}
+}
