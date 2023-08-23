b88a21202e27caaca1fd5ca228341ebc0fdbe071
Radu Berinde <radu@cockroachlabs.com>
2023-08-23 10:58:59 -0700

    github: code coverage workflows
    This change adds two GitHub Action workflows which run on each PR. One
    generates unit test code coverage data, and one publishes that data to
    a GCS bucket from where Reviewable can access it.
    
    We generate coverage data using `bazel coverage`, but we restrict it
    to only test the packages that have been modified by the PR.
    
    Two workflows are required for security (the first workflow runs
    potentially malicious code from a fork); for more details, see
    https://securitylab.github.com/research/github-actions-preventing-pwn-requests/
    
    Epic: none
    Release note: None

---
+++ build/ghactions/pr-codecov-run-tests.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+output_json_file="$1"
+packages="$2"
+
+if [ -z "${packages}" ]; then
+  echo "No packages; skipping"
+  touch "${output_json_file}"
+  exit 0
+fi
+
+
+# Find the targets. We need to convert from, e.g.
+#   pkg/util/log/logpb pkg/util/quotapool
+# to
+#   //pkg/util/log/logpb:* + //pkg/util/quotapool:*
+
+paths=""
+sep=""
+for p in ${packages}; do
+  paths="${paths}${sep}//$p:*"
+  sep=" + "
+done
+
+targets=$(bazel query "kind(\".*_test\", ${paths})")
+
+if [[ -z "${targets}" ]]; then
+  echo "No test targets found"
+  exit 0
+fi
+
+echo "Running tests"
+
+# TODO(radu): do we need --strip=never?
+bazel coverage \
+  --@io_bazel_rules_go//go/config:cover_format=lcov --combined_report=lcov \
+  --instrumentation_filter="//pkg/..." \
+  ${targets}
+
+lcov_file="$(bazel info output_path)/_coverage/_coverage_report.dat"
+if [ ! -f "${lcov_file}" ]; then
+  echo "Coverage file ${lcov_file} does not exist"
+  exit 1
+fi
+
+echo "Converting coverage file"
+bazel run @go_sdk//:bin/go -- run github.com/cockroachdb/code-cov-utils/lcov2json@v1.0.0 "${lcov_file}" "${output_json_file}"
