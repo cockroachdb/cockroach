843943d8ae7570f4232a204cc3cfa2a52c1b7cb9
wenyihu6 <wenyi.hu@cockroachlabs.com>
2023-08-25 04:26:36 -0400

    asim: add assertion, mutation-assertion events
    Previously, the simulator relied on test code to create functions to be called
    for delayed events. As we want to introduce more dynamic events through the
    randomized testing framework, this approach becomes increasingly complex and
    error-prone. In addition, we want to introduce new event types, such as
    assertion events and mutation-assertion events to validate the simulation’s
    state and verify the effects of prior mutation events.
    
    To achieve these goals, the following changes were made in this patch:
    1. Instead of directly adding functions to be invoked to simulator’s
    delayEventList, events are now organized into different struct
    (`SetSpanConfigEvent`, `AddNodeEvent`, `SetNodeLivenessEvent`, and
    `SetCapacityOverrideEvent`). Each struct is equipped to generate its
    corresponding function which can be called externally for event execution. These
    events can be scheduled with `StaticEvents` using `ScheduleEvent` method with
    just the required parameters.
    2. `AssertionEvent` struct can now be used to schedule assertion events as part
    of the simulation using `ScheduleEvent`.
    3. `MutationWithAssertionEvent` can also be used now to schedule mutation events
    coupled with subsequent assertion events using the
    `ScheduleMutationWithAssertionEvent` method.
    
    Under the hood, these events all implement the event interface. This interface
    outlines
    1. Func() returns a function that can be called externally.
    2. String() returns a formatted output string. To accommodate varying function
    types returned by `Func()`, we introduced another interface for different
    function types to be used as an event func.
    
    When event executor executes these events at their scheduled time, it retrieves
    the functions calling Func() and calls them with the simulator's current state
    and history.
    
    Part of: #106192
    Release note: none

---
+++ pkg/kv/kvserver/asim/gen/event_generator.go
@@ -0,0 +1,113 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package gen
+
+import (
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/config"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/event"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/scheduled"
+)
+
+// EventGen provides a  method to generate a list of events that will apply to
+// the simulated cluster. Currently, only delayed (fixed time) events are
+// supported.
+type EventGen interface {
+	// Generate returns an eventExecutor storing a sorted list of events and
+	// being ready execute events for the simulation execution.
+	Generate(seed int64, settings *config.SimulationSettings) scheduled.EventExecutor
+	// String returns the concise string representation of the event executor,
+	// detailing the number of scheduled events.
+	String() string
+}
+
+// StaticEvents implements the EventGen interface. For proper initialization,
+// please use NewStaticEventsWithNoEvents constructor instead of direct struct
+// literal assignment.
+// TODO(kvoli): introduce conditional events.
+type StaticEvents struct {
+	// eventExecutor handles the registration of scheduled event and ensures
+	// they are sorted chronologically.
+	eventExecutor scheduled.EventExecutor
+}
+
+// NewStaticEventsWithNoEvents is StaticEvents's constructor. It ensures that
+// both the eventExecutor interface and its underlying struct are initialized
+// and non-nil.
+func NewStaticEventsWithNoEvents() StaticEvents {
+	return StaticEvents{
+		eventExecutor: scheduled.NewExecutorWithNoEvents(),
+	}
+}
+
+// ScheduleEvent registers the event with the eventExecutor scheduled at
+// startTime.Add(delay). After registration, events remain unsorted by their
+// order until Generate() is called.
+func (se StaticEvents) ScheduleEvent(startTime time.Time, delay time.Duration, event event.Event) {
+	if se.eventExecutor == nil {
+		panic("StaticEvents.eventExecutor is a nil interface; " +
+			"use NewStaticEventsWithNoEvents for proper initialization.")
+	}
+	se.eventExecutor.RegisterScheduledEvent(scheduled.ScheduledEvent{
+		At:          startTime.Add(delay),
+		TargetEvent: event,
+	})
+}
+
+// ScheduleMutationWithAssertionEvent registers the mutation event with the
+// eventExecutor scheduled at startTime.Add(delay) followed by the assertion
+// event scheduled DurationToAssert after executing the mutation event at
+// startTime.Add(delay).Add(DurationToAssert).
+func (se StaticEvents) ScheduleMutationWithAssertionEvent(
+	startTime time.Time, delay time.Duration, event event.MutationWithAssertionEvent,
+) {
+	if se.eventExecutor == nil {
+		panic("StaticEvents.eventExecutor is a nil interface; " +
+			"use NewStaticEventsWithNoEvents for proper initialization.")
+	}
+	if err := event.Validate(); err != nil {
+		panic(err)
+	}
+
+	se.eventExecutor.RegisterScheduledEvent(scheduled.ScheduledEvent{
+		At:          startTime.Add(delay),
+		TargetEvent: event.MutationEvent,
+	})
+
+	se.eventExecutor.RegisterScheduledEvent(scheduled.ScheduledEvent{
+		At:          startTime.Add(delay).Add(event.DurationToAssert),
+		TargetEvent: event.AssertionEvent,
+	})
+
+}
+
+// String returns the concise string representation of the event executor,
+// detailing the number of scheduled events.
+func (se StaticEvents) String() string {
+	if se.eventExecutor == nil {
+		panic("StaticEvents.eventExecutor is a nil interface; " +
+			"use NewStaticEventsWithNoEvents for proper initialization.")
+	}
+	return se.eventExecutor.PrintEventSummary()
+}
+
+// Generate returns an eventExecutor populated with a sorted list of events. It
+// is now prepared to execute events for the simulation execution.
+func (se StaticEvents) Generate(
+	seed int64, settings *config.SimulationSettings,
+) scheduled.EventExecutor {
+	if se.eventExecutor == nil {
+		panic("StaticEvents.eventExecutor is a nil interface; " +
+			"use NewStaticEventsWithNoEvents for proper initialization.")
+	}
+	return se.eventExecutor
+}
