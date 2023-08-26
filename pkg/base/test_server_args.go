6e5586a463e21d2edac3de4cf0f60a1033ca73a5
Raphael 'kena' Poss <knz@thaumogen.net>
2023-08-26 12:22:12 +0200

    base: better document some options
    Release note: None

--- pkg/base/test_server_args.go
+++ pkg/base/test_server_args.go
@@ -153,12 +153,13 @@ type TestServerArgs struct {
 	// config span.
 	DisableSpanConfigs bool
 
-	// TestServer will probabilistically start a single test tenant on each node
-	// for multi-tenant testing, and default all connections through that tenant.
-	// Use this flag to change this behavior. You might want/need to alter this
-	// behavior if your test case is already leveraging tenants, or if some of the
-	// functionality being tested is not accessible from within tenants. See
-	// DefaultTestTenantOptions for alternative options that suits your test case.
+	// DefaultTestTenant determines whether a test's application
+	// workload will be redirected to a virtual cluster (secondary
+	// tenant) automatically.
+	//
+	// The default behavior is to do this probabilistically. See
+	// DefaultTestTenantOptions and its various instances defined
+	// below for alternative options that suits your test case.
 	DefaultTestTenant DefaultTestTenantOptions
 
 	// StartDiagnosticsReporting checks cluster.TelemetryOptOut(), and
@@ -215,13 +216,14 @@ type TestClusterArgs struct {
 	ReusableListenerReg *listenerutil.ListenerRegistry
 }
 
-// DefaultTestTenantOptions specifies the conditions under which the default
-// test tenant will be started.
+// DefaultTestTenantOptions specifies the conditions under which a
+// virtual cluster (secondary tenant) will be automatically started to
+// bear load for a unit test.
 type DefaultTestTenantOptions struct {
 	testBehavior testBehavior
 
 	// Whether the testserver will allow or block attempts to create
-	// additional secondary tenants. (Default is to block.)
+	// additional virtual clusters. (Default is to block.)
 	allowAdditionalTenants bool
 
 	// If test tenant is disabled, issue and label to link in log message.
@@ -238,16 +240,20 @@ const (
 )
 
 var (
-	// TestTenantProbabilisticOnly will start the default test tenant on a
-	// probabilistic basis. It will also prevent the starting of additional
-	// tenants by raising an error if it is attempted.
-	// This is the default behavior.
+	// TestTenantProbabilisticOnly starts the test under a virtual
+	// cluster on a probabilistic basis. It will also prevent the
+	// starting of additional virtual clusters by raising an error if it
+	// is attempted. This is the default behavior.
 	TestTenantProbabilisticOnly = DefaultTestTenantOptions{testBehavior: ttProb, allowAdditionalTenants: false}
-	// TestTenantProbabilistic will start the default test tenant on a
-	// probabilistic basis. It allows the starting of additional tenants.
+
+	// TestTenantProbabilisticOnly starts the test under a virtual
+	// cluster on a probabilistic basis. It allows the starting of
+	// additional virtual clusters.
 	TestTenantProbabilistic = DefaultTestTenantOptions{testBehavior: ttProb, allowAdditionalTenants: true}
-	// TestTenantAlwaysEnabled will always start the default test tenant. This is useful
-	// for quickly verifying that a test works with tenants enabled.
+
+	// TestTenantAlwaysEnabled will always redirect the test workload to
+	// a virtual cluster. This is useful for quickly verifying that a
+	// test works under cluster virtualization.
 	//
 	// Note: this value should not be used for checked in test code
 	// unless there is a good reason to do so. We want the common case
