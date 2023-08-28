30b176b7668bde4d25aec2c4bbd6b26e3856200b
Raphael 'kena' Poss <knz@thaumogen.net>
2023-08-28 14:52:57 +0200

    serverutils: implement the testserver interfaces indirectly
    TLDR: unit tests using TestServer/TestCluster now produce warning and
    notice messages upon certain suspicious API usage.
    
    For example:
    ```
    $ dev test //pkg/server/storage_api -f TestStatusGetFiles
    === RUN   TestStatusGetFiles
        ...
            pkg/server/storage_api/storage_api_test_test/pkg/server/storage_api/files_test.go:46: (TestStatusGetFiles)
                    NOTICE: .GetStatusClient() called via implicit interface ApplicationLayerInterface;
            HINT: consider using .ApplicationLayer().GetStatusClient() instead.
        conditional_wrap.go:193: TIP: consider replacing the test server initialization from:
    
                ts, db, kvDB := serverutils.StartServer(t, ...)
                // or:
                tc := serverutils.StartCluster(t, ...)
                ts := tc.Server(0)
    
            To:
    
                srv, db, kvDB := serverutils.StartServer(t, ...)
                defer srv.Stop(...)
                ts := srv.ApplicationLayer()
                // or:
                tc := serverutils.StartCluster(t, ...)
                ts := tc.Server(0).ApplicationLayer()
    ```
    
    The following warnings/notices are implemented.
    
    **ApplicationLayerInterface**
    
    The problem here is that the implicit implementation of
    `ApplicationLayerInterface` inside `TestServerInterface` is likely
    misused. If/when the test server automatically starts a secondary
    tenant, the *implicit* `ApplicationLayerInterface` refers to the
    system tenant, whereas only `.ApplicationLayer()` refers to the
    secondary tenant. It's likely that the test code should use the
    latter.
    
    For this, a warning is printed when a method from the *implicit*
    `ApplicationLayerInterface` is called.
    
    The warning is not printed if the test has indicated (via
    `DefaultTestTenant: base.TestIsForStorageLayerAndNeedsASystemTenant`)
    that it is ever only interested in the storage layer.
    
    **StorageLayerInterface**
    
    We want to promote the use of the explicit interface accessor
    `.StorageLayer()` to access the methods of `StorageLayerInterface`.
    
    For this, a notice is printed when a method from the *implicit*
    `StorageLayerInterface` is called.
    
    The notice is not printed if the test has indicated (via
    `DefaultTestTenant: base.TestIsForStorageLayerAndNeedsASystemTenant`)
    that it is ever only interested in the storage layer.
    
    **TenantControlInterface**
    
    We want to promote the use of the explicit interface accessor
    `.TenantController()` to access the methods of
    `TenantControlInterface`.
    
    For this, a notice is printed when a method from the *implicit*
    `TenantControlInterface` is called.
    
    Release note: None

--- pkg/testutils/serverutils/test_server_shim.go
+++ pkg/testutils/serverutils/test_server_shim.go
@@ -101,9 +101,9 @@ func ShouldStartDefaultTestTenant(
 		}
 		if v {
 			t.Log(defaultTestTenantMessage + "\n(override via COCKROACH_TEST_TENANT)")
-			return base.TestTenantAlwaysEnabled
+			return base.InternalNonDefaultDecision(baseArg, true)
 		}
-		return base.InternalNonDefaultDecision
+		return base.InternalNonDefaultDecision(baseArg, false)
 	}
 
 	if globalDefaultSelectionOverride.isSet {
@@ -126,9 +126,9 @@ func ShouldStartDefaultTestTenant(
 		t.Log(defaultTestTenantMessage)
 	}
 	if enabled {
-		return base.TestTenantAlwaysEnabled
+		return base.InternalNonDefaultDecision(baseArg, true)
 	}
-	return base.InternalNonDefaultDecision
+	return base.InternalNonDefaultDecision(baseArg, false)
 }
 
 // globalDefaultSelectionOverride is used when an entire package needs
@@ -190,6 +190,13 @@ func StartServerOnlyE(t TestLogger, params base.TestServerArgs) (TestServerInter
 		return nil, err
 	}
 
+	if t != nil {
+		if w, ok := s.(*wrap); ok {
+			// Redirect the info/warning messages to the test logs.
+			w.loggerFn = t.Logf
+		}
+	}
+
 	ctx := context.Background()
 
 	if err := s.Start(ctx); err != nil {
@@ -239,13 +246,14 @@ func NewServer(params base.TestServerArgs) (TestServerInterface, error) {
 	}
 	tcfg := params.DefaultTestTenant
 	if !(tcfg.TestTenantAlwaysEnabled() || tcfg.TestTenantAlwaysDisabled()) {
-		return nil, errors.AssertionFailedf("programming error: DefaultTestTenant does not contain a decision\n(maybe call ShouldStartDefaultTestTennat?)")
+		return nil, errors.AssertionFailedf("programming error: DefaultTestTenant does not contain a decision\n(maybe call ShouldStartDefaultTestTenant?)")
 	}
 
 	srv, err := srvFactoryImpl.New(params)
 	if err != nil {
 		return nil, err
 	}
+	srv = wrapTestServer(srv.(TestServerInterfaceRaw), tcfg)
 	return srv.(TestServerInterface), nil
 }
 
