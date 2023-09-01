6e68bdafa7aa5b0f1e507af16bb7b272c881adce
Rafi Shamim <rafi@cockroachlabs.com>
2023-09-01 11:29:30 -0400

    roachtest: update version of nodejs used by ORM tests
    Node 16 no longer is supported, so installation of it fails. Now we
    update to a supported version.
    
    Release note: None

--- pkg/cmd/roachtest/tests/typeorm.go
+++ pkg/cmd/roachtest/tests/typeorm.go
@@ -90,7 +90,7 @@ func registerTypeORM(r registry.Registry) {
 			c,
 			node,
 			"add nodesource repository",
-			`sudo apt install ca-certificates && curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -`,
+			`sudo apt install ca-certificates && curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -`,
 		); err != nil {
 			t.Fatal(err)
 		}
