a6e97968ef94e5ffb826d885e9ba432a8eca3d37
sumeerbhola <sumeer@cockroachlabs.com>
2023-09-01 11:55:43 -0400

    requestbatcher,intentresolver: default to no inflight backpressure
    For point and ranged intent resolution, the concurrency is already
    controlled by the number of goroutines that are waiting to resolve
    intents. The one exception is when there are a huge number of point
    intents being resolved by a single waiter (which should be very rare
    since if those intents are from a single txn, they should be ranged
    intents), but in that case we have already paid the memory cost of
    buffering these intents, so we may as well send the intent resolution
    RPCs.
    
    In case we have overlooked something, inflight backpressure can be
    restored via a cluster setting.
    
    This change is a prerequisite simplification for doing admission control
    on intent resolution. We don't want an inflight RPC limit to be reached
    due to admission control being active on a node/store, starving out
    intent resolution requests for other stores. There is also the async
    task limit of IntentResolver.sem, but many cases that hit that limit
    fallback to synchronous waiting (allowSyncProcessing=true), so that
    limit is not considered harmful.
    
    Informs #97108
    
    Epic: CRDB-25458
    
    Release note (ops change): A cluster setting is introduced
    (kv.intent_resolver.batcher.in_flight_backpressure_limit.enabled),
    that defaults to false, and decides whether an in-flight RPC limit is
    enforced on intent resolution RPCs.

--- pkg/kv/kvserver/store.go
+++ pkg/kv/kvserver/store.go
@@ -2004,6 +2004,7 @@ func (s *Store) Start(ctx context.Context, stopper *stop.Stopper) error {
 		AmbientCtx:           s.cfg.AmbientCtx,
 		TestingKnobs:         s.cfg.TestingKnobs.IntentResolverKnobs,
 		RangeDescriptorCache: intentResolverRangeCache,
+		Settings:             s.cfg.Settings,
 	})
 	s.metrics.registry.AddMetricStruct(s.intentResolver.Metrics)
 
