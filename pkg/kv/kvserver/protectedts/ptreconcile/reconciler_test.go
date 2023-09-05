6af0e3f847b9202f1ce737c7ca7c10fb9fa7a496
Raphael 'kena' Poss <knz@thaumogen.net>
2023-09-05 15:11:11 +0200

    protectedts/ptreconcile: fix and simplify test
    Release note: None

--- pkg/kv/kvserver/protectedts/ptreconcile/reconciler_test.go
+++ pkg/kv/kvserver/protectedts/ptreconcile/reconciler_test.go
@@ -26,8 +26,9 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/isql"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
-	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 	"github.com/cockroachdb/errors"
@@ -39,18 +40,18 @@ func TestReconciler(t *testing.T) {
 
 	ctx := context.Background()
 	testutils.RunTrueAndFalse(t, "reconciler", func(t *testing.T, withDeprecatedSpans bool) {
-		tc := testcluster.StartTestCluster(t, 1, base.TestClusterArgs{
-			ServerArgs: base.TestServerArgs{
-				Knobs: base.TestingKnobs{
-					ProtectedTS: &protectedts.TestingKnobs{DisableProtectedTimestampForMultiTenant: withDeprecatedSpans},
-				},
+		defer log.Scope(t).Close(t)
+
+		srv := serverutils.StartServerOnly(t, base.TestServerArgs{
+			Knobs: base.TestingKnobs{
+				ProtectedTS: &protectedts.TestingKnobs{DisableProtectedTimestampForMultiTenant: withDeprecatedSpans},
 			},
 		})
-		defer tc.Stopper().Stop(ctx)
+		defer srv.Stopper().Stop(ctx)
+		s0 := srv.ApplicationLayer()
 
 		// Now I want to create some artifacts that should get reconciled away and
 		// then make sure that they do and others which should not do not.
-		s0 := tc.Server(0)
 		insqlDB := s0.InternalDB().(isql.DB)
 		ptp := s0.ExecutorConfig().(sql.ExecutorConfig).ProtectedTimestampProvider
 		pts := ptstorage.WithDatabase(ptp, insqlDB)
@@ -74,7 +75,7 @@ func TestReconciler(t *testing.T) {
 					return shouldRemove, nil
 				},
 			})
-		require.NoError(t, r.StartReconciler(ctx, s0.Stopper()))
+		require.NoError(t, r.StartReconciler(ctx, s0.AppStopper()))
 		recMeta := "a"
 		rec1 := ptpb.Record{
 			ID:        uuid.MakeV4().GetBytes(),
