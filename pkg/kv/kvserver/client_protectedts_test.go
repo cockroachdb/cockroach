6990c77c208c57ff8e1e30b86aa4dd72addbfd91
Pavel Kalinnikov <pavel@cockroachlabs.com>
2023-09-05 20:15:23 +0000

    kvserver: propagate GCHint for partial deletions
    Previously, GCHint was used for 3 purposes:
    1. Instruct GC to run at specific times. This is used by SQL table/index
       drop jobs, to limit the amount of time they wait on data GC.
    2. Hint GC that a range is fully covered by range tombstones, so that GC
       can use ClearRange for faster deletes. This is useful for bulk data
       deletions as well.
    3. Hint GC that a range was fully covered by range tombstones at some
       recent point. GC prioritizes going through such ranges, and processes
       them together, because this makes Pebble compaction more efficient.
    
    The use-case 1 was broken. If a range tombstone does not fully cover the
    Range keyspace, GCHint is not written. Correspondingly, for small
    table/index deletions (spanning less than a single range), or deletions
    that don't perfectly match range bounds, there will be some ranges with
    no hints. For these ranges, GC might be arbitrarily delayed, and the
    schema change jobs would be stuck.
    
    In addition, GCHint propagation during merges was lossy. It could drop
    the hint if either LHS or RHS doesn't have a hint, or has some data.
    
    This commit extends GCHint to fix the use-case 1. Two fields are added:
    min and max deletion timestamp. These fields are populated even for
    partial range deletions, and are propagated during splits and merges
    unconditionally. The choice of min-max approach is explained in the
    comments to the corresponding GCHint fields.
    
    Release note (bug fix): Fixed a bug that could occasionally cause schema
    change jobs (e.g. table/index drops) to appear stuck in state "waiting
    for MVCC GC" for much longer than expected. The fix only applies to
    future schema changes -- existing stuck jobs can be processed by
    manually force-enqueueing the relevant ranges in the MVCC GC queue under
    the DB Console's advanced debug page.
    
    Epic: none

--- pkg/kv/kvserver/client_protectedts_test.go
+++ pkg/kv/kvserver/client_protectedts_test.go
@@ -41,6 +41,7 @@ import (
 // It works by writing a lot of data and waiting for the GC heuristic to allow
 // for GC. Because of this, it's very slow and expensive. It should
 // potentially be made cheaper by injecting hooks to force GC.
+// TODO(pavelkalinnikov): use the GCHint for this.
 //
 // Probably this test should always be skipped until it is made cheaper,
 // nevertheless it's a useful test.
