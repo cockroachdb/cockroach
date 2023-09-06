98d45afef02d80f900dad25ad933eb7c91349afe
Darin Peshev <darinp@gmail.com>
2023-09-06 01:47:22 +0000

    server: sql listen before tenant id set
    Previously, running SQL tenant processes required to specify the tenant
    id as command flag. That prevented starting an SQL tenant process before
    the tenant id associated is known and made it impossible to have a pool
    of generic warm pods waiting to be associated with a tenant. Not being
    able to have such pool makes also the cold start latency include the
    cost of starting a new SQL tenant process. It was also possible that a
    tenant connection is forwarded to a newly started tenant SQL process
    before its SQL listener is on so clients may get refused connections. To
    solve the problem, this PR adds a new flag that allows the tenant id to
    be set by reading a file after the process starts. Such file, will be
    empty when the SQL process starts and will be populated when the SQL
    process is to be associated with a specific tenant. When the tenant id
    is set through a file, and the file is empty at startup, the SQL process
    will block and wait for the tenant id to be written in the file. This is
    combined with a change where the SQL listener is created before the
    tenant id is read from the file. This eliminates the posibility that a
    client connection may be refused if it is forwarded quickly to the SQL
    process.
    
    Epic: CC-8504
    
    Release note: None

---
+++ pkg/server/tenant_delayed_id_set_test.go
@@ -0,0 +1,110 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package server
+
+import (
+	"context"
+	"net/url"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/security/username"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/jackc/pgx/v4"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStartTenantWithDelayedID(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	ctx := context.Background()
+
+	clusterSettings := cluster.MakeTestingClusterSettings()
+	args := base.TestServerArgs{
+		Settings:          clusterSettings,
+		DefaultTestTenant: base.TestControlsTenantsExplicitly,
+	}
+	s := serverutils.StartServerOnly(t, args)
+	defer s.Stopper().Stop(ctx)
+
+	func() {
+		// Create the tenant.
+		tenantStopper := stop.NewStopper()
+		defer tenantStopper.Stop(ctx)
+		_, db := serverutils.StartTenant(t, s, base.TestTenantArgs{
+			Stopper:  tenantStopper,
+			TenantID: serverutils.TestTenantID()})
+		defer db.Close()
+	}()
+
+	st := cluster.MakeTestingClusterSettings()
+	baseCfg := makeTestBaseConfig(st, s.Stopper().Tracer())
+	sqlCfg := makeTestSQLConfig(st, roachpb.TenantID{})
+	sqlCfg.TenantLoopbackAddr = s.AdvRPCAddr()
+
+	var tenantIDSet, listenerReady sync.WaitGroup
+	tenantIDSet.Add(1)
+	listenerReady.Add(1)
+
+	var timeTenantIDSet time.Time
+	sqlCfg.DelayedSetTenantID = func() (roachpb.TenantID, error) {
+		// Unblock the connect code bellow, so it can try to connect.
+		listenerReady.Done()
+		// Wait until getting a go ahead with setting the tenant id.
+		tenantIDSet.Wait()
+		return serverutils.TestTenantID(), nil
+	}
+
+	go func() {
+		sw, err := NewSeparateProcessTenantServer(
+			ctx,
+			s.Stopper(),
+			baseCfg,
+			sqlCfg,
+			roachpb.NewTenantNameContainer("delayed-tenant-set"),
+		)
+		require.NoError(t, err)
+		require.NoError(t, sw.Start(ctx))
+	}()
+
+	listenerReady.Wait()
+	// Try a connection.
+	pgURL, cleanupFn, err := sqlutils.PGUrlE(
+		baseCfg.SQLAdvertiseAddr, "testConn", url.User(username.RootUser))
+	require.NoError(t, err)
+	defer cleanupFn()
+	go func() {
+		time.Sleep(200 * time.Millisecond)
+		timeTenantIDSet = timeutil.Now()
+		tenantIDSet.Done()
+	}()
+	c, err := pgx.Connect(ctx, pgURL.String())
+	durationFromTenantIDSetToConnect := timeutil.Since(timeTenantIDSet)
+	require.NoError(t, err)
+	defer func() { _ = c.Close(ctx) }()
+	t.Logf("cold connect duration (from tenant id set to connect) %s", durationFromTenantIDSetToConnect)
+
+	connectStart := timeutil.Now()
+	c, err = pgx.Connect(ctx, pgURL.String())
+	warmConnectDuration := timeutil.Since(connectStart)
+	require.NoError(t, err)
+	defer func() { _ = c.Close(ctx) }()
+	t.Logf("warm connect duration %s", warmConnectDuration)
+}
