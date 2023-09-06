482026d1ef461acf258e049a6cd26d13eba5d2e9
Andy Yang <yang@cockroachlabs.com>
2023-09-06 02:18:30 -0400

    sql: add CREATEDB system privilege
    Release note (sql change): There is now a `CREATEDB` system privilege,
    which is analogous to the existing `CREATEDB` role option, but can
    also be inherited by role membership.

--- pkg/ccl/backupccl/restore_planning.go
+++ pkg/ccl/backupccl/restore_planning.go
@@ -56,7 +56,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgnotice"
 	"github.com/cockroachdb/cockroach/pkg/sql/privilege"
-	"github.com/cockroachdb/cockroach/pkg/sql/roleoption"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/catconstants"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/catid"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
@@ -1556,7 +1555,7 @@ func checkPrivilegesForRestore(
 			"RESTORE system privilege.", deprecatedPrivilegesRestorePreamble, p.User().Normalized())
 		p.BufferClientNotice(ctx, pgnotice.Newf("%s", notice))
 
-		hasCreateDB, err := p.HasRoleOption(ctx, roleoption.CREATEDB)
+		hasCreateDB, err := p.HasGlobalPrivilegeOrRoleOption(ctx, privilege.CREATEDB)
 		if err != nil {
 			return err
 		}
