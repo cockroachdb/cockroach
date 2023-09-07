4bbd67269ced0587189bf19f408f781d88561503
Rafi Shamim <rafi@cockroachlabs.com>
2023-09-07 00:10:21 -0400

    acceptance: use password in ComposeGSS test
    It appears that upgrading the docker version has somehow made this test
    unable to login using certs like it was before. That its not really the
    purpose of the test, so we can use the password, just like how TestGSS
    (in the same file) was doing already.
    
    Release note: None

--- pkg/acceptance/compose/gss/psql/gss_test.go
+++ pkg/acceptance/compose/gss/psql/gss_test.go
@@ -190,7 +190,7 @@ func TestGSS(t *testing.T) {
 }
 
 func TestGSSFileDescriptorCount(t *testing.T) {
-	rootConnector, err := pq.NewConnector("user=root sslmode=require")
+	rootConnector, err := pq.NewConnector("user=root password=rootpw sslmode=require")
 	if err != nil {
 		t.Fatal(err)
 	}
