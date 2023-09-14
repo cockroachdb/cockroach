3adef4e61629336ddcf6c3002daa46c5bffac306
Rafi Shamim <rafi@cockroachlabs.com>
2023-09-14 14:33:21 -0400

    build: give a longer timeout to column changes in RSG tests
    The code intended for column change operations to have a longer timeout
    than others. However, this was not reflected in the script that runs the
    tests.
    
    Release note: None

--- build/teamcity/cockroach/nightlies/random_syntax_tests_impl.sh
+++ build/teamcity/cockroach/nightlies/random_syntax_tests_impl.sh
@@ -10,7 +10,7 @@ BAZEL_BIN=$(bazel info bazel-bin --config=ci)
 exit_status=0
 $BAZEL_BIN/pkg/cmd/bazci/bazci_/bazci -- test --config=ci \
     //pkg/sql/tests:tests_test \
-    --test_arg -rsg=5m --test_arg -rsg-routines=8 --test_arg -rsg-exec-timeout=1m \
+    --test_arg -rsg=5m --test_arg -rsg-routines=8 --test_arg -rsg-exec-timeout=1m --test_arg -rsg-exec-column-change-timeout=90s \
     --test_timeout 3600 --test_filter 'TestRandomSyntax' \
     --test_sharding_strategy=disabled \
     || exit_status=$?
