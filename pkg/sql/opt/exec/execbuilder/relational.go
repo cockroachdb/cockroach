3c74c326819e5fec9447471390a1d5fa80afb02b
Drew Kimball <drewk@cockroachlabs.com>
2023-09-20 14:12:33 -0600

    plpgsql: implement rollback behavior for EXCEPTION blocks
    When an exception occurs within the EXCEPTION block of a PLpgSQL routine,
    all changes to database state are rolled back, but the updated values for
    the variables are still visible to the exception handler. For example:
    ```
    CREATE FUNCTION f() RETURNS INT AS $$
      DECLARE
        i INT;
      BEGIN
        INSERT INTO xy VALUES (-100, -100);
        i := 100;
        RETURN 1 // 0;
      EXCEPTION WHEN division_by_zero THEN
        RETURN i;
      END
    $$ LANGUAGE PLpgSQL;
    ```
    The above function would not modify `xy`, since the exception would
    revert the insert. However, the function would return `1`, since the
    variable assignment occurred before the error was thrown.
    
    This patch implements the required rollback behavior through internal
    savepoints; when the top-level routine of a PLpgSQL block begins
    executing, it creates a savepoint (if it has an exception handler).
    When the exception handler catches an error, it calls `RollbackSavepoint`
    to revert any changes to database state within the scope of the block.
    
    Fixes #105253
    
    Release note: None

--- pkg/sql/opt/exec/execbuilder/relational.go
+++ pkg/sql/opt/exec/execbuilder/relational.go
@@ -3149,7 +3149,7 @@ func (b *Builder) buildCall(c *memo.CallExpr) (execPlan, error) {
 		udf.Def.SetReturning,
 		udf.TailCall,
 		true, /* procedure */
-		nil,  /* exceptionHandler */
+		nil,  /* blockState */
 	)
 
 	var ep execPlan
