2a842dbaf6fbc9de2a52f0efc0b6dff21777c179
Raphael 'kena' Poss <knz@thaumogen.net>
2023-09-25 16:01:08 +0200

    tenantsettingswatcher: extend override store with alternate defaults
    As an end-goal, we want that when there is no override in
    `system.tenant_settings`, i.e. the rangefeed over this table has
    nothing to stay, we pick up the current value of `TenantReadOnly`
    settings from `system.settings` instead to feed to virtual clusters.
    
    To achieve this, we need to merge the current `TenantReadOnly` values
    "underneath" the tenant overrides, i.e. use the current
    `TenantReadOnly` as default override value when the rangefeed over
    `tenant_settings` has nothing to say.
    
    This commit updates the overrides store accordingly. It does so by
    maintaining an `alternateDefaults` override slice, updated from
    changes to `TenantReadOnly` settings from `system.settings`.
    
    Then, updates to the overrides map (in `tenantOverrides`) for the
    special tenant ID `allTenantOverridesID` are "merged" from
    `alternateDefaults` with the following rule:
    
    - when a change to `alternateDefaults` is made, the current
      `allTenantOverridesID` override slice is updated for each
      setting *not* currently set from the rangefeed (those missing from the
      `explicitKeys` map).
    
    - when an *addition* or *modification* is observed from the
      rangefeed (for tenant ID `allTenantOverridesID`), the overrides slice
      is updated with the rangefeed value, and the rangefeed setting key is
      added to `explicitKeys`.
    
    - when a *deletion* is observed from the rangefeed, the key is removed
      from `explicitKeys` and any override from `alternateDefaults` is
      inserted instead.
    
    Note that entries are never deleted from `alternateDefaults`, even
    when a TenantReadOnly customization is removed from
    `system.settings` (i.e. the callback `setAlternateDefaults` is given a
    smaller slice than the current `alternateDefaults`). This is on
    purpose: to properly support mixed-version deployments, we want
    that SQL servers observe the storage layer's idea of what the default
    value of a TenantReadOnly setting should be. To achieve this, it's
    important that the storage server always reports an override for every
    TenantReadOnly setting.
    
    Release note: None

--- pkg/server/tenantsettingswatcher/overrides_store_test.go
+++ pkg/server/tenantsettingswatcher/overrides_store_test.go
@@ -20,7 +20,9 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/stretchr/testify/require"
 )
 
 func TestOverridesStore(t *testing.T) {
@@ -31,14 +33,6 @@ func TestOverridesStore(t *testing.T) {
 	s.Init()
 	t1 := roachpb.MustMakeTenantID(1)
 	t2 := roachpb.MustMakeTenantID(2)
-	st := func(key settings.InternalKey, val string) kvpb.TenantSetting {
-		return kvpb.TenantSetting{
-			InternalKey: key,
-			Value: settings.EncodedValue{
-				Value: val,
-			},
-		}
-	}
 	expect := func(o *tenantOverrides, expected string) {
 		t.Helper()
 		var vals []string
@@ -89,4 +83,184 @@ func TestOverridesStore(t *testing.T) {
 	s.setTenantOverride(ctx, t3, st("x", "xx"))
 	o3 := s.getTenantOverrides(ctx, t3)
 	expect(o3, "x=xx")
+
+	// Verify that overrides also work for the special "all tenants" ID.
+	expect(s.getTenantOverrides(ctx, allTenantOverridesID), "")
+	s.setTenantOverride(ctx, allTenantOverridesID, st("a", "aa"))
+	s.setTenantOverride(ctx, allTenantOverridesID, st("d", "dd"))
+	expect(s.getTenantOverrides(ctx, allTenantOverridesID), "a=aa d=dd")
+
+	// Set an alternate default.
+	s.setAlternateDefaults(ctx, []kvpb.TenantSetting{st("d", "d2")})
+
+	// If a custom override is provided, the alternate default is
+	// hidden.
+	expect(s.getTenantOverrides(ctx, allTenantOverridesID), "a=aa d=dd")
+
+	// Alternate defaults do not show up for regular tenant IDs.
+	expect(s.getTenantOverrides(ctx, t3), "x=xx")
+
+	// If the custom override is removed, the alternate appears.
+	s.setTenantOverride(ctx, allTenantOverridesID, kvpb.TenantSetting{InternalKey: "d"})
+	expect(s.getTenantOverrides(ctx, allTenantOverridesID), "a=aa d=d2")
+
+	// If there was no override to start with, the new alternate is
+	// added as pseudo-override. If not mentioned in new calls to
+	// `setAlternateDefaults()`, existing alternate defaults are
+	// preserved.
+	s.setAlternateDefaults(ctx, []kvpb.TenantSetting{st("e", "ee")})
+	expect(s.getTenantOverrides(ctx, allTenantOverridesID), "a=aa d=d2 e=ee")
+
+	// If a custom override is added again, the alternate default gets
+	// hidden again.
+	s.setTenantOverride(ctx, allTenantOverridesID, st("d", "dd"))
+	expect(s.getTenantOverrides(ctx, allTenantOverridesID), "a=aa d=dd e=ee")
+}
+
+func st(key settings.InternalKey, val string) kvpb.TenantSetting {
+	return kvpb.TenantSetting{
+		InternalKey: key,
+		Value: settings.EncodedValue{
+			Value: val,
+		},
+	}
+}
+
+func TestFindAlternateDefault(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	type kts = kvpb.TenantSetting
+
+	for idx, tc := range []struct {
+		defaults []kvpb.TenantSetting
+		key      settings.InternalKey
+
+		expectedVal string
+	}{
+		{[]kts{st("a", "aa"), st("c", "cc")}, "d", ""},
+		{[]kts{st("a", "aa"), st("c", "cc")}, "b", ""},
+		{[]kts{st("c", "cc"), st("d", "dd")}, "a", ""},
+		{[]kts{st("a", "aa"), st("c", "cc")}, "a", "aa"},
+		{[]kts{st("a", "aa"), st("c", "cc")}, "c", "cc"},
+	} {
+		t.Run(fmt.Sprint(idx), func(t *testing.T) {
+			val, found := findAlternateDefault(tc.key, tc.defaults)
+			require.Equal(t, tc.expectedVal != "", found)
+			require.Equal(t, tc.expectedVal, val.Value)
+		})
+	}
+}
+
+func TestUpdateDefaults(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	type kts = kvpb.TenantSetting
+
+	for idx, tc := range []struct {
+		defaults     []kvpb.TenantSetting
+		moreDefaults []kvpb.TenantSetting
+
+		expected []kvpb.TenantSetting
+	}{
+		// Empty case as sanity check.
+		{nil, nil, nil},
+		// No defaults to start with: produce at least the new defaults.
+		{nil, []kts{st("a", "aa")}, []kts{st("a", "aa")}},
+		// Some defaults already known.
+		// update from the new defaults.
+		{[]kts{st("a", "aa")}, []kts{st("a", "bb")}, []kts{st("a", "bb")}},
+
+		// Some defaults known. No new defaults.
+		{[]kts{st("a", "aa")}, nil, []kts{st("a", "aa")}},
+
+		// Alternatives to the above, with more elements.
+		{[]kts{st("a", "aa"), st("c", "cc")},
+			[]kts{st("b", "bb")},
+			[]kts{st("a", "aa"), st("b", "bb"), st("c", "cc")}},
+		{[]kts{st("a", "aa"), st("c", "cc")},
+			[]kts{st("b", "bb"), st("c", "c2"), st("d", "dd")},
+			[]kts{st("a", "aa"), st("b", "bb"), st("c", "c2"), st("d", "dd")}},
+		{[]kts{st("b", "bb"), st("d", "dd")},
+			// Edge case: the number of alternate defaults before the first
+			// explicit value, is larger than the number of explicit values
+			// to start with. This test catches a pitfall in the splice
+			// logic.
+			[]kts{st("a", "aa"), st("a2", "aa"), st("a3", "aa"),
+				st("b", "b2"), st("c", "cc"), st("c2", "cc"), st("d", "d2"), st("e", "ee")},
+			[]kts{st("a", "aa"), st("a2", "aa"), st("a3", "aa"),
+				st("b", "b2"), st("c", "cc"), st("c2", "cc"), st("d", "d2"), st("e", "ee")}},
+	} {
+		t.Run(fmt.Sprint(idx), func(t *testing.T) {
+			result := updateDefaults(tc.defaults, tc.moreDefaults)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestSpliceOverrideDefaults(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	mexp := func(keys ...settings.InternalKey) map[settings.InternalKey]struct{} {
+		m := make(map[settings.InternalKey]struct{}, len(keys))
+		for _, k := range keys {
+			m[k] = struct{}{}
+		}
+		return m
+	}
+
+	type kts = kvpb.TenantSetting
+
+	for idx, tc := range []struct {
+		explicitKeys      map[settings.InternalKey]struct{}
+		overrides         []kvpb.TenantSetting
+		alternateDefaults []kvpb.TenantSetting
+
+		expected []kvpb.TenantSetting
+	}{
+		// Empty case as sanity check.
+		{nil, nil, nil, nil},
+		// No overrides to start with: produce at least the alternate defaults.
+		{nil, nil, []kts{st("a", "aa")}, []kts{st("a", "aa")}},
+		// Overrides previously set from alternate defaults (no explicitKeys):
+		// update from the new alternate defaults.
+		{nil, []kts{st("a", "aa")}, []kts{st("a", "bb")}, []kts{st("a", "bb")}},
+		// Overrides previously set from rangefeed (key in explicitKeys):
+		// keep the override.
+		{mexp("a"), []kts{st("a", "aa")}, []kts{st("a", "bb")}, []kts{st("a", "aa")}},
+
+		// Override previously set from alternate default (no explicitKeys)
+		// and the alternate default is removed. Remove the override.
+		{nil, []kts{st("a", "aa")}, nil, []kts{}},
+
+		// Alternatives to the above, with more elements.
+		{nil,
+			[]kts{st("a", "aa"), st("c", "cc")},
+			[]kts{st("b", "bb")},
+			[]kts{st("b", "bb")}},
+		{mexp("a", "c"),
+			[]kts{st("a", "aa"), st("c", "cc")},
+			[]kts{st("b", "bb")},
+			[]kts{st("a", "aa"), st("b", "bb"), st("c", "cc")}},
+		{mexp("a"),
+			[]kts{st("a", "aa"), st("c", "cc")},
+			[]kts{st("b", "bb"), st("c", "c2"), st("d", "dd")},
+			[]kts{st("a", "aa"), st("b", "bb"), st("c", "c2"), st("d", "dd")}},
+		{mexp("b", "d"),
+			[]kts{st("b", "bb"), st("d", "dd")},
+			// Edge case: the number of alternate defaults before the first
+			// explicit value, is larger than the number of explicit values
+			// to start with. This test catches a pitfall in the splice
+			// logic.
+			[]kts{st("a", "aa"), st("a2", "aa"), st("a3", "aa"),
+				st("b", "b2"), st("c", "cc"), st("c2", "cc"), st("d", "d2"), st("e", "ee")},
+			[]kts{st("a", "aa"), st("a2", "aa"), st("a3", "aa"),
+				st("b", "bb"), st("c", "cc"), st("c2", "cc"), st("d", "dd"), st("e", "ee")}},
+	} {
+		testutils.RunTrueAndFalse(t, "copy", func(t *testing.T, copy bool) {
+			t.Run(fmt.Sprint(idx), func(t *testing.T) {
+				result := spliceOverrideDefaults(tc.explicitKeys, tc.overrides, tc.alternateDefaults, copy)
+				require.Equal(t, tc.expected, result)
+			})
+		})
+	}
 }
