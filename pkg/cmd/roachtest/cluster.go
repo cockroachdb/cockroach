d88871e98dbb7ae1753a1fa5b1900324589d3549
Miral Gadani <miral@cockroachlabs.com>
2023-09-25 12:53:27 -0400

    roachtest: add api functions accepting []string over ...string
    The existing function implementations are delegated to the new
    ones which end in []string and not ...string. This allows us to add
    functional options as the last arg in a future change.
    
    Epic: none
    Release note: none

--- pkg/cmd/roachtest/cluster.go
+++ pkg/cmd/roachtest/cluster.go
@@ -2162,19 +2162,27 @@ func (c *clusterImpl) Wipe(ctx context.Context, preserveCerts bool, nodes ...opt
 	}
 }
 
-// Run a command on the specified nodes and call test.Fatal if there is an error.
 func (c *clusterImpl) Run(ctx context.Context, node option.NodeListOption, args ...string) {
-	err := c.RunE(ctx, node, args...)
+	c.RunExt(ctx, node, args)
+}
+
+// Run a command on the specified nodes and call test.Fatal if there is an error.
+func (c *clusterImpl) RunExt(ctx context.Context, node option.NodeListOption, args []string) {
+	err := c.RunEExt(ctx, node, args)
 	if err != nil {
 		c.t.Fatal(err)
 	}
+}
+
+func (c *clusterImpl) RunE(ctx context.Context, node option.NodeListOption, args ...string) error {
+	return c.RunEExt(ctx, node, args)
 }
 
 // RunE runs a command on the specified node, returning an error. The output
 // will be redirected to a file which is logged via the cluster-wide logger in
 // case of an error. Logs will sort chronologically. Failing invocations will
 // have an additional marker file with a `.failed` extension instead of `.log`.
-func (c *clusterImpl) RunE(ctx context.Context, nodes option.NodeListOption, args ...string) error {
+func (c *clusterImpl) RunEExt(ctx context.Context, nodes option.NodeListOption, args []string) error {
 	if len(args) == 0 {
 		return errors.New("No command passed")
 	}
@@ -2205,12 +2213,18 @@ func (c *clusterImpl) RunE(ctx context.Context, nodes option.NodeListOption, arg
 	return nil
 }
 
+func (c *clusterImpl) RunWithDetailsSingleNode(
+	ctx context.Context, testLogger *logger.Logger, nodes option.NodeListOption, args ...string,
+) (install.RunResultDetails, error) {
+	return c.RunWithDetailsSingleNodeExt(ctx, testLogger, nodes, args)
+}
+
 // RunWithDetailsSingleNode is just like RunWithDetails but used when 1) operating
 // on a single node AND 2) an error from roachprod itself would be treated the same way
 // you treat an error from the command. This makes error checking easier / friendlier
 // and helps us avoid code replication.
-func (c *clusterImpl) RunWithDetailsSingleNode(
-	ctx context.Context, testLogger *logger.Logger, nodes option.NodeListOption, args ...string,
+func (c *clusterImpl) RunWithDetailsSingleNodeExt(
+	ctx context.Context, testLogger *logger.Logger, nodes option.NodeListOption, args []string,
 ) (install.RunResultDetails, error) {
 	if len(nodes) != 1 {
 		return install.RunResultDetails{}, errors.Newf("RunWithDetailsSingleNode received %d nodes. Use RunWithDetails if you need to run on multiple nodes.", len(nodes))
@@ -2219,12 +2233,18 @@ func (c *clusterImpl) RunWithDetailsSingleNode(
 	return results[0], errors.CombineErrors(err, results[0].Err)
 }
 
+func (c *clusterImpl) RunWithDetails(
+	ctx context.Context, testLogger *logger.Logger, nodes option.NodeListOption, args ...string,
+) ([]install.RunResultDetails, error) {
+	return c.RunWithDetailsExt(ctx, testLogger, nodes, args)
+}
+
 // RunWithDetails runs a command on the specified nodes, returning the results
 // details and a `roachprod` error. The output will be redirected to a file which is logged
 // via the cluster-wide logger in case of an error. Failing invocations will have
 // an additional marker file with a `.failed` extension instead of `.log`.
-func (c *clusterImpl) RunWithDetails(
-	ctx context.Context, testLogger *logger.Logger, nodes option.NodeListOption, args ...string,
+func (c *clusterImpl) RunWithDetailsExt(
+	ctx context.Context, testLogger *logger.Logger, nodes option.NodeListOption, args []string,
 ) ([]install.RunResultDetails, error) {
 	if len(args) == 0 {
 		return nil, errors.New("No command passed")
