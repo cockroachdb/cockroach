bc6676204aa201885d05681cad2020dd803089dc
Miral Gadani <miral@cockroachlabs.com>
2023-09-25 16:10:10 -0400

    roachprod: replace parallelopts with runopts via cluster api
    This change introduces `install.RunOptions` to be able to
    configure how and whether a command is retried via `c.Run`.
    The new API functions now accept a variadic `...install.RunOption`
    which can be any of `WithFailSlow(..), WithRetryOpts(..),
    WithRetryFn()`.
    
    Since we have delegated the existing API functions to preserve
    current behaviour, no roachtests need be updated.
    
    Epic: none
    Release note: none

--- pkg/cmd/roachtest/cluster.go
+++ pkg/cmd/roachtest/cluster.go
@@ -2183,8 +2183,10 @@ func (c *clusterImpl) Run(ctx context.Context, node option.NodeListOption, args 
 }
 
 // Run a command on the specified nodes and call test.Fatal if there is an error.
-func (c *clusterImpl) RunExt(ctx context.Context, node option.NodeListOption, args []string) {
-	err := c.RunEExt(ctx, node, args)
+func (c *clusterImpl) RunExt(
+	ctx context.Context, node option.NodeListOption, args []string, opts ...install.RunOption,
+) {
+	err := c.RunEExt(ctx, node, args, opts...)
 	if err != nil {
 		c.t.Fatal(err)
 	}
@@ -2198,7 +2200,9 @@ func (c *clusterImpl) RunE(ctx context.Context, node option.NodeListOption, args
 // will be redirected to a file which is logged via the cluster-wide logger in
 // case of an error. Logs will sort chronologically. Failing invocations will
 // have an additional marker file with a `.failed` extension instead of `.log`.
-func (c *clusterImpl) RunEExt(ctx context.Context, nodes option.NodeListOption, args []string) error {
+func (c *clusterImpl) RunEExt(
+	ctx context.Context, nodes option.NodeListOption, args []string, opts ...install.RunOption,
+) error {
 	if len(args) == 0 {
 		return errors.New("No command passed")
 	}
@@ -2240,7 +2244,11 @@ func (c *clusterImpl) RunWithDetailsSingleNode(
 // you treat an error from the command. This makes error checking easier / friendlier
 // and helps us avoid code replication.
 func (c *clusterImpl) RunWithDetailsSingleNodeExt(
-	ctx context.Context, testLogger *logger.Logger, nodes option.NodeListOption, args []string,
+	ctx context.Context,
+	testLogger *logger.Logger,
+	nodes option.NodeListOption,
+	args []string,
+	opts ...install.RunOption,
 ) (install.RunResultDetails, error) {
 	if len(nodes) != 1 {
 		return install.RunResultDetails{}, errors.Newf("RunWithDetailsSingleNode received %d nodes. Use RunWithDetails if you need to run on multiple nodes.", len(nodes))
@@ -2260,7 +2268,11 @@ func (c *clusterImpl) RunWithDetails(
 // via the cluster-wide logger in case of an error. Failing invocations will have
 // an additional marker file with a `.failed` extension instead of `.log`.
 func (c *clusterImpl) RunWithDetailsExt(
-	ctx context.Context, testLogger *logger.Logger, nodes option.NodeListOption, args []string,
+	ctx context.Context,
+	testLogger *logger.Logger,
+	nodes option.NodeListOption,
+	args []string,
+	opts ...install.RunOption,
 ) ([]install.RunResultDetails, error) {
 	if len(args) == 0 {
 		return nil, errors.New("No command passed")
@@ -2279,7 +2291,7 @@ func (c *clusterImpl) RunWithDetailsExt(
 	}
 
 	l.Printf("> %s", cmd)
-	results, err := roachprod.RunWithDetails(ctx, l, c.MakeNodes(nodes), "" /* SSHOptions */, "" /* processTag */, c.IsSecure(), args)
+	results, err := roachprod.RunWithDetails(ctx, l, c.MakeNodes(nodes), "" /* SSHOptions */, "" /* processTag */, c.IsSecure(), args, opts...)
 
 	var logFileFull string
 	if l.File != nil {
--- pkg/roachprod/install/cluster_synced_test.go
+++ pkg/roachprod/install/cluster_synced_test.go
@@ -97,58 +97,58 @@ func TestRunWithMaybeRetry(t *testing.T) {
 
 	attempt := 0
 	cases := []struct {
-		f                func(ctx context.Context) (*RunResultDetails, error)
-		shouldRetryFn    func(*RunResultDetails) bool
+		f                func(ctx context.Context) (*install.RunResultDetails, error)
+		shouldRetryFn    func(*install.RunResultDetails) bool
 		nilRetryOpts     bool
 		expectedAttempts int
 		shouldError      bool
 	}{
 		{ // 1. Happy path: no error, no retry required
-			f: func(ctx context.Context) (*RunResultDetails, error) {
+			f: func(ctx context.Context) (*install.RunResultDetails, error) {
 				return newResult(0), nil
 			},
 			expectedAttempts: 1,
 			shouldError:      false,
 		},
 		{ // 2. Error, but with no retries
-			f: func(ctx context.Context) (*RunResultDetails, error) {
+			f: func(ctx context.Context) (*install.RunResultDetails, error) {
 				return newResult(1), nil
 			},
-			shouldRetryFn: func(*RunResultDetails) bool {
+			shouldRetryFn: func(*install.RunResultDetails) bool {
 				return false
 			},
 			expectedAttempts: 1,
 			shouldError:      true,
 		},
 		{ // 3. Error, but no retry function specified
-			f: func(ctx context.Context) (*RunResultDetails, error) {
+			f: func(ctx context.Context) (*install.RunResultDetails, error) {
 				return newResult(1), nil
 			},
 			expectedAttempts: 3,
 			shouldError:      true,
 		},
 		{ // 4. Error, with retries exhausted
-			f: func(ctx context.Context) (*RunResultDetails, error) {
+			f: func(ctx context.Context) (*install.RunResultDetails, error) {
 				return newResult(255), nil
 			},
-			shouldRetryFn:    func(d *RunResultDetails) bool { return d.RemoteExitStatus == 255 },
+			shouldRetryFn:    func(d *install.RunResultDetails) bool { return d.RemoteExitStatus == 255 },
 			expectedAttempts: 3,
 			shouldError:      true,
 		},
 		{ // 5. Eventual success after retries
-			f: func(ctx context.Context) (*RunResultDetails, error) {
+			f: func(ctx context.Context) (*install.RunResultDetails, error) {
 				attempt++
 				if attempt == 3 {
 					return newResult(0), nil
 				}
 				return newResult(255), nil
 			},
-			shouldRetryFn:    func(d *RunResultDetails) bool { return d.RemoteExitStatus == 255 },
+			shouldRetryFn:    func(d *install.RunResultDetails) bool { return d.RemoteExitStatus == 255 },
 			expectedAttempts: 3,
 			shouldError:      false,
 		},
 		{ // 6. Error, runs once because nil retryOpts
-			f: func(ctx context.Context) (*RunResultDetails, error) {
+			f: func(ctx context.Context) (*install.RunResultDetails, error) {
 				return newResult(255), nil
 			},
 			nilRetryOpts:     true,
@@ -160,9 +160,9 @@ func TestRunWithMaybeRetry(t *testing.T) {
 	for idx, tc := range cases {
 		attempt = 0
 		t.Run(fmt.Sprintf("%d", idx+1), func(t *testing.T) {
-			var retryOpts *RunRetryOpts
+			var retryOpts *RetryOpts
 			if !tc.nilRetryOpts {
-				retryOpts = newRunRetryOpts(testRetryOpts, tc.shouldRetryFn)
+				retryOpts = NewRetryOpts(testRetryOpts, tc.shouldRetryFn)
 			}
 			res, _ := runWithMaybeRetry(context.Background(), l, retryOpts, tc.f)
 
@@ -176,12 +176,12 @@ func TestRunWithMaybeRetry(t *testing.T) {
 	}
 }
 
-func newResult(exitCode int) *RunResultDetails {
+func newResult(exitCode int) *install.RunResultDetails {
 	var err error
 	if exitCode != 0 {
 		err = errors.Newf("Error with exit code %v", exitCode)
 	}
-	return &RunResultDetails{RemoteExitStatus: exitCode, Err: err}
+	return &install.RunResultDetails{RemoteExitStatus: exitCode, Err: err}
 }
 
 func nilLogger() *logger.Logger {
