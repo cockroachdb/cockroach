a87f0c71ab05a1569641744cb687e8c913d46f1b
Zach Lite <zach@cockroachlabs.com>
2023-09-27 14:31:57 -0400

    sql: create new system observability tables and update job
    This commit is the combination of two separate work streams,
    brought together for resolving logic test fallout simultaneously.
    
    The first, authored by @koorosh is the creation of
    system.transaction_exec_insights and system.statement_exec_insights.
    
    The second, authored by @zachlite in #111365 is the creation of
    system.mvcc_statistics and the MVCCStatisticsUpdate job.
    
    Regarding persisted insights:
    Before, this data was kept in memory only and tracked limited
    number of latest insights. These tables will be used to persist
    this data periodically.
    
    Tables allow to store the same information as in memory insights
    without aggregation.
    
    To control the amount of data stored in tables, there will be
    follow up PR to run GC job and prune old records.
    
    To make tables flexible to changes when some columns might become
    obsolete, most of the columns defined as nullable.
    
    Regarding persisted MVCC Statistics:
    The system.mvcc_statistics table stores historical mvcc data
    for a tenant's SQL objects. It's purpose it to serve mvcc data for a
    SQL object quickly - The span stats API is too slow to use in a hot path.
    Storing data over time unlocks new use cases like showing a table or
    index's accumulated garbage over time.
    
    The MVCCStatisticsUpdate Job is responsible for managing the contents of
    the table, decoupled from the read-hotpath.
    
    Both the table and job are baked when a cluster bootstraps itself, or upgrades
    itself from a previous version.
    
    This PR supersedes #111365 with the following changes:
    - Descriptor fixes to the mvcc_statistics table. No logical changes,
    just housekeeping to make sure that the create table schema and descriptors
    produce the same table.
    - Fixes to the job to make sure the job system can wind down.
    
    Partially resolves: #104582
    Epic: CRDB-25491
    Release note: None

--- pkg/sql/catalog/bootstrap/metadata.go
+++ pkg/sql/catalog/bootstrap/metadata.go
@@ -475,6 +475,9 @@ func addSystemDescriptorsToSchema(target *MetadataSchema) {
 
 	// Tables introduced in 23.2.
 	target.AddDescriptor(systemschema.RegionLivenessTable)
+	target.AddDescriptor(systemschema.SystemMVCCStatisticsTable)
+	target.AddDescriptor(systemschema.TransactionExecInsightsTable)
+	target.AddDescriptor(systemschema.StatementExecInsightsTable)
 
 	// Adding a new system table? It should be added here to the metadata schema,
 	// and also created as a migration for older clusters.
@@ -488,7 +491,7 @@ func addSystemDescriptorsToSchema(target *MetadataSchema) {
 // NumSystemTablesForSystemTenant is the number of system tables defined on
 // the system tenant. This constant is only defined to avoid having to manually
 // update auto stats tests every time a new system table is added.
-const NumSystemTablesForSystemTenant = 52
+const NumSystemTablesForSystemTenant = 55
 
 // addSplitIDs adds a split point for each of the PseudoTableIDs to the supplied
 // MetadataSchema.
---
+++ pkg/upgrade/upgrades/mvcc_statistics_migration_test.go
@@ -0,0 +1,80 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package upgrades_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
+	"github.com/cockroachdb/cockroach/pkg/server"
+	"github.com/cockroachdb/cockroach/pkg/sql"
+	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
+	"github.com/cockroachdb/cockroach/pkg/upgrade/upgrades"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMVCCStatisticsMigration(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	clusterArgs := base.TestClusterArgs{
+		ServerArgs: base.TestServerArgs{
+			Knobs: base.TestingKnobs{
+				Server: &server.TestingKnobs{
+					DisableAutomaticVersionUpgrade: make(chan struct{}),
+					BinaryVersionOverride:          clusterversion.ByKey(clusterversion.BinaryMinSupportedVersionKey),
+					BootstrapVersionKeyOverride:    clusterversion.BinaryMinSupportedVersionKey,
+				},
+			},
+		},
+	}
+
+	var (
+		ctx = context.Background()
+
+		tc    = testcluster.StartTestCluster(t, 1, clusterArgs)
+		s     = tc.Server(0)
+		sqlDB = tc.ServerConn(0)
+	)
+	defer tc.Stopper().Stop(ctx)
+	require.True(t, s.ExecutorConfig().(sql.ExecutorConfig).Codec.ForSystemTenant())
+
+	// Introduce the sequence.
+	upgrades.Upgrade(
+		t,
+		sqlDB,
+		clusterversion.V23_2_MVCCStatisticsTable,
+		nil,
+		false,
+	)
+
+	db := tc.ServerConn(0)
+	defer db.Close()
+
+	t.Run("mvcc_statistics_table", func(t *testing.T) {
+		_, err := sqlDB.Exec("SELECT * FROM system.mvcc_statistics")
+		require.NoError(t, err, "system.public.mvcc_statistics exists")
+	})
+
+	t.Run("mvcc_stats_job", func(t *testing.T) {
+		row := db.QueryRow("SELECT count(*) FROM system.public.jobs WHERE id = 104")
+		require.NotNil(t, row)
+		require.NoError(t, row.Err())
+		var count int
+		err := row.Scan(&count)
+		require.NoError(t, err)
+		require.Equal(t, 1, count)
+	})
+}
