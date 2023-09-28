3eef7179efa94f128fd6a511cea7ceb9bd201aa5
Aaditya Sondhi <20070511+aadityasondhi@users.noreply.github.com>
2023-09-28 10:34:01 -0400

    kvflowcontrol: deflake TestFlowControlGranterAdmitOneByOne
    This test was flaky because of the async update of cluster settings to
    other nodes. When we set `kvflowcontrol.Mode` to `apply_to_all`, it
    would trigger the `OnChange` callback for that setting that returns all
    tokens early, which disrupted the change. Most of the time the setting
    worked, but sometimes, it would trigger the callback during the workload
    causing the test to fail.
    
    This change now uses `Override()` to synchronously update this setting on
    each node to make sure the callback is called before returning from the
    `init()` func.
    
    Fixes #110425
    
    Release note: None

--- pkg/kv/kvserver/flow_control_integration_test.go
+++ pkg/kv/kvserver/flow_control_integration_test.go
@@ -2331,11 +2331,13 @@ func newFlowControlTestHelper(t *testing.T, tc *testcluster.TestCluster) *flowCo
 }
 
 func (h *flowControlTestHelper) init() {
-	if _, err := h.tc.Conns[0].Exec(`SET CLUSTER SETTING kvadmission.flow_control.enabled = true`); err != nil {
-		h.t.Fatal(err)
-	}
-	if _, err := h.tc.Conns[0].Exec(`SET CLUSTER SETTING kvadmission.flow_control.mode = 'apply_to_all'`); err != nil {
-		h.t.Fatal(err)
+	// Reach into each server's cluster setting and override. This causes any
+	// registered change callbacks to run immediately, which is important since
+	// running them with some lag (which happens when using SQL and `SET CLUSTER
+	// SETTING`) interferes with the later activities in these tests.
+	for _, s := range h.tc.Servers {
+		kvflowcontrol.Enabled.Override(context.Background(), &s.ClusterSettings().SV, true)
+		kvflowcontrol.Mode.Override(context.Background(), &s.ClusterSettings().SV, int64(kvflowcontrol.ApplyToAll))
 	}
 }
 
