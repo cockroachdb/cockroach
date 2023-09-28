4f767119e64f575ef441773732982ea47e563ac2
maryliag <marylia@cockroachlabs.com>
2023-09-28 19:51:29 -0300

    ui: switch order of metrics
    On chart Replication -> Ranges, swithc order of Metrics
    so that the `Unavailable` metric is the red line.
    Otherwise could be confusing seeing any other as Red, and
    the `Unavailable` as green.
    
    Fixes #107637
    
    Release note: None

--- pkg/ui/workspaces/db-console/src/views/cluster/containers/nodeGraphs/dashboards/replication.tsx
+++ pkg/ui/workspaces/db-console/src/views/cluster/containers/nodeGraphs/dashboards/replication.tsx
@@ -49,12 +49,12 @@ export default function (props: GraphDashboardProps) {
       <Axis label="ranges">
         <Metric name="cr.store.ranges" title="Ranges" />
         <Metric name="cr.store.replicas.leaders" title="Leaders" />
+        <Metric name="cr.store.ranges.unavailable" title="Unavailable" />
         <Metric name="cr.store.replicas.leaseholders" title="Lease Holders" />
         <Metric
           name="cr.store.replicas.leaders_not_leaseholders"
           title="Leaders w/o Lease"
         />
-        <Metric name="cr.store.ranges.unavailable" title="Unavailable" />
         <Metric
           name="cr.store.ranges.underreplicated"
           title="Under-replicated"
