d1da26efbdfbe025402531639b557317f342099a
Radu Berinde <radu@cockroachlabs.com>
2023-10-01 13:21:40 -0700

    roachtest: add --suite and --owner flags, improve test filtering
    This commit adds `--suite` and `--owner` flags to `list`, `run`, and
    `bench` commands, and adds filtering of tests based on cloud
    compatibility.
    
    To ignore cloud compatibility when running tests (the old behavior), a
    new flag `--force-cloud-compat` can be used.
    
    As part of this change, we improve usability around test selection:
     - we now print a user-friendly description of the filter;
     - when no tests match we print detailed error messages which identify
       which part of the filter causes no matches.
    
    As part of this change, we move the "benchmarks only" flag from the
    registry to the `TestFilter`.
    
    Epic: none
    Release note: None

--- pkg/cmd/roachtest/registry/filter.go
+++ pkg/cmd/roachtest/registry/filter.go
@@ -11,14 +11,30 @@
 package registry
 
 import (
+	"fmt"
 	"regexp"
+	"sort"
 	"strings"
+	"unicode"
 )
 
 // TestFilter holds the name and tag filters for filtering tests.
 // See NewTestFilter.
 type TestFilter struct {
 	Name *regexp.Regexp
+
+	// Cloud, if set, restricts the set of tests to those compatible with this cloud.
+	Cloud string
+
+	// Suite, if set, restricts the set of tests to those that are part of this suite.
+	Suite string
+
+	// Owner, if set, restricts the set of tests to those with this owner.
+	Owner Owner
+
+	// OnlyBenchmarks, if set, restricts the set of tests to benchmarks.
+	OnlyBenchmarks bool
+
 	// Multiple `tag:` parameters can be passed for which only one needs to match, but the
 	// value of a single `tag:` parameter can be a comma-separated list of tags which all need
 	// to match.
@@ -30,14 +46,35 @@ type TestFilter struct {
 	Tags map[string]struct{}
 }
 
-// NewTestFilter initializes a new filter. The strings are interpreted
-// as regular expressions. As a special case, a `tag:` prefix implies
-// that the remainder of the string filters tests by tag, and not by
-// name.
-func NewTestFilter(filter []string) *TestFilter {
+// TestFilterOption can be passed to NewTestFilter.
+type TestFilterOption func(tf *TestFilter)
+
+// WithCloud restricts the set of tests to those compatible with this cloud.
+func WithCloud(cloud string) TestFilterOption {
+	return func(tf *TestFilter) { tf.Cloud = cloud }
+}
+
+// WithSuite restricts the set of tests to those that are part of this suite.
+func WithSuite(suite string) TestFilterOption {
+	return func(tf *TestFilter) { tf.Suite = suite }
+}
+
+// WithOwner restricts the set of tests to those with this owner.
+func WithOwner(owner Owner) TestFilterOption {
+	return func(tf *TestFilter) { tf.Owner = owner }
+}
+
+func OnlyBenchmarks() TestFilterOption {
+	return func(tf *TestFilter) { tf.OnlyBenchmarks = true }
+}
+
+// NewTestFilter initializes a new filter. The strings are interpreted as
+// regular expressions. As a special case, a `tag:` prefix implies that the
+// remainder of the string filters tests by tag, and not by name.
+func NewTestFilter(regexps []string, options ...TestFilterOption) *TestFilter {
 	var name []string
 	tags := make(map[string]struct{})
-	for _, v := range filter {
+	for _, v := range regexps {
 		if strings.HasPrefix(v, "tag:") {
 			tags[strings.TrimPrefix(v, "tag:")] = struct{}{}
 		} else {
@@ -59,8 +96,348 @@ func NewTestFilter(filter []string) *TestFilter {
 		}
 	}
 
-	return &TestFilter{
+	tf := &TestFilter{
 		Name: makeRE(name),
 		Tags: tags,
 	}
+	for _, o := range options {
+		o(tf)
+	}
+	return tf
+}
+
+// MatchFailReason describes the reason(s) a filter did not match the test.
+type MatchFailReason struct {
+	// If true, the filter requires a benchmark and the test is not one.
+	IsNotBenchmark bool
+	// If true, the test name does not match the filter regexp.
+	NameMismatch bool
+	// If true, the test owner does not match the owner in the filter.
+	OwnerMismatch bool
+	// If true, the test is not part of the suite in the filter.
+	NotPartOfSuite bool
+	// If true, the test is not compatible with the cloud in the filter.
+	CloudNotCompatible bool
+	// If true, the tags don't match those in the filter.
+	TagsMismatch bool
+}
+
+// Matches returns true if the filter matches the test. If the test doesn't
+// match, returns the reason(s).
+func (filter *TestFilter) Matches(t *TestSpec) (matches bool, reason MatchFailReason) {
+	reason.IsNotBenchmark = filter.OnlyBenchmarks && !t.Benchmark
+	reason.NameMismatch = !filter.Name.MatchString(t.Name)
+	reason.OwnerMismatch = filter.Owner != "" && t.Owner != filter.Owner
+	reason.NotPartOfSuite = filter.Suite != "" && !t.Suites.Contains(filter.Suite)
+	reason.CloudNotCompatible = filter.Cloud != "" && !t.CompatibleClouds.Contains(filter.Cloud)
+
+	if len(filter.Tags) > 0 {
+		// Tags will go away soon.
+		matchesTags := func() bool {
+			for tag := range filter.Tags {
+				// If the tag is a single CSV e.g. "foo,bar,baz", we match all the tags
+				if matchesAll(t.Tags, strings.Split(tag, ",")) {
+					return true
+				}
+			}
+			return false
+		}()
+		if !matchesTags {
+			reason.TagsMismatch = true
+		}
+	}
+	// We have a match if all fields are false.
+	return reason == MatchFailReason{}, reason
+}
+
+// MatchFailReasonString returns a user-friendly string describing the reason(s)
+// a filter failed to match a test (returned by Matches). Returns the empty
+// string if the reason is zero.
+//
+// Sample results:
+//   - does not match regex "foo"
+//   - does not match regex "foo" and is not part of the "nightly" suite
+//   - does not match regex "foo", is not part of the "nightly" suite, and is
+//     not compatible with cloud "gce"
+func (filter *TestFilter) MatchFailReasonString(r MatchFailReason) string {
+	var reasons []string
+	appendIf := func(b bool, format string, args ...interface{}) {
+		if b {
+			reasons = append(reasons, fmt.Sprintf(format, args...))
+		}
+	}
+	appendIf(r.IsNotBenchmark, "is not a benchmark")
+	appendIf(r.NameMismatch, "does not match regex %q", filter.Name)
+	appendIf(r.OwnerMismatch, "does not have owner %q", filter.Owner)
+	appendIf(r.NotPartOfSuite, "is not part of the %q suite", filter.Suite)
+	appendIf(r.CloudNotCompatible, "is not compatible with %s", filter.cloudStr())
+	appendIf(r.TagsMismatch, "does not match tags")
+
+	if len(reasons) <= 2 {
+		// 0 reasons: ""
+		// 1 reason:  "reason0"
+		// 2 reasons: "reason0 and reason1"
+		return strings.Join(reasons, " and ")
+	}
+	// 3 or more reasons: "reason0, reason1, reason2, and reason3
+	return strings.Join(reasons[:len(reasons)-1], ", ") + ", and " + reasons[len(reasons)-1]
+}
+
+// Filter returns the test specs in the given list that match the filter (in the
+// same order).
+func (filter *TestFilter) Filter(tests []TestSpec) []TestSpec {
+	var res []TestSpec
+	for i := range tests {
+		if ok, _ := filter.Matches(&tests[i]); ok {
+			res = append(res, tests[i])
+		}
+	}
+	return res
+}
+
+// NoMatchesHint identifies some common situations when a filter matches no tests.
+//
+// The hints help us produce helpful error message. We want to zero in on most
+// common problems. For example, if there is a typo in one aspect of the filter,
+// we want the message to highlight that.
+type NoMatchesHint int
+
+const (
+	// WrongCloud indicates that some tests match all aspects of the filter except
+	// the cloud. Since cloud compatibility was added recently, we expect this to
+	// be a common case.
+	WrongCloud = 1 + iota
+
+	// NoSuchName indicates tha the name regexp is invalid, i.e. no tests (with
+	// any owner, suite, cloud, etc) match the regexp.
+	NoSuchName
+
+	// NoSuchSuite indicates that the suite is invalid, i.e. there are no tests
+	// (with ay name, owner, cloud, etc) that match the suite.
+	NoSuchSuite
+
+	// NoTestsWithNameAndSuite indicates that there are no tests that match both
+	// the name regexp and the suite.
+	NoTestsWithNameAndSuite
+
+	// NoSuchOwner indicates that the owner is invalid, i.e. there are no tests
+	// (with any name, suite, cloud, etc) that match the owner.
+	NoSuchOwner
+
+	// NoTestsWithNameAndOwner indicates that there are no tests that match both
+	// the name regexp and the owner.
+	NoTestsWithNameAndOwner
+
+	// NoHint indicates any other situation that leads to a match failure.
+	NoHint
+)
+
+// FilterWithHint returns the test specs in the given list that match the filter
+// (in the same order). If there are no matches, returns a hint about what might
+// be wrong with the filter.
+func (filter *TestFilter) FilterWithHint(tests []TestSpec) ([]TestSpec, NoMatchesHint) {
+	res := filter.Filter(tests)
+	if len(res) > 0 {
+		return res, 0
+	}
+
+	// noFilter matches all tests/benchmarks.
+	noFilter := TestFilter{Name: regexp.MustCompile(`.`), OnlyBenchmarks: filter.OnlyBenchmarks}
+
+	// 1. Is the cloud likely the problem?
+	// We want to see if the desired tests exist but are not compatible with the
+	// given cloud (which is a recent feature). We use all fields from the
+	// original filter except the cloud and see if we get matches.
+	if filter.Cloud != "" {
+		noCloudFilter := *filter
+		noCloudFilter.Cloud = ""
+		if n := len(noCloudFilter.Filter(tests)); n > 0 {
+			return nil, WrongCloud
+		}
+	}
+
+	// In the checks below, the idea is to create a relaxed filter - one that only
+	// sets one or two fields from the original filter - and see if that still
+	// gets us no matches - in which chase that small number of fields are the
+	// problem.
+
+	// 2. Is the Name regexp the problem?
+	// We check if no tests match the regexp, in which case, the regexp is the problem.
+	if filter.Name.String() != "." {
+		nameOnlyFilter := noFilter
+		nameOnlyFilter.Name = filter.Name
+		if len(nameOnlyFilter.Filter(tests)) == 0 {
+			return nil, NoSuchName
+		}
+	}
+
+	// Check potential problems related to the suite.
+	if filter.Suite != "" {
+		// 3. Is the suite incorrect?
+		// We check if no tests match the suite, in which case the suite is the problem.
+		suiteOnlyFilter := noFilter
+		suiteOnlyFilter.Suite = filter.Suite
+		if len(suiteOnlyFilter.Filter(tests)) == 0 {
+			return nil, NoSuchSuite
+		}
+
+		// 4. Is the suite+regexp incorrect?
+		// We check if no tests match the suite AND the regexp.
+		suiteAndNameFilter := suiteOnlyFilter
+		suiteAndNameFilter.Name = filter.Name
+		if len(suiteAndNameFilter.Filter(tests)) == 0 {
+			return nil, NoTestsWithNameAndSuite
+		}
+	}
+
+	// Check potential problems related to the owner.
+	if filter.Owner != "" {
+		// 5. Is the owner incorrect?
+		// We check if no tests match the owner, in which case the owner is the problem.
+		ownerOnlyFilter := noFilter
+		ownerOnlyFilter.Owner = filter.Owner
+		if len(ownerOnlyFilter.Filter(tests)) == 0 {
+			return nil, NoSuchOwner
+		}
+		// 6. Is the owner+regexp incorrect?
+		// We check if no tests match the owner AND the regexp.
+		ownerAndNameFilter := ownerOnlyFilter
+		ownerAndNameFilter.Name = filter.Name
+		if len(ownerAndNameFilter.Filter(tests)) == 0 {
+			return nil, NoTestsWithNameAndOwner
+		}
+	}
+
+	// We failed to produce a useful message. It's an uncommon combination of
+	// criteria that leads to no tests matching.
+	return nil, NoHint
+}
+
+// NoMatchesHintString returns a user-friendly string describing the hint.
+func (filter *TestFilter) NoMatchesHintString(h NoMatchesHint) string {
+	noun := filter.noun()
+	switch h {
+	case WrongCloud:
+		return fmt.Sprintf(
+			"no %s match criteria; some %s match but are not compatible with %s",
+			noun, noun, filter.cloudStr(),
+		)
+	case NoSuchName:
+		return fmt.Sprintf("no %s match regexp %q", noun, filter.Name)
+	case NoSuchSuite:
+		return fmt.Sprintf("no %s in suite %q", noun, filter.Suite)
+	case NoTestsWithNameAndSuite:
+		return fmt.Sprintf("no %s in suite %q match regexp %q", noun, filter.Suite, filter.Name)
+	case NoSuchOwner:
+		return fmt.Sprintf("no %s with owner %q", noun, filter.Owner)
+	case NoTestsWithNameAndOwner:
+		return fmt.Sprintf("no %s with owner %q match regexp %q", noun, filter.Owner, filter.Name)
+	default:
+		return fmt.Sprintf("no %s match criteria", noun)
+	}
+}
+
+// Describe returns a multi-line string that describes the filter in a user-friendly way.
+// A verb like "Listing " or "Running" can be prepended to the first line. A
+// period can be prepended to the last line.
+//
+// Examples:
+// 1. all tests
+// 2. all benchmarks matching regexp "foo"
+// 3. all tests compatible with GCE
+// 4. tests which:
+//   - are compatible with GCE, and
+//   - have owner "foo", and
+//   - match regexp "bar"
+func (filter *TestFilter) Describe() []string {
+	b2i := map[bool]int{false: 0, true: 1}
+	numCriteria := b2i[filter.Name.String() != "."] +
+		b2i[filter.Cloud != ""] +
+		b2i[filter.Suite != ""] +
+		b2i[filter.Owner != ""] +
+		b2i[len(filter.Tags) > 0]
+
+	noun := filter.noun()
+
+	if numCriteria == 0 {
+		return []string{fmt.Sprintf("all %s", noun)}
+	}
+
+	var tags []string
+	for tag := range filter.Tags {
+		tags = append(tags, tag)
+	}
+	sort.Strings(tags)
+	tagsStr := strings.Join(tags, " OR ")
+
+	// Special cases for a single criterion.
+	if numCriteria == 1 {
+		switch {
+		case filter.Name.String() != ".":
+			return []string{fmt.Sprintf("all %s matching regexp %q", noun, filter.Name)}
+		case filter.Cloud != "":
+			return []string{fmt.Sprintf("all %s compatible with %s", noun, filter.cloudStr())}
+		case filter.Suite != "":
+			return []string{fmt.Sprintf("all %s in suite %q", noun, filter.Suite)}
+		case filter.Owner != "":
+			return []string{fmt.Sprintf("all %s with owner %q", noun, filter.Owner)}
+		case len(filter.Tags) > 0:
+			return []string{fmt.Sprintf("all %s with tag(s) %s", noun, tagsStr)}
+		}
+	}
+
+	// Special cases for cloud + one other criterion.
+	if numCriteria == 2 && filter.Cloud != "" {
+		switch {
+		case filter.Name.String() != ".":
+			return []string{fmt.Sprintf("%s compatible with %s matching regexp %q", noun, filter.cloudStr(), filter.Name)}
+		case filter.Suite != "":
+			return []string{fmt.Sprintf("%s compatible with %s in suite %q", noun, filter.cloudStr(), filter.Suite)}
+		case filter.Owner != "":
+			return []string{fmt.Sprintf("%s compatible with %s with owner %q", noun, filter.cloudStr(), filter.Owner)}
+		}
+	}
+
+	// List all criteria.
+	res := []string{fmt.Sprintf("%s which:", noun)}
+	appendIf := func(test bool, format string, args ...interface{}) {
+		if test {
+			res = append(res, fmt.Sprintf(" - "+format, args...))
+		}
+	}
+	appendIf(filter.Cloud != "", "are compatible with %s", filter.cloudStr())
+	appendIf(filter.Suite != "", "are part of the %q suite", filter.Suite)
+	appendIf(filter.Owner != "", "have owner %q", filter.Owner)
+	appendIf(filter.Name.String() != ".", "match regex %q", filter.Name)
+	appendIf(len(filter.Tags) > 0, "match tag(s) %s", tagsStr)
+	for i := 1; i < len(res)-1; i++ {
+		res[i] = res[i] + ", and"
+	}
+	return res
+}
+
+// cloudStr returns the cloud as a user-friendly string.
+func (filter *TestFilter) cloudStr() string {
+	if filter.Cloud == "" {
+		return ""
+	}
+	runes := []rune(strings.ToLower(filter.Cloud))
+	if len(runes) < 4 {
+		// e.g. GCE, AWS
+		for i := range runes {
+			runes[i] = unicode.ToUpper(runes[i])
+		}
+	} else {
+		// e.g. Local, Azure
+		runes[0] = unicode.ToUpper(runes[0])
+	}
+	return string(runes)
+}
+
+// noun returns "tests" or "benchmarks" depending on the OnlyBenchmarks field.
+func (filter *TestFilter) noun() string {
+	if filter.OnlyBenchmarks {
+		return "benchmarks"
+	}
+	return "tests"
 }
