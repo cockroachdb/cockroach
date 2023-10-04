82dcb2c599952a6a72af7b9cef047eca310a69ae
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-10-03 18:52:42 -0700

    roachpb: optimize Spans.MemUsage
    In `Spans.MemUsage` I think we can assume that spans past the length
    (i.e. in `[len, cap]` range) are empty, so we can optimize `MemUsage`
    function a bit. This is also now enforced in the test builds.
    
    Epic: None
    
    Release note: None

--- pkg/sql/colfetcher/colbatch_scan.go
+++ pkg/sql/colfetcher/colbatch_scan.go
@@ -183,7 +183,7 @@ func newColBatchScanBase(
 		// (because the cFetcher requires that its allocator is not shared with
 		// any other component), but we can use the memory account of the KV
 		// fetcher.
-		if err = kvFetcherMemAcc.Grow(ctx, s.Spans.MemUsage()); err != nil {
+		if err = kvFetcherMemAcc.Grow(ctx, s.Spans.MemUsageUpToLen()); err != nil {
 			return nil, nil, nil, err
 		}
 		s.MakeSpansCopy()
