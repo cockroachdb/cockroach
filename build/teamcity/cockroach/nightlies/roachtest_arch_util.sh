de874547ece8fc7297d6e8ed78815cb15c96a450
Radu Berinde <radu@cockroachlabs.com>
2023-10-04 05:25:40 -0700

    build: improve roachtest build scripts
    We improve the roachtest scripts to support building finer-grained
    components: the `roachtest_compile_component.sh` script builds a
    single component (cockroach, cockroach-ea, workload, libgeos,
    roachtest). For each component we define the bazel arguments and build
    products, minimizing duplication of other code. There should be no
    functional changes.
    
    Epic: none
    Release note: None

---
+++ build/teamcity/cockroach/nightlies/roachtest_arch_util.sh
@@ -0,0 +1,27 @@
+# arch_to_config returns the bazel config for the given arch.
+function arch_to_config() {
+  case "$1" in
+    amd64)
+      echo "crosslinux"
+      ;;
+    arm64)
+      echo "crosslinuxarm"
+      ;;
+    amd64-fips)
+      echo "crosslinuxfips"
+      ;;
+    *)
+      echo "Error: invalid arch '$1'" >&2
+      exit 1
+      ;;
+  esac
+}
+
+# get_host_arch determines the current host's CPU architecture.
+function get_host_arch() {
+  if [[ "$(uname -m)" =~ (arm64|aarch64)$ ]]; then
+    echo "arm64"
+  else
+    echo "amd64"
+  fi
+}
