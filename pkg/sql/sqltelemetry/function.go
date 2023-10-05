1d5269497f9c9290c9378cbd2eafc9e94ff45ce7
Andy Yang <yang@cockroachlabs.com>
2023-10-05 02:14:01 -0400

    sqltelemetry: add framework to count builtin function calls
    This patch adds a framework to collect telemetry for builtin function
    calls. The counters will have a prefix of `sql.builtins` followed by
    the builtin type and then the builtin name.
    
    Release note: None

--- pkg/sql/sqltelemetry/function.go
+++ pkg/sql/sqltelemetry/function.go
@@ -10,10 +10,54 @@
 
 package sqltelemetry
 
-import "github.com/cockroachdb/cockroach/pkg/server/telemetry"
+import (
+	"fmt"
+
+	"github.com/cockroachdb/cockroach/pkg/server/telemetry"
+)
 
 // IncrementPlpgsqlStmtCounter is to be incremented every time a new plpgsql stmt is
 // used for a newly created function.
 func IncrementPlpgsqlStmtCounter(stmtTag string) {
 	telemetry.Inc(telemetry.GetCounter("sql.plpgsql." + stmtTag))
+}
+
+const builtinFunctionTelemetryPrefix = "sql.builtins"
+
+// BuiltinFunctionTelemetryType is an enum for the type of builtin function
+// that we are collecting telemetry for.
+type BuiltinFunctionTelemetryType int
+
+const (
+	_ BuiltinFunctionTelemetryType = iota
+)
+
+var builtinFunctionTelemetryNameMap = map[BuiltinFunctionTelemetryType]string{}
+
+var builtinFunctionTelemetryCounters map[BuiltinFunctionTelemetryType]map[string]telemetry.Counter
+
+func init() {
+	builtinFunctionTelemetryCounters = make(map[BuiltinFunctionTelemetryType]map[string]telemetry.Counter)
+	for typ := range builtinFunctionTelemetryNameMap {
+		builtinFunctionTelemetryCounters[typ] = make(map[string]telemetry.Counter)
+	}
+}
+
+// IncBuiltinFunctionCounter increments counter for the specified builtin.
+func IncBuiltinFunctionCounter(builtinType BuiltinFunctionTelemetryType, builtinName string) {
+	builtinTypeName := builtinFunctionTelemetryNameMap[builtinType]
+	builtinTypeCounters := builtinFunctionTelemetryCounters[builtinType]
+	if builtinTypeName == "" || builtinTypeCounters == nil {
+		// If the builtin type is missing from either map then it must be invalid.
+		return
+	}
+
+	counter, ok := builtinTypeCounters[builtinName]
+	if !ok {
+		counter = telemetry.GetCounter(fmt.Sprintf("%s.%s.%s",
+			builtinFunctionTelemetryPrefix, builtinTypeName, builtinName))
+		builtinTypeCounters[builtinName] = counter
+	}
+
+	telemetry.Inc(counter)
 }
