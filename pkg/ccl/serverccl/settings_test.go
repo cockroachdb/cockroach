36d0c6a4685c90c64f05712e744ff5f0a52bbccb
Raphael 'kena' Poss <knz@thaumogen.net>
2023-10-06 16:26:12 +0200

    ccl,ts: fix a couple setting classes
    These settings are used in DB Console -- retrieved from the API
    endpoint -- and thus must be readable from the application-level API
    handler.
    
    Release note: None

---
+++ pkg/ccl/serverccl/settings_test.go
@@ -0,0 +1,31 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package serverccl
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/settings"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+)
+
+func TestConsoleKeysVisibility(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	for _, key := range settings.ConsoleKeys() {
+		setting, found := settings.LookupForLocalAccessByKey(key, true /* forSystemTenant */)
+		if !found {
+			t.Fatalf("not found: %q", key)
+		}
+
+		if setting.Class() == settings.SystemOnly {
+			t.Errorf("setting %q used in console, cannot be system-only", key)
+		}
+	}
+}
