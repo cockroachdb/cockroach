cockroachdb/cockroach

commit 53ed1cd27a33e6ac89d314b5bc0c7db52f9f70d5

sql: disable READ COMMITTED syntax by default
Release note (sql change): The cluster setting
sql.txn.read_committed_syntax.enabled was added. It defaults to false.
When set to true, the following statements will configure transactions
to run under READ COMMITTED isolation, rather than being automatically
interpeted as SERIALIZABLE.
- BEGIN TRANSACTION ISOLATION LEVEL READ COMMITTED
- SET TRANSACTION ISOLATION LEVEL READ COMMITTED
- SET default_transaction_isolation = 'read committed'
- SET SESSION CHARACTERISTICS AS TRANSACTION ISOLATION LEVEL READ COMMITTED

This setting is added since READ COMMITTED transactions are a preview
feature, so usage of it is opt-in for v23.2. In a future CockroachDB major
version, this setting will change to default to true.

docs/generated/settings/settings.html
@@ -251,6 +251,7 @@
 <tr><td><div id="setting-sql-ttl-default-delete-rate-limit" class="anchored"><code>sql.ttl.default_delete_rate_limit</code></div></td><td>integer</td><td><code>0</code></td><td>default delete rate limit for all TTL jobs. Use 0 to signify no rate limit.</td><td>Serverless/Dedicated/Self-Hosted</td></tr>
 <tr><td><div id="setting-sql-ttl-default-select-batch-size" class="anchored"><code>sql.ttl.default_select_batch_size</code></div></td><td>integer</td><td><code>500</code></td><td>default amount of rows to select in a single query during a TTL job</td><td>Serverless/Dedicated/Self-Hosted</td></tr>
 <tr><td><div id="setting-sql-ttl-job-enabled" class="anchored"><code>sql.ttl.job.enabled</code></div></td><td>boolean</td><td><code>true</code></td><td>whether the TTL job is enabled</td><td>Serverless/Dedicated/Self-Hosted</td></tr>
+<tr><td><div id="setting-sql-txn-read-committed-syntax-enabled" class="anchored"><code>sql.txn.read_committed_syntax.enabled</code></div></td><td>boolean</td><td><code>false</code></td><td>set to true to allow transactions to use the READ COMMITTED isolation level if specified by BEGIN/SET commands</td><td>Serverless/Dedicated/Self-Hosted</td></tr>
 <tr><td><div id="setting-sql-txn-fingerprint-id-cache-capacity" class="anchored"><code>sql.txn_fingerprint_id_cache.capacity</code></div></td><td>integer</td><td><code>100</code></td><td>the maximum number of txn fingerprint IDs stored</td><td>Serverless/Dedicated/Self-Hosted</td></tr>
 <tr><td><div id="setting-storage-experimental-eventually-file-only-snapshots-enabled" class="anchored"><code>storage.experimental.eventually_file_only_snapshots.enabled</code></div></td><td>boolean</td><td><code>false</code></td><td>set to true to use eventually-file-only-snapshots</td><td>Dedicated/Self-Hosted</td></tr>
 <tr><td><div id="setting-storage-max-sync-duration" class="anchored"><code>storage.max_sync_duration</code></div></td><td>duration</td><td><code>20s</code></td><td>maximum duration for disk operations; any operations that take longer than this setting trigger a warning log entry or process crash</td><td>Serverless/Dedicated/Self-Hosted (read-only)</td></tr>

pkg/cmd/roachtest/tests/orm_helpers.go
@@ -63,8 +63,9 @@ func alterZoneConfigAndClusterSettings(
 		// Test with SCRAM password authentication.
 		`SET CLUSTER SETTING server.user_login.password_encryption = 'scram-sha-256';`,
 
-		// Enable experimental features.
+		// Enable experimental/preview features.
 		`SET CLUSTER SETTING sql.defaults.experimental_temporary_tables.enabled = 'true';`,
+		`SET CLUSTER SETTING sql.txn.read_committed_syntax.enabled = 'true';`,
 	} {
 		if _, err := db.ExecContext(ctx, cmd); err != nil {
 			return err

pkg/sql/conn_executor.go
@@ -3410,11 +3410,20 @@ func (ex *connExecutor) setTransactionModes(
 
 var allowSnapshotIsolation = settings.RegisterBoolSetting(
 	settings.TenantWritable,
-	"sql.txn.snapshot_isolation.enabled",
+	"sql.txn.snapshot_isolation_syntax.enabled",
 	"set to true to allow transactions to use the SNAPSHOT isolation level. At "+
 		"the time of writing, this setting is intended only for usage by "+
 		"CockroachDB developers.",
 	false,
+)
+
+var allowReadCommittedIsolation = settings.RegisterBoolSetting(
+	settings.TenantWritable,
+	"sql.txn.read_committed_syntax.enabled",
+	"set to true to allow transactions to use the READ COMMITTED isolation "+
+		"level if specified by BEGIN/SET commands",
+	false,
+	settings.WithPublic,
 )
 
 func (ex *connExecutor) txnIsolationLevelToKV(
@@ -3432,7 +3441,14 @@ func (ex *connExecutor) txnIsolationLevelToKV(
 			// this: https://www.postgresql.org/docs/current/transaction-iso.html.
 			fallthrough
 		case tree.ReadCommittedIsolation:
-			ret = isolation.ReadCommitted
+			// READ COMMITTED is only allowed if the cluster setting is enabled.
+			// Otherwise  it is mapped to SERIALIZABLE.
+			allowReadCommitted := allowReadCommittedIsolation.Get(&ex.server.cfg.Settings.SV)
+			if allowReadCommitted {
+				ret = isolation.ReadCommitted
+			} else {
+				ret = isolation.Serializable
+			}
 		case tree.RepeatableReadIsolation:
 			// REPEATABLE READ is mapped to SNAPSHOT.
 			fallthrough

pkg/sql/conn_executor_test.go
@@ -1451,7 +1451,9 @@ func TestInjectRetryErrors(t *testing.T) {
 	defer s.Stopper().Stop(ctx)
 	defer db.Close()
 
-	_, err := db.Exec("SET inject_retry_errors_enabled = 'true'")
+	_, err := db.Exec(`SET CLUSTER SETTING sql.txn.read_committed_syntax.enabled = true`)
+	require.NoError(t, err)
+	_, err = db.Exec("SET inject_retry_errors_enabled = 'true'")
 	require.NoError(t, err)
 
 	t.Run("with_savepoints", func(t *testing.T) {

pkg/sql/logictest/testdata/logic_test/fk_read_committed
@@ -1,5 +1,8 @@
 # LogicTest: !local-mixed-22.2-23.1
 
+statement ok
+SET CLUSTER SETTING sql.txn.read_committed_syntax.enabled = true
+
 # Some foreign key checks are prohibited under weaker isolation levels until we
 # improve locking. See #80683, #100156, #100193.
 

pkg/sql/logictest/testdata/logic_test/read_committed
@@ -1,5 +1,8 @@
 # LogicTest: !local-mixed-22.2-23.1
 
+statement ok
+SET CLUSTER SETTING sql.txn.read_committed_syntax.enabled = true
+
 subtest select_for_update
 
 # SELECT FOR UPDATE is prohibited under weaker isolation levels until we improve
@@ -46,7 +49,7 @@ ROLLBACK
 
 # It should also fail under snapshot isolation.
 statement ok
-SET CLUSTER SETTING sql.txn.snapshot_isolation.enabled = true
+SET CLUSTER SETTING sql.txn.snapshot_isolation_syntax.enabled = true
 
 statement ok
 BEGIN TRANSACTION ISOLATION LEVEL SNAPSHOT
@@ -58,7 +61,7 @@ statement ok
 ROLLBACK
 
 statement ok
-RESET CLUSTER SETTING sql.txn.snapshot_isolation.enabled
+RESET CLUSTER SETTING sql.txn.snapshot_isolation_syntax.enabled
 
 # SELECT FOR UPDATE in a subquery should also fail under read committed.
 statement ok

pkg/sql/logictest/testdata/logic_test/txn
@@ -208,6 +208,9 @@ BEGIN TRANSACTION; SET TRANSACTION ISOLATION LEVEL READ COMMITTED; COMMIT
 # It is an error to change the isolation level of a running transaction.
 
 statement ok
+SET CLUSTER SETTING sql.txn.read_committed_syntax.enabled = true
+
+statement ok
 BEGIN TRANSACTION
 
 statement ok
@@ -233,6 +236,9 @@ SET transaction_isolation = 'READ COMMITTED'
 statement ok
 ROLLBACK
 
+statement ok
+RESET CLUSTER SETTING sql.txn.read_committed_syntax.enabled
+
 # Transactions default to serializable.
 
 statement ok
@@ -245,6 +251,15 @@ serializable
 
 query T
 SHOW transaction_isolation
+----
+serializable
+
+# READ COMMITTED is mapped to serializable by default.
+statement ok
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+query T
+SHOW TRANSACTION ISOLATION LEVEL
 ----
 serializable
 
@@ -265,9 +280,38 @@ serializable
 statement ok
 COMMIT
 
+statement ok
+BEGIN TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+query T
+SHOW TRANSACTION ISOLATION LEVEL
+----
+serializable
+
+statement ok
+COMMIT
+
+# Since read_committed_isolation_syntax.enabled is true, setting isolation level
+# to REPEATABLE READ should map to SNAPSHOT.
+statement ok
+SET default_transaction_isolation = 'read committed'
+
+query T
+SHOW default_transaction_isolation
+----
+serializable
+
+statement ok
+SET SESSION CHARACTERISTICS AS TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+query T
+SHOW DEFAULT_TRANSACTION_ISOLATION
+----
+serializable
+
 # SNAPSHOT can be used with a hidden cluster setting
 statement ok
-SET CLUSTER SETTING sql.txn.snapshot_isolation.enabled = true
+SET CLUSTER SETTING sql.txn.snapshot_isolation_syntax.enabled = true
 
 statement ok
 BEGIN
@@ -303,7 +347,7 @@ statement ok
 COMMIT
 
 # We can't set isolation level to an unsupported one.
-statement error invalid value for parameter "transaction_isolation": "this is made up"\n.*Available values: serializable,snapshot,read committed
+statement error invalid value for parameter "transaction_isolation": "this is made up"\n.*Available values: serializable,snapshot
 SET transaction_isolation = 'this is made up'
 
 # We can explicitly start a transaction with isolation level specified.
@@ -335,7 +379,10 @@ statement ok
 COMMIT
 
 statement ok
-SET CLUSTER SETTING sql.txn.snapshot_isolation.enabled = false
+SET CLUSTER SETTING sql.txn.snapshot_isolation_syntax.enabled = false
+
+statement ok
+SET CLUSTER SETTING sql.txn.read_committed_syntax.enabled = true
 
 statement ok
 BEGIN TRANSACTION ISOLATION LEVEL READ COMMITTED
@@ -680,7 +727,7 @@ statement ok
 COMMIT
 
 statement ok
-SET CLUSTER SETTING sql.txn.snapshot_isolation.enabled = true
+SET CLUSTER SETTING sql.txn.snapshot_isolation_syntax.enabled = true
 
 statement ok
 BEGIN TRANSACTION ISOLATION LEVEL SNAPSHOT, PRIORITY LOW
@@ -723,7 +770,7 @@ SHOW DEFAULT_TRANSACTION_ISOLATION
 snapshot
 
 statement ok
-SET CLUSTER SETTING sql.txn.snapshot_isolation.enabled = false
+SET CLUSTER SETTING sql.txn.snapshot_isolation_syntax.enabled = false
 
 statement ok
 SET SESSION CHARACTERISTICS AS TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
@@ -795,7 +842,7 @@ SHOW default_transaction_isolation
 read committed
 
 statement ok
-SET CLUSTER SETTING sql.txn.snapshot_isolation.enabled = true
+SET CLUSTER SETTING sql.txn.snapshot_isolation_syntax.enabled = true
 
 # Since snapshot_isolation.enabled is true, setting isolation level to
 # REPEATABLE READ should map to SNAPSHOT.
@@ -816,7 +863,7 @@ SHOW DEFAULT_TRANSACTION_ISOLATION
 snapshot
 
 statement ok
-SET CLUSTER SETTING sql.txn.snapshot_isolation.enabled = false
+SET CLUSTER SETTING sql.txn.snapshot_isolation_syntax.enabled = false
 
 # Since snapshot_isolation.enabled is false, setting isolation level to SNAPSHOT
 # should map to SERIALIZABLE.

pkg/sql/opt/exec/execbuilder/testdata/explain_analyze
@@ -1,6 +1,9 @@
 # LogicTest: local
 
 statement ok
+SET CLUSTER SETTING sql.txn.read_committed_syntax.enabled = true
+
+statement ok
 CREATE TABLE kv (k INT PRIMARY KEY, v INT, FAMILY (k, v))
 
 query T

commit b43590a0fc6a83f386c55e23db1fc41eae22dd2c

roachtest: fix read committed variant of ycsb
This was broken by 53ed1cd2.

Epic: None
Release note: None

pkg/cmd/roachtest/tests/ycsb.go
@@ -12,6 +12,7 @@ package tests
 
 import (
 	"context"
+	gosql "database/sql"
 	"fmt"
 	"os"
 
@@ -69,8 +70,13 @@ func registerYCSB(r registry.Registry) {
 		c.Put(ctx, t.Cockroach(), "./cockroach", c.Range(1, nodes))
 		c.Put(ctx, t.DeprecatedWorkload(), "./workload", c.Node(nodes+1))
 		c.Start(ctx, t.L(), option.DefaultStartOptsNoBackups(), settings, c.Range(1, nodes))
-		err := WaitFor3XReplication(ctx, t, c.Conn(ctx, t.L(), 1))
+
+		db := c.Conn(ctx, t.L(), 1)
+		err := enableIsolationLevels(ctx, t, db)
 		require.NoError(t, err)
+		err = WaitFor3XReplication(ctx, t, db)
+		require.NoError(t, err)
+		require.NoError(t, db.Close())
 
 		t.Status("running workload")
 		m := c.NewMonitor(ctx, c.Range(1, nodes))
@@ -162,4 +168,16 @@ func registerYCSB(r registry.Registry) {
 			}
 		}
 	}
+}
+
+func enableIsolationLevels(ctx context.Context, t test.Test, db *gosql.DB) error {
+	for _, cmd := range []string{
+		`SET CLUSTER SETTING sql.txn.snapshot_isolation_syntax.enabled = 'true';`,
+		`SET CLUSTER SETTING sql.txn.read_committed_syntax.enabled = 'true';`,
+	} {
+		if _, err := db.ExecContext(ctx, cmd); err != nil {
+			return err
+		}
+	}
+	return nil
 }