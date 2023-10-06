55816936e9a8b0bb763e1f82ef129029593bda77
Renato Costa <renato@cockroachlabs.com>
2023-10-06 11:21:21 -0400

    roachtest: remove direct references to cockroach.service from tests
    After #111064, we started naming the systemd unit for cockroach
    processes differently, since we now might have multiple cockroach
    processes in the same VM in the context of SQL server processes and
    cluster virtualization.
    
    That change broke a few tests that directly referenced the
    `cockroach.service` unit. In this change, we create a util function
    that is reponsible for encapsulating that name and replace direct
    references to the systemd unit name with calls to this new function.
    
    Fixes: #111902
    
    Release note: None

---
+++ pkg/cmd/roachtest/roachtestutil/utils.go
@@ -0,0 +1,19 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package roachtestutil
+
+import "github.com/cockroachdb/cockroach/pkg/roachprod/install"
+
+// SystemInterfaceSystemdUnitName is a convenience function that
+// returns the systemd unit name for the system interface
+func SystemInterfaceSystemdUnitName() string {
+	return install.VirtualClusterLabel(install.SystemInterfaceName, 0)
+}
