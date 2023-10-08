ece032b005011a38e409655e7998a75c3249e8c3
Celia La <celia@cockroachlabs.com>
2023-10-08 12:39:11 -0700

    clusterversion: set developmentBranch to false
    This sets developmentBranch to false for the release branch.
    We plan to backport this commit as soon as the branch is cut.
    
    Release note: none.
    Epic: REL-437.

--- pkg/ccl/logictestccl/tests/3node-tenant/generated_test.go
+++ pkg/ccl/logictestccl/tests/3node-tenant/generated_test.go
@@ -1206,6 +1206,13 @@ func TestTenantLogic_merge_join(
 	runLogicTest(t, "merge_join")
 }
 
+func TestTenantLogic_mixed_version_range_tombstones(
+	t *testing.T,
+) {
+	defer leaktest.AfterTest(t)()
+	runLogicTest(t, "mixed_version_range_tombstones")
+}
+
 func TestTenantLogic_multi_statement(
 	t *testing.T,
 ) {
