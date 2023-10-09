44f8cd482ff5c513159d37a7d3b1f5852fc7facb
Steven Danna <danna@cockroachlabs.com>
2023-10-09 17:35:34 +0100

    server: remove --secondary-tenant-port-offset
    The new internal-rpc-port-range provides the user to specify a known
    range of ports to use for internal RPC listeners. The port offset
    setting was attempting to provide predictability but without a clear
    upper bound.
    
    While we could leave this configuration option in place, it only
    becomes harder to remove if we continue to have it in place in 23.2.
    
    Release note (cluster virtualization): This removes the hidden
    --secondary-tenant-port-offset option. Users who were previously using
    this option should use --internal-rpc-port-range instead.

--- pkg/base/addr_validation.go
+++ pkg/base/addr_validation.go
@@ -83,11 +83,6 @@ func (cfg *Config) ValidateAddrs(ctx context.Context) error {
 	}
 	cfg.HTTPAddr = net.JoinHostPort(httpHost, httpPort)
 
-	// Validate secondary tenant port configuration.
-	if cfg.SecondaryTenantPortOffset > 0 && cfg.ApplicationInternalRPCPortMin > 0 {
-		return errors.Newf("cannot specify both %s and %s", cliflags.ApplicationInternalRPCPortRange.Name, cliflags.SecondaryTenantPortOffset.Name)
-	}
-
 	return nil
 }
 
