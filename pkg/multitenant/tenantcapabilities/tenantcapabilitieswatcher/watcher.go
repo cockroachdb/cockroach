601d6cc69151c7b1b1961158336a8cb9fb71598f
Raphael 'kena' Poss <knz@thaumogen.net>
2023-10-10 16:09:32 +0200

    tenantcapabilitieswatcher: make the watcher react faster
    Prior to this patch, the tenant info watcher would only react to
    changes to `system.tenants` upon rangefeed cache flushes, which could
    be (in default config) up to 3 seconds after the change is committed.
    
    This commit accelerates the behavior by processing updates as soon as
    the change is observed in the rangefeed.
    
    This new behavior is similar to the way that cluster settings changes
    are effected immediately in the settings watcher (pkg/settingswatcher).
    
    Release note: None

--- pkg/multitenant/tenantcapabilities/tenantcapabilitieswatcher/watcher.go
+++ pkg/multitenant/tenantcapabilities/tenantcapabilitieswatcher/watcher.go
@@ -18,6 +18,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/kv/kvclient/rangefeed"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvclient/rangefeed/rangefeedbuffer"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvclient/rangefeed/rangefeedcache"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
 	"github.com/cockroachdb/cockroach/pkg/multitenant/tenantcapabilities"
 	"github.com/cockroachdb/cockroach/pkg/multitenant/tenantcapabilities/tenantcapabilitiespb"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
@@ -231,8 +232,8 @@ func (w *Watcher) startRangeFeed(ctx context.Context) error {
 		int(w.bufferMemLimit/tenantInfoEntrySize), /* bufferSize */
 		[]roachpb.Span{tenantsTableSpan},
 		true, /* withPrevValue */
-		w.decoder.translateEvent,
-		w.handleUpdate,
+		w.handleIncrementalUpdate,
+		w.handleRangefeedCacheEvent,
 		rfcTestingKnobs,
 	)
 	if err := rangefeedcache.Start(ctx, w.stopper, rfc, w.onError); err != nil {
@@ -275,34 +276,18 @@ func (w *Watcher) onError(err error) {
 	}
 }
 
-func (w *Watcher) handleUpdate(ctx context.Context, u rangefeedcache.Update) {
-	var updates []tenantcapabilities.Update
-	for _, ev := range u.Events {
-		update := ev.(*bufferEvent).update
-		updates = append(updates, update)
-	}
-
-	if fn := w.knobs.WatcherUpdatesInterceptor; fn != nil {
-		fn(u.Type, updates)
-	}
-
+func (w *Watcher) handleRangefeedCacheEvent(ctx context.Context, u rangefeedcache.Update) {
 	switch u.Type {
 	case rangefeedcache.CompleteUpdate:
 		log.Info(ctx, "received results of a full table scan for tenant capabilities")
-		w.handleCompleteUpdate(ctx, updates)
 		if !w.initialScan.done {
 			w.initialScan.done = true
 			close(w.initialScan.ch)
 		}
 	case rangefeedcache.IncrementalUpdate:
-		w.handleIncrementalUpdate(ctx, updates)
 	default:
 		err := errors.AssertionFailedf("unknown update type: %v", u.Type)
 		logcrash.ReportOrPanic(ctx, &w.st.SV, "%w", err)
-	}
-
-	if len(updates) > 0 {
-		w.closeAnyChangeCh()
 	}
 }
 
@@ -313,75 +298,54 @@ func (w *Watcher) closeAnyChangeCh() {
 	w.mu.anyChangeCh = make(chan struct{})
 }
 
-func (w *Watcher) handleCompleteUpdate(ctx context.Context, updates []tenantcapabilities.Update) {
-	// Populate a fresh store with the supplied updates.
-	// A Complete update indicates that the initial table scan is complete. This
-	// happens when the rangefeed is first established, or if it's restarted for
-	// some reason. Either way, we want to throw away any accumulated state so
-	// far, and reconstruct it using the result of the scan.
-	freshStore := make(map[roachpb.TenantID]*watcherEntry)
-	byName := make(map[roachpb.TenantName]roachpb.TenantID)
-	for i := range updates {
-		up := &updates[i]
-		if log.V(2) {
-			log.Infof(ctx, "adding initial tenant entry to cache: %+v", up.Entry)
-		}
-		freshStore[up.TenantID] = &watcherEntry{
-			Entry:    &up.Entry,
-			changeCh: make(chan struct{}),
-		}
-		if up.Entry.Name != "" {
-			byName[up.Entry.Name] = up.TenantID
-		}
+func (w *Watcher) handleIncrementalUpdate(
+	ctx context.Context, ev *kvpb.RangeFeedValue,
+) rangefeedbuffer.Event {
+	hasEvent, update := w.decoder.translateEvent(ctx, ev)
+	if !hasEvent {
+		return nil
+	}
+
+	if fn := w.knobs.WatcherUpdatesInterceptor; fn != nil {
+		fn(update)
 	}
 
 	w.mu.Lock()
 	defer w.mu.Unlock()
 
-	// Notify any previous watchers that the state has changed.
-	for _, entry := range w.mu.store {
+	tid := update.TenantID
+	if entry := w.mu.store[tid]; entry != nil {
+		// Notify previous watchers, if any, that the entry is getting updated.
 		close(entry.changeCh)
 	}
 
-	w.mu.store = freshStore
-	w.mu.byName = byName
-}
+	// Notify previous watchers of the all-tenants accessor, if any,
+	// that some entry is getting updated.
+	close(w.mu.anyChangeCh)
+	w.mu.anyChangeCh = make(chan struct{})
 
-func (w *Watcher) handleIncrementalUpdate(
-	ctx context.Context, updates []tenantcapabilities.Update,
-) {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-
-	for i := range updates {
-		update := &updates[i]
-		tid := update.TenantID
-		if entry := w.mu.store[tid]; entry != nil {
-			// Notify previous watchers, if any, that the entry is getting updated.
-			close(entry.changeCh)
+	if update.Deleted {
+		if log.V(2) {
+			log.Infof(ctx, "removing tenant entry from cache: %+v", update.Entry)
 		}
-
-		if update.Deleted {
-			if log.V(2) {
-				log.Infof(ctx, "removing tenant entry from cache: %+v", update.Entry)
-			}
-			if entry := w.mu.store[tid]; entry != nil {
-				delete(w.mu.byName, entry.Name)
-			}
-			delete(w.mu.store, tid)
-		} else {
-			if log.V(2) {
-				log.Infof(ctx, "adding tenant entry to cache: %+v", update.Entry)
-			}
-			w.mu.store[update.TenantID] = &watcherEntry{
-				Entry:    &update.Entry,
-				changeCh: make(chan struct{}),
-			}
-			if update.Entry.Name != "" {
-				w.mu.byName[update.Entry.Name] = update.TenantID
-			}
+		if entry := w.mu.store[tid]; entry != nil {
+			delete(w.mu.byName, entry.Name)
+		}
+		delete(w.mu.store, tid)
+	} else {
+		if log.V(2) {
+			log.Infof(ctx, "adding tenant entry to cache: %+v", update.Entry)
+		}
+		w.mu.store[update.TenantID] = &watcherEntry{
+			Entry:    &update.Entry,
+			changeCh: make(chan struct{}),
+		}
+		if update.Entry.Name != "" {
+			w.mu.byName[update.Entry.Name] = update.TenantID
 		}
 	}
+
+	return nil
 }
 
 func (w *Watcher) TestingRestart() {
