4773dd08137ec67bcc221181de0a1ea793357b20
Annie Pompa <annie@cockroachlabs.com>
2023-10-18 11:25:08 -0400

    backupccl: retire bulkio.restore.remove_regions.enabled
    This patch adds cluster setting bulkio.restore.remove_regions.enabled
    to the retiredSettings map. The cluster setting was added to 23.1 with
    the following context: #111863.
    
    Epic: none
    Fixes: #112199
    
    Release note: None

--- pkg/settings/registry.go
+++ pkg/settings/registry.go
@@ -227,6 +227,7 @@ var retiredSettings = map[InternalKey]struct{}{
 	"changefeed.lagging_ranges_polling_rate":                   {},
 	"trace.jaeger.agent":                                       {},
 	"bulkio.restore.use_simple_import_spans":                   {},
+	"bulkio.restore.remove_regions.enabled":                    {},
 
 	// removed as of 24.1
 	"storage.mvcc.range_tombstones.enabled": {},
