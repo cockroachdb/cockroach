7745487ff7884caf6e22612e73aaa02614bb512e
Arul Ajmani <arulajmani@gmail.com>
2023-10-19 17:34:22 -0500

    concurrency: correctly handle lock updates that decrease its strength
    When a lock is updated on behalf of a non-finalized transaction, the
    lock table simply forgets any replicated lock information it was
    previously tracking. Now, if a lock is held with both replicated and
    unreplicated durability, and the replicated lock's strength is stronger
    than the unreplicated lock's strength, forgetting the replicated lock
    means there may be requests waiting on a key that no longer conflict
    with what's being tracked in memory[1]. In such cases, requests that no
    longer conflict with the in-memory tracking should no longer wait on
    this key -- they should be allowed to proceed with their scan. It's
    not guaranteed that they no longer conflict with the lock -- they might
    rediscover the replicated lock again and start waiting. However, if they
    no longer conflict with the lock, blocking them indefinitely can lead to
    undetected deadlocks in rare cases[2].
    
    [1] Concretely, consider the following construction:
    - key a: [txn 1, (repl intent ts@10), (unrepl shared)]
      - wait-queue readers: [none ts@12], [none ts@15]
      - wait-queue locking requests: [shared], [shared], [exclusive] [shared]
    In this case, updating the intent's timestamp to ts@14 (and forgetting
    it) should allow both the non-locking readers and first two shared
    locking requests to become compatible with what's being tracked in
    memory for this key.
    [2] This can happen if the lock update corresponds to a successful
    PUSH_TIMESTAMP. The pusher will be blocked in the lock table waiter
    waiting for a `waitingState` update that never comes. Now, if there
    was a dependency cycle between the pusher and pushee had the push
    timestamp been blocking, we would find ourselves in an undetected
    deeadlock situation.
    
    Fixes #112608
    
    Release note: None

--- pkg/kv/kvserver/concurrency/lock/locking_test.go
+++ pkg/kv/kvserver/concurrency/lock/locking_test.go
@@ -404,3 +404,70 @@ func TestCheckLockConflicts_IntentWithIntent(t *testing.T) {
 		)
 	}
 }
+
+// TestLockModeWeaker tests strength comparison semantics for various lock mode
+// combinations.
+func TestLockModeWeaker(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	tsBelow := makeTS(1)
+	tsLock := makeTS(2)
+	tsAbove := makeTS(3)
+
+	testCases := []struct {
+		m1  lock.Mode
+		m2  lock.Mode
+		exp bool
+	}{
+		{
+			m1:  lock.MakeModeNone(tsLock, isolation.Serializable),
+			m2:  lock.MakeModeNone(tsBelow, isolation.Serializable), // stronger
+			exp: true,
+		},
+		{
+			m1:  lock.MakeModeNone(tsLock, isolation.Serializable), // stronger
+			m2:  lock.MakeModeNone(tsAbove, isolation.Serializable),
+			exp: false,
+		},
+		{
+			m1:  lock.MakeModeIntent(tsLock), // stronger
+			m2:  lock.MakeModeNone(tsBelow, isolation.Serializable),
+			exp: false,
+		},
+		{
+			m1:  lock.MakeModeIntent(tsLock),
+			m2:  lock.MakeModeIntent(tsBelow), // stronger
+			exp: true,
+		},
+		{
+			m1:  lock.MakeModeIntent(tsLock), // stronger
+			m2:  lock.MakeModeIntent(tsAbove),
+			exp: false,
+		},
+		{
+			m1:  lock.MakeModeIntent(tsLock), // stronger
+			m2:  lock.MakeModeShared(),
+			exp: false,
+		},
+		{
+			m1:  lock.MakeModeIntent(tsLock), // stronger
+			m2:  lock.MakeModeUpdate(),
+			exp: false,
+		},
+		{
+			m1:  lock.MakeModeIntent(tsLock), // stronger
+			m2:  lock.MakeModeExclusive(tsBelow, isolation.Serializable),
+			exp: false,
+		},
+		{
+			m1:  lock.MakeModeIntent(tsLock), // stronger
+			m2:  lock.MakeModeExclusive(tsAbove, isolation.Serializable),
+			exp: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		require.Equal(t, tc.exp, tc.m1.Weaker(tc.m2))
+	}
+}
