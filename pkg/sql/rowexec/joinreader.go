e775d1f7fd94ec4bda24b01a1fe9a50f0459bf9d
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-10-25 20:28:21 +0000

    sql: add session variable to force streamer to maintain ordering
    This commit adds `streamer_always_maintain_ordering` session variable
    that - when set to `true` - forces all current usages of the streamer in
    SQL layer (lookup and index joins) to ask it to maintain the ordering,
    even if this is not stricly necessary by the query. This variable is
    introduced as a possible workaround in case we find more scenarios where
    we currently are incorrectly using the OutOfOrder mode of the streamer.
    
    Release note: None

--- pkg/sql/rowexec/joinreader.go
+++ pkg/sql/rowexec/joinreader.go
@@ -537,6 +537,9 @@ func newJoinReader(
 				jr.streamerInfo.maintainOrdering = true
 			}
 		}
+		if jr.FlowCtx.EvalCtx.SessionData().StreamerAlwaysMaintainOrdering {
+			jr.streamerInfo.maintainOrdering = true
+		}
 
 		var diskBuffer kvstreamer.ResultDiskBuffer
 		if jr.streamerInfo.maintainOrdering {
