2d75c70a411865ab6a2f64cfe2f6fe35dfe68db3
Rafi Shamim <rafi@cockroachlabs.com>
2023-10-25 13:05:36 -0400

    sql/catalog: fix SHOW CREATE for columns with collated strings
    This commit also fixes a test bug that prevented an existing test from
    verifying that all the tables in the test would round-trip with SHOW CREATE.
    
    Release note (bug fix): The result of SHOW CREATE for a table that has a
    collated string column with a default expression was incorrect, since
    the statement was not parseable. This is now fixed.

--- pkg/sql/show_create_all_tables_builtin_test.go
+++ pkg/sql/show_create_all_tables_builtin_test.go
@@ -18,6 +18,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/stretchr/testify/require"
 )
 
 // Use the output from crdb_internal.show_create_all_tables() to recreate the
@@ -36,22 +37,33 @@ func TestRecreateTables(t *testing.T) {
 	sqlRunner.Exec(t, `USE test;`)
 	sqlRunner.Exec(t, `CREATE TABLE foo(x INT primary key);`)
 	sqlRunner.Exec(t, `CREATE TABLE bar(x INT, y INT, z STRING, FAMILY f1(x, y, z))`)
+	sqlRunner.Exec(t, `
+		CREATE TABLE tab (
+			a STRING COLLATE en,
+			b STRING COLLATE en_US,
+			c STRING COLLATE en_US DEFAULT ('c' COLLATE en_US),
+			d STRING COLLATE en_u_ks_level1 DEFAULT ('d'::STRING COLLATE en_u_ks_level1),
+			e STRING COLLATE en_US AS (a COLLATE en_US) STORED,
+			f STRING COLLATE en_US ON UPDATE ('f' COLLATE en_US))`)
 
-	row := sqlRunner.QueryRow(t, "SELECT crdb_internal.show_create_all_tables('test')")
-	var recreateTablesStmt string
-	row.Scan(&recreateTablesStmt)
+	var recreateTablesStmts []string
+	for _, r := range sqlRunner.QueryStr(t, "SELECT crdb_internal.show_create_all_tables('test')") {
+		recreateTablesStmts = append(recreateTablesStmts, r[0])
+	}
 
 	// Use the recreateTablesStmt to recreate the tables, perform another
 	// show_create_all_tables and compare that the output is the same.
 	sqlRunner.Exec(t, `DROP DATABASE test;`)
 	sqlRunner.Exec(t, `CREATE DATABASE test;`)
-	sqlRunner.Exec(t, recreateTablesStmt)
 
-	row = sqlRunner.QueryRow(t, "SELECT crdb_internal.show_create_all_tables('test')")
-	var recreateTablesStmt2 string
-	row.Scan(&recreateTablesStmt2)
-
-	if recreateTablesStmt != recreateTablesStmt2 {
-		t.Fatalf("got: %s\nexpected: %s", recreateTablesStmt2, recreateTablesStmt)
+	for _, stmt := range recreateTablesStmts {
+		sqlRunner.Exec(t, stmt)
 	}
+
+	var recreateTablesStmts2 []string
+	for _, r := range sqlRunner.QueryStr(t, "SELECT crdb_internal.show_create_all_tables('test')") {
+		recreateTablesStmts2 = append(recreateTablesStmts2, r[0])
+	}
+
+	require.ElementsMatch(t, recreateTablesStmts, recreateTablesStmts2)
 }
