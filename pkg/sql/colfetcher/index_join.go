53d2f87cf26e1989973f6da124a1563ea6a10ec6
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-10-25 13:28:21 -0700

    sql: add session variable to force streamer to maintain ordering
    This commit adds `streamer_always_maintain_ordering` session variable
    that - when set to `true` - forces all current usages of the streamer in
    SQL layer (lookup and index joins) to ask it to maintain the ordering,
    even if this is not stricly necessary by the query. This variable is
    introduced as a possible workaround in case we find more scenarios where
    we currently are incorrectly using the OutOfOrder mode of the streamer.
    
    Release note: None

--- pkg/sql/colfetcher/index_join.go
+++ pkg/sql/colfetcher/index_join.go
@@ -498,9 +498,6 @@ func getIndexJoinBatchSize(
 }
 
 // NewColIndexJoin creates a new ColIndexJoin operator.
-//
-// If spec.MaintainOrdering is true, or spec.SplitFamilyIDs has more than one
-// family, then the diskMonitor argument must be non-nil.
 func NewColIndexJoin(
 	ctx context.Context,
 	allocator *colmem.Allocator,
@@ -559,6 +556,9 @@ func NewColIndexJoin(
 		// single SQL row are contiguous we must ask the streamer to preserve
 		// the ordering. See #113013 for an example.
 		maintainOrdering := spec.MaintainOrdering || len(spec.SplitFamilyIDs) > 1
+		if flowCtx.EvalCtx.SessionData().StreamerAlwaysMaintainOrdering {
+			maintainOrdering = true
+		}
 		if maintainOrdering && diskMonitor == nil {
 			return nil, errors.AssertionFailedf("diskMonitor is nil when ordering needs to be maintained")
 		}
