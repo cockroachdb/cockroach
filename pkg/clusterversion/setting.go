8e918191431de2c7627795976c704da37e2b65da
Pavel Kalinnikov <pavel@cockroachlabs.com>
2023-10-25 13:58:12 +0000

    clusterversion: rm allocs in ClusterVersion.Unmarshal
    Epic: none
    Release note: none

--- pkg/clusterversion/setting.go
+++ pkg/clusterversion/setting.go
@@ -153,7 +153,7 @@ func (cv *clusterVersionSetting) isActive(
 // Decode is part of the VersionSettingImpl interface.
 func (cv *clusterVersionSetting) Decode(val []byte) (settings.ClusterVersionImpl, error) {
 	var clusterVersion ClusterVersion
-	if err := protoutil.Unmarshal(val, &clusterVersion); err != nil {
+	if err := clusterVersion.Unmarshal(val); err != nil {
 		return nil, err
 	}
 	return clusterVersion, nil
@@ -164,7 +164,7 @@ func (cv *clusterVersionSetting) ValidateVersionUpgrade(
 	_ context.Context, sv *settings.Values, curRawProto, newRawProto []byte,
 ) error {
 	var newCV ClusterVersion
-	if err := protoutil.Unmarshal(newRawProto, &newCV); err != nil {
+	if err := newCV.Unmarshal(newRawProto); err != nil {
 		return err
 	}
 
@@ -173,7 +173,7 @@ func (cv *clusterVersionSetting) ValidateVersionUpgrade(
 	}
 
 	var oldCV ClusterVersion
-	if err := protoutil.Unmarshal(curRawProto, &oldCV); err != nil {
+	if err := oldCV.Unmarshal(curRawProto); err != nil {
 		return err
 	}
 
@@ -209,7 +209,7 @@ func (cv *clusterVersionSetting) ValidateBinaryVersions(
 	}()
 
 	var ver ClusterVersion
-	if err := protoutil.Unmarshal(rawProto, &ver); err != nil {
+	if err := ver.Unmarshal(rawProto); err != nil {
 		return err
 	}
 	return cv.validateBinaryVersions(ver.Version, sv)
