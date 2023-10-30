955adc50f13ba0a1f1f1d13c2b54dc590d689d80
Erik Grinaker <grinaker@cockroachlabs.com>
2023-10-30 12:31:41 +0000

    rangefeed: add `kv.rangefeed.push_txns.enabled`
    This patch adds `kv.rangefeed.push_txns.enabled`, which can be used to
    disable rangefeed txn pushes, e.g. if they cause excessive contention.
    The cluster setting is meant to be used temporarily, since disabling txn
    pushes can cause the rangefeed resolved timestamp to fall arbitrarily
    far behind.
    
    Epic: none
    Release note: None

--- pkg/kv/kvserver/rangefeed/processor.go
+++ pkg/kv/kvserver/rangefeed/processor.go
@@ -18,6 +18,8 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/settings"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/storage/enginepb"
 	"github.com/cockroachdb/cockroach/pkg/util/future"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
@@ -39,6 +41,17 @@ const (
 	defaultCheckStreamsInterval = 1 * time.Second
 )
 
+var (
+	// PushTxnsEnabled can be used to disable rangefeed txn pushes, typically to
+	// temporarily alleviate contention.
+	PushTxnsEnabled = settings.RegisterBoolSetting(
+		settings.SystemOnly,
+		"kv.rangefeed.push_txns.enabled",
+		"periodically push txn write timestamps to advance rangefeed resolved timestamps",
+		true,
+	)
+)
+
 // newErrBufferCapacityExceeded creates an error that is returned to subscribers
 // if the rangefeed processor is not able to keep up with the flow of incoming
 // events and is forced to drop events in order to not block.
@@ -51,9 +64,10 @@ func newErrBufferCapacityExceeded() *kvpb.Error {
 // Config encompasses the configuration required to create a Processor.
 type Config struct {
 	log.AmbientContext
-	Clock   *hlc.Clock
-	RangeID roachpb.RangeID
-	Span    roachpb.RSpan
+	Clock    *hlc.Clock
+	Settings *cluster.Settings
+	RangeID  roachpb.RangeID
+	Span     roachpb.RSpan
 
 	TxnPusher TxnPusher
 	// PushTxnsInterval specifies the interval at which a Processor will push
@@ -358,9 +372,9 @@ func (p *Processor) run(
 
 		// Check whether any unresolved intents need a push.
 		case <-txnPushTickerC:
-			// Don't perform transaction push attempts until the resolved
-			// timestamp has been initialized.
-			if !p.rts.IsInit() {
+			// Don't perform transaction push attempts if disabled or until the
+			// resolved timestamp has been initialized.
+			if !PushTxnsEnabled.Get(&p.Settings.SV) || !p.rts.IsInit() {
 				continue
 			}
 
