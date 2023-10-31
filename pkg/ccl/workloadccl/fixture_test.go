8cc02c3d227ad8b4c5e959f3425d05c80dbc5cf6
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-10-31 10:10:28 -0700

    workloadccl: disable multi-tenant config in TestImportFixture
    This test regularly fails under stress in multi-tenant setup even with
    the retry loop waiting for auto stats to be collected after the import
    succeeds. In order to avoid unnecessary noise, this commit disables
    multi-tenant config.
    
    Release note: None

--- pkg/ccl/workloadccl/fixture_test.go
+++ pkg/ccl/workloadccl/fixture_test.go
@@ -182,7 +182,12 @@ func TestImportFixture(t *testing.T) {
 	stats.DefaultRefreshInterval = time.Millisecond
 	stats.DefaultAsOfTime = 10 * time.Millisecond
 
-	s, db, _ := serverutils.StartServer(t, base.TestServerArgs{})
+	s, db, _ := serverutils.StartServer(t, base.TestServerArgs{
+		// Occasionally, for some reason auto stats aren't collected within the
+		// retry window after the import is finished in multi-tenant setup, so
+		// for now we disable this config.
+		DefaultTestTenant: base.TestIsForStuffThatShouldWorkWithSecondaryTenantsButDoesntYet(110708),
+	})
 	defer s.Stopper().Stop(ctx)
 	sqlDB := sqlutils.MakeSQLRunner(db)
 
