5d768e46343623b17cc3133d8f901cd5af9fdc88
Drew Kimball <drewk@cockroachlabs.com>
2023-11-02 00:25:48 -0600

    execstats: add latch wait time into cumulative contention time
    This patch adds a field `IsLatch` to the `ContentionEvent` protobuf struct that
    indicates that a contention event was emitted by the latch table. The event is
    added to the trace and used to increment the contention time metrics, but is
    only added to the contention registry if
    `register_latch_wait_contention_events` is set to true.
    
    This commit also propagates the transaction ID into the latch manager and the
    contention events listener. This information is used during a latch wait event
    to distinguish between time waiting for a transaction's own requests
    (e.g. for QueryIntent) and for real contention between different transactions.
    
    Fixes #103713
    
    Release note (ui change): The contention time metric now includes time spent
    waiting to acquire latches in addition to time spent acquiring locks. This
    applies to both the cumulative contention time visible in the db console and
    the annotations of an `EXPLAIN ANALYZE` output.

--- pkg/sql/rowexec/tablereader.go
+++ pkg/sql/rowexec/tablereader.go
@@ -169,6 +169,9 @@ func newTableReader(
 	}
 
 	if execstats.ShouldCollectStats(ctx, flowCtx.CollectStats) {
+		if flowTxn := flowCtx.EvalCtx.Txn; flowTxn != nil {
+			tr.contentionEventsListener.Init(flowTxn.ID())
+		}
 		tr.fetcher = newRowFetcherStatCollector(&fetcher)
 		tr.ExecStatsForTrace = tr.execStatsForTrace
 	} else {
