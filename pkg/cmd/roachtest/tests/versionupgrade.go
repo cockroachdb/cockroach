54f977bd02b507504f6f1144006ae97bf383fa66
Renato Costa <renato@cockroachlabs.com>
2023-11-02 17:29:53 -0400

    roachprod: never start cockroach processes concurrently
    This changes removes the `Sequential` roachprod option (and command
    line flag) so that we always start cockroach processes sequentially:
    this ensures that node IDs match hostnames. Previously, we were
    starting nodes concurrently for upgrade tests because initialization
    tasks performed by roachprod required quorum.
    
    Fixes: #113384.
    
    Release note: None

--- pkg/cmd/roachtest/tests/versionupgrade.go
+++ pkg/cmd/roachtest/tests/versionupgrade.go
@@ -302,8 +302,6 @@ func uploadAndStartFromCheckpointFixture(
 		}
 		binary := uploadVersion(ctx, t, u.c, nodes, v)
 		startOpts := option.DefaultStartOpts()
-		// NB: can't start sequentially since cluster already bootstrapped.
-		startOpts.RoachprodOpts.Sequential = false
 		if err := clusterupgrade.StartWithSettings(
 			ctx, t.L(), u.c, nodes, startOpts, install.BinaryOption(binary),
 		); err != nil {
@@ -316,8 +314,6 @@ func uploadAndStart(nodes option.NodeListOption, v *clusterupgrade.Version) vers
 	return func(ctx context.Context, t test.Test, u *versionUpgradeTest) {
 		binary := uploadVersion(ctx, t, u.c, nodes, v)
 		startOpts := option.DefaultStartOpts()
-		// NB: can't start sequentially since cluster already bootstrapped.
-		startOpts.RoachprodOpts.Sequential = false
 		if err := clusterupgrade.StartWithSettings(
 			ctx, t.L(), u.c, nodes, startOpts, install.BinaryOption(binary),
 		); err != nil {
