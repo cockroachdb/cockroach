93b55ca5217b898b10028568538583a757730008
Arul Ajmani <arulajmani@gmail.com>
2023-11-06 15:39:08 -0500

    kv: add server-side metrics for {successful,failed} 1PC evaluation
    This patch adds metrics for successful and failed 1PC evaluations, which
    together give us the number of attempted 1PC evaluations by a store. We
    then modify an existing test to use these metrics to verify that it was
    able to successfully commit using 1PC.
    
    Epic: None
    Release note: None

--- pkg/kv/kvserver/metrics.go
+++ pkg/kv/kvserver/metrics.go
@@ -474,6 +474,18 @@ var (
 		Measurement: "KV Transactions",
 		Unit:        metric.Unit_COUNT,
 	}
+	metaSuccessful1PC = metric.Metadata{
+		Name:        "txn.server_side.1PC.success",
+		Help:        "Number of batches that were successfully committed using 1PC",
+		Measurement: "KV Transactions",
+		Unit:        metric.Unit_COUNT,
+	}
+	metaFailed1PC = metric.Metadata{
+		Name:        "txn.server_side.1PC.success",
+		Help:        "Number of batches that attempted to commit using 1PC",
+		Measurement: "KV Transactions",
+		Unit:        metric.Unit_COUNT,
+	}
 
 	//Ingest metrics
 	metaIngestCount = metric.Metadata{
@@ -2357,6 +2369,8 @@ type StoreMetrics struct {
 	ReadEvaluationServerSideRetryFailure                     *metric.Counter
 	ReadWithinUncertaintyIntervalErrorServerSideRetrySuccess *metric.Counter
 	ReadWithinUncertaintyIntervalErrorServerSideRetryFailure *metric.Counter
+	Successful1PC                                            *metric.Counter
+	Failed1PC                                                *metric.Counter
 
 	// Storage (pebble) metrics. Some are named RocksDB which is what we used
 	// before pebble, and this name is kept for backwards compatibility despite
@@ -3035,6 +3049,8 @@ func newStoreMetrics(histogramWindow time.Duration) *StoreMetrics {
 		ReadEvaluationServerSideRetryFailure:                     metric.NewCounter(metaReadEvaluationServerSideRetryFailure),
 		ReadWithinUncertaintyIntervalErrorServerSideRetrySuccess: metric.NewCounter(metaReadWithinUncertaintyIntervalErrorServerSideRetrySuccess),
 		ReadWithinUncertaintyIntervalErrorServerSideRetryFailure: metric.NewCounter(metaReadWithinUncertaintyIntervalErrorServerSideRetryFailure),
+		Successful1PC: metric.NewCounter(metaSuccessful1PC),
+		Failed1PC:     metric.NewCounter(metaFailed1PC),
 
 		// Pebble metrics.
 		//
