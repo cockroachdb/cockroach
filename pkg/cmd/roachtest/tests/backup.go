40b8b255faa4d93eaa7e80fd01e61cf35b3cf91b
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-11-10 18:18:51 -0800

    colfetcher,roachtest: re-enable direct scans metamorphization
    This commit makes a couple of tweaks to the backup roachtests which
    allows us to make the default value of `sql.distsql.direct_columnar_scans.enabled`
    a metamorphic variable again. In particular, we need to disable this
    setting for backups on `bank` data set due to pathological case as
    described in #113816. This also allows us to reduce `--max-sql-memory`
    since we don't expect memory usage to be as high without direct scans.
    
    Fixes: #113816.
    
    Release note: None

--- pkg/cmd/roachtest/tests/backup.go
+++ pkg/cmd/roachtest/tests/backup.go
@@ -100,7 +100,10 @@ func importBankDataSplit(
 
 	// NB: starting the cluster creates the logs dir as a side effect,
 	// needed below.
-	c.Start(ctx, t.L(), maybeUseMemoryBudget(t, 50), install.MakeClusterSettings())
+	c.Start(ctx, t.L(), option.DefaultStartOptsNoBackups(), install.MakeClusterSettings())
+	// See #113816 for why this is needed for now (probably until #94850 is
+	// resolved).
+	c.Run(ctx, c.Node(1), "./cockroach sql --insecure -e 'SET CLUSTER SETTING sql.distsql.direct_columnar_scans.enabled = false;'")
 	runImportBankDataSplit(ctx, rows, ranges, t, c)
 	return dest
 }
@@ -744,7 +747,7 @@ func runBackupMVCCRangeTombstones(
 ) {
 	if !config.skipClusterSetup {
 		c.Put(ctx, t.DeprecatedWorkload(), "./workload") // required for tpch
-		c.Start(ctx, t.L(), maybeUseMemoryBudget(t, 50), install.MakeClusterSettings())
+		c.Start(ctx, t.L(), option.DefaultStartOptsNoBackups(), install.MakeClusterSettings())
 	}
 	t.Status("starting csv servers")
 	c.Run(ctx, c.All(), `./cockroach workload csv-server --port=8081 &> logs/workload-csv-server.log < /dev/null &`)
