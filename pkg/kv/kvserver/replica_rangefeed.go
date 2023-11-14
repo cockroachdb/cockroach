1dbdbe9ad378571d4db1d16f2cd9fdbd39f6f42e
Erik Grinaker <grinaker@cockroachlabs.com>
2023-11-14 10:41:13 +0000

    kvserver: enable rangefeed scheduler by default
    Epic: none
    Release note (performance improvement): rangefeeds, the infrastructure
    used for changefeeds, now use a more efficient engine that reduces the
    number of goroutines and the associated Go scheduler pressure and
    latency.

--- pkg/kv/kvserver/replica_rangefeed.go
+++ pkg/kv/kvserver/replica_rangefeed.go
@@ -86,7 +86,7 @@ var RangeFeedUseScheduler = settings.RegisterBoolSetting(
 	"use shared fixed pool of workers for all range feeds instead of a "+
 		"worker per range (worker pool size is determined by "+
 		"COCKROACH_RANGEFEED_SCHEDULER_WORKERS env variable)",
-	util.ConstantWithMetamorphicTestBool("kv_rangefeed_scheduler_enabled", false),
+	util.ConstantWithMetamorphicTestBool("kv_rangefeed_scheduler_enabled", true),
 )
 
 // RangefeedSchedulerDisabled is a kill switch for scheduler based rangefeed
