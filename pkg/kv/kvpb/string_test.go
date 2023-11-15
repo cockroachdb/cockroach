37f77b74e1be44bdf975c4765879b8bb2467bbcc
Eric.Yang <eyang@roblox.com>
2023-11-14 23:19:47 -0800

    kv: include locking strength and durability in Get/Scan/RevScan SafeFormat
    The goal of this pr is improving observability. We are adding lock strength
    and lock durability with Get/Scan/RevScan request if the request is locking.
    This is implemented by introducing an optional extension interface to Request
    Interface called SafeFormatterRequest. We are also refactoring inside
    BatchRequest.SafeFormat with the added interface. Please note the subtle
    changed introduced here: if the EndKey is not present we print only Key with
    square brackets, and this applies to all the request types.
    
    Fixes: #114475
    
    Release note: None.

--- pkg/kv/kvpb/string_test.go
+++ pkg/kv/kvpb/string_test.go
@@ -63,7 +63,7 @@ func TestBatchRequestString(t *testing.T) {
 	ba.Requests = append(ba.Requests, ru)
 
 	{
-		exp := `Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min),... 76 skipped ..., Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), Get [/Min,/Min), EndTxn(abort) [/Min], [txn: 6ba7b810], [wait-policy: Error], [protect-ambiguous-replay], [can-forward-ts], [bounded-staleness, min_ts_bound: 0.000000001,0, min_ts_bound_strict, max_ts_bound: 0.000000002,0]`
+		exp := `Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min], Get [/Min],... 76 skipped ..., Get [/Min], Get [/Min], Get [/Min], Get [/Min], EndTxn(abort) [/Min], [txn: 6ba7b810], [wait-policy: Error], [protect-ambiguous-replay], [can-forward-ts], [bounded-staleness, min_ts_bound: 0.000000001,0, min_ts_bound_strict, max_ts_bound: 0.000000002,0]`
 		act := ba.String()
 		require.Equal(t, exp, act)
 	}
@@ -97,4 +97,97 @@ func TestAmbiguousResultError(t *testing.T) {
 
 	s := fmt.Sprintf("%s\n%s", err, redact.Sprint(err))
 	echotest.Require(t, s, filepath.Join("testdata", "ambiguous_result_error.txt"))
+}
+
+// Unit test the requests that implemented SafeFormatterRequest interface.
+func TestRequestSafeFormat(t *testing.T) {
+	txn := roachpb.MakeTransaction("txn1", []byte("abc"), 0, 0, hlc.Timestamp{WallTime: 10}, 0, 6, 0)
+	fixedUuid, _ := uuid.FromString("00fbff57-c1ee-48ce-966c-da568d50e425")
+	txn.ID = fixedUuid
+	pusherTxn := roachpb.MakeTransaction("txn2", []byte("123"), 0, 0, hlc.Timestamp{WallTime: 10}, 0, 1, 0)
+	pusheeTxn := roachpb.MakeTransaction("txn3", []byte("1234"), 0, 0, hlc.Timestamp{WallTime: 10}, 0, 1, 0)
+	fixedUuid2, _ := uuid.FromString("00fbff58-c1ee-48ce-966c-da568d50e425")
+	fixedUuid3, _ := uuid.FromString("00fbff59-c1ee-48ce-966c-da568d50e425")
+	pusherTxn.ID = fixedUuid2
+	pusheeTxn.ID = fixedUuid3
+
+	testCases := []struct {
+		req        kvpb.Request
+		redactable string
+		redacted   string
+	}{
+		{
+			req: &kvpb.GetRequest{
+				RequestHeader: kvpb.RequestHeader{
+					Key: roachpb.Key("a"),
+				},
+				KeyLockingStrength:   lock.Shared,
+				KeyLockingDurability: lock.Unreplicated,
+			},
+			redactable: "Get(Shared,Unreplicated)",
+			redacted:   "Get(Shared,Unreplicated)",
+		},
+		{
+			req: &kvpb.ScanRequest{
+				RequestHeader: kvpb.RequestHeader{
+					Key:    roachpb.Key("b"),
+					EndKey: roachpb.Key("d"),
+				},
+				KeyLockingStrength:   lock.Shared,
+				KeyLockingDurability: lock.Unreplicated,
+			},
+			redactable: "Scan(Shared,Unreplicated)",
+			redacted:   "Scan(Shared,Unreplicated)",
+		},
+		{
+			req: &kvpb.ReverseScanRequest{
+				RequestHeader: kvpb.RequestHeader{
+					Key:    roachpb.Key("c"),
+					EndKey: roachpb.Key("d"),
+				},
+				KeyLockingStrength:   lock.Shared,
+				KeyLockingDurability: lock.Unreplicated,
+			},
+			redactable: "ReverseScan(Shared,Unreplicated)",
+			redacted:   "ReverseScan(Shared,Unreplicated)",
+		},
+		{
+			req: &kvpb.EndTxnRequest{
+				RequestHeader: kvpb.RequestHeader{
+					Key: roachpb.Key("ab"),
+				},
+				Commit: true,
+			},
+			redactable: "EndTxn(commit)",
+			redacted:   "EndTxn(commit)",
+		},
+		{
+			req: &kvpb.RecoverTxnRequest{
+				RequestHeader: kvpb.RequestHeader{
+					Key: roachpb.Key("abc"),
+				},
+				Txn:                 txn.TxnMeta,
+				ImplicitlyCommitted: false,
+			},
+			redactable: "RecoverTxn(00fbff57, abort)",
+			redacted:   "RecoverTxn(00fbff57, abort)",
+		},
+		{
+			req: &kvpb.PushTxnRequest{
+				RequestHeader: kvpb.RequestHeader{
+					Key: roachpb.Key("123"),
+				},
+				PusherTxn: pusherTxn,
+				PusheeTxn: pusheeTxn.TxnMeta,
+			},
+			redactable: "PushTxn(‹PUSH_TIMESTAMP›,00fbff58->00fbff59)",
+			redacted:   "PushTxn(‹×›,00fbff58->00fbff59)",
+		},
+	}
+	for _, c := range testCases {
+		t.Run(c.req.Method().String(), func(t *testing.T) {
+			require.EqualValues(t, c.redactable, redact.Sprint(c.req))
+			require.EqualValues(t, c.redacted, redact.Sprint(c.req).Redact())
+		})
+	}
 }
