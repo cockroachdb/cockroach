65acc2e84a6140ab2aefee33a0243eb6d05f2fe8
Erik Grinaker <grinaker@cockroachlabs.com>
2023-11-15 12:23:20 +0000

    rangefeed: fix premature checkpoint due to intent resolution race
    It was possible for rangefeeds to emit a premature checkpoint, before
    all writes below its timestamp had been emitted. This in turn would
    cause changefeeds to not emit these write events at all.
    
    This could happen because `PushTxn` may return a false `ABORTED` status
    for a transaction that has in fact been committed, if the transaction
    record has been GCed (after resolving all intents). The timestamp cache
    does not retain sufficient information to disambiguate a committed
    transaction from an aborted one in this case, so it pessimistically
    assumes an abort (see `Replica.CanCreateTxnRecord` and
    `batcheval.SynthesizeTxnFromMeta`).
    
    However, the rangefeed txn pusher trusted this `ABORTED` status,
    ignoring the pending txn intents and allowing the resolved timestamp to
    advance past them before emitting the committed intents. This can lead to
    the following scenario:
    
    - A rangefeed is running on a lagging follower.
    - A txn writes an intent, which is replicated to the follower.
    - The closed timestamp advances past the intent.
    - The txn commits and resolves the intent at the original write timestamp,
      then GCs its txn record. This is not yet applied on the follower.
    - The rangefeed pushes the txn to advance its resolved timestamp.
    - The txn is GCed, so the push returns ABORTED (it can't know whether the
      txn was committed or aborted after its record is GCed).
    - The rangefeed disregards the "aborted" txn and advances the resolved
      timestamp, emitting a checkpoint.
    - The follower applies the resolved intent and emits an event below
      the checkpoint, violating the checkpoint guarantee.
    - The changefeed sees an event below its frontier and drops it, never
      emitting these events at all.
    
    This patch fixes the bug by submitting a barrier command to the
    leaseholder which waits for all past and ongoing writes (including
    intent resolution) to complete and apply, and then waits for the local
    replica to apply the barrier as well. This ensures any committed intent
    resolution will be applied and emitted before the transaction is removed
    from resolved timestamp tracking.
    
    Epic: none
    Release note (bug fix): fixed a bug where a changefeed could omit events
    in rare cases, logging the error "cdc ux violation: detected timestamp
    ... that is less or equal to the local frontier". This can happen if a
    rangefeed runs on a follower replica that lags significantly behind the
    leaseholder, a transaction commits and removes its transaction record
    before its intent resolution is applied on the follower, the follower's
    closed timestamp has advanced past the transaction commit timestamp, and
    the rangefeed attempts to push the transaction to a new timestamp (at
    least 10 seconds after the transaction began). This may cause the
    rangefeed to prematurely emit a checkpoint before emitting writes at
    lower timestamps, which in turn may cause the changefeed to drop these
    events entirely, never emitting them.

--- pkg/kv/kvserver/rangefeed/task_test.go
+++ pkg/kv/kvserver/rangefeed/task_test.go
@@ -360,13 +360,13 @@ func TestInitResolvedTSScan(t *testing.T) {
 }
 
 type testTxnPusher struct {
-	pushTxnsFn       func(context.Context, []enginepb.TxnMeta, hlc.Timestamp) ([]*roachpb.Transaction, error)
+	pushTxnsFn       func(context.Context, []enginepb.TxnMeta, hlc.Timestamp) ([]*roachpb.Transaction, bool, error)
 	resolveIntentsFn func(ctx context.Context, intents []roachpb.LockUpdate) error
 }
 
 func (tp *testTxnPusher) PushTxns(
 	ctx context.Context, txns []enginepb.TxnMeta, ts hlc.Timestamp,
-) ([]*roachpb.Transaction, error) {
+) ([]*roachpb.Transaction, bool, error) {
 	return tp.pushTxnsFn(ctx, txns, ts)
 }
 
@@ -374,8 +374,12 @@ func (tp *testTxnPusher) ResolveIntents(ctx context.Context, intents []roachpb.L
 	return tp.resolveIntentsFn(ctx, intents)
 }
 
+func (tp *testTxnPusher) Barrier(ctx context.Context) error {
+	return nil
+}
+
 func (tp *testTxnPusher) mockPushTxns(
-	fn func(context.Context, []enginepb.TxnMeta, hlc.Timestamp) ([]*roachpb.Transaction, error),
+	fn func(context.Context, []enginepb.TxnMeta, hlc.Timestamp) ([]*roachpb.Transaction, bool, error),
 ) {
 	tp.pushTxnsFn = fn
 }
@@ -434,7 +438,7 @@ func TestTxnPushAttempt(t *testing.T) {
 	var tp testTxnPusher
 	tp.mockPushTxns(func(
 		ctx context.Context, txns []enginepb.TxnMeta, ts hlc.Timestamp,
-	) ([]*roachpb.Transaction, error) {
+	) ([]*roachpb.Transaction, bool, error) {
 		require.Equal(t, 4, len(txns))
 		require.Equal(t, txn1Meta, txns[0])
 		require.Equal(t, txn2Meta, txns[1])
@@ -445,7 +449,7 @@ func TestTxnPushAttempt(t *testing.T) {
 		// Return all four protos. The PENDING txn is pushed.
 		txn1ProtoPushed := txn1Proto.Clone()
 		txn1ProtoPushed.WriteTimestamp = ts
-		return []*roachpb.Transaction{txn1ProtoPushed, txn2Proto, txn3Proto, txn4Proto}, nil
+		return []*roachpb.Transaction{txn1ProtoPushed, txn2Proto, txn3Proto, txn4Proto}, false, nil
 	})
 	tp.mockResolveIntentsFn(func(ctx context.Context, intents []roachpb.LockUpdate) error {
 		require.Len(t, intents, 7)
