402186e90d0f7be9015a6ac2e665e71fba2c0c74
Wenyi Hu <wenyi@cockroachlabs.com>
2023-11-16 14:17:29 -0500

    changefeedccl: better doc for balance_range_distribution.enable
    Previously, the description for changefeed.balance_range_distribution.enable
    could be misleading. It suggests equal range distribution among nodes, but this
    is only supported in export only mode (initial_scan=only). This patch clarifies
    the description, prevent confusion for users expecting regular changefeeds to be
    affected by this cluster setting.
    
    Fixes: #114236
    
    Release note (doc): cluster setting for
    changefeed.balance_range_distribution.enable now has a clearer description.

--- pkg/ccl/changefeedccl/changefeed_dist.go
+++ pkg/ccl/changefeedccl/changefeed_dist.go
@@ -318,7 +318,8 @@ func startDistChangefeed(
 var enableBalancedRangeDistribution = settings.RegisterBoolSetting(
 	settings.ApplicationLevel,
 	"changefeed.balance_range_distribution.enable",
-	"if enabled, the ranges are balanced equally among all nodes",
+	"if enabled, the ranges are balanced equally among all nodes. "+
+		"Note that this is supported only in export mode with initial_scan=only.",
 	util.ConstantWithMetamorphicTestBool(
 		"changefeed.balance_range_distribution.enabled", false),
 	settings.WithName("changefeed.balance_range_distribution.enabled"),
