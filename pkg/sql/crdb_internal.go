6492d9974017da3777349933ec34be6077859054
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-11-16 13:47:09 -0800

    sql: fix possible nil pointer in crdb_internal.cluster_locks
    In 1187a2b2cddd5f29a25cf0f7bbcb89c8194392c6 we introduced a regression
    that could lead to a nil pointer when `curLock.LockHolder` is nil (which
    is valid). This is now fixed.
    
    No release note since we haven't had a stable release with the bug yet.
    
    Release note: None

--- pkg/sql/crdb_internal.go
+++ pkg/sql/crdb_internal.go
@@ -7923,9 +7923,10 @@ func genClusterLocksGenerator(
 			if curLock == nil || fErr != nil {
 				return nil, fErr
 			}
-			strengthDatum := tree.DNull
 			txnIDDatum := tree.DNull
 			tsDatum := tree.DNull
+			isolationLevelDatum := tree.DNull
+			strengthDatum := tree.DNull
 			durationDatum := tree.DNull
 			granted := false
 			// Utilize -1 to indicate that the row represents the lock holder.
@@ -7933,6 +7934,7 @@ func genClusterLocksGenerator(
 				if curLock.LockHolder != nil {
 					txnIDDatum = tree.NewDUuid(tree.DUuid{UUID: curLock.LockHolder.ID})
 					tsDatum = eval.TimestampToInexactDTimestamp(curLock.LockHolder.WriteTimestamp)
+					isolationLevelDatum = tree.NewDString(tree.IsolationLevelFromKVTxnIsolationLevel(curLock.LockHolder.IsoLevel).String())
 					strengthDatum = tree.NewDString(curLock.LockStrength.String())
 					durationDatum = tree.NewDInterval(
 						duration.MakeDuration(curLock.HoldDuration.Nanoseconds(), 0 /* days */, 0 /* months */),
@@ -7945,6 +7947,7 @@ func genClusterLocksGenerator(
 				if waiter.WaitingTxn != nil {
 					txnIDDatum = tree.NewDUuid(tree.DUuid{UUID: waiter.WaitingTxn.ID})
 					tsDatum = eval.TimestampToInexactDTimestamp(waiter.WaitingTxn.WriteTimestamp)
+					isolationLevelDatum = tree.NewDString(tree.IsolationLevelFromKVTxnIsolationLevel(waiter.WaitingTxn.IsoLevel).String())
 				}
 				strengthDatum = tree.NewDString(waiter.Strength.String())
 				durationDatum = tree.NewDInterval(
@@ -7983,7 +7986,7 @@ func genClusterLocksGenerator(
 				tree.MakeDBool(tree.DBool(granted)),          /* granted */
 				tree.MakeDBool(tree.DBool(contented)),        /* contended */
 				durationDatum,                                /* duration */
-				tree.NewDString(tree.IsolationLevelFromKVTxnIsolationLevel(curLock.LockHolder.IsoLevel).String()), /* isolation_level */
+				isolationLevelDatum,                          /* isolation_level */
 			}, nil
 
 		}, nil, nil
