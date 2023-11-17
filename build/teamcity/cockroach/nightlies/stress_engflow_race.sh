26607ed04d3d4bc1d83a1bf5d5d9cd8709b9596e
Ricky Stewart <ricky@cockroachlabs.com>
2023-11-17 17:02:27 -0600

    stress: tweak args for `deadlock` and `race` nightlies
    Increase timeouts, reduce the number of test runs, unset
    GITHUB_API_TOKEN for nightlies that aren't working fully yet.
    
    Also set `GOTRACEBACK=all` for all EngFlow tests.
    
    Epic: CRDB-8308
    Release note: None

--- build/teamcity/cockroach/nightlies/stress_engflow_race.sh
+++ build/teamcity/cockroach/nightlies/stress_engflow_race.sh
@@ -2,9 +2,11 @@
 
 set -euo pipefail
 
-export RUNS_PER_TEST=5
-export EXTRA_TEST_ARGS="--@io_bazel_rules_go//go/config:race --test_env=GORACE=halt_on_error=1"
+export RUNS_PER_TEST=3
+export EXTRA_TEST_ARGS="--config=race --test_timeout=1800,3600,5395,5395"
 
 THIS_DIR=$(cd "$(dirname "$0")" && pwd)
+
+unset GITHUB_API_TOKEN
 
 $THIS_DIR/stress_engflow_impl.sh
