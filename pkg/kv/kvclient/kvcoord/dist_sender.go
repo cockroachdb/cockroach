c4900a04608009e7132108d26966a6fbd8f957cf
Andrew Baptist <baptist@cockroachlabs.com>
2023-11-17 12:35:58 -0500

    kvclient: remove indirection to HealthFunc
    This PR cleans up an unnecessary AtomicPointer to the HealthFunc. The
    HealthFunc is now injected during DistSenderConfig construction rather
    than being injected later.
    
    Epic: none
    
    Release note: None

--- pkg/kv/kvclient/kvcoord/dist_sender.go
+++ pkg/kv/kvclient/kvcoord/dist_sender.go
@@ -564,7 +564,7 @@ type DistSender struct {
 	latencyFunc LatencyFunc
 
 	// HealthFunc returns true if the node is alive and not draining.
-	healthFunc atomic.Pointer[HealthFunc]
+	healthFunc HealthFunc
 
 	onRangeSpanningNonTxnalBatch func(ba *kvpb.BatchRequest) *kvpb.Error
 
@@ -637,6 +637,8 @@ type DistSenderConfig struct {
 	KVInterceptor multitenant.TenantSideKVInterceptor
 
 	TestingKnobs ClientTestingKnobs
+
+	HealthFunc HealthFunc
 }
 
 // NewDistSender returns a batch.Sender instance which connects to the
@@ -663,6 +665,7 @@ func NewDistSender(cfg DistSenderConfig) *DistSender {
 		metrics:       makeDistSenderMetrics(),
 		kvInterceptor: cfg.KVInterceptor,
 		locality:      cfg.Locality,
+		healthFunc:    cfg.HealthFunc,
 	}
 	if ds.st == nil {
 		ds.st = cluster.MakeTestingClusterSettings()
@@ -741,22 +744,14 @@ func NewDistSender(cfg DistSenderConfig) *DistSender {
 		ds.onRangeSpanningNonTxnalBatch = cfg.TestingKnobs.OnRangeSpanningNonTxnalBatch
 	}
 
-	// Placeholder function until we inject the real health function in using
-	// SetHealthFunc.
-	// TODO(baptist): Restructure the code to allow injecting the correct
-	// HealthFunc at construction time.
-	healthFunc := HealthFunc(func(id roachpb.NodeID) bool {
-		return true
-	})
-	ds.healthFunc.Store(&healthFunc)
+	// Some tests don't set the healthFunc.
+	if ds.healthFunc == nil {
+		ds.healthFunc = func(id roachpb.NodeID) bool {
+			return true
+		}
+	}
 
 	return ds
-}
-
-// SetHealthFunc is called after construction due to the circular dependency
-// between DistSender and NodeLiveness.
-func (ds *DistSender) SetHealthFunc(healthFn HealthFunc) {
-	ds.healthFunc.Store(&healthFn)
 }
 
 // LatencyFunc returns the LatencyFunc of the DistSender.
@@ -766,7 +761,7 @@ func (ds *DistSender) LatencyFunc() LatencyFunc {
 
 // HealthFunc returns the HealthFunc of the DistSender.
 func (ds *DistSender) HealthFunc() HealthFunc {
-	return *ds.healthFunc.Load()
+	return ds.healthFunc
 }
 
 // DisableFirstRangeUpdates disables updates of the first range via
@@ -2285,7 +2280,7 @@ func (ds *DistSender) sendToReplicas(
 		// First order by latency, then move the leaseholder to the front of the
 		// list, if it is known.
 		if !ds.dontReorderReplicas {
-			replicas.OptimizeReplicaOrder(ds.st, ds.nodeIDGetter(), ds.HealthFunc(), ds.latencyFunc, ds.locality)
+			replicas.OptimizeReplicaOrder(ds.st, ds.nodeIDGetter(), ds.healthFunc, ds.latencyFunc, ds.locality)
 		}
 
 		idx := -1
@@ -2304,7 +2299,7 @@ func (ds *DistSender) sendToReplicas(
 	case kvpb.RoutingPolicy_NEAREST:
 		// Order by latency.
 		log.VEvent(ctx, 2, "routing to nearest replica; leaseholder not required")
-		replicas.OptimizeReplicaOrder(ds.st, ds.nodeIDGetter(), ds.HealthFunc(), ds.latencyFunc, ds.locality)
+		replicas.OptimizeReplicaOrder(ds.st, ds.nodeIDGetter(), ds.healthFunc, ds.latencyFunc, ds.locality)
 
 	default:
 		log.Fatalf(ctx, "unknown routing policy: %s", ba.RoutingPolicy)
