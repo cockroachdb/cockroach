f5369c6f7ab153e3e49229d4cda71988599c6945
Renato Costa <renato@cockroachlabs.com>
2023-11-28 17:04:10 -0500

    release: fix check for valid pre-releases when updating predecessors file
    Previously, the `update-releases-file` command was trying to match the
    pre-relrease to exactly `rc` or `beta`. However, we typically name our
    pre-releases `{rc,beta}.N` (e.g., `v23.2.0-beta.1`).
    
    This fixes the check by ensuring that the pre-release has a valid
    prefix instead.
    
    Epic: none
    
    Release note: None

--- pkg/cmd/release/update_releases.go
+++ pkg/cmd/release/update_releases.go
@@ -122,7 +122,7 @@ func processReleaseData(data []Release) map[string]release.Series {
 		// For the purposes of the cockroach_releases file, we are only
 		// interested in beta and rc pre-releases, as we do not support
 		// upgrades from alpha releases.
-		if pre := v.PreRelease(); pre != "" && pre != "rc" && pre != "beta" {
+		if pre := v.PreRelease(); pre != "" && !strings.HasPrefix(pre, "rc") && !strings.HasPrefix(pre, "beta") {
 			continue
 		}
 		// Skip cloud-only releases, because the binaries are not yet publicly available.
