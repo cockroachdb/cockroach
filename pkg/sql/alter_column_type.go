649572566503cb90c0f508ce0a18cf08e650580a
Andrew Delph <andrewdelph@live.ca>
2023-12-01 13:39:22 -0500

    sql: only allow altering a column type when an assignment cast is allowed
    Fixes: #82416
    
    Release note (sql change): Reject changing column
    types unless an explicitly cast is provided where
    cannot automatically cast. This replicates logic
    in postures. Prior to this change, casts such as
    BOOL to INT were possible without explicitly
    casting.

--- pkg/sql/alter_column_type.go
+++ pkg/sql/alter_column_type.go
@@ -336,6 +336,17 @@ func alterColumnTypeGeneral(
 			SyntaxMode: tree.CastShort,
 		}
 		inverseExpr = tree.Serialize(&oldColComputeExpr)
+
+		// Validate that the column can be automatically cast to toType without explicit casting.
+		if validCast := cast.ValidCast(col.GetType(), toType, cast.ContextAssignment); !validCast {
+			return pgerror.WithCandidateCode(
+				errors.WithHintf(
+					errors.Newf("column %q cannot be cast automatically to type %s",
+						col.GetName(),
+						toType.SQLString()),
+					"You might need to specify \"USING %s\".", s),
+				pgcode.DatatypeMismatch)
+		}
 	}
 	// Create the default expression for the new column.
 	hasDefault := col.HasDefault()
