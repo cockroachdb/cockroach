aa4efa9cc2a03e4d2df6e7babb9f7331b59ffd71
Jackson Owens <jackson@cockroachlabs.com>
2023-12-05 10:40:33 -0500

    Revert "storage: assert local single delete safety"
    This reverts commit 36a2f4c5502fedde772a8c83619d6d77f2b32ca7. This commit has
    already been reverted from the release-23.2 branch. We may be able to bring
    back some version of this optimization through the introduction of an iterator
    option that disables sstable obsolete-bit filtering. For now, avoid false
    positive assertion failures.
    
    Release note: none
    Epic: none

--- pkg/storage/mvcc.go
+++ pkg/storage/mvcc.go
@@ -334,16 +334,6 @@ func getMaxConcurrentCompactions() int {
 var l0SubLevelCompactionConcurrency = envutil.EnvOrDefaultInt(
 	"COCKROACH_L0_SUB_LEVEL_CONCURRENCY", 2)
 
-// assertSingleDeleteSafety configures whether or not to perform verification
-// that SINGLEDELs are deterministic when applied to the local Engine during
-// intent resolution. See call sites of CanDeterministicallySingleDelete. See
-// cockroach#114421 for the motivation.
-//
-// An environment variable is provided for disabling these assertions in case
-// conditions under which false positives or severe peformance regressions are
-// possible.
-var assertSingleDeleteSafety = envutil.EnvOrDefaultBool("COCKROACH_SINGLEDEL_ASSERT", true)
-
 // MakeValue returns the inline value.
 func MakeValue(meta enginepb.MVCCMetadata) roachpb.Value {
 	return roachpb.Value{RawBytes: meta.RawBytes}
@@ -5066,38 +5056,7 @@ func MVCCResolveWriteIntent(
 		if err != nil {
 			return false, 0, nil, false, err
 		}
-		switch outcome {
-		case lockNoop:
-			// Do nothing.
-		case lockClearedBySingleDelete:
-			ok = true
-			if !assertSingleDeleteSafety {
-				continue
-			}
-
-			// Single deletes rely on subtle invariants and logic. We can detect
-			// misuse if the local store's internal state could result in
-			// nondeterministic behavior if we write a single delete. This
-			// doesn't guard against writes to the key committed to the engine
-			// after we opened ltIter but before the single delete is applied,
-			// and it also doesn't guarantee the single delete will be okay on
-			// other replicas' engines.
-			if isDeterministic, err := ltIter.CanDeterministicallySingleDelete(); err != nil {
-				return false, 0, nil, false, errors.Wrap(err, "validating single delete invariant")
-			} else if !isDeterministic {
-				err := errors.AssertionFailedf("deleting by single delete is unsafe")
-				if key, keyErr := ltIter.EngineKey(); keyErr != nil {
-					err = errors.WithSecondaryError(err, keyErr)
-				} else {
-					err = errors.Wrapf(err, "resolving lock key %s", key)
-				}
-				log.Fatalf(ctx, "intent resolution: %v", err)
-			}
-		case lockClearedByDelete, lockOverwritten:
-			ok = true
-		default:
-			panic("unreachable")
-		}
+		ok = ok || outcome != lockNoop
 	}
 	numBytes = int64(rw.BufferedSize() - beforeBytes)
 	return ok, numBytes, nil, replLocksReleased, nil
@@ -5847,38 +5806,11 @@ func MVCCResolveWriteIntentRange(
 		if err != nil {
 			log.Warningf(ctx, "failed to resolve intent for key %q: %+v", lastResolvedKey, err)
 		}
-
-		switch outcome {
-		case lockNoop:
-			// Do nothing.
-		case lockClearedBySingleDelete:
-			if assertSingleDeleteSafety {
-				// Single deletes rely on subtle invariants and logic. We can
-				// detect misuse if the local store's internal state could
-				// result in nondeterministic behavior if we write a single
-				// delete. This doesn't guard against writes to the key
-				// committed to the engine after we opened ltIter but before the
-				// single delete is applied, and it also doesn't guarantee the
-				// single delete will be okay on other replicas' engines.
-				if ok, err := ltIter.CanDeterministicallySingleDelete(); err != nil {
-					return 0, 0, nil, 0, false, errors.Wrap(err, "validating single delete invariant")
-				} else if !ok {
-					log.Fatalf(ctx, "resolving lock key %s: %+v", ltKey,
-						errors.AssertionFailedf("deleting by single delete is unsafe"))
-				}
-			}
-
-			// Fallthrough to update numKeys and lastResolvedKeyOk if necessary.
-			fallthrough
-		case lockClearedByDelete, lockOverwritten:
-			if !lastResolvedKeyOk {
-				// We only count the first successfully resolved lock/intent on a
-				// given key towards the returned key count and key limit.
-				lastResolvedKeyOk = true
-				numKeys++
-			}
-		default:
-			panic("unreachable")
+		if outcome != lockNoop && !lastResolvedKeyOk {
+			// We only count the first successfully resolved lock/intent on a
+			// given key towards the returned key count and key limit.
+			lastResolvedKeyOk = true
+			numKeys++
 		}
 		numBytes += int64(rw.BufferedSize() - beforeBytes)
 	}
