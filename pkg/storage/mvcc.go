c19237a4f994b75fd5ceda345596df2a7234ad19
Radu Berinde <radu@cockroachlabs.com>
2023-12-05 15:52:31 -0800

    storage: populate OmitInRangefeeds in MVCC code
    This change populates the `MVCCValueHEader.OmitInRangeFeeds` flag
    based on the corresponding flag in the transaction.
    
    Fixes: #113634
    Release note: None

--- pkg/storage/mvcc.go
+++ pkg/storage/mvcc.go
@@ -2659,6 +2659,9 @@ func mvccPutInternal(
 	versionValue := MVCCValue{}
 	versionValue.Value = value
 	versionValue.LocalTimestamp = opts.LocalTimestamp
+	if opts.Txn != nil {
+		versionValue.OmitInRangefeeds = opts.Txn.OmitInRangefeeds
+	}
 
 	if buildutil.CrdbTestBuild {
 		if seq, seqOK := kvnemesisutil.FromContext(ctx); seqOK {
