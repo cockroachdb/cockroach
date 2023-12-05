f858d2be78ddaca31af5ae52c29d48617571abdd
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-12-05 12:25:03 -0800

    WIP on fixup

--- pkg/ccl/plpgsqlccl/plpgsql.go
+++ pkg/ccl/plpgsqlccl/plpgsql.go
@@ -1,20 +1,23 @@
+// Copyright 2023 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
 package plpgsqlccl
 
 import (
 	"github.com/cockroachdb/cockroach/pkg/ccl/utilccl"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql/plpgsql"
-	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 )
 
 func init() {
 	plpgsql.CheckClusterSupportsPLpgSQL = checkClusterSupportsPLpgSQL
 }
 
-func CheckClusterSupportsPLpgSQL(settings *cluster.Settings, clusterID uuid.UUID) error {
-	return utilccl.CheckEnterpriseEnabled(
-		settings,
-		clusterID,
-		"PL/pgSQL",
-	)
+func checkClusterSupportsPLpgSQL(settings *cluster.Settings) error {
+	return utilccl.CheckEnterpriseEnabled(settings, "PL/pgSQL")
 }
