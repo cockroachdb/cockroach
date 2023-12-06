33d6c2d8e8a05ffa5cc67d30a7864793f46d4829
rharding6373 <rharding6373@users.noreply.github.com>
2023-12-05 22:15:49 -0800

    sql: support sequence and udt name rewriting in plpgsql
    CRDB rewrites sequence and UDT names as IDs in views and functions so
    that if the sequence or UDT is renamed the views and functions using
    them don't break. This PR adds support for this in PLpgSQL.
    
    Epic: None
    Fixes: #115627
    
    Release note (sql change): Fixes a bug in PLpgSQL where altering the
    name of a sequence or UDT that was used in a PLpgSQL function or
    procedure could break them. This is only present in 23.2 alpha and beta
    releases.

--- pkg/sql/sem/plpgsqltree/exception.go
+++ pkg/sql/sem/plpgsqltree/exception.go
@@ -22,6 +22,13 @@ type Exception struct {
 	Action     []Statement
 }
 
+func (s *Exception) CopyNode() *Exception {
+	copyNode := *s
+	copyNode.Conditions = append([]Condition(nil), copyNode.Conditions...)
+	copyNode.Action = append([]Statement(nil), copyNode.Action...)
+	return &copyNode
+}
+
 func (s *Exception) Format(ctx *tree.FmtCtx) {
 	ctx.WriteString("WHEN ")
 	for i, cond := range s.Conditions {
@@ -44,11 +51,19 @@ func (s *Exception) PlpgSQLStatementTag() string {
 	return "proc_exception"
 }
 
-func (s *Exception) WalkStmt(visitor StatementVisitor) {
-	visitor.Visit(s)
-	for _, stmt := range s.Action {
-		stmt.WalkStmt(visitor)
+func (s *Exception) WalkStmt(visitor StatementVisitor) (newStmt Statement, changed bool) {
+	newStmt, changed = visitor.Visit(s)
+	for i, stmt := range s.Action {
+		ns, ch := stmt.WalkStmt(visitor)
+		if ch {
+			changed = true
+			if newStmt == s {
+				newStmt = s.CopyNode()
+			}
+			newStmt.(*Exception).Action[i] = ns
+		}
 	}
+	return newStmt, changed
 }
 
 type Condition struct {
