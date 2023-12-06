b03b54855ef8db81559c838174282e7d4f3f5cbc
maryliag <marylia@cockroachlabs.com>
2023-12-06 16:34:46 -0500

    ui,obsservice: update queries
    Update queries on the Insights API to use Obs Service or CRDB,
    depending on if is CC or DB Console.
    
    Also updates the parsing when we get the results, depending on
    the source of the data.
    
    Fixes CC-26437
    Fixes CC-26440
    
    Release note: None

--- pkg/ui/workspaces/db-console/src/views/insights/workloadInsightsPage.tsx
+++ pkg/ui/workspaces/db-console/src/views/insights/workloadInsightsPage.tsx
@@ -37,7 +37,6 @@ import { bindActionCreators } from "redux";
 import { LocalSetting } from "src/redux/localsettings";
 import { setGlobalTimeScaleAction } from "src/redux/statements";
 import { selectTimeScale } from "src/redux/timeScale";
-import { selectCsExportInsights } from "src/redux/clusterSettings/clusterSettings.selectors";
 
 export const insightStatementColumnsLocalSetting = new LocalSetting<
   AdminUIState,
@@ -80,7 +79,7 @@ const statementMapStateToProps = (
   timeScale: selectTimeScale(state),
   isLoading: selectStmtInsightsLoading(state),
   maxSizeApiReached: selectStmtInsightsMaxApiReached(state),
-  csExportInsights: selectCsExportInsights(state),
+  useObsService: false,
 });
 
 const TransactionDispatchProps = {
