30197f30858771cf2a80423c33d49a797f68707b
Marcus Gartner <marcus@cockroachlabs.com>
2023-12-11 12:24:11 -0800

    sql: skip TestShowLastQueryStatistics under race
    This commit skips TestShowLastQueryStatistics under the `race` test flag
    because the added overhead of the race detector makes latency checks in
    the test fail.
    
    Fixes #115934
    
    Release note: None

--- pkg/sql/conn_executor_test.go
+++ pkg/sql/conn_executor_test.go
@@ -46,6 +46,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/pgtest"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
 	"github.com/cockroachdb/cockroach/pkg/util/duration"
@@ -1323,6 +1324,7 @@ CREATE TABLE t1.test (k INT PRIMARY KEY, v TEXT);
 }
 
 func TestShowLastQueryStatistics(t *testing.T) {
+	skip.UnderRace(t)
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 
