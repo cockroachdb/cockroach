5a7ee3574d3d822ac4f113a621c844079917f1c7
Steven Danna <danna@cockroachlabs.com>
2023-12-12 12:12:07 +0000

    Revert "cloud/gcp: use cloud http client for gcs"
    This reverts commit 7b920676e33111b3527d82323f6eaf0c1f9dcdd4.
    
    Fixes #116186

--- pkg/cloud/gcp/gcs_storage.go
+++ pkg/cloud/gcp/gcs_storage.go
@@ -174,13 +174,7 @@ func makeGCSStorage(
 		}
 	}
 
-	httpClient, err := cloud.MakeHTTPClient(args.Settings)
-	if err != nil {
-		return nil, err
-	}
-
-	opts := []option.ClientOption{option.WithScopes(scope), option.WithHTTPClient(httpClient)}
-
+	opts := []option.ClientOption{option.WithScopes(scope)}
 	// Once credentials have been obtained via implicit or specified params, we
 	// then check if we should use the credentials directly or whether they should
 	// be used to assume another role.
