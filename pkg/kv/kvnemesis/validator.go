9a27728aa2da50b6815db3a73506421d1ca8b528
Erik Grinaker <grinaker@cockroachlabs.com>
2023-12-14 12:34:17 +0000

    kvnemesis: properly encode range keys
    Previously, kvnemesis did not properly encode range keys with a trailing
    `0` separator, instead using the user key directly. This trips Pebble
    assertions.
    
    Epic: none
    Release note: None

--- pkg/kv/kvnemesis/validator.go
+++ pkg/kv/kvnemesis/validator.go
@@ -979,8 +979,10 @@ func (v *validator) checkAtomicCommitted(
 					panic(err)
 				}
 			} else { // ranged write
+				key := storage.EngineKey{Key: o.Key}.Encode()
+				endKey := storage.EngineKey{Key: o.EndKey}.Encode()
 				suffix := storage.EncodeMVCCTimestampSuffix(o.Timestamp)
-				if err := batch.RangeKeyUnset(o.Key, o.EndKey, suffix, nil); err != nil {
+				if err := batch.RangeKeyUnset(key, endKey, suffix, nil); err != nil {
 					panic(err)
 				}
 			}
@@ -1024,8 +1026,10 @@ func (v *validator) checkAtomicCommitted(
 					panic(err)
 				}
 			} else {
+				key := storage.EngineKey{Key: o.Key}.Encode()
+				endKey := storage.EngineKey{Key: o.EndKey}.Encode()
 				suffix := storage.EncodeMVCCTimestampSuffix(writeTS)
-				if err := batch.RangeKeySet(o.Key, o.EndKey, suffix, o.Value.RawBytes, nil); err != nil {
+				if err := batch.RangeKeySet(key, endKey, suffix, o.Value.RawBytes, nil); err != nil {
 					panic(err)
 				}
 			}
