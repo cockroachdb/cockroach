acf211e90cbbc73515211202924b63afc87dee5a
Rafi Shamim <rafi@cockroachlabs.com>
2023-12-15 17:45:02 -0500

    catalog: skip TestRangefeedUpdatesHandledProperlyInTheFaceOfRaces under duress
    Release note: None

--- pkg/sql/catalog/lease/lease_test.go
+++ pkg/sql/catalog/lease/lease_test.go
@@ -2345,6 +2345,7 @@ func TestRangefeedUpdatesHandledProperlyInTheFaceOfRaces(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 	defer ensureTestTakesLessThan(t, 30*time.Second)()
+	skip.UnderDuress(t, "test must take less than 30 seconds, so avoid slow configs")
 
 	ctx := context.Background()
 	var interestingTable atomic.Value
