c543aa4e67221696912a3aafc4cef6862f91c239
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-12-16 00:03:58 +0000

    sqlsmith: don't use crdb_internal.reset_sql_stats builtin
    This builtin under the hood issues multiple TRUNCATE commands that can
    take non-trivial amount of time to execute.
    
    Release note: None

--- pkg/internal/sqlsmith/schema.go
+++ pkg/internal/sqlsmith/schema.go
@@ -526,6 +526,7 @@ var functions = func() map[tree.FunctionClass]map[oid.Oid][]function {
 			"crdb_internal.revalidate_unique_constraint",
 			"crdb_internal.request_statement_bundle",
 			"crdb_internal.set_compaction_concurrency",
+			"crdb_internal.reset_sql_stats",
 		} {
 			skip = skip || strings.Contains(def.Name, substr)
 		}
