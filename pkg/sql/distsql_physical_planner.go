fac74ec02cfa133a9b7cd62525e3c4b75e3c636c
Jayant Shrivastava <jayants@cockroachlabs.com>
2023-12-19 10:10:22 -0500

    sql: count ranges per partition in `PartitionSpans`
    This change updates span partitioning to count ranges while making
    partitions. This allows callers to rebalance partitions based on
    range counts without having to iterate over the spans to count
    ranges.
    
    Release note: None
    Epic: None

--- pkg/sql/distsql_physical_planner.go
+++ pkg/sql/distsql_physical_planner.go
@@ -1204,6 +1204,34 @@ func (r SpanPartitionReason) String() string {
 type SpanPartition struct {
 	SQLInstanceID base.SQLInstanceID
 	Spans         roachpb.Spans
+
+	haveRangeInfo bool
+	numRanges     int
+}
+
+// NumRanges returns the number of ranges in a partition only if this
+// information is present. The returned bool indicates if the result is correct.
+// Note that the returned count is not for distinct ranges. If there are two
+// spans belonging to the same range in the partition (ex. two disjoint spans),
+// this counts as two ranges.
+func (sp *SpanPartition) NumRanges() (int, bool) {
+	if !sp.haveRangeInfo {
+		return 0, false
+	}
+	return sp.numRanges, true
+}
+
+// MakeSpanPartitionWithRangeCount constructs a SpanPartition with the
+// specified count of ranges.
+func MakeSpanPartitionWithRangeCount(
+	instanceID base.SQLInstanceID, spans roachpb.Spans, numRanges int,
+) SpanPartition {
+	return SpanPartition{
+		SQLInstanceID: instanceID,
+		Spans:         spans,
+		haveRangeInfo: true,
+		numRanges:     numRanges,
+	}
 }
 
 type distSQLNodeHealth struct {
@@ -1309,7 +1337,8 @@ func (dsp *DistSQLPlanner) partitionSpansEx(
 		// always ignore misplanned ranges for local-only plans, and we choose
 		// to return `true` to explicitly highlight this fact, yet the boolean
 		// doesn't really matter.
-		return []SpanPartition{{dsp.gatewaySQLInstanceID, spans}}, true /* ignoreMisplannedRanges */, nil
+		return []SpanPartition{{SQLInstanceID: dsp.gatewaySQLInstanceID, Spans: spans}},
+			true /* ignoreMisplannedRanges */, nil
 	}
 	if dsp.useGossipPlanning(ctx, planCtx) {
 		return dsp.deprecatedPartitionSpansSystem(ctx, planCtx, spans)
@@ -1339,7 +1368,7 @@ func (dsp *DistSQLPlanner) partitionSpan(
 	nodeMap map[base.SQLInstanceID]int,
 	getSQLInstanceIDForKVNodeID func(roachpb.NodeID) (base.SQLInstanceID, SpanPartitionReason),
 	ignoreMisplannedRanges *bool,
-) (_ []SpanPartition, lastPartitionIdx int, _ error) {
+) (_ []SpanPartition, lastPartitionIdx int, err error) {
 	it := planCtx.spanIter
 	// rSpan is the span we are currently partitioning.
 	rSpan, err := keys.SpanAddr(span)
@@ -1384,7 +1413,7 @@ func (dsp *DistSQLPlanner) partitionSpan(
 		partitionIdx, inNodeMap := nodeMap[sqlInstanceID]
 		if !inNodeMap {
 			partitionIdx = len(partitions)
-			partitions = append(partitions, SpanPartition{SQLInstanceID: sqlInstanceID})
+			partitions = append(partitions, SpanPartition{SQLInstanceID: sqlInstanceID, numRanges: 0, haveRangeInfo: true})
 			nodeMap[sqlInstanceID] = partitionIdx
 		}
 		partition := &partitions[partitionIdx]
@@ -1396,6 +1425,7 @@ func (dsp *DistSQLPlanner) partitionSpan(
 			// Thus, we include the span into partition.Spans without trying to
 			// merge it with the last span.
 			partition.Spans = append(partition.Spans, span)
+			partition.numRanges += 1
 			if log.ExpensiveLogEnabled(ctx, 2) {
 				log.VEventf(ctx, 2, "partition span: %s, instance ID: %d, reason: %s",
 					span, sqlInstanceID, reason)
@@ -1421,6 +1451,7 @@ func (dsp *DistSQLPlanner) partitionSpan(
 		} else {
 			partition.Spans = append(partition.Spans, partitionedSpan)
 		}
+		partition.numRanges += 1
 
 		if !endKey.Less(rSpan.EndKey) {
 			// Done.
@@ -1990,7 +2021,7 @@ func (dsp *DistSQLPlanner) maybeParallelizeLocalScans(
 		if err != nil {
 			// For some reason we couldn't partition the spans - fallback to
 			// having a single TableReader.
-			spanPartitions = []SpanPartition{{dsp.gatewaySQLInstanceID, info.spans}}
+			spanPartitions = []SpanPartition{{SQLInstanceID: dsp.gatewaySQLInstanceID, Spans: info.spans}}
 			parallelizeLocal = false
 			return spanPartitions, parallelizeLocal
 		}
@@ -2043,7 +2074,7 @@ func (dsp *DistSQLPlanner) maybeParallelizeLocalScans(
 				// We weren't able to acquire the quota for any additional
 				// goroutines, so we will fallback to having a single
 				// TableReader.
-				spanPartitions = []SpanPartition{{dsp.gatewaySQLInstanceID, info.spans}}
+				spanPartitions = []SpanPartition{{SQLInstanceID: dsp.gatewaySQLInstanceID, Spans: info.spans}}
 			}
 		}
 		if len(spanPartitions) == 1 {
@@ -2053,7 +2084,7 @@ func (dsp *DistSQLPlanner) maybeParallelizeLocalScans(
 			parallelizeLocal = false
 		}
 	} else {
-		spanPartitions = []SpanPartition{{dsp.gatewaySQLInstanceID, info.spans}}
+		spanPartitions = []SpanPartition{{SQLInstanceID: dsp.gatewaySQLInstanceID, Spans: info.spans}}
 	}
 	return spanPartitions, parallelizeLocal
 }
@@ -2086,7 +2117,7 @@ func (dsp *DistSQLPlanner) planTableReaders(
 		if err != nil {
 			return err
 		}
-		spanPartitions = []SpanPartition{{sqlInstanceID, info.spans}}
+		spanPartitions = []SpanPartition{{SQLInstanceID: sqlInstanceID, Spans: info.spans}}
 		// The spans to scan might actually live on different nodes, so we don't
 		// want to create "misplanned ranges" metadata since it can result in
 		// false positives.
