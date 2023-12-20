cockroachdb/cockroach

commit f9a720dec070179ed91f8daf1e481c9510406c1a

changefeedccl: Add a test utility to timeout when waiting for error
Add `ExpectErrWithTimeout` test utility which times out
if the error does not occur withing expected threshold.
This utility function differes from ExpectErrSucceedsSoon
in that it does not retry the call -- it merely expects
an error to be returned withing timeout threshold.

Informs #110300

Release note: None

pkg/testutils/sqlutils/sql_runner.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/errors"
 	"github.com/lib/pq"
 	"github.com/stretchr/testify/require"
@@ -197,6 +198,24 @@ func (sr *SQLRunner) ExpectErrSucceedsSoon(
 ) {
 	helperOrNoop(t)()
 	sr.succeedsWithin(t, func() error {
+		_, err := sr.DB.ExecContext(context.Background(), query, args...)
+		if !testutils.IsError(err, errRE) {
+			return errors.Newf("expected error '%s', got: %s", errRE, pgerror.FullError(err))
+		}
+		return nil
+	})
+}
+
+// ExpectErrWithTimeout wraps ExpectErr with a timeout..
+func (sr *SQLRunner) ExpectErrWithTimeout(
+	t Fataler, errRE string, query string, args ...interface{},
+) {
+	helperOrNoop(t)()
+	d := sr.SucceedsSoonDuration
+	if d == 0 {
+		d = testutils.DefaultSucceedsSoonDuration
+	}
+	_ = timeutil.RunWithTimeout(context.Background(), "expect-err", d, func(ctx context.Context) error {
 		_, err := sr.DB.ExecContext(context.Background(), query, args...)
 		if !testutils.IsError(err, errRE) {
 			return errors.Newf("expected error '%s', got: %s", errRE, pgerror.FullError(err))

pkg/testutils/sqlutils/BUILD.bazel
@@ -30,6 +30,7 @@ go_library(
         "//pkg/testutils",
         "//pkg/util/fileutil",
         "//pkg/util/protoutil",
+        "//pkg/util/timeutil",
         "@com_github_cockroachdb_cockroach_go_v2//crdb",
         "@com_github_cockroachdb_errors//:errors",
         "@com_github_jackc_pgx_v4//:pgx",

commit 9183ef2c31070b90dcb08fcec4451e1f57a324fd

changefeedccl: don't ignore error in ExpectErrWithTimeout
This commit fixes a bug in ExpectErrWithTimeout where incorrect errors
were ignored. This bug was recently introduced in f9a720d, so we've been
missing test coverage in changefeed_test.go for a few months.

Epic: None
Release note: None

pkg/testutils/sqlutils/sql_runner.go
@@ -229,7 +229,7 @@ func (sr *SQLRunner) ExpectErrSucceedsSoon(
 	})
 }
 
-// ExpectErrWithTimeout wraps ExpectErr with a timeout..
+// ExpectErrWithTimeout wraps ExpectErr with a timeout.
 func (sr *SQLRunner) ExpectErrWithTimeout(
 	t Fataler, errRE string, query string, args ...interface{},
 ) {
@@ -238,13 +238,14 @@ func (sr *SQLRunner) ExpectErrWithTimeout(
 	if d == 0 {
 		d = testutils.DefaultSucceedsSoonDuration
 	}
-	_ = timeutil.RunWithTimeout(context.Background(), "expect-err", d, func(ctx context.Context) error {
+	err := timeutil.RunWithTimeout(context.Background(), "expect-err", d, func(ctx context.Context) error {
 		_, err := sr.DB.ExecContext(ctx, query, args...)
 		if !testutils.IsError(err, errRE) {
 			return errors.Newf("expected error '%s', got: %s", errRE, pgerror.FullError(err))
 		}
 		return nil
 	})
+	require.NoError(requireT{t}, err)
 }
 
 // Query is a wrapper around gosql.Query that kills the test on error.