a6acbe8849d074be1e173cacd411e7b44ebedd98
Yahor Yuzefovich <yahor@cockroachlabs.com>
2023-12-20 10:14:59 -0800

    logictestccl: skip 3node-tenant config under race
    We just skipped 3node-tenant config under race for non-ccl logic tests
    and forgot to do so for the ccl ones. This commit fixes that as well as
    adjusts the reason for the skip.
    
    Release note: None

--- pkg/ccl/logictestccl/tests/3node-tenant-multiregion/generated_test.go
+++ pkg/ccl/logictestccl/tests/3node-tenant-multiregion/generated_test.go
@@ -85,7 +85,7 @@ func runLogicTest(t *testing.T, file string) {
 	logictest.RunLogicTest(t, logictest.TestServerArgs{}, configIdx, filepath.Join(logicTestDir, file))
 }
 func runCCLLogicTest(t *testing.T, file string) {
-	skip.UnderRace(t, "times out and/or OOM's")
+	skip.UnderRace(t, "large engflow executor is overloaded by this config")
 	skip.UnderDeadlock(t, "times out and/or hangs")
 	logictest.RunLogicTest(t, logictest.TestServerArgs{}, configIdx, filepath.Join(cclLogicTestDir, file))
 }
