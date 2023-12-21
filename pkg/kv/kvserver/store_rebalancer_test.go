623b0ce61ed79832478ac018ba8949afd143b493
Andrew Baptist <baptist@cockroachlabs.com>
2023-12-21 09:56:09 -0500

    kvserver: remove direct use of repl.mu.conf
    The repl.mu.conf will be removed and tests that use it need to change to
    set the span config differently.
    This commit removes it in TestChooseRangeToRebalanceAcrossHeterogeneousZones.
    
    Epic: none
    
    Release note: None

--- pkg/kv/kvserver/store_rebalancer_test.go
+++ pkg/kv/kvserver/store_rebalancer_test.go
@@ -497,7 +497,6 @@ func loadRanges(rr *ReplicaRankings, s *Store, ranges []testRange) {
 		rangeID := roachpb.RangeID(i + 1)
 		repl := &Replica{store: s, RangeID: rangeID}
 		repl.shMu.state.Desc = &roachpb.RangeDescriptor{RangeID: rangeID}
-		repl.mu.conf = s.cfg.DefaultSpanConfig
 		for _, storeID := range r.voters {
 			repl.shMu.state.Desc.InternalReplicas = append(repl.shMu.state.Desc.InternalReplicas, roachpb.ReplicaDescriptor{
 				NodeID:    roachpb.NodeID(storeID),
@@ -1267,6 +1266,12 @@ func TestChooseRangeToRebalanceAcrossHeterogeneousZones(t *testing.T) {
 			)
 
 			hottestRanges := sr.replicaRankings.TopLoad(lbRebalanceDimension)
+			// TODO(baptist): Remove this once the cached span conf is removed
+			// from the replica. The DefaultSpanConfig can be removed also once
+			// this change is made.
+			for _, replica := range hottestRanges {
+				replica.Repl().SetSpanConfig(s.cfg.DefaultSpanConfig, replica.Desc().KeySpan().AsRawSpanWithNoLocals())
+			}
 			options := sr.scorerOptions(ctx, lbRebalanceDimension)
 			rctx := sr.NewRebalanceContext(ctx, options, hottestRanges, LBRebalancingLeasesAndReplicas)
 			rctx.options.IOOverloadOptions = allocatorimpl.IOOverloadOptions{
@@ -1524,6 +1529,12 @@ func TestChooseRangeToRebalanceOffHotNodes(t *testing.T) {
 			)
 
 			hottestRanges := sr.replicaRankings.TopLoad(lbRebalanceDimension)
+			// TODO(baptist): Remove this once the cached span conf is removed
+			// from the replica. The DefaultSpanConfig can be removed also once
+			// this change is made.
+			for _, replica := range hottestRanges {
+				replica.Repl().SetSpanConfig(s.cfg.DefaultSpanConfig, replica.Desc().KeySpan().AsRawSpanWithNoLocals())
+			}
 			options := sr.scorerOptions(ctx, lbRebalanceDimension)
 			rctx := sr.NewRebalanceContext(ctx, options, hottestRanges, sr.RebalanceMode())
 			rctx.options.IOOverloadOptions = allocatorimpl.IOOverloadOptions{
@@ -1791,6 +1802,12 @@ func TestStoreRebalancerIOOverloadCheck(t *testing.T) {
 			loadRanges(rr, s, []testRange{{voters: []roachpb.StoreID{1, 3, 5}, qps: 100, reqCPU: 100 * float64(time.Millisecond)}})
 
 			hottestRanges := sr.replicaRankings.TopLoad(lbRebalanceDimension)
+			// TODO(baptist): Remove this once the cached span conf is removed
+			// from the replica. The DefaultSpanConfig can be removed also once
+			// this change is made.
+			for _, replica := range hottestRanges {
+				replica.Repl().SetSpanConfig(s.cfg.DefaultSpanConfig, replica.Desc().KeySpan().AsRawSpanWithNoLocals())
+			}
 			options := sr.scorerOptions(ctx, lbRebalanceDimension)
 			rctx := sr.NewRebalanceContext(ctx, options, hottestRanges, sr.RebalanceMode())
 			require.Greater(t, len(rctx.hottestRanges), 0)
