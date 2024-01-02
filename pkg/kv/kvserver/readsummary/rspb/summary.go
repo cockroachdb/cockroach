4a3557a241d73d3da84b4c595af5066264b23cde
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2024-01-02 18:25:25 -0500

    kv: add read spans to ReadSummary structure
    Informs #61986.
    
    This PR adds read spans to the ReadSummary message, which is a compressed
    summary all read requests served on a range. The reads are held in sorted order
    and are non-overlapping, permitting a linear-time merge operation. With the
    inclusion of read spans, the ReadSummary message can now be used to serialize
    a leaseholder's timestamp cache.
    
    The merge operation is implemented in a subsequent commit.
    
    Release note: None

--- pkg/kv/kvserver/readsummary/rspb/summary.go
+++ pkg/kv/kvserver/readsummary/rspb/summary.go
@@ -11,11 +11,14 @@
 package rspb
 
 import (
-	"context"
+	"bytes"
+	"fmt"
 
+	"github.com/cockroachdb/cockroach/pkg/util"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
-	"github.com/cockroachdb/cockroach/pkg/util/log"
 )
+
+const assertEnabled = util.RaceEnabled
 
 // FromTimestamp constructs a read summary from the provided timestamp, treating
 // the argument as the low water mark of each segment in the summary.
@@ -28,10 +31,11 @@ func FromTimestamp(ts hlc.Timestamp) ReadSummary {
 }
 
 // Clone performs a deep-copy of the receiver.
-func (c ReadSummary) Clone() *ReadSummary {
-	// NOTE: When ReadSummary is updated to include pointers to non-contiguous
-	// memory, this will need to be updated.
-	return &c
+func (c *ReadSummary) Clone() *ReadSummary {
+	res := new(ReadSummary)
+	res.Local = c.Local.Clone()
+	res.Global = c.Global.Clone()
+	return res
 }
 
 // Merge combines two read summaries, resulting in a single summary that
@@ -42,23 +46,42 @@ func (c *ReadSummary) Merge(o ReadSummary) {
 	c.Global.merge(o.Global)
 }
 
+// AddReadSpan adds a read span to the segment. The span must be sorted after
+// all existing spans in the segment and must not overlap with any existing
+// spans.
+func (c *Segment) AddReadSpan(s ReadSpan) {
+	if assertEnabled {
+		if len(s.EndKey) != 0 {
+			if bytes.Compare(s.Key, s.EndKey) >= 0 {
+				panic(fmt.Sprintf("inverted span: %v", s))
+			}
+		}
+		if len(c.ReadSpans) > 0 {
+			last := c.ReadSpans[len(c.ReadSpans)-1]
+			if bytes.Compare(last.Key, s.Key) >= 0 {
+				panic(fmt.Sprintf("out of order spans: %v %v", last, s))
+			}
+			if len(last.EndKey) != 0 && bytes.Compare(last.EndKey, s.Key) > 0 {
+				panic(fmt.Sprintf("overlapping spans: %v %v", last, s))
+			}
+		}
+	}
+	if s.Timestamp.LessEq(c.LowWater) {
+		return // ignore
+	}
+	c.ReadSpans = append(c.ReadSpans, s)
+}
+
+// Clone performs a deep-copy of the receiver.
+func (c *Segment) Clone() Segment {
+	res := *c
+	if len(c.ReadSpans) != 0 {
+		res.ReadSpans = make([]ReadSpan, len(c.ReadSpans))
+		copy(res.ReadSpans, c.ReadSpans)
+	}
+	return res
+}
+
 func (c *Segment) merge(o Segment) {
 	c.LowWater.Forward(o.LowWater)
 }
-
-// AssertNoRegression asserts that all reads in the parameter's summary are
-// reflected in the receiver's summary with at least as high of a timestamp.
-func (c *ReadSummary) AssertNoRegression(ctx context.Context, o ReadSummary) {
-	c.Local.assertNoRegression(ctx, o.Local, "local")
-	c.Global.assertNoRegression(ctx, o.Global, "global")
-}
-
-func (c *Segment) assertNoRegression(ctx context.Context, o Segment, name string) {
-	if c.LowWater.Less(o.LowWater) {
-		log.Fatalf(ctx, "read summary regression in %s segment, was %s, now %s",
-			name, o.LowWater, c.LowWater)
-	}
-}
-
-// Ignore unused warning.
-var _ = (*ReadSummary).AssertNoRegression
