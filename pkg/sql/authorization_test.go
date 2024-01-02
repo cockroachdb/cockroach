994963bd1b02c711939d1ca4ac53ee2d8edb5448
Xiang Gu <xiang@cockroachlabs.com>
2024-01-02 15:34:26 -0800

    sql: use high priority for populating RoleMemberCache
    Previously, when the RoleMemberCache is invalid, it launches a new txn
    in a singleflight to read from `system.role_members` table to populate
    the cache. If, however, the original txn has previously laid a write
    intent on the same system table, then we end up having a deadlock:
    original txn waits for this new txn; this new txn waits for original txn
    This commit therefore make the singleflight txn high priority so that
    it won't be blocked by the original txn.
    
    However, if the original txn is already high priority, the deadlock
    becomes possible again. Fortunately, in this case, txn in retry is a
    prerequisite for the deadlock to occur (see
    https://github.com/cockroachdb/cockroach/pull/117258#issuecomment-1877808997)
    so our solution to it is to not use singleflight to populate
    RoleMemberCache if txn is high priority and in retry but instead
    directly use txn itself to read and return the result.
    
    Release note (bug fix): Fixed a bug where concurrent GRANTs can cause
    deadlocks.

--- pkg/sql/authorization_test.go
+++ pkg/sql/authorization_test.go
@@ -12,12 +12,18 @@ package sql_test
 
 import (
 	"context"
+	"fmt"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descs"
 	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/stretchr/testify/require"
@@ -83,4 +89,75 @@ func TestCheckAnyPrivilegeForNodeUser(t *testing.T) {
 		t.Fatal(err)
 	}
 
+}
+
+// TestConcurrentGrant tests that populating MembershipCache with a high
+// priority can avoid deadlock from concurrent GRANTs, an issue detailed in
+// #117144.
+func TestConcurrentGrants(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	skip.UnderStress(t, "this test needs to sleep half a second")
+
+	runConcurrentGrantsWithPriority := func(t *testing.T, priority string) {
+		ctx := context.Background()
+		s, db, _ := serverutils.StartServer(t, base.TestServerArgs{})
+		defer s.Stopper().Stop(ctx)
+		tdb := sqlutils.MakeSQLRunner(db)
+
+		// Shortening this cluster setting is essential for this test. A small value
+		// will force `txn1` to bump up its write timestamp by the time it gets to
+		// commit. This is needed to force `txn2` to encounter a WriteTooOldError
+		// after being unblocked, and hence enter a retry, as retry in `txn2` is
+		// prerequisite for the potential deadlock described in #117144.
+		tdb.Exec(t, "SET CLUSTER SETTING kv.closed_timestamp.target_duration = '1ms'")
+
+		tdb.Exec(t, "CREATE ROLE developer;")
+		tdb.Exec(t, "CREATE USER user1")
+		tdb.Exec(t, "CREATE USER user2")
+
+		// 1. Start a txn1, GRANT something
+		// 2. Start another txn2 that issues a concurrent GRANT (it will block)
+		// 3. Wait a short while
+		// 4. Commit txn1 and that will unblock txn2
+		cg := ctxgroup.WithContext(ctx)
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+
+		cg.Go(func() error {
+			wg.Wait()
+			// txn2
+			_, err := db.Exec(fmt.Sprintf("BEGIN PRIORITY %s; GRANT developer TO user2; COMMIT", priority))
+			return err
+		})
+
+		txn1, err := db.Begin()
+		require.NoError(t, err)
+		_, err = txn1.Exec(fmt.Sprintf("SET TRANSACTION PRIORITY %s", priority))
+		require.NoError(t, err)
+		_, err = txn1.Exec("GRANT developer TO user1")
+		require.NoError(t, err)
+		wg.Done()
+
+		// Wait for a few seconds. It allows:
+		// 1. txn2 to start running and blocking
+		// 2. (more importantly) `txn1` to bump up its write timestamp upon commit,
+		//    so, it will cause txn2 to encounter a WriteTooOld error after
+		//    unblocking and retry.
+		time.Sleep(500 * time.Millisecond)
+		err = txn1.Commit()
+		require.NoError(t, err)
+
+		// Wait to ensure `grant developer to user2` also completed successfully.
+		err = cg.Wait()
+		require.NoError(t, err)
+	}
+
+	for _, priority := range []string{"NORMAL", "HIGH"} {
+		t.Run(fmt.Sprintf("concurrent GRANTs with %s priority", priority), func(
+			t *testing.T,
+		) {
+			runConcurrentGrantsWithPriority(t, priority)
+		})
+	}
 }
