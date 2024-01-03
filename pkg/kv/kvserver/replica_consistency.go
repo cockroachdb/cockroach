2ec154b292b924ac1ad230403cfba16bdae4433b
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2024-01-03 18:32:47 -0500

    kv: bump ReplicaChecksumVersion, disable cross v23.2/v24.1 consistency checks
    Informs #101938.
    
    This commit bumps the ReplicaChecksumVersion to disable replica consistency
    checks between v23.2 and v24.1 nodes when in a mixed-version cluster. This
    avoids the backwards incompatibility discussed in #117302.
    
    While here and permitted to change the replica consistency check logic, we
    unset the Synthetic flag from RangeAppliedState.RaftClosedTimestamp during
    stats-only consistency checks. This form of consistency check is rarely used,
    but this prevents it from causing trouble with #101938.
    
    While here and allowed to change the consistency check hash computation, we
    also switch from using the LegacyTimestamp encoding to the Timestamp encoding
    for the hash contribution of MVCCKeys.
    
    Release note: None

--- pkg/kv/kvserver/replica_consistency.go
+++ pkg/kv/kvserver/replica_consistency.go
@@ -496,7 +496,7 @@ func CalcReplicaDigest(
 
 	// Iterate over all the data in the range.
 	var intBuf [8]byte
-	var legacyTimestamp hlc.LegacyTimestamp
+	var timestamp hlc.Timestamp
 	var timestampBuf []byte
 	var uuidBuf [uuid.Size]byte
 	hasher := sha512.New()
@@ -556,13 +556,13 @@ func CalcReplicaDigest(
 		if _, err := hasher.Write(unsafeKey.Key); err != nil {
 			return err
 		}
-		legacyTimestamp = unsafeKey.Timestamp.ToLegacyTimestamp()
-		if size := legacyTimestamp.Size(); size > cap(timestampBuf) {
+		timestamp = unsafeKey.Timestamp
+		if size := timestamp.Size(); size > cap(timestampBuf) {
 			timestampBuf = make([]byte, size)
 		} else {
 			timestampBuf = timestampBuf[:size]
 		}
-		if _, err := protoutil.MarshalToSizedBuffer(&legacyTimestamp, timestampBuf); err != nil {
+		if _, err := protoutil.MarshalToSizedBuffer(&timestamp, timestampBuf); err != nil {
 			return err
 		}
 		if _, err := hasher.Write(timestampBuf); err != nil {
@@ -600,13 +600,13 @@ func CalcReplicaDigest(
 		if _, err := hasher.Write(rangeKV.RangeKey.EndKey); err != nil {
 			return err
 		}
-		legacyTimestamp = rangeKV.RangeKey.Timestamp.ToLegacyTimestamp()
-		if size := legacyTimestamp.Size(); size > cap(timestampBuf) {
+		timestamp = rangeKV.RangeKey.Timestamp
+		if size := timestamp.Size(); size > cap(timestampBuf) {
 			timestampBuf = make([]byte, size)
 		} else {
 			timestampBuf = timestampBuf[:size]
 		}
-		if _, err := protoutil.MarshalToSizedBuffer(&legacyTimestamp, timestampBuf); err != nil {
+		if _, err := protoutil.MarshalToSizedBuffer(&timestamp, timestampBuf); err != nil {
 			return err
 		}
 		if _, err := hasher.Write(timestampBuf); err != nil {
@@ -680,6 +680,8 @@ func CalcReplicaDigest(
 	result.PersistedMS = rangeAppliedState.RangeStats.ToStats()
 
 	if statsOnly {
+		// Unset the synthetic flag, to ease the migration of deleting this field.
+		rangeAppliedState.RaftClosedTimestamp.Synthetic = false
 		b, err := protoutil.Marshal(rangeAppliedState)
 		if err != nil {
 			return nil, err
