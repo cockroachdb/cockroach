ba038300527ef49f211cd5bf04e38d7b445a265d
DarrylWong <darryl@cockroachlabs.com>
2024-01-05 16:36:24 -0500

    roachtest: add helpers to extract sessionID and create default HTTPClient
    These helpers are for roachtests to easily create a HTTPClient with
    appropriate cookies. It also sets InsecureSkipVerify to true as any
    certs we create won't be valid anyway.
    
    This change is so that we can create http requests to secure clusters.
    
    Release note: None

--- pkg/cmd/roachtest/roachtestutil/utils.go
+++ pkg/cmd/roachtest/roachtestutil/utils.go
@@ -12,13 +12,20 @@ package roachtestutil
 
 import (
 	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/cluster"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/option"
+	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/test"
 	"github.com/cockroachdb/cockroach/pkg/roachprod"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/config"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/logger"
+	"github.com/cockroachdb/cockroach/pkg/util/httputil"
+	"github.com/pkg/errors"
 )
 
 // SystemInterfaceSystemdUnitName is a convenience function that
@@ -55,4 +62,78 @@ func SetDefaultAdminUIPort(c cluster.Cluster, opts *install.StartOpts) {
 	if !c.IsLocal() {
 		opts.AdminUIPort = config.DefaultAdminUIPort
 	}
+}
+
+func getSessionIDOnSingleNode(
+	ctx context.Context, c cluster.Cluster, l *logger.Logger, node option.NodeListOption,
+) (string, error) {
+	loginCmd := fmt.Sprintf(
+		"%s auth-session login root --port={pgport%s} --certs-dir ./certs --format raw",
+		test.DefaultCockroachPath, node,
+	)
+	res, err := c.RunWithDetailsSingleNode(ctx, l, option.WithNodes(node), loginCmd)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to authenticate")
+	}
+
+	var sessionCookie string
+	for _, line := range strings.Split(res.Stdout, "\n") {
+		if strings.HasPrefix(line, "session=") {
+			sessionCookie = line
+		}
+	}
+	if sessionCookie == "" {
+		return "", fmt.Errorf("failed to find session cookie in `login` output")
+	}
+
+	return sessionCookie, nil
+}
+
+func GetSessionID(
+	ctx context.Context, c cluster.Cluster, l *logger.Logger, nodes option.NodeListOption,
+) (string, error) {
+	var err error
+	var cookie string
+	// The session ID should be the same for all nodes so stop after we successfully
+	// get it from one node.
+	for _, node := range nodes {
+		cookie, err = getSessionIDOnSingleNode(ctx, c, l, c.Node(node))
+		if err == nil {
+			break
+		}
+	}
+	if err != nil {
+		return "", errors.Wrapf(err, "roachtestutils.GetSessionID")
+	}
+	sessionID := strings.Split(cookie, ";")[0]
+	return sessionID, nil
+}
+
+// DefaultHttpClientWithSessionCookie returns a default Http client as defined in httputil,
+// and adds the session id to the cookie jar for the specified url.
+func DefaultHttpClientWithSessionCookie(
+	ctx context.Context,
+	c cluster.Cluster,
+	l *logger.Logger,
+	nodes option.NodeListOption,
+	cookieUrl string,
+) (http.Client, error) {
+	sessionID, err := GetSessionID(ctx, c, l, nodes)
+	if err != nil {
+		return http.Client{}, err
+	}
+	name, value, found := strings.Cut(sessionID, "=")
+	if !found {
+		return http.Client{}, errors.New("Cookie not formatted correctly")
+	}
+	client := httputil.DefaultClient.Client
+	url, err := url.Parse(cookieUrl)
+	if err != nil {
+		return http.Client{}, err
+	}
+	err = httputil.SetCookies(client, url, []*http.Cookie{{Name: name, Value: value}})
+	if err != nil {
+		return http.Client{}, err
+	}
+	return *client, nil
 }
