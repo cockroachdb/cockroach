44edd1f38e45589410bedf58db21d35d937eda17
Xiang Gu <xiang@cockroachlabs.com>
2024-01-08 16:58:18 -0500

    scpb: Introduce DatabaseZoneConfig element
    This commit introduces the DatabaseZoneConfig element that will become
    relevant for supporting `ALTER DATABASE ... CONFIGURE ZONE` in DSC.
    
    Release note: None

--- pkg/sql/schemachanger/screl/attr.go
+++ pkg/sql/schemachanger/screl/attr.go
@@ -71,6 +71,14 @@ const (
 	// RecreateSourceIndexID is the index ID that we are replacing with
 	// this new index.
 	RecreateSourceIndexID
+	// SeqNum is a number given to different elements of the same conceptual
+	// object. It is used so we can differentiate those elements in the graph.
+	//
+	// E.g. Updating a zone config of some database `db` translates to the
+	// dropping of the existing zone config (if any) element and adding of another
+	// zone config element. Those two DatabaseZoneConfig nodes in the graph will
+	// have different SeqNum attribute.
+	SeqNum
 
 	// TargetStatus is the target status of an element.
 	TargetStatus
@@ -362,6 +370,10 @@ var elementSchemaOptions = []rel.SchemaOption{
 		rel.EntityAttr(DescID, "TableID"),
 		rel.EntityAttr(IndexID, "IndexID"),
 		rel.EntityAttr(ColumnID, "ColumnID"),
+	),
+	rel.EntityMapping(t((*scpb.DatabaseZoneConfig)(nil)),
+		rel.EntityAttr(DescID, "DatabaseID"),
+		rel.EntityAttr(SeqNum, "SeqNum"),
 	),
 	rel.EntityMapping(t((*scpb.TableZoneConfig)(nil)),
 		rel.EntityAttr(DescID, "TableID"),
