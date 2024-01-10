dcde10a7868798cb752855be80e02d0490d3951d
Andrew Baptist <baptist@cockroachlabs.com>
2024-01-10 14:17:08 -0500

    kvclient: remove retries within sendToReplicas
    This change removes a retry loop within sendToReplicas that occured when
    we hit a NLHE. We still retry within the outer loop of sendPartialBatch
    and removing this also removes the only remaining reordering of the
    replicas that are retrieved from the RangeCache.
    
    There was a metric distsender.errors.inleasetransferbackoffs that tracks
    the number of retries within this now removed loop that is also removed.
    
    Epic: none
    
    Release note: none

--- pkg/kv/kvclient/kvcoord/dist_sender_test.go
+++ pkg/kv/kvclient/kvcoord/dist_sender_test.go
@@ -823,6 +823,8 @@ func TestRetryOnNotLeaseHolderError(t *testing.T) {
 				attempts++
 				reply := &kvpb.BatchResponse{}
 				if attempts == 1 {
+					// Bump the generation so dist sender will use this new descriptor.
+					tc.nlhe.RangeDesc.Generation = 2
 					reply.Error = kvpb.NewError(&tc.nlhe)
 					return reply, nil
 				}
@@ -1308,22 +1310,17 @@ func TestDistSenderIgnoresNLHEBasedOnOldRangeGeneration(t *testing.T) {
 			require.Nil(t, pErr)
 
 			require.Equal(t, int64(0), ds.Metrics().RangeLookups.Count())
-			// We expect to backoff and retry the same replica 11 times when we get an
-			// NLHE with stale info. See `sameReplicaRetryLimit`.
-			require.Equal(t, int64(11), ds.Metrics().NextReplicaErrCount.Count())
-			require.Equal(t, int64(11), ds.Metrics().NotLeaseHolderErrCount.Count())
+			// We expect to retry once due to the NHLE.
+			require.Equal(t, int64(1), ds.Metrics().NextReplicaErrCount.Count())
+			require.Equal(t, int64(1), ds.Metrics().NotLeaseHolderErrCount.Count())
 
-			// Ensure that we called Node 2 11 times and then finally called Node 1.
-			var expectedCalls []roachpb.NodeID
-			for i := 0; i < 11; i++ {
-				expectedCalls = append(expectedCalls, roachpb.NodeID(2))
-			}
-			expectedCalls = append(expectedCalls, roachpb.NodeID(1))
+			// Ensure that we called Node 2 once and then Node 1.
+			expectedCalls := []roachpb.NodeID{2, 1}
 			require.Equal(t, expectedCalls, calls)
 
 			require.Regexp(
 				t,
-				"backing off due to .* stale info",
+				"retry with updated routing info",
 				finishAndGetRecording().String(),
 			)
 		})
@@ -5934,7 +5931,8 @@ func TestDistSenderNLHEFromUninitializedReplicaDoesNotCauseUnboundedBackoff(t *t
 		},
 	}
 	leaseResp := roachpb.Lease{
-		Replica: roachpb.ReplicaDescriptor{NodeID: 4, StoreID: 4, ReplicaID: 4},
+		Sequence: 1,
+		Replica:  roachpb.ReplicaDescriptor{NodeID: 4, StoreID: 4, ReplicaID: 4},
 	}
 
 	call := 0
