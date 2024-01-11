962bc142ba5f43378d851e8a4226287011c4ff97
Stan Rosenberg <stan@cockroachlabs.com>
2024-01-11 03:54:21 +0000

    roachtest: expand support for metamorphic arm64 runs in CI
    CI support for metamorphic arm64 runs was enabled in [1]
    with some limitations. This PR addresses those limitations.
    
    Specifically, we now support all compatible performance
    tests by separating the resulting performance artifacts
    from runs using a different cpu architecture.
    (The corresponding artifacts path includes the cpu arch.)
    
    We also fix up docker install script to support arm64
    runs, e.g., tpce.
    
    Resolves: https://github.com/cockroachdb/cockroach/issues/104213
    Epic: none
    Release note: None
    
    [1] https://github.com/cockroachdb/cockroach/pull/104303

--- build/teamcity/util/roachtest_util.sh
+++ build/teamcity/util/roachtest_util.sh
@@ -18,7 +18,7 @@ stats_dir="$(date +"%Y%m%d")-${TC_BUILD_ID}"
 
 # Set up a function we'll invoke at the end.
 function upload_stats {
- if tc_release_branch; then
+  if tc_release_branch; then
       bucket="${ROACHTEST_BUCKET:-cockroach-nightly-${CLOUD}}"
       if [[ "${CLOUD}" == "gce" ]]; then
           # GCE, having been there first, gets an exemption.
@@ -32,6 +32,7 @@ function upload_stats {
         # the location.
         remote_artifacts_dir="artifacts"
       fi
+      # TODO: FIPS_ENABLED is deprecated, use roachtest --metamorphic-fips-probability, instead.
       # In FIPS-mode, keep artifacts separate by using the 'fips' suffix.
       if [[ ${FIPS_ENABLED:-0} == 1 ]]; then
         remote_artifacts_dir="${remote_artifacts_dir}-fips"
@@ -48,7 +49,14 @@ function upload_stats {
       (cd "${artifacts}" && \
         while IFS= read -r f; do
           if [[ -n "${f}" ]]; then
-            gsutil cp "${f}" "gs://${bucket}/${remote_artifacts_dir}/${stats_dir}/${f}"
+            artifacts_dir="${remote_artifacts_dir}"
+            # If 'cpu_arch=xxx' is encoded in the path, use it as suffix to separate artifacts by cpu_arch.
+            if [[ "${f}" == *"/cpu_arch=arm64/"* ]]; then
+              artifacts_dir="${artifacts_dir}-arm64"
+            elif [[ "${f}" == *"/cpu_arch=fips/"* ]]; then
+              artifacts_dir="${artifacts_dir}-fips"
+            fi
+            gsutil cp "${f}" "gs://${bucket}/${artifacts_dir}/${stats_dir}/${f}"
           fi
         done <<< "$(find . -name stats.json | sed 's/^\.\///')")
   fi
--- pkg/roachprod/install/cluster_synced.go
+++ pkg/roachprod/install/cluster_synced.go
@@ -2178,7 +2178,7 @@ func (c *SyncedCluster) Put(
 	}
 
 	if finalErr != nil {
-		return errors.Wrapf(finalErr, "put %s failed", src)
+		return errors.Wrapf(finalErr, "put %q failed", src)
 	}
 	return nil
 }
