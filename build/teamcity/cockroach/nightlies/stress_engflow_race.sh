e7fa1b8caf5d6617fe28a2175dd2d2a6fcaacba4
Ricky Stewart <ricky@cockroachlabs.com>
2024-01-12 16:32:03 -0600

    stress: reduce timeouts for `deadlock` and `race` under RBE
    `deadlock` timeouts appear to be much higher than they need to: `race`
    can also be reduced somewhat.
    
    Epic: CRDB-8308
    Release note: None

--- build/teamcity/cockroach/nightlies/stress_engflow_race.sh
+++ build/teamcity/cockroach/nightlies/stress_engflow_race.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 export RUNS_PER_TEST=3
-export EXTRA_TEST_ARGS="--config=race --test_timeout=1800,3600,5395,5395"
+export EXTRA_TEST_ARGS="--config=race --test_timeout=1200,2500,3200,4600"
 export EXTRA_ISSUE_PARAMS=race
 
 THIS_DIR=$(cd "$(dirname "$0")" && pwd)
