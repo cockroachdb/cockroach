d9a0ed9ef4d9af32fd9a6fb614d463022a9c34a6
Erik Grinaker <grinaker@cockroachlabs.com>
2024-01-15 16:26:13 +0000

    kvpb: mark `BarrierRequest` as `isAlone`
    Otherwise, `BatchRequest.RequiresConsensus()` may return `false` and not
    submit the barrier through Raft. Similarly,
    `shouldWaitOnLatchesWithoutAcquiring` will return `false` so it will
    contend with later writes.
    
    Barriers are not used in recent releases, so this does not have any
    mixed-version concerns.
    
    Epic: none
    Release note: None

--- pkg/kv/kvpb/batch.go
+++ pkg/kv/kvpb/batch.go
@@ -215,6 +215,12 @@ func (ba *BatchRequest) IsSingleRequest() bool {
 	return len(ba.Requests) == 1
 }
 
+// IsSingleBarrierRequest returns true iff the batch contains a single request,
+// and that request is a Barrier.
+func (ba *BatchRequest) IsSingleBarrierRequest() bool {
+	return ba.isSingleRequestWithMethod(Barrier)
+}
+
 // IsSingleSkipsLeaseCheckRequest returns true iff the batch contains a single
 // request, and that request has the skipsLeaseCheck flag set.
 func (ba *BatchRequest) IsSingleSkipsLeaseCheckRequest() bool {
@@ -349,7 +355,7 @@ func (ba *BatchRequest) IsSingleExportRequest() bool {
 // a no-op. The Barrier request requires consensus even though its evaluation
 // is a no-op.
 func (ba *BatchRequest) RequiresConsensus() bool {
-	return ba.isSingleRequestWithMethod(Barrier) || ba.isSingleRequestWithMethod(Probe)
+	return ba.IsSingleBarrierRequest() || ba.IsSingleProbeRequest()
 }
 
 // IsCompleteTransaction determines whether a batch contains every write in a
