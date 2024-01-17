a17793a3dca0e8ae9cd525dd749d3832526a1db9
Austen McClernon <austen@cockroachlabs.com>
2024-01-17 23:31:36 +0000

    allocatorimpl: fix replace constraints check function
    Previously, it was possible for replica replacement to not return a
    valid target when some constraint was initially undersatisfied in
    addition to the replica being replaced satisfying a separate constraint.
    
    When this occurred, it could stall decommissioning a node, as replicas
    become stuck with the allocator returning no valid replacement target.
    
    Update the `replaceConstraintsCheck` function to correctly consider
    whether the replacement store satisfies the constraint when computing if
    the replacement is necessary.
    
    Fixes: #117886
    Part of: #117891
    Release note (bug fix): Decommissioning replicas which are part of a
    mis-replicated range will no longer get stuck on a rebalance operation
    that was falsely determined to be unsafe. This bug was introduced in
    23.1.0.

--- pkg/kv/kvserver/store_test.go
+++ pkg/kv/kvserver/store_test.go
@@ -3778,7 +3778,6 @@ func TestAllocatorCheckRange(t *testing.T) {
 			expectedAction:     allocatorimpl.AllocatorReplaceDecommissioningVoter,
 			expectAllocatorErr: true,
 			expectedErrStr:     "replicas must match constraints",
-			expectedLogMessage: "cannot allocate necessary voter on s3",
 		},
 		{
 			name:   "decommissioning without satisfying multiple partial constraints",
@@ -3818,7 +3817,6 @@ func TestAllocatorCheckRange(t *testing.T) {
 			expectedAction:     allocatorimpl.AllocatorReplaceDecommissioningVoter,
 			expectAllocatorErr: true,
 			expectedErrStr:     "replicas must match constraints",
-			expectedLogMessage: "cannot allocate necessary voter on s3",
 		},
 		{
 			name:   "decommissioning during upreplication with partial constraints",
