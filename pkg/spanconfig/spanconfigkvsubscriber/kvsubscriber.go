1300ab7d3bb8edcdb19b9d24f50b85b89ad866d1
Arul Ajmani <arulajmani@gmail.com>
2024-01-19 17:31:56 -0500

    spanconfig: correctly order updates when applying in the KVSubscriber
    Previously, there was no ordering guarantee between KVSubscriber
    events at the same timestamp. As a result, if a batch of events
    included updates to overlapping spans at the same timestamp, we could
    apply additions before deletions -- this would cause the additions to
    get clobbered, which was not the intention. This could lead to missing
    span configurations, resulting in bugs such as the linked issue.
    
    This patch fixes the issue by sorting deletions before additions if
    two span configuration events have the same timestamp.
    
    Closes https://github.com/cockroachdb/cockroach/issues/110908
    
    Release note (bug fix): Previously, altering from a Regional By Row
    table to a Regional By Table table could cause leaseholders to never
    move to the databse's primary region. This is now fixed.

--- pkg/spanconfig/spanconfigkvsubscriber/kvsubscriber.go
+++ pkg/spanconfig/spanconfigkvsubscriber/kvsubscriber.go
@@ -12,6 +12,7 @@ package spanconfigkvsubscriber
 
 import (
 	"context"
+	"sort"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/keys"
@@ -439,6 +440,27 @@ func (s *KVSubscriber) setLastUpdatedLocked(ts hlc.Timestamp) {
 func (s *KVSubscriber) handlePartialUpdate(
 	ctx context.Context, ts hlc.Timestamp, events []rangefeedbuffer.Event,
 ) {
+	// The events we've received from the rangefeed buffer are sorted in
+	// increasing timestamp order. However, any updates with the same timestamp
+	// may be ordered arbitrarily. That's okay if they don't overlap. However, if
+	// they do overlap, the assumption is that an overlapping delete should be
+	// ordered before an addition it overlaps with -- not doing would cause the
+	// addition to get clobbered by the deletion, which will result in the store
+	// having missing span configurations. As such, we re-sort the list of events
+	// before applying it to our store, using Deletion() as a tie-breaker when
+	// timestamps are equal.
+	sort.Slice(events, func(i, j int) bool {
+		switch events[i].Timestamp().Compare(events[j].Timestamp()) {
+		case -1: // ts(i) < ts(j)
+			return true
+		case 1: // ts(i) > ts(j)
+			return false
+		case 0: // ts(i) == ts(j); deletions sort before additions
+			return events[i].(*BufferEvent).Deletion() // no need to worry about the sort being stable
+		default:
+			panic("unexpected")
+		}
+	})
 	handlers := func() []handler {
 		s.mu.Lock()
 		defer s.mu.Unlock()
