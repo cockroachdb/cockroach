8fa54f267632dc47c673cdf78f4de2839d571be3
Arul Ajmani <arulajmani@gmail.com>
2024-01-19 17:31:56 -0500

    spanconfig: correctly order updates when applying in the KVSubscriber
    Previously, there was no ordering guarantee between KVSubscriber
    events at the same timestamp. As a result, if a batch of events
    included updates to overlapping spans at the same timestamp, we could
    apply additions before deletions -- this would cause the additions to
    get clobbered, which was not the intention. This could lead to missing
    span configurations, resulting in bugs such as the linked issue.
    
    This patch fixes the issue by sorting deletions before additions if
    two span configuration events have the same timestamp.
    
    Closes https://github.com/cockroachdb/cockroach/issues/110908
    
    Release note (bug fix): Previously, altering from a Regional By Row
    table to a Regional By Table table could cause leaseholders to never
    move to the databse's primary region. This is now fixed.

---
+++ pkg/ccl/spanconfigccl/spanconfigkvsubscriberccl/kvsubscriber_test.go
@@ -0,0 +1,76 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package spanconfigkvsubscriberccl
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/ccl/multiregionccl/multiregionccltestutils"
+	"github.com/cockroachdb/cockroach/pkg/jobs"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+)
+
+// TestSpanConfigUpdatesApplyInCorrectOrder ensures the KVSubscriber applies
+// updates to its in-memory store in correct order. In particular, when there
+// are both deletions and additions at the same timestamp, the deletions should
+// be applied before the additions. This scenario is created by altering from a
+// regional by row table to a regional table -- doing so creates overlapping
+// updates with both deletions and additions with the same timestamo.
+//
+// Regression test for https://github.com/cockroachdb/cockroach/issues/110908.
+func TestSpanConfigUpdatesApplyInCorrectOrder(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	tc, _, cleanup := multiregionccltestutils.TestingCreateMultiRegionCluster(t, 3, base.TestingKnobs{
+		Store: &kvserver.StoreTestingKnobs{
+			DisableMergeQueue: true, // we don't want the partitions to merge away
+		},
+		JobsTestingKnobs: jobs.NewTestingKnobsWithShortIntervals(), // speed up schema changes.
+	})
+	defer cleanup()
+	sqlDB := sqlutils.MakeSQLRunner(tc.ServerConn(0 /* idx */))
+
+	// Speed up the test.
+	sqlDB.Exec(t, `SET CLUSTER SETTING kv.closed_timestamp.target_duration = '50ms'`)
+	sqlDB.Exec(t, `SET CLUSTER SETTING kv.closed_timestamp.side_transport_interval = '50ms'`)
+	sqlDB.Exec(t, `SET CLUSTER SETTING kv.rangefeed.closed_timestamp_refresh_interval = '50ms'`)
+
+	sqlDB.Exec(t, `CREATE DATABASE mr PRIMARY REGION "us-east1" REGIONS "us-east2", "us-east3"; USE mr`)
+	sqlDB.Exec(t, `CREATE TABLE t() LOCALITY REGIONAL BY ROW`)
+
+	testutils.SucceedsSoon(t, func() error {
+		var count int
+		sqlDB.QueryRow(t,
+			"SELECT count(distinct lease_holder) from [show ranges from table t with details]",
+		).Scan(&count)
+		if count == 3 {
+			return nil
+		}
+		return errors.New("waiting for each region to pick up leaseholders")
+	})
+
+	sqlDB.Exec(t, `ALTER TABLE t SET LOCALITY REGIONAL BY TABLE`)
+	testutils.SucceedsSoon(t, func() error {
+		var count int
+		sqlDB.QueryRow(t,
+			"SELECT count(distinct lease_holder) from [show ranges from table t with details]",
+		).Scan(&count)
+		if count == 1 {
+			return nil
+		}
+		return errors.New("waiting for all partition leases to move to the primary region")
+	})
+}
