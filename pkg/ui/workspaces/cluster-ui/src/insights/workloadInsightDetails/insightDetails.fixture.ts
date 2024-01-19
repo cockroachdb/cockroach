733fbf30d3e76b07ae30573c9755dd4236aadc5e
Andrew Kimball <andyk@cockroachlabs.com>
2024-01-18 20:14:02 -0800

    cluster-ui: enable Explain Plan for tenant clusters
    Previously, Explain Plan tabs were not available for tenant clusters.
    This commit enables them, as there is no technical reason to disable
    them at this point. The commit also adds some simple tests for
    components that are being modified, as there were no tests before,
    making it harder to verify the fixes. It also converts an existing
    spec to use the React Testing Library rather than Enzyme, as the RTL
    is the preferred testing library going forward.
    
    Part of: https://github.com/cockroachdb/cockroach/issues/83422
    
    Release note: The Explain Plan tab is now shown for statements and
    insight pages, for tenant clusters.

---
+++ pkg/ui/workspaces/cluster-ui/src/insights/workloadInsightDetails/insightDetails.fixture.ts
@@ -0,0 +1,101 @@
+// Copyright 202 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import moment from "moment-timezone";
+import { createMemoryHistory } from "history";
+import { noop } from "lodash";
+import { StatementInsightDetailsProps } from "./statementInsightDetails";
+import {
+  InsightEventBase,
+  InsightExecEnum,
+  InsightNameEnum,
+  StatementStatus,
+  StmtInsightEvent,
+} from "../types";
+
+const insightEventBaseFixture: InsightEventBase = {
+  application: "app",
+  cpuSQLNanos: 25000000000,
+  elapsedTimeMillis: 127.0,
+  endTime: moment.utc("2021-12-10T05:06:07"),
+  implicitTxn: true,
+  insights: [
+    {
+      name: InsightNameEnum.slowExecution,
+      label: "label",
+      description: "slow query",
+      tooltipDescription: "a really slow query",
+    },
+    {
+      name: InsightNameEnum.suboptimalPlan,
+      label: "label2",
+      description: "bad plan",
+      tooltipDescription: "a really bad plan",
+    },
+  ],
+  priority: "high",
+  query: "SELECT count(*) FROM foo",
+  retries: 0,
+  rowsRead: 64500,
+  rowsWritten: 0,
+  sessionID: "123456789",
+  startTime: moment.utc("2021-12-08T15:19:42"),
+  transactionExecutionID: "87123cf6-13ea-40a8-9257-48b00b2af4cc",
+  transactionFingerprintID: "8a239b0afd5ebd3a",
+  username: "bob",
+  errorCode: "",
+  errorMsg: "",
+};
+
+const insightEventFixture: StmtInsightEvent = Object.assign(
+  insightEventBaseFixture,
+  {
+    statementExecutionID: "17a8d80bd38900b80000000000000005",
+    statementFingerprintID: "254026467b5f0ae5",
+    isFullScan: true,
+    indexRecommendations: [],
+    planGist: "AgGA////nxkAAAYAAAADBQIGAg==",
+    databaseName: "defaultdb",
+    execType: InsightExecEnum.STATEMENT,
+    status: StatementStatus.COMPLETED,
+  },
+);
+
+export const getStatementInsightPropsFixture =
+  (): StatementInsightDetailsProps => {
+    const history = createMemoryHistory({ initialEntries: ["/insights"] });
+    return {
+      history,
+      location: {
+        pathname: "/insights",
+        search: "",
+        hash: "",
+        state: null,
+      },
+      match: {
+        path: "/insights",
+        url: "/insights",
+        isExact: true,
+        params: {},
+      },
+      timeScale: {
+        windowSize: moment.duration(5, "day"),
+        sampleSize: moment.duration(5, "minutes"),
+        fixedWindowEnd: moment.utc("2021-12-12"),
+        key: "Custom",
+      },
+      insightEventDetails: insightEventFixture,
+      insightError: null,
+      hasAdminRole: true,
+      useObsService: false,
+      setTimeScale: noop,
+      refreshUserSQLRoles: noop,
+    };
+  };
