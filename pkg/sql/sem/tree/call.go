51544f09fbd657bb66b4d5f106c71a12b73295c5
Drew Kimball <drewk@cockroachlabs.com>
2024-01-19 20:51:12 -0700

    sql: add AOST clause to CALL statement
    It is now possible to specify an `AS OF SYSTEM TIME` clause for a `CALL`
    statement as in the following example:
    ```
    CALL f(1, 'foo') AS OF SYSTEM TIME '-1s';
    ```
    As with other cases that allow AOST, this syntax is incompatible with
    mutations, so it can only be used with read-only stored procedures.
    In addition, this syntax is only allowed in an explicit transaction if
    it matches with the transaction's timestamp.
    
    NOTE: The AOST clause applies not only to the body statements within the
    stored proc - it also applies to resolution of the proc itself. This means
    that running the above example will fail with an "unknown procedure" error
    if the proc was created less than one second ago.
    
    Epic: CRDB-34165
    
    Release note (sql change): Introduced an `AS OF SYSTEM TIME` clause to
    the `CALL` statement like the following: `CALL f() AS OF SYSTEM TIME '-1s';`.
    NOTE: AOST applies not only to the stored procedure's body statements, but
    also to resolution of the stored procedure itself. If the supplied timestamp
    is earlier than the SP's creation time, the SP will not be found.

--- pkg/sql/sem/tree/call.go
+++ pkg/sql/sem/tree/call.go
@@ -14,12 +14,19 @@ package tree
 type Call struct {
 	// Proc contains the procedure reference and the arguments of the procedure.
 	Proc *FuncExpr
+
+	// AsOf is an optional AS OF SYSTEM TIME clause.
+	AsOf AsOfClause
 }
 
 // Format implements the NodeFormatter interface.
 func (node *Call) Format(ctx *FmtCtx) {
 	ctx.WriteString("CALL ")
 	ctx.FormatNode(node.Proc)
+	if node.AsOf.Expr != nil {
+		ctx.WriteByte(' ')
+		ctx.FormatNode(&node.AsOf)
+	}
 }
 
 var _ Statement = &Call{}
