401ef27c3413b4b4f164193de750cdabc77074b5
zyf123123 <53106468+zyf123123@users.noreply.github.com>
2024-01-21 22:28:43 +0800

    change for lint

--- pkg/sql/create_role_test.go
+++ pkg/sql/create_role_test.go
@@ -1,16 +1,26 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
 package sql
 
 import (
 	"context"
+	"testing"
+
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
-	"testing"
 )
 
+// Test for create role infinity. #116714
 func TestCreateRoleInfinity(t *testing.T) {
-	// Test for create role infinity. #116714
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 
