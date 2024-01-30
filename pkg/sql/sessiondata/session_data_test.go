1a35b02010a9a9bde43308e2abdb8468073d275a
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-01-29 19:11:24 -0800

    sql: introduce `crdb_internal.execute_internally` builtin
    This commit introduces a new undocumented generator builtin
    `crdb_internal.execute_internally` which executes the provided string
    argument (which can be almost any single query) via the internal
    executor and then converts each resulting row into a string that is
    returned. This seems like a useful thing to have in general, but in
    particular when trying to understand the plans used by the internal
    queries.
    
    Additionally, this commit provides several overloads of this new
    builtin:
    - an optional boolean argument 'session_bound' indicates whether the
    query execution should happen via the IE that is bound to the session
    invoking it or not (if not, then it'll be "jobs-like" execution that
    creates a fresh session data - the only exception is that the user of
    this fresh session data is overridden to the session's user)
    - an optional string argument 'overrides' which specifies
    comma-separated list of session overrides, e.g.
    `'Database=foo,OptimizerUseHistograms=true'`.  These overrides apply
    as the very last step when initializing the session data for the
    internal executor and could allow us reproduce queries from
    different contexts. Note that these overrides are applied on a
    best-effort basis and mostly don't support custom types in the
    protobuf messages.
    - an optional boolean argument 'use_session_txn' which specifies that
    the session's txn must be used when 'session_bound' is false. Usage of
    this parameter requires that 'overrides' parameter is also specified (in
    order to distinguish this boolean from the other one). Note that an
    error is returned whenever using the session-bound executor because
    there we always use the session's txn, so this parameter would be
    redundant and probably confusing (like can it be run as a nested txn?).
    
    The builtin is undocumented, so there is no release note. I envision
    that only CRDB people should use this builtin. The only requirement
    imposed on it is that the user is the admin. The query gets executed
    under the session's user, so this should be ok from the security's point
    of view. Also some statements that modify txn state are prohibited.
    
    Release note: None

--- pkg/sql/sessiondata/session_data_test.go
+++ pkg/sql/sessiondata/session_data_test.go
@@ -11,6 +11,7 @@
 package sessiondata
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/sql/sessiondatapb"
@@ -82,4 +83,56 @@ func TestStack(t *testing.T) {
 	copiedElem.Database = "some other value"
 	require.Equal(t, s.Elems(), []*SessionData{initialElem, secondElem, copiedElem})
 	require.Equal(t, s.Base(), initialElem)
+}
+
+func TestUpdateSessionData(t *testing.T) {
+	var sd SessionData
+	unchangedLocal := sd.LocalOnlySessionData.String()
+	unchangedRemote := sd.SessionData.String()
+	for _, tc := range []struct {
+		variable  string
+		value     string
+		localOnly bool
+		// If set, then the update must have succeeded.
+		expectedSubstring string
+	}{
+		// string type.
+		{variable: "Database", value: "foo", localOnly: false, expectedSubstring: `database:"foo"`},
+		// This variable uses string type under the hood but has the custom
+		// cast type which isn't handled, so the update should be a no-op.
+		{variable: "UserProto", value: "foo", localOnly: false},
+		// This variable has a custom type that isn't handled, so the update
+		// should be a no-op.
+		{variable: "DataConversionConfig", value: "foo", localOnly: false},
+		// This variable has a custom type that is handled.
+		{variable: "VectorizeMode", value: "on", localOnly: false, expectedSubstring: `vectorize_mode:on`},
+		// duration type.
+		{variable: "LockTimeout", value: "42s", localOnly: false, expectedSubstring: `lock_timeout:<seconds:42 >`},
+		// This variable has a custom name, int64 type.
+		{variable: "OptimizerFKCascadesLimit", value: "123", localOnly: true, expectedSubstring: `optimizer_fk_cascades_limit:123`},
+		// bool type.
+		{variable: "DefaultTxnReadOnly", value: "true", localOnly: true, expectedSubstring: `default_txn_read_only:true`},
+		// float64 type.
+		{variable: "LargeFullScanRows", value: "12.34", localOnly: true, expectedSubstring: `large_full_scan_rows:12.34`},
+		// int32 type.
+		{variable: "OptSplitScanLimit", value: "123", localOnly: true, expectedSubstring: `opt_split_scan_limit:123`},
+	} {
+		sd = SessionData{}
+		ok := sd.Update(tc.variable, tc.value)
+		local, remote := sd.LocalOnlySessionData.String(), sd.SessionData.String()
+		if tc.expectedSubstring != "" {
+			require.True(t, ok)
+			if tc.localOnly {
+				require.True(t, strings.Contains(local, tc.expectedSubstring))
+				require.Equal(t, unchangedRemote, remote)
+			} else {
+				require.True(t, strings.Contains(remote, tc.expectedSubstring))
+				require.Equal(t, unchangedLocal, local)
+			}
+		} else {
+			require.False(t, ok)
+			require.Equal(t, unchangedLocal, local)
+			require.Equal(t, unchangedRemote, remote)
+		}
+	}
 }
