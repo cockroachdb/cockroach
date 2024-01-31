32f5a923fae1263c1f10fcab2d9cda9f70f2a4bd
Michael Erickson <michae2@cockroachlabs.com>
2024-01-30 16:56:42 -0800

    sql: rename checkExpr to checkExprForDistSQL
    This function checks whether an expression supports distributed
    execution. The name was a little ambiguous, especially in the giant
    sql package.
    
    Epic: None
    Release note: None

--- pkg/sql/distsql_physical_planner.go
+++ pkg/sql/distsql_physical_planner.go
@@ -385,9 +385,9 @@ func hasOidType(t *types.T) bool {
 	return false
 }
 
-// checkExpr verifies that an expression doesn't contain things that are not yet
-// supported by distSQL, like distSQL-blocklisted functions.
-func checkExpr(expr tree.Expr) error {
+// checkExprForDistSQL verifies that an expression doesn't contain things that
+// are not yet supported by distSQL, like distSQL-blocklisted functions.
+func checkExprForDistSQL(expr tree.Expr) error {
 	if expr == nil {
 		return nil
 	}
@@ -521,7 +521,7 @@ func checkSupportForPlanNode(node planNode) (distRecommendation, error) {
 		return checkSupportForPlanNode(n.source)
 
 	case *filterNode:
-		if err := checkExpr(n.filter); err != nil {
+		if err := checkExprForDistSQL(n.filter); err != nil {
 			return cannotDistribute, err
 		}
 		return checkSupportForPlanNode(n.source.plan)
@@ -560,7 +560,7 @@ func checkSupportForPlanNode(node planNode) (distRecommendation, error) {
 			// TODO(nvanbenschoten): lift this restriction.
 			return cannotDistribute, cannotDistributeRowLevelLockingErr
 		}
-		if err := checkExpr(n.onExpr); err != nil {
+		if err := checkExprForDistSQL(n.onExpr); err != nil {
 			return cannotDistribute, err
 		}
 		rec, err := checkSupportForPlanNode(n.input)
@@ -570,7 +570,7 @@ func checkSupportForPlanNode(node planNode) (distRecommendation, error) {
 		return rec.compose(shouldDistribute), nil
 
 	case *joinNode:
-		if err := checkExpr(n.pred.onCond); err != nil {
+		if err := checkExprForDistSQL(n.pred.onCond); err != nil {
 			return cannotDistribute, err
 		}
 		recLeft, err := checkSupportForPlanNode(n.left.plan)
@@ -609,13 +609,13 @@ func checkSupportForPlanNode(node planNode) (distRecommendation, error) {
 			return cannotDistribute, cannotDistributeRowLevelLockingErr
 		}
 
-		if err := checkExpr(n.lookupExpr); err != nil {
+		if err := checkExprForDistSQL(n.lookupExpr); err != nil {
 			return cannotDistribute, err
 		}
-		if err := checkExpr(n.remoteLookupExpr); err != nil {
+		if err := checkExprForDistSQL(n.remoteLookupExpr); err != nil {
 			return cannotDistribute, err
 		}
-		if err := checkExpr(n.onCond); err != nil {
+		if err := checkExprForDistSQL(n.onCond); err != nil {
 			return cannotDistribute, err
 		}
 		rec, err := checkSupportForPlanNode(n.input)
@@ -631,7 +631,7 @@ func checkSupportForPlanNode(node planNode) (distRecommendation, error) {
 
 	case *projectSetNode:
 		for i := range n.exprs {
-			if err := checkExpr(n.exprs[i]); err != nil {
+			if err := checkExprForDistSQL(n.exprs[i]); err != nil {
 				return cannotDistribute, err
 			}
 		}
@@ -639,7 +639,7 @@ func checkSupportForPlanNode(node planNode) (distRecommendation, error) {
 
 	case *renderNode:
 		for _, e := range n.render {
-			if err := checkExpr(e); err != nil {
+			if err := checkExprForDistSQL(e); err != nil {
 				return cannotDistribute, err
 			}
 		}
@@ -710,7 +710,7 @@ func checkSupportForPlanNode(node planNode) (distRecommendation, error) {
 
 		for _, tuple := range n.tuples {
 			for _, expr := range tuple {
-				if err := checkExpr(expr); err != nil {
+				if err := checkExprForDistSQL(expr); err != nil {
 					return cannotDistribute, err
 				}
 			}
@@ -744,7 +744,7 @@ func checkSupportForPlanNode(node planNode) (distRecommendation, error) {
 				return cannotDistribute, cannotDistributeRowLevelLockingErr
 			}
 		}
-		if err := checkExpr(n.onCond); err != nil {
+		if err := checkExprForDistSQL(n.onCond); err != nil {
 			return cannotDistribute, err
 		}
 		return shouldDistribute, nil
