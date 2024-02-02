b73f0a5d8016b49174c3addbd78e71bd59f72f3c
Marcus Gartner <marcus@cockroachlabs.com>
2024-02-02 16:57:35 -0500

    schemaexpr: refactor NameResolutionVisitor
    The `NameResolutionVisitor` is not used outside the `schemaexpr`
    package, so it has been unexported. Additionally, it no longer attempts
    to rebind indexed vars or normalized function names. Neither of these
    steps are necessary because this struct is only used to resolved column
    names to indexed vars for computed column and partial index expressions.
    
    Release note: None

--- pkg/sql/catalog/schemaexpr/select_name_resolution.go
+++ pkg/sql/catalog/schemaexpr/select_name_resolution.go
@@ -20,33 +20,23 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 )
 
-// NameResolutionVisitor is a tree.Visitor implementation used to
-// resolve the column names in an expression.
-type NameResolutionVisitor struct {
+// nameResolutionVisitor is a tree.Visitor implementation used to resolve column
+// names in an expression as indexed vars.
+type nameResolutionVisitor struct {
 	err        error
 	iVarHelper tree.IndexedVarHelper
 	resolver   colinfo.ColumnResolver
 }
 
-var _ tree.Visitor = &NameResolutionVisitor{}
+var _ tree.Visitor = &nameResolutionVisitor{}
 
 // VisitPre implements tree.Visitor.
-func (v *NameResolutionVisitor) VisitPre(expr tree.Expr) (recurse bool, newNode tree.Expr) {
+func (v *nameResolutionVisitor) VisitPre(expr tree.Expr) (recurse bool, newNode tree.Expr) {
 	if v.err != nil {
 		return false, expr
 	}
 
 	switch t := expr.(type) {
-	case *tree.IndexedVar:
-		// If the indexed var is a standalone ordinal reference, ensure it
-		// becomes a fully bound indexed var.
-		t, v.err = v.iVarHelper.BindIfUnbound(t)
-		if v.err != nil {
-			return false, expr
-		}
-
-		return false, t
-
 	case *tree.UnresolvedName:
 		vn, err := t.NormalizeVarName()
 		if err != nil {
@@ -56,7 +46,7 @@ func (v *NameResolutionVisitor) VisitPre(expr tree.Expr) (recurse bool, newNode 
 		return v.VisitPre(vn)
 
 	case *tree.ColumnItem:
-		_, err := colinfo.ResolveColumnItem(context.TODO(), &v.resolver, t)
+		_, err := colinfo.ResolveColumnItem(context.Background(), &v.resolver, t)
 		if err != nil {
 			v.err = err
 			return false, expr
@@ -65,50 +55,9 @@ func (v *NameResolutionVisitor) VisitPre(expr tree.Expr) (recurse bool, newNode 
 		colIdx := v.resolver.ResolverState.ColIdx
 		ivar := v.iVarHelper.IndexedVar(colIdx)
 		return true, ivar
-
-	case *tree.FuncExpr:
-		// Check for invalid use of *, which, if it is an argument, is the only argument.
-		if len(t.Exprs) != 1 {
-			break
-		}
-		vn, ok := t.Exprs[0].(tree.VarName)
-		if !ok {
-			break
-		}
-		vn, v.err = vn.NormalizeVarName()
-		if v.err != nil {
-			return false, expr
-		}
-		// Save back to avoid re-doing the work later.
-		t.Exprs[0] = vn
-		return true, t
 	}
-
 	return true, expr
 }
 
 // VisitPost implements tree.Visitor.
-func (*NameResolutionVisitor) VisitPost(expr tree.Expr) tree.Expr { return expr }
-
-// ResolveNamesUsingVisitor resolves the names in the given expression.
-func ResolveNamesUsingVisitor(
-	v *NameResolutionVisitor,
-	expr tree.Expr,
-	source *colinfo.DataSourceInfo,
-	ivarHelper tree.IndexedVarHelper,
-) (tree.Expr, error) {
-	*v = NameResolutionVisitor{
-		iVarHelper: ivarHelper,
-		resolver: colinfo.ColumnResolver{
-			Source: source,
-		},
-	}
-
-	expr, _ = tree.WalkExpr(v, expr)
-	return expr, v.err
-}
-
-// Err returns visitor error.
-func (v *NameResolutionVisitor) Err() error {
-	return v.err
-}
+func (*nameResolutionVisitor) VisitPost(expr tree.Expr) tree.Expr { return expr }
