30a517dc8a95b5da21ba27ac8267c2d9008e1947
Steven Danna <danna@cockroachlabs.com>
2024-02-02 12:08:46 +0000

    jobs: add job_id index to crdb_internal.jobs virtual table
    To produce rows, `crdb_internal.jobs` queries
    `crdb_internal.system_jobs`. The latter is very expensive in the
    abscence of a filter on job ID because it does 2 full scans of the
    job_info table.
    
    This is a backportable change that adds a job_id index to the
    crdb_internal.jobs virtual table, allowing it to produce a query to
    crdb_internal.system_jobs that takes advantage of system_job's index
    on id.
    
    We should pursue a more complete fix, perhaps by changing these
    virtual tables to virtual views, allowing the optimizer to solve many
    of these problems for us. For example, this change does not
    substantially improve `SHOW CHANGEFEED JOB <id>` without a follow up
    commit.
    
    Before this change, `SHOW JOB 101` on a fresh demo cluster produced
    the following scans:
    
    ```
    Scan /Tenant/2/Table/15/1/10{1-2}, /Tenant/2/Table/15/1/10{2-3}, /Tenant/2/Table/15/1/10{3-4}, /Tenant/2/Table/15/1/10{4-5}, /Tenant/2/Table/15/1/9397966460656353{29-30}, /Tenant/2/Table/15/1/93979664607451545{7-8}, /Tenant/2/Table/15/1/93979664613556224{1-2}, /Tenant/2/Table/15/1/93979664625333043{3-4}, /Tenant/2/Table/15/1/93979664629252096{1-2}, /Tenant/2/Table/15/1/93979664633554534{5-6}, /Tenant/2/Table/15/1/93979664639180800{1-2}, /Tenant/2/Table/15/1/93979664643502899{3-4}, /Tenant/2/Table/15/1/93979664647874150{5-6}, /Tenant/2/Table/15/1/93979664652251955{3-4}, /Tenant/2/Table/15/1/93979664656652697{7-8}, /Tenant/2/Table/15/1/9397966468795924{49-50}, /Tenant/2/Table/15/1/93979664688050995{3-4}, /Tenant/2/Table/15/1/93979664688146022{5-6}, /Tenant/2/Table/15/1/93979664694434201{7-8}, /Tenant/2/Table/15/1/93979664816006758{5-6}, /Tenant/2/Table/15/1/93979664825014681{7-8}, /Tenant/2/Table/15/1/93979664834094694{5-6}, /Tenant/2/Table/15/1/93979664842948608{1-2}, /Tenant/2/Table/15/1/93979664852251443{3-4}
    Scan /Tenant/2/Table/53/{1-2}
    Scan /Tenant/2/Table/53/{1-2}
    ```
    
    Afer, it produces
    
    ```
    Scan /Tenant/2/Table/15/1/10{1-2}
    Scan /Tenant/2/Table/53/1/101/"legacy_payload"{-/PrefixEnd}
    Scan /Tenant/2/Table/53/1/101/"legacy_progress"{-/PrefixEnd}
    ```
    
    ```
    > benchstat old.txt new.txt
    goos: darwin
    goarch: arm64
                        │    old.txt    │               new.txt                │
                        │    sec/op     │    sec/op     vs base                │
    ShowJob/SHOW_JOB-10   185.658m ± 4%   6.306m ± 13%  -96.60% (p=0.000 n=10)
    ```
    
    Release note: None
    
    Epic: None

--- pkg/sql/crdb_internal.go
+++ pkg/sql/crdb_internal.go
@@ -922,7 +922,7 @@ const (
 WITH
 	latestpayload AS (SELECT job_id, value FROM system.job_info AS payload WHERE info_key = 'legacy_payload' ORDER BY written DESC),
 	latestprogress AS (SELECT job_id, value FROM system.job_info AS progress WHERE info_key = 'legacy_progress' ORDER BY written DESC)
-	SELECT 
+	SELECT
 		DISTINCT(id), status, created, payload.value AS payload, progress.value AS progress,
 		created_by_type, created_by_id, claim_session_id, claim_instance_id, num_runs, last_run, job_type
 	FROM system.jobs AS j
@@ -1135,6 +1135,7 @@ const (
 	// user is not allowed to see.
 	jobsQFrom         = ` FROM crdb_internal.system_jobs`
 	jobsBackoffArgs   = `(SELECT $1::FLOAT AS initial_delay, $2::FLOAT AS max_delay) args`
+	jobIDFilter       = ` WHERE id = $3`
 	jobsStatusFilter  = ` WHERE status = $3`
 	oldJobsTypeFilter = ` WHERE crdb_internal.job_payload_type(payload) = $3`
 	jobsTypeFilter    = ` WHERE job_type = $3`
@@ -1175,11 +1176,18 @@ CREATE TABLE crdb_internal.jobs (
   num_runs              INT,
   execution_errors      STRING[],
   execution_events      JSONB,
+  INDEX(job_id),
   INDEX(status),
   INDEX(job_type)
 )`,
 	comment: `decoded job metadata from crdb_internal.system_jobs (KV scan)`,
 	indexes: []virtualIndex{{
+		populate: func(ctx context.Context, unwrappedConstraint tree.Datum, p *planner, _ catalog.DatabaseDescriptor, addRow func(...tree.Datum) error) (matched bool, err error) {
+			q := jobsQuery + jobIDFilter
+			targetID := tree.MustBeDInt(unwrappedConstraint)
+			return makeJobsTableRows(ctx, p, addRow, q, p.execCfg.JobRegistry.RetryInitialDelay(), p.execCfg.JobRegistry.RetryMaxDelay(), targetID)
+		},
+	}, {
 		populate: func(ctx context.Context, unwrappedConstraint tree.Datum, p *planner, _ catalog.DatabaseDescriptor, addRow func(...tree.Datum) error) (matched bool, err error) {
 			q := jobsQuery + jobsStatusFilter
 			targetStatus := tree.MustBeDString(unwrappedConstraint)
@@ -1188,8 +1196,8 @@ CREATE TABLE crdb_internal.jobs (
 	}, {
 		populate: func(ctx context.Context, unwrappedConstraint tree.Datum, p *planner, _ catalog.DatabaseDescriptor, addRow func(...tree.Datum) error) (matched bool, err error) {
 			q := jobsQuery + getCRDBInternalJobsTableTypeFilter(ctx, p.execCfg.Settings.Version)
-			targetStatus := tree.MustBeDString(unwrappedConstraint)
-			return makeJobsTableRows(ctx, p, addRow, q, p.execCfg.JobRegistry.RetryInitialDelay(), p.execCfg.JobRegistry.RetryMaxDelay(), targetStatus)
+			targetType := tree.MustBeDString(unwrappedConstraint)
+			return makeJobsTableRows(ctx, p, addRow, q, p.execCfg.JobRegistry.RetryInitialDelay(), p.execCfg.JobRegistry.RetryMaxDelay(), targetType)
 		},
 	}},
 	populate: func(ctx context.Context, p *planner, db catalog.DatabaseDescriptor, addRow func(...tree.Datum) error) error {
@@ -6734,7 +6742,7 @@ CREATE VIEW crdb_internal.statement_activity AS
 				contention_time_avg_seconds,
 				cpu_sql_avg_nanos,
 				service_latency_avg_seconds,
-				service_latency_p99_seconds 
+				service_latency_p99_seconds
       FROM
           system.statement_activity`,
 	resultColumns: colinfo.ResultColumns{
@@ -7210,10 +7218,10 @@ CREATE TABLE crdb_internal.transaction_contention_events (
     contention_duration          INTERVAL NOT NULL,
     contending_key               BYTES NOT NULL,
     contending_pretty_key     	 STRING NOT NULL,
-		    
+
     waiting_stmt_id              string NOT NULL,
     waiting_stmt_fingerprint_id  BYTES NOT NULL,
-    
+
     database_name       				 STRING NOT NULL,
     schema_name         				 STRING NOT NULL,
     table_name          				 STRING NOT NULL,
