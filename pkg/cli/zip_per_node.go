cockroachdb/cockroach

commit da0e542b3a0504f84fc092724bfe4631ca748e94

server, cli: make debug zip tolerate corrupted log entries
This change changes the way log file decoder handles corrupted
log strings. Before, decoder would panic, recover and propagate errors
during parsing back to status server and then status server would stop
decoding remaining records and returns error only.
It doesn't allow to get at least some logs that might be important as well.

Now, decoder doesn't panic during failed string parsing and returns this
error which is then added to Errors slice as part of the `LogEntriesResponse`
and status server continues decoding remaining records.

Release note (bug fix): debug zip doesn't fail on corrupted log files.

pkg/cli/zip_per_node.go
@@ -16,6 +16,7 @@ import (
 	"net/url"
 	"os"
 	"sort"
+	"strings"
 	"sync"
 	"time"
 
@@ -471,6 +472,17 @@ func (zc *debugZipContext) collectPerNodeData(
 				}); requestErr != nil {
 				if err := zc.z.createError(sf, name, requestErr); err != nil {
 					return err
+				}
+				// Log out the list of errors that occurred during log entries request.
+				if len(entries.ParseErrors) > 0 {
+					sf.shout("%d parsing errors occurred:", len(entries.ParseErrors))
+					for _, err := range entries.ParseErrors {
+						sf.shout("%s", err)
+					}
+					parseErr := fmt.Errorf("%d errors occurred:\n%s", len(entries.ParseErrors), strings.Join(entries.ParseErrors, "\n"))
+					if err := zc.z.createError(sf, name, parseErr); err != nil {
+						return err
+					}
 				}
 				continue
 			}

pkg/server/serverpb/status.proto
@@ -668,6 +668,9 @@ message LogsRequest {
 message LogEntriesResponse {
   repeated cockroach.util.log.Entry entries = 1
       [ (gogoproto.nullable) = false ];
+  // parse_errors contains list of errors that occurred during retrieving individual log entries
+  // that don't prevent to return at least partial response.
+  repeated string parse_errors = 2;
 }
 
 message LogFilesListRequest {

pkg/server/status.go
@@ -1315,6 +1315,12 @@ func (s *statusServer) LogFile(
 			if err == io.EOF {
 				break
 			}
+			if errors.Is(err, log.ErrMalformedLogEntry) {
+				resp.ParseErrors = append(resp.ParseErrors, err.Error())
+				// Proceed decoding next entry, as we want to retrieve as much logs
+				// as possible.
+				continue
+			}
 			return nil, srverrors.ServerError(ctx, err)
 		}
 		if tenantIDFilter != "" && entry.TenantID != tenantIDFilter {

pkg/server/storage_api/logfiles_test.go
@@ -14,6 +14,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"os"
 	"strings"
 	"testing"
 	"time"
@@ -426,4 +427,51 @@ func TestStatusLogRedaction(t *testing.T) {
 					})
 			}
 		})
+}
+
+// TestStatusLogCorruptedEntry checks that RPC returns partial result with log
+// entries if log file is corrupted and includes occurred errors.
+func TestStatusLogCorruptedEntry(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	if log.V(3) {
+		skip.IgnoreLint(t, "Test only works with low verbosity levels")
+	}
+
+	s := log.ScopeWithoutShowLogs(t)
+	defer s.Close(t)
+
+	// This test cares about the number of output files. Ensure
+	// there's just one.
+	defer s.SetupSingleFileLogging()()
+
+	srv := serverutils.StartServerOnly(t, base.TestServerArgs{})
+	defer srv.Stopper().Stop(context.Background())
+	ts := srv.ApplicationLayer()
+
+	logCtx := ts.AnnotateCtx(context.Background())
+
+	log.Errorf(logCtx, "TestStatusLogCorruptedEntry test message")
+	log.FlushFiles()
+
+	files, err := log.ListLogFiles()
+	require.NoError(t, err)
+	require.Equal(t, len(files), 1)
+	file := files[0]
+	f, err := os.OpenFile(fmt.Sprintf("%s%s%s", s.GetDirectory(), string(os.PathSeparator), file.Name), os.O_APPEND|os.O_WRONLY, 0600)
+	require.NoError(t, err)
+	// Insert empty lines into log file to simulate corrupted rows that cannot be
+	// parsed. And then append random log.
+	_, err = f.WriteString("\n\n")
+	require.NoError(t, err)
+	err = f.Close()
+	require.NoError(t, err)
+	log.Errorf(logCtx, "TestStatusLogCorruptedEntry test message 2")
+	log.FlushFiles()
+
+	var wrapper serverpb.LogEntriesResponse
+	err = srvtestutils.GetStatusJSONProto(ts, "logfiles/local/"+file.Name, &wrapper)
+	require.NoError(t, err)
+	require.NotEmpty(t, wrapper.Entries)
+	require.NotEmpty(t, wrapper.ParseErrors)
+	require.Equal(t, len(wrapper.ParseErrors), 1)
 }

pkg/util/log/format_crdb_v2.go
@@ -504,20 +504,16 @@ type entryDecoderV2 struct {
 // Decode decodes the next log entry into the provided protobuf message.
 func (d *entryDecoderV2) Decode(entry *logpb.Entry) (err error) {
 	defer func() {
-		switch r := recover().(type) {
-		case nil: // do nothing
-		case error:
-			err = errors.Wrapf(r, "decoding on line %d", d.lines)
-		default:
-			panic(r)
+		if err != nil && !errors.Is(err, io.EOF) {
+			err = errors.Wrapf(err, "decoding on line %d", d.lines)
 		}
 	}()
-	frag, atEOF := d.peekNextFragment()
-	if atEOF {
-		return io.EOF
+	frag, err := d.peekNextFragment()
+	if err != nil {
+		return err
 	}
 	d.popFragment()
-	if err := d.initEntryFromFirstLine(entry, frag); err != nil {
+	if err = d.initEntryFromFirstLine(entry, frag); err != nil {
 		return err
 	}
 
@@ -527,12 +523,16 @@ func (d *entryDecoderV2) Decode(entry *logpb.Entry) (err error) {
 
 	// While the entry has additional lines, collect the full message.
 	for {
-		frag, atEOF := d.peekNextFragment()
-		if atEOF || !frag.isContinuation() {
+		frag, err = d.peekNextFragment()
+		if err != nil || !frag.isContinuation() {
+			// Ignore this error as it is relevant to the next line and we don't
+			// know if it is continuation line or not.
 			break
 		}
 		d.popFragment()
-		d.addContinuationFragmentToEntry(entry, &entryMsg, frag)
+		if err = d.addContinuationFragmentToEntry(entry, &entryMsg, frag); err != nil {
+			return err
+		}
 	}
 
 	r := redactablePackage{
@@ -548,7 +548,7 @@ func (d *entryDecoderV2) Decode(entry *logpb.Entry) (err error) {
 
 func (d *entryDecoderV2) addContinuationFragmentToEntry(
 	entry *logpb.Entry, entryMsg *bytes.Buffer, frag entryDecoderV2Fragment,
-) {
+) error {
 	switch frag.getContinuation() {
 	case '+':
 		entryMsg.WriteByte('\n')
@@ -568,25 +568,26 @@ func (d *entryDecoderV2) addContinuationFragmentToEntry(
 			entryMsg.Write(frag.getMsg())
 		}
 	default:
-		panic(errors.Errorf("unexpected continuation character %c", frag.getContinuation()))
+		return errors.Wrapf(ErrMalformedLogEntry, "unexpected continuation character %c", frag.getContinuation())
 	}
+	return nil
 }
 
 // peekNextFragment populates the nextFragment buffer by reading from the
 // underlying reader a line at a time until a valid line is reached.
-// It will panic if a malformed log line is discovered. It permits the first
+// It returns error if malformed log line is discovered. It permits the first
 // line in the decoder to be malformed and it will skip that line. Upon EOF,
 // if there is no text left to consume, the atEOF return value will be true.
-func (d *entryDecoderV2) peekNextFragment() (_ entryDecoderV2Fragment, atEOF bool) {
+func (d *entryDecoderV2) peekNextFragment() (entryDecoderV2Fragment, error) {
 	for d.nextFragment == nil {
 		d.lines++
 		nextLine, err := d.reader.ReadBytes('\n')
-		if isEOF := errors.Is(err, io.EOF); isEOF {
+		if errors.Is(err, io.EOF) {
 			if len(nextLine) == 0 {
-				return nil, true
+				return nil, err
 			}
 		} else if err != nil {
-			panic(err)
+			return nil, err
 		}
 		nextLine = bytes.TrimSuffix(nextLine, []byte{'\n'})
 		m := entryREV2.FindSubmatch(nextLine)
@@ -594,11 +595,11 @@ func (d *entryDecoderV2) peekNextFragment() (_ entryDecoderV2Fragment, atEOF boo
 			if d.lines == 1 { // allow non-matching lines if we've never seen a line
 				continue
 			}
-			panic(errors.New("malformed log entry"))
+			return nil, ErrMalformedLogEntry
 		}
 		d.nextFragment = m
 	}
-	return d.nextFragment, false
+	return d.nextFragment, nil
 }
 
 func (d *entryDecoderV2) popFragment() {

pkg/util/log/format_crdb_v2_test.go
@@ -25,6 +25,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/log/severity"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/datadriven"
+	"github.com/cockroachdb/errors"
 	"github.com/cockroachdb/logtags"
 	"github.com/kr/pretty"
 )
@@ -265,6 +266,9 @@ func TestCrdbV2Decode(t *testing.T) {
 					if err := d.Decode(&e); err != nil {
 						if err == io.EOF {
 							break
+						}
+						if errors.Is(err, ErrMalformedLogEntry) {
+							continue
 						}
 						td.Fatalf(t, "error while decoding: %v", err)
 					}

pkg/util/log/format_json.go
@@ -14,6 +14,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"sort"
 	"strconv"
 	"strings"
@@ -523,10 +524,10 @@ type jsonCommon struct {
 type JSONEntry struct {
 	jsonCommon
 
-	//Channel         Channel  `json:"channel,omitempty"`
+	// Channel         Channel  `json:"channel,omitempty"`
 	ChannelNumeric int64  `json:"channel_numeric,omitempty"`
 	Timestamp      string `json:"timestamp,omitempty"`
-	//Severity        Severity `json:"severity,omitempty"`
+	// Severity        Severity `json:"severity,omitempty"`
 	SeverityNumeric int64  `json:"severity_numeric,omitempty"`
 	Goroutine       int64  `json:"goroutine,omitempty"`
 	File            string `json:"file,omitempty"`
@@ -545,10 +546,10 @@ type JSONEntry struct {
 type JSONCompactEntry struct {
 	jsonCommon
 
-	//Channel         Channel  `json:"C,omitempty"`
+	// Channel         Channel  `json:"C,omitempty"`
 	ChannelNumeric int64  `json:"c,omitempty"`
 	Timestamp      string `json:"t,omitempty"`
-	//Severity        Severity `json:"sev,omitempty"`
+	// Severity        Severity `json:"sev,omitempty"`
 	SeverityNumeric int64  `json:"s,omitempty"`
 	Goroutine       int64  `json:"g,omitempty"`
 	File            string `json:"f,omitempty"`
@@ -652,12 +653,19 @@ func (e *JSONCompactEntry) toEntry(entry *JSONEntry) {
 }
 
 // Decode decodes the next log entry into the provided protobuf message.
-func (d *entryDecoderJSON) Decode(entry *logpb.Entry) error {
+func (d *entryDecoderJSON) Decode(entry *logpb.Entry) (err error) {
+	defer func() {
+		// Wrap all errors except EOF as a malformed entries to make it easier to
+		// handle this type of error later on.
+		if err != nil && err != io.EOF {
+			err = errors.CombineErrors(ErrMalformedLogEntry, err)
+		}
+	}()
 	var rp *redactablePackage
 	var e JSONEntry
 	if d.compact {
 		var compact JSONCompactEntry
-		err := d.decoder.Decode(&compact)
+		err = d.decoder.Decode(&compact)
 		if err != nil {
 			return err
 		}
@@ -668,7 +676,7 @@ func (d *entryDecoderJSON) Decode(entry *logpb.Entry) error {
 			return err
 		}
 	}
-	rp, err := e.populate(entry, d)
+	rp, err = e.populate(entry, d)
 	if err != nil {
 		return err
 	}

pkg/util/log/format_json_test.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/log/severity"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/datadriven"
+	"github.com/cockroachdb/errors"
 	"github.com/cockroachdb/logtags"
 	"github.com/kr/pretty"
 )
@@ -119,7 +120,7 @@ func TestJsonDecode(t *testing.T) {
 				for {
 					var e logpb.Entry
 					if err := d.Decode(&e); err != nil {
-						if err == io.EOF {
+						if err == io.EOF || errors.Is(err, ErrMalformedLogEntry) {
 							break
 						}
 						td.Fatalf(t, "error while decoding: %v", err)

commit eaea30ee27ea666988d064be187a62154f380ebe

cli: fix TestUnavailableZip failed
This patch fixes issue introduced in da0e542b3a0504f84fc092724bfe4631ca748e94
where code (that handles unparsed entries due to errors) was added in "if error" block
instead of below it. It was wrong because in case of timeout error, `entries` variable
is always nil.
Now logic that handles partially failed entries parsing moved outside and
invoked only if no timeout error happens.

Release note: None

pkg/cli/zip_per_node.go
@@ -472,18 +472,18 @@ func (zc *debugZipContext) collectPerNodeData(
 				if err := zc.z.createError(sf, name, requestErr); err != nil {
 					return err
 				}
-				// Log out the list of errors that occurred during log entries request.
-				if len(entries.ParseErrors) > 0 {
-					sf.shout("%d parsing errors occurred:", len(entries.ParseErrors))
-					for _, err := range entries.ParseErrors {
-						sf.shout("%s", err)
-					}
-					parseErr := fmt.Errorf("%d errors occurred:\n%s", len(entries.ParseErrors), strings.Join(entries.ParseErrors, "\n"))
-					if err := zc.z.createError(sf, name, parseErr); err != nil {
-						return err
-					}
-				}
 				continue
+			}
+			// Log the list of errors that occurred during log entries request.
+			if len(entries.ParseErrors) > 0 {
+				sf.shout("%d parsing errors occurred:", len(entries.ParseErrors))
+				for _, err := range entries.ParseErrors {
+					sf.shout("%s", err)
+				}
+				parseErr := fmt.Errorf("%d errors occurred:\n%s", len(entries.ParseErrors), strings.Join(entries.ParseErrors, "\n"))
+				if err := zc.z.createError(sf, name, parseErr); err != nil {
+					return err
+				}
 			}
 			sf.progress("writing output: %s", name)
 			warnRedactLeak := false