4a353ae7c452b814153dd89c261344707c6ffbe6
Jayant Shrivastava <jayants@cockroachlabs.com>
2024-02-05 14:03:29 -0500

    upgrades: migrate old-style PTS records
    This change adds an upgrade for 24.1 which migrates all old-style
    protected timestamp records. This is done through a migration
    which reads all PTS records which are protecting spans. These
    spans are decoded so all targeted table IDs can be targeted.
    Then, each record is updated to store targets instead of spans.
    
    Release note: None
    Epic: None
    Informs: #82888

---
+++ pkg/upgrade/upgrades/v24_1_migrate_pts_records.go
@@ -0,0 +1,117 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package upgrades
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
+	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/protectedts/ptpb"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/protectedts/ptstorage"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/isql"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
+	"github.com/cockroachdb/cockroach/pkg/upgrade"
+	"github.com/cockroachdb/cockroach/pkg/util/encoding"
+	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
+	"github.com/cockroachdb/errors"
+)
+
+func migrateOldStylePTSRecords(
+	ctx context.Context, cv clusterversion.ClusterVersion, deps upgrade.TenantDeps,
+) error {
+	// Read deprecated PTS records in one sweep and buffer them. Avoid using an iterator
+	// to reduce the lifetime of the transaction. This helps reduce contention on the table.
+	ids, err := deps.InternalExecutor.QueryBufferedEx(ctx, "pts-migration-read-ids", nil, /* txn */
+		sessiondata.NodeUserSessionDataOverride, `
+		SELECT id FROM system.protected_ts_records WHERE target IS NULL;
+	`)
+	if err != nil {
+		return err
+	}
+
+	for _, dID := range ids {
+		id := dID[0].(*tree.DUuid).UUID.GetBytes()
+		// Use a separate transaction for each record to avoid a long-running transaction.
+		err := deps.DB.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+			rows, err := txn.QueryBufferedEx(ctx, "pts-migration-read-record", txn.KV(), sessiondata.NodeUserSessionDataOverride,
+				"SELECT spans FROM system.protected_ts_records WHERE id = $1", id)
+			if err != nil {
+				return err
+			}
+			// The PTS record may have been deleted since the read transaction above.
+			if len(rows) == 0 {
+				return nil
+			}
+
+			row := rows[0]
+
+			var spans ptstorage.Spans
+			if err := protoutil.Unmarshal([]byte(*row[0].(*tree.DBytes)), &spans); err != nil {
+				return errors.Wrapf(err, "failed to unmarshal span for %v", id)
+			}
+
+			var tableIDs descpb.IDs
+			for _, sp := range spans.Spans {
+				var startKey, endkey roachpb.Key
+				var err error
+				startKey, _, err = keys.DecodeTenantPrefix(sp.Key)
+				if err != nil {
+					return err
+				}
+				_, startTableID, err := encoding.DecodeUvarintAscending(startKey)
+				if err != nil {
+					return err
+				}
+
+				if !sp.EndKey.Equal(sp.Key.PrefixEnd()) {
+					endkey, _, err = keys.DecodeTenantPrefix(sp.EndKey)
+					if err != nil {
+						return err
+					}
+					_, endTableID, err := encoding.DecodeUvarintAscending(endkey)
+					if err != nil {
+						return err
+					}
+					// Track all tables between the two keys.
+					for tID := startTableID; tID <= endTableID; tID += 1 {
+						tableIDs = append(tableIDs, descpb.ID(tID))
+					}
+				} else {
+					tableIDs = append(tableIDs, descpb.ID(startTableID))
+				}
+			}
+
+			encodedTarget, err := protoutil.Marshal(&ptpb.Target{
+				Union: ptpb.MakeSchemaObjectsTarget(tableIDs).GetUnion(),
+			})
+			if err != nil {
+				return err
+			}
+
+			_, err = txn.ExecEx(ctx, "pts-migration-write-record", txn.KV(), sessiondata.NodeUserSessionDataOverride,
+				"UPDATE system.protected_ts_records SET spans = $1, target = $2 WHERE id = $3", encodedTarget, encodedTarget, id)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
