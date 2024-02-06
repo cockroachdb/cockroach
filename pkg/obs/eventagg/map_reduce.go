fe8d17ddb93e549ac3b21bed2caf1462810511af
Alex Barganier <abarganier@cockroachlabs.com>
2024-02-06 12:11:31 -0400

    pkg/obs/eventagg: introduce the eventagg package, MapReduceAggregator
    The eventagg package is (currently) a proof of concept ("POC") that aims
    to provide an easy-to-use library that standardizes the way in which we
    aggregate Observability event data in CRDB. The goal is to eventually
    emit that data as "exhaust" from CRDB, which downstream systems can consume
    to build Observability features that do not rely on CRDB's own availability to
    aid in debugging & investigations.
    
    This commit contains the first bits of work to scaffold such a library.
    It focuses on creating the core building block of the eventagg package,
    which includes the Mergeable interface (used to define how to derive
    keys from, and aggregate events) and the MapReduceAggregator (a common
    library used to perform map/reduce-like aggregations).
    
    See pkg/obs/eventagg/doc.go for more details
    
    Since this feature is currently experimental, it's gated by the
    `COCKROACH_ENABLE_STRUCTURED_EVENTS` environment variable, which is
    disabled by default.
    
    Release note: none

---
+++ pkg/obs/eventagg/map_reduce.go
@@ -0,0 +1,86 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package eventagg
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+)
+
+// MapReduceAggregator performs map/reduce aggregations on type Agg, keyed by type K,
+// which are reduced into type Agg.
+//
+// It leverages the Mergeable interface to define the aggregation key and the reduce
+// logic.
+//
+// MapReduceAggregator can be configured with one or more flushConsumers,
+// which will be given a reference to the flushed data for further processing on
+// Flush.
+type MapReduceAggregator[E Mergeable[K, Agg], K comparable, Agg any] struct {
+	newFn func() Agg
+	mu    struct {
+		syncutil.Mutex
+		cache map[K]Agg
+	}
+	consumers []flushConsumer[K, Agg]
+}
+
+// NewMapReduceAggregator returns a new MapReduceAggregator[Agg].
+func NewMapReduceAggregator[E Mergeable[K, V], K comparable, V any](
+	newFn func() V, flushConsumers ...flushConsumer[K, V],
+) *MapReduceAggregator[E, K, V] {
+	m := &MapReduceAggregator[E, K, V]{
+		newFn:     newFn,
+		consumers: flushConsumers,
+	}
+	m.mu.cache = make(map[K]V)
+	return m
+}
+
+// Add implements the aggregator interface.
+func (m *MapReduceAggregator[E, K, Agg]) Add(_ context.Context, e E) {
+	if !envEnableStructuredEvents {
+		return
+	}
+	k := e.GroupingKey()
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	v, ok := m.mu.cache[k]
+	if !ok {
+		v = m.newFn()
+		m.mu.cache[k] = v
+	}
+	e.MergeInto(v)
+}
+
+// Flush triggers a flush of the in-memory aggregate data, which resets the
+// underlying cache for a new aggregation window.
+//
+// The flushed data will be passed to each of the configured flushConsumer's
+// provided at construction for further processing.
+// TODO(abarganier): implement more robust flush mechanism, with configurable triggers.
+func (m *MapReduceAggregator[E, K, Agg]) Flush(ctx context.Context) {
+	flushed := func() map[K]Agg {
+		m.mu.Lock()
+		defer m.mu.Unlock()
+		flushed := m.mu.cache
+		m.mu.cache = make(map[K]Agg)
+		return flushed
+	}()
+	// TODO(abarganier): We should probably use a stopper async task here.
+	// TODO(abarganier): Should we make whether this is done async configurable?
+	go func() {
+		for _, c := range m.consumers {
+			c.onFlush(ctx, flushed)
+		}
+	}()
+}
