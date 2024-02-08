89df4a02a770a3faad7321ee836e7e8bca68e388
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2024-02-08 15:46:30 -0500

    kv: remove settings param from Txn.DeadlineLikelySufficient
    This isn't needed anymore and was responsible for a significant amount
    of plumbing which was added in dfd52050.
    
    Epic: None
    Release note: None

--- pkg/sql/tablewriter_delete.go
+++ pkg/sql/tablewriter_delete.go
@@ -42,9 +42,9 @@ func (td *tableDeleter) walkExprs(_ func(desc string, index int, expr tree.Typed
 
 // init is part of the tableWriter interface.
 func (td *tableDeleter) init(
-	_ context.Context, txn *kv.Txn, evalCtx *eval.Context, sv *settings.Values,
+	_ context.Context, txn *kv.Txn, evalCtx *eval.Context
 ) error {
-	return td.tableWriterBase.init(txn, td.tableDesc(), evalCtx, sv)
+	return td.tableWriterBase.init(txn, td.tableDesc(), evalCtx)
 }
 
 // row is part of the tableWriter interface.
