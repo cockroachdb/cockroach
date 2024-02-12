e599c7103a609b62804b296113b56a8a43d6a1e0
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-02-12 12:24:41 -0800

    roachtest: delete tpchvec/smithcmp
    This tpchvec config was added about 4 years ago, uses `smithcmp`
    binary to issue TPCH queries with randomized placeholder values
    against a cluster with preloaded data set, and then compares results of
    `vectorize=off` against `vectorize=on` connections. However, this test
    has never found a single problem, so it doesn't seem that useful.
    Recently there were some changes to roachtest infra that made the test
    fail on older branches, so I think the burden of maintaining this test
    outweighs its benefits, so this commit removes it altogether (and it'll
    be removed on older branches too).
    
    This commit also does a minor cleanup of the roachtest to inline some
    parameters that now have only one variant.
    
    Epic: None
    
    Release note: None

--- pkg/cmd/roachtest/tests/tpchvec.go
+++ pkg/cmd/roachtest/tests/tpchvec.go
@@ -18,7 +18,6 @@ import (
 	"fmt"
 	"math"
 	"regexp"
-	"runtime"
 	"sort"
 	"strconv"
 	"strings"
@@ -28,7 +27,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/registry"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/test"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
-	"github.com/cockroachdb/cockroach/pkg/util/binfetcher"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/cockroachdb/cockroach/pkg/workload/tpch"
 	"github.com/stretchr/testify/require"
@@ -41,7 +39,7 @@ var tpchTables = []string{
 
 // tpchVecTestRunConfig specifies the configuration of a tpchvec test run.
 type tpchVecTestRunConfig struct {
-	// numRunsPerQuery determines how many time a single query runs, set to 1
+	// numRunsPerQuery determines how many times a single query runs, set to 1
 	// by default.
 	numRunsPerQuery int
 	// clusterSetups specifies all cluster setup queries that need to be
@@ -71,8 +69,6 @@ type tpchVecTestCase interface {
 	sharedProcessMT() bool
 	// getRunConfig returns the configuration of tpchvec test run.
 	getRunConfig() tpchVecTestRunConfig
-	// preQueryRunHook is called before each tpch query is run.
-	preQueryRunHook(t test.Test, conn *gosql.DB, clusterSetup []string)
 	// postQueryRunHook is called after each tpch query is run with the output and
 	// the index of the setup it was run in.
 	postQueryRunHook(t test.Test, output []byte, setupIdx int)
@@ -98,10 +94,6 @@ func (b tpchVecTestCaseBase) getRunConfig() tpchVecTestRunConfig {
 		}},
 		setupNames: []string{"default"},
 	}
-}
-
-func (b tpchVecTestCaseBase) preQueryRunHook(t test.Test, conn *gosql.DB, clusterSetup []string) {
-	performClusterSetup(t, conn, clusterSetup)
 }
 
 func (b tpchVecTestCaseBase) postQueryRunHook(test.Test, []byte, int) {}
@@ -507,100 +499,7 @@ func getTPCHVecWorkloadCmd(numRunsPerQuery, queryNum int, sharedProcessMT bool) 
 		numRunsPerQuery, queryNum, url)
 }
 
-func baseTestRun(
-	ctx context.Context, t test.Test, c cluster.Cluster, conn *gosql.DB, tc tpchVecTestCase,
-) {
-	runConfig := tc.getRunConfig()
-	for queryNum := 1; queryNum <= tpch.NumQueries; queryNum++ {
-		for setupIdx, setup := range runConfig.clusterSetups {
-			tc.preQueryRunHook(t, conn, setup)
-			result, err := c.RunWithDetailsSingleNode(
-				ctx, t.L(), option.WithNodes(c.Node(1)),
-				getTPCHVecWorkloadCmd(runConfig.numRunsPerQuery, queryNum, tc.sharedProcessMT()),
-			)
-			workloadOutput := result.Stdout + result.Stderr
-			t.L().Printf(workloadOutput)
-			if err != nil {
-				// Note: if you see an error like "exit status 1", it is likely caused
-				// by the erroneous output of the query.
-				t.Fatal(err)
-			}
-			tc.postQueryRunHook(t, []byte(workloadOutput), setupIdx)
-		}
-	}
-}
-
-type tpchVecSmithcmpTest struct {
-	tpchVecTestCaseBase
-}
-
-const tpchVecSmithcmp = "smithcmp"
-
-func smithcmpPreTestRunHook(
-	ctx context.Context, t test.Test, c cluster.Cluster, conn *gosql.DB, clusterSetup []string,
-) {
-	performClusterSetup(t, conn, clusterSetup)
-	const smithcmpSHA = "a3f41f5ba9273249c5ecfa6348ea8ee3ac4b77e3"
-	node := c.Node(1)
-	if c.IsLocal() && runtime.GOOS != "linux" {
-		t.Fatalf("must run on linux os, found %s", runtime.GOOS)
-	}
-	// This binary has been manually compiled using
-	// './build/builder.sh go build ./pkg/cmd/smithcmp' and uploaded to S3
-	// bucket at cockroach/smithcmp. The binary shouldn't change much, so it is
-	// acceptable.
-	smithcmp, err := binfetcher.Download(ctx, binfetcher.Options{
-		Component: tpchVecSmithcmp,
-		Binary:    tpchVecSmithcmp,
-		Version:   smithcmpSHA,
-		GOOS:      "linux",
-		GOARCH:    "amd64",
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-	c.Put(ctx, smithcmp, "./"+tpchVecSmithcmp, node)
-}
-
-func smithcmpTestRun(
-	ctx context.Context, t test.Test, c cluster.Cluster, conn *gosql.DB, tc tpchVecTestCase,
-) {
-	runConfig := tc.getRunConfig()
-	smithcmpPreTestRunHook(ctx, t, c, conn, runConfig.clusterSetups[0])
-	const configFile = `tpchvec_smithcmp.toml`
-
-	firstNode := c.Node(1)
-	if err := c.PutE(ctx, t.L(), "./pkg/cmd/roachtest/tests/"+configFile, configFile); err != nil {
-		t.Fatal(err)
-	}
-	port, err := c.SQLPorts(ctx, t.L(), c.Node(1), "", 0)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// We don't know the pgurl ahead of time, so we must edit it into the config file.
-	// We create the URL manually, opposed to using DefaultPgUrl(), as we need to:
-	//		Escape & as it's a special character in sed.
-	//		Use postgresql instead of postgres.
-	// 		Use tpch and localhost.
-	pgurl := fmt.Sprintf(
-		"postgresql://%[1]s:%[2]s@localhost:%[3]d/tpch?sslcert=certs%%2Fclient.%[1]s.crt\\&sslkey=certs%%2Fclient.%[1]s.key\\&sslmode=verify-full\\&sslrootcert=certs%%2Fca.crt",
-		install.DefaultUser, install.DefaultPassword, port[0])
-	if err := c.RunE(ctx, option.WithNodes(firstNode), fmt.Sprintf(`sed -i "s|PG_Connection_String|%s|g" %s`, pgurl, configFile)); err != nil {
-		t.Fatal(err)
-	}
-	cmd := fmt.Sprintf("./%s %s", tpchVecSmithcmp, configFile)
-	if err := c.RunE(ctx, option.WithNodes(firstNode), cmd); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func runTPCHVec(
-	ctx context.Context,
-	t test.Test,
-	c cluster.Cluster,
-	testCase tpchVecTestCase,
-	testRun func(ctx context.Context, t test.Test, c cluster.Cluster, conn *gosql.DB, tc tpchVecTestCase),
-) {
+func runTPCHVec(ctx context.Context, t test.Test, c cluster.Cluster, testCase tpchVecTestCase) {
 	firstNode := c.Node(1)
 	c.Put(ctx, t.DeprecatedWorkload(), "./workload", firstNode)
 	c.Start(ctx, t.L(), option.DefaultStartOptsNoBackups(), install.MakeClusterSettings())
@@ -634,7 +533,24 @@ func runTPCHVec(
 	err := WaitFor3XReplication(ctx, t, t.L(), conn)
 	require.NoError(t, err)
 
-	testRun(ctx, t, c, conn, testCase)
+	runConfig := testCase.getRunConfig()
+	for queryNum := 1; queryNum <= tpch.NumQueries; queryNum++ {
+		for setupIdx, clusterSetup := range runConfig.clusterSetups {
+			performClusterSetup(t, conn, clusterSetup)
+			result, err := c.RunWithDetailsSingleNode(
+				ctx, t.L(), option.WithNodes(c.Node(1)),
+				getTPCHVecWorkloadCmd(runConfig.numRunsPerQuery, queryNum, testCase.sharedProcessMT()),
+			)
+			workloadOutput := result.Stdout + result.Stderr
+			t.L().Printf(workloadOutput)
+			if err != nil {
+				// Note: if you see an error like "exit status 1", it is likely caused
+				// by the erroneous output of the query.
+				t.Fatal(err)
+			}
+			testCase.postQueryRunHook(t, []byte(workloadOutput), setupIdx)
+		}
+	}
 	testCase.postTestRunHook(ctx, t, c, conn)
 }
 
@@ -653,7 +569,7 @@ func registerTPCHVec(r registry.Registry) {
 				"sql.defaults.vectorize", /* settingName */
 				1.5,                      /* slownessThreshold */
 				false,                    /* sharedProcessMT */
-			), baseTestRun)
+			))
 		},
 	})
 
@@ -664,19 +580,7 @@ func registerTPCHVec(r registry.Registry) {
 		CompatibleClouds: registry.AllExceptAWS,
 		Suites:           registry.Suites(registry.Nightly),
 		Run: func(ctx context.Context, t test.Test, c cluster.Cluster) {
-			runTPCHVec(ctx, t, c, tpchVecDiskTest{}, baseTestRun)
-		},
-	})
-
-	r.Add(registry.TestSpec{
-		Name:             "tpchvec/smithcmp",
-		Owner:            registry.OwnerSQLQueries,
-		Cluster:          r.MakeClusterSpec(tpchVecNodeCount),
-		CompatibleClouds: registry.AllExceptAWS,
-		Suites:           registry.Suites(registry.Nightly),
-		RequiresLicense:  true,
-		Run: func(ctx context.Context, t test.Test, c cluster.Cluster) {
-			runTPCHVec(ctx, t, c, tpchVecSmithcmpTest{}, smithcmpTestRun)
+			runTPCHVec(ctx, t, c, tpchVecDiskTest{})
 		},
 	})
 
@@ -692,7 +596,7 @@ func registerTPCHVec(r registry.Registry) {
 				"sql.distsql.use_streamer.enabled", /* settingName */
 				1.5,                                /* slownessThreshold */
 				false,                              /* sharedProcessMT */
-			), baseTestRun)
+			))
 		},
 	})
 
@@ -708,7 +612,7 @@ func registerTPCHVec(r registry.Registry) {
 				"sql.distsql.direct_columnar_scans.enabled", /* settingName */
 				1.5,   /* slownessThreshold */
 				false, /* sharedProcessMT */
-			), baseTestRun)
+			))
 		},
 	})
 
@@ -724,7 +628,7 @@ func registerTPCHVec(r registry.Registry) {
 				"sql.distsql.direct_columnar_scans.enabled", /* settingName */
 				1.5,  /* slownessThreshold */
 				true, /* sharedProcessMT */
-			), baseTestRun)
+			))
 		},
 	})
 
@@ -756,7 +660,7 @@ func registerTPCHVec(r registry.Registry) {
 				clusterSetups,
 				setupNames,
 			)
-			runTPCHVec(ctx, t, c, benchTest, baseTestRun)
+			runTPCHVec(ctx, t, c, benchTest)
 		},
 	})
 }
