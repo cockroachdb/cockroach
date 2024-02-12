a4519750a88230bcf0c761640271d3f0d234601a
Marcus Gartner <marcus@cockroachlabs.com>
2024-02-12 12:53:48 -0500

    opt/execbuilder: faster maximum ordinal method for colOrdMap
    This commit makes `colOrdMap.MaxOrd()` a constant-time operation in most
    cases. See the newly added comments for more details.
    
    Release note: None

--- pkg/sql/opt/exec/execbuilder/col_ord_map.go
+++ pkg/sql/opt/exec/execbuilder/col_ord_map.go
@@ -91,6 +91,11 @@ type colOrdMap struct {
 	// 1, which allows the map to store the zero ordinal and have the zero value
 	// in the map represent an unset column ID.
 	ords []int32
+	// maxOrd is the maximum ordinal in the map and is used by MaxOrd. If it is
+	// 0, then the set is empty. If it is -1, then the current maximum ordinal
+	// is "unknown" and MaxOrd must scan the entire map to find the maximum
+	// ordinal. See Set and MaxOrd for more details.
+	maxOrd int
 }
 
 // newColOrdMap returns a new column mapping that can store column IDs less than
@@ -111,6 +116,20 @@ func (m *colOrdMap) Set(col opt.ColumnID, ord int) {
 	if ord > math.MaxInt32 {
 		panic(errors.AssertionFailedf("ordinal %d exceeds max ordinal %d", ord-1, math.MaxInt32-1))
 	}
+	switch {
+	case m.maxIsUnknown():
+		// If the maximum ordinal is currently unknown, then leave it as-is.
+	case m.ords[col] > 0 && m.ords[col] == int32(m.maxOrd) && ord < m.maxOrd:
+		// If we are overriding an ordinal that was previously the maximum
+		// ordinal with a smaller ordinal, then we don't know what the new
+		// maximum ordinal is. So we set the maximum ordinal as "unknown". This
+		// makes MaxOrd scan the map to find the maximum ordinal. See MaxOrd for
+		// more details.
+		m.setUnknownMax()
+	default:
+		// Otherwise, set the known maximum ordinal.
+		m.maxOrd = max(m.maxOrd, ord)
+	}
 	m.ords[col] = int32(ord)
 }
 
@@ -128,19 +147,34 @@ func (m colOrdMap) Get(col opt.ColumnID) (ord int, ok bool) {
 	return ord - 1, true
 }
 
-// MaxOrd returns the maximum ordinal stored in the map, or -1 if the map is
-// empty.
+// MaxOrd returns the maximum ordinal in the map, or -1 if the map is empty.
+//
+// In most cases, MaxOrd has constant time complexity. If the map had a previous
+// maximum ordinal that was overwritten by a smaller ordinal, then MaxOrd has
+// linear time complexity with respect to the size of the map. In this case, the
+// result is memoized, so future calls to MaxOrd are constant time until the
+// maximum ordinal is overwritten by a smaller ordinal again.
+//
+// The case with linear time complexity should be rare in practice. The benefit
+// of this approach is that it allows for constant time complexity in most cases
+// with only constant additional space. Making MaxOrd constant-time in all cases
+// would require non-constant additional space to keep a history of all previous
+// maximum ordinals.
 func (m colOrdMap) MaxOrd() int {
-	maxOrd := -1
-	for _, ord := range m.ords {
-		if ord == 0 {
-			continue
-		}
+	// If the maximum ordinal is known, return it.
+	if !m.maxIsUnknown() {
 		// Reverse the bias when fetching the max ordinal from the map.
-		ord--
-		maxOrd = max(maxOrd, int(ord))
+		return m.maxOrd - 1
 	}
-	return maxOrd
+	// Otherwise, maxOrd is negative, meaning that a previous maximum ordinal
+	// was overwritten by a smaller ordinal. So we have to search for the
+	// maximum ordinal in the set.
+	m.maxOrd = 0
+	for _, ord := range m.ords {
+		m.maxOrd = max(m.maxOrd, int(ord))
+	}
+	// Reverse the bias when fetching the max ordinal from the map.
+	return m.maxOrd - 1
 }
 
 // ForEach calls the given function for each column ID and ordinal pair in the
@@ -163,11 +197,21 @@ func (m *colOrdMap) CopyFrom(other colOrdMap) {
 			len(m.ords), len(other.ords)))
 	}
 	copy(m.ords, other.ords)
+	m.maxOrd = other.maxOrd
 }
 
 // Clear clears the map. The allocated memory is retained for future reuse.
-func (m colOrdMap) Clear() {
+func (m *colOrdMap) Clear() {
 	for i := range m.ords {
 		m.ords[i] = 0
 	}
+	m.maxOrd = 0
+}
+
+func (m colOrdMap) maxIsUnknown() bool {
+	return m.maxOrd == -1
+}
+
+func (m *colOrdMap) setUnknownMax() {
+	m.maxOrd = -1
 }
