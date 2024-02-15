942b388e3379dc3437794d852dab31f51c181692
Ricky Stewart <ricky@cockroachlabs.com>
2024-02-15 14:11:40 -0600

    workflows: adjust shutdown logic
    This script was flawed in a couple ways:
    1. `bazel shutdown` is too nice for what we need to do as it waits for
       running builds to complete before the exit actually occurs.
    2. We used `EXIT` which runs unconditionally on process exit.
    
    Instead we `killall bazel` if we receive a `SIGINT` or `SIGTERM`.
    
    Epic: CRDB-8308
    Release note: None

--- build/github/run-bazel.sh
+++ build/github/run-bazel.sh
@@ -1,11 +1,17 @@
+#!/usr/bin/env bash
+
 # Any arguments passed to this script will be directly passed to Bazel. In
 # addition to just calling Bazel, this script has the property that the Bazel
 # server will be killed if we receive an interrupt.
 
-bazel_shutdown() {
-    bazel shutdown
+kill_bazel() {
+    PIDS=$(ps -ef | grep bazel | grep -v run-bazel.sh | grep -v grep | awk '{print $2}')
+    for PID in $PIDS
+    do
+        kill -KILL $PID
+    done
 }
 
-trap bazel_shutdown EXIT
+trap kill_bazel INT TERM
 
 bazel "$@"
