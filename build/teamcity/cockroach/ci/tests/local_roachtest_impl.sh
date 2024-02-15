efb978a35721f740187e250acb791c11f0572b27
Renato Costa <renato@cockroachlabs.com>
2024-02-15 12:45:26 -0500

    roachtest: require libGEOS in tests that run schemachange
    The schemachange workload started performing statements that might
    require libGEOS to be present in the cluster. Several tests invoke
    that workload without indicating that they need lib GEOS. In this
    commit, we update tests in this situation.
    
    Fixes: #119123
    Fixes: #119125
    
    Release note: None

--- build/teamcity/cockroach/ci/tests/local_roachtest_impl.sh
+++ build/teamcity/cockroach/ci/tests/local_roachtest_impl.sh
@@ -8,12 +8,19 @@ else
   export CROSSLINUX_CONFIG="crosslinux"
 fi
 
+BAZEL_BIN=$(bazel info bazel-bin --config=$CROSSLINUX_CONFIG --config=ci)
+
 bazel build --config=$CROSSLINUX_CONFIG --config=ci //pkg/cmd/cockroach-short \
       //pkg/cmd/roachtest \
       //pkg/cmd/roachprod \
       //pkg/cmd/workload
 
-BAZEL_BIN=$(bazel info bazel-bin --config=$CROSSLINUX_CONFIG --config=ci)
+bazel build --config=$CROSSLINUX_CONFIG --config=ci --config=force_build_cdeps //c-deps:libgeos
+
+mkdir -p lib
+cp $BAZEL_BIN/c-deps/libgeos_foreign/lib/libgeos.so lib/libgeos.so
+cp $BAZEL_BIN/c-deps/libgeos_foreign/lib/libgeos_c.so lib/libgeos_c.so
+chmod a+w lib/libgeos.so lib/libgeos_c.so
 
 # if there are any local clusters on this host, stop them before we
 # attempt to run acceptance tests. While this is generally not the
