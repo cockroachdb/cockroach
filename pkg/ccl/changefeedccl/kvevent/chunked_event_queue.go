177dbc2f9222c3a60a530788d6804e60bb50a89a
Jayant Shrivastava <jayants@cockroachlabs.com>
2024-02-18 18:43:11 -0500

    util/queue: add generic queue implementation
    This change adds a queue implementation. This queue is implemented as a linked list.
    This linked list uses chunking to amortize the overhead of linked list
    elements.
    
    This implementation is partially copied from `pkg/ccl/changefeedccl/kvevent/chunked_event_queue.go`.
    A TODO is left to migrate that queue to use the generic implementation. Tests are copied
    as well. The original implementation has been around for over 1 year and has been tested
    at high scales (millions of elements, throughput of 100k items / sec, handling 100s of MBs
    of data flowing through). The implementation is efficient in terms of memory and CPU usage.
    
    The purpose of this change is to introduce a datastructure which scales and resizes
    efficiently specifically for #109667. There is no existing implementation which does
    so except for `chunked_event_queue.go`. Unfortunately, that queue does not support
    generic types, so this change makes it generic.
    
    Release note: None
    Epic: None

--- pkg/ccl/changefeedccl/kvevent/chunked_event_queue.go
+++ pkg/ccl/changefeedccl/kvevent/chunked_event_queue.go
@@ -13,6 +13,7 @@ import "sync"
 const bufferEventChunkArrSize = 128
 
 // bufferEventChunkQueue is a queue implemented as a linked-list of bufferEntry.
+// TODO(#120216): This data structure should use `util/queue.Queue`.
 type bufferEventChunkQueue struct {
 	head, tail *bufferEventChunk
 }
