471f7893d26494f68d14391ce55fdb161c917841
Faizan Qazi <faizan@cockroachlabs.com>
2024-02-22 17:32:44 -0500

    release-23.1: catalog/lease: optimize lease count query
    Previously, the query to count leases would end up scanning the entire
    system.leases table because of the addition of crdb_region as the first
    column on 23.1. This meant that scans not specifying a region would
    always be forced to scan the entire table. To address this, this patch
    will update the count query to specify the default region when working
    on single region system databases.
    
    Fixes: #119456
    
    Release note (bug fix): Schema changes with a large number of descriptors
    could end up doing full table scans on system.leases.

--- pkg/sql/catalog/lease/count.go
+++ pkg/sql/catalog/lease/count.go
@@ -15,6 +15,9 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/sql/enum"
 	"github.com/cockroachdb/cockroach/pkg/sql/isql"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
@@ -22,27 +25,48 @@ import (
 	"github.com/cockroachdb/errors"
 )
 
+// getSingleRegionClause returns an extra clause if the system database is in
+// a single region to optimize scans on the system.leases table.
+func getSingleRegionClause(
+	ctx context.Context, isMultiRegion bool, st *cluster.Settings,
+) (query string, queryParam []byte) {
+	query, queryParam = "", nil
+	if !isMultiRegion && st.Version.IsActive(ctx, clusterversion.V23_1_SystemRbrSingleWrite) {
+		query = " AND crdb_region=$2"
+		queryParam = enum.One
+	}
+	return query, queryParam
+}
+
 // CountLeases returns the number of unexpired leases for a number of descriptors
 // each at a particular version at a particular time.
 func CountLeases(
-	ctx context.Context, executor isql.Executor, versions []IDVersion, at hlc.Timestamp,
+	ctx context.Context,
+	executor isql.Executor,
+	isMultiRegion bool,
+	st *cluster.Settings,
+	versions []IDVersion,
+	at hlc.Timestamp,
 ) (int, error) {
-	var whereClauses []string
-	for _, t := range versions {
-		whereClauses = append(whereClauses,
-			fmt.Sprintf(`("descID" = %d AND version = %d AND expiration > $1)`,
-				t.ID, t.Version),
-		)
+	whereClauses := strings.Builder{}
+	for i, t := range versions {
+		if i != 0 {
+			whereClauses.WriteString(" OR ")
+		}
+		whereClauses.WriteString(fmt.Sprintf(`("descID" = %d AND version = %d AND expiration > $1)`,
+			t.ID, t.Version))
 	}
 
-	stmt := fmt.Sprintf(`SELECT count(1) FROM system.public.lease AS OF SYSTEM TIME '%s' WHERE `,
-		at.AsOfSystemTime()) +
-		strings.Join(whereClauses, " OR ")
+	singleRegionClause, singleRegionParam := getSingleRegionClause(ctx, isMultiRegion, st)
+	stmt := fmt.Sprintf(`SELECT count(1) FROM system.public.lease AS OF SYSTEM TIME '%s' WHERE (%s) %s `,
+		at.AsOfSystemTime(),
+		whereClauses.String(),
+		singleRegionClause)
 
 	values, err := executor.QueryRowEx(
 		ctx, "count-leases", nil, /* txn */
 		sessiondata.RootUserSessionDataOverride,
-		stmt, at.GoTime(),
+		stmt, at.GoTime(), singleRegionParam,
 	)
 	if err != nil {
 		return 0, err
