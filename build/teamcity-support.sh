ae7fcd62aac5cabc533c51d5305b45031dcdc081
Rail Aliiev <rail@iqchoice.com>
2024-02-26 10:02:10 -0500

    ci: treat staging branches as release branches
    Previously, the staging branches were not included in the list of
    release branches, what caused unnecessary test runs.
    
    This PR adds the staging branch pattern of release branches.
    
    Epic: none
    Release note: None
    Release justification: CI changes

--- build/teamcity-support.sh
+++ build/teamcity-support.sh
@@ -257,7 +257,7 @@ tc_build_branch() {
 # function.
 tc_release_branch() {
   branch=$(tc_build_branch)
-  [[ "$branch" == master || "$branch" == release-* || "$branch" == provisional_* ]]
+  [[ "$branch" == master || "$branch" == release-* || "$branch" == provisional_*  || "$branch" == "staging-"* ]]
 }
 
 tc_bors_branch() {
