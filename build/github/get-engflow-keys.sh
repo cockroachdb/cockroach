90f62f499a2f630f81106bb87852574c35d50870
Ricky Stewart <ricky@cockroachlabs.com>
2024-02-27 13:59:45 -0600

    workflows: use new version of certificate
    Epic: CRDB-8308
    Release note: None

--- build/github/get-engflow-keys.sh
+++ build/github/get-engflow-keys.sh
@@ -2,5 +2,5 @@
 
 set -euxo pipefail
 
-gcloud secrets versions access 1 --secret=engflow-mesolite-key > /home/agent/engflow.key
-gcloud secrets versions access 1 --secret=engflow-mesolite-crt > /home/agent/engflow.crt
+gcloud secrets versions access 2 --secret=engflow-mesolite-key > /home/agent/engflow.key
+gcloud secrets versions access 2 --secret=engflow-mesolite-crt > /home/agent/engflow.crt
