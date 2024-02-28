45e5c40211071df17ec7cd1f9a0a779375ef3cd0
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-02-28 18:31:35 +0000

    roachtest: fix float comparison so 0 always matches -0 inside arrays
    This commit extends the fix from
    43c899635ecc86006a07981c97ed7c22a0c47f93 to also apply to float arrays.
    
    Release note: None

--- pkg/testutils/floatcmp/floatcmp.go
+++ pkg/testutils/floatcmp/floatcmp.go
@@ -97,6 +97,40 @@ func FloatsMatchApprox(expectedString, actualString string) (bool, error) {
 	return EqualApprox(expected, actual, CloseFraction, CloseMargin), nil
 }
 
+// splitFloatArray splits the textual representation of the float array of the
+// form {1.2,3.4,5.6,...} into its elements (in the textual form).
+func splitFloatArray(array string) []string {
+	return strings.Split(strings.Trim(array, "{}"), ",")
+}
+
+// floatArraysMatchImpl is the common logic for verifying whether two float
+// arrays match where per-element verification is done by the provided function.
+func floatArraysMatchImpl(
+	expected, actual string, matchFn func(string, string) (bool, error),
+) (bool, error) {
+	if expected == "NULL" || actual == "NULL" {
+		// Default to string matching for NULL arrays.
+		return expected == actual, nil
+	}
+	exp, act := splitFloatArray(expected), splitFloatArray(actual)
+	if len(exp) != len(act) {
+		return false, nil
+	}
+	for i := range exp {
+		match, err := matchFn(exp[i], act[i])
+		if !match || err != nil {
+			return false, err
+		}
+	}
+	return true, nil
+}
+
+// FloatArraysMatchApprox returns whether two floating point arrays represented
+// as strings are equal within a tolerance.
+func FloatArraysMatchApprox(expectedString, actualString string) (bool, error) {
+	return floatArraysMatchImpl(expectedString, actualString, FloatsMatchApprox)
+}
+
 // FloatsCmp returns -1 if aString is less than bString, 0 if they are equal,
 // and 1 otherwise when aString and bString are parsed as SQL floats. NULL
 // sorts before NaN, which sorts before all other numbers.
@@ -130,6 +164,40 @@ func FloatsCmp(aString, bString string) (int, error) {
 	} else {
 		return 1, nil
 	}
+}
+
+// FloatArraysCmp returns -1 if aString is less than bString, 0 if they are
+// equal, and 1 otherwise when aString and bString are parsed as SQL float
+// arrays. NULL sorts before NaN, which sorts before all other numbers.
+func FloatArraysCmp(aString, bString string) (int, error) {
+	if aString == "NULL" {
+		if bString != "NULL" {
+			return -1, nil
+		}
+		return 0, nil
+	} else if bString == "NULL" {
+		return 1, nil
+	}
+	aFloats, bFloats := splitFloatArray(aString), splitFloatArray(bString)
+	// Comparison of arrays is such that we check the "shared" elements first,
+	// and only if all of them are equal, then we check the lengths of arrays
+	// (see tree.DArray.CompareError).
+	toCheck := len(aFloats)
+	if len(bFloats) < len(aFloats) {
+		toCheck = len(bFloats)
+	}
+	for i := 0; i < toCheck; i++ {
+		cmp, err := FloatsCmp(aFloats[i], bFloats[i])
+		if cmp != 0 || err != nil {
+			return cmp, err
+		}
+	}
+	if len(aFloats) < len(bFloats) {
+		return -1, nil
+	} else if len(aFloats) > len(bFloats) {
+		return 1, nil
+	}
+	return 0, nil
 }
 
 // FloatsMatch returns whether two floating point numbers represented as
@@ -197,6 +265,13 @@ func FloatsMatch(expectedString, actualString string) (bool, error) {
 		actual -= (actual - float64(actDigit)) * 10
 	}
 	return true, nil
+}
+
+// FloatArraysMatch returns whether two floating point number arrays represented
+// as strings have matching 15 significant decimal digits (this is the precision
+// that Postgres supports for 'double precision' type) for each element.
+func FloatArraysMatch(expectedString, actualString string) (bool, error) {
+	return floatArraysMatchImpl(expectedString, actualString, FloatsMatch)
 }
 
 // RoundFloatsInString rounds floats in a given string to the given number of significant figures.
