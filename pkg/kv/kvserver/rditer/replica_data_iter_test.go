c3edbe5dc57444877d0dabadce4f9bd8cbbdd34e
Mira Radeva <mira@cockroachlabs.com>
2024-03-01 14:11:25 -0500

    rditer: add functions to compute the MVCC stats for user-only and non-user spans
    ComputeStatsForRange iterates over an entire range and computes its MVCC
    stats.
    
    This patch adds ComputeStatsForRangeUserOnly and
    ComputeStatsForRangeExcludingUser, which compute the MVCC stats
    corresponding to the user-only and non-user spans in the range,
    respectively. These functions are useful for computing the estimated
    stats proposed in #119499 by allowing to quickly iterate only over the
    non-user key spans in the range, and separately iterate over the
    user-only key spans.
    
    Part of: #119499
    Release note: None

--- pkg/kv/kvserver/rditer/replica_data_iter_test.go
+++ pkg/kv/kvserver/rditer/replica_data_iter_test.go
@@ -24,12 +24,14 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/storage"
+	"github.com/cockroachdb/cockroach/pkg/storage/enginepb"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/datapathutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/echotest"
 	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/cockroachdb/cockroach/pkg/util/uuid"
 	"github.com/olekukonko/tablewriter"
@@ -118,6 +120,13 @@ func createRangeData(
 			TxnUUID:  testTxnID,
 		},
 	}
+	lockMeta := enginepb.MVCCMetadata{
+		Txn:      &enginepb.TxnMeta{ID: testTxnID},
+		KeyBytes: storage.MVCCVersionTimestampSize,
+		ValBytes: int64(len(value.RawBytes)),
+	}
+	lockVal, err := protoutil.Marshal(&lockMeta)
+	require.NoError(t, err)
 
 	rs = append(rs, storage.MVCCRangeKey{ // emitted last because we emit all point keys before range keys
 		StartKey:  desc.StartKey.AsRawKey().Clone(),
@@ -134,7 +143,7 @@ func createRangeData(
 	}
 	for _, l := range locks {
 		sl, _ := l.ToEngineKey(nil)
-		require.NoError(t, eng.PutEngineKey(sl, []byte("fake lock")))
+		require.NoError(t, eng.PutEngineKey(sl, lockVal))
 	}
 
 	return ps, rs
