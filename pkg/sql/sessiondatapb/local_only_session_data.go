328069516629731b99d355ddeb9a7621ae0e0427
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-03-04 12:33:16 -0800

    sessiondatapb: remove allocations whenn parsing QoS
    I noticed this when looking at a profile of establishing new
    connections. The idea is to use `ToLower` instead of `ToUpper` because
    our QoS constants already use the lower case.
    
    Release note: None

--- pkg/sql/sessiondatapb/local_only_session_data.go
+++ pkg/sql/sessiondatapb/local_only_session_data.go
@@ -355,12 +355,12 @@ var qosLevelsDict = map[QoSLevel]string{
 
 // ParseQoSLevelFromString converts a string into a QoSLevel
 func ParseQoSLevelFromString(val string) (_ QoSLevel, ok bool) {
-	switch strings.ToUpper(val) {
-	case strings.ToUpper(UserHighName):
+	switch strings.ToLower(val) {
+	case UserHighName:
 		return UserHigh, true
-	case strings.ToUpper(UserLowName):
+	case UserLowName:
 		return UserLow, true
-	case strings.ToUpper(NormalName):
+	case NormalName:
 		return Normal, true
 	default:
 		return 0, false
