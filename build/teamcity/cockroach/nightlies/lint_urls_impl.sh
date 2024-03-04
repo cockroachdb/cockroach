6988b23071fe1ebbec9279fb02134a3dec4332aa
Ricky Stewart <ricky@cockroachlabs.com>
2024-03-04 16:32:41 -0600

    lint: various fixes and improvements
    1. Use code generation to remove the `lint` build tag for running lints.
       On the Bazel side, you're not likely to accidentally run these tests
       and build tags just slow things down.
    2. Fix the script for the nightly lint to exit with the correct status
       code and generate code before running.
    3. Fix some lint errors in `lint_test.go` itself.
    
    Epic: none
    Release note: None

--- build/teamcity/cockroach/nightlies/lint_urls_impl.sh
+++ build/teamcity/cockroach/nightlies/lint_urls_impl.sh
@@ -4,12 +4,19 @@ set -xeuo pipefail
 
 dir="$(dirname $(dirname $(dirname $(dirname "${0}"))))"
 
+# GCAssert and unused need generated files in the workspace to work properly.
+# generated files requirements -- begin
+bazel run //pkg/gen:code
+bazel run //pkg/cmd/generate-cgo:generate-cgo --run_under="cd $(bazel info workspace) && "
+# generated files requirements -- end
+
 bazel build //pkg/cmd/bazci --config=ci
 BAZEL_BIN=$(bazel info bazel-bin --config=ci)
 exit_status=0
 XML_OUTPUT_FILE=/artifacts/test.xml GO_TEST_WRAP_TESTV=1 GO_TEST_WRAP=1 bazel \
-    run --config=ci --config=test --define gotags=bazel,gss,nightly \
+    run --config=ci --config=test --define gotags=bazel,gss,nightly,lint \
     //build/bazelutil:lint || exit_status=$?
 # The schema of the output test.xml will be slightly wrong -- ask `bazci` to fix
 # it up.
 $BAZEL_BIN/pkg/cmd/bazci/bazci_/bazci munge-test-xml /artifacts/test.xml
+exit $exit_status
