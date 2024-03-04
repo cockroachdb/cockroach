cf0d6e1e94f7fa9ac36c25b2c59057141cbfe7e5
Alex Barganier <abarganier@cockroachlabs.com>
2024-03-04 15:21:01 -0400

    workload: add sqlstats workload
    This new workload generates a high rate of insert queries that fill up
    the sql stats pipeline by generating a high number of fingerprints.
    
    This happens because the order of the 10 columns is shuffled in each
    request and our fingerprint algorithm treates these as unique for now.
    
    This workload isn't a perfect solution to stressing the stats system
    but gets us started. Running it for 10 minutes on a local cluster will
    easily generate very high request times for sql stats queries on the
    SQL Activity page when large timescales are selected, which bypass the
    sql_activity tables.
    
    Fixes: #119311
    Epic: None
    
    Release note: None

---
+++ pkg/workload/sqlstats/sqlstats.go
@@ -0,0 +1,179 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package sqlstats
+
+import (
+	"context"
+	gosql "database/sql"
+	"fmt"
+	"strings"
+
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/cockroachdb/cockroach/pkg/workload"
+	"github.com/cockroachdb/cockroach/pkg/workload/histogram"
+	"github.com/cockroachdb/errors"
+	"github.com/spf13/pflag"
+	"golang.org/x/exp/rand"
+)
+
+const (
+	dummyTable = `(
+		id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+		col1 INT,
+		col2 INT,
+		col3 INT,
+		col4 INT,
+		col5 INT,
+		col6 INT,
+		col7 INT,
+		col8 INT,
+		col9 INT,
+		col10 INT
+	)`
+	tableName     = "sql_stats_workload"
+	defaultDbName = "sql_stats"
+)
+
+var RandomSeed = workload.NewUint64RandomSeed()
+
+type sqlStats struct {
+	flags     workload.Flags
+	connFlags *workload.ConnFlags
+}
+
+var _ workload.Generator = &sqlStats{}
+var _ workload.Opser = &sqlStats{}
+var _ workload.Hookser = &sqlStats{}
+
+func init() {
+	workload.Register(sqlStatsMeta)
+}
+
+var sqlStatsMeta = workload.Meta{
+	Name:          "sqlstats",
+	Description:   `Make SQL Activity cry`,
+	RandomSeed:    RandomSeed,
+	Version:       "1.0.0",
+	TestInfraOnly: true,
+	New: func() workload.Generator {
+		g := &sqlStats{}
+		g.flags.FlagSet = pflag.NewFlagSet(`sqlstats`, pflag.ContinueOnError)
+		RandomSeed.AddFlag(&g.flags)
+		g.connFlags = workload.NewConnFlags(&g.flags)
+		return g
+	},
+}
+
+func (s *sqlStats) Meta() workload.Meta {
+	return sqlStatsMeta
+}
+
+func (s *sqlStats) Flags() workload.Flags {
+	return s.flags
+}
+
+func (s *sqlStats) Tables() []workload.Table {
+	return []workload.Table{{
+		Name:   tableName,
+		Schema: dummyTable,
+		Splits: workload.BatchedTuples{},
+	}}
+}
+
+func (s *sqlStats) Hooks() workload.Hooks {
+	return workload.Hooks{
+		Validate: func() error {
+			return nil
+		},
+	}
+}
+
+type gen struct {
+	syncutil.Mutex
+
+	in  []string
+	rng *rand.Rand
+}
+
+func (g *gen) Next() string {
+	g.Shuffle()
+
+	g.Lock()
+	defer g.Unlock()
+	s := strings.Join(g.in, ", ")
+	return fmt.Sprintf(`
+		INSERT INTO sql_stats_workload
+		(%s) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+	`, s)
+}
+
+func (g *gen) Shuffle() {
+	g.Lock()
+	defer g.Unlock()
+
+	g.rng.Shuffle(10, func(i, j int) {
+		g.in[i], g.in[j] = g.in[j], g.in[i]
+	})
+}
+
+func genPermutations() *gen {
+	rng := rand.New(rand.NewSource(RandomSeed.Seed()))
+	return &gen{
+		rng: rng,
+		in:  []string{"col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8", "col9", "col10"},
+	}
+}
+
+func (s *sqlStats) Ops(
+	ctx context.Context, urls []string, reg *histogram.Registry,
+) (workload.QueryLoad, error) {
+	sqlDatabase, err := workload.SanitizeUrls(s, s.connFlags.DBOverride, urls)
+	if err != nil {
+		return workload.QueryLoad{}, err
+	}
+	db, err := gosql.Open(`cockroach`, strings.Join(urls, ` `))
+	if err != nil {
+		return workload.QueryLoad{}, err
+	}
+	// Allow a maximum of concurrency+1 connections to the database.
+	db.SetMaxOpenConns(s.connFlags.Concurrency + 1)
+	db.SetMaxIdleConns(s.connFlags.Concurrency + 1)
+
+	gen := genPermutations()
+
+	ql := workload.QueryLoad{
+		SQLDatabase: sqlDatabase,
+		Close: func(_ context.Context) error {
+			return db.Close()
+		},
+	}
+	for i := 0; i < s.connFlags.Concurrency; i++ {
+		rng := rand.New(rand.NewSource(RandomSeed.Seed()))
+		hists := reg.GetHandle()
+		workerFn := func(ctx context.Context) error {
+			start := timeutil.Now()
+
+			query := gen.Next()
+			updateStmt, err := db.Prepare(query)
+			if err != nil {
+				return errors.CombineErrors(err, db.Close())
+			}
+
+			_, err = updateStmt.Exec(rng.Int(), rng.Int(), rng.Int(), rng.Int(), rng.Int(), rng.Int(), rng.Int(), rng.Int(), rng.Int(), rng.Int())
+			elapsed := timeutil.Since(start)
+			hists.Get(`transfer`).Record(elapsed)
+			return err
+		}
+		ql.WorkerFns = append(ql.WorkerFns, workerFn)
+	}
+	return ql, nil
+}
