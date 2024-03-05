1a22e178b90be32c30b5ba87318ca05f0464bc69
Ben Darnell <ben@cockroachlabs.com>
2024-03-05 10:15:10 +0530

    cliccl,engineccl: Generate and load JWKS-format keys for EAR
    Fixes: #119767
    Release note (enterprise change): `cockroach gen encryption-key` now
    accepts a `--version=2` parameter. Version 2 keys activate a new
    encryption implementation with improved performance. This is
    expected to become the default in CockroachDB 24.2.

--- pkg/ccl/cliccl/gen.go
+++ pkg/ccl/cliccl/gen.go
@@ -10,28 +10,79 @@ package cliccl
 
 import (
 	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
-	"io"
 	"os"
 
+	"github.com/cockroachdb/cockroach/pkg/ccl/storageccl/engineccl/enginepbccl"
 	"github.com/cockroachdb/cockroach/pkg/cli"
+	"github.com/cockroachdb/errors"
+	"github.com/lestrrat-go/jwx/jwk"
 	"github.com/spf13/cobra"
 )
 
 var aesSizeFlag int
 var overwriteKeyFlag bool
+var keyVersionFlag int
 
-func genEncryptionKey(encryptionKeyPath string, aesSize int, overwriteKey bool) error {
+func genEncryptionKey(
+	encryptionKeyPath string, aesSize int, overwriteKey bool, keyVersion int,
+) error {
 	// Check encryptionKeySize is suitable for the encryption algorithm.
 	if aesSize != 128 && aesSize != 192 && aesSize != 256 {
 		return fmt.Errorf("store key size should be 128, 192, or 256 bits, got %d", aesSize)
 	}
 
-	// 32 bytes are reserved for key ID.
-	kSize := aesSize/8 + 32
-	b := make([]byte, kSize)
-	if _, err := rand.Read(b); err != nil {
-		return fmt.Errorf("failed to create key with size %d bytes", kSize)
+	keyID := make([]byte, 32)
+	if _, err := rand.Read(keyID); err != nil {
+		return fmt.Errorf("failed to create random key ID")
+	}
+	key := make([]byte, aesSize/8)
+	if _, err := rand.Read(key); err != nil {
+		return fmt.Errorf("failed to create key with size %d bytes", aesSize/8)
+	}
+
+	var b []byte
+	switch keyVersion {
+	case 1:
+		b = append(b, keyID...)
+		b = append(b, key...)
+	case 2:
+		var et enginepbccl.EncryptionType
+		switch aesSize {
+		case 128:
+			et = enginepbccl.EncryptionType_AES_128_CTR_V2
+		case 192:
+			et = enginepbccl.EncryptionType_AES_192_CTR_V2
+		case 256:
+			et = enginepbccl.EncryptionType_AES_256_CTR_V2
+		}
+
+		symKey := jwk.NewSymmetricKey()
+		if err := symKey.FromRaw(key); err != nil {
+			return errors.Wrap(err, "error setting key bytes")
+		}
+		if err := symKey.Set(jwk.KeyIDKey, hex.EncodeToString(keyID)); err != nil {
+			return errors.Wrap(err, "error setting key id")
+		}
+		alg, err := et.JWKAlgorithm()
+		if err != nil {
+			return err
+		}
+		if err := symKey.Set(jwk.AlgorithmKey, alg); err != nil {
+			return errors.Wrap(err, "error setting algorithm")
+		}
+
+		keySet := jwk.NewSet()
+		keySet.Add(symKey)
+
+		b, err = json.Marshal(keySet)
+		if err != nil {
+			return errors.Wrap(err, "error writing key to json: %s")
+		}
+	default:
+		return fmt.Errorf("unsupported version %d", keyVersion)
 	}
 
 	// Write key to the file with owner read/write permission.
@@ -44,10 +95,7 @@ func genEncryptionKey(encryptionKeyPath string, aesSize int, overwriteKey bool) 
 	if err != nil {
 		return err
 	}
-	n, err := f.Write(b)
-	if err == nil && n < len(b) {
-		err = io.ErrShortWrite
-	}
+	_, err = f.Write(b)
 	if err1 := f.Close(); err == nil {
 		err = err1
 	}
@@ -68,7 +116,7 @@ The resulting key file will be 32 bytes (random key ID) + key_size in bytes.
 	RunE: func(cmd *cobra.Command, args []string) error {
 		encryptionKeyPath := args[0]
 
-		err := genEncryptionKey(encryptionKeyPath, aesSizeFlag, overwriteKeyFlag)
+		err := genEncryptionKey(encryptionKeyPath, aesSizeFlag, overwriteKeyFlag, keyVersionFlag)
 
 		if err != nil {
 			return err
@@ -86,4 +134,6 @@ func init() {
 		"AES key size for encryption at rest (one of: 128, 192, 256)")
 	genEncryptionKeyCmd.PersistentFlags().BoolVar(&overwriteKeyFlag, "overwrite", false,
 		"Overwrite key if it exists")
+	genEncryptionKeyCmd.PersistentFlags().IntVar(&keyVersionFlag, "version", 1,
+		"Encryption format version (1 or 2)")
 }
---
+++ pkg/ccl/storageccl/engineccl/enginepbccl/key_registry.go
@@ -0,0 +1,50 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package enginepbccl
+
+import fmt "fmt"
+
+// JWKAlgorithm returns the JWK algorithm name for this EncryptionType.
+func (e EncryptionType) JWKAlgorithm() (string, error) {
+	switch e {
+	case EncryptionType_AES128_CTR:
+		return "cockroach-aes-128-ctr-v1", nil
+	case EncryptionType_AES192_CTR:
+		return "cockroach-aes-192-ctr-v1", nil
+	case EncryptionType_AES256_CTR:
+		return "cockroach-aes-256-ctr-v1", nil
+
+	case EncryptionType_AES_128_CTR_V2:
+		return "cockroach-aes-128-ctr-v2", nil
+	case EncryptionType_AES_192_CTR_V2:
+		return "cockroach-aes-192-ctr-v2", nil
+	case EncryptionType_AES_256_CTR_V2:
+		return "cockroach-aes-192-ctr-v2", nil
+	}
+	return "", fmt.Errorf("unknown EncryptionType %d", e)
+}
+
+// EncryptionTypeFromJWKAlgorithm returns the EncryptionType for the given algorithm.
+func EncryptionTypeFromJWKAlgorithm(s string) (EncryptionType, error) {
+	switch s {
+	case "cockroach-aes-128-ctr-v1":
+		return EncryptionType_AES128_CTR, nil
+	case "cockroach-aes-192-ctr-v1":
+		return EncryptionType_AES192_CTR, nil
+	case "cockroach-aes-256-ctr-v1":
+		return EncryptionType_AES256_CTR, nil
+	case "cockroach-aes-128-ctr-v2":
+		return EncryptionType_AES_128_CTR_V2, nil
+	case "cockroach-aes-192-ctr-v2":
+		return EncryptionType_AES_192_CTR_V2, nil
+	case "cockroach-aes-256-ctr-v2":
+		return EncryptionType_AES_256_CTR_V2, nil
+	}
+	return 0, fmt.Errorf("unknown JWK algorithm name %s", s)
+}
