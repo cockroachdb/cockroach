2a7742d1621d4e5746e003088bb58770b7834fff
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-03-05 19:10:19 -0800

    sql: clean up txn fingerprint ID cache a bit
    This commit cleans up the txn fingerprint ID cache in the following
    manner:
    - it properly performs the memory accounting for each id. Previously, we
    didn't connect the account to actual monitor (it was uninitialized) and
    we only grew by 1 byte (which is significant underestimate). We now
    remove unused monitor and create an account bound to the session
    monitor.
    - we now only store the id in the "key" part of the cache entry
    (previously we stored it also in "value" which was redundant).
    
    The main motivation for this change was reducing the memory leak that is
    (hopefully) fixed in the following commit.
    
    Release note: None

--- pkg/sql/conn_executor.go
+++ pkg/sql/conn_executor.go
@@ -1059,6 +1059,7 @@ func (s *Server) newConnExecutor(
 		memMetrics.TxnMaxBytesHist,
 		-1 /* increment */, noteworthyMemoryUsageBytes, s.cfg.Settings,
 	)
+	txnFingerprintIDCacheAcc := sessionMon.MakeBoundAccount()
 
 	nodeIDOrZero, _ := s.cfg.NodeInfo.NodeID.OptionalNodeID()
 	ex := &connExecutor{
@@ -1100,7 +1101,8 @@ func (s *Server) newConnExecutor(
 		indexUsageStats:           s.indexUsageStats,
 		txnIDCacheWriter:          s.txnIDCache,
 		totalActiveTimeStopWatch:  timeutil.NewStopWatch(),
-		txnFingerprintIDCache:     NewTxnFingerprintIDCache(s.cfg.Settings, sessionRootMon),
+		txnFingerprintIDCache:     NewTxnFingerprintIDCache(ctx, s.cfg.Settings, &txnFingerprintIDCacheAcc),
+		txnFingerprintIDAcc:       &txnFingerprintIDCacheAcc,
 	}
 
 	ex.state.txnAbortCount = ex.metrics.EngineMetrics.TxnAbortCount
@@ -1308,6 +1310,7 @@ func (ex *connExecutor) close(ctx context.Context, closeType closeType) {
 	ex.mu.IdleInSessionTimeout.Stop()
 	ex.mu.IdleInTransactionSessionTimeout.Stop()
 
+	ex.txnFingerprintIDAcc.Close(ctx)
 	if closeType != panicClose {
 		ex.state.mon.Stop(ctx)
 		ex.sessionPreparedMon.Stop(ctx)
@@ -1699,6 +1702,7 @@ type connExecutor struct {
 	// txnFingerprintIDCache is used to track the most recent
 	// txnFingerprintIDs executed in this session.
 	txnFingerprintIDCache *TxnFingerprintIDCache
+	txnFingerprintIDAcc   *mon.BoundAccount
 
 	// totalActiveTimeStopWatch tracks the total active time of the session.
 	// This is defined as the time spent executing transactions and statements.
