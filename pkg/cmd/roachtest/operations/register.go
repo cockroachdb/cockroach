b7cfe48b5b6b3103df4e7daf4becfaaf8bed861c
Bilal Akhtar <bilal@cockroachlabs.com>
2024-03-06 18:25:32 -0500

    roachtest: add operations to add column, indexes
    This change builds on the operation framework added in
    previous changes to add new operations that randomly add
    columns and indexes to an existing cluster.
    
    Epic: none
    
    Release note: None

--- pkg/cmd/roachtest/operations/register.go
+++ pkg/cmd/roachtest/operations/register.go
@@ -14,4 +14,6 @@ import "github.com/cockroachdb/cockroach/pkg/cmd/roachtest/registry"
 
 // RegisterOperations registers all tests to the Registry. This powers `roachtest run-operations`.
 func RegisterOperations(r registry.Registry) {
+	registerAddColumn(r)
+	registerAddIndex(r)
 }
