2308696444c2de71942b6820cfb0b1063c29a3c3
Marcus Gartner <marcus@cockroachlabs.com>
2024-03-07 16:16:24 -0500

    opt: fix row count estimates for inverted filters
    Previously, inverted filter row counts were set to their input's row
    count. This was often an over-estimation because inverted filters
    de-duplicate inverted index tuples from their input by primary key, and
    inverted indexes can contain multiple tuples for each logical row.
    
    This commit fixes this over-estimation by setting the row count of
    inverted filters to the distinct count of the primary key columns from
    the input.
    
    Fixes #120055
    
    Release note (performance improvement): A bug has been fixed that caused
    the optimizer to over-estimate the cost of inverted index scans in some
    cases. Now, plans with inverted index scans should be selected in more
    cases where they are optimal.

--- pkg/sql/opt/memo/statistics_builder.go
+++ pkg/sql/opt/memo/statistics_builder.go
@@ -1166,8 +1166,13 @@ func (sb *statisticsBuilder) buildInvertedFilter(
 
 	// Calculate selectivity and row count
 	// -----------------------------------
-	inputStats := invFilter.Input.Relational().Statistics()
-	s.RowCount = inputStats.RowCount
+	// Inverted filters de-duplicate inverted index tuples by the primary key,
+	// so they produce no more rows than the distinct count of the PK columns.
+	tabID := sb.md.ColumnMeta(invFilter.InvertedColumn).Table
+	pkCols := sb.md.TableMeta(tabID).IndexKeyColumns(0)
+	keyStats := sb.colStatFromChild(pkCols, invFilter, 0 /* childIdx */)
+	s.RowCount = keyStats.DistinctCount
+
 	corr := sb.correlationFromMultiColDistinctCounts(constrainedCols, invFilter, s)
 	s.ApplySelectivity(sb.selectivityFromConstrainedCols(constrainedCols, histCols, invFilter, s, corr))
 	s.ApplySelectivity(sb.selectivityFromNullsRemoved(invFilter, relProps.NotNullCols, constrainedCols))
