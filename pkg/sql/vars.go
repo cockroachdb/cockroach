21c3af6252110e12b2bf64d7d59f3d872c9ace21
Michael Erickson <michae2@cockroachlabs.com>
2024-03-07 22:58:55 -0800

    sql: disallow SET transaction_read_only = false during AOST txn
    Fixes: #44200
    
    Release note (bug fix): Fix a bug in which it was possible to `SET
    transaction_read_only = false` during an AS OF SYSTEM TIME transaction.

--- pkg/sql/vars.go
+++ pkg/sql/vars.go
@@ -1489,8 +1489,7 @@ var varGen = map[string]sessionVar{
 			if err != nil {
 				return err
 			}
-			m.SetReadOnly(b)
-			return nil
+			return m.SetReadOnly(b)
 		},
 		Get: func(evalCtx *extendedEvalContext, _ *kv.Txn) (string, error) {
 			return formatBoolAsPostgresSetting(evalCtx.TxnReadOnly), nil
