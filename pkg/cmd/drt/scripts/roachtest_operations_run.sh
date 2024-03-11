7945984987cfde80500bacc316546b2bdf4d1fbe
Adam Storm <storm@cockroachlabs.com>
2024-03-11 12:11:15 -0400

    test: First drop of DRT scripts
    Previously, all of the DRT scripts were either stored in private gists, or on
    the workload node of the cluster. This commit properly stores the collection of
    scripts in the repo.
    
    Release note: None
    
    Epic: none

---
+++ pkg/cmd/drt/scripts/roachtest_operations_run.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+cd ~
+
+while true; do
+  ./roachtest-operations run-operation ".*" --certs-dir ./certs --cluster "cct-232" --cockroach-binary "cockroach" --virtual-cluster "application" | tee -a roachtest_ops.log
+  sleep 10
+done
---
+++ pkg/cmd/drt/scripts/stop_all_workloads.sh
@@ -0,0 +1,4 @@
+sudo systemctl stop cct_schemachange
+sudo systemctl stop cct_kv
+sudo systemctl stop cct_tpcc
+sudo systemctl stop cct_tpcc_drop
