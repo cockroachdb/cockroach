8dd42891c04785f4fd3b10c67508b8f166f6ed9a
Adam Storm <storm@cockroachlabs.com>
2024-03-14 09:10:37 -0400

    drt: cleanup workload runner scripts
    Cleanup the workload runner scripts to:
    
    - Add the chaos test to systemd-run
    - Fix a few errors in cct_tpcc_drop
    
    Release note: none
    Epic: none

--- pkg/cmd/drt/scripts/start_all_workloads.sh
+++ pkg/cmd/drt/scripts/start_all_workloads.sh
@@ -2,3 +2,4 @@ sudo systemd-run --working-directory=/home/ubuntu --service-type exec --collect 
 sudo systemd-run --working-directory=/home/ubuntu --service-type exec --collect --unit cct_tpcc ./tpcc_run.sh
 sudo systemd-run --working-directory=/home/ubuntu --service-type exec --collect --unit cct_tpcc_drop ./cct_tpcc_drop.sh
 sudo systemd-run --working-directory=/home/ubuntu --service-type exec --collect --unit cct_schemachange ./schemachange_run.sh
+sudo systemd-run --working-directory=/home/ubuntu --service-type exec --uid=1000 --gid=1000 --collect --unit chaos_test ./chaos_helper.sh
