4c08dd277097d5c8ef91f1baeb8d46ffa44da5f3
Michael Erickson <michae2@cockroachlabs.com>
2024-03-15 13:01:17 -0700

    opt/memo: add tests for memo.(*statisticsBuilder).colStatTopK
    PR #120448 added the missing colStatTopK function to statistics builder,
    but did not include tests. This commit adds some opttests using exprgen.
    
    Epic: None
    Release note: None

--- pkg/sql/opt/optgen/exprgen/private.go
+++ pkg/sql/opt/optgen/exprgen/private.go
@@ -126,7 +126,9 @@ func (eg *exprGen) convertPrivateFieldValue(
 	if res := eg.castToDesiredType(value, fieldType); res != nil {
 		return res
 	}
-	panic(errorf("invalid value for %s.%s: %v", privType, fieldName, value))
+	panic(errorf(
+		"invalid value for %s.%s (%v): %v (%T)", privType, fieldName, fieldType, value, value,
+	))
 }
 
 // addTable resolves the given table name and adds the table to the metadata.
