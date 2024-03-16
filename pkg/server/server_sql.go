eeda7fc3e16ef6dbbed0b5ed660f3c5b5b7906be
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-03-16 13:33:46 -0700

    WIP on stopping temp storage disk monitor

--- pkg/server/server_sql.go
+++ pkg/server/server_sql.go
@@ -854,6 +854,9 @@ func newSQLServer(ctx context.Context, cfg sqlServerArgs) (*SQLServer, error) {
 		RootSQLMemoryPoolSize:    cfg.MemoryPoolSize,
 	}
 	cfg.TempStorageConfig.Mon.SetMetrics(distSQLMetrics.CurDiskBytesCount, distSQLMetrics.MaxDiskBytesHist)
+	cfg.stopper.AddCloser(stop.CloserFn(func() {
+		cfg.TempStorageConfig.Mon.EmergencyStop(ctx)
+	}))
 	if distSQLTestingKnobs := cfg.TestingKnobs.DistSQL; distSQLTestingKnobs != nil {
 		distSQLCfg.TestingKnobs = *distSQLTestingKnobs.(*execinfra.TestingKnobs)
 	}
