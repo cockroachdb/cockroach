6f233fd7f12392ee71561c2e2b89d0be80a37e8b
Ricky Stewart <ricky@cockroachlabs.com>
2024-03-19 14:57:58 -0500

    workflows: tag `cockroach` builds for integration tests
    ... with the tag `integration-test-artifact-build`. We do this to track
    how long it takes to build these artifacts specifically.
    
    Epic: CRDB-8308
    Release note: None

--- build/github/examples-orms.sh
+++ build/github/examples-orms.sh
@@ -3,7 +3,10 @@
 set -euxo pipefail
 
 pushd cockroach
-bazel build //pkg/cmd/cockroach-short --config crosslinux --jobs 100 $(./build/github/engflow-args.sh)
+bazel build //pkg/cmd/cockroach-short \
+      --config crosslinux --jobs 100 \
+      --bes_keywords integration-test-artifact-build \
+      $(./build/github/engflow-args.sh)
 cp _bazel/bin/pkg/cmd/cockroach-short/cockroach-short_/cockroach-short ../examples-orms/cockroach
 # We need Go in the `PATH`.
 export PATH=$(dirname $(bazel run @go_sdk//:bin/go --run_under=realpath)):$PATH
