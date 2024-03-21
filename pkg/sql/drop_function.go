4ea03c40581c5669218a1d6826c3e89edcd9dbde
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-03-20 18:23:27 -0700

    sql: implement SQL-compliant DROP PROCEDURE
    This commit extends the overload resolution logic to match the
    SQL-compliant behavior of PG on DROP PROCEDURE when no parameter has the
    parameter class specified (i.e. all parameters use the "default" class).
    In such a scenario, when matching the requested signature to the
    overload PG applies two matching approaches:
    - the main one - based on the input types
    - the SQL-compliant one where all parameters are considered.
    
    For context, SQL standard allows function and procedure overloads only
    when they have different number of parameters (regardless of their
    class), but PG allows more - namely the input parameters must be
    different between overloads. To partially bring the PG behavior closer
    to the spec they implemented this two-way resolution approach in
    https://github.com/postgres/postgres/commit/e56bce5d43789cce95d099554ae9593ada92b3b7
    which can lead to ambiguity errors in some rare cases.
    
    Release note: None

--- pkg/sql/drop_function.go
+++ pkg/sql/drop_function.go
@@ -61,7 +61,7 @@ func (p *planner) DropFunction(ctx context.Context, n *tree.DropRoutine) (ret pl
 	}
 	fnResolved := intsets.MakeFast()
 	for _, fn := range n.Routines {
-		ol, err := p.matchRoutine(ctx, &fn, !n.IfExists, routineType)
+		ol, err := p.matchRoutine(ctx, &fn, !n.IfExists, routineType, true /* inDropContext */)
 		if err != nil {
 			return nil, err
 		}
@@ -123,7 +123,11 @@ func (n *dropFunctionNode) Close(ctx context.Context)           {}
 // returned if the function is not found. An error is also returning if a
 // builtin function is matched.
 func (p *planner) matchRoutine(
-	ctx context.Context, routineObj *tree.RoutineObj, required bool, routineType tree.RoutineType,
+	ctx context.Context,
+	routineObj *tree.RoutineObj,
+	required bool,
+	routineType tree.RoutineType,
+	inDropContext bool,
 ) (*tree.QualifiedOverload, error) {
 	path := p.CurrentSearchPath()
 	unresolvedName := routineObj.FuncName.ToUnresolvedObjectName().ToUnresolvedName()
@@ -141,11 +145,7 @@ func (p *planner) matchRoutine(
 		return nil, err
 	}
 
-	signatureTypes, err := routineObj.SignatureTypes(ctx, p)
-	if err != nil {
-		return nil, err
-	}
-	ol, err := fnDef.MatchOverload(signatureTypes, routineObj.FuncName.Schema(), &path, routineType)
+	ol, err := fnDef.MatchOverload(ctx, p, routineObj, &path, routineType, inDropContext)
 	if err != nil {
 		if !required && errors.Is(err, tree.ErrRoutineUndefined) {
 			return nil, nil
