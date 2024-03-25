40d677376e29e5bc713aea24ec3c4b05a87eb7d7
Renato Costa <renato@cockroachlabs.com>
2024-03-25 16:26:25 -0400

    build: validate release branch before running roachtest nightly
    This introduces some validation to the `roachtest_nightly_impl` script
    to achieve two goals:
    
    * **Reduce the number of tests run every night**. We have seen a quick
    increase in the number of legitimate release branches created recently
    due to the baking period policy and perhaps a more quick paced patch
    release schedule for existing release series. Currently, we have 7
    active release branches, with a new one being created soon. This
    volume of concurrent cluster creation leads to a lot of cluster
    creation errors ("zone exhausted" errors on GCE) and sometimes DNS
    errors as well. In this commit, we implement a policy where `-rc`
    branches only run 40% of the test suite by default.
    
    * **Avoid running the full suite on accidentally created release
    branches**. It is not immediately obvious that creating a branch with
    a `release-` prefix leads to a roachtest nightly build being created
    for that branch. The roachtest script is updated in this commit to
    check for these "invalid" release branches and abort execution
    early. It is still possible to force execution by setting an
    environment variable.
    
    Epic: none
    
    Release note: None

--- build/teamcity/cockroach/nightlies/roachtest_nightly_impl.sh
+++ build/teamcity/cockroach/nightlies/roachtest_nightly_impl.sh
@@ -19,9 +19,45 @@ $root/build/teamcity/cockroach/nightlies/roachtest_compile_bits.sh $arch
 artifacts=/artifacts
 source $root/build/teamcity/util/roachtest_util.sh
 
+# Standard release branches are in the format `release-24.1` for the
+# 24.1 release, for example.
+release_branch_regex="^release-[0-9][0-9]\.[0-9]"
+
+if [[ "${TC_BUILD_BRANCH}" == "master" ]]; then
+  # We default to running all tests on master, unless explicitly
+  # overriden in the TeamCity UI.
+  select_probability="${SELECT_PROBABILITY:-1.0}"
+elif [[ "${TC_BUILD_BRANCH}" =~ ${release_branch_regex}$ ]]; then
+  # Same for release branches.
+  select_probability="${SELECT_PROBABILITY:-1.0}"
+elif [[ "${TC_BUILD_BRANCH}" =~ ${release_branch_regex}\.[0-9]{1,2}-rc$ ]]; then
+  # If we are running an `-rc` branch for a specific patch release
+  # (for instance, `release-24.1.1-rc`), then only run 40% of the test
+  # suite by default. This is to avoid a high volume of concurrent
+  # cluster creation attempts across all existing release branches.
+  #
+  # NOTE: in the future, instead of choosing the tests randomly as we
+  # do here, we plan to utilize a smarter test selection strategy (see
+  # #119630).
+  select_probability="${SELECT_PROBABILITY:-0.4}"
+elif [[ "${TC_BUILD_BRANCH}" =~ ^release- && "${ROACHTEST_FORCE_RUN_INVALID_RELEASE_BRANCH}" != "true" ]]; then
+  # The only valid release branches are the ones handled above. That
+  # said, from time to time we might have cases where a branch with
+  # the `release-` prefix is created by accident, activating the
+  # TeamCity trigger for the Roachtest Nightly build. We abort
+  # execution in these cases to avoid unnecessarily running a test
+  # suite on these branches.
+  echo "Refusing to run roachtest nightly suite on invalid release branch: ${TC_BUILD_BRANCH}."
+  exit 1
+else
+  # Use a 0.1 default in all other branches, to reduce the chances of
+  # an accidental full-suite run on feature branches.
+  select_probability="${SELECT_PROBABILITY:-0.1}"
+fi
+
 build/teamcity-roachtest-invoke.sh \
   --metamorphic-encryption-probability=0.5 \
-  --select-probability="${SELECT_PROBABILITY:-1.0}" \
+  --select-probability="${select_probability}" \
   --cloud="${CLOUD}" \
   --count="${COUNT-1}" \
   --parallelism="${PARALLELISM}" \
