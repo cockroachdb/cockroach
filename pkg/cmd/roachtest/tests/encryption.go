d9e27dc5c7dc53c2aef61f3a24e686565f08d65d
Ben Darnell <ben@cockroachlabs.com>
2024-03-26 15:43:36 +0530

    engineccl: Fix data key generation for v2 encryption
    Prior tests of v2 encryption were too low-level and did not cover the data key generation.
    The new roachtest provides end-to-end coverage.
    
    This fixes a bug in not-yet-released functionality
    
    Epic: none
    Release note: none

--- pkg/cmd/roachtest/tests/encryption.go
+++ pkg/cmd/roachtest/tests/encryption.go
@@ -13,85 +13,88 @@ package tests
 import (
 	"context"
 	"fmt"
+	"strings"
 
+	"github.com/cockroachdb/cockroach/pkg/ccl/storageccl/engineccl/enginepbccl"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/cluster"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/option"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/registry"
+	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/roachtestutil"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/test"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
+	"github.com/cockroachdb/cockroach/pkg/server/serverpb"
+	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
 	"github.com/cockroachdb/errors"
 )
 
 func registerEncryption(r registry.Registry) {
-	// Note that no workload is run in this roachtest because kv roachtest
-	// ideally runs with encryption turned on to see the performance impact and
-	// to test the correctness of encryption at rest.
-	runEncryption := func(ctx context.Context, t test.Test, c cluster.Cluster) {
+	// Note that no workload is run in this roachtest: it's testing basic operations
+	// and not performance. Performance tests of encryption are performed in other
+	// tests based on the TestSpec.EncryptionSupport field.
+	runEncryptionRotation := func(ctx context.Context, t test.Test, c cluster.Cluster) {
 		nodes := c.Spec().NodeCount
-		c.Start(ctx, t.L(), option.DefaultStartOpts(), install.MakeClusterSettings(), c.Range(1, nodes))
+		httpClient := roachtestutil.DefaultHTTPClient(c, t.L())
 
-		// Check that /_status/stores/local endpoint has encryption status.
-		adminAddrs, err := c.InternalAdminUIAddr(ctx, t.L(), c.Range(1, nodes))
-		if err != nil {
-			t.Fatal(err)
-		}
-		for _, addr := range adminAddrs {
-			if err := c.RunE(ctx, option.WithNodes(c.Node(nodes)), fmt.Sprintf(`curl http://%s/_status/stores/local | (! grep '"encryptionStatus": null')`, addr)); err != nil {
-				t.Fatalf("encryption status from /_status/stores/local endpoint is null")
-			}
-		}
-
-		for i := 1; i <= nodes; i++ {
-			if err := c.StopCockroachGracefullyOnNode(ctx, t.L(), i); err != nil {
-				t.Fatal(err)
-			}
-		}
-
-		// Restart node with encryption turned on to verify old key works.
-		c.Start(ctx, t.L(), option.DefaultStartOpts(), install.MakeClusterSettings(), c.Range(1, nodes))
-
-		testCLIGenKey := func(size int) error {
-			// Generate encryption store key through `./cockroach gen encryption-key -s=size aes-size.key`.
-			if err := c.RunE(ctx, option.WithNodes(c.Node(nodes)), fmt.Sprintf("./cockroach gen encryption-key -s=%[1]d aes-%[1]d.key", size)); err != nil {
-				return errors.Wrapf(err, "failed to generate AES key with size %d through CLI", size)
-			}
-
-			// Check the size of generated aes key has expected size.
-			if err := c.RunE(ctx, option.WithNodes(c.Node(nodes)), fmt.Sprintf(`size=$(wc -c <"aes-%d.key"); test $size -eq %d && exit 0 || exit 1`, size, 32+size/8)); err != nil {
-				return errors.Errorf("expected aes-%d.key has size %d bytes, but got different size", size, 32+size/8)
-			}
-
-			return nil
-		}
-
-		// Check that CLI can generated key with specified sizes.
+		// Starting with two copies of "plain" lets us avoid special-casing the first key in our rotation test.
+		keys := []string{"plain", "plain"}
+		// Rotate through multiple keys with different key sizes and both implementation versions.
 		for _, size := range []int{128, 192, 256} {
-			if err := testCLIGenKey(size); err != nil {
-				t.Fatal(err)
+			for _, version := range []int{1, 2} {
+				filename := fmt.Sprintf("aes-%d-v%d.key", size, version)
+				if err := c.RunE(ctx, option.WithNodes(c.Node(nodes)), fmt.Sprintf("./cockroach gen encryption-key -s %d --version=%d %s",
+					size, version, filename)); err != nil {
+					t.Fatal(errors.Wrapf(err, "failed to generate AES key"))
+				}
+				keys = append(keys, filename)
 			}
 		}
 
-		// Check that CLI returns error if AES key size is incorrect.
-		for _, size := range []int{20, 88, 90} {
-			// Cannot check for specific error message from CLI because command
-			// is run through roachprod and it will return exist status 1.
-			if err := testCLIGenKey(size); err == nil {
-				t.Fatalf("expected error from CLI gen encryption-key, but got nil")
+		for keyIdx := 1; keyIdx < len(keys); keyIdx++ {
+			opts := option.DefaultStartOpts()
+			opts.RoachprodOpts.ExtraArgs = []string{fmt.Sprintf("--enterprise-encryption=path=data,key=%s,old-key=%s", keys[keyIdx], keys[keyIdx-1])}
+			c.Start(ctx, t.L(), opts, install.MakeClusterSettings(), c.Range(1, nodes))
+			WaitForReady(ctx, t, c, c.Range(1, nodes))
+
+			// Check that /_status/stores/local endpoint has encryption status.
+			adminAddrs, err := c.ExternalAdminUIAddr(ctx, t.L(), c.Range(1, nodes))
+			if err != nil {
+				t.Fatal(err)
+			}
+			for _, addr := range adminAddrs {
+				url := fmt.Sprintf("http://%s/_status/stores/local", addr)
+				var storesResponse serverpb.StoresResponse
+				if err := httpClient.GetJSON(ctx, url, &storesResponse); err != nil {
+					t.Fatal(err)
+				}
+				var encryptionStatus enginepbccl.EncryptionStatus
+				if err := protoutil.Unmarshal(storesResponse.Stores[0].EncryptionStatus, &encryptionStatus); err != nil {
+					t.Fatal(err)
+				}
+				if !strings.HasSuffix(encryptionStatus.ActiveStoreKey.Source, keys[keyIdx]) {
+					t.Fatalf("expected active key %s, but found %s", keys[keyIdx], encryptionStatus.ActiveStoreKey.Source)
+				}
+			}
+
+			for i := 1; i <= nodes; i++ {
+				if err := c.StopCockroachGracefullyOnNode(ctx, t.L(), i); err != nil {
+					t.Fatal(err)
+				}
 			}
 		}
 	}
 
 	for _, n := range []int{1} {
 		r.Add(registry.TestSpec{
-			Name:              fmt.Sprintf("encryption/nodes=%d", n),
-			EncryptionSupport: registry.EncryptionAlwaysEnabled,
+			Name: fmt.Sprintf("encryption/rotation/nodes=%d", n),
+			// This test controls encryption manually.
+			EncryptionSupport: registry.EncryptionAlwaysDisabled,
 			Leases:            registry.MetamorphicLeases,
 			Owner:             registry.OwnerStorage,
 			Cluster:           r.MakeClusterSpec(n),
 			CompatibleClouds:  registry.AllExceptAWS,
 			Suites:            registry.Suites(registry.Nightly),
 			Run: func(ctx context.Context, t test.Test, c cluster.Cluster) {
-				runEncryption(ctx, t, c)
+				runEncryptionRotation(ctx, t, c)
 			},
 		})
 	}
