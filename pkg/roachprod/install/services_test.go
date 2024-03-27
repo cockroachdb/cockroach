fe0cfc71fc0595b2ae2d36c37b3f486477473a30
Herko Lategan <herko@cockroachlabs.com>
2024-03-27 17:01:22 +0000

    roachprod: wider test coverage for services and DNS
    Previously, it was difficult to test any of the service or DNS implementations
    due to interactions with external CLI tools (`gcloud`).
    
    This change implements a Test DNS Server which emulates `gcloud` and the server
    it would communicate with by keeping an in-memory store of DNS records.
    
    New tests have been added to validate that registering, and deleting multiple
    services do not cause issues with the DNS cache or service itself. Additionally,
    a small test has been added to ensure `maybeRegisterServices` only tries to
    register new services if no other services exist.
    
    Epic: None
    Release Note: None

--- pkg/roachprod/install/services_test.go
+++ pkg/roachprod/install/services_test.go
@@ -12,13 +12,21 @@ package install
 
 import (
 	"context"
+	"fmt"
+	"math/rand"
 	"net"
+	"reflect"
 	"sort"
 	"testing"
+	"testing/quick"
 
 	"github.com/cockroachdb/cockroach/pkg/roachprod/cloud"
+	"github.com/cockroachdb/cockroach/pkg/roachprod/logger"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/vm"
+	"github.com/cockroachdb/cockroach/pkg/roachprod/vm/gce"
+	"github.com/cockroachdb/cockroach/pkg/roachprod/vm/gce/testutils"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/vm/local"
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/stretchr/testify/require"
 )
 
@@ -93,4 +101,150 @@ func TestServiceNameComponents(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, "tenant-100", virtualClusterName)
 	require.Equal(t, ServiceTypeSQL, serviceType)
+}
+
+func TestMaybeRegisterServices(t *testing.T) {
+	ctx := context.Background()
+	rng, _ := randutil.NewTestRand()
+	dnsServer, _, providerName := testutils.ProviderWithTestDNSServer(rng)
+
+	// Create a VM and a cluster with 3 nodes.
+	makeVM := func(clusterName string, i int) vm.VM {
+		return vm.VM{
+			Provider:    providerName,
+			DNSProvider: providerName,
+			PublicDNS:   fmt.Sprintf("%s.%s", vm.Name(clusterName, i), gce.Subdomain),
+		}
+	}
+	makeCluster := func() *SyncedCluster {
+		clusterName := fmt.Sprintf("cluster-%d", rng.Uint32())
+		c := &SyncedCluster{
+			Cluster: cloud.Cluster{
+				Name: clusterName,
+			},
+		}
+		nodeCount := 3
+		for i := 1; i <= nodeCount; i++ {
+			c.VMs = append(c.VMs, makeVM(c.Name, i))
+			c.Nodes = append(c.Nodes, Node(i))
+		}
+		return c
+	}
+
+	// Create a portFunc that returns a list of ports starting from 500.
+	portFunc := func(ctx context.Context, l *logger.Logger, node Node, startPort, count int) ([]int, error) {
+		ports := make([]int, count)
+		for i := 0; i < count; i++ {
+			ports[i] = 500 + i
+		}
+		return ports, nil
+	}
+
+	c := makeCluster()
+	startOpts := StartOpts{
+		Target:      StartDefault,
+		AdminUIPort: 22222,
+	}
+
+	// Register services for the first time.
+	err := c.maybeRegisterServices(ctx, nilLogger(), startOpts, portFunc)
+	require.NoError(t, err)
+
+	// Expect only 2 create call to the DNS server, one for the SQL service and one for the AdminUI service.
+	require.Equal(t, 2, dnsServer.Metrics().CreateCalls)
+
+	// Register services again, this time no new services should be created, the existing services should be used.
+	err = c.maybeRegisterServices(ctx, nilLogger(), startOpts, portFunc)
+	require.NoError(t, err)
+
+	// Expect the create call count to remain the same.
+	require.Equal(t, 2, dnsServer.Metrics().CreateCalls)
+}
+
+func TestMultipleRegistrations(t *testing.T) {
+	ctx := context.Background()
+	rng, _ := randutil.NewTestRand()
+	_, testDNS, providerName := testutils.ProviderWithTestDNSServer(rng)
+
+	generator := func(values []reflect.Value, rng *rand.Rand) {
+		makeVM := func(clusterName string, i int) vm.VM {
+			return vm.VM{
+				Provider:    providerName,
+				DNSProvider: providerName,
+				PublicDNS:   fmt.Sprintf("%s.%s", vm.Name(clusterName, i), gce.Subdomain),
+			}
+		}
+		clusterName := fmt.Sprintf("cluster-%d", rng.Uint32())
+		c := &SyncedCluster{
+			Cluster: cloud.Cluster{
+				Name: clusterName,
+			},
+		}
+		nodeCount := rng.Intn(5) + 3
+		for i := 1; i <= nodeCount; i++ {
+			c.VMs = append(c.VMs, makeVM(c.Name, i))
+			c.Nodes = append(c.Nodes, Node(i))
+		}
+		randomServiceType := func(rng *rand.Rand) ServiceType {
+			serviceTypes := []ServiceType{ServiceTypeSQL, ServiceTypeUI}
+			return serviceTypes[rng.Intn(len(serviceTypes))]
+		}
+		randomServiceMode := func(rng *rand.Rand) ServiceMode {
+			serviceModes := []ServiceMode{ServiceModeExternal, ServiceModeShared}
+			return serviceModes[rng.Intn(len(serviceModes))]
+		}
+		randomServiceName := func(rng *rand.Rand) string {
+			return fmt.Sprintf("service-%d", rng.Intn(5))
+		}
+
+		deleteCount := rng.Intn(3) + 1
+		deletePoints := make(map[int]struct{})
+		for len(deletePoints) < deleteCount {
+			deletePoints[rng.Intn(nodeCount)] = struct{}{}
+		}
+
+		servicesToRegister := make([][]ServiceDesc, rng.Intn(50)+10)
+		for j := 0; j < len(servicesToRegister); j++ {
+			if _, ok := deletePoints[j]; ok {
+				// An empty slice means to delete all DNS records.
+				continue
+			}
+			serviceName := randomServiceName(rng)
+			groupSize := rng.Intn(20) + 3
+			for i := 0; i < groupSize; i++ {
+				// Register a random number of services.
+				// Duplicate services are allowed.
+				servicesToRegister[j] = append(servicesToRegister[j], ServiceDesc{
+					VirtualClusterName: serviceName,
+					ServiceType:        randomServiceType(rng),
+					ServiceMode:        randomServiceMode(rng),
+					Node:               Node(rng.Intn(len(c.Nodes)) + 1),
+					Port:               500 + rng.Intn(5),
+					Instance:           rng.Intn(2),
+				})
+			}
+		}
+		values[0] = reflect.ValueOf(c)
+		values[1] = reflect.ValueOf(servicesToRegister)
+	}
+
+	// Verify no errors occur when registering multiple services.
+	verify := func(c *SyncedCluster, servicesToRegister [][]ServiceDesc) bool {
+		for _, services := range servicesToRegister {
+			if len(services) == 0 {
+				err := testDNS.DeleteRecordsBySubdomain(ctx, c.Name)
+				require.NoError(t, err)
+				continue
+			}
+			err := c.RegisterServices(ctx, services)
+			require.NoError(t, err)
+		}
+		return true
+	}
+
+	require.NoError(t, quick.Check(verify, &quick.Config{
+		MaxCount: 150,
+		Rand:     rng,
+		Values:   generator,
+	}))
 }
---
+++ pkg/roachprod/vm/gce/testutils/dns_server.go
@@ -0,0 +1,180 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package testutils
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"os/exec"
+	"strings"
+
+	"github.com/cockroachdb/cockroach/pkg/roachprod/vm"
+	"github.com/cockroachdb/cockroach/pkg/roachprod/vm/gce"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/errors"
+)
+
+type testProvider struct {
+	vm.Provider
+	vm.DNSProvider
+}
+
+type testDNSRecord struct {
+	Name       string   `json:"name"`
+	Kind       string   `json:"kind"`
+	RecordType string   `json:"type"`
+	TTL        int      `json:"ttl"`
+	RRDatas    []string `json:"rrdatas"`
+}
+
+type Metrics struct {
+	ListCalls, CreateCalls, UpdateCalls, DeleteCalls int
+}
+
+func createTestJSONRecord(records []testDNSRecord) ([]byte, error) {
+	return json.Marshal(records)
+}
+
+// TestDNSServer is a DNS "server" that can be used for testing purposes. It
+// emulates the `gcloud dns` command responses, by storing the records in memory
+// and providing the same output as the `gcloud dns` command. It will fail in
+// the same way as the `gcloud dns` command if a record already exists or does
+// not exist, depending on the command.
+type TestDNSServer interface {
+	Metrics() Metrics
+	Count() int
+}
+
+type testDNSServer struct {
+	mu      syncutil.Mutex
+	records map[string]vm.DNSRecord
+	metrics Metrics
+}
+
+func (t *testDNSServer) storedName(name string) string {
+	// Google DNS always returns the name with a trailing ".",
+	// so we emulate that here.
+	if !strings.HasSuffix(name, ".") {
+		name += "."
+	}
+	return name
+}
+
+func (t *testDNSServer) list(filter string) ([]byte, error) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	t.metrics.ListCalls++
+	records := make([]testDNSRecord, 0)
+	for name, r := range t.records {
+		if filter == "" || strings.Contains(name, filter) {
+			records = append(records, testDNSRecord{
+				Name:       r.Name,
+				Kind:       "dns#resourceRecordSet",
+				RecordType: string(r.Type),
+				TTL:        vm.DNSRecordTTL,
+				RRDatas:    strings.Split(r.Data, ","),
+			})
+		}
+	}
+	return createTestJSONRecord(records)
+}
+
+func (t *testDNSServer) create(name string, data string) ([]byte, error) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	t.metrics.CreateCalls++
+	if _, ok := t.records[t.storedName(name)]; ok {
+		return nil, errors.Newf("can't create record %s already exists", name)
+	}
+	// Keep the data grouped to avoid having to join it later.
+	t.records[t.storedName(name)] = vm.DNSRecord{
+		Type: vm.SRV,
+		Name: t.storedName(name),
+		Data: data,
+	}
+	return []byte{}, nil
+}
+
+func (t *testDNSServer) update(name string, data string) ([]byte, error) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	t.metrics.UpdateCalls++
+	if _, ok := t.records[t.storedName(name)]; !ok {
+		return nil, errors.Newf("can't update record %s it does not exist", name)
+	}
+	t.records[t.storedName(name)] = vm.DNSRecord{
+		Type: vm.SRV,
+		Name: t.storedName(name),
+		Data: data,
+	}
+	return []byte{}, nil
+}
+
+func (t *testDNSServer) delete(name string) ([]byte, error) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	t.metrics.DeleteCalls++
+	if _, ok := t.records[t.storedName(name)]; !ok {
+		return nil, errors.Newf("can't delete record it does not exist", name)
+	}
+	delete(t.records, t.storedName(name))
+	return []byte{}, nil
+}
+
+func (t *testDNSServer) Count() int {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	return len(t.records)
+}
+
+func (t *testDNSServer) Metrics() Metrics {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	return t.metrics
+}
+
+func (t *testDNSServer) execFunc(cmd *exec.Cmd) ([]byte, error) {
+	getArg := func(args []string, arg string) string {
+		for i, a := range args {
+			if a == arg {
+				return args[i+1]
+			}
+		}
+		return ""
+	}
+	for _, arg := range cmd.Args {
+		switch arg {
+		case "list":
+			return t.list(getArg(cmd.Args, "--filter"))
+		case "create":
+			return t.create(getArg(cmd.Args, "create"), getArg(cmd.Args, "--rrdatas"))
+		case "update":
+			return t.update(getArg(cmd.Args, "update"), getArg(cmd.Args, "--rrdatas"))
+		case "delete":
+			return t.delete(getArg(cmd.Args, "delete"))
+		}
+	}
+	return nil, errors.New("unknown command")
+}
+
+// ProviderWithTestDNSServer initializes a test DNS server and a DNS capable
+// provider pointing to the test server. It returns the test DNS server, the
+// test DNS provider, and the provider name.
+func ProviderWithTestDNSServer(rng *rand.Rand) (TestDNSServer, vm.DNSProvider, string) {
+	testServer := &testDNSServer{records: make(map[string]vm.DNSRecord)}
+	testDNS := gce.NewDNSProviderWithExec(testServer.execFunc)
+	// Since this is a global variable, we need to make sure the provider name is
+	// unique, in order to avoid conflicts with other tests.
+	providerName := fmt.Sprintf("testProvider-%d", rng.Uint32())
+	vm.Providers[providerName] = &testProvider{DNSProvider: testDNS}
+	return testServer, testDNS, providerName
+}
