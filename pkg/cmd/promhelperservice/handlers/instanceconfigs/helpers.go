63c1e24db3d3327e5af4dff1e40e9a14123e078a
Bhaskarjyoti Bora <bhaskar.bora@cockroachlabs.com>
2024-03-29 12:59:04 +0530

    roachprod: support for dynamic admin url port
    In order to support multiple tenants on the same host, a unique, custom port can be assigned for DefaultAdminUIPort. This breaks because the port is hard-coded in prometheus config for scraping.
    
    The change is to have a http server running in prometheus server that can dynamically update the configuration when a new cluster is brought up. More details of the solution is explained in the page - https://cockroachlabs.atlassian.net/wiki/spaces/~7120207825326fb5e546c194029506f2c5335e/pages/3458531376/Dynamic+Scrape+Configs+on+Prometheus+for+Roachprod
    
    Fixes: #117125
    Epic: none

---
+++ pkg/cmd/promhelperservice/handlers/instanceconfigs/helpers.go
@@ -0,0 +1,91 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package instanceconfigs
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+)
+
+const (
+	// resourceName and version for the API
+	resourceName    = "instance-configs"
+	resourceVersion = "v1"
+	// ClusterIDParam is the url parameter
+	ClusterIDParam = "cluster_id"
+	// defaultInstanceConfigDir is the default instance config directory from where the yml files are created/removed.
+	defaultInstanceConfigDir = "/opt/prom/prometheus/instance-configs"
+
+	instanceConfigFileExt = "yml"
+)
+
+var instanceConfigDir = "/opt/prom/prometheus/instance-configs"
+
+// BuildInstanceConfigUrl returns the url with cluster ID for instance config
+func BuildInstanceConfigUrl() string {
+	return fmt.Sprintf("/%s/%s/{%s}", resourceVersion, resourceName, ClusterIDParam)
+}
+
+// RegisterFlag registers the flags needed by the handler at the application start
+func RegisterFlag() {
+	flag.StringVar(&instanceConfigDir, "instance-config-dir", defaultInstanceConfigDir,
+		"intance config directory for prometheus")
+}
+
+// instanceConfig is the HTTP request received for generating instance config
+type instanceConfig struct {
+	Config string `json:"config"`
+}
+
+// validate validates the instanceConfig
+func (r *instanceConfig) validate() error {
+	if r.Config == "" {
+		return fmt.Errorf("no config available")
+	}
+
+	return nil
+}
+
+// instanceConfigResponse is the response for create instance config
+type instanceConfigResponse struct {
+	ClusterID     string `json:"cluster_id"`
+	FailureReason string `json:"failure_reason,omitempty"`
+}
+
+// getConfig returns the instanceConfig after parsing the request json
+func getConfig(body io.ReadCloser) (*instanceConfig, error) {
+	var insConfigReq instanceConfig
+	if err := json.NewDecoder(body).Decode(&insConfigReq); err != nil {
+		return nil, err
+	}
+	return &insConfigReq, nil
+}
+
+// generateInstanceConfigResponse returns the instanceConfig after parsing the request json
+func generateInstanceConfigResponse(insConfigRes *instanceConfigResponse) ([]byte, error) {
+	var res []byte
+	var err error
+	if res, err = json.Marshal(insConfigRes); err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+// formFailureResponse forms the failure response in bytes
+func formFailureResponse(clusterID, errorMessage string) []byte {
+	response, _ := generateInstanceConfigResponse(&instanceConfigResponse{
+		ClusterID:     clusterID,
+		FailureReason: errorMessage,
+	})
+	return response
+}
---
+++ pkg/cmd/promhelperservice/handlers/instanceconfigs/put_test.go
@@ -0,0 +1,91 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package instanceconfigs
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPutHandler(t *testing.T) {
+	instanceConfigDir = fmt.Sprintf("%s/%s", t.TempDir(), "instance-configs")
+	require.Nil(t, os.Mkdir(instanceConfigDir, 0755))
+	t.Run("invalid request", func(t *testing.T) {
+		rr := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPut,
+			fmt.Sprintf("/%s/%s/%s", resourceVersion, resourceName, "c1"),
+			nil)
+		req.SetPathValue(ClusterIDParam, "c1")
+		PutHandler(rr, req)
+		result := rr.Result()
+		require.Equal(t, http.StatusBadRequest, result.StatusCode)
+		var insConfigRes instanceConfigResponse
+		require.Nil(t, json.NewDecoder(result.Body).Decode(&insConfigRes))
+		require.Equal(t, "c1", insConfigRes.ClusterID)
+		require.Contains(t, "invalid/empty request", insConfigRes.FailureReason)
+	})
+	t.Run("invalid request with missing cluster id", func(t *testing.T) {
+		rr := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPut,
+			fmt.Sprintf("/%s/%s/%s", resourceVersion, resourceName, "c1"),
+			bytes.NewReader([]byte("{}")))
+		PutHandler(rr, req)
+		result := rr.Result()
+		require.Equal(t, http.StatusBadRequest, result.StatusCode)
+		var insConfigRes instanceConfigResponse
+		require.Nil(t, json.NewDecoder(result.Body).Decode(&insConfigRes))
+		require.Empty(t, insConfigRes.ClusterID)
+		require.Contains(t, "could not extract cluster id from URL", insConfigRes.FailureReason)
+	})
+	t.Run("invalid request with missing config", func(t *testing.T) {
+		rr := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPut,
+			fmt.Sprintf("/%s/%s/%s", resourceVersion, resourceName, "c1"),
+			bytes.NewReader([]byte(`{}`)))
+		req.SetPathValue(ClusterIDParam, "c1")
+		PutHandler(rr, req)
+		result := rr.Result()
+		require.Equal(t, http.StatusBadRequest, result.StatusCode)
+		var insConfigRes instanceConfigResponse
+		require.Nil(t, json.NewDecoder(result.Body).Decode(&insConfigRes))
+		require.Equal(t, "c1", insConfigRes.ClusterID)
+		require.Contains(t, "invalid request: no config available", insConfigRes.FailureReason)
+	})
+	t.Run("valid request", func(t *testing.T) {
+		clusterName := fmt.Sprintf("test-cluster_%d", time.Now().UnixNano())
+		fileName := fmt.Sprintf("%s/%s.%s", instanceConfigDir, clusterName, instanceConfigFileExt)
+		rr := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPut,
+			fmt.Sprintf("/%s/%s/%s", resourceVersion, resourceName, clusterName),
+			bytes.NewReader([]byte(`{"config": "config content"}`)))
+		req.SetPathValue(ClusterIDParam, clusterName)
+		PutHandler(rr, req)
+		result := rr.Result()
+		require.Equal(t, http.StatusOK, result.StatusCode)
+		var insConfigRes instanceConfigResponse
+		require.Nil(t, json.NewDecoder(result.Body).Decode(&insConfigRes))
+		require.Equal(t, clusterName, insConfigRes.ClusterID)
+		require.Empty(t, insConfigRes.FailureReason)
+		_, err := os.Stat(fileName)
+		require.Nil(t, err)
+		b, e := os.ReadFile(fileName)
+		require.Nil(t, e)
+		require.Equal(t, `config content`, string(b))
+	})
+}
