6711b511e1c4f050dac7537cc8d0dc7b66a5a388
Rebecca Taft <becca@cockroachlabs.com>
2024-03-30 20:08:57 -0500

    sql: add setting optimizer_use_improved_multi_column_selectivity_estimate
    Informs #121397
    
    Release note (sql change): Added a setting
    optimizer_use_improved_multi_column_selectivity_estimate, which if enabled,
    causes the optimizer to use an improved selectivity estimate for multi-column
    predicates. This setting will default to true on versions 24.2+, and false
    on prior versions.

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -3761,6 +3761,10 @@ func (m *sessionDataMutator) SetOptimizerUseImprovedZigzagJoinCosting(val bool) 
 	m.data.OptimizerUseImprovedZigzagJoinCosting = val
 }
 
+func (m *sessionDataMutator) SetOptimizerUseImprovedMultiColumnSelectivityEstimate(val bool) {
+	m.data.OptimizerUseImprovedMultiColumnSelectivityEstimate = val
+}
+
 // Utility functions related to scrubbing sensitive information on SQL Stats.
 
 // quantizeCounts ensures that the Count field in the
