87c882719621a23b48dc449d38eb89a2b3347f58
Alex Barganier <abarganier@cockroachlabs.com>
2024-04-02 15:42:21 -0400

    eventagg: add FlushTrigger mechanism to MapReduceAggregator
    This patch introduces the FlushTrigger interface, which can be used by
    the MapReduceAggregator to determine when it's time to flush the current
    aggregation.
    
    Along with the interface, an initial implementation is provided called
    `WindowedFlush`. `WindowedFlush` aligns event aggregations to truncated
    time intervals given a user-provided time window.
    
    For example, if a window of 5 minutes was given, the `WindowedFlush`
    would enforce the following window boundaries:
    
    - [12:00:00, 12:05:00)
    - [12:05:00, 12:10:00)
    - [12:10:00, 12:15:00)
    - etc.
    
    This is a first pass implementation of the flush mechanism used in
    the eventagg package. As needs evolve, the interface and/or
    implementation is subject to change. For the purposed of prototyping
    though, this meets our needs.
    
    Release note: none

--- pkg/obs/eventagg/map_reduce.go
+++ pkg/obs/eventagg/map_reduce.go
@@ -32,14 +32,18 @@ type MapReduceAggregator[E Mergeable[K, Agg], K comparable, Agg any] struct {
 	newFn   func() Agg
 	mu      struct {
 		syncutil.Mutex
-		cache map[K]Agg
+		cache        map[K]Agg
+		flushTrigger FlushTrigger
 	}
 	consumers []flushConsumer[K, Agg]
 }
 
 // NewMapReduceAggregator returns a new MapReduceAggregator[Agg].
 func NewMapReduceAggregator[E Mergeable[K, V], K comparable, V any](
-	stopper *stop.Stopper, newFn func() V, flushConsumers ...flushConsumer[K, V],
+	stopper *stop.Stopper,
+	newFn func() V,
+	flushTrigger FlushTrigger,
+	flushConsumers ...flushConsumer[K, V],
 ) *MapReduceAggregator[E, K, V] {
 	m := &MapReduceAggregator[E, K, V]{
 		stopper:   stopper,
@@ -47,17 +51,24 @@ func NewMapReduceAggregator[E Mergeable[K, V], K comparable, V any](
 		consumers: flushConsumers,
 	}
 	m.mu.cache = make(map[K]V)
+	m.mu.flushTrigger = flushTrigger
 	return m
 }
 
 // Add implements the aggregator interface.
-func (m *MapReduceAggregator[E, K, Agg]) Add(_ context.Context, e E) {
+func (m *MapReduceAggregator[E, K, Agg]) Add(ctx context.Context, e E) {
 	if !envEnableStructuredEvents {
 		return
 	}
 	k := e.GroupingKey()
 	m.mu.Lock()
 	defer m.mu.Unlock()
+	// If it's time to flush, do so async before processing the event.
+	// This will reset the cache, meaning our event will be added to
+	// a fresh aggregation window.
+	if m.mu.flushTrigger.shouldFlush() {
+		m.flushAsync(ctx, m.getAndResetCacheLocked())
+	}
 	v, ok := m.mu.cache[k]
 	if !ok {
 		v = m.newFn()
@@ -72,14 +83,7 @@ func (m *MapReduceAggregator[E, K, Agg]) Add(_ context.Context, e E) {
 // The flushed data will be passed to each of the configured flushConsumer's
 // provided at construction for further processing.
 // TODO(abarganier): implement more robust flush mechanism, with configurable triggers.
-func (m *MapReduceAggregator[E, K, Agg]) Flush(ctx context.Context) {
-	flushed := func() map[K]Agg {
-		m.mu.Lock()
-		defer m.mu.Unlock()
-		flushed := m.mu.cache
-		m.mu.cache = make(map[K]Agg)
-		return flushed
-	}()
+func (m *MapReduceAggregator[E, K, Agg]) flushAsync(ctx context.Context, flushed map[K]Agg) {
 	if err := m.stopper.RunAsyncTask(ctx, "map-reduce-flush", func(ctx context.Context) {
 		for _, c := range m.consumers {
 			c.onFlush(ctx, flushed)
@@ -87,4 +91,10 @@ func (m *MapReduceAggregator[E, K, Agg]) Flush(ctx context.Context) {
 	}); err != nil {
 		log.Errorf(ctx, "a problem occurred attempting to flush an aggregation: %v", err)
 	}
+}
+
+func (m *MapReduceAggregator[E, K, V]) getAndResetCacheLocked() map[K]V {
+	flushed := m.mu.cache
+	m.mu.cache = make(map[K]V)
+	return flushed
 }
