3f60027e17ed19c5f00c985d27903ea3084259b5
Alex Barganier <abarganier@cockroachlabs.com>
2024-04-03 17:02:22 -0400

    pkg/util/log: introduce log.Structured, wire up to aggregator
    This patch introduces log.Structured to the log package API. It aims to
    serve as a prototype for the log facility we will use for exporting
    "exhaust" from CRDB in the form of JSON objects. The intention is that
    this exhaust can be consumed externally and be sufficient enough to
    build features around.
    
    This iteration has some serious limitations, the main one being that it
    is not redaction-aware. The `log.StructuredEvent` exists alongside it
    for now. Both implementations are quite similar, so they should probably
    be reconciled and/or combined, but this is left as a TODO to avoid
    slowing down the prototyping process. For now, it's sufficient for
    prototyping.
    
    The patch also introduces a new logging channel explicitly for the new
    `log.Structured` API, called `STRUCTURED_EVENTS`. The ability to segment these
    types of logs from the rest of the logs is what motivates this separate
    channel. The name works for now, but we should consider if there's a
    better name available.
    
    A following patch will focus on internal consumption of these events.
    
    Release note: none

--- pkg/obs/eventagg/map_reduce.go
+++ pkg/obs/eventagg/map_reduce.go
@@ -86,8 +86,8 @@ func (m *MapReduceAggregator[E, K, Agg]) Add(ctx context.Context, e E) {
 	// If it's time to flush, do so async before processing the event.
 	// This will reset the cache, meaning our event will be added to
 	// a fresh aggregation window.
-	if m.mu.flushTrigger.shouldFlush() {
-		m.flushAsync(ctx, m.getAndResetCacheLocked())
+	if shouldFlush, aggInfo := m.mu.flushTrigger.shouldFlush(); shouldFlush {
+		m.flushAsync(ctx, aggInfo, m.getAndResetCacheLocked())
 	}
 	v, ok := m.mu.cache[k]
 	if !ok {
@@ -102,10 +102,12 @@ func (m *MapReduceAggregator[E, K, Agg]) Add(ctx context.Context, e E) {
 //
 // The flushed data will be passed to each of the configured flushConsumer's
 // provided at construction for further processing.
-func (m *MapReduceAggregator[E, K, Agg]) flushAsync(ctx context.Context, flushed map[K]Agg) {
+func (m *MapReduceAggregator[E, K, Agg]) flushAsync(
+	ctx context.Context, aggInfo AggInfo, flushed map[K]Agg,
+) {
 	if err := m.stopper.RunAsyncTask(ctx, "map-reduce-flush", func(ctx context.Context) {
 		for _, c := range m.consumers {
-			c.onFlush(ctx, flushed)
+			c.onFlush(ctx, aggInfo, flushed)
 		}
 	}); err != nil {
 		log.Errorf(ctx, "a problem occurred attempting to flush an aggregation: %v", err)
