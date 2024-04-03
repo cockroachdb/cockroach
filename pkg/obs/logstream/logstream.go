43e7bb62b5cf56a051650bfc05218c72a4c840b6
Alex Barganier <abarganier@cockroachlabs.com>
2024-04-03 17:02:22 -0400

    pkg/util/log: introduce log.Structured, wire up to aggregator
    This patch introduces log.Structured to the log package API. It aims to
    serve as a prototype for the log facility we will use for exporting
    "exhaust" from CRDB in the form of JSON objects. The intention is that
    this exhaust can be consumed externally and be sufficient enough to
    build features around.
    
    This iteration has some serious limitations, the main one being that it
    is not redaction-aware. The `log.StructuredEvent` exists alongside it
    for now. Both implementations are quite similar, so they should probably
    be reconciled and/or combined, but this is left as a TODO to avoid
    slowing down the prototyping process. For now, it's sufficient for
    prototyping.
    
    The patch also introduces a new logging channel explicitly for the new
    `log.Structured` API, called `STRUCTURED_EVENTS`. The ability to segment these
    types of logs from the rest of the logs is what motivates this separate
    channel. The name works for now, but we should consider if there's a
    better name available.
    
    A following patch will focus on internal consumption of these events.
    
    Release note: none

---
+++ pkg/obs/logstream/logstream.go
@@ -0,0 +1,113 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package logstream
+
+import (
+	"context"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/errors"
+)
+
+// controller is the singleton streamController used by the exported interface
+// of the logstream package.
+var controller = newStreamController()
+
+// streamController provides the primary functionality of the logstream package, which is built
+// to route structured events logged via log.Structured to one or more processors, registered via
+// RegisterProcessor. This is done using tenant separation.
+type streamController struct {
+	rmu struct {
+		syncutil.RWMutex
+		tenantRouters map[roachpb.TenantID]*asyncProcessorRouter
+	}
+}
+
+var _ log.StructuredLogProcessor = (*streamController)(nil)
+
+func newStreamController() *streamController {
+	sc := &streamController{}
+	sc.rmu.tenantRouters = make(map[roachpb.TenantID]*asyncProcessorRouter)
+	return sc
+}
+
+// Inject our controller to pkg/util/log as a dependency.
+func init() {
+	log.SetStructuredLogProcessor(controller)
+}
+
+// Processor defines internally how a component can process streams of structured log events as they're
+// passed to log.Structured. If you're looking to create a new structured log processor externally, See the
+// LogProcessor interface for a more convenient way, to be used in conjunction with StructuredLogProcessor.
+type Processor interface {
+	// Process processes a single log stream event.
+	process(context.Context, any) error
+}
+
+// RegisterProcessor registers the given StructuredLogProcessor to consume all log stream events of
+// the given EventType, logged via log.Structured.
+//
+// The processing is done asynchronously from the log.Structured call, which simply buffers the event.
+// Each tenant has their own processor(s) and async buffer, meaning each tenant is responsible for
+// making their own calls to RegisterProcessor.
+func RegisterProcessor(
+	ctx context.Context, stopper *stop.Stopper, eventType log.EventType, processor Processor,
+) {
+	if controller == nil {
+		panic(errors.AssertionFailedf("attempted to registry logstream processor before controller was initialized"))
+	}
+	tID, ok := roachpb.ClientTenantFromContext(ctx)
+	if !ok {
+		tID = roachpb.SystemTenantID
+	}
+	controller.rmu.Lock()
+	defer controller.rmu.Unlock()
+	tenantRouter, ok := controller.rmu.tenantRouters[tID]
+	if !ok {
+		// TODO(abarganier): config knobs around flush trigger criteria.
+		tenantRouter = newAsyncProcessorRouter(
+			10*time.Second,
+			100,  /* triggerLen */
+			1000, /* maxLen */
+		)
+		controller.rmu.tenantRouters[tID] = tenantRouter
+		if err := tenantRouter.Start(ctx, stopper); err != nil {
+			panic(errors.AssertionFailedf(
+				"failed to start asyncProcessorRouter for tenant ID %d, event type %s", tID, eventType))
+		}
+	}
+	tenantRouter.register(eventType, processor)
+}
+
+// Process implements the log.StructuredLogProcessor interface.
+func (l *streamController) Process(ctx context.Context, eventType log.EventType, e any) {
+	tID, ok := roachpb.ClientTenantFromContext(ctx)
+	if !ok {
+		tID = roachpb.SystemTenantID
+	}
+	l.rmu.RLock()
+	defer l.rmu.RUnlock()
+	processor, ok := l.rmu.tenantRouters[tID]
+	if !ok {
+		// We don't mandate that a processor has been registered.
+		return
+	}
+	if err := processor.Process(ctx, &typedEvent{
+		eventType: eventType,
+		event:     e,
+	}); err != nil {
+		log.Errorf(ctx, "error consuming exhaust: %v", err)
+	}
+}
