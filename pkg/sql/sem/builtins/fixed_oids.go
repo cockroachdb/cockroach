4c4d3698d7121c09f0e4cb7772ee2dfa73f73427
David Taylor <tinystatemachine@gmail.com>
2024-04-08 10:01:08 +0000

    builtins: add tenant version of index_span
    Release note: none.
    Epic: none.

--- pkg/sql/sem/builtins/fixed_oids.go
+++ pkg/sql/sem/builtins/fixed_oids.go
@@ -2577,6 +2577,7 @@ var builtinOidsArray = []string{
 	2609: `crdb_internal.request_statement_bundle(stmtFingerprint: string, samplingProbability: float, minExecutionLatency: interval, expiresAfter: interval, redacted: bool) -> bool`,
 	2610: `crdb_internal.request_statement_bundle(stmtFingerprint: string, planGist: string, samplingProbability: float, minExecutionLatency: interval, expiresAfter: interval, redacted: bool) -> bool`,
 	2611: `crdb_internal.request_statement_bundle(stmtFingerprint: string, planGist: string, antiPlanGist: bool, samplingProbability: float, minExecutionLatency: interval, expiresAfter: interval, redacted: bool) -> bool`,
+	2612: `crdb_internal.index_span(tenant_id: int, table_id: int, index_id: int) -> bytes[]`,
 }
 
 var builtinOidsBySignature map[string]oid.Oid
