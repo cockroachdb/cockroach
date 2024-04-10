ff019faafc7731ebc9c6259b539bc46db2e1f5a6
Arul Ajmani <arulajmani@gmail.com>
2024-04-09 23:17:08 -0400

    concurrency: correctly handle replay attempts from SKIP LOCKED requests
    Previously, whenever a SKIP LOCKED request encountered another request
    from its own transaction in a lock's wait queue, it would return an
    error. However, we've wanted to handle such cases by treating the found
    request as non-conflicting -- see discussion on the attached issue. This
    patch does exactly that. In doing so, we're also able to clean up some
    error special casing in KVNemesis.
    
    Closes https://github.com/cockroachdb/cockroach/issues/111527
    
    Release note: None

--- pkg/kv/kvserver/concurrency/lock_table.go
+++ pkg/kv/kvserver/concurrency/lock_table.go
@@ -750,22 +750,11 @@ func (g *lockTableGuardImpl) IsKeyLockedByConflictingTxn(
 			break
 		}
 		if qqg.guard.txnMeta() != nil && g.isSameTxn(qqg.guard.txnMeta()) {
-			// A SKIP LOCKED request should not find another waiting request from its
-			// own transaction, at least not in the way that SQL uses KV. The only way
-			// we can end up finding another request in the lock's wait queue from our
-			// own transaction is if we're a replay.  We could handle this case by
-			// treating it as a non-conflict, but doing so expands the testing surface
-			// area -- we would want to include tests for:
-			// 1. Just our own request in the wait queue, treated as a non-conflict.
-			// 2. A request from a different transaction in the wait queue, with a
-			// lower sequence number, that conflicts.
-			// 3. A request from a different transaction in the wait queue, with a
-			// higher sequence number, that conflicts.
-			// 4. A request from a different transaction in the wait queue, with a
-			// lower sequence number, that does not conflict.
-			// For now, we simply return an error, and mark it for the benefit of
-			// KVNemesis.
-			return false, nil, MarkSkipLockedReplayError(errors.Errorf("SKIP LOCKED request should not find another waiting request from the same transaction"))
+			// Normally, in the way SQL uses KV, a SKIP LOCKED request should never
+			// find another request from its own transaction in a lock's wait queue.
+			// However, this is possible in case there is a replay involved. We handle
+			// this by treating the request as non-conflicting.
+			continue
 		}
 		if lock.Conflicts(qqg.mode, makeLockMode(str, g.txnMeta(), g.ts), &g.lt.settings.SV) {
 			return true, nil, nil // the conflict isn't with a lock holder, nil is returned
@@ -4692,22 +4681,4 @@ func MarkLockPromotionError(cause error) error {
 		return nil
 	}
 	return errors.Mark(cause, &LockPromotionError{})
-}
-
-// SkipLockedReplayError is used to mark errors resulting from replayed SKIP
-// LOCKED requests that discover other requests from their own transactions in
-// a lock's wait queue. We mark such errors for the benefit of KVNemesis.
-type SkipLockedReplayError struct{}
-
-func (e *SkipLockedReplayError) Error() string {
-	return "skip locked replay error"
-}
-
-// MarkSkipLockedReplayError wraps the given error, if non-nil, as a skip locked
-// replay error.
-func MarkSkipLockedReplayError(cause error) error {
-	if cause == nil {
-		return nil
-	}
-	return errors.Mark(cause, &SkipLockedReplayError{})
 }
