cockroachdb/cockroach

commit a555107be68d0404aae30255981fd3933709c8f2

kvcoord: add DistSender circuit breaker grace period for writes
Unlike read requests, cancelled write requests can't be automatically
retried, and instead return an ambiguous result error to the client.
Because of this, we should be more conservative in cancelling such
requests, giving the replica a chance to recover and the write to
succeed.

This patch adds a grace period for write cancellation. The probe must
continually fail for this period in order to cancel in-flight write
requests. The breaker is still tripped on the first failure, which
prevents new write requests from being sent to the replica.

The default grace period is set to 10 seconds, which is longer than the
default Raft election timeout (up to 4 seconds), lease interval (6
seconds), and reproposal timeout (3 seconds). This gives the range a
chance to recover after e.g. losing a leaseholder or quorum before
giving up on the write.

Note that lease requests are not subject to the write grace period,
since they are submitted directly to the local replica instead of via
the DistSender.

Epic: none
Release note: None

pkg/kv/kvclient/kvcoord/dist_sender_circuit_breaker.go
@@ -71,6 +71,23 @@ var (
 		true,
 		settings.WithPublic,
 	)
+
+	CircuitBreakerCancellationWriteGracePeriod = settings.RegisterDurationSetting(
+		settings.ApplicationLevel,
+		"kv.dist_sender.circuit_breaker.cancellation.write_grace_period",
+		"how long after the circuit breaker trips to cancel write requests "+
+			"(these can't retry internally, so should be long enough to allow quorum/lease recovery)",
+		10*time.Second,
+		settings.WithPublic,
+		settings.WithValidateDuration(func(t time.Duration) error {
+			// This prevents probes from exiting when idle, which can lead to buildup
+			// of probe goroutines, so cap it at 1 minute.
+			if t > time.Minute {
+				return errors.New("grace period can't be more than 1 minute")
+			}
+			return nil
+		}),
+	)
 )
 
 const (
@@ -85,6 +102,33 @@ const (
 	// failing probe should exit. It will be relaunched on the next request.
 	cbProbeIdleTimeout = 10 * time.Second
 )
+
+// cbRequestKind classifies a batch request.
+type cbRequestKind int
+
+const (
+	cbReadRequest cbRequestKind = iota
+	cbWriteRequest
+	cbNumRequestKinds // must be last in list
+)
+
+func (k cbRequestKind) String() string {
+	switch k {
+	case cbReadRequest:
+		return "read"
+	case cbWriteRequest:
+		return "write"
+	default:
+		panic(errors.AssertionFailedf("unknown request kind %d", k))
+	}
+}
+
+func cbRequestKindFromBatch(ba *kvpb.BatchRequest) cbRequestKind {
+	if ba.IsWrite() {
+		return cbWriteRequest
+	}
+	return cbReadRequest
+}
 
 // cbKey is a key in the DistSender replica circuit breakers map.
 type cbKey struct {
@@ -120,7 +164,9 @@ type cbKey struct {
 //   - Only if there are still in-flight requests.
 //
 // The breaker is only tripped once the probe fails (never in response to user
-// request failures alone).
+// request failures alone). If enabled, in-flight reads are cancelled
+// immediately when the breaker trips, and writes are cancelled after a grace
+// period (since they can't be automatically retried).
 //
 // The probe sends a LeaseInfo request and expects either a successful response
 // (if it is the leaseholder) or a NLHE (if it knows a leaseholder or leader
@@ -338,8 +384,13 @@ type ReplicaCircuitBreaker struct {
 		syncutil.Mutex
 
 		// cancelFns contains context cancellation functions for all in-flight
-		// requests. Only tracked if cancellation is enabled.
-		cancelFns map[*kvpb.BatchRequest]context.CancelCauseFunc
+		// requests, segmented by request type. Reads can be retried by the
+		// DistSender, so we cancel these immediately when the breaker trips.
+		// Writes can't automatically retry, and will return ambiguous result errors
+		// to clients, so we only cancel them after a grace period.
+		//
+		// Only tracked if cancellation is enabled.
+		cancelFns [cbNumRequestKinds]map[*kvpb.BatchRequest]context.CancelCauseFunc
 	}
 }
 
@@ -362,7 +413,9 @@ func newReplicaCircuitBreaker(
 		AsyncProbe:   r.launchProbe,
 		EventHandler: r,
 	})
-	r.mu.cancelFns = map[*kvpb.BatchRequest]context.CancelCauseFunc{}
+	for i := range r.mu.cancelFns {
+		r.mu.cancelFns[i] = map[*kvpb.BatchRequest]context.CancelCauseFunc{}
+	}
 	return r
 }
 
@@ -494,8 +547,9 @@ func (r *ReplicaCircuitBreaker) Track(
 
 		if !hasTimeout {
 			sendCtx, token.cancel = context.WithCancelCause(ctx)
+			reqKind := cbRequestKindFromBatch(ba)
 			r.mu.Lock()
-			r.mu.cancelFns[ba] = token.cancel
+			r.mu.cancelFns[reqKind][ba] = token.cancel
 			r.mu.Unlock()
 		}
 	}
@@ -523,8 +577,9 @@ func (r *ReplicaCircuitBreaker) done(
 	}
 
 	if cancel != nil {
+		reqKind := cbRequestKindFromBatch(ba)
 		r.mu.Lock()
-		delete(r.mu.cancelFns, ba) // nolint:deferunlockcheck
+		delete(r.mu.cancelFns[reqKind], ba) // nolint:deferunlockcheck
 		r.mu.Unlock()
 		cancel(nil)
 	}
@@ -643,12 +698,33 @@ func (r *ReplicaCircuitBreaker) launchProbe(report func(error), done func()) {
 		}
 		defer transport.Release()
 
+		// Start the write grace timer. Unlike reads, writes can't automatically be
+		// retried by the DistSender, so we don't cancel them immediately when the
+		// breaker trips but only after it has remained tripped for a grace period.
+		// This should be long enough to wait out a Raft election timeout and lease
+		// interval and then repropose the write, in case the range is temporarily
+		// unavailable (e.g. following leaseholder loss).
+		//
+		// If the breaker is already tripped, the previous probe already waited out
+		// the grace period, so we don't have to. The grace timer channel is set to
+		// nil when there is no timer running.
+		//
+		// NB: lease requests aren't subject to the write grace period, despite
+		// being write requests, since they are submitted directly to the local
+		// replica instead of via the DistSender.
+		var writeGraceTimer timeutil.Timer
+		defer writeGraceTimer.Stop()
+		if period := CircuitBreakerCancellationWriteGracePeriod.Get(&r.d.settings.SV); period > 0 {
+			if !r.isTripped() {
+				writeGraceTimer.Reset(period)
+			}
+		}
+
 		// Continually probe the replica until it succeeds or the replica stops
 		// seeing traffic. We probe immediately since we only trip the breaker on
 		// probe failure.
 		var timer timeutil.Timer
 		defer timer.Stop()
-		timer.Reset(CircuitBreakerProbeInterval.Get(&r.d.settings.SV))
 
 		for {
 			// Untrip the breaker and stop probing if circuit breakers are disabled.
@@ -656,6 +732,10 @@ func (r *ReplicaCircuitBreaker) launchProbe(report func(error), done func()) {
 				report(nil)
 				return
 			}
+
+			// Start the interval before sending the probe, to avoid skewing the
+			// interval, instead preferring frequent probes.
+			timer.Reset(CircuitBreakerProbeInterval.Get(&r.d.settings.SV))
 
 			// Probe the replica.
 			err := r.sendProbe(ctx, transport)
@@ -675,31 +755,44 @@ func (r *ReplicaCircuitBreaker) launchProbe(report func(error), done func()) {
 				return
 			}
 
-			// Cancel in-flight requests on failure. We do this on every failure, and
-			// also remove the cancel functions from the map (even though done() will
-			// also clean them up), in case another request makes it in after the
-			// breaker trips. There should typically never be any contention here.
-			func() {
+			// Cancel in-flight read requests on failure, and write requests if the
+			// grace timer has expired. We do this on every failure, and also remove
+			// the cancel functions from the map (even though done() will also clean
+			// them up), in case another request makes it in after the breaker trips.
+			// There should typically never be any contention here.
+			cancelRequests := func(reqKind cbRequestKind) {
 				r.mu.Lock()
 				defer r.mu.Unlock()
-				for ba, cancel := range r.mu.cancelFns {
-					delete(r.mu.cancelFns, ba)
+
+				if l := len(r.mu.cancelFns[reqKind]); l > 0 {
+					log.VEventf(ctx, 2, "cancelling %d %s requests for %s", l, reqKind, r.id())
+				}
+				for ba, cancel := range r.mu.cancelFns[reqKind] {
+					delete(r.mu.cancelFns[reqKind], ba)
 					cancel(errors.Wrapf(err, "%s is unavailable (circuit breaker tripped)", r.id()))
 					r.d.metrics.CircuitBreaker.ReplicasRequestsCancelled.Inc(1)
 				}
-			}()
-
-			select {
-			case <-timer.C:
-				timer.Read = true
-			case <-r.d.stopper.ShouldQuiesce():
-				return
-			case <-ctx.Done():
-				return
 			}
 
-			probeInterval := CircuitBreakerProbeInterval.Get(&r.d.settings.SV)
-			timer.Reset(probeInterval)
+			cancelRequests(cbReadRequest)
+			if writeGraceTimer.C == nil {
+				cancelRequests(cbWriteRequest)
+			}
+
+			for !timer.Read { // select until probe interval timer fires
+				select {
+				case <-timer.C:
+					timer.Read = true
+				case <-writeGraceTimer.C:
+					cancelRequests(cbWriteRequest)
+					writeGraceTimer.Read = true
+					writeGraceTimer.Stop() // sets C = nil
+				case <-r.d.stopper.ShouldQuiesce():
+					return
+				case <-ctx.Done():
+					return
+				}
+			}
 
 			// If there haven't been any recent requests, stop probing but keep the
 			// breaker tripped. A new probe will be launched on the next request.
@@ -708,7 +801,11 @@ func (r *ReplicaCircuitBreaker) launchProbe(report func(error), done func()) {
 			// frequently spawning new probe goroutines, instead waiting to see if any
 			// requests come in.
 			if r.lastRequestDuration(timeutil.Now().UnixNano()) >= cbProbeIdleTimeout {
-				return
+				// Keep probing if the write grace timer hasn't expired yet, since we
+				// need to cancel pending writes first.
+				if writeGraceTimer.C == nil {
+					return
+				}
 			}
 		}
 	})

docs/generated/settings/settings.html
@@ -68,6 +68,7 @@
 <tr><td><div id="setting-kv-closed-timestamp-side-transport-interval" class="anchored"><code>kv.closed_timestamp.side_transport_interval</code></div></td><td>duration</td><td><code>200ms</code></td><td>the interval at which the closed timestamp side-transport attempts to advance each range&#39;s closed timestamp; set to 0 to disable the side-transport</td><td>Serverless/Dedicated/Self-Hosted (read-only)</td></tr>
 <tr><td><div id="setting-kv-closed-timestamp-target-duration" class="anchored"><code>kv.closed_timestamp.target_duration</code></div></td><td>duration</td><td><code>3s</code></td><td>if nonzero, attempt to provide closed timestamp notifications for timestamps trailing cluster time by approximately this duration</td><td>Serverless/Dedicated/Self-Hosted (read-only)</td></tr>
 <tr><td><div id="setting-kv-dist-sender-circuit-breaker-cancellation-enabled" class="anchored"><code>kv.dist_sender.circuit_breaker.cancellation.enabled</code></div></td><td>boolean</td><td><code>true</code></td><td>when enabled, in-flight requests will be cancelled when the circuit breaker trips</td><td>Serverless/Dedicated/Self-Hosted</td></tr>
+<tr><td><div id="setting-kv-dist-sender-circuit-breaker-cancellation-write-grace-period" class="anchored"><code>kv.dist_sender.circuit_breaker.cancellation.write_grace_period</code></div></td><td>duration</td><td><code>10s</code></td><td>how long after the circuit breaker trips to cancel write requests (these can&#39;t retry internally, so should be long enough to allow quorum/lease recovery)</td><td>Serverless/Dedicated/Self-Hosted</td></tr>
 <tr><td><div id="setting-kv-dist-sender-circuit-breaker-enabled" class="anchored"><code>kv.dist_sender.circuit_breaker.enabled</code></div></td><td>boolean</td><td><code>true</code></td><td>enable circuit breakers for failing or stalled replicas</td><td>Serverless/Dedicated/Self-Hosted</td></tr>
 <tr><td><div id="setting-kv-dist-sender-circuit-breaker-probe-interval" class="anchored"><code>kv.dist_sender.circuit_breaker.probe.interval</code></div></td><td>duration</td><td><code>3s</code></td><td>interval between replica probes</td><td>Serverless/Dedicated/Self-Hosted</td></tr>
 <tr><td><div id="setting-kv-dist-sender-circuit-breaker-probe-threshold" class="anchored"><code>kv.dist_sender.circuit_breaker.probe.threshold</code></div></td><td>duration</td><td><code>3s</code></td><td>duration of errors or stalls after which a replica will be probed</td><td>Serverless/Dedicated/Self-Hosted</td></tr>

commit 54b71e9d01b1adc56106aeb859fd6d248452ed73

kv: subject query intents to circuit breaker grace period
Fixes #121997.

This commit subjects the "pre-commit query intent" sub-batch of a
parallel commit to the same circuit breaker grace period as writes
(introduced in a555107b). This prevents them from being eagerly
cancelled on tripped DistSender circuit breakers, leading to ambiguous
errors.

Release note: None

pkg/kv/kvclient/kvcoord/dist_sender.go
@@ -2727,7 +2727,7 @@ func (ds *DistSender) sendToReplicas(
 
 		tBegin := timeutil.Now() // for slow log message
 		sendCtx, cbToken, cbErr := ds.circuitBreakers.ForReplica(desc, &curReplica).
-			Track(ctx, ba, tBegin.UnixNano())
+			Track(ctx, ba, withCommit, tBegin.UnixNano())
 		if cbErr != nil {
 			// Circuit breaker is tripped. err will be handled below.
 			err = cbErr

pkg/kv/kvclient/kvcoord/dist_sender_circuit_breaker.go
@@ -103,31 +103,39 @@ const (
 	cbProbeIdleTimeout = 10 * time.Second
 )
 
-// cbRequestKind classifies a batch request.
-type cbRequestKind int
+// cbRequestCancellationPolicy classifies a batch request.
+type cbRequestCancellationPolicy int
 
 const (
-	cbReadRequest cbRequestKind = iota
-	cbWriteRequest
+	cbCancelImmediately cbRequestCancellationPolicy = iota
+	cbCancelAfterGracePeriod
 	cbNumRequestKinds // must be last in list
 )
 
-func (k cbRequestKind) String() string {
+func (k cbRequestCancellationPolicy) String() string {
 	switch k {
-	case cbReadRequest:
-		return "read"
-	case cbWriteRequest:
-		return "write"
+	case cbCancelImmediately:
+		return "immediately"
+	case cbCancelAfterGracePeriod:
+		return "after grace period"
 	default:
 		panic(errors.AssertionFailedf("unknown request kind %d", k))
 	}
 }
 
-func cbRequestKindFromBatch(ba *kvpb.BatchRequest) cbRequestKind {
-	if ba.IsWrite() {
-		return cbWriteRequest
+func cbRequestCancellationPolicyFromBatch(
+	ba *kvpb.BatchRequest, withCommit bool,
+) cbRequestCancellationPolicy {
+	// If the batch request is writing or is part of a transaction commit, we
+	// can't automatically retry it without risking an ambiguous error, so we
+	// cancel it after a grace period. Otherwise, we cancel it immediately and
+	// allow DistSender to retry.
+	// TODO(nvanbenschoten): a batch request that is writing and is not part of a
+	// transaction commit can be retried. Do we need the IsWrite condition here?
+	if ba.IsWrite() || withCommit {
+		return cbCancelAfterGracePeriod
 	}
-	return cbReadRequest
+	return cbCancelImmediately
 }
 
 // cbKey is a key in the DistSender replica circuit breakers map.
@@ -461,6 +469,9 @@ type replicaCircuitBreakerToken struct {
 
 	// ba is the batch request being tracked.
 	ba *kvpb.BatchRequest
+
+	// withCommit denotes whether the request is part of a transaction commit.
+	withCommit bool
 }
 
 // Done records the result of the request and untracks it. If the request was
@@ -469,7 +480,7 @@ type replicaCircuitBreakerToken struct {
 func (t replicaCircuitBreakerToken) Done(
 	br *kvpb.BatchResponse, sendErr error, nowNanos int64,
 ) error {
-	return t.r.done(t.ctx, t.cancelCtx, t.ba, br, sendErr, nowNanos)
+	return t.r.done(t.ctx, t.cancelCtx, t.ba, t.withCommit, br, sendErr, nowNanos)
 }
 
 // id returns a string identifier for the replica.
@@ -546,7 +557,7 @@ func (r *ReplicaCircuitBreaker) isClosed() bool {
 // for the send and a token which the caller must call Done() on with the result
 // of the request.
 func (r *ReplicaCircuitBreaker) Track(
-	ctx context.Context, ba *kvpb.BatchRequest, nowNanos int64,
+	ctx context.Context, ba *kvpb.BatchRequest, withCommit bool, nowNanos int64,
 ) (context.Context, replicaCircuitBreakerToken, error) {
 	if r == nil {
 		return ctx, replicaCircuitBreakerToken{}, nil // circuit breakers disabled
@@ -566,9 +577,10 @@ func (r *ReplicaCircuitBreaker) Track(
 
 	// Set up the request token.
 	token := replicaCircuitBreakerToken{
-		r:   r,
-		ctx: ctx,
-		ba:  ba,
+		r:          r,
+		ctx:        ctx,
+		ba:         ba,
+		withCommit: withCommit,
 	}
 
 	// Record in-flight requests. If this is the only request, tentatively start
@@ -602,7 +614,7 @@ func (r *ReplicaCircuitBreaker) Track(
 			sendCtx, cancel = context.WithCancelCause(ctx)
 			token.cancelCtx = sendCtx
 
-			reqKind := cbRequestKindFromBatch(ba)
+			reqKind := cbRequestCancellationPolicyFromBatch(ba, withCommit)
 			r.mu.Lock()
 			r.mu.cancelFns[reqKind][ba] = cancel
 			r.mu.Unlock()
@@ -621,6 +633,7 @@ func (r *ReplicaCircuitBreaker) done(
 	ctx context.Context,
 	cancelCtx context.Context,
 	ba *kvpb.BatchRequest,
+	withCommit bool,
 	br *kvpb.BatchResponse,
 	sendErr error,
 	nowNanos int64,
@@ -647,7 +660,7 @@ func (r *ReplicaCircuitBreaker) done(
 		}
 
 		// Clean up the cancel function.
-		reqKind := cbRequestKindFromBatch(ba)
+		reqKind := cbRequestCancellationPolicyFromBatch(ba, withCommit)
 		r.mu.Lock()
 		cancel := r.mu.cancelFns[reqKind][ba]
 		delete(r.mu.cancelFns[reqKind], ba) // nolint:deferunlockcheck
@@ -839,12 +852,12 @@ func (r *ReplicaCircuitBreaker) launchProbe(report func(error), done func()) {
 			// the cancel functions from the map (even though done() will also clean
 			// them up), in case another request makes it in after the breaker trips.
 			// There should typically never be any contention here.
-			cancelRequests := func(reqKind cbRequestKind) {
+			cancelRequests := func(reqKind cbRequestCancellationPolicy) {
 				r.mu.Lock()
 				defer r.mu.Unlock()
 
 				if l := len(r.mu.cancelFns[reqKind]); l > 0 {
-					log.VEventf(ctx, 2, "cancelling %d %s requests for %s", l, reqKind, r.id())
+					log.VEventf(ctx, 2, "cancelling %d requests %s for %s", l, reqKind, r.id())
 				}
 				for ba, cancel := range r.mu.cancelFns[reqKind] {
 					delete(r.mu.cancelFns[reqKind], ba)
@@ -853,9 +866,9 @@ func (r *ReplicaCircuitBreaker) launchProbe(report func(error), done func()) {
 				}
 			}
 
-			cancelRequests(cbReadRequest)
+			cancelRequests(cbCancelImmediately)
 			if writeGraceTimer.C == nil {
-				cancelRequests(cbWriteRequest)
+				cancelRequests(cbCancelAfterGracePeriod)
 			}
 
 			for !timer.Read { // select until probe interval timer fires
@@ -863,7 +876,7 @@ func (r *ReplicaCircuitBreaker) launchProbe(report func(error), done func()) {
 				case <-timer.C:
 					timer.Read = true
 				case <-writeGraceTimer.C:
-					cancelRequests(cbWriteRequest)
+					cancelRequests(cbCancelAfterGracePeriod)
 					writeGraceTimer.Read = true
 					writeGraceTimer.Stop() // sets C = nil
 				case <-r.closedC:

pkg/kv/kvclient/kvcoord/dist_sender_circuit_breaker_test.go
@@ -228,7 +228,7 @@ func benchmarkCircuitBreakersTrack(
 	// If this shouldn't be the only request, add another concurrent request to
 	// the tracking.
 	if !alone {
-		_, _, err := cbs.ForReplica(rangeDesc, replDesc).Track(ctx, ba, 1)
+		_, _, err := cbs.ForReplica(rangeDesc, replDesc).Track(ctx, ba, false /* withCommit */, 1)
 		require.NoError(b, err)
 	}
 
@@ -272,7 +272,7 @@ func benchmarkCircuitBreakersTrack(
 			// Adjust b.N for concurrency.
 			for i := 0; i < b.N/conc; i++ {
 				cb := cbs.ForReplica(rangeDesc, replDesc)
-				_, cbToken, err := cb.Track(sendCtx, ba, nowNanos)
+				_, cbToken, err := cb.Track(sendCtx, ba, false /* withCommit */, nowNanos)
 				if err != nil {
 					assert.NoError(b, err)
 					return