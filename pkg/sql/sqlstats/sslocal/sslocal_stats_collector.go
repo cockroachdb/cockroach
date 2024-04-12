fa500be57eaf91b41ad0755cb3578d9db45d63ec
Xin Hao Zhang <xzhang@cockroachlabs.com>
2024-04-12 12:04:37 -0400

    sqlstats: introduce insights writer to StatsCollector
    This commit adds the insights writer to the `StatsCollector` struct. This
    is in an effort to shift the responsibility of writing insights from being
    owned by the `ssmemstorage.Container` to `sslocal.StatsCollector`. It makes
    more sense for the StatsCollector, which is already responsible for
    coordinating the writing of sql stats for a conn executor, to also
    orchestrate insights collection operations. The sql stats application
    container's intended purpose is to store per-application sql statistics.
    
    Epic: none

--- pkg/sql/sqlstats/sslocal/sslocal_stats_collector.go
+++ pkg/sql/sqlstats/sslocal/sslocal_stats_collector.go
@@ -17,12 +17,16 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/appstatspb"
 	"github.com/cockroachdb/cockroach/pkg/sql/sessionphase"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlstats"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlstats/insights"
 )
 
 // StatsCollector is used to collect statistics for transactions and
 // statements for the entire lifetime of a session.
 type StatsCollector struct {
 	sqlstats.ApplicationStats
+
+	// Allows StatsCollector to send statement and transaction stats to the insights system.
+	insightsWriter insights.Writer
 
 	// phaseTimes tracks session-level phase times.
 	phaseTimes *sessionphase.Times
@@ -42,15 +46,18 @@ var _ sqlstats.ApplicationStats = &StatsCollector{}
 func NewStatsCollector(
 	st *cluster.Settings,
 	appStats sqlstats.ApplicationStats,
+	insights insights.Writer,
 	phaseTime *sessionphase.Times,
 	knobs *sqlstats.TestingKnobs,
 ) *StatsCollector {
-	return &StatsCollector{
+	sc := &StatsCollector{
 		ApplicationStats: appStats,
+		insightsWriter:   insights,
 		phaseTimes:       phaseTime.Clone(),
 		st:               st,
 		knobs:            knobs,
 	}
+	return sc
 }
 
 // PhaseTimes returns the sessionphase.Times that this StatsCollector is
