7fb0486e055dd08f743a49f49c37d34b3024c6c5
Andrew Baptist <baptist@cockroachlabs.com>
2024-04-12 09:57:02 -0400

    roachtest: run Cleanup after Ctrl-C
    Previously if `roachtest run-operation ...` was run and the user
    terminated the process with a Ctrl-C, the Cleanup of the Operation was
    not run. In the case of network partitions, this would leave the
    partition in place. This change removes the early exit when Ctrl-C is
    pressed by falling through and calling Cleanup with a fresh Context.
    This is more expected behavior.
    
    Epic: none
    
    Release note: None

--- pkg/cmd/roachtest/run.go
+++ pkg/cmd/roachtest/run.go
@@ -530,13 +530,14 @@ func runOperation(register func(registry.Registry), filter string, clusterName s
 
 	op.Status(fmt.Sprintf("operation ran successfully; waiting %s before cleanup", roachtestflags.WaitBeforeCleanup))
 	select {
+	// Don't exit if the context is done due to a Ctrl-C, instead still run the
+	// cleanup code.
 	case <-ctx.Done():
-		return ctx.Err()
 	case <-time.After(roachtestflags.WaitBeforeCleanup):
 	}
 	op.Status("running cleanup")
 	func() {
-		ctx, cancel := context.WithTimeout(ctx, opSpec.Timeout)
+		ctx, cancel := context.WithTimeout(context.Background(), opSpec.Timeout)
 		defer cancel()
 
 		cleanup.Cleanup(ctx, op, c)
