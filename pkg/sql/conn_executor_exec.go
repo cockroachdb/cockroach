e798ef3f97fb7d2a4699f3ff2fdcb55b6429f25a
Xin Hao Zhang <xzhang@cockroachlabs.com>
2024-04-18 15:10:59 -0400

    sqlstats,insights: use StatsCollector to record txn insights
    This commit moves the recording of transaction insights one layer up.
    This decouples the writing of transaction insights from the writing
    of transaction statistics to the sql stats containers. Previously,
    if writing transaction stats was throttled we also skipped writing
    transaction insights. This can lead to a memory leak if we've already
    recorded statement insights for this transaction, as that memory is only
    released once the corresponding transaction insight is sent. The two
    processes may now occur independently.
    
    This fixes a memory leak in the scenario where we continue to send
    statement insights to the insights ingester without sending any
    transaction insights. The ingester frees the memory used by statement
    insights once it receives a transaction event. Previously, due to the
    coupling of writing sql stats and insights, if we transaction stats
    were throttled (e.g. due to in-memory capacities), we would start
    accumulating statement insights.
    
    Epic: none
    Fixes: #121803
    Release note: None

--- pkg/sql/conn_executor_exec.go
+++ pkg/sql/conn_executor_exec.go
@@ -3320,6 +3320,8 @@ func (ex *connExecutor) recordTransactionFinish(
 		)
 	}
 
+	ex.statsCollector.ObserveTransaction(ctx, transactionFingerprintID, recordedTxnStats)
+
 	return ex.statsCollector.RecordTransaction(
 		ctx,
 		transactionFingerprintID,
