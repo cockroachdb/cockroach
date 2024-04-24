54a5a4611bfbc8b7ad0c32337883ece676959d8e
Rafi Shamim <rafi@cockroachlabs.com>
2024-04-24 18:21:37 -0400

    cli/interactive_tests: wait for liveness to update in demo test
    Release note: None

--- pkg/cli/interactive_tests/test_demo_node_cmds.tcl
+++ pkg/cli/interactive_tests/test_demo_node_cmds.tcl
@@ -59,10 +59,12 @@ send "SELECT count(*) FROM system.users;\r"
 eexpect "1"
 eexpect "defaultdb>"
 
-# Now restart the node.
+# Now restart the node, and sleep to wait for liveness to update.
+# See: https://github.com/cockroachdb/cockroach/issues/122134#issuecomment-2050667479.
 send "\\demo restart 3\r"
 eexpect "node 3 has been restarted"
 eexpect "defaultdb>"
+sleep 1
 
 send "select node_id, draining, membership from crdb_internal.kv_node_liveness ORDER BY node_id;\r"
 eexpect "1 |    f     | active"
