81c80c315d6cc5072f9ec07592e3d203c16073ca
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-04-25 02:10:42 +0000

    replicastats: inline replicaStatsRecord into ReplicaStats
    Previously, we stored an array of 6 pointers to `replicaStatsRecord` in
    `ReplicaStats`, but I don't see a reason why we need to do that, so this
    commit switches to storing the records by value. As a result, we have
    6 less objects to track per `ReplicaStats` object. I've observed these
    to be very prominent in inuse_objects profiles.
    
    Here is a snippet I observed on 23.1.19 on a cluster with about 19k ranges:
    ```
        778276     778276 (flat, cum) 13.93% of Total
             .          .    188:	}
             .          .    189:}
             .          .    190:
             .          .    191:// NewReplicaStats constructs a new ReplicaStats tracker.
             .          .    192:func NewReplicaStats(now time.Time, getNodeLocality LocalityOracle) *ReplicaStats {
        188439     188439    193:	rs := &ReplicaStats{
             .          .    194:		getNodeLocality: getNodeLocality,
             .          .    195:	}
             .          .    196:
             .          .    197:	rs.lastRotate = now
             .          .    198:	for i := range rs.records {
        567991     567991    199:		rs.records[i] = &replicaStatsRecord{}
             .          .    200:		// Only create the locality counts when a locality oracle is given.
             .          .    201:		if getNodeLocality != nil {
         21846      21846    202:			rs.records[i].localityCounts = &PerLocalityCounts{}
             .          .    203:		}
             .          .    204:	}
             .          .    205:	// Set the first record to active. All other records will be initially
             .          .    206:	// inactive and empty.
             .          .    207:	rs.records[rs.idx].activate()
    ```
    This commit should remove about 10% of all live objects.
    
    Release note: None

--- pkg/kv/kvserver/replicastats/replica_stats.go
+++ pkg/kv/kvserver/replicastats/replica_stats.go
@@ -77,9 +77,9 @@ type ReplicaStats struct {
 	// require more memory than this slightly less precise windowing method:
 	//   http://dimacs.rutgers.edu/~graham/pubs/papers/fwddecay.pdf
 	idx int
-	// the window records are initialized once, then each window is reused
-	// internally by flipping an active field and clearing the fields.
-	records    [6]*replicaStatsRecord
+	// Each window record is reused internally by flipping an active field and
+	// clearing the fields.
+	records    [6]replicaStatsRecord
 	lastRotate time.Time
 
 	// Testing only.
@@ -136,7 +136,7 @@ func (rsr *replicaStatsRecord) deactivate() {
 }
 
 // mergeReplicaStatsRecords combines the record passed in with the receiver.
-func (rsr *replicaStatsRecord) mergeReplicaStatsRecords(other *replicaStatsRecord) {
+func (rsr *replicaStatsRecord) mergeReplicaStatsRecords(other replicaStatsRecord) {
 	// If the other record is inactive, then there is nothing to merge into the
 	// resulting record. Otherwise, regardless of whether the resulting record
 	// was originally active or inactive, we should merge the stats in and
@@ -191,18 +191,16 @@ func NewReplicaStats(now time.Time, getNodeLocality LocalityOracle) *ReplicaStat
 	rs := &ReplicaStats{
 		getNodeLocality: getNodeLocality,
 	}
-
-	rs.lastRotate = now
-	for i := range rs.records {
-		rs.records[i] = &replicaStatsRecord{}
-		// Only create the locality counts when a locality oracle is given.
-		if getNodeLocality != nil {
+	// Only create the locality counts when a locality oracle is given.
+	if getNodeLocality != nil {
+		for i := range rs.records {
 			rs.records[i].localityCounts = &PerLocalityCounts{}
 		}
 	}
 	// Set the first record to active. All other records will be initially
 	// inactive and empty.
 	rs.records[rs.idx].activate()
+	rs.lastRotate = now
 	return rs
 }
 
@@ -247,7 +245,7 @@ func (rs *ReplicaStats) SplitRequestCounts(other *ReplicaStats) {
 		// Otherwise, activate the rhs record and split the request count
 		// between the two.
 		other.records[i].activate()
-		rs.records[i].split(other.records[i])
+		rs.records[i].split(&other.records[i])
 	}
 }
 
@@ -255,7 +253,7 @@ func (rs *ReplicaStats) SplitRequestCounts(other *ReplicaStats) {
 func (rs *ReplicaStats) RecordCount(now time.Time, count float64, nodeID roachpb.NodeID) {
 	rs.maybeRotate(now)
 
-	record := rs.records[rs.idx]
+	record := &rs.records[rs.idx]
 	record.sum += count
 
 	if rs.getNodeLocality != nil {
