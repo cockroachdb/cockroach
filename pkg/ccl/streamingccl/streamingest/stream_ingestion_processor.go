2318f284a04073c8297d6322ff3bae1ff537b336
Michael Butler <butler@cockroachlabs.com>
2024-04-25 16:46:08 -0400

    streamingccl: replicate manual split points
    This patch teaches PCR to replicate source cluster manual split points to the
    destination cluster. Specifically, this patch configures the event_stream
    rangefeeds to emit rangefeed metadata events that indicate if a rangefeed
    spawned due to a manual split. Then, the event stream sends the manual split
    key over to the destination cluster.
    
    Informs #122846
    
    Release note: none

--- pkg/ccl/streamingccl/streamingest/stream_ingestion_processor.go
+++ pkg/ccl/streamingccl/streamingest/stream_ingestion_processor.go
@@ -101,6 +101,13 @@ var quantize = settings.RegisterDurationSettingWithExplicitUnit(
 	5*time.Second,
 )
 
+var ingestSplitEvent = settings.RegisterBoolSetting(
+	settings.SystemOnly,
+	"physical_replication.consumer.ingest_split_event",
+	"whether to ingest split events",
+	false,
+)
+
 var streamIngestionResultTypes = []*types.T{
 	types.Bytes, // jobspb.ResolvedSpans
 }
@@ -742,6 +749,10 @@ func (sip *streamIngestionProcessor) handleEvent(event partitionEvent) error {
 			return err
 		}
 		return nil
+	case streamingccl.SplitEvent:
+		if err := sip.handleSplitEvent(event.GetSplitEvent()); err != nil {
+			return err
+		}
 	default:
 		return errors.Newf("unknown streaming event type %v", event.Type())
 	}
@@ -837,6 +848,26 @@ func (sip *streamIngestionProcessor) bufferRangeKeyVal(
 	}
 	sip.buffer.addRangeKey(rangeKeyVal)
 	return nil
+}
+
+func (sip *streamIngestionProcessor) handleSplitEvent(key *roachpb.Key) error {
+	ctx, sp := tracing.ChildSpan(sip.Ctx(), "replicated-split")
+	defer sp.Finish()
+	if !ingestSplitEvent.Get(&sip.EvalCtx.Settings.SV) {
+		return nil
+	}
+
+	kvDB := sip.FlowCtx.Cfg.DB.KV()
+	rekey, ok, err := sip.rekey(*key)
+	if err != nil {
+		return err
+	}
+	if !ok {
+		return nil
+	}
+	log.VInfof(ctx, 2, "replicating split at %s", rekey)
+	expiration := kvDB.Clock().Now().AddDuration(time.Hour)
+	return kvDB.AdminSplit(ctx, rekey, expiration)
 }
 
 func (sip *streamIngestionProcessor) bufferKV(kv *roachpb.KeyValue) error {
@@ -986,7 +1017,7 @@ func (r *rangeKeyBatcher) flush(ctx context.Context, toFlush mvccRangeKeyValues)
 			ingestAsWrites = true
 		}
 
-		log.VInfof(ctx, 2,"sending SSTable [%s, %s) of size %d (as write: %v)", start, end, len(data), ingestAsWrites)
+		log.VInfof(ctx, 2, "sending SSTable [%s, %s) of size %d (as write: %v)", start, end, len(data), ingestAsWrites)
 		_, _, err := r.db.AddSSTable(ctx, start, end, data,
 			false /* disallowConflicts */, false, /* disallowShadowing */
 			hlc.Timestamp{}, nil /* stats */, ingestAsWrites,
@@ -999,7 +1030,7 @@ func (r *rangeKeyBatcher) flush(ctx context.Context, toFlush mvccRangeKeyValues)
 				}
 
 				split := mr.Desc.EndKey.AsRawKey()
-				log.VInfof(ctx,2, "SSTable cannot be added spanning range bounds. Spliting at %v", split)
+				log.VInfof(ctx, 2, "SSTable cannot be added spanning range bounds. Spliting at %v", split)
 				left, right, err := splitRangeKeySSTAtKey(ctx, r.settings, start, end, split, data)
 				if err != nil {
 					return err
