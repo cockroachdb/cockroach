23f29f3836a77d9f4bf9b49f014c59c0de58c4e0
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-04-25 01:03:00 +0000

    kvstreamer: speed up TestStreamerVaryingResponseSizes
    We just saw a test timeout due to BatchBytesLimit randomization, so this
    commit disables batch randomizations for `TestStreamerVaryingResponseSizes`.
    
    Release note: None

--- pkg/kv/kvclient/kvstreamer/streamer_test.go
+++ pkg/kv/kvclient/kvstreamer/streamer_test.go
@@ -30,6 +30,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/concurrency/lock"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/eval"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
@@ -554,7 +555,15 @@ func TestStreamerVaryingResponseSizes(t *testing.T) {
 
 	skip.UnderDuress(t)
 
-	s, db, _ := serverutils.StartServer(t, base.TestServerArgs{})
+	s, db, _ := serverutils.StartServer(t, base.TestServerArgs{
+		Knobs: base.TestingKnobs{
+			SQLEvalContext: &eval.TestingKnobs{
+				// We disable the randomization of some batch sizes because with
+				// some low values the test takes much longer.
+				ForceProductionValues: true,
+			},
+		},
+	})
 	defer s.Stopper().Stop(context.Background())
 
 	runner := sqlutils.MakeSQLRunner(db)
