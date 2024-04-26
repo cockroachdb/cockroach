9e85e5f03c02853b9455876ab49f268707b19af3
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-04-26 23:25:32 +0000

    cli: include cpu profiles into debug.zip
    This commit extends `GetFiles` API to support sending CPU profiles
    (collected by the CPU profiler) and uses this in the debug.zip to
    automatically include all relevant CPU profiles (this is in addition to
    collecting fresh CPU profiles at the time debug.zip is taken). In 24.1
    time frame we enabled the CPU profiler by default, so this is a nice
    addition to that. (Note that on CC clusters we had the CPU profiler
    enabled already, and SREs had to manually fetch the CPU profiles since
    it wasn't included into the debug.zip. This will remove that extra
    overhead.)
    
    Release note: None

--- pkg/server/get_local_files_test.go
+++ pkg/server/get_local_files_test.go
@@ -11,6 +11,7 @@
 package server
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
@@ -23,9 +24,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TestGetLocalFiles tests that retrieving local heap and goroutine profiles
-// returns and reads files when they exist and returns the expected error
-// when the file on encountered errors.
+// TestGetLocalFiles tests that retrieving local heap, goroutine, and cpu
+// profiles returns and reads files when they exist and returns the expected
+// error when the file on encountered errors.
 // Note that there is more extensive end to end testing of this done in
 // pkg/server/storage_api/files_test.go. This test was created to test the
 // error handling in response to #114453.
@@ -41,18 +42,30 @@ func TestGetLocalFiles(t *testing.T) {
 	if err := os.MkdirAll(testHeapDir, os.ModePerm); err != nil {
 		t.Fatal(err)
 	}
+	testCPUDir := filepath.Join(tempDir, "cpu_profiles")
+	if err := os.MkdirAll(testCPUDir, os.ModePerm); err != nil {
+		t.Fatal(err)
+	}
 
 	t.Run("read files successfully", func(t *testing.T) {
-		testHeapFile := filepath.Join(testHeapDir, "im-in-the-heap-dir")
-		if err := os.WriteFile(testHeapFile, []byte("heap stuff"), 0644); err != nil {
-			t.Fatal(err)
+		for _, test := range []string{"heap", "cpu"} {
+			t.Run(test, func(t *testing.T) {
+				testDir, typ := testHeapDir, serverpb.FileType_HEAP
+				if test == "cpu" {
+					testDir, typ = testCPUDir, serverpb.FileType_CPU
+				}
+				fileName := fmt.Sprintf("im-in-the-%s-dir", test)
+				testFile := filepath.Join(testDir, fileName)
+				if err := os.WriteFile(testFile, []byte(fmt.Sprintf("%s stuff", test)), 0644); err != nil {
+					t.Fatal(err)
+				}
+				req := &serverpb.GetFilesRequest{NodeId: "local", Type: typ, Patterns: []string{"*"}}
+				res, err := getLocalFiles(req, testHeapDir, "", testCPUDir, os.Stat, os.ReadFile)
+				require.NoError(t, err)
+				require.Equal(t, 1, len(res.Files))
+				require.Equal(t, fileName, res.Files[0].Name)
+			})
 		}
-		req := &serverpb.GetFilesRequest{
-			NodeId: "local", Type: serverpb.FileType_HEAP, Patterns: []string{"*"}}
-		res, err := getLocalFiles(req, testHeapDir, "", os.Stat, os.ReadFile)
-		require.NoError(t, err)
-		require.Equal(t, 1, len(res.Files))
-		require.Equal(t, "im-in-the-heap-dir", res.Files[0].Name)
 	})
 
 	t.Run("stat file error", func(t *testing.T) {
@@ -66,7 +79,7 @@ func TestGetLocalFiles(t *testing.T) {
 		}
 		req := &serverpb.GetFilesRequest{
 			NodeId: "local", Type: serverpb.FileType_HEAP, Patterns: []string{"*"}}
-		_, err := getLocalFiles(req, testHeapDir, "", statFileWithErr, os.ReadFile)
+		_, err := getLocalFiles(req, testHeapDir, "", "", statFileWithErr, os.ReadFile)
 		require.ErrorContains(t, err, "stat error")
 	})
 
@@ -81,19 +94,24 @@ func TestGetLocalFiles(t *testing.T) {
 		}
 		req := &serverpb.GetFilesRequest{
 			NodeId: "local", Type: serverpb.FileType_HEAP, Patterns: []string{"*"}}
-		_, err := getLocalFiles(req, testHeapDir, "", os.Stat, readFileWithErr)
+		_, err := getLocalFiles(req, testHeapDir, "", "", os.Stat, readFileWithErr)
 		require.ErrorContains(t, err, "read error")
 	})
 
 	t.Run("dirs not implemented", func(t *testing.T) {
 		req := &serverpb.GetFilesRequest{
 			NodeId: "local", Type: serverpb.FileType_HEAP, Patterns: []string{"*"}}
-		_, err := getLocalFiles(req, "", "nonexistent", os.Stat, os.ReadFile)
+		_, err := getLocalFiles(req, "", "nonexistent", "nonexistent", os.Stat, os.ReadFile)
 		require.ErrorContains(t, err, "dump directory not configured")
 
 		req = &serverpb.GetFilesRequest{
 			NodeId: "local", Type: serverpb.FileType_GOROUTINES, Patterns: []string{"*"}}
-		_, err = getLocalFiles(req, "nonexistent", "", os.Stat, os.ReadFile)
+		_, err = getLocalFiles(req, "nonexistent", "", "nonexistent", os.Stat, os.ReadFile)
+		require.ErrorContains(t, err, "dump directory not configured")
+
+		req = &serverpb.GetFilesRequest{
+			NodeId: "local", Type: serverpb.FileType_CPU, Patterns: []string{"*"}}
+		_, err = getLocalFiles(req, "nonexistent", "nonexistent", "", os.Stat, os.ReadFile)
 		require.ErrorContains(t, err, "dump directory not configured")
 	})
 }
