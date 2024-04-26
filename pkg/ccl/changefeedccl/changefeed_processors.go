72615a02d9317c4014a03f92a8e5606870582332
Miles Frankel <miles.frankel@cockroachlabs.com>
2024-04-26 15:07:20 -0400

    changefeedccl: add new table bytes metric for usage data
    Add a new metric to facilitate the new changefeed
    billing strategy -
    `changefeed.usage.table_bytes` - This is a sum
    of the bytes watched per changefeed. It
    intentionally counts tables watched by multiple
    changefeeds multiple times. Also add auxiliary
    metrics about usage data collection:
    `changefeed.usage.error_count` (counter of
    errors encountered running the query that
    generates the table_bytes metric) and
    `changefeed.usage.query_duration` (histogram of
    how long the latter took). The collection of this
    data and emission of these metrics are gated
    behind an environment variable only intended to be
    set in ccloud:
    `COCKROACH_ENABLE_CLOUD_BILLING_ACCOUNTING`
    
    Epic: CRDB-37031
    Resolves: #121781
    
    Release note (enterprise change): Add a new metric
    to facilitate the new changefeed billing strategy
    for usage in Cockroach Cloud.

--- pkg/ccl/changefeedccl/changefeed_processors.go
+++ pkg/ccl/changefeedccl/changefeed_processors.go
@@ -12,6 +12,7 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/ccl/changefeedccl/cdcutils"
@@ -25,6 +26,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/execinfra"
 	"github.com/cockroachdb/cockroach/pkg/sql/execinfrapb"
 	"github.com/cockroachdb/cockroach/pkg/sql/isql"
@@ -32,6 +34,8 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
 	"github.com/cockroachdb/cockroach/pkg/sql/rowenc"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/util/cancelchecker"
+	"github.com/cockroachdb/cockroach/pkg/util/envutil"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/log/logcrash"
@@ -43,7 +47,13 @@ import (
 	"github.com/cockroachdb/errors"
 	"github.com/cockroachdb/logtags"
 	"github.com/cockroachdb/redact"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+const EnableCloudBillingAccountingEnvVar = "COCKROACH_ENABLE_CLOUD_BILLING_ACCOUNTING"
+
+var EnableCloudBillingAccounting = envutil.EnvOrDefaultBool(EnableCloudBillingAccountingEnvVar, false)
 
 type changeAggregator struct {
 	execinfra.ProcessorBase
@@ -976,6 +986,9 @@ type changeFrontier struct {
 	sliMetricsID int64
 
 	knobs TestingKnobs
+
+	usageWg       sync.WaitGroup
+	usageWgCancel context.CancelFunc
 }
 
 const (
@@ -1122,11 +1135,12 @@ func newChangeFrontierProcessor(
 	}
 
 	cf := &changeFrontier{
-		flowCtx:  flowCtx,
-		spec:     spec,
-		memAcc:   memMonitor.MakeBoundAccount(),
-		input:    input,
-		frontier: sf,
+		flowCtx:       flowCtx,
+		spec:          spec,
+		memAcc:        memMonitor.MakeBoundAccount(),
+		input:         input,
+		frontier:      sf,
+		usageWgCancel: func() {},
 	}
 
 	if cfKnobs, ok := flowCtx.TestingKnobs().Changefeed.(*TestingKnobs); ok {
@@ -1279,6 +1293,15 @@ func (cf *changeFrontier) Start(ctx context.Context) {
 			// running status around for a while before we override it.
 			cf.js.lastRunStatusUpdate = timeutil.Now()
 		}
+
+		// Start the usage metric reporting goroutine.
+		usageCtx, usageCancel := context.WithCancel(ctx)
+		cf.usageWgCancel = usageCancel
+		cf.usageWg.Add(1)
+		go func() {
+			defer cf.usageWg.Done()
+			cf.runUsageMetricReporting(usageCtx)
+		}()
 	}
 
 	func() {
@@ -1304,7 +1327,70 @@ func (cf *changeFrontier) Start(ctx context.Context) {
 	}()
 }
 
+func (cf *changeFrontier) runUsageMetricReporting(ctx context.Context) {
+	if cf.spec.JobID == 0 { // don't report for core (non-enterprise) changefeeds
+		return
+	}
+
+	// If cloud billing accounting is not enabled via an env var, don't do this work.
+	if !EnableCloudBillingAccounting {
+		return
+	}
+
+	// Deregister the usage metric when the goroutine exits so we don't over-report.
+	defer cf.metrics.UsageMetrics.DeregisterJobMetrics(cf.spec.JobID)
+
+	skipNextWait := cf.knobs.SkipFirstUsageMetricsReportingWait
+
+	var t timeutil.Timer
+	defer t.Stop()
+	var lastDuration time.Duration
+	for ctx.Err() == nil {
+		reportingInterval := jitter(changefeedbase.UsageMetricsReportingInterval.Get(&cf.flowCtx.Cfg.Settings.SV))
+		var start time.Time
+
+		// Skip the first wait if requested, in tests.
+		if !skipNextWait {
+			t.Reset(reportingInterval - lastDuration)
+			select {
+			case start = <-t.C:
+				t.Read = true
+			case <-ctx.Done():
+				return
+			}
+		} else {
+			start = timeutil.Now()
+			skipNextWait = false
+		}
+
+		execCfg := cf.flowCtx.Cfg.ExecutorConfig.(*sql.ExecutorConfig)
+		if cf.knobs.OverrideExecCfg != nil {
+			execCfg = cf.knobs.OverrideExecCfg(execCfg)
+		}
+
+		// Set a timeout of half the ivl so we still have a duty cycle in degenerate scenarios. This is just a guess though..
+		reqCtx, cancel := context.WithTimeout(ctx, reportingInterval/2)
+		res, err := FetchChangefeedUsageBytes(reqCtx, execCfg, cf.js.job.Payload())
+		cancel()
+		if err != nil {
+			// Don't increment the error count if it's due to us being shut down, or due to a backing table being dropped (since that will result in us shutting down also).
+			if shouldCountUsageError(err) {
+				log.Warningf(ctx, "failed to fetch usage bytes: %v", err)
+				cf.metrics.UsageMetrics.RecordError()
+			}
+			continue
+		}
+		lastDuration = timeutil.Since(start)
+		cf.metrics.UsageMetrics.UpdateTableBytes(cf.spec.JobID, res, lastDuration)
+	}
+}
+
 func (cf *changeFrontier) close() {
+	// Shut down the usage metric reporting goroutine first, since otherwise
+	// we can use a span after it's finished.
+	cf.usageWgCancel()
+	cf.usageWg.Wait()
+
 	if cf.InternalClose() {
 		if cf.metrics != nil {
 			cf.closeMetrics()
@@ -1949,4 +2035,20 @@ func (f *schemaChangeFrontier) hasLaggingSpans(
 		frontier = defaultIfEmpty
 	}
 	return frontier.Add(lagThresholdNanos, 0).Less(f.latestTs)
+}
+
+// jitter returns a new duration with +-10% jitter applied to the given duration.
+func jitter(d time.Duration) time.Duration {
+	if d == 0 {
+		return 0
+	}
+	start, end := int64(d-d/10), int64(d+d/10)
+	return time.Duration(start + rand.Int63n(end-start))
+}
+
+func shouldCountUsageError(err error) bool {
+	return !errors.Is(err, context.Canceled) &&
+		!errors.Is(err, cancelchecker.QueryCanceledError) &&
+		pgerror.GetPGCode(err) != pgcode.UndefinedTable &&
+		status.Code(err) != codes.Canceled
 }
