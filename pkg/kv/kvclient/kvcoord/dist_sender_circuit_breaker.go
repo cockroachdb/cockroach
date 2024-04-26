81673f33dd37bed28139fc50ca4e96063657583e
Arul Ajmani <arulajmani@gmail.com>
2024-04-25 20:56:17 -0400

    kvcoord: disable DistSender circuit breakers by default
    Closes https://github.com/cockroachdb/cockroach/issues/122983
    
    Release note: None

--- pkg/kv/kvclient/kvcoord/dist_sender_circuit_breaker.go
+++ pkg/kv/kvclient/kvcoord/dist_sender_circuit_breaker.go
@@ -63,7 +63,8 @@ const (
 	// level issue that causes a large number of circuit breakers to be tripped on
 	// a client has the potential of causing a big goroutine spike on the client.
 	// We currently don't have good scalability numbers to measure the impact of
-	// this -- so this is deemed risky.
+	// this -- once we do, we can revaluate the default mode we run in if the
+	// numbers look good. Else we may want to make these things more scalable.
 	DistSenderCircuitBreakersAllRanges
 )
 
@@ -72,7 +73,7 @@ var (
 		settings.ApplicationLevel,
 		"kv.dist_sender.circuit_breakers.mode",
 		"whether to trip circuit breakers for failing or stalled replicas",
-		"all ranges",
+		"off",
 		map[int64]string{
 			int64(DistSenderCircuitBreakersOff):               "off",
 			int64(DistSenderCircuitBreakersLivenessRangeOnly): "liveness range only",
