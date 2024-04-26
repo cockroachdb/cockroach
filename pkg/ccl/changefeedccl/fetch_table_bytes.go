6a619bd1300fa6fd0c1a3639a1e78b5a6c5559c1
Miles Frankel <miles.frankel@cockroachlabs.com>
2024-04-26 15:07:20 -0400

    changefeedccl: add new table bytes metric for usage data
    Add a new metric to facilitate the new changefeed
    billing strategy -
    `changefeed.usage.table_bytes` - This is a sum
    of the bytes watched per changefeed. It
    intentionally counts tables watched by multiple
    changefeeds multiple times. Also add auxiliary
    metrics about usage data collection:
    `changefeed.usage.error_count` (counter of
    errors encountered running the query that
    generates the table_bytes metric) and
    `changefeed.usage.query_duration` (histogram of
    how long the latter took). The collection of this
    data and emission of these metrics are gated
    behind an environment variable only intended to be
    set in ccloud:
    `COCKROACH_ENABLE_CLOUD_BILLING_ACCOUNTING`
    
    Epic: CRDB-37031
    Resolves: #121781
    
    Release note (enterprise change): Add a new metric
    to facilitate the new changefeed billing strategy
    for usage in Cockroach Cloud.

---
+++ pkg/ccl/changefeedccl/fetch_table_bytes.go
@@ -0,0 +1,124 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package changefeedccl
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/sql"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descs"
+	"github.com/cockroachdb/cockroach/pkg/sql/isql"
+	"github.com/cockroachdb/cockroach/pkg/util/admission/admissionpb"
+	"github.com/cockroachdb/errors"
+)
+
+// FetchChangefeedUsageBytes fetches the total number of bytes of data watched
+// by the given changefeed.
+func FetchChangefeedUsageBytes(
+	ctx context.Context, execCfg *sql.ExecutorConfig, payload jobspb.Payload,
+) (int64, error) {
+	details, err := detailsFromPayload(payload)
+	if err != nil {
+		return 0, err
+	}
+
+	uniqueTableIDs := make(map[descpb.ID]struct{})
+	// Check both possible locations for table data due to older proto version.
+	// Inspired by AllTargets in changefeedccl/changefeed.go.
+	if len(details.TargetSpecifications) > 0 {
+		for _, ts := range details.TargetSpecifications {
+			if ts.TableID > 0 {
+				uniqueTableIDs[ts.TableID] = struct{}{}
+			}
+		}
+	} else {
+		for id := range details.Tables {
+			uniqueTableIDs[id] = struct{}{}
+		}
+	}
+
+	tableIDs := make([]descpb.ID, 0, len(uniqueTableIDs))
+	for id := range uniqueTableIDs {
+		tableIDs = append(tableIDs, id)
+	}
+
+	total, err := fetchSizes(ctx, execCfg, tableIDs)
+	if err != nil {
+		return 0, err
+	}
+	return total, nil
+}
+
+func fetchSizes(
+	ctx context.Context, execCfg *sql.ExecutorConfig, tableIDs []descpb.ID,
+) (total int64, err error) {
+	// Build list of spans for all tables.
+	allSpans := make([]roachpb.Span, 0, len(tableIDs))
+	for _, id := range tableIDs {
+		desc, err := getTableDesc(ctx, execCfg, id)
+		if err != nil {
+			return 0, err
+		}
+
+		// Include all indexes, not just the primary.
+		tableSpans := desc.AllIndexSpans(execCfg.Codec)
+		allSpans = append(allSpans, tableSpans...)
+	}
+
+	// Fetch span stats and fill in table sizes.
+	// NodeID=0 means "fan out to all nodes".
+	req := &roachpb.SpanStatsRequest{NodeID: "0", Spans: allSpans}
+	resp, err := execCfg.TenantStatusServer.SpanStats(ctx, req)
+	if err != nil {
+		return 0, err
+	}
+	if len(resp.Errors) > 0 {
+		return 0, errors.Newf("errors fetching span stats: %v", resp.Errors)
+	}
+
+	for _, stats := range resp.SpanToStats {
+		total += stats.ApproximateTotalStats.LiveBytes
+	}
+
+	return total, nil
+}
+
+func getTableDesc(
+	ctx context.Context, execCfg *sql.ExecutorConfig, tableID descpb.ID,
+) (catalog.TableDescriptor, error) {
+	var desc catalog.TableDescriptor
+	f := func(ctx context.Context, txn descs.Txn) error {
+		tableDesc, err := txn.Descriptors().ByID(txn.KV()).WithoutNonPublic().Get().Table(ctx, tableID)
+		if err != nil {
+			return err
+		}
+		desc = tableDesc
+		return nil
+	}
+	if err := execCfg.InternalDB.DescsTxn(ctx, f, isql.WithPriority(admissionpb.LowPri)); err != nil {
+		return nil, err
+	}
+	return desc, nil
+}
+
+func detailsFromPayload(payload jobspb.Payload) (*jobspb.ChangefeedDetails, error) {
+	details := payload.GetDetails()
+	if details == nil {
+		return nil, errors.AssertionFailedf("no details")
+	}
+	cfDetails, ok := details.(*jobspb.Payload_Changefeed)
+	if !ok {
+		return nil, errors.AssertionFailedf("unexpected details type %T", details)
+	}
+	return cfDetails.Changefeed, nil
+}
