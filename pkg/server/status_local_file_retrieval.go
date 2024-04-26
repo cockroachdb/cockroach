600cbda585962008c7da57747d79f0262f4d2c1f
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-04-26 16:25:32 -0700

    cli: include cpu profiles into debug.zip
    This commit extends `GetFiles` API to support sending CPU profiles
    (collected by the CPU profiler) and uses this in the debug.zip to
    automatically include all relevant CPU profiles (this is in addition to
    collecting fresh CPU profiles at the time debug.zip is taken). In 24.1
    time frame we enabled the CPU profiler by default, so this is a nice
    addition to that. (Note that on CC clusters we had the CPU profiler
    enabled already, and SREs had to manually fetch the CPU profiles since
    it wasn't included into the debug.zip. This will remove that extra
    overhead.)
    
    Release note: None

--- pkg/server/status_local_file_retrieval.go
+++ pkg/server/status_local_file_retrieval.go
@@ -103,6 +103,7 @@ func getLocalFiles(
 	req *serverpb.GetFilesRequest,
 	heapProfileDirName string,
 	goroutineDumpDirName string,
+	cpuProfileDirName string,
 	statFileFn func(string) (os.FileInfo, error),
 	readFileFn func(string) ([]byte, error),
 ) (*serverpb.GetFilesResponse, error) {
@@ -114,6 +115,8 @@ func getLocalFiles(
 		dir = heapProfileDirName
 	case serverpb.FileType_GOROUTINES: // Requesting for saved Goroutine dumps.
 		dir = goroutineDumpDirName
+	case serverpb.FileType_CPU: // Requesting for saved CPU Profiles.
+		dir = cpuProfileDirName
 	default:
 		return nil, status.Errorf(codes.InvalidArgument, "unknown file type: %s", req.Type)
 	}
