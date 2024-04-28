eeb36a02359afba64429d57ec218cf2322dc9edf
Wenyi Hu <wenyi@cockroachlabs.com>
2024-04-28 16:28:38 -0400

    rangefeed: improve regression seen from memory accounting
    This patch improves the regression we saw from #123076. Note that this pr only
    refactors the existing code but doesn't change any functionalities.
    
    Related: https://github.com/cockroachdb/cockroach/issues/123076
    Release note: none

--- pkg/kv/kvserver/rangefeed/scheduled_processor.go
+++ pkg/kv/kvserver/rangefeed/scheduled_processor.go
@@ -448,7 +448,7 @@ func (p *ScheduledProcessor) enqueueEventInternal(
 	// inserting value into channel.
 	var alloc *SharedBudgetAllocation
 	if p.MemBudget != nil {
-		size := e.MemUsage()
+		size := MemUsage(e)
 		if size > 0 {
 			var err error
 			// First we will try non-blocking fast path to allocate memory budget.
