71240d3b53b1a4003f4ec16541b0898f4110bded
Miles Frankel <miles.frankel@cockroachlabs.com>
2024-04-29 17:34:58 -0400

    testwip

---
+++ pkg/jobs/metricspoller/changefeed_billing_test.go
@@ -0,0 +1,32 @@
+package metricspoller
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/security/username"
+	"github.com/cockroachdb/cockroach/pkg/sql"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFetchChangefeedBillingBytes(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	ctx := context.Background()
+	params := base.TestServerArgs{}
+	s := serverutils.StartServerOnly(t, params)
+	defer s.Stopper().Stop(ctx)
+
+	execCfg := s.ExecutorConfig().(*sql.ExecutorConfig)
+	execCtx, close := sql.MakeJobExecContext(ctx, "test", username.NodeUserName(), nil, execCfg)
+	defer close()
+
+	res, err := fetchChangefeedBillingBytes(ctx, execCtx)
+	require.NoError(t, err)
+	_ = res
+}
