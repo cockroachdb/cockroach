7de4b893da3750e065bf04ddbb867328b7e098c3
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-04-29 10:36:55 -0700

    stats: add a testing knob to disable initial table collection
    This is needed to stabilize `TestScheduledJobsConsumption` on 24.1 and
    earlier branches. This test became flaky due to the partial backport of
    121861 to all branches.
    
    Release note: None

--- pkg/server/server_sql.go
+++ pkg/server/server_sql.go
@@ -1132,12 +1132,17 @@ func newSQLServer(ctx context.Context, cfg sqlServerArgs) (*SQLServer, error) {
 		}
 	}
 
+	var tableStatsTestingKnobs *stats.TableStatsTestingKnobs
+	if tableStatsKnobs := cfg.TestingKnobs.TableStatsKnobs; tableStatsKnobs != nil {
+		tableStatsTestingKnobs = tableStatsKnobs.(*stats.TableStatsTestingKnobs)
+	}
 	statsRefresher := stats.MakeRefresher(
 		cfg.AmbientCtx,
 		cfg.Settings,
 		cfg.circularInternalExecutor,
 		execCfg.TableStatsCache,
 		stats.DefaultAsOfTime,
+		tableStatsTestingKnobs,
 	)
 	execCfg.StatsRefresher = statsRefresher
 
