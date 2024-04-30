46acd12d4edcb5d56f57409f2e5a6b4a6bfaae79
Marcus Gartner <marcus@cockroachlabs.com>
2024-04-30 13:26:12 -0400

    opt: add session setting for virtual computed column implication
    The `optimizer_prove_implication_with_virtual_computed_columns` has been
    added which, when enabled, indicates that virtual computed columns
    should be considered during partial index implication to try to prove
    that a filter implies a predicate.
    
    Release note: None

--- pkg/sql/opt/xform/scan_index_iter.go
+++ pkg/sql/opt/xform/scan_index_iter.go
@@ -321,8 +321,12 @@ func (it *scanIndexIter) filtersImplyPredicate(
 	pred memo.FiltersExpr,
 ) (remainingFilters memo.FiltersExpr, ok bool) {
 	// Return the remaining filters if the filters imply the predicate.
+	var computedCols map[opt.ColumnID]opt.ScalarExpr
+	if it.e.evalCtx.SessionData().OptimizerProveImplicationWithVirtualComputedColumns {
+		computedCols = it.tabMeta.ComputedCols
+	}
 	if remainingFilters, ok =
-		it.im.FiltersImplyPredicate(it.filters, pred, it.tabMeta.ComputedCols); ok {
+		it.im.FiltersImplyPredicate(it.filters, pred, computedCols); ok {
 		return remainingFilters, true
 	}
 
@@ -335,7 +339,7 @@ func (it *scanIndexIter) filtersImplyPredicate(
 	// originalFilters-implication.
 	if it.originalFilters != nil {
 		if remainingFilters, ok =
-			it.im.FiltersImplyPredicate(it.originalFilters, pred, it.tabMeta.ComputedCols); ok {
+			it.im.FiltersImplyPredicate(it.originalFilters, pred, computedCols); ok {
 			return remainingFilters, true
 		}
 	}
