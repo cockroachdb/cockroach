3c78362060efcf93ccc83f15bf0fc378e6aea83b
Annie Pompa <annie@cockroachlabs.com>
2024-05-02 15:10:46 -0400

    doctor: skip validation for dropped descriptors
    In some cases, dropped descriptors appear in our `system.descriptors`
    table with dangling job mutations without an associated job.
    This patch teaches debug doctor examine to skip validation
    on such dropped descriptors.
    
    Epic: none
    
    Fixes: #123477
    Fixes: #122956
    
    Release note: none

--- pkg/cli/doctor_test.go
+++ pkg/cli/doctor_test.go
@@ -20,7 +20,14 @@ import (
 	"github.com/cockroachdb/datadriven"
 )
 
-var descriptorWithFKMutation = `'{
+func descriptorWithFKMutation(isDropped bool) string {
+	dropState := ``
+	dropTime := ``
+	if isDropped {
+		dropState = `"state": "DROP",`
+		dropTime = `"dropTime": "1713940113794672911",`
+	}
+	return `'{
       "table": {
           "columns": [
               {
@@ -32,8 +39,9 @@ var descriptorWithFKMutation = `'{
                       "width": 64
                   }
               }
-          ],
-          "createAsOfTime": {
+          ],` +
+		dropTime +
+		`"createAsOfTime": {
               "logical": 1,
               "wallTime": "1713940112376217646"
           },
@@ -85,7 +93,7 @@ var descriptorWithFKMutation = `'{
                   "state": "DELETE_ONLY"
               }
           ],
-          "name": "bar",
+          "name": "foo",
           "nextColumnId": 2,
           "nextConstraintId": 3,
           "nextFamilyId": 1,
@@ -107,7 +115,7 @@ var descriptorWithFKMutation = `'{
                   1
               ],
               "keyColumnNames": [
-                  "col143_w3_144"
+                  "i"
               ],
               "name": "table_w3_143_pkey",
               "partitioning": {},
@@ -133,15 +141,18 @@ var descriptorWithFKMutation = `'{
           },
           "replacementOf": {
               "time": {}
-          },
-          "unexposedParentSchemaId": 381,
+          },` +
+		dropState +
+		`"unexposedParentSchemaId": 381,
           "version": "2"
       }
   }'`
+}
 
 // This test doctoring a secure cluster.
 func TestDoctorCluster(t *testing.T) {
 	defer leaktest.AfterTest(t)()
+	//
 	c := NewCLITest(TestCLIParams{T: t})
 	defer c.Cleanup()
 
@@ -174,16 +185,20 @@ func TestDoctorCluster(t *testing.T) {
 // TestDoctorClusterBroken tests that debug doctor examine will pick up a multitude of issues on a corrupt descriptor.
 func TestDoctorClusterBroken(t *testing.T) {
 	defer leaktest.AfterTest(t)()
-	c := NewCLITest(TestCLIParams{T: t})
+	c := NewCLITest(TestCLIParams{T: t, DisableAutoStats: true})
 	defer c.Cleanup()
 
-	jobDesc := fmt.Sprintf("SELECT crdb_internal.unsafe_upsert_descriptor('foo'::regclass::oid::int,"+
-		"crdb_internal.json_to_pb('cockroach.sql.sqlbase.Descriptor', %s::jsonb), true)", descriptorWithFKMutation)
+	desc := fmt.Sprintf("SELECT crdb_internal.unsafe_upsert_descriptor('foo'::regclass::oid::int,"+
+		"crdb_internal.json_to_pb('cockroach.sql.sqlbase.Descriptor', %s::jsonb), true)", descriptorWithFKMutation(false /* isDropped */))
 
-	// Introduce a descriptor with an attached job mutation (along with other issues).
+	// Introduce a descriptor with an attached job mutation (along with other issues). We want to ensure that the number of
+	// jobs created is deterministic (auto table stats will be collected due to the "schema change" on foo); therefore,
+	// we should disable automatic stats collection and instead create our own.
 	c.RunWithArgs([]string{"sql", "-e", strings.Join([]string{
 		"CREATE TABLE foo (i INT)",
-		jobDesc,
+		desc,
+		"CREATE STATISTICS foo_stats FROM foo",
+		"SELECT pg_catalog.pg_sleep(1)",
 	}, ";\n"),
 	})
 
@@ -195,6 +210,38 @@ func TestDoctorClusterBroken(t *testing.T) {
 
 		// Using datadriven allows TESTFLAGS=-rewrite.
 		datadriven.RunTest(t, datapathutils.TestDataPath(t, "doctor", "test_examine_cluster_jobs"), func(t *testing.T, td *datadriven.TestData) string {
+			return out
+		})
+	})
+}
+
+// TestDoctorClusterDropped tests that debug doctor examine will avoid validating dropped descriptors.
+func TestDoctorClusterDropped(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	c := NewCLITest(TestCLIParams{T: t})
+	defer c.Cleanup()
+
+	desc := fmt.Sprintf("SELECT crdb_internal.unsafe_upsert_descriptor('foo'::regclass::oid::int,"+
+		"crdb_internal.json_to_pb('cockroach.sql.sqlbase.Descriptor', %s::jsonb), true)", descriptorWithFKMutation(true /* isDropped */))
+	// Introduce a dropped descriptor with an attached job mutation (along with other issues).
+	c.RunWithArgs([]string{"sql", "-e", strings.Join([]string{
+		"CREATE TABLE foo (i INT)",
+		desc,
+		"INSERT INTO system.users VALUES ('node', NULL, true, 3)",
+		"GRANT node TO root",
+		"DELETE FROM system.namespace WHERE name = 'foo'",
+		"SELECT pg_catalog.pg_sleep(1)",
+	}, ";\n"),
+	})
+
+	t.Run("examine", func(t *testing.T) {
+		out, err := c.RunWithCapture("debug doctor examine cluster")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Using datadriven allows TESTFLAGS=-rewrite.
+		datadriven.RunTest(t, datapathutils.TestDataPath(t, "doctor", "test_examine_cluster_dropped"), func(t *testing.T, td *datadriven.TestData) string {
 			return out
 		})
 	})
--- pkg/cli/testutils.go
+++ pkg/cli/testutils.go
@@ -34,6 +34,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/security/securitytest"
 	"github.com/cockroachdb/cockroach/pkg/security/username"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlstats"
+	"github.com/cockroachdb/cockroach/pkg/sql/stats"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/errors"
@@ -100,6 +101,10 @@ type TestCLIParams struct {
 	// UseSystemTenant is used to force the test to target the system tenant
 	// in a shared process multitenant test.
 	UseSystemTenant bool
+
+	// DisableAutoStats is used to disable the collection of automatic table statistics
+	// for the entire cluster.
+	DisableAutoStats bool
 }
 
 // testTempFilePrefix is a sentinel marker to be used as the prefix of a
@@ -145,6 +150,11 @@ func newCLITestWithArgs(params TestCLIParams, argsFn func(args *base.TestServerA
 
 	c.cleanupFunc = func() error { return nil }
 
+	settings := makeClusterSettings()
+	if params.DisableAutoStats {
+		stats.AutomaticStatisticsClusterMode.Override(context.Background(), &settings.SV, false)
+	}
+
 	if !params.NoServer {
 		if !params.Insecure {
 			c.cleanupFunc = securitytest.CreateTestCerts(certsDir)
@@ -154,6 +164,7 @@ func newCLITestWithArgs(params TestCLIParams, argsFn func(args *base.TestServerA
 			DefaultTestTenant: base.TestControlsTenantsExplicitly,
 
 			Insecure:      params.Insecure,
+			Settings:      settings,
 			SSLCertsDir:   c.certsDir,
 			StoreSpecs:    params.StoreSpecs,
 			Locality:      params.Locality,
