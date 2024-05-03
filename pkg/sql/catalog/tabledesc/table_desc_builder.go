9d8618b49b133f4e28ddaa39cda0b8f242d189cc
Faizan Qazi <faizan@cockroachlabs.com>
2024-05-03 12:33:38 -0400

    catalog: repair descriptors with incorrect self references in FK
    Previously, we had a corruption bug that could lead to descriptors with
    self references that pointed to incorrect descriptor IDs. This could
    lead to validation errors that make tables inaccessible. To address
    this, this patch will add an automatic post deserialization repair to
    fix these values to the current descriptor ID.
    
    Fixes: #123555, #123493
    
    Release note (bug fix): automatically repair tables which see the
    following error "invalid inbound foreign key ... origin table ID should
    be" or "invalid outbound foreign key ... reference table ID should be".

--- pkg/sql/catalog/tabledesc/table_desc_builder.go
+++ pkg/sql/catalog/tabledesc/table_desc_builder.go
@@ -403,6 +403,7 @@ func maybeFillInDescriptor(
 	set(catalog.SetCheckConstraintColumnIDs, maybeSetCheckConstraintColumnIDs(desc))
 	set(catalog.StrippedDanglingSelfBackReferences, maybeStripDanglingSelfBackReferences(desc))
 	set(catalog.FixSecondaryIndexEncodingType, maybeFixSecondaryIndexEncodingType(desc))
+	set(catalog.FixedIncorrectForeignKeyOrigins, maybeFixForeignKeySelfReferences(desc))
 	return changes, nil
 }
 
@@ -1230,4 +1231,24 @@ func resolveTableNamesForIDs(
 	}
 
 	return result, nil
+}
+
+// maybeFixForeignKeySelfReferences fixes references that should point to the
+// current descriptor inside OutboundFKs and InboundFKs.
+func maybeFixForeignKeySelfReferences(tableDesc *descpb.TableDescriptor) (wasRepaired bool) {
+	for idx := range tableDesc.OutboundFKs {
+		fk := &tableDesc.OutboundFKs[idx]
+		if fk.OriginTableID != tableDesc.ID {
+			fk.OriginTableID = tableDesc.ID
+			wasRepaired = true
+		}
+	}
+	for idx := range tableDesc.InboundFKs {
+		fk := &tableDesc.InboundFKs[idx]
+		if fk.ReferencedTableID != tableDesc.ID {
+			fk.ReferencedTableID = tableDesc.ID
+			wasRepaired = true
+		}
+	}
+	return wasRepaired
 }
