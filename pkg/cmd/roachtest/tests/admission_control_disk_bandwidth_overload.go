f61ae9a6ecd52fdef2744adba335b3ea1bc1328d
Aaditya Sondhi <20070511+aadityasondhi@users.noreply.github.com>
2024-05-15 12:59:21 -0400

    wip - add metric assertion

--- pkg/cmd/roachtest/tests/admission_control_disk_bandwidth_overload.go
+++ pkg/cmd/roachtest/tests/admission_control_disk_bandwidth_overload.go
@@ -19,6 +19,7 @@ import (
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/cluster"
+	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/clusterstats"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/grafana"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/option"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/registry"
@@ -27,6 +28,9 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/test"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/prometheus"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/cockroachdb/errors"
+	"github.com/stretchr/testify/require"
 )
 
 // This test sets up 2 workloads – kv0 consisting of "normal" priority writes
@@ -62,21 +66,23 @@ func registerDiskBandwidthOverload(r registry.Registry) {
 			}
 
 			t.Status(fmt.Sprintf("setting up prometheus/grafana (<%s)", 2*time.Minute))
-			{
-				promCfg := &prometheus.Config{}
-				promCfg.WithPrometheusNode(c.Node(promNode).InstallNodes()[0])
-				promCfg.WithNodeExporter(c.Range(1, c.Spec().NodeCount-1).InstallNodes())
-				promCfg.WithCluster(c.Range(1, c.Spec().NodeCount-1).InstallNodes())
-				promCfg.ScrapeConfigs = append(promCfg.ScrapeConfigs, prometheus.MakeWorkloadScrapeConfig("workload",
-					"/", makeWorkloadScrapeNodes(c.Node(promNode).InstallNodes()[0], []workloadInstance{
-						{nodes: c.Node(promNode)},
-					})))
-				promCfg.WithGrafanaDashboardJSON(grafana.SnapshotAdmissionControlGrafanaJSON)
-				_, cleanupFunc := setupPrometheusForRoachtest(ctx, t, c, promCfg, nil)
-				defer cleanupFunc()
-			}
+			promCfg := &prometheus.Config{}
+			promCfg.WithPrometheusNode(c.Node(promNode).InstallNodes()[0])
+			promCfg.WithNodeExporter(c.Range(1, c.Spec().NodeCount-1).InstallNodes())
+			promCfg.WithCluster(c.Range(1, c.Spec().NodeCount-1).InstallNodes())
+			promCfg.ScrapeConfigs = append(promCfg.ScrapeConfigs, prometheus.MakeWorkloadScrapeConfig("workload",
+				"/", makeWorkloadScrapeNodes(c.Node(promNode).InstallNodes()[0], []workloadInstance{
+					{nodes: c.Node(promNode)},
+				})))
+			promCfg.WithGrafanaDashboardJSON(grafana.SnapshotAdmissionControlGrafanaJSON)
+			_, cleanupFunc := setupPrometheusForRoachtest(ctx, t, c, promCfg, nil)
+			defer cleanupFunc()
 
-			// TODO(aaditya): This function shares a lot of logic with roachtestutil.DiskStaller. Consider merging the two.
+			promClient, err := clusterstats.SetupCollectorPromClient(ctx, c, t.L(), promCfg)
+			require.NoError(t, err)
+			statCollector := clusterstats.NewStatsCollector(ctx, promClient)
+
+			// TODO(aaditya): This function shares some of the logic with roachtestutil.DiskStaller. Consider merging the two.
 			setBandwidthLimit := func(nodes option.NodeListOption, rw string, bw int, max bool) error {
 				res, err := c.RunWithDetailsSingleNode(context.TODO(), t.L(), option.WithNodes(nodes[:1]), "lsblk | grep /mnt/data1 | awk '{print $2}'")
 				if err != nil {
@@ -115,15 +121,16 @@ func registerDiskBandwidthOverload(r registry.Registry) {
 			}
 
 			// TODO(aaditya): Extend this test to also limit reads once we have a
-			// mechanism to pace read traffic in AC>
+			// mechanism to pace read traffic in AC.
 
 			db := c.Conn(ctx, t.L(), crdbNodes)
 			defer db.Close()
 
+			const provisionedBandwidth = 75
 			if _, err := db.ExecContext(
 				// We intentionally set this to much lower than the provisioned value
 				// above to clearly show that the bandwidth limiter works.
-				ctx, "SET CLUSTER SETTING kvadmission.store.provisioned_bandwidth = '75MiB'"); err != nil {
+				ctx, fmt.Sprintf("SET CLUSTER SETTING kvadmission.store.provisioned_bandwidth = '%dMiB'", provisionedBandwidth)); err != nil {
 				t.Fatalf("failed to set kvadmission.store.provisioned_bandwidth: %v", err)
 			}
 
@@ -148,6 +155,84 @@ func registerDiskBandwidthOverload(r registry.Registry) {
 					"--splits=1000 --read-percent=0 --min-block-bytes=4096 --max-block-bytes=4096 " +
 					"--txn-qos='background' --tolerate-errors" + dur + url
 				c.Run(ctx, option.WithNodes(c.Node(elasticNode)), cmd)
+				return nil
+			})
+
+			m.Go(func(ctx context.Context) error {
+				const subLevelMetric = "storage_l0_sublevels"
+				const writeBytesMetric = "sys_host_disk_write_bytes"
+				getMetricVal := func(metricName string) (float64, error) {
+					point, err := statCollector.CollectPoint(ctx, t.L(), timeutil.Now(), metricName)
+					if err != nil {
+						t.L().Errorf("could not query prom %s", err.Error())
+						return 0, err
+					}
+					const labelName = "store"
+					val := point[labelName]
+					if len(val) != 1 {
+						err = errors.Errorf(
+							"unexpected number %d of points for metric %s", len(val), metricName)
+						t.L().Errorf("%s", err.Error())
+						return 0, err
+					}
+					for storeID, v := range val {
+						t.L().Printf("%s(store=%s): %f", metricName, storeID, v.Value)
+						return v.Value, nil
+					}
+					// Unreachable.
+					panic("unreachable")
+				}
+
+				const subLevelThreshold = 20
+				// Allow a 5% threshold.
+				const bandwidthThreshold = provisionedBandwidth * 1.05
+				const collectionIntervalSeconds = 10.0
+				// We want to use the mean of the last 2m of data to avoid short-lived
+				// spikes causing failures.
+				const sampleCount = int(2 / (collectionIntervalSeconds / 60))
+				// Loop for ~20 minutes.
+				const numIterations = int(20 / (collectionIntervalSeconds / 60))
+				numErrors := 0
+				numSuccesses := 0
+				var l0SublevelCount []float64
+				var diskWriteBytes []float64
+				for i := 0; i < numIterations; i++ {
+					time.Sleep(collectionIntervalSeconds * time.Second)
+
+					// L0 Sublevels
+					val, err := getMetricVal(subLevelMetric)
+					if err != nil {
+						numErrors++
+						continue
+					}
+					l0SublevelCount = append(l0SublevelCount, val)
+					if len(l0SublevelCount) >= sampleCount {
+						latestSampleMeanL0Sublevels := getMeanOverLastN(sampleCount, l0SublevelCount)
+						if latestSampleMeanL0Sublevels > subLevelThreshold {
+							t.Fatalf("sub-level mean %f over last %d iterations exceeded threshold", latestSampleMeanL0Sublevels, sampleCount)
+						}
+					}
+
+					// Disk Bandwidth
+					val, err = getMetricVal(writeBytesMetric)
+					if err != nil {
+						numErrors++
+						continue
+					}
+					diskWriteBytes = append(diskWriteBytes, val)
+					if len(diskWriteBytes) >= sampleCount {
+						diff := diskWriteBytes[len(diskWriteBytes)-1] - diskWriteBytes[len(diskWriteBytes)-1-sampleCount]
+						rate := diff / (float64(sampleCount * collectionIntervalSeconds))
+						if rate > bandwidthThreshold {
+							t.Fatalf("disk write bandwidth rate %f over last %d seconds exceeded threshold of %f", rate, sampleCount*collectionIntervalSeconds, bandwidthThreshold)
+						}
+					}
+					numSuccesses++
+				}
+				t.Status(fmt.Sprintf("done waiting errors: %d successes: %d", numErrors, numSuccesses))
+				if numErrors > numSuccesses {
+					t.Fatalf("too many errors retrieving metrics")
+				}
 				return nil
 			})
 
