831718f602f44a60fb893bba20f9e2525e90218d
Drew Kimball <drewk@cockroachlabs.com>
2024-05-15 00:01:37 -0700

    sql: add setting to enable registering latch wait contention events
    This commit adds a session setting (default off) that will cause contention
    events emitted by the latch table to be included in the `*_contention_events`
    virtual tables. This behavior is hidden behind a setting because latch wait
    events are more common than events emitted by the lock table. Tracking them
    in the virtual tables could increase system load and flood the statement
    insights with uninteresting events. For now, this setting does nothing, but
     the following commit will use it.
    
    Informs #103713
    
    Release note: None

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -3787,6 +3787,10 @@ func (m *sessionDataMutator) SetOptimizerProveImplicationWithVirtualComputedColu
 	m.data.OptimizerProveImplicationWithVirtualComputedColumns = val
 }
 
+func (m *sessionDataMutator) SetRegisterLatchWaitContentionEvents(val bool) {
+	m.data.RegisterLatchWaitContentionEvents = val
+}
+
 // Utility functions related to scrubbing sensitive information on SQL Stats.
 
 // quantizeCounts ensures that the Count field in the
