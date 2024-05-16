cockroachdb/cockroach

commit a32b2c1d8a70440269fad2dab0427771aa1c90fc

sql: add session setting for scan costing methodology
In this PR we add a session setting to gate usage of an upcoming feature
in which the optimizer will use `AvgSize`, the average column size, from
table stats to cost scans and index joins. When enabled, the optimizer
will revert to the old default method of costing scans, where each
column is treated as the same size. By default, this setting will be
off.

Informs: #72332

Release note: None

pkg/sql/vars.go
@@ -1889,6 +1889,25 @@ var varGen = map[string]sessionVar{
 			return rowexec.ParallelizeMultiKeyLookupJoinsEnabled.String(sv)
 		},
 	},
+
+	// TODO(harding): Remove this when costing scans based on average column size
+	// is fully supported.
+	// CockroachDB extension.
+	`cost_scans_with_default_col_size`: {
+		GetStringVal: makePostgresBoolGetStringValFn(`cost_scans_with_default_col_size`),
+		Set: func(_ context.Context, m sessionDataMutator, s string) error {
+			b, err := paramparse.ParseBoolVar(`cost_scans_with_default_col_size`, s)
+			if err != nil {
+				return err
+			}
+			m.SetCostScansWithDefaultColSize(b)
+			return nil
+		},
+		Get: func(evalCtx *extendedEvalContext) (string, error) {
+			return formatBoolAsPostgresSetting(evalCtx.SessionData().CostScansWithDefaultColSize), nil
+		},
+		GlobalDefault: globalFalse,
+	},
 }
 
 const compatErrMsg = "this parameter is currently recognized only for compatibility and has no effect in CockroachDB."

docs/generated/settings/settings.html
@@ -92,6 +92,7 @@
 <tr><td><code>sql.cross_db_sequence_owners.enabled</code></td><td>boolean</td><td><code>false</code></td><td>if true, creating sequences owned by tables from other databases is allowed</td></tr>
 <tr><td><code>sql.cross_db_sequence_references.enabled</code></td><td>boolean</td><td><code>false</code></td><td>if true, sequences referenced by tables from other databases are allowed</td></tr>
 <tr><td><code>sql.cross_db_views.enabled</code></td><td>boolean</td><td><code>false</code></td><td>if true, creating views that refer to other databases is allowed</td></tr>
+<tr><td><code>sql.defaults.cost_scans_with_default_col_size.enabled</code></td><td>boolean</td><td><code>false</code></td><td>setting to true uses the same size for all columns to compute scan cost</td></tr>
 <tr><td><code>sql.defaults.datestyle</code></td><td>enumeration</td><td><code>iso, mdy</code></td><td>default value for DateStyle session setting [iso, mdy = 0, iso, dmy = 1, iso, ymd = 2]</td></tr>
 <tr><td><code>sql.defaults.datestyle.enabled</code></td><td>boolean</td><td><code>false</code></td><td>default value for datestyle_enabled session setting</td></tr>
 <tr><td><code>sql.defaults.default_hash_sharded_index_bucket_count</code></td><td>integer</td><td><code>8</code></td><td>used as bucket count if bucket count is not specified in hash sharded index definition</td></tr>

pkg/sql/exec_util.go
@@ -707,6 +707,13 @@ var largeFullScanRows = settings.RegisterFloatSetting(
 	1000.0,
 ).WithPublic()
 
+var costScansWithDefaultColSize = settings.RegisterBoolSetting(
+	settings.TenantWritable,
+	`sql.defaults.cost_scans_with_default_col_size.enabled`,
+	"setting to true uses the same size for all columns to compute scan cost",
+	false,
+).WithPublic()
+
 var errNoTransactionInProgress = errors.New("there is no transaction in progress")
 var errTransactionInProgress = errors.New("there is already a transaction in progress")
 
@@ -3074,6 +3081,12 @@ func (m *sessionDataMutator) SetJoinReaderOrderingStrategyBatchSize(val int64) {
 
 func (m *sessionDataMutator) SetParallelizeMultiKeyLookupJoinsEnabled(val bool) {
 	m.data.ParallelizeMultiKeyLookupJoinsEnabled = val
+}
+
+// TODO(harding): Remove this when costing scans based on average column size
+// is fully supported.
+func (m *sessionDataMutator) SetCostScansWithDefaultColSize(val bool) {
+	m.data.CostScansWithDefaultColSize = val
 }
 
 // Utility functions related to scrubbing sensitive information on SQL Stats.

pkg/sql/explain_bundle.go
@@ -536,6 +536,7 @@ func (c *stmtEnvCollector) PrintSessionSettings(w io.Writer) error {
 		{sessionSetting: "datestyle_enabled", clusterSetting: dateStyleEnabled, convFunc: boolToOnOff},
 		{sessionSetting: "disallow_full_table_scans", clusterSetting: disallowFullTableScans, convFunc: boolToOnOff},
 		{sessionSetting: "large_full_scan_rows", clusterSetting: largeFullScanRows},
+		{sessionSetting: "cost_scans_with_default_col_size", clusterSetting: costScansWithDefaultColSize, convFunc: boolToOnOff},
 		{sessionSetting: "distsql", clusterSetting: DistSQLClusterExecMode, convFunc: distsqlConv},
 		{sessionSetting: "vectorize", clusterSetting: VectorizeClusterMode, convFunc: vectorizeConv},
 	}

pkg/sql/logictest/testdata/logic_test/information_schema
@@ -4645,6 +4645,7 @@ bytea_output                                          hex
 check_function_bodies                                 on
 client_encoding                                       UTF8
 client_min_messages                                   notice
+cost_scans_with_default_col_size                      off
 database                                              test
 datestyle                                             ISO, MDY
 datestyle_enabled                                     off

pkg/sql/logictest/testdata/logic_test/pg_catalog
@@ -4045,6 +4045,7 @@ bytea_output                                          hex                 NULL  
 check_function_bodies                                 on                  NULL      NULL        NULL        string
 client_encoding                                       UTF8                NULL      NULL        NULL        string
 client_min_messages                                   notice              NULL      NULL        NULL        string
+cost_scans_with_default_col_size                      off                 NULL      NULL        NULL        string
 database                                              test                NULL      NULL        NULL        string
 datestyle                                             ISO, MDY            NULL      NULL        NULL        string
 datestyle_enabled                                     off                 NULL      NULL        NULL        string
@@ -4158,6 +4159,7 @@ bytea_output                                          hex                 NULL  
 check_function_bodies                                 on                  NULL  user     NULL      on                  on
 client_encoding                                       UTF8                NULL  user     NULL      UTF8                UTF8
 client_min_messages                                   notice              NULL  user     NULL      notice              notice
+cost_scans_with_default_col_size                      off                 NULL  user     NULL      off                 off
 database                                              test                NULL  user     NULL      ·                   test
 datestyle                                             ISO, MDY            NULL  user     NULL      ISO, MDY            ISO, MDY
 datestyle_enabled                                     off                 NULL  user     NULL      off                 off
@@ -4265,6 +4267,7 @@ bytea_output                                          NULL    NULL     NULL     
 check_function_bodies                                 NULL    NULL     NULL     NULL        NULL
 client_encoding                                       NULL    NULL     NULL     NULL        NULL
 client_min_messages                                   NULL    NULL     NULL     NULL        NULL
+cost_scans_with_default_col_size                      NULL    NULL     NULL     NULL        NULL
 crdb_version                                          NULL    NULL     NULL     NULL        NULL
 database                                              NULL    NULL     NULL     NULL        NULL
 datestyle                                             NULL    NULL     NULL     NULL        NULL

pkg/sql/logictest/testdata/logic_test/show_source
@@ -33,6 +33,7 @@ bytea_output                                          hex
 check_function_bodies                                 on
 client_encoding                                       UTF8
 client_min_messages                                   notice
+cost_scans_with_default_col_size                      off
 database                                              test
 datestyle                                             ISO, MDY
 datestyle_enabled                                     off

pkg/sql/opt/memo/memo.go
@@ -135,22 +135,23 @@ type Memo struct {
 	// planning. We need to cross-check these before reusing a cached memo.
 	// NOTE: If you add new fields here, be sure to add them to the relevant
 	//       fields in explain_bundle.go.
-	reorderJoinsLimit       int
-	zigzagJoinEnabled       bool
-	useHistograms           bool
-	useMultiColStats        bool
-	localityOptimizedSearch bool
-	safeUpdates             bool
-	preferLookupJoinsForFKs bool
-	saveTablesPrefix        string
-	dateStyleEnabled        bool
-	intervalStyleEnabled    bool
-	dateStyle               pgdate.DateStyle
-	intervalStyle           duration.IntervalStyle
-	propagateInputOrdering  bool
-	disallowFullTableScans  bool
-	largeFullScanRows       float64
-	nullOrderedLast         bool
+	reorderJoinsLimit           int
+	zigzagJoinEnabled           bool
+	useHistograms               bool
+	useMultiColStats            bool
+	localityOptimizedSearch     bool
+	safeUpdates                 bool
+	preferLookupJoinsForFKs     bool
+	saveTablesPrefix            string
+	dateStyleEnabled            bool
+	intervalStyleEnabled        bool
+	dateStyle                   pgdate.DateStyle
+	intervalStyle               duration.IntervalStyle
+	propagateInputOrdering      bool
+	disallowFullTableScans      bool
+	largeFullScanRows           float64
+	nullOrderedLast             bool
+	costScansWithDefaultColSize bool
 
 	// curRank is the highest currently in-use scalar expression rank.
 	curRank opt.ScalarRank
@@ -180,23 +181,24 @@ func (m *Memo) Init(evalCtx *tree.EvalContext) {
 	// This initialization pattern ensures that fields are not unwittingly
 	// reused. Field reuse must be explicit.
 	*m = Memo{
-		metadata:                m.metadata,
-		reorderJoinsLimit:       int(evalCtx.SessionData().ReorderJoinsLimit),
-		zigzagJoinEnabled:       evalCtx.SessionData().ZigzagJoinEnabled,
-		useHistograms:           evalCtx.SessionData().OptimizerUseHistograms,
-		useMultiColStats:        evalCtx.SessionData().OptimizerUseMultiColStats,
-		localityOptimizedSearch: evalCtx.SessionData().LocalityOptimizedSearch,
-		safeUpdates:             evalCtx.SessionData().SafeUpdates,
-		preferLookupJoinsForFKs: evalCtx.SessionData().PreferLookupJoinsForFKs,
-		saveTablesPrefix:        evalCtx.SessionData().SaveTablesPrefix,
-		intervalStyleEnabled:    evalCtx.SessionData().IntervalStyleEnabled,
-		dateStyleEnabled:        evalCtx.SessionData().DateStyleEnabled,
-		dateStyle:               evalCtx.SessionData().GetDateStyle(),
-		intervalStyle:           evalCtx.SessionData().GetIntervalStyle(),
-		propagateInputOrdering:  evalCtx.SessionData().PropagateInputOrdering,
-		disallowFullTableScans:  evalCtx.SessionData().DisallowFullTableScans,
-		largeFullScanRows:       evalCtx.SessionData().LargeFullScanRows,
-		nullOrderedLast:         evalCtx.SessionData().NullOrderedLast,
+		metadata:                    m.metadata,
+		reorderJoinsLimit:           int(evalCtx.SessionData().ReorderJoinsLimit),
+		zigzagJoinEnabled:           evalCtx.SessionData().ZigzagJoinEnabled,
+		useHistograms:               evalCtx.SessionData().OptimizerUseHistograms,
+		useMultiColStats:            evalCtx.SessionData().OptimizerUseMultiColStats,
+		localityOptimizedSearch:     evalCtx.SessionData().LocalityOptimizedSearch,
+		safeUpdates:                 evalCtx.SessionData().SafeUpdates,
+		preferLookupJoinsForFKs:     evalCtx.SessionData().PreferLookupJoinsForFKs,
+		saveTablesPrefix:            evalCtx.SessionData().SaveTablesPrefix,
+		intervalStyleEnabled:        evalCtx.SessionData().IntervalStyleEnabled,
+		dateStyleEnabled:            evalCtx.SessionData().DateStyleEnabled,
+		dateStyle:                   evalCtx.SessionData().GetDateStyle(),
+		intervalStyle:               evalCtx.SessionData().GetIntervalStyle(),
+		propagateInputOrdering:      evalCtx.SessionData().PropagateInputOrdering,
+		disallowFullTableScans:      evalCtx.SessionData().DisallowFullTableScans,
+		largeFullScanRows:           evalCtx.SessionData().LargeFullScanRows,
+		nullOrderedLast:             evalCtx.SessionData().NullOrderedLast,
+		costScansWithDefaultColSize: evalCtx.SessionData().CostScansWithDefaultColSize,
 	}
 	m.metadata.Init()
 	m.logPropsBuilder.init(evalCtx, m)
@@ -312,7 +314,8 @@ func (m *Memo) IsStale(
 		m.propagateInputOrdering != evalCtx.SessionData().PropagateInputOrdering ||
 		m.disallowFullTableScans != evalCtx.SessionData().DisallowFullTableScans ||
 		m.largeFullScanRows != evalCtx.SessionData().LargeFullScanRows ||
-		m.nullOrderedLast != evalCtx.SessionData().NullOrderedLast {
+		m.nullOrderedLast != evalCtx.SessionData().NullOrderedLast ||
+		m.costScansWithDefaultColSize != evalCtx.SessionData().CostScansWithDefaultColSize {
 		return true, nil
 	}
 

commit 59fc432334ee1f6a79a06da7513fb08c687e8ada

sql: fix cost_scans_with_default_col_size
This commit fixes an omission where `cost_scans_with_default_col_size`
session variable didn't actually use the corresponding cluster setting
for its default value. In other words, the cluster setting actually had
no effect. The bug has been present since this variable was introduced
in a32b2c1d8a70440269fad2dab0427771aa1c90fc.

Release note: None

pkg/sql/vars.go
@@ -2244,7 +2244,9 @@ var varGen = map[string]sessionVar{
 		Get: func(evalCtx *extendedEvalContext, _ *kv.Txn) (string, error) {
 			return formatBoolAsPostgresSetting(evalCtx.SessionData().CostScansWithDefaultColSize), nil
 		},
-		GlobalDefault: globalFalse,
+		GlobalDefault: func(sv *settings.Values) string {
+			return formatBoolAsPostgresSetting(costScansWithDefaultColSize.Get(sv))
+		},
 	},
 
 	// CockroachDB extension.