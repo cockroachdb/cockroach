45140cb280d1d9a958f03be58ce91df7b8291531
Annie Pompa <annie@cockroachlabs.com>
2024-05-17 11:00:37 -0400

    sql: ensure truncate preserves pkey zone configs
    Previously, we did not copy zone configs over
    from old indexes to new ones created during a
    truncate for pkeys. This was due to truncate
    re-using the same logic as our `PrimaryKeySwap`
    mutation. This patch adds an override in said logic to
    allow for zcs to be copied from old to new indexes.
    
    Epic: CRDB-22729
    Fixes: #94188
    
    Release note (bug fix): Fixed bug where zone configs
    on pkeys disappeared during truncate.

--- pkg/sql/truncate.go
+++ pkg/sql/truncate.go
@@ -290,7 +290,8 @@ func (p *planner) truncateTable(ctx context.Context, id descpb.ID, jobDesc strin
 		NewIndexes:        newIndexIDs[1:],
 	}
 	if err := maybeUpdateZoneConfigsForPKChange(
-		ctx, p.InternalSQLTxn(), p.ExecCfg(), p.ExtendedEvalContext().Tracing.KVTracingEnabled(), tableDesc, swapInfo,
+		ctx, p.InternalSQLTxn(), p.ExecCfg(), p.ExtendedEvalContext().Tracing.KVTracingEnabled(),
+		tableDesc, swapInfo, true, /* forceSwap */
 	); err != nil {
 		return err
 	}
