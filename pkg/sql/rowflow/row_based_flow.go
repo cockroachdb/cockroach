4fb7642713d9ad4272eaeb8138f486ffea89f393
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-05-18 14:23:38 -0700

    flowinfra: always call ConsumerClosed on flow cleanup
    This commit adjusts the flow cleanup to always call
    `execinfra.RowSource.ConsumerClosed` method unconditionally. Generally,
    this is not necessary (because the row source is exhausted or
    ConsumerClosed is called in the `execinfra.Run` loop), but in edge cases
    the method might never be called (e.g. when the row source is created
    but is never `Run` due to server shutting down). As a result, some
    resources (like memory monitors) might not be released in such edge
    cases, and even though it's not a problem on its own, it will be when
    we introduce some assertions on the server shutdown around memory
    monitors.
    
    This commit adjusts the contract of `ConsumerClosed` method to explicity
    call out that multiple calls are ok and fixes a single instance where it
    wasn't.
    
    Release note: None

--- pkg/sql/rowflow/row_based_flow.go
+++ pkg/sql/rowflow/row_based_flow.go
@@ -469,8 +469,6 @@ func (f *rowBasedFlow) Cleanup(ctx context.Context) {
 	startCleanup, endCleanup := f.FlowBase.GetOnCleanupFns()
 	startCleanup()
 	defer endCleanup()
-	// Ensure that the "head" processor is always closed.
-	f.ConsumerClosedOnHeadProc()
 	f.FlowBase.Cleanup(ctx)
 	f.Release()
 }
