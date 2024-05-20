e9d42ecd71922ef9604cddb78d744953cc9a733e
Marcus Gartner <marcus@cockroachlabs.com>
2024-05-20 11:38:25 -0400

    opt: add optimizer_push_offset_into_index_join
    The `optimizer_push_offset_into_index_join` session setting has been
    added. If enabled, the `PushOffsetIntoIndexJoin` rule is enabled which
    attempts to push offset expressions into index joins to reduce the
    number of lookup rows. It is enabled by default, but will be disabled in
    some backports by default to avoid query plan changes.
    
    Release note (sql change): The `optimizer_push_offset_into_index_join`
    session setting has been added. When enabled, the optimizer will attempt
    to push offset expressions into index join expressions to produce more
    efficient query plans. It is enabled by default on 24.1+ and disabled on
    23.2.

--- pkg/sql/opt/testutils/opttester/opt_tester.go
+++ pkg/sql/opt/testutils/opttester/opt_tester.go
@@ -315,6 +315,7 @@ func New(catalog cat.Catalog, sql string) *OptTester {
 	ot.evalCtx.SessionData().OptimizerMergeJoinsEnabled = true
 	ot.evalCtx.SessionData().OptimizerUseVirtualComputedColumnStats = true
 	ot.evalCtx.SessionData().TrigramSimilarityThreshold = 0.3
+	ot.evalCtx.SessionData().OptimizerPushOffsetIntoIndexJoin = true
 
 	return ot
 }
