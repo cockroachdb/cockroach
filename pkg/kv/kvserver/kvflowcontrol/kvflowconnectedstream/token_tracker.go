84dca64786ada4aa2893b17864cb0318a3e2759a
sumeerbhola <sumeer@cockroachlabs.com>
2024-05-24 13:29:23 -0400

    copy over Tracker and tweak it

---
+++ pkg/kv/kvserver/kvflowcontrol/kvflowconnectedstream/token_tracker.go
@@ -0,0 +1,143 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package kvflowconnectedstream
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvflowcontrol"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvflowcontrol/kvflowcontrolpb"
+	"github.com/cockroachdb/cockroach/pkg/util/admission/admissionpb"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+)
+
+// Tracker tracks flow token deductions for a replicaSendStream. Tokens are
+// deducted for an in-flight log entry (identified by raft log position), with
+// a given admissionpb.WorkPriority.
+type Tracker struct {
+	// TODO(irfansharif,aaditya): Everytime we track something, we incur a map
+	// assignment (shows up in CPU profiles). We could introduce a struct that
+	// internally embeds this list of tracked deductions, and append there
+	// instead. Do this as part of #104154.
+	//
+	// TODO: Priorities here are the original priorities. So the key will always be
+	// based on the original priority. Just the AC StoreQueue will admit based on overridden
+	// priority.
+	trackedM map[admissionpb.WorkPriority][]tracked
+
+	stream kvflowcontrol.Stream // used for logging only
+}
+
+// tracked represents tracked flow tokens; they're tracked with respect to a
+// raft log position (typically where the proposed command is expected to end
+// up).
+type tracked struct {
+	tokens   kvflowcontrol.Tokens
+	position kvflowcontrolpb.RaftLogPosition
+}
+
+// New constructs a new Tracker with the given lower bound raft log position
+// (below which we're not allowed to deduct tokens).
+func (dt *Tracker) Init(stream kvflowcontrol.Stream) {
+	*dt = Tracker{
+		trackedM: make(map[admissionpb.WorkPriority][]tracked),
+		stream:   stream,
+	}
+}
+
+// Track token deductions of the given priority with the given raft log
+// position.
+func (dt *Tracker) Track(
+	ctx context.Context,
+	pri admissionpb.WorkPriority,
+	tokens kvflowcontrol.Tokens,
+	pos kvflowcontrolpb.RaftLogPosition,
+) bool {
+	if len(dt.trackedM[pri]) >= 1 {
+		last := dt.trackedM[pri][len(dt.trackedM[pri])-1]
+		if !last.position.Less(pos) {
+			log.Fatalf(ctx, "expected in order tracked log positions (%s < %s)",
+				last.position, pos)
+			return false
+		}
+	}
+	// TODO(irfansharif,aaditya): The tracked instances here make up about ~0.4%
+	// of allocations under kv0/enc=false/nodes=3/cpu=9. Maybe clean it up as
+	// part of #104154, by using a sync.Pool perhaps.
+	dt.trackedM[pri] = append(dt.trackedM[pri], tracked{
+		tokens:   tokens,
+		position: pos,
+	})
+	if log.V(1) {
+		log.Infof(ctx, "tracking %s flow control tokens for pri=%s stream=%s pos=%s",
+			tokens, pri, dt.stream, pos)
+	}
+	return true
+}
+
+// Untrack all token deductions of the given priority that have log positions
+// less than or equal to the one provided.
+func (dt *Tracker) Untrack(
+	ctx context.Context, pri admissionpb.WorkPriority, upto kvflowcontrolpb.RaftLogPosition,
+) kvflowcontrol.Tokens {
+	if dt == nil {
+		return 0
+	}
+	if _, ok := dt.trackedM[pri]; !ok {
+		return 0
+	}
+
+	var untracked int
+	var tokens kvflowcontrol.Tokens
+	for {
+		if untracked == len(dt.trackedM[pri]) {
+			break
+		}
+
+		deduction := dt.trackedM[pri][untracked]
+		if !deduction.position.LessEq(upto) {
+			break
+		}
+		untracked += 1
+		tokens += deduction.tokens
+	}
+
+	trackedBefore := len(dt.trackedM[pri])
+	dt.trackedM[pri] = dt.trackedM[pri][untracked:]
+	if log.V(1) {
+		remaining := ""
+		if len(dt.trackedM[pri]) > 0 {
+			remaining = fmt.Sprintf(" (%s, ...)", dt.trackedM[pri][0].tokens)
+		}
+		log.Infof(ctx, "released %s flow control tokens for %d out of %d tracked deductions for pri=%s stream=%s, up to %s; %d tracked deduction(s) remain%s",
+			tokens, untracked, trackedBefore, pri, dt.stream, upto, len(dt.trackedM[pri]), remaining)
+	}
+	if len(dt.trackedM[pri]) == 0 {
+		delete(dt.trackedM, pri)
+	}
+	return tokens
+}
+
+// UntrackAll iterates through all tracked token deductions, invoking the
+// provided callback with the sum of all tokens at a per-priority level, and
+// untracking.
+func (dt *Tracker) UntrackAll(
+	_ context.Context, f func(admissionpb.WorkPriority, kvflowcontrol.Tokens),
+) {
+	for pri, deductions := range dt.trackedM {
+		var tokens kvflowcontrol.Tokens
+		for _, deduction := range deductions {
+			tokens += deduction.tokens
+		}
+		f(pri, tokens)
+	}
+}
