a15d0e9a7d6f75491b5634f67e1db6b183bc8ae7
Andrii Vorobiov <and.vorobiov@gmail.com>
2024-05-30 16:00:04 +0300

    ui: don't use _.chain function in `db-console`
    Db Console relied on _.chain function to make it
    a bit easier to handle multiple data manipulations, but
    with updated linting rules it's not possible to import
    `chain` function as `es` module (ie `import chain from "lodash/chain"`)
    as it requires to load all functions.
    This change uses following alternative approach to avoid using `_.chain`:
    - use `flow` func instead, it allows to compose list of functions that pass
    result to successive function.
    - simplify expressions to use native functions instead
    of lodash functions.
    
    Release note: None

--- pkg/ui/workspaces/db-console/src/views/reports/components/nodeFilterList/index.tsx
+++ pkg/ui/workspaces/db-console/src/views/reports/components/nodeFilterList/index.tsx
@@ -61,9 +61,8 @@ export function localityToString(locality: protos.cockroach.roachpb.ILocality) {
 export function NodeFilterList(props: NodeFilterListProps) {
   const { nodeIDs, localityRegex } = props;
   const filters: string[] = [];
-  if (!_.isNil(nodeIDs) && nodeIDs.size > 0) {
-    const nodeList = _.chain(Array.from(nodeIDs.keys()))
-      .sort()
+  if (!isNil(nodeIDs) && nodeIDs.size > 0) {
+    const nodeList = sortBy(Array.from(nodeIDs.keys()))
       .map(nodeID => `n${nodeID}`)
       .join(",");
     filters.push(`Only nodes: ${nodeList}`);
