0b01e692d819baed9ffe2778444d9ce2ccdd65a6
Steven Danna <danna@cockroachlabs.com>
2024-05-30 10:50:05 +0100

    kvserver: bump max range size in TestStoreRangeSplitBackpressureWrites
    Similar to #124818, this test assumes that no other ranges will
    ever split but the max range size was very close to the size of an
    existing system range.
    
    Epic: none
    Release note: None

--- pkg/kv/kvserver/client_split_test.go
+++ pkg/kv/kvserver/client_split_test.go
@@ -1239,7 +1239,7 @@ func TestStoreRangeSplitBackpressureWrites(t *testing.T) {
 			// size without adding 2x64MB of data.
 			defer zonepb.TestingSetMinRangeMaxBytes(1 << 16)()
 			const minBytes = 1 << 12
-			const maxBytes = 1 << 17
+			const maxBytes = 1 << 18
 			zoneConfig := zonepb.DefaultZoneConfig()
 			zoneConfig.RangeMinBytes = proto.Int64(minBytes)
 			zoneConfig.RangeMaxBytes = proto.Int64(maxBytes)
