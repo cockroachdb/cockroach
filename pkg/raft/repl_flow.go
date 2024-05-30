9a93e58866ffd30340f9f0cc53ed5f83b2726d23
Pavel Kalinnikov <pavel@cockroachlabs.com>
2024-05-30 17:41:32 +0100

    raft: add FlowControl implementation stub
    Epic: none
    Release note: none

--- pkg/raft/repl_flow.go
+++ pkg/raft/repl_flow.go
@@ -10,7 +10,11 @@
 
 package raft
 
-import "github.com/cockroachdb/cockroach/pkg/raft/tracker"
+import (
+	"math"
+
+	"github.com/cockroachdb/cockroach/pkg/raft/tracker"
+)
 
 // NodeID is a unique ID of a node in a raft group.
 type NodeID uint64
@@ -26,6 +30,11 @@ type FlowState struct {
 	//
 	// Invariant: 0 <= Match <= Sent.
 	Sent uint64
+}
+
+// Ready returns whether this flow has outstanding entries to send.
+func (f FlowState) Ready(lastIndex uint64) bool {
+	return f.Sent < lastIndex
 }
 
 // FlowStateFrom constructs the FlowState from the Progress struct.
@@ -88,3 +97,86 @@ func (f FlowControlNoop) Update(NodeID, FlowState) {}
 func (f FlowControlNoop) Disconnect(NodeID)        {}
 func (f FlowControlNoop) Remove(NodeID)            {}
 func (f FlowControlNoop) Stop()                    {}
+
+// ReplTracker tracks the log replication flow from the leader to all connected
+// followers in StateReplicate.
+//
+// This is a stub of a FlowControl implementation reflecting integration with
+// the raft scheduler, ready handling, and token tracker.
+type ReplTracker struct {
+	// Last is the index of the last entry in this leader's log.
+	Last uint64
+	// Flows maps node ID of a connected follower to its replication flow state.
+	Flows map[NodeID]FlowState
+}
+
+func (r *ReplTracker) Lead(_, lastIndex uint64) {
+	r.Last = lastIndex
+}
+
+func (r *ReplTracker) Append(lastIndex uint64) {
+	r.Last = lastIndex
+	for id := range r.Flows {
+		r.signalSendQueue(id)
+	}
+}
+
+func (r *ReplTracker) Update(to NodeID, state FlowState) {
+	if _, ok := r.Flows[to]; !ok {
+		// create a connected stream from state.Match
+	} else {
+		r.returnTokens(to, state.Match)
+	}
+	r.Flows[to] = state
+	if state.Sent < r.Last {
+		r.signalSendQueue(to)
+	}
+}
+
+func (r *ReplTracker) Disconnect(to NodeID) {
+	r.returnTokens(to, math.MaxUint64)
+	delete(r.Flows, to)
+}
+
+func (r *ReplTracker) Remove(to NodeID) {
+	r.returnTokens(to, math.MaxUint64)
+	delete(r.Flows, to)
+}
+
+func (r *ReplTracker) Stop() {
+	for id := range r.Flows {
+		r.returnTokens(id, math.MaxUint64)
+	}
+	r.Last = 0
+	clear(r.Flows)
+}
+
+func (r *ReplTracker) signalSendQueue(id NodeID) {
+	// Signal the send queue about this flows update.
+	_ = id
+	// Eventually, the send queue will schedule a Ready which will send some
+	// appends.
+	r.signalReady()
+}
+
+func (r *ReplTracker) signalReady() {
+	// Signal to the scheduler that there is outstanding repl work. When handling
+	// Ready, we will construct MsgApp messages for all peers in r.Flows who are
+	// ready to send some entries.
+}
+
+func (r *ReplTracker) handleReady() {
+	for id, state := range r.Flows {
+		if !state.Ready(r.Last) {
+			continue
+		}
+		// Call a raft method returning some entries in (state.Sent, r.Last]. It
+		// will construct and send message(s) to us, and call back into r.Update().
+		_ = id
+	}
+}
+
+func (r *ReplTracker) returnTokens(id NodeID, index uint64) {
+	_, _ = id, index
+	// return all the tokens up to the given log index
+}
