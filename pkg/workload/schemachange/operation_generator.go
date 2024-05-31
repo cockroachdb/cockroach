cockroachdb/cockroach

commit b13706d11bb3497d3c42ea3e325f47e78ca03936

workload/schemachange: add routine input DEFAULT expressions
Previously, the randomized schema changer workload would generate
functions with no default parameters and only use enum types for input
parameters. This limited what was tested by this workload. To address this,
this patch will generate functions with default values and non-enum type
parameters.  Additionally, function invocations will now use non-null
values for input parameters.

Fixes: #121718

Release note: None

pkg/workload/schemachange/operation_generator.go
@@ -39,6 +39,7 @@ import (
 	"github.com/cockroachdb/errors"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/lib/pq/oid"
 	"golang.org/x/exp/slices"
 )
 
@@ -3936,7 +3937,9 @@ func (og *operationGenerator) createFunction(ctx context.Context, tx pgx.Tx) (*o
 	}, `SELECT
 	quote_ident(schema_id::REGNAMESPACE::STRING) AS schema,
 	quote_ident(name) AS name,
-	array_to_string(proargnames, ',') AS args
+	array_to_string(proargnames, ',') AS args,
+	proargtypes AS argtypes,
+	pronargdefaults as numdefaults
 FROM
 	functions
 	INNER JOIN pg_catalog.pg_proc ON oid = (id + 100000)
@@ -3968,6 +3971,7 @@ FROM
 	var droppingEnumMembers []string
 	var possibleBodyReferences []string
 	var possibleParamReferences []string
+	var possibleParamReferencesWithDefaults []string
 	var possibleReturnReferences []string
 	var fnDuplicate bool
 
@@ -3978,6 +3982,55 @@ FROM
 		}
 		possibleReturnReferences = append(possibleReturnReferences, enum["name"].(string))
 		possibleParamReferences = append(possibleParamReferences, fmt.Sprintf(`enum_%d %s`, i, enum["name"]))
+	}
+	// PGLSN was added in 23.2.
+	pgLSNNotSupported, err := isClusterVersionLessThan(
+		ctx,
+		tx,
+		clusterversion.V23_2.Version())
+	if err != nil {
+		return nil, err
+	}
+	// REFCURSOR was added in 23.2.
+	refCursorNotSupported, err := isClusterVersionLessThan(
+		ctx,
+		tx,
+		clusterversion.V23_2.Version())
+	if err != nil {
+		return nil, err
+	}
+
+	defaultExpressionsNotSupported, err := isClusterVersionLessThan(
+		ctx,
+		tx,
+		clusterversion.V24_1.Version())
+	if err != nil {
+		return nil, err
+	}
+
+	// Generate random parameters / values for builtin types.
+	for i, typeVal := range randgen.SeedTypes {
+		// If we have types where invalid values can exist then skip over these,
+		// since randgen will not introspect to generate valid values (i.e. OID
+		// and REGCLASS).
+		if typeVal.Identical(types.AnyTuple) ||
+			typeVal.IsWildcardType() ||
+			typeVal == types.RegClass ||
+			typeVal.Family() == types.OidFamily ||
+			(pgLSNNotSupported && typeVal.Family() == types.PGLSNFamily) ||
+			(refCursorNotSupported && typeVal.Family() == types.RefCursorFamily) {
+			continue
+		}
+		possibleReturnReferences = append(possibleReturnReferences, typeVal.SQLStandardName())
+		possibleParamReferences = append(possibleParamReferences, fmt.Sprintf("val_%d %s", i+len(enums), typeVal.SQLStandardName()))
+		optionalDefaultValue := randgen.RandDatum(og.params.rng, typeVal, true)
+		if !defaultExpressionsNotSupported {
+			possibleParamReferencesWithDefaults = append(possibleParamReferencesWithDefaults, fmt.Sprintf("val_%d %s DEFAULT %s",
+				i+len(enums)+len(randgen.SeedTypes),
+				typeVal.SQLStandardName(),
+				tree.AsStringWithFlags(optionalDefaultValue, tree.FmtParsable)))
+		}
+
 	}
 
 	for _, member := range enumMembers {
@@ -4003,14 +4056,41 @@ FROM
 			args := ""
 			if function["args"] != nil {
 				args = function["args"].(string)
+				argTypesStr := strings.Split(function["argtypes"].(string), " ")
+				argTypes := make([]int, 0, len(argTypesStr))
+				// Determine how many default arguemnts should be used.
+				numDefaultArgs := int(function["numdefaults"].(int16))
+				if numDefaultArgs > 0 {
+					numDefaultArgs = rand.Intn(numDefaultArgs)
+				}
+				// Populate the arguments for this signature, and select some number
+				// of default arguments.
+				for _, oidStr := range argTypesStr {
+					oid, err := strconv.Atoi(oidStr)
+					if err != nil {
+						return nil, err
+					}
+					argTypes = append(argTypes, oid)
+				}
 				argIn := strings.Builder{}
-				// TODO(fqazi): Longer term we should populate actual arguments and
-				// not just NULLs.
-				for range strings.Split(args, ",") {
+				for idx := range strings.Split(args, ",") {
+					// We have hit the default arguments that we want to not populate.
+					if idx > len(argTypesStr)-numDefaultArgs {
+						break
+					}
 					if argIn.Len() > 0 {
 						argIn.WriteString(",")
 					}
-					argIn.WriteString("NULL")
+					// Resolve the type for each column and if possible generate a random
+					// value via randgen.
+					oidValue := oid.Oid(argTypes[idx])
+					typ, ok := types.OidToType[oidValue]
+					if !ok {
+						argIn.WriteString("NULL")
+					} else {
+						randomDatum := randgen.RandDatum(og.params.rng, typ, true)
+						argIn.WriteString(tree.AsStringWithFlags(randomDatum, tree.FmtParsable))
+					}
 				}
 				args = argIn.String()
 			}
@@ -4034,8 +4114,12 @@ FROM
 		},
 		"ParamRefs": func() (string, error) {
 			refs, err := PickBetween(og.params.rng, 1, 99, possibleParamReferences)
+			if err != nil {
+				return "", err
+			}
+			refsWithDefaults, err := PickBetween(og.params.rng, 1, 99, possibleParamReferencesWithDefaults)
 			if useParamRefs && err == nil {
-				return strings.Join(refs, ", "), nil
+				return strings.Join(append(refs, refsWithDefaults...), ", "), nil
 			}
 			return "", nil //nolint:returnerrcheck
 		},
@@ -4109,7 +4193,7 @@ func (og *operationGenerator) dropFunction(ctx context.Context, tx pgx.Tx) (*opS
 			FROM functions
 			JOIN LATERAL (
 				SELECT
-					COALESCE(array_agg(quote_ident(typnamespace::REGNAMESPACE::TEXT) || '.' || quote_ident(typname)), '{}') AS funcargs
+					COALESCE(array_agg(replace(quote_ident(typnamespace::REGNAMESPACE::TEXT) || '.' || quote_ident(typname), 'pg_catalog.', '')), '{}') AS funcargs
 				FROM pg_catalog.pg_type
 				JOIN LATERAL (
 					SELECT unnest(proargtypes) AS oid FROM pg_catalog.pg_proc WHERE oid = (id + 100000)
@@ -4190,7 +4274,7 @@ func (og *operationGenerator) alterFunctionRename(ctx context.Context, tx pgx.Tx
 			FROM functions
 			JOIN LATERAL (
 				SELECT
-					COALESCE(array_agg(quote_ident(typnamespace::REGNAMESPACE::TEXT) || '.' || quote_ident(typname)), '{}') AS funcargs
+					COALESCE(array_agg(replace(quote_ident(typnamespace::REGNAMESPACE::TEXT) || '.' || quote_ident(typname), 'pg_catalog.', '')), '{}') AS funcargs
 				FROM pg_catalog.pg_type
 				JOIN LATERAL (
 					SELECT unnest(proargtypes) AS oid FROM pg_catalog.pg_proc WHERE oid = (id + 100000)
@@ -4283,7 +4367,7 @@ func (og *operationGenerator) alterFunctionSetSchema(
 			FROM functions
 			JOIN LATERAL (
 				SELECT
-					COALESCE(array_agg(quote_ident(typnamespace::REGNAMESPACE::TEXT) || '.' || quote_ident(typname)), '{}') AS funcargs
+					COALESCE(array_agg(replace(quote_ident(typnamespace::REGNAMESPACE::TEXT) || '.' || quote_ident(typname), 'pg_catalog.', '')), '{}') AS funcargs
 				FROM pg_catalog.pg_type
 				JOIN LATERAL (
 					SELECT unnest(proargtypes) AS oid FROM pg_catalog.pg_proc WHERE oid = (id + 100000)

commit 684edc40e1f7d3f0eb51a4f04d50ed472abb0dfc

workload/schemachange: avoid more than 99 UDF parameters
The check for max number of parameters got broken in
b13706d11bb3497d3c42ea3e325f47e78ca03936, when we added default
parameters.

Release note: None

pkg/workload/schemachange/operation_generator.go
@@ -3961,15 +3961,27 @@ FROM
 			return PickOne(og.params.rng, droppingEnumMembers)
 		},
 		"ParamRefs": func() (string, error) {
-			refs, err := PickBetween(og.params.rng, 1, 99, possibleParamReferences)
+			refs, err := PickBetween(
+				og.params.rng, min(1, len(possibleParamReferences)),
+				98, possibleParamReferences,
+			)
 			if err != nil {
 				return "", err
 			}
-			refsWithDefaults, err := PickBetween(og.params.rng, 1, 99, possibleParamReferencesWithDefaults)
-			if useParamRefs && err == nil {
-				return strings.Join(append(refs, refsWithDefaults...), ", "), nil
+			// The max number to pick is 99-len(refs), since we end up combining
+			// the slices together, and we don't want the total length to exceed 99.
+			refsWithDefaults, err := PickBetween(
+				og.params.rng, min(1, len(possibleParamReferencesWithDefaults)),
+				99-len(refs), possibleParamReferencesWithDefaults,
+			)
+			if err != nil {
+				return "", err
 			}
-			return "", nil //nolint:returnerrcheck
+			refs = append(refs, refsWithDefaults...)
+			if useParamRefs && len(refs) != 0 {
+				return strings.Join(refs, ", "), nil
+			}
+			return "", nil
 		},
 		"ReturnRefs": func() (string, error) {
 			ref, err := PickOne(og.params.rng, possibleReturnReferences)