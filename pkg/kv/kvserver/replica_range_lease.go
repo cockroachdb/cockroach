795aa581947a5ad505fed618aafd1b04ce7282ad
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2024-06-03 14:13:15 -0400

    kv: add env var for leader leases

--- pkg/kv/kvserver/replica_range_lease.go
+++ pkg/kv/kvserver/replica_range_lease.go
@@ -101,7 +101,7 @@ var PreferLeaderLeaseOverEpochBasedLease = settings.RegisterBoolSetting(
 	settings.SystemOnly,
 	"kv.lease.prefer_leader_lease_over_epoch_based_lease.enabled",
 	"controls whether leader leases are preferred over epoch-based leases",
-	true,
+	envutil.EnvOrDefaultBool("COCKROACH_LEADER_LEASES", false),
 )
 
 // ExpirationLeasesMaxReplicasPerNode converts from expiration back to epoch
