97681545d49e0101946ff79a87aff3036371de97
Matt Spilchen <matt.spilchen@cockroachlabs.com>
2024-06-05 11:25:36 -0300

    sql: allow FK over stored computed columns
    Previously, any attempt to construct a foreign key that used a computed
    column was blocked. This will remove that block when the computed column
    is stored. We continue to block for virtual computed columns.
    
    Even though this is about removing the blocking code, I had to add some
    new blocking code since we cannot support all options for foreign key.
    Specifically, any of the ON UPDATE or ON DELETE clauses that would cause
    an update to the computed column. Since the column value is computed, we
    cannot allow it to change. Note, we do support ON DELETE CASCADE since
    that doesn't modify the row rather it removes the row entirely.
    
    There is some duplication of the checks, since it required updates in
    two codepaths for ALTER TABLE to handle the different schema changer. I
    tried to modularize as best as possible.
    
    Epic: none
    Fixes: #46672
    Release note (sql change): Allow foreign keys to be created over
    stored computed columns. However, most ON UPDATE and ON DELETE options for
    foreign key constraints are not allowed with computed columns.  Only the
    following are supported:
    ON UPDATE (NO ACTION|RESTRICT), ON DELETE (NO ACTION|RESTRICT|CASCADE).

--- pkg/sql/sqlerrors/errors.go
+++ pkg/sql/sqlerrors/errors.go
@@ -12,6 +12,7 @@
 package sqlerrors
 
 import (
+	"github.com/cockroachdb/redact"
 	"sort"
 	"strings"
 
@@ -199,6 +200,20 @@ func NewSchemaAlreadyExistsError(name string) error {
 func NewUnsupportedUnvalidatedConstraintError(constraintType catconstants.ConstraintType) error {
 	return pgerror.Newf(pgcode.FeatureNotSupported,
 		"%v constraints cannot be marked NOT VALID", constraintType)
+}
+
+// NewInvalidActionOnComputedFKColumnError creates an error when there is an
+// attempt to have an unsupported action on a FK over a computed column.
+func NewInvalidActionOnComputedFKColumnError(onUpdateAction bool) error {
+	// Pick the 'ON UPDATE' or 'ON DELETE' keyword. If both actions are set we
+	// include 'ON UPDATE' in the error text. This is consistent with postgres.
+	var keyword redact.SafeString = "DELETE"
+	if onUpdateAction {
+		keyword = "UPDATE"
+	}
+	return pgerror.Newf(pgcode.InvalidForeignKey,
+		"invalid ON %s action for foreign key constraint containing computed column", keyword,
+	)
 }
 
 // MakeObjectAlreadyExistsError creates an error for a namespace collision
