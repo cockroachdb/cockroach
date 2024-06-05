f498b9328c40dd483499050910c6397aa73ab44a
Michael Butler <butler@cockroachlabs.com>
2024-06-05 17:51:59 -0400

    streamingccl: tweak node lag replanning policy
    This patch changes the policy for when pcr replans based on a lagging node:
    
    - previously, lag node replanning would not occur if the replicated time had
      not advanced during the flow. This patch relaxes this requirement. If one
    node is lagging behind all other nodes, and it is preventing the replicated
    time from advancing, we really should replan!
    
    - previously, lag node replanning would occur if the lagging node was more than
      x minutes behind the second most lagging node. But, if 2 nodes are lagging
    behind all other nodes, we would not replan, even though we should. Instead,
    this patch will trigger replanning if the lagging node's frontier is more than
    x minutes behind the mean frontier of all participating nodes.
    
    Overall, this patch relaxes the requirements for lag replanning, which will
    become much less costly once #125044 lands, as it reduces the amount of wasted
    work on each dist sql flow restart.
    
    Informs #124915
    
    Relase note: none

--- pkg/ccl/streamingccl/streamingest/node_lag_detector.go
+++ pkg/ccl/streamingccl/streamingest/node_lag_detector.go
@@ -15,13 +15,14 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/errors"
 )
 
 var ErrNodeLagging = errors.New("node frontier too far behind other nodes")
 
 // checkLaggingNode returns an error if there exists a destination node lagging
-// more than maxAllowable lag behind any other destination node. This function
+// more than maxAllowable lag behind the mean frontier of all destination nodes. This function
 // assumes that all nodes have finished their initial scan (i.e. have a nonzero hwm).
 func checkLaggingNodes(
 	ctx context.Context, executionDetails []frontierExecutionDetails, maxAllowableLag time.Duration,
@@ -29,18 +30,22 @@ func checkLaggingNodes(
 	if maxAllowableLag == 0 {
 		return nil
 	}
-	laggingNode, minLagDifference := computeMinLagDifference(executionDetails)
-	log.VEventf(ctx, 2, "computed min lag diff: %d lagging node, difference %.2f", laggingNode, minLagDifference.Minutes())
-	if maxAllowableLag < minLagDifference {
-		return errors.Wrapf(ErrNodeLagging, "node %d is %.2f minutes behind the next node. Try replanning", laggingNode, minLagDifference.Minutes())
+	laggingNode, maxLagDifference := computeMeanLagDifference(ctx, executionDetails)
+	log.VEventf(ctx, 2, "computed mean lag diff: %d lagging node, difference %.2f", laggingNode, maxLagDifference.Minutes())
+	if maxAllowableLag < maxLagDifference {
+		return errors.Wrapf(ErrNodeLagging, "node %d is %.2f minutes behind the average frontier. Try replanning", laggingNode, maxLagDifference.Minutes())
 	}
 	return nil
 }
 
-func computeMinLagDifference(
-	executionDetails []frontierExecutionDetails,
+// computeMeanLagDifference computes the difference between the mean frontier by
+// node and the node with the lowest frontier.
+func computeMeanLagDifference(
+	ctx context.Context, executionDetails []frontierExecutionDetails,
 ) (base.SQLInstanceID, time.Duration) {
-	oldestHWM := hlc.MaxTimestamp.GoTime()
+	lowestFrontier := hlc.MaxTimestamp.GoTime()
+
+	// First find the frontier for each node.
 	var laggingNode base.SQLInstanceID
 	destNodeFrontier := make(map[base.SQLInstanceID]time.Time)
 	for _, detail := range executionDetails {
@@ -50,8 +55,8 @@ func computeMinLagDifference(
 		} else if destNodeFrontier[detail.destInstanceID].After(frontier) {
 			destNodeFrontier[detail.destInstanceID] = frontier
 		}
-		if oldestHWM.After(frontier) {
-			oldestHWM = frontier
+		if lowestFrontier.After(frontier) {
+			lowestFrontier = frontier
 			laggingNode = detail.destInstanceID
 		}
 	}
@@ -59,16 +64,17 @@ func computeMinLagDifference(
 		// If there are fewer than 2 nodes in the frontier, we can't compare relative lag.
 		return base.SQLInstanceID(0), 0
 	}
+	meanFrontier := getMeanFrontier(destNodeFrontier)
+	log.VEventf(ctx, 2, "mean frontier: %s, lowest frontier %s", meanFrontier, lowestFrontier)
 
-	minlagDifference := hlc.MaxTimestamp.GoTime().Sub(hlc.MinTimestamp.GoTime())
-	for id, frontier := range destNodeFrontier {
-		if id == laggingNode {
-			continue
-		}
+	return laggingNode, meanFrontier.Sub(lowestFrontier)
+}
 
-		if diff := frontier.Sub(oldestHWM); diff < minlagDifference {
-			minlagDifference = diff
-		}
+func getMeanFrontier(destNodeFrontier map[base.SQLInstanceID]time.Time) time.Time {
+	var sum int64
+	for _, frontier := range destNodeFrontier {
+		sum += frontier.Unix()
 	}
-	return laggingNode, minlagDifference
+	frontierMean := timeutil.Unix(sum/int64(len(destNodeFrontier)), 0)
+	return frontierMean
 }
