ae6946b29d43864ee180a957d3dcdb9045728f75
Herko Lategan <herko@cockroachlabs.com>
2024-06-07 09:06:07 +0100

    roachprod: add shrink command to roachprod cli Expose shrink cluster on the `roachprod` CLI tool.
    Epic: CRDB-33832
    Resolves: #119367
    
    Release Note: None

--- pkg/roachprod/cloud/cluster_cloud.go
+++ pkg/roachprod/cloud/cluster_cloud.go
@@ -348,14 +348,35 @@ func GrowCluster(l *logger.Logger, c *Cluster, NumNodes int) error {
 	}
 
 	providers := c.Clouds()
-	if len(providers) != 1 && providers[0] != gce.ProviderName {
-		return errors.Errorf("cluster %s is not on gce, growing a cluster is currently only supported on %s",
+	if len(providers) != 1 || providers[0] != gce.ProviderName {
+		return errors.Errorf("cannot grow cluster %s, growing a cluster is currently only supported on %s",
 			c.Name, gce.ProviderName)
 	}
 
 	// Only GCE supports expanding a cluster.
 	return vm.ForProvider(gce.ProviderName, func(p vm.Provider) error {
 		return p.Grow(l, c.VMs, c.Name, names)
+	})
+}
+
+// ShrinkCluster removes tail nodes from an existing cluster.
+func ShrinkCluster(l *logger.Logger, c *Cluster, numNodes int) error {
+	providers := c.Clouds()
+	if len(providers) != 1 || providers[0] != gce.ProviderName {
+		return errors.Errorf("cannot shrink cluster %s, shrinking a cluster is currently only supported on %s",
+			c.Name, gce.ProviderName)
+	}
+
+	if numNodes >= len(c.VMs) {
+		return errors.Errorf("cannot shrink cluster %s by %d nodes, only %d nodes in cluster",
+			c.Name, numNodes, len(c.VMs))
+	}
+	// Always delete from the tail.
+	vmsToDelete := c.VMs[len(c.VMs)-numNodes:]
+
+	// Only GCE supports shrinking a cluster.
+	return vm.ForProvider(gce.ProviderName, func(p vm.Provider) error {
+		return p.Shrink(l, vmsToDelete, c.Name)
 	})
 }
 
