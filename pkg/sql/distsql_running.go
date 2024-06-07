25569947165dbe9cc2c4a7f030f25445c05bf55b
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-06-07 16:00:39 -0700

    changefeedccl: create separate root changefeed memory monitor
    This commit introduces a "root" changefeed memory monitor that will be
    the parent for all CDC DistSQL flows. The primary motivation behind
    this change is to disconnect the CDC flow from the planner's monitor
    (which is "txn" monitor of the connection that started the changefeed).
    This is needed since that connection can be closed _before_ the CDC flow
    is cleaned up which triggers the "short-living non-stopped monitor"
    assertion. An additional benefit of this change is improved
    observability since we can now easily track the memory usage across all
    changefeeds.
    
    Release note: None

--- pkg/sql/distsql_running.go
+++ pkg/sql/distsql_running.go
@@ -466,7 +466,11 @@ func (dsp *DistSQLPlanner) setupFlows(
 		batchReceiver = recv
 	}
 	origCtx := ctx
-	ctx, flow, opChains, err := dsp.distSQLSrv.SetupLocalSyncFlow(ctx, evalCtx.Planner.Mon(), &setupReq, recv, batchReceiver, localState)
+	parentMonitor := evalCtx.Planner.Mon()
+	if planCtx.OverridePlannerMon != nil {
+		parentMonitor = planCtx.OverridePlannerMon
+	}
+	ctx, flow, opChains, err := dsp.distSQLSrv.SetupLocalSyncFlow(ctx, parentMonitor, &setupReq, recv, batchReceiver, localState)
 	if err == nil && planCtx.saveFlows != nil {
 		err = planCtx.saveFlows(flows, opChains, planCtx.infra.LocalProcessors, isVectorized)
 	}
@@ -696,7 +700,6 @@ func (dsp *DistSQLPlanner) Run(
 	localState.Txn = txn
 	localState.LocalProcs = plan.LocalProcessors
 	localState.LocalVectorSources = plan.LocalVectorSources
-	localState.MarkFlowMonitorAsLongLiving = planCtx.MarkFlowMonitorAsLongLiving
 	if planCtx.planner != nil {
 		// Note that the planner's collection will only be used for local plans.
 		localState.Collection = planCtx.planner.Descriptors()
