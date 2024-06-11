376404f0548eb77ae9b4b2df33b84e63a869dc01
DarrylWong <darryl@cockroachlabs.com>
2024-06-11 15:13:26 -0400

    roachprod: support non uniform cluster specs
    This change allows for roachprod to create clusters
    with non uniform cluster specs.
    
    Generally, non uniform VM specs are not supported for a CRDB
    cluster, but we often want to provision an additional
    "workload node". This node often times does not need the same
    CPU count as the rest of the cluster.
    
    i.e. it is overkill for a 3 node 32 CPU cluster to have a 32 CPU
    workload node, but a 50 node 8 CPU cluster might find a 8 CPU
    workload node inadequate.

--- pkg/cmd/roachtest/cluster.go
+++ pkg/cmd/roachtest/cluster.go
@@ -884,9 +884,9 @@ func (f *clusterFactory) newCluster(
 
 	providerOptsContainer := vm.CreateProviderOptionsContainer()
 
-	cloud := roachtestflags.Cloud
+	clusterCloud := roachtestflags.Cloud
 	params := spec.RoachprodClusterConfig{
-		Cloud:                  cloud,
+		Cloud:                  clusterCloud,
 		UseIOBarrierOnLocalSSD: cfg.useIOBarrier,
 		PreferredArch:          cfg.arch,
 	}
@@ -901,8 +901,8 @@ func (f *clusterFactory) newCluster(
 	if err != nil {
 		return nil, nil, err
 	}
-	if cloud != spec.Local {
-		providerOptsContainer.SetProviderOpts(cloud, providerOpts)
+	if clusterCloud != spec.Local {
+		providerOptsContainer.SetProviderOpts(clusterCloud, providerOpts)
 	}
 
 	createFlagsOverride(&createVMOpts)
@@ -938,7 +938,7 @@ func (f *clusterFactory) newCluster(
 		}
 
 		c := &clusterImpl{
-			cloud:      cloud,
+			cloud:      clusterCloud,
 			name:       genName,
 			spec:       cfg.spec,
 			expiration: cfg.spec.Expiration(),
@@ -954,7 +954,8 @@ func (f *clusterFactory) newCluster(
 
 		l.PrintfCtx(ctx, "Attempting cluster creation (attempt #%d/%d)", i, maxAttempts)
 		createVMOpts.ClusterName = c.name
-		err = create(ctx, l, cfg.username, cfg.spec.NodeCount, createVMOpts, providerOptsContainer)
+		opts := cloud.ClusterCreateOpts{Nodes: cfg.spec.NodeCount, CreateOpts: createVMOpts, ProviderOptsContainer: providerOptsContainer}
+		err = create(ctx, l, cfg.username, &opts)
 		if err == nil {
 			if err := f.r.registerCluster(c); err != nil {
 				return nil, nil, err
