cockroachdb/cockroach

commit d2f48b991a6f7dd5c753738fbe14f7e30a8fb68a

sql: fast path uniqueness checks for single-row insert
This adds support for building and executing simple INSERT statements
with a single-row VALUES clause where any required uniqueness constraint
checks can be handled via a constrained scan on an index.

This includes INSERT cases such as:

- a single-row VALUES clause into a REGIONAL BY ROW table with a
PRIMARY KEY which has a UUID column generated by default, ie.
`id UUID PRIMARY KEY DEFAULT gen_random_uuid()`, where the
crdb_region column is not specified in the VALUES clause; either
the gateway region is used or it is computed based on other column
values.
- a single-row VALUES clause into a REGIONAL BY ROW table with a
hash sharded unique index where the crdb_region column is not specified
in the VALUES clause

In optbuild, when creating a uniqueness check for rows which are added
to a table, a fast path index check relation is also built when the
mutation source is a single-row values expression or WithScan from
a single-row values expression. That relation is a filtered
Select of a Scan from the target table, where the filters equate
all of the unique check columns with their corresponding
constants or placeholders from the Values expression. If there is a
uniqueness check with a partial index predicate, fast path is
disallowed.

A new exploration rule called InsertFastPath is added to walk the memo
group members created during exploration in `FastPathUniqueChecks` of
the `InsertExpr`, to find any which have been rewritten as a constrained
`ScanExpr`. If found, that means that Scan fully represents the lookup
needed to check for duplicate entries and the Scan constraint can be
used to identify the constants to use in a KV lookup on the scanned
index in a fast path check.

Function CanUseUniqueChecksForInsertFastPath walks the expressions
generated during exploration of the `FastPathUniqueChecks.Check`
relation.  If a constrained scan is found, it is used to build elements

pkg/sql/opt/exec/execbuilder/mutation.go
@@ -16,7 +16,6 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/colinfo"
 	"github.com/cockroachdb/cockroach/pkg/sql/lexbase"
-	"github.com/cockroachdb/cockroach/pkg/sql/mutations"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/cat"
 	"github.com/cockroachdb/cockroach/pkg/sql/opt/exec"
@@ -144,27 +143,59 @@ func (b *Builder) tryBuildFastPathInsert(ins *memo.InsertExpr) (_ execPlan, ok b
 		return execPlan{}, false, nil
 	}
 
-	//  - the input is Values with at most mutations.MaxBatchSize, and there are no
-	//    subqueries;
-	//    (note that mutations.MaxBatchSize() is a quantity of keys in the batch
-	//     that we send, not a number of rows. We use this as a guideline only,
-	//     and there is no guarantee that we won't produce a bigger batch.)
-	values, ok := ins.Input.(*memo.ValuesExpr)
-	if !ok ||
-		values.ChildCount() > mutations.MaxBatchSize(false /* forceProductionMaxBatchSize */) ||
-		values.Relational().HasSubquery ||
-		values.Relational().HasUDF {
-		return execPlan{}, false, nil
-	}
-
-	// We cannot use the fast path if any uniqueness checks are needed.
-	// TODO(rytaft): try to relax this restriction (see #58047).
-	if len(ins.UniqueChecks) > 0 {
+	insInput := ins.Input
+	values, ok := insInput.(*memo.ValuesExpr)
+	// Values expressions containing subqueries or UDFs, or having a size larger
+	// than the max mutation batch size are disallowed.
+	if !ok || !memo.ValuesLegalForInsertFastPath(values) {
 		return execPlan{}, false, nil
 	}
 
 	md := b.mem.Metadata()
 	tab := md.Table(ins.Table)
+
+	uniqChecks := make([]exec.InsertFastPathCheck, len(ins.UniqueChecks))
+	for i := range ins.FastPathUniqueChecks {
+		c := &ins.FastPathUniqueChecks[i]
+		if len(c.DatumsFromConstraint) == 0 {
+			// We need at least one DatumsFromConstraint in order to perform
+			// uniqueness checks during fast-path insert. Even if DatumsFromConstraint
+			// contains no Datums, that case indicates that all values to check come
+			// from the input row.
+			return execPlan{}, false, nil
+		}
+		execFastPathCheck := &uniqChecks[i]
+		// Set up the execbuilder structure from the elements built during
+		// exploration.
+		execFastPathCheck.ReferencedTable = md.Table(c.ReferencedTableID)
+		execFastPathCheck.ReferencedIndex = execFastPathCheck.ReferencedTable.Index(c.ReferencedIndexOrdinal)
+		execFastPathCheck.CheckOrdinal = c.CheckOrdinal
+		locking, err := b.buildLocking(c.Locking)
+		if err != nil {
+			return execPlan{}, false, err
+		}
+		execFastPathCheck.Locking = locking
+		execFastPathCheck.InsertCols = make([]exec.TableColumnOrdinal, len(c.InsertCols))
+		for j, insertCol := range c.InsertCols {
+			execFastPathCheck.InsertCols[j] = exec.TableColumnOrdinal(md.ColumnMeta(insertCol).Table.ColumnOrdinal(insertCol))
+		}
+		datumsFromConstraintSpec := c.DatumsFromConstraint
+		execFastPathCheck.DatumsFromConstraint = make([]tree.Datums, len(datumsFromConstraintSpec))
+		for j, row := range datumsFromConstraintSpec {
+			execFastPathCheck.DatumsFromConstraint[j] = make(tree.Datums, tab.ColumnCount())
+			tuple := row.(*memo.TupleExpr)
+			if len(c.InsertCols) != len(tuple.Elems) {
+				panic(errors.AssertionFailedf("expected %d tuple elements in insert fast path uniqueness check, found %d", len(c.InsertCols), len(tuple.Elems)))
+			}
+			for k := 0; k < len(tuple.Elems); k++ {
+				constExpr, _ := tuple.Elems[k].(*memo.ConstExpr)
+				execFastPathCheck.DatumsFromConstraint[j][execFastPathCheck.InsertCols[k]] = constExpr.Value
+			}
+		}
+		execFastPathCheck.MkErr = func(values tree.Datums) error {
+			return mkFastPathUniqueCheckErr(md, &ins.UniqueChecks[i], values, execFastPathCheck.ReferencedIndex)
+		}
+	}
 
 	//  - there are no self-referencing foreign keys;
 	//  - all FK checks can be performed using direct lookups into unique indexes.
@@ -276,6 +307,7 @@ func (b *Builder) tryBuildFastPathInsert(ins *memo.InsertExpr) (_ execPlan, ok b
 		returnOrds,
 		checkOrds,
 		fkChecks,
+		uniqChecks,
 		b.allowAutoCommit,
 	)
 	if err != nil {
@@ -818,6 +850,42 @@ func mkUniqueCheckErr(md *opt.Metadata, c *memo.UniqueChecksItem, keyVals tree.D
 	)
 }
 
+// mkFastPathUniqueCheckErr is a wrapper for mkUniqueCheckErr in the insert fast
+// path flow, which reorders the keyVals row according to the ordering of the
+// key columns in index `idx`. This is needed because mkUniqueCheckErr assumes
+// the ordering of columns in `keyVals` matches the ordering of columns in
+// `cat.UniqueConstraint.ColumnOrdinal(tabMeta.Table, i)`.
+func mkFastPathUniqueCheckErr(
+	md *opt.Metadata, c *memo.UniqueChecksItem, keyVals tree.Datums, idx cat.Index,
+) error {
+
+	tabMeta := md.TableMeta(c.Table)
+	uc := tabMeta.Table.Unique(c.CheckOrdinal)
+
+	newKeyVals := make(tree.Datums, 0, uc.ColumnCount())
+
+	for i := 0; i < uc.ColumnCount(); i++ {
+		ord := uc.ColumnOrdinal(tabMeta.Table, i)
+		found := false
+		for j := 0; j < idx.ColumnCount(); j++ {
+			keyCol := idx.Column(j)
+			keyColOrd := keyCol.Column.Ordinal()
+			if ord == keyColOrd {
+				newKeyVals = append(newKeyVals, keyVals[j])
+				found = true
+				break
+			}
+		}
+		if !found {
+			// We still need to return an error, even if the key values could not be
+			// determined.
+			return errors.AssertionFailedf(
+				"insert fast path failed uniqueness check, but could not find unique columns for row, %v", keyVals)
+		}
+	}
+	return mkUniqueCheckErr(md, c, newKeyVals)
+}
+
 // mkFKCheckErr generates a user-friendly error describing a foreign key
 // violation. The keyVals are the values that correspond to the
 // cat.ForeignKeyConstraint columns.
@@ -923,59 +991,6 @@ func (b *Builder) buildFKCascades(withID opt.WithID, cascades memo.FKCascades) e
 		b.cascades = append(b.cascades, cb.setupCascade(&cascades[i]))
 	}
 	return nil
-}
-
-// canAutoCommit determines if it is safe to auto commit the mutation contained
-// in the expression.
-//
-// Mutations can commit the transaction as part of the same KV request,
-// potentially taking advantage of the 1PC optimization. This is not ok to do in
-// general; a sufficient set of conditions is:
-//  1. There is a single mutation in the query.
-//  2. The mutation is the root operator, or it is directly under a Project
-//     with no side-effecting expressions. An example of why we can't allow
-//     side-effecting expressions: if the projection encounters a
-//     division-by-zero error, the mutation shouldn't have been committed.
-//
-// An extra condition relates to how the FK checks are run. If they run before
-// the mutation (via the insert fast path), auto commit is possible. If they run
-// after the mutation (the general path), auto commit is not possible. It is up
-// to the builder logic for each mutation to handle this.
-//
-// Note that there are other necessary conditions related to execution
-// (specifically, that the transaction is implicit); it is up to the exec
-// factory to take that into account as well.
-func (b *Builder) canAutoCommit(rel memo.RelExpr) bool {
-	if !rel.Relational().CanMutate {
-		// No mutations in the expression.
-		return false
-	}
-
-	switch rel.Op() {
-	case opt.InsertOp, opt.UpsertOp, opt.UpdateOp, opt.DeleteOp:
-		// Check that there aren't any more mutations in the input.
-		// TODO(radu): this can go away when all mutations are under top-level
-		// With ops.
-		return !rel.Child(0).(memo.RelExpr).Relational().CanMutate
-
-	case opt.ProjectOp:
-		// Allow Project on top, as long as the expressions are not side-effecting.
-		proj := rel.(*memo.ProjectExpr)
-		for i := 0; i < len(proj.Projections); i++ {
-			if !proj.Projections[i].ScalarProps().VolatilitySet.IsLeakproof() {
-				return false
-			}
-		}
-		return b.canAutoCommit(proj.Input)
-
-	case opt.DistributeOp:
-		// Distribute is currently a no-op, so check whether the input can
-		// auto-commit.
-		return b.canAutoCommit(rel.(*memo.DistributeExpr).Input)
-
-	default:
-		return false
-	}
 }
 
 // forUpdateLocking is the row-level locking mode used by mutations during their

pkg/sql/logictest/testdata/logic_test/unique
@@ -948,3 +948,81 @@ NULL
 NULL
 NULL
 NULL
+
+subtest multirowValuesInsertFastPath
+
+statement ok
+CREATE TABLE t1 (
+  pk int PRIMARY KEY,
+  pk2 int,
+  a int,
+  b int,
+  j JSON,
+  INDEX (a),
+  UNIQUE WITHOUT INDEX(b, a),
+  INDEX (b,a) STORING(pk2, j),
+  INVERTED INDEX (j),
+  FAMILY (pk, pk2, a, b)
+)
+
+statement ok
+INSERT INTO t1 (pk, pk2, a, b, j) VALUES
+   (0, 0, 1, 3, '{"a": "b"}');
+
+# This uses insert fast-path. Verify it catches the duplicate value of (b,a).
+statement error pq: duplicate key value violates unique constraint "unique_b_a"\nDETAIL: Key \(a, b\)=\(1, 3\) already exists\.
+INSERT INTO t1 (pk, pk2, a, b, j) VALUES
+   (2, 2, 1, 3, '{"a": "b"}'), (4, 5, 6, 7, '{"a": "b"}');
+
+statement ok
+CREATE TABLE multiple_uniq (
+  a INT,
+  b INT,
+  c INT,
+  d INT,
+  UNIQUE WITHOUT INDEX (b, c),
+  UNIQUE WITHOUT INDEX (a, b, d),
+  UNIQUE WITHOUT INDEX (a),
+  INDEX (a, b, d),
+  INDEX (b, c),
+  FAMILY (a),
+  FAMILY (b),
+  FAMILY (c),
+  FAMILY (d)
+)
+
+# This uses insert fast-path and should succeed.
+statement ok
+INSERT INTO multiple_uniq
+VALUES (1,2,3,4), (5,6,7,8)
+
+# A table with multiple unique without index checks is eligible for insert fast
+# path. Verify it catches the duplicate value of (a).
+statement error pq: duplicate key value violates unique constraint "unique_a"\nDETAIL: Key \(a\)=\(1\) already exists\.
+INSERT INTO multiple_uniq
+VALUES (1,12,13,14), (15,16,17,18)
+
+# A table with multiple unique without index checks is eligible for insert fast
+# path. Verify it catches the duplicate value of (b,c).
+statement error pq: duplicate key value violates unique constraint "unique_b_c"\nDETAIL: Key \(b, c\)=\(2, 3\) already exists\.
+INSERT INTO multiple_uniq (a,c,b,d)
+VALUES (11,3,2,14), (15,16,17,18)
+
+statement ok
+PREPARE p1 AS INSERT INTO multiple_uniq
+VALUES ($1, $1, $1, $1), ($2, $2, $2, $2)
+
+# A table with multiple unique without index checks is eligible for parameterized insert fast
+# path. Verify it catches the duplicate value of (a).
+statement error pq: duplicate key value violates unique constraint "unique_a"\nDETAIL: Key \(a\)=\(1\) already exists\.
+EXECUTE p1(1, 2)
+
+statement ok
+ALTER TABLE multiple_uniq ADD CONSTRAINT uniq_fk FOREIGN KEY (d) REFERENCES uniq (k) ON DELETE CASCADE;
+
+# A table with multiple unique without index checks and an FK constraint is
+# eligible for insert fast path. Verify it catches the foreign key violation on
+# (d).
+statement error pq: insert on table "multiple_uniq" violates foreign key constraint "uniq_fk"\nDETAIL: Key \(d\)=\(14\) is not present in table "uniq"\.
+INSERT INTO multiple_uniq (a,b,c,d)
+VALUES (11,12,13,14), (15,16,17,18)

commit 01952d5f55364f48ca3f2e07b186f0c5550f446f

execbuilder: fix nil pointer in tryBuildFastPathInsert
This commit fixes a nil pointer error that we previously would always
hit in `tryBuildFastPathInsert` whenever we had a bool value in the
VALUES clause. The bug was introduced in
d2f48b991a6f7dd5c753738fbe14f7e30a8fb68a where we added the support for
fast path with single row VALUES clauses. The issue is that we ignored
the "ok" boolean for the type conversion, which is now fixed by
explicitly checking a couple of bool expressions as well as not using
the fast path if non-const non-bool expression is present.

Release note (bug fix): Previously, CockroachDB would hit an internal
error when evaluating INSERTs into REGIONAL BY ROW tables where the
source is a VALUES clause with a single row and at least one boolean
expression, and this is now fixed. The bug was introduced in 23.2.0
release.

pkg/sql/opt/exec/execbuilder/mutation.go
@@ -204,8 +204,18 @@ func (b *Builder) tryBuildFastPathInsert(
 				panic(errors.AssertionFailedf("expected %d tuple elements in insert fast path uniqueness check, found %d", len(c.InsertCols), len(tuple.Elems)))
 			}
 			for k := 0; k < len(tuple.Elems); k++ {
-				constExpr, _ := tuple.Elems[k].(*memo.ConstExpr)
-				execFastPathCheck.DatumsFromConstraint[j][execFastPathCheck.InsertCols[k]] = constExpr.Value
+				var constDatum tree.Datum
+				switch e := tuple.Elems[k].(type) {
+				case *memo.ConstExpr:
+					constDatum = e.Value
+				case *memo.TrueExpr:
+					constDatum = tree.DBoolTrue
+				case *memo.FalseExpr:
+					constDatum = tree.DBoolFalse
+				default:
+					return execPlan{}, colOrdMap{}, false, nil
+				}
+				execFastPathCheck.DatumsFromConstraint[j][execFastPathCheck.InsertCols[k]] = constDatum
 			}
 		}
 		uniqCheck := &ins.UniqueChecks[i]

pkg/sql/logictest/testdata/logic_test/unique
@@ -1052,3 +1052,23 @@ statement error pgcode 23505 duplicate key value violates unique constraint \"un
 INSERT INTO t115377 VALUES (2, 1, 1, 'east')
 
 subtest end
+
+# Regression test for hitting a nil pointer in the insert fast path when seeing
+# a bool expression in the VALUES clause (#123103).
+statement ok
+CREATE TABLE t123103 (
+	id STRING DEFAULT gen_random_uuid()::STRING NOT NULL,
+	r  region NOT NULL DEFAULT 'us-east'::region,
+	a  STRING NOT NULL,
+	b  BOOL NOT NULL,
+	CONSTRAINT tab_pkey PRIMARY KEY (r ASC, id ASC),
+	CONSTRAINT tab_id_key UNIQUE WITHOUT INDEX (id ASC),
+	CONSTRAINT tab_r_a_b_key UNIQUE (r ASC, a ASC, b ASC),
+	CONSTRAINT tab_a_b_key UNIQUE WITHOUT INDEX (a ASC, b ASC)
+);
+INSERT INTO t123103 (id, a, b) VALUES ('1234567890', 'foo', true);
+
+query TTB
+SELECT id, a, b FROM t123103;
+----
+1234567890  foo  true