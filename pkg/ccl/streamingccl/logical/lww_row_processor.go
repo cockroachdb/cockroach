3c2e0eb3ec9ec4c2c6c8cb0855d02cecd631ed37
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-06-13 16:20:40 -0700

    sql,logical: attribute ingestion queries to "external" things
    This commit introduces a way for the internally executed queries to be
    attributed to the user which is needed so that "external" observability
    would pick it up. In particular, we now can specify an
    InternalExecutorOverride option so that when instantiating the
    connExecutor we set the application name prefix to `$ attributed-internal`
    as well as we use the "server" metrics. As a result, such attributed
    internal queries will show up on the SQL Activity page as well as will
    be included into metrics like number of queries executed whenever this
    option is used. Note that these queries will keep contributing to
    "internal" things for less important stuff (like memory monitor metrics
    as well as txn guardrail metrics). No changes to the underlying internal
    executor machinery are applied so this _should_ be a pretty safe change.
    
    The only hiccup I can think of right now is whether the hard-coded app
    name prefix that doesn't start with `$ internal` (or `$$` for
    "delegated" internal queries) can break some assumptions elsewhere.
    
    This option is now utilized to attribute INSERT and DELETE queries of
    the ingestion pipeline of the LDR to the user.
    
    Release note: None

--- pkg/ccl/streamingccl/logical/lww_row_processor.go
+++ pkg/ccl/streamingccl/logical/lww_row_processor.go
@@ -28,6 +28,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/catid"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/eval"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/errors"
 )
@@ -114,6 +115,10 @@ func (lww *sqlLastWriteWinsRowProcessor) ProcessRow(
 	}
 }
 
+var attributeToUser = sessiondata.InternalExecutorOverride{
+	AttributeToUser: true,
+}
+
 func (lww *sqlLastWriteWinsRowProcessor) insertRow(
 	ctx context.Context, txn isql.Txn, row cdcevent.Row,
 ) error {
@@ -146,7 +151,7 @@ func (lww *sqlLastWriteWinsRowProcessor) insertRow(
 	if !ok {
 		return errors.Errorf("no pre-generated insert query for table %d column family %d", row.TableID, row.FamilyID)
 	}
-	if _, err := txn.ExecParsed(ctx, "replicated-insert", txn.KV(), insertQuery, datums...); err != nil {
+	if _, err := txn.ExecParsed(ctx, "replicated-insert", txn.KV(), attributeToUser, insertQuery, datums...); err != nil {
 		log.Warningf(ctx, "replicated insert failed (query: %s): %s", insertQuery.SQL, err.Error())
 		return err
 	}
@@ -164,7 +169,7 @@ func (lww *sqlLastWriteWinsRowProcessor) deleteRow(
 		return err
 	}
 	deleteQuery := lww.queryBuffer.deleteQueries[row.TableID]
-	if _, err := txn.ExecParsed(ctx, "replicated-delete", txn.KV(), deleteQuery, datums...); err != nil {
+	if _, err := txn.ExecParsed(ctx, "replicated-delete", txn.KV(), attributeToUser, deleteQuery, datums...); err != nil {
 		log.Warningf(ctx, "replicated delete failed (query: %s): %s", deleteQuery.SQL, err.Error())
 		return err
 	}
