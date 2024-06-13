dfd49e410702ed491add0fed9b7ebbd7429f24bb
Xin Hao Zhang <xzhang@cockroachlabs.com>
2024-06-13 17:53:35 -0400

    sql: add cluster insights response to mem account
    When constructing virtual tables for cluster-wide insights
    we should add the response size to the memory account, and
    return with an error if the budget is exceeded. This mirrors
    behaviour done with the vtable fetching cluster-wide in-memory
    sql stats.
    
    Part of: #125340
    Epic: none
    Release note: None

--- pkg/sql/crdb_internal.go
+++ pkg/sql/crdb_internal.go
@@ -8263,8 +8263,15 @@ func populateTxnExecutionInsights(
 		return noViewActivityOrViewActivityRedactedRoleError(p.User())
 	}
 
+	acc := p.Mon().MakeBoundAccount()
+	defer acc.Close(ctx)
+
 	response, err := p.extendedEvalCtx.SQLStatusServer.ListExecutionInsights(ctx, request)
 	if err != nil {
+		return err
+	}
+
+	if err := acc.Grow(ctx, int64(response.Size())); err != nil {
 		return err
 	}
 
@@ -8456,10 +8463,18 @@ func populateStmtInsights(
 		return noViewActivityOrViewActivityRedactedRoleError(p.User())
 	}
 
+	acct := p.Mon().MakeBoundAccount()
+	defer acct.Close(ctx)
+
 	response, err := p.extendedEvalCtx.SQLStatusServer.ListExecutionInsights(ctx, request)
 	if err != nil {
 		return err
 	}
+
+	if err := acct.Grow(ctx, int64(response.Size())); err != nil {
+		return err
+	}
+
 	for _, insight := range response.Insights {
 		// We don't expect the transaction to be null here, but we should provide
 		// this check to ensure we only show valid data.
