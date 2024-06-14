c50d83867feb8a38a8794d83e43dd2ee5740abd0
Marcus Gartner <marcus@cockroachlabs.com>
2024-06-14 14:59:27 -0400

    sql: add plan_cache_mode session setting
    The `plan_cache_mode` session setting has been added. It allows for
    three options: `force_custom_plan`, `force_generic_plan`, and `auto`.
    The session currently has no effect. In future commits it will control
    how the system chooses between using a custom or generic query plan.
    
    Release note: None

--- pkg/sql/vars.go
+++ pkg/sql/vars.go
@@ -3377,6 +3377,30 @@ var varGen = map[string]sessionVar{
 		},
 		GlobalDefault: globalTrue,
 	},
+
+	`plan_cache_mode`: {
+		GetStringVal: makePostgresBoolGetStringValFn(`optimizer_push_offset_into_index_join`),
+		Set: func(_ context.Context, m sessionDataMutator, s string) error {
+			mode, ok := sessiondatapb.PlanCacheModeFromString(s)
+			if !ok {
+				return newVarValueError(
+					`plan_cache_mode`,
+					s,
+					sessiondatapb.PlanCacheModeForceCustom.String(),
+					sessiondatapb.PlanCacheModeForceGeneric.String(),
+					sessiondatapb.PlanCacheModeAuto.String(),
+				)
+			}
+			m.SetPlanCacheMode(mode)
+			return nil
+		},
+		Get: func(evalCtx *extendedEvalContext, _ *kv.Txn) (string, error) {
+			return evalCtx.SessionData().PlanCacheMode.String(), nil
+		},
+		GlobalDefault: func(sv *settings.Values) string {
+			return sessiondatapb.PlanCacheModeForceCustom.String()
+		},
+	},
 }
 
 func ReplicationModeFromString(s string) (sessiondatapb.ReplicationMode, error) {
