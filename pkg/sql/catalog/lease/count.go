fac52238351b64c4d055620e2fad4e24db87db2f
Matt Spilchen <matt.spilchen@cockroachlabs.com>
2024-06-14 11:22:10 -0300

    sql/schemachanger: Enhanced debugging for long-running schema changes
    This improves debugging of long-running schema changes by leveraging
    enhancements in the job infrastructure.
    
    1. Retain Trace Events
    Trace events are now retained and persisted after the schema change,
    including lower-level stats from the KV layer. This helps us understand
    where time is being spent. To achieve this, I implemented the
    `TraceableJob` interface for `newSchemaChangeResumer`.
    
    2. Collect Timing Stats
    We gather statistics on the time spent in `WaitForOneVersion` and
    `WaitForNoVersion` and include these stats in the retained trace events
    for review after the schema change. To include these stats in trace
    spans, I implemented the `tracing.Structured` interface, which happens
    automatically for protobuf messages. The new stats are added in
    `lease.proto`.
    
    The stats track the number of retries, elapsed time, the number of
    leases we are waiting on, the number of distinct nodes we are waiting
    on, and one node ID that we are waiting on. These last three stats are
    useful for long waits as we periodically dump the stats.
    
    During a schema change, we may wait multiple times. We do not aggregate
    the stats, as this proved tricky and is left as a follow-on item if
    needed.
    
    3. Pretty Print Current Plan
    We provide a formatted output of the current running phase plan, similar
    to `EXPLAIN (DDL)`. This can be requested through the job profiler in
    the DBConsole (under 'Jobs -> Advanced Debugging -> Request Execution
    Details'). This will clarify the status field (e.g., PostCommitPhase
    stage 2 of 7 with 1 BackfillType op pending) by showing the stages
    within the phase.
    
    The file will appear with a name like
    ‘explain-phase.20240612_135233.70.txt’ in the `job_info` table.
    
    I compute the explain output optimistically, rather than on demand, to
    avoid the complexity of storing the `scplan.Plan` struct. It seemed
    easiest to store the explain output and keep the `scplan.Plan` struct local.
    
    Epic: CRDB-38033
    Closes: #124075
    Release note: None
    
    Signed-off-by: Matt Spilchen <matt.spilchen@cockroachlabs.com>

--- pkg/sql/catalog/lease/count.go
+++ pkg/sql/catalog/lease/count.go
@@ -34,6 +34,17 @@ import (
 	"github.com/cockroachdb/errors"
 )
 
+type countDetail struct {
+	// count is the number of unexpired leases
+	count int
+	// numSQLInstances is the number of distinct SQL instances with unexpired leases.
+	numSQLInstances int
+	// sampleSQLInstanceID is one of the sql_instance_id values we are waiting on,
+	// but only if we are waiting on at least one lease. If the count is 0, this
+	// value will also be 0.
+	sampleSQLInstanceID int
+}
+
 // CountLeases returns the number of unexpired leases for a number of descriptors
 // each at a particular version at a particular time.
 func CountLeases(
@@ -46,6 +57,24 @@ func CountLeases(
 	at hlc.Timestamp,
 	forAnyVersion bool,
 ) (int, error) {
+	detail, err := countLeasesWithDetail(ctx, db, codec, cachedDatabaseRegions,
+		settings, versions, at, forAnyVersion)
+	if err != nil {
+		return 0, err
+	}
+	return detail.count, nil
+}
+
+func countLeasesWithDetail(
+	ctx context.Context,
+	db isql.DB,
+	codec keys.SQLCodec,
+	cachedDatabaseRegions regionliveness.CachedDatabaseRegions,
+	settings *clustersettings.Settings,
+	versions []IDVersion,
+	at hlc.Timestamp,
+	forAnyVersion bool,
+) (countDetail, error) {
 	// Indicates if the leasing descriptor has been upgraded for session based
 	// leasing. Note: Unit tests will never provide cached database regions
 	// so resolve the version from the cluster settings.
@@ -103,7 +132,7 @@ func CountLeases(
 		whereClauseIdx = append(whereClauseIdx, 1)
 	}
 
-	var count int
+	var detail countDetail
 	if err := db.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
 		txn.KV().SetDebugName("count-leases")
 		if err := txn.KV().SetFixedTimestamp(ctx, at); err != nil {
@@ -129,9 +158,9 @@ func CountLeases(
 						forceMultiRegionQuery {
 						// If we are injecting a raw leases descriptors, that will not have the enum
 						// type set, so convert the region to byte equivalent physical representation.
-						count, err = countLeasesByRegion(ctx, txn, prober, regionMap, cachedDatabaseRegions, len(descsToInject) > 0, at, whereClause)
+						detail, err = countLeasesByRegion(ctx, txn, prober, regionMap, cachedDatabaseRegions, len(descsToInject) > 0, at, whereClause)
 					} else {
-						count, err = countLeasesNonMultiRegion(ctx, txn, at, whereClause)
+						detail, err = countLeasesNonMultiRegion(ctx, txn, at, whereClause)
 						// It's possible that our cached database descriptor is stale relative to the
 						// visible lease table descriptor. Because the lease manager uses rangefeeds to
 						// keep track of new descriptor versions, when converting to a multi-region
@@ -150,26 +179,28 @@ func CountLeases(
 				return err
 			}
 			// Exit if either the session or expiry based counts are zero.
-			if count > 0 {
+			if detail.count > 0 {
 				return nil
 			}
 		}
 		return nil
 	}); err != nil {
-		return 0, err
+		return countDetail{}, err
 	}
-	return count, nil
+	return detail, nil
 }
 
 // Counts leases in non multi-region environments.
 func countLeasesNonMultiRegion(
 	ctx context.Context, txn isql.Txn, at hlc.Timestamp, whereClauses []string,
-) (int, error) {
+) (countDetail, error) {
 	stmt := fmt.Sprintf(
-		`SELECT count(1) FROM system.public.lease AS OF SYSTEM TIME '%s' WHERE 
-crdb_region=$2 AND`,
+		`SELECT %[1]s FROM system.public.lease AS OF SYSTEM TIME '%[2]s' WHERE 
+crdb_region=$2 AND %[3]s`,
+		getCountLeaseColumns(whereClauses[0]),
 		at.AsOfSystemTime(),
-	) + strings.Join(whereClauses, " OR ")
+		strings.Join(whereClauses, " OR "),
+	)
 	values, err := txn.QueryRowEx(
 		ctx, "count-leases", txn.KV(),
 		sessiondata.NodeUserSessionDataOverride,
@@ -178,13 +209,16 @@ crdb_region=$2 AND`,
 		enum.One, // Single region database can only have one region prefix assigned.
 	)
 	if err != nil {
-		return 0, err
+		return countDetail{}, err
 	}
 	if values == nil {
-		return 0, errors.New("failed to count leases")
+		return countDetail{}, errors.New("failed to count leases")
 	}
-	count := int(tree.MustBeDInt(values[0]))
-	return count, nil
+	return countDetail{
+		count:               int(tree.MustBeDInt(values[0])),
+		numSQLInstances:     int(tree.MustBeDInt(values[1])),
+		sampleSQLInstanceID: int(tree.MustBeDInt(values[2])),
+	}, nil
 }
 
 // Counts leases by region in MR environments.
@@ -197,16 +231,18 @@ func countLeasesByRegion(
 	convertRegionsToBytes bool,
 	at hlc.Timestamp,
 	whereClauses []string,
-) (int, error) {
+) (countDetail, error) {
 	regionClause := "crdb_region=$2::system.crdb_internal_region"
 	if convertRegionsToBytes {
 		regionClause = "crdb_region=$2"
 	}
 	stmt := fmt.Sprintf(
-		`SELECT count(1) FROM system.public.lease AS OF SYSTEM TIME '%s' WHERE `,
+		`SELECT %[1]s FROM system.public.lease AS OF SYSTEM TIME '%[2]s' WHERE %[3]s `,
+		getCountLeaseColumns(whereClauses[0]),
 		at.AsOfSystemTime(),
-	) + regionClause + ` AND (` + strings.Join(whereClauses, " OR ") + ")"
-	var count int
+		regionClause+` AND (`+strings.Join(whereClauses, " OR ")+")",
+	)
+	var detail countDetail
 	if err := regionMap.ForEach(func(region string) error {
 		regionEnumValue := region
 		// The leases table descriptor injected does not have the type of the column
@@ -261,10 +297,32 @@ func countLeasesByRegion(
 		if values == nil {
 			return errors.New("failed to count leases")
 		}
-		count += int(tree.MustBeDInt(values[0]))
+		detail.count += int(tree.MustBeDInt(values[0]))
+		detail.numSQLInstances += int(tree.MustBeDInt(values[1]))
+		if detail.sampleSQLInstanceID == 0 { // only retain the first sample ID
+			detail.sampleSQLInstanceID = int(tree.MustBeDInt(values[2]))
+		}
 		return nil
 	}); err != nil {
-		return 0, err
+		return countDetail{}, err
 	}
-	return count, nil
+	return detail, nil
+}
+
+func getCountLeaseColumns(whereClause string) string {
+	var sb strings.Builder
+	sb.WriteString("count(1)")
+	// We only care about the count of leases, which is the first column. For
+	// debugging purposes, we also return the number of distinct nodes we are
+	// waiting on and one of the nodes we are waiting on. These two details will
+	// appear in the periodic dumping of wait stats.
+	//
+	// The system.lease table went through some column renames in past versions.
+	// Pick the correct version based on the columns used in WHERE clause.
+	if strings.Contains(whereClause, "desc_id") {
+		sb.WriteString(`, count(distinct sql_instance_id), ifnull(min(sql_instance_id),0)`)
+		return sb.String()
+	}
+	sb.WriteString(`, count(distinct "nodeID"), ifnull(min("nodeID"),0)`)
+	return sb.String()
 }
--- pkg/sql/catalog/lease/lease_internal_test.go
+++ pkg/sql/catalog/lease/lease_internal_test.go
@@ -29,7 +29,9 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/desctestutils"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/systemschema"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/tabledesc"
+	"github.com/cockroachdb/cockroach/pkg/sql/isql"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/util/admission"
@@ -1610,4 +1612,65 @@ func TestSessionLeasingClusterSetting(t *testing.T) {
 			require.False(t, lm.sessionBasedLeasingModeAtLeast(ctx, mode))
 		}
 	}
+}
+
+// TestLeaseCountDetail will test out the extra debugging info that is emitted
+// from countLeasesWithDetail
+func TestLeaseCountDetail(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	ctx := context.Background()
+	st := cluster.MakeTestingClusterSettings()
+	// We are going to disable session based leasing as its easier to add a lot of
+	// fake leases into system.leases.
+	LeaseEnableSessionBasedLeasing.Override(ctx, &st.SV, int64(SessionBasedLeasingOff))
+	srv := serverutils.StartServerOnly(t, base.TestServerArgs{
+		Settings:          st,
+		DefaultTestTenant: base.TestNeedsTightIntegrationBetweenAPIsAndTestingKnobs,
+		Knobs:             base.TestingKnobs{},
+	})
+	defer srv.Stopper().Stop(ctx)
+
+	s := srv.ApplicationLayer()
+	idb := srv.InternalDB().(isql.DB)
+	executor := idb.Executor()
+	descID := 10 // Descriptor ID that we will insert and query in this test
+	// Insert using a synthetic descriptor.
+	err := executor.WithSyntheticDescriptors(catalog.Descriptors{systemschema.LeaseTable_V23_2()}, func() error {
+		nodeIDs := []string{"2", "2", "8", "3", "2"}
+		for i, nodeID := range nodeIDs {
+			version := i + 1
+			expiration := "2124-06-12-10:10:10" // Choose a long expiration, so they are unexpired when we count
+			region := "E'\\\\x80'"              // Single region
+			_, err := executor.Exec(ctx, "add-rows-for-test", nil,
+				fmt.Sprintf("INSERT INTO system.lease VALUES (%d, %d, %s, '%s', %s)",
+					descID, version, nodeID, expiration, region))
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	require.NoError(t, err)
+
+	codec := s.Codec()
+	now := hlc.Timestamp{WallTime: timeutil.Now().UnixNano()}
+
+	// Count leases for any version
+	detail, err := countLeasesWithDetail(ctx, idb, codec, nil, st,
+		[]IDVersion{{ID: descpb.ID(descID), Version: 1}}, now, true)
+	require.NoError(t, err)
+	require.Equal(t, 5, detail.count)
+	require.Equal(t, 3, detail.numSQLInstances)
+	require.Equal(t, 2, detail.sampleSQLInstanceID)
+
+	// Count leases only for a specific version
+	detail, err = countLeasesWithDetail(ctx, idb, codec, nil, st,
+		[]IDVersion{{ID: descpb.ID(descID), Version: 3}}, now, false)
+	require.NoError(t, err)
+	// We should see the fake leases we added plus the one added by the system.
+	require.Equal(t, 1, detail.count)
+	require.Equal(t, 1, detail.numSQLInstances)
+	require.Equal(t, 8, detail.sampleSQLInstanceID)
 }
