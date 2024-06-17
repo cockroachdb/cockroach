41426fda9e6699740b7bf0b7c35890f176ad9bb0
Andrii Vorobiov <and.vorobiov@gmail.com>
2024-06-17 15:00:43 +0300

    ui: fix Storybooks for Db Console
    This patch changes Storybooks configuration to rely
    on `main.js` file which is a default place for configs
    now.
    Also, it moves default global styles imports to decorators
    file.
    
    Release note: None

---
+++ pkg/ui/workspaces/db-console/.storybook/main.js
@@ -0,0 +1,27 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+const path = require("path");
+// const appConfig = require("../webpack.config");
+
+// const customConfig = appConfig();
+
+module.exports = {
+  stories: ["../src/**/*.stories.tsx"],
+  // addons: ["@storybook/addon-actions", "@storybook/addon-links"],
+  typescript: { reactDocgen: false },
+  // webpackFinal: async config => {
+  //   config.module.rules = [...customConfig.module.rules];
+  //   config.resolve.extensions.push(".ts", ".tsx");
+  //   config.resolve.alias.src = path.resolve(__dirname, "../src");
+  //   return config;
+  // },
+  framework: "@storybook/react",
+};
