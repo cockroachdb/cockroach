5a1977bc1f6c5d04d148efa08dfc151acecd932c
Andrii Vorobiov <and.vorobiov@gmail.com>
2024-06-17 15:00:43 +0300

    ui: fix Storybooks for Db Console
    This patch changes Storybooks configuration to rely
    on `main.js` file which is a default place for configs
    now.
    Also, it moves default global styles imports to decorators
    file.
    
    Release note: None

--- pkg/ui/workspaces/db-console/.storybook/decorators/withBackground.tsx
+++ pkg/ui/workspaces/db-console/.storybook/decorators/withBackground.tsx
@@ -10,6 +10,11 @@
 
 import React from "react";
 import {RenderFunction} from "storybook__react";
+import "nvd3/build/nv.d3.min.css";
+import "react-select/dist/react-select.css";
+import "styl/app.styl";
+import "../styles.css";
+import "src/views/app/containers/layout/layout.styl";
 
 export const withBackgroundFactory = (backgroundColor = "#F5F7FA") => (storyFn: RenderFunction) => (
   <div style={{backgroundColor}}>
