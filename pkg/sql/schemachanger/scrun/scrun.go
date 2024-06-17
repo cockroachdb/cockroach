389619e9ef66bdc4afb9c235fd642c0105c19c8c
Matt Spilchen <matt.spilchen@cockroachlabs.com>
2024-06-17 12:14:36 -0300

    sql/schemachanger: Enhanced debugging for long-running schema changes
    This improves debugging of long-running schema changes by leveraging
    enhancements in the job infrastructure.
    
    1. Retain Trace Events
    Trace events are now retained and persisted after the schema change,
    including lower-level stats from the KV layer. This helps us understand
    where time is being spent. To achieve this, I implemented the
    `TraceableJob` interface for `newSchemaChangeResumer`.
    
    2. Collect Timing Stats
    We gather statistics on the time spent in `WaitForOneVersion` and
    `WaitForNoVersion` and include these stats in the retained trace events
    for review after the schema change. To include these stats in trace
    spans, I implemented the `tracing.Structured` interface, which happens
    automatically for protobuf messages. The new stats are added in
    `lease.proto`.
    
    The stats track the number of retries, elapsed time, the number of
    leases we are waiting on, the number of distinct nodes we are waiting
    on, and one node ID that we are waiting on. These last three stats are
    useful for long waits as we periodically dump the stats.
    
    During a schema change, we may wait multiple times. We do not aggregate
    the stats, as this proved tricky and is left as a follow-on item if
    needed.
    
    3. Pretty Print Current Plan
    We provide a formatted output of the current running phase plan, similar
    to `EXPLAIN (DDL)`. This can be requested through the job profiler in
    the DBConsole (under 'Jobs -> Advanced Debugging -> Request Execution
    Details'). This will clarify the status field (e.g., PostCommitPhase
    stage 2 of 7 with 1 BackfillType op pending) by showing the stages
    within the phase.
    
    The file will appear with a name like
    ‘explain-phase.20240612_135233.70.txt’ in the `job_info` table.
    
    I compute the explain output optimistically, rather than on demand, to
    avoid the complexity of storing the `scplan.Plan` struct. It seemed
    easiest to store the explain output and keep the `scplan.Plan` struct local.
    
    Epic: CRDB-38033
    Closes: #124075
    Release note: None
    
    Signed-off-by: Matt Spilchen <matt.spilchen@cockroachlabs.com>

--- pkg/sql/schemachanger/scrun/scrun.go
+++ pkg/sql/schemachanger/scrun/scrun.go
@@ -132,6 +132,7 @@ func RunSchemaChangesInJob(
 		}
 		return err
 	}
+	deps.SetExplain(p.ExplainCompact())
 	for i := range p.Stages {
 		// Execute each stage in its own transaction.
 		if err := deps.WithTxnInJob(ctx, func(ctx context.Context, td scexec.Dependencies, el EventLogger) error {
