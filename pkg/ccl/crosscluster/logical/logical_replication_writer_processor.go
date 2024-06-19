8285451d854c2c40859a9da7bff5de368cab5dd2
David Taylor <tinystatemachine@gmail.com>
2024-06-19 12:07:55 +0000

    streamingccl/logical: cleanup metrics in-code names
    This cleans up the in-code names of the metrics in the logical package, to
    use StreamBatch or ApplyBatch and standardize on Hist suffix appearing last,
    and to have the meta names match the field names.
    
    A follow-up change may update the persisted names and the dashboard accordingly;
    this change is only changing the in-code variable names.
    
    Release note: none.
    Epic: none.

--- pkg/ccl/crosscluster/logical/logical_replication_writer_processor.go
+++ pkg/ccl/crosscluster/logical/logical_replication_writer_processor.go
@@ -487,7 +487,7 @@ func (lrw *logicalReplicationWriterProcessor) flushBuffer(
 				batchTime := timeutil.Since(preBatchTime)
 
 				lrw.debug.RecordBatchApplied(batchTime, int64(batchEnd-batchStart))
-				lrw.metrics.BatchHistNanos.RecordValue(batchTime.Nanoseconds())
+				lrw.metrics.ApplyBatchNanosHist.RecordValue(batchTime.Nanoseconds())
 				flushByteSize.Add(int64(batchStats.byteSize))
 			}
 			return nil
@@ -506,14 +506,13 @@ func (lrw *logicalReplicationWriterProcessor) flushBuffer(
 	keyCount, byteCount := int64(len(kvs)), flushByteSize.Load()
 	lrw.debug.RecordFlushComplete(flushTime, keyCount, byteCount)
 
-	lrw.metrics.FlushHistNanos.RecordValue(flushTime)
-	lrw.metrics.FlushRowCountHist.RecordValue(keyCount)
-	lrw.metrics.FlushBytesHist.RecordValue(byteCount)
-	lrw.metrics.IngestedLogicalBytes.Inc(byteCount)
+	lrw.metrics.AppliedRowUpdates.Inc(int64(len(kvs)))
+	lrw.metrics.AppliedLogicalBytes.Inc(byteCount)
+	lrw.metrics.CommitToCommitLatency.RecordValue(timeutil.Since(firstKeyTS).Nanoseconds())
 
-	lrw.metrics.CommitLatency.RecordValue(timeutil.Since(firstKeyTS).Nanoseconds())
-	lrw.metrics.IngestedEvents.Inc(int64(len(kvs)))
-
+	lrw.metrics.StreamBatchNanosHist.RecordValue(flushTime)
+	lrw.metrics.StreamBatchRowsHist.RecordValue(keyCount)
+	lrw.metrics.StreamBatchBytesHist.RecordValue(byteCount)
 	return nil
 }
 
