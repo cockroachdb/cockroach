f5f728486cdbe0d84719ee69bceeeb74b51dbf1a
Renato Costa <renato@cockroachlabs.com>
2024-06-20 18:03:26 +0000

    mixedversion: automatically set build versions for every test
    This allows us to change the `OldestSupportedVersion` constant without
    changing the output of any test.
    
    Epic: none
    
    Release note: None

---
+++ pkg/cmd/roachtest/roachtestutil/mixedversion/main_test.go
@@ -0,0 +1,23 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+package mixedversion
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	reset := setDefaultVersions()
+	code := m.Run()
+	reset()
+
+	os.Exit(code)
+}
