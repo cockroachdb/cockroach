ffb7852d929489bf50a347984132579486851904
Kyle Wong <kyle.wong@cockroachlabs.com>
2024-06-24 16:58:03 -0400

    ui: fix database index usage  page error
    For serverless customers, the database index usage
    page was broken and wouldn't load. This was occuring
    due to the expectation of region data in the
    `AggregationStatistics.stats` field which wasn't
    properly being set.
    
    Now, region data is correctly being set in
    `AggregationStatistics.stats`
    
    Fixes: #126037
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/api/statementsApi.ts
+++ pkg/ui/workspaces/cluster-ui/src/api/statementsApi.ts
@@ -177,6 +177,7 @@ type Statistics = {
   rowsWritten: NumericStat;
   runLat: NumericStat;
   svcLat: NumericStat;
+  regions: string[];
 };
 
 type ExecStats = {
@@ -262,6 +263,7 @@ export function convertStatementRawFormatToAggregatedStatistics(
       run_lat: s.statistics.statistics.runLat,
       service_lat: s.statistics.statistics.svcLat,
       sql_type: s.metadata.stmtType,
+      regions: s.statistics.statistics.regions,
     },
   };
 }
