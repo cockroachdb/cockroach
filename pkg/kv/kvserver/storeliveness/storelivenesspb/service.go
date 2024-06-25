3850480690d5ca192500a6e1b9a3ed23f75b2a3a
Mira Radeva <mira@cockroachlabs.com>
2024-06-25 09:48:35 -0400

    storeliveness: add proto definitions and service
    This commit adds the proto definitions for the Store Liveness messages
    and streaming RPC service, as well as the proto definitions for all
    metadata that needs to be persisted for the correctness of Store
    Liveness across restarts.
    
    Fixes: #125059
    
    Release note: None

---
+++ pkg/kv/kvserver/storeliveness/storelivenesspb/service.go
@@ -0,0 +1,17 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package storelivenesspb
+
+// Epoch is an epoch in the Store Liveness fabric, referencing an uninterrupted
+// period of support from one store to another. A store can unilaterally
+// increment the epoch for which it requests support from another store (e.g.
+// after a restart).
+type Epoch int64
