54185ab5b365c330ef14fe8254859eed881446fe
Miles Frankel <miles.frankel@cockroachlabs.com>
2024-06-25 14:21:16 -0400

    hack tests

--- pkg/ccl/changefeedccl/sink.go
+++ pkg/ccl/changefeedccl/sink.go
@@ -188,7 +188,8 @@ var KafkaV2Enabled = settings.RegisterBoolSetting(
 	"changefeed.new_kafka_sink_enabled",
 	"if enabled, this setting enables a new implementation of the kafka sink with improved reliability",
 	// TODO: delete the original kafka sink code
-	metamorphic.ConstantWithTestBool("changefeed.new_kafka_sink.enabled", false),
+	// TODO: set this metamorphic-false before finalizing pr
+	true, // metamorphic.ConstantWithTestBool("changefeed.new_kafka_sink.enabled", false),
 	settings.WithName("changefeed.new_kafka_sink.enabled"),
 )
 
