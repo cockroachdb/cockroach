9867be37b649d6c2036f0ce0425a289b0a0ec754
Anne Zhu <anne.zhu@cockroachlabs.com>
2024-06-25 17:01:01 -0400

    crosscluster: implement dlq client
    In this PR, we define the DLQ client interface and implement
    the MVP version of `DeadLetterQueueClient`. The client gets
    initialized in `logical_replication_writer_processor` and logs
    conflict when an insert/delete query errors out. Currently,
    `DeadLetterQueueClient` does not create a DLQ table and is only
    responsible for logging LWW conflict info. In later iterations,
    we should be appending the LWW conflict logs as rows to the DLQ
    table.
    
    Part of: https://cockroachlabs.atlassian.net/browse/DOC-10483
    
    Epic: CRDB-38992
    
    Release note: None

---
+++ pkg/ccl/crosscluster/logical/dead_letter_queue_test.go
@@ -0,0 +1,84 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package logical
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/ccl/changefeedccl/cdcevent"
+	"github.com/cockroachdb/cockroach/pkg/ccl/changefeedccl/cdctest"
+	"github.com/cockroachdb/cockroach/pkg/repstream/streampb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoggingDLQClient(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	ctx := context.Background()
+
+	srv, db, _ := serverutils.StartServer(t, base.TestServerArgs{})
+	defer srv.Stopper().Stop(context.Background())
+	s := srv.ApplicationLayer()
+
+	sqlDB := sqlutils.MakeSQLRunner(db)
+	sqlDB.Exec(t, `CREATE TABLE foo (a INT)`)
+
+	tableDesc := cdctest.GetHydratedTableDescriptor(t, s.ExecutorConfig(), "foo")
+	familyDesc := &descpb.ColumnFamilyDescriptor{
+		ID:   descpb.FamilyID(1),
+		Name: "",
+	}
+
+	ed, err := cdcevent.NewEventDescriptor(tableDesc, familyDesc, false, false, hlc.Timestamp{})
+	require.NoError(t, err)
+
+	dlqClient := InitDeadLetterQueueClient()
+	require.NoError(t, dlqClient.Create())
+
+	type testCase struct {
+		name           string
+		expectedErrMsg string
+
+		jobID          int64
+		kv             streampb.StreamEvent_KV
+		cdcEventRow    cdcevent.Row
+		writeTimestamp hlc.Timestamp
+		conflictReason string
+	}
+
+	testCases := []testCase{
+		{
+			name:        "log conflict for query",
+			cdcEventRow: cdcevent.Row{EventDescriptor: ed},
+		},
+		{
+			name:           "expect error when given nil cdcEventRow",
+			expectedErrMsg: "cdc event row not initialized",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := dlqClient.Log(ctx, tc.jobID, tc.kv, tc.cdcEventRow, tc.writeTimestamp, tc.conflictReason)
+			if tc.expectedErrMsg == "" {
+				require.NoError(t, err)
+			} else {
+				require.ErrorContains(t, err, tc.expectedErrMsg)
+			}
+		})
+	}
+}
