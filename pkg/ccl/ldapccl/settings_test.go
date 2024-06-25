c888ed65ed36ce157108bb8cbffa27c864c39858
Sourav Sarangi <sourav.sarangi@cockroachlabs.com>
2024-06-25 15:50:47 +0530

    pgwire, ccl: add support for LDAP server authentication
    informs #125087
    fixes #124307
    fixes #125076
    fixes #125080
    Epic CRDB-33829
    
    Release note(enterprise, security): We will be adding a new authentication
    mechanism `authLDAP` to connect to AD servers over LDAPs. Example hba conf entry
    for this auth method:
    ```
     # TYPE    DATABASE      USER           ADDRESS             METHOD             OPTIONS
     # Allow all users to connect to using LDAP authentication with search and bind
       host    all           all            all                 ldap               ldapserver=ldap.example.com ldapport=636 "ldapbasedn=ou=users,dc=example,dc=com" "ldapbinddn=cn=readonly,dc=example,dc=com" ldapbindpasswd=readonly_password ldapsearchattribute=uid "ldapsearchfilter=(memberof=cn=cockroachdb_users,ou=groups,dc=example,dc=com)"
     # Fallback to password authentication for the root user
       host    all           root           0.0.0.0/0          password
    ```
    Example to use for azure AD server:
    ```
    SET cluster setting server.host_based_authentication.configuration = 'host    all           all            all                 ldap ldapserver=azure.dev ldapport=636 "ldapbasedn=OU=AADDC Users,DC=azure,DC=dev" "ldapbinddn=CN=Some User,OU=AADDC Users,DC=azure,DC=dev" ldapbindpasswd=my_pwd ldapsearchattribute=sAMAccountName "ldapsearchfilter=(memberOf=CN=azure-dev-domain-sync-users,OU=AADDC Users,DC=crlcloud,DC=dev)"
    host    all           root           0.0.0.0/0          password';
    ```
    
    We also add the following cluster settings:
    1. `server.ldap_authentication.domain_ca` to allow operators to set a custom CA
    for their domain (i.e. `example.com`).
    2. `server.ldap_authentication.client.tls_certificate` and
    `server.ldap_authentication.client.tls_key` to allow operators to set the client
    certificate and key for establishing mTLS connection with LDAP server.
    
    Post configuration users should be able to authenticate to LDAP server if:
    1. The distinguished name corresponding to their sql username exists (we search
    for the sql username using `ldapbinddn` and `ldapbindpasswd` in the `ldapbasedn`
    domain with filter set to `ldapsearchfilter` and `ldapsearchattribute` key
    having value sql username in db connection string) and we retrieve the DN.
    2. Their bind attempt is successful with LDAP server using the retrieved DN and
    provided ldap password in db connection string.
    
    Example DB client sql login commands. Note `LDAP_SEARCH_VAL` and `SQL_USERNAME`
    are same. In case of azure `LDAP_SEARCH_VAL` will be value for `sAMAccountName`:
    1.
    ```
    cockroach sql --url "postgresql://{LDAP_SEARCH_VAL}:{LDAP_PASSWORD}@{CLUSTER_HOST}:26257" --certs-dir={CLUSTER_CERT_DIR}
    ```
    2.
    ```
    export PGPASSWORD='LDAP_PASSWORD'
    cockroach sql --certs-dir=certs --url "postgresql://{LDAP_SEARCH_VAL}@{CLUSTER_HOST}:26257"
    ```

---
+++ pkg/ccl/ldapccl/settings_test.go
@@ -0,0 +1,110 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package ldapccl
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/security/certnames"
+	"github.com/cockroachdb/cockroach/pkg/security/securityassets"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidateLDAPDomainCACertificate(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	caCertPEMBlock, err := securityassets.GetLoader().ReadFile(
+		filepath.Join(certnames.EmbeddedCertsDir, certnames.EmbeddedCACert))
+	require.NoError(t, err)
+
+	tests := []struct {
+		name        string
+		setting     string
+		wantErr     bool
+		expectedErr string
+	}{
+		{"empty string", "", false, ""},
+		{"valid CA certificate", string(caCertPEMBlock), false, ""},
+		{"invalid CA certificate", "--BEGIN INVALID CERT `[]!@#%#^$&*", true,
+			"LDAP initialization failed: invalid cert PEM block provided"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := validateCertificate(nil, tt.setting); (err != nil) != tt.wantErr {
+				t.Errorf("validateLDAPDomainCACertificate() error = %v, wantErr %v", err, tt.wantErr)
+			} else if err != nil {
+				require.Regexp(t, tt.expectedErr, err.Error())
+			}
+		})
+	}
+}
+
+func TestValidateLDAPClientCertificate(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	clientCertPEMBlock, err := securityassets.GetLoader().ReadFile(
+		filepath.Join(certnames.EmbeddedCertsDir, certnames.EmbeddedRootCert))
+	require.NoError(t, err)
+
+	tests := []struct {
+		name        string
+		setting     string
+		wantErr     bool
+		expectedErr string
+	}{
+		{"empty string", "", false, ""},
+		{"valid client certificate", string(clientCertPEMBlock), false, ""},
+		{"invalid client certificate", "--BEGIN INVALID CERT `[]!@#%#^$&*", true,
+			"LDAP initialization failed: invalid cert PEM block provided"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := validateCertificate(nil, tt.setting); (err != nil) != tt.wantErr {
+				t.Errorf("validateCertificate() error = %v, wantErr %v", err, tt.wantErr)
+			} else if err != nil {
+				require.Regexp(t, tt.expectedErr, err.Error())
+			}
+		})
+	}
+}
+
+func TestValidateLDAPClientPrivateKey(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	clientPrivateKeyPEMBlock, err := securityassets.GetLoader().ReadFile(
+		filepath.Join(certnames.EmbeddedCertsDir, certnames.EmbeddedRootKey))
+	require.NoError(t, err)
+
+	tests := []struct {
+		name        string
+		setting     string
+		wantErr     bool
+		expectedErr string
+	}{
+		{"empty string", "", false, ""},
+		{"valid client private key", string(clientPrivateKeyPEMBlock), false, ""},
+		{"invalid private key certificate", "---BEGIN INVALID RSA PRIVATE KEY `[]!@#%#^$&*", true,
+			"LDAP initialization failed: invalid private key PEM provided"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := validatePrivateKey(nil, tt.setting); (err != nil) != tt.wantErr {
+				t.Errorf("validatePrivateKey() error = %v, wantErr %v", err, tt.wantErr)
+			} else if err != nil {
+				require.Regexp(t, tt.expectedErr, err.Error())
+			}
+		})
+	}
+}
