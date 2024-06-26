7f4b31a9e753581d77e2b2a1d4bf9fe921679083
Annie Pompa <annie@cockroachlabs.com>
2024-06-26 18:53:49 -0400

    sql: override StmtTimeout to 0 for background jobs
    For background jobs and schema changes, our Internal
    Executor inherits from cluster settings. If a statement
    timeout is set cluster-wide, then background jobs will
    respect this -- which does not make much sense; thus,
    this patch overrides the StmtTimeout to 0s for background
    jobs.
    
    Fixes: #126261
    Release note (bug fix): Background jobs no longer respect
    a statement timeout.

---
+++ pkg/sql/tests/job_statement_timeout_test.go
@@ -0,0 +1,71 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package tests_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/jobs"
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBackgroundJobIgnoresStatementTimeout(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	s, sqlDB, _ := serverutils.StartServer(t, base.TestServerArgs{
+		Knobs: base.TestingKnobs{
+			JobsTestingKnobs: &jobs.TestingKnobs{
+				BeforeUpdate: func(orig, updated jobs.JobMetadata) error {
+					if orig.Payload.Type() != jobspb.TypeNewSchemaChange {
+						return nil
+					}
+					time.Sleep(5 * time.Second)
+					return nil
+				},
+			},
+		},
+	})
+	defer s.Stopper().Stop(context.Background())
+	tDB := sqlutils.MakeSQLRunner(sqlDB)
+
+	// Set up
+	tDB.Exec(t, `
+CREATE TABLE t(
+  a INT PRIMARY KEY,
+  b INT
+);
+INSERT INTO t VALUES (1);
+`,
+	)
+
+	var jobCount int
+	// We don't set statement timeout earlier because
+	// migration-job-find-already-completed will fail unless a fairly large
+	// timeout is set.
+	tDB.Exec(t, "SET CLUSTER SETTING sql.defaults.statement_timeout = '3s'")
+	tDB.ExecSucceedsSoon(t, `CREATE UNIQUE INDEX bar ON t (b)`)
+	q := `SELECT COUNT(*) 
+FROM crdb_internal.jobs 
+WHERE job_type = 'NEW SCHEMA CHANGE' AND status = 'succeeded'
+AND description ILIKE 'CREATE UNIQUE INDEX bar%'`
+	tDB.QueryRow(t, q).Scan(&jobCount)
+	// Assert that the job has completed
+	assert.Equal(t, 1, jobCount)
+}
