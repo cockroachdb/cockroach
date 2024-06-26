cockroachdb/cockroach

commit 9e2c09f5014feab28530369ee6170add33243de1

logical: use implicit txns for each INSERT and DELETE
This commit introduces another strategy for processing rows. Previously,
we would handle a batch of rows within an explicit txn, and this commit
makes it so that each row is handled in a separate implicit txn. This
strategy is now used by default and can be disabled via newly-added
`logical_replication.consumer.use_implicit_txns.enabled` cluster
setting. Usage of the implicit txns allows us to hit 1PC down in the KV
layer in some cases.

Release note: None

pkg/ccl/crosscluster/logical/BUILD.bazel
@@ -22,6 +22,7 @@ go_library(
         "//pkg/jobs/jobspb",
         "//pkg/jobs/jobsprofiler",
         "//pkg/keys",
+        "//pkg/kv",
         "//pkg/repstream",
         "//pkg/repstream/streampb",
         "//pkg/roachpb",
@@ -89,6 +90,7 @@ go_test(
         "//pkg/sql/catalog/descpb",
         "//pkg/sql/catalog/desctestutils",
         "//pkg/sql/isql",
+        "//pkg/sql/sessiondata",
         "//pkg/testutils",
         "//pkg/testutils/jobutils",
         "//pkg/testutils/serverutils",

pkg/ccl/crosscluster/logical/logical_replication_writer_processor.go
@@ -21,6 +21,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/repstream/streampb"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descs"
 	"github.com/cockroachdb/cockroach/pkg/sql/execinfra"
@@ -116,13 +117,19 @@ func newLogicalReplicationWriterProcessor(
 
 	bhPool := make([]BatchHandler, maxWriterWorkers)
 	for i := range bhPool {
-		rp, err := makeSQLLastWriteWinsHandler(ctx, flowCtx.Cfg.Settings, spec.TableDescriptors)
+		rp, err := makeSQLLastWriteWinsHandler(
+			ctx, flowCtx.Cfg.Settings, spec.TableDescriptors,
+			// Initialize the executor with the copy of the current session's
+			// variables in order to avoid creating a fresh copy on each usage.
+			flowCtx.Cfg.DB.Executor(isql.WithSessionData(flowCtx.EvalCtx.SessionData().Clone())),
+		)
 		if err != nil {
 			return nil, err
 		}
 		bhPool[i] = &txnBatch{
-			db: flowCtx.Cfg.DB,
-			rp: rp,
+			db:       flowCtx.Cfg.DB,
+			rp:       rp,
+			settings: flowCtx.Cfg.Settings,
 		}
 	}
 
@@ -531,17 +538,28 @@ type BatchHandler interface {
 // RowProcessor knows how to process a single row from an event stream.
 type RowProcessor interface {
 	SetMetrics(metrics *Metrics)
+	// ProcessRow processes a single KV update by inserting or deleting a row.
+	// Txn argument can be nil. The provided value is the "previous value",
+	// before the change was applied on the source.
 	ProcessRow(context.Context, isql.Txn, roachpb.KeyValue, roachpb.Value) error
 }
 
 type txnBatch struct {
-	db descs.DB
-	rp RowProcessor
+	db       descs.DB
+	rp       RowProcessor
+	settings *cluster.Settings
 }
 
 func (t *txnBatch) SetMetrics(metrics *Metrics) {
 	t.rp.SetMetrics(metrics)
 }
+
+var useImplicitTxns = settings.RegisterBoolSetting(
+	settings.ApplicationLevel,
+	"logical_replication.consumer.use_implicit_txns.enabled",
+	"determines whether the consumer processes each row in a separate implicit txn",
+	true,
+)
 
 func (t *txnBatch) HandleBatch(
 	ctx context.Context, batch []streampb.StreamEvent_KV,
@@ -550,24 +568,29 @@ func (t *txnBatch) HandleBatch(
 	defer sp.Finish()
 
 	stats := batchStats{}
-	err := t.db.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
-		// TODO(ssd): For now, we SetOmitInRangefeeds to
-		// prevent the data from being emitted back to the source.
-		// However, I don't think we want to do this in the long run.
-		// Rather, we want to store the inbound cluster ID and store that
-		// in a way that allows us to choose to filter it out from or not.
-		// Doing it this way means that you can't choose to run CDC just from
-		// one side and not the other.
-		txn.KV().SetOmitInRangefeeds()
+	var err error
+	// TODO(yuzefovich): we should have a better heuristic for when to use the
+	// implicit vs explicit txns (for example, depending on the presence of the
+	// secondary indexes).
+	if useImplicitTxns.Get(&t.settings.SV) {
 		for _, kv := range batch {
 			stats.byteSize += kv.Size()
-			if err := t.rp.ProcessRow(ctx, txn, kv.KeyValue, kv.PrevValue); err != nil {
-				return err
+			if err = t.rp.ProcessRow(ctx, nil /* txn */, kv.KeyValue, kv.PrevValue); err != nil {
+				return stats, err
 			}
-
 		}
-		return nil
-	})
+	} else {
+		err = t.db.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+			for _, kv := range batch {
+				stats.byteSize += kv.Size()
+				if err := t.rp.ProcessRow(ctx, txn, kv.KeyValue, kv.PrevValue); err != nil {
+					return err
+				}
+
+			}
+			return nil
+		})
+	}
 	return stats, err
 }
 

pkg/ccl/crosscluster/logical/lww_row_processor.go
@@ -17,6 +17,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/ccl/changefeedccl/changefeedbase"
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
 	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/kv"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
@@ -58,6 +59,7 @@ type sqlLastWriteWinsRowProcessor struct {
 	decoder     cdcevent.Decoder
 	queryBuffer queryBuffer
 	settings    *cluster.Settings
+	ie          isql.Executor
 
 	metrics *Metrics
 
@@ -83,7 +85,10 @@ const (
 )
 
 func makeSQLLastWriteWinsHandler(
-	ctx context.Context, settings *cluster.Settings, tableDescs map[int32]descpb.TableDescriptor,
+	ctx context.Context,
+	settings *cluster.Settings,
+	tableDescs map[int32]descpb.TableDescriptor,
+	ie isql.Executor,
 ) (*sqlLastWriteWinsRowProcessor, error) {
 	descs := make(map[catid.DescID]catalog.TableDescriptor)
 	qb := queryBuffer{
@@ -120,6 +125,7 @@ func makeSQLLastWriteWinsHandler(
 		queryBuffer: qb,
 		decoder:     cdcevent.NewEventDecoderWithCache(ctx, rfCache, false, false),
 		settings:    settings,
+		ie:          ie,
 	}, nil
 }
 
@@ -147,8 +153,15 @@ func (lww *sqlLastWriteWinsRowProcessor) ProcessRow(
 	}
 }
 
-var attributeToUser = sessiondata.InternalExecutorOverride{
+var ieOverrides = sessiondata.InternalExecutorOverride{
 	AttributeToUser: true,
+	// TODO(ssd): we do this for now to prevent the data from being emitted back
+	// to the source. However, I don't think we want to do this in the long run.
+	// Rather, we want to store the inbound cluster ID and store that in a way
+	// that allows us to choose to filter it out from or not. Doing it this way
+	// means that you can't choose to run CDC just from one side and not the
+	// other.
+	DisableChangefeedReplication: true,
 }
 
 var tryOptimisticInsertEnabled = settings.RegisterBoolSetting(
@@ -163,6 +176,10 @@ var tryOptimisticInsertEnabled = settings.RegisterBoolSetting(
 func (lww *sqlLastWriteWinsRowProcessor) insertRow(
 	ctx context.Context, txn isql.Txn, row cdcevent.Row, prevValue roachpb.Value,
 ) error {
+	var kvTxn *kv.Txn
+	if txn != nil {
+		kvTxn = txn.KV()
+	}
 	datums := lww.scratch.datums[:0]
 	err := row.ForAllColumns().Datum(func(d tree.Datum, col cdcevent.ResultColumn) error {
 		if col.Computed {
@@ -200,7 +217,7 @@ func (lww *sqlLastWriteWinsRowProcessor) insertRow(
 		// insert that didn't hit a conflict. In both cases we'll try the
 		// optimistic insert first.
 		query := insertQueries[insertQueriesOptimisticIndex]
-		if _, err = txn.ExecParsed(ctx, "replicated-optimistic-insert", txn.KV(), attributeToUser, query, datums...); err != nil {
+		if _, err = lww.ie.ExecParsed(ctx, "replicated-optimistic-insert", kvTxn, ieOverrides, query, datums...); err != nil {
 			// If the optimistic insert failed with unique violation, we have to
 			// fall back to the pessimistic path. If we got a different error,
 			// then we bail completely.
@@ -215,7 +232,7 @@ func (lww *sqlLastWriteWinsRowProcessor) insertRow(
 		}
 	}
 	query := insertQueries[insertQueriesPessimisticIndex]
-	if _, err = txn.ExecParsed(ctx, "replicated-insert", txn.KV(), attributeToUser, query, datums...); err != nil {
+	if _, err = lww.ie.ExecParsed(ctx, "replicated-insert", kvTxn, ieOverrides, query, datums...); err != nil {
 		log.Warningf(ctx, "replicated insert failed (query: %s): %s", query.SQL, err.Error())
 		return err
 	}
@@ -225,6 +242,10 @@ func (lww *sqlLastWriteWinsRowProcessor) insertRow(
 func (lww *sqlLastWriteWinsRowProcessor) deleteRow(
 	ctx context.Context, txn isql.Txn, row cdcevent.Row,
 ) error {
+	var kvTxn *kv.Txn
+	if txn != nil {
+		kvTxn = txn.KV()
+	}
 	datums := lww.scratch.datums[:0]
 	if err := row.ForEachKeyColumn().Datum(func(d tree.Datum, col cdcevent.ResultColumn) error {
 		datums = append(datums, d)
@@ -236,7 +257,7 @@ func (lww *sqlLastWriteWinsRowProcessor) deleteRow(
 	datums = append(datums, &lww.scratch.ts)
 	lww.scratch.datums = datums[:0]
 	deleteQuery := lww.queryBuffer.deleteQueries[row.TableID]
-	if _, err := txn.ExecParsed(ctx, "replicated-delete", txn.KV(), attributeToUser, deleteQuery, datums...); err != nil {
+	if _, err := lww.ie.ExecParsed(ctx, "replicated-delete", kvTxn, ieOverrides, deleteQuery, datums...); err != nil {
 		log.Warningf(ctx, "replicated delete failed (query: %s): %s", deleteQuery.SQL, err.Error())
 		return err
 	}

pkg/sql/internal.go
@@ -923,6 +923,9 @@ func applyOverrides(o sessiondata.InternalExecutorOverride, sd *sessiondata.Sess
 	if o.OptimizerUseHistograms {
 		sd.OptimizerUseHistograms = true
 	}
+	if o.DisableChangefeedReplication {
+		sd.DisableChangefeedReplication = true
+	}
 
 	if o.MultiOverride != "" {
 		overrides := strings.Split(o.MultiOverride, ",")

pkg/sql/sessiondata/internal.go
@@ -65,6 +65,9 @@ type InternalExecutorOverride struct {
 	// directly result in modification of the SessionData. Instead, it changes
 	// the construction of the connExecutor used for the query.
 	AttributeToUser bool
+	// DisableChangefeedReplication, when true, disables changefeed events from
+	// being emitted for changes to data made in a session.
+	DisableChangefeedReplication bool
 }
 
 // NoSessionDataOverride is the empty InternalExecutorOverride which does not

pkg/ccl/crosscluster/logical/lww_row_processor_test.go
@@ -22,6 +22,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/desctestutils"
 	"github.com/cockroachdb/cockroach/pkg/sql/isql"
+	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
@@ -79,7 +80,7 @@ func TestLWWInsertQueryGeneration(t *testing.T) {
 			desc := desctestutils.TestingGetPublicTableDescriptor(s.DB(), s.Codec(), "defaultdb", tableName)
 			rp, err := makeSQLLastWriteWinsHandler(ctx, s.ClusterSettings(), map[int32]descpb.TableDescriptor{
 				int32(desc.GetID()): *desc.TableDesc(),
-			})
+			}, s.InternalExecutor().(isql.Executor))
 			require.NoError(t, err)
 
 			keyValue := replicationtestutils.EncodeKV(t, s.Codec(), desc, tc.row...)
@@ -93,7 +94,7 @@ func TestLWWInsertQueryGeneration(t *testing.T) {
 			desc := desctestutils.TestingGetPublicTableDescriptor(s.DB(), s.Codec(), "defaultdb", tableName)
 			rp, err := makeSQLLastWriteWinsHandler(ctx, s.ClusterSettings(), map[int32]descpb.TableDescriptor{
 				int32(desc.GetID()): *desc.TableDesc(),
-			})
+			}, s.InternalExecutor().(isql.Executor))
 			require.NoError(t, err)
 
 			keyValue := replicationtestutils.EncodeKV(t, s.Codec(), desc, tc.row...)
@@ -129,7 +130,11 @@ func BenchmarkLastWriteWinsInsert(b *testing.B) {
 					// evaluating INSERT stmts via the internal executor, we'll
 					// inject errors for them so that the processing of the KV
 					// request stops once it reaches the KV client.
-					if !discardWrite.Load() || !ba.IsWrite() || len(ba.Requests) != 1 {
+					//
+					// With explicit txns we expect to have a batch with a
+					// single request, with implicit txns - with two requests
+					// (second being EndTxn).
+					if !discardWrite.Load() || !ba.IsWrite() || len(ba.Requests) > 2 {
 						return nil
 					}
 					switch req := ba.Requests[0].GetInner().(type) {
@@ -161,9 +166,17 @@ func BenchmarkLastWriteWinsInsert(b *testing.B) {
 	runner.Exec(b, "ALTER TABLE tab ADD COLUMN crdb_internal_origin_timestamp DECIMAL NOT VISIBLE DEFAULT NULL ON UPDATE NULL")
 
 	desc := desctestutils.TestingGetPublicTableDescriptor(kvDB, s.Codec(), "defaultdb", tableName)
+	// Simulate the setup of the main code path where we have access to the
+	// session data defaults.
+	var sd *sessiondata.SessionData
+	err := s.InternalDB().(isql.DB).Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+		sd = txn.SessionData().Clone()
+		return nil
+	})
+	require.NoError(b, err)
 	rp, err := makeSQLLastWriteWinsHandler(ctx, s.ClusterSettings(), map[int32]descpb.TableDescriptor{
 		int32(desc.GetID()): *desc.TableDesc(),
-	})
+	}, s.InternalDB().(isql.DB).Executor(isql.WithSessionData(sd)))
 	require.NoError(b, err)
 
 	// We'll be simulating processing the same INSERT over and over in the loop.
@@ -171,33 +184,49 @@ func BenchmarkLastWriteWinsInsert(b *testing.B) {
 	keyValue.Value.Timestamp = hlc.Timestamp{WallTime: timeutil.Now().UnixNano()}
 	key = keyValue.Key
 
-	for _, tc := range []struct {
-		name      string
-		prevValue roachpb.Value
-	}{
-		{name: "noPrevValue"},
-		{name: "withPrevValue", prevValue: keyValue.Value},
-	} {
-		b.Run(tc.name, func(b *testing.B) {
-			b.ReportAllocs()
-			numInjectedErrors.Store(0)
-			defer discardWrite.Store(false)
-			discardWrite.Store(true)
-			var lastTxnErr, lastRowErr error
-			var i int
-			for i = 0; i < b.N; i++ {
-				// We need to start a fresh Txn since we're injecting an error.
-				lastTxnErr = s.InternalDB().(isql.DB).Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
-					// The error is expected here due to injected error.
-					lastRowErr = rp.ProcessRow(ctx, txn, keyValue, tc.prevValue)
-					keyValue.Value.Timestamp.WallTime += 1
-					return nil
-				})
-			}
-			require.Error(b, lastTxnErr)
-			require.Error(b, lastRowErr)
-			// Sanity check that an error was injected on each iteration.
-			require.Equal(b, numInjectedErrors.Load(), int64(i))
-		})
+	for _, implicitTxn := range []bool{false, true} {
+		namePrefix := "explicitTxn"
+		if implicitTxn {
+			namePrefix = "implicitTxn"
+		}
+		for _, tc := range []struct {
+			name      string
+			prevValue roachpb.Value
+		}{
+			{name: "noPrevValue"},
+			{name: "withPrevValue", prevValue: keyValue.Value},
+		} {
+			b.Run(namePrefix+"/"+tc.name, func(b *testing.B) {
+				b.ReportAllocs()
+				numInjectedErrors.Store(0)
+				defer discardWrite.Store(false)
+				discardWrite.Store(true)
+				var lastRowErr error
+				var i int
+				if implicitTxn {
+					for i = 0; i < b.N; i++ {
+						// The error is expected here due to injected error.
+						lastRowErr = rp.ProcessRow(ctx, nil /* txn */, keyValue, tc.prevValue)
+						keyValue.Value.Timestamp.WallTime += 1
+					}
+				} else {
+					var lastTxnErr error
+					for i = 0; i < b.N; i++ {
+						// We need to start a fresh Txn since we're injecting an
+						// error.
+						lastTxnErr = s.InternalDB().(isql.DB).Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+							// The error is expected here due to injected error.
+							lastRowErr = rp.ProcessRow(ctx, txn, keyValue, tc.prevValue)
+							keyValue.Value.Timestamp.WallTime += 1
+							return nil
+						})
+					}
+					require.Error(b, lastTxnErr)
+				}
+				require.Error(b, lastRowErr)
+				// Sanity check that an error was injected on each iteration.
+				require.Equal(b, numInjectedErrors.Load(), int64(i))
+			})
+		}
 	}
 }

commit c333b60b6d22ef8ebfdec2b87852174ead493b91

crosscluster/logical: omit replicated changes with explicit txns
This commit fixes a regression that was introduced in
9e2c09f5014feab28530369ee6170add33243de1. Namely, in that commit we made
it so that we would rely on the DisableChangefeedReplication session
override option of the internal executor for both implicit and explicit
txns. The problem is that this session variable applies only to _new_
txns, so it wasn't doing anything in the explicit txn case. As a result,
we were including the replicated changes into the rangefeed which would
have to be replicated on the source of the original change (if we had
bi-directional replication set up). This commit fixes this by bringing
back setting the appropriate option directly on the txn in the explicit
txn case. It also adds some protections from running into this problem
in the future.

Release note: None

pkg/ccl/crosscluster/logical/BUILD.bazel
@@ -83,6 +83,8 @@ go_test(
         "//pkg/ccl/storageccl",
         "//pkg/jobs",
         "//pkg/jobs/jobspb",
+        "//pkg/kv/kvpb",
+        "//pkg/kv/kvserver",
         "//pkg/roachpb",
         "//pkg/security/securityassets",
         "//pkg/security/securitytest",
@@ -91,6 +93,7 @@ go_test(
         "//pkg/sql/catalog/descpb",
         "//pkg/sql/catalog/desctestutils",
         "//pkg/sql/isql",
+        "//pkg/sql/rowenc",
         "//pkg/sql/sessiondata",
         "//pkg/testutils",
         "//pkg/testutils/jobutils",

pkg/ccl/crosscluster/logical/logical_replication_writer_processor.go
@@ -34,6 +34,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/metamorphic"
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
 	"github.com/cockroachdb/cockroach/pkg/util/span"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
@@ -558,7 +559,7 @@ var useImplicitTxns = settings.RegisterBoolSetting(
 	settings.ApplicationLevel,
 	"logical_replication.consumer.use_implicit_txns.enabled",
 	"determines whether the consumer processes each row in a separate implicit txn",
-	true,
+	metamorphic.ConstantWithTestBool("logical_replication.consumer.use_implicit_txns.enabled", true),
 )
 
 func (t *txnBatch) HandleBatch(
@@ -583,6 +584,17 @@ func (t *txnBatch) HandleBatch(
 	} else {
 		var txnStats batchStats
 		err = t.db.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+			// Note that we cannot use the DisableChangefeedReplication override
+			// option in LWW row processor because it only affects new txns, and
+			// we already have one.
+			// TODO(ssd): For now, we SetOmitInRangefeeds to
+			// prevent the data from being emitted back to the source.
+			// However, I don't think we want to do this in the long run.
+			// Rather, we want to store the inbound cluster ID and store that
+			// in a way that allows us to choose to filter it out from or not.
+			// Doing it this way means that you can't choose to run CDC just from
+			// one side and not the other.
+			txn.KV().SetOmitInRangefeeds()
 			txnStats = batchStats{}
 			for _, kv := range batch {
 				rowStats, err := t.rp.ProcessRow(ctx, txn, kv.KeyValue, kv.PrevValue)

pkg/ccl/crosscluster/logical/lww_row_processor.go
@@ -150,16 +150,21 @@ func (lww *sqlLastWriteWinsRowProcessor) ProcessRow(
 	return stats, err
 }
 
-var ieOverrides = sessiondata.InternalExecutorOverride{
-	AttributeToUser: true,
-	// TODO(ssd): we do this for now to prevent the data from being emitted back
-	// to the source. However, I don't think we want to do this in the long run.
-	// Rather, we want to store the inbound cluster ID and store that in a way
-	// that allows us to choose to filter it out from or not. Doing it this way
-	// means that you can't choose to run CDC just from one side and not the
-	// other.
-	DisableChangefeedReplication: true,
-}
+var (
+	implicitTxnOverrides = sessiondata.InternalExecutorOverride{
+		AttributeToUser: true,
+		// TODO(ssd): we do this for now to prevent the data from being emitted back
+		// to the source. However, I don't think we want to do this in the long run.
+		// Rather, we want to store the inbound cluster ID and store that in a way
+		// that allows us to choose to filter it out from or not. Doing it this way
+		// means that you can't choose to run CDC just from one side and not the
+		// other.
+		DisableChangefeedReplication: true,
+	}
+	explicitTxnOverrides = sessiondata.InternalExecutorOverride{
+		AttributeToUser: true,
+	}
+)
 
 var tryOptimisticInsertEnabled = settings.RegisterBoolSetting(
 	settings.ApplicationLevel,
@@ -174,8 +179,10 @@ func (lww *sqlLastWriteWinsRowProcessor) insertRow(
 	ctx context.Context, txn isql.Txn, row cdcevent.Row, prevValue roachpb.Value,
 ) (batchStats, error) {
 	var kvTxn *kv.Txn
+	ieOverrides := implicitTxnOverrides
 	if txn != nil {
 		kvTxn = txn.KV()
+		ieOverrides = explicitTxnOverrides
 	}
 	datums := lww.scratch.datums[:0]
 	err := row.ForAllColumns().Datum(func(d tree.Datum, col cdcevent.ResultColumn) error {
@@ -241,8 +248,10 @@ func (lww *sqlLastWriteWinsRowProcessor) deleteRow(
 	ctx context.Context, txn isql.Txn, row cdcevent.Row,
 ) (batchStats, error) {
 	var kvTxn *kv.Txn
+	ieOverrides := implicitTxnOverrides
 	if txn != nil {
 		kvTxn = txn.KV()
+		ieOverrides = explicitTxnOverrides
 	}
 	datums := lww.scratch.datums[:0]
 	if err := row.ForEachKeyColumn().Datum(func(d tree.Datum, col cdcevent.ResultColumn) error {

pkg/sql/internal.go
@@ -1145,6 +1145,9 @@ func (ie *InternalExecutor) execInternal(
 			sessionDataOverride.MultiOverride = globalOverride
 		}
 	}
+	if txn != nil && sessionDataOverride.DisableChangefeedReplication {
+		return nil, errors.AssertionFailedf("DisableChangefeedReplication session override cannot apply with non-nil txn")
+	}
 
 	applyInternalExecutorSessionExceptions(sd)
 	applyOverrides(sessionDataOverride, sd)

pkg/sql/sessiondata/internal.go
@@ -66,7 +66,8 @@ type InternalExecutorOverride struct {
 	// the construction of the connExecutor used for the query.
 	AttributeToUser bool
 	// DisableChangefeedReplication, when true, disables changefeed events from
-	// being emitted for changes to data made in a session.
+	// being emitted for changes to data made in a session. It is illegal to set
+	// this option when using the internal executor with an outer txn.
 	DisableChangefeedReplication bool
 }
 

pkg/sql/sessiondatapb/local_only_session_data.proto
@@ -461,7 +461,8 @@ message LocalOnlySessionData {
   // fixes a bug that incorrectly truncated the provided ordering (see #113072).
   bool optimizer_use_provided_ordering_fix = 115;
   // DisableChangefeedReplication, when true, disables changefeed events from
-  // being emitted for changes to data made in a session.
+  // being emitted for changes to data made in a session. This will apply only
+  // to new txns created in the session.
   bool disable_changefeed_replication = 116;
   // CopyTxnQualityOfService indicates the default QoSLevel/WorkPriority of the
   // transactions used to evaluate COPY commands.

pkg/ccl/crosscluster/logical/logical_replication_job_test.go
@@ -9,15 +9,21 @@
 package logical
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"net/url"
+	"sync/atomic"
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/jobs"
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver"
 	"github.com/cockroachdb/cockroach/pkg/security/username"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/desctestutils"
+	"github.com/cockroachdb/cockroach/pkg/sql/rowenc"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/jobutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
@@ -51,25 +57,51 @@ func TestLogicalStreamIngestionJob(t *testing.T) {
 	defer log.Scope(t).Close(t)
 
 	ctx := context.Background()
+	// keyPrefix will be set later, but before countPuts is set.
+	var keyPrefix []byte
+	var countPuts atomic.Bool
+	var numPuts, numCPuts atomic.Int64
 	clusterArgs := base.TestClusterArgs{
 		ServerArgs: base.TestServerArgs{
 			DefaultTestTenant: base.TestControlsTenantsExplicitly,
 			Knobs: base.TestingKnobs{
 				JobsTestingKnobs: jobs.NewTestingKnobsWithShortIntervals(),
+				Store: &kvserver.StoreTestingKnobs{
+					TestingRequestFilter: func(_ context.Context, ba *kvpb.BatchRequest) *kvpb.Error {
+						if !countPuts.Load() || !ba.IsWrite() || len(ba.Requests) > 2 {
+							return nil
+						}
+						switch req := ba.Requests[0].GetInner().(type) {
+						case *kvpb.PutRequest:
+							if bytes.HasPrefix(req.Key, keyPrefix) {
+								numPuts.Add(1)
+							}
+							return nil
+						case *kvpb.ConditionalPutRequest:
+							if bytes.HasPrefix(req.Key, keyPrefix) {
+								numCPuts.Add(1)
+							}
+							return nil
+						default:
+							return nil
+						}
+					},
+				},
 			},
 		},
 	}
 
 	server := testcluster.StartTestCluster(t, 1, clusterArgs)
 	defer server.Stopper().Stop(ctx)
+	s := server.Server(0).ApplicationLayer()
 
 	_, err := server.Conns[0].Exec("CREATE DATABASE a")
 	require.NoError(t, err)
 	_, err = server.Conns[0].Exec("CREATE DATABASE B")
 	require.NoError(t, err)
 
-	dbA := sqlutils.MakeSQLRunner(server.Server(0).ApplicationLayer().SQLConn(t, serverutils.DBName("a")))
-	dbB := sqlutils.MakeSQLRunner(server.Server(0).ApplicationLayer().SQLConn(t, serverutils.DBName("b")))
+	dbA := sqlutils.MakeSQLRunner(s.SQLConn(t, serverutils.DBName("a")))
+	dbB := sqlutils.MakeSQLRunner(s.SQLConn(t, serverutils.DBName("b")))
 
 	for _, s := range testClusterSettings {
 		dbA.Exec(t, s)
@@ -81,13 +113,17 @@ func TestLogicalStreamIngestionJob(t *testing.T) {
 	dbA.Exec(t, lwwColumnAdd)
 	dbB.Exec(t, lwwColumnAdd)
 
+	desc := desctestutils.TestingGetPublicTableDescriptor(s.DB(), s.Codec(), "a", "tab")
+	keyPrefix = rowenc.MakeIndexKeyPrefix(s.Codec(), desc.GetID(), desc.GetPrimaryIndexID())
+	countPuts.Store(true)
+
 	dbA.Exec(t, "INSERT INTO tab VALUES (1, 'hello')")
 	dbB.Exec(t, "INSERT INTO tab VALUES (1, 'goodbye')")
 
-	dbAURL, cleanup := sqlutils.PGUrl(t, server.Server(0).ApplicationLayer().SQLAddr(), t.Name(), url.User(username.RootUser))
+	dbAURL, cleanup := sqlutils.PGUrl(t, s.SQLAddr(), t.Name(), url.User(username.RootUser))
 	dbAURL.Path = "a"
 	defer cleanup()
-	dbBURL, cleanupB := sqlutils.PGUrl(t, server.Server(0).ApplicationLayer().SQLAddr(), t.Name(), url.User(username.RootUser))
+	dbBURL, cleanupB := sqlutils.PGUrl(t, s.SQLAddr(), t.Name(), url.User(username.RootUser))
 	defer cleanupB()
 	dbBURL.Path = "b"
 
@@ -120,6 +156,19 @@ func TestLogicalStreamIngestionJob(t *testing.T) {
 	}
 	dbA.CheckQueryResults(t, "SELECT * from a.tab", expectedRows)
 	dbB.CheckQueryResults(t, "SELECT * from b.tab", expectedRows)
+
+	// Verify that we didn't have the data looping problem. We expect 3 CPuts
+	// with writes directly against the DB and 3 Puts with replicated writes.
+	expPuts, expCPuts := 3, 3
+	if tryOptimisticInsertEnabled.Get(&s.ClusterSettings().SV) {
+		// We have 1 conflict, so if we're using the optimistic insert strategy,
+		// it would result in an additional CPut (that ultimately fails). The
+		// cluster setting is randomized in tests, so we need to handle both
+		// cases.
+		expCPuts++
+	}
+	require.Equal(t, int64(expPuts), numPuts.Load())
+	require.Equal(t, int64(expCPuts), numCPuts.Load())
 }
 
 func TestLogicalStreamIngestionJobWithColumnFamilies(t *testing.T) {