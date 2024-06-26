cc8a14ca72b94d3fa04350d642f5b1e79340ca7f
David Taylor <tinystatemachine@gmail.com>
2024-06-26 19:38:56 +0000

    jobsprofiler: calculate diag before txn
    Release note: none.
    Epic: none.

--- pkg/jobs/jobsprofiler/profiler.go
+++ pkg/jobs/jobsprofiler/profiler.go
@@ -45,14 +45,15 @@ func StorePlanDiagram(
 		ctx, cancel = stopper.WithCancelOnQuiesce(ctx)
 		defer cancel()
 
-		err := db.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
-			flowSpecs := p.GenerateFlowSpecs()
-			_, diagURL, err := execinfrapb.GeneratePlanDiagramURL(fmt.Sprintf("job:%d", jobID), flowSpecs,
-				execinfrapb.DiagramFlags{})
-			if err != nil {
-				return err
-			}
-
+		flowSpecs := p.GenerateFlowSpecs()
+		_, diagURL, err := execinfrapb.GeneratePlanDiagramURL(fmt.Sprintf("job:%d", jobID), flowSpecs,
+			execinfrapb.DiagramFlags{})
+		if err != nil {
+			log.Warningf(ctx, "plan diagram failed to generate: %v", err)
+			return
+		}
+		diagString := diagURL.String()
+		if err := db.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
 			dspKey := profilerconstants.MakeDSPDiagramInfoKey(timeutil.Now().UnixNano())
 			infoStorage := jobs.InfoStorageForJob(txn, jobID)
 
@@ -71,14 +72,9 @@ func StorePlanDiagram(
 			}
 
 			// Write the new diagram.
-			return infoStorage.Write(ctx, dspKey, []byte(diagURL.String()))
-		})
-		// Don't log the error if the context has been canceled. This will likely be
-		// when the node is shutting down and so it doesn't add value to spam the
-		// logs with the error.
-		if err != nil && ctx.Err() == nil {
-			log.Warningf(ctx, "failed to generate and write DistSQL diagram for job %d: %v",
-				jobID, err.Error())
+			return infoStorage.Write(ctx, dspKey, []byte(diagString))
+		}); err != nil && ctx.Err() == nil {
+			log.Warningf(ctx, "failed to write DistSQL diagram for job %d: %v", jobID, err.Error())
 		}
 	}); err != nil {
 		log.Warningf(ctx, "failed to spawn task to generate DistSQL plan diagram for job %d: %v",
