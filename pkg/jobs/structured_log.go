c2bad2909dab65dd66e8c99aded0b627b7117ef4
Kyle Wong <kyle.wong@cockroachlabs.com>
2024-06-28 16:21:22 -0400

    log,job: log status change on update via updater
    Adds a new `status_change` job event that logs
    on every status update.
    
    Epic: CRDB-37556
    
    Release note (general change): Job status changes
    now log events to the OPS channel, indicating the
    previous status and the new status.

---
+++ pkg/jobs/structured_log.go
@@ -0,0 +1,58 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package jobs
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/log/eventpb"
+	"github.com/cockroachdb/cockroach/pkg/util/log/severity"
+	"github.com/cockroachdb/redact"
+)
+
+// LogStatusChangeStructured job state change using structured logging, if job is not nil.
+func LogStatusChangeStructured(
+	ctx context.Context,
+	id jobspb.JobID,
+	jobType string,
+	payload *jobspb.Payload,
+	runStats *RunStats,
+	prevStatus Status,
+	status Status,
+) {
+	var runNum int
+	if runStats != nil {
+		runNum = runStats.NumRuns
+	}
+
+	out := eventpb.StatusChange{
+		JobID:          int64(id),
+		JobType:        jobType,
+		Description:    redact.Sprintf("status changed to: %s", status),
+		PreviousStatus: string(prevStatus),
+		NewStatus:      string(status),
+		RunNum:         int32(runNum),
+	}
+
+	if payload != nil {
+		if payload.FinalResumeError != nil {
+			out.FinalResumeErr = payload.FinalResumeError.String()
+		}
+
+		if payload.Error != "" {
+			out.Error = payload.Error
+		}
+	}
+
+	log.StructuredEvent(ctx, &out, severity.INFO, 1)
+}
