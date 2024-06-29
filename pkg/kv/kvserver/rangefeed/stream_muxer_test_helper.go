f71b5bf1d6e1d42bf40d612aa0787ad884536f3b
Wenyi Hu <wenyi@cockroachlabs.com>
2024-06-28 21:20:05 -0400

    kvserver/rangefeed: move active streams to stream muxer
    This patch moves the existing active stream management to StreamMuxer
    without changing any existing behaviour. The main purpose is to make future
    commits cleaner.
    
    Part of: #126561
    Release note: none

---
+++ pkg/kv/kvserver/rangefeed/stream_muxer_test_helper.go
@@ -0,0 +1,90 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package rangefeed
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"sync"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+)
+
+// testServerStream mocks grpc server stream for testing.
+type testServerStream struct {
+	syncutil.Mutex
+	// eventsSent is the total number of events sent.
+	eventsSent int
+	// streamEvents is a map of streamID to a list of events sent to that stream.
+	streamEvents map[int64][]*kvpb.MuxRangeFeedEvent
+}
+
+func newTestServerStream() *testServerStream {
+	return &testServerStream{
+		streamEvents: make(map[int64][]*kvpb.MuxRangeFeedEvent),
+	}
+}
+
+func (s *testServerStream) totalEventsSent() int {
+	s.Lock()
+	defer s.Unlock()
+	return s.eventsSent
+}
+
+// hasEvent returns true if the event is found in the streamEvents map. Note
+// that it does a deep equal comparison.
+func (s *testServerStream) hasEvent(e *kvpb.MuxRangeFeedEvent) bool {
+	if e == nil {
+		return false
+	}
+	s.Lock()
+	defer s.Unlock()
+	for _, streamEvent := range s.streamEvents[e.StreamID] {
+		if reflect.DeepEqual(e, streamEvent) {
+			return true
+		}
+	}
+	return false
+}
+
+// String returns a string representation of the events sent in the stream.
+func (s *testServerStream) String() string {
+	str := strings.Builder{}
+	for streamID, eventList := range s.streamEvents {
+		fmt.Fprintf(&str, "StreamID:%d, Len:%d\n", streamID, len(eventList))
+	}
+	return str.String()
+}
+
+func (s *testServerStream) SendIsThreadSafe() {}
+
+// Send mocks grpc.ServerStream Send method. It only counts events and stores
+// events by streamID in streamEvents.
+func (s *testServerStream) Send(e *kvpb.MuxRangeFeedEvent) error {
+	s.Lock()
+	defer s.Unlock()
+	s.eventsSent++
+	s.streamEvents[e.StreamID] = append(s.streamEvents[e.StreamID], e)
+	return nil
+}
+
+// BlockSend blocks any subsequent Send methods until the unblock callback is
+// called.
+func (s *testServerStream) BlockSend() (unblock func()) {
+	s.Lock()
+	var once sync.Once
+	return func() {
+		// safe to call multiple times, e.g. defer and explicit
+		once.Do(s.Unlock) //nolint:deferunlockcheck
+	}
+}
