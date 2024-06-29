ae0b5c9808ea91e4dc782b95614d06cadabb0118
Wenyi Hu <wenyi@cockroachlabs.com>
2024-06-28 21:56:01 -0400

    kvserver/rangefeed: remove unused cancel
    This patch removes the unused field cancel from setRangeIDEventSink.
    
    Part of:
    Epic: none
    Release note: none

--- pkg/server/node.go
+++ pkg/server/node.go
@@ -1832,7 +1832,6 @@ func (n *Node) RangeLookup(
 // calls.
 type setRangeIDEventSink struct {
 	ctx      context.Context
-	cancel   context.CancelFunc
 	rangeID  roachpb.RangeID
 	streamID int64
 	wrapped  *lockedMuxStream
@@ -1912,7 +1911,6 @@ func (n *Node) MuxRangeFeed(stream kvpb.Internal_MuxRangeFeedServer) error {
 
 		streamSink := &setRangeIDEventSink{
 			ctx:      streamCtx,
-			cancel:   cancel,
 			rangeID:  req.RangeID,
 			streamID: req.StreamID,
 			wrapped:  muxStream,
