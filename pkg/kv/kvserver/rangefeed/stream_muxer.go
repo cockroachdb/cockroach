4499e0570b440db9165e25f5353d362b28d81da3
Wenyi Hu <wenyi@cockroachlabs.com>
2024-06-28 20:50:31 -0400

    kvserver/rangefeed: introduce stream muxer
    This patch introduces a new StreamMuxer struct to manage server rangefeed
    streams. Currently, it just takes over the responsibilities as the existing
    rangefeed completion watcher without changing any existing behaviour. In a future
    commit, we will move the rangefeed cleanup process to StreamMuxer as well.
    
    Part of: #126561
    Epic: none
    Release note: none

---
+++ pkg/kv/kvserver/rangefeed/stream_muxer.go
@@ -0,0 +1,103 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package rangefeed
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+)
+
+// Note that Send must be thread safe to be called concurrently.
+type severStreamSender interface {
+	Send(*kvpb.MuxRangeFeedEvent) error
+	SendIsThreadSafe()
+}
+
+// StreamMuxer is responsible for managing a set of active rangefeed streams and
+// forwarding rangefeed completion errors to the client.
+type StreamMuxer struct {
+	// Note that lockedMuxStream wraps the underlying grpc server stream, ensuring
+	// thread safety.
+	sender  severStreamSender
+
+	// notifyMuxError is a buffered channel of size 1 used to signal the presence
+	// of muxErrors. Additional signals are dropped if the channel is already full
+	// so that it's unblocking.
+	notifyMuxError chan struct{}
+
+	mu struct {
+		syncutil.Mutex
+		muxErrors []*kvpb.MuxRangeFeedEvent
+	}
+}
+
+func NewStreamMuxer(sender severStreamSender) *StreamMuxer {
+	return &StreamMuxer{
+		sender:         sender,
+		notifyMuxError: make(chan struct{}, 1),
+	}
+}
+
+func (sm *StreamMuxer) AppendMuxError(e *kvpb.MuxRangeFeedEvent) {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	sm.mu.muxErrors = append(sm.mu.muxErrors, e)
+	// Note that notify is unblocking.
+	select {
+	case sm.notifyMuxError <- struct{}{}:
+	default:
+	}
+}
+
+func (sm *StreamMuxer) detachMuxErrors() []*kvpb.MuxRangeFeedEvent {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	toSend := sm.mu.muxErrors
+	sm.mu.muxErrors = nil
+	return toSend
+}
+
+// Run forwards rangefeed errors.
+//
+// Run is expected to be called in a goroutine and will block until the context
+// is done or the stopper is quiesced. Example usage:
+//
+// muxer := NewStreamMuxer(muxStream, metrics)
+// var wg sync.WaitGroup
+// defer wg.Wait()
+// defer stopper.Stop(ctx) // or defer cancel() - important to stop the muxer before wg.Wait()
+//
+//	wg.Add(1)
+//	if err := stopper.RunAsyncTask(ctx, "mux-term-forwarder", func(ctx context.Context) {
+//		defer wg.Done()
+//		muxer.Run(ctx, stopper)
+//	}); err != nil {
+//		wg.Done()
+//	}
+func (sm *StreamMuxer) Run(ctx context.Context, stopper *stop.Stopper) {
+	for {
+		select {
+		case <-sm.notifyMuxError:
+			for _, clientErr := range sm.detachMuxErrors() {
+				if err := sm.sender.Send(clientErr); err != nil {
+					return
+				}
+			}
+		case <-ctx.Done():
+			return
+		case <-stopper.ShouldQuiesce():
+			return
+		}
+	}
+}
