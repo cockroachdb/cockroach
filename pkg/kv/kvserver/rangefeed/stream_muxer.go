00d08105a94fabc846e4d1c7dc79435c529c6716
Wenyi Hu <wenyi@cockroachlabs.com>
2024-06-28 21:20:05 -0400

    kvserver/rangefeed: move active streams to stream muxer
    This patch moves the existing active stream management to StreamMuxer
    without changing any existing behaviour. The main purpose is to make future
    commits cleaner.
    
    Part of: #126561
    Epic: none
    Release note: none

--- pkg/kv/kvserver/rangefeed/stream_muxer.go
+++ pkg/kv/kvserver/rangefeed/stream_muxer.go
@@ -12,8 +12,11 @@ package rangefeed
 
 import (
 	"context"
+	"sync"
 
 	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 )
@@ -29,7 +32,10 @@ type severStreamSender interface {
 type StreamMuxer struct {
 	// Note that lockedMuxStream wraps the underlying grpc server stream, ensuring
 	// thread safety.
-	sender  severStreamSender
+	sender severStreamSender
+
+	// streamID -> streamInfo for active rangefeeds
+	activeStreams sync.Map
 
 	// notifyMuxError is a buffered channel of size 1 used to signal the presence
 	// of muxErrors. Additional signals are dropped if the channel is already full
@@ -49,14 +55,65 @@ func NewStreamMuxer(sender severStreamSender) *StreamMuxer {
 	}
 }
 
-func (sm *StreamMuxer) AppendMuxError(e *kvpb.MuxRangeFeedEvent) {
+// AddStream registers a server rangefeed stream with the muxer. It remains
+// active until DisconnectRangefeedWithError is called with the same streamID.
+// Caller must ensure no duplicate stream IDs are added without disconnecting
+// the old one first.
+func (sm *StreamMuxer) AddStream(streamID int64, cancel context.CancelFunc) {
+	if _, loaded := sm.activeStreams.LoadOrStore(streamID, cancel); loaded {
+		log.Fatalf(context.Background(), "stream %d already exists", streamID)
+	}
+
+}
+
+// transformRangefeedErrToClientError converts a rangefeed error to a client
+// error to be sent back to client. This also handles nil values, preventing nil
+// pointer dereference.
+func transformRangefeedErrToClientError(err *kvpb.Error) *kvpb.Error {
+	if err == nil {
+		// When processor is stopped when it no longer has any registrations, it
+		// would attempt to close all feeds again with a nil error. Theoretically,
+		// this should never happen as processor would always stop with a reason if
+		// feeds are active.
+		return kvpb.NewError(kvpb.NewRangeFeedRetryError(kvpb.RangeFeedRetryError_REASON_RANGEFEED_CLOSED))
+	}
+	return err
+}
+
+func (sm *StreamMuxer) appendMuxError(e *kvpb.MuxRangeFeedEvent) {
 	sm.mu.Lock()
 	defer sm.mu.Unlock()
 	sm.mu.muxErrors = append(sm.mu.muxErrors, e)
-	// Note that notify is unblocking.
+	// Note that notifyMuxError is non-blocking.
 	select {
 	case sm.notifyMuxError <- struct{}{}:
 	default:
+	}
+}
+
+// DisconnectRangefeedWithError disconnects a stream with an error. Safe to call
+// repeatedly for the same stream, but subsequent errors are ignored. It ensures
+// 1. cancel stream context 2. send error back to client once for the stream.
+//
+// Note that this function can be called by the processor worker while holding
+// raftMu, so it is important that this function doesn't block IO. It does so by
+// delegating the responsibility of sending mux error to muxer.Run.
+func (sm *StreamMuxer) DisconnectRangefeedWithError(
+	streamID int64, rangeID roachpb.RangeID, err *kvpb.Error,
+) {
+	if cancelFunc, ok := sm.activeStreams.LoadAndDelete(streamID); ok {
+		if f, ok := cancelFunc.(context.CancelFunc); ok {
+			f()
+		}
+		clientErrorEvent := transformRangefeedErrToClientError(err)
+		ev := &kvpb.MuxRangeFeedEvent{
+			StreamID: streamID,
+			RangeID:  rangeID,
+		}
+		ev.SetValue(&kvpb.RangeFeedError{
+			Error: *clientErrorEvent,
+		})
+		sm.appendMuxError(ev)
 	}
 }
 
@@ -89,7 +146,8 @@ func (sm *StreamMuxer) Run(ctx context.Context, stopper *stop.Stopper) {
 	for {
 		select {
 		case <-sm.notifyMuxError:
-			for _, clientErr := range sm.detachMuxErrors() {
+			toSend := sm.detachMuxErrors()
+			for _, clientErr := range toSend {
 				if err := sm.sender.Send(clientErr); err != nil {
 					return
 				}
