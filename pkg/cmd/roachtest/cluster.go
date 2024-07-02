a3d75e88bad591e6cf14f7b0ef683a1edcc671f7
Miles Frankel <miles.frankel@cockroachlabs.com>
2024-07-02 13:17:08 -0400

    changefeedccl: add webhook-chaos roachtest
    Add new webhook-chaos roachtest with order validation.
    
    Epic: CRDB-38755
    Part of: #124148
    
    Release note: none

--- pkg/cmd/roachtest/cluster.go
+++ pkg/cmd/roachtest/cluster.go
@@ -1976,6 +1976,41 @@ func (c *clusterImpl) Get(
 	return errors.Wrap(roachprod.Get(ctx, l, c.MakeNodes(opts...), src, dest), "cluster.Get")
 }
 
+// Tail tails files from remote hosts asynchronously.
+func (c *clusterImpl) Tail(
+	ctx context.Context, l *logger.Logger, src string, opts ...option.Option,
+) (stdout io.ReadCloser, stderr io.ReadCloser, errChan chan error, err error) {
+	if ctx.Err() != nil {
+		return nil, nil, nil, errors.Wrap(ctx.Err(), "cluster.Get")
+	}
+	c.status(fmt.Sprintf("tailing %v", src))
+	defer c.status("")
+	errChan = make(chan error, 1)
+	stdoutRead, stdoutWrite := io.Pipe()
+	stderrRead, stderrWrite := io.Pipe()
+	go func() {
+		defer close(errChan)
+		defer func() { _ = stdoutWrite.Close() }()
+		defer func() { _ = stderrWrite.Close() }()
+		var err error
+		defer l.Printf("stopped tailing %v: %v", src, err)
+		err = roachprod.Run(ctx, l, c.MakeNodes(opts...), "", "", false, stdoutWrite, stderrWrite, []string{fmt.Sprintf("tail -F %s", src)}, install.RunOptions{})
+		if err != nil {
+			errChan <- err
+		}
+	}()
+
+	// Close the pipes when the context is canceled.
+	go func() {
+		<-ctx.Done()
+		_ = stdoutRead.Close()
+		_ = stderrRead.Close()
+	}()
+
+	c.status(fmt.Sprintf("spawned tail -F %v", src))
+	return stdoutRead, stderrRead, errChan, nil
+}
+
 // PutString into the specified file on the remote(s).
 func (c *clusterImpl) PutString(
 	ctx context.Context, content, dest string, mode os.FileMode, nodes ...option.Option,
