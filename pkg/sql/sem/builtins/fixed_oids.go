aa8281a463e4e230efe888848024224876af48ff
David Taylor <tinystatemachine@gmail.com>
2024-07-04 20:14:57 +0000

    crosscluster/logical: add ability to inject synthetic application failures
    Release note: none.
    Epic: none.

--- pkg/sql/sem/builtins/fixed_oids.go
+++ pkg/sql/sem/builtins/fixed_oids.go
@@ -2583,6 +2583,7 @@ var builtinOidsArray = []string{
 	2615: `crdb_internal.scatter(key: bytes, end_key: bytes) -> void`,
 	2616: `crdb_internal.plan_logical_replication(req: bytes) -> bytes`,
 	2617: `crdb_internal.start_replication_stream_for_tables(req: bytes) -> bytes`,
+	2618: `crdb_internal.logical_replication_inject_failures(strea: int, proc: int, percent: int) -> void`,
 }
 
 var builtinOidsBySignature map[string]oid.Oid
