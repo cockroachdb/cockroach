3f6e3c18ef20f3a343de7ad85ce29516d9922a24
David Taylor <tinystatemachine@gmail.com>
2024-07-04 19:22:10 +0000

    crosscluster/purgatory: limit size of purgatory on bytes
    Total bytes being stored is what will OOM or not OOM a node, so limit on that, not levels.
    
    Release note: none.
    Epic: none.

--- pkg/ccl/crosscluster/logical/purgatory_test.go
+++ pkg/ccl/crosscluster/logical/purgatory_test.go
@@ -34,18 +34,21 @@ func TestPurgatory(t *testing.T) {
 
 	var flushed, resolved int
 	p := &purgatory{
-		levelLimit: 5,
+		byteLimit: 5 << 20,
 		flush: func(
 			_ context.Context, ev []streampb.StreamEvent_KV, _, _ bool,
-		) ([]streampb.StreamEvent_KV, error) {
+		) ([]streampb.StreamEvent_KV, int64, error) {
+			var unappliedBytes int64
 			for i := range ev {
 				if i%2 == 0 {
 					flushed++
 					t.Logf("flush %s", ev[i].KeyValue.Key)
 					ev[i] = streampb.StreamEvent_KV{}
+				} else {
+					unappliedBytes += int64(ev[i].Size())
 				}
 			}
-			return filterRemaining(ev), nil
+			return filterRemaining(ev), unappliedBytes, nil
 		},
 		checkpoint: func(_ context.Context, sp []jobspb.ResolvedSpan) error {
 			resolved = int(sp[0].Timestamp.WallTime)
@@ -53,36 +56,45 @@ func TestPurgatory(t *testing.T) {
 		},
 	}
 
+	sz := int64((&streampb.StreamEvent_KV{KeyValue: roachpb.KeyValue{Key: roachpb.Key("a")}}).Size())
+	t.Logf("size of one kv: %d", sz)
 	// Adding events makes it non-empty.
 	require.True(t, p.Empty())
-	require.NoError(t, p.Store(ctx, []streampb.StreamEvent_KV{skv("a"), skv("b")}))
-	require.NoError(t, p.Store(ctx, []streampb.StreamEvent_KV{skv("c"), skv("d")}))
+	require.NoError(t, p.Store(ctx, []streampb.StreamEvent_KV{skv("a"), skv("b")}, sz*2))
+	require.Equal(t, sz*2, p.bytes)
+	require.NoError(t, p.Store(ctx, []streampb.StreamEvent_KV{skv("c"), skv("d")}, sz*2))
+	require.Equal(t, sz*4, p.bytes)
 	p.Checkpoint(ctx, ts(1))
 
-	require.NoError(t, p.Store(ctx, []streampb.StreamEvent_KV{skv("e"), skv("f"), skv("g"), skv("h")}))
+	require.NoError(t, p.Store(ctx, []streampb.StreamEvent_KV{skv("e"), skv("f"), skv("g"), skv("h")}, sz*4))
+	require.Equal(t, sz*8, p.bytes)
 	p.Checkpoint(ctx, ts(2))
 
-	require.NoError(t, p.Store(ctx, []streampb.StreamEvent_KV{skv("x")}))
+	require.NoError(t, p.Store(ctx, []streampb.StreamEvent_KV{skv("x")}, sz*1))
 	require.False(t, p.Empty())
+	require.Equal(t, 4, len(p.levels))
+	require.Equal(t, sz*9, p.bytes)
 
 	// Nothing has drained yet, so no movement in flushed or resolved.
 	require.Equal(t, 0, flushed)
 	require.Equal(t, 0, resolved)
-	require.Equal(t, 4, len(p.levels))
 
 	// Draining drains half the events, but no checkpoints yet.
 	require.NoError(t, p.Drain(ctx))
 	require.Equal(t, 5, flushed)
 	require.Equal(t, 0, resolved)
+	require.Equal(t, sz*4, p.bytes)
 
 	// Draining drains half the events, now checkpointing after lvl 2.
 	require.NoError(t, p.Drain(ctx))
 	require.Equal(t, 8, flushed)
 	require.Equal(t, 1, resolved)
+	require.Equal(t, sz*1, p.bytes)
 	require.False(t, p.Empty())
 
 	require.NoError(t, p.Drain(ctx))
 	require.Equal(t, 9, flushed)
 	require.Equal(t, 2, resolved)
+	require.Equal(t, sz*0, p.bytes)
 	require.True(t, p.Empty())
 }
