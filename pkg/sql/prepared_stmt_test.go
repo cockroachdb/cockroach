e86bfd026d9dd94f1f6b406d5be87a521491b995
Marcus Gartner <marcus@cockroachlabs.com>
2024-07-08 16:37:44 -0400

    sql: implement plan_cache_mode=auto
    The `plan_cache_mode=auto` session setting has been implemented which
    instructs the database to pick generic query plans for prepared
    statements when they have a cost less than or equal to custom plans.
    
    The current strategy matches Postgres's strategy quite closely. Under
    `plan_cache_mode=auto`, custom query plans will be generated for the
    first five executions of a prepared statement. On the sixth execution, a
    generic query plan will be generated. If the cost of the generic query
    plan is less than the average cost of the custom plans (plus some
    overhead for re-optimization), then the generic query plan will be used.
    
    Release note (sql change): The session setting `plan_cache_mode=auto`
    can now be used to instruct the system to automatically determine
    whether to use "custom" or "generic" query plans for the execution of a
    prepared statement. Custom query plans are optimized on every execution,
    while generic plans are optimized once and reused on future executions
    as-is. Generic query plans are beneficial in cases where query
    optimization contributes significant overhead to the total cost of
    executing a query.

---
+++ pkg/sql/prepared_stmt_test.go
@@ -0,0 +1,51 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package sql
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/opt/memo"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+)
+
+func TestPlanCosts(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	type testCase struct {
+		input       []memo.Cost
+		expectedNum int
+		expectedAvg memo.Cost
+	}
+	testCases := []testCase{
+		{input: []memo.Cost{}, expectedNum: 0, expectedAvg: 0},
+		{input: []memo.Cost{0, 0}, expectedNum: 2, expectedAvg: 0},
+		{input: []memo.Cost{1, 1}, expectedNum: 2, expectedAvg: 1},
+		{input: []memo.Cost{1, 2, 3, 4, 5}, expectedNum: 5, expectedAvg: 3},
+		{input: []memo.Cost{1, 2, 3, 4, 5, 6}, expectedNum: 5, expectedAvg: 4},
+		{input: []memo.Cost{9, 9, 9, 9, 9, 1, 2, 3, 4, 5}, expectedNum: 5, expectedAvg: 3},
+	}
+	var pc planCosts
+	for _, tc := range testCases {
+		pc.ClearCustom()
+		for _, cost := range tc.input {
+			pc.AddCustom(cost)
+		}
+		if pc.NumCustom() != tc.expectedNum {
+			t.Errorf("expected Len() to be %d, got %d", tc.expectedNum, pc.NumCustom())
+		}
+		if pc.AvgCustom() != tc.expectedAvg {
+			t.Errorf("expected Avg() to be %f, got %f", tc.expectedAvg, pc.AvgCustom())
+		}
+	}
+}
