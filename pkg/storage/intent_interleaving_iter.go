72c424e03374dc915bf83804385b2d323386eb98
sumeerbhola <sumeer@cockroachlabs.com>
2024-07-08 11:33:29 -0400

    storage: remove unnecessarily strict RangeBounds assertion
    pebble.Iterator permits calling `RangeBounds` when there is no range key.
    
    Epic: none
    
    Release note: None

--- pkg/storage/intent_interleaving_iter.go
+++ pkg/storage/intent_interleaving_iter.go
@@ -1049,9 +1049,6 @@ func (i *intentInterleavingIter) HasPointAndRange() (bool, bool) {
 
 // RangeBounds implements SimpleMVCCIterator.
 func (i *intentInterleavingIter) RangeBounds() roachpb.Span {
-	if _, hasRange := i.HasPointAndRange(); !hasRange {
-		return roachpb.Span{}
-	}
 	return i.iter.RangeBounds()
 }
 
