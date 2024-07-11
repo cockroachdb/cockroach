96a63c1323b18ed8eb3bec0d8ebf8efb8ae9fb79
Herko Lategan <herko@cockroachlabs.com>
2024-07-11 17:24:37 +0100

    roachtest: add resize operation
    This change adds a new resize operation that will grow clusters and start
    cockroach on the new nodes. It copies the current binary and libraries from the
    first node, and uses the provided operation config to supply start options and
    cluster settings when starting cockroach on the new nodes.
    
    In the new state the operation will wait for clean up before draining and
    then decommissioning the new nodes.
    
    If the operation succeeded the cleanup function will shrink the cluster back to
    its original size. But if the operation fails, the cleanup function is not
    invoked and the cluster will remain at its new size. This is useful for
    investigating the new nodes for issues, but also should be monitored since if
    the operation is run again it will keep increasing the cluster in size.
    
    Release Note: None
    Epic: None

---
+++ pkg/cmd/roachtest/operations/resize.go
@@ -0,0 +1,108 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package operations
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/cluster"
+	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/operation"
+	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/registry"
+	"github.com/cockroachdb/errors"
+)
+
+type cleanupResize struct {
+	origClusterSize int
+	growCount       int
+}
+
+// Cleanup shrinks the cluster back to its original size.
+func (cl *cleanupResize) Cleanup(ctx context.Context, o operation.Operation, c cluster.Cluster) {
+	dynamicCluster := c.(cluster.DynamicCluster)
+	defer func() {
+		err := dynamicCluster.Shrink(ctx, o.L(), cl.growCount)
+		if err != nil {
+			o.Status(fmt.Sprintf("error shrinking cluster: %s", err))
+		} else {
+			o.Status("shrunk cluster back to original size")
+		}
+	}()
+	for i := 0; i < cl.growCount; i++ {
+		drainNode(ctx, o, c, c.Node(cl.origClusterSize+i+1))
+		decommissionNode(ctx, o, c, c.Node(cl.origClusterSize+i+1))
+	}
+}
+
+// resizeCluster grows the cluster by the specified count, waits for a specified
+// duration, then drains and decommissions the new nodes.
+func resizeCluster(
+	ctx context.Context,
+	o operation.Operation,
+	c cluster.Cluster,
+	growCount int,
+) *cleanupResize {
+	// Create a temporary directory to store the required files for this operation.
+	tmpDir, err := os.MkdirTemp("", "")
+	if err != nil {
+		o.Fatal(err)
+	}
+	defer func() { _ = os.RemoveAll(tmpDir) }()
+	dynamicCluster, ok := c.(cluster.DynamicCluster)
+	if !ok {
+		o.Fatal("cluster does not implement the DynamicCluster interface")
+	}
+
+	// Grab required files from the first node.
+	err = c.Get(ctx, o.L(), "cockroach", path.Join(tmpDir, "cockroach"), c.Node(1))
+	if err != nil {
+		o.Fatal(errors.Wrap(err, "failed to get cockroach, please ensure cockroach is running on this cluster"))
+	}
+	err = c.Get(ctx, o.L(), "lib", path.Join(tmpDir, "lib"), c.Node(1))
+	if err != nil {
+		o.Fatal(errors.Wrap(err, "failed to get cockroach libs"))
+	}
+
+	// Grow the cluster, but keep track of the original cluster size.
+	origClusterSize := c.Spec().NodeCount
+	err = dynamicCluster.Grow(ctx, o.L(), growCount)
+	if err != nil {
+		o.Fatal(err)
+	}
+	newNodes := c.Range(origClusterSize+1, origClusterSize+growCount)
+
+	// Copy the required files to the new nodes.
+	c.Put(ctx, path.Join(tmpDir, "cockroach"), "cockroach", newNodes)
+	c.Put(ctx, path.Join(tmpDir, "lib"), "lib", newNodes)
+
+	// Start the new nodes.
+	startOpts := o.StartOpts()
+	startOpts.RoachprodOpts.IsRestart = true
+	c.Start(ctx, o.L(), startOpts, o.ClusterSettings(), newNodes)
+
+	return &cleanupResize{growCount: growCount, origClusterSize: origClusterSize}
+}
+
+func registerResize(r registry.Registry) {
+	r.AddOperation(registry.OperationSpec{
+		Name:             "resize/grow=3",
+		Owner:            registry.OwnerStorage,
+		Timeout:          30 * time.Minute,
+		CompatibleClouds: registry.OnlyGCE,
+		Dependencies:     []registry.OperationDependency{registry.OperationRequiresNodes},
+		Run: func(ctx context.Context, o operation.Operation, c cluster.Cluster) registry.OperationCleanup {
+			return resizeCluster(ctx, o, c, 3)
+		},
+	})
+}
