59f5cb92dd7681bc9a044c392610117afe8a9ea2
Rafi Shamim <rafi@cockroachlabs.com>
2024-07-11 17:15:10 -0400

    bootstrap: create an explicit zoneconfig for timeseries data
    This will make it more clear that the timeseries zone config can be
    changed independently from all the other zone configs.
    
    Release note: None

--- pkg/sql/catalog/bootstrap/metadata.go
+++ pkg/sql/catalog/bootstrap/metadata.go
@@ -511,6 +511,10 @@ func InitialZoneConfigKVs(
 	metaRangeZoneConf := protoutil.Clone(defaultSystemZoneConfig).(*zonepb.ZoneConfig)
 	livenessZoneConf := protoutil.Clone(defaultSystemZoneConfig).(*zonepb.ZoneConfig)
 
+	// The timeseries zone is the same as the default zone, but we create it
+	// explicitly here so it's more clearly reported with SHOW ALL ZONE CONFIGURATIONS.
+	timeseriesZoneConf := protoutil.Clone(defaultZoneConfig).(*zonepb.ZoneConfig)
+
 	// .meta zone config entry with a shorter GC time.
 	metaRangeZoneConf.GC.TTLSeconds = 60 * 60 // 1h
 
@@ -534,6 +538,7 @@ func InitialZoneConfigKVs(
 	add(keys.MetaRangesID, metaRangeZoneConf)
 	add(keys.LivenessRangesID, livenessZoneConf)
 	add(keys.SystemRangesID, systemZoneConf)
+	add(keys.TimeseriesRangesID, timeseriesZoneConf)
 	add(keys.SystemDatabaseID, systemZoneConf)
 	add(keys.ReplicationConstraintStatsTableID, replicationConstraintStatsZoneConf)
 	add(keys.ReplicationStatsTableID, replicationStatsZoneConf)
