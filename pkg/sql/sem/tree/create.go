421aa9abc4b80c4493b8375d80aa4b95c4044975
Michael Erickson <michae2@cockroachlabs.com>
2024-07-11 16:46:33 -0700

    tree, parser, externalconn: use FormatURI in CREATE EXTERNAL CONNECTION
    Use `tree.(*FmtCtx).FormatURI` in `CREATE EXTERNAL CONNECTION`.
    
    Epic: None
    
    Release note (security update): URLs in the following SQL statements are
    now sanitized of any secrets before being written to unredacted logs:
    - `CREATE EXTERNAL CONNECTION`

--- pkg/sql/sem/tree/create.go
+++ pkg/sql/sem/tree/create.go
@@ -2159,7 +2159,7 @@ func (node *CreateExternalConnection) Format(ctx *FmtCtx) {
 	ctx.WriteString("CREATE EXTERNAL CONNECTION")
 	ctx.FormatNode(&node.ConnectionLabelSpec)
 	ctx.WriteString(" AS ")
-	ctx.FormatNode(node.As)
+	ctx.FormatURI(node.As)
 }
 
 // CreateTenant represents a CREATE VIRTUAL CLUSTER statement.
