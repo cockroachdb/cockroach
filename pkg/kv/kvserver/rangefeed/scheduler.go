462a94cfc067ebab3c16a8cb99582363979d5733
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2024-07-12 13:47:00 -0400

    *: use syncutil.Set
    This commit replaces syncutil.Map with syncutil.Set, where appropriate.
    
    Epic: None
    Release note: None

--- pkg/kv/kvserver/rangefeed/scheduler.go
+++ pkg/kv/kvserver/rangefeed/scheduler.go
@@ -154,7 +154,7 @@ type Scheduler struct {
 	// separate shards to reduce mutex contention. Allocation is modulo
 	// processors, with shard 0 reserved for priority processors.
 	shards      []*schedulerShard // 1 + id%(len(shards)-1)
-	priorityIDs syncutil.Map[int64, struct{}]
+	priorityIDs syncutil.Set[int64]
 	wg          sync.WaitGroup
 }
 
@@ -282,10 +282,10 @@ func (s *Scheduler) register(id int64, f Callback, priority bool) error {
 	// Make sure we register the priority ID before registering the callback,
 	// since we can otherwise race with enqueues, using the wrong shard.
 	if priority {
-		s.priorityIDs.Store(id, new(struct{}))
+		s.priorityIDs.Add(id)
 	}
 	if err := s.shards[shardIndex(id, len(s.shards), priority)].register(id, f); err != nil {
-		s.priorityIDs.Delete(id)
+		s.priorityIDs.Remove(id)
 		return err
 	}
 	return nil
@@ -300,9 +300,9 @@ func (s *Scheduler) register(id int64, f Callback, priority bool) error {
 // Any attempts to enqueue events for processor after this call will return an
 // error.
 func (s *Scheduler) unregister(id int64) {
-	_, priority := s.priorityIDs.Load(id)
+	priority := s.priorityIDs.Contains(id)
 	s.shards[shardIndex(id, len(s.shards), priority)].unregister(id)
-	s.priorityIDs.Delete(id)
+	s.priorityIDs.Remove(id)
 }
 
 func (s *Scheduler) Stop() {
@@ -328,7 +328,7 @@ func (s *Scheduler) stopProcessor(id int64) {
 // Enqueue event for existing callback. The event is ignored if the processor
 // does not exist.
 func (s *Scheduler) enqueue(id int64, evt processorEventType) {
-	_, priority := s.priorityIDs.Load(id)
+	priority := s.priorityIDs.Contains(id)
 	s.shards[shardIndex(id, len(s.shards), priority)].enqueue(id, evt)
 }
 
@@ -583,7 +583,7 @@ type SchedulerBatch struct {
 	priorityIDs map[int64]bool
 }
 
-func newSchedulerBatch(numShards int, priorityIDs *syncutil.Map[int64, struct{}]) *SchedulerBatch {
+func newSchedulerBatch(numShards int, priorityIDs *syncutil.Set[int64]) *SchedulerBatch {
 	b := schedulerBatchPool.Get().(*SchedulerBatch)
 	if cap(b.ids) >= numShards {
 		b.ids = b.ids[:numShards]
@@ -595,7 +595,7 @@ func newSchedulerBatch(numShards int, priorityIDs *syncutil.Map[int64, struct{}]
 	}
 	// Cache the priority range IDs in an owned map, since we expect this to be
 	// very small or empty and we do a lookup for every Add() call.
-	priorityIDs.Range(func(id int64, _ *struct{}) bool {
+	priorityIDs.Range(func(id int64) bool {
 		b.priorityIDs[id] = true
 		return true
 	})
