0ec7e8855b112680d09048f1c006f4ce0d0d3055
Faizan Qazi <faizan@cockroachlabs.com>
2024-07-15 16:40:46 -0400

    workload/tpccmultidb: add support for invoking the web console APIs
    Previously, the tpcc workload had support for invoking normal SQL before
    any txn. This allowed us to test out ORM queries for large schema
    benchmarks when used with the tpccmultidb variant. To further be able to
    validate CRDB performance on large schemas we also need to be able to
    ensure the web console performs well. To address this, we are going to
    add support for invoking web console v2 API.
    
    Informs: #123767
    Release note: None

--- pkg/workload/tpcc/tpcc_multi_db.go
+++ pkg/workload/tpcc/tpcc_multi_db.go
@@ -12,16 +12,25 @@ package tpcc
 
 import (
 	"context"
+	"crypto/tls"
 	gosql "database/sql"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
 	"strings"
 	"sync"
 	"sync/atomic"
 
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/util/retry"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/cockroach/pkg/workload"
 	"github.com/cockroachdb/cockroach/pkg/workload/histogram"
+	"github.com/cockroachdb/errors"
 	"github.com/jackc/pgx/v5"
 )
 
@@ -32,6 +41,20 @@ type tpccMultiDB struct {
 	// created on and have the workload executed on.
 	dbListFile string
 	dbList     []*tree.ObjectNamePrefix
+
+	adminUrlStr string
+	adminUrls   []string
+
+	consoleAPICommandFile string
+	consoleAPICommands    []string
+	consoleAPITime        *histogram.NamedHistogram
+
+	mu struct {
+		// cachedRestAPISessions sessions which are cached so that authentication
+		// not needed for each call.
+		cachedRestAPISessions []string
+		syncutil.Mutex
+	}
 
 	// nextDatabase selects the next database in a round robin manner.
 	nextDatabase atomic.Uint64
@@ -52,8 +75,15 @@ var tpccMultiDBMeta = workload.Meta{
 		g.tpcc = tpccMeta.New().(*tpcc)
 		g.tpcc.workloadName = "tpccmultidb"
 		g.flags.Meta["txn-preamble-file"] = workload.FlagMeta{RuntimeOnly: true}
+		g.flags.Meta["admin-urls"] = workload.FlagMeta{RuntimeOnly: true}
+		g.flags.Meta["console-api-file"] = workload.FlagMeta{RuntimeOnly: true}
 		// Support accessing multiple databases via the client driver.
 		g.flags.StringVar(&g.dbListFile, "db-list-file", "", "a file containing a list of databases.")
+		g.flags.StringVar(&g.adminUrlStr, "admin-urls", "", "a list of admin URLs, seperated by commas")
+		g.flags.StringVar(&g.consoleAPICommandFile,
+			"console-api-file",
+			"",
+			"a list of commands to run at the start of each txn")
 		// Because this workload can create a large number of objects, the import
 		// concurrent may need to be limited.
 		g.flags.Int(workload.ImportDataLoaderConcurrencyFlag, 32, workload.ImportDataLoaderConcurrencyFlagDescription)
@@ -61,13 +91,152 @@ var tpccMultiDBMeta = workload.Meta{
 	},
 }
 
+// getRestAPISession gets a session ID for the web API, which will attempt
+// to use a cached session or generate a new one if the cache is empty. A
+// function is returned to allow the session to be returned back to the cache.
+func (t *tpccMultiDB) getRestAPISession(
+	ctx context.Context, client *http.Client, adminUrl string,
+) (string, func(), error) {
+	// Used to fetch a session ID from the cache.
+	getCached := func() string {
+		t.mu.Lock()
+		defer t.mu.Unlock()
+		if len(t.mu.cachedRestAPISessions) > 0 {
+			token := t.mu.cachedRestAPISessions[0]
+			t.mu.cachedRestAPISessions = t.mu.cachedRestAPISessions[1:]
+			return token
+		}
+		return ""
+	}
+
+	// Attempt to get a cached token first.
+	token := getCached()
+	// We did not find any cached session ID, so invoke the login
+	// end poin t.
+	if token == "" {
+		loginUrl := fmt.Sprintf("%s/api/v2/login/", adminUrl)
+		values := url.Values{
+			"username": {"roachadmin"},
+			"password": {"roacher"},
+		}
+		req, err := http.NewRequestWithContext(ctx, "POST", loginUrl, strings.NewReader(values.Encode()))
+		if err != nil {
+			return "", nil, err
+		}
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		loginResp, err := client.Do(req)
+		if err != nil {
+			return "", nil, err
+		}
+		defer loginResp.Body.Close()
+		if loginResp.StatusCode != http.StatusOK {
+			return "", nil, errors.AssertionFailedf("unexpected status from end point during auth %s (%d)",
+				loginResp.Status,
+				loginResp.StatusCode)
+		}
+		d := json.NewDecoder(loginResp.Body)
+		var sessionInfo struct {
+			Session string
+		}
+		err = d.Decode(&sessionInfo)
+		if err != nil {
+			return "", nil, err
+		}
+		token = sessionInfo.Session
+	}
+
+	return token, func() {
+		t.mu.Lock()
+		defer t.mu.Unlock()
+		t.mu.cachedRestAPISessions = append(t.mu.cachedRestAPISessions, token)
+	}, nil
+}
+
+// runWebAPICommands before txn executes any calls into the API.
+func (t *tpccMultiDB) runWebAPICommands(
+	ctx context.Context, targetDb string, adminUrl string,
+) error {
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			// Roachprod clusters may have invalid certificates.
+			InsecureSkipVerify: true,
+		},
+	}
+	client := http.Client{Transport: transport}
+	defer client.CloseIdleConnections()
+	const maxAPIRetries = 10
+	var sessionID string
+	var releaseFunc func()
+
+	// For resilience, tolerate any internal errors from the server.
+	if err := retry.WithMaxAttempts(ctx, retry.Options{}, maxAPIRetries, func() error {
+		var err error
+		sessionID, releaseFunc, err = t.getRestAPISession(ctx, &client, adminUrl)
+		if err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+	defer releaseFunc()
+
+	invokeApi := func(apiCommand string) error {
+		targetUrl := fmt.Sprintf("%s/%s", adminUrl, apiCommand)
+		getTablesRequest, err := http.NewRequestWithContext(ctx, "GET", targetUrl, nil)
+		if err != nil {
+			return err
+		}
+		getTablesRequest.Header.Add("X-Cockroach-API-Session", sessionID)
+		resp, err := client.Do(getTablesRequest)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			return errors.AssertionFailedf("unexpected status from end point (%q) %s (%d)",
+				apiCommand,
+				resp.Status,
+				resp.StatusCode)
+		}
+		_, err = io.ReadAll(resp.Body)
+		return err
+	}
+
+	startTime := timeutil.Now()
+	for _, apiCommand := range t.consoleAPICommands {
+		apiCommandResolved := os.Expand(apiCommand, func(s string) string {
+			switch s {
+			case "targetDb":
+				return targetDb
+			default:
+				return s
+			}
+		})
+		// Attempt the end point multiple times in case we hit internal errors
+		// due to the load on the server.
+		if err := retry.WithMaxAttempts(ctx, retry.Options{}, maxAPIRetries, /*max attempts */
+			func() error {
+				return invokeApi(apiCommandResolved)
+			}); err != nil {
+			return err
+		}
+	}
+	// Record the time that the invocation took.
+	t.consoleAPITime.Record(timeutil.Since(startTime))
+	return nil
+}
+
 // runBeforeEachTxn is executed at the start of each transaction
 // inside normal tpcc.
 func (t *tpccMultiDB) runBeforeEachTxn(ctx context.Context, tx pgx.Tx) error {
 	// If multiple DBs are specified via list, select one
 	// in a roundrobin manner.
+	nextIdx := t.nextDatabase.Add(1)
+	targetDb := "tpccmultidb"
 	if t.dbList != nil {
-		databaseIdx := int(t.nextDatabase.Add(1) % uint64(len(t.dbList)))
+		databaseIdx := int(nextIdx % uint64(len(t.dbList)))
+		targetDb = t.dbList[databaseIdx].Catalog()
 		if _, err := tx.Exec(ctx, "USE $1", t.dbList[databaseIdx].Catalog()); err != nil {
 			return err
 		}
@@ -75,7 +244,12 @@ func (t *tpccMultiDB) runBeforeEachTxn(ctx context.Context, tx pgx.Tx) error {
 			return err
 		}
 	}
-
+	if len(t.adminUrls) > 0 {
+		adminUrl := t.adminUrls[int(nextIdx%uint64(len(t.adminUrls)))]
+		if err := t.runWebAPICommands(ctx, targetDb, adminUrl); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
@@ -85,6 +259,9 @@ func (t *tpccMultiDB) Ops(
 ) (workload.QueryLoad, error) {
 	if err := t.runInit(); err != nil {
 		return workload.QueryLoad{}, err
+	}
+	if t.consoleAPITime == nil {
+		t.consoleAPITime = reg.GetHandle().Get("consoleAPITime")
 	}
 	return t.tpcc.Ops(ctx, urls, reg)
 }
@@ -135,6 +312,20 @@ func (t *tpccMultiDB) runInit() error {
 				}
 				t.dbList = append(t.dbList, prefix)
 			}
+		}
+		if t.adminUrlStr != "" {
+			t.adminUrls = strings.Split(t.adminUrlStr, ",")
+		}
+		if t.consoleAPICommandFile != "" {
+			file, err := os.ReadFile(t.consoleAPICommandFile)
+			if err != nil {
+				return
+			}
+			strConsoleAPIList := strings.Split(string(file), "\n")
+			if v := len(strConsoleAPIList); v > 0 && len(strConsoleAPIList[v-1]) == 0 {
+				strConsoleAPIList = strConsoleAPIList[:v-1]
+			}
+			t.consoleAPICommands = strConsoleAPIList
 		}
 		// Execute extra logic at the start of each txn.
 		t.onTxnStartFns = append(t.onTxnStartFns, t.runBeforeEachTxn)
