de0f0591ca6036316f130c6c909b372f66d44121
Herko Lategan <herko@cockroachlabs.com>
2024-07-15 13:34:16 +0100

    roachprod-microbench: add stage command
    Previously, copying and extracting the binaries were done as part of the run
    command. To make CI more flexible and allow the usage of binaries cached on GCE,
    a new stage command has been added. The command can stage binaries from both a
    local or cloud bucket source. It will extract the binaries, create the necessary
    directors and clean up afterward.
    
    Release note: None
    Epic: None

---
+++ pkg/cmd/roachprod-microbench/stage.go
@@ -0,0 +1,74 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package main
+
+import (
+	"context"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/cockroachdb/cockroach/pkg/roachprod"
+	"github.com/cockroachdb/cockroach/pkg/roachprod/logger"
+	"github.com/cockroachdb/errors"
+)
+
+// stage copies the specified archive to the remote machine and extracts it to
+// the specified directory (creating it if it does not exist).
+func stage(cluster, archivePath, remoteDest string) (err error) {
+	ctx := context.Background()
+
+	initRoachprod()
+	loggerCfg := logger.Config{Stdout: os.Stdout, Stderr: os.Stderr}
+	l, err := loggerCfg.NewLogger("")
+	if err != nil {
+		return
+	}
+
+	archiveName := path.Base(archivePath)
+	archiveRemotePath := path.Join("/tmp", archiveName)
+
+	defer func() {
+		// Remove the remote archive after we're done.
+		cleanUpErr := roachprodRun(cluster, l, []string{"rm", "-rf", archiveRemotePath})
+		err = errors.CombineErrors(err, errors.Wrapf(cleanUpErr, "removing remote archive: %s", archiveRemotePath))
+	}()
+
+	// Remove the remote archive and destination directory if they exist.
+	if err = roachprodRun(cluster, l, []string{"rm", "-rf", archiveRemotePath}); err != nil {
+		return errors.Wrapf(err, "removing remote archive: %s", archiveRemotePath)
+	}
+	if err = roachprodRun(cluster, l, []string{"rm", "-rf", remoteDest}); err != nil {
+		return errors.Wrapf(err, "removing remote destination: %s", remoteDest)
+	}
+
+	// Copy the archive to the remote machine.
+	copyFromGCS := strings.HasPrefix(archivePath, "gs://")
+	if copyFromGCS {
+		if err = roachprodRun(cluster, l, []string{"gsutil", "-q", "-m", "cp", archivePath, archiveRemotePath}); err != nil {
+			return errors.Wrapf(err, "copying archive from GCS: %s", archivePath)
+		}
+	} else {
+		if err = roachprod.Put(ctx, l, cluster, archivePath, archiveRemotePath, true); err != nil {
+			return errors.Wrapf(err, "copying archive: %s", archivePath)
+		}
+	}
+
+	// Extract the archive on the remote machine.
+	if err = roachprodRun(cluster, l, []string{"mkdir", "-p", remoteDest}); err != nil {
+		return errors.Wrapf(err, "creating remote destination: %s", remoteDest)
+	}
+	if err = roachprodRun(cluster, l, []string{"tar", "-C", remoteDest, "-xzf", archiveRemotePath}); err != nil {
+		return errors.Wrapf(err, "extracting archive: %s", archiveRemotePath)
+	}
+
+	return
+}
