473bf871ef4db47637ff421329e16f6545614df9
Faizan Qazi <faizan@cockroachlabs.com>
2024-07-16 08:39:04 -0400

    sql: alter type drop value can crash formatting out dependent rows
    Previously, we modified drop type to only select accessible columns of a
    given index when detecting rows still using enum values. We
    unfortunately, did not corrects update logic that formats out the error
    message to map the names correctly to visible accessible columns only.
    To address this, this patch updates error logic to use the accessible
    columns.
    
    Fixes: #127136
    Release note: None

--- pkg/sql/type_change.go
+++ pkg/sql/type_change.go
@@ -925,7 +925,7 @@ func (t *typeSchemaChanger) canRemoveEnumValue(
 			if len(rows) > 0 {
 				return pgerror.Newf(pgcode.DependentObjectsStillExist,
 					"could not remove enum value %q as it is being used by %q in row: %s",
-					member.LogicalRepresentation, desc.GetName(), labeledRowValues(desc.PublicColumns(), rows))
+					member.LogicalRepresentation, desc.GetName(), labeledRowValues(desc.AccessibleColumns(), rows))
 			}
 		}
 
