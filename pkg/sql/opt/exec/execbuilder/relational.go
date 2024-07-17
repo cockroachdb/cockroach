27a0a53e8adbf4662ec89c4aaaad9a93be38e0b1
Michael Erickson <michae2@cockroachlabs.com>
2024-07-16 17:14:47 -0700

    sql: allow DISCARD in read-only transactions
    To match PostgreSQL, allow DISCARD in read-only transactions.
    
    Fixes: #124150
    
    Release note (sql change): Fix a bug in which the DISCARD statement was
    disallowed with `default_transaction_read_only = on`.

--- pkg/sql/opt/exec/execbuilder/relational.go
+++ pkg/sql/opt/exec/execbuilder/relational.go
@@ -150,8 +150,14 @@ func (b *Builder) buildRelational(e memo.RelExpr) (_ execPlan, outputCols colOrd
 		b.flags.Set(exec.PlanFlagContainsMutation)
 		// Raise error if mutation op is part of a read-only transaction.
 		if b.evalCtx.TxnReadOnly {
-			return execPlan{}, colOrdMap{}, pgerror.Newf(pgcode.ReadOnlySQLTransaction,
-				"cannot execute %s in a read-only transaction", b.statementTag(e))
+			switch tag := b.statementTag(e); tag {
+			// DISCARD can drop temp tables but is still allowed in read-only
+			// transactions for PG compatibility.
+			case "DISCARD ALL", "DISCARD":
+			default:
+				return execPlan{}, colOrdMap{}, pgerror.Newf(pgcode.ReadOnlySQLTransaction,
+					"cannot execute %s in a read-only transaction", tag)
+			}
 		}
 	}
 
