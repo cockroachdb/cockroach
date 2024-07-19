3469436fd874c1876067c57d2215bc6cf609c9b3
souravcrl <sourav.sarangi@cockroachlabs.com>
2024-07-19 06:44:14 +0000

    jwtauthccl: add mapper for issuer URL to jwks URI
    For provisioning JWKS auto fetch feature we currently depend on issuer's
    well-known endpoint. This conflicts for some JWT issuers who either don't have a
    well-known endpoint or path of the well-known endpoint is non-standard. In such
    cases, operators require to manually set the JWKS URI mapping for each JWT
    issuer they are intending to allow JWT login. This is facilitated by this PR by
    allowing `server.jwt_authentication.issuers.configuration` to now take in json
    object mapping of issuer URL to JWKS URI.
    
    informs https://github.com/cockroachlabs/support/issues/2956
    fixes https://github.com/cockroachdb/cockroach/issues/124976
    Epic CRDB-39178
    
    Release note(security, ops): The cluster setting
    `server.jwt_authentication.issuers` can now take multiple values to support
    various kinds of providers and their mapped JWKS URI. This can be set to one of
    the following values:
    1. Simple string that Go can parse as a valid issuer URL.
    2. String that can be parsed as valid JSON array of issuer URLs list.
    3. String that can be parsed as valid JSON and deserialized into a map of issuer
    URLs to corresponding JWKS URIs.
    In the third case we will be overriding the JWKS URI present in the issuer's
    well-known endpoint.
    Example valid values:
    - 'https://accounts.google.com'
    - ['example.com/adfs','https://accounts.google.com']
    - '{
       "issuer_jwks_map": {
           "https://accounts.google.com": "https://www.googleapis.com/oauth2/v3/certs",
           "example.com/adfs": "https://example.com/adfs/discovery/keys"
        }
       }'
    When `issuer_jwks_map` is set, we directly use the JWKS URI to get the key set. In
    all other cases where JWKSAutoFetchEnabled is set we obtain the JWKS URI first
    from issuer's well-known endpoint and then use this endpoint.

--- pkg/ccl/jwtauthccl/authentication_jwt.go
+++ pkg/ccl/jwtauthccl/authentication_jwt.go
@@ -67,7 +67,7 @@ type jwtAuthenticator struct {
 type jwtAuthenticatorConf struct {
 	audience             []string
 	enabled              bool
-	issuers              []string
+	issuersConf          issuerURLConf
 	jwks                 jwk.Set
 	claim                string
 	jwksAutoFetchEnabled bool
@@ -87,7 +87,7 @@ func (authenticator *jwtAuthenticator) reloadConfigLocked(
 	conf := jwtAuthenticatorConf{
 		audience:             mustParseValueOrArray(JWTAuthAudience.Get(&st.SV)),
 		enabled:              JWTAuthEnabled.Get(&st.SV),
-		issuers:              mustParseValueOrArray(JWTAuthIssuers.Get(&st.SV)),
+		issuersConf:          mustParseJWTIssuersConf(JWTAuthIssuersConfig.Get(&st.SV)),
 		jwks:                 mustParseJWKS(JWTAuthJWKS.Get(&st.SV)),
 		claim:                JWTAuthClaim.Get(&st.SV),
 		jwksAutoFetchEnabled: JWKSAutoFetchEnabled.Get(&st.SV),
@@ -157,25 +157,15 @@ func (authenticator *jwtAuthenticator) ValidateJWTLogin(
 	}
 
 	// Check for issuer match against configured issuers.
-	issuerUrl := ""
-	issuerMatch := false
-	for _, issuer := range authenticator.mu.conf.issuers {
-		if issuer == unverifiedToken.Issuer() {
-			issuerMatch = true
-			issuerUrl = issuer
-			break
-		}
-	}
-	if !issuerMatch {
-		return "", errors.WithDetailf(
-			errors.Newf("JWT authentication: invalid issuer"),
-			"token issued by %s", unverifiedToken.Issuer())
+	tokenIssuer := unverifiedToken.Issuer()
+	if err = authenticator.mu.conf.issuersConf.checkIssuerConfigured(tokenIssuer); err != nil {
+		return "", errors.WithDetailf(err, "token issued by %s", tokenIssuer)
 	}
 
 	var jwkSet jwk.Set
-	// If auto-fetch is enabled, fetch the JWKS remotely from the issuer's well known jwks url.
+	// If auto-fetch is enabled, fetch the JWKS remotely from the issuer's well known jwks URI.
 	if authenticator.mu.conf.jwksAutoFetchEnabled {
-		jwkSet, err = authenticator.remoteFetchJWKS(ctx, issuerUrl)
+		jwkSet, err = authenticator.remoteFetchJWKS(ctx, tokenIssuer)
 		if err != nil {
 			return fmt.Sprintf("unable to fetch jwks: %v", err),
 				errors.Newf("JWT authentication: unable to validate token")
@@ -278,16 +268,27 @@ func (authenticator *jwtAuthenticator) ValidateJWTLogin(
 	return "", nil
 }
 
-// remoteFetchJWKS fetches the JWKS from the provided URI.
+// remoteFetchJWKS fetches the JWKS URI from the provided issuer URL.
 func (authenticator *jwtAuthenticator) remoteFetchJWKS(
-	ctx context.Context, issuerUrl string,
+	ctx context.Context, issuerURL string,
 ) (jwk.Set, error) {
-	jwksUrl, err := authenticator.getJWKSUrl(ctx, issuerUrl)
+	var jwksURI string
+	// if JWKS URI is configured in JWTAuthIssuersConfig use that instead of URL
+	// from issuer's well-known endpoint
+	err := authenticator.mu.conf.issuersConf.checkJWKSConfigured()
 	if err != nil {
-		return nil, err
+		jwksURI, err = authenticator.getJWKSURI(ctx, issuerURL)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		jwksURI, err = authenticator.mu.conf.issuersConf.getJWKSURI(issuerURL)
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	body, err := getHttpResponse(ctx, jwksUrl, authenticator)
+	body, err := getHttpResponse(ctx, jwksURI, authenticator)
 	if err != nil {
 		return nil, err
 	}
@@ -298,8 +299,8 @@ func (authenticator *jwtAuthenticator) remoteFetchJWKS(
 	return jwkSet, nil
 }
 
-// getJWKSUrl returns the JWKS URI from the OpenID configuration endpoint.
-func (authenticator *jwtAuthenticator) getJWKSUrl(
+// getJWKSURI returns the JWKS URI from the OpenID configuration endpoint.
+func (authenticator *jwtAuthenticator) getJWKSURI(
 	ctx context.Context, issuerUrl string,
 ) (string, error) {
 	type OIDCConfigResponse struct {
@@ -361,7 +362,7 @@ var ConfigureJWTAuth = func(
 	JWTAuthEnabled.SetOnChange(&st.SV, func(ctx context.Context) {
 		authenticator.reloadConfig(ambientCtx.AnnotateCtx(ctx), st)
 	})
-	JWTAuthIssuers.SetOnChange(&st.SV, func(ctx context.Context) {
+	JWTAuthIssuersConfig.SetOnChange(&st.SV, func(ctx context.Context) {
 		authenticator.reloadConfig(ambientCtx.AnnotateCtx(ctx), st)
 	})
 	JWTAuthJWKS.SetOnChange(&st.SV, func(ctx context.Context) {
