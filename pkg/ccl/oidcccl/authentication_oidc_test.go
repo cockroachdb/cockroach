0f56d798a48e8cf3a4c5e5a23585029001b3a899
souravcrl <sourav.sarangi@cockroachlabs.com>
2024-07-19 06:44:14 +0000

    jwtauthccl: add mapper for issuer URL to jwks URI
    For provisioning JWKS auto fetch feature we currently depend on issuer's
    well-known endpoint. This conflicts for some JWT issuers who either don't have a
    well-known endpoint or path of the well-known endpoint is non-standard. In such
    cases, operators require to manually set the JWKS URI mapping for each JWT
    issuer they are intending to allow JWT login. This is facilitated by this PR by
    allowing `server.jwt_authentication.issuers.configuration` to now take in json
    object mapping of issuer URL to JWKS URI.
    
    informs https://github.com/cockroachlabs/support/issues/2956
    fixes https://github.com/cockroachdb/cockroach/issues/124976
    Epic CRDB-39178
    
    Release note(security, ops): The cluster setting
    `server.jwt_authentication.issuers` is now changed to client facing name
    `server.jwt_authentication.issuers.configuration` to be reflective of the value
    the setting can take. This can be set to one of the following values:
    1. Simple string that Go can parse as a valid issuer URL.
    2. String that can be parsed as valid JSON array of issuer URLs list.
    3. String that can be parsed as valid JSON and deserialized into a map of issuer
    URLs to corresponding JWKS URIs.
    In the third case we will be overriding the JWKS URI present in the issuer's
    well-known endpoint.
    Example valid values:
    - 'https://accounts.google.com'
    - ['example.com/adfs','https://accounts.google.com']
    - '{
       "issuer_jwks_map": {
           "https://accounts.google.com": "https://www.googleapis.com/oauth2/v3/certs",
           "example.com/adfs": "https://example.com/adfs/discovery/keys"
        }
       }'
    When `issuer_jwks_map` is set, we directly use the JWKS URI to get the key set. In
    all other cases where JWKSAutoFetchEnabled is set we obtain the JWKS URI first
    from issuer's well-known endpoint and then use this endpoint.

--- pkg/ccl/oidcccl/authentication_oidc_test.go
+++ pkg/ccl/oidcccl/authentication_oidc_test.go
@@ -532,7 +532,7 @@ func TestOIDCProviderInitialization(t *testing.T) {
 				Issuer      string   `json:"issuer"`
 				AuthURL     string   `json:"authorization_endpoint"`
 				TokenURL    string   `json:"token_endpoint"`
-				JWKSURL     string   `json:"jwks_uri"`
+				JWKSURI     string   `json:"jwks_uri"`
 				UserInfoURL string   `json:"userinfo_endpoint"`
 				Algorithms  []string `json:"id_token_signing_alg_values_supported"`
 			}
