84101b2f4885b6db879fd8cea784c93cc8b8a506
Renato Costa <renato@cockroachlabs.com>
2024-07-22 17:58:11 +0000

    server: do not initialize cluster version to minimum supported
    Previously, shared-process tenants would initialize their view of the
    cluster version early and set it to the `MinSupportedVersion` as a
    "safe" option. However, that setting can likely be wrong, especially
    now that we plan to support skip-version upgrades.
    
    In this commit, we remove that code and rely on the initialization
    that happens from storage in `preStart`. This means that we now expose
    a non-negligible amount of code to a state where we don't have an
    initialized cluster version. However, having that error out is better
    than have code assume a potentially wrong version.
    
    A similar discussion and approach happened for separate process
    tenants a while ago: see #100684 and #104859.
    
    Fixes: #126754
    
    Release note: None

--- pkg/server/server_sql.go
+++ pkg/server/server_sql.go
@@ -1471,16 +1471,48 @@ func (s *SQLServer) preStart(
 	// NB: In the context of the system tenant, we may not have
 	// the version setting in SQL yet even though the in memory
 	// setting has been initialized in.
-	currentVersion := s.execCfg.Settings.Version.ActiveVersionOrEmpty(ctx).Version
-	if currentVersion.Equal(roachpb.Version{}) {
-		if err := s.execCfg.DB.Txn(ctx, func(ctx context.Context, txn *kv.Txn) (err error) {
-			v, err := s.settingsWatcher.GetClusterVersionFromStorage(ctx, txn)
-			if err != nil {
-				return err
+	initializeClusterVersion := func(ctx context.Context) error {
+		currentVersion := s.execCfg.Settings.Version.ActiveVersionOrEmpty(ctx).Version
+		if currentVersion.Equal(roachpb.Version{}) {
+			if err := s.execCfg.DB.Txn(ctx, func(ctx context.Context, txn *kv.Txn) (err error) {
+				v, err := s.settingsWatcher.GetClusterVersionFromStorage(ctx, txn)
+				if err != nil {
+					return err
+				}
+				return clusterversion.Initialize(ctx, v.Version, &s.execCfg.Settings.SV)
+			}); err != nil {
+				return errors.Wrap(err, "initializing cluster version")
 			}
-			return clusterversion.Initialize(ctx, v.Version, &s.execCfg.Settings.SV)
-		}); err != nil {
-			return errors.Wrap(err, "initializing cluster version")
+		}
+
+		return nil
+	}
+
+	if s.execCfg.Codec.ForSystemTenant() {
+		if err := initializeClusterVersion(ctx); err != nil {
+			return err
+		}
+	} else {
+		// When initializing the cluster version for tenants, we retry if
+		// the error indicates that the KV authorizer hasn't seen our
+		// service-mode transition. This is possible because both the
+		// service controller and the authorizer receive tenant mode
+		// updates asynchronously via rangefeeds.
+		opts := retry.Options{MaxRetries: 5}
+		var nonRetryableErr error
+		err := opts.Do(ctx, func(ctx context.Context) error {
+			if err := initializeClusterVersion(ctx); err != nil {
+				if strings.Contains(err.Error(), `operation not allowed when in service mode "none"`) {
+					return err
+				}
+				nonRetryableErr = err
+			}
+
+			return nil
+		})
+
+		if err != nil || nonRetryableErr != nil {
+			return errors.CombineErrors(err, nonRetryableErr)
 		}
 	}
 
