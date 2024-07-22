1bc1c854490ac19c4cf6c0d94f89d6f0b4fa32b5
Ibrahim Kettaneh <ibrahim.kettaneh@cockroachlabs.com>
2024-07-22 11:40:09 -0400

    spanconfig: log spanconfig changes
    Whenever there is a SpanConfig change, log it to the distribution
    channel. Note that we only log changes if it modifies something in the
    SpanConfig other than the ProtectedTimestamps to reduce the number of
    things to log.
    
    Fixes: #127171
    
    Release note: None

--- pkg/spanconfig/spanconfigstore/store.go
+++ pkg/spanconfig/spanconfigstore/store.go
@@ -185,6 +185,14 @@ func (s *Store) getFallbackConfig() roachpb.SpanConfig {
 func (s *Store) Apply(
 	ctx context.Context, dryrun bool, updates ...spanconfig.Update,
 ) (deleted []spanconfig.Target, added []spanconfig.Record) {
+
+	// Log the potential span config changes.
+	if !dryrun {
+		for _, update := range updates {
+			s.maybeLogUpdate(ctx, &update)
+		}
+	}
+
 	deleted, added, err := s.applyInternal(ctx, dryrun, updates...)
 	if err != nil {
 		log.Fatalf(ctx, "%v", err)
@@ -314,4 +322,50 @@ func (s *Store) Iterate(f func(spanconfig.Record) error) error {
 			}
 			return f(record)
 		})
+}
+
+// maybeLogUpdate logs the SpanConfig changes to the distribution channel. It
+// also logs changes to the span boundaries. It doesn't log the changes to the
+// SpanConfig for high churn fields like protected timestamps.
+func (s *Store) maybeLogUpdate(ctx context.Context, update *spanconfig.Update) {
+	nextSC := update.GetConfig()
+	target := update.GetTarget()
+
+	// Return early from SystemTarget updates because they correspond to PTS
+	// updates.
+	if !target.IsSpanTarget() {
+		return
+	}
+
+	rKey, err := keys.Addr(target.GetSpan().Key)
+	if err != nil {
+		return
+	}
+
+	var curSpanConfig roachpb.SpanConfig
+	var curSpan roachpb.Span
+	var found bool
+	func() {
+		s.mu.RLock()
+		defer s.mu.RUnlock()
+		curSpanConfig, curSpan, found = s.mu.spanConfigStore.getSpanConfigForKey(ctx, rKey)
+	}()
+
+	// Check if the span bounds have changed.
+	if found &&
+		(!curSpan.Key.Equal(target.GetSpan().Key) ||
+			!curSpan.EndKey.Equal(target.GetSpan().EndKey)) {
+		log.KvDistribution.Infof(ctx,
+			"changing the span boundaries for span:%+v from:[%+v:%+v) to:[%+v:%+v) "+
+				"with config: %+v", target, curSpan.Key, curSpan.EndKey, target.GetSpan().Key,
+			target.GetSpan().EndKey, nextSC)
+	}
+
+	// Log if there is a SpanConfig change in any field other than
+	// ProtectedTimestamps to avoid logging PTS updates.
+	if found && curSpanConfig.HasConfigurationChange(nextSC) {
+		log.KvDistribution.Infof(ctx,
+			"changing the spanconfig for span:%+v from:%+v to:%+v",
+			target, curSpanConfig, nextSC)
+	}
 }
