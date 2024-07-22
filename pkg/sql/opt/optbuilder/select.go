afaf16540801d4b5a53f8e8fe3b04c7c99eb15ad
Michael Erickson <michae2@cockroachlabs.com>
2024-07-22 10:07:03 -0700

    opt: add skip-locked to unlocked scans below SKIP LOCKED
    When using the new Lock operator to implement FOR UPDATE and FOR SHARE,
    we build unlocked scans that are then followed by a final Lock
    operation. If the Lock operator uses SKIP LOCKED, however, these
    unlocked scans still need to be flagged with the skip-locked wait policy
    to avoid blocking, even if they do not take locks themselves.
    
    Fixes: #121917
    
    Release note (bug fix): Fix a bug in which some SELECT FOR UPDATE or
    SELECT FOR SHARE queries using SKIP LOCKED could still block on locked
    rows when using optimizer_use_lock_op_for_serializable under
    Serializable isolation. This bug was present when
    optimizer_use_lock_op_for_serializable was introduced in v23.2.0.

--- pkg/sql/opt/optbuilder/select.go
+++ pkg/sql/opt/optbuilder/select.go
@@ -168,7 +168,20 @@ func (b *Builder) buildDataSource(
 				}
 			}
 			if b.shouldBuildLockOp() {
-				locking = nil
+				// If we're implementing FOR UPDATE / FOR SHARE with a Lock operator on
+				// top of the plan, then this can be an unlocked scan. But if the
+				// locking uses SKIP LOCKED then we still need this scan to skip over
+				// locks even if it does not take any locks itself.
+				if locking.get().WaitPolicy == tree.LockWaitSkipLocked {
+					// Create a dummy lockingSpec to get just the skip locked behavior.
+					locking = lockingSpec{&lockingItem{
+						item: &tree.LockingItem{
+							WaitPolicy: tree.LockWaitSkipLocked,
+						},
+					}}
+				} else {
+					locking = nil
+				}
 			}
 			return b.buildScan(
 				tabMeta,
@@ -504,7 +517,20 @@ func (b *Builder) buildScanFromTableRef(
 		}
 	}
 	if b.shouldBuildLockOp() {
-		locking = nil
+		// If we're implementing FOR UPDATE / FOR SHARE with a Lock operator on top
+		// of the plan, then this can be an unlocked scan. But if the locking uses
+		// SKIP LOCKED then we still need this scan to skip over locks even if it
+		// does not take any locks itself.
+		if locking.get().WaitPolicy == tree.LockWaitSkipLocked {
+			// Create a dummy lockingSpec to get just the skip locked behavior.
+			locking = lockingSpec{&lockingItem{
+				item: &tree.LockingItem{
+					WaitPolicy: tree.LockWaitSkipLocked,
+				},
+			}}
+		} else {
+			locking = nil
+		}
 	}
 	return b.buildScan(
 		tabMeta, ordinals, indexFlags, locking, inScope, false, /* disableNotVisibleIndex */
@@ -747,7 +773,7 @@ func (b *Builder) buildScan(
 	}
 	if locking.isSet() {
 		private.Locking = locking.get()
-		if b.shouldUseGuaranteedDurability() {
+		if private.Locking.IsLocking() && b.shouldUseGuaranteedDurability() {
 			// Under weaker isolation levels we use fully-durable locks for SELECT FOR
 			// UPDATE statements, SELECT FOR SHARE statements, and constraint checks
 			// (e.g. FK checks), regardless of locking strength and wait policy.
