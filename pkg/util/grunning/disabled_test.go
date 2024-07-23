7ea20140f56d1c6a4fddf7b676acc5a930330abc
yasiribmcon <107119183+yasiribmcon@users.noreply.github.com>
2024-07-23 09:26:01 +0530

    Include grunning for s390x.
    PR https://github.com/cockroachdb/cockroach/pull/95291 excluded grunning from s390x as patched Go runtime was not available.
    Since we can refer go patch at https://github.com/cockroachdb/cockroach/blob/master/build/teamcity/internal/release/build-and-publish-patched-go/diff.patch, we can manually patch go runtime for s390x so that grunning works for s390x.
    Hence enabling back this library for s390x.

--- pkg/util/grunning/disabled_test.go
+++ pkg/util/grunning/disabled_test.go
@@ -10,8 +10,8 @@
 
 // See grunning.Supported() for an explanation behind this build tag.
 //
-//go:build (linux && s390x) || !bazel
-// +build linux,s390x !bazel
+//go:build !bazel
+// +build !bazel
 
 package grunning_test
 
