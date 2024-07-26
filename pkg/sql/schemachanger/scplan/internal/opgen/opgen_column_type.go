cockroachdb/cockroach

commit 52c12c046386d0ad3e18301832529a810992da34

sql/schemachanger: Add DSC support for trivial data type changes
This moves ALTER COLUMN .. SET TYPE support into the declarative schema
changer (DSC). Prior to this, it was always done in the legacy schema
changer.

Only trivial alterations are moved to the DSC in this commit. Trivial
changes are those where no backfill is needed for the data. An example
is changing a SMALLINT to a BIGINT; the existing data is fine and
doesn’t need to be rewritten. If a complex alteration is requested, we
will error out and fall back to the legacy schema changer.

Much of the logic in AlterColumnType, which is for the legacy schema
changer, was duplicated since the DSC has a different state system. Many
of the catalog state checks were reimplemented using the element state
system that DSC uses. There is one check for TTL (in
ValidateTTLExpressionDoesNotDependOnColumn) that hasn’t been duplicated
yet. That will be handled in a follow-up change.

There are some changes to ensure the error messages are the same between
the legacy and DSC. This allows us to run the same test twice, once with
each schema changer.

Epic: CRDB-25314
Informs: #126143
Release note: None

pkg/sql/schemachanger/scplan/internal/opgen/opgen_column_type.go
@@ -12,8 +12,10 @@ package opgen
 
 import (
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scop"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/screl"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/catid"
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
 )
@@ -43,7 +45,13 @@ func init() {
 			scpb.Status_PUBLIC,
 			to(scpb.Status_ABSENT,
 				revertible(false),
-				emit(func(this *scpb.ColumnType) *scop.RemoveDroppedColumnType {
+				emit(func(this *scpb.ColumnType, md *opGenContext) *scop.RemoveDroppedColumnType {
+					// Make this a no-op if the column type already exists. The
+					// RemoveDroppedColumnType op is meant to be emitted only for columns
+					// that were dropped.
+					if checkIfColumnTypeExists(this.TableID, this.ColumnID, md) {
+						return nil
+					}
 					if ids := referencedTypeIDs(this); len(ids) > 0 {
 						return &scop.RemoveDroppedColumnType{
 							TableID:  this.TableID,
@@ -77,4 +85,32 @@ func referencedTypeIDs(this *scpb.ColumnType) []catid.DescID {
 		ids.Add(id)
 	}
 	return ids.Ordered()
+}
+
+// checkIfColumnTypeExists will determine if we are changing the given columns
+// type. It does this by looking at all the targets and checking if there are
+// two ColumnType's.
+func checkIfColumnTypeExists(tableID descpb.ID, columnID descpb.ColumnID, md *opGenContext) bool {
+	foundAbsentToPublicTransition := false
+	foundPublicToAbsentTransition := false
+	for idx, t := range md.Targets {
+		if screl.GetDescID(t.Element()) != tableID {
+			continue
+		}
+		switch t.Element().(type) {
+		case *scpb.ColumnType:
+			if t.Element().(*scpb.ColumnType).ColumnID != columnID {
+				continue
+			}
+			if md.Initial[idx] == scpb.Status_ABSENT &&
+				md.TargetState.Targets[idx].TargetStatus == scpb.Status_PUBLIC {
+				foundAbsentToPublicTransition = true
+			}
+			if md.Initial[idx] == scpb.Status_PUBLIC &&
+				md.TargetState.Targets[idx].TargetStatus == scpb.Status_ABSENT {
+				foundPublicToAbsentTransition = true
+			}
+		}
+	}
+	return foundPublicToAbsentTransition && foundAbsentToPublicTransition
 }

pkg/ccl/schemachangerccl/backup_base_generated_test.go
@@ -165,6 +165,20 @@ func TestBackupRollbacks_base_alter_table_alter_column_set_not_null(t *testing.T
 	sctest.BackupRollbacks(t, path, sctest.SingleNodeTestClusterFactory{})
 }
 
+func TestBackupRollbacks_base_alter_table_alter_column_type_noop(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	const path = "pkg/sql/schemachanger/testdata/end_to_end/alter_table_alter_column_type_noop"
+	sctest.BackupRollbacks(t, path, sctest.SingleNodeTestClusterFactory{})
+}
+
+func TestBackupRollbacks_base_alter_table_alter_column_type_trivial(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	const path = "pkg/sql/schemachanger/testdata/end_to_end/alter_table_alter_column_type_trivial"
+	sctest.BackupRollbacks(t, path, sctest.SingleNodeTestClusterFactory{})
+}
+
 func TestBackupRollbacks_base_alter_table_alter_primary_key_drop_rowid(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
@@ -645,6 +659,20 @@ func TestBackupRollbacksMixedVersion_base_alter_table_alter_column_set_not_null(
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 	const path = "pkg/sql/schemachanger/testdata/end_to_end/alter_table_alter_column_set_not_null"
+	sctest.BackupRollbacksMixedVersion(t, path, sctest.SingleNodeTestClusterFactory{})
+}
+
+func TestBackupRollbacksMixedVersion_base_alter_table_alter_column_type_noop(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	const path = "pkg/sql/schemachanger/testdata/end_to_end/alter_table_alter_column_type_noop"
+	sctest.BackupRollbacksMixedVersion(t, path, sctest.SingleNodeTestClusterFactory{})
+}
+
+func TestBackupRollbacksMixedVersion_base_alter_table_alter_column_type_trivial(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	const path = "pkg/sql/schemachanger/testdata/end_to_end/alter_table_alter_column_type_trivial"
 	sctest.BackupRollbacksMixedVersion(t, path, sctest.SingleNodeTestClusterFactory{})
 }
 
@@ -1131,6 +1159,20 @@ func TestBackupSuccess_base_alter_table_alter_column_set_not_null(t *testing.T) 
 	sctest.BackupSuccess(t, path, sctest.SingleNodeTestClusterFactory{})
 }
 
+func TestBackupSuccess_base_alter_table_alter_column_type_noop(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	const path = "pkg/sql/schemachanger/testdata/end_to_end/alter_table_alter_column_type_noop"
+	sctest.BackupSuccess(t, path, sctest.SingleNodeTestClusterFactory{})
+}
+
+func TestBackupSuccess_base_alter_table_alter_column_type_trivial(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	const path = "pkg/sql/schemachanger/testdata/end_to_end/alter_table_alter_column_type_trivial"
+	sctest.BackupSuccess(t, path, sctest.SingleNodeTestClusterFactory{})
+}
+
 func TestBackupSuccess_base_alter_table_alter_primary_key_drop_rowid(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
@@ -1611,6 +1653,20 @@ func TestBackupSuccessMixedVersion_base_alter_table_alter_column_set_not_null(t 
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 	const path = "pkg/sql/schemachanger/testdata/end_to_end/alter_table_alter_column_set_not_null"
+	sctest.BackupSuccessMixedVersion(t, path, sctest.SingleNodeTestClusterFactory{})
+}
+
+func TestBackupSuccessMixedVersion_base_alter_table_alter_column_type_noop(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	const path = "pkg/sql/schemachanger/testdata/end_to_end/alter_table_alter_column_type_noop"
+	sctest.BackupSuccessMixedVersion(t, path, sctest.SingleNodeTestClusterFactory{})
+}
+
+func TestBackupSuccessMixedVersion_base_alter_table_alter_column_type_trivial(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	const path = "pkg/sql/schemachanger/testdata/end_to_end/alter_table_alter_column_type_trivial"
 	sctest.BackupSuccessMixedVersion(t, path, sctest.SingleNodeTestClusterFactory{})
 }
 

pkg/sql/alter_column_type.go
@@ -16,7 +16,6 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/build"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
-	"github.com/cockroachdb/cockroach/pkg/sql/catalog/colinfo"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/schemaexpr"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/tabledesc"
@@ -27,7 +26,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/cast"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/volatility"
-	"github.com/cockroachdb/cockroach/pkg/sql/sqlerrors"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util/errorutil/unimplemented"
 	"github.com/cockroachdb/errors"
@@ -65,6 +63,8 @@ func AlterColumnType(
 	cmds tree.AlterTableCmds,
 	tn *tree.TableName,
 ) error {
+	objType := "column"
+	op := "alter type of"
 	for _, tableRef := range tableDesc.DependedOnBy {
 		found := false
 		for _, colID := range tableRef.ColumnIDs {
@@ -74,14 +74,14 @@ func AlterColumnType(
 		}
 		if found {
 			return params.p.dependentError(
-				ctx, "column", col.GetName(), tableDesc.ParentID, tableRef.ID, "alter type of",
+				ctx, objType, col.GetName(), tableDesc.ParentID, tableRef.ID, op,
 			)
 		}
 	}
 	if err := schemaexpr.ValidateTTLExpressionDoesNotDependOnColumn(tableDesc, tableDesc.GetRowLevelTTL(), col); err != nil {
 		return err
 	}
-	if err := schemaexpr.ValidateComputedColumnExpressionDoesNotDependOnColumn(tableDesc, col); err != nil {
+	if err := schemaexpr.ValidateComputedColumnExpressionDoesNotDependOnColumn(tableDesc, col, objType, op); err != nil {
 		return err
 	}
 
@@ -90,39 +90,15 @@ func AlterColumnType(
 		return err
 	}
 
-	// Special handling for STRING COLLATE xy to verify that we recognize the language.
-	if t.Collation != "" {
-		if types.IsStringType(typ) {
-			typ = types.MakeCollatedString(typ, t.Collation)
-		} else {
-			return pgerror.New(pgcode.Syntax, "COLLATE can only be used with string types")
-		}
-	}
-
-	// Special handling for IDENTITY column to make sure it cannot be altered into
-	// a non-integer type.
-	if col.IsGeneratedAsIdentity() {
-		if typ.InternalType.Family != types.IntFamily {
-			return sqlerrors.NewIdentityColumnTypeError()
-		}
-	}
-
-	err = colinfo.ValidateColumnDefType(ctx, params.EvalContext().Settings, typ)
+	typ, err = schemachange.ValidateAlterColumnTypeChecks(ctx, t,
+		params.EvalContext().Settings, typ, col.IsGeneratedAsIdentity())
 	if err != nil {
 		return err
 	}
 
-	var kind schemachange.ColumnConversionKind
-	if t.Using != nil {
-		// If an expression is provided, we always need to try a general conversion.
-		// We have to follow the process to create a new column and backfill it
-		// using the expression.
-		kind = schemachange.ColumnConversionGeneral
-	} else {
-		kind, err = schemachange.ClassifyConversion(ctx, col.GetType(), typ)
-		if err != nil {
-			return err
-		}
+	kind, err := schemachange.ClassifyConversionFromTree(ctx, t, col.GetType(), typ)
+	if err != nil {
+		return err
 	}
 
 	switch kind {

pkg/sql/alter_table.go
@@ -1905,7 +1905,8 @@ func dropColumnImpl(
 
 			if colIDs.Contains(colToDrop.GetID()) {
 				containsThisColumn = true
-				return nil, sqlerrors.NewColumnReferencedByPartialIndex(string(colToDrop.ColName()), idx.GetName())
+				return nil, sqlerrors.ColumnReferencedByPartialIndex(
+					"drop", "column", string(colToDrop.ColName()), idx.GetName())
 			}
 		}
 		// Perform the DROP.
@@ -1948,7 +1949,8 @@ func dropColumnImpl(
 			}
 
 			if colIDs.Contains(colToDrop.GetID()) {
-				return nil, sqlerrors.NewColumnReferencedByPartialUniqueWithoutIndexConstraint(string(colToDrop.ColName()), uwoi.GetName())
+				return nil, sqlerrors.ColumnReferencedByPartialUniqueWithoutIndexConstraint(
+					"drop", "column", string(colToDrop.ColName()), uwoi.GetName())
 			}
 		}
 		if uwoi.Dropped() || !uwoi.CollectKeyColumnIDs().Contains(colToDrop.GetID()) {

pkg/sql/catalog/schemaexpr/expr.go
@@ -492,7 +492,7 @@ func ValidateTTLExpressionDoesNotDependOnColumn(
 // ValidateComputedColumnExpressionDoesNotDependOnColumn verifies that the
 // expression of a computed column does not depend on the given column.
 func ValidateComputedColumnExpressionDoesNotDependOnColumn(
-	tableDesc catalog.TableDescriptor, dependentCol catalog.Column,
+	tableDesc catalog.TableDescriptor, dependentCol catalog.Column, objType, op string,
 ) error {
 	for _, col := range tableDesc.AllColumns() {
 		if dependentCol.GetID() == col.GetID() {
@@ -502,7 +502,7 @@ func ValidateComputedColumnExpressionDoesNotDependOnColumn(
 			if hasRef, err := validateExpressionDoesNotDependOnColumn(tableDesc, col.GetComputeExpr(), dependentCol.GetID()); err != nil {
 				return err
 			} else if hasRef {
-				return sqlerrors.NewDependentBlocksOpError("alter", "column",
+				return sqlerrors.NewDependentBlocksOpError(op, objType,
 					string(dependentCol.ColName()), "computed column", string(col.ColName()))
 			}
 		}

pkg/sql/logictest/testdata/logic_test/alter_column_type
@@ -593,7 +593,7 @@ ALTER TABLE t30 ALTER COLUMN x TYPE BYTES USING x::BYTES
 statement ok
 CREATE VIEW v AS SELECT x FROM t29
 
-statement error cannot alter type of column "x" because view "v" depends on it
+statement error cannot alter type of column "x" because view "v" depends on it\nHINT: consider dropping "v" first.
 ALTER TABLE t29 ALTER COLUMN x TYPE INT2
 
 statement ok
@@ -647,13 +647,13 @@ statement ok
 CREATE TABLE tab_w_computed (real1 int, real2 int, comp1 bigint as (real1 + real2) stored, comp2 bigint as (real2 * 2) virtual, comp3 int as (real1 * 2) virtual);
 INSERT INTO tab_w_computed (real1, real2) VALUES (1, 2), (3, 4), (5, 6);
 
-statement error pq: cannot alter column "real1" because computed column "comp[0-9]" depends on it\nHINT: consider dropping "comp[0-9]" first.
+statement error pq: cannot alter type of column "real1" because computed column "comp[0-9]" depends on it\nHINT: consider dropping "comp[0-9]" first.
 ALTER TABLE tab_w_computed ALTER COLUMN real1 TYPE bigint;
 
 statement ok
 ALTER TABLE tab_w_computed DROP COLUMN comp1;
 
-statement error pq: cannot alter column "real1" because computed column "comp[0-9]" depends on it\nHINT: consider dropping "comp[0-9]" first.
+statement error pq: cannot alter type of column "real1" because computed column "comp[0-9]" depends on it\nHINT: consider dropping "comp[0-9]" first.
 ALTER TABLE tab_w_computed ALTER COLUMN real1 TYPE bigint;
 
 statement ok
@@ -662,7 +662,7 @@ ALTER TABLE tab_w_computed DROP COLUMN comp3;
 statement ok
 ALTER TABLE tab_w_computed ALTER COLUMN real1 TYPE bigint;
 
-statement error pq: cannot alter column "real2" because computed column "comp[0-9]" depends on it\nHINT: consider dropping "comp[0-9]" first.
+statement error pq: cannot alter type of column "real2" because computed column "comp[0-9]" depends on it\nHINT: consider dropping "comp[0-9]" first.
 ALTER TABLE tab_w_computed ALTER COLUMN real2 TYPE string;
 
 statement ok
@@ -828,4 +828,22 @@ t_with_default  CREATE TABLE public.t_with_default (
 statement ok
 DROP TABLE t_with_default;
 
+subtest dep_function
+
+statement ok
+CREATE TABLE T_FOR_FUNCTION (C1 INT, C2 INT);
+
+statement ok
+CREATE OR REPLACE FUNCTION F1() RETURNS INT AS 'SELECT C2 FROM T_FOR_FUNCTION' LANGUAGE SQL;
+
+statement error pq: cannot alter type of column "c2" because function "f1" depends on it\nHINT: consider dropping "f1" first.
+ALTER TABLE T_FOR_FUNCTION ALTER COLUMN C2 SET DATA TYPE TEXT;
+
+statement ok
+DROP FUNCTION F1;
+
+statement ok
+DROP TABLE T_FOR_FUNCTION;
+
 subtest end
+

pkg/sql/logictest/testdata/logic_test/partial_index
@@ -205,7 +205,7 @@ CREATE TABLE t8 (
 )
 
 # TODO(mgartner): Lift this restriction. See #96924.
-statement error column "c" cannot be dropped because it is referenced by partial index "t8_a_idx1"
+statement error cannot drop column "c" because it is referenced by partial index "t8_a_idx1"\nHINT: drop the partial index first, then drop the column
 ALTER TABLE t8 DROP COLUMN c
 
 query TT
@@ -1975,7 +1975,7 @@ CREATE TABLE t96924 (
 statement ok
 ALTER TABLE t96924 DROP COLUMN a
 
-statement error pq: column "b" cannot be dropped because it is referenced by partial index "t96924_b_key"
+statement error pq: cannot drop column "b" because it is referenced by partial index "t96924_b_key"\nHINT: drop the partial index first, then drop the column
 ALTER TABLE t96924 DROP COLUMN b
 
 statement ok
@@ -1984,7 +1984,7 @@ DROP INDEX t96924_b_key CASCADE
 statement ok
 ALTER TABLE t96924 DROP COLUMN b
 
-statement error pq: column "c" cannot be dropped because it is referenced by partial index "t96924_c_key"
+statement error pq: cannot drop column "c" because it is referenced by partial index "t96924_c_key"\nHINT: drop the partial index first, then drop the column
 ALTER TABLE t96924 DROP COLUMN c
 
 statement ok
@@ -1993,7 +1993,7 @@ DROP INDEX t96924_c_key CASCADE
 statement ok
 ALTER TABLE t96924 DROP COLUMN c
 
-statement error pq: column "d" cannot be dropped because it is referenced by partial unique constraint "unique_d"
+statement error pq: cannot drop column "d" because it is referenced by partial unique constraint "unique_d"\nHINT: drop the unique constraint first, then drop the column
 ALTER TABLE t96924 DROP COLUMN d
 
 statement ok
@@ -2002,7 +2002,7 @@ ALTER TABLE t96924 DROP CONSTRAINT unique_d
 statement ok
 ALTER TABLE t96924 DROP COLUMN d
 
-statement error pq: column "e" cannot be dropped because it is referenced by partial index "t96924_e_f_idx"
+statement error pq: cannot drop column "e" because it is referenced by partial index "t96924_e_f_idx"\nHINT: drop the partial index first, then drop the column
 ALTER TABLE t96924 DROP COLUMN e
 
 statement ok
@@ -2018,7 +2018,7 @@ ALTER TABLE t96924 DROP COLUMN f
 
 # Column `h` is used in the predicate of another index that does not key on `h`,
 # we will prevent dropping column `h` as well.
-statement error pq: column "h" cannot be dropped because it is referenced by partial index "t96924_g_idx"
+statement error pq: cannot drop column "h" because it is referenced by partial index "t96924_g_idx"\nHINT: drop the partial index first, then drop the column
 ALTER TABLE t96924 DROP COLUMN h
 
 statement ok
@@ -2029,7 +2029,7 @@ ALTER TABLE t96924 DROP COLUMN h
 
 # Similarly, column `i` cannot be dropped since it's used in the predicate of
 # of a UWI constraint.
-statement error pq: column "i" cannot be dropped because it is referenced by partial unique constraint "unique_g"
+statement error pq: cannot drop column "i" because it is referenced by partial unique constraint "unique_g"\nHINT: drop the unique constraint first, then drop the column
 ALTER TABLE t96924 DROP COLUMN i
 
 statement ok
@@ -2054,7 +2054,7 @@ statement ok
 CREATE INDEX t96924_idx_1 ON t96924(a) WHERE (rowid > 0);
 
 skipif config local-legacy-schema-changer
-statement error column "rowid" cannot be dropped because it is referenced by partial index "t96924_idx_1"
+statement error cannot drop column "rowid" because it is referenced by partial index "t96924_idx_1"\nHINT: drop the partial index first, then drop the column
 ALTER TABLE t96924 ALTER PRIMARY KEY USING COLUMNS (a);
 
 statement ok

pkg/sql/schemachange/BUILD.bazel
@@ -10,9 +10,12 @@ go_library(
     importpath = "github.com/cockroachdb/cockroach/pkg/sql/schemachange",
     visibility = ["//visibility:public"],
     deps = [
+        "//pkg/settings/cluster",
+        "//pkg/sql/catalog/colinfo",
         "//pkg/sql/pgwire/pgcode",
         "//pkg/sql/pgwire/pgerror",
         "//pkg/sql/sem/tree",
+        "//pkg/sql/sqlerrors",
         "//pkg/sql/types",
     ],
 )

commit b80d045ea51852b47a71706f5383513b48eb01c6

sql/schemachange: Fix drop database degradation
This fixes a recent issue with dropping databases introduced in commit
52c12c0. That change added initial DSC support for ALTER TABLE ALTER
COLUMN ... TYPE operations. It was modeled in the DSC elements as a drop
followed by an add of ColumnType elements. ColumnType was an existing
element with its own rules for dropping a column, table, or database,
and would emit the RemoveDroppedColumnType operation. However, I didn't
want to emit this when altering the type of the column.

To achieve this, I check all the targets to see if a ColumnType is being
added for the same column. This can be quite expensive, especially for a
large database, because for each column being removed, it loops through
all the elements involved in the operation to see if we are also adding
a ColumnType of the same type.

Fortunately, RemoveDroppedColumnType is only emitted when removing a
column that references a user-defined type, which is rare. So, I
optimized the checks by first verifying if we are removing a column that
references type IDs, which is relatively cheap to detect, and then
checking if it’s a column that we are altering.

No release note is required for this because the degradation only went
in a week ago.

Closes: #127747
Release note: None

pkg/sql/schemachanger/scplan/internal/opgen/opgen_column_type.go
@@ -46,13 +46,13 @@ func init() {
 			to(scpb.Status_ABSENT,
 				revertible(false),
 				emit(func(this *scpb.ColumnType, md *opGenContext) *scop.RemoveDroppedColumnType {
-					// Make this a no-op if the column type already exists. The
-					// RemoveDroppedColumnType op is meant to be emitted only for columns
-					// that were dropped.
-					if checkIfColumnTypeExists(this.TableID, this.ColumnID, md) {
-						return nil
-					}
 					if ids := referencedTypeIDs(this); len(ids) > 0 {
+						// Make this a no-op if the column type already exists. The
+						// RemoveDroppedColumnType op is meant to be emitted only for columns
+						// that were dropped.
+						if checkIfColumnTypeExists(this.TableID, this.ColumnID, md) {
+							return nil
+						}
 						return &scop.RemoveDroppedColumnType{
 							TableID:  this.TableID,
 							ColumnID: this.ColumnID,