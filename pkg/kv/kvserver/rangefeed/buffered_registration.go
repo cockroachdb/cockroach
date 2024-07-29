bef5e37e1a20c1cb11442feab5be23f7f2c16b02
Wenyi Hu <wenyi@cockroachlabs.com>
2024-07-28 21:51:47 -0400

    a

--- pkg/kv/kvserver/rangefeed/buffered_registration.go
+++ pkg/kv/kvserver/rangefeed/buffered_registration.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The Cockroach Authors.
+// Copyright 2024 The Cockroach Authors.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt.
