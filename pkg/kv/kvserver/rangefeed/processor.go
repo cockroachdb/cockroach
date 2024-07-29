8119a4bb3cf4f39872a48d5b8434862532613b63
Wenyi Hu <wenyi@cockroachlabs.com>
2024-07-29 09:45:13 -0400

    kvserver/rangefeed: rename registration to buffered registration
    This patch renames registration to buffered registration. In a future commit, we
    will introduce another type of registration - nonbuffered registration. This is
    to distinguish them.
    
    Part of: #126560
    Release note: none

--- pkg/kv/kvserver/rangefeed/processor.go
+++ pkg/kv/kvserver/rangefeed/processor.go
@@ -264,8 +264,8 @@ type LegacyProcessor struct {
 	reg registry
 	rts resolvedTimestamp
 
-	regC       chan *registration
-	unregC     chan *registration
+	regC       chan *bufferedRegistration
+	unregC     chan *bufferedRegistration
 	lenReqC    chan struct{}
 	lenResC    chan int
 	filterReqC chan struct{}
@@ -349,8 +349,8 @@ func NewLegacyProcessor(cfg Config) *LegacyProcessor {
 		reg:    makeRegistry(cfg.Metrics),
 		rts:    makeResolvedTimestamp(cfg.Settings),
 
-		regC:       make(chan *registration),
-		unregC:     make(chan *registration),
+		regC:       make(chan *bufferedRegistration),
+		unregC:     make(chan *bufferedRegistration),
 		lenReqC:    make(chan struct{}),
 		lenResC:    make(chan int),
 		filterReqC: make(chan struct{}),
@@ -437,7 +437,7 @@ func (p *LegacyProcessor) run(
 		// Handle new registrations.
 		case r := <-p.regC:
 			if !p.Span.AsRawSpanWithNoLocals().Contains(r.span) {
-				log.Fatalf(ctx, "registration %s not in Processor's key range %v", r, p.Span)
+				log.Fatalf(ctx, "bufferedRegistration %s not in Processor's key range %v", r, p.Span)
 			}
 
 			// Add the new registration to the registry.
@@ -600,7 +600,7 @@ func (p *LegacyProcessor) Register(
 	p.syncEventC()
 
 	blockWhenFull := p.Config.EventChanTimeout == 0 // for testing
-	r := newRegistration(
+	r := newBufferedRegistration(
 		span.AsRawSpanWithNoLocals(), startTS, catchUpIter, withDiff, withFiltering, withOmitRemote,
 		p.Config.EventChanCap, blockWhenFull, p.Metrics, stream, disconnectFn,
 	)
