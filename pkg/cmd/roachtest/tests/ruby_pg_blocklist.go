b9c52873f95f2c72b201737fccde09dab481c757
Annie Pompa <annie@cockroachlabs.com>
2024-07-30 15:17:14 -0400

    roachtest: ignore 2 flaky ruby-pg tests
    Informs: #127612
    Release note: None

--- pkg/cmd/roachtest/tests/ruby_pg_blocklist.go
+++ pkg/cmd/roachtest/tests/ruby_pg_blocklist.go
@@ -204,6 +204,8 @@ var rubyPGIgnorelist = blocklist{
 	`PG::Connection handles server close while asynchronous connect`:                                                                                                               "flaky",
 	`PG::Connection OS thread support Connection.new shouldn't block a second thread`:                                                                                              "flaky",
 	`PG::Connection multinationalization support respect and convert character encoding of input strings should convert error string to #put_copy_end`:                             "unknown",
+	`PG::Connection in nonblocking mode rejects to send lots of COPY data`:                                                                                                         "flaky",
+	`running with sync_* methods PG::Connection in nonblocking mode rejects to send lots of COPY data`:                                                                             "flaky",
 	`running with sync_* methods PG::Connection consume_input should raise ConnectionBad for a closed connection`:                                                                  "flaky",
 	`running with sync_* methods PG::Connection OS thread support Connection.new shouldn't block a second thread`:                                                                  "flaky",
 	`running with sync_* methods PG::Connection handles server close while asynchronous connect`:                                                                                   "flaky",
