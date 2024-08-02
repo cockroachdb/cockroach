6d3f2e28fe4719523c2d906599183555fece233d
Austen McClernon <austen@cockroachlabs.com>
2024-08-02 13:18:17 -0400

    rac2: introduce send token watcher interface
    Introduce the `SendTokenWatcher` interface, which will be used to watch
    and be notified when there are elastic send tokens available. It is
    intended to be used when a send queue develops and the caller wishes to
    be notified of available elastic send tokens, in order to dequeue and
    send entries.
    
    `SendTokenWatcherProvider` is also introduced, which will be used to
    access `SendTokenWatcher` for a given stream.
    
    Resolves: #128011
    Release note: None

--- pkg/kv/kvserver/kvflowcontrol/rac2/store_stream.go
+++ pkg/kv/kvserver/kvflowcontrol/rac2/store_stream.go
@@ -10,7 +10,11 @@
 
 package rac2
 
-import "github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvflowcontrol"
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvflowcontrol"
+)
 
 // StreamTokenCounterProvider is the interface for retrieving token counters
 // for a given stream.
@@ -22,3 +26,39 @@ type StreamTokenCounterProvider interface {
 	// Send returns the send token counter for the given stream.
 	Send(kvflowcontrol.Stream) TokenCounter
 }
+
+// SendTokenWatcherHandleID is a unique identifier for a handle that is
+// watching for available elastic send tokens on a stream.
+type SendTokenWatcherHandleID int64
+
+// SendTokenWatcherProvider is the interface for retrieving SendTokenWatchers.
+// It is intended to be used to retrieve a SendTokenWatcher when initializing a
+// replica send stream.
+//
+// TODO(kvoli): Consider de-interfacing if not necessary for testing.
+type SendTokenWatcherProvider interface {
+	// SendTokenWatcher returns a SendTokenWatcher for the given stream.
+	SendTokenWatcher(kvflowcontrol.Stream) SendTokenWatcher
+}
+
+// SendTokenWatcher is the interface for watching and waiting on available
+// elastic send tokens, for a stream. The watcher registers a notification,
+// which will be called when elastic tokens are available for the stream this
+// watcher is monitoring. Note only elastic tokens are watched as this is
+// intended to be used when a send queue exists.
+//
+// TODO(kvoli): Consider de-interfacing if not necessary for testing.
+type SendTokenWatcher interface {
+	// NotifyWhenAvailable queues up for elastic tokens for the given stream's
+	// send token counter. When elastic tokens are available, the provided
+	// TokenGrantNotification is called. It is the caller's responsibility to
+	// call CancelHandle when tokens are no longer needed, or when the caller is
+	// done.
+	NotifyWhenAvailable(TokenGrantNotification) SendTokenWatcherHandleID
+	// CancelHandle cancels the given handle, stopping it from being notified
+	// when tokens are available. CancelHandle should be called at most once.
+	CancelHandle(SendTokenWatcherHandleID)
+}
+
+// TokenGrantNotification is called when tokens are available.
+type TokenGrantNotification func(context.Context)
