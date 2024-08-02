c2b14c8f4fd50354e07d3e474a097f93ca12401e
Austen McClernon <austen@cockroachlabs.com>
2024-08-02 12:41:16 -0400

    rac2: introduce stream token counter provider interface
    Introduce the `StreamTokenCounterProvider` interface, which will be used
    by replication flow control v2 to access the `TokenCounter` for a given
    tenant, store stream.
    
    Note the original interface in the v2 prototype included two additional
    methods for metrics gathering which are omitted here.
    
    Resolves: #128010
    Release note: None

---
+++ pkg/kv/kvserver/kvflowcontrol/rac2/store_stream.go
@@ -0,0 +1,24 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package rac2
+
+import "github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvflowcontrol"
+
+// StreamTokenCounterProvider is the interface for retrieving token counters
+// for a given stream.
+//
+// TODO(kvoli): Consider de-interfacing if not necessary for testing.
+type StreamTokenCounterProvider interface {
+	// Eval returns the evaluation token counter for the given stream.
+	Eval(kvflowcontrol.Stream) TokenCounter
+	// Send returns the send token counter for the given stream.
+	Send(kvflowcontrol.Stream) TokenCounter
+}
