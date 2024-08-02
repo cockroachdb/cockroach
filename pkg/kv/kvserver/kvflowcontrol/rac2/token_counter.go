544b5323270a22c37f5d582d9d8fc2747e17b28f
Austen McClernon <austen@cockroachlabs.com>
2024-08-02 12:30:32 -0400

    kvflowcontrol: introduce token counter interface
    Introduce the `TokenCounter` interface, underneath a new package, `rac2`.
    The `TokenCounter` will be used by replication flow control v2 in a
    similar, but distinct manner from the token buckets in v1. Notably, the
    `TokenCounter` supports returning an encapsulated handle that may be
    used in waiting for available tokens, `TokenWaitingHandle`.
    
    Resolves: #128014
    Release note: None

---
+++ pkg/kv/kvserver/kvflowcontrol/rac2/token_counter.go
@@ -0,0 +1,59 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package rac2
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvflowcontrol"
+	"github.com/cockroachdb/cockroach/pkg/util/admission/admissionpb"
+)
+
+// TokenCounter is the interface for a token counter that can be used to deduct
+// and return flow control tokens. Additionally, it can be used to wait for
+// tokens to become available, and to check if tokens are available without
+// blocking.
+type TokenCounter interface {
+	// TokensAvailable returns true if tokens are available. If false, it returns
+	// a handle that may be uses for waiting for tokens to become available.
+	TokensAvailable(admissionpb.WorkClass) (available bool, tokenWaitingHandle TokenWaitingHandle)
+	// TryDeduct attempts to deduct flow tokens for the given work class. If
+	// there are no tokens available, 0 tokens are returned. When less than the
+	// requested token count is available, partial tokens are returned
+	// corresponding to this partial amount.
+	TryDeduct(
+		context.Context, admissionpb.WorkClass, kvflowcontrol.Tokens) kvflowcontrol.Tokens
+	// Deduct deducts (without blocking) flow tokens for the given work class. If
+	// there are not enough available tokens, the token counter will go into debt
+	// (negative available count) and still issue the requested number of tokens.
+	Deduct(context.Context, admissionpb.WorkClass, kvflowcontrol.Tokens)
+	// Return returns flow tokens for the given work class.
+	Return(context.Context, admissionpb.WorkClass, kvflowcontrol.Tokens)
+}
+
+// TokenWaitingHandle is the interface for waiting for positive tokens from a
+// token counter.
+type TokenWaitingHandle interface {
+	// WaitChannel is the channel that will be signaled if tokens are possibly
+	// available. If signaled, the caller must call
+	// TryDeductAndUnblockNextWaiter.
+	WaitChannel() <-chan struct{}
+	// TryDeductAndUnblockNextWaiter is called to deduct some tokens. The tokens
+	// parameter can be zero, when the waiter is only waiting for positive tokens
+	// (such as when waiting before eval). The tokens granted will be less than
+	// or equal to the requested tokens and deducted from the token counter.
+	// haveTokens is true iff there are tokens available after this grant. When
+	// the tokens argument is zero, granted will be zero, and haveTokens
+	// represents whether there were positive tokens. If the caller is
+	// unsatisfied with the return values, it can resume waiting using
+	// WaitChannel.
+	TryDeductAndUnblockNextWaiter(tokens kvflowcontrol.Tokens) (granted kvflowcontrol.Tokens, haveTokens bool)
+}
