a7cbcb48fe2c216a85776eafbdf256f9b7348d56
Austen McClernon <austen@cockroachlabs.com>
2024-08-02 13:18:17 -0400

    rac2: introduce stream token watcher interface
    Introduce the `StreamTokenWatcher` interface, which will be used to
    watch and be notified when there are tokens available.
    
    Resolves: #128011
    Release note: None

--- pkg/kv/kvserver/kvflowcontrol/rac2/store_stream.go
+++ pkg/kv/kvserver/kvflowcontrol/rac2/store_stream.go
@@ -10,7 +10,11 @@
 
 package rac2
 
-import "github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvflowcontrol"
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvflowcontrol"
+)
 
 // StreamTokenCounterProvider is the interface for retrieving token counters
 // for a given stream.
@@ -20,3 +24,29 @@ type StreamTokenCounterProvider interface {
 	// Send returns the send token counter for the given stream.
 	Send(kvflowcontrol.Stream) TokenCounter
 }
+
+// StreamWatcherHandleID is a unique identifier for a handle that is
+// watching for available tokens on a stream.
+type StreamWatcherHandleID int64
+
+// StreamTokenWatcher is the interface for watching and waiting on available
+// elastic tokens. The watcher registers a notification, which will be called
+// when elastic tokens are available for a given stream. Note only elastic
+// tokens are watched as this is intended to be used when a send queue exists.
+type StreamTokenWatcher interface {
+	// NotifyWhenAvailable queues up for elastic tokens for the given token
+	// counter. When elastic tokens are available, the provided
+	// TokenGrantNotification is called. It is the caller's responsibility to
+	// call CancelHandle when tokens are no longer needed, or when the caller is
+	// done.
+	NotifyWhenAvailable(
+		TokenCounter,
+		TokenGrantNotification,
+	) StreamWatcherHandleID
+	// CancelHandle cancels the given handle, stopping it from being notified
+	// when tokens are available.
+	CancelHandle(StreamWatcherHandleID)
+}
+
+// TokenGrantNotification is called when tokens are available.
+type TokenGrantNotification func(context.Context)
