35e519d774869c3bc8293d6e6273156271648030
Michael Erickson <michae2@cockroachlabs.com>
2024-08-02 11:29:08 -0700

    logictestccl: deflake select_for_update_read_committed
    This commit fixes an oversight in the original testcase added for #127718.
    The initial `SELECT * FROM xyz WHERE x = 1 FOR UPDATE` run under
    serializable isolation was sometimes using a different plan that caused
    it to lock more than one row. I needed to turn on
    `optimizer_use_lock_op_for_serializable` for the testuser connection,
    too, to ensure the first SELECT only locks a single row.
    
    Also change `opt.Locking.IsNonZeroLocking` to `opt.Locking.IsNoOp` which
    is more meaningful.
    
    Fixes: #128177
    
    Release note: None

--- pkg/sql/opt/memo/expr_format.go
+++ pkg/sql/opt/memo/expr_format.go
@@ -1642,7 +1642,7 @@ func (f *ExprFmtCtx) formatLocking(tp treeprinter.Node, locking opt.Locking) {
 func (f *ExprFmtCtx) formatLockingWithPrefix(
 	tp treeprinter.Node, labelPrefix string, locking opt.Locking,
 ) {
-	if !locking.IsNonZeroLocking() {
+	if locking.IsNoOp() {
 		return
 	}
 	strength := ""
