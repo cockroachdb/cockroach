b92e38a8ec5ff7ccef9d4e932ac6a48e2a2bd993
sumeerbhola <sumeer@cockroachlabs.com>
2024-08-05 11:47:24 -0400

    raftpb: add Priority for use in RACv2
    Informs #123509
    
    Epic: none
    
    Release note: None

--- pkg/raft/raftpb/raft.go
+++ pkg/raft/raftpb/raft.go
@@ -10,6 +10,8 @@
 
 package raftpb
 
+import "github.com/cockroachdb/redact"
+
 // PeerID is a custom type for peer IDs in a raft group.
 type PeerID uint64
 
@@ -22,3 +24,36 @@ type Epoch int64
 
 // SafeValue implements the redact.SafeValue interface.
 func (e Epoch) SafeValue() {}
+
+// Priority is used for replication admission control v2 (RAC2), where Raft
+// tracks admitted values per priority, and provides liveness by pinging when
+// the admitted values are lagging.
+type Priority uint8
+
+const (
+	LowPri Priority = iota
+	NormalPri
+	AboveNormalPri
+	HighPri
+	NumPriorities
+)
+
+func (p Priority) String() string {
+	return redact.StringWithoutMarkers(p)
+}
+
+// SafeFormat implements the redact.SafeFormatter interface.
+func (p Priority) SafeFormat(w redact.SafePrinter, _ rune) {
+	switch p {
+	case LowPri:
+		w.Printf("LowPri")
+	case NormalPri:
+		w.Printf("NormalPri")
+	case AboveNormalPri:
+		w.Printf("AboveNormalPri")
+	case HighPri:
+		w.Printf("HighPri")
+	default:
+		panic("invalid raft priority")
+	}
+}
