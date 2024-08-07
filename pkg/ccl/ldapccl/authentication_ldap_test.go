062e1dc4f22a7f6622960d9fcc43e8f018fdbf82
souravcrl <sourav.sarangi@cockroachlabs.com>
2024-08-08 01:37:00 +0530

    ccl, sql: add authZ handler for ldap groups retrieval
    informs #125087
    fixes CRDB-39227
    Epic CRDB-33829
    
    This PR adds support for fetching LDAP groups info for sql user trying to gain
    authorization for cockroach cluster via LDAP. This change will enable us to
    assign appropriate role privileges for user on CRDB.
    
    Release note(enterprise, security): We add support for authorization to CRDB
    cluster via LDAP, retrieving AD groups membership information for LDAP user. The
    new HBA conf cluster setting option `ldapgrouplistfilter` performs filtered
    search query on LDAP for matching groups.
    
    Example hba conf entry to support LDAP authZ configuration:
    ```
     # TYPE    DATABASE      USER           ADDRESS             METHOD             OPTIONS
     # Allow all users to connect to using LDAP authentication with search and bind
       host    all           all            all                 ldap               ldapserver=ldap.example.com ldapport=636 "ldapbasedn=ou=users,dc=example,dc=com" "ldapbinddn=cn=readonly,dc=example,dc=com" ldapbindpasswd=readonly_password ldapsearchattribute=uid "ldapsearchfilter=(memberof=cn=cockroachdb_users,ou=groups,dc=example,dc=com)" "ldapgrouplistfilter=(objectClass=groupOfNames)"
     # Fallback to password authentication for the root user
       host    all           root           0.0.0.0/0          password
    ```
    Example to use for azure AD server:
    ```
    SET cluster setting server.host_based_authentication.configuration = 'host    all           all            all                 ldap ldapserver=azure.dev ldapport=636 "ldapbasedn=OU=AADDC Users,DC=azure,DC=dev" "ldapbinddn=CN=Some User,OU=AADDC Users,DC=azure,DC=dev" ldapbindpasswd=my_pwd ldapsearchattribute=sAMAccountName "ldapsearchfilter=(memberOf=CN=azure-dev-domain-sync-users,OU=AADDC Users,DC=crlcloud,DC=dev)" "ldapgrouplistfilter=(objectCategory=CN=Group,CN=Schema,CN=Configuration,DC=crlcloud,DC=dev)"
    host    all           root           0.0.0.0/0          password';
    ```
    
    Post configuration CRDB cluster should be able to authorize users via LDAP
    server if:
    1. Users LDAP authentication attempt is successful, and it has the user's DN for
    LDAP server.
    2. `ldapgrouplistfilter` is properly configured, and it successfully syncs
    groups of the user.

--- pkg/ccl/ldapccl/authentication_ldap_test.go
+++ pkg/ccl/ldapccl/authentication_ldap_test.go
@@ -12,117 +12,18 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/security/username"
-	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/hba"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/errors"
 	"github.com/cockroachdb/redact"
-	"github.com/go-ldap/ldap/v3"
 	"github.com/stretchr/testify/require"
 )
-
-const (
-	emptyParam   = "empty"
-	invalidParam = "invalid"
-)
-
-type mockLDAPUtil struct {
-	conn      *ldap.Conn
-	tlsConfig *tls.Config
-}
-
-// InitLDAPsConn implements the ILDAPUtil interface.
-func (lu *mockLDAPUtil) InitLDAPsConn(ctx context.Context, conf ldapAuthenticatorConf) error {
-	if strings.Contains(conf.ldapServer, invalidParam) {
-		return errors.Newf(ldapsFailureMessage + ": invalid ldap server provided")
-	} else if strings.Contains(conf.ldapPort, invalidParam) {
-		return errors.Newf(ldapsFailureMessage + ": invalid ldap port provided")
-	}
-	lu.conn = &ldap.Conn{}
-	return nil
-}
-
-// Bind implements the ILDAPUtil interface.
-func (lu *mockLDAPUtil) Bind(ctx context.Context, userDN string, ldapPwd string) error {
-	if strings.Contains(userDN, invalidParam) {
-		return errors.Newf(bindFailureMessage + ": invalid username provided")
-	} else if strings.Contains(ldapPwd, invalidParam) {
-		return errors.Newf(bindFailureMessage + ": invalid password provided")
-	}
-
-	return nil
-}
-
-// Search implements the ILDAPUtil interface.
-func (lu *mockLDAPUtil) Search(
-	ctx context.Context, conf ldapAuthenticatorConf, username string,
-) (userDN string, err error) {
-	if err := lu.Bind(ctx, conf.ldapBindDN, conf.ldapBindPassword); err != nil {
-		return "", errors.Wrap(err, searchFailureMessage)
-	}
-	if strings.Contains(conf.ldapBaseDN, invalidParam) {
-		return "", errors.Newf(searchFailureMessage+": invalid base DN %q provided", conf.ldapBaseDN)
-	}
-	if strings.Contains(conf.ldapSearchFilter, invalidParam) {
-		return "", errors.Newf(searchFailureMessage+": invalid search filter %q provided", conf.ldapSearchFilter)
-	}
-	if strings.Contains(conf.ldapSearchAttribute, invalidParam) {
-		return "", errors.Newf(searchFailureMessage+": invalid search attribute %q provided", conf.ldapSearchAttribute)
-	}
-	if strings.Contains(username, invalidParam) {
-		return "", errors.Newf(searchFailureMessage+": invalid search value %q provided", username)
-	}
-	distinguishedNames := strings.Split(username, ",")
-	switch {
-	case len(username) == 0:
-		return "", errors.Newf(searchFailureMessage+": user %q does not exist", username)
-	case len(distinguishedNames) > 1:
-		return "", errors.Newf(searchFailureMessage+": too many matching entries returned for user %q", username)
-	}
-	return distinguishedNames[0], nil
-}
-
-var _ ILDAPUtil = &mockLDAPUtil{}
-
-func constructHBAEntry(
-	t *testing.T,
-	hbaEntryBase string,
-	hbaConfLDAPDefaultOpts map[string]string,
-	hbaConfLDAPOpts map[string]string,
-) hba.Entry {
-	hbaEntryLDAP := hbaEntryBase
-	// add options from default and override default options when provided with one
-	for opt, value := range hbaConfLDAPDefaultOpts {
-		setValue := value
-		if hbaConfLDAPOpts[opt] == emptyParam {
-			continue
-		} else if hbaConfLDAPOpts[opt] != "" {
-			setValue = hbaConfLDAPOpts[opt]
-		}
-		hbaEntryLDAP += fmt.Sprintf("\"%s=%s\" ", opt, setValue)
-	}
-	// add non default options
-	for additionalOpt, additionalOptValue := range hbaConfLDAPOpts {
-		if _, ok := hbaConfLDAPDefaultOpts[additionalOpt]; !ok {
-			hbaEntryLDAP += fmt.Sprintf("\"%s=%s\" ", additionalOpt, additionalOptValue)
-		}
-	}
-	hbaConf, err := hba.ParseAndNormalize(hbaEntryLDAP)
-	if err != nil {
-		t.Fatalf("error parsing hba conf: %v", err)
-	}
-	if len(hbaConf.Entries) != 1 {
-		t.Fatalf("hba conf value invalid: should contain only 1 entry")
-	}
-	return hbaConf.Entries[0]
-}
 
 func TestLDAPAuthentication(t *testing.T) {
 	defer leaktest.AfterTest(t)()
@@ -130,13 +31,13 @@ func TestLDAPAuthentication(t *testing.T) {
 	// Intercept the call to NewLDAPUtil and return the mocked NewLDAPUtil function
 	defer testutils.TestingHook(
 		&NewLDAPUtil,
-		func(ctx context.Context, conf ldapAuthenticatorConf) (ILDAPUtil, error) {
+		func(ctx context.Context, conf ldapConfig) (ILDAPUtil, error) {
 			return &mockLDAPUtil{tlsConfig: &tls.Config{}}, nil
 		})()
 	ctx := context.Background()
 	s := serverutils.StartServerOnly(t, base.TestServerArgs{})
 	defer s.Stopper().Stop(ctx)
-	verifier := ConfigureLDAPAuth(ctx, s.AmbientCtx(), s.ClusterSettings(), s.StorageClusterID())
+	manager := ConfigureLDAPAuth(ctx, s.AmbientCtx(), s.ClusterSettings(), s.StorageClusterID())
 	hbaEntryBase := "host all all all ldap "
 	hbaConfLDAPDefaultOpts := map[string]string{
 		"ldapserver": "localhost", "ldapport": "636", "ldapbasedn": "dc=localhost", "ldapbinddn": "cn=readonly,dc=localhost",
@@ -167,24 +68,24 @@ func TestLDAPAuthentication(t *testing.T) {
 			expectedDetailedErrMsg: `error parsing hba conf options for LDAP: invalid LDAP option provided in hba conf: ‹invalidOpt›`},
 		{testName: "empty server",
 			hbaConfLDAPOpts: map[string]string{"ldapserver": emptyParam}, user: "foo", pwd: "bar", ldapAuthSuccess: false,
-			expectedErr:            "LDAP authentication: unable to validate authenticator options",
-			expectedDetailedErrMsg: "error validating hba conf options for LDAP: ldap params in HBA conf missing ldap server"},
+			expectedErr:            "LDAP authentication: unable to validate authManager base options",
+			expectedDetailedErrMsg: "error validating base hba conf options for LDAP: ldap params in HBA conf missing ldap server"},
 		{testName: "invalid server",
 			hbaConfLDAPOpts: map[string]string{"ldapserver": invalidParam}, user: "foo", pwd: "bar", ldapAuthSuccess: false,
 			expectedErr:            "LDAP authentication: unable to establish LDAP connection",
 			expectedDetailedErrMsg: "error when trying to create LDAP connection: LDAPs connection failed: invalid ldap server provided"},
 		{testName: "empty port",
 			hbaConfLDAPOpts: map[string]string{"ldapport": emptyParam}, user: "foo", pwd: "bar", ldapAuthSuccess: false,
-			expectedErr:            "LDAP authentication: unable to validate authenticator options",
-			expectedDetailedErrMsg: "error validating hba conf options for LDAP: ldap params in HBA conf missing ldap port"},
+			expectedErr:            "LDAP authentication: unable to validate authManager base options",
+			expectedDetailedErrMsg: "error validating base hba conf options for LDAP: ldap params in HBA conf missing ldap port"},
 		{testName: "invalid port",
 			hbaConfLDAPOpts: map[string]string{"ldapport": invalidParam}, user: "foo", pwd: "bar", ldapAuthSuccess: false,
 			expectedErr:            "LDAP authentication: unable to establish LDAP connection",
 			expectedDetailedErrMsg: "error when trying to create LDAP connection: LDAPs connection failed: invalid ldap port provided"},
 		{testName: "empty base dn",
 			hbaConfLDAPOpts: map[string]string{"ldapbasedn": emptyParam}, user: "foo", pwd: "bar", ldapAuthSuccess: false,
-			expectedErr:            "LDAP authentication: unable to validate authenticator options",
-			expectedDetailedErrMsg: "error validating hba conf options for LDAP: ldap params in HBA conf missing base DN"},
+			expectedErr:            "LDAP authentication: unable to validate authManager base options",
+			expectedDetailedErrMsg: "error validating base hba conf options for LDAP: ldap params in HBA conf missing base DN"},
 		{testName: "invalid base dn",
 			hbaConfLDAPOpts: map[string]string{"ldapbasedn": invalidParam}, user: "foo", pwd: "bar", ldapAuthSuccess: false,
 			expectedErr:            "LDAP authentication: unable to find LDAP user distinguished name",
@@ -192,8 +93,8 @@ func TestLDAPAuthentication(t *testing.T) {
 			expectedDetailedErrMsg: `error when searching for user in LDAP server: LDAP search failed: invalid base DN ‹"invalid"› provided`},
 		{testName: "empty bind dn",
 			hbaConfLDAPOpts: map[string]string{"ldapbinddn": emptyParam}, user: "foo", pwd: "bar", ldapAuthSuccess: false,
-			expectedErr:            "LDAP authentication: unable to validate authenticator options",
-			expectedDetailedErrMsg: "error validating hba conf options for LDAP: ldap params in HBA conf missing bind DN"},
+			expectedErr:            "LDAP authentication: unable to validate authManager base options",
+			expectedDetailedErrMsg: "error validating base hba conf options for LDAP: ldap params in HBA conf missing bind DN"},
 		{testName: "invalid bind dn",
 			hbaConfLDAPOpts: map[string]string{"ldapbinddn": invalidParam}, user: "foo", pwd: "bar", ldapAuthSuccess: false,
 			expectedErr:            "LDAP authentication: unable to find LDAP user distinguished name",
@@ -201,8 +102,8 @@ func TestLDAPAuthentication(t *testing.T) {
 			expectedDetailedErrMsg: "error when searching for user in LDAP server: LDAP search failed: LDAP bind failed: invalid username provided"},
 		{testName: "empty bind pwd",
 			hbaConfLDAPOpts: map[string]string{"ldapbindpasswd": emptyParam}, user: "foo", pwd: "bar", ldapAuthSuccess: false,
-			expectedErr:            "LDAP authentication: unable to validate authenticator options",
-			expectedDetailedErrMsg: "error validating hba conf options for LDAP: ldap params in HBA conf missing bind password"},
+			expectedErr:            "LDAP authentication: unable to validate authManager base options",
+			expectedDetailedErrMsg: "error validating base hba conf options for LDAP: ldap params in HBA conf missing bind password"},
 		{testName: "invalid bind pwd",
 			hbaConfLDAPOpts: map[string]string{"ldapbindpasswd": invalidParam}, user: "foo", pwd: "bar", ldapAuthSuccess: false,
 			expectedErr:            "LDAP authentication: unable to find LDAP user distinguished name",
@@ -210,8 +111,8 @@ func TestLDAPAuthentication(t *testing.T) {
 			expectedDetailedErrMsg: "error when searching for user in LDAP server: LDAP search failed: LDAP bind failed: invalid password provided"},
 		{testName: "empty search attribute",
 			hbaConfLDAPOpts: map[string]string{"ldapsearchattribute": emptyParam}, user: "foo", pwd: "bar", ldapAuthSuccess: false,
-			expectedErr:            "LDAP authentication: unable to validate authenticator options",
-			expectedDetailedErrMsg: "error validating hba conf options for LDAP: ldap params in HBA conf missing search attribute"},
+			expectedErr:            "LDAP authentication: unable to validate authManager authentication options",
+			expectedDetailedErrMsg: "error validating authentication hba conf options for LDAP: ldap authentication params in HBA conf missing search attribute"},
 		{testName: "invalid search attribute",
 			hbaConfLDAPOpts: map[string]string{"ldapsearchattribute": invalidParam}, user: "foo", pwd: "bar", ldapAuthSuccess: false,
 			expectedErr:            "LDAP authentication: unable to find LDAP user distinguished name",
@@ -219,8 +120,8 @@ func TestLDAPAuthentication(t *testing.T) {
 			expectedDetailedErrMsg: `error when searching for user in LDAP server: LDAP search failed: invalid search attribute ‹"invalid"› provided`},
 		{testName: "empty search filter",
 			hbaConfLDAPOpts: map[string]string{"ldapsearchfilter": emptyParam}, user: "foo", pwd: "bar", ldapAuthSuccess: false,
-			expectedErr:            "LDAP authentication: unable to validate authenticator options",
-			expectedDetailedErrMsg: "error validating hba conf options for LDAP: ldap params in HBA conf missing search filter"},
+			expectedErr:            "LDAP authentication: unable to validate authManager authentication options",
+			expectedDetailedErrMsg: "error validating authentication hba conf options for LDAP: ldap authentication params in HBA conf missing search filter"},
 		{testName: "invalid search filter",
 			hbaConfLDAPOpts: map[string]string{"ldapsearchfilter": invalidParam}, user: "foo", pwd: "bar", ldapAuthSuccess: false,
 			expectedErr:            "LDAP authentication: unable to find LDAP user distinguished name",
@@ -241,12 +142,12 @@ func TestLDAPAuthentication(t *testing.T) {
 		{testName: "invalid ldap password",
 			user: "foo", pwd: invalidParam, ldapAuthSuccess: false, expectedErr: "LDAP authentication: unable to bind as LDAP user",
 			expectedErrDetails:     "credentials invalid for LDAP server user foo",
-			expectedDetailedErrMsg: `error when binding as user ‹foo› with DN(‹foo›) in LDAP server: LDAP bind failed: invalid password provided`},
+			expectedDetailedErrMsg: `error when binding as user ‹foo› with DN(‹CN=foo›) in LDAP server: LDAP bind failed: invalid password provided`},
 	}
 	for i, tc := range testCases {
 		t.Run(fmt.Sprintf("%d: testName:%v hbConfOpts:%v user:%v password:%v", i, tc.testName, tc.hbaConfLDAPOpts, tc.user, tc.pwd), func(t *testing.T) {
 			hbaEntry := constructHBAEntry(t, hbaEntryBase, hbaConfLDAPDefaultOpts, tc.hbaConfLDAPOpts)
-			detailedErrorMsg, err := verifier.ValidateLDAPLogin(
+			_, detailedErrorMsg, err := manager.ValidateLDAPLogin(
 				ctx, s.ClusterSettings(), username.MakeSQLUsernameFromPreNormalizedString(tc.user), tc.pwd, &hbaEntry, nil)
 
 			if (err == nil) != tc.ldapAuthSuccess {
---
+++ pkg/ccl/ldapccl/ldap_test_util.go
@@ -0,0 +1,143 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package ldapccl
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/hba"
+	"github.com/cockroachdb/errors"
+	"github.com/go-ldap/ldap/v3"
+)
+
+const (
+	emptyParam   = "empty"
+	invalidParam = "invalid"
+)
+
+type mockLDAPUtil struct {
+	conn      *ldap.Conn
+	tlsConfig *tls.Config
+}
+
+// MaybeInitLDAPsConn implements the ILDAPUtil interface.
+func (lu *mockLDAPUtil) MaybeInitLDAPsConn(ctx context.Context, conf ldapConfig) error {
+	if strings.Contains(conf.ldapServer, invalidParam) {
+		return errors.Newf(ldapsFailureMessage + ": invalid ldap server provided")
+	} else if strings.Contains(conf.ldapPort, invalidParam) {
+		return errors.Newf(ldapsFailureMessage + ": invalid ldap port provided")
+	}
+	lu.conn = &ldap.Conn{}
+	return nil
+}
+
+// Bind implements the ILDAPUtil interface.
+func (lu *mockLDAPUtil) Bind(ctx context.Context, userDN string, ldapPwd string) error {
+	if strings.Contains(userDN, invalidParam) {
+		return errors.Newf(bindFailureMessage + ": invalid username provided")
+	} else if strings.Contains(ldapPwd, invalidParam) {
+		return errors.Newf(bindFailureMessage + ": invalid password provided")
+	}
+
+	return nil
+}
+
+// Search implements the ILDAPUtil interface.
+func (lu *mockLDAPUtil) Search(
+	ctx context.Context, conf ldapConfig, username string,
+) (userDN string, err error) {
+	if err := lu.Bind(ctx, conf.ldapBindDN, conf.ldapBindPassword); err != nil {
+		return "", errors.Wrap(err, searchFailureMessage)
+	}
+	if strings.Contains(conf.ldapBaseDN, invalidParam) {
+		return "", errors.Newf(searchFailureMessage+": invalid base DN %q provided", conf.ldapBaseDN)
+	}
+	if strings.Contains(conf.ldapSearchFilter, invalidParam) {
+		return "", errors.Newf(searchFailureMessage+": invalid search filter %q provided", conf.ldapSearchFilter)
+	}
+	if strings.Contains(conf.ldapSearchAttribute, invalidParam) {
+		return "", errors.Newf(searchFailureMessage+": invalid search attribute %q provided", conf.ldapSearchAttribute)
+	}
+	if strings.Contains(username, invalidParam) {
+		return "", errors.Newf(searchFailureMessage+": invalid search value %q provided", username)
+	}
+	commonNames := strings.Split(username, ",")
+	switch {
+	case len(username) == 0:
+		return "", errors.Newf(searchFailureMessage+": user %q does not exist", username)
+	case len(commonNames) > 1:
+		return "", errors.Newf(searchFailureMessage+": too many matching entries returned for user %q", username)
+	}
+
+	distinguishedName := "CN=" + commonNames[0]
+	return distinguishedName, nil
+}
+
+// ListGroups implements the ILDAPUtil interface.
+func (lu *mockLDAPUtil) ListGroups(
+	ctx context.Context, conf ldapConfig, userDN string,
+) (ldapGroupsDN []string, err error) {
+	if err := lu.Bind(ctx, conf.ldapBindDN, conf.ldapBindPassword); err != nil {
+		return nil, errors.Wrap(err, groupListFailureMessage)
+	}
+	if strings.Contains(conf.ldapBaseDN, invalidParam) {
+		return nil, errors.Newf(groupListFailureMessage+": invalid base DN %q provided", conf.ldapBaseDN)
+	}
+	if strings.Contains(conf.ldapSearchFilter, invalidParam) {
+		return nil, errors.Newf(groupListFailureMessage+": invalid search filter %q provided", conf.ldapSearchFilter)
+	}
+	if strings.Contains(conf.ldapGroupListFilter, invalidParam) {
+		return nil, errors.Newf(groupListFailureMessage+": invalid group list filter %q provided", conf.ldapGroupListFilter)
+	}
+	if len(userDN) == 0 {
+		return nil, errors.Newf(groupListFailureMessage+": user dn %q does not belong to any groups", userDN)
+	}
+
+	ldapGroupsDN = strings.Split(userDN, ",")
+	return ldapGroupsDN, nil
+}
+
+var _ ILDAPUtil = &mockLDAPUtil{}
+
+func constructHBAEntry(
+	t *testing.T,
+	hbaEntryBase string,
+	hbaConfLDAPDefaultOpts map[string]string,
+	hbaConfLDAPOpts map[string]string,
+) hba.Entry {
+	hbaEntryLDAP := hbaEntryBase
+	// add options from default and override default options when provided with one
+	for opt, value := range hbaConfLDAPDefaultOpts {
+		setValue := value
+		if hbaConfLDAPOpts[opt] == emptyParam {
+			continue
+		} else if hbaConfLDAPOpts[opt] != "" {
+			setValue = hbaConfLDAPOpts[opt]
+		}
+		hbaEntryLDAP += fmt.Sprintf("\"%s=%s\" ", opt, setValue)
+	}
+	// add non default options
+	for additionalOpt, additionalOptValue := range hbaConfLDAPOpts {
+		if _, ok := hbaConfLDAPDefaultOpts[additionalOpt]; !ok {
+			hbaEntryLDAP += fmt.Sprintf("\"%s=%s\" ", additionalOpt, additionalOptValue)
+		}
+	}
+	hbaConf, err := hba.ParseAndNormalize(hbaEntryLDAP)
+	if err != nil {
+		t.Fatalf("error parsing hba conf: %v", err)
+	}
+	if len(hbaConf.Entries) != 1 {
+		t.Fatalf("hba conf value invalid: should contain only 1 entry")
+	}
+	return hbaConf.Entries[0]
+}
