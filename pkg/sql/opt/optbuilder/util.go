6dfd593f0796ad65bb9c69f07c956709b20f11a7
Drew Kimball <drewk@cockroachlabs.com>
2024-08-07 21:25:20 -0600

    opt: add support for CREATE TRIGGER in optimizer
    This commit finalizes support for `CREATE TRIGGER` by adding support
    for it in the optimizer. This includes validation for the statement,
    dependency resolution, and logic tests.
    
    Fixes #126359
    
    Release note (sql change): Added support for CREATE TRIGGER. Note that
    OR REPLACE syntax is not yet supported. Also note that triggers cannot
    yet be executed, so creation is currently a no-op.

--- pkg/sql/opt/optbuilder/util.go
+++ pkg/sql/opt/optbuilder/util.go
@@ -662,7 +662,7 @@ func (b *Builder) resolveDataSource(
 	tn *tree.TableName, priv privilege.Kind,
 ) (cat.DataSource, opt.MDDepName, cat.DataSourceName) {
 	var flags cat.Flags
-	if b.insideViewDef || b.insideFuncDef {
+	if b.insideViewDef || b.insideFuncDef || b.insideTriggerDef {
 		// Avoid taking descriptor leases when we're creating a view or a
 		// function.
 		flags.AvoidDescriptorCaches = true
@@ -690,7 +690,7 @@ func (b *Builder) resolveDataSourceRef(
 	ref *tree.TableRef, priv privilege.Kind,
 ) (cat.DataSource, opt.MDDepName) {
 	var flags cat.Flags
-	if b.insideViewDef || b.insideFuncDef {
+	if b.insideViewDef || b.insideFuncDef || b.insideTriggerDef {
 		// Avoid taking table leases when we're creating a view or a function.
 		flags.AvoidDescriptorCaches = true
 	}
