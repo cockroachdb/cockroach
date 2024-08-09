5248090d024416a209d6102b87c345f1d8085062
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2024-08-09 12:28:21 -0400

    kv: support gradual rollout of Leader leases
    Fixes #125237.
    
    This commit adds a pair of new cluster settings:
    * kv.raft.leader_fortification.fraction_enabled
    * kv.leases.leader_leases.enabled
    
    The first is a public setting which controls the fraction of ranges for which
    the raft leader fortification protocol is enabled. Leader fortification is
    needed for a range to use a Leader lease. It can be set to 0.0 to disable leader
    fortification and, by extension, Leader leases. It can be set to 1.0 to enable
    leader fortification for all ranges and, by extension, use Leader leases for all
    ranges which do not require expiration-based leases. It can be set to a value
    between 0.0 and 1.0 to gradually roll out Leader leases across the ranges in a
    cluster.
    
    The second is a private setting which controls whether leader leases are enabled
    for range which use the raft leader fortification protocol. The setting is
    private because most users should interact with
    kv.raft.leader_fortification.fraction_enabled instead. The setting is only
    available for rare cases where an operator wants to disable leader leases
    without disabling the raft leader fortification.
    
    Release note: None

---
+++ pkg/kv/kvserver/replica_store_liveness_test.go
@@ -0,0 +1,92 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package kvserver
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRaftFortificationEnabledForRangeID(t *testing.T) {
+	// Ensure fracEnabled=0.0 never returns true.
+	t.Run("fracEnabled=0.0", func(t *testing.T) {
+		for i := range 10_000 {
+			require.False(t, raftFortificationEnabledForRangeID(0.0, roachpb.RangeID(i)))
+		}
+	})
+
+	// Ensure fracEnabled=1.0 always returns true.
+	t.Run("fracEnabled=1.0", func(t *testing.T) {
+		for i := range 10_000 {
+			require.True(t, raftFortificationEnabledForRangeID(1.0, roachpb.RangeID(i)))
+		}
+	})
+
+	// Test some specific cases with partially enabled fortification.
+	testCases := []struct {
+		fracEnabled float64
+		rangeID     roachpb.RangeID
+		exp         bool
+	}{
+		// 25% enabled.
+		{0.25, 1, false},
+		{0.25, 2, true},
+		{0.25, 3, false},
+		{0.25, 4, true},
+		{0.25, 5, false},
+		{0.25, 6, false},
+		{0.25, 7, false},
+		{0.25, 8, false},
+		// 50% enabled.
+		{0.50, 1, false},
+		{0.50, 2, true},
+		{0.50, 3, false},
+		{0.50, 4, true},
+		{0.50, 5, false},
+		{0.50, 6, false},
+		{0.50, 7, true},
+		{0.50, 8, false},
+		// 75% enabled.
+		{0.75, 1, false},
+		{0.75, 2, true},
+		{0.75, 3, true},
+		{0.75, 4, true},
+		{0.75, 5, true},
+		{0.75, 6, false},
+		{0.75, 7, true},
+		{0.75, 8, false},
+	}
+	for _, tc := range testCases {
+		name := fmt.Sprintf("fracEnabled=%f,rangeID=%d", tc.fracEnabled, tc.rangeID)
+		t.Run(name, func(t *testing.T) {
+			require.Equal(t, tc.exp, raftFortificationEnabledForRangeID(tc.fracEnabled, tc.rangeID))
+		})
+	}
+
+	// Test panic on invalid fracEnabled.
+	t.Run("fracEnabled=invalid", func(t *testing.T) {
+		require.Panics(t, func() {
+			raftFortificationEnabledForRangeID(-0.1, 1)
+		})
+		require.Panics(t, func() {
+			raftFortificationEnabledForRangeID(1.1, 1)
+		})
+	})
+}
+
+func BenchmarkRaftFortificationEnabledForRangeID(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = raftFortificationEnabledForRangeID(0.5, roachpb.RangeID(i))
+	}
+}
