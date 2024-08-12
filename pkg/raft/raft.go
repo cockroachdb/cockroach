466eb535fc645a21365481a83a783bab9b93578a
Ibrahim Kettaneh <ibrahim.kettaneh@cockroachlabs.com>
2024-08-12 14:02:24 -0400

    raft: remove commit index advancement dependence on heartbeats
    This commit removes the commit index advancement's dependence on
    heartbeats. It does so by making sure that the leader sends an empty
    MsgApp request if it knows that the follower's commit index is stale.
    
    Backward compatability arguments:
    
    1) Followers can accept commit index advancement using both paths:
    Heartbeats & MsgApps.
    
    2) If the leader's point of view of the cluster version is old, it
    uses the old path of advancing the followers' commit index using
    heartbeats.
    
    3) If the leader's point of view of the cluster version is new, it
    means that all nodes in the cluster (including all followers) have the
    latest binary for sure. At that moment, the leader uses the new logic
    of advancing the commit index using MsgApp rather than heartbeats.
    
    Fixes: #125266
    
    Epic: None
    
    Release note: None

--- pkg/raft/raft.go
+++ pkg/raft/raft.go
@@ -19,6 +19,7 @@ package raft
 
 import (
 	"bytes"
+	"context"
 	"crypto/rand"
 	"errors"
 	"fmt"
@@ -602,7 +603,7 @@ func (r *raft) maybeSendAppend(to pb.PeerID) bool {
 	pr := r.trk.Progress(to)
 
 	last, commit := r.raftLog.lastIndex(), r.raftLog.committed
-	if !pr.ShouldSendMsgApp(last, commit) {
+	if !pr.ShouldSendMsgApp(last, commit, r.advanceCommitViaMsgAppOnly()) {
 		return false
 	}
 
@@ -675,14 +676,23 @@ func (r *raft) sendHeartbeat(to pb.PeerID) {
 	// or it might not have all the committed entries.
 	// The leader MUST NOT forward the follower's commit to
 	// an unmatched index.
-	commit := min(pr.Match, r.raftLog.committed)
+	// NOTE: Starting from V24_3_AdvanceCommitIndexViaMsgApps, heartbeats do not
+	// advance the commit index. Instead, MsgApp are used for that purpose.
+	// TODO(iskettaneh): Remove the commit from the heartbeat message once no
+	// longer needed.
+	var commit uint64
+	if !r.advanceCommitViaMsgAppOnly() {
+		commit = min(pr.Match, r.raftLog.committed)
+	}
 	r.send(pb.Message{
 		To:     to,
 		Type:   pb.MsgHeartbeat,
 		Commit: commit,
 		Match:  pr.Match,
 	})
-	pr.SentCommit(commit)
+	if commit != 0 {
+		pr.SentCommit(commit)
+	}
 }
 
 // sendFortify sends a fortification RPC to the given peer.
@@ -1471,7 +1481,7 @@ func stepLeader(r *raft, m pb.Message) error {
 		// an MsgAppResp to acknowledge the appended entries in the last Ready.
 
 		pr.RecentActive = true
-
+		pr.MaybeUpdateReceivedCommit(m.Commit)
 		if m.Reject {
 			// RejectHint is the suggested next base entry for appending (i.e.
 			// we try to append entry RejectHint+1 next), and LogTerm is the
@@ -1966,9 +1976,12 @@ func (r *raft) handleHeartbeat(m pb.Message) {
 	// commit index if accTerm >= m.Term.
 	// TODO(pav-kv): move this logic to raftLog.commitTo, once the accTerm has
 	// migrated to raftLog/unstable.
-	mark := logMark{term: m.Term, index: min(m.Commit, r.raftLog.lastIndex())}
-	if mark.term == r.raftLog.accTerm() {
-		r.raftLog.commitTo(mark)
+	// TODO(iskettaneh): Remove this logic once no longer needed.
+	if m.Commit != 0 {
+		mark := logMark{term: m.Term, index: min(m.Commit, r.raftLog.lastIndex())}
+		if mark.term == r.raftLog.accTerm() {
+			r.raftLog.commitTo(mark)
+		}
 	}
 	r.send(pb.Message{To: m.From, Type: pb.MsgHeartbeatResp})
 }
@@ -2284,4 +2297,13 @@ func (r *raft) reduceUncommittedSize(s entryPayloadSize) {
 	} else {
 		r.uncommittedSize -= s
 	}
+}
+
+// onlyAdvanceCommitViaMsgApp returns true if the commit index is advanced on
+// the followers using MsgApp only. This means that heartbeats are not used to
+// advance the commit index. This function returns true only if followers
+// understand that heartbeats might not contain a commit index.
+func (r *raft) advanceCommitViaMsgAppOnly() bool {
+	return r.crdbVersion.IsActive(context.Background(),
+		clusterversion.V24_3_AdvanceCommitIndexViaMsgApps)
 }
