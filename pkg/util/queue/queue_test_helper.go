a37b8da2610e11365053707485a740168b53f786
Wenyi Hu <wenyi@cockroachlabs.com>
2024-08-12 22:42:28 -0400

    util/queue: add QueueWithFixedChunkSize
    This patch adds a new data structure QueueWithFixedChunkSize which is like Queue
    but uses a fixed size for the chunked linked list. Each chunk has a fixed size
    of 4096 elements. This implementation uses sync.Pool to reduce the number of
    allocations. It should be used when the queue is used to store a large number of
    elements.
    
    Enqueue, Dequeue, Len, Empty run in constant time. RemoveAll runs in linear time
    with respect to the number of elements in the queue. This structure is not safe
    for concurrent use.
    
    ```
    goarch: arm64
    BenchmarkQueueWithFixedChunkSize
    BenchmarkQueueWithFixedChunkSize           26445            118984 ns/op              11 B/op          0 allocs/op
    BenchmarkQueue
    BenchmarkQueue                             28495            124318 ns/op          239060 B/op        446 allocs/op
    PASS
    ```
    
    Part of: https://github.com/cockroachdb/cockroach/issues/126560
    Release note: none

--- pkg/util/queue/queue_test_helper.go
+++ pkg/util/queue/queue_test_helper.go
@@ -16,6 +16,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func (q *QueueWithFixedChunkSize[T]) removeAll() {
+	q.RemoveAll(func(e T) {})
+}
+
 func (q *Queue[T]) removeAll() {
 	for q.head != nil {
 		q.head = q.head.next
@@ -29,15 +33,55 @@ func (q *Queue[T]) checkNil(t *testing.T) {
 	require.Nil(t, q.tail)
 }
 
+func (q *QueueWithFixedChunkSize[T]) checkNil(t *testing.T) {
+	require.Nil(t, q.head)
+	require.Nil(t, q.tail)
+}
+
 // noop for Queue implementation since it doesn't track event count in the
 // queue.
 func (q *Queue[T]) checkEventCount(t *testing.T, _ int) {
+}
+
+func (q *QueueWithFixedChunkSize[T]) checkEventCount(t *testing.T, count int) {
+	require.Equal(t, count, int(q.eventCount))
 }
 
 // checkInvariants checks the invariants of the queue.
 func (q *Queue[T]) checkInvariants(t *testing.T) {
 	if q.head == nil && q.tail == nil {
 		require.True(t, q.Empty())
+	} else if q.head != nil && q.tail == nil {
+		t.Fatal("head is nil but tail is non-nil")
+	} else if q.head == nil && q.tail != nil {
+		t.Fatal("tail is nil but head is non-nil")
+	} else {
+		// The queue maintains an invariant that it contains no finished chunks.
+		require.False(t, q.head.finished())
+		require.False(t, q.tail.finished())
+
+		if q.head == q.tail {
+			require.Nil(t, q.head.next)
+		} else {
+			// q.tail is non-nil and not equal to q.head. There must be a non-empty
+			// chunk after q.head.
+			require.False(t, q.Empty())
+			if q.head.empty() {
+				require.False(t, q.head.next.empty())
+			}
+			// The q.tail is never empty in this case. The tail can only be empty
+			// when it is equal to q.head and q.head is empty.
+			require.False(t, q.tail.empty())
+		}
+	}
+}
+
+// checkInvariants checks the invariants of the queue. Same as the Queue[T]
+// version. Cannot avoid duplication due to the lack of generics.
+func (q *QueueWithFixedChunkSize[T]) checkInvariants(t *testing.T) {
+	if q.head == nil && q.tail == nil {
+		require.True(t, q.Empty())
+		require.Equal(t, q.eventCount, int64(0))
 	} else if q.head != nil && q.tail == nil {
 		t.Fatal("head is nil but tail is non-nil")
 	} else if q.head == nil && q.tail != nil {
