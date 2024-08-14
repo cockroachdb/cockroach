bf72a4df906036e315883a011deb3256ccbc68ee
Marcus Gartner <marcus@cockroachlabs.com>
2024-08-14 16:13:55 -0400

    opt: fix BPCHAR type and CASE typing
    This commit addresses inconsistencies from Postgres' behavior. First, it
    makes the `BPCHAR` type distinct from `CHAR`. The former is a
    blank-padded character type with no type width, meaning that it could
    have any length. The latter is a blank-padded character type with a type
    width of exactly 1 - it is essentially an alias of `CHAR(1)`.
    Previously, a column of type `BPCHAR` behaved the same as a column of
    type `CHAR(1)` - it enforced a length limit of 1.
    
    Second, the typing of `CASE` and `CASE`-like expressions has been fixed.
    The branches of these conditionals is no longer forced to have the same
    type-width.
    
    Fixes #127889
    Fixes #108360
    
    Release note (bug fix): A bug has been fixed that caused incorrect
    evaluation of `CASE`, `COALESCE`, and `IF` expressions with branches
    producing fixed-width string-like types, such as `CHAR`. In addition,
    the `BPCHAR` type has been fixed so that it no longer incorrectly
    imposes a length limit of 1.

--- pkg/sql/logictest/tests/local/generated_test.go
+++ pkg/sql/logictest/tests/local/generated_test.go
@@ -282,6 +282,13 @@ func TestLogic_bit(
 	runLogicTest(t, "bit")
 }
 
+func TestLogic_bpchar(
+	t *testing.T,
+) {
+	defer leaktest.AfterTest(t)()
+	runLogicTest(t, "bpchar")
+}
+
 func TestLogic_builtin_function(
 	t *testing.T,
 ) {
@@ -308,6 +315,13 @@ func TestLogic_cascade(
 ) {
 	defer leaktest.AfterTest(t)()
 	runLogicTest(t, "cascade")
+}
+
+func TestLogic_case(
+	t *testing.T,
+) {
+	defer leaktest.AfterTest(t)()
+	runLogicTest(t, "case")
 }
 
 func TestLogic_case_sensitive_names(
