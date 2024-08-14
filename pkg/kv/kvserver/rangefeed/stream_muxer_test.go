4c06aa5e9f24c612327d6872ade632433abc8302
Wenyi Hu <wenyi@cockroachlabs.com>
2024-08-13 22:32:22 -0400

    add rangefeed cleanup

--- pkg/kv/kvserver/rangefeed/stream_muxer_test.go
+++ pkg/kv/kvserver/rangefeed/stream_muxer_test.go
@@ -13,6 +13,7 @@ package rangefeed
 import (
 	"context"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -22,6 +23,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
 	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/require"
@@ -65,7 +67,7 @@ func TestStreamMuxer(t *testing.T) {
 		require.Equal(t, 1, testServerStream.totalEventsSent())
 		require.True(t, testServerStream.hasEvent(expectedErrEvent))
 
-		// Repeat closing the stream does nothing.
+		// Repeatedly closing the stream does nothing.
 		muxer.DisconnectStreamWithError(streamID, rangeID,
 			kvpb.NewError(kvpb.NewRangeFeedRetryError(kvpb.RangeFeedRetryError_REASON_RANGEFEED_CLOSED)))
 		time.Sleep(10 * time.Millisecond)
@@ -174,4 +176,134 @@ func TestStreamMuxerOnBlockingIO(t *testing.T) {
 	// Receive the event after getting unblocked.
 	require.Truef(t, testServerStream.hasEvent(expectedErrEvent),
 		"expected event %v not found in %v", ev, testServerStream)
+}
+
+// TestStreamMuxerWithDisconnect tests that StreamMuxer can handle stream
+// disconnects properly including context canceled, metrics updates, rangefeed
+// cleanup.
+func TestStreamMuxerWithDisconnect(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	ctx := context.Background()
+
+	stopper := stop.NewStopper()
+	defer stopper.Stop(ctx)
+	testServerStream := newTestServerStream()
+	testRangefeedCounter := newTestRangefeedCounter()
+	muxer := NewStreamMuxer(testServerStream, testRangefeedCounter)
+	require.NoError(t, muxer.Start(ctx, stopper))
+	defer muxer.Stop()
+
+	t.Run("basic operation", func(t *testing.T) {
+		const streamID = 0
+		var num atomic.Int32
+		streamCtx, cancel := context.WithCancel(context.Background())
+		muxer.AddStream(int64(streamID), 1, cancel)
+		muxer.RegisterRangefeedCleanUp(int64(streamID), func() {
+			num.Add(1)
+		})
+		muxer.DisconnectStreamWithError(int64(streamID), 1, kvpb.NewError(nil))
+		time.Sleep(10 * time.Millisecond)
+		// Ensure that the rangefeed clean up is called.
+		require.Equal(t, int32(1), num.Load())
+		// Ensure that the stream is properly disconnected.
+		require.Equal(t, context.Canceled, streamCtx.Err())
+		require.Equal(t, int32(0), testRangefeedCounter.get())
+	})
+	t.Run("cleanup map and active streams map out of sync", func(t *testing.T) {
+		const streamID = 0
+		var num atomic.Int32
+		streamCtx, cancel := context.WithCancel(context.Background())
+		muxer.AddStream(int64(streamID), 1, cancel)
+		require.Equal(t, int32(1), testRangefeedCounter.get())
+
+		// Disconnect stream without registering clean up should still work.
+		muxer.DisconnectStreamWithError(int64(streamID), 1, kvpb.NewError(nil))
+		require.Equal(t, context.Canceled, streamCtx.Err())
+		require.Equal(t, int32(0), num.Load())
+		require.Equal(t, int32(0), testRangefeedCounter.get())
+
+		// Although not possible theoretically, register clean up with an already
+		// disconnected stream should still work if the stream is disconnected
+		// again. This is more robust.
+		muxer.RegisterRangefeedCleanUp(int64(streamID), func() {
+			num.Add(1)
+		})
+		muxer.DisconnectStreamWithError(int64(streamID), 1, kvpb.NewError(nil))
+		time.Sleep(10 * time.Millisecond)
+		require.Equal(t, int32(1), num.Load())
+		require.Equal(t, int32(0), testRangefeedCounter.get())
+	})
+
+	t.Run("multiple clean up should do nothing", func(t *testing.T) {
+		const streamID = 0
+		var num atomic.Int32
+		_, cancel := context.WithCancel(context.Background())
+		muxer.AddStream(int64(streamID), 1, cancel)
+		muxer.RegisterRangefeedCleanUp(int64(streamID), func() {
+			num.Add(1)
+		})
+
+		muxer.DisconnectStreamWithError(int64(streamID), 1, kvpb.NewError(nil))
+		time.Sleep(10 * time.Millisecond)
+		require.Equal(t, int32(1), num.Load())
+		require.Equal(t, int32(0), testRangefeedCounter.get())
+
+		// Disconnecting the stream again should do nothing.
+		muxer.DisconnectStreamWithError(int64(streamID), 1, kvpb.NewError(nil))
+		time.Sleep(10 * time.Millisecond)
+		require.Equal(t, int32(1), num.Load())
+		require.Equal(t, int32(0), testRangefeedCounter.get())
+	})
+}
+
+func TestStreamMuxerOnStop(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	ctx := context.Background()
+
+	stopper := stop.NewStopper()
+	defer stopper.Stop(ctx)
+	testServerStream := newTestServerStream()
+	testRangefeedCounter := newTestRangefeedCounter()
+	muxer := NewStreamMuxer(testServerStream, testRangefeedCounter)
+	require.NoError(t, muxer.Start(ctx, stopper))
+
+	rng, _ := randutil.NewTestRand()
+	var actualSum atomic.Int32
+
+	// [streamIdStart,streamIDEnd) are in the active streams. streamIdStart <=
+	// streamIDEnd. If streamIDStart == streamIDEnd, no elements yet. [0,
+	// streamIdStart) are disconnected.
+	streamIdStart := int64(0)
+	streamIdEnd := int64(0)
+
+	defer func() {
+		muxer.Stop()
+		// Ensure that all streams are disconnected and cleanups are properly
+		// called.
+		require.Equal(t, int32(0), testRangefeedCounter.get())
+		require.Equal(t, int32(streamIdEnd), actualSum.Load())
+		require.Equal(t, streamIdStart, int64(testServerStream.totalEventsSent()))
+	}()
+
+	for i := 0; i < 100000; i++ {
+		randBool := rng.Intn(2) == 0
+		require.LessOrEqualf(t, streamIdStart, streamIdEnd, "test programming error")
+		if randBool || streamIdStart == streamIdEnd {
+			_, cancel := context.WithCancel(context.Background())
+			muxer.AddStream(streamIdEnd, 1, cancel)
+			muxer.RegisterRangefeedCleanUp(streamIdEnd, func() {
+				actualSum.Add(1)
+			})
+			streamIdEnd++
+		} else {
+			muxer.DisconnectStreamWithError(streamIdStart, 1, kvpb.NewError(nil))
+			streamIdStart++
+		}
+	}
+	time.Sleep(100 * time.Millisecond)
+	require.Equal(t, int32(streamIdStart), actualSum.Load())
+	require.Equal(t, streamIdStart, int64(testServerStream.totalEventsSent()))
+	require.Equal(t, int32(streamIdEnd-streamIdStart), testRangefeedCounter.get())
 }
