94fb782bed26e1ff420cd400c7b9cd4b7378b64d
Bilal Akhtar <bilal@cockroachlabs.com>
2024-08-15 16:19:10 -0400

    kvserver: reenable splitting of snapshot sstables
    This change updates the snapshot strategy's sender side
    to iterate over points and ranges together, instead of only
    iterating on points first, then only ranges. This allows us to
    more efficiently split snapshot sstables on the receiver side.
    To avoid the need to add a version gate on the receiver side, we
    propagate a bool, RangeKeysInOrder, to the receiver which is a signal
    to it to enable sstable splits.
    
    Fixes #129026.
    
    Epic: none
    
    Release note: None

--- pkg/kv/kvserver/rditer/replica_data_iter.go
+++ pkg/kv/kvserver/rditer/replica_data_iter.go
@@ -404,7 +404,7 @@ func IterateReplicaKeySpans(
 	reader storage.Reader,
 	replicatedOnly bool,
 	replicatedSpansFilter ReplicatedSpansFilter,
-	visitor func(storage.EngineIterator, roachpb.Span, storage.IterKeyType) error,
+	visitor func(storage.EngineIterator, roachpb.Span) error,
 ) error {
 	if !reader.ConsistentIterators() {
 		panic("reader must provide consistent iterators")
@@ -426,28 +426,25 @@ func IterateReplicaKeySpans(
 			UnreplicatedByRangeID: true,
 		})
 	}
-	keyTypes := []storage.IterKeyType{storage.IterKeyTypePointsOnly, storage.IterKeyTypeRangesOnly}
 	for _, span := range spans {
-		for _, keyType := range keyTypes {
-			err := func() error {
-				iter, err := reader.NewEngineIterator(ctx, storage.IterOptions{
-					KeyTypes:   keyType,
-					LowerBound: span.Key,
-					UpperBound: span.EndKey,
-				})
-				if err != nil {
-					return err
-				}
-				defer iter.Close()
-				ok, err := iter.SeekEngineKeyGE(storage.EngineKey{Key: span.Key})
-				if err == nil && ok {
-					err = visitor(iter, span, keyType)
-				}
-				return err
-			}()
+		err := func() error {
+			iter, err := reader.NewEngineIterator(ctx, storage.IterOptions{
+				KeyTypes:   storage.IterKeyTypePointsAndRanges,
+				LowerBound: span.Key,
+				UpperBound: span.EndKey,
+			})
 			if err != nil {
-				return iterutil.Map(err)
+				return err
 			}
+			defer iter.Close()
+			ok, err := iter.SeekEngineKeyGE(storage.EngineKey{Key: span.Key})
+			if err == nil && ok {
+				err = visitor(iter, span)
+			}
+			return err
+		}()
+		if err != nil {
+			return iterutil.Map(err)
 		}
 	}
 	return nil
