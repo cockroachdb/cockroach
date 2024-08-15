8e980f3416828e6c19a7711071c717f4e3ffbd90
Marcus Gartner <marcus@cockroachlabs.com>
2024-08-15 10:09:57 -0400

    opt: disable `GenerateParameterizedJoin` when forcing custom plans
    The exploration rule `GenerateParameterizedJoin` is now disabled when
    the `plan_cache_mode` session setting is set to `force_custom_plan`.
    This prevents possible regressions in plans for this mode in the rare
    case that a stable function is not folded during normalization (due to
    an error encountered during folding). In most cases, the check for
    placeholders and stable functions in `GenerateParameterizedJoin` is
    sufficient for preventing the rule from firing when a generic query plan
    is not being built—before optimizing a custom plan placeholders are
    always replaced with constants and stable functions are usually folded
    to constants.
    
    Release note: None

--- pkg/sql/opt/testutils/opttester/opt_tester.go
+++ pkg/sql/opt/testutils/opttester/opt_tester.go
@@ -61,6 +61,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/eval"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/volatility"
+	"github.com/cockroachdb/cockroach/pkg/sql/sessiondatapb"
 	"github.com/cockroachdb/cockroach/pkg/sql/stats"
 	"github.com/cockroachdb/cockroach/pkg/testutils/datapathutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/floatcmp"
@@ -164,6 +165,9 @@ type Flags struct {
 
 	// DisableRules is a set of rules that are not allowed to run.
 	DisableRules RuleSet
+
+	// Generic enables optimizations for generic query plans.
+	Generic bool
 
 	// ExploreTraceRule restricts the ExploreTrace output to only show the effects
 	// of a specific rule.
@@ -478,6 +482,11 @@ func New(catalog cat.Catalog, sql string) *OptTester {
 //   - disable: disables optimizer rules by name. Examples:
 //     opt disable=ConstrainScan
 //     norm disable=(NegateOr,NegateAnd)
+//
+//   - generic: enables optimizations for generic query plans.
+//     NOTE: This flag sets the plan_cache_mode session setting to "auto", which
+//     cannot be done via the "set" flag because it requires a CCL license,
+//     which optimizer tests are not set up to utilize.
 //
 //   - rule: used with exploretrace; the value is the name of a rule. When
 //     specified, the exploretrace output is filtered to only show expression
@@ -987,6 +996,9 @@ func (f *Flags) Set(arg datadriven.CmdArg) error {
 			}
 			f.DisableRules.Add(int(r))
 		}
+
+	case "generic":
+		f.evalCtx.SessionData().PlanCacheMode = sessiondatapb.PlanCacheModeAuto
 
 	case "rule":
 		if len(arg.Vals) != 1 {
