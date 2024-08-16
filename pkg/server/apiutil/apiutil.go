ea5105adf00fb28d2d0beff99d2fbeb6297d88e0
Kyle Wong <37189875+kyle-a-wong@users.noreply.github.com>
2024-08-16 16:27:32 -0400

    server: Create new api for table metadata
    adds a new api v2 endpoint,
    /api/v2/table_metadata/.
    
    This new API enables the ability to retrieve
    paginated table metadata for a given database
    
    Resolves: #128894
    Epic: CRDB-37558
    Release note: None

--- pkg/server/apiutil/apiutil.go
+++ pkg/server/apiutil/apiutil.go
@@ -14,6 +14,8 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+	"net/url"
+	"strconv"
 
 	"github.com/cockroachdb/cockroach/pkg/server/srverrors"
 )
@@ -29,4 +31,60 @@ func WriteJSONResponse(ctx context.Context, w http.ResponseWriter, code int, pay
 		return
 	}
 	_, _ = w.Write(res)
+}
+
+// GetQueryStringVal gets the string value of a Query String parameter, param, from the provided url query string
+// values.
+//
+// Returns a QueryStringVal struct with the value, if the param exists in queryStringVals. If not, the Exists field
+// will be false.
+func GetQueryStringVal(queryStringVals url.Values, param string) QueryStringVal[string] {
+	if queryStringVals.Has(param) {
+		return QueryStringVal[string]{Value: queryStringVals.Get(param), Exists: true}
+	}
+	return QueryStringVal[string]{Exists: false}
+}
+
+// GetIntQueryStringVal gets the int value of a Query String parameter, param, from the provided url query string
+// values.
+//
+// Returns a QueryStringVal struct with the value, if the param exists in queryStringVals and can be cast to an int.
+// If not, the Exists field will be false.
+func GetIntQueryStringVal(queryStringVals url.Values, param string) QueryStringVal[int] {
+	if queryStringVals.Has(param) {
+		queryArgStr := queryStringVals.Get(param)
+		queryArgInt, err := strconv.Atoi(queryArgStr)
+		if err == nil {
+			return QueryStringVal[int]{Value: queryArgInt, Exists: true}
+		}
+	}
+	return QueryStringVal[int]{Exists: false}
+}
+
+// GetIntQueryStringVals gets all int values of a Query String parameter, param, from the provided url query string
+// values.
+//
+// Returns a QueryStringVal struct with the values, if the params exists in queryStringVals and can be cast to an int.
+// If not, the Exists field will be false.
+func GetIntQueryStringVals(queryStringVals url.Values, param string) QueryStringVal[[]int] {
+	if queryStringVals.Has(param) {
+		queryArgStrs := queryStringVals[param]
+		queryArgInts := make([]int, 0, len(queryArgStrs))
+		for _, a := range queryArgStrs {
+
+			i, err := strconv.Atoi(a)
+			if err != nil {
+				return QueryStringVal[[]int]{Exists: false}
+			}
+			queryArgInts = append(queryArgInts, i)
+		}
+
+		return QueryStringVal[[]int]{Value: queryArgInts, Exists: true}
+	}
+	return QueryStringVal[[]int]{Exists: false}
+}
+
+type QueryStringVal[T any] struct {
+	Value  T
+	Exists bool
 }
