028f233dc98c268398975d404664df907828e768
Wenyi Hu <wenyi@cockroachlabs.com>
2024-08-16 00:49:57 -0400

    proc test changes

--- pkg/kv/kvserver/rangefeed/registry_helpers_test.go
+++ pkg/kv/kvserver/rangefeed/registry_helpers_test.go
@@ -57,10 +57,6 @@ func (s *testStream) Context() context.Context {
 	return s.ctx
 }
 
-func (s *testStream) Cancel() {
-	s.ctxDone()
-}
-
 func (s *testStream) SendIsThreadSafe() {}
 
 func (s *testStream) Send(e *kvpb.RangeFeedEvent) error {
