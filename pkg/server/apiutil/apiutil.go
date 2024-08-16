8fcdf7a1ea3257f4f64cd402a57e8ef524a7de9b
Kyle Wong <37189875+kyle-a-wong@users.noreply.github.com>
2024-08-16 16:27:32 -0400

    server: create new api for table metadata
    Adds a new API v2 endpoint,
    `/api/v2/table_metadata/`, That accepts
    the following query parameters:
     - dbId
     - name
     - sortBy
     - sortOrder
     - pageSize
     - pageNum
     - storeId
    
    This new API enables the ability to retrieve
    table metadata, using the supported query
    to filter, sort, and paginate the response.
    
    The query params dbId, name, and storeId are
    used to filter down the total results of the
    table_metadata query.
    
    The query params sortBy and sortOrder affect the
    sorting of the total results, before pagination.
    
    The query params pageSize and pageNum limit the
    size of the result set returned to the client.
    
    The user making the request to this new endpoint
    will only see tables for databases they have the
    `CONNECT` database privilege for. If the user has
    the `ADMIN` privilege, they will be able to see
    all tables in all databases.
    
    Resolves: #128894
    Epic: CRDB-37558
    Release note: None

--- pkg/server/apiutil/apiutil.go
+++ pkg/server/apiutil/apiutil.go
@@ -14,6 +14,8 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+	"net/url"
+	"strconv"
 
 	"github.com/cockroachdb/cockroach/pkg/server/srverrors"
 )
@@ -29,4 +31,42 @@ func WriteJSONResponse(ctx context.Context, w http.ResponseWriter, code int, pay
 		return
 	}
 	_, _ = w.Write(res)
+}
+
+// GetIntQueryStringVal gets the int value of a Query String parameter, param, from the provided url query string
+// values.
+//
+// Returns an int if the param exists and can be cast into an int. Otherwise, returns 0.
+func GetIntQueryStringVal(queryStringVals url.Values, param string) (int, error) {
+	if queryStringVals.Has(param) {
+		queryArgStr := queryStringVals.Get(param)
+		queryArgInt, err := strconv.Atoi(queryArgStr)
+		if err != nil {
+			return 0, err
+		}
+		return queryArgInt, nil
+	}
+	return 0, nil
+}
+
+// GetIntQueryStringVals gets all int values of a Query String parameter, param, from the provided url query string
+// values.
+//
+// Returns an int slice if the param exists and ALL the values can be cast to ints. Otherwise, returns an empty slice.
+func GetIntQueryStringVals(queryStringVals url.Values, param string) ([]int, error) {
+	if queryStringVals.Has(param) {
+		queryArgStrs := queryStringVals[param]
+		queryArgInts := make([]int, 0, len(queryArgStrs))
+		for _, a := range queryArgStrs {
+
+			i, err := strconv.Atoi(a)
+			if err != nil {
+				return []int{}, err
+			}
+			queryArgInts = append(queryArgInts, i)
+		}
+
+		return queryArgInts, nil
+	}
+	return []int{}, nil
 }
