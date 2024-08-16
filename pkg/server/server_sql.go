4ebd1eb1e970456346e3d60033c792f8cc778e76
Andrew Baptist <baptist@cockroachlabs.com>
2024-08-16 16:56:51 -0400

    changefeedccl: add network tracking
    Adds network tracing infrasturcture to changefeeds. This commit adds the
    metrics but does not populate them for any of the existing changefeeds.
    
    Part of: #130097
    
    Epic: none
    
    Release note (ops change): This commit adds two metrics
    changefeed.network.bytes_in and changefeed.network.bytes_out. These
    metrics track the number of bytes sent by the individual changefeeds to
    different sinks.

--- pkg/server/server_sql.go
+++ pkg/server/server_sql.go
@@ -652,6 +652,7 @@ func newSQLServer(ctx context.Context, cfg sqlServerArgs) (*SQLServer, error) {
 			},
 			jobAdoptionStopFile,
 			jobsKnobs,
+			cfg.CidrLookup,
 		)
 	}
 	cfg.registry.AddMetricStruct(jobRegistry.MetricsStruct())
