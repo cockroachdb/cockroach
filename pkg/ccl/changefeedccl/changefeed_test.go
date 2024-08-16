63a210c2f1adb99daf393bac017d63c6f6bbbc4e
Andrew Baptist <baptist@cockroachlabs.com>
2024-08-16 16:56:51 -0400

    changefeedccl: add network tracking
    Adds network tracing infrasturcture to changefeeds. This commit adds the
    metrics but does not populate them for any of the existing changefeeds.
    
    Part of: #130097
    
    Epic: none
    
    Release note (ops change): This commit adds two metrics
    changefeed.network.bytes_in and changefeed.network.bytes_out. These
    metrics track the number of bytes sent by the individual changefeeds to
    different sinks.

--- pkg/ccl/changefeedccl/changefeed_test.go
+++ pkg/ccl/changefeedccl/changefeed_test.go
@@ -78,6 +78,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
 	"github.com/cockroachdb/cockroach/pkg/util"
+	"github.com/cockroachdb/cockroach/pkg/util/cidr"
 	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
@@ -7025,7 +7026,7 @@ func TestCheckpointFrequency(t *testing.T) {
 	ts := timeutil.NewManualTime(timeutil.Now())
 	js := newJobState(nil, /* job */
 		cluster.MakeTestingClusterSettings(),
-		MakeMetrics(time.Second).(*Metrics), ts,
+		MakeMetrics(time.Second, cidr.NewTestLookup()).(*Metrics), ts,
 	)
 
 	ctx := context.Background()
