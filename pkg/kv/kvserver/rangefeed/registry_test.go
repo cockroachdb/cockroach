46cdf5cd8eef4b280051d2f313e8bbca1c0838ba
Wenyi Hu <wenyi@cockroachlabs.com>
2024-08-18 01:54:17 -0400

    kvserver/rangefeed: add newRetryErrBufferCapacityExceeded
    This patch refactors the error for kvpb.RangeFeedRetryError_REASON_SLOW_CONSUMER
    into newRetryErrBufferCapacityExceeded.
    
    Part of: #126560
    Release note: none

--- pkg/kv/kvserver/rangefeed/registry_test.go
+++ pkg/kv/kvserver/rangefeed/registry_test.go
@@ -102,7 +102,7 @@ func TestRegistrationBasic(t *testing.T) {
 				overflowReg.publish(ctx, ev1, nil /* alloc */)
 			}
 			go overflowReg.runOutputLoop(ctx, 0)
-			require.Equal(t, kvpb.NewRangeFeedRetryError(kvpb.RangeFeedRetryError_REASON_SLOW_CONSUMER), s.WaitForError(t))
+			require.Equal(t, newRetryErrBufferCapacityExceeded(), s.WaitForError(t))
 			require.Equal(t, capOfBuf, len(s.Events()))
 		})
 		t.Run("stream error", func(t *testing.T) {
