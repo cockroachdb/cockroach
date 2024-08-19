425ba55b61b7845fa4a017cc63e4ec72c9b372a2
Austen McClernon <austen@cockroachlabs.com>
2024-08-19 13:22:20 -0400

    rac2: implement wait for eval
    Implement `RangeController.WaitForEval(..)`, which will be used to wait
    on a configurable number of streams to have available tokens. Note that
    the functionality is currently incomplete without replica state
    management, done in `HandleRaftReady`.
    
    Resolves: #128910
    Release note: None

--- pkg/kv/kvserver/kvflowcontrol/rac2/range_controller.go
+++ pkg/kv/kvserver/kvflowcontrol/rac2/range_controller.go
@@ -13,11 +13,15 @@ package rac2
 import (
 	"cmp"
 	"context"
+	"reflect"
 	"slices"
 
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvflowcontrol"
 	"github.com/cockroachdb/cockroach/pkg/raft/raftpb"
+	"github.com/cockroachdb/cockroach/pkg/raft/tracker"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/util/admission/admissionpb"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/redact"
 )
 
@@ -57,6 +61,43 @@ type RangeController interface {
 	//
 	// Requires replica.raftMu to be held.
 	CloseRaftMuLocked(ctx context.Context)
+}
+
+// TODO(pav-kv): This interface a placeholder for the interface containing raft
+// methods. Replace this as part of #128019.
+type RaftInterface interface {
+	// FollowerState returns the current state of a follower. The value of
+	// Match, Next, Admitted are populated iff in StateReplicate. All entries >=
+	// Next have not had MsgApps constructed during the lifetime of this
+	// StateReplicate (they may have been constructed previously).
+	//
+	// When a follower transitions from {StateProbe,StateSnapshot} =>
+	// StateReplicate, we start trying to send MsgApps. We should
+	// notice such transitions both in HandleRaftEvent and SetReplicasLocked.
+	//
+	// RACv1 also cared about three other cases where the follower behaved as if
+	// it were disconnected (a) paused follower, (b) follower is behind, (c)
+	// follower is inactive (see
+	// replicaFlowControlIntegrationImpl.notActivelyReplicatingTo). (b) and (c)
+	// were needed since it paced at rate of slowest replica, while for regular
+	// work we will in v2 pace at slowest in quorum (and we don't care about
+	// elastic experiencing a hiccup, given it paces at rate of slowest). For
+	// (a), we plan to remove follower pausing. So the v2 code will be
+	// simplified.
+	//
+	// Requires Replica.raftMu to be held, Replica.mu is not held.
+	FollowerState(replicaID roachpb.ReplicaID) FollowerStateInfo
+}
+
+type FollowerStateInfo struct {
+	State tracker.StateType
+
+	// Remaining only populated in StateReplicate.
+	// (Match, Next) is in-flight.
+	Match uint64
+	Next  uint64
+	// Invariant: Admitted[i] <= Match.
+	Admitted [raftpb.NumPriorities]uint64
 }
 
 // TODO(pav-kv): This struct is a placeholder for the interface or struct
@@ -99,3 +140,338 @@ func (rs ReplicaSet) SafeFormat(w redact.SafePrinter, _ rune) {
 func (rs ReplicaSet) String() string {
 	return redact.StringWithoutMarkers(rs)
 }
+
+type RangeControllerOptions struct {
+	RangeID  roachpb.RangeID
+	TenantID roachpb.TenantID
+	// LocalReplicaID is the ReplicaID of the local replica, which is the
+	// leader.
+	LocalReplicaID roachpb.ReplicaID
+	// SSTokenCounter provides access to all the TokenCounters that will be
+	// needed (keyed by (tenantID, storeID)).
+	SSTokenCounter *StreamTokenCounterProvider
+	RaftInterface  RaftInterface
+}
+
+// RangeControllerInitState is the initial state at the time of creation.
+type RangeControllerInitState struct {
+	// Must include RangeControllerOptions.ReplicaID.
+	ReplicaSet ReplicaSet
+	// Leaseholder may be set to NoReplicaID, in which case the leaseholder is
+	// unknown.
+	Leaseholder roachpb.ReplicaID
+}
+
+type rangeController struct {
+	opts       RangeControllerOptions
+	replicaSet ReplicaSet
+	// leaseholder can be NoReplicaID or not be in ReplicaSet, i.e., it is
+	// eventually consistent with the set of replicas.
+	leaseholder roachpb.ReplicaID
+
+	mu struct {
+		syncutil.Mutex
+
+		// State for waiters. When anything in voterSets changes, voterSetRefreshCh
+		// is closed, and replaced with a new channel. The voterSets is
+		// copy-on-write, so waiters make a shallow copy.
+		voterSets         []voterSet
+		voterSetRefreshCh chan struct{}
+	}
+
+	replicaMap map[roachpb.ReplicaID]*replicaState
+}
+
+// voterStateForWaiters informs whether WaitForEval is required to wait for
+// eval-tokens for a voter.
+type voterStateForWaiters struct {
+	replicaID        roachpb.ReplicaID
+	isLeader         bool
+	isLeaseHolder    bool
+	isStateReplicate bool
+	evalTokenCounter TokenCounter
+}
+
+type voterSet []voterStateForWaiters
+
+var _ RangeController = &rangeController{}
+
+func NewRangeController(
+	ctx context.Context, o RangeControllerOptions, init RangeControllerInitState,
+) *rangeController {
+	rc := &rangeController{
+		opts:        o,
+		leaseholder: init.Leaseholder,
+		replicaMap:  make(map[roachpb.ReplicaID]*replicaState),
+	}
+	rc.mu.voterSetRefreshCh = make(chan struct{})
+	rc.updateReplicaSet(ctx, init.ReplicaSet)
+	rc.updateVoterSets()
+	return rc
+}
+
+// This blocks until there are positive tokens available for the request to
+// be admitted for evaluation. Note the number of tokens required by the
+// request is not considered, only the priority of the request, as the number
+// of tokens is not known until eval.
+//
+// No mutexes should be held.
+func (rc *rangeController) WaitForEval(ctx context.Context, pri admissionpb.WorkPriority) error {
+	wc := admissionpb.WorkClassFromPri(pri)
+	waitForAllReplicateHandles := false
+	if wc == admissionpb.ElasticWorkClass {
+		waitForAllReplicateHandles = true
+	}
+	var handles []tokenWaitingHandleInfo
+	var scratch []reflect.SelectCase
+
+retry:
+	// Snapshot the voterSets and voterSetRefreshCh.
+	rc.mu.Lock()
+	vss := rc.mu.voterSets
+	vssRefreshCh := rc.mu.voterSetRefreshCh
+	rc.mu.Unlock()
+
+	if vssRefreshCh == nil {
+		// RangeControllerImpl is closed.
+		return nil
+	}
+	for _, vs := range vss {
+		quorumCount := (len(vs) + 2) / 2
+		haveEvalTokensCount := 0
+		handles = handles[:0]
+		requiredWait := false
+		for _, v := range vs {
+			available, handle := v.evalTokenCounter.TokensAvailable(wc)
+			if available {
+				haveEvalTokensCount++
+				continue
+			}
+			// Don't have eval tokens, and have a handle.
+			handleInfo := tokenWaitingHandleInfo{
+				handle: handle,
+				requiredWait: v.isLeader || v.isLeaseHolder ||
+					(waitForAllReplicateHandles && v.isStateReplicate),
+			}
+			handles = append(handles, handleInfo)
+			if !requiredWait && handleInfo.requiredWait {
+				requiredWait = true
+			}
+		}
+		remainingForQuorum := quorumCount - haveEvalTokensCount
+		if remainingForQuorum < 0 {
+			remainingForQuorum = 0
+		}
+		if remainingForQuorum > 0 || requiredWait {
+			var state WaitEndState
+			state, scratch = WaitForEval(ctx, vssRefreshCh, handles, remainingForQuorum, scratch)
+			switch state {
+			case WaitSuccess:
+				continue
+			case ContextCanceled:
+				return ctx.Err()
+			case RefreshWaitSignaled:
+				goto retry
+			}
+		}
+	}
+	return nil
+}
+
+// HandleRaftEventRaftMuLocked handles the provided raft event for the range.
+//
+// Requires replica.raftMu to be held.
+func (rc *rangeController) HandleRaftEventRaftMuLocked(ctx context.Context, e RaftEvent) error {
+	panic("unimplemented")
+}
+
+// HandleSchedulerEventRaftMuLocked processes an event scheduled by the
+// controller.
+//
+// Requires replica.raftMu to be held.
+func (rc *rangeController) HandleSchedulerEventRaftMuLocked(ctx context.Context) error {
+	panic("unimplemented")
+}
+
+// SetReplicasRaftMuLocked sets the replicas of the range. The caller will
+// never mutate replicas, and neither should the callee.
+//
+// Requires replica.raftMu to be held.
+func (rc *rangeController) SetReplicasRaftMuLocked(ctx context.Context, replicas ReplicaSet) error {
+	rc.updateReplicaSet(ctx, replicas)
+	rc.updateVoterSets()
+	return nil
+}
+
+// SetLeaseholderRaftMuLocked sets the leaseholder of the range.
+//
+// Requires raftMu to be held.
+func (rc *rangeController) SetLeaseholderRaftMuLocked(
+	ctx context.Context, replica roachpb.ReplicaID,
+) {
+	if replica == rc.leaseholder {
+		return
+	}
+	rc.leaseholder = replica
+	rc.updateVoterSets()
+}
+
+// CloseRaftMuLocked closes the range controller.
+//
+// Requires replica.raftMu to be held.
+func (rc *rangeController) CloseRaftMuLocked(ctx context.Context) {
+	rc.mu.Lock()
+	defer rc.mu.Unlock()
+
+	rc.mu.voterSets = nil
+	close(rc.mu.voterSetRefreshCh)
+	rc.mu.voterSetRefreshCh = nil
+}
+
+func (rc *rangeController) updateReplicaSet(ctx context.Context, newSet ReplicaSet) {
+	prevSet := rc.replicaSet
+	for r := range prevSet {
+		desc, ok := newSet[r]
+		if !ok {
+			delete(rc.replicaMap, r)
+		} else {
+			rs := rc.replicaMap[r]
+			rs.desc = desc
+		}
+	}
+	for r, desc := range newSet {
+		_, ok := prevSet[r]
+		if ok {
+			// Already handled above.
+			continue
+		}
+		rc.replicaMap[r] = NewReplicaState(ctx, rc, desc)
+	}
+	rc.replicaSet = newSet
+}
+
+func (rc *rangeController) updateVoterSets() {
+	rc.mu.Lock()
+	defer rc.mu.Unlock()
+
+	setCount := 1
+	for _, r := range rc.replicaSet {
+		isOld := r.IsVoterOldConfig()
+		isNew := r.IsVoterNewConfig()
+		if !isOld && !isNew {
+			continue
+		}
+		if !isOld && isNew {
+			setCount++
+			break
+		}
+	}
+	var voterSets []voterSet
+	for len(voterSets) < setCount {
+		voterSets = append(voterSets, voterSet{})
+	}
+	for _, r := range rc.replicaSet {
+		isOld := r.IsVoterOldConfig()
+		isNew := r.IsVoterNewConfig()
+		if !isOld && !isNew {
+			continue
+		}
+		// Is a voter.
+		rs := rc.replicaMap[r.ReplicaID]
+		vsfw := voterStateForWaiters{
+			replicaID:     r.ReplicaID,
+			isLeader:      r.ReplicaID == rc.opts.LocalReplicaID,
+			isLeaseHolder: r.ReplicaID == rc.leaseholder,
+			// TODO(rac2): Once the send stream is added, check that the send stream
+			// is initialized here instead and also check that the elastic eval
+			// tokens are positive.
+			isStateReplicate: rs.connectedState == replicate,
+			evalTokenCounter: rs.evalTokenCounter,
+		}
+		if isOld {
+			voterSets[0] = append(voterSets[0], vsfw)
+		}
+		if isNew && setCount == 2 {
+			voterSets[1] = append(voterSets[1], vsfw)
+		}
+	}
+	rc.mu.voterSets = voterSets
+	close(rc.mu.voterSetRefreshCh)
+	rc.mu.voterSetRefreshCh = make(chan struct{})
+}
+
+type replicaState struct {
+	parent *rangeController
+	// stream aggregates across the streams for the same (tenant, store). This
+	// is the identity that is used to deduct tokens or wait for tokens to be
+	// positive.
+	stream           kvflowcontrol.Stream
+	evalTokenCounter TokenCounter
+	desc             roachpb.ReplicaDescriptor
+	connectedState   connectedState
+}
+
+func NewReplicaState(
+	ctx context.Context, parent *rangeController, desc roachpb.ReplicaDescriptor,
+) *replicaState {
+	stream := kvflowcontrol.Stream{TenantID: parent.opts.TenantID, StoreID: desc.StoreID}
+	rs := &replicaState{
+		parent:           parent,
+		stream:           stream,
+		evalTokenCounter: parent.opts.SSTokenCounter.Eval(stream),
+		desc:             desc,
+	}
+
+	// TODO(rac2): Construct the sendStream state here if the replica is in state
+	// replicate.
+	state := parent.opts.RaftInterface.FollowerState(desc.ReplicaID)
+	switch state.State {
+	case tracker.StateReplicate:
+		rs.connectedState = replicate
+	case tracker.StateProbe:
+		rs.connectedState = probeRecentlyReplicate
+	case tracker.StateSnapshot:
+		rs.connectedState = snapshot
+	}
+	return rs
+}
+
+type connectedState uint32
+
+// Local replicas are always in state replicate.
+//
+// Initial state for a replicaSendStream is always replicate, since it is
+// created in StateReplicate. We don't care about whether the transport is
+// connected or disconnected, since there is buffering capacity in the
+// RaftTransport, which allows for some buffering and immediate sending when
+// the RaftTransport stream reconnects (which may happen before the next
+// HandleRaftEvent), which is desirable.
+//
+// The first false return value from SendRaftMessage will trigger a
+// notification to Raft that the replica is unreachable (see
+// Replica.sendRaftMessage calling Replica.addUnreachableRemoteReplica), and
+// that raftpb.MsgUnreachable will cause the transition out of StateReplicate
+// to StateProbe. The false return value happens either when the (generous)
+// RaftTransport buffer is full, or when the circuit breaker opens. The
+// circuit breaker opens 3-6s after no more TCP packets are flowing.
+//
+// A single transient message drop, and nack, can also cause a transition to
+// StateProbe. At this layer we don't bother distinguishing on why this
+// transition happened and first transition to probeRecentlyReplicate. We stay
+// in this state for 1 second, and then close the replicaSendStream.
+//
+// The only difference in behavior between replicate and
+// probeRecentlyReplicate is that we don't try to construct MsgApps in the
+// latter.
+//
+// Initial states: replicate
+// State transitions:
+//
+//	replicate <=> {probeRecentlyReplicate, snapshot}
+//	snapshot => replicaSendStream closed (when observe StateProbe)
+//	probeRecentlyReplicate => replicaSendStream closed (after short delay)
+const (
+	replicate connectedState = iota
+	probeRecentlyReplicate
+	snapshot
+)
