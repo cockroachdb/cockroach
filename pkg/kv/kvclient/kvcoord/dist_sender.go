8492ab267ce37c30bb5827c8f4e78611c8236f08
Austen McClernon <austen@cockroachlabs.com>
2024-08-19 13:42:25 -0400

    kvcoord: log replica order on nearest routing policy
    Previously, we would log an event when routing to the nearest replica,
    which would then show up in traces and could be used to determine the
    routing policy of a query.
    
    It is also useful to know how the nearest replica was selected i.e.,
    latency, locality or by health. Log the event after sorting by the
    replicas' distance to the sender and include the replica order.
    
    Informs: #129031
    Release note: None

--- pkg/kv/kvclient/kvcoord/dist_sender.go
+++ pkg/kv/kvclient/kvcoord/dist_sender.go
@@ -2119,8 +2119,8 @@ func (ds *DistSender) sendToReplicas(
 
 	case kvpb.RoutingPolicy_NEAREST:
 		// Order by latency.
-		log.VEvent(ctx, 2, "routing to nearest replica; leaseholder not required")
 		replicas.OptimizeReplicaOrder(ds.st, ds.getNodeID(), ds.HealthFunc(), ds.latencyFunc, ds.locality)
+		log.VEventf(ctx, 2, "routing to nearest replica; leaseholder not required order=%v", replicas)
 
 	default:
 		log.Fatalf(ctx, "unknown routing policy: %s", ba.RoutingPolicy)
