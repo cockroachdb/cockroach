1c69cf0f0d492eca56b8683d0248ab375bd0f785
Austen McClernon <austen@cockroachlabs.com>
2024-08-21 12:07:46 -0400

    rac2: add token tracker
    This commit introduces the token `Tracker`, which will be used to track
    token deductions for in-flight entries at a given `RaftPriority`.
    
    Resolves: #128026
    Release note: None

---
+++ pkg/kv/kvserver/kvflowcontrol/rac2/token_tracker.go
@@ -0,0 +1,129 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package rac2
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvflowcontrol"
+	"github.com/cockroachdb/cockroach/pkg/raft/raftpb"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+)
+
+// Tracker tracks flow token deductions for a replicaSendStream. Tokens are
+// deducted for an in-flight log entry identified by its raft log index and
+// term with a given RaftPriority.
+type Tracker struct {
+	// tracked entries are stored in increasing order of (term, index), per
+	// priority.
+	tracked [raftpb.NumPriorities][]tracked
+
+	stream kvflowcontrol.Stream // used for logging only
+}
+
+// tracked represents tracked flow tokens; they're tracked with respect to a
+// raft log index and term.
+type tracked struct {
+	tokens      kvflowcontrol.Tokens
+	index, term uint64
+}
+
+func NewTracker(stream kvflowcontrol.Stream) *Tracker {
+	return &Tracker{
+		tracked: [int(raftpb.NumPriorities)][]tracked{},
+		stream:  stream,
+	}
+}
+
+// Track token deductions of the given priority with the given raft log index and term.
+func (t *Tracker) Track(
+	ctx context.Context, term uint64, index uint64, pri raftpb.Priority, tokens kvflowcontrol.Tokens,
+) bool {
+	if len(t.tracked[pri]) >= 1 {
+		last := t.tracked[pri][len(t.tracked[pri])-1]
+		// Tracker exists in the context of a single replicaSendStream, which cannot
+		// span the leader losing leadership and regaining it. So the indices must
+		// advance.
+		if last.index >= index {
+			log.Fatalf(ctx, "expected in order tracked log indexes (%d < %d)",
+				last.index, index)
+			return false
+		}
+		if last.term > term {
+			log.Fatalf(ctx, "expected in order tracked leader terms (%d < %d)",
+				last.term, term)
+			return false
+		}
+	}
+
+	t.tracked[pri] = append(t.tracked[pri], tracked{
+		tokens: tokens,
+		index:  index,
+		term:   term,
+	})
+
+	return true
+}
+
+// Untrack all token deductions of the given priority that have indexes less
+// than or equal to the one provided, per priority, and terms less than or
+// equal to the leader term.
+func (t *Tracker) Untrack(
+	term uint64, admitted [raftpb.NumPriorities]uint64,
+) (returned [raftpb.NumPriorities]kvflowcontrol.Tokens) {
+	for pri := range admitted {
+		uptoIndex := admitted[pri]
+		var untracked int
+		for n := len(t.tracked[pri]); untracked < n; untracked++ {
+			tracked := t.tracked[pri][untracked]
+			if tracked.term > term || (tracked.term == term && tracked.index > uptoIndex) {
+				break
+			}
+			returned[pri] += tracked.tokens
+		}
+		t.tracked[pri] = t.tracked[pri][untracked:]
+	}
+
+	return returned
+}
+
+// UntrackGE untracks all token deductions of the given priority that have
+// indexes greater than or equal to the one provided.
+func (t *Tracker) UntrackGE(index uint64) (returned [raftpb.NumPriorities]kvflowcontrol.Tokens) {
+	for pri := range t.tracked {
+		j := len(t.tracked[pri]) - 1
+		for j >= 0 {
+			tr := t.tracked[pri][j]
+			if tr.index >= index {
+				returned[pri] += tr.tokens
+				j--
+			} else {
+				break
+			}
+		}
+		t.tracked[pri] = t.tracked[pri][:j+1]
+	}
+
+	return returned
+}
+
+// UntrackAll iterates through all tracked token deductions, untracking all of them
+// and returning the sum of tokens for each priority.
+func (t *Tracker) UntrackAll() (returned [raftpb.NumPriorities]kvflowcontrol.Tokens) {
+	for pri, deductions := range t.tracked {
+		for _, deduction := range deductions {
+			returned[pri] += deduction.tokens
+		}
+	}
+	t.tracked = [raftpb.NumPriorities][]tracked{}
+
+	return returned
+}
---
+++ pkg/kv/kvserver/kvflowcontrol/rac2/token_tracker_test.go
@@ -0,0 +1,129 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package rac2
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvflowcontrol"
+	"github.com/cockroachdb/cockroach/pkg/raft/raftpb"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/datadriven"
+	"github.com/stretchr/testify/require"
+)
+
+func formatTrackerState(t *Tracker) string {
+	var result strings.Builder
+	for pri, tracked := range t.tracked {
+		if len(tracked) > 0 {
+			result.WriteString(fmt.Sprintf("%v:\n", raftpb.Priority(pri)))
+			for _, tr := range tracked {
+				result.WriteString(fmt.Sprintf("  term=%d index=%-2d tokens=%-3d\n",
+					tr.term, tr.index, tr.tokens))
+			}
+		}
+	}
+	return result.String()
+}
+
+func formatUntracked(untracked [raftpb.NumPriorities]kvflowcontrol.Tokens) string {
+	var buf strings.Builder
+	for pri, tokens := range untracked {
+		if tokens > 0 {
+			buf.WriteString(fmt.Sprintf("returned: tokens=%-4d pri=%v\n", tokens, raftpb.Priority(pri)))
+		}
+	}
+	return buf.String()
+}
+
+func TestTokenTracker(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	ctx := context.Background()
+	tracker := NewTracker(kvflowcontrol.Stream{})
+	datadriven.RunTest(t, "testdata/token_tracker", func(t *testing.T, d *datadriven.TestData) string {
+		switch d.Cmd {
+		case "track":
+			var buf strings.Builder
+			for _, line := range strings.Split(d.Input, "\n") {
+				line = strings.TrimSpace(line)
+				parts := strings.Split(line, " ")
+				require.Len(t, parts, 4)
+
+				parts[0] = strings.TrimSpace(parts[0])
+				require.True(t, strings.HasPrefix(parts[0], "term="))
+				parts[0] = strings.TrimPrefix(parts[0], "term=")
+				term, err := strconv.ParseUint(parts[0], 10, 64)
+				require.NoError(t, err)
+
+				parts[1] = strings.TrimSpace(parts[1])
+				require.True(t, strings.HasPrefix(parts[1], "index="))
+				parts[1] = strings.TrimPrefix(parts[1], "index=")
+				index, err := strconv.ParseUint(parts[1], 10, 64)
+				require.NoError(t, err)
+
+				parts[2] = strings.TrimSpace(parts[2])
+				require.True(t, strings.HasPrefix(parts[2], "tokens="))
+				parts[2] = strings.TrimPrefix(parts[2], "tokens=")
+				tokens, err := strconv.ParseInt(parts[2], 10, 64)
+				require.NoError(t, err)
+
+				parts[3] = strings.TrimSpace(parts[3])
+				require.True(t, strings.HasPrefix(parts[3], "pri="))
+				parts[3] = strings.TrimPrefix(parts[3], "pri=")
+				pri := AdmissionToRaftPriority(parsePriority(t, parts[3]))
+
+				tracker.Track(ctx, term, index, pri, kvflowcontrol.Tokens(tokens))
+				buf.WriteString(fmt.Sprintf("tracked: term=%d index=%-2d tokens=%-3d pri=%v\n",
+					term, index, tokens, pri))
+			}
+			return buf.String()
+
+		case "untrack":
+			var term uint64
+			d.ScanArgs(t, "term", &term)
+			var admitted [raftpb.NumPriorities]uint64
+			for _, line := range strings.Split(d.Input, "\n") {
+				line = strings.TrimSpace(line)
+				if line == "" {
+					continue
+				}
+				parts := strings.Split(line, "=")
+				require.Len(t, parts, 2)
+				priStr := strings.TrimSpace(parts[0])
+				indexStr := strings.TrimSpace(parts[1])
+				pri := AdmissionToRaftPriority(parsePriority(t, priStr))
+				index, err := strconv.ParseUint(indexStr, 10, 64)
+				require.NoError(t, err)
+				admitted[pri] = index
+			}
+			return formatUntracked(tracker.Untrack(term, admitted))
+
+		case "untrack_ge":
+			var index uint64
+			d.ScanArgs(t, "index", &index)
+			return formatUntracked(tracker.UntrackGE(index))
+
+		case "untrack_all":
+			return formatUntracked(tracker.UntrackAll())
+
+		case "state":
+			return formatTrackerState(tracker)
+
+		default:
+			return fmt.Sprintf("unknown command: %s", d.Cmd)
+		}
+	})
+}
