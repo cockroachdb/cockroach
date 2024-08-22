f5035b60d097f5985b64f07c1a8331551adf3c06
Rafi Shamim <rafi@cockroachlabs.com>
2024-08-22 17:11:27 -0400

    sql: fix checkClusterSettingValuesAreEquivalent for final release fence versions
    When the `internal` field of a clusterversion is `0`, which
    it would be for all final cluster versions, the corresponding
    fence version will have an internal version of `-1`.
    
    The logic in checkClusterSettingValuesAreEquivalent did not account for
    this. It was assuming that one could always decrement the internal
    version by 1 in order to get the predecessor version. Now, we use
    ListBetween instead, which does not need this assumption.
    
    Release note (bug fix): Fixed a rare bug in SHOW CLUSTER SETTING that
    could cause it to fail with an error like `timed out: value differs
    between local setting and KV`.

--- pkg/clusterversion/clusterversion.go
+++ pkg/clusterversion/clusterversion.go
@@ -270,6 +270,31 @@ func (cv ClusterVersion) Encode() []byte {
 	return encoded
 }
 
+// FenceVersion is the fence version -- the internal immediately prior -- for
+// the given version.
+//
+// Fence versions allow the upgrades infrastructure to safely step through
+// consecutive cluster versions in the presence of Nodes (running any binary
+// version) being added to the cluster. See the upgrademanager package for
+// intended usage.
+//
+// Fence versions (and the upgrades infrastructure entirely) were introduced in
+// the 21.1 release cycle. In the same release cycle, we introduced the
+// invariant that new user-defined versions (users being crdb engineers) must
+// always have even-numbered Internal versions, thus reserving the odd numbers
+// to slot in fence versions for each cluster version. See top-level
+// documentation in the clusterversion package for more details.
+func (cv ClusterVersion) FenceVersion() ClusterVersion {
+	if (cv.Internal % 2) != 0 {
+		panic(errors.Newf("only even numbered internal versions allowed, found %s", cv.Version))
+	}
+	// NB: Internal may be negative after this. This is the case for all final
+	// versions for a release.
+	fenceCV := cv
+	fenceCV.Internal--
+	return fenceCV
+}
+
 var _ settings.ClusterVersionImpl = ClusterVersion{}
 
 // EncodingFromVersionStr is a shorthand to generate an encoded cluster version
