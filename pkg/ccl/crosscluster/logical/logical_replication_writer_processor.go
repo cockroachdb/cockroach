9b4e3e305fc81440abf434452da8641b32b9157d
Michael Butler <butler@cockroachlabs.com>
2024-08-23 15:51:48 -0400

    crosscluster/logical: rename FilterRangefeed to IgnoreCDCIgnoredTTLDeletes
    This clarifies how bespoke the cdc ignored ttl delete logic is.
    
    Epic: None
    
    Release note: none

--- pkg/ccl/crosscluster/logical/logical_replication_writer_processor.go
+++ pkg/ccl/crosscluster/logical/logical_replication_writer_processor.go
@@ -289,7 +289,7 @@ func (lrw *logicalReplicationWriterProcessor) Start(ctx context.Context) {
 
 	if streamingKnobs, ok := lrw.FlowCtx.TestingKnobs().StreamingTestingKnobs.(*sql.StreamingTestingKnobs); ok {
 		if streamingKnobs != nil && streamingKnobs.BeforeClientSubscribe != nil {
-			streamingKnobs.BeforeClientSubscribe(addr, string(token), lrw.frontier, lrw.spec.FilterRangefeed)
+			streamingKnobs.BeforeClientSubscribe(addr, string(token), lrw.frontier, lrw.spec.IgnoreCDCIgnoredTTLDeletes)
 		}
 	}
 	sub, err := streamClient.Subscribe(ctx,
@@ -297,7 +297,7 @@ func (lrw *logicalReplicationWriterProcessor) Start(ctx context.Context) {
 		int32(lrw.FlowCtx.NodeID.SQLInstanceID()), lrw.ProcessorID,
 		token,
 		lrw.spec.InitialScanTimestamp, lrw.frontier,
-		streamclient.WithFiltering(lrw.spec.FilterRangefeed),
+		streamclient.WithFiltering(lrw.spec.IgnoreCDCIgnoredTTLDeletes),
 		streamclient.WithDiff(true),
 	)
 	if err != nil {
