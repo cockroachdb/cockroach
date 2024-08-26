dd6077ba729690783ce0254a27461cba131d9d22
rharding6373 <harding@cockroachlabs.com>
2024-08-26 11:11:25 -0700

    server: wrap ctx err on cancelation when iterating over nodes
    When iterating over all nodes, if the context is done causing the
    iteration to cancel, wrap the context error to include it for consumers
    of the error.
    
    Epic: None
    Fixes: #129531
    
    Release note: None

--- pkg/server/status.go
+++ pkg/server/status.go
@@ -3248,7 +3248,7 @@ func iterateNodes[Client, Result any](
 				responseFn(res.nodeID, res.response)
 			}
 		case <-ctx.Done():
-			resultErr = errors.Errorf("request of %s canceled before completion", errorCtx)
+			resultErr = errors.Wrapf(ctx.Err(), "request of %s canceled before completion", errorCtx)
 		}
 		numNodes--
 	}
