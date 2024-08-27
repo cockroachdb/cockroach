5881ed1c3acb5d15c4a19e091cb0003cf0a632a0
David Taylor <tinystatemachine@gmail.com>
2024-08-27 18:14:13 +0000

    backupccl: do restore chunk work dispatch/wait in group
    Previously restore's split and scatter processor created one group, g2,
    that ran the tasks which split off coarse chunks, and another group g
    that ran a task which waited on g2 to complete and also ran all the tasks
    which processed the chunks created by the tasks in g2.
    
    However, if the workers in this second group encountered an error and
    exited, they would no longer be consuming chunks placed on the channel
    by the coarse tasks in g2, but the g2 group itself did not cancel since
    it was created using the same parent context as used to create g, rather
    than the context in g which is canceled when one of g's tasks fails.
    
    This meant the chunk creation goroutines did not cancel when the chunk
    processing goroutines exited on failure, and became stuck.
    
    Now the entire chunk creation phase -- creating the g2 group, starting
    its workers and then waiting for them to finish -- is moved into a task
    in the g group that will cancel if any other task in that group fails.
    
    Release note (bug fix): fix a bug that could cause RESTORE to hang after
    encountering transient errors from the storage layer.
    
    Epic: none.

--- pkg/ccl/backupccl/generative_split_and_scatter_processor_test.go
+++ pkg/ccl/backupccl/generative_split_and_scatter_processor_test.go
@@ -29,6 +29,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
+	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/require"
 )
 
@@ -229,23 +230,34 @@ func TestRunGenerativeSplitAndScatterRandomizedDestOnFailScatter(t *testing.T) {
 		// and can break if it changes.
 		require.GreaterOrEqual(t, cnt, 2)
 	}
+
+	// Also test that errors from split mid-chunk are returned (this deadlocked at
+	// one point).
+	spec.ChunkSize = 2
+	require.Error(t, runGenerativeSplitAndScatter(ctx, &flowCtx, &spec,
+		[]splitAndScatterer{&scatterAlwaysFailsSplitScatterer{}},
+		[]splitAndScatterer{&scatterAlwaysFailsSplitScatterer{err: errors.New("injected")}},
+		make(chan entryNode, 1000),
+		&cache,
+	))
 }
 
 // scatterAlwaysFailsSplitScatterer always fails the scatter and returns 0 as
 // the chunk destination.
 type scatterAlwaysFailsSplitScatterer struct {
+	err error
 }
 
 func (t *scatterAlwaysFailsSplitScatterer) split(
 	ctx context.Context, codec keys.SQLCodec, splitKey roachpb.Key,
 ) error {
-	return nil
+	return t.err
 }
 
 func (t *scatterAlwaysFailsSplitScatterer) scatter(
 	ctx context.Context, codec keys.SQLCodec, scatterKey roachpb.Key,
 ) (roachpb.NodeID, error) {
-	return 0, nil
+	return 0, t.err
 }
 
 func makeTestingGenerativeSplitAndScatterSpec(
