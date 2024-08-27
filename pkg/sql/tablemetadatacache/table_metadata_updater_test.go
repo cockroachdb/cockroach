438456944d4e70068302945f73dcd549f3fdb162
Xin Hao Zhang <xzhang@cockroachlabs.com>
2024-08-27 16:58:31 -0400

    tablemetadatacache: collect data & write to system.table_metadata
    This commit adds the execution logic to the UPDATE TABLE
    METADATA job to populate the system.table_metadata
    table. The update process is managed by the new
    `tableMetadataUpdater` struct.
    
    Job execution summary:
    1. Rows in `system.table_metadata` are pruned if the table_id
    no longer exists in `system.namespace`.
    2. tableMetadataIterator collects table metadata in batches by
    paginating `system.namespace` (current batch size of 20).
    3. For each row from system.namespace, we decode the descriptor
    from system.namespace.
    3. One RPC is issued per batch to collect the batch's table span
    stats.
    4. Each batch is prepped and upserted into system.table_metadata.
    
    Fixes: #128993
    Epic: CRDB-37558
    Release note: None

---
+++ pkg/sql/tablemetadatacache/table_metadata_updater_test.go
@@ -0,0 +1,75 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package tablemetadatacache
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/sql/isql"
+	"github.com/cockroachdb/cockroach/pkg/testutils/datapathutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/datadriven"
+)
+
+// TestDataDrivenTableMetadataCacheUpdater tests the operations performed by
+// tableMetadataCacheUpdater. It reads data written to system.table_metadata
+// by the cache updater to ensure the udpates are valid.
+func TestDataDrivenTableMetadataCacheUpdater(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	ctx := context.Background()
+	s := serverutils.StartServerOnly(t, base.TestServerArgs{})
+	defer s.Stopper().Stop(ctx)
+
+	queryConn := s.ApplicationLayer().SQLConn(t)
+	s.ApplicationLayer().DB()
+
+	datadriven.Walk(t, datapathutils.TestDataPath(t, ""), func(t *testing.T, path string) {
+		datadriven.RunTest(t, path, func(t *testing.T, d *datadriven.TestData) string {
+			switch d.Cmd {
+			case "query":
+				rows, err := queryConn.Query(d.Input)
+				if err != nil {
+					return err.Error()
+				}
+				res, err := sqlutils.RowsToDataDrivenOutput(rows)
+				if err != nil {
+					return err.Error()
+				}
+				return res
+			case "update-cache":
+				updater := newTableMetadataUpdater(s.InternalExecutor().(isql.Executor))
+				updated, err := updater.updateCache(ctx)
+				if err != nil {
+					return err.Error()
+				}
+				return fmt.Sprintf("updated %d table(s)", updated)
+			case "prune-cache":
+				updater := newTableMetadataUpdater(s.InternalExecutor().(isql.Executor))
+				pruned, err := updater.pruneCache(ctx)
+				if err != nil {
+					return err.Error()
+				}
+				return fmt.Sprintf("pruned %d table(s)", pruned)
+			default:
+				return "unknown command"
+			}
+		})
+	})
+
+}
