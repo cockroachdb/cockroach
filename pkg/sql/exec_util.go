9314e38385b9525cfe651ccd41be067053b6baab
Matt Spilchen <matt.spilchen@cockroachlabs.com>
2024-08-28 11:53:21 -0300

    server/license: store cluster init time in the KV system keyspace
    We need to store cluster-level metadata for license policy enforcement.
    This metadata must be protected from modification, so it cannot be
    stored in any system table. This PR introduces a new field in the system
    keyspace to store the cluster's initialization start time, which will be
    used in a follow-up PR to track the grace period for clusters without a
    license. The metadata needs to be stored at the cluster level to align
    with the license's scope.
    
    A new struct, License.Enforcer, is responsible for writing and caching
    the new timestamp. This struct will be extended in future PRs to check
    for throttling in clusters that violate our license policies.
    
    This update will be backported to versions 23.1, 23.2, 24.1, and 24.2.
    
    Epic: CRDB-39988
    Informs: CRDB-40064
    Release note: None

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -53,6 +53,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/rpc"
 	"github.com/cockroachdb/cockroach/pkg/security/username"
 	"github.com/cockroachdb/cockroach/pkg/server/autoconfig/acprovider"
+	"github.com/cockroachdb/cockroach/pkg/server/license"
 	"github.com/cockroachdb/cockroach/pkg/server/pgurl"
 	"github.com/cockroachdb/cockroach/pkg/server/serverpb"
 	"github.com/cockroachdb/cockroach/pkg/server/status/statuspb"
@@ -1500,6 +1501,9 @@ type ExecutorConfig struct {
 	// VirtualClusterName contains the name of the virtual cluster
 	// (tenant).
 	VirtualClusterName roachpb.TenantName
+
+	// LicenseEnforcer is used to enforce the license profiles.
+	LicenseEnforcer *license.Enforcer
 }
 
 // UpdateVersionSystemSettingHook provides a callback that allows us
