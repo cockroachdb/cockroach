7330463086e65983b8d69af8c31915681db35767
Pavel Kalinnikov <pavel@cockroachlabs.com>
2024-08-28 12:53:14 +0100

    logstore: introduce MustSync helper
    Epic: none
    Release note: none

--- pkg/kv/kvserver/logstore/logstore.go
+++ pkg/kv/kvserver/logstore/logstore.go
@@ -73,6 +73,11 @@ func MakeMsgStorageAppend(m raftpb.Message) MsgStorageAppend {
 	return MsgStorageAppend(m)
 }
 
+// MustSync returns true if this storage write must be synced.
+func (m *MsgStorageAppend) MustSync() bool {
+	return len(m.Responses) != 0
+}
+
 // OnDone returns the storage write post-processing information.
 func (m *MsgStorageAppend) OnDone() MsgStorageAppendDone { return m.Responses }
 
@@ -271,7 +276,7 @@ func (s *LogStore) storeEntriesAndCommitBatch(
 	// (Replica), so this comment might need to move.
 	stats.PebbleBegin = timeutil.Now()
 	stats.PebbleBytes = int64(batch.Len())
-	wantsSync := len(m.Responses) > 0
+	wantsSync := m.MustSync()
 	willSync := wantsSync && !DisableSyncRaftLog.Get(&s.Settings.SV)
 	// Use the non-blocking log sync path if we are performing a log sync ...
 	nonBlockingSync := willSync &&
