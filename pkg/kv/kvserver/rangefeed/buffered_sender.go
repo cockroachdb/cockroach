f0276175231bc94f9e8b0c1ec9a60f3e62a37dbd
Wenyi Hu <wenyi@cockroachlabs.com>
2024-09-03 02:52:36 -0400

    kvserver/rangefeed: add node level buffered sender
    This patch is the last step for reducing long-running O(ranges) goroutines in
    kvserver/rangefeed. It changes the BufferedSender to use a queue to buffer
    events before forwarding them to underlying grpc stream.
    
    Part of: #110432
    Release note: note

--- pkg/kv/kvserver/rangefeed/buffered_sender.go
+++ pkg/kv/kvserver/rangefeed/buffered_sender.go
@@ -9,7 +9,10 @@ import (
 	"context"
 
 	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/errors"
 )
 
 //            ┌─────────────────┐
@@ -39,6 +42,7 @@ import (
 //                    │                           │               │
 //                    └───────────────────────────┘───────────────┘
 //               BufferedPerRangeEventSink.Send    BufferedPerRangeEventSink.SendError
+//
 
 // BufferedSender is embedded in every rangefeed.BufferedPerRangeEventSink,
 // serving as a helper which buffers events before forwarding events to the
@@ -50,12 +54,24 @@ type BufferedSender struct {
 	// Note that lockedMuxStream wraps the underlying grpc server stream, ensuring
 	// thread safety.
 	sender ServerStreamSender
+
+	queueMu struct {
+		syncutil.Mutex
+		stopped bool
+		buffer  *eventQueue
+	}
+
+	// Unblocking channel to notify the BufferedSender.run goroutine that there
+	// are events to send.
+	notifyDataC chan struct{}
 }
 
 func NewBufferedSender(sender ServerStreamSender) *BufferedSender {
 	bs := &BufferedSender{
 		sender: sender,
 	}
+	bs.queueMu.buffer = newEventQueue()
+	bs.notifyDataC = make(chan struct{}, 1)
 	return bs
 }
 
@@ -66,7 +82,37 @@ func NewBufferedSender(sender ServerStreamSender) *BufferedSender {
 // and release it if the return error is non-nil. Note that it is safe to send
 // error events without being blocked for too long.
 func (bs *BufferedSender) send(ev *kvpb.MuxRangeFeedEvent, alloc *SharedBudgetAllocation) error {
-	panic("unimplemented: buffered sender for rangefeed #126560")
+	// Make sure this is from catch up scan.
+	if ev.Error != nil && alloc != nil {
+		return bs.sendUnbuffered(ev)
+	}
+	return bs.sendBuffered(ev, alloc)
+}
+
+// sendBuffered buffers the event before sending it to the underlying grpc
+// stream. It should not block since errors are sent directly here.
+func (bs *BufferedSender) sendBuffered(
+	ev *kvpb.MuxRangeFeedEvent, alloc *SharedBudgetAllocation,
+) error {
+	bs.queueMu.Lock()
+	defer bs.queueMu.Unlock()
+	if bs.queueMu.stopped {
+		log.Errorf(context.Background(), "stream sender is stopped")
+		return errors.New("stream sender is stopped")
+	}
+	alloc.Use(context.Background())
+	bs.queueMu.buffer.pushBack(sharedMuxEvent{ev, alloc})
+	select {
+	case bs.notifyDataC <- struct{}{}:
+	default:
+	}
+	return nil
+}
+
+// sendUnbuffered sends the event directly to the underlying ServerStreamSender.
+// It bypasses the buffer.
+func (bs *BufferedSender) sendUnbuffered(ev *kvpb.MuxRangeFeedEvent) error {
+	return bs.sender.Send(ev)
 }
 
 // run forwards buffered events back to the client. run is expected to be called
@@ -77,11 +123,50 @@ func (bs *BufferedSender) send(ev *kvpb.MuxRangeFeedEvent, alloc *SharedBudgetAl
 func (bs *BufferedSender) run(
 	ctx context.Context, stopper *stop.Stopper, onError func(streamID int64),
 ) error {
-	panic("unimplemented: buffered sender for rangefeed #126560")
+	for {
+		select {
+		case <-ctx.Done():
+			// Top level goroutine will receive the context cancellation and handle
+			// ctx.Err().
+			return nil
+		case <-stopper.ShouldQuiesce():
+			// Top level goroutine will receive the stopper quiesce signal and handle
+			// error.
+			return nil
+		case <-bs.notifyDataC:
+			for {
+				e, success := bs.popFront()
+				if success {
+					err := bs.sender.Send(e.ev)
+					e.alloc.Release(ctx)
+					if e.ev.Error != nil {
+						onError(e.ev.StreamID)
+					}
+					if err != nil {
+						return err
+					}
+				} else {
+					break
+				}
+			}
+		}
+	}
+}
+
+// popFront pops the front event from the buffer queue. It returns the event and
+// a boolean indicating if the event was successfully popped.
+func (bs *BufferedSender) popFront() (e sharedMuxEvent, success bool) {
+	bs.queueMu.Lock()
+	defer bs.queueMu.Unlock()
+	event, ok := bs.queueMu.buffer.popFront()
+	return event, ok
 }
 
 // cleanup is called when the sender is stopped. It is expected to free up
 // buffer queue and no new events should be buffered after this.
 func (bs *BufferedSender) cleanup() {
-	panic("unimplemented: buffered sender for rangefeed #126560")
+	bs.queueMu.Lock()
+	defer bs.queueMu.Unlock()
+	bs.queueMu.stopped = true
+	bs.queueMu.buffer.free()
 }
