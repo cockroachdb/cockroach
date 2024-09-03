2c83ec1e1289728b7e131709d19f0a85cc242725
Renato Costa <renato@cockroachlabs.com>
2024-09-03 13:57:30 -0400

    roachtest: pass an `ExpanderConfig` in the `RunWithDetails` API
    This API was missed when the ability to set a default virtual cluster
    was introduced. This meant that commands called using
    `c.RunWithDetails` (and sibling functions) would not expand in the
    same way, particularly when it comes to computing pgurls and ports.
    
    Epic: none
    
    Release note: None

--- pkg/roachprod/install/cluster_synced.go
+++ pkg/roachprod/install/cluster_synced.go
@@ -1380,6 +1380,7 @@ func (c *SyncedCluster) RunWithDetails(
 			includeRoachprodEnvVars: true,
 			stdout:                  l.Stdout,
 			stderr:                  l.Stderr,
+			expanderConfig:          options.ExpanderConfig,
 		}
 		result, err := c.runCmdOnSingleNode(ctx, l, node, cmd, opts)
 		return result, err
