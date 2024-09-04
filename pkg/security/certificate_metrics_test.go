d1d4eaaea57e5d8d1a10b5a3ab414054a173a4a4
angles-n-daemons <brian.dillmann@cockroachlabs.com>
2024-09-04 16:17:26 -0400

    Epic: CRDB-40209 Fixes: #77376
    Release note (ops change):
         ---
         security: add ttl metrics for certificate expiration
    
         Currently, cockroach only exposes point in time certificate
         expiration metrics. If the certificate is to expire 1 day from now,
         we expose a gauge `security.certificate.expiration.<cert-type>`
         which is the unix timestamp when it will expire. This PR also
         exposes a ttl metric `security.certificate.ttl.<cert-type>` so that
         consumers of this information can run operations based on their
         distance to certificate expiration without additional
         transformations.
    
         Additionally, this PR refactors how the expiration gauges are set,
         so that reads of the gauge directly reference the value of the
         certificate.

---
+++ pkg/security/certificate_metrics_test.go
@@ -0,0 +1,57 @@
+package security_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/security"
+	"github.com/cockroachdb/cockroach/pkg/security/securityassets"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+)
+
+// TestMetricsValues asserts that with the appropriate certificates on disk,
+// the correct expiration and ttl values are set on the metrics vars that are
+// exposed to our collectors. It uses a single key pair to approximate the
+// behavior across other key pairs.
+func TestMetricsValues(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	// required to read certs from disk in tests
+	securityassets.ResetLoader()
+	defer ResetTest()
+
+	now := timeutil.Unix(1, 0)
+	certsDir := t.TempDir()
+	_, caCert := makeTestCert(t, "", 0, nil, timeutil.Unix(2, 0))
+
+	for fileNum, f := range []struct {
+		name     string
+		mode     os.FileMode
+		contents []byte
+	}{
+		{"ca.crt", 0777, caCert},
+		{"ca.key", 0777, []byte("ca.key")},
+	} {
+
+		n := f.name
+		if err := os.WriteFile(filepath.Join(certsDir, n), f.contents, f.mode); err != nil {
+			t.Fatalf("#%d: could not write file %s: %v", fileNum, n, err)
+		}
+	}
+
+	cm, err := security.NewCertificateManager(certsDir, security.CommandTLSSettings{}, security.WithTimeSource(timeutil.NewManualTime(now)))
+	if err != nil {
+		t.Error(err)
+	}
+
+	exp := cm.Metrics().CAExpiration.Value()
+	ttl := cm.Metrics().CATTL.Value()
+	if exp != 2 {
+		t.Fatalf("Expected certificate expiration to be %d, but instead got %d", 2, exp)
+	}
+	if ttl != 1 {
+		t.Fatalf("Expected certificate ttl to be %d, but instead got %d", 1, ttl)
+	}
+}
