ced1bd7689b35d2eebfc488e8ee56d35795670b8
Matt Spilchen <matt.spilchen@cockroachlabs.com>
2024-09-04 12:34:38 -0300

    server/license: Add core throttling logic for license policy enforcement
    As part of deprecating the CockroachDB core license, new logic has been
    added to throttle access when license policies are violated. This change
    introduces the core logic, though it does not yet integrate with the SQL
    engine, so throttling won't affect SQL users at this stage.
    
    The LicenseEnforcer has been enhanced with throttling logic, including a
    new API, MaybeFailIfThrottled, which determines whether throttling
    should occur and returns an error if it is active.
    
    To make throttling decisions, the LicenseEnforcer requires access to
    license state. However, since the license code remains in the CCL
    package, which cannot be imported directly into the server code, a
    workaround is used. When the enterprise.license changes, a callback
    (from CCL to server packages) updates the state. Initially, this
    callback points to a stub function, which the CCL package replaces with
    the actual implementation upon import.
    
    The implementation also includes handling for mandatory telemetry data
    required by certain licenses. While the logic is in place to make
    decisions based on the last telemetry ping, the integration with the
    diagnostics reporter for actual telemetry ping times will be completed
    in a follow-up change.
    
    This change will be backported to 23.1, 23.2, 24.1 and 24.2.
    
    Epic: CRDB-39988
    Informs: CRDB-39991
    Release note: None

--- pkg/server/license/enforcer_test.go
+++ pkg/server/license/enforcer_test.go
@@ -12,7 +12,10 @@ package license_test
 
 import (
 	"context"
+	"fmt"
+	"regexp"
 	"testing"
+	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/server"
@@ -26,6 +29,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type mockTelemetryStatusReporter struct {
+	lastPingTime time.Time
+}
+
+func (m mockTelemetryStatusReporter) GetLastSuccessfulTelemetryPing() time.Time {
+	return m.lastPingTime
+}
+
 func TestGracePeriodInitTSCache(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
@@ -33,6 +44,7 @@ func TestGracePeriodInitTSCache(t *testing.T) {
 	// This is the timestamp that we'll override the grace period init timestamp with.
 	// This will be set when bringing up the server.
 	ts1 := timeutil.Unix(1724329716, 0)
+	ts1End := ts1.Add(7 * 24 * time.Hour) // Calculate the end of the grace period based on ts1
 
 	ctx := context.Background()
 	srv := serverutils.StartServerOnly(t, base.TestServerArgs{
@@ -51,20 +63,111 @@ func TestGracePeriodInitTSCache(t *testing.T) {
 	// timestamp that was already setup.
 	enforcer := &license.Enforcer{}
 	ts2 := ts1.Add(1)
+	ts2End := ts2.Add(7 * 24 * time.Hour) // Calculate the end of the grace period
 	enforcer.TestingKnobs = &license.TestingKnobs{
 		EnableGracePeriodInitTSWrite: true,
 		OverrideStartTime:            &ts2,
 	}
 	// Ensure request for the grace period init ts1 before start just returns the start
 	// time used when the enforcer was created.
-	require.Equal(t, ts2, enforcer.GetGracePeriodInitTS())
+	require.Equal(t, ts2End, enforcer.GetClusterInitGracePeriodEndTS())
 	// Start the enforcer to read the timestamp from the KV.
-	err := enforcer.Start(ctx, srv.SystemLayer().InternalDB().(descs.DB))
+	enforcer.SetTelemetryStatusReporter(&mockTelemetryStatusReporter{lastPingTime: ts1})
+	err := enforcer.Start(ctx, srv.ClusterSettings(), srv.SystemLayer().InternalDB().(descs.DB), false /* initialStart */)
 	require.NoError(t, err)
-	require.Equal(t, ts1, enforcer.GetGracePeriodInitTS())
+	require.Equal(t, ts1End, enforcer.GetClusterInitGracePeriodEndTS())
 
 	// Access the enforcer that is cached in the executor config to make sure they
 	// work for the system tenant and secondary tenant.
-	require.Equal(t, ts1, srv.SystemLayer().ExecutorConfig().(sql.ExecutorConfig).LicenseEnforcer.GetGracePeriodInitTS())
-	require.Equal(t, ts1, srv.ApplicationLayer().ExecutorConfig().(sql.ExecutorConfig).LicenseEnforcer.GetGracePeriodInitTS())
+	require.Equal(t, ts1End, srv.SystemLayer().ExecutorConfig().(sql.ExecutorConfig).LicenseEnforcer.GetClusterInitGracePeriodEndTS())
+	require.Equal(t, ts1End, srv.ApplicationLayer().ExecutorConfig().(sql.ExecutorConfig).LicenseEnforcer.GetClusterInitGracePeriodEndTS())
+}
+
+func TestThrottle(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	ctx := context.Background()
+
+	const UnderTxnThreshold = 3
+	const OverTxnThreshold = 7
+
+	t0 := time.Unix(1724884362, 0)
+	t1d := t0.Add(24 * time.Hour)
+	t8d := t0.Add(8 * 24 * time.Hour)
+	t10d := t0.Add(10 * 24 * time.Hour)
+	t15d := t0.Add(15 * 24 * time.Hour)
+	t17d := t0.Add(17 * 24 * time.Hour)
+	t18d := t0.Add(18 * 24 * time.Hour)
+	t30d := t0.Add(30 * 24 * time.Hour)
+	t45d := t0.Add(45 * 24 * time.Hour)
+	t46d := t0.Add(46 * 24 * time.Hour)
+
+	for i, tc := range []struct {
+		openTxnsCount         int64
+		licType               license.LicType
+		gracePeriodInit       time.Time
+		lastTelemetryPingTime time.Time
+		licExpiry             time.Time
+		checkTs               time.Time
+		expectedErrRegex      string
+	}{
+		// Expired free license but under the transaction threshold
+		{UnderTxnThreshold, license.LicTypeFree, t0, t1d, t8d, t45d, ""},
+		// Expired trial license but under the transaction threshold
+		{UnderTxnThreshold, license.LicTypeTrial, t0, t30d, t8d, t45d, ""},
+		// Over the transaction threshold but not expired
+		{OverTxnThreshold, license.LicTypeFree, t0, t10d, t45d, t10d, ""},
+		// Expired free license, past the grace period
+		{OverTxnThreshold, license.LicTypeFree, t0, t30d, t10d, t45d, "License expired"},
+		// Expired free license, but not past the grace period
+		{OverTxnThreshold, license.LicTypeFree, t0, t30d, t10d, t17d, ""},
+		// Valid free license, but telemetry ping hasn't been received in 7 days.
+		{OverTxnThreshold, license.LicTypeFree, t0, t10d, t45d, t17d, ""},
+		// Valid free license, but telemetry ping hasn't been received in 8 days.
+		{OverTxnThreshold, license.LicTypeFree, t0, t10d, t45d, t18d, "diagnostic reporting"},
+		// No license but within grace period still
+		{OverTxnThreshold, license.LicTypeNone, t0, t0, t0, t1d, ""},
+		// No license but beyond grace period
+		{OverTxnThreshold, license.LicTypeNone, t0, t0, t0, t8d, "No license installed"},
+		// Trial license has expired but still within grace period
+		{OverTxnThreshold, license.LicTypeTrial, t0, t30d, t10d, t15d, ""},
+		// Trial license has expired and just at the edge of the grace period.
+		{OverTxnThreshold, license.LicTypeTrial, t0, t45d, t10d, t17d, ""},
+		// Trial license has expired and just beyond the grace period.
+		{OverTxnThreshold, license.LicTypeTrial, t0, t45d, t10d, t18d, "License expired"},
+		// No throttling if past the expiry of an enterprise license
+		{OverTxnThreshold, license.LicTypeEnterprise, t0, t0, t8d, t46d, ""},
+		// Telemetry isn't needed for enterprise license
+		{OverTxnThreshold, license.LicTypeEnterprise, t0, t0, t45d, t30d, ""},
+		// Telemetry isn't needed for evaluation license
+		{OverTxnThreshold, license.LicTypeEvaluation, t0, t0, t45d, t30d, ""},
+		// Evaluation license doesn't throttle if expired but within grace period.
+		{OverTxnThreshold, license.LicTypeEvaluation, t0, t0, t15d, t30d, ""},
+		// Evaluation license does throttle if expired and beyond grace period.
+		{OverTxnThreshold, license.LicTypeEvaluation, t0, t0, t15d, t46d, "License expired"},
+	} {
+		t.Run(fmt.Sprintf("test %d", i), func(t *testing.T) {
+			e := license.Enforcer{
+				TestingKnobs: &license.TestingKnobs{
+					OverrideStartTime:         &tc.gracePeriodInit,
+					OverrideThrottleCheckTime: &tc.checkTs,
+				},
+			}
+			e.SetTelemetryStatusReporter(&mockTelemetryStatusReporter{
+				lastPingTime: tc.lastTelemetryPingTime,
+			})
+			e.RefreshForLicenseChange(tc.licType, tc.licExpiry)
+			err := e.MaybeFailIfThrottled(ctx, tc.openTxnsCount)
+			if tc.expectedErrRegex == "" {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				re := regexp.MustCompile(tc.expectedErrRegex)
+				match := re.MatchString(err.Error())
+				require.NotNil(t, match, "Error text %q doesn't match the expected regexp of %q",
+					err.Error(), tc.expectedErrRegex)
+			}
+		})
+	}
 }
