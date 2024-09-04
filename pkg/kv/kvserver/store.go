637cd5dbb2744d1bd125631315892ad989c00249
Austen McClernon <austen@cockroachlabs.com>
2024-09-04 17:04:16 -0400

    kvserver: enqueue decom ranges at an interval behind a setting
    When `kv.enqueue_in_replicate_queue_on_problem.interval` is set to a
    positive non-zero value, leaseholder replicas of ranges which have
    decommissioning replicas will be enqueued into the replicate queue every
    `kv.enqueue_in_replicate_queue_on_problem.interval` interval.
    
    When `kv.enqueue_in_replicate_queue_on_problem.interval` is set to 0,
    no enqueueing on decommissioning will take place, outside of the regular
    replica scanner.
    
    A recommended value for users enabling the enqueue (non-zero), is at
    least 15 minutes e.g.,
    
    ```
    SET CLUSTER SETTING
    kv.enqueue_in_replicate_queue_on_problem.interval='900s'
    ```
    
    Resolves: #130085
    Informs: #130199
    Release note: None

--- pkg/kv/kvserver/store.go
+++ pkg/kv/kvserver/store.go
@@ -3238,6 +3238,7 @@ func (s *Store) updateReplicationGauges(ctx context.Context) error {
 	)
 
 	now := s.cfg.Clock.NowAsClockTimestamp()
+	goNow := now.ToTimestamp().GoTime()
 	clusterNodes := s.ClusterNodeCount()
 
 	s.mu.RLock()
@@ -3302,6 +3303,9 @@ func (s *Store) updateReplicationGauges(ctx context.Context) error {
 				overreplicatedRangeCount++
 			}
 			if metrics.Decommissioning {
+				// NB: Enqueue is disabled by default from here and throttled async if
+				// enabled.
+				rep.maybeEnqueueProblemRange(ctx, goNow, metrics.LeaseValid, metrics.Leaseholder)
 				decommissioningRangeCount++
 			}
 		}
