8891143fc9d5e84a24c46968c819ba840822846b
Andrew Baptist <baptist@cockroachlabs.com>
2024-09-05 13:34:38 -0400

    roachtest: correctly wait for errors
    Previously the test did not check for any errors in any of the workers
    it launched. This made it hard to track down bugs or problems with
    running the workload.
    
    Epic: #129671
    
    Release note: None

--- pkg/cmd/roachtest/tests/admission_control_latency.go
+++ pkg/cmd/roachtest/tests/admission_control_latency.go
@@ -642,7 +642,7 @@ func (v variations) runTest(ctx context.Context, t test.Test, c cluster.Cluster)
 	t.Status("T2: running workload at stable rate")
 	var data *workloadData
 	cancelWorkload := m.GoWithCancel(func(ctx context.Context) error {
-		if data, err = v.workload.runWorkload(ctx, v, 0, stableRatePerNode); !errors.Is(err, context.Canceled) {
+		if data, err = v.workload.runWorkload(ctx, v, 0, stableRatePerNode); err != nil && !errors.Is(err, context.Canceled) {
 			return err
 		}
 		return nil
@@ -670,7 +670,7 @@ func (v variations) runTest(ctx context.Context, t test.Test, c cluster.Cluster)
 	t.L().Printf("Recovery interval     : %s", afterInterval)
 
 	cancelWorkload()
-	m.Wait()
+	require.NoError(t, m.WaitE())
 
 	baselineStats := data.worstStats(baselineInterval)
 	perturbationStats := data.worstStats(perturbationInterval)
