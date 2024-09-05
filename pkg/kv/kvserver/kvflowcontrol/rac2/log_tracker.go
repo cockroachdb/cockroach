9ed80c34562276bb4ad6d331d42e1d1fb16df367
Pavel Kalinnikov <pavel@cockroachlabs.com>
2024-09-05 18:25:12 +0100

    rac2: allow a snapshot at the last log mark
    There are cases when we accept a snapshot mathing the last log mark.
    This fixes TestAtomicReplicationChange.
    
    Epic: none
    Release note: none

--- pkg/kv/kvserver/kvflowcontrol/rac2/log_tracker.go
+++ pkg/kv/kvserver/kvflowcontrol/rac2/log_tracker.go
@@ -224,7 +224,7 @@ func (l *LogTracker) LogAdmitted(ctx context.Context, at LogMark, pri raftpb.Pri
 //
 // Returns true when the admitted vector might have changed.
 func (l *LogTracker) SnapSynced(ctx context.Context, mark LogMark) bool {
-	if !mark.After(l.last) {
+	if l.last.After(mark) {
 		l.errorf(ctx, "syncing stale snapshot %+v", mark)
 		return false
 	}
