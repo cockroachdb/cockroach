2a8038dfb479568133448602e6004049befc3634
angles-n-daemons <brian.dillmann@cockroachlabs.com>
2024-09-09 11:12:08 -0400

    db-console: update notification icon in alert box to use new cockroach logo
    The current icon for the notification alert is the outdated cockroach db
    logo. To make it consistent with the rest of the system, it needed to be
    updated. This change both removes the old logo from the codebase as well
    as uses the new one from our shared icon libary.
    
    Epic: none
    Fixes: #127829
    
    Release note (ui change): changes notification alert icon

--- pkg/ui/workspaces/db-console/src/views/shared/components/alertBox/index.tsx
+++ pkg/ui/workspaces/db-console/src/views/shared/components/alertBox/index.tsx
@@ -8,27 +8,27 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
+import { CockroachMarkLightFull } from "@cockroachlabs/icons";
 import classNames from "classnames";
 import React from "react";
 
 import { AlertInfo, AlertLevel } from "src/redux/alerts";
-import { trustIcon } from "src/util/trust";
-import {
-  warningIcon,
-  notificationIcon,
-  criticalIcon,
-} from "src/views/shared/components/icons";
+import { warningIcon, criticalIcon } from "src/views/shared/components/icons";
 
 import "./alertbox.styl";
+
+export function alertIconFromSVG(svg: string): JSX.IntrinsicElements["img"] {
+  return <img src={`data:image/svg+xml;utf8,${encodeURIComponent(svg)}`} />;
+}
 
 function alertIcon(level: AlertLevel) {
   switch (level) {
     case AlertLevel.CRITICAL:
-      return trustIcon(criticalIcon);
+      return criticalIcon;
     case AlertLevel.WARNING:
-      return trustIcon(warningIcon);
+      return alertIconFromSVG(warningIcon);
     default:
-      return trustIcon(notificationIcon);
+      return <CockroachMarkLightFull />;
   }
 }
 
@@ -68,10 +68,7 @@ export class AlertBox extends React.Component<AlertBoxProps, {}> {
           `alert-box--${AlertLevel[this.props.level].toLowerCase()}`,
         )}
       >
-        <div
-          className="alert-box__icon"
-          dangerouslySetInnerHTML={alertIcon(this.props.level)}
-        />
+        <div className="alert-box__icon">{alertIcon(this.props.level)}</div>
         {content}
         <div className="alert-box__dismiss">
           <a className="alert-box__link" onClick={this.props.dismiss}>
