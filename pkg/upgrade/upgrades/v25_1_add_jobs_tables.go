0933df456a1a2797eef97ccd61e9aa55a11abf1f
David Taylor <tinystatemachine@gmail.com>
2024-09-11 21:18:08 +0000

    upgrades: add job_progress, job_status tables
    This adds a new job_status and job_progress system table similar to the
    job_info table but for storing info that is shown in SHOW JOBS, while also
    extending system.jobs with additional state data (timestamps, messages)
    also shown in SHOW JOBS. Combined, these new columns and tables are intended
    to eventually back SHOW JOBS rather than decoding fields from the legacy
    info keys.
    
    Release note: none.
    Epic: none.

---
+++ pkg/upgrade/upgrades/v25_1_add_jobs_tables.go
@@ -0,0 +1,63 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package upgrades
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descs"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/systemschema"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/cockroach/pkg/upgrade"
+)
+
+// addJobsTables adds the job_progress, job_progress_history, job_status and
+// job_message tables.
+func addJobsTables(
+	ctx context.Context, cs clusterversion.ClusterVersion, d upgrade.TenantDeps,
+) error {
+	return d.DB.DescsTxn(ctx, func(ctx context.Context, txn descs.Txn) error {
+		if err := createSystemTable(
+			ctx, d.DB, d.Settings, d.Codec,
+			systemschema.SystemJobProgressTable,
+			tree.LocalityLevelTable,
+		); err != nil {
+			return err
+		}
+
+		if err := createSystemTable(
+			ctx, d.DB, d.Settings, d.Codec,
+			systemschema.SystemJobProgressHistoryTable,
+			tree.LocalityLevelTable,
+		); err != nil {
+			return err
+		}
+
+		if err := createSystemTable(
+			ctx, d.DB, d.Settings, d.Codec,
+			systemschema.SystemJobStatusTable,
+			tree.LocalityLevelTable,
+		); err != nil {
+			return err
+		}
+
+		if err := createSystemTable(
+			ctx, d.DB, d.Settings, d.Codec,
+			systemschema.SystemJobMessageTable,
+			tree.LocalityLevelTable,
+		); err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
