8788d6eeff64fad01dca1da3416dacfb965b3a33
Annie Pompa <annie@cockroachlabs.com>
2024-09-12 16:10:35 -0400

    schemachanger: fix subzone writes
    Updating a subzone config on a table without a
    pre-configured zone config needs to create a
    subzone placeholder. This is done by setting
    `num_replicas` to 0 in the table zone config.
    
    Fixes: #130605
    
    Release note: None

--- pkg/sql/schemachanger/scbuild/internal/scbuildstmt/zone_config_helpers.go
+++ pkg/sql/schemachanger/scbuild/internal/scbuildstmt/zone_config_helpers.go
@@ -108,11 +108,7 @@ type zoneConfigRetriever interface {
 	//     (if our target is a database).
 	//   - Otherwise, we will just return the found zone config
 	//     (so `subzoneId` and `subzone` will be nil).
-	getZoneConfig(b BuildCtx, inheritDefaultRange bool) (
-		*zonepb.ZoneConfig,
-		*zonepb.ZoneConfig,
-		error,
-	)
+	getZoneConfig(b BuildCtx, inheritDefaultRange bool) (*zonepb.ZoneConfig, error)
 }
 
 type zoneConfigMutator interface {
@@ -399,7 +395,7 @@ func fillIndexAndPartitionFromZoneSpecifier(
 // table by `targetID`.
 // If `targetID` is not found, a nil `zone` is returned.
 func lookUpSystemZonesTable(
-	b BuildCtx, objType zoneConfigObject, inheritDefaultRange bool,
+	b BuildCtx, objType zoneConfigObject, inheritDefaultRange bool, isSubzoneConfig bool,
 ) (zone *zonepb.ZoneConfig, subzones []zonepb.Subzone, err error) {
 	// Get the zone config of the DEFAULT RANGE
 	if inheritDefaultRange {
@@ -411,9 +407,9 @@ func lookUpSystemZonesTable(
 	} else {
 		// It's a descriptor-backed target (i.e. a database ID or a table ID)
 		zone = objType.retrievePartialZoneConfig(b)
-		// If we are dealing with Index subzones, clear out the zone config and
+		// If we are dealing with index subzones, clear out the zone config and
 		// just use the subzones.
-		if _, ok := objType.(*indexZoneConfigObj); ok {
+		if isSubzoneConfig {
 			if zone != nil && zone.Subzones != nil {
 				subzones = zone.Subzones
 				zone = nil
