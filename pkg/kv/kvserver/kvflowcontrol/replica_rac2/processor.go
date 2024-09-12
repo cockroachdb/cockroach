34a45c7dc4173ac8a6f118d91f3f175dde7c31b4
Pavel Kalinnikov <pavel@cockroachlabs.com>
2024-09-12 13:33:16 +0100

    replica_rac2: add pendingAdmitted mutex
    Epic: none
    Release note: none

--- pkg/kv/kvserver/kvflowcontrol/replica_rac2/processor.go
+++ pkg/kv/kvserver/kvflowcontrol/replica_rac2/processor.go
@@ -435,6 +435,10 @@ type processorImpl struct {
 		// State when leader, i.e., when leaderID == opts.ReplicaID, and v2
 		// protocol is enabled.
 		leader struct {
+			// pendingAdmittedMu protects pendingAdmitted. The map is populated from
+			// calls initiated by RaftTransport, so this mutex must be narrow.
+			// Locking order: pendingAdmittedMu < rcReferenceUpdateMu.
+			pendingAdmittedMu syncutil.RWMutex
 			// pendingAdmitted contains recently delivered admitted vectors. When this
 			// map is not empty, the range is scheduled for applying these vectors to
 			// the corresponding streams / token trackers. The map is cleared when the
@@ -684,13 +688,15 @@ func (p *processorImpl) closeLeaderStateRaftMuLockedProcLocked(ctx context.Conte
 		return
 	}
 	p.mu.leader.rc.CloseRaftMuLocked(ctx)
-	func() {
-		p.mu.leader.rcReferenceUpdateMu.Lock()
-		defer p.mu.leader.rcReferenceUpdateMu.Unlock()
-		p.mu.leader.rc = nil
-	}()
-	p.mu.leader.pendingAdmitted = nil
 	p.mu.leader.term = 0
+
+	p.mu.leader.pendingAdmittedMu.Lock()
+	defer p.mu.leader.pendingAdmittedMu.Unlock()
+	p.mu.leader.pendingAdmitted = nil
+
+	p.mu.leader.rcReferenceUpdateMu.Lock()
+	defer p.mu.leader.rcReferenceUpdateMu.Unlock()
+	p.mu.leader.rc = nil
 }
 
 func (p *processorImpl) createLeaderStateRaftMuLockedProcLocked(
@@ -699,21 +705,24 @@ func (p *processorImpl) createLeaderStateRaftMuLockedProcLocked(
 	if p.mu.leader.rc != nil {
 		panic("RangeController already exists")
 	}
-	func() {
-		p.mu.leader.rcReferenceUpdateMu.Lock()
-		defer p.mu.leader.rcReferenceUpdateMu.Unlock()
-		p.mu.leader.rc = p.opts.RangeControllerFactory.New(ctx, rangeControllerInitState{
-			replicaSet:     p.raftMu.replicas,
-			leaseholder:    p.mu.leaseholderID,
-			nextRaftIndex:  nextUnstableIndex,
-			rangeID:        p.opts.RangeID,
-			tenantID:       p.raftMu.tenantID,
-			localReplicaID: p.opts.ReplicaID,
-			raftInterface:  p.raftMu.raftNode,
-		})
-	}()
 	p.mu.leader.term = term
+	rc := p.opts.RangeControllerFactory.New(ctx, rangeControllerInitState{
+		replicaSet:     p.raftMu.replicas,
+		leaseholder:    p.mu.leaseholderID,
+		nextRaftIndex:  nextUnstableIndex,
+		rangeID:        p.opts.RangeID,
+		tenantID:       p.raftMu.tenantID,
+		localReplicaID: p.opts.ReplicaID,
+		raftInterface:  p.raftMu.raftNode,
+	})
+
+	p.mu.leader.pendingAdmittedMu.Lock()
+	defer p.mu.leader.pendingAdmittedMu.Unlock()
 	p.mu.leader.pendingAdmitted = map[roachpb.ReplicaID]rac2.AdmittedVector{}
+
+	p.mu.leader.rcReferenceUpdateMu.Lock()
+	defer p.mu.leader.rcReferenceUpdateMu.Unlock()
+	p.mu.leader.rc = rc
 }
 
 // HandleRaftReadyRaftMuLocked implements Processor.
@@ -890,6 +899,9 @@ func (p *processorImpl) EnqueuePiggybackedAdmittedAtLeader(
 	}
 	var admitted [raftpb.NumPriorities]uint64
 	copy(admitted[:], state.Admitted)
+
+	p.mu.leader.pendingAdmittedMu.Lock()
+	defer p.mu.leader.pendingAdmittedMu.Unlock()
 	// Merge in the received admitted vector. We are only interested in the
 	// highest admitted marks. Zero value always merges in favour of the new one.
 	p.mu.leader.pendingAdmitted[from] = p.mu.leader.pendingAdmitted[from].Merge(
@@ -901,7 +913,15 @@ func (p *processorImpl) ProcessPiggybackedAdmittedAtLeaderRaftMuLocked(ctx conte
 	p.opts.Replica.RaftMuAssertHeld()
 	p.mu.Lock()
 	defer p.mu.Unlock()
-	if p.mu.destroyed || len(p.mu.leader.pendingAdmitted) == 0 {
+	if p.mu.destroyed {
+		return false
+	}
+	p.mu.leader.pendingAdmittedMu.Lock()
+	defer p.mu.leader.pendingAdmittedMu.Unlock()
+	// TODO(pav-kv): narrow down holding pendingAdmittedMu to only copy the
+	// pendingAdmitted map. Alternatively, have a pre-allocated "scratch" map, and
+	// just swap it with pendingAdmitted.
+	if len(p.mu.leader.pendingAdmitted) == 0 {
 		return false
 	}
 	for replicaID, state := range p.mu.leader.pendingAdmitted {
