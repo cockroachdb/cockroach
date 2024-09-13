06425a459297ae1987ebf3170b2a86d5cfa5c53f
Xin Hao Zhang <xzhang@cockroachlabs.com>
2024-09-13 11:29:35 -0400

    cluster-ui: copy some base components from cloud
    This commit ports over some basic components from cloud. They are
    placed in a new directory, `sharedFromCloud` and will be deleted
    once the new package is ready. This is simply a temporary solution.
    
    Componentsported over, with their dependencies:
    - Search
    - PageHeader
    - PageCount (for displaying table pagination)
    
    Epic: CRDB-37558
    
    Release note: None

---
+++ pkg/ui/workspaces/cluster-ui/src/sharedFromCloud/breadcrumbs.tsx
@@ -0,0 +1,95 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+import classnames from "classnames/bind";
+import React, { ReactElement, ReactChild } from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./breadcrumbs.module.scss";
+const cx = classnames.bind(styles);
+
+export interface BreadcrumbItemWithLink {
+  name: ReactChild;
+  link: string;
+  onClick?: () => void;
+}
+
+interface BreadcrumbItem {
+  name: ReactChild;
+  key: string;
+}
+
+/** Whether the item is of type `BreadcrumbItemWithLink`. */
+function isBreadcrumbWithLink(
+  item: BreadcrumbItemWithLink | BreadcrumbItem,
+): item is BreadcrumbItemWithLink {
+  return (item as BreadcrumbItemWithLink).link !== undefined;
+}
+
+interface BreadcrumbsProps {
+  items: (BreadcrumbItem | BreadcrumbItemWithLink)[];
+  divider?: ReactElement;
+  className?: string;
+  /** Whether to use compact or default spacing for breadcrumbs. */
+  condensed?: boolean;
+}
+
+function Breadcrumbs({
+  items,
+  divider = <>/</>,
+  className,
+  condensed = false,
+}: BreadcrumbsProps) {
+  if (items.length === 0) {
+    return null;
+  }
+  const lastItem = items.slice(-1)[0];
+  const itemsWithoutLast = items.slice(0, -1);
+  return (
+    <div
+      className={cx(
+        "breadcrumbs",
+        { "breadcrumbs--condensed": condensed },
+        className,
+      )}
+      aria-label="breadcrumbs"
+    >
+      {itemsWithoutLast.map(item => {
+        const includeLink = isBreadcrumbWithLink(item);
+        return (
+          <div
+            className={cx("item-container")}
+            key={includeLink ? item.link : item.key}
+          >
+            {includeLink ? (
+              <Link
+                className={cx("item", "item-link")}
+                to={item.link}
+                onClick={item.onClick}
+              >
+                {item.name}
+              </Link>
+            ) : (
+              <span className={cx("item")}>{item.name}</span>
+            )}
+            <span className={cx("item-divider")} aria-hidden={true}>
+              {divider}
+            </span>
+          </div>
+        );
+      })}
+      <div className={cx("item-container", "item-last")} aria-current="page">
+        {lastItem?.name}
+      </div>
+    </div>
+  );
+}
+
+export default Breadcrumbs;
