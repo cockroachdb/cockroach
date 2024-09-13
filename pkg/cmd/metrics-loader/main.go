c9e257ad78d71e3657e8fd19f45ab7b826f26501
Herko Lategan <herko@cockroachlabs.com>
2024-09-13 10:22:46 +0100

    metrics-loader: configure and run
    This adds the required configuration via flags for all components to be able to
    start the watcher. In the `RunE` function we configure a `Sender` with a given
    remote-write URL, and use the default `OpenMetricsReader`, with the `Processor`
    to parse OpenMetrics files. A GoogleWatcher is configured to start watching a
    bucket via a PubSub subscription.
    
    The app will continue to run until interrupted or if a serious error occurs
    within the `watcher` which prevents if from monitoring the bucket.
    
    Epic: None
    Release note: None

--- pkg/cmd/metrics-loader/main.go
+++ pkg/cmd/metrics-loader/main.go
@@ -11,14 +11,27 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/spf13/cobra"
 )
 
 func makeMetricsLoaderCommand() *cobra.Command {
-	return &cobra.Command{
+	config := GoogleWatcherConfig{
+		TopicID:             "metrics-loader",
+		SubscriptionID:      "metrics-loader-sub",
+		ProjectID:           "cockroach-testeng-infra",
+		BucketName:          "cockroach-testeng-metrics",
+		BucketWatchPath:     "metrics-loader/incoming",
+		BucketCompletedPath: "metrics-loader/processed",
+	}
+	senderURL := "http://localhost:9201/write"
+	cmd := &cobra.Command{
 		Use:     "metrics-loader (flags)",
 		Short:   "metrics-loader streams metrics from OpenMetrics files to a Prometheus remote-write endpoint.",
 		Version: "v0.0",
@@ -31,7 +44,47 @@ be moved to the completed directory if the file was successfully processed.
 `,
 		SilenceUsage:  true,
 		SilenceErrors: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			sender := NewHTTPSender(senderURL)
+			processor := NewProcessor(NewOpenMetricsReader(), sender)
+
+			ctx, cancel := context.WithCancel(context.Background())
+			watcher, err := NewGoogleWatcher(ctx, processor, config)
+			if err != nil {
+				cancel()
+				return err
+			}
+
+			wg := sync.WaitGroup{}
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				err = watcher.Start()
+				cancel()
+			}()
+
+			sigCh := make(chan os.Signal, 1)
+			defer close(sigCh)
+			signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+			select {
+			case <-sigCh:
+				log.Println("Received SIGTERM, exiting gracefully...")
+				cancel()
+			case <-ctx.Done():
+				log.Println("Received cancellation, exiting gracefully...")
+			}
+			wg.Wait()
+			return err
+		},
 	}
+	cmd.Flags().StringVar(&config.ProjectID, "project-id", config.ProjectID, "Google Cloud project ID")
+	cmd.Flags().StringVar(&config.BucketName, "bucket-name", config.BucketName, "Google Cloud Storage bucket name")
+	cmd.Flags().StringVar(&config.BucketWatchPath, "bucket-watch-path", config.BucketWatchPath, "Google Cloud Storage bucket path to watch for incoming metrics")
+	cmd.Flags().StringVar(&config.BucketCompletedPath, "bucket-completed-path", config.BucketCompletedPath, "Google Cloud Storage bucket path to move processed metrics")
+	cmd.Flags().StringVar(&config.TopicID, "topic-id", config.TopicID, "Google Cloud Pub/Sub topic ID")
+	cmd.Flags().StringVar(&config.SubscriptionID, "subscription-id", config.SubscriptionID, "Google Cloud Pub/Sub subscription ID")
+	cmd.Flags().StringVar(&senderURL, "write-url", senderURL, "Prometheus remote-write URL")
+	return cmd
 }
 
 func main() {
