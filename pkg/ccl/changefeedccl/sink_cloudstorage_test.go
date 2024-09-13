899a35c73286487bf8c2c62f4e990ed3aa975758
Wenyi Hu <wenyi@cockroachlabs.com>
2024-09-13 10:50:17 -0400

    changefeedccl: skip TestCloudStorageSinkFastGzip under race
    This patch skips TestCloudStorageSinkFastGzip under race due to an identified
    data race.. This commit skips for now to unblock CI.
    
    Informs: #130651
    Release note: none

--- pkg/ccl/changefeedccl/sink_cloudstorage_test.go
+++ pkg/ccl/changefeedccl/sink_cloudstorage_test.go
@@ -39,6 +39,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/tabledesc"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/ioctx"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
@@ -844,6 +845,7 @@ func (o explicitTimestampOracle) inclusiveLowerBoundTS() hlc.Timestamp {
 func TestCloudStorageSinkFastGzip(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
+	skip.UnderRace(t, "#130651")
 
 	ctx := context.Background()
 	settings := cluster.MakeTestingClusterSettings()
