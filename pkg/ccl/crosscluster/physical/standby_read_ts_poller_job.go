c43fbb4063d2a25f320acb1581aead86a298e924
Anne Zhu <anne.zhu@cockroachlabs.com>
2024-09-13 13:53:50 -0400

    crosscluster, jobs: create reader tenant job
    This commit creates a new job type `StandbyReadTSPollerJob`.
    `StandbyReadTSPollerJob` polls the `ReadFromTenantInfo` API
    every 15s and updates replicated time for the reader tenant
    if it has advanced.
    
    Epic: CRDB-23575
    Informs: https://github.com/cockroachdb/cockroach/issues/130445
    Release note: None

---
+++ pkg/ccl/crosscluster/physical/standby_read_ts_poller_job.go
@@ -0,0 +1,125 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Licensed as a CockroachDB Enterprise file under the Cockroach Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/cockroachdb/cockroach/blob/master/licenses/CCL.txt
+
+package physical
+
+import (
+	"context"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/jobs"
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/security/username"
+	"github.com/cockroachdb/cockroach/pkg/settings"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/sql"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/replication"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/errors"
+)
+
+var standbyReadTSPollInterval = settings.RegisterDurationSetting(
+	settings.ApplicationLevel,
+	"bulkio.stream_ingestion.standby_read_ts_poll_interval",
+	"the interval at which a StandbyReadTSPoller job polls for replicated timestamp",
+	15*time.Second,
+	settings.PositiveDuration)
+
+type standbyReadTSPollerResumer struct {
+	job *jobs.Job
+}
+
+var _ jobs.Resumer = (*standbyReadTSPollerResumer)(nil)
+
+// Resume implements the jobs.Resumer interface.
+func (r *standbyReadTSPollerResumer) Resume(
+	ctx context.Context, execCtxI interface{},
+) (jobErr error) {
+	execCtx := execCtxI.(sql.JobExecContext)
+	return r.poll(ctx, execCtx.ExecCfg())
+}
+
+// OnFailOrCancel implements the jobs.Resumer interface.
+func (r *standbyReadTSPollerResumer) OnFailOrCancel(
+	_ context.Context, _ interface{}, jobErr error,
+) error {
+	if jobs.HasErrJobCanceled(jobErr) {
+		return errors.NewAssertionErrorWithWrappedErrf(
+			jobErr,
+			"standby read ts poller job is not cancelable")
+	}
+	return nil
+}
+
+// CollectProfile implements the jobs.Resumer interface.
+func (r *standbyReadTSPollerResumer) CollectProfile(_ context.Context, _ interface{}) error {
+	return nil
+}
+
+func (r *standbyReadTSPollerResumer) poll(ctx context.Context, execCfg *sql.ExecutorConfig) error {
+	ticker := time.NewTicker(standbyReadTSPollInterval.Get(&execCfg.Settings.SV))
+	defer ticker.Stop()
+	var previousReplicatedTimestamp hlc.Timestamp
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			tenantInfoAccessor := execCfg.UpgradeJobDeps.SystemDeps().TenantInfoAccessor
+			if tenantInfoAccessor == nil {
+				return errors.AssertionFailedf("tenant info accessor cannot be nil")
+			}
+			tenantID, replicatedTime, err := tenantInfoAccessor.ReadFromTenantInfo(ctx)
+			if err != nil {
+				log.Warningf(ctx, "failed to read tenant info of tenant {%d}: %v", tenantID, err)
+				continue
+			}
+			// No need to call SetupOrAdvanceStandbyReaderCatalog() if
+			// replicated time has not advanced.
+			if replicatedTime.Equal(previousReplicatedTimestamp) {
+				continue
+			}
+			previousReplicatedTimestamp = replicatedTime
+			if err = replication.SetupOrAdvanceStandbyReaderCatalog(
+				ctx,
+				tenantID,
+				replicatedTime,
+				execCfg.InternalDB,
+				execCfg.Settings,
+			); err != nil {
+				log.Warningf(ctx, "failed to advance replicated timestamp for reader tenant {%d}: %v", tenantID, err)
+			}
+		}
+	}
+}
+
+func makeStandbyReadTSPollerJobRecord(
+	registry *jobs.Registry, usrname username.SQLUsername,
+) jobs.Record {
+	return jobs.Record{
+		JobID:         registry.MakeJobID(),
+		Description:   "standby read-only timestamp poller job",
+		Username:      usrname,
+		Details:       jobspb.StandbyReadTSPollerDetails{},
+		Progress:      jobspb.StandbyReadTSPollerProgress{},
+		NonCancelable: true,
+	}
+}
+
+func init() {
+	jobs.RegisterConstructor(jobspb.TypeStandbyReadTSPoller,
+		func(job *jobs.Job, settings *cluster.Settings) jobs.Resumer {
+			return &standbyReadTSPollerResumer{
+				job: job,
+			}
+		},
+		jobs.UsesTenantCostControl,
+	)
+}
