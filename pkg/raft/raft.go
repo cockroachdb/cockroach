b7e34e1a7e2b4b8abbb763b233c64073337c90f1
Ibrahim Kettaneh <ibrahim.kettaneh@cockroachlabs.com>
2024-09-16 09:27:03 -0400

    raft: refortify followers if they are not fortified
    This commit makes the leader periodically attempt to fortify unfortified
    followers.
    
    Before this change, the leader used to send a fortification message to
    followers, but doesn't try to refortify unfortified followers.

--- pkg/raft/raft.go
+++ pkg/raft/raft.go
@@ -1041,6 +1041,10 @@ func (r *raft) tickHeartbeat() {
 		if err := r.Step(pb.Message{From: r.id, Type: pb.MsgBeat}); err != nil {
 			r.logger.Debugf("error occurred during checking sending heartbeat: %v", err)
 		}
+
+		// Try to refortify any followers that don't currently support us.
+		r.bcastFortify()
+		// TODO(ibrahim): add/call maybeUnpauseAndBcastAppend() here.
 	}
 }
 
