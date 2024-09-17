173e6401555edebc0eb96c7c2f4bb3f64a8dc1ba
Matt Spilchen <matt.spilchen@cockroachlabs.com>
2024-09-17 13:02:30 -0300

    server/license: use system.migrations to check if cluster is new or old
    The grace period when no license is installed varies based on whether
    the cluster is new or existing. Older clusters receive a 30-day grace
    period, while new clusters only get 7 days. This logic was introduced in
    a previous commit, but it relied on state being passed down to the
    license enforcer, which was really only intended for debugging purposes.
    
    This update changes that approach by allowing the enforcer to determine
    whether the cluster is new or old independently, using the minimum
    timestamp from system.migrations. If the enforcer's start time is within
    a 2-hour window of the earliest timestamp in system.migrations, the
    cluster is considered new.
    
    This change will be backported to 23.1, 23.2, 24.1 and 24.2.
    
    Epic: CRDB-39988
    Informs: CRDB-39991
    Release note: None

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2092,7 +2092,6 @@ func (s *topLevelServer) PreStart(ctx context.Context) error {
 			s.stopper,
 			s.cfg.TestingKnobs,
 			orphanedLeasesTimeThresholdNanos,
-			s.InitialStart(),
 		); err != nil {
 			return err
 		}
