387149618e4a5b7be837ae0f4951ce6f96d7cb8a
James H. Linder <jamesl@cockroachlabs.com>
2024-09-19 16:18:19 -0400

    license: change file headers w/BSL to use CockroachDB Software License
    Change the file headers for files under the BSL license to use the
    CockroachDB Software License instead.
    
    Part of RE-658
    
    Release note: none

--- pkg/testutils/lint/passes/shadow/shadow.go
+++ pkg/testutils/lint/passes/shadow/shadow.go
@@ -1,12 +1,7 @@
 // Copyright 2022 The Cockroach Authors.
 //
-// Use of this software is governed by the Business Source License
-// included in the file licenses/BSL.txt.
-//
-// As of the Change Date specified in that file, in accordance with
-// the Business Source License, use of this software will be governed
-// by the Apache License, Version 2.0, included in the file
-// licenses/APL.txt.
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
 
 // Package shadow defines an Analyzer that is a slightly modified version
 // of the shadow Analyzer from upstream (golang.org/x/tools).
