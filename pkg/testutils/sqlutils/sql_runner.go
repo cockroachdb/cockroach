d76577eb54b13c50602191d1eb95a5b8aebcf76e
Michael Erickson <michae2@cockroachlabs.com>
2024-09-19 11:41:29 -0700

    row: add debugging for TestExternalRowData
    When modifying keys in the BatchRequest for external row data, we
    sometimes see that the key has already been modified. We suspect that
    multiple keys in the BatchRequest share an underlying array. Change the
    crdb-test-build-only assertion to a panic to get a stacktrace during
    this rare test failure.
    
    Informs: #128070
    
    Release note: None

--- pkg/testutils/sqlutils/sql_runner.go
+++ pkg/testutils/sqlutils/sql_runner.go
@@ -279,16 +279,29 @@ func (sr *SQLRunner) QueryRow(t Fataler, query string, args ...interface{}) *Row
 	return &Row{t, sr.DB.QueryRowContext(context.Background(), query, args...)}
 }
 
-// QueryStr runs a Query and converts the result using RowsToStrMatrix. Kills
-// the test on errors.
-func (sr *SQLRunner) QueryStr(t Fataler, query string, args ...interface{}) [][]string {
+// QueryStrMeta runs a Query and converts the result using RowsToStrMatrix. Kills
+// the test on errors, including meta string in error message.
+func (sr *SQLRunner) QueryStrMeta(
+	t Fataler, meta string, query string, args ...interface{},
+) [][]string {
 	helperOrNoop(t)()
 	rows := sr.Query(t, query, args...)
 	r, err := RowsToStrMatrix(rows)
 	if err != nil {
-		t.Fatalf("%v", err)
+		if meta == "" {
+			meta = query
+		} else {
+			meta = fmt.Sprintf("%v (%v)", query, meta)
+		}
+		t.Fatalf("%v: %v", meta, err)
 	}
 	return r
+}
+
+// QueryStr runs a Query and converts the result using RowsToStrMatrix. Kills
+// the test on errors.
+func (sr *SQLRunner) QueryStr(t Fataler, query string, args ...interface{}) [][]string {
+	return sr.QueryStrMeta(t, "", query, args)
 }
 
 // RowsToStrMatrix converts the given result rows to a string matrix; nulls are
