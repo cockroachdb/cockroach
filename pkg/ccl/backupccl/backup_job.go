6389a543aa8dc1ecabfe4a41c0f05e637847a921
Michael Butler <butler@cockroachlabs.com>
2024-09-25 11:53:36 -0400

    backupccl: fix scheduled backup pts pushing race
    Informs #128013
    
    Epic: none

--- pkg/ccl/backupccl/backup_job.go
+++ pkg/ccl/backupccl/backup_job.go
@@ -663,6 +663,21 @@ func (b *backupResumer) Resume(ctx context.Context, execCtx interface{}) error {
 		}
 	}
 
+	// Choose which scheduled backup pts we will update at the the end of the
+	// backup _before_ we resolve the destination of the backup. This avoids a
+	// race with inc backups where backup destination resolution leads this backup
+	// to extend a chain that is about to be superseded by a new full backup
+	// chain, which could cause this inc to accidentally push the pts for the
+	// _new_ chain instead of the old chain it is apart of. By choosing the pts to
+	// move before we resolve the destination, we guarantee that we push the old
+	// chain.
+	insqlDB := p.ExecCfg().InternalDB
+	if err := insqlDB.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+		return planSchedulePTSChaining(ctx, p.ExecCfg().JobsKnobs(), txn, &details, b.job.CreatedBy())
+	}); err != nil {
+		return err
+	}
+
 	var backupManifest *backuppb.BackupManifest
 
 	// Populate the BackupDetails with the resolved backup
@@ -672,7 +687,6 @@ func (b *backupResumer) Resume(ctx context.Context, execCtx interface{}) error {
 	// details and manifest in a prior resumption.
 	//
 	// TODO(adityamaru: Break this code block into helper methods.
-	insqlDB := p.ExecCfg().InternalDB
 	if details.URI == "" {
 		initialDetails := details
 		if err := insqlDB.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
@@ -724,12 +738,6 @@ func (b *backupResumer) Resume(ctx context.Context, execCtx interface{}) error {
 		}
 
 		if err := p.ExecCfg().JobRegistry.CheckPausepoint("backup.after.write_first_checkpoint"); err != nil {
-			return err
-		}
-
-		if err := insqlDB.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
-			return planSchedulePTSChaining(ctx, p.ExecCfg().JobsKnobs(), txn, &details, b.job.CreatedBy())
-		}); err != nil {
 			return err
 		}
 
