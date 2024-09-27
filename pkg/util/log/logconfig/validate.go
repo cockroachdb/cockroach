1e5cd54896378272a9ce16487a663aefbb8d403c
Xin Hao Zhang <xzhang@cockroachlabs.com>
2024-09-27 13:33:37 -0400

    util/log/logconfig: allow `format` for stderr log sink
    Previously in #113532 we fixed the stderr sink log format
    to `crdb-v2-tty`. This was deemed to be a regression, as
    the stderr sink could previously be set to any available
    format. This commit reverts the restriction.
    
    The following are still true after this commit:
    - `format` in `file-defaults` is not propagated to
    `format` of the stderr sink. To set `stderr` format,
    one must explicitly define it in the `stderr` sink
    section of the log config.
    - `format-options` in `file-defaults` are only applied
    to stderr if the format of the stderr sink and that in
    file-defaults are compatible.
    
    Epic: none
    Fixes: #128665
    
    Release note (ops change): Users may set the log format
    for the stderr sink by setting the `format` field in the
    logging config under the `stderr` sink section.

--- pkg/util/log/logconfig/validate.go
+++ pkg/util/log/logconfig/validate.go
@@ -177,17 +177,12 @@ func (c *Config) Validate(defaultLogDir *string) (resErr error) {
 		c.Sinks.Stderr.Criticality = &bt
 	}
 	c.Sinks.Stderr.Auditable = nil
-	// The format parameter for stderr is set to `crdb-v2-tty` and cannot be changed.
-	// See docs: https://www.cockroachlabs.com/docs/stable/configure-logs#output-to-stderr
-	if *c.Sinks.Stderr.Format != DefaultStderrFormat {
-		f := DefaultStderrFormat
-		c.Sinks.Stderr.Format = &f
-	}
+
 	// FormatOptions are format-specific. We should only copy them over to StdErr from
-	// FileDefaults if FileDefaults is also making use of a crdb-v2 format. Otherwise,
+	// FileDefaults if FileDefaults specifies the same format as the stderr sink. Otherwise,
 	// we are likely to error when trying to apply an unsupported format option.
 	if c.FileDefaults.CommonSinkConfig.Format != nil &&
-		!strings.Contains(*c.FileDefaults.CommonSinkConfig.Format, "v2") &&
+		!canShareFormatOptions(*c.FileDefaults.CommonSinkConfig.Format, *c.Sinks.Stderr.Format) &&
 		!stdErrFormatOptionsOriginallySet {
 		c.Sinks.Stderr.CommonSinkConfig.FormatOptions = map[string]string{}
 	}
@@ -537,4 +532,15 @@ func propagateDefaults(target, source interface{}) {
 			tf.Set(s.Field(i))
 		}
 	}
+}
+
+// canShareFormatOptions returns true if f1 and f2 can share format options.
+// See log.FormatParsers for full list of supported formats.
+// Examples:
+//
+//	canShareFormatOptions("json", "json") => true
+//	canShareFormatOptions("crdb-v2", "crdb-v2-tty") => true
+//	canShareFormatOptions("crdb-v2", "json") => false
+func canShareFormatOptions(f1, f2 string) bool {
+	return strings.HasPrefix(f1, f2) || strings.HasPrefix(f2, f1)
 }
