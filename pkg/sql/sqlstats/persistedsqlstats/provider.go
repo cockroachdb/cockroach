5f09cc3102ab4cf952666e7d6f94989f554aaca2
James H. Linder <jamesl@cockroachlabs.com>
2024-10-01 11:37:52 -0400

    license: change pkg/sql file headers w/BSL to use the CSL
    Change the file headers for files under pkg/sql directory using the BSL
    license to use the CockroachDB Software License (CSL) instead.
    
    Part of RE-658
    
    Release note (general change): Change the license cockroach is distributed
    under to the new CockroachDB Software License (CSL).

--- pkg/sql/sqlstats/persistedsqlstats/provider.go
+++ pkg/sql/sqlstats/persistedsqlstats/provider.go
@@ -1,12 +1,7 @@
 // Copyright 2021 The Cockroach Authors.
 //
-// Use of this software is governed by the Business Source License
-// included in the file licenses/BSL.txt.
-//
-// As of the Change Date specified in that file, in accordance with
-// the Business Source License, use of this software will be governed
-// by the Apache License, Version 2.0, included in the file
-// licenses/APL.txt.
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
 //
 // persistedsqlstats is a subsystem that is responsible for flushing node-local
 // in-memory stats into persisted system tables.
