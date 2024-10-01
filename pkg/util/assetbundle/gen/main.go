049c30ac6956fa8f307ba2adae8b4388f5907859
James H. Linder <jamesl@cockroachlabs.com>
2024-10-01 11:59:04 -0400

    license: change pkg/util + pkg/cmd file headers to use the CSL
    Change the file headers for files under pkg/util and pkg/cmd directories
    using the BSL license to use the CockroachDB Software License (CSL) instead.
    
    Part of RE-658
    
    Release note (general change): Change the license cockroach is distributed
    under to the new CockroachDB Software License (CSL).

--- pkg/util/assetbundle/gen/main.go
+++ pkg/util/assetbundle/gen/main.go
@@ -1,12 +1,7 @@
 // Copyright 2017 The Cockroach Authors.
 //
-// Use of this software is governed by the Business Source License
-// included in the file licenses/BSL.txt.
-//
-// As of the Change Date specified in that file, in accordance with
-// the Business Source License, use of this software will be governed
-// by the Apache License, Version 2.0, included in the file
-// licenses/APL.txt.
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
 
 // Given the path to a directory and an output path, this executable creates a
 // .tar.zst archive. This is not feature-complete (at all) compared to the `tar`
