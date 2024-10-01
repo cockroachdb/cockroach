4fa30ea424843ffb080d6cd312ac00607a272573
James H. Linder <jamesl@cockroachlabs.com>
2024-10-01 11:42:07 -0400

    license: change pkg/ui file headers w/BSL to use the CSL
    Change the file headers for files under pkg/ui directory using the BSL
    license to use the CockroachDB Software License (CSL) instead.
    
    Part of RE-658
    
    Release note (general change): Change the license cockroach is distributed
    under to the new CockroachDB Software License (CSL).

--- pkg/ui/workspaces/db-console/src/app.spec.tsx
+++ pkg/ui/workspaces/db-console/src/app.spec.tsx
@@ -1,12 +1,7 @@
 // Copyright 2020 The Cockroach Authors.
 //
-// Use of this software is governed by the Business Source License
-// included in the file licenses/BSL.txt.
-//
-// As of the Change Date specified in that file, in accordance with
-// the Business Source License, use of this software will be governed
-// by the Apache License, Version 2.0, included in the file
-// licenses/APL.txt.
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
 
 // stubComponentInModule functions must be called before any other import in this test file to make sure
 // components are stubbed and then stubbed instances of components are imported as part of regular module
