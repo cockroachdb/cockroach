55ad8cf8f6d2404add53997937ae600dccc60166
Herko Lategan <herko@cockroachlabs.com>
2024-10-01 13:43:21 +0100

    roachtest: EveryN util without CRDB log
    Previously, tests made use of the EveryN utility in "pkg/util/log" that also
    interacts with the CRDB log to check verbosity levels. This feature is not used
    in `roachtest` as generally CRDB log should not be used explicitly, nor will it
    be configured for verbosity levels.
    
    This change adds the utility to `roachtest`, which still leverages the base
    utility EveryN from "pgk/util", but without interacting with CRDB log.
    
    Epic: None
    Release note: None

--- pkg/cmd/roachtest/roachtestutil/utils.go
+++ pkg/cmd/roachtest/roachtestutil/utils.go
@@ -6,9 +6,13 @@
 package roachtestutil
 
 import (
+	"time"
+
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/cluster"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/config"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
+	"github.com/cockroachdb/cockroach/pkg/util"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 )
 
 // SystemInterfaceSystemdUnitName is a convenience function that
@@ -31,4 +35,22 @@ func SetDefaultAdminUIPort(c cluster.Cluster, opts *install.StartOpts) {
 	if !c.IsLocal() {
 		opts.AdminUIPort = config.DefaultAdminUIPort
 	}
+}
+
+// EveryN provides a way to rate limit noisy log messages. It tracks how
+// recently a given log message has been emitted so that it can determine
+// whether it's worth logging again.
+type EveryN struct {
+	util.EveryN
+}
+
+// Every is a convenience constructor for an EveryN object that allows a log
+// message every n duration.
+func Every(n time.Duration) EveryN {
+	return EveryN{EveryN: util.Every(n)}
+}
+
+// ShouldLog returns whether it's been more than N time since the last event.
+func (e *EveryN) ShouldLog() bool {
+	return e.ShouldProcess(timeutil.Now())
 }
