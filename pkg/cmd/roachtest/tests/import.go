39150c7a2e4fb9de2de1ffa8c510fde59384b11a
Herko Lategan <herko@cockroachlabs.com>
2024-10-01 14:10:33 +0100

    roachtest: remove CRDB log usage in roachtest tests
    Previously, CRDB log was used directly in some of the tests. Instead, the test
    logger should be used to better attribute a failure to a given test without
    having to go compare timestamps against a different log.
    
    Epic: None
    Release note: None

--- pkg/cmd/roachtest/tests/import.go
+++ pkg/cmd/roachtest/tests/import.go
@@ -21,7 +21,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/test"
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
-	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/retry"
 	"github.com/cockroachdb/errors"
 )
@@ -151,10 +150,10 @@ func registerImportTPCC(r registry.Registry) {
 			// runner copies it into an appropriate directory path.
 			dest := filepath.Join(t.PerfArtifactsDir(), "stats.json")
 			if err := c.RunE(ctx, c.Node(1), "mkdir -p "+filepath.Dir(dest)); err != nil {
-				log.Errorf(ctx, "failed to create perf dir: %+v", err)
+				t.L().ErrorfCtx(ctx, "failed to create perf dir: %+v", err)
 			}
 			if err := c.PutString(ctx, perfBuf.String(), dest, 0755, c.Node(1)); err != nil {
-				log.Errorf(ctx, "failed to upload perf artifacts to node: %s", err.Error())
+				t.L().ErrorfCtx(ctx, "failed to upload perf artifacts to node: %s", err.Error())
 			}
 			return nil
 		})
@@ -319,10 +318,10 @@ func registerImportTPCH(r registry.Registry) {
 					// runner copies it into an appropriate directory path.
 					dest := filepath.Join(t.PerfArtifactsDir(), "stats.json")
 					if err := c.RunE(ctx, c.Node(1), "mkdir -p "+filepath.Dir(dest)); err != nil {
-						log.Errorf(ctx, "failed to create perf dir: %+v", err)
+						t.L().ErrorfCtx(ctx, "failed to create perf dir: %+v", err)
 					}
 					if err := c.PutString(ctx, perfBuf.String(), dest, 0755, c.Node(1)); err != nil {
-						log.Errorf(ctx, "failed to upload perf artifacts to node: %s", err.Error())
+						t.L().ErrorfCtx(ctx, "failed to upload perf artifacts to node: %s", err.Error())
 					}
 					return nil
 				})
