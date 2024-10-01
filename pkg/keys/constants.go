39cf06a04bc36d9439a93e15eeb7c038d69435e3
Matt Spilchen <matt.spilchen@cockroachlabs.com>
2024-10-01 17:25:36 -0300

    server/license: Limit enterprise trial license usage to once per cluster
    Previously, clusters could install a second enterprise trial license. To
    prevent misuse, we are now restricting the use of this license type to only
    once per cluster. This update adds the necessary tracking mechanism.
    
    A new key has been introduced in the KV system key range to record the number
    of enterprise trial licenses used, ensuring the data persists across restarts.
    
    This change will be backported to versions 24.2, 24.1, 23.2, and 23.1.
    
    Epic: CRDB-39988
    Closes: CRDB-40416
    Release note (general change): Attempting to install a second enterprise trial
    license on the same cluster will now fail.

--- pkg/keys/constants.go
+++ pkg/keys/constants.go
@@ -305,6 +305,9 @@ var (
 	// set during cluster initialization, by which a license must be installed to avoid
 	// throttling. The value is stored as the number of seconds since the Unix epoch.
 	ClusterInitGracePeriodTimestamp = roachpb.Key(makeKey(SystemPrefix, roachpb.RKey("lic-gpi-ts")))
+	// TrialLicenseUsageCount is used to keep track of the number of times a trial
+	// license was installed on the cluster.
+	TrialLicenseUsageCount = roachpb.Key(makeKey(SystemPrefix, roachpb.RKey("lic-tluc")))
 	//
 	// NodeIDGenerator is the global node ID generator sequence.
 	NodeIDGenerator = roachpb.Key(makeKey(SystemPrefix, roachpb.RKey("node-idgen")))
