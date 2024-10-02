1d7eca65e7565476d7fabbcd7545a852280c13f5
Xin Hao Zhang <xzhang@cockroachlabs.com>
2024-10-02 15:15:20 -0400

    cluster-ui: populate db name in db details page
    This commit populates the db name as the page title
    in the v2 db details page as well as breadcrumb navigation
    back to the db list page. To do so we all the get db metadata
    by id api on the details page.
    
    Epic: CRDB-37558
    Fixes: #131119
    
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/databaseDetailsV2/index.tsx
+++ pkg/ui/workspaces/cluster-ui/src/databaseDetailsV2/index.tsx
@@ -3,15 +3,18 @@
 // Use of this software is governed by the CockroachDB Software License
 // included in the /LICENSE file.
 
-import { Tabs } from "antd";
+import { Skeleton, Tabs } from "antd";
 import React from "react";
 import { useHistory, useLocation } from "react-router";
 
+import { useDatabaseMetadataByID } from "src/api/databases/getDatabaseMetadataApi";
 import { commonStyles } from "src/common";
+import { useRouteParams } from "src/hooks/useRouteParams";
 import { PageLayout } from "src/layouts";
 import { PageHeader } from "src/sharedFromCloud/pageHeader";
+import { queryByName, tabAttr } from "src/util";
 
-import { queryByName, tabAttr } from "../util";
+import { DB_PAGE_PATH } from "../util/routes";
 
 import { DbGrantsView } from "./dbGrantsView";
 import { TablesPageV2 } from "./tablesView";
@@ -21,6 +24,9 @@ enum TabKeys {
   GRANTS = "grants",
 }
 export const DatabaseDetailsPageV2 = () => {
+  const { dbID: dbIdRouteParam } = useRouteParams();
+  const dbId = parseInt(dbIdRouteParam, 10);
+  const { data, isLoading, error } = useDatabaseMetadataByID(dbId);
   const history = useHistory();
   const location = useLocation();
   const tab = queryByName(location, tabAttr) ?? TabKeys.TABLES;
@@ -38,7 +44,6 @@ export const DatabaseDetailsPageV2 = () => {
     });
   };
 
-  // TODO (xinhaoz) #131119 - Populate db name here.
   const tabItems = [
     {
       key: TabKeys.TABLES,
@@ -52,9 +57,25 @@ export const DatabaseDetailsPageV2 = () => {
     },
   ];
 
+  const dbName =
+    error?.status === 404 || !data
+      ? "Database Not Found"
+      : data.metadata.db_name;
+
+  const breadCrumbItems = [
+    { name: "Databases", link: DB_PAGE_PATH },
+    {
+      name: dbName,
+      link: null,
+    },
+  ];
+
   return (
     <PageLayout>
-      <PageHeader title="myDB" />
+      <PageHeader
+        breadcrumbItems={breadCrumbItems}
+        title={<Skeleton loading={isLoading}>{dbName}</Skeleton>}
+      />
       <Tabs
         defaultActiveKey={TabKeys.TABLES}
         className={commonStyles("cockroach--tabs")}
