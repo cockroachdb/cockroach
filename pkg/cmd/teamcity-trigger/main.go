b10db3814f4fa8b5baa3d1392f4c9d0250e2af83
James H. Linder <jamesl@cockroachlabs.com>
2024-10-04 16:12:27 -0400

    license: change pkg/util + pkg/cmd file headers to use the CSL
    Change the file headers for files under pkg/util and pkg/cmd directories
    using the BSL license to use the CockroachDB Software License (CSL) instead.
    
    Part of RE-658
    
    Release note (general change): Change the license cockroach is distributed
    under to the new CockroachDB Software License (CSL).

--- pkg/cmd/teamcity-trigger/main.go
+++ pkg/cmd/teamcity-trigger/main.go
@@ -1,12 +1,7 @@
 // Copyright 2016 The Cockroach Authors.
 //
-// Use of this software is governed by the Business Source License
-// included in the file licenses/BSL.txt.
-//
-// As of the Change Date specified in that file, in accordance with
-// the Business Source License, use of this software will be governed
-// by the Apache License, Version 2.0, included in the file
-// licenses/APL.txt.
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
 
 // teamcity-trigger launches a variety of nightly build jobs on TeamCity using
 // its REST API. It is intended to be run from a meta-build on a schedule
