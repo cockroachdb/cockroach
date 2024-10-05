36829d61ab861fdb36c8e5a2a7fff0576f5c0c13
James H. Linder <jamesl@cockroachlabs.com>
2024-10-04 22:23:55 -0400

    license: change pkg/sql file headers w/BSL to use the CSL
    Change the file headers for files under pkg/sql directory using the BSL
    license to use the CockroachDB Software License (CSL) instead.
    
    Part of RE-658
    
    Release note (general change): Change the license cockroach is distributed
    under to the new CockroachDB Software License (CSL).

--- pkg/sql/sem/eval/doc.go
+++ pkg/sql/sem/eval/doc.go
@@ -1,12 +1,7 @@
 // Copyright 2022 The Cockroach Authors.
 //
-// Use of this software is governed by the Business Source License
-// included in the file licenses/BSL.txt.
-//
-// As of the Change Date specified in that file, in accordance with
-// the Business Source License, use of this software will be governed
-// by the Apache License, Version 2.0, included in the file
-// licenses/APL.txt.
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
 
 // Package eval contains logic to evaluate tree.TypedExpr expressions as
 // well as tree.UnaryOp and tree.BinaryOp operators.
