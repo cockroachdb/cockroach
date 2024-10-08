ea21910482160160a1a097f4440eb61590c40a8d
Michael Erickson <michae2@cockroachlabs.com>
2024-10-08 10:12:08 -0700

    sql: add TestExternalRowDataDistSQL
    Add a test that the DistSQL physical planner correctly plans flows
    reading from external table data.
    
    Release note: None

--- pkg/sql/row/main_test.go
+++ pkg/sql/row/main_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/security/securitytest"
 	"github.com/cockroachdb/cockroach/pkg/server"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/testcluster"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 )
 
@@ -20,5 +21,6 @@ func TestMain(m *testing.M) {
 	securityassets.SetLoader(securitytest.EmbeddedAssets)
 	randutil.SeedForTests()
 	serverutils.InitTestServerFactory(server.TestServerFactory)
+	serverutils.InitTestClusterFactory(testcluster.TestClusterFactory)
 	os.Exit(m.Run())
 }
