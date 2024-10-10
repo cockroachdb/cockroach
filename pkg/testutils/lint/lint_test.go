e1469053dfff5bf06da47ab5a832c8a01fd55549
Ricky Stewart <ricky@cockroachlabs.com>
2024-10-10 14:29:21 -0500

    lint: add license headers to `staticcheck` files in `build` dir
    ... and update linters to evaluate what's inside the `build` dir as
    well.
    
    Part of: RE-658
    Epic: DEVINF-1246
    Release note: None
    Release justification: Non-production code changes

--- pkg/testutils/lint/lint_test.go
+++ pkg/testutils/lint/lint_test.go
@@ -274,8 +274,13 @@ func TestLint(t *testing.T) {
 		extensions := []string{
 			"*.go", "*.cc", "*.h", "*.js", "*.ts", "*.tsx", "*.s", "*.S", "*.scss", "*.styl", "*.proto", "*.rl",
 		}
+		fullExtensions := make([]string, len(extensions)*2)
+		for i, extension := range extensions {
+			fullExtensions[i*2] = "build/**/" + extension
+			fullExtensions[i*2+1] = "pkg/**/" + extension
+		}
 
-		cmd, stderr, filter, err := dirCmd(pkgDir, "git", append([]string{"ls-files"}, extensions...)...)
+		cmd, stderr, filter, err := dirCmd(crdbDir, "git", append([]string{"ls-files"}, fullExtensions...)...)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -300,11 +305,11 @@ func TestLint(t *testing.T) {
 			// These files are copied from bazel upstream with its own license.
 			stream.GrepNot(`build/bazel/bes/.*.proto$`),
 			// These files are copied from raft upstream with its own license.
-			stream.GrepNot(`^raft/.*`),
+			stream.GrepNot(`^pkg/raft/.*`),
 			// Generated files for plpgsql.
 			stream.GrepNot(`sql/plpgsql/parser/plpgsqllexbase/.*.go`),
 		), func(filename string) {
-			file, err := os.Open(filepath.Join(pkgDir, filename))
+			file, err := os.Open(filepath.Join(crdbDir, filename))
 			if err != nil {
 				t.Error(err)
 				return
@@ -317,7 +322,7 @@ func TestLint(t *testing.T) {
 			}
 			data = data[0:n]
 
-			isApache := strings.HasPrefix(filename, "obsservice")
+			isApache := strings.HasPrefix(filename, "pkg/obsservice")
 			switch {
 			case isApache:
 				if apacheHeader.Find(data) == nil {
