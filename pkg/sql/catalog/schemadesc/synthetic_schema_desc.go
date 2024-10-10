4bf415a5e45b1bbc55045b359e9146725300eafd
Marcus Gartner <marcus@cockroachlabs.com>
2024-10-10 10:19:42 -0700

    sql/catalog: reduce allocations when checking for hydratable types
    The `catalog.MaybeRequiresHydration` function has been replaced with the
    `Descriptor.MaybeRequiresTypeHydration` interface method. This avoids a
    heap allocation that was required in `catalog.MaybeRequiresHydration`.
    Arguments to interface methods typically escape to the heap, so the
    closure passed to `Descriptor.ForEachUDTDependentForHydration` was heap
    allocated. The new interface method avoids this.
    
    Release note: None

--- pkg/sql/catalog/schemadesc/synthetic_schema_desc.go
+++ pkg/sql/catalog/schemadesc/synthetic_schema_desc.go
@@ -149,6 +149,9 @@ func (p synthetic) ForEachUDTDependentForHydration(fn func(t *types.T) error) er
 	return nil
 }
 
+// MaybeRequiresTypeHydration implements the catalog.Descriptor interface.
+func (p synthetic) MaybeRequiresTypeHydration() bool { return false }
+
 func (p synthetic) GetRawBytesInStorage() []byte {
 	return nil
 }
