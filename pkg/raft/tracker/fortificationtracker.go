07f078590bdf98ebb00ada174d78dfc03de52ba2
Arul Ajmani <arulajmani@gmail.com>
2024-10-14 15:38:24 -0400

    raft: remove redundant FortificationEnabled checks
    Previously, we were checking whether fortification was enabled by
    consulting StoreLiveness in a few different places. A few of these
    checks were redundant, which this commit removes. While here, we also
    start tracking whether fortification was enabled at any point during
    a leadership term. This then allows us to treat enabling raft
    fortification as a one way door for a given term -- fortification may
    be enabled at any point, but once enabled, it'll remain unabled for the
    duration of the term. This will be useful in an upcoming patch to decide
    whether we should broadcast MsgDeFortifyLeader or not when stepping down.
    
    Epic: none
    Release note: None

--- pkg/raft/tracker/fortificationtracker.go
+++ pkg/raft/tracker/fortificationtracker.go
@@ -35,6 +35,12 @@ type FortificationTracker struct {
 	// a leader at since it was restarted.
 	term uint64
 
+	// fortificationEnabledForTerm tracks whether fortification was enabled or not
+	// at any point for the leadership term being tracked in the
+	// FortificationTracker. If it was, we must conservatively assume that the
+	// raft leader attempted to fortify its term, and save this state.
+	fortificationEnabledForTerm bool
+
 	// fortification contains a map of nodes which have fortified the leader
 	// through fortification handshakes, and the corresponding Store Liveness
 	// epochs that they have supported the leader in.
@@ -68,18 +74,25 @@ func NewFortificationTracker(
 	config *quorum.Config, storeLiveness raftstoreliveness.StoreLiveness, logger raftlogger.Logger,
 ) *FortificationTracker {
 	st := FortificationTracker{
-		config:        config,
-		storeLiveness: storeLiveness,
-		fortification: map[pb.PeerID]pb.Epoch{},
-		logger:        logger,
+		config:                      config,
+		storeLiveness:               storeLiveness,
+		fortificationEnabledForTerm: false,
+		fortification:               map[pb.PeerID]pb.Epoch{},
+		logger:                      logger,
 	}
 	return &st
 }
 
-// FortificationEnabled returns whether the raft fortification protocol is
-// enabled or not.
-func (ft *FortificationTracker) FortificationEnabled() bool {
-	return ft.storeLiveness.SupportFromEnabled()
+// FortificationEnabledForTerm returns whether the raft fortification should be
+// enabled for the term being tracked in the FortificationTracker.
+//
+// NB: Fortification may be enabled while a leadership term is in progress.
+// However, once fortification has been enabled for a term, it will never flip
+// back.
+func (ft *FortificationTracker) FortificationEnabledForTerm() bool {
+	fortificationCurrentlyEnabled := ft.storeLiveness.SupportFromEnabled()
+	ft.fortificationEnabledForTerm = ft.fortificationEnabledForTerm || fortificationCurrentlyEnabled
+	return ft.fortificationEnabledForTerm
 }
 
 // RecordFortification records fortification of the given peer for the supplied
@@ -94,6 +107,7 @@ func (ft *FortificationTracker) RecordFortification(id pb.PeerID, epoch pb.Epoch
 // fortification tracker to be used by a newly elected leader.
 func (ft *FortificationTracker) Reset(term uint64) {
 	ft.term = term
+	ft.fortificationEnabledForTerm = false
 	clear(ft.fortification)
 	ft.leaderMaxSupported.Reset()
 }
