17cd48dc49949e51e90258775008a49a54d4ce19
Bhaskarjyoti Bora <bhaskar.bora@cockroachlabs.com>
2024-10-17 18:30:44 +0530

    roachprod: add command to download the latest pprof file
    Currently, whenever we see a spike in the CPU for a specific cluster, we have to download the latest pprof file for analysis. We generally select the latest file across all nodes or a specific set of nodes. This becomes a very manual process as we need to check each cluster to find the suitable file to download.
    This command helps in finding the latest pprof to download across all nodes or given nodes. The command also takes a time parameter as input with which we can download the latest pprof before a specific time.
    
    Epic: none
    Release note: None

--- pkg/cmd/roachprod/flags.go
+++ pkg/cmd/roachprod/flags.go
@@ -105,6 +105,7 @@ var (
 	opentelemetryConfig opentelemetry.Config
 
 	fetchLogsTimeout time.Duration
+	pprofTimeBefore  *string
 )
 
 func initFlags() {
@@ -490,4 +491,6 @@ func initFlags() {
 		"time-range", []int64{}, "grafana annotation time range in epoch time")
 	fetchLogsCmd.Flags().DurationVarP(&fetchLogsTimeout,
 		"timeout", "t", 5*time.Minute, "Timeout for fetching the logs from the cluster nodes")
+	pprofTimeBefore = getLatestPProf.Flags().StringP(
+		"time-before", "t", "", "The latest pprof file is downloaded which is created on or before the provided time.")
 }
