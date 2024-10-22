24ce33c7d800582737d314cfbf37f694cabbeb1f
Drew Kimball <drewk@cockroachlabs.com>
2024-10-22 03:55:39 +0000

    opt: refactor cascade planning in the optimizer
    This commit makes some mechanical changes to how cascades are planned
    so that it will be easier to add trigger planning later.
    * The `CascadeBuilder` (now `PostQueryBuilder`) now handles mapping from
      columns in the original memo to the post-query memo. This simplifies
      handling for different kinds of post-queries that keep different sets
      of columns.
    * The `build-cascades` opt test directive is now `build-post-queries`.
    * Various names and comments are modified to refer to "post-queries"
      instead of just "cascades".
    
    Informs #126361
    
    Release note: None

--- pkg/sql/opt/column_meta.go
+++ pkg/sql/opt/column_meta.go
@@ -8,6 +8,7 @@ package opt
 import (
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/cockroachdb/cockroach/pkg/util"
+	"github.com/cockroachdb/errors"
 )
 
 // ColumnID uniquely identifies the usage of a column within the scope of a
@@ -64,6 +65,20 @@ func (cl ColList) Equals(other ColList) bool {
 // CopyAndMaybeRemapColumns copies this ColList, remapping any columns present
 // in the provided ColMap.
 func (cl ColList) CopyAndMaybeRemapColumns(m ColMap) (res ColList) {
+	return cl.remapColumnsImpl(m, true /* allowMissingEntries */)
+}
+
+// RemapColumns remaps this ColList using the provided ColMap. It panics if any
+// column in the list is not present in the ColMap.
+func (cl ColList) RemapColumns(m ColMap) (res ColList) {
+	return cl.remapColumnsImpl(m, false /* allowMissingEntries */)
+}
+
+// remapColumnsImpl handles the common logic of CopyAndMaybeRemapColumns and
+// RemapColumns. If allowMissingEntries is true, it allows the column to be
+// missing from the ColMap, and it will be copied as-is. If it is false, the
+// column must be present in the ColMap.
+func (cl ColList) remapColumnsImpl(m ColMap, allowMissingEntries bool) (res ColList) {
 	if len(cl) == 0 {
 		return nil
 	}
@@ -71,7 +86,11 @@ func (cl ColList) CopyAndMaybeRemapColumns(m ColMap) (res ColList) {
 	for i := range cl {
 		val, ok := m.Get(int(cl[i]))
 		if !ok {
-			res[i] = cl[i]
+			if allowMissingEntries {
+				res[i] = cl[i]
+			} else {
+				panic(errors.AssertionFailedf("column %d not in mapping %s\n", cl[i], m.String()))
+			}
 		} else {
 			res[i] = ColumnID(val)
 		}
