acc7e41613ce740f842b7f881f2658db4b154ce5
Shailendra Patel <shailendra.patel@cockroachlabs.com>
2024-10-23 11:12:54 +0530

    drtprod: add init/run script for tpch workload
    Changes as part of this PR:
    - Added `generate_tpch_init.sh` script for import tpch workload.
    - Added `generate_tpch_run.sh` script for running tpch workload.
    - Modified `tpcc_init.sh` to use fixtures for importing data.
    - Added `tpcc_run_multiregion.sh` to run tpcc workload on multinode cluster.
    - Modified `drt-large.yaml` and `drt-scale.yaml` to use above changes.
    
    Epic: none
    
    Release note: None

---
+++ pkg/cmd/drtprod/scripts/generate_tpch_run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Copyright 2024 The Cockroach Authors.
+#
+# Use of this software is governed by the CockroachDB Software License
+# included in the /LICENSE file.
+
+# This script sets up the tpch run workload script in the workload nodes
+# The --scale-factor and other flags are passed as argument to this script
+# NOTE - This uses CLUSTER and WORKLOAD_CLUSTER environment variable, if not set the script fails
+if [ "$#" -lt 3 ]; then
+  echo "Usage: $0 <script_suffix> <flags to init:--scale-factor,--db>"
+  exit 1
+fi
+suffix=$1
+shift
+
+if [ -z "${CLUSTER}" ]; then
+  echo "environment CLUSTER is not set"
+  exit 1
+fi
+
+if [ -z "${WORKLOAD_CLUSTER}" ]; then
+  echo "environment WORKLOAD_CLUSTER is not set"
+  exit 1
+fi
+
+absolute_path=$(roachprod run "${WORKLOAD_CLUSTER}":1 -- "realpath ./tpch_run_${suffix}.sh")
+pwd=$(roachprod run "${WORKLOAD_CLUSTER}":1 -- "dirname ${absolute_path}")
+PGURLS=$(roachprod pgurl "${CLUSTER}":1)
+
+roachprod ssh "${WORKLOAD_CLUSTER}":1 -- "tee tpch_run_${suffix}.sh > /dev/null << 'EOF'
+#!/bin/bash
+
+${pwd}/cockroach workload run tpch $@ --verbose --prometheus-port 2113 $PGURLS
+EOF"
+roachprod ssh "${WORKLOAD_CLUSTER}":1 -- "chmod +x tpch_run_${suffix}.sh"
