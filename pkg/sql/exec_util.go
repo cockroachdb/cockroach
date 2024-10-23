ea3f400bcad53f04bf40dd2b2ffc27ed91d2f693
Drew Kimball <drewk@cockroachlabs.com>
2024-10-23 00:20:47 -0500

    opt: mechanical changes for BEFORE triggers on cascades
    This commit consists of the following changes to prepare for adding
    support for row-level BEFORE triggers to cascading mutations:
    * Added `unsafe_allow_triggers_modifying_cascades` which will determine
      whether a trigger can modify the rows mutated by a cascade, default off.
    * Added a "cascade" argument to `buildRowLevelBeforeTriggers` to provide
      the context when building a row-level BEFORE trigger.
    * Refactoring the logic for invoking `crdb_internal.plpgsql_raise` to be
      re-used for triggers.
    
    Informs #132971
    
    Release note: None

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -3888,6 +3888,10 @@ func (m *sessionDataMutator) SetOptimizerPushLimitIntoProjectFilteredScan(val bo
 	m.data.OptimizerPushLimitIntoProjectFilteredScan = val
 }
 
+func (m *sessionDataMutator) SetUnsafeAllowTriggersModifyingCascades(val bool) {
+	m.data.UnsafeAllowTriggersModifyingCascades = val
+}
+
 // Utility functions related to scrubbing sensitive information on SQL Stats.
 
 // quantizeCounts ensures that the Count field in the
