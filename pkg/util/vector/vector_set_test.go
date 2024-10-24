815e93ef224e276b7e0cea5392d2c22e998d47cc
Andrew Kimball <andyk@cockroachlabs.com>
2024-10-23 19:59:26 -0700

    vector: add vector.Set implementation
    Implement vector.Set, which is a set of float32 vectors of equal dimension.
    Vectors in the set are stored contiguously in a slice, in row-wise order.
    Future PR's will take advantage of this layout to implement fast set
    operations.
    
    Epic: CRDB-42943
    
    Release note: None

---
+++ pkg/util/vector/vector_set_test.go
@@ -0,0 +1,62 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package vector
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestVectorSet(t *testing.T) {
+	vs := MakeVectorSet(2)
+	require.Equal(t, 2, vs.Dims)
+	require.Equal(t, 0, vs.Count)
+
+	// Add methods.
+	v1 := T{1, 2}
+	v2 := T{5, 3}
+	v3 := T{6, 6}
+	vs.Add(v1)
+	vs.Add(v2)
+	vs.Add(v3)
+	require.Equal(t, 3, vs.Count)
+	require.Equal(t, []float32{1, 2, 5, 3, 6, 6}, vs.Data)
+
+	vs.AddSet(&vs)
+	require.Equal(t, 6, vs.Count)
+	require.Equal(t, []float32{1, 2, 5, 3, 6, 6, 1, 2, 5, 3, 6, 6}, vs.Data)
+
+	vs.AddZero(2)
+	vs.AddZero(0)
+	require.Equal(t, 8, vs.Count)
+	require.Equal(t, []float32{1, 2, 5, 3, 6, 6, 1, 2, 5, 3, 6, 6, 0, 0, 0, 0}, vs.Data)
+
+	// Remove.
+	vs.Remove(1)
+	vs.Remove(4)
+	require.Equal(t, 6, vs.Count)
+	require.Equal(t, []float32{1, 2, 0, 0, 6, 6, 1, 2, 0, 0, 6, 6}, vs.Data)
+
+	vs3 := MakeVectorSetFromRawData(vs.Data, 2)
+	require.Equal(t, vs, vs3)
+
+	// Ensure capacity.
+	vs4 := MakeVectorSet(3)
+	vs4.EnsureCapacity(5)
+	require.Equal(t, 0, len(vs4.Data))
+	require.GreaterOrEqual(t, cap(vs4.Data), 15)
+	vs4.AddZero(2)
+	require.Equal(t, 2, vs4.Count)
+	require.Equal(t, 6, len(vs4.Data))
+
+	// AsSet.
+	vs5 := T{1, 2, 3}.AsSet()
+	require.Equal(t, 3, cap(vs5.Data))
+	vs4.AddSet(&vs5)
+	require.Equal(t, 3, vs4.Count)
+	require.Equal(t, []float32{0, 0, 0, 0, 0, 0, 1, 2, 3}, vs4.Data)
+}
