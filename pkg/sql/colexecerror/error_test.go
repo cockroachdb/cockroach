5e05c2d968b70513842ad1e04223ba523564096c
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-10-28 22:44:25 +0000

    colexecerror: improve the catcher due to a recent regression
    Earlier this year we made the vectorized panic-catcher much more
    efficient (in #123277) by switching from using `debug.Stack()` to
    `runtime.CallersFrames`. It appears that there is slight difference in
    the behavior between the two: the former omits frames from within the
    runtime (only a single frame for the panic itself is included) whereas
    the latter keeps the additional runtime frames. As a result, if a panic
    occurs due to a Go runtime internal violation (e.g. invalid interface
    assertion) it is no longer caught to be converted into an internal CRDB
    error and now crashes the server. This commit fixes this regression by
    skipping over the frames that belong to the Go runtime. Note that we
    will do so only for up to 5 frames within the runtime, so if there
    happens to be more deeply-nested panic there, we'll still crash the CRDB
    server.
    
    Release note: None

--- pkg/sql/colexecerror/error_test.go
+++ pkg/sql/colexecerror/error_test.go
@@ -79,6 +79,36 @@ func TestNonCatchablePanicIsNotCaught(t *testing.T) {
 	})
 }
 
+type testInterface interface {
+	foo()
+}
+
+type testImpl1 struct{}
+
+var _ testInterface = &testImpl1{}
+
+func (t testImpl1) foo() {}
+
+type testImpl2 struct{}
+
+var _ testInterface = &testImpl2{}
+
+func (t testImpl2) foo() {}
+
+// TestRuntimePanicIsCaught verifies that if a runtime panic occurs in the
+// safe-to-catch package (which this test package is), then it is converted into
+// an internal error (#133167).
+func TestRuntimePanicIsCaught(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	require.Error(t, colexecerror.CatchVectorizedRuntimeError(func() {
+		// Attempt an invalid interface conversion.
+		var o testInterface = &testImpl1{}
+		_ = o.(*testImpl2)
+	}))
+}
+
 // BenchmarkCatchVectorizedRuntimeError measures the time for
 // CatchVectorizedRuntimeError to catch and process an error.
 func BenchmarkCatchVectorizedRuntimeError(b *testing.B) {
