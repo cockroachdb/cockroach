bb0e49c34f34e44fd33e8b88ec66fff5379432a1
Andrew Baptist <baptist@cockroachlabs.com>
2024-10-29 09:08:24 -0400

    roachtest: move metamorphic perturbation tests to their own suite
    The metamorphic perturbation tests are intended to find new bugs in
    existing code. They are not a regression test against existing code. As
    such they have funadmentally different properties that typical
    regression roachtests and instead are more like the DRT cluster. These
    tests already have a complex set of conditions they work around and it
    would be even more complex to try and backport those conditions to older
    releases.
    
    Epic: none
    
    Release note: None

---
+++ build/teamcity/cockroach/nightlies/roachtest_nightly_gce_perturbation.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Copyright 2024 The Cockroach Authors.
+#
+# Use of this software is governed by the CockroachDB Software License
+# included in the /LICENSE file.
+
+
+set -exuo pipefail
+
+dir="$(dirname $(dirname $(dirname $(dirname "${0}"))))"
+
+source "$dir/teamcity-support.sh"  # For $root
+source "$dir/teamcity-bazel-support.sh"  # For run_bazel
+
+BAZEL_SUPPORT_EXTRA_DOCKER_ARGS="-e LITERAL_ARTIFACTS_DIR=$root/artifacts -e BUILD_VCS_NUMBER -e CLOUD -e COCKROACH_DEV_LICENSE -e TESTS -e COUNT -e GITHUB_API_TOKEN -e GITHUB_ORG -e GITHUB_REPO -e GOOGLE_EPHEMERAL_CREDENTIALS -e GOOGLE_KMS_KEY_A -e GOOGLE_KMS_KEY_B -e GOOGLE_CREDENTIALS_ASSUME_ROLE -e GOOGLE_SERVICE_ACCOUNT -e SLACK_TOKEN -e TC_BUILDTYPE_ID -e TC_BUILD_BRANCH -e TC_BUILD_ID -e TC_SERVER_URL -e SELECT_PROBABILITY -e COCKROACH_RANDOM_SEED -e ROACHTEST_ASSERTIONS_ENABLED_SEED -e ROACHTEST_FORCE_RUN_INVALID_RELEASE_BRANCH -e GRAFANA_SERVICE_ACCOUNT_JSON -e GRAFANA_SERVICE_ACCOUNT_AUDIENCE -e USE_SPOT -e SFUSER -e SFPASSWORD -e SIDE_EYE_API_TOKEN -e COCKROACH_EA_PROBABILITY" \
+			       run_bazel build/teamcity/cockroach/nightlies/perturbation_nightly_metamorphic_impl.sh
