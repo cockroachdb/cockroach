cockroachdb/cockroach

commit 61b8933cdc31d97ae78b892f5d558accce35addb

raft: don't assume MsgSnap comes from leader
We can't consider MsgSnap to be from the leader of Message.Term until we
address #127348 and #127349.

Epic: None
Release note: None

pkg/raft/raft.go
@@ -2075,6 +2075,11 @@ func stepCandidate(r *raft, m pb.Message) error {
 	case pb.MsgProp:
 		r.logger.Infof("%x no leader at term %d; dropping proposal", r.id, r.Term)
 		return ErrProposalDropped
+	case pb.MsgSnap:
+		// TODO(nvanbenschoten): we can't consider MsgSnap to be from the leader of
+		// Message.Term until we address #127348 and #127349.
+		r.becomeFollower(m.Term, None)
+		r.handleSnapshot(m)
 	case myVoteRespType:
 		gr, rj, res := r.poll(m.From, m.Type, !m.Reject)
 		r.logger.Infof("%x has received %d %s votes and %d vote rejections", r.id, gr, m.Type, rj)

pkg/raft/util.go
@@ -50,8 +50,10 @@ var isResponseMsg = [...]bool{
 }
 
 var isMsgFromLeader = [...]bool{
-	pb.MsgApp:             true,
-	pb.MsgSnap:            true,
+	pb.MsgApp: true,
+	// TODO(nvanbenschoten): we can't consider MsgSnap to be from the leader of
+	// Message.Term until we address #127348 and #127349.
+	// pb.MsgSnap:            true,
 	pb.MsgHeartbeat:       true,
 	pb.MsgTimeoutNow:      true,
 	pb.MsgFortifyLeader:   true,

pkg/raft/util_test.go
@@ -165,7 +165,7 @@ func TestMsgFromLeader(t *testing.T) {
 		{pb.MsgAppResp, false},
 		{pb.MsgVote, false},
 		{pb.MsgVoteResp, false},
-		{pb.MsgSnap, true},
+		{pb.MsgSnap, false},
 		{pb.MsgHeartbeat, true},
 		{pb.MsgHeartbeatResp, false},
 		{pb.MsgTimeoutNow, true},

pkg/raft/raft_test.go
@@ -2615,7 +2615,7 @@ func TestRestoreFromSnapMsg(t *testing.T) {
 	sm := newTestRaft(2, 10, 1, newTestMemoryStorage(withPeers(1, 2)))
 	sm.Step(m)
 
-	assert.Equal(t, pb.PeerID(1), sm.lead)
+	assert.Equal(t, None, sm.lead)
 	// TODO(bdarnell): what should this test?
 }
 

commit be18a93881353ac0525654d120d998fe9bbaf775

raft: don't panic on defortifying snapshot from new term
Informs #132762.

This commit fixes a bug introduced by 61b8933c. Now that we no longer assume
that snapshots are coming from the leader, we were not defortifying the raft
leadership when receiving a snapshot. This meant that if a snapshot came from
a new term, we would hit an assertion failure, which the new test reproduces.

This commit addresses this bug by distinguishing between messages that are
always sent from the leader of the message's term and messages that indicate
that there is a new leader of the message's term, even if the message is not
from the leader itself.

This is temporary, and can be removed when we address #127349.

Release note: None

pkg/raft/raft.go
@@ -1065,8 +1065,7 @@ func (r *raft) setTerm(term uint64) {
 	assertTrue(term > r.Term, "term cannot regress")
 	r.Term = term
 	r.Vote = None
-	r.lead = None
-	r.leadEpoch = 0
+	r.resetLead()
 }
 
 func (r *raft) setVote(id pb.PeerID) {
@@ -1087,7 +1086,7 @@ func (r *raft) setLead(lead pb.PeerID) {
 
 func (r *raft) resetLead() {
 	r.lead = None
-	r.leadEpoch = 0
+	r.resetLeadEpoch()
 }
 
 func (r *raft) setLeadEpoch(leadEpoch pb.Epoch) {
@@ -1537,13 +1536,21 @@ func (r *raft) Step(m pb.Message) error {
 		default:
 			r.logger.Infof("%x [term: %d] received a %s message with higher term from %x [term: %d]",
 				r.id, r.Term, m.Type, m.From, m.Term)
-			if IsMsgFromLeader(m.Type) {
-				// We've just received a message from a leader which was elected
-				// at a higher term. The old leader is no longer fortified, so it's
-				// safe to de-fortify at this point.
+			if IsMsgIndicatingLeader(m.Type) {
+				// We've just received a message that indicates that a new leader
+				// was elected at a higher term, but the message may not be from the
+				// leader itself. Either way, the old leader is no longer fortified,
+				// so it's safe to de-fortify at this point.
 				r.deFortify(m.From, m.Term)
-				r.becomeFollower(m.Term, m.From)
+				var lead pb.PeerID
+				if IsMsgFromLeader(m.Type) {
+					lead = m.From
+				}
+				r.becomeFollower(m.Term, lead)
 			} else {
+				// We've just received a message that does not indicate that a new
+				// leader was elected at a higher term. All it means is that some
+				// other peer has this term.
 				r.becomeFollower(m.Term, None)
 			}
 		}

pkg/raft/util.go
@@ -49,11 +49,27 @@ var isResponseMsg = [...]bool{
 	pb.MsgFortifyLeaderResp: true,
 }
 
+// isMsgFromLeader contains message types that come from the leader of the
+// message's term.
 var isMsgFromLeader = [...]bool{
 	pb.MsgApp: true,
 	// TODO(nvanbenschoten): we can't consider MsgSnap to be from the leader of
 	// Message.Term until we address #127348 and #127349.
 	// pb.MsgSnap:            true,
+	pb.MsgHeartbeat:       true,
+	pb.MsgTimeoutNow:      true,
+	pb.MsgFortifyLeader:   true,
+	pb.MsgDeFortifyLeader: true,
+}
+
+// isMsgIndicatingLeader contains message types that indicate that there is a
+// leader at the message's term, even if the message is not from the leader
+// itself.
+//
+// TODO(nvanbenschoten): remove this when we address the TODO above.
+var isMsgIndicatingLeader = [...]bool{
+	pb.MsgApp:             true,
+	pb.MsgSnap:            true,
 	pb.MsgHeartbeat:       true,
 	pb.MsgTimeoutNow:      true,
 	pb.MsgFortifyLeader:   true,
@@ -75,6 +91,10 @@ func IsResponseMsg(msgt pb.MessageType) bool {
 
 func IsMsgFromLeader(msgt pb.MessageType) bool {
 	return isMsgInArray(msgt, isMsgFromLeader[:])
+}
+
+func IsMsgIndicatingLeader(msgt pb.MessageType) bool {
+	return isMsgInArray(msgt, isMsgIndicatingLeader[:])
 }
 
 func IsLocalMsgTarget(id pb.PeerID) bool {

pkg/raft/util_test.go
@@ -186,6 +186,50 @@ func TestMsgFromLeader(t *testing.T) {
 		if got != tt.isMsgFromLeader {
 			t.Errorf("#%d: got %v, want %v", i, got, tt.isMsgFromLeader)
 		}
+		if got {
+			require.True(t, IsMsgIndicatingLeader(tt.msgt),
+				"IsMsgFromLeader should imply IsMsgIndicatingLeader")
+		}
+	}
+}
+
+func TestMsgIndicatingLeader(t *testing.T) {
+	tests := []struct {
+		msgt                  pb.MessageType
+		isMsgIndicatingLeader bool
+	}{
+		{pb.MsgHup, false},
+		{pb.MsgBeat, false},
+		{pb.MsgUnreachable, false},
+		{pb.MsgSnapStatus, false},
+		{pb.MsgCheckQuorum, false},
+		{pb.MsgTransferLeader, false},
+		{pb.MsgProp, false},
+		{pb.MsgApp, true},
+		{pb.MsgAppResp, false},
+		{pb.MsgVote, false},
+		{pb.MsgVoteResp, false},
+		{pb.MsgSnap, true},
+		{pb.MsgHeartbeat, true},
+		{pb.MsgHeartbeatResp, false},
+		{pb.MsgTimeoutNow, true},
+		{pb.MsgPreVote, false},
+		{pb.MsgPreVoteResp, false},
+		{pb.MsgStorageAppend, false},
+		{pb.MsgStorageAppendResp, false},
+		{pb.MsgStorageApply, false},
+		{pb.MsgStorageApplyResp, false},
+		{pb.MsgForgetLeader, false},
+		{pb.MsgFortifyLeader, true},
+		{pb.MsgFortifyLeaderResp, false},
+		{pb.MsgDeFortifyLeader, true},
+	}
+
+	for i, tt := range tests {
+		got := IsMsgIndicatingLeader(tt.msgt)
+		if got != tt.isMsgIndicatingLeader {
+			t.Errorf("#%d: got %v, want %v", i, got, tt.isMsgIndicatingLeader)
+		}
 	}
 }
 