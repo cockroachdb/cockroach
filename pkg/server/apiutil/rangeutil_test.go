b2b7d4e82a47a8fac95e28eae8c6efd40a11d212
Brian Dillmann <brian.dillmann@cockroachlabs.com>
2024-10-30 09:58:04 -0400

    apiutil, roachpb: create utilities to map descriptors to ranges
    Previously each range correlated to a single table, or even a single
    index in a database, so all that was required to identify which tables,
    indexes were in the range were to look at the start key of the range and
    map it accordingly.
    
    With range coalescing however, it's possible for one, or many, tables,
    indexes and the like to reside within the same index. To properly
    identify the contents of a range, this PR adds the following utilities:
    
     1. A utility function which turns a range into a span, and binds it
    		within its tenant's descriptor space.
     2. A utility function which takes the above spans and uses the catalog
    		and new descriptor by span utility to turn those spans into a set of
    		table descriptors ordered by id.
     3. A utility function which transforms those table descriptors into a
    		set of (database, table, index) names which deduplicate and identify
    		each index uniquely.
     4. A utility function, which merges the ranges and indexes into a map
    		keyed by RangeID whose values are the above index names.
     5. A primary entrypoint for consumers from which a set of ranges can be
    		passed in and a mapping from those ranges to indexes can be
    		returned.
    
    A variety of cavets come with this approach. It attempts to scan the
    desciptors all at once, but it still will scan a sizable portion of the
    descriptors table if the request is large enough. This makes no attempt
    to describe system information which does not have a descriptor. It will
    describe system tables which appear in the descriptors table, but it
    will not try to explain "tables" which do not have descriptors (example
    tsdb), or any other information stored in the keyspace without a
    descriptor (PseudoTableIDs, GossipKeys for example).
    
    Throughout this work, many existing utilities were duplicated, and then
    un-duplicated (`keys.TableDataMin`, `roachpb.Span.Overlap`, etc). If you
    see anything that seems to already exist, feel free to point it out
    accordingly.
    
    Epic: none
    Fixes: #130997
    
    Release note: None

---
+++ pkg/server/apiutil/rangeutil_test.go
@@ -0,0 +1,223 @@
+package apiutil_test
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/server/apiutil"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/dbdesc"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/tabledesc"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/catid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMapRangesToIndexes(t *testing.T) {
+	k := func(c byte) roachpb.Key {
+		return roachpb.Key([]byte{c})
+	}
+	rk := func(c byte) roachpb.RKey {
+		return roachpb.RKey(k(c))
+	}
+	ranges := []roachpb.RangeDescriptor{
+		{RangeID: 1, StartKey: rk('d'), EndKey: rk('m')},
+		{RangeID: 2, StartKey: rk('m'), EndKey: rk('x')},
+	}
+
+	indexes := []apiutil.IndexNames{
+		{Index: "Totally before first", Span: roachpb.Span{Key: k('a'), EndKey: k('c')}},
+		{Index: "Start and before first", Span: roachpb.Span{Key: k('c'), EndKey: k('e')}},
+		{Index: "Middle of first range", Span: roachpb.Span{Key: k('e'), EndKey: k('f')}},
+		{Index: "Overlaps with both", Span: roachpb.Span{Key: k('f'), EndKey: k('o')}},
+		{Index: "Middle of second range", Span: roachpb.Span{Key: k('o'), EndKey: k('q')}},
+		{Index: "End and after second", Span: roachpb.Span{Key: k('q'), EndKey: k('y')}},
+		{Index: "Totally after end", Span: roachpb.Span{Key: k('y'), EndKey: k('z')}},
+	}
+
+	expected := map[roachpb.RangeID][]apiutil.IndexNames{
+		1: {
+			{Index: "Start and before first"},
+			{Index: "Middle of first range"},
+			{Index: "Overlaps with both"},
+		},
+		2: {
+			{Index: "Overlaps with both"},
+			{Index: "Middle of second range"},
+			{Index: "End and after second"},
+		},
+	}
+
+	result := apiutil.MapRangesToIndexes(ranges, indexes)
+
+	if len(result) != len(expected) {
+		t.Fatalf("expected %d ranges, got %d", len(expected), len(result))
+	}
+
+	for rangeID, expectedIndexes := range expected {
+		actualIndexes, ok := result[rangeID]
+		if !ok {
+			t.Fatalf("expected rangeID %d not found", rangeID)
+		}
+		if len(actualIndexes) != len(expectedIndexes) {
+			t.Fatalf("expected %d indexes for rangeID %d, got %d", len(expectedIndexes), rangeID, len(actualIndexes))
+		}
+		for i, expectedIndex := range expectedIndexes {
+			if actualIndexes[i].Index != expectedIndex.Index {
+				t.Fatalf("expected index %v, got %v", expectedIndex, actualIndexes[i])
+			}
+		}
+	}
+}
+
+func TestRangesToTableSpans(t *testing.T) {
+	codec := keys.MakeSQLCodec(roachpb.MustMakeTenantID(1))
+	ranges := []roachpb.RangeDescriptor{
+		// should be zero len
+		{
+			StartKey: roachpb.RKey(codec.TablePrefix(0).Prevish(1)),
+			EndKey:   roachpb.RKey(codec.TablePrefix(0)),
+		},
+		// should also be zero len
+		{
+			StartKey: roachpb.RKey(codec.TablePrefix(1)),
+			EndKey:   roachpb.RKey(codec.TablePrefix(1)),
+		},
+		{
+			StartKey: roachpb.RKey(codec.TablePrefix(1)),
+			EndKey:   roachpb.RKey(codec.TablePrefix(3)),
+		},
+		{
+			StartKey: roachpb.RKey(codec.TablePrefix(3)),
+			EndKey:   roachpb.RKey(codec.TablePrefix(5)),
+		},
+		{
+			StartKey: roachpb.RKey(codec.TablePrefix(5)),
+			EndKey:   roachpb.RKey(codec.TablePrefix(6)),
+		},
+	}
+
+	expectedSpans := []roachpb.Span{
+		{
+			Key:    codec.TablePrefix(1),
+			EndKey: codec.TablePrefix(3),
+		},
+		{
+			Key:    codec.TablePrefix(3),
+			EndKey: codec.TablePrefix(5),
+		},
+		{
+			Key:    codec.TablePrefix(5),
+			EndKey: codec.TablePrefix(6),
+		},
+	}
+
+	result := apiutil.RangesToTableSpans(codec, ranges)
+
+	if len(result) != len(expectedSpans) {
+		t.Fatalf("expected %d spans, got %d", len(expectedSpans), len(result))
+	}
+
+	for i, expectedSpan := range expectedSpans {
+		if !result[i].Equal(expectedSpan) {
+			t.Fatalf("expected span %v, got %v", expectedSpan, result[i])
+		}
+	}
+}
+
+func makeDBDesc(id uint32, name string) catalog.DatabaseDescriptor {
+
+	db := &dbdesc.Mutable{}
+	db.SetName(name)
+	db.ID = catid.DescID(id)
+	descriptor := db.ImmutableCopy()
+	return descriptor.(catalog.DatabaseDescriptor)
+}
+
+func makeTableDesc(databaseID uint32, name string, indexes []string) catalog.TableDescriptor {
+	descIndexes := []descpb.IndexDescriptor{}
+	table := tabledesc.NewBuilder(&descpb.TableDescriptor{
+		Name:     name,
+		ParentID: catid.DescID(databaseID),
+		Indexes:  descIndexes,
+	}).BuildCreatedMutableTable()
+	for i, name := range indexes {
+		if i == 0 {
+			table.AddPrimaryIndex(descpb.IndexDescriptor{Name: name})
+		} else {
+
+			table.AddSecondaryIndex(descpb.IndexDescriptor{Name: name})
+		}
+	}
+	return table.NewBuilder().BuildImmutable().(catalog.TableDescriptor)
+}
+
+func TestTableDescriptorsToIndexNames(t *testing.T) {
+	// test straightforward path with three tables, two databases
+	codec := keys.MakeSQLCodec(roachpb.MustMakeTenantID(1))
+	databases := map[descpb.ID]catalog.DatabaseDescriptor{
+		1: makeDBDesc(1, "db1"),
+		2: makeDBDesc(2, "db2"),
+	}
+	tables := []catalog.TableDescriptor{
+		makeTableDesc(1, "table1", []string{"pkey"}),
+		makeTableDesc(2, "table2", []string{"pkey", "table2_secondary_column"}),
+		makeTableDesc(2, "table3", []string{"pkey"}),
+	}
+
+	expected := []apiutil.IndexNames{
+		{Database: "db1", Table: "table1", Index: "pkey"},
+		{Database: "db2", Table: "table2", Index: "pkey"},
+		{Database: "db2", Table: "table2", Index: "table2_secondary_column"},
+		{Database: "db2", Table: "table3", Index: "pkey"},
+	}
+	indexes, err := apiutil.TableDescriptorsToIndexNames(codec, databases, tables)
+
+	require.NoError(t, err)
+	require.Equal(t, len(expected), len(indexes))
+	for i, index := range indexes {
+		if !index.Equal(expected[i]) {
+			t.Fatalf("resulting index did not match expected output: %s %s", index, expected[i])
+		}
+	}
+}
+
+func TestTableDescriptorsToIndexNamesDeduplicates(t *testing.T) {
+	// verify that duplicate descriptors are de-duplicated
+	codec := keys.MakeSQLCodec(roachpb.MustMakeTenantID(1))
+	databases := map[descpb.ID]catalog.DatabaseDescriptor{
+		1: makeDBDesc(1, "db1"),
+	}
+	tables := []catalog.TableDescriptor{
+		makeTableDesc(1, "table1", []string{"pkey", "table1_secondary_column"}),
+		makeTableDesc(1, "table1", []string{"pkey", "table1_secondary_column"}),
+	}
+
+	expected := []apiutil.IndexNames{
+		{Database: "db1", Table: "table1", Index: "pkey"},
+		{Database: "db1", Table: "table1", Index: "table1_secondary_column"},
+	}
+	indexes, err := apiutil.TableDescriptorsToIndexNames(codec, databases, tables)
+
+	require.NoError(t, err)
+	require.Equal(t, len(expected), len(indexes))
+	for i, index := range indexes {
+		if !index.Equal(expected[i]) {
+			t.Fatalf("resulting index did not match expected output: %s %s", index, expected[i])
+		}
+	}
+}
+
+func TestGetIndexNamesFromDescriptorsMissingDatabase(t *testing.T) {
+	codec := keys.MakeSQLCodec(roachpb.MustMakeTenantID(1))
+	databases := map[descpb.ID]catalog.DatabaseDescriptor{
+		1: makeDBDesc(1, "db1"),
+	}
+	tables := []catalog.TableDescriptor{
+		makeTableDesc(2, "table2", []string{"pkey", "table2_secondary_column"}),
+	}
+
+	_, err := apiutil.TableDescriptorsToIndexNames(codec, databases, tables)
+	require.Errorf(t, err, "could not find database for table %s", tables[0].GetName())
+}
