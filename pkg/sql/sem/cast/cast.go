0e2cd651f638535158eecf383fdb2294ac01a5ec
Yahor Yuzefovich <yahor@cockroachlabs.com>
2024-10-31 14:51:39 -0700

    builtins: populate cast builtin overloads deterministically
    This commit adjusts a couple of places where we populate the overloads
    of the cast builtin function to be in determinstic order. This will
    allow for easier reproduction of some tests that use sqlsmith. Note that
    there were other sources of non-determinism with `TestExplainGist` that
    I couldn't track down.
    
    Release note: None

--- pkg/sql/sem/cast/cast.go
+++ pkg/sql/sem/cast/cast.go
@@ -10,6 +10,8 @@
 package cast
 
 import (
+	"sort"
+
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/volatility"
 	"github.com/cockroachdb/cockroach/pkg/sql/types"
 	"github.com/lib/pq/oid"
@@ -128,14 +130,31 @@ type Cast struct {
 }
 
 // ForEachCast calls fn for every valid cast from a source type to a target
-// type.
+// type. Iteration order is deterministic.
 func ForEachCast(
 	fn func(
 		src oid.Oid, tgt oid.Oid, castCtx Context, ctxOrigin ContextOrigin, v volatility.V,
 	),
 ) {
-	for src, tgts := range castMap {
-		for tgt, cast := range tgts {
+	srcOids := make([]oid.Oid, 0, len(castMap))
+	for src := range castMap {
+		srcOids = append(srcOids, src)
+	}
+	sort.Slice(srcOids, func(i, j int) bool {
+		return srcOids[i] < srcOids[j]
+	})
+	var tgtOids []oid.Oid
+	for _, src := range srcOids {
+		tgts := castMap[src]
+		tgtOids = tgtOids[:0]
+		for tgt := range tgts {
+			tgtOids = append(tgtOids, tgt)
+		}
+		sort.Slice(tgtOids, func(i, j int) bool {
+			return tgtOids[i] < tgtOids[j]
+		})
+		for _, tgt := range tgtOids {
+			cast := tgts[tgt]
 			fn(src, tgt, cast.MaxContext, cast.origin, cast.Volatility)
 		}
 	}
