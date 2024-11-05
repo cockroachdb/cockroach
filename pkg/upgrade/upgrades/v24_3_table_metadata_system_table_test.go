22e6898652284640e253c14bffad804ea4237769
Radu Berinde <radu@cockroachlabs.com>
2024-11-05 14:27:52 -0800

    upgrades: add missing SkipWhenMinSupportedVersionIsAtLeast
    This was supposed to be used so that we don't have to necessarily
    remove these tests in the PR which bumps `MinVersion` (which is
    typically very large as-is).
    
    Epic: none
    Release note: None

--- pkg/upgrade/upgrades/v24_3_table_metadata_system_table_test.go
+++ pkg/upgrade/upgrades/v24_3_table_metadata_system_table_test.go
@@ -23,6 +23,8 @@ func TestAddTableMetadataTable(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 
+	clusterversion.SkipWhenMinSupportedVersionIsAtLeast(t, 24, 3)
+
 	clusterArgs := base.TestClusterArgs{
 		ServerArgs: base.TestServerArgs{
 			Knobs: base.TestingKnobs{
