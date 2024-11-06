bba7be697570e02196c39f24b9c046edf7959ff6
Ibrahim Kettaneh <ibrahim.kettaneh@cockroachlabs.com>
2024-11-06 22:49:02 +0000

    raft: establish for metrics inside raft
    This commit sets the basis for metrics inside Raft. It links them
    with the store registry. Future commits add some raft metrics.
    
    References: #132754
    
    Release note: None

--- pkg/raft/raft.go
+++ pkg/raft/raft.go
@@ -257,6 +257,7 @@ type Config struct {
 	// CRDBVersion exposes the active version to Raft. This helps version-gating
 	// features.
 	CRDBVersion clusterversion.Handle
+	Metrics     *Metrics
 }
 
 func (c *Config) validate() error {
@@ -428,6 +429,7 @@ type raft struct {
 	logger        raftlogger.Logger
 	storeLiveness raftstoreliveness.StoreLiveness
 	crdbVersion   clusterversion.Handle
+	metrics       *Metrics
 }
 
 func newRaft(c *Config) *raft {
@@ -458,6 +460,7 @@ func newRaft(c *Config) *raft {
 		disableConfChangeValidation: c.DisableConfChangeValidation,
 		storeLiveness:               c.StoreLiveness,
 		crdbVersion:                 c.CRDBVersion,
+		metrics:                     c.Metrics,
 	}
 	lastID := r.raftLog.lastEntryID()
 
