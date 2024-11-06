18fb61737ba2ac03c1b347a181bdb5e51c662aab
Ibrahim Kettaneh <ibrahim.kettaneh@cockroachlabs.com>
2024-11-06 17:56:11 -0500

    raft: add raft fortification metrics
    This adds the following raft metrics:
    
    1) raft.fortification_resp.accepted: Reports the number of fortification
    responses that are accepted.
    
    2) raft.fortification_resp.rejected: Reports the number of fortification
    responses that are rejected.
    
    3) raft.fortification.skipped_no_support: Reports the number of
    fortification requests that were skipped due to the lack of store
    liveness support.
    
    References: #132754
    
    Release note: None

--- pkg/raft/metrics.go
+++ pkg/raft/metrics.go
@@ -5,10 +5,44 @@
 
 package raft
 
-// Metrics contains all the metrics reported inside Raft.
-type Metrics struct{}
+import "github.com/cockroachdb/cockroach/pkg/util/metric"
+
+// Metrics all the metrics reported in Raft.
+type Metrics struct {
+	AcceptedFortificationResponses         *metric.Counter
+	RejectedFortificationResponses         *metric.Counter
+	SkippedFortificationDueToLackOfSupport *metric.Counter
+}
 
 // NewMetrics creates a new Metrics instance with all related metric fields.
 func NewMetrics() *Metrics {
-	return &Metrics{}
+	return &Metrics{
+		AcceptedFortificationResponses: metric.NewCounter(
+			metric.Metadata{
+				Name:        "raft.fortification_resp.accepted",
+				Help:        "The number of accepted fortification responses",
+				Measurement: "Accepted Fortification Responses",
+				Unit:        metric.Unit_COUNT,
+			},
+		),
+
+		RejectedFortificationResponses: metric.NewCounter(
+			metric.Metadata{
+				Name:        "raft.fortification_resp.rejected",
+				Help:        "The number of rejected fortification responses",
+				Measurement: "Rejected Fortification Responses",
+				Unit:        metric.Unit_COUNT,
+			},
+		),
+
+		SkippedFortificationDueToLackOfSupport: metric.NewCounter(
+			metric.Metadata{
+				Name: "raft.fortification.skipped_no_support",
+				Help: "The number of fortification requests that were skipped due to lack of store" +
+					" liveness support",
+				Measurement: "Skipped Fortifications",
+				Unit:        metric.Unit_COUNT,
+			},
+		),
+	}
 }
