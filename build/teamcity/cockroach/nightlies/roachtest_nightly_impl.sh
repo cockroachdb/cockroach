7dbcb9b512a5253c6a662b83ba7d3132959d175b
DarrylWong <darryl@cockroachlabs.com>
2024-11-11 12:01:09 -0500

    dnm: bumping runtime assertion prob to 1.0, fake test branch

--- build/teamcity/cockroach/nightlies/roachtest_nightly_impl.sh
+++ build/teamcity/cockroach/nightlies/roachtest_nightly_impl.sh
@@ -68,7 +68,7 @@ fi
 build/teamcity-roachtest-invoke.sh \
   --metamorphic-encryption-probability=0.5 \
   --metamorphic-arm64-probability="${ARM_PROBABILITY:-0.5}" \
-  --metamorphic-cockroach-ea-probability="${COCKROACH_EA_PROBABILITY:-0.2}" \
+  --metamorphic-cockroach-ea-probability="${COCKROACH_EA_PROBABILITY:-1.0}" \
   --select-probability="${select_probability}" \
   --use-spot="${USE_SPOT:-auto}" \
   --cloud="${CLOUD}" \
