62e84487d0753c60cc6a976d117186805727e3b0
Aaditya Sondhi <20070511+aadityasondhi@users.noreply.github.com>
2024-11-11 13:37:56 -0500

    admission: per-store work queue metrics
    This patch splits store work queue metrics to be per-store. It also does
    some general clean up around the `storeGrantCoordinators` code to
    centralize initialization of requesters and metrics.
    
    Fixes #131562
    
    Release note: None

--- pkg/server/node.go
+++ pkg/server/node.go
@@ -1381,7 +1381,9 @@ func (pmp *nodePebbleMetricsProvider) GetPebbleMetrics() []admission.StoreMetric
 			StoreID:         store.StoreID(),
 			Metrics:         m.Metrics,
 			WriteStallCount: m.WriteStallCount,
-			DiskStats:       diskStats})
+			DiskStats:       diskStats,
+			MetricsRegistry: store.Registry(),
+		})
 		return nil
 	})
 	return metrics
