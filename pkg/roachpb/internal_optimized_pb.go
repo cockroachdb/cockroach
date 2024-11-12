87d114056632650bbb3bc928d447bf623c86efb5
Tobias Grieger <tobias.b.grieger@gmail.com>
2024-11-12 09:35:29 +0100

    roachpb: optimized MarshalToSizedBuffer

--- pkg/roachpb/internal_optimized_pb.go
+++ pkg/roachpb/internal_optimized_pb.go
@@ -8,6 +8,8 @@ package roachpb
 import (
 	encoding_binary "encoding/binary"
 	"math"
+
+	"github.com/cockroachdb/cockroach/pkg/util/buildutil"
 )
 
 // ResetRetainingSlices clears all fields in the InternalTimeSeriesData, but
@@ -35,7 +37,7 @@ type InternalTimeSeriesDataMarshaller struct {
 	countSize int
 }
 
-func (marshaller *InternalTimeSeriesDataMarshaller) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+func (marshaller *InternalTimeSeriesDataMarshaller) MarshalToSizedBuffer(dAtA []byte) {
 	m := marshaller.data
 	i := len(dAtA)
 	_ = i
@@ -92,20 +94,28 @@ func (marshaller *InternalTimeSeriesDataMarshaller) MarshalToSizedBuffer(dAtA []
 		dAtA[i] = 0x3a
 	}
 	if len(m.Count) > 0 {
-		dAtA7 := make([]byte, len(m.Count)*10)
-		var j6 int
-		for _, num := range m.Count {
-			for num >= 1<<7 {
-				dAtA7[j6] = uint8(uint64(num)&0x7f | 0x80)
-				num >>= 7
+		i -= marshaller.countSize
+		// Fast path for the case where all the values are small.
+		if marshaller.countSize == len(m.Count) {
+			for k, num := range m.Count {
+				dAtA[i+k] = uint8(num)
+			}
+		} else {
+			j6 := i
+			for _, num := range m.Count {
+				for num >= 1<<7 {
+					dAtA[j6] = uint8(uint64(num)&0x7f | 0x80)
+					num >>= 7
+					j6++
+				}
+				dAtA[j6] = uint8(num)
 				j6++
 			}
-			dAtA7[j6] = uint8(num)
-			j6++
+			if buildutil.CrdbTestBuild && j6-i != marshaller.countSize {
+				panic("countSize mismatch")
+			}
 		}
-		i -= j6
-		copy(dAtA[i:], dAtA7[:j6])
-		i = encodeVarintInternal(dAtA, i, uint64(j6))
+		i = encodeVarintInternal(dAtA, i, uint64(marshaller.countSize))
 		i--
 		dAtA[i] = 0x32
 	}
@@ -120,31 +130,37 @@ func (marshaller *InternalTimeSeriesDataMarshaller) MarshalToSizedBuffer(dAtA []
 		dAtA[i] = 0x2a
 	}
 	if len(m.Offset) > 0 {
-		dAtA10 := make([]byte, len(m.Offset)*10)
-		var j9 int
-		for _, num1 := range m.Offset {
-			num := uint64(num1)
-			for num >= 1<<7 {
-				dAtA10[j9] = uint8(uint64(num)&0x7f | 0x80)
-				num >>= 7
+		i -= marshaller.offsetSize
+		// Fast path for the case where all the values are small.
+		if marshaller.offsetSize == len(m.Offset) {
+			for k, num := range m.Offset {
+				dAtA[i+k] = uint8(num)
+			}
+		} else {
+			j9 := i
+			for _, num1 := range m.Offset {
+				num := uint64(num1)
+				for num >= 1<<7 {
+					dAtA[j9] = uint8(num&0x7f | 0x80)
+					num >>= 7
+					j9++
+				}
+				dAtA[j9] = uint8(num)
 				j9++
 			}
-			dAtA10[j9] = uint8(num)
-			j9++
+			if buildutil.CrdbTestBuild && j9-i != marshaller.offsetSize {
+				panic("offsetSize mismatch")
+			}
 		}
-		i -= j9
-		copy(dAtA[i:], dAtA10[:j9])
-		i = encodeVarintInternal(dAtA, i, uint64(j9))
+		i = encodeVarintInternal(dAtA, i, uint64(marshaller.offsetSize))
 		i--
 		dAtA[i] = 0x22
 	}
 	if len(m.Samples) > 0 {
 		for iNdEx := len(m.Samples) - 1; iNdEx >= 0; iNdEx-- {
 			{
-				size, err := m.Samples[iNdEx].MarshalToSizedBuffer(dAtA[:i])
-				if err != nil {
-					return 0, err
-				}
+				// This method never returns an error.
+				size, _ := m.Samples[iNdEx].MarshalToSizedBuffer(dAtA[:i])
 				i -= size
 				i = encodeVarintInternal(dAtA, i, uint64(size))
 			}
@@ -158,7 +174,9 @@ func (marshaller *InternalTimeSeriesDataMarshaller) MarshalToSizedBuffer(dAtA []
 	i = encodeVarintInternal(dAtA, i, uint64(m.StartTimestampNanos))
 	i--
 	dAtA[i] = 0x8
-	return len(dAtA) - i, nil
+	if i != 0 {
+		panic("mismatch in size of marshalled data")
+	}
 }
 
 // Size is equivalent to m.data.Size().
