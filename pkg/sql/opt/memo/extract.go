90c8fee27b0d5a6c07a485b95fe0fbb1fe5a854a
Drew Kimball <drewk@cockroachlabs.com>
2024-11-13 10:36:57 +0000

    opt: implement TCO for subqueries
    This commit adds tail-call optimization support for the nested routines
    that are built for subqueries within the body of a routine. This allows
    for nested PL/pgSQL sub-routines to execute within the same context
    (without growing the call-stack) even when the nested sub-routine call
    is within a subquery. This significantly improves performance for PL/pgSQL
    loops.
    
    Fixes #135063
    
    Release note (performance improvement): Performance for some PL/pgSQL loops
    is now significantly improved, by as much as 3-4x. This is due to applying
    tail-call optimization in more cases to the recursive sub-routines that
    implement loops.

--- pkg/sql/opt/memo/extract.go
+++ pkg/sql/opt/memo/extract.go
@@ -493,7 +493,7 @@ func ExtractValueForConstColumn(
 // NOTE: ExtractTailCalls does not take into account whether the calling routine
 // has an exception handler. The execution engine must take this into account
 // before applying tail-call optimization.
-func ExtractTailCalls(expr opt.Expr, tailCalls map[*UDFCallExpr]struct{}) {
+func ExtractTailCalls(expr opt.Expr, tailCalls map[opt.ScalarExpr]struct{}) {
 	switch t := expr.(type) {
 	case *ProjectExpr:
 		// * The cardinality cannot be greater than one: Otherwise, a nested routine
@@ -525,16 +525,6 @@ func ExtractTailCalls(expr opt.Expr, tailCalls map[*UDFCallExpr]struct{}) {
 			ExtractTailCalls(t.Rows[0].(*TupleExpr).Elems[0], tailCalls)
 		}
 
-	case *SubqueryExpr:
-		// A subquery within a routine is lazily evaluated and passes through a
-		// single input row. Similar to Project, we require that the input have only
-		// one row and one column, since otherwise work may happen after the nested
-		// routine evaluates.
-		if t.Input.Relational().Cardinality.IsZeroOrOne() &&
-			t.Input.Relational().OutputCols.Len() == 1 {
-			ExtractTailCalls(t.Input, tailCalls)
-		}
-
 	case *CaseExpr:
 		// Case expressions guarantee that exactly one branch is evaluated, and pass
 		// through the result of the chosen branch. Therefore, a routine within a
@@ -543,6 +533,11 @@ func ExtractTailCalls(expr opt.Expr, tailCalls map[*UDFCallExpr]struct{}) {
 			ExtractTailCalls(t.Whens[i].(*WhenExpr).Value, tailCalls)
 		}
 		ExtractTailCalls(t.OrElse, tailCalls)
+
+	case *SubqueryExpr:
+		// A subquery within a routine is lazily evaluated as a nested routine.
+		// Therefore, we consider it a tail call.
+		tailCalls[t] = struct{}{}
 
 	case *UDFCallExpr:
 		// If we reached a scalar UDFCall expression, it is a tail call.
