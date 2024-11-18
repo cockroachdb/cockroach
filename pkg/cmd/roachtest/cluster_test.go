a86dbcea4332d3417d1a56b9a7611b2acd49fdb9
DarrylWong <darryl@cockroachlabs.com>
2024-11-18 11:38:17 -0500

    roachtest: use full cockroach binary for cockroach-ea
    Previously we compiled cockroach-ea for nightlies using
    cockroach-short mainly due to historical reasons. There
    was no good way to specify a cockroach-ea binary so we
    used cockroach-short as a placeholder to mean assertions on.
    
    However, we have since reworked the framework and this is now
    no longer an issue. This change switches cockroach-ea to be
    compiled with the full cockroach binary instead, as we now
    have roachtests that require the ui.

--- pkg/cmd/roachtest/cluster_test.go
+++ pkg/cmd/roachtest/cluster_test.go
@@ -79,7 +79,7 @@ func (t testWrapper) StandardCockroach() string {
 }
 
 func (t testWrapper) RuntimeAssertionsCockroach() string {
-	return "./dummy-path/to/cockroach-short"
+	return "./dummy-path/to/cockroach-ea"
 }
 
 func (t testWrapper) DeprecatedWorkload() string {
