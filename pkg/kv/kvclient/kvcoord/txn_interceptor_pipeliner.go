20be3fd31bd9f52de414e5ebcadc4845a9f5a6dc
Andrew Baptist <baptist@cockroachlabs.com>
2024-11-21 16:16:15 -0500

    kvserver: add setting to reject overly large transactions
    Previously we had an ability to reject transactions that required too
    much memory, however this was tied together with the condense limit. The
    limits have too different purposes and should be set independently. The
    condense size limit (kv.transaction.max_intents_bytes) is used to
    protect the memory on the client side by using less precise tracking
    once it passes a certain size. The new limit
    `kv.transaction.max_intents_and_locks` is intended to protect the server
    side by preventing a client from creating a transaction with too many
    operations in it. Large transactions with many intents can have a
    negative impact especially in a multi-tenant system.
    
    Epic: none
    
    Fixes: #135841
    
    Release note (ops change): Adds a new configurable parameter
    kv.transaction.max_intents_and_locks which will prevent
    transactions from creating too many intents.

--- pkg/kv/kvclient/kvcoord/txn_interceptor_pipeliner.go
+++ pkg/kv/kvclient/kvcoord/txn_interceptor_pipeliner.go
@@ -111,6 +111,16 @@ var rejectTxnOverTrackedWritesBudget = settings.RegisterBoolSetting(
 	false,
 	settings.WithPublic)
 
+// rejectTxnMaxCount will reject transactions if the number of inserts or locks
+// exceeds this value. It is preferable to use this setting instead of
+// kv.transaction.reject_over_max_intents_budget.enabled.
+var rejectTxnMaxCount = settings.RegisterIntSetting(
+	settings.ApplicationLevel,
+	"kv.transaction.max_intents_and_locks",
+	"maximum count of inserts or durable locks for a single transactions, 0 to disable",
+	0,
+	settings.WithPublic)
+
 // txnPipeliner is a txnInterceptor that pipelines transactional writes by using
 // asynchronous consensus. The interceptor then tracks all writes that have been
 // asynchronously proposed through Raft and ensures that all interfering
@@ -297,7 +307,7 @@ func (tp *txnPipeliner) SendLocked(
 		return nil, pErr
 	}
 
-	// If we're configured to reject txns over budget, we pre-emptively check
+	// If we're configured to reject txns over budget, we preemptively check
 	// whether this current batch is likely to push us over the edge and, if it
 	// does, we reject it. Note that this check is not precise because generally
 	// we can't know exactly the size of the locks that will be taken by a
@@ -305,10 +315,9 @@ func (tp *txnPipeliner) SendLocked(
 	// budget.
 	rejectOverBudget := rejectTxnOverTrackedWritesBudget.Get(&tp.st.SV)
 	maxBytes := TrackedWritesMaxSize.Get(&tp.st.SV)
-	if rejectOverBudget {
-		if err := tp.maybeRejectOverBudget(ba, maxBytes); err != nil {
-			return nil, kvpb.NewError(err)
-		}
+	rejectTxnMaxCount := rejectTxnMaxCount.Get(&tp.st.SV)
+	if err := tp.maybeRejectOverBudget(ba, maxBytes, rejectOverBudget, rejectTxnMaxCount); err != nil {
+		return nil, kvpb.NewError(err)
 	}
 
 	ba.AsyncConsensus = tp.canUseAsyncConsensus(ctx, ba)
@@ -355,7 +364,9 @@ func (tp *txnPipeliner) SendLocked(
 // the transaction commits. If it fails, then we'd add the lock spans to our
 // tracking and exceed the budget. It's easier for this code and more
 // predictable for the user if we just reject this batch, though.
-func (tp *txnPipeliner) maybeRejectOverBudget(ba *kvpb.BatchRequest, maxBytes int64) error {
+func (tp *txnPipeliner) maybeRejectOverBudget(
+	ba *kvpb.BatchRequest, maxBytes int64, rejectIfWouldCondense bool, rejectTxnMaxCount int64,
+) error {
 	// Bail early if the current request is not locking, even if we are already
 	// over budget. In particular, we definitely want to permit rollbacks. We also
 	// want to permit lone commits, since the damage in taking too much memory has
@@ -377,9 +388,18 @@ func (tp *txnPipeliner) maybeRejectOverBudget(ba *kvpb.BatchRequest, maxBytes in
 	locksBudget := maxBytes - tp.ifWrites.byteSize()
 
 	estimate := tp.lockFootprint.estimateSize(spans, locksBudget)
-	if estimate > locksBudget {
+	if rejectIfWouldCondense && estimate > locksBudget {
 		tp.txnMetrics.TxnsRejectedByLockSpanBudget.Inc(1)
 		bErr := newLockSpansOverBudgetError(estimate+tp.ifWrites.byteSize(), maxBytes, ba)
+		return pgerror.WithCandidateCode(bErr, pgcode.ConfigurationLimitExceeded)
+	}
+
+	totalCount := tp.lockFootprint.insertCount + int64(tp.ifWrites.len())
+	// TODO(baptist): We use the same error message as the one above, to avoid
+	// adding additional encoding and decoding for a backport. We could consider
+	// splitting this error message in the future.
+	if rejectTxnMaxCount > 0 && totalCount > rejectTxnMaxCount {
+		bErr := newLockSpansOverBudgetError(tp.lockFootprint.insertCount, rejectTxnMaxCount, ba)
 		return pgerror.WithCandidateCode(bErr, pgcode.ConfigurationLimitExceeded)
 	}
 	return nil
