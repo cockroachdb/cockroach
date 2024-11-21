7293fd042df858531f8a27540df477cc60bada2f
Annie Pompa <annie@cockroachlabs.com>
2024-11-21 11:39:48 -0500

    roachtest: ignore flaky activerecord tests
    Fixes: #134811
    
    Release note: None

--- pkg/cmd/roachtest/tests/activerecord_blocklist.go
+++ pkg/cmd/roachtest/tests/activerecord_blocklist.go
@@ -44,5 +44,8 @@ var activeRecordBlocklist = blocklist{
 }
 
 var activeRecordIgnoreList = blocklist{
-	`ActiveRecord::ConnectionAdapters::PostgreSQLAdapterTest#test_translate_no_connection_exception_to_not_established`: "pg_terminate_backend not implemented",
+	`ActiveRecord::ConnectionAdapters::PostgreSQLAdapterTest#test_translate_no_connection_exception_to_not_established`:                                        "pg_terminate_backend not implemented",
+	`TestAutosaveAssociationOnAHasAndBelongsToManyAssociation#test_should_not_save_and_return_false_if_a_callback_cancelled_saving_in_either_create_or_update`: "flaky",
+	`TestAutosaveAssociationOnAHasAndBelongsToManyAssociation#test_should_not_update_children_when_parent_creation_with_no_reason`:                             "flaky",
+	`TestAutosaveAssociationOnAHasAndBelongsToManyAssociation#test_should_update_children_when_autosave_is_true_and_parent_is_new_but_child_is_not`:            "flaky",
 }
