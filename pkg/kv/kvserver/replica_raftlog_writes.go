26f9feef8745c61ca182359e9dc05ba578f24445
Pavel Kalinnikov <pavel@cockroachlabs.com>
2024-11-29 15:10:39 +0000

    kvserver: factor out log append lifecycle
    Epic: none
    Release note: none

---
+++ pkg/kv/kvserver/replica_raftlog_writes.go
@@ -0,0 +1,39 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package kvserver
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/logstore"
+)
+
+// stateRaftMuLocked returns the current raft log storage state.
+func (r *replicaLogStorage) stateRaftMuLocked() logstore.RaftState {
+	return logstore.RaftState{
+		LastIndex: r.shMu.lastIndexNotDurable,
+		LastTerm:  r.shMu.lastTermNotDurable,
+		ByteSize:  r.shMu.raftLogSize,
+	}
+}
+
+// appendRaftMuLocked carries out a raft log append, and returns the new raft
+// log storage state.
+func (r *replicaLogStorage) appendRaftMuLocked(
+	ctx context.Context, app logstore.MsgStorageAppend, stats *logstore.AppendStats,
+) (logstore.RaftState, error) {
+	state := r.stateRaftMuLocked()
+	cb := (*replicaSyncCallback)(r)
+	return r.raftMu.logStorage.StoreEntries(ctx, state, app, cb, stats)
+}
+
+// updateStateRaftMuLockedMuLocked updates the in-memory reflection of the raft
+// log storage state.
+func (r *replicaLogStorage) updateStateRaftMuLockedMuLocked(state logstore.RaftState) {
+	r.shMu.lastIndexNotDurable = state.LastIndex
+	r.shMu.lastTermNotDurable = state.LastTerm
+	r.shMu.raftLogSize = state.ByteSize
+}
