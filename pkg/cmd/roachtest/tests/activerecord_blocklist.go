8387f34333d7caf64e600f3301d2f58110bcb60c
Matt Spilchen <matt.spilchen@cockroachlabs.com>
2024-11-29 10:57:13 -0400

    roachtest: update activerecord blocklist
    This refreshes the blocklist for the activerecord test. A bunch of tests
    that failed previously now are passing.
    
    I had a clean run with this update to roachtest:
    ```
    2024/11/29 14:31:43 orm_helpers.go:191: Tests run on Cockroach v25.1.0-alpha.00000000-dev-27657e4e0c880225f83ff2393857aae727c917fe
    Tests run against activerecord 7.2.1
    8487 Total Tests Run
    8486 tests passed
    1 test failed
    60 tests skipped
    5 tests ignored
    0 tests passed unexpectedly
    0 tests failed unexpectedly
    0 tests expected failed but skipped
    0 tests expected failed but not run
    ```
    
    Epic: None
    Closes #136066
    Release note: none

--- pkg/cmd/roachtest/tests/activerecord_blocklist.go
+++ pkg/cmd/roachtest/tests/activerecord_blocklist.go
@@ -18,29 +18,7 @@ package tests
 // in the test log.
 
 var activeRecordBlocklist = blocklist{
-	`ActiveRecord::CockroachDB::Migration::PGChangeSchemaTest#test_change_string_to_date`:                                               "unknown",
-	`ActiveRecord::CockroachDB::Migration::PGChangeSchemaTest#test_change_type_with_array`:                                              "unknown",
-	`ActiveRecord::CockroachDB::Migration::PGChangeSchemaTest#test_change_type_with_symbol`:                                             "unknown",
-	`ActiveRecord::CockroachDB::Migration::PGChangeSchemaTest#test_change_type_with_symbol_using_datetime`:                              "unknown",
-	`ActiveRecord::CockroachDB::Migration::PGChangeSchemaTest#test_change_type_with_symbol_using_datetime_with_timestamptz_as_default`:  "unknown",
-	`ActiveRecord::CockroachDB::Migration::PGChangeSchemaTest#test_change_type_with_symbol_using_timestamp_with_timestamptz_as_default`: "unknown",
-	`ActiveRecord::CockroachDB::Migration::PGChangeSchemaTest#test_change_type_with_symbol_with_timestamptz`:                            "unknown",
-	`ActiveRecord::CockroachDB::Migration::PGChangeSchemaTest#test_change_type_with_symbol_with_timestamptz_as_default`:                 "unknown",
-	`CompatibilityTest4_2#test_datetime_doesnt_set_precision_on_change_column`:                                                          "unknown",
-	`CompatibilityTest4_2#test_options_are_not_validated`:                                                                               "unknown",
-	`CompatibilityTest5_0#test_datetime_doesnt_set_precision_on_change_column`:                                                          "unknown",
-	`CompatibilityTest5_0#test_options_are_not_validated`:                                                                               "unknown",
-	`CompatibilityTest5_1#test_datetime_doesnt_set_precision_on_change_column`:                                                          "unknown",
-	`CompatibilityTest5_1#test_options_are_not_validated`:                                                                               "unknown",
-	`CompatibilityTest5_2#test_datetime_doesnt_set_precision_on_change_column`:                                                          "unknown",
-	`CompatibilityTest5_2#test_options_are_not_validated`:                                                                               "unknown",
-	`CompatibilityTest6_0#test_datetime_doesnt_set_precision_on_change_column`:                                                          "unknown",
-	`CompatibilityTest6_0#test_options_are_not_validated`:                                                                               "unknown",
-	`CompatibilityTest6_1#test_datetime_doesnt_set_precision_on_change_column`:                                                          "unknown",
-	`CompatibilityTest6_1#test_options_are_not_validated`:                                                                               "unknown",
-	`CompatibilityTest7_0#test_datetime_sets_precision_6_on_change_column`:                                                              "unknown",
-	`CompatibilityTest7_0#test_options_are_not_validated`:                                                                               "unknown",
-	`PostGISTest#test_point_to_json`:                                                                                                    "unknown",
+	`PostGISTest#test_point_to_json`: "unknown",
 }
 
 var activeRecordIgnoreList = blocklist{
