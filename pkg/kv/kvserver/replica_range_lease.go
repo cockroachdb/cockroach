3904daa7feeb1b9d537337daec3ade121c76171e
Arul Ajmani <arulajmani@gmail.com>
2024-12-02 12:24:50 -0500

    kvserver: fix OverrideLeaderLeaseMetamorphism
    We were unintentionally overriding metamorphism for using expiration
    based leases.
    
    Epic: none
    
    Release note: None

--- pkg/kv/kvserver/replica_range_lease.go
+++ pkg/kv/kvserver/replica_range_lease.go
@@ -180,9 +180,9 @@ func OverrideDefaultLeaseType(ctx context.Context, sv *settings.Values, typ roac
 
 // OverrideLeaderLeaseMetamorphism overrides the default lease type to be
 // epoch based leases, regardless of whether leader leases were metamorphically
-// enabled or not. Any
+// enabled or not.
 func OverrideLeaderLeaseMetamorphism(ctx context.Context, sv *settings.Values) {
-	OverrideDefaultLeaseType(ctx, sv, roachpb.LeaseEpoch)
+	RaftLeaderFortificationFractionEnabled.Override(ctx, sv, 0.0)
 }
 
 // leaseRequestHandle is a handle to an asynchronous lease request.
