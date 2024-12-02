542dc6dd7cb7d1fcd043a34782a7fa0031c57d33
Austen McClernon <austen@cockroachlabs.com>
2024-12-02 14:15:34 -0500

    kvflowcontrol: add force flush index to inspectz (range) handle
    This commit introduces a new field on `kvflowinspectpb.Handle`,
    `ForceFlushIndex`.
    
    `ForceFlushIndex` is an index up to (and including) which the range
    controller running in pull mode must force-flush all send streams.
    
    This is intended for use in live debugging and asserting on state in
    testing.
    
    Part of: #132614
    Release note: None

--- pkg/kv/kvserver/kvflowcontrol/rac2/range_controller.go
+++ pkg/kv/kvserver/kvflowcontrol/rac2/range_controller.go
@@ -1609,6 +1609,7 @@ func (rc *rangeController) InspectRaftMuLocked(ctx context.Context) kvflowinspec
 	return kvflowinspectpb.Handle{
 		RangeID:          rc.opts.RangeID,
 		ConnectedStreams: streams,
+		ForceFlushIndex:  roachpb.ForceFlushIndex{rc.forceFlushIndex},
 	}
 }
 
