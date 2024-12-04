643b49e44ca5575f05e5b34865c74ec4a3b5a629
Austen McClernon <austen@cockroachlabs.com>
2024-12-04 17:09:54 -0500

    kvserver: add TestFlowControlSendQueueRangeSplitMergeMixedVersion
    Add a new flow control integration test, described by this comment:
    
    ```sql
    -- This test is running with kvadmission.flow_control.mode="apply_to_elastic"
    -- and the cluster version set to the previous version. We will exhaust the
    -- elastic tokens towards s3, using a single 2 MiB write. Then, we will
    -- split and merge the range, expecting no send queue to form for any stream,
    -- as apply_to_elastic is the flow control mode. We also expect that the split
    -- and merge will proceed without issue. Note that admission is currently
    -- blocked on n3(s3).
    ```
    
    Part of: #132614
    Release note: None

--- pkg/kv/kvserver/flow_control_integration_test.go
+++ pkg/kv/kvserver/flow_control_integration_test.go
@@ -42,6 +42,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
+	"github.com/cockroachdb/cockroach/pkg/util/tracing"
 	"github.com/cockroachdb/errors"
 	"github.com/dustin/go-humanize"
 	"github.com/olekukonko/tablewriter"
@@ -5461,6 +5462,215 @@ func TestFlowControlSendQueueRangeRelocate(t *testing.T) {
 	})
 }
 
+// TestFlowControlRangeSplitMergeMixedVersion attempts a split and merge while
+// the elastic tokens are exhausted to one store and the flow control mode is
+// apply_to_elastic. See the longer SQL comment for a detailed view of the
+// steps taken by the test.
+func TestFlowControlRangeSplitMergeMixedVersion(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	ctx := context.Background()
+	const numNodes = 3
+	prevVKey := clusterversion.PreviousRelease
+	latestVKey := clusterversion.Latest
+	settings := cluster.MakeTestingClusterSettingsWithVersions(
+		latestVKey.Version(), prevVKey.Version(), false)
+	var disableWorkQueueGranting atomic.Bool
+	disableWorkQueueGranting.Store(true)
+	// We want to exhaust the elastic tokens, but not the regular tokens.
+	kvflowcontrol.ElasticTokensPerStream.Override(ctx, &settings.SV, 2<<20)
+
+	disableWorkQueueGrantingServers := make([]atomic.Bool, numNodes)
+	setTokenReturnEnabled := func(enabled bool, serverIdxs ...int) {
+		for _, serverIdx := range serverIdxs {
+			disableWorkQueueGrantingServers[serverIdx].Store(!enabled)
+		}
+	}
+
+	argsPerServer := make(map[int]base.TestServerArgs)
+	for i := range disableWorkQueueGrantingServers {
+		disableWorkQueueGrantingServers[i].Store(true)
+		argsPerServer[i] = base.TestServerArgs{
+			Settings: settings,
+			Knobs: base.TestingKnobs{
+				Server: &server.TestingKnobs{
+					ClusterVersionOverride:         prevVKey.Version(),
+					DisableAutomaticVersionUpgrade: make(chan struct{}),
+				},
+				Store: &kvserver.StoreTestingKnobs{
+					FlowControlTestingKnobs: &kvflowcontrol.TestingKnobs{
+						UseOnlyForScratchRanges: true,
+						OverrideTokenDeduction: func(tokens kvflowcontrol.Tokens) kvflowcontrol.Tokens {
+							return kvflowcontrol.Tokens(2 << 20)
+						},
+						OverrideAlwaysRefreshSendStreamStats: true,
+					},
+				},
+				AdmissionControl: &admission.TestingKnobs{
+					DisableWorkQueueFastPath: true,
+					DisableWorkQueueGranting: func() bool {
+						idx := i
+						return disableWorkQueueGrantingServers[idx].Load()
+					},
+				},
+			},
+		}
+	}
+
+	tc := testcluster.StartTestCluster(t, numNodes, base.TestClusterArgs{
+		ReplicationMode:   base.ReplicationManual,
+		ServerArgsPerNode: argsPerServer,
+	})
+	defer tc.Stopper().Stop(ctx)
+
+	k := tc.ScratchRange(t)
+	tc.AddVotersOrFatal(t, k, tc.Targets(1, 2)...)
+
+	h := newFlowControlTestHelper(
+		t, tc, "flow_control_integration_v2", /* testdata */
+		kvflowcontrol.V2EnabledWhenLeaderV2Encoding, true, /* isStatic */
+	)
+
+	const mode = kvflowcontrol.ApplyToElastic
+	h.init(mode)
+	defer h.close("range_split_merge_mixed_version")
+
+	desc, err := tc.LookupRange(k)
+	require.NoError(t, err)
+	h.enableVerboseRaftMsgLoggingForRange(desc.RangeID)
+	h.enableVerboseRaftMsgLoggingForRange(desc.RangeID + 1)
+	n1 := sqlutils.MakeSQLRunner(tc.ServerConn(0))
+	h.waitForConnectedStreams(ctx, desc.RangeID, 3, 0 /* serverIdx */)
+	h.resetV2TokenMetrics(ctx)
+
+	h.log(fmt.Sprintf("cluster_version=%v(%+v) latest_version=%v(%+v)",
+		prevVKey.Version(), prevVKey, latestVKey.Version(), latestVKey))
+	setTokenReturnEnabled(true /* enabled */, 0, 1)
+	h.comment(`
+-- This test is running with kvadmission.flow_control.mode="apply_to_elastic"
+-- and the cluster version set to the previous version. We will exhaust the
+-- elastic tokens towards s3, using a single 2 MiB write. Then, we will
+-- split and merge the range, expecting no send queue to form for any stream,
+-- as apply_to_elastic is the flow control mode. We also expect that the split
+-- and merge will proceed without issue. Note that admission is currently 
+-- blocked on n3(s3).
+`)
+
+	h.put(ctx, roachpb.Key(desc.StartKey), 1, admissionpb.BulkNormalPri)
+	h.comment(`(Sent 2 MiB BulkNormalPri put request to pre-split range)`)
+
+	traceCtxPre, recPre := tracing.ContextWithRecordingSpan(ctx,
+		tc.GetFirstStoreFromServer(t, 0 /* server */).GetStoreConfig().Tracer(), "pre-split")
+	cancelPre, chPre := h.putAsync(traceCtxPre, roachpb.Key(desc.StartKey), 1, admissionpb.BulkNormalPri)
+	h.comment(`(Sent 2 MiB BulkNormalPri put request to pre-split range)`)
+
+	h.comment(`-- (Splitting range.)`)
+	left, right := tc.SplitRangeOrFatal(t, k.Next())
+	h.waitForConnectedStreams(ctx, left.RangeID, 3, 0 /* serverIdx */)
+	h.waitForConnectedStreams(ctx, right.RangeID, 3, 0 /* serverIdx */)
+
+	h.comment(`-- Observe the newly split off replica, with its own three streams.`)
+	h.query(n1, `
+  SELECT range_id, count(*) AS streams
+    FROM crdb_internal.kv_flow_control_handles_v2
+GROUP BY (range_id)
+ORDER BY streams DESC;
+`, "range_id", "stream_count")
+
+	traceCtxLeft, recLeft := tracing.ContextWithRecordingSpan(ctx,
+		tc.GetFirstStoreFromServer(t, 0 /* server */).GetStoreConfig().Tracer(), "lhs")
+	cancelLeft, chLeft := h.putAsync(traceCtxLeft, roachpb.Key(left.StartKey), 1, admissionpb.BulkNormalPri)
+	h.comment(`(Sent 2 MiB BulkNormalPri put request to post-split LHS range)`)
+	traceCtxRight, recRight := tracing.ContextWithRecordingSpan(ctx,
+		tc.GetFirstStoreFromServer(t, 0 /* server */).GetStoreConfig().Tracer(), "rhs")
+	cancelRight, chRight := h.putAsync(traceCtxRight, roachpb.Key(right.StartKey), 1, admissionpb.BulkNormalPri)
+	h.comment(`(Sent 2 MiB BulkNormalPri put request to post-split RHS range)`)
+	h.waitForAllTokensReturnedForStreamsV2(ctx, 0, /* serverIdx */
+		testingMkFlowStream(0), testingMkFlowStream(1))
+
+	h.comment(`
+-- Send queue and flow token metrics from n1, post-split and 1 MiB put on
+-- each side.`)
+	h.query(n1, flowSendQueueQueryStr)
+	h.query(n1, flowPerStoreTokenQueryStr, flowPerStoreTokenQueryHeaderStrs...)
+
+	h.comment(`-- (Merging ranges.)`)
+	merged := tc.MergeRangesOrFatal(t, left.StartKey.AsRawKey())
+	h.waitForConnectedStreams(ctx, merged.RangeID, 3, 0 /* serverIdx */)
+	h.waitForSendQueueSize(ctx, merged.RangeID, 0 /* expSize 0 MiB */, 0 /* serverIdx */)
+
+	h.comment(`
+-- Send queue and flow token metrics from n1, post-split-merge.
+-- We expect to not see a force flush of the send queue for s3 again.`)
+	h.query(n1, flowSendQueueQueryStr)
+	h.query(n1, flowPerStoreTokenQueryStr, flowPerStoreTokenQueryHeaderStrs...)
+
+	traceCtxMerge, recMerge := tracing.ContextWithRecordingSpan(ctx,
+		tc.GetFirstStoreFromServer(t, 0 /* server */).GetStoreConfig().Tracer(), "merge")
+	cancelMerge, chMerge := h.putAsync(traceCtxMerge, roachpb.Key(merged.StartKey), 1, admissionpb.BulkNormalPri)
+	h.comment(`(Sent 2 MiB BulkNormalPri put request to post-split-merge range)`)
+	h.waitForSendQueueSize(ctx, merged.RangeID, 0 /* expSize 0 MiB */, 0 /* serverIdx */)
+
+	h.comment(`
+-- Send queue and flow token metrics from n1, post-split-merge and 2 MiB put.
+-- We do not expect to see the send queue develop for s3.`)
+	h.query(n1, flowSendQueueQueryStr)
+	h.query(n1, flowPerStoreTokenQueryStr, flowPerStoreTokenQueryHeaderStrs...)
+
+	h.comment(`
+-- Allow admission to proceed on all nodes and wait for all tokens to be
+-- returned.`)
+	setTokenReturnEnabled(true /* enabled */, 0, 1, 2)
+	h.waitForAllTokensReturned(ctx, 3 /* expStreamCount */, 0 /* serverIdx */)
+
+	select {
+	// The pre-split put should have returned after enabling admission.
+	case res := <-chPre:
+		h.comment(fmt.Sprintf("pre-split response br=%v pErr=%v", res.BatchResponse, res.Error))
+	default:
+		cancelPre()
+		t.Fatalf("expected pre-split put to return after enabling admission, trace: %s", recPre())
+	}
+	select {
+	// Likewise for the post-split LHS put.
+	case res := <-chLeft:
+		h.comment(fmt.Sprintf("post-split LHS response br=%v pErr=%v", res.BatchResponse, res.Error))
+	default:
+		cancelLeft()
+		t.Fatalf("expected LHS put to return after enabling admission, trace: %s", recLeft())
+	}
+	select {
+	// And for the post-split RHS put. Note that the RHS put will return to the
+	// dist sender after the RHS range controller is closed (merge). After this,
+	// the dist sender will exhaust the other replicas which have also been
+	// removed from the range (n2,n3), before refreshing the range cache and
+	// redirecting the request to the merged range which succeeds.
+	case res := <-chRight:
+		h.comment(fmt.Sprintf("post-split RHS response br=%v pErr=%v", res.BatchResponse, res.Error))
+	default:
+		cancelRight()
+		t.Fatalf("expected RHS put to return after enabling admission, trace: %s", recRight())
+	}
+	select {
+	// And for the post-split-merge put.
+	case res := <-chMerge:
+		h.comment(fmt.Sprintf("post-merge response br=%v pErr=%v", res.BatchResponse, res.Error))
+	default:
+		cancelMerge()
+		t.Fatalf("expected merge put to return after enabling admission, trace: %s", recMerge())
+	}
+
+	h.comment(`-- Flow token metrics from n1, all tokens should be returned.`)
+	h.query(n1, v2FlowTokensQueryStr)
+
+	// Ensure all the tracing spans are closed.
+	_ = recPre()
+	_ = recLeft()
+	_ = recRight()
+	_ = recMerge()
+}
+
 func TestFlowControlSendQueueRangeMigrate(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
@@ -6133,6 +6343,46 @@ func (h *flowControlTestHelper) query(runner *sqlutils.SQLRunner, sql string, he
 	tbl.SetHeader(headers)
 	tbl.SetAutoFormatHeaders(false)
 	tbl.Render()
+}
+
+type testingSendResult struct {
+	serverIdx int
+	*kvpb.Error
+	*kvpb.BatchResponse
+}
+
+// putAsync issues a put request for the given key at the priority specified,
+// against the first server in the cluster. Unlike put, this function does not
+// wait for the request to be processed. It returns a cancel function and a
+// channel to receive the result of the request(s).
+func (h *flowControlTestHelper) putAsync(
+	ctx context.Context, key roachpb.Key, size int, pri admissionpb.WorkPriority, serverIdxs ...int,
+) (context.CancelFunc, chan testingSendResult) {
+	if len(serverIdxs) == 0 {
+		// Default to the first server if none are given.
+		serverIdxs = []int{0}
+	}
+	// Generate the values now, so that we don't race on the rand source inside
+	// the function below.
+	values := make([]roachpb.Value, len(serverIdxs))
+	for i := range serverIdxs {
+		values[i] = roachpb.MakeValueFromString(randutil.RandString(h.rng, size, randutil.PrintableKeyAlphabet))
+	}
+	resCh := make(chan testingSendResult, len(serverIdxs))
+	cancelCtx, cancel := context.WithCancel(ctx)
+	for _, serverIdx := range serverIdxs {
+		require.NoError(h.t, h.tc.Server(serverIdx).Stopper().RunAsyncTask(cancelCtx,
+			fmt.Sprintf("put-async-idx-%d", serverIdx), func(ctx context.Context) {
+				value := values[serverIdx]
+				ba := &kvpb.BatchRequest{}
+				ba.Add(kvpb.NewPut(key, value))
+				ba.AdmissionHeader.Priority = int32(pri)
+				ba.AdmissionHeader.Source = kvpb.AdmissionHeader_FROM_SQL
+				br, pErr := h.tc.Server(serverIdx).DB().NonTransactionalSender().Send(cancelCtx, ba)
+				resCh <- testingSendResult{serverIdx: serverIdx, Error: pErr, BatchResponse: br}
+			}))
+	}
+	return cancel, resCh
 }
 
 // put issues a put request for the given key at the priority specified,
