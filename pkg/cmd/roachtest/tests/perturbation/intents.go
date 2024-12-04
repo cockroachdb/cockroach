02f657121c9e1e62ea91d7ee7e08a2031a4f7faf
Andrew Baptist <baptist@cockroachlabs.com>
2024-12-04 09:48:16 -0500

    roachtest: rename maxBlockBytes to blockSize for perturbation/* tests
    This commit changes the perturbation framework to rename maxBlockBytes
    to blockSize to make it more consistent in logging and usage. We also
    use the size as both a min and max size in the kv workload.
    
    Epic: none
    
    Release note: None

--- pkg/cmd/roachtest/tests/perturbation/intents.go
+++ pkg/cmd/roachtest/tests/perturbation/intents.go
@@ -62,7 +62,7 @@ func (intents) startPerturbation(ctx context.Context, t test.Test, v variations)
 	i := 0
 	for ; timeutil.Since(startTime) < v.perturbationDuration; i++ {
 		startKey := int64(rng.Uint64())
-		bytes := make([]byte, v.maxBlockBytes)
+		bytes := make([]byte, v.blockSize)
 		for b := range bytes {
 			bytes[b] = byte(rng.Int() & 0xff)
 		}
