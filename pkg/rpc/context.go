dc9b1f906ba99cd804a42355c9d429972d3e613f
Tobias Grieger <tobias.b.grieger@gmail.com>
2024-12-06 23:58:50 +0100

    set ReaderOptions.MaximumBufferSize
    Couldn't `sysbench prepare`, it'd always get stuck on the index addition
    jobs. Saw message like these:
    
     E241206 22:39:10.777982 1418 rpc/context.go:197 ⋮ [-] 174  TBG server error ‹manager closed›: ‹protocol error›: ‹data overflow (len:4259840)›
    
    That's just over 4mb. Realized there is a default value for
    `ReaderOptions.MaximumBufferSize` of 4mb, so these AddSST calls would
    never succeed.
    
    This commit sets that limit to infinity. Will have to check that this
    doesn't cause large memory consumption if the buffer grows once. Will
    it reduce in size again? I think I saw code like this but need to check.
    
    There's also a StreamOptions.SplitSize. It's probably better to keep the
    max read buffer size limited but set a SplitSize of a couple of MBs
    worth. Trying that out is for another commit though, today I just want
    something that works!
    
    This commit also backs out some of the "close on rpc error" logic since
    it was just adding a lot of spam and now that I understand what the
    problem was was misguided. We weren't accidentally keeping broken
    connections in the pool. We were constantly making and breaking
    connections, I think.

--- pkg/rpc/context.go
+++ pkg/rpc/context.go
@@ -49,8 +49,10 @@ import (
 	"google.golang.org/grpc/encoding"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/stats"
+	"storj.io/drpc/drpcmanager"
 	"storj.io/drpc/drpcmux"
 	"storj.io/drpc/drpcserver"
+	"storj.io/drpc/drpcwire"
 )
 
 // NewServer sets up an RPC server. Depending on the ServerOptions, the Server
@@ -193,7 +195,14 @@ func NewServerEx(
 
 	s = grpc.NewServer(grpcOpts...)
 	dmux := drpcmux.New()
-	dsrv := drpcserver.NewWithOptions(dmux, drpcserver.Options{ /* TODO */ })
+	dsrv := drpcserver.NewWithOptions(dmux, drpcserver.Options{
+		Log: func(err error) {
+			log.Warningf(context.Background(), "drpc server error %v", err)
+		},
+		// The reader's max buffer size defaults to 4mb, and if it is exceeded (such
+		// as happens with AddSSTable) the RPCs fail.
+		Manager: drpcmanager.Options{Reader: drpcwire.ReaderOptions{MaximumBufferSize: math.MaxInt}},
+	})
 	RegisterHeartbeatServer(s, rpcCtx.NewHeartbeatService())
 	tlsCfg, err := rpcCtx.GetServerTLSConfig()
 	if err != nil {
