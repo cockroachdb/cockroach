abecd478c299f1c878c48f56cc965e8f462ae703
Rafi Shamim <rafi@cockroachlabs.com>
2024-12-06 12:18:36 -0500

    sql: do not redact names when formatting statement for logs
    Previously, only the TELEMETRY log channel would use the
    FmtOmitNameRedaction flag when logging SQL statements. Now all log
    channels will use this flag.
    
    Release note (ops change): Schema object identifiers (e.g. database
    names, schema names, table names, and function names) are no longer
    redacted when logging statements in the EXEC or SQL_SCHEMA channels.
    
    If redaction of these names is required, then the new cluster setting
    sql.log.redact_names.enabled can be set to true. The default value of
    the setting if false.

--- pkg/sql/schema_change_plan_node.go
+++ pkg/sql/schema_change_plan_node.go
@@ -36,15 +36,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/retry"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
-	"github.com/cockroachdb/redact"
 )
-
-// FormatAstAsRedactableString implements scbuild.AstFormatter
-func (p *planner) FormatAstAsRedactableString(
-	statement tree.Statement, annotations *tree.Annotations,
-) redact.RedactableString {
-	return formatStmtKeyAsRedactableString(statement, annotations, tree.FmtSimple|tree.FmtOmitNameRedaction)
-}
 
 // SchemaChange provides the planNode for the new schema changer.
 func (p *planner) SchemaChange(ctx context.Context, stmt tree.Statement) (planNode, error) {
