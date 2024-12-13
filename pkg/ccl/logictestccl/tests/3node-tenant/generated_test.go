7b23988a264675e40653654a696cc2d236450aa0
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2024-12-13 16:46:56 -0500

    multitenant: add can_prepare_txns tenant capability
    Informs #22329.
    
    This commit adds a new `can_prepare_txns` tenant capability, so that we
    don't allow secondary tenants to prepare transactions by default. Allowing
    an untrusted tenant to prepare transactions would allow it to block the
    progress of system-wide backups, so it is too dangerous to allow by default.
    
    Release note: None

--- pkg/ccl/logictestccl/tests/3node-tenant/generated_test.go
+++ pkg/ccl/logictestccl/tests/3node-tenant/generated_test.go
@@ -2206,13 +2206,6 @@ func TestTenantLogic_tuple(
 	runLogicTest(t, "tuple")
 }
 
-func TestTenantLogic_two_phase_commit(
-	t *testing.T,
-) {
-	defer leaktest.AfterTest(t)()
-	runLogicTest(t, "two_phase_commit")
-}
-
 func TestTenantLogic_txn(
 	t *testing.T,
 ) {
