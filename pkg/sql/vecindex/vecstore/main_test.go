4b65130f905c341e584411951dbd21952c45604f
Matt White <matt.white@cockroachlabs.com>
2024-12-13 14:42:45 -0800

    vecstore: implement partition manipulation functions
    Add persistent storage to vecstore. Implement the methods for partition
    creation, retrieval and deletion. Add a simple test to ensure these
    methods are functioning.

--- pkg/sql/vecindex/vecstore/main_test.go
+++ pkg/sql/vecindex/vecstore/main_test.go
@@ -6,6 +6,10 @@
 package vecstore
 
 import (
+	"github.com/cockroachdb/cockroach/pkg/security/securityassets"
+	"github.com/cockroachdb/cockroach/pkg/security/securitytest"
+	"github.com/cockroachdb/cockroach/pkg/server"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"os"
 	"testing"
 
@@ -15,7 +19,9 @@ import (
 //go:generate ../util/leaktest/add-leaktest.sh *_test.go
 
 func TestMain(m *testing.M) {
+	securityassets.SetLoader(securitytest.EmbeddedAssets)
 	randutil.SeedForTests()
+	serverutils.InitTestServerFactory(server.TestServerFactory)
 
 	os.Exit(m.Run())
 }
