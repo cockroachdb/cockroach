03475714ee785233b4e404ce770c42fac9cf1699
Tobias Grieger <tobias.b.grieger@gmail.com>
2024-12-16 22:55:04 +0100

    kvserverbase: make tracing alloc in hot path conditional

--- pkg/kv/kvserver/replica_raft.go
+++ pkg/kv/kvserver/replica_raft.go
@@ -184,9 +184,11 @@ func (r *Replica) evalAndPropose(
 
 		// Fork the proposal's context span so that the proposal's context
 		// can outlive the original proposer's context.
-		ctx, sp := tracing.ForkSpan(ctx, "async consensus")
-		proposal.ctx.Store(&ctx)
-		proposal.sp = sp
+		if s := tracing.SpanFromContext(ctx); s != nil && !s.IsNoop() {
+			ctx, sp := tracing.ForkSpan(ctx, "async consensus")
+			proposal.ctx.Store(&ctx)
+			proposal.sp = sp
+		}
 		if proposal.sp != nil {
 			// We can't leak this span if we fail to hand the proposal to the
 			// replication layer, so finish it later in this method if we are to
