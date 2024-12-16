ec0bd5062ea0b25a3c2b3081ee499c67673c6ee8
Annie Pompa <annie@cockroachlabs.com>
2024-12-16 16:21:36 -0500

    sql: remove redundant metrics
    Epic: none
    
    Release note (backwards-incompatible change): Several metrics are redundant
    and have been removed. Below is a mapping from removed metrics to
    an existing, identical metric:
    ```
    [REMOVED] sql.schema_changer.running
    ----
    jobs.schema_change.currently_running
    
    [REMOVED] sql.schema_changer.successes
    ----
    jobs.schema_change.resume_completed
    
    [REMOVED] sql.schema_changer.retry_errors
    ----
    jobs.schema_change.resume_retry_error
    
    [REMOVED] sql.schema_changer.permanent_errors
    ----
    jobs.schema_change.resume_failed
    
    ```

--- pkg/sql/schema_changer_metrics.go
+++ pkg/sql/schema_changer_metrics.go
@@ -13,41 +13,10 @@ import (
 
 // TODO(ajwerner): Add many more metrics.
 
-var (
-	metaRunning = metric.Metadata{
-		Name:        "sql.schema_changer.running",
-		Help:        "Gauge of currently running schema changes",
-		Measurement: "Schema changes",
-		Unit:        metric.Unit_COUNT,
-	}
-	metaSuccesses = metric.Metadata{
-		Name:        "sql.schema_changer.successes",
-		Help:        "Counter of the number of schema changer resumes which succeed",
-		Measurement: "Schema changes",
-		Unit:        metric.Unit_COUNT,
-	}
-	metaRetryErrors = metric.Metadata{
-		Name:        "sql.schema_changer.retry_errors",
-		Help:        "Counter of the number of retriable errors experienced by the schema changer",
-		Measurement: "Errors",
-		Unit:        metric.Unit_COUNT,
-	}
-	metaPermanentErrors = metric.Metadata{
-		Name:        "sql.schema_changer.permanent_errors",
-		Help:        "Counter of the number of permanent errors experienced by the schema changer",
-		Measurement: "Errors",
-		Unit:        metric.Unit_COUNT,
-	}
-)
-
 // SchemaChangerMetrics are metrics corresponding to the schema changer.
 type SchemaChangerMetrics struct {
-	RunningSchemaChanges *metric.Gauge
-	Successes            *metric.Counter
-	RetryErrors          *metric.Counter
-	PermanentErrors      *metric.Counter
-	ConstraintErrors     telemetry.Counter
-	UncategorizedErrors  telemetry.Counter
+	ConstraintErrors    telemetry.Counter
+	UncategorizedErrors telemetry.Counter
 }
 
 // MetricStruct makes SchemaChangerMetrics a metric.Struct.
@@ -58,11 +27,7 @@ var _ metric.Struct = (*SchemaChangerMetrics)(nil)
 // NewSchemaChangerMetrics constructs a new SchemaChangerMetrics.
 func NewSchemaChangerMetrics() *SchemaChangerMetrics {
 	return &SchemaChangerMetrics{
-		RunningSchemaChanges: metric.NewGauge(metaRunning),
-		Successes:            metric.NewCounter(metaSuccesses),
-		RetryErrors:          metric.NewCounter(metaRetryErrors),
-		PermanentErrors:      metric.NewCounter(metaPermanentErrors),
-		ConstraintErrors:     sqltelemetry.SchemaChangeErrorCounter("constraint_violation"),
-		UncategorizedErrors:  sqltelemetry.SchemaChangeErrorCounter("uncategorized"),
+		ConstraintErrors:    sqltelemetry.SchemaChangeErrorCounter("constraint_violation"),
+		UncategorizedErrors: sqltelemetry.SchemaChangeErrorCounter("uncategorized"),
 	}
 }
