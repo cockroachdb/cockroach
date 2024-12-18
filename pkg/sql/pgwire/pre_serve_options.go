38519c80d321322d89a18a4b175e99f7764fcbe0
Faizan Qazi <faizan@cockroachlabs.com>
2024-12-18 13:13:00 +0000

    pgwire: rename ReadBuffer.GetString to GetUnsafeString
    Previously, it was not clear that GetString would not copy data from the
    ReadBuffer. This could be problematic if the object was long lived,
    since the entire buffer would have been kept alive. To reduce risk, this
    patch renames GetString to GetUnsafeString. It also adds a GetSafeString
    method for cases where a copy is needed. The latter is adopted inside:
    parseClientProvidedSessionParameters
    
    Informs: #137627
    Release note: None

--- pkg/sql/pgwire/pre_serve_options.go
+++ pkg/sql/pgwire/pre_serve_options.go
@@ -54,7 +54,9 @@ func parseClientProvidedSessionParameters(
 	hasTenantSelectOption := false
 	for {
 		// Read a key-value pair from the client.
-		key, err := buf.GetString()
+		// Note: GetSafeString is used since the key/value will live well past the
+		// life of the message.
+		key, err := buf.GetSafeString()
 		if err != nil {
 			return args, pgerror.Wrap(
 				err, pgcode.ProtocolViolation,
@@ -65,7 +67,7 @@ func parseClientProvidedSessionParameters(
 			// End of parameter list.
 			break
 		}
-		value, err := buf.GetString()
+		value, err := buf.GetSafeString()
 		if err != nil {
 			return args, pgerror.Wrapf(
 				err, pgcode.ProtocolViolation,
