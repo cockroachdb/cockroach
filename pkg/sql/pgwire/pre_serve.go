82fd0dcccc14dd5e1ea323fb3205ebfb18d8823e
souravcrl <sourav.sarangi@cockroachlabs.com>
2024-12-19 16:04:32 +0530

    cli, security: add support for configurable TLS cipher suites
    fixes #136999
    Epic CRDB-45351
    
    Release Note(security,ops): We will be providing a new cockroach start command
    cli flag `tls-cipher-suites` which is a string of comma separated list of cipher
    suites to be used for all incoming TLS connections to the node. For TLS 1.2,
    this should strictly be a subset of suites defined in
    security/tls_ciphersuites.go as RecommendedCipherSuites or OldCipherSuites. For
    TLS 1.3, this should be configured to a subset of ciphers in
    crypto/tls/cipher_suites.go. The flag will restrict TLS connections to the node
    for all 3 types of connection(i.e. sql, rpc, http) where node acts as the server
    for the connections and close non-conforming ones.
    
    The updated cockroach start command with the ciphers argument is as follows:
    ```
    cockroach start --certs-dir=certs --listen-addr=localhost:26257
    --http-addr=localhost:8080
    --tls-cipher-suites=TLS_AES_256_GCM_SHA384,TLS_AES_128_GCM_SHA256
    ```

--- pkg/sql/pgwire/pre_serve.go
+++ pkg/sql/pgwire/pre_serve.go
@@ -16,6 +16,7 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/security"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/hba"
@@ -490,12 +491,20 @@ func (s *PreServeConnHandler) maybeUpgradeToSecureConn(
 			return
 		}
 		newConn = tls.Server(conn, tlsConfig)
+		// Conditionally perform handshake connection and determine additional restrictions.
+		serverErr = security.TLSCipherRestrict(newConn)
+		if serverErr != nil {
+			_ = newConn.Close()
+		}
 		newConnType = hba.ConnHostSSL
 	}
 	s.tenantIndependentMetrics.PreServeBytesOutCount.Inc(int64(n))
 
 	// Finally, re-read the version/command from the client.
 	newVersion, *buf, serverErr = s.readVersion(newConn)
+	if serverErr != nil {
+		return
+	}
 	return
 }
 
