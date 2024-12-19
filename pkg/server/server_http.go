71f71022e6702b9677307de07ff8a4d6a216a2a7
souravcrl <sourav.sarangi@cockroachlabs.com>
2024-12-19 16:04:32 +0530

    cli, security: add support for configurable TLS cipher suites
    fixes #136999
    Epic CRDB-45351
    
    Release Note(security,ops): We will be providing a new cockroach start command
    cli flag `tls-cipher-suites` which is a string of comma separated list of cipher
    suites to be used for all incoming TLS connections to the node. For TLS 1.2,
    this should strictly be a subset of suites defined in
    security/tls_ciphersuites.go as RecommendedCipherSuites or OldCipherSuites. For
    TLS 1.3, this should be configured to a subset of ciphers in
    crypto/tls/cipher_suites.go. The flag will restrict TLS connections to the node
    for all 3 types of connection(i.e. sql, rpc, http) where node acts as the server
    for the connections and close non-conforming ones.
    
    The updated cockroach start command with the ciphers argument is as follows:
    ```
    cockroach start --certs-dir=certs --listen-addr=localhost:26257
    --http-addr=localhost:8080
    --join=localhost:26257,localhost:26258,localhost:26259
    --tls-cipher-suites=TLS_AES_256_GCM_SHA384,TLS_AES_128_GCM_SHA256
    ```

--- pkg/server/server_http.go
+++ pkg/server/server_http.go
@@ -9,13 +9,16 @@ import (
 	"context"
 	"crypto/tls"
 	"encoding/json"
+	"net"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/NYTimes/gziphandler"
 	"github.com/cockroachdb/cmux"
 	"github.com/cockroachdb/cockroach/pkg/inspectz"
 	"github.com/cockroachdb/cockroach/pkg/rpc"
+	"github.com/cockroachdb/cockroach/pkg/security"
 	"github.com/cockroachdb/cockroach/pkg/server/apiconstants"
 	"github.com/cockroachdb/cockroach/pkg/server/authserver"
 	"github.com/cockroachdb/cockroach/pkg/server/debug"
@@ -331,7 +334,7 @@ func startHTTPService(
 			return err
 		}
 
-		httpLn = tls.NewListener(tlsL, uiTLSConfig)
+		httpLn = newTLSRestrictLn(tlsL, uiTLSConfig)
 	}
 
 	// The connManager is responsible for tearing down the net.Conn
@@ -345,6 +348,46 @@ func startHTTPService(
 	return stopper.RunAsyncTask(workersCtx, "server-http", func(context.Context) {
 		netutil.FatalIfUnexpected(connManager.Serve(httpLn))
 	})
+}
+
+type tlsRestrictLn struct {
+	net.Listener
+}
+
+// Accept wraps the net.Listener Accept method to apply http based tls
+// constraints on the incoming connection.
+func (l tlsRestrictLn) Accept() (c net.Conn, err error) {
+	if c, err = l.Listener.Accept(); err == nil {
+		if err = httpTLSRestrictFn(c); err != nil {
+			_ = c.Close()
+			return
+		}
+	}
+	return
+}
+
+func newTLSRestrictLn(ln net.Listener, config *tls.Config) tlsRestrictLn {
+	return tlsRestrictLn{
+		Listener: tls.NewListener(ln, config),
+	}
+}
+
+func httpTLSRestrictFn(conn net.Conn) (err error) {
+	tlsConn, ok := conn.(*tls.Conn)
+	if !ok {
+		return nil
+	}
+
+	if !tlsConn.ConnectionState().HandshakeComplete {
+		// we provide a timebound context for handshake as it ensures client
+		// failures are properly handled.
+		ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+		defer cancel()
+		if err = tlsConn.HandshakeContext(ctx); err != nil {
+			return
+		}
+	}
+	return security.TLSCipherRestrict(conn)
 }
 
 // baseHandler is the top-level HTTP handler for all HTTP traffic, before
