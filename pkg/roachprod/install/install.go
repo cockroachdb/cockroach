a8d331eef412399f047b75a838cf6dd3f79f84d3
Tobias Grieger <tobias.b.grieger@gmail.com>
2024-12-19 09:15:55 +0100

    roachprod: hardcode gce project for side-eye secret
    See
    https://cockroachlabs.slack.com/archives/C06KHP4PU4X/p1734533459160169.
    
    Epic: none

--- pkg/roachprod/install/install.go
+++ pkg/roachprod/install/install.go
@@ -115,7 +115,9 @@ var installLocalCmds = map[string]map[string]*exec.Cmd{
 	},
 }
 
-var sideEyeSecretCmd = exec.Command("gcloud", "secrets", "versions", "access", "latest", "--secret", "side-eye-key")
+var sideEyeSecretCmd = exec.Command("gcloud",
+	"--project", "cockroach-ephemeral",
+	"secrets", "versions", "access", "latest", "--secret", "side-eye-key")
 
 // SortedCmds TODO(peter): document
 func SortedCmds() []string {
