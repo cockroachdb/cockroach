d9bc093dfe9d4afc3d51d7a8b74242bacd6a237f
Jackson Owens <jackson@cockroachlabs.com>
2024-12-30 17:27:21 -0500

    Revert "release-24.3: storage: remove columnar cluster setting"
    This reverts commit a5c782ff86ae55461388f910a56086671c779934.
    
    Epic: CRDB-37584
    Informs #133189.
    Release note: Introduces the `storage.columnar_blocks.enabled` cluster setting
    allowing opting into the columnar block sstable format.

--- pkg/storage/pebble.go
+++ pkg/storage/pebble.go
@@ -102,15 +102,14 @@ var IngestSplitEnabled = settings.RegisterBoolSetting(
 	settings.WithPublic,
 )
 
-// TODO(radu): re-enable this setting when we are confident in the implementation.
 // columnarBlocksEnabled controls whether columnar-blocks are enabled in Pebble.
-//var columnarBlocksEnabled = settings.RegisterBoolSetting(
-//	settings.SystemVisible,
-//	"storage.columnar_blocks.enabled",
-//	"set to true to enable columnar-blocks to store KVs in a columnar format",
-//	false, // TODO(jackson): Metamorphicize this.
-//	settings.WithPublic,
-//)
+var columnarBlocksEnabled = settings.RegisterBoolSetting(
+	settings.SystemVisible,
+	"storage.columnar_blocks.enabled",
+	"set to true to enable columnar-blocks to store KVs in a columnar format",
+	false, // TODO(jackson): Metamorphicize this.
+	settings.WithPublic,
+)
 
 // deleteCompactionsCanExcise controls whether delete compactions can
 // apply rangedels/rangekeydels on sstables they partially apply to, through
@@ -1245,9 +1244,11 @@ func newPebble(ctx context.Context, cfg engineConfig) (p *Pebble, err error) {
 		return IngestSplitEnabled.Get(&cfg.settings.SV)
 	}
 	cfg.opts.Experimental.EnableColumnarBlocks = func() bool {
-		// TODO(radu): re-enable this setting when we are confident in the implementation.
-		// return columnarBlocksEnabled.Get(&cfg.settings.SV)
-		return false
+		// TODO(radu): disable completely for now since the format is not finalized.
+		if true {
+			return false
+		}
+		return columnarBlocksEnabled.Get(&cfg.settings.SV)
 	}
 	cfg.opts.Experimental.EnableDeleteOnlyCompactionExcises = func() bool {
 		return deleteCompactionsCanExcise.Get(&cfg.settings.SV)
