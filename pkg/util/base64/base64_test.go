416ee313ffc300dad19170d4920f45cd5fbd02c4
Marcus Gartner <marcus@cockroachlabs.com>
2025-01-02 17:10:12 -0500

    util/base64: add `Encoder`
    The `base64.Encoder` utility has been added for encoding byte slice
    streams into a base64 string. It is adapted from the standard library's
    base64 encoder returned by `base64.NewEncoder` with some simplifications
    and an API that helps eliminate unnecessary allocations.
    
    Release note: None

---
+++ pkg/util/base64/base64_test.go
@@ -0,0 +1,60 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package base64
+
+import (
+	"bytes"
+	"encoding/base64"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
+)
+
+func TestEncoder(t *testing.T) {
+	const (
+		iters  = 1000
+		maxLen = 1000
+	)
+
+	rng, _ := randutil.NewTestRand()
+
+	var enc Encoder
+	enc.Init(base64.StdEncoding)
+
+	if s := enc.String(); s != "" {
+		t.Errorf("expected empty string, got %q", s)
+	}
+
+	for i := 0; i < iters; i++ {
+		// Generate a random byte slice.
+		b := randutil.RandBytes(rng, rng.Intn(maxLen+1))
+
+		// Write b in randomly sized chunks.
+		rest := b
+		for len(rest) > 0 {
+			chunk := rng.Intn(len(rest)) + 1
+			enc.Write(rest[:chunk])
+			rest = rest[chunk:]
+		}
+
+		// Decode the encoded string.
+		s := enc.String()
+		dec, err := base64.StdEncoding.DecodeString(s)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Check that the decoded string equals the original byte slice.
+		if !bytes.Equal(b, dec) {
+			t.Errorf("failed round-trip encoding/decoding of %v", b)
+		}
+	}
+}
