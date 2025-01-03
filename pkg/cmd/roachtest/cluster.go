39d8b9ea8e63a53f794be34b5887ddf026b11282
Herko Lategan <herko@cockroachlabs.com>
2025-01-03 13:12:44 +0000

    roachtest: health status for given nodes
    Previously, `cluster.HealthStatus` seemed to check all nodes, but rely on the
    count of a nodes list passed in as a parameter.
    
    This change fixes the function to use the passed nodes during the request phase
    as well. It also adds a small retry mechanism during the 15 seconds it checks
    the health of the nodes.
    
    The issue was found when updating the health check to only look at
    `c.CRDBNodes()` and exclude the workload node if one is part of the cluster.
    
    Informs: #118214
    Epic: None

--- pkg/cmd/roachtest/cluster.go
+++ pkg/cmd/roachtest/cluster.go
@@ -16,6 +16,7 @@ import (
 	"log"
 	"math/rand"
 	"net"
+	"net/http"
 	"net/url"
 	"os"
 	"os/exec"
@@ -1513,20 +1514,24 @@ func newHealthStatusResult(node int, status int, body []byte, err error) *Health
 	}
 }
 
-// HealthStatus returns the result of the /health?ready=1 endpoint for each node.
+// HealthStatus returns the result of the /health?ready=1 endpoint for the
+// specified nodes.
 func (c *clusterImpl) HealthStatus(
 	ctx context.Context, l *logger.Logger, nodes option.NodeListOption,
 ) ([]*HealthStatusResult, error) {
-	if len(nodes) < 1 {
+	nodeCount := len(nodes)
+	if nodeCount < 1 {
 		return nil, nil // unit tests
 	}
+
 	adminAddrs, err := c.ExternalAdminUIAddr(ctx, l, nodes)
 	if err != nil {
 		return nil, errors.WithDetail(err, "Unable to get admin UI address(es)")
 	}
 	client := roachtestutil.DefaultHTTPClient(c, l)
-	getStatus := func(ctx context.Context, node int) *HealthStatusResult {
-		url := fmt.Sprintf(`https://%s/health?ready=1`, adminAddrs[node-1])
+	getStatus := func(ctx context.Context, nodeIndex, node int) *HealthStatusResult {
+		url := fmt.Sprintf(`https://%s/health?ready=1`, adminAddrs[nodeIndex])
+
 		resp, err := client.Get(ctx, url)
 		if err != nil {
 			return newHealthStatusResult(node, 0, nil, err)
@@ -1538,16 +1543,26 @@ func (c *clusterImpl) HealthStatus(
 		return newHealthStatusResult(node, resp.StatusCode, body, err)
 	}
 
-	results := make([]*HealthStatusResult, c.spec.NodeCount)
+	results := make([]*HealthStatusResult, nodeCount)
 
 	_ = timeutil.RunWithTimeout(ctx, "health status", 15*time.Second, func(ctx context.Context) error {
 		var wg sync.WaitGroup
-		wg.Add(c.spec.NodeCount)
-		for i := 1; i <= c.spec.NodeCount; i++ {
-			go func(node int) {
+		wg.Add(nodeCount)
+		for i := 0; i < nodeCount; i++ {
+			go func() {
 				defer wg.Done()
-				results[node-1] = getStatus(ctx, node)
-			}(i)
+				for {
+					results[i] = getStatus(ctx, i, nodes[i])
+					if results[i].Err == nil && results[i].Status == http.StatusOK {
+						return
+					}
+					select {
+					case <-ctx.Done():
+						return
+					case <-time.After(3 * time.Second):
+					}
+				}
+			}()
 		}
 		wg.Wait()
 		return nil
