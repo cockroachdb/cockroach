fb23f5fd81289b3b81d5289b9ed17caf46f2e69d
Yahor Yuzefovich <yahor@cockroachlabs.com>
2025-01-06 12:30:36 -0800

    sql: handle DB and schema names with special characters in bundles
    This commit makes it so that we properly handle database and schema
    names that have special characters like periods and single quotes (that
    require double-quoting in some contexts). It is achieved by using `tree.Name`
    type instead of `string` and relying on the `Format` implementation to
    properly escape identifiers or explicitly calling `EscapeSQLString`,
    depending on the context.
    
    Release note: None

--- pkg/cli/interactive_tests/test_sb_recreate.tcl
+++ pkg/cli/interactive_tests/test_sb_recreate.tcl
@@ -9,28 +9,28 @@ proc file_exists {filepath} {
   }
 }
 
-start_test "Ensure that 'debug sb recreate' works with cross-DB references"
+start_test "Ensure that 'debug sb recreate' works with cross-DB references and special characters"
 
 spawn $argv demo --no-line-editor --empty --log-dir=logs
 eexpect "defaultdb>"
 
-send "CREATE DATABASE db_table;\r"
-send "USE db_table;\r"
+send "CREATE DATABASE \"db.table\";\r"
+send "USE \"db.table\";\r"
 send "CREATE TYPE udt AS ENUM ('a', 'b');\r"
-send "CREATE SCHEMA sc_t;\r"
-send "CREATE TABLE sc_t.t (k INT PRIMARY KEY, e udt);\r"
+send "CREATE SCHEMA \"sc.t\";\r"
+send "CREATE TABLE \"sc.t\".t (k INT PRIMARY KEY, e udt);\r"
 
-send "CREATE DATABASE db_view;\r"
-send "USE db_view;\r"
+send "CREATE DATABASE \" db ' view \";\r"
+send "USE \" db ' view \";\r"
 send "CREATE TABLE t (k INT PRIMARY KEY);\r"
-send "CREATE SCHEMA sc_view;\r"
-send "CREATE VIEW sc_view.v AS SELECT * FROM t;\r"
+send "CREATE SCHEMA \"sc ' view\";\r"
+send "CREATE VIEW \"sc ' view\".v AS SELECT * FROM t;\r"
 
 send "RESET database;\r"
 send "CREATE FUNCTION f() RETURNS INT LANGUAGE SQL AS 'SELECT 1';\r"
 eexpect "defaultdb>"
 
-send "EXPLAIN ANALYZE (DEBUG) SELECT *, f() FROM db_table.sc_t.t, db_view.sc_view.v;\r"
+send "EXPLAIN ANALYZE (DEBUG) SELECT *, f() FROM \"db.table\".\"sc.t\".t, \" db ' view \".\"sc ' view\".v;\r"
 eexpect "Statement diagnostics bundle generated."
 expect -re "SQL shell: \\\\statement-diag download (\\d+)" {
   set id1 $expect_out(1,string)
