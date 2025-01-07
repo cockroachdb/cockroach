6b9591f2f8c68ca77c972bdc55c4d5d2f506871b
Drew Kimball <drewk@cockroachlabs.com>
2025-01-07 03:29:02 -0800

    plpgsql/parser: parse PL/pgSQL DO statements
    This commit adds support for DO statements to the PL/pgSQL parser.
    The helper function `makeDoStmt` analyzes the DO statement options
    (language and code string) and then calls into the PL/pgSQL parser
    for the code string. A following commit will inject this helper into
    the SQL parser as well.
    
    Informs #115268
    
    Release note: None

--- pkg/sql/sem/tree/format.go
+++ pkg/sql/sem/tree/format.go
@@ -296,7 +296,8 @@ type FmtCtx struct {
 	location             *time.Location
 
 	// NOTE: if you add more flags to this structure, make sure to add
-	// corresponding cleanup code in FmtCtx.Close().
+	// corresponding cleanup code in FmtCtx.Close(), as well as handling in
+	// FmtCtx.Clone().
 
 	// The flags to use for pretty-printing.
 	flags FmtFlags
@@ -391,6 +392,21 @@ func NewFmtCtx(f FmtFlags, opts ...FmtCtxOption) *FmtCtx {
 	return ctx
 }
 
+// Clone returns a new FmtCtx with all the same flags and options as the
+// original.
+func (ctx *FmtCtx) Clone() *FmtCtx {
+	newCtx := fmtCtxPool.Get().(*FmtCtx)
+	newCtx.flags = ctx.flags
+	newCtx.ann = ctx.ann
+	newCtx.indexedVarFormat = ctx.indexedVarFormat
+	newCtx.placeholderFormat = ctx.placeholderFormat
+	newCtx.tableNameFormatter = ctx.tableNameFormatter
+	newCtx.indexedTypeFormatter = ctx.indexedTypeFormatter
+	newCtx.dataConversionConfig = ctx.dataConversionConfig
+	newCtx.location = ctx.location
+	return newCtx
+}
+
 // SetDataConversionConfig sets the DataConversionConfig on ctx and returns the
 // old one.
 func (ctx *FmtCtx) SetDataConversionConfig(
@@ -427,6 +443,18 @@ func (ctx *FmtCtx) WithFlags(flags FmtFlags, fn func()) {
 	}
 	oldFlags := ctx.flags
 	ctx.flags = flags
+	defer func() { ctx.flags = oldFlags }()
+
+	fn()
+}
+
+// WithoutConstantRedaction modifies FmtCtx to ensure that constants are
+// displayed rather than being replaced by '_', calls fn, then restores the
+// original flags.
+func (ctx *FmtCtx) WithoutConstantRedaction(fn func()) {
+	oldFlags := ctx.flags
+	ctx.flags &= ^FmtHideConstants
+	ctx.flags &= ^FmtAnonymize
 	defer func() { ctx.flags = oldFlags }()
 
 	fn()
