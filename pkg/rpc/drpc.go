b0ae3171018e556096f005af3ff4930f5ac8d143
Chandra Thumuluru <chandra.thumuluru@cockroachlabs.com>
2025-01-08 05:27:44 +0000

    rpc, kv: use drpc client for batch requests
    This commit allows `*rpc.Connection` to also maintain a `drpcpool.Conn`.
    When drpc is enabled, clients have a choice to either use gRPC or drpc.
    
    Epic: None
    Release note: None

--- pkg/rpc/drpc.go
+++ pkg/rpc/drpc.go
@@ -7,12 +7,17 @@ package rpc
 
 import (
 	"context"
+	"crypto/tls"
 	"math"
 
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"storj.io/drpc/drpcconn"
 	"storj.io/drpc/drpcmanager"
+	"storj.io/drpc/drpcmigrate"
 	"storj.io/drpc/drpcmux"
+	"storj.io/drpc/drpcpool"
 	"storj.io/drpc/drpcserver"
+	"storj.io/drpc/drpcstream"
 	"storj.io/drpc/drpcwire"
 )
 
@@ -51,4 +56,70 @@ func newDRPCServer(ctx context.Context, rpcCtx *Context) (*DRPCServer, error) {
 		Mux:    dmux,
 		TLSCfg: tlsCfg,
 	}, nil
+}
+
+func dialDRPC(rpcCtx *Context) func(ctx context.Context, target string) (drpcpool.Conn, error) {
+	return func(ctx context.Context, target string) (drpcpool.Conn, error) {
+		// TODO(server): could use connection class instead of empty key here.
+		pool := drpcpool.New[struct{}, drpcpool.Conn](drpcpool.Options{})
+		pooledConn := pool.Get(ctx /* unused */, struct{}{}, func(ctx context.Context,
+			_ struct{}) (drpcpool.Conn, error) {
+
+			netConn, err := drpcmigrate.DialWithHeader(ctx, "tcp", target, drpcmigrate.DRPCHeader)
+			if err != nil {
+				return nil, err
+			}
+
+			opts := drpcconn.Options{
+				Manager: drpcmanager.Options{
+					Reader: drpcwire.ReaderOptions{
+						MaximumBufferSize: math.MaxInt,
+					},
+					Stream: drpcstream.Options{
+						MaximumBufferSize: 0, // unlimited
+					},
+				},
+			}
+			var conn *drpcconn.Conn
+			if rpcCtx.ContextOptions.Insecure {
+				conn = drpcconn.NewWithOptions(netConn, opts)
+			} else {
+				tlsConfig, err := rpcCtx.GetClientTLSConfig()
+				if err != nil {
+					return nil, err
+				}
+				// TODO(server): at least with testing certs, we get: manager closed:
+				// tls: either ServerName or InsecureSkipVerify must be specified in
+				// the tls.Config from drpcmanager.(*Manager).manageReader:234
+				//
+				// This is possibly avoided in gRPC by setting ServerName in
+				// (*tlsCreds).ClientHandshake.
+				tlsConfig = tlsConfig.Clone()
+				tlsConfig.InsecureSkipVerify = true // HACK
+				tlsConn := tls.Client(netConn, tlsConfig)
+				conn = drpcconn.NewWithOptions(tlsConn, opts)
+			}
+
+			return conn, nil
+		})
+		// `pooledConn.Close` doesn't tear down any of the underlying TCP
+		// connections but simply marks the pooledConn handle as returning
+		// errors. When we "close" this conn, we want to tear down all of
+		// the connections in the pool (in effect mirroring the behavior of
+		// gRPC where a single conn is shared).
+		return &closeEntirePoolConn{
+			Conn: pooledConn,
+			pool: pool,
+		}, nil
+	}
+}
+
+type closeEntirePoolConn struct {
+	drpcpool.Conn
+	pool *drpcpool.Pool[struct{}, drpcpool.Conn]
+}
+
+func (c *closeEntirePoolConn) Close() error {
+	_ = c.Conn.Close()
+	return c.pool.Close()
 }
