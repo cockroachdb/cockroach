5082a7a67496bb45f94b5b9bc3d322df3ede47a1
Tobias Grieger <tobias.b.grieger@gmail.com>
2025-01-09 17:03:19 +0100

    cli: fix typo

--- pkg/cmd/roachtest/tests/decommission.go
+++ pkg/cmd/roachtest/tests/decommission.go
@@ -1055,7 +1055,7 @@ func runDecommissionDrains(ctx context.Context, t test.Test, c cluster.Cluster) 
 			return err
 		}
 
-		// Check to see if the node has been drained.
+		// Check to see if the node has been drained or decommissioned.
 		// If not, queries should not fail.
 		if err = run(decommNodeDB, `SHOW DATABASES`); err != nil {
 			if strings.Contains(err.Error(), "not accepting clients") { // drained
