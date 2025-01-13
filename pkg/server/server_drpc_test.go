3b07ca59e933e58732fcf895a3e43d217d1d423d
Chandra Thumuluru <chandra.thumuluru@cockroachlabs.com>
2025-01-13 14:13:22 +0000

    server: simple query test with DPRC enabled/disabled.
    Epic: None
    Release note: None

---
+++ pkg/server/server_drpc_test.go
@@ -0,0 +1,57 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package server
+
+import (
+	"context"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/util/envutil"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSelectQuery(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	// Run the test with both DRPC enabled and disabled.
+	testutils.RunTrueAndFalse(t, "enableDRPC", func(t *testing.T, enableDRPC bool) {
+		clear := envutil.TestSetEnv(t, "COCKROACH_EXPERIMENTAL_DRPC_ENABLED", strconv.FormatBool(enableDRPC))
+		defer clear()
+
+		ctx := context.Background()
+		tc := serverutils.StartCluster(t, 3, base.TestClusterArgs{
+			ServerArgs: base.TestServerArgs{
+				Insecure: true,
+			},
+		})
+		defer tc.Stopper().Stop(ctx)
+
+		db := tc.ServerConn(1)
+		defer db.Close()
+
+		ctx, cancel := context.WithTimeout(ctx, time.Second)
+		defer cancel()
+
+		rows, err := db.QueryContext(ctx, "SELECT COUNT(*) FROM system.tenants")
+		require.NoError(t, err)
+		defer rows.Close()
+
+		for rows.Next() {
+			var count int
+			require.NoError(t, rows.Scan(&count))
+			require.Equal(t, 1, count)
+		}
+		require.NoError(t, rows.Close())
+	})
+}
