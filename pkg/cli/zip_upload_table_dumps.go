83a30edc3b14ec3d5df57d21595f9fb70548aa18
Arjun Mahishi <arjun.mahishi@gmail.com>
2025-01-13 15:00:45 +0530

    pkg/cli: add all the remaining table dumps to debugzip upload
    This commit adds all the remaining table dumps to the debug zip upload
    flow. This concludes the table dump upload work. This commit also
    introduces a consistency check to make sure that all the tables
    mentioned in this upload flow actually exist (by cross referencing with
    the debug zip table registry).
    
    Part of: CC-30998
    Epic: CC-28996
    Release note: None

--- pkg/cli/zip_upload_table_dumps.go
+++ pkg/cli/zip_upload_table_dumps.go
@@ -18,7 +18,11 @@ import (
 	"strings"
 
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
 	"github.com/cockroachdb/cockroach/pkg/multitenant/mtinfopb"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/util/encoding"
 	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
 	"github.com/cockroachdb/errors"
 )
@@ -26,6 +30,13 @@ import (
 type tsvColumnParserFn func(string) (any, error)
 
 type columnParserMap map[string]tsvColumnParserFn
+
+// skipColumn is an empty struct that is used to indicate that a column should
+// be skipped. This indicator has to be a type and not a value like bool
+// because, it could conflict with the actual column value. Parsing functions
+// can return this type conditionally or unconditionally to indicate that the
+// column should be skipped.
+type skipColumn struct{}
 
 // clusterWideTableDumps is a map of table dumps and their column parsers.
 // Column parsers are required for columns that require special interpretation.
@@ -57,35 +68,133 @@ var clusterWideTableDumps = map[string]columnParserMap{
 	"crdb_internal.jobs.txt":                        {},
 	"crdb_internal.regions.txt":                     {},
 	"system.table_statistics.txt":                   {},
+	"system.statement_diagnostics_requests.txt":     {},
+	"system.statement_diagnostics.txt":              {},
+	"system.sql_stats_cardinality.txt":              {},
+	"system.settings.txt":                           {},
+	"system.reports_meta.txt":                       {},
+	"system.replication_stats.txt":                  {},
+	"system.replication_critical_localities.txt":    {},
+	"system.replication_constraint_stats.txt":       {},
+	"crdb_internal.cluster_distsql_flows.txt":       {},
+	"system.tenant_tasks.txt":                       {},
+	"system.tenant_settings.txt":                    {},
+	"system.task_payloads.txt":                      {},
+	"system.role_options.txt":                       {},
+	"system.protected_ts_meta.txt":                  {},
+	"system.privileges.txt":                         {},
+	"system.external_connections.txt":               {},
+	"system.database_role_settings.txt":             {},
+	"crdb_internal.super_regions.txt":               {},
+	"crdb_internal.schema_changes.txt":              {},
+	"crdb_internal.partitions.txt":                  {},
+	"crdb_internal.kv_system_privileges.txt":        {},
+	"crdb_internal.kv_protected_ts_records.txt":     {},
+	"crdb_internal.invalid_objects.txt":             {},
+	"crdb_internal.create_type_statements.txt":      {},
+	"crdb_internal.create_procedure_statements.txt": {},
+	"crdb_internal.create_function_statements.txt":  {},
+	"crdb_internal.logical_replication_spans.txt":   {},
+	"crdb_internal.cluster_replication_spans.txt":   {},
+	"system.protected_ts_records.txt":               {},
 
-	// table dumps with columns that need to be interpreted as protos
+	// table dumps with columns that need to be decoded
 	"crdb_internal.system_jobs.txt": {
 		"progress": makeProtoColumnParser[*jobspb.Progress](),
 	},
 	"system.tenants.txt": {
 		"info": makeProtoColumnParser[*mtinfopb.ProtoInfo](),
 	},
+	"system.statement_statistics_limit_5000.txt": {
+		"fingerprint_id":             decodeUUID,
+		"transaction_fingerprint_id": decodeUUID,
+		"plan_hash":                  decodeUUID,
+	},
+	"system.sql_instances.txt": {
+		"session_id": decodeUUID,
+	},
+	"system.sqlliveness.txt": {
+		"session_id":  decodeUUID,
+		"crdb_region": decodeRegion,
+	},
+	"system.lease.txt": {
+		"session_id":  decodeUUID,
+		"crdb_region": decodeRegion,
+	},
+	"system.eventlog.txt": {
+		"uniqueID": decodeUUID,
+	},
+	"system.descriptor.txt": {
+		"descriptor": makeProtoColumnParser[*descpb.Descriptor](),
+	},
+	"system.scheduled_jobs.txt": {
+		"schedule_state":   makeProtoColumnParser[*jobspb.ScheduleState](),
+		"schedule_details": makeProtoColumnParser[*jobspb.ScheduleDetails](),
+		"execution_args":   makeProtoColumnParser[*jobspb.ExecutionArguments](),
+	},
+	"system.tenant_usage.txt": {
+		"total_consumption": makeProtoColumnParser[*kvpb.TenantConsumption](),
+		"current_rates":     makeProtoColumnParser[*kvpb.TenantConsumptionRates](),
+		"next_rates":        makeProtoColumnParser[*kvpb.TenantConsumptionRates](),
+	},
+	"system.span_configurations.txt": {
+		"start_key": decodeKey,
+		"end_key":   decodeKey,
+		"config":    makeProtoColumnParser[*roachpb.SpanConfig](),
+	},
+	"crdb_internal.cluster_locks.txt": {
+		"lock_key": skipColumnFn, // lock_key can be skipped because there is another column called lock_key_pretty
+	},
+	"crdb_internal.transaction_contention_events.fallback.txt": {
+		"blocking_txn_fingerprint_id": decodeUUID,
+		"waiting_stmt_fingerprint_id": decodeUUID,
+		"waiting_txn_fingerprint_id":  decodeUUID,
+	},
+	"crdb_internal.transaction_contention_events.txt": {
+		"blocking_txn_fingerprint_id": decodeUUID,
+		"waiting_stmt_fingerprint_id": decodeUUID,
+		"waiting_txn_fingerprint_id":  decodeUUID,
+	},
 }
 
 var nodeSpecificTableDumps = map[string]columnParserMap{
-	"crdb_internal.node_metrics.txt":                   {},
-	"crdb_internal.node_txn_stats.txt":                 {},
-	"crdb_internal.node_contention_events.txt":         {},
-	"crdb_internal.gossip_liveness.txt":                {},
-	"crdb_internal.gossip_nodes.txt":                   {},
-	"crdb_internal.node_runtime_info.txt":              {},
-	"crdb_internal.node_transaction_statistics.txt":    {},
-	"crdb_internal.node_tenant_capabilities_cache.txt": {},
-	"crdb_internal.node_sessions.txt":                  {},
-	"crdb_internal.node_statement_statistics.txt":      {},
-	"crdb_internal.leases.txt":                         {},
-	"crdb_internal.node_build_info.txt":                {},
-	"crdb_internal.node_memory_monitors.txt":           {},
-	"crdb_internal.active_range_feeds.txt":             {},
-	"crdb_internal.gossip_alerts.txt":                  {},
-	"crdb_internal.node_transactions.txt":              {},
-	"crdb_internal.feature_usage.txt":                  {},
-	"crdb_internal.node_queries.txt":                   {},
+	"crdb_internal.node_metrics.txt":                                {},
+	"crdb_internal.node_txn_stats.txt":                              {},
+	"crdb_internal.node_contention_events.txt":                      {},
+	"crdb_internal.gossip_liveness.txt":                             {},
+	"crdb_internal.gossip_nodes.txt":                                {},
+	"crdb_internal.node_runtime_info.txt":                           {},
+	"crdb_internal.node_transaction_statistics.txt":                 {},
+	"crdb_internal.node_tenant_capabilities_cache.txt":              {},
+	"crdb_internal.node_sessions.txt":                               {},
+	"crdb_internal.node_statement_statistics.txt":                   {},
+	"crdb_internal.leases.txt":                                      {},
+	"crdb_internal.node_build_info.txt":                             {},
+	"crdb_internal.node_memory_monitors.txt":                        {},
+	"crdb_internal.active_range_feeds.txt":                          {},
+	"crdb_internal.gossip_alerts.txt":                               {},
+	"crdb_internal.node_transactions.txt":                           {},
+	"crdb_internal.feature_usage.txt":                               {},
+	"crdb_internal.node_queries.txt":                                {},
+	"crdb_internal.cluster_replication_node_stream_checkpoints.txt": {},
+	"crdb_internal.logical_replication_node_processors.txt":         {},
+	"crdb_internal.cluster_replication_node_streams.txt":            {},
+	"crdb_internal.node_inflight_trace_spans.txt":                   {},
+	"crdb_internal.node_distsql_flows.txt":                          {},
+	"crdb_internal.cluster_replication_node_stream_spans.txt":       {},
+
+	// table dumps with columns that need to be decoded
+	"crdb_internal.node_txn_execution_insights.txt": {
+		"txn_fingerprint_id": decodeUUID,
+	},
+	"crdb_internal.node_execution_insights.txt": {
+		"txn_fingerprint_id":  decodeUUID,
+		"stmt_fingerprint_id": decodeUUID,
+	},
+	"crdb_internal.kv_session_based_leases.txt": {
+		"session_id":  decodeUUID,
+		"crdb_region": decodeRegion,
+	},
 }
 
 // makeProtoColumnParser returns a generic function that can parse a column
@@ -120,7 +229,7 @@ func processTableDump(
 
 	var (
 		lines     = [][]byte{}
-		tableName = strings.TrimSuffix(fileName, filepath.Ext(fileName))
+		tableName = strings.TrimSuffix(filepath.Base(fileName), filepath.Ext(fileName))
 		tags      = append(
 			[]string{"env:debug", "source:debug-zip"}, makeDDTag(uploadIDTag, uploadID),
 			makeDDTag(clusterTag, debugZipUploadOpts.clusterName), makeDDTag(tableTag, tableName),
@@ -146,6 +255,10 @@ func processTableDump(
 				colBytes, err := parser(cols[i])
 				if err != nil {
 					return err
+				}
+
+				if colBytes == (skipColumn{}) {
+					continue
 				}
 
 				headerColumnMapping[h] = colBytes
@@ -215,4 +328,29 @@ func getNodeSpecificTableDumps(debugDirPath string) ([]string, error) {
 	}
 
 	return filteredTxtFiles, nil
+}
+
+func decodeUUID(inp string) (any, error) {
+	_, u, err := encoding.DecodeUUIDValue([]byte(inp))
+	return u, err
+}
+
+func decodeRegion(s string) (any, error) {
+	if s == `\x80` {
+		return nil, nil
+	}
+	return s, nil
+}
+
+func decodeKey(s string) (any, error) {
+	b, ok := interpretString(s)
+	if !ok {
+		return nil, fmt.Errorf("failed to interpret column value: %s", s)
+	}
+
+	return roachpb.Key(b).String(), nil
+}
+
+func skipColumnFn(s string) (any, error) {
+	return skipColumn{}, nil
 }
