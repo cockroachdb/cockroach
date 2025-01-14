c63baf662dacbb91c1b8d98851b9007db7c4548e
Miles Frankel <miles.frankel@cockroachlabs.com>
2025-01-14 13:02:35 -0500

    changefeedccl: refactor kafka auth to be plugin-based
    Epic: none
    Release note: None

---
+++ pkg/ccl/changefeedccl/kafkaauth/doc.go
@@ -0,0 +1,4 @@
+// Package kafkaauth provides Kafka SASL authentication mechanisms for use with
+// the changefeed kafka sinks (v1 & v2). Most standard SASL mechanisms are
+// supported, and some custom ones as well.
+package kafkaauth
