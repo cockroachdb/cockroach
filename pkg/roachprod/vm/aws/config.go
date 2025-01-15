5761a8134fe14ac3a225f0a8f09430bc31393ca4
Stan Rosenberg <stan.rosenberg@gmail.com>
2025-01-14 23:30:45 -0500

    roachprod: user-specified `awsConfigValue` may get overriden by `DefaultConfig`
    A recent refactoring [1] has exposed a latent bug, wherein
    user-specified `awsConfigValue`, i.e., `--aws-config`
    may get overridden by the default configuration.
    
    This PR fixes the bug.
    
    [1] https://github.com/cockroachdb/cockroach/pull/137394
    
    Epic: none
    Fixes: #139102
    Release note: None

--- pkg/roachprod/vm/aws/config.go
+++ pkg/roachprod/vm/aws/config.go
@@ -170,7 +170,13 @@ func (c *awsConfigValue) Set(path string) (err error) {
 	if err != nil {
 		return err
 	}
-	return json.Unmarshal(data, &c.awsConfig)
+	err = json.Unmarshal(data, &c.awsConfig)
+	if err != nil {
+		return err
+	}
+	// Update the provider's config with the user-specified config.
+	providerInstance.Config = &c.awsConfig
+	return nil
 }
 
 // Type is part of the pflag.Value interface.
