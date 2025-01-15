9c594d001f7cb8f831ac734ed8e4efd07eef2160
Pavel Kalinnikov <pavel@cockroachlabs.com>
2025-01-15 18:46:48 +0000

    move

--- pkg/kv/kvserver/stateloader/engine.go
+++ pkg/kv/kvserver/stateloader/engine.go
@@ -1,9 +1,4 @@
-// Copyright 2025 The Cockroach Authors.
-//
-// Use of this software is governed by the CockroachDB Software License
-// included in the /LICENSE file.
-
-package kvstorage
+package stateloader
 
 import "github.com/cockroachdb/cockroach/pkg/storage"
 
