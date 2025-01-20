d7b08f9fe46e7040b1c46c3bfc8bcf72aa4a7315
Shubham Dhama <shubham.dhama@cockroachlabs.com>
2025-01-20 09:52:55 +0530

    server: enable tenant testing for server/admin APIs
    This change is mostly straightforward:
    * Removed remaining usages of `TODOTestTenantDisabled`, enabling tenant
      testing for those tests.
    * Made some improvements to a couple of tests.
    * Created a new function (hopefully temporary)
      `TestSkippedForExternalModeDueToPerformance` to exclude external mode for
      `TestAdminAPIEvents` due to poor performance.
    
    Informs: #138912
    Epic: CRDB-38970
    Release note: None

--- pkg/server/application_api/insights_test.go
+++ pkg/server/application_api/insights_test.go
@@ -74,9 +74,6 @@ func TestDatabaseAndTableIndexRecommendations(t *testing.T) {
 	stubDropUnusedDuration := time.Hour
 
 	s, sqlDB, _ := serverutils.StartServer(t, base.TestServerArgs{
-		// Disable the default test tenant for now as this tests fails
-		// with it enabled. Tracked with #81590.
-		DefaultTestTenant: base.TODOTestTenantDisabled,
 		Knobs: base.TestingKnobs{
 			UnusedIndexRecommendKnobs: &idxusage.UnusedIndexRecommendationTestingKnobs{
 				GetCreatedAt:   stubTime.getCreatedAt,
