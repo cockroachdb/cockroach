00b32f7ca6e827d5c17bea23d1119b72126609e8
Bergin Dedej <bergin.dedej@cockroachlabs.com>
2025-01-20 14:32:53 -0500

    sql/schema: guard DROP INDEX behind sql_safe_updates
    This PR makes it so that DROP INDEX is now guarded behind
    `sql_safe_updates`.
    
    Fixes: #136830
    Release note: DROP INDEX can now only be run when `sql_safe_updates`
    are set to false.

--- pkg/sql/drop_index.go
+++ pkg/sql/drop_index.go
@@ -75,6 +75,23 @@ func (p *planner) DropIndex(ctx context.Context, n *tree.DropIndex) (planNode, e
 	return &dropIndexNode{n: n, idxNames: idxNames}, nil
 }
 
+// failDropIndexIfSafeUpdates checks if the sql_safe_updates is present, and if so, it
+// will fail the operation.
+func failDropIndexIfSafeUpdates(params runParams) {
+	if params.SessionData().SafeUpdates {
+		panic(
+			pgerror.WithCandidateCode(
+				errors.WithMessage(
+					errors.New(
+						"DROP INDEX"),
+					"rejected (sql_safe_updates = true)",
+				),
+				pgcode.Warning,
+			),
+		)
+	}
+}
+
 // ReadingOwnWrites implements the planNodeReadingOwnWrites interface.
 // This is because DROP INDEX performs multiple KV operations on descriptors
 // and expects to see its own writes.
@@ -82,6 +99,8 @@ func (n *dropIndexNode) ReadingOwnWrites() {}
 
 func (n *dropIndexNode) startExec(params runParams) error {
 	telemetry.Inc(sqltelemetry.SchemaChangeDropCounter("index"))
+
+	failDropIndexIfSafeUpdates(params)
 
 	if n.n.Concurrently {
 		params.p.BufferClientNotice(
