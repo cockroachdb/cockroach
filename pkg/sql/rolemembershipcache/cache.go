2f2b28cb7db702f813f74b71cc37b23d7156f5e0
Rafi Shamim <rafi@cockroachlabs.com>
2025-01-21 19:15:34 -0500

    sql: retry when reading pg_catalog tables with an old timestamp
    In some situations, it's possible that the timestamp we choose to read
    from in RunAtCacheReadTS is before the GC threshold. In these cases, we
    have no choice but to run the operation in the current transaction.
    
    Release note (bug fix): Fixed a bug that could cause SHOW TABLES and
    other introspection operations to encounter a "batch timestamp
    must be after replica GC threshold" error.

--- pkg/sql/rolemembershipcache/cache.go
+++ pkg/sql/rolemembershipcache/cache.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 
 	"github.com/cockroachdb/cockroach/pkg/keys"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/security/username"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
@@ -102,12 +103,20 @@ func (m *MembershipCache) RunAtCacheReadTS(
 	}
 
 	// If we found a historical timestamp to use, run in a different transaction.
-	return db.DescsTxn(ctx, func(ctx context.Context, newTxn descs.Txn) error {
+	if err := db.DescsTxn(ctx, func(ctx context.Context, newTxn descs.Txn) error {
 		if err := newTxn.KV().SetFixedTimestamp(ctx, readTS); err != nil {
 			return err
 		}
 		return f(ctx, newTxn)
-	})
+	}); err != nil {
+		if errors.HasType(err, (*kvpb.BatchTimestampBeforeGCError)(nil)) {
+			// It's still possible that we picked a timestamp that is too old, so
+			// in that case we'll retry using the current transaction.
+			return f(ctx, txn)
+		}
+		return err
+	}
+	return nil
 }
 
 // GetRolesForMember looks up all the roles 'member' belongs to (direct and
