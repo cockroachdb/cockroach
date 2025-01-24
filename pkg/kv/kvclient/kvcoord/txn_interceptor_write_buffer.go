429db1b41ae0f0b153804fab3875499365a7ef2e
Arul Ajmani <arulajmani@gmail.com>
2025-01-24 10:51:37 -0500

    kvclient: introduce txnWriteBuffer interceptor
    This patch introduces a new txn interceptor to the TxnCoordSender's
    interceptor stack -- the txnWriteBuffer. This commit is mostly
    boilerplate code with some forward looking commentary, so for now, the
    interceptor is as no-op as it gets. Soon, we'll expand this to actually
    introduce client side write buffering.
    
    References https://github.com/cockroachdb/cockroach/issues/139053
    
    Release note: None

---
+++ pkg/kv/kvclient/kvcoord/txn_interceptor_write_buffer.go
@@ -0,0 +1,143 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package kvcoord
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/settings"
+)
+
+// BufferedWritesEnabled is used to enable write buffering.
+var BufferedWritesEnabled = settings.RegisterBoolSetting(
+	settings.ApplicationLevel,
+	"kv.transaction.write_buffering.enabled",
+	"if enabled, transactional writes are buffered on the client",
+	false,
+	settings.WithPublic,
+)
+
+// txnWriteBuffer is a txnInterceptor that buffers transactional writes until
+// commit time. Moreover, it also decomposes read-write KV operations (e.g.
+// CPuts, InitPuts) into separate (locking) read and write operations, buffering
+// the latter until commit time.
+//
+// Buffering writes until commit time has four main benefits:
+//
+// 1. It allows for more batching of writes, which can be more efficient.
+// Instead of sending write batches one at a time, we can batch all write
+// batches and send the in a single batch at commit time. This is a win even if
+// writes would otherwise be pipelined through raft.
+//
+// 2. It allows for the elimination of redundant writes. If a client writes to
+// the same key multiple times in a transaction, only the last write needs to be
+// written to the KV layer.
+//
+// 3. It allows the client to serve read-your-own-writes locally, which can be
+// much faster and cheaper than sending them to the leaseholder. This is
+// especially true when the leaseholder isn't colocated with the client.
+//
+// By serving read-your-own-writes locally from the gateway, write buffering
+// also avoids the problem of pipeline stalls that can occur when a client reads
+// a pipelined write before it has finished replicating through raft. For
+// details on pipeline stalls, see txnPipeliner.
+//
+// 4. It allows clients to passively hit the 1-phase commit fast path, instead
+// of requiring clients to carefully construct "auto-commit" BatchRequests to
+// make use of the optimization. By buffering writes on the client before
+// commit, we avoid immediately disabling the fast path when the client issues
+// their first write. Then, at commit time, we flush the buffer and will happen
+// to hit the 1-phase commit fast path if all writes end up going to the same
+// range.
+//
+// However, buffering writes comes with some challenges.
+//
+// The first challenge is that read-only requests need to be aware of any
+// buffered writes, as they may need to serve some reads from the buffer
+// instead of the KV layer (read-your-own-writes).
+//
+// Similarly, any read-write requests, such as CPuts, that we decompose into
+// separate read and write operations, need to be aware of any buffered writes
+// that may affect their read half. The read portion must be served from the
+// buffer instead of the KV-layer if the key has already been written to
+// previously. However, we aren't guaranteed to have acquired a corresponding
+// lock on the key if a buffered write exists for the key -- as such, we must
+// still send a locking read request to the KV layer to acquire a lock.
+//
+// The picture is further complicated when distributed execution is introduced
+// into the mix. A read request that is distributed by the client also needs to
+// be aware of the write buffer. As such, when constructing a leaf transaction
+// to serve a distributed execution read, we must also ship[1] the write buffer
+// along.
+//
+// The second challenge is around the handling of savepoints. In particular,
+// when a savepoint is rolled back, we must clear out any writes that happened
+// after the (now) rolled back savepoint. This means that if a key is written to
+// multiple times in a transaction, we must retain all writes to the key. Note
+// that this is only required when the transaction is in progress -- once the
+// transaction is ready to commit, only the last value written to the key needs
+// to be flushed to the KV layer.
+//
+// The third challenge is around memory limits and preventing OOMs. As writes
+// are buffered in-memory, per-transaction, we need to be careful not to OOM
+// nodes by buffering too many writes. To that end, a per-transaction memory
+// limit on the write buffer must be enforced. If this limit is exceeded, no
+// more writes are buffered, and the buffer (either in its entirety or
+// partially[2]) must be force flushed. Force flushing entails sending all
+// buffered writes to the KV layer prematurely (i.e. before commit time).
+//
+// [1] Instead of shipping the entire write buffer, we can constrain this to
+// just the portion that overlaps with the span of the read request that's being
+// distributed for evaluation by the client.
+//
+// [2] The decision to flush the buffer in its entirety vs. partially is a
+// tradeoff. Flushing the entire buffer is simpler and frees up more memory.
+// Flushing the buffer partially preserves some (all but the fourth) of the
+// listed benefits of buffering writes for the unflushed portion of the buffer.
+type txnWriteBuffer struct {
+	enabled bool
+
+	wrapped lockedSender
+}
+
+func (twb *txnWriteBuffer) SendLocked(
+	ctx context.Context, ba *kvpb.BatchRequest,
+) (*kvpb.BatchResponse, *kvpb.Error) {
+	if !twb.enabled {
+		return twb.wrapped.SendLocked(ctx, ba)
+	}
+	panic("unimplemented")
+}
+
+// setWrapped implements the txnInterceptor interface.
+func (twb *txnWriteBuffer) setWrapped(wrapped lockedSender) {
+	twb.wrapped = wrapped
+}
+
+// populateLeafInputState is part of the txnInterceptor interface.
+func (twb *txnWriteBuffer) populateLeafInputState(*roachpb.LeafTxnInputState) {}
+
+// populateLeafFinalState is part of the txnInterceptor interface.
+func (twb *txnWriteBuffer) populateLeafFinalState(*roachpb.LeafTxnFinalState) {}
+
+// importLeafFinalState is part of the txnInterceptor interface.
+func (twb *txnWriteBuffer) importLeafFinalState(context.Context, *roachpb.LeafTxnFinalState) error {
+	return nil
+}
+
+// epochBumpedLocked implements the txnInterceptor interface.
+func (twb *txnWriteBuffer) epochBumpedLocked() {}
+
+// createSavepointLocked is part of the txnInterceptor interface.
+func (twb *txnWriteBuffer) createSavepointLocked(context.Context, *savepoint) {}
+
+// rollbackToSavepointLocked is part of the txnInterceptor interface.
+func (twb *txnWriteBuffer) rollbackToSavepointLocked(ctx context.Context, s savepoint) {}
+
+// closeLocked implements the txnInterceptor interface.
+func (twb *txnWriteBuffer) closeLocked() {}
