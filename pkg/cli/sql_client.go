fbad019550c1ca67ee96ee8fd7c1bffcbff5ce2f
Matt Spilchen <matt.spilchen@cockroachlabs.com>
2025-01-24 17:40:41 -0400

    pkg/cli: Migrate to pgx v5
    This updates the pkg/cli packages to use version 5 of the jackc/pgx
    library. While most changes were straightforward, transitioning from v4
    to v5 required a few API updates:
    - ConnInfo was renamed to TypeMap. Any references to connInfo were
      updated to typeMap for consistency.
    - pgtype.Numeric can now be converted to []byte using MarshalJSON.
      Previously, this was done using EncodeText, which has been removed in
      v5.
    
    Epic: none
    Release note: none
    
    Informs #137595

--- pkg/cli/sql_client.go
+++ pkg/cli/sql_client.go
@@ -20,7 +20,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/envutil"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/errors"
-	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v5/pgconn"
 )
 
 // sqlConnTimeout is the default SQL connect timeout. This can also be
