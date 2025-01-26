e20f0a5db544875981be0bb4e11a639ca3be62dc
Shaik Zakir Hussain <zakir.shaik@cockroachlabs.com>
2025-01-26 11:21:35 +0530

    server/storage_api: update TestRangeResponse unit test, to work correctly with secondary application tenant
    What was there before: the change focuses on updating the unit test which was failing with secondary tenant while hitting the
    _status/range/{id} endpoint. This was happening because the the test had a hard-coded range id `1` to be
    fetched from the aforementioned endpoint. And this range id lies outside the scope of a secondary application
    tenant.
    
    resolution: made changes to the test to include a range id which lies well within the scope of the secondary tenant.
    Fixes: #110018
    Part of: https://cockroachlabs.atlassian.net/browse/CRDB-31224
    Epic: CRDB-38968
    
    Release note: None

--- pkg/server/storage_api/ranges_test.go
+++ pkg/server/storage_api/ranges_test.go
@@ -132,9 +132,7 @@ func TestRangeResponse(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 	defer kvserver.EnableLeaseHistoryForTesting(100)()
-	srv := serverutils.StartServerOnly(t, base.TestServerArgs{
-		DefaultTestTenant: base.TestIsForStuffThatShouldWorkWithSecondaryTenantsButDoesntYet(110018),
-	})
+	srv := serverutils.StartServerOnly(t, base.TestServerArgs{})
 	defer srv.Stopper().Stop(context.Background())
 	ts := srv.ApplicationLayer()
 
@@ -143,29 +141,54 @@ func TestRangeResponse(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// TODO(#110018): grant a special capability to the secondary tenant
-	// before the endpoint can be accessed.
+	t.Run("access range endpoint within current tenant's scope", func(t *testing.T) {
+		var currentTenantRangeId int64
+		// Create a new table in the current tenant.
+		r := sqlutils.MakeSQLRunner(ts.SQLConn(t))
+		r.Exec(t, "CREATE TABLE t (x INT PRIMARY KEY, xsquared INT)")
+		for i := 0; i < 5; i++ {
+			r.Exec(t, fmt.Sprintf("ALTER TABLE t SPLIT AT VALUES (%d)", 100*i/5))
+		}
+		// Get some ranges from the given table
+		r.QueryRow(t, "SELECT range_id FROM [SHOW RANGES FROM TABLE t WITH DETAILS] LIMIT 1;").Scan(&currentTenantRangeId)
+		t.Logf("Table 't' range: %d", currentTenantRangeId)
+		require.NoError(t, validateRangeBelongsToTenant(ts, currentTenantRangeId))
+	})
 
+	t.Run("access range endpoint within system tenant's scope", func(t *testing.T) {
+		if !srv.StartedDefaultTestTenant() {
+			require.NoError(t, validateRangeBelongsToTenant(ts, 1))
+		} else {
+			expectedError := fmt.Errorf("got the wrong number of ranges in the response, expected %d, actual %d", 1, 0)
+			actualError := validateRangeBelongsToTenant(ts, 1)
+			require.Error(t, actualError)
+			require.Equal(t, expectedError, actualError)
+		}
+
+	})
+}
+
+func validateRangeBelongsToTenant(ts serverutils.ApplicationLayerInterface, rangeId int64) error {
 	var response serverpb.RangeResponse
-	if err := srvtestutils.GetStatusJSONProto(ts, "range/1", &response); err != nil {
-		t.Fatal(err)
+	if err := srvtestutils.GetStatusJSONProto(ts, fmt.Sprintf("range/%d", rangeId), &response); err != nil {
+		return err
 	}
 
 	// This is a single node cluster, so only expect a single response.
 	if e, a := 1, len(response.ResponsesByNodeID); e != a {
-		t.Errorf("got the wrong number of responses, expected %d, actual %d", e, a)
+		return fmt.Errorf("got the wrong number of responses, expected %d, actual %d", e, a)
 	}
 
 	node1Response := response.ResponsesByNodeID[response.NodeID]
 
 	// The response should come back as valid.
 	if !node1Response.Response {
-		t.Errorf("node1's response returned as false, expected true")
+		return fmt.Errorf("node1's response returned as false, expected true")
 	}
 
 	// The response should include just the one range.
 	if e, a := 1, len(node1Response.Infos); e != a {
-		t.Fatalf("got the wrong number of ranges in the response, expected %d, actual %d", e, a)
+		return fmt.Errorf("got the wrong number of ranges in the response, expected %d, actual %d", e, a)
 	}
 
 	info := node1Response.Infos[0]
@@ -177,18 +200,19 @@ func TestRangeResponse(t *testing.T) {
 
 	// Check some other values.
 	if len(info.State.Desc.InternalReplicas) != 1 || info.State.Desc.InternalReplicas[0] != expReplica {
-		t.Errorf("unexpected replica list %+v", info.State.Desc.InternalReplicas)
+		return fmt.Errorf("unexpected replica list %+v", info.State.Desc.InternalReplicas)
 	}
 
 	if info.State.Lease == nil || info.State.Lease.Empty() {
-		t.Error("expected a nontrivial Lease")
+		return fmt.Errorf("expected a nontrivial Lease")
 	}
 
 	if info.State.LastIndex == 0 {
-		t.Error("expected positive LastIndex")
+		return fmt.Errorf("expected positive LastIndex")
 	}
 
 	if len(info.LeaseHistory) == 0 {
-		t.Error("expected at least one lease history entry")
+		return fmt.Errorf("expected at least one lease history entry")
 	}
+	return nil
 }
