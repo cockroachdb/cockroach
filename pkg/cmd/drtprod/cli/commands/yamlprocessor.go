dfa06391056b727787a79520c114b59d65ca591d
Bhaskarjyoti Bora <bhaskar.bora@cockroachlabs.com>
2025-01-26 21:51:31 +0530

    drtprod: support for remote execution
    currently drtprod execute runs the execution of the YAML file locally. This is an issue in cases where we have long running deployments. So, to solve this problem, this change provides a new option to run the execution on a remote machine. This machine is created and all the dependent files are copied across to that machine. All executions are done in the remote machines.
    In addition, I have added another feature to define the dependent scripts. This becomes more important as the impact of failure in remote execution is more as compared to running locally. So, we have to ensure that the required files are present.
    
    Epic: None
    Release Note: None

--- pkg/cmd/drtprod/cli/commands/yamlprocessor.go
+++ pkg/cmd/drtprod/cli/commands/yamlprocessor.go
@@ -7,16 +7,24 @@ package commands
 
 import (
 	"context"
+	_ "embed"
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"slices"
 	"strings"
 	"sync"
 
 	"github.com/cockroachdb/cockroach/pkg/cmd/drtprod/helpers"
+	"github.com/cockroachdb/cockroach/pkg/roachprod"
+	"github.com/cockroachdb/cockroach/pkg/roachprod/config"
+	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/errors"
 	"github.com/spf13/cobra"
 	"golang.org/x/exp/maps"
+	"golang.org/x/sync/errgroup"
 	"gopkg.in/yaml.v2"
 )
 
@@ -28,13 +36,24 @@ const (
 	targetResultFailure              // the target has failed
 )
 
-// commandExecutor is responsible for executing the shell commands
-var commandExecutor = helpers.ExecuteCmdWithPrefix
+var (
+	// commandExecutor is responsible for executing the shell commands
+	commandExecutor = helpers.ExecuteCmdWithPrefix
+
+	// roachprodRun executes roachprod.Run. This helps in unit tests
+	roachprodRun = roachprod.Run
+
+	// roachprodPut executes roachprod.Put. This helps in unit tests
+	roachprodPut = roachprod.Put
+
+	drtprodLocation = "artifacts/drtprod"
+)
 
 // GetYamlProcessor creates a new Cobra command for processing a YAML file.
 // The command expects a YAML file as an argument and runs the commands defined in it.
 func GetYamlProcessor(ctx context.Context) *cobra.Command {
 	displayOnly := false
+	remoteConfigYaml := ""
 	userProvidedTargetNames := make([]string, 0)
 	cobraCmd := &cobra.Command{
 		Use:   "execute <yaml file> [flags]",
@@ -45,19 +64,23 @@ You can also specify the rollback commands in case of a step failure.
 		Args: cobra.ExactArgs(1),
 		// Wraps the command execution with additional error handling
 		Run: helpers.Wrap(func(cmd *cobra.Command, args []string) (retErr error) {
-			yamlFileLocation := args[0]
-			// Read the YAML file from the specified location
-			yamlContent, err := os.ReadFile(yamlFileLocation)
+			_, err := exec.LookPath("drtprod")
 			if err != nil {
+				// drtprod is needed in the path to run yaml commands
 				return err
 			}
-			return processYaml(ctx, yamlContent, displayOnly, userProvidedTargetNames)
+			yamlFileLocation := args[0]
+			return processYamlFile(ctx, yamlFileLocation, displayOnly, remoteConfigYaml, userProvidedTargetNames)
 		}),
 	}
 	cobraCmd.Flags().BoolVarP(&displayOnly,
-		"display-only", "d", false, "displays the commands that will be executed without running them")
+		"display-only", "d", false, "displays the commands that will be executed without running them.")
 	cobraCmd.Flags().StringArrayVarP(&userProvidedTargetNames,
 		"targets", "t", nil, "the targets to execute. executes all if not mentioned.")
+	cobraCmd.Flags().StringVarP(&remoteConfigYaml,
+		"remote-config-yaml", "r", "", "runs the deployment remotely in the monitor node. This is "+
+			"useful for long running deployments as the local execution completes after creating a remote cluster. The remote "+
+			"config YAML provides the configuration of the remote monitor cluster.")
 	return cobraCmd
 }
 
@@ -106,8 +129,9 @@ type target struct {
 
 // yamlConfig represents the structure of the entire YAML configuration file.
 type yamlConfig struct {
-	Environment map[string]string `yaml:"environment"` // Environment variables to set
-	Targets     []target          `yaml:"targets"`     // List of target clusters with their steps
+	DependentFileLocations []string          `yaml:"dependent_file_locations,omitempty"` // location of all the dependent files - scripts/binaries
+	Environment            map[string]string `yaml:"environment"`                        // Environment variables to set
+	Targets                []target          `yaml:"targets"`                            // List of target clusters with their steps
 }
 
 // command is a simplified representation of a shell command that needs to be executed.
@@ -127,16 +151,203 @@ func (c *command) String() string {
 	return cmdStr
 }
 
-// processYaml reads the YAML file, parses it, sets the environment variables, and processes the targets.
-func processYaml(
-	ctx context.Context, yamlContent []byte, displayOnly bool, userProvidedTargetNames []string,
+// processYamlFile is responsible for processing the yaml configuration. The YAML commands can be executed
+// either locally or in a remote VM based on whether "remoteConfigYaml" is provided or not.
+// If the remoteConfigYaml is provided, a new VM is created based on the configuration provided in the remoteConfigYaml
+// is created and the current YAML is copied across along with the required scripts and binaries and execution
+// is run there. This is useful for long-running deployments where the local deployment just delegates
+// the execution of the deployment to the remote machine.
+func processYamlFile(
+	ctx context.Context,
+	yamlFileLocation string,
+	displayOnly bool,
+	remoteConfigYaml string,
+	userProvidedTargetNames []string,
 ) (err error) {
+	if _, err = os.Stat(yamlFileLocation); err != nil {
+		// if YAML config file is not available, we cannot proceed
+		return errors.Wrapf(err, "%s is not present", yamlFileLocation)
+	}
+	// Read the YAML file from the specified location
+	yamlContent, err := os.ReadFile(yamlFileLocation)
+	if err != nil {
+		return errors.Wrapf(err, "error reading %s", yamlFileLocation)
+	}
+	remoteDeployYamlContent := make([]byte, 0)
+	if remoteConfigYaml != "" {
+		if _, err = os.Stat(remoteConfigYaml); err != nil {
+			// if remote YAML config file is not available, we cannot proceed with remote deployment
+			return errors.Wrapf(err, "%s is not present", remoteConfigYaml)
+		}
+		remoteDeployYamlContent, err = os.ReadFile(remoteConfigYaml)
+		if err != nil {
+			return errors.Wrapf(err, "error reading %s", remoteConfigYaml)
+		}
+	}
+	return processYaml(ctx, yamlFileLocation, yamlContent, remoteDeployYamlContent, displayOnly, userProvidedTargetNames)
+}
 
+// processYaml processes the YAML content as explained in processYamlFile. A separate function taking the binary direct
+// helps in writing unit tests.
+func processYaml(
+	ctx context.Context,
+	yamlFileLocation string,
+	yamlContent, remoteDeployYamlContent []byte,
+	displayOnly bool,
+	userProvidedTargetNames []string,
+) error {
 	// Unmarshal the YAML content into the yamlConfig struct
-	var config yamlConfig
-	if err = yaml.UnmarshalStrict(yamlContent, &config); err != nil {
+	var clusterConfig yamlConfig
+	if err := yaml.UnmarshalStrict(yamlContent, &clusterConfig); err != nil {
 		return err
 	}
+	if !displayOnly {
+		if err := checkForDependentFiles(clusterConfig.DependentFileLocations); err != nil {
+			return err
+		}
+	}
+	if len(remoteDeployYamlContent) > 0 {
+		// Unmarshal the YAML content for the remote deployment and the cluster config will be executed in that remote VM.
+		var remoteDeploymentConfig yamlConfig
+		if err := yaml.UnmarshalStrict(remoteDeployYamlContent, &remoteDeploymentConfig); err != nil {
+			return err
+		}
+		return processYamlRemote(ctx, yamlFileLocation, clusterConfig, remoteDeploymentConfig, displayOnly, userProvidedTargetNames)
+	}
+	return processYamlConfig(ctx, clusterConfig, displayOnly, userProvidedTargetNames)
+}
+
+// checkForDependentFiles checks if the dependent files are missing and raises an error if not
+func checkForDependentFiles(dependentFileLocations []string) error {
+	missingFiles := make([]string, 0)
+	for _, f := range dependentFileLocations {
+		_, err := os.Stat(f)
+		if err != nil {
+			missingFiles = append(missingFiles, f)
+		}
+	}
+	if len(missingFiles) == 0 {
+		return nil
+	}
+	return errors.Errorf("dependent files are missing for the YAML: %s", strings.Join(missingFiles, ", "))
+}
+
+// processYamlRemote executes the YAML remotely
+func processYamlRemote(
+	ctx context.Context,
+	yamlFileLocation string,
+	config, remoteDeploymentConfig yamlConfig,
+	displayOnly bool,
+	userProvidedTargetNames []string,
+) (err error) {
+	if _, err = os.Stat(drtprodLocation); err != nil {
+		// if drtprod binary is not available in artifacts, we cannot proceed as this is needed for executing
+		// the YAML remotely
+		return errors.Wrapf(err, "%s must be available for executing remotely", drtprodLocation)
+	}
+	// displayOnly has to be run locally as this is just for displaying the commands
+	if displayOnly {
+		return errors.Errorf("display option is not valid for remote execution")
+	}
+	// the MONITOR_CLUSTER is overwritten with the YAML file name
+	yamlFileName := strings.Split(filepath.Base(yamlFileLocation), ".")[0]
+	monitorClusterName := fmt.Sprintf("%s-monitor", strings.ReplaceAll(yamlFileName, "_", "-"))
+	remoteDeploymentConfig.Environment["MONITOR_CLUSTER"] = monitorClusterName
+	// processing the remoteDeploymentConfig creates the VM for the remote execution.
+	err = processYamlConfig(ctx, remoteDeploymentConfig, false, make([]string, 0))
+	if err != nil {
+		return err
+	}
+	// Once the cluster is ready, the dependent files are uploaded
+	err = uploadAllDependentFiles(ctx, yamlFileLocation, config, monitorClusterName)
+	if err != nil {
+		return err
+	}
+	// The last step is to setup and execute the command on the remote VM
+	return setupAndExecute(ctx, yamlFileLocation, userProvidedTargetNames, monitorClusterName)
+}
+
+// setupAndExecute moves the drtprod binary to /usr/bin on the remote cluster
+// and then starts its execution using systemd, optionally targeting specific user-provided targets.
+func setupAndExecute(
+	ctx context.Context,
+	yamlFileLocation string,
+	userProvidedTargetNames []string,
+	monitorClusterName string,
+) error {
+	logger := config.Logger
+	// Move the drtprod binary to /usr/bin to ensure it is available system-wide on the cluster.
+	err := roachprodRun(ctx, logger, monitorClusterName, "", "", true,
+		os.Stdout, os.Stderr,
+		[]string{fmt.Sprintf("sudo mv %s /usr/bin", drtprodLocation)},
+		install.RunOptions{FailOption: install.FailSlow})
+	if err != nil {
+		return err
+	}
+
+	// Prepare the systemd command to execute the drtprod binary.
+	executeArgs := fmt.Sprintf(
+		"sudo systemd-run --unit %s --same-dir --uid $(id -u) --gid $(id -g) drtprod execute ./%s",
+		monitorClusterName,
+		yamlFileLocation)
+
+	// If the user provided specific target names, add them to the execution command.
+	if len(userProvidedTargetNames) > 0 {
+		executeArgs = fmt.Sprintf("%s -t %s", executeArgs, strings.Join(userProvidedTargetNames, " "))
+	}
+
+	// Run the systemd command on the remote cluster.
+	return roachprodRun(ctx, logger, monitorClusterName, "", "", true,
+		os.Stdout, os.Stderr,
+		[]string{executeArgs},
+		install.RunOptions{FailOption: install.FailSlow})
+}
+
+// uploadAllDependentFiles uploads all dependent files (including the YAML file and others)
+// to the specified remote cluster in parallel using goroutines.
+// If any error occurs during the upload, it is tracked and returned at the end.
+func uploadAllDependentFiles(
+	ctx context.Context, yamlFileLocation string, c yamlConfig, monitorClusterName string,
+) error {
+	logger := config.Logger
+	var g errgroup.Group
+	// Loop through all dependent files and upload them in parallel.
+	for _, fileLocation := range append(c.DependentFileLocations, yamlFileLocation, drtprodLocation) {
+		fl := fileLocation
+		g.Go(func() error {
+			// Create directory on the remote if the file contains a directory path.
+			if strings.Contains(fl, "/") {
+				dirLocation := filepath.Dir(fl)
+				// Use roachprod to create the directory on the remote.
+				err := roachprodRun(ctx, logger, monitorClusterName, "", "", true,
+					os.Stdout, os.Stderr,
+					[]string{fmt.Sprintf("mkdir -p %s", dirLocation)},
+					install.RunOptions{FailOption: install.FailSlow})
+				if err != nil {
+					return errors.Wrapf(err, "Error while creating directory for file %s", fl)
+				}
+			}
+			// Upload the file using roachprod.
+			err := roachprodPut(ctx, logger, monitorClusterName, fl, fl, true)
+			if err != nil {
+				return errors.Wrapf(err, "Error while putting file %s", fl)
+			}
+			return nil
+		})
+	}
+	if err := g.Wait(); err != nil {
+		return err
+	}
+	// Log success if all files were uploaded successfully.
+	fmt.Printf("All the dependencies are uploaded\n")
+	return nil
+}
+
+// processYamlConfig executes the YAML configuration. This execution may happen locally or remotely.
+// This reads the YAML to set the environment variables, and processes the targets.
+func processYamlConfig(
+	ctx context.Context, config yamlConfig, displayOnly bool, userProvidedTargetNames []string,
+) (err error) {
 
 	// Set the environment variables specified in the YAML
 	if err = setEnv(config.Environment, displayOnly); err != nil {
@@ -144,11 +355,7 @@ func processYaml(
 	}
 
 	// Process the targets defined in the YAML
-	if err = processTargets(ctx, config.Targets, displayOnly, userProvidedTargetNames); err != nil {
-		return err
-	}
-
-	return nil
+	return processTargets(ctx, config.Targets, displayOnly, userProvidedTargetNames)
 }
 
 // setEnv sets the environment variables as defined in the YAML configuration.
@@ -197,17 +404,15 @@ func processTargets(
 		}
 		return nil
 	}
-	// Use a WaitGroup to wait for commands executed concurrently
-	wg := sync.WaitGroup{}
-	for _, t := range targets {
+	var g errgroup.Group
+	for _, theTarget := range targets {
+		t := theTarget
 		if shouldSkipTarget(targetNameMap, t, userProvidedTargetNames) {
 			continue
 		}
-		wg.Add(1)
-		go func(t target) {
+		g.Go(func() error {
 			// defer complete the wait group for the dependent targets to proceed
 			defer waitGroupTracker[t.TargetName].Done()
-			defer wg.Done()
 			err := waitForDependentTargets(t, waitGroupTracker, sr)
 			// dependent targets must be success for executing further commands
 			if err == nil {
@@ -216,14 +421,13 @@ func processTargets(
 			if err != nil {
 				fmt.Printf("[%s] Error executing commands: %v\n", t.TargetName, err)
 				sr.setTargetStatus(t.TargetName, targetResultFailure)
-				return
+				return errors.Wrapf(err, "target %s failed", t.TargetName)
 			}
 			sr.setTargetStatus(t.TargetName, targetResultSuccess)
-		}(t)
+			return nil
+		})
 	}
-	// final wait for all targets to complete
-	wg.Wait()
-	return nil
+	return g.Wait()
 }
 
 // waitForDependentTargets waits for the dependent targets and returns an error if ignore_dependent_failure is false
@@ -244,7 +448,7 @@ func waitForDependentTargets(
 			if !t.IgnoreDependentFailure && sr.getTargetStatus(dt) == targetResultFailure {
 				fmt.Printf("[%s] Not proceeding as the dependent target %s was not successful.\n", t.TargetName, dt)
 				// if the dependent target has failed, the current target is marked as failure
-				return fmt.Errorf("[%s] not proceeding as the dependent target %s was not successful", t.TargetName, dt)
+				return errors.Errorf("[%s] not proceeding as the dependent target %s was not successful", t.TargetName, dt)
 			}
 		}
 	}
