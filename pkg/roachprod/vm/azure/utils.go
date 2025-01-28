555ad25f62eed3ed9021bfc13bd7d587b8ee80ad
Ludovic Leroux <ludo.leroux@cockroachlabs.com>
2025-01-28 14:05:55 -0500

    roachprod: refactor providers startup scripts
    Previously, each `vm.Provider{}` had its own complete version of pretty
    much the same startup script. This has led to patches being applied on
    one provider, but not the other ones, and duplicates a lot of logic.
    
    This patch introduces a central `vm.StartupScriptTemplateParts` map that
    contains most of the logic of the startup script templates as generic
    templates fragments. These fragments are called in each provider's
    templates.
    
    Epic: none
    Release note: None

--- pkg/roachprod/vm/azure/utils.go
+++ pkg/roachprod/vm/azure/utils.go
@@ -9,7 +9,10 @@ import (
 	"bytes"
 	"compress/gzip"
 	"encoding/base64"
-	"text/template"
+	"io"
+
+	"github.com/cockroachdb/cockroach/pkg/roachprod/vm"
+	"github.com/cockroachdb/errors"
 )
 
 // Startup script used to find/format/mount all local or attached disks.
@@ -17,91 +20,54 @@ import (
 // created from /mnt/data<disknum> to the mount point.
 // azureStartupArgs specifies template arguments for the setup template.
 type azureStartupArgs struct {
-	RemoteUser           string // The uname for /data* directories.
-	AttachedDiskLun      *int   // Use attached disk, with specified LUN; Use local ssd if nil.
-	DisksInitializedFile string // File to touch when disks are initialized.
-	OSInitializedFile    string // File to touch when OS is initialized.
-	StartupLogs          string // File to redirect startup script output logs.
-	DiskControllerNVMe   bool   // Interface data disk via NVMe
+	vm.StartupArgs
+	AttachedDiskLun    *int // Use attached disk, with specified LUN; Use local ssd if nil.
+	DiskControllerNVMe bool // Interface data disk via NVMe
 }
 
 const azureStartupTemplate = `#!/bin/bash
 
 # Script for setting up a Azure machine for roachprod use.
-# ensure any failure fails the entire script
-set -eux
 
-# Redirect output to stdout/err and a log file
-exec &> >(tee -a {{ .StartupLogs }})
+function setup_disks() {
+	mount_opts="defaults"
 
-# Log the startup of the script with a timestamp
-echo "startup script starting: $(date -u)"
-mount_opts="defaults"
-
-devices=()
+	devices=()
 {{if .DiskControllerNVMe}}
-# Setup nvme network storage, need to remove nvme OS disk from the device list.
-devices=($(realpath -qe /dev/disk/by-id/nvme-* | grep -v "nvme0n1" | sort -u))
+	# Setup nvme network storage, need to remove nvme OS disk from the device list.
+	devices=($(realpath -qe /dev/disk/by-id/nvme-* | grep -v "nvme0n1" | sort -u))
 {{else if .AttachedDiskLun}}
-# Setup network attached storage
-devices=("/dev/disk/azure/scsi1/lun{{.AttachedDiskLun}}")
+	# Setup network attached storage
+	devices=("/dev/disk/azure/scsi1/lun{{.AttachedDiskLun}}")
 {{end}}
 
-if (( ${#devices[@]} == 0 ));
-then
-  # Use /mnt directly.
-  echo "No attached or NVME disks found, creating /mnt/data1"
-  mkdir -p /mnt/data1
-  chown {{.RemoteUser}} /mnt/data1
-else
-  for d in "${!devices[@]}"; do
-    disk=${devices[$d]}
-    mount="/data$((d+1))"
-    sudo mkdir -p "${mount}"
-    sudo mkfs.ext4 -F "${disk}"
-    sudo mount -o "${mount_opts}" "${disk}" "${mount}"
-    echo "${disk} ${mount} ext4 ${mount_opts} 1 1" | sudo tee -a /etc/fstab
-    ln -s "${mount}" "/mnt/$(basename $mount)"
-    tune2fs -m 0 ${disk}
-  done
-  chown {{.RemoteUser}} /data*
-fi
+	if (( ${#devices[@]} == 0 ));
+	then
+		# Use /mnt directly.
+		echo "No attached or NVME disks found, creating /mnt/data1"
+		mkdir -p /mnt/data1
+		chown {{.SharedUser}} /mnt/data1
+		else
+		for d in "${!devices[@]}"; do
+			disk=${devices[$d]}
+			mount="/data$((d+1))"
+			sudo mkdir -p "${mount}"
+			sudo mkfs.ext4 -F "${disk}"
+			sudo mount -o "${mount_opts}" "${disk}" "${mount}"
+			echo "${disk} ${mount} ext4 ${mount_opts} 1 1" | sudo tee -a /etc/fstab
+			ln -s "${mount}" "/mnt/$(basename $mount)"
+			tune2fs -m 0 ${disk}
+		done
+		chown {{.SharedUser}} /data*
+	fi
+	sudo touch {{ .DisksInitializedFile }}
+}
 
-# increase the number of concurrent unauthenticated connections to the sshd
-# daemon. See https://en.wikibooks.org/wiki/OpenSSH/Cookbook/Load_Balancing.
-# By default, only 10 unauthenticated connections are permitted before sshd
-# starts randomly dropping connections.
-sh -c 'echo "MaxStartups 64:30:128" >> /etc/ssh/sshd_config'
-# Crank up the logging for issues such as:
-# https://github.com/cockroachdb/cockroach/issues/36929
-sed -i'' 's/LogLevel.*$/LogLevel DEBUG3/' /etc/ssh/sshd_config
-# N.B. RSA SHA1 is no longer supported in the latest versions of OpenSSH. Existing tooling, e.g.,
-# jepsen still relies on it for authentication.
-sudo sh -c 'echo "PubkeyAcceptedAlgorithms +ssh-rsa" >> /etc/ssh/sshd_config'
-service sshd restart
-# increase the default maximum number of open file descriptors for
-# root and non-root users. Load generators running a lot of concurrent
-# workers bump into this often.
-sh -c 'echo "root - nofile 1048576\n* - nofile 1048576" > /etc/security/limits.d/10-roachprod-nofiles.conf'
+{{ template "head_utils" . }}
+{{ template "apt_packages" . }}
 
-# Send TCP keepalives every minute since GCE will terminate idle connections
-# after 10m. Note that keepalives still need to be requested by the application
-# with the SO_KEEPALIVE socket option.
-cat <<EOF > /etc/sysctl.d/99-roachprod-tcp-keepalive.conf
-net.ipv4.tcp_keepalive_time=60
-net.ipv4.tcp_keepalive_intvl=60
-net.ipv4.tcp_keepalive_probes=5
-EOF
-
-# N.B. Ubuntu 22.04 changed the location of tcpdump to /usr/bin. Since existing tooling, e.g.,
-# jepsen uses /usr/sbin, we create a symlink.
-# See https://ubuntu.pkgs.org/22.04/ubuntu-main-amd64/tcpdump_4.99.1-3build2_amd64.deb.html
-sudo ln -s /usr/bin/tcpdump /usr/sbin/tcpdump
-
-# Uninstall unattended-upgrades
-systemctl stop unattended-upgrades
-sudo rm -rf /var/log/unattended-upgrades
-apt-get purge -y unattended-upgrades
+# Initialize disks.
+setup_disks
 
 # Disable Hyper-V Time Synchronization device
 # See https://www.cockroachlabs.com/docs/stable/deploy-cockroachdb-on-microsoft-azure#step-3-synchronize-clocks
@@ -109,53 +75,18 @@ curl -O https://raw.githubusercontent.com/torvalds/linux/master/tools/hv/lsvmbus
 ts_dev_id=$(python3 lsvmbus -vv | grep -w "Time Synchronization" -A 3 | grep "Device_ID" | awk -F '[{}]' '{print $2}')
 echo $ts_dev_id | sudo tee /sys/bus/vmbus/drivers/hv_utils/unbind
 
-# Override the chrony config. In particular,
-# log aggressively when clock is adjusted (0.01s)
-# and exclusively use a single time server.
-sudo cat <<EOF > /etc/chrony/chrony.conf
-keyfile /etc/chrony/chrony.keys
-commandkey 1
-driftfile /var/lib/chrony/chrony.drift
-log tracking measurements statistics
-logdir /var/log/chrony
-maxupdateskew 100.0
-dumponexit
-dumpdir /var/lib/chrony
-logchange 0.01
-hwclockfile /etc/adjtime
-rtcsync
-server time1.google.com iburst
-server time2.google.com iburst
-server time3.google.com iburst
-server time4.google.com iburst
-makestep 0.1 3
-EOF
+{{ template "ulimits" . }}
+{{ template "tcpdump" . }}
+{{ template "keepalives" . }}
+{{ template "cron_utils" . }}
+{{ template "chrony_utils" . }}
+{{ template "timers_services_utils" . }}
+{{ template "core_dumps_utils" . }}
+{{ template "hostname_utils" . }}
+{{ template "fips_utils" . }}
+{{ template "ssh_utils" . }}
 
-sudo /etc/init.d/chrony restart
-sudo chronyc -a waitsync 30 0.01 | sudo tee -a /root/chrony.log
-
-# Enable core dumps
-cat <<EOF > /etc/security/limits.d/core_unlimited.conf
-* soft core unlimited
-* hard core unlimited
-root soft core unlimited
-root hard core unlimited
-EOF
-
-mkdir -p /mnt/data1/cores
-chmod a+w /mnt/data1/cores
-CORE_PATTERN="/mnt/data1/cores/core.%e.%p.%h.%t"
-echo "$CORE_PATTERN" > /proc/sys/kernel/core_pattern
-sed -i'~' 's/enabled=1/enabled=0/' /etc/default/apport
-sed -i'~' '/.*kernel\\.core_pattern.*/c\\' /etc/sysctl.conf
-echo "kernel.core_pattern=$CORE_PATTERN" >> /etc/sysctl.conf
-sysctl --system  # reload sysctl settings
-
-sudo sed -i 's/#LoginGraceTime .*/LoginGraceTime 0/g' /etc/ssh/sshd_config
-sudo service ssh restart
-
-touch {{ .DisksInitializedFile }}
-touch {{ .OSInitializedFile }}
+sudo touch {{ .OSInitializedFile }}
 `
 
 // evalStartupTemplate evaluates startup template defined above and returns
@@ -173,15 +104,22 @@ func evalStartupTemplate(args azureStartupArgs) (string, error) {
 	cloudInit := bytes.NewBuffer(nil)
 	encoder := base64.NewEncoder(base64.StdEncoding, cloudInit)
 	gz := gzip.NewWriter(encoder)
-	t := template.Must(template.New("start").Parse(azureStartupTemplate))
-	if err := t.Execute(gz, args); err != nil {
-		return "", err
+
+	err := generateStartupScript(gz, args)
+	if err != nil {
+		return "", errors.Wrapf(err, "unable to generate startup script")
 	}
+
 	if err := gz.Close(); err != nil {
 		return "", err
 	}
+
 	if err := encoder.Close(); err != nil {
 		return "", err
 	}
 	return cloudInit.String(), nil
+}
+
+func generateStartupScript(w io.Writer, args azureStartupArgs) error {
+	return vm.GenerateStartupScript(w, azureStartupTemplate, args)
 }
