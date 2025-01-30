daa0a602e58adc9082c0fcb1cc10170a428658a7
Steven Danna <danna@cockroachlabs.com>
2025-01-30 15:54:32 +0000

    concurrency: save IgnoredSeqNums for unreplicated locks
    The ignoredSeqNums slice is the last-seen ignoredSeqNums
    from the acquiring transaction. At a given point in time,
    the transaction may have a larger set of ignoredSeqNums.
    
    The intent is to use this for potential re-acquisition of unreplicated
    locks as replicated locks. When doing this cquisition, we want to
    avoid clobbering existing replicatd locks.
    
    Motivating Example:
    
        replicated lock @ seq1 (no ignored seq nums)
        unreplicated lock @ seq2 (seq 1 ignored)
    
    In this case, when we go to flush our unreplicated lock as replicated
    lock we need to ignore the lock at seq1.
    
    But, then consider this case:
    
        replicated lock @ seq1 (no ignored seq nums)
        unreplicated lock @ seq2 (no ignored seq nums)
        unreplicated lock @ seq3 (seq 2 ignored)
    
    Here, when we go to flush our unreplicated lock@seq 3 as a replicated
    lock, we want to actually preserve the seq 1 lock, we can't do without
    _some_ kind of tracking of the ignore sequence numbers.
    
    It is sufficient to store the ignored sequence number at the time of
    acquisition. For instance, in the example, consider that if seq3 has
    been rolled back since we stored the ignoredSeqNums, that is OK, we
    have a lock at seq 1 still. If seq 1 has been rolled back since we
    stored the ingoredSeqNums, that is also OK, because future requests
    will include it in the ignored seq number list and we also know there
    is no way to rollback 1 without rolling back 3.
    
    We may ask whether the min sequence number in strengths above is
    sufficient. But I don't believe it is. In the cases above, the minimum
    sequence of the held unreplicated lock cannot tell you whether or not
    you need to preserve the replicated lock at seq1 or overwrite it.
    
    Informs #139141
    
    Release Note: None

--- pkg/kv/kvserver/concurrency/lock_table_test.go
+++ pkg/kv/kvserver/concurrency/lock_table_test.go
@@ -2075,8 +2075,8 @@ func TestKeyLocksSafeFormat(t *testing.T) {
 	holder.txn = &enginepb.TxnMeta{ID: uuid.NamespaceDNS}
 	holder.unreplicatedInfo.init()
 	holder.unreplicatedInfo.ts = hlc.Timestamp{WallTime: 123, Logical: 7}
-	require.NoError(t, holder.unreplicatedInfo.acquire(lock.Exclusive, 1))
-	require.NoError(t, holder.unreplicatedInfo.acquire(lock.Shared, 3))
+	require.NoError(t, holder.unreplicatedInfo.acquire(lock.Exclusive, 1, nil))
+	require.NoError(t, holder.unreplicatedInfo.acquire(lock.Shared, 3, nil))
 	replTS := hlc.Timestamp{WallTime: 125, Logical: 1}
 	holder.replicatedInfo.acquire(lock.Intent, replTS)
 	holder.replicatedInfo.acquire(lock.Shared, replTS)
@@ -2103,11 +2103,11 @@ func TestKeyLocksSafeFormatMultipleLockHolders(t *testing.T) {
 	holder1.txn = &enginepb.TxnMeta{ID: uuid.NamespaceDNS}
 	holder1.unreplicatedInfo.init()
 	holder1.unreplicatedInfo.ts = hlc.Timestamp{WallTime: 123, Logical: 7}
-	require.NoError(t, holder1.unreplicatedInfo.acquire(lock.Shared, 3))
+	require.NoError(t, holder1.unreplicatedInfo.acquire(lock.Shared, 3, nil))
 	holder2.txn = &enginepb.TxnMeta{ID: uuid.NamespaceURL}
 	holder2.unreplicatedInfo.init()
 	holder2.unreplicatedInfo.ts = hlc.Timestamp{WallTime: 125, Logical: 1}
-	require.NoError(t, holder2.unreplicatedInfo.acquire(lock.Shared, 6))
+	require.NoError(t, holder2.unreplicatedInfo.acquire(lock.Shared, 6, nil))
 	require.EqualValues(t,
 		" lock: ‹\"KEY\"›\n"+
 			"  holders: txn: 6ba7b810-9dad-11d1-80b4-00c04fd430c8 epoch: 0, iso: Serializable, info: unrepl [(str: Shared seq: 3)]\n"+
@@ -2133,7 +2133,7 @@ func TestKeyLocksSafeFormatWaitQueue(t *testing.T) {
 	holder.txn = &enginepb.TxnMeta{ID: uuid.NamespaceDNS}
 	holder.unreplicatedInfo.init()
 	holder.unreplicatedInfo.ts = hlc.Timestamp{WallTime: 123, Logical: 7}
-	require.NoError(t, holder.unreplicatedInfo.acquire(lock.Shared, 3))
+	require.NoError(t, holder.unreplicatedInfo.acquire(lock.Shared, 3, nil))
 	waiter := queuedGuard{
 		guard:  newLockTableGuardImpl(),
 		active: true,
