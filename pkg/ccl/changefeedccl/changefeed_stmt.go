965fecf2ffda69f58bc26ae1bd14b06a6720611b
Wenyi Hu <wenyi@cockroachlabs.com>
2025-02-04 12:06:00 -0500

    changefeedccl: restore fine grained checkpoint from job proto
    As part of the fine-grained checkpointing project, changefeed now saves resolved
    spans for each span above the high-water mark. This patch finalizes the
    restoration process for fine-grained checkpoints. Upon resuming, changefeed
    reloads span level checkpoints from the job proto and restores progress during
    startup.
    Restoration is required in three key areas:
    1. When aggregators are initialized, including the initialization of each
    aggregator frontier.
    2. When `rangeFeedResumeFrontier` is initialized during every kvfeed.run.
    3. When kvfeed determines which checkpointed spans to filter out from those
    requiring backfill.
    
    Note: This remains unused for now until issue #137692 is resolved.
    NB: `reconcileJobStateWithLocalState` is intentionally left unchanged. We
    decided to update it as part of issue #137692, as it depends on
    `cachedState.SetCheckpoint`.
    
    Resolves: #137693
    Release Note: None

--- pkg/ccl/changefeedccl/changefeed_stmt.go
+++ pkg/ccl/changefeedccl/changefeed_stmt.go
@@ -1475,6 +1475,7 @@ func reloadDest(ctx context.Context, id jobspb.JobID, execCfg *sql.ExecutorConfi
 
 // reconcileJobStateWithLocalState ensures that the job progress information
 // is consistent with the state present in the local state.
+// TODO(#137692): SetCheckpoint should take in old and new checkpoints proto.
 func reconcileJobStateWithLocalState(
 	ctx context.Context, jobID jobspb.JobID, localState *cachedState, execCfg *sql.ExecutorConfig,
 ) error {
