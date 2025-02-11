aeefd81c007906ba1bf723b4368c1761a996c7cb
Herko Lategan <herko@cockroachlabs.com>
2025-02-11 18:10:31 +0000

    microbench-ci: context based checkout
    Previously, the checkout steps of the microbenchmark workflow specified
    revisions explicitly. It should rather use the defaults that will change based
    on if the workflow is triggered on "pull_request" or "pull_request_target". The
    latter will check out the base revision by default, while "pull_request" checks
    out the PR head revision by default. Hence, if changes to the microbenchmark
    workflow need to be tested the flow can be updated to "pull_request", and before
    merging changed back to "pull_request_target" (that is necessary for the
    permissions required for commenting on a PR).
    
    The microbench-ci utility will now be built for each job that requires it, as
    the revision is dependent on the context. Build times for the utility is low,
    and as it is done through eng-flow there will be some caching. The test binaries
    still get cached on GCS, as developers need access to these in case they wish to
    reproduce the results.
    
    Epic: None
    Release note: None

--- build/github/microbenchmarks/post.sh
+++ build/github/microbenchmarks/post.sh
@@ -10,19 +10,11 @@ set -euxo pipefail
 working_dir=$(mktemp -d)
 storage_bucket="$BUCKET"
 
-# Build microbenchmark CI utility (base version)
-bazel build --config=crosslinux $(./build/github/engflow-args.sh) \
-  --jobs 100 \
-  --bes_keywords integration-test-artifact-build \
-  //pkg/cmd/microbench-ci
-
-bazel_bin=$(bazel info bazel-bin --config=crosslinux)
-
 # Grab the summary from the previous step
 gcloud storage cp "gs://${storage_bucket}/results/${HEAD_SHA}/${BUILD_ID}/summary.md" "$working_dir/summary.md"
 
 # Post summary to GitHub on the PR
-"$bazel_bin/pkg/cmd/microbench-ci/microbench-ci_/microbench-ci" post \
+./build/github/microbenchmarks/util.sh post \
   --github-summary="$working_dir/summary.md" \
   --pr-number="$PR_NUMBER" \
   --repo="$REPO"
