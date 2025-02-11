4b484d9e8734f28d6bf1a502f5cb6a543c5b04fc
Rafi Shamim <rafi@cockroachlabs.com>
2025-02-11 16:43:02 -0500

    sql: crdb_internal.zones no longer fails with offline database
    Release note (bug fix): Fixed a bug that could prevent SHOW CREATE TABLE
    from working if a database was offline (e.g., due to a RESTORE on that
    database).

--- pkg/sql/crdb_internal.go
+++ pkg/sql/crdb_internal.go
@@ -5016,7 +5016,7 @@ CREATE TABLE crdb_internal.zones (
 
 			var table catalog.TableDescriptor
 			if zs.Database != "" {
-				database, err := p.Descriptors().ByIDWithoutLeased(p.txn).WithoutNonPublic().Get().Database(ctx, descpb.ID(id))
+				database, err := p.Descriptors().ByIDWithoutLeased(p.txn).Get().Database(ctx, descpb.ID(id))
 				if err != nil {
 					return err
 				}
@@ -5026,7 +5026,7 @@ CREATE TABLE crdb_internal.zones (
 					continue
 				}
 			} else if zoneSpecifier.TableOrIndex.Table.ObjectName != "" {
-				tableEntry, err := p.Descriptors().ByIDWithoutLeased(p.txn).WithoutDropped().Get().Table(ctx, descpb.ID(id))
+				tableEntry, err := p.Descriptors().ByIDWithoutLeased(p.txn).Get().Table(ctx, descpb.ID(id))
 				if err != nil {
 					return err
 				}
