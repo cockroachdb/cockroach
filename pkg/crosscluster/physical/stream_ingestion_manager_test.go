1012c64a0300bcb6ac07d7e79deffb353a951b0e
Rafi Shamim <rafi@cockroachlabs.com>
2025-02-13 02:11:19 -0500

    spanconfigptsreader: break deps on testing packages
    Release note: None

--- pkg/crosscluster/physical/stream_ingestion_manager_test.go
+++ pkg/crosscluster/physical/stream_ingestion_manager_test.go
@@ -193,7 +193,7 @@ func TestRevertTenantToTimestampPTS(t *testing.T) {
 		t.Logf("udating PTS reader cache to %s", asOf)
 		require.NoError(
 			t,
-			spanconfigptsreader.TestingRefreshPTSState(ctx, t, ptsReader, asOf),
+			spanconfigptsreader.TestingRefreshPTSState(ctx, ptsReader, asOf),
 		)
 		require.NoError(t, repl.ReadProtectedTimestampsForTesting(ctx))
 
