d6030bb20a12c9c3f6d484e7dfb0bf215d3191e0
Kyle Wong <37189875+kyle-a-wong@users.noreply.github.com>
2025-02-14 15:08:58 -0500

    server,sqlstats: add ssRemote.SQLStats
    ssRemote.SQLStats does implements SSDrainer and does a
    cluster wide DrainStats. This is done using a new
    gRPC via /_status/drainstats
    
    Epic: CRDB-45771
    Release note: None

---
+++ pkg/sql/sqlstats/ssremote/ssremote_provider.go
@@ -0,0 +1,60 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package ssremote
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/server/serverpb"
+	"github.com/cockroachdb/cockroach/pkg/sql"
+	"github.com/cockroachdb/cockroach/pkg/sql/appstatspb"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlstats"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+)
+
+// SQLStats is a remote implementation of the sqlstats.SSDrainer interface.
+type SQLStats struct {
+	execConfig *sql.ExecutorConfig
+}
+
+var _ sqlstats.SSDrainer = &SQLStats{}
+
+func New(execCfg *sql.ExecutorConfig) *SQLStats {
+	return &SQLStats{execConfig: execCfg}
+}
+
+// DrainStats drains the stats from the cluster using the DrainSqlStats RPC.
+// See statusServer.DrainSqlStats for more details.
+func (ss *SQLStats) DrainStats(
+	ctx context.Context,
+) (
+	[]*appstatspb.CollectedStatementStatistics,
+	[]*appstatspb.CollectedTransactionStatistics,
+	int64,
+) {
+	resp, err := ss.execConfig.SQLStatusServer.DrainSqlStats(ctx, &serverpb.DrainSqlStatsRequest{})
+	if err != nil {
+		log.Warningf(ctx, "Error calling statusServer.DrainSqlStats. err=%s", err.Error())
+		return nil, nil, 0
+	}
+	stmtStats := make([]*appstatspb.CollectedStatementStatistics, 0, len(resp.Statements))
+	txnStats := make([]*appstatspb.CollectedTransactionStatistics, 0, len(resp.Transactions))
+	for _, stmt := range resp.Statements {
+		stmtStats = append(stmtStats, &stmt)
+	}
+	for _, txn := range resp.Transactions {
+		txnStats = append(txnStats, &txn)
+	}
+
+	log.Infof(ctx, "DrainSqlStats done stmts=%d txns=%d", len(stmtStats), len(txnStats))
+	return stmtStats, txnStats, resp.FingerprintCount
+}
+
+// Reset Resets in-memory stats for cluster.
+func (ss *SQLStats) Reset(ctx context.Context) error {
+	_, err := ss.execConfig.SQLStatusServer.ResetSQLStats(ctx, &serverpb.ResetSQLStatsRequest{ResetPersistedStats: false})
+	return err
+}
