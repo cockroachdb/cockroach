2eb37c856850e368a901827b4295a8c66f12c795
Bergin Dedej <bergin.dedej@cockroachlabs.com>
2025-02-18 17:56:05 -0500

    sql: added support for row level security to pg_class
    Previously the `pg_class` table was not populating the `relrowlevelsecurity`
    and `relforcerowlevelsecurity` columns.  With these code changes these columns
    are now populated accordingly.
    
    Epic: CRDB-45205
    Informs: #136702
    Release note: None

--- pkg/sql/pg_catalog.go
+++ pkg/sql/pg_catalog.go
@@ -794,13 +794,13 @@ https://www.postgresql.org/docs/9.5/catalog-pg-class.html`,
 			tree.DNull,      // relacl
 			relOptions,      // reloptions
 			// These columns were automatically created by pg_catalog_test's missing column generator.
-			tree.DNull,                 // relforcerowsecurity
+			tree.MakeDBool(tree.DBool(table.IsRowLevelSecurityForced())), // relforcerowsecurity
 			tree.DNull,                 // relispartition
 			tree.DNull,                 // relispopulated
 			tree.NewDString(replIdent), // relreplident
 			tree.DNull,                 // relrewrite
-			tree.DNull,                 // relrowsecurity
-			tree.DNull,                 // relpartbound
+			tree.MakeDBool(tree.DBool(table.IsRowLevelSecurityEnabled())), // relrowsecurity
+			tree.DNull, // relpartbound
 			// These columns were automatically created by pg_catalog_test's missing column generator.
 			tree.DNull, // relminmxid
 		); err != nil {
