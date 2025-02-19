6760087620c29531fcd9a9840abcbda40b518546
sumeerbhola <sumeer@cockroachlabs.com>
2025-02-19 18:54:22 -0500

    mma: misc small cleanups
    Informs #103320
    
    Epic: CRDB-25222
    
    Release note: None

--- pkg/kv/kvserver/allocator/mma/cluster_state.go
+++ pkg/kv/kvserver/allocator/mma/cluster_state.go
@@ -94,9 +94,8 @@ type pendingReplicaChange struct {
 	startTime time.Time
 
 	// When the change is known to be enacted based on the authoritative
-	// information received from the leaseholder, this value is set, so that
-	// even if the store with a replica affected by this pending change does not
-	// tell us about the enactment, we can garbage collect this change.
+	// information received from the leaseholder, this value is set, so that we
+	// can garbage collect this change.
 	enactedAtTime time.Time
 }
 
@@ -309,20 +308,15 @@ type storeState struct {
 		// - leaseholder provided state shows that the change has been enacted, it
 		//   will set enactedAtTime, but not remove from loadPendingChanges since
 		//   this pending change is still needed to compensate for the store
-		//   reported load.
-		//
-		// Unilateral removal from loadPendingChanges happens if the load reported
-		// by the store shows that this pending change has been enacted. We no
-		// longer need to adjust the load for this pending change.
-		//
-		// Removal from loadPendingChanges also happens if sufficient duration has
-		// elapsed from enactedAtTime.
+		//   reported load. Once computePendingChangesReflectedInLatestLoad
+		//   determines that the latest load state must include the pending
+		//   change, it will be removed.
 		//
 		// In summary, guaranteed removal of a load pending change because of
 		// failure of enactment or GC happens via clusterState.pendingChanges.
 		// Only the case where enactment happened is where a load pending change
 		// can live on -- but since that will set enactedAtTime, we are guaranteed
-		// to always remove it.
+		// to eventually remove it.
 		loadPendingChanges map[changeID]*pendingReplicaChange
 
 		enactedHistory storeEnactedHistory
@@ -331,7 +325,7 @@ type storeState struct {
 
 		// replicas is computed from the authoritative information provided by
 		// various leaseholders in storeLeaseholderMsgs and adjusted for pending
-		// changes in cluserState.pendingChanges/rangeState.pendingChanges.
+		// changes in clusterState.pendingChanges/rangeState.pendingChanges.
 		replicas map[roachpb.RangeID]replicaState
 	}
 	// This is a locally incremented seqnum which is incremented whenever the
@@ -437,12 +431,6 @@ type rangeState struct {
 
 	// TODO(sumeer): populate and use.
 	diversityIncreaseLastFailedAttempt time.Time
-
-	// lastHeardTime is the latest time when this range was heard about from any
-	// store, via storeLeaseholderMsg or storeLoadMsg. Despite the best-effort
-	// GC it is possible we will miss something. If this lastHeardTime is old
-	// enough, use some other source to verify that this range still exists.
-	lastHeardTime time.Time
 }
 
 // clusterState is the state of the cluster known to the allocator, including
@@ -634,7 +622,6 @@ var _ = rangeState{}.conf
 var _ = rangeState{}.pendingChanges
 var _ = rangeState{}.constraints
 var _ = rangeState{}.diversityIncreaseLastFailedAttempt
-var _ = rangeState{}.lastHeardTime
 var _ = clusterState{}.nodes
 var _ = clusterState{}.stores
 var _ = clusterState{}.ranges
