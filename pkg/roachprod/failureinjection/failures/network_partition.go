DarrylWong/fork

roachprod/failure-injection: add failure-injection registry
This registry will allow for future usage of the failure
injection library through the CLI and the failure injection
planner/controller.

pkg/roachprod/failureinjection/failures/network_partition.go
@@ -57,6 +57,10 @@ func MakeIPTablesPartitionNode(clusterName string, l *logger.Logger, secure bool
 
 const IPTablesNetworkPartitionName = "iptables-network-partition"
 
+func registerIPTablesPartitionNode(r *FailureRegistry) {
+	r.add(IPTablesNetworkPartitionName, NetworkPartitionArgs{}, MakeIPTablesPartitionNode)
+}
+
 func (f *IPTablesPartitionNode) Description() string {
 	return IPTablesNetworkPartitionName
 }

cockroachdb/cockroach

pkg/roachprod/failureinjection/failures/network_partition.go
@@ -65,6 +65,10 @@ func MakeIPTablesPartitionFailure(
 
 const IPTablesNetworkPartitionName = "iptables-network-partition"
 
+func registerIPTablesPartitionFailure(r *FailureRegistry) {
+	r.add(IPTablesNetworkPartitionName, NetworkPartitionArgs{}, MakeIPTablesPartitionFailure)
+}
+
 func (f *IPTablesPartitionFailure) Description() string {
 	return IPTablesNetworkPartitionName
 }