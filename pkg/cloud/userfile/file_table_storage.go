87a5cf6127de670d6aea440b9e448038ae614bee
Jeff Swenson <jeffswenson@betterthannull.com>
2025-02-20 13:46:54 -0500

    cloud: cloud.Delete returns nil if the object does not exist
    Previously, the external storage providers had different behavior when
    deleting an object that does not exist. S3 treats deleting a
    non-existent file as OK. All other storage providers returned an error.
    This change updates all storage providers so they match the S3 behavior.
    
    An alternative design would be to have Delete return ErrFileNotFound. I
    was unable to find an efficient way to implement that in the S3 storage
    provider. The S3 delete response provides no hint as to whether there
    was an object at the path. So generating ErrFileNotFound would require
    reading the path before issuing the delete.
    
    Release note: none
    Epic: none

--- pkg/cloud/userfile/file_table_storage.go
+++ pkg/cloud/userfile/file_table_storage.go
@@ -208,6 +208,11 @@ func checkBaseAndJoinFilePath(prefix, basename string) (string, error) {
 	return path.Join(prefix, basename), nil
 }
 
+// isNotExistErr checks if the error indicates a file does not exist
+func isNotExistErr(err error) bool {
+	return oserror.IsNotExist(err)
+}
+
 // ReadFile implements the ExternalStorage interface and returns the contents of
 // the file stored in the user scoped FileToTableSystem.
 func (f *fileTableStorage) ReadFile(
@@ -218,11 +223,9 @@ func (f *fileTableStorage) ReadFile(
 		return nil, 0, err
 	}
 	reader, size, err := f.fs.ReadFile(ctx, filepath, opts.Offset)
-	if oserror.IsNotExist(err) {
-		return nil, 0, errors.Wrapf(cloud.ErrFileDoesNotExist,
-			"file %s does not exist in the UserFileTableSystem", filepath)
+	if err != nil && isNotExistErr(err) {
+		return nil, 0, cloud.WrapErrFileDoesNotExist(err, "file does not exist in the UserFileTableSystem")
 	}
-
 	return reader, size, err
 }
 
@@ -276,7 +279,11 @@ func (f *fileTableStorage) Delete(ctx context.Context, basename string) error {
 	if err != nil {
 		return err
 	}
-	return f.fs.DeleteFile(ctx, filepath)
+	err = f.fs.DeleteFile(ctx, filepath)
+	if isNotExistErr(err) {
+		return nil
+	}
+	return err
 }
 
 // Size implements the ExternalStorage interface and returns the size of the
