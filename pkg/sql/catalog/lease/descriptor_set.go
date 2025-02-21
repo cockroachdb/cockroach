2e168924dffb265ac20b865aff1fe4d27c13e471
Faizan Qazi <faizan@cockroachlabs.com>
2025-02-20 23:28:44 -0500

    catalog/lease: avoid clearing session from stale versions
    Previously, we supported both expiry and session-based leases. Stale
    session-based leases were expired by clearing the session and setting an
    expiration time. This was necessary during the dual-write phase to
    support both types. Now that expiry-based leasing has been removed, we
    only need to check the expiration time to determine if a descriptor is
    stale. This patch avoids clearing the session and relies solely on the
    expiration to identify stale versions.
    
    Fixes: #141801
    
    Release note: None

--- pkg/sql/catalog/lease/descriptor_set.go
+++ pkg/sql/catalog/lease/descriptor_set.go
@@ -111,7 +111,7 @@ func (l *descriptorSet) findPreviousToExpire(dropped bool) *descriptorVersionSta
 	// If this version is not active, then it will go away on its own
 	// from the leases table, so no expiry needs to be setup. If the
 	// session is already cleared then an expiry has been setup too.
-	if exp.mu.refcount == 0 || exp.mu.session == nil {
+	if exp.mu.refcount == 0 || !exp.mu.expiration.IsEmpty() {
 		return nil
 	}
 	return exp
