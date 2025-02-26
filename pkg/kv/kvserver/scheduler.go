f92e85373a618ab5ba0655ef6f8bf66cda2f428a
Tobias Grieger <tobias.b.grieger@gmail.com>
2025-02-26 23:11:07 +0100

    kvserver: launch raft scheduler goroutines at caller

--- pkg/kv/kvserver/scheduler.go
+++ pkg/kv/kvserver/scheduler.go
@@ -307,21 +307,25 @@ func (s *raftScheduler) Start(stopper *stop.Stopper) {
 
 	for _, shard := range s.shards {
 		s.done.Add(shard.numWorkers)
+		f := func(ctx context.Context, hdl *stop.Handle) {
+			ctx, release := hdl.Activate(ctx)
+			defer release(ctx, hdl)
+			defer s.done.Done()
+			shard.worker(ctx, s.processor, s.metrics)
+		}
+
 		for i := 0; i < shard.numWorkers; i++ {
-			if err := stopper.RunAsyncTaskEx(ctx,
+			hdl, err := stopper.GetHandle(ctx,
 				stop.TaskOpts{
 					TaskName: "raft-worker",
 					// This task doesn't reference a parent because it runs for the server's
 					// lifetime.
 					SpanOpt: stop.SterileRootSpan,
-				},
-				func(ctx context.Context) {
-					shard.worker(ctx, s.processor, s.metrics)
-					s.done.Done()
-				},
-			); err != nil {
+				})
+			if err != nil {
 				s.done.Done()
 			}
+			go f(ctx, hdl)
 		}
 	}
 }
