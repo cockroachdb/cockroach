91fddcf8ad6f7416f09a840a5e4a61c8f1b95331
Pavel Kalinnikov <pavel@cockroachlabs.com>
2025-02-26 00:56:12 +0000

    logstore: load last entry term
    We already read the last entry term when initializing RawNode, so there
    is no additional overhead, except the fact that this entry is now not
    stashed into the raft entry cache. There appears to be low benefit in
    putting it into the cache though. Typically, we wouldn't need exactly
    the LastIndex entry post startup.
    
    Eventually, on startup we will load the term cache instead of the last
    entry.
    
    Epic: none
    Release note: none

--- pkg/kv/kvserver/replica_init.go
+++ pkg/kv/kvserver/replica_init.go
@@ -309,8 +309,8 @@ func (r *Replica) initRaftMuLockedReplicaMuLocked(s kvstorage.LoadedReplicaState
 		r.flowControlV2.ForceFlushIndexChangedLocked(context.TODO(), r.shMu.state.ForceFlushIndex.Index)
 	}
 	r.shMu.raftTruncState = s.TruncState
-	r.shMu.lastIndexNotDurable = s.LastIndex
-	r.shMu.lastTermNotDurable = invalidLastTerm
+	r.shMu.lastIndexNotDurable = s.LastEntryID.Index
+	r.shMu.lastTermNotDurable = s.LastEntryID.Term
 
 	// Initialize the Raft group. This may replace a Raft group that was installed
 	// for the uninitialized replica to process Raft requests or snapshots.
