d588d4a58055b95270616fbbefd3e5618c0ce233
Radu Berinde <radu@cockroachlabs.com>
2025-02-27 13:00:24 -0800

    goschedstats: use runtime.NumRunnableGoroutines
    Use the implementation we have added to our Go fork and remove all
    other implementations.
    
    Fixes: #141977
    Release note: None

--- pkg/util/goschedstats/runnable_disabled.go
+++ pkg/util/goschedstats/runnable_disabled.go
@@ -7,7 +7,7 @@
 // to the internal goroutine machinery. This would be because we are using an
 // upstream Go (not our fork) and are running a version after 1.23.
 //
-//go:build !bazel && gc && go1.23
+//go:build !bazel
 
 package goschedstats
 
