ea1b15726d73f592ecd8d3af157b1a913ad8238d
DarrylWong <darryl@cockroachlabs.com>
2025-03-03 11:17:05 -0500

    mixedversion: add MinBootstrapVersion option
    This change allows test writers to set a MinBootstrapVersion.
    This option will tell to the mixed version framework to not
    generate any plans that create a cluster on any versions
    older than MinBootstrapVersion.
    
    With this new option, the http-register-routes/mixed-versions test
    now sets the MinBootstrapVersion to 24.2, the oldest release that
    has the fix contained in #138342.

--- pkg/cmd/roachtest/tests/versionupgrade.go
+++ pkg/cmd/roachtest/tests/versionupgrade.go
@@ -316,6 +316,12 @@ func runHTTPRestart(ctx context.Context, t test.Test, c cluster.Cluster) {
 	mvt := mixedversion.NewTest(ctx, t, t.L(), c,
 		c.CRDBNodes(),
 		mixedversion.AlwaysUseLatestPredecessors,
+		// We set the min bootstrap version to v24.2, as the fix for the
+		// race condition was only backported to v24.2+ but exists as early
+		// as v23.2. We use this over setting min supported version as this
+		// test is concerned about testing cluster startup. We don't want the
+		// framework to bootstrap the cluster before we can start running hooks.
+		mixedversion.MinimumBootstrapVersion("v24.2.0"),
 	)
 
 	// Any http request requiring auth will do.
