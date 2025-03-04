619c2469e5f739a9c8f6f481c5b8fe93e08303ea
Tobias Grieger <tobias.b.grieger@gmail.com>
2025-03-04 11:59:52 +0100

    admission: fail open when grunning unsupported
    See https://github.com/cockroachdb/cockroach/issues/142262.
    
    Epic: none
    
    Release note: None

--- pkg/util/admission/kv_slot_adjuster.go
+++ pkg/util/admission/kv_slot_adjuster.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/util/grunning"
 	"github.com/cockroachdb/cockroach/pkg/util/metric"
 )
 
@@ -116,5 +117,7 @@ func (kvsa *kvSlotAdjuster) CPULoad(runnable int, procs int, samplePeriod time.D
 }
 
 func (kvsa *kvSlotAdjuster) isOverloaded() bool {
-	return kvsa.granter.usedSlots >= kvsa.granter.totalSlots && !kvsa.granter.skipSlotEnforcement
+	return kvsa.granter.usedSlots >= kvsa.granter.totalSlots && !kvsa.granter.skipSlotEnforcement &&
+		// See https://github.com/cockroachdb/cockroach/issues/142262.
+		grunning.Supported()
 }
