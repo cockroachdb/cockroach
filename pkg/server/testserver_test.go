9c3b62702ae8506581848050cd5f252967c0904b
Rafi Shamim <rafi@cockroachlabs.com>
2025-03-04 18:06:42 -0500

    server: don't override leader fortification setting in test
    Since 1 is the default value now, we should have this benchmark test
    with that value in order to accurately measure how long testserver
    startup takes.
    
    Release note: None

--- pkg/server/testserver_test.go
+++ pkg/server/testserver_test.go
@@ -10,7 +10,6 @@ import (
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/base"
-	"github.com/cockroachdb/cockroach/pkg/kv/kvserver"
 	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/eval"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
@@ -49,9 +48,6 @@ func BenchmarkTestServerStartup(b *testing.B) {
 						},
 					},
 				}
-				// Disable leader fortification as it currently causes large variability in runtime.
-				kvserver.RaftLeaderFortificationFractionEnabled.Override(context.Background(), &args.Settings.SV, 0.0)
-
 				s := serverutils.StartServerOnly(b, args)
 				s.Stopper().Stop(context.Background())
 			}
@@ -77,8 +73,6 @@ func TestServerStartup(t *testing.T) {
 			DefaultTestTenant: tc.tenantOpt,
 			Settings:          cluster.MakeTestingClusterSettings(),
 		}
-		// Disable leader fortification as it currently causes large variability in runtime.
-		kvserver.RaftLeaderFortificationFractionEnabled.Override(context.Background(), &args.Settings.SV, 0.0)
 
 		t.Run(tc.name, func(t *testing.T) {
 			testutils.RunTrueAndFalse(t, "slim", func(t *testing.T, useSlimServer bool) {
