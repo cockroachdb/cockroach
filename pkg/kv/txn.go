d26ad4a163a260fda1aa62a7336bd84df8fb45c6
Rafi Shamim <rafi@cockroachlabs.com>
2025-03-11 22:36:00 -0400

    kv, sql: allow configuration of max retry limit; use it for some operations
    79219f705fc76eae5753ea5e28b7077dfacfd213 added a cluster setting that
    limits the maximum number of times a transaction will be retried
    internally in the KV layer.
    
    This patch adds more configurability by adding a function that can
    configure the max retry limit for a specific transaction. If nothing is
    set this way, the limit falls back to using the cluster setting.
    
    As part of this, an error message was tweaked to no longer mention the
    cluster setting. Since the setting was internal anyway, it makes more
    sense not to mention its name.
    
    Finally, the new max retry function is used in a few places that already
    have its own retry logic to retry a transaction with a smaller batch
    size. This should help reduce latency of these batch operations.
    
    Release note (performance improvement): Index backfills and
    row-level-TTL deletions that encounter transaction contention will now
    be retried with smaller batch sizes more quickly, which reduces the
    latency of these jobs under high contention workloads.

--- pkg/kv/txn.go
+++ pkg/kv/txn.go
@@ -103,6 +103,11 @@ type Txn struct {
 		// The txn has to be committed by this deadline. A zero value indicates no
 		// deadline.
 		deadline hlc.Timestamp
+
+		// maxAutoRetries is the number of times this transaction can be retried
+		// automatically in the KV layer. If it is zero, then the
+		// kv.transaction.internal.max_auto_retries setting is used.
+		maxAutoRetries int
 	}
 
 	// admissionHeader is used for admission control for work done in this
@@ -260,6 +265,28 @@ func (txn *Txn) ID() uuid.UUID {
 	txn.mu.Lock()
 	defer txn.mu.Unlock()
 	return txn.mu.ID
+}
+
+// SetMaxAutoRetries sets the maximum number of times the transaction can
+// be retried internally in the KV layer.
+func (txn *Txn) SetMaxAutoRetries(maxAutoRetries int) {
+	txn.mu.Lock()
+	defer txn.mu.Unlock()
+	txn.mu.maxAutoRetries = maxAutoRetries
+}
+
+// MaxAutoRetries returns the maximum number of times the transaction can be
+// retried internally in the KV layer.
+func (txn *Txn) MaxAutoRetries() int {
+	txn.mu.Lock()
+	defer txn.mu.Unlock()
+	if r := txn.mu.maxAutoRetries; r != 0 {
+		return r
+	} else if txn.db.ctx.Settings != nil {
+		// txn.db.ctx.Settings == nil is only expected in tests.
+		return int(MaxInternalTxnAutoRetries.Get(&txn.db.ctx.Settings.SV))
+	}
+	return math.MaxInt64
 }
 
 // Key returns the current "anchor" key of the transaction, or nil if no such
@@ -1140,11 +1167,7 @@ func (txn *Txn) exec(ctx context.Context, fn func(context.Context, *Txn) error) 
 		// automatic retries. We check this after each failed attempt to allow the
 		// cluster setting to be changed while a transaction is stuck in a retry
 		// loop.
-		maxRetries := math.MaxInt64
-		if txn.db.ctx.Settings != nil {
-			// txn.db.ctx.Settings == nil is only expected in tests.
-			maxRetries = int(MaxInternalTxnAutoRetries.Get(&txn.db.ctx.Settings.SV))
-		}
+		maxRetries := txn.MaxAutoRetries()
 		// Add 1 because r.CurrentAttempt() starts at 0.
 		attempt := r.CurrentAttempt() + 1
 		if attempt > maxRetries {
@@ -1154,9 +1177,12 @@ func (txn *Txn) exec(ctx context.Context, fn func(context.Context, *Txn) error) 
 			// to terminate it here. Instead, we mark the error to allow callers to
 			// detect this condition and avoid automatic retries. We also include the
 			// original error in the error message.
-			err = errors.Mark(errors.Errorf("have retried transaction: %s %d times, most recently because of the "+
-				"retryable error: %s. Terminating retry loop and returning error due to cluster setting %s (%d). "+
-				"Rollback error: %v.", txn.DebugName(), attempt, err, MaxInternalTxnAutoRetries.Name(), maxRetries, rollbackErr),
+			err = errors.Mark(
+				errors.Errorf("have retried transaction: %s %d times, most recently because of the "+
+					"retryable error: %s. Terminating retry loop and returning error due to max retry limit (%d). "+
+					"Rollback error: %v.",
+					txn.DebugName(), attempt, err, maxRetries, rollbackErr,
+				),
 				ErrAutoRetryLimitExhausted)
 			log.Warningf(ctx, "%v", err)
 			break
