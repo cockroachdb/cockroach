72cef825e1b87a2c2eec61cd6e4eaf9b02d01376
Kevin Cao <kevin.cao@cockroachlabs.com>
2025-03-11 20:06:09 -0400

    ui: refactor jobs detail page to functional components
    This commit refactors the job details page to use React functional
    components and hooks. It also begins some work on moving the job details
    API fetch out of the global state and into local `useSWR` hooks.
    
    Epic: none
    
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/jobs/jobDetailsPage/jobProfilerView.tsx
+++ pkg/ui/workspaces/cluster-ui/src/jobs/jobDetailsPage/jobProfilerView.tsx
@@ -9,11 +9,11 @@ import { Space } from "antd";
 import classNames from "classnames";
 import classnames from "classnames/bind";
 import long from "long";
-import moment from "moment-timezone";
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useState } from "react";
 
-import { RequestState } from "src/api";
 import {
+  CollectExecutionDetailsRequest,
+  CollectExecutionDetailsResponse,
   GetJobProfilerExecutionDetailRequest,
   GetJobProfilerExecutionDetailResponse,
   ListJobProfilerExecutionDetailsRequest,
@@ -24,40 +24,101 @@ import { EmptyTable } from "src/empty";
 import { ColumnDescriptor, SortSetting, SortedTable } from "src/sortedtable";
 import { SummaryCard, SummaryCardItem } from "src/summaryCard";
 import summaryCardStyles from "src/summaryCard/summaryCard.module.scss";
-import { useScheduleFunction } from "src/util/hooks";
+import {
+  useSwrMutationWithClusterId,
+  useSwrWithClusterId,
+} from "src/util/hooks";
 
 import styles from "./jobProfilerView.module.scss";
 
 const cardCx = classNames.bind(summaryCardStyles);
 const cx = classnames.bind(styles);
 
-export type JobProfilerStateProps = {
+export interface JobProfilerViewProps {
   jobID: long;
-  executionDetailFilesResponse: RequestState<ListJobProfilerExecutionDetailsResponse>;
-  lastUpdated: moment.Moment;
-  isDataValid: boolean;
-  onDownloadExecutionFileClicked: (
+  onFetchExecutionDetailFiles: (
+    req: ListJobProfilerExecutionDetailsRequest,
+  ) => Promise<ListJobProfilerExecutionDetailsResponse>;
+  onCollectExecutionDetails: (
+    req: CollectExecutionDetailsRequest,
+  ) => Promise<CollectExecutionDetailsResponse>;
+  onDownloadExecutionFile: (
     req: GetJobProfilerExecutionDetailRequest,
   ) => Promise<GetJobProfilerExecutionDetailResponse>;
-};
+}
 
-export type JobProfilerDispatchProps = {
-  refreshExecutionDetailFiles: (
-    req: ListJobProfilerExecutionDetailsRequest,
-  ) => void;
-  onRequestExecutionDetails: (jobID: long) => void;
-};
+export function JobProfilerView({
+  jobID,
+  onFetchExecutionDetailFiles,
+  onCollectExecutionDetails,
+  onDownloadExecutionFile,
+}: JobProfilerViewProps): React.ReactElement {
+  const { data: detailFiles, mutate: refreshFiles } = useSwrWithClusterId(
+    { name: "jobProfilerExecutionFiles", jobID },
+    () => onFetchExecutionDetailFiles({ job_id: jobID }),
+    {
+      refreshInterval: 10 * 1000, // 10s polling interval
+    },
+  );
+  const { trigger } = useSwrMutationWithClusterId(
+    { name: "collectExecutionDetails", jobID },
+    async () => {
+      const resp = await onCollectExecutionDetails({ job_id: jobID });
+      if (resp.req_resp) {
+        refreshFiles();
+      }
+    },
+  );
 
-export type JobProfilerViewProps = JobProfilerStateProps &
-  JobProfilerDispatchProps;
+  const columns = makeJobProfilerViewColumns(jobID, onDownloadExecutionFile);
+  const [sortSetting, setSortSetting] = useState<SortSetting>({
+    ascending: true,
+    columnTitle: "executionDetailFiles",
+  });
+  const onChangeSortSetting = (ss: SortSetting): void => {
+    setSortSetting(ss);
+  };
 
-export function extractFileExtension(filename: string): string {
+  // This URL results in a cluster-wide CPU profile to be collected for 5
+  // seconds. We set `tagfocus` (tf) to only view the samples corresponding to
+  // this job's execution.
+  const url = `debug/pprof/ui/cpu?node=all&seconds=5&labels=true&tf=job.*${jobID}`;
+  return (
+    <Space direction="vertical" size="middle" className={cx("full-width")}>
+      <SummaryCard className={cardCx("summary-card")}>
+        <SummaryCardItem
+          label="Cluster-wide CPU Profile"
+          value={<a href={url}>Profile</a>}
+        />
+        <InlineAlert
+          intent="warning"
+          title="This operation buffers profiles in memory for all the nodes in the cluster and can result in increased memory usage."
+        />
+      </SummaryCard>
+      <Space direction="vertical" align="end" className={cx("full-width")}>
+        <Button intent="secondary" onClick={trigger}>
+          Request Execution Details
+        </Button>
+      </Space>
+      <SortedTable
+        data={detailFiles?.files}
+        columns={columns}
+        tableWrapperClassName={cx("sorted-table")}
+        sortSetting={sortSetting}
+        onChangeSortSetting={onChangeSortSetting}
+        renderNoResult={<EmptyTable title="No execution detail files found." />}
+      />
+    </Space>
+  );
+}
+
+function extractFileExtension(filename: string): string {
   const parts = filename.split(".");
   // The extension is the last part after the last dot (if it exists).
   return parts.length > 1 ? parts[parts.length - 1] : "";
 }
 
-export function getContentTypeForFile(filename: string): string {
+function getContentTypeForFile(filename: string): string {
   const extension = extractFileExtension(filename);
   switch (extension) {
     case "txt":
@@ -71,8 +132,8 @@ export function getContentTypeForFile(filename: string): string {
   }
 }
 
-export function makeJobProfilerViewColumns(
-  jobID: Long,
+function makeJobProfilerViewColumns(
+  jobID: long,
   onDownloadExecutionFileClicked: (
     req: GetJobProfilerExecutionDetailRequest,
   ) => Promise<GetJobProfilerExecutionDetailResponse>,
@@ -157,82 +218,3 @@ export function makeJobProfilerViewColumns(
     },
   ];
 }
-
-export const JobProfilerView: React.FC<JobProfilerViewProps> = ({
-  jobID,
-  executionDetailFilesResponse,
-  lastUpdated,
-  isDataValid,
-  onDownloadExecutionFileClicked,
-  refreshExecutionDetailFiles,
-  onRequestExecutionDetails,
-}: JobProfilerViewProps) => {
-  const columns = makeJobProfilerViewColumns(
-    jobID,
-    onDownloadExecutionFileClicked,
-  );
-  const [sortSetting, setSortSetting] = useState<SortSetting>({
-    ascending: true,
-    columnTitle: "executionDetailFiles",
-  });
-  const req = useMemo(
-    () =>
-      new cockroach.server.serverpb.ListJobProfilerExecutionDetailsRequest({
-        job_id: jobID,
-      }),
-    [jobID],
-  );
-  const refresh = useCallback(() => {
-    refreshExecutionDetailFiles(req);
-  }, [refreshExecutionDetailFiles, req]);
-  const [refetch] = useScheduleFunction(
-    refresh,
-    true,
-    10 * 1000, // 10s polling interval
-    lastUpdated,
-  );
-  useEffect(() => {
-    if (!isDataValid) refetch();
-  }, [isDataValid, refetch]);
-
-  const onChangeSortSetting = (ss: SortSetting): void => {
-    setSortSetting(ss);
-  };
-
-  // This URL results in a cluster-wide CPU profile to be collected for 5
-  // seconds. We set `tagfocus` (tf) to only view the samples corresponding to
-  // this job's execution.
-  const url = `debug/pprof/ui/cpu?node=all&seconds=5&labels=true&tf=job.*${jobID}`;
-  return (
-    <Space direction="vertical" size="middle" className={cx("full-width")}>
-      <SummaryCard className={cardCx("summary-card")}>
-        <SummaryCardItem
-          label="Cluster-wide CPU Profile"
-          value={<a href={url}>Profile</a>}
-        />
-        <InlineAlert
-          intent="warning"
-          title="This operation buffers profiles in memory for all the nodes in the cluster and can result in increased memory usage."
-        />
-      </SummaryCard>
-      <Space direction="vertical" align="end" className={cx("full-width")}>
-        <Button
-          intent="secondary"
-          onClick={() => {
-            onRequestExecutionDetails(jobID);
-          }}
-        >
-          Request Execution Details
-        </Button>
-      </Space>
-      <SortedTable
-        data={executionDetailFilesResponse.data?.files}
-        columns={columns}
-        tableWrapperClassName={cx("sorted-table")}
-        sortSetting={sortSetting}
-        onChangeSortSetting={onChangeSortSetting}
-        renderNoResult={<EmptyTable title="No execution detail files found." />}
-      />
-    </Space>
-  );
-};
