cea29480b437798f4cda7b073659eff766ff2c13
Andy Yang <yang@cockroachlabs.com>
2025-03-13 01:10:57 -0400

    span: change Frontier.SpanEntries to return an iterator
    This patch is a pure refactoring change to make span.Frontier's
    SpanEntries method return an iterator instead of accepting a
    span.Operation function.
    
    Release note: None

--- pkg/crosscluster/physical/stream_ingestion_processor_test.go
+++ pkg/crosscluster/physical/stream_ingestion_processor_test.go
@@ -352,10 +352,10 @@ func TestStreamIngestionProcessor(t *testing.T) {
 			if token == string(p2) {
 				sp = p2Span
 			}
-			clientStartTimes.SpanEntries(sp, func(s roachpb.Span, t hlc.Timestamp) (done span.OpResult) {
+			for _, t := range clientStartTimes.SpanEntries(sp) {
 				lastClientStart[token] = t
-				return span.StopMatch
-			})
+				break
+			}
 		}}
 		out, err := runStreamIngestionProcessor(ctx, t, registry, db,
 			topology, initialScanTimestamp, checkpoint, tenantRekey, mockClient,
