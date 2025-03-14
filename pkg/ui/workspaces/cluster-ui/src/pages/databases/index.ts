76420e62948d90578cb32d9942fca683c3ea74c8
Xin Hao Zhang <xzhang@cockroachlabs.com>
2025-03-14 12:14:51 -0400

    cluster-ui: move db pages components to `/pages/databases/components`
    Epic: none
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/pages/databases/index.ts
+++ pkg/ui/workspaces/cluster-ui/src/pages/databases/index.ts
@@ -3,4 +3,6 @@
 // Use of this software is governed by the CockroachDB Software License
 // included in the /LICENSE file.
 
-export * from "./databases";
+export * from "./databasesPageRoot";
+export * from "./views/databaseDetails";
+export * from "./views/tableDetailsV2";
