655a04dbedf3716e0512153733f31a99f018ecec
wenyihu6 <wenyi@cockroachlabs.com>
2025-03-15 19:02:11 -0400

    kvserver/closedts: introduce policy refresher
    This commit introduces `policyrefresher.PolicyRefresher` which refreshes the
    closed timestamp policies of leaseholder ranges based on observed network
    latency periodically at the interval of
    kv.closed_timestamp.policy_refresh_interval.
    
    Part of: #59680
    Release note: none

---
+++ pkg/kv/kvserver/closedts/policyrefresher/policy_refresher_test.go
@@ -0,0 +1,179 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package policyrefresher
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/closedts"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/closedts/ctpb"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/errors"
+	"github.com/stretchr/testify/require"
+)
+
+func newNoopPolicyRefresher(stopper *stop.Stopper, settings *cluster.Settings) *PolicyRefresher {
+	return NewPolicyRefresher(stopper, settings,
+		func() []Replica { return nil },
+		func() map[roachpb.NodeID]time.Duration {
+			return nil
+		})
+}
+
+type mockSpanConfig struct {
+	isGlobalRead bool
+}
+
+type mockReplica struct {
+	// Note that all fields below are protected by mu.
+	mu     syncutil.Mutex
+	conf   mockSpanConfig
+	policy ctpb.RangeClosedTimestampPolicy
+}
+
+func (m *mockReplica) RefreshPolicy(_ map[roachpb.NodeID]time.Duration) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if m.conf.isGlobalRead {
+		m.policy = ctpb.LEAD_FOR_GLOBAL_READS_WITH_NO_LATENCY_INFO
+	} else {
+		m.policy = ctpb.LAG_BY_CLUSTER_SETTING
+	}
+}
+
+func (m *mockReplica) GetPolicy() ctpb.RangeClosedTimestampPolicy {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	return m.policy
+}
+
+func (m *mockReplica) BlockReplica() (unblock func()) {
+	m.mu.Lock()
+	var once sync.Once
+	return func() {
+		once.Do(m.mu.Unlock) //nolint:deferunlockcheck
+	}
+}
+
+func (m *mockReplica) SetSpanConfig(isGlobalRead bool) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.conf.isGlobalRead = isGlobalRead
+}
+
+// TestPolicyRefresher tests that the policy refresher correctly calls
+// RefreshPolicy for replicas.
+func TestPolicyRefresher(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	ctx := context.Background()
+	stopper := stop.NewStopper()
+	defer stopper.Stop(ctx)
+
+	pr := newNoopPolicyRefresher(stopper, cluster.MakeTestingClusterSettings())
+	r := &mockReplica{}
+	require.Equal(t, r.GetPolicy(), ctpb.LAG_BY_CLUSTER_SETTING)
+	r.SetSpanConfig(true)
+	pr.refreshPolicies([]Replica{r})
+	require.Equal(t, r.GetPolicy(), ctpb.LEAD_FOR_GLOBAL_READS_WITH_NO_LATENCY_INFO)
+}
+
+// TestPolicyRefresherOnEnqueue tests that the policy refresher correctly
+// enqueues and refreshes replicas. It also tests that the policy refresher is
+// reactive to policy refresh interval change.
+func TestPolicyRefresherOnEnqueue(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	ctx := context.Background()
+	stopper := stop.NewStopper()
+	defer stopper.Stop(ctx)
+	st := cluster.MakeTestingClusterSettings()
+
+	r1 := &mockReplica{}
+	r2 := &mockReplica{}
+	r3 := &mockReplica{}
+
+	getLeaseholders := func() []Replica { return []Replica{r1, r2} }
+	getLatencies := func() map[roachpb.NodeID]time.Duration { return nil }
+
+	pr := NewPolicyRefresher(stopper, st, getLeaseholders, getLatencies)
+	require.NotNil(t, pr)
+
+	// Enable auto-tune.
+	closedts.RangeClosedTimestampPolicyRefreshInterval.Override(ctx, &st.SV, 10*time.Millisecond)
+
+	// Start the refresher.
+	pr.Run(ctx)
+
+	r1.SetSpanConfig(true)
+	r3.SetSpanConfig(true)
+
+	// Enqueue replicas which are also leaseholders and check if there is anything unexpected.
+	pr.EnqueueReplicaForRefresh(r1)
+	pr.EnqueueReplicaForRefresh(r3)
+
+	testutils.SucceedsSoon(t, func() error {
+		if r1.GetPolicy() != ctpb.LEAD_FOR_GLOBAL_READS_WITH_NO_LATENCY_INFO {
+			return errors.Newf("expected replica to have policy %v, got %v", ctpb.LEAD_FOR_GLOBAL_READS_WITH_NO_LATENCY_INFO, r1.GetPolicy())
+		}
+		if r2.GetPolicy() != ctpb.LAG_BY_CLUSTER_SETTING {
+			return errors.Newf("expected replica to have policy %v, got %v", ctpb.LAG_BY_CLUSTER_SETTING, r2.GetPolicy())
+		}
+		if r3.GetPolicy() != ctpb.LEAD_FOR_GLOBAL_READS_WITH_NO_LATENCY_INFO {
+			return errors.Newf("expected replica to have policy %v, got %v", ctpb.LEAD_FOR_GLOBAL_READS_WITH_NO_LATENCY_INFO, r3.GetPolicy())
+		}
+		return nil
+	})
+}
+
+// TestPolicyRefresherEnqueueOnBlockingReplica tests that the policy refresher
+// correctly enqueues without blocking when a replica.RefreshPolicy is blocked.
+func TestPolicyRefresherEnqueueOnBlockingReplica(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	ctx := context.Background()
+	stopper := stop.NewStopper()
+	defer stopper.Stop(ctx)
+	st := cluster.MakeTestingClusterSettings()
+
+	r := &mockReplica{}
+	pr := newNoopPolicyRefresher(stopper, st)
+	require.NotNil(t, pr)
+
+	// Enable auto-tune.
+	closedts.LeadForGlobalReadsAutoTuneEnabled.Override(ctx, &st.SV, true)
+	closedts.RangeClosedTimestampPolicyRefreshInterval.Override(ctx, &st.SV, 10*time.Millisecond)
+
+	// Start the refresher.
+	pr.Run(ctx)
+
+	// Set the span config to lead for global reads.
+	r.SetSpanConfig(true)
+
+	// Block the replica refresh.
+	unblock := r.BlockReplica()
+
+	// Although replica is blocked on RefreshPolicy, EnqueueReplicaForRefresh
+	// should not block.
+	pr.EnqueueReplicaForRefresh(r)
+	unblock()
+
+	// Policy on r1 should be refreshed after getting unblocked.
+	testutils.SucceedsSoon(t, func() error {
+		if r.GetPolicy() != ctpb.LEAD_FOR_GLOBAL_READS_WITH_NO_LATENCY_INFO {
+			return errors.Newf("expected replica to have policy %v, got %v", ctpb.LEAD_FOR_GLOBAL_READS_WITH_NO_LATENCY_INFO, r.GetPolicy())
+		}
+		return nil
+	})
+}
