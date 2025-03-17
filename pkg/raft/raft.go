37f7953e222f0bc65ddfca4f26c82c83bd0e8ed7
Ibrahim Kettaneh <ibrahim.kettaneh@cockroachlabs.com>
2025-03-17 14:01:36 -0400

    raft: ignore setting the lead field from a MsgDeFortify at current term
    When receiving a MsgDeFortifyLeader at the same term as we are, we
    should not set the lead field to the sender. Mainly for two reasons:
    
    1) By definition, a MsgDeFortifyLeader is sent by an ex-leader until it
    hears of a new committed term. If we forgot the leader at the current
    term, we shouldn't remember it since we are using the fact that
    lead==None to indicate that this replica has been leaderless for
    some time. Read the leaderlessWatcher for more details.
    
    2) This could lead to a situation where no replica can win an election
    as it could require votes from some replicas that still know who know
    the leader is (due to MsgDefortifyLeader), and that have recently
    campaigned and lost due to not having the most up-to-date log, which
    reset the electionElapsed to 0. Meaning that this replica is in a
    heartbeat lease, and will reject MsgVotes.
    
    Fixes: #142994
    
    Release note: None

--- pkg/raft/raft.go
+++ pkg/raft/raft.go
@@ -2258,11 +2258,11 @@ func stepCandidate(r *raft, m pb.Message) error {
 
 func stepFollower(r *raft, m pb.Message) error {
 	if IsMsgFromLeader(m.Type) {
-		r.setLead(m.From)
 		if m.Type != pb.MsgDeFortifyLeader {
 			// If we receive any message from the leader except a MsgDeFortifyLeader,
 			// we know that the leader is still alive and still acting as the leader,
 			// so reset the election timer.
+			r.setLead(m.From)
 			r.electionElapsed = 0
 		}
 	}
