743d8bda5dc9e0b57f58ddfd4f7f2a3975ee2c02
Norman Chen <norman.chen@cockroachlabs.com>
2025-03-19 16:14:57 -0400

    jsonpath/eval: add unwrap parameter to jsonpath.eval
    Previously, jsonpath conditionals/filters would unwrap infinitely. For
    example, `SELECT jsonb_path_query('{"a": [[[[{"b": 1}]]]]}', '$.a ? (@.b == 1)');`
    would return an entry, when postgres would not. This commit adds an
    unwrap boolean parameter to `eval`, which allows for control over when
    to stop unwrapping json arrays. Additionally, this commit updates
    `eval` to take in a `json.JSON` rather than a `[]json.JSON`, simplifying
    the evaluation logic.
    
    Epic: None
    Release note: None

--- pkg/util/jsonpath/eval/key.go
+++ pkg/util/jsonpath/eval/key.go
@@ -10,39 +10,26 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
 	"github.com/cockroachdb/cockroach/pkg/util/json"
 	"github.com/cockroachdb/cockroach/pkg/util/jsonpath"
-	"github.com/cockroachdb/errors"
 )
 
-func (ctx *jsonpathCtx) evalKey(k jsonpath.Key, current []json.JSON) ([]json.JSON, error) {
-	var agg []json.JSON
-	for _, j := range current {
-		if j.Type() == json.ObjectJSONType {
-			val, err := j.FetchValKey(string(k))
-			if err != nil {
-				return nil, err
-			}
-			if val == nil {
-				if ctx.strict {
-					return nil, pgerror.Newf(pgcode.SQLJSONMemberNotFound, "JSON object does not contain key %q", string(k))
-				}
-				continue
-			}
-			agg = append(agg, val)
-		} else if !ctx.strict && j.Type() == json.ArrayJSONType {
-			arr, ok := j.AsArray()
-			if !ok {
-				return nil, errors.AssertionFailedf("array expected")
-			}
-			for _, elem := range arr {
-				results, err := ctx.eval(k, []json.JSON{elem})
-				if err != nil {
-					return nil, err
-				}
-				agg = append(agg, results...)
-			}
-		} else if ctx.strict {
-			return nil, pgerror.Newf(pgcode.SQLJSONMemberNotFound, "jsonpath member accessor can only be applied to an object")
+func (ctx *jsonpathCtx) evalKey(
+	key jsonpath.Key, jsonValue json.JSON, unwrap bool,
+) ([]json.JSON, error) {
+	if jsonValue.Type() == json.ObjectJSONType {
+		val, err := jsonValue.FetchValKey(string(key))
+		if err != nil {
+			return nil, err
 		}
+		if val == nil && ctx.strict {
+			return nil, pgerror.Newf(pgcode.SQLJSONMemberNotFound, "JSON object does not contain key %q", string(key))
+		} else if val != nil {
+			return []json.JSON{val}, nil
+		}
+		return []json.JSON{}, nil
+	} else if unwrap && jsonValue.Type() == json.ArrayJSONType {
+		return ctx.unwrapCurrentTargetAndEval(key, jsonValue, false /* unwrapNext */)
+	} else if ctx.strict {
+		return nil, pgerror.Newf(pgcode.SQLJSONMemberNotFound, "jsonpath member accessor can only be applied to an object")
 	}
-	return agg, nil
+	return []json.JSON{}, nil
 }
