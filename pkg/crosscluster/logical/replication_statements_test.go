ab68149c87bca988383a25491a300aea22b8642d
Jeff Swenson <jeffswenson@betterthannull.com>
2025-03-19 15:41:29 -0400

    crosscluster: add new insert/update/delete replication statements
    These statements take advantage of the KV level LWW validation added by
    PR #143100 and do not explicitly validate the origin timestamp. Update
    and Delete specify the whole row in the where clause, which is intended
    to enable future SQL optimizations that generate a CPUT to replace the
    row instead
    
    Release note: none
    Epic: CRDB-48647

---
+++ pkg/crosscluster/logical/replication_statements_test.go
@@ -0,0 +1,106 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package logical
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/desctestutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/datapathutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/datadriven"
+	"github.com/stretchr/testify/require"
+)
+
+func TestReplicationStatements(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	ctx := context.Background()
+	s, sqlDB, _ := serverutils.StartServer(t, base.TestServerArgs{
+		Locality: roachpb.Locality{
+			Tiers: []roachpb.Tier{
+				{Key: "region", Value: "us-east1"},
+			},
+		},
+	})
+	defer s.Stopper().Stop(ctx)
+
+	getTableDesc := func(tableName string) catalog.TableDescriptor {
+		return desctestutils.TestingGetTableDescriptor(
+			s.DB(),
+			s.Codec(),
+			"defaultdb",
+			"public",
+			tableName,
+		)
+	}
+
+	datadriven.Walk(t, datapathutils.TestDataPath(t), func(t *testing.T, path string) {
+		datadriven.RunTest(t, path, func(t *testing.T, d *datadriven.TestData) string {
+			switch d.Cmd {
+			case "exec":
+				_, err := sqlDB.Exec(d.Input)
+				if err != nil {
+					return err.Error()
+				}
+				return "ok"
+			case "show-insert":
+				var tableName string
+				d.ScanArgs(t, "table", &tableName)
+
+				desc := getTableDesc(tableName)
+
+				insertStmt, err := newInsertStatement(desc)
+				require.NoError(t, err)
+
+				// Test preparing the statement to ensure it is valid SQL.
+				_, err = sqlDB.Exec(fmt.Sprintf("PREPARE stmt_%d AS %s", rand.Int(), insertStmt.String()))
+				require.NoError(t, err)
+
+				return insertStmt.String()
+			case "show-update":
+				var tableName string
+				d.ScanArgs(t, "table", &tableName)
+
+				desc := getTableDesc(tableName)
+
+				updateStmt, err := newUpdateStatement(desc)
+				require.NoError(t, err)
+
+				// Test preparing the statement to ensure it is valid SQL.
+				_, err = sqlDB.Exec(fmt.Sprintf("PREPARE stmt_%d AS %s", rand.Int(), updateStmt.String()))
+				require.NoError(t, err)
+
+				return updateStmt.String()
+			case "show-delete":
+				var tableName string
+				d.ScanArgs(t, "table", &tableName)
+
+				desc := getTableDesc(tableName)
+
+				deleteStmt, err := newDeleteStatement(desc)
+				require.NoError(t, err)
+
+				// Test preparing the statement to ensure it is valid SQL.
+				_, err = sqlDB.Exec(fmt.Sprintf("PREPARE stmt_%d AS %s", rand.Int(), deleteStmt.String()))
+				require.NoError(t, err)
+
+				return deleteStmt.String()
+			default:
+				return "unknown command: " + d.Cmd
+			}
+		})
+	})
+}
