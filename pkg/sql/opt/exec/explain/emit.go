7d0b30cec436feb8ce40dd47263eecdfe3bd8f94
Yahor Yuzefovich <yahor@cockroachlabs.com>
2025-03-20 22:42:32 -0700

    explain: fix decoding plan gist with CALL stmts
    Previously we'd hit a nil pointer error when trying to print `Proc`
    argument when it is `nil`. The problem is that it is stored as
    `*tree.RoutineExpr`, and `(*tree.RoutineExpr)(nil)` is different from
    `(tree.TypedExpr)(nil)` that we check for (and short-circuit on) in the
    `Expr` call.
    
    I have an idea for how we could've caught this, but it'll be done in a
    separate commit.
    
    Release note (bug fix): CockroachDB would previously encounter an
    internal error when decoding plan gists of the plans with CALL
    statements. The bug has been present since 23.2 version and is now
    fixed.

--- pkg/sql/opt/exec/explain/emit.go
+++ pkg/sql/opt/exec/explain/emit.go
@@ -1172,7 +1172,14 @@ func (e *emitter) emitNodeAttributes(ctx context.Context, evalCtx *eval.Context,
 
 	case callOp:
 		a := n.args.(*callArgs)
-		ob.Expr("procedure", a.Proc, nil /* columns */)
+		if a.Proc != nil {
+			// Unlike other expressions, we store Proc as *tree.RoutineExpr, so
+			// the nil value in Proc is different from the nil value that is
+			// checked in Expr:
+			//  (*tree.RoutineExpr)(nil)  vs (tree.TypedExpr)(nil)
+			// so we need an explicit nil check.
+			ob.Expr("procedure", a.Proc, nil /* columns */)
+		}
 
 	case simpleProjectOp,
 		serializingProjectOp,
