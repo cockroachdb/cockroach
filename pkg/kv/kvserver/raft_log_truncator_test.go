cb1b6a07fe51ae1b5ac99ece17a26db0ce942e95
Pavel Kalinnikov <pavel@cockroachlabs.com>
2025-03-21 02:57:16 +0000

    kvserver: fix durability bug in loosely coupled truncs
    This commit fixes a bug equivalent to the one we had in trightly coupled
    truncations stack. The log engine is now synced when applying a
    truncation that removes sideloaded entries. Not doing so could result in
    a log with missing entries, and crashes when the leader attempts to
    replicate such a log.
    
    Epic: none
    Release note: none

--- pkg/kv/kvserver/raft_log_truncator_test.go
+++ pkg/kv/kvserver/raft_log_truncator_test.go
@@ -140,12 +140,14 @@ func (r *replicaTruncatorTest) getPendingTruncs() *pendingLogTruncations {
 	return &r.pendingTruncs
 }
 
-func (r *replicaTruncatorTest) sideloadedBytesIfTruncatedFromTo(
+func (r *replicaTruncatorTest) sideloadedStats(
 	_ context.Context, span kvpb.RaftSpan,
-) (freed int64, _ error) {
-	fmt.Fprintf(r.buf, "r%d.sideloadedBytesIfTruncatedFromTo(%d, %d)\n",
-		r.rangeID, span.After, span.Last)
-	return r.sideloadedFreed, r.sideloadedErr
+) (entries uint64, size int64, _ error) {
+	fmt.Fprintf(r.buf, "r%d.sideloadedStats(%d, %d)\n", r.rangeID, span.After, span.Last)
+	if r.sideloadedFreed != 0 {
+		entries = 1 // Make it look like we are removing some sideloaded entries.
+	}
+	return entries, r.sideloadedFreed, r.sideloadedErr
 }
 
 func (r *replicaTruncatorTest) getStateLoader() stateloader.StateLoader {
@@ -390,9 +392,10 @@ func printTruncatorState(t *testing.T, buf *strings.Builder, truncator *raftLogT
 	}
 }
 
-func (pt pendingTruncation) delta(delta int64, trusted bool) pendingTruncation {
+func (pt pendingTruncation) delta(delta int64, hasSideloaded, trusted bool) pendingTruncation {
 	pt.logDeltaBytes = delta
 	pt.isDeltaTrusted = trusted
+	pt.hasSideloaded = hasSideloaded
 	return pt
 }
 
@@ -409,30 +412,43 @@ func TestPendingTruncationMerge(t *testing.T) {
 			expectedFirstIndex: kvpb.RaftIndex(from + 1),
 		}
 	}
+	const hasSL, noSL = true, false
 	for _, tt := range []struct {
 		prev pendingTruncation
 		next pendingTruncation
 		want pendingTruncation
 	}{{
-		prev: pt(100, 200, 10).delta(1024, true),
-		next: pt(200, 300, 11).delta(1024, true),
-		want: pt(100, 300, 11).delta(2048, true),
+		prev: pt(100, 200, 10).delta(1024, noSL, true),
+		next: pt(200, 300, 11).delta(1024, noSL, true),
+		want: pt(100, 300, 11).delta(2048, noSL, true),
 	}, {
-		prev: pt(100, 200, 10).delta(1024, false),
-		next: pt(200, 300, 11).delta(1024, true),
-		want: pt(100, 300, 11).delta(2048, false),
+		prev: pt(100, 200, 10).delta(1024, noSL, false),
+		next: pt(200, 300, 11).delta(1024, noSL, true),
+		want: pt(100, 300, 11).delta(2048, noSL, false),
 	}, {
-		prev: pt(100, 200, 10).delta(1024, true),
-		next: pt(200, 300, 11).delta(1024, false),
-		want: pt(100, 300, 11).delta(2048, false),
+		prev: pt(100, 200, 10).delta(1024, noSL, true),
+		next: pt(200, 300, 11).delta(1024, noSL, false),
+		want: pt(100, 300, 11).delta(2048, noSL, false),
 	}, {
-		prev: pt(100, 200, 10).delta(1024, true),
-		next: pt(150, 300, 11).delta(2048, true),
-		want: pt(100, 300, 11).delta(3072, false),
+		prev: pt(100, 200, 10).delta(1024, hasSL, true),
+		next: pt(200, 300, 11).delta(1024, noSL, true),
+		want: pt(100, 300, 11).delta(2048, hasSL, true),
 	}, {
-		prev: pt(100, 200, 10).delta(1024, false),
-		next: pt(250, 400, 11).delta(2048, true),
-		want: pt(100, 400, 11).delta(3072, false),
+		prev: pt(100, 200, 10).delta(1024, noSL, true),
+		next: pt(200, 300, 11).delta(1024, hasSL, true),
+		want: pt(100, 300, 11).delta(2048, hasSL, true),
+	}, {
+		prev: pt(100, 200, 10).delta(1024, hasSL, true),
+		next: pt(200, 300, 11).delta(1024, hasSL, true),
+		want: pt(100, 300, 11).delta(2048, hasSL, true),
+	}, {
+		prev: pt(100, 200, 10).delta(1024, noSL, true),
+		next: pt(150, 300, 11).delta(2048, noSL, true),
+		want: pt(100, 300, 11).delta(3072, noSL, false),
+	}, {
+		prev: pt(100, 200, 10).delta(1024, noSL, false),
+		next: pt(250, 400, 11).delta(2048, noSL, true),
+		want: pt(100, 400, 11).delta(3072, noSL, false),
 	}} {
 		t.Run("", func(t *testing.T) {
 			require.Equal(t, tt.want, tt.next.merge(tt.prev))
