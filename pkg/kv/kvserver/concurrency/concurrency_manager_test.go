536e5c006608ee4a0fc6fd9a94eca3b23b599ee3
Steven Danna <danna@cockroachlabs.com>
2025-03-24 11:02:47 +0000

    kvserver: add per-operation lock reliability settings
    Preserving unreplicated locks during split, merge, and lease transfers have
    different trade offs. For instance, during a split all lock updates
    are done in memory without any new replicated writes, whereas for
    merge and lease transfers requiring replicating locks through raft.
    
    Here, we put the different operations under different settings since
    we may want to ship different defaults for the different operations.
    
    Epic: none
    Release note: None

--- pkg/kv/kvserver/concurrency/concurrency_manager_test.go
+++ pkg/kv/kvserver/concurrency/concurrency_manager_test.go
@@ -735,7 +735,9 @@ func newClusterWithSettings(st *clustersettings.Settings) *cluster {
 	// Set the latch manager's long latch threshold to infinity to disable
 	// logging, which could cause a test to erroneously fail.
 	spanlatch.LongLatchHoldThreshold.Override(context.Background(), &st.SV, math.MaxInt64)
-	concurrency.UnreplicatedLockReliability.Override(context.Background(), &st.SV, true)
+	concurrency.UnreplicatedLockReliabilitySplit.Override(context.Background(), &st.SV, true)
+	concurrency.UnreplicatedLockReliabilityMerge.Override(context.Background(), &st.SV, true)
+	concurrency.UnreplicatedLockReliabilityLeaseTransfer.Override(context.Background(), &st.SV, true)
 	manual := timeutil.NewManualTime(timeutil.Unix(123, 0))
 	return &cluster{
 		nodeDesc:  &roachpb.NodeDescriptor{NodeID: 1},
