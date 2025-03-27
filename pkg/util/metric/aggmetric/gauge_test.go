ae857371f3162bb5780af2026d94bc390b4a7d7f
Akshay Joshi <akshay@cockroachlabs.com>
2025-03-27 12:43:53 +0530

    aggmetric: add clear method in ChildStorage
    This patch introduces `clear` method in ChildStorage. It will clear all
    children which are part of parent metric. This method will be used during
    reinitialisation of aggmetric on cluster setting changes.
    
    Epic: CRDB-43153
    Part of: CRDB-48253
    Release note: None

--- pkg/util/metric/aggmetric/gauge_test.go
+++ pkg/util/metric/aggmetric/gauge_test.go
@@ -20,13 +20,14 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/metric"
 	"github.com/cockroachdb/crlib/testutils/require"
 	"github.com/prometheus/common/expfmt"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAggGaugeEviction(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	const cacheSize = 10
 	r := metric.NewRegistry()
-	writePrometheusMetrics := writePrometheusMetricsFunc(r)
+	writePrometheusMetrics := WritePrometheusMetricsFunc(r)
 
 	g := NewGauge(metric.Metadata{
 		Name: "foo_gauge",
@@ -101,11 +102,30 @@ func TestAggGaugeMethods(t *testing.T) {
 	echotest.Require(t, writePrometheusMetrics(t), datapathutils.TestDataPath(t, testFile))
 }
 
+func TestPanicForAggGaugeWithBtreeStorage(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	g := NewGauge(metric.Metadata{
+		Name: "foo_gauge",
+	}, "tenant_id")
+
+	assert.Panics(t, func() {
+		g.Inc(1, "1", "1")
+	}, "expected panic when Inc is invoked on AggGauge with BTreeStorage")
+
+	assert.Panics(t, func() {
+		g.Dec(1, "1", "1")
+	}, "expected panic when Dec is invoked on AggGauge with BTreeStorage")
+
+	assert.Panics(t, func() {
+		g.Update(1, "1", "1")
+	}, "expected panic when Update is invoked on AggGauge with BTreeStorage")
+}
+
 func TestAggGaugeFloat64Eviction(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	const cacheSize = 10
 	r := metric.NewRegistry()
-	writePrometheusMetrics := writePrometheusMetricsFunc(r)
+	writePrometheusMetrics := WritePrometheusMetricsFunc(r)
 
 	g := NewGaugeFloat64(metric.Metadata{
 		Name: "foo_gauge_float64",
@@ -136,22 +156,13 @@ func TestAggGaugeFloat64Eviction(t *testing.T) {
 	echotest.Require(t, writePrometheusMetrics(t), datapathutils.TestDataPath(t, testFile))
 }
 
-func writePrometheusMetricsFunc(r *metric.Registry) func(t *testing.T) string {
-	writePrometheusMetrics := func(t *testing.T) string {
-		var in bytes.Buffer
-		ex := metric.MakePrometheusExporter()
-		scrape := func(ex *metric.PrometheusExporter) {
-			ex.ScrapeRegistry(r, true /* includeChildMetrics */, true)
-		}
-		require.NoError(t, ex.ScrapeAndPrintAsText(&in, expfmt.FmtText, scrape))
-		var lines []string
-		for sc := bufio.NewScanner(&in); sc.Scan(); {
-			if !bytes.HasPrefix(sc.Bytes(), []byte{'#'}) {
-				lines = append(lines, sc.Text())
-			}
-		}
-		sort.Strings(lines)
-		return strings.Join(lines, "\n")
-	}
-	return writePrometheusMetrics
+func TestPanicForAggGaugeFloat64WithBtreeStorage(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	g := NewGaugeFloat64(metric.Metadata{
+		Name: "foo_gauge",
+	}, "tenant_id")
+
+	assert.Panics(t, func() {
+		g.Update(1, "1", "1")
+	}, "expected panic when Update is invoked on AggGaugeFloat64 with BTreeStorage")
 }
