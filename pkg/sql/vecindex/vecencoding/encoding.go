1fc5f5a2a98a4cbe8813475aae7b6bfb8c08d557
Matt White <matt.white@cockroachlabs.com>
2025-03-28 10:58:50 -0700

    vecencoding: Add functions to extract vector index kv pair
    For backfill, we need to be able to take an encoded kv pair and extract
    the partition ID, level and unquantized vector so that the writer
    threads can re-encode with a new partition ID, level & quantization.
    These functions treat the rest of the key as opaque so that we don't
    have to re-implement key decoding and encoding.
    
    Epic: CRDB-42943
    Release note: None

--- pkg/sql/vecindex/vecencoding/encoding.go
+++ pkg/sql/vecindex/vecencoding/encoding.go
@@ -30,6 +30,46 @@ Value:
   └────────────────────────┴────────────────────────┘
 */
 
+type VectorIndexKey struct {
+	Prefix       cspann.TreeKey
+	PartitionKey cspann.PartitionKey
+	Level        cspann.Level
+	Suffix       []byte
+}
+
+func ExtractKey(keyBytes []byte, numPrefixColumns int) (vecIndexKey VectorIndexKey, err error) {
+	prefixLen := 0
+	for i := 0; i < numPrefixColumns; i++ {
+		columnWidth, err := encoding.PeekLength(keyBytes[prefixLen:])
+		if err != nil {
+			return vecIndexKey, err
+		}
+		prefixLen += columnWidth
+	}
+	if prefixLen > 0 {
+		vecIndexKey.Prefix = keyBytes[:prefixLen]
+		keyBytes = keyBytes[prefixLen:]
+	}
+
+	partitionKey, keyBytes, err := DecodePartitionKey(keyBytes)
+	if err != nil {
+		return vecIndexKey, err
+	}
+	vecIndexKey.PartitionKey = partitionKey
+
+	level, keyBytes, err := DecodePartitionLevel(keyBytes)
+	if err != nil {
+		return vecIndexKey, err
+	}
+	vecIndexKey.Level = level
+
+	if len(keyBytes) > 0 {
+		vecIndexKey.Suffix = keyBytes
+	}
+
+	return vecIndexKey, nil
+}
+
 // EncodePartitionMetadata encodes the metadata for a partition.
 func EncodePartitionMetadata(level cspann.Level, centroid vector.T) ([]byte, error) {
 	// The encoding consists of 8 bytes for the level, and a 4-byte length,
@@ -178,4 +218,21 @@ func DecodeChildKey(encChildKey []byte, level cspann.Level) (cspann.ChildKey, er
 		}
 		return cspann.ChildKey{PartitionKey: cspann.PartitionKey(childPartitionKey)}, nil
 	}
+}
+
+func DecodePartitionKey(encodedPartitionKey []byte) (cspann.PartitionKey, []byte, error) {
+	remainingBytes, partitionKey, err := encoding.DecodeUvarintAscending(encodedPartitionKey)
+	if err != nil {
+		return 0, nil, err
+	}
+	return cspann.PartitionKey(partitionKey), remainingBytes, nil
+}
+
+func DecodePartitionLevel(encodedLevel []byte) (cspann.Level, []byte, error) {
+	remainingBytes, level, err := encoding.DecodeUvarintAscending(encodedLevel)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	return cspann.Level(level), remainingBytes, nil
 }
