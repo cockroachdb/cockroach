fa554826a452c62a1b04b7ccf8865e3fb098ab6a
Pavel Kalinnikov <pavel@cockroachlabs.com>
2025-03-29 22:11:52 +0000

    raft: remove ReadyTODO
    All the callers have migrated to the new Ready API.
    
    Epic: none
    Release note: none

--- pkg/raft/rawnode.go
+++ pkg/raft/rawnode.go
@@ -230,17 +230,6 @@ func (rn *RawNode) Ready() Ready {
 	return rd
 }
 
-// ReadyTODO returns a legacy Ready which includes pb.MsgStorageAppend in the
-// Messages field.
-// TODO(pav-kv): migrate all the users to the new Ready() call.
-func (rn *RawNode) ReadyTODO() Ready {
-	rd := rn.Ready()
-	if app := rd.StorageAppend; !app.Empty() {
-		rd.Messages = append(rd.Messages, app.ToMessage(rn.raft.id))
-	}
-	return rd
-}
-
 func (rn *RawNode) needStorageAppend() bool {
 	r := rn.raft
 	// Return true if log entries, HardState, or a snapshot need to be written to
