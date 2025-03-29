1b8d902f8f8e1144b38ad7a4175dcdfabefd7695
Pavel Kalinnikov <pavel@cockroachlabs.com>
2025-03-29 16:07:40 +0000

    rafttest: make AppendWork a StorageAppend
    Epic: none
    Release note: none

--- pkg/raft/rafttest/interaction_env.go
+++ pkg/raft/rafttest/interaction_env.go
@@ -53,7 +53,7 @@ type Node struct {
 	// storage writes, using asyncWrites == false is unnecessary.
 	asyncWrites bool
 
-	AppendWork []pb.Message // []MsgStorageAppend
+	AppendWork []raft.StorageAppend
 	ApplyWork  pb.LogSpan
 	History    []pb.Snapshot
 }
