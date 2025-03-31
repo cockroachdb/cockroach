6086051dc36d94eb902b5de043a1a55bd49f1bc8
Faizan Qazi <faizan@cockroachlabs.com>
2025-03-31 15:46:06 -0400

    sql: automatically set / unset schema_locked for legacy statements
    Previously, schema_locked had to be manually set or unset by users when
    making schema changes. We modified the declarative schema changer to
    cover some of the more common backfilling schema changes to set / unset
    this automatically. Unfortunately, there are a still a number of schema
    changes in the legacy schema changer that are not covered. To address
    this, this patch adds support for setting / unsetting schema_locked in
    implicit transactions with the legacy schema changer.
    
    This patch works by:
    1) Deferring descriptor updates to the job phase when schema_locked is
       detected. The descriptor is only modified to unset schema_locked and
       add the ID for the job responsible for the change. The connExecutor
       will automatically do this on commit.
    2) The schema change job is modified to first apply an initial set of
       descriptor updates. These are what the statement would have normally
       applied during commit before starting the job.
    3) The schema change job when it finishes will restore schema_locked as
       the last step.
    4) To make this entire process safe with concurrency, mutable descriptor
       cannot ever be checked out if the table descriptor as the
       SchemaLockedJobID set. If some other job or statement checks these
       out a retryable error will be returned.
    
    Informs: #129694
    Release note: None

--- pkg/sql/create_index.go
+++ pkg/sql/create_index.go
@@ -90,7 +90,7 @@ func (p *planner) CreateIndex(ctx context.Context, n *tree.CreateIndex) (planNod
 	}
 
 	// Disallow schema changes if this table's schema is locked.
-	if err := checkSchemaChangeIsAllowed(tableDesc, n); err != nil {
+	if err := p.checkSchemaChangeIsAllowed(ctx, tableDesc, n); err != nil {
 		return nil, err
 	}
 
