091d88b49e09564c247d6e66f3cfb22ece709d97
Norman Chen <norman.chen@cockroachlabs.com>
2025-04-07 17:36:27 -0400

    jsonpath: add support for .size() method
    This commit adds support for the `.size()` method in JSONPath queries,
    which returns the length of the array or 1 for non-array values (in lax
    mode). In strict mode, it returns an error when applied to non-array
    values.
    
    This commit also sets up the rest of the JSONPath methods, adding
    unimplemented errors when they are parsed.
    
    Release note (sql change): Add support for `.size()` method in JSONPath
    expressions. For example, `SELECT jsonb_path_query('[1, 2, 3]',
    '$.size()');`.

---
+++ pkg/util/jsonpath/method.go
@@ -0,0 +1,29 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package jsonpath
+
+import "fmt"
+
+type MethodType int
+
+const (
+	InvalidMethod MethodType = iota
+	SizeMethod
+)
+
+var MethodTypeStrings = map[MethodType]string{
+	SizeMethod: "size",
+}
+
+type Method struct {
+	Type MethodType
+}
+
+var _ Path = Method{}
+
+func (m Method) String() string {
+	return fmt.Sprintf(".%s()", MethodTypeStrings[m.Type])
+}
