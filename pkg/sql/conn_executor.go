14660969b6c09ae7deaf91ab5cf0d6f625eb2597
Marcus Gartner <marcus@cockroachlabs.com>
2025-04-08 12:42:42 -0700

    prep: decrement statement reference counts in Cache.Init
    Prepared statement reference counts are now decremented in `Cache.Init`
    if the cache is non-empty. This makes it easier and safe to clear a
    `Cache` with `Init`.
    
    Also, some minor changes have been made to simplify the test oracle.
    
    Release note: None

--- pkg/sql/conn_executor.go
+++ pkg/sql/conn_executor.go
@@ -1255,7 +1255,7 @@ func (s *Server) newConnExecutor(
 	}
 
 	ex.extraTxnState.underOuterTxn = underOuterTxn
-	ex.extraTxnState.prepStmtsNamespace.prepStmts.Init()
+	ex.extraTxnState.prepStmtsNamespace.prepStmts.Init(ctx)
 	ex.extraTxnState.prepStmtsNamespace.portals = make(map[string]PreparedPortal)
 	ex.extraTxnState.prepStmtsNamespace.portalsSnapshot = make(map[string]PreparedPortal)
 	ex.extraTxnState.prepStmtsNamespaceMemAcc = ex.sessionMon.MakeBoundAccount()
@@ -2030,14 +2030,7 @@ func (ns *prepStmtNamespace) String() string {
 func (ns *prepStmtNamespace) clear(
 	ctx context.Context, prepStmtsNamespaceMemAcc *mon.BoundAccount,
 ) {
-	// Commit the prepared statements cache to ensure that any statements that
-	// have been removed but not committed are not leaked.
-	// TODO(mgartner): Make a Clear method of prep.Cache to make this automatic.
-	ns.prepStmts.Commit(ctx, 0)
-	ns.prepStmts.ForEach(func(name string, stmt *prep.Statement) {
-		stmt.DecRef(ctx)
-	})
-	ns.prepStmts.Init()
+	ns.prepStmts.Init(ctx)
 	ns.closeAllPortals(ctx, prepStmtsNamespaceMemAcc)
 }
 
