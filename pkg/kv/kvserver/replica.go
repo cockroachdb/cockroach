c8b7ce3637c9d644fc2a5c0cdbae314294816ae7
wenyihu6 <wenyi@cockroachlabs.com>
2025-04-09 07:00:42 -0400

    kvserver: add the defer I think it was missing

--- pkg/kv/kvserver/replica.go
+++ pkg/kv/kvserver/replica.go
@@ -7,8 +7,8 @@ package kvserver
 
 import (
 	"context"
-	"math"
 	"fmt"
+	"math"
 	"slices"
 	"sync"
 	"sync/atomic"
@@ -3093,7 +3093,7 @@ func (r *Replica) TryConstructMMARangeMsg() (mma.RangeMsg, bool) {
 	var conf roachpb.SpanConfig
 	func() {
 		r.mu.RLock()
-		r.mu.RUnlock()
+		defer r.mu.RUnlock()
 		raftStatus = r.raftStatusRLocked()
 		needed = r.mu.mmaRangeMessageNeeded.getNeededAndReset(
 			rload, raftStatus, sendStreamStats)
