93002dd52080a7b5b73fdc878fefb94fa796da92
Steven Danna <danna@cockroachlabs.com>
2025-04-09 10:47:05 +0000

    kvcoord: avoid erroneous rollback of buffered write
    Previously, if a transaction had only done non-locking reads and
    buffered writes, a rollback would erroneously rollback the last issued
    buffered write because savepoint creation only incremented the
    sequence number if a lock had been acquired.
    
    This case is never encountered in SQL-generated transactions because
    we never completely buffer a Put or Delete without having previously
    issued at least 1 locking read that we expect to return a lock.
    
    But, we do hit these kind of transactions in KVNemesis.
    
    Epic: none
    
    Release note: None

--- pkg/kv/kvclient/kvcoord/txn_test.go
+++ pkg/kv/kvclient/kvcoord/txn_test.go
@@ -2046,3 +2046,39 @@ func TestTxnBufferedWritesConditionalPuts(t *testing.T) {
 		}
 	})
 }
+
+// TestTxnBufferedWritesRollbackToSavepointAllBuffered is a regression
+// test for a bug encountered during development where the sequence
+// number of a savepoint was not correctly advanced when all writes in
+// a transaction had been buffered.
+func TestTxnBufferedWritesRollbackToSavepointAllBuffered(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	s := createTestDB(t)
+	defer s.Stop()
+
+	ctx := context.Background()
+	value1 := []byte("value1")
+	testutils.RunTrueAndFalse(t, "buffered_write", func(t *testing.T, bufferedWritesEnabled bool) {
+		keyA := []byte(fmt.Sprintf("keyA-%v", bufferedWritesEnabled))
+
+		err := s.DB.Txn(ctx, func(ctx context.Context, txn *kv.Txn) error {
+			txn.SetBufferedWritesEnabled(bufferedWritesEnabled)
+			// Should not be rolled back because the savepoint is
+			// created after it.
+			if err := txn.Put(ctx, keyA, value1); err != nil {
+				return err
+			}
+			sp, err := txn.CreateSavepoint(ctx)
+			if err != nil {
+				return err
+			}
+			return txn.RollbackToSavepoint(ctx, sp)
+		})
+		require.NoError(t, err)
+		actualA, err := s.DB.Get(ctx, keyA)
+		require.NoError(t, err)
+		require.Equal(t, actualA.ValueBytes(), value1)
+	})
+
+}
