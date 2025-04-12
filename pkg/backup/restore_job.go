75dcee2008662870496d71214bdf624a1475aba2
Matt Spilchen <matt.spilchen@cockroachlabs.com>
2025-04-12 07:52:21 -0300

    sql: avoid unnecessary version bumps for UDTs during ALTER TABLE
    Previously, in the legacy schema changer, we unconditionally updated
    backreferences from tables to user-defined types (UDTs) during ALTER
    TABLE operations. This happened even when the backreferences remained
    unchanged.
    
    While this behavior was functionally harmless, it caused the version of
    the referenced types to be incremented unnecessarily.
    
    This change avoids version bumps unless the backreference actually
    changes.
    
    Fixes: #144293
    
    Epic: none
    Release note: none

--- pkg/backup/restore_job.go
+++ pkg/backup/restore_job.go
@@ -1390,7 +1390,7 @@ func createImportingDescriptors(
 					if err != nil {
 						return err
 					}
-					typDesc.AddReferencingDescriptorID(table.GetID())
+					_ = typDesc.AddReferencingDescriptorID(table.GetID())
 					if err := descsCol.WriteDescToBatch(
 						ctx, kvTrace, typDesc, b,
 					); err != nil {
@@ -3072,7 +3072,7 @@ func (r *restoreResumer) removeExistingTypeBackReferences(
 				}
 				existing := desc.(*typedesc.Mutable)
 				existing.MaybeIncrementVersion()
-				existing.RemoveReferencingDescriptorID(tbl.ID)
+				_ = existing.RemoveReferencingDescriptorID(tbl.ID)
 			}
 		}
 	}
