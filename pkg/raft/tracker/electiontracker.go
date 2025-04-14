734934dce57275b70572aa33452bf668cabe6211
Anthony Xu <anthony.xu@cockroachlabs.com>
2025-04-14 11:22:31 -0400

    raft: record hint information into the candidate's election tracker
    Previously, the election tracker only records whether a peer voted for the
    candidate during an election.
    
    This commit introduces data structures to store hint information in the election
    tracker. And also the logic for a candidate to record those hint information
    which originated from MsgVoteResp upon receiving those vote responses.
    
    Those information can be used for optimizing the first round of replication if
    this candidate becomes the leader. Which ultimately improves replication tail
    latency.
    
    It is redundant to store conflict index if the voter responded with a
    MsgVoteResp(reject) since we don't utilize them now.
    
    But as a future improvement, the hint from MsgVoteResp{reject} can still be
    taken advantage of.
    
    Epic: none
    
    Release note: None

--- pkg/raft/tracker/electiontracker.go
+++ pkg/raft/tracker/electiontracker.go
@@ -12,25 +12,43 @@ import (
 
 // ElectionTracker is used to track votes from the currently active configuration
 // and determine election results.
+// Also used for storing hints which will benefit the first round of MsgApp if
+// the candidate becomes leader.
 type ElectionTracker struct {
-	config *quorum.Config
+	config       *quorum.Config
+	votes        map[pb.PeerID]bool
+	conflictInfo map[pb.PeerID]hintInfo
+}
 
-	votes map[pb.PeerID]bool
+// hintInfo stores information needed for the new leader to decide where to send
+// entryIDs from after winning leader election.
+type hintInfo struct {
+	// conflictIdx is the best guess on where the new leader should send to the
+	// follower if the candidate wins the election.
+	conflictIdx pb.Index
+	// match indicates if the particular conflictIdx is a durable match for the
+	// voter's log if the current candidate wins the election.
+	match bool
 }
 
 func MakeElectionTracker(config *quorum.Config) ElectionTracker {
 	return ElectionTracker{
-		config: config,
-		votes:  map[pb.PeerID]bool{},
+		config:       config,
+		votes:        map[pb.PeerID]bool{},
+		conflictInfo: map[pb.PeerID]hintInfo{},
 	}
 }
 
 // RecordVote records that the node with the given id voted for this Raft
-// instance if v == true (and declined it otherwise).
-func (e *ElectionTracker) RecordVote(id pb.PeerID, vote bool) {
+// instance if vote == true (and declined it otherwise).
+func (e *ElectionTracker) RecordVote(id pb.PeerID, vote bool, conflictIdx pb.Index, match bool) {
 	_, ok := e.votes[id]
 	if !ok {
 		e.votes[id] = vote
+		e.conflictInfo[id] = hintInfo{
+			conflictIdx: conflictIdx,
+			match:       match,
+		}
 	}
 }
 
@@ -55,9 +73,28 @@ func (e *ElectionTracker) TallyVotes() (granted int, rejected int, _ quorum.Vote
 	return granted, rejected, result
 }
 
+// GetGuessMatchEntryID returns the conflictIdx and match status for the
+// given peer ID.
+// If match is true, it means an entryID at conflictIdx on the
+// follower's log matches the entryID on the leader's log.
+// If match is false, it means the entryID at conflictIdx is either a best guess
+// on where to send to the follower or just r.raftLog.LastIndex().
+func (e *ElectionTracker) GetGuessMatchEntryID(
+	id pb.PeerID,
+) (conflictID pb.Index, match bool, exist bool) {
+	hint, ok := e.conflictInfo[id]
+	return hint.conflictIdx, hint.match, ok
+}
+
+func (e *ElectionTracker) GetVote(id pb.PeerID) bool {
+	vote := e.votes[id]
+	return vote
+}
+
 // ResetVotes prepares for a new round of vote counting via recordVote.
 func (e *ElectionTracker) ResetVotes() {
 	clear(e.votes)
+	clear(e.conflictInfo)
 }
 
 // TestingGetVotes exports the votes map for testing.
