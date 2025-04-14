e0cc86930bdd2738511666f1bf32f5219d75e447
Drew Kimball <drewk@cockroachlabs.com>
2025-04-14 15:11:43 -0600

    sql: add session seting for leaf txn admission control behavior
    This commit adds a `propagate_admission_header_to_leaf_transactions`
    session setting to control whether leaf txns inherit the admission header
    from root txns. The setting is on by default.
    
    Informs #144421
    
    Release note: None

--- pkg/sql/exec_util.go
+++ pkg/sql/exec_util.go
@@ -4181,6 +4181,10 @@ func (m *sessionDataMutator) SetVectorSearchBeamSize(val int32) {
 	m.data.VectorSearchBeamSize = val
 }
 
+func (m *sessionDataMutator) SetPropagateAdmissionHeaderToLeafTransactions(val bool) {
+	m.data.PropagateAdmissionHeaderToLeafTransactions = val
+}
+
 // Utility functions related to scrubbing sensitive information on SQL Stats.
 
 // quantizeCounts ensures that the Count field in the
