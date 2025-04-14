a707508e72d31a3a945b0ae08041cf6a736588cf
Brian Dillmann <brian.dillmann@cockroachlabs.com>
2025-04-14 15:35:06 -0400

    structlogging: conditionally log hot ranges if the node is burdened
    Prior to this change, if the system were enabled to, the hot ranges
    would log on a regular interval from each node in the system. This
    interval however was long (4h by default) and would leave a lot of gaps
    in the system, any hotspot which wasn't effectively continuous was
    likely to be missed from it.
    
    So that partner teams have better historical visibility into the system,
    we here add a check every minute for whether the node is in a "burdened"
    state. Burdened here is defined as any given range using greater than
    250ms cpu time per second. If it is, we log the hot ranges for that node.
    
    Doing so allows for both better precision, if a range is hot, it's
    likely to be logged, due to the minute by minute check, without
    sacrificing recall, in that these decisions are made at a node local
    level.
    
    For multi-tenant deployments, this check happens instead once every 5
    minutes, and polls & logs the entire cluster.
    
    Fixes: #138767
    Epic: CRDB-43150
    
    Release note (ops change): If the cluster setting
    `server.telemetry.hot_ranges_stats.enabled` is enabled,

--- pkg/server/structlogging/hot_ranges_log_job.go
+++ pkg/server/structlogging/hot_ranges_log_job.go
@@ -13,6 +13,14 @@ import (
 	"github.com/cockroachdb/errors"
 )
 
+// hot_ranges_log_job.go adds the required functions to satisfy
+// the jobs.Scheduler interface for the hot ranges logging job.
+// This is only required for app tenants in a multi-tenant deployment
+// as the app tenants have no notion of "local" ranges, and therefore
+// require a fanout to be performed to collect the hot ranges.
+// It's run as a job, as since fanout is required, only one node
+// needs to run it at any given time, as opposed to the every
+// node task behavior otherwise.
 func (s *hotRangesLoggingScheduler) Resume(ctx context.Context, execCtxI interface{}) error {
 	// This job is a forever running background job, and it is always safe to
 	// terminate the SQL pod whenever the job is running, so mark it as idle.
