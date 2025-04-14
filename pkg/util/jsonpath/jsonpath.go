31c7e26edfccb3ce700c63feeb71531f518104ce
Norman Chen <norman.chen@cockroachlabs.com>
2025-04-14 17:04:09 -0400

    jsonpath: validate variables before jsonpath evaluation
    Currently, our JSONPath evaluation implementation suppresses errors that
    occur during filter evaluation, which matches Postgres behaviour.
    However, we also suppress undefined variable errors, while Postgres
    throws these errors regardless of where they appear in the expression.
    
    This commit fixes this behaviour by:
    - Adding variable tracking during AST parsing to collect all variable names.
    - Adding upfront variable validation before evaluation begins.
    
    Release note: None

---
+++ pkg/util/jsonpath/jsonpath.go
@@ -0,0 +1,34 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package jsonpath
+
+type Jsonpath struct {
+	Strict bool
+	Path   Path
+}
+
+func (j Jsonpath) String() string {
+	var mode string
+	if j.Strict {
+		mode = "strict "
+	}
+	return mode + j.Path.String()
+}
+
+// ValidateAndCollectVariables walks the Jsonpath AST and collects all variable
+// names. It returns an error if the AST is invalid (last not in array subscripts,
+// @ in root expressions).
+func (j Jsonpath) ValidateAndCollectVariables() ([]string, error) {
+	vars := map[string]struct{}{}
+	if err := j.Path.Validate(vars, 0 /* nestingLevel */, false /* insideArraySubscript */); err != nil {
+		return nil, err
+	}
+	varsList := make([]string, 0, len(vars))
+	for v := range vars {
+		varsList = append(varsList, v)
+	}
+	return varsList, nil
+}
