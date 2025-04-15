8e932e02f430ac2dee482448c73f56c05eb01b00
Anthony Xu <anthony.xu@cockroachlabs.com>
2025-04-15 15:24:13 -0400

    raft: utilize hints from voting process to set follower progress post election
    After winning election and becoming leader,use the hints we recorded during the
    election phase to set follower progress to optimize for the first round of
    MsgApp/replication.
    
    If the follower voted for this new leader during election phase, then the leader
    can either:
     - If we know the voter's log matches ours, send the first round of MsgApp
       starting at the correct fork point and set the follower's state to
    StateReplicate.
     - If the voter's log doesn't match ours, send the first round of MsgApp at a
       best guess index.
    
    The term cache can be utilized for further optimization.
    
    See interaction test output changes to examine behavior changes.
    
    Epic: none
    Fixes: #143713
    Release note (performance improvement): Reduce replication tail latency after every raft leader election.

--- pkg/raft/raft.go
+++ pkg/raft/raft.go
@@ -1342,7 +1342,34 @@ func (r *raft) becomeLeader() {
 		panic("invalid transition [follower -> leader]")
 	}
 	r.step = stepLeader
-	r.reset(r.Term, nil)
+
+	// A special visit function to use the recorded hints and optimize the first
+	// round of MsgApp after becoming leader.
+	r.reset(r.Term, func(id pb.PeerID, pr *tracker.Progress) {
+		matchGuess := tracker.MatchGuess{Index: r.raftLog.lastIndex()}
+		if guess, ok := r.electionTracker.MatchGuess(id); ok {
+			matchGuess = guess
+		}
+
+		*pr = tracker.Progress{
+			Match:       0,
+			MatchCommit: 0,
+			Next:        matchGuess.Index + 1,
+			Inflights:   tracker.NewInflights(r.maxInflight, r.maxInflightBytes),
+			IsLearner:   pr.IsLearner,
+		}
+
+		if matchGuess.Match {
+			// Follower's log matches the leader's log at next-1.
+			// Set the follower's state to StateReplicate early.
+			r.becomeReplicate(pr)
+			// NB: this is a bit of a hack, but we need to set the Next again since
+			// r.becomeReplicate sets pr.Next to match + 1.
+			// Where match is 0 in this case.
+			pr.Next = matchGuess.Index + 1
+		}
+	})
+
 	// NB: The fortificationTracker holds state from a peer's leadership stint
 	// that's acted upon after it has stepped down. We reset it right before
 	// stepping up to become leader again, but not when stepping down as leader
@@ -1365,11 +1392,14 @@ func (r *raft) becomeLeader() {
 			return
 		}
 		// All peer flows, except the leader's own, are initially in StateProbe.
+		// However, if discovered during voting that the follower's log matches the
+		// leader's, the follower is set to StateReplicate.
 		// Account the probe state entering in metrics here. All subsequent flow
 		// state changes, while we are the leader, are counted in the corresponding
 		// methods: becomeProbe, becomeReplicate, becomeSnapshot.
-		assertTrue(pr.State == tracker.StateProbe, "peers must be in StateProbe on leader step up")
-		r.metrics.FlowsEnteredStateProbe.Inc(1)
+		if pr.State == tracker.StateProbe {
+			r.metrics.FlowsEnteredStateProbe.Inc(1)
+		}
 	})
 
 	// Conservatively set the pendingConfIndex to the last index in the
