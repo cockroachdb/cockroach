d8ac362fefbffc89edd9f3b34c69cef463e9f55d
Rebecca Taft <becca@cockroachlabs.com>
2025-04-17 10:55:03 -0500

    sql: disable buffered writes if ReturnRawMVCCValues needed
    Buffered writes don't yet support the ReturnRawMVCCValues option of
    Gets, Scans, and ReverseScans, which is needed to produce certain
    system columns. Therefore, this commit disables buffered writes if
    those system columns are requested when scanning data.
    
    Fixes #144273
    
    Release note: None

--- pkg/sql/colfetcher/index_join.go
+++ pkg/sql/colfetcher/index_join.go
@@ -602,6 +602,7 @@ func NewColIndexJoin(
 		false, /* singleUse */
 		shouldCollectStats,
 		false, /* alwaysReallocate */
+		txn,
 	}
 	if err = fetcher.Init(
 		fetcherAllocator, kvFetcher, tableArgs,
