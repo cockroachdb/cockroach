af4a003bcc36b33910509c9496ab351639f0e01d
Pavel Kalinnikov <pavel@cockroachlabs.com>
2025-04-17 16:16:45 +0100

    kvserver: add TODO for using a cheaper batch
    Epic: none
    Release note: none

--- pkg/kv/kvserver/replica_application_state_machine.go
+++ pkg/kv/kvserver/replica_application_state_machine.go
@@ -143,6 +143,9 @@ func (sm *replicaStateMachine) NewBatch() apply.Batch {
 	// make it safer.
 	b.r = r
 	b.applyStats = &sm.applyStats
+	// TODO(#144627): most commands do not need to read. Use NewWriteBatch because
+	// it is more efficient. If there are exceptions, sparingly use NewReader or
+	// NewBatch (if it needs to read its own writes, which is unlikely).
 	b.batch = r.store.TODOEngine().NewBatch()
 	r.mu.RLock()
 	b.state = r.shMu.state
