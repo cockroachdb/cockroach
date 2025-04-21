b3efda79f42f2059aa7434f65d1c13fde2e28da9
Ricky Stewart <ricky@cockroachlabs.com>
2025-04-21 11:51:00 -0500

    pgwire: skip `TestAuthenticationAndHBARules`
    See #144782
    
    Epic: none
    Release note: None

--- pkg/sql/pgwire/auth_test.go
+++ pkg/sql/pgwire/auth_test.go
@@ -127,6 +127,7 @@ import (
 // alongside the "ok" or "ERROR" message.
 func TestAuthenticationAndHBARules(t *testing.T) {
 	defer leaktest.AfterTest(t)()
+	skip.WithIssue(t, 144782)
 	skip.UnderRace(t, "takes >1min under race")
 
 	testutils.RunTrueAndFalse(t, "insecure", func(t *testing.T, insecure bool) {
