55db948618921151df699e1b987770d3f16cad8c
Faizan Qazi <faizan@cockroachlabs.com>
2025-04-23 18:02:48 -0400

    sql: automatically set / unset schema_locked for legacy statements
    Previously, schema_locked had to be manually set or unset by users when
    making schema changes. We modified the declarative schema changer to
    cover some of the more common backfilling schema changes to set / unset
    this automatically. Unfortunately, there are a still a number of schema
    changes in the legacy schema changer that are not covered. To address
    this, this patch adds support for setting / unsetting schema_locked in
    implicit transactions with the legacy schema changer.
    
    This patch adds support for a white list of non-backfilling schema
    changes, which do not impact schema locked. Updates made by the schema
    changes will be ignored by the schema feed, so unsetting schema_locked
    is unnecessary for them.
    
    Informs: #129694
    Release note: None

--- pkg/sql/alter_table.go
+++ pkg/sql/alter_table.go
@@ -14,10 +14,10 @@ import (
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/build"
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
 	"github.com/cockroachdb/cockroach/pkg/jobs"
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
 	"github.com/cockroachdb/cockroach/pkg/server/telemetry"
-	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
 	"github.com/cockroachdb/cockroach/pkg/sql/auditlogging/auditevents"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catalogkeys"
@@ -103,7 +103,7 @@ func (p *planner) AlterTable(ctx context.Context, n *tree.AlterTable) (planNode,
 
 	// Disallow schema changes if this table's schema is locked, unless it is to
 	// set/reset the "schema_locked" storage parameter.
-	if err = checkSchemaChangeIsAllowed(tableDesc, n, p.ExecCfg().Settings); err != nil {
+	if err = p.checkSchemaChangeIsAllowed(ctx, tableDesc, n); err != nil {
 		return nil, err
 	}
 
@@ -462,7 +462,7 @@ func (n *alterTableNode) startExec(params runParams) error {
 				for _, updated := range affected {
 					// Disallow schema change if the FK references a table whose schema is
 					// locked.
-					if err := checkSchemaChangeIsAllowed(updated, n.n, params.ExecCfg().Settings); err != nil {
+					if err := params.p.checkSchemaChangeIsAllowed(params.ctx, updated, n.n); err != nil {
 						return err
 					}
 					if err := params.p.writeSchemaChange(
@@ -2343,16 +2343,54 @@ func (p *planner) tryRemoveFKBackReferences(
 // checkSchemaChangeIsAllowed checks if a schema change is allowed on
 // this table. A schema change is disallowed if one of the following is true:
 //   - The schema_locked table storage parameter is true, and this statement is
-//     not modifying the value of schema_locked.
+//     cannot set schema_locked automatically via a whitelist.
 //   - The table is referenced by logical data replication jobs, and the statement
 //     is not in the allow list of LDR schema changes.
-func checkSchemaChangeIsAllowed(
-	desc catalog.TableDescriptor, n tree.Statement, settings *cluster.Settings,
+func (p *planner) checkSchemaChangeIsAllowed(
+	ctx context.Context, desc catalog.TableDescriptor, n tree.Statement,
 ) (ret error) {
-	if desc == nil {
+	// Adding descriptors can be skipped.
+	if desc == nil || desc.Adding() || p.descCollection.IsNewUncommitedDescriptor(desc.GetID()) {
 		return nil
 	}
-	if desc.IsSchemaLocked() && !tree.IsSetOrResetSchemaLocked(n) {
+	// Check if this schema change is on the allowed list, which will only
+	// be simple non-back filling schema changes. All commands except set/reset
+	// schema_locked are unsupported before 25.2
+	preventedBySchemaLocked := !p.ExecCfg().Settings.Version.IsActive(ctx, clusterversion.V25_3) &&
+		!tree.IsSetOrResetSchemaLocked(n)
+	// These schema changes are allowed because the events generated will always
+	// be ignored by the schema_locked. The tableEventFilter (in CDC schemafeed)
+	// only cares about a limited number of events:
+	// - ADD / DROP COLUMN (visible column)
+	// - TRUNCATE
+	// - ALTER PRIMARY KEY
+	// - ALTER LOCALITY
+	switch stmt := n.(type) {
+	case *tree.AlterTable:
+		for _, cmd := range stmt.Cmds {
+			switch t := cmd.(type) {
+			case *tree.AlterTableSetStorageParams:
+				// TTL expire after expressions can end up adding a column implicitly,
+				// so if this parameter is being mutated, we will block any modifications
+				// on a schema_locked table.
+				if t.StorageParams.GetVal("ttl_expire_after") != nil {
+					preventedBySchemaLocked = true
+				}
+			case *tree.AlterTableRenameColumn, *tree.AlterTableRenameConstraint,
+				*tree.AlterTableResetStorageParams, *tree.AlterTablePartitionByTable,
+				*tree.AlterTableSetOnUpdate, *tree.AlterTableDropNotNull,
+				*tree.AlterTableSetVisible, *tree.AlterTableDropStored,
+				*tree.AlterTableValidateConstraint, *tree.AlterTableInjectStats:
+			default:
+				preventedBySchemaLocked = true
+			}
+		}
+	case *tree.AlterIndex, *tree.DropTable, *tree.RenameColumn, *tree.RenameIndex,
+		*tree.RenameTable, *tree.AlterTableSetSchema:
+	default:
+		preventedBySchemaLocked = true
+	}
+	if desc.IsSchemaLocked() && preventedBySchemaLocked {
 		return sqlerrors.NewSchemaChangeOnLockedTableErr(desc.GetName())
 	}
 	if len(desc.TableDesc().LDRJobIDs) > 0 {
@@ -2362,7 +2400,7 @@ func checkSchemaChangeIsAllowed(
 				virtualColNames = append(virtualColNames, col.GetName())
 			}
 		}
-		kvWriterEnabled := sqlclustersettings.LDRWriterType(sqlclustersettings.LDRImmediateModeWriter.Get(&settings.SV))
+		kvWriterEnabled := sqlclustersettings.LDRWriterType(sqlclustersettings.LDRImmediateModeWriter.Get(&p.execCfg.Settings.SV))
 		if !tree.IsAllowedLDRSchemaChange(n, virtualColNames, kvWriterEnabled == sqlclustersettings.LDRWriterTypeLegacyKV) {
 			return sqlerrors.NewDisallowedSchemaChangeOnLDRTableErr(desc.GetName(), desc.TableDesc().LDRJobIDs)
 		}
