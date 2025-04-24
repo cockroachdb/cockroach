ea2a669aa18aee16b1eb20eedcdf179679519dbf
Matt White <matt.white@cockroachlabs.com>
2025-04-24 10:10:36 -0700

    sql: version gate vector index creation to 25.2 or later
    Vector indexes put down KV bytes that are nonsense to prior versions of
    CockroachDB. To avoid the case where a user starts upgrading a cluster
    to 25.2, creates a vector index, and then rolls back to a prior version,
    we're adding a gate to prevent index creation until the cluster is fully
    upgraded.
    
    Fixes: #144903
    Release note (sql change): Vector indexes will only be available once
    the entire cluster has been finalized on 25.2 or later.

--- pkg/sql/logictest/tests/local-mixed-24.3/generated_test.go
+++ pkg/sql/logictest/tests/local-mixed-24.3/generated_test.go
@@ -479,25 +479,11 @@ func TestLogic_create_as(
 	runLogicTest(t, "create_as")
 }
 
-func TestLogic_create_index(
-	t *testing.T,
-) {
-	defer leaktest.AfterTest(t)()
-	runLogicTest(t, "create_index")
-}
-
 func TestLogic_create_statements(
 	t *testing.T,
 ) {
 	defer leaktest.AfterTest(t)()
 	runLogicTest(t, "create_statements")
-}
-
-func TestLogic_create_table(
-	t *testing.T,
-) {
-	defer leaktest.AfterTest(t)()
-	runLogicTest(t, "create_table")
 }
 
 func TestLogic_cross_join(
@@ -806,13 +792,6 @@ func TestLogic_export(
 ) {
 	defer leaktest.AfterTest(t)()
 	runLogicTest(t, "export")
-}
-
-func TestLogic_expression_index(
-	t *testing.T,
-) {
-	defer leaktest.AfterTest(t)()
-	runLogicTest(t, "expression_index")
 }
 
 func TestLogic_external_connection_privileges(
@@ -1310,13 +1289,6 @@ func TestLogic_parallel_stmts_compat(
 ) {
 	defer leaktest.AfterTest(t)()
 	runLogicTest(t, "parallel_stmts_compat")
-}
-
-func TestLogic_partial_index(
-	t *testing.T,
-) {
-	defer leaktest.AfterTest(t)()
-	runLogicTest(t, "partial_index")
 }
 
 func TestLogic_partial_txn_commit(
@@ -2500,13 +2472,6 @@ func TestLogic_values(
 ) {
 	defer leaktest.AfterTest(t)()
 	runLogicTest(t, "values")
-}
-
-func TestLogic_vector_index(
-	t *testing.T,
-) {
-	defer leaktest.AfterTest(t)()
-	runLogicTest(t, "vector_index")
 }
 
 func TestLogic_vectorize_agg(
