d6201378d068e032c7635a1e4e4009728e64eecc
Jeff Swenson <jeffswenson@betterthannull.com>
2025-04-24 11:49:25 -0400

    streamclient: add internal app name prefix
    Previously, the replication queries executed by LDR and PCR were
    reported as user queries. Reporting user latency metrics for the
    replication streams is misleading because they are intended to have long
    execution times.
    
    Now, the LDR/PCR queries set an internal app name so the .internal
    metrics are used instead.
    
    Fixes: #144253
    Release note: SQL queries run on the source cluster by LDR and PCR will
    account to internal metrics like sql.statements.active.internal instead
    of the metrics like sql.statements.active that are used to monitor
    application workload.

--- pkg/ccl/crosscluster/streamclient/client.go
+++ pkg/ccl/crosscluster/streamclient/client.go
@@ -16,6 +16,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descs"
 	"github.com/cockroachdb/cockroach/pkg/sql/isql"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/catconstants"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/span"
@@ -317,11 +318,13 @@ type options struct {
 }
 
 func (o *options) appName() string {
-	const appNameBase = "repstream"
+	// NOTE: use an internal app name prefix so that the sql.*.internal metrics
+	// are used instead of the user facing metrics. The logic responsible for
+	// picking the metric family lives in conn_executor.go:SetupConn.
 	if o.streamID != 0 {
-		return fmt.Sprintf("%s job id=%d", appNameBase, o.streamID)
+		return fmt.Sprintf("%s repstream job id=%d", catconstants.InternalAppNamePrefix, o.streamID)
 	} else {
-		return appNameBase
+		return fmt.Sprintf("%s repstream", catconstants.InternalAppNamePrefix)
 	}
 }
 
