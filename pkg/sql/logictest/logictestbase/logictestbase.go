360ecef2f97b260e2624db33d5f3323d8d3a10d3
Mira Radeva <mira@cockroachlabs.com>
2025-04-28 12:17:49 -0400

    logictest: enable local-mixed-25.2 config
    Added `local-mixed-25.2` logictests.
    
    Epic: None
    Release note: None

--- pkg/sql/logictest/logictestbase/logictestbase.go
+++ pkg/sql/logictest/logictestbase/logictestbase.go
@@ -504,6 +504,16 @@ var LogicTestConfigs = []TestClusterConfig{
 		DeclarativeCorpusCollection: true,
 	},
 	{
+		// This config runs tests using 25.2 cluster version, simulating a node that
+		// is operating in a mixed-version cluster.
+		Name:                        "local-mixed-25.2",
+		NumNodes:                    1,
+		OverrideDistSQLMode:         "off",
+		BootstrapVersion:            clusterversion.V25_2,
+		DisableUpgrade:              true,
+		DeclarativeCorpusCollection: true,
+	},
+	{
 		// This config runs a cluster with 3 nodes, with a separate process per
 		// node. The nodes initially start on v24.3.
 		Name:                     "cockroach-go-testserver-24.3",
@@ -607,6 +617,7 @@ var DefaultConfigSets = map[string]ConfigSet{
 		"fakedist-disk",
 		"local-mixed-24.3",
 		"local-mixed-25.1",
+		"local-mixed-25.2",
 	),
 
 	// Special alias for all 5 node configs.
