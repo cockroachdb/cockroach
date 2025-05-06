0efb99fe052288a6b3cf773f189e7932ed7e8775
Drew Kimball <drewk@cockroachlabs.com>
2025-05-06 17:52:37 -0600

    opt: enable improved routine dependency tracking
    This commit flips `use_improved_routine_dependency_tracking` to on by
    default. This means that newly-created routines will not add unnecessary
    implicit insert columns to the list of dependencies, and will correctly
    track dependencies in the RETURNING clause of UPDATE and DELETE statements.
    
    Informs #145098
    Informs #146414
    
    Release note: None

--- pkg/sql/vars.go
+++ pkg/sql/vars.go
@@ -4049,7 +4049,7 @@ var varGen = map[string]sessionVar{
 		Get: func(evalCtx *extendedEvalContext, _ *kv.Txn) (string, error) {
 			return formatBoolAsPostgresSetting(evalCtx.SessionData().UseImprovedRoutineDependencyTracking), nil
 		},
-		GlobalDefault: globalFalse,
+		GlobalDefault: globalTrue,
 	},
 }
 
