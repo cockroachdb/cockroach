2dc55122d509e6d0223cf13a9ab79d4f3b2e7f55
Annie Pompa <annie@cockroachlabs.com>
2025-05-06 20:41:51 -0400

    dnm: regression test against premium ssdv1

--- pkg/roachprod/vm/azure/azure.go
+++ pkg/roachprod/vm/azure/azure.go
@@ -1062,7 +1062,7 @@ func (p *Provider) createVM(
 			// premium-disk specific disk configurations.
 			dataDisks[0].CreateOption = compute.DiskCreateOptionTypesEmpty
 			dataDisks[0].ManagedDisk = &compute.ManagedDiskParameters{
-				StorageAccountType: compute.StorageAccountTypesPremiumV2LRS,
+				StorageAccountType: compute.StorageAccountTypesPremiumLRS,
 			}
 		default:
 			err = errors.Newf("unsupported network disk type: %s", providerOpts.NetworkDiskType)
