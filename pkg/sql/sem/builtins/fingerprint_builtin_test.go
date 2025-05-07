cockroachdb/cockroach

commit 5e42a93a5a0cfab2c369806cbf86582268c67a0a

sql: fingerprint span of one range at a time
Previously entire 'spans' from partition spans were placed in each of
the ExportRequests used to export fingerprints. These potentially were
for spans much larger than a single range, making the five minute timeout
potentially too short. Instead, we now chunk these spans up along the range
boundaries to fingerprint just a range at a time. At the same time, this
changes the way we split work between works further: now instead of putting the
i'th span of each partition on one channel that all workers pull from -- in
the hope that this means workers end up hitting spans from different partitions
more or less evenly -- we instead spin up separate, smaller pools of workers
for each partition, each of which does this range-sized-span chunking. Thus the
total worker pool is no longer a fixed size (64) but rather is now 5 workers per
partition.

Release note: none.
Epic: none.

pkg/sql/fingerprint_span.go
@@ -30,8 +30,8 @@ import (
 var maxFingerprintNumWorkers = settings.RegisterIntSetting(
 	settings.ApplicationLevel,
 	"sql.fingerprint.max_span_parallelism",
-	"the maximum number of workers used to issue fingerprint ExportRequests",
-	64,
+	"the maximum number of workers per partition used to issue fingerprint ExportRequests",
+	5,
 	settings.PositiveInt,
 )
 
@@ -123,30 +123,6 @@ func (p *planner) fingerprintSpanFanout(
 		return 0, nil, err
 	}
 
-	maxLen := 0
-	count := 0
-	for _, partition := range spanPartitions {
-		length := len(partition.Spans)
-		maxLen = max(maxLen, length)
-		count += length
-	}
-	// Take as many workers as partitions to efficiently distribute work
-	maxWorkerCount = min(maxWorkerCount, count)
-
-	// Each span partition contains spans that are likely to be served by
-	// the same node. By round robin grabbing a span from each partition
-	// and pushing to the channel, ideally sequential workers won't attempt
-	// to read from the same node at the same time.
-	spanChannel := make(chan roachpb.Span, count)
-	for i := range maxLen {
-		for _, partition := range spanPartitions {
-			if i < len(partition.Spans) {
-				spanChannel <- partition.Spans[i]
-			}
-		}
-	}
-	close(spanChannel)
-
 	rv := struct {
 		syncutil.Mutex
 		ssts        [][]byte
@@ -155,34 +131,72 @@ func (p *planner) fingerprintSpanFanout(
 		ssts: make([][]byte, 0, len(spanPartitions)),
 	}
 
-	grp := ctxgroup.WithContext(ctx)
-	for range maxWorkerCount {
-		grp.GoCtx(func(ctx context.Context) error {
-			// Run until channel is empty
-			for {
-				select {
-				case <-ctx.Done():
-					return ctx.Err()
-				case sp, ok := <-spanChannel:
-					if !ok {
-						return nil
+	fingerprintPartition := func(
+		partition roachpb.Spans,
+	) func(ctx context.Context) error {
+		return func(ctx context.Context) error {
+			ch := make(chan roachpb.Span)
+
+			grp := ctxgroup.WithContext(ctx)
+			for range maxWorkerCount {
+				grp.GoCtx(func(ctx context.Context) error {
+					// Run until channel is empty
+					for {
+						select {
+						case <-ctx.Done():
+							return ctx.Err()
+						case sp, ok := <-ch:
+							if !ok {
+								return nil
+							}
+							localFingerprint, localSSTs, err := fingerprintSpanImpl(ctx, evalCtx, sp, startTime, allRevisions, stripped)
+							if err != nil {
+								return err
+							}
+							rv.Lock()
+							rv.ssts = append(rv.ssts, localSSTs...) // nolint:deferunlockcheck
+							rv.fingerprint = rv.fingerprint ^ localFingerprint
+							rv.Unlock()
+						}
 					}
-					localFingerprint, localSSTs, err := fingerprintSpanImpl(ctx, evalCtx, sp, startTime, allRevisions, stripped)
-					if err != nil {
-						return err
+				})
+			}
+
+			for _, part := range partition {
+				rdi, err := p.execCfg.RangeDescIteratorFactory.NewLazyIterator(ctx, part, 64)
+				if err != nil {
+					return err
+				}
+				remainingSpan := part
+				for ; rdi.Valid(); rdi.Next() {
+					rangeDesc := rdi.CurRangeDescriptor()
+					rangeSpan := roachpb.Span{Key: rangeDesc.StartKey.AsRawKey(), EndKey: rangeDesc.EndKey.AsRawKey()}
+					subspan := remainingSpan.Intersect(rangeSpan)
+					if !subspan.Valid() {
+						return errors.AssertionFailedf("%s not in %s of %s", rangeSpan, remainingSpan, part)
 					}
-					rv.Lock()
-					rv.ssts = append(rv.ssts, localSSTs...) // nolint:deferunlockcheck
-					rv.fingerprint = rv.fingerprint ^ localFingerprint
-					rv.Unlock()
+					ch <- subspan
+					remainingSpan.Key = subspan.EndKey
+				}
+				if err := rdi.Error(); err != nil {
+					return err
 				}
 			}
-		})
+			close(ch)
+			return grp.Wait()
+		}
 	}
 
+	// Start one span splitter/group of workers per partition, each of which waits
+	// for all its workers to finish before returning, and then wait for them all.
+	grp := ctxgroup.WithContext(ctx)
+	for _, part := range spanPartitions {
+		grp.GoCtx(fingerprintPartition(part.Spans))
+	}
 	if err := grp.Wait(); err != nil {
 		return 0, nil, err
 	}
+
 	return rv.fingerprint, rv.ssts, nil
 }
 

commit 2f1f85a58d81a97ae2d531acd2639f53765ca862

sql: fix crdb_internal.fingerprint in edge cases
This commit fixes a couple of bugs that were introduced in
5e42a93a5a0cfab2c369806cbf86582268c67a0a in `crdb_internal.fingerprint`
builtin. Namely, after that change it was possible that the new
coordinator goroutine never exits (because we forgot to check for the
context cancellation) which could block the server shutdown. It could
also happen if we had a `statement_timeout` set.

Relatedly, if the coordinator encounters an error itself (rather than
receives it from one of the workers), the coordinator would exit without
notifying the workers. This was the case since we only close the work
channel after having pushed out all sub-spans. I don't think this was
problematic in practice, but this is now fixed by explicitly canceling
the context of workers.

Release note: None

pkg/sql/fingerprint_span.go
@@ -13,7 +13,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/settings"
-	"github.com/cockroachdb/cockroach/pkg/sql/sem/eval"
 	"github.com/cockroachdb/cockroach/pkg/storage"
 	"github.com/cockroachdb/cockroach/pkg/util/admission/admissionpb"
 	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
@@ -102,7 +101,7 @@ func (p *planner) fingerprintSpanFanout(
 	defer sp.Finish()
 
 	var (
-		evalCtx    = p.EvalContext()
+		txn        = p.EvalContext().Txn
 		execCfg    = p.ExecutorConfig().(*ExecutorConfig)
 		dsp        = p.DistSQLPlanner()
 		extEvalCtx = p.ExtendedEvalContext()
@@ -110,7 +109,7 @@ func (p *planner) fingerprintSpanFanout(
 
 	maxWorkerCount := int(maxFingerprintNumWorkers.Get(execCfg.SV()))
 	if maxWorkerCount == 1 {
-		return fingerprintSpanImpl(ctx, evalCtx, span, startTime, allRevisions, stripped)
+		return fingerprintSpanImpl(ctx, txn, span, startTime, allRevisions, stripped)
 	}
 
 	planCtx, _, err := dsp.SetupAllNodesPlanning(ctx, extEvalCtx, execCfg)
@@ -135,21 +134,30 @@ func (p *planner) fingerprintSpanFanout(
 		partition roachpb.Spans,
 	) func(ctx context.Context) error {
 		return func(ctx context.Context) error {
-			ch := make(chan roachpb.Span)
+			// workCh is used to divide up the partition between workers. It is
+			// closed whenever there is no work to do. It might not be closed if
+			// the coordinator encounters an error.
+			workCh := make(chan roachpb.Span)
+			// The context will be canceled when the coordinator exits
+			// guaranteeing that the worker goroutines aren't leaked.
+			ctx, cancel := context.WithCancel(ctx)
+			defer cancel()
 
 			grp := ctxgroup.WithContext(ctx)
 			for range maxWorkerCount {
 				grp.GoCtx(func(ctx context.Context) error {
-					// Run until channel is empty
+					// Run until the work channel is empty or the coordinator
+					// exits.
 					for {
 						select {
 						case <-ctx.Done():
 							return ctx.Err()
-						case sp, ok := <-ch:
+						case sp, ok := <-workCh:
 							if !ok {
+								// No more work.
 								return nil
 							}
-							localFingerprint, localSSTs, err := fingerprintSpanImpl(ctx, evalCtx, sp, startTime, allRevisions, stripped)
+							localFingerprint, localSSTs, err := fingerprintSpanImpl(ctx, txn, sp, startTime, allRevisions, stripped)
 							if err != nil {
 								return err
 							}
@@ -175,14 +183,18 @@ func (p *planner) fingerprintSpanFanout(
 					if !subspan.Valid() {
 						return errors.AssertionFailedf("%s not in %s of %s", rangeSpan, remainingSpan, part)
 					}
-					ch <- subspan
+					select {
+					case workCh <- subspan:
+					case <-ctx.Done():
+						return ctx.Err()
+					}
 					remainingSpan.Key = subspan.EndKey
 				}
 				if err := rdi.Error(); err != nil {
 					return err
 				}
 			}
-			close(ch)
+			close(workCh)
 			return grp.Wait()
 		}
 	}
@@ -202,7 +214,7 @@ func (p *planner) fingerprintSpanFanout(
 
 func fingerprintSpanImpl(
 	ctx context.Context,
-	evalCtx *eval.Context,
+	txn *kv.Txn,
 	span roachpb.Span,
 	startTime hlc.Timestamp,
 	allRevisions, stripped bool,
@@ -213,7 +225,7 @@ func fingerprintSpanImpl(
 		filter = kvpb.MVCCFilter_All
 	}
 	header := kvpb.Header{
-		Timestamp: evalCtx.Txn.ReadTimestamp(),
+		Timestamp: txn.ReadTimestamp(),
 		// NOTE(ssd): Setting this disables async sending in
 		// DistSender.
 		ReturnElasticCPUResumeSpans: true,
@@ -246,7 +258,7 @@ func fingerprintSpanImpl(
 			5*time.Minute, func(ctx context.Context) error {
 				sp := tracing.SpanFromContext(ctx)
 				ctx, exportSpan := sp.Tracer().StartSpanCtx(ctx, "fingerprint.ExportRequest", tracing.WithParent(sp))
-				rawResp, pErr = kv.SendWrappedWithAdmission(ctx, evalCtx.Txn.DB().NonTransactionalSender(), header, admissionHeader, req)
+				rawResp, pErr = kv.SendWrappedWithAdmission(ctx, txn.DB().NonTransactionalSender(), header, admissionHeader, req)
 				recording = exportSpan.FinishAndGetConfiguredRecording()
 				if pErr != nil {
 					return pErr.GoError()

pkg/sql/sem/builtins/fingerprint_builtin_test.go
@@ -23,6 +23,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/stretchr/testify/require"
@@ -286,4 +287,46 @@ func TestFingerprintStripped(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, fingerprints["test"], fingerprints["test2"])
+}
+
+// TestFingerprintCancellation verifies that all goroutines used by the
+// fingerprint builtin respect the cancellation signal. It's a regression for
+// #146320.
+func TestFingerprintCancellation(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	rng, _ := randutil.NewTestRand()
+	ctx := context.Background()
+	s, sqlDB, _ := serverutils.StartServer(t, base.TestServerArgs{})
+	defer s.Stopper().Stop(ctx)
+	db := sqlutils.MakeSQLRunner(sqlDB)
+
+	// Create a table with decent number of ranges with some data in each range.
+	db.Exec(t, "CREATE TABLE t_cancel (k INT PRIMARY KEY, blob STRING)")
+	db.Exec(t, "INSERT INTO t_cancel SELECT i, repeat(i::STRING, 10000) FROM generate_series(1, 1000) AS g(i)")
+	db.Exec(t, "ALTER TABLE t_cancel SPLIT AT SELECT generate_series(1, 1000, 50)")
+
+	const query = `
+SELECT *
+FROM
+	crdb_internal.fingerprint(
+		crdb_internal.table_span((SELECT id FROM system.namespace WHERE name = 't_cancel')),
+		0::TIMESTAMPTZ,
+		false
+	)
+`
+	// Run the query a few times with different statement timeout.
+	conn, err := sqlDB.Conn(ctx)
+	require.NoError(t, err)
+	for i := 0; i < 5; i++ {
+		timeout := rng.Intn(30) + 1
+		_, err = conn.ExecContext(ctx, fmt.Sprintf("SET statement_timeout = '%dms'", timeout))
+		require.NoError(t, err)
+		_, err = conn.ExecContext(ctx, query)
+		// No error is ok, only an unexpected error is problematic.
+		if err != nil {
+			require.ErrorContains(t, err, "query execution canceled")
+		}
+	}
 }