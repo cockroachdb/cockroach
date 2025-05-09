24405001c5ac1ebd2e53c5a5d7b19ce096ffaa1e
Ricky Stewart <ricky@cockroachlabs.com>
2025-05-09 16:21:56 -0500

    *: upgrade to Go 1.24.3
    To keep the FIPS build working, some changes were necessary; as of the
    latest version, due to upstream changes to support FIPS in the standard
    Go distribution, Go refuses to use the X25519 curve. We opt into the
    `P256` curve instead explicitly only in FIPS mode. Further, this
    required moving some code around as otherwise this would introduce a
    dependency cycle. I expose the functions that were previously in
    `fipsccl` and moved to `fipscclbase` as aliases to minimize the amount
    of impacted code.
    
    Some other minor changes mostly in test code were also necessary to keep
    tests running.
    
    * [ ] Adjust the Pebble tests to run in new version.
    * [ ] Update [our `go` fork](https://github.com/cockroachdb/go) with a new branch containing our patches. Create a new branch `cockroach-go$GO_VERSION` and take note of the commit ID.
    * [ ] Update `build/teamcity/internal/release/build-and-publish-patched-go/commit.txt` with the commit ID in the `go` fork.
    * [ ] Update `build/teamcity/internal/release/build-and-publish-patched-go/impl.sh` with the new `GOVERS` and adjust SHA256 sums as necessary.
    * [ ] Adjust `GO_FIPS_COMMIT` for the FIPS Go toolchain ([source](./teamcity/internal/release/build-and-publish-patched-go/impl-fips.sh)).
    * [ ] Run the `Internal / Cockroach / Build / Toolchains / Publish Patched Go for Mac` build configuration in TeamCity with your latest version of the script above. Note the job depends on another job `Build and Publish Patched Go`. That job prints out the SHA256 of all tarballs, which you will need to copy-paste into `WORKSPACE` (see below). `Publish Patched Go for Mac` is an extra step that publishes the *signed* `go` binaries for macOS. That job also prints out the SHA256 of the Mac tarballs in particular.
    * [ ] Adjust `--@io_bazel_rules_go//go/toolchain:sdk_version` in [.bazelrc](../.bazelrc).
    * [ ] Bump the version in `WORKSPACE` under `go_download_sdk`. You may need to bump [rules_go](https://github.com/bazelbuild/rules_go/releases). Also edit the filenames listed in `sdks` and update all the hashes to match what you built in the step above.
    * [ ] Bump the version in `WORKSPACE` under `go_download_sdk` for the FIPS version of Go (`go_sdk_fips`).
    * [ ] Upgrade golang.org/x packages; these are maintained by the Go project and it's reasonable to upgrade them when doing our Go upgrade. Run `grep -e '^\tgolang.org/x' go.mod | grep -v vcs | grep -v image | grep -v typeparams | cut -w -f2 | sed 's/$/@latest/' | xargs go get`. (Note: we don't upgrade certain libraries that are not linked into CRDB, hence the `grep -v`.)
    * [ ] Run `./dev generate bazel --mirror`, then `bazel fetch @distdir//:archives` to ensure you've updated all hashes to the correct value.
    * [ ] Bump the go version in `go.mod`.
    * [ ] Bump the default installed version of Go in `bootstrap-debian.sh` ([source](./bootstrap/bootstrap-debian.sh)).
    * [ ] Replace other mentions of the older version of go (grep for `golang:<old_version>` and `go<old_version>`).
    
    Epic: DEVINF-1477
    Release note (build change): Upgrade to Go 1.24.3

--- build/teamcity/internal/release/build-and-publish-patched-go/impl-fips.sh
+++ build/teamcity/internal/release/build-and-publish-patched-go/impl-fips.sh
@@ -9,7 +9,7 @@
 set -xeuo pipefail
 
 GO_FIPS_REPO=https://github.com/golang-fips/go
-GO_FIPS_COMMIT=12327118900b0833266189a293cba8ad674901c1
+GO_FIPS_COMMIT=495095e7a12b6ce4cb63f32b9c2677efc298e9e9
 GOCOMMIT=$(grep -v ^# /bootstrap/commit.txt | head -n1)
 
 # Install build dependencies
@@ -25,17 +25,6 @@ cd /workspace
 git clone $GO_FIPS_REPO go
 cd go
 git checkout $GO_FIPS_COMMIT
-# Delete a patch that we don't want. This shouldn't be necessary when we upgrade
-# to Ubuntu 24.04. Without this removal, attempting to run the binary on our
-# current build infrastructure results in the following error:
-#     version `GLIBC_2.32' not found (required by external/go_sdk_fips/bin/go)
-rm ./patches/017-fix-linkage.patch
-# This patch doesn't apply on Go 1.23.6, but all the files are test-only, so
-# they shouldn't make a difference for the purpose of building the SDK.
-# When golang-fips supports Go 1.23.6, this line can be deleted.
-rm ./patches/023-crypto-tls-fix-config-time.patch
-# Lower the requirements in case we need to bootstrap with an older Go version
-sed -i "s/go mod tidy/go mod tidy -go=1.16/g" scripts/create-secondary-patch.sh
 GOLANG_REPO=https://github.com/cockroachdb/go.git ./scripts/full-initialize-repo.sh "$GOCOMMIT"
 cd go/src
 # add a special version modifier so we can explicitly use it in bazel
