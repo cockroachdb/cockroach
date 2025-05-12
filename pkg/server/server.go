07d19aebaf6d1e5922b3895ccee7c1677277df28
Shubham Dhama <shubham.dhama@cockroachlabs.com>
2025-05-12 18:07:41 +0530

    gossip: register `Gossip` service with DRPC server
    Enable the `MultiRaft` service on the DRPC server in addition to gRPC. This
    is controlled by `rpc.experimental_drpc.enabled` (off by default).
    
    Note: This only registers the service; the client is not updated to use the
    DRPC client, so this service will not have any functional effect.
    
    Epic: CRDB-48925
    Release note: None

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -411,6 +411,9 @@ func NewServer(cfg Config, stopper *stop.Stopper) (serverctl.ServerStartupInterf
 	}
 
 	gossip.RegisterGossipServer(grpcServer.Server, g)
+	if err := gossip.DRPCRegisterGossip(drpcServer.mux, g.AsDRPCServer()); err != nil {
+		return nil, err
+	}
 
 	var dialerKnobs nodedialer.DialerTestingKnobs
 	if dk := cfg.TestingKnobs.DialerKnobs; dk != nil {
