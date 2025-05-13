360f92ac07e55c220c59c3d3d58523500823cb78
Tobias Grieger <tobias.b.grieger@gmail.com>
2025-05-13 16:38:26 +0200

    aggmetric: remove unused method

--- pkg/util/metric/metric.go
+++ pkg/util/metric/metric.go
@@ -967,21 +967,6 @@ func (c *CounterFloat64) Atomic() *syncutil.AtomicFloat64 {
 	return &c.count
 }
 
-// Update atomically sets the current value of the counter. The value must not
-// be smaller than the existing value.
-//
-// Update is intended to be used when the counter itself is not the source of
-// truth; instead it is a (periodically updated) copy of a counter that is
-// maintained elsewhere.
-func (c *CounterFloat64) Update(val float64) {
-	if buildutil.CrdbTestBuild {
-		if prev := c.count.Load(); val < prev {
-			panic(fmt.Sprintf("Counters should not decrease, prev: %f, new: %f.", prev, val))
-		}
-	}
-	c.count.Store(val)
-}
-
 // UpdateIfHigher atomically sets the current value of the counter, unless the
 // current value is already greater.
 func (c *CounterFloat64) UpdateIfHigher(i float64) {
