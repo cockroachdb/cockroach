89c6fa1887e80f85f7978a157766e0c2ce6f4928
Tobias Grieger <tobias.b.grieger@gmail.com>
2025-05-13 16:34:35 +0200

    aggmetric: change Update to UpdateIfHigher
    This avoids incrementing the underlying Counter by a negative
    value, which we assert against.

--- pkg/util/metric/aggmetric/agg_metric_test.go
+++ pkg/util/metric/aggmetric/agg_metric_test.go
@@ -118,7 +118,7 @@ func TestAggMetric(t *testing.T) {
 
 	t.Run("basic", func(t *testing.T) {
 		c2.Inc(2)
-		c3.Update(3)
+		c3.UpdateIfHigher(3)
 		c3.Inc(1)
 		d2.Inc(123456.5)
 		d3.UpdateIfHigher(9.5)
