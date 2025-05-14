bd5008a45ed078e6f122a6f91f1eb418accf9c52
Steven Danna <danna@cockroachlabs.com>
2025-05-14 11:37:41 +0100

    sql: enable BufferedWrites in a TestTxnAutoRetry and TestAbortedTxnOnlyRetriedOnce
    Tests related to transaction retries often depend on intercepting and
    modifying specific KV requests generated by SQL transactions. Since
    buffered writes transforms requests and in general changes the
    timeline of transactions, it invalidates the tests assumptions. We are
    currently auditing tests where we've disabled buffered writes to
    ensure we haven't ignored an actual problem.
    
    Here, I enable buffered writes in two tests and explain why some
    assertions need to change when buffered writes are enabled.
    
    Informs #146238
    
    Release note: None

--- pkg/kv/kvclient/kvcoord/txn_interceptor_write_buffer.go
+++ pkg/kv/kvclient/kvcoord/txn_interceptor_write_buffer.go
@@ -33,7 +33,7 @@ var BufferedWritesEnabled = settings.RegisterBoolSetting(
 	settings.ApplicationLevel,
 	"kv.transaction.write_buffering.enabled",
 	"if enabled, transactional writes are buffered on the client",
-	metamorphic.ConstantWithTestBool("kv.transactional.write_buffering.enabled", false /* defaultValue */),
+	metamorphic.ConstantWithTestBool("kv.transaction.write_buffering.enabled", false /* defaultValue */),
 	settings.WithPublic,
 )
 
