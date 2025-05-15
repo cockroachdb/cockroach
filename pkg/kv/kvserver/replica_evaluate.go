15b04c69a4c2ce3f3013c72e53112b03cf59f63c
Steven Danna <danna@cockroachlabs.com>
2025-05-15 09:38:55 +0100

    roachpb: remove WriteTooOld bit from Transaction
    In 83233220, we removed the mechanism that set WriteTooOld to
    true. Here, we remove the option completely as we no longer require
    support for nodes on 23.1 or earlier.
    
    Epic: None
    
    Release note: None

--- pkg/kv/kvserver/replica_evaluate.go
+++ pkg/kv/kvserver/replica_evaluate.go
@@ -215,15 +215,6 @@ func evaluateBatch(
 	evalPath batchEvalPath,
 	omitInRangefeeds bool, // only relevant for transactional writes
 ) (_ *kvpb.BatchResponse, _ result.Result, retErr *kvpb.Error) {
-	defer func() {
-		// Ensure that errors don't carry the WriteTooOld flag set. The client
-		// handles non-error responses with the WriteTooOld flag set, and errors
-		// with this flag set confuse it.
-		if retErr != nil && retErr.GetTxn() != nil {
-			retErr.GetTxn().WriteTooOld = false
-		}
-	}()
-
 	// NB: Don't mutate BatchRequest directly.
 	baReqs := ba.Requests
 
