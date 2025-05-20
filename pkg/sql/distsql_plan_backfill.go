cockroachdb/cockroach

commit c9ca8bdc4037e63f82a3181a2dbb9e0cf09aef5c

bulk: optionally create initial splits before first flush
This adds an option to 'pre-split' before the first buffer is flushes when
bulk ingesting data, using the data of the first buffer to pick split points.

Release note: none.

pkg/sql/distsql_plan_backfill.go
@@ -65,6 +65,7 @@ func (dsp *DistSQLPlanner) createBackfillerPhysicalPlan(
 	for i, sp := range spanPartitions {
 		ib := &execinfrapb.BackfillerSpec{}
 		*ib = spec
+		ib.InitialSplits = int32(len(spanPartitions))
 		ib.Spans = sp.Spans
 
 		proc := physicalplan.Processor{

pkg/ccl/importccl/import_processor_planning.go
@@ -264,6 +264,7 @@ func makeImportReaderSpecs(
 				ResumePos:             make(map[int32]int64),
 				UserProto:             user.EncodeProto(),
 				DatabasePrimaryRegion: details.DatabasePrimaryRegion,
+				InitialSplits:         int32(len(sqlInstanceIDs)),
 			}
 			inputSpecs = append(inputSpecs, spec)
 		}

pkg/ccl/importccl/import_processor.go
@@ -351,14 +351,15 @@ func ingestKvs(
 	minBufferSize, maxBufferSize, stepSize := importBufferConfigSizes(flowCtx.Cfg.Settings,
 		true /* isPKAdder */)
 	pkIndexAdder, err := flowCtx.Cfg.BulkAdder(ctx, flowCtx.Cfg.DB, writeTS, kvserverbase.BulkAdderOptions{
-		Name:                   "pkAdder",
-		DisallowShadowingBelow: writeTS,
-		SkipDuplicates:         true,
-		MinBufferSize:          minBufferSize,
-		MaxBufferSize:          maxBufferSize,
-		StepBufferSize:         stepSize,
-		SSTSize:                flushSize,
-		WriteAtRequestTime:     writeAtRequestTime,
+		Name:                     "pkAdder",
+		DisallowShadowingBelow:   writeTS,
+		SkipDuplicates:           true,
+		MinBufferSize:            minBufferSize,
+		MaxBufferSize:            maxBufferSize,
+		StepBufferSize:           stepSize,
+		SSTSize:                  flushSize,
+		InitialSplitsIfUnordered: int(spec.InitialSplits),
+		WriteAtRequestTime:       writeAtRequestTime,
 	})
 	if err != nil {
 		return nil, err
@@ -368,14 +369,15 @@ func ingestKvs(
 	minBufferSize, maxBufferSize, stepSize = importBufferConfigSizes(flowCtx.Cfg.Settings,
 		false /* isPKAdder */)
 	indexAdder, err := flowCtx.Cfg.BulkAdder(ctx, flowCtx.Cfg.DB, writeTS, kvserverbase.BulkAdderOptions{
-		Name:                   "indexAdder",
-		DisallowShadowingBelow: writeTS,
-		SkipDuplicates:         true,
-		MinBufferSize:          minBufferSize,
-		MaxBufferSize:          maxBufferSize,
-		StepBufferSize:         stepSize,
-		SSTSize:                flushSize,
-		WriteAtRequestTime:     writeAtRequestTime,
+		Name:                     "indexAdder",
+		DisallowShadowingBelow:   writeTS,
+		SkipDuplicates:           true,
+		MinBufferSize:            minBufferSize,
+		MaxBufferSize:            maxBufferSize,
+		StepBufferSize:           stepSize,
+		SSTSize:                  flushSize,
+		InitialSplitsIfUnordered: int(spec.InitialSplits),
+		WriteAtRequestTime:       writeAtRequestTime,
 	})
 	if err != nil {
 		return nil, err

pkg/kv/bulk/buffering_adder.go
@@ -49,6 +49,8 @@ type BufferingAdder struct {
 
 	sorted bool
 
+	initialSplits int
+
 	flushCounts struct {
 		total      int
 		bufferSize int
@@ -119,6 +121,7 @@ func MakeBulkAdder(
 		incrementBufferSize: opts.StepBufferSize,
 		bulkMon:             bulkMon,
 		sorted:              true,
+		initialSplits:       opts.InitialSplitsIfUnordered,
 	}
 
 	// If no monitor is attached to the instance of a bulk adder, we do not
@@ -189,15 +192,11 @@ func (b *BufferingAdder) Add(ctx context.Context, key roachpb.Key, value []byte)
 			if err := b.memAcc.Grow(ctx, b.incrementBufferSize); err != nil {
 				// If we are unable to reserve the additional memory then flush the
 				// buffer, and continue as normal.
-				b.flushCounts.bufferSize++
-				log.VEventf(ctx, 3, "buffer size triggering flush of %s buffer", sz(b.curBuf.MemSize))
-				return b.Flush(ctx)
+				return b.sizeFlush(ctx)
 			}
 			b.curBufferSize += b.incrementBufferSize
 		} else {
-			b.flushCounts.bufferSize++
-			log.VEventf(ctx, 3, "buffer size triggering flush of %s buffer", sz(b.curBuf.MemSize))
-			return b.Flush(ctx)
+			return b.sizeFlush(ctx)
 		}
 	}
 	return nil
@@ -213,8 +212,18 @@ func (b *BufferingAdder) IsEmpty() bool {
 	return b.curBuf.Len() == 0
 }
 
+func (b *BufferingAdder) sizeFlush(ctx context.Context) error {
+	b.flushCounts.bufferSize++
+	log.VEventf(ctx, 3, "buffer size triggering flush of %s buffer", sz(b.curBuf.MemSize))
+	return b.doFlush(ctx, true)
+}
+
 // Flush flushes any buffered kvs to the batcher.
 func (b *BufferingAdder) Flush(ctx context.Context) error {
+	return b.doFlush(ctx, false)
+}
+
+func (b *BufferingAdder) doFlush(ctx context.Context, forSize bool) error {
 	if b.curBuf.Len() == 0 {
 		if b.onFlush != nil {
 			b.onFlush(b.sink.GetBatchSummary())
@@ -238,6 +247,14 @@ func (b *BufferingAdder) Flush(ctx context.Context) error {
 
 	beforeFlush := timeutil.Now()
 	b.flushCounts.totalSort += beforeFlush.Sub(beforeSort)
+
+	// If this is the first flush and is due to size, if it was unsorted then
+	// create initial splits if requested before flushing.
+	if b.flushCounts.total == 0 && forSize && b.initialSplits != 0 && !b.sorted {
+		if err := b.createInitialSplits(ctx); err != nil {
+			return err
+		}
+	}
 
 	for i := range b.curBuf.entries {
 		mvccKey.Key = b.curBuf.Key(i)
@@ -280,6 +297,22 @@ func (b *BufferingAdder) Flush(ctx context.Context) error {
 		b.onFlush(b.sink.GetBatchSummary())
 	}
 	b.curBuf.Reset()
+	return nil
+}
+
+func (b *BufferingAdder) createInitialSplits(ctx context.Context) error {
+	targetSize := b.curBuf.Len() / b.initialSplits
+	log.Infof(ctx, "creating up to %d initial splits from %d keys in %s buffer", b.initialSplits, b.curBuf.Len(), sz(b.curBuf.MemSize))
+
+	hour := hlc.Timestamp{WallTime: timeutil.Now().Add(time.Hour).UnixNano()}
+
+	for i := targetSize; i < b.curBuf.Len(); i += targetSize {
+		k := b.curBuf.Key(i)
+		log.VEventf(ctx, 1, "splitting at key %d / %d: %s", i, b.curBuf.Len(), k)
+		if err := b.sink.db.SplitAndScatter(ctx, k, hour); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 

pkg/kv/kvserver/kvserverbase/bulk_adder.go
@@ -71,6 +71,15 @@ type BulkAdderOptions struct {
 	// WriteAtRequestTime is used to set the corresponding field when sending
 	// constructed SSTables to AddSSTable. See roachpb.AddSSTableRequest.
 	WriteAtRequestTime bool
+
+	// InitialSplitsIfUnordered specifies a number of splits to make before the
+	// first flush of the buffer if the contents of that buffer were unsorted.
+	// Being unsorted suggests the remaining input is likely unsorted as well and
+	// thus future flushes and flushes on other nodes will overlap, so we want to
+	// pre-split the target span now before we start filling it, using the keys in
+	// the first buffer to pick split points in the hope it is a representative
+	// sample of the overall input.
+	InitialSplitsIfUnordered int
 }
 
 // DisableExplicitSplits can be returned by a SplitAndScatterAfter function to

pkg/sql/execinfrapb/processors_bulk_io.proto
@@ -72,6 +72,8 @@ message BackfillerSpec {
   repeated uint32 indexes_to_backfill = 8 [(gogoproto.casttype) = "github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb.IndexID"];
 
   reserved 6;
+
+  optional int32 initial_splits = 11 [(gogoproto.nullable) = false];
 }
 
 // JobProgress identifies the job to report progress on. This reporting
@@ -152,7 +154,9 @@ message ReadImportDataSpec {
     (gogoproto.casttype) = "github.com/cockroachdb/cockroach/pkg/sql/catalog/catpb.RegionName"
   ];
 
-  // NEXTID: 18
+  optional int32 initial_splits = 18 [(gogoproto.nullable) = false];
+
+  // NEXTID: 19
 }
 
 message StreamIngestionDataSpec {

pkg/sql/rowexec/indexbackfiller.go
@@ -193,12 +193,13 @@ func (ib *indexBackfiller) ingestIndexEntries(
 	sstSize := func() int64 { return backillerSSTSize.Get(&ib.flowCtx.Cfg.Settings.SV) }
 	stepSize := backfillerBufferIncrementSize.Get(&ib.flowCtx.Cfg.Settings.SV)
 	opts := kvserverbase.BulkAdderOptions{
-		SSTSize:        sstSize,
-		MinBufferSize:  minBufferSize,
-		MaxBufferSize:  maxBufferSize,
-		StepBufferSize: stepSize,
-		SkipDuplicates: ib.ContainsInvertedIndex(),
-		BatchTimestamp: ib.spec.ReadAsOf,
+		SSTSize:                  sstSize,
+		MinBufferSize:            minBufferSize,
+		MaxBufferSize:            maxBufferSize,
+		StepBufferSize:           stepSize,
+		SkipDuplicates:           ib.ContainsInvertedIndex(),
+		BatchTimestamp:           ib.spec.ReadAsOf,
+		InitialSplitsIfUnordered: int(ib.spec.InitialSplits),
 	}
 	adder, err := ib.flowCtx.Cfg.BulkAdder(ctx, ib.flowCtx.Cfg.DB, ib.spec.WriteAsOf, opts)
 	if err != nil {

commit 0564fd474865284b0f145cf7f116fc996b13dd8c

sql: improve IMPORTs and index backfills on very large clusters
This commit fixes an oversight of the behavior introduced in
c9ca8bdc4037e63f82a3181a2dbb9e0cf09aef5c as it pertains to very large
(200+ node) clusters. In particular, in that change we introduced the logic to
create initial splits via the BulkAdder when it is flushed due to
reaching the maximum size (128MiB for IMPORT into PKs, and 512MiB for IMPORT
into secondary indexes as well as index backfills). The rationale for
having _some_ initial splits is sound; however, in that change we made
it so that _each processor_ can create N initial split points where N is
the number of nodes in the cluster. In other words, a single bulk
operation could result in N^2 number of splits, which could overwhelm
the node that happened to be the leaseholder for the initial empty range
for the ingest.

I think the intention was that we would create a total number of splits
that is linearly dependent on the number of nodes in the cluster, so
this commit introduces a couple of cluster settings that control the
multiple for the linear function. I did a few runs of IMPORT of TPCC 50k
and TPCC 100k on 100 node cluster, and it looks like a few numbers in
[2, 16] range behave roughly the same, so I picked 3 as the default
value for both cluster settings. In other words, IMPORTs and index
backfills will now create N x 3 number of initial split points by
default.

An alternative strategy could have been to assign one (or small number)
processor to do some number of splits based on its first SST file.
I decided to not pursue this alternative under the assumption that we'll
get better initial split point distribution if they are created across
all nodes. With just one processor doing the splits, we could over-split
a part of the key space that that particular node happens to write.

I decided to not include a release note given that we expect this to
have noticeable impact only on 200+ node clusters.

Release note: None

pkg/sql/distsql_plan_backfill.go
@@ -11,6 +11,7 @@ import (
 	"unsafe"
 
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/settings"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/execinfrapb"
@@ -71,6 +72,14 @@ func initIndexBackfillMergerSpec(
 	}, nil
 }
 
+var initialSplitsPerProcessor = settings.RegisterIntSetting(
+	settings.ApplicationLevel,
+	"bulkio.index_backfill.initial_splits_per_processor",
+	"number of initial splits each index backfill processor with enough data will create",
+	3,
+	settings.NonNegativeInt,
+)
+
 // createBackfiller generates a plan consisting of index/column backfiller
 // processors, one for each node that has spans that we are reading. The plan is
 // finalized.
@@ -92,10 +101,11 @@ func (dsp *DistSQLPlanner) createBackfillerPhysicalPlan(
 	}
 	stageID := p.NewStage(containsRemoteProcessor, false /* allowPartialDistribution */)
 	p.ResultRouters = make([]physicalplan.ProcessorIdx, len(spanPartitions))
+	initialSplits := initialSplitsPerProcessor.Get(&planCtx.ExtendedEvalCtx.Settings.SV)
 	for i, sp := range spanPartitions {
 		ib := &execinfrapb.BackfillerSpec{}
 		*ib = spec
-		ib.InitialSplits = int32(len(spanPartitions))
+		ib.InitialSplits = int32(initialSplits)
 		ib.Spans = sp.Spans
 
 		proc := physicalplan.Processor{

pkg/sql/importer/BUILD.bazel
@@ -30,7 +30,6 @@ go_library(
     importpath = "github.com/cockroachdb/cockroach/pkg/sql/importer",
     visibility = ["//visibility:public"],
     deps = [
-        "//pkg/base",
         "//pkg/cloud",
         "//pkg/clusterversion",
         "//pkg/col/coldata",

pkg/sql/importer/import_job.go
@@ -100,6 +100,14 @@ var processorsPerNode = settings.RegisterIntSetting(
 	settings.PositiveInt,
 )
 
+var initialSplitsPerProcessor = settings.RegisterIntSetting(
+	settings.ApplicationLevel,
+	"bulkio.import.initial_splits_per_processor",
+	"number of initial splits each import processor with enough data will create",
+	3,
+	settings.NonNegativeInt,
+)
+
 var performConstraintValidation = settings.RegisterBoolSetting(
 	settings.ApplicationLevel,
 	"bulkio.import.constraint_validation.unsafe.enabled",
@@ -306,9 +314,12 @@ func (r *importResumer) Resume(ctx context.Context, execCtx interface{}) error {
 	}
 
 	procsPerNode := int(processorsPerNode.Get(&p.ExecCfg().Settings.SV))
+	initialSplitsPerProc := int(initialSplitsPerProcessor.Get(&p.ExecCfg().Settings.SV))
 
-	res, err := ingestWithRetry(ctx, p, r.job, tables, typeDescs, files, format, details.Walltime,
-		r.testingKnobs, procsPerNode)
+	res, err := ingestWithRetry(
+		ctx, p, r.job, tables, typeDescs, files, format, details.Walltime,
+		r.testingKnobs, procsPerNode, initialSplitsPerProc,
+	)
 	if err != nil {
 		return err
 	}
@@ -1287,6 +1298,7 @@ func ingestWithRetry(
 	walltime int64,
 	testingKnobs importTestingKnobs,
 	procsPerNode int,
+	initialSplitsPerProc int,
 ) (kvpb.BulkOpSummary, error) {
 	ctx, sp := tracing.ChildSpan(ctx, "importer.ingestWithRetry")
 	defer sp.Finish()
@@ -1308,7 +1320,7 @@ func ingestWithRetry(
 	for r := retry.StartWithCtx(ctx, retryOpts); r.Next(); {
 		for {
 			res, err = distImport(
-				ctx, execCtx, job, tables, typeDescs, from, format, walltime, testingKnobs, procsPerNode,
+				ctx, execCtx, job, tables, typeDescs, from, format, walltime, testingKnobs, procsPerNode, initialSplitsPerProc,
 			)
 			// If we got a re-planning error, then do at least one more attempt
 			// regardless of the retry duration.

pkg/sql/importer/import_processor_planning.go
@@ -12,7 +12,6 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/jobs"
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobsprofiler"
@@ -63,6 +62,7 @@ func distImport(
 	walltime int64,
 	testingKnobs importTestingKnobs,
 	procsPerNode int,
+	initialSplitsPerProc int,
 ) (kvpb.BulkOpSummary, error) {
 	ctx, sp := tracing.ChildSpan(ctx, "importer.distImport")
 	defer sp.Finish()
@@ -87,8 +87,10 @@ func distImport(
 			sqlInstanceIDs[i], sqlInstanceIDs[j] = sqlInstanceIDs[j], sqlInstanceIDs[i]
 		})
 
-		inputSpecs := makeImportReaderSpecs(job, tables, typeDescs, from, format, sqlInstanceIDs, walltime,
-			execCtx.User(), procsPerNode)
+		inputSpecs := makeImportReaderSpecs(
+			job, tables, typeDescs, from, format, len(sqlInstanceIDs), /* numSQLInstances */
+			walltime, execCtx.User(), procsPerNode, initialSplitsPerProc,
+		)
 
 		p := planCtx.NewPhysicalPlan()
 
@@ -294,14 +296,15 @@ func makeImportReaderSpecs(
 	typeDescs []*descpb.TypeDescriptor,
 	from []string,
 	format roachpb.IOFileFormat,
-	sqlInstanceIDs []base.SQLInstanceID,
+	numSQLInstances int,
 	walltime int64,
 	user username.SQLUsername,
 	procsPerNode int,
+	initialSplitsPerProc int,
 ) []*execinfrapb.ReadImportDataSpec {
 	details := job.Details().(jobspb.ImportDetails)
 	// For each input file, assign it to a node.
-	inputSpecs := make([]*execinfrapb.ReadImportDataSpec, 0, len(sqlInstanceIDs)*procsPerNode)
+	inputSpecs := make([]*execinfrapb.ReadImportDataSpec, 0, numSQLInstances*procsPerNode)
 	progress := job.Progress()
 	importProgress := progress.GetImport()
 	for i, input := range from {
@@ -322,7 +325,7 @@ func makeImportReaderSpecs(
 				ResumePos:             make(map[int32]int64),
 				UserProto:             user.EncodeProto(),
 				DatabasePrimaryRegion: details.DatabasePrimaryRegion,
-				InitialSplits:         int32(len(sqlInstanceIDs)),
+				InitialSplits:         int32(initialSplitsPerProc),
 			}
 			inputSpecs = append(inputSpecs, spec)
 		}