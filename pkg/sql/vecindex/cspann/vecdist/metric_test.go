c24ad12906418ed31c30cea53c4518545f488173
Andrew Kimball <andyk@cockroachlabs.com>
2025-05-21 05:16:00 -0400

    cspann: use mean centroid for RaBitQ quantization
    Switch RaBitQ quantization to always use the mean centroid, even for
    InnerProduct and Cosine distance metrics. Preivously, these other metrics
    were computing the spherical centroid (i.e. normalizing the mean centroid
    to be a unit vector). While the RaBitQ paper only mentions other distance
    functions in passing, it looks like the mean centroid should be used for
    them as well. Spherical centroids should only be used when performing
    K-means clustering, as with FAISS.
    
    Note that there are no backwards-compat concerns here, because other
    distance functions have not yet been enabled - they're still in the
    development phase.
    
    Epic: CRDB-42943
    Release note: None

--- pkg/sql/vecindex/cspann/vecdist/metric_test.go
+++ pkg/sql/vecindex/cspann/vecdist/metric_test.go
@@ -10,10 +10,51 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/util/vector"
 	"github.com/stretchr/testify/require"
+	"gonum.org/v1/gonum/floats/scalar"
 )
 
 func TestMetric(t *testing.T) {
-	require.Equal(t, float32(10), Measure(L2Squared, vector.T{1, 2}, vector.T{4, 3}))
-	require.Equal(t, float32(-10), Measure(InnerProduct, vector.T{1, 2}, vector.T{4, 3}))
-	require.Equal(t, float32(-9), Measure(Cosine, vector.T{1, 2}, vector.T{4, 3}))
+	l2Sq := Measure(L2Squared, vector.T{1, 2}, vector.T{4, 3})
+	require.Equal(t, float32(10), l2Sq)
+
+	ip := Measure(InnerProduct, vector.T{1, 2}, vector.T{4, 3})
+	require.Equal(t, float32(-10), ip)
+
+	cos := Measure(Cosine, vector.T{1, 2}, vector.T{4, 3})
+	require.Equal(t, float32(-9), cos)
+
+	cos = Measure(Cosine, vector.T{1, 0}, vector.T{0.7071, 0.7071})
+	require.Equal(t, float64(0.2929), scalar.Round(float64(cos), 4))
+
+	// Test zero product of norms.
+	cos = Measure(Cosine, vector.T{1, 0}, vector.T{0, 1})
+	require.Equal(t, float32(1), cos)
+}
+
+func TestParseMetric(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expect   Metric
+		hasError bool
+	}{
+		{L2Squared.String(), L2Squared, false},
+		{"l2squared", L2Squared, false},
+		{"l2-squared", L2Squared, false},
+		{InnerProduct.String(), InnerProduct, false},
+		{"innerproduct", InnerProduct, false},
+		{"inner-product", InnerProduct, false},
+		{Cosine.String(), Cosine, false},
+		{"cosine", Cosine, false},
+		{"not-a-metric", 0, true},
+	}
+
+	for _, tc := range testCases {
+		metric, err := ParseMetric(tc.input)
+		if tc.hasError {
+			require.Errorf(t, err, "input: %q", tc.input)
+		} else {
+			require.NoErrorf(t, err, "input: %q", tc.input)
+			require.Equal(t, tc.expect, metric, "input: %q", tc.input)
+		}
+	}
 }
