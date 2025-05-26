db23828a04ab8763c849966b06e6b45ee2777552
souravcrl <sourav.sarangi@cockroachlabs.com>
2025-05-27 03:14:43 +0530

    sql,sessioninit: return PROVISIONING_SOURCE role option from GetUserSessionInitInfo
    This allows the PROVISIONING_SOURCE that has been configured to be used during
    authentication.
    
    Release note: None

--- pkg/security/provisioning/provisioning_source.go
+++ pkg/security/provisioning/provisioning_source.go
@@ -35,6 +35,7 @@ type Source struct {
 // method prefixed IDP URI.
 func ParseProvisioningSource(sourceStr string) (source *Source, err error) {
 	var idp string
+	source = new(Source)
 	if source.authMethod, idp, err = parseAuthMethod(sourceStr); err != nil {
 		return nil, err
 	}
@@ -54,9 +55,13 @@ func parseAuthMethod(sourceStr string) (authMethod string, idp string, err error
 	return
 }
 
-func parseIDP(idp string) (url *url.URL, err error) {
-	if url, err = url.Parse(idp); err != nil {
+func parseIDP(idp string) (u *url.URL, err error) {
+	if u, err = url.Parse(idp); err != nil {
 		return nil, errors.Wrapf(err, "provided IDP %q in PROVISIONING_SOURCE is non parseable", idp)
 	}
 	return
+}
+
+func (source *Source) Size() int {
+	return len(source.authMethod) + len(source.idp.String())
 }
