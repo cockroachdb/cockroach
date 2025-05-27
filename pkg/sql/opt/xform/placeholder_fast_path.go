572025d8a5824ba80b78bd9cbb74aa710198c830
Marcus Gartner <marcus@cockroachlabs.com>
2025-05-27 16:29:20 -0400

    opt: fix hints for placeholder fast path
    Fixes #147363
    
    Release note (bug fix): A bug has been fixed that cause the optimizer to
    ignore index hints when optimizing some forms of prepared statements.
    This could result in one of two unexepcted behaviors: a query errors
    with the message "index cannot be used for this query" when the index
    can actually be used, or query using an index that does not adhere to
    the hint. The hints relevant to this bug are regular index hints, e.g.,
    `SELECT * FROM tab@index`, `FORCE_INVERTED_INDEX` and `FORCE_ZIGZAG`.

--- pkg/sql/opt/xform/placeholder_fast_path.go
+++ pkg/sql/opt/xform/placeholder_fast_path.go
@@ -88,6 +88,11 @@ func (o *Optimizer) TryPlaceholderFastPath() (ok bool, err error) {
 		return false, nil
 	}
 
+	if scan.Flags.ForceInvertedIndex || scan.Flags.ForceZigzag {
+		// We don't support inverted or zigzag indexes in the fast path.
+		return false, nil
+	}
+
 	var constrainedCols opt.ColSet
 	for i := range sel.Filters {
 		// Each condition must be an equality between a variable and a constant
@@ -124,6 +129,10 @@ func (o *Optimizer) TryPlaceholderFastPath() (ok bool, err error) {
 		index := tabMeta.Table.Index(ord)
 		if index.Type() != idxtype.FORWARD {
 			// Skip inverted and vector indexes.
+			continue
+		}
+		if scan.Flags.ForceIndex && scan.ScanPrivate.Flags.Index != ord {
+			// If an index is forced, skip all other indexes.
 			continue
 		}
 
