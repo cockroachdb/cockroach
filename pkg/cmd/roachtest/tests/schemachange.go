a0d3821a6ef5b4d53a8602b640c2296a3940d9e5
Rafi Shamim <rafi@cockroachlabs.com>
2025-05-29 17:00:29 -0400

    roachtest: give more disk space to schemachange/bulkingest
    The test was failing due to insufficient space. The test size was
    recently increased in f63c38811ce8edee5b1fb765f5dd0d1e1c91f4d1, which is
    the likely reason for the issue.
    
    Release note: None

--- pkg/cmd/roachtest/tests/schemachange.go
+++ pkg/cmd/roachtest/tests/schemachange.go
@@ -339,7 +339,7 @@ func makeSchemaChangeBulkIngestTest(
 	return registry.TestSpec{
 		Name:             "schemachange/bulkingest",
 		Owner:            registry.OwnerSQLFoundations,
-		Cluster:          r.MakeClusterSpec(numNodes, spec.WorkloadNode()),
+		Cluster:          r.MakeClusterSpec(numNodes, spec.WorkloadNode(), spec.VolumeSize(200)),
 		CompatibleClouds: registry.AllExceptAWS,
 		Suites:           registry.Suites(registry.Nightly),
 		Leases:           registry.MetamorphicLeases,
