51c83bc769b4cc577393c9716e1fbb7a885fd0ff
Chandra Thumuluru <chandra.thumuluru@cockroachlabs.com>
2025-06-02 23:14:50 +0530

    blobs: consolidate RPC clients creation
    This commit consolidates blob RPC client creation logic in the blobs
    package. It is a continuation of the work done in https://github.com/cockroachdb/cockroach/pull/147606.
    
    Epic: CRDB-48923
    Informs: https://github.com/cockroachdb/cockroach/issues/147757
    Release note: none

---
+++ pkg/blobs/blobspb/rpc_clients.go
@@ -0,0 +1,29 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package blobspb
+
+import (
+	context "context"
+
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/rpc/rpcbase"
+)
+
+// DialBlobClient establishes a DRPC connection if enabled; otherwise,
+// it falls back to gRPC. The established connection is used to create a
+// BlobClient.
+func DialBlobClient(
+	nd rpcbase.NodeDialer, ctx context.Context, nodeID roachpb.NodeID, class rpcbase.ConnectionClass,
+) (BlobClient, error) {
+	if !rpcbase.TODODRPC {
+		conn, err := nd.Dial(ctx, nodeID, class)
+		if err != nil {
+			return nil, err
+		}
+		return NewBlobClient(conn), nil
+	}
+	return nil, nil
+}
