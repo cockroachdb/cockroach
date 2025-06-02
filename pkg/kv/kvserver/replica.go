8b575720c468c6710cda67043c4be2d8a607d18f
sumeerbhola <sumeer@cockroachlabs.com>
2025-06-02 18:19:49 -0400

    mma: don't transfer lease to an existing voter that is lagging

--- pkg/kv/kvserver/replica.go
+++ pkg/kv/kvserver/replica.go
@@ -2835,7 +2835,8 @@ func (m *mmaRangeMessageNeeded) getSendStreamStatsScratch() *rac2.RangeSendStrea
 func (m *mmaRangeMessageNeeded) getNeededAndReset(
 	rangeLoad mma.RangeLoad,
 	raftStatus *raft.Status,
-	// TODO: use RangeSendStreamStats.
+	// TODO: Perhaps use RangeSendStreamStats. See the comment about whether
+	// this is really needed in mma.ReplicaState.
 	sendStreamStats *rac2.RangeSendStreamStats,
 ) bool {
 	needed := m.needed
@@ -2845,9 +2846,6 @@ func (m *mmaRangeMessageNeeded) getNeededAndReset(
 	}
 	if !needed {
 		for replicaID, ls := range m.lastLaggingState {
-			// TODO: Unsure whether using ReplicaIsBehind is what we want here, since
-			// it is stronger than the lack of a send-queue. And we needed the
-			// send-queue state for something in the allocator.
 			if ls.isLagging != raftutil.ReplicaIsBehind(raftStatus, replicaID) {
 				needed = true
 				break
