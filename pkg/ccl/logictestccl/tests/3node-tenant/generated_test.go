5e1106bd7bc07715bf04c8ae6a9605c05cd0f731
Ibrahim Kettaneh <ibrahim.kettaneh@cockroachlabs.com>
2025-06-03 08:35:52 -0400

    sql/logictest: delete mixed_version_row_level_security
    Verified with the test author that this test can be deleted.
    
    Epic: REL-2848
    
    Release note: None

--- pkg/ccl/logictestccl/tests/3node-tenant/generated_test.go
+++ pkg/ccl/logictestccl/tests/3node-tenant/generated_test.go
@@ -1261,13 +1261,6 @@ func TestTenantLogic_merge_join(
 	runLogicTest(t, "merge_join")
 }
 
-func TestTenantLogic_mixed_version_row_level_security(
-	t *testing.T,
-) {
-	defer leaktest.AfterTest(t)()
-	runLogicTest(t, "mixed_version_row_level_security")
-}
-
 func TestTenantLogic_multi_statement(
 	t *testing.T,
 ) {
