e289521f321a6f7999a39c618b1a823855f39e3e
David Hartunian <davidh@cockroachlabs.com>
2025-06-03 11:17:16 -0400

    structlogging: rewrite test to use stub hot ranges
    Previously, this test used a full server to get hot range data, but
    that proved to be quite difficult to work with. For the purposes of
    this test, which is meant to exercise the business logic of the hot
    range logger, we assume that the endpoint will work correctly and
    return ranges with a QPS and a CPU time.
    
    Resolves: #145412
    Resolves: #147238
    
    Release note: None

--- pkg/server/structlogging/main_test.go
+++ pkg/server/structlogging/main_test.go
@@ -24,3 +24,5 @@ func TestMain(m *testing.M) {
 	serverutils.InitTestClusterFactory(testcluster.TestClusterFactory)
 	os.Exit(m.Run())
 }
+
+//go:generate ../../util/leaktest/add-leaktest.sh *_test.go
