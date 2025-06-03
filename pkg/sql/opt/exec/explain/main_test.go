0ad8b543c5ff0b4fd847d66ff4623eb045706eb1
Michael Erickson <michae2@cockroachlabs.com>
2025-06-02 17:15:57 -0700

    sql: add statement retry information to EXPLAIN ANALYZE
    Add statement retry count and duration to EXPLAIN ANALYZE output.
    
    Informs: #145377
    
    Release note: None

--- pkg/sql/opt/exec/explain/main_test.go
+++ pkg/sql/opt/exec/explain/main_test.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/ccl"
 	"github.com/cockroachdb/cockroach/pkg/security/securityassets"
 	"github.com/cockroachdb/cockroach/pkg/security/securitytest"
 	"github.com/cockroachdb/cockroach/pkg/server"
@@ -16,6 +17,7 @@ import (
 )
 
 func TestMain(m *testing.M) {
+	defer ccl.TestingEnableEnterprise()()
 	securityassets.SetLoader(securitytest.EmbeddedAssets)
 	serverutils.InitTestServerFactory(server.TestServerFactory)
 	os.Exit(m.Run())
