f2036311d315e4f757717bab855d4320cde96424
Shubham Dhama <shubham.dhama@cockroachlabs.com>
2025-06-04 12:59:39 +0530

    drpc: move drpc server back to pkg/rpc
    `NewRaftTransport` needs a `drpc.Mux` to register its server
    implementations, so the straightforward approach is to pass
    `drpcServer.Mux`. However, some tests within the `kvserver` package (not
    `kvserver_test` itself) also require this `drpcServer.Mux`. This would
    necessitate exporting `newDRPCServer` and `drpcServer.mux` as
    `NewDRPCServer` and `drpcServer.Mux` respectively. But even then, this
    won't resolve the situation; it will introduce a cyclic dependency. Moving
    these tests to `kvserver_test` is not a viable option, as they rely on
    private methods within `kvserver`. Even if we managed to untangle this
    dependency mess, the whole setup just feels wrong and screams for a proper
    refactor. We should simply align with how gRPC handles this.
    
    Additionally, as part of this change, it's more appropriate for the DRPC
    server implementation residing in `pkg/rpc` to be exposed as an interface
    to other packages, rather than as a concrete object.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -975,7 +975,7 @@ func NewServer(cfg Config, stopper *stop.Stopper) (serverctl.ServerStartupInterf
 		cfg.LicenseEnforcer,
 	)
 	kvpb.RegisterInternalServer(grpcServer.Server, node)
-	if err := kvpb.DRPCRegisterKVBatch(drpcServer.mux, node.AsDRPCKVBatchServer()); err != nil {
+	if err := kvpb.DRPCRegisterKVBatch(drpcServer, node.AsDRPCKVBatchServer()); err != nil {
 		return nil, err
 	}
 	kvserver.RegisterPerReplicaServer(grpcServer.Server, node.perReplicaServer)
