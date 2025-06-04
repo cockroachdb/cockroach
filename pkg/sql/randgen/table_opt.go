1a6136a4f615d0523e188baf5906599d4e0f7936
Jeff Swenson <jeffswenson@betterthannull.com>
2025-06-04 14:38:25 -0400

    randgen: rework TableOpt to follow option pattern
    This commit reworks randgen so that TableOpt follows the standard go
    option convention. This allows for spelling non-binary options like type
    filters or transformations.
    
    Release note: none

---
+++ pkg/sql/randgen/table_opt.go
@@ -0,0 +1,61 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package randgen
+
+// TableOption represents a single option for table generation.
+type TableOption func(*tableOptions)
+
+type tableOptions struct {
+	primaryIndexRequired       bool
+	skipColumnFamilyMutations  bool
+	multiRegion                bool
+	allowPartiallyVisibleIndex bool
+	crazyNames                 bool
+}
+
+// WithPrimaryIndexRequired requires that the table has a primary index.
+func WithPrimaryIndexRequired() TableOption {
+	return func(o *tableOptions) {
+		o.primaryIndexRequired = true
+	}
+}
+
+// WithSkipColumnFamilyMutations skips column family mutations.
+func WithSkipColumnFamilyMutations() TableOption {
+	return func(o *tableOptions) {
+		o.skipColumnFamilyMutations = true
+	}
+}
+
+// WithMultiRegion enables multi-region table generation.
+func WithMultiRegion() TableOption {
+	return func(o *tableOptions) {
+		o.multiRegion = true
+	}
+}
+
+// WithAllowPartiallyVisibleIndex allows partially visible indexes.
+func WithAllowPartiallyVisibleIndex() TableOption {
+	return func(o *tableOptions) {
+		o.allowPartiallyVisibleIndex = true
+	}
+}
+
+// WithCrazyNames enables generation of crazy table and column names.
+func WithCrazyNames() TableOption {
+	return func(o *tableOptions) {
+		o.crazyNames = true
+	}
+}
+
+// applyOptions applies the given options to a tableOptions struct.
+func applyOptions(opts []TableOption) tableOptions {
+	var o tableOptions
+	for _, opt := range opts {
+		opt(&o)
+	}
+	return o
+}
