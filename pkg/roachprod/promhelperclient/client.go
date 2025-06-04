eed5d99d985b91575ee1e827622cfffac0e2f558
Kyle <kyle.li@cockroachlabs.com>
2025-06-04 16:27:28 -0400

    roachprod: create type alias to hold NodeInfo map
    Previously the NodeInfo to Node was stored in an ambiguous map
    structure, leading to an improper printing of the ip ports stored in it,
    this patch creates a type alias with a String() method to replace all
    instances of this map structure.
    
    Release note: none
    Fixes: none
    Informs: none

--- pkg/roachprod/promhelperclient/client.go
+++ pkg/roachprod/promhelperclient/client.go
@@ -130,7 +130,7 @@ func (c *PromClient) setUrl(url string) {
 
 // instanceConfigRequest is the HTTP request received for generating instance config
 type instanceConfigRequest struct {
-	//Config is the content of the yaml file
+	// Config is the content of the yaml file
 	Config   string `json:"config"`
 	Insecure bool   `json:"insecure"`
 }
@@ -140,7 +140,7 @@ func (c *PromClient) UpdatePrometheusTargets(
 	ctx context.Context,
 	clusterName string,
 	forceFetchCreds bool,
-	nodes map[int][]*NodeInfo,
+	nodes NodeTargets,
 	insecure bool,
 	l *logger.Logger,
 ) error {
@@ -253,8 +253,22 @@ type NodeInfo struct {
 	CustomLabels map[string]string // Custom labels to be added to the cluster config
 }
 
+type NodeTargets map[int][]*NodeInfo
+
+func (nt NodeTargets) String() string {
+	var parts []string
+	for port, infos := range nt {
+		var targets []string
+		for _, info := range infos {
+			targets = append(targets, info.Target)
+		}
+		parts = append(parts, fmt.Sprintf("%d:[%s]", port, strings.Join(targets, ",")))
+	}
+	return strings.Join(parts, " ")
+}
+
 // createClusterConfigFile creates the cluster config file per node
-func buildCreateRequest(nodes map[int][]*NodeInfo, insecure bool) (io.Reader, error) {
+func buildCreateRequest(nodes NodeTargets, insecure bool) (io.Reader, error) {
 	configs := make([]*CCParams, 0)
 	for _, n := range nodes {
 		for _, node := range n {
