e889214ae4ad6a74ab20b3b60e8ffcbabd3a5795
Kyle <kyle.li@cockroachlabs.com>
2025-06-04 16:27:28 -0400

    roachprod: create type alias to hold NodeInfo map
    Previously the NodeInfo to Node was stored in an ambiguous map
    structure, leading to an improper printing of the ip ports stored in it,
    this patch creates a type alias with a String() method to replace all
    instances of this map structure.
    
    Release note: none
    Fixes: none
    Informs: none

--- pkg/roachprod/roachprod.go
+++ pkg/roachprod/roachprod.go
@@ -818,7 +818,7 @@ func updatePrometheusTargets(
 	}
 
 	cl := promhelperclient.NewPromClient()
-	nodeIPPorts := make(map[int][]*promhelperclient.NodeInfo)
+	nodeIPPorts := promhelperclient.NodeTargets{}
 	nodeIPPortsMutex := syncutil.RWMutex{}
 	var wg sync.WaitGroup
 	for _, node := range c.Nodes {
