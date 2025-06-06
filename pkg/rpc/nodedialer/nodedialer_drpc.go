a5260c73afa76af0e247c4339124c7704158beca
Chandra Thumuluru <chandra.thumuluru@cockroachlabs.com>
2025-06-06 23:31:54 +0530

    rpc: remove DRPC dependency from Peer and Connection generics
    Although `Peer` and `Connection` are generic and intended to support both
    gRPC and DRPC connections, the current implementation has a hardcoded
    dependency on DRPC. As a result, a DRPC connection is always dialed,
    regardless of the intended type. This PR removes the direct dependency
    on DRPC, allowing the appropriate connection type (gRPC or DRPC) to be
    used based on the generic parameter.
    
    Epic: CRDB-48923
    Fixes: none
    Release note: none

--- pkg/rpc/nodedialer/nodedialer_drpc.go
+++ pkg/rpc/nodedialer/nodedialer_drpc.go
@@ -13,18 +13,20 @@ import (
 )
 
 type unaryDRPCBatchServiceToInternalAdapter struct {
-	useStreamPoolClient bool
-	rpc.RestrictedInternalClient
-	drpcClient     kvpb.DRPCKVBatchClient
+	client         kvpb.RPCInternalClient
 	drpcStreamPool *rpc.DRPCBatchStreamPool
 }
 
 func (a *unaryDRPCBatchServiceToInternalAdapter) Batch(
 	ctx context.Context, in *kvpb.BatchRequest,
 ) (*kvpb.BatchResponse, error) {
-	if a.useStreamPoolClient && a.drpcStreamPool != nil {
+	if a.drpcStreamPool != nil {
 		return a.drpcStreamPool.Send(ctx, in)
 	}
+	return a.client.Batch(ctx, in)
+}
 
-	return a.drpcClient.Batch(ctx, in)
+func (a *unaryDRPCBatchServiceToInternalAdapter) MuxRangeFeed(
+	ctx context.Context) (kvpb.RPCInternal_MuxRangeFeedClient, error) {
+	return a.client.MuxRangeFeed(ctx)
 }
