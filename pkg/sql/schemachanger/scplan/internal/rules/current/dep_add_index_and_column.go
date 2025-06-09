7736d22d3d38fd3ec087b5312702db6ed1ab55ec
Faizan Qazi <faizan@cockroachlabs.com>
2025-06-09 11:07:52 -0400

    sql/schemachanger: prevent runtime errors adding multiple columns
    Previously, index columns would be added in order, but no specific rule
    ensured they were added in that specific order. This became problematic
    when schema_locked support was added in the declarative schema changer,
    since out of order execution can lead to runtime errors. This can be
    observed if a user uses multiple AddGeometryColumn builtins in a
    statement. To address this, this patch adds a new explicit rule to
    ensure index columns are added in an increasing order.
    
    Fixes: #147993
    
    Release note (bug fix): Adding multiple columns with AddGeometryColumn
    in a single statement would run into runtime errors.

--- pkg/sql/schemachanger/scplan/internal/rules/current/dep_add_index_and_column.go
+++ pkg/sql/schemachanger/scplan/internal/rules/current/dep_add_index_and_column.go
@@ -118,3 +118,24 @@ func init() {
 	)
 
 }
+
+// This rule ensures that index columns depend on each other in increasing order.
+func init() {
+	registerDepRule(
+		"ensure index columns are in increasing order",
+		scgraph.Precedence,
+		"later-column", "earlier-column",
+		func(from, to NodeVars) rel.Clauses {
+			return rel.Clauses{
+				from.Type((*scpb.IndexColumn)(nil)),
+				from.JoinTargetNode(),
+				to.Type((*scpb.IndexColumn)(nil)),
+				JoinOnIndexID(from, to, "table-id", "index-id"),
+				ToPublicOrTransient(from, to),
+				StatusesToPublicOrTransient(from, scpb.Status_PUBLIC, to, scpb.Status_PUBLIC),
+				FilterElements("SmallerColumnIDFirst", from, to, func(from, to *scpb.IndexColumn) bool {
+					return from.OrdinalInKind < to.OrdinalInKind && from.Kind == to.Kind
+				}),
+			}
+		})
+}
