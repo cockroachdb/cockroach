DarrylWong/fork

failure-injection: use DB connection pools
Now that we have helpers to help query for SQLReady,
replication, etc., lets use them. To do so, first we must
convert our failureModes to start creating connection pools.

pkg/roachprod/failureinjection/failures/disk_stall.go
@@ -28,13 +28,14 @@ type CGroupDiskStaller struct {
 	GenericFailure
 }
 
-func MakeCgroupDiskStaller(clusterName string, l *logger.Logger, secure bool) (FailureMode, error) {
-	c, err := roachprod.GetClusterFromCache(l, clusterName, install.SecureOption(secure))
+func MakeCgroupDiskStaller(
+	clusterName string, l *logger.Logger, connectionInfo ConnectionInfo,
+) (FailureMode, error) {
+	genericFailure, err := makeGenericFailure(clusterName, l, connectionInfo, CgroupsDiskStallName)
 	if err != nil {
 		return nil, err
 	}
-	genericFailure := GenericFailure{c: c, runTitle: CgroupsDiskStallName}
-	return &CGroupDiskStaller{GenericFailure: genericFailure}, nil
+	return &CGroupDiskStaller{GenericFailure: *genericFailure}, nil
 }
 
 func registerCgroupDiskStall(r *FailureRegistry) {
@@ -82,6 +83,8 @@ fi
 	return nil
 }
 func (s *CGroupDiskStaller) Cleanup(ctx context.Context, l *logger.Logger, args FailureArgs) error {
+	defer s.CloseConnections()
+
 	stallType := []bandwidthType{readBandwidth, writeBandwidth}
 	nodes := args.(DiskStallArgs).Nodes
 
@@ -214,7 +217,7 @@ func (s *CGroupDiskStaller) WaitForFailureToRecover(
 ) error {
 	nodes := args.(DiskStallArgs).Nodes
 	return forEachNode(nodes, func(n install.Nodes) error {
-		return s.WaitForSQLReady(ctx, l, n, time.Minute)
+		return s.WaitForSQLReady(ctx, l, n)
 	})
 }
 
@@ -316,15 +319,13 @@ type DmsetupDiskStaller struct {
 }
 
 func MakeDmsetupDiskStaller(
-	clusterName string, l *logger.Logger, secure bool,
+	clusterName string, l *logger.Logger, connectionInfo ConnectionInfo,
 ) (FailureMode, error) {
-	c, err := roachprod.GetClusterFromCache(l, clusterName, install.SecureOption(secure))
+	genericFailure, err := makeGenericFailure(clusterName, l, connectionInfo, DmsetupDiskStallName)
 	if err != nil {
 		return nil, err
 	}
-
-	genericFailure := GenericFailure{c: c, runTitle: DmsetupDiskStallName}
-	return &DmsetupDiskStaller{GenericFailure: genericFailure}, nil
+	return &DmsetupDiskStaller{GenericFailure: *genericFailure}, nil
 }
 
 func registerDmsetupDiskStall(r *FailureRegistry) {
@@ -421,6 +422,8 @@ func (s *DmsetupDiskStaller) Recover(
 func (s *DmsetupDiskStaller) Cleanup(
 	ctx context.Context, l *logger.Logger, args FailureArgs,
 ) error {
+	defer s.CloseConnections()
+
 	diskStallArgs := args.(DiskStallArgs)
 	if diskStallArgs.RestartNodes {
 		stopOpts := roachprod.DefaultStopOpts()
@@ -490,6 +493,6 @@ func (s *DmsetupDiskStaller) WaitForFailureToRecover(
 ) error {
 	nodes := args.(DiskStallArgs).Nodes
 	return forEachNode(nodes, func(n install.Nodes) error {
-		return s.WaitForSQLReady(ctx, l, n, time.Minute)
+		return s.WaitForSQLReady(ctx, l, n)
 	})
 }

pkg/roachprod/failureinjection/failures/latency.go
@@ -10,7 +10,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/cockroachdb/cockroach/pkg/roachprod"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/logger"
 	"github.com/cockroachdb/errors"
@@ -53,16 +52,14 @@ func registerNetworkLatencyFailure(r *FailureRegistry) {
 }
 
 func MakeNetworkLatencyFailure(
-	clusterName string, l *logger.Logger, secure bool,
+	clusterName string, l *logger.Logger, connectionInfo ConnectionInfo,
 ) (FailureMode, error) {
-	c, err := roachprod.GetClusterFromCache(l, clusterName, install.SecureOption(secure))
+	genericFailure, err := makeGenericFailure(clusterName, l, connectionInfo, NetworkLatencyName)
 	if err != nil {
 		return nil, err
 	}
-
-	genericFailure := GenericFailure{c: c, runTitle: "latency"}
 	return &NetworkLatency{
-		GenericFailure:       genericFailure,
+		GenericFailure:       *genericFailure,
 		classesInUse:         make(map[int]struct{}),
 		nextAvailableClass:   1,
 		filterNameToClassMap: make(map[string]int),

pkg/roachprod/failureinjection/failures/network_partition.go
@@ -9,7 +9,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/cockroachdb/cockroach/pkg/roachprod"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/logger"
 )
@@ -50,15 +49,13 @@ type IPTablesPartitionFailure struct {
 }
 
 func MakeIPTablesPartitionFailure(
-	clusterName string, l *logger.Logger, secure bool,
+	clusterName string, l *logger.Logger, connectionInfo ConnectionInfo,
 ) (FailureMode, error) {
-	c, err := roachprod.GetClusterFromCache(l, clusterName, install.SecureOption(secure))
+	genericFailure, err := makeGenericFailure(clusterName, l, connectionInfo, IPTablesNetworkPartitionName)
 	if err != nil {
 		return nil, err
 	}
-
-	genericFailure := GenericFailure{c: c, runTitle: "iptables"}
-	return &IPTablesPartitionFailure{genericFailure}, nil
+	return &IPTablesPartitionFailure{*genericFailure}, nil
 }
 
 const IPTablesNetworkPartitionName = "iptables-network-partition"

cockroachdb/cockroach

pkg/roachprod/failureinjection/failures/disk_stall.go
@@ -32,12 +32,11 @@ type CGroupDiskStaller struct {
 func MakeCgroupDiskStaller(
 	clusterName string, l *logger.Logger, clusterOpts ClusterOptions,
 ) (FailureMode, error) {
-	c, err := roachprod.GetClusterFromCache(l, clusterName, install.SecureOption(clusterOpts.secure))
+	genericFailure, err := makeGenericFailure(clusterName, l, CgroupsDiskStallName, clusterOpts)
 	if err != nil {
 		return nil, err
 	}
-	genericFailure := GenericFailure{c: c, runTitle: CgroupsDiskStallName}
-	return &CGroupDiskStaller{GenericFailure: genericFailure}, nil
+	return &CGroupDiskStaller{GenericFailure: *genericFailure}, nil
 }
 
 func registerCgroupDiskStall(r *FailureRegistry) {
@@ -108,7 +107,9 @@ fi
 	return nil
 }
 func (s *CGroupDiskStaller) Cleanup(ctx context.Context, l *logger.Logger, args FailureArgs) error {
+	defer s.CloseConnections()
 	diskStallArgs := args.(DiskStallArgs)
+
 	stallType := []bandwidthType{readBandwidth, writeBandwidth}
 	nodes := diskStallArgs.Nodes
 
@@ -241,7 +242,7 @@ func (s *CGroupDiskStaller) WaitForFailureToRecover(
 ) error {
 	nodes := args.(DiskStallArgs).Nodes
 	return forEachNode(nodes, func(n install.Nodes) error {
-		return s.WaitForSQLReady(ctx, l, n, time.Minute)
+		return s.WaitForSQLReady(ctx, l, n)
 	})
 }
 
@@ -345,13 +346,11 @@ type DmsetupDiskStaller struct {
 func MakeDmsetupDiskStaller(
 	clusterName string, l *logger.Logger, clusterOpts ClusterOptions,
 ) (FailureMode, error) {
-	c, err := roachprod.GetClusterFromCache(l, clusterName, install.SecureOption(clusterOpts.secure))
+	genericFailure, err := makeGenericFailure(clusterName, l, DmsetupDiskStallName, clusterOpts)
 	if err != nil {
 		return nil, err
 	}
-
-	genericFailure := GenericFailure{c: c, runTitle: DmsetupDiskStallName}
-	return &DmsetupDiskStaller{GenericFailure: genericFailure}, nil
+	return &DmsetupDiskStaller{GenericFailure: *genericFailure}, nil
 }
 
 func registerDmsetupDiskStall(r *FailureRegistry) {
@@ -448,6 +447,8 @@ func (s *DmsetupDiskStaller) Recover(
 func (s *DmsetupDiskStaller) Cleanup(
 	ctx context.Context, l *logger.Logger, args FailureArgs,
 ) error {
+	defer s.CloseConnections()
+
 	diskStallArgs := args.(DiskStallArgs)
 	if diskStallArgs.RestartNodes {
 		stopOpts := roachprod.DefaultStopOpts()
@@ -517,6 +518,6 @@ func (s *DmsetupDiskStaller) WaitForFailureToRecover(
 ) error {
 	nodes := args.(DiskStallArgs).Nodes
 	return forEachNode(nodes, func(n install.Nodes) error {
-		return s.WaitForSQLReady(ctx, l, n, time.Minute)
+		return s.WaitForSQLReady(ctx, l, n)
 	})
 }

pkg/roachprod/failureinjection/failures/latency.go
@@ -10,7 +10,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/cockroachdb/cockroach/pkg/roachprod"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/logger"
 	"github.com/cockroachdb/errors"
@@ -55,14 +54,12 @@ func registerNetworkLatencyFailure(r *FailureRegistry) {
 func MakeNetworkLatencyFailure(
 	clusterName string, l *logger.Logger, clusterOpts ClusterOptions,
 ) (FailureMode, error) {
-	c, err := roachprod.GetClusterFromCache(l, clusterName, install.SecureOption(clusterOpts.secure))
+	genericFailure, err := makeGenericFailure(clusterName, l, NetworkLatencyName, clusterOpts)
 	if err != nil {
 		return nil, err
 	}
-
-	genericFailure := GenericFailure{c: c, runTitle: "latency"}
 	return &NetworkLatency{
-		GenericFailure:       genericFailure,
+		GenericFailure:       *genericFailure,
 		classesInUse:         make(map[int]struct{}),
 		nextAvailableClass:   1,
 		filterNameToClassMap: make(map[string]int),

pkg/roachprod/failureinjection/failures/network_partition.go
@@ -9,7 +9,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/cockroachdb/cockroach/pkg/roachprod"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/logger"
 )
@@ -52,13 +51,11 @@ type IPTablesPartitionFailure struct {
 func MakeIPTablesPartitionFailure(
 	clusterName string, l *logger.Logger, clusterOpts ClusterOptions,
 ) (FailureMode, error) {
-	c, err := roachprod.GetClusterFromCache(l, clusterName, install.SecureOption(clusterOpts.secure))
+	genericFailure, err := makeGenericFailure(clusterName, l, IPTablesNetworkPartitionName, clusterOpts)
 	if err != nil {
 		return nil, err
 	}
-
-	genericFailure := GenericFailure{c: c, runTitle: "iptables"}
-	return &IPTablesPartitionFailure{genericFailure}, nil
+	return &IPTablesPartitionFailure{*genericFailure}, nil
 }
 
 const IPTablesNetworkPartitionName = "iptables-network-partition"