21a40ff9c73457c97e253178adf0a0da633ae06a
Yahor Yuzefovich <yahor@cockroachlabs.com>
2025-06-11 12:43:33 -0700

    WIP on geos 1 byte modification

--- pkg/geo/geos/geos.cc
+++ pkg/geo/geos/geos.cc
@@ -415,8 +415,9 @@ CR_GEOS_String toGEOSString(const char* data, size_t len) {
   if (len == 0) {
     return result;
   }
-  result.data = static_cast<char*>(malloc(len));
+  result.data = static_cast<char*>(malloc(len+1));
   memcpy(result.data, data, len);
+  result.data[len] = '\0';
   return result;
 }
 
