442f38c86ca1fbd17b46f8e466847e54785f64b2
Chandra Thumuluru <chandra.thumuluru@cockroachlabs.com>
2025-06-13 01:12:56 +0530

    kv: replace `StoreLivenessClient` RPC client with adapter
    gRPC and DRPC clients have different signatures. To unify them, a common
    interface was created for both types of clients, and replaced the gRPC
    and DRPC clients with adapters.
    
    Epic: CRDB-48923
    Informs:#148353
    Release note: none

--- pkg/kv/kvserver/storeliveness/storelivenesspb/rpc_clients.go
+++ pkg/kv/kvserver/storeliveness/storelivenesspb/rpc_clients.go
@@ -14,16 +14,16 @@ import (
 
 // DialStoreLivenessClient establishes a DRPC connection if enabled; otherwise,
 // it falls back to gRPC. The established connection is used to create a
-// StoreLivenessClient.
+// RPCStoreLivenessClient.
 func DialStoreLivenessClient(
 	nd rpcbase.NodeDialer, ctx context.Context, nodeID roachpb.NodeID, class rpcbase.ConnectionClass,
-) (StoreLivenessClient, error) {
+) (RPCStoreLivenessClient, error) {
 	if !rpcbase.TODODRPC {
 		conn, err := nd.Dial(ctx, nodeID, class)
 		if err != nil {
 			return nil, err
 		}
-		return NewStoreLivenessClient(conn), nil
+		return NewGRPCStoreLivenessClientAdapter(conn), nil
 	}
 	return nil, nil
 }
