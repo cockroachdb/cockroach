5e8d5a85a5c4b17fb47108a0c3537ed74fd7f1cf
Tobias Grieger <tobias.b.grieger@gmail.com>
2025-06-12 12:32:52 +0200

    run-pgo-build: use artifacts dependency
    It now expects the artifacts of the delegate job to be present in ./artifacts.
    
    The job[^1] was updated to that effect.
    
    [^1]: https://teamcity.cockroachdb.com/buildConfiguration/Internal_Cockroach_Build_Ci_GenerateProfile

--- pkg/cmd/run-pgo-build/main.go
+++ pkg/cmd/run-pgo-build/main.go
@@ -10,150 +10,21 @@
 package main
 
 import (
-	"archive/zip"
 	"bytes"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
 	"sync"
-	"time"
 
 	"github.com/cockroachdb/errors"
 	"github.com/google/pprof/profile"
 )
 
-// NB: this code could be condensed significantly if we set up the Generate
-// Profile job with a build and artifact dependency on the job that runs the
-// roachtests that provide the CPU profiles. We would not need TC credentials,
-// we would not need any code that triggers and waits for a delegate build, and
-// we would not need to download artifacts, plus it would be easier to reason
-// about the jobs since they would be more obviously connected on TeamCity.
-
-const (
-	buildConfigID = "Cockroach_Nightlies_Roachtests_ValidateProfile"
-)
-
-var (
-	branch    = os.Getenv("TC_BUILD_BRANCH")
-	serverURL = os.Getenv("TC_SERVER_URL")
-	username  = os.Getenv("TC_API_USER")
-	password  = os.Getenv("TC_API_PASSWORD")
-
-	outFile = flag.String("out", "", "where to store the result pprof profile")
-)
-
-type profileWithName struct {
-	filename        string
-	profileContents []byte
-}
-
-type Build struct {
-	ID         int64
-	FinishDate string
-	Status     string
-	WebUrl     string
-}
-
-type ReadAtCloser interface {
-	io.ReaderAt
-	io.ReadCloser
-}
-
-func doRequest(req *http.Request) (*Build, error) {
-	req.SetBasicAuth(username, password)
-	req.Header.Add("Accept", "application/json")
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	if resp.StatusCode >= 400 {
-		return nil, fmt.Errorf("failed to send HTTP request; got status code %d", resp.StatusCode)
-	}
-	defer resp.Body.Close()
-	var buf bytes.Buffer
-	if _, err := io.Copy(&buf, resp.Body); err != nil {
-		return nil, err
-	}
-	var ret Build
-	if err := json.Unmarshal(buf.Bytes(), &ret); err != nil {
-		return nil, err
-	}
-	return &ret, nil
-}
-
-func queueBuild(buildConfigID, branch string) (*Build, error) {
-	reqStruct := struct {
-		BuildTypeID string `json:"buildTypeId,omitempty"`
-		BranchName  string `json:"branchName,omitempty"`
-	}{
-		BuildTypeID: buildConfigID,
-		BranchName:  branch,
-	}
-	reqJson, err := json.Marshal(reqStruct)
-	if err != nil {
-		return nil, err
-	}
-	req, err := http.NewRequest("POST", fmt.Sprintf("https://%s/httpAuth/app/rest/buildQueue", serverURL), bytes.NewReader(reqJson))
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Add("Content-Type", "application/json")
-	return doRequest(req)
-}
-
-func getBuild(id int64) (*Build, error) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("https://%s/httpAuth/app/rest/builds/id:%d?fields=id,finishDate,status", serverURL, id), nil)
-	if err != nil {
-		return nil, err
-	}
-	return doRequest(req)
-}
-
-// downloadArtifacts downloads the artifacts.zip archive for the given build and
-// returns the ReadAtCloser corresponding to it. The file will be stored on
-// disk in the given tmpDir. The caller is responsible for closing the file.
-// The returned int64 is the length of the file.
-func downloadArtifacts(buildID int64, tmpDir string) (ReadAtCloser, int64, error) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("https://%s/repository/downloadAll/%s/%d:id", serverURL, buildConfigID, buildID), nil)
-	if err != nil {
-		return nil, 0, err
-	}
-	req.SetBasicAuth(username, password)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, 0, err
-	}
-	if resp.StatusCode >= 400 {
-		return nil, 0, fmt.Errorf("failed to download artifacts: got status code %d", resp.StatusCode)
-	}
-	defer resp.Body.Close()
-
-	zipFile, err := os.Create(filepath.Join(tmpDir, "artifacts.zip"))
-	if err != nil {
-		return nil, 0, err
-	}
-	zipLength, err := io.Copy(zipFile, resp.Body)
-	if err != nil {
-		zipFile.Close()
-		return nil, 0, err
-	}
-	_, err = zipFile.Seek(0, 0)
-	if err != nil {
-		zipFile.Close()
-		return nil, 0, err
-	}
-
-	return zipFile, zipLength, nil
-}
+var outFile = flag.String("out", "", "where to store the result pprof profile")
 
 // processProfiles reads all the parsed profiles from the given channel,
-// merges all of the profiles, and dumps the results profile to a final
-// location. wg.Done() will be called at the end of this function.
+// normalizes and merges all the profiles, and returns the merged result.
 func processProfiles(profilesChan chan *profile.Profile) (*profile.Profile, error) {
 	var profiles []*profile.Profile
 	var first *profile.Profile
@@ -179,60 +50,16 @@ func processProfiles(profilesChan chan *profile.Profile) (*profile.Profile, erro
 func main() {
 	flag.Parse()
 
-	if branch == "" || serverURL == "" || username == "" || password == "" {
-		panic("ensure credentials, server URL, and branch are supplied")
-	}
 	if *outFile == "" {
 		panic("must supply -out")
 	}
 
-	tmpDir, err := os.MkdirTemp("", "run-pgo-build")
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		_ = os.RemoveAll(tmpDir)
-	}()
-
-	build, err := queueBuild(buildConfigID, branch)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("queued build with ID %d: %s\n", build.ID, build.WebUrl)
-
-	for {
-		if build.FinishDate != "" {
-			break
-		}
-		time.Sleep(3 * time.Minute)
-		lookupID := build.ID
-		newBuild, err := getBuild(lookupID)
-		if err != nil {
-			fmt.Printf("failed to get build %d; got error %+v -- this will be retried\n", lookupID, err)
-		} else {
-			build = newBuild
-		}
-	}
-
-	if build.Status != "SUCCESS" {
-		panic(fmt.Sprintf("expected build to succeed; got status %s for build %+v", build.Status, build))
-	}
-
-	zipFile, zipLength, err := downloadArtifacts(build.ID, tmpDir)
-	if err != nil {
-		panic(err)
-	}
-	defer zipFile.Close()
-
-	zipReader, err := zip.NewReader(zipFile, zipLength)
-	if err != nil {
-		panic(err)
-	}
-
-	profilesChan := make(chan *profile.Profile)
-
 	var readWg sync.WaitGroup
 	readWg.Add(1)
+	profilesChan := make(chan *profile.Profile)
+	defer readWg.Wait()
+	defer close(profilesChan)
+
 	go func() {
 		defer readWg.Done()
 		res, err := processProfiles(profilesChan)
@@ -247,27 +74,22 @@ func main() {
 		}
 	}()
 
-	for _, file := range zipReader.File {
-		if strings.HasSuffix(file.FileHeader.Name, "/merged.cpu.pb.gz") && file.UncompressedSize64 > 0 {
-			fmt.Printf("found profile %s\n", file.FileHeader.Name)
-			pprofFile, err := file.Open()
-			if err != nil {
-				panic(err)
-			}
-			var buf bytes.Buffer
-			if _, err := io.Copy(&buf, pprofFile); err != nil {
-				panic(err)
-			}
-			_ = pprofFile.Close()
-			prof, err := profile.Parse(bytes.NewReader(buf.Bytes()))
-			if err != nil {
-				panic(err)
-			}
-			profilesChan <- prof
-			continue
+	if err := filepath.WalkDir("./artifacts", func(path string, d os.DirEntry, err error) error {
+		if d.IsDir() || d.Name() != "merged.cpu.pb.gz" {
+			return nil
 		}
+		fmt.Printf("found profile %s\n", path)
+		b, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		prof, err := profile.Parse(bytes.NewReader(b))
+		if err != nil {
+			return err
+		}
+		profilesChan <- prof
+		return nil
+	}); err != nil {
+		panic(err)
 	}
-	// wg.Wait()
-	close(profilesChan)
-	readWg.Wait()
 }
