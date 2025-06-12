4f1c8b82aa48bff08a5cf29a9dbff2d85035cba0
Chandra Thumuluru <chandra.thumuluru@cockroachlabs.com>
2025-06-13 01:08:46 +0530

    util: replace `TracingClient` RPC client with adapter
    gRPC and DRPC clients have different signatures. To unify them, a common
    interface was created for both types of clients, and replaced the gRPC
    and DRPC clients with adapters.
    
    Epic: CRDB-48923
    Informs:#147757
    Release note: none

--- pkg/util/tracing/tracingservicepb/rpc_clients.go
+++ pkg/util/tracing/tracingservicepb/rpc_clients.go
@@ -14,16 +14,16 @@ import (
 
 // DialTracingClient establishes a DRPC connection if enabled; otherwise,
 // it falls back to gRPC. The established connection is used to create a
-// TracingClient.
+// RPCTracingClient.
 func DialTracingClient(
 	nd rpcbase.NodeDialer, ctx context.Context, nodeID roachpb.NodeID, class rpcbase.ConnectionClass,
-) (TracingClient, error) {
+) (RPCTracingClient, error) {
 	if !rpcbase.TODODRPC {
 		conn, err := nd.Dial(ctx, nodeID, class)
 		if err != nil {
 			return nil, err
 		}
-		return NewTracingClient(conn), nil
+		return NewGRPCTracingClientAdapter(conn), nil
 	}
 	return nil, nil
 }
