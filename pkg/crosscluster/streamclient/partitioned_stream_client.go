69c826bb20526677dae170f6587830eec6e4fefe
Kevin Cao <kevin.cao@cockroachlabs.com>
2025-06-12 17:40:49 -0400

    crosscluster: do not panic on poorly formatted inline certs
    Previously, when parsing inline SSL certificates in an external
    connection, if the certificate was not formatted correctly we would run
    into a panic from a nil pointer. This fixes the parser to properly
    surface an error instead of crashing.
    
    Fixes: #145949
    
    Release note: CockroachDB now raises an error when encountering improper
    inline SSL credentials instead of panicking.

--- pkg/crosscluster/streamclient/partitioned_stream_client.go
+++ pkg/crosscluster/streamclient/partitioned_stream_client.go
@@ -44,16 +44,16 @@ type partitionedStreamClient struct {
 func NewPartitionedStreamClient(
 	ctx context.Context, remote ClusterUri, opts ...Option,
 ) (*partitionedStreamClient, error) {
-	options := processOptions(opts)
-	conn, config, err := newPGConnForClient(ctx, remote.URL(), options)
+	streamOpts := processOptions(opts)
+	conn, config, err := newPGConnForClient(ctx, remote.URL(), streamOpts)
 	if err != nil {
 		return nil, err
 	}
 	client := partitionedStreamClient{
 		clusterUri: remote,
 		pgxConfig:  config,
-		compressed: options.compressed,
-		logical:    options.logical,
+		compressed: streamOpts.compressed,
+		logical:    streamOpts.logical,
 	}
 	client.mu.activeSubscriptions = make(map[*partitionedStreamSubscription]struct{})
 	client.mu.srcConn = conn
