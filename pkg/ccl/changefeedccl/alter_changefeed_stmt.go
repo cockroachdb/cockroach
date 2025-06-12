fc5ceccecec96ef2792e62a7236f8ab715352ce6
Keith Chow <keith.chow@cockroachlabs.com>
2025-06-12 12:49:38 -0400

    changefeedccl: create db-level changefeed job
    Support the creation of database-level changefeeds
    e.g.
    CREATE CHANGEFEED FOR DATABASE d;
    
    We do this by modifying AllTargets to expand
    database-level changefeed target spec to fetch
    all tables in the database.
    
    Resolves: #147371
    Epic: CRDB-1421
    
    Release note: None

--- pkg/ccl/changefeedccl/alter_changefeed_stmt.go
+++ pkg/ccl/changefeedccl/alter_changefeed_stmt.go
@@ -251,7 +251,7 @@ func alterChangefeedPlanHook(
 
 		telemetry.Count(telemetryPath)
 
-		logAlterChangefeedTelemetry(ctx, j, jobPayload.Description)
+		logAlterChangefeedTelemetry(ctx, j, jobPayload.Description, p.ExecCfg())
 
 		select {
 		case <-ctx.Done():
@@ -447,7 +447,7 @@ func generateAndValidateNewTargets(
 		return nil, nil, hlc.Timestamp{}, nil, err
 	}
 
-	prevTargets := AllTargets(prevDetails)
+	prevTargets := AllTargets(prevDetails, ctx, p.ExecCfg())
 	noLongerExist := make(map[string]descpb.ID)
 	if err := prevTargets.EachTarget(func(targetSpec changefeedbase.Target) error {
 		k := targetKey{TableID: targetSpec.DescID, FamilyName: tree.Name(targetSpec.FamilyName)}
