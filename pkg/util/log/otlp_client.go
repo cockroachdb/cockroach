91ccf85a5c3321311a7a46fc8153b31b83fb66e3
Mudit Somani <mudit.somani@cockroachlabs.com>
2025-06-13 12:37:33 +0530

    pkg/util/log: add otlp log sink
    OpenTelemetry is now an industry standard for o11y and CRDB
    should support its logging protocol so that it works
    seamlessly with OTEL compatible tooling.
    
    This commit implements the log sink functionality, it uses
    gRPC to transmit logs in the OTEL-standard protobuf format.
    This sink can be used to ship logs on OTEL compatible targets
    like otel-collector, datadog-agent or even directly to
    platforms like datadog and Loki.
    
    Parsing the config for the otlp log sink was done in
    a previous commit.
    
    Resolves: #143049
    
    Release note (general change): Added otlp log sink
    that can export logs to OpenTelemetry-compatible
    targets over gRPC.

---
+++ pkg/util/log/otlp_client.go
@@ -0,0 +1,146 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package log
+
+import (
+	"bytes"
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/build"
+	"github.com/cockroachdb/cockroach/pkg/cli/exit"
+	"github.com/cockroachdb/cockroach/pkg/util/log/logconfig"
+	collpb "go.opentelemetry.io/proto/otlp/collector/logs/v1"
+	cpb "go.opentelemetry.io/proto/otlp/common/v1"
+	lpb "go.opentelemetry.io/proto/otlp/logs/v1"
+	rpb "go.opentelemetry.io/proto/otlp/resource/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/connectivity"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/encoding/gzip"
+	"google.golang.org/grpc/status"
+)
+
+const (
+	// attribute attached to the outgoing logs
+	// helpful for identifying where the logs are coming from
+	logAttributeServiceKey   = "service.name"
+	logAttributeServiceValue = "cockroachdb"
+	logAttributeSinkKey      = "sink.name"
+
+	// name of the instrumentation library (which is supposed to create the logs)
+	// we use a homegrown implementation so we can name it
+	logIntrumentationLibrary = "cockroachdb.logger"
+)
+
+// OpenTelemetry log sink
+type otlpSink struct {
+	conn *grpc.ClientConn
+	lsc  collpb.LogsServiceClient
+	name string
+}
+
+func newOTLPSink(config logconfig.OTLPSinkConfig) (*otlpSink, error) {
+	dialOpts := []grpc.DialOption{grpc.WithUserAgent("CRDB OTLP over gRPC logs exporter")}
+	if *config.Insecure {
+		dialOpts = append(dialOpts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	} else {
+		// Default to using the host's root CA.
+		dialOpts = append(dialOpts, grpc.WithTransportCredentials(credentials.NewTLS(nil)))
+	}
+	if *config.Compression == logconfig.GzipCompression {
+		dialOpts = append(dialOpts, grpc.WithDefaultCallOptions(grpc.UseCompressor(gzip.Name)))
+	}
+
+	conn, err := grpc.Dial(config.Address, dialOpts...)
+	if err != nil {
+		return nil, err
+	}
+
+	lsc := collpb.NewLogsServiceClient(conn)
+	return &otlpSink{
+		conn: conn,
+		lsc:  lsc,
+		name: config.SinkName,
+	}, nil
+}
+
+func (sink *otlpSink) isNotShutdown() bool {
+	return sink.conn.GetState() != connectivity.Shutdown
+}
+
+func (sink *otlpSink) active() bool {
+	return true
+}
+
+func (sink *otlpSink) attachHints(stacks []byte) []byte {
+	return stacks
+}
+
+func (sink *otlpSink) exitCode() exit.Code {
+	return exit.LoggingNetCollectorUnavailable()
+}
+
+// converts the raw bytes into OTEL log records
+func extractRecordsToOTLP(b []byte) []*lpb.LogRecord {
+	bodies := bytes.Split(b, []byte("\n"))
+	records := make([]*lpb.LogRecord, 0, len(bodies))
+
+	for _, body := range bodies {
+		body = bytes.TrimSpace(body)
+		if len(body) > 0 {
+			records = append(records, &lpb.LogRecord{
+				Body: &cpb.AnyValue{
+					Value: &cpb.AnyValue_StringValue{
+						StringValue: string(body),
+					},
+				},
+			})
+		}
+	}
+
+	return records
+}
+
+func (sink *otlpSink) output(b []byte, opts sinkOutputOptions) error {
+	records := extractRecordsToOTLP(b)
+	ctx := context.Background()
+
+	_, err := sink.lsc.Export(ctx, &collpb.ExportLogsServiceRequest{
+		ResourceLogs: []*lpb.ResourceLogs{
+			{
+				Resource: &rpb.Resource{
+					Attributes: []*cpb.KeyValue{
+						{
+							Key:   logAttributeServiceKey,
+							Value: &cpb.AnyValue{Value: &cpb.AnyValue_StringValue{StringValue: logAttributeServiceValue}},
+						},
+						{
+							Key:   logAttributeSinkKey,
+							Value: &cpb.AnyValue{Value: &cpb.AnyValue_StringValue{StringValue: sink.name}},
+						},
+					},
+				},
+				InstrumentationLibraryLogs: []*lpb.InstrumentationLibraryLogs{
+					{
+						InstrumentationLibrary: &cpb.InstrumentationLibrary{
+							Name:    logIntrumentationLibrary,
+							Version: build.BinaryVersion(),
+						},
+						Logs: records,
+					},
+				},
+			},
+		},
+	})
+
+	if status.Code(err) == codes.OK {
+		return nil
+	}
+
+	return err
+}
