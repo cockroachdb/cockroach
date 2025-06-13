c640827820e24a8bb204b0713c1c463e17691c68
Brendan Gerrity <brendan.gerrity@cockroachlabs.com>
2025-06-13 11:34:27 -0400

    sql: default to node-level cache for sequences
    The session-level cache was implemented first and shared node-level
    cache was added in #118546.
    The better behaving node-level cache is not the default option.
    Changes the default for `CACHE` option to set node-level cache. A new
    option to allows the session-level cache to be set.
    
    Epic: CRDB-42587
    Fixes: #131531
    
    Release note (sql change): Changed the basic sequence cache option to
    set cache at the node-level. The PER SESSION NODE sequence option
    (syntax is  is [ PER SESSION CACHE # ]) provides the previous behavior
    to set session-level cache.

--- pkg/sql/catalog/serial_helper.go
+++ pkg/sql/catalog/serial_helper.go
@@ -61,7 +61,7 @@ func SequenceOptionsFromNormalizationMode(
 	case sessiondatapb.SerialUsesCachedSQLSequences:
 		cacheValue := sqlclustersettings.CachedSequencesCacheSizeSetting.Get(&st.SV)
 		seqOpts = append(seqOpts, tree.SequenceOption{
-			Name:   tree.SeqOptCache,
+			Name:   tree.SeqOptCacheSession,
 			IntVal: &cacheValue,
 		})
 	case sessiondatapb.SerialUsesCachedNodeSQLSequences:
