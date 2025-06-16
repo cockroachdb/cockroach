a3999e908701b1280e7ed6f2348690df9d7f1e65
Steven Danna <danna@cockroachlabs.com>
2025-06-16 19:19:58 +0100

    kvcoord: add test cases to txnWriteBuffer
    We weren't hitting a couple of cases in validateRequests. Now we are hitting
    them.
    
    Epic: none
    Release note: None

--- pkg/kv/kvclient/kvcoord/txn_interceptor_write_buffer_test.go
+++ pkg/kv/kvclient/kvcoord/txn_interceptor_write_buffer_test.go
@@ -1969,7 +1969,7 @@ func TestTxnWriteBufferBatchRequestValidation(t *testing.T) {
 
 	tests := []testCase{
 		{
-			name: "batch with OriginTimestamp",
+			name: "batch with OriginTimestamp in WriteOptions",
 			ba: func() *kvpb.BatchRequest {
 				header := kvpb.Header{
 					Txn: &txn,
@@ -1981,7 +1981,7 @@ func TestTxnWriteBufferBatchRequestValidation(t *testing.T) {
 			},
 		},
 		{
-			name: "batch with OriginID",
+			name: "batch with OriginID in WriteOptions",
 			ba: func() *kvpb.BatchRequest {
 				header := kvpb.Header{
 					Txn: &txn,
@@ -1990,6 +1990,20 @@ func TestTxnWriteBufferBatchRequestValidation(t *testing.T) {
 					},
 				}
 				return &kvpb.BatchRequest{Header: header}
+			},
+		},
+		{
+			name: "batch with OriginTimestamp in ConditionalPutRequest",
+			ba: func() *kvpb.BatchRequest {
+				header := kvpb.Header{
+					Txn: &txn,
+				}
+				b := &kvpb.BatchRequest{Header: header}
+				r := &kvpb.ConditionalPutRequest{
+					OriginTimestamp: hlc.Timestamp{WallTime: 1},
+				}
+				b.Add(r)
+				return b
 			},
 		},
 		{
@@ -2048,6 +2062,15 @@ func TestTxnWriteBufferBatchRequestValidation(t *testing.T) {
 					ScanFormat:    kvpb.COL_BATCH_RESPONSE,
 					RequestHeader: kvpb.RequestHeader{Key: keyA, EndKey: keyC, Sequence: txn.Sequence},
 				}
+				b.Add(r)
+				return b
+			},
+		},
+		{
+			name: "batch with unsupported request",
+			ba: func() *kvpb.BatchRequest {
+				b := &kvpb.BatchRequest{Header: kvpb.Header{Txn: &txn}}
+				r := &kvpb.TruncateLogRequest{}
 				b.Add(r)
 				return b
 			},
