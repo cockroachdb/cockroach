2bb22940d76b3128742fd96ee966c453a4964b54
Herko Lategan <herko@cockroachlabs.com>
2025-06-17 16:47:11 +0100

    testutils: add roachtest analyzer
    Previously, there was no linter that would detect use of the `go` keyword in
    roachtest. All roachtest goroutines must be launched through the `Task` interface
    provided by the roachtest framework.
    
    This change reuses a linter from the `lint passes forbiddenmethod` package to
    detect use of the `go` keyword in roachtest.
    
    The roachtest go analyzer only operates on the roachtest package, with a filter
    hardcoded in the analyzer. Thus we can still add excludes in the nogo config to
    allow goroutines in other packages.
    
    Epic: None
    Release note: None

--- pkg/testutils/lint/passes/forbiddenmethod/naked_go.go
+++ pkg/testutils/lint/passes/forbiddenmethod/naked_go.go
@@ -16,72 +16,84 @@ import (
 	"golang.org/x/tools/go/ast/inspector"
 )
 
-const nakedGoPassName = "nakedgo"
-
 // NakedGoAnalyzer prevents use of the `go` keyword.
-var NakedGoAnalyzer = &analysis.Analyzer{
-	Name:     nakedGoPassName,
-	Doc:      "Prevents direct use of the 'go' keyword. Goroutines should be launched through Stopper.",
-	Requires: []*analysis.Analyzer{inspect.Analyzer},
-	Run: func(pass *analysis.Pass) (interface{}, error) {
-		inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-		inspect.Preorder([]ast.Node{
-			(*ast.GoStmt)(nil),
-		}, func(n ast.Node) {
-			node := n.(*ast.GoStmt)
+var NakedGoAnalyzer = NewNakedGoAnalyzer(
+	"nakedgo",
+	"Use of go keyword not allowed, use a Stopper instead",
+	"Prevents direct use of the 'go' keyword. Goroutines should be launched through Stopper.",
+	nil,
+)
 
-			const debug = false
+type FilterFunc func(pass *analysis.Pass) bool
 
-			// NB: we're not using passesutil.HasNoLintComment because it
-			// has false negatives (i.e. comments apply to infractions that
-			// they were clearly not intended for).
-			//
-			// The approach here is inspired by `analysistest.check` - the
-			// nolint comment has to be on the same line as the *end* of the
-			// `*GoStmt`.
-			f := passesutil.FindContainingFile(pass, n)
-			cm := ast.NewCommentMap(pass.Fset, node, f.Comments)
-			var cc *ast.Comment
-			for _, cg := range cm[n] {
-				for _, c := range cg.List {
-					if c.Pos() < node.Go {
-						// The current comment is "before" the `go` invocation, so it's
-						// not relevant for silencing the lint.
-						continue
-					}
-					if cc == nil || cc.Pos() > node.Go {
-						// This comment is after, but closer to the `go` invocation than
-						// previous candidate.
-						cc = c
-						if debug {
-							fmt.Printf("closest comment now %d-%d: %s\n", cc.Pos(), cc.End(), cc.Text)
+func NewNakedGoAnalyzer(name, message, doc string, filter FilterFunc) *analysis.Analyzer {
+	return &analysis.Analyzer{
+		Name:     name,
+		Doc:      doc,
+		Requires: []*analysis.Analyzer{inspect.Analyzer},
+		Run: func(pass *analysis.Pass) (interface{}, error) {
+			if filter != nil && !filter(pass) {
+				return nil, nil
+			}
+			inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+			inspect.Preorder([]ast.Node{
+				(*ast.GoStmt)(nil),
+			}, func(n ast.Node) {
+				node := n.(*ast.GoStmt)
+
+				const debug = false
+
+				// NB: we're not using passesutil.HasNoLintComment because it
+				// has false negatives (i.e. comments apply to infractions that
+				// they were clearly not intended for).
+				//
+				// The approach here is inspired by `analysistest.check` - the
+				// nolint comment has to be on the same line as the *end* of the
+				// `*GoStmt`.
+				f := passesutil.FindContainingFile(pass, n)
+				cm := ast.NewCommentMap(pass.Fset, node, f.Comments)
+				var cc *ast.Comment
+				for _, cg := range cm[n] {
+					for _, c := range cg.List {
+						if c.Pos() < node.Go {
+							// The current comment is "before" the `go` invocation, so it's
+							// not relevant for silencing the lint.
+							continue
+						}
+						if cc == nil || cc.Pos() > node.Go {
+							// This comment is after, but closer to the `go` invocation than
+							// previous candidate.
+							cc = c
+							if debug {
+								fmt.Printf("closest comment now %d-%d: %s\n", cc.Pos(), cc.End(), cc.Text)
+							}
 						}
 					}
 				}
-			}
-			if cc != nil && strings.Contains(cc.Text, "nolint:"+nakedGoPassName) {
-				if debug {
-					fmt.Printf("GoStmt at: %d-%d\n", n.Pos(), n.End())
-					fmt.Printf("GoStmt.Go at: %d\n", node.Go)
-					fmt.Printf("GoStmt.Call at: %d-%d\n", node.Call.Pos(), node.Call.End())
-				}
-
-				goPos := pass.Fset.Position(node.End())
-				cmPos := pass.Fset.Position(cc.Pos())
-
-				if goPos.Line == cmPos.Line {
+				if cc != nil && strings.Contains(cc.Text, "nolint:"+name) {
 					if debug {
-						fmt.Printf("suppressing lint because of %d-%d: %s\n", cc.Pos(), cc.End(), cc.Text)
+						fmt.Printf("GoStmt at: %d-%d\n", n.Pos(), n.End())
+						fmt.Printf("GoStmt.Go at: %d\n", node.Go)
+						fmt.Printf("GoStmt.Call at: %d-%d\n", node.Call.Pos(), node.Call.End())
 					}
-					return
-				}
-			}
 
-			pass.Report(analysis.Diagnostic{
-				Pos:     n.Pos(),
-				Message: "Use of go keyword not allowed, use a Stopper instead",
+					goPos := pass.Fset.Position(node.End())
+					cmPos := pass.Fset.Position(cc.Pos())
+
+					if goPos.Line == cmPos.Line {
+						if debug {
+							fmt.Printf("suppressing lint because of %d-%d: %s\n", cc.Pos(), cc.End(), cc.Text)
+						}
+						return
+					}
+				}
+
+				pass.Report(analysis.Diagnostic{
+					Pos:     n.Pos(),
+					Message: message,
+				})
 			})
-		})
-		return nil, nil
-	},
+			return nil, nil
+		},
+	}
 }
