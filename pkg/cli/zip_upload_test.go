fb87824d00cf35bb471d3a925fa67412a0666eb1
Akshay Joshi <akshay@cockroachlabs.com>
2025-06-18 15:16:07 +0530

    cli(debug.zip): update Datadog link for table dumps
    This patch updates the Datadog dashboard and log explorer links which are
    generated as part of debug.zip Datadog upload. The table dump dashboard link
    contains filtering based on upload_id along with default timeframe of 1 month.
    
    Epic: None
    Part of: CRDB-51113
    Release note: None

--- pkg/cli/zip_upload_test.go
+++ pkg/cli/zip_upload_test.go
@@ -212,6 +212,10 @@ func TestUploadZipEndToEnd(t *testing.T) {
 				includeFlag = "--include=misc"
 			}
 
+			defer testutils.TestingHook(&getCurrentTime, func() time.Time {
+				return time.Date(2024, 11, 14, 0, 0, 0, 0, time.UTC)
+			})()
+
 			debugDir, cleanup := setupZipDir(t, testInput)
 			defer cleanup()
 
