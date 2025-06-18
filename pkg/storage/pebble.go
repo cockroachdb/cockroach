53f1207e2db26c1134f79e2f8ef1915115688ced
Jackson Owens <jackson@cockroachlabs.com>
2025-06-18 10:42:11 -0400

    storage: set format major verison for v25.3
    Set the storage engine's format major version for cluster version v25.3.
    
    Epic: none
    Release note: None

--- pkg/storage/pebble.go
+++ pkg/storage/pebble.go
@@ -2168,6 +2168,7 @@ func (p *Pebble) CreateCheckpoint(dir string, spans []roachpb.Span) error {
 // named version, it can be assumed all *nodes* have ratcheted to the pebble
 // version associated with it, since they did so during the fence version.
 var pebbleFormatVersionMap = map[clusterversion.Key]pebble.FormatMajorVersion{
+	clusterversion.V25_3: pebble.FormatValueSeparation,
 	clusterversion.V25_2: pebble.FormatTableFormatV6,
 }
 
