b4b0da16dfd0124cd7a15cc047cc41f03365788a
souravcrl <sourav.sarangi@cockroachlabs.com>
2025-06-20 19:08:47 +0530

    sql: update SHOW users/roles to support provisioned users
    The SHOW users/roles view currently does not have a mechanism to filter users
    via the role options that are assigned to them. This support is needed as we
    will be adding `PROVISIONSRC` role option in #148200 and this needs to have
    filters support. Additionally, we will be populating the last login
    time(estimated) for the `system.users` table and this value will be piped here as
    well. Since this value is unreliable we will be adding a notice mentioning the
    same.
    
    informs #147602
    fixes #147599
    Epic CRDB-21590
    
    Release note (enterprise change): Post the change a filter query for SHOW
    users/role will be support as shown hence:
    
    ```
    root@localhost:26257/defaultdb> select * from [show roles] as r WHERE EXISTS (SELECT 1 FROM unnest(r.options) AS m(option) where option like 'SUBJECT=cn%');
      username  |            options             | member_of | estimated_last_login_time
    ------------+--------------------------------+-----------+----------------------------
      souravcrl | {NOLOGIN,SUBJECT=cn=souravcrl} | {admin}   | NULL
    (1 row)
    
    NOTICE: estimated_last_login_time is computed on a best effort basis; it is not guaranteed to capture every login event
    Time: 8ms total (execution 8ms / network 0ms)
    ```

--- pkg/sql/delegate/show_roles.go
+++ pkg/sql/delegate/show_roles.go
@@ -6,6 +6,8 @@
 package delegate
 
 import (
+	"github.com/cockroachdb/cockroach/pkg/clusterversion"
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgnotice"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqltelemetry"
 )
@@ -14,15 +16,24 @@ import (
 // Privileges: SELECT on system.users.
 func (d *delegator) delegateShowRoles() (tree.Statement, error) {
 	sqltelemetry.IncrementShowCounter(sqltelemetry.Roles)
-	return d.parse(`
+	selectClause := `
 SELECT
 	u.username,
-	IFNULL(string_agg(o.option || COALESCE('=' || o.value, ''), ', ' ORDER BY o.option), '') AS options,
-	ARRAY (SELECT role FROM system.role_members AS rm WHERE rm.member = u.username ORDER BY 1) AS member_of
+	COALESCE(array_remove(array_agg(o.option || COALESCE('=' || o.value, '') ORDER BY o.option), NULL), ARRAY[]::STRING[]) AS options,
+	ARRAY (SELECT role FROM system.role_members AS rm WHERE rm.member = u.username ORDER BY 1) AS member_of`
+	selectLastLoginTime := `,
+	u.estimated_last_login_time`
+	endingClauses := `
 FROM
 	system.users AS u LEFT JOIN system.role_options AS o ON u.username = o.username
 GROUP BY
 	u.username
 ORDER BY 1;
-`)
+`
+	if d.evalCtx.Settings.Version.IsActive(d.ctx, clusterversion.V25_3) {
+		d.evalCtx.ClientNoticeSender.BufferClientNotice(d.ctx, pgnotice.Newf(
+			"estimated_last_login_time is computed on a best effort basis; it is not guaranteed to capture every login event"))
+		return d.parse(selectClause + selectLastLoginTime + endingClauses)
+	}
+	return d.parse(selectClause + endingClauses)
 }
