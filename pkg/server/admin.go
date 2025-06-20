34a8b9b50d689e2598d35d00c3fda360115c1155
Shubham Dhama <shubham.dhama@cockroachlabs.com>
2025-06-20 12:44:22 +0530

    server: register `Admin` service with DRPC server
    Enable these service on the DRPC server in addition to gRPC. This is
    controlled by `rpc.experimental_drpc.enabled` (off by default).
    
    This change is part of a series and is similar to: #146926
    
    Note: This only registers the service; the client is not updated to use the
    DRPC client, so this service will not have any functional effect.
    
    Epic: CRDB-48925
    Release note: None

--- pkg/server/admin.go
+++ pkg/server/admin.go
@@ -82,6 +82,8 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	grpcstatus "google.golang.org/grpc/status"
+	"storj.io/drpc"
+	"storj.io/drpc/drpcerr"
 )
 
 // Number of empty ranges for table descriptors that aren't actually tables. These
@@ -251,6 +253,26 @@ func (s *systemAdminServer) RegisterService(g *grpc.Server) {
 // RegisterService registers the GRPC service.
 func (s *adminServer) RegisterService(g *grpc.Server) {
 	serverpb.RegisterAdminServer(g, s)
+}
+
+type drpcSystemAdminServer struct {
+	*systemAdminServer
+}
+
+// RegisterDRPCService registers the Admin service with the DRPC server running
+// in the system tenant.
+func (s *systemAdminServer) RegisterDRPCService(d drpc.Mux) error {
+	return serverpb.DRPCRegisterAdmin(d, &drpcSystemAdminServer{systemAdminServer: s})
+}
+
+type drpcAdminServer struct {
+	*adminServer
+}
+
+// RegisterDRPCService registers the Admin service with the DRPC server running
+// in a secondary tenant.
+func (s *adminServer) RegisterDRPCService(d drpc.Mux) error {
+	return serverpb.DRPCRegisterAdmin(d, &drpcAdminServer{adminServer: s})
 }
 
 // RegisterGateway starts the gateway (i.e. reverse proxy) that proxies HTTP requests
@@ -3312,9 +3334,23 @@ func (s *systemAdminServer) SendKVBatch(
 	return br, nil
 }
 
+func (s *drpcSystemAdminServer) RecoveryCollectReplicaInfo(
+	request *serverpb.RecoveryCollectReplicaInfoRequest,
+	stream serverpb.DRPCAdmin_RecoveryCollectReplicaInfoStream,
+) error {
+	return s.recoveryCollectReplicaInfo(request, stream)
+}
+
 func (s *systemAdminServer) RecoveryCollectReplicaInfo(
 	request *serverpb.RecoveryCollectReplicaInfoRequest,
 	stream serverpb.Admin_RecoveryCollectReplicaInfoServer,
+) error {
+	return s.recoveryCollectReplicaInfo(request, stream)
+}
+
+func (s *systemAdminServer) recoveryCollectReplicaInfo(
+	request *serverpb.RecoveryCollectReplicaInfoRequest,
+	stream serverpb.RPCAdmin_RecoveryCollectReplicaInfoStream,
 ) error {
 	ctx := stream.Context()
 	ctx = s.server.AnnotateCtx(ctx)
@@ -3327,9 +3363,23 @@ func (s *systemAdminServer) RecoveryCollectReplicaInfo(
 	return s.server.recoveryServer.ServeClusterReplicas(ctx, request, stream, s.server.db)
 }
 
+func (s *drpcSystemAdminServer) RecoveryCollectLocalReplicaInfo(
+	request *serverpb.RecoveryCollectLocalReplicaInfoRequest,
+	stream serverpb.DRPCAdmin_RecoveryCollectLocalReplicaInfoStream,
+) error {
+	return s.recoveryCollectLocalReplicaInfo(request, stream)
+}
+
 func (s *systemAdminServer) RecoveryCollectLocalReplicaInfo(
 	request *serverpb.RecoveryCollectLocalReplicaInfoRequest,
 	stream serverpb.Admin_RecoveryCollectLocalReplicaInfoServer,
+) error {
+	return s.recoveryCollectLocalReplicaInfo(request, stream)
+}
+
+func (s *systemAdminServer) recoveryCollectLocalReplicaInfo(
+	request *serverpb.RecoveryCollectLocalReplicaInfoRequest,
+	stream serverpb.RPCAdmin_RecoveryCollectLocalReplicaInfoStream,
 ) error {
 	ctx := stream.Context()
 	ctx = s.server.AnnotateCtx(ctx)
@@ -4042,4 +4092,22 @@ func (s *systemAdminServer) ReadFromTenantInfo(
 	}
 
 	return &serverpb.ReadFromTenantInfoResponse{ReadFrom: dstID, ReadAt: progress.GetStreamIngest().ReplicatedTime}, nil
+}
+
+// RecoveryCollectReplicaInfo is unimplemented here because adminServer also
+// have it delegated from embedded serverpb.UnimplementedAdminServer.
+func (s *drpcAdminServer) RecoveryCollectReplicaInfo(
+	request *serverpb.RecoveryCollectReplicaInfoRequest,
+	stream serverpb.DRPCAdmin_RecoveryCollectReplicaInfoStream,
+) error {
+	return drpcerr.WithCode(errors.New("Unimplemented"), drpcerr.Unimplemented)
+}
+
+// RecoveryCollectLocalReplicaInfo is unimplemented here because adminServer
+// also have it delegated from embedded serverpb.UnimplementedAdminServer.
+func (s *drpcAdminServer) RecoveryCollectLocalReplicaInfo(
+	request *serverpb.RecoveryCollectLocalReplicaInfoRequest,
+	stream serverpb.DRPCAdmin_RecoveryCollectLocalReplicaInfoStream,
+) error {
+	return drpcerr.WithCode(errors.New("Unimplemented"), drpcerr.Unimplemented)
 }
