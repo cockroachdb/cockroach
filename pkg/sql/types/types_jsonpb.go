a17c94fdc37b048f70c2efb56129345f3df2a3a4
Rafi Shamim <rafi@cockroachlabs.com>
2025-06-23 12:24:30 -0400

    types: remove downgradeType function
    This function was only needed for mixed-version clusters that had nodes
    running on 19.1, which did not know about the new format for encoding
    types.
    
    There's no need to persist the old format anywhere now. We will need to
    keep upgradeType until we no longer need compatibility with the older
    type format. As part of this change, upgradeType was also modified so it
    does not attempt to set the OID if it has already been set. This is
    because the OID only will have been set if the type is in the new
    format.
    
    Release note: None

--- pkg/sql/types/types_jsonpb.go
+++ pkg/sql/types/types_jsonpb.go
@@ -18,12 +18,8 @@ import (
 // MarshalJSONPB marshals the T to json. This is necessary as otherwise
 // this field will be lost to the crdb_internal.pb_to_json and the likes.
 func (t *T) MarshalJSONPB(marshaler *jsonpb.Marshaler) ([]byte, error) {
-	temp := *t
-	if err := temp.downgradeType(); err != nil {
-		return nil, err
-	}
 	var buf bytes.Buffer
-	if err := marshaler.Marshal(&buf, &temp.InternalType); err != nil {
+	if err := marshaler.Marshal(&buf, &t.InternalType); err != nil {
 		return nil, err
 	}
 	return buf.Bytes(), nil
