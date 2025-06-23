2fad5087757a024c09d124c9fc86a4abd2a884ad
Rafi Shamim <rafi@cockroachlabs.com>
2025-06-23 12:24:30 -0400

    types: remove downgradeType function
    This function was only needed for mixed-version clusters that had nodes
    running on 19.1, which did not know about the new format for encoding
    types.
    
    There's no need to persist the old format anywhere now. We will need to
    keep upgradeType until we no longer need compatibility with the older
    type format. As part of this change, upgradeType was also modified so it
    does not attempt to set the OID if it has already been set. This is
    because the OID only will have been set if the type is in the new
    format.
    
    Release note: None

--- pkg/sql/crdb_internal_test.go
+++ pkg/sql/crdb_internal_test.go
@@ -231,6 +231,7 @@ CREATE TABLE t.test (k INT);
 		kvDB, s.Codec(), "t", "test")
 	for i := range tableDesc.Columns {
 		if tableDesc.Columns[i].Name == "k" {
+			tableDesc.Columns[i].Type.InternalType.Oid = 0         // Pre-2.1 types don't have an OID.
 			tableDesc.Columns[i].Type.InternalType.VisibleType = 4 // Pre-2.1 BIT.
 			tableDesc.Columns[i].Type.InternalType.Width = 12      // Arbitrary non-std INT size.
 			break
