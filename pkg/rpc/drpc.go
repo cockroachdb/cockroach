732ebe82be96fef322cd678da855e30c18d5a00d
Shaik Zakir Hussain <zakir.shaik@cockroachlabs.com>
2025-06-23 14:26:24 +0530

    fixup! fixup! fixup! fixup! rpc: wire drpc client interceptors in dialer

--- pkg/rpc/drpc.go
+++ pkg/rpc/drpc.go
@@ -79,12 +79,12 @@ func DialDRPC(
 
 		if rpcCtx.Knobs.UnaryClientInterceptorDRPC != nil {
 			if interceptor := rpcCtx.Knobs.UnaryClientInterceptorDRPC(target, rpcbase.DefaultClass); interceptor != nil {
-				rpcCtx.clientUnaryInterceptorsDRPC = []drpcclient.UnaryClientInterceptor{interceptor}
+				rpcCtx.clientUnaryInterceptorsDRPC = append(rpcCtx.clientUnaryInterceptorsDRPC, interceptor)
 			}
 		}
 		if rpcCtx.Knobs.StreamClientInterceptorDRPC != nil {
 			if interceptor := rpcCtx.Knobs.StreamClientInterceptorDRPC(target, rpcbase.DefaultClass); interceptor != nil {
-				rpcCtx.clientStreamInterceptorsDRPC = []drpcclient.StreamClientInterceptor{interceptor}
+				rpcCtx.clientStreamInterceptorsDRPC = append(rpcCtx.clientStreamInterceptorsDRPC, interceptor)
 			}
 		}
 		clientConn, _ := drpcclient.NewClientConnWithOptions(
