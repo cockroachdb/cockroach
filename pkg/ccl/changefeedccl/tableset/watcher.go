64d72d992bb3a8c0e5e6c31c06f43915c48d6f3c
Miles Frankel <miles.frankel@cockroachlabs.com>
2025-06-26 17:15:02 -0400

    rf version

--- pkg/ccl/changefeedccl/tableset/watcher.go
+++ pkg/ccl/changefeedccl/tableset/watcher.go
@@ -4,33 +4,52 @@ package tableset
 import (
 	"context"
 	"fmt"
+	"time"
 
+	"github.com/cockroachdb/cockroach/pkg/ccl/changefeedccl/cdcevent"
+	"github.com/cockroachdb/cockroach/pkg/ccl/changefeedccl/changefeedbase"
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/kv"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvclient/rangefeed"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/sql"
-	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/catalogkeys"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
-	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descs"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/systemschema"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/mon"
+	"github.com/cockroachdb/cockroach/pkg/util/span"
 	"github.com/cockroachdb/cockroach/pkg/util/tracing"
+	"github.com/cockroachdb/errors"
 	"github.com/cockroachdb/logtags"
 )
 
 type Filter struct {
+	DatabaseID    descpb.ID
+	SchemaID      descpb.ID
 	IncludeTables []string
 	ExcludeTables []string
 }
 
-func (f Filter) Includes(table catalog.TableDescriptor) bool {
+func (f Filter) Includes(tableInfo Table) bool {
+	if f.DatabaseID != 0 && tableInfo.NameInfo.ParentID != f.DatabaseID {
+		return false
+	}
+	if f.SchemaID != 0 && tableInfo.NameInfo.ParentSchemaID != f.SchemaID {
+		return false
+	}
 	if len(f.ExcludeTables) > 0 {
 		for _, exclude := range f.ExcludeTables {
-			if table.GetName() == exclude {
+			if tableInfo.NameInfo.Name == exclude {
 				return false
 			}
 		}
 		return true
 	} else if len(f.IncludeTables) > 0 {
 		for _, include := range f.IncludeTables {
-			if table.GetName() == include {
+			if tableInfo.NameInfo.Name == include {
 				return true
 			}
 		}
@@ -43,12 +62,12 @@ func (f Filter) Includes(table catalog.TableDescriptor) bool {
 }
 
 type Table struct {
-	Name string
-	ID   descpb.ID
+	descpb.NameInfo
+	ID descpb.ID
 }
 
 func (t Table) String() string {
-	return fmt.Sprintf("%s.%d", t.Name, t.ID)
+	return fmt.Sprintf("parent_id=%d, parent_schema_id=%d, name=%s, id=%d", t.NameInfo.ParentID, t.NameInfo.ParentSchemaID, t.NameInfo.Name, t.ID)
 }
 
 type TableSet struct {
@@ -57,8 +76,7 @@ type TableSet struct {
 }
 
 type Watcher struct {
-	filter     Filter
-	databaseID descpb.ID
+	filter Filter
 
 	id      string
 	mon     *mon.BytesMonitor
@@ -67,77 +85,199 @@ type Watcher struct {
 	tablesets []TableSet
 }
 
-func NewWatcher(databaseID descpb.ID, filter Filter, execCfg *sql.ExecutorConfig, mon *mon.BytesMonitor, id string) *Watcher {
-	return &Watcher{databaseID: databaseID, filter: filter, execCfg: execCfg, mon: mon, id: id}
+func NewWatcher(filter Filter, execCfg *sql.ExecutorConfig, mon *mon.BytesMonitor, id string) *Watcher {
+	return &Watcher{filter: filter, execCfg: execCfg, mon: mon, id: id}
 }
 
-func (w *Watcher) Run(ctx context.Context, initialTS hlc.Timestamp, restart chan<- struct{}) error {
-	ctx = logtags.AddTag(ctx, "changefeed.tableset.watcher.filter", w.filter)
-	ctx = logtags.AddTag(ctx, "changefeed.tableset.watcher.id", w.id)
+func (w *Watcher) Start(ctx context.Context, initialTS hlc.Timestamp) error {
+	ctx = logtags.AddTag(ctx, "tableset.watcher.filter", w.filter)
 	ctx, sp := tracing.ChildSpan(ctx, "changefeed.tableset.watcher.start")
 	defer sp.Finish()
 
-	tableSet, err := w.TableSet(ctx, initialTS)
+	acc := w.mon.MakeBoundAccount()
+
+	errCh := make(chan error, 1)
+
+	setErr := func(err error) {
+		if err == nil {
+			return
+		}
+		select {
+		case errCh <- err:
+		default:
+		}
+	}
+
+	// TODO: should really be on system.descriptor too/instead
+
+	var cfTargets changefeedbase.Targets
+	// TODO: not getting any data except id when i specify that fam
+	// cfTargets.Add(changefeedbase.Target{
+	// 	TableID:           systemschema.NamespaceTable.TableDescriptor.GetID(),
+	// 	StatementTimeName: changefeedbase.StatementTimeName(systemschema.NamespaceTable.TableDescriptor.TableDesc().Name),
+	// 	Type:              jobspb.ChangefeedTargetSpecification_COLUMN_FAMILY,
+	// 	FamilyName:        "fam_4_id",
+	// })
+	cfTargets.Add(changefeedbase.Target{
+		TableID:           systemschema.NamespaceTable.GetID(),
+		StatementTimeName: changefeedbase.StatementTimeName(systemschema.NamespaceTable.GetName()),
+		Type:              jobspb.ChangefeedTargetSpecification_EACH_FAMILY,
+	})
+	dec, err := cdcevent.NewEventDecoder(ctx, w.execCfg, cfTargets, false, false)
 	if err != nil {
 		return err
 	}
 
-	for {
-		w.tablesets = append(w.tablesets, tableSet)
-		initialTS = tableSet.AsOf
-		restart <- struct{}{}
+	// called from initial scans and maybe other places (catchups?)
+	onValues := func(ctx context.Context, values []kv.KeyValue) {
+		setErr(func() error {
+			for _, kv := range values {
+				if !kv.Value.IsPresent() {
+					continue // ?
+				}
+				kvpb := roachpb.KeyValue{Key: kv.Key, Value: *kv.Value}
+				table, err := kvToTable(ctx, kvpb, dec, w)
+				if err != nil {
+					return err
+				}
+				fmt.Printf("(scan?) table: %s\n", table)
+			}
+			return nil
+		}())
 	}
 
-	return nil
-}
-
-// what if tables come and go between polls? we should capture that. -> get all versions like schemafeed, or do rangefeed stuff
-
-// do we need a peek and pop like the schemafeed? we don't want to store schema changes forever..
-// maybe we only need to store between calls?
-// changes between the last call and now, delete the old ones.
-
-// is this all we need?
-func (w *Watcher) TableSet(ctx context.Context, asOf hlc.Timestamp) (TableSet, error) {
-	ctx = logtags.AddTag(ctx, "changefeed.tableset.watcher.filter", w.filter)
-	ctx = logtags.AddTag(ctx, "changefeed.tableset.watcher.id", w.id)
-	ctx, sp := tracing.ChildSpan(ctx, "changefeed.tableset.watcher.tableset")
-	defer sp.Finish()
-
-	tableSet := TableSet{
-		Set:  make(map[Table]struct{}),
-		AsOf: asOf,
-	}
-	err := w.execCfg.InternalDB.DescsTxn(ctx, func(ctx context.Context, txn descs.Txn) error {
-		if err := txn.KV().SetFixedTimestamp(ctx, asOf); err != nil {
-			return err
-		}
-		dbDesc, err := txn.Descriptors().ByIDWithoutLeased(txn.KV()).Get().Database(ctx, w.databaseID)
-		if err != nil {
-			return err
-		}
-		tables, err := txn.Descriptors().GetAllTablesInDatabase(ctx, txn.KV(), dbDesc)
-		if err != nil {
-			return err
-		}
-		tables.ForEachDescriptor(func(desc catalog.Descriptor) error {
-			tableDesc, err := catalog.AsTableDescriptor(desc)
+	// called with ordinary rangefeed values
+	onValue := func(ctx context.Context, kv *kvpb.RangeFeedValue) {
+		setErr(func() error {
+			if !kv.Value.IsPresent() {
+				return nil
+			}
+			pbkv := roachpb.KeyValue{Key: kv.Key, Value: kv.Value}
+			table, err := kvToTable(ctx, pbkv, dec, w)
 			if err != nil {
 				return err
 			}
-			if w.filter.Includes(tableDesc) {
-				table := Table{
-					Name: tableDesc.GetName(),
-					ID:   tableDesc.GetID(),
-				}
-				tableSet.Set[table] = struct{}{}
+			if !w.filter.Includes(table) {
+				return nil
 			}
+			fmt.Printf("(onValue) table: %s\n", table)
 			return nil
-		})
+		}())
+	}
+
+	// Common rangefeed options.
+	opts := []rangefeed.Option{
+		rangefeed.WithPProfLabel("job", fmt.Sprintf("id=%s", w.id)),
+		rangefeed.WithMemoryMonitor(w.mon),
+		rangefeed.WithOnCheckpoint(func(ctx context.Context, checkpoint *kvpb.RangeFeedCheckpoint) {}),
+		rangefeed.WithOnInternalError(func(ctx context.Context, err error) { setErr(err) }),
+		rangefeed.WithFrontierQuantized(1 * time.Second),
+		rangefeed.WithOnValues(onValues),
+		rangefeed.WithDiff(true),
+		rangefeed.WithConsumerID(int64(42)),
+		rangefeed.WithInvoker(func(fn func() error) error { return fn() }),
+		rangefeed.WithFiltering(false),
+		rangefeed.WithInitialScan(func(ctx context.Context) {
+			fmt.Printf("initial scan done\n")
+		}),
+	}
+
+	// // find family to watch
+	// var indexID descpb.IndexID
+	// for _, family := range systemschema.NamespaceTable.TableDescriptor.TableDesc().Families {
+	// 	if family.Name == "fam_4_id" {
+	// 		indexID = descpb.IndexID(family.ID)
+	// 		break
+	// 	}
+	// }
+	// watchSpans := roachpb.Spans{systemschema.NamespaceTable.IndexSpan(w.execCfg.Codec, indexID)}
+	watchSpans := roachpb.Spans{systemschema.NamespaceTable.TableSpan(w.execCfg.Codec)}
+
+	frontier, err := span.MakeFrontier(watchSpans...)
+	if err != nil {
+		return err
+	}
+	frontier = span.MakeConcurrentFrontier(frontier)
+	defer frontier.Release()
+
+	// TODO: is this our buffer size? do we need this?
+	if err := acc.Grow(ctx, 1024); err != nil {
+		return errors.Wrapf(err, "failed to allocated %d bytes from monitor", 1024)
+	}
+
+	// Start rangefeed.
+	rf := w.execCfg.RangeFeedFactory.New(
+		fmt.Sprintf("tableset.watcher.id=%s", w.id), initialTS, onValue, opts...,
+	)
+	defer rf.Close()
+
+	fmt.Printf("starting rangefeed\n")
+
+	if err := rf.StartFromFrontier(ctx, frontier); err != nil {
+		return err
+	}
+
+	fmt.Printf("rangefeed started\n")
+
+	// wait for shutdown due to error or context cancellation
+	select {
+	case err := <-errCh:
+		fmt.Printf("shutting down due to error: %v\n", err)
+		return err
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
+func (w *Watcher) OnChange(cb func(tableSet TableSet)) {}
+
+func (w *Watcher) ValidAsOf(tableSet TableSet, ts hlc.Timestamp) bool {
+	return false
+}
+
+func (w *Watcher) Close() {}
+
+func kvToTable(ctx context.Context, kv roachpb.KeyValue, dec cdcevent.Decoder, w *Watcher) (Table, error) {
+	row, err := dec.DecodeKV(ctx, kv, cdcevent.CurrentRow, kv.Value.Timestamp, false)
+	if err != nil {
+		return Table{}, err
+	}
+
+	// decode the row into the table id, and the key into name info
+	var tableId descpb.ID
+	row.ForEachColumn().Datum(func(d tree.Datum, col cdcevent.ResultColumn) error {
+		if col.Name == "id" {
+			tableId = descpb.ID(tree.MustBeDInt(d))
+		}
 		return nil
 	})
+
+	nameInfo, err := catalogkeys.DecodeNameMetadataKey(w.execCfg.Codec, kv.Key)
 	if err != nil {
-		return TableSet{}, err
+		fmt.Printf("failed to decode namespace key: %v\n", err)
+		return Table{}, err
 	}
-	return tableSet, nil
+
+	return Table{
+		NameInfo: nameInfo,
+		ID:       tableId,
+	}, nil
 }
+
+// prettyRow := func(row cdcevent.Row) string {
+// 	var b strings.Builder
+// 	fmt.Fprintf(&b, "Row{")
+// 	err := row.ForEachColumn().Datum(func(d tree.Datum, col cdcevent.ResultColumn) error {
+// 		fmt.Fprintf(&b, "%s: %+v, ", col.Name, d)
+// 		return nil
+// 	})
+// 	if err != nil {
+// 		return "err: " + err.Error()
+// 	}
+// 	fmt.Fprintf(&b, "}")
+// 	return b.String()
+// }
+
+// prettyKey := func(key roachpb.Key) string {
+// 	return catalogkeys.PrettyKey(nil, key, -1)
+// }
--- pkg/ccl/changefeedccl/tableset/watcher_test.go
+++ pkg/ccl/changefeedccl/tableset/watcher_test.go
@@ -1,9 +1,92 @@
 package tableset
 
 import (
+	"context"
+	"fmt"
 	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
+	"github.com/cockroachdb/cockroach/pkg/sql"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descs"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/mon"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
 )
 
-func TestWatcher(t *testing.T) {
+func TestTablesetDebug(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
 
+	ctx, cancel := context.WithCancel(context.Background())
+	_ = cancel
+	s, db, _ := serverutils.StartServer(t, base.TestServerArgs{})
+	defer s.Stopper().Stop(ctx)
+
+	execCfg := s.ExecutorConfig().(sql.ExecutorConfig)
+
+	mm := mon.NewMonitor(mon.Options{
+		Name:      mon.MakeName("test-mm"),
+		Limit:     1024 * 1024,
+		Increment: 128,
+		Settings:  cluster.MakeTestingClusterSettings(),
+	})
+	mm.Start(context.Background(), nil, mon.NewStandaloneBudget(1024*2024))
+	defer mm.Stop(ctx)
+
+	var dbId descpb.ID
+	require.NoError(t, execCfg.InternalDB.DescsTxn(ctx, func(ctx context.Context, txn descs.Txn) error {
+		dbs, err := txn.Descriptors().GetAllDatabases(ctx, txn.KV())
+		if err != nil {
+			return err
+		}
+		dbs = dbs.FilterByNames([]descpb.NameInfo{{Name: "defaultdb"}})
+		dbs.ForEachDescriptor(func(desc catalog.Descriptor) error {
+			dbId = desc.GetID()
+			return nil
+		})
+		return nil
+	}))
+
+	filter := Filter{
+		DatabaseID:    dbId,
+		ExcludeTables: []string{"exclude_me"},
+	}
+	watcher := NewWatcher(filter, &execCfg, mm, "testwatcher1")
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	ts := hlc.Timestamp{WallTime: timeutil.Now().UnixNano()}
+
+	eg.Go(func() error {
+		return watcher.Start(ctx, ts)
+	})
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	eg.Go(func() error {
+		for i := 0; ; i++ {
+			select {
+			case <-ticker.C:
+				db.Exec(fmt.Sprintf("create table foo_%d (id int primary key)", i))
+			case <-ctx.Done():
+				return ctx.Err()
+			}
+		}
+	})
+
+	time.AfterFunc(1*time.Minute, func() {
+		cancel()
+	})
+
+	require.NoError(t, eg.Wait())
 }
