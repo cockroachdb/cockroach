34917766be115595d79e10bfce78d1237db39fe8
Pradyum <pradyum.agarwal@cockroachlabs.com>
2025-06-26 14:24:37 +0530

    workload_generator: parse DDLs and build schema structs for debug-zip workloads
    Before this patch, there was no support for parsing CREATE TABLE DDLs or representing their schema metadata in Go.
    This was inadequate because the workload generator could not programmatically consume table definitions, column order, or constraint information from CockroachDB debug zip outputs.
    To address this, this patch introduces a GenerateDDLs function and TableSchema structs that parse DDL statements exported in debug zips and build internal schema representations (tracking column order, primary keys, unique and foreign key constraints). This forms the foundation for generating workloads directly from debug-zip schemas.
    Fixes: CRDB-51752

---
+++ pkg/workload/workload_generator/constants.go
@@ -0,0 +1,19 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package workload_generator
+
+const (
+
+	// SQL keywords for constraints
+	sqlIndex      = "INDEX"       // sqlIndex is a constant string used to represent index definitions in column definitions
+	sqlNull       = "NULL"        // sqlNull is a constant string used to represent NULL values in column definitions
+	sqlNotNull    = "NOT NULL"    // sqlNotNull is a constant string used to represent NOT NULL constraints in column definitions
+	sqlPrimaryKey = "PRIMARY KEY" // sqlPrimaryKey is a constant string used to represent primary key constraints in column definitions
+	sqlUnique     = "UNIQUE"      // sqlUnique is a constant string used to represent unique constraints in column definitions
+	sqlDefault    = "DEFAULT"     // sqlDefault is a constant string used to represent default value expressions in column definitions
+	sqlCheck      = "CHECK"       // sqlCheck is a constant string used to represent CHECK constraints in column definitions
+	sqlForeignKey = "FOREIGN KEY" // sqlForeignKey is a constant string used to represent foreign key constraints in column definitions
+)
