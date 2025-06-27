15ed8cadb24a69e71a06b3dc54d73c8898b84465
Rafi Shamim <rafi@cockroachlabs.com>
2025-06-27 16:45:23 -0400

    schemachanger: attempt to deflake schemachanger integration tests
    We are seeing more timeouts in the tests. This patch attempts to deflake
    the test by forcing production values for constants and settings, rather
    than using metamorphic settings.
    
    It also skips more tests under deadlock, which were already skipped
    under race.
    
    Release note: None

--- pkg/ccl/schemachangerccl/schemachanger_ccl_test.go
+++ pkg/ccl/schemachangerccl/schemachanger_ccl_test.go
@@ -17,6 +17,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scexec"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/sctest"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/eval"
 	"github.com/cockroachdb/cockroach/pkg/testutils/datapathutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
@@ -57,6 +58,9 @@ func (f MultiRegionTestClusterFactory) Run(
 ) {
 	const numServers = 3
 	knobs := base.TestingKnobs{
+		SQLEvalContext: &eval.TestingKnobs{
+			ForceProductionValues: true,
+		},
 		JobsTestingKnobs: jobs.NewTestingKnobsWithShortIntervals(),
 		SQLExecutor: &sql.ExecutorTestingKnobs{
 			UseTransactionalDescIDGenerator: true,
