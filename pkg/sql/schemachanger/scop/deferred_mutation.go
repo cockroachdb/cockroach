935f1631777f128c7355a150873f0be5ba1d6d71
Brendan Gerrity <brendan.gerrity@cockroachlabs.com>
2025-06-27 16:07:38 -0400

    sql: use friendly schema changes status messages
    Previously the job status message provided limited information.
    It required knowledge of the schema change architecture to be
    actionable.
    The status message is expanded to be more user-friendly.
    
    Fixes: #125560
    Epic: CRDB-42485
    
    Release note (sql change): Schema change job status messages provide
    additional information.

--- pkg/sql/schemachanger/scop/deferred_mutation.go
+++ pkg/sql/schemachanger/scop/deferred_mutation.go
@@ -9,6 +9,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scpb"
+	"github.com/cockroachdb/redact"
 )
 
 //go:generate go run ./generate_visitor.go scop DeferredMutation deferred_mutation.go deferred_mutation_visitor_generated.go
@@ -48,7 +49,7 @@ type UpdateSchemaChangerJob struct {
 	deferredMutationOp
 	IsNonCancelable       bool
 	JobID                 jobspb.JobID
-	RunningStatus         string
+	RunningStatus         redact.RedactableString
 	DescriptorIDsToRemove []descpb.ID
 }
 
@@ -64,7 +65,7 @@ type CreateSchemaChangerJob struct {
 	// NonCancelable maps to the job's property, but in the schema changer can
 	// be thought of as !Revertible.
 	NonCancelable bool
-	RunningStatus string
+	RunningStatus redact.RedactableString
 }
 
 // RemoveDatabaseRoleSettings is used to delete a role setting for a database.
