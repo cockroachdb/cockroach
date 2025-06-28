64088643478f9257561fb88f167fe37e98087923
Drew Kimball <drewk@cockroachlabs.com>
2025-06-27 18:46:09 -0600

    workload/tpcds: remove remaining queries that can't be parsed
    This commit removes some queries from `QueriesByNumber` that can't
    be parsed because of the `rollup()` function.
    
    Epic: None
    
    Release note: None

--- pkg/workload/tpcds/queries.go
+++ pkg/workload/tpcds/queries.go
@@ -11,11 +11,12 @@ const NumQueries = 99
 // QueriesByNumber is a mapping from the number of a TPC-DS query to the actual
 // query. Only queries that can be parsed by CockroachDB are present.
 var QueriesByNumber = map[int]string{
-	1:  query1,
-	2:  query2,
-	3:  query3,
-	4:  query4,
-	5:  query5,
+	1: query1,
+	2: query2,
+	3: query3,
+	4: query4,
+	// TODO: adjust this query.
+	//5:  query5,
 	6:  query6,
 	7:  query7,
 	8:  query8,
@@ -24,15 +25,18 @@ var QueriesByNumber = map[int]string{
 	11: query11,
 	12: query12,
 	13: query13,
-	14: query14,
+	// TODO: adjust this query.
+	// 14: query14,
 	15: query15,
 	16: query16,
 	17: query17,
-	18: query18,
+	// TODO: adjust this query.
+	// 18: query18,
 	19: query19,
 	20: query20,
 	21: query21,
-	22: query22,
+	// TODO: adjust the query.
+	// 22: query22,
 	23: query23,
 	24: query24,
 	25: query25,
@@ -79,7 +83,8 @@ var QueriesByNumber = map[int]string{
 	64: query64,
 	65: query65,
 	66: query66,
-	67: query67,
+	// TODO: adjust the query.
+	// 67: query67,
 	68: query68,
 	69: query69,
 	// TODO(yuzefovich): adjust the query.
@@ -90,10 +95,12 @@ var QueriesByNumber = map[int]string{
 	74: query74,
 	75: query75,
 	76: query76,
-	77: query77,
+	// TODO: adjust the query.
+	// 77: query77,
 	78: query78,
 	79: query79,
-	80: query80,
+	// TODO: adjust the query.
+	// 80: query80,
 	81: query81,
 	82: query82,
 	83: query83,
@@ -118,9 +125,16 @@ var QueriesByNumber = map[int]string{
 
 // TODO(yuzefovich): remove once these queries are "enabled."
 var (
+	_ = query5
+	_ = query14
+	_ = query18
+	_ = query22
 	_ = query27
 	_ = query36
+	_ = query67
 	_ = query70
+	_ = query77
+	_ = query80
 	_ = query86
 )
 
@@ -2985,7 +2999,7 @@ SELECT
 	c_birth_country,
 	c_login,
 	c_email_address,
-	c_last_review_date_sk,
+	c_last_review_date,
 	ctr_total_return
 FROM
 	customer_total_return AS ctr1,
@@ -3016,7 +3030,7 @@ ORDER BY
 	c_birth_country,
 	c_login,
 	c_email_address,
-	c_last_review_date_sk,
+	c_last_review_date,
 	ctr_total_return
 LIMIT
 	100;
