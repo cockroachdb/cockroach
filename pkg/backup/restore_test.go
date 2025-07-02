e75db478f7949e37c6b3083c3d55c9dbc42da81f
Kevin Cao <kevin.cao@cockroachlabs.com>
2025-07-02 17:32:28 -0400

    restore: add secondary retry policy to restore
    This commit teaches restore to use a more dynamic retry policy that
    depends on the progress of the restore. Previously, restore would
    attempt a retry a set amount of times before failing with some minimal
    max backoff. This approach works relatively well in cases where a
    restore is failing early into a job and presumably there exists some
    issue blocking restore and we want to fail early.
    
    However, for restores that have been running to near completion, it
    becomes increasingly more likely that the error is a transient and due
    to some temporary cluster unhealthiness. In these cases, it is
    beneficial to retry with a longer backup and for a longer period of time
    before reporting a failure.
    
    We now update restore such that if the restore progress has exceeded
    some predefined threshold, we switch from an attempt-limited retry
    mechanism to a duration-limited mechanism with larger backoffs.
    
    Epic: CRDB-51362
    
    Fixes: #148028
    
    Release note: None

--- pkg/backup/restore_test.go
+++ pkg/backup/restore_test.go
@@ -7,16 +7,25 @@ package backup
 
 import (
 	"context"
+	"syscall"
 	"testing"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/backup/backuptestutils"
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/jobs"
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/sql"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/jobutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/retry"
+	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/require"
 )
 
@@ -84,4 +93,125 @@ func TestFailAfterCleanupSystemTables(t *testing.T) {
 
 	sqlDB.Exec(t, "CANCEL JOB $1", jobID)
 	jobutils.WaitForJobToCancel(t, sqlDB, jobID)
+}
+
+func TestDynamicRestoreRetryPolicySwitching(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	skip.UnderRace(t, "test is flaky without extending duration of retry policy, takes too long")
+	skip.UnderDeadlock(t, "test is flaky under deadlock detector, takes too long")
+
+	// Max duration needs to be long enough such that the restore job
+	// runtime does not exceed the max duration of the retry policy, or
+	// else very few attempts will be made.
+	maxDuration := 100 * time.Millisecond
+	if skip.Stress() {
+		// Under stress, the restore will take longer to complete, so we need to
+		// increase max duration accordingly.
+		maxDuration = 500 * time.Millisecond
+	}
+	maxRetries := 3
+	const numNodes = 1
+	const numAccounts = 10
+
+	// This will run a restore job and fail it repeatedly at the start of the
+	// restore until the initial retry policy is exhausted. After that, it will
+	// allow progress to be made, and then start failing the restore at the end of
+	// the job to exhaust the retry policy again (regardless of whether it
+	// switched or not). It tracks the number of attempts made by the restore job
+	// and returns it.
+	runRestoreAndTrackAttempts := func(
+		t *testing.T, policySwitchThreshold float32, endStatus jobs.State,
+	) int {
+		mu := struct {
+			syncutil.Mutex
+			attemptCount int
+		}{}
+		// waitForProgress is a channel that will be closed whenever we detect that
+		// the restore job has made progress.
+		waitForProgress := make(chan struct{})
+
+		testKnobs := &sql.BackupRestoreTestingKnobs{
+			InitialRestoreDistSQLRetryPolicy: &retry.Options{
+				InitialBackoff: time.Microsecond,
+				Multiplier:     2,
+				MaxBackoff:     2 * time.Microsecond,
+				MaxRetries:     maxRetries,
+			},
+			SecondaryRestoreDistSQLRetryPolicy: &retry.Options{
+				InitialBackoff: 2 * time.Microsecond,
+				Multiplier:     2,
+				MaxBackoff:     100 * time.Millisecond,
+				MaxDuration:    maxDuration,
+			},
+			RestoreRetryPolicySwitchThreshold: policySwitchThreshold,
+			RunBeforeRestoreFlow: func() error {
+				mu.Lock()
+				defer mu.Unlock()
+				mu.attemptCount++
+
+				if mu.attemptCount <= maxRetries {
+					// Have not consumed all retries on initial policy.
+					return syscall.ECONNRESET
+				}
+
+				return nil
+			},
+			RunAfterRestoreFlow: func() error {
+				// Wait for progress to persist, then continue sending retryable errors
+				<-waitForProgress
+				return syscall.ECONNRESET
+			},
+		}
+		var params base.TestClusterArgs
+		params.ServerArgs.Knobs.BackupRestore = testKnobs
+
+		_, sqlDB, _, cleanupFn := backuptestutils.StartBackupRestoreTestCluster(
+			t, numNodes, backuptestutils.WithParams(params), backuptestutils.WithBank(numAccounts),
+		)
+		defer cleanupFn()
+
+		sqlDB.Exec(t, "BACKUP DATABASE data INTO 'nodelocal://1/backup'")
+		var restoreJobID jobspb.JobID
+		sqlDB.QueryRow(
+			t,
+			`RESTORE DATABASE data FROM LATEST IN 'nodelocal://1/backup'
+			WITH detached, new_db_name = 'restored_data'`,
+		).Scan(&restoreJobID)
+
+		testutils.SucceedsSoon(t, func() error {
+			jobProgress := jobutils.GetJobProgress(t, sqlDB, restoreJobID)
+			if len(jobProgress.GetRestore().Checkpoint) == 0 {
+				return errors.Newf("frontier has not advanced yet")
+			}
+			return nil
+		})
+		close(waitForProgress)
+		jobutils.WaitForJobToHaveStatus(t, sqlDB, restoreJobID, endStatus)
+		mu.Lock()
+		defer mu.Unlock()
+		return mu.attemptCount
+	}
+
+	// If we were to stay with the initial retry policy the entire time, this
+	// would be the expected number of attempts made by the restore job.
+	var expTotalAttemptsFirstPolicy = maxRetries*2 + 2
+
+	t.Run("retry policy switches when enough progress is made", func(t *testing.T) {
+		attempts := runRestoreAndTrackAttempts(t, 0.01 /* policySwitchThreshold */, jobs.StatePaused)
+		// If we successfully switched retry policies, the job should have performed
+		// more attempts than	permitted by the initial retry policy. We multiply by 2
+		// to ensure that this isn't caused by the reset of the initial retry policy.
+		require.Greater(t, attempts, expTotalAttemptsFirstPolicy)
+	})
+
+	t.Run("retry policy does not switch if insufficient progress is made", func(t *testing.T) {
+		// Set an impossibly high threshold so that the restore job never
+		// sufficiently makes enough progress to switch retry policies.
+		attempts := runRestoreAndTrackAttempts(t, 1.5 /* policySwitchThreshold */, jobs.StateFailed)
+		// Since we do allow progress to be made, we expect the restore job to reset
+		// the retry loop, but retry policy will stay the same.
+		require.Equal(t, expTotalAttemptsFirstPolicy, attempts)
+	})
 }
