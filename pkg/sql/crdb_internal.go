83f12d9c6dbe615d680f54d870050a7df93514b7
Yahor Yuzefovich <yahor@cockroachlabs.com>
2025-07-03 16:06:07 -0700

    sql: include txn retry info into SHOW QUERIES
    This commit attempts to make txn retry information more visible. We
    already expose the number of times the txn is retried via `num_retries`
    and `num_auto_retries` columns of `{cluster,node}_transactions` virtual
    tables. However, at least for me personally, SHOW QUERIES is often the
    first entry point to understand what's happening on the cluster right
    now, and I don't always remember that there might retries going on when
    a given query has been running for a long time. To make that jump
    quicker we now include `num_txn_retries` and `num_txn_auto_retries` into
    the output of SHOW QUERIES (as well as the virtual table that it's
    powered by).
    
    Note that this might be a source of confusion in multi-stmt transactions
    because for each active query we'll print the number of txn retries
    regardless of which stmt encountered the retry. Still, it seems better
    than having to remember to join against the transactions virtual table
    on the session ID, plus we avoid possible fanout since we already have
    the necessary information in protos when populating the active queries.
    
    Also note that I believe we don't need to do anything special for the
    mixed version state since the virtual table scans prohibits the query
    distribution, meaning that if new columns are requested for SHOW
    QUERIES, then we're on a new binary and it knows how to populate them
    (even if the proto response came from the older node).
    
    Release note (sql change): New columns `num_txn_retries` and
    `num_txn_auto_retries` are included into
    `crdb_internal.{cluster,node}_queries` virtual tables as well as output
    of SHOW QUERIES. These columns, when not NULL, have the same information
    as `num_retries` and `num_auto_retries` columns of
    `crdb_internal.{cluster,node}_transactions` virtual tables for the same
    transaction in which the active query is executed.

--- pkg/sql/crdb_internal.go
+++ pkg/sql/crdb_internal.go
@@ -2367,20 +2367,22 @@ func populateTransactionsTable(
 
 const queriesSchemaPattern = `
 CREATE TABLE crdb_internal.%s (
-  query_id         STRING,         -- the cluster-unique ID of the query
-  txn_id           UUID,           -- the unique ID of the query's transaction
-  node_id          INT NOT NULL,   -- the node on which the query is running
-  session_id       STRING,         -- the ID of the session
-  user_name        STRING,         -- the user running the query
-  start            TIMESTAMPTZ,    -- the start time of the query
-  query            STRING,         -- the SQL code of the query
-  client_address   STRING,         -- the address of the client that issued the query
-  application_name STRING,         -- the name of the application as per SET application_name
-  distributed      BOOL,           -- whether the query is running distributed
-  phase            STRING,         -- the current execution phase
-  full_scan        BOOL,           -- whether the query contains a full table or index scan
-  plan_gist        STRING,         -- Compressed logical plan.
-  database         STRING          -- the database the statement was executed on
+  query_id             STRING,         -- the cluster-unique ID of the query
+  txn_id               UUID,           -- the unique ID of the query's transaction
+  node_id              INT NOT NULL,   -- the node on which the query is running
+  session_id           STRING,         -- the ID of the session
+  user_name            STRING,         -- the user running the query
+  start                TIMESTAMPTZ,    -- the start time of the query
+  query                STRING,         -- the SQL code of the query
+  client_address       STRING,         -- the address of the client that issued the query
+  application_name     STRING,         -- the name of the application as per SET application_name
+  distributed          BOOL,           -- whether the query is running distributed
+  phase                STRING,         -- the current execution phase
+  full_scan            BOOL,           -- whether the query contains a full table or index scan
+  plan_gist            STRING,         -- Compressed logical plan.
+  database             STRING,         -- the database the statement was executed on
+  num_txn_retries      INT,            -- number of txn retries
+  num_txn_auto_retries INT             -- number of auto retries via SQL conn executor
 )`
 
 func (p *planner) makeSessionsRequest(
@@ -2498,6 +2500,11 @@ func populateQueriesTable(
 			continue
 		}
 		sessionID := getSessionID(session)
+		numTxnRetries, numTxnAutoRetries := tree.DNull, tree.DNull
+		if txn := session.ActiveTxn; txn != nil {
+			numTxnRetries = tree.NewDInt(tree.DInt(txn.NumRetries))
+			numTxnAutoRetries = tree.NewDInt(tree.DInt(txn.NumAutoRetries))
+		}
 		for _, query := range session.ActiveQueries {
 			isDistributedDatum := tree.DNull
 			isFullScanDatum := tree.DNull
@@ -2516,16 +2523,6 @@ func populateQueriesTable(
 
 			if query.Progress > 0 {
 				phase = fmt.Sprintf("%s (%.2f%%)", phase, query.Progress*100)
-			}
-
-			var txnID tree.Datum
-			// query.TxnID and query.TxnStart were only added in 20.1. In case this
-			// is a mixed cluster setting, report NULL if these values were not filled
-			// out by the remote session.
-			if query.ID == "" {
-				txnID = tree.DNull
-			} else {
-				txnID = tree.NewDUuid(tree.DUuid{UUID: query.TxnID})
 			}
 
 			ts, err := tree.MakeDTimestampTZ(query.Start, time.Microsecond)
@@ -2547,7 +2544,7 @@ func populateQueriesTable(
 			}
 			if err := addRow(
 				tree.NewDString(query.ID),
-				txnID,
+				tree.NewDUuid(tree.DUuid{UUID: query.TxnID}),
 				tree.NewDInt(tree.DInt(session.NodeID)),
 				sessionID,
 				tree.NewDString(session.Username),
@@ -2560,6 +2557,8 @@ func populateQueriesTable(
 				isFullScanDatum,
 				planGistDatum,
 				tree.NewDString(query.Database),
+				numTxnRetries,
+				numTxnAutoRetries,
 			); err != nil {
 				return err
 			}
