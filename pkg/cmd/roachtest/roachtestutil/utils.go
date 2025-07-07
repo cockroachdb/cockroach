DarrylWong/fork

roachprod: create node stability helpers
In roachtests, a common pattern is to restart or take down nodes
and wait until ranges are upreplicated or until SQL is available. With
the introduction of the failure injection library, we now see a need
for roachprod to do the same.

This change extracts helpers used among roachtest and failure injection
to monitor the cluster for stability after a restart and moves them to
a shared location.

pkg/cmd/roachtest/roachtestutil/utils.go
@@ -20,11 +20,11 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/cluster"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/option"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/test"
+	"github.com/cockroachdb/cockroach/pkg/roachprod"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/config"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/logger"
 	"github.com/cockroachdb/cockroach/pkg/util"
-	"github.com/cockroachdb/cockroach/pkg/util/retry"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/require"
@@ -77,11 +77,7 @@ func (e *EveryN) ShouldLog() bool {
 // asynchronously at server startup.
 // (See "Root Cause" in https://github.com/cockroachdb/cockroach/issues/137988)
 func WaitForSQLReady(ctx context.Context, db *gosql.DB) error {
-	retryOpts := retry.Options{MaxRetries: 5}
-	return retryOpts.Do(ctx, func(ctx context.Context) error {
-		_, err := db.ExecContext(ctx, "SELECT 1")
-		return err
-	})
+	return roachprod.WaitForSQLReady(ctx, db, roachprod.WithMaxRetries(5))
 }
 
 // WaitForReady waits until the given nodes report ready via health checks.

cockroachdb/cockroach

pkg/cmd/roachtest/roachtestutil/utils.go
@@ -20,11 +20,11 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/cluster"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/option"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/test"
+	"github.com/cockroachdb/cockroach/pkg/roachprod"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/config"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/logger"
 	"github.com/cockroachdb/cockroach/pkg/util"
-	"github.com/cockroachdb/cockroach/pkg/util/retry"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/require"
@@ -77,11 +77,7 @@ func (e *EveryN) ShouldLog() bool {
 // asynchronously at server startup.
 // (See "Root Cause" in https://github.com/cockroachdb/cockroach/issues/137988)
 func WaitForSQLReady(ctx context.Context, db *gosql.DB) error {
-	retryOpts := retry.Options{MaxRetries: 5}
-	return retryOpts.Do(ctx, func(ctx context.Context) error {
-		_, err := db.ExecContext(ctx, "SELECT 1")
-		return err
-	})
+	return roachprod.WaitForSQLReady(ctx, db, install.WithMaxRetries(5))
 }
 
 // WaitForReady waits until the given nodes report ready via health checks.