97a5a0cdbee95aea52c5e18505cb7f7f2d9d9814
Annie Pompa <annie@cockroachlabs.com>
2025-07-07 14:08:02 -0400

    schemachanger: generate deprules for 25.4
    **Checklist**:
    
    - [x] Update the `scplan` rules in `pkg/sql/schemachanger/scplan/internal/rules`:
      - copy the contents of `current` into a new release directory for the previous version
        (e.g. `release_24_1`)
      - change package name in all files (including `BUILD.bazel`), and update the
        version in `current/helpers.go`
      - update `rulesForReleases` in `scplan/plan.go`
      - rewrite the test outputs: `./dev test pkg/sql/schemachanger/scplan/internal/rules/... --rewrite`
      - rewrite `TestDeclarativeRules` output: `./dev test pkg/cli -f DeclarativeRules --rewrite`
    
    Epic: REL-2733
    Release note: None

---
+++ pkg/sql/schemachanger/scplan/internal/rules/release_25_3/dep_create_policy.go
@@ -0,0 +1,54 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package release_25_3
+
+import (
+	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/rel"
+	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scpb"
+	. "github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scplan/internal/rules"
+	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scplan/internal/scgraph"
+)
+
+func init() {
+	registerDepRule(
+		"dependents added after policy",
+		scgraph.SameStagePrecedence,
+		"policy", "dependent",
+		func(from, to NodeVars) rel.Clauses {
+			return rel.Clauses{
+				from.Type((*scpb.Policy)(nil)),
+				to.TypeFilter(rulesVersionKey, isPolicyDependent),
+				JoinOnPolicyID(from, to, "table-id", "policy-id"),
+				StatusesToPublicOrTransient(from, scpb.Status_PUBLIC, to, scpb.Status_PUBLIC),
+			}
+		},
+	)
+}
+
+func init() {
+	// This rule ensures that when a policy dependent is being swapped are
+	// done in the correct order. The dropped element should disappear before
+	// the replacement element is added.
+	registerDepRule(
+		"policy dependents are swapped in order",
+		scgraph.Precedence,
+		"drop-policy-dependent", "add-policy-dependent",
+		func(from, to NodeVars) rel.Clauses {
+			return rel.Clauses{
+				from.TypeFilter(rulesVersionKey, isPolicyDependent),
+				to.TypeFilter(rulesVersionKey, isPolicyDependent),
+				// Confirm we are joining on the same types.
+				from.El.AttrEqVar(rel.Type, "sameType"),
+				to.El.AttrEqVar(rel.Type, "sameType"),
+				JoinOnPolicyID(from, to, "table-id", "policy-id"),
+				from.TargetStatus(scpb.ToAbsent),
+				from.CurrentStatus(scpb.Status_ABSENT),
+				to.TargetStatus(scpb.ToPublic, scpb.TransientAbsent),
+				to.CurrentStatus(scpb.Status_PUBLIC),
+			}
+		},
+	)
+}
