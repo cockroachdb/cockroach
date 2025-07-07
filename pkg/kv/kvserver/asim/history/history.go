d49e53b3594b7a4d30f9efb2656e2b44ef99eca0
Tobias Grieger <tobias.b.grieger@gmail.com>
2025-07-07 12:33:47 +0200

    asim: add replica_placement, min_key, max_key
    and make other changes that were required to port the
    `load_distribution_movement_disabled` datadriven file from #149531. Not
    everything in that file works yet; TODOs have been added.
    
    Co-authored-by: wenyihu6 <wenyi@cockroachlabs.com>

--- pkg/kv/kvserver/asim/history/history.go
+++ pkg/kv/kvserver/asim/history/history.go
@@ -7,9 +7,13 @@ package history
 
 import (
 	"context"
+	"fmt"
+	"sort"
+	"strings"
 
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/metrics"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/state"
+	"github.com/montanaflynn/stats"
 )
 
 // History contains recorded information that summarizes a simulation run.
@@ -23,4 +27,75 @@ type History struct {
 // Listen implements the metrics.StoreMetricListener interface.
 func (h *History) Listen(ctx context.Context, sms []metrics.StoreMetrics) {
 	h.Recorded = append(h.Recorded, sms)
+}
+
+func (h *History) ShowRecordedValueAt(idx int, stat string) string {
+	var buf strings.Builder
+	type storeIDWithStat struct {
+		StoreID int64
+		Value   float64
+	}
+	tick := h.Recorded[idx]
+	orderedStoreIDs := make([]storeIDWithStat, 0, len(tick))
+	values := make([]float64, 0, len(tick))
+	for _, sm := range tick {
+		var value float64
+		switch stat {
+		case "qps":
+			value = float64(sm.QPS)
+		// case "cpu":
+		// 	value = float64(sm.CPU)
+		// case "write_bytes_per_second":
+		// 	value = float64(sm.WriteBytesPerSecond)
+		case "write":
+			value = float64(sm.WriteKeys)
+		case "write_b":
+			value = float64(sm.WriteBytes)
+		case "read":
+			value = float64(sm.ReadKeys)
+		case "read_b":
+			value = float64(sm.ReadBytes)
+		case "replicas":
+			value = float64(sm.Replicas)
+		case "leases":
+			value = float64(sm.Leases)
+		case "lease_moves":
+			value = float64(sm.LeaseTransfers)
+		case "replica_moves":
+			value = float64(sm.Rebalances)
+		case "replica_b_rcvd":
+			value = float64(sm.RebalanceRcvdBytes)
+		case "replica_b_sent":
+			value = float64(sm.RebalanceSentBytes)
+		case "range_splits":
+			value = float64(sm.RangeSplits)
+		case "disk_fraction_used":
+			value = sm.DiskFractionUsed
+		default:
+			return "<unrecognized stat>"
+		}
+		orderedStoreIDs = append(orderedStoreIDs, storeIDWithStat{
+			StoreID: sm.StoreID,
+			Value:   value,
+		})
+		values = append(values, value)
+	}
+	sort.Slice(orderedStoreIDs, func(i, j int) bool {
+		return orderedStoreIDs[i].StoreID < orderedStoreIDs[j].StoreID
+	})
+	_, _ = fmt.Fprintf(&buf, "[")
+	for i, store := range orderedStoreIDs {
+		if i > 0 {
+			_, _ = fmt.Fprintf(&buf, " ")
+		}
+		if stat == "disk_fraction_used" {
+			_, _ = fmt.Fprintf(&buf, "s%v=%.2f", store.StoreID, store.Value)
+		} else {
+			_, _ = fmt.Fprintf(&buf, "s%v=%.0f", store.StoreID, store.Value)
+		}
+	}
+	_, _ = fmt.Fprintf(&buf, "]")
+	stddev, _ := stats.StandardDeviation(values)
+	_, _ = fmt.Fprintf(&buf, " (stddev=%.2f)", stddev)
+	return buf.String()
 }
