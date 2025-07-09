6285deb430f54a42d3dbc01ba23b7c4d1c1de2a5
Matt White <matt.white@cockroachlabs.com>
2025-07-09 12:00:18 -0700

    sql: disable vector index backfill in the legacy schema changer
    Through an oversight, the legacy schema changer never received a copy of
    the vector manager, required for backfilling vector indexes. In
    practice, we always use the declarative schema changer for creating
    vector indexes, so this wasn't caught in QA. Sentry caught the
    oversight.
    
    Completely fixing this problem is more complicated than what we have
    time to do in 25.3, so for now the plan is to disable backfilling vector
    indices with the legacy schema changer, which should be a corner case to
    begin with.
    
    Informs: #149236
    Release note (bug fix): Attempting to create a vector index with the
    legacy schema changer will now fail gracefully instead of crashing the
    node.

--- pkg/sql/backfill/backfill.go
+++ pkg/sql/backfill/backfill.go
@@ -40,6 +40,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/vecindex/vecencoding"
 	"github.com/cockroachdb/cockroach/pkg/sql/vecindex/vecstore"
 	"github.com/cockroachdb/cockroach/pkg/sql/vecindex/vecstore/vecstorepb"
+	"github.com/cockroachdb/cockroach/pkg/util/errorutil/unimplemented"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/mon"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
@@ -605,6 +606,7 @@ func (ib *IndexBackfiller) InitForLocalUse(
 	semaCtx *tree.SemaContext,
 	desc catalog.TableDescriptor,
 	mon *mon.BytesMonitor,
+	vecIndexManager *vecindex.Manager,
 ) (retErr error) {
 	if mon == nil {
 		return errors.AssertionFailedf("memory monitor must be provided")
@@ -616,7 +618,8 @@ func (ib *IndexBackfiller) InitForLocalUse(
 	}()
 
 	// Initialize ib.added.
-	if err := ib.initIndexes(ctx, evalCtx, desc, nil /* allowList */, 0 /*sourceIndex*/, nil); err != nil {
+	// TODO(150163): Pass vecIndexManager once vector index build is supported with the legacy schema changer.
+	if err := ib.initIndexes(ctx, evalCtx, desc, nil /* allowList */, 0 /*sourceIndex*/, nil /*vecIndexManager*/); err != nil {
 		return err
 	}
 
@@ -919,6 +922,13 @@ func (ib *IndexBackfiller) initIndexes(
 		if idx.GetType() != idxtype.VECTOR {
 			ib.VectorOnly = false
 			continue
+		}
+
+		if vecIndexManager == nil {
+			return unimplemented.NewWithIssue(
+				150163,
+				"vector index build not supported with the legacy schema changer",
+			)
 		}
 
 		if ib.VectorIndexes == nil {
