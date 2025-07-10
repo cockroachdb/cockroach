54ce3dca09ab6a27b4ddd846bf29bec6c5c8ee5e
Kevin Cao <kevin.cao@cockroachlabs.com>
2025-07-10 14:03:02 -0400

    backup: reset retry counter for download job when progress is detected
    In #1448821, we added a retry loop to the download phase of online
    restore, but the retry policy allows at most 5 retries. In the event of
    download progress, we want to be more lenient with the retry policy, so
    this commit teaches the download phase to reset the retry counter if
    progress is detected.
    
    Epic: CRDB-51394
    
    Fixes: #149893

--- pkg/backup/restore_job.go
+++ pkg/backup/restore_job.go
@@ -672,10 +672,11 @@ func loadBackupSQLDescs(
 type restoreResumer struct {
 	job *jobs.Job
 
-	settings      *cluster.Settings
-	execCfg       *sql.ExecutorConfig
-	restoreStats  roachpb.RowCount
-	downloadJobID jobspb.JobID
+	settings        *cluster.Settings
+	execCfg         *sql.ExecutorConfig
+	restoreStats    roachpb.RowCount
+	downloadJobID   jobspb.JobID
+	downloadJobProg float32
 
 	mu struct {
 		syncutil.Mutex
