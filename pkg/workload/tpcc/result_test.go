7c99d8f95169d6316934604c45264b3f3092f31d
Mira Radeva <mira@cockroachlabs.com>
2025-07-14 11:10:53 -0400

    roachtest: actually fail TPCC bench if reached max warehouses
    In 1bfe55b, we attempted to fail the TPCC bench test run if the
    configured maximum warehouses were reached and the success criteria
    were met. The idea was that this would prompt us to increase the max
    warehouses. However, that commit failed only the specific line search
    run, not the full test run.
    
    This commit moves the max warehouses check out of the result handling
    and actually fatals the test.
    
    Part of: #148235
    
    Release note: None

--- pkg/workload/tpcc/result_test.go
+++ pkg/workload/tpcc/result_test.go
@@ -15,7 +15,7 @@ func TestNewResult(t *testing.T) {
 	// Ensure you don't get panics when calling common methods
 	// on a trivial Result that doesn't have any data attached.
 	res := NewResult(1000, 0, 0, nil)
-	require.Error(t, res.FailureError(0))
+	require.Error(t, res.FailureError())
 	require.Zero(t, res.Efficiency())
 	require.Zero(t, res.TpmC())
 }
