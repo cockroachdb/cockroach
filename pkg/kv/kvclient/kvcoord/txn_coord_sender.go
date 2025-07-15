a2f57d7505d49bbed574a7ada2cae3042f000f2b
Steven Danna <danna@cockroachlabs.com>
2025-07-15 22:20:05 +0100

    kvcoord: disallow pipelining while write buffering is enabled
    The interaction between pipelining and write buffering has revealed a
    couple of subtle bugs. Further, these two optimisations are largely
    overlapping. In the happy case, when write buffering is enabled, we
    expect almost not write pipelining.
    
    Here, we disable pipelining when write buffering is enabled.
    
    The upside of this is:
    
    1. It avoids other possible bugs in the interaction between these
       features.
    
    2. It makes it a bit easier to reason about the behaviour of
       write-buffered transactions.
    
    The downsides include:
    
    1. This results in a negative performance impact for users who
       turn on write buffering but whose transactions often result in
       buffering later being disabled.
    
    2. Write pipelining has been the default for many releases and it is
       not clear that running with write pipelining disabled is as well
       tested as the enabled code path.
    
    Fixes #149911
    Epic: none
    Release note: None

--- pkg/kv/kvclient/kvcoord/txn_coord_sender.go
+++ pkg/kv/kvclient/kvcoord/txn_coord_sender.go
@@ -1192,6 +1192,9 @@ func (tc *TxnCoordSender) SetBufferedWritesEnabled(enabled bool) {
 		panic("cannot enable buffered writes on a running transaction")
 	}
 	tc.interceptorAlloc.txnWriteBuffer.setEnabled(enabled)
+	if enabled {
+		tc.interceptorAlloc.txnPipeliner.disabled = true
+	}
 }
 
 // BufferedWritesEnabled is part of the kv.TxnSender interface.
