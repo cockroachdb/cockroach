bb165e40e65429215d7cfb7a65c3f14d8a11e671
Andy Yang <yang@cockroachlabs.com>
2025-07-16 13:22:29 -0400

    fixup! fixup! enhance testing more

--- pkg/ccl/changefeedccl/protected_timestamps_test.go
+++ pkg/ccl/changefeedccl/protected_timestamps_test.go
@@ -504,9 +504,10 @@ func TestChangefeedCanceledWhenPTSIsOld(t *testing.T) {
 					}()
 
 					jobFeed := feed.(cdctest.EnterpriseTestFeed)
-					require.NoError(t, jobFeed.Pause())
 
 					if initialProtect != "none" {
+						require.NoError(t, jobFeed.Pause())
+
 						// Wait a little bit and make sure the job ISN'T canceled.
 						require.ErrorContains(t, jobFeed.WaitDurationForState(10*time.Second, func(s jobs.State) bool {
 							return s == jobs.StateCanceled
