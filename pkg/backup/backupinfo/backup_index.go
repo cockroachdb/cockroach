8c37b3f66c659134ab3661cc90f0026bb9f2ec81
Kevin Cao <kevin.cao@cockroachlabs.com>
2025-07-16 22:04:04 -0400

    backup: create backup index file on backup completion
    This commit teaches backup to write a backup index file upon completion
    of the backup as outlined in the [design
    doc](https://docs.google.com/document/d/1zXGQ7c9jvKmatO64jVKNrO5BBODAE38y-TqNTeb81yA).
    
    Epic: CRDB-47942
    
    Fixes: #150326
    
    Release note: None

---
+++ pkg/backup/backupinfo/backup_index.go
@@ -0,0 +1,107 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package backupinfo
+
+import (
+	"bytes"
+	"context"
+	"encoding/hex"
+	"fmt"
+	"strings"
+
+	"github.com/cockroachdb/cockroach/pkg/backup/backupbase"
+	"github.com/cockroachdb/cockroach/pkg/backup/backuppb"
+	"github.com/cockroachdb/cockroach/pkg/backup/backuputils"
+	"github.com/cockroachdb/cockroach/pkg/cloud"
+	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
+	"github.com/cockroachdb/cockroach/pkg/security/username"
+	"github.com/cockroachdb/cockroach/pkg/util/encoding"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/cockroach/pkg/util/protoutil"
+	"github.com/cockroachdb/cockroach/pkg/util/tracing"
+	"github.com/cockroachdb/errors"
+)
+
+// WriteBackupIndexMetadata writes an index file for the backup described by the
+// job details. The provided ExternalStorage needs to be rooted at the specific
+// directory that the index file should be written to.
+func WriteBackupIndexMetadata(
+	ctx context.Context,
+	user username.SQLUsername,
+	makeExternalStorageFromURI cloud.ExternalStorageFromURIFactory,
+	details jobspb.BackupDetails,
+) error {
+	ctx, sp := tracing.ChildSpan(ctx, "backupinfo.WriteBackupIndexMetadata")
+	defer sp.Finish()
+
+	if details.EndTime.IsEmpty() {
+		return errors.AssertionFailedf("end time must be set in backup details")
+	}
+	if details.Destination.Exists && details.StartTime.IsEmpty() {
+		return errors.AssertionFailedf("incremental backup details missing a start time")
+	}
+
+	metadata := &backuppb.BackupIndexMetadata{
+		StartTime: details.StartTime,
+		EndTime:   details.EndTime,
+	}
+	metadataBytes, err := protoutil.Marshal(metadata)
+	if err != nil {
+		return errors.Wrapf(err, "failed to marshal backup index metadata")
+	}
+
+	indexStore, err := makeExternalStorageFromURI(
+		ctx, details.CollectionURI, user,
+	)
+	if err != nil {
+		return errors.Wrapf(err, "creating external storage")
+	}
+
+	indexFilePath, err := getBackupIndexFilePath(
+		details.Destination.Subdir,
+		details.StartTime,
+		details.EndTime,
+	)
+	if err != nil {
+		return errors.Wrapf(err, "getting index file path")
+	}
+
+	return cloud.WriteFile(
+		ctx, indexStore, indexFilePath, bytes.NewReader(metadataBytes),
+	)
+}
+
+// getBackupIndexFilePath returns the path to the backup index file representing
+// a backup that starts and ends at the given timestamps, including
+// the filename and extension. The path is relative to the collection URI.
+func getBackupIndexFilePath(subdir string, startTime, endTime hlc.Timestamp) (string, error) {
+	if strings.EqualFold(subdir, backupbase.LatestFileName) {
+		return "", errors.AssertionFailedf("expected subdir to be resolved and not be 'LATEST'")
+	}
+	return backuputils.JoinURLPath(
+		backupbase.BackupIndexDirectoryPath,
+		subdir,
+		getBackupIndexFileName(startTime, endTime),
+	), nil
+}
+
+// getBackupIndexFilename generates the filename (including the extension) for a
+// backup index file that represents a backup that starts ad ends at the given
+// timestamps.
+func getBackupIndexFileName(startTime, endTime hlc.Timestamp) string {
+	var buffer []byte
+	buffer = encoding.EncodeStringDescending(buffer, endTime.GoTime().String())
+	descEndTs := hex.EncodeToString(buffer)
+	formattedStartTime := startTime.GoTime().Format(backupbase.BackupIndexFilenameTimestampFormat)
+	if startTime.IsEmpty() {
+		formattedStartTime = "0" // Use a placeholder for empty start time.
+	}
+	formattedEndTime := endTime.GoTime().Format(backupbase.BackupIndexFilenameTimestampFormat)
+	return fmt.Sprintf(
+		"backup_%s_%s_%s_metadata.pb",
+		descEndTs, formattedStartTime, formattedEndTime,
+	)
+}
