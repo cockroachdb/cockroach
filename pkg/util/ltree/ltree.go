66cfa48a7ac8063a14bc791598bb9e899e3029f2
Paul Niziolek <paul.niziolek@cockroachlabs.com>
2025-07-22 00:53:35 -0400

    builtins: add implementations for all ltree functions
    Informs: cockroachdb#44657
    Epic: CRDB-148
    
    Release note (sql change): All builtin functions for LTREE from Postgres are
    now supported. Namely, `subltree()`, `subpath()`, `nlevel()`, `index()`,
    `text2ltree()`, `ltree2text()`, and `lca()`. The details for each of these are
    documented on the Postgres docs:
    https://www.postgresql.org/docs/9.1/ltree.html#LTREE-FUNC-TABLE. Additionally,
    the `lca()` function in Postgres specifically limits up to 8 LTREE args, while
    the CRDB `lca()` function accepts any variable number of ltree args.

--- pkg/util/ltree/ltree.go
+++ pkg/util/ltree/ltree.go
@@ -106,14 +106,6 @@ func (lt T) ForEachLabel(fn func(int, string)) {
 	}
 }
 
-// LabelAt returns the label at the specified index in an LTree path.
-func (lt T) LabelAt(idx int) (string, error) {
-	if idx < 0 || idx >= lt.Len() {
-		return "", pgerror.Newf(pgcode.InvalidParameterValue, "index %d out of bounds", idx)
-	}
-	return lt.path[idx], nil
-}
-
 // Compare compares two LTrees lexicographically based on their labels.
 func (lt T) Compare(other T) int {
 	minLen := min(lt.Len(), other.Len())
@@ -168,4 +160,77 @@ func validateLabel(l string) error {
 // isValidChar returns true if the character is valid in an LTree label.
 func isValidChar(c byte) bool {
 	return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_') || (c == '-')
+}
+
+// SubPath returns a sub-path of the LTree starting from the given offset
+// and of the specified length. If the offset is negative, it counts from the end of the path.
+// If the length is negative, it extends to the end of the path from the offset.
+func (lt T) SubPath(offset, length int) (T, error) {
+	start := offset
+	if start < 0 {
+		start += lt.Len()
+	}
+	if length < 0 {
+		length += lt.Len() - start
+	}
+	if start < 0 || start >= lt.Len() || length < 0 {
+		return Empty, pgerror.Newf(pgcode.InvalidParameterValue, "invalid positions")
+	}
+
+	end := min(start+length, lt.Len())
+	return T{
+		path: lt.path[start:end:end],
+	}, nil
+}
+
+// IndexOf returns the first index in l that matches the other l sub-ltree,
+// starting from offset. If offset is negative, it counts from the end of the ltree.
+// If the sub-ltree is not found, it returns -1.
+func (lt T) IndexOf(other T, offset int) int {
+	// TODO(paulniziolek): We could optimize this by using a more efficient search algorithm
+	start := offset
+	if start < 0 {
+		start += lt.Len()
+	}
+	// The max() is necessary here, as start could still be negative even after
+	// adding the length to `offset` since offset isn't restricted.
+	for i := max(start, 0); i < lt.Len()-other.Len()+1; i++ {
+		match := true
+		for j := 0; j < other.Len(); j++ {
+			if lt.path[i+j] != other.path[j] {
+				match = false
+				break
+			}
+		}
+		if match {
+			return i
+		}
+	}
+	return -1
+}
+
+// LCA computes the Lowest Common Ancestor of a non-empty slice of LTree instances.
+// The LCA is the longest common prefix of the paths in the provided LTree slice.
+// If the paths have no common prefix, it returns an empty LTree.
+func LCA(ltrees []T) T {
+	if len(ltrees) == 0 {
+		return T{}
+	}
+	if len(ltrees) == 1 {
+		return ltrees[0]
+	}
+	var minLength = ltrees[0].Len()
+	for _, ltree := range ltrees {
+		minLength = min(minLength, ltree.Len())
+	}
+	var i int
+	for i = 0; i < minLength; i++ {
+		label := ltrees[0].path[i]
+		for _, t := range ltrees[1:] {
+			if t.path[i] != label {
+				break
+			}
+		}
+	}
+	return T{path: ltrees[0].path[:i:i]}
 }
