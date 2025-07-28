e23a05369661126b7b6cc2c559f4b40079d5aaab
Tobias Grieger <tobias.b.grieger@gmail.com>
2025-07-28 11:18:35 +0200

    asim: investigate charts using a single-page app
    I'm realizing the pngs will be too churny and also that it's much nicer to be
    able to interactively compare experiments.
    
    The SPA (single page app) in this commit was created in collaboration with
    Claude Code.

--- pkg/kv/kvserver/asim/tests/plotting_test.go
+++ pkg/kv/kvserver/asim/tests/plotting_test.go
@@ -7,12 +7,15 @@ package tests
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"hash"
 	"os"
 	"path/filepath"
 	"sort"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/history"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/metrics"
@@ -61,12 +64,14 @@ func generatePlot(t *testing.T, stat string, sl [][]float64) []byte {
 // Returns a slice of filenames for all generated plots.
 func generateAllPlots(
 	t *testing.T,
+	buf *strings.Builder,
 	h history.History,
 	testName string,
 	sample int,
 	outputDir string,
 	hasher hash.Hash,
 	rewrite bool,
+	tickInterval time.Duration,
 ) {
 	ts := metrics.MakeTS(h.Recorded)
 
@@ -77,13 +82,54 @@ func generateAllPlots(
 	}
 	sort.Strings(statNames)
 
+	// JSON format:
+	//
+	// tickInterval: <nanos>
+	// metrics:
+	//   some_metric:
+	//     s1: [0, 1, 2, ...]
+	jsonData := map[string]map[string][]float64{}
 	for _, stat := range statNames {
 		hasher.Write([]byte(fmt.Sprintf("%v", ts[stat])))
 		if rewrite {
 			_ = os.MkdirAll(outputDir, 0755)
-			path := filepath.Join(outputDir, fmt.Sprintf("%s_%d_%s.png", testName, sample, stat))
-			b := generatePlot(t, stat, ts[stat])
-			require.NoError(t, os.WriteFile(path, b, 0644))
+			if false {
+				// TODO(tbg): across the test suite, plots currently add up
+				// to >20mb, which is too much to store in git, especially
+				// given the frequent churn as allocator logic or the testing
+				// changes just slightly.
+				path := filepath.Join(outputDir, fmt.Sprintf("%s_%d_%s.png", testName, sample, stat))
+				b := generatePlot(t, stat, ts[stat])
+				require.NoError(t, os.WriteFile(path, b, 0644))
+			}
+
+			m := map[string][]float64{}
+			for storeID, vals := range ts[stat] {
+				m[fmt.Sprintf("s%d", storeID)] = vals
+			}
+			jsonData[stat] = m
 		}
+
+		// TODO(tbg): there are too many metrics to print here; they clutter up
+		// the datadriven output. We should rely on assertions instead.
+		if false {
+			at0, ok0 := h.ShowRecordedValueAt(0, stat)
+			s, ok := h.ShowRecordedValueAt(len(h.Recorded)-1, stat)
+			if ok0 {
+				_, _ = fmt.Fprintf(buf, "%s/%d: first: %s\n", stat, sample, at0)
+			}
+			if ok0 || ok {
+				_, _ = fmt.Fprintf(buf, "%s/%d: last:  %s\n", stat, sample, s)
+			}
+		}
+	}
+	if rewrite {
+		path := filepath.Join(outputDir, fmt.Sprintf("%s_%d.json", testName, sample))
+		b, err := json.Marshal(map[string]interface{}{
+			"tickInterval": tickInterval.Nanoseconds(),
+			"metrics":      jsonData,
+		})
+		require.NoError(t, err)
+		require.NoError(t, os.WriteFile(path, b, 0644))
 	}
 }
