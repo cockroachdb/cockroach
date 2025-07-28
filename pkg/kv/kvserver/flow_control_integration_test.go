8b25d278011557adb69ec1cdf29815211ae9dfda
Tobias Grieger <tobias.b.grieger@gmail.com>
2025-07-28 13:34:28 +0200

    kvserver: skip TestFlowControlAdmissionPostSplitMergeV2 under duress
    Closes #150840.
    
    Epic: none

--- pkg/kv/kvserver/flow_control_integration_test.go
+++ pkg/kv/kvserver/flow_control_integration_test.go
@@ -381,6 +381,7 @@ func TestFlowControlBlockedAdmissionV2(t *testing.T) {
 func TestFlowControlAdmissionPostSplitMergeV2(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
+	skip.UnderDuressWithIssue(t, 150840)
 	defer setRACv2DebugVModule(t)()
 
 	testutils.RunValues(t, "kvadmission.flow_control.mode", []kvflowcontrol.ModeT{
