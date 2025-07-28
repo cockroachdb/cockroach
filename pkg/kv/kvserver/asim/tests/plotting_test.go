b18169b84daa8f55848108472167148aa8e2e9f1
Tobias Grieger <tobias.b.grieger@gmail.com>
2025-07-28 12:52:28 +0200

    asim: remove plot
    Port all the tests and remove the plot command.

--- pkg/kv/kvserver/asim/tests/plotting_test.go
+++ pkg/kv/kvserver/asim/tests/plotting_test.go
@@ -6,7 +6,6 @@
 package tests
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"hash"
@@ -20,43 +19,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/history"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/asim/metrics"
 	"github.com/stretchr/testify/require"
-	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/plotutil"
-	"gonum.org/v1/plot/vg"
 )
-
-// generatePlot creates a single plot for the given stat from simulation history.
-// Hashes the plot file content directly to the provided hasher.
-// If rewrite is false, the plot is generated but not saved to disk.
-// Returns the filename or an error if plot generation fails.
-func generatePlot(t *testing.T, stat string, sl [][]float64) []byte {
-	p := plot.New()
-	p.Title.Text = stat
-	// TODO(tbg): convert to time.
-	p.X.Label.Text = "Ticks"
-	p.Y.Label.Text = stat
-
-	var plotArgs []interface{}
-	for storeIdx, storeTS := range sl {
-		storeID := storeIdx + 1
-		pts := make(plotter.XYs, len(storeTS))
-		for i, val := range storeTS {
-			pts[i].X = float64(i) // tick
-			pts[i].Y = val
-		}
-		plotArgs = append(plotArgs, fmt.Sprintf("s%d", storeID), pts)
-	}
-
-	err := plotutil.AddLinePoints(p, plotArgs...)
-	require.NoError(t, err)
-	wt, err := p.WriterTo(8*vg.Inch, 6*vg.Inch, "png")
-	require.NoError(t, err)
-	var buf bytes.Buffer
-	_, err = wt.WriteTo(&buf)
-	require.NoError(t, err)
-	return buf.Bytes()
-}
 
 // generateAllPlots creates plots for all available metrics from simulation history.
 // All plot files are hashed directly to the provided hasher.
@@ -64,6 +27,7 @@ func generatePlot(t *testing.T, stat string, sl [][]float64) []byte {
 // Returns a slice of filenames for all generated plots.
 //
 // TODO(tbg): introduce a SimulationEnv and make this a method on it.
+// TODO(tbg): rename to generateArtifacts.
 func generateAllPlots(
 	t *testing.T,
 	buf *strings.Builder,
@@ -95,17 +59,6 @@ func generateAllPlots(
 	for _, stat := range statNames {
 		hasher.Write([]byte(fmt.Sprintf("%v", ts[stat])))
 		if rewrite {
-			_ = os.MkdirAll(outputDir, 0755)
-			if false {
-				// TODO(tbg): across the test suite, plots currently add up
-				// to >20mb, which is too much to store in git, especially
-				// given the frequent churn as allocator logic or the testing
-				// changes just slightly.
-				path := filepath.Join(outputDir, fmt.Sprintf("%s_%d_%s.png", testName, sample, stat))
-				b := generatePlot(t, stat, ts[stat])
-				require.NoError(t, os.WriteFile(path, b, 0644))
-			}
-
 			m := map[string][]float64{}
 			for storeID, vals := range ts[stat] {
 				m[fmt.Sprintf("s%d", storeID)] = vals
@@ -127,6 +80,7 @@ func generateAllPlots(
 		}
 	}
 	if rewrite {
+		_ = os.MkdirAll(outputDir, 0755)
 		path := filepath.Join(outputDir, fmt.Sprintf("%s_%d.json", testName, sample))
 		b, err := json.Marshal(map[string]interface{}{
 			"tickInterval": tickInterval.Nanoseconds(),
