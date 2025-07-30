3e52bbcf9fd5b81d938a505442afecd12a6ff5da
Brendan Gerrity <brendan.gerrity@cockroachlabs.com>
2025-07-30 12:32:22 -0700

    sql: add sql grammar for inspect command
    The `INSPECT` commands are being added to support data consistency
    validation.
    These new statements require new SQL grammar.
    The grammar is added in this change and the implementations will be
    added in future PRs.
    
    Epic: CRDB-30356
    Part of: #148272
    
    Release note (sql change): Introduces the `INSPECT TABLE` and `INSPECT
    DATABASE` statements that are unimplemented. The new
    `enable_inspect_command` cluster setting feature flag configures access
    to the new features as they're implemented.

--- pkg/sql/sem/tree/stmt.go
+++ pkg/sql/sem/tree/stmt.go
@@ -1365,6 +1365,15 @@ func (*Import) StatementTag() string { return ImportTag }
 func (*Import) cclOnlyStatement() {}
 
 // StatementReturnType implements the Statement interface.
+func (*Inspect) StatementReturnType() StatementReturnType { return Rows }
+
+// StatementType implements the Statement interface.
+func (*Inspect) StatementType() StatementType { return TypeDML }
+
+// StatementTag returns a short string identifying the type of statement.
+func (*Inspect) StatementTag() string { return "INSPECT" }
+
+// StatementReturnType implements the Statement interface.
 func (*LiteralValuesClause) StatementReturnType() StatementReturnType { return Rows }
 
 // StatementType implements the Statement interface.
@@ -2598,6 +2607,7 @@ func (n *Grant) String() string                               { return AsString(
 func (n *GrantRole) String() string                           { return AsString(n) }
 func (n *MoveCursor) String() string                          { return AsString(n) }
 func (n *Insert) String() string                              { return AsString(n) }
+func (n *Inspect) String() string                             { return AsString(n) }
 func (n *Import) String() string                              { return AsString(n) }
 func (n *LiteralValuesClause) String() string                 { return AsString(n) }
 func (n *ParenSelect) String() string                         { return AsString(n) }
