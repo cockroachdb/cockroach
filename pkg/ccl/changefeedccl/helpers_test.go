9721c8abb19b272b81fea3bdf905d821a566a1c4
Miles Frankel <miles.frankel@cockroachlabs.com>
2025-08-01 12:45:38 -0400

    kv/rangefeed: filter and test internal rangefeed registry
    Filter and test internal rangefeed registry.
    
    Epic: None
    Release note: None

--- pkg/ccl/changefeedccl/helpers_test.go
+++ pkg/ccl/changefeedccl/helpers_test.go
@@ -1407,6 +1407,9 @@ func ChangefeedJobPermissionsTestSetup(t *testing.T, s TestServer) {
 		`GRANT CHANGEFEED ON table_a TO userWithSomeGrants`,
 
 		`CREATE USER regularUser`,
+
+		`CREATE USER viewClusterMetadataUser`,
+		`GRANT SYSTEM VIEWCLUSTERMETADATA TO viewClusterMetadataUser`,
 	)
 }
 
