597f7047887af3b0c8b9cf3c4a0d18069a1bae81
Paul Niziolek <paul.niziolek@cockroachlabs.com>
2025-08-04 16:52:06 -0400

    sql: add LTREE valueside encoding
    LTREE datums are valueside encoded by their byte representation including path
    separators.
    
    Informs: cockroachdb#44657
    Epic: CRDB-148
    
    Release note: None

---
+++ pkg/util/ltree/encoding.go
@@ -0,0 +1,31 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package ltree
+
+import "github.com/cockroachdb/cockroach/pkg/util/encoding"
+
+// EncodeLTree encodes a ltree into a serialized representation
+// that's suitable for on-disk storage.
+// This encoding is currently used for value-side encoding of LTREE columns.
+func EncodeLTree(appendTo []byte, l T) ([]byte, error) {
+	appendTo = encoding.EncodeUntaggedBytesValue(appendTo, encoding.UnsafeConvertStringToBytes(l.String()))
+	return appendTo, nil
+}
+
+// DecodeLTree decodes a ltree from the input byte slice.
+// See: EncodeLTree
+func DecodeLTree(b []byte) (T, error) {
+	var l T
+	_, pathBytes, err := encoding.DecodeUntaggedBytesValue(b)
+	if err != nil {
+		return l, err
+	}
+	l, err = ParseLTree(string(pathBytes))
+	if err != nil {
+		return l, err
+	}
+	return l, nil
+}
