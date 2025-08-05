b2acc18ee3b7f07f9356360c7e2a074272f08b78
Radu Berinde <radu@cockroachlabs.com>
2025-08-05 20:02:45 +0300

    storage: use latest supported table format when writing SSTs
    When CRDB is writing SSTs (for ingestion or transport), it is using a
    fixed table format version. We improve this to use the latest version
    guaranteed to be supported by all nodes in the cluster (based on the
    current cluster version).
    
    During upgrade, the Pebble format is bumped at fence versions (e.g.
    24.3-7) which guarantees that all nodes are using that version at the
    non-fence version (e.g. 24.3-8).
    
    Epic: none
    Release note: None

--- pkg/storage/pebble_test.go
+++ pkg/storage/pebble_test.go
@@ -1609,15 +1609,46 @@ func TestPebbleFormatVersion(t *testing.T) {
 	latestFmv := pebbleFormatVersionMap[latestKey]
 
 	require.Equal(t, pebbleFormatVersion(latestVersion), latestFmv)
+	require.Equal(t, minPebbleFormatVersionInCluster(latestVersion), latestFmv)
 
 	// We upgrade the pebble format as soon as we reach the fence version.
 	require.Equal(t, pebbleFormatVersion(latestVersion.FenceVersion()), latestFmv)
+	// But at the fence version, we don't have a guarantee that all nodes have
+	// upgraded.
+	require.Less(t, minPebbleFormatVersionInCluster(latestVersion.FenceVersion()), latestFmv)
 
 	require.Less(t, pebbleFormatVersion((latestKey - 1).Version()), latestFmv)
+	require.Less(t, minPebbleFormatVersionInCluster((latestKey - 1).Version()), latestFmv)
 
 	v := latestVersion
 	v.Minor++
 	require.Equal(t, pebbleFormatVersion(latestVersion), latestFmv)
+	require.Equal(t, minPebbleFormatVersionInCluster(latestVersion), latestFmv)
+
+	require.Equal(t, pebbleFormatVersion(clusterversion.MinSupported.Version()), MinimumSupportedFormatVersion)
+	require.Equal(t, minPebbleFormatVersionInCluster(clusterversion.MinSupported.Version()), MinimumSupportedFormatVersion)
+
+	// Gather all possible versions since MinSupported.
+	var versions []roachpb.Version
+	for k := clusterversion.MinSupported + 1; k <= clusterversion.Latest; k++ {
+		versions = append(versions, k.Version().FenceVersion(), k.Version())
+	}
+
+	prevFMV := MinimumSupportedFormatVersion
+	for i, v := range versions {
+		fmv := pebbleFormatVersion(v)
+		if fmv != prevFMV {
+			require.True(t, v.IsFence())
+			// minPebbleFormatVersionInCluster() should return the previous format.
+			require.Equal(t, prevFMV, minPebbleFormatVersionInCluster(v))
+			// For the next version, minPebbleFormatVersionInCluster() should return
+			// the new format.
+			require.Equal(t, fmv, minPebbleFormatVersionInCluster(versions[i+1]))
+		} else {
+			require.Equal(t, fmv, minPebbleFormatVersionInCluster(v))
+		}
+		prevFMV = fmv
+	}
 }
 
 // delayFS injects a delay on each read.
