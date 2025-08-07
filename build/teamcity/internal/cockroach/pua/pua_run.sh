d0be6afe8f9a3f2184f25edd32b4d96d60f57933
Shailendra Patel <shailendra.patel@cockroachlabs.com>
2025-08-07 11:19:42 +0530

    teamcity: add new CI job for PUA
    During each release, there is a need to run PUA.
    This new CI job allows triggering the PUA workflow on an as-needed
    basis for a specific release. The job generates a JSON file as output,
    which is consumed by the benchmarking portal to display the results.
    
    Epic: none
    
    Release note: None

---
+++ build/teamcity/internal/cockroach/pua/pua_run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Copyright 2025 The Cockroach Authors.
+#
+# Use of this software is governed by the CockroachDB Software License
+# included in the /LICENSE file.
+
+
+set -exuo pipefail
+
+dir="$(dirname $(dirname $(dirname $(dirname $(dirname "${0}")))))"
+
+source "$dir/teamcity-support.sh"  # For $root
+source "$dir/teamcity-bazel-support.sh"  # For run_bazel
+#
+BAZEL_SUPPORT_EXTRA_DOCKER_ARGS="-e LITERAL_ARTIFACTS_DIR=$root/artifacts -e GOOGLE_APPLICATION_CREDENTIALS_CONTENT -e GOOGLE_SERVICE_ACCOUNT" \
+			       run_bazel build/teamcity/internal/cockroach/pua/pua_run_impl.sh
