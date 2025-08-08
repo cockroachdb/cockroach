856a9642de95f9bc6f6934d14cdc59dde92f1132
Shailendra Patel <shailendra.patel@cockroachlabs.com>
2025-08-08 10:36:31 +0530

    drtprod: update PUA yaml configs
    This commits updates the PUA yaml configs to run via
    CI in cockroach-ephemeral project.
    
    Epic: none
    
    Release note: None

--- build/teamcity/internal/cockroach/pua/pua_run.sh
+++ build/teamcity/internal/cockroach/pua/pua_run.sh
@@ -5,7 +5,6 @@
 # Use of this software is governed by the CockroachDB Software License
 # included in the /LICENSE file.
 
-
 set -exuo pipefail
 
 dir="$(dirname $(dirname $(dirname $(dirname $(dirname "${0}")))))"
@@ -13,5 +12,5 @@ dir="$(dirname $(dirname $(dirname $(dirname $(dirname "${0}")))))"
 source "$dir/teamcity-support.sh"  # For $root
 source "$dir/teamcity-bazel-support.sh"  # For run_bazel
 #
-BAZEL_SUPPORT_EXTRA_DOCKER_ARGS="-e LITERAL_ARTIFACTS_DIR=$root/artifacts -e GOOGLE_APPLICATION_CREDENTIALS_CONTENT -e GOOGLE_SERVICE_ACCOUNT" \
+BAZEL_SUPPORT_EXTRA_DOCKER_ARGS="-e LITERAL_ARTIFACTS_DIR=$root/artifacts -e GOOGLE_APPLICATION_CREDENTIALS_CONTENT -e GOOGLE_SERVICE_ACCOUNT -e PUA_CONFIG -e CRDB_VERSION -e CRDB_UPGRADE_VERSION -e DD_API_KEY -e DD_APP_KEY" \
 			       run_bazel build/teamcity/internal/cockroach/pua/pua_run_impl.sh
