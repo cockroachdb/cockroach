9d9bce1866bbc1aade43f3f2be13cf32508b0afe
Michael Butler <butler@cockroachlabs.com>
2025-08-12 17:42:51 -0400

    roachtest: route schema workload flakes in pcr tests to foundations
    Informs #150548
    Informs #151645
    
    Release note: none

--- pkg/cmd/roachtest/tests/backup_restore_roundtrip.go
+++ pkg/cmd/roachtest/tests/backup_restore_roundtrip.go
@@ -45,6 +45,16 @@ var (
 	}
 )
 
+func handleSchemaChangeWorkloadError(err error) error {
+	// If the UNEXPECTED ERROR detail appears, the workload likely flaked.
+	// Otherwise, the workload could have failed due to other reasons like a node
+	// crash.
+	if err != nil && strings.Contains(errors.FlattenDetails(err), "UNEXPECTED ERROR") {
+		return registry.ErrorWithOwner(registry.OwnerSQLFoundations, errors.Wrapf(err, "schema change workload failed"))
+	}
+	return err
+}
+
 const numFullBackups = 3
 
 type roundTripSpecs struct {
@@ -261,18 +271,9 @@ func startBackgroundWorkloads(
 		return nil, err
 	}
 
-	handleChemaChangeError := func(err error) error {
-		// If the UNEXPECTED ERROR detail appears, the workload likely flaked.
-		// Otherwise, the workload could have failed due to other reasons like a node
-		// crash.
-		if err != nil && strings.Contains(errors.FlattenDetails(err), "UNEXPECTED ERROR") {
-			return registry.ErrorWithOwner(registry.OwnerSQLFoundations, errors.Wrapf(err, "schema change workload failed"))
-		}
-		return err
-	}
 	err = c.RunE(ctx, option.WithNodes(workloadNode), scInit.String())
 	if err != nil {
-		return nil, handleChemaChangeError(err)
+		return nil, handleSchemaChangeWorkloadError(err)
 	}
 
 	run := func() (func(), error) {
@@ -289,7 +290,7 @@ func startBackgroundWorkloads(
 		})
 		stopSC := workloadWithCancel(m, func(ctx context.Context) error {
 			if err := c.RunE(ctx, option.WithNodes(workloadNode), scRun.String()); err != nil {
-				return handleChemaChangeError(err)
+				return handleSchemaChangeWorkloadError(err)
 			}
 			return nil
 		})
