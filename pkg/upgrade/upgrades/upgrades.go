ea4376313a0962a46d76005d56d126ca0eff925f
Brendan Gerrity <brendan.gerrity@cockroachlabs.com>
2025-08-13 16:47:57 -0400

    sql: add system table for INSPECT results
    The planned `INSPECT` command will use the table to store the results of
    its consistency validation jobs.
    
    Fixes: #148266
    Epic: CRDB-30356
    
    Release note (sql change): The inspect_errors system table is
    introduced.

--- pkg/upgrade/upgrades/upgrades.go
+++ pkg/upgrade/upgrades/upgrades.go
@@ -87,6 +87,14 @@ var upgrades = []upgradebase.Upgrade{
 
 	newFirstUpgrade(clusterversion.V25_4_Start.Version()),
 
+	upgrade.NewTenantUpgrade(
+		"add new system.inspect_errors table",
+		clusterversion.V25_4_InspectErrorsTable.Version(),
+		upgrade.NoPrecondition,
+		createInspectErrorsTable,
+		upgrade.RestoreActionNotRequired("cluster restore does not restore this table"),
+	),
+
 	// Note: when starting a new release version, the first upgrade (for
 	// Vxy_zStart) must be a newFirstUpgrade. Keep this comment at the bottom.
 }
