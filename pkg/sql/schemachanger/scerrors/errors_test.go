bb9582656bc0de6243e3babaac0391750482235f
Faizan Qazi <faizan@cockroachlabs.com>
2025-08-15 15:30:29 -0400

    sql/schemachanger: add decoder for schemaChangerUserError
    Previously, if a schemaChangerUserError was sent over the network during
    a backfill, it was not properly recognized on the receiving node.
    Without a decoder, the error would be an unresolved opaqueWrapper. As a
    result, the HasSchemaChangerUserError function, which confirms the error
    by checking its type, would not work correctly. This patch adds a
    decoder for this error type, allowing it to be decoded properly on
    remote nodes.
    
    Fixes: #141352
    
    Release note: None

---
+++ pkg/sql/schemachanger/scerrors/errors_test.go
@@ -0,0 +1,23 @@
+package scerrors_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scerrors"
+	"github.com/cockroachdb/errors"
+	"github.com/cockroachdb/errors/errbase"
+	"github.com/stretchr/testify/require"
+)
+
+// TestEncodeSchemaChangeUserError tests that the error is encoded and decoded
+// correctly if sent over the network.
+func TestEncodeSchemaChangeUserError(t *testing.T) {
+	ctx := context.Background()
+	base := scerrors.SchemaChangerUserError(errors.New("boom"))
+	err := errbase.EncodeError(ctx, base)
+
+	decodeError := errbase.DecodeError(ctx, err)
+	require.True(t, scerrors.HasSchemaChangerUserError(decodeError))
+
+}
