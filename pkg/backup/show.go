418c789eb7fbc0ff36f351c7f39e68f8fb999804
Kevin Cao <kevin.cao@cockroachlabs.com>
2025-08-19 23:18:48 -0400

    backup: teach SHOW BACKUPS to use backup index
    `SHOW BACKUPS` previously would find all full backups by delimiting on
    the `data/` prefix and listing all objects from the root store. This was
    not very performant as while the data SSTs in all the backups would not
    be listed, all of the other files would be. This commit teaches `SHOW
    BACKUPS` to use the new backup index introduced in #150384 when listing
    backups. This method relies on listing the top-level contents in the
    `index/` directory and delimiting on `/`, which means only the required
    objects are listed.
    
    This behavior is gated behind the `WITH INDEX` option that has been
    added to `SHOW BACKUPS`. This is because in 25.4, it is still possible
    to restore from backups made before the backup index was added. Since
    the new behavior relies entirely on the `index/`, we would miss any
    backups that do not have a corresponding index item created. To capture
    all backups in a collection, we'd need to list both from the `index/`
    and via the legacy path, which would be counterintuitive. Instead, we
    will instead rely on `WITH INDEX` until v26.1, at which point all
    restorable backups should have an index created for them. At that time,
    we will make `WITH INDEX` the default and use `WITH LEGACY LISTING` to
    list older backups.
    
    Epic: CRDB-47942
    
    Fixes: #150328
    
    Release note (general change): Added `WITH INDEX` option to `SHOW
    BACKUPS` for faster listing of 25.4+ backups.

--- pkg/backup/show.go
+++ pkg/backup/show.go
@@ -1392,7 +1392,7 @@ func showBackupsInCollectionPlanHook(
 			return errors.Wrapf(err, "connect to external storage")
 		}
 		defer store.Close()
-		res, err := backupdest.ListFullBackupsInCollection(ctx, store)
+		res, err := backupdest.ListFullBackupsInCollection(ctx, store, showStmt.Options.Index)
 		if err != nil {
 			return err
 		}
