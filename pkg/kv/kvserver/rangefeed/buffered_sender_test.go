150bd1cb69a85da6edced61cb42e7815c53f58e8
Michael Xu <61663933+xuchef@users.noreply.github.com>
2025-08-21 16:02:43 +0000

    WIP
    event queue benchmark

--- pkg/kv/kvserver/rangefeed/buffered_sender_test.go
+++ pkg/kv/kvserver/rangefeed/buffered_sender_test.go
@@ -7,9 +7,13 @@ package rangefeed
 
 import (
 	"context"
+	"fmt"
+	"runtime/trace"
+	"sort"
 	"sync"
 	"sync/atomic"
 	"testing"
+	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
@@ -18,6 +22,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/require"
 )
@@ -163,4 +168,152 @@ func TestBufferedSenderChaosWithStop(t *testing.T) {
 		require.Equal(t, bs.sendBuffered(muxEv, nil).Error(), errors.New("stream sender is stopped").Error())
 		require.Equal(t, 0, bs.len())
 	})
+}
+
+// We create a new stream manager for each MuxRangeFeed call (to handle the gRPC stream).
+// Since we care about buffered sender, this doesn't matter to us.
+// Just wanted to note that a processor could potentially have different registrations
+// registered on it that correspond to different senders (stream managers).
+// But since we're stress testing, we can assume that every registration is for the same
+// underlying stream to maximize the load on a single sender.
+
+// A: From here on we focus on 1 MuxRangeFeed invocation, in which we
+//  1. Create a stream manager to manage a buffered sender for the locked gRPC stream
+// *2. Start the stream manager, which runs the sender's `run` method in a goroutine
+//      ↳ this is done with `stopper` to get a handle on it
+//  3. Start listening for requests to create a new rangefeed
+
+// -- we've got a stream manager and are running the sender's polling method
+
+// B: When we receive a RangeFeedRequest, we
+//  1. Ask our stream manager for a new input stream called a `streamSink`
+//  2. Register it with the rangefeed processor on the replica of note
+//      ↳ this requires searching for that replica, hence: stores -> store -> replica
+//      ↳ it's of note that if no processor exists yet, we create it here
+//  3. Once it's registered, we save our stream's disconnector in the stream manager
+
+// -- we've got an input pipe into our sender
+
+// C: Continuing from step 2 above, to register the stream with the rangefeed processor, we
+//  1. Create an unbuffered registration that pipes into the stream
+//  2. Add it to the registry
+// *3. Start a goroutine for the registration's `runOutputLoop`
+//      ↳ for unbuffered registration, this is a quick job for catchup scans exclusively
+//      ↳ for buffered registration (deprecated) it's a polling loop
+
+// -- now the processor knows where to forward it's logical operations to
+// -- all the infra is setup
+
+//	We only have K=(32|64|128) concurrent scheduler workers
+//
+// ==> we can only send K events concurrently at any instant
+func latencyPercentile(latencies []time.Duration, p float64) time.Duration {
+	sort.Slice(latencies, func(i, j int) bool {
+		return latencies[i] < latencies[j]
+	})
+	index := int(float64(len(latencies)) * p)
+	if index >= len(latencies) {
+		index = len(latencies) - 1
+	}
+	if index < 0 {
+		index = 0
+	}
+	return latencies[index]
+}
+
+func TestThroughput(t *testing.T) {
+	const (
+		workers         = 32
+		eventsPerWorker = eventQueueChunkSize
+		// we assume that the each event overlaps with all registrations
+		// so the total # of events = workers * eventsPerWorker * registrationsPerProcessor
+		registrationsPerProcessor = 10
+	)
+
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+	ctx := context.Background()
+
+	stopper := stop.NewStopper()
+	defer stopper.Stop(ctx)
+
+	// this mock stream gives us observability into the # of events that were actually sent
+	testServerStream := newTestServerStream()
+	bs := NewBufferedSender(testServerStream, NewBufferedSenderMetrics())
+
+	// initialize the stream manager like normal
+	smMetrics := NewStreamManagerMetrics()
+	sm := NewStreamManager(bs, smMetrics)
+
+	// start the sender's `run` loop in a goroutine
+	require.NoError(t, sm.Start(ctx, stopper))
+
+	var wg sync.WaitGroup
+	SpawnProcessorWorker := func(id int) {
+		wg.Add(1)
+
+		taskCtx, task := trace.NewTask(ctx, fmt.Sprintf("Handler-%d", id))
+		defer task.End()
+
+		// spawn a worker in a goroutine
+		require.NoError(t, stopper.RunAsyncTask(taskCtx, fmt.Sprintf("Worker %v", id),
+			func(ctx context.Context) {
+				stream, _ := sm.NewStream(int64(id), 42).(BufferedStream)
+
+				// We offset the start times to make it a little more realistic
+				time.Sleep(time.Duration(id) * time.Millisecond)
+
+				for range eventsPerWorker {
+					// at each tick, we consume a logical op, and create an event from it
+					val := roachpb.Value{
+						RawBytes: []byte("val"),
+						Timestamp: hlc.Timestamp{
+							WallTime: timeutil.Now().UnixNano(),
+						},
+					}
+					event := new(kvpb.RangeFeedEvent) // note: this only allocates one event per tick
+					event.MustSetValue(&kvpb.RangeFeedValue{Key: keyA, Value: val, PrevValue: val})
+
+					// the event gets published to each overlapping registration (which we assume to be all of them)
+					// we don't need a real registration because all unbuffered registration does is call stream.SendBuffered
+					j := 0
+					for j < registrationsPerProcessor {
+						if err := stream.SendBuffered(event, nil); err != nil {
+							// NOTE: this isn't realistic
+							// In reality, if we hit the buffer limit, we'd drop the registration and start a catchup scan,
+							// so we wouldn't be placing continuous pressure on the mutex like we're doing here.
+							// However, the overhead of a catchup scan is highly variable so it's hard to place a
+							// time.Sleep() here that would accurately capture the impact that running a catchup
+							// scan would have on the overall throughput.
+							continue
+						}
+						j++
+					}
+				}
+				wg.Done()
+			}))
+	}
+
+	// let the boys run
+	start := time.Now()
+	for i := range workers {
+		SpawnProcessorWorker(i)
+	}
+	wg.Wait()
+	testServerStream.waitForEventCount(t, eventsPerWorker*workers*registrationsPerProcessor)
+
+	dur := time.Since(start)
+
+	totalEventsSentBySender := testServerStream.totalEventsSent()
+	throughput := float64(totalEventsSentBySender) / dur.Seconds()
+
+	for streamID, latencies := range testServerStream.latencies {
+		p50 := latencyPercentile(latencies, 0.5)
+		p90 := latencyPercentile(latencies, 0.9)
+		max := latencyPercentile(latencies, 1)
+		fmt.Printf("StreamID:%v, p50: %v, p90: %v, max: %v\n", streamID, p50, p90, max)
+	}
+
+	fmt.Printf("totalEventsSentBySender: %v in %v\n", totalEventsSentBySender, dur)
+	fmt.Printf("throughput: %v\n", throughput)
 }
