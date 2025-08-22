941b64afe1885ae164178d58dc7ffae7e62c23ad
Brendan Gerrity <brendan.gerrity@cockroachlabs.com>
2025-08-22 16:45:43 -0400

    sql: implement SHOW INSPECT ERRORS command
    Adds the implementation for `SHOW INSPECT ERRORS` which reads from the
    `system.inspect_errors` table. This command is used to review the
    results of the data consistency validation jobs spun up by `INSPECT`.
    
    Part of: #148287
    Epic: CRDB-30356
    
    Release note: None

--- pkg/sql/delegate/show_inspect_errors.go
+++ pkg/sql/delegate/show_inspect_errors.go
@@ -6,9 +6,12 @@
 package delegate
 
 import (
+	"fmt"
 	"strings"
 
+	"github.com/cockroachdb/cockroach/pkg/jobs"
 	"github.com/cockroachdb/cockroach/pkg/sql/privilege"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/catid"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqltelemetry"
 	"github.com/cockroachdb/cockroach/pkg/sql/syntheticprivilege"
@@ -22,16 +25,71 @@ func (d *delegator) delegateShowInspectErrors(n *tree.ShowInspectErrors) (tree.S
 		return nil, err
 	}
 
+	var tableID = catid.InvalidDescID
 	if n.TableName != nil {
-		_, _, err := d.catalog.ResolveDataSource(d.ctx, resolveFlags, n.TableName)
+		resolvedTableID, err := d.evalCtx.Planner.ResolveTableName(d.ctx, n.TableName)
 		if err != nil {
 			return nil, err
 		}
 
+		tableID = catid.DescID(resolvedTableID)
 	}
 
 	var query strings.Builder
-	query.WriteString("SELECT * FROM system.inspect_errors")
+
+	query.WriteString(`WITH
+	completed_inspect_jobs AS (
+		SELECT id
+		FROM system.jobs
+		WHERE job_type = 'INSPECT'
+	`)
+	if n.JobID == nil {
+		query.WriteString(fmt.Sprintf(` AND status IN ('%s', '%s', '%s', '%s')`,
+			jobs.StateFailed, jobs.StateSucceeded, jobs.StateCanceled, jobs.StateRevertFailed)) // in terminal state
+	} else {
+		query.WriteString(fmt.Sprintf(" AND id = %d", *n.JobID))
+	}
+
+	// TODO(148287): query the inspect job payload to figure out if a job touches a particular table or database ID
+	// If a table was specified, only consider jobs that reported errors on it.
+	// If a job ID was specified, only consider that job. The records from the
+	// most recent completed job that satisfies those criteria is used.
+	query.WriteString(`),
+	job_id AS (
+		SELECT max(cij.id) as id
+		FROM completed_inspect_jobs cij
+		JOIN system.inspect_errors ie ON cij.id = ie.job_id
+		WHERE 1=1
+	`)
+	if tableID != catid.InvalidDescID {
+		query.WriteString(fmt.Sprintf(" AND ie.id = %d", tableID))
+	}
+	if n.JobID != nil {
+		query.WriteString(fmt.Sprintf(" AND ie.job_id = %d", *n.JobID))
+	}
+
+	query.WriteString(`)
+	SELECT 
+		ie.error_type,
+		COALESCE(d.name, '<unknown>') AS database_name,
+		COALESCE(s.name, '<unknown>') AS schema_name,
+		COALESCE(t.name, '<unknown>') AS table_name,
+		primary_key,
+		ie.job_id, 
+		to_char(aost, 'YYYY-MM-DD HH24:MI:SS.US') as aost`)
+	if n.WithDetails {
+		query.WriteString(", jsonb_pretty(ie.details) as details")
+	}
+	query.WriteString(`
+		FROM system.inspect_errors ie
+		LEFT JOIN system.namespace d ON ie.database_id = d.id AND d."parentID" = 0
+		LEFT JOIN system.namespace s ON ie.schema_id = s.id AND s."parentID" = ie.database_id
+		LEFT JOIN system.namespace t ON ie.id = t.id AND t."parentSchemaID" = ie.schema_id
+		WHERE ie.job_id IN (SELECT id FROM job_id)`)
+	if tableID != catid.InvalidDescID {
+		query.WriteString(fmt.Sprintf(" AND ie.id = %d", tableID))
+	}
+	query.WriteString(` ORDER BY ie.error_type, ie.error_id`)
 
 	return d.parse(query.String())
 }
