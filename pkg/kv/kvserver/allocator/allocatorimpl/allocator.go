b30236f2c07ef91499e02d890f178eded79cd628
wenyihu6 <wenyi@cockroachlabs.com>
2025-08-26 15:42:07 -0400

    fixup! allocatorimpl: adds a priority assertion to computeAction

--- pkg/kv/kvserver/allocator/allocatorimpl/allocator.go
+++ pkg/kv/kvserver/allocator/allocatorimpl/allocator.go
@@ -957,7 +957,7 @@ func (a *Allocator) ComputeAction(
 	// priority inversion. If the priority is not -1, the range might be re-queued
 	// to be processed with the correct priority.
 	if priority == -1 && buildutil.CrdbTestBuild {
-		log.Fatalf(ctx, fmt.Sprintf("allocator returned -1 priority for range %s: %v", desc, action))
+		log.Fatalf(ctx, "allocator returned -1 priority for range %s: %v", desc, action)
 	}
 	return action, priority
 }
