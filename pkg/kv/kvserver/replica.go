a92ae9bf77f5f24e46d6c4a7b72fd5d8b47227a5
wenyihu6 <wenyi@cockroachlabs.com>
2025-08-25 20:59:34 -0400

    kvserver: simplify logging in maybeEnqueueProblemRange
    This commit simplifies the logging in `maybeEnqueueProblemRange` to log two
    booleans directly.

--- pkg/kv/kvserver/replica.go
+++ pkg/kv/kvserver/replica.go
@@ -2928,17 +2928,8 @@ func (r *Replica) maybeEnqueueProblemRange(
 	// unnecessary work. We expect this method to be called in the context of
 	// updating metrics.
 	if !isLeaseholder || !leaseValid {
-		// The replicate queue will not process the replica without a valid lease.
-		// Track when we skip enqueuing for these reasons.
-		boolToInt := func(b bool) int {
-			if b {
-				return 1
-			}
-			return 0
-		}
-		reasons := []string{"is not the leaseholder", "the lease is not valid"}
-		reason := reasons[boolToInt(isLeaseholder)]
-		log.KvDistribution.VInfof(ctx, 1, "not enqueuing replica %s because %s", r.Desc(), reason)
+		log.KvDistribution.Infof(ctx, "not enqueuing replica %s because isLeaseholder=%t, leaseValid=%t",
+			r.Desc(), isLeaseholder, leaseValid)
 		r.store.metrics.DecommissioningNudgerNotLeaseholderOrInvalidLease.Inc(1)
 		return
 	}
