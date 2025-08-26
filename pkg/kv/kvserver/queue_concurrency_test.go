2201c4fb61dfd58632fc8ad21d836c040e9cb120
wenyihu6 <wenyi@cockroachlabs.com>
2025-08-26 13:14:18 -0400

    allocatorimpl: add invariants on priority to base queue tests
    This commit adds additional invariants to verify the correctness of priority
    plumbing for range items in base queue tests.

--- pkg/kv/kvserver/queue_concurrency_test.go
+++ pkg/kv/kvserver/queue_concurrency_test.go
@@ -112,7 +112,7 @@ func TestBaseQueueConcurrent(t *testing.T) {
 			})
 		}
 		g.Go(func() error {
-			bq.assertInvariants()
+			bq.assertInvariants(func(item *replicaItem) {})
 			return nil
 		})
 	}
