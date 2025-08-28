74498834989704dc0219009f9b7c82b2dc1dc63d
Pavel Kalinnikov <pavel@cockroachlabs.com>
2025-08-28 14:48:11 +0100

    print: pretty print WAG node contents
    Epic: none
    Release note: none

--- pkg/kv/kvserver/kvstorage/wag/wagpb/wag.go
+++ pkg/kv/kvserver/kvstorage/wag/wagpb/wag.go
@@ -5,6 +5,19 @@
 
 package wagpb
 
-// TODO(pav-kv): remove this once the dev gen bazel picks up this dependency
-// from the .pb.go file.
-import _ "github.com/cockroachdb/cockroach/pkg/kv/kvpb"
+import (
+	// TODO(pav-kv): remove this once the dev gen bazel picks up this dependency
+	// from the .pb.go file.
+	_ "github.com/cockroachdb/cockroach/pkg/kv/kvpb"
+	"github.com/cockroachdb/redact"
+)
+
+// String implements the fmt.Stringer interface.
+func (a Addr) String() string {
+	return redact.StringWithoutMarkers(a)
+}
+
+// SafeFormat implements the redact.SafeFormatter interface.
+func (a Addr) SafeFormat(w redact.SafePrinter, _ rune) {
+	w.Printf("r%d/%d:%d", a.RangeID, a.ReplicaID, a.Index)
+}
