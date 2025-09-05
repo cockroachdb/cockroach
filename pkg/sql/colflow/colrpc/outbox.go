76f41b828a10735c52913b75654481acff080cb5
Chandra Thumuluru <chandra.thumuluru@cockroachlabs.com>
2025-09-05 16:54:53 +0530

    server,sql: use generic dialers to create RPC clients
    Epic: CRDB-48935
    Informs: None
    Release note: None

--- pkg/sql/colflow/colrpc/outbox.go
+++ pkg/sql/colflow/colrpc/outbox.go
@@ -180,7 +180,7 @@ func (o *Outbox) Run(
 
 	var stream execinfrapb.RPCDistSQL_FlowStreamClient
 	if err := func() error {
-		client, err := execinfra.GetDistSQLClientForOutbox(ctx, dialer, sqlInstanceID, connectionTimeout)
+		client, err := execinfra.GetDistSQLClientForOutbox(ctx, dialer, o.flowCtx.Cfg.Settings, sqlInstanceID, connectionTimeout)
 		if err != nil {
 			log.Dev.VWarningf(ctx, 1, "Outbox Dial connection error, distributed query will fail: %+v", err)
 			return err
