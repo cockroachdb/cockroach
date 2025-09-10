48e964880a1f6aa71ad3f427207f3f177766c19b
Marcus Gartner <marcus@cockroachlabs.com>
2025-09-10 12:52:57 -0400

    sql: remove some EXPLAIN [ANALYZE] fields in non-VERBOSE mode
    The output of `EXPLAIN [ANALYZE]` in non-`VERBOSE` mode is now more
    succinct.
    
    In the header above the query plan the following changes have been made:
    
      1. "isolation level" is only shown if it is not "serializable".
      2. "priority" is only shown if it is not "normal".
      3. "quality of service" is only shown if it is not "regular".
      4. "kv cpu time" is not shown.
      5. "sql cpu time" is not shown.
      6. "DistSQL network usage" is only show if it is greater than 0.
      7. "maximum memory usage" is not shown.
      8. "cumulative time spent in KV" is not shown.
      9. "vectorized" is only shown if it is "false".
     10. "buffered writes enabled" is not shown.
    
    In each node of the query plan the following changes have been made:
    
      1. "KV pairs read" is not shown.
      2. "KV bytes read" is not shown.
      3. "KV gRPC calls" is not shown.
      4. "estimated max memory allocated" is not shown.
      5. "estimated max sql temp disk usage" is not shown.
      6. "actual row count" is directly above "estimated row count".
    
    Here's an example of what `EXPLAIN ANALYZE` looked like before:
    
    ```
    planning time: 243µs
    execution time: 632µs
    distribution: local
    vectorized: true
    plan type: generic, reused
    cumulative time spent in KV: 331µs
    maximum memory usage: 20 KiB
    DistSQL network usage: 0 B (0 messages)
    regions: us-east1
    sql cpu time: 20µs
    estimated RUs consumed: 0
    isolation level: serializable
    priority: normal
    quality of service: regular
    
    • scan
      sql nodes: n1
      kv nodes: n1
      regions: us-east1
      actual row count: 0
      KV time: 331µs
      KV rows decoded: 0
      KV bytes read: 0 B
      KV gRPC calls: 1
      estimated max memory allocated: 20 KiB
      sql cpu time: 20µs
      missing stats
      table: t@t_pkey
      spans: [/0 - /0]
    ```
    
    And here's the output with the same query after the changes in this
    commit:
    
    ```
    planning time: 325µs
    execution time: 974µs
    distribution: local
    plan type: generic, reused
    regions: us-east1
    estimated RUs consumed: 0
    
    • scan
      sql nodes: n1
      kv nodes: n1
      regions: us-east1
      KV time: 610µs
      KV rows decoded: 0
      sql cpu time: 29µs
      actual row count: 0
      missing stats
      table: t@t_pkey
      spans: [/0 - /0]
    ```
    
    Release note (sql change): The output of `EXPLAIN [ANALYZE]` in
    non-`VERBOSE` mode is now more succinct.

--- pkg/sql/pgwire/pgwire_test.go
+++ pkg/sql/pgwire/pgwire_test.go
@@ -705,7 +705,6 @@ func TestPGPreparedQuery(t *testing.T) {
 		{"EXPLAIN SELECT 1", []preparedQueryTest{
 			baseTest.SetArgs().
 				Results("distribution: local").
-				Results("vectorized: true").
 				Results("").
 				Results("• values").
 				Results("  size: 1 column, 1 row"),
