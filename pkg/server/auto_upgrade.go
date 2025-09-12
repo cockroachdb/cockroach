6ba3c63cb899fa9afd8000d3ab9188866e520900
Steven Danna <danna@cockroachlabs.com>
2025-09-12 17:29:00 +0100

    server: only auto-upgrade system tenant from meta1 leaseholder
    Running the auto-upgrade process from all nodes does some amount of
    wasted work. Let's avoid that by only running it from the meta1
    leaseholder. This only applies to the system tenant.
    
    For shared-process multi-tenancy, we may consider allowing secondary
    tenants access to this same information.
    
    For separate process multi-tenancy, a different solution would be needed.
    
    Informs #152916
    
    Release note: None

--- pkg/server/auto_upgrade.go
+++ pkg/server/auto_upgrade.go
@@ -47,6 +47,14 @@ func (s *topLevelServer) startAttemptUpgrade(ctx context.Context) error {
 		}
 
 		for r := retry.StartWithCtx(ctx, retryOpts); r.Next(); {
+			if clusterversion.AutoUpgradeSystemClusterFromMeta1Leaseholder.Get(&s.ClusterSettings().SV) {
+				isMeta1LH, err := s.sqlServer.isMeta1Leaseholder(ctx, s.clock.NowAsClockTimestamp())
+				if err != nil || !isMeta1LH {
+					log.Ops.VInfof(ctx, 2, "not upgrading since we are not the Meta1 leaseholder; err=%v", err)
+					continue
+				}
+			}
+
 			clusterVersion, err := s.clusterVersion(ctx)
 			if err != nil {
 				log.Dev.Errorf(ctx, "unable to retrieve cluster version: %v", err)
