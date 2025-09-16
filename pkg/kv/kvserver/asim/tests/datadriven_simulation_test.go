4ec2d66c5babedc4f7c175c9f1cc3368a8a430d6
wenyihu6 <wenyi@cockroachlabs.com>
2025-09-16 11:43:39 -0400

    asim: rename sma-only to sma-count and remove default
    This commit renames sma-only to sma-count specify that it enables single-metric
    store rebalancer and count-based rebalancing. It also removes default since it
    is unclear what it was intended for and does the same thing as sma-count now.

--- pkg/kv/kvserver/asim/tests/datadriven_simulation_test.go
+++ pkg/kv/kvserver/asim/tests/datadriven_simulation_test.go
@@ -463,7 +463,7 @@ func TestDataDriven(t *testing.T) {
 				if len(cfgs) == 0 {
 					// TODO(tbg): force each test to specify the configs it wants to run
 					// under.
-					cfgs = []string{"default"}
+					cfgs = []string{"sma-count"}
 				}
 
 				metricsMap := map[string]struct{}{}
@@ -475,9 +475,7 @@ func TestDataDriven(t *testing.T) {
 				require.NotZero(t, rangeGen)
 
 				knownConfigurations := map[string]func(eg *gen.StaticEvents){
-					// In default mode, the test manages its rebalancer-related settings
-					// manually.
-					"default": func(eg *gen.StaticEvents) {
+					"sma-count": func(eg *gen.StaticEvents) {
 						eg.ScheduleEvent(settingsGen.Settings.StartTime, 0,
 							event.SetSimulationSettingsEvent{
 								IsClusterSetting: true,
@@ -487,14 +485,6 @@ func TestDataDriven(t *testing.T) {
 					},
 					// 'mma-only' runs with the multi-metric allocator and turns off the
 					// replicate and lease queues.
-					"sma-only": func(eg *gen.StaticEvents) {
-						eg.ScheduleEvent(settingsGen.Settings.StartTime, 0,
-							event.SetSimulationSettingsEvent{
-								IsClusterSetting: true,
-								Key:              "LBRebalancingMode",
-								Value:            int64(kvserverbase.LBRebalancingLeasesAndReplicas),
-							})
-					},
 					"mma-only": func(eg *gen.StaticEvents) {
 						eg.ScheduleEvent(settingsGen.Settings.StartTime, 0,
 							event.SetSimulationSettingsEvent{
