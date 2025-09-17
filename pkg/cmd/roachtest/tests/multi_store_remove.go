8c42d67f977b61dbdff9b944b46749fa4e44b2b7
Ibrahim Kettaneh <ibrahim.kettaneh@cockroachlabs.com>
2025-09-17 19:30:44 +0000

    roachtest: increase the verbosity of runMultiStoreRemove
    Recent failures in runMultiStoreRemove seems to indicate a potential
    race between follower replica removal and leader fortification.
    
    References: #153517
    
    Release note: None

--- pkg/cmd/roachtest/tests/multi_store_remove.go
+++ pkg/cmd/roachtest/tests/multi_store_remove.go
@@ -52,14 +52,15 @@ func runMultiStoreRemove(ctx context.Context, t test.Test, c cluster.Cluster) {
 	t.Status("starting cluster")
 	startOpts := option.DefaultStartOpts()
 	startOpts.RoachprodOpts.StoreCount = multiStoreStoresPerNode
-	// TODO(jackson): Allow WAL failover to be enabled once it's able to
-	// tolerate the removal of a store. Today, the mapping of failover
-	// secondaries is fixed, making WAL failover incompatible with the removal
-	// of a store.
-	startOpts.RoachprodOpts.WALFailover = ""
 	startSettings := install.MakeClusterSettings()
 	// Speed up the replicate queue.
 	startSettings.Env = append(startSettings.Env, "COCKROACH_SCAN_INTERVAL=30s")
+
+	// Increase the verbosity of this test to help debug future failures.
+	startOpts.RoachprodOpts.ExtraArgs = append(startOpts.RoachprodOpts.ExtraArgs,
+		"--vmodule=*=1,raft=3",
+	)
+
 	c.Start(ctx, t.L(), startOpts, startSettings, c.Range(1, 3))
 
 	// Confirm that there are 6 stores live.
@@ -101,7 +102,7 @@ func runMultiStoreRemove(ctx context.Context, t test.Test, c cluster.Cluster) {
 	// Bring down node 1.
 	t.Status("removing store from n1")
 	node := c.Node(1)
-	m := c.NewDeprecatedMonitor(ctx, node)
+	m := c.NewMonitor(ctx, node)
 	m.ExpectDeaths(1)
 	stopOpts := option.DefaultStopOpts()
 	c.Stop(ctx, t.L(), stopOpts, node)
