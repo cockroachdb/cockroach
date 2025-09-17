71447ae0152f9091c30d9383903f4abdf39ce9d2
Nukitt <nukit.tailor@cockroachlabs.com>
2025-09-17 14:46:10 +0530

    rpc, server: add DRPC metrics server interceptor
    Previously, there was no support for metrics interceptor in
    DRPC, and capturing drpc metrics wouldn't be possible.
    In this PR, we add metrics server interceptor support in DRPC and
    extend the `rpc_server_request_duration_nanos` metric to have an
    `rpcServer` label which can have "grpc" and "drpc". This way we
    can observe both gRPC and DRPC latency through interceptors.
    Since this metric is behind a cluster setting, we also set up an
    alias for it so that it can be related to both the rpc framework.
    
    Epic: CRDB-49359
    Fixes: #144373
    Release note (ui change): Now when the metric
    `rpc_server_request_duration_nanos`is emitted on the endpoint,
    it will contain an additional label called `rpcServer` which
    can have 2 possible values: "grpc" and "drpc", depending on
    whichever is set.

--- pkg/rpc/metrics.go
+++ pkg/rpc/metrics.go
@@ -23,6 +23,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
+	"storj.io/drpc/drpcmux"
 )
 
 // gwRequestKey is a field set on the context to indicate a request
@@ -205,7 +206,7 @@ over this connection.
 	}
 	metaRequestDuration = metric.Metadata{
 		Name:        "rpc.server.request.duration.nanos",
-		Help:        "Duration of an grpc request in nanoseconds.",
+		Help:        "Duration of an RPC request in nanoseconds.",
 		Measurement: "Duration",
 		Unit:        metric.Unit_NANOSECONDS,
 		MetricType:  prometheusgo.MetricType_HISTOGRAM,
@@ -430,6 +431,7 @@ func (m *Metrics) acquire(k peerKey, l roachpb.Locality) (peerMetrics, localityM
 const (
 	RpcMethodLabel     = "methodName"
 	RpcStatusCodeLabel = "statusCode"
+	RpcServerLabel     = "rpcServer"
 )
 
 // RequestMetrics contains metrics for RPC requests.
@@ -442,11 +444,15 @@ func NewRequestMetrics() *RequestMetrics {
 		Duration: metric.NewExportedHistogramVec(
 			metaRequestDuration,
 			metric.ResponseTime30sBuckets,
-			[]string{RpcMethodLabel, RpcStatusCodeLabel}),
+			[]string{RpcMethodLabel, RpcStatusCodeLabel, RpcServerLabel}),
 	}
 }
 
 type RequestMetricsInterceptor grpc.UnaryServerInterceptor
+
+const (
+	GRPCServer = "grpc"
+)
 
 // NewRequestMetricsInterceptor creates a new gRPC server interceptor that records
 // the duration of each RPC. The metric is labeled by the method name and the
@@ -478,6 +484,49 @@ func NewRequestMetricsInterceptor(
 		requestMetrics.Duration.Observe(map[string]string{
 			RpcMethodLabel:     info.FullMethod,
 			RpcStatusCodeLabel: code.String(),
+			RpcServerLabel:     GRPCServer,
+		}, float64(duration.Nanoseconds()))
+		return resp, err
+	}
+}
+
+type DRPCRequestMetricsInterceptor drpcmux.UnaryServerInterceptor
+
+const (
+	ServerDRPC = "drpc"
+)
+
+// NewDRPCRequestMetricsInterceptor creates a new DRPC server interceptor that records
+// the duration of each RPC. The metric is labeled by the method name and the
+// status code of the RPC. The interceptor will only record durations if
+// shouldRecord returns true. Otherwise, this interceptor will be a no-op.
+func NewDRPCRequestMetricsInterceptor(
+	requestMetrics *RequestMetrics, shouldRecord func(rpc string) bool,
+) DRPCRequestMetricsInterceptor {
+	return func(
+		ctx context.Context,
+		req any,
+		rpc string,
+		handler drpcmux.UnaryHandler,
+	) (any, error) {
+		if !shouldRecord(rpc) {
+			return handler(ctx, req)
+		}
+		startTime := timeutil.Now()
+		resp, err := handler(ctx, req)
+		duration := timeutil.Since(startTime)
+		var code codes.Code
+		if err != nil {
+			// TODO(nukitt): use drpc status code
+			code = status.Code(err)
+		} else {
+			code = codes.OK
+		}
+
+		requestMetrics.Duration.Observe(map[string]string{
+			RpcMethodLabel:     rpc,
+			RpcStatusCodeLabel: code.String(),
+			RpcServerLabel:     ServerDRPC,
 		}, float64(duration.Nanoseconds()))
 		return resp, err
 	}
