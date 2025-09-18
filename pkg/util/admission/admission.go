01e2c8ff84bb492027f742cf239e2a258ca3fcd3
Josh Imhoff <josh@cockroachlabs.com>
2025-09-18 11:28:19 -0400

    admission: introduce a granter for CPU time tokens
    This commit introduces a granter called cpuTimeTokenGranter.
    cpuTimeTokenGranter uses token buckets to limit CPU usage. This
    commit introduces the granter, but it does not implement any
    logic to periodically refill the token buckets, or compute the
    token rates, based on observed CPU usage. That will come in later
    PRs.
    
    https://github.com/cockroachdb/cockroach/issues/153591
    
    Release note: None.

--- pkg/util/admission/admission.go
+++ pkg/util/admission/admission.go
@@ -121,6 +121,7 @@
 package admission
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/util/admission/admissionpb"
@@ -142,6 +143,17 @@ const (
 	noBurst
 	numBurstQualifications
 )
+
+func (bq burstQualification) String() string {
+	switch bq {
+	case canBurst:
+		return "canBurst"
+	case noBurst:
+		return "noBurst"
+	default:
+		return fmt.Sprintf("burstQualification(%d)", bq)
+	}
+}
 
 // requester is an interface implemented by an object that orders admission
 // work for a particular WorkKind. See WorkQueue for the implementation of
