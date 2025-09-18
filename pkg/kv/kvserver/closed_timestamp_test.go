81505cde663d066bd2a22812dc38d11ef9a96533
Ibrahim Kettaneh <ibrahim.kettaneh@cockroachlabs.com>
2025-09-18 15:26:28 +0000

    kvserver: deflake TestClosedTimestampFrozenAfterSubsumption
    This commit deflakes TestClosedTimestampFrozenAfterSubsumption by making sure
    that when running with leader leases, we first make sure that the leader is
    fortified before stopping storeliveness heartbeats.
    Otherwise, there could be a case where the leader hasn't been fortified yet, and
    blocking storeliveness heartbeats won't necessarily cause the leader to step down
    as the leader will be sending normal raft heartbeats since it never got fortified
    for this term.
    
    Fixes: #153679
    
    Release note: None

--- pkg/kv/kvserver/closed_timestamp_test.go
+++ pkg/kv/kvserver/closed_timestamp_test.go
@@ -624,8 +624,11 @@ func TestClosedTimestampFrozenAfterSubsumption(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 
-	skip.UnderRace(t)
-	skip.UnderDeadlock(t)
+	skip.UnderDuress(t)
+
+	// Increase the verbosity of the logs to help debug the test if it fails, especially
+	// raft related logs when the test tries to transfer the lease non-cooperatively.
+	require.NoError(t, log.SetVModule("raft=4,*=1"))
 
 	for _, test := range []struct {
 		name string
