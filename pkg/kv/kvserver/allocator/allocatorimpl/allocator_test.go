2110a7d43bc6116e45e0503d00869772c8b06c0d
Tobias Grieger <tobias.b.grieger@gmail.com>
2025-09-22 13:27:18 +0200

    allocatorimpl: disable follow-the-workload by default

--- pkg/kv/kvserver/allocator/allocatorimpl/allocator_test.go
+++ pkg/kv/kvserver/allocator/allocatorimpl/allocator_test.go
@@ -6139,6 +6139,7 @@ func TestAllocatorTransferLeaseTargetLoadBased(t *testing.T) {
 
 	ctx := context.Background()
 	st := cluster.MakeTestingClusterSettings()
+	EnableLoadBasedLeaseRebalancing.Override(ctx, &st.SV, true)
 	stopper, g, _, storePool, _ := storepool.CreateTestStorePool(ctx, st,
 		liveness.TestTimeUntilNodeDeadOff, true, /* deterministic */
 		func() int { return 10 }, /* nodeCount */
