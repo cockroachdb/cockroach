fdcdd995b462432252e2f738dd5b8cb18cdfa7d8
Ludovic Leroux <ludo.leroux@cockroachlabs.com>
2025-09-25 13:51:23 -0400

    roachprod: prepare centralized - enrich vm.Provider interface
    This patch prepares the ground work for roachprod-centralized by
    enriching the vm.Provider interface with two new methods:
    - `String()`: returns a human-readable (and map key) for the provider in
      its current configuration (e.g. `{ProviderName}-{project}`,
    `{ProviderName}-{AccountNumber}`, ...)
    - `IsLocalProvider()`: returns whether or not the provider is local
    
    These two new methods will ease the work of spawning multiple instances
    of the same provider, in different configurations.
    
    Epic: none
    Release note: None

--- pkg/roachprod/vm/local/local.go
+++ pkg/roachprod/vm/local/local.go
@@ -120,6 +120,11 @@ type Provider struct {
 	vm.DNSProvider
 }
 
+// IsLocalProvider returns true because it is a local provider.
+func (p *Provider) IsLocalProvider() bool {
+	return true
+}
+
 func (p *Provider) DefaultZones(_ string, _ bool) []string {
 	return []string{}
 }
@@ -363,4 +368,9 @@ func (p *Provider) Active() bool {
 // ProjectActive is part of the vm.Provider interface.
 func (p *Provider) ProjectActive(project string) bool {
 	return project == ""
+}
+
+// String is part of the vm.Provider interface.
+func (p *Provider) String() string {
+	return ProviderName
 }
