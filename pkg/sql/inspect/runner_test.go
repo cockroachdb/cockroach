90ceff4d3837e69cf0889c36d3c09c9261a1c7d9
Matt Spilchen <matt.spilchen@cockroachlabs.com>
2025-09-25 09:24:22 -0300

    sql/inspect: skip INSPECT checks for non-applicable spans earlier
    INSPECT jobs generate a set of checks along with a separate list of spans,
    which only apply to some of the checks. Previously, filtering to ensure a span
    applied to a check happened late in the process.
    
    This commit moves that filtering earlier, so checks immediately skip spans
    that are not relevant. This improves efficiency and prepares the system for
    upcoming progress tracking logic, which will be added in a separate PR.
    
    Informs: #148297
    Epic: CRDB-30356
    
    Release note: none

--- pkg/sql/inspect/runner_test.go
+++ pkg/sql/inspect/runner_test.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/execinfra"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
@@ -28,6 +29,13 @@ type mockInspectCheck struct {
 }
 
 var _ inspectCheck = &mockInspectCheck{}
+var _ inspectCheckApplicability = &mockInspectCheck{}
+
+// AppliesTo implements the inspectCheckApplicability interface.
+func (m *mockInspectCheck) AppliesTo(codec keys.SQLCodec, span roachpb.Span) (bool, error) {
+	// For testing, assume all checks apply to all spans
+	return true, nil
+}
 
 func (m *mockInspectCheck) Started() bool {
 	return m.started
