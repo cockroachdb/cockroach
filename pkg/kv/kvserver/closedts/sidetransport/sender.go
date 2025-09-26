ee5f376a6fd069b63fe6fbcc96e254d57b9190ca
iskettaneh <173953022+iskettaneh@users.noreply.github.com>
2025-09-26 12:00:45 -0400

    sidetransport: pace sidetransport sender messages
    Before this commit, every sidetransport sending interval, we used to
    wake up up-to N goroutines to send the sidetransport messages where
    N=number of nodes. This adds an overhead on the go scheduler as the
    number of nodes in the cluster gets larger.
    
    This commit addresses that by adding a small pacing duration while we
    wake up the goroutines to send messages.
    
    Fixes: #148211
    
    Release note: None

--- pkg/kv/kvserver/closedts/sidetransport/sender.go
+++ pkg/kv/kvserver/closedts/sidetransport/sender.go
@@ -33,6 +33,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/netutil"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
+	"github.com/cockroachdb/cockroach/pkg/util/taskpacer"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/errors"
 )
@@ -114,6 +115,21 @@ type streamState struct {
 type connTestingKnobs struct {
 	beforeSend         func(destNodeID roachpb.NodeID, msg *ctpb.Update)
 	sleepOnErrOverride time.Duration
+}
+
+// pacerConfig implements taskpacer.Config using cluster settings.
+type pacerConfig struct {
+	st *cluster.Settings
+}
+
+// GetRefresh implements the taskpacer.Config interface
+func (c *pacerConfig) GetRefresh() time.Duration {
+	return closedts.SideTransportPacingRefreshInterval.Get(&c.st.SV)
+}
+
+// GetSmear implements the taskpacer.Config interface
+func (c *pacerConfig) GetSmear() time.Duration {
+	return closedts.SideTransportPacingSmearInterval.Get(&c.st.SV)
 }
 
 // trackedRange contains the information that the side-transport last published
@@ -212,7 +228,7 @@ func newSenderWithConnFactory(
 		st:          st,
 		clock:       clock,
 		connFactory: connFactory,
-		buf:         newUpdatesBuf(),
+		buf:         newUpdatesBuf(st),
 	}
 	s.trackedMu.tracked = make(map[roachpb.RangeID]trackedRange)
 	s.trackedMu.lastClosed = make(map[ctpb.RangeClosedTimestampPolicy]hlc.Timestamp)
@@ -236,6 +252,12 @@ func (s *Sender) Run(ctx context.Context, nodeID roachpb.NodeID) {
 		}
 	}
 	closedts.SideTransportCloseInterval.SetOnChange(&s.st.SV, confChanged)
+
+	// Set up callback for pacing refresh interval changes.
+	pacingConfChanged := func(ctx context.Context) {
+		s.buf.updatePacer(s.st)
+	}
+	closedts.SideTransportPacingRefreshInterval.SetOnChange(&s.st.SV, pacingConfChanged)
 
 	_ /* err */ = s.stopper.RunAsyncTask(ctx, "closedts side-transport publisher",
 		func(ctx context.Context) {
@@ -540,6 +562,10 @@ type updatesBuf struct {
 	mu struct {
 		syncutil.Mutex
 
+		// pacer controls the timing of broadcast updates to avoid overloading the
+		// system.
+		pacer *taskpacer.Pacer
+
 		// We use two condition variables that we atomically swap to avoid signaling
 		// the same goroutine multiple times. Without this, a goroutine could:
 		// 1. Wake up from a signal.
@@ -580,12 +606,20 @@ type updatesBuf struct {
 // little while and not have to send a snapshot when it resumes.
 const updatesBufSize = 50
 
-func newUpdatesBuf() *updatesBuf {
+func newUpdatesBuf(st *cluster.Settings) *updatesBuf {
 	buf := &updatesBuf{}
 	buf.mu.updated1.L = &buf.mu
 	buf.mu.updated2.L = &buf.mu
 	buf.mu.data = make([]*ctpb.Update, updatesBufSize)
+	buf.mu.pacer = taskpacer.New(&pacerConfig{st: st})
 	return buf
+}
+
+// updatePacer atomically replaces the task pacer with a new one using the current cluster settings.
+func (b *updatesBuf) updatePacer(st *cluster.Settings) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	b.mu.pacer = taskpacer.New(&pacerConfig{st: st})
 }
 
 // Push adds a new update to the back of the buffer.
@@ -632,20 +666,39 @@ func (b *updatesBuf) Push(ctx context.Context, update *ctpb.Update) {
 	// Notify everybody who might have been waiting for this message - we expect
 	// all the connections to be blocked waiting.
 	b.mu.Unlock()
-	b.PaceBroadcastUpdate(condVar, numWaiters)
+	b.PaceBroadcastUpdate(ctx, condVar, numWaiters)
 }
 
-// PaceBroadcastUpdate paces the conditional variable signaling to avoid
-// overloading the system.
-func (b *updatesBuf) PaceBroadcastUpdate(condVar *sync.Cond, numWaiters *int) {
-	// TODO(ibrahim): We can just hook this with the taskPacer.
+// PaceBroadcastUpdate paces the conditional variable signaling to avoid overloading the system.
+func (b *updatesBuf) PaceBroadcastUpdate(ctx context.Context, condVar *sync.Cond, numWaiters *int) {
 	b.mu.Lock()
 	originalNumWaiters := *numWaiters
-	for originalNumWaiters > 0 {
-		condVar.Signal()
-		originalNumWaiters--
+	if originalNumWaiters <= 0 {
+		b.mu.Unlock()
+		return
 	}
+
+	// Get the current pacer (which uses the cluster setting refresh interval).
+	pacer := b.mu.pacer
 	b.mu.Unlock()
+
+	pacer.StartTask(timeutil.Now())
+
+	workLeft := originalNumWaiters
+	for workLeft > 0 {
+		todo, by := pacer.Pace(timeutil.Now(), workLeft)
+
+		b.mu.Lock()
+		for i := 0; i < todo && workLeft > 0; i++ {
+			condVar.Signal()
+			workLeft--
+		}
+		b.mu.Unlock()
+
+		if workLeft > 0 && timeutil.Now().Before(by) {
+			time.Sleep(by.Sub(timeutil.Now()))
+		}
+	}
 }
 
 func (b *updatesBuf) lastIdxLocked() int {
@@ -741,6 +794,14 @@ func (b *updatesBuf) Close() {
 	b.mu.closed = true
 	b.mu.updated1.Broadcast()
 	b.mu.updated2.Broadcast()
+}
+
+// TestingGetTotalNumWaiters returns the total number of goroutines waiting
+// on the buffer. For testing purposes only.
+func (b *updatesBuf) TestingGetTotalNumWaiters() int {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	return b.mu.numWaiters1 + b.mu.numWaiters2
 }
 
 // connFactory is capable of creating new connections to specific nodes.
