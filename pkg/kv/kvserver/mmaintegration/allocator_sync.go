6386c47e249d1d146d20dd0795ee65ff0259c0b3
wenyihu6 <wenyi@cockroachlabs.com>
2025-09-26 15:27:02 -0400

    allocatorimpl: consult with mma for replicate queue rebalancing
    Previously, LBRebalancingMultiMetricAndCount allowed both MMA- and count-based
    rebalancing, which could lead to conflicting goals and thrashing. This commit
    updates the replicate queue to consult MMA on the final rebalancing target
    selected. Critical rebalances that repair constraints, disk fullness, or
    diversity bypass the MMA check; all other rebalances consult MMA before
    proceeding.
    
    Some asim tests showed even more thrashing, possibly due to two factors:
    1. The replicate queue may have a stale view of in-progress changes.
    2. The mean calculation of replica count now includes overloaded stores that
       might not be viable targets, leading to suboptimal decisions. For example,
       the queue may rebalance from a slightly overfull store toward an
       around-the-mean store, but the mean can be deflated by overloaded stores with
       very low range counts. As a result, slightly overfull store might appear to
       deserve a rebalance even though it won't be able to pick a very good
       candidate in the end.
    We are okay with this for now and will revisit if needed.
    
    The lease queue does not yet apply this check; future commits will extend it.
    
    I plan to add more data-driven tests to ensure that constraints are still
    repaired even when MMA rejects certain actions. These will be included in
    follow-up PRs.

--- pkg/kv/kvserver/mmaintegration/allocator_sync.go
+++ pkg/kv/kvserver/mmaintegration/allocator_sync.go
@@ -45,6 +45,14 @@ type mmaState interface {
 	// AdjustPendingChangesDisposition is called by the allocator sync to adjust
 	// the disposition of pending changes.
 	AdjustPendingChangesDisposition(changeIDs []mmaprototype.ChangeID, success bool)
+	// BuildMMARebalanceAdvisor is called by the allocator sync to build a
+	// MMARebalanceAdvisor for the given existing store and candidates. The
+	// advisor should be later passed to IsInConflictWithMMA to determine if a
+	// given candidate is in conflict with the existing store.
+	BuildMMARebalanceAdvisor(existing roachpb.StoreID, cands []roachpb.StoreID) mmaprototype.MMARebalanceAdvisor
+	// IsInConflictWithMMA is called by the allocator sync to determine if the
+	// given candidate is in conflict with the existing store.
+	IsInConflictWithMMA(cand roachpb.StoreID, advisor mmaprototype.MMARebalanceAdvisor, cpuOnly bool) bool
 }
 
 // TODO(wenyihu6): make sure allocator sync can tolerate cluster setting
