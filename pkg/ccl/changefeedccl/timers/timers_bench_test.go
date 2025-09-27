f186c869fa4d47de84e25c59781511bfccdc7643
Steven Danna <danna@cockroachlabs.com>
2025-09-27 22:17:33 +0100

    changefeedccl: avoid heap allocation in timers
    Unfortunately, the closure from Start() seemed to always be heap
    allocated. While minor, it was enough to spot when reading
    rangefeed-related memory profiles.
    
    ```
    BenchmarkTimerStart/closure-based                8992018               128.1 ns/op            24 B/op          1 allocs/op
    BenchmarkTimerStart/handle-based                12124416                98.81 ns/op            0 B/op          0 allocs/op
    ```
    
    Epic: none
    Release note: None

---
+++ pkg/ccl/changefeedccl/timers/timers_bench_test.go
@@ -0,0 +1,39 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package timers
+
+import (
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/util/metric"
+	"github.com/cockroachdb/cockroach/pkg/util/metric/aggmetric"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func BenchmarkTimerStart(b *testing.B) {
+	histOpts := metric.HistogramOptions{
+		Metadata: metric.Metadata{
+			Name:        "test.timer",
+			Help:        "Test timer",
+			Unit:        metric.Unit_NANOSECONDS,
+			Measurement: "Latency",
+		},
+		Duration: time.Hour,
+		Buckets:  prometheus.ExponentialBucketsRange(float64(1*time.Microsecond), float64(1*time.Hour), 60),
+		Mode:     metric.HistogramModePrometheus,
+	}
+	builder := aggmetric.MakeBuilder("scope")
+	hist := builder.Histogram(histOpts).AddChild("test")
+	timer := &timer{hist: hist}
+
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		handle := timer.Start()
+		_ = handle.End()
+	}
+
+}
