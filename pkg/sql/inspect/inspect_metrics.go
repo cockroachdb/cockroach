04b2de9a80fd9a7fdd42dca151ef04fc92fcc1e1
Matt Spilchen <matt.spilchen@cockroachlabs.com>
2025-10-02 09:09:21 -0300

    sql/inspect: add job metrics for INSPECT jobs
    Implements three metrics for INSPECT jobs:
    - jobs.inspect.runs: Total number of INSPECT job executions
    - jobs.inspect.runs_with_issues: Count of jobs that found
      inconsistencies
    - jobs.inspect.issues_found: Aggregate count of issues found across all
      processors
    
    Closes #153884
    
    Epic: CRDB-30356
    Release note: none

---
+++ pkg/sql/inspect/inspect_metrics.go
@@ -0,0 +1,59 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package inspect
+
+import (
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/jobs"
+	"github.com/cockroachdb/cockroach/pkg/util/metric"
+)
+
+// InspectMetrics holds metrics for INSPECT job operations.
+type InspectMetrics struct {
+	Runs           *metric.Counter
+	RunsWithIssues *metric.Counter
+	IssuesFound    *metric.Counter
+}
+
+var _ metric.Struct = (*InspectMetrics)(nil)
+
+// MetricStruct implements the metric.Struct interface.
+func (InspectMetrics) MetricStruct() {}
+
+var (
+	metaInspectRuns = metric.Metadata{
+		Name:        "jobs.inspect.runs",
+		Help:        "Number of INSPECT jobs executed",
+		Measurement: "Jobs",
+		Unit:        metric.Unit_COUNT,
+	}
+	metaInspectRunsWithIssues = metric.Metadata{
+		Name:        "jobs.inspect.runs_with_issues",
+		Help:        "Number of INSPECT jobs that found at least one issue",
+		Measurement: "Jobs",
+		Unit:        metric.Unit_COUNT,
+	}
+	metaInspectIssuesFound = metric.Metadata{
+		Name:        "jobs.inspect.issues_found",
+		Help:        "Total count of issues found by INSPECT jobs",
+		Measurement: "Issues",
+		Unit:        metric.Unit_COUNT,
+	}
+)
+
+// MakeInspectMetrics instantiates the metrics for INSPECT jobs.
+func MakeInspectMetrics(histogramWindow time.Duration) metric.Struct {
+	return &InspectMetrics{
+		Runs:           metric.NewCounter(metaInspectRuns),
+		RunsWithIssues: metric.NewCounter(metaInspectRunsWithIssues),
+		IssuesFound:    metric.NewCounter(metaInspectIssuesFound),
+	}
+}
+
+func init() {
+	jobs.MakeInspectMetricsHook = MakeInspectMetrics
+}
