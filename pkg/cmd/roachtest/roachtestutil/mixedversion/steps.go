288412ac1c91adaa46e2a8526bf4905dcc61c4a3
DarrylWong <darryl@cockroachlabs.com>
2025-10-07 17:27:05 -0400

    mixedversion: add protectedNodePartition mutator
    This mutator designates a quorum of nodes to be protected.
    These nodes will never be partitioned from each other and
    will have voter replicas pinned to them for all ranges. This
    means we can create partitions on any number of other nodes
    and still maintain availability as long as we connect to the
    protected nodes.

--- pkg/cmd/roachtest/roachtestutil/mixedversion/steps.go
+++ pkg/cmd/roachtest/roachtestutil/mixedversion/steps.go
@@ -1070,7 +1070,7 @@ func (s pinVoterReplicasStep) Run(
 		`, nodeFilter)
 
 		var totalRanges, rangesWithAllVoters int
-		row := h.QueryRow(rng, query)
+		row := h.System.QueryRow(rng, query)
 		if err := row.Scan(&totalRanges, &rangesWithAllVoters); err != nil {
 			return errors.Wrap(err, "failed to query voter replicas")
 		}
