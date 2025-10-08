862b5b30e07061bce51b1ff2a2471986055db80e
Kyle Wong <37189875+kyle-a-wong@users.noreply.github.com>
2025-10-08 16:53:33 -0400

    Revert "ui: fix statement activity timepicker for sub-hour ranges"
    This reverts commit a4449871729ef7d04d032a4733f4995f74f4e1c6.
    
    This PR was intended to allow the sql activity timepicker to support
    sub 1-hour time ranges. Previously, any time range selected, whether
    a preset or an explicit range, always rounded to the top of the previous
    hour. The commit being reverted made changes to some of the components
    that rounded the time, but not all of them. It also introduced a new
    type of bug that could potentially cause the sql activity page to
    miss expected sql activity statistics.
    
    Due to the latter bug that was mentioned, we are opting to revert
    this change as its sql stats aggregation buckets are almost always
    set to 1 hour and is not a public setting we advise for people to
    change.
    
    Fixes: https://cockroachlabs.atlassian.net/browse/CC-33789
    Epic: CC-33582
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/statementsPage/statementsPage.tsx
+++ pkg/ui/workspaces/cluster-ui/src/statementsPage/statementsPage.tsx
@@ -88,7 +88,7 @@ import {
   getValidOption,
   TimeScale,
   timeScale1hMinOptions,
-  toDateRange,
+  toRoundedDateRange,
 } from "../timeScaleDropdown";
 import timeScaleStyles from "../timeScaleDropdown/timeScale.module.scss";
 
@@ -175,7 +175,7 @@ type RequestParams = Pick<
 >;
 
 function stmtsRequestFromParams(params: RequestParams): StatementsRequest {
-  const [start, end] = toDateRange(params.timeScale);
+  const [start, end] = toRoundedDateRange(params.timeScale);
   return createCombinedStmtsRequest({
     start,
     end,
