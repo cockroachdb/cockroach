dda06f850b892e0a2912e8493b7603b5e764b09f
Kevin Cao <39608887+kev-cao@users.noreply.github.com>
2025-10-14 13:41:26 -0400

    jobs: convert max adoptions per period to cluster setting
    Previously, tuning the maximum number of adoptions per adoption period
    was configured via the `COCKROACH_JOB_ADOPTIONS_PER_PERIOD` environment
    variable. This made configuring the maximum number of adoptions somewhat
    tedious as it required a restart of the Cockroach process to refresh the
    setting. This commit converts the setting from an environment variable
    to a cluster setting.
    
    Informs: #146799
    
    Release note: Add `jobs.registry.max_adoptions_per_loop` cluster setting
    to configure maximum number of jobs a node can adopt per adoption loop.

--- pkg/jobs/registry.go
+++ pkg/jobs/registry.go
@@ -30,7 +30,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlliveness"
 	"github.com/cockroachdb/cockroach/pkg/util/cidr"
-	"github.com/cockroachdb/cockroach/pkg/util/envutil"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/metric"
@@ -885,9 +884,6 @@ func (r *Registry) LoadJobWithTxn(
 	}
 	return j, nil
 }
-
-// TODO (sajjad): make maxAdoptionsPerLoop a cluster setting.
-var maxAdoptionsPerLoop = envutil.EnvOrDefaultInt(`COCKROACH_JOB_ADOPTIONS_PER_PERIOD`, 10)
 
 const removeClaimsForDeadSessionsQuery = `
 UPDATE system.jobs
