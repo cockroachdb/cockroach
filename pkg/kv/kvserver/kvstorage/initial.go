ec07dabaff82d91c00ecd35b57dbc990a9e64cc8
Pavel Kalinnikov <pavel@cockroachlabs.com>
2025-10-17 03:23:31 +0100

    kvstorage: use StateRW in WriteInitialReplicaState
    Epic: none
    Release note: none

--- pkg/kv/kvserver/kvstorage/initial.go
+++ pkg/kv/kvserver/kvstorage/initial.go
@@ -50,7 +50,7 @@ const (
 // are returned.
 func WriteInitialReplicaState(
 	ctx context.Context,
-	readWriter storage.ReadWriter,
+	stateRW StateRW,
 	ms enginepb.MVCCStats,
 	desc roachpb.RangeDescriptor,
 	lease roachpb.Lease,
@@ -75,31 +75,31 @@ func WriteInitialReplicaState(
 	}
 
 	rsl := MakeStateLoader(desc.RangeID)
-	if existingLease, err := rsl.LoadLease(ctx, readWriter); err != nil {
+	if existingLease, err := rsl.LoadLease(ctx, stateRW); err != nil {
 		return enginepb.MVCCStats{}, errors.Wrap(err, "error reading lease")
 	} else if (existingLease != roachpb.Lease{}) {
 		log.KvExec.Fatalf(ctx, "expected trivial lease, but found %+v", existingLease)
 	}
 
-	if existingGCThreshold, err := rsl.LoadGCThreshold(ctx, readWriter); err != nil {
+	if existingGCThreshold, err := rsl.LoadGCThreshold(ctx, stateRW); err != nil {
 		return enginepb.MVCCStats{}, errors.Wrap(err, "error reading GCThreshold")
 	} else if !existingGCThreshold.IsEmpty() {
 		log.KvExec.Fatalf(ctx, "expected trivial GCthreshold, but found %+v", existingGCThreshold)
 	}
 
-	if existingGCHint, err := rsl.LoadGCHint(ctx, readWriter); err != nil {
+	if existingGCHint, err := rsl.LoadGCHint(ctx, stateRW); err != nil {
 		return enginepb.MVCCStats{}, errors.Wrap(err, "error reading GCHint")
 	} else if !existingGCHint.IsEmpty() {
 		return enginepb.MVCCStats{}, errors.AssertionFailedf("expected trivial GCHint, but found %+v", existingGCHint)
 	}
 
-	if existingVersion, err := rsl.LoadVersion(ctx, readWriter); err != nil {
+	if existingVersion, err := rsl.LoadVersion(ctx, stateRW); err != nil {
 		return enginepb.MVCCStats{}, errors.Wrap(err, "error reading Version")
 	} else if (existingVersion != roachpb.Version{}) {
 		log.KvExec.Fatalf(ctx, "expected trivial version, but found %+v", existingVersion)
 	}
 
-	newMS, err := rsl.Save(ctx, readWriter, s)
+	newMS, err := rsl.Save(ctx, stateRW, s)
 	if err != nil {
 		return enginepb.MVCCStats{}, err
 	}
