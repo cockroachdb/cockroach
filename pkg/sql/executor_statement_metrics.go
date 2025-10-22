be3f050b3bc96416d3eab634c77bc4893056f6bf
Andrew Kimball <andyk@cockroachlabs.com>
2025-10-21 17:21:36 -0700

    sql: add bytes_read metric
    Add new `sql.statements.bytes_read.count` metric that counts the number of
    bytes scanned by SQL statements. This is the same value that's collected by
    SQL stats for each statement and transaction, except in aggregated metric
    form.
    
    Epic: AOP-30
    
    Release note (sql change): Added sql.statements.bytes_read.count metric that
    counts the number of bytes scanned by SQL statements.

--- pkg/sql/executor_statement_metrics.go
+++ pkg/sql/executor_statement_metrics.go
@@ -88,6 +88,10 @@ type EngineMetrics struct {
 	// primary and secondary indexes. Note that some secondary indexes can have
 	// multiple index rows per primary index row (e.g. inverted and vector).
 	StatementRowsRead *metric.Counter
+
+	// StatementBytesRead counts the number of bytes scanned by SQL statements
+	// from primary and secondary indexes.
+	StatementBytesRead *metric.Counter
 }
 
 // EngineMetrics implements the metric.Struct interface.
@@ -205,6 +209,7 @@ func (ex *connExecutor) recordStatementSummary(
 
 	// Update SQL statement metrics.
 	ex.metrics.EngineMetrics.StatementRowsRead.Inc(stats.rowsRead)
+	ex.metrics.EngineMetrics.StatementBytesRead.Inc(stats.bytesRead)
 
 	recordedStmtStats := &sqlstats.RecordedStmtStats{
 		FingerprintID:        stmtFingerprintID,
