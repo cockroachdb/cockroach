64f9dad654c1ab76c605656184c3e69644b34cc9
Fabian Schöndorff <fabian.schoendorff@googlemail.com>
2025-10-26 17:31:28 +0100

    changefeedccl: changefeed-specific option for num of sink workers
    Previously, the number of SinkIOWorkers could only be defined in
    cluster-wide setting.
    
    This patch allows to set the number of SinkIOWorkers per changefeed.
    
    If both (cluster-wide, per-changefeed) values are given, the min of the
    two will be used.
    
    Release note (sql change): The CREATE CHANGEFEED statement was extended
    with an optional `num_sink_workers` setting that can be used to set the
    number of SinkIOWorkers per changefeed. Note that the number of workers
    is capped by the cluster-wide setting if present.
    
    Fixes: #154546

--- pkg/ccl/changefeedccl/sink_test.go
+++ pkg/ccl/changefeedccl/sink_test.go
@@ -9,6 +9,7 @@ import (
 	"context"
 	"fmt"
 	"net/url"
+	"runtime"
 	"strconv"
 	"strings"
 	"sync"
@@ -22,6 +23,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/ccl/changefeedccl/kvevent"
 	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
 	"github.com/cockroachdb/cockroach/pkg/security/username"
+	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog/tabledesc"
 	"github.com/cockroachdb/cockroach/pkg/testutils/pgurlutils"
@@ -1013,4 +1015,105 @@ func TestChangefeedConsistentPartitioning(t *testing.T) {
 		require.Equal(t, expected, actual)
 	}
 
+}
+
+func TestNumSinkIOWorkers(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	ctx := context.Background()
+	s := serverutils.StartServerOnly(t, base.TestServerArgs{})
+	defer s.Stopper().Stop(ctx)
+
+	execCfg := s.ExecutorConfig().(sql.ExecutorConfig)
+
+	tests := []struct {
+		name             string
+		clusterSetting   int64
+		changefeedOption string
+		expectedWorkers  int
+		expectError      bool
+	}{
+		{
+			name:             "both zero - use default",
+			clusterSetting:   0,
+			changefeedOption: "0",
+			expectedWorkers:  runtime.GOMAXPROCS(0),
+		},
+		{
+			name:             "cluster positive, option not set - use cluster",
+			clusterSetting:   10,
+			changefeedOption: "",
+			expectedWorkers:  10,
+		},
+		{
+			name:             "cluster zero, option positive - use option",
+			clusterSetting:   0,
+			changefeedOption: "15",
+			expectedWorkers:  15,
+		},
+		{
+			name:             "both positive, option smaller - use option",
+			clusterSetting:   10,
+			changefeedOption: "5",
+			expectedWorkers:  5,
+		},
+		{
+			name:             "both positive, cluster smaller - use cluster",
+			clusterSetting:   10,
+			changefeedOption: "15",
+			expectedWorkers:  10,
+		},
+		{
+			name:             "cluster negative - use default",
+			clusterSetting:   -1,
+			changefeedOption: "",
+			expectedWorkers:  runtime.GOMAXPROCS(0),
+		},
+		{
+			name:             "option negative - use default",
+			clusterSetting:   0,
+			changefeedOption: "-1",
+			expectedWorkers:  runtime.GOMAXPROCS(0),
+		},
+		{
+			name:             "both negative - use default",
+			clusterSetting:   -1,
+			changefeedOption: "-1",
+			expectedWorkers:  runtime.GOMAXPROCS(0),
+		},
+		{
+			name:             "option not set, cluster zero - use default",
+			clusterSetting:   0,
+			changefeedOption: "",
+			expectedWorkers:  runtime.GOMAXPROCS(0),
+		},
+		{
+			name:             "invalid changefeed option returns error",
+			clusterSetting:   10,
+			changefeedOption: "invalid",
+			expectError:      true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			changefeedbase.SinkIOWorkers.Override(ctx, &execCfg.Settings.SV, test.clusterSetting)
+
+			optsMap := make(map[string]string)
+			if test.changefeedOption != "" {
+				optsMap[changefeedbase.OptNumSinkWorkers] = test.changefeedOption
+			}
+			opts := changefeedbase.MakeStatementOptions(optsMap)
+
+			result, err := numSinkIOWorkers(&execCfg.DistSQLSrv.ServerConfig, opts)
+
+			if test.expectError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, test.expectedWorkers, result)
+			}
+		})
+	}
 }
