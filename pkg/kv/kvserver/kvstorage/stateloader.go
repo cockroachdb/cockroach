dbc0ee19f2cc2e0b88ab2bec7e05ea3cb782a6bb
Pavel Kalinnikov <pavel@cockroachlabs.com>
2025-10-31 20:08:37 +0000

    kvstorage: rm Load{RangeTombstone,RaftReplicaID}
    There are no more users of these methods. Going forward, LoadReplicaMark
    must be used instead.
    
    Epic: none
    Release note: none

--- pkg/kv/kvserver/kvstorage/stateloader.go
+++ pkg/kv/kvserver/kvstorage/stateloader.go
@@ -320,22 +320,6 @@ func (s StateLoader) SetForceFlushIndex(
 		hlc.Timestamp{}, ffIndex, storage.MVCCWriteOptions{Stats: ms})
 }
 
-// LoadRaftReplicaID loads the RaftReplicaID.
-func (s StateLoader) LoadRaftReplicaID(
-	ctx context.Context, stateRO StateRO,
-) (kvserverpb.RaftReplicaID, error) {
-	var replicaID kvserverpb.RaftReplicaID
-	if found, err := storage.MVCCGetProto(
-		ctx, stateRO, s.RaftReplicaIDKey(), hlc.Timestamp{}, &replicaID,
-		storage.MVCCGetOptions{ReadCategory: fs.ReplicationReadCategory},
-	); err != nil {
-		return kvserverpb.RaftReplicaID{}, err
-	} else if !found {
-		return kvserverpb.RaftReplicaID{}, errors.AssertionFailedf("no replicaID persisted")
-	}
-	return replicaID, nil
-}
-
 // SetRaftReplicaID overwrites the RaftReplicaID.
 func (s StateLoader) SetRaftReplicaID(
 	ctx context.Context, stateWO StateWO, replicaID roachpb.ReplicaID,
@@ -373,22 +357,6 @@ func (s StateLoader) LoadReplicaMark(ctx context.Context, stateRO StateRO) (Repl
 		return ReplicaMark{}, err
 	}
 	return mark, mark.check()
-}
-
-// LoadRangeTombstone loads the RangeTombstone of the range.
-func (s StateLoader) LoadRangeTombstone(
-	ctx context.Context, stateRO StateRO,
-) (kvserverpb.RangeTombstone, error) {
-	var ts kvserverpb.RangeTombstone
-	if ok, err := storage.MVCCGetProto(
-		ctx, stateRO, s.RangeTombstoneKey(), hlc.Timestamp{}, &ts, storage.MVCCGetOptions{},
-	); err != nil || !ok {
-		// NB: when err == nil && !ok, there is no RangeTombstone. It is valid to
-		// return RangeTombstone{} with a zero NextReplicaID, signifying that there
-		// hasn't been a single replica removed for the RangeID.
-		return kvserverpb.RangeTombstone{}, err
-	}
-	return ts, nil
 }
 
 // SetRangeTombstone writes the RangeTombstone.
