0c6289b594ab1d7c688e950dc33d1e8688f56cb2
Steven Danna <danna@cockroachlabs.com>
2025-11-01 23:13:46 +0000

    batcheval: return unmodified staging txn in IndeterminateCommitError
    When a EndTxn(abort) enouncouters a transaction record in STAGING, it returns
    an IndeterminateCommitError so that the transaction recovery process can be
    started. When constructing this error, we previously returned a copy of the
    staging transaction that had its write timestamp forwarded the txn found on
    the request header.
    
    According to this comment, this is rather important.
    
    	// We're using existingTxn on the reply, although it can be stale
    	// compared to the Transaction in the request (e.g. the Sequence,
    	// and various timestamps). We must be careful to update it with the
    	// supplied ba.Txn if we return it with an error which might be
    	// retried, as for example to avoid client-side serializable restart.
    
    However, a side-effect is that the txnrecovery performed in response to an
    IndeterminateCommitError could erroneously find that the the transaction was
    commited because it was checking for the implicit commit criteria using a higher
    timestamp. The actual recovery of the transaction would then hit the following
    assertion:
    
        programming error: timestamp change by implicitly committed transaction
    
    Here, we solve this by returning the unmodified txn in an IndeterminateCommitError.
    While the comment warns us against this, I think in the case of here, this transaction
    has nothing (other than the EndTxn) to retry since we are rolling back. Further, the
    error is handled server-side immediately and then the user receives either the error
    from the recovery process or the error from the retried request.
    
    Fixes #156698
    
    Release note (bug fix): Fix a bug that could result in a transaction encountering
    the following assertion failure:
    
       failed indeterminate commit recovery: programming error: timestamp change by
       implicitly committed transaction

--- pkg/kv/kvserver/batcheval/cmd_end_transaction.go
+++ pkg/kv/kvserver/batcheval/cmd_end_transaction.go
@@ -305,7 +305,7 @@ func EndTxn(
 		// and various timestamps). We must be careful to update it with the
 		// supplied ba.Txn if we return it with an error which might be
 		// retried, as for example to avoid client-side serializable restart.
-		reply.Txn = &existingTxn
+		reply.Txn = existingTxn.Clone()
 
 		// Verify that we can either commit it or abort it (according
 		// to args.Commit), and also that the Timestamp and Epoch have
@@ -498,7 +498,22 @@ func EndTxn(
 		// committed. Doing so is only possible if we can guarantee that under no
 		// circumstances can an implicitly committed transaction be rolled back.
 		if reply.Txn.Status == roachpb.STAGING {
-			err := kvpb.NewIndeterminateCommitError(*reply.Txn)
+			// Note that reply.Txn has been updated with the Txn from the request
+			// header. But, the transaction might have been pushed since it was
+			// written. In fact, the transaction from the request header might
+			// actually be in a state that _would have_ been implicitly committed IF
+			// it had been able to write a transaction record with this new state.
+			// However, here we are aborting the transaction and don't want to end up
+			// erroneously committing it during the transaction recovery that is about
+			// to follow.
+			var txnForError roachpb.Transaction
+			if recordAlreadyExisted {
+				txnForError = existingTxn
+			} else {
+				txnForError = *reply.Txn
+			}
+
+			err := kvpb.NewIndeterminateCommitError(txnForError)
 			log.VEventf(ctx, 1, "%v", err)
 			return result.Result{}, err
 		}
