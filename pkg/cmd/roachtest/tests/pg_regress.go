4fdcd7a14fdb9464c97e56138a091632d4cd1f4b
DarrylWong <darryl@cockroachlabs.com>
2025-11-03 12:25:09 -0500

    roachprod: allow opting out of allow_unsafe_internals
    We sometimes need to construct pgurls for psql or postgres dbs
    which don't support this CRDB only parameter.

--- pkg/cmd/roachtest/tests/pg_regress.go
+++ pkg/cmd/roachtest/tests/pg_regress.go
@@ -110,7 +110,7 @@ func initPGRegress(ctx context.Context, t test.Test, c cluster.Cluster) {
 		}
 	}
 
-	urls, err := c.InternalPGUrl(ctx, t.L(), node, roachprod.PGURLOptions{})
+	urls, err := c.InternalPGUrl(ctx, t.L(), node, roachprod.PGURLOptions{DisallowUnsafeInternals: true})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -149,7 +149,8 @@ func runPGRegress(ctx context.Context, t test.Test, c cluster.Cluster) {
 	initPGRegress(ctx, t, c)
 
 	node := c.Node(1)
-	urls, err := c.InternalPGUrl(ctx, t.L(), node, roachprod.PGURLOptions{})
+	// psql doesn't support the CRDB specific "allow_unsafe_internals" parameter.
+	urls, err := c.InternalPGUrl(ctx, t.L(), node, roachprod.PGURLOptions{DisallowUnsafeInternals: true})
 	if err != nil {
 		t.Fatal(err)
 	}
