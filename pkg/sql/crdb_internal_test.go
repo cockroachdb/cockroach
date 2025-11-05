470aff4b8571162a5c496d2f216c82d036492707
Brian Dillmann <brian.dillmann@cockroachlabs.com>
2025-11-05 13:17:38 -0500

    sql: add index_usage_stats and datums_to_bytes to information_schema
    As part of the stable sql api effort, we're aiming to expose all
    "supported" internals to the information_schema. So that teams have an
    examplar, we here move index_usage_stats and datums_to_bytes both to
    information_schema, providing a foundation for future exposure of system
    internals.
    
    Fixes: #156677
    Epic: CRDB-56107
    Release note (sql change): Duplicates crdb_internal.index_usage_stats and
    crdb_internal.datums_to_bytes in information_schema
    (information_schema.crdb_index_usage_stats and
    information_schema.crdb_datums_to_bytes respectively).

--- pkg/sql/crdb_internal_test.go
+++ pkg/sql/crdb_internal_test.go
@@ -1834,3 +1834,49 @@ func TestMVCCValueHeaderSystemColumns(t *testing.T) {
 		})
 	}
 }
+
+// TestSupportedCRDBInternalTablesNotChanged verifies that the
+// SupportedCRDBInternalTables map has not changed from its expected values.
+// This test ensures no tables are inadvertently added to this locked list.
+func TestSupportedCRDBInternalTablesNotChanged(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	// Hardcoded expected values for SupportedCRDBInternalTables
+	// IMPORTANT: This list is LOCKED and should NOT be modified.
+	// New tables should be added to information_schema instead.
+	expectedTables := map[string]struct{}{
+		`cluster_contended_indexes`:     {},
+		`cluster_contended_keys`:        {},
+		`cluster_contended_tables`:      {},
+		`cluster_contention_events`:     {},
+		`cluster_locks`:                 {},
+		`cluster_queries`:               {},
+		`cluster_sessions`:              {},
+		`cluster_transactions`:          {},
+		`index_usage_statistics`:        {},
+		`statement_statistics`:          {},
+		`transaction_contention_events`: {},
+		`transaction_statistics`:        {},
+		`zones`:                         {},
+	}
+
+	// Check that the actual map matches the expected map
+	if len(sql.SupportedCRDBInternalTables) != len(expectedTables) {
+		t.Fatalf("FAILURE: SupportedCRDBInternalTables has been modified!\n"+
+			"This list is LOCKED and should NOT be changed.\n"+
+			"New crdb_internal tables should be added to information_schema instead.\n"+
+			"Expected %d tables, but found %d tables.\n"+
+			"See: https://docs.google.com/document/d/1STbb8bljTzK_jXRIJrxtijWsPhGErdH1vZdunzPwXvs/edit",
+			len(expectedTables), len(sql.SupportedCRDBInternalTables))
+	}
+
+	// Check each expected table is present
+	for table := range expectedTables {
+		if _, ok := sql.SupportedCRDBInternalTables[table]; !ok {
+			t.Fatalf("FAILURE: SupportedCRDBInternalTables has been modified!\n" +
+				"This list is LOCKED and should NOT be changed.\n" +
+				"New crdb_internal tables should be added to information_schema instead.\n" +
+				"See: https://docs.google.com/document/d/1STbb8bljTzK_jXRIJrxtijWsPhGErdH1vZdunzPwXvs/edit")
+		}
+	}
+}
--- pkg/sql/sem/builtins/builtins.go
+++ pkg/sql/sem/builtins/builtins.go
@@ -4748,37 +4748,8 @@ value if you rely on the HLC for accuracy.`,
 			Volatility: volatility.Volatile,
 		}),
 
-	"crdb_internal.datums_to_bytes": makeBuiltin(
-		tree.FunctionProperties{
-			Category:             builtinconstants.CategorySystemInfo,
-			Undocumented:         true,
-			CompositeInsensitive: true,
-		},
-		tree.Overload{
-			// Note that datums_to_bytes(a) == datums_to_bytes(b) iff (a IS NOT DISTINCT FROM b)
-			Info: "Converts datums into key-encoded bytes. " +
-				"Supports NULLs and all data types which may be used in index keys",
-			Types:      tree.VariadicType{VarType: types.Any},
-			ReturnType: tree.FixedReturnType(types.Bytes),
-			Fn: func(_ context.Context, _ *eval.Context, args tree.Datums) (tree.Datum, error) {
-				var out []byte
-				for i, arg := range args {
-					var err error
-					out, err = keyside.Encode(out, arg, encoding.Ascending)
-					if err != nil {
-						return nil, pgerror.Newf(
-							pgcode.DatatypeMismatch,
-							"illegal argument %d of type %s",
-							i, arg.ResolvedType(),
-						)
-					}
-				}
-				return tree.NewDBytes(tree.DBytes(out)), nil
-			},
-			Volatility:        volatility.Immutable,
-			CalledOnNullInput: true,
-		},
-	),
+	"crdb_internal.datums_to_bytes":           datumsToBytes,
+	"information_schema.crdb_datums_to_bytes": datumsToBytes,
 	"crdb_internal.merge_statement_stats": makeBuiltin(arrayProps(),
 		tree.Overload{
 			Types:      tree.ParamTypes{{Name: "input", Typ: types.JSONBArray}},
@@ -12890,5 +12861,37 @@ func exprSliceToStrSlice(exprs []tree.Expr) []string {
 		return tree.AsStringWithFlags(expr, tree.FmtBareStrings)
 	})
 }
+
+var datumsToBytes = makeBuiltin(
+	tree.FunctionProperties{
+		Category:             builtinconstants.CategorySystemInfo,
+		Undocumented:         true,
+		CompositeInsensitive: true,
+	},
+	tree.Overload{
+		// Note that datums_to_bytes(a) == datums_to_bytes(b) iff (a IS NOT DISTINCT FROM b)
+		Info: "Converts datums into key-encoded bytes. " +
+			"Supports NULLs and all data types which may be used in index keys",
+		Types:      tree.VariadicType{VarType: types.Any},
+		ReturnType: tree.FixedReturnType(types.Bytes),
+		Fn: func(_ context.Context, _ *eval.Context, args tree.Datums) (tree.Datum, error) {
+			var out []byte
+			for i, arg := range args {
+				var err error
+				out, err = keyside.Encode(out, arg, encoding.Ascending)
+				if err != nil {
+					return nil, pgerror.Newf(
+						pgcode.DatatypeMismatch,
+						"illegal argument %d of type %s",
+						i, arg.ResolvedType(),
+					)
+				}
+			}
+			return tree.NewDBytes(tree.DBytes(out)), nil
+		},
+		Volatility:        volatility.Immutable,
+		CalledOnNullInput: true,
+	},
+)
 
 var nilRegionsError = errors.AssertionFailedf("evalCtx.Regions is nil")
