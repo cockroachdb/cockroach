62beeaba80a039d1c3389e6ddc472c53fe2c8822
Pavel Kalinnikov <pavel@cockroachlabs.com>
2025-11-11 12:47:01 +0000

    sql: enable multi-tenancy in TestReacquireLeaseOnRestart
    Epic: none
    Release note: none

--- pkg/sql/txn_restart_test.go
+++ pkg/sql/txn_restart_test.go
@@ -1315,7 +1315,6 @@ func TestReacquireLeaseOnRestart(t *testing.T) {
 
 	params.Knobs.Store = storeTestingKnobs
 	params.Knobs.KVClient = clientTestingKnobs
-	params.DefaultTestTenant = base.TestDoesNotWorkWithExternalProcessMode(156333)
 	srv, sqlDB, _ := serverutils.StartServer(t, params)
 	defer srv.Stopper().Stop(context.Background())
 	s = srv.ApplicationLayer()
