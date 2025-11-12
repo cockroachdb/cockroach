f88078d8e2d75fbe6bfe57c5aeb5aeb2afef7d7e
Matt Spilchen <matt.spilchen@cockroachlabs.com>
2025-11-12 10:02:45 -0400

    partition validation for ADD PARTITION

--- pkg/sql/schemachanger/scbuild/internal/scbuildstmt/alter_table_add_partition.go
+++ pkg/sql/schemachanger/scbuild/internal/scbuildstmt/alter_table_add_partition.go
@@ -6,6 +6,9 @@
 package scbuildstmt
 
 import (
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/tabledesc"
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgcode"
+	"github.com/cockroachdb/cockroach/pkg/sql/pgwire/pgerror"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 )
@@ -17,5 +20,26 @@ func alterTableAddPartition(
 	stmt tree.Statement,
 	t *tree.AlterTableAddPartition,
 ) {
-	// No-op: Future implementation will handle adding partitions to range-partitioned tables.
+	// Get the primary index for the table.
+	primaryIndexID := getCurrentPrimaryIndexID(b, tbl.TableID)
+
+	// Query for existing partitions on the primary index and check for duplicates.
+	// Use Normalize() for case-insensitive comparison.
+	newPartitionName := t.Partition.Name.Normalize()
+	b.QueryByID(tbl.TableID).FilterIndexPartitioning().ForEach(
+		func(_ scpb.Status, _ scpb.TargetStatus, e *scpb.IndexPartitioning) {
+			if e.IndexID != primaryIndexID {
+				return
+			}
+			// Check for duplicate partition names.
+			partition := tabledesc.NewPartitioning(&e.PartitioningDescriptor)
+			_ = partition.ForEachPartitionName(func(name string) error {
+				if name == newPartitionName {
+					panic(pgerror.Newf(pgcode.InvalidObjectDefinition,
+						"PARTITION %s: name must be unique (used twice in index %q)",
+						t.Partition.Name, "primary"))
+				}
+				return nil
+			})
+		})
 }
