00c32bbfa4b075b59d02688b99b723672451c833
sumeerbhola <sumeer@cockroachlabs.com>
2025-11-18 15:55:15 -0500

    mma: eliminate pendingChangeNoRollback
    Instead, the ReplicaChange.prev field is updated to reflect the latest
    state reported by the leaseholder.
    
    In addition to simplifyin the code, it fixes an existing issue where an
    undo could rollback to a state preceding the latest leaseholder state.
    
    Informs #157049
    
    Epic: CRDB-55052
    
    Release note: None

--- pkg/kv/kvserver/allocator/mmaprototype/allocator_state.go
+++ pkg/kv/kvserver/allocator/mmaprototype/allocator_state.go
@@ -257,16 +257,12 @@ func (a *allocatorState) ProcessStoreLoadMsg(ctx context.Context, msg *StoreLoad
 func (a *allocatorState) AdjustPendingChangeDisposition(change ExternalRangeChange, success bool) {
 	a.mu.Lock()
 	defer a.mu.Unlock()
-	rs, ok := a.cs.ranges[change.RangeID]
+	_, ok := a.cs.ranges[change.RangeID]
 	if !ok {
 		// Range no longer exists. This can happen if the StoreLeaseholderMsg
 		// which included the effect of the change that transferred the lease away
 		// was already processed, causing the range to no longer be tracked by the
 		// allocator.
-		return
-	}
-	if !success && rs.pendingChangeNoRollback {
-		// Not allowed to undo.
 		return
 	}
 	// NB: It is possible that some of the changes have already been enacted via
