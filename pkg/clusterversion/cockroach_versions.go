87df892a46302627b7f1c6cfb608e2e82fce8aa9
Celia La <celia@cockroachlabs.com>
2025-11-20 14:14:35 -0800

    clusterversion, storage: bump MinSupported from v25.3 to v25.4
    Part of the quarterly M.4 "Bump MinSupported" task as outlined in
    `pkg/clusterversion/README.md`.
    
    This commit updates the MinSupported constant from V25_3 to V25_4,
    along with all code that references the MinSupported version.
    
    After this change, clusters running v25.3 can no longer connect to
    clusters running master, and direct upgrades from v25.3 to master are
    no longer supported.
    
    Changes include updates to:
    - MinSupported constant in pkg/clusterversion/cockroach_versions.go
    - Storage layer: MinimumSupportedFormatVersion updated to FormatV2BlobFiles
    
    Note: pebbleFormatVersionMap is NOT modified in this commit. V25_3 will
    be removed from the map in Commit 4 (when removing local-mixed-25.3
    configuration), following the pattern from reference PR #157767.
    
    Release note: None

--- pkg/clusterversion/cockroach_versions.go
+++ pkg/clusterversion/cockroach_versions.go
@@ -322,7 +322,7 @@ var versionTable = [numKeys]roachpb.Version{
 const Latest Key = numKeys - 1
 
 // MinSupported is the minimum logical cluster version supported by this branch.
-const MinSupported Key = V25_3
+const MinSupported Key = V25_4
 
 // PreviousRelease is the logical cluster version of the previous release (which must
 // have at least an RC build published).
