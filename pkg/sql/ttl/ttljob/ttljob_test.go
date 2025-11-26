5dd220efea188beb56ed3cd5aa974422ed863b6e
Nukitt <nukit.tailor@cockroachlabs.com>
2025-11-26 23:23:16 +0530

    sql/ttl/ttljob: disable DRPC for `TestRowLevelTTLJobMultipleNodes`
    Disabling DRPC in `TestRowLevelTTLJobMultipleNodes` test as its failing
    randomly when its enabled. Inconsistently reproducible(~2/3 runs it fails).
    
    Epic: None
    Fixes: #158317
    Release note: none

--- pkg/sql/ttl/ttljob/ttljob_test.go
+++ pkg/sql/ttl/ttljob/ttljob_test.go
@@ -128,6 +128,7 @@ func newRowLevelTTLTestJobTestHelper(
 			Settings:          makeSettings(),
 			Knobs:             baseTestingKnobs,
 			InsecureWebAccess: true,
+			DefaultDRPCOption: base.TestDRPCDisabled,
 		},
 	})
 	th.testCluster = testCluster
@@ -593,7 +594,6 @@ INSERT INTO t (id, crdb_internal_expiration) VALUES (1, now() - '1 month'), (2, 
 func TestRowLevelTTLJobMultipleNodes(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
-	skip.WithIssue(t, 158317)
 
 	skip.UnderRace(t, "the test times out under race")
 
