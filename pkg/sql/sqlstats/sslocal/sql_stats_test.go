5132652a97381b9e51a134e4b749907cbda4a5c8
David Hartunian <davidh@cockroachlabs.com>
2025-12-08 16:30:29 -0500

    sqlstats: skip TestSQLStatsDiscardStatsOnFingerprintLimit under race
    Resolves: #158978
    Epic: None
    Release note: None

--- pkg/sql/sqlstats/sslocal/sql_stats_test.go
+++ pkg/sql/sqlstats/sslocal/sql_stats_test.go
@@ -35,6 +35,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlstats/ssmemstorage"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/util"
 	"github.com/cockroachdb/cockroach/pkg/util/ctxgroup"
@@ -1640,6 +1641,7 @@ FROM crdb_internal.statement_statistics WHERE app_name = $1`, appName)
 func TestSQLStatsDiscardStatsOnFingerprintLimit(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
+	skip.UnderRace(t)
 
 	ctx := context.Background()
 	s := serverutils.StartServerOnly(t, base.TestServerArgs{})
