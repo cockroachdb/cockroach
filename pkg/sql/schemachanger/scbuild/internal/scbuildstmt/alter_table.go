f0c3852a8b172bc8b2cc7c08c675961c87b7afc9
Rafi Shamim <rafi@cockroachlabs.com>
2025-12-09 12:20:24 -0500

    schemachanger: handle RESET storage param in declarative schema changer
    This reuses the logic that was added to handle SET. The only difference
    is that RESET always drops the StorageParam element.
    
    Release note: None

--- pkg/sql/schemachanger/scbuild/internal/scbuildstmt/alter_table.go
+++ pkg/sql/schemachanger/scbuild/internal/scbuildstmt/alter_table.go
@@ -50,6 +50,7 @@ var supportedAlterTableStatements = map[reflect.Type]supportedAlterTableCommand{
 	reflect.TypeOf((*tree.AlterTableSetVisible)(nil)):         {fn: alterTableAlterColumnSetVisible, on: true, checks: isV261Active},
 	reflect.TypeOf((*tree.AlterTableIdentity)(nil)):           {fn: alterTableAlterColumnIdentity, on: true, checks: isV261Active},
 	reflect.TypeOf((*tree.AlterTableSetStorageParams)(nil)):   {fn: AlterTableSetStorageParams, on: true, checks: isV261Active},
+	reflect.TypeOf((*tree.AlterTableResetStorageParams)(nil)): {fn: AlterTableResetStorageParams, on: true, checks: isV261Active},
 }
 
 func init() {
