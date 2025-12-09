15a155bafa28233610b798a044bb2b504357d07f
ZhouXing19 <zhouxing@uchicago.edu>
2025-12-09 15:06:06 -0500

    sql: hide canary stats settings pending feature completion
    This commit hides the `sql.stats.canary_fraction` cluster setting and
    `canary_stats_mode` session variable since the canary statistics feature
    is not yet complete.
    
    The table storage parameter `sql_stats_canary_window` remains unchanged
    because its default value of 0 prevents it from appearing in SHOW CREATE
    TABLE output, which is the primary way users would discover this parameter
    outside of documentation.
    
    Fixes #159080
    Release note: None

--- pkg/sql/vars.go
+++ pkg/sql/vars.go
@@ -4475,6 +4475,7 @@ var varGen = map[string]sessionVar{
 	},
 
 	`canary_stats_mode`: {
+		Hidden:       true,
 		GetStringVal: makePostgresBoolGetStringValFn(`canary_stats_mode`),
 		Set: func(ctx context.Context, m sessionmutator.SessionDataMutator, s string) error {
 			mode, ok := sessiondatapb.CanaryStatsModeFromString(s)
