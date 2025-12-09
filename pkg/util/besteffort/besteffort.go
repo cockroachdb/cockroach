aa27876b737c24b6351d0e34917725e603965c08
Andrew Thomas <andrew.thomas@cockroachlabs.com>
2025-12-09 11:31:09 -0500

    backup: refactor backup telemetry to use besteffort
    Previously backup telemetry used explicit logging and now uses the besteffort
    package.  The backups hooks goroutine handling was cleaned up as part of
    besteffort refactoring.
    
    Release note: None
    Part of: #152677

--- pkg/util/besteffort/besteffort.go
+++ pkg/util/besteffort/besteffort.go
@@ -62,38 +62,3 @@ func Error(ctx context.Context, name string, do func(ctx context.Context) error)
 		log.Dev.Errorf(ctx, "best effort operation '%s' failed: %+v", name, err)
 	}
 }
-package besteffort
-
-import (
-	"context"
-
-	"github.com/cockroachdb/cockroach/pkg/util/log"
-)
-
-func Warning(ctx context.Context, name string, do func(ctx context.Context) error) {
-	if shouldSkip(name) {
-		log.Dev.Infof(ctx, "skipping best effort operation '%s'", name)
-		return
-	}
-	err := do(ctx)
-	if err != nil {
-		if !isAllowedFailure(name) {
-			panic(err)
-		}
-		log.Dev.Warningf(ctx, "best effort operation '%s' failed: %+v", name, err)
-	}
-}
-
-func Error(ctx context.Context, name string, do func(ctx context.Context) error) {
-	if shouldSkip(name) {
-		log.Dev.Infof(ctx, "skipping best effort operation '%s'", name)
-		return
-	}
-	err := do(ctx)
-	if err != nil {
-		if !isAllowedFailure(name) {
-			panic(err)
-		}
-		log.Dev.Errorf(ctx, "best effort operation '%s' failed: %+v", name, err)
-	}
-}
