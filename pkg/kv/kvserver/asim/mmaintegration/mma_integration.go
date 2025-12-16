280375b38d83bf51133c2e17ae71b410bd7b003e
Angela Dietz <dietz@cockroachlabs.com>
2025-12-16 16:19:08 -0500

    asim: only populate spanconfig in store leaseholder msg when needed
    Previously, the StoreLeaseholderMsg MaybeSpanConf was always populated in
    asim, but in the real MMA system this field is only populated when the
    span config has a possible change, such as when there is a new
    leaseholder for the range, or if there is a new replica. This meant that
    asim didn't adequately test codepaths where the span config was not
    populated. Now, the span config is only populated in asim when there was
    a probable change. This is done by adding a mmaSpanConfigIsUpToDate to
    the asim `replica` struct, similar to the mmaSpanConfigIsUpToDate field
    on the `Replica` type.
    
    Epic: CRDB-55052
    Fixes: #158674
    Release note: none

--- pkg/kv/kvserver/asim/mmaintegration/mma_integration.go
+++ pkg/kv/kvserver/asim/mmaintegration/mma_integration.go
@@ -15,6 +15,11 @@ import (
 
 // MakeStoreLeaseholderMsgFromState creates a StoreLeaseholderMsg from the
 // state of the simulator.
+//
+// The span config is populated when the replica's mmaSpanConfigIsUpToDate flag
+// is false (new replica, span config changed, or lease was just acquired).
+// The caller is responsible for setting mmaSpanConfigIsUpToDate=true on replicas
+// after successfully sending the message to MMA.
 func MakeStoreLeaseholderMsgFromState(
 	s state.State, storeID state.StoreID,
 ) mmaprototype.StoreLeaseholderMsg {
@@ -76,13 +81,21 @@ func MakeStoreLeaseholderMsgFromState(
 		rl.Load[mmaprototype.CPURate] = mmaprototype.LoadValue(load.RaftCPUNanosPerSecond + load.RequestCPUNanosPerSecond)
 		rl.RaftCPU = mmaprototype.LoadValue(load.RaftCPUNanosPerSecond)
 
-		rangeMessages = append(rangeMessages, mmaprototype.RangeMsg{
-			RangeID:                  roachpb.RangeID(replica.Range()),
-			MaybeSpanConfIsPopulated: true,
+		rngSpanConf := rng.SpanConfig()
+		rangeID := roachpb.RangeID(replica.Range())
+
+		spanConfIsPopulated := !replica.MMASpanConfigIsUpToDate() && rngSpanConf != nil
+
+		rangeMsg := mmaprototype.RangeMsg{
+			RangeID:                  rangeID,
+			MaybeSpanConfIsPopulated: spanConfIsPopulated,
 			Replicas:                 replicas,
-			MaybeSpanConf:            *rng.SpanConfig(),
 			RangeLoad:                rl,
-		})
+		}
+		if spanConfIsPopulated {
+			rangeMsg.MaybeSpanConf = *rngSpanConf
+		}
+		rangeMessages = append(rangeMessages, rangeMsg)
 	}
 
 	return mmaprototype.StoreLeaseholderMsg{
