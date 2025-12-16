019e21610a92bdd511fb7c41d978bcc0e19aba21
ZhouXing19 <zhouxing@uchicago.edu>
2025-12-16 11:31:51 -0500

    sql: fix SHOW CREATE FUNCTION with column casting to UDT
    Fixes #152233
    Fixes #159145
    Fixes #87475
    
    Previously, if a function contains cast from a column to UDT in the body,
    the SHOW CREATE TABLE statement for this function will error out. This
    commit is to fix it.
    
    Release note (bug fix): enable SHOW CREATE TABLE for function created with
    columns casting to user defined types.

--- pkg/sql/show_create_clauses.go
+++ pkg/sql/show_create_clauses.go
@@ -388,18 +388,8 @@ func formatViewQueryTypesForDisplay(
 
 // formatFunctionQueryTypesForDisplay is similar to
 // formatViewQueryTypesForDisplay but can only be used for functions.
-// nil is used as the table descriptor for schemaexpr.FormatExprForDisplay call.
-// This is fine assuming that UDFs cannot be created with expression casting a
-// column/var to an enum in function body. This is super rare case for now, and
-// it's tracked with issue #87475. We should also unify this function with
-// formatViewQueryTypesForDisplay.
 func formatFunctionQueryTypesForDisplay(
-	ctx context.Context,
-	evalCtx *eval.Context,
-	semaCtx *tree.SemaContext,
-	sessionData *sessiondata.SessionData,
-	queries string,
-	lang catpb.Function_Language,
+	ctx context.Context, semaCtx *tree.SemaContext, queries string, lang catpb.Function_Language,
 ) (string, error) {
 	// replaceFunc is a visitor function that replaces user defined type IDs in
 	// SQL expressions with their names.
@@ -426,17 +416,26 @@ func formatFunctionQueryTypesForDisplay(
 		if !typ.UserDefined() {
 			return true, expr, nil
 		}
-		formattedExpr, err := schemaexpr.FormatExprForDisplay(
-			ctx, nil, expr.String(), evalCtx, semaCtx, sessionData, tree.FmtParsable,
-		)
+
+		typedExpr, err := expr.TypeCheck(ctx, semaCtx, types.AnyElement)
 		if err != nil {
 			return false, expr, err
 		}
-		newExpr, err = parser.ParseExpr(formattedExpr)
-		if err != nil {
-			return false, expr, err
+
+		if dEnum, ok := typedExpr.(*tree.DEnum); ok {
+			switch n := expr.(type) {
+			case *tree.CastExpr:
+				n.Expr = tree.NewStrVal(dEnum.LogicalRep)
+				n.Type = dEnum.EnumTyp
+			case *tree.AnnotateTypeExpr:
+				n.Expr = tree.NewStrVal(dEnum.LogicalRep)
+				n.Type = dEnum.EnumTyp
+			}
+		} else {
+			return false, expr, errors.Newf("unsupported expression type %T", typedExpr)
 		}
-		return false, newExpr, nil
+
+		return false, expr, nil
 	}
 	// replaceTypeFunc is a visitor function that replaces type annotations
 	// containing user defined types IDs with their name. This is currently only
