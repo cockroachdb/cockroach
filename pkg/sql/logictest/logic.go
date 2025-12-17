b169d90916123f814cf59f61b481fdf4b02116b4
Tobias Grieger <tobias.b.grieger@gmail.com>
2025-12-17 16:04:33 +0000

    logictest: use unique TestClusterName
    This allows https://github.com/cockroachdb/cockroach/pull/157868 to do its thing,
    avoiding flakes such as:
    
    Fixes (when backported to 25.4): #159528

--- pkg/sql/logictest/logic.go
+++ pkg/sql/logictest/logic.go
@@ -1547,7 +1547,6 @@ func (t *logicTest) newCluster(
 					IgnoreOnDeleteRangeError: ignoreMVCCRangeTombstoneErrors,
 				},
 			},
-			ClusterName:   "testclustername",
 			ExternalIODir: t.sharedIODir,
 		},
 		// For distributed SQL tests, we use the fake span resolver; it doesn't
