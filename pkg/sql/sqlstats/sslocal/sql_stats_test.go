7ba85465d0ae2c6b3d620fd1e545b3406980c13d
Jason Fong <jason.fong@cockroachlabs.com>
2025-12-24 11:43:53 -0500

    sslocal: link insights ingester into the memory monitor
    The sql stats ingester is not currently plugged into the memory monitor, and this has caused issues in the past with long running transactions having their statements stats occupy memory while being unaccounted for.
    
    This change introduces the memory monitor into sql stats ingester and uses it to account for the memory usage for statements.
    
    Resolves: #158453
    Epic: None
    Release Note: None

--- pkg/sql/sqlstats/sslocal/sql_stats_test.go
+++ pkg/sql/sqlstats/sslocal/sql_stats_test.go
@@ -462,7 +462,7 @@ func TestExplicitTxnFingerprintAccounting(t *testing.T) {
 		nil, /* knobs */
 	)
 
-	ingester := sslocal.NewSQLStatsIngester(st, nil /* knobs */, sslocal.NewIngesterMetrics(), sqlStats)
+	ingester := sslocal.NewSQLStatsIngester(st, nil /* knobs */, sslocal.NewIngesterMetrics(), nil /* parentMon */, sqlStats)
 	ingester.Start(ctx, stopper)
 
 	appStats := sqlStats.GetApplicationStats("" /* appName */)
@@ -587,7 +587,7 @@ func TestAssociatingStmtStatsWithTxnFingerprint(t *testing.T) {
 			nil,
 			nil,
 		)
-		ingester := sslocal.NewSQLStatsIngester(st, nil /* knobs */, sslocal.NewIngesterMetrics(), sqlStats)
+		ingester := sslocal.NewSQLStatsIngester(st, nil /* knobs */, sslocal.NewIngesterMetrics(), nil /* parentMon */, sqlStats)
 		appStats := sqlStats.GetApplicationStats("" /* appName */)
 		statsCollector := sslocal.NewStatsCollector(
 			st,
