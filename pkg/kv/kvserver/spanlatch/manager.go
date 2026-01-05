f62aec61989047ad5630b54ce6526055b01deded
Angela Dietz <dietz@cockroachlabs.com>
2026-01-05 10:30:14 -0500

    Revert "spanlatch: add cluster setting for slow latch request threshold"
    This reverts commit 0d90aae8a32bbd2975c353e85bc65cd5fb2a8dca.
    This was a backported commit, but it was later decided that it should be
    backported to this release branch since it isn't necessary.

--- pkg/kv/kvserver/spanlatch/manager.go
+++ pkg/kv/kvserver/spanlatch/manager.go
@@ -11,6 +11,7 @@ import (
 	"time"
 	"unsafe"
 
+	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/concurrency/poison"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/spanset"
@@ -584,9 +585,7 @@ func (m *Manager) waitForSignal(
 	}
 	log.Eventf(ctx, "waiting to acquire %s latch %s, held by %s latch %s", waitType, wait, heldType, held)
 	poisonCh := held.g.poison.signalChan()
-
-	slowThreshold := m.slowLatchRequestThreshold()
-	t.Reset(slowThreshold)
+	t.Reset(base.SlowRequestThreshold)
 	for {
 		select {
 		case <-held.g.done.signalChan():
@@ -609,7 +608,7 @@ func (m *Manager) waitForSignal(
 			}
 		case <-t.C:
 			log.KvExec.Warningf(ctx, "have been waiting %s to acquire %s latch %s, held by %s latch %s",
-				slowThreshold, waitType, wait, heldType, held)
+				base.SlowRequestThreshold, waitType, wait, heldType, held)
 			if m.slowReqs != nil {
 				m.slowReqs.Inc(1)
 				defer m.slowReqs.Dec(1) //nolint:deferloop
@@ -708,14 +707,6 @@ func (m *Manager) longLatchHoldThreshold() time.Duration {
 		return math.MaxInt64 // disable
 	}
 	return LongLatchHoldThreshold.Get(&m.settings.SV)
-}
-
-// slowLatchRequestThreshold returns the threshold for logging slow latch requests.
-func (m *Manager) slowLatchRequestThreshold() time.Duration {
-	if m.settings == nil {
-		return math.MaxInt64 // disable
-	}
-	return SlowLatchRequestThreshold.Get(&m.settings.SV)
 }
 
 // Metrics holds information about the state of a Manager.
