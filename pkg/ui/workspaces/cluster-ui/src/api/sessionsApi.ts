91d7ef7a8ec81813f3667adbdb1060022cad2465
alyshanjahani-crl <alyshan@cockroachlabs.com>
2026-01-06 22:14:23 +0000

    ui: Exclude closed sessions by default
    This commit modifies the UI to set the Session Status filter to Idle, Active
    by default.
    
    And it makes the ListSessions API call use the exclude_closed_sessions
    parameter accordingly.
    
    Fixes: #159785
    Release note (ui change): The sessions page sets the Session Status filter by
    default. It is set to Active,Idle.

--- pkg/ui/workspaces/cluster-ui/src/api/sessionsApi.ts
+++ pkg/ui/workspaces/cluster-ui/src/api/sessionsApi.ts
@@ -14,10 +14,21 @@ export type SessionsRequestMessage =
 export type SessionsResponseMessage =
   cockroach.server.serverpb.ListSessionsResponse;
 
+export interface SessionsRequest {
+  excludeClosedSessions?: boolean;
+}
+
 // getSessions gets all cluster sessions.
-export const getSessions = (): Promise<SessionsResponseMessage> => {
-  return fetchData(
-    cockroach.server.serverpb.ListSessionsResponse,
-    SESSIONS_PATH,
-  );
+export const getSessions = (
+  req?: SessionsRequest,
+): Promise<SessionsResponseMessage> => {
+  const params = new URLSearchParams();
+  if (req?.excludeClosedSessions) {
+    params.set("exclude_closed_sessions", "true");
+  }
+  const path =
+    params.toString() !== ""
+      ? `${SESSIONS_PATH}?${params.toString()}`
+      : SESSIONS_PATH;
+  return fetchData(cockroach.server.serverpb.ListSessionsResponse, path);
 };
