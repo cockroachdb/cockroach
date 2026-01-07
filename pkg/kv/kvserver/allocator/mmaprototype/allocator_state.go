da45beded3701d057e08aa4a6ffd87f6883c0501
wenyihu6 <wenyi@cockroachlabs.com>
2026-01-07 10:52:53 -0500

    fixup! mmaintegration: add StoreStatus to mma status translation plumbing

--- pkg/kv/kvserver/allocator/mmaprototype/allocator_state.go
+++ pkg/kv/kvserver/allocator/mmaprototype/allocator_state.go
@@ -204,8 +204,8 @@ func (a *allocatorState) ProcessStoreLoadMsg(ctx context.Context, msg *StoreLoad
 	a.cs.processStoreLoadMsg(ctx, msg)
 }
 
-// RefreshStoreStatus implements the Allocator interface.
-func (a *allocatorState) RefreshStoreStatus(
+// UpdateStoreStatus implements the Allocator interface.
+func (a *allocatorState) UpdateStoreStatus(
 	ctx context.Context, storeStatuses map[roachpb.StoreID]Status,
 ) {
 	a.mu.Lock()
