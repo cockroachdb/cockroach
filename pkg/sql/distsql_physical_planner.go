9dd708c2547703a3319c99342f8635e913d739a5
Yahor Yuzefovich <yahor@cockroachlabs.com>
2026-01-07 15:33:52 -0800

    sql: add a couple of metrics about SetupFlowRequest RPCs
    We have a pool of distsql runner workers (proportional to vCPUs per
    node) that execute SetupFlowRequest RPCs in parallel with the main
    goroutine of the gateway flow. This commit adds two counters to track
    number of requests done via those parallel workers as well as
    sequentially via the main goroutine, which should give us signal whether
    a cluster needs adjustment of `sql.distsql.num_runners` cluster setting.
    
    Release note: None

--- pkg/sql/distsql_physical_planner.go
+++ pkg/sql/distsql_physical_planner.go
@@ -220,7 +220,7 @@ func NewDistSQLPlanner(
 		rpcCtx.Stopper.AddCloser(dsp.parallelLocalScansSem.Closer("stopper"))
 	}
 
-	dsp.runnerCoordinator.init(ctx, stopper, &st.SV)
+	dsp.runnerCoordinator.init(ctx, stopper, &st.SV, distSQLSrv.Metrics.RunnerReqParallelCount)
 	dsp.initCancelingWorkers(ctx)
 	return dsp
 }
