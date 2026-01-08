ce8d31357bd11177f3cc6fff1534e8375bcbfdca
Swapneeth Gorantla <swapneeth.gorantla@cockroachlabs.com>
2026-01-08 13:34:13 -0500

    kvserver: move `LastReplicaGCTimestamp` writes to split apply time
    Previously, `LastReplicaGCTimestamp` was written to the state machine
    log during split evaluations as part of the replicated batch, even when
    `LastReplicaGCTimestamp` is supposed to be an unreplicated key. The
    key was written regardless via the use of 
    `spanset.DisableForbiddenSpanAssertions`.
    
    Unreplicated keys should not be part of replicated batches that are
    applied across all replicas; such keys should only be written to the 
    log engine as they are local to the replica.
    
    
    This diff addresses this by moving the `LastReplicaGCTimestamp` write
    from the evaluation phase (in `splitTriggerHelper`) to the split apply 
    (in `splitPreApply`), when the RHS replica is initialized, ensuring that
    the key is written locally to the log engine. There is also a check in
    `splitPreApply` which clears the key from the state engine batch if it
    exists (in the case of historical split proposals). Data-driven test
    `.txt` files were updated via the `--rewrite` flag of `dev test`.
    
    Fixes: #157897
    Release note: None

--- pkg/kv/kvserver/store_split.go
+++ pkg/kv/kvserver/store_split.go
@@ -9,10 +9,12 @@ import (
 	"bytes"
 	"context"
 
+	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvserverpb"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvstorage"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/load"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
+	"github.com/cockroachdb/cockroach/pkg/storage"
 	"github.com/cockroachdb/cockroach/pkg/storage/enginepb"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
@@ -145,6 +147,19 @@ func splitPreApply(
 		log.KvExec.Fatalf(ctx, "cannot clear RaftTruncatedState: %v", err)
 	}
 
+	// Similar to RaftTruncatedState above, historical split proposals may write
+	// LastReplicaGCTimestamp. Clear it from the state engine batch if present,
+	// since it's an unreplicated key that belongs in the log engine. The key is
+	// written below only if necessary (when we're initializing the RHS).
+	//
+	// TODO(#157897): remove this workaround when there are no historical
+	// proposals with LastReplicaGCTimestamp, e.g. after a below-raft migration.
+	if err := stateRW.ClearUnversioned(
+		keys.RangeLastReplicaGCTimestampKey(in.rhsDesc.RangeID),
+		storage.ClearOptions{}); err != nil {
+		log.KvExec.Fatalf(ctx, "cannot clear LastReplicaGCTimestamp: %v", err)
+	}
+
 	if in.destroyed {
 		// The RHS replica has already been removed from the store. To apply the
 		// split, we must clear the user data the RHS would have inherited from the
@@ -190,6 +205,18 @@ func splitPreApply(
 	if err := rsl.SetClosedTimestamp(ctx, stateRW, in.initClosedTimestamp); err != nil {
 		log.KvExec.Fatalf(ctx, "%s", err)
 	}
+	// Set the initial LastReplicaGCTimestamp for the RHS. This is an unreplicated
+	// key written to the log engine. We set it to the zero (default  value of
+	// hlc.Timestamp), meaning the replica GC queue will check this replica on
+	// its next pass. This is appropriate for a newly created replica after a
+	// split as we want the replica to be processed by the replica GC queue as
+	// soon as possible for validity checks.
+	if err := storage.MVCCBlindPutProto(ctx, raftRW.WO,
+		keys.RangeLastReplicaGCTimestampKey(in.rhsDesc.RangeID),
+		hlc.Timestamp{}, &hlc.Timestamp{}, storage.MVCCWriteOptions{}); err != nil {
+		log.KvExec.Fatalf(ctx, "cannot set LastReplicaGCTimestamp: %v", err)
+	}
+
 }
 
 // splitPostApply is the part of the split trigger which coordinates the actual
