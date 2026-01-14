946f97ce384205ef182100727868a1ba2bee46cb
wenyihu6 <wenyi@cockroachlabs.com>
2026-01-14 15:58:19 -0500

    storeliveness: regression test for SupportState after restarts
    This commit extends TestSupportManagerRestart to test for a bug where
    SupportState would incorrectly return StateNotSupporting for stores
    with previously withdrawn support after restarts.

--- pkg/kv/kvserver/storeliveness/support_manager.go
+++ pkg/kv/kvserver/storeliveness/support_manager.go
@@ -124,6 +124,21 @@ func (sm *SupportManager) SupportFor(id slpb.StoreIdent) (slpb.Epoch, bool) {
 // SupportState implements the SupportStatus interface.
 func (sm *SupportManager) SupportState(id slpb.StoreIdent) (SupportState, hlc.ClockTimestamp) {
 	ss := sm.supporterStateHandler.getSupportFor(id)
+	return computeSupportState(ss)
+}
+
+// computeSupportState determines the SupportState and withdrawal timestamp from
+// a supportState. This is extracted as a standalone function for testability.
+//
+//	Expiration | lastSupportWithdrawnTime | Result
+//	-----------+--------------------------+-------------------------------------------------------
+//	empty      | empty                    | StateUnknown (never supported this store, or support
+//	           |                          | was withdrawn prior to restart)
+//	empty      | non-empty                | StateNotSupporting (withdrawn, timestamp known)
+//	non-empty  | empty                    | StateSupporting (not withdrawn or timestamp lost on
+//	           |                          | restart after re-establishment)
+//	non-empty  | non-empty                | StateSupporting (active, even if withdrawn previously)
+func computeSupportState(ss supportState) (SupportState, hlc.ClockTimestamp) {
 	// If we've never seen this store, return StateUnknown.
 	if ss.empty() {
 		return StateUnknown, hlc.ClockTimestamp{}
