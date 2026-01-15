19e119dde6eb299cb385cae86e83730d2dce397f
Pavel Kalinnikov <pavel@cockroachlabs.com>
2026-01-15 23:03:50 +0000

    kvserver: encapsulate prepareSnapApply checks
    Epic: none
    Release note: none

--- pkg/kv/kvserver/snapshot_apply_prepare_test.go
+++ pkg/kv/kvserver/snapshot_apply_prepare_test.go
@@ -9,6 +9,7 @@ import (
 	"context"
 	"fmt"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/keys"
@@ -31,10 +32,83 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestVerifySnap(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	// Parses spans like "a-b".
+	desc := func(s string) roachpb.RangeDescriptor {
+		if s == "" {
+			return roachpb.RangeDescriptor{}
+		}
+		require.True(t, len(s) == 3 && s[1] == '-')
+		return roachpb.RangeDescriptor{StartKey: roachpb.RKey(s[:1]), EndKey: roachpb.RKey(s[2:])}
+	}
+	// Parses lists of spans like "a-b c-d".
+	sub := func(s string) []kvstorage.DestroyReplicaInfo {
+		if s == "" {
+			return nil
+		}
+		var res []kvstorage.DestroyReplicaInfo
+		for _, part := range strings.Split(s, " ") {
+			d := desc(part)
+			res = append(res, kvstorage.DestroyReplicaInfo{Keys: d.RSpan()})
+		}
+		return res
+	}
+
+	for _, tc := range []struct {
+		snap  string
+		prev  string
+		sub   string
+		want  roachpb.RKey
+		notOk bool
+	}{
+		{snap: "b-z", prev: "a-z", notOk: true},    // StartKey changed
+		{snap: "a-z", sub: "c-d a-b", notOk: true}, // subsumed is unordered
+		{snap: "a-z", sub: "a-c b-d", notOk: true}, // overlapping subsumed
+		// Subsumed replicas not strictly to the right from StartKey.
+		{snap: "c-d", sub: "a-b", notOk: true},
+		{snap: "c-d", sub: "a-c", notOk: true},
+		{snap: "c-d", sub: "a-d", notOk: true},
+		{snap: "c-d", sub: "c-d", notOk: true},
+		// Subsumed replicas beyond the EndKey.
+		{snap: "a-b", sub: "b-c", notOk: true},
+		{snap: "a-b", sub: "c-d", notOk: true},
+		// Initializing a replica. Cases of subsumed replicas wider and narrower.
+		{snap: "a-f", want: nil},
+		{snap: "a-f", sub: "b-e", want: nil},
+		{snap: "a-f", sub: "b-f", want: nil},
+		{snap: "a-f", sub: "b-c d-f", want: nil},
+		{snap: "a-f", sub: "b-z", want: roachpb.RKey("z")},
+		{snap: "a-f", sub: "b-e e-z", want: roachpb.RKey("z")},
+		// Snapshot into an initialized replica. Cases where replica+subsumed is
+		// wider or narrower than the snapshot.
+		{snap: "a-k", prev: "a-c", want: nil},
+		{snap: "a-k", prev: "a-k", want: nil},
+		{snap: "a-k", prev: "a-c", sub: "c-d", want: nil},
+		{snap: "a-k", prev: "a-c", sub: "c-k", want: nil},
+		{snap: "a-k", prev: "a-c", sub: "c-d e-k", want: nil},
+		{snap: "a-k", prev: "a-z", want: roachpb.RKey("z")},
+		{snap: "a-k", prev: "a-c", sub: "c-z", want: roachpb.RKey("z")},
+		{snap: "a-k", prev: "a-c", sub: "c-d e-z", want: roachpb.RKey("z")},
+	} {
+		t.Run("", func(t *testing.T) {
+			prev, snap, subsume := desc(tc.prev), desc(tc.snap), sub(tc.sub)
+			clearEnd, err := verifySnap(&prev, &snap, subsume)
+			require.Equal(t, tc.notOk, err != nil)
+			require.Equal(t, tc.want, clearEnd)
+		})
+	}
+}
+
 // TestPrepareSnapApply tests the snapshot application code, which prepares a
-// set of SSTs to be ingested together with the snapshot SSTs. It this test, the
-// snapshot [a,k) subsumes replicas [a,b) and [b,z). Note that the key span
-// covered by the subsumed replicas is wider than the snapshot's.
+// set of SSTs to be ingested together with the snapshot SSTs. It this test, a
+// replica [a,c) receives a snapshot [a,k) which makes it wider and also
+// subsumes replica [c,e) and [f,z) that it overlaps.
+//
+// Note that the key span covered by the pre-snapshot replica and the subsumed
+// replicas is wider than the snapshot's.
 func TestPrepareSnapApply(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
@@ -73,8 +147,8 @@ func TestPrepareSnapApply(t *testing.T) {
 
 	const replicaID = 4
 	id := roachpb.FullReplicaID{RangeID: 123, ReplicaID: replicaID}
-	descA := desc(101, "a", "b")
-	descB := desc(102, "b", "z")
+	descA := desc(101, "c", "e")
+	descB := desc(102, "f", "z")
 	createRangeData(t, eng, *descA)
 	createRangeData(t, eng, *descB)
 
@@ -93,7 +167,7 @@ func TestPrepareSnapApply(t *testing.T) {
 		truncState: kvserverpb.RaftTruncatedState{Index: 100, Term: 20},
 		hardState:  raftpb.HardState{Term: 20, Commit: 100},
 		desc:       desc(id.RangeID, "a", "k"),
-		origDesc:   desc(id.RangeID, "a", "k"),
+		origDesc:   desc(id.RangeID, "a", "c"),
 		subsume: []kvstorage.DestroyReplicaInfo{
 			{FullReplicaID: roachpb.FullReplicaID{RangeID: descA.RangeID, ReplicaID: replicaID}, Keys: descA.RSpan()},
 			{FullReplicaID: roachpb.FullReplicaID{RangeID: descB.RangeID, ReplicaID: replicaID}, Keys: descB.RSpan()},
