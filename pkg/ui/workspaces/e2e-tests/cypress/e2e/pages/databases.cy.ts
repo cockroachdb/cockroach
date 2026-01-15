e2eba37f16b8740447bf932b0d6aed7b40794d16
Jason Fong <jason.fong@cockroachlabs.com>
2026-01-15 12:23:40 -0500

    ui,e2e: use data-testid for cypress tests Instead of looking for elements directly, we add data-testid attributes to the items we want to test, and search for these directly in the tests.
    Epic: None
    Release Note: None

--- pkg/ui/workspaces/e2e-tests/cypress/e2e/pages/databases.cy.ts
+++ pkg/ui/workspaces/e2e-tests/cypress/e2e/pages/databases.cy.ts
@@ -18,15 +18,24 @@ describe("databases page", () => {
 
   it("displays table of databases", () => {
     function checkDatabases(): void {
-      cy.get("table").should("exist");
-      cy.get("table").contains("Name");
-      cy.get("table").contains("Size");
-      cy.get("table").contains("Tables");
-      cy.get("table").contains("Regions / Nodes");
+      cy.get('[data-testid="databases-table"]').should("exist");
+      cy.get('[data-testid="databases-table"]').contains("Name");
+      cy.get('[data-testid="databases-table"]').contains("Size");
+      cy.get('[data-testid="databases-table"]').contains("Tables");
+      cy.get('[data-testid="databases-table"]').contains("Regions / Nodes");
 
-      cy.get("table tbody").should("contain.text", "defaultdb");
-      cy.get("table tbody").should("contain.text", "postgres");
-      cy.get("table tbody").should("contain.text", "system");
+      cy.get('[data-testid="databases-table"]').should(
+        "contain.text",
+        "defaultdb",
+      );
+      cy.get('[data-testid="databases-table"]').should(
+        "contain.text",
+        "postgres",
+      );
+      cy.get('[data-testid="databases-table"]').should(
+        "contain.text",
+        "system",
+      );
     }
 
     // make 2 checks: once with a direct navigation and once after a refresh
@@ -40,14 +49,20 @@ describe("databases page", () => {
     cy.get("@dbSearch").clear().type("sys{enter}");
 
     // 'system' should be visible
-    cy.get("table tbody").should("contain.text", "system");
+    cy.get('[data-testid="databases-table"]').should("contain.text", "system");
     // 'defaultdb' and 'postgres' should not be present
-    cy.get("table tbody").should("not.contain.text", "defaultdb");
-    cy.get("table tbody").should("not.contain.text", "postgres");
+    cy.get('[data-testid="databases-table"]').should(
+      "not.contain.text",
+      "defaultdb",
+    );
+    cy.get('[data-testid="databases-table"]').should(
+      "not.contain.text",
+      "postgres",
+    );
   });
 
   it("displays database metadata", () => {
-    cy.get("table tbody").contains("system").click();
+    cy.get('[data-testid="databases-table"]').contains("system").click();
     cy.location("hash").should("include", "/databases/1");
 
     cy.get('input[placeholder="Search tables"]').should("exist");
@@ -56,30 +71,36 @@ describe("databases page", () => {
       .should("exist");
 
     // Table of tables assertions
-    cy.get("table").should("exist");
-    cy.get("table").contains("Name");
-    cy.get("table").contains("Replication Size");
-    cy.get("table").contains("Ranges");
-    cy.get("table").contains("Columns");
-    cy.get("table").contains("Indexes");
-    cy.get("table").contains("Regions / Nodes");
-    cy.get("table").contains("% of Live data");
-    cy.get("table").contains("Table auto stats enabled");
-    cy.get("table").contains("Stats last updated");
-    cy.get("table tbody tr").should("have.length.at.least", 1);
+    cy.get('[data-testid="tables-table"]').should("exist");
+    cy.get('[data-testid="tables-table"]').contains("Name");
+    cy.get('[data-testid="tables-table"]').contains("Replication Size");
+    cy.get('[data-testid="tables-table"]').contains("Ranges");
+    cy.get('[data-testid="tables-table"]').contains("Columns");
+    cy.get('[data-testid="tables-table"]').contains("Indexes");
+    cy.get('[data-testid="tables-table"]').contains("Regions / Nodes");
+    cy.get('[data-testid="tables-table"]').contains("% of Live data");
+    cy.get('[data-testid="tables-table"]').contains("Table auto stats enabled");
+    cy.get('[data-testid="tables-table"]').contains("Stats last updated");
+    cy.get('[data-testid="tables-table"] tbody tr').should(
+      "have.length.at.least",
+      1,
+    );
 
     // Click the 'Grants' tab
     cy.contains("Grants").click();
 
-    cy.get("table").contains("Grantee");
-    cy.get("table").contains("Privileges");
-    cy.get("table tbody tr").should("have.length.at.least", 1);
+    cy.get('[data-testid="database-grants-table"]').contains("Grantee");
+    cy.get('[data-testid="database-grants-table"]').contains("Privileges");
+    cy.get('[data-testid="database-grants-table"] tbody tr').should(
+      "have.length.at.least",
+      1,
+    );
   });
 
   it("displays table metadata", () => {
-    cy.get("table tbody").contains("system").click();
+    cy.get('[data-testid="databases-table"]').contains("system").click();
     // just an arbitrary table in the systems database
-    cy.get("table tbody").contains("public.comments").click();
+    cy.get('[data-testid="tables-table"]').contains("public.comments").click();
     cy.location("hash").should("match", /\/table\/\d+/);
 
     // summaries of table
@@ -96,18 +117,24 @@ describe("databases page", () => {
 
     // grants of the table
     cy.contains("Grants").click();
-    cy.get("table").contains("Grantee");
-    cy.get("table").contains("Privileges");
-    cy.get("table tbody tr").should("have.length.at.least", 1);
+    cy.get('[data-testid="table-grants-table"]').contains("Grantee");
+    cy.get('[data-testid="table-grants-table"]').contains("Privileges");
+    cy.get('[data-testid="table-grants-table"] tbody tr').should(
+      "have.length.at.least",
+      1,
+    );
 
     // indexes of the table
     cy.contains("Indexes").click();
 
-    cy.get("table").contains("Index Name");
-    cy.get("table").contains("Last Read");
-    cy.get("table").contains("Total Reads");
-    cy.get("table").contains("Recommendations");
-    cy.get("table").contains("Action");
-    cy.get("table tbody tr").should("have.length.at.least", 1);
+    cy.get('[data-testid="table-indexes-table"]').contains("Index Name");
+    cy.get('[data-testid="table-indexes-table"]').contains("Last Read");
+    cy.get('[data-testid="table-indexes-table"]').contains("Total Reads");
+    cy.get('[data-testid="table-indexes-table"]').contains("Recommendations");
+    cy.get('[data-testid="table-indexes-table"]').contains("Action");
+    cy.get('[data-testid="table-indexes-table"] tbody tr').should(
+      "have.length.at.least",
+      1,
+    );
   });
 });
