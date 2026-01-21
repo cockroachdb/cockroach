475a6710832fc91b3194d0570c9cb7c574a1e2e3
William Choe <williamchoe3@gmail.com>
2026-01-21 13:23:59 -0500

    refactor bazel test rule target name

--- pkg/cmd/roachprod/test/list_test.go
+++ pkg/cmd/roachprod/test/list_test.go
@@ -3,7 +3,7 @@
 // Use of this software is governed by the CockroachDB Software License
 // included in the /LICENSE file.
 
-package roachprodtest
+package test
 
 // TestCloudList tests listing clusters
 // Commenting because this just makes the log unusable rn...
