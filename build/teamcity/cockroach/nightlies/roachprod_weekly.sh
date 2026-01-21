d4191b78d5c850408ce411392ebe19968c73890c
William Choe <williamchoe3@gmail.com>
2026-01-21 13:23:59 -0500

    refactor bazel test rule target name

--- build/teamcity/cockroach/nightlies/roachprod_weekly.sh
+++ build/teamcity/cockroach/nightlies/roachprod_weekly.sh
@@ -96,7 +96,7 @@ BAZEL_SUPPORT_EXTRA_DOCKER_ARGS="-e GOOGLE_EPHEMERAL_CREDENTIALS -e GOOGLE_PROJE
   run_bazel <<EOF
 # Inside Docker container, artifacts are mounted at /artifacts
 ./bin/bazci --artifacts_dir=/artifacts -- test \
-  //pkg/cmd/roachprod/roachprodtest:roachprodtest_test \
+  //pkg/cmd/roachprod/test:test \
   --test_env=GOOGLE_EPHEMERAL_CREDENTIALS \
   --test_env=GOOGLE_PROJECT \
   --test_env=ROACHPROD_USER \
