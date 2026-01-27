b1417ae6ee2fd40b38fa7e5cbdac7e947b164d74
Drew Kimball <drewk@cockroachlabs.com>
2026-01-27 13:01:26 -0600

    opt: gate new norm rule behind session setting
    This commit adds a session setting optimizer_inline_any_unnest_subquery
    to gate the new optimizer rule InlineAnyProjectSet in case we want
    to backport. The setting is off by default, and will be switched on
    in the next commit.
    
    Informs #161871
    
    Release note (sql change): Added a session setting to enable/disable the
    optimizer rule InlineAnyProjectSet. The session setting is
    optimizer_inline_any_unnest_subquery, and will be on by default in 26.2+.

--- pkg/sql/vars.go
+++ pkg/sql/vars.go
@@ -4558,6 +4558,23 @@ var varGen = map[string]sessionVar{
 		},
 		GlobalDefault: globalFalse,
 	},
+
+	// CockroachDB extension.
+	`optimizer_inline_any_unnest_subquery`: {
+		GetStringVal: makePostgresBoolGetStringValFn(`optimizer_inline_any_unnest_subquery`),
+		Set: func(_ context.Context, m sessionmutator.SessionDataMutator, s string) error {
+			b, err := paramparse.ParseBoolVar("optimizer_inline_any_unnest_subquery", s)
+			if err != nil {
+				return err
+			}
+			m.SetOptimizerInlineAnyUnnestSubquery(b)
+			return nil
+		},
+		Get: func(evalCtx *extendedEvalContext, _ *kv.Txn) (string, error) {
+			return formatBoolAsPostgresSetting(evalCtx.SessionData().OptimizerInlineAnyUnnestSubquery), nil
+		},
+		GlobalDefault: globalFalse,
+	},
 }
 
 func ReplicationModeFromString(s string) (sessiondatapb.ReplicationMode, error) {
