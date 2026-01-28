6a9b486bb3194b7c23d3d199689fe2993c0d72d5
Kevin Cao <39608887+kev-cao@users.noreply.github.com>
2026-01-28 13:45:56 -0500

    backup: add WITH REVISION START TIME logic to SHOW BACKUPS
    This commit teaches `SHOW BACKUPS` to only show revision start times if
    `WITH REVISION START TIME` is set.
    
    Epic: CRDB-57536
    
    Resolve: #161326
    
    Release note: Added `REVISION START TIME` option to new `SHOW BACKUPS`
    UX. Use the option to view revision start times of revision history
    backups.

--- pkg/backup/show_test.go
+++ pkg/backup/show_test.go
@@ -1002,6 +1002,64 @@ func TestShowBackupsWithIDs(t *testing.T) {
 	})
 }
 
+func TestShowBackupsWithIDsAndRevisionHistory(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	const numAccounts = 11
+	_, sqlDB, _, cleanupFn := backupRestoreTestSetup(t, singleNode, numAccounts, InitManualReplication)
+	defer cleanupFn()
+	sqlDB.Exec(t, "SET SESSION use_backups_with_ids = true")
+
+	const collectionURI = "nodelocal://1/backup"
+
+	sqlDB.Exec(t, `BACKUP INTO $1 WITH revision_history`, collectionURI)
+	// We add a short sleep between backups so they show up with different times.
+	time.Sleep(time.Second)
+	sqlDB.Exec(t, `BACKUP INTO LATEST IN $1 WITH revision_history`, collectionURI)
+	time.Sleep(time.Second)
+	sqlDB.Exec(t, `BACKUP INTO LATEST IN $1 WITH revision_history`, collectionURI)
+
+	t.Run("WITH REVISION START TIME", func(t *testing.T) {
+		revStartTimes := sqlDB.QueryStr(
+			t,
+			fmt.Sprintf(
+				`SELECT backup_time, revision_start_time FROM [SHOW BACKUPS IN '%s' WITH REVISION START TIME]`,
+				collectionURI,
+			),
+		)
+		require.Len(t, revStartTimes, 3, "expected 3 backups")
+		for idx, row := range revStartTimes {
+			require.NotEqual(t, "NULL", row[1], "expected non-empty revision start time (row %d)", idx)
+
+			require.Less(
+				t, row[1], row[0], "expected revision start time to be before backup time (row %d)", idx,
+			)
+			if idx < len(revStartTimes)-1 {
+				require.Less(
+					t,
+					revStartTimes[idx+1][1], row[1],
+					"expected revision start times to be in descending order (row %d)", idx,
+				)
+			}
+		}
+	})
+
+	t.Run("WITHOUT REVISION START TIME", func(t *testing.T) {
+		revStartTimes := sqlDB.QueryStr(
+			t,
+			fmt.Sprintf(
+				`SELECT revision_start_time FROM [SHOW BACKUPS IN '%s']`,
+				collectionURI,
+			),
+		)
+		require.Len(t, revStartTimes, 3, "expected 3 backups")
+		for idx, row := range revStartTimes {
+			require.Equal(t, "NULL", row[0], "expected empty revision start time (row %d)", idx)
+		}
+	})
+}
+
 func TestShowBackupWithIDs(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
