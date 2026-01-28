990ecfcb2be5bd6a5f376041ea4cec247ec3a92a
Yahor Yuzefovich <yahor@cockroachlabs.com>
2026-01-27 20:28:01 -0800

    colexecbuiltins: add benchmark for fingerprint query for index
    This will be used to estimate performance improvements of vectorizing
    the builtins used by the query that powers SHOW FINGERPRINTS.
    
    Release note: None

--- pkg/sql/colexec/colexecbuiltins/inject_setup_test.go
+++ pkg/sql/colexec/colexecbuiltins/inject_setup_test.go
@@ -6,12 +6,20 @@
 package colexecbuiltins_test
 
 import (
+	"github.com/cockroachdb/cockroach/pkg/base"
+	"github.com/cockroachdb/cockroach/pkg/security/securityassets"
+	"github.com/cockroachdb/cockroach/pkg/security/securitytest"
+	"github.com/cockroachdb/cockroach/pkg/server"
 	"github.com/cockroachdb/cockroach/pkg/sql/colexec/colbuilder"
 	"github.com/cockroachdb/cockroach/pkg/sql/colexec/colexecargs"
+	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
 )
 
 func init() {
 	// Inject a testing helper for NewColOperator so the tests can
 	// use NewColOperator without an import cycle.
 	colexecargs.TestNewColOperator = colbuilder.NewColOperator
+	securityassets.SetLoader(securitytest.EmbeddedAssets)
+	serverutils.InitTestServerFactory(server.TestServerFactory)
+	serverutils.TestingGlobalDRPCOption(base.TestDRPCEnabledRandomly)
 }
