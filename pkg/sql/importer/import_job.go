89a0bf53c847fbe00f41a73eb7cd6968df619a9d
Brendan Gerrity <brendan.gerrity@cockroachlabs.com>
2026-02-03 11:21:51 -0500

    sql: row count the import table after offlining
    Previously, the row count prior to import was run in the same
    transaction as the offlining of the table. The shared transaction did
    not protect against concurrent transactions inserting or deleting rows.
    This change scans the table in full after it's taken offline to store a
    correct initial row count.
    
    Epic: CRDB-58692
    
    Part of: #161276
    Part of: #155472
    Part of: #161279
    
    Release note: None

--- pkg/sql/importer/import_job.go
+++ pkg/sql/importer/import_job.go
@@ -169,25 +169,39 @@ func (r *importResumer) Resume(ctx context.Context, execCtx interface{}) error {
 	files := details.URIs
 	format := details.Format
 
+	updateDetails := func(txn isql.Txn, details jobspb.ImportDetails) error {
+		if err := r.job.WithTxn(txn).Update(ctx, func(
+			txn isql.Txn, md jobs.JobMetadata, ju *jobs.JobUpdater,
+		) error {
+			pl := md.Payload
+			*pl.GetImport() = details
+
+			// Update the set of descriptors for later observability.
+			// TODO(ajwerner): Do we need this idempotence test?
+			prev := md.Payload.DescriptorIDs
+			if prev == nil {
+				pl.DescriptorIDs = []descpb.ID{details.Table.Desc.ID}
+			}
+			ju.UpdatePayload(pl)
+			return nil
+		}); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	// Telemetry for multi-region.
+	table, err := getTable(details)
+	if err != nil {
+		return err
+	}
+
 	// Skip prepare stage on job resumption, if it has already been completed.
 	if !details.PrepareComplete {
 		if err := sql.DescsTxn(ctx, p.ExecCfg(), func(
 			ctx context.Context, txn isql.Txn, descsCol *descs.Collection,
 		) error {
-			// Telemetry for multi-region.
-			table, err := getTable(details)
-			if err != nil {
-				return err
-			}
-
-			// Run the count before making the table unreadable in the IMPORTING
-			// state.
-			rowCountDetails, err := r.detailsWithInitialRowCount(ctx, p, txn, table, details)
-			if err != nil {
-				return err
-			}
-
-			preparedDetails, err := r.prepareTableForIngestion(ctx, p, rowCountDetails, txn.KV(), descsCol)
+			preparedDetails, err := r.prepareTableForIngestion(ctx, p, details, txn.KV(), descsCol)
 			if err != nil {
 				return err
 			}
@@ -202,21 +216,7 @@ func (r *importResumer) Resume(ctx context.Context, execCtx interface{}) error {
 
 			// Update the job details now that the schemas and table descs have
 			// been "prepared".
-			return r.job.WithTxn(txn).Update(ctx, func(
-				txn isql.Txn, md jobs.JobMetadata, ju *jobs.JobUpdater,
-			) error {
-				pl := md.Payload
-				*pl.GetImport() = preparedDetails
-
-				// Update the set of descriptors for later observability.
-				// TODO(ajwerner): Do we need this idempotence test?
-				prev := md.Payload.DescriptorIDs
-				if prev == nil {
-					pl.DescriptorIDs = []descpb.ID{table.Desc.GetID()}
-				}
-				ju.UpdatePayload(pl)
-				return nil
-			})
+			return updateDetails(txn, preparedDetails)
 		}); err != nil {
 			return err
 		}
@@ -228,10 +228,36 @@ func (r *importResumer) Resume(ctx context.Context, execCtx interface{}) error {
 		})
 	}
 
+	if details.Table.InitialRowCount == 0 {
+		// Check if the table being imported into is starting empty, in which case
+		// we can cheaply clear-range instead of DeleteRange to cleanup.
+		//
+		// Run the count after the table has been taken offline.
+		//
+		// The count is low-cost for empty tables and otherwise the expensive
+		// full scan is only run the one time.
+		rowCountDetails, err := r.detailsWithInitialRowCount(ctx, p, details)
+		if err != nil {
+			return err
+		}
+
+		if err := p.ExecCfg().InternalDB.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
+			return updateDetails(txn, rowCountDetails)
+		}); err != nil {
+			return err
+		}
+
+		// Re-initialize details after the row count update.
+		details = r.job.Details().(jobspb.ImportDetails)
+		besteffort.Warning(ctx, "import-event", func(ctx context.Context) error {
+			return emitImportJobEvent(ctx, p, jobs.StateRunning, r.job)
+		})
+	}
+
 	// Note that this getTable call has to be separate from the one we did for
 	// multi-region telemetry above since we've just updated the details after
 	// the prepare step.
-	table, err := getTable(details)
+	table, err = getTable(details)
 	if err != nil {
 		return err
 	}
@@ -405,27 +431,19 @@ func (r *importResumer) Resume(ctx context.Context, execCtx interface{}) error {
 func (r *importResumer) detailsWithInitialRowCount(
 	ctx context.Context,
 	p sql.JobExecContext,
-	txn isql.Txn,
-	table jobspb.ImportDetails_Table,
 	details jobspb.ImportDetails,
 ) (jobspb.ImportDetails, error) {
 	rowCountDetails := details
 
-	// Check if the table being imported into is starting empty, in which case
-	// we can cheaply clear-range instead of DeleteRange to cleanup.
-	log.Eventf(ctx, "querying for row count for table %s", table.Name)
-	row, err := txn.QueryRowEx(
-		ctx,
-		"import-initial-row-count",
-		txn.KV(),
-		sessiondata.NodeUserSessionDataOverride,
-		fmt.Sprintf("SELECT count(*) FROM [%d AS t]", table.Desc.ID),
-	)
+	tblDesc := tabledesc.NewBuilder(details.Table.Desc).BuildImmutableTable()
+	tblSpan := tblDesc.TableSpan(p.ExecCfg().Codec)
+	// A scan is used instead of a `count(*)` because the query can't read the offline table.
+	rows, err := p.ExecCfg().DB.Scan(ctx, tblSpan.Key, tblSpan.EndKey, 0 /* maxRows */)
 	if err != nil {
-		return jobspb.ImportDetails{}, errors.Wrap(err, "querying initial row count")
+		return jobspb.ImportDetails{}, errors.Wrap(err, "scanning for initial row count")
 	}
 
-	rowCount := uint64(tree.MustBeDInt(row[0]))
+	rowCount := uint64(len(rows))
 	rowCountDetails.Table.WasEmpty = rowCount == 0
 	rowCountDetails.Tables[0].WasEmpty = rowCount == 0
 	rowCountDetails.Table.InitialRowCount = rowCount
