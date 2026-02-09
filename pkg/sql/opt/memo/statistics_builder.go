e465512b4fb60b5239a480c5336c30dd3e09da35
Drew Kimball <drewk@cockroachlabs.com>
2026-02-09 15:27:26 -0600

    opt: apply min_row_count to anti-joins
    Previously, the `optimizer_min_row_count` setting did not apply to the
    final selectivity calculated for AntiJoin expressions. This meant that
    the setting was applied asymmetrically, which could cause the optimizer
    to choose bad plans. This commit fixes the issue, gated behind a session
    setting `optimizer_use_min_row_count_anti_join_fix`.
    
    Fixes #162258
    
    Release note (bug fix): Fixed a bug that prevented the optimizer_min_row_count
    setting from applying to anti-join expressions, which could lead to bad query
    plans. The fix is gated behind `optimizer_use_min_row_count_anti_join_fix`, and
    will be on by default on 26.2+, and off by default in prior versions.

--- pkg/sql/opt/memo/statistics_builder.go
+++ pkg/sql/opt/memo/statistics_builder.go
@@ -1548,8 +1548,17 @@ func (sb *statisticsBuilder) buildJoin(
 	// Fix the stats for anti join.
 	switch h.joinType {
 	case opt.AntiJoinOp, opt.AntiJoinApplyOp:
-		s.RowCount = max(leftStats.RowCount-s.RowCount, epsilon)
-		s.Selectivity = props.MakeSelectivity(1 - s.Selectivity.AsFloat())
+		if sb.evalCtx.SessionData().OptimizerUseMinRowCountAntiJoinFix {
+			// Make sure to use ApplySelectivity instead of setting the selectivity and
+			// row count directly so that min_row_count is respected.
+			originalSel := s.Selectivity
+			s.RowCount = leftStats.RowCount
+			s.Selectivity = props.OneSelectivity
+			s.ApplySelectivity(props.MakeSelectivity(1 - originalSel.AsFloat()))
+		} else {
+			s.RowCount = max(leftStats.RowCount-s.RowCount, epsilon)
+			s.Selectivity = props.MakeSelectivity(1 - s.Selectivity.AsFloat())
+		}
 
 		// Converting column stats is error-prone. If any column stats are needed,
 		// colStatJoin will use the selectivity calculated above to estimate the
