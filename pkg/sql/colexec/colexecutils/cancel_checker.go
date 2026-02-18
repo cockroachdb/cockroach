e9f02d626644a4277b68352ff216a1b2604955c8
Yahor Yuzefovich <yahor@cockroachlabs.com>
2026-02-18 10:54:04 -0800

    Revert "colexec: fix data race in CancelChecker CPU handle registration"
    This reverts commit 9ffc8cf49d1229b888252d61c4449e66f546b114.

--- pkg/sql/colexec/colexecutils/cancel_checker.go
+++ pkg/sql/colexec/colexecutils/cancel_checker.go
@@ -30,14 +30,7 @@ type CancelChecker struct {
 	// check.
 	callsSinceLastCheck uint32
 
-	// sqlCPUHandle is the SQL-level CPU handle from which a per-goroutine
-	// handle is lazily registered on the first CheckEveryCall. Registration
-	// is deferred from Init to the first check because Init and Next may
-	// run on different goroutines (e.g. ParallelUnorderedSynchronizer
-	// initializes operator trees on the main goroutine but runs them on
-	// child goroutines).
-	sqlCPUHandle *admission.SQLCPUHandle
-	cpuHandle    *admission.GoroutineCPUHandle
+	cpuHandle *admission.GoroutineCPUHandle
 }
 
 var _ colexecop.Operator = &CancelChecker{}
@@ -57,7 +50,10 @@ func (c *CancelChecker) Init(ctx context.Context) {
 		// Check*() methods, and the input remains nil then.
 		c.Input.Init(c.Ctx)
 	}
-	c.sqlCPUHandle = admission.SQLCPUHandleFromContext(ctx)
+	cpuHandle := admission.SQLCPUHandleFromContext(ctx)
+	if cpuHandle != nil {
+		c.cpuHandle = cpuHandle.RegisterGoroutine()
+	}
 }
 
 // Next is part of colexecop.Operator interface.
@@ -93,11 +89,9 @@ func (c *CancelChecker) CheckEveryCall() {
 		colexecerror.ExpectedError(cancelchecker.QueryCanceledError)
 	default:
 	}
-	if c.cpuHandle == nil && c.sqlCPUHandle != nil {
-		c.cpuHandle = c.sqlCPUHandle.RegisterGoroutine()
-	}
 	if c.cpuHandle != nil {
-		if err := c.cpuHandle.MeasureAndAdmit(c.Ctx); err != nil {
+		err := c.cpuHandle.MeasureAndAdmit(c.Ctx)
+		if err != nil {
 			colexecerror.ExpectedError(cancelchecker.QueryCanceledError)
 		}
 	}
