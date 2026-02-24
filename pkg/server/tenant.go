8d52569ec8b08b04a863f9e6c9b1e2f98673a9fe
Sujatha Sivaramakrishnan <sujatha.sivaramakrishnan@cockroachlabs.com>
2026-02-24 15:14:39 +0530

    rpc, server: add drpc client and server metrics This PR adds better observability when we migrate from gRPC to DRPC for inter-node RPC communication. All the new metrics use HistogramVec/CounterVec (Prometheus-export-only, not TSDB) and are gated behind the existing cluster setting server.rpc.request_metrics.enabled (default: false). There is no method prefix filtering - all DRPC methods are recorded when enabled.
    Epic: CRDB-48931
    Part of: #163593
    Release note: None

--- pkg/server/tenant.go
+++ pkg/server/tenant.go
@@ -1341,12 +1341,14 @@ func makeTenantSQLServerArgs(
 
 	requestMetrics := rpc.NewRequestMetrics()
 	registry.AddMetricStruct(requestMetrics)
+	drpcRequestMetrics := rpc.NewDRPCServerRequestMetrics()
+	registry.AddMetricStruct(drpcRequestMetrics)
 
 	grpcServer, err := newGRPCServer(startupCtx, rpcContext, requestMetrics)
 	if err != nil {
 		return sqlServerArgs{}, err
 	}
-	drpcServer, err := newDRPCServer(startupCtx, rpcContext, requestMetrics)
+	drpcServer, err := newDRPCServer(startupCtx, rpcContext, drpcRequestMetrics)
 	if err != nil {
 		return sqlServerArgs{}, err
 	}
