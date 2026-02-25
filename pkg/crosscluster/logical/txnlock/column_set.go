8e55cfbcfcac4bd8df8e2b0d4b3cda97303de1eb
Jeff Swenson <jeffswenson@betterthannull.com>
2026-02-25 16:01:41 +0000

    txnlock: implement lock synthesis and sorting within txn
    The txnlock package provides lock synthesis and write set ordering for
    ldr's transaction mode. This change handles overlapping write sets and
    unique constraints. A future change will include support for foreign
    keys.
    
    Epic: CRDB-57649
    Release note: None

---
+++ pkg/crosscluster/logical/txnlock/column_set.go
@@ -0,0 +1,100 @@
+// Copyright 2026 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package txnlock
+
+import (
+	"context"
+	"encoding/binary"
+	"hash/fnv"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/rowenc"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/eval"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/cockroachdb/errors"
+)
+
+func uniqueIndexMixin(tableID descpb.ID, ucID descpb.ConstraintID) (uint64, error) {
+	h := fnv.New64a()
+	var buf [8]byte
+	binary.BigEndian.PutUint32(buf[:4], uint32(tableID))
+	binary.BigEndian.PutUint32(buf[4:], uint32(ucID))
+	if _, err := h.Write(buf[:]); err != nil {
+		return 0, errors.Wrap(err, "hashing unique index mixin")
+	}
+	return h.Sum64(), nil
+}
+
+// A columnSet is a collection of columns that are relevant for an individual
+// constraint.
+type columnSet struct {
+	columns []int32
+	// mixin is an integer that is combined with the hash. It is used to ensure
+	// different tables or unique constraints produce different hashes.
+	mixin uint64
+}
+
+// hash computes the hash that is used as the lock. hash(rowA) != hash(rowB)
+// implies !equal(rowA, rowB).
+func (c *columnSet) hash(ctx context.Context, row tree.Datums) (LockHash, error) {
+	h := fnv.New64a()
+	var prefixBytes [8]byte
+	binary.BigEndian.PutUint64(prefixBytes[:], c.mixin)
+	if _, err := h.Write(prefixBytes[:]); err != nil {
+		return 0, errors.Wrap(err, "hashing mixin for lock derivation")
+	}
+	for _, idx := range c.columns {
+		datum := row[idx]
+		ed := rowenc.EncDatum{Datum: datum}
+		encoded, err := ed.Fingerprint(
+			ctx,
+			datum.ResolvedType(),
+			&tree.DatumAlloc{},
+			nil, /* appendTo */
+			nil, /* acc */
+		)
+		if err != nil {
+			return 0, errors.Wrap(err, "hashing datum for lock derivation")
+		}
+		if _, err := h.Write(encoded); err != nil {
+			return 0, errors.Wrap(err, "hashing encoded datum for lock derivation")
+		}
+	}
+	return LockHash(h.Sum64()), nil
+}
+
+// null returns true if any of the columns are null.
+func (c *columnSet) null(row tree.Datums) bool {
+	if len(row) == 0 {
+		return true
+	}
+	for _, idx := range c.columns {
+		if row[idx] == tree.DNull {
+			return true
+		}
+	}
+	return false
+}
+
+// equal returns true if the columns are equal. equal(rowA, rowB) implies
+// hash(rowA) == hash(rowB).
+func (c *columnSet) equal(
+	ctx context.Context, evalCtx *eval.Context, rowA, rowB tree.Datums,
+) (bool, error) {
+	if c.null(rowA) || c.null(rowB) {
+		return false, nil
+	}
+	for _, idx := range c.columns {
+		cmp, err := rowA[idx].Compare(ctx, evalCtx, rowB[idx])
+		if err != nil {
+			return false, errors.Wrap(err, "comparing datums for lock derivation")
+		}
+		if cmp != 0 {
+			return false, nil
+		}
+	}
+	return true, nil
+}
---
+++ pkg/crosscluster/logical/txnlock/lock_synthesis.go
@@ -0,0 +1,154 @@
+// Copyright 2026 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package txnlock
+
+import (
+	"context"
+
+	"github.com/cockroachdb/cockroach/pkg/crosscluster/logical/ldrdecoder"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/catalog/lease"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/eval"
+	"github.com/cockroachdb/cockroach/pkg/util/hlc"
+	"github.com/cockroachdb/errors"
+)
+
+type LockHash uint64
+
+type Lock struct {
+	Hash LockHash
+	// TODO(jeffswenson): Read will be set to true for foreign key locks that
+	// only require a shared/read lock rather than an exclusive/write lock.
+	Read bool
+}
+
+type LockSet struct {
+	Locks      []Lock
+	SortedRows []ldrdecoder.DecodedRow
+}
+
+type LockSynthesizer struct {
+	tableConstraints map[descpb.ID]*tableConstraints
+}
+
+func NewLockSynthesizer(
+	ctx context.Context,
+	evalCtx *eval.Context,
+	lm *lease.Manager,
+	clock *hlc.Clock,
+	tableMappings []ldrdecoder.TableMapping,
+) (*LockSynthesizer, error) {
+	ls := &LockSynthesizer{
+		tableConstraints: make(map[descpb.ID]*tableConstraints, len(tableMappings)),
+	}
+
+	timestamp := lease.TimestampToReadTimestamp(clock.Now())
+	for _, mapping := range tableMappings {
+		tc, err := func() (*tableConstraints, error) {
+			leasedDesc, err := lm.Acquire(ctx, timestamp, mapping.DestID)
+			if err != nil {
+				return nil, err
+			}
+			defer leasedDesc.Release(ctx)
+
+			tableDesc, ok := leasedDesc.Underlying().(catalog.TableDescriptor)
+			if !ok {
+				return nil, errors.AssertionFailedf(
+					"expected table descriptor for %d, got %T",
+					mapping.DestID, leasedDesc.Underlying(),
+				)
+			}
+			return newTableConstraints(evalCtx, tableDesc)
+		}()
+		if err != nil {
+			return nil, err
+		}
+		ls.tableConstraints[mapping.DestID] = tc
+	}
+
+	return ls, nil
+}
+
+type rowsWithLock struct {
+	rows   []int32
+	isRead bool
+}
+
+func (ls *LockSynthesizer) DeriveLocks(
+	ctx context.Context, rows []ldrdecoder.DecodedRow,
+) (LockSet, error) {
+	// We build a table of locks for two reasons:
+	// 1. We use it to eliminate duplicate locks for the same row.
+	// 2. We need to topologically sort the rows to get an apply order and
+	//    we can use the set of lock conflicts to identify possible dependencies.
+	locks := map[LockHash]rowsWithLock{}
+	rowLocks := make([][]Lock, len(rows))
+
+	for i, row := range rows {
+		var err error
+		rowLocks[i], err = ls.deriveLocks(ctx, row)
+		if err != nil {
+			return LockSet{}, err
+		}
+		for _, lock := range rowLocks[i] {
+			lr, ok := locks[lock.Hash]
+			if !ok {
+				lr.isRead = lock.Read
+			} else {
+				lr.isRead = lr.isRead && lock.Read
+			}
+			lr.rows = append(lr.rows, int32(i))
+			locks[lock.Hash] = lr
+		}
+	}
+
+	sorted, err := ls.sort(ctx, rows, rowLocks, locks)
+	if err != nil {
+		return LockSet{}, err
+	}
+
+	lockSet := LockSet{
+		SortedRows: sorted,
+		Locks:      make([]Lock, 0, len(locks)),
+	}
+
+	for hash, lr := range locks {
+		lockSet.Locks = append(lockSet.Locks, Lock{
+			Hash: hash,
+			Read: lr.isRead,
+		})
+	}
+
+	return lockSet, nil
+}
+
+func (ls *LockSynthesizer) deriveLocks(
+	ctx context.Context, row ldrdecoder.DecodedRow,
+) ([]Lock, error) {
+	tc, ok := ls.tableConstraints[row.TableID]
+	if !ok {
+		// We return an error here because it indicates we were passed a row for a
+		// table that was not included when the LockSynthesizer was initialized.
+		return nil, errors.AssertionFailedf("unknown table %d", row.TableID)
+	}
+	return tc.deriveLocks(ctx, row)
+}
+
+// dependsOn returns true if row b must be applied before row a. It can be read
+// as a `dependsOn` b.
+func (ls *LockSynthesizer) dependsOn(
+	ctx context.Context, a, b ldrdecoder.DecodedRow,
+) (bool, error) {
+	if a.TableID != b.TableID {
+		return false, nil
+	}
+	tc, ok := ls.tableConstraints[a.TableID]
+	if !ok {
+		return false, nil
+	}
+	return tc.DependsOn(ctx, a, b)
+}
