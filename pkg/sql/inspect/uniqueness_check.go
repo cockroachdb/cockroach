5f365770e190e57fd7d9f2e8388f1b75aed780c4
Brendan Gerrity <brendan.gerrity@cockroachlabs.com>
2026-02-26 14:23:20 -0500

    sql/inspect: make uniqueness check available from `INSPECT` command
    Adds the uniqueness check to the job specs for `INSPECT` commands.
    
    Epic: CRDB-58692
    
    Fixes: #154551
    
    Release note (sql change): On an `INSPECT` run a new check validates
    unique column values in regional by row tables.

--- pkg/sql/inspect/uniqueness_check.go
+++ pkg/sql/inspect/uniqueness_check.go
@@ -10,7 +10,6 @@ import (
 	"fmt"
 	"slices"
 	"strings"
-	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/keys"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
@@ -22,7 +21,6 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/isql"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
-	"github.com/cockroachdb/cockroach/pkg/util/buildutil"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/errors"
 	"github.com/cockroachdb/redact"
@@ -404,10 +402,8 @@ func (c *uniquenessCheck) loadCatalogInfo(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	if !tableDesc.IsLocalityRegionalByRow() {
-		if !buildutil.CrdbTestBuild && !testing.Testing() { // For ease of testing, allow faked multi-region databases.
-			return errors.AssertionFailedf("uniqueness check is only supported for tables with REGIONAL BY ROW locality")
-		}
+	if !isRegionalByRow(tableDesc) {
+		return errors.AssertionFailedf("uniqueness check is only supported for tables with REGIONAL BY ROW locality")
 	}
 	c.tableDesc = tableDesc
 
