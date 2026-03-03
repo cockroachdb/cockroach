e4a18ba3c7b231da698ce34519cde7a063396c35
Andy Yang <yang@cockroachlabs.com>
2026-03-03 15:14:15 -0500

    changefeedccl: fix min_checkpoint_frequency=0 regression
    In adfa8890a81c5cbc5d34a95c222137ee5f41948d, we advertently made it
    so that setting `min_checkpoint_frequency` to 0 would cause the changefeed's
    highwater to not advance/not send resolved timestamps. This patch fixes that.
    
    Release note (bug fix): In a recent change that was included in 25.4+,
    we inadvertently made it so that setting `min_checkpoint_frequency` to
    0 would cause the changefeed's highwater to not advance/not send
    resolved timestamps. This bug has been fixed. Note however that setting
    `min_checkpoint_frequency` to lower than `500ms` is not recommended as
    it may cause degraded changefeed performance.

--- pkg/ccl/changefeedccl/changefeed_processors.go
+++ pkg/ccl/changefeedccl/changefeed_processors.go
@@ -2562,7 +2562,7 @@ func newSaveRateLimiter(
 // canSave returns whether enough time has passed to save progress again.
 func (l *saveRateLimiter) canSave(ctx context.Context) bool {
 	interval := l.config.interval()
-	if interval <= 0 {
+	if interval < 0 {
 		return false
 	}
 	if l.config.jitter != nil {

cockroachdb/cockroach

commit adfa8890a81c5cbc5d34a95c222137ee5f41948d

changefeedccl: have aggregators send their frontiers regularly
This patch updates the aggregators to send their frontiers regularly
every `min_checkpoint_frequency` (limited by average time to flush)
to support periodic span frontier persistence.

Release note: None

pkg/ccl/changefeedccl/changefeed_processors.go
@@ -106,9 +106,11 @@ type changeAggregator struct {
 	// eventConsumer consumes the event.
 	eventConsumer eventConsumer
 
-	nextHighWaterFlush time.Time     // next time high watermark may be flushed.
-	flushFrequency     time.Duration // how often high watermark can be checkpointed.
-	lastSpanFlush      time.Time     // last time expensive, span based checkpoint was written.
+	flushFrequency time.Duration // how often high watermark can be checkpointed.
+
+	// frontierFlushLimiter is a rate limiter for flushing the span frontier
+	// to the coordinator.
+	frontierFlushLimiter *saveRateLimiter
 
 	// frontier keeps track of resolved timestamps for spans along with schema change
 	// boundary information.
@@ -279,6 +281,22 @@ func newChangeAggregatorProcessor(
 		ca.flushFrequency = *checkpointFreq
 	} else {
 		ca.flushFrequency = changefeedbase.DefaultMinCheckpointFrequency
+	}
+
+	ca.frontierFlushLimiter, err = newSaveRateLimiter(saveRateConfig{
+		name: "frontier",
+		intervalName: func() redact.SafeValue {
+			return redact.SafeString(changefeedbase.OptMinCheckpointFrequency)
+		},
+		interval: func() time.Duration {
+			return ca.flushFrequency
+		},
+		jitter: func() float64 {
+			return aggregatorFlushJitter.Get(&ca.FlowCtx.Cfg.Settings.SV)
+		},
+	}, timeutil.DefaultTimeSource{})
+	if err != nil {
+		return nil, err
 	}
 
 	return ca, nil
@@ -461,9 +479,6 @@ func (ca *changeAggregator) Start(ctx context.Context) {
 
 	// Init heartbeat timer.
 	ca.lastPush = timeutil.Now()
-
-	// Generate expensive checkpoint only after we ran for a while.
-	ca.lastSpanFlush = timeutil.Now()
 }
 
 func (ca *changeAggregator) startKVFeed(
@@ -730,18 +745,6 @@ var aggregatorFlushJitter = settings.RegisterFloatSetting(
 	settings.WithPublic,
 )
 
-func nextFlushWithJitter(s timeutil.TimeSource, d time.Duration, j float64) (time.Time, error) {
-	if j < 0 || d < 0 {
-		return s.Now(), errors.AssertionFailedf("invalid jitter value: %f, duration: %s", j, d)
-	}
-	maxJitter := int64(j * float64(d))
-	if maxJitter == 0 {
-		return s.Now().Add(d), nil
-	}
-	nextFlush := d + time.Duration(rand.Int63n(maxJitter))
-	return s.Now().Add(nextFlush), nil
-}
-
 // Next is part of the RowSource interface.
 func (ca *changeAggregator) Next() (rowenc.EncDatumRow, *execinfrapb.ProducerMetadata) {
 	shouldEmitHeartBeat := func() bool {
@@ -897,7 +900,7 @@ func (ca *changeAggregator) flushBufferedEvents(ctx context.Context) error {
 // noteResolvedSpan periodically flushes Frontier progress from the current
 // changeAggregator node to the changeFrontier node to allow the changeFrontier
 // to persist the overall changefeed's progress
-func (ca *changeAggregator) noteResolvedSpan(resolved jobspb.ResolvedSpan) (returnErr error) {
+func (ca *changeAggregator) noteResolvedSpan(resolved jobspb.ResolvedSpan) error {
 	ctx, sp := tracing.ChildSpan(ca.Ctx(), "changefeed.aggregator.note_resolved_span")
 	defer sp.Finish()
 
@@ -935,32 +938,17 @@ func (ca *changeAggregator) noteResolvedSpan(resolved jobspb.ResolvedSpan) (retu
 	// TODO(yevgeniy): Consider doing something similar to how job checkpointing
 	//  works in the frontier where if we missed the window to checkpoint, we will attempt
 	//  the checkpoint at the next opportune moment.
-	checkpointFrontier := advanced &&
-		(forceFlush || timeutil.Now().After(ca.nextHighWaterFlush))
+	checkpointFrontier := (advanced && forceFlush) || ca.frontierFlushLimiter.canSave(ctx)
 
 	if checkpointFrontier {
-		defer func() {
-			ca.nextHighWaterFlush, err = nextFlushWithJitter(
-				timeutil.DefaultTimeSource{}, ca.flushFrequency, aggregatorFlushJitter.Get(sv))
-			if err != nil {
-				returnErr = errors.CombineErrors(returnErr, err)
-			}
-		}()
-		return ca.flushFrontier(ctx)
+		now := timeutil.Now()
+		if err := ca.flushFrontier(ctx); err != nil {
+			return err
+		}
+		ca.frontierFlushLimiter.doneSave(timeutil.Since(now))
 	}
 
-	// At a lower frequency, we checkpoint specific spans in the job progress
-	// either in backfills or if the highwater mark is excessively lagging behind.
-	checkpointSpans := (ca.frontier.InBackfill(resolved) || ca.frontier.HasLaggingSpans(sv)) &&
-		canCheckpointSpans(sv, ca.lastSpanFlush)
-
-	if checkpointSpans {
-		defer func() {
-			ca.lastSpanFlush = timeutil.Now()
-		}()
-		return ca.flushFrontier(ctx)
-	}
-	return returnErr
+	return nil
 }
 
 // flushFrontier flushes sink and emits resolved spans to the change frontier.
@@ -1313,8 +1301,18 @@ func newChangeFrontierProcessor(
 		cf.freqEmitResolved = emitNoResolved
 	}
 
-	cf.frontierPersistenceLimiter = newSaveRateLimiter(
-		"frontier" /* name */, changefeedbase.FrontierPersistenceInterval)
+	cf.frontierPersistenceLimiter, err = newSaveRateLimiter(saveRateConfig{
+		name: "frontier",
+		intervalName: func() redact.SafeValue {
+			return changefeedbase.FrontierPersistenceInterval.Name()
+		},
+		interval: func() time.Duration {
+			return changefeedbase.FrontierPersistenceInterval.Get(&cf.FlowCtx.Cfg.Settings.SV)
+		},
+	}, timeutil.DefaultTimeSource{})
+	if err != nil {
+		return nil, err
+	}
 
 	encodingOpts, err := opts.GetEncodingOptions()
 	if err != nil {
@@ -1919,7 +1917,7 @@ func (cf *changeFrontier) maybePersistFrontier(ctx context.Context) error {
 
 	if cf.spec.JobID == 0 ||
 		!cf.evalCtx.Settings.Version.IsActive(ctx, clusterversion.V25_4) ||
-		!cf.frontierPersistenceLimiter.canSave(ctx, &cf.FlowCtx.Cfg.Settings.SV) {
+		!cf.frontierPersistenceLimiter.canSave(ctx) {
 		return nil
 	}
 
@@ -2349,10 +2347,12 @@ func shouldCountUsageError(err error) bool {
 		status.Code(err) != codes.Canceled
 }
 
-// durationSetting is a duration cluster setting.
-type durationSetting interface {
-	Name() settings.SettingName
-	Get(sv *settings.Values) time.Duration
+// saveRateConfig is the config for a saveRateLimiter.
+type saveRateConfig struct {
+	name         redact.SafeString
+	intervalName func() redact.SafeValue
+	interval     func() time.Duration
+	jitter       func() float64 // optional
 }
 
 // saveRateLimiter is a rate limiter for saving a piece of progress.
@@ -2360,30 +2360,49 @@ type durationSetting interface {
 // It also limits saving to not be more frequent than the average
 // duration it takes to save progress.
 type saveRateLimiter struct {
-	name         redact.SafeString
-	saveInterval durationSetting
-	warnEveryN   util.EveryN
+	config     saveRateConfig
+	warnEveryN util.EveryN
+
+	clock timeutil.TimeSource
 
 	lastSave        time.Time
 	avgSaveDuration time.Duration
 }
 
 // newSaveRateLimiter returns a new saveRateLimiter.
-func newSaveRateLimiter(name redact.SafeString, saveInterval durationSetting) *saveRateLimiter {
-	return &saveRateLimiter{
-		name:         name,
-		saveInterval: saveInterval,
-		warnEveryN:   util.Every(time.Minute),
+func newSaveRateLimiter(
+	config saveRateConfig, clock timeutil.TimeSource,
+) (*saveRateLimiter, error) {
+	if len(config.name) == 0 {
+		return nil, errors.AssertionFailedf("name is required")
 	}
+	if config.intervalName == nil {
+		return nil, errors.AssertionFailedf("interval name is required")
+	}
+	if config.interval == nil {
+		return nil, errors.AssertionFailedf("interval is required")
+	}
+	return &saveRateLimiter{
+		config:     config,
+		warnEveryN: util.Every(time.Minute),
+		clock:      clock,
+	}, nil
 }
 
 // canSave returns whether enough time has passed to save progress again.
-func (l *saveRateLimiter) canSave(ctx context.Context, sv *settings.Values) bool {
-	interval := l.saveInterval.Get(sv)
-	if interval == 0 {
+func (l *saveRateLimiter) canSave(ctx context.Context) bool {
+	interval := l.config.interval()
+	if interval <= 0 {
 		return false
 	}
-	now := timeutil.Now()
+	if l.config.jitter != nil {
+		if jitter := l.config.jitter(); jitter > 0 {
+			if maxJitter := time.Duration(jitter * float64(interval)); maxJitter > 0 {
+				interval += time.Duration(rand.Int63n(int64(maxJitter) + 1))
+			}
+		}
+	}
+	now := l.clock.Now()
 	elapsed := now.Sub(l.lastSave)
 	if elapsed < interval {
 		return false
@@ -2393,8 +2412,7 @@ func (l *saveRateLimiter) canSave(ctx context.Context, sv *settings.Values) bool
 			log.Changefeed.Warningf(ctx, "cannot save %s even though %s has elapsed "+
 				"since last save and %s is set to %s because average duration to save was %s "+
 				"and further saving is disabled until that much time elapses",
-				l.name, elapsed, l.saveInterval.Name(),
-				interval, l.avgSaveDuration)
+				l.config.name, elapsed, l.config.intervalName(), interval, l.avgSaveDuration)
 		}
 		return false
 	}
@@ -2404,7 +2422,7 @@ func (l *saveRateLimiter) canSave(ctx context.Context, sv *settings.Values) bool
 // doneSave must be called after each save is completed with the duration
 // it took to save progress.
 func (l *saveRateLimiter) doneSave(saveDuration time.Duration) {
-	l.lastSave = timeutil.Now()
+	l.lastSave = l.clock.Now()
 
 	// Update the average save duration using an exponential moving average.
 	if l.avgSaveDuration == 0 {

pkg/ccl/changefeedccl/changefeed_processors_test.go
@@ -1,0 +1,154 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package changefeedccl
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/cockroachdb/redact"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSaveRateLimiter(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	ctx := context.Background()
+
+	intervals := map[string]time.Duration{
+		"positive interval":   30 * time.Second,
+		"negative interval":   -30 * time.Second,
+		"zero interval":       0,
+		"very small interval": time.Nanosecond,
+	}
+
+	jitters := map[string]float64{
+		"positive jitter":   0.1,
+		"negative jitter":   -0.1,
+		"zero jitter":       0,
+		"very small jitter": 1e-12,
+	}
+
+	for intervalName, interval := range intervals {
+		for jitterName, jitter := range jitters {
+			t.Run(fmt.Sprintf("%s with %s", intervalName, jitterName), func(t *testing.T) {
+				// Set up the mock clock for testing.
+				now := timeutil.Now()
+				clock := timeutil.NewManualTime(now)
+
+				// Create the save rate limiter.
+				l, err := newSaveRateLimiter(saveRateConfig{
+					name: "test",
+					intervalName: func() redact.SafeValue {
+						return redact.SafeString(intervalName)
+					},
+					interval: func() time.Duration {
+						return interval
+					},
+					jitter: func() float64 {
+						return jitter
+					},
+				}, clock)
+				require.NoError(t, err)
+
+				// A non-positive interval indicates that saving is disabled so we only
+				// need to test that we can't save at all.
+				if interval <= 0 {
+					require.False(t, l.canSave(ctx))
+					clock.Advance(24 * time.Hour)
+					require.False(t, l.canSave(ctx))
+					return
+				}
+
+				// We can do one save right away if the interval.
+				require.True(t, l.canSave(ctx))
+				l.doneSave(0 /* saveDuration */)
+
+				// Can't immediately save again.
+				require.False(t, l.canSave(ctx))
+
+				// Make sure interval and jitter works correctly.
+				var maxJitter time.Duration
+				if jitter > 0 {
+					maxJitter = time.Duration(jitter * float64(interval))
+				}
+				clock.Advance(interval + maxJitter)
+				require.True(t, l.canSave(ctx))
+
+				// Set the save duration to something high to make sure we can't save
+				// due to high average save duration.
+				l.doneSave(time.Hour)
+				clock.Advance(interval + maxJitter)
+				require.False(t, l.canSave(ctx))
+				clock.Advance(time.Hour - (interval + maxJitter))
+				require.True(t, l.canSave(ctx))
+
+				// Set the save duration to something even higher to make sure the
+				// average algorithm works.
+				l.doneSave(2 * time.Hour)
+				clock.Advance(time.Hour)
+				require.False(t, l.canSave(ctx))
+				clock.Advance(time.Hour)
+				require.True(t, l.canSave(ctx))
+				l.doneSave(0 /* saveDuration */)
+			})
+		}
+	}
+}
+
+func TestSaveRateLimiterError(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	name := redact.SafeString("test")
+	intervalName := func() redact.SafeValue {
+		return redact.SafeString("interval")
+	}
+	interval := func() time.Duration {
+		return 30 * time.Second
+	}
+
+	for testName, tc := range map[string]struct {
+		config      saveRateConfig
+		expectedErr string
+	}{
+		"missing name": {
+			config: saveRateConfig{
+				intervalName: intervalName,
+				interval:     interval,
+			},
+			expectedErr: "name is required",
+		},
+		"missing interval name": {
+			config: saveRateConfig{
+				name:     name,
+				interval: interval,
+			},
+			expectedErr: "interval name is required",
+		},
+		"missing interval": {
+			config: saveRateConfig{
+				name:         name,
+				intervalName: intervalName,
+			},
+			expectedErr: "interval is required",
+		},
+	} {
+		t.Run(testName, func(t *testing.T) {
+			if tc.expectedErr == "" {
+				t.Fatal("missing expected error")
+			}
+			_, err := newSaveRateLimiter(tc.config, timeutil.DefaultTimeSource{})
+			require.ErrorContains(t, err, tc.expectedErr)
+		})
+	}
+}

pkg/ccl/changefeedccl/BUILD.bazel
@@ -203,6 +203,7 @@ go_test(
         "alter_changefeed_test.go",
         "changefeed_dist_test.go",
         "changefeed_job_info_test.go",
+        "changefeed_processors_test.go",
         "changefeed_progress_test.go",
         "changefeed_stmt_test.go",
         "changefeed_test.go",

pkg/ccl/changefeedccl/alter_changefeed_test.go
@@ -1524,13 +1524,7 @@ func TestAlterChangefeedAddTargetsDuringBackfill(t *testing.T) {
 			defer rndMu.Unlock()
 
 			if r.Span.Equal(fooTableSpan) {
-				// Do not emit resolved events for the entire table span.
-				// We "simulate" large table by splitting single table span into many parts, so
-				// we want to resolve those sub-spans instead of the entire table span.
-				// However, we have to emit something -- otherwise the entire changefeed
-				// machine would not work.
-				r.Span.EndKey = fooTableSpan.Key.Next()
-				return false, nil
+				return true, nil
 			}
 			if haveGaps {
 				return rndMu.rnd.Intn(10) > 7, nil

pkg/ccl/changefeedccl/changefeed_test.go
@@ -9299,7 +9299,7 @@ func TestCoreChangefeedBackfillScanCheckpoint(t *testing.T) {
 			return nil
 		}
 
-		foo := feed(t, f, `CREATE CHANGEFEED FOR TABLE foo`)
+		foo := feed(t, f, `CREATE CHANGEFEED FOR TABLE foo WITH min_checkpoint_frequency='1ns'`)
 		defer closeFeed(t, foo)
 
 		payloads := make([]string, rowCount+1)
@@ -9362,139 +9362,6 @@ func TestCheckpointFrequency(t *testing.T) {
 	js.checkpointCompleted(ctx, 42*time.Second)
 	require.Equal(t, completionTime, js.lastProgressUpdate)
 	require.False(t, js.progressUpdatesSkipped)
-}
-
-func TestFlushJitter(t *testing.T) {
-	defer leaktest.AfterTest(t)()
-	defer log.Scope(t).Close(t)
-
-	// Test the logic around applying jitter to the flush logic.
-	// The more involved test that would try to capture flush times would likely
-	// be pretty flaky due to the fact that flush times do not happen at exactly
-	// min_flush_frequency period, and thus it would be hard to tell if the
-	// difference is due to jitter or not.  Just verify nextFlushWithJitter function
-	// works as expected with controlled time source.
-
-	ts := timeutil.NewManualTime(timeutil.Now())
-	const numIters = 100
-
-	for _, tc := range []struct {
-		flushFrequency        time.Duration
-		jitter                float64
-		expectedFlushDuration time.Duration
-		expectedErr           bool
-	}{
-		// Negative jitter.
-		{
-			flushFrequency:        -1,
-			jitter:                -0.1,
-			expectedFlushDuration: 0,
-			expectedErr:           true,
-		},
-		{
-			flushFrequency:        0,
-			jitter:                -0.1,
-			expectedFlushDuration: 0,
-			expectedErr:           true,
-		},
-		{
-			flushFrequency:        10 * time.Millisecond,
-			jitter:                -0.1,
-			expectedFlushDuration: 0,
-			expectedErr:           true,
-		},
-		{
-			flushFrequency:        100 * time.Millisecond,
-			jitter:                -0.1,
-			expectedFlushDuration: 0,
-			expectedErr:           true,
-		},
-		// Disable Jitter.
-		{
-			flushFrequency:        -1,
-			jitter:                0,
-			expectedFlushDuration: 0,
-			expectedErr:           true,
-		},
-		{
-			flushFrequency:        0,
-			jitter:                0,
-			expectedFlushDuration: 0,
-			expectedErr:           false,
-		},
-		{
-			flushFrequency:        10 * time.Millisecond,
-			jitter:                0,
-			expectedFlushDuration: 10 * time.Millisecond,
-			expectedErr:           false,
-		},
-		{
-			flushFrequency:        100 * time.Millisecond,
-			jitter:                0,
-			expectedFlushDuration: 100 * time.Millisecond,
-			expectedErr:           false,
-		},
-		// Enable Jitter.
-		{
-			flushFrequency:        -1,
-			jitter:                0.1,
-			expectedFlushDuration: 0,
-			expectedErr:           true,
-		},
-		{
-			flushFrequency:        0,
-			jitter:                0.1,
-			expectedFlushDuration: 0,
-			expectedErr:           false,
-		},
-		{
-			flushFrequency:        10 * time.Millisecond,
-			jitter:                0.1,
-			expectedFlushDuration: 10 * time.Millisecond,
-			expectedErr:           false,
-		},
-		{
-			flushFrequency:        100 * time.Millisecond,
-			jitter:                0.1,
-			expectedFlushDuration: 100 * time.Millisecond,
-			expectedErr:           false,
-		},
-		// Expect actual jitter to be 0 since flushFrequency * jitter < 1.
-		{
-			flushFrequency:        1,
-			jitter:                0.1,
-			expectedFlushDuration: 1,
-			expectedErr:           false,
-		},
-		// Expect actual jitter to be 0 since flushFrequency * jitter < 1.
-		{
-			flushFrequency:        10,
-			jitter:                0.01,
-			expectedFlushDuration: 10,
-			expectedErr:           false,
-		},
-	} {
-		t.Run(fmt.Sprintf("flushfrequency=%sjitter=%f", tc.flushFrequency, tc.jitter), func(t *testing.T) {
-			for i := 0; i < numIters; i++ {
-				next, err := nextFlushWithJitter(ts, tc.flushFrequency, tc.jitter)
-				if tc.expectedErr {
-					require.Error(t, err)
-				} else {
-					require.NoError(t, err)
-				}
-				if tc.jitter > 0 {
-					minBound := tc.expectedFlushDuration
-					maxBound := tc.expectedFlushDuration + time.Duration(float64(tc.expectedFlushDuration)*tc.jitter)
-					actualDuration := next.Sub(ts.Now())
-					require.LessOrEqual(t, minBound, actualDuration)
-					require.LessOrEqual(t, actualDuration, maxBound)
-				} else {
-					require.Equal(t, tc.expectedFlushDuration, next.Sub(ts.Now()))
-				}
-				ts.AdvanceTo(next)
-			}
-		})
-	}
 }
 
 func TestChangefeedOrderingWithErrors(t *testing.T) {

pkg/ccl/changefeedccl/show_changefeed_jobs_test.go
@@ -129,7 +129,7 @@ func TestShowChangefeedJobsShowsHighWaterTimestamp(t *testing.T) {
 	testFn := func(t *testing.T, s TestServer, f cdctest.TestFeedFactory) {
 		sqlDB := sqlutils.MakeSQLRunner(s.DB)
 		sqlDB.Exec(t, `CREATE TABLE foo(a INT PRIMARY KEY)`)
-		foo := feed(t, f, `CREATE CHANGEFEED FOR foo WITH resolved='0s',min_checkpoint_frequency='0s'`)
+		foo := feed(t, f, `CREATE CHANGEFEED FOR foo WITH resolved, min_checkpoint_frequency='1s'`)
 
 		defer closeFeed(t, foo)
 

pkg/cmd/roachtest/tests/cdc.go
@@ -1039,6 +1039,7 @@ type cdcCheckpointType int
 const (
 	cdcNormalCheckpoint cdcCheckpointType = iota
 	cdcShutdownCheckpoint
+	cdcFrontierPersistence
 )
 
 // runCDCInitialScanRollingRestart runs multiple initial-scan-only changefeeds
@@ -1086,8 +1087,9 @@ func runCDCInitialScanRollingRestart(
 	// Setup a large table with 1M rows and a small table with 5 rows.
 	// Keep ranges off n1 so that our plans use 2, 3, and 4.
 	const (
-		largeRowCount = 1000000
-		smallRowCount = 5
+		largeRowCount   = 1000000
+		largeSplitCount = 500
+		smallRowCount   = 5
 	)
 	t.L().Printf("setting up test data...")
 	setupStmts := []string{
@@ -1096,24 +1098,31 @@ func runCDCInitialScanRollingRestart(
 		`ALTER TABLE large SCATTER`,
 		fmt.Sprintf(`CREATE TABLE small (id PRIMARY KEY) AS SELECT generate_series(%d, %d)`, largeRowCount+1, largeRowCount+smallRowCount),
 		`ALTER TABLE small SCATTER`,
+		// Split some bigger chunks up to scatter it a bit more.
+		fmt.Sprintf(`ALTER TABLE large SPLIT AT SELECT id FROM large ORDER BY random() LIMIT %d`, largeSplitCount/4),
+		`ALTER TABLE large SCATTER`,
+		// Finish splitting, so that drained ranges spread out evenly.
+		fmt.Sprintf(`ALTER TABLE large SPLIT AT SELECT id FROM large ORDER BY random() LIMIT %d`, largeSplitCount),
+		`ALTER TABLE large SCATTER`,
 	}
 	switch checkpointType {
 	case cdcNormalCheckpoint:
 		setupStmts = append(setupStmts,
-			`SET CLUSTER SETTING changefeed.span_checkpoint.interval = '1s'`,
+			`SET CLUSTER SETTING changefeed.span_checkpoint.interval = '5s'`,
 			`SET CLUSTER SETTING changefeed.shutdown_checkpoint.enabled = 'false'`,
+			`SET CLUSTER SETTING changefeed.progress.frontier_persistence.interval = '10m'`,
 		)
 	case cdcShutdownCheckpoint:
-		const largeSplitCount = 5
 		setupStmts = append(setupStmts,
 			`SET CLUSTER SETTING changefeed.span_checkpoint.interval = '0'`,
 			`SET CLUSTER SETTING changefeed.shutdown_checkpoint.enabled = 'true'`,
-			// Split some bigger chunks up to scatter it a bit more.
-			fmt.Sprintf(`ALTER TABLE large SPLIT AT SELECT id FROM large ORDER BY random() LIMIT %d`, largeSplitCount/4),
-			`ALTER TABLE large SCATTER`,
-			// Finish splitting, so that drained ranges spread out evenly.
-			fmt.Sprintf(`ALTER TABLE large SPLIT AT SELECT id FROM large ORDER BY random() LIMIT %d`, largeSplitCount),
-			`ALTER TABLE large SCATTER`,
+			`SET CLUSTER SETTING changefeed.progress.frontier_persistence.interval = '10m'`,
+		)
+	case cdcFrontierPersistence:
+		setupStmts = append(setupStmts,
+			`SET CLUSTER SETTING changefeed.span_checkpoint.interval = '0'`,
+			`SET CLUSTER SETTING changefeed.shutdown_checkpoint.enabled = 'false'`,
+			`SET CLUSTER SETTING changefeed.progress.frontier_persistence.interval = '5s'`,
 		)
 	}
 	for _, s := range setupStmts {
@@ -1179,7 +1188,9 @@ func runCDCInitialScanRollingRestart(
 			t.L().Printf("starting changefeed %d...", i)
 			var job int
 			if err := db.QueryRow(
-				fmt.Sprintf("CREATE CHANGEFEED FOR TABLE large, small INTO 'webhook-%s/?insecure_tls_skip_verify=true' WITH initial_scan='only'", sinkURL),
+				fmt.Sprintf(`CREATE CHANGEFEED FOR TABLE large, small
+INTO 'webhook-%s/?insecure_tls_skip_verify=true'
+WITH initial_scan='only', min_checkpoint_frequency='1s'`, sinkURL),
 			).Scan(&job); err != nil {
 				t.Fatal(err)
 			}
@@ -1963,6 +1974,17 @@ func registerCDC(r registry.Registry) {
 		Timeout:          30 * time.Minute,
 		Run: func(ctx context.Context, t test.Test, c cluster.Cluster) {
 			runCDCInitialScanRollingRestart(ctx, t, c, cdcShutdownCheckpoint)
+		},
+	})
+	r.Add(registry.TestSpec{
+		Name:             "cdc/initial-scan-rolling-restart/frontier-persistence",
+		Owner:            registry.OwnerCDC,
+		Cluster:          r.MakeClusterSpec(4),
+		CompatibleClouds: registry.OnlyGCE,
+		Suites:           registry.Suites(registry.Nightly),
+		Timeout:          30 * time.Minute,
+		Run: func(ctx context.Context, t test.Test, c cluster.Cluster) {
+			runCDCInitialScanRollingRestart(ctx, t, c, cdcFrontierPersistence)
 		},
 	})
 	r.Add(registry.TestSpec{

commit e4a18ba3c7b231da698ce34519cde7a063396c35

changefeedccl: fix min_checkpoint_frequency=0 regression
In adfa8890a81c5cbc5d34a95c222137ee5f41948d, we advertently made it
so that setting `min_checkpoint_frequency` to 0 would cause the changefeed's
highwater to not advance/not send resolved timestamps. This patch fixes that.

Release note (bug fix): In a recent change that was included in 25.4+,
we inadvertently made it so that setting `min_checkpoint_frequency` to
0 would cause the changefeed's highwater to not advance/not send
resolved timestamps. This bug has been fixed. Note however that setting
`min_checkpoint_frequency` to lower than `500ms` is not recommended as
it may cause degraded changefeed performance.

pkg/ccl/changefeedccl/changefeed_processors.go
@@ -2562,7 +2562,7 @@ func newSaveRateLimiter(
 // canSave returns whether enough time has passed to save progress again.
 func (l *saveRateLimiter) canSave(ctx context.Context) bool {
 	interval := l.config.interval()
-	if interval <= 0 {
+	if interval < 0 {
 		return false
 	}
 	if l.config.jitter != nil {

pkg/ccl/changefeedccl/changefeed_processors_test.go
@@ -65,12 +65,22 @@ func TestSaveRateLimiter(t *testing.T) {
 				}, clock)
 				require.NoError(t, err)
 
-				// A non-positive interval indicates that saving is disabled so we only
+				// A negative interval indicates that saving is disabled so we only
 				// need to test that we can't save at all.
-				if interval <= 0 {
+				if interval < 0 {
 					require.False(t, l.canSave(ctx))
 					clock.Advance(24 * time.Hour)
 					require.False(t, l.canSave(ctx))
+					return
+				}
+
+				// A zero interval means save as often as possible.
+				if interval == 0 {
+					require.True(t, l.canSave(ctx))
+					l.doneSave(0 /* saveDuration */)
+					// With zero interval, we can always save again immediately.
+					require.True(t, l.canSave(ctx))
+					l.doneSave(0 /* saveDuration */)
 					return
 				}
 

pkg/ccl/changefeedccl/changefeed_progress_test.go
@@ -317,6 +317,61 @@ WITH no_initial_scan, min_checkpoint_frequency='1s', resolved, metrics_label='%s
 	})
 }
 
+// TestChangefeedProgressUpdatesWithZeroMinCheckpointFrequency verifies that
+// resolved timestamps are still sent when we set min_checkpoint_frequency='0'.
+// This is a regression test for #164866.
+func TestChangefeedProgressUpdatesWithZeroMinCheckpointFrequency(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	testFn := func(t *testing.T, s TestServer, f cdctest.TestFeedFactory) {
+		sqlDB := sqlutils.MakeSQLRunner(s.DB)
+		sqlDB.Exec(t, `CREATE TABLE foo (a INT PRIMARY KEY)`)
+		sqlDB.Exec(t, `INSERT INTO foo VALUES (1), (2), (3)`)
+
+		foo := feed(t, f, `CREATE CHANGEFEED FOR foo WITH resolved='10ms', min_checkpoint_frequency='0'`)
+		defer closeFeed(t, foo)
+
+		// Drain the initial rows.
+		assertPayloads(t, foo, []string{
+			`foo: [1]->{"after": {"a": 1}}`,
+			`foo: [2]->{"after": {"a": 2}}`,
+			`foo: [3]->{"after": {"a": 3}}`,
+		})
+
+		// Collect a resolved timestamp and verify it's non-empty.
+		var first hlc.Timestamp
+		testutils.SucceedsSoon(t, func() error {
+			ts, _ := expectResolvedTimestamp(t, foo)
+			if ts.IsEmpty() {
+				return errors.New("waiting for non-empty resolved timestamp")
+			}
+			first = ts
+			return nil
+		})
+
+		// Insert more data to push the frontier forward.
+		sqlDB.Exec(t, `INSERT INTO foo VALUES (4), (5)`)
+
+		// Drain the new rows.
+		assertPayloads(t, foo, []string{
+			`foo: [4]->{"after": {"a": 4}}`,
+			`foo: [5]->{"after": {"a": 5}}`,
+		})
+
+		// Wait for a resolved timestamp that is strictly greater than the first.
+		testutils.SucceedsSoon(t, func() error {
+			ts, _ := expectResolvedTimestamp(t, foo)
+			if !first.Less(ts) {
+				return errors.Newf("waiting for resolved timestamp to advance beyond %s", first)
+			}
+			return nil
+		})
+	}
+
+	cdcTest(t, testFn)
+}
+
 // TestCoreChangefeedProgress verifies that core (sinkless) changefeeds
 // save and restore their full span frontier across retries. It creates
 // a changefeed over two tables with one table's resolved timestamps