4fd4f07d5398c4b1feb7933d2650f4e643709e1a
Yahor Yuzefovich <yahor@cockroachlabs.com>
2026-03-10 19:08:07 -0700

    sql,stats: add automatic 'fix misestimates' stats collection
    Wire up the stats.Refresher to consume misestimate reports from the
    execution engine and issue internal CREATE STATISTICS ... WHERE
    statements that collect partial statistics on the affected table spans.
    We use `spansToBounds` added in the previous commit to construct
    a separate WHERE clause for each span for a given table.  Add a
    `sql.stats.fix_misestimates_concurrency_limit` cluster setting
    (default 128) to cap concurrent fix-up jobs.
    
    TODO: settle on the naming of the stats job (both in the system and in
    the comments / variable names), cluster setting, table-level parameter,
    etc.
    TODO: consider adding a test with "ascending key problem", with
    forecasts disabled, and ensuring that fixup partial stats continuously
    advance the "frontier".
    
    Release note: None

--- pkg/server/server_sql.go
+++ pkg/server/server_sql.go
@@ -1225,6 +1225,7 @@ func newSQLServer(ctx context.Context, cfg sqlServerArgs) (*SQLServer, error) {
 	statsRefresher := stats.MakeRefresher(
 		cfg.AmbientCtx,
 		cfg.Settings,
+		codec,
 		internalDB,
 		execCfg.TableStatsCache,
 		stats.DefaultAsOfTime,
