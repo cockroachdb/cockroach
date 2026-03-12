3238ade7b17e2bc277994a7abb0f05207e4c6a6b
Jason Fong <jason.fong@cockroachlabs.com>
2026-03-12 18:34:38 -0400

    ui: delete Redux store artifacts for jobs
    Remove all Redux infrastructure for jobs now that both the jobs list
    and job details pages use SWR hooks:
    - cluster-ui: store/jobDetails (reducer, sagas, saga tests, barrel)
    - cluster-ui: store/jobs (reducer, sagas, saga tests, barrel, jobProfiler reducer and sagas)
    - cluster-ui: unregister from combineReducers and root saga
    - db-console: apiReducers (jobs, job, jobProfiler reducers and refresh functions)
    - db-console: redux/jobs (actions, sagas) and root saga fork
    
    Epic: CRDB-58145
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/store/jobs/index.ts
+++ pkg/ui/workspaces/cluster-ui/src/store/jobs/index.ts
@@ -3,6 +3,4 @@
 // Use of this software is governed by the CockroachDB Software License
 // included in the /LICENSE file.
 
-export * from "./jobs.reducer";
-export * from "./jobs.sagas";
 export * from "./jobs.selectors";
