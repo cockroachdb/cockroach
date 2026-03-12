69fca847f18a40c46025ffcf10319a99c271a2f7
Jason Fong <jason.fong@cockroachlabs.com>
2026-03-12 18:33:15 -0400

    ui: migrate jobs list page from Redux to SWR
    Replace Redux-based data fetching in the jobs list page with the useJobs SWR hook.
    The component now calls useJobs directly with refreshInterval and keepPreviousData,
    eliminating the need for dispatch(refresh()) and mapStateToProps plumbing.
    
    JobsPageConnected is simplified to only pass localStorage/analytics concerns as props.
    The db-console wrapper is similarly simplified to use hooks instead of connect().
    
    Remove the storybook file (would break without an SWR provider) and
    clean up dead fixture exports that only the storybook consumed.
    
    Epic: CRDB-58145
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/jobs/jobsPage/jobsPageConnected.tsx
+++ pkg/ui/workspaces/cluster-ui/src/jobs/jobsPage/jobsPageConnected.tsx
@@ -3,62 +3,40 @@
 // Use of this software is governed by the CockroachDB Software License
 // included in the /LICENSE file.
 
-import { connect } from "react-redux";
-import { RouteComponentProps, withRouter } from "react-router-dom";
-import { Dispatch } from "redux";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { JobsRequest } from "src/api/jobsApi";
-import { AppState } from "src/store";
+import { SortSetting } from "../../sortedtable";
+import { actions as analyticsActions } from "../../store/analytics";
 import {
   selectShowSetting,
   selectSortSetting,
   selectTypeSetting,
   selectStatusSetting,
   selectColumns,
-  initialState,
-  actions as jobsActions,
-} from "src/store/jobs";
-
-import { SortSetting } from "../../sortedtable";
-import { actions as analyticsActions } from "../../store/analytics";
+} from "../../store/jobs/jobs.selectors";
 import { actions as localStorageActions } from "../../store/localStorage";
 
-import {
-  JobsPageStateProps,
-  JobsPageDispatchProps,
-  JobsPage,
-} from "./jobsPage";
+import { JobsPage } from "./jobsPage";
 
-const mapStateToProps = (
-  state: AppState,
-  _: RouteComponentProps,
-): JobsPageStateProps => {
-  const sort = selectSortSetting(state);
-  const status = selectStatusSetting(state);
-  const show = selectShowSetting(state);
-  const type = selectTypeSetting(state);
-  const columns = selectColumns(state);
-  return {
-    sort,
-    status,
-    show,
-    type,
-    columns,
-    jobsResponse: state.adminUI?.jobs ?? initialState,
-  };
-};
+export const JobsPageConnected: React.FC = () => {
+  const dispatch = useDispatch();
+  const sort = useSelector(selectSortSetting);
+  const status = useSelector(selectStatusSetting);
+  const show = useSelector(selectShowSetting);
+  const type = useSelector(selectTypeSetting);
+  const columns = useSelector(selectColumns);
 
-const mapDispatchToProps = (dispatch: Dispatch): JobsPageDispatchProps => ({
-  setShow: (showValue: string) => {
-    dispatch(jobsActions.invalidated());
+  const setShow = (showValue: string) => {
     dispatch(
       localStorageActions.update({
         key: "showSetting/JobsPage",
         value: showValue,
       }),
     );
-  },
-  setSort: (ss: SortSetting) => {
+  };
+
+  const setSort = (ss: SortSetting) => {
     dispatch(
       localStorageActions.update({
         key: "sortSetting/JobsPage",
@@ -73,18 +51,18 @@ const mapDispatchToProps = (dispatch: Dispatch): JobsPageDispatchProps => ({
         columnName: ss.columnTitle,
       }),
     );
-  },
-  setStatus: (statusValue: string) => {
-    dispatch(jobsActions.invalidated());
+  };
+
+  const setStatus = (statusValue: string) => {
     dispatch(
       localStorageActions.update({
         key: "statusSetting/JobsPage",
         value: statusValue,
       }),
     );
-  },
-  setType: (jobValue: number) => {
-    dispatch(jobsActions.invalidated());
+  };
+
+  const setType = (jobValue: number) => {
     dispatch(
       localStorageActions.update({
         key: "typeSetting/JobsPage",
@@ -98,35 +76,38 @@ const mapDispatchToProps = (dispatch: Dispatch): JobsPageDispatchProps => ({
         value: jobValue.toString(),
       }),
     );
-  },
-  onColumnsChange: (selectedColumns: string[]) => {
-    const columns =
+  };
+
+  const onColumnsChange = (selectedColumns: string[]) => {
+    const cols =
       selectedColumns.length === 0 ? " " : selectedColumns.join(",");
     dispatch(
       localStorageActions.update({
         key: "showColumns/JobsPage",
-        value: columns,
+        value: cols,
       }),
     );
     dispatch(
       analyticsActions.track({
         name: "Columns Selected change",
         page: "Jobs",
-        value: columns,
+        value: cols,
       }),
     );
-  },
-  refreshJobs: (req: JobsRequest) => dispatch(jobsActions.refresh(req)),
-});
+  };
 
-export const JobsPageConnected = withRouter(
-  connect<
-    JobsPageStateProps,
-    JobsPageDispatchProps,
-    RouteComponentProps,
-    AppState
-  >(
-    mapStateToProps,
-    mapDispatchToProps,
-  )(JobsPage),
-);
+  return (
+    <JobsPage
+      sort={sort}
+      status={status}
+      show={show}
+      type={type}
+      columns={columns}
+      setSort={setSort}
+      setStatus={setStatus}
+      setShow={setShow}
+      setType={setType}
+      onColumnsChange={onColumnsChange}
+    />
+  );
+};
