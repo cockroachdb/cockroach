a5fd29cd92c7211f1d2da0df36471052c1188294
Brendan Gerrity <brendan.gerrity@cockroachlabs.com>
2026-03-13 12:39:22 -0400

    sql/schemachanger: add `ALTER TYPE ... ADD VALUE` to declarative schema changer
    This change implements the add value for enum UDTs and rewires the
    command to it.
    
    Epic: CRDB-31325
    Closes: #164218
    
    Release note: None

--- pkg/sql/schemachanger/scbuild/internal/scbuildstmt/drop_type.go
+++ pkg/sql/schemachanger/scbuild/internal/scbuildstmt/drop_type.go
@@ -14,6 +14,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/catid"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqltelemetry"
+	"github.com/cockroachdb/errors"
 )
 
 // DropType implements DROP TYPE.
@@ -30,6 +31,14 @@ func DropType(b BuildCtx, n *tree.DropType) {
 		var typ scpb.Element
 		var typeID, arrayTypeID catid.DescID
 		if _, _, enum := scpb.FindEnumType(elts); enum != nil {
+			// Multi-region enums cannot be dropped directly.
+			if enum.IsMultiRegion {
+				prefix := b.NamePrefix(enum)
+				typeName := tree.MakeTypeNameWithPrefix(prefix, name.Object())
+				panic(errors.WithHintf(pgerror.Newf(pgcode.DependentObjectsStillExist,
+					"%q is a multi-region enum and cannot be modified directly", typeName.FQString()),
+					"try ALTER DATABASE DROP REGION %s", name.Object()))
+			}
 			b.IncrementEnumCounter(sqltelemetry.EnumDrop)
 			typeID, arrayTypeID = enum.TypeID, enum.ArrayTypeID
 			typ = enum
