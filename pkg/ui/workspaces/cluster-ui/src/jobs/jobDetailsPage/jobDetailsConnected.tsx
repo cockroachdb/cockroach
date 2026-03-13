4df5e5196ae395b066768d5537384e5fa81a8da7
Jason Fong <jason.fong@cockroachlabs.com>
2026-03-13 10:42:20 -0400

    ui: migrate job details page from Redux to SWR
    Replace Redux-based data fetching in the job details page with the useJobDetails SWR hook,
    including isPaused to stop polling when the job reaches a terminal state.
    
    Also migrate the admin role check from Redux (adminRoleSelector prop + refreshUserSQLRoles dispatch)
    to the useUserSQLRoles SWR hook, deriving hasAdminRole directly from the hook response.
    This removes the AppState generic and simplifies both the cluster-ui connected component and
    the db-console wrapper.
    
    Update tests to mock useUserSQLRoles via jest.spyOn and wrap in SWRConfig
    instead of a Redux Provider.
    
    Epic: CRDB-58145
    Release note: None

--- pkg/ui/workspaces/cluster-ui/src/jobs/jobDetailsPage/jobDetailsConnected.tsx
+++ pkg/ui/workspaces/cluster-ui/src/jobs/jobDetailsPage/jobDetailsConnected.tsx
@@ -3,69 +3,22 @@
 // Use of this software is governed by the CockroachDB Software License
 // included in the /LICENSE file.
 
-import long from "long";
-import { connect } from "react-redux";
-import { RouteComponentProps, withRouter } from "react-router-dom";
-import { Dispatch } from "redux";
+import React from "react";
 
 import {
-  ListJobProfilerExecutionDetailsRequest,
-  createInitialState,
+  listExecutionDetailFiles,
+  collectExecutionDetails,
   getExecutionDetailFile,
 } from "src/api";
-import { JobRequest, JobResponse } from "src/api/jobsApi";
-import { AppState, uiConfigActions } from "src/store";
-import { actions as jobActions } from "src/store/jobDetails";
-import {
-  initialState,
-  actions as jobProfilerActions,
-} from "src/store/jobs/jobProfiler.reducer";
-import { selectHasAdminRole } from "src/store/uiConfig";
 
-import { selectID } from "../../selectors";
+import { JobDetailsV2 } from "./jobDetails";
 
-import {
-  JobDetailsStateProps,
-  JobDetailsDispatchProps,
-  JobDetails,
-} from "./jobDetails";
-
-const emptyState = createInitialState<JobResponse>();
-
-const mapStateToProps = (
-  state: AppState,
-  props: RouteComponentProps,
-): JobDetailsStateProps => {
-  const jobID = selectID(state, props);
-  return {
-    jobRequest: state.adminUI?.job?.cachedData[jobID] ?? emptyState,
-    jobProfilerExecutionDetailFilesResponse:
-      state.adminUI?.executionDetailFiles ?? initialState,
-    jobProfilerLastUpdated: state.adminUI?.executionDetailFiles?.lastUpdated,
-    jobProfilerDataIsValid: state.adminUI?.executionDetailFiles?.valid,
-    onDownloadExecutionFileClicked: getExecutionDetailFile,
-    hasAdminRole: selectHasAdminRole(state),
-  };
+export const JobDetailsPageConnected: React.FC = () => {
+  return (
+    <JobDetailsV2
+      onFetchExecutionDetailFiles={listExecutionDetailFiles}
+      onCollectExecutionDetails={collectExecutionDetails}
+      onDownloadExecutionFile={getExecutionDetailFile}
+    />
+  );
 };
-
-const mapDispatchToProps = (dispatch: Dispatch): JobDetailsDispatchProps => ({
-  refreshJob: (req: JobRequest) => dispatch(jobActions.refresh(req)),
-  refreshExecutionDetailFiles: (req: ListJobProfilerExecutionDetailsRequest) =>
-    dispatch(jobProfilerActions.refresh(req)),
-  onRequestExecutionDetails: (jobID: long) => {
-    dispatch(jobProfilerActions.collectExecutionDetails({ job_id: jobID }));
-  },
-  refreshUserSQLRoles: () => dispatch(uiConfigActions.refreshUserSQLRoles()),
-});
-
-export const JobDetailsPageConnected = withRouter(
-  connect<
-    JobDetailsStateProps,
-    JobDetailsDispatchProps,
-    RouteComponentProps,
-    AppState
-  >(
-    mapStateToProps,
-    mapDispatchToProps,
-  )(JobDetails),
-);
