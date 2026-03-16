6e272fdeb9c4c7150eb1d39bddff5ed3e14312fc
Pavel Kalinnikov <pavel@cockroachlabs.com>
2026-03-16 02:22:48 +0000

    kvserver: wire in BatchFactory in dd test
    Epic: none
    Release note: none

--- pkg/kv/kvserver/replica_lifecycle_datadriven_test.go
+++ pkg/kv/kvserver/replica_lifecycle_datadriven_test.go
@@ -669,12 +669,13 @@ type testCtx struct {
 	ranges      map[roachpb.RangeID]*rangeState
 	splits      map[roachpb.RangeID]pendingSplit
 	merges      map[roachpb.RangeID]pendingMerge
+
 	// The storage engines correspond to a single store, (n1, s1). The engines
 	// are logically separated into two -- one for the state machine, and one
 	// for Raft.
-	eng kvstorage.Engines
-
+	eng    kvstorage.Engines
 	wagSeq wag.Seq
+	bf     kvstorage.BatchFactory
 }
 
 // newTestCtx constructs and returns a new testCtx.
@@ -682,7 +683,7 @@ func newTestCtx() *testCtx {
 	st := cluster.MakeTestingClusterSettings()
 	manual := timeutil.NewManualTime(timeutil.Unix(0, 10))
 	clock := hlc.NewClockForTesting(manual)
-	return &testCtx{
+	tc := &testCtx{
 		st:                 st,
 		clock:              clock,
 		rangeLeaseDuration: 99 * time.Nanosecond,
@@ -696,6 +697,8 @@ func newTestCtx() *testCtx {
 			storage.NewDefaultInMemForTesting(),
 		),
 	}
+	tc.bf = kvstorage.MakeBatchFactory(&tc.eng, &tc.wagSeq)
+	return tc
 }
 
 // makeEvalCtx constructs a batcheval.EvalContext for the given range.
