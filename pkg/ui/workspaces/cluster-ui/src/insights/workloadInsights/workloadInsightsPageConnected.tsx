d2e4f11561ca628b7edc58fcb1a16efa6217ae0e
Jason Fong <jason.fong@cockroachlabs.com>
2026-03-17 11:35:06 -0400

    ui: convert insights overview containers from connect() to hooks
    The workload insights page containers in both cluster-ui and db-console
    use the connect() HOC with mapStateToProps/mapDispatchToProps to wire up
    Redux state and dispatch to the child views.
    
    Replace connect()/withRouter with functional components using
    useDispatch/useSelector/useCallback. Since the child views now fetch their
    data via SWR, the containers no longer pass data, loading, or refresh props.
    Use separate per-tab handler functions to preserve correct analytics
    page names for transaction vs statement tabs.
    
    Part of CRDB-61195
    Epic: CRDB-58145
    Release Note: None

--- pkg/ui/workspaces/cluster-ui/src/insights/workloadInsights/workloadInsightsPageConnected.tsx
+++ pkg/ui/workspaces/cluster-ui/src/insights/workloadInsights/workloadInsightsPageConnected.tsx
@@ -3,251 +3,196 @@
 // Use of this software is governed by the CockroachDB Software License
 // included in the /LICENSE file.
 
-import { connect } from "react-redux";
-import { RouteComponentProps, withRouter } from "react-router-dom";
-import { Dispatch } from "redux";
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { StmtInsightsReq, TxnInsightsRequest } from "src/api";
-import { selectStmtInsights } from "src/selectors/common";
+import { InsightEnumToLabel } from "src/insights";
 import { SortSetting } from "src/sortedtable";
-import { AppState } from "src/store";
-import {
-  actions as statementInsights,
-  selectColumns,
-  selectInsightTypes,
-  selectStmtInsightsError,
-  selectStmtInsightsLoading,
-  selectStmtInsightsMaxApiReached,
-} from "src/store/insights/statementInsights";
-import {
-  actions as txnInsights,
-  selectTransactionInsights,
-  selectTransactionInsightsError,
-  selectFilters,
-  selectSortSetting,
-  selectTransactionInsightsLoading,
-  selectTransactionInsightsMaxApiReached,
-} from "src/store/insights/transactionInsights";
 import { actions as localStorageActions } from "src/store/localStorage";
-import { actions as sqlActions } from "src/store/sqlStats";
+import { localStorageSelector } from "src/store/utils/selectors";
 
 import { actions as analyticsActions } from "../../store/analytics";
+import { actions as sqlActions } from "../../store/sqlStats";
 import { selectTimeScale } from "../../store/utils/selectors";
 import { TimeScale } from "../../timeScaleDropdown";
 import { WorkloadInsightEventFilters } from "../types";
 
-import {
-  StatementInsightsViewDispatchProps,
-  StatementInsightsViewStateProps,
-} from "./statementInsights";
-import {
-  TransactionInsightsViewDispatchProps,
-  TransactionInsightsViewStateProps,
-} from "./transactionInsights";
-import {
-  WorkloadInsightsViewProps,
-  WorkloadInsightsRootControl,
-} from "./workloadInsightRootControl";
+import { WorkloadInsightsRootControl } from "./workloadInsightRootControl";
 
-const transactionMapStateToProps = (
-  state: AppState,
-  _props: RouteComponentProps,
-): TransactionInsightsViewStateProps => ({
-  isDataValid: state.adminUI?.txnInsights?.valid,
-  lastUpdated: state.adminUI?.txnInsights.lastUpdated,
-  transactions: selectTransactionInsights(state),
-  transactionsError: selectTransactionInsightsError(state),
-  insightTypes: selectInsightTypes(),
-  filters: selectFilters(state),
-  sortSetting: selectSortSetting(state),
-  timeScale: selectTimeScale(state),
-  isLoading: selectTransactionInsightsLoading(state),
-  maxSizeApiReached: selectTransactionInsightsMaxApiReached(state),
-});
-
-const statementMapStateToProps = (
-  state: AppState,
-  _props: RouteComponentProps,
-): StatementInsightsViewStateProps => ({
-  isDataValid: state.adminUI?.stmtInsights?.valid,
-  lastUpdated: state.adminUI?.stmtInsights.lastUpdated,
-  statements: selectStmtInsights(state),
-  statementsError: selectStmtInsightsError(state),
-  insightTypes: selectInsightTypes(),
-  filters: selectFilters(state),
-  sortSetting: selectSortSetting(state),
-  selectedColumnNames: selectColumns(state),
-  timeScale: selectTimeScale(state),
-  isLoading: selectStmtInsightsLoading(state),
-  maxSizeApiReached: selectStmtInsightsMaxApiReached(state),
-});
-
-const TransactionDispatchProps = (
-  dispatch: Dispatch,
-): TransactionInsightsViewDispatchProps => ({
-  onFiltersChange: (filters: WorkloadInsightEventFilters) => {
-    dispatch(
-      localStorageActions.update({
-        key: "filters/InsightsPage",
-        value: filters,
-      }),
-    );
-    dispatch(
-      analyticsActions.track({
-        name: "Filter Clicked",
-        page: "Workload Insights - Transaction",
-        filterName: "filters",
-        value: filters.toString(),
-      }),
-    );
-  },
-  onSortChange: (ss: SortSetting) => {
-    dispatch(
-      localStorageActions.update({
-        key: "sortSetting/InsightsPage",
-        value: ss,
-      }),
-    );
-    dispatch(
-      analyticsActions.track({
-        name: "Column Sorted",
-        page: "Workload Insights - Transaction",
-        tableName: "Workload Transaction Insights Table",
-        columnName: ss.columnTitle,
-      }),
-    );
-  },
-  setTimeScale: (ts: TimeScale) => {
-    dispatch(
-      sqlActions.updateTimeScale({
-        ts: ts,
-      }),
-    );
-    dispatch(
-      analyticsActions.track({
-        name: "TimeScale changed",
-        page: "Workload Insights - Transaction",
-        value: ts.key,
-      }),
-    );
-  },
-  refreshTransactionInsights: (req: TxnInsightsRequest) => {
-    dispatch(txnInsights.refresh(req));
-  },
-});
-
-const StatementDispatchProps = (
-  dispatch: Dispatch,
-): StatementInsightsViewDispatchProps => ({
-  onFiltersChange: (filters: WorkloadInsightEventFilters) => {
-    dispatch(
-      localStorageActions.update({
-        key: "filters/InsightsPage",
-        value: filters,
-      }),
-    );
-    dispatch(
-      analyticsActions.track({
-        name: "Filter Clicked",
-        page: "Workload Insights - Statement",
-        filterName: "filters",
-        value: filters.toString(),
-      }),
-    );
-  },
-  onSortChange: (ss: SortSetting) => {
-    dispatch(
-      localStorageActions.update({
-        key: "sortSetting/InsightsPage",
-        value: ss,
-      }),
-    );
-    dispatch(
-      analyticsActions.track({
-        name: "Column Sorted",
-        page: "Workload Insights - Statement",
-        tableName: "Workload Statement Insights Table",
-        columnName: ss.columnTitle,
-      }),
-    );
-  },
-  // We use `null` when the value was never set and it will show all columns.
-  // If the user modifies the selection and no columns are selected,
-  // the function will save the value as a blank space, otherwise
-  // it gets saved as `null`.
-  onColumnsChange: (value: string[]) => {
-    const columns = value.length === 0 ? " " : value.join(",");
-    dispatch(
-      localStorageActions.update({
-        key: "showColumns/StatementInsightsPage",
-        value: columns,
-      }),
-    );
-    dispatch(
-      analyticsActions.track({
-        name: "Columns Selected change",
-        page: "Workload Insights - Statement",
-        value: columns,
-      }),
-    );
-  },
-  setTimeScale: (ts: TimeScale) => {
-    dispatch(
-      sqlActions.updateTimeScale({
-        ts: ts,
-      }),
-    );
-    dispatch(
-      analyticsActions.track({
-        name: "TimeScale changed",
-        page: "Workload Insights - Statement",
-        value: ts.key,
-      }),
-    );
-  },
-  refreshStatementInsights: (req: StmtInsightsReq) => {
-    dispatch(statementInsights.refresh(req));
-  },
-});
-
-type StateProps = {
-  transactionInsightsViewStateProps: TransactionInsightsViewStateProps;
-  statementInsightsViewStateProps: StatementInsightsViewStateProps;
+const selectInsightTypes = (): string[] => {
+  const insights: string[] = [];
+  InsightEnumToLabel.forEach(insight => {
+    insights.push(insight);
+  });
+  return insights;
 };
 
-type DispatchProps = {
-  transactionInsightsViewDispatchProps: TransactionInsightsViewDispatchProps;
-  statementInsightsViewDispatchProps: StatementInsightsViewDispatchProps;
-};
+export const WorkloadInsightsPageConnected: React.FC = () => {
+  const dispatch = useDispatch();
+  const timeScale = useSelector(selectTimeScale);
+  const localStorage = useSelector(localStorageSelector);
+  const filters = localStorage["filters/InsightsPage"];
+  const sortSetting = localStorage["sortSetting/InsightsPage"];
+  const selectedColumnNames = localStorage["showColumns/StatementInsightsPage"]
+    ? localStorage["showColumns/StatementInsightsPage"]?.split(",")
+    : null;
 
-export const WorkloadInsightsPageConnected = withRouter(
-  connect<
-    StateProps,
-    DispatchProps,
-    RouteComponentProps,
-    WorkloadInsightsViewProps,
-    AppState
-  >(
-    (state: AppState, props: RouteComponentProps) => ({
-      transactionInsightsViewStateProps: transactionMapStateToProps(
-        state,
-        props,
-      ),
-      statementInsightsViewStateProps: statementMapStateToProps(state, props),
-    }),
-    dispatch => ({
-      transactionInsightsViewDispatchProps: TransactionDispatchProps(dispatch),
-      statementInsightsViewDispatchProps: StatementDispatchProps(dispatch),
-    }),
-    (stateProps, dispatchProps) => ({
-      transactionInsightsViewProps: {
-        ...stateProps.transactionInsightsViewStateProps,
-        ...dispatchProps.transactionInsightsViewDispatchProps,
-      },
-      statementInsightsViewProps: {
-        ...stateProps.statementInsightsViewStateProps,
-        ...dispatchProps.statementInsightsViewDispatchProps,
-      },
-    }),
-  )(WorkloadInsightsRootControl),
-);
+  // --- Transaction tab handlers ---
+
+  const txnOnFiltersChange = useCallback(
+    (f: WorkloadInsightEventFilters) => {
+      dispatch(
+        localStorageActions.update({
+          key: "filters/InsightsPage",
+          value: f,
+        }),
+      );
+      dispatch(
+        analyticsActions.track({
+          name: "Filter Clicked",
+          page: "Workload Insights - Transaction",
+          filterName: "filters",
+          value: f.toString(),
+        }),
+      );
+    },
+    [dispatch],
+  );
+
+  const txnOnSortChange = useCallback(
+    (ss: SortSetting) => {
+      dispatch(
+        localStorageActions.update({
+          key: "sortSetting/InsightsPage",
+          value: ss,
+        }),
+      );
+      dispatch(
+        analyticsActions.track({
+          name: "Column Sorted",
+          page: "Workload Insights - Transaction",
+          tableName: "Workload Transaction Insights Table",
+          columnName: ss.columnTitle,
+        }),
+      );
+    },
+    [dispatch],
+  );
+
+  const txnSetTimeScale = useCallback(
+    (ts: TimeScale) => {
+      dispatch(sqlActions.updateTimeScale({ ts }));
+      dispatch(
+        analyticsActions.track({
+          name: "TimeScale changed",
+          page: "Workload Insights - Transaction",
+          value: ts.key,
+        }),
+      );
+    },
+    [dispatch],
+  );
+
+  // --- Statement tab handlers ---
+
+  const stmtOnFiltersChange = useCallback(
+    (f: WorkloadInsightEventFilters) => {
+      dispatch(
+        localStorageActions.update({
+          key: "filters/InsightsPage",
+          value: f,
+        }),
+      );
+      dispatch(
+        analyticsActions.track({
+          name: "Filter Clicked",
+          page: "Workload Insights - Statement",
+          filterName: "filters",
+          value: f.toString(),
+        }),
+      );
+    },
+    [dispatch],
+  );
+
+  const stmtOnSortChange = useCallback(
+    (ss: SortSetting) => {
+      dispatch(
+        localStorageActions.update({
+          key: "sortSetting/InsightsPage",
+          value: ss,
+        }),
+      );
+      dispatch(
+        analyticsActions.track({
+          name: "Column Sorted",
+          page: "Workload Insights - Statement",
+          tableName: "Workload Statement Insights Table",
+          columnName: ss.columnTitle,
+        }),
+      );
+    },
+    [dispatch],
+  );
+
+  const stmtSetTimeScale = useCallback(
+    (ts: TimeScale) => {
+      dispatch(sqlActions.updateTimeScale({ ts }));
+      dispatch(
+        analyticsActions.track({
+          name: "TimeScale changed",
+          page: "Workload Insights - Statement",
+          value: ts.key,
+        }),
+      );
+    },
+    [dispatch],
+  );
+
+  const onColumnsChange = useCallback(
+    (value: string[]) => {
+      const columns = value.length === 0 ? " " : value.join(",");
+      dispatch(
+        localStorageActions.update({
+          key: "showColumns/StatementInsightsPage",
+          value: columns,
+        }),
+      );
+      dispatch(
+        analyticsActions.track({
+          name: "Columns Selected change",
+          page: "Workload Insights - Statement",
+          value: columns,
+        }),
+      );
+    },
+    [dispatch],
+  );
+
+  const insightTypes = selectInsightTypes();
+
+  return (
+    <WorkloadInsightsRootControl
+      transactionInsightsViewProps={{
+        insightTypes,
+        filters,
+        sortSetting,
+        timeScale,
+        onFiltersChange: txnOnFiltersChange,
+        onSortChange: txnOnSortChange,
+        setTimeScale: txnSetTimeScale,
+      }}
+      statementInsightsViewProps={{
+        insightTypes,
+        filters,
+        sortSetting,
+        selectedColumnNames,
+        timeScale,
+        onFiltersChange: stmtOnFiltersChange,
+        onSortChange: stmtOnSortChange,
+        onColumnsChange,
+        setTimeScale: stmtSetTimeScale,
+      }}
+    />
+  );
+};
--- pkg/ui/workspaces/db-console/src/views/insights/workloadInsightsPage.tsx
+++ pkg/ui/workspaces/db-console/src/views/insights/workloadInsightsPage.tsx
@@ -6,33 +6,18 @@
 import {
   WorkloadInsightEventFilters,
   SortSetting,
-  StatementInsightsViewDispatchProps,
-  StatementInsightsViewStateProps,
-  TransactionInsightsViewDispatchProps,
-  TransactionInsightsViewStateProps,
   WorkloadInsightsRootControl,
-  WorkloadInsightsViewProps,
+  InsightEnumToLabel,
+  TimeScale,
+  defaultFilters,
 } from "@cockroachlabs/cluster-ui";
-import { connect } from "react-redux";
-import { RouteComponentProps, withRouter } from "react-router-dom";
-import { bindActionCreators } from "redux";
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { refreshStmtInsights, refreshTxnInsights } from "src/redux/apiReducers";
 import { LocalSetting } from "src/redux/localsettings";
 import { AdminUIState } from "src/redux/state";
 import { setGlobalTimeScaleAction } from "src/redux/statements";
 import { selectTimeScale } from "src/redux/timeScale";
-import {
-  filtersLocalSetting,
-  selectStmtInsights,
-  sortSettingLocalSetting,
-  selectTransactionInsights,
-  selectStmtInsightsLoading,
-  selectTransactionInsightsLoading,
-  selectInsightTypes,
-  selectStmtInsightsMaxApiReached,
-  selectTxnInsightsMaxApiReached,
-} from "src/views/insights/insightsSelectors";
 
 export const insightStatementColumnsLocalSetting = new LocalSetting<
   AdminUIState,
@@ -43,104 +28,94 @@ export const insightStatementColumnsLocalSetting = new LocalSetting<
   null,
 );
 
-const transactionMapStateToProps = (
-  state: AdminUIState,
-  _props: RouteComponentProps,
-): TransactionInsightsViewStateProps => ({
-  isDataValid: state.cachedData.txnInsights?.valid,
-  lastUpdated: state.cachedData.txnInsights?.setAt,
-  transactions: selectTransactionInsights(state),
-  insightTypes: selectInsightTypes(),
-  transactionsError: state.cachedData?.txnInsights?.lastError,
-  filters: filtersLocalSetting.selector(state),
-  sortSetting: sortSettingLocalSetting.selector(state),
-  timeScale: selectTimeScale(state),
-  isLoading: selectTransactionInsightsLoading(state),
-  maxSizeApiReached: selectTxnInsightsMaxApiReached(state),
+const filtersLocalSetting = new LocalSetting<
+  AdminUIState,
+  WorkloadInsightEventFilters
+>("filters/InsightsPage", (state: AdminUIState) => state.localSettings, {
+  app: defaultFilters.app,
+  workloadInsightType: defaultFilters.workloadInsightType,
 });
 
-const statementMapStateToProps = (
-  state: AdminUIState,
-  _props: RouteComponentProps,
-): StatementInsightsViewStateProps => ({
-  isDataValid: state.cachedData.stmtInsights?.valid,
-  lastUpdated: state.cachedData.stmtInsights?.setAt,
-  statements: selectStmtInsights(state),
-  statementsError: state.cachedData?.stmtInsights?.lastError,
-  filters: filtersLocalSetting.selector(state),
-  insightTypes: selectInsightTypes(),
-  sortSetting: sortSettingLocalSetting.selector(state),
-  selectedColumnNames:
-    insightStatementColumnsLocalSetting.selectorToArray(state),
-  timeScale: selectTimeScale(state),
-  isLoading: selectStmtInsightsLoading(state),
-  maxSizeApiReached: selectStmtInsightsMaxApiReached(state),
-});
-
-const TransactionDispatchProps = {
-  onFiltersChange: (filters: WorkloadInsightEventFilters) =>
-    filtersLocalSetting.set(filters),
-  onSortChange: (ss: SortSetting) => sortSettingLocalSetting.set(ss),
-  setTimeScale: setGlobalTimeScaleAction,
-  refreshTransactionInsights: refreshTxnInsights,
-};
-
-const StatementDispatchProps: StatementInsightsViewDispatchProps = {
-  onFiltersChange: (filters: WorkloadInsightEventFilters) =>
-    filtersLocalSetting.set(filters),
-  onSortChange: (ss: SortSetting) => sortSettingLocalSetting.set(ss),
-  refreshStatementInsights: refreshStmtInsights,
-  onColumnsChange: (value: string[]) =>
-    insightStatementColumnsLocalSetting.set(value.join(",")),
-  setTimeScale: setGlobalTimeScaleAction,
-};
-
-type StateProps = {
-  transactionInsightsViewStateProps: TransactionInsightsViewStateProps;
-  statementInsightsViewStateProps: StatementInsightsViewStateProps;
-};
-
-type DispatchProps = {
-  transactionInsightsViewDispatchProps: TransactionInsightsViewDispatchProps;
-  statementInsightsViewDispatchProps: StatementInsightsViewDispatchProps;
-};
-
-const WorkloadInsightsPage = withRouter(
-  connect<
-    StateProps,
-    DispatchProps,
-    RouteComponentProps,
-    WorkloadInsightsViewProps,
-    AdminUIState
-  >(
-    (state: AdminUIState, props: RouteComponentProps) => ({
-      transactionInsightsViewStateProps: transactionMapStateToProps(
-        state,
-        props,
-      ),
-      statementInsightsViewStateProps: statementMapStateToProps(state, props),
-    }),
-    dispatch => ({
-      transactionInsightsViewDispatchProps: bindActionCreators(
-        TransactionDispatchProps,
-        dispatch,
-      ),
-      statementInsightsViewDispatchProps: bindActionCreators(
-        StatementDispatchProps,
-        dispatch,
-      ),
-    }),
-    (stateProps, dispatchProps) => ({
-      transactionInsightsViewProps: {
-        ...stateProps.transactionInsightsViewStateProps,
-        ...dispatchProps.transactionInsightsViewDispatchProps,
-      },
-      statementInsightsViewProps: {
-        ...stateProps.statementInsightsViewStateProps,
-        ...dispatchProps.statementInsightsViewDispatchProps,
-      },
-    }),
-  )(WorkloadInsightsRootControl),
+const sortSettingLocalSetting = new LocalSetting<AdminUIState, SortSetting>(
+  "sortSetting/InsightsPage",
+  (state: AdminUIState) => state.localSettings,
+  {
+    ascending: false,
+    columnTitle: "startTime",
+  },
 );
+
+const selectInsightTypes = (): string[] => {
+  const insights: string[] = [];
+  InsightEnumToLabel.forEach(insight => {
+    insights.push(insight);
+  });
+  return insights;
+};
+
+const WorkloadInsightsPage: React.FC = () => {
+  const dispatch = useDispatch();
+  const timeScale = useSelector(selectTimeScale);
+  const filters = useSelector(filtersLocalSetting.selector);
+  const sortSetting = useSelector(sortSettingLocalSetting.selector);
+  const selectedColumnNames = useSelector(
+    insightStatementColumnsLocalSetting.selectorToArray,
+  );
+
+  const onFiltersChange = useCallback(
+    (f: WorkloadInsightEventFilters) => {
+      dispatch(filtersLocalSetting.set(f));
+    },
+    [dispatch],
+  );
+
+  const onSortChange = useCallback(
+    (ss: SortSetting) => {
+      dispatch(sortSettingLocalSetting.set(ss));
+    },
+    [dispatch],
+  );
+
+  const setTimeScale = useCallback(
+    (ts: TimeScale) => {
+      dispatch(setGlobalTimeScaleAction(ts));
+    },
+    [dispatch],
+  );
+
+  const onColumnsChange = useCallback(
+    (value: string[]) => {
+      dispatch(insightStatementColumnsLocalSetting.set(value.join(",")));
+    },
+    [dispatch],
+  );
+
+  const insightTypes = selectInsightTypes();
+
+  return (
+    <WorkloadInsightsRootControl
+      transactionInsightsViewProps={{
+        insightTypes,
+        filters,
+        sortSetting,
+        timeScale,
+        onFiltersChange,
+        onSortChange,
+        setTimeScale,
+      }}
+      statementInsightsViewProps={{
+        insightTypes,
+        filters,
+        sortSetting,
+        selectedColumnNames,
+        timeScale,
+        onFiltersChange,
+        onSortChange,
+        onColumnsChange,
+        setTimeScale,
+      }}
+    />
+  );
+};
 
 export default WorkloadInsightsPage;
