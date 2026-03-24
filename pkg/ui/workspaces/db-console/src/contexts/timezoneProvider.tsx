2c96685c8bc2b89cd02114e5fd986dc9400bdb7d
Kyle Wong <37189875+kyle-a-wong@users.noreply.github.com>
2026-03-24 11:43:10 -0400

    ui/db-console: migrate 5 consumers from refreshSettings to useClusterSettings
    Replace Redux-based refreshSettings dispatching and cachedData selectors
    with the new useClusterSettings SWR hook in all React component consumers:
    
    - TimezoneProvider: reads ui.default_timezone and ui.display_timezone
    - MetricsDataProvider: reads timeseries storage TTL settings for
      resolution computation
    - NodeGraphs: reads timeseries storage TTL settings
    - KeyVisualizerPage: reads keyvisualizer.enabled and sample_interval
    - Settings report page: renders all cluster settings; removes
      connect()/mapStateToProps/mapDispatchToProps entirely
    
    The alertDataProvider in alerts.ts still uses refreshSettings because it
    is a Redux store subscriber (not a React component) and cannot use hooks.
    The settingsReducerObj and its two consumed selectors
    (selectClusterSettings, selectClusterSettingVersion) are retained for
    this reason.
    
    Remove 7 now-unused Redux selectors from clusterSettings.selectors.ts:
    selectTimezoneSetting, selectResolution10sStorageTTL,
    selectResolution30mStorageTTL, selectAutomaticStatsCollectionEnabled,
    selectIndexRecommendationsEnabled, selectIndexUsageStatsEnabled, and
    selectDropUnusedIndexDuration.
    
    Update MetricsDataProvider tests to mock useClusterSettings and provide
    metricsTime-shaped data matching the refactored component internals.
    
    Epic: CRDB-58145
    Release note: None

--- pkg/ui/workspaces/db-console/src/contexts/timezoneProvider.tsx
+++ pkg/ui/workspaces/db-console/src/contexts/timezoneProvider.tsx
@@ -3,26 +3,21 @@
 // Use of this software is governed by the CockroachDB Software License
 // included in the /LICENSE file.
 
-import { TimezoneContext } from "@cockroachlabs/cluster-ui";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-
-import { refreshSettings } from "src/redux/apiReducers";
 import {
-  selectClusterSettings,
-  selectTimezoneSetting,
-} from "src/redux/clusterSettings";
+  TimezoneContext,
+  CoordinatedUniversalTime,
+  useClusterSettings,
+} from "@cockroachlabs/cluster-ui";
+import React from "react";
 
 export const TimezoneProvider = (props: any) => {
-  // Refresh cluster settings if needed.
-  const dispatch = useDispatch();
-  const settings = useSelector(selectClusterSettings);
-  useEffect(() => {
-    dispatch(refreshSettings());
-  }, [settings, dispatch]);
-
-  // Grab the timezone value from the store, and pass it to our context.
-  const timezone = useSelector(selectTimezoneSetting);
+  const { settingValues } = useClusterSettings({
+    names: ["ui.default_timezone", "ui.display_timezone"],
+  });
+  const timezone =
+    settingValues["ui.default_timezone"]?.value ||
+    settingValues["ui.display_timezone"]?.value ||
+    CoordinatedUniversalTime;
   return (
     <TimezoneContext.Provider value={timezone}>
       {props.children}
--- pkg/ui/workspaces/db-console/src/redux/clusterSettings/clusterSettings.selectors.ts
+++ pkg/ui/workspaces/db-console/src/redux/clusterSettings/clusterSettings.selectors.ts
@@ -3,76 +3,15 @@
 // Use of this software is governed by the CockroachDB Software License
 // included in the /LICENSE file.
 
-import { CoordinatedUniversalTime, util } from "@cockroachlabs/cluster-ui";
-import moment from "moment-timezone";
 import { createSelector } from "reselect";
 
 import { cockroach } from "src/js/protos";
 import { AdminUIState } from "src/redux/state";
-import { indexUnusedDuration } from "src/util/constants";
 
 export const selectClusterSettings = createSelector(
   (state: AdminUIState) => state.cachedData.settings?.data,
   (settings: cockroach.server.serverpb.SettingsResponse) =>
     settings?.key_values,
-);
-
-export const selectTimezoneSetting = createSelector(
-  selectClusterSettings,
-  settings => {
-    if (!settings) {
-      return CoordinatedUniversalTime;
-    }
-    return (
-      settings["ui.default_timezone"]?.value ||
-      settings["ui.display_timezone"]?.value ||
-      CoordinatedUniversalTime
-    );
-  },
-);
-
-export const selectResolution10sStorageTTL = createSelector(
-  selectClusterSettings,
-  (settings): moment.Duration | undefined => {
-    if (!settings) {
-      return undefined;
-    }
-    const value = settings["timeseries.storage.resolution_10s.ttl"]?.value;
-    return util.durationFromISO8601String(value);
-  },
-);
-
-export const selectResolution30mStorageTTL = createSelector(
-  selectClusterSettings,
-  settings => {
-    if (!settings) {
-      return undefined;
-    }
-    const value = settings["timeseries.storage.resolution_30m.ttl"]?.value;
-    return util.durationFromISO8601String(value);
-  },
-);
-
-export const selectAutomaticStatsCollectionEnabled = createSelector(
-  selectClusterSettings,
-  (settings): boolean | undefined => {
-    if (!settings) {
-      return undefined;
-    }
-    const value = settings["sql.stats.automatic_collection.enabled"]?.value;
-    return value === "true";
-  },
-);
-
-export const selectIndexRecommendationsEnabled = createSelector(
-  selectClusterSettings,
-  (settings): boolean => {
-    if (!settings) {
-      return false;
-    }
-    const value = settings["version"]?.value || "";
-    return util.greaterOrEqualThanVersion(value, [22, 2, 0]);
-  },
 );
 
 export const selectClusterSettingVersion = createSelector(
@@ -82,29 +21,5 @@ export const selectClusterSettingVersion = createSelector(
       return "";
     }
     return settings["version"]?.value ?? "";
-  },
-);
-
-export const selectIndexUsageStatsEnabled = createSelector(
-  selectClusterSettings,
-  (settings): boolean => {
-    if (!settings) {
-      return false;
-    }
-    const value = settings["version"]?.value || "";
-    return util.greaterOrEqualThanVersion(value, [22, 1, 0]);
-  },
-);
-
-export const selectDropUnusedIndexDuration = createSelector(
-  selectClusterSettings,
-  (settings): string => {
-    if (!settings) {
-      return indexUnusedDuration;
-    }
-    return (
-      settings["sql.index_recommendation.drop_unused_duration"]?.value ||
-      indexUnusedDuration
-    );
   },
 );
