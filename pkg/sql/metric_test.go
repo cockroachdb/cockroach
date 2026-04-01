cockroachdb/cockroach

commit 03c09a32fa11f5927253669aaba678c7f7c11352

sql: add txn rows written/read guardrails
This commit introduces the guardrails on the number of rows written/read
by a single txn. The limits are enforced after each statement of a txn
has been fully executed (i.e. we don't proactively cancel work in the
middle of the execution if the txn has just reached the limits). This is
done in the connExecutor since it is a very convenient place to enforce
the limits so that they apply only to the desired statements. Notably,
things for which we don't want the application of the limits (things
like BACKUP, IMPORT, CREATE STATISTICS, etc) don't go through the
connExecutor and, thus, aren't affected.

The accumulation of the number of rows read by a txn has already been in
place, and this commit introduces the explicit collection of the number
of rows written via the same mechanism - by propagating "rows written"
metrics during the draining of the execution flow. Initially, we
considered using "rows affected" values, but those have a different
meaning from what we want. This metrics collection required teaching the
planNodeToRowSource adapter to ask the mutation planNodes for the number
of rows written.

Note that in many cases, the internal executor doesn't have the
sessionData properly set (i.e. the default values are used), so we'll
never log anything then. This seems acceptable since the focus of these
guardrails is on the externally initiated queries.

One notable change is because of our "after the fact" enforcement of
these limits, this commit disables the auto commit option for the
mutation statements in the execbuilder for non-internal statements if
`transaction_rows_read_err` guardrail is enabled.

Release note (ops change): New cluster settings
`sql.defaults.transaction_rows_written_log`,
`sql.defaults.transaction_rows_written_err`,
`sql.defaults.transaction_rows_read_log`, and
`sql.defaults.transaction_rows_read_err` (as well as the corresponding
session variables have been introduced. These settings determine the

pkg/sql/plan_node_to_row_source.go
@@ -91,7 +91,21 @@ func (p *planNodeToRowSource) InitWithOutput(
 		0, /* processorID */
 		output,
 		nil, /* memMonitor */
-		execinfra.ProcStateOpts{},
+		execinfra.ProcStateOpts{
+			TrailingMetaCallback: func() []execinfrapb.ProducerMetadata {
+				var meta []execinfrapb.ProducerMetadata
+				if p.InternalClose() {
+					// Check if we're wrapping a mutation and emit the rows
+					// written metric if so.
+					if m, ok := p.node.(mutationPlanNode); ok {
+						metrics := execinfrapb.GetMetricsMeta()
+						metrics.RowsWritten = m.rowsWritten()
+						meta = []execinfrapb.ProducerMetadata{{Metrics: metrics}}
+					}
+				}
+				return meta
+			},
+		},
 	)
 }
 

docs/generated/settings/settings.html
@@ -114,6 +114,10 @@
 <tr><td><code>sql.defaults.serial_normalization</code></td><td>enumeration</td><td><code>rowid</code></td><td>default handling of SERIAL in table definitions [rowid = 0, virtual_sequence = 1, sql_sequence = 2, sql_sequence_cached = 3]</td></tr>
 <tr><td><code>sql.defaults.statement_timeout</code></td><td>duration</td><td><code>0s</code></td><td>default value for the statement_timeout; default value for the statement_timeout session setting; controls the duration a query is permitted to run before it is canceled; if set to 0, there is no timeout</td></tr>
 <tr><td><code>sql.defaults.stub_catalog_tables.enabled</code></td><td>boolean</td><td><code>true</code></td><td>default value for stub_catalog_tables session setting</td></tr>
+<tr><td><code>sql.defaults.transaction_rows_read_err</code></td><td>integer</td><td><code>0</code></td><td>the limit for the number of rows read by a SQL transaction which - once reached - will fail the transaction (or will trigger a logging event to SQL_INTERNAL_PERF for internal transactions); use 0 to disable</td></tr>
+<tr><td><code>sql.defaults.transaction_rows_read_log</code></td><td>integer</td><td><code>0</code></td><td>the threshold for the number of rows read by a SQL transaction which - once reached - will trigger a logging event to SQL_PERF (or SQL_INTERNAL_PERF for internal transactions); use 0 to disable</td></tr>
+<tr><td><code>sql.defaults.transaction_rows_written_err</code></td><td>integer</td><td><code>0</code></td><td>the limit for the number of rows written by a SQL transaction which - once reached - will fail the transaction (or will trigger a logging event to SQL_INTERNAL_PERF for internal transactions); use 0 to disable</td></tr>
+<tr><td><code>sql.defaults.transaction_rows_written_log</code></td><td>integer</td><td><code>0</code></td><td>the threshold for the number of rows written by a SQL transaction which - once reached - will trigger a logging event to SQL_PERF (or SQL_INTERNAL_PERF for internal transactions); use 0 to disable</td></tr>
 <tr><td><code>sql.defaults.vectorize</code></td><td>enumeration</td><td><code>on</code></td><td>default vectorize mode [on = 0, on = 2, experimental_always = 3, off = 4]</td></tr>
 <tr><td><code>sql.defaults.zigzag_join.enabled</code></td><td>boolean</td><td><code>true</code></td><td>default value for enable_zigzag_join session setting; allows use of zig-zag join by default</td></tr>
 <tr><td><code>sql.distsql.max_running_flows</code></td><td>integer</td><td><code>500</code></td><td>maximum number of concurrent flows that can be run on a node</td></tr>

pkg/sql/conn_executor.go
@@ -305,6 +305,10 @@ type Metrics struct {
 
 	// StatsMetrics contains metrics for SQL statistics collection.
 	StatsMetrics StatsMetrics
+
+	// GuardrailMetrics contains metrics related to different guardrails in the
+	// SQL layer.
+	GuardrailMetrics GuardrailMetrics
 }
 
 // NewServer creates a new Server. Start() needs to be called before the Server
@@ -415,6 +419,12 @@ func makeMetrics(cfg *ExecutorConfig, internal bool) Metrics {
 				getMetricMeta(MetaSQLStatsFlushDuration, internal), 6*metricsSampleInterval,
 			),
 			SQLStatsRemovedRows: metric.NewCounter(getMetricMeta(MetaSQLStatsRemovedRows, internal)),
+		},
+		GuardrailMetrics: GuardrailMetrics{
+			TxnRowsWrittenLogCount: metric.NewCounter(getMetricMeta(MetaTxnRowsWrittenLog, internal)),
+			TxnRowsWrittenErrCount: metric.NewCounter(getMetricMeta(MetaTxnRowsWrittenErr, internal)),
+			TxnRowsReadLogCount:    metric.NewCounter(getMetricMeta(MetaTxnRowsReadLog, internal)),
+			TxnRowsReadErrCount:    metric.NewCounter(getMetricMeta(MetaTxnRowsReadErr, internal)),
 		},
 	}
 }
@@ -1223,6 +1233,16 @@ type connExecutor struct {
 		// QueryLevelStats which are sampled.
 		rowsRead  int64
 		bytesRead int64
+
+		// rowsWritten tracks the number of rows written (modified) by all
+		// statements in this txn so far.
+		rowsWritten int64
+
+		// rowsWrittenLogged and rowsReadLogged indicates whether we have
+		// already logged an event about reaching written/read rows setting,
+		// respectively.
+		rowsWrittenLogged bool
+		rowsReadLogged    bool
 
 		// hasAdminRole is used to cache if the user running the transaction
 		// has admin privilege. hasAdminRoleCache is set for the first statement

pkg/sql/conn_executor_exec.go
@@ -47,6 +47,8 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/fsm"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
+	"github.com/cockroachdb/cockroach/pkg/util/log/eventpb"
+	"github.com/cockroachdb/cockroach/pkg/util/metric"
 	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
 	"github.com/cockroachdb/cockroach/pkg/util/tracing"
 	"github.com/cockroachdb/errors"
@@ -1020,6 +1022,7 @@ func (ex *connExecutor) dispatchToExecutionEngine(
 
 	ex.extraTxnState.rowsRead += stats.rowsRead
 	ex.extraTxnState.bytesRead += stats.bytesRead
+	ex.extraTxnState.rowsWritten += stats.rowsWritten
 
 	// Record the statement summary. This also closes the plan if the
 	// plan has not been closed earlier.
@@ -1031,7 +1034,130 @@ func (ex *connExecutor) dispatchToExecutionEngine(
 		ex.server.cfg.TestingKnobs.AfterExecute(ctx, stmt.String(), res.Err())
 	}
 
+	if limitsErr := ex.handleTxnRowsWrittenReadLimits(ctx); limitsErr != nil && res.Err() == nil {
+		res.SetError(limitsErr)
+	}
+
 	return err
+}
+
+// handleTxnRowsGuardrails handles either "written" or "read" rows guardrails.
+func (ex *connExecutor) handleTxnRowsGuardrails(
+	ctx context.Context,
+	numRows, logLimit, errLimit int64,
+	alreadyLogged *bool,
+	isRead bool,
+	logCounter, errCounter *metric.Counter,
+) error {
+	var err error
+	shouldLog := logLimit != 0 && numRows >= logLimit
+	shouldErr := errLimit != 0 && numRows >= errLimit
+	if !shouldLog && !shouldErr {
+		return nil
+	}
+	commonTxnRowsLimitDetails := eventpb.CommonTxnRowsLimitDetails{
+		TxnID:     ex.state.mu.txn.ID().String(),
+		SessionID: ex.sessionID.String(),
+		// Limit will be set below.
+		ViolatesTxnRowsLimitErr: shouldErr,
+		IsRead:                  isRead,
+	}
+	if shouldErr && ex.executorType == executorTypeInternal {
+		// Internal work should never err and always log if violating either
+		// limit.
+		shouldErr = false
+		if !shouldLog {
+			shouldLog = true
+			logLimit = errLimit
+		}
+	}
+	if *alreadyLogged {
+		// We have already logged this kind of event about this transaction.
+		if shouldErr {
+			// But this time we also reached the error limit, so we want to log
+			// an event again (it will have ViolatesTxnRowsLimitErr set to
+			// true). Note that we couldn't have reached the error limit when we
+			// logged the event the previous time because that would have
+			// aborted the execution of the transaction.
+			shouldLog = true
+			logLimit = errLimit
+		} else {
+			shouldLog = false
+		}
+	} else {
+		*alreadyLogged = shouldLog
+	}
+	if shouldLog {
+		commonSQLEventDetails := ex.planner.getCommonSQLEventDetails()
+		commonTxnRowsLimitDetails.Limit = logLimit
+		var event eventpb.EventPayload
+		if ex.executorType == executorTypeInternal {
+			if isRead {
+				event = &eventpb.TxnRowsReadLimitInternal{
+					CommonSQLEventDetails:     commonSQLEventDetails,
+					CommonTxnRowsLimitDetails: commonTxnRowsLimitDetails,
+				}
+			} else {
+				event = &eventpb.TxnRowsWrittenLimitInternal{
+					CommonSQLEventDetails:     commonSQLEventDetails,
+					CommonTxnRowsLimitDetails: commonTxnRowsLimitDetails,
+				}
+			}
+		} else {
+			if isRead {
+				event = &eventpb.TxnRowsReadLimit{
+					CommonSQLEventDetails:     commonSQLEventDetails,
+					CommonTxnRowsLimitDetails: commonTxnRowsLimitDetails,
+				}
+			} else {
+				event = &eventpb.TxnRowsWrittenLimit{
+					CommonSQLEventDetails:     commonSQLEventDetails,
+					CommonTxnRowsLimitDetails: commonTxnRowsLimitDetails,
+				}
+			}
+			log.StructuredEvent(ctx, event)
+			logCounter.Inc(1)
+		}
+	}
+	if shouldErr {
+		commonTxnRowsLimitDetails.Limit = errLimit
+		err = pgerror.WithCandidateCode(&commonTxnRowsLimitDetails, pgcode.ProgramLimitExceeded)
+		errCounter.Inc(1)
+	}
+	return err
+}
+
+// handleTxnRowsWrittenReadLimits checks whether the current transaction has
+// reached the limits on the number of rows written/read and logs the
+// corresponding event or returns an error. It should be called after executing
+// a single statement.
+func (ex *connExecutor) handleTxnRowsWrittenReadLimits(ctx context.Context) error {
+	// Note that in many cases, the internal executor doesn't have the
+	// sessionData properly set (i.e. the default values are used), so we'll
+	// never log anything then. This seems acceptable since the focus of these
+	// guardrails is on the externally initiated queries.
+	sd := ex.sessionData()
+	writtenErr := ex.handleTxnRowsGuardrails(
+		ctx,
+		ex.extraTxnState.rowsWritten,
+		sd.TxnRowsWrittenLog,
+		sd.TxnRowsWrittenErr,
+		&ex.extraTxnState.rowsWrittenLogged,
+		false, /* isRead */
+		ex.metrics.GuardrailMetrics.TxnRowsWrittenLogCount,
+		ex.metrics.GuardrailMetrics.TxnRowsWrittenErrCount,
+	)
+	readErr := ex.handleTxnRowsGuardrails(
+		ctx,
+		ex.extraTxnState.rowsRead,
+		sd.TxnRowsReadLog,
+		sd.TxnRowsReadErr,
+		&ex.extraTxnState.rowsReadLogged,
+		true, /* isRead */
+		ex.metrics.GuardrailMetrics.TxnRowsReadLogCount,
+		ex.metrics.GuardrailMetrics.TxnRowsReadErrCount,
+	)
+	return errors.CombineErrors(writtenErr, readErr)
 }
 
 // makeExecPlan creates an execution plan and populates planner.curPlan using
@@ -1074,6 +1200,8 @@ type topLevelQueryStats struct {
 	bytesRead int64
 	// rowsRead is the number of rows read from disk.
 	rowsRead int64
+	// rowsWritten is the number of rows written.
+	rowsWritten int64
 }
 
 // execWithDistSQLEngine converts a plan to a distributed SQL physical plan and
@@ -1652,6 +1780,9 @@ func (ex *connExecutor) recordTransactionStart() (
 	ex.extraTxnState.accumulatedStats = execstats.QueryLevelStats{}
 	ex.extraTxnState.rowsRead = 0
 	ex.extraTxnState.bytesRead = 0
+	ex.extraTxnState.rowsWritten = 0
+	ex.extraTxnState.rowsWrittenLogged = false
+	ex.extraTxnState.rowsReadLogged = false
 	if txnExecStatsSampleRate := collectTxnStatsSampleRate.Get(&ex.server.GetExecutorConfig().Settings.SV); txnExecStatsSampleRate > 0 {
 		ex.extraTxnState.shouldCollectTxnExecutionStats = txnExecStatsSampleRate > ex.rng.Float64()
 	}
@@ -1678,6 +1809,7 @@ func (ex *connExecutor) recordTransactionStart() (
 		ex.extraTxnState.accumulatedStats = execstats.QueryLevelStats{}
 		ex.extraTxnState.rowsRead = 0
 		ex.extraTxnState.bytesRead = 0
+		ex.extraTxnState.rowsWritten = 0
 
 		if ex.server.cfg.TestingKnobs.BeforeRestart != nil {
 			ex.server.cfg.TestingKnobs.BeforeRestart(ex.Ctx(), ex.extraTxnState.autoRetryReason)

pkg/sql/create_stats.go
@@ -610,12 +610,12 @@ func (r *createStatsResumer) Resume(ctx context.Context, execCtx interface{}) er
 			evalCtx.ExecCfg, txn,
 			0, /* depth: use event_log=2 for vmodule filtering */
 			eventLogOptions{dst: LogEverywhere},
-			sqlEventCommonExecPayload{
-				user:         evalCtx.SessionData().User(),
-				appName:      evalCtx.SessionData().ApplicationName,
-				stmt:         redact.Sprint(details.Statement),
-				stmtTag:      "CREATE STATISTICS",
-				placeholders: nil, /* no placeholders known at this point */
+			eventpb.CommonSQLEventDetails{
+				Statement:         redact.Sprint(details.Statement),
+				Tag:               "CREATE STATISTICS",
+				User:              evalCtx.SessionData().User().Normalized(),
+				ApplicationName:   evalCtx.SessionData().ApplicationName,
+				PlaceholderValues: []string{}, /* no placeholders known at this point */
 			},
 			eventLogEntry{
 				targetID: int32(details.Table.ID),

pkg/sql/delete.go
@@ -62,6 +62,8 @@ type deleteRun struct {
 	rowIdxToRetIdx []int
 }
 
+var _ mutationPlanNode = &deleteNode{}
+
 func (d *deleteNode) startExec(params runParams) error {
 	// cache traceKV during execution, to avoid re-evaluating it for every row.
 	d.run.traceKV = params.p.ExtendedEvalContext().Tracing.KVTracingEnabled()
@@ -132,6 +134,7 @@ func (d *deleteNode) BatchedNext(params runParams) (bool, error) {
 	}
 
 	if lastBatch {
+		d.run.td.setRowsWrittenLimit(params.extendedEvalCtx.SessionData())
 		if err := d.run.td.finalize(params.ctx); err != nil {
 			return false, err
 		}
@@ -207,6 +210,10 @@ func (d *deleteNode) Close(ctx context.Context) {
 	d.run.td.close(ctx)
 	*d = deleteNode{}
 	deleteNodePool.Put(d)
+}
+
+func (d *deleteNode) rowsWritten() int64 {
+	return d.run.td.rowsWritten
 }
 
 func (d *deleteNode) enableAutoCommit() {

pkg/sql/delete_range.go
@@ -61,6 +61,7 @@ type deleteRangeNode struct {
 var _ planNode = &deleteRangeNode{}
 var _ planNodeFastPath = &deleteRangeNode{}
 var _ batchedPlanNode = &deleteRangeNode{}
+var _ mutationPlanNode = &deleteRangeNode{}
 
 // BatchedNext implements the batchedPlanNode interface.
 func (d *deleteRangeNode) BatchedNext(params runParams) (bool, error) {
@@ -80,6 +81,10 @@ func (d *deleteRangeNode) BatchedValues(rowIdx int) tree.Datums {
 // FastPathResults implements the planNodeFastPath interface.
 func (d *deleteRangeNode) FastPathResults() (int, bool) {
 	return d.rowCount, true
+}
+
+func (d *deleteRangeNode) rowsWritten() int64 {
+	return int64(d.rowCount)
 }
 
 // startExec implements the planNode interface.

pkg/sql/distsql_running.go
@@ -886,6 +886,7 @@ func (r *DistSQLReceiver) pushMeta(meta *execinfrapb.ProducerMetadata) execinfra
 	if meta.Metrics != nil {
 		r.stats.bytesRead += meta.Metrics.BytesRead
 		r.stats.rowsRead += meta.Metrics.RowsRead
+		r.stats.rowsWritten += meta.Metrics.RowsWritten
 		if r.progressAtomic != nil && r.expectedRowsRead != 0 {
 			progress := float64(r.stats.rowsRead) / float64(r.expectedRowsRead)
 			atomic.StoreUint64(r.progressAtomic, math.Float64bits(progress))

commit 0b4ac3107fe780f3177cc12e69d57b8bcd8790f2

sql: fix "rows written" metrics
This commit fixes a bug that was introduced long time ago in
03c09a32fa11f5927253669aaba678c7f7c11352. In particular, in that change
we began tracking "rows written" for the purposes of the guardrails, and
later this information was also added into statement statistics as well
as extended to some metrics.

The bug was that we only check the first wrapped `planNode` for being
a `mutationPlanNode`, but it's actually possible for a single
`planNodeToRowSource` wrapper to wrap multiple `planNode`s at once. For
example, if we have a mutation in the CTE, then we could have
`bufferNode -> insertNode -> valuesNode`, all handled by a single
wrapper. Previously, we would only check `bufferNode`, and since it's
not a mutation planNode, we'd emit no "written" metrics.

This commit fixes the issue by traversing the planNode tree until we
either reach the first not-wrapped planNode (i.e. it's handled by
DistSQL separately) or reach the leaf nodes.

Note that this bug was further hidden by the fact that if we collect
execution stats (which we do for stmt bundles and when the txn is
sampled), then we wrap each planNode separately, so in those scenarios
the behavior was correct.

Release note (bug fix): Previously, if a mutation was executed in
a subquery (e.g. as a CTE), the "rows written" metrics like
`sql.statements.index_rows_written.count` and
`sql.statements.index_bytes_written.count` might not be incremented
correctly. This is now fixed.

pkg/sql/plan_node_to_row_source.go
@@ -253,19 +253,42 @@ func (p *planNodeToRowSource) forwardMetadata(metadata *execinfrapb.ProducerMeta
 }
 
 func (p *planNodeToRowSource) trailingMetaCallback() []execinfrapb.ProducerMetadata {
+	if !p.InternalClose() {
+		return nil
+	}
 	var meta []execinfrapb.ProducerMetadata
-	if p.InternalClose() {
-		// Check if we're wrapping a mutation and emit the rows written metric
-		// if so.
-		if m, ok := p.node.(mutationPlanNode); ok {
-			metrics := execinfrapb.GetMetricsMeta()
-			metrics.RowsWritten = m.rowsWritten()
-			metrics.IndexRowsWritten = m.indexRowsWritten()
-			metrics.IndexBytesWritten = m.indexBytesWritten()
-			metrics.KVCPUTime = m.kvCPUTime()
-			meta = []execinfrapb.ProducerMetadata{{Metrics: metrics}}
+	// Check if we're wrapping a mutation and emit the rows written metric if
+	// so.
+	maybeEmitMeta := func(node planNode) {
+		m, ok := node.(mutationPlanNode)
+		if !ok {
+			return
+		}
+		metrics := execinfrapb.GetMetricsMeta()
+		metrics.RowsWritten = m.rowsWritten()
+		metrics.IndexRowsWritten = m.indexRowsWritten()
+		metrics.IndexBytesWritten = m.indexBytesWritten()
+		metrics.KVCPUTime = m.kvCPUTime()
+		meta = append(meta, execinfrapb.ProducerMetadata{Metrics: metrics})
+	}
+	// Note that we could be wrapping multiple planNodes at once, so we have to
+	// traverse the tree until we either reach the first DistSQL-plannable
+	// planNode or the leaf.
+	var visitNode func(node planNode)
+	visitNode = func(node planNode) {
+		if node == p.firstNotWrapped {
+			return
+		}
+		maybeEmitMeta(node)
+		for i, n := 0, node.InputCount(); i < n; i++ {
+			child, err := node.Input(i)
+			if err != nil {
+				continue
+			}
+			visitNode(child)
 		}
 	}
+	visitNode(p.node)
 	return meta
 }
 

pkg/sql/metric_test.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/metamorphic"
 	"github.com/cockroachdb/cockroach/pkg/util/metric"
+	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 	"github.com/stretchr/testify/require"
 )
 
@@ -214,6 +215,7 @@ func TestStatementMetrics(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 
+	rng, _ := randutil.NewTestRand()
 	srv, sqlDB, _ := serverutils.StartServer(t, base.TestServerArgs{})
 	defer srv.Stopper().Stop(context.Background())
 	runner := sqlutils.MakeSQLRunner(sqlDB)
@@ -256,8 +258,14 @@ func TestStatementMetrics(t *testing.T) {
 		// Expect to write 5 rows - update primary index, delete and insert two
 		// rows in "y" secondary index.
 		{query: `UPDATE db.t SET y = 100 WHERE y >= 10 AND y <= 20`, rowsRead: 4, indexRowsWritten: 6},
+		// Same scenario as above but the UPDATE is executed in a CTE.
 		{
-			query:            `UPSERT INTO db.t VALUES (6, 60, ARRAY['date'], '[100, 110]')`,
+			query:            `WITH cte (c) AS (UPDATE db.t SET y = 100 WHERE y >= 50 AND y <= 60 RETURNING x) SELECT c FROM cte`,
+			rowsRead:         4,
+			indexRowsWritten: 6,
+		},
+		{
+			query:            `UPSERT INTO db.t VALUES (6, 100, ARRAY['date'], '[100, 110]')`,
 			rowsRead:         1,
 			indexRowsWritten: 3,
 		},
@@ -289,6 +297,13 @@ func TestStatementMetrics(t *testing.T) {
 			runner.Exec(t, `INSERT INTO db.t VALUES (1, 10, ARRAY['apple', 'orange'], '[1, 2]')`)
 			runner.Exec(t, `INSERT INTO db.t VALUES (2, 20, ARRAY['banana'], '[3, 4]')`)
 			runner.Exec(t, `INSERT INTO db.t VALUES (3, 30, ARRAY['apple', 'pear', 'mango'], '[5, 6]')`)
+
+			// Randomize whether we disable txn sampling or not (if it's
+			// disabled, then we don't collect execution stats, which could have
+			// an impact on the metrics' updates).
+			if rng.Float64() < 0.5 {
+				runner.Exec(t, `SET CLUSTER SETTING sql.txn_stats.sample_rate = 0`)
+			}
 
 			for _, tc := range testCases {
 				t.Run(tc.query, func(t *testing.T) {