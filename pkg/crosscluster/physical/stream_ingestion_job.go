2c5f23722debcff43b722167b4402f300de34f31
Andrew Thomas <andrew.thomas@cockroachlabs.com>
2026-04-09 23:11:52 -0400

    crosscluster/physical: fix ingest retry progress bug
    This patch fixes two bugs in `ingestWithRetries`.
    1. the resumer's job never has its in memory progress updated within a
       single resume, as the frontier processor writes progress updates
       directly to the db via job id.
    2. `previousPersistedSpans` is never updated, which, in combination with
       the above, means, unless the job is resumed more than once, the
       branch to reset the retrier is never taken, as they are both always
       zero-values.
    
    The fix is to refresh the resumer's job and update the previous value
    each loop iteration.
    
    Fixes: #167384
    
    Release note: None

--- pkg/crosscluster/physical/stream_ingestion_job.go
+++ pkg/crosscluster/physical/stream_ingestion_job.go
@@ -413,6 +413,19 @@ func ingestWithRetries(
 		}
 		log.Dev.Infof(ctx, "hit retryable error %s", err)
 
+		// Reload the job's in-memory progress from the database so that we see accurate progress.
+		reloadErr := execCtx.ExecCfg().InternalDB.Txn(ctx, func(ctx context.Context, t isql.Txn) error {
+			job, err := execCtx.ExecCfg().JobRegistry.LoadClaimedJobWithTxn(ctx, resumer.job.ID(), t)
+			if err != nil {
+				return err
+			}
+			resumer.job = job
+			return nil
+		})
+		if reloadErr != nil {
+			log.Dev.Warningf(ctx, "error loading job progress: %v", reloadErr)
+		}
+
 		currentPersistedSpans = resumer.job.Progress().Details.(*jobspb.Progress_StreamIngest).StreamIngest.Checkpoint.ResolvedSpans
 		if !currentPersistedSpans.Equal(previousPersistedSpans) {
 			// If the previous persisted spans are different than the current, it
@@ -420,6 +433,8 @@ func ingestWithRetries(
 			r.Reset()
 			log.Dev.Infof(ctx, "resolved spans have advanced since last retry, resetting retry counter")
 		}
+		previousPersistedSpans = currentPersistedSpans
+
 		if knobs := execCtx.ExecCfg().StreamingTestingKnobs; knobs != nil && knobs.AfterRetryIteration != nil {
 			knobs.AfterRetryIteration(err)
 		}
