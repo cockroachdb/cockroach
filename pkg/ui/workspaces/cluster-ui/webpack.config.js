d31ed4ed3ea38dbecab5a9ff9c9b0408402c17d3
Kyle Wong <37189875+kyle-a-wong@users.noreply.github.com>
2026-04-13 10:44:34 -0400

    cluster-ui: only inline assets for npm release builds
    Previously, cluster-ui's webpack config always used `asset/inline` for
    images and fonts, which base64-encodes all assets into the bundle. This
    slows down the db-console build since it consumes the larger bundle.
    
    This change makes asset inlining opt-in via a `--env inline-assets`
    webpack flag. The default `build` script now uses `asset` (letting
    webpack decide based on size), while a new `build:release` script
    passes the flag to produce a self-contained npm package. The GitHub
    Actions publish workflows are updated to use `build:release`.
    
    Epic: none
    Release note: None

--- pkg/ui/workspaces/cluster-ui/webpack.config.js
+++ pkg/ui/workspaces/cluster-ui/webpack.config.js
@@ -57,7 +57,7 @@ module.exports = (env, argv) => {
       rules: [
         {
           test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-          type: "asset/inline",
+          type: env["inline-assets"] ? "asset/inline" : "asset",
           exclude: /node_modules/,
         },
         // Styles in current project use SCSS preprocessing language with CSS modules.
