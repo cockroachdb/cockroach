fcf78b52cbaad4fa4393c79273d4f6a6da73eae6
Silvano Ravotto <silvano@cockroachlabs.com>
2026-04-21 19:17:45 +0200

    generate-import-fixtures: add TPC-H table schemas and format interfaces
    Introduce the `generate-import-fixtures` tool skeleton with TPC-H table
    schema definitions (all 8 tables including region and nation) and the
    OutputFormat/FormatWriter interfaces that format implementations will
    satisfy.
    
    Part of: #164461
    Release note: None
    Epic: none
    Co-authored-by: Matt White <75503202+mw5h@users.noreply.github.com>

---
+++ pkg/cmd/generate-import-fixtures/main.go
@@ -0,0 +1,10 @@
+// Copyright 2026 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+// generate-import-fixtures converts TPC-H CSV fixtures into other file formats
+// (Avro, Parquet) for use in IMPORT roachtests.
+package main
+
+func main() {}
