b460a4e04f8a29bb1c0108ebba6d557536263324
Brendan Gerrity <brendan.gerrity@cockroachlabs.com>
2026-05-08 14:45:06 -0400

    sql: add type-specific error for dropped descriptors
    The generic dropping descriptor error is clumsy. This change adds
    type-specific details so testing code can expect a particularly labeled
    error.
    
    Epic: none
    Part of: #169638
    
    Release note: None

--- pkg/sql/catalog/descriptor.go
+++ pkg/sql/catalog/descriptor.go
@@ -1101,9 +1101,8 @@ type FunctionDescriptor interface {
 func FilterDroppedDescriptor(desc Descriptor) error {
 	if !desc.Dropped() {
 		return nil
-
 	}
-	return NewInactiveDescriptorError(ErrDescriptorDropped)
+	return NewInactiveDescriptorError(NewDescriptorDroppedError(desc))
 }
 
 // FilterOfflineDescriptor returns an error if the descriptor state is OFFLINE.
