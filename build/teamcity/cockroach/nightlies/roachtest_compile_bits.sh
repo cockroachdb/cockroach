179b66456565e83102062d5da51ccede4735ec13
Ludovic Leroux <ludo.leroux@cockroachlabs.com>
2026-05-12 17:24:02 -0400

    build/roachtest: cache builds to speed up roachtest
    Previously, both `cockroach` and `roachtest` binaries were cached in a
    GCS bucket at the end of the build on a release branch. While this
    allowed for reproduction of failures, it wasn't improving nightlies
    build time.
    
    To address this, this patch changes two things:
    1. introduction of an independent `roachtest_compile.sh` script that
       compiles and caches all components (binaries and libraries) to GCS
    2. cache hit checks in `roachtest_compile_component.sh` to check if the
       component already exists in cache
    
    The general idea is to change the dependencies of the roachtest nightly
    TeamCity builds so that `roachtest_compile.sh` is first called once per
    branch before each cloud provider's roachtest is triggered. This will
    cut down the compile time of each roachtest by an hour or so.
    
    The change also becomes available for adhoc TeamCity smoke tests when
    multiple runs are triggered without the commit SHA being changed (e.g.
    short test then longer test or rerun because of build parameters
    mistakes).
    
    Epic: none
    Release note: None

--- build/teamcity/cockroach/nightlies/roachtest_compile_bits.sh
+++ build/teamcity/cockroach/nightlies/roachtest_compile_bits.sh
@@ -13,6 +13,10 @@ set -euo pipefail
 
 source $root/build/teamcity/util/roachtest_arch_util.sh
 
+if [[ "${ROACHTEST_BUILD_CACHE:-}" == "true" ]]; then
+  gcs_setup_credentials
+fi
+
 if [ "$#" -eq 0 ]; then
   echo "Builds all bits needed for roachtests and stages them in bin/ and lib/."
   echo ""
