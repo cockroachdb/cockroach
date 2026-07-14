abba83b83327e698e6a99db14b3f60861ea07d4e
Kyle Li <kyle.li@cockroachlabs.com>
2026-07-14 12:01:23 -0400

    workload/fixture: deflake TestImportFixtureCSVServer
    TestImportFixtureCSVServer has the same structure and susceptibility to
    the flake in TestImportFixtureNodeCount, so the fix has been extended to
    this test as well.
    
    See also: cockroachdb/cockroach#172262
    Epic: none
    
    Release note: None

--- pkg/ccl/workloadccl/fixture_test.go
+++ pkg/ccl/workloadccl/fixture_test.go
@@ -239,9 +239,17 @@ func TestImportFixtureCSVServer(t *testing.T) {
 	ts := httptest.NewServer(workload.CSVMux(workload.Registered()))
 	defer ts.Close()
 
+	// Disable auto stats collection. This test is not for verifying stats, and the
+	// auto stats job can leak a goroutine if it is still running at teardown and
+	// get flagged as a failure by leaktest.AfterTest.
+	st := cluster.MakeTestingClusterSettings()
+	stats.AutomaticStatisticsClusterMode.Override(ctx, &st.SV, false)
+	stats.AutomaticStatisticsOnSystemTables.Override(ctx, &st.SV, false)
+
 	s, db, _ := serverutils.StartServer(t,
 		base.TestServerArgs{
 			UseDatabase: `d`,
+			Settings:    st,
 		},
 	)
 	defer s.Stopper().Stop(ctx)
